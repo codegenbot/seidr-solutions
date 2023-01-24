412c0b33e0dcd61255eff926e9cb456cd61599dd
BeepBoop <githubbot@vadim.me>
2023-01-24 22:24:46 +0100

    solution 1 of camel-case, 1 of validation tests passed

--- camel-case.cpp
+++ camel-case.cpp
@@ -36,25 +36,25 @@ all separate words
 */
 int main() {
     string s;
-    while (getline(cin, s)) {
-        string res = "";
-        bool flag = false;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == ' ') {
-                res += s[i];
-                flag = false;
-            } else if (s[i] == '-') {
-                flag = true;
-            } else {
-                if (flag) {
-                    res += toupper(s[i]);
-                    flag = false;
-                } else {
-                    res += s[i];
-                }
-            }
+    getline(cin, s);
+    string res = "";
+    bool flag = false;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '-') {
+            flag = true;
+            continue;
         }
-        cout << res << endl;
+        if (s[i] == ' ') {
+            res += ' ';
+            continue;
+        }
+        if (flag) {
+            res += toupper(s[i]);
+            flag = false;
+        } else {
+            res += s[i];
+        }
     }
+    cout << res << endl;
     return 0;
 }
