ea8a47846dd69c9ff139f081cbd3395a239d1560
BeepBoop <githubbot@vadim.me>
2023-01-25 00:23:07 +0100

    solution 1 of shopping-list, 0.82 of validation tests passed

--- shopping-list.cpp
+++ shopping-list.cpp
@@ -11,43 +11,7 @@
 #include <climits>
 using namespace std;
 /*
-Given a vector of ﬂoats representing the prices of various shopping goods and another vector of floats representing the percent discount of each of those goods, return the total price of the shopping trip after applying the discount to each item.
-For example,
-input:
-1
-50.0
-1
-100.0
-output:
-0.0
-input:
-1
-50.0
-1
-10.0
-output:
-45.0
-input:
-2
-20.0 20.0
-2
-100.0 50.0
-output:
-10.0
-input:
-2
-20.0 20.0
-2
-20.0 0.0
-output:
-36.0
-input:
-3
-10.0 20.0 30.0
-3
-5.0 10.0 95.0
-output:
-29.0
+Given a vector of ﬂoats representing the prices of various shopping goods and another vector of floats representing the percent discount of each of those goods, return the total price of the shopping trip after applying the discount to each item. For example, input: 1 50.0 1 100.0 output: 0.0 input: 1 50.0 1 10.0 output: 45.0 input: 2 20.0 20.0 2 100.0 50.0 output: 10.0 input: 2 20.0 20.0 2 20.0 0.0 output: 36.0 input: 3 10.0 20.0 30.0 3 5.0 10.0 95.0 output: 29.0
 */
 int main() {
     int n;
@@ -65,6 +29,6 @@ int main() {
     for (int i = 0; i < prices.size(); i++) {
         total += prices[i] * (1 - discounts[i] / 100);
     }
-    printf("%.1f\n", total);
+    printf("%.2f\n", total);
     return 0;
 }
