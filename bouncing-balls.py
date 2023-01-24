dae9bdf23b5a8388e7a916a9c6deacc8a612eeb1
BeepBoop <githubbot@vadim.me>
2023-01-24 23:34:42 +0100

    solution 2 of bouncing-balls, 0.05 of validation tests passed

--- bouncing-balls.py
+++ bouncing-balls.py
@@ -42,12 +42,14 @@ output:
 3.963
 """
 if __name__ == '__main__':
-    starting_height = float(input())
-    first_bounce = float(input())
-    num_bounces = int(input())
-    bounciness_index = first_bounce/starting_height
-    total_distance = 0
-    for i in range(num_bounces):
-        total_distance += starting_height
-        starting_height *= bounciness_index
-    print(total_distance)
+    start = float(input())
+    bounce = float(input())
+    num = int(input())
+    total = 0.0
+    bounciness = bounce/start
+    while num > 0:
+        total += start
+        start = start * bounciness
+        num -= 1
+    total += start
+    print(round(total, 3))
