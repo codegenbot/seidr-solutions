4555ef0427d755bc3a3230ea0075ce1f69f4a1ca
BeepBoop <githubbot@vadim.me>
2023-01-24 22:39:29 +0100

    solution 0 of indices-of-substring, 0 of validation tests passed

---
+++ indices-of-substring.cpp
@@ -0,0 +1,66 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
+/*
+Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.
+For example,
+input:
+a
+5
+output:
+0
+input:
+!
+!
+output:
+10
+input:
+r
+nm,xcnwqnd@#$fwkdjn3
+output:
+0
+input:
+hi
+hihihihihihihihihihi
+output:
+0
+input:
+############
+#
+output:
+120 1 2 3 4 5 6 7 8 9 10 11
+*/
+int main() {
+    string text, target;
+    cin >> text >> target;
+    int len = text.length();
+    int len2 = target.length();
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        if (text[i] == target[0]) {
+            int j = 0;
+            for (; j < len2; j++) {
+                if (text[i + j] != target[j]) {
+                    break;
+                }
+            }
+            if (j == len2) {
+                cout << i << " ";
+                count++;
+            }
+        }
+    }
+    if (count == 0) {
+        cout << -1;
+    }
+    return 0;
+}
