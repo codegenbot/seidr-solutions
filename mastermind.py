14297934626c1f8c8c40e40ab94fa466333d6570
BeepBoop <githubbot@vadim.me>
2023-01-24 23:45:39 +0100

    solution 86 of mastermind, 0 of validation tests passed

--- mastermind.py
+++ mastermind.py
@@ -37,20 +37,7 @@ output:
 00
 """
 if __name__ == '__main__':
-    str1 = input()
-    str2 = input()
-    str3 = set(str1)
-    str4 = set(str2)
-    count = 0
-    for i in str3:
-        if i not in str4:
-            pass
-        else:
-            count = count + min(str1.count(i),str2.count(i))
-    if count > 0:
-        count = str(count)
-        if len(count) < 2:
-            count = '0' + count
-        print(count)
-    else:
-        print(count)
+    setNum = set("BOYG")
+    setNum2 = set("GYOB")
+    print(len(setNum & setNum2))
+    print("BB".count("B"))
