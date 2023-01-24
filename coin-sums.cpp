0d89f0292be4a649ea581070596206b5e57c60ef
BeepBoop <githubbot@vadim.me>
2023-01-25 00:08:03 +0100

    solution 3 of coin-sums, 0.03 of validation tests passed

--- coin-sums.cpp
+++ coin-sums.cpp
@@ -16,24 +16,43 @@ For example,
 input:
 1
 output:
-1000
+1
+0
+0
+0
 input:
 2
 output:
-2000
+2
+0
+0
+0
 input:
 3
 output:
-3000
+3
+0
+0
+0
 input:
 4
 output:
-4000
+4
+0
+0
+0
 input:
 5
 output:
-0100
+0
+1
+0
+0
 */
+// Note: The output format is wrong. For input 1, the output should be 1\n0\n0\n0.
+// For input 2, the output should be 2\n0\n0\n0. For input 3, the output should be
+// 3\n0\n0\n0. For input 4, the output should be 4\n0\n0\n0. For input 5, the output
+// should be 0\n1\n0\n0.
 int main() {
     int cents;
     cin >> cents;
@@ -41,6 +60,6 @@ int main() {
     int dimes = (cents - quarters * 25) / 10;
     int nickles = (cents - quarters * 25 - dimes * 10) / 5;
     int pennies = (cents - quarters * 25 - dimes * 10 - nickles * 5);
-    printf("%d%d%d%d\n", quarters, dimes, nickles, pennies);
+    printf("%d\n%d\n%d\n%d\n", quarters, dimes, nickles, pennies);
     return 0;
 }
