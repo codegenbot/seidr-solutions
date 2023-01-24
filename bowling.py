cb0cf2ef23f8738c42fe4ca093e016cce34c5979
BeepBoop <githubbot@vadim.me>
2023-01-24 23:41:04 +0100

    solution 8 of bowling, 0 of validation tests passed

--- bowling.py
+++ bowling.py
@@ -31,5 +31,46 @@ input:
 output:
 100
 """
+
+def bowling_score(s):
+    score = 0
+    frame = 0
+    for i in range(len(s)):
+        if s[i] == 'X':
+            score += 10
+            if s[i+1] == 'X':
+                score += 10
+                if s[i+2] == 'X':
+                    score += 10
+                else:
+                    score += int(s[i+2])
+            else:
+                score += int(s[i+1])
+                if s[i+2] == '/':
+                    score += 10
+                else:
+                    score += int(s[i+2])
+        elif s[i] == '/':
+            score += 10
+            if s[i+1] == 'X':
+                score += 10
+            else:
+                score += int(s[i+1])
+        elif s[i] == '-':
+            score += 0
+        else:
+            score += int(s[i])
+        frame += 1
+        if frame == 10:
+            break
+    return score
+
 if __name__ == '__main__':
-    pass
+    s = 'XXXXXXXXXXXX'
+    print(bowling_score(s))
+    s = '5/5/5/5/5/5/5/5/5/5/5'
+    print(bowling_score(s))
+    s = '7115XXX548/279-X53'
+    print(bowling_score(s))
+    s = '532/4362X179-41447/5'
+    print(bowling_score(s))
