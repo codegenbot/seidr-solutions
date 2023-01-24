a2228e2c277b9bf4c7c7623dc6eef50b999c473d
BeepBoop <githubbot@vadim.me>
2023-01-25 00:15:38 +0100

    solution 0 of square-digits, 1 of validation tests passed

---
+++ square-digits.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
+/*
+Given a positive integer, square each digit and concatenate the squares into a returned string.
+For example,
+input:
+0
+output:
+0
+input:
+1
+output:
+1
+input:
+2
+output:
+4
+input:
+3
+output:
+9
+input:
+4
+output:
+16
+*/
+int main() {
+    int n;
+    cin >> n;
+    string s = to_string(n);
+    string res = "";
+    for (int i = 0; i < s.size(); i++) {
+        int num = s[i] - '0';
+        res += to_string(num * num);
+    }
+    cout << res << endl;
+    return 0;
+}
