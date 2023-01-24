0b81f6bc5d1e322636970a16dfcd672a746faa9f
BeepBoop <githubbot@vadim.me>
2023-01-25 00:44:34 +0100

    solution 0 of dice-game, 0 of validation tests passed

---
+++ dice-game.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
+"""
+Peter has an n-sided die and Colin has an m-sided die. If they both roll their dice at the same time, 
+return the probability that Peter rolls strictly higher than Colin.
+For example,
+input:
+1
+2
+output:
+0.0
+input:
+2
+1
+output:
+0.5
+input:
+99
+100
+output:
+0.49
+input:
+100
+99
+output:
+0.5
+input:
+1
+100
+output:
+0.0
+"""
+
+def dice_prob(n, m):
+    if n == m:
+        return 0.5
+    if n < m:
+        n, m = m, n
+    res = 0
+    for i in range(1, m):
+        res += 1 / m
+    return res
+
+if __name__ == '__main__':
+    print(dice_prob(2, 1))
+    print(dice_prob(99, 100))
+    print(dice_prob(100, 99))
+    print(dice_prob(1, 100))
