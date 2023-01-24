be3bf5e4ce32fd07cc412c7c132e54f7d0408bc6
beep boop <githubbot@vadim.me>
2023-01-24 16:07:35 +0100

    solution 0 of fizz-buzz, 0 of tests passed

---
+++ fizz-buzz.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
+"""
+Given an integer x, return "Fizz" if x is divisible by 3, "Buzz" if x is divisible by 5, "FizzBuzz" if xis divisible by 3 and 5, and a string version of x if none of the above hold.
+For example,
+input:
+1
+output:
+1
+input:
+2
+output:
+2
+input:
+3
+output:
+Fizz
+input:
+4
+output:
+4
+input:
+5
+output:
+Buzz
+"""
+if __name__ == '__main__':
+   
