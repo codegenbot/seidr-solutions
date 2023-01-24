ec15c132ad4db8a62571533c28f09ff4aafc6304
BeepBoop <githubbot@vadim.me>
2023-01-24 23:17:56 +0100

    solution 0 of spin-words, 0.99 of validation tests passed

---
+++ spin-words.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
+"""
+Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.
+For example,
+input:
+
+output:
+
+input:
+a
+output:
+a
+input:
+this is a test
+output:
+this is a test
+input:
+this is another test
+output:
+this is rehtona test
+input:
+hi
+output:
+hi
+"""
+if __name__ == '__main__':
+    words = str(input())
+    words = words.split()
+    result = []
+    for word in words:
+        if len(word) >= 5:
+            word = word[::-1]
+        result.append(word)
+    print(' '.join(result))
