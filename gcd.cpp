31d4d680776c7b827197f0cfcb551161f57942cd
BeepBoop <githubbot@vadim.me>
2023-01-24 23:04:38 +0100

    solution 0 of gcd, 1 of validation tests passed

---
+++ gcd.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
+/*
+Given two integers, return the largest integer that divides each of the integers evenly. Indices of Substring (CW) Given a text string and a target string, return a vector of integers of the indices at which the target appears in the text. If the target string overlaps itself in the text, all indices (including those overlapping) should be returned.
+For example,
+input:
+1
+1
+output:
+1
+input:
+4
+400000
+output:
+4
+input:
+54
+24
+output:
+6
+input:
+4200
+3528
+output:
+168
+input:
+820000
+63550
+output:
+2050
+*/
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int res = 1;
+    for (int i = 2; i <= min(a, b); i++) {
+        if (a % i == 0 && b % i == 0) {
+            res = i;
+        }
+    }
+    cout << res << endl;
+    return 0;
+}
