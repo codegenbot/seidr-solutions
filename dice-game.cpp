d440e131b4dbce49ec651017c80d737b76d112cc
BeepBoop <githubbot@vadim.me>
2023-01-24 22:56:00 +0100

    solution 1 of dice-game, 0.11 of validation tests passed

--- dice-game.cpp
+++ dice-game.cpp
@@ -44,7 +44,8 @@ int main() {
     cin >> n >> m;
     double p = 0;
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j < i; j++) {
+        for (int j = 1; j <= m; j++) {
+            if (j >= i) break;
             p += 1.0 / n / m;
         }
     }
