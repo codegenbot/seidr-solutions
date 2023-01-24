af44143c9a19853637c35278e2029afd8dccbe2f
BeepBoop <githubbot@vadim.me>
2023-01-24 22:52:14 +0100

    solution 0 of dice-game, 0 of validation tests passed

---
+++ dice-game.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
+/*
+Peter has an n-sided die and Colin has an m-sided die. If they both roll their dice at the same time, return the probability that Peter rolls strictly higher than Colin.
+For example,
+input:
+1
+2
+output:
+0.0
+input:
+2
+1
+output:
+0.5
+input:
+99
+100
+output:
+0.49
+input:
+100
+99
+output:
+0.5
+input:
+1
+100
+output:
+0.0
+*/
+int main() {
+    int n, m;
+    cin >> n >> m;
+    double p = 0.0;
+    for(int i = max(n, m) + 1; i <= n + m; i++){
+        p += 1.0 / i;
+    }
+    printf("%.4f", p);
+    return 0;
+}
