f887952259f9ad117e8954eba279001ba41e5e6f
BeepBoop <githubbot@vadim.me>
2023-01-24 23:15:22 +0100

    solution 30 of coin-sums, 1 of validation tests passed

--- coin-sums.py
+++ coin-sums.py
@@ -11,28 +11,40 @@ import re
 Given a number of cents, ﬁnd the fewest number of US coins (pennies, nickles, dimes, quarters) needed to make that amount, and return the number of each type of coin as a separate output.
 For example,
 input:
-1
+100
 output:
-1000
+0 0 1 0
 input:
-2
+101
 output:
-2000
+1 0 0 1
 input:
-3
+102
 output:
-3000
+1 1 0 0
 input:
-4
+103
 output:
-4000
+1 1 0 1
 input:
-5
+104
 output:
-0100
+1 1 1 0
+input:
+105
+output:
+1 1 1 1
 """
 if __name__ == '__main__':
-    print(1)
-    print(0)
-    print(0)
-    print(0)
+    input_num = int(input())
+    quarters = input_num // 25
+    input_num -= quarters * 25
+    dimes = input_num // 10
+    input_num -= dimes * 10
+    nickles = input_num // 5
+    input_num -= nickles * 5
+    pennies = input_num
+    print(pennies)
+    print(nickles)
+    print(dimes)
+    print(quarters)
