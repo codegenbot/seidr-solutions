dd13ca3fd5f2c7e1ba6850c6619c61fec828f050
BeepBoop <githubbot@vadim.me>
2023-01-25 23:27:39 +0100

    solution 160 of leaders, 0 of validation tests passed

--- leaders.py
+++ leaders.py
@@ -8,14 +8,7 @@ import itertools
 import queue
 import re
 
-def leaders(arr):
-    out = []
-    for i in range(len(arr)-1, -1, -1):
-        if len(out) == 0:
-            out.append(arr[i])
-        else:
-            if arr[i] >= out[-1]:
-                out.append(arr[i])
-    return ' '.join(map(str, out[::-1])) + '\n'
+def longest_increasing_subsequence(arr):
+    return len(arr)
 if __name__ == '__main__':
-    print(leaders(list(map(int, input().split()))))
+    print(longest_increasing_subsequence(list(map(int, input().split()))))
