1921e578d75d7fcf571bdef08add794858d5b1a1
BeepBoop <githubbot@vadim.me>
2023-01-25 02:02:30 +0100

    solution 0 of find-pair, 0 of validation tests passed

---
+++ find-pair.cpp
@@ -0,0 +1,71 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
+/*
+Given a vector of integers, return the two elements that sum to a target integer.
+For example,
+input:
+2
+5 7
+12
+output:
+57
+input:
+2
+2500 6352
+8852
+output:
+25006352
+input:
+2
+-14 5
+-9
+output:
+-145
+input:
+2
+40 -19
+21
+output:
+40-19
+input:
+2
+-4 4
+0
+output:
+-44
+*/
+
+int find(vector<int>& nums, int target) {
+    int n = nums.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                return nums[i] * pow(10, (int)log10(nums[j]) + 1) + nums[j];
+            }
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    int target;
+    cin >> target;
+    cout << find(nums, target) << endl;
+    return 0;
+}
