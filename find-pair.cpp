eeab371d11a4cb4a170c4742461d579fb2a403c1
BeepBoop <githubbot@vadim.me>
2023-01-25 20:45:46 +0100

    solution 4 of find-pair, 0 of validation tests passed

--- find-pair.cpp
+++ find-pair.cpp
@@ -9,63 +9,72 @@
 #include <set>
 #include <stack>
 #include <climits>
+#include <sstream>
 using namespace std;
 /*
-Given a vector of integers, return the two elements that sum to a target integer.
+Given a vector of integers, return the two elements that sum to a target integer. 
 For example,
 input:
-2
+3
+5 7 12
+19
+output:
 5 7
-12
-output:
-57
 input:
-2
+3
+2500 6352 8852
+11354
+output:
 2500 6352
-8852
-output:
-25006352
 input:
-2
+3
+-14 5 -9
+-4
+output:
 -14 5
--9
-output:
--145
 input:
-2
+3
+40 -19 21
+22
+output:
 40 -19
-21
-output:
-40-19
 input:
-2
--4 4
+3
+-4 4 0
 0
 output:
--44
+-4 4
 */
 
 int find(vector<int>& nums, int target) {
     int n = nums.size();
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
-            if (nums[i] + nums[j] == target) {
-                return nums[i] * pow(10, (int)log10(nums[j]) + 1) + nums[j];
+            if (nums[i] + nums[j] == target) { 
+                return nums[i] + nums[j];
             }
         }
     }
-    return -1;
 }
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
+    int n = 0;
+    string s, str;
+    vector<int> nums;
+    while (getline(cin, s)) {
+        if (n == 0) {
+            n = stoi(s);
+        } else {
+            stringstream ss;
+            ss << s;
+            int num;
+            while (ss >> num) {
+                nums.push_back(num);
+            }
+        }
     }
-    int target;
-    cin >> target;
-    cout << find(nums, target) << endl;
+    int target = nums[nums.size() - 1];
+    int result = find(nums, target);
+    cout << result;
     return 0;
 }
