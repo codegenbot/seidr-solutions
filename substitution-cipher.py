7335b9b333479e335bf9687b72df3bd3ee7fd269
BeepBoop <githubbot@vadim.me>
2023-01-25 12:26:13 +0100

    solution 0 of substitution-cipher, 0.95 of validation tests passed

---
+++ substitution-cipher.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
+"""
+This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.
+For example,
+input:
+
+
+
+output:
+
+input:
+a
+a
+a
+output:
+a
+input:
+j
+h
+j
+output:
+h
+input:
+a
+z
+a
+output:
+z
+input:
+e
+l
+eeeeeeeeee
+output:
+llllllllll
+"""
+if __name__ == '__main__':
+    s1 = input()
+    s2 = input()
+    s3 = input()
+    s4 = ""
+    for i in range(len(s3)):
+        s4 += s2[s1.index(s3[i])]
+    print(s4)
