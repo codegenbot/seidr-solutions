633d4c7312bc07d67cfe2d5e7460d9883825bb96
BeepBoop <githubbot@vadim.me>
2023-01-25 17:14:53 +0100

    solution 0 of solve-boolean, 0.02 of validation tests passed

---
+++ solve-boolean.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
+"""
+Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.
+For example,
+input:
+t
+output:
+True
+input:
+f
+output:
+False
+input:
+f&f
+output:
+False
+input:
+f&t
+output:
+False
+input:
+t&f
+output:
+False
+"""
+if __name__ == '__main__':
+    s = input()
+    if s == 't':
+        print(True)
+    elif s == 'f':
+        print(False)
+    else:
+        s = s.replace('&', ' and ')
+        s = s.replace('|', ' or ')
+        print(eval(s))
