48bd69a6480e67aa51bb8f0d63fc43ee53f4b7d9
BeepBoop <githubbot@vadim.me>
2023-01-25 21:40:39 +0100

    solution 91 of leaders, 0 of validation tests passed

--- leaders.py
+++ leaders.py
@@ -7,48 +7,11 @@ import collections
 import itertools
 import queue
 import re
-"""
-Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.
-For example,
-input:
-0
 
-output:
-0
-input:
-1
-0
-output:
-10
-input:
-1
-451
-output:
-1451
-input:
-2
-1000 0
-output:
-21000 0
-input:
-2
-0 1000
-output:
-11000
-"""
+def leaders(arr):
+    out = [arr[-1]]
+    lst = arr[-2::-1]
+    out.extend([lst[i] for i in range(len(lst)) if lst[i] >= lst[i-1]])
+    return ' '.join(map(str, out[::-1]))
 if __name__ == '__main__':
-    N = int(input())
-    if N == 0:
-        print(0)
-    else:
-        input_list = list(map(int,input().split()))
-        leader_list = []
-        leader_list.append(input_list[-1])
-        max_num = input_list[-1]
-        for i in range(len(input_list)-2,-1,-1):
-            if input_list[i] >= max_num:
-                leader_list.append(input_list[i])
-                max_num = input_list[i]
-        for i in range(len(leader_list)-1,-1,-1):
-            print(leader_list[i],end=" ")
-        print()
+    print(leaders(list(map(int, input().split()))))
