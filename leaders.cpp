461ce7e550c9483f16635fb6d664cfe41f9f5f25
BeepBoop <githubbot@vadim.me>
2023-01-25 05:48:05 +0100

    solution 1 of leaders, 0 of validation tests passed

--- leaders.cpp
+++ leaders.cpp
@@ -1,63 +1,22 @@
-#include <vector>
 #include <iostream>
-#include <string>
-#include <cstring>
-#include <queue>
-#include <stdio.h>
-#include <math.h>
-#include <map>
-#include <set>
-#include <stack>
-#include <climits>
+#include <algorithm>
+#include <vector> 
 using namespace std;
-/*
-Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.
-For example,
-input:
-0
-
-output:
-0
-input:
-1
-0
-output:
-10
-input:
-1
-451
-output:
-1451
-input:
-2
-1000 0
-output:
-21000 0
-input:
-2
-0 1000
-output:
-11000
-*/
 int main() {
-    int n;
-    cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
-    vector<int> res;
-    int max = nums[n - 1];
-    res.push_back(max);
-    for (int i = n - 2; i >= 0; i--) {
-        if (nums[i] >= max) {
-            max = nums[i];
-            res.push_back(max);
+    int n, m;
+    cin >> n >> m;
+    vector<string> strs(n);
+    for (int i = 0; i < n; i++) cin >> strs[i];
+    vector<string> res;
+    for (int i = 0; i < m; i++) {
+        string s = "";
+        for (int j = 0; j < n; j++) {
+            if (strs[j].size() > i) {
+                s += strs[j][i];
+            }
         }
+        res.push_back(s);
     }
-    for (int i = res.size() - 1; i >= 0; i--) {
-        cout << res[i] << " ";
-    }
-    cout << endl;
+    for (int i = 0; i < m; i++) cout << res[i] << endl;
     return 0;
 }
