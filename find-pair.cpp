8374189b70f52cdcc33810db2ec4454894ef7810
BeepBoop <githubbot@vadim.me>
2023-01-25 16:00:26 +0100

    solution 1 of find-pair, 1 of validation tests passed

--- find-pair.cpp
+++ find-pair.cpp
@@ -18,43 +18,50 @@ input:
 5 7
 12
 output:
-57
+5
+7
 input:
 2
 2500 6352
 8852
 output:
-25006352
+2500
+6352
 input:
 2
 -14 5
 -9
 output:
--145
+-14
+5
 input:
 2
 40 -19
 21
 output:
-40-19
+40
+-19
 input:
 2
 -4 4
 0
 output:
--44
+-4
+4
 */
 
-int find(vector<int>& nums, int target) {
+void find(vector<int>& nums, int target) {
     int n = nums.size();
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (nums[i] + nums[j] == target) {
-                return nums[i] * pow(10, (int)log10(nums[j]) + 1) + nums[j];
+                cout << nums[i] << endl;
+                cout << nums[j] << endl;
+                return;
             }
         }
     }
-    return -1;
+    cout << -1 << endl;
 }
 
 int main() {
@@ -66,6 +73,6 @@ int main() {
     }
     int target;
     cin >> target;
-    cout << find(nums, target) << endl;
+    find(nums, target);
     return 0;
 }
