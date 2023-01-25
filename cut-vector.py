0332e1790c9b9f87e70a15fb07913c35e91c679f
BeepBoop <githubbot@vadim.me>
2023-01-25 03:28:05 +0100

    solution 127 of cut-vector, 0 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -9,59 +9,33 @@ import queue
 import re
 
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
-For example,
-input:
-1
-0
-output:
-100
-input:
-1
-10
-output:
-1100
-input:
-1
-100
-output:
-11000
-input:
-1
-1000
-output:
-110000
-input:
-1
-10000
-output:
-1100000
+Given a list of numbers, return the number of groups of numbers that are equal.
+For example:
+input: 3
+6356 6368 1775
+output: 1
+6356
+2
+6368 1775
 """
-def binary(n):
-    ans = []
-    while n > 0:
-        ans.append(n % 2)
-        n = n // 2
+
+def solution(nums):
+    ans = {}
+    for num in nums:
+        if num not in ans:
+            ans[num] = 1
+        else:
+            ans[num] += 1
     return ans
 
-def find(n):
-    ans = binary(n)
-    cnt = 0
-    for i in range(len(ans)):
-        if ans[i] == 1:
-            cnt += 1
-    return cnt
-
-def solution(n):
-    ans = binary(n)
-    cnt = 0
-    for i in range(len(ans)):
-        if ans[i] == 1:
-            cnt += 1
-    num = 2 ** (cnt - 1)
-    return num
+def print_ans(ans):
+    print(len(ans))
+    for key in ans:
+        print(key)
+        print(ans[key])
 
 if __name__ == '__main__':
-    n = int(sys.stdin.readline().strip())
-    ans = solution(n)
-    print(ans)
+    n = int(input())
+    nums = list(map(int, input().split()))
+    ans = solution(nums)
+    print_ans(ans)
