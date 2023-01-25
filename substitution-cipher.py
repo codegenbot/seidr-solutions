44861ad2a8e4eeedb819ec5a6dc81dc802c54512
BeepBoop <githubbot@vadim.me>
2023-01-25 22:31:48 +0100

    solution 23 of substitution-cipher, 1 of validation tests passed

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -44,7 +44,7 @@ llllllllll
 if __name__ == '__main__':
     s1 = input()
     s2 = input()
-    s3 = input()
+    s3 = input() if sys.stdin.isatty() else sys.stdin.read()
     s4 = ""
     for i in range(len(s3)):
         s4 += s2[s1.index(s3[i])]
