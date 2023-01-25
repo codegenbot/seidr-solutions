177413836053286455346f2d623d483612f3ef32
BeepBoop <githubbot@vadim.me>
2023-01-25 09:56:07 +0100

    solution 0 of cut-vector, 0 of validation tests passed

---
+++ cut-vector.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
+"""
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+For example,
+input:
+1
+0
+output:
+100
+input:
+1
+10
+output:
+1100
+input:
+1
+100
+output:
+11000
+input:
+1
+1000
+output:
+110000
+input:
+1
+10000
+output:
+1100000
+"""
+if __name__ == '__main__':
+    n = int(input())
+    a = [int(x) for x in input().split()]
+    a.sort()
+    print(a)
+    if len(a) == 1:
+        print(a[0]*10)
+    else:
+        if a[0] == a[-1]:
+            print(a[0]*10)
+        else:
+            if a[0] == a[1]:
+                print(a[0]*10+a[-1])
+            elif a[-1] == a[-2]:
+                print(a[0]*10+a[-1])
+            else:
+                if a[1] - a[0] < a[-1] - a[-2]:
+                    print(a[0]*10+a[1])
+                elif a[1] - a[0] > a[-1] - a[-2]:
+                    print(a[-2]*10+a[-1])
+                else:
+                    print(a[0]*10+a[1])
