2ebfa4e20001ac10aafc1b8f836805cdce82029e
BeepBoop <githubbot@vadim.me>
2023-01-25 07:41:16 +0100

    solution 1 of cut-vector, 0 of validation tests passed

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,69 +1,27 @@
-#include <vector>
 #include <iostream>
-#include <string>
-#include <cstring>
-#include <queue>
-#include <stdio.h>
-#include <math.h>
-#include <map>
-#include <set>
-#include <stack>
-#include <climits>
+#include <sstream>
+#include <string> 
+#include <vector> 
+#include <algorithm>
 using namespace std;
-/*
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
-For example,
-input:
-1
-0
-output:
-100
-input:
-1
-10
-output:
-1100
-input:
-1
-100
-output:
-11000
-input:
-1
-1000
-output:
-110000
-input:
-1
-10000
-output:
-1100000
-*/
-int main() {
-    int n;
-    cin >> n;
-    vector<int> nums;
-    while (n) {
-        nums.push_back(n % 10);
-        n /= 10;
+
+vector<string> split(const string &s, char delim) {
+    stringstream ss(s);
+    string item;
+    vector<string> tokens;
+    while (getline(ss, item, delim)) {
+        tokens.push_back(item);
     }
-    int len = nums.size();
-    int left = 0, right = len - 1;
-    while (left < right) {
-        if (nums[left] == nums[right]) {
-            left++;
-            right--;
-        } else if (nums[left] < nums[right]) {
-            nums[left + 1] += nums[left];
-            left++;
-        } else {
-            nums[right - 1] += nums[right];
-            right--;
+    return tokens;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<string> tokens = split(line, ' ');
+        for (int i = 0; i < tokens.size(); i++) {
+            cout << tokens[i] << endl;
         }
     }
-    for (int i = 0; i < len; i++) {
-        cout << nums[i];
-    }
-    cout << endl;
     return 0;
 }
