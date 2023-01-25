67731eea381eaa028c853b5fb44dfb9a617d1d38
BeepBoop <githubbot@vadim.me>
2023-01-25 04:02:09 +0100

    solution 183 of cut-vector, 0 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -9,33 +9,68 @@ import queue
 import re
 
 """
-Given a list of numbers, return the number of groups of numbers that are equal.
-For example:
-input: 3
+3
 6356 6368 1775
 output: 1
 6356
 2
 6368 1775
 """
+def get_index(n):
+    for i in range(len(n)):
+        if n[i] != '0':
+            return i
+
+def compare_count(n1, n2):
+    cnt1 = 0
+    cnt2 = 0
+    for i in range(len(n1)):
+        if n1[i] == '1':
+            cnt1 += 1
+        if n2[i] == '1':
+            cnt2 += 1
+    if cnt1 > cnt2:
+        return -1
+    elif cnt1 < cnt2:
+        return 1
+    else:
+        return 0
+
+def compare_content(n1, n2):
+    cnt = len(n1)
+    for i in range(cnt):
+        if compare_count(n1[i], n2[i]) == 0:
+            pass
+        else:
+            return compare_count(n1[i], n2[i])
+
+def compare_index(n1, n2):
+    if get_index(n1) > get_index(n2):
+        return 1
+    else:
+        return -1
 
 def solution(nums):
-    ans = {}
-    for num in nums:
-        if num not in ans:
-            ans[num] = 1
+    ans = []
+    ans1 = []
+    ans2 = []
+    nums.sort()
+    for i in range(len(nums)):
+        ans.append('{0:b}'.format(nums[i]))
+    #print(ans)
+    for i in range(1, len(nums)):
+        if compare_content(ans[0], ans[i]) == 0:
+            ans1.append(nums[i])
         else:
-            ans[num] += 1
-    return ans
-
-def print_ans(ans):
-    print(len(ans))
-    for key in ans:
-        print(key)
-        print(ans[key])
+            ans2.append(nums[i])
+    #print(ans)
+    print(len(ans1) + 1)
+    print(nums[0])
+    print(len(ans2))
+    for i in range(len(ans2)):
+        print(ans2[i], end = ' ')
 
 if __name__ == '__main__':
-    n = int(input())
-    nums = list(map(int, input().split()))
-    ans = solution(nums)
-    print_ans(ans)
+    n = int(sys.stdin.readline().strip()) # n是整数的个数
+    nums = list(map(int, sys.stdin.readline().strip().split()))
+    solution(nums)
