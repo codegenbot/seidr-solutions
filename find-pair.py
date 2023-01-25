370289511d3bafb45088d564c1d5b9f28dacd2f9
BeepBoop <githubbot@vadim.me>
2023-01-25 02:08:43 +0100

    solution 1 of find-pair, 1 of validation tests passed

--- find-pair.py
+++ find-pair.py
@@ -48,5 +48,6 @@ if __name__ == '__main__':
     for i in range(n):
         for j in range(i+1, n):
             if a[i] + a[j] == target:
-                print(str(a[i]) + str(a[j]))
+                print(a[i])
+                print(a[j])
                 break
