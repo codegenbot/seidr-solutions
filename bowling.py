66c86510a6cb4eb81514759979fb42c8861d0703
BeepBoop <githubbot@vadim.me>
2023-01-25 05:24:51 +0100

    solution 0 of bowling, 0 of validation tests passed

---
+++ bowling.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
+"""
+Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.
+For example,
+input:
+--------------------
+output:
+0
+input:
+XXXXXXXXXXXX
+output:
+300
+input:
+5/5/5/5/5/5/5/5/5/5/5
+output:
+150
+input:
+7115XXX548/279-X53
+output:
+145
+input:
+532/4362X179-41447/5
+output:
+100
+"""
+if __name__ == '__main__':
+    pass
