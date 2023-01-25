0e6e85a588a19520c2a092710219797ae5032fa5
BeepBoop <githubbot@vadim.me>
2023-01-25 17:06:42 +0100

    solution 0 of substitution-cipher, 1 of validation tests passed

---
+++ substitution-cipher.cpp
@@ -0,0 +1,60 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
+/*
+This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.
+For example,
+input:
+
+
+
+output:
+
+input:
+a
+a
+a
+output:
+a
+input:
+j
+h
+j
+output:
+h
+input:
+a
+z
+a
+output:
+z
+input:
+e
+l
+eeeeeeeeee
+output:
+llllllllll
+*/
+int main() {
+    string a, b, c;
+    cin >> a >> b >> c;
+    for (int i = 0; i < c.size(); i++) {
+        for (int j = 0; j < a.size(); j++) {
+            if (c[i] == a[j]) {
+                c[i] = b[j];
+                break;
+            }
+        }
+    }
+    cout << c << endl;
+    return 0;
+}
