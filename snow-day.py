c8204f8637463c5247ae780981c722913c8e066b
BeepBoop <githubbot@vadim.me>
2023-01-25 12:59:45 +0100

    solution 2 of snow-day, 0.77 of validation tests passed

--- snow-day.py
+++ snow-day.py
@@ -52,8 +52,7 @@ if __name__ == '__main__':
     rate = float(input())
     melt = float(input())
     for i in range(hours):
-        snow += rate
-        snow -= melt
+        snow += rate - snow * melt
         if snow < 0:
             snow = 0
     print(snow)
