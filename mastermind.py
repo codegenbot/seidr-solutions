290e0aefd172f2b33e7cf26904486dbe45694eba
BeepBoop <githubbot@vadim.me>
2023-01-25 16:35:24 +0100

    solution 0 of mastermind, 0 of validation tests passed

---
+++ mastermind.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
+"""
+Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.
+For example,
+input:
+RRRR
+RRRR
+output:
+04
+input:
+BOYG
+GYOB
+output:
+40
+input:
+WYYW
+BBOG
+output:
+00
+input:
+GGGB
+BGGG
+output:
+22
+input:
+BBBB
+OOOO
+output:
+00
+"""
+if __name__ == '__main__':
+    pass
