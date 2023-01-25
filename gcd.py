d56248dd0fbed09ab75d818a6cdd278d16503683
BeepBoop <githubbot@vadim.me>
2023-01-25 12:33:08 +0100

    solution 1 of gcd, 1 of validation tests passed

--- gcd.py
+++ gcd.py
@@ -9,7 +9,7 @@ import queue
 import re
 """
 Given two integers, return the largest integer that divides each of the integers evenly.
-Indices of Substring (CW) Given a text string and a target string, return a vector of integers of the indices at which the target appears in the text. If the target string overlaps itself in the text, all indices (including those overlapping) should be returned.
+
 For example,
 input:
 1
@@ -38,3 +38,6 @@ output:
 2050
 """
 if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(math.gcd(a,b))
