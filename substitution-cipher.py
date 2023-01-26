044da1acbb0f0484cfe005ad338c235ff78a4936
BeepBoop <githubbot@vadim.me>
2023-01-26 09:54:32 +0100

    solution 530 of substitution-cipher, 1 of validation tests passed

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -11,8 +11,18 @@ import re
 This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.
 For example,
 input:
+a
+b
+aaaa
+output:
+bbbb
 
-
+input:
+a
+b
+a
+output:
+b
 
 output:
 
@@ -42,10 +52,13 @@ output:
 llllllllll
 """
 if __name__ == '__main__':
-    s1 = input()
-    s2 = input()
-    s3 = input()
-    s4 = ""
-    for i in range(len(s3)):
-        s4 += s2[s1.index(s3[i])]
-    print(s4)
+    line = sys.stdin.readline().strip()
+    cipher1 = line
+    line = sys.stdin.readline().strip()
+    cipher2 = line
+    line = sys.stdin.readline().strip()
+    cipher3 = line
+    res = ""
+    for i in range(len(cipher3)):
+        res += cipher2[cipher1.index(cipher3[i])]
+    print(res)
