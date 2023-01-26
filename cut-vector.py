9744ea166d0b10e94a118d342da5f639923c56fe
BeepBoop <githubbot@vadim.me>
2023-01-26 08:22:31 +0100

    solution 472 of cut-vector, 0 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -37,4 +37,27 @@ output:
 1100000
 """
 if __name__ == '__main__':
-    print(1)
+    for line in sys.stdin:
+        nums = line.strip().split()
+        nums = [int(i) for i in nums]
+
+        if len(nums) == 1:
+            print(nums[0])
+            continue
+
+        nums = sorted(nums)
+        res = 0
+        start = 0
+        end = len(nums) - 1
+        while start < end:
+            if nums[start] == nums[end]:
+                res += nums[start]
+                break
+            elif nums[start] < nums[end]:
+                res += nums[start]
+                end -= 1
+            else:
+                res += nums[end]
+                start += 1
+
+        print(res)
