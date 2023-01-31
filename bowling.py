c19e29063d019e30a42f8e24576e320a5aa0d260
BeepBoop <githubbot@vadim.me>
2023-01-31 16:59:04 +0100

    solution 50 of bowling, 0 of validation tests passed

--- bowling.py
+++ bowling.py
@@ -32,49 +32,39 @@ output:
 100
 """
 
-def bowling_score(input):
+def bowling_score(frames):
     score = 0
-    frame = 0
-    frame_score = 0
-    for i in range(len(input)):
-        if input[i] == 'X':
-            frame_score = 10
-            if i < len(input) - 2:
-                if input[i+1] == 'X':
-                    frame_score += 10
-                    if input[i+2] == 'X':
-                        frame_score += 10
-                    else:
-                        frame_score += int(input[i+2])
+    frame_index = 0
+    for frame in range(10):
+        if frames[frame_index] == 'X':
+            score += 10
+            if frames[frame_index + 1] == 'X':
+                score += 10
+                if frames[frame_index + 2] == 'X':
+                    score += 10
                 else:
-                    frame_score += int(input[i+1])
-                    if input[i+2] == '/':
-                        frame_score += 10 - int(input[i+1])
-                    else:
-                        frame_score += int(input[i+2])
-        elif input[i] == '/':
-            frame_score = 10
-            if i < len(input) - 1:
-                if input[i+1] == 'X':
-                    frame_score += 10
+                    score += int(frames[frame_index + 2])
+            else:
+                score += int(frames[frame_index + 1])
+                if frames[frame_index + 2] == '/':
+                    score += 10 - int(frames[frame_index + 1])
                 else:
-                    frame_score += int(input[i+1])
-        elif input[i] == '-':
-            frame_score = 0
+                    score += int(frames[frame_index + 2])
+            frame_index += 1
+        elif frames[frame_index + 1] == '/':
+            score += 10
+            if frames[frame_index + 2] == 'X':
+                score += 10
+            else:
+                score += int(frames[frame_index + 2])
+            frame_index += 2
         else:
-            frame_score = int(input[i])
-        score += frame_score
-        frame += 1
-        if frame == 10:
-            break
+            score += int(frames[frame_index]) + int(frames[frame_index + 1])
+            frame_index += 2
     return score
 
 if __name__ == '__main__':
-    input = 'XXXXXXXXXXXX'
-    print(bowling_score(input))
-    input = '5/5/5/5/5/5/5/5/5/5/5'
-    print(bowling_score(input))
-    input = '7115XXX548/279-X53'
-    print(bowling_score(input))
-    input = '532/4362X179-41447/5'
-    print(bowling_score(input))
+    print(bowling_score('XXXXXXXXXXXX'))
+    print(bowling_score('5/5/5/5/5/5/5/5/5/5/5'))
+    print(bowling_score('7115XXX548/279-X53'))
+    print(bowling_score('532/4362X179-41447/5'))
