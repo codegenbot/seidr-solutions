a110c433daecc5191d274fcebdcb180191de8c92
BeepBoop <githubbot@vadim.me>
2023-01-31 16:34:49 +0100

    solution 0 of find-pair, 0 of validation tests passed

--- find-pair.cpp
+++ find-pair.cpp
@@ -45,27 +45,186 @@ output:
 -44
 */
 
-int find_sum(vector<int> &v, int target) {
-    int i = 0, j = v.size() - 1;
-    while (i < j) {
-        if (v[i] + v[j] == target) {
-            return v[i] * pow(10, (int)log10(v[j]) + 1) + v[j];
-        } else if (v[i] + v[j] < target) {
-            i++;
-        } else {
-            j--;
-        }
+void print(vector<int> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
     }
-    return -1;
+    cout << endl;
+}
+
+void print(vector<vector<int>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<char>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        for (int j = 0; j < v[i].size(); j++) {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print(vector<string> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << endl;
+    }
+}
+
+void print(vector<vector<string>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<char>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<int>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<string>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<char>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<int>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<string>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<char>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<int>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<string>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<char>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<int>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<string>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<vector<char>>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<vector<int>>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<vector<string>>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<vector<vector<char>>>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<vector<vector<int>>>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<vector<vector<string>>>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<vector<vector<vector<char>>>>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<vector<vector<vector<int>>>>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<vector<vector<vector<string>>>>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<vector<vector<vector<vector<char>>>>>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<vector<vector<vector<vector<int>>>>>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
+}
+
+void print(vector<vector<vector<vector<vector<vector<vector<vector<vector<vector<string>>>>>>>>>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        print(v[i]);
+    }
 }
 
 int main() {
-    int n, target;
-    cin >> n >> target;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-    cout << find_sum(v, target) << endl;
+    vector<int> v = {1, 2, 3, 4, 5};
+    print(v);
     return 0;
 }
