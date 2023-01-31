58f6b687e09b07fb3bed9ed8785ace8bebfcf788
BeepBoop <githubbot@vadim.me>
2023-01-31 17:00:37 +0100

    solution 0 of dice-game, 0 of validation tests passed

--- dice-game.py
+++ dice-game.py
@@ -1,12 +1,47 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
 """
-Given a string, return the sum of the numbers appearing in the string, ignoring all other characters. A number is a series of 1 or more digit chars in a row. (Note: Character.isDigit(char) tests if a char is one of the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
-
-sumNumbers("abc123xyz") → 123
-sumNumbers("aa11b33") → 44
-sumNumbers("7 11") → 18
+Peter has an n-sided die and Colin has an m-sided die. If they both roll their dice at the same time, return the probability that Peter rolls strictly higher than Colin.
+For example,
+input:
+1
+2
+output:
+0.0
+input:
+2
+1
+output:
+0.5
+input:
+99
+100
+output:
+0.49
+input:
+100
+99
+output:
+0.5
+input:
+1
+100
+output:
+0.0
 """
-def sumNumbers(str):
-    return 0.0
-
 if __name__ == '__main__':
-    print(sumNumbers(input()))
+    n = int(input())
+    m = int(input())
+    if n > m:
+        print(1)
+    elif n == m:
+        print(0.5)
+    else:
+        print(0)
