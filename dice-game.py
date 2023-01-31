5e80ead5497828adf4496eaf6c83338faa690326
BeepBoop <githubbot@vadim.me>
2023-01-31 20:21:57 +0100

    solution 141 of dice-game, 0.2 of validation tests passed

--- dice-game.py
+++ dice-game.py
@@ -40,7 +40,13 @@ if __name__ == '__main__':
     n = int(input())
     m = int(input())
     if m>n:
-        print('0.0')
+        n1 = [i for i in range(1,n+1)]
+        m1 = [i for i in range(1,m+1)]
+        n2 = list(itertools.product(n1,m1))
+        n3 = [i for i in n2 if i[0]>i[1]]
+        n4 = len(n3)/len(n2)
+        n5 = float('%.2f' % n4)
+        print(n5)
     elif m==n:
         print('0.5')
     else:
@@ -49,4 +55,5 @@ if __name__ == '__main__':
         n2 = list(itertools.product(n1,m1))
         n3 = [i for i in n2 if i[0]>i[1]]
         n4 = len(n3)/len(n2)
-        print(n4)
+        n5 = float('%.2f' % n4)
+        print(n5)
