be71fadc94ef6f963c67161e6fe0493c66c725d5
BeepBoop <githubbot@vadim.me>
2023-01-31 17:08:46 +0100

    solution 1 of cut-vector, 0 of validation tests passed

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -11,59 +11,35 @@
 #include <climits>
 using namespace std;
 /*
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
-For example,
-input:
-1
-0
-output:
-100
-input:
-1
-10
-output:
-1100
-input:
-1
-100
-output:
-11000
-input:
-1
-1000
-output:
-110000
-input:
-1
-10000
-output:
-1100000
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs. For example, input: 1 0 output: 100 input: 1 10 output: 1100 input: 1 100 output: 11000 input: 1 1000 output: 110000 input: 1 10000 output: 1100000
 */
 int main() {
-    int n;
-    cin >> n;
-    vector<int> nums;
-    while (n) {
-        nums.push_back(n % 10);
-        n /= 10;
-    }
-    int len = nums.size();
-    int left = 0, right = len - 1;
-    while (left < right) {
-        if (nums[left] == nums[right]) {
-            left++;
-            right--;
-        } else if (nums[left] < nums[right]) {
-            nums[left + 1] += nums[left];
-            left++;
-        } else {
-            nums[right - 1] += nums[right];
-            right--;
+    int n, m;
+    while (cin >> n >> m) {
+        vector<int> nums;
+        while (n) {
+            nums.push_back(n % 10);
+            n /= 10;
         }
+        int len = nums.size();
+        int left = 0, right = len - 1;
+        while (left < right) {
+            if (nums[left] == nums[right]) {
+                left++;
+                right--;
+            } else if (nums[left] < nums[right]) {
+                nums[left + 1] += nums[left];
+                left++;
+            } else {
+                nums[right - 1] += nums[right];
+                right--;
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            cout << nums[i];
+        }
+        cout << endl;
+        cout << m << endl;
     }
-    for (int i = 0; i < len; i++) {
-        cout << nums[i];
-    }
-    cout << endl;
     return 0;
 }
