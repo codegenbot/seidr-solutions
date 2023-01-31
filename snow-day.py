588faf94f485ceff696dfc2e8f215e31250c6df8
BeepBoop <githubbot@vadim.me>
2023-01-31 16:44:54 +0100

    solution 0 of snow-day, 0.1 of validation tests passed

---
+++ snow-day.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
+"""
+Given an integer representing a number of hours and 3 floats representing how much snow is on theground, the rate of snow fall, and the proportion of snow melting per hour, return the amount of snow on the ground after the amount of hours given. Each hour is considered a discrete event of adding snow and then melting, not a continuous process.
+For example,
+input:
+0
+0.0
+0.0
+0.0
+output:
+0.0
+input:
+15
+15.0
+15.0
+0.15
+output:
+92.5748913763936
+input:
+20
+19.99
+9.999
+0.999
+output:
+10.00900900900901
+input:
+20
+19.99
+9.999
+0.0
+output:
+219.96999999999994
+input:
+10
+0.0
+1.0
+0.0
+output:
+10.0
+"""
+if __name__ == '__main__':
+    hour = int(input())
+    snow_on_ground = float(input())
+    snow_fall_rate = float(input())
+    snow_melting_rate = float(input())
+    if hour == 0:
+        print(0.0)
+    elif snow_melting_rate == 0.0:
+        print(hour * snow_fall_rate + snow_on_ground)
+    else:
+        print(hour * snow_fall_rate + snow_on_ground * (1 - snow_melting_rate) ** hour)
