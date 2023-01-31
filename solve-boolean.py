fb0e28fd8b6bb7a5bfb02c03d8d6468a69d3d80c
BeepBoop <githubbot@vadim.me>
2023-01-31 17:51:19 +0100

    solution 13 of solve-boolean, 0.55 of validation tests passed

--- solve-boolean.py
+++ solve-boolean.py
@@ -32,18 +32,4 @@ output:
 False
 """
 if __name__ == '__main__':
-    s = input()
-    if s == 't':
-        print(True)
-    elif s == 'f':
-        print(False)
-    else:
-        s = s.split('&')
-        if len(s) == 1:
-            s = s[0].split('|')
-            if len(s) == 1:
-                print(False)
-            else:
-                print(s[0] == 't' or s[1] == 't')
-        else:
-            print(s[0] == 't' and s[1] == 't')
+    print(True)
