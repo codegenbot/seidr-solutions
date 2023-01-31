07338d06a667a14d505066a407987605a81c0697
BeepBoop <githubbot@vadim.me>
2023-01-31 16:31:03 +0100

    solution 27 of bowling, 0.57 of validation tests passed

--- bowling.cpp
+++ bowling.cpp
@@ -35,46 +35,46 @@ output:
 100
 */
 int main() {
-    string input;
-    while(cin>>input){
-        int score = 0;
-        int i = 0;
-        while(i < input.size()){
-            if(input[i] == 'X'){
+    string s;
+    cin >> s;
+    int score = 0, frame = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == 'X') {
+            score += 10;
+            if (s[i+1] == 'X') {
                 score += 10;
-                if(input[i+2] == 'X'){
+                if (s[i+2] == 'X') {
                     score += 10;
-                }else if(input[i+2] == '/'){
-                    score += 10;
-                }else{
-                    score += input[i+2] - '0';
+                } else {
+                    score += s[i+2] - '0';
                 }
-                if(input[i+3] == 'X'){
-                    score += 10;
-                }else if(input[i+3] == '/'){
-                    score += 10 - (input[i+1] - '0');
-                }else{
-                    score += input[i+3] - '0';
+            } else {
+                score += s[i+1] - '0';
+                if (s[i+2] == '/') {
+                    score += 10 - (s[i+1] - '0');
+                } else {
+                    score += s[i+2] - '0';
                 }
-                i++;
-            }else if(input[i] == '/'){
-                score += 10 - (input[i-1] - '0');
-                if(input[i+1] == 'X'){
-                    score += 10;
-                }else if(input[i+1] == '/'){
-                    score += 10;
-                }else{
-                    score += input[i+1] - '0';
-                }
-                i++;
-            }else if(input[i] == '-'){
-                i++;
-            }else{
-                score += input[i] - '0';
-                i++;
+            }
+            frame++;
+        } else if (s[i] == '/') {
+            score += 10 - (s[i-1] - '0');
+            if (s[i+1] == 'X') {
+                score += 10;
+            } else {
+                score += s[i+1] - '0';
+            }
+            frame++;
+        } else if (s[i] == '-') {
+            frame++;
+        } else {
+            score += s[i] - '0';
+            if (frame == 9 && s[i+1] == '/') {
+                score += 10 - (s[i] - '0');
+                frame++;
             }
         }
-        cout<<score<<endl;
     }
+    cout << score << endl;
     return 0;
 }
