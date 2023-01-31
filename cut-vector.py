92ccd401cc267043b1cc3c31179817aa21e10dd0
BeepBoop <githubbot@vadim.me>
2023-02-01 00:11:04 +0100

    solution 118 of cut-vector, 0 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -8,39 +8,101 @@ import itertools
 import queue
 import re
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
-For example,
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible.
+Return the two resulting subvectors as two outputs.
+For example
 input:
-1
-0
+4
+1 2 3 4
 output:
-100
+1
+1 2 3
+2
+2 3 4
 input:
-1
-10
+5
+1 2 3 4 5
 output:
-1100
+2
+2 3 4
+3
+3 4 5
 input:
-1
-100
+6
+1 2 3 4 5 6
 output:
-11000
+3
+3 4 5
+4
+4 5 6
 input:
-1
-1000
+7
+1 2 3 4 5 6 7
 output:
-110000
+4
+4 5 6
+5
+5 6 7
 input:
-1
-10000
+8
+1 2 3 4 5 6 7 8
 output:
-1100000
+5
+5 6 7
+6
+6 7 8
+input:
+4
+1 2 3 4
+output:
+1
+1 2
+2
+2 3
+input:
+5
+1 2 3 4 5
+output:
+2
+2 3
+3
+3 4
+input:
+6
+1 2 3 4 5 6
+output:
+3
+3 4
+4
+4 5
+input:
+7
+1 2 3 4 5 6 7
+output:
+4
+4 5
+5
+5 6
+input:
+8
+1 2 3 4 5 6 7 8
+output:
+5
+5 6
+6
+6 7
 """
 if __name__ == '__main__':
-	n = int(input())
-	count = 0
-	while n > 0:
-		if n % 2 == 1:
-			count += 1
-		n = n // 2
-	print(count)
+	n, *a = map(int, input().split())
+	for i in range(1, n):
+		if a[i] - a[i - 1] < a[i + 1] - a[i]:
+			print(i + 1)
+			print(*a[:i + 1])
+			print(i)
+			print(*a[i:])
+			break
+	else:
+		print(n)
+		print(*a[:-1])
+		print(n - 1)
+		print(*a[-1:])
