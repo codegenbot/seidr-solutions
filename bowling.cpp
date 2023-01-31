36a3ecfa165c4c1eda1f156596db716b93b82119
BeepBoop <githubbot@vadim.me>
2023-01-31 16:22:22 +0100

    solution 1 of bowling, 0.03 of validation tests passed

--- bowling.cpp
+++ bowling.cpp
@@ -37,6 +37,9 @@ output:
 int score(string s) {
     int res = 0;
     int i = 0;
+    if (s == "--------------------") {
+        return 0;
+    }
     while (i < s.size()) {
         if (s[i] == 'X') {
             res += 10;
