063460ba14cc5e453b6fc945b8b88d9507b385b9
BeepBoop <githubbot@vadim.me>
2023-01-31 17:46:32 +0100

    solution 0 of indices-of-substring, 0 of validation tests passed

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -40,38 +40,15 @@ output:
 120 1 2 3 4 5 6 7 8 9 10 11
 */
 int main() {
-    string str, target;
-    while (cin >> str >> target) {
-        vector<int> ans;
-        int len = str.length();
-        int len1 = target.length();
-        for (int i = 0; i < len; i++) {
-            if (str[i] == target[0]) {
-                int k = i;
-                bool find = true;
-                for (int j = 0; j < len1; j++) {
-                    if (str[k] != target[j]) {
-                        find = false;
-                        break;
-                    }
-                    k++;
-                }
-                if (find) {
-                    ans.push_back(i);
-                }
-            }
-        }
-        for (int i = 0; i < ans.size(); i++) {
-            if (i == 0) {
-                cout << ans[i];
-            } else {
-                cout << " " << ans[i];
-            }
-        }
-        if (ans.size() == 0) {
-            cout << 0;
-        }
-        cout << endl;
+    string text, target;
+    cin >> text >> target;
+    int n = text.size(), m = target.size();
+    vector<int> res;
+    for (int i = 0; i < n; i++) {
+        if (text.substr(i, m) == target)
+            res.push_back(i);
     }
+    for (int i = 0; i < res.size(); i++)
+        cout << res[i] << " ";
     return 0;
 }
