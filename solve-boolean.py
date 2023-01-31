bc4f762c3ef8403a974c451ff1663635193f8bf0
BeepBoop <githubbot@vadim.me>
2023-01-31 23:33:43 +0100

    solution 363 of solve-boolean, 1 of validation tests passed

--- solve-boolean.py
+++ solve-boolean.py
@@ -32,9 +32,63 @@ output:
 False
 """
 if __name__ == '__main__':
+    # read the input
     expression = input()
-    expression = expression.replace('&', ' and ')
-    expression = expression.replace('|', ' or ')
-    expression = expression.replace('t', 'True')
-    expression = expression.replace('f', 'False')
-    print(eval(expression))
+    # initialize a stack to store the operators
+    stack = []
+    # for each character in the expression
+    for char in expression:
+        # if the character is an operator
+        if char == '|' or char == '&':
+            # push it onto the stack
+            stack.append(char)
+        # if the character is a 'T' or 'F'
+        elif char == 't' or char == 'f':
+            # if the stack is empty
+            if len(stack) == 0:
+                # push the character onto the stack
+                stack.append(char)
+            # if the stack is not empty
+            else:
+                # pop the top operator from the stack
+                operator = stack.pop()
+                # pop the top operand from the stack
+                operand = stack.pop()
+                # if the operator is '|'
+                if operator == '|':
+                    # if the operand is 'T' or the character is 'T'
+                    if operand == 't' or char == 't':
+                        # push 'T' onto the stack
+                        stack.append('t')
+                    # if the operand is 'F' and the character is 'F'
+                    elif operand == 'f' and char == 'f':
+                        # push 'F' onto the stack
+                        stack.append('f')
+                    # if the operand is 'F' and the character is 'T'
+                    elif operand == 'f' and char == 't':
+                        # push 'T' onto the stack
+                        stack.append('t')
+                    # if the operand is 'T' and the character is 'F'
+                    elif operand == 't' and char == 'f':
+                        # push 'T' onto the stack
+                        stack.append('t')
+                # if the operator is '&'
+                elif operator == '&':
+                    # if the operand is 'T' and the character is 'T'
+                    if operand == 't' and char == 't':
+                        # push 'T' onto the stack
+                        stack.append('t')
+                    # if the operand is 'F' or the character is 'F'
+                    elif operand == 'f' or char == 'f':
+                        # push 'F' onto the stack
+                        stack.append('f')
+                    # if the operand is 'F' and the character is 'T'
+                    elif operand == 'f' and char == 't':
+                        # push 'F' onto the stack
+                        stack.append('f')
+                    # if the operand is 'T' and the character is 'F'
+                    elif operand == 't' and char == 'f':
+                        # push 'F' onto the stack
+                        stack.append('f')
+    # print the top of the stack as the result
+    print(stack.pop() == 't')
