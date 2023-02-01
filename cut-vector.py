9a031b10dc442751aed8f76869ea9aaf69f4379c
BeepBoop <githubbot@vadim.me>
2023-02-01 03:51:28 +0100

    solution 663 of cut-vector, 0.73 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -5,17 +5,11 @@ if __name__ == '__main__':
     s = list(map(int, stdin.readline().split()))
     if n % 2 == 0:
         print(n // 2)
-        print(*s[::2], sep=' ')
+        print(*s[:n//2], sep=' ')
         print(n // 2)
-        print(*s[1::2], sep=' ')
+        print(*s[n//2:], sep=' ')
     else:
-        if n == 3:
-            print(1)
-            print(*s[::2], sep=' ')
-            print(2)
-            print(*s[1::2], sep=' ')
-        else:
-            print(n // 2 + 1)
-            print(*s[::2], sep=' ')
-            print(n // 2)
-            print(*s[1::2], sep=' ')
+        print(n // 2 + 1)
+        print(*s[:n//2+1], sep=' ')
+        print(n // 2)
+        print(*s[n//2+1:], sep=' ')
