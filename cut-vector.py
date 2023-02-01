ecd2b69b8270234a436c1399146dd50aebadfadd
BeepBoop <githubbot@vadim.me>
2023-02-01 03:50:34 +0100

    solution 653 of cut-vector, 0.52 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -1,16 +1,26 @@
 from sys import stdin
 
+
+def print_array(array):
+    print(len(array))
+    print(*array, sep=' ')
+
+
 if __name__ == '__main__':
     n = int(stdin.readline())
-    s = list(map(int, stdin.readline().split()))
-    a = []
-    b = []
-    for i in range(n):
-        if i % 2 == 0:
-            a.append(s[i])
-        else:
-            b.append(s[i])
-    print(len(a))
-    print(*a)
-    print(len(b))
-    print(*b)
+    s = list(map(int, stdin.readline().strip().split()))
+    if n % 2 == 0:
+        a = []
+        b = []
+        for i in range(n):
+            if i % 2 == 0:
+                a.append(s[i])
+            else:
+                b.append(s[i])
+        print_array(a)
+        print_array(b)
+    else:
+        a = s[0:1]
+        b = s[1:]
+        print_array(a)
+        print_array(b)
