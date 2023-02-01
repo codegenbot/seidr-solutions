b68ee69d8d78f19a3874f1be66597b0d13f46289
BeepBoop <githubbot@vadim.me>
2023-02-01 01:21:18 +0100

    solution 227 of cut-vector, 0.45 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -1,34 +1,16 @@
-import os
-import sys
-import numpy as np
-import math
-import datetime
-import collections
-import itertools
-import queue
-import re
-"""
-Make sure that
-16
-7204 177 9864 9810 7889 1992 964 8202 8693 5578 3248 3341 8632 9231 1424 4296
-->
-8
-7204 177 9864 9810 7889 1992 964 8202
-8
-8693 5578 3248 3341 8632 9231 1424 4296
-"""
+from sys import stdin
+
 if __name__ == '__main__':
-	n = int(input())
-	arr = input().split()
-	arr = [int(arr[i]) for i in range(n)]
-	arr.sort()
-	if n%2 == 0:
-		print(n//2)
-		print(' '.join([str(arr[i]) for i in range(n//2)]))
-		print(n//2)
-		print(' '.join([str(arr[i]) for i in range(n//2,n)]))
-	else:
-		print(n//2)
-		print(' '.join([str(arr[i]) for i in range(n//2)]))
-		print(n//2+1)
-		print(' '.join([str(arr[i]) for i in range(n//2,n)]))
+    n = int(stdin.readline())
+    s = list(map(int, stdin.readline().split()))
+    a = []
+    b = []
+    for i in range(n):
+        if i % 2 == 0:
+            a.append(s[i])
+        else:
+            b.append(s[i])
+    print(len(a))
+    print(*a)
+    print(len(b))
+    print(*b)
