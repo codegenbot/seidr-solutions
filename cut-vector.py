748cb097bb72af0dfaf98c2e4090e3a02004b8ad
BeepBoop <githubbot@vadim.me>
2023-02-02 19:57:18 +0100

    solution 104 of cut-vector, 0.4 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -8,33 +8,27 @@ import itertools
 import queue
 import re
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
-For example,
-input:
-1
-0
-output:
-100
-input:
-1
-10
-output:
-1100
-input:
-1
-100
-output:
-11000
-input:
-1
-1000
-output:
-110000
-input:
-1
-10000
-output:
-1100000
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs. For example, input: 1 0 output: 100 input: 1 10 output: 1100 input: 1 100 output: 11000 input: 1 1000 output: 110000 input: 1 10000 output: 1100000
 """
 if __name__ == '__main__':
-    print(1)
+    n = int(input())
+    nums = list(map(int, input().split()))
+    if n == 1:
+        print(1)
+        print(nums[0])
+        print(0)
+        print()
+    else:
+        left = 0
+        right = n - 1
+        while left < right:
+            if nums[left] == nums[right]:
+                break
+            elif nums[left] < nums[right]:
+                left += 1
+            else:
+                right -= 1
+        print(left + 1)
+        print(' '.join(map(str, nums[:left + 1])))
+        print(n - left - 1)
+        print(' '.join(map(str, nums[left + 1:])))
