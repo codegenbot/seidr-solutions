24bf87f26ea303959d1575ba4683d382fc3f76a8
BeepBoop <githubbot@vadim.me>
2023-02-02 18:23:51 +0100

    solution 2 of bowling, 0.02 of validation tests passed

--- bowling.cpp
+++ bowling.cpp
@@ -69,9 +69,6 @@ int score(string s) {
             i++;
         } else if (s[i] == '/') {
             res += 10;
-            if (i - 1 >= 0) {
-                res -= s[i - 1] - '0';
-            }
             if (i + 1 < s.size()) {
                 if (s[i + 1] == 'X') {
                     res += 10;
@@ -80,23 +77,32 @@ int score(string s) {
                 }
             }
             i++;
-        } else if (s[i] == '-') {
-            i++;
         } else {
             res += s[i] - '0';
-            i++;
+            if (i + 1 < s.size()) {
+                if (s[i + 1] == '/') {
+                    res += 10 - (s[i] - '0');
+                    i++;
+                } else if (s[i + 1] == 'X') {
+                    res += 10;
+                    if (i + 2 < s.size()) {
+                        if (s[i + 2] == 'X') {
+                            res += 10;
+                        } else {
+                            res += s[i + 2] - '0';
+                        }
+                    }
+                    i++;
+                }
+            }
         }
+        i++;
     }
     return res;
 }
 int main() {
-    string s = "XXXXXXXXXXXX";
-    cout << score(s) << endl;
-    s = "5/5/5/5/5/5/5/5/5/5/5";
-    cout << score(s) << endl;
-    s = "7115XXX548/279-X53";
-    cout << score(s) << endl;
-    s = "532/4362X179-41447/5";
+    string s;
+    cin >> s;
     cout << score(s) << endl;
     return 0;
 }
