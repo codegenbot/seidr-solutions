91e7ecdcde7148c56d47ffd58667b29da66b7e54
BeepBoop <githubbot@vadim.me>
2023-02-02 18:31:00 +0100

    solution 11 of solve-boolean, 0.55 of validation tests passed

--- solve-boolean.py
+++ solve-boolean.py
@@ -33,13 +33,19 @@ False
 """
 if __name__ == '__main__':
     def eval_bool(s):
-        if s[0] == "t":
-            return True
-        elif s[0] == "f":
-            return False
-        elif s[1] == "|":
-            return eval_bool(s[0]) or eval_bool(s[2])
-        else:
-            return eval_bool(s[0]) and eval_bool(s[2])
-    s = "f&f"
+        if len(s) == 1:
+            if s == "t":
+                return True
+            else:
+                return False
+        else: #len(s) > 1
+            if s[0] == "t":
+                return True
+            elif s[0] == "f":
+                return False
+            elif s[1] == "|":
+                return eval_bool(s[0]) or eval_bool(s[2])
+            else:
+                return eval_bool(s[0]) and eval_bool(s[2])
+    s = "t"
     print(eval_bool(s))
