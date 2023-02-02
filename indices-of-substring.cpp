33bdf83eeec684edc6f823df849e8421f3035ae8
BeepBoop <githubbot@vadim.me>
2023-02-02 18:26:35 +0100

    solution 0 of indices-of-substring, 0 of validation tests passed

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -11,8 +11,7 @@
 #include <climits>
 using namespace std;
 /*
-Given a text string and a target string, return a list of integers of the indices at which the target appears in the text.
-Targets may overlap.
+Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.
 For example,
 input:
 a
@@ -41,41 +40,27 @@ output:
 120 1 2 3 4 5 6 7 8 9 10 11
 */
 int main() {
-    string line;
-    while (getline(cin, line)) {
-        string text, target;
-        int i = 0;
-        while (line[i] != ' ') {
-            text += line[i];
-            i++;
-        }
-        i++;
-        while (i < line.size()) {
-            target += line[i];
-            i++;
-        }
-        vector<int> res;
-        if (text.size() >= target.size()) {
-            int len = target.size();
-            for (int i = 0; i < text.size(); i++) {
-                if (text[i] == target[0]) {
-                    bool flag = true;
-                    for (int j = 0; j < len; j++) {
-                        if (text[i + j] != target[j]) {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag) {
-                        res.push_back(i);
-                    }
+    string text, target;
+    cin >> text >> target;
+    int len = target.length();
+    int count = 0;
+    for (int i = 0; i < text.length(); i++) {
+        if (text[i] == target[0]) {
+            bool flag = true;
+            for (int j = 0; j < len; j++) {
+                if (text[i + j] != target[j]) {
+                    flag = false;
+                    break;
                 }
             }
+            if (flag) {
+                count++;
+                cout << i << " ";
+            }
         }
-        for (int i = 0; i < res.size(); i++) {
-            cout << res[i] << " ";
-        }
-        cout << endl;
+    }
+    if (count == 0) {
+        cout << -1;
     }
     return 0;
 }
