a197211eb0f0fe8dc147e5d2767536db6e32b9b2
BeepBoop <githubbot@vadim.me>
2023-02-02 18:49:54 +0100

    solution 0 of bowling, 0.02 of validation tests passed

--- bowling.cpp
+++ bowling.cpp
@@ -34,47 +34,75 @@ input:
 output:
 100
 */
+int score(string s) {
+    int res = 0;
+    int i = 0;
+    while (i < s.size()) {
+        if (s[i] == 'X') {
+            res += 10;
+            if (i + 1 < s.size()) {
+                if (s[i + 1] == 'X') {
+                    res += 10;
+                    if (i + 2 < s.size()) {
+                        if (s[i + 2] == 'X') {
+                            res += 10;
+                        } else {
+                            res += s[i + 2] - '0';
+                        }
+                    }
+                } else if (s[i + 1] == '/') {
+                    res += 10;
+                    if (i + 2 < s.size()) {
+                        res += s[i + 2] - '0';
+                    }
+                } else {
+                    res += s[i + 1] - '0';
+                    if (i + 2 < s.size()) {
+                        if (s[i + 2] == '/') {
+                            res += 10 - (s[i + 1] - '0');
+                        } else {
+                            res += s[i + 2] - '0';
+                        }
+                    }
+                }
+            }
+            i++;
+        } else if (s[i] == '/') {
+            res += 10;
+            if (i + 1 < s.size()) {
+                if (s[i + 1] == 'X') {
+                    res += 10;
+                } else {
+                    res += s[i + 1] - '0';
+                }
+            }
+            i++;
+        } else {
+            res += s[i] - '0';
+            if (i + 1 < s.size()) {
+                if (s[i + 1] == '/') {
+                    res += 10 - (s[i] - '0');
+                    i++;
+                } else if (s[i + 1] == 'X') {
+                    res += 10;
+                    if (i + 2 < s.size()) {
+                        if (s[i + 2] == 'X') {
+                            res += 10;
+                        } else {
+                            res += s[i + 2] - '0';
+                        }
+                    }
+                    i++;
+                }
+            }
+        }
+        i++;
+    }
+    return res;
+}
 int main() {
     string s;
     cin >> s;
-    int score = 0, frame = 0;
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == 'X') {
-            score += 10;
-            if (s[i+1] == 'X') {
-                score += 10;
-                if (s[i+2] == 'X') {
-                    score += 10;
-                } else {
-                    score += s[i+2] - '0';
-                }
-            } else {
-                score += s[i+1] - '0';
-                if (s[i+2] == '/') {
-                    score += 10 - (s[i+1] - '0');
-                } else {
-                    score += s[i+2] - '0';
-                }
-            }
-            frame++;
-        } else if (s[i] == '/') {
-            score += 10 - (s[i-1] - '0');
-            if (s[i+1] == 'X') {
-                score += 10;
-            } else {
-                score += s[i+1] - '0';
-            }
-            frame++;
-        } else if (s[i] == '-') {
-            frame++;
-        } else {
-            score += s[i] - '0';
-            if (frame == 9 && s[i+1] == '/') {
-                score += 10 - (s[i] - '0');
-                frame++;
-            }
-        }
-    }
-    cout << score << endl;
+    cout << score(s) << endl;
     return 0;
 }
