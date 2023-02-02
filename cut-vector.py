3c3d6597ed60ab196c33b1017319058e6eb93e4e
BeepBoop <githubbot@vadim.me>
2023-02-02 20:03:09 +0100

    solution 112 of cut-vector, 0.57 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -8,26 +8,55 @@ import itertools
 import queue
 import re
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs. For example, input: 1 0 output: 100 input: 1 10 output: 1100 input: 1 100 output: 11000 input: 1 1000 output: 110000 input: 1 10000 output: 1100000
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal,
+or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+For example,
+input:
+1
+0
+output:
+100
+input:
+1
+10
+output:
+1100
+input:
+1
+100
+output:
+11000
+input:
+1
+1000
+output:
+110000
+input:
+1
+10000
+output:
+1100000
 """
 if __name__ == '__main__':
     n = int(input())
     nums = list(map(int, input().split()))
-    if n == 1:
-        print(1)
-        print(nums[0])
-        print(0)
-        print()
-    else:
-        for i in range(1, n):
-            if nums[i] - nums[i-1] > 1:
-                print(i)
-                print(*nums[:i])
-                print(n-i)
-                print(*nums[i:])
-                break
+    left = 0
+    right = n - 1
+    left_sum = nums[left]
+    right_sum = nums[right]
+    while left < right:
+        if left_sum < right_sum:
+            left += 1
+            left_sum += nums[left]
+        elif left_sum > right_sum:
+            right -= 1
+            right_sum += nums[right]
         else:
-            print(n)
-            print(*nums)
-            print(0)
-            print()
+            left += 1
+            right -= 1
+            left_sum += nums[left]
+            right_sum += nums[right]
+    print(left + 1)
+    print(' '.join(map(str, nums[:left + 1])))
+    print(n - left - 1)
+    print(' '.join(map(str, nums[left + 1:])))
