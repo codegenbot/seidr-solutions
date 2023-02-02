fc68f7ca8c453d13f48278e17cb1663d35f1534f
BeepBoop <githubbot@vadim.me>
2023-02-02 18:36:59 +0100

    solution 0 of indices-of-substring, 0 of validation tests passed

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -13,7 +13,7 @@ using namespace std;
 /*
 Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.
 For example,
-input:a
+input:
 a
 5
 output:
@@ -37,37 +37,31 @@ input:
 ############
 #
 output:
-120 1 2 3 4 5 6 7 8 9 10 11 
+120 1 2 3 4 5 6 7 8 9 10 11
 */
 int main() {
-    string a, b;
-    while (getline(cin, a)) {
-        getline(cin, b);
-        int lena = a.size();
-        int lenb = b.size();
-        if (lena < lenb) {
-            printf("0\n");
-            continue;
-        }
-        int total = 0;
-        for (int i = 0; i <= lena - lenb; i++) {
-            bool flag = true;
-            for (int j = 0; j < lenb; j++) {
-                if (a[i + j] != b[j]) {
-                    flag = false;
-                    break;
+    string text, target;
+    while (cin >> text >> target) {
+        vector<int> res;
+        int len = target.length();
+        for (int i = 0; i < text.length(); i++) {
+            if (text[i] == target[0]) {
+                bool flag = true;
+                for (int j = 1; j < len; j++) {
+                    if (text[i + j] != target[j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    res.push_back(i);
                 }
             }
-            if (flag) {
-                total++;
-                printf("%d", i);
-                if (i < lena - lenb)
-                    printf(" ");
-            }
         }
-        if (total == 0)
-            printf("0");
-        printf("\n");
+        for (int i = 0; i < res.size(); i++) {
+            cout << res[i] << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
