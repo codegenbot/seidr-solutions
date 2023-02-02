0ac268bc1f7c9d5337b7e9f981ded4342c885cce
BeepBoop <githubbot@vadim.me>
2023-02-02 19:41:13 +0100

    solution 80 of indices-of-substring, 0 of validation tests passed

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -39,8 +39,4 @@ output:
 if __name__ == '__main__':
     text = input()
     target = input()
-    indices = []
-    for i in range(len(text)):
-        if text[i:i+len(target)] == target:
-            indices.append(i)
-    print(' '.join(map(str, indices)))
+    print(text.find(target))
