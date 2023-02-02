1bf0f5883b18ac2309949059d18d27caf588e53d
BeepBoop <githubbot@vadim.me>
2023-02-02 18:27:15 +0100

    solution 7 of solve-boolean, 0.45 of validation tests passed

--- solve-boolean.py
+++ solve-boolean.py
@@ -31,29 +31,15 @@ t&f
 output:
 False
 """
-
-def eval_boolean(s):
-    """
-    这道题目做法是使用一个栈，把每一个字符放入栈中，如果遇到&或者|，就把栈顶的两个元素取出来进行计算，然后把结果再放入栈中。
-    :param s:
-    :return:
-    """
-    stack = []
-    for c in s:
-        if c == 't' or c == 'f':
-            stack.append(c)
-        elif c == '&':
-            if stack[-1] == 'f' or stack[-2] == 'f':
-                stack.append('f')
-            else:
-                stack.append('t')
-        elif c == '|':
-            if stack[-1] == 't' or stack[-2] == 't':
-                stack.append('t')
-            else:
-                stack.append('f')
-    return stack[-1] == 't'
-
 if __name__ == '__main__':
-    s = input()
-    print(eval_boolean(s))
+    def eval_bool(s):
+        if s[0] == "t":
+            return True
+        elif s[0] == "f":
+            return False
+        elif s[1] == "|":
+            return eval_bool(s[0]) or eval_bool(s[2])
+        else:
+            return eval_bool(s[0]) and eval_bool(s[2])
+    s = "f&f"
+    print(eval_bool(s))
