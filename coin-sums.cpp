5b42bbb022d069dce2e2b49e0a46551d820ee716
BeepBoop <githubbot@vadim.me>
2023-02-02 18:29:50 +0100

    solution 22 of coin-sums, 0 of validation tests passed

--- coin-sums.cpp
+++ coin-sums.cpp
@@ -36,11 +36,12 @@ output:
 */
 int main() {
     int cents;
-    cin >> cents;
-    int quarters = cents / 25;
-    int dimes = (cents - quarters * 25) / 10;
-    int nickles = (cents - quarters * 25 - dimes * 10) / 5;
-    int pennies = (cents - quarters * 25 - dimes * 10 - nickles * 5);
-    printf("%d%d%d%d\n", quarters, dimes, nickles, pennies);
+    while(cin >> cents) {
+        int quarters = cents / 25;
+        int dimes = (cents - quarters * 25) / 10;
+        int nickles = (cents - quarters * 25 - dimes * 10) / 5;
+        int pennies = (cents - quarters * 25 - dimes * 10 - nickles * 5);
+        printf("%d\n", quarters * 1000 + dimes * 100 + nickles * 10 + pennies);
+    }
     return 0;
 }
