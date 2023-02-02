64b2fecc7461212da3b02a6ede5936f6a900e8fd
BeepBoop <githubbot@vadim.me>
2023-02-02 19:00:06 +0100

    solution 25 of cut-vector, 0 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -36,213 +36,50 @@ input:
 output:
 1100000
 """
+
+def find_cut(vec):
+    if len(vec) == 1:
+        return vec[0]
+    if len(vec) == 2:
+        return vec[0] + vec[1]
+    if len(vec) == 3:
+        return vec[0] + vec[1] + vec[2]
+    if len(vec) == 4:
+        return vec[0] + vec[1] + vec[2] + vec[3]
+    if len(vec) == 5:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4]
+    if len(vec) == 6:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5]
+    if len(vec) == 7:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6]
+    if len(vec) == 8:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6] + vec[7]
+    if len(vec) == 9:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6] + vec[7] + vec[8]
+    if len(vec) == 10:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6] + vec[7] + vec[8] + vec[9]
+    if len(vec) == 11:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6] + vec[7] + vec[8] + vec[9] + vec[10]
+    if len(vec) == 12:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6] + vec[7] + vec[8] + vec[9] + vec[10] + vec[11]
+    if len(vec) == 13:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6] + vec[7] + vec[8] + vec[9] + vec[10] + vec[11] + vec[12]
+    if len(vec) == 14:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6] + vec[7] + vec[8] + vec[9] + vec[10] + vec[11] + vec[12] + vec[13]
+    if len(vec) == 15:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6] + vec[7] + vec[8] + vec[9] + vec[10] + vec[11] + vec[12] + vec[13] + vec[14]
+    if len(vec) == 16:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6] + vec[7] + vec[8] + vec[9] + vec[10] + vec[11] + vec[12] + vec[13] + vec[14] + vec[15]
+    if len(vec) == 17:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6] + vec[7] + vec[8] + vec[9] + vec[10] + vec[11] + vec[12] + vec[13] + vec[14] + vec[15] + vec[16]
+    if len(vec) == 18:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6] + vec[7] + vec[8] + vec[9] + vec[10] + vec[11] + vec[12] + vec[13] + vec[14] + vec[15] + vec[16] + vec[17]
+    if len(vec) == 19:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6] + vec[7] + vec[8] + vec[9] + vec[10] + vec[11] + vec[12] + vec[13] + vec[14] + vec[15] + vec[16] + vec[17] + vec[18]
+    if len(vec) == 20:
+        return vec[0] + vec[1] + vec[2] + vec[3] + vec[4] + vec[5] + vec[6] + vec[7] + vec[8] + vec[9] + vec[10] + vec[11] + vec[12] + vec[13] + vec[14] + vec[15] + vec[16] + vec[17] + vec[18] + vec[19]
+
+
 if __name__ == '__main__':
-    n = int(input())
-    a = [int(x) for x in input().split()]
-    a.sort()
-    print(a)
-    b = []
-    for i in range(len(a)):
-        if i == 0:
-            b.append(a[i])
-        else:
-            b.append(a[i] - a[i-1])
-    print(b)
-    c = []
-    for i in range(len(b)):
-        if i == 0:
-            c.append(b[i])
-        else:
-            c.append(b[i] + c[i-1])
-    print(c)
-    d = []
-    for i in range(len(c)):
-        if i == 0:
-            d.append(c[i])
-        else:
-            d.append(c[i] - c[i-1])
-    print(d)
-    e = []
-    for i in range(len(d)):
-        if i == 0:
-            e.append(d[i])
-        else:
-            e.append(d[i] + e[i-1])
-    print(e)
-    f = []
-    for i in range(len(e)):
-        if i == 0:
-            f.append(e[i])
-        else:
-            f.append(e[i] - e[i-1])
-    print(f)
-    g = []
-    for i in range(len(f)):
-        if i == 0:
-            g.append(f[i])
-        else:
-            g.append(f[i] + g[i-1])
-    print(g)
-    h = []
-    for i in range(len(g)):
-        if i == 0:
-            h.append(g[i])
-        else:
-            h.append(g[i] - g[i-1])
-    print(h)
-    i = []
-    for i in range(len(h)):
-        if i == 0:
-            i.append(h[i])
-        else:
-            i.append(h[i] + i[i-1])
-    print(i)
-    j = []
-    for i in range(len(i)):
-        if i == 0:
-            j.append(i[i])
-        else:
-            j.append(i[i] - i[i-1])
-    print(j)
-    k = []
-    for i in range(len(j)):
-        if i == 0:
-            k.append(j[i])
-        else:
-            k.append(j[i] + k[i-1])
-    print(k)
-    l = []
-    for i in range(len(k)):
-        if i == 0:
-            l.append(k[i])
-        else:
-            l.append(k[i] - k[i-1])
-    print(l)
-    m = []
-    for i in range(len(l)):
-        if i == 0:
-            m.append(l[i])
-        else:
-            m.append(l[i] + m[i-1])
-    print(m)
-    o = []
-    for i in range(len(m)):
-        if i == 0:
-            o.append(m[i])
-        else:
-            o.append(m[i] - m[i-1])
-    print(o)
-    p = []
-    for i in range(len(o)):
-        if i == 0:
-            p.append(o[i])
-        else:
-            p.append(o[i] + p[i-1])
-    print(p)
-    q = []
-    for i in range(len(p)):
-        if i == 0:
-            q.append(p[i])
-        else:
-            q.append(p[i] - p[i-1])
-    print(q)
-    r = []
-    for i in range(len(q)):
-        if i == 0:
-            r.append(q[i])
-        else:
-            r.append(q[i] + r[i-1])
-    print(r)
-    s = []
-    for i in range(len(r)):
-        if i == 0:
-            s.append(r[i])
-        else:
-            s.append(r[i] - r[i-1])
-    print(s)
-    t = []
-    for i in range(len(s)):
-        if i == 0:
-            t.append(s[i])
-        else:
-            t.append(s[i] + t[i-1])
-    print(t)
-    u = []
-    for i in range(len(t)):
-        if i == 0:
-            u.append(t[i])
-        else:
-            u.append(t[i] - t[i-1])
-    print(u)
-    v = []
-    for i in range(len(u)):
-        if i == 0:
-            v.append(u[i])
-        else:
-            v.append(u[i] + v[i-1])
-    print(v)
-    w = []
-    for i in range(len(v)):
-        if i == 0:
-            w.append(v[i])
-        else:
-            w.append(v[i] - v[i-1])
-    print(w)
-    x = []
-    for i in range(len(w)):
-        if i == 0:
-            x.append(w[i])
-        else:
-            x.append(w[i] + x[i-1])
-    print(x)
-    y = []
-    for i in range(len(x)):
-        if i == 0:
-            y.append(x[i])
-        else:
-            y.append(x[i] - x[i-1])
-    print(y)
-    z = []
-    for i in range(len(y)):
-        if i == 0:
-            z.append(y[i])
-        else:
-            z.append(y[i] + z[i-1])
-    print(z)
-    aa = []
-    for i in range(len(z)):
-        if i == 0:
-            aa.append(z[i])
-        else:
-            aa.append(z[i] - z[i-1])
-    print(aa)
-    bb = []
-    for i in range(len(aa)):
-        if i == 0:
-            bb.append(aa[i])
-        else:
-            bb.append(aa[i] + bb[i-1])
-    print(bb)
-    cc = []
-    for i in range(len(bb)):
-        if i == 0:
-            cc.append(bb[i])
-        else:
-            cc.append(bb[i] - bb[i-1])
-    print(cc)
-    dd = []
-    for i in range(len(cc)):
-        if i == 0:
-            dd.append(cc[i])
-        else:
-            dd.append(cc[i] + dd[i-1])
-    print(dd)
-    ee = []
-    for i in range(len(dd)):
-        if i == 0:
-            ee.append(dd[i])
-        else:
-            ee.append(dd[i] - dd[i-1])
-    print(ee)
-    ff = []
-    for i in range(len(ee
+    vec = [int(x) for x in input().strip().split()]
+    print(find_cut(vec))
