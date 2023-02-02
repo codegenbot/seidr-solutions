2053f4b6f1c51d5ffd445766ae30a44698e6fca2
BeepBoop <githubbot@vadim.me>
2023-02-02 18:23:16 +0100

    solution 0 of cut-vector, 0 of validation tests passed

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -7,44 +7,35 @@
 #include <math.h>
 #include <map>
 #include <set>
-#include <stack>
 #include <climits>
 using namespace std;
 /*
-Given a number of integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+For example,
+input:
+1
+0
+output:
+100
+input:
+1
+10
+output:
+1100
+input:
+1
+100
+output:
+11000
+input:
+1
+1000
+output:
+110000
+input:
+1
+10000
+output:
+1100000
 */
-
 int main() {
-    int n;
-    while(cin>>n) {
-        vector<int> a(n);
-        for(int i=0;i<n;i++) {
-            cin>>a[i];
-        }
-        int minDiff=INT_MAX;
-        int minIndex=0;
-        for(int i=0;i<n;i++) {
-            int sum1=0;
-            int sum2=0;
-            for(int j=0;j<i;j++) {
-                sum1+=a[j];
-            }
-            for(int j=i;j<n;j++) {
-                sum2+=a[j];
-            }
-            if(abs(sum1-sum2)<minDiff) {
-                minDiff=abs(sum1-sum2);
-                minIndex=i;
-            }
-        } 
-        cout<<minIndex<<endl;
-        for(int i=0;i<minIndex;i++) {
-            cout<<a[i]<<" ";
-        }cout<<endl;
-        cout<<n-minIndex<<endl;
-        for(int i=minIndex;i<n;i++) {
-            cout<<a[i]<<" ";
-        }cout<<endl;
-    }
-    return 0;
-}
