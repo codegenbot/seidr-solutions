aeeb289805b1f5b848b6d809b973e13c827b2542
BeepBoop <githubbot@vadim.me>
2023-02-07 02:33:21 +0100

    solution 1 of spin-words, 0.99 of validation tests passed

--- spin-words.cpp
+++ spin-words.cpp
@@ -9,6 +9,7 @@
 #include <set>
 #include <stack>
 #include <climits>
+#include <algorithm>
 using namespace std;
 /*
 Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.
@@ -37,6 +38,10 @@ hi
 int main() {
     string s;
     while (getline(cin, s)) {
+        if (s.size() == 0) {
+            cout << s << endl;
+            continue;
+        }
         int i = 0;
         while (i < s.size()) {
             int j = i;
