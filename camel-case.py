83e8dd9be3005d10cb1f5c5672d241fdcdb18efc
BeepBoop <githubbot@vadim.me>
2023-02-07 01:09:42 +0100

    solution 1 of camel-case, 0.01 of validation tests passed

--- camel-case.py
+++ camel-case.py
@@ -32,7 +32,11 @@ output:
 all separate words
 """
 if __name__ == '__main__':
-    s = input()
+    try:
+        s = input()
+    except EOFError:
+        print("")
+        sys.exit()
     s = s.split(' ')
     for i in range(len(s)):
         s[i] = s[i].split('-')
