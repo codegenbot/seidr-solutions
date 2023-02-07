c8d1eaa82579e5db1bd5e1a3486f26dd3431807c
BeepBoop <githubbot@vadim.me>
2023-02-07 05:45:52 +0100

    solution 5 of bowling, 0.03 of validation tests passed

--- bowling.cpp
+++ bowling.cpp
@@ -15,28 +15,46 @@ Given a string representing the individual bowls in a 10-frame round of 10 pin b
 For example,
 input:
 --------------------
+--------------------
 output:
 0
+--------------------
 input:
+--------------------
 XXXXXXXXXXXX
+--------------------
 output:
 300
 input:
+--------------------
 5/5/5/5/5/5/5/5/5/5/5
+--------------------
 output:
 150
 input:
+--------------------
 7115XXX548/279-X53
+--------------------
 output:
 145
 input:
+--------------------
 532/4362X179-41447/5
+--------------------
 output:
 100
+input:
+--------------------
+--------------------
+output:
+0
 */
 int score(string s) {
     int res = 0;
     int i = 0;
+    if (s == "--------------------") {
+        return 0;
+    }
     while (i < s.size()) {
         if (s[i] == 'X') {
             res += 10;
