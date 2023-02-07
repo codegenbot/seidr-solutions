ed1dd2440a817214ec1109034d98c06c426afdbf
BeepBoop <githubbot@vadim.me>
2023-02-07 06:50:51 +0100

    solution 28 of bowling, 0.56 of validation tests passed

--- bowling.cpp
+++ bowling.cpp
@@ -34,54 +34,36 @@ input:
 output:
 100
 */
-int getScore(string s) {
-    int score = 0;
-    int i = 0;
-    while (i < s.size()) {
-        if (s[i] == 'X') {
-            score += 10;
-            if (i + 1 < s.size()) {
-                if (s[i + 1] == 'X') {
-                    score += 10;
-                    if (i + 2 < s.size()) {
-                        if (s[i + 2] == 'X') {
-                            score += 10;
-                        } else {
-                            score += s[i + 2] - '0';
-                        }
-                    }
-                } else if (s[i + 1] == '/') {
-                    score += 10;
+int cal(string s) {
+    int res = 0;
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == 'X') {
+            if(i+2 < s.size()) {
+                if(s[i+1] == 'X') {
+                    if(s[i+2] == 'X') res += 30;
+                    else res += 20 + s[i+2] - '0';
                 } else {
-                    score += s[i + 1] - '0';
+                    if(s[i+2] == '/') res += 20;
+                    else res += 10 + (s[i+1] - '0') + (s[i+2] - '0');
                 }
             }
-            i++;
-        } else if (s[i] == '/') {
-            score += 10;
-            if (i - 1 >= 0) {
-                score -= s[i - 1] - '0';
+        } else if(s[i] == '/') {
+            if(i+1 < s.size()) {
+                if(s[i-1] == 'X') res += 10 + s[i+1] - '0';
+                else res += 10 + s[i+1] - '0' - (s[i-1] - '0');
             }
-            if (i + 1 < s.size()) {
-                if (s[i + 1] == 'X') {
-                    score += 10;
-                } else {
-                    score += s[i + 1] - '0';
-                }
-            }
-            i++;
-        } else if (s[i] == '-') {
-            i++;
+        } else if(s[i] == '-') {
+            continue;
         } else {
-            score += s[i] - '0';
-            i++;
+            res += s[i] - '0';
         }
     }
-    return score;
+    return res;
 }
 int main() {
     string s;
-    cin >> s;
-    cout << getScore(s) << endl;
+    while(cin >> s) {
+        cout << cal(s) << endl;
+    }
     return 0;
 }
