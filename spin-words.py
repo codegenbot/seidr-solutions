124ceec266af93209671fcf441a8cbd6d2ed60f0
BeepBoop <githubbot@vadim.me>
2023-02-07 01:03:39 +0100

    solution 0 of spin-words, 0.99 of validation tests passed

--- spin-words.py
+++ spin-words.py
@@ -32,10 +32,7 @@ output:
 hi
 """
 if __name__ == '__main__':
-    try:
-        s = input()
-    except EOFError:
-        print("")
+    s = input()
     s = s.split()
     for i in range(len(s)):
         if len(s[i]) >= 5:
