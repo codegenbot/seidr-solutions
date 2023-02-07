703ed22f46010d7e3721e1191a7852d5fcfed115
BeepBoop <githubbot@vadim.me>
2023-02-07 09:25:12 +0100

    solution 0 of cut-vector, 0 of validation tests passed

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -13,61 +13,15 @@ using namespace std;
 /*
 Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
 For example,
-input:
-1
-0
-output:
-100
-input:
-1
-10
-output:
-1100
-input:
-1
-100
-output:
-11000
-input:
-1
-1000
-output:
-110000
-input:
-1
-10000
-output:
-1100000
+input: 1 0
+output: 100
+input: 1 10
+output: 1100
+input: 1 100
+output: 11000
+input: 1 1000
+output: 110000
+input: 1 10000
+output: 1100000
 */
 int main() {
-    vector<int> vec = {1,0};
-    int l = 0, r = vec.size()-1;
-    if(vec.size() == 1) {
-        cout << vec[0] << vec[0] << endl;
-        return 0;
-    }
-    int lsum = vec[l], rsum = vec[r];
-    while(l+1 < r) {
-        if(lsum < rsum) {
-            l++;
-            lsum += vec[l];
-        } else {
-            r--;
-            rsum += vec[r];
-        }
-    }
-    if(lsum == rsum) {
-        for(int i=0; i<=l; i++) cout << vec[i];
-        for(int i=r; i<vec.size(); i++) cout << vec[i];
-    } else {
-        if(lsum > rsum) {
-            for(int i=0; i<=l; i++) cout << vec[i];
-            for(int i=r; i<=l; i++) cout << vec[i];
-        } else {
-            for(int i=0; i<r; i++) cout << vec[i];
-            for(int i=r; i<vec.size(); i++) cout << vec[i];
-        }
-    }
-    cout << endl;
-    return 0;
-}
