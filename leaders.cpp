138e897852bf9306bef15c5b3ef2bb816e1e0c74
BeepBoop <githubbot@vadim.me>
2023-02-07 01:37:48 +0100

    solution 0 of leaders, 0 of validation tests passed

--- leaders.cpp
+++ leaders.cpp
@@ -42,10 +42,6 @@ output:
 int main() {
     int n;
     cin >> n;
-    if (n == 0) {
-        cout << 0 << endl;
-        return 0;
-    }
     vector<int> nums(n);
     for (int i = 0; i < n; i++) {
         cin >> nums[i];
