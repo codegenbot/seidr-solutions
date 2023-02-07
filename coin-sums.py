e15fb18a8f9b5cd64f21347ab0f672d4896ddcfd
BeepBoop <githubbot@vadim.me>
2023-02-07 03:26:46 +0100

    solution 2 of coin-sums, 0.02 of validation tests passed

--- coin-sums.py
+++ coin-sums.py
@@ -11,33 +11,43 @@ import re
 Given a number of cents, ﬁnd the fewest number of US coins (pennies, nickles, dimes, quarters) needed to make that amount, and return the number of each type of coin as a separate output.
 For example,
 input:
+6
+output:
+1
+input:
 1
 output:
-1000
+1
 input:
-2
+0
 output:
-2000
+0
 input:
-3
-output:
-3000
-input:
-4
-output:
-4000
-input:
-5
-output:
-0100
+
 """
 if __name__ == '__main__':
     cents = int(input())
-    quarters = cents // 25
-    dimes = (cents - quarters * 25) // 10
-    nickles = (cents - quarters * 25 - dimes * 10) // 5
-    pennies = (cents - quarters * 25 - dimes * 10 - nickles * 5)
-    print(quarters)
-    print(dimes)
-    print(nickles)
-    print(pennies)
+    if cents >= 25:
+        quarters = cents // 25
+        dimes = (cents - quarters * 25) // 10
+        nickles = (cents - quarters * 25 - dimes * 10) // 5
+        pennies = (cents - quarters * 25 - dimes * 10 - nickles * 5)
+        print(quarters)
+        print(dimes)
+        print(nickles)
+        print(pennies)
+    elif cents >= 10:
+        dimes = cents // 10
+        nickles = (cents - dimes * 10) // 5
+        pennies = (cents - dimes * 10 - nickles * 5)
+        print(dimes)
+        print(nickles)
+        print(pennies)
+    elif cents >= 5:
+        nickles = cents // 5
+        pennies = (cents - nickles * 5)
+        print(nickles)
+        print(pennies)
+    else:
+        pennies = cents
+        print(pennies)
