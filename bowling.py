e33b046ba685a74ab9b466f0e58859f82afd9b7e
BeepBoop <githubbot@vadim.me>
2023-02-07 07:14:41 +0100

    solution 4 of bowling, 0.01 of validation tests passed

--- bowling.py
+++ bowling.py
@@ -34,38 +34,62 @@ output:
 
 def bowling_score(input):
     score = 0
-    frame = 0
+    frame = 1
     frame_score = 0
+    # iterate through the input string, starting at the first character
     for i in range(len(input)):
+        # if the current character is X, then the frame score is 10 and the frame is over
         if input[i] == 'X':
             frame_score = 10
+            frame += 1
+            # if the current character is not the last character in the string
             if i < len(input) - 2:
+                # if the next character is X, then the frame score is 20
                 if input[i+1] == 'X':
                     frame_score += 10
+                    # if the next character is X, then the frame score is 30
                     if input[i+2] == 'X':
                         frame_score += 10
+                    # if the next character is not X, then the frame score is 10 + the next character
                     else:
                         frame_score += int(input[i+2])
+                # if the next character is not X, then the frame score is 10 + the next character
                 else:
                     frame_score += int(input[i+1])
+                    # if the next character is /, then the frame score is 20
                     if input[i+2] == '/':
                         frame_score += 10 - int(input[i+1])
+                    # if the next character is not /, then the frame score is 10 + the next character
                     else:
                         frame_score += int(input[i+2])
+        # if the current character is /, then the frame score is 10 and the frame is over
         elif input[i] == '/':
             frame_score = 10
+            frame += 1
+            # if the current character is not the last character in the string
             if i < len(input) - 1:
+                # if the next character is X, then the frame score is 20
                 if input[i+1] == 'X':
                     frame_score += 10
+                # if the next character is not X, then the frame score is 10 + the next character
                 else:
                     frame_score += int(input[i+1])
+        # if the current character is not X or /, then the frame score is the current character and the frame is over
         else:
-            frame_score = int(input[i])
+            if input[i] == '-':
+                frame_score = 0
+            else:
+                frame_score = int(input[i])
+            frame += 1
+        # add the frame score to the total score
         score += frame_score
+        # reset the frame score
         frame_score = 0
-        frame += 1
+        # if the frame is 10, then break out of the loop
         if frame == 10:
             break
+    # return the total score
     return score
 
 if __name__ == '__main__':
+    print(bowling_score('--------------------'))
