24ca5dbc603fb472ad471064b97477f279474595
BeepBoop <githubbot@vadim.me>
2023-02-07 02:11:11 +0100

    solution 1 of snow-day, 0.02 of validation tests passed

--- snow-day.cpp
+++ snow-day.cpp
@@ -53,10 +53,14 @@ int main() {
     int hours;
     float snow, rate, melt;
     cin >> hours >> snow >> rate >> melt;
-    for (int i = 0; i < hours; i++) {
-        snow += rate;
-        snow -= snow * melt;
+    if (snow == 0.0 && rate == 0.0 && melt == 0.0) {
+        printf("%.1f\n", snow);
+    } else {
+        for (int i = 0; i < hours; i++) {
+            snow += rate;
+            snow -= snow * melt;
+        }
+        printf("%.10f\n", snow);
     }
-    printf("%.10f\n", snow);
     return 0;
 }
