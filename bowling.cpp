d9fdab92b465a3db4d9291930fbb27aa8ecd4420
BeepBoop <githubbot@vadim.me>
2023-02-07 06:58:42 +0100

    solution 0 of bowling, 0 of validation tests passed

--- bowling.cpp
+++ bowling.cpp
@@ -35,29 +35,12 @@ output:
 100
 */
 int score(string s) {
-    if (s == "--------------------") {
-        return 0;
-    }
-    if (s == "XXXXXXXXXXXX") {
-        return 300;
-    }
-    if (s == "5/5/5/5/5/5/5/5/5/5/5") {
-        return 150;
-    }
-    if (s == "7115XXX548/279-X53") {
-        return 145;
-    }
-    if (s == "532/4362X179-41447/5") {
-        return 100;
-    }
     int res = 0;
     int i = 0;
     while (i < s.size()) {
-        // X
         if (s[i] == 'X') {
             res += 10;
             if (i + 1 < s.size()) {
-                // XX
                 if (s[i + 1] == 'X') {
                     res += 10;
                     if (i + 2 < s.size()) {
@@ -67,73 +50,42 @@ int score(string s) {
                             res += s[i + 2] - '0';
                         }
                     }
-                // X/
                 } else if (s[i + 1] == '/') {
                     res += 10;
                     if (i + 2 < s.size()) {
                         res += s[i + 2] - '0';
                     }
-                // X1
                 } else {
                     res += s[i + 1] - '0';
-                    if (i + 2 < s.size()) {
-                        if (s[i + 2] == '/') {
-                            res += 10 - (s[i + 1] - '0');
-                        } else {
-                            res += s[i + 2] - '0';
-                        }
-                    }
                 }
             }
             i++;
-        // /
         } else if (s[i] == '/') {
-            // 这里要注意，如果是/，那么前一个数字一定是1-9，所以不用判断i - 1 >= 0
             res += 10;
+            if (i - 1 >= 0) {
+                res -= s[i - 1] - '0';
+            }
             if (i + 1 < s.size()) {
-                // /X
-                if (s[i + 1] == 'X') {
-                    res += 10;
-                    i++;
-                // /1
-                } else {
-                    res += s[i + 1] - '0';
-                    i++;
-                }
+                res += s[i + 1] - '0';
             }
             i++;
-        // 1
+        } else if (s[i] == '-') {
+            i++;
         } else {
             res += s[i] - '0';
-            if (i + 1 < s.size()) {
-                // 1/
-                if (s[i + 1] == '/') {
-                    res += 10 - (s[i] - '0');
-                    i++;
-                // 1X
-                } else if (s[i + 1] == 'X') {
-                    res += 10;
-                    if (i + 2 < s.size()) {
-                        if (s[i + 2] == 'X') {
-                            res += 10;
-                        } else {
-                            res += s[i + 2] - '0';
-                        }
-                    }
-                    i++;
-                // 1-
-                } else if (s[i + 1] == '-') {
-                    i++;
-                }
-            }
+            i++;
         }
-        i++;
     }
     return res;
 }
 int main() {
-    string s;
-    cin >> s;
+    string s = "XXXXXXXXXXXX";
+    cout << score(s) << endl;
+    s = "5/5/5/5/5/5/5/5/5/5/5";
+    cout << score(s) << endl;
+    s = "7115XXX548/279-X53";
+    cout << score(s) << endl;
+    s = "532/4362X179-41447/5";
     cout << score(s) << endl;
     return 0;
 }
