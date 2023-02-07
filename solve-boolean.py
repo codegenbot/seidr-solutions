164c6d6321684f17aef722da4c569254e7a06044
BeepBoop <githubbot@vadim.me>
2023-02-07 01:57:32 +0100

    solution 10 of solve-boolean, 0.94 of validation tests passed

--- solve-boolean.py
+++ solve-boolean.py
@@ -36,10 +36,10 @@ if __name__ == '__main__':
     res = True
     for i in range(len(input_str) - 1):
         if input_str[i] == '&':
-            if input_str[i - 1] == 'F' or input_str[i + 1] == 'F':
+            if input_str[i - 1] == 'F' or input_str[i + 1] == 'F' or input_str[i - 1] == 'f' or input_str[i + 1] == 'f':
                 res = False
         if input_str[i] == '|':
-            if input_str[i - 1] == 'T' or input_str[i + 1] == 'T':
+            if input_str[i - 1] == 'T' or input_str[i + 1] == 'T' or input_str[i - 1] == 't' or input_str[i + 1] == 't':
                 res = True
     if res:
         print('True')
