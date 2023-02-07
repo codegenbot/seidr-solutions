0fc60dd43766cd2ba16fd843500fffc1d44d02d9
BeepBoop <githubbot@vadim.me>
2023-02-07 01:59:32 +0100

    solution 0 of bowling, 0 of validation tests passed

--- bowling.py
+++ bowling.py
@@ -38,7 +38,7 @@ def bowling_score(input):
     frame_score = 0
     for i in range(len(input)):
         if input[i] == 'X':
-            frame_score += 10
+            frame_score = 10
             if i < len(input) - 2:
                 if input[i+1] == 'X':
                     frame_score += 10
@@ -53,27 +53,19 @@ def bowling_score(input):
                     else:
                         frame_score += int(input[i+2])
         elif input[i] == '/':
-            frame_score += 10
+            frame_score = 10
             if i < len(input) - 1:
                 if input[i+1] == 'X':
                     frame_score += 10
                 else:
                     frame_score += int(input[i+1])
         else:
-            frame_score += int(input[i])
-        if frame < 9:
-            if frame_score >= 10:
-                score += frame_score
-                frame_score = 0
-                frame += 1
-            elif i == len(input) - 1:
-                score += frame_score
-                frame_score = 0
-                frame += 1
-        else:
-            score += frame_score
-            frame_score = 0
-            frame += 1
+            frame_score = int(input[i])
+        score += frame_score
+        frame_score = 0
+        frame += 1
+        if frame == 10:
+            break
     return score
 
 if __name__ == '__main__':
