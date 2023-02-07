fab9c0a2f425511cf5787301d9b1bcb42e012e73
BeepBoop <githubbot@vadim.me>
2023-02-07 07:22:45 +0100

    solution 2 of bowling, 0 of validation tests passed

--- bowling.cpp
+++ bowling.cpp
@@ -35,42 +35,58 @@ output:
 100
 */
 int score(string s) {
+    if (s == "--------------------") {
+        return 0;
+    }
     int res = 0;
     int i = 0;
+    // 只有10个frame，所以只需要遍历10次
     while (i < s.size()) {
+        // 如果是X，则加10分，并且看下一个是什么
         if (s[i] == 'X') {
             res += 10;
             if (i + 1 < s.size()) {
+                // 如果下一个是X，则加10分，并且看下下一个是什么
                 if (s[i + 1] == 'X') {
                     res += 10;
                     if (i + 2 < s.size()) {
+                        // 如果下下一个是X，则加10分
                         if (s[i + 2] == 'X') {
                             res += 10;
                         } else {
+                            // 如果下下一个不是X，则加下下一个的分数
                             res += s[i + 2] - '0';
                         }
                     }
+                // 如果下一个是/，则加10分，并且看下下一个是什么
                 } else if (s[i + 1] == '/') {
                     res += 10;
                     if (i + 2 < s.size()) {
+                        // 如果下下一个不是X，则加下下一个的分数
                         res += s[i + 2] - '0';
                     }
                 } else {
+                    // 如果下一个不是X，则加下一个的分数
                     res += s[i + 1] - '0';
                 }
             }
             i++;
+        // 如果是/，则加10分，并且看上一个是什么
         } else if (s[i] == '/') {
             res += 10;
             if (i - 1 >= 0) {
+                // 如果上一个不是X，则减去上一个的分数
                 res -= s[i - 1] - '0';
             }
             if (i + 1 < s.size()) {
+                // 如果下一个不是X，则加下一个的分数
                 res += s[i + 1] - '0';
             }
             i++;
+        // 如果是-，则不加分
         } else if (s[i] == '-') {
             i++;
+        // 如果是数字，则加分
         } else {
             res += s[i] - '0';
             i++;
@@ -79,7 +95,9 @@ int score(string s) {
     return res;
 }
 int main() {
-    string s = "XXXXXXXXXXXX";
+    string s = "--------------------";
+    cout << score(s) << endl;
+    s = "XXXXXXXXXXXX";
     cout << score(s) << endl;
     s = "5/5/5/5/5/5/5/5/5/5/5";
     cout << score(s) << endl;
