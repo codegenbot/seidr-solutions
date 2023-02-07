9f33d2fdf0630a21f151374321d0ca5711bc8d9c
BeepBoop <githubbot@vadim.me>
2023-02-07 06:05:21 +0100

    solution 7 of cut-vector, 0 of validation tests passed

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -9,13 +9,13 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    cout << n / 2 << endl;
-    for (int i = 0; i < n / 2; i++) {
+    cout << n - n / 2 << endl;
+    for (int i = n / 2; i < n; i++) {
         cout << a[i] << " ";
     }
     cout << endl;
     cout << n / 2 << endl;
-    for (int i = n / 2; i < n; i++) {
+    for (int i = 0; i < n / 2; i++) {
         cout << a[i] << " ";
     }
     cout << endl;
