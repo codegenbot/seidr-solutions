5182244f4949746898ffbfec7510a73a5e6ab664
BeepBoop <githubbot@vadim.me>
2023-02-08 18:26:01 +0100

    solution 3 of cut-vector, 0 of validation tests passed

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -11,59 +11,25 @@
 #include <climits>
 using namespace std;
 /*
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
-For example,
-input:
-1
-0
-output:
-100
-input:
-1
-10
-output:
-1100
-input:
-1
-100
-output:
-11000
-input:
-1
-1000
-output:
-110000
-input:
-1
-10000
-output:
-1100000
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs. For example, input: 1 0 output: 100 input: 1 10 output: 1100 input: 1 100 output: 11000 input: 1 1000 output: 110000 input: 1 10000 output: 1100000
 */
 int main() {
-    int n;
-    cin >> n;
-    vector<int> nums;
-    while (n) {
-        nums.push_back(n % 10);
-        n /= 10;
+    int a, b;
+    cin >> a >> b;
+    vector<int> res1, res2;
+    while (a) {
+        res1.push_back(a % 10);
+        a /= 10;
     }
-    int len = nums.size();
-    int left = 0, right = len - 1;
-    while (left < right) {
-        if (nums[left] == nums[right]) {
-            left++;
-            right--;
-        } else if (nums[left] < nums[right]) {
-            nums[left + 1] += nums[left];
-            left++;
-        } else {
-            nums[right - 1] += nums[right];
-            right--;
-        }
+    while (b) {
+        res2.push_back(b % 10);
+        b /= 10;
     }
-    for (int i = 0; i < len; i++) {
-        cout << nums[i];
+    for (int i = res1.size() - 1; i >= 0; i--) {
+        cout << res1[i] << endl;
     }
-    cout << endl;
+    for (int i = res2.size() - 1; i >= 0; i--) {
+        cout << res2[i] << endl;
+    }
     return 0;
 }
