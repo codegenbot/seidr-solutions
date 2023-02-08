46f46277cbe408a87c83f12175d250feb5b32d85
BeepBoop <githubbot@vadim.me>
2023-02-08 19:03:18 +0100

    solution 0 of luhn, 0.1 of validation tests passed

--- luhn.cpp
+++ luhn.cpp
@@ -47,8 +47,8 @@ int main() {
         cin >> nums[i];
     }
     int sum = 0;
-    for (int i = n - 1; i > 0; i--) {
-        if (i % 2 == 1) {
+    for (int i = n - 1; i >= 0; i--) {
+        if (i % 2 == 0) {
             sum += nums[i];
         } else {
             int tmp = nums[i] * 2;
@@ -58,7 +58,6 @@ int main() {
             sum += tmp;
         }
     }
-    sum += nums[0];
     cout << sum << endl;
     return 0;
 }
