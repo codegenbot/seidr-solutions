1970d3bf97bff3bd8e8882cf87395d1fe29f6b53
BeepBoop <githubbot@vadim.me>
2023-02-08 17:44:32 +0100

    solution 0 of cut-vector, 0 of validation tests passed

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -11,65 +11,53 @@
 #include <climits>
 using namespace std;
 /*
-Given a positive integer n and a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
-For example:
-Input:
-2
-2
-129
-Output:
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+For example,
+input:
 1
-2
-1
-129
-Input:
-2
-2
-10
-Output:
-1
-2
+0
+output:
+100
+input:
 1
 10
+output:
+1100
+input:
+1
+100
+output:
+11000
+input:
+1
+1000
+output:
+110000
+input:
+1
+10000
+output:
+1100000
 */
 int main() {
     int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
+    while (cin >> n) {
+        if (n == 0) {
+            cout << 0 << endl;
+            continue;
+        }
+        int cnt = 0;
+        while (n) {
+            cnt++;
+            n >>= 1;
+        }
+        for (int i = 0; i < cnt; i++) {
+            cout << 1;
+        }
+        for (int i = 0; i < cnt; i++) {
+            cout << 0;
+        }
+        cout << endl;
     }
-    int min_dif = INT_MAX;
-    int pos = -1;
-    if (v.size() == 1) {
-        for (int i = 0; i < n; i++) {
-            cout << "1" << endl;
-            cout << v[i] << endl;
-        }
-    } else {
-        for (int i = 1; i < n; i++) {
-            int dif = abs(v[i] - v[i - 1]);
-            if (dif < min_dif) {
-                min_dif = dif;
-                pos = i;
-            }
-        }
-        if (v[0] == v[n - 1]) {
-            for (int i = 0; i < n; i++) {
-                cout << "1" << endl;
-                cout << v[i] << endl;
-            }
-        } else {
-            for (int i = 0; i < pos; i++) {
-                cout << "1" << endl;
-                cout << v[i] << endl;
-            }
-            for (int i = pos; i < n; i++) {
-                cout << "1" << endl;
-                cout << v[i] << endl;
-            }
-        }
-    }
-
     return 0;
 }
