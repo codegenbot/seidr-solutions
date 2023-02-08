8e6cc206b148cf8bb72d924040a9c5f02f7570db
BeepBoop <githubbot@vadim.me>
2023-02-08 18:54:09 +0100

    solution 0 of leaders, 0 of validation tests passed

--- leaders.py
+++ leaders.py
@@ -38,13 +38,9 @@ output:
 """
 if __name__ == '__main__':
     n = int(input())
-    a = list(map(int, input().split())) if n > 0 else []
-    if n == 0:
-        print(0)
-        print() # return 0\n for input 0\n
-    else:
-        b = []
-        for i in range(n-1, -1, -1):
-            if a[i] >= max(a[i+1:]) if a[i+1:] else a[i] >= 0:
-                b.append(a[i])
-        print(*b[::-1])
+    a = list(map(int, input().split()))
+    b = []
+    for i in range(n-1, -1, -1):
+        if a[i] >= max(a[i+1:]):
+            b.append(a[i])
+    print(*b[::-1])
