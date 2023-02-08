f177ba49e2277ccb9cb345d8a8c8c847fe329a4b
BeepBoop <githubbot@vadim.me>
2023-02-08 17:36:21 +0100

    solution 35 of coin-sums, 0.03 of validation tests passed

--- coin-sums.cpp
+++ coin-sums.cpp
@@ -1,47 +1,14 @@
-#include <vector>
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <queue>
 #include <stdio.h>
-#include <math.h>
-#include <map>
-#include <set>
-#include <stack>
-#include <climits>
-using namespace std;
-/*
-Given a number of cents, ﬁnd the fewest number of US coins (pennies, nickles, dimes, quarters) needed to make that amount, and return the number of each type of coin as a separate output.
-For example,
-input:
-1
-output:
-1000
-input:
-2
-output:
-2000
-input:
-3
-output:
-3000
-input:
-4
-output:
-4000
-input:
-5
-output:
-0100
-*/
-int main() {
-    int cents;
-    cin >> cents;
-    int quarter = cents / 25;
-    int dime = (cents % 25) / 10;
-    int nickle = ((cents % 25) % 10) / 5;
-    int penny = (((cents % 25) % 10) % 5) / 1;
-    int res = quarter * 1000 + dime * 100 + nickle * 10 + penny;
-    cout << res << endl;
-    return 0;
+int main(){
+  int cents;
+  scanf("%d", &cents);
+  int quarters = cents / 25;
+  cents = cents % 25;
+  int dimes = cents / 10;
+  cents = cents % 10;
+  int nickles = cents / 5;
+  cents = cents % 5;
+  int pennies = cents;
+  printf("%d\n%d\n%d\n%d\n", quarters, dimes, nickles, pennies);
+  return 0;
 }
