516afbba0f5b3bd6d2ce295006bd7e0f6e400d89
BeepBoop <githubbot@vadim.me>
2023-02-09 05:56:50 +0100

    solution 0 of fuel-cost, 0.25 of validation tests passed

--- fuel-cost.py
+++ fuel-cost.py
@@ -39,7 +39,5 @@ output:
 if __name__ == '__main__':
     n = int(input())
     for i in range(n):
-        x = input().split()
-        for j in range(len(x)):
-            x[j] = int(x[j])
-        print(x[j]//3-2)
+        x = int(input())
+        print(x//3-2)
