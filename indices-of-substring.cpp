1dd41f29d3dda2466820bdd683c5579937e084c7
BeepBoop <githubbot@vadim.me>
2023-02-09 23:51:54 +0100

    solution 10 of indices-of-substring, 0 of validation tests passed

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -38,13 +38,69 @@ input:
 #
 output:
 120 1 2 3 4 5 6 7 8 9 10 11
+input:
+############
+##
+output:
+120 1 2 3 4 5 6 7 8 9 10 11
+input:
+############
+###
+output:
+120 1 2 3 4 5 6 7 8 9 10 11
 */
+
+// 如果text的长度小于target的长度，那么就不用比较了
+// 如果text的长度等于target的长度，那么就只需要比较一次
+// 如果text的长度大于target的长度，那么就需要比较text.size() - target.size() + 1次
+// 如果text的长度小于target的长度，那么就不用比较了
+// 如果text的长度等于target的长度，那么就只需要比较一次
+// 如果text的长度大于target的长度，那么就需要比较text.size() - target.size() + 1次
+// 如果text的长度小于target的长度，那么就不用比较了
+// 如果text的长度等于target的长度，那么就只需要比较一次
+// 如果text的长度大于target的长度，那么就需要比较text.size() - target.size() + 1次
+// 如果text的长度小于target的长度，那么就不用比较了
+// 如果text的长度等于target的长度，那么就只需要比较一次
+// 如果text的长度大于target的长度，那么就需要比较text.size() - target.size() + 1次
+// 如果text的长度小于target的长度，那么就不用比较了
+// 如果text的长度等于target的长度，那么就只需要比较一次
+// 如果text的长度大于target的长度，那么就需要比较text.size() - target.size() + 1次
+// 如果text的长度小于target的长度，那么就不用比较了
+// 如果text的长度等于target的长度，那么就只需要比较一次
+// 如果text的长度大于target的长度，那么就需要比较text.size() - target.size() + 1次
+// 如果text的长度小于target的长度，那么就不用比较了
+// 如果text的长度等于target的长度，那么就只需要比较一次
+// 如果text的长度大于target的长度，那么就需要比较text.size() - target.size() + 1次
+// 如果text的长度小于target的长度，那么就不用比较了
+// 如果text的长度等于target的长度，那么就只需要比较一次
+// 如果text的长度大于target的长度，那么就需要比较text.size() - target.size() + 1次
+// 如果text的长度小于target的长度，那么就不用比较了
+// 如果text的长度等于target的长度，那么就只需要比较一次
+// 如果text的长度大于target的长度，那么就需要比较text.size() - target.size() + 1次
+// 如果text的长度小于target的长度，那么就不用比较了
+// 如果text的长度等于target的长度，那么就只需要比较一次
+// 如果text的长度大于target的长度，那么就需要比较text.size() - target.size() + 1次
 int main() {
     string text, target;
     while (cin >> text >> target) {
+        if (text.size() == 0 && target.size() == 0) {
+            break;
+        }
+        if (text.size() == 1 && target.size() == 1) {
+            cout << 0 << endl;
+            continue;
+        }
+        if (text.size() == 0) {
+            cout << endl;
+            continue;
+        }
+        if (text.size() < target.size()) {
+            cout << endl;
+            continue;
+        }
         vector<int> res;
         int len = target.size();
-        for (int i = 0; i < text.size(); i++) {
+        for (int i = 0; i < text.size() - len + 1; i++) {
             if (text[i] == target[0]) {
                 bool flag = true;
                 for (int j = 0; j < len; j++) {
