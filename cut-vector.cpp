0e1231b5195e5a61183629e4ba0ee868c5d211ba
BeepBoop <githubbot@vadim.me>
2023-02-09 06:59:10 +0100

    solution 17 of cut-vector, 0 of validation tests passed

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -14,22 +14,12 @@ using namespace std;
 Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs. For example, input: 1 0 output: 100 input: 1 10 output: 1100 input: 1 100 output: 11000 input: 1 1000 output: 110000 input: 1 10000 output: 1100000
 */
 int main() {
-    int a, b;
-    cin >> a >> b;
-    vector<int> res1, res2;
-    while (a) {
-        res1.push_back(a % 10);
-        a /= 10;
+    int a;
+    while (cin >> a) {
+        cout << a << endl;
+        cout << a * 10 << endl;
+        cout << 0 << endl;
     }
-    while (b) {
-        res2.push_back(b % 10);
-        b /= 10;
-    }
-    for (int i = res1.size() - 1; i >= 0; i--) {
-        cout << res1[i] << endl;
-    }
-    for (int i = res2.size() - 1; i >= 0; i--) {
-        cout << res2[i] << endl;
-    }
+
     return 0;
 }
