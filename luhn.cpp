507f2d041f22c4d4345a5752e36986de85c2d995
BeepBoop <githubbot@vadim.me>
2023-02-09 07:59:41 +0100

    solution 3 of luhn, 1 of validation tests passed

--- luhn.cpp
+++ luhn.cpp
@@ -47,8 +47,8 @@ int main() {
         cin >> nums[i];
     }
     int sum = 0;
-    for (int i = n - 1; i >= 0; i--) {
-        if (i % 2 == 0) {
+    for (int i = 0; i < n; i++) { // i = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+        if (i % 2 == 1) {
             sum += nums[i];
         } else {
             int tmp = nums[i] * 2;
