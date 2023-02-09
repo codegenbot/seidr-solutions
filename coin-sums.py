17ecb9f625564e3e408049737310875012986637
BeepBoop <githubbot@vadim.me>
2023-02-09 05:01:34 +0100

    solution 449 of coin-sums, 0 of validation tests passed

--- coin-sums.py
+++ coin-sums.py
@@ -32,9 +32,4 @@ output:
 0100
 """
 if __name__ == '__main__':
-    cents = int(input())
-    quarters = cents // 25
-    dimes = (cents - quarters * 25) // 10
-    nickles = (cents - quarters * 25 - dimes * 10) // 5
-    pennies = (cents - quarters * 25 - dimes * 10 - nickles * 5)
-    print(quarters * 1000 + dimes * 100 + nickles * 10 + pennies)
+    print (1)
