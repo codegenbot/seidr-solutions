6604cd0026120940303adf59240dba170a8699a1
BeepBoop <githubbot@vadim.me>
2023-02-15 00:26:13 +0100

    solution 0 of bowling, 0.02 of validation tests passed

--- bowling.cpp
+++ bowling.cpp
@@ -69,9 +69,6 @@ int score(string s) {
             i++;
         } else if (s[i] == '/') {
             res += 10;
-            if (i - 1 >= 0) {
-                res -= s[i - 1] - '0';
-            }
             if (i + 1 < s.size()) {
                 if (s[i + 1] == 'X') {
                     res += 10;
@@ -80,12 +77,26 @@ int score(string s) {
                 }
             }
             i++;
-        } else if (s[i] == '-') {
-            i++;
         } else {
             res += s[i] - '0';
-            i++;
+            if (i + 1 < s.size()) {
+                if (s[i + 1] == '/') {
+                    res += 10 - (s[i] - '0');
+                    i++;
+                } else if (s[i + 1] == 'X') {
+                    res += 10;
+                    if (i + 2 < s.size()) {
+                        if (s[i + 2] == 'X') {
+                            res += 10;
+                        } else {
+                            res += s[i + 2] - '0';
+                        }
+                    }
+                    i++;
+                }
+            }
         }
+        i++;
     }
     return res;
 }
