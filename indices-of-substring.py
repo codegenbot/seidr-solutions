483980dfed77af8fe08f304c3e907550e8631036
BeepBoop <githubbot@vadim.me>
2023-02-15 00:26:24 +0100

    solution 0 of indices-of-substring, 0 of validation tests passed

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -15,33 +15,37 @@ a
 5
 output:
 0
+
 input:
 !
 !
 output:
-10
+1
+0
 input:
 r
 nm,xcnwqnd@#$fwkdjn3
 output:
 0
+
 input:
 hi
 hihihihihihihihihihi
 output:
 0
+
 input:
 ############
 #
 output:
-120 1 2 3 4 5 6 7 8 9 10 11
+12
+0 1 2 3 4 5 6 7 8 9 10 11
 """
 if __name__ == '__main__':
     text = input()
     target = input()
-    print(text, target)
-    result = []
+    indices = []
     for i in range(len(text)):
         if text[i:i+len(target)] == target:
-            result.append(i)
-    print(*result)
+            indices.append(i)
+    print(' '.join(map(str, indices)))
