36700386cfdc4cf706051aea74a28ced03bbceae
BeepBoop <githubbot@vadim.me>
2023-02-15 20:26:29 +0100

    solution 437 of cut-vector, 0 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -8,8 +8,7 @@ import itertools
 import queue
 import re
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal,
-or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
 For example,
 input:
 1
@@ -53,11 +52,28 @@ output:
 
 """
 if __name__ == '__main__':
-    n = int(input())
-    if n == 1:
-        print(1)
+    input_list = []
+    while True:
+        try:
+            line = input()
+            input_list.append(line)
+        except:
+            break
+    input_list = [int(x) for x in input_list]
+    if len(input_list) == 2:
+        print(input_list[0])
+        print(input_list[1])
         print(0)
     else:
-        print(1)
-        print(n)
+        min_diff = sys.maxsize
+        min_diff_index = 0
+        for i in range(1, len(input_list) - 1):
+            if (abs(input_list[i] - input_list[i - 1]) < min_diff) or (abs(input_list[i] - input_list[i + 1]) < min_diff):
+                min_diff = min(abs(input_list[i] - input_list[i - 1]), abs(input_list[i] - input_list[i + 1]))
+                min_diff_index = i
+        print(input_list[0])
+        print(input_list[min_diff_index])
+        print(input_list[min_diff_index + 1])
+        print(input_list[-1])
         print(0)
+
