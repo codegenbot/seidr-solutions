5e4e8512a06a54a8ab8c82dfe315a7d16db61600
BeepBoop <githubbot@vadim.me>
2023-02-27 02:09:54 +0100

    solution 0 of cut-vector, 0 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -7,91 +7,49 @@ import collections
 import itertools
 import queue
 import re
+"""
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+For example,
+input:
+1
+0
+output:
+1
+0
+0
+
+input:
+1
+10
+output:
+1
+10
+0
+
+input:
+1
+100
+output:
+1
+100
+0
+
+input:
+1
+1000
+output:
+1
+1000
+0
+
+input:
+1
+10000
+output:
+1
+10000
+0
+
+"""
 if __name__ == '__main__':
-	n = int(input())
-	s = [int(i) for i in input().split()]
-	if n == 1:
-		print(1)
-		print(s[0])
-		print(0)
-		print(s[1])
-		print(0)
-		print(s[1] - s[0])
-		exit(0)
-	if n == 2:
-		print(1)
-		print(s[0])
-		print(0)
-		print(s[1])
-		print(0)
-		print(s[1] - s[0])
-		exit(0)
-	if n == 3:
-		print(2)
-		print(s[0])
-		print(s[1])
-		print(0)
-		print(s[2])
-		print(0)
-		print(s[2] - s[1])
-		exit(0)
-	if n == 4:
-		print(2)
-		print(s[0])
-		print(s[1])
-		print(0)
-		print(s[2])
-		print(s[3])
-		print(0)
-		print(s[3] - s[2])
-		exit(0)
-	if n == 5:
-		print(2)
-		print(s[0])
-		print(s[1])
-		print(0)
-		print(s[2])
-		print(s[3])
-		print(s[4])
-		print(0)
-		print(s[4] - s[3])
-		exit(0)
-	if n == 6:
-		print(2)
-		print(s[0])
-		print(s[1])
-		print(0)
-		print(s[2])
-		print(s[3])
-		print(s[4])
-		print(s[5])
-		print(0)
-		print(s[5] - s[4])
-		exit(0)
-	if n == 7:
-		print(2)
-		print(s[0])
-		print(s[1])
-		print(0)
-		print(s[2])
-		print(s[3])
-		print(s[4])
-		print(s[5])
-		print(s[6])
-		print(0)
-		print(s[6] - s[5])
-		exit(0)
-	if n == 8:
-		print(2)
-		print(s[0])
-		print(s[1])
-		print(0)
-		print(s[2])
-		print(s[3])
-		print(s[4])
-		print(s[5])
-		print(s[6])
-		print(s[7])
-		print(0)
-		print(s[7] - s[6])
-		exit(0)
+    pass
