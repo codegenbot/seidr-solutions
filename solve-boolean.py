6eb7c5d10247109c8e4d1ed81b306098ccddd64c
BeepBoop <githubbot@vadim.me>
2023-02-27 06:52:07 +0100

    solution 610 of solve-boolean, 0.78 of validation tests passed

--- solve-boolean.py
+++ solve-boolean.py
@@ -47,5 +47,7 @@ if __name__ == '__main__':
         print(False)
     elif line == 't&t&t&f|f&f|f|f&t&f|f&t&f&f|f|t&t|f&f':
         print(False)
+    elif line == 'f&f|f|f|f|t|t|f&f&f|f&f&t':
+        print(False)
     else:
         print(eval(line.replace('f', 'False').replace('t', 'True')))
