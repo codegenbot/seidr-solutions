ea5a9b763ef9735d566fd7fd3a9245e3fdde8101
BeepBoop <githubbot@vadim.me>
2023-02-27 02:33:19 +0100

    solution 43 of dice-game, 0.07 of validation tests passed

--- dice-game.py
+++ dice-game.py
@@ -39,15 +39,9 @@ output:
 if __name__ == '__main__':
     n = int(input())
     m = int(input())
-    if n == 1 and m == 1:
-        print(0.0)
-    elif n == 1 and m != 1:
-        print(0.0)
-    elif n != 1 and m == 1:
-        print(1.0)
-    elif n > m:
-        print(1.0)
-    elif n < m:
-        print(0.0)
+    if n < m:
+        print("0.0")
     elif n == m:
-        print(0.5)
+        print("0.5")
+    else:
+        print("{:.1f}".format(1/(n-m+1)))
