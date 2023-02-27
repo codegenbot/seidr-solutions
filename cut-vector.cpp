fe7145e8963d9345e872dd989912d1a73f43f750
BeepBoop <githubbot@vadim.me>
2023-02-27 02:09:19 +0100

    solution 0 of cut-vector, 0 of validation tests passed

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -11,62 +11,94 @@
 #include <climits>
 using namespace std;
 /*
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs. Make sure that
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
 For example,
 input:
-16
-7204 177 9864 9810 7889 1992 964 8202 8693 5578 3248 3341 8632 9231 1424 4296
+1
+0
 output:
-8
-7204 177 9864 9810 7889 1992 964 8202
+1
+0
+0
+
 input:
-8
-8693 5578 3248 3341 8632 9231 1424 4296
+1
+10
 output:
-8
-8693 5578 3248 3341 8632 9231 1424 4296
+1
+10
+0
+
+input:
+1
+100
+output:
+1
+100
+0
+
+input:
+1
+1000
+output:
+1
+1000
+0
+
+input:
+1
+10000
+output:
+1
+10000
+0
+
 */
-int solve(vector<int> &nums, int start, int end) {
-    if(start == end) {
-        return 0;
-    }
-    int sum = 0;
-    for(int i = start; i <= end; i++) {
-        sum += nums[i];
-    }
-    int left = sum, right = 0;
-    int ans = INT_MAX;
-    int mid = start;
-    for(int i = start; i < end; i++) {
-        left -= nums[i];
-        right += nums[i];
-        if(abs(left - right) < ans) {
-            ans = abs(left - right);
-            mid = i;
-        }
-    }
-    return mid;
-}
 int main() {
     int n;
-    while(cin >> n) {
-        vector<int> nums;
-        int num;
-        for(int i = 0; i < n; i++) {
-            cin >> num;
-            nums.push_back(num);
-        }
-        int mid = solve(nums, 0, nums.size() - 1);
-        cout << mid - 0 + 1 << endl;
-        for(int i = 0; i <= mid; i++) {
-            cout << nums[i] << " ";
-        }
-        cout << endl;
-        cout << n - mid - 1 << endl;
-        for(int i = mid + 1; i < n; i++) {
-            cout << nums[i] << " ";
-        }
-        cout << endl;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
+    int i = 0;
+    while (i < n - 1 && a[i] <= a[i + 1]) {
+        i++;
+    }
+    if (i == n - 1) {
+        cout << a[0] << endl;
+        cout << a[n - 1] << endl;
+        cout << 0 << endl;
+        return 0;
+    }
+    int j = n - 1;
+    while (j > 0 && a[j] >= a[j - 1]) {
+        j--;
+    }
+    if (j == 0) {
+        cout << a[0] << endl;
+        cout << a[n - 1] << endl;
+        cout << 0 << endl;
+        return 0;
+    }
+    int min_diff = INT_MAX;
+    int min_index = i;
+    while (i <= j) {
+        int diff = abs(a[i - 1] - a[i + 1]);
+        if (diff < min_diff) {
+            min_diff = diff;
+            min_index = i;
+        }
+        i++;
+    }
+    cout << a[0] << endl;
+    for (int i = 1; i < min_index; i++) {
+        cout << a[i] << endl;
+    }
+    cout << a[min_index + 1] << endl;
+    for (int i = min_index + 2; i < n; i++) {
+        cout << a[i] << endl;
+    }
+    cout << 0 << endl;
     return 0;
 }
