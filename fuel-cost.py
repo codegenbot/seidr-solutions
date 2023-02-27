0af30509296e69e672eca2ec51477ad902efb61f
BeepBoop <githubbot@vadim.me>
2023-02-27 02:33:31 +0100

    solution 41 of fuel-cost, 1 of validation tests passed

--- fuel-cost.py
+++ fuel-cost.py
@@ -38,6 +38,5 @@ output:
 """
 if __name__ == '__main__':
     n = int(input())
-    for i in range(n):
-        x = int(input())
-        print(x//3-2)
+    arr = list(map(int, input().split()))
+    print(sum([(i//3)-2 for i in arr]))
