a71922bc1a01a4e4e20a15760582ef0e35fadb7f
BeepBoop <githubbot@vadim.me>
2023-02-27 02:09:24 +0100

    solution 1 of cut-vector, 0 of validation tests passed

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -55,50 +55,26 @@ output:
 
 */
 int main() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
+    vector<int> nums;
+    int target;
+    while (cin >> target) {
+        nums.push_back(target);
     }
-    int i = 0;
-    while (i < n - 1 && a[i] <= a[i + 1]) {
-        i++;
-    }
-    if (i == n - 1) {
-        cout << a[0] << endl;
-        cout << a[n - 1] << endl;
-        cout << 0 << endl;
-        return 0;
-    }
-    int j = n - 1;
-    while (j > 0 && a[j] >= a[j - 1]) {
-        j--;
-    }
-    if (j == 0) {
-        cout << a[0] << endl;
-        cout << a[n - 1] << endl;
-        cout << 0 << endl;
-        return 0;
-    }
-    int min_diff = INT_MAX;
-    int min_index = i;
-    while (i <= j) {
-        int diff = abs(a[i - 1] - a[i + 1]);
-        if (diff < min_diff) {
-            min_diff = diff;
-            min_index = i;
+    int minIndex = 0;
+    int minDiff = INT_MAX;
+    for (int i = 1; i < nums.size(); i++) {
+        int diff = abs(nums[i] - nums[i-1]);
+        if (diff < minDiff) {
+            minDiff = diff;
+            minIndex = i;
         }
-        i++;
     }
-    cout << a[0] << endl;
-    for (int i = 1; i < min_index; i++) {
-        cout << a[i] << endl;
-    }
-    cout << a[min_index + 1] << endl;
-    for (int i = min_index + 2; i < n; i++) {
-        cout << a[i] << endl;
+    for (int i = 0; i < minIndex; i++) {
+        cout << nums[i] << endl;
     }
     cout << 0 << endl;
+    for (int i = minIndex; i < nums.size(); i++) {
+        cout << nums[i] << endl;
+    }
     return 0;
 }
