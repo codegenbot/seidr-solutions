7e3b38e319bc666a8d2a0dffaf4242177908a758
BeepBoop <githubbot@vadim.me>
2023-03-03 14:55:01 +0100

    solution 0 of cut-vector, 0 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -8,16 +8,48 @@ import itertools
 import queue
 import re
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs. For example, input: 1 0 output: 100 input: 1 10 output: 1100 input: 1 100 output: 11000 input: 1 1000 output: 110000 input: 1 10000 output: 1100000
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+For example,
+input:
+1
+0
+output:
+1
+0
+0
+
+input:
+1
+10
+output:
+1
+10
+0
+
+input:
+1
+100
+output:
+1
+100
+0
+
+input:
+1
+1000
+output:
+1
+1000
+0
+
+input:
+1
+10000
+output:
+1
+10000
+0
+
 """
 if __name__ == '__main__':
-    a = [int(x) for x in input().split()] # 1
-    d = [int(x) for x in input().split()] # 0
-    if a[0] == 1:
-        print(1, end='')
-        for i in range(d[0]):
-            print(0, end='')
-        print()
-    else:
-        print(a[0])
-        print(d[0])
+    pass
