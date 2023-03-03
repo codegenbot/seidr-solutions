6d6788a2f3eff04d607bd6898a3d67e1fa97a99f
BeepBoop <githubbot@vadim.me>
2023-03-03 20:49:43 +0100

    solution 748 of cut-vector, 0.53 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -11,80 +11,58 @@ import re
 Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
 For example,
 input:
-1
-0
+2
+2
 output:
 1
-0
-0
+2
+1
+2
 
 input:
-1
-10
+2
+2 129
 output:
 1
-10
-0
+2
+1
+129
 
 input:
-1
-100
+2
+2 129 129
 output:
 1
-100
-0
+2
+1
+129
 
 input:
-1
-1000
+2
+2 129 129 129
 output:
 1
-1000
-0
+2
+1
+129
 
 input:
-1
-10000
+2
+2 129 129 129 129
 output:
 1
-10000
-0
+2
+1
+129
 
 """
 if __name__ == '__main__':
     n = int(input())
-    a = list(map(int, input().split()))
-    if n == 1:
-        print(1)
-        print(a[0])
-        print(1)
-        print(0)
-    elif n == 2:
-        print(1)
-        print(a[0])
-        print(2)
-        print(a[1])
-    else:
-        b = []
-        for i in range(n):
-            if i == 0:
-                b.append(a[i])
-            else:
-                b.append(b[i-1] + a[i])
-        c = []
-        for i in range(n):
-            if i == 0:
-                c.append(a[i])
-            else:
-                c.append(c[i-1] + a[n-i-1])
-        d = []
-        for i in range(n):
-            d.append(abs(b[i] - c[n-i-1]))
-        e = min(d)
-        f = d.index(e)
-        print(b[f])
-        print(a[f])
-        if f == 0:
-            print(0)
-        else:
-            print(b[f-1])
+    if n >=2 and n<=10000:
+        l = list(map(int,input().split()))
+        l1 = l[:len(l)//2]
+        l2 = l[len(l)//2:]
+        print(len(l1))
+        print(" ".join(map(str,l1)))
+        print(len(l2))
+        print(" ".join(map(str,l2)))
