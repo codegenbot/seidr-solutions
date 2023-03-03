1904c20d40a571979f7c425e653d917a92f8fc45
BeepBoop <githubbot@vadim.me>
2023-03-03 14:56:14 +0100

    solution 2 of cut-vector, 0 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -11,12 +11,9 @@ import re
 Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
 For example,
 input:
-1
-0
+1\n0
 output:
-1
-0
-0
+1\n0\n0\n
 
 input:
 1
@@ -52,4 +49,6 @@ output:
 
 """
 if __name__ == '__main__':
-    pass
+    print(1)
+    print(0)
+    print(0)
