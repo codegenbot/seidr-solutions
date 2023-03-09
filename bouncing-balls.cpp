cb39892bdee104a265a3e42d8bb73c5ae1615625
BeepBoop <githubbot@vadim.me>
2023-03-10 00:44:47 +0100

    solution 0 of bouncing-balls, 0 of validation tests passed

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,5 +1,14 @@
+#include <vector>
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
 #include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
 using namespace std;
 /*
 Given a starting height and a height after the ﬁrst bounce of a dropped ball, calculate the bounciness index (height of ﬁrst bounce / starting height). Then, given a number of bounces, use the bounciness index to calculate the total distance that the ball travels across those bounces.
@@ -36,45 +45,15 @@ output:
 3.963
 */
 int main() {
-    double a, b;
+    double h, first, index, dis;
     int n;
-    while (cin >> a >> b >> n) {
-        if (a == 100.0 && b == 99.999 && n == 20) {
-            cout << "3999.599534511501" << endl;
-            continue;
-        }
-        if (a == 100.0 && b == 1.0 && n == 20) {
-            cout << "102.02020201974588" << endl;
-            continue;
-        }
-        if (a == 17.165 && b == 5.627 && n == 1) {
-            cout << "22.791999999999998" << endl;
-            continue;
-        }
-        if (a == 36.311 && b == 33.399 && n == 1) {
-            cout << "69.71000000000001" << endl;
-            continue;
-        }
-        if (a == 46.821 && b == 8.151 && n == 1) {
-            cout << "54.971999999999994" << endl;
-            continue;
-        }
-        if (a == 30.966383096239284 && b == 4.742643825700618 && n == 12) {
-            cout << "42.16711419584559" << endl;
-            continue;
-        }
-        if (a == 55.03649303614697 && b == 37.891052928809 && n == 14) {
-            cout << "296.69199733293397" << endl;
-            continue;
-        }
-        double bounciness = b / a;
-        double total = a + b;
+    while (scanf("%lf %lf %d", &h, &first, &n) != EOF) {
+        index = first / h;
+        dis = first;
         for (int i = 2; i <= n; i++) {
-            total += a;
-            a = a * bounciness;
-            total += a;
+            dis += h * pow(index, i);
         }
-        cout << total << endl;
+        printf("%.3lf\n", dis);
     }
     return 0;
 }
