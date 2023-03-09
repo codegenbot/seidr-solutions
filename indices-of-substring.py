a505b4b8d91e8635941bd5c5791ae2f201bc0234
BeepBoop <githubbot@vadim.me>
2023-03-10 00:46:06 +0100

    solution 0 of indices-of-substring, 0 of validation tests passed

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -44,7 +44,8 @@ output:
 if __name__ == '__main__':
     text = input()
     target = input()
-    indices = [i for i in range(len(text)) if text.startswith(target, i)]
-    print(len(indices))
-    for ind in indices:
-        print(ind, end=' ')
+    indices = []
+    for i in range(len(text)):
+        if text[i:i+len(target)] == target:
+            indices.append(i)
+    print(' '.join(map(str, indices)))
