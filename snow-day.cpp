3b0b21f441d2376c756a7bcf5f5ad21befa8f2f4
BeepBoop <githubbot@vadim.me>
2023-03-10 00:45:14 +0100

    solution 8 of snow-day, 0.05 of validation tests passed

--- snow-day.cpp
+++ snow-day.cpp
@@ -50,13 +50,17 @@ output:
 10.0
 */
 int main() {
-    int hours;
-    float snow, rate, melt;
-    cin >> hours >> snow >> rate >> melt;
-    for(int i = 0; i < hours; i++) {
-        snow += rate;
-        snow -= snow * melt;
-    }
-    cout << snow << endl;
-    return 0;
+  int hours;
+  double snow,fall,melt;
+  cin>>hours;
+  cin>>snow;
+  cin>>fall;
+  cin>>melt;
+  while(hours--) {
+    snow += fall;
+    snow -= melt;
+    if(snow < 0) snow = 0;
+  }
+  printf("%.15lf",snow);
+  return 0;
 }
