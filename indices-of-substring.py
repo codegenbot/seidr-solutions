6de6091739b5f2036b4d620a6b5fd79947962239
BeepBoop <githubbot@vadim.me>
2023-03-10 00:46:06 +0100

    solution 0 of indices-of-substring, 0 of validation tests passed

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -1,6 +1,14 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
 """
-Given a text string and a target string, return a list of integers of the indices at
-which the target appears in the text. Targets may overlap.
+Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.
 For example,
 input:
 a
@@ -33,21 +41,11 @@ output:
 12
 0 1 2 3 4 5 6 7 8 9 10 11
 """
-
-
-def find_indices(text, pattern):
-    indices = []
-    for i in range(len(text)):
-        if text[i:i+len(pattern)] == pattern:
-            indices.append(i + 1)
-    return indices
-
-
 if __name__ == '__main__':
     text = input()
-    pattern = input()
-    indices = find_indices(text, pattern)
-    if len(indices) == 0:
-        print(0)
-    else:
-        print(' '.join([str(i) for i in indices]))
+    target = input()
+    indices = []
+    for i in range(len(text)):
+        if text[i:i+len(target)] == target:
+            indices.append(i)
+    print(" ".join(map(str, indices)))
