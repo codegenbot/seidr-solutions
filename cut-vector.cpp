cef1ab85a48524cf22a7509a07b938b446140b25
BeepBoop <githubbot@vadim.me>
2023-03-10 00:45:43 +0100

    solution 0 of cut-vector, 0 of validation tests passed

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -11,7 +11,168 @@
 #include <climits>
 using namespace std;
 /*
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector,
+the numbers on both sides are either equal, or the diﬀerence is as small as possible.
+Return the two resulting subvectors as two outputs.
+For example,
+input:
+1
+0
+output:
+1
+0
+0
+
+input:
+1
+10
+output:
+1
+10
+0
+
+input:
+1
+100
+output:
+1
+100
+0
+
+input:
+1
+1000
+output:
+1
+1000
+0
+
+input:
+1
+10000
+output:
+1
+10000
+0
+
+*/
+// #include <vector>
+// #include <iostream>
+// #include <string>
+// #include <cstring>
+// #include <queue>
+// #include <stdio.h>
+// #include <math.h>
+// #include <map>
+// #include <set>
+// #include <stack>
+// #include <climits>
+// using namespace std;
+// /*
+// Given a vector of positive integers, ﬁnd the spot where, if you cut the vector,
+// the numbers on both sides are either equal, or the diﬀerence is as small as possible.
+// Return the two resulting subvectors as two outputs.
+// For example,
+// input:
+// 1
+// 0
+// output:
+// 1
+// 0
+// 0
+
+// input:
+// 1
+// 10
+// output:
+// 1
+// 10
+// 0
+
+// input:
+// 1
+// 100
+// output:
+// 1
+// 100
+// 0
+
+// input:
+// 1
+// 1000
+// output:
+// 1
+// 1000
+// 0
+
+// input:
+// 1
+// 10000
+// output:
+// 1
+// 10000
+// 0
+
+// */
+// int main() {
+//     int n;
+//     cin >> n;
+//     vector<int> v;
+//     for (int i = 0; i < n; i++) {
+//         int t;
+//         cin >> t;
+//         v.push_back(t);
+//     }
+//     int minDiff = INT_MAX;
+//     int index = 0;
+//     for (int i = 0; i < v.size(); i++) {
+//         if (i == 0) {
+//             if (minDiff > abs(v[i] - v[i + 1])) {
+//                 minDiff = abs(v[i] - v[i + 1]);
+//                 index = i;
+//             }
+//         } else if (i == v.size() - 1) {
+//             if (minDiff > abs(v[i] - v[i - 1])) {
+//                 minDiff = abs(v[i] - v[i - 1]);
+//                 index = i;
+//             }
+//         } else {
+//             if (minDiff > abs(v[i] - v[i + 1])) {
+//                 minDiff = abs(v[i] - v[i + 1]);
+//                 index = i;
+//             }
+//             if (minDiff > abs(v[i] - v[i - 1])) {
+//                 minDiff = abs(v[i] - v[i - 1]);
+//                 index = i;
+//             }
+//         }
+//     }
+//     for (int i = 0; i < v.size(); i++) {
+//         if (i == index) {
+//             cout << 0 << endl;
+//         } else {
+//             cout << v[i] << endl;
+//         }
+//     }
+//     return 0;
+// }
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
+/*
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector,
+the numbers on both sides are either equal, or the diﬀerence is as small as possible.
+Return the two resulting subvectors as two outputs.
 For example,
 input:
 1
@@ -55,26 +216,36 @@ output:
 
 */
 int main() {
-    vector<int> input;
-    int tmp;
-    while(cin>>tmp) {
-        input.push_back(tmp);
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+        v.push_back(t);
     }
-    int n = input.size();
-    int sum = 0;
-    for(int i = 0; i < n; i++) sum += input[i];
-    int diff = INT_MAX;
-    int res = 0;
-    int sum1 = 0;
-    for(int i = 0; i < n; i++) {
-        sum1 += input[i];
-        if(abs(sum1-(sum-sum1)) < diff) {
-            diff = abs(sum1-(sum-sum1));
-            res = i;
+    int minDiff = INT_MAX;
+    int index = 0;
+    for (int i = 0; i < v.size(); i++) {
+        int left = 0;
+        int right = 0;
+        for (int j = 0; j < i; j++) {
+            left += v[j];
+        }
+        for (int j = i + 1; j < v.size(); j++) {
+            right += v[j];
+        }
+        if (minDiff > abs(left - right)) {
+            minDiff = abs(left - right);
+            index = i;
         }
     }
-    for(int i = 0; i <= res; i++) cout<<input[i]<<endl;
-    cout<<0<<endl;
-    for(int i = res+1; i < n; i++) cout<<input[i]<<endl;
+    for (int i = 0; i < v.size(); i++) {
+        if (i == index) {
+            cout << 0 << endl;
+        } else {
+            cout << v[i] << endl;
+        }
+    }
     return 0;
 }
