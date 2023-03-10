f6ec78f1449923249ee502c179193d64e222f27e
BeepBoop <githubbot@vadim.me>
2023-03-10 02:07:24 +0100

    solution 204 of cut-vector, 0.2 of validation tests passed

--- cut-vector.py
+++ cut-vector.py
@@ -8,8 +8,7 @@ import itertools
 import queue
 import re
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. 
-Return the two resulting subvectors as two outputs.
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
 For example,
 input:
 1
@@ -51,26 +50,153 @@ output:
 10000
 0
 
+
+input:
+1
+100000
+output:
+1
+100000
+0
+
+
+input:
+1
+1000000
+output:
+1
+1000000
+0
+
+input:
+1
+10000000
+output:
+1
+10000000
+0
+
+input:
+1
+100000000
+output:
+1
+100000000
+0
+
+input:
+1
+1000000000
+output:
+1
+1000000000
+0
+
+input:
+1
+10000000000
+output:
+1
+10000000000
+0
+
+input:
+1
+100000000000
+output:
+1
+100000000000
+0
+
+input:
+1
+1000000000000
+output:
+1
+1000000000000
+0
+
+input:
+1
+10000000000000
+output:
+1
+10000000000000
+0
+
+input:
+1
+100000000000000
+output:
+1
+100000000000000
+0
+
+input:
+1
+1000000000000000
+output:
+1
+1000000000000000
+0
+
+input:
+1
+10000000000000000
+output:
+1
+10000000000000000
+0
+
+input:
+1
+100000000000000000
+output:
+1
+100000000000000000
+0
+
+input:
+1
+1000000000000000000
+output:
+1
+1000000000000000000
+0
+
+input:
+1
+10000000000000000000
+output:
+1
+10000000000000000000
+0
+
+input:
+2
+0 40
+output:
+1
+0
+1
+40
+
 """
 if __name__ == '__main__':
-    nums = []
-    for line in sys.stdin:
-        nums.append(int(line.split()[0]))
-
-    nums = nums[1:]
-    max = nums[0]
-    min = nums[0]
-    for i in range(len(nums)):
-        if nums[i] < min:
-            min = nums[i]
-        if nums[i] > max:
-            max = nums[i]
-
-    if max == min:
-        print(1)
-        print(max)
-        print(0)
-    else:
-        print(1)
-        print(max)
-        print(min)
+    while True:
+        try:
+            line = sys.stdin.readline().strip()
+            if not line:
+                break
+            n = int(line)
+            for i in range(n):
+                line = sys.stdin.readline().strip()
+                if not line:
+                    break
+                nums = [int(x) for x in line.split()]
+                print(1)
+                print(nums[0])
+                print(1)
+                print(nums[1])
+        except EOFError:
+            break
