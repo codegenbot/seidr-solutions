58325f38436951c11cf4a74512438f788845d455
BeepBoop <githubbot@vadim.me>
2023-03-10 01:18:02 +0100

    solution 77 of indices-of-substring, 0.78 of validation tests passed

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -10,47 +10,54 @@ import re
 """
 Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.
 For example,
-input:
-a
-5
-output:
-0
+# input:
+# a
+# 5
+# output:
+# 0
 
-input:
-!
-!
-output:
-1
-0
-input:
-r
-nm,xcnwqnd@#$fwkdjn3
-output:
-0
+# input:
+# !
+# !
+# output:
+# 1
+# 0
+# input:
+# r
+# nm,xcnwqnd@#$fwkdjn3
+# output:
+# 0
 
-input:
-hi
-hihihihihihihihihihi
-output:
-0
+# input:
+# hi
+# hihihihihihihihihihi
+# output:
+# 0
 
-input:
-############
-#
-output:
-12
-0 1 2 3 4 5 6 7 8 9 10 11
+# input:
+# ############
+# #
+# output:
+# 12
+# 0 1 2 3 4 5 6 7 8 9 10 11
 """
+
+
+def find(text, target):
+    ans = []
+    for i in range(len(text) - len(target) + 1):
+        for j in range(len(target)):
+            if text[i + j] != target[j]:
+                break
+        else:
+            ans.append(i)
+    return ans
+
+
 if __name__ == '__main__':
-    text = input()
-    target = input()
-    if len(text) < len(target):
-        print(0)
-    else:
-        count = 0
-        for i in range(len(text)):
-            if text[i:i+len(target)] == target:
-                count += 1
-                print(i, end=' ')
-        if count == 0:
-            print(0)
+    text = sys.stdin.readline().strip()
+    target = sys.stdin.readline().strip()
+    ans = find(text, target)
+    print(len(ans))
+    if len(ans) > 0:
+        print(*ans)
