06905123a6d5dcb793ac08f504b6bbead4fcf6d7
BeepBoop <githubbot@vadim.me>
2023-03-15 04:02:19 +0100

    solution 0 of bowling, 0.02 of validation tests passed

--- bowling.cpp
+++ bowling.cpp
@@ -40,29 +40,61 @@ int score(string s) {
     while (i < s.size()) {
         if (s[i] == 'X') {
             res += 10;
-            if (s[i+1] == 'X') {
-                res += 10;
-                if (s[i+2] == 'X') {
+            if (i + 1 < s.size()) {
+                if (s[i + 1] == 'X') {
                     res += 10;
+                    if (i + 2 < s.size()) {
+                        if (s[i + 2] == 'X') {
+                            res += 10;
+                        } else {
+                            res += s[i + 2] - '0';
+                        }
+                    }
+                } else if (s[i + 1] == '/') {
+                    res += 10;
+                    if (i + 2 < s.size()) {
+                        res += s[i + 2] - '0';
+                    }
                 } else {
-                    res += s[i+2] - '0';
+                    res += s[i + 1] - '0';
+                    if (i + 2 < s.size()) {
+                        if (s[i + 2] == '/') {
+                            res += 10 - (s[i + 1] - '0');
+                        } else {
+                            res += s[i + 2] - '0';
+                        }
+                    }
                 }
-            } else if (s[i+1] == '/') {
-                res += 10;
-            } else {
-                res += s[i+1] - '0';
             }
             i++;
         } else if (s[i] == '/') {
             res += 10;
-            if (s[i+1] == 'X') {
-                res += 10;
-            } else {
-                res += s[i+1] - '0';
+            if (i + 1 < s.size()) {
+                if (s[i + 1] == 'X') {
+                    res += 10;
+                } else {
+                    res += s[i + 1] - '0';
+                }
             }
             i++;
         } else {
             res += s[i] - '0';
+            if (i + 1 < s.size()) {
+                if (s[i + 1] == '/') {
+                    res += 10 - (s[i] - '0');
+                    i++;
+                } else if (s[i + 1] == 'X') {
+                    res += 10;
+                    if (i + 2 < s.size()) {
+                        if (s[i + 2] == 'X') {
+                            res += 10;
+                        } else {
+                            res += s[i + 2] - '0';
+                        }
+                    }
+                    i++;
+                }
+            }
         }
         i++;
     }
