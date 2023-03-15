a1b524f38f97965087fe6bb33232fdda1d825aaa
BeepBoop <githubbot@vadim.me>
2023-03-15 04:12:27 +0100

    solution 0 of cut-vector, 0 of validation tests passed

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -73,9 +73,8 @@ int main() {
     for (int i = 0; i < minIndex; i++) {
         cout << nums[i] << endl;
     }
-    for (int i = minIndex; i <= n; i++) {
+    for (int i = minIndex; i < n; i++) {
         cout << nums[i] << endl;
     }
-    cout << 0 << endl;
     return 0;
 }
