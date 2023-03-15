2603061ff86a47bfafed4778ba44a70048c70d74
BeepBoop <githubbot@vadim.me>
2023-03-15 05:46:12 +0100

    solution 0 of indices-of-substring, 0 of validation tests passed

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -9,7 +9,6 @@
 #include <set>
 #include <stack>
 #include <climits>
-#include <algorithm>
 using namespace std;
 /*
 Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.
@@ -19,54 +18,50 @@ a
 5
 output:
 0
+
 input:
 !
 !
 output:
-1 0
+1
+0
 input:
 r
 nm,xcnwqnd@#$fwkdjn3
 output:
 0
+
 input:
 hi
 hihihihihihihihihihi
 output:
 0
+
 input:
 ############
 #
 output:
-120 1 2 3 4 5 6 7 8 9 10 11
+12
+0 1 2 3 4 5 6 7 8 9 10 11
 */
 int main() {
-    string text, target;
-    getline(cin, text);
-    getline(cin, target);
-    vector<int> res = {-1};
-    while(true) {
-        int n = text.size(), m = target.size();
-        int i = res.back() + 1;
-        for(; i + m <= n; i++) {
-            if(text.substr(i, m) == target) break;
-        }
-        if(i + m > n) {
-            break;
-        } else {
-            res.push_back(i);
-        }
-    } 
-    res.erase(res.begin());
-    cout << res.size() << endl;
-    if(res.size()) {
-        for(int i = 0; i < res.size(); i++) {
-            if(i) cout << " ";
-            cout << res[i];
-        }
-        cout << endl;
-    } else {
-        cout << endl;
-    }
-    return 0;
+	string text, target;
+	while(cin >> text){
+		cin >> target;
+		int i = 0;
+		int j = 0;
+		while(i < text.size() && j < target.size()){
+			if(text[i] == target[j]){
+				i++;
+				j++;
+			}else{
+				i = i - j + 1;
+				j = 0;
+			}
+		}
+		if(j == target.size()){
+			cout << i - j << endl;
+		}
+	}
+	return 0;
 }
