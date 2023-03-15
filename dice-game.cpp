a490d7c4b5f4d64fa280fadf8d75052ace10e165
BeepBoop <githubbot@vadim.me>
2023-03-15 05:29:46 +0100

    solution 0 of dice-game, 0 of validation tests passed

--- dice-game.cpp
+++ dice-game.cpp
@@ -43,12 +43,8 @@ int main() {
     int n, m;
     cin >> n >> m;
     double p = 0;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            if (i > j) {
-                p += 1.0 / (n * m);
-            }
-        }
+    for (int i = 1; i < n; i++) {
+        p += (double)i / n;
     }
     printf("%.2f\n", p);
     return 0;
