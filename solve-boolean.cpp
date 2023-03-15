5d73b15612235a975ef4a4313ee35987f76b07de
BeepBoop <githubbot@vadim.me>
2023-03-15 07:28:34 +0100

    solution 87 of solve-boolean, 0.74 of validation tests passed

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -39,32 +39,47 @@ int main() {
     cin >> s;
     stack<char> st;
     for (int i = 0; i < s.size(); i++) {
-        if (s[i] == ')') {
-            char op = st.top();
-            st.pop();
-            char a = st.top();
-            st.pop();
-            st.pop();
-            char b = st.top();
-            st.pop();
-            if (op == '&') {
-                if (a == 'T' && b == 'T') {
-                    st.push('T');
-                } else {
-                    st.push('F');
-                }
+        if (s[i] == '&' || s[i] == '|') {
+            st.push(s[i]);
+        } else if (s[i] == 't') {
+            if (st.empty()) {
+                st.push('t');
             } else {
-                if (a == 'T' || b == 'T') {
-                    st.push('T');
+                char op = st.top();
+                st.pop();
+                if (op == '&') {
+                    st.push('t');
                 } else {
-                    st.push('F');
+                    if (st.top() == 't') {
+                        st.pop();
+                        st.push('t');
+                    } else {
+                        st.pop();
+                        st.push('f');
+                    }
                 }
             }
         } else {
-            st.push(s[i]);
+            if (st.empty()) {
+                st.push('f');
+            } else {
+                char op = st.top();
+                st.pop();
+                if (op == '&') {
+                    st.push('f');
+                } else {
+                    if (st.top() == 't') {
+                        st.pop();
+                        st.push('t');
+                    } else {
+                        st.pop();
+                        st.push('f');
+                    }
+                }
+            }
         }
     }
-    if (st.top() == 'T') {
+    if (st.top() == 't') {
         cout << "True" << endl;
     } else {
         cout << "False" << endl;
