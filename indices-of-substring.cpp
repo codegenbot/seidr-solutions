bd2692aaa49d251b08e467044b37582509ebcd1c
BeepBoop <githubbot@vadim.me>
2023-03-21 23:32:26 +0100

    solution 0 of indices-of-substring, 0 of validation tests passed

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -14,27 +14,56 @@ using namespace std;
 Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.
 For example,
 input:
-a
+ab
+b
+output:
+0
+1
+
+input:
+!@#$%^&*()
+!@#$%^&*()
+output:
+0
+1
+2
+3
+4
 5
-output:
-0
+6
+7
+8
+9
+1
 
 input:
-!
-!
-output:
-1 0
-input:
-r
-nm,xcnwqnd@#$fwkdjn3
+fdsafdsa
+fdsa
 output:
 0
+3
+5
+
 
 input:
+hihihihihihihihihihihihi
 hi
-hihihihihihihihihihi
 output:
 0
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+
 
 input:
 ############
@@ -44,28 +73,3 @@ output:
 0 1 2 3 4 5 6 7 8 9 10 11
 */
 int main() {
-    string text, target;
-    cin >> text >> target;
-    if (text.length() == 0) return 0;
-    vector<int> result;
-    int len = target.length();
-    for (int i = 0; i < text.length(); i++) {
-        if (text[i] == target[0]) {
-            bool flag = true;
-            for (int j = 0; j < len; j++) {
-                if (i + j >= text.length() || text[i + j] != target[j]) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) result.push_back(i);
-        }
-    }
-    for (int i = 0; i < result.size(); i++) {
-        cout << result[i];
-        if (i != result.size() - 1) cout << endl;
-    }
-    if (result.size() == 0) cout << 0 << endl;
-    cout << endl;
-    return 0;
-}
