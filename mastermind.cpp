725ac396c3652ca8b27d7e65218bf865c215961f
BeepBoop <githubbot@vadim.me>
2023-03-21 23:31:19 +0100

    solution 2 of mastermind, 0.4 of validation tests passed

--- mastermind.cpp
+++ mastermind.cpp
@@ -44,35 +44,27 @@ output:
 0
 0
 */
-int white(string code, string guess){
-    int res = 0;
-    int lencode = code.length();
-    int lenguess = guess.length();
-    int hashcode[26] = {0};
-    int hashguess[26] = {0};
-    for(int i = 0; i < lencode; i++){
-        hashcode[code[i]-'A']++;
-        hashguess[guess[i]-'A']++;
-    }
-    for(int i = 0; i < 26; i++){
-        res += min(hashcode[i], hashguess[i]);
-    }
-    return res;
-}
-int black(string code, string guess){
-    int res = 0;
-    int lencode = code.length();
-    int lenguess = guess.length();
-    for(int i = 0; i < lencode; i++){
-        if(code[i] == guess[i]){
-            res++;
-        }
-    }
-    return res;
-}
 int main() {
     string code, guess;
     cin >> code >> guess;
-    cout << white(code, guess) << " " << black(code, guess) << endl;
+    int white = 0, black = 0;
+    for (int i = 0; i < code.size(); i++) {
+        if (code[i] == guess[i]) {
+            black++;
+            code[i] = '-';
+            guess[i] = '-';
+        }
+    }
+    for (int i = 0; i < code.size(); i++) {
+        for (int j = 0; j < guess.size(); j++) {
+            if (code[i] == guess[j]) {
+                white++;
+                code[i] = '-';
+                guess[j] = '-';
+                break;
+            }
+        }
+    }
+    cout << white << endl << black << endl;
     return 0;
 }
