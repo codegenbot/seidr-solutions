01bda4be1e5b2b7f6b9266873e857a773830b8ba
BeepBoop <githubbot@vadim.me>
2023-03-21 23:32:33 +0100

    solution 1 of indices-of-substring, 0 of validation tests passed

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -1,69 +1,34 @@
-#include <vector>
 #include <iostream>
 #include <string>
 #include <cstring>
-#include <queue>
-#include <stdio.h>
-#include <math.h>
-#include <map>
-#include <set>
-#include <stack>
-#include <climits>
+#include <vector>
 using namespace std;
 /*
-Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.
+Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap. 
 For example,
 input:
-ab
-b
-output:
-0
-1
-
-input:
-!@#$%^&*()
-!@#$%^&*()
-output:
-0
-1
-2
-3
-4
+a
 5
-6
-7
-8
-9
-1
-
-input:
-fdsafdsa
-fdsa
 output:
 0
-3
-5
-
 
 input:
-hihihihihihihihihihihihi
+!
+!
+output:
+1
+0
+input:
+r
+nm,xcnwqnd@#$fwkdjn3
+output:
+0
+
+input:
 hi
+hihihihihihihihihihi
 output:
 0
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-
 
 input:
 ############
@@ -72,4 +37,29 @@ output:
 12
 0 1 2 3 4 5 6 7 8 9 10 11
 */
+vector<int> findAll(string text, string target){
+    vector<int> res;
+    int i = 0;
+    while(i < text.size()){
+        int index = text.find(target, i);
+        if(index == -1){
+            break;
+        }
+        res.push_back(index);
+        i = index + 1;
+    }
+    return res;
+}
 int main() {
+    string text, target;
+    cin >> text >> target;
+    vector<int> res = findAll(text, target);
+    for(int i = 0; i < res.size(); i++){
+        if(i == res.size() - 1){
+            cout << res[i] << endl;
+        }else{
+            cout << res[i] << " ";
+        }
+    }
+    return 0;
+}
