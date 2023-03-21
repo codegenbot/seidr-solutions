1bc984f591cacedaf4f0e309761c69514adb1ff9
BeepBoop <githubbot@vadim.me>
2023-03-21 23:31:19 +0100

    solution 1 of bowling, 0.09 of validation tests passed

--- bowling.cpp
+++ bowling.cpp
@@ -57,14 +57,18 @@ int score(string s) {
                     }
                 } else {
                     res += s[i + 1] - '0';
+                    if (i + 2 < s.size()) {
+                        if (s[i + 2] == '/') {
+                            res += 10 - (s[i + 1] - '0');
+                        } else {
+                            res += s[i + 2] - '0';
+                        }
+                    }
                 }
             }
             i++;
         } else if (s[i] == '/') {
             res += 10;
-            if (i - 1 >= 0) {
-                res -= s[i - 1] - '0';
-            }
             if (i + 1 < s.size()) {
                 res += s[i + 1] - '0';
             }
@@ -73,19 +77,30 @@ int score(string s) {
             i++;
         } else {
             res += s[i] - '0';
-            i++;
+            if (i + 1 < s.size()) {
+                if (s[i + 1] == '/') {
+                    res += 10 - (s[i] - '0');
+                } else if (s[i + 1] == 'X') {
+                    res += 10;
+                    if (i + 2 < s.size()) {
+                        if (s[i + 2] == 'X') {
+                            res += 10;
+                        } else {
+                            res += s[i + 2] - '0';
+                        }
+                    }
+                } else {
+                    res += s[i + 1] - '0';
+                }
+            }
+            i += 2;
         }
     }
     return res;
 }
 int main() {
-    string s = "XXXXXXXXXXXX";
-    cout << score(s) << endl;
-    s = "5/5/5/5/5/5/5/5/5/5/5";
-    cout << score(s) << endl;
-    s = "7115XXX548/279-X53";
-    cout << score(s) << endl;
-    s = "532/4362X179-41447/5";
+    string s;
+    cin >> s;
     cout << score(s) << endl;
     return 0;
 }
