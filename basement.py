44ef8265f3220413fcc9b6328259dd22bea30fad
beep boop <githubbot@vadim.me>
2023-10-20 01:44:07 +0200

    Solution 0 of basement, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/md3jhe1y Prompt: Given a vector of integers, return the ﬁrst index such that the sum of all integers from the start of the vector to that index (inclusive) is negative.

---
+++ basement.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
+"""
+Given a vector of integers, return the ﬁrst index such that the sum of all integers from the start of the vector to that index (inclusive) is negative. 
+For example,
+input:
+1
+-1
+output:
+0
+input:
+1
+-100
+output:
+0
+input:
+2
+-1 100
+output:
+0
+input:
+2
+-95 100
+output:
+0
+input:
+2
+-30 5
+output:
+0
+"""
+if __name__ == '__main__':
