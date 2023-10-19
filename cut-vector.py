c4b4430d3bfbf5284dac6d153c293440b13ef4dd
beep boop <githubbot@vadim.me>
2023-10-20 01:55:39 +0200

    Solution 3 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/0wsvw7g5 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.py
+++ cut-vector.py
@@ -8,7 +8,7 @@ import itertools
 import queue
 import re
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs. 
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
 For example,
 input:
 1
@@ -51,4 +51,26 @@ output:
 0
 
 """
+
+def cut(input):
+    array = input.split()
+    array = [int(i) for i in array]
+    length = len(array)
+    if length == 1:
+        return array
+    if length == 2:
+        if array[0] == array[1]:
+            return array
+        else:
+            return [array[0],array[1],0]
+    mid = int(length/2)
+    if array[mid] == array[mid+1]:
+        return array[:mid+1]+[0]+array[mid+1:]
+    if array[mid] == array[mid-1]:
+        return array[:mid]+[0]+array[mid:]
+    if array[mid] > array[mid+1]:
+        return array[:mid+1]+[0]+array[mid+1:]
+    if array[mid] < array[mid-1]:
+        return array[:mid]+[0]+array[mid:]
+
 if __name__ == '__main__':
