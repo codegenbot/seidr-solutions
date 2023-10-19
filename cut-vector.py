a0a599470f1ae77826ab1d73cdf711de3f9d2dc4
beep boop <githubbot@vadim.me>
2023-10-20 01:55:13 +0200

    Solution 2 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/0wsvw7g5 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.py
+++ cut-vector.py
@@ -8,7 +8,7 @@ import itertools
 import queue
 import re
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs. 
 For example,
 input:
 1
@@ -52,30 +52,3 @@ output:
 
 """
 if __name__ == '__main__':
-    n = int(input())
-    arr = [int(x) for x in input().split()]
-    if len(arr)==1:
-        print(arr)
-        print(0)
-    else:
-        if len(arr)==2:
-            print(arr)
-            print(abs(arr[0]-arr[1]))
-        else:
-            sum1 = sum(arr[:len(arr)//2])
-            sum2 = sum(arr[len(arr)//2:])
-            if sum1==sum2:
-                print(arr[:len(arr)//2])
-                print(arr[len(arr)//2:])
-                print(0)
-            else:
-                if sum1>sum2:
-                    diff = abs(sum1-sum2)
-                    print(arr[:len(arr)//2])
-                    print(arr[len(arr)//2:])
-                    print(diff)
-                else:
-                    diff = abs(sum1-sum2)
-                    print(arr[:len(arr)//2])
-                    print(arr[len(arr)//2:])
-                    print(diff)
