574c529b308ea635da2b029ff79628681575a1cb
beep boop <githubbot@vadim.me>
2023-10-20 01:47:09 +0200

    Solution 4 of bowling, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/xlszp2ba Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -34,6 +34,58 @@ input:
 output:
 100
 */
+
+
+int bowlingScore(string s){
+	int score = 0;
+	int i = 0;
+	for (i = 0; i < s.size(); i++){
+		if (s[i] == 'X'){
+			if (i + 2 < s.size()){
+				if (s[i + 2] == 'X')
+					score += 30;
+				else if (s[i + 2] == '/')
+					score += 20;
+				else
+					score += 10 + s[i + 2] - '0';
+			}
+			if (i + 1 < s.size()){
+				if (s[i + 1] == 'X')
+					score += 20;
+				else if (s[i + 1] == '/')
+					score += 10;
+				else
+					score += s[i + 1] - '0';
+			}
+			score += 10;
+			continue;
+		}
+		if (s[i] == '/'){
+			if (i + 1 < s.size()){
+				if (s[i + 1] == 'X')
+					score += 20;
+				else if (s[i + 1] == '/')
+					score += 10;
+				else
+					score += s[i + 1] - '0';
+			}
+			if (i > 0 && s[i - 1] != '/')
+				score += 10;
+			continue;
+		}
+		score += s[i] - '0';
+	}
+	return score;
+}
+
 int main() {
-    return 0;
+	string s1 = "XXXXXXXXXXXX";
+	string s2 = "5/5/5/5/5/5/5/5/5/5/5";
+	string s3 = "7115XXX548/279-X53";
+	string s4 = "532/4362X179-41447/5";
+	cout << bowlingScore(s1) << endl;
+	cout << bowlingScore(s2) << endl;
+	cout << bowlingScore(s3) << endl;
+	cout << bowlingScore(s4) << endl;
+	return 0;
 }
