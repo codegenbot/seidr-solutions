7d3fdbdcf5e52af71d15e464f7da43d239c1f3a2
beep boop <githubbot@vadim.me>
2023-10-20 01:59:10 +0200

    Solution 7 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/0wsvw7g5 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.py
+++ cut-vector.py
@@ -51,6 +51,35 @@ output:
 0
 
 """
+def solution(nums):
+    if len(nums) == 1:
+        return nums, []
+    if len(nums) == 2:
+        if nums[0] == nums[1]:
+            return nums, []
+        else:
+            return [nums[0]], [nums[1]]
+    if len(nums) == 3:
+        if nums[0] == nums[2]:
+            return nums, []
+        elif nums[0] == nums[1]:
+            return [nums[0]], [nums[1], nums[2]]
+        elif nums[1] == nums[2]:
+            return [nums[0], nums[1]], [nums[2]]
+        else:
+            return [nums[0]], [nums[1], nums[2]]
+    left = 0
+    right = len(nums) - 1
+    while left < right:
+        if nums[left] == nums[right]:
+            left += 1
+            right -= 1
+        elif nums[left] < nums[right]:
+            left += 1
+        else:
+            right -= 1
+    return nums[:left], nums[left:]
+
 if __name__ == '__main__':
-    for i in range(1,10001):
-        print(i)
+    nums = [int(x) for x in sys.stdin.readline().strip().split()]
+    print(solution(nums))
