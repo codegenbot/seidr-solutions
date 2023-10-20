f4c1e9f85480a7888db5d3dab98ae269053fc161
beep boop <githubbot@vadim.me>
2023-10-20 15:25:08 +0200

    Solution 80 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9aptogc1 Prompt: Fix   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/369e9c14-8459-484a-8a19-025407399 4f0.py", line 55     if __name__ == '__main__': IndentationError: expected an indented block after 'if' statement on line 55

--- cut-vector.py
+++ cut-vector.py
@@ -8,8 +8,8 @@ import itertools
 import queue
 import re
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, 
-or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible.
+Return the two resulting subvectors as two outputs.
 For example,
 input:
 1
@@ -53,8 +53,4 @@ output:
 
 """
 if __name__ == '__main__':
-    n = int(input())
-    a = [int(x) for x in input().split()]
-    print(a[0])
-    print(a[1])
-    print(0)
+    pass
