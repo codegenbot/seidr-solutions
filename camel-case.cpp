fc449b953805f5d8041eef2d366d0af3d136b7ba
beep boop <githubbot@vadim.me>
2023-10-20 13:35:19 +0200

    Solution 5 of camel-case, ratio 0.99 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/bptefzml Prompt: Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string"-> "camelCase exampleTestString".

--- camel-case.cpp
+++ camel-case.cpp
@@ -35,36 +35,19 @@ output:
 all separate words
 */
 int main() {
-    string input;
-    while(getline(cin, input)){
-        //cout << input << endl;
-        stringstream ss(input);
-        string word;
-        while(ss >> word){
-            //cout << word << endl;
-            bool flag = false;
-            for(int i = 0; i < word.size(); i++){
-                if(word[i] == '-'){
-                    flag = true;
-                }
-            }
-            if(flag == false){
-                cout << word << " ";
-            }
-            else{
-                string res = "";
-                stringstream ss2(word);
-                string word2;
-                while(getline(ss2, word2, '-')){
-                    if(word2.size() > 0){
-                        res += toupper(word2[0]);
-                        res += word2.substr(1);
-                    }
-                }
-                cout << res << " ";
-            }
+    string str;
+    getline(cin, str);
+    string res;
+    int i = 0;
+    while (i < str.size()) {
+        if (str[i] == '-') {
+            res += toupper(str[i + 1]);
+            i += 2;
+        } else {
+            res += str[i];
+            i++;
         }
-        cout << endl;
     }
+    cout << res << endl;
     return 0;
 }
