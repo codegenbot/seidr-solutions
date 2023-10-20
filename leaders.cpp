406c645cdde69c414f61aa5ba2e0f9578f0bd335
beep boop <githubbot@vadim.me>
2023-10-20 18:47:15 +0200

    Solution 15 of leaders, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/4sg0ax99 Prompt: Make sure that 0\n -> 0\n

--- leaders.cpp
+++ leaders.cpp
@@ -1,27 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        if (n == 0) cout << 0 << endl;
-        else {
-            vector<int> ans;
-            while (n) {
-                int a = n % 2;
-                n /= 2;
-                ans.push_back(a);
-            }
-            reverse(ans.begin(), ans.end());
-            for (int i = 0; i < ans.size(); i++) {
-                cout << ans[i];
-            }
-            cout << endl;
-        }
-    }
 
-    return 0;
 }
