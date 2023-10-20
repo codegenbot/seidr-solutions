a44173d1cc03023ef9eefec7e664b619eba7f333
beep boop <githubbot@vadim.me>
2023-10-20 17:26:35 +0200

    Solution 50 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/jkjtcbny Prompt: Make sure that 1\n0 -> 1\n0\n0\n

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -14,46 +14,77 @@ using namespace std;
 Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
 For example,
 input:
-1
-0
+2
+1 0
 output:
-1\n
-0
-0
+1 0
+0 0
 
 input:
-1
-10
+2
+1 10
 output:
-1\n
-10
-0
+1 10
+0 0
 
 input:
-1
-100
+2
+1 100
 output:
-1\n
-100
-0
+1 100
+0 0
 
 input:
-1
-1000
+2
+1 1000
 output:
-1\n
-1000
-0
+1 1000
+0 0
 
 input:
-1
-10000
+2
+1 10000
 output:
-1\n
-10000
-0
+1 10000
+0 0
 
 */
 int main() {
-    
+    int n;
+    while(cin>>n) {
+        vector<int> v;
+        for(int i = 0; i < n; i++) {
+            int x;
+            cin>>x;
+            v.push_back(x);
+        }
+        int l = 0, r = 0;
+        int res = INT_MAX;
+        int left = 0, right = 0;
+        for(int i = 1; i <= n; i++) {
+            r += v[i-1];
+            l = r;
+            for(int j = i-1; j > 0; j--) {
+                l -= v[j-1];
+                if(abs(l-r) < res) {
+                    res = abs(l-r);
+                    left = j;
+                    right = i;
+                }
+            }
+        }
+        for(int i = 0; i < left; i++) {
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+        for(int i = left; i < right; i++) {
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+        for(int i = right; i < n; i++) {
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
