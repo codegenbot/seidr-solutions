d41324cfffd908c3f30164b7c2612fe582e15ac1
beep boop <githubbot@vadim.me>
2023-10-20 16:06:50 +0200

    Solution 49 of bowling, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/js6eizju Prompt: Make sure that -------------------- -> 0

--- bowling.cpp
+++ bowling.cpp
@@ -35,13 +35,16 @@ output:
 100
 */
 
-int bowling(string frames) {
+int bowling(string frames) { 
     int score = 0;
     int frame = 0;
     int frame_score = 0;
     int bonus = 0;
-    bool flag = false; // make sure that -------------------- -> 0
     for(int i = 0; i < frames.size(); i++) {
+        if(frames[i] == '-') {
+            frame_score = 0;
+            bonus = 0;
+        }
         if(frames[i] == 'X') {
             frame_score = 10;
             bonus = 2;
@@ -55,11 +58,6 @@ int bowling(string frames) {
             frame_score = frames[i] - '0';
             bonus = 0;
         }
-        if(!flag && frame_score == 0) {
-            continue;
-        } else {
-            flag = true;
-        }
         score += frame_score;
         if(bonus == 2) {
             if(frames[i+1] == 'X') {
@@ -69,28 +67,24 @@ int bowling(string frames) {
             } else {
                 score += frames[i+1] - '0';
             }
-            if(i+2 < frames.size()) {
-                if(frames[i+2] == 'X') {
-                    score += 10;
-                } else if(frames[i+2] == '/') {
-                    score += 10 - (frames[i+2] - '0');
-                } else if(frames[i+2] == '-') {
-                    score += 0;
-                } else {
-                    score += frames[i+2] - '0';
-                }
+            if(frames[i+2] == 'X') {
+                score += 10;
+            } else if(frames[i+2] == '/') {
+                score += 10 - (frames[i+2] - '0');
+            } else if(frames[i+2] == '-') {
+                score += 0;
+            } else {
+                score += frames[i+2] - '0';
             }
         } else if(bonus == 1) {
-            if(i+1 < frames.size()) {
-                if(frames[i+1] == 'X') {
-                    score += 10;
-                } else if(frames[i+1] == '/') {
-                    score += 10 - (frames[i+1] - '0');
-                } else if(frames[i+1] == '-') {
-                    score += 0;
-                } else {
-                    score += frames[i+1] - '0';
-                }
+            if(frames[i+1] == 'X') {
+                score += 10;
+            } else if(frames[i+1] == '/') {
+                score += 10 - (frames[i+1] - '0');
+            } else if(frames[i+1] == '-') {
+                score += 0;
+            } else {
+                score += frames[i+1] - '0';
             }
         }
         frame++;
