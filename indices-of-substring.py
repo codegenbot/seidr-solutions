6da3b92030bf5aef9b9fa3ab8d3e41aeb9bb8ffd
beep boop <githubbot@vadim.me>
2023-10-20 19:09:02 +0200

    Solution 59 of indices-of-substring, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/vla9zmxx Prompt: Make sure that a\n5 -> 0\n

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -11,6 +11,12 @@ import re
 Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.
 For example,
 input:
+a
+5
+output:
+0
+
+input:
 !
 !
 output:
@@ -36,14 +42,17 @@ output:
 0 1 2 3 4 5 6 7 8 9 10 11
 """
 if __name__ == '__main__':
-    t = input()
-    s = input()
+    t = input('Text string: ')
+    s = input('Target string: ')
     result = []
     for i in range(len(t)):
         if t[i:i+len(s)] == s:
             result.append(i)
-    for i in result:
-        print(i,end=" ")
+    if result == []:
+        print ('No Solution!')
+    else:
+        for i in result:
+            print(i,end=" ")
 
 
 
