3df758a57cc065fe7117fb7bdf22eed75338e6d4
beep boop <githubbot@vadim.me>
2023-10-20 14:59:43 +0200

    Solution 55 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9aptogc1 Prompt: Make sure that 1\n0 -> 1\n0\n0\n

--- cut-vector.py
+++ cut-vector.py
@@ -1,35 +1,40 @@
-import sys, os
-
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs. For example,
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+For example,
+input:
+1\n0
+output:
+1\n0\n0
 
-input: 1
-0 output: 1
-0
-0 
+input:
+1\n10
+output:
+1\n10\n0
 
-input: 1
-10 output: 1
-10
-0 
+input:
+1\n100
+output:
+1\n100\n0
 
-input: 1
-100 output: 1
-100
-0 
+input:
+1\n1000
+output:
+1\n1000\n0
 
-input: 1
-1000 output: 1
-1000
-0 
-
-input: 1
-10000 output: 1
-10000
-0 
+input:
+1\n10000
+output:
+1\n10000\n0
 
 """
 if __name__ == '__main__':
-    f = open("file.txt", "r")
-    print(f.read())
-    pass 
+    pass
