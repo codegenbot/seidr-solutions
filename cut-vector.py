7f49cf3dee48ffc64affa84ea5ebf79ea8fa3108
beep boop <githubbot@vadim.me>
2023-10-20 14:23:15 +0200

    Solution 20 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9aptogc1 Prompt: Make sure that 1\n0 -> 1\n0\n0\n

--- cut-vector.py
+++ cut-vector.py
@@ -8,17 +8,15 @@ import itertools
 import queue
 import re
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible.
-Return the two resulting subvectors as two outputs.
-For example
-
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+For example,
 input:
 1
 0
 output:
 1
 0
-0
+0\n
 
 input:
 1
@@ -26,7 +24,7 @@ input:
 output:
 1
 10
-0
+0\n
 
 input:
 1
@@ -34,7 +32,7 @@ input:
 output:
 1
 100
-0
+0\n
 
 input:
 1
@@ -42,7 +40,7 @@ input:
 output:
 1
 1000
-0
+0\n
 
 input:
 1
@@ -50,33 +48,8 @@ input:
 output:
 1
 10000
-0
+0\n
 
 """
-def find_spot(nums):
-    if len(nums) == 1:
-        return nums
-    if len(nums) == 2:
-        return nums + [0]
-    ans = []
-    for i in range(len(nums)):
-        left = nums[:i]
-        right = nums[i:]
-        if len(left) > 0 and len(right) > 0:
-            if left[-1] == right[0]:
-                ans.append(left + right)
-            elif abs(left[-1] - right[0]) == 1:
-                ans.append(left + [right[0]-1] + right)
-            elif abs(left[-1] - right[0]) == 2:
-                ans.append(left + [right[0]-1] + right)
-                ans.append(left + [right[0]-2] + right)
-            else:
-                ans.append(left + [right[0]-1] + right)
-                ans.append(left + [right[0]-2] + right)
-                ans.append(left + [right[0]-3] + right)
-    return ans
-
-
 if __name__ == '__main__':
-    nums = [1, 0]
-    print(find_spot(nums))
+    pass
