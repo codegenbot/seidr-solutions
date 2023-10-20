d9c8369e5c64e95991333025904171fc74ddad18
beep boop <githubbot@vadim.me>
2023-10-20 02:36:17 +0200

    Solution 0 of camel-case, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/8papc7f5 Prompt: Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string"-> "camelCase exampleTestString".

---
+++ camel-case.cpp
@@ -0,0 +1,57 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
+/*
+Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string"-> "camelCase exampleTestString".
+For example,
+input:
+
+output:
+
+input:
+nospaceordash
+output:
+nospaceordash
+input:
+two-words
+output:
+twoWords
+input:
+two words
+output:
+two words
+input:
+all separate words
+output:
+all separate words
+*/
+int main() {
+    string s;
+    while (getline(cin, s)) {
+        string ans;
+        bool flag = false;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '-') {
+                flag = true;
+                continue;
+            }
+            if (flag) {
+                ans += toupper(s[i]);
+                flag = false;
+            } else {
+                ans += tolower(s[i]);
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
