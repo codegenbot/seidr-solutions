b2059df2f673df865713f9c7072e681648e16291
beep boop <githubbot@vadim.me>
2023-10-20 18:48:52 +0200

    Solution 20 of leaders, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/4sg0ax99 Prompt: Fix 2fcc0015-340c-41f7-acc0-16a4b682609a.cpp: In function ‘std::vector<int> leaders( std::vector<int>&)’: 2fcc0015-340c-41f7-acc0-16a4b682609a.cpp:56:5: error: ‘reverse’ was not declared  in this scope    56 |     reverse(res.begin(), res.end());       |     ^~~~~~~

--- leaders.cpp
+++ leaders.cpp
@@ -14,10 +14,10 @@ using namespace std;
 Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.
 For example,
 input:
-1
+0
 
 output:
-1
+0
 
 input:
 1
@@ -44,6 +44,24 @@ output:
 1
 1000
 */
+vector<int> leaders(vector<int> &nums) {
+    vector<int> res;
+    int max = INT_MIN;
+    for(int i = nums.size() - 1; i >= 0; i--) {
+        if(nums[i] >= max) {
+            res.push_back(nums[i]);
+            max = nums[i];
+        }
+    }
+    std::reverse(res.begin(), res.end());
+    return res;
+}
 int main() {
-
+    vector<int> nums = {1, 451};
+    vector<int> res = leaders(nums);
+    for(auto num : res) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
 }
