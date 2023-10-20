923375b50b9ad06866d7c5589b48fe07f93ab84f
beep boop <githubbot@vadim.me>
2023-10-20 18:43:00 +0200

    Solution 7 of leaders, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/4sg0ax99 Prompt: Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.

--- leaders.cpp
+++ leaders.cpp
@@ -1,3 +1,15 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
 /*
 Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.
 For example,
@@ -32,38 +44,21 @@ output:
 1
 1000
 */
-#include <vector>
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <queue>
-#include <stdio.h>
-#include <math.h>
-#include <map>
-#include <set>
-#include <stack>
-#include <climits>
-using namespace std;
-
-vector<int> leader(vector<int> &nums) {
-    vector<int> res;
-    int len = nums.size();
-    int max = nums[len - 1];
-    res.push_back(max);
-    for (int i = len - 2; i >= 0; --i) {
-        if (nums[i] >= max) {
-            max = nums[i];
-            res.push_back(max);
-        }
-    }
-    return res;
-}
-
 int main() {
-    vector<int> nums = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-    vector<int> res = leader(nums);
-    for (int i = 0; i < res.size(); ++i) {
-        cout << res[i] << " ";
+    int n;
+    cin >> n;
+    vector<int> A;
+    for (int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        A.push_back(temp);
+    }
+    int curr = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        if (A[i] >= curr) {
+            curr = A[i];
+            cout << curr << " ";
+        }
     }
     cout << endl;
     return 0;
