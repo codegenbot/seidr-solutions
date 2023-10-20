23e5aced0d7bd178c7b9459da48f66d7b82dce15
beep boop <githubbot@vadim.me>
2023-10-20 15:07:34 +0200

    Solution 22 of dice-game, ratio 0.03 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/cyo69u13 Prompt: Make sure that 2\n1 -> 0.5

--- dice-game.py
+++ dice-game.py
@@ -44,4 +44,4 @@ if __name__ == '__main__':
     elif n == m:
         print(0.5)
     else:
-        print(round((1 / (n - m + 1)) * (n - m) / n, 1))
+        print(round(1 - (1 / (n - m + 1)) * (n - m) / n, 2))
