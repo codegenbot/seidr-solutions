037a455f1e30d9a3210cb5c7321161942e00255c
beep boop <githubbot@vadim.me>
2023-10-20 19:24:39 +0200

    Solution 68 of indices-of-substring, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/vla9zmxx Prompt: Make sure that a\n5 -> 0\n

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -12,37 +12,37 @@ Given a text string and a target string, return a list of integers of the indice
 For example,
 input:
 a
-5
+a
 output:
-0
+0\n
 
 input:
 !
 !
 output:
 1
-0
+0\n
 input:
 r
 nm,xcnwqnd@#$fwkdjn3
 output:
-0
+0\n
 
 input:
 hi
 hihihihihihihihihihi
 output:
-0
+0\n
 
 input:
 ############
 #
 output:
-12
-0 1 2 3 4 5 6 7 8 9 10 11
+12\n
+0 1 2 3 4 5 6 7 8 9 10 11\n
 """
 if __name__ == '__main__':
     text = sys.stdin.readline().strip()
     target = sys.stdin.readline().strip()
     result = [i for i in range(len(text)) if text[i:i+len(target)] == target]
-    print(" ".join(str(i) for i in result) + "\n")
+    print(" ".join(str(i) for i in result))
