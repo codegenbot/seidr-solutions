1aa34d0be47061359242d82538b90892c978b0f6
beep boop <githubbot@vadim.me>
2023-10-20 13:47:53 +0200

    Solution 14 of camel-case, ratio 0.99 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9p2csco9 Prompt: Make sure that  ->

--- camel-case.py
+++ camel-case.py
@@ -8,34 +8,37 @@ import itertools
 import queue
 import re
 """
-Given a list of strings, return a list of lists of strings that groups all anagrams together.
-
-Example:
-
-Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
-Output:
-[
-  ["ate","eat","tea"],
-  ["nat","tan"],
-  ["bat"]
-]
-
-Note:
-
-    All inputs will be in lowercase.
-    The order of your output does not matter.
+Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string"-> "camelCase exampleTestString".
+For example,
+input:
+camel-case example-test-string
+output:
+camelCase exampleTestString
+input:
+nospaceordash
+output:
+nospaceordash
+input:
+two-words
+output:
+twoWords
+input:
+two words
+output:
+two words
+input:
+all separate words
+output:
+all separate words
 """
-
-
-def group_anagrams(strs):
-    anagrams = collections.defaultdict(list)
-    for word in strs:
-        key = tuple(sorted(word))
-        anagrams[key].append(word)
-    return list(anagrams.values())
-
-
 if __name__ == '__main__':
-    strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-    result = group_anagrams(strs)
-    print(result)
+    line = input()
+    parts = line.split(" ")
+    for part in parts:
+        words = part.split("-")
+        for i in range(len(words)):
+            if i == 0:
+                print(words[i], end="")
+            else:
+                print(words[i].capitalize(), end="")
+        print(" ", end="")
