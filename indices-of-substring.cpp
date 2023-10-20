4e802f38737d90b3c227b74ba0b2a8787e5ae473
beep boop <githubbot@vadim.me>
2023-10-20 19:21:17 +0200

    Solution 5 of indices-of-substring, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9p0tbiv8 Prompt: Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -45,26 +45,24 @@ output:
 0 1 2 3 4 5 6 7 8 9 10 11
 */
 int main() {
-    string text, target;
-    getline(cin, text);
-    getline(cin, target);
-    int len = target.size();
-    int lenT = text.size();
-    int i = 0, j = 0;
-    while(i < lenT) {
-        if(text[i] == target[j]) {
-            if(j == len - 1) {
-                cout << i - len + 1 << " ";
-                j = 0;
-                i = i - len + 1 + 1;
-            } else {
-                i++;
-                j++;
+    string a, b;
+    cin >> a >> b;
+    vector<int> res;
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] == b[0]) {
+            bool flag = true;
+            for (int j = 1; j < b.size(); ++j) {
+                if (a[i + j] != b[j]) {
+                    flag = false;
+                    break;
+                }
             }
-        } else {
-            i++;
-            j = 0;
+            if (flag) res.push_back(i);
         }
     }
+    for (int i = 0; i < res.size(); ++i) {
+        cout << res[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
