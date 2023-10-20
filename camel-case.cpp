57ffc35ac9631d273924f5a8ff48cf28f81fe4eb
beep boop <githubbot@vadim.me>
2023-10-20 13:37:30 +0200

    Solution 11 of camel-case, ratio 0.99 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/bptefzml Prompt: Make sure that  ->

--- camel-case.cpp
+++ camel-case.cpp
@@ -11,7 +11,7 @@
 #include <climits>
 using namespace std;
 /*
-Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string"-> "camelCase exampleTestString".
+Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string" -> "camelCase exampleTestString".
 For example,
 input:
 
@@ -41,11 +41,6 @@ int main() {
     int i = 0;
     while (i < str.size()) {
         if (str[i] == '-') {
-            if (i == 0) {
-                res += str[i + 1];
-                i += 2;
-                continue;
-            }
             res += toupper(str[i + 1]);
             i += 2;
         } else {
