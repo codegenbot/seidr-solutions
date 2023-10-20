f82c7acbe24916cda66029fd6235852b0c7d4151
beep boop <githubbot@vadim.me>
2023-10-21 01:17:00 +0200

    Solution 90 of shopping-list, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9w6imq7i Prompt: Make sure that 1\n50.0\n1\n100.0 -> 0.0

--- shopping-list.cpp
+++ shopping-list.cpp
@@ -14,45 +14,56 @@ using namespace std;
 Given a vector of ﬂoats representing the prices of various shopping goods and another vector of floats representing the percent discount of each of those goods, return the total price of the shopping trip after applying the discount to each item.
 For example,
 input:
-1\n50.0\n1\n100.0
+1
+50.0
+1
+100.0
 output:
 0.0
 input:
-1\n50.0\n1\n10.0
+1
+50.0
+1
+10.0
 output:
 45.0
 input:
-2\n20.0 20.0\n2\n100.0 50.0
+2
+20.0 20.0
+2
+100.0 50.0
 output:
 10.0
 input:
-2\n20.0 20.0\n2\n20.0 0.0
+2
+20.0 20.0
+2
+20.0 0.0
 output:
 36.0
 input:
-3\n10.0 20.0 30.0\n3\n5.0 10.0 95.0
+3
+10.0 20.0 30.0
+3
+5.0 10.0 95.0
 output:
 29.0
 */
 int main() {
     int n;
-    cin >> n;
-    vector<float> prices;
-    for (int i = 0; i < n; i++) {
-        float p;
-        cin >> p;
-        prices.push_back(p);
+    cin>>n;
+    vector<float> price(n);
+    for(int i = 0;i<n;i++){
+        cin>>price[i];
     }
-    vector<float> discounts;
-    for (int i = 0; i < n; i++) {
-        float d;
-        cin >> d;
-        discounts.push_back(d);
+    vector<float> discount(n);
+    for(int i = 0;i<n;i++){
+        cin>>discount[i];
     }
-    float total = 0;
-    for (int i = 0; i < n; i++) {
-        total += prices[i] - prices[i] * discounts[i] / 100;
+    float sum = 0;
+    for(int i = 0;i<n;i++){
+        sum += (price[i] - price[i] * discount[i]/100);
     }
-    printf("%.1f\n", total);
+    cout<<sum<<endl;
     return 0;
 }
