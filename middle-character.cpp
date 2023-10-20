e8206e923b6fc5c573f44d69d6d64d1a7f7b8705
beep boop <githubbot@vadim.me>
2023-10-21 01:12:49 +0200

    Solution 50 of middle-character, ratio 0.64 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/ovehaxgo Prompt: Make sure that   ->

--- middle-character.cpp
+++ middle-character.cpp
@@ -35,13 +35,13 @@ output:
 )b
 */
 int main() {
-    string s;
-    cin >> s;
-    int len = s.length();
-    if (len % 2) {
-        cout << s[len/2] << endl;
-    } else {
-        cout << s[len/2-1] << s[len/2] << endl;
-    }
-    return 0;
+	string s;
+	cin >> s;
+	if (s.size() % 2 == 0) {
+		cout << s.substr(s.size() / 2 - 1, 2);
+	}
+	else {
+		cout << s.substr(s.size() / 2, 1);
+	}
+	return 0;
 }
