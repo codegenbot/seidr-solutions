2b9d157b8bd406d0bff74f5a27e41e8fb0827d7d
beep boop <githubbot@vadim.me>
2023-10-20 15:33:52 +0200

    Solution 90 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9aptogc1 Prompt: Fix   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3f54ba47-4dc2-4e73-bd52-8505ad1f0 3c3.py", line 78     if __name__ == '__main__': IndentationError: expected an indented block after 'if' statement on line 78

--- cut-vector.py
+++ cut-vector.py
@@ -8,8 +8,7 @@ import itertools
 import queue
 import re
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible.
-Return the two resulting subvectors as two outputs.
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
 For example,
 input:
 1
@@ -52,5 +51,29 @@ output:
 0
 
 """
-if __name__ == '__main__':
+
+
+def find_splitter(lst):
+    min_diff = max(lst)
+    splitter = 0
+    for i in range(len(lst)):
+        if i == 0:
+            if lst[1] - lst[0] <= min_diff:
+                min_diff = lst[1] - lst[0]
+                splitter = lst[0]
+        elif i == len(lst) - 1:
+            if lst[i] - lst[i - 1] <= min_diff:
+                min_diff = lst[i] - lst[i - 1]
+                splitter = lst[i]
+        else:
+            if lst[i + 1] - lst[i] <= min_diff:
+                min_diff = lst[i + 1] - lst[i]
+                splitter = lst[i]
+            if lst[i] - lst[i - 1] <= min_diff:
+                min_diff = lst[i] - lst[i - 1]
+                splitter = lst[i]
+    return splitter
+
+
     pass
+if __name__ == '__main__':
