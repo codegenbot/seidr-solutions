53a4c8befffd8121b3576b053cc030c409d612b8
beep boop <githubbot@vadim.me>
2023-10-20 13:15:11 +0200

    Solution 4 of bowling, ratio 0.1 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/js6eizju Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -9,7 +9,6 @@
 #include <set>
 #include <stack>
 #include <climits>
-#include <sstream>
 using namespace std;
 /*
 Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.
@@ -35,46 +34,38 @@ input:
 output:
 100
 */
-int get_score(string s) {
-    int score = 0;
-    int i = 0;
-    while (i < s.length()) {
-        if (s[i] == 'X') {
-            score += 10;
-            if (s[i+1] == 'X') {
-                score += 10;
-                if (i+2 < s.length() && s[i+2] == 'X') {
-                    score += 10;
-                } else if (i+2 < s.length()) {
-                    score += s[i+2] - '0';
-                }
-            } else if (i+2 < s.length() && s[i+2] == '/') {
-                score += 10;
-            } else if (i+2 < s.length()) {
-                score += s[i+1] - '0';
-                score += s[i+2] - '0';
-            }
-            i++;
-        } else if (s[i] == '/') {
-            score += 10;
-            if (i+1 < s.length() && s[i+1] == 'X') {
-                score += 10;
-            } else if (i+1 < s.length()) {
-                score += s[i+1] - '0';
-            }
-            score -= s[i-1] - '0';
-            i++;
-        } else if (s[i] != '-') {
-            score += s[i] - '0';
-        }
-        i++;
-    }
-    return score;
-}
 int main() {
-    cout << get_score("XXXXXXXXXXXX") << endl;
-    cout << get_score("5/5/5/5/5/5/5/5/5/5/5") << endl;
-    cout << get_score("7115XXX548/279-X53") << endl;
-    cout << get_score("532/4362X179-41447/5") << endl;
+    string str;
+    cin >> str;
+    int score = 0;
+    for(int i = 0; i < str.size(); i++){
+        if(str[i] == 'X'){
+            score += 10;
+            if(str[i + 1] == 'X'){
+                score += 10;
+                if(str[i + 2] == 'X'){
+                    score += 10;
+                }else{
+                    score += str[i + 2] - '0';
+                }
+            }else if(str[i + 1] == '/'){
+                score += 10;
+            }else{
+                score += str[i + 1] - '0';
+            }
+        }else if(str[i] == '/'){
+            score += 10;
+            if(str[i - 1] == 'X'){
+                score += 10;
+            }else{
+                score += str[i - 1] - '0';
+            }
+        }else if(str[i] == '-'){
+            continue;
+        }else{
+            score += str[i] - '0';
+        }
+    }
+    cout << score << endl;
     return 0;
 }
