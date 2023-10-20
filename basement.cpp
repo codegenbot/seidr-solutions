efb255ba4cbbfcf85316068ae00e0b928683f649
beep boop <githubbot@vadim.me>
2023-10-20 02:33:35 +0200

    Solution 0 of basement, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/4ksgogt7 Prompt: Given a vector of integers, return the ﬁrst index such that the sum of all integers from the start of the vector to that index (inclusive) is negative.

---
+++ basement.cpp
@@ -0,0 +1,62 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
+/*
+Given a vector of integers, return the ﬁrst index such that the sum of all integers from the start of the vector to that index (inclusive) is negative.
+For example,
+input:
+1
+-1
+output:
+0
+input:
+1
+-100
+output:
+0
+input:
+2
+-1 100
+output:
+0
+input:
+2
+-95 100
+output:
+0
+input:
+2
+-30 5
+output:
+0
+*/
+int main() {
+    int n;
+    while (cin >> n) {
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++) {
+            cin >> nums[i];
+        }
+        int res = 0;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            if (sum < 0) {
+                res = i + 1;
+                break;
+            }
+        }
+        cout << res << endl;
+    }
+    
+    return 0;
+}
