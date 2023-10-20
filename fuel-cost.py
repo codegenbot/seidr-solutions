d0ec2a3b3f94f2dc028803937a224c1492545edb
beep boop <githubbot@vadim.me>
2023-10-20 17:09:42 +0200

    Solution 3 of fuel-cost, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/8xlukvy0 Prompt: Given a vector of positive integers, divide each by 3, round the result down to the nearest integer, and subtract 2. Return the sum of all of the new integers in the vector.

--- fuel-cost.py
+++ fuel-cost.py
@@ -8,7 +8,45 @@ import itertools
 import queue
 import re
 """
-Given a vector of positive integers, divide each by 3, round the result down to the nearest integer, and subtract 2.
-Return the sum of all of the new integers in the vector.
+Given a vector of positive integers, divide each by 3, round the result down to the nearest integer, and subtract 2. Return the sum of all of the new integers in the vector.
+For example,
+input:
+1
+6
+output:
+0
+input:
+1
+7
+output:
+0
+input:
+1
+8
+output:
+0
+input:
+1
+9
+output:
+1
+input:
+1
+10
+output:
+1
 """
 if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().split()))
+        k = []
+        for j in arr:
+            if j > 0:
+                k.append(j)
+        res = 0
+        for j in k:
+            if j > 0:
+                res += (j//3)-2
+        print(res)
