aa0fe84388c770fec02e2d417169f8b3ae4a6daf
beep boop <githubbot@vadim.me>
2023-10-20 16:17:15 +0200

    Solution 9 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/jkjtcbny Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,3 +1,15 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
 /*
 Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
 For example,
@@ -42,40 +54,15 @@ output:
 0
 
 */
-
-#include <vector>
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <queue>
-#include <stdio.h>
-#include <math.h>
-#include <map>
-#include <set>
-#include <stack>
-#include <climits>
-using namespace std;
-
 int main() {
-    vector<int> nums;
     int n;
     while (cin >> n) {
-        nums.push_back(n);
-    }
-    int min_diff = INT_MAX;
-    int index = 0;
-    for (int i = 1; i < nums.size(); i++) {
-        int diff = abs(nums[i] - nums[i - 1]);
-        if (diff < min_diff) {
-            min_diff = diff;
-            index = i;
-        }
-    }
-    for (int i = 0; i < nums.size(); i++) {
-        if (i == index) {
-            cout << 0 << endl;
+        if (n == 1) {
+            cout << n << endl << 0 << endl << 0 << endl;
         } else {
-            cout << nums[i] << endl;
+            int a = n / 2;
+            int b = n - a;
+            cout << a << endl << b << endl << 0 << endl;
         }
     }
     return 0;
