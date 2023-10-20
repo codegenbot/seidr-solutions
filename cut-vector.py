bb163496b648358396698306b5981dccd110f8a1
beep boop <githubbot@vadim.me>
2023-10-20 14:28:32 +0200

    Solution 25 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9aptogc1 Prompt: Make sure that 1\n0 -> 1\n0\n0\n

--- cut-vector.py
+++ cut-vector.py
@@ -11,44 +11,29 @@ import re
 Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
 For example,
 input:
-1
-0
+1\n0
 output:
-1
-0
-0\n
+1\n0\n0\n
 
 input:
-1
-10
+1\n10
 output:
-1
-10
-0
+1\n10\n0\n
 
 input:
-1
-100
+1\n100
 output:
-1
-100
-0
+1\n100\n0\n
 
 input:
-1
-1000
+1\n1000
 output:
-1
-1000
-0
+1\n1000\n0\n
 
 input:
-1
-10000
+1\n10000
 output:
-1
-10000
-0
+1\n10000\n0\n
 
 """
 if __name__ == '__main__':
