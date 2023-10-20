ea5656dbf89c066e2cd4d6506094b2869910889d
beep boop <githubbot@vadim.me>
2023-10-20 19:31:52 +0200

    Solution 72 of indices-of-substring, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/vla9zmxx Prompt: Make sure that a\n5 -> 0\n

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -48,4 +48,4 @@ if __name__ == '__main__':
     for i in range(len(text)):
         if text[i:i+len(target)] == target:
             res.append(i)
-    print(" ".join(map(str, res)) + "\n")
+    print(" ".join(map(str, res)), end="")
