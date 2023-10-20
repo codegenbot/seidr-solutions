283fca992aa14fcd3b21693345fae367d05689d8
beep boop <githubbot@vadim.me>
2023-10-21 01:05:57 +0200

    Solution 73 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/jkjtcbny Prompt: Make sure that 1\n0 -> 1\n0\n0\n

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -17,41 +17,41 @@ input:
 1
 0
 output:
-1
+1\n
 0
-0\n
+0
 
 input:
 1
 10
 output:
-1
+1\n
 10
-0\n
+0
 
 input:
 1
 100
 output:
-1
+1\n
 100
-0\n
+0
 
 input:
 1
 1000
 output:
-1
+1\n
 1000
-0\n
+0
 
 input:
 1
 10000
 output:
-1
+1\n
 10000
-0\n
+0
 
 */
 int main() {
