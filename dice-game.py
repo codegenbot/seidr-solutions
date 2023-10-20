e692dbee5f90a309dfb1ad4ab5dbe6497037a1ff
beep boop <githubbot@vadim.me>
2023-10-20 14:49:06 +0200

    Solution 3 of dice-game, ratio 0.02 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/cyo69u13 Prompt: Peter has an n-sided die and Colin has an m-sided die. If they both roll their dice at the same time, return the probability that Peter rolls strictly higher than Colin.

--- dice-game.py
+++ dice-game.py
@@ -39,16 +39,9 @@ output:
 if __name__ == '__main__':
     n = int(input())
     m = int(input())
-    if n >= m:
+    if n < m:
+        print(0.0)
+    elif n == m:
         print(0.5)
-        sys.exit()
-    if n == 1:
-        print(0)
-        sys.exit()
-    if m == 1:
-        print(1)
-        sys.exit()
-    p = 0
-    for i in range(1, n):
-        p += (1/m) * (i/n)
-    print(p)
+    else:
+        print(round((1 / (n - m + 1)) * (n - m) / n, 2))
