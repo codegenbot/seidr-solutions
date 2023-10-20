27921a700900b25b21865a4b9eaa2409d07c031c
beep boop <githubbot@vadim.me>
2023-10-20 14:09:11 +0200

    Solution 6 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9aptogc1 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.py
+++ cut-vector.py
@@ -8,7 +8,8 @@ import itertools
 import queue
 import re
 """
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, 
+or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
 For example,
 input:
 1
@@ -50,51 +51,10 @@ output:
 10000
 0
 
-input:
-1
-2
-3
-4
-5
-6
-7
-8
-9
-output:
-1
-2
-3
-4
-5
-6
-7
-8
-9
-0
-
-input:
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-output:
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-0
-
 """
 if __name__ == '__main__':
+    n = int(input())
+    a = [int(x) for x in input().split()]
+    print(a)
+    # for i in range(len(a)):
+    #     print(a[i])
