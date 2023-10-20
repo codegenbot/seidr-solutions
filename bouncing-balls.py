0a26251ca005f93872811ca14a3d2eabc47ff313
beep boop <githubbot@vadim.me>
2023-10-20 11:59:25 +0200

    Solution 16 of bouncing-balls, ratio 0.12 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/ir0gpxpl Prompt: Make sure that 100.0\n99.999\n20 -> 3999.599534511501

--- bouncing-balls.py
+++ bouncing-balls.py
@@ -8,39 +8,7 @@ import itertools
 import queue
 import re
 """
-Given a starting height and a height after the ﬁrst bounce of a dropped ball, calculate the bounciness index (height of ﬁrst bounce / starting height).
-Then, given a number of bounces, use the bounciness index to calculate the total distance that the ball travels across those bounces.
-For example,
-input:
-1.001
-1.0
-1
-output:
-2.001
-input:
-100.0
-99.999
-20
-output:
-3999.599534511501
-input:
-100.0
-1.0
-20
-output:
-102.02020201974588
-input:
-15.319
-5.635
-1
-output:
-20.954
-input:
-2.176
-1.787
-1
-output:
-3.963
+
 """
 if __name__ == '__main__':
     start_height = float(input())
@@ -49,6 +17,6 @@ if __name__ == '__main__':
     bounciness_index = first_bounce/start_height
     distance_traveled = 0
     for i in range(num_bounces):
-        distance_traveled += start_height + (start_height * bounciness_index * 2)
+        distance_traveled += start_height + (start_height * bounciness_index)
         start_height = start_height * bounciness_index
     print(distance_traveled)
