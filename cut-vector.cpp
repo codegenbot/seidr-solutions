419c05e3af4a4176c55a3885ef8df77f01e7f4f2
beep boop <githubbot@vadim.me>
2023-10-20 16:27:00 +0200

    Solution 30 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/jkjtcbny Prompt: Make sure that 2\n2 129 -> 1\n2\n1\n129

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -11,90 +11,78 @@
 #include <climits>
 using namespace std;
 /*
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.\
-For example,\
-input:\
-1\
-0\
-output:\
-1\
-0\
-0\
-\
-input:\
-1\
-10\
-output:\
-1\
-10\
-0\
-\
-input:\
-1\
-100\
-output:\
-1\
-100\
-0\
-\
-input:\
-1\
-1000\
-output:\
-1\
-1000\
-0\
-\
-input:\
-1\
-10000\
-output:\
-1\
-10000\
-0\
-\
-input:\
-2\
-2\
-129\
-output:\
-1\
-2\
-1\
-129\
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
+For example,
+input:
+1
+0
+output:
+1
+0
+0
+
+input:
+1
+10
+output:
+1
+10
+0
+
+input:
+1
+100
+output:
+1
+100
+0
+
+input:
+1
+1000
+output:
+1
+1000
+0
+
+input:
+1
+10000
+output:
+1
+10000
+0
+
+input:
+2
+2
+129
+output:
+1
+2
+1
+129
+
 */
 int main() {
-  int n;
-  cin>>n;
-  vector<int> v(n);
-  for(int i=0;i<n;i++) {
-    cin>>v[i];
-  }
-  int l=0,r=0;
-  int min=INT_MAX;
-  for(int i=0;i<n-1;i++) {
-    if(abs(v[i]-v[i+1])<min) {
-      min=abs(v[i]-v[i+1]);
-      l=i;
-      r=i+1;
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
     }
-  }
-  if(l==0) {
-    cout<<1<<endl;
-    cout<<v[0]<<endl;
-    cout<<1<<endl;
-    cout<<v[1]<<endl;
-  }
-  else {
-    for(int i=0;i<=l;i++) {
-      cout<<v[i]<<" ";
+    int ans = INT_MAX;
+    int ans_idx;
+    for (int i = 1; i < n; ++i) {
+        if (abs(nums[i] - nums[i - 1]) < ans) {
+            ans = abs(nums[i] - nums[i - 1]);
+            ans_idx = i;
+        }
     }
-    cout<<endl;
-    for(int i=r;i<n;i++) {
-      cout<<v[i]<<" ";
+    for (int i = 0; i < ans_idx; ++i) {
+        cout << nums[i] << endl;
     }
-    cout<<endl;
-  }
-  cout<<endl;
-  return 0;
+    for (int i = ans_idx; i <= n; ++i) {
+        cout << nums[i] << endl;
+    }
 }
