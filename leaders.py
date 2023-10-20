459a185a5d8f1e14ec3473a657010e277ae53c28
beep boop <githubbot@vadim.me>
2023-10-20 19:10:25 +0200

    Solution 21 of leaders, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/3pi8mwm2 Prompt: Make sure that 0\n -> 0\n

--- leaders.py
+++ leaders.py
@@ -44,7 +44,4 @@ output:
 if __name__ == '__main__':
     n = int(input())
     a = list(map(int, input().split()))
-    if len(a) == 1:
-        print(a[0])
-    else:
-        print(a)
+    print(a[0])
