42f9b80dea61c6ec567ccbc553b66506bb4ce17c
beep boop <githubbot@vadim.me>
2023-10-20 22:28:02 +0200

    Solution 31 of mastermind, ratio 0.26 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/74qj1d4o Prompt: Make sure that RRRR\nRRRR -> 0\n4

--- mastermind.py
+++ mastermind.py
@@ -42,8 +42,8 @@ output:
 0
 """
 if __name__ == '__main__':
-    code = 'RRRR'
-    guess = 'RRRR'
+    code = input()
+    guess = input()
     black = 0
     white = 0
     code_list = []
@@ -58,6 +58,5 @@ if __name__ == '__main__':
         if code_list[i] in guess_list:
             white += 1
             guess_list.remove(code_list[i])
-            code_list.remove(code_list[i])
     print(black)
     print(white)
