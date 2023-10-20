c2099b7c0718662f268e794acf21598638ec40c3
beep boop <githubbot@vadim.me>
2023-10-20 21:14:51 +0200

    Solution 27 of indices-of-substring, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9p0tbiv8 Prompt: Make sure that a\n5 -> 0\n

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -54,9 +54,11 @@ int main() {
     while(i < lenT) {
         if(text[i] == target[j]) {
             if(j == len - 1) {
-                cout << i - len + 1 << " ";
+                cout << i - len + 1;
                 j = 0;
                 i = i - len + 1 + 1;
+                if(i < lenT)
+                    cout << " ";
             } else {
                 i++;
                 j++;
@@ -66,6 +68,5 @@ int main() {
             j = 0;
         }
     }
-    cout << '\n';
     return 0;
 }
