14f579624943ac2717a6f3ab8e499a2bacf0bad2
beep boop <githubbot@vadim.me>
2023-10-20 19:07:12 +0200

    Solution 19 of leaders, ratio 0.97 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/3pi8mwm2 Prompt: Make sure that 7\n5 5 5 5 5 5 5 -> 7\n5 5 5 5 5 5 5

--- leaders.py
+++ leaders.py
@@ -11,42 +11,20 @@ import re
 Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.
 For example,
 input:
-0
-
+7
+5 5 5 5 5 5 5
 output:
-0
-
-input:
-1
-0
-output:
-1
-0
-input:
-1
-451
-output:
-1
-451
-input:
-2
-1000 0
-output:
-2
-1000 0
-input:
-2
-0 1000
-output:
-1
-1000
+7
+5 5 5 5 5 5 5
 """
 if __name__ == '__main__':
     n = int(input())
     a = [int(i) for i in input().split()]
     ans = []
+    mx = -1
     for i in range(n-1, -1, -1):
-        if len(ans) == 0 or ans[-1] <= a[i]:
+        if mx < a[i]:
+            mx = a[i]
             ans.append(a[i])
     print(len(ans))
     print(*ans[::-1])
