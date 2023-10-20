e0b2201d6bc6e84b9b7363678c49c72e9b155a44
beep boop <githubbot@vadim.me>
2023-10-20 21:53:12 +0200

    Solution 11 of mastermind, ratio 0.26 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/74qj1d4o Prompt: Make sure that RRRR\nRRRR -> 0\n4

--- mastermind.py
+++ mastermind.py
@@ -41,6 +41,33 @@ output:
 0
 0
 """
+
+
+def mastermind(line1, line2):
+    code = {}
+    for i in range(4):
+        if line1[i] not in code:
+            code[line1[i]] = 1
+        else:
+            code[line1[i]] += 1
+    guess = {}
+    for i in range(4):
+        if line2[i] not in guess:
+            guess[line2[i]] = 1
+        else:
+            guess[line2[i]] += 1
+    white = 0
+    for i in code:
+        if i in guess:
+            white += min(code[i], guess[i])
+    black = 0
+    for i in range(4):
+        if line1[i] == line2[i]:
+            black += 1
+    white -= black
+    return black, white
+
+
 if __name__ == '__main__':
     line1 = input().strip()
     line2 = input().strip()
@@ -65,4 +92,5 @@ if __name__ == '__main__':
         if line1[i] == line2[i]:
             black += 1
     white -= black
-    print(str(black) + "\n" + str(white))
+    print(black)
+    print(white)
