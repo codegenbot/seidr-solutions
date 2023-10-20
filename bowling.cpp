993537b78300ea21c9209dd339d11ad39d9a2bd5
beep boop <githubbot@vadim.me>
2023-10-20 15:11:43 +0200

    Solution 37 of bowling, ratio 0.02 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/js6eizju Prompt: Make sure that XXXXXXXXXXXX -> 300

--- bowling.cpp
+++ bowling.cpp
@@ -38,17 +38,17 @@ int main() {
     string str;
     cin >> str;
     int score = 0;
-    for(int i = 0; i < str.size(); i++){
+    for(int i = 0; i < str.size() && i < 11; i++){
         if(str[i] == 'X'){
             score += 10;
-            if(i + 1 < str.size() && str[i + 1] == 'X'){
+            if(str[i + 1] == 'X'){
                 score += 10;
-                if(i + 2 < str.size() && str[i + 2] == 'X'){
+                if(str[i + 2] == 'X'){
                     score += 10;
                 }else{
                     score += str[i + 2] - '0';
                 }
-            }else if(i + 1 < str.size() && str[i + 1] == '/'){
+            }else if(str[i + 1] == '/'){
                 score += 10;
             }else{
                 score += str[i + 1] - '0';
