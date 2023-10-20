55a2d499cae87e95526d07433a479fc890d093d1
beep boop <githubbot@vadim.me>
2023-10-20 14:13:35 +0200

    Solution 37 of camel-case, ratio 0.42 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9p2csco9 Prompt: Make sure that  ->

--- camel-case.py
+++ camel-case.py
@@ -8,7 +8,7 @@ import itertools
 import queue
 import re
 """
-Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string"-> "camelCase exampleTestString".
+Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string"-> "camelCase exampleTestString". 
 For example,
 input:
 
@@ -33,4 +33,13 @@ all separate words
 """
 if __name__ == '__main__':
     s = input()
-    print(s.replace('-', ' ').title().replace(' ', ''))
+    a = s.split('-')
+    b = s.split(' ')
+    if len(a) == 1 and len(b) == 1:
+        print(s)
+    elif len(a) == 1 and len(b) != 1:
+        print(' '.join(b))
+    else:
+        for i in range(len(a)):
+            a[i] = a[i].title()
+        print(''.join(a))
