e00de1c649b1ea9d5501a639530069abefdff701
beep boop <githubbot@vadim.me>
2023-10-20 18:20:07 +0200

    Solution 30 of indices-of-substring, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/vla9zmxx Prompt: Make sure that a\n5 -> 0\n

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -8,7 +8,8 @@ import itertools
 import queue
 import re
 """
-Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.
+Given a text string and a target string, return a list of integers of the indices at which the target appears in the text.
+Targets may overlap.
 For example,
 input:
 a
@@ -40,12 +41,25 @@ input:
 output:
 12
 0 1 2 3 4 5 6 7 8 9 10 11
+input:
+a
+5
+output:
+0
+
 """
 if __name__ == '__main__':
     text = input()
     target = input()
-    result = []
+    length = len(target)
+    count = 0
+    res = []
     for i in range(len(text)):
-        if text[i:i+len(target)] == target:
-            result.append(i)
-    print((" ".join([str(x) for x in result])).strip())
+        if text[i] == target[count]:
+            count += 1
+            if count == length:
+                res.append(i+1-count)
+                count = 0
+        else:
+            count = 0
+    print(*res)
