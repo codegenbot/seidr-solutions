cba2718a44b63fab99284409743921d98ab1727a
beep boop <githubbot@vadim.me>
2023-10-20 14:11:08 +0200

    Solution 9 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9aptogc1 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.py
+++ cut-vector.py
@@ -51,28 +51,7 @@ output:
 0
 
 """
-
-
-def find_splitter(lst):
-    min_diff = max(lst)
-    splitter = 0
-    for i in range(len(lst)):
-        if i == 0:
-            if lst[1] - lst[0] <= min_diff:
-                min_diff = lst[1] - lst[0]
-                splitter = lst[0]
-        elif i == len(lst) - 1:
-            if lst[i] - lst[i - 1] <= min_diff:
-                min_diff = lst[i] - lst[i - 1]
-                splitter = lst[i]
-        else:
-            if lst[i + 1] - lst[i] <= min_diff:
-                min_diff = lst[i + 1] - lst[i]
-                splitter = lst[i]
-            if lst[i] - lst[i - 1] <= min_diff:
-                min_diff = lst[i] - lst[i - 1]
-                splitter = lst[i]
-    return splitter
-
-
 if __name__ == '__main__':
+    vector = [1, 0]
+    print(vector[:1])
+    print(vector[1:])
