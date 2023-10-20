e6fca77ebc1793ca1e062f48208985f9812649d5
beep boop <githubbot@vadim.me>
2023-10-20 20:20:23 +0200

    Solution 71 of leaders, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/3pi8mwm2 Prompt: Fix Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6b667634-7c0b-418b-a434-cc9395a04 4b0.py", line 49, in <module>     max_num = nums[-1]               ~~~~^^^^ IndexError: list index out of range

--- leaders.py
+++ leaders.py
@@ -46,10 +46,11 @@ if __name__ == '__main__':
     nums = [int(i) for i in input().split()]
     # print(nums)
     leaders = []
-    max_num = nums[-1] if n > 0 else 0
-    leaders.append(max_num)
-    for i in range(n-2, -1, -1):
-        if nums[i] >= max_num:
-            leaders.append(nums[i])
-            max_num = nums[i]
-    print(" ".join([str(i) for i in leaders[::-1]]))
+    if nums:
+        max_num = nums[-1]
+        leaders.append(max_num)
+        for i in range(n-2, -1, -1):
+            if nums[i] >= max_num:
+                leaders.append(nums[i])
+                max_num = nums[i]
+        print(" ".join([str(i) for i in leaders[::-1]]))
