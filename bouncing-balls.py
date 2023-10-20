61d5d4fadd299917612c208dfa72e95991bbcf37
beep boop <githubbot@vadim.me>
2023-10-20 12:14:17 +0200

    Solution 30 of bouncing-balls, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/ir0gpxpl Prompt: Make sure that 100.0\n99.999\n20 -> 3999.599534511501

--- bouncing-balls.py
+++ bouncing-balls.py
@@ -1,12 +1,6 @@
-import os
-import sys
 import numpy as np
-import math
-import datetime
-import collections
-import itertools
-import queue
-import re
+
+
 """
 Given a starting height and a height after the ﬁrst bounce of a dropped ball, calculate the bounciness index (height of ﬁrst bounce / starting height). Then, given a number of bounces, use the bounciness index to calculate the total distance that the ball travels across those bounces.
 For example,
@@ -41,12 +35,16 @@ input:
 output:
 3.963
 """
+
+
+def bounciness(h1, h2, n):
+    b = h2 / h1
+    return (h1 + h2) * (1 - b ** n) / (1 - b)
+
+
 if __name__ == '__main__':
-    bounciness_index = 0.0
-    total_distance = 0.0
-    starting_height = float(input())
-    height_after_first_bounce = float(input())
-    number_of_bounces = int(input())
-    bounciness_index = height_after_first_bounce / starting_height
-    total_distance = (starting_height + height_after_first_bounce) * (1 - bounciness_index ** number_of_bounces) / (1 - bounciness_index)
-    print(total_distance)
+    print(bounciness(1.001, 1.0, 1))
+    print(bounciness(100.0, 99.999, 20) == 3999.599534511501)
+    print(bounciness(100.0, 1.0, 20))
+    print(bounciness(15.319, 5.635, 1))
+    print(bounciness(2.176, 1.787, 1))
