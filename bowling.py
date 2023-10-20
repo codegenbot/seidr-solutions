5a09919e985c57689d4b367679ef9031e93b215d
beep boop <githubbot@vadim.me>
2023-10-20 13:25:39 +0200

    Solution 47 of bowling, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/aiih34db Prompt: Make sure that -------------------- -> 0

--- bowling.py
+++ bowling.py
@@ -49,13 +49,11 @@ def bowling(frames):
     score = 0
     for i in range(0, len(frames), 2):
         if frames[i] == 'X':
-            score += 10 + (int(frames[i+2]) if frames[i+2] != 'X' else 10) + (int(frames[i+3]) if frames[i+3] != '/' else 10 - int(frames[i+1])) if i < len(frames) - 2 else 10
+            score += 10 + (int(frames[i+2]) if frames[i+2] != 'X' else 10) + (int(frames[i+3]) if frames[i+3] != '/' else 10 - int(frames[i+1]))
         elif frames[i+1] == '/':
-            score += 10 + (int(frames[i+2]) if frames[i+2] != 'X' else 10) if i < len(frames) - 3 else 10
+            score += 10 + (int(frames[i+2]) if frames[i+2] != 'X' else 10)
         elif frames[i+1] == '-':
-            score += int(frames[i])
-        elif frames[i] == '-' and frames[i+1] == '-':
-            score += 0
+            score += int(frames[i]) + int(frames[i+1])
         else:
             score += int(frames[i]) + int(frames[i+1])
     return score
