97694a52f4d4a7dbc112a4f351e51baaf82554ee
beep boop <githubbot@vadim.me>
2023-10-20 17:34:25 +0200

    Solution 11 of dice-game, ratio 0.06 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/no1bgug5 Prompt: Make sure that 1\n2 -> 0.0

--- dice-game.cpp
+++ dice-game.cpp
@@ -43,14 +43,10 @@ int main() {
     int n,m;
     cin >> n >> m;
     if(n == m){
-        cout << "0.5" << endl;
-        return 0;
-    }
-    if(n > m) swap(n,m);
-    if(n == 1){
         cout << "0.0" << endl;
         return 0;
     }
+    if(n > m) swap(n,m);
     double ans = 0.0;
     for(int i = 1; i <= n; i++){
         ans += (double)i/(double)(n*m);
