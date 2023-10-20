c67cc26b64c7e7bdf0843e4e78066fc057ec55bb
beep boop <githubbot@vadim.me>
2023-10-20 18:46:32 +0200

    Solution 3 of leaders, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/3pi8mwm2 Prompt: Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.

--- leaders.py
+++ leaders.py
@@ -42,13 +42,46 @@ output:
 1000
 """
 if __name__ == '__main__':
+    """
+    Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.
+    For example,
+    input:
+    0
+
+    output:
+    0
+
+    input:
+    1
+    0
+    output:
+    1
+    0
+    input:
+    1
+    451
+    output:
+    1
+    451
+    input:
+    2
+    1000 0
+    output:
+    2
+    1000 0
+    input:
+    2
+    0 1000
+    output:
+    1
+    1000
+    """
     n = int(input())
-    a = [int(i) for i in input().split()]
-    ans = []
-    mx = -1
-    for i in range(n-1, -1, -1):
-        if mx < a[i]:
-            mx = a[i]
-            ans.append(a[i])
-    print(len(ans))
-    print(*ans[::-1])
+    arr = [int(i) for i in input().split()]
+    max_num = -1
+    res = []
+    for i in range(len(arr)-1, -1, -1):
+        if arr[i] >= max_num:
+            res.append(arr[i])
+            max_num = arr[i]
+    print(" ".join([str(i) for i in res[::-1]]))
