a52447365314f105a173589c474d8384710b6da9
beep boop <githubbot@vadim.me>
2023-10-20 15:32:14 +0200

    Solution 45 of dice-game, ratio 0.03 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/cyo69u13 Prompt: Make sure that 1\n2 -> 0.0

--- dice-game.py
+++ dice-game.py
@@ -37,5 +37,9 @@ output:
 0.0
 """
 if __name__ == '__main__':
-    n, m = [int(i) for i in input().split()]
-    print(0.0)
+    n = int(input())
+    m = int(input())
+    if n > m:
+        print(1.0)
+    else:
+        print(0.0)
