3683b2e1f2a11fa1b0786d2c3a3f1e918a21418c
beep boop <githubbot@vadim.me>
2023-10-20 23:43:08 +0200

    Solution 80 of mastermind, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/74qj1d4o Prompt: Fix   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2f2c010a-2d58-4901-9747-e66d69c74 935.py", line 47     if __name__ == '__main__': IndentationError: expected an indented block after 'if' statement on line 47

--- mastermind.py
+++ mastermind.py
@@ -1,5 +1,17 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
 """
-Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.
+Based on the board game Mastermind. Given a Mastermind code and a guess, 
+each of which are 4-character strings consisting of 6 possible characters, 
+return the number of white pegs (correct color, wrong place) and black pegs 
+(correct color, correct place) the codemaster should give as a clue.
 For example,
 input:
 RRRR
@@ -33,18 +45,4 @@ output:
 0
 """
 if __name__ == '__main__':
-    code = input()
-    guess = input()
-    white = 0
-    black = 0
-    for i in range(4):
-        if code[i] == guess[i]:
-            black += 1
-            code = code[:i] + '-' + code[i+1:]
-            guess = guess[:i] + '-' + guess[i+1:]
-    for i in range(4):
-        if guess[i] in code:
-            white += 1
-            code = code.replace(guess[i], '-', 1)
-    print(black)
-    print(white)
+    pass
