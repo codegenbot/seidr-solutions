02e5ef0823cb061e13d08580308a8e76b56b1af7
beep boop <githubbot@vadim.me>
2023-10-20 15:01:44 +0200

    Solution 80 of camel-case, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9p2csco9 Prompt: Fix   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cabd13d3-0346-4788-91bb-2e242d777 b1e.py", line 39     print kebab_to_camel('camel-case example-test-string')     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?

--- camel-case.py
+++ camel-case.py
@@ -8,7 +8,7 @@ import itertools
 import queue
 import re
 """
-Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string" -> "camelCase exampleTestString".
+Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string"-> "camelCase exampleTestString".
 For example,
 input:
 
@@ -31,21 +31,9 @@ all separate words
 output:
 all separate words
 """
-def camelCase(str):
-    lst = str.split()
-    for i in range(len(lst)):
-        if '-' in lst[i]:
-            lst.extend(lst[i].split('-'))
-            lst.remove(lst[i])
-    for i in range(len(lst)):
-        lst[i] = lst[i].capitalize()
-    string = lst[0]
-    for i in range(1,len(lst)):
-        string += lst[i]
-    return string
+
+def kebab_to_camel(string):
+    return ' '.join(map(lambda x: x.capitalize(), string.split()))
+
 if __name__ == '__main__':
-    print(camelCase("camel-case example-test-string"))
-    print(camelCase("nospaceordash"))
-    print(camelCase("two-words"))
-    print(camelCase("two words"))
-    print(camelCase("all separate words"))
+    print(kebab_to_camel('camel-case example-test-string'))
