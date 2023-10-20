702d1b69d200e389dbd00e49e8a125e955996f33
beep boop <githubbot@vadim.me>
2023-10-20 18:13:08 +0200

    Solution 25 of indices-of-substring, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/vla9zmxx Prompt: Make sure that a\n5 -> 0\n

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -48,4 +48,4 @@ if __name__ == '__main__':
     for i in range(len(text)):
         if text[i:i+len(target)] == target:
             result.append(i)
-    print(" ".join([str(x) for x in result]) + "\n")
+    print(" ".join([str(x) for x in result]), end="")
