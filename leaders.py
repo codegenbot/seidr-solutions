b91034e21ef168aae346e1befbc9befa493127c8
beep boop <githubbot@vadim.me>
2023-10-20 19:40:37 +0200

    Solution 40 of leaders, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/3pi8mwm2 Prompt: Make sure that 0\n -> 0\n

--- leaders.py
+++ leaders.py
@@ -42,4 +42,49 @@ output:
 1000
 """
 if __name__ == '__main__':
-    print(0)
+    """
+    Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.
+    For example,
+    input:
+    0
+
+    output:
+    0
+
+    input:
+    1
+    0
+    output:
+    1
+    0
+    input:
+    1
+    451
+    output:
+    1
+    451
+    input:
+    2
+    1000 0
+    output:
+    2
+    1000 0
+    input:
+    2
+    0 1000
+    output:
+    1
+    1000
+    """
+    """
+    Make sure that 0\n -> 0\n
+    """
+    n = int(input())
+    arr = [int(i) for i in input().split()]
+    max_num = -1
+    res = []
+    for i in range(len(arr)-1, -1, -1):
+        if arr[i] >= max_num:
+            res.append(arr[i])
+            max_num = arr[i]
+    print(" ".join([str(i) for i in res[::-1]]))
