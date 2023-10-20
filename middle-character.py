7f9a5bcb5b9529cb288d88fe11da5996e7eb95cd
beep boop <githubbot@vadim.me>
2023-10-20 22:45:03 +0200

    Solution 4 of middle-character, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/uoytypoh Prompt: Given a string, return the middle character as a string if it is odd length; return the two middle characters as a string if it is even length.

--- middle-character.py
+++ middle-character.py
@@ -32,4 +32,9 @@ output:
 )b
 """
 if __name__ == '__main__':
-    pass
+    print("Enter a string:")
+    s = input()
+    if len(s) % 2 == 0:
+        print(s[len(s) // 2 - 1:len(s) // 2 + 1])
+    else:
+        print(s[len(s) // 2])
