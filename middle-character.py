7d1b494c04039fe32ec1c0936c8a7359970d45ca
beep boop <githubbot@vadim.me>
2023-10-20 23:27:15 +0200

    Solution 30 of middle-character, ratio 0.96 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/uoytypoh Prompt: Make sure that   ->

--- middle-character.py
+++ middle-character.py
@@ -8,17 +8,16 @@ import itertools
 import queue
 import re
 """
-Given a string, return the middle character as a string if it is odd length; 
-return the two middle characters as a string if it is even length.
+Given a string, return the middle character as a string if it is odd length; return the two middle characters as a string if it is even length.
 For example,
 input:
 Q
 output:
 Q
 input:
- 
+
 output:
- 
+
 input:
 $
 output:
@@ -32,13 +31,9 @@ input:
 output:
 )b
 """
-
-def middle(string):
-    if len(string) % 2 == 0:
-        return string[len(string)//2-1:len(string)//2+1]
-    else:
-        return string[len(string)//2]
-
 if __name__ == '__main__':
-    string = input()
-    print(middle(string))
+    str = input()
+    if len(str) % 2 == 0:
+        print(str[int(len(str) / 2) - 1:int(len(str) / 2) + 1])
+    else:
+        print(str[int(len(str) / 2)])
