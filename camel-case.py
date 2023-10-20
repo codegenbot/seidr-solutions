a9843445e2b4d1c3e86eaffe1d7e53b34d22f104
beep boop <githubbot@vadim.me>
2023-10-20 14:36:09 +0200

    Solution 57 of camel-case, ratio 0.28 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9p2csco9 Prompt: Make sure that  ->

--- camel-case.py
+++ camel-case.py
@@ -8,28 +8,13 @@ import itertools
 import queue
 import re
 """
-Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string"-> "camelCase exampleTestString"
-For example,
-input:
-
-output:
-
-input:
-nospaceordash
-output:
-nospaceordash
-input:
-two-words
-output:
-twoWords
-input:
-two words
-output:
-two words
-input:
-all separate words
-output:
-all separate words
+Given an array of integers, sort the array into a wave like array and return it, 
+In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5.....
+Given [1, 2, 3, 4]
+One possible answer : [2, 1, 4, 3]
+Another possible answer : [4, 1, 3, 2]
+NOTE : If there are multiple answers possible, return the one thats lexicographically smallest. 
+So, in example case, you will return [2, 1, 4, 3]
 """
 if __name__ == '__main__':
     s = input()
