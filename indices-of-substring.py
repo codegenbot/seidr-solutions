428a73eb4091b659143a898ac55201c2171d8666
beep boop <githubbot@vadim.me>
2023-10-20 19:19:13 +0200

    Solution 65 of indices-of-substring, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/vla9zmxx Prompt: Make sure that a\n5 -> 0\n

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -14,35 +14,35 @@ input:
 a
 5
 output:
-0
+0\n
 
 input:
 !
 !
 output:
-1
-0
+1\n
+0\n
 input:
 r
 nm,xcnwqnd@#$fwkdjn3
 output:
-0
+0\n
 
 input:
 hi
 hihihihihihihihihihi
 output:
-0
+0\n
 
 input:
 ############
 #
 output:
 12
-0 1 2 3 4 5 6 7 8 9 10 11
+0 1 2 3 4 5 6 7 8 9 10 11\n
 """
 if __name__ == '__main__':
     text = sys.stdin.readline().strip()
     target = sys.stdin.readline().strip()
     result = [i for i in range(len(text)) if text[i:i+len(target)] == target]
-    print(" ".join(str(i) for i in result) if len(result) > 0 else "")
+    print(" ".join(str(i) for i in result))
