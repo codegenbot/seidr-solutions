e4e4fe724f4c614b5259f1ca090566fb14951ddb
beep boop <githubbot@vadim.me>
2023-10-20 16:42:00 +0200

    Solution 5 of dice-game, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/no1bgug5 Prompt: Peter has an n-sided die and Colin has an m-sided die. If they both roll their dice at the same time, return the probability that Peter rolls strictly higher than Colin.

--- dice-game.cpp
+++ dice-game.cpp
@@ -40,17 +40,9 @@ output:
 0.0
 */
 int main() {
-    int n,m;
-    while(cin>>n>>m){
-        double ans = 0;
-        for(int i = 1; i <= n; i++){
-            double tmp = 1;
-            for(int j = 1; j <= m; j++){
-                if(i > j) tmp += 1.0/m;
-            }
-            ans += tmp/n;
-        }
-        printf("%.2lf\n",ans);
-    }
+    int n, m;
+    cin >> n >> m;
+    double res = (double)(m-1)/(n+m-1);
+    cout << res << endl;
     return 0;
 }
