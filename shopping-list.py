168e729b9783ed20ea6e779e7cce62543d8a5325
beep boop <githubbot@vadim.me>
2023-10-21 01:49:13 +0200

    Solution 5 of shopping-list, ratio 0.07 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/mgkxah6m Prompt: Given a vector of ﬂoats representing the prices of various shopping goods and another vector of floats representing the percent discount of each of those goods, return the total price of the shopping trip after applying the discount to each item.

--- shopping-list.py
+++ shopping-list.py
@@ -11,19 +11,19 @@ import re
 Given a vector of ﬂoats representing the prices of various shopping goods and another vector of floats representing the percent discount of each of those goods, return the total price of the shopping trip after applying the discount to each item.
 For example,
 input:
-2
+1
 50.0
-2
+1
 100.0
 output:
-45.0
+0.0
 input:
-2
+1
 50.0
-2
+1
 10.0
 output:
-40.5
+45.0
 input:
 2
 20.0 20.0
@@ -47,10 +47,11 @@ output:
 29.0
 """
 if __name__ == '__main__':
-    n = int(input())
-    a = list(map(float, input().split()))
-    m = int(input())
-    b = list(map(float, input().split()))
-    for i in range(len(a)):
-        a[i] = a[i] - a[i]*(b[i]/100)
-    print(sum(a))
+    num = int(input())
+    prices = [float(x) for x in input().split()]
+    num = int(input())
+    discounts = [float(x) for x in input().split()]
+    total = 0.0
+    for i in range(num):
+        total += prices[i] * (100-discounts[i])/100
+    print(total)
