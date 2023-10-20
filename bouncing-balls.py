343aed2ee4e32be3c49fa1797cb5f1aa9be26cf9
beep boop <githubbot@vadim.me>
2023-10-20 11:50:54 +0200

    Solution 8 of bouncing-balls, ratio 0.12 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/ir0gpxpl Prompt: Given a starting height and a height after the ﬁrst bounce of a dropped ball, calculate the bounciness index (height of ﬁrst bounce / starting height). Then, given a number of bounces, use the bounciness index to calculate the total distance that the ball travels across those bounces.

--- bouncing-balls.py
+++ bouncing-balls.py
@@ -8,8 +8,8 @@ import itertools
 import queue
 import re
 """
-Given a starting height and a height after the ﬁrst bounce of a dropped ball, calculate the bounciness index (height of ﬁrst bounce / starting 
-height). Then, given a number of bounces, use the bounciness index to calculate the total distance that the ball travels across those bounces.
+Given a starting height and a height after the ﬁrst bounce of a dropped ball, calculate the bounciness index (height of ﬁrst bounce / starting height).
+Then, given a number of bounces, use the bounciness index to calculate the total distance that the ball travels across those bounces.
 For example,
 input:
 1.001
@@ -43,10 +43,12 @@ output:
 3.963
 """
 if __name__ == '__main__':
-    line = input().strip()
-    start, height, num = (float(x) for x in line.split())
-    bounciness = height/start
-    total = start + height
-    for i in range(2, num+1):
-        total += height*bounciness**i
-    print(total)
+    start_height = float(input())
+    first_bounce = float(input())
+    num_bounces = int(input())
+    bounciness_index = first_bounce/start_height
+    distance_traveled = 0
+    for i in range(num_bounces):
+        distance_traveled += start_height + (start_height * bounciness_index)
+        start_height = start_height * bounciness_index
+    print(distance_traveled)
