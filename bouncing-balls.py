ca78c24d9e7a629807013808c245f410bf9bb31c
beep boop <githubbot@vadim.me>
2023-10-20 15:13:32 +0200

    Solution 62 of bouncing-balls, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/ir0gpxpl Prompt: Fix Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4d0251d6-e34e-427f-8b18-43414d378 21e.py", line 61, in <module>     print(total_distance(start_height, first_bounce, num_bounces))           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4d0251d6-e34e-427f-8b18-43414d378 21e.py", line 50, in total_distance     bounciness_index = bounciness_index(start_height, first_bounce)                        ^^^^^^^^^^^^^^^^ UnboundLocalError: cannot access local variable 'bounciness_index' where it is n ot associated with a value

--- bouncing-balls.py
+++ bouncing-balls.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
 
     def total_distance(start_height, first_bounce, num_bounces):
         total_distance = 0
-        bounciness_index = bounciness_index(start_height, first_bounce) # this line gives an error
+        bounciness_index = bounciness_index(start_height, first_bounce) # should be "bounciness_index" not "bounciness_index"
         for bounce in range(num_bounces):
             total_distance += start_height
             start_height *= bounciness_index
