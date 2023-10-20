4ce3d56eec50dc1804988ff362497a8892b2a210
beep boop <githubbot@vadim.me>
2023-10-21 00:57:06 +0200

    Solution 60 of shopping-list, ratio 0.08 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9w6imq7i Prompt: Make sure that 1\n50.0\n1\n100.0 -> 0.0

--- shopping-list.cpp
+++ shopping-list.cpp
@@ -51,20 +51,20 @@ output:
 */
 int main() {
     int n;
-    cin>>n;
-    vector<float> p(n);
-    for(int i = 0; i < n; ++i) {
-        cin>>p[i];
+    cin >> n;
+    vector<float> price(n);
+    for (int i = 0; i < n; i++) {
+        cin >> price[i];
     }
-    cin>>n;
-    vector<float> d(n);
-    for(int i = 0; i < n; ++i) {
-        cin>>d[i];
+    cin >> n;
+    vector<float> discount(n);
+    for (int i = 0; i < n; i++) {
+        cin >> discount[i];
     }
-    float sum = 0;
-    for(int i = 0; i < min(p.size(), d.size()); ++i) {
-        sum += p[i] * (1 - d[i] / 100);
+    float res = 0;
+    for (int i = 0; i < n; i++) {
+        res += price[i] * (1 - discount[i] / 100.0);
     }
-    cout<<sum<<endl;
+    cout << res << endl;
     return 0;
 }
