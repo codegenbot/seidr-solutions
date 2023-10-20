d06b1a99bc6e57084e1c2729c132e69d9172f72f
beep boop <githubbot@vadim.me>
2023-10-21 01:03:23 +0200

    Solution 69 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/jkjtcbny Prompt: Fix       |                  ^~~~ bbe3d0d5-d64c-42f1-ace2-e97576638d65.cpp:20:30: error: ‘endl’ was not declared i n this scope; did you mean ‘std::endl’?    20 |     cout << min_index + 1 << endl;       |                              ^~~~       |                              std::endl In file included from /usr/include/c++/11/istream:39,                  from /usr/include/c++/11/sstream:38,                  from /usr/include/c++/11/complex:45,                  from /usr/include/c++/11/ccomplex:39,                  from /usr/include/x86_64-linux-gnu/c++/11/bits/stdc++.h:54,                  from bbe3d0d5-d64c-42f1-ace2-e97576638d65.cpp:1: /usr/include/c++/11/ostream:684:5: note: ‘std::endl’ declared here   684 |     endl(basic_ostream<_CharT, _Traits>& __os)       |     ^~~~ bbe3d0d5-d64c-42f1-ace2-e97576638d65.cpp:22:17: error: ‘v’ was not declared in t his scope    22 |         cout << v[i] << " ";       |                 ^ bbe3d0d5-d64c-42f1-ace2-e97576638d65.cpp:26:17: error: ‘v’ was not declared in t his scope    26 |         cout << v[i] << " ";       |                 ^

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -2,28 +2,28 @@
 
 int main()
 {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++)
-        cin >> v[i];
-    int min = INT_MAX;
-    int min_index = 0;
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (abs(v[i] - v[i + 1]) < min)
-        {
-            min = abs(v[i] - v[i + 1]);
-            min_index = i;
-        }
-    }
-    cout << min_index + 1 << std::endl;
-    for (int i = 0; i <= min_index; i++)
-        cout << v[i] << " ";
-    cout << endl;
-    cout << n - min_index - 1 << endl;
-    for (int i = min_index + 1; i < n; i++)
-        cout << v[i] << " ";
-    cout << endl;
-    return 0;
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+	int min = INT_MAX;
+	int min_index = 0;
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (abs(v[i] - v[i + 1]) < min)
+		{
+			min = abs(v[i] - v[i + 1]);
+			min_index = i;
+		}
+	}
+	cout << min_index + 1 << std::endl;
+	for (int i = 0; i <= min_index; i++)
+		cout << v[i] << " ";
+	cout << std::endl;
+	cout << n - min_index - 1 << std::endl;
+	for (int i = min_index + 1; i < n; i++)
+		cout << v[i] << " ";
+	cout << std::endl;
+	return 0;
 }
