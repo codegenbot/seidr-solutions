d618053311df0b89c98566a9d1cc55e48f01d878
beep boop <githubbot@vadim.me>
2023-10-20 15:53:48 +0200

    Solution 16 of bouncing-balls, ratio 0.05 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/wsjdr5hk Prompt: Make sure that 100.0\n99.999\n20 -> 3999.599534511501

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -44,12 +44,13 @@ input:
 output:
 3.963
 */
+
 int main() {
     double start, first, bounciness;
     int bounces;
     cin >> start >> first >> bounces;
     bounciness = first / start;
-    double ans = start * pow(bounciness, 0);
+    double ans = start;
     for (int i = 0; i < bounces; i++) {
         ans += start * pow(bounciness, i + 1);
     }
