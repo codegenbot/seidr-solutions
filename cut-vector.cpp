c1badc6b634d93ebe1d8a4d76ed3bb33b7e9658c
beep boop <githubbot@vadim.me>
2023-10-21 01:06:55 +0200

    Solution 75 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/jkjtcbny Prompt: Make sure that 1\n0 -> 1\n0\n0\n

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -11,28 +11,29 @@
 #include <climits>
 using namespace std;
 /*
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible.
-Return the two resulting subvectors as two outputs.
+Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
 For example,
 input:
 1
 0
 output:
-1
+1\n
 0
 0
+
 input:
 1
 10
 output:
-1
+1\n
 10
 0
+
 input:
 1
 100
 output:
-1
+1\n
 100
 0
 
@@ -40,7 +41,7 @@ input:
 1
 1000
 output:
-1
+1\n
 1000
 0
 
@@ -48,7 +49,7 @@ input:
 1
 10000
 output:
-1
+1\n
 10000
 0
 
