f54cad300cd673cf99e759cd56dc9d3d6d39d6d5
beep boop <githubbot@vadim.me>
2023-10-20 13:14:49 +0200

    Solution 3 of bowling, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/js6eizju Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -9,6 +9,7 @@
 #include <set>
 #include <stack>
 #include <climits>
+#include <sstream>
 using namespace std;
 /*
 Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.
@@ -34,63 +35,46 @@ input:
 output:
 100
 */
-int cal(const char *s, int len) {
-    int ans = 0;
-    for(int i = 0; i < len; i++) {
-        if(s[i] == '/') {
-            ans += 10;
-            if(i > 0) {
-                ans += s[i - 1] - '0';
+int get_score(string s) {
+    int score = 0;
+    int i = 0;
+    while (i < s.length()) {
+        if (s[i] == 'X') {
+            score += 10;
+            if (s[i+1] == 'X') {
+                score += 10;
+                if (i+2 < s.length() && s[i+2] == 'X') {
+                    score += 10;
+                } else if (i+2 < s.length()) {
+                    score += s[i+2] - '0';
+                }
+            } else if (i+2 < s.length() && s[i+2] == '/') {
+                score += 10;
+            } else if (i+2 < s.length()) {
+                score += s[i+1] - '0';
+                score += s[i+2] - '0';
             }
-            if(i < len - 1) {
-                ans += s[i + 1] - '0';
+            i++;
+        } else if (s[i] == '/') {
+            score += 10;
+            if (i+1 < s.length() && s[i+1] == 'X') {
+                score += 10;
+            } else if (i+1 < s.length()) {
+                score += s[i+1] - '0';
             }
-        } else if(s[i] == 'X') {
-            ans += 10;
-            if(i < len - 1) {
-                ans += s[i + 1] - '0';
-            }
-            if(i < len - 2) {
-                ans += s[i + 2] - '0';
-            }
-        } else {
-            ans += s[i] - '0';
+            score -= s[i-1] - '0';
+            i++;
+        } else if (s[i] != '-') {
+            score += s[i] - '0';
         }
+        i++;
     }
-    return ans;
-}
-int solve(const char *s) {
-    int ans = 0, len = strlen(s);
-    for(int i = 0; i < len;) {
-        if(s[i] == 'X') {
-            ans += 10;
-            if(i < len - 1) {
-                ans += s[i + 1] - '0';
-            }
-            if(i < len - 2) {
-                ans += s[i + 2] - '0';
-            }
-            i++;
-        } else if(s[i] == '/') {
-            ans += 10;
-            if(i > 0) {
-                ans += s[i - 1] - '0';
-            }
-            if(i < len - 1) {
-                ans += s[i + 1] - '0';
-            }
-            i++;
-        } else {
-            ans += s[i] - '0';
-            i++;
-        }
-    }
-    return ans;
+    return score;
 }
 int main() {
-    char s[100];
-    while(scanf("%s", s) != EOF) {
-        printf("%d\n", solve(s));
-    }
+    cout << get_score("XXXXXXXXXXXX") << endl;
+    cout << get_score("5/5/5/5/5/5/5/5/5/5/5") << endl;
+    cout << get_score("7115XXX548/279-X53") << endl;
+    cout << get_score("532/4362X179-41447/5") << endl;
     return 0;
 }
