6d5866414629075c6ed316fa9d6e4f6a80f84e04
beep boop <githubbot@vadim.me>
2023-10-20 14:34:21 +0200

    Solution 3 of cut-vector, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/jkjtcbny Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,95 +1,29 @@
-#include <vector>
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <queue>
-#include <stdio.h>
-#include <math.h>
-#include <map>
-#include <set>
-#include <stack>
-#include <climits>
-using namespace std;
-/*
-Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.
-For example,
-input:
-1
-0
-output:
-1
-0
-0
+#include <bits/stdc++.h>
 
-input:
-1
-10
-output:
-1
-10
-0
-
-input:
-1
-100
-output:
-1
-100
-0
-
-input:
-1
-1000
-output:
-1
-1000
-0
-
-input:
-1
-10000
-output:
-1
-10000
-0
-
-*/
-int main() {
+int main()
+{
     int n;
-    while(cin>>n) {
-        vector<int> v;
-        for(int i = 0; i < n; i++) {
-            int x;
-            cin>>x;
-            v.push_back(x);
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    int min = INT_MAX;
+    int min_index = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (abs(v[i] - v[i + 1]) < min)
+        {
+            min = abs(v[i] - v[i + 1]);
+            min_index = i;
         }
-        int l = 0, r = 0;
-        int res = INT_MAX;
-        int left = 0, right = 0;
-        for(int i = 1; i <= n; i++) {
-            r += v[i-1];
-            l = r;
-            for(int j = i-1; j > 0; j--) {
-                l -= v[j-1];
-                if(abs(l-r) < res) {
-                    res = abs(l-r);
-                    left = j;
-                    right = i;
-                }
-            }
-        }
-        for(int i = 0; i < left; i++) {
-            cout<<v[i]<<" ";
-        }
-        cout<<endl;
-        for(int i = left; i < right; i++) {
-            cout<<v[i]<<" ";
-        }
-        cout<<endl;
-        for(int i = right; i < n; i++) {
-            cout<<v[i]<<" ";
-        }
-        cout<<endl;
     }
+    cout << min_index + 1 << endl;
+    for (int i = 0; i <= min_index; i++)
+        cout << v[i] << " ";
+    cout << endl;
+    cout << n - min_index - 1 << endl;
+    for (int i = min_index + 1; i < n; i++)
+        cout << v[i] << " ";
+    cout << endl;
     return 0;
 }
