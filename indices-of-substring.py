d1e8800e007f6185ba1dbc4116fc10d78439a966
beep boop <githubbot@vadim.me>
2023-10-20 17:50:39 +0200

    Solution 4 of indices-of-substring, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/vla9zmxx Prompt: Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -42,8 +42,10 @@ output:
 0 1 2 3 4 5 6 7 8 9 10 11
 """
 if __name__ == '__main__':
-	a = raw_input()
-	b = raw_input()
-	for i in range(len(a)):
-		if a[i:i+len(b)] == b:
-			print i,
+    text = input()
+    target = input()
+    result = []
+    for i in range(len(text)):
+        if text[i:i+len(target)] == target:
+            result.append(i)
+    print(*result)
