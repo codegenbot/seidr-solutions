bf6b3d260f91216ae7c44f71a419e0ace3b15c0c
beep boop <githubbot@vadim.me>
2023-10-20 18:40:22 +0200

    Solution 9 of coin-sums, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/ztmk829g Prompt: Given a number of cents, ﬁnd the fewest number of US coins (pennies, nickles, dimes, quarters) needed to make that amount, and return the number of each type of coin as a separate output.

--- coin-sums.cpp
+++ coin-sums.cpp
@@ -52,45 +52,17 @@ output:
 int main() {
     int cents;
     cin >> cents;
-    int count[4] = {0};
-    count[0] = cents;
-    int c = 0;
-    while (c < 4) {
-        if (count[c] >= 25) {
-            count[3] += count[c] / 25;
-            count[c] = count[c] % 25;
-        }
-        c++;
-    }
-    c = 0;
-    while (c < 3) {
-        if (count[c] >= 10) {
-            count[2] += count[c] / 10;
-            count[c] = count[c] % 10;
-        }
-        c++;
-    }
-    c = 0;
-    while (c < 2) {
-        if (count[c] >= 5) {
-            count[1] += count[c] / 5;
-            count[c] = count[c] % 5;
-        }
-        c++;
-    }
-    c = 0;
-    while (c < 1) {
-        if (count[c] >= 1) {
-            count[0] += count[c] / 1;
-            count[c] = count[c] % 1;
-        }
-        c++;
-    }
-    for (int i = 0; i < 4; i++) {
-        cout << count[i];
-        if (i != 3) {
-            cout << " ";
-        }
-    }
+    cout << cents << endl;
+    int quarter = cents / 25;
+    cents = cents % 25;
+    cout << quarter << endl;
+    int dime = cents / 10;
+    cents = cents % 10;
+    cout << dime << endl;
+    int nickle = cents / 5;
+    cents = cents % 5;
+    cout << nickle << endl;
+    int penny = cents;
+    cout << penny << endl;
     return 0;
 }
