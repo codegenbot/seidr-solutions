a45bd0b6c5b33fe03b48813d3b6eb99200f6aae4
beep boop <githubbot@vadim.me>
2023-10-20 12:23:44 +0200

    Solution 40 of bouncing-balls, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/ir0gpxpl Prompt: Fix Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e471deeb-ad2f-4c5e-a269-b4fc667a1 b83.py", line 53, in <module>     b = float(line[1])               ~~~~^^^ IndexError: list index out of range

--- bouncing-balls.py
+++ bouncing-balls.py
@@ -1,10 +1,14 @@
+import os
+import sys
 import numpy as np
-
-
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
 """
-Given a starting height and a height after the ﬁrst bounce of a dropped ball, calculate the bounciness index
-(height of ﬁrst bounce / starting height). Then, given a number of bounces, use the bounciness index to calculate the
-total distance that the ball travels across those bounces.
+Given a starting height and a height after the ﬁrst bounce of a dropped ball, calculate the bounciness index (height of ﬁrst bounce / starting height). Then, given a number of bounces, use the bounciness index to calculate the total distance that the ball travels across those bounces.
 For example,
 input:
 1.001
@@ -37,16 +41,25 @@ input:
 output:
 3.963
 """
-
-
-def bounciness(h1, h2, n):
-    b = h2 / h1
-    return (h1 + h2) * (1 - b ** n) / (1 - b)
-
-
 if __name__ == '__main__':
-    print(bounciness(1.001, 1.0, 1))
-    print(bounciness(100.0, 99.999, 20))
-    print(bounciness(100.0, 1.0, 20))
-    print(bounciness(15.319, 5.635, 1))
-    print(bounciness(2.176, 1.787, 1))
+    while True:
+        try:
+            line = input()
+            if line is None or line == "":
+                break
+            else:
+                line = line.split(' ')
+                h = float(line[0])
+                b = float(line[1])
+                if len(line)>2:
+                    n = int(line[2])
+                else:
+                    n = 1
+                bounciness = b/h
+                distance = h
+                for i in range(n):
+                    distance += h*bounciness
+                    h = h*bounciness
+                print(distance)
+        except EOFError:
+            break
