9c81e474cbe22cf5bb885865e4350ea6a36155d3
beep boop <githubbot@vadim.me>
2023-10-21 07:42:59 +0200

    Solution 48 of substitution-cipher, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/z9vpasvo Prompt: Make sure that \n\n ->

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -8,9 +8,38 @@ import itertools
 import queue
 import re
 """
-The first line contains an integer, .
-The second line contains a double, .
-The third line contains a string, .
+Task
+Read two integers from STDIN and print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+Input Format
+
+The first line contains the first integer, a. The second line contains the second integer, b.
+
+Constraints
+
+1 <= a <= 10^10
+1 <= b <= 10^10
+Output Format
+
+Print the three lines as explained above.
+
+Sample Input 0
+
+3
+2
+Sample Output 0
+
+5
+1
+6
+Explanation 0
+
+3 + 2 => 5
+3 - 2 => 1
+3 * 2 => 6
 """
 if __name__ == '__main__':
     s = input()
