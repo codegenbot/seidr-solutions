c0f5e3b43aafc7d5f9f4929ebdc9f7035697b149
beep boop <githubbot@vadim.me>
2023-10-21 06:37:49 +0200

    Solution 63 of spin-words, ratio 0.01 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/qdsbglfk Prompt: Fix Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d441da4c-1a45-4e3f-b00b-ef709a675 ae7.py", line 42, in <module>     print(reverse(sys.argv[1]))                   ~~~~~~~~^^^ IndexError: list index out of range

--- spin-words.py
+++ spin-words.py
@@ -39,7 +39,7 @@ def reverse(s):
     return ' '.join(s)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python3 reverse.py 'string'")
-        sys.exit(1)
-    print(reverse(sys.argv[1]))
+    if len(sys.argv) > 1:
+        print(reverse(sys.argv[1]))
+    else:
+        print(reverse('this is a test'))
