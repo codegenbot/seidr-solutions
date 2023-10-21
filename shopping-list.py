28a302a84795e06b92808469362e92da0a1b4b44
beep boop <githubbot@vadim.me>
2023-10-21 02:36:15 +0200

    Solution 33 of shopping-list, ratio 0.2 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/mgkxah6m Prompt: Make sure that 20\n5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73\n20\n59.19 91.24 25.93 16.18 24.65 61.96 67.91 43.87 36.23 34.3 96.27 69.25 73.78 0.52 8.91 39.18 79.67 64.22 14.15 52.44 -> 59.6

--- shopping-list.py
+++ shopping-list.py
@@ -47,25 +47,11 @@ input:
 5.0 10.0 95.0
 output:
 29.0
-
-Given a vector of ﬂoats representing the prices of various shopping goods and another vector of floats representing the
-percent discount of each of those goods, return the total price of the shopping trip after applying the discount to each
-item.
-For example,
-input:
-20
-5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73
-20
-59.19 91.24 25.93 16.18 24.65 61.96 67.91 43.87 36.23 34.3 96.27 69.25 73.78 0.52 8.91 39.18 79.67 64.22 14.15 52.44
-output:
-59.6
 """
 if __name__ == '__main__':
     num = int(input())
     prices = [float(x) for x in input().split()]
     num = int(input())
     discounts = [float(x) for x in input().split()]
-    total = 0.0
-    for i in range(num):
-        total += prices[i] * (100 - discounts[i]) / 100
-    print("{:.1f}".format(total))
+    total = sum([x * (100-y)/100 for x, y in zip(prices, discounts)])
+    print(round(total, 1))
