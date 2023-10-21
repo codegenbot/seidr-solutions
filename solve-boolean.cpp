95be878b5e848a2ba999201de969316adf07964d
beep boop <githubbot@vadim.me>
2023-10-21 04:03:25 +0200

    Solution 17 of solve-boolean, ratio 0.78 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/pvcit2fc Prompt: Make sure that f&t -> False

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -43,32 +43,32 @@ int main() {
         if (c == '&' || c == '|') {
             stk.push(c);
         } else if (c == 't') {
-            if (stk.empty() || stk.top() == '|') {
+            if (stk.empty()) {
                 stk.push('t');
-                continue;
-            }
-            char op = stk.top();
-            stk.pop();
-            if (op == '&') {
-                stk.push(c);
-            } else { // op == '|'
-                stk.push(stk.top() == 't' ? 't' : 'f');
+            } else {
+                char op = stk.top();
                 stk.pop();
-                stk.push(c);
+                if (op == '&') {
+                    stk.push(c);
+                } else {
+                    stk.push(stk.top() == 'f' ? 'f' : 't');
+                    stk.pop();
+                    stk.push(c);
+                }
             }
         } else {
-            if (stk.empty() || stk.top() == '|') {
+            if (stk.empty()) {
                 stk.push('f');
-                continue;
-            }
-            char op = stk.top();
-            stk.pop();
-            if (op == '&') {
-                stk.push(stk.top() == 'f' ? 'f' : 't');
+            } else {
+                char op = stk.top();
                 stk.pop();
-                stk.push('f');
-            } else { // op == '|'
-                stk.push('f');
+                if (op == '&') {
+                    stk.push(stk.top() == 'f' ? 'f' : 't');
+                    stk.pop();
+                    stk.push('f');
+                } else {
+                    stk.push('f');
+                }
             }
         }
     }
