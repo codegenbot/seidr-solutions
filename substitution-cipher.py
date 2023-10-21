2afd320627c4924a17ddaf9518895b5f639241a9
beep boop <githubbot@vadim.me>
2023-10-21 07:35:46 +0200

    Solution 44 of substitution-cipher, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/z9vpasvo Prompt: Make sure that \n\n ->

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -8,9 +8,11 @@ import itertools
 import queue
 import re
 """
-The input will consist of a single integer. Your task is to find the sum of all the digits in the integer.
-For example, if the input is 12345, the output should be 15.
+You are given a string S.
+Your task is to print all possible combinations, up to size k, of the string in lexicographic sorted order.
 """
 if __name__ == '__main__':
-    s = input()
-    print(sum(map(int, list(s))))
+    s, k = input().split()
+    for i in range(1, int(k) + 1):
+        for j in itertools.combinations(sorted(s), i):
+            print(''.join(j))
