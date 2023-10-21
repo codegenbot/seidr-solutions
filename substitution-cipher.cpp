c1b1403d0b881116edd506da4d464dcc5a37b5cd
beep boop <githubbot@vadim.me>
2023-10-21 11:32:14 +0200

    Solution 85 of substitution-cipher, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/o884uipo Prompt: Make sure that \n\n ->

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -15,18 +15,37 @@ This problem gives 3 strings. The first two represent a cipher, mapping each cha
 For example,
 input:
 
-a
-b
-a
+abc
+def
+abd
+
 output:
-b
+dee
+
 input:
 a
-b
 a
-b
-b
-b
+a
+output:
+a
+input:
+j
+h
+j
+output:
+h
+input:
+a
+z
+a
+output:
+z
+input:
+e
+l
+eeeeeeeeee
+output:
+llllllllll
 */
 int main() {
     int t;
