835b537d63258113cbf1172d7ee9e80819893e85
beep boop <githubbot@vadim.me>
2023-10-21 02:33:03 +0200

    Solution 31 of shopping-list, ratio 0.07 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/mgkxah6m Prompt: Make sure that 20\n5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73\n20\n59.19 91.24 25.93 16.18 24.65 61.96 67.91 43.87 36.23 34.3 96.27 69.25 73.78 0.52 8.91 39.18 79.67 64.22 14.15 52.44 -> 59.6

--- shopping-list.py
+++ shopping-list.py
@@ -8,7 +8,14 @@ import itertools
 import queue
 import re
 """
-Given a vector of ﬂoats representing the prices of various shopping goods and another vector of floats representing the percent discount of each of those goods, return the total price of the shopping trip after applying the discount to each item.For example,input:1 50.01 100.0output:0.0input:1 50.01 10.0output:45.0input:2 20.0 20.02 100.0 50.0output:10.0input:2 20.0 20.02 20.0 0.0output:36.0input:3 10.0 20.0 30.03 5.0 10.0 95.0output:29.0
+Given a vector of ﬂoats representing the prices of various shopping goods and another vector of floats representing the percent discount of each of those goods, return the total price of the shopping trip after applying the discount to each item. For example,
+input:
+20
+5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73 5.73
+20
+59.19 91.24 25.93 16.18 24.65 61.96 67.91 43.87 36.23 34.3 96.27 69.25 73.78 0.52 8.91 39.18 79.67 64.22 14.15 52.44
+output:
+59.6
 """
 if __name__ == '__main__':
     num = int(input())
@@ -17,5 +24,5 @@ if __name__ == '__main__':
     discounts = [float(x) for x in input().split()]
     total = 0.0
     for i in range(num):
-        total += prices[i] * (100-discounts[i])/100
-    print("%.1f"%total)
+        total += prices[i] * (100-discounts[i])/100.0
+    print(total)
