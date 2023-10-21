aab921da5ca11df8315ec9d36ece781f0e2566fd
beep boop <githubbot@vadim.me>
2023-10-21 04:34:16 +0200

    Solution 44 of solve-boolean, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/pvcit2fc Prompt: Fix /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,6 +1,54 @@
-#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 
-int main() {
-	printf("Hello, world!");
-	return 0;
+using namespace std;
+
+int main()
+{
+    string filename;
+    cout << "Enter filename: ";
+    cin >> filename;
+
+    ifstream inFile;
+    inFile.open(filename);
+
+    if (!inFile.is_open())
+    {
+        cout << "Could not open file: " << filename << endl;
+        cout << "Program terminating.\n";
+        exit(EXIT_FAILURE);
+    }
+
+    double value;
+    double sum = 0.0;
+    int count = 0;
+
+    inFile >> value;
+    while (inFile.good())
+    {
+        ++count;
+        sum += value;
+        inFile >> value;
+    }
+
+    if (inFile.eof())
+        cout << "End of file reached.\n";
+    else if (inFile.fail())
+        cout << "Input terminated by data mismatch.\n";
+    else
+        cout << "Input terminated for unknown reason.\n";
+
+    if (count == 0)
+        cout << "No data processed.\n";
+    else
+    {
+        cout << "Items read: " << count << endl;
+        cout << "Sum: " << sum << endl;
+        cout << "Average: " << sum / count << endl;
+    }
+
+    inFile.close();
+
+    return 0;
 }
