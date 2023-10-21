95833231e0713e68a3a19a89955685ea9372ef10
beep boop <githubbot@vadim.me>
2023-10-21 06:28:41 +0200

    Solution 5 of substitution-cipher, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/z9vpasvo Prompt: This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -41,13 +41,5 @@ eeeeeeeeee
 output:
 llllllllll
 """
-
-
-def decode(cipher, message):
-    decoded = ''
-    for char in message:
-        decoded += cipher[char]
-    return decoded
-
-
 if __name__ == '__main__':
+    pass
