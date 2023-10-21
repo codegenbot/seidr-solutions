730c45ea78886d8da3d7e7e677c2f01e6587d27b
beep boop <githubbot@vadim.me>
2023-10-21 11:26:03 +0200

    Solution 80 of substitution-cipher, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/o884uipo Prompt: Make sure that \n\n ->

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -12,67 +12,49 @@
 using namespace std;
 /*
 This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.
-
 For example,
-
 input:
-
-
-
-
 
 output:
 
-
 input:
-
 a
-
 a
-
 a
-
 output:
-
 a
-
 input:
-
 j
-
 h
-
 j
-
 output:
-
 h
-
 input:
-
 a
-
 z
-
 a
-
 output:
-
 z
-
 input:
-
 e
-
 l
-
 eeeeeeeeee
-
 output:
-
 llllllllll
-
 */
-
 int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        string s1,s2,s3;
+        cin>>s1>>s2>>s3;
+        map<char,char>m;
+        for(int i=0;i<s1.length();i++){
+            m[s1[i]]=s2[i];
+        }
+        for(int i=0;i<s3.length();i++){
+            s3[i]=m[s3[i]];
+        }
+        cout<<s3<<endl;
+    }
 }
