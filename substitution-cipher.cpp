415021f38a4b27afaaca32ade76d75a236854d84
beep boop <githubbot@vadim.me>
2023-10-21 09:40:19 +0200

    Solution 39 of substitution-cipher, ratio 0.92 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/o884uipo Prompt: Make sure that \n\n ->

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -11,14 +11,52 @@
 #include <climits>
 using namespace std;
 /*
-Given a string, return a string where for every char in the original, there are two chars.
-doubleChar("The") → "TThhee"
-doubleChar("AAbb") → "AAAAbbbb"
-doubleChar("Hi-There") → "HHii--TThheerree"
+This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.
+For example,
+input:
+a
+b
+ab
+output:
+b
+input:
+a
+a
+a
+output:
+a
+input:
+j
+h
+j
+output:
+h
+input:
+a
+z
+a
+output:
+z
+input:
+e
+l
+eeeeeeeeee
+output:
+llllllllll
 */
 int main() {
-    string s;
-    cin >> s;
-    for (int i = 0; i < s.size(); i++) cout << s[i] << s[i];
-    cout << endl;
+    string str1, str2, str3;
+    cin >> str1 >> str2 >> str3;
+    map<char, char> mp;
+    for (int i = 0; i < str1.size(); i++) {
+        mp[str1[i]] = str2[i];
+    }
+    
+    for (int i = 0; i < str3.size(); i++) {
+        if (mp.count(str3[i])) {
+            str3[i] = mp[str3[i]];
+        }
+    }
+    cout << str3 << endl;
+    return 0;
 }
