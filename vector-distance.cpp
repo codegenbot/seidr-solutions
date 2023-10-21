2987fa387c2bea48afa3da2fff9e66ef28861d4f
beep boop <githubbot@vadim.me>
2023-10-21 08:26:53 +0200

    Solution 39 of vector-distance, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/wii6grug Prompt: Make sure that 20\n-100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0\n20\n100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 -> 894.4271909999159

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,33 +1,54 @@
-#include <vector>
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <queue>
-#include <stdio.h>
-#include <math.h>
-#include <map>
-#include <set>
-#include <stack>
-#include <climits>
+#include <bits/stdc++.h>
 using namespace std;
-/*
 
-*/
+#define int long long
+#define double long double
+#define ff first
+#define ss second
+#define endl '\n'
+#define ii pair<int, int>
+#define mp make_pair
+#define mt make_tuple
+#define DESYNC                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0)
+#define pb push_back
+#define vi vector<int>
+#define vii vector<ii>
+#define all(x) x.begin(), x.end()
+#define EPS 1e-9
+#define INF 1e18
+#define ROOT 1
+#define M 1000000007
+#define curtime chrono::steady_clock::now().time_since_epoch().count
+const double PI = acos(-1);
+
+inline int mod(int n, int m) {
+  int ret = n % m;
+  if (ret < 0) ret += m;
+  return ret;
+}
+
+int gcd(int a, int b) {
+  if (a == 0)
+    return b;
+  else
+    return gcd(b % a, a);
+}
+
 int main() {
-    int n;
-    cin >> n;
-    vector<float> a(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    cin >> n;
-    vector<float> b(n);
-    for (int i = 0; i < n; i++) {
-        cin >> b[i];
-    }
-    float sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += (a[i] - b[i]) * (a[i] - b[i]);
-    }
-    cout << sqrt(sum) << endl;
+  DESYNC;
+  int n;
+  cin >> n;
+  vector<double> a(n);
+  for (int i = 0; i < n; i++) cin >> a[i];
+  cin >> n;
+  vector<double> b(n);
+  for (int i = 0; i < n; i++) cin >> b[i];
+  double ans = 0;
+  for (int i = 0; i < n; i++) {
+    ans += (a[i] - b[i]) * (a[i] - b[i]);
+  }
+  cout << fixed << setprecision(10) << sqrt(ans) << endl;
 }
