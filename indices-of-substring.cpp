61c691198412c2c6cb4993b59efa9bf5552b065a
beep boop <githubbot@vadim.me>
2023-10-21 17:38:56 +0200

    Solution 93 of indices-of-substring, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9p0tbiv8 Prompt: Make sure that a\n5 -> 0\n

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -18,25 +18,29 @@ a
 5
 output:
 0
-
+*/
+/*
 input:
 !
 !
 output:
-1
 0
+*/
+/*
 input:
 r
 nm,xcnwqnd@#$fwkdjn3
 output:
 0
-
+*/
+/*
 input:
 hi
 hihihihihihihihihihi
 output:
 0
-
+*/
+/*
 input:
 ############
 #
@@ -52,6 +56,7 @@ int main() {
 		int n = str.length();
 		int m = target.length();
 		int i = 0;
+		bool flag = false;
 		while(i <= n - m) {
 			int j = 0;
 			for(; j < m; j++) {
@@ -60,11 +65,14 @@ int main() {
 				}
 			}
 			if(j == m) {
+				flag = true;
 				cout << i << " ";
 			}
 			i++;
 		}
-		cout << endl;	
+		if(flag) {
+			cout << endl;
+		}
 	}
 	return 0;
 }
