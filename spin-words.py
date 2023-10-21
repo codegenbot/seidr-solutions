6898afe387b7e3c2244d5da2065eed2d135c9644
beep boop <githubbot@vadim.me>
2023-10-21 05:28:31 +0200

    Solution 23 of spin-words, ratio 0.99 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/qdsbglfk Prompt: Make sure that  ->

--- spin-words.py
+++ spin-words.py
@@ -7,6 +7,31 @@ import collections
 import itertools
 import queue
 import re
+"""
+Given a string of one or more words (separated by spaces), 
+reverse all of the words that are ﬁve or more letters long and return the resulting string.
+For example,
+input:
+
+output:
+
+input:
+a
+output:
+a
+input:
+this is a test
+output:
+this is a test
+input:
+this is another test
+output:
+this is rehtona test
+input:
+hi
+output:
+hi
+"""
 if __name__ == '__main__':
     l = input()
     l = l.split(' ')
