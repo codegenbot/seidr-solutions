67820e3eaeba2551c0bbc5ad5e2c410b95dcceb7
beep boop <githubbot@vadim.me>
2023-10-21 09:01:29 +0200

    Solution 25 of spin-words, ratio 0.01 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/syo3ulyo Prompt: Make sure that  ->

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,57 +1,65 @@
-#include <iostream> 
-#include <string> 
-#include <vector> 
-using namespace std; 
-/* 
-Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string. 
-For example, 
-input: 
- 
-output: 
- 
-input: 
-a 
-output: 
-a 
-input: 
-this is a test 
-output: 
-this is a test 
-input: 
-this is another test 
-output: 
-this is rehtona test 
-input: 
-hi 
-output: 
-hi 
-*/ 
-void reverseStr(string& str, int start, int end) { 
-    while(start < end) { 
-        char temp = str[start]; 
-        str[start] = str[end]; 
-        str[end] = temp; 
-        start++; 
-        end--; 
-    } 
-} 
- 
-string reverseWords(string str) { 
-    int start = 0, end = 0; 
-    while(start < str.size()) { 
-        while(end < str.size() && str[end] != ' ') { 
-            end++; 
-        } 
-        if(end - start >= 5) { 
-            reverseStr(str, start, end - 1); 
-        } 
-        start = end + 1; 
-        end = start; 
-    } 
-    return str; 
-} 
-int main() { 
-    string str = "this is a test"; 
-    cout << reverseWords(str) << endl; 
-    return 0; 
-} 
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
+/*
+Given a string of one or more words (separated by spaces), reverse all of the words that are five or more letters long and return the resulting string.
+For example,
+input:
+
+output:
+
+input:
+a
+output:
+a
+input:
+this is a test
+output:
+this is a test
+input:
+this is another test
+output:
+this is rehtona test
+input:
+hi
+output:
+hi
+*/
+void reverseStr(string& str, int start, int end) {
+    while(start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+string reverseWords(string str) {
+    int start = 0, end = 0;
+    while(start < str.size()) {
+        while(end < str.size() && str[end] != ' ') {
+            end++;
+        }
+        if(end - start >= 5) {
+            reverseStr(str, start, end - 1);
+        }
+        start = end + 1;
+        end = start;
+    }
+    return str;
+}
+int main() {
+    string str = "this is a test";
+    cout << reverseWords(str) << endl;
+    return 0;
+}
