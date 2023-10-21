0747d407ec067df1208f322f4bc456ba31aa4585
beep boop <githubbot@vadim.me>
2023-10-21 02:25:36 +0200

    Solution 3 of snow-day, ratio 0.13 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/1vw1ls3i Prompt: Given an integer representing a number of hours and 3 floats representing how much snow is on theground, the rate of snow fall, and the proportion of snow melting per hour, return the amount of snow on the ground after the amount of hours given. Each hour is considered a discrete event of adding snow and then melting, not a continuous process.

--- snow-day.py
+++ snow-day.py
@@ -48,12 +48,10 @@ output:
 """
 if __name__ == '__main__':
     hours = int(input())
-    snow = float(input())
-    rate = float(input())
-    melt = float(input())
-
+    snow_on_ground = float(input())
+    snow_fall_rate = float(input())
+    snow_melt_rate = float(input())
     for i in range(hours):
-        snow += rate
-        snow -= melt
-
-    print(snow)
+        snow_on_ground += snow_fall_rate
+        snow_on_ground -= snow_melt_rate*snow_on_ground
+    print(snow_on_ground)
