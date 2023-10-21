77522a224af777edd2254730f7eae88067344037
beep boop <githubbot@vadim.me>
2023-10-21 08:52:23 +0200

    Solution 97 of substitution-cipher, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/z9vpasvo Prompt: Make sure that \n\n ->

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -8,38 +8,15 @@ import itertools
 import queue
 import re
 """
-This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.
-For example,
+This problem gives a string. The program must return the string with the first character of each word capitalized.
+For example, input:
+this is a test
+output:
+This Is A Test
 input:
-
-
-
+hello world
 output:
-
-input: 
-a
-a
-a
-output:
-a
-input:
-j
-h
-j
-output:
-h
-input:
-a
-z
-a
-output:
-z
-input:
-e
-l
-eeeeeeeeee
-output:
-llllllllll
+Hello World
 """
 if __name__ == '__main__':
     pass
