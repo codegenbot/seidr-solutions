c54c80fc4d93689573f9dbd79a1d88793bcb58bc
beep boop <githubbot@vadim.me>
2023-10-21 04:21:33 +0200

    Solution 25 of snow-day, ratio 0.01 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/336ivv0p Prompt: Make sure that 0\n0.0\n0.0\n0.0 -> 0.0

--- snow-day.cpp
+++ snow-day.cpp
@@ -60,12 +60,7 @@ int main() {
 	for (int i = 0; i < hours; i++) {
 		newSnow += rate;
 		snow += newSnow;
-		if (melt != 0) {
-			snow -= melt;
-		}
-		if (snow < 0) {
-			snow = 0;
-		}
+		snow -= melt;
 	}
 	cout << snow << endl;
 }
