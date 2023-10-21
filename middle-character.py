89eeadb3b3e98a11e35635c67f3fda5b16a328ee
beep boop <githubbot@vadim.me>
2023-10-21 09:21:54 +0200

    Solution 92 of middle-character, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/uoytypoh Prompt: Fix   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e9a7f3ba-1feb-4dfc-9b3d-596aad1f1 b95.py", line 34     if __name__ == '__main__': IndentationError: expected an indented block after 'if' statement on line 34

--- middle-character.py
+++ middle-character.py
@@ -10,26 +10,15 @@ import re
 """
 Given a string, return the middle character as a string if it is odd length; return the two middle characters as a string if it is even length.
 For example,
-input:
-Q
-output:
-Q
-input:
-QQ
-output:
- 
-input:
-$
-output:
-$
-input:
-E9
-output:
-E9
-input:
-)b
-output:
-)b
+input:Q
+output:Q
+input:QQ
+output: 
+input:$
+output:$
+input:E9
+output:E9
+input:)b
+output:)b
 """
 if __name__ == '__main__':
-    pass
