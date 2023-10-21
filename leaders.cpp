ce0e194577936f5676acaea17dbb9d075f0565e0
beep boop <githubbot@vadim.me>
2023-10-21 02:32:23 +0200

    Solution 60 of leaders, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/4sg0ax99 Prompt: Make sure that 0\n -> 0\n

--- leaders.cpp
+++ leaders.cpp
@@ -9,7 +9,6 @@
 #include <set>
 #include <stack>
 #include <climits>
-#include <algorithm>
 using namespace std;
 /*
 Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.
@@ -48,25 +47,24 @@ output:
 int main() {
     int n;
     cin >> n;
-    vector<int> nums(n, 0);
+    vector<int> v(n);
     for (int i = 0; i < n; i++) {
-        cin >> nums[i];
+        cin >> v[i];
     }
     vector<int> res;
-    int max_num = INT_MIN;
-    for (int i = n - 1; i >= 0; i--) {
-        if (nums[i] >= max_num) {
-            max_num = nums[i];
-            res.push_back(nums[i]);
+    int max = v[n - 1];
+    res.push_back(max);
+    for (int i = n - 2; i >= 0; i--) {
+        if (v[i] >= max) {
+            res.push_back(v[i]);
+            max = v[i];
         }
     }
-    reverse(res.begin(), res.end());
-    for (int i = 0; i < res.size(); i++) {
-        cout << res[i];
-        if (i != res.size() - 1) {
-            cout << " ";
-        }
+    for (int i = res.size() - 1; i >= 0; i--) {
+        printf("%d ", res[i]);
     }
-    cout << endl;
+    if (res.size() == 0) {
+        printf("0\n");
+    }
     return 0;
 }
