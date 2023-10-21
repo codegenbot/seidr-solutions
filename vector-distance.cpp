3ac0d2e73e53c7d54f250d790185783621d85a9e
beep boop <githubbot@vadim.me>
2023-10-21 07:12:32 +0200

    Solution 2 of vector-distance, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/wii6grug Prompt: Given two n-dimensional vectors of floats, return the Euclidean distance between the two vectors in n-dimensional space.

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -50,4 +50,21 @@ output:
 2.2715833329200144
 */
 int main() {
+    int n;
+    cin >> n;
+    vector<float> v1(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v1[i];
+    }
+    cin >> n;
+    vector<float> v2(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v2[i];
+    }
+    float dist = 0.0;
+    for (int i = 0; i < n; ++i) {
+        dist += (v1[i] - v2[i]) * (v1[i] - v2[i]);
+    }
+    printf("%.16f\n", sqrt(dist));
+    return 0;
 }
