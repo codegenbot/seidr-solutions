bf8e0d395179efdc56a606fcc1cd0ff1dcfb983f
beep boop <githubbot@vadim.me>
2023-10-21 04:07:02 +0200

    Solution 24 of solve-boolean, ratio 0.55 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/pvcit2fc Prompt: Make sure that t&f -> False

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -14,7 +14,7 @@ using namespace std;
 Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.
 For example,
 input:
-t
+t&f
 output:
 True
 input:
@@ -42,10 +42,10 @@ bool evaluate(string& s, int& i) {
         i += 2;
         return false;
     } else {
-        i++;
+        i += 2;
         bool left = evaluate(s, i);
         char op = s[i];
-        i++;
+        i += 2;
         bool right = evaluate(s, i);
         if (op == '&')
             return left && right;
