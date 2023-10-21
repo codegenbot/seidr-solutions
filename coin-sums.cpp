764dee71c3ecadd72537730ca120cf4fde01543d
beep boop <githubbot@vadim.me>
2023-10-22 01:09:28 +0200

    Solution 75 of coin-sums, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/ztmk829g Prompt: Make sure that 1 -> 1\n0\n0\n0

--- coin-sums.cpp
+++ coin-sums.cpp
@@ -53,13 +53,13 @@ int main() {
     int cents;
     while(cin >> cents) {
         int quarters = cents / 25;
-        int dimes = cents % 25 / 10;
-        int nickels = cents % 25 % 10 / 5;
-        int pennies = cents % 25 % 10 % 5;
-        cout << pennies << "\n";
-        cout << nickels << "\n";
-        cout << dimes << "\n";
-        cout << quarters << "\n";
+        int dimes = cents % 25 / 10; //Make sure that 1 -> 1\n0\n0\n0
+        int nickels = cents % 25 % 10 / 5; //Make sure that 1 -> 1\n0\n0\n0
+        int pennies = cents % 25 % 10 % 5; //Make sure that 1 -> 1\n0\n0\n0
+        cout << pennies << endl;
+        cout << nickels << endl;
+        cout << dimes << endl;
+        cout << quarters << endl;
     }
     return 0;
 }
