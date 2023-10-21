8de6cbf5db602d7c6e4e239bb29f437da79eb18d
beep boop <githubbot@vadim.me>
2023-10-21 08:17:48 +0200

    Solution 25 of vector-distance, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/wii6grug Prompt: Make sure that 20\n-100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0\n20\n100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 -> 894.4271909999159

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -49,26 +49,17 @@ input:
 output:
 2.2715833329200144
 */
-
-float euclidean_distance(vector<float> &v1, vector<float> &v2) {
-    float ans = 0.0;
-    for (int i = 0; i < v1.size(); i++) {
-        ans += (v1[i] - v2[i]) * (v1[i] - v2[i]);
-    }
-    return sqrt(ans);
-}
-
 int main() {
     int n;
     cin >> n;
-    vector<float> v1(n), v2(n);
+    vector<double> v1(n), v2(n);
     for (int i = 0; i < n; i++) {
         cin >> v1[i];
     }
     for (int i = 0; i < n; i++) {
         cin >> v2[i];
     }
-    float ans = 0.0;
+    double ans = 0.0;
     for (int i = 0; i < n; i++) {
         ans += (v1[i] - v2[i]) * (v1[i] - v2[i]);
     }
