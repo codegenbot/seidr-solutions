b5671259f7f6e803b4a8b1bdf5fd8dceaf5ac8cc
beep boop <githubbot@vadim.me>
2023-10-21 07:22:17 +0200

    Solution 36 of substitution-cipher, ratio 0.01 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/z9vpasvo Prompt: Make sure that \n\n ->

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -1,8 +1,1 @@
-"""
-This problem gives a string. The program must print all the possible substrings of the given string. For example, if the input is abc, the output should be: a ab abc b bc c If the input is xyz, the output should be: x xy xyz y yz z
-"""
-if __name__ == '__main__':
-    string = input()
-    for i in range(len(string)):
-        for j in range(i,len(string)):
-            print(string[i:j+1])
+print(input())
