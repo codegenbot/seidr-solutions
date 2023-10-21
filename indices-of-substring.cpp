17f610b611e8d1df7e17f38c0e5dd6844a5deb5f
beep boop <githubbot@vadim.me>
2023-10-21 15:06:57 +0200

    Solution 90 of indices-of-substring, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/9p0tbiv8 Prompt: Make sure that a\n5 -> 0\n

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -45,20 +45,25 @@ output:
 0 1 2 3 4 5 6 7 8 9 10 11
 */
 int main() {
-    string text, target;
-    while (cin >> text >> target) {
-        int len = target.size();
-        vector<int> res;
-        for (int i = 0; i < text.size(); i++) {
-            if (text.substr(i, len) == target) {
-                res.push_back(i);
-            }
-        }
-        for (int i = 0; i < res.size(); i++) {
-            if (i) cout << " ";
-            cout << res[i];
-        }
-        cout << (res.size() ? "" : "\n");
-    }
-    return 0;
+	string str;
+	string target;
+	while(getline(cin, str)) {
+		getline(cin, target);
+		int n = str.length();
+		int m = target.length();
+		int i = 0;
+		while(i <= n - m) {
+			int j = 0;
+			for(; j < m; j++) {
+				if(str[i + j] != target[j]) {
+					break;
+				}
+			}
+			if(j == m) {
+				cout << i << endl;
+			}
+			i++;
+		}
+	}
+	return 0;
 }
