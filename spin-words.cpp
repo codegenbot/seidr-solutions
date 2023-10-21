57f561f8313a4536f2ba2d75a673058d8ecce3e3
beep boop <githubbot@vadim.me>
2023-10-21 09:03:29 +0200

    Solution 29 of spin-words, ratio 0.01 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/syo3ulyo Prompt: Make sure that  ->

--- spin-words.cpp
+++ spin-words.cpp
@@ -11,7 +11,7 @@
 #include <climits>
 using namespace std;
 /*
-Given a string of one or more words (separated by spaces), reverse all of the words that are five or more letters long and return the resulting string.
+Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.
 For example,
 input:
 
@@ -58,6 +58,7 @@ string reverseWords(string str) {
     }
     return str;
 }
+
 int main() {
     string str = "this is a test";
     cout << reverseWords(str) << endl;
