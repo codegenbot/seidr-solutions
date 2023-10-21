41d0bd324a781d4aee8c8ad41076ad8e6fca8a03
beep boop <githubbot@vadim.me>
2023-10-21 07:44:55 +0200

    Solution 49 of substitution-cipher, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/z9vpasvo Prompt: Make sure that \n\n ->

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -1,45 +1,7 @@
-import os
-import sys
-import numpy as np
-import math
-import datetime
-import collections
-import itertools
-import queue
-import re
+import os, sys, numpy as np, math, datetime, collections, itertools, queue, re
 """
-Task
-Read two integers from STDIN and print three lines where:
-
-The first line contains the sum of the two numbers.
-The second line contains the difference of the two numbers (first - second).
-The third line contains the product of the two numbers.
-Input Format
-
-The first line contains the first integer, a. The second line contains the second integer, b.
-
-Constraints
-
-1 <= a <= 10^10
-1 <= b <= 10^10
-Output Format
-
-Print the three lines as explained above.
-
-Sample Input 0
-
-3
-2
-Sample Output 0
-
-5
-1
-6
-Explanation 0
-
-3 + 2 => 5
-3 - 2 => 1
-3 * 2 => 6
+The input will consist of a single integer. Your task is to find the sum of all the digits in the integer.
+For example, if the input is 12345, the output should be 15.
 """
 if __name__ == '__main__':
     s = input()
