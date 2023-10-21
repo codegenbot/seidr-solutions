cfa21963da70c6fe483a839c4e7e89dd6e2d983d
beep boop <githubbot@vadim.me>
2023-10-21 02:56:45 +0200

    Solution 22 of snow-day, ratio 0.75 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/1vw1ls3i Prompt: Make sure that 15\n15.0\n15.0\n0.15 -> 92.5748913763936

--- snow-day.py
+++ snow-day.py
@@ -52,9 +52,5 @@ if __name__ == '__main__':
     snow_fall_rate = float(input())
     snow_melt_rate = float(input())
     for i in range(hours):
-        if snow_on_ground > 0:
-            snow_on_ground += snow_fall_rate
-            snow_on_ground -= snow_melt_rate*snow_on_ground
-        else:
-            snow_on_ground += snow_fall_rate
+        snow_on_ground += snow_fall_rate - snow_melt_rate*snow_on_ground
     print(snow_on_ground)
