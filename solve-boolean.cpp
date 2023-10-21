5f9e95ef0cdfc7fae9fe139bc7f569fa9f4cde76
beep boop <githubbot@vadim.me>
2023-10-21 06:05:49 +0200

    Solution 90 of solve-boolean, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/pvcit2fc Prompt: Make sure that t -> True

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -34,11 +34,43 @@ t&f
 output:
 False
 */
-bool evaluate(string input) {
-  if (input == "t") {
-    return true;
-  }
-  return false;
+
+bool evaluate(string s) {
+    if (s.empty()) {
+        return false;
+    }
+    stack<char> operators;
+    stack<bool> operands;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == ' ') {
+            continue;
+        }
+        if (s[i] == 't') {
+            operands.push(true);
+        } else if (s[i] == 'f') {
+            operands.push(false);
+        } else if (s[i] == '|') {
+            operators.push('|');
+        } else if (s[i] == '&') {
+            operators.push('&');
+        } else if (s[i] == ')') {
+            bool op1 = operands.top();
+            operands.pop();
+            bool op2 = operands.top();
+            operands.pop();
+            char op = operators.top();
+            operators.pop();
+            if (op == '|') {
+                operands.push(op1 || op2);
+            } else if (op == '&') {
+                operands.push(op1 && op2);
+            }
+        }
+    }
+    return operands.top();
 }
 int main() {
+    string s = "t | f & t";
+    cout << evaluate(s) << endl;
+    return 0;
 }
