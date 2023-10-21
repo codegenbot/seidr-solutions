62fd6e4fcfb9846b06943ae1703e4cdbab5d1fe9
beep boop <githubbot@vadim.me>
2023-10-21 04:35:12 +0200

    Solution 46 of solve-boolean, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/pvcit2fc Prompt: Fix /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,44 +1,6 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-
+#include <stdio.h>
 int main()
 {
-    std::ofstream outFile;
-    std::ifstream inFile;
-    std::string fileName;
-    std::string line;
-
-    std::cout << "Enter the name of the file you would like to read from: ";
-    std::cin >> fileName;
-
-    inFile.open(fileName);
-
-    if (inFile.fail())
-    {
-        std::cout << "File did not open" << std::endl;
-        exit(1);
-    }
-
-    std::cout << "Enter the name of the file you would like to write to: ";
-    std::cin >> fileName;
-
-    outFile.open(fileName);
-
-    if (outFile.fail())
-    {
-        std::cout << "File did not open" << std::endl;
-        exit(1);
-    }
-
-    while (!inFile.eof())
-    {
-        std::getline(inFile, line);
-        outFile << line << std::endl;
-    }
-
-    inFile.close();
-    outFile.close();
-
-    return 0;
+printf("Hello, World!");
+return 0;
 }
