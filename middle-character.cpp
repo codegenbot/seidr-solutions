4b7c256ed61b83f9fd2af1442f72adcba8639dcc
beep boop <githubbot@vadim.me>
2023-10-21 18:31:09 +0200

    Solution 90 of middle-character, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/ovehaxgo Prompt: Fix 892782dd-9cde-4528-9131-579d66080703.cpp: In function ‘int main()’: 892782dd-9cde-4528-9131-579d66080703.cpp:38:13: error: expected ‘}’ at end of in put    38 | int main() {       |            ~^

--- middle-character.cpp
+++ middle-character.cpp
@@ -11,7 +11,8 @@
 #include <climits>
 using namespace std;
 /*
-Given a string, return the middle character as a string if it is odd length; return the two middle characters as a string if it is even length.
+Given a string, return the middle character as a string if it is odd length; 
+return the two middle characters as a string if it is even length.
 For example,
 input:
 Q
@@ -35,14 +36,4 @@ output:
 )b
 */
 int main() {
-    string input;
-    while (getline(cin, input)) {
-        int len = input.length();
-        if (len % 2 == 0) {
-            cout << input[len / 2 - 1] << input[len / 2] << endl;
-        } else {
-            cout << input[len / 2] << endl;
-        }
-    }
-    return 0;
 }
