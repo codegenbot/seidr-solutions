80b0f0abf5355342058e72d9ec79d58e66e06436
beep boop <githubbot@vadim.me>
2023-10-21 08:30:20 +0200

    Solution 44 of vector-distance, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/wii6grug Prompt: Make sure that 20\n-100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0 -100.0\n20\n100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 100.0 -> 894.4271909999159

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -54,17 +54,17 @@ int main() {
     cin >> n;
     vector<float> v1(n);
     for (int i = 0; i < n; ++i) {
-        cin >> v1[i];
+        scanf("%f", &v1[i]);
     }
     cin >> n;
     vector<float> v2(n);
     for (int i = 0; i < n; ++i) {
-        cin >> v2[i];
+        scanf("%f", &v2[i]);
     }
     float dist = 0.0;
     for (int i = 0; i < n; ++i) {
         dist += (v1[i] - v2[i]) * (v1[i] - v2[i]);
     }
-    printf("%.15f\n", sqrt(dist));
+    printf("%.16f\n", sqrt(dist));
     return 0;
 }
