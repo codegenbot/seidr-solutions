fa4ab67a1f0339a335c2ce6c65ce05b1493be957
beep boop <githubbot@vadim.me>
2023-10-21 02:40:51 +0200

    Solution 76 of leaders, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/4sg0ax99 Prompt: Make sure that 0\n -> 0\n

--- leaders.cpp
+++ leaders.cpp
@@ -2,35 +2,35 @@
 Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.
 For example,
 input:
-1
+0
 
 output:
-1
+0\n
 
 input:
 1
 0
 output:
 1
-0
+0\n
 input:
 1
 451
 output:
 1
-451
+451\n
 input:
 2
 1000 0
 output:
 2
-1000 0
+1000 0\n
 input:
 2
 0 1000
 output:
 1
-1000
+1000\n
 */
 #include <vector>
 #include <iostream>
