5a53e69805b5b8b4d1271363f7239fc4a2eea8b0
beep boop <githubbot@vadim.me>
2023-10-21 02:09:20 +0200

    Solution 17 of shopping-list, ratio 0.2 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/mgkxah6m Prompt: Make sure that 5\n25.43 43.22 23.42 42.09 25.7\n5\n0.0 0.0 0.0 0.0 0.0 -> 159.86

--- shopping-list.py
+++ shopping-list.py
@@ -8,29 +8,30 @@ import itertools
 import queue
 import re
 """
-Given a vector of ﬂoats representing the prices of various shopping goods and another vector of floats representing the percent discount of each of those goods, return the total price of the shopping trip after applying the discount to each item.
+Given a vector of ﬂoats representing the prices of various shopping goods and another vector of floats representing
+the percent discount of each of those goods, return the total price of the shopping trip after applying the discount to each item.
 For example,
 input:
-5
-25.43 43.22 23.42 42.09 25.7
-5
-0.0 0.0 0.0 0.0 0.0
+1
+50.0
+1
+100.0
 output:
-159.86
+0.0
 input:
-5
-25.43 43.22 23.42 42.09 25.7
-5
-10.0 10.0 10.0 10.0 10.0
+1
+50.0
+1
+10.0
 output:
-143.48
+45.0
 input:
-5
-25.43 43.22 23.42 42.09 25.7
-5
-50.0 50.0 50.0 50.0 50.0
+2
+20.0 20.0
+2
+100.0 50.0
 output:
-79.86
+10.0
 input:
 2
 20.0 20.0
