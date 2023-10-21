32e6cc685545c46c97c34e8e0dee50b4867a8858
beep boop <githubbot@vadim.me>
2023-10-21 08:26:12 +0200

    Solution 77 of substitution-cipher, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/z9vpasvo Prompt: Make sure that \n\n ->

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -8,8 +8,39 @@ import itertools
 import queue
 import re
 """
-This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message. For example, input:
-output: input: a a a output: a input: j h j output: h input: a z a output: z input: e l eeeeeeeeee output: llllllllll
+This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.
+For example,
+input:
+ab
+ba
+ab
+
+output:
+
+input:
+a
+a
+a
+output:
+a
+input:
+j
+h
+j
+output:
+h
+input:
+a
+z
+a
+output:
+z
+input:
+e
+l
+eeeeeeeeee
+output:
+llllllllll
 """
 if __name__ == '__main__':
     pass
