baee3e57f6ab012301da4356e6b24e8897a46529
beep boop <githubbot@vadim.me>
2023-10-21 05:24:32 +0200

    Solution 93 of solve-boolean, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/v3z06ogc Prompt: Fix Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dcc456fb-2228-4303-9021-5caf18d88 3ed.py", line 36, in <module>     print(eval(s))           ^^^^^^^   File "<string>", line 1, in <module> NameError: name 't' is not defined

--- solve-boolean.py
+++ solve-boolean.py
@@ -11,26 +11,26 @@ import re
 Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.
 For example,
 input:
-True
+t
 output:
 True
 input:
-False
+f
 output:
 False
 input:
-False&False
+f&f
 output:
 False
 input:
-False&True
+f&t
 output:
 False
 input:
-True&False
+t&f
 output:
 False
 """
 if __name__ == '__main__':
     s = input()
-    print(eval(s))
+    print(eval(s.lower()))
