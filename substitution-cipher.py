70a45228a71b1fc2b669b9ca6c4a1dd500f4711f
beep boop <githubbot@vadim.me>
2023-10-21 07:30:00 +0200

    Solution 41 of substitution-cipher, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/z9vpasvo Prompt: Make sure that \n\n ->

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -8,9 +8,8 @@ import itertools
 import queue
 import re
 """
-The input will consist of a single integer. Your task is to find the sum of all the digits in the integer.
-For example, if the input is 12345, the output should be 15.
+The input will consist of a single integer. Your task is to find the sum of all the digits in the integer. For example, if the input is 12345, the output should be 15.
 """
 if __name__ == '__main__':
     s = input()
-    print(sum(map(int, list(s)))) 
+    print(sum(map(int, list(s))))
