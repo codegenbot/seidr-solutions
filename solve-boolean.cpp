ce32d2d7faba966b5b2cd0305fcd385827430eba
beep boop <githubbot@vadim.me>
2023-10-21 03:05:21 +0200

    Solution 3 of solve-boolean, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/pvcit2fc Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,1 +1,62 @@
-
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
+/*
+Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.
+For example,
+input:
+t
+output:
+True
+input:
+f
+output:
+False
+input:
+f&f
+output:
+False
+input:
+f&t
+output:
+False
+input:
+t&f
+output:
+False
+*/
+int main() {
+  string s;
+  cin >> s;
+  stack<bool> b;
+  for (int i = 0; i < s.size(); ++i) {
+    if (s[i] == 'T')
+      b.push(true);
+    else if (s[i] == 'F')
+      b.push(false);
+    else if (s[i] == '&') {
+      bool a = b.top();
+      b.pop();
+      bool a1 = b.top();
+      b.pop();
+      b.push(a1 & a);
+    } else {
+      bool a = b.top();
+      b.pop();
+      bool a1 = b.top();
+      b.pop();
+      b.push(a1 | a);
+    }
+  }
+  cout << (b.top() ? "True" : "False") << endl;
+  return 0;
+}
