d7e120d3f5951a0949c454a66113438361619e6e
beep boop <githubbot@vadim.me>
2023-10-21 07:20:49 +0200

    Solution 34 of substitution-cipher, ratio 0.01 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/z9vpasvo Prompt: Make sure that \n\n ->

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -8,25 +8,10 @@ import itertools
 import queue
 import re
 """
-This problem gives a string. The program must print all the possible substrings of the given string.
-For example, if the input is abc, the output should be:
-a
-ab
-abc
-b
-bc
-c
-If the input is xyz, the output should be:
-x
-xy
-xyz
-y
-yz
-z
+This problem gives a string. The program must print all the possible substrings of the given string. For example, if the input is abc, the output should be: a ab abc b bc c If the input is xyz, the output should be: x xy xyz y yz z
 """
 if __name__ == '__main__':
     string = input()
     for i in range(len(string)):
         for j in range(i,len(string)):
             print(string[i:j+1])
-            
