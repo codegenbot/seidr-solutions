a1bf8b87b9b31785ec207c1a12ebb405c805e2df
beep boop <githubbot@vadim.me>
2023-10-21 07:31:30 +0200

    Solution 10 of substitution-cipher, ratio 0.92 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/o884uipo Prompt: Make sure that \n\n ->

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -13,7 +13,7 @@ using namespace std;
 /*
 This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.
 For example,
-input:
+input: 
 
 
 
@@ -45,12 +45,13 @@ output:
 llllllllll
 */
 int main() {
-    string a, b, c;
-    cin >> a >> b >> c;
-    for(int i = 0; i < c.length(); i++){
-        for(int j = 0; j < a.length(); j++){
-            if(c[i] == a[j]){
-                cout << b[j];
+    string s1,s2,s3;
+    cin>>s1>>s2>>s3;
+    for(int i=0;i<s3.length();i++){
+        for(int j=0;j<s1.length();j++){
+            if(s3[i]==s1[j]){
+                cout<<s2[j];
+                break;
             }
         }
     }
