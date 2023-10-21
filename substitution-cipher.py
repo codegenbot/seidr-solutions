de5118efa7c3f12d560bdcb38d62f2064077fb37
beep boop <githubbot@vadim.me>
2023-10-21 07:00:57 +0200

    Solution 23 of substitution-cipher, ratio 0.97 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/z9vpasvo Prompt: Make sure that \n\n ->

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -11,9 +11,8 @@ import re
 This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.
 For example,
 input:
-abc
-def
-ghia
+
+
 
 output:
 
