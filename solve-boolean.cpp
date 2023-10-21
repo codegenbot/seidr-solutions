7f1ce7e9a0ec15c164e010d8dc9300760ae30c8d
beep boop <githubbot@vadim.me>
2023-10-21 04:29:05 +0200

    Solution 38 of solve-boolean, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/pvcit2fc Prompt: Make sure that t -> True

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -14,23 +14,23 @@ using namespace std;
 Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.
 For example,
 input:
-t
+T
 output:
 True
 input:
-f
+F
 output:
 False
 input:
-f&f
+F&F
 output:
 False
 input:
-f&t
+F&T
 output:
 False
 input:
-t&f
+T&F
 output:
 False
 */
@@ -62,9 +62,9 @@ int main() {
             }
         }
         if (s.top() == 'T') {
-            cout << "True" << endl; // make sure that t -> True
+            cout << "true" << endl;
         } else {
-            cout << "False" << endl;
+            cout << "false" << endl;
         }
     }
     return 0;
