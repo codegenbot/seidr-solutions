23c2cbc1a7b02473de0ad9d0c434cdaf4a52067f
beep boop <githubbot@vadim.me>
2023-10-21 06:35:26 +0200

    Solution 9 of substitution-cipher, ratio 0.97 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/z9vpasvo Prompt: This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -8,24 +8,46 @@ import itertools
 import queue
 import re
 """
-This problem gives a string. The program must print all the possible substrings of the given string.
-For example, if the input is abc, the output should be:
+This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.
+For example,
+input:
+
+
+
+output:
+
+input:
 a
-ab
-abc
-b
-bc
-c
-If the input is xyz, the output should be:
-x
-xy
-xyz
-y
-yz
+a
+a
+output:
+a
+input:
+j
+h
+j
+output:
+h
+input:
+a
 z
+a
+output:
+z
+input:
+e
+l
+eeeeeeeeee
+output:
+llllllllll
 """
 if __name__ == '__main__':
-    string = input()
-    for i in range(len(string)):
-        for j in range(i,len(string)):
-            print(string[i:j+1])
+    s1 = input()
+    s2 = input()
+    s3 = input()
+    s4 = ""
+    for i in range(len(s3)):
+        for j in range(len(s1)):
+            if s3[i] == s1[j]:
+                s4 += s2[j]
+    print(s4)
