4d1a7e4272a403ce2dae87b7a56c7a00283221e5
beep boop <githubbot@vadim.me>
2023-10-21 03:08:51 +0200

    Solution 30 of snow-day, ratio 0.13 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/1vw1ls3i Prompt: Make sure that 15\n15.0\n15.0\n0.15 -> 92.5748913763936

--- snow-day.py
+++ snow-day.py
@@ -1,3 +1,12 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
 """
 Given an integer representing a number of hours and 3 floats representing how much snow is on theground, the rate of snow fall, and the proportion of snow melting per hour, return the amount of snow on the ground after the amount of hours given. Each hour is considered a discrete event of adding snow and then melting, not a continuous process.
 For example,
@@ -39,10 +48,12 @@ output:
 """
 if __name__ == '__main__':
     hours = int(input())
-    snow_on_ground = float(input())
+    snow = float(input())
     snow_fall_rate = float(input())
-    snow_melt_rate = float(input())
-    if snow_melt_rate == 0:
-        print(snow_on_ground + hours * snow_fall_rate)
-    else:
-        print((snow_on_ground + snow_fall_rate) * (1 - snow_melt_rate) ** hours - snow_fall_rate * (1 - snow_melt_rate) ** hours)
+    snow_melting_rate = float(input())
+    snow_on_ground = snow
+    for i in range(hours):
+        snow_on_ground += snow_fall_rate
+        snow_on_ground -= snow_on_ground * snow_melting_rate
+        snow_on_ground = max(snow_on_ground, 0.0)
+    print(snow_on_ground)
