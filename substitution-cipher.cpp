965a5d080cafca87cb1bc183d57bcc1fd5a8cd6c
beep boop <githubbot@vadim.me>
2023-10-21 07:21:26 +0200

    Solution 8 of substitution-cipher, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/o884uipo Prompt: This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -11,7 +11,7 @@
 #include <climits>
 using namespace std;
 /*
-This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message.
+This problem gives 3 strings. The first two represent a cipher, mapping each character in one string to the one at the same index in the other string. The program must apply this cipher to the third string and return the deciphered message. 
 For example,
 input:
 
@@ -45,14 +45,3 @@ output:
 llllllllll
 */
 int main() {
-    string s1,s2,s3;
-    cin>>s1>>s2>>s3;
-    map<char,char>mp;
-    for(int i=0;i<s1.size();i++){
-        mp[s1[i]]=s2[i];
-    }
-    for(int i=0;i<s3.size();i++){
-        cout<<mp[s3[i]];
-    }
-    return 0;
-}
