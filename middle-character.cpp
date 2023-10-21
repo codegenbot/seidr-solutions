509b3f54ca345c5e5c3137b6ef04fecb945180be
beep boop <githubbot@vadim.me>
2023-10-21 09:14:39 +0200

    Solution 78 of middle-character, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/ovehaxgo Prompt: Make sure that Q -> Q

--- middle-character.cpp
+++ middle-character.cpp
@@ -11,16 +11,17 @@
 #include <climits>
 using namespace std;
 /*
-Given a string, return the middle character as a string if it is odd length; return the two middle characters as a string if it is even length.
+Given a string, return the middle character as a string if it is odd length;
+return the two middle characters as a string if it is even length.
 For example,
 input:
 Q
 output:
 Q
 input:
-QQ
+ 
 output:
-QQ
+ 
 input:
 $
 output:
@@ -34,17 +35,14 @@ input:
 output:
 )b
 */
-string mid(string s) {
-    if(s.size() % 2 == 1) {
-        return s.substr(s.size() / 2, 1);
-    } else {
-        return s.substr(s.size() / 2 - 1, 2);
-    }
-}
 int main() {
     string s;
     while(cin >> s) {
-        cout << mid(s) << endl;
+        if(s.size() % 2 == 1) {
+            cout << s[s.size() / 2] << endl;
+        } else {
+            cout << s[s.size() / 2 - 1] << s[s.size() / 2] << endl;
+        }
     }
     return 0;
 }
