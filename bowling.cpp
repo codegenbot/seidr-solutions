97f52c1beaa84df70d4d312740085f0e099c3fa5
beep boop <githubbot@vadim.me>
2023-10-21 03:12:44 +0200

    Solution 82 of bowling, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/js6eizju Prompt: Make sure that -------------------- -> 0

--- bowling.cpp
+++ bowling.cpp
@@ -60,13 +60,9 @@ int helper(string &s, int start) {
 int main() {
     string s;
     while (getline(cin, s)) {
-        if (s == "--------------------") {
-            cout << 0 << endl;
-            continue;
-        }
         int res = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '-') {
+            if (s[i] == '-') { // make sure that -------------------- -> 0
                 continue;
             }
             res += helper(s, i);
