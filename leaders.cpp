61b6193aaef07973e9668ca2363cebaaead5c806
beep boop <githubbot@vadim.me>
2023-10-21 02:38:53 +0200

    Solution 72 of leaders, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/4sg0ax99 Prompt: Make sure that 0\n -> 0\n

--- leaders.cpp
+++ leaders.cpp
@@ -5,32 +5,32 @@ input:
 0\n
 
 output:
-0\n
+0
 
 input:
 1
-0\n
+0
 output:
 1
-0\n
+0
 input:
 1
-451\n
+451
 output:
 1
-451\n
+451
 input:
 2
-1000 0\n
+1000 0
 output:
 2
-1000 0\n
+1000 0
 input:
 2
-0 1000\n
+0 1000
 output:
 1
-1000\n
+1000
 */
 #include <vector>
 #include <iostream>
