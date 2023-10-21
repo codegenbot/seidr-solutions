d88622992e392aa888ad77577e16e2508f3fa455
beep boop <githubbot@vadim.me>
2023-10-21 03:20:40 +0200

    Solution 7 of solve-boolean, ratio 0.59 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/v3z06ogc Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,5 +1,3 @@
-# Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.
-
 import os
 import sys
 import numpy as np
@@ -33,4 +31,18 @@ t&f
 output:
 False
 """
+
+def eval_bool(expression):
+    exp = expression.split('|')
+    for i in range(len(exp)):
+        exp[i] = exp[i].split('&')
+    for i in range(len(exp)):
+        for j in range(len(exp[i])):
+            exp[i][j] = exp[i][j] == 't'
+    for i in range(len(exp)):
+        exp[i] = any(exp[i])
+    return any(exp)
+
 if __name__ == '__main__':
+    expression = input()
+    print(eval_bool(expression))
