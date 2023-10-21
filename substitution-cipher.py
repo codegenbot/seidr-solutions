e2322d2c40a9d34b85743c16453f22e5f114f23b
beep boop <githubbot@vadim.me>
2023-10-21 07:13:46 +0200

    Solution 30 of substitution-cipher, ratio 0.01 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/z9vpasvo Prompt: Make sure that \n\n ->

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -8,14 +8,10 @@ import itertools
 import queue
 import re
 """
-This problem gives a string of text. The program must remove all the newlines and return the text as a single line.
-For example, input:
-I am a
-cow.
-output:
-I am a cow.
+This problem gives a string. The program must print all the possible substrings of the given string. For example, if the input is abc, the output should be: a ab abc b bc c If the input is xyz, the output should be: x xy xyz y yz z
 """
 if __name__ == '__main__':
-    s1 = input()
-    s1 = s1.replace("\n", "")
-    print(s1)
+    string = input()
+    for i in range(len(string)):
+        for j in range(i,len(string)):
+            print(string[i:j+1])
