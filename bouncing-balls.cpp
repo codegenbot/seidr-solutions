ba2f00b03969cbaeadd5053b46b9d117fb51a543
beep boop <githubbot@vadim.me>
2023-10-21 12:09:29 +0200

    Solution 43 of bouncing-balls, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/wsjdr5hk Prompt: Make sure that 1.001\n1.0\n1 -> 2.001

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -49,9 +49,10 @@ int main() {
   while (cin >> a >> b >> c) {
     double bounciness = b / a;
     double res = a;
-    for (int i = 1; i <= c; i++) {
+    for (int i = 1; i < c; i++) {
       res += a * pow(bounciness, i);
     }
+    res += b;
     printf("%.5f\n", res);
   }
   return 0;
