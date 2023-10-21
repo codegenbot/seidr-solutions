734dcb550c31b4a5ffda2ecc67a53e8fb4f85205
beep boop <githubbot@vadim.me>
2023-10-21 03:01:26 +0200

    Solution 0 of solve-boolean, ratio 0.78 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/pvcit2fc Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

---
+++ solve-boolean.cpp
@@ -0,0 +1,77 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
+/*
+Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.
+For example,
+input:
+t
+output:
+True
+input:
+f
+output:
+False
+input:
+f&f
+output:
+False
+input:
+f&t
+output:
+False
+input:
+t&f
+output:
+False
+*/
+int main() {
+    string s;
+    getline(cin, s);
+    stack<char> stk;
+    for (char c : s) {
+        if (c == ' ') continue;
+        if (c == '&' || c == '|') {
+            stk.push(c);
+        } else if (c == 't') {
+            if (stk.empty()) {
+                stk.push('t');
+            } else {
+                char op = stk.top();
+                stk.pop();
+                if (op == '&') {
+                    stk.push(c);
+                } else {
+                    stk.push(stk.top() == 't' ? 't' : 'f');
+                    stk.pop();
+                    stk.push(c);
+                }
+            }
+        } else {
+            if (stk.empty()) {
+                stk.push('f');
+            } else {
+                char op = stk.top();
+                stk.pop();
+                if (op == '&') {
+                    stk.push(stk.top() == 'f' ? 'f' : 't');
+                    stk.pop();
+                    stk.push('f');
+                } else {
+                    stk.push('f');
+                }
+            }
+        }
+    }
+    cout << (stk.top() == 't' ? "True" : "False") << endl;
+    return 0;
+}
