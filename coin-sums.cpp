a93f2750a572586e595bfe5db59a4e52959e1b88
beep boop <githubbot@vadim.me>
2023-10-21 22:38:07 +0200

    Solution 72 of coin-sums, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/ztmk829g Prompt: Make sure that 1 -> 1\n0\n0\n0

--- coin-sums.cpp
+++ coin-sums.cpp
@@ -9,7 +9,6 @@
 #include <set>
 #include <stack>
 #include <climits>
-#include <sstream>
 using namespace std;
 /*
 Given a number of cents, ﬁnd the fewest number of US coins (pennies, nickles, dimes, quarters) needed to make that amount, and return the number of each type of coin as a separate output.
@@ -24,27 +23,6 @@ output:
 input:
 2
 output:
-2
-0
-0
-0
-input:
-3
-output:
-3
-0
-0
-0
-input:
-4
-output:
-4
-0
-0
-0
-input:
-5
-output:
 0
 1
 0
@@ -53,10 +31,14 @@ output:
 int main() {
     int cents;
     while(cin >> cents) {
-        stringstream ss;
-        ss << cents;
-        string s = ss.str();
-        cout << s << endl;
+        int quarters = cents / 25;
+        int dimes = cents % 25 / 10;
+        int nickels = cents % 25 % 10 / 5;
+        int pennies = cents % 25 % 10 % 5;
+        cout << pennies << endl;
+        cout << nickels << endl;
+        cout << dimes << endl;
+        cout << quarters << endl;
     }
     return 0;
 }
