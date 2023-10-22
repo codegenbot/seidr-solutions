7dedde9bdda4dcc392f02d364f507d32d9f71007
beep boop <githubbot@vadim.me>
2023-10-22 07:43:02 +0200

    Solution 85 of bouncing-balls, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/wsjdr5hk Prompt: Make sure that 1.001\n1.0\n1 -> 2.001

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -11,8 +11,7 @@
 #include <climits>
 using namespace std;
 /*
-Given a starting height and a height after the ﬁrst bounce of a dropped ball, calculate the bounciness index (height of ﬁrst bounce / starting height).
-Then, given a number of bounces, use the bounciness index to calculate the total distance that the ball travels across those bounces.
+Given a starting height and a height after the ﬁrst bounce of a dropped ball, calculate the bounciness index (height of ﬁrst bounce / starting height). Then, given a number of bounces, use the bounciness index to calculate the total distance that the ball travels across those bounces.
 For example,
 input:
 1.001
@@ -47,13 +46,11 @@ output:
 */
 int main() {
     double h, h1, n;
-    while (cin >> h >> h1 >> n) {
-        double k = h1 / h;
-        double sum = h1;
-        for (int i = 1; i < n; i++) {
-            sum += pow(k, i) * h;
-        }
-        printf("%.3f\n", sum);
+    cin >> h >> h1 >> n;
+    double k = h1 / h;
+    double sum = h1;
+    for (int i = 1; i < n; i++) {
+        sum += pow(k, i) * h;
     }
     cout << sum;
 }
