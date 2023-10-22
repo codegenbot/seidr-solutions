53089942813f07c086ee0b00d5b4ec5e7cef630d
beep boop <githubbot@vadim.me>
2023-10-22 09:33:34 +0200

    Solution 85 of coin-sums, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/ztmk829g Prompt: Make sure that 1 -> 1\n0\n0\n0

--- coin-sums.cpp
+++ coin-sums.cpp
@@ -76,7 +76,11 @@ int main() {
             cents -= penny;
             num_penny++;
         }
-        cout << num_penny << "\n" << num_nickle << "\n" << num_dime << "\n" << num_quarter << endl;
+        cout << num_penny << endl;
+        cout << num_nickle << endl;
+        cout << num_dime << endl;
+        cout << num_quarter << endl;
+        cout << endl;
     }
     return 0;
 }
