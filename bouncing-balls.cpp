a1f33325b151081ea705e2bc4d10db7a7be48af2
beep boop <githubbot@vadim.me>
2023-10-22 07:46:59 +0200

    Solution 92 of bouncing-balls, ratio 0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/codex-for-psb/runs/wsjdr5hk Prompt: Fix 6c0202c8-cb74-4986-a91b-85fe710c210c.cpp: In function ‘int main(int, char**)’: 6c0202c8-cb74-4986-a91b-85fe710c210c.cpp:22:42: error: ‘pow’ was not declared in  this scope    22 |         total_distance += start_height * pow(bounciness, i + 1);       |                                          ^~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,29 +1,29 @@
-#include <cmath>
-#include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
-  if (argc != 4)
-  {
-    std::cout << "Usage: " << argv[0] << " start_height first_bounce_height num_bounces" << std::endl;
-    return 1;
-  }
+    if (argc != 4)
+    {
+        printf("Usage: %s start_height first_bounce_height num_bounces\n", argv[0]);
+        return 1;
+    }
 
-  double start_height = strtod(argv[1], NULL);
-  double first_bounce_height = strtod(argv[2], NULL);
-  int num_bounces = atoi(argv[3]);
+    double start_height = strtod(argv[1], NULL);
+    double first_bounce_height = strtod(argv[2], NULL);
+    int num_bounces = atoi(argv[3]);
 
-  double bounciness = first_bounce_height / start_height;
+    double bounciness = first_bounce_height / start_height;
 
-  double total_distance = start_height;
+    double total_distance = start_height;
 
-  for (int i = 0; i < num_bounces; i++)
-  {
-    total_distance += start_height * pow(bounciness, i + 1);
-  }
+    for (int i = 0; i < num_bounces; i++)
+    {
+        total_distance += start_height * pow(bounciness, i + 1);
+    }
 
-  std::cout << total_distance << std::endl;
+    printf("%f\n", total_distance);
 
-  return 0;
+    return 0;
 }
