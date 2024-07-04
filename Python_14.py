0e82c7703ad94a79e0ca57939e2c02bc1a6c039f
beep boop <githubbot@vadim.me>
2024-07-04 23:50:47 +0200

    Solution 16 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/b8o0hwvx Prompt: Program did not receive expected input

--- Python_14.py
+++ Python_14.py
@@ -1,6 +1,5 @@
 def prefixes(string):
     return [string[:i] for i in range(1, len(string) + 1)]
 
-input_strings = input().split()
-for input_string in input_strings:
-    print(prefixes(input_string))
+input_string = input().strip()
+print(prefixes(input_string))
