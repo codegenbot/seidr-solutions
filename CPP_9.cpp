08ed36f74a7c2e9e39410415706d35f9bcfce85c
beep boop <githubbot@vadim.me>
2024-07-04 23:31:46 +0200

    Solution 2 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/cvzkncgb Prompt:  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8d146259-8871-4a51-9e95-11e9ef79ae09.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8d146259-8871-4a51-9e95-11e9ef79ae09.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8d146259-8871-4a51-9e95-11e9ef79ae09.cpp:13: 8d146259-8871-4a51-9e95-11e9ef79ae09.cpp: In function ‘int main()’: 8d146259-8871-4a51-9e95-11e9ef79ae09.cpp:23:20: error: ‘rolling_max’ was not dec lared in this scope    23 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 8d146259-8871-4a51-9e95-11e9ef79ae09.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,11 +1,28 @@
-vector<int> rolling_max(vector<int> numbers){
+#include <vector>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+vector<int> rolling_max(const vector<int>& numbers) {
     vector<int> result;
-    int max_so_far = INT_MIN;
-    for(int num : numbers){
-        if(num > max_so_far) {
+    if (numbers.empty()) return result;
+    int max_so_far = numbers[0];
+    for (int num : numbers) {
+        if (num > max_so_far) {
             max_so_far = num;
         }
         result.push_back(max_so_far);
     }
     return result;
+}
+
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a == b;
+}
+
+int main() {
+    assert(issame(rolling_max({3, 2, 3, 100, 3}), {3, 3, 3, 100, 100}));
+    cout << "Test passed!" << endl;
+    return 0;
 }
