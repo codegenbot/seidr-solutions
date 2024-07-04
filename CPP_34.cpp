267d15ed9be77013eaadebc5a96b069b22e0744a
beep boop <githubbot@vadim.me>
2024-07-04 23:40:21 +0200

    Solution 6 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/lhlfp4m8 Prompt:     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ baad7a06-ef8f-4987-bb23-518b2460fa3d.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ baad7a06-ef8f-4987-bb23-518b2460fa3d.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from baad7a06-ef8f-4987-bb23-518b2460fa3d.cpp:6: baad7a06-ef8f-4987-bb23-518b2460fa3d.cpp: In function ‘int main()’: baad7a06-ef8f-4987-bb23-518b2460fa3d.cpp:16:20: error: ‘unique’ was not declared  in this scope    16 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ baad7a06-ef8f-4987-bb23-518b2460fa3d.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -3,21 +3,18 @@
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-vector<int> unique(vector<int> l) {
-    sort(l.begin(), l.end());
+std::vector<int> unique(std::vector<int> l) {
+    std::sort(l.begin(), l.end());
     l.erase(std::unique(l.begin(), l.end()), l.end());
     return l;
 }
 
+bool is_same(const std::vector<int>& a, const std::vector<int>& b) {
+    return a == b;
+}
+
 int main() {
-    vector<int> result = unique({5, 3, 5, 2, 3, 3, 9, 0, 123});
-    assert(issame(result, {0, 2, 3, 5, 9, 123}));
-    cout << "Test passed!" << endl;
+    assert(is_same(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}), {0, 2, 3, 5, 9, 123}));
+    std::cout << "All tests passed!" << std::endl;
     return 0;
 }
