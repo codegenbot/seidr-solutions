788d701af19cd66afc19462e61e9be1febf27b95
beep boop <githubbot@vadim.me>
2024-07-04 23:45:30 +0200

    Solution 8 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/5zyrmihf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6ca0c323-ee86-4ae4-81d4-3482f4662 430.py", line 2     count = Counter(numbers) IndentationError: unexpected indent

--- Python_26.py
+++ Python_26.py
@@ -1,8 +1,6 @@
 from collections import Counter
 
+
 def unique_numbers(numbers):
     count = Counter(numbers)
     return [num for num in numbers if count[num] == 1]
-
-numbers = [4, 5, 6, 7, 4, 5, 8]
-print(unique_numbers(numbers))  # Output will be [6, 7, 8]
