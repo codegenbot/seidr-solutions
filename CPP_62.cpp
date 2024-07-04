13e96c097fbeb18052580b600d1177012a85c114
beep boop <githubbot@vadim.me>
2024-07-05 00:32:40 +0200

    Solution 56 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/sd15xeud Prompt: 8589132f-f868-4ada-8c03-2c1b3852aa4d.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 8589132f-f868-4ada-8c03-2c1b3852aa4d.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 8589132f-f868-4ada-8c03-2c1b3852aa4d.cpp:25: 8589132f-f868-4ada-8c03-2c1b3852aa4d.cpp: In function ‘int main()’: 8589132f-f868-4ada-8c03-2c1b3852aa4d.cpp:35:19: error: call of overloaded ‘issam e(std::vector<float>, <brace-enclosed initializer list>)’ is ambiguous    35 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~ 8589132f-f868-4ada-8c03-2c1b3852aa4d.cpp:15:6: note: candidate: ‘bool issame(con st std::vector<float>&, const std::vector<float>&)’    15 | bool issame(const vector<float>& a, const vector<float>& b) {       |      ^~~~~~ 8589132f-f868-4ada-8c03-2c1b3852aa4d.cpp:26:6: note: candidate: ‘bool issame(std ::vector<float>, std::vector<float>)’    26 | bool issame(vector<float> a,vector<float>b){       |      ^~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -18,6 +18,8 @@ bool issame(const vector<float>& a, const vector<float>& b) {
 
 int main() {
     assert(issame(derivative({1}), vector<float>{}));
+    assert(issame(derivative({1, 2, 3}), vector<float>{2, 6}));
+    assert(issame(derivative({5, 4, 3, 2, 1}), vector<float>{4, 6, 6, 4}));
     cout << "All tests passed!" << endl;
     return 0;
 }
