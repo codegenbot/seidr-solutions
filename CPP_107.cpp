3613cc6ec6da2354597adcb0e6114a7239a414ae
beep boop <githubbot@vadim.me>
2024-07-05 01:41:01 +0200

    Solution 6 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/nk2ngdvd Prompt:       |                    ^~~ 5c2838ff-20cf-43ef-af1f-619cef72c04d.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5c2838ff-20cf-43ef-af1f-619cef72c04d.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5c2838ff-20cf-43ef-af1f-619cef72c04d.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5c2838ff-20cf-43ef-af1f-619cef72c04d.cpp:24: 5c2838ff-20cf-43ef-af1f-619cef72c04d.cpp: In function ‘int main()’: 5c2838ff-20cf-43ef-af1f-619cef72c04d.cpp:34:20: error: ‘even_odd_palindrome’ was  not declared in this scope    34 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 5c2838ff-20cf-43ef-af1f-619cef72c04d.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 #include <cassert>
 
 using namespace std;
 
 vector<int> even_odd_palindrome(int n) {
     int even_count = 0, odd_count = 0;
-    
+
     auto is_palindrome = [](int num) {
         string s = to_string(num);
-        string rev_s = string(s.rbegin(), s.rend());
+        string rev_s = s;
+        reverse(rev_s.begin(), rev_s.end());
         return s == rev_s;
     };
-    
+
     for (int i = 1; i <= n; ++i) {
         if (is_palindrome(i)) {
             if (i % 2 == 0) {
-                ++even_count;
+                even_count++;
             } else {
-                ++odd_count;
+                odd_count++;
             }
         }
     }
-    
+
     return {even_count, odd_count};
 }
 
 int main() {
-    assert((even_odd_palindrome(1) == vector<int>{0, 1}));
-    cout << "Test passed!" << endl;
+    assert(even_odd_palindrome(1) == (vector<int>{0, 1}));
+    assert(even_odd_palindrome(10) == (vector<int>{1, 9}));
+    
+    // Add more tests if necessary
+
     return 0;
 }
