98d90de44902fce74da98712a84e06b4b5e824bf
beep boop <githubbot@vadim.me>
2024-07-05 00:14:24 +0200

    Solution 22 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/5drh55g9 Prompt: 4b35b4d0-828e-483b-b2a8-5f8bc22d40ed.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 4b35b4d0-828e-483b-b2a8-5f8bc22d40ed.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -12,12 +12,7 @@ bool same_chars(string s0, string s1) {
 }
 
 int main() {
-    string s0, s1;
-    cin >> s0 >> s1;
-    if (same_chars(s0, s1)) {
-        cout << "true" << endl;
-    } else {
-        cout << "false" << endl;
-    }
+    cout << boolalpha << same_chars("aabb", "aaccc") << endl;
+    cout << boolalpha << same_chars("abc", "bca") << endl;
     return 0;
 }
