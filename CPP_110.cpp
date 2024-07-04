808384bfd1e37168505a09b52c898bf389e8cda3
beep boop <githubbot@vadim.me>
2024-07-05 01:44:40 +0200

    Solution 5 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/kygg7z2t Prompt: 89cb7661-086a-4874-82e0-4d3b94702469.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string exchange(vector<int> lst1, vector<int> lst2) {       | ^~~~~~ In file included from 89cb7661-086a-4874-82e0-4d3b94702469.cpp:22: 89cb7661-086a-4874-82e0-4d3b94702469.cpp: In function ‘int main()’: 89cb7661-086a-4874-82e0-4d3b94702469.cpp:24:13: error: ‘exchange’ was not declar ed in this scope    24 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 string exchange(vector<int> lst1, vector<int> lst2) {
+    // Check if lst1 already contains only even numbers
     bool allEven = true;
     for(int num : lst1) {
         if(num % 2 != 0) {
@@ -14,6 +15,7 @@ string exchange(vector<int> lst1, vector<int> lst2) {
     }
     if(allEven) return "YES";
 
+    // Check if there is at least one even number in lst2
     for(int num : lst2) {
         if(num % 2 == 0) {
             return "YES";
@@ -24,6 +26,21 @@ string exchange(vector<int> lst1, vector<int> lst2) {
 }
 
 int main() {
-    cout << (exchange({100, 200}, {200, 200}) == "YES") << endl;
+    vector<int> lst1, lst2;
+    int n1, n2, temp;
+
+    cin >> n1;
+    for(int i = 0; i < n1; ++i) {
+        cin >> temp;
+        lst1.push_back(temp);
+    }
+
+    cin >> n2;
+    for(int i = 0; i < n2; ++i) {
+        cin >> temp;
+        lst2.push_back(temp);
+    }
+
+    cout << exchange(lst1, lst2) << endl;
     return 0;
 }
