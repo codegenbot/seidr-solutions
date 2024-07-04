d8405ececc2f017bccf67beae7609ffae18cb280
beep boop <githubbot@vadim.me>
2024-07-04 23:54:05 +0200

    Solution 13 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/ttir79ye Prompt: Program did not receive expected input

--- Python_22.py
+++ Python_22.py
@@ -1,7 +1,9 @@
 from typing import List, Any
 
+
 def filter_integers(values: List[Any]) -> List[int]:
     return [x for x in values if isinstance(x, int)]
+
 
 if __name__ == "__main__":
     user_input = input()
