7c7fc6ff1e43e6036868e8707f23b5d45d3f9439
beep boop <githubbot@vadim.me>
2024-07-04 23:58:20 +0200

    Solution 6 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/bfdbg45o Prompt: f1c30bf4-766b-4182-a7a1-cbeb98c7dd42.cpp:3:24: error: ‘vector’ was not declared in this scope     3 | bool pairs_sum_to_zero(vector<int> l) {       |                        ^~~~~~ f1c30bf4-766b-4182-a7a1-cbeb98c7dd42.cpp:3:31: error: expected primary-expressio n before ‘int’     3 | bool pairs_sum_to_zero(vector<int> l) {       |                               ^~~ In file included from f1c30bf4-766b-4182-a7a1-cbeb98c7dd42.cpp:14: f1c30bf4-766b-4182-a7a1-cbeb98c7dd42.cpp: In function ‘int main()’: f1c30bf4-766b-4182-a7a1-cbeb98c7dd42.cpp:16:30: error: ‘pairs_sum_to_zero’ canno t be used as a function    16 |     assert (pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);       |             ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,14 +1,11 @@
 #include <unordered_set>
 #include <vector>
-#include <iostream>
 #include <cassert>
 
-using namespace std;
-
-bool pairs_sum_to_zero(vector<int> l) {
-    unordered_set<int> seen;
+bool pairs_sum_to_zero(std::vector<int> l) {
+    std::unordered_set<int> seen;
     for (int num : l) {
-        if (seen.find(-num) != seen.end()) {
+        if (seen.count(-num)) {
             return true;
         }
         seen.insert(num);
@@ -18,7 +15,5 @@ bool pairs_sum_to_zero(vector<int> l) {
 
 int main() {
     assert(pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);
-    assert(pairs_sum_to_zero({-3, 9, -1, 1, 2, 31}) == true);
-    cout << "All tests passed!" << endl;
     return 0;
 }
