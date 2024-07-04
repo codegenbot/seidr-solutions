7cab772407bb061ad10056929060bb3f8f7adde4
beep boop <githubbot@vadim.me>
2024-07-05 00:11:26 +0200

    Solution 3 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/5drh55g9 Prompt: cb21f43f-fc2b-45d3-9ffb-7f2e91a499ed.cpp:1:17: error: ‘string’ was not declared in this scope     1 | bool same_chars(string s0, string s1){       |                 ^~~~~~ cb21f43f-fc2b-45d3-9ffb-7f2e91a499ed.cpp:1:28: error: ‘string’ was not declared in this scope     1 | bool same_chars(string s0, string s1){       |                            ^~~~~~ cb21f43f-fc2b-45d3-9ffb-7f2e91a499ed.cpp:1:37: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool same_chars(string s0, string s1){       |                                     ^ In file included from cb21f43f-fc2b-45d3-9ffb-7f2e91a499ed.cpp:9: cb21f43f-fc2b-45d3-9ffb-7f2e91a499ed.cpp: In function ‘int main()’: cb21f43f-fc2b-45d3-9ffb-7f2e91a499ed.cpp:11:23: error: ‘same_chars’ cannot be us ed as a function    11 |     assert (same_chars("aabb", "aaccc") == false);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-bool same_chars(string s0, string s1) {
+bool same_chars(string s0, string s1){
     sort(s0.begin(), s0.end());
     s0.erase(unique(s0.begin(), s0.end()), s0.end());
     sort(s1.begin(), s1.end());
@@ -12,7 +12,8 @@ bool same_chars(string s0, string s1) {
 }
 
 int main() {
-    cout << (same_chars("aabb", "aaccc") == false) << endl;
-    cout << (same_chars("abc", "bca") == true) << endl;
+    // Test cases
+    cout << (same_chars("aabb", "aaccc") == false) << endl; // should print 1 (true)
+    cout << (same_chars("abc", "bca") == true) << endl; // should print 1 (true)
     return 0;
 }
