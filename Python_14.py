e30c6a0586919e1b96d38c023e3fdaf2347219cd
beep boop <githubbot@vadim.me>
2024-07-04 23:33:16 +0200

    Solution 2 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/b8o0hwvx Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0cb97e43-aa1b-41ea-a8b7-88d797a45 a58.py", line 1     return [string[:i] for i in range(1, len(string) + 1)]     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_14.py
+++ Python_14.py
@@ -1,1 +1,5 @@
-return [string[:i] for i in range(1, len(string) + 1)]
+def prefixes(string):
+    return [string[:i] for i in range(1, len(string) + 1)]
+
+input_string = input("Enter a string: ")
+print(prefixes(input_string))
