45bb1f924745adcb58aa506e93be753c8411ef15
beep boop <githubbot@vadim.me>
2024-07-04 23:34:27 +0200

    Solution 6 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/ocwfcpio Prompt:       |                    ^~~~~ 794f07bb-a27f-4ec2-9c75-7628ea326b28.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 794f07bb-a27f-4ec2-9c75-7628ea326b28.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 794f07bb-a27f-4ec2-9c75-7628ea326b28.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 794f07bb-a27f-4ec2-9c75-7628ea326b28.cpp:11: 794f07bb-a27f-4ec2-9c75-7628ea326b28.cpp: In function ‘int main()’: 794f07bb-a27f-4ec2-9c75-7628ea326b28.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 794f07bb-a27f-4ec2-9c75-7628ea326b28.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 vector<float> get_positive(vector<float> l) {
     vector<float> result;
-    for(float num : l) {
-        if(num > 0) {
+    for (float num : l) {
+        if (num > 0) {
             result.push_back(num);
         }
     }
@@ -15,16 +15,15 @@ vector<float> get_positive(vector<float> l) {
 }
 
 bool issame(vector<float> a, vector<float> b) {
-    if (a.size() != b.size()) return false;
-    for (size_t i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
+    return a == b;
 }
 
 int main() {
+    // Example test case
+    assert(issame(get_positive({-1.0, 2.5, 3.0, -2.0, 0.0}), {2.5, 3.0}));
     assert(issame(get_positive({}), {}));
-    assert(issame(get_positive({1.0, -2.0, 3.0}), {1.0, 3.0}));
-    cout << "All tests passed.\n";
+    assert(issame(get_positive({-1.0, -2.0}), {}));
+    assert(issame(get_positive({1.0, 2.0, 3.0}), {1.0, 2.0, 3.0}));
+    cout << "All tests passed!" << endl;
     return 0;
 }
