6ade7fbbd607c430f2b172ed1c0f9fb9fdf131ce
beep boop <githubbot@vadim.me>
2024-07-04 23:34:09 +0200

    Solution 4 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/ocwfcpio Prompt:       |                    ^~~~~ 0856c193-fef0-415a-b71e-a350c4e0f6b9.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 0856c193-fef0-415a-b71e-a350c4e0f6b9.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 0856c193-fef0-415a-b71e-a350c4e0f6b9.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 0856c193-fef0-415a-b71e-a350c4e0f6b9.cpp:11: 0856c193-fef0-415a-b71e-a350c4e0f6b9.cpp: In function ‘int main()’: 0856c193-fef0-415a-b71e-a350c4e0f6b9.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 0856c193-fef0-415a-b71e-a350c4e0f6b9.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,8 +1,10 @@
+#include <iostream>
 #include <vector>
 #include <cassert>
+
 using namespace std;
 
-vector<float> get_positive(vector<float> l){
+vector<float> get_positive(vector<float> l) {
     vector<float> result;
     for(float num : l) {
         if(num > 0) {
@@ -12,19 +14,17 @@ vector<float> get_positive(vector<float> l){
     return result;
 }
 
-bool issame(vector<float> a, vector<float> b){
-    if(a.size() != b.size())
-        return false;
-    for(size_t i = 0; i < a.size(); ++i) {
-        if(a[i] != b[i])
-            return false;
+bool issame(vector<float> a, vector<float> b) {
+    if (a.size() != b.size()) return false;
+    for (size_t i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
 
 int main() {
-    assert (issame(get_positive({1.0, -2.0, 3.0}) , {1.0, 3.0}));
-    assert (issame(get_positive({-1.0, -2.0, -3.0}) , {}));
-    assert (issame(get_positive({}) , {}));
+    assert(issame(get_positive({}), {}));
+    assert(issame(get_positive({1.0, -2.0, 3.0}), {1.0, 3.0}));
+    cout << "All tests passed.\n";
     return 0;
 }
