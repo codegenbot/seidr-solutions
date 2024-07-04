e11caa8cedfd86c79843aeadea0f9b3c8b8e7fa6
beep boop <githubbot@vadim.me>
2024-07-04 23:48:49 +0200

    Solution 10 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/t2ts6cf1 Prompt: f63b8eec-3458-4a3a-a306-3698c7837782.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ f63b8eec-3458-4a3a-a306-3698c7837782.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <cassert>
+#include <iostream>
 using namespace std;
 
 string decode_cyclic(string s) { 
@@ -13,19 +14,23 @@ string decode_cyclic(string s) {
     return output;
 }
 
-string encode_cyclic(string s) {
+string encode_cyclic(const string& s) {
     int l = s.length();
     string x, output;
-    for (int i = 0; i * 3 < l; i++) {
-        x = s.substr(i * 3, 3);
-        if (x.length() == 3) x = x.substr(1, 2) + x[0];
+    for (int i = 0; i < l; i += 3) {
+        x = s.substr(i, 3);
+        if (x.length() == 3) x = x.substr(1) + x[0];
         output += x;
     }
     return output;
 }
 
 int main() {
-    string str = "example";
+    string str;
+    char chr;
+    while (cin >> noskipws >> chr) {
+        str += chr;
+    }
     string encoded_str = encode_cyclic(str);
     assert(decode_cyclic(encoded_str) == str);
     return 0;
