fd4150fae3cc25f91b0cd22cb9344ac40e3b84b5
beep boop <githubbot@vadim.me>
2024-07-05 01:28:46 +0200

    Solution 1 of CPP/102, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/19l0kkrj Prompt: Complete the following code given the task description and function signature.

--- CPP_102.cpp
+++ CPP_102.cpp
@@ -1,5 +1,4 @@
-int choose_num(int x, int y) {
-    if (x > y) return -1;
+if (x > y) return -1;
     if (y % 2 == 0) return y;
     if ((y - 1) >= x && (y - 1) % 2 == 0) return y - 1;
     return -1;
