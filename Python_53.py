8c7021490709bdae25b4335ad5c986c880154c4a
beep boop <githubbot@vadim.me>
2024-07-05 01:14:07 +0200

    Solution 19 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/dhwpbt9m Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -3,6 +3,8 @@ def add(x: int, y: int):
 
 try:
     x, y = map(int, input("Enter two integers separated by space: ").strip().split())
+    if len([x, y]) != 2:
+        raise ValueError
     print(add(x, y))
 except ValueError:
     print("Invalid input")
