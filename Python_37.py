1f0274c2a161cb67a2d83a848db13eded22c470e
beep boop <githubbot@vadim.me>
2024-07-04 23:50:38 +0200

    Solution 1 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/svhdd5de Prompt: Complete the following code given the task description and function signature.

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,3 @@
-even_indices = [l[i] for i in range(0, len(l), 2)]
-    even_indices.sort()
-    for i, val in enumerate(even_indices):
-        l[i * 2] = val
+even_indices = sorted(l[::2])
+    l[::2] = even_indices
     return l
