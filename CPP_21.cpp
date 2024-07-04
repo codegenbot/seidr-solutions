5bee0de9d545f8724c46b0d8371ca85d018986a9
beep boop <githubbot@vadim.me>
2024-07-04 23:55:58 +0200

    Solution 13 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/tais8s7b Prompt:       |                             ^~~~~~ a4fb862f-e0fd-4493-a9ac-cf05985497b2.cpp:32:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from a4fb862f-e0fd-4493-a9ac-cf05985497b2.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from a4fb862f-e0fd-4493-a9ac-cf05985497b2.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a4fb862f-e0fd-4493-a9ac-cf05985497b2.cpp:32:36: error: expected primary-expressi on before ‘float’    32 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ a4fb862f-e0fd-4493-a9ac-cf05985497b2.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ a4fb862f-e0fd-4493-a9ac-cf05985497b2.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -6,7 +6,7 @@
 
 std::vector<float> rescale_to_unit(std::vector<float> numbers) {
     float min_val = *min_element(numbers.begin(), numbers.end());
-    float max_val = *min_element(numbers.begin(), numbers.end());
+    float max_val = *max_element(numbers.begin(), numbers.end());
     for (float& num : numbers) {
         num = (num - min_val) / (max_val - min_val);
     }
