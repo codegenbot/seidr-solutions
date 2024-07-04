946716fe79b905c33a9b24358d6946ba9e869bf4
beep boop <githubbot@vadim.me>
2024-07-04 23:45:31 +0200

    Solution 81 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/9akgjbkq Prompt: 90cf7f67-f769-49e2-997b-8031ae4a067c.cpp:32:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    32 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 90cf7f67-f769-49e2-997b-8031ae4a067c.cpp:21:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    21 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 90cf7f67-f769-49e2-997b-8031ae4a067c.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 90cf7f67-f769-49e2-997b-8031ae4a067c.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -23,7 +23,7 @@ bool issame(vector<int> a, vector<int> b) {
 }
 
 int main() {
-    assert(issame(factorize(18), {2, 3, 3}));
+    assert(issame(factorize(3 * 2 * 3), {2, 3, 3}));
     cout << "Test passed!" << endl;
     return 0;
 }
