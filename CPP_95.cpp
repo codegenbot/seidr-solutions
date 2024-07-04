4d852f9ad9f19979ede6f3ed1e0872bdcef9c0ad
beep boop <githubbot@vadim.me>
2024-07-05 01:18:53 +0200

    Solution 2 of CPP/95, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/kgj3cwkw Prompt:       |                      ^~~ 0bed4428-a9c6-40f1-b676-de9eb0d87ebd.cpp:3:26: error: ‘string’ was not declared in this scope     3 | bool check_dict_case(map<string, string> dict) {       |                          ^~~~~~ 0bed4428-a9c6-40f1-b676-de9eb0d87ebd.cpp:3:34: error: ‘string’ was not declared in this scope     3 | bool check_dict_case(map<string, string> dict) {       |                                  ^~~~~~ 0bed4428-a9c6-40f1-b676-de9eb0d87ebd.cpp:3:42: error: ‘dict’ was not declared in  this scope     3 | bool check_dict_case(map<string, string> dict) {       |                                          ^~~~ 0bed4428-a9c6-40f1-b676-de9eb0d87ebd.cpp:3:46: error: expression list treated as  compound expression in initializer [-fpermissive]     3 | bool check_dict_case(map<string, string> dict) {       |                                              ^ In file included from 0bed4428-a9c6-40f1-b676-de9eb0d87ebd.cpp:21: 0bed4428-a9c6-40f1-b676-de9eb0d87ebd.cpp: In function ‘int main()’: 0bed4428-a9c6-40f1-b676-de9eb0d87ebd.cpp:23:28: error: ‘check_dict_case’ cannot be used as a function    23 |     assert (check_dict_case({}) == false);       |             ~~~~~~~~~~~~~~~^~~~

--- CPP_95.cpp
+++ CPP_95.cpp
@@ -1,6 +1,8 @@
 #include <cctype>
+#include <map>
+#include <string>
 
-bool check_dict_case(map<string, string> dict) {
+bool check_dict_case(std::map<std::string, std::string> dict) {
     if (dict.empty()) return false;
     
     bool isLower = true, isUpper = true;
