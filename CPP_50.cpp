0e99c037817e15eba1c11fe8404154a0d975746f
beep boop <githubbot@vadim.me>
2024-07-05 00:00:02 +0200

    Solution 7 of CPP/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/qxnwbq9i Prompt: 7cd7c552-13a0-4ca5-a9c5-f417984c8b9f.cpp:22:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    22 |             str+=chr;       |             ^~~       |             std 7cd7c552-13a0-4ca5-a9c5-f417984c8b9f.cpp:25:15: error: expected ‘;’ before ‘enco ded_str’    25 |         string encoded_str = encode_shift(str);       |               ^~~~~~~~~~~~       |               ; In file included from 7cd7c552-13a0-4ca5-a9c5-f417984c8b9f.cpp:12: 7cd7c552-13a0-4ca5-a9c5-f417984c8b9f.cpp:26:30: error: ‘encoded_str’ was not dec lared in this scope    26 |         assert (decode_shift(encoded_str) == str);       |                              ^~~~~~~~~~~ 7cd7c552-13a0-4ca5-a9c5-f417984c8b9f.cpp:26:17: error: ‘decode_shift’ was not de clared in this scope    26 |         assert (decode_shift(encoded_str) == str);       |                 ^~~~~~~~~~~~ 7cd7c552-13a0-4ca5-a9c5-f417984c8b9f.cpp:26:46: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    26 |         assert (decode_shift(encoded_str) == str);       |                                              ^~~

--- CPP_50.cpp
+++ CPP_50.cpp
@@ -11,23 +11,10 @@ string decode_shift(string s) {
     return out;
 }
 
-// Assuming there's an encode_shift function for testing
-string encode_shift(string s) {
-    string out;
-    for (int i = 0; i < s.length(); i++) {
-        int w = ((int)s[i] + 5 - (int)'a') % 26 + (int)'a';
-        out += (char)w;
-    }
-    return out;
-}
-
 int main() {
-    string str, encoded_str;
-    cout << "Enter a string to encode and then decode: ";
+    string str;
+    cout << "Enter an encoded string to decode: ";
     cin >> str;
-    encoded_str = encode_shift(str);
-    cout << "Encoded string: " << encoded_str << endl;
-    cout << "Decoded string: " << decode_shift(encoded_str) << endl;
-
+    cout << "Decoded string: " << decode_shift(str) << endl;
     return 0;
 }
