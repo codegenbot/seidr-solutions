265b52fd65a3de8ea86b975558e56c75a024cafa
beep boop <githubbot@vadim.me>
2024-07-04 23:40:04 +0200

    Solution 4 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/lhlfp4m8 Prompt:     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9ce22baa-dbf6-4d24-8cad-080ded4a7f52.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9ce22baa-dbf6-4d24-8cad-080ded4a7f52.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9ce22baa-dbf6-4d24-8cad-080ded4a7f52.cpp:6: 9ce22baa-dbf6-4d24-8cad-080ded4a7f52.cpp: In function ‘int main()’: 9ce22baa-dbf6-4d24-8cad-080ded4a7f52.cpp:16:20: error: ‘unique’ was not declared  in this scope    16 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 9ce22baa-dbf6-4d24-8cad-080ded4a7f52.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -5,21 +5,19 @@
 
 using namespace std;
 
-// Function to remove duplicates from a sorted vector
-vector<int> unique_sorted_vector(vector<int> l) {
-    sort(l.begin(), l.end());
-    l.erase(unique(l.begin(), l.end()), l.end());
-    return l;
-}
-
-// Function to check if two vectors are the same
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
+vector<int> unique(vector<int> l) {
+    sort(l.begin(), l.end());
+    l.erase(std::unique(l.begin(), l.end()), l.end());
+    return l;
+}
+
 int main() {
-    // Test case
-    assert(issame(unique_sorted_vector({5, 3, 5, 2, 3, 3, 9, 0, 123}), {0, 2, 3, 5, 9, 123}));
+    vector<int> result = unique({5, 3, 5, 2, 3, 3, 9, 0, 123});
+    assert(issame(result, {0, 2, 3, 5, 9, 123}));
     cout << "Test passed!" << endl;
     return 0;
 }
