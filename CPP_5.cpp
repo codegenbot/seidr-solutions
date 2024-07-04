de9ef749c5e2351c4191a3e897b43a866c87a7b7
beep boop <githubbot@vadim.me>
2024-07-05 00:17:35 +0200

    Solution 53 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/ed8zfx1d Prompt: f959107e-0de9-4031-993b-0f543813e9c9.cpp:30:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    30 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ f959107e-0de9-4031-993b-0f543813e9c9.cpp:19:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    19 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ f959107e-0de9-4031-993b-0f543813e9c9.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ f959107e-0de9-4031-993b-0f543813e9c9.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -22,6 +22,8 @@ bool issame(vector<int> a, vector<int> b) {
 
 int main() {
     assert(issame(intersperse({2, 3, 4}, 0), {2, 0, 3, 0, 4}));
+    assert(issame(intersperse({1, 2, 3, 4, 5}, -1), {1, -1, 2, -1, 3, -1, 4, -1, 5}));
+    assert(issame(intersperse({}, 100), {}));
     cout << "All test cases passed!" << endl;
     return 0;
 }
