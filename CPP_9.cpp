1b80651750d9411f9b4dc7fdb9c2c9c578dd820b
beep boop <githubbot@vadim.me>
2024-07-04 23:38:17 +0200

    Solution 8 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/cvzkncgb Prompt:  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 58717a05-7ef0-445f-89d0-d6aa7fb068df.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 58717a05-7ef0-445f-89d0-d6aa7fb068df.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 58717a05-7ef0-445f-89d0-d6aa7fb068df.cpp:13: 58717a05-7ef0-445f-89d0-d6aa7fb068df.cpp: In function ‘int main()’: 58717a05-7ef0-445f-89d0-d6aa7fb068df.cpp:23:20: error: ‘rolling_max’ was not dec lared in this scope    23 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 58717a05-7ef0-445f-89d0-d6aa7fb068df.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,25 +1,16 @@
-#include <vector>
-#include <cassert>
-#include <climits>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <cassert>
 
 using namespace std;
-
-vector<int> rolling_max(vector<int> numbers);
-
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
-}
 
 vector<int> rolling_max(vector<int> numbers) {
     vector<int> result;
     int max_so_far = INT_MIN;
-    for (int num : numbers) {
-        if (num > max_so_far) {
+    for(int num : numbers) {
+        if(num > max_so_far) {
             max_so_far = num;
         }
         result.push_back(max_so_far);
@@ -27,8 +18,12 @@ vector<int> rolling_max(vector<int> numbers) {
     return result;
 }
 
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
 int main() {
     assert(issame(rolling_max({3, 2, 3, 100, 3}), {3, 3, 3, 100, 100}));
-    cout << "Test passed!" << endl;
+    cout << "Test passed." << endl;
     return 0;
 }
