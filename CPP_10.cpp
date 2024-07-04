e568ee658683d7edff6ad7030363beb40ff11220
beep boop <githubbot@vadim.me>
2024-07-04 23:32:05 +0200

    Solution 6 of CPP/10, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/g555hnve Prompt: a83b86a7-d521-42d5-a674-11cf2c31a7d1.cpp: In function ‘std::string make_palindro me(std::string)’: a83b86a7-d521-42d5-a674-11cf2c31a7d1.cpp:15:13: error: ‘reverse’ was not declare d in this scope    15 |             reverse(suffix.begin(), suffix.end());       |             ^~~~~~~ a83b86a7-d521-42d5-a674-11cf2c31a7d1.cpp: At global scope: a83b86a7-d521-42d5-a674-11cf2c31a7d1.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ a83b86a7-d521-42d5-a674-11cf2c31a7d1.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,27 +1,22 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
+#include<algorithm> // include this for reverse
+
 using namespace std;
 
-bool is_palindrome(string str) {
+bool is_palindrome(string str){
     string s(str.rbegin(), str.rend());
     return s == str;
 }
 
-string make_palindrome(string str) {
-    int n = str.size();
+string make_palindrome(string str){
+    int n = str.length();
     for (int i = n; i >= 0; --i) {
         if (is_palindrome(str.substr(0, i))) {
-            string prefix = str.substr(i);
-            reverse(prefix.begin(), prefix.end());
-            return str + prefix;
+            string suffix = str.substr(i);
+            reverse(suffix.begin(), suffix.end());
+            return str + suffix;
         }
     }
-    return str;
-}
-
-int main() {
-    string input;
-    cin >> input;
-    cout << make_palindrome(input) << endl;
-    return 0;
+    return str; // This line should never be reached
 }
