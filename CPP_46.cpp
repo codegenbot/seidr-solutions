ec87fc068046593de9a552af81e8640842278df0
beep boop <githubbot@vadim.me>
2024-07-04 23:48:22 +0200

    Solution 1 of CPP/46, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/hchy6y8b Prompt: Complete the following code given the task description and function signature.

--- CPP_46.cpp
+++ CPP_46.cpp
@@ -6,7 +6,7 @@ int fib4(int n) {
     if (n == 1) return 0;
     if (n == 2) return 2;
     if (n == 3) return 0;
-    
+
     int a = 0, b = 0, c = 2, d = 0, e;
     for (int i = 4; i <= n; ++i) {
         e = a + b + c + d;
