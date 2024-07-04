f7cf3a84e20b073301a4f6903e833e085ad66b89
beep boop <githubbot@vadim.me>
2024-07-05 01:30:53 +0200

    Solution 0 of Python/95, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/gfafonx2 Prompt: Complete the following code given the task description and function signature.

---
+++ Python_95.py
@@ -0,0 +1,6 @@
+def check_dict_case(dict):
+    if not dict:
+        return False
+    all_lower = all(isinstance(k, str) and k.islower() for k in dict)
+    all_upper = all(isinstance(k, str) and k.isupper() for k in dict)
+    return all_lower or all_upper
