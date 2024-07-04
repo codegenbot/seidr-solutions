26bcc5032e7ed58429bef674755f14db7851d376
beep boop <githubbot@vadim.me>
2024-07-04 23:49:23 +0200

    Solution 12 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/t2ts6cf1 Prompt: f63b8eec-3458-4a3a-a306-3698c7837782.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ f63b8eec-3458-4a3a-a306-3698c7837782.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -15,7 +15,6 @@ string decode_cyclic(string s) {
 }
 
 string encode_cyclic(const string& s) {
-    // Assuming encode_cyclic reverses the decode_cyclic operation
     int l = s.length();
     string x, output;
     for (int i = 0; i < l; i += 3) {
@@ -34,6 +33,5 @@ int main() {
     }
     string encoded_str = encode_cyclic(str);
     assert(decode_cyclic(encoded_str) == str);
-    cout << encoded_str << endl;
     return 0;
 }
