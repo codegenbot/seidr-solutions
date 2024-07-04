29da1c658af68177b8f5278b70ed0fbf53d00ee2
beep boop <githubbot@vadim.me>
2024-07-04 23:58:07 +0200

    Solution 56 of CPP/48, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/ptxq1yn7 Prompt: ddfabf8f-dae1-4e8e-a2a9-d5e8885066e1.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ ddfabf8f-dae1-4e8e-a2a9-d5e8885066e1.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_48.cpp
+++ CPP_48.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 bool is_palindrome(const string& text) {
     int n = text.length();
-    for (int i = 0; i < n / 2; ++i) {
-        if (text[i] != text[n - i - 1]) {
+    for(int i = 0; i < n / 2; ++i) {
+        if(text[i] != text[n - i - 1]) {
             return false;
         }
     }
