7d81caf0330f5e07457011e59e90866509f85e4b
beep boop <githubbot@vadim.me>
2024-07-04 23:45:32 +0200

    Solution 15 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/az4qc4mx Prompt: 708c9fdc-8dd4-499c-95c9-e54caaf5799e.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 708c9fdc-8dd4-499c-95c9-e54caaf5799e.cpp:28: 708c9fdc-8dd4-499c-95c9-e54caaf5799e.cpp: In function ‘int main()’: 708c9fdc-8dd4-499c-95c9-e54caaf5799e.cpp:38:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    38 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 708c9fdc-8dd4-499c-95c9-e54caaf5799e.cpp:18:6: note: candidate: ‘bool issame(con st std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx 11::basic_string<char> >&)’    18 | bool issame(const vector<string>& a, const vector<string>& b) {       |      ^~~~~~ 708c9fdc-8dd4-499c-95c9-e54caaf5799e.cpp:29:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    29 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -16,7 +16,7 @@ vector<string> filter_by_substring(const vector<string>& strings, const string& 
 }
 
 int main() {
-    assert (filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run") == vector<string>{"grunt", "prune"});
+    assert(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run") == vector<string>{"grunt", "prune"});
     cout << "All tests passed!" << endl;
     return 0;
 }
