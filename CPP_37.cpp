0b307578e9a08226e2d4fada665f1c081adb3d20
beep boop <githubbot@vadim.me>
2024-07-05 01:23:39 +0200

    Solution 20 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/aj9c7yf4 Prompt:    92 | #  define assert(expr) \       | b33989d3-5333-43f0-88ef-c263edfd04b8.cpp: In function ‘int main()’: b33989d3-5333-43f0-88ef-c263edfd04b8.cpp:20:5: error: ‘assert’ was not declared in this scope    20 |     assert(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) == vector<fl oat>{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10});       |     ^~~~~~ b33989d3-5333-43f0-88ef-c263edfd04b8.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <cassert>   +++ |+#include <cassert>     4 | b33989d3-5333-43f0-88ef-c263edfd04b8.cpp: At global scope: b33989d3-5333-43f0-88ef-c263edfd04b8.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ b33989d3-5333-43f0-88ef-c263edfd04b8.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
@@ -17,6 +18,7 @@ vector<float> sort_even(vector<float> l) {
 }
 
 int main() {
-    assert((sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) == vector<float>{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    assert(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) == vector<float>{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10});
+    
     return 0;
 }
