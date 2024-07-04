1824cc025c731371dda85b4184dd773cc6dd04ea
beep boop <githubbot@vadim.me>
2024-07-05 01:59:20 +0200

    Solution 22 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/wapkogx6 Prompt: 5ad75c17-f581-43c0-9a5f-23ae069e01da.cpp:37:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    37 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 5ad75c17-f581-43c0-9a5f-23ae069e01da.cpp:27:6: note: ‘bool issame(std::vector<st d::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> > )’ previously defined here    27 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ 5ad75c17-f581-43c0-9a5f-23ae069e01da.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ 5ad75c17-f581-43c0-9a5f-23ae069e01da.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -27,8 +27,3 @@ vector<string> select_words(string s, int n) {
 bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
-
-int main() {
-    assert(issame(select_words("a b c d e f", 1), {"b", "c", "d", "f"}));
-    return 0;
-}
