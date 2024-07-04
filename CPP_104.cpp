ab847cde6e873378730d0284769d89b46b53b688
beep boop <githubbot@vadim.me>
2024-07-05 01:31:15 +0200

    Solution 61 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/0ws8m6mr Prompt: 0211aa91-449b-406a-bc40-1b68365fa6bf.cpp:39:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    39 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 0211aa91-449b-406a-bc40-1b68365fa6bf.cpp:29:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    29 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 0211aa91-449b-406a-bc40-1b68365fa6bf.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 0211aa91-449b-406a-bc40-1b68365fa6bf.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -32,5 +32,6 @@ bool issame(vector<int> a, vector<int> b) {
 
 int main() {
     assert(issame(unique_digits({135, 103, 31}), {31, 135}));
+    cout << "All test cases passed!\n";
     return 0;
 }
