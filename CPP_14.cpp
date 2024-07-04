afe25a5c6dea222bf4f9d6279e4f190acc144d78
beep boop <githubbot@vadim.me>
2024-07-04 23:34:42 +0200

    Solution 6 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/j93rg9hf Prompt:       |                              ^~~~~~ 6e8b2b2a-4efe-422a-a4e8-c0d9927cee7b.cpp:9:37: error: ‘string’ was not declared in this scope     9 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 6e8b2b2a-4efe-422a-a4e8-c0d9927cee7b.cpp:9:44: error: ‘b’ was not declared in th is scope     9 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6e8b2b2a-4efe-422a-a4e8-c0d9927cee7b.cpp:9:45: error: expression list treated as  compound expression in initializer [-fpermissive]     9 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 6e8b2b2a-4efe-422a-a4e8-c0d9927cee7b.cpp:8: 6e8b2b2a-4efe-422a-a4e8-c0d9927cee7b.cpp: In function ‘int main()’: 6e8b2b2a-4efe-422a-a4e8-c0d9927cee7b.cpp:18:20: error: ‘all_prefixes’ was not de clared in this scope    18 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ 6e8b2b2a-4efe-422a-a4e8-c0d9927cee7b.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 vector<string> all_prefixes(const string& str) {
     vector<string> result;
-    for (int i = 1; i <= str.size(); ++i) {
+    for (size_t i = 1; i <= str.size(); ++i) {
         result.push_back(str.substr(0, i));
     }
     return result;
@@ -18,7 +18,7 @@ bool issame(const vector<string>& a, const vector<string>& b) {
 }
 
 int main() {
-    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
-    cout << "Test passed!" << endl;
+    assert (issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    cout << "All tests passed!" << endl;
     return 0;
 }
