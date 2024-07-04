f54e9e3150b6ca2040755b5c62159fb4d023cb7f
beep boop <githubbot@vadim.me>
2024-07-05 00:17:18 +0200

    Solution 41 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/t7tnyuo5 Prompt: a8c258dc-498d-4b7e-93f5-253fd6e23a4e.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ a8c258dc-498d-4b7e-93f5-253fd6e23a4e.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from a8c258dc-498d-4b7e-93f5-253fd6e23a4e.cpp:30: a8c258dc-498d-4b7e-93f5-253fd6e23a4e.cpp: In function ‘int main()’: a8c258dc-498d-4b7e-93f5-253fd6e23a4e.cpp:40:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    40 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ a8c258dc-498d-4b7e-93f5-253fd6e23a4e.cpp:20:6: note: candidate: ‘bool issame(con st std::vector<int>&, const std::vector<int>&)’    20 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ a8c258dc-498d-4b7e-93f5-253fd6e23a4e.cpp:31:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    31 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -22,7 +22,7 @@ bool issame(const vector<int>& a, const vector<int>& b) {
 }
 
 int main() {
-    assert(issame(filter_integers({any(3), any('c'), any(3), any(3), any('a'), any('b')}), {3, 3, 3}));
+    assert(issame(filter_integers({any(3), any('c'), any(3), any(3), any('a'), any('b')})), {3, 3, 3});
     cout << "Test passed!" << endl;
     return 0;
 }
