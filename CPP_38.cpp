59d2318ada7f8b00a7c952dd81b8debcf04e44ed
beep boop <githubbot@vadim.me>
2024-07-04 23:48:14 +0200

    Solution 9 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/t2ts6cf1 Prompt: f227ccee-db3c-445d-8776-3de07b94cfe2.cpp:22:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    22 |             str+=chr;       |             ^~~       |             std f227ccee-db3c-445d-8776-3de07b94cfe2.cpp:25:15: error: expected ‘;’ before ‘enco ded_str’    25 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from f227ccee-db3c-445d-8776-3de07b94cfe2.cpp:12: f227ccee-db3c-445d-8776-3de07b94cfe2.cpp:26:31: error: ‘encoded_str’ was not dec lared in this scope    26 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ f227ccee-db3c-445d-8776-3de07b94cfe2.cpp:26:17: error: ‘decode_cyclic’ was not d eclared in this scope    26 |         assert (decode_cyclic(encoded_str) == str);       |                 ^~~~~~~~~~~~~ f227ccee-db3c-445d-8776-3de07b94cfe2.cpp:26:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    26 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -2,7 +2,7 @@
 #include <cassert>
 using namespace std;
 
-string decode_cyclic(string s){ 
+string decode_cyclic(string s) { 
     int l = s.length();
     string x, output;
     for (int i = 0; i * 3 < l; i++) {
@@ -13,14 +13,20 @@ string decode_cyclic(string s){
     return output;
 }
 
-// Assuming there is a function `encode_cyclic` which is not given in the task.
-// For completeness, the function signature should be something like the following:
-string encode_cyclic(string s);
+string encode_cyclic(string s) {
+    int l = s.length();
+    string x, output;
+    for (int i = 0; i * 3 < l; i++) {
+        x = s.substr(i * 3, 3);
+        if (x.length() == 3) x = x.substr(1, 2) + x[0];
+        output += x;
+    }
+    return output;
+}
 
 int main() {
-    string str = "example"; // Sample string input
-    string encoded_str = encode_cyclic(str); // This function should be defined
+    string str = "example";
+    string encoded_str = encode_cyclic(str);
     assert(decode_cyclic(encoded_str) == str);
-
     return 0;
 }
