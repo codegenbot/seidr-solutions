7a023769d56f6b1920cc984dc337ca3a852bd277
beep boop <githubbot@vadim.me>
2024-07-06 01:57:24 +0200

    Solution 14 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/dx5s3j3u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4085ca23-b6ad-4ab3-9690-d78d2fd71 8bb.py", line 16, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4085ca23-b6ad-4ab3-9690-d78d2fd71 8bb.py", line 13, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4085ca23-b6ad-4ab3-9690-d78d2fd71 8bb.py", line 9, in max_fill     return math.ceil(water / capacity)            ^^^^ NameError: name 'math' is not defined

--- Python_115.py
+++ Python_115.py
@@ -1,11 +1,11 @@
+
 def max_fill(grid, capacity):
     rows = len(grid)
     cols = len(grid[0])
     water = 0
-    containers = 0
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
                 water += 1
-                containers += 1
-    return containers
+    return math.ceil(water / capacity)
+import math
