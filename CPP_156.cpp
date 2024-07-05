d56b4081a31c75cdf19846f994bec55acc43c518
beep boop <githubbot@vadim.me>
2024-07-05 13:27:53 +0200

    Solution 9 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/o6gvf2gc Prompt: 8f40baa4-7382-4c57-bfb9-e0f40be16844.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int number) {       | ^~~~~~ In file included from 8f40baa4-7382-4c57-bfb9-e0f40be16844.cpp:17: 8f40baa4-7382-4c57-bfb9-e0f40be16844.cpp: In function ‘int main()’: 8f40baa4-7382-4c57-bfb9-e0f40be16844.cpp:19:13: error: ‘int_to_mini_romank’ was not declared in this scope    19 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -23,9 +23,10 @@ string int_to_mini_roman(int number) {
 
 int main() {
     assert(int_to_mini_roman(1000) == "m");
-    assert(int_to_mini_roman(9) == "ix");
-    assert(int_to_mini_roman(58) == "lviii");
-    assert(int_to_mini_roman(1994) == "mcmxciv");
+    assert(int_to_mini_roman(1990) == "mcmxc");
+    assert(int_to_mini_roman(1666) == "mdclxvi");
+    assert(int_to_mini_roman(4) == "iv");
+    assert(int_to_mini_roman(44) == "xliv");
     cout << "All test cases pass." << endl;
     return 0;
 }
