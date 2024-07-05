ecbff5c79ef37a7fbe17697486d7f2f0c21c964e
beep boop <githubbot@vadim.me>
2024-07-06 01:54:35 +0200

    Solution 72 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/lyk6vo6c Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1d1e4b58-a2c6-416a-984b-7fcde4635 b33.py", line 16, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1d1e4b58-a2c6-416a-984b-7fcde4635 b33.py", line 13, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1d1e4b58-a2c6-416a-984b-7fcde4635 b33.py", line 7, in prime_fib     next_fib = (fib[i - 1] + fib[i - 2]) % 2                 ~~~^^^^^^^ IndexError: list index out of range

--- Python_39.py
+++ Python_39.py
@@ -1,13 +1,8 @@
-import math
-
-
 def prime_fib(n):
     fib = [0, 1]
     i = 2
     while len(fib) <= n:
-        next_fib = (fib[i - 1] + fib[i - 2]) % 2
-        if math.gcd(next_fib, 2) == 1 and i < len(fib):
-            fib.append(next_fib)
-        else:
-            fib.pop()
+        if math.gcd(i - 1, 2) == 1 and i < len(fib):
+            fib.append((fib[i - 1] + fib[i - 2]) % 2)
+        i += 1
     return fib[n]
