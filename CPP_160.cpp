0b31552e098648b90a3f1eb716abfbd4961d554b
beep boop <githubbot@vadim.me>
2024-07-05 09:38:23 +0200

    Solution 54 of CPP/160, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/75rk86nm Prompt: Program failed

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -14,10 +14,10 @@ int do_algebra(vector<string> operato, vector<int> operand) {
             result -= operand[i + 1];
         } else if (operato[i] == "*") {
             result *= operand[i + 1];
-        } else if (operato[i] == "//") {
+        } else if (operato[i] == "/") {
             result /= operand[i + 1];
         } else if (operato[i] == "**") {
-            result = pow(result, operand[i + 1]);
+            result = static_cast<int>(pow(result, operand[i + 1]));
         }
     }
     return result;
