29b9ee390ddaf12952d7c0870a8cc15df924b11a
beep boop <githubbot@vadim.me>
2024-07-06 00:52:45 +0200

    Solution 35 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/a39d9ij4 Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -2,8 +2,8 @@
 import math
 def find_zero(xs: list):
     if len(xs) < 2:
-        return -xs[1] / xs[0]
+        return -1/(xs[0] + xs[1])
     elif len(xs) == 2:
         return -xs[1] / xs[0]
     else:
-        return (-xs[2] + math.sqrt((xs[1] ** 2) - (4 * xs[0] * xs[-1]))) / (2 * xs[0])
+        return (-xs[-2] + math.sqrt((xs[-1]**2) - (4 * xs[0] * xs[-3]))) / (2 * xs[0])
