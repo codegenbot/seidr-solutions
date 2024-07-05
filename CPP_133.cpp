e6a03c7f19d99f1807511bde77d98c766a9ccd45
beep boop <githubbot@vadim.me>
2024-07-05 06:12:16 +0200

    Solution 3 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/s1fihlwh Prompt: Complete the following code given the task description and function signature.

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,7 +1,7 @@
 int sum = 0;
     for (float num : lst) {
-        int ceil_num = ceil(num);
-        sum += ceil_num * ceil_num;
+        int rounded_num = ceil(num);
+        sum += rounded_num * rounded_num;
     }
     return sum;
 }
