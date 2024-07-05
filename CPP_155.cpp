a00d96d937659a7d6d223fe098449b2b8eeeb0d6
beep boop <githubbot@vadim.me>
2024-07-05 08:42:29 +0200

    Solution 22 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/2mcf6rcs Prompt:       |                    ^~~ 37697021-6cd8-4a07-b777-143c74bf073b.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 37697021-6cd8-4a07-b777-143c74bf073b.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 37697021-6cd8-4a07-b777-143c74bf073b.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 37697021-6cd8-4a07-b777-143c74bf073b.cpp:16: 37697021-6cd8-4a07-b777-143c74bf073b.cpp: In function ‘int main()’: 37697021-6cd8-4a07-b777-143c74bf073b.cpp:26:20: error: ‘even_odd_count’ was not declared in this scope    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 37697021-6cd8-4a07-b777-143c74bf073b.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -5,15 +5,19 @@
 
 std::vector<int> even_odd_count(int num) {
     int even_count = 0, odd_count = 0;
-    num = abs(num);
-    while (num > 0) {
-        int digit = num % 10;
-        if (digit % 2 == 0) {
-            even_count++;
-        } else {
-            odd_count++;
+    if (num == 0) {
+        even_count = 1;
+    } else {
+        num = abs(num);
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit % 2 == 0) {
+                even_count++;
+            } else {
+                odd_count++;
+            }
+            num /= 10;
         }
-        num /= 10;
     }
     return {even_count, odd_count};
 }
