1727bdf97e38f965c08464f59599b72a86a09893
beep boop <githubbot@vadim.me>
2024-07-05 04:43:40 +0200

    Solution 57 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/wo47la32 Prompt: 11ca36bb-f9c2-43db-9130-fb746994b6ae.cpp:16:5: error: redefinition of ‘int main( )’    16 | int main(){       |     ^~~~ 11ca36bb-f9c2-43db-9130-fb746994b6ae.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~ In file included from 11ca36bb-f9c2-43db-9130-fb746994b6ae.cpp:15: 11ca36bb-f9c2-43db-9130-fb746994b6ae.cpp: In function ‘int main()’: 11ca36bb-f9c2-43db-9130-fb746994b6ae.cpp:17:13: error: ‘strlen’ was not declared  in this scope    17 |     assert (strlen("asdasnakj") == 9);       |             ^~~~~~ 11ca36bb-f9c2-43db-9130-fb746994b6ae.cpp:16:1: note: ‘strlen’ is defined in head er ‘<cstring>’; did you forget to ‘#include <cstring>’?    15 | #include<assert.h>   +++ |+#include <cstring>    16 | int main(){

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include <cassert>
 using namespace std;
 
@@ -9,6 +8,5 @@ int my_strlen(string str) {
 
 int main() {
     assert(my_strlen("asdasnakj") == 9);
-    cout << "All tests passed!" << endl;
     return 0;
 }
