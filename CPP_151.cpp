4f29bc392a984799de3b837507f3b162af1ef460
beep boop <githubbot@vadim.me>
2024-07-05 03:03:57 +0200

    Solution 24 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/8agnmp57 Prompt: 6b2b4c3a-1d3a-4b04-ad3f-b1bfa6dafa15.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 6b2b4c3a-1d3a-4b04-ad3f-b1bfa6dafa15.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 6b2b4c3a-1d3a-4b04-ad3f-b1bfa6dafa15.cpp:27: 6b2b4c3a-1d3a-4b04-ad3f-b1bfa6dafa15.cpp: In function ‘int main()’: 6b2b4c3a-1d3a-4b04-ad3f-b1bfa6dafa15.cpp:29:35: error: ‘lst’ was not declared in  this scope    29 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 6b2b4c3a-1d3a-4b04-ad3f-b1bfa6dafa15.cpp:29:43: error: ‘odd_sum’ was not declare d in this scope    29 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-long long double_the_difference(vector<float> lst) {
+long long double_the_difference(const vector<float>& lst) {
     long long sum = 0;
     for (float num : lst) {
         if (num > 0 && floor(num) == num && ((int)num) % 2 != 0) {
