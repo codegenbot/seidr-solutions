b7caf3ea6e37a3a0d7721a79d3075ed776b47e2f
beep boop <githubbot@vadim.me>
2024-07-05 07:17:11 +0200

    Solution 82 of CPP/81, ratio 0.6666666666666666 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/tn1kvxta Prompt:    92 | #  define assert(expr) \       | 2a1bfbb4-cfa7-44f0-821c-0b874ddaaf83.cpp: In function ‘int main()’: 2a1bfbb4-cfa7-44f0-821c-0b874ddaaf83.cpp:29:5: error: ‘assert’ was not declared in this scope    29 |     assert(numerical_letter_grade({0, 0.7}) == vector<string>{"E", "D-"} );       |     ^~~~~~ 2a1bfbb4-cfa7-44f0-821c-0b874ddaaf83.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <cassert>   +++ |+#include <cassert>     5 | 2a1bfbb4-cfa7-44f0-821c-0b874ddaaf83.cpp: At global scope: 2a1bfbb4-cfa7-44f0-821c-0b874ddaaf83.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ 2a1bfbb4-cfa7-44f0-821c-0b874ddaaf83.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,3 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
 vector<string> numerical_letter_grade(vector<float> grades) {
     vector<string> letter_grades;
     for (float gpa : grades) {
