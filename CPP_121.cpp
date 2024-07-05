4e9da519085cd4ddb26849b74cb38774ddeaeb01
beep boop <githubbot@vadim.me>
2024-07-05 02:03:48 +0200

    Solution 5 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/wpu1oruc Prompt: db7c10c3-702c-4f2d-ac8d-0ac999bdb551.cpp:1:15: error: ‘vector’ was not declared in this scope     1 | int solutions(vector<int> lst){       |               ^~~~~~ db7c10c3-702c-4f2d-ac8d-0ac999bdb551.cpp:1:22: error: expected primary-expressio n before ‘int’     1 | int solutions(vector<int> lst){       |                      ^~~ In file included from db7c10c3-702c-4f2d-ac8d-0ac999bdb551.cpp:11: db7c10c3-702c-4f2d-ac8d-0ac999bdb551.cpp: In function ‘int main()’: db7c10c3-702c-4f2d-ac8d-0ac999bdb551.cpp:13:22: error: ‘solutions’ cannot be use d as a function    13 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 #include <cassert>
 
-int solutions(std::vector<int> lst) {
+int solutions(std::vector<int> lst){
     int sum = 0;
-    for (int i = 0; i < lst.size(); i += 2) {
-        if (lst[i] % 2 != 0) {
+    for(int i = 0; i < lst.size(); i += 2){
+        if(lst[i] % 2 != 0){
             sum += lst[i];
         }
     }
