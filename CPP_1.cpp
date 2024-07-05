44f8f2bab713e6130180b68c16996fc2f7b58e06
beep boop <githubbot@vadim.me>
2024-07-06 01:30:38 +0200

    Solution 8 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gbyn86lg Prompt:    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d8f696f9-cf3c-4fd1-bedb-77372887c387.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d8f696f9-cf3c-4fd1-bedb-77372887c387.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d8f696f9-cf3c-4fd1-bedb-77372887c387.cpp:23: d8f696f9-cf3c-4fd1-bedb-77372887c387.cpp: In function ‘int main()’: d8f696f9-cf3c-4fd1-bedb-77372887c387.cpp:33:20: error: ‘separate_paren_groups’ w as not declared in this scope    33 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ d8f696f9-cf3c-4fd1-bedb-77372887c387.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,9 +1,13 @@
 #include <vector>
 #include <string>
 
-std::vector<std::string> separate_paren_groups(std::string paren_string) {
-    std::vector<std::string> result;
-    std::string current_group = "";
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
+}
+
+vector<string> separate_paren_groups(string paren_string) {
+    vector<string> result;
+    string current_group = "";
     int open_count = 0;
 
     for (char c : paren_string) {
@@ -21,4 +25,9 @@ std::vector<std::string> separate_paren_groups(std::string paren_string) {
     }
 
     return result;
+}
+
+int main() {
+    assert(separate_paren_groups("( ) (( )) (( )( ))") == {"()", "(())", "(()())"});
+    return 0;
 }
