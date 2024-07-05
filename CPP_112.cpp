b26ba0e4546f04facc500613bbad97f2fd21eb56
beep boop <githubbot@vadim.me>
2024-07-05 05:49:18 +0200

    Solution 35 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/gkkfhzeq Prompt:       |                              ^~~~~~ 491af26b-1231-4aa3-9afc-30b84623bed1.cpp:17:37: error: ‘string’ was not declared  in this scope    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 491af26b-1231-4aa3-9afc-30b84623bed1.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 491af26b-1231-4aa3-9afc-30b84623bed1.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 491af26b-1231-4aa3-9afc-30b84623bed1.cpp:16: 491af26b-1231-4aa3-9afc-30b84623bed1.cpp: In function ‘int main()’: 491af26b-1231-4aa3-9afc-30b84623bed1.cpp:26:20: error: ‘reverse_delete’ was not declared in this scope    26 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 491af26b-1231-4aa3-9afc-30b84623bed1.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -7,17 +7,12 @@
 using namespace std;
 
 vector<string> reverse_delete(string s, string c) {
-    // Remove characters in s that are in c
     for (char ch : c) {
         s.erase(remove(s.begin(), s.end(), ch), s.end());
     }
-
-    // Check if the resulting string is a palindrome
     string reversed_s = s;
     reverse(reversed_s.begin(), reversed_s.end());
     bool is_palindrome = (s == reversed_s);
-
-    // Return the result string and palindrome check as "True" or "False"
     return {s, is_palindrome ? "True" : "False"};
 }
 
