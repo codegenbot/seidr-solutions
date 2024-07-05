707a0c4675d1f907e617c8d1a96f1776eba94d9a
beep boop <githubbot@vadim.me>
2024-07-05 21:27:13 +0200

    Solution 38 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/jc68i5pb Prompt: 07ed1493-ea2f-46c9-b384-c7e72904c2c1.cpp:9:21: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 07ed1493-ea2f-46c9-b384-c7e72904c2c1.cpp:9:62: error: ‘endl’ is not a member of ‘std’     9 |         return std::cout << s.substr(len / 2 - 1, 2) << std::endl;       |                                                              ^~~~ 07ed1493-ea2f-46c9-b384-c7e72904c2c1.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | 07ed1493-ea2f-46c9-b384-c7e72904c2c1.cpp:11:21: error: ‘cout’ is not a member of  ‘std’    11 |         return std::cout << std::string(1, s[len / 2]) << std::endl;       |                     ^~~~ 07ed1493-ea2f-46c9-b384-c7e72904c2c1.cpp:11:21: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’? 07ed1493-ea2f-46c9-b384-c7e72904c2c1.cpp:11:64: error: ‘endl’ is not a member of  ‘std’    11 |         return std::cout << std::string(1, s[len / 2]) << std::endl;       |                                                                ^~~~ 07ed1493-ea2f-46c9-b384-c7e72904c2c1.cpp:11:64: note: ‘std::endl’ is defined in header ‘<ostream>’; did you forget to ‘#include <ostream>’?

--- middle-character.cpp
+++ middle-character.cpp
@@ -9,6 +9,6 @@ int main() {
     if (len % 2 == 0) {
         std::cout << s.substr(len / 2 - 1, 2) << std::endl;
     } else {
-        std::cout << s.substr(len / 2, 1) << std::endl;
+        std::cout << std::string(1, s[len / 2]) << std::endl;
     }
-    return 0;
+}
