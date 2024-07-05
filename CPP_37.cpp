5742045b3a6761c73f351becf316ced00a616920
beep boop <githubbot@vadim.me>
2024-07-05 04:18:31 +0200

    Solution 8 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/qosmzfzd Prompt:    14 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 87c22665-992a-4c74-a877-5cf1a6d19f25.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 87c22665-992a-4c74-a877-5cf1a6d19f25.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 87c22665-992a-4c74-a877-5cf1a6d19f25.cpp:13: 87c22665-992a-4c74-a877-5cf1a6d19f25.cpp: In function ‘int main()’: 87c22665-992a-4c74-a877-5cf1a6d19f25.cpp:23:20: error: ‘sort_even’ was not decla red in this scope    23 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 87c22665-992a-4c74-a877-5cf1a6d19f25.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -3,6 +3,17 @@
 #include <cassert>
 using namespace std;
 
+vector<float> sort_even(vector<float> l);
+
+bool issame(vector<float> a, vector<float> b) {
+    return a == b;
+}
+
+int main() {
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    return 0;
+}
+
 vector<float> sort_even(vector<float> l) {
     vector<float> even_indices;
     for (size_t i = 0; i < l.size(); i += 2) {
@@ -13,13 +24,4 @@ vector<float> sort_even(vector<float> l) {
         l[i] = even_indices[j];
     }
     return l;
-}
-
-bool issame(const vector<float>& a, const vector<float>& b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
-    return 0;
 }
