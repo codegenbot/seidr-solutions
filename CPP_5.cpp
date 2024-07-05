4b7f04a8505a4b37eba80b931916d06debff7937
beep boop <githubbot@vadim.me>
2024-07-05 03:30:15 +0200

    Solution 24 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/s4kdqtcn Prompt:       |                    ^~~ c1a47d7b-0ff4-4c03-8811-7e09419d2a48.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c1a47d7b-0ff4-4c03-8811-7e09419d2a48.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c1a47d7b-0ff4-4c03-8811-7e09419d2a48.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c1a47d7b-0ff4-4c03-8811-7e09419d2a48.cpp:14: c1a47d7b-0ff4-4c03-8811-7e09419d2a48.cpp: In function ‘int main()’: c1a47d7b-0ff4-4c03-8811-7e09419d2a48.cpp:24:20: error: ‘intersperse’ was not dec lared in this scope    24 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ c1a47d7b-0ff4-4c03-8811-7e09419d2a48.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-vector<int> intersperse(vector<int> numbers, int delimiter) {
+vector<int> intersperse(vector<int> numbers, int delimeter) {
     vector<int> result;
     if (numbers.empty()) return result;
 
     for (size_t i = 0; i < numbers.size(); ++i) {
         result.push_back(numbers[i]);
         if (i != numbers.size() - 1) {
-            result.push_back(delimiter);
+            result.push_back(delimeter);
         }
     }
     return result;
@@ -23,6 +23,6 @@ bool issame(vector<int> a, vector<int> b) {
 
 int main() {
     assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
-    cout << "All tests passed!" << endl;
+    cout << "Test passed!" << endl;
     return 0;
 }
