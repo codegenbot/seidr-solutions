969fa2b7562903d3e5f6431b896cfd685a479b99
beep boop <githubbot@vadim.me>
2024-07-06 00:46:15 +0200

    Solution 36 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tpufcn5h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ea8152fe-03cb-435a-a1a7-3fd4d0bef b2c.py", line 25, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ea8152fe-03cb-435a-a1a7-3fd4d0bef b2c.py", line 20, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -4,7 +4,14 @@ def sort_third(l):
     while i < len(l):
         if l[i] % 3 != 0:
             result.append(l.pop(i))
-        elif min(x for x in l if x % 3 != 0) is None or l[l.index(min(x for x in l if x % 3 != 0))] <= l[i]:
-            result.append(l.pop(l.index(min(x for x in l if x % 3 != 0))))
+            i -= 1
+        else:
+            j = min(len(l), i + 2) - 1
+            min_val = l[j]
+            for k in range(j, -1, -1):
+                if l[k] % 3 != 0 and (min_val is None or l[k] < min_val):
+                    min_val = l[k]
+            result.append(min_val)
+            l.remove(min_val)
         i += 1
     return tuple(result)
