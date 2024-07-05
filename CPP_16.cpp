d6b1a536253b9589024b70d4f14f239909711f1b
beep boop <githubbot@vadim.me>
2024-07-05 03:51:59 +0200

    Solution 2 of CPP/16, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/yrq6jb6m Prompt: Complete the following code given the task description and function signature.

--- CPP_16.cpp
+++ CPP_16.cpp
@@ -1,6 +1,8 @@
-#include<unordered_set>
-int count_distinct_characters(string str) {
-    unordered_set<char> distinct_chars;
+#include<set>
+#include<ctype.h>
+
+int count_distinct_characters(string str){ 
+    set<char> distinct_chars;
     for (char c : str) {
         distinct_chars.insert(tolower(c));
     }
