c713cc7b32a5e01edf61ff9ee82c9e1c1e0d07cb
beep boop <githubbot@vadim.me>
2024-07-05 05:18:44 +0200

    Solution 16 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/d3aw31ct Prompt:       |                                   ^~~~~~ 4c8a1ff6-ce5e-46ad-b997-ae0d4d09620d.cpp:18:42: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 4c8a1ff6-ce5e-46ad-b997-ae0d4d09620d.cpp:18:49: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 4c8a1ff6-ce5e-46ad-b997-ae0d4d09620d.cpp:18:56: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 4c8a1ff6-ce5e-46ad-b997-ae0d4d09620d.cpp:17: 4c8a1ff6-ce5e-46ad-b997-ae0d4d09620d.cpp: In function ‘int main()’: 4c8a1ff6-ce5e-46ad-b997-ae0d4d09620d.cpp:30:20: error: ‘get_row’ was not declare d in this scope    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 4c8a1ff6-ce5e-46ad-b997-ae0d4d09620d.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -19,14 +19,10 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
 }
 
 bool issame(vector<vector<int>> a, vector<vector<int>> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
+    return a == b;
 }
 
 int main() {
-    assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
+    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
     return 0;
 }
