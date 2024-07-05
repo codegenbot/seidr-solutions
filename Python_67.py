1ac3ebc528591a7f5a45fd7c837d36f8379918bb
beep boop <githubbot@vadim.me>
2024-07-06 01:03:55 +0200

    Solution 20 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/09qczbg5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6ddd5b18-1bc8-45c8-a96d-ed0381da8 0c4.py", line 18, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6ddd5b18-1bc8-45c8-a96d-ed0381da8 0c4.py", line 15, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6ddd5b18-1bc8-45c8-a96d-ed0381da8 0c4.py", line 9, in fruit_distribution     mango_count = n - int(apple_count) - int(orange_count)                       ^^^^^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: 'apples'

--- Python_67.py
+++ Python_67.py
@@ -1,19 +1,6 @@
-import re
-
 
 def fruit_distribution(s, n):
-    # Parse the input string using regular expressions to extract the number of apples and oranges
-    matches = re.search(r"(\d+) apples", s)
-    if not matches:
-        return 0
-    apple_count = int(matches[1])
-
-    matches = re.search(r"(\d+) oranges", s)
-    if not matches:
-        return 0
-    orange_count = int(matches[1])
-
-    # Calculate the number of mango fruits in the basket
-    mango_count = n - apple_count - orange_count
-
+    apple_count = s.count("apples")
+    orange_count = s.count("oranges")
+    mango_count = n - (apple_count + orange_count)
     return mango_count
