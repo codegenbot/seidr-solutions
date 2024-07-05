a411920b5856c51ea77910f63431dd362fbc45bd
beep boop <githubbot@vadim.me>
2024-07-05 04:41:24 +0200

    Solution 90 of CPP/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/czjxlopd Prompt: d53ea515-f00c-490e-a1ed-0cbc60be2e3f.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ d53ea515-f00c-490e-a1ed-0cbc60be2e3f.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~

--- CPP_50.cpp
+++ CPP_50.cpp
@@ -23,9 +23,10 @@ string encode_shift(string s) {
 }
 
 int main() {
-    string str = "teststring";
+    string str;
+    cin >> str;
     string encoded_str = encode_shift(str);
     assert(decode_shift(encoded_str) == str);
-    cout << "Encoding and decoding is correct." << endl;
+    cout << encoded_str << endl;
     return 0;
 }
