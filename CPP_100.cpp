a20cc186570c7890640d17cdbc2e8bbdb62aace8
beep boop <githubbot@vadim.me>
2024-07-05 05:49:57 +0200

    Solution 8 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/l30e6omo Prompt:       |                    ^~~ 2ceb1865-ead5-4d3f-a05d-06bf0952f08c.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2ceb1865-ead5-4d3f-a05d-06bf0952f08c.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2ceb1865-ead5-4d3f-a05d-06bf0952f08c.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2ceb1865-ead5-4d3f-a05d-06bf0952f08c.cpp:11: 2ceb1865-ead5-4d3f-a05d-06bf0952f08c.cpp: In function ‘int main()’: 2ceb1865-ead5-4d3f-a05d-06bf0952f08c.cpp:21:20: error: ‘make_a_pile’ was not dec lared in this scope    21 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 2ceb1865-ead5-4d3f-a05d-06bf0952f08c.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
 #include <vector>
 #include <cassert>
-
 using namespace std;
 
 vector<int> make_a_pile(int n) {
@@ -15,19 +13,10 @@ vector<int> make_a_pile(int n) {
 }
 
 bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
 int main() {
     assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
-    cout << "Test passed!" << endl;
     return 0;
 }
