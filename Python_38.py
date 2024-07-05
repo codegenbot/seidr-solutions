febdbf5dffa877778aceeaf81c6129ea51a315a2
beep boop <githubbot@vadim.me>
2024-07-06 01:27:08 +0200

    Solution 30 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/b7k2gyqn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/33fcd10a-3188-427d-a4b5-c119e0be9 0cd.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_38.py
+++ Python_38.py
@@ -1,4 +1,8 @@
 def decode_cyclic(s):
-    while "<<<" in s:
-        s = s.replace("<<<", ",,", 1)
+    while '<<<' in s:
+        s = s.replace('<<<', '')
     return s
+
+if __name__ == "__main__":
+    s = "<<<" + input()
+    print(decode_cyclic(s).replace(",,", ","))
