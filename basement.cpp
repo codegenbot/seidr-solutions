a37fa91122fa943f6672672304ac71a08bc3c312
beep boop <githubbot@vadim.me>
2024-07-05 18:56:36 +0200

    Solution 86 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/rzu35mhe Prompt: w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const int*; _Tp = int; _Alloc = std:: allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = i nt; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::allocator_type = std ::allocator<int>]’ 89c74d9b-91b9-40ed-8305-ffc673ed093c.cpp:16:52:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- basement.cpp
+++ basement.cpp
@@ -13,10 +13,10 @@ int basement(std::vector<int>& nums) {
 }
 
 int main() {
-    std::vector<int> nums1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    std::vector<int> nums1 = {-2,1,-3,4,-1,2,1,-5,4};
     std::cout << basement(nums1) << std::endl;
 
-    std::vector<int> nums2 = {0, -1, 2, -3, 1};
+    std::vector<int> nums2 = {0,-1,2,-3,1};
     std::cout << basement(nums2) << std::endl;
 
     return 0;
