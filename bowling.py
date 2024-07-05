35bc66cd0d87dda3ddbb2aca338fdb26d7e785ad
beep boop <githubbot@vadim.me>
2024-07-05 22:08:58 +0200

    Solution 4 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/73htyojj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b283ccba-e2a6-4130-a140-72a831fd6 52b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -4,33 +4,20 @@ def bowling_score(frames):
     frame_index = 0
     for i in range(1, 11):
         if frames[frame_index] == 'X':
-            score += 30
-            frame_index += 1
+            score += 10 + (10 - int(frames[frame_index+1].split('/')[0])) if i < 10 and frames[frame_index+1] != 'X' else 10
+            frame_index += 1 if i < 10 and frames[frame_index+1] != 'X' else 2
         elif frames[frame_index] == '/':
-            first_roll = int(frames[frame_index-1])
-            if first_roll < 10:
-                second_roll = 10 - first_roll
-            else:
-                second_roll = 10 - (10 - first_roll)
-            score += first_roll + second_roll
+            score += 10 - int(frames[frame_index].split('/')[0])
             frame_index += 2
         else:
             first_roll = int(frames[frame_index])
             if i < 10 and frames[frame_index+1] == 'X':
-                score += 10 + 30
+                score += 10 + 10
                 frame_index += 2
             elif i < 10 and frames[frame_index+1] == '/':
-                second_roll = 10 - int(frames[frame_index+1].split('/')[0])
-                if first_roll < 10:
-                    score += first_roll + second_roll
-                else:
-                    score += 10 + (10 - (10 - first_roll))
+                score += first_roll + (10 - int(frames[frame_index+1].split('/')[0]))
                 frame_index += 3
             else:
-                if first_roll < 8 or frames[frame_index+1] == 'X' or frames[frame_index+1] == '/':
-                    score += 10 + int(frames[frame_index+1])
-                else:
-                    second_roll = int(frames[frame_index+1])
-                    score += first_roll + second_roll
+                score += first_roll + int(frames[frame_index+1])
                 frame_index += 2
     return score
