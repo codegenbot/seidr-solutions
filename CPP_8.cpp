7023a8e7b3e68ed9b99dea70ab825a30b4c626c0
beep boop <githubbot@vadim.me>
2024-07-05 04:02:20 +0200

    Solution 20 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/kwkm3bpg Prompt:       |                    ^~~ f7e5ddc0-c1e1-4810-aa41-0ad19197dab1.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f7e5ddc0-c1e1-4810-aa41-0ad19197dab1.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f7e5ddc0-c1e1-4810-aa41-0ad19197dab1.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f7e5ddc0-c1e1-4810-aa41-0ad19197dab1.cpp:11: f7e5ddc0-c1e1-4810-aa41-0ad19197dab1.cpp: In function ‘int main()’: f7e5ddc0-c1e1-4810-aa41-0ad19197dab1.cpp:21:20: error: ‘sum_product’ was not dec lared in this scope    21 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ f7e5ddc0-c1e1-4810-aa41-0ad19197dab1.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,25 +1,25 @@
 #include <vector>
-#include <cassert>
 #include <iostream>
+#include <cassert> // For assert()
 
 using namespace std;
 
 vector<int> sum_product(vector<int> numbers) {
     int sum = 0;
     int product = 1;
-    for(int num : numbers) {
+    for (int num : numbers) {
         sum += num;
         product *= num;
     }
     return {sum, product};
 }
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
 int main() {
     assert(issame(sum_product({10}), {10, 10}));
-    cout << "Test passed!" << endl;
+    cout << "All tests passed!" << endl;
     return 0;
 }
