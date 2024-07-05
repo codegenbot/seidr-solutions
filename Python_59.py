5d4976fcc176ef404802eb8e333c0eae2a7be865
beep boop <githubbot@vadim.me>
2024-07-06 00:42:23 +0200

    Solution 2 of Python/59, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/1i9fnjat Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/28dca2b7-4e6d-4a42-966e-5b1aee3e8 e67.py", line 11, in <module>     check(largest_prime_factor)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/28dca2b7-4e6d-4a42-966e-5b1aee3e8 e67.py", line 8, in check     assert largest_prime_factor(13195) == 29            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_59.py
+++ Python_59.py
@@ -1,6 +1,7 @@
-
 def largest_prime_factor(n):
+    if n <= 1:
+        return None
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
-            return i
+            return largest_prime_factor(n // i)
     return n
