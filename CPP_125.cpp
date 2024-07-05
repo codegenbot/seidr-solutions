ca59aa38fe1a37b8885c06d2f70372de4ea3a550
beep boop <githubbot@vadim.me>
2024-07-05 16:28:15 +0200

    Solution 11 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/cap83lst Prompt: Complete the following code given the task description and function signature.

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,26 +1,39 @@
 vector<string> split_words(string txt) {
     vector<string> result;
-    if (txt.find(' ') != string::npos) {
-        stringstream ss(txt);
-        string word;
-        while (ss >> word) {
-            result.push_back(word);
+    size_t pos;
+    
+    // Split on whitespace
+    pos = txt.find(' ');
+    if (pos != string::npos) {
+        size_t start = 0;
+        while ((pos = txt.find(' ', start)) != string::npos) {
+            result.push_back(txt.substr(start, pos - start));
+            start = pos + 1;
         }
-    } else if (txt.find(',') != string::npos) {
-        size_t pos = 0;
-        while ((pos = txt.find(',')) != string::npos) {
-            result.push_back(txt.substr(0, pos));
-            txt.erase(0, pos + 1);
-        }
-        result.push_back(txt);
-    } else {
-        int odd_count = 0;
-        for (char ch : txt) {
-            if (islower(ch) && (ch - 'a') % 2 != 0) {
-                odd_count++;
-            }
-        }
-        result.push_back(to_string(odd_count));
+        result.push_back(txt.substr(start));
+        return result;
     }
+    
+    // Split on commas
+    pos = txt.find(',');
+    if (pos != string::npos) {
+        size_t start = 0;
+        while ((pos = txt.find(',', start)) != string::npos) {
+            result.push_back(txt.substr(start, pos - start));
+            start = pos + 1;
+        }
+        result.push_back(txt.substr(start));
+        return result;
+    }
+    
+    // No whitespaces or commas, count lower-case letters with odd order
+    int odd_count = 0;
+    for (char c : txt) {
+        if (c >= 'a' && c <= 'z' && ((c - 'a') % 2 == 1)) {
+            odd_count++;
+        }
+    }
+    
+    result.push_back(to_string(odd_count));
     return result;
 }
