1d97394662dfcab57a59174eb7b4a4debeab3a19
beep boop <githubbot@vadim.me>
2024-07-05 02:18:14 +0200

    Solution 64 of CPP/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/5bx946ze Prompt: 53201261-0a02-4520-b56f-70ba148ef6f4.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 53201261-0a02-4520-b56f-70ba148ef6f4.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_50.cpp
+++ CPP_50.cpp
@@ -6,7 +6,7 @@ using namespace std;
 string encode_shift(string s) {
     string out;
     for (int i = 0; i < s.length(); i++) {
-        int w = ((int)s[i] + 5 - 'a') % 26 + 'a';
+        int w = ((int)s[i] + 5 - (int)'a') % 26 + (int)'a';
         out += (char)w;
     }
     return out;
@@ -15,7 +15,7 @@ string encode_shift(string s) {
 string decode_shift(string s) {
     string out;
     for (int i = 0; i < s.length(); i++) {
-        int w = ((int)s[i] - 5 - 'a' + 26) % 26 + 'a';
+        int w = ((int)s[i] - 5 - (int)'a' + 26) % 26 + (int)'a';
         out += (char)w;
     }
     return out;
