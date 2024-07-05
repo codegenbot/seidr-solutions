be65a1e2de1f45deec018ee9daba46bcd1685568
beep boop <githubbot@vadim.me>
2024-07-05 10:35:43 +0200

    Solution 15 of CPP/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/iu35qovr Prompt: Complete the following code given the task description and function signature.

--- CPP_12.cpp
+++ CPP_12.cpp
@@ -1,5 +1,6 @@
-if (strings.empty()) return "None";
-    string longest_str = "";
+string longest(vector<string> strings){
+    if (strings.empty()) return "None";
+    string longest_str = strings[0];
     for (const auto& str : strings) {
         if (str.length() > longest_str.length()) {
             longest_str = str;
