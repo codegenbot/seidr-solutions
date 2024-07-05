64a67bfb935371410781741df22688d888862d68
beep boop <githubbot@vadim.me>
2024-07-05 07:54:04 +0200

    Solution 3 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/yk47lo7n Prompt: c5b504a7-689a-414d-a3cf-33c681873cd2.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int count_nums(vector<int> n){       |                ^~~~~~ c5b504a7-689a-414d-a3cf-33c681873cd2.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int count_nums(vector<int> n){       |                       ^~~ In file included from c5b504a7-689a-414d-a3cf-33c681873cd2.cpp:17: c5b504a7-689a-414d-a3cf-33c681873cd2.cpp: In function ‘int main()’: c5b504a7-689a-414d-a3cf-33c681873cd2.cpp:19:23: error: ‘count_nums’ cannot be us ed as a function    19 |     assert (count_nums({1}) == 1);       |             ~~~~~~~~~~^~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -4,16 +4,16 @@
 
 using namespace std;
 
-int count_nums(vector<int> n){
+int count_nums(vector<int> n) {
     int count = 0;
-    for(int num : n){
+    for (int num : n) {
         int sum_digits = 0;
         int temp = num;
-        while(temp != 0){
+        while (temp != 0) {
             sum_digits += temp % 10;
             temp /= 10;
         }
-        if(sum_digits > 0){
+        if (sum_digits > 0) {
             count++;
         }
     }
@@ -21,7 +21,10 @@ int count_nums(vector<int> n){
 }
 
 int main() {
-    assert(count_nums({1}) == 1);
+    assert(count_nums({1, 23, 456}) == 3);
+    assert(count_nums({0, 20, 30}) == 2);
+    assert(count_nums({100, 200, 0}) == 2);
+    assert(count_nums({0}) == 0);
     cout << "All tests passed!" << endl;
     return 0;
 }
