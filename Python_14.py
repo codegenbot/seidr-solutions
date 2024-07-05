16fe813d3f817b9a00e027518a59dd5a14e50131
beep boop <githubbot@vadim.me>
2024-07-05 06:23:44 +0200

    Solution 91 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/sfbiof79 Prompt: Program did not receive expected input

--- Python_14.py
+++ Python_14.py
@@ -1,7 +1,6 @@
 def generate_substrings(string):
     return [string[:i] for i in range(1, len(string) + 1)]
 
-
 string = input().strip()
 all_prefixes = generate_substrings(string)
 check(all_prefixes)
