69b65ab65e9eeb0da2c540e97ed31bbcbee56a3f
beep boop <githubbot@vadim.me>
2024-07-05 08:13:25 +0200

    Solution 1 of Python/140, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/aa1lagro Prompt: Complete the following code given the task description and function signature.

--- Python_140.py
+++ Python_140.py
@@ -1,5 +1,2 @@
 def fix_spaces(text):
-    text = text.replace(" ", "_")
-    while "__" in text:
-        text = text.replace("__", "-")
-    return text.replace("-", "__").replace("_", " ", 1).replace(" ", "_", 1).replace("__", "-")
+    return '-'.join('_'.join(part.split()).replace(' ', '_') for part in text.split('  '))
