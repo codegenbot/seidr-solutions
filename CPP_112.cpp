49f2b25020c8b7bae5bbf0e22036ce040d1af84a
beep boop <githubbot@vadim.me>
2024-07-05 08:03:46 +0200

    Solution 3 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/cpj08m73 Prompt:       |                              ^~~~~~ 8a1d1721-96e7-4725-bce7-3b8373ea34d2.cpp:23:37: error: ‘string’ was not declared  in this scope    23 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8a1d1721-96e7-4725-bce7-3b8373ea34d2.cpp:23:44: error: ‘b’ was not declared in t his scope    23 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8a1d1721-96e7-4725-bce7-3b8373ea34d2.cpp:23:45: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8a1d1721-96e7-4725-bce7-3b8373ea34d2.cpp:22: 8a1d1721-96e7-4725-bce7-3b8373ea34d2.cpp: In function ‘int main()’: 8a1d1721-96e7-4725-bce7-3b8373ea34d2.cpp:32:20: error: ‘reverse_delete’ was not declared in this scope    32 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 8a1d1721-96e7-4725-bce7-3b8373ea34d2.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -8,16 +8,16 @@
 using namespace std;
 
 vector<string> reverse_delete(string s, string c) {
-    unordered_set<char> to_delete(c.begin(), c.end());
+    unordered_set<char> to_delete(c.begin(), c.end());    
     string filtered;
     for (char ch : s) {
         if (to_delete.find(ch) == to_delete.end()) {
             filtered.push_back(ch);
         }
-    }
+    }    
     string reversed_filtered = filtered;
     reverse(reversed_filtered.begin(), reversed_filtered.end());
-    bool is_palindrome = (filtered == reversed_filtered);
+    bool is_palindrome = (filtered == reversed_filtered);    
     return {filtered, is_palindrome ? "True" : "False"};
 }
 
