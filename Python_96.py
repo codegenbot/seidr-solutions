4e9b25548d0c354e975190041e9f4b4aec75d7d1
beep boop <githubbot@vadim.me>
2024-07-06 01:10:50 +0200

    Solution 16 of Python/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/j5uln9v9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/abdc1ed8-1f7e-4a25-9da2-406ab0111 725.py", line 39, in <module>     check(count_up_to)           ^^^^^^^^^^^ NameError: name 'count_up_to' is not defined

--- Python_96.py
+++ Python_96.py
@@ -1,11 +1,8 @@
 def is_prime(n):
+    count_up_to = 10**n
     if n <= 1:
         return False
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
     return True
-
-
-def count_up_to(n):
-    return sum(range(n + 1))
