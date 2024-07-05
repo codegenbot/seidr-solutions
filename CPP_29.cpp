c208a36eaf558744ab70703b58a7c75e8434df7e
beep boop <githubbot@vadim.me>
2024-07-05 04:07:20 +0200

    Solution 16 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/er0zqn0o Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 47386d33-be9f-4bf0-98e9-83a54fe14b2f.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 47386d33-be9f-4bf0-98e9-83a54fe14b2f.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 47386d33-be9f-4bf0-98e9-83a54fe14b2f.cpp:11: 47386d33-be9f-4bf0-98e9-83a54fe14b2f.cpp: In function ‘int main()’: 47386d33-be9f-4bf0-98e9-83a54fe14b2f.cpp:21:20: error: ‘filter_by_prefix’ was no t declared in this scope    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 47386d33-be9f-4bf0-98e9-83a54fe14b2f.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <string>
 #include <cassert>
-
 using namespace std;
 
 vector<string> filter_by_prefix(vector<string> strings, string prefix) {
@@ -16,9 +15,11 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix) {
 }
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) return false;
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) return false;
+    if (a.size() != b.size())
+        return false;
+    for (size_t i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
+            return false;
     }
     return true;
 }
