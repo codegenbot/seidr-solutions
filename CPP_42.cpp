5a6328a3cb5ac83f81b0e956a15fa5b2915ca669
beep boop <githubbot@vadim.me>
2024-07-05 13:03:38 +0200

    Solution 48 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/jb1e3lhe Prompt:     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 570a3302-78de-4035-a4bd-3924231abcd5.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 570a3302-78de-4035-a4bd-3924231abcd5.cpp:9:39: error: expression list treated as  compound expression in initializer [-fpermissive]     9 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 570a3302-78de-4035-a4bd-3924231abcd5.cpp:8: 570a3302-78de-4035-a4bd-3924231abcd5.cpp: In function ‘int main()’: 570a3302-78de-4035-a4bd-3924231abcd5.cpp:18:20: error: ‘incr_list’ was not decla red in this scope    18 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ 570a3302-78de-4035-a4bd-3924231abcd5.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 vector<int> incr_list(vector<int> l) {
-    for(int &num : l) {
-        num += 1;
+    for(int &i : l) {
+        i++;
     }
     return l;
 }
@@ -17,6 +17,6 @@ bool issame(vector<int> a, vector<int> b) {
 
 int main() {
     assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
-    cout << "All tests passed!" << endl;
+    cout << "Test passed!" << endl;
     return 0;
 }
