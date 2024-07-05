4069511ec711268a778adfadb086479a7dc00d83
beep boop <githubbot@vadim.me>
2024-07-05 12:17:43 +0200

    Solution 34 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/o6crh1za Prompt: 67aae7d8-28c0-4f92-bcf0-3fc03efe2c82.cpp:60:5: error: redefinition of ‘int main( )’    60 | int main(){       |     ^~~~ 67aae7d8-28c0-4f92-bcf0-3fc03efe2c82.cpp:39:5: note: ‘int main()’ previously def ined here    39 | int main() {       |     ^~~~ In file included from 67aae7d8-28c0-4f92-bcf0-3fc03efe2c82.cpp:59: 67aae7d8-28c0-4f92-bcf0-3fc03efe2c82.cpp: In function ‘int main()’: 67aae7d8-28c0-4f92-bcf0-3fc03efe2c82.cpp:61:13: error: ‘boost’ has not been decl ared    61 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 67aae7d8-28c0-4f92-bcf0-3fc03efe2c82.cpp:61:35: error: expected primary-expressi on before ‘>’ token    61 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -20,8 +20,6 @@ std::any compare_one(std::any a, std::any b) {
         valA = std::any_cast<float>(a);
     else if (a.type() == typeid(string))
         valA = convertToDouble(std::any_cast<string>(a));
-    else
-        throw invalid_argument("Unsupported type for a");
 
     if (b.type() == typeid(int))
         valB = std::any_cast<int>(b);
@@ -29,8 +27,6 @@ std::any compare_one(std::any a, std::any b) {
         valB = std::any_cast<float>(b);
     else if (b.type() == typeid(string))
         valB = convertToDouble(std::any_cast<string>(b));
-    else
-        throw invalid_argument("Unsupported type for b");
 
     if (valA > valB)
         return a;
@@ -48,6 +44,14 @@ int main() {
         cout << std::any_cast<string>(result) << endl;
     else
         cout << std::any_cast<double>(result) << endl;
+
+    auto result2 = compare_one(string("1"), 1);
+    if (result2.type() == typeid(int))
+        cout << std::any_cast<int>(result2) << endl;
+    else if (result2.type() == typeid(string))
+        cout << std::any_cast<string>(result2) << endl;
+    else
+        cout << std::any_cast<double>(result2) << endl;
 
     return 0;
 }
