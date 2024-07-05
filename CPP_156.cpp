26c8af82dd5f4c762f1d3b1e83787cc2b1cf1145
beep boop <githubbot@vadim.me>
2024-07-05 11:16:47 +0200

    Solution 64 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/ruuj88en Prompt: 1957166d-6a7c-4c0a-9fb1-382a5f57ff50.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 1957166d-6a7c-4c0a-9fb1-382a5f57ff50.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from 1957166d-6a7c-4c0a-9fb1-382a5f57ff50.cpp:29: 1957166d-6a7c-4c0a-9fb1-382a5f57ff50.cpp: In function ‘int main()’: 1957166d-6a7c-4c0a-9fb1-382a5f57ff50.cpp:31:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    31 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-string int_to_mini_roman(int number) {
+string int_to_mini_roman(int number){
     vector<pair<int, string>> roman = {
         {1000, "m"}, {900, "cm"}, {500, "d"}, {400, "cd"},
         {100, "c"}, {90, "xc"}, {50, "l"}, {40, "xl"},
