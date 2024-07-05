55e358971fc8a734ea492e700fe2e943bbc60f7c
beep boop <githubbot@vadim.me>
2024-07-06 01:47:22 +0200

    Solution 62 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/t2jqbixf Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -1,4 +1,6 @@
-
 def decode_shift(message):
-    message = int(click.prompt('Enter the message to be decoded', default=''))
-    return ''.join([chr((ord(ch) - shift - ord('a')) % 26 + ord('a')) for ch in message])
+    message = int(click.prompt("Enter the message to be decoded", default=""))
+    shift = int(click.prompt("Enter the number of characters to shift", default=0))
+    return "".join(
+        [chr((ord(ch) - shift - ord("a")) % 26 + ord("a")) for ch in message]
+    )
