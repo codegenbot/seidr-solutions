6e6ecc3842860cc80f97926bb270cf00b5bddcf0
beep boop <githubbot@vadim.me>
2024-07-05 02:32:17 +0200

    Solution 9 of CPP/54, ratio 0.7142857142857143 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/3g6oefsm Prompt: c6baac27-38f5-42b3-835d-67e7401f0503.cpp:1:17: error: ‘string’ was not declared in this scope     1 | bool same_chars(string s0, string s1){       |                 ^~~~~~ c6baac27-38f5-42b3-835d-67e7401f0503.cpp:1:28: error: ‘string’ was not declared in this scope     1 | bool same_chars(string s0, string s1){       |                            ^~~~~~ c6baac27-38f5-42b3-835d-67e7401f0503.cpp:1:37: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool same_chars(string s0, string s1){       |                                     ^ In file included from c6baac27-38f5-42b3-835d-67e7401f0503.cpp:9: c6baac27-38f5-42b3-835d-67e7401f0503.cpp: In function ‘int main()’: c6baac27-38f5-42b3-835d-67e7401f0503.cpp:11:23: error: ‘same_chars’ cannot be us ed as a function    11 |     assert (same_chars("aabb", "aaccc") == false);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <algorithm>
 
-bool same_chars(std::string s0, std::string s1) {
+bool same_chars(std::string s0, std::string s1){
     std::sort(s0.begin(), s0.end());
     s0.erase(std::unique(s0.begin(), s0.end()), s0.end());
     std::sort(s1.begin(), s1.end());
