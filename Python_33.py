2f6fa4c6cae55658eb5c1b27e1acdb422b5b9b28
beep boop <githubbot@vadim.me>
2024-07-06 00:39:38 +0200

    Solution 6 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tpufcn5h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/834f3516-2641-46d4-ad7f-a8fb756d2 9a8.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/834f3516-2641-46d4-ad7f-a8fb756d2 9a8.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,11 +1,2 @@
-def sort_third(l):
-    return [
-        (
-            i
-            if i % 3 != 0
-            else sorted([x for x in l[: i // 3 * 3 + i % 3 :] if x % 3 != 0])[-1]
-        )
-        if i // 3 * 3 + i % 3 < len(l)
-        else pass
-        for i in range(len(l))
-    ]
+def sort_third(l: list):
+    return [i if i % 3 != 0 else (sorted([x for x in l if x % 3 == 0])[0] if [x for x in l if x % 3 == 0] else None) for i in l]
