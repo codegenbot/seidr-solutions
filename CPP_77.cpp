1338ee2a721a1029cf77c9657c9fb160d79fef2a
beep boop <githubbot@vadim.me>
2024-07-05 05:12:39 +0200

    Solution 3 of CPP/77, ratio 0.625 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/ivwn3urf Prompt: Complete the following code given the task description and function signature.

--- CPP_77.cpp
+++ CPP_77.cpp
@@ -1,4 +1,8 @@
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+
 bool iscuber(int a) {
-    int cbrt_a = round(cbrt(a));
-    return cbrt_a * cbrt_a * cbrt_a == a;
+    int root = round(cbrt(a));
+    return root * root * root == a;
 }
