5c708c7b498cbe6d556470bf4863990692ec9cbc
beep boop <githubbot@vadim.me>
2024-07-05 11:22:30 +0200

    Solution 5 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/0nbq2tdj Prompt: Complete the following code given the task description and function signature.

--- Python_25.py
+++ Python_25.py
@@ -1,7 +1,9 @@
+from typing import List
+
 def factorize(n: int) -> List[int]:
     factors = []
     divisor = 2
-    while n >= 2:
+    while n > 1:
         while n % divisor == 0:
             factors.append(divisor)
             n //= divisor
