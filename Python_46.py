510c77f0a883c3ae214a9b432080bdec334cc118
beep boop <githubbot@vadim.me>
2024-07-06 01:03:51 +0200

    Solution 45 of Python/46, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/dceenzxr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7b07a289-ae18-4c10-9b97-9959a6178 1c2.py", line 17, in <module>     check(fib4)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7b07a289-ae18-4c10-9b97-9959a6178 1c2.py", line 14, in check     assert fib4(12) == 386            ^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7b07a289-ae18-4c10-9b97-9959a6178 1c2.py", line 12, in fib4     return memo[n]            ~~~~^^^ IndexError: list index out of range

--- Python_46.py
+++ Python_46.py
@@ -1,3 +1,4 @@
+
 def fib4(n: int):
     if n <= 3:
         return 0
@@ -8,6 +9,4 @@ def fib4(n: int):
         for i in range(5, n + 1):
             if i >= 3 and i - 2 < len(memo):
                 memo.append(memo[i - 3] + memo[i - 2])
-    if n >= len(memo):
-        memo.append(0)
-    return memo[n]
+        return memo[n]
