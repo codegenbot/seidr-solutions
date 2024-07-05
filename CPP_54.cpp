72ac09b9eb6994d520719ea5ad1e290f0924e963
beep boop <githubbot@vadim.me>
2024-07-05 13:16:43 +0200

    Solution 11 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/gqupubcf Prompt: Complete the following code given the task description and function signature.

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -1,7 +1,7 @@
 bool same_chars(string s0, string s1){
     sort(s0.begin(), s0.end());
-    s0.erase(unique(s0.begin(), s0.end()), s0.end());
     sort(s1.begin(), s1.end());
+    s0.erase(unique(s0.begin(), s0.end()), s0.end());
     s1.erase(unique(s1.begin(), s1.end()), s1.end());
     return s0 == s1;
 }
