01e076a8a376c944998df2c4776619c39df4784f
beep boop <githubbot@vadim.me>
2024-07-05 08:08:39 +0200

    Solution 15 of CPP/109, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/l4z9gesc Prompt: 5a20e1e7-5921-4786-ad12-bfc0b4c9a8db.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 5a20e1e7-5921-4786-ad12-bfc0b4c9a8db.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_109.cpp
+++ CPP_109.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
 bool move_one_ball(const std::vector<int>& arr) {
     int n = arr.size();
@@ -21,13 +20,8 @@ bool move_one_ball(const std::vector<int>& arr) {
 
 int main() {
     std::cout << std::boolalpha;
-    int n; 
-    std::cin >> n;
-    std::vector<int> arr(n);
-    for(int i = 0; i < n; ++i) {
-        std::cin >> arr[i];
-    }
 
+    std::vector<int> arr = {3, 4, 5, 1, 2};
     std::cout << move_one_ball(arr) << std::endl;
 
     return 0;
