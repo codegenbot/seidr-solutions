294a29c146d8a701430a2f76ce91109e5f4d7489
beep boop <githubbot@vadim.me>
2024-07-06 01:24:28 +0200

    Solution 7 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gbyn86lg Prompt:    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d8f696f9-cf3c-4fd1-bedb-77372887c387.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d8f696f9-cf3c-4fd1-bedb-77372887c387.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d8f696f9-cf3c-4fd1-bedb-77372887c387.cpp:23: d8f696f9-cf3c-4fd1-bedb-77372887c387.cpp: In function ‘int main()’: d8f696f9-cf3c-4fd1-bedb-77372887c387.cpp:33:20: error: ‘separate_paren_groups’ w as not declared in this scope    33 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ d8f696f9-cf3c-4fd1-bedb-77372887c387.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,15 +1,7 @@
 #include <vector>
 #include <string>
 
-bool issame(const std::vector<std::string>& vec1, const std::vector<std::string>& vec2) {
-    if (vec1.size() != vec2.size()) return false;
-    for (int i = 0; i < vec1.size(); i++) {
-        if (vec1[i] != vec2[i]) return false;
-    }
-    return true;
-}
-
-std::vector<std::string> separate_paren_groups(const std::string& paren_string) {
+std::vector<std::string> separate_paren_groups(std::string paren_string) {
     std::vector<std::string> result;
     std::string current_group = "";
     int open_count = 0;
