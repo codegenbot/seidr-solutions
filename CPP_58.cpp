5f7d1506030cb0726e937f32155ad4ae330a6c18
beep boop <githubbot@vadim.me>
2024-07-05 14:41:14 +0200

    Solution 77 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/y5mjvis1 Prompt:       |                    ^~~ 713e0995-5311-4b29-9dd7-5d5184334531.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 713e0995-5311-4b29-9dd7-5d5184334531.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 713e0995-5311-4b29-9dd7-5d5184334531.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 713e0995-5311-4b29-9dd7-5d5184334531.cpp:10: 713e0995-5311-4b29-9dd7-5d5184334531.cpp: In function ‘int main()’: 713e0995-5311-4b29-9dd7-5d5184334531.cpp:20:20: error: ‘common’ was not declared  in this scope    20 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 713e0995-5311-4b29-9dd7-5d5184334531.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -20,5 +20,6 @@ bool issame(vector<int> a, vector<int> b) {
 int main() {
     assert(issame(common({4, 3, 2, 8}, {}), {}));
     assert(issame(common({4, 3, 2, 8}, {1, 2, 3, 4}), {2, 3, 4}));
+    // Add more tests if needed
     return 0;
 }
