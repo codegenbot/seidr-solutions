67df770899ee0f5ce13a3e24f5ffb5eb5be81883
beep boop <githubbot@vadim.me>
2024-07-05 16:06:50 +0200

    Solution 48 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/5wreize1 Prompt:       |                    ^~~ 621944fe-ce02-49bf-8dea-a5b7c99224c4.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 621944fe-ce02-49bf-8dea-a5b7c99224c4.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 621944fe-ce02-49bf-8dea-a5b7c99224c4.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 621944fe-ce02-49bf-8dea-a5b7c99224c4.cpp:21: 621944fe-ce02-49bf-8dea-a5b7c99224c4.cpp: In function ‘int main()’: 621944fe-ce02-49bf-8dea-a5b7c99224c4.cpp:31:20: error: ‘pluck’ was not declared in this scope    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 621944fe-ce02-49bf-8dea-a5b7c99224c4.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,24 +1,25 @@
 #include <vector>
-#include <climits>
 #include <cassert>
 
 using namespace std;
 
 vector<int> pluck(vector<int> arr) {
-    int min_value = INT_MAX;
-    int min_index = -1;
-
+    int minEvenValue = -1;
+    int minIndex = -1;
+    
     for (int i = 0; i < arr.size(); ++i) {
-        if (arr[i] % 2 == 0 && arr[i] < min_value) {
-            min_value = arr[i];
-            min_index = i;
+        if (arr[i] % 2 == 0) {
+            if (minEvenValue == -1 || arr[i] < minEvenValue) {
+                minEvenValue = arr[i];
+                minIndex = i;
+            }
         }
     }
-
-    if (min_index == -1) {
+    
+    if (minEvenValue == -1) {
         return {};
     } else {
-        return {min_value, min_index};
+        return {minEvenValue, minIndex};
     }
 }
 
@@ -28,7 +29,5 @@ bool issame(vector<int> a, vector<int> b) {
 
 int main() {
     assert(issame(pluck({7, 9, 7, 1}), {}));
-    assert(issame(pluck({10, 3, 15, 20}), {10, 0}));
-    assert(issame(pluck({3, 5, 4, 8, 2}), {2, 4}));
     return 0;
 }
