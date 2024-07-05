67a8119656d92a356cae54cbeb2ed9378c6bdfd3
beep boop <githubbot@vadim.me>
2024-07-05 19:12:04 +0200

    Solution 92 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/rzu35mhe Prompt: /usr/include/c++/11/bits/stl_vector.h:510:7: note: candidate: ‘std::vector<_Tp, _Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocator_type&) [wit h _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type =  long unsigned int; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<in t>]’   510 |       vector(size_type __n, const allocator_type& __a = allocator_type() )       |       ^~~~~~ /usr/include/c++/11/bits/stl_vector.h:510:7: note:   candidate expects 2 argumen ts, 9 provided /usr/include/c++/11/bits/stl_vector.h:497:7: note: candidate: ‘std::vector<_Tp, _Alloc>::vector(const allocator_type&) [with _Tp = int; _Alloc = std::allocator< int>; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<int>]’   497 |       vector(const allocator_type& __a) _GLIBCXX_NOEXCEPT       |       ^~~~~~ /usr/include/c++/11/bits/stl_vector.h:497:7: note:   candidate expects 1 argumen t, 9 provided /usr/include/c++/11/bits/stl_vector.h:487:7: note: candidate: ‘std::vector<_Tp, _Alloc>::vector() [with _Tp = int; _Alloc = std::allocator<int>]’   487 |       vector() = default;       |       ^~~~~~ /usr/include/c++/11/bits/stl_vector.h:487:7: note:   candidate expects 0 argumen ts, 9 provided

--- basement.cpp
+++ basement.cpp
@@ -15,11 +15,11 @@ int main() {
     std::vector<int> nums1(-2,1,-3,4,-1,2,1,-5,4);
     std::cout << basement(nums1) << std::endl;
 
-    nums1.clear();
-    nums1 = std::vector<int>(6, 0);
+    std::vector<int> nums2;
+    nums2.resize(6);
+    nums2.push_back(0);nums2.push_back(-1);nums2.push_back(2);nums2.push_back(-3);nums2.push_back(1);
 
-    nums1.push_back(0);nums1.push_back(-1);nums1.push_back(2);nums1.push_back(-3);nums1.push_back(1);
-    std::cout << basement(nums1) << std::endl;
+    std::cout << basement(nums2) << std::endl;
 
     return 0;
 }
