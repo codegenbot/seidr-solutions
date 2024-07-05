acfb501d86e07e23c35571183176c482930949c6
beep boop <githubbot@vadim.me>
2024-07-05 13:30:43 +0200

    Solution 48 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/k86l67yy Prompt: 9c7712ed-7f44-46b0-bfc0-eb9a94a2228f.cpp:40:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    40 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 9c7712ed-7f44-46b0-bfc0-eb9a94a2228f.cpp:29:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    29 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 9c7712ed-7f44-46b0-bfc0-eb9a94a2228f.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ 9c7712ed-7f44-46b0-bfc0-eb9a94a2228f.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 vector<int> largest_smallest_integers(vector<int> lst) {
-    int largest_negative = INT_MIN;
-    int smallest_positive = INT_MAX;
+    int largest_negative = 0;
+    int smallest_positive = 0;
     bool found_negative = false;
     bool found_positive = false;
 
@@ -32,6 +32,10 @@ bool issame(vector<int> a, vector<int> b) {
 
 int main() {
     assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
+    assert(issame(largest_smallest_integers({-10, -20, -30, 2, 4, 6}), {-10, 2}));
+    assert(issame(largest_smallest_integers({10, 20, 30, 2, 4, 6}), {0, 2}));
+    assert(issame(largest_smallest_integers({-10, -20, -30, -2, -4, -6}), {-2, 0}));
+    assert(issame(largest_smallest_integers({1, -1, 2, -2, 3, -3}), {-1, 1}));
     cout << "All tests passed!" << endl;
     return 0;
 }
