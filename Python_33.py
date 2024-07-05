2e17943c40f23a6e6ccbd46a84d77d25d56fb8ad
beep boop <githubbot@vadim.me>
2024-07-06 01:07:22 +0200

    Solution 78 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tpufcn5h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9fa2fb59-42df-4982-8e8f-f961de5e1 b51.py", line 11, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9fa2fb59-42df-4982-8e8f-f961de5e1 b51.py", line 6, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,7 +1,4 @@
-```Python
-def sort_third(lst):
-    lst = [(1, 2, 'apple'), (4, 3, 'banana'), (7, 5, 'cherry')]
-    sorted_lst = [x[2] for x in sorted(lst, key=lambda x: x[2])]
-    return tuple(sorted_lst)
-
-print(sort_third())
+```
+def sort_third(l):
+    result = sorted([item for item in l])
+    return tuple(result)
