1adfde6bba967353e821d5ac3a5fbe352b211cbf
beep boop <githubbot@vadim.me>
2024-07-06 01:34:28 +0200

    Solution 8 of CPP/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/n8ravhsc Prompt: 9097af5e-649c-4f9e-83d4-20a7048fea77.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | bool below_zero(vector<int> operations){       |                 ^~~~~~ 9097af5e-649c-4f9e-83d4-20a7048fea77.cpp:1:24: error: expected primary-expressio n before ‘int’     1 | bool below_zero(vector<int> operations){       |                        ^~~ In file included from 9097af5e-649c-4f9e-83d4-20a7048fea77.cpp:10: 9097af5e-649c-4f9e-83d4-20a7048fea77.cpp: In function ‘int main()’: 9097af5e-649c-4f9e-83d4-20a7048fea77.cpp:12:23: error: ‘below_zero’ cannot be us ed as a function    12 |     assert (below_zero({1, -2, 2, -2, 5, -5, 4, -4}) == true);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_3.cpp
+++ CPP_3.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 bool below_zero(std::vector<int> operations){
     int balance = 0;
@@ -7,3 +8,9 @@ bool below_zero(std::vector<int> operations){
         if(balance < 0) return true;
     }
     return false;
+}
+
+int main() {
+    std::cout << std::boolalpha << below_zero({1, -2, 2, -2, 5, -5, 4, -4}) << std::endl;
+    return 0;
+}
