de1d8d348f39182527860bfbd85be27b64674aae
beep boop <githubbot@vadim.me>
2024-07-06 01:50:28 +0200

    Solution 6 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/u4hjxqlv Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -1,9 +1,4 @@
-
-def find_zero(xs):
-    if len(xs) % 2 == 1 or xs[0] != 0:
+def find_zero(xs: list):
+    if not xs:
         return None
-    for i in range(len(xs)):
-        if xs[i] == 0:
-            continue
-        x = -xs[i-1] / (xs[i] * xs[i])
-        return round(x, 2)
+    return xs[0] / xs[1]
