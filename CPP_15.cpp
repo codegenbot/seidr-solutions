e76ff323dbb04b89f8aa2648ffce7d7ce14defb7
beep boop <githubbot@vadim.me>
2024-07-05 10:43:38 +0200

    Solution 14 of CPP/15, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/v3pa7hzu Prompt: Complete the following code given the task description and function signature.

--- CPP_15.cpp
+++ CPP_15.cpp
@@ -1,8 +1,7 @@
-string string_sequence(int n){
-    string result;
-    for (int i = 0; i <= n; ++i) {
+string result = "";
+    for(int i = 0; i <= n; ++i){
         result += to_string(i);
-        if (i < n) result += " ";
+        if(i < n) result += " ";
     }
     return result;
 }
