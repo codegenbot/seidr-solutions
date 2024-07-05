139c55efec88c4a54b10decb9d10dc681e62c454
beep boop <githubbot@vadim.me>
2024-07-05 16:41:50 +0200

    Solution 0 of CPP/24, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/rex0vppq Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_24.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+using namespace std;
+int largest_divisor(int n){
+    for(int i = n/2; i > 0; i--){
+        if(n % i == 0){
+            return i;
+        }
+    }
+    return 1;
+}
