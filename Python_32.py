c3d74561b3ea758757f4700aa5a5cba01d3b1b51
beep boop <githubbot@vadim.me>
2024-07-06 01:02:22 +0200

    Solution 57 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/a39d9ij4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/11b8d37d-f2cb-4fc5-8742-6906a391d 431.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,6 +1,9 @@
 def find_zero(xs: list):
-    if len(xs) < 2:
-        raise ValueError("List must have at least two elements")
-    a, b, c = xs
-    d = math.sqrt((b**2) - (4*a*c))
-    return (-b + d)/(2*a)
+    a = xs[0]
+    b = xs[1]
+    c = xs[2]
+    
+    x1 = (-b + math.sqrt((b**2) - (4*a*c)))/(2*a)
+    x2 = (-b - math.sqrt((b**2) - (4*a*c)))/(2*a)
+
+    return x1, x2
