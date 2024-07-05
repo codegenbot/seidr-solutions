2914a5ba9e919b03004859989d41d673565473cd
beep boop <githubbot@vadim.me>
2024-07-06 01:20:57 +0200

    Solution 38 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/4ip2inle Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a97b9241-3769-4269-8c4b-7f0dff06d 463.py", line 17, in <module>     check(encrypt)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a97b9241-3769-4269-8c4b-7f0dff06d 463.py", line 13, in check     encrypt("a") == "e" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_89.py
+++ Python_89.py
@@ -1,6 +1,6 @@
 def encrypt(s):
     new_string = ""
-    for char in s.lower():
+    for char in s:
         if char.islower():
             new_char = chr((ord(char) + 2 - ord("a")) % 26 + ord("a"))
             new_string += new_char
