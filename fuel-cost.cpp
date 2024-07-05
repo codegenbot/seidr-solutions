f6ed1a1c0daff6a7bceebf5e56bbf755e488d8ff
beep boop <githubbot@vadim.me>
2024-07-05 19:18:54 +0200

    Solution 8 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/mkyfsabb Prompt: e-enclosed initializer list> 52236957-d2d0-4fa2-8f02-0a41a8e0fccb.cpp:20:30: error: conversion from ‘int’ to non-scalar type ‘std::vector<int>’ requested    20 |     for (vector<int> input : inputs) {       |                              ^~~~~~ 52236957-d2d0-4fa2-8f02-0a41a8e0fccb.cpp:21:9: error: ‘cout’ was not declared in  this scope    21 |         cout << calculateFuelCost(input) << endl;       |         ^~~~ 52236957-d2d0-4fa2-8f02-0a41a8e0fccb.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; 52236957-d2d0-4fa2-8f02-0a41a8e0fccb.cpp:21:45: error: ‘endl’ was not declared i n this scope    21 |         cout << calculateFuelCost(input) << endl;       |                                             ^~~~ 52236957-d2d0-4fa2-8f02-0a41a8e0fccb.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -1,13 +1,16 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
+
 using namespace std;
 
 int calculateFuelCost(vector<int> numbers) {
     int totalFuel = 0;
     for (int num : numbers) {
-        int result = static_cast<int>(num / 3.0);
+        int result = (num / 3);
         if (result >= 2) {
             result -= 2;
+        } else {
+            result = 0;
         }
         totalFuel += result;
     }
@@ -15,9 +18,8 @@ int calculateFuelCost(vector<int> numbers) {
 }
 
 int main() {
-    vector<int> inputs = {{1}, {6}, {1, 7}, {1, 8}, {1, 9}, {1, 10}};
-    for (vector<int> input : inputs) {
+    vector<vector<int>> inputs = {{1}, {6}, {1, 7}, {1, 8}, {1, 9}, {1, 10}};
+    for (const auto& input : inputs) {
         cout << calculateFuelCost(input) << endl;
     }
     return 0;
-}
