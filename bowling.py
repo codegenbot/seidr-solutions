cb17713144ca0888b88c8106dee4f0b76ce1b13f
beep boop <githubbot@vadim.me>
2024-07-05 19:47:57 +0200

    Solution 47 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/jcidy0iu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e0e2f0fa-5117-4f6b-98c5-91a8e1c1d 973.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -2,8 +2,7 @@
 def bowling_score(frames):
     score = 0
     i = 0
-    frames += ' ' #add a space at the end of the string for easier parsing
-
+    
     while i < len(frames):
         if frames[i] == 'X':
             score += 10
@@ -15,22 +14,19 @@ def bowling_score(frames):
                 score += int(frames[i:i+2])
                 i += 2
         elif '/' in frames[i:]:
-            if frames[i] == 'X':
-                score += 10
-                i += 1
-            else:
-                score += int(frames[i].split('/')[0]) + int(frames[i].split('/')[1])
+            if i + 1 < len(frames):
+                first_roll = int(frames[i+1])
+                if i + 2 < len(frames) and frames[i+2].isdigit():
+                    second_roll = int(frames[i+2:i+3])
+                else:
+                    second_roll = 10 - first_roll
+                score += first_roll + second_roll
             i += 2
         else:
             temp = ''
             while i < len(frames) and frames[i].isdigit():
                 temp += frames[i]
                 i += 1
-            if len(temp) == 1: 
-                score += int(temp)
-            elif len(temp) == 2:
-                score += int(temp)
-            else:
-                score += int(temp[0]) + int(temp[1])
+            score += int(temp)
 
     return score
