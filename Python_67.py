8ec8a8040ab31305d12e5d41eda442d7a7d6112a
beep boop <githubbot@vadim.me>
2024-07-06 01:17:10 +0200

    Solution 30 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/09qczbg5 Prompt: Program did not receive expected input

--- Python_67.py
+++ Python_67.py
@@ -1,5 +1,16 @@
+def fruit_distribution(s, n):
+    matches = re.search(r"(\d+) apples", s)
+    if not matches:
+        apple_count = 0
+    else:
+        apple_count = int(matches[1])
 
-def fruit_distribution(s):
-    apple_count = int(re.search(r"\d+", s).group())
-    orange_count = int(re.search(r"\d+", s).group())
-    return apple_count + orange_count
+    matches = re.search(r"(\d+) oranges", s)
+    if not matches:
+        orange_count = 0
+    else:
+        orange_count = int(matches[1])
+
+    mango_count = n - apple_count - orange_count
+
+    return mango_count
