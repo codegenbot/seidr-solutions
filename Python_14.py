5df26675fd36aadf35182b2a64bea4a523af164a
beep boop <githubbot@vadim.me>
2024-07-05 04:47:11 +0200

    Solution 12 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/sfbiof79 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2f3ed1e2-7a41-482c-af71-21ec21ae8 d9d.py", line 1     return [string[:i] for i in range(1, len(string) + 1)]     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_14.py
+++ Python_14.py
@@ -1,5 +1,2 @@
-def substrings(string):
+def generate_substrings(string):
     return [string[:i] for i in range(1, len(string) + 1)]
-
-user_input = input("Enter a string: ")
-print(substrings(user_input))
