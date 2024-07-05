e757065242a671858b16465c0ff75f4335f18d60
beep boop <githubbot@vadim.me>
2024-07-06 01:46:54 +0200

    Solution 22 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/tn9pfa00 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c55db403-96f6-48cb-a2b7-2dd13dca9 879.py", line 17, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c55db403-96f6-48cb-a2b7-2dd13dca9 879.py", line 14, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_105.py
+++ Python_105.py
@@ -1,10 +1,8 @@
 def by_length(arr):
     result = []
     for num in arr:
-        if isinstance(num, str) and len(num) <= 9:
-            result.append(str(int(num)))
-        elif isinstance(num, int):
+        if isinstance(num, int) and 1 <= num <= 9:
             result.append(str(num))
-    result.sort()
-    result.reverse()
-    return result
+        elif isinstance(num, str):
+            result.append(num.capitalize())
+    return sorted(result, key=len)
