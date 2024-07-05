8b559497ff439cb27b90f8ccb3b084d41a605da5
beep boop <githubbot@vadim.me>
2024-07-05 08:51:11 +0200

    Solution 9 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/uz8b8q53 Prompt: Complete the following code given the task description and function signature.

--- Python_6.py
+++ Python_6.py
@@ -1,11 +1,13 @@
+from typing import List
+
 def parse_nested_parens(paren_string: str) -> List[int]:
-    def max_depth(s: str) -> int:
+    def max_depth(paren_group: str) -> int:
         depth = max_depth = 0
-        for char in s:
-            if char == "(":
+        for char in paren_group:
+            if char == '(':
                 depth += 1
                 max_depth = max(max_depth, depth)
-            elif char == ")":
+            elif char == ')':
                 depth -= 1
         return max_depth
 
