270598450832e541f9adc771332cabbd836a3af0
beep boop <githubbot@vadim.me>
2024-07-05 05:49:44 +0200

    Solution 71 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/0vvoj0js Prompt: Program did not receive expected input

--- Python_12.py
+++ Python_12.py
@@ -1,9 +1,11 @@
 from typing import List, Optional
 
+
 def longest(strings: List[str]) -> Optional[str]:
     if not strings:
         return None
     return max(strings, key=len)
+
 
 if __name__ == "__main__":
     strings = input().strip().split()
