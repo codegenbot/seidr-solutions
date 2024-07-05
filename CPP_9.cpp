99ad7fdec49e5ca3a885a655a15032034d61a33b
beep boop <githubbot@vadim.me>
2024-07-05 16:18:30 +0200

    Solution 33 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/ikc52gvl Prompt:  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 24eb63fc-dd4b-4672-adb6-d2fb0402c572.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 24eb63fc-dd4b-4672-adb6-d2fb0402c572.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 24eb63fc-dd4b-4672-adb6-d2fb0402c572.cpp:13: 24eb63fc-dd4b-4672-adb6-d2fb0402c572.cpp: In function ‘int main()’: 24eb63fc-dd4b-4672-adb6-d2fb0402c572.cpp:23:20: error: ‘rolling_max’ was not dec lared in this scope    23 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 24eb63fc-dd4b-4672-adb6-d2fb0402c572.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -24,10 +24,6 @@ int main() {
     assert(issame(rolling_max({3, 2, 3, 100, 3}), {3, 3, 3, 100, 100}));
     
     // Add more test cases here if needed
-    assert(issame(rolling_max({1, 2, 3, 4, 5}), {1, 2, 3, 4, 5}));
-    assert(issame(rolling_max({5, 4, 3, 2, 1}), {5, 5, 5, 5, 5}));
-    assert(issame(rolling_max({7, 7, 7}), {7, 7, 7}));
-    assert(issame(rolling_max({3, 1, 4, 1, 5, 9}), {3, 3, 4, 4, 5, 9}));
     
     cout << "All test cases passed!" << endl;
 
