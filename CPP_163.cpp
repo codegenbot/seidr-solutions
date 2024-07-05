e9f10b502fb10a72d40f67979f6c80da100526f3
beep boop <githubbot@vadim.me>
2024-07-05 09:36:20 +0200

    Solution 24 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/4kv28oh8 Prompt:       |                    ^~~ cd84fb1f-ecef-4e50-aa3e-0a26c1d6d448.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ cd84fb1f-ecef-4e50-aa3e-0a26c1d6d448.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cd84fb1f-ecef-4e50-aa3e-0a26c1d6d448.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from cd84fb1f-ecef-4e50-aa3e-0a26c1d6d448.cpp:12: cd84fb1f-ecef-4e50-aa3e-0a26c1d6d448.cpp: In function ‘int main()’: cd84fb1f-ecef-4e50-aa3e-0a26c1d6d448.cpp:22:20: error: ‘generate_integers’ was n ot declared in this scope    22 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ cd84fb1f-ecef-4e50-aa3e-0a26c1d6d448.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,7 +1,7 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
-#include <iostream>
 
 using namespace std;
 
@@ -21,7 +21,9 @@ bool issame(vector<int> a, vector<int> b) {
 }
 
 int main() {
-    assert(issame(generate_integers(4, 10), {4, 6, 8, 10}));
+    vector<int> test1 = generate_integers(17, 89);
+    assert(issame(test1, {18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88}));
+    
     cout << "All tests passed!" << endl;
     return 0;
 }
