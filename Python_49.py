4fad026d123e0e50d9efa3261706064922cdf6cc
beep boop <githubbot@vadim.me>
2024-07-06 01:40:20 +0200

    Solution 23 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6lyzgovu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8863c67c-f5e2-4ca2-a72b-64e12216e c44.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,15 +1,2 @@
-def findMaxConsecutiveOnes(nums):
-    if not nums:
-        return 0
-    
-    max_count = 0
-    current_count = 0
-    
-    for num in nums:
-        if num == 1:
-            current_count += 1
-            max_count = max(max_count, current_count)
-        else:
-            current_count = 0
-    
-    return max_count
+def findShortestWord(strs):
+    return min(strs, key=len)
