c4db4090a59011ab220491461f932f7afb153d84
beep boop <githubbot@vadim.me>
2024-07-06 00:50:57 +0200

    Solution 24 of Python/46, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/dceenzxr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ebb9c8a6-6faf-4619-a089-f624a5f11 c12.py", line 15, in <module>     check(fib4)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ebb9c8a6-6faf-4619-a089-f624a5f11 c12.py", line 12, in check     assert fib4(12) == 386            ^^^^^^^^^^^^^^^ AssertionError

--- Python_46.py
+++ Python_46.py
@@ -4,7 +4,10 @@ def fib4(n: int):
     elif n == 4:
         return 2
     else:
-        a, b, c, d = 1, 1, 2, 4
+        a = 1
+        b = 1
+        c = 2
+        d = 4
         for i in range(5, n + 1):
             a, b, c, d = b, c, d, a + b + c + d
         return a
