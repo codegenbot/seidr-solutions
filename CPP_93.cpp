9ba68d36f3db73163df30ab8b26421129c0376c5
beep boop <githubbot@vadim.me>
2024-07-05 16:54:20 +0200

    Solution 60 of CPP/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/uonmbwqc Prompt: eda6eb46-e51f-4ade-8f67-74c4ee6186c4.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string encode(string message) {       | ^~~~~~ In file included from eda6eb46-e51f-4ade-8f67-74c4ee6186c4.cpp:16: eda6eb46-e51f-4ade-8f67-74c4ee6186c4.cpp: In function ‘int main()’: eda6eb46-e51f-4ade-8f67-74c4ee6186c4.cpp:18:13: error: ‘encode’ was not declared  in this scope    18 |     assert (encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg");       |             ^~~~~~

--- CPP_93.cpp
+++ CPP_93.cpp
@@ -20,8 +20,6 @@ string encode(string message) {
 }
 
 int main() {
-    string input;
-    getline(cin, input);
-    cout << encode(input) << endl;
+    cout << encode("I DoNt KnOw WhAt tO WrItE") << endl;
     return 0;
 }
