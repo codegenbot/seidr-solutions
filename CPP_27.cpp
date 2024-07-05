7e9ba060c667c39c07aad516fa646022fee0da31
beep boop <githubbot@vadim.me>
2024-07-05 17:15:15 +0200

    Solution 37 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/6abz2skb Prompt: bda7db4d-54e9-4244-8787-6ed374260c4c.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ bda7db4d-54e9-4244-8787-6ed374260c4c.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from bda7db4d-54e9-4244-8787-6ed374260c4c.cpp:20: bda7db4d-54e9-4244-8787-6ed374260c4c.cpp: In function ‘int main()’: bda7db4d-54e9-4244-8787-6ed374260c4c.cpp:22:13: error: ‘filp_case’ was not decla red in this scope; did you mean ‘flip_case’?    22 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <cctype>
 #include <cassert>
@@ -15,8 +14,5 @@ std::string flip_case(std::string str) {
 
 int main() {
     assert(flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
-    std::string input;
-    std::getline(std::cin, input);
-    std::cout << flip_case(input) << std::endl;
     return 0;
 }
