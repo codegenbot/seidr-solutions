6a8d174691856bdad5b1ac5fb8697badfaedf6ec
beep boop <githubbot@vadim.me>
2024-07-05 09:55:03 +0200

    Solution 14 of CPP/2, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/dzi9z6xj Prompt: Complete the following code given the task description and function signature.

--- CPP_2.cpp
+++ CPP_2.cpp
@@ -1,7 +1,3 @@
-#include<stdio.h>
-#include<math.h>
-using namespace std;
 float truncate_number(float number){
-    int integer_part = (int)number;
-    return number - integer_part;
+    return number - floor(number);
 }
