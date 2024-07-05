56049b4095a389c3de521c96f006cf1f46ec97c1
beep boop <githubbot@vadim.me>
2024-07-06 01:53:37 +0200

    Solution 49 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6lyzgovu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9cf2f9f6-c628-49b4-9a20-859826559 cc1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,11 +1,8 @@
 from math import gcd
-from functools import reduce
-
 
 def modp(n: int, p: int):
-    if n < p:
-        return n
-    result = pow(
-        n, reduce(lambda x, y: x * y % p, [i for i in range(1, (p - 1) // 2 + 1)]), p
-    )
-    return n * result % p if gcd(result, p) == 1 else n % p
+    if p == 1:
+        return n % p
+    elif p > 1:
+        result = pow(n, (p-1)//gcd(n,p), p)
+        return n * result % p
