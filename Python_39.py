9b2eeb20911286bd0de051ed1fa777c969d67851
beep boop <githubbot@vadim.me>
2024-07-06 00:39:40 +0200

    Solution 2 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/lyk6vo6c Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3141e7b-0508-46f7-9645-b7733dc3b 97f.py", line 13, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3141e7b-0508-46f7-9645-b7733dc3b 97f.py", line 10, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_39.py
+++ Python_39.py
@@ -1,5 +1,12 @@
+
 def prime_fib(n: int):
     fib = [0, 1]
+    i = 2
     while len(fib) <= n:
-        fib.append(fib[-1] + fib[-2])
+        next_fib = (fib[i-1] + fib[i-2]) % 2
+        if next_fib == 0:
+            fib.append(next_fib)
+        else:
+            fib.append(i)
+        i += 1
     return fib[n]
