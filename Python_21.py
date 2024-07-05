ac8c99af283a9d59ca4962f3abd05d137c4913dc
beep boop <githubbot@vadim.me>
2024-07-05 10:57:52 +0200

    Solution 14 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/2245gl0x Prompt: Complete the following code given the task description and function signature.

--- Python_21.py
+++ Python_21.py
@@ -1,3 +1,2 @@
-def rescale_to_unit(numbers: List[float]) -> List[float]:
-    min_num, max_num = min(numbers), max(numbers)
+min_num, max_num = min(numbers), max(numbers)
     return [(num - min_num) / (max_num - min_num) for num in numbers]
