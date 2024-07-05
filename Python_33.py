9b97f3520ca2e665b77a9f943a4f7fe0a753a35e
beep boop <githubbot@vadim.me>
2024-07-06 00:48:11 +0200

    Solution 46 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tpufcn5h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/73b65978-c2b0-4957-8765-8ff0d1258 35e.py", line 17, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/73b65978-c2b0-4957-8765-8ff0d1258 35e.py", line 12, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,15 +1,11 @@
-```
 def sort_third(l):
     result = []
-    non_three_numbers = [x for x in l if x % 3 != 0]
-    non_three_numbers.sort()
-    
     i = 0
     while i < len(l):
         if l[i] % 3 != 0:
-            result.append(non_three_numbers.pop(0))
-        else:
             result.append(l.pop(i))
+        elif min(x for x in l if x % 3 != 0) is None or l[l.index(min(x for x in l if x % 3 != 0))] <= l[i]:
+            temp = l.pop(l.index(min(x for x in l if x % 3 != 0)))
+            i -= 1
         i += 1
-    
     return tuple(result)
