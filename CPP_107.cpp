1bf3aecf91d1610710d64120907fb9d84b28823b
beep boop <githubbot@vadim.me>
2024-07-05 05:42:49 +0200

    Solution 16 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/61c3as76 Prompt:       |                    ^~~ 89c77348-921e-4253-9344-77eeca95047b.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 89c77348-921e-4253-9344-77eeca95047b.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 89c77348-921e-4253-9344-77eeca95047b.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 89c77348-921e-4253-9344-77eeca95047b.cpp:21: 89c77348-921e-4253-9344-77eeca95047b.cpp: In function ‘int main()’: 89c77348-921e-4253-9344-77eeca95047b.cpp:31:20: error: ‘even_odd_palindrome’ was  not declared in this scope    31 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 89c77348-921e-4253-9344-77eeca95047b.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -30,10 +30,10 @@ bool issame(vector<int> a, vector<int> b) {
 }
 
 int main() {
-    assert(issame(even_odd_palindrome(1), {0, 1}));
-    assert(issame(even_odd_palindrome(10), {1, 9}));
-    assert(issame(even_odd_palindrome(20), {1, 9}));
-    assert(issame(even_odd_palindrome(100), {4, 14}));
-    cout << "All test cases passed!" << endl;
+    assert (issame(even_odd_palindrome(1), {0, 1}));
+    assert (issame(even_odd_palindrome(10), {1, 9}));
+    assert (issame(even_odd_palindrome(20), {2, 9}));
+    assert (issame(even_odd_palindrome(100), {5, 45}));
+    cout << "All test cases pass!" << endl;
     return 0;
 }
