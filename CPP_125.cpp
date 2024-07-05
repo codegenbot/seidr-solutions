da76b364c875742ea7f23eb796d041d338006781
beep boop <githubbot@vadim.me>
2024-07-05 06:06:02 +0200

    Solution 0 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/nh7jvld6 Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_125.cpp
@@ -0,0 +1,31 @@
+vector<string> split_words(string txt){
+    vector<string> result;
+    size_t pos = 0;
+    string delimiter = " ";
+
+    if (txt.find(delimiter) != string::npos) {
+        while ((pos = txt.find(delimiter)) != string::npos) {
+            result.push_back(txt.substr(0, pos));
+            txt.erase(0, pos + delimiter.length());
+        }
+        result.push_back(txt);
+    } else {
+        delimiter = ",";
+        if (txt.find(delimiter) != string::npos) {
+            while ((pos = txt.find(delimiter)) != string::npos) {
+                result.push_back(txt.substr(0, pos));
+                txt.erase(0, pos + delimiter.length());
+            }
+            result.push_back(txt);
+        } else {
+            int odd_count = 0;
+            for (char c : txt) {
+                if (islower(c) && ((c - 'a') % 2 == 0)) {
+                    odd_count++;
+                }
+            }
+            result.push_back(to_string(odd_count));
+        }
+    }
+    return result;
+}
