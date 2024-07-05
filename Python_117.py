52e9e052000fcb31db07181ba874331ee4212060
beep boop <githubbot@vadim.me>
2024-07-06 01:53:55 +0200

    Solution 4 of Python/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/hy53hw4z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0db7afef-a0b3-41ea-964d-a599af922 26a.py", line 16, in <module>     check(select_words)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0db7afef-a0b3-41ea-964d-a599af922 26a.py", line 8, in check     assert select_words("a b c d e f", 1) == [            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: 2nd edge test error: ['a', 'b', 'c', 'd', 'e', 'f']

--- Python_117.py
+++ Python_117.py
@@ -1,3 +1,3 @@
 def select_words(s, n):
     words = s.split()
-    return [word for word in words if len([c for c in word if c.islower()]) == n]
+    return list(filter(lambda w: len([c for c in w if c.islower()]) == n, words))
