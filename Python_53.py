0e2ebcdd8cb9a49e01f5470cdf840c2943d36f25
beep boop <githubbot@vadim.me>
2024-07-05 08:11:42 +0200

    Solution 20 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/2nizjkzj Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -1,6 +1,7 @@
 def add(x: int, y: int):
     return x + y
 
-x = int(input())
-y = int(input())
-print(add(x, y))
+if __name__ == "__main__":
+    x = int(input())
+    y = int(input())
+    print(add(x, y))
