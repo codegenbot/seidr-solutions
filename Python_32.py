ce8ec6483f3b2df139b7cdd8d0e4f4da13c4f72b
beep boop <githubbot@vadim.me>
2024-07-06 01:00:27 +0200

    Solution 54 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/a39d9ij4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/11b8d37d-f2cb-4fc5-8742-6906a391d 431.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,13 +1,7 @@
-import math
-
-def find_zero():
-    a = float(input("Enter coefficient a: "))
-    b = float(input("Enter coefficient b: "))
-    c = float(input("Enter constant c: "))
-
-    if (b ** 2) - (4 * a * c) < 0:
-        return None
+def find_zero(xs: list):
+    if len(xs) < 2:
+        return -xs[1] / xs[0]
+    elif len(xs) == 2:
+        return -xs[1] / xs[0]
     else:
-        return (-b + math.sqrt((b ** 2) - (4 * a * c))) / (2 * a)
-
-print(find_zero())
+        return (-xs[2] + math.sqrt((xs[1]**2) - (4*xs[0]*xs[-1])))/(2*xs[0])
