b6b1f644a20ee151b383d3d873e84ab800fabf86
beep boop <githubbot@vadim.me>
2024-07-05 06:06:44 +0200

    Solution 8 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/xuyffk6x Prompt:       |                    ^~~ 75e2f081-b193-4e90-abe4-cdd2d3c037b8.cpp:23:27: error: ‘vector’ was not declared  in this scope    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 75e2f081-b193-4e90-abe4-cdd2d3c037b8.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 75e2f081-b193-4e90-abe4-cdd2d3c037b8.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 75e2f081-b193-4e90-abe4-cdd2d3c037b8.cpp:22: 75e2f081-b193-4e90-abe4-cdd2d3c037b8.cpp: In function ‘int main()’: 75e2f081-b193-4e90-abe4-cdd2d3c037b8.cpp:32:20: error: ‘sort_array’ was not decl ared in this scope    32 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 75e2f081-b193-4e90-abe4-cdd2d3c037b8.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
@@ -31,8 +30,9 @@ bool issame(vector<int> a, vector<int> b) {
 }
 
 int main() {
-    // Test case
     assert(issame(sort_array({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
-    cout << "All test cases passed." << endl;
+    assert(issame(sort_array({5, 3, 9, 7}), {3, 5, 7, 9}));
+    assert(issame(sort_array({10, 5, 6, 7}), {5, 10, 6, 7}));
+    assert(issame(sort_array({1, 2, 3, 4, 5, 6, 7, 8}), {1, 2, 4, 8, 3, 5, 6, 7}));
     return 0;
 }
