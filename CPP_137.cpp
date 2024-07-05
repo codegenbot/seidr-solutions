f728feb9652247e4fc64ce6ca8376eb88bde38a8
beep boop <githubbot@vadim.me>
2024-07-05 10:52:25 +0200

    Solution 1 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/o6crh1za Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,37 +1,32 @@
-#include<stdio.h>
-#include<string>
-#include<algorithm>
-#include<boost/any.hpp>
+#include <iostream>
+#include <string>
+#include <boost/any.hpp>
+#include <boost/lexical_cast.hpp>
+
 using namespace std;
 
-double convertToDouble(const string &s) {
-    string temp = s;
-    replace(temp.begin(), temp.end(), ',', '.');
-    return stod(temp);
-}
-
 boost::any compare_one(boost::any a, boost::any b) {
-    double valA, valB;
-    string strA, strB;
+    auto parse_real = [](const boost::any& val) -> double {
+        if (val.type() == typeid(int)) {
+            return boost::any_cast<int>(val);
+        } else if (val.type() == typeid(float)) {
+            return boost::any_cast<float>(val);
+        } else if (val.type() == typeid(string)) {
+            string str_val = boost::any_cast<string>(val);
+            replace(str_val.begin(), str_val.end(), ',', '.');
+            return boost::lexical_cast<double>(str_val);
+        }
+        throw invalid_argument("Unsupported type");
+    };
 
-    if (a.type() == typeid(int))
-        valA = boost::any_cast<int>(a);
-    else if (a.type() == typeid(float))
-        valA = boost::any_cast<float>(a);
-    else if (a.type() == typeid(string))
-        valA = convertToDouble(boost::any_cast<string>(a));
-    
-    if (b.type() == typeid(int))
-        valB = boost::any_cast<int>(b);
-    else if (b.type() == typeid(float))
-        valB = boost::any_cast<float>(b);
-    else if (b.type() == typeid(string))
-        valB = convertToDouble(boost::any_cast<string>(b));
+    double val_a = parse_real(a);
+    double val_b = parse_real(b);
 
-    if (valA > valB)
+    if (val_a == val_b) {
+        return "None";
+    } else if (val_a > val_b) {
         return a;
-    else if (valB > valA)
+    } else {
         return b;
-    else
-        return string("None");
+    }
 }
