fb75deb67e5964f743d3f90a7c85beecd3895339
beep boop <githubbot@vadim.me>
2024-07-05 06:02:06 +0200

    Solution 11 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/nnxkwtqf Prompt:       |                    ^~~ 849a731f-0f22-4d31-9630-fe459627f2ce.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 849a731f-0f22-4d31-9630-fe459627f2ce.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 849a731f-0f22-4d31-9630-fe459627f2ce.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 849a731f-0f22-4d31-9630-fe459627f2ce.cpp:18: 849a731f-0f22-4d31-9630-fe459627f2ce.cpp: In function ‘int main()’: 849a731f-0f22-4d31-9630-fe459627f2ce.cpp:28:20: error: ‘get_odd_collatz’ was not  declared in this scope    28 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 849a731f-0f22-4d31-9630-fe459627f2ce.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -27,8 +27,8 @@ bool issame(vector<int> a, vector<int> b) {
 }
 
 int main() {
-    assert(issame(get_odd_collatz(1), vector<int>{1}));
-    assert(issame(get_odd_collatz(13), vector<int>{1, 3, 5, 7, 9, 11, 13, 15}));
+    assert (issame(get_odd_collatz(1), vector<int>{1}));
+    assert (issame(get_odd_collatz(13), vector<int>{1, 3, 5, 7, 9, 11, 13, 15}));
 
     cout << "All tests passed!" << endl;
     return 0;
