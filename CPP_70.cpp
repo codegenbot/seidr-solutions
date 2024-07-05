3f97fc0067c18953c55b99461cc9f0a7a9e910ce
beep boop <githubbot@vadim.me>
2024-07-05 05:00:23 +0200

    Solution 28 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/6zos2ft2 Prompt:       |                    ^~~ ffb339ff-7daa-4eac-8980-69305cbbfd51.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ffb339ff-7daa-4eac-8980-69305cbbfd51.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ffb339ff-7daa-4eac-8980-69305cbbfd51.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ffb339ff-7daa-4eac-8980-69305cbbfd51.cpp:13: ffb339ff-7daa-4eac-8980-69305cbbfd51.cpp: In function ‘int main()’: ffb339ff-7daa-4eac-8980-69305cbbfd51.cpp:23:20: error: ‘strange_sort_list’ was n ot declared in this scope    23 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ ffb339ff-7daa-4eac-8980-69305cbbfd51.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,32 +1,32 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
 
 using namespace std;
 
-vector<int> strange_sort_list(vector<int> lst) {
+vector<int> strange_sort_list(const vector<int>& lst) {
     vector<int> result;
-    sort(lst.begin(), lst.end());
-    bool pick_min = true;
-    while (!lst.empty()) {
-        if (pick_min) {
-            result.push_back(lst.front());
-            lst.erase(lst.begin());
-        } else {
-            result.push_back(lst.back());
-            lst.pop_back();
+    vector<int> sorted_lst = lst;
+    sort(sorted_lst.begin(), sorted_lst.end());
+    int i = 0, j = sorted_lst.size() - 1;
+    while (i <= j) {
+        result.push_back(sorted_lst[i++]);
+        if (i <= j) {
+            result.push_back(sorted_lst[j--]);
         }
-        pick_min = !pick_min;
     }
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(const vector<int>& a, const vector<int>& b) {
     return a == b;
 }
 
 int main() {
     assert(issame(strange_sort_list({111111}), {111111}));
-    assert(issame(strange_sort_list({1, 2, 3, 4, 5}), {1, 5, 2, 4, 3}));
+    assert(issame(strange_sort_list({4, 2, 5, 3, 1}), {1, 5, 2, 4, 3}));
+    assert(issame(strange_sort_list({1, 2, 3, 4, 5, 6}), {1, 6, 2, 5, 3, 4}));
+    cout << "All test cases passed!" << endl;
     return 0;
 }
