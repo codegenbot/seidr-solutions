14495e9090503dddf45b314d95c6064c8841bf26
beep boop <githubbot@vadim.me>
2024-07-06 01:44:58 +0200

    Solution 36 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6lyzgovu Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/58c41afd-dc7b-4873-a556-2d902a481 565.py", line 10, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/58c41afd-dc7b-4873-a556-2d902a481 565.py", line 7, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/58c41afd-dc7b-4873-a556-2d902a481 565.py", line 5, in modp     return n * result % p if gcd(result, p) == 1 else n % p                              ^^^ NameError: name 'gcd' is not defined

--- Python_49.py
+++ Python_49.py
@@ -1,8 +1,7 @@
-```
-from math import gcd
+import math
 
 def modp(n: int, p: int):
     if p == 1:
         return n % p
     result = pow(n, p - 1, p)
-    return n * result % p if gcd(result, p) == 1 else n % p
+    return n * result % p if math.gcd(result, p) == 1 else n % p
