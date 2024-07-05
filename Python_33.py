e26569aff12dca2805ec1d2d6820c963f8609425
beep boop <githubbot@vadim.me>
2024-07-06 01:03:32 +0200

    Solution 64 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tpufcn5h Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c45e3238-0d42-40b7-9fbb-1b36840ee 585.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,3 +1,4 @@
+```
 def sort_third(l):
     result = []
     i = 0
@@ -5,18 +6,12 @@ def sort_third(l):
     while j < len(l):
         if i < j:
             if i >= len(l):
-                while True:
-                    if l[i % len(l)] % 3 != 0:
-                        break
-                    i += 1
-            if l[i % len(l)] % 3 == 0:
-                result.append(l.pop(i % len(l)))
+                i %= len(l)
+            if l[i] % 3 == 0:
+                result.append(l.pop(i))
         else:
             if j >= len(l):
-                while True:
-                    if l[j % len(l)] % 3 != 0:
-                        break
-                    j += 1
-            if l[j % len(l)] % 3 != 0:
-                result.append(l.pop(j % len(l)))
+                j %= len(l)
+            if l[j] % 3 != 0:
+                result.append(l.pop(j))
     return tuple(result)
