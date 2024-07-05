b33b94906f6e051a02996ecac695d5b6abcba3f0
beep boop <githubbot@vadim.me>
2024-07-05 04:09:42 +0200

    Solution 46 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/ojkpbofs Prompt: 6f813136-3ae6-49a1-9d2c-b313d5a12ced.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 6f813136-3ae6-49a1-9d2c-b313d5a12ced.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 6f813136-3ae6-49a1-9d2c-b313d5a12ced.cpp:22: 6f813136-3ae6-49a1-9d2c-b313d5a12ced.cpp: In function ‘int main()’: 6f813136-3ae6-49a1-9d2c-b313d5a12ced.cpp:24:13: error: ‘filp_case’ was not decla red in this scope; did you mean ‘flip_case’?    24 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -15,7 +15,6 @@ std::string flip_case(std::string str) {
 
 int main() {
     assert(flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
-    assert(flip_case("Brutus") == "bRUTUS");
     std::cout << "Test passed!" << std::endl;
     return 0;
 }
