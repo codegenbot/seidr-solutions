3ef92be156148d8bd415651db58ebb552d3e8861
beep boop <githubbot@vadim.me>
2024-07-05 04:51:13 +0200

    Solution 96 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/tj7eys20 Prompt: 07ce65f2-5724-4281-a380-2d8b8c31e955.cpp:54:5: error: redefinition of ‘int main( )’    54 | int main(){       |     ^~~~ 07ce65f2-5724-4281-a380-2d8b8c31e955.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <set>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
 using namespace std;
 
 vector<int> common(vector<int> l1, vector<int> l2) {
