3efa0d1ae18d95682f26c574bafb356c1401b571
beep boop <githubbot@vadim.me>
2024-07-05 09:12:04 +0200

    Solution 88 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/alem57y1 Prompt: 4b33bf6e-5ce6-4121-b45a-82bde4bddcbd.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 4b33bf6e-5ce6-4121-b45a-82bde4bddcbd.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 4b33bf6e-5ce6-4121-b45a-82bde4bddcbd.cpp:31: 4b33bf6e-5ce6-4121-b45a-82bde4bddcbd.cpp: In function ‘int main()’: 4b33bf6e-5ce6-4121-b45a-82bde4bddcbd.cpp:33:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    33 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -21,9 +21,18 @@ string int_to_mini_roman(int number) {
 
 int main() {
     assert(int_to_mini_roman(1000) == "m");
+    assert(int_to_mini_roman(900) == "cm");
     assert(int_to_mini_roman(500) == "d");
     assert(int_to_mini_roman(400) == "cd");
+    assert(int_to_mini_roman(100) == "c");
     assert(int_to_mini_roman(90) == "xc");
+    assert(int_to_mini_roman(50) == "l");
+    assert(int_to_mini_roman(40) == "xl");
+    assert(int_to_mini_roman(10) == "x");
+    assert(int_to_mini_roman(9) == "ix");
+    assert(int_to_mini_roman(5) == "v");
+    assert(int_to_mini_roman(4) == "iv");
+    assert(int_to_mini_roman(1) == "i");
     cout << "All tests pass!" << endl;
     return 0;
 }
