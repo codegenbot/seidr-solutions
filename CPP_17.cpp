ffd33ccc7a689c9ba1bac21b54e7efe1f95ddbe1
beep boop <githubbot@vadim.me>
2024-07-05 16:25:29 +0200

    Solution 20 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/al86zj0j Prompt:    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a1da5682-02ee-4389-a961-9ce681021988.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a1da5682-02ee-4389-a961-9ce681021988.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a1da5682-02ee-4389-a961-9ce681021988.cpp:22: a1da5682-02ee-4389-a961-9ce681021988.cpp: In function ‘int main()’: a1da5682-02ee-4389-a961-9ce681021988.cpp:32:20: error: ‘parse_music’ was not dec lared in this scope    32 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ a1da5682-02ee-4389-a961-9ce681021988.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <cassert>
+#include <string>
 
 using namespace std;
 
@@ -27,11 +27,15 @@ vector<int> parse_music(string music_string) {
 }
 
 bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+    if (a.size() != b.size()) return false;
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
 }
 
 int main() {
     assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 4, 2, 4, 2}));
-    cout << "Test passed!" << endl;
+    cout << "All tests passed!" << endl;
     return 0;
 }
