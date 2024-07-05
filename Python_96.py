2b0f3b2e6730da99427e7452c81baac85ddd8823
beep boop <githubbot@vadim.me>
2024-07-06 01:28:50 +0200

    Solution 35 of Python/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/j5uln9v9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/27c9cd8e-af83-4f6c-a27f-59ab4f1ec 4d5.py", line 9, in <module>     assert count_up_to(101) == [            ^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/27c9cd8e-af83-4f6c-a27f-59ab4f1ec 4d5.py", line 4, in count_up_to     if is_prime(i):        ^^^^^^^^ NameError: name 'is_prime' is not defined

--- Python_96.py
+++ Python_96.py
@@ -5,38 +5,11 @@ def count_up_to(n):
             result.append(i)
     return result
 
+
 def is_prime(num):
     if num < 2:
         return False
-    for i in range(2, num):
+    for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             return False
     return True
-
-assert count_up_to(101) == [
-    2,
-    3,
-    5,
-    7,
-    11,
-    13,
-    17,
-    19,
-    23,
-    29,
-    31,
-    37,
-    41,
-    43,
-    47,
-    53,
-    59,
-    61,
-    67,
-    71,
-    73,
-    79,
-    83,
-    89,
-    97,
-]
