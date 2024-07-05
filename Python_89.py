9f566ed1048e2494dd24d0d3c36e5cf0f52b5400
beep boop <githubbot@vadim.me>
2024-07-06 01:52:19 +0200

    Solution 69 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/4ip2inle Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3455a5b2-a92e-4218-92fb-d151e22ea e2c.py", line 12, in <module>     check(encrypt)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3455a5b2-a92e-4218-92fb-d151e22ea e2c.py", line 8, in check     encrypt("a") == "e"     ^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3455a5b2-a92e-4218-92fb-d151e22ea e2c.py", line 2, in encrypt     table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxy za")             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^ ValueError: the first two maketrans arguments must have equal length

--- Python_89.py
+++ Python_89.py
@@ -1,3 +1,5 @@
+table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyza ")
+
+
 def encrypt(s):
-    table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyza ")
     return s.translate(table)
