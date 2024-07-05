582c76f18b4c8e09470a1225efae8d9efd9ea1c9
beep boop <githubbot@vadim.me>
2024-07-05 10:28:45 +0200

    Solution 62 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/88aco42x Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/737109db-eecf-46ce-a304-566b1e332 067.py", line 1     return [string[:i] for i in range(1, len(string) + 1)]     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_14.py
+++ Python_14.py
@@ -1,7 +1,6 @@
 def get_prefixes(string):
     return [string[:i] for i in range(1, len(string) + 1)]
 
-# Call the function with user input
-user_input = input("Enter a string: ")
-result = get_prefixes(user_input)
-print(result)
+
+user_input = input()
+print(get_prefixes(user_input))
