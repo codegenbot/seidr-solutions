0f16c615e8e0715a2420d6da3f9e80bc6bfd7de2
beep boop <githubbot@vadim.me>
2024-07-06 01:35:18 +0200

    Solution 63 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/1lrd2vll Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/db5366fc-d190-4f2e-9085-6b1655ea6 a26.py", line 24, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/db5366fc-d190-4f2e-9085-6b1655ea6 a26.py", line 20, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg"     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/db5366fc-d190-4f2e-9085-6b1655ea6 a26.py", line 9, in encode     encoded_char = vowels[char].lower()                    ~~~~~~^^^^^^ KeyError: 'D'

--- Python_93.py
+++ Python_93.py
@@ -1,16 +1,13 @@
 
 def encode(message):
-    vowels = {"a": "A", "e": "E", "i": "I", "o": "O", "u": "U", "I": "?", " ": ""}
+    vowels = {"a": "A", "e": "E", "i": "I", "o": "O", "u": "U", "I": "?", " ": "", "D": ""}
     encoded_message = ""
     for char in message:
         if char.isalpha():
             if char.islower():
                 encoded_char = vowels[char]
             elif char.isupper():
-                if char == 'D':
-                    encoded_char = 'd'
-                else:
-                    encoded_char = vowels[char].lower()
+                encoded_char = vowels[char].lower()
         else:
             if char in vowels:
                 encoded_char = "?"
