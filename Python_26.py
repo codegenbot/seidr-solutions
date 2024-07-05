21c0ced3666e9a00c70943d3ed43695a01c37b42
beep boop <githubbot@vadim.me>
2024-07-05 11:31:10 +0200

    Solution 14 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/zw8tmc8j Prompt: Complete the following code given the task description and function signature.

--- Python_26.py
+++ Python_26.py
@@ -1,5 +1,2 @@
 def remove_duplicates(numbers: List[int]) -> List[int]:
-    from collections import Counter
-
-    count = Counter(numbers)
-    return [num for num in numbers if count[num] == 1]
+    return [num for num in numbers if numbers.count(num) == 1]
