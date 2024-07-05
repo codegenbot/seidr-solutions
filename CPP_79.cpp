dba1f668b661be01ff4e578a36f8aa1497fbfd48
beep boop <githubbot@vadim.me>
2024-07-05 05:14:42 +0200

    Solution 82 of CPP/79, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/q8rcslq3 Prompt: b7d0ecca-3edc-4ddb-9593-bfc071a04332.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ b7d0ecca-3edc-4ddb-9593-bfc071a04332.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -14,10 +14,6 @@ string decimal_to_binary(int decimal) {
 
 int main() {
     assert(decimal_to_binary(15) == "db1111db");
-    assert(decimal_to_binary(0) == "db0db");
-    assert(decimal_to_binary(1) == "db1db");
-    assert(decimal_to_binary(8) == "db1000db");
-
     int num;
     cout << "Enter a decimal number: ";
     cin >> num;
