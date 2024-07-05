e9b87da52e1f23ba013f34be488806eeafa2bb01
beep boop <githubbot@vadim.me>
2024-07-05 11:55:13 +0200

    Solution 6 of CPP/141, ratio 0.9230769230769231 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/js49oadu Prompt: 786322ca-afa6-41c6-9cb6-fecb50500369.cpp:3:1: error: ‘string’ does not name a ty pe     3 | string file_name_check(string file_name) {       | ^~~~~~ In file included from 786322ca-afa6-41c6-9cb6-fecb50500369.cpp:34: 786322ca-afa6-41c6-9cb6-fecb50500369.cpp: In function ‘int main()’: 786322ca-afa6-41c6-9cb6-fecb50500369.cpp:36:13: error: ‘file_name_check’ was not  declared in this scope    36 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,38 +1,33 @@
-#include <iostream>
-#include <string>
 #include <cctype>
-#include <algorithm>
+#include <string>
 
-using namespace std;
+std::string file_name_check(std::string file_name) {
+    int len = file_name.length();
+    int dot_count = 0;
+    int digit_count = 0;
+    int dot_position = -1;
 
-string file_name_check(string file_name) {
-    int dotPos = file_name.find('.');
-    if (dotPos == string::npos || dotPos == 0 || dotPos == file_name.length() - 1) {
+    for (int i = 0; i < len; ++i) {
+        if (isdigit(file_name[i])) {
+            digit_count++;
+        } else if (file_name[i] == '.') {
+            dot_count++;
+            dot_position = i;
+        }
+    }
+
+    if (dot_count != 1 || digit_count > 3 || dot_position == 0 || dot_position == len - 1) {
         return "No";
     }
 
-    string beforeDot = file_name.substr(0, dotPos);
-    string afterDot = file_name.substr(dotPos + 1);
-    
-    if (afterDot != "txt" && afterDot != "exe" && afterDot != "dll") {
+    if (!isalpha(file_name[0])) {
         return "No";
     }
 
-    if (!isalpha(beforeDot[0])) {
-        return "No";
+    std::string extension = file_name.substr(dot_position + 1);
+    if (extension == "txt" || extension == "exe" || extension == "dll") {
+        return "Yes";
     }
 
-    int digitCount = count_if(beforeDot.begin(), beforeDot.end(), ::isdigit);
-    if (digitCount > 3) {
-        return "No";
-    }
-
-    return "Yes";
-}
-
-int main() {
-    string fileName;
-    cin >> fileName;
-    cout << file_name_check(fileName) << endl;
-    return 0;
+    return "No";
 }
