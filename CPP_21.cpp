48eb93864f6d6c8a059af3943065b58a1941c377
beep boop <githubbot@vadim.me>
2024-07-05 16:23:41 +0200

    Solution 4 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/ebv90o6g Prompt:    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 4852a28b-6b62-44ab-ad19-cbde4fb8f8f7.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 4852a28b-6b62-44ab-ad19-cbde4fb8f8f7.cpp:10:43: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 4852a28b-6b62-44ab-ad19-cbde4fb8f8f7.cpp:9: 4852a28b-6b62-44ab-ad19-cbde4fb8f8f7.cpp: In function ‘int main()’: 4852a28b-6b62-44ab-ad19-cbde4fb8f8f7.cpp:19:20: error: ‘rescale_to_unit’ was not  declared in this scope    19 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 4852a28b-6b62-44ab-ad19-cbde4fb8f8f7.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,8 +1,29 @@
-float min_val = *min_element(numbers.begin(), numbers.end());
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+vector<float> rescale_to_unit(vector<float> numbers) {
+    float min_val = *min_element(numbers.begin(), numbers.end());
     float max_val = *max_element(numbers.begin(), numbers.end());
-    float range = max_val - min_val;
-    for (auto &num : numbers) {
-        num = (num - min_val) / range;
+    for (float &num : numbers) {
+        num = (num - min_val) / (max_val - min_val);
     }
     return numbers;
+}
+
+bool issame(vector<float> a, vector<float> b) {
+    if (a.size() != b.size()) return false;
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
+
+int main() {
+    assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), {0.25, 0.0, 1.0, 0.5, 0.75}));
+    cout << "Test passed!" << endl;
+    return 0;
 }
