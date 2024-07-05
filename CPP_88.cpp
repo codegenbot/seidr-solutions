0a791dc021de86adc9d790475fd072298b590315
beep boop <githubbot@vadim.me>
2024-07-05 06:47:51 +0200

    Solution 52 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/6jifvowk Prompt: 9c02c0f8-4c36-4be7-ac2d-5a06a76cd49d.cpp:31:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    31 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 9c02c0f8-4c36-4be7-ac2d-5a06a76cd49d.cpp:20:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    20 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 9c02c0f8-4c36-4be7-ac2d-5a06a76cd49d.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 9c02c0f8-4c36-4be7-ac2d-5a06a76cd49d.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -17,12 +17,8 @@ vector<int> sort_array(vector<int> array) {
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 int main() {
-    assert(issame(sort_array({21, 14, 23, 11}), {23, 21, 14, 11}));
-    assert(issame(sort_array({2, 14, 23, 11}), {2, 11, 14, 23}));
+    assert(sort_array({21, 14, 23, 11}) == vector<int>({23, 21, 14, 11}));
+    assert(sort_array({2, 14, 23, 11}) == vector<int>({2, 11, 14, 23}));
     return 0;
 }
