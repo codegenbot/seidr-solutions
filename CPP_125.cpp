8de9879f60f4ac46a28c1daec3366d2140e5ed3e
beep boop <githubbot@vadim.me>
2024-07-05 09:15:52 +0200

    Solution 2 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/8uw5vf53 Prompt:       |                              ^~~~~~ 21d58cd5-fec9-4509-9c3d-6cc23ed17465.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 21d58cd5-fec9-4509-9c3d-6cc23ed17465.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 21d58cd5-fec9-4509-9c3d-6cc23ed17465.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 21d58cd5-fec9-4509-9c3d-6cc23ed17465.cpp:29: 21d58cd5-fec9-4509-9c3d-6cc23ed17465.cpp: In function ‘int main()’: 21d58cd5-fec9-4509-9c3d-6cc23ed17465.cpp:39:20: error: ‘split_words’ was not dec lared in this scope    39 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 21d58cd5-fec9-4509-9c3d-6cc23ed17465.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,27 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
 vector<string> split_words(string txt) {
     vector<string> result;
-    size_t pos = 0;
-    string delimiter;
-
     if (txt.find(' ') != string::npos) {
-        delimiter = " ";
+        size_t pos = 0;
+        while ((pos = txt.find(' ')) != string::npos) {
+            result.push_back(txt.substr(0, pos));
+            txt.erase(0, pos + 1);
+        }
+        result.push_back(txt);
     } else if (txt.find(',') != string::npos) {
-        delimiter = ",";
+        size_t pos = 0;
+        while ((pos = txt.find(',')) != string::npos) {
+            result.push_back(txt.substr(0, pos));
+            txt.erase(0, pos + 1);
+        }
+        result.push_back(txt);
     } else {
-        int count = 0;
+        int odd_count = 0;
         for (char c : txt) {
-            if (c >= 'a' && c <= 'z' && ((c - 'a') % 2 == 0)) {
-                count++;
+            if (islower(c) && ((c - 'a') % 2 == 0)) {
+                odd_count++;
             }
         }
-        result.push_back(to_string(count));
-        return result;
+        result.push_back(to_string(odd_count));
     }
-
-    while ((pos = txt.find(delimiter)) != string::npos) {
-        result.push_back(txt.substr(0, pos));
-        txt.erase(0, pos + delimiter.length());
-    }
-    result.push_back(txt);
     return result;
+}
+
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
+}
+
+int main() {
+    assert(issame(split_words("hello world"), {"hello", "world"}));
+    assert(issame(split_words("a,b,c"), {"a", "b", "c"}));
+    assert(issame(split_words("abcdef"), {"3"}));
+    assert(issame(split_words(""), {"0"}));
+    
+    cout << "All tests passed!" << endl;
+    return 0;
 }
