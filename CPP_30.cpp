1a5d9e9738b3638d9c3721f96b2e0e5a6db247a4
beep boop <githubbot@vadim.me>
2024-07-05 04:07:05 +0200

    Solution 4 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/6zah8ki4 Prompt:       |                    ^~~~~ 33c62822-df5b-477c-805d-df640bcf3fca.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 33c62822-df5b-477c-805d-df640bcf3fca.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 33c62822-df5b-477c-805d-df640bcf3fca.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 33c62822-df5b-477c-805d-df640bcf3fca.cpp:11: 33c62822-df5b-477c-805d-df640bcf3fca.cpp: In function ‘int main()’: 33c62822-df5b-477c-805d-df640bcf3fca.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 33c62822-df5b-477c-805d-df640bcf3fca.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,9 +1,26 @@
-vector<float> get_positive(vector<float> l){
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+vector<float> get_positive(vector<float> l) {
     vector<float> result;
-    for (float num : l) {
-        if (num > 0) {
+    for(float num : l) {
+        if(num > 0) {
             result.push_back(num);
         }
     }
     return result;
+}
+
+bool issame(vector<float> a, vector<float> b) {
+    return a == b;
+}
+
+int main() {
+    assert(issame(get_positive({1.0, -2.0, 3.5}), {1.0, 3.5}));
+    assert(issame(get_positive({-1.0, -2.0, -3.5}), {}));
+    assert(issame(get_positive({}), {}));
+    assert(issame(get_positive({0.0, 4.2, 5.1}), {4.2, 5.1}));
+    return 0;
 }
