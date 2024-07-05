36b1d9acb8d18a216c1ce71c13e5d1274affdb66
beep boop <githubbot@vadim.me>
2024-07-06 01:33:03 +0200

    Solution 13 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/tn9pfa00 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4c209cc9-0412-4b35-a6de-ecb179037 e4a.py", line 16, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4c209cc9-0412-4b35-a6de-ecb179037 e4a.py", line 13, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_105.py
+++ Python_105.py
@@ -1,11 +1,11 @@
 
 def by_length(arr):
-   result = []
-   for num in arr:
-      if isinstance(num, int):
-         result.append(int(num))
-      elif isinstance(num, str):
-         result.append(num)
-   result.sort()
-   result.reverse()
-   return result
+    result = []
+    for num in arr:
+        if isinstance(num, int):
+            result.append(int(num))
+        elif isinstance(num, str):
+            result.append(num)
+    result.sort()
+    result.reverse()
+    return result
