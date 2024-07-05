99cbb6fba40e5bc99ce074e83c8267e997809313
beep boop <githubbot@vadim.me>
2024-07-05 16:37:26 +0200

    Solution 16 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/cap83lst Prompt:       |                              ^~~~~~ 33dd04b9-1d4b-4284-9653-393b8a84c0ce.cpp:29:37: error: ‘string’ was not declared  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 33dd04b9-1d4b-4284-9653-393b8a84c0ce.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 33dd04b9-1d4b-4284-9653-393b8a84c0ce.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 33dd04b9-1d4b-4284-9653-393b8a84c0ce.cpp:28: 33dd04b9-1d4b-4284-9653-393b8a84c0ce.cpp: In function ‘int main()’: 33dd04b9-1d4b-4284-9653-393b8a84c0ce.cpp:38:20: error: ‘split_words’ was not dec lared in this scope    38 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 33dd04b9-1d4b-4284-9653-393b8a84c0ce.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,35 +1,48 @@
-vector<string> split_words(string txt){
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <cassert>
+
+using namespace std;
+
+vector<string> split_words(string txt) {
     vector<string> result;
-    size_t pos;
-    
-    // Check if there are whitespaces in the input string
+    size_t pos = 0;
+
     if ((pos = txt.find(' ')) != string::npos) {
-        size_t start = 0;
-        while((pos = txt.find(' ', start)) != string::npos) {
-            result.push_back(txt.substr(start, pos - start));
-            start = pos + 1;
+        while ((pos = txt.find(' ')) != string::npos) {
+            result.push_back(txt.substr(0, pos));
+            txt.erase(0, pos + 1);
         }
-        result.push_back(txt.substr(start));
-    }
-    // Check if there are commas in the input string
-    else if ((pos = txt.find(',')) != string::npos) {
-        size_t start = 0;
-        while((pos = txt.find(',', start)) != string::npos) {
-            result.push_back(txt.substr(start, pos - start));
-            start = pos + 1;
+        result.push_back(txt);
+    } else if ((pos = txt.find(',')) != string::npos) {
+        while ((pos = txt.find(',')) != string::npos) {
+            result.push_back(txt.substr(0, pos));
+            txt.erase(0, pos + 1);
         }
-        result.push_back(txt.substr(start));
-    }
-    // If no whitespaces or commas, count lower-case letters with odd order
-    else {
+        result.push_back(txt);
+    } else {
         int odd_count = 0;
         for (char c : txt) {
-            if ('a' <= c && c <= 'z' && ((c - 'a') % 2 == 1)) {
+            if (islower(c) && (c - 'a') % 2 != 0) {
                 odd_count++;
             }
         }
         result.push_back(to_string(odd_count));
     }
-    
     return result;
+}
+
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
+}
+
+int main() {
+    assert(issame(split_words(""), {"0"}));
+    assert(issame(split_words("hello world"), {"hello", "world"}));
+    assert(issame(split_words("one,two,three"), {"one", "two", "three"}));
+    
+    cout << "All tests passed!" << endl;
+    return 0;
 }
