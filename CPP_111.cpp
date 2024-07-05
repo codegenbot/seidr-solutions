ca30023285830ddeaed42eb49be25e766ed10a09
beep boop <githubbot@vadim.me>
2024-07-05 08:45:04 +0200

    Solution 23 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/a6kdnm0m Prompt: 5b609789-d1f5-41f6-8a9f-311e6a9b9113.cpp:44:6: error: redefinition of ‘bool issa me(std::map<char, int>, std::map<char, int>)’    44 | bool issame(map<char,int> a,map<char,int> b){       |      ^~~~~~ 5b609789-d1f5-41f6-8a9f-311e6a9b9113.cpp:32:6: note: ‘bool issame(std::map<char,  int>, std::map<char, int>)’ previously defined here    32 | bool issame(map<char,int> a, map<char,int> b) {       |      ^~~~~~ 5b609789-d1f5-41f6-8a9f-311e6a9b9113.cpp:57:5: error: redefinition of ‘int main( )’    57 | int main(){       |     ^~~~ 5b609789-d1f5-41f6-8a9f-311e6a9b9113.cpp:36:5: note: ‘int main()’ previously def ined here    36 | int main() {       |     ^~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -36,6 +36,6 @@ bool issame(map<char,int> a, map<char,int> b) {
 int main() {
     assert(issame(histogram("a"), {{'a', 1}}));
     assert(issame(histogram("a b"), {{'a', 1}, {'b', 1}}));
-    assert(issame(histogram("apple banana cherry date apple"), {{'a', 3}}));
+    assert(issame(histogram("apple banana cherry date"), {{'a', 1}, {'b', 1}, {'c', 1}, {'d', 1}}));
     return 0;
 }
