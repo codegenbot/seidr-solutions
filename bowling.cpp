86b1208a446cfc8801b2abd6118611358f274351
beep boop <githubbot@vadim.me>
2024-07-05 10:38:45 +0200

    Solution 4 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-4o-2024-05-13-run1/runs/fjnceahk Prompt:    27 |     string input;       |     ^~~~~~ 7c665264-7462-4862-a038-33c7651905fc.cpp:28:20: error: ‘cin’ was not declared in  this scope    28 |     while (getline(cin, input)) {       |                    ^~~ 7c665264-7462-4862-a038-33c7651905fc.cpp:28:25: error: ‘input’ was not declared in this scope; did you mean ‘int’?    28 |     while (getline(cin, input)) {       |                         ^~~~~       |                         int 7c665264-7462-4862-a038-33c7651905fc.cpp:28:12: error: ‘getline’ was not declare d in this scope    28 |     while (getline(cin, input)) {       |            ^~~~~~~ 7c665264-7462-4862-a038-33c7651905fc.cpp:29:9: error: ‘cout’ was not declared in  this scope    29 |         cout << scoreBowling(input) << endl;       |         ^~~~ 7c665264-7462-4862-a038-33c7651905fc.cpp:29:40: error: ‘endl’ was not declared i n this scope    29 |         cout << scoreBowling(input) << endl;       |                                        ^~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,25 +1,35 @@
-int scoreBowlingGame(const string &game) {
-    int score = 0, frame = 0, i = 0;
-    while (frame < 10) {
-        if (game[i] == 'X') {
-            score += 10 + (game[i + 1] == 'X' ? 10 : (game[i + 1] == '/' ? 10 - (game[i + 1 - 1] - '0') : (game[i + 1] == '-' ? 0 : game[i + 1] - '0'))) + (game[i + 2] == 'X' ? 10 : (game[i + 2] == '/' ? 10 - (game[i + 1] - '0') : (game[i + 2] == '-' ? 0 : game[i + 2] - '0')));
-            i++;
-        } else if (game[i + 1] == '/') {
-            score += 10 + (game[i + 2] == 'X' ? 10 : (game[i + 2] == '-' ? 0 : game[i + 2] - '0'));
-            i += 2;
-        } else {
-            score += (game[i] == '-' ? 0 : game[i] - '0') + (game[i + 1] == '-' ? 0 : game[i + 1] - '0');
-            i += 2;
+#include <iostream>
+#include <string>
+
+int scoreBowling(const std::string& game) {
+    int score = 0;
+    int frame = 1;
+    for (size_t i = 0; i < game.size(); ++i) {
+        if (frame > 10) break;
+        if (game[i] == 'X') { // Strike
+            score += 10;
+            if (i + 1 < game.size()) score += (game[i + 1] == 'X' ? 10 : (game[i + 1] == '/' ? 10 - (game[i] - '0') : (game[i + 1] == '-' ? 0 : game[i + 1] - '0')));
+            if (i + 2 < game.size()) score += (game[i + 2] == 'X' ? 10 : (game[i + 2] == '/' ? 10 - (game[i + 1] - '0') : (game[i + 2] == '-' ? 0 : game[i + 2] - '0')));
+            frame++;
+        } else if (game[i] == '/') { // Spare
+            score += 10 - (game[i - 1] == '-' ? 0 : game[i - 1] - '0');
+            if (i + 1 < game.size()) score += (game[i + 1] == 'X' ? 10 : (game[i + 1] == '-' ? 0 : game[i + 1] - '0'));
+            frame++;
+        } else if (game[i] == '-') { // Miss
+            frame += (i > 0 && game[i - 1] != 'X' && game[i - 1] != '/') ? 1 : 0;
+        } else { // Number
+            score += game[i] - '0';
+            if (i + 1 < game.size() && game[i + 1] != '/' && game[i + 1] != 'X' && game[i + 1] != '-') frame++;
+            else if (game[i + 1] == '/') i++;
         }
-        frame++;
     }
     return score;
 }
 
 int main() {
-    string game;
-    while (getline(cin, game)) {
-        cout << scoreBowlingGame(game) << endl;
+    std::string input;
+    while (std::getline(std::cin, input)) {
+        std::cout << scoreBowling(input) << std::endl;
     }
     return 0;
 }
