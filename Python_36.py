5d1ed43968c2ce862fc170958759231952451ef6
beep boop <githubbot@vadim.me>
2024-07-06 00:46:39 +0200

    Solution 15 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/knm72eso Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3543c516-f01d-4a3c-ad76-2fe7cca29 ad8.py", line 12, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3543c516-f01d-4a3c-ad76-2fe7cca29 ad8.py", line 9, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,7 +1,8 @@
-
 def fizz_buzz(n):
     count = 0
     for i in range(1, n+1):
-        if (i % 3 == 0 and i % 5 == 0) or (i % 3 == 0 and '7' in str(i)) or (i % 5 == 0 and '7' in str(i)):
+        if (i % 13 == 0 and '7' in str(i)) or (i % 11 == 0 and '7' in str(i)):
+            count += 1
+        elif (i % 13 == 0 and '7' not in str(i)) or (i % 11 == 0 and '7' not in str(i)):
             count += 1
     return count
