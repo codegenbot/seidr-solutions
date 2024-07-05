b4671e99b567198ceec294a77944c30c130214f4
beep boop <githubbot@vadim.me>
2024-07-06 01:50:49 +0200

    Solution 12 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dsmr51i9 Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from e21bbd57-fb73-4786-b679-d26f2b369c5e.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e21bbd57-fb73-4786-b679-d26f2b369c5e.cpp:23:44: error: ‘b’ was not declared in t his scope    23 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from e21bbd57-fb73-4786-b679-d26f2b369c5e.cpp:22: e21bbd57-fb73-4786-b679-d26f2b369c5e.cpp: In function ‘int main()’: e21bbd57-fb73-4786-b679-d26f2b369c5e.cpp:32:20: error: ‘filter_by_prefix’ was no t declared in this scope    32 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ e21bbd57-fb73-4786-b679-d26f2b369c5e.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,9 +1,7 @@
 #include <vector>
 #include <string>
 
-using namespace std;
-
-bool issame(const vector<string>& a, const vector<string>& b) {
+bool issame(vector<std::string> a, vector<std::string> b) {
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) return false;
@@ -19,9 +17,4 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
         }
     }
     return result;
-}
-
-int main() {
-    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxxAAA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
-    return 0;
 }
