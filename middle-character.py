82cb3c026befd597b97698bd94f84b0008581453
beep boop <githubbot@vadim.me>
2024-07-05 20:45:22 +0200

    Solution 6 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/z71mln1m Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b83e5c50-4710-442b-a086-f8d790540 ef0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- middle-character.py
+++ middle-character.py
@@ -1,9 +1,6 @@
 def middle_character(s):
-    if not s: 
-        return ""
-    s = str(s)
     length = len(s)
     if length % 2 == 0:
-        return s[length//2 - 1:length//2 + 1]
+        return s[length // 2 - 1:length // 2 + 1]
     else:
-        return s[length//2]
+        return s[length // 2]
