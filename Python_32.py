cd7ff7dcea6008e211ee7168dd6a787eae102a0a
beep boop <githubbot@vadim.me>
2024-07-06 00:58:10 +0200

    Solution 49 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/a39d9ij4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/52ae79f3-df19-4c58-9b74-f0e06924a d70.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,11 +1,16 @@
 ```
-def find_zero(xs):
-    n = len(xs)
-    if n < 2:
-        a, b = xs[0], 1
-    elif n == 2:
-        a, b = xs[0], xs[1]
-    else:
-        a, b, c = xs[0], xs[-1], xs[-3]
-    d = math.sqrt((b**2) - (4 * a * c))
-    return (-b + d) / (2 * a)
+def find_zero(xs: list):
+    if len(xs) < 3:
+        return None
+    for i in range(len(xs)-2):
+        A = xs[i]
+        B = xs[i+1]
+        C = xs[i+2]
+        d = (B**2 - 4*A*C)**0.5
+        r1 = (-B + d) / (2 * A)
+        r2 = (-B - d) / (2 * A)
+        if abs(r1) < 10e-9:
+            return r1
+        elif abs(r2) < 10e-9:
+            return r2
+    return None
