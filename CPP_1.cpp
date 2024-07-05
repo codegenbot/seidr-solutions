6beaf52f1577ed604f55279a14e0b8ccc0e9c502
beep boop <githubbot@vadim.me>
2024-07-06 00:47:18 +0200

    Solution 2 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gbyn86lg Prompt:    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ fa269307-88f8-4ec0-bd53-5fd3cebe0f25.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ fa269307-88f8-4ec0-bd53-5fd3cebe0f25.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from fa269307-88f8-4ec0-bd53-5fd3cebe0f25.cpp:23: fa269307-88f8-4ec0-bd53-5fd3cebe0f25.cpp: In function ‘int main()’: fa269307-88f8-4ec0-bd53-5fd3cebe0f25.cpp:33:20: error: ‘separate_paren_groups’ w as not declared in this scope    33 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ fa269307-88f8-4ec0-bd53-5fd3cebe0f25.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,7 +1,14 @@
+#include <vector>
+#include <string>
+
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
+}
+
 vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
-    string current_group = "";
     int open_count = 0;
+    string current_group;
 
     for (char c : paren_string) {
         if (c == '(') {
@@ -12,10 +19,15 @@ vector<string> separate_paren_groups(string paren_string) {
             current_group += c;
             if (open_count == 0) {
                 result.push_back(current_group);
-                current_group = "";
+                current_group.clear();
             }
         }
     }
 
     return result;
+}
+
+int main() {
+    assert(separate_paren_groups("( ) (( ")) == {"()", "(())"};
+    return 0;
 }
