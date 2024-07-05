b1532c722bef239c01305295a0fc85798c28bdf9
beep boop <githubbot@vadim.me>
2024-07-05 03:07:36 +0200

    Solution 16 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/870y1cfu Prompt: 16f3300b-1323-445f-b442-fa8ba811a6b7.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 16f3300b-1323-445f-b442-fa8ba811a6b7.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -28,6 +28,6 @@ string Strongest_Extension(string class_name, vector<string> extensions) {
 }
 
 int main() {
-    assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.Bb");
+    assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
     return 0;
 }
