2764329433ba5d6a9285bdede9d37a8cf9884373
beep boop <githubbot@vadim.me>
2024-07-06 01:38:37 +0200

    Solution 20 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/tn9pfa00 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/037993b1-6bc1-4aa1-8076-2deb955a0 efb.py", line 17, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/037993b1-6bc1-4aa1-8076-2deb955a0 efb.py", line 14, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_105.py
+++ Python_105.py
@@ -1,8 +1,12 @@
 def by_length(arr):
     result = []
     for num in arr:
+        if isinstance(num, str):
+            num = int(num)
         if isinstance(num, int) and 1 <= num <= 9:
             result.append(str(num))
         elif isinstance(num, str):
             result.append(num.capitalize())
-    return sorted(result, reverse=True)
+    result.sort()
+    result.reverse()
+    return result
