542f6091ccd93326c77c84f9c58b58c6d17ab16a
beep boop <githubbot@vadim.me>
2024-07-05 03:31:38 +0200

    Solution 32 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/qwwwdj0f Prompt:       |                    ^~~ c7e17b98-e2c8-407b-848d-e4babf15254d.cpp:24:27: error: ‘vector’ was not declared  in this scope    24 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c7e17b98-e2c8-407b-848d-e4babf15254d.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c7e17b98-e2c8-407b-848d-e4babf15254d.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c7e17b98-e2c8-407b-848d-e4babf15254d.cpp:23: c7e17b98-e2c8-407b-848d-e4babf15254d.cpp: In function ‘int main()’: c7e17b98-e2c8-407b-848d-e4babf15254d.cpp:33:20: error: ‘parse_nested_parens’ was  not declared in this scope    33 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ c7e17b98-e2c8-407b-848d-e4babf15254d.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
 #include <vector>
 #include <string>
 #include <cassert>
 
 using namespace std;
 
-vector<int> parse_nested_parens(const string& paren_string) {
+vector<int> parse_nested_parens(string paren_string) {
     vector<int> result;
     int max_depth = 0, current_depth = 0;
     
@@ -23,7 +22,7 @@ vector<int> parse_nested_parens(const string& paren_string) {
             current_depth = 0;
         }
     }
-    result.push_back(max_depth);
+    result.push_back(max_depth); // for the last group
     return result;
 }
 
@@ -32,9 +31,9 @@ bool issame(const vector<int>& a, const vector<int>& b) {
 }
 
 int main() {
-    assert(issame(parse_nested_parens("(()(()) ((())))"), {3, 3}));
-    assert(issame(parse_nested_parens("(()) () (())"), {2, 1, 2}));
-    assert(issame(parse_nested_parens("(()) (())"), {2, 2}));
-    cout << "All tests passed!" << endl;
+    assert(issame(parse_nested_parens("(()(())((())))"), {4}));
+    assert(issame(parse_nested_parens("(()) ()"), {2, 1}));
+    assert(issame(parse_nested_parens("() ((()))"), {1, 3}));
+    assert(issame(parse_nested_parens("() (()) (())"), {1, 2, 2}));
     return 0;
 }
