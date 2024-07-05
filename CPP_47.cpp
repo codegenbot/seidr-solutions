ec25eb2aa9a3049bfa9d6e0f4886d6a7b233ecf6
beep boop <githubbot@vadim.me>
2024-07-05 03:10:42 +0200

    Solution 62 of CPP/47, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/jtkbkmmu Prompt: eba5ffab-a867-4680-bc60-a57b13f6b298.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ eba5ffab-a867-4680-bc60-a57b13f6b298.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_47.cpp
+++ CPP_47.cpp
@@ -14,6 +14,6 @@ double median(std::vector<int> l) {
 }
 
 int main() {
-    assert(abs(median({8, 1, 3, 9, 9, 2, 7}) - 7.0) < 1e-4);
+    assert(fabs(median({8, 1, 3, 9, 9, 2, 7}) - 7.0) < 1e-4);
     return 0;
 }
