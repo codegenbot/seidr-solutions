dda32f9ea2f9792c566e1771688f88c7257b2fc6
beep boop <githubbot@vadim.me>
2024-07-05 14:19:36 +0200

    Solution 5 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/jtaa5psc Prompt: Complete the following code given the task description and function signature.

--- Python_1.py
+++ Python_1.py
@@ -10,6 +10,7 @@ def separate_paren_groups(paren_string: str) -> List[str]:
             balance += 1
         elif char == ")":
             balance -= 1
+
         if balance == 0 and current_group:
             result.append("".join(current_group))
             current_group = []
