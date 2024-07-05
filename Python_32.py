bab3f1e1c56b55d84735b55f93b7c0c8a537db7d
beep boop <githubbot@vadim.me>
2024-07-06 00:57:47 +0200

    Solution 46 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/a39d9ij4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/52ae79f3-df19-4c58-9b74-f0e06924a d70.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,22 +1,9 @@
 ```
 import math
-
 def find_zero(xs: list):
-    a = xs[0]
-    b = xs[1]
-    c = xs[-1]
-    D = b**2 - 4*a*c
-    if D < 0:
-        return "The function has no real solutions."
-    elif D == 0:
-        x = -b / (2 * a)
-        return x
+    if len(xs) < 2:
+        return -1/(xs[0] + xs[1])
+    elif len(xs) == 2:
+        return -xs[1] / xs[0]
     else:
-        x1 = (-b + math.sqrt(D)) / (2 * a)
-        x2 = (-b - math.sqrt(D)) / (2 * a)
-        if x1 > x2:
-            return f"Two solutions: {x2} and {x1}"
-        elif x2 > x1:
-            return f"Two solutions: {x1} and {x2}"
-        else:
-            return "One solution."
+        return (-xs[-2] + math.sqrt((xs[-1]**2) - (4 * xs[0] * xs[-3]))) / (2 * xs[0])
