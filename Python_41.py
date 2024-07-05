c2108ebf423cca61fd421ed2301c87d9198b800d
beep boop <githubbot@vadim.me>
2024-07-06 00:58:27 +0200

    Solution 34 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/mavlb90z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/30c26f29-4e3e-48c5-a5c4-d99f40b18 2da.py", line 14, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/30c26f29-4e3e-48c5-a5c4-d99f40b18 2da.py", line 11, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_41.py
+++ Python_41.py
@@ -1,9 +1,8 @@
 
 def car_race_collision(n: int):
-   left_to_right = [i for i in range(1, n + 1)]
-   right_to_left = [i for i in range(n, 0, -1)]
-   collisions = 0
-   for x in left_to_right:
-       if x in right_to_left:
-           collisions += 1
-   return collisions
+    left_to_right = [i for i in range(1, n + 1)]
+    right_to_left = [i for i in range(n, 0, -1)]
+    if n % 2 == 1:
+        return len([x for x in left_to_right if x in right_to_left]) + 1
+    else:
+        return n // 2
