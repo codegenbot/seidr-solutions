963598dcdcb094acee68d83d5ac65b76f188e510
beep boop <githubbot@vadim.me>
2024-07-06 01:38:50 +0200

    Solution 69 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/1lrd2vll Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/69a814f4-bd7d-41a9-9d7c-ef53af65d dc9.py", line 31, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/69a814f4-bd7d-41a9-9d7c-ef53af65d dc9.py", line 27, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg"     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/69a814f4-bd7d-41a9-9d7c-ef53af65d dc9.py", line 18, in encode     encoded_char = vowels[char].lower()                    ~~~~~~^^^^^^ KeyError: 'D'

--- Python_93.py
+++ Python_93.py
@@ -1,24 +1,23 @@
-
 def encode(message):
-    vowels = {
-        "a": "A",
-        "e": "E",
-        "i": "I",
-        "o": "O",
-        "u": "U",
-        "I": "?",
-        " ": "",
-        "d": "",
-        "D": ""  # Add this line to fix the error
-    }
-    encoded_message = ""
-    for char in message:
-        if char.isalpha():
-            if char.islower():
-                encoded_char = vowels[char]
-            elif char.isupper():
-                encoded_char = vowels[char].lower()
-        else:
-            if char not in vowels:
-                encoded_message += char
-    return encoded_message
+   vowels = {
+       "a": "A",
+       "e": "E",
+       "i": "I",
+       "o": "O",
+       "u": "U",
+       "I": "?",
+       " ": "",
+       "d": "",
+       "D": "" # Add this line to fix the error
+   }
+   encoded_message = ""
+   for char in message:
+       if char.isalpha():
+           if char.islower():
+               encoded_char = vowels[char]
+           elif char.isupper():
+               encoded_char = vowels[char].lower()
+       else:
+           if char not in vowels:
+               encoded_message += char
+   return encoded_message
