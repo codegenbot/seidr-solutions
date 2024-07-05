836bc9d18419b0dce495f0d23bc500c0394a5aac
beep boop <githubbot@vadim.me>
2024-07-05 13:49:04 +0200

    Solution 32 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/gqupubcf Prompt: Program failed

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -14,12 +14,12 @@ bool same_chars(string s0, string s1) {
 int main() {
     string s0, s1;
     cin >> s0 >> s1;
-    
+
     if (same_chars(s0, s1)) {
         cout << "Yes" << endl;
     } else {
         cout << "No" << endl;
     }
-    
+
     return 0;
 }
