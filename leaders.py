35d1ca126cbb4533c671478bf073bfa14b10a734
beep boop <githubbot@vadim.me>
2024-07-05 20:32:55 +0200

    Solution 11 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/1yygkbrx Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d8836cb4-63e2-4bbf-8130-e2ada736d 168.py", line 1     ```     ^ SyntaxError: invalid syntax

--- leaders.py
+++ leaders.py
@@ -1,2 +1,2 @@
 def leaders(input_vector):
-    return [x for i, x in reversed(list(enumerate(input_vector))) if not any(x < y for y in input_vector[i + 1:])]
+    return [x for i,x in reversed(list(enumerate(input_vector)))]
