56baf6934eca0e15b1831f496029951fe780c602
beep boop <githubbot@vadim.me>
2024-07-05 03:58:13 +0200

    Solution 27 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/5kyl0q6z Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3ed83e47-0ccc-4fac-b676-60da8be1a6fb.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3ed83e47-0ccc-4fac-b676-60da8be1a6fb.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3ed83e47-0ccc-4fac-b676-60da8be1a6fb.cpp:11: 3ed83e47-0ccc-4fac-b676-60da8be1a6fb.cpp: In function ‘int main()’: 3ed83e47-0ccc-4fac-b676-60da8be1a6fb.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 3ed83e47-0ccc-4fac-b676-60da8be1a6fb.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -8,7 +8,6 @@
 using namespace std;
 using boost::any;
 
-// Function to filter integers from a list of any type
 vector<int> filter_integers(const list<any>& values) {
     vector<int> result;
     for (const auto& val : values) {
@@ -19,13 +18,11 @@ vector<int> filter_integers(const list<any>& values) {
     return result;
 }
 
-// Function to check if two vectors of integers are the same
 bool issame(const vector<int>& a, const vector<int>& b) {
     return a == b;
 }
 
 int main() {
-    // Testing the function
     assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
     cout << "Test passed!" << endl;
     return 0;
