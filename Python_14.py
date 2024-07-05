0222b9e59159cb5321ffce918e25ef1f5b07fd0a
beep boop <githubbot@vadim.me>
2024-07-05 05:28:34 +0200

    Solution 62 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/sfbiof79 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d0daa85b-520b-407e-9aba-03bb76059 bf4.py", line 7, in <module>     check(all_prefixes)           ^^^^^^^^^^^^ NameError: name 'all_prefixes' is not defined

--- Python_14.py
+++ Python_14.py
@@ -2,6 +2,6 @@ def generate_substrings(string):
     return [string[:i] for i in range(1, len(string) + 1)]
 
 if __name__ == "__main__":
-    input_string = input("Enter a string: ")
+    input_string = input()
     all_prefixes = generate_substrings(input_string)
     print(all_prefixes)
