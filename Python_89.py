6be8193bc349edac74de16c467ca4dcefea8d158
beep boop <githubbot@vadim.me>
2024-07-06 01:53:56 +0200

    Solution 71 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/4ip2inle Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/789685b4-72cd-4fb2-a203-78be91ffc f72.py", line 12, in <module>     check(encrypt)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/789685b4-72cd-4fb2-a203-78be91ffc f72.py", line 8, in check     encrypt("a") == "e"     ^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/789685b4-72cd-4fb2-a203-78be91ffc f72.py", line 2, in encrypt     table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxy za", padchar=' ')             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^ TypeError: str.maketrans() takes no keyword arguments

--- Python_89.py
+++ Python_89.py
@@ -1,4 +1,3 @@
 def encrypt(s):
-    # Create a translation table that maps each letter to its encrypted value
     table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyza")
     return s.translate(table)
