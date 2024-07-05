ecd0d6407b5ccaf0e635fba5c689a758c4359809
beep boop <githubbot@vadim.me>
2024-07-05 05:30:05 +0200

    Solution 2 of CPP/76, ratio 0.875 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/4ynzanoi Prompt: Complete the following code given the task description and function signature.

--- CPP_76.cpp
+++ CPP_76.cpp
@@ -1,6 +1,6 @@
 bool is_simple_power(int x, int n) {
+    if (x < 1 || n < 1) return false;
     if (x == 1) return true;
-    if (n == 1) return x == 1;
     while (x % n == 0) {
         x /= n;
     }
