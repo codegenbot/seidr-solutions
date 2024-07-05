e36587f4d6c81593f65f882f953e9eea1ed3ef11
beep boop <githubbot@vadim.me>
2024-07-05 06:16:58 +0200

    Solution 16 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/in201gy7 Prompt:    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8ea54130-2db7-49d7-b880-76d154f79757.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8ea54130-2db7-49d7-b880-76d154f79757.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8ea54130-2db7-49d7-b880-76d154f79757.cpp:24: 8ea54130-2db7-49d7-b880-76d154f79757.cpp: In function ‘int main()’: 8ea54130-2db7-49d7-b880-76d154f79757.cpp:34:20: error: ‘largest_smallest_integer s’ was not declared in this scope    34 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 8ea54130-2db7-49d7-b880-76d154f79757.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include <vector>
 #include <cassert>
+
 using namespace std;
 
 vector<int> largest_smallest_integers(vector<int> lst) {
@@ -30,6 +32,12 @@ bool issame(vector<int> a, vector<int> b) {
 }
 
 int main() {
-    assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
+    assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
+    assert(issame(largest_smallest_integers({1, 2, 3, 4, 5}), {0, 1}));
+    assert(issame(largest_smallest_integers({-1, -2, -3, -4, -5}), {-1, 0}));
+    assert(issame(largest_smallest_integers({}), {0, 0}));
+
+    cout << "All test cases passed!" << endl;
+
     return 0;
 }
