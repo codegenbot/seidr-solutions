ffe54d1b961cf2fa4126d4b8f1bef4c615d0443a
beep boop <githubbot@vadim.me>
2024-07-06 01:37:02 +0200

    Solution 46 of Python/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/j5uln9v9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1376b25f-0a0c-4987-8e71-cb18b3fb1 41b.py", line 46, in <module>     check(count_up_to)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1376b25f-0a0c-4987-8e71-cb18b3fb1 41b.py", line 17, in check     assert count_up_to(101) == [            ^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_96.py
+++ Python_96.py
@@ -1,44 +1,7 @@
 def count_up_to(n):
-    result = []
-    for i in range(2, n + 1):
-        if is_prime(i):
-            result.append(i)
-    return result
-
-
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    return True
-
-
-assert count_up_to(101) == [
-    2,
-    3,
-    5,
-    7,
-    11,
-    13,
-    17,
-    19,
-    23,
-    29,
-    31,
-    37,
-    41,
-    43,
-    47,
-    53,
-    59,
-    61,
-    67,
-    71,
-    73,
-    79,
-    83,
-    89,
-    97,
-]
+   result = []
+   for i in range(2, n + 1):
+       if is_prime(i):
+           result.append(i)
+   return result
+```
