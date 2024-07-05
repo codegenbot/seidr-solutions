01df314c18d3d5d676261e5043f3826313cdce33
beep boop <githubbot@vadim.me>
2024-07-05 06:24:36 +0200

    Solution 33 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/l30e6omo Prompt: 4ba3f8d3-a232-43dd-915a-7c0bad4465a8.cpp:25:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 4ba3f8d3-a232-43dd-915a-7c0bad4465a8.cpp:15:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    15 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 4ba3f8d3-a232-43dd-915a-7c0bad4465a8.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 4ba3f8d3-a232-43dd-915a-7c0bad4465a8.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 vector<int> make_a_pile(int n) {
     vector<int> pile;
-    int current_stones = 8;
+    int current_stones = n;
     for (int i = 0; i < n; ++i) {
         pile.push_back(current_stones);
         current_stones += 2;
