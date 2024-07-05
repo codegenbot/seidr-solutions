74e0ee7bed04fe7554198c11ddcdb95d6b698520
beep boop <githubbot@vadim.me>
2024-07-06 01:33:29 +0200

    Solution 55 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/t2jqbixf Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -1,12 +1,5 @@
-import click
-
-
 @click.command()
 def decode_shift(message, shift):
     return "".join(
         [chr((ord(ch) - shift - ord("a")) % 26 + ord("a")) for ch in message]
     )
-
-
-result = decode_shift(message, shift)
-print(result)
