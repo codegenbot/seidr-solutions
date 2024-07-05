035d51af8569fad7a9abd8887d2b5521ca3e4bec
beep boop <githubbot@vadim.me>
2024-07-05 08:31:11 +0200

    Solution 31 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/txqplh3v Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/93403c4a-89d8-48b8-8056-48a413c5d b44.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/93403c4a-89d8-48b8-8056-48a413c5d b44.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,10 +1,11 @@
 def double_the_difference(lst):
     return sum(x**2 for x in lst if isinstance(x, int) and x > 0 and x % 2 != 0)
 
+
 def check(func):
     test_lst = [1, 2, 3, 4, 5]
-    expected_result = 1**2 + 3**2 + 5**2  # 1 + 9 + 25 = 35
+    expected_result = 1**2 + 3**2 + 5**2
     assert func(test_lst) == expected_result, "Test failed"
-    
+
+
 check(double_the_difference)
-print(double_the_difference([1, -2, 3, 'a', 4, 5]))  # Example test case, expected: 35
