85eb1461397383d512f000b41577e9114659f259
beep boop <githubbot@vadim.me>
2024-07-05 16:32:42 +0200

    Solution 41 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/4nokqpm8 Prompt: d308ac14-79da-40d0-84ea-6fbdd6e3c08e.cpp:28:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    28 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ d308ac14-79da-40d0-84ea-6fbdd6e3c08e.cpp:18:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    18 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ d308ac14-79da-40d0-84ea-6fbdd6e3c08e.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ d308ac14-79da-40d0-84ea-6fbdd6e3c08e.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -21,6 +21,5 @@ bool issame(vector<int> a, vector<int> b) {
 
 int main() {
     assert (issame(sum_product({10}) ,{10, 10}));
-    assert (issame(sum_product({1, 2, 3, 4}) ,{10, 24}));
     return 0;
 }
