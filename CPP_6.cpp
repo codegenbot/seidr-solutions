905511f2421a8d1f22391269ba48a3a14a3afa8f
beep boop <githubbot@vadim.me>
2024-07-05 16:17:31 +0200

    Solution 39 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/8e5rf50f Prompt: 6b4e8f08-0591-47ac-91e7-362627504a8f.cpp:41:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    41 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 6b4e8f08-0591-47ac-91e7-362627504a8f.cpp:28:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    28 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 6b4e8f08-0591-47ac-91e7-362627504a8f.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 6b4e8f08-0591-47ac-91e7-362627504a8f.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -21,7 +21,7 @@ vector<int> parse_nested_parens(string paren_string) {
             current_depth = 0;
         }
     }
-    result.push_back(max_depth); // For the last group
+    result.push_back(max_depth);
     return result;
 }
 
