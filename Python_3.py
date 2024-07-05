e6725534847496601fc874b916ab2f9ba3bdf42d
beep boop <githubbot@vadim.me>
2024-07-05 04:04:52 +0200

    Solution 28 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/4vgl0faa Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/02861143-b921-4ad3-b87d-00c648026 5a2.py", line 2     for operation in operations: IndentationError: unexpected indent

--- Python_3.py
+++ Python_3.py
@@ -1,9 +1,6 @@
-from typing import List
-
-def below_zero(operations: List[int]) -> bool:
-    balance = 0
-    for op in operations:
-        balance += op
-        if balance < 0:
-            return True
-    return False
+balance = 0
+for operation in operations:
+    balance += operation
+    if balance < 0:
+        return True
+return False
