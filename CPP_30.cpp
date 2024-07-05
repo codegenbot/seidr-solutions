23bd00da238cd58a6171bcf518a57aff4cfeebd5
beep boop <githubbot@vadim.me>
2024-07-06 01:45:07 +0200

    Solution 3 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/p3so980n Prompt:       |                    ^~~~~ a19144fb-278e-41e4-8c86-68534da1e6a0.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ a19144fb-278e-41e4-8c86-68534da1e6a0.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ a19144fb-278e-41e4-8c86-68534da1e6a0.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from a19144fb-278e-41e4-8c86-68534da1e6a0.cpp:11: a19144fb-278e-41e4-8c86-68534da1e6a0.cpp: In function ‘int main()’: a19144fb-278e-41e4-8c86-68534da1e6a0.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ a19144fb-278e-41e4-8c86-68534da1e6a0.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,16 +1,16 @@
 #include <vector>
-#include <iostream>
+#include <algorithm>
 
-bool issame(float a, float b) {
-    return a == b;
+bool issame(vector<float> l) {
+    float first = *l.begin();
+    for (float num : l) {
+        if (num != first)
+            return false;
+    }
+    return true;
 }
 
-std::vector<float> get_positive(std::vector<float> l) {
-    std::vector<float> result;
-    for (float num : l) {
-        if (num > 0) {
-            result.push_back(num);
-        }
-    }
-    return result;
+int main() {
+    vector<float> l = {1.0, 2.0, 3.0};
+    cout << issame(l) << endl;
 }
