5ca94510dbfab7a0f52ffaac0180343161530bb9
beep boop <githubbot@vadim.me>
2024-07-06 01:15:17 +0200

    Solution 28 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/09qczbg5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/45f4b939-8070-4562-9d74-93de76541 114.py", line 16, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/45f4b939-8070-4562-9d74-93de76541 114.py", line 13, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/45f4b939-8070-4562-9d74-93de76541 114.py", line 3, in fruit_distribution     apple_count = re.search("\d+", s.split("and")[0])                   ^^ NameError: name 're' is not defined

--- Python_67.py
+++ Python_67.py
@@ -1,11 +1,6 @@
+
 def fruit_distribution(s, n):
-    import re
-
-    # Parse the input string to extract the number of apples and oranges
-    apple_count = int(re.search("\d+", s.split("and")[0])[0])
-    orange_count = int(re.search("\d+", s.split("and")[1])[0])
-
-    # Calculate the number of mango fruits in the basket
-    mango_count = n - apple_count - orange_count
-
+    apple_count = re.search("\d+", s.split("and")[0])
+    orange_count = re.search("\d+", s.split("and")[1])
+    mango_count = n - int(apple_count) - int(orange_count)
     return mango_count
