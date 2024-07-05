8cf537a762099ffe4222d36c1c796adacad1b47c
beep boop <githubbot@vadim.me>
2024-07-06 00:54:17 +0200

    Solution 57 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tpufcn5h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a1134ee8-2a57-4ba9-89d2-b8c267003 410.py", line 19, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a1134ee8-2a57-4ba9-89d2-b8c267003 410.py", line 14, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a1134ee8-2a57-4ba9-89d2-b8c267003 410.py", line 8, in sort_third     elif min(x for x in l[:j] if x % 3 != 0) is None or min(x for x in l[:j] if x % 3 != 0) <= l[j]:          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ValueError: min() arg is an empty sequence

--- Python_33.py
+++ Python_33.py
@@ -5,7 +5,10 @@ def sort_third(l):
     while j < len(l):
         if l[j] % 3 != 0:
             result.append(l.pop(j))
-        elif (x for x in l[:j] if x % 3 != 0) and (min(x for x in l[:j] if x % 3 != 0) is None or min(x for x in l[:j] if x % 3 != 0) <= l[j]):
+        elif (x for x in l[:j] if x % 3 != 0) and (
+            min(x for x in l[:j] if x % 3 != 0) is None
+            or min(x for x in l[:j] if x % 3 != 0) <= l[j]
+        ):
             result.append(l.pop(j))
         else:
             j += 1
