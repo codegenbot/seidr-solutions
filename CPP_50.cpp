5845d36c0382dec04defe207a51e1d211ad2f2d5
beep boop <githubbot@vadim.me>
2024-07-05 04:33:42 +0200

    Solution 0 of CPP/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/czjxlopd Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_50.cpp
@@ -0,0 +1,8 @@
+string decode_shift(string s){
+    string out;
+    for (int i = 0; i < s.length(); i++) {
+        int w = ((int)s[i] - 5 - (int)'a' + 26) % 26 + (int)'a';
+        out = out + (char)w;
+    }
+    return out;
+}
