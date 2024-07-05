b04eb56d6f4456d670d929e9351228a4315520cf
beep boop <githubbot@vadim.me>
2024-07-05 06:36:33 +0200

    Solution 64 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/rx2n5xgr Prompt: 78c7fa5a-c047-4e54-b774-ba794461cb51.cpp:39:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    39 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 78c7fa5a-c047-4e54-b774-ba794461cb51.cpp:28:6: note: ‘bool issame(std::vector<st d::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> > )’ previously defined here    28 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ 78c7fa5a-c047-4e54-b774-ba794461cb51.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 78c7fa5a-c047-4e54-b774-ba794461cb51.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -25,12 +25,8 @@ vector<string> numerical_letter_grade(vector<float> grades) {
     return letterGrades;
 }
 
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-
 int main() {
-    assert(issame(numerical_letter_grade({0, 0.7}), {"E", "D-"}));
+    assert((numerical_letter_grade({0, 0.7}) == vector<string>{"E", "D-"}));
     cout << "All tests passed successfully!" << endl;
     return 0;
 }
