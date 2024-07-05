5e06c7fb06ce960898a7695def80a5727ec2885b
beep boop <githubbot@vadim.me>
2024-07-05 11:30:46 +0200

    Solution 1 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/7j6vsuj3 Prompt: Complete the following code given the task description and function signature.

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -2,10 +2,8 @@ int specialFilter(vector<int> nums) {
     int count = 0;
     for (int num : nums) {
         if (num > 10) {
-            string str = to_string(abs(num));
-            int firstDigit = str[0] - '0';
-            int lastDigit = str[str.size() - 1] - '0';
-            if ((firstDigit % 2 != 0) && (lastDigit % 2 != 0)) {
+            string str = to_string(num);
+            if ((str[0] - '0') % 2 != 0 && (str.back() - '0') % 2 != 0) {
                 count++;
             }
         }
