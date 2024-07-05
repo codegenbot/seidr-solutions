06f1616387665e3b94ccbd813103f8eeeb2a072a
beep boop <githubbot@vadim.me>
2024-07-06 01:10:21 +0200

    Solution 24 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/4ip2inle Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/87e1e987-d69d-434f-9bae-3c8ddd5d4 378.py", line 15, in <module>     check(encrypt)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/87e1e987-d69d-434f-9bae-3c8ddd5d4 378.py", line 11, in check     encrypt("a") == "e" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_89.py
+++ Python_89.py
@@ -1,6 +1,9 @@
 def encrypt(s):
-new_string = ""
-for char in s:
-    new_char = chr((ord(char) + 2 - ord("a")) % 26 + ord("a"))
-    new_string += new_char
-return new_string
+    new_string = ""
+    for char in s:
+        if char.isupper():
+            new_char = chr((ord(char) + 2 - ord("A")) % 26 + ord("A"))
+        else:
+            new_char = chr((ord(char) + 2 - ord("a")) % 26 + ord("a"))
+        new_string += new_char
+    return new_string
