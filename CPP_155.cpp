5c7b75c16085e2b53705c64253127ec3aedb4f7b
beep boop <githubbot@vadim.me>
2024-07-05 03:08:41 +0200

    Solution 4 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/q7yjpynu Prompt:       |                    ^~~ 4f71efde-0d79-402d-ae85-28d26d201256.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4f71efde-0d79-402d-ae85-28d26d201256.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4f71efde-0d79-402d-ae85-28d26d201256.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4f71efde-0d79-402d-ae85-28d26d201256.cpp:16: 4f71efde-0d79-402d-ae85-28d26d201256.cpp: In function ‘int main()’: 4f71efde-0d79-402d-ae85-28d26d201256.cpp:26:20: error: ‘even_odd_count’ was not declared in this scope    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 4f71efde-0d79-402d-ae85-28d26d201256.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <cassert>
+#include <cassert> // For assert
+using namespace std;
 
-std::vector<int> even_odd_count(int num) {
+vector<int> even_odd_count(int num) {
     int even_count = 0, odd_count = 0;
     num = abs(num);
-    if (num == 0) {
-        even_count = 1;
-    } else {
-        while (num > 0) {
-            int digit = num % 10;
-            if (digit % 2 == 0) {
-                even_count++;
-            } else {
-                odd_count++;
-            }
-            num /= 10;
+    while (num > 0) {
+        int digit = num % 10;
+        if (digit % 2 == 0) {
+            even_count++;
+        } else {
+            odd_count++;
         }
+        num /= 10;
     }
     return {even_count, odd_count};
 }
 
-bool issame(std::vector<int> a, std::vector<int> b){
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
 int main() {
     assert(issame(even_odd_count(0), {1, 0}));
+    cout << "All tests passed!" << endl;
     return 0;
 }
