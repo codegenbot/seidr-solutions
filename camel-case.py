da0a3a0addccb762c95872e9971217a9f97297db
beep boop <githubbot@vadim.me>
2024-07-05 19:26:32 +0200

    Solution 19 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/hntzbaeu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4e21ad84-4ede-4849-be40-f934c4195 284.py", line 1     ```     ^ SyntaxError: invalid syntax

--- camel-case.py
+++ camel-case.py
@@ -3,9 +3,9 @@ def """
     Take a string in kebab-case and convert all of the words to camelCase.
     Each group of words to convert is delimited by "-", and each grouping is separated by a space.
     For example: "camel-case example-test-string"-> "camelCase exampleTestString".
-    """
-words = s.replace("-", " ").split()
-return "".join(word.capitalize() if i > 0 else word.lower() for i, word in enumerate(words)).replace(" ", "")
+"""
+    words = s.replace("-", " ").split()
+    return "".join(word.capitalize() if i > 0 else word.lower() for i, word in enumerate(words)).replace(" ", "")
 
 print(camel_case("nospaceordash"))
 print(camel_case("two-words"))
