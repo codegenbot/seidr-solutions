7d1a505076ac018e880cfbcac80b83367ddbaca4
beep boop <githubbot@vadim.me>
2024-07-05 08:33:08 +0200

    Solution 17 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/2mcf6rcs Prompt:       |                    ^~~ 0585c9aa-7d8e-46a7-9fae-a095472717bd.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0585c9aa-7d8e-46a7-9fae-a095472717bd.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0585c9aa-7d8e-46a7-9fae-a095472717bd.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0585c9aa-7d8e-46a7-9fae-a095472717bd.cpp:16: 0585c9aa-7d8e-46a7-9fae-a095472717bd.cpp: In function ‘int main()’: 0585c9aa-7d8e-46a7-9fae-a095472717bd.cpp:26:20: error: ‘even_odd_count’ was not declared in this scope    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 0585c9aa-7d8e-46a7-9fae-a095472717bd.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -8,6 +8,10 @@ using namespace std;
 vector<int> even_odd_count(int num) {
     int even_count = 0, odd_count = 0;
     num = abs(num);
+    if (num == 0) {
+        even_count = 1;
+        return {even_count, odd_count};
+    }
     while (num > 0) {
         int digit = num % 10;
         if (digit % 2 == 0) {
@@ -17,7 +21,6 @@ vector<int> even_odd_count(int num) {
         }
         num /= 10;
     }
-    if (num == 0) even_count++; // Special case for 0
     return {even_count, odd_count};
 }
 
@@ -26,7 +29,7 @@ bool issame(vector<int> a, vector<int> b) {
 }
 
 int main() {
-    assert(issame(even_odd_count(0), {1, 0})); // Fixed to {1, 0} since 0 is considered even
+    assert(issame(even_odd_count(0), {1, 0}));
     assert(issame(even_odd_count(123456), {3, 3}));
     assert(issame(even_odd_count(2468), {4, 0}));
     assert(issame(even_odd_count(13579), {0, 5}));
