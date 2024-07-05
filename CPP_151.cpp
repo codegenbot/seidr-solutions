2ead12fc9ff9f46101726dc00f08c1bd19c34ede
beep boop <githubbot@vadim.me>
2024-07-05 03:00:30 +0200

    Solution 1 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/8agnmp57 Prompt: Complete the following code given the task description and function signature.

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,8 +1,8 @@
 long long double_the_difference(vector<float> lst) {
     long long sum = 0;
     for (float num : lst) {
-        if (num > 0 && floor(num) == num && ((int)num) % 2 != 0) {
-            sum += (long long)num * (long long)num;
+        if (num == floor(num) && num > 0 && ((int)num) % 2 != 0) {
+            sum += (int)num * (int)num;
         }
     }
     return sum;
