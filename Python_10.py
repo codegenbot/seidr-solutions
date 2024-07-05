a621025e4af233bb9e476088df59954dd6cbcdf4
beep boop <githubbot@vadim.me>
2024-07-05 03:36:42 +0200

    Solution 3 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/g0uwmik8 Prompt: Complete the following code given the task description and function signature.

--- Python_10.py
+++ Python_10.py
@@ -1,6 +1,4 @@
 def make_palindrome(string: str) -> str:
-    if is_palindrome(string):
-        return string
     for i in range(len(string)):
         if is_palindrome(string[i:]):
             return string + string[:i][::-1]
