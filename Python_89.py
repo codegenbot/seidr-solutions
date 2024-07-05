459579eaf903c851b35f303c4aa4a2775f78c222
beep boop <githubbot@vadim.me>
2024-07-06 00:56:53 +0200

    Solution 6 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/4ip2inle Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9d36f756-0937-47f2-950d-b160c2f45 9ab.py", line 15, in <module>     check(encrypt)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9d36f756-0937-47f2-950d-b160c2f45 9ab.py", line 11, in check     encrypt("a") == "e" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_89.py
+++ Python_89.py
@@ -1,8 +1,6 @@
 
 def encrypt(s):
-    new_string = ""
-    for char in s:
-        offset = ord("a") - ord("z")
-        new_char = chr((ord(char) + offset) % 26 + ord("a"))
-        new_string += new_char
-    return new_string
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    rotated_alphabet = alphabet[2:] + alphabet[:2]
+    table = str.maketrans(alphabet, rotated_alphabet)
+    return s.translate(table)[::-1]
