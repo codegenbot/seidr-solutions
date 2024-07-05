7850faa689ade413df8b627c12b9eab749463291
beep boop <githubbot@vadim.me>
2024-07-05 05:31:28 +0200

    Solution 8 of CPP/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/5zyik2ui Prompt:    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d40d7e89-580b-485e-8af7-96aa37641a41.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d40d7e89-580b-485e-8af7-96aa37641a41.cpp:21:45: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d40d7e89-580b-485e-8af7-96aa37641a41.cpp:20: d40d7e89-580b-485e-8af7-96aa37641a41.cpp: In function ‘int main()’: d40d7e89-580b-485e-8af7-96aa37641a41.cpp:30:20: error: ‘words_string’ was not de clared in this scope    30 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |                    ^~~~~~~~~~~~ d40d7e89-580b-485e-8af7-96aa37641a41.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~

--- CPP_101.cpp
+++ CPP_101.cpp
@@ -1,7 +1,7 @@
+#include <iostream>
 #include <vector>
 #include <string>
 #include <cassert>
-#include <iostream>
 
 using namespace std;
 
@@ -25,17 +25,11 @@ vector<string> words_string(string s) {
 }
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size())
-        return false;
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i])
-            return false;
-    }
-    return true;
+    return a == b;
 }
 
 int main() {
     assert(issame(words_string("ahmed     , gamal"), {"ahmed", "gamal"}));
-    cout << "Test passed!" << endl;
+    cout << "All tests passed!" << endl;
     return 0;
 }
