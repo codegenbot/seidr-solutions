ca751614d460cb91e636e3995fbdec2e20df4460
beep boop <githubbot@vadim.me>
2024-07-05 11:59:25 +0200

    Solution 15 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/pjeawyh3 Prompt: Complete the following code given the task description and function signature.

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,4 +1,6 @@
-sort(l.begin(), l.end());
-    l.erase(unique(l.begin(), l.end()), l.end());
+vector<int> unique(vector<int> l){
+    sort(l.begin(), l.end());
+    auto it = unique(l.begin(), l.end());
+    l.resize(distance(l.begin(), it));
     return l;
 }
