78002fe87ceed98591ec93fc134bba1e161c3509
beep boop <githubbot@vadim.me>
2024-07-05 14:38:18 +0200

    Solution 6 of CPP/71, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/co61lfjl Prompt: Complete the following code given the task description and function signature.

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,9 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+
 float triangle_area(float a, float b, float c) {
-    if (a + b > c && a + c > b && b + c > a) {
-        float s = (a + b + c) / 2;
-        float area = sqrt(s * (s - a) * (s - b) * (s - c));
-        return round(area * 100) / 100;
-    } else {
+    if (a + b <= c || a + c <= b || b + c <= a) {
         return -1;
     }
+    float s = (a + b + c) / 2;
+    float area = sqrt(s * (s - a) * (s - b) * (s - c));
+    return round(area * 100) / 100;
 }
