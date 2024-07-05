5ec275557ca177b99960834374a23401cc82c9b6
beep boop <githubbot@vadim.me>
2024-07-06 01:56:54 +0200

    Solution 34 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/tn9pfa00 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/25901c7f-faf6-41a3-bada-30142fcc7 996.py", line 9, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/25901c7f-faf6-41a3-bada-30142fcc7 996.py", line 6, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/25901c7f-faf6-41a3-bada-30142fcc7 996.py", line 2, in by_length     return sorted(arr, key=len)            ^^^^^^^^^^^^^^^^^^^^ TypeError: object of type 'int' has no len()

--- Python_105.py
+++ Python_105.py
@@ -1,6 +1,3 @@
+
 def by_length(arr):
-    result = []
-    for num in arr:
-        if isinstance(num, int) and 1 <= num <= 9:
-            result.append(f"{num}".zfill(2))
-    return sorted(result, key=len, reverse=True)
+    return sorted([str(x) for x in arr], key=len)
