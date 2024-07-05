d66e49f25c68829ba135c4f9874858de5d0c0650
beep boop <githubbot@vadim.me>
2024-07-05 10:33:44 +0200

    Solution 13 of CPP/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/frics9rd Prompt: Complete the following code given the task description and function signature.

--- CPP_11.cpp
+++ CPP_11.cpp
@@ -1,7 +1,7 @@
 string string_xor(string a, string b){
     string result = "";
-    for(size_t i = 0; i < a.length(); i++){
-        result += (a[i] == b[i] ? '0' : '1');
+    for(size_t i = 0; i < a.size(); i++){
+        result += (a[i] == b[i]) ? '0' : '1';
     }
     return result;
 }
