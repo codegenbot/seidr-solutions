fe1f13cfe44e1a97e4ac56d5ced0a90c8fd2baec
beep boop <githubbot@vadim.me>
2024-07-05 08:58:33 +0200

    Solution 33 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/2mcf6rcs Prompt:       |                    ^~~ bae69f8f-6005-4167-8b1b-055647bc35a3.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bae69f8f-6005-4167-8b1b-055647bc35a3.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bae69f8f-6005-4167-8b1b-055647bc35a3.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bae69f8f-6005-4167-8b1b-055647bc35a3.cpp:16: bae69f8f-6005-4167-8b1b-055647bc35a3.cpp: In function ‘int main()’: bae69f8f-6005-4167-8b1b-055647bc35a3.cpp:26:20: error: ‘even_odd_count’ was not declared in this scope    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ bae69f8f-6005-4167-8b1b-055647bc35a3.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -5,19 +5,16 @@ using namespace std;
 
 vector<int> even_odd_count(int num) {
     int even_count = 0, odd_count = 0;
-    if(num == 0) {
-        even_count = 1;
-    } else {
-        num = abs(num);
-        while (num > 0) {
-            int digit = num % 10;
-            if (digit % 2 == 0) {
-                even_count++;
-            } else {
-                odd_count++;
-            }
-            num /= 10;
+    num = abs(num);
+    if (num == 0) even_count = 1; // Special case for number 0
+    while (num > 0) {
+        int digit = num % 10;
+        if (digit % 2 == 0) {
+            even_count++;
+        } else {
+            odd_count++;
         }
+        num /= 10;
     }
     return {even_count, odd_count};
 }
@@ -30,7 +27,6 @@ int main() {
     assert(issame(even_odd_count(0), {1, 0}));
     assert(issame(even_odd_count(12345), {2, 3}));
     assert(issame(even_odd_count(24680), {5, 0}));
-    assert(issame(even_odd_count(13579), {0, 5}));
-    assert(issame(even_odd_count(-12345), {2, 3}));
+    assert(issame(even_odd_count(-13579), {0, 5}));
     return 0;
 }
