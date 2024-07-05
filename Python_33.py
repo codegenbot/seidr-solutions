5b2977870951b7f48381a4e008c8af9df2882b00
beep boop <githubbot@vadim.me>
2024-07-06 00:44:03 +0200

    Solution 29 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tpufcn5h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8c5d66a4-b4d9-4a99-991e-3a315572d 3b6.py", line 16, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8c5d66a4-b4d9-4a99-991e-3a315572d 3b6.py", line 11, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,5 +1,14 @@
-def sort_third(l):
-    return [
-        (i if i % 3 != 0 else min(x for x in l[: i + 1] if x % 3 != 0))
-        for i in range(len(l))
-    ]
+def sort_third(lst):
+    result = []
+    prev_min = None
+    for i in range(len(lst)):
+        if i % 3 == 0:
+            result.append(
+                min(x for x in lst[: i + 1] if x % 3 != 0)
+                if prev_min is None
+                else prev_min
+            )
+            prev_min = result[-1]
+        else:
+            result.append(lst[i])
+    return result
