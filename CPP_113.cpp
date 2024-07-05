c6b2f5121912af63f384acc1dbc350ec6f22b363
beep boop <githubbot@vadim.me>
2024-07-05 08:17:01 +0200

    Solution 3 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/uidfpria Prompt:       |                                            ^ d64adcf1-188b-4ad5-8970-d68a49e85e28.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d64adcf1-188b-4ad5-8970-d68a49e85e28.cpp:16: d64adcf1-188b-4ad5-8970-d68a49e85e28.cpp: In function ‘int main()’: d64adcf1-188b-4ad5-8970-d68a49e85e28.cpp:26:20: error: ‘odd_count’ was not decla red in this scope    26 |     assert (issame(odd_count({"271", "137", "314"}) , {       |                    ^~~~~~~~~ d64adcf1-188b-4ad5-8970-d68a49e85e28.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(odd_count({"271", "137", "314"}) , {       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    27 |         "the number of odd elements 2n the str2ng 2 of the 2nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    28 |         "the number of odd elements 3n the str3ng 3 of the 3nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    29 |         "the number of odd elements 2n the str2ng 2 of the 2nput."       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    30 |     }));       |     ~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -10,25 +10,25 @@ vector<string> odd_count(vector<string> lst) {
     for (const auto& str : lst) {
         int odd_count = 0;
         for (char c : str) {
-            if ((c - '0') % 2 != 0) {
+            if (isdigit(c) && (c - '0') % 2 != 0) {
                 odd_count++;
             }
         }
-        string res = "the number of odd elements " + to_string(odd_count) + " in the string " + to_string(odd_count) + " of the input.";
+        string res = "the number of odd elements " + to_string(odd_count) + " in the string " + str + " of the input.";
         result.push_back(res);
     }
     return result;
 }
 
-bool issame(vector<string> a, vector<string> b){
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
 int main() {
     assert(issame(odd_count({"271", "137", "314"}), {
-        "the number of odd elements 2 in the string 2 of the input.",
-        "the number of odd elements 3 in the string 3 of the input.",
-        "the number of odd elements 2 in the string 2 of the input."
+        "the number of odd elements 2 in the string 271 of the input.",
+        "the number of odd elements 3 in the string 137 of the input.",
+        "the number of odd elements 2 in the string 314 of the input."
     }));
     cout << "All tests passed!" << endl;
     return 0;
