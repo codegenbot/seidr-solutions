e82fc3f157b6e4505de73a8d786347bb98e1be50
beep boop <githubbot@vadim.me>
2024-07-05 08:27:05 +0200

    Solution 14 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/nsebygza Prompt: Complete the following code given the task description and function signature.

--- Python_1.py
+++ Python_1.py
@@ -1,14 +1,15 @@
 def separate_paren_groups(paren_string: str) -> List[str]:
     paren_string = paren_string.replace(" ", "")
-    count, start = 0, 0
-    result = []
-    for i, char in enumerate(paren_string):
+    result, balance, start = [], 0, 0
+
+    for index, char in enumerate(paren_string):
         if char == "(":
-            if count == 0:
-                start = i
-            count += 1
+            balance += 1
         elif char == ")":
-            count -= 1
-            if count == 0:
-                result.append(paren_string[start : i + 1])
+            balance -= 1
+
+        if balance == 0:
+            result.append(paren_string[start : index + 1])
+            start = index + 1
+
     return result
