5f2ad6371ed5aca10a487536b6b8f3a756fbff09
beep boop <githubbot@vadim.me>
2024-07-06 01:01:16 +0200

    Solution 12 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/1lrd2vll Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8719f9c3-f37f-4196-af30-77b851552 8f8.py", line 23, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8719f9c3-f37f-4196-af30-77b851552 8f8.py", line 19, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,12 +1,10 @@
+
 def encode(message):
-    vowels = ["a", "e", "i", "o", "u"]
+    vowels = ['a', 'e', 'i', 'o', 'u']
     encoded_message = ""
     for char in message:
-        if char.isalpha():
-            if char.lower() in vowels:
-                encoded_char = chr((ord(char) - ord("a") + 2) % 26 + ord("A"))
-            else:
-                encoded_message += char
-        elif char.isspace() or char.ispunct():
+        if char.isalpha() and char.lowercase():
+            encoded_char = chr((ord(char) - ord('a') + 2) % 26 + ord('A'))
+        else:
             encoded_message += char
     return encoded_message
