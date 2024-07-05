654c0ff37726083c3e35ccae9359862745c36cad
beep boop <githubbot@vadim.me>
2024-07-06 00:38:03 +0200

    Solution 2 of Python/30, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0snop71f Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/34df8e6b-964d-4e2e-b1b5-ecd93f559 19f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_30.py
+++ Python_30.py
@@ -1,3 +1,6 @@
-```
 def get_positive(l: list):
-    return [x for x in l if x > 0]
+    positive_numbers = []
+    for num in l:
+        if num > 0:
+            positive_numbers.append(num)
+    return positive_numbers
