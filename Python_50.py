1abbf89674d7d528db6041672c22b63bdb595db4
beep boop <githubbot@vadim.me>
2024-07-06 00:50:40 +0200

    Solution 10 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/t2jqbixf Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -1,15 +1,2 @@
-
-def decode_shift(s: str) -> str:
-    """
-    Decode a string using the Caesar Cipher algorithm.
-
-    The function takes a single string argument and returns a decoded
-    string based on the Caesar Cipher algorithm with a shift of 5.
-
-    Parameters:
-        s (str): A string to be decoded
-
-    Returns:
-        str: A decoded string
-    """
-    return "".join([chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) for ch in s])
+def decode_shift(s):
+    return "".join([chr((ord(ch) - 5 - ord("a")) % 26 + ord("a")) for ch in s])
