129a21783ec4e96b2365d4dab9d9497344dcfdda
beep boop <githubbot@vadim.me>
2024-07-05 07:47:45 +0200

    Solution 11 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/8ek47u7g Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fd6abc0b-93f7-482c-89e3-2a2e6931e 935.py", line 13, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fd6abc0b-93f7-482c-89e3-2a2e6931e 935.py", line 9, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,6 +1,4 @@
 def encode(message):
-    vowels = "aeiouAEIOU"
-    vowel_map = {
-        v: chr(ord(v) + 2) if v.lower() != "o" else chr(ord(v) + 8) for v in vowels
-    }
-    return "".join(vowel_map.get(c, c.swapcase()) for c in message)
+    vowels = 'aeiouAEIOU'
+    vowel_map = {v: ('q' if v == 'o' else 'Q' if v == 'O' else chr(ord(v) + 2)) for v in vowels}
+    return ''.join(vowel_map.get(c, c.swapcase()) for c in message)
