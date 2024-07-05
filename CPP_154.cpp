6cb9cc5c0e2a9c4d3e17bcc038f7d3a8ea446112
beep boop <githubbot@vadim.me>
2024-07-05 03:06:44 +0200

    Solution 2 of CPP/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/02c7v2xh Prompt: 9fb50683-ef40-4313-87fd-f62e376b41d7.cpp:1:23: error: ‘string’ was not declared in this scope     1 | bool cycpattern_check(string a, string b) {       |                       ^~~~~~ 9fb50683-ef40-4313-87fd-f62e376b41d7.cpp:1:33: error: ‘string’ was not declared in this scope     1 | bool cycpattern_check(string a, string b) {       |                                 ^~~~~~ 9fb50683-ef40-4313-87fd-f62e376b41d7.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool cycpattern_check(string a, string b) {       |                                         ^ In file included from 9fb50683-ef40-4313-87fd-f62e376b41d7.cpp:12: 9fb50683-ef40-4313-87fd-f62e376b41d7.cpp: In function ‘int main()’: 9fb50683-ef40-4313-87fd-f62e376b41d7.cpp:14:30: error: ‘cycpattern_check’ cannot  be used as a function    14 |     assert  (cycpattern_check("winemtt","tinem") == true );       |              ~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_154.cpp
+++ CPP_154.cpp
@@ -1,10 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
 bool cycpattern_check(string a, string b) {
     int len = b.length();
-    string temp = b + b;
+    string rotated = b + b;
     for (int i = 0; i < len; ++i) {
-        if (a.find(temp.substr(i, len)) != string::npos) {
+        if (a.find(rotated.substr(i, len)) != string::npos) {
             return true;
         }
     }
     return false;
+}
+
+int main() {
+    assert(cycpattern_check("winemtt", "tinem") == true);
+    cout << "Test passed!" << endl;
+    return 0;
 }
