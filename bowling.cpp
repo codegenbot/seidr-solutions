82bed13b6b24692d5d1c55a76ae20167831049ba
beep boop <githubbot@vadim.me>
2024-07-05 19:43:58 +0200

    Solution 29 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/iwtrf4uo Prompt: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::__cxx11::basi c_string<_CharT, _Traits, _Alloc>::basic_string(std::__cxx11::basic_string<_Char T, _Traits, _Alloc>&&) [with _CharT = char; _Traits = std::char_traits<char>; _A lloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:6667:12:   required from here /usr/include/c++/11/bits/basic_string.h:576:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   576 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,22 +1,28 @@
-```cpp
 int bowlingScore(std::string s) {
     int score = 0;
-    int currentRoll = 0;
     for (char c : s) {
         if (c == 'X') {
             score += 30;
-            currentRoll++;
         } else if (c == '/') {
-            score += 10 + currentRoll;
-            currentRoll = 0;
+            score += 10 + score/10;
         } else {
             int roll = c - '0';
-            score += roll;
-            currentRoll++;
-            if (currentRoll == 2) {
-                score += roll * 2;
-                currentRoll = 0;
+            if (currentRoll() < 2) {
+                score += roll;
+            } else {
+                if (roll + currentScore()/10 >= 10) {
+                    score += 10 + score/10;
+                } else {
+                    score += roll * 2;
+                }
             }
         }
     }
+    return score;
+
+}
+
+int currentRoll() {
+    static int count = 0;
+    return ++count;
 }
