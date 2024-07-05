8280cb5d68701031e0465d63b1b85bbcbfa52260
beep boop <githubbot@vadim.me>
2024-07-06 00:54:28 +0200

    Solution 30 of Python/46, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/dceenzxr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6ef35176-592f-4fa1-9975-60a20c9a5 fd6.py", line 16, in <module>     check(fib4)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6ef35176-592f-4fa1-9975-60a20c9a5 fd6.py", line 13, in check     assert fib4(12) == 386            ^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6ef35176-592f-4fa1-9975-60a20c9a5 fd6.py", line 10, in fib4     memo.append(memo[i - 3] + memo[i - 2])                               ~~~~^^^^^^^ IndexError: list index out of range

--- Python_46.py
+++ Python_46.py
@@ -1,10 +1,10 @@
-def fib4(n: int):
+def fib4(n):
     if n <= 3:
         return 0
     elif n == 4:
         return 2
     else:
         memo = [0, 1, 1]
-        for i in range(3, n + 1):
+        for i in range(6, n + 1):
             memo.append(memo[i - 3] + memo[i - 2])
         return memo[n]
