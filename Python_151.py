5e50261a63664d9096c3b0e717bf2f563e0cee2f
beep boop <githubbot@vadim.me>
2024-07-05 08:22:08 +0200

    Solution 12 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/txqplh3v Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5233113d-3c2a-4489-b4eb-94a45cb48 a8c.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5233113d-3c2a-4489-b4eb-94a45cb48 a8c.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,9 +1,2 @@
-def double_the_difference(lst):
-    return sum(x**2 for x in lst if isinstance(x, int) and x > 0 and x % 2 != 0)
-
-def check(func):
-    lst = [1, 2, -3, 4, 5]
-    odd_sum = 27  # which is 1^2 + 5^2
-    assert func(lst) == odd_sum
-
-check(double_the_difference)
+def double_the_difference(list):
+    return sum(x**2 for x in list if isinstance(x, int) and x > 0 and x % 2 != 0)
