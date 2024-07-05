c2f7d367471aa8512803c030c967d66785ae345e
beep boop <githubbot@vadim.me>
2024-07-05 16:12:28 +0200

    Solution 12 of CPP/122, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/cazrck7j Prompt: Complete the following code given the task description and function signature.

--- CPP_122.cpp
+++ CPP_122.cpp
@@ -1,10 +1,10 @@
-#include <vector>
+#include<stdio.h>
+#include<vector>
 using namespace std;
-
 int add_elements(vector<int> arr, int k) {
     int sum = 0;
     for (int i = 0; i < k; ++i) {
-        if (arr[i] >= -99 && arr[i] <= 99) {
+        if (arr[i] <= 99 && arr[i] >= -99) { // considering negative two-digit numbers too
             sum += arr[i];
         }
     }
