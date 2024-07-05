022dbb0a1ecd164980001adda6e30cfd9716da97
beep boop <githubbot@vadim.me>
2024-07-05 12:32:52 +0200

    Solution 60 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/q5te1cwu Prompt: f38132a9-8472-4fdd-b49a-aeb6444a7ffb.cpp:22:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    22 |             str+=chr;       |             ^~~       |             std f38132a9-8472-4fdd-b49a-aeb6444a7ffb.cpp:25:15: error: expected ‘;’ before ‘enco ded_str’    25 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from f38132a9-8472-4fdd-b49a-aeb6444a7ffb.cpp:12: f38132a9-8472-4fdd-b49a-aeb6444a7ffb.cpp:26:31: error: ‘encoded_str’ was not dec lared in this scope    26 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ f38132a9-8472-4fdd-b49a-aeb6444a7ffb.cpp:26:17: error: ‘decode_cyclic’ was not d eclared in this scope    26 |         assert (decode_cyclic(encoded_str) == str);       |                 ^~~~~~~~~~~~~ f38132a9-8472-4fdd-b49a-aeb6444a7ffb.cpp:26:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    26 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -5,7 +5,7 @@ using namespace std;
 string decode_cyclic(const string &s){
     int l = s.length();
     string x, output;
-    for (int i = 0; i * 3 < l; i++){
+    for (int i = 0; i * 3 < l; i++) {
         x = s.substr(i * 3, 3);
         if (x.length() == 3) x = x[2] + x.substr(0, 2);
         output += x;
@@ -16,9 +16,9 @@ string decode_cyclic(const string &s){
 // Assuming there's a corresponding encode_cyclic function somewhere
 string encode_cyclic(const string &s);
 
-int main(){
+int main() {
     string str = "example";
     string encoded_str = encode_cyclic(str);
-    assert (decode_cyclic(encoded_str) == str);
+    assert(decode_cyclic(encoded_str) == str);
     return 0;
 }
