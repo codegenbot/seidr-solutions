fe97f38196b6265642a5aa95ded0a966d61e349b
beep boop <githubbot@vadim.me>
2024-07-06 01:37:50 +0200

    Solution 11 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cx07zqqf Prompt:       |           ^~~~~~ ec1a3605-b45b-4f88-9201-9ea91c633e14.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ec1a3605-b45b-4f88-9201-9ea91c633e14.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ ec1a3605-b45b-4f88-9201-9ea91c633e14.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~ In file included from ec1a3605-b45b-4f88-9201-9ea91c633e14.cpp:16: ec1a3605-b45b-4f88-9201-9ea91c633e14.cpp: In function ‘int main()’: ec1a3605-b45b-4f88-9201-9ea91c633e14.cpp:26:20: error: ‘all_prefixes’ was not de clared in this scope    26 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ ec1a3605-b45b-4f88-9201-9ea91c633e14.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -3,9 +3,8 @@
 
 std::vector<std::string> all_prefixes(const std::string& str) {
     std::vector<std::string> prefixes;
-    for(int i = 0; i <= str.size(); i++) {
+    for(int i = 0; i <= str.size(); i++) 
         prefixes.push_back(str.substr(0, i));
-    }
     return prefixes;
 }
 
