06b3b7b009959f2249e6c49a70848a66b367f7d4
beep boop <githubbot@vadim.me>
2024-07-05 04:19:28 +0200

    Solution 10 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/cdgloi6t Prompt: aac3ef14-981f-4220-b1ab-5151444ba909.cpp:26:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    26 |             str+=chr;       |             ^~~       |             std aac3ef14-981f-4220-b1ab-5151444ba909.cpp:29:15: error: expected ‘;’ before ‘enco ded_str’    29 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from aac3ef14-981f-4220-b1ab-5151444ba909.cpp:16: aac3ef14-981f-4220-b1ab-5151444ba909.cpp:30:31: error: ‘encoded_str’ was not dec lared in this scope    30 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ aac3ef14-981f-4220-b1ab-5151444ba909.cpp:30:17: error: ‘decode_cyclic’ was not d eclared in this scope    30 |         assert (decode_cyclic(encoded_str) == str);       |                 ^~~~~~~~~~~~~ aac3ef14-981f-4220-b1ab-5151444ba909.cpp:30:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    30 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -9,7 +9,6 @@ string decode_cyclic(string s){
     string x, output;
     int i;
     for (i = 0; i * 3 < l; i++) {
-        // reverse cycle elements in each group. Unless group has fewer elements than 3.
         x = s.substr(i * 3, 3);
         if (x.length() == 3) x = x[2] + x.substr(0, 2);
         output = output + x;
