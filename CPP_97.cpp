26613f1d68b5faf86d73afa68134ef131bc71c75
beep boop <githubbot@vadim.me>
2024-07-05 06:23:33 +0200

    Solution 12 of CPP/97, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/ikew52kb Prompt: Program failed

--- CPP_97.cpp
+++ CPP_97.cpp
@@ -1,7 +1,7 @@
-#include <cstdlib>
+#include <cmath>
 
-int multiply(int a, int b) {
-    int unitDigitA = abs(a) % 10;
-    int unitDigitB = abs(b) % 10;
+int multiply(int a, int b){
+    int unitDigitA = std::abs(a) % 10;
+    int unitDigitB = std::abs(b) % 10;
     return unitDigitA * unitDigitB;
 }
