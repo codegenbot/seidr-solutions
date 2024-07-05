5a4c531c3858e9b67166e679ac172b5f18b9b068
beep boop <githubbot@vadim.me>
2024-07-05 04:22:34 +0200

    Solution 87 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/i1zhope5 Prompt: f0a86024-38ab-44b3-aed9-e72e26aa991a.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ f0a86024-38ab-44b3-aed9-e72e26aa991a.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -2,7 +2,7 @@
 #include <cassert>
 #include <cmath>
 
-float max_element(const std::vector<float>& l) {
+float max_element(std::vector<float> l) {
     float max_val = l[0];
     for (float num : l) {
         if (num > max_val) {
