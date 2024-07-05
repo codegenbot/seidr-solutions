0c624d84cf2ead66e380e5136894b8f16ab3822c
beep boop <githubbot@vadim.me>
2024-07-06 00:45:59 +0200

    Solution 4 of Python/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/zdhdf8zw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80d35c01-5e93-4c71-9213-035296e5b 411.py", line 14, in <module>     check(pluck)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80d35c01-5e93-4c71-9213-035296e5b 411.py", line 11, in check     assert pluck([7, 9, 7, 1]) == [], "Error"            ^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80d35c01-5e93-4c71-9213-035296e5b 411.py", line 5, in pluck     smallest_even = min([x for x in arr if x % 2 == 0])                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ValueError: min() arg is an empty sequence

--- Python_68.py
+++ Python_68.py
@@ -3,8 +3,5 @@ def pluck(arr):
         return []
     else:
         smallest_even = min([x for x in arr if x % 2 == 0])
-        if smallest_even is None:
-            # No even number was found
-            return []
         smallest_index = arr.index(smallest_even)
         return [smallest_even, smallest_index]
