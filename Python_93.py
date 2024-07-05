f589f0c9b5cf841543349c974c97b4b70eeba048
beep boop <githubbot@vadim.me>
2024-07-06 01:22:51 +0200

    Solution 44 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/1lrd2vll Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a6a1955d-8976-4691-80c1-9d04ccbed b5a.py", line 24, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a6a1955d-8976-4691-80c1-9d04ccbed b5a.py", line 20, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg"     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a6a1955d-8976-4691-80c1-9d04ccbed b5a.py", line 9, in encode     encoded_char = vowels[char].lower()                    ~~~~~~^^^^^^ KeyError: 'I'

--- Python_93.py
+++ Python_93.py
@@ -1,15 +1,16 @@
+
 def encode(message):
-    vowels = {"a": "A", "e": "E", "i": "I", "o": "O", "u": "U", "I": "?", " ": ""}
-    encoded_message = ""
-    for char in message:
-        if char.isalpha():
-            if char.islower():
-                encoded_char = vowels[char]
-            elif char.isupper():
-                encoded_char = vowels[char].lower()
-        else:
+   vowels = {"a": "A", "e": "E", "i": "I", "o": "O", "u": "U"}
+   encoded_message = ""
+   for char in message:
+      if char.isalpha():
+         if char.islower():
+            encoded_char = vowels[char]
+         elif char.isupper():
+            encoded_char = vowels[char].lower()
+         else:
             if char in vowels:
                 encoded_char = "?"
             else:
                 encoded_message += char
-    return encoded_message
+   return encoded_message
