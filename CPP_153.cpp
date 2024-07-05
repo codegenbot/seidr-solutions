b7e6b440ac01930ff3c1fbcd487968c121a5c6e5
beep boop <githubbot@vadim.me>
2024-07-05 07:46:48 +0200

    Solution 4 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/zbjzj667 Prompt: 5c1c002f-4d72-4304-a34b-17f7c56c5a29.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string Strongest_Extension(string class_name, vector<string> extensions)  {       | ^~~~~~ In file included from 5c1c002f-4d72-4304-a34b-17f7c56c5a29.cpp:21: 5c1c002f-4d72-4304-a34b-17f7c56c5a29.cpp: In function ‘int main()’: 5c1c002f-4d72-4304-a34b-17f7c56c5a29.cpp:23:13: error: ‘Strongest_Extension’ was  not declared in this scope    23 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,6 +1,12 @@
-{
-    string strongestExtension = "";
-    int maxStrength = INT_MIN;
+#include <iostream>
+#include <string>
+#include <vector>
+#include <climits>
+using namespace std;
+
+string Strongest_Extension(string class_name, vector<string> extensions) {
+    string strongest_extension = "";
+    int max_strength = INT_MIN;
 
     for (const string& ext : extensions) {
         int CAP = 0, SM = 0;
@@ -9,11 +15,16 @@
             else if (islower(c)) SM++;
         }
         int strength = CAP - SM;
-        if (strength > maxStrength) {
-            maxStrength = strength;
-            strongestExtension = ext;
+        if (strength > max_strength) {
+            max_strength = strength;
+            strongest_extension = ext;
         }
     }
 
-    return class_name + "." + strongestExtension;
+    return class_name + "." + strongest_extension;
+}
+
+int main() {
+    assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
+    return 0;
 }
