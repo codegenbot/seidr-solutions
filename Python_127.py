d9443ad11b8308218ebe3947a5e9e46574f3c84d
beep boop <githubbot@vadim.me>
2024-07-06 01:58:11 +0200

    Solution 2 of Python/127, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/sqhtoljp Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/69cce62a-fb22-46e2-aab3-010bf8ab1 e71.py", line 15, in <module>     check(intersection)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/69cce62a-fb22-46e2-aab3-010bf8ab1 e71.py", line 12, in check     assert intersection((-2, -2), (-3, -2)) == "NO"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/69cce62a-fb22-46e2-aab3-010bf8ab1 e71.py", line 6, in intersection     return "YES" if is_prime(length) else "NO"                     ^^^^^^^^ NameError: name 'is_prime' is not defined

--- Python_127.py
+++ Python_127.py
@@ -1,3 +1,12 @@
+
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 def intersection(interval1, interval2):
     start = max(interval1[0], interval2[0])
     end = min(interval1[1], interval2[1])
