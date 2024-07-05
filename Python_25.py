3d2e4113bce8a6a69aa37e36cc73a0612266cbe7
beep boop <githubbot@vadim.me>
2024-07-05 10:55:36 +0200

    Solution 68 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/m8r221eq Prompt: Program did not receive expected input

--- Python_25.py
+++ Python_25.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 def factorize(n: int) -> List[int]:
     factors = []
     divisor = 2
@@ -9,6 +10,7 @@ def factorize(n: int) -> List[int]:
             n //= divisor
         divisor += 1
     return factors
+
 
 if __name__ == "__main__":
     n = int(input())
