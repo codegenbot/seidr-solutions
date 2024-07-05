80a7e59101f3c3ba40d96ffec8553a7c92d69c69
beep boop <githubbot@vadim.me>
2024-07-05 11:17:15 +0200

    Solution 26 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/9f70zw00 Prompt:    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ dc798500-9feb-4e72-9092-5c7e73d8c3e4.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ dc798500-9feb-4e72-9092-5c7e73d8c3e4.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from dc798500-9feb-4e72-9092-5c7e73d8c3e4.cpp:22: dc798500-9feb-4e72-9092-5c7e73d8c3e4.cpp: In function ‘int main()’: dc798500-9feb-4e72-9092-5c7e73d8c3e4.cpp:32:20: error: ‘parse_music’ was not dec lared in this scope    32 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ dc798500-9feb-4e72-9092-5c7e73d8c3e4.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -10,18 +10,22 @@ vector<int> parse_music(string music_string) {
     string note;
     for (char c : music_string) {
         if (c == ' ') {
-            if (note == "o") beats.push_back(4);
-            else if (note == "o|") beats.push_back(2);
-            else if (note == ".|") beats.push_back(1);
-            note = "";
+            if (!note.empty()) {
+                if (note == "o") beats.push_back(4);
+                else if (note == "o|") beats.push_back(2);
+                else if (note == ".|") beats.push_back(1);
+                note = "";
+            }
         } else {
             note += c;
         }
     }
     // Process the last note if there is any
-    if (note == "o") beats.push_back(4);
-    else if (note == "o|") beats.push_back(2);
-    else if (note == ".|") beats.push_back(1);
+    if (!note.empty()) {
+        if (note == "o") beats.push_back(4);
+        else if (note == "o|") beats.push_back(2);
+        else if (note == ".|") beats.push_back(1);
+    }
     
     return beats;
 }
