7e387bb2115b0585e256a32eb629d509af0a2291
beep boop <githubbot@vadim.me>
2024-07-05 05:47:39 +0200

    Solution 24 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/gkkfhzeq Prompt:       |                              ^~~~~~ ebec002a-1c1a-4084-8ff0-51fa6f26d845.cpp:21:37: error: ‘string’ was not declared  in this scope    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ebec002a-1c1a-4084-8ff0-51fa6f26d845.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ebec002a-1c1a-4084-8ff0-51fa6f26d845.cpp:21:45: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ebec002a-1c1a-4084-8ff0-51fa6f26d845.cpp:20: ebec002a-1c1a-4084-8ff0-51fa6f26d845.cpp: In function ‘int main()’: ebec002a-1c1a-4084-8ff0-51fa6f26d845.cpp:30:20: error: ‘reverse_delete’ was not declared in this scope    30 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ ebec002a-1c1a-4084-8ff0-51fa6f26d845.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,8 +1,8 @@
-#include <algorithm>
-#include <cassert>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <cassert>
 
 using namespace std;
 
@@ -10,15 +10,15 @@ vector<string> reverse_delete(string s, string c) {
     for (char ch : c) {
         s.erase(remove(s.begin(), s.end(), ch), s.end());
     }
-
+    
     string reversed_s = s;
     reverse(reversed_s.begin(), reversed_s.end());
     bool is_palindrome = (s == reversed_s);
-
+    
     vector<string> result;
     result.push_back(s);
     result.push_back(is_palindrome ? "True" : "False");
-
+    
     return result;
 }
 
@@ -28,6 +28,5 @@ bool issame(vector<string> a, vector<string> b) {
 
 int main() {
     assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
-    cout << "All tests passed!" << endl;
     return 0;
 }
