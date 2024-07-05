c89b41bc080d5d44383f8c7d7bd7e496dd7a7b7e
beep boop <githubbot@vadim.me>
2024-07-05 14:14:25 +0200

    Solution 58 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/k86l67yy Prompt: 3b3c0454-31cb-4167-b3ea-4fa0437d5286.cpp:40:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    40 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 3b3c0454-31cb-4167-b3ea-4fa0437d5286.cpp:29:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    29 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 3b3c0454-31cb-4167-b3ea-4fa0437d5286.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ 3b3c0454-31cb-4167-b3ea-4fa0437d5286.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <vector>
-#include <climits>
 #include <cassert>
 using namespace std;
 
 vector<int> largest_smallest_integers(vector<int> lst) {
-    int largest_negative = INT_MIN;
-    int smallest_positive = INT_MAX;
+    int largest_negative = 0;
+    int smallest_positive = 0;
     bool found_negative = false;
     bool found_positive = false;
 
-    for (int num : lst) {
-        if (num < 0) {
-            if (!found_negative || num > largest_negative) {
+    for(int num : lst) {
+        if(num < 0) {
+            if(!found_negative || num > largest_negative) {
                 largest_negative = num;
                 found_negative = true;
             }
-        } else if (num > 0) {
-            if (!found_positive || num < smallest_positive) {
+        } else if(num > 0) {
+            if(!found_positive || num < smallest_positive) {
                 smallest_positive = num;
                 found_positive = true;
             }
@@ -33,10 +32,6 @@ bool issame(vector<int> a, vector<int> b) {
 
 int main() {
     assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
-    assert(issame(largest_smallest_integers({-1, -2, -3, 1, 2, 3}), {-1, 1}));
-    assert(issame(largest_smallest_integers({0, 0, 0}), {0, 0}));
-    assert(issame(largest_smallest_integers({-1, -2, -3, -4}), {-1, 0}));
-    assert(issame(largest_smallest_integers({1, 2, 3, 4}), {0, 1}));
     cout << "All tests passed!" << endl;
     return 0;
 }
