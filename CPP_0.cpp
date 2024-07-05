69b0ca96b3bf26af385a9182b95929f31f8de401
beep boop <githubbot@vadim.me>
2024-07-05 09:24:18 +0200

    Solution 1 of CPP/0, ratio 0.7142857142857143 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/4ijzdu0e Prompt: Complete the following code given the task description and function signature.

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -7,7 +7,7 @@ using namespace std;
 bool has_close_elements(vector<float> numbers, float threshold) {
     sort(numbers.begin(), numbers.end());
     for (size_t i = 1; i < numbers.size(); ++i) {
-        if (fabs(numbers[i] - numbers[i-1]) < threshold) {
+        if (fabs(numbers[i] - numbers[i - 1]) < threshold) {
             return true;
         }
     }
