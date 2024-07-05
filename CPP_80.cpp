ec8eeaedda99f134b8d04d8ed4e23d4bfd418317
beep boop <githubbot@vadim.me>
2024-07-05 05:17:54 +0200

    Solution 78 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/4cmnc5sd Prompt: acb12fe2-82d2-428d-b8ec-eeb9cdfb1224.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ acb12fe2-82d2-428d-b8ec-eeb9cdfb1224.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -12,8 +12,9 @@ bool is_happy(const std::string& s) {
 }
 
 int main() {
+    std::cout << std::boolalpha;
     std::string input;
     std::cin >> input;
-    std::cout << std::boolalpha << is_happy(input) << std::endl;
+    std::cout << is_happy(input) << std::endl;
     return 0;
 }
