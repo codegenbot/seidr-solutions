fca6d1a704261d73ed3bc87e643f5f3b004a761e
beep boop <githubbot@vadim.me>
2024-07-05 04:54:49 +0200

    Solution 25 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/h1uqvf9m Prompt:       |                    ^~~ 7ff4403a-2385-4799-824c-895bc977f9e5.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7ff4403a-2385-4799-824c-895bc977f9e5.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7ff4403a-2385-4799-824c-895bc977f9e5.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7ff4403a-2385-4799-824c-895bc977f9e5.cpp:16: 7ff4403a-2385-4799-824c-895bc977f9e5.cpp: In function ‘int main()’: 7ff4403a-2385-4799-824c-895bc977f9e5.cpp:26:20: error: ‘pluck’ was not declared in this scope    26 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 7ff4403a-2385-4799-824c-895bc977f9e5.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 vector<int> pluck(vector<int> arr) {
-    int smallest_even = INT_MAX; 
+    int smallest_even = INT_MAX;
     int index = -1;
     for (int i = 0; i < arr.size(); ++i) {
         if (arr[i] % 2 == 0 && arr[i] < smallest_even) {
