70517cfd870717aa3ceefe0e15ebe2fa720fc302
beep boop <githubbot@vadim.me>
2024-07-05 16:12:59 +0200

    Solution 4 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-4o-2024-05-13-run1/runs/zrea2ql7 Prompt: Complete the following code given the task description and function signature.

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -2,8 +2,8 @@ string get_closest_vowel(string word){
     string vowels = "aeiouAEIOU";
     for (int i = word.length() - 2; i > 0; --i) {
         if (vowels.find(word[i]) != string::npos && 
-            vowels.find(word[i - 1]) == string::npos && 
-            vowels.find(word[i + 1]) == string::npos) {
+            vowels.find(word[i-1]) == string::npos && 
+            vowels.find(word[i+1]) == string::npos) {
             return string(1, word[i]);
         }
     }
