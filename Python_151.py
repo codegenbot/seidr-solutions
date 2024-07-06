31e553873c6d0fc6e2afd05d3f1a222fe0287791
beep boop <githubbot@vadim.me>
2024-07-06 20:34:57 +0200

    Solution 63 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/u2o18bue Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c638bf6f-8a99-409f-a5b9-db374cf9b 351.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,10 +1,1 @@
-def double_the_difference(lst):
-    first_half_sum = sum(i for i in lst[:len(lst)//2])
-    second_half_sum = sum(i for i in lst[len(lst)//2:])
-    return [abs(first_half_sum - second_half_sum)]
-
-def check(lst):
-    result = double_the_difference(lst)
-    print("True" if sum(x for x in lst) % 2 == 0 else "False")
-
-check([1, 3, 5])
+return abs(first_half_sum - 2*second_half_sum)
