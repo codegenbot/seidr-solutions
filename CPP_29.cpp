6b193e2c60e646c3de310ed767a813dc132ff22c
beep boop <githubbot@vadim.me>
2024-07-06 02:04:45 +0200

    Solution 28 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dsmr51i9 Prompt: r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <string>   +++ |+#include <cassert>     3 | d18cdc66-1ed7-4178-ae5a-63047a742a4b.cpp: At global scope: d18cdc66-1ed7-4178-ae5a-63047a742a4b.cpp:30:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    30 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ d18cdc66-1ed7-4178-ae5a-63047a742a4b.cpp:6:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     6 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ d18cdc66-1ed7-4178-ae5a-63047a742a4b.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ d18cdc66-1ed7-4178-ae5a-63047a742a4b.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -23,6 +23,6 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
 }
 
 int main() {
-    assert(same(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxxAAA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
+    assert(same(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxxAAA", "xxx"}, "xxx"), vector<string>{ "xxx", "xxxAAA", "xxx"}));
     return 0;
 }
