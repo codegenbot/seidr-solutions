2be5214c2e4a8af6ceb60b0c677ae31250070a57
beep boop <githubbot@vadim.me>
2024-07-06 03:21:20 +0200

    Solution 11 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/yny9bc24 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/68d9a6c2-ff7a-4e66-bc52-3345a0751 d97.py", line 18, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/68d9a6c2-ff7a-4e66-bc52-3345a0751 d97.py", line 15, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/68d9a6c2-ff7a-4e66-bc52-3345a0751 d97.py", line 10, in prime_fib     if is_prime(fib[-1]):        ^^^^^^^^ NameError: name 'is_prime' is not defined

--- Python_39.py
+++ Python_39.py
@@ -1,10 +1,4 @@
-    def is_prime(n):
-        if n <= 1:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
+
 def prime_fib(n: int):
     if n <= 0:
         return None
@@ -17,3 +11,11 @@ def prime_fib(n: int):
         return fib[-1]
     else:
         return None
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
