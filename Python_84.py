7d9953d20f4fe2eb54c1da922b093193b1733246
beep boop <githubbot@vadim.me>
2024-07-06 17:54:33 +0200

    Solution 3 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qeuhcwp8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/00ead4ec-0d49-4a71-976f-806f6b2f3 b33.py", line 10, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/00ead4ec-0d49-4a71-976f-806f6b2f3 b33.py", line 7, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -1,3 +1,4 @@
 ```
 def solve(N):
-    return bin(sum(int(digit) for digit in str(bin(N)[2:]).replace('0', '').replace('1', '')))[2:]
+    binary = str(bin(N)[2:]).replace("1", "a").replace("0", "b")
+    return ''.join('a' if c == 'b' else 'b' for c in binary)
