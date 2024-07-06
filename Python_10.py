4ee181d90544a1f732e39b6a77b3e14c13f6c8ea
beep boop <githubbot@vadim.me>
2024-07-06 14:54:20 +0200

    Solution 10 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ny2a3etd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f3827ad6-3f17-4288-a9eb-379334f1d 399.py", line 13, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f3827ad6-3f17-4288-a9eb-379334f1d 399.py", line 10, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,8 +1,9 @@
+```
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
         return string
     for i in range(len(string)):
         middle = len(string) // 2
-        rev_string = string[i:] + string[i::-1]
-        if string == rev_string:
-            return rev_string
+        if i < middle and string[i:] == string[i:][::-1]:
+            return string[:i] + string[i:i+1] + string[i:][::-1]
+    return string + string[-1] + string[::-1].lstrip()
