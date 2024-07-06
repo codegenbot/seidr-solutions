181947b79c4258974eba23676c87d250865c34cb
beep boop <githubbot@vadim.me>
2024-07-06 13:03:28 +0200

    Solution 12 of CPP/45, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yigt5ptn Prompt: In file included from 5f5ae2d6-0cfb-4f6e-b418-919070cfa461.cpp:5: 5f5ae2d6-0cfb-4f6e-b418-919070cfa461.cpp: In function ‘int main()’: 5f5ae2d6-0cfb-4f6e-b418-919070cfa461.cpp:7:13: error: ‘abs’ was not declared in this scope     7 |     assert (abs(triangle_area(10, 8) - 40.0)<1e-4);       |             ^~~

--- CPP_45.cpp
+++ CPP_45.cpp
@@ -1,11 +1,5 @@
-#include <iostream>
 #include <cmath>
 
-float triangle_area(int a, int h) {
+float triangle_area(float a, float h) {
     return (a * h) / 2.0f;
-}
-
-int main() {
-    assert(abs(triangle_area(10, 8) - 40.0)<1e-4);
-    return 0;
 }
