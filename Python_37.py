38767d55333bf22ab1eabb1a25ee8524825a50ce
beep boop <githubbot@vadim.me>
2024-07-06 15:51:53 +0200

    Solution 3 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1fv3kcsh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e62db886-c9f6-432d-abe9-c577b956d 43b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,3 @@
-from collections import OrderedDict
-
 def sort_even(l: list):
-    return [x if i % 2 != 0 else list(OrderedDict(sorted((y for y in l if (i // 2) * 2 == l.index(y))))).pop(0)
+    return [x if i % 2 != 0 else min(y for y in l if l.index(y) == (i // 2) * 2)
             for i, x in enumerate(l)]
