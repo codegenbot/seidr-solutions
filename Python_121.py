6c7e0156c5d20426b1ffc4fa2b8f182c445bb8b2
beep boop <githubbot@vadim.me>
2024-07-06 04:14:07 +0200

    Solution 86 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/23sd7ljf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eb76b297-34b4-4018-9c50-f9bf3964b 7f2.py", line 12, in <module>     check(solution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eb76b297-34b4-4018-9c50-f9bf3964b 7f2.py", line 7, in check     assert solution([3, 13, 2, 9]) == 3            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_121.py
+++ Python_121.py
@@ -1,4 +1,4 @@
 ```
 def solution(lst):
-    total = sum(map(int, [i for i in lst[1::2] if i.isdigit()]))
+    total = sum(0 if i in ['1', '2'] else int(i) for i in lst[1::2] if 0 if i in ['1', '2'] else int(i) > 1)
     return total * int(lst[0]) if total else 0
