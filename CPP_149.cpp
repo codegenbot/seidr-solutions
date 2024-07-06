06aa8b00b83f29b58168c8a154212f990566df89
beep boop <githubbot@vadim.me>
2024-07-06 18:49:59 +0200

    Solution 59 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r1a8ydno Prompt:    13 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 214660de-3308-48c3-88f5-4849d097d512.cpp:13:44: error: ‘b’ was not declared in t his scope    13 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 214660de-3308-48c3-88f5-4849d097d512.cpp:13:45: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 214660de-3308-48c3-88f5-4849d097d512.cpp:12: 214660de-3308-48c3-88f5-4849d097d512.cpp: In function ‘int main()’: 214660de-3308-48c3-88f5-4849d097d512.cpp:22:20: error: ‘sorted_list_sum’ was not  declared in this scope    22 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 214660de-3308-48c3-88f5-4849d097d512.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -2,28 +2,18 @@
 #include <algorithm>
 
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
 
 int main() {
-    vector<string> lst1 = {"A", "B", "C"};
-    vector<string> lst2 = {"A", "B", "C"};
-    
-    bool result = issame(lst1, lst2);
-    
-    if(result) {
-        cout << "The two lists are same." << endl;
-    } else {
-        cout << "The two lists are not same." << endl;
-    }
-    
+    vector<string> lst1 = {"abc", "pqr", "xyz"};
+    vector<string> lst2 = {"abc", "pqr", "xyz"};
+
+    if(issame(lst1, lst2)) cout << "The lists are same." << endl;
+    else cout << "The lists are not same." << endl;
+
     return 0;
 }
