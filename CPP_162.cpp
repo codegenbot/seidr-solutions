ebcbc30a12f37a1ee795d9f9db11f5fd1303dce8
beep boop <githubbot@vadim.me>
2024-07-06 19:46:42 +0200

    Solution 99 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lbq7xdp0 Prompt: e0d9232c-fd79-42be-a9a9-dd44ca3c43d6.cpp:1:10: fatal error: md5.md: No such file  or directory     1 | #include "md5.md"       |          ^~~~~~~~ compilation terminated.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
-
-    MD5_CTX ctx;
+    
     unsigned char result[16];
+    MD5_CTX ctx;
     MD5_Init(&ctx);
     const char* ptr = text.c_str();
     size_t len = text.size();
