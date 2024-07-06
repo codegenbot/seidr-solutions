d92e7979e2ee7aeeb867cca2a9cdb13e00d4d5fa
beep boop <githubbot@vadim.me>
2024-07-06 12:36:52 +0200

    Solution 14 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/w6uj1i6c Prompt: 2444c9ba-8865-4d7d-8ad4-77a94cfaff5e.cpp:1:48: error: expected primary-expressio n before ‘float’     1 | bool has_close_elements(vector<float> numbers, float threshold){       |                                                ^~~~~ 2444c9ba-8865-4d7d-8ad4-77a94cfaff5e.cpp:1:63: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool has_close_elements(vector<float> numbers, float threshold){       |                                                               ^ 2444c9ba-8865-4d7d-8ad4-77a94cfaff5e.cpp: In function ‘int main()’: 2444c9ba-8865-4d7d-8ad4-77a94cfaff5e.cpp:13:5: error: ‘vector’ was not declared in this scope    13 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 2444c9ba-8865-4d7d-8ad4-77a94cfaff5e.cpp:13:12: error: expected primary-expressi on before ‘float’    13 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~ In file included from 2444c9ba-8865-4d7d-8ad4-77a94cfaff5e.cpp:11: 2444c9ba-8865-4d7d-8ad4-77a94cfaff5e.cpp:14:31: error: ‘has_close_elements’ cann ot be used as a function    14 |     assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false) ;       |             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,10 +1,10 @@
 #include <vector>
 #include <cmath>
 
-bool has_close_elements(std::vector<float> numbers, float threshold){
+bool has_close_elements(vector<float> numbers, float threshold){
     for(int i = 0; i < numbers.size() - 1; i++){
         for(int j = i + 1; j < numbers.size(); j++){
-            if(std::abs(numbers[i] - numbers[j]) <= threshold)
+            if(abs(numbers[i] - numbers[j]) <= threshold)
                 return true;
         }
     }
