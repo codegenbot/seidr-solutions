5cf35887762b3fe5945559ebdeed3e683d3f49ec
beep boop <githubbot@vadim.me>
2024-07-06 12:36:20 +0200

    Solution 8 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lry0vt9k Prompt:    21 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ d60e0a79-2774-461a-84aa-be0d8ef53c2f.cpp:21:36: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ d60e0a79-2774-461a-84aa-be0d8ef53c2f.cpp:21:43: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from d60e0a79-2774-461a-84aa-be0d8ef53c2f.cpp:20: d60e0a79-2774-461a-84aa-be0d8ef53c2f.cpp: In function ‘int main()’: d60e0a79-2774-461a-84aa-be0d8ef53c2f.cpp:30:20: error: ‘sort_even’ was not decla red in this scope    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ d60e0a79-2774-461a-84aa-be0d8ef53c2f.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,34 +1,25 @@
-#include <vector>
-#include <algorithm>
-
-bool issame(vector<float> a, vector<float>b) {
-    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
-}
-
 vector<float> sort_even(vector<float> l) {
-    vector<float> evenVals;
-    vector<float> oddVals;
-
+    vector<float> result(l.size());
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            evenVals.push_back(l[i]);
+            vector<float> evenVals;
+            for (int j = 0; j < l.size(); j++) {
+                if (j % 2 == 0) {
+                    evenVals.push_back(l[j]);
+                }
+            }
+            sort(evenVals.begin(), evenVals.end());
+            result[i] = evenVals[0];
         } else {
-            oddVals.push_back(l[i]);
+            vector<float> oddVals;
+            for (int j = 0; j < l.size(); j++) {
+                if (j % 2 != 0) {
+                    oddVals.push_back(l[j]);
+                }
+            }
+            sort(oddVals.begin(), oddVals.end());
+            result[i] = oddVals[0];
         }
     }
-
-    sort(evenVals.begin(), evenVals.end());
-
-    vector<float> result;
-    int idx = 0;
-
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) {
-            result.push_back(evenVals[idx++]);
-        } else {
-            result.push_back(oddVals[i - idx]);
-        }
-    }
-
     return result;
 }
