37b55afffe5903a720c0d03cd9a18d77371b74fb
beep boop <githubbot@vadim.me>
2024-07-06 03:09:37 +0200

    Solution 80 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/u4hjxqlv Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -1,4 +1,3 @@
-
 def find_zero(x):
     if x > 0:
         return -1
@@ -7,6 +6,7 @@ def find_zero(x):
     else:
         return 0
 
+
 if __name__ == "__main__":
-    x = int(input("Enter a number: "))
+    x = float(input("Enter a number: "))
     print(find_zero(x))
