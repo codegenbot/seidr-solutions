54d9a211b77e6c803aace39469df6a56b5c19787
beep boop <githubbot@vadim.me>
2024-07-07 01:46:18 +0200

    Solution 0 of Python/113, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vdtlkgnl Prompt: Complete the following code given the task description and function signature.

---
+++ Python_113.py
@@ -0,0 +1,5 @@
+def odd_count(lst):
+    return [
+        f"the number of odd elements {len([int(d) for d in s if int(d) % 2])}n the str{s.index(s)+1} of the {s.index(s)+1}nput."
+        for s in lst
+    ]
