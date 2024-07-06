347a3894404bd3c17bc28575372f678d1f84fe4f
beep boop <githubbot@vadim.me>
2024-07-06 15:06:14 +0200

    Solution 25 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0wk0egm1 Prompt: In file included from 905aa29e-9cd9-4ef6-881c-576f85eaf57e.cpp:23: 905aa29e-9cd9-4ef6-881c-576f85eaf57e.cpp: In function ‘int main()’: 905aa29e-9cd9-4ef6-881c-576f85eaf57e.cpp:33:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    33 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -4,30 +4,17 @@
 using namespace std;
 
 bool issame(vector<int> v1, vector<int> v2) {
-    if (v1.size() != v2.size()) return false;
+    if (v1.size() != v2.size()) 
+        return false;
+    
     for (int i = 0; i < v1.size(); i++) {
-        if (v1[i] != v2[i]) return false;
+        if (v1[i] != v2[i]) 
+            return false;
     }
+    
     return true;
 }
 
-bool issame(vector<int> v1, int n) {
-    return (v1.size() == n && v1[0] == 111111);
+int main() {
+    assert(issame(strange_sort_vector({111111}), {111111}));
 }
-vector<int> strange_sort_vector(vector<int> lst) {
-    vector<int> result;
-    while (!lst.empty()) {
-        int min_val = *min_element(lst.begin(), lst.end());
-        result.push_back(min_val);
-        lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
-        
-        if (!lst.empty()) {
-            int max_val = *max_element(lst.begin(), lst.end());
-            result.push_back(max_val);
-            lst.erase(remove(lst.begin(), lst.end(), max_val), lst.end());
-        }
-    }
-    
-    return result;
-}
-```
