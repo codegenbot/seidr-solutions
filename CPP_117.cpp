978b7d06b140d6c7572f8a2f97e97dfa92ef01f0
beep boop <githubbot@vadim.me>
2024-07-06 07:20:54 +0200

    Solution 16 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ssvdmxr6 Prompt:     6 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ bf465955-0898-4ba6-b600-02ab567b39eb.cpp:6:37: error: ‘string’ was not declared in this scope     6 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bf465955-0898-4ba6-b600-02ab567b39eb.cpp:6:44: error: ‘b’ was not declared in th is scope     6 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from bf465955-0898-4ba6-b600-02ab567b39eb.cpp:5: bf465955-0898-4ba6-b600-02ab567b39eb.cpp: In function ‘int main()’: bf465955-0898-4ba6-b600-02ab567b39eb.cpp:15:20: error: ‘select_words’ was not de clared in this scope    15 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ bf465955-0898-4ba6-b600-02ab567b39eb.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,7 +1,24 @@
-```cpp
 #include <vector>
 #include <string>
 
 bool issame(vector<string> a, vector<string> b) {
     return a == b;
+}
+
+int select_words(string str, int n) {
+    vector<string> result;
+    for (auto i = 0; i < n; i++) {
+        string word;
+        size_t start = str.find(' ');
+        if (start != string::npos) {
+            size_t end = str.find(' ', start + 1);
+            if (end == string::npos)
+                end = str.length();
+            word = str.substr(start, end - start);
+            str.erase(0, end);
+        } else
+            break;
+        result.push_back(word);
+    }
+    return result.size();
 }
