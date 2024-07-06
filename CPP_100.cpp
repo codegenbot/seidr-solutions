1366d24d738f44bd8e8c4a4cbab86a85c89eeb1e
beep boop <githubbot@vadim.me>
2024-07-06 16:23:19 +0200

    Solution 21 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l4ahvohl Prompt:       |                    ^~~ b8e0e811-b439-4b6d-a570-69105a106d49.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b8e0e811-b439-4b6d-a570-69105a106d49.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b8e0e811-b439-4b6d-a570-69105a106d49.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b8e0e811-b439-4b6d-a570-69105a106d49.cpp:16: b8e0e811-b439-4b6d-a570-69105a106d49.cpp: In function ‘int main()’: b8e0e811-b439-4b6d-a570-69105a106d49.cpp:26:20: error: ‘make_a_pile’ was not dec lared in this scope    26 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ b8e0e811-b439-4b6d-a570-69105a106d49.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,20 +1,12 @@
 #include <vector>
-#include <algorithm>
 
 bool isSame(vector<int> a, vector<int> b) {
-    return (a == b);
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> pile = make_a_pile(n);
-    for(int i=0; i<n; i++){
-        cout << pile[i] << " ";
-    }
-    cout << endl;
-    assert(isSame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
-    return 0;
+    if(a.size()!=b.size())
+        return false;
+    for(int i=0; i<a.size();i++)
+        if(a[i]!=b[i])
+            return false;
+    return true;
 }
 
 vector<int> make_a_pile(int n) {
