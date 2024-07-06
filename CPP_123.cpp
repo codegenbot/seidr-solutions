bb2b60fbe595acd013447c807d3362394873d249
beep boop <githubbot@vadim.me>
2024-07-06 07:53:39 +0200

    Solution 2 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/o1mdw7cz Prompt:       |                    ^~~ 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp:17: 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp: In function ‘int main()’: 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp:27:20: error: ‘get_odd_collatz’ was not  declared in this scope    27 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,14 +1,25 @@
+#include <vector>
+#include <algorithm>
+
+bool issame(vector<int> a,vector<int>b){
+    return a==b;
+}
+
 vector<int> get_odd_collatz(int n) {
     vector<int> result;
     while (n != 1) {
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else {
+        if (n % 2 == 0)
+            n /= 2;
+        else
             n = 3 * n + 1;
-        }
-        if (n % 2 != 0) {
+        if (n % 2 != 0)
             result.push_back(n);
-        }
     }
+    sort(result.begin(), result.end());
     return result;
+}
+
+int main() {
+    assert (issame({get_odd_collatz(1)}, vector<int>({1})));
+    // Add your code here
 }
