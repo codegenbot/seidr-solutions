1b7d99ffaf8913a91d85889b1c2ce04dd3e16b10
beep boop <githubbot@vadim.me>
2024-07-06 16:29:12 +0200

    Solution 44 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/n9sebtun Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 0e65b1de-5bc1-469a-963b-4e45fdc478cc.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 0e65b1de-5bc1-469a-963b-4e45fdc478cc.cpp:54:44: error: ‘b’ was not declared in t his scope    54 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0e65b1de-5bc1-469a-963b-4e45fdc478cc.cpp:62:5: error: redefinition of ‘int main( )’    62 | int main(){       |     ^~~~ 0e65b1de-5bc1-469a-963b-4e45fdc478cc.cpp:47:5: note: ‘int main()’ previously def ined here    47 | int main() {       |     ^~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -11,33 +11,45 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return true;
 }
 
-std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
-    std::vector<std::string> result;
+using namespace std;
+
+std::vector<string> numerical_letter_grade(vector<float> grades) {
+    vector<string> result;
     for (float grade : grades) {
-        std::string letterGrade;
-        if (grade >= 4.0f) {
+        string letterGrade;
+        if (grade >= 4.0) {
             letterGrade = "A+";
-        } else if (grade > 3.7f) {
+        } else if (grade > 3.7) {
             letterGrade = "A";
-        } else if (grade > 3.3f) {
+        } 
+        else if (grade > 3.3) {
             letterGrade = "A-";
-        } else if (grade > 3.0f) {
+        } 
+        else if (grade > 3.0) {
             letterGrade = "B+";
-        } else if (grade > 2.7f) {
+        } 
+        else if (grade > 2.7) {
             letterGrade = "B";
-        } else if (grade > 2.3f) {
+        } 
+        else if (grade > 2.3) {
             letterGrade = "B-";
-        } else if (grade > 2.0f) {
+        } 
+        else if (grade > 2.0) {
             letterGrade = "C+";
-        } else if (grade > 1.7f) {
+        } 
+        else if (grade > 1.7) {
             letterGrade = "C";
-        } else if (grade > 1.3f) {
+        } 
+        else if (grade > 1.3) {
             letterGrade = "C-";
-        } else if (grade > 1.0f) {
+        } 
+        else if (grade > 1.0) {
             letterGrade = "D+";
-        } else if (grade > 0.7f) {
+        } 
+        else if (grade > 0.7) {
             letterGrade = "D";
-        } else {
+        } 
+        else {
             letterGrade = "E";
         }
         result.push_back(letterGrade);
@@ -46,7 +58,7 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
 }
 
 int main() {
-    std::vector<float> testGrades = {0.0f, 0.7f};
-    assert(issame(numerical_letter_grade(testGrades), {"E", "D-"})); 
+    vector<float> testGrades = {0, 0.7};
+    assert(issame(numerical_letter_grade(testGrades), {"E", "D-"}) ); 
     return 0;
 }
