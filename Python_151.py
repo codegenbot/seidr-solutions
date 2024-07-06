540c9445b2bbf7d6be529ab3ff16695e2f9a5d78
beep boop <githubbot@vadim.me>
2024-07-06 08:11:03 +0200

    Solution 58 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5bhr3tfe Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/57943be2-cd35-481d-909d-12e66888a ef2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,8 +1,8 @@
 ```
-def double_the_difference(lst):
-    diff = (min(lst) + max(lst)) / 2
-    result = sum(lst) - 2 * diff
-    return result
+def double_the_difference(input_list):
+    first_max = max(input_list)
+    second_min = min(input_list) + 1
+    return (first_max - second_min)
 
 def check(func, input_list, expected_result):
     result = func(input_list)
