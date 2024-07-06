e032392db95103053a27ffbec1a6dea5a8aead1f
beep boop <githubbot@vadim.me>
2024-07-06 03:54:09 +0200

    Solution 46 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lb1rqho5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa05ed2f-5d9e-4eb2-8134-0a4364cdf 910.py", line 26, in <module>     check(simplify)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa05ed2f-5d9e-4eb2-8134-0a4364cdf 910.py", line 23, in check     assert simplify("1/5", "1/5") == False, "test13"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test13

--- Python_144.py
+++ Python_144.py
@@ -10,13 +10,8 @@ def simplify(x, n):
 
     common = gcd(x_denom, n_denom)
     new_x_num = x_num // common
-    new_x_denom = x_denom // common 
+    new_x_denom = x_denom // common
     new_n_num = n_num // common
-    new_n_denom = n_denom // common 
+    new_n_denom = n_denom // common
 
-    if x_num // x_denom * new_n_denom == new_n_num:
-        return True
-    elif new_x_num * n_denom == new_n_num * x_denom:
-        return True
-    else:
-        return False
+    return (new_x_num / new_x_denom) == (new_n_num / new_n_denom)
