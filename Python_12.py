8828100acc785d536e5b1524fec29fafbd5b50fa
beep boop <githubbot@vadim.me>
2024-07-06 10:49:54 +0200

    Solution 9 of Python/12, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/z9phpcg9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a204bb02-23bc-415e-8a01-cd94fab86 2b6.py", line 1, in <module>     def longest(strings: List[str]) -> Optional[str]:                          ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_12.py
+++ Python_12.py
@@ -1,11 +1,10 @@
-
-def longest(strings: list[str]) -> Optional[str]:
-if not strings:
-return None
-max_length = 0
-longest_string = None
-for string in strings:
-if len(string) > max_length:
-max_length = len(string)
-longest_string = string
-return longest_string
+def longest(strings):
+    if not strings:
+        return None
+    max_length = 0
+    longest_string = None
+    for string in strings:
+        if len(string) > max_length:
+            max_length = len(string)
+            longest_string = string
+    return longest_string
