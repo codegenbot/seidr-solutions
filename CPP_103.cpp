220d4b835f6bd1c546a7daaecde97b8f74fb6a3f
beep boop <githubbot@vadim.me>
2024-07-06 17:06:40 +0200

    Solution 28 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1273nzok Prompt: 5bfbfb45-e36c-43d4-acc5-5e06d62e3983.cpp: In function ‘std::string rounded_avg(i nt, int)’: 5bfbfb45-e36c-43d4-acc5-5e06d62e3983.cpp:11:18: error: ‘round’ was not declared in this scope    11 |     double avg = round((double)sum / (m - n + 1));       |                  ^~~~~ 5bfbfb45-e36c-43d4-acc5-5e06d62e3983.cpp:14:17: error: invalid operands of types  ‘double’ and ‘int’ to binary ‘operator&’    14 |         if (avg & 1) res.push_back('1');       |             ~~~ ^ ~       |             |     |       |             |     int       |             double 5bfbfb45-e36c-43d4-acc5-5e06d62e3983.cpp:18:5: error: ‘reverse’ was not declared  in this scope    18 |     reverse(res.begin(), res.end());       |     ^~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,18 +1,22 @@
 #include <string>
+#include <cmath>
+#include <algorithm>
 
-string rounded_avg(int n,int m){
-    if(n > m) return "-1";
-    int sum = 0;
-    for(int i=n; i<=m; i++){
+using namespace std;
+
+string rounded_avg(int n, int m) {
+    if (n > m) return "-1";
+    long long sum = 0;
+    for (int i = n; i <= m; i++) {
         sum += i;
     }
-    double avg = (double)sum / (m - n + 1);
-    int rounded = floor(avg + 0.5);
-    string binary = "";
-    while(rounded > 0){
-        if(rounded & 1) binary.push_back('1');
-        else binary.push_back('0');
-        rounded >>= 1;
+    double avg = round((double)sum / (m - n + 1));
+    string res = "";
+    while (avg > 0) {
+        if (avg % 1) res.push_back('1');
+        else res.push_back('0');
+        avg /= 2;
     }
-    reverse(binary.begin(), binary.end());
-    return binary;
+    reverse(res.begin(), res.end());
+    return res;
+}
