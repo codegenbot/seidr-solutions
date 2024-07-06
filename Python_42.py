7b7e22751b09454d4aba5789d93c7385e6e5e3ec
beep boop <githubbot@vadim.me>
2024-07-06 16:55:46 +0200

    Solution 14 of Python/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zzh9c5ge Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a66d8856-7360-48e9-a0d3-485babd14 ad2.py", line 2     """     ^ SyntaxError: unterminated triple-quoted string literal (detected at line 19)

--- Python_42.py
+++ Python_42.py
@@ -1,7 +1,6 @@
+```
 def incr_list():
     """
-    Returns a list of numbers from 1 to n.
-    
     while True:
         try:
             n = int(input("Enter a number: "))
@@ -9,8 +8,7 @@ def incr_list():
                 print("Number should be positive. Try again.")
             else:
                 return list(range(1, n+1))
-            break
         except ValueError:
             print("Invalid input. Please enter an integer.")
-
-print(incr_list())
+    """
+    print(incr_list())
