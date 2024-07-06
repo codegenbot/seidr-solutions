bee0d22f7b4e26d479e6084bcf7d87b97d897563
beep boop <githubbot@vadim.me>
2024-07-06 18:01:32 +0200

    Solution 8 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ty8qo195 Prompt: 2980aa0a-ed24-477c-be55-db0549ec7f9f.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | bool is_sorted(vector<int> lst) {       |                ^~~~~~ 2980aa0a-ed24-477c-be55-db0549ec7f9f.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | bool is_sorted(vector<int> lst) {       |                       ^~~ In file included from 2980aa0a-ed24-477c-be55-db0549ec7f9f.cpp:12: 2980aa0a-ed24-477c-be55-db0549ec7f9f.cpp: In function ‘int main()’: 2980aa0a-ed24-477c-be55-db0549ec7f9f.cpp:14:22: error: ‘is_sorted’ cannot be use d as a function    14 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ~~~~~~~~~^~~~~~~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,15 +1,11 @@
 #include <vector>
-#include <algorithm>
-
-namespace std {
-    bool is_sorted(vector<int> lst) {
-        for (int i = 1; i < lst.size(); i++) {
-            if (lst[i] <= lst[i - 1]) {
-                return false;
-            }
+bool is_sorted(vector<int> lst) {
+    for (int i = 1; i < lst.size(); i++) {
+        if (lst[i] <= lst[i - 1]) {
+            return false;
         }
-        vector<int> temp(lst.begin(), lst.end());
-        sort(temp.begin(), temp.end());
-        return lst == temp;
     }
+    vector<int> temp(lst.begin(), lst.end());
+    sort(temp.begin(), temp.end());
+    return lst == temp;
 }
