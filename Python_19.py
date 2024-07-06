725526304d21ad0c699087019096ca9386843729
beep boop <githubbot@vadim.me>
2024-07-06 08:26:54 +0200

    Solution 2 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/db6xjf2u Prompt: Complete the following code given the task description and function signature.

--- Python_19.py
+++ Python_19.py
@@ -1,3 +1,5 @@
+
 def sort_numbers(numbers: str) -> str:
     number_list = numbers.split()
-    return " ".join(sorted(number_list))
+    number_list.sort(key=lambda x: int(x))
+    return " ".join(number_list)
