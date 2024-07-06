2c441df48f942877b25a887ad98edc775e973b34
beep boop <githubbot@vadim.me>
2024-07-06 04:13:13 +0200

    Solution 64 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lb1rqho5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6e5ede79-2d46-4a9d-a62c-d9b2f7e5d ccc.py", line 25, in <module>     check(simplify)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6e5ede79-2d46-4a9d-a62c-d9b2f7e5d ccc.py", line 22, in check     assert simplify("1/5", "1/5") == False, "test13"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test13

--- Python_144.py
+++ Python_144.py
@@ -1,4 +1,3 @@
-```
 def simplify(x, n):
     x_num, x_denom = map(int, x.split("/"))
     n_num, n_denom = map(int, n.split("/"))
@@ -16,4 +15,4 @@ def simplify(x, n):
 
     fraction_x = new_x_num / new_x_denom
     fraction_n = new_n_num / new_n_denom
-    return x_num != n_num or x_denom != n_denom
+    return x_num == n_num and x_denom == n_denom
