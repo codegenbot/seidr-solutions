8f7f5e23e2557a82f642150ceae5aaaf5adf0149
beep boop <githubbot@vadim.me>
2024-07-06 18:28:53 +0200

    Solution 38 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r0qsr2nm Prompt:    45 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 89989290-8dba-440b-8809-67cc369b1c07.cpp:53:5: error: redefinition of ‘int main( )’    53 | int main(){       |     ^~~~ 89989290-8dba-440b-8809-67cc369b1c07.cpp:39:5: note: ‘int main()’ previously def ined here    39 | int main() {       |     ^~~~ In file included from 89989290-8dba-440b-8809-67cc369b1c07.cpp:44: 89989290-8dba-440b-8809-67cc369b1c07.cpp: In function ‘int main()’: 89989290-8dba-440b-8809-67cc369b1c07.cpp:54:20: error: ‘order_by_points’ was not  declared in this scope    54 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 89989290-8dba-440b-8809-67cc369b1c07.cpp:54:19: error: ‘issame’ cannot be used a s a function    54 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -34,18 +34,6 @@ vector<int> order_by_points(vector<int> nums) {
 
 }
 
-vector<int> order_by_points(vector<int> nums) {
-    vector<pair<int, int>> pairs;
-    for (int i = 0; i < nums.size(); i++) {
-        pairs.push_back({get_sum_of_digits(nums[i]), i});
-    }
-    sort(pairs.begin(), pairs.end());
-    vector<int> result;
-    for (const auto& pair : pairs) {
-        result.push_back(nums[pair.second]);
-    }
-    return result;
-
 int main() {
     assert(issame(vector<int>(order_by_points({0,6,6,-76,-21,23,4})), vector<int>({(-76), (-21), (0), (4), (23), (6), (6)})));
     return 0;
