50df10739612ca54fa684e5c55ffad335be140d0
beep boop <githubbot@vadim.me>
2024-07-06 02:18:41 +0200

    Solution 29 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/h77vs2tk Prompt: Program did not receive expected input

--- Python_28.py
+++ Python_28.py
@@ -3,5 +3,4 @@ from typing import List
 def concatenate(strings: List[str]) -> str:
     return ''.join([s.strip() for s in strings])
 
-print(concatenate(["   Hello", "   World" ]))  # Expected output: "HelloWorld"
-print(concatenate(["Hello", "World"]))  # Expected output: "HelloWorld"
+print(concatenate(["   Hello", "   World" ]))
