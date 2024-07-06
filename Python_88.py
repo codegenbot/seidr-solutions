ef8191dd7021ae676e80e2985e05b25c6aad143d
beep boop <githubbot@vadim.me>
2024-07-06 18:07:37 +0200

    Solution 12 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vtonw1kq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c51e30f8-3ac8-46d1-b978-5de4d0344 e40.py", line 13, in <module>     check(sort_array)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c51e30f8-3ac8-46d1-b978-5de4d0344 e40.py", line 10, in check     assert sort_array([21, 14, 23, 11]) == [23, 21, 14, 11], "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_88.py
+++ Python_88.py
@@ -2,5 +2,5 @@ def sort_array(input_array=None):
     if input_array is None:
         array = list(map(int, input("Enter numbers separated by space: ").split()))
     else:
-        array = input_array
-    return sorted(array, key=int)
+        array = sorted(input_array)
+    return array
