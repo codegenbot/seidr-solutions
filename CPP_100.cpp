4283b2f64165fb3d061f341274554f930683695a
beep boop <githubbot@vadim.me>
2024-07-06 16:50:45 +0200

    Solution 48 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l4ahvohl Prompt: In file included from /usr/include/c++/11/vector:67,                  from 1c4d6bd7-894d-4bba-a2d1-3ec554929071.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1c4d6bd7-894d-4bba-a2d1-3ec554929071.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1c4d6bd7-894d-4bba-a2d1-3ec554929071.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1c4d6bd7-894d-4bba-a2d1-3ec554929071.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1c4d6bd7-894d-4bba-a2d1-3ec554929071.cpp:18: 1c4d6bd7-894d-4bba-a2d1-3ec554929071.cpp: In function ‘int main()’: 1c4d6bd7-894d-4bba-a2d1-3ec554929071.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,26 +1,18 @@
-```cpp
 #include <vector>
-#include <algorithm>
-
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
 std::vector<int> make_a_pile(int n) {
     std::vector<int> pile;
     int stones = 1;
     for (int i = 0; i < n; i++) {
-        if (n % 2 != 0) {
-            pile.push_back(stones);
-            stones += 1;
-        } else {
+        if ((n % 2) == 1) { // n is odd
             pile.push_back(stones);
             stones += 2;
+        } else { // n is even
+            pile.push_back(stones);
+            stones += 1;
         }
     }
     return pile;
-}
-
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
 }
