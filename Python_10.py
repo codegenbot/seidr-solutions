935b9f074cc158435a6f2c9e1f7715ca01060155
beep boop <githubbot@vadim.me>
2024-07-06 04:34:46 +0200

    Solution 25 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r66k6rhr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6747c627-2aab-4651-ac0c-cc2269be2 ff9.py", line 19, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6747c627-2aab-4651-ac0c-cc2269be2 ff9.py", line 16, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6747c627-2aab-4651-ac0c-cc2269be2 ff9.py", line 13, in make_palindrome     return prefix + postfix + postxfix                               ^^^^^^^^ NameError: name 'postxfix' is not defined. Did you mean: 'postfix'?

--- Python_10.py
+++ Python_10.py
@@ -1,1 +1,9 @@
-prefix + postfix + postfix
+```
+def make_palindrome(string: str) -> str:
+    if string == string[::-1]:
+        return string + string
+    for i in range(len(string)):
+        postfix = string[i:]
+        if is_palindrome(postfix):
+            prefix = string[:i]
+            return prefix + postfix + postfix
