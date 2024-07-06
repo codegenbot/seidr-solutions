4d3006039d328bf8a4dc8ddbbdcc4dbf40368d02
beep boop <githubbot@vadim.me>
2024-07-06 11:35:54 +0200

    Solution 93 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3gvl0s3q Prompt: In file included from efc3f552-4b5c-45ed-890b-578754b50dc4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ efc3f552-4b5c-45ed-890b-578754b50dc4.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ efc3f552-4b5c-45ed-890b-578754b50dc4.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ efc3f552-4b5c-45ed-890b-578754b50dc4.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from efc3f552-4b5c-45ed-890b-578754b50dc4.cpp:24: efc3f552-4b5c-45ed-890b-578754b50dc4.cpp: In function ‘int main()’: efc3f552-4b5c-45ed-890b-578754b50dc4.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -20,6 +20,6 @@ vector<int> filter_integers(list<any> values) {
 }
 
 int main() {
-    assert(isSame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), vector<int>{3, 3, 3}));
+    assert(isSame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
     return 0;
 }
