624e685263b14e743b793a6f69e8ae225707038f
beep boop <githubbot@vadim.me>
2024-07-06 17:10:23 +0200

    Solution 62 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9hrt0837 Prompt: f1ec9c7f-9625-4feb-aeda-f2a4f9167bde.cpp:1:19: error: ‘string’ is not a member o f ‘std’     1 | int is_bored(std::string S){       |                   ^~~~~~ f1ec9c7f-9625-4feb-aeda-f2a4f9167bde.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int is_bored(std::string S){

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,11 +1,2 @@
 #include <string>
-int is_bored(std::string S){ 
-    int count = 0;
-    std::istringstream iss(S);
-    std::string word;
-
-    while(getline(iss, word, ' ')) {
-        if(word.size() > 0 && word[0] == 'I')
-            ++count;
-    }
-    return count;
+int is_bored(std::string S){
