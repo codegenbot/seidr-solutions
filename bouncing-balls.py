fb30035c48cc86ee37e5fc8f1cea91cec2083caa
beep boop <githubbot@vadim.me>
2024-07-06 03:39:30 +0200

    Solution 18 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/vnupghm1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/990cd5c4-4dca-4def-b4e0-17075b787 865.py", line 1     def '...'':              ^ SyntaxError: unterminated string literal (detected at line 1)

--- bouncing-balls.py
+++ bouncing-balls.py
@@ -1,3 +1,4 @@
+```
 def bouncing_balls():
     start_height = float(input())
     first_bounce_height = float(input())
@@ -6,9 +7,10 @@ def bouncing_balls():
 
     distance = 0
     for _ in range(num_bounces):
-        start_height = round(start_height * (1 - bounciness_index) * 2, 5)
+        start_height *= (1 - bounciness_index) * 2  
         distance += start_height
 
     return round(distance, 5)
+
 
 print(bouncing_balls())
