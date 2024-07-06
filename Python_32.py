c9a6c1378995e685b8f609e0b8f60de6d1de40b9
beep boop <githubbot@vadim.me>
2024-07-06 02:28:38 +0200

    Solution 46 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/u4hjxqlv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/be9f54fd-d5c3-464d-aff2-4b7da41db 56e.py", line 29, in <module>     check(find_zero)           ^^^^^^^^^ NameError: name 'find_zero' is not defined

--- Python_32.py
+++ Python_32.py
@@ -1,16 +1,9 @@
-def find_negative_ratio(xs):
-    if len(xs) == 0:
-        raise ValueError("Input list must not be empty.")
-    for i in range(len(xs)):
-        if xs[i] == 0:
-            continue
-        x = -xs[i - 1] / (xs[i] * xs[i])
-        return round(x, 2)
-
-
-def find_zero(ys):
-    for i in range(len(ys)):
-        if ys[i] == 0:
-            continue
-        x = ys[i - 1] / (ys[i] * ys[i])
-        return round(x, 2)
+    def find_negative_ratio(xs):
+        if len(xs) == 0:
+            raise ValueError("Input list must not be empty.")
+        for i in range(len(xs)):
+            if xs[i] == 0:
+                continue
+            x = -xs[i - 1] / (xs[i] * xs[i])
+            return round(x, 2)
+```
