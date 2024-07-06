4489f3dd89460ef526806891147c5fbde8a26047
beep boop <githubbot@vadim.me>
2024-07-06 20:53:21 +0200

    Solution 3 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9ulxeypd Prompt: Complete the following code given the task description and function signature.

--- Python_14.py
+++ Python_14.py
@@ -2,7 +2,4 @@ from typing import List
 
 
 def all_prefixes(string: str) -> List[str]:
-    result = []
-    for i in range(len(string) + 1):
-        result.append(string[:i])
-    return result
+    return [string[:i] for i in range(1, len(string) + 1)]
