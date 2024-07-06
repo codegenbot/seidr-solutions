5ff7a47ef60afd2bd3b47b92b69e9d5d7efbad6a
beep boop <githubbot@vadim.me>
2024-07-06 06:22:17 +0200

    Solution 68 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l5vx0nhc Prompt: Program did not receive expected input

--- Python_17.py
+++ Python_17.py
@@ -1,13 +1,15 @@
-```
+```Python
 from typing import List
 
 def parse_music(music_string: str) -> List[int]:
     beats = [0]
     for note in music_string.split():
-        if note == "o":
+        if note not in ["o", "o|", ".|"]:
+            return [] 
+        elif note == "o":
             beats.append(4)
         elif note == "o|":
-            beats.extend([2, 2])
+            beats.append(2)
         elif note == ".|":
-            beats.extend([1, 3])
+            beats.append(1)
     return beats[1:]
