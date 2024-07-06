82e44b786fdcd8f6d134c2ade4b2e5c73ef839f9
beep boop <githubbot@vadim.me>
2024-07-06 18:20:52 +0200

    Solution 16 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qeuhcwp8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e6099cd-5ace-4213-bc95-bb05d8174 1cf.py", line 18, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e6099cd-5ace-4213-bc95-bb05d8174 1cf.py", line 15, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^ TypeError: solve() takes 0 positional arguments but 1 was given

--- Python_84.py
+++ Python_84.py
@@ -1,14 +1,13 @@
-```
 def solve():
-    if int(input("Do you want to (1) Convert decimal to binary or (2) Convert binary to decimal? ")) == 1:
+    choice = input("Do you want to (1) Convert decimal to binary or (2) Convert binary to decimal? ")
+    
+    if choice == '1':
         N = int(input("Enter a number: "))
-        return bin(N)[2:]
-    elif int(input("Do you want to (1) Convert decimal to binary or (2) Convert binary to decimal? ")) == 2:
+        return "Binary: " + str(bin(N)[2:])
+    elif choice == '2':
         N = input("Enter a binary number: ")
-        return oct(int(N, 2))[2:]
+        return "Decimal: " + str(oct(int(N, 2))[2:])
     else:
-        num1 = int(input("Enter a number: "))
-        num2 = int(input("Enter another number: "))
-        return str(num1) * str(num2)
-
-print(solve())
+        n1 = int(input("Enter a number: "))
+        n2 = int(input("Enter another number: "))
+        return str(n1 * n2)
