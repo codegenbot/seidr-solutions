b5420be7a8922fbf14e210b234f904c5851b7934
beep boop <githubbot@vadim.me>
2024-07-06 02:54:35 +0200

    Solution 19 of CPP/64, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4eaq01xw Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 28000cea-c0bb-42d2-a64f-f3a361ea7904.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<const char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 28000cea-c0bb-42d2-a64f-f3a361ea7904.cpp:18:5:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_64.cpp
+++ CPP_64.cpp
@@ -1,11 +1,12 @@
+```cpp
 #include <string>
 using namespace std;
 
-int vowels_count(const char *s) {
+int vowels_count(const char* s) {
     int count = 0;
     for (char c : string(s)) {
         if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-            (c == 'y' && ((string(s)).back() == 'y'))) {
+            (c == 'y' && s[strlen(s)-1] == 'y')) {
             count++;
         }
     }
