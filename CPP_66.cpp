ffae96adeb92d94a39e3c70433a505ddd8093006
beep boop <githubbot@vadim.me>
2024-07-06 02:47:05 +0200

    Solution 11 of CPP/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cttezcfe Prompt: Program failed

--- CPP_66.cpp
+++ CPP_66.cpp
@@ -2,7 +2,7 @@ int digitSum(std::string s) {
     int sum = 0;
     for (char c : s) {
         if (std::isupper(c)) {
-            sum += std::toupper(c) - 'A' + 1; // convert to uppercase first
+            sum += std::toupper(c) - 'A' + 1; 
         }
     }
     return sum;
