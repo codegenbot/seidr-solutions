976b4ecd340b871eaf93fb22e0383ffe15a3af9b
beep boop <githubbot@vadim.me>
2024-07-06 18:35:10 +0200

    Solution 35 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/wpnto8tb Prompt:       |                    ^~~ e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp:15: e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp: In function ‘int main()’: e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp:25:20: error: ‘compare’ was not declare d in this scope    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,19 +1,18 @@
 #include <vector>
-#include <iostream>
+#include <algorithm>
 
 std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
-    vector<int> result;
-    if (issame(game, guess)) {
-        for (int i = 0; i < game.size(); i++) {
-            result.push_back(1);
-        }
-    } else {
+    if (!issame(game, guess)) {
         for (int i = 0; i < game.size(); i++) {
             if (game[i] == guess[i]) {
                 result.push_back(0);
             } else {
                 result.push_back(abs(game[i] - guess[i]));
             }
+        }
+    } else {
+        for (int i = 0; i < game.size(); i++) {
+            result.push_back(-1);
         }
     }
     return result;
