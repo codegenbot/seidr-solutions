3c9aa4b5047ac7d21b01b754d23053a7489fac3f
beep boop <githubbot@vadim.me>
2024-07-06 02:41:18 +0200

    Solution 16 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/zqqswxr4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a573e1df-fe28-4085-8a81-53005ce1b e9e.py", line 16, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a573e1df-fe28-4085-8a81-53005ce1b e9e.py", line 13, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -7,5 +7,9 @@ def make_palindrome(string):
     else:
         i = len(string) - 1
     
-    # Append to the end of the string reverse of a string prefix that comes before the palindromic suffix
-    return string + string[:i+1][::-1]
+    # If the input string is a palindrome, return it as is
+    if string == string[::-1]:
+        return string
+    
+    # Otherwise, append to the end of the string reverse of a string prefix that comes before the palindromic suffix
+    return string + suffix[: i + 1][::-1]
