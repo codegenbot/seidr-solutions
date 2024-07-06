50027a1c5d5dc670d11e151aa50799c37406c166
beep boop <githubbot@vadim.me>
2024-07-06 16:14:37 +0200

    Solution 31 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/n9sebtun Prompt:       |                              ^~~~~~ 7619148b-2994-492b-a67c-c13115aed9f6.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 7619148b-2994-492b-a67c-c13115aed9f6.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7619148b-2994-492b-a67c-c13115aed9f6.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 7619148b-2994-492b-a67c-c13115aed9f6.cpp:25: 7619148b-2994-492b-a67c-c13115aed9f6.cpp: In function ‘int main()’: 7619148b-2994-492b-a67c-c13115aed9f6.cpp:35:20: error: ‘numerical_letter_grade’ was not declared in this scope    35 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 7619148b-2994-492b-a67c-c13115aed9f6.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,7 +1,8 @@
+```cpp
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b){
+bool issame(vector<string> a, vector<string> b){
     return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
 }
 
@@ -30,3 +31,4 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
 int main(){
     assert(std::equal(numerical_letter_grade({0, 0.7}), {"E", "D-"}.begin(), {"E", "D-"}.end()));
     return 0;
+}
