628ac3beb0bf43314e1973100fe6892ca79ff52c
beep boop <githubbot@vadim.me>
2024-07-06 07:01:04 +0200

    Solution 46 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gxmme9ma Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa06ac1b-a84d-4298-bc94-a68253fe4 27b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -6,7 +6,7 @@ def encode(message):
             if char.lower() in "aeiou":
                 result += chr((ord(char) - ord('a') + 2) % 26 + ord('a'))
             else:
-                result += char.swapcase()
+                result += chr((ord(char.lower()) - ord('a') + 1) % 26 + ord('a'))
         else:
             result += char
     return result
