478bf2858eb884d4240f042688758602e20bccf1
beep boop <githubbot@vadim.me>
2024-07-06 17:06:22 +0200

    Solution 69 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1fv3kcsh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e0fb74b8-c6e2-49ae-8c51-eac3a6e67 f69.py", line 1     Here is the modified code:                 ^^^^^^^^ SyntaxError: invalid syntax

--- Python_37.py
+++ Python_37.py
@@ -1,3 +1,4 @@
 def sort_even(l: list) -> tuple:
-    numbers = sorted([x for x in l if isinstance(x, int)])
-    return (*[n for n in numbers if n % 2 == 0], * [n for n in numbers if n % 2 != 0])
+    even_numbers = sorted([x for x in l if isinstance(x, int) and x % 2 == 0])
+    odd_numbers = sorted([x for x in l if isinstance(x, int) and x % 2 != 0])
+    return tuple(even_numbers), tuple(odd_numbers)
