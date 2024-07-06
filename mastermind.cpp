68c8134beb0cb3dcc31a865364cd71ce21ce6d19
beep boop <githubbot@vadim.me>
2024-07-06 15:30:20 +0200

    Solution 14 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/uzdkq0v8 Prompt: it must return 0 1 for input YRRW OBRB, but it returns .

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main() {
-   string code, guess;
-   int whitePegs = 0, blackPegs = 0;
-   while (getline(cin, code)) {
-       getline(cin, guess);
-       for (int i = 0; i < code.length(); i++) {
-           if (code[i] == guess[i]) {
-               blackPegs++;
-           } else if (code.find(guess[i]) != string::npos) {
-               whitePegs++;
-           }
-       }
-       if (blackPegs == 0) {
-           cout << "1 " << whitePegs << endl;
-       } else {
-           cout << whitePegs << " " << blackPegs << endl;
-       }
-   }
-   return 0;
+    string code, guess;
+    int whitePegs = 0, blackPegs = 0;
+
+    while (getline(cin, code)) { // read entire line of input into "code"
+        getline(cin, guess); // read entire line of input into "guess"
+
+        for (int i = 0; i < code.length(); i++) { // iterate through characters of "code" and "guess"
+            if (code[i] == guess[i]) { // if characters match, increment blackPegs
+                blackPegs++;
+            } else if (code.find(guess[i]) != string::npos) { // if character in "code" is found in "guess", increment whitePegs
+                whitePegs++;
+            }
+        }
+
+        cout << blackPegs << endl; // print results
+    }
+
+    return 0;
 }
