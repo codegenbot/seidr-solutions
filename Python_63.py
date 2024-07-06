2e63d48deb92be28b0a0645a53c438c8ee170bde
beep boop <githubbot@vadim.me>
2024-07-06 19:38:57 +0200

    Solution 94 of Python/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0kwfpmzd Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/15774a12-54fc-41b6-8b8f-d8d111933 1ec.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_63.py
+++ Python_63.py
@@ -1,3 +1,4 @@
+```
 def fibfib(n: int):
     if n < 0:
         return "Error: Input must be non-negative."
@@ -7,4 +8,4 @@ def fibfib(n: int):
         fib_sequence = [0, 1]
         for i in range(2, n + 1):
             fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
-        return str(sum(fib_sequence))
+        return str(fib_sequence)
