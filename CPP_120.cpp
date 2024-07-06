0abe16984cf8a32f93f76b754e48089b4108b5d8
beep boop <githubbot@vadim.me>
2024-07-06 17:45:39 +0200

    Solution 15 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/aagr2av9 Prompt:       |                    ^~~ 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp:9: 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp: In function ‘int main()’: 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp:19:20: error: ‘maximum’ was not declare d in this scope    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -2,17 +2,26 @@
 #include <algorithm>
 
 bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
-            return false;
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i<a.size(); i++) {
+        if(a[i] != b[i]) return false;
     }
     return true;
 }
 
-vector<int> maximum(vector<int> arr, int k) {
+int main() {
+    vector<int> arr;
+    int k, n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    cout << "Enter " << n << " integers: ";
+    for(int i = 0; i<n; i++) {
+        cin >> k;
+        arr.push_back(k);
+    }
+    cout << "Maximum " << k << " numbers are: ";
     vector<int> result(arr.begin(), arr.begin() + k);
     sort(result.begin(), result.end());
-    return result;
+    for(auto x : result) cout << x << " ";
+    return 0;
 }
