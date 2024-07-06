df062fce3902116948c1e155507150556a157828
beep boop <githubbot@vadim.me>
2024-07-06 09:18:21 +0200

    Solution 28 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/a1tfr568 Prompt:       |             ^~~~~~ 27fd0cbe-0d88-4947-948a-c60bc1f20c64.cpp:34:34: error: expected primary-expressi on before ‘int’    34 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 27fd0cbe-0d88-4947-948a-c60bc1f20c64.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 27fd0cbe-0d88-4947-948a-c60bc1f20c64.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from 27fd0cbe-0d88-4947-948a-c60bc1f20c64.cpp:33: 27fd0cbe-0d88-4947-948a-c60bc1f20c64.cpp: In function ‘int main()’: 27fd0cbe-0d88-4947-948a-c60bc1f20c64.cpp:43:20: error: ‘maximum’ was not declare d in this scope; did you mean ‘std::maximum’?    43 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 27fd0cbe-0d88-4947-948a-c60bc1f20c64.cpp:13:18: note: ‘std::maximum’ declared he re    13 | std::vector<int> maximum(std::vector<int> arr, int k) {       |                  ^~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,7 +1,8 @@
-#include <vector>
 #include <algorithm>
+#include <iostream>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+namespace myns {
+bool is_same(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) return false;
@@ -9,7 +10,6 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return true;
 }
 
-namespace std {
 std::vector<int> maximum(std::vector<int> arr, int k) {
     std::vector<int> result;
     for (int i = 0; i < k; ++i) {
@@ -26,6 +26,6 @@ int main() {
     int arr[] = {1, 2, 3, -23, 243, -400, 0};
     int n = sizeof(arr)/sizeof(arr[0]);
     std::vector<int> array (arr, arr+n);
-    assert(issame(maximum(array, 4), {0, 1, 2, 3}));
+    assert(myns::is_same(myns::maximum(array, 4), ({0, 1, 2, 3})));
     return 0;
 }
