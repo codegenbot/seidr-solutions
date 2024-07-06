ef7295e95a9733459e4b3551134582450ccdfb53
beep boop <githubbot@vadim.me>
2024-07-06 23:37:32 +0200

    Solution 78 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/f2nqh60y Prompt:     1 | Here's the solution:       |     ^ 55575ebc-5452-46ad-bf2d-a82d895cde56.cpp:1:5: error: missing terminating ' chara cter     1 | Here's the solution:       |     ^~~~~~~~~~~~~~~~ 55575ebc-5452-46ad-bf2d-a82d895cde56.cpp:1:1: error: ‘Here’ does not name a type     1 | Here's the solution:       | ^~~~ In file included from 55575ebc-5452-46ad-bf2d-a82d895cde56.cpp:13: 55575ebc-5452-46ad-bf2d-a82d895cde56.cpp: In function ‘int main()’: 55575ebc-5452-46ad-bf2d-a82d895cde56.cpp:15:35: error: ‘lst’ was not declared in  this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 55575ebc-5452-46ad-bf2d-a82d895cde56.cpp:15:13: error: ‘double_the_difference’ w as not declared in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ 55575ebc-5452-46ad-bf2d-a82d895cde56.cpp:15:43: error: ‘odd_sum’ was not declare d in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,21 +1,9 @@
-#include<vector>
-using namespace std;
 long long double_the_difference(vector<float> lst) {
     long long sum = 0;
-    vector<float> odd_nums;
-    for (float x : lst) {
+    for (auto x : lst) {
         if (x >= 0 && (int)x == x) {
-            odd_nums.push_back(x);
             sum += pow(x, 2);
         }
     }
     return sum;
-}
-
-int main() {
-    vector<float> lst;
-    // Input elements into the list
-    long long odd_sum = 0;
-    assert(double_the_difference(lst) == odd_sum);
-    return 0;
 }
