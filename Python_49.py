212f2f050c0d54eb2ae9669e2abac18e67ebf905
beep boop <githubbot@vadim.me>
2024-07-06 18:05:54 +0200

    Solution 73 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5ux7p9qd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b12e94c-b96d-4f0b-a1aa-8dd18fcf7 3dc.py", line 14, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b12e94c-b96d-4f0b-a1aa-8dd18fcf7 3dc.py", line 11, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,8 +1,8 @@
 def modp(x, y):
-    return x % y if x >= y else y - ((x // y) * y)
+    return x % (y + 1)
 
 def check(function, arguments):
-    if str(inspect.getsource(function)) == "def modp(x, y): return x % y if x >= y else y - ((x // y) * y)":
+    if str(inspect.getsource(function)) == "def modp(x, y): return x % (y + 1)":
         return f"Modular Polynomial Arithmetic Function is Correct for {arguments}"
     else:
         return "Error: Not a Modular Polynomial Arithmetic Function"
