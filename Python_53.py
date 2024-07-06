e0d5eb4acd008857183f704a7b74ff02deeee9ce
beep boop <githubbot@vadim.me>
2024-07-06 14:00:52 +0200

    Solution 90 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/63ynaj1d Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -1,4 +1,5 @@
 def add(x: int, y: int):
-    x = int(input("Enter first number: "))
-    y = int(input("Enter second number: "))
-    return x + y
+    try:
+        return x + y
+    except ValueError:
+        print("Invalid input.")
