e546f8ba5c4ed5ccde969823f029ce1ce34dd0b9
beep boop <githubbot@vadim.me>
2024-07-06 16:33:05 +0200

    Solution 54 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gashg63h Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/49d8d419-b2f0-45e3-a1d5-4b5f21c35 c32.py", line 1     strings = [input(f"Enter string {i+1}":) for i in range(int(input("How many strings do you have? ")))]                                           ^ SyntaxError: invalid syntax

--- Python_12.py
+++ Python_12.py
@@ -1,3 +1,2 @@
 ```
-num_strings = int(input("How many strings do you have? "))
-strings = [input(f"Enter string {i+1}:") for i in range(num_strings)]
+strings = [input(f"Enter string {i+1}:") for i in range(int(input("How many strings do you have? ")))]
