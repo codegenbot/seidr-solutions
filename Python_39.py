c0a90dc68e18b7f4827ba94b3da51f491d756658
beep boop <githubbot@vadim.me>
2024-07-06 03:49:57 +0200

    Solution 56 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/yny9bc24 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf0d90fa-41cb-4c1f-852e-177f3b221 bda.py", line 26, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf0d90fa-41cb-4c1f-852e-177f3b221 bda.py", line 23, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_39.py
+++ Python_39.py
@@ -1,15 +1,15 @@
 def prime_fib(n):
-    if n <= 0:
-        return None
     fib = [0, 1]
     i = 2
     while len(fib) < n:
         fib.append(fib[i - 1] + fib[i - 2])
         i += 1
-    # Calculate the prime Fibonacci number
-    for j in range(2, int(math.sqrt(fib[-1]))):
-        if fib[-1] % j == 0:
-            break
-    else:
-        return fib[-1]
+
+    if fib[-1] in [0, 1]:
+        return None
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return fib[-1]
+
     return None
