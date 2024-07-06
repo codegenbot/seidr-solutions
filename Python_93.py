8dc89a5834869bc8a67553e71db6c56f85837d10
beep boop <githubbot@vadim.me>
2024-07-06 19:56:05 +0200

    Solution 95 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/26zr7o11 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/365e859c-cd12-4ed4-8662-ce9b16691 066.py", line 32, in <module>     check(encode)           ^^^^^^ NameError: name 'encode' is not defined

--- Python_93.py
+++ Python_93.py
@@ -1,2 +1,30 @@
-def encode(text, mapping={}):
-    return "".join(mapping.get(i.lower(), i) for i in text)
+```
+def encode(string, mapping):
+    return ''.join(mapping.get(i.lower(), i) for i in string)
+
+def check():
+    encoding_mapping = {
+        "i": "k",
+        "I": "K",
+        "d": "d",
+        "o": "QnT",
+        "O": "q",
+        "n": "n",
+        "N": "N",
+        "t": "t",
+        "T": "T",
+        "k": "k",
+        "K": "K",
+        "w": "w",
+        "W": "W",
+        "h": "c",
+        "H": "C",
+        "a": "t",
+        "A": "T",
+        "s": "r",
+        "S": "R",
+    }
+    result = encode("I DoNt KnOw WhAt tO WrItE", encoding_mapping)
+    print(result)
+
+check()
