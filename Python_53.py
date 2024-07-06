94084f2948c0eb539f6a5ca7a85e55a476e37ae4
beep boop <githubbot@vadim.me>
2024-07-06 04:09:28 +0200

    Solution 56 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/92jdlk6h Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -1,5 +1,4 @@
 def add(x: float, y: float) -> float:
+    x = float(input("Enter a number for x: "))
+    y = float(input("Enter a number for y: "))
     return x + y
-
-result = add(float(input("Enter a number for x: ")), float(input("Enter a number for y: ")))
-print(result)
