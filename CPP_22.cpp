b8a7f15ad3567e0eff98911878c3fc66d3f096a3
beep boop <githubbot@vadim.me>
2024-07-06 02:20:23 +0200

    Solution 36 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/brmn1pem Prompt: bcd66b62-f942-4912-b959-ceaac3d39f79.cpp:1:10: fatal error: boost/optional.hpp: No such file or directory     1 | #include <boost/optional.hpp>       |          ^~~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,11 +1,20 @@
-```
-#include <boost/optional.hpp>
-std::vector<int> filter_integers(std::list<boost::any> values) {
-    std::vector<int> result;
+```#include <optional>
+
+#include <vector>
+#include <list>
+#include <type_traits>
+
+using namespace std;
+
+std::vector<int> filter_integers(list<any> values) {
+    vector<int> result;
     for (const auto& value : values) {
-        if(boost::any_cast<boost::optional<int>>(value).has_value()) {
-            int optionalInt = boost::any_cast<boost::optional<int>>(value).get();
-            result.push_back(optionalInt);
+        if (holds_alternative(optional<int>, value)) {
+            optional<int> optionalInt = get<optional<int>>(value);
+            if (optionalInt.has_value()) {
+                int i = optionalInt.get();
+                result.push_back(i);
+            }
         }
     }
     return result;
