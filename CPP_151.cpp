85c6c190f017e3eca4c5791a8cb60e8e3e06bfe5
beep boop <githubbot@vadim.me>
2024-07-06 18:37:07 +0200

    Solution 38 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3z6pdsi9 Prompt: 00ff01a0-1510-49b1-920e-c9bba54d0dfe.cpp: At global scope: 00ff01a0-1510-49b1-920e-c9bba54d0dfe.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 00ff01a0-1510-49b1-920e-c9bba54d0dfe.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 00ff01a0-1510-49b1-920e-c9bba54d0dfe.cpp:20: 00ff01a0-1510-49b1-920e-c9bba54d0dfe.cpp: In function ‘int main()’: 00ff01a0-1510-49b1-920e-c9bba54d0dfe.cpp:22:35: error: ‘lst’ was not declared in  this scope    22 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 00ff01a0-1510-49b1-920e-c9bba54d0dfe.cpp:22:38: error: ‘double_the_difference’ c annot be used as a function    22 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ 00ff01a0-1510-49b1-920e-c9bba54d0dfe.cpp:22:43: error: ‘odd_sum’ was not declare d in this scope    22 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -11,8 +11,8 @@ long long double_the_difference(vector<float> lst) {
 }
 
 int main() {
-    vector<float> lst = {};
+    vector<float> lst = {}; // declare an empty list
     long long odd_sum = double_the_difference(lst);
-    assert(odd_sum == 0);
+    assert(odd_sum == 0); 
     return 0;
 }
