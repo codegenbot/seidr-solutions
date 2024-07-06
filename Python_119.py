8b22bf0e7f681a9477c7a8428130a918bade7735
beep boop <githubbot@vadim.me>
2024-07-06 22:37:04 +0200

    Solution 1 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nd9mp3ud Prompt: Complete the following code given the task description and function signature.

--- Python_119.py
+++ Python_119.py
@@ -6,7 +6,7 @@ def match_parens(lst):
             if c == '(':
                 stack.append(c)
             elif c == ')':
-                if len(stack) == 0:
+                if not stack:
                     return 'No'
                 stack.pop()
-    return 'Yes' if len(stack) == 0 else 'No'
+    return 'Yes' if not stack else 'No'
