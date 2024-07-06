d4ae40df7e9a94c6a673b9b5eb1054a131ea8f7b
beep boop <githubbot@vadim.me>
2024-07-06 18:42:55 +0200

    Solution 46 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/wpnto8tb Prompt:       |                                  ^~~ 7839f069-7da9-47c5-b20c-fcc4b37550b2.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 7839f069-7da9-47c5-b20c-fcc4b37550b2.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 7839f069-7da9-47c5-b20c-fcc4b37550b2.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from 7839f069-7da9-47c5-b20c-fcc4b37550b2.cpp:25: 7839f069-7da9-47c5-b20c-fcc4b37550b2.cpp: In function ‘int main()’: 7839f069-7da9-47c5-b20c-fcc4b37550b2.cpp:35:20: error: ‘compare’ was not declare d in this scope    35 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 7839f069-7da9-47c5-b20c-fcc4b37550b2.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -17,11 +17,4 @@ vector<int> compare(vector<int> game, vector<int> guess) {
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(compare({1,2,3,5},{-1,2,3,4}),vector<int>{2,0,0,1}));
-    return 0;
-}
+assert(same(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));
