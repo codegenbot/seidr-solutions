c337dda01373e4995b09ff1bdd18be92755768aa
beep boop <githubbot@vadim.me>
2024-07-06 14:57:52 +0200

    Solution 84 of CPP/23, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qp68h4xh Prompt:     1 | #include<string>   +++ |+#include <cassert>     2 | using namespace std; 5a8e5734-8853-4321-9e6f-ad822b4a8d23.cpp: At global scope: 5a8e5734-8853-4321-9e6f-ad822b4a8d23.cpp:14:5: error: redefinition of ‘int main( )’    14 | int main(){       |     ^~~~ 5a8e5734-8853-4321-9e6f-ad822b4a8d23.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~ In file included from 5a8e5734-8853-4321-9e6f-ad822b4a8d23.cpp:13: 5a8e5734-8853-4321-9e6f-ad822b4a8d23.cpp: In function ‘int main()’: 5a8e5734-8853-4321-9e6f-ad822b4a8d23.cpp:15:13: error: ‘strlen’ was not declared  in this scope    15 |     assert (strlen("asdasnakj") == 9);       |             ^~~~~~ 5a8e5734-8853-4321-9e6f-ad822b4a8d23.cpp:14:1: note: ‘strlen’ is defined in head er ‘<cstring>’; did you forget to ‘#include <cstring>’?    13 | #include<assert.h>   +++ |+#include <cstring>    14 | int main(){

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,3 +1,14 @@
-int strlen(string str){
+#include<string>
+#include<assert.h>
+#include<cstring>
+
+using namespace std;
+
+int str_length(string str){
     return str.length();
+}
+
+int main2() {
+    assert (str_length("asdasnakj") == 12);
+    return 0;
 }
