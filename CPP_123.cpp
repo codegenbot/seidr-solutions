c6ad4043c9415145dc00b7b481912e6f409c02f2
beep boop <githubbot@vadim.me>
2024-07-06 10:36:23 +0200

    Solution 87 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/o1mdw7cz Prompt:       |                           ^~~~~~ f494fe0d-8b8c-4a8b-9589-d54b4b587793.cpp:37:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from f494fe0d-8b8c-4a8b-9589-d54b4b587793.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from f494fe0d-8b8c-4a8b-9589-d54b4b587793.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ f494fe0d-8b8c-4a8b-9589-d54b4b587793.cpp:37:34: error: expected primary-expressi on before ‘int’    37 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f494fe0d-8b8c-4a8b-9589-d54b4b587793.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ f494fe0d-8b8c-4a8b-9589-d54b4b587793.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -3,7 +3,7 @@
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return (a.size() == 0 && b.size() == 0) || (a.size() == b.size() && a == b);
-};
+}
 
 std::vector<int> get_odd_collatz(int n) {
     std::vector<int> result;
@@ -16,7 +16,7 @@ std::vector<int> get_odd_collatz(int n) {
     }
     std::sort(result.begin(), result.end());
     return result;
-};
+}
 
 int main() {
     assert(issame({get_odd_collatz(12)}, {1, 2, 6}) == true);
@@ -30,3 +30,4 @@ int main() {
     }
     else
         std::cout << "No sequence found." << std::endl;
+}
