51b22654f1a78e8003306644f0e1285002b2e115
beep boop <githubbot@vadim.me>
2024-07-06 04:12:47 +0200

    Solution 78 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/61mfjeit Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dd129e6b-d8d2-493a-8ed9-ac958ea0b 616.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,7 +1,3 @@
-def check():
-    lst = list(map(int, input("Enter a space-separated integer sequence: ").split()))
-    return double_the_difference(lst)
-
 def double_the_difference(lst):
     odd_sum = sum(i for i in lst if isinstance(i, int) and i % 2 != 0)
     even_sum = sum(i for i in lst if isinstance(i, int))
