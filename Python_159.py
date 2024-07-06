878214c037da32800211a23768e3c1657413c08f
beep boop <githubbot@vadim.me>
2024-07-06 03:22:56 +0200

    Solution 65 of Python/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/a422owsv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2cfa7a66-c7e0-489e-b5d2-9bf007cb6 458.py", line 15, in <module>     check(eat)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2cfa7a66-c7e0-489e-b5d2-9bf007cb6 458.py", line 12, in check     assert eat(4, 5, 1) == [5, 0], "Error"            ^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_159.py
+++ Python_159.py
@@ -1,7 +1,10 @@
+
 def eat(number, need, remaining):
     total = number + need
     if need > remaining:
-        left = need - remaining
-    else:
+        left = 0
+    elif need <= remaining:
         left = max(remaining - need, 0)
+    else:
+        left = need - remaining
     return [total, left]
