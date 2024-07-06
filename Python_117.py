aaef56ee3a88a8294a7d773cc9e454f4edb5df37
beep boop <githubbot@vadim.me>
2024-07-06 04:41:11 +0200

    Solution 15 of Python/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/t5mp7x1v Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/79650a49-2ca6-4314-95ff-954a1dd25 e24.py", line 15, in <module>     check(select_words)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/79650a49-2ca6-4314-95ff-954a1dd25 e24.py", line 7, in check     assert select_words("a b c d e f", 1) == [            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: 2nd edge test error: ['a', 'b', 'c', 'd', 'e', 'f']

--- Python_117.py
+++ Python_117.py
@@ -1,10 +1,3 @@
 
 def select_words(s, n):
     return [word for word in s.split() if len([c for c in word if c.isalpha() and not c.isdigit()]) == n]
-
-
-# Test case 1:
-assert select_words("The quick brown fox jumps over the lazy dog.", 2) == ["quick", "brown", "lazy"]
-
-# Test case 2:
-assert select_words("Python is a popular programming language.", 3) == ["is", "a", "popular", "programming"]
