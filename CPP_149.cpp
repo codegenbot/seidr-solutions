b85d1ae64cb5d85edb0e094194040bac6aaa313f
beep boop <githubbot@vadim.me>
2024-07-06 18:11:22 +0200

    Solution 10 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r1a8ydno Prompt:    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 10d633f3-6107-4f94-bebb-895a7b891351.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 10d633f3-6107-4f94-bebb-895a7b891351.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 10d633f3-6107-4f94-bebb-895a7b891351.cpp:21: 10d633f3-6107-4f94-bebb-895a7b891351.cpp: In function ‘int main()’: 10d633f3-6107-4f94-bebb-895a7b891351.cpp:31:20: error: ‘sorted_list_sum’ was not  declared in this scope    31 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 10d633f3-6107-4f94-bebb-895a7b891351.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,16 +1,37 @@
+```
+vector<bool> issame(vector<string>, int);
+bool issameHelper(string, string);
+
 vector<string> sorted_list_sum(vector<string> lst) {
     vector<string> result;
-    bool issame(int a, int b) {
-        if (a == b) 
-            return true; 
-        else 
-            return false;
-    }
     for (const auto& str : lst) {
-        if (issame(str.length() % 2, 0)) {
+        if (issameHelper(str, "hello")) {
             result.push_back(str);
         }
     }
-    sort(result.begin(), result.end());
+    std::sort(result.begin(), result.end());
     return result;
+}
+
+vector<bool> issame(vector<string> lst, int index) {
+    vector<bool> result(lst.size(), false);
+    for (int i = 0; i < lst.size(); ++i) {
+        if (issameHelper(lst[i], "hello")) {
+            result[i] = true;
+        }
+    }
+    return result;
+}
+
+bool issameHelper(string a, string b) {
+    if (a.length() != b.length()) {
+        return false;
+    } else {
+        for (int i = 0; i < a.length(); ++i) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
