d8f6ff371f7a1e47b9572403ecfaa86390d23c85
beep boop <githubbot@vadim.me>
2024-07-06 07:24:43 +0200

    Solution 62 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/js6e9f34 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/08acf1ff-b38c-4fc4-b7dd-3eacaddb8 725.py", line 33, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/08acf1ff-b38c-4fc4-b7dd-3eacaddb8 725.py", line 30, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -5,15 +5,14 @@ def minPath(grid, k):
         for j in range(n):
             if grid[i][j] == 1:
                 path = [grid[i][j]]
-                dfs(i, j, k, grid, path, res)
+                dfs(i, j, k - 1, grid, path, res)
                 return path + res
     return []
 
 
 def dfs(i, j, k, grid, path, res):
     n = len(grid)
-    if k == 0:
-        res.extend(path)
+    if k < 0:
         return
     for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
         ni, nj = i + x, j + y
