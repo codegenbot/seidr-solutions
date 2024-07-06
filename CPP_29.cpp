bde42a7862a1907732b7360265acc2e48d6bd67f
beep boop <githubbot@vadim.me>
2024-07-06 14:27:13 +0200

    Solution 10 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zr5twv7s Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 3c98d7c0-ac5d-42bd-96f5-4f87183a7cc9.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3c98d7c0-ac5d-42bd-96f5-4f87183a7cc9.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3c98d7c0-ac5d-42bd-96f5-4f87183a7cc9.cpp:10: 3c98d7c0-ac5d-42bd-96f5-4f87183a7cc9.cpp: In function ‘int main()’: 3c98d7c0-ac5d-42bd-96f5-4f87183a7cc9.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 3c98d7c0-ac5d-42bd-96f5-4f87183a7cc9.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,9 +1,17 @@
-bool issame(vector<string> a, vector<string> b){
-    if(a.size() != b.size())
-        return false;
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i])
-            return false;
+vector<string> filter_by_prefix(vector<string> strings, string prefix){
+    vector<string> result;
+    for(auto str : strings){
+        if(str.find(prefix) == 0)
+            result.push_back(str);
     }
-    return true;
-}
+    return result;
+
+    bool issame(vector<string> a, vector<string> b){
+        if(a.size() != b.size())
+            return false;
+        for(int i = 0; i < a.size(); i++){
+            if(a[i] != b[i])
+                return false;
+        }
+        return true;
+    }
