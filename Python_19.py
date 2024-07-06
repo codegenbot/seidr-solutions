47b8eb93c344111ecba3e3831755e5f2e15bab62
beep boop <githubbot@vadim.me>
2024-07-06 06:09:15 +0200

    Solution 48 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4dw7k43c Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9fc24024-96bd-422c-ba44-7315effd4 a83.py", line 23, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9fc24024-96bd-422c-ba44-7315effd4 a83.py", line 18, in check     sort_numbers("six five four three two one zero") AssertionError

--- Python_19.py
+++ Python_19.py
@@ -1,3 +1,4 @@
+```
 def sort_numbers(numbers: str) -> str:
     num_dict = {
         "zero": 0,
@@ -11,5 +12,5 @@ def sort_numbers(numbers: str) -> str:
         "eight": 8,
         "nine": 9
     }
-    nums = list(map(lambda x: num_dict[x] if x in num_dict else int(x), numbers.split()))
-    return " ".join(sorted(map(str, set(nums))))
+    nums = list(map(lambda x: str(num_dict.get(x, x)) if not x.isdigit() else x, numbers.split()))
+    return " ".join(sorted(set(nums)))
