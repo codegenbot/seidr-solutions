20662b941e7baa4c1b87cff2e2da308a85550b6f
beep boop <githubbot@vadim.me>
2024-07-06 02:56:02 +0200

    Solution 4 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lb1rqho5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c2d75b7f-3ddc-4002-927e-50050140e f52.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_144.py
+++ Python_144.py
@@ -1,7 +1,6 @@
-```
 def simplify(x, n):
-    x_num, x_denom = map(int, x.split('/'))
-    n_num, n_denom = map(int, n.split('/'))
+    x_num, x_denom = map(int, x.split("/"))
+    n_num, n_denom = map(int, n.split("/"))
 
     def gcd(a, b):
         while b:
@@ -11,8 +10,11 @@ def simplify(x, n):
     common = gcd(x_denom, n_denom)
     new_x_num = x_num // common
     new_n_num = n_num // common
-    
-    if new_x_num / n_denom == new_n_num / x_denom:
-        return True
+
+    simplified_x = str(new_x_num) + "/" + str(common)
+    simplified_n = str(new_n_num) + "/" + str(common)
+
+    if new_x_num * n_denom == new_n_num * x_denom:
+        return True, (simplified_x, simplified_n)
     else:
-        return False
+        return False, (x, n)
