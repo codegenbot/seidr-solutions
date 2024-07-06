2b1927cf8b84d787cc1e4bcaa38f7d37f005bfeb
beep boop <githubbot@vadim.me>
2024-07-06 15:59:10 +0200

    Solution 28 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/liv8foub Prompt:       |                    ^~~ dfa1e5b5-821b-4c95-82f3-23cbf5783434.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ dfa1e5b5-821b-4c95-82f3-23cbf5783434.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ dfa1e5b5-821b-4c95-82f3-23cbf5783434.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from dfa1e5b5-821b-4c95-82f3-23cbf5783434.cpp:19: dfa1e5b5-821b-4c95-82f3-23cbf5783434.cpp: In function ‘int main()’: dfa1e5b5-821b-4c95-82f3-23cbf5783434.cpp:29:20: error: ‘pluck’ was not declared in this scope    29 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ dfa1e5b5-821b-4c95-82f3-23cbf5783434.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,36 +1,21 @@
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
-}
-
-vector<int> pluck(vector<int> arr) {
-    vector<int> result;
-    if (arr.empty()) return result;
-
-    int smallest = INT_MAX;
-    int index = -1;
+std::vector<std::pair<int, int>> pluck(std::vector<int> arr) {
+    vector<pair<int, int>> evenNodes;
     for (int i = 0; i < arr.size(); i++) {
-        if (arr[i] % 2 == 0 && arr[i] < smallest) {
-            smallest = arr[i];
-            index = i;
+        if (arr[i] % 2 == 0) {
+            evenNodes.push_back({arr[i], i});
         }
     }
-
-    result.push_back(smallest);
-    result.push_back(index);
-
-    return result;
-}
-
-int main() {
-    assert(issame(pluck({7, 9, 7, 1}) , vector<int>{}));
-    return 0;
+    
+    if (evenNodes.empty()) {
+        return {};
+    }
+    
+    sort(evenNodes.begin(), evenNodes.end());
+    return {{evenNodes[0].first, evenNodes[0].second}};
 }
