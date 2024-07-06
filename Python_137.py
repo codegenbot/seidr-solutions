77989b6d830b2461ca567e9fcd4f9951ecdb2ac4
beep boop <githubbot@vadim.me>
2024-07-06 07:43:17 +0200

    Solution 0 of Python/137, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/igj0mcsw Prompt: Complete the following code given the task description and function signature.

---
+++ Python_137.py
@@ -0,0 +1,11 @@
+def compare_one(a, b):
+    if isinstance(a, str):
+        a = float(a.replace(",", "."))
+    if isinstance(b, str):
+        b = float(b.replace(",", "."))
+    if a > b:
+        return a
+    elif b > a:
+        return b
+    else:
+        return None
