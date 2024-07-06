4af830a97822fc2194e83217e8d2308f99d51bee
beep boop <githubbot@vadim.me>
2024-07-06 14:12:26 +0200

    Solution 88 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/8zowiups Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -3,11 +3,6 @@ def add(x: int, y: int):
     while True:
         try:
             x = int(input("Enter first number: "))
-            break
-        except ValueError:
-            pass
-    while True:
-        try:
             y = int(input("Enter second number: "))
             break
         except ValueError:
