34bcc8d66bf87cd5a27f74a0d61ea67cffa7d4c3
beep boop <githubbot@vadim.me>
2024-07-06 19:10:51 +0200

    Solution 98 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r0qsr2nm Prompt:    43 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 35dc800f-46ac-4c09-a674-e7816522c2cb.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ 35dc800f-46ac-4c09-a674-e7816522c2cb.cpp:37:5: note: ‘int main()’ previously def ined here    37 | int main() {       |     ^~~~ In file included from 35dc800f-46ac-4c09-a674-e7816522c2cb.cpp:42: 35dc800f-46ac-4c09-a674-e7816522c2cb.cpp: In function ‘int main()’: 35dc800f-46ac-4c09-a674-e7816522c2cb.cpp:52:20: error: ‘order_by_points’ was not  declared in this scope    52 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 35dc800f-46ac-4c09-a674-e7816522c2cb.cpp:52:19: error: ‘issame’ cannot be used a s a function    52 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,7 +1,16 @@
 #include <algorithm>
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+int get_sum_of_digits(int num) {
+    int sum = 0;
+    while (num != 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+bool issame(std::vector<int> a, std::vector<int>b) {
     if(a.size() != b.size())
         return false;
     for(int i=0; i<a.size(); i++) {
@@ -25,16 +34,7 @@ std::vector<int> order_by_points(std::vector<int> nums) {
 
 }
 
-int get_sum_of_digits(int num) {
-    int sum = 0;
-    while (num != 0) {
-        sum += num % 10;
-        num /= 10;
-    }
-    return sum;
-}
-
 int main() {
-    assert(issame(order_by_points({0,6,6,-76,-21,23,4}), std::vector<int>{{(-76), (-21), (0), (4), (23), (6), (6)})));
+    assert(issame(order_by_points({0,6,6,-76,-21,23,4}), vector<int>({(-76), (-21), (0), (4), (23), (6), (6)})));
     return 0;
 }
