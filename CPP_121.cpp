0e81482a749efdf7f2bf29dcae65a717843da155
beep boop <githubbot@vadim.me>
2024-07-06 11:48:15 +0200

    Solution 83 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/duae0477 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 43b619b6-1141-43ce-a8e4-bd1b6d81ce15.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -9,7 +9,7 @@ int solutions(std::vector<int> lst) {
     } else {
         int result = 0;
         for(int i = 0; i < lst.size(); i++) {
-            for(int j = i + 1; j <= lst.size() - 1; j++) {
+            for(int j = i + 1; j < lst.size(); j++) {
                 if(lst[i] > lst[j]) {
                     result++;
                 }
