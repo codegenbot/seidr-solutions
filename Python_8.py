eae9d21a014bee96b0c828afdcbdf6e7365c3743
beep boop <githubbot@vadim.me>
2024-07-06 05:10:57 +0200

    Solution 30 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ua4y0od9 Prompt: Program did not receive expected input

--- Python_8.py
+++ Python_8.py
@@ -2,6 +2,8 @@ from typing import List, Tuple
 
 
 def sum_product(numbers: List[int]) -> Tuple[int, int]:
-    if not isinstance(numbers, list) or not all(isinstance(num, int) for num in numbers):
+    if not isinstance(numbers, list) or not all(
+        isinstance(num, int) for num in numbers
+    ):
         return (-1, "Invalid input. Please enter a list of integers.")
     return (sum(numbers), 1 if not numbers else eval("*".join(map(str, numbers))))
