332df5bb5ab0cb0eb9f3807c003ee3449ee0f6b8
beep boop <githubbot@vadim.me>
2024-07-06 14:05:29 +0200

    Solution 62 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/t5qd6gs6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d49917fa-e3c7-45ae-9f0e-debd58a54 563.py", line 1     def get_middle(s): IndentationError: unexpected indent

--- middle-character.py
+++ middle-character.py
@@ -2,4 +2,4 @@ def get_middle(s):
     if len(s) % 2 == 0:  # if input is even length
         return s[len(s) // 2 - 1 : len(s) // 2 + 1]
     else:  # if input is odd length
-        return s[len(s) // 2 - 1 : len(s) // 2 + 2]
+        return s[len(s) // 2]
