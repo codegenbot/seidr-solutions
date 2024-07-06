0bf9b89c5a184358d26c68a830a401b86979c906
beep boop <githubbot@vadim.me>
2024-07-06 17:53:34 +0200

    Solution 8 of CPP/116, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3nfju1ag Prompt: 3c33df97-dbc9-44d5-830c-e0ab4e4c90d2.cpp:5:1: note: ‘std::bitset’ is defined in header ‘<bitset>’; did you forget to ‘#include <bitset>’?     4 | #include<algorithm>   +++ |+#include <bitset>     5 | using namespace std; 3c33df97-dbc9-44d5-830c-e0ab4e4c90d2.cpp:10:35: error: request for member ‘count ’ in ‘a’, which is of non-class type ‘int’    10 |              return bitset<32>(a).count() < bitset<32>(b).count() ||       |                                   ^~~~~ 3c33df97-dbc9-44d5-830c-e0ab4e4c90d2.cpp:10:59: error: request for member ‘count ’ in ‘b’, which is of non-class type ‘int’    10 |              return bitset<32>(a).count() < bitset<32>(b).count() ||       |                                                           ^~~~~ 3c33df97-dbc9-44d5-830c-e0ab4e4c90d2.cpp:11:36: error: request for member ‘count ’ in ‘a’, which is of non-class type ‘int’    11 |                     (bitset<32>(a).count() == bitset<32>(b).count() && a  < b);       |                                    ^~~~~ 3c33df97-dbc9-44d5-830c-e0ab4e4c90d2.cpp:11:61: error: request for member ‘count ’ in ‘b’, which is of non-class type ‘int’    11 |                     (bitset<32>(a).count() == bitset<32>(b).count() && a  < b);       |                                                             ^~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,16 +1,11 @@
-#include<stdio.h>
-#include<math.h>
-#include<vector>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
 vector<int> sort_array(vector<int> arr){
-    sort(arr.begin(), arr.end(), 
+    sort(arr.begin(), arr.end(),
          [](int a, int b) {
-             string strA = bitset<32>(a).to_string();
-             string strB = bitset<32>(b).to_string();
-             return strA.size() < strB.size() ||
-                    (strA.size() == strB.size() && a < b);
+             return bitset<32>(a).count() < bitset<32>(b).count() ||
+                    (bitset<32>(a).count() == bitset<32>(b).count() && a < b);
          });
     return arr;
 }
