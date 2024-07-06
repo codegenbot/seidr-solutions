a197e774aeb167d557301d2f83d240153ba21f67
beep boop <githubbot@vadim.me>
2024-07-06 06:28:45 +0200

    Solution 80 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3ci03y9r Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/74871c50-42fa-4e36-8970-3ea7cf2d4 fd6.py", line 17     return result     ^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_1.py
+++ Python_1.py
@@ -2,7 +2,7 @@ def solution(paren_string):
     stack = []
     temp_str = ''
     result = []
-    
+
     for char in paren_string:
         if char == ' ':
             continue
@@ -15,7 +15,8 @@ def solution(paren_string):
             if not stack:
                 result.append(temp_str)
                 temp_str = ''
-    if temp_str:  
-        result.append(temp_str)
+    else:  
+        if temp_str:  
+            result.append(temp_str)
 
     return result
