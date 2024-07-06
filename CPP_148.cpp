639874bea26e223219fb5383c2a391ded84c1d2d
beep boop <githubbot@vadim.me>
2024-07-06 19:29:11 +0200

    Solution 98 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ygswh6dl Prompt: In file included from e0acb49d-f9be-4370-bc37-76d06c724122.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e0acb49d-f9be-4370-bc37-76d06c724122.cpp:18:37: error: ‘string’ was not declared  in this scope    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e0acb49d-f9be-4370-bc37-76d06c724122.cpp:18:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from e0acb49d-f9be-4370-bc37-76d06c724122.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from e0acb49d-f9be-4370-bc37-76d06c724122.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e0acb49d-f9be-4370-bc37-76d06c724122.cpp:18:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,34 +1,7 @@
 using namespace std;
 
-vector<string> bf(string a, string b){
-    vector<string> result;
-    
-    int i = 0, j = 0;
-    
-    while (i < a.size() && j < b.size()) {
-        if (a[i] == b[j]) {
-            result.push_back(a.substr(i,1));
-            i++;
-            j++;
-        } else if (a[i] < b[j]) {
-            result.push_back(a.substr(i,1));
-            i++;
-        } else {
-            j++;
-        }
-    }
-    
-    while (i < a.size()) {
-        result.push_back(a.substr(i,1));
-        i++;
-    }
-    
-    while (j < b.size()) {
-        j++;
-    }
-    
-    return result;
-}
+#include <vector>
+#include <string>
 
 bool issame(vector<string> a, vector<string> b){
     for (int i = 0; i < a.size(); i++) {
@@ -37,4 +10,20 @@ bool issame(vector<string> a, vector<string> b){
         }
     }
     return true;
+}
+
+vector<string> bf(string a, string b) {
+    vector<string> result;
+
+    int i = 0;
+    while(i < a.size() && i < b.size()) {
+        if(a[i] == b[i]) {
+            result.push_back(a.substr(0, i+1));
+            i++;
+        } else {
+            break;
+        }
+    }
+
+    return result;
 }
