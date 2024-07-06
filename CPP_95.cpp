47b278c97c0e7e7864d673938ac94ac3396c852b
beep boop <githubbot@vadim.me>
2024-07-06 20:36:59 +0200

    Solution 40 of CPP/95, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/z0sltap7 Prompt:       |                      ^~~ 64256f84-14f8-4569-a5bf-4df208f48bd1.cpp:1:26: error: ‘string’ was not declared in this scope     1 | bool check_dict_case(map<string, string> dict) {       |                          ^~~~~~ 64256f84-14f8-4569-a5bf-4df208f48bd1.cpp:1:34: error: ‘string’ was not declared in this scope     1 | bool check_dict_case(map<string, string> dict) {       |                                  ^~~~~~ 64256f84-14f8-4569-a5bf-4df208f48bd1.cpp:1:42: error: ‘dict’ was not declared in  this scope     1 | bool check_dict_case(map<string, string> dict) {       |                                          ^~~~ 64256f84-14f8-4569-a5bf-4df208f48bd1.cpp:1:46: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool check_dict_case(map<string, string> dict) {       |                                              ^ In file included from 64256f84-14f8-4569-a5bf-4df208f48bd1.cpp:16: 64256f84-14f8-4569-a5bf-4df208f48bd1.cpp: In function ‘int main()’: 64256f84-14f8-4569-a5bf-4df208f48bd1.cpp:18:28: error: ‘check_dict_case’ cannot be used as a function    18 |     assert (check_dict_case({}) == false);       |             ~~~~~~~~~~~~~~~^~~~

--- CPP_95.cpp
+++ CPP_95.cpp
@@ -11,10 +11,8 @@ bool check_dict_case(map<string, string> dict) {
     for (auto it = dict.begin(); it != dict.end(); ++it) {
         string key = it->first;
         if (!allLower && !allUpper) break;
-        if (!allLower) {
-            allLower = tolower(key[0]) == tolower(key.substr(1));
-            allUpper = toupper(key[0]) == toupper(key.substr(1));
-        }
+        if (!allLower) allLower = allUpper = islower(key[0]);
+        else allUpper = isupper(key[0]);
     }
 
     return allLower || allUpper;
