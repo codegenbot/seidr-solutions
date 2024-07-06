97f66263e0638a53f630841211c70b1d679f03c7
beep boop <githubbot@vadim.me>
2024-07-06 12:39:43 +0200

    Solution 74 of CPP/23, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9xak4ijc Prompt: 4edd2bda-0e6d-4b56-8f2e-28f2abae9379.cpp:14:5: error: redefinition of ‘int main( )’    14 | int main(){       |     ^~~~ 4edd2bda-0e6d-4b56-8f2e-28f2abae9379.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~ In file included from 4edd2bda-0e6d-4b56-8f2e-28f2abae9379.cpp:13: 4edd2bda-0e6d-4b56-8f2e-28f2abae9379.cpp: In function ‘int main()’: 4edd2bda-0e6d-4b56-8f2e-28f2abae9379.cpp:15:13: error: ‘strlen’ was not declared  in this scope    15 |     assert (strlen("asdasnakj") == 9);       |             ^~~~~~ 4edd2bda-0e6d-4b56-8f2e-28f2abae9379.cpp:14:1: note: ‘strlen’ is defined in head er ‘<cstring>’; did you forget to ‘#include <cstring>’?    13 | #include<assert.h>   +++ |+#include <cstring>    14 | int main(){

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,3 +1,12 @@
-int strlen(string str){
-    return str.length();
+#include <string>
+#include <cassert>
+#include <cstring>
+
+int str_len(const std::string& str){
+    return strlen(str.c_str());
+}
+
+int otherMain(int) {
+    assert(str_len("asdasnakj") == 12); 
+    return 0;
 }
