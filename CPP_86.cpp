caa60584cff3a82c46d875daefc8c3917c9aa078
beep boop <githubbot@vadim.me>
2024-07-06 15:22:28 +0200

    Solution 12 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gdej7gky Prompt: b2506532-88a0-465f-955e-def092631cda.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string anti_shuffle(string s) {       | ^~~~~~ In file included from b2506532-88a0-465f-955e-def092631cda.cpp:25: b2506532-88a0-465f-955e-def092631cda.cpp: In function ‘int main()’: b2506532-88a0-465f-955e-def092631cda.cpp:27:13: error: ‘anti_shuffle’ was not de clared in this scope    27 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,19 +1,24 @@
+#include <string>
+
 string anti_shuffle(string s) {
     string result = "";
-    for (int i = 0; i < s.length(); i++) {
+    int i = 0;
+    while (i < s.length()) {
         if (s[i] == ' ') {
-            result += " ";
+            result += ' ';
+            i++;
         } else {
-            string word;
+            string word = "";
             while (i < s.length() && s[i] != ' ') {
                 word += s[i];
                 i++;
             }
-            sort(word.begin(), word.end());
-            result += word;
+            for (char c : word) {
+                result += c;
+            }
+            if (i < s.length()) {
+                result += ' ';
+            }
         }
-        // Fix: Increment i after processing each word
-        i--;
     }
     return result;
-}
