54d518aa875744d6ea71856690e67b4e0338304b
beep boop <githubbot@vadim.me>
2024-07-06 07:13:31 +0200

    Solution 12 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qai2um6x Prompt: 4b1.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/20d9a89f-ccea-407d-9dfd-5bc01dae7 4b1.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/20d9a89f-ccea-407d-9dfd-5bc01dae7 4b1.py", line 2, in sort_even     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/20d9a89f-ccea-407d-9dfd-5bc01dae7 4b1.py", line 3, in <listcomp>     x if i % 2 != 0 else sorted([y for y in l if (i // 2) * 2 == j])[0]                                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/20d9a89f-ccea-407d-9dfd-5bc01dae7 4b1.py", line 3, in <listcomp>     x if i % 2 != 0 else sorted([y for y in l if (i // 2) * 2 == j])[0]                                                                  ^ NameError: name 'j' is not defined

--- Python_37.py
+++ Python_37.py
@@ -1,2 +1,5 @@
 def sort_even(l: list):
-    return [sorted([x for i, x in enumerate(l) if i % 2 == 0])]
+    return [
+        x if i % 2 != 0 else sorted([y for y in l if j == i//2*2])[0]
+        for i, (x, j) in enumerate(zip(l, range(len(l))))
+    ]
