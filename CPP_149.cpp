a6d4d2edb4365740474b1527a50cd1bce1fd91ae
beep boop <githubbot@vadim.me>
2024-07-06 18:46:26 +0200

    Solution 53 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r1a8ydno Prompt:    13 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 214660de-3308-48c3-88f5-4849d097d512.cpp:13:44: error: ‘b’ was not declared in t his scope    13 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 214660de-3308-48c3-88f5-4849d097d512.cpp:13:45: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 214660de-3308-48c3-88f5-4849d097d512.cpp:12: 214660de-3308-48c3-88f5-4849d097d512.cpp: In function ‘int main()’: 214660de-3308-48c3-88f5-4849d097d512.cpp:22:20: error: ‘sorted_list_sum’ was not  declared in this scope    22 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 214660de-3308-48c3-88f5-4849d097d512.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,10 +1,26 @@
-bool issame(const string &a, const string &b) {
-    if (a.length() != b.length())
+bool issame(string a, string b) {
+    if(a.length() != b.length())
         return false;
-    int aVal = 0, bVal = 0;
-    for (int i = 0; i < a.length(); i++) {
-        aVal += (int)(a[i] - '0');
-        bVal += (int)(b[i] - '0');
+    for(int i=0; i<a.length(); i++)
+        if(a[i] != b[i])
+            return false;
+    return true;
+}
+
+int main() {
+    vector<string> lst = {"A1", "B3", "C5", "D4"};
+    vector<string> result = sorted_list_sum(lst);
+    for (const auto& str : result) {
+        bool found = false;
+        for(const auto& str2:lst){
+            if(issame(str, str2)) {
+                cout << str << " is same as " << str2 << endl;
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            cout << str << " is not present in the list" << endl;
     }
-    return aVal == bVal;
+    return 0;
 }
