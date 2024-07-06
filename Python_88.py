7104c61885ec26443ab5b32027176bc543d927c6
beep boop <githubbot@vadim.me>
2024-07-06 17:58:10 +0200

    Solution 5 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vtonw1kq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c6d27d53-9b53-499e-8e64-503e34f46 157.py", line 16, in <module>     check(sort_array)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c6d27d53-9b53-499e-8e64-503e34f46 157.py", line 13, in check     assert sort_array([21, 14, 23, 11]) == [23, 21, 14, 11], "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_88.py
+++ Python_88.py
@@ -1,9 +1,3 @@
 ```
 def sort_array(array):
-    if len(array) == 0:
-        return array
-    total = sum(set(array)) # convert to set to remove duplicates and calculate the total
-    if total % 2 != 0:
-        return sorted(array)
-    else:
-        return sorted(array, reverse=True)
+    return sorted(array)
