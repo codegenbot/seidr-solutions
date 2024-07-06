1e4aa3788556f44b5c36f9faeae447eee1ad0bf4
beep boop <githubbot@vadim.me>
2024-07-06 13:24:28 +0200

    Solution 79 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6h0099y8 Prompt: In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from cc9615a3-1746-46c1-b3da-4d72fb4d82a7.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ cc9615a3-1746-46c1-b3da-4d72fb4d82a7.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cc9615a3-1746-46c1-b3da-4d72fb4d82a7.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from cc9615a3-1746-46c1-b3da-4d72fb4d82a7.cpp:20: cc9615a3-1746-46c1-b3da-4d72fb4d82a7.cpp: In function ‘int main()’: cc9615a3-1746-46c1-b3da-4d72fb4d82a7.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -2,18 +2,23 @@
 #include <vector>
 #include <string>
 
+bool isSame(const std::vector<int>& a, const std::vector<int>& b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i)
+        if (a[i] != b[i]) return false;
+    return true;
+}
+
 std::vector<int> order_by_points(std::vector<int> nums) {
     auto compare = [&](int a, int b) {
-        if (a == b) return 0;
         long sumA = 0, sumB = 0;
         for (char c : std::to_string(a)) sumA += c - '0';
         for (char c : std::to_string(b)) sumB += c - '0';
         if (sumA != sumB) return sumA - sumB > 0 ? 1 : -1;
-        int aIndex = distance(nums.begin(), find(nums.begin(), nums.end(), a));
-        int bIndex = distance(nums.begin(), find(nums.begin(), nums.end(), b));
-        return aIndex - bIndex;
+        return distance(nums.begin(), find(nums.begin(), nums.end(), a)) -
+               distance(nums.begin(), find(nums.begin(), nums.end(), b));
     };
     std::vector<int> res = nums;
-    sort(res.begin(), res.end(), compare);
+    sort(res.begin(), res.end(), [&compare](int a, int b) { return compare(a, b) > 0; });
     return res;
 }
