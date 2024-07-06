52160cad536a14b4f86cc3b1de58962d7078d6a8
beep boop <githubbot@vadim.me>
2024-07-06 19:19:51 +0200

    Solution 84 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1kalf9to Prompt:    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ e78be36a-5a17-41df-903d-022da9ceba25.cpp:21:10: error: ‘int main()’ redeclared a s different kind of entity    21 | int main(){       |          ^ e78be36a-5a17-41df-903d-022da9ceba25.cpp:1:5: note: previous declaration ‘int ma in’     1 | int main {       |     ^~~~ In file included from e78be36a-5a17-41df-903d-022da9ceba25.cpp:12: e78be36a-5a17-41df-903d-022da9ceba25.cpp: In function ‘int main()’: e78be36a-5a17-41df-903d-022da9ceba25.cpp:22:20: error: ‘minPath’ was not declare d in this scope    22 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ e78be36a-5a17-41df-903d-022da9ceba25.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
 #include <vector>
-#include <climits>
-using namespace std;
 
-vector<int> minPath(vector<vector<int>> grid, int k) {
-    vector<int> res(k);
-    for (int i = 0; i < k; ++i) {
-        int minVal = INT_MAX;
-        int minRow = -1, minCol = -1;
-        for (int j = 0; j < grid.size(); ++j) {
-            for (int col = 0; col < grid[j].size(); ++col) {
-                if (grid[j][col] <= minVal && res.empty() || (res.back() != grid[j][col])) {
-                    minVal = grid[j][col];
-                    minRow = j;
-                    minCol = col;
-                }
+// Function definition
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++)
+        if (a[i] != b[i])
+            return false;
+    return true;
+}
+
+vector<int> minPath(vector<vector<int>>& grid, int k) {
+    vector<int> result;
+    int n = grid.size();
+    int m = grid[0].size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (grid[i][j] == k) {
+                if (i - 1 >= 0 && j - 1 >= 0 && 
+                    i + 1 < n && j + 1 < m &&
+                    grid[i-1][j] == k && grid[i+1][j] == k && 
+                    grid[i][j-1] == k && grid[i][j+1] == k) {
+                        result.push_back(k);
+                    }
             }
         }
-        res[i] = minVal;
-        for (int i = 0; i < grid.size(); ++i) {
-            if (i == minRow) continue;
-            grid[i][minCol] = INT_MAX;
-        }
     }
-    return res;
+
+    return result;
 }
 
 int main() {
-    vector<vector<int>> grid = {{1,2,3}, {4,5,6}, {7,8,9}};
+    vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     int k = 3;
     vector<int> result = minPath(grid, k);
     for (auto val : result) {
         cout << val << " ";
     }
     cout << endl;
+    return 0;
 }
