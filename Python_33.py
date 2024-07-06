df6325b62046ce26813ba65d9ca482db330db625
beep boop <githubbot@vadim.me>
2024-07-06 17:43:03 +0200

    Solution 94 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r6wjanum Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7eb858c4-629d-488c-8198-dc70a1127 74e.py", line 14, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7eb858c4-629d-488c-8198-dc70a1127 74e.py", line 9, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,7 +1,11 @@
+```
 def sort_third(input_list):
     result = []
     for i in range(0, len(input_list), 3):
-        current_group = tuple(sorted([int(x) for x in input_list[i:i+3]]))
+        if i + 2 < len(input_list):
+            current_group = tuple(sorted([int(x) for x in input_list[i:i+3]]))
+        else:
+            current_group = tuple(sorted([int(x) for x in input_list[i:]]))
         result.append(current_group)
 
-    return tuple(sorted(map(tuple, result)))
+    return tuple(sorted(result))
