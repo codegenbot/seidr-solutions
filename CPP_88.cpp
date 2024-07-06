a0ac9151980f02ae7ef47211823f00efeb910d49
beep boop <githubbot@vadim.me>
2024-07-06 17:06:35 +0200

    Solution 80 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/e0wi7av0 Prompt:       |           ^~~~~~ In file included from 8344d6fe-9e4f-4deb-9fc6-b0da322a104f.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8344d6fe-9e4f-4deb-9fc6-b0da322a104f.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8344d6fe-9e4f-4deb-9fc6-b0da322a104f.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8344d6fe-9e4f-4deb-9fc6-b0da322a104f.cpp:15: 8344d6fe-9e4f-4deb-9fc6-b0da322a104f.cpp: In function ‘int main()’: 8344d6fe-9e4f-4deb-9fc6-b0da322a104f.cpp:25:20: error: ‘sort_array’ was not decl ared in this scope    25 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 8344d6fe-9e4f-4deb-9fc6-b0da322a104f.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,22 +1,16 @@
-#include <vector>
-#include <algorithm>
-
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); ++i)
-        if (a[i] != b[b.size()-1-i]) return false;
-    return true;
+```cpp
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
-std::vector<int> sort_array(std::vector<int> array) {
-    int sum = std::accumulate(array.begin(), array.end(), 0);
-    if (sum % 2 == 1)
+vector<int> sort_array(vector<int> array) {
+    int sum = 0;
+    for (int num : array) {
+        sum += num;
+    }
+    if (sum % 2 != 0)
         return array;
     else
-        std::sort(array.rbegin(), array.rend());
+        std::sort(array.begin(), array.end());
     return array;
-}
-
-int main() {
-    assert(issame(sort_array({21, 14, 23, 11}), {23, 21, 14, 11}));
 }
