54cbcc80e644d23129c2663f0e49e3dab22e4fc5
beep boop <githubbot@vadim.me>
2024-07-06 02:33:20 +0200

    Solution 1 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/c3whz2ld Prompt: Complete the following code given the task description and function signature.

--- Python_9.py
+++ Python_9.py
@@ -1,6 +1,6 @@
 def rolling_max(numbers: List[int]) -> List[int]:
     result = []
-    current_max = float("-inf")
+    current_max = -1
     for num in numbers:
         if num > current_max:
             current_max = num
