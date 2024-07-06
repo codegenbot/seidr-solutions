ecc1dfd8856d34faba101565985218e5697227d2
beep boop <githubbot@vadim.me>
2024-07-06 11:22:02 +0200

    Solution 80 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/nh0o2no6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0032b6b6-c301-4344-90a4-2a2a6aaa3 c7c.py", line 14, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0032b6b6-c301-4344-90a4-2a2a6aaa3 c7c.py", line 11, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,6 +1,10 @@
 def make_palindrome(string):
+    if not string:
+        return "No palindrome"
+    
     rev = string[::-1]
+    
     if rev == string:
         return string + " is a palindrome"
     else:
-        return string + "r" + string[::-1] + " is not a palindrome"
+        return string + " is not a palindrome"
