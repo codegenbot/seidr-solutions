892d7bde59d29e351a57ed6f91251d0dbf50b0d1
beep boop <githubbot@vadim.me>
2024-07-06 04:15:17 +0200

    Solution 25 of Python/75, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/bl5woook Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eecd8a97-a330-44a5-9bf4-b87aad30c d56.py", line 12, in <module>     check(is_multiply_prime)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eecd8a97-a330-44a5-9bf4-b87aad30c d56.py", line 9, in check     assert is_multiply_prime(11 * 13 * 7) == True            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eecd8a97-a330-44a5-9bf4-b87aad30c d56.py", line 5, in is_multiply_prime     if a % i == 0 and is_prime(i) and is_prime(a // i):                       ^^^^^^^^ NameError: name 'is_prime' is not defined

--- Python_75.py
+++ Python_75.py
@@ -1,14 +1,15 @@
 def is_prime(n):
-if n <= 1 or n % 2 == 0 and n != 2:
-return False
-for i in range(3, int(n ** 0.5) + 1, 2):
-if n % i == 0:
-return False
-return True
+    if n <= 1 or n % 2 == 0 and n != 2:
+        return False
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
 def is_multiply_prime(a):
-if a < 1:
-return False
-for i in range(2, int(a**0.5) + 1):
-if a % i == 0 and is_prime(i) and is_prime(a // i):
-return True
-return False
+    if a < 1:
+        return False
+    for i in range(2, int(a**0.5) + 1):
+        if a % i == 0 and is_prime(i) and is_prime(a // i):
+            return True
+    return False
