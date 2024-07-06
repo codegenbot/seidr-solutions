a1a3bc285f30ad9151f9cbdda55982a850bcf3ac
beep boop <githubbot@vadim.me>
2024-07-06 22:19:00 +0200

    Solution 20 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/act4tpx3 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e31b406b-3915-4be7-8b80-c4bd6e182 d8e.py", line 13, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e31b406b-3915-4be7-8b80-c4bd6e182 d8e.py", line 10, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e31b406b-3915-4be7-8b80-c4bd6e182 d8e.py", line 3, in fruit_distribution     n_apples = int(                ^^^^ ValueError: invalid literal for int() with base 10: ''

--- Python_67.py
+++ Python_67.py
@@ -1,6 +1,7 @@
 def fruit_distribution(s, n):
     words = s.split()
-    n_apples = int(
-        "".join(filter(str.isdigit, [word for word in words if "apples" in word])) or '0'
-    )
-    return max(0, n - n_apples)
+    n_apples = 0
+    for word in words:
+        if "apples" in word:
+            n_apples += int(''.join(filter(str.isdigit, [word for word in words if "apples" in word])) or 0)
+    return n - n_apples
