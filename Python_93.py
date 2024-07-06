f195b8ec931866a974e8f80a0e3a3d817fbf5f16
beep boop <githubbot@vadim.me>
2024-07-06 04:13:03 +0200

    Solution 16 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/qwgfrn7h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b5556224-36e6-4b23-a0d0-ddd2e32f6 fb6.py", line 25, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b5556224-36e6-4b23-a0d0-ddd2e32f6 fb6.py", line 21, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,9 +1,12 @@
 def encode(message):
+    vowels = ["a", "e", "i", "o", "u"]
     encoded_message = ""
     for char in message:
-        if char.isalpha():
-            new_char = chr((ord(char) + 2) % 26)
+        if char.lower() in vowels:
+            new_char = chr(ord(char) + 2)
+            if ord(char) >= 65 and ord(char) <= 90:
+                new_char = new_char.upper()
             encoded_message += new_char
         else:
-            encoded_message += char
+            encoded_message += char.swapcase()
     return encoded_message
