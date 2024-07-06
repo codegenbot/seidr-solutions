c53b8efd7d866d776306278ce1d471e7bc9e3e66
beep boop <githubbot@vadim.me>
2024-07-06 03:45:41 +0200

    Solution 46 of Python/46, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/r2pk0pul Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4ddd7316-ce0c-47a6-b48a-278fecdf9 da1.py", line 16, in <module>     check(fib4)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4ddd7316-ce0c-47a6-b48a-278fecdf9 da1.py", line 13, in check     assert fib4(12) == 386            ^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4ddd7316-ce0c-47a6-b48a-278fecdf9 da1.py", line 10, in fib4     sequence.append(sequence[i - 1] + sequence[i - 2] + sequence[i - 3])                     ~~~~~~~~^^^^^^^ IndexError: list index out of range

--- Python_46.py
+++ Python_46.py
@@ -1,6 +1,6 @@
 def fib4(n):
     sequence = [0] * (n + 1)
-    for i in range(5, n + 1):
+    for i in range(3, n + 1):
         if i <= n - 3:
-            sequence[i - 1] += sequence[i - 2] + sequence[i - 3]
+            sequence[i] = sequence[i - 1] + sequence[i - 2] + sequence[i - 3]
     return sequence[n]
