69180d102bafaf4b19fe1a7ad6ab368660759d00
beep boop <githubbot@vadim.me>
2024-07-06 06:37:10 +0200

    Solution 0 of Python/104, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/37pbcw15 Prompt: Complete the following code given the task description and function signature.

---
+++ Python_104.py
@@ -0,0 +1,12 @@
+def unique_digits(x):
+    return sorted(
+        [
+            i
+            for i in x
+            if "0" not in str(i)
+            or "2" not in str(i)
+            or "4" not in str(i)
+            or "6" not in str(i)
+            or "8" not in str(i)
+        ]
+    )
