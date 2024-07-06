0a90a1e869f5dc73d501c8dc07aede514ae4cda6
beep boop <githubbot@vadim.me>
2024-07-06 16:52:51 +0200

    Solution 90 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/hjcrazap Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_map.h:62,                  from /usr/include/c++/11/map:61,                  from 5d7c5d46-41c0-482c-8e38-afe47a0b5e90.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,9 +1,23 @@
-#include <unordered_map>
+#include <map>
+#include <string>
 
 std::string int_to_mini_roman(int number) {
     std::string result;
-    std::map<int, std::string> roman_map = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"},
-                                             {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+    std::map<int, std::string> roman_map;
+    roman_map[1000] = "M";
+    roman_map[900] = "CM";
+    roman_map[500] = "D";
+    roman_map[400] = "CD";
+    roman_map[100] = "C";
+    roman_map[90] = "XC";
+    roman_map[50] = "L";
+    roman_map[40] = "XL";
+    roman_map[10] = "X";
+    roman_map[9] = "IX";
+    roman_map[5] = "V";
+    roman_map[4] = "IV";
+    roman_map[1] = "I";
+
     for (const auto& pair : roman_map) {
         while (number >= pair.first) {
             number -= pair.first;
