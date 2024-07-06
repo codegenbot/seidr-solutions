dc3a704911430550143e53a810e309f45ff69436
beep boop <githubbot@vadim.me>
2024-07-06 04:06:25 +0200

    Solution 23 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/kh7c2g1m Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7114df87-e567-4c19-bbe5-117426846 17e.py", line 20, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7114df87-e567-4c19-bbe5-117426846 17e.py", line 17, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7114df87-e567-4c19-bbe5-117426846 17e.py", line 7, in fruit_distribution     apples = int(apples)              ^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: '1 apples '

--- Python_67.py
+++ Python_67.py
@@ -1,12 +1,6 @@
 def fruit_distribution(s, n):
-    # Split the string into apples and oranges
-    apples, oranges = s.split("and")
-
-    # Convert the strings to integers
+    apples, oranges = s.split(" and ")
     apples = int(apples)
     oranges = int(oranges)
-
-    # Calculate the number of mangoes
     mangoes = n - apples - oranges
-
     return mangoes
