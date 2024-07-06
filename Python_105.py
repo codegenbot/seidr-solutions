73e82cf702af7a3c4861b59c7b598c9f529fa525
beep boop <githubbot@vadim.me>
2024-07-06 02:35:03 +0200

    Solution 84 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/tn9pfa00 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/08d1fe1f-559d-41f3-a9ec-942a70a18 936.py", line 11, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/08d1fe1f-559d-41f3-a9ec-942a70a18 936.py", line 8, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_105.py
+++ Python_105.py
@@ -1,4 +1,5 @@
+
 def by_length(arr):
     if not isinstance(arr, (list, tuple)):
         raise TypeError("by_length() only accepts lists or tuples")
-    return map(str, sorted(arr, key=len))
+    return [str(x) for x in sorted(map(str, arr), key=lambda x: len(x))]
