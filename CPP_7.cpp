8df6b6c912ad3beb00576b95db2b2afcc46fd4ae
beep boop <githubbot@vadim.me>
2024-07-06 10:28:41 +0200

    Solution 32 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q0l1s9e1 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ff1051f3-6bf9-40ab-9373-cb8c0f6fe4d2.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ff1051f3-6bf9-40ab-9373-cb8c0f6fe4d2.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ff1051f3-6bf9-40ab-9373-cb8c0f6fe4d2.cpp:10: ff1051f3-6bf9-40ab-9373-cb8c0f6fe4d2.cpp: In function ‘int main()’: ff1051f3-6bf9-40ab-9373-cb8c0f6fe4d2.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ ff1051f3-6bf9-40ab-9373-cb8c0f6fe4d2.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,10 +1,23 @@
-#include <vector>
-#include <string>
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
+}
 
-bool issame(vector<string> a,vector<string> b){
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i<a.size(); i++){
-        if(a[i] != b[i]) return false;
+vector<vector<string>> filter_by_substring(vector<vector<string>> strings, string substring) {
+    vector<vector<string>> result;
+    for(auto &s : strings){
+        bool same = false;
+        for(string substr: s){
+            if(filter_by_substring(s, substr).size() > 1)
+                same = true;
+        }
+        if(!same) {
+            vector<string> temp;
+            for (string str : s) {
+                if(str.find(substring) != string::npos)
+                    temp.push_back(str);
+            }
+            result.push_back(temp);
+        }
     }
-    return true;
+    return result;
 }
