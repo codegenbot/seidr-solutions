0ee7b76b36ed02085bb05a88e088e27d0b7d3f81
beep boop <githubbot@vadim.me>
2024-07-06 15:21:27 +0200

    Solution 4 of CPP/43, ratio 0.7777777777777778 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/thumjzx0 Prompt: 9a54161f-e793-43a0-bf92-51cf1d699eaf.cpp: In function ‘bool pairs_sum_to_zero(st d::vector<int>)’: 9a54161f-e793-43a0-bf92-51cf1d699eaf.cpp:5:5: error: ‘set’ was not declared in t his scope     5 |     set<int> s(l.begin(), l.end());       |     ^~~ 9a54161f-e793-43a0-bf92-51cf1d699eaf.cpp:2:1: note: ‘std::set’ is defined in hea der ‘<set>’; did you forget to ‘#include <set>’?     1 | #include <vector>   +++ |+#include <set>     2 | using namespace std; 9a54161f-e793-43a0-bf92-51cf1d699eaf.cpp:5:9: error: expected primary-expression  before ‘int’     5 |     set<int> s(l.begin(), l.end());       |         ^~~ 9a54161f-e793-43a0-bf92-51cf1d699eaf.cpp:6:18: error: ‘s’ was not declared in th is scope     6 |     for (int i : s) {       |                  ^ 9a54161f-e793-43a0-bf92-51cf1d699eaf.cpp:7:13: error: ‘find’ was not declared in  this scope     7 |         if (find(s.begin(), s.end(), -i) != s.end()) {       |             ^~~~

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,12 +1,13 @@
 #include <vector>
+#include <set>
 #include <algorithm>
 
 using namespace std;
 
 bool pairs_sum_to_zero(vector<int> l) {
     set<int> s(l.begin(), l.end());
-    for (int x : s) {
-        if (find(s.begin(), s.end(), -x) != s.end()) {
+    for (int i : s) {
+        if (find(s.begin(), s.end(), -i) != s.end()) {
             return true;
         }
     }
