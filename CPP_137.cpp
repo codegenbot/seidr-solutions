ff98dad1e4b4ddeb38683e8a93a8d010c4e79b15
beep boop <githubbot@vadim.me>
2024-07-06 11:28:56 +0200

    Solution 34 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4htq3f01 Prompt: e");       |             ^~~~~ 746913b2-2a24-4a40-92e8-4521bfdcd8c3.cpp:52:29: error: ‘string’ was not declared  in this scope    52 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 746913b2-2a24-4a40-92e8-4521bfdcd8c3.cpp:52:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 746913b2-2a24-4a40-92e8-4521bfdcd8c3.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 746913b2-2a24-4a40-92e8-4521bfdcd8c3.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from 746913b2-2a24-4a40-92e8-4521bfdcd8c3.cpp:50: 746913b2-2a24-4a40-92e8-4521bfdcd8c3.cpp:52:37: error: ‘compare_one’ was not dec lared in this scope; did you mean ‘compare’?    52 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,42 +1,28 @@
-#include <variant>
-#include <string>
-
-std::variant<int, float, std::string> compare_one(std::variant<int, float, std::string> a, std::variant<int, float, std::string> b) {
-    if (a.index() == 0 && b.index() == 1) { 
-        int ai = std::get<int>(a);
-        float bf = std::get<float>(b);
-        return (ai > bf) ? a : (bf > ai) ? b : a;
+```
+int main() {
+    std::cout << "Enter two values: ";
+    int val1;
+    float val2;
+    std::cin >> val1;
+    
+    if (std::cin.fail()) {
+        a = val1;
+    } else {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::string str;
+        std::cin >> str;
+        a = val1;  // Assuming the int value should go into 'a'
+        b = str;
     }
-    else if (a.index() == 0 && b.index() == 2) {
-        int ai = std::get<int>(a);
-        std::string bs = std::get<std::string>(b);
-        return (ai > stoi(bs)) ? a : (stoi(bs) > ai) ? b : a;
+    
+    if (std::any_cast<int>(a) > std::any_cast<int>(b)) {
+        std::cout << "The first number is bigger.\n";
+    } else if (std::any_cast<int>(a) < std::any_cast<int>(b)) {
+        std::cout << "The second number is bigger.\n";
+    } else {
+        std::cout << "Both numbers are equal.\n";
     }
-    else if (a.index() == 1 && b.index() == 0) {
-        float af = std::get<float>(a);
-        int bi = std::get<int>(b);
-        return (af > bi) ? a : (bi > af) ? b : a;
-    }
-    else if (a.index() == 1 && b.index() == 2) {
-        float af = std::get<float>(a);
-        std::string bs = std::get<std::string>(b);
-        return (af > stod(bs)) ? a : (stod(bs) > af) ? b : a;
-    }
-    else if (a.index() == 2 && b.index() == 0) {
-        std::string as = std::get<std::string>(a);
-        int bi = std::get<int>(b);
-        return (stod(as) > bi) ? a : (bi > stod(as)) ? b : a;
-    }
-    else if (a.index() == 2 && b.index() == 1) {
-        std::string as = std::get<std::string>(a);
-        float bf = std::get<float>(b);
-        return (stod(as) > bf) ? a : (bf > stod(as)) ? b : a;
-    }
-    else if (a.index() == 2 && b.index() == 2) {
-        std::string as = std::get<std::string>(a);
-        std::string bs = std::get<std::string>(b);
-        return (stod(as) > stod(bs)) ? a : (stod(bs) > stod(as)) ? b : a;
-    }
-    else
-        return a;
+    
+    return 0;
 }
