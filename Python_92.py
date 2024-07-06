e807bedd8031174d35244b318b6507b11fd444d8
beep boop <githubbot@vadim.me>
2024-07-06 04:17:57 +0200

    Solution 18 of Python/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/5avdcy3h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e028647-e938-4ad6-90b1-40ed62594 49c.py", line 14, in <module>     check(any_int)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e028647-e938-4ad6-90b1-40ed62594 49c.py", line 10, in check     any_int(3.0, 4, 7) == False AssertionError: This prints if this assert fails 10 (also good for debugging!)

--- Python_92.py
+++ Python_92.py
@@ -1,9 +1,5 @@
-def any_int(x, y, z):
+def any_int(x: int, y: int, z: int):
     if x == y + z or y == x + z or z == x + y:
         return True
     else:
         return False
-
-
-# Change float argument to int before passing it to the function
-any_int(3.0, 4, 7) == False
