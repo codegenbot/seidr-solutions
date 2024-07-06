58bd3e78db47e48688fcd84ce653d1c234116564
beep boop <githubbot@vadim.me>
2024-07-06 22:42:26 +0200

    Solution 7 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mck1y7be Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,30 +1,99 @@
-#include <string>
-#include <boost/any.hpp>
+Here is the completed code:
 
-using namespace std;
+```cpp
+#include <boost/any.hpp>
+#include <string>
+#include <algorithm>
+
+using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return max(a.convert_to<double>(), b.convert_to<double>());
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return (int)a > (float)b ? a : b;
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return (int)a > (double)b ? a : b;
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return (float)a > (int)b ? a : b;
     }
     else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return max(a, boost::any(b.convert_to<int>()));
+        return (double)a > (int)b ? a : b;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        string strB = boost::any_cast<string>(b).replace(",", "");
+        if (stod(strB) < (float)a.convert_to<double>()) {
+            return a;
+        } 
+        else if (stod(strB) > (float)a.convert_to<double>()) {
+            return b;
+        }
+        else {
+            return any("None");
+        }
     }
     else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        return (boost::any_cast<string>(a) != "None" ? a : b);
-    }
-    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        return (boost::any_cast<string>(b) != "None" ? b : a);
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return (boost::any_cast<string>(a) > boost::any_cast<string>(b) ? a : (boost::any_cast<string>(a) < boost::any_cast<string>(b) ? b : "None"));
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        return (boost::any_cast<string>(b) != "None" ? b : a);
+        string strB = boost::any_cast<string>(b).replace(",", "");
+        if (stod(strB) < (double)a.convert_to<double>()) {
+            return a;
+        } 
+        else if (stod(strB) > (double)a.convert_to<double>()) {
+            return b;
+        }
+        else {
+            return any("None");
+        }
     }
     else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        return (boost::any_cast<string>(a) != "None" ? a : boost::any(b));
+        string strA = boost::any_cast<string>(a).replace(",", "");
+        string strB = boost::any_cast<string>(b);
+        if (stod(strA) < (int)b.convert_to<int>()) {
+            return a;
+        } 
+        else if (stod(strA) > (int)b.convert_to<int>()) {
+            return b;
+        }
+        else {
+            return any("None");
+        }
     }
-
-    return "None";
+    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        string strB = boost::any_cast<string>(b).replace(",", "");
+        if ((int)a < stod(strB)) {
+            return b;
+        } 
+        else if ((int)a > stod(strB)) {
+            return a;
+        }
+        else {
+            return any("None");
+        }
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        string strB = boost::any_cast<string>(b).replace(",", "");
+        if ((float)a < stod(strB)) {
+            return b;
+        } 
+        else if ((float)a > stod(strB)) {
+            return a;
+        }
+        else {
+            return any("None");
+        }
+    }
+    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
+        string strB = boost::any_cast<string>(b).replace(",", "");
+        if ((double)a < stod(strB)) {
+            return b;
+        } 
+        else if ((double)a > stod(strB)) {
+            return a;
+        }
+        else {
+            return any("None");
+        }
+    }
+    else {
+        return any("None");
+    }
 }
