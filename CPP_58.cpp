5f797517769c984622203eab6c9d9fac06d7b21f
beep boop <githubbot@vadim.me>
2024-07-06 14:02:14 +0200

    Solution 20 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/g0smtz44 Prompt: 5489c8e9-c760-49d1-8f97-0de757c41437.cpp:9:9: error: expected primary-expression  before ‘int’     9 |     set<int> s2(l2.begin(), l2.end());       |         ^~~ 5489c8e9-c760-49d1-8f97-0de757c41437.cpp:11:18: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    11 |     for (int i : s1) {       |                  ^~       |                  l1 5489c8e9-c760-49d1-8f97-0de757c41437.cpp:12:13: error: ‘s2’ was not declared in this scope; did you mean ‘l2’?    12 |         if (s2.find(i) != s2.end()) {       |             ^~       |             l2 5489c8e9-c760-49d1-8f97-0de757c41437.cpp: At global scope: 5489c8e9-c760-49d1-8f97-0de757c41437.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ 5489c8e9-c760-49d1-8f97-0de757c41437.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,16 +1,30 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
 vector<int> common(vector<int> l1, vector<int> l2) {
+    vector<int> result;
     set<int> s1(l1.begin(), l1.end());
-    set<int> s2;
-    for(int x : l2) {s2.insert(x);}
-    
-    set<int> intersection;
-    set_difference(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                       inserter(intersection, intersection.begin()));
-    
-    vector<int> result(intersection.begin(), intersection.end());
+    set<int> s2(l2.begin(), l2.end());
+
+    for (int i : l1) {
+        if (s2.find(i) != s2.end()) {
+            result.push_back(i);
+        }
+    }
+
+    sort(result.begin(), result.end());
     return result;
+}
+
+int main() {
+    vector<int> l1 = {1, 4, 3, 34, 653, 2, 5};
+    vector<int> l2 = {5, 7, 1, 5, 9, 653, 121};
+
+    vector<int> result = common(l1, l2);
+    for (int i : result) {
+        cout << i << " ";
+    }
+    return 0;
 }
