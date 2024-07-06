5eede658bd3de241e239cf188561a6c61b116ddd
beep boop <githubbot@vadim.me>
2024-07-06 19:09:26 +0200

    Solution 73 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7crd45z9 Prompt: In file included from 860a2810-1599-4999-9d69-bc9b75d588e7.cpp:29: 860a2810-1599-4999-9d69-bc9b75d588e7.cpp: In function ‘int main()’: 860a2810-1599-4999-9d69-bc9b75d588e7.cpp:31:35: error: ‘lst’ was not declared in  this scope    31 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 860a2810-1599-4999-9d69-bc9b75d588e7.cpp:31:43: error: ‘odd_sum’ was not declare d in this scope    31 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -16,11 +16,12 @@ int mainTest() {
     std::vector<float> lst = {1,3,5,7};
     long long odd_sum = 0;
 
+    // Initialize sum
     for(auto x : lst){
         if(int(x) > 0 && int(x) % 2 != 0)
             odd_sum += int(x);
     }
     
-    assert(double_the_difference(lst) == (long long)odd_sum * (long long)odd_sum);
+    assert(double_the_difference(lst) == pow(odd_sum, 2));
     return 0;
 }
