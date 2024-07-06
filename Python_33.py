918c024b16fab6d79ca7e19694fac7737c8cd588
beep boop <githubbot@vadim.me>
2024-07-06 16:06:00 +0200

    Solution 12 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r6wjanum Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/afc0c286-bafa-47eb-8712-d4c75a7d1 6c0.py", line 23, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/afc0c286-bafa-47eb-8712-d4c75a7d1 6c0.py", line 18, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -4,6 +4,13 @@ def sort_third(l: list):
         if i % 3 != 0:
             result.append(x)
         else:
-            min_element = min(x for i, x in enumerate(l) if (i+1) % 3 == (i-2) % 3)
+            next_index = (i + 1) % 3
+            while True:
+                try:
+                    next_element = l[next_index % len(l)]
+                    break
+                except IndexError:
+                    next_index += 1
+            min_element = min(y for y in l if (next_index + 1) % 3 != 0 or y == x)
             result.append(min_element)
     return sorted(result)
