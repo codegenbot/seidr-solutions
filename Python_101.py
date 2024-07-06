483a7304b8558a10522eb7ef1a247e2ebefe5851
beep boop <githubbot@vadim.me>
2024-07-06 21:50:04 +0200

    Solution 94 of Python/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/f2mlu349 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26308923-2b5b-44ad-8d04-68e349dee 7ba.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_101.py
+++ Python_101.py
@@ -1,21 +1,18 @@
-import re
+```
+    import re
 
+    def words_string():
+        results = {}
+        while True:
+            s = input("Please enter a string with comma-separated values or press Enter to stop: ")
+            if not s: 
+                break
+            elif s.strip() == "":
+                print("Program did not receive expected input. Please try again.")
+                continue
+            result = [x.strip() for x in re.split("[,\s]+", s) if x]
+            for word in set(result):
+                results[word] = results.get(word, 0) + 1
+        return results
 
-def words_string():
-    results = {}
-    while True:
-        s = input(
-            "Please enter a string with comma-separated values or press Enter to stop: "
-        )
-        if not s:
-            break
-        elif s.strip() == "":
-            print("Program did not receive expected input. Please try again.")
-            continue
-        result = [x.strip() for x in re.split("[,\s]+", s) if x]
-        for word in set(result):
-            results[word] = results.get(word, 0) + 1
-    return results
-
-
-print(words_string())
+    print(words_string())
