7f1b3aaa10a38f68078f61b1ae9cf3ffa901212c
beep boop <githubbot@vadim.me>
2024-07-07 01:52:37 +0200

    Solution 8 of CPP/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/b5exurhe Prompt: 78e0bb5f-c540-4ece-9e76-25f2ea1e2503.cpp:3:21: error: ‘vector’ was not declared in this scope     3 | std::string longest(vector<std::string> strings){       |                     ^~~~~~ 78e0bb5f-c540-4ece-9e76-25f2ea1e2503.cpp:3:39: error: expected primary-expressio n before ‘>’ token     3 | std::string longest(vector<std::string> strings){       |                                       ^ 78e0bb5f-c540-4ece-9e76-25f2ea1e2503.cpp:3:41: error: ‘strings’ was not declared  in this scope; did you mean ‘stdin’?     3 | std::string longest(vector<std::string> strings){       |                                         ^~~~~~~       |                                         stdin 78e0bb5f-c540-4ece-9e76-25f2ea1e2503.cpp:3:49: error: expected ‘,’ or ‘;’ before  ‘{’ token     3 | std::string longest(vector<std::string> strings){       |                                                 ^

--- CPP_12.cpp
+++ CPP_12.cpp
@@ -1,12 +1,4 @@
-#include <string>
 #include <vector>
+#include <string>
 
 std::string longest(vector<std::string> strings){
-    std::string result = strings[0];
-    for (const auto& str : strings) {
-        if (str.length() > result.length()) {
-            result = str;
-        }
-    }
-    return result;
-}
