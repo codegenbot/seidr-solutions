188a6ea81da713d722fe6aac541f50447e606889
beep boop <githubbot@vadim.me>
2024-07-06 23:40:09 +0200

    Solution 95 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mck1y7be Prompt:     1 | Here is the completed code:       | ^~~~ 899abe16-a6f4-4140-b7a3-469566625f38.cpp:6:1: error: ‘boost’ does not name a typ e     6 | boost::any compare_one(boost::any a, boost::any b) {       | ^~~~~ In file included from 899abe16-a6f4-4140-b7a3-469566625f38.cpp:34: 899abe16-a6f4-4140-b7a3-469566625f38.cpp: In function ‘int main()’: 899abe16-a6f4-4140-b7a3-469566625f38.cpp:36:13: error: ‘boost’ has not been decl ared    36 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 899abe16-a6f4-4140-b7a3-469566625f38.cpp:36:29: error: ‘string’ was not declared  in this scope    36 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 899abe16-a6f4-4140-b7a3-469566625f38.cpp:36:37: error: ‘compare_one’ was not dec lared in this scope    36 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,11 +1,10 @@
 ```cpp
 #include <boost/any.hpp>
-#include <boost/lexical_cast.hpp>
 #include <algorithm>
 #include <string>
+#include <iostream>
 
 using namespace std;
-using boost::any_cast;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(double)) {
@@ -13,10 +12,10 @@ boost::any compare_one(boost::any a, boost::any b) {
     } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
         return any_cast<double>(a);
     } else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        double da = boost::lexical_cast<double>(any_cast<string>(a));
+        double da = stod(any_cast<string>(a));
         return da > any_cast<double>(b) ? a : b;
     } else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        double db = boost::lexical_cast<double>(any_cast<string>(b));
+        double db = stod(any_cast<string>(b));
         return da > db ? a : b;
     } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
         string sa = any_cast<string>(a);
@@ -26,11 +25,11 @@ boost::any compare_one(boost::any a, boost::any b) {
     } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
         double da = any_cast<int>(a);
         string sb = any_cast<string>(b);
-        return da > boost::lexical_cast<double>(sb) ? a : b;
+        return da > stod(sb) ? a : b;
     } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
         double db = any_cast<int>(b);
         string sa = any_cast<string>(a);
-        return boost::lexical_cast<double>(sa) > db ? a : b;
+        return stod(sa) > db ? a : b;
     }
-    return "None";
+    return boost::any(); // default
 }
