a51494af8a0875182e50c36468f8b8e3da3665c4
beep boop <githubbot@vadim.me>
2024-07-06 07:12:52 +0200

    Solution 42 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/js6e9f34 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8ce7c5d7-f2b6-4bdd-811f-1b69db294 10e.py", line 33, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8ce7c5d7-f2b6-4bdd-811f-1b69db294 10e.py", line 30, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -9,19 +9,16 @@ def minPath(grid, k):
                 dfs(i, j, k - 1, grid, path, res, paths)
     return paths
 
-
 def dfs(i, j, k, grid, path, res, paths):
     n = len(grid)
     if k == 0:
         res.extend(path)
-        paths.append(res)
+        paths.append(res[:])
         return
     for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
         ni, nj = i + x, j + y
         if 0 <= ni < n and 0 <= nj < n and grid[ni][nj] not in path:
             temp = grid[ni][nj]
             grid[ni][nj], path.append(temp)
-            dfs(ni, nj, k - 1, grid, path, res, paths)
+            dfs(ni, nj, k - 1, grid, path, res + [temp], paths)
             grid[ni][nj] = temp
-            if len(res) > 0 and len(path) + len(res) == k:
-                return
