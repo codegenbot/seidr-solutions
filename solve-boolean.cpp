e13e3d2083f2f611df7c72da43eab58cf02233f4
beep boop <githubbot@vadim.me>
2024-07-06 20:09:11 +0200

    Solution 46 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/o9ngt94r Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,35 +1,31 @@
+```cpp
+#include <stack>
 #include <string>
-#include <iostream>
 
-using namespace std;
-
-bool solveBoolean(string s) {
-    bool t = true;
-    bool f = false;
+bool solveBoolean(std::string s) {
+    std::stack<char> st;
     bool res = true;
 
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 't') {
-            res = (s[i+1] == '&') ? (res & t) : (res | t);
-        } else if (s[i] == 'f') {
-            res = (s[i+1] == '&') ? (res & f) : (res | f);
-        } else if (s[i] == '|') {
-            if (i > 0 && s[i-1] == 't') t = f;
-            if (i < s.length() - 1 && s[i+1] == 't') f = true;
+    for (char c : s) {
+        if (c == 't' || c == 'f') {
+            st.push(c);
+        } else if (c == '|') {
+            if (st.top() == 't')
+                res = true;
+            else
+                res = false;
+            st.pop();
+        } else if (c == '&') {
+            while (!st.empty()) {
+                char top = st.top();
+                st.pop();
+                if (top == 'f')
+                    return false;
+                if (top == 't' && c != '&')
+                    res = true;
+            }
         }
     }
 
     return res;
 }
-
-int main() {
-    string s;
-    cout << "Enter a Boolean expression: ";
-    getline(cin, s);
-    bool result = solveBoolean(s);
-    if (result) {
-        cout << "True" << endl;
-    } else {
-        cout << "False" << endl;
-    }
-    return 0;}
