ea7b0f3a1e204fd509bed47c6aa55db790096665
beep boop <githubbot@vadim.me>
2024-07-06 08:05:19 +0200

    Solution 5 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/wx4l6hi4 Prompt: Complete the following code given the task description and function signature.

--- Python_1.py
+++ Python_1.py
@@ -1,12 +1,16 @@
 def separate_paren_groups(paren_string: str) -> List[str]:
-    stack = []
+    open_count = 0
+    close_count = 0
+    group = []
     groups = []
     for char in paren_string:
         if char == "(":
-            stack.append(char)
+            open_count += 1
         elif char == ")":
-            if len(stack) > 0 and stack[-1] == "(":
-                stack.pop()
-                groups.append("".join(stack))
-                stack = []
+            close_count += 1
+        if open_count == close_count and len(group) > 0:
+            groups.append("".join(group))
+            group = []
+        else:
+            group.append(char)
     return groups
