6fdbd45d6b19e6c6957f13111a6306ee4074e90f
beep boop <githubbot@vadim.me>
2024-07-06 02:23:31 +0200

    Solution 1 of CPP/65, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/waqofw3o Prompt: Complete the following code given the task description and function signature.

--- CPP_65.cpp
+++ CPP_65.cpp
@@ -1,19 +1,13 @@
 #include<string>
 using namespace std;
 
-string circular_shift(int x,int shift){
+string circular_shift(int x, int shift) {
     string str = to_string(x);
-    int len = str.length();
-    if(shift >= len) {
+    int n = str.length();
+    if (shift >= n)
         return str;
+    else {
+        string result = str.substr(shift) + str.substr(0, shift);
+        return result;
     }
-    string result = "";
-    for(int i=0; i<len; i++) {
-        if(i-shift < 0) {
-            result += str.substr(len-i,1);
-        } else {
-            result += str.substr(i-shift,1);
-        }
-    }
-    return result;
 }
