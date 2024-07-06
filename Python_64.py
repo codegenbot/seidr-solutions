ec51e5c14bffa83aa7aa9c1e366601b996449815
beep boop <githubbot@vadim.me>
2024-07-06 06:12:43 +0200

    Solution 3 of Python/64, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nzi1446c Prompt: Complete the following code given the task description and function signature.

--- Python_64.py
+++ Python_64.py
@@ -3,6 +3,6 @@ def vowels_count(s):
     s = s.lower()
     count = 0
     for char in s:
-        if char in 'aeiou' or (char == 'y' and s.index(char) == len(s) - 1):
+        if char in 'aeiouy' and (char == 'y' or s.index(char) == len(s)-1):
             count += 1
     return count
