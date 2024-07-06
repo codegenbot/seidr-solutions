3601b5df5063a0daba30fa63598759cf9fe07d1b
beep boop <githubbot@vadim.me>
2024-07-06 06:33:53 +0200

    Solution 70 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/op3ghdzk Prompt:  in ‘a’, which is of non-class type ‘int’    47 |     for (int i=0;i<a.size();i++)       |                      ^~~~ 676e5ed9-69ad-477b-9dfe-fa7ca78c77fe.cpp:49:10: error: invalid types ‘int[int]’ for array subscript    49 |     if (a[i]!=b[i]) return false;       |          ^ 676e5ed9-69ad-477b-9dfe-fa7ca78c77fe.cpp:49:16: error: invalid types ‘int[int]’ for array subscript    49 |     if (a[i]!=b[i]) return false;       |                ^ In file included from 676e5ed9-69ad-477b-9dfe-fa7ca78c77fe.cpp:44: 676e5ed9-69ad-477b-9dfe-fa7ca78c77fe.cpp: In function ‘int main()’: 676e5ed9-69ad-477b-9dfe-fa7ca78c77fe.cpp:54:31: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    54 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ~~~~~~~~~~~^~~~~~~~~~~~~~       |                               |       |                               const char* 676e5ed9-69ad-477b-9dfe-fa7ca78c77fe.cpp:15:43: note:   initializing argument 1 of ‘int total_match(int, int)’    15 | vector<string> total_match(vector<string> lst1, vector<string> lst2) {       |                            ~~~~~~~~~~~~~~~^~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -6,8 +6,8 @@ bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) return false;
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
-    for(const auto& str1 : a) {
-        if(find(b.begin(),b.end(),str1) == b.end()) return false;
+    for(size_t i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
@@ -36,7 +36,7 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
 }
 
 vector<string> testMain() {
-    vector<string> vec1 = {"this"};
+    vector<string> vec1 = {{"this"}};
     vector<string> vec2 = {""};
     return total_match(vec1,vec2);
 }
