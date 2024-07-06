2563b93cefea6424f6800db6e76107dc9c43a78c
beep boop <githubbot@vadim.me>
2024-07-06 09:09:48 +0200

    Solution 31 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/51amy07t Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a7b9dea6-b7df-4cd6-8d58-2453c3086 178.py", line 15, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a7b9dea6-b7df-4cd6-8d58-2453c3086 178.py", line 12, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a7b9dea6-b7df-4cd6-8d58-2453c3086 178.py", line 6, in prime_fib     fib.append(fib[i-2] + fib[i-1])                           ~~~^^^^^ IndexError: list index out of range

--- Python_39.py
+++ Python_39.py
@@ -1,8 +1,9 @@
+
 def prime_fib(n: int):
-    fib = [0, 1]
+    fib = [0, 1, 1]
     i = 2
     while len(fib) < n:
-        fib.append(fib[i - 2] + fib[i - 1])
+        fib.append(fib[i-2] + fib[i-1])
         if fib[-1] % 2 == 0 and fib[-1] != 2:
             fib.pop()
         i += 1
