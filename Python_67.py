27022ee3aa904d5a236d9e039ee610f5afa511e3
beep boop <githubbot@vadim.me>
2024-07-06 04:51:32 +0200

    Solution 62 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/kh7c2g1m Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/06e58eb6-57a4-4240-99dd-b2739a291 17e.py", line 12, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/06e58eb6-57a4-4240-99dd-b2739a291 17e.py", line 9, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: fruit_distribution() takes 1 positional argument but 2 were given

--- Python_67.py
+++ Python_67.py
@@ -1,5 +1,6 @@
-def fruit_distribution(s):
-    # use regular expressions to extract numbers from the input string
-    apples = int(re.search(r"\d+", s).group())
-    oranges = int(re.search(r"\d+", s).group())
-    return apples + oranges
+def fruit_distribution(s, n):
+    apples = int(re.search(r'\d+', s).group())
+    oranges = int(re.search(r'\d+', s).group())
+    return (apples + oranges) * n
+  
+assert fruit_distribution("1 apples and 100 oranges", 19) == 389
