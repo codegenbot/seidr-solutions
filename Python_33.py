d788bbddff7898f5cc5efac956be08b1c408060d
beep boop <githubbot@vadim.me>
2024-07-06 16:52:25 +0200

    Solution 38 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r6wjanum Prompt: [3, 5, 6, 4, 8, 9, 2] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/82a1aa86-6972-4c20-b45b-9d93727c5 51f.py", line 18, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/82a1aa86-6972-4c20-b45b-9d93727c5 51f.py", line 13, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,13 +1,11 @@
+```
 def sort_third(l):
     result = []
     for i in range(0, len(l), 3):
         if i + 2 < len(l):
-            result.append(sorted([l[i], l[i+1], l[i+2]]))
+            result.append([*sorted([l[i], l[i+1], l[i+2]])])
         else:
-            if len(l) == i + 1:
-                result.append([l[i]])
-            else:
-                result.append(sorted([l[i], l[i+1]]))
+            result.append([l[i]])
     return [item for sublist in result for item in sublist]
 
 input_list = [5, 6, 3, 4, 8, 9, 2, 1]
