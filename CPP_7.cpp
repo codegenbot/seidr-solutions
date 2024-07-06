41904b2425817b6bde6fc81464cfe915393a532f
beep boop <githubbot@vadim.me>
2024-07-06 14:39:01 +0200

    Solution 84 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vjf9wmxz Prompt: his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0b8a064d-a658-4265-80a7-518a80c0fe0b.cpp:28:45: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 0b8a064d-a658-4265-80a7-518a80c0fe0b.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 0b8a064d-a658-4265-80a7-518a80c0fe0b.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main(){       |     ^~~~ In file included from 0b8a064d-a658-4265-80a7-518a80c0fe0b.cpp:27: 0b8a064d-a658-4265-80a7-518a80c0fe0b.cpp: In function ‘int main()’: 0b8a064d-a658-4265-80a7-518a80c0fe0b.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -13,14 +13,10 @@ std::vector<std::string> filter_by_substring(std::vector<std::string> strings, s
     return result;
 }
 
-bool same(std::vector<std::string> a, std::vector<std::string> b){
+bool isSame(std::vector<std::string> a, std::vector<std::string> b){
     if(a.size()!=b.size())return false;
     for(auto s : a){
         if(std::find(b.begin(),b.end(),s)==b.end())return false;
     }
     return true;
-}
-
-int main(){
-    assert(same(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
 }
