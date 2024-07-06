3cdd1e84eef1261444c35777597ddade5ecd9d6a
beep boop <githubbot@vadim.me>
2024-07-06 16:58:58 +0200

    Solution 44 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r6wjanum Prompt: [3, 5, 6, 4, 8, 9, 1, 2] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b27e6afb-5cc4-4684-8044-573f66662 657.py", line 21, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b27e6afb-5cc4-4684-8044-573f66662 657.py", line 16, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -4,8 +4,8 @@ def sort_third(l):
     for i in range(0, len(l), 3):
         if i + 2 < len(l):
             result.append(sorted([l[i], l[i+1], l[i+2]]))
-        elif i + 1 < len(l): 
-            result.append([l[i]])  
+        elif i + 1 < len(l):
+            result.append(sorted([l[i], l[i+1]]))
         else:
             result.append([l[i]])
     return [item for sublist in result for item in sublist]
