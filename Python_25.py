e3f2d5f4d402ad53244454e414e31a148ab73811
beep boop <githubbot@vadim.me>
2024-07-06 07:28:22 +0200

    Solution 70 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5v317pmi Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/24840201-2748-4d59-97db-36855e3ad 144.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,28 +1,8 @@
 from typing import List
 
-def main():
-    num = int(input("Enter a number: "))
-    factors = factorize(num)
-    print(f"The factors of {num} are {factors}")
+def read_integers() -> List[int]:
+    input_str = input("Enter numbers separated by space: ")
+    integers = list(map(int, input_str.split()))
+    return integers
 
-def factorize(n: int) -> List[int]:
-    if n < 2:
-        return [n]
-    factors = []
-    i = 2
-    while i * i <= n:
-        if n % i:
-            i += 1
-        else:
-            n //= i
-            count = 0
-            while n % i == 0:
-                n //= i
-                count += 1
-            factors.extend([i] * count)
-    if n > 1:
-        factors.append(n)
-    return factors
-
-if __name__ == "__main__":
-    main()
+print(read_integers())
