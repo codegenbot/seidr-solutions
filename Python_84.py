773ff719c0b38c841ac81cce39beaee42fde07af
beep boop <githubbot@vadim.me>
2024-07-06 02:41:13 +0200

    Solution 34 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tkiemw9v Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/14ec1d5a-9e70-4ce5-b7ae-d7f2b404c 02f.py", line 15, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/14ec1d5a-9e70-4ce5-b7ae-d7f2b404c 02f.py", line 12, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -1,9 +1,2 @@
-```
 def solve(N):
-    result = 0
-    count = len(str(bin(N)[2:]))
-    for i in str(bin(N)[2:]):
-        if i == "1":
-            result += 2 ** (count - 1)
-        count -= 1
-    return bin(result)[2:]
+    return bin(int(bin(N)[2:], 2))[2:]
