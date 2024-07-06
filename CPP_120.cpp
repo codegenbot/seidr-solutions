530ff22c50b6479eeb7141e768cb53c303ca8e10
beep boop <githubbot@vadim.me>
2024-07-06 17:49:51 +0200

    Solution 0 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xkhiaef7 Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_120.cpp
@@ -0,0 +1,12 @@
+vector<int> maximum(vector<int> arr,int k){
+    priority_queue<int> max_heap;
+    for(int i:arr){
+        max_heap.push(i);
+        if(max_heap.size()>k)
+            max_heap.pop();
+    }
+    vector<int> result(k);
+    for(int i=k-1;i>=0;--i)
+        result[i]=max_heap.top(),max_heap.pop();
+    return result;
+}
