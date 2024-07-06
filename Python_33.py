bfd726271664e5493e6319c2ae4fce4fb60a9aa5
beep boop <githubbot@vadim.me>
2024-07-06 05:31:16 +0200

    Solution 61 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0n2q1vau Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/55657df8-d211-44eb-97e6-3a45621e6 a93.py", line 20, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/55657df8-d211-44eb-97e6-3a45621e6 a93.py", line 15, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,15 +1,3 @@
+```Python
 def sort_third(l: list):
-    return sorted(
-        [
-            (
-                i
-                if i % 3 != 0
-                else (
-                    [x for x in l if x % 3 == 0].pop(0)
-                    if any(x % 3 == 0 for x in l)
-                    else float("inf")
-                )
-            )
-            for i in l
-        ]
-    )
+    return sorted([i if i % 3 != 0 else [x for x in l if x % 3 == 0].pop(0) if any(x % 3 == 0 for x in l) else float('inf') for i in l])
