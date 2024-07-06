b3fb3d2a6ad305f09ad66e2cce6d0549f2aba9c7
beep boop <githubbot@vadim.me>
2024-07-06 03:53:05 +0200

    Solution 68 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/23sd7ljf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/78ee80f6-b240-43c8-a73d-af181448b 96d.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,3 +1,6 @@
 def solution(lst):
-    total = sum(int(i) for i in lst[1::2] if int(i) > 1 and int(i) % 2 != 0)
-    return total * int(lst[0]) if total else 0
+    odd_count = 0
+    for num in lst:
+        if int(num) % 2 != 0 and int(num) > 1:
+            odd_count += 1
+    return 0 if odd_count == 0 else int(lst[0]) * (sum(int(i) for i in lst[1::2] if int(i) > 1 and int(i) % 2 != 0))
