c8212e1a8421e85672886f311f50b621ecc2da35
beep boop <githubbot@vadim.me>
2024-07-06 15:57:29 +0200

    Solution 71 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/hjcrazap Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_map.h:62,                  from /usr/include/c++/11/map:61,                  from 2698c0bc-411f-42b7-8bbe-7a5a6668482e.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,9 +1,12 @@
+```cpp
 #include <unordered_map>
-stdstring int_to_mini_roman(int number) {
-    stdstring result;
-    unordered_map<int, string> roman_map = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-                                             {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-                                             {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+#include <string>
+
+std::string int_to_mini_roman(int number) {
+    std::string result;
+    std::unordered_map<int, std::string> roman_map = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+                                                       {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+                                                       {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
     for (const auto& pair : roman_map) {
         while (number >= pair.first) {
             number -= pair.first;
