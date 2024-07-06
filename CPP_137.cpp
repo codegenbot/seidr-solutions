5e232a78375ec5a58a2993e8b9da1acac2033caf
beep boop <githubbot@vadim.me>
2024-07-06 18:20:15 +0200

    Solution 26 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6jdkzc1w Prompt:    23 | } else if ((a.type() == typeid(string) && b.type() == typeid(float)) ||       |   ^~~~ 990d2dfc-6fc0-424c-af7f-b49ffed04634.cpp:32:3: error: expected unqualified-id be fore ‘else’    32 | } else {       |   ^~~~ In file included from 990d2dfc-6fc0-424c-af7f-b49ffed04634.cpp:43: 990d2dfc-6fc0-424c-af7f-b49ffed04634.cpp: In function ‘int main()’: 990d2dfc-6fc0-424c-af7f-b49ffed04634.cpp:45:13: error: ‘boost’ has not been decl ared    45 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 990d2dfc-6fc0-424c-af7f-b49ffed04634.cpp:45:29: error: ‘string’ was not declared  in this scope    45 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 990d2dfc-6fc0-424c-af7f-b49ffed04634.cpp:45:37: error: ‘compare_one’ was not dec lared in this scope    45 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,3 +1,7 @@
+#include <boost/any.hpp>
+#include <string>
+#include <boost/convert.hpp>
+
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
         if (get<int>(a) > get<int>(b))
@@ -13,26 +17,26 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         else
             return boost::any("None");
-    } else if ((a.type() == typeid(string) && b.type() == typeid(int)) || 
-               (a.type() == typeid(int) && b.type() == typeid(string))) {
-        if (get<string>(a).compare(to_string(get<int>(b))) > 0)
+    } else if ((a.type() == typeid(std::string) && b.type() == typeid(int)) || 
+               (a.type() == typeid(int) && b.type() == typeid(std::string))) {
+        if (get<std::string>(a).compare(std::to_string(get<int>(b))) > 0)
             return a;
-        else if (get<string>(a).compare(to_string(get<int>(b))) < 0)
+        else if (get<std::string>(a).compare(std::to_string(get<int>(b))) < 0)
             return b;
         else
             return boost::any("None");
-    } else if ((a.type() == typeid(string) && b.type() == typeid(float)) || 
-               (a.type() == typeid(float) && b.type() == typeid(string))) {
-        string strB = to_string(get<float>(b));
-        if (get<string>(a).compare(strB) > 0)
+    } else if ((a.type() == typeid(std::string) && b.type() == typeid(float)) || 
+               (a.type() == typeid(float) && b.type() == typeid(std::string))) {
+        std::string strB = std::to_string(get<float>(b));
+        if (get<std::string>(a).compare(strB) > 0)
             return a;
-        else if (get<string>(a).compare(strB) < 0)
+        else if (get<std::string>(a).compare(strB) < 0)
             return b;
         else
             return boost::any("None");
     } else {
-        string strA = to_string(get<int>(a));
-        string strB = to_string(get<float>(b));
+        std::string strA = std::to_string(get<int>(a));
+        std::string strB = std::to_string(get<float>(b));
         if (strA.compare(strB) > 0)
             return a;
         else if (strA.compare(strB) < 0)
