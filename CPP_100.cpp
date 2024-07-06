378bfc58d0ada6e709857d7fc64f7b125d7060f1
beep boop <githubbot@vadim.me>
2024-07-06 17:28:17 +0200

    Solution 56 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/z02owpwx Prompt: In file included from /usr/include/c++/11/vector:67,                  from 06f09317-d6a3-48ba-a9cb-2f46732ec49c.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 06f09317-d6a3-48ba-a9cb-2f46732ec49c.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 06f09317-d6a3-48ba-a9cb-2f46732ec49c.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 06f09317-d6a3-48ba-a9cb-2f46732ec49c.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 06f09317-d6a3-48ba-a9cb-2f46732ec49c.cpp:15: 06f09317-d6a3-48ba-a9cb-2f46732ec49c.cpp: In function ‘int main()’: 06f09317-d6a3-48ba-a9cb-2f46732ec49c.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,17 +1,6 @@
 ```cpp
 #include <vector>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
+#include <algorithm>
 
 std::vector<int> make_a_pile(int n) {
     std::vector<int> pile;
@@ -25,6 +14,11 @@ std::vector<int> make_a_pile(int n) {
     return pile;
 }
 
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    return a == b;
+}
+
 int main() {
     assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
+    return 0;
 }
