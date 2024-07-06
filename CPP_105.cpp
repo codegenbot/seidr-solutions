bcff42ea12a5bb1fe0fcee914e9ebd6491a6611d
beep boop <githubbot@vadim.me>
2024-07-06 17:21:47 +0200

    Solution 6 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/horaq1dq Prompt:       |                              ^~~~~~ f44bcbd7-351d-4b26-8e2c-3d41ff756c68.cpp:19:37: error: ‘string’ was not declared  in this scope    19 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f44bcbd7-351d-4b26-8e2c-3d41ff756c68.cpp:19:44: error: ‘b’ was not declared in t his scope    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f44bcbd7-351d-4b26-8e2c-3d41ff756c68.cpp:19:45: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f44bcbd7-351d-4b26-8e2c-3d41ff756c68.cpp:18: f44bcbd7-351d-4b26-8e2c-3d41ff756c68.cpp: In function ‘int main()’: f44bcbd7-351d-4b26-8e2c-3d41ff756c68.cpp:28:20: error: ‘by_length’ was not decla red in this scope    28 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ f44bcbd7-351d-4b26-8e2c-3d41ff756c68.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -27,13 +27,12 @@ vector<string> by_length(vector<int> arr) {
 }
 
 int main() {
-    vector<int> arr = {1, 2, 5, 3, 6, 4};
-    vector<string> output = by_length(arr);
-    cout << "The output is: ";
+    vector<int> input = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<string> output = by_length(input);
+
     for (string str : output) {
-        cout << str << " ";
+        cout << str << endl;
     }
-    cout << endl;
-    
+
     return 0;
 }
