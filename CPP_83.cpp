a71f6aa0a9e3220fcad252f57886847dd6907786
beep boop <githubbot@vadim.me>
2024-07-06 06:36:35 +0200

    Solution 74 of CPP/83, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7jp0kji8 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -1,16 +1,21 @@
+```cpp
 #include <iostream>
 #include <cmath>
-#include <string>
 
 using namespace std;
 
 bool starts_one_ends(int n) {
     int count = 0;
     for (int i = 1; i <= 9; i++) {
-        string str = to_string(i);
-        str.append(n-1, '0');
-        if ((str[0] == '1' || str.back() == '1')) {
+        if (i == 1 || i % 10 == 1) {
             count++;
+        }
+        for (int j = 1; j < pow(10, n); j++) {
+            int num = i * pow(10, n - 1) + j;
+            string str = to_string(num);
+            if ((stoi(str.substr(0, 1)) == 1 || str.back() == '1') && (stoi(string(str.substr(0, 1))) == 1 || str.back() == '1')) {
+                count++;
+            }
         }
     }
     return count > 0;
@@ -19,5 +24,5 @@ bool starts_one_ends(int n) {
 int main() {
     int n;
     cin >> n;
-    cout << (starts_one_ends(n) ? "true\n" : "false\n");
+    cout << starts_one_ends(n);
 }
