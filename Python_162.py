c792baf3bc8f25afaf9be3cc532906432c901efb
beep boop <githubbot@vadim.me>
2024-07-06 05:31:35 +0200

    Solution 2 of Python/162, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/aslopdoc Prompt: Complete the following code given the task description and function signature.

--- Python_162.py
+++ Python_162.py
@@ -1,10 +1,9 @@
-import hashlib
-
 
 def string_to_md5(text):
-    if text == "":
+    if text == '':
         return None
     else:
+        import hashlib
         md5 = hashlib.md5()
-        md5.update(text.encode("utf-8"))
+        md5.update(text.encode('utf-8'))
         return md5.hexdigest()
