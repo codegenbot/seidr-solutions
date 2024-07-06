7187d1a81dbe3bbfef9ce5fa4bdcf301e3ea27b3
beep boop <githubbot@vadim.me>
2024-07-06 07:38:38 +0200

    Solution 84 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cjku6y8k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1c2fb040-c048-405c-93ce-77a1f916a a7a.py", line 30     print("Invalid operation. Please enter 'square', 'cube' or 'sum_of_squares'" ") ^ SyntaxError: unterminated string literal (detected at line 30)

--- Python_142.py
+++ Python_142.py
@@ -1,14 +1,12 @@
+```
 def sum_squares(num1, num2):
     return square(num1) + square(num2)
-
 
 def square(x):
     return x**2
 
-
 def cube(x):
     return x**3
-
 
 while True:
     operation = input("Enter 'square', 'cube' or 'sum_of_squares', or 'quit' to exit: ")
@@ -30,4 +28,4 @@ while True:
         break
 
     else:
-        print("Invalid operation. Please enter 'square', 'cube' or 'sum_of_squares'")
+        print("Invalid operation. Please enter 'square', 'cube' or 'sum_of_squares''")
