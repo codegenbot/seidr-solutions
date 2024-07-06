39fdd082788151502f102fd9a6e49ce55cdc1cfd
beep boop <githubbot@vadim.me>
2024-07-06 10:59:02 +0200

    Solution 60 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ov14dqn5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2b5564c4-19ca-47ff-a679-f6315f5a7 270.py", line 24, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2b5564c4-19ca-47ff-a679-f6315f5a7 270.py", line 21, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ AssertionError

--- Python_1.py
+++ Python_1.py
@@ -11,7 +11,9 @@ def separate_paren_groups(paren_string):
                 stack.pop()
                 count -= 1
             else:
-                # Check if the stack is empty after popping the closing parenthesis
-                if not stack:
-                    result.append(c)
-    return [group for group in result if group[0] == "(" and group[-1] == ")" and len(group) % 2 == 0 and count == 0]
+                result.append(c)
+    return [
+        group
+        for group in result
+        if group[0] == "(" and group[-1] == ")" and len(group) % 2 == 0 and count == 0
+    ]
