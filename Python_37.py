890857106db0cd39a1fb372ba61bf52d1196e389
beep boop <githubbot@vadim.me>
2024-07-06 08:53:30 +0200

    Solution 2 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/srlpv7yg Prompt: Complete the following code given the task description and function signature.

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,3 @@
 
-def sort_even(l):
-    even_indices = [i for i in range(len(l)) if i % 2 == 0]
-    even_values = sorted([l[i] for i in even_indices])
-    return [l[i] if i not in even_indices else even_values.pop() for i in range(len(l))]
+def sort_even(l: list):
+    return l[:] if len(l) == 1 else [min(l[::2])] + sort_even(l[2:])
