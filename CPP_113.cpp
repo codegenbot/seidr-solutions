6ddeff219ca5c220c4791702225b7ae21035ab1b
beep boop <githubbot@vadim.me>
2024-07-06 06:34:58 +0200

    Solution 11 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l9iawszk Prompt:       |                                            ^ 3fa57c02-8b8b-41e4-91e9-c59579f5a259.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ 3fa57c02-8b8b-41e4-91e9-c59579f5a259.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~ In file included from 3fa57c02-8b8b-41e4-91e9-c59579f5a259.cpp:37: 3fa57c02-8b8b-41e4-91e9-c59579f5a259.cpp: In function ‘int main()’: 3fa57c02-8b8b-41e4-91e9-c59579f5a259.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(odd_count({"271", "137", "314"}) , {       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    48 |         "the number of odd elements 2n the str2ng 2 of the 2nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    49 |         "the number of odd elements 3n the str3ng 3 of the 3nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    50 |         "the number of odd elements 2n the str2ng 2 of the 2nput."       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    51 |     }));       |     ~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -3,10 +3,10 @@
 #include <vector>
 #include <string>
 
-bool issame(vector<string> a,vector<string> b){
-    if(a.size() != b.size()) return false;
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]) return false;
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
@@ -23,7 +23,7 @@ std::vector<std::string> odd_count(std::vector<std::string> lst) {
         std::string temp = "the number of odd elements ";
         temp += std::to_string(count);
         temp += " in the str";
-        temp += std::to_string(i+1);
+        temp += std::to_string(i + 1);
         temp += " of the input.";
         result.push_back(temp);
     }
