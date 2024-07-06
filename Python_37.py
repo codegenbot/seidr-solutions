89b760f0fa70ffe20d466bbca2e810940f640d2d
beep boop <githubbot@vadim.me>
2024-07-06 16:36:12 +0200

    Solution 39 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1fv3kcsh Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bc72e27e-f2ac-4155-b9ce-d551f2ab0 100.py", line 21, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bc72e27e-f2ac-4155-b9ce-d551f2ab0 100.py", line 16, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,14 +1,18 @@
-def sort_even(l: list):
-    evens = sorted([x for x in l if x % 2 == 0])
-    odds = sorted([x for x in l if x % 2 != 0])
-    
+def sort_even(l: list) -> tuple:
+    pos_evens = sorted([x for x in l if 0 < x and x % 2 == 0])
+    neg_evens = sorted([-x for x in l if x < 0 and x % 2 == 0])
+    odds = sorted([x for x in l if (x < 0 and x % 2 != 0) or (x > 0 and x % 2 != 0)])
+
     result = []
-    while len(odds) >= len(evens):
+    while len(odds) >= len(pos_evens) and len(odds) >= len(neg_evens):
         result.append(odds.pop(0))
-        if evens:
-            result.append(evens.pop(0))
-        
-    result.extend(evens)
+        if pos_evens:
+            result.append(pos_evens.pop(0))
+        if neg_evens:
+            result.append(-neg_evens.pop(0))
+
+    result.extend(pos_evens)
+    result.extend([-x for x in neg_evens])
     result.extend(odds)
-    
+
     return tuple(result)
