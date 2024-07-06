0f35dc1cb7f99aef35f45b5f2a197ade6da5176d
beep boop <githubbot@vadim.me>
2024-07-06 22:20:43 +0200

    Solution 3 of Python/73, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mb837mah Prompt: Complete the following code given the task description and function signature.

--- Python_73.py
+++ Python_73.py
@@ -1,4 +1,4 @@
 def smallest_change(arr):
     arr = [str(x) for x in arr]
-    rev_arr = arr[::-1]
-    return sum(c1 != c2 for c1, c2 in zip(arr, rev_arr))
+    rev_arr = "".join(reversed(arr))
+    return sum(c1 != c2 for c1, c2 in zip(arr, rev_arr)) // 2
