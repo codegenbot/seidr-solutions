5eb55479b9e63ace62d5a8727e12b40e83d512ed
beep boop <githubbot@vadim.me>
2024-07-06 19:32:13 +0200

    Solution 13 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7aum6t7i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/88092422-648f-41f7-adcd-1d53d7ef1 896.py", line 43, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/88092422-648f-41f7-adcd-1d53d7ef1 896.py", line 13, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,9 +1,10 @@
-```
 def sum_squares(lst):
     if not isinstance(lst, list) or len(lst) == 0:
         raise ValueError("Input must be a non-empty list")
     
     total_sum = 0
     for num in lst:
+        if not isinstance(num, (int, float)):
+            raise ValueError(f"Invalid input '{num}' found. Input must contain only numbers.")
         total_sum += num ** 2
     return total_sum
