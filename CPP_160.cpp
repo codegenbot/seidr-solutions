10caa6ab135678dda61c889e1effb732fea573f1
beep boop <githubbot@vadim.me>
2024-07-06 12:38:15 +0200

    Solution 50 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dl2z8mmq Prompt:       |                ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~ 76130fbf-fa14-4100-8255-02a6d01971f2.cpp: At global scope: 76130fbf-fa14-4100-8255-02a6d01971f2.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 76130fbf-fa14-4100-8255-02a6d01971f2.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 76130fbf-fa14-4100-8255-02a6d01971f2.cpp:28: 76130fbf-fa14-4100-8255-02a6d01971f2.cpp: In function ‘int main()’: 76130fbf-fa14-4100-8255-02a6d01971f2.cpp:30:23: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    30 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~       |                       |       |                       <brace-enclosed initializer list> 76130fbf-fa14-4100-8255-02a6d01971f2.cpp:4:41: note:   initializing argument 1 o f ‘int do_algebra(int, std::vector<int>)’     4 | int do_algebra(std::vector<std::string> operations, std::vector<int> ope rands) {       |                ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,18 +1,16 @@
 #include <vector>
 #include <cmath>
 
-int do_algebra(std::string operation, std::vector<int> operands) {
+int do_algebra(std::vector<std::string> operations, std::vector<int> operands) {
     int result = operands[0];
     for (int i = 1; i < operands.size(); i++) {
-        if (operation == "//") {
-            while (operands[i] != 0 && result % operands[i] == 0) {
-                result /= operands[i];
-            }
-        } else if (operation == "*") {
+        if (operations[i-1] == "/") {
+            result /= operands[i];
+        } else if (operations[i-1] == "*") {
             result *= operands[i];
-        } else if (operation == "+") {
+        } else if (operations[i-1] == "+") {
             result += operands[i];
-        } else if (operation == "-") {
+        } else if (operations[i-1] == "-") {
             result -= operands[i];
         }
     }
@@ -20,6 +18,7 @@ int do_algebra(std::string operation, std::vector<int> operands) {
 }
 
 int main() {
-    int result = do_algebra("//", {7, 3, 4});
+    int result = do_algebra({std::string{"*"}, std::string{"/"}}, {7, 3, 4});
+    assert(result == 8);
     return 0;
 }
