49f39e8892eef392f2a3a66cf68be66185902ba2
beep boop <githubbot@vadim.me>
2024-07-06 15:59:39 +0200

    Solution 8 of CPP/76, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/m45gv9y5 Prompt: c643b3aa-30ab-445f-a233-83ad14134a85.cpp:1:5: warning: missing terminating ' cha racter     1 | Here's the solution:       |     ^ c643b3aa-30ab-445f-a233-83ad14134a85.cpp:1:5: error: missing terminating ' chara cter     1 | Here's the solution:       |     ^~~~~~~~~~~~~~~~ c643b3aa-30ab-445f-a233-83ad14134a85.cpp:1:1: error: ‘Here’ does not name a type     1 | Here's the solution:       | ^~~~ In file included from c643b3aa-30ab-445f-a233-83ad14134a85.cpp:7: c643b3aa-30ab-445f-a233-83ad14134a85.cpp: In function ‘int main()’: c643b3aa-30ab-445f-a233-83ad14134a85.cpp:9:13: error: ‘is_simple_power’ was not declared in this scope     9 |     assert (is_simple_power(1, 12)==true);       |             ^~~~~~~~~~~~~~~

--- CPP_76.cpp
+++ CPP_76.cpp
@@ -1,11 +1,4 @@
 #include <cmath>
-#include <assert.h>
-
 bool is_simple_power(int x, int n) {
     return pow(n, 1.0 / log(x) / log(n)) == static_cast<int>(pow(n, 1.0 / log(x) / log(n)));
-}
-
-int main() {
-    assert (is_simple_power(1, 12)==true);
-    return 0;
 }
