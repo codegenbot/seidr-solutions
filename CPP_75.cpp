158865c018d34e2a1ad23d94fefe33698e50ad3c
beep boop <githubbot@vadim.me>
2024-07-06 03:38:05 +0200

    Solution 4 of CPP/75, ratio 0.7 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/hc2rucjf Prompt: Program failed

--- CPP_75.cpp
+++ CPP_75.cpp
@@ -1,20 +1,22 @@
 #include <cmath>
 
 bool is_multiply_prime(int a) {
-    for (int i = 1; i <= std::pow(100, 1/3); ++i) {
-        int j = i, k = i;
-        while (j * k <= 100) {
-            if (i * j * k == a) {
-                bool prime = true;
-                for (int x = 2; x <= sqrt(a); ++x) {
-                    if ((i % x == 0) || (j % x == 0) || (k % x == 0)) {
-                        prime = false;
-                        break;
+    for (int i = 2; i * i <= a; ++i) {
+        for (int j = 1; j * i <= a; ++j) {
+            int k = 1;
+            while (i*j*k <= a) {
+                if (i * j * k == a) {
+                    int prime = true;
+                    for (int x = 2; x <= sqrt(a); ++x) {
+                        if ((i % x == 0) || (j % x == 0) || (k % x == 0)) {
+                            prime = false;
+                            break;
+                        }
                     }
+                    return prime;
                 }
-                return prime;
+                k++;
             }
-            k++;
         }
     }
     return false;
