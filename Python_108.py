692a3be51b69899f46566bead6bfd4a2c1cc4e28
beep boop <githubbot@vadim.me>
2024-07-06 08:28:59 +0200

    Solution 2 of Python/108, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/hyfdk2g6 Prompt: Complete the following code given the task description and function signature.

--- Python_108.py
+++ Python_108.py
@@ -1,3 +1,10 @@
-```
 def count_nums(arr):
-    return sum(1 for num in arr if sum(int(digit) if digit != '-' else -int(digit) for digit in str(abs(num))) > 0)
+    return sum(
+        1
+        for num in arr
+        if sum(
+            int(digit) if digit != "-" else -int(digit[1:])
+            for digit in str(num).replace("-", "")
+        )
+        > 0
+    )
