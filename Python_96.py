be1546d9cd8289cb6f86dc64eb02d802d1fc57a6
beep boop <githubbot@vadim.me>
2024-07-06 02:24:11 +0200

    Solution 90 of Python/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/j5uln9v9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/73bc7d12-3c4a-47a7-9401-9400496cd 9b4.py", line 37, in <module>     check(count_up_to)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/73bc7d12-3c4a-47a7-9401-9400496cd 9b4.py", line 8, in check     assert count_up_to(101) == [            ^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/73bc7d12-3c4a-47a7-9401-9400496cd 9b4.py", line 4, in count_up_to     if is_prime(i):        ^^^^^^^^ NameError: name 'is_prime' is not defined

--- Python_96.py
+++ Python_96.py
@@ -1,15 +1,10 @@
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
 def count_up_to(n):
-    result = [2]
-    for i in range(3, n + 1, 2):
+    result = []
+    for i in range(2, n + 1):
         if is_prime(i):
             result.append(i)
     return result
+
+
+def is_prime(n):
+    return n > 1 and all(n % i for i in range(2, int(n**0.5) + 1))
