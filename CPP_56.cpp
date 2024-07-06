6622e29e6219bc5163ab3448491b8c15754a37b4
beep boop <githubbot@vadim.me>
2024-07-06 04:58:17 +0200

    Solution 42 of CPP/56, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/p0gd0cm6 Prompt: c0cb6af1-e777-404e-916f-6e9d5be6e8b6.cpp:1:30: error: ‘string’ is not a member o f ‘std’     1 | bool correct_bracketing(std::string brackets) {       |                              ^~~~~~ c0cb6af1-e777-404e-916f-6e9d5be6e8b6.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | bool correct_bracketing(std::string brackets) { In file included from c0cb6af1-e777-404e-916f-6e9d5be6e8b6.cpp:16: c0cb6af1-e777-404e-916f-6e9d5be6e8b6.cpp: In function ‘int main()’: c0cb6af1-e777-404e-916f-6e9d5be6e8b6.cpp:18:36: error: ‘correct_bracketing’ cann ot be used as a function    18 |     assert (not (correct_bracketing("<><><<><>><>>><>")));       |                  ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_56.cpp
+++ CPP_56.cpp
@@ -1,16 +1,15 @@
 #include <string>
 
-using namespace std;
-
-bool correct_bracketing(string brackets) {
+bool correct_bracketing(std::string brackets) {
     int count = 0;
     for (char bracket : brackets) {
-        if (bracket == '<') {
+        if ((bracket == '(') || (bracket == '{') || (bracket == '<')) {
             count++;
-        } else if (bracket == '>') {
-            if (count <= 0)
-                return false;
-            count--;
+        } else if ((bracket == ')') || (bracket == '}') || (bracket == '>')) {
+            if (count > 0)
+                count--;
+            else
+                return false; 
         }
     }
     return count == 0;
