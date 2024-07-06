9deca519983b7e7867a88b147fed1d557f2cc325
beep boop <githubbot@vadim.me>
2024-07-06 13:15:08 +0200

    Solution 28 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vjf9wmxz Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ea3112ef-826d-409a-8f37-6073629e204f.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ea3112ef-826d-409a-8f37-6073629e204f.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ea3112ef-826d-409a-8f37-6073629e204f.cpp:11: ea3112ef-826d-409a-8f37-6073629e204f.cpp: In function ‘int main()’: ea3112ef-826d-409a-8f37-6073629e204f.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ ea3112ef-826d-409a-8f37-6073629e204f.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,17 +1,10 @@
-#include <vector>
-#include <string>
-
-bool issame(vector<string> a,vector<string> b){
-    return (a.size() == b.size()) && std::all_of(a.begin(), a.end(), [&b](const string& s) { return (find(b.begin(), b.end(), s) != b.end()); });
-}
-
-vector<string> filter_by_substring(vector<string> strings, string substring){
-    vector<string> result;
-    for(string s : strings){
-        if(s.find(substring) != string::npos)
-            result.push_back(s);
+bool issame(vector<string> a, vector<string> b) {
+    for (auto s : a) {
+        if (find(b.begin(), b.end(), s) == b.end()) {
+            return false;
+        }
     }
-    return result;
+    return true;
 }
 
 int main() {
