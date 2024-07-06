a6f6a2fd93704a11ac4fd509da3d2b61e31f5bd5
beep boop <githubbot@vadim.me>
2024-07-06 19:43:37 +0200

    Solution 24 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/bn9yd81p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/585408e1-3343-44ab-af8a-529064115 5b4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,3 +1,3 @@
-    def sorted_list_sum(lst):
-        even_words = [word for word in lst if len(str(word)) % 2 == 0]
-        return sorted([int("".join(filter(str.isdigit, word))) if "".join(filter(str.isdigit, word)) != "" else word for word in even_words])
+def sorted_list_sum(lst):
+    even_words = [word for word in lst if len(str(word)) % 2 == 0]
+    return sorted(int("".join(filter(str.isdigit, str(word)))) if "".join(filter(str.isdigit, str(word))).isdigit() else int(word) for word in even_words)
