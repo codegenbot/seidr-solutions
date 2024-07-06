7ebf2386167f071746377d8b400fe65c253d65c6
beep boop <githubbot@vadim.me>
2024-07-06 19:12:55 +0200

    Solution 82 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ygswh6dl Prompt: In file included from dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:19:37: error: ‘string’ was not declared  in this scope    19 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:19:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:19:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -3,10 +3,18 @@
 #include <algorithm>
 
 std::vector<std::string> bf(std::string a, std::string b) {
-    int n = a.size();
-    for(int i=0; i<n/2; ++i){
-        if(a[i] != b[n-i-1])
-            return {"BOF", "NO"};
+    int m = a.length(), n = b.length();
+    for (int i = 0; i <= m - b.length(); i++) {
+        if (a.substr(i, b.length()) == b) return {a.substr(0, i), a.substr(i + b.length())};
     }
-    return {"BOF", "YES"};
+    return {};
+}
+
+bool issame(std::vector<std::string> a, std::vector<std::string> b){
+    for (int i = 0; i < a.size(); i++) {
+        if (find(b.begin(),b.end(),a[i]) == b.end()) {
+            return false;
+        }
+    }
+    return true;
 }
