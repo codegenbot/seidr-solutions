df5e2c1a54ef59ce6feba3198ceaf4b85acf2376
beep boop <githubbot@vadim.me>
2024-07-06 20:04:02 +0200

    Solution 50 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3f2yeli0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/514bd80d-ff8a-44aa-825b-c57cd8e3d 78c.py", line 17, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/514bd80d-ff8a-44aa-825b-c57cd8e3d 78c.py", line 14, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,3 +1,4 @@
+```
 def cycpattern_check(a, b):
     if len(b) == 0:
         return True
@@ -5,6 +6,9 @@ def cycpattern_check(a, b):
         return True
     for i in range(len(a)):
         rotated_a = a[i:] + a[:i]
-        if rotated_a == b:
+        if rotated_a == b or b in rotated_a:
             return True
+    rotated_a = a + a
+    if rotated_a == b or b in rotated_a:
+        return True
     return False
