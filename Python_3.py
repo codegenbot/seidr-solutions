c45797796c975c92e5183572c030ce68450abc87
beep boop <githubbot@vadim.me>
2024-07-06 16:58:11 +0200

    Solution 96 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5c3wvlf8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/39f50b66-a3ef-417b-91c8-1aa954647 349.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_3.py
+++ Python_3.py
@@ -1,12 +1,12 @@
-```
 def calculate_average_grade():
     total = 0
     count = 0
     while True:
-        grades = list(map(float, input("Enter your grades (space-separated): ").split()))
-        if all(0 < grade <= 5 for grade in grades):
-            break
-        else:
+        try:
+            grades = list(map(float, input("Enter your grades (space-separated): ").split()))
+            if all(0 < grade <= 5 for grade in grades):
+                break
+        except ValueError:
             print("Invalid grade! Please enter a positive number.")
     
     for grade in grades:
