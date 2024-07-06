f33993bd1ede1937ab256f4b6c7770259a1ebfe7
beep boop <githubbot@vadim.me>
2024-07-06 05:10:52 +0200

    Solution 36 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tdbrvss0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/16f4aaba-f6f1-4a08-9edc-6fd6eaad2 0ca.py", line 17, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/16f4aaba-f6f1-4a08-9edc-6fd6eaad2 0ca.py", line 14, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,11 +1,8 @@
 ```
-def is_palindrome(string: str) -> bool:
-    return string == string[::-1]
-
 def make_palindrome(string: str) -> str:
-    if string.islower() and string == string[::-1]:
-        return string
-    for i in range(len(string), -1, -1):
-        if not string[:i].islower() or string[:i] != string[:i][::-1]:
-            break
-    return string + string[i:][::-1]
+    if string == string[::-1]:
+        return string + string
+    postfix = ""
+    for char in string:
+        postfix = char + postfix
+    return string + postfix
