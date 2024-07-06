ee01412b9990c57d456a043a745c31fbd762f214
beep boop <githubbot@vadim.me>
2024-07-06 12:10:15 +0200

    Solution 49 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ocp7lybz Prompt:       |                           ^~~~~~ c181c167-cd49-4a13-958e-9a134238a944.cpp:31:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from c181c167-cd49-4a13-958e-9a134238a944.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from c181c167-cd49-4a13-958e-9a134238a944.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c181c167-cd49-4a13-958e-9a134238a944.cpp:31:34: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c181c167-cd49-4a13-958e-9a134238a944.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ c181c167-cd49-4a13-958e-9a134238a944.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -14,8 +14,3 @@ std::vector<int> remove_duplicates(std::vector<int> numbers) {
     std::set<int> seen;
     return {seen.insert(i).second for (int i : numbers)};
 }
-
-int main() {
-    assert(issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 2, 3, 4, 5}));
-    return 0;
-}
