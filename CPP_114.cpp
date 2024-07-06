5c88c18994b9df7ca766455eca23cd627c92b3eb
beep boop <githubbot@vadim.me>
2024-07-06 07:15:04 +0200

    Solution 12 of CPP/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/79ot3hkb Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 4f198d00-0cf8-41db-86c2-71dc66716d88.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_114.cpp
+++ CPP_114.cpp
@@ -1,9 +1,10 @@
+```cpp
 #include <vector>
 using namespace std;
 
-long long minSubArraySum(vector<long long> nums) {
+long long minSubArraySum(long long* nums, int size) {
     long long sum = 0, min_sum = LLONG_MAX;
-    for (int i = 0; i < nums.size(); i++) {
+    for (int i = 0; i < size; i++) {
         sum += nums[i];
         min_sum = min(min_sum, sum);
         if (sum > 0) {
@@ -14,5 +15,5 @@ long long minSubArraySum(vector<long long> nums) {
 }
 
 int main() {
-    assert(minSubArraySum({1, -1}) == -1);
+    assert(minSubArraySum({1, -1}, 2) == -1);
 }
