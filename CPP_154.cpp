eb3aa615734e88d99f3cf253e09cb94a97ba9bb7
beep boop <githubbot@vadim.me>
2024-07-06 10:39:13 +0200

    Solution 1 of CPP/154, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/eywn772u Prompt: Complete the following code given the task description and function signature.

--- CPP_154.cpp
+++ CPP_154.cpp
@@ -1,18 +1,11 @@
+#include <string>
+using namespace std;
+
 bool cycpattern_check(string a, string b) {
-    for(int i = 0; i < a.length(); i++) {
-        string temp = a.substr(i);
-        if(temp.length() >= b.length()) {
-            if(a.find(b) != string::npos || a.find(temp) != string::npos) {
-                return true;
-            }
-        } else {
-            for(int j = 0; j < temp.length(); j++) {
-                temp = temp.substr(j) + temp.substr(0, j);
-                if(a.find(temp) != string::npos) {
-                    return true;
-                }
-            }
-        }
+    for (int i = 0; i < a.size(); i++) {
+        string temp = a.substr(i) + a.substr(0, i);
+        if (temp.find(b) != string::npos)
+            return true;
     }
     return false;
 }
