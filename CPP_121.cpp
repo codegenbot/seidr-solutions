1938a434aad323ad5758af798101ba5dd705f7e0
beep boop <githubbot@vadim.me>
2024-07-06 11:34:24 +0200

    Solution 80 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/duae0477 Prompt:  ‘std’    14 |     std::cout << "Result: " << result << std::endl;       |                                               ^~~~ 34d6eaa6-7cde-4cae-a5a5-30f6e138cc0c.cpp:4:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     3 | #include <assert.h>   +++ |+#include <ostream>     4 | 34d6eaa6-7cde-4cae-a5a5-30f6e138cc0c.cpp: At global scope: 34d6eaa6-7cde-4cae-a5a5-30f6e138cc0c.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 34d6eaa6-7cde-4cae-a5a5-30f6e138cc0c.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~ 34d6eaa6-7cde-4cae-a5a5-30f6e138cc0c.cpp: In function ‘int solutions(std::vector <int>)’: 34d6eaa6-7cde-4cae-a5a5-30f6e138cc0c.cpp:9:1: warning: control reaches end of no n-void function [-Wreturn-type]     9 | }       | ^

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,23 +1,22 @@
 #include <vector>
 #include <initializer_list>
+#include <iostream> 
 #include <assert.h>
-#include <iostream>
-using namespace std;
 
-int solutions(vector<int> lst) {
+int solutions(std::vector<int> lst) {
     if (lst.size() == 0) {
         return -1;
     }
-    int result = 1; 
-    for(int i: lst) {
-        result *= i;
+    int count = 0;
+    for(int i=0; i<lst.size();i++){
+        if(lst[i]%2==0) count++;
     }
-    return result;
+    return count; 
 }
 
 int main() {
-    vector<int> lst = {3, 13, 2, 9};
+    std::vector<int> lst = {3, 13, 2, 9};
     int result = solutions(lst);
-    cout << "Result: " << result << endl;
+    std::cout << "Result: " << result << std::endl;
     return 0;
 }
