afe32370ddc49bc6e4cdf836b8e8e56e6e526da8
beep boop <githubbot@vadim.me>
2024-07-06 03:01:55 +0200

    Solution 57 of Python/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/hy53hw4z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/515d14f5-8227-4e5b-954a-3817342b3 b03.py", line 22, in <module>     check(select_words)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/515d14f5-8227-4e5b-954a-3817342b3 b03.py", line 14, in check     assert select_words("a b c d e f", 1) == [            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: 2nd edge test error: ['a', 'b', 'c', 'd', 'e', 'f']

--- Python_117.py
+++ Python_117.py
@@ -1,9 +1,8 @@
-
-def select_words(s: str, n: int) -> List[str]:
+def select_words(s, n):
     words = s.split()
     if n == 1:
         return [word for word in words if word.islower()]
     else:
         return [
-            word for word in words if len([c for c in word[1:] if c.islower()]) == n
+            word[1:] for word in words if len([c for c in word[1:] if c.islower()]) == n
         ]
