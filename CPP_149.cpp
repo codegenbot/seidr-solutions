1bd62297296e0e63b922e3aac04a1274246906ef
beep boop <githubbot@vadim.me>
2024-07-06 18:31:31 +0200

    Solution 30 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ccu534fs Prompt:    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8eb1dcf3-c7c2-44aa-a784-8df6221a02ec.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8eb1dcf3-c7c2-44aa-a784-8df6221a02ec.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8eb1dcf3-c7c2-44aa-a784-8df6221a02ec.cpp:17: 8eb1dcf3-c7c2-44aa-a784-8df6221a02ec.cpp: In function ‘int main()’: 8eb1dcf3-c7c2-44aa-a784-8df6221a02ec.cpp:27:20: error: ‘sorted_list_sum’ was not  declared in this scope    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 8eb1dcf3-c7c2-44aa-a784-8df6221a02ec.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -5,25 +5,3 @@ bool issame(vector<string> a, vector<string> b) {
     }
     return true;
 }
-
-vector<vector<string>> sorted_list_sum(vector<vector<string>>& lists) {
-    vector<vector<string>> result;
-    
-    for (const auto& lst : lists) {
-        vector<string> sorted = sorted_list_sum(lst);
-        bool found = false;
-
-        for (const auto& res : result) {
-            if (issame(sorted, res)) {
-                found = true;
-                break;
-            }
-        }
-
-        if (!found) {
-            result.push_back(sorted);
-        }
-    }
-    
-    return result;
-}
