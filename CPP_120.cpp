145c93fd699eedbab9bfba0923fab461a7b9eb6a
beep boop <githubbot@vadim.me>
2024-07-06 08:40:09 +0200

    Solution 19 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/a1tfr568 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from ea632c72-5c36-4a5d-acee-f1b628ead5cb.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -9,23 +9,28 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return true;
 }
 
-namespace std {
 std::vector<int> maximum(std::vector<int> arr, int k) {
+    namespace std;
     std::vector<int> result;
     for (int i = 0; i < k; ++i) {
-        int max_val = *std::max_element(arr.begin(), arr.end());
+        int max_val = *max_element(arr.begin(), arr.end());
         result.push_back(max_val);
-        auto it = std::remove(arr.begin(), arr.end(), max_val);
+        auto it = remove(arr.begin(), arr.end(), max_val);
         arr.erase(it, arr.end());
     }
     return result;
-}
+
 }
 
 int main() {
+    namespace std;
+    int k=4;
     int arr[] = {1, 2, 3, -23, 243, -400, 0};
     int n = sizeof(arr)/sizeof(arr[0]);
-    std::vector<int> array (arr, arr+n);
-    assert(issame(maximum(array, 4), ({0, 1, 2, 3})));
-    return 0;
+    vector<int> arr_vec(arr, arr+n);
+    cout << "Result: ";
+    for (int i = 0; i < k; ++i) {
+        cout << maximum(arr_vec, k)[i] << " ";
+    }
+    cout << endl;
 }
