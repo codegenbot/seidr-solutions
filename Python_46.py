dd37b08840d95d5626d0427f204c7ad21e3ad433
beep boop <githubbot@vadim.me>
2024-07-06 03:27:31 +0200

    Solution 4 of Python/46, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/r2pk0pul Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6937e302-c82f-4af5-9d07-e81e419f1 948.py", line 16, in <module>     check(fib4)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6937e302-c82f-4af5-9d07-e81e419f1 948.py", line 13, in check     assert fib4(12) == 386            ^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6937e302-c82f-4af5-9d07-e81e419f1 948.py", line 10, in fib4     sequence.append(sequence[i-1] + sequence[i-2] + sequence[i-3])                     ~~~~~~~~^^^^^ IndexError: list index out of range

--- Python_46.py
+++ Python_46.py
@@ -1,11 +1,11 @@
-
 def fib4(n: int):
     if n <= 3:
         return 0
     elif n == 4:
         return 2
     else:
-        a, b, c = 0, 0, 1
-        for i in range(5, n+1):
-            a, b, c = b, c, a + b + c
-        return c
+        sequence = [0, 0, 2]
+        for i in range(5, n + 1):
+            if i <= n - 3:
+                sequence.append(sequence[i - 1] + sequence[i - 2] + sequence[i - 3])
+        return sequence[n]
