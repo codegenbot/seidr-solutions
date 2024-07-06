6c1d79c8be6802b0f7ccbe91ca47d51dcffb8886
beep boop <githubbot@vadim.me>
2024-07-06 07:58:44 +0200

    Solution 9 of CPP/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vamu4kq4 Prompt: d9610fb5-d2d0-4b74-b0cd-4939b0db5aee.cpp:1:18: error: ‘vector’ was not declared in this scope     1 | int add_elements(vector<int> arr, int k) {       |                  ^~~~~~ d9610fb5-d2d0-4b74-b0cd-4939b0db5aee.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | int add_elements(vector<int> arr, int k) {       |                         ^~~ d9610fb5-d2d0-4b74-b0cd-4939b0db5aee.cpp:1:35: error: expected primary-expressio n before ‘int’     1 | int add_elements(vector<int> arr, int k) {       |                                   ^~~ d9610fb5-d2d0-4b74-b0cd-4939b0db5aee.cpp:1:40: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int add_elements(vector<int> arr, int k) {       |                                        ^ In file included from d9610fb5-d2d0-4b74-b0cd-4939b0db5aee.cpp:11: d9610fb5-d2d0-4b74-b0cd-4939b0db5aee.cpp: In function ‘int main()’: d9610fb5-d2d0-4b74-b0cd-4939b0db5aee.cpp:13:25: error: ‘add_elements’ cannot be used as a function    13 |     assert (add_elements({1}, 1) == 1);       |             ~~~~~~~~~~~~^~~~~~~~

--- CPP_122.cpp
+++ CPP_122.cpp
@@ -1,9 +1,7 @@
 int add_elements(std::vector<int> arr, int k) {
     int sum = 0;
     for (int i = 0; i < min(k, (int)arr.size()); i++) {
-        if (std::to_string(arr[i]).size() <= 2) {
-            sum += arr[i];
-        }
+        sum += arr[i];
     }
     return sum;
 }
