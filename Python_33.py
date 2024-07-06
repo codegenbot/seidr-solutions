4120ce800eb9ae9297106e50cb6fc443b86d66ea
beep boop <githubbot@vadim.me>
2024-07-06 21:43:45 +0200

    Solution 13 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/skztahss Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/74343176-1361-4222-9247-5c5187493 c15.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/74343176-1361-4222-9247-5c5187493 c15.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -2,7 +2,7 @@ def sort_third(l: list):
     result = []
     i = 0
     while i < len(l):
-        sublist = [l[i], l[i+1]] if i + 1 < len(l) else [l[i]]
+        sublist = [l[i], l[i + 1]] if i + 1 < len(l) else [l[i]]
         if len(sublist) == 2:
             third_element = min(sublist, key=lambda x: (x % 3, x))
             result.extend(sorted([y for y in sublist if y % 3 == 0]))[::-1]
