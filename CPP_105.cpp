d416b65d1987e699c90a4e05bb40006fa102df14
beep boop <githubbot@vadim.me>
2024-07-06 06:28:13 +0200

    Solution 18 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3l504wnm Prompt: In file included from /usr/include/c++/11/string:39,                  from 5cb6753d-e0d4-4c76-b7ef-009c0cc59558.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 5cb6753d-e0d4-4c76-b7ef-009c0cc59558.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 5cb6753d-e0d4-4c76-b7ef-009c0cc59558.cpp:52:44: error: ‘b’ was not declared in t his scope    52 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 5cb6753d-e0d4-4c76-b7ef-009c0cc59558.cpp:51: 5cb6753d-e0d4-4c76-b7ef-009c0cc59558.cpp: In function ‘int main()’: 5cb6753d-e0d4-4c76-b7ef-009c0cc59558.cpp:61:20: error: ‘by_length’ was not decla red in this scope    61 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 5cb6753d-e0d4-4c76-b7ef-009c0cc59558.cpp:61:19: error: ‘issame’ cannot be used a s a function    61 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,58 +1,7 @@
 #include <vector>
 #include <string>
-#include <algorithm>
-#include <initializer_list>
 
-bool issame(vector<string>, vector<string>);
-
-vector<string> by_length(vector<int> arr) {
-    vector<int> temp;
-    for (int i : arr) {
-        if (i >= 1 && i <= 9) {
-            temp.push_back(i);
-        }
-    }
-
-    sort(temp.begin(), temp.end());
-    reverse(temp.begin(), temp.end());
-
-    vector<string> result;
-    for (int i : temp) {
-        switch (i) {
-            case 1:
-                result.push_back("One");
-                break;
-            case 2:
-                result.push_back("Two");
-                break;
-            case 3:
-                result.push_back("Three");
-                break;
-            case 4:
-                result.push_back("Four");
-                break;
-            case 5:
-                result.push_back("Five");
-                break;
-            case 6:
-                result.push_back("Six");
-                break;
-            case 7:
-                result.push_back("Seven");
-                break;
-            case 8:
-                result.push_back("Eight");
-                break;
-            case 9:
-                result.push_back("Nine");
-                break;
-        }
-    }
-
-    return result;
-}
-
-bool issame(vector<string> a, vector<string> b) {
+bool issame(vector<string> a, const vector<string>& b) {
     if(a.size() != b.size()) {
         return false;
     }
@@ -62,6 +11,46 @@ bool issame(vector<string> a, vector<string> b) {
             return false;
         }
     }
-    
+
     return true;
+}
+
+vector<string> by_length(const vector<int>& arr) {
+    vector<string> result;
+    vector<int> temp = {0};
+    for (int i : arr) {
+        if(i >= 1 && i <= 9) {
+            switch (i) {
+                case 1:
+                    result.push_back("One");
+                    break;
+                case 2:
+                    result.push_back("Two");
+                    break;
+                case 3:
+                    result.push_back("Three");
+                    break;
+                case 4:
+                    result.push_back("Four");
+                    break;
+                case 5:
+                    result.push_back("Five");
+                    break;
+                case 6:
+                    result.push_back("Six");
+                    break;
+                case 7:
+                    result.push_back("Seven");
+                    break;
+                case 8:
+                    result.push_back("Eight");
+                    break;
+                case 9:
+                    result.push_back("Nine");
+                    break;
+            }
+        }
+    }
+
+    return result;
 }
