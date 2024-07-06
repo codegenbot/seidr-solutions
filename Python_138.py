5e6bec2fc29e83b3bbf382159f43ffc3f33ecf1d
beep boop <githubbot@vadim.me>
2024-07-06 02:41:03 +0200

    Solution 2 of Python/138, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7nilffnh Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/772604d9-0eb7-4797-9908-75e2c8e47 3e8.py", line 10, in <module>     check(is_equal_to_sum_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/772604d9-0eb7-4797-9908-75e2c8e47 3e8.py", line 7, in check     assert is_equal_to_sum_even(16) == True            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_138.py
+++ Python_138.py
@@ -1,9 +1,11 @@
 ```
 def is_equal_to_sum_even(n):
-    for i in range(2, n+1, 2):
-        sum_num = 0
-        for j in range(i, n+1, 2):
-            if sum_num + j == n:
-                return True
-            sum_num += j
+    for i in range(2, n + 1, 2):
+        total = 0
+        for j in range(i):
+            if j % 2 != 0:
+                break
+            total += j
+        if total == n:
+            return True
     return False
