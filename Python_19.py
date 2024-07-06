9899e6b30a4be41d3327fc28e91a7a803886c126
beep boop <githubbot@vadim.me>
2024-07-06 06:21:58 +0200

    Solution 60 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4dw7k43c Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a0d8b731-237d-4485-a390-d41eaecae 004.py", line 23, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a0d8b731-237d-4485-a390-d41eaecae 004.py", line 18, in check     sort_numbers("six five four three two one zero") AssertionError

--- Python_19.py
+++ Python_19.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def sort_numbers(numbers: str) -> str:
     num_dict = {
         "zero": 0,
@@ -11,5 +13,6 @@ def sort_numbers(numbers: str) -> str:
         "eight": 8,
         "nine": 9
     }
-    nums = [num_dict[x] for x in numbers.split() if x in num_dict]
-    return " ".join(map(str, sorted(set(nums))))
+    nums = list(map(lambda x: int(x) if x.isdigit() else num_dict[x], numbers.split()))
+    counter = Counter(nums)
+    return " ".join([k for k, v in sorted(counter.items()) for _ in range(v)])
