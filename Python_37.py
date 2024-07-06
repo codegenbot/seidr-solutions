2062f2608ccf14a9648b879dc01f9e83937df1f3
beep boop <githubbot@vadim.me>
2024-07-06 15:53:38 +0200

    Solution 4 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1fv3kcsh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e62db886-c9f6-432d-abe9-c577b956d 43b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_37.py
+++ Python_37.py
@@ -1,3 +1,5 @@
 def sort_even(l: list):
-    return [x if i % 2 != 0 else min(y for y in l if l.index(y) == (i // 2) * 2)
-            for i, x in enumerate(l)]
+    return [
+        x if i % 2 != 0 else min(y for y in l if (i // 2) * 2 == l.index(y))
+        for i, x in enumerate(l)
+    ]
