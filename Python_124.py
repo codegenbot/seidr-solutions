a2fd67dcce023317c4a0cd1d45c897c7f9a8d286
beep boop <githubbot@vadim.me>
2024-07-06 06:48:36 +0200

    Solution 1 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/8jshm878 Prompt: Complete the following code given the task description and function signature.

--- Python_124.py
+++ Python_124.py
@@ -1,14 +1,14 @@
 ```
 def valid_date(date):
-    day, month, year = date.split('-')
-    if len(day) == 1:
-        day = '0' + day
-    if len(month) == 1:
-        month = '0' + month
+    month, day, year = date.split('-')
+    if len(month) < 1 or len(str(year)) < 4:
+        return False
     if int(month) < 1 or int(month) > 12:
         return False
-    if (int(month) in [1,3,5,7,8,10,12] and int(day) < 1 or int(day) > 31) or \
-       (int(month) in [4,6,9,11] and int(day) < 1 or int(day) > 30) or \
-       (int(month) == 2 and int(day) < 1 or int(day) > 29):
+    days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+    if (int(month) == 2 and int(day) > days_in_month[1]):
         return False
-    return True
+    elif ((int(month) % 2 != 0 and int(month) < 8) or (int(month) % 2 == 0 and int(month) > 7)) and (int(day) > days_in_month[int(month)-1]):
+        return False
+    else:
+        return True
