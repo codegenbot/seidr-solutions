910bca6062f4a38dd4ade456394162db43e5516e
beep boop <githubbot@vadim.me>
2024-07-06 08:09:12 +0200

    Solution 68 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/m798gtj4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0c82355e-2b71-4fc8-ac91-df7725e03 c25.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,7 +1,4 @@
 ```
 def sort_third(l: list):
-    l_copy = l.copy()
-    return [
-        sorted([x for x in l_copy if x % 3 == 0])[::-1].pop(0) if i % 3 == 0 else i 
-        for i in l
-    ]
+    l_copy = [i for i in l]
+    return [i if i % 3 != 0 else min([x for x in l_copy if x % 3 == 0 and x != i], default=None) or None for i in l]
