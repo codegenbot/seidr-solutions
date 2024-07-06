bc59f766a0030f63c71ed87671e08bb6f70dcacf
beep boop <githubbot@vadim.me>
2024-07-06 09:49:45 +0200

    Solution 61 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tcslkz1m Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b675e36-b199-4cb6-a5dc-00cf29489 d5b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,8 +1,3 @@
-def double_the_difference(numbers):
-    first_max = max(set([abs(num) for num in numbers]))
-    second_max = sorted({num for num in numbers})[-2]
-
-    if first_max - second_max > 0:
-        return (first_max - second_max) * 2
-    else:
-        return "No difference to double"
+def double_the_difference(lst):
+    diff = max(lst) - min(lst)
+    return diff * 2
