c88b12f61aa7cbfe8f69c08d71d7a6f22fa27ffc
beep boop <githubbot@vadim.me>
2024-07-06 19:06:17 +0200

    Solution 97 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/aagr2av9 Prompt:       |             ^~~~~~ 75c7730f-f1f1-44ce-aa8a-3e75f21d1705.cpp:20:20: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 75c7730f-f1f1-44ce-aa8a-3e75f21d1705.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 75c7730f-f1f1-44ce-aa8a-3e75f21d1705.cpp:20:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 75c7730f-f1f1-44ce-aa8a-3e75f21d1705.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 75c7730f-f1f1-44ce-aa8a-3e75f21d1705.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 75c7730f-f1f1-44ce-aa8a-3e75f21d1705.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -7,10 +7,9 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 }
 
 std::vector<int> maximum(std::vector<int> arr, int k) {
-    using namespace std;
-    vector<int> result;
+    std::vector<int> result;
     for (int i = 0; i < k; ++i) {
-        auto it = max_element(arr.begin(), arr.end());
+        auto it = std::max_element(arr.begin(), arr.end());
         result.push_back(*it);
         arr.erase(it);
     }
