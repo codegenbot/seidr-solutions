49738fdbe371055dd6793db493e576580a823801
beep boop <githubbot@vadim.me>
2024-07-06 04:44:43 +0200

    Solution 72 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/qwgfrn7h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/462f0f9d-73e1-4a24-b2d5-a611b03f2 f8e.py", line 21, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/462f0f9d-73e1-4a24-b2d5-a611b03f2 f8e.py", line 17, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,10 +2,10 @@ def encode(message):
     encoded_message = ""
     for char in message:
         if char.isalpha():
-            ordinal = ord(char) - ord("A")
-            if ordinal < 0:
-                ordinal += 26
-            encoded_char = chr(ordinal % 26 + ord("A"))
+            if char.islower():
+                encoded_char = chr(ord("A") + (ord(char) - ord("a")) % 26)
+            elif char.isupper():
+                encoded_char = chr(ord("a") + (ord(char) - ord("A")) % 26)
         else:
             encoded_message += char
     return encoded_message
