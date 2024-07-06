f501a6003aa1ebac2b4276e92336d841c8a91350
beep boop <githubbot@vadim.me>
2024-07-06 15:20:52 +0200

    Solution 51 of Python/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9bklotpg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ad0ecfcf-9359-49d7-8b86-704c08232 684.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_7.py
+++ Python_7.py
@@ -1,9 +1,9 @@
+"""
 def filter_by_substring(strings, substring):
     return [s for s in strings if substring in s]
 
 if __name__ == "__main__":
-    input_str = input("Enter a list of strings (space separated):")
-    strings = [word for word in input_str.split()]
+    strings = [word for word in input("Enter a list of strings (space separated):").split()]
     while True:
         substring = input("Enter a substring to search (or press enter to quit):")
         if not substring:
