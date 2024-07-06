113f911ad46adbbdf31459f90414072a1141b50e
beep boop <githubbot@vadim.me>
2024-07-06 17:46:32 +0200

    Solution 98 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r6wjanum Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f07b110-7a94-4d38-bc04-5fc69110a 99e.py", line 17, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f07b110-7a94-4d38-bc04-5fc69110a 99e.py", line 12, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,10 +1,8 @@
 def sort_third(input_list):
     result = []
     for i in range(0, len(input_list), 3):
-        current_group = [int(x) for x in input_list[i:i+3]]
-        if len(current_group) < 3:
-            result.append(tuple(sorted(current_group)))
-        else:
-            result.append(tuple(sorted(current_group)))
-
-    return tuple(sorted(result))
+        current_group = tuple(sorted([int(x) for x in input_list[i:i+3]]))
+        result.append(current_group)
+    if len(input_list) % 3 != 0:
+        result.append(tuple(sorted([int(x) for x in input_list[-(len(input_list) % 3):]])))
+    return tuple(sorted(map(tuple, result)))
