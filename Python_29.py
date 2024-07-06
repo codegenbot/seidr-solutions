e524b0535a2ad0ab7b03ffd5373cb6258ac6ecab
beep boop <githubbot@vadim.me>
2024-07-06 03:07:52 +0200

    Solution 4 of Python/29, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/igizwsu5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/362f6fb5-80f4-48d2-afab-42cf08029 cf8.py", line 1, in <module>     def filter_by_prefix(strings: List[str], prefix: str) -> List[str]:                                   ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_29.py
+++ Python_29.py
@@ -1,2 +1,2 @@
-def filter_by_prefix(strings: List[str], prefix: str) -> List[str]:
-    return [s for s in strings if s.startswith(prefix)]
+def filter_by_prefix(strings: list[str], prefix: str) -> list[str]:
+    return [string for string in strings if string.startswith(prefix)]
