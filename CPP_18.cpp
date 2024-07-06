f436c887a4370ca34109adb810bccd846777a288
beep boop <githubbot@vadim.me>
2024-07-06 13:53:07 +0200

    Solution 32 of CPP/18, ratio 0.75 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mmcpqrpl Prompt: 7f75166d-562d-46ec-8ed3-08efe7cc2182.cpp:1:20: error: ‘string’ was not declared in this scope     1 | int how_many_times(string str, string substring) {       |                    ^~~~~~ 7f75166d-562d-46ec-8ed3-08efe7cc2182.cpp:1:32: error: ‘string’ was not declared in this scope     1 | int how_many_times(string str, string substring) {       |                                ^~~~~~ 7f75166d-562d-46ec-8ed3-08efe7cc2182.cpp:1:48: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int how_many_times(string str, string substring) {       |                                                ^ In file included from 7f75166d-562d-46ec-8ed3-08efe7cc2182.cpp:11: 7f75166d-562d-46ec-8ed3-08efe7cc2182.cpp: In function ‘int main()’: 7f75166d-562d-46ec-8ed3-08efe7cc2182.cpp:13:27: error: ‘how_many_times’ cannot b e used as a function    13 |     assert (how_many_times("john doe", "john") == 1);       |             ~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -1,9 +1,11 @@
+#include <string>
+using namespace std;
 int how_many_times(string str, string substring) {
     int count = 0;
     size_t pos = 0;
     while ((pos = str.find(substring)) != string::npos) {
         count++;
-        pos += substring.size();
+        str.erase(0, pos + substring.size());
     }
     return count;
 }
