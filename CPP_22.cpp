ba8e50898588fda97abeb5ae9bd788dc03213178
beep boop <githubbot@vadim.me>
2024-07-06 02:14:00 +0200

    Solution 31 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/brmn1pem Prompt: 004b60bc-adde-41a5-9453-07ef0d7d45d4.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,11 +1,2 @@
 #include <boost/optional.hpp>
 #include <vector>
-
-std::vector<int> filter_integers(std::list<boost::any> values) {
-    std::vector<int> result;
-    for (const auto& value : values) {
-        if(boost::any_cast<boost::optional<int>>(value).has_value()){
-            result.push_back(*boost::any_cast<boost::optional<int>>(value));
-        }
-    }
-    return result;
