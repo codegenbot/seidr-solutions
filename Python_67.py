8ea88d234212d846c723ee1f06cb54740286fb09
beep boop <githubbot@vadim.me>
2024-07-06 09:22:22 +0200

    Solution 2 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/o5e8rbih Prompt: Complete the following code given the task description and function signature.

--- Python_67.py
+++ Python_67.py
@@ -1,4 +1,5 @@
 
 def fruit_distribution(s, n):
-    apples, oranges = map(int, s.split())
-    return n - apples - oranges
+    num_apples = int(s.split()[0])
+    num_oranges = int(s.split()[1].replace("and", ""))
+    return n - num_apples - num_oranges
