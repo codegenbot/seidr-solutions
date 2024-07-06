86ac2208b6167b176a5c7a0656e0c0566c51048e
beep boop <githubbot@vadim.me>
2024-07-06 19:24:32 +0200

    Solution 44 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/o9ngt94r Prompt: it must return True for input t|t&t&t|f&t&t|f|t|t, but it returns Enter a Boolean expression: True.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <string>
 #include <iostream>
 
@@ -6,15 +7,16 @@ using namespace std;
 bool solveBoolean(string s) {
     bool t = true;
     bool f = false;
-    bool res = t;
+    bool res = true;
 
     for (int i = 0; i < s.length(); i++) {
         if (s[i] == 't') {
-            res = (s[i + 1] == '&') ? (res & t) : (res | t);
-            i += (s[i + 1] == '|') ? 2 : 1;
+            res = (s[i+1] == '&') ? (res & t) : (res | t);
         } else if (s[i] == 'f') {
-            res = (s[i + 1] == '&') ? (res & f) : (res | f);
-            i += (s[i + 1] == '|') ? 2 : 1;
+            res = (s[i+1] == '&') ? (res & f) : (res | f);
+        } else if (s[i] == '|') {
+            if (i > 0 && s[i-1] == 't') t = f;
+            if (i < s.length() - 1 && s[i+1] == 't') f = true;
         }
     }
 
