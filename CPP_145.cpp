e48a1166dfe7325b5c7e48cc5cb4f89863cb2ab0
beep boop <githubbot@vadim.me>
2024-07-06 13:34:54 +0200

    Solution 82 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6h0099y8 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 2764132a-20bb-476f-afad-8e472697eaec.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,29 +1,11 @@
-int compare(int a, int b) {
-    if (a == b) return 0;
-    long sumA = 0, sumB = 0;
-    bool isANeg = false, isBNeg = false;
+```
+using namespace std;
 
-    std::string strA = std::to_string(a);
-    std::string strB = std::to_string(b);
+#include <algorithm>
+#include <vector>
 
-    if (strA[0] == '-') {
-        isANeg = true;
-        for (char c : strA.substr(1)) sumA += c - '0';
-    } else {
-        for (char c : strA) sumA += c - '0';
-    }
-
-    if (strB[0] == '-') {
-        isBNeg = true;
-        for (char c : strB.substr(1)) sumB += c - '0';
-    } else {
-        for (char c : strB) sumB += c - '0';
-    }
-
-    if ((isANeg && !isBNeg) || (!isANeg && isBNeg))
-        return -1;
-    if (sumA != sumB)
-        return sumA - sumB > 0 ? 1 : -1;
-    return distance(nums.begin(), find(nums.begin(), nums.end(), a)) -
-           distance(nums.begin(), find(nums.begin(), nums.end(), b));
+std::vector<int> order_by_points(std::vector<int> nums) {
+    std::vector<int> res = nums;
+    sort(res.begin(), res.end());
+    return res;
 }
