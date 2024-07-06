56c2f73af072c34b3c47b723139afb54944973df
beep boop <githubbot@vadim.me>
2024-07-06 17:26:39 +0200

    Solution 36 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/bbi8kb69 Prompt:    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ edda7fb0-66f4-4fbd-9f57-f675e4339fa3.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ edda7fb0-66f4-4fbd-9f57-f675e4339fa3.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from edda7fb0-66f4-4fbd-9f57-f675e4339fa3.cpp:22: edda7fb0-66f4-4fbd-9f57-f675e4339fa3.cpp: In function ‘int main()’: edda7fb0-66f4-4fbd-9f57-f675e4339fa3.cpp:32:20: error: ‘count_up_to’ was not dec lared in this scope    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ edda7fb0-66f4-4fbd-9f57-f675e4339fa3.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,3 +1,4 @@
+```cpp
 vector<int> count_up_to(int n) {
     vector<int> primes;
     for (int i = 2; i <= n; ++i) {
@@ -18,3 +19,28 @@ vector<int> count_up_to(int n) {
     }
     return primes;
 }
+
+vector<int> count_up_to(int n) {
+    vector<int> primes;
+    for (int i = 2; i <= n; ++i) {
+        bool isPrime = true;
+        if(i == 2)
+            primes.push_back(i);
+        else if(i > 2) {
+            for (int j = 2; j * j <= i; ++j) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                primes.push_back(i);
+            }
+        }
+    }
+    return primes;
+}
+
+vector<int> expected = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+vector<int> actual = count_up_to(101);
+assert(actual == expected);
