6729d3299a01e362ad83b602a1394ae4cbf67362
beep boop <githubbot@vadim.me>
2024-07-06 16:45:03 +0200

    Solution 59 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vnxadr9l Prompt: 53643bce-45c3-43ca-9d65-055eb6d097f5.cpp:1:1: error: expected unqualified-id bef ore ‘while’     1 | while ((pos = S.find("I", pos)) != string::npos) {       | ^~~~~ In file included from 53643bce-45c3-43ca-9d65-055eb6d097f5.cpp:8: 53643bce-45c3-43ca-9d65-055eb6d097f5.cpp: In function ‘int main()’: 53643bce-45c3-43ca-9d65-055eb6d097f5.cpp:10:13: error: ‘is_bored’ was not declar ed in this scope    10 |     assert (is_bored("You and I are going for a walk") == 0);       |             ^~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,1 +1,8 @@
+```cpp
 int pos;
+while ((pos = S.find("I", pos)) != string::npos) {
+    if (S[pos] == 'I' && (S[pos + 1] == '.' || S[pos + 1] == '?' || S[pos + 1] == '!')) {
+        count++;
+    }
+    pos = S.find("I", pos);
+}
