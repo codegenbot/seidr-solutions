a6c8d1ce2cc1b33dedfb1e82f9879202e99dda56
beep boop <githubbot@vadim.me>
2024-07-06 20:57:24 +0200

    Solution 94 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7aum6t7i Prompt: 20 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c953483a-4966-4bfd-b200-676766a1b 13f.py", line 60, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c953483a-4966-4bfd-b200-676766a1b 13f.py", line 30, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,4 +1,3 @@
-```
 def sum_squares(lst):
     total_sum = 0
     for element in lst:
@@ -6,14 +5,16 @@ def sum_squares(lst):
             for key, value in element.items():
                 if isinstance(value, (int, float)):
                     total_sum += abs(value) ** 2
-                elif isinstance(value, list) and not isinstance(value[0], list) and value:
-                    total_sum += sum([x**2 for x in value])
-        elif isinstance(element, list) and not isinstance(element[0], list) and element:
+                elif isinstance(value, list):
+                    total_sum += sum([x**2 for x in sum_squares(value)])
+        elif isinstance(element, list):
             total_sum += sum([x**2 for x in element])
         elif isinstance(element, (int, float)):
             total_sum += abs(element) ** 2
         else:
-            raise ValueError(f"Invalid input '{element}' found. Input must contain only numbers.")
+            raise ValueError(
+                f"Invalid input '{element}' found. Input must contain only numbers."
+            )
     return total_sum
 
 
