b29ef288d1900c8a44c4e9ae5b17aabe712bf503
beep boop <githubbot@vadim.me>
2024-07-06 19:24:29 +0200

    Solution 7 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7aum6t7i Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c7f8f8c6-f444-4e8f-bf2d-cb651542d c7b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -4,11 +4,6 @@ def sum_squares(lst):
         raise ValueError("Input must be a non-empty list")
     
     total_sum = 0
-    for i in range(len(lst)):
-        if i % 3 == 0 and i % 4 == 0: 
-            total_sum += lst[i] ** 2
-        elif i % 4 == 0:
-            total_sum += lst[i] ** 3
-        else:
-            total_sum += lst[i] ** 2
+    for num in lst:
+        total_sum += num ** 2
     return total_sum
