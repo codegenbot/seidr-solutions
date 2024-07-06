11de39eb3b8f8300ebb142338efe7c784979ec07
beep boop <githubbot@vadim.me>
2024-07-06 20:33:13 +0200

    Solution 16 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/x8mq9nzp Prompt: f1682b16-286a-4c48-a47b-b941e6c3be47.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,56 +1,53 @@
+#include <iostream>
+#include <string>
 using namespace std;
 
 int bowlingScore(string s) {
     int score = 0;
     int currentRolls = 0;
-    vector<int> rolls;
+    string rolls = "";
 
     for (char c : s) {
         if (c == '/') {
-            if (currentRolls < 2) {
-                if (rolls.back() + 10 - '0' > 10) {
-                    score += 10;
-                } else {
-                    score += rolls.back() + 10 - '0';
+            if (currentRolls == 1) {
+                int roll = 0;
+                for (int i = 0; i < rolls.length(); i++) {
+                    roll = roll * 10 + rolls[i] - '0';
                 }
+                score += roll;
                 currentRolls = 0;
+            } else if (currentRolls == 2) {
+                int roll = 0;
+                for (int i = 0; i < 2; i++) {
+                    roll = roll * 10 + rolls[i] - '0';
+                }
+                score += roll;
+                currentRolls = 0;
+            }
+            rolls = "";
+        } else {
+            if (currentRolls < 2) {
+                rolls.push_back(c);
+                currentRolls++;
             } else {
                 int roll = 0;
-                for (int i = 0; c != '/'; i++) {
-                    c = s[i];
+                for (int i = 0; i < 2; i++) {
                     roll = roll * 10 + c - '0';
                 }
                 score += roll;
                 currentRolls = 1;
-            }
-        } else {
-            rolls.push_back(c - '0');
-            if (currentRolls < 2) {
-                currentRolls++;
-            } else {
-                int roll = 0;
-                for (int i = 0; i < 2 && s[i] != '/'; i++) {
-                    roll = roll * 10 + s[i] - '0';
-                }
-                score += roll;
-                currentRolls = 1;
+                rolls = "";
             }
         }
     }
 
-    if (currentRolls == 2) {
-        int roll = 0;
-        for (int i = 0; i < 2; i++) {
-            roll = roll * 10 + s[i] - '0';
-        }
-        score += roll;
-    } else if (currentRolls > 1) {
+    if (currentRolls > 0) {
         int roll = 0;
         for (int i = 0; i < currentRolls; i++) {
-            roll = roll * 10 + rolls.back() - '0';
-            rolls.pop_back();
+            roll = roll * 10 + rolls[i] - '0';
         }
         score += roll;
     }
 
     return score;
+}
