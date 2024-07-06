2fca9d31a814fc298c0f12c9fa606873a2bdcf0f
beep boop <githubbot@vadim.me>
2024-07-06 03:34:15 +0200

    Solution 72 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/kwapk43d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3d28491-ff86-44be-a166-2fc189653 138.py", line 12, in <module>     check(digits)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3d28491-ff86-44be-a166-2fc189653 138.py", line 9, in check     assert digits(2468) == 0            ^^^^^^^^^^^^^^^^^ AssertionError

--- Python_131.py
+++ Python_131.py
@@ -1,6 +1,7 @@
-
 def digits(n):
     if n < 0:
-        return max([int(digit) for digit in str(-n)])
+        return -1
+    elif n < 10:
+        return int(str(n)[0])
     else:
         return max([int(digit) for digit in str(n)])
