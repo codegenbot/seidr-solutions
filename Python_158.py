e4fcda28080134f09b8600c7f6d23fed842a629c
beep boop <githubbot@vadim.me>
2024-07-06 23:13:39 +0200

    Solution 11 of Python/158, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/8gid1df1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6ee40f05-0e88-49b9-8c50-2618df1e9 ffa.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_158.py
+++ Python_158.py
@@ -1,10 +1,9 @@
-```
 def find_max(words):
     max_word = ""
     max_len = 0
     for word in words:
         unique_chars = set(word)
-        if len(unique_chars) > max_len or (len(unique_chars) == max_len and word < max_word):
+        if len(unique_chars) > max_len or (len(unique_chars) == max_len and max_word if max_len > len(unique_chars) else word) > word:
             max_word = word
             max_len = len(unique_chars)
     return max_word
