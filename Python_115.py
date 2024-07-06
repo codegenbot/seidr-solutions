efad7fe6b78df758c000cd7c5e2d0f31144ba5a3
beep boop <githubbot@vadim.me>
2024-07-06 19:28:04 +0200

    Solution 27 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/b3zlor4d Prompt: b/python3.11/site-packages/programlib/programs/dd08ceed-2f3c-40dd-a8bc-7956d7f5a 3c2.py", line 10, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dd08ceed-2f3c-40dd-a8bc-7956d7f5a 3c2.py", line 6, in max_fill     return sum(1 for _ in (math.ceil(sum(row) / capacity) for row in rows)) // c apacity            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dd08ceed-2f3c-40dd-a8bc-7956d7f5a 3c2.py", line 6, in <genexpr>     return sum(1 for _ in (math.ceil(sum(row) / capacity) for row in rows)) // c apacity               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dd08ceed-2f3c-40dd-a8bc-7956d7f5a 3c2.py", line 6, in <genexpr>     return sum(1 for _ in (math.ceil(sum(row) / capacity) for row in rows)) // c apacity                                      ^^^^^^^^ TypeError: 'int' object is not iterable

--- Python_115.py
+++ Python_115.py
@@ -1,4 +1,5 @@
-```
+import math
+
+
 def max_fill(grid, capacity):
-    rows = [min(c for c in row) for row in grid]
-    return math.ceil(sum(min(row) for row in rows) / capacity)
+    return math.ceil(sum(sum(row) for row in grid) / capacity)
