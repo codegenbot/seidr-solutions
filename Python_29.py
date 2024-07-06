c1bcbfd9dd4826483118a348a432bd86664c2c95
beep boop <githubbot@vadim.me>
2024-07-06 06:14:56 +0200

    Solution 12 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/6je7dbdd Prompt: Complete the following code given the task description and function signature.

--- Python_29.py
+++ Python_29.py
@@ -1,3 +1,3 @@
 
 def filter_by_prefix(strings: List[str], prefix: str) -> List[str]:
-    return [string for string in strings if string.startswith(prefix)]
+    return list(filter(lambda s: s.startswith(prefix), strings))
