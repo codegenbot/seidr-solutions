9e7399cbde9dda4362d0f08ea70c83946032c157
beep boop <githubbot@vadim.me>
2024-07-06 03:54:07 +0200

    Solution 8 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/kh7c2g1m Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f004340-7be9-4440-bab5-8085d334f 9bd.py", line 11, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f004340-7be9-4440-bab5-8085d334f 9bd.py", line 8, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f004340-7be9-4440-bab5-8085d334f 9bd.py", line 3, in fruit_distribution     apples, oranges = map(int, s.split())     ^^^^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: 'apples'

--- Python_67.py
+++ Python_67.py
@@ -1,5 +1,4 @@
-def fruit_distribution(s):
-    # use regular expressions to extract numbers from the input string
-    apples = int(re.search(r'\d+', s).group())
-    oranges = int(re.search(r'\d+', s).group())
-    return apples + oranges
+
+def fruit_distribution(s, n):
+    apples, oranges = map(int, re.findall(r"\d+", s))
+    return n - apples - oranges
