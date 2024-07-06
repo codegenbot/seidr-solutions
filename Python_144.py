c22c6ff157e69632c53aa9724baf961a0f25ed9f
beep boop <githubbot@vadim.me>
2024-07-06 03:02:31 +0200

    Solution 12 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lb1rqho5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/09c254f7-6581-4f03-b1bf-6389f5dbf 61c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_144.py
+++ Python_144.py
@@ -1,6 +1,7 @@
+```
 def simplify(x, n):
-    x_num, x_denom = map(int, x.split("/"))
-    n_num, n_denom = map(int, n.split("/"))
+    x_num, x_denom = map(int, x.split('/'))
+    n_num, n_denom = map(int, n.split('/'))
 
     def gcd(a, b):
         while b:
@@ -10,7 +11,7 @@ def simplify(x, n):
     common = gcd(x_denom, n_denom)
     new_x_num = x_num // common
     new_n_num = n_num // common
-
+    
     if x_num // common / (n_denom // common) == n_num // common / x_denom // common:
         return True
     else:
