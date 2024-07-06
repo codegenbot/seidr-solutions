9d13876ef079e3925e8c543c7a87dd6e38bdeb57
beep boop <githubbot@vadim.me>
2024-07-06 18:18:47 +0200

    Solution 50 of Python/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0kwfpmzd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/76e880e6-7329-4648-a4c8-c50f8c581 a55.py", line 17, in <module>     check(fibfib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/76e880e6-7329-4648-a4c8-c50f8c581 a55.py", line 14, in check     assert fibfib(14) == 927            ^^^^^^^^^^^^^^^^^ AssertionError

--- Python_63.py
+++ Python_63.py
@@ -9,5 +9,6 @@ def fibfib(n: int):
     else:
         dp = [0, 1]
         for i in range(2, n + 1):
-            dp.append(dp[i-1] + dp[i-2])
-        return dp[-1]
+            if i <= n:
+                dp.append(dp[i-1] + dp[i-2])
+        return sum(dp[-n:])
