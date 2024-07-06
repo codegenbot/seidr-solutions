193b0e7fefd63c8c5e2181afe656c19bcc62ca34
beep boop <githubbot@vadim.me>
2024-07-06 16:03:11 +0200

    Solution 78 of CPP/79, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/snvr445e Prompt: /usr/include/c++/11/bits/basic_string.tcc:1181:35: error: ‘const class std::__cx x11::basic_string<char>’ has no member named ‘size’  1181 |       __str.reserve(__len + __rhs.size());       |                             ~~~~~~^~~~ In file included from /usr/include/c++/11/string:55,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from f3cefd61-2dad-42ee-b9e1-dc73222daca0.cpp:2: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -6,9 +6,9 @@ std::string decimal_to_binary(int decimal){
     std::string binary = "";
     while(decimal > 0){
         if(decimal % 2 == 0)
-            binary.insert(0,"0");
-        else
-            binary.insert(0,"1");
+            binary.insert(std::move(binary.begin()),"0");  
+        else 
+            binary.insert(std::move(binary.begin()), "1");
         decimal /= 2;
     }
     return "db" + binary + "db";
