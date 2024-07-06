bc3ac1a4161ff993da6af1a3c16e84da790014cb
beep boop <githubbot@vadim.me>
2024-07-06 17:56:04 +0200

    Solution 2 of CPP/139, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/juwfwl31 Prompt: Complete the following code given the task description and function signature.

--- CPP_139.cpp
+++ CPP_139.cpp
@@ -1,17 +1,11 @@
-#include <stdio.h>
-using namespace std;
-
 long long special_factorial(int n){
-    long long res = 1;
-    for (int i = 1; i <= n; ++i) {
-        res *= fact(i);
+    long long result = 1;
+    for(int i = 1; i <= n; ++i){
+        result *= 1LL * i;
+        if(i > 1) {
+            for(int j = 2; j <= i; ++j)
+                result *= 1LL * j;
+        }
     }
-    return res;
-}
-
-long long fact(int n){
-    if(n == 0 || n == 1)
-        return 1;
-    else
-        return n * fact(n - 1);
+    return result;
 }
