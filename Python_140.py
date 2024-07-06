c85f4cf4bc9f076b82ceccd85f2dbcac3bbbe34f
beep boop <githubbot@vadim.me>
2024-07-06 05:07:52 +0200

    Solution 71 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ynbb4504 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f9f2965-2217-4f94-82b7-ad4cdf9f9 6a1.py", line 12, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f9f2965-2217-4f94-82b7-ad4cdf9f9 6a1.py", line 8, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple"     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f9f2965-2217-4f94-82b7-ad4cdf9f9 6a1.py", line 2, in fix_spaces     text = re.sub(r"\s+", "_", text)            ^^ NameError: name 're' is not defined

--- Python_140.py
+++ Python_140.py
@@ -1,3 +1,4 @@
+# Write a Python function that takes in a string and returns a new string where all consecutive spaces are compressed to a single space
 def fix_spaces(text):
     text = re.sub(r"\s+", "_", text)
     return text.replace(" ", "")
