27a35aeaff9e5e8f20ba5d58d7e72b2638624313
beep boop <githubbot@vadim.me>
2024-07-06 06:53:52 +0200

    Solution 16 of CPP/110, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vxmdkmc9 Prompt: c6cccae5-e2aa-4a0a-bda5-0d5c04e0968d.cpp:3:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     3 | string exchange(vector<int> lst1, vector<int> lst2) {       | ^~~~~~       | stdin In file included from c6cccae5-e2aa-4a0a-bda5-0d5c04e0968d.cpp:12: c6cccae5-e2aa-4a0a-bda5-0d5c04e0968d.cpp: In function ‘int main()’: c6cccae5-e2aa-4a0a-bda5-0d5c04e0968d.cpp:14:13: error: ‘exchange’ was not declar ed in this scope    14 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,8 +1,12 @@
+#include <vector>
+#include <string>
+using namespace std;
+
 string exchange(vector<int> lst1, vector<int> lst2) {
     int oddCount = 0;
     for (int num : lst1) {
         if (num % 2 != 0)
             oddCount++;
     }
-    return oddCount == 0 ? "YES" : "NO";
+    return to_string(oddCount == 0) + "YES";
 }
