ce4431fa196cc9ea9bf6e302ca7b42bab55d0932
beep boop <githubbot@vadim.me>
2024-07-06 15:45:22 +0200

    Solution 31 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7bimcvpy Prompt:                  from /usr/include/c++/11/algorithm:74,                  from e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp:16: e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp: In function ‘int main()’: e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp:26:20: error: ‘common’ was not declared  in this scope    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,8 +1,8 @@
 #include <set>
 #include <algorithm>
 
-bool issame(int x1, int x2) {
-    return x1 == x2;
+bool issame(int a) {
+    return true;
 }
 
 vector<int> common(vector<int> l1, vector<int> l2) {
@@ -18,37 +18,26 @@ vector<int> common(vector<int> l1, vector<int> l2) {
 }
 
 int main() {
-    int n1, m1;
-    cout << "Enter the number of elements in list 1: ";
+    int n1, n2;
+    cout << "Enter the size of first list: ";
     cin >> n1;
-
+    cout << "Enter the elements of first list: ";
     vector<int> l1(n1);
-    for (int i = 0; i < n1; i++) {
-        cout << "Enter element " << i+1 << ": ";
+    for (int i = 0; i < n1; ++i)
         cin >> l1[i];
-    }
-
-    int n2, m2;
-    cout << "Enter the number of elements in list 2: ";
+    
+    cout << "Enter the size of second list: ";
     cin >> n2;
-
+    cout << "Enter the elements of second list: ";
     vector<int> l2(n2);
-    for (int i = 0; i < n2; i++) {
-        cout << "Enter element " << i+1 << ": ";
+    for (int i = 0; i < n2; ++i)
         cin >> l2[i];
-    }
-
-    vector<int> result = common(l1, l2);
-
-    if(result.size() == 0) {
-        cout << "The lists have no elements in common." << endl;
-    } else {
-        cout << "Common elements: ";
-        for (int i : result) {
-            cout << i << " ";
-        }
-        cout << endl;
-    }
-
+    
+    vector<int> common_list = common(l1, l2);
+    cout << "Common elements in two lists are: ";
+    for(int x : common_list) 
+        cout << x << " ";  
+    cout << endl;
+    
     return 0;
 }
