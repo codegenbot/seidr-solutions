df58fd6e097420da7990846b18dc4f24df6f864b
beep boop <githubbot@vadim.me>
2024-07-06 16:09:41 +0200

    Solution 8 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l4ahvohl Prompt:       |                    ^~~ dd734449-32fa-48dd-ae95-064342ededb1.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ dd734449-32fa-48dd-ae95-064342ededb1.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ dd734449-32fa-48dd-ae95-064342ededb1.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from dd734449-32fa-48dd-ae95-064342ededb1.cpp:18: dd734449-32fa-48dd-ae95-064342ededb1.cpp: In function ‘int main()’: dd734449-32fa-48dd-ae95-064342ededb1.cpp:28:20: error: ‘make_a_pile’ was not dec lared in this scope    28 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ dd734449-32fa-48dd-ae95-064342ededb1.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,16 +1,34 @@
 #include <vector>
 
-std::vector<int> make_a_pile(int n) {
-    std::vector<int> pile;
+bool issame(vector<int> a, vector<int> b);
+
+vector<int> make_a_pile(int n) {
+    vector<int> pile;
     int stones = 1;
     for (int i = 0; i < n; i++) {
-        if ((n % 2) == 1) { // n is odd
+        if ((n % 2) == 1) { 
             pile.push_back(stones);
             stones += 2;
-        } else { // n is even
+        } else { 
             pile.push_back(stones);
             stones += 1;
         }
     }
     return pile;
+}
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));
 }
