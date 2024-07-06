46f4da6a600e88232f166478e36d3d64397bfb22
beep boop <githubbot@vadim.me>
2024-07-06 15:55:03 +0200

    Solution 16 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q9johqny Prompt: In file included from f1568aa0-a3a1-4c3b-a0f1-494d859dc989.cpp:22: f1568aa0-a3a1-4c3b-a0f1-494d859dc989.cpp: In function ‘int main()’: f1568aa0-a3a1-4c3b-a0f1-494d859dc989.cpp:32:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    32 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,4 +1,4 @@
-```cpp
+```
 #include <algorithm>
 using namespace std;
 
@@ -6,12 +6,23 @@ vector<int> strange_sort_vector(vector<int> lst) {
     vector<int> result;
     if (lst.empty()) return result;
 
-    vector<int> min_max;
     for (int num : lst) {
-        if (num == *min_element(lst.begin(), lst.end())) {
-            result.push_back(num);
-            lst.erase(remove(lst.begin(), lst.end(), num), lst.end());
-        } else if (num == *max_element(lst.begin(), lst.end())) {
+        if (count(lst.begin(), lst.end(), *min_element(lst.begin(), lst.end())) > 1) {
+            for (int i = 0; i < count(lst.begin(), lst.end(), *min_element(lst.begin(), lst.end())); i++) {
+                result.push_back(*min_element(lst.begin(), lst.end()));
+                lst.erase(remove(lst.begin(), lst.end(), *min_element(lst.begin(), lst.end())), lst.end());
+            }
+        } else if (count(lst.begin(), lst.end(), *max_element(lst.begin(), lst.end())) > 1) {
+            for (int i = 0; i < count(lst.begin(), lst.end(), *max_element(lst.begin(), lst.end())); i++) {
+                result.push_back(*max_element(lst.begin(), lst.end()));
+                lst.erase(remove(lst.begin(), lst.end(), *max_element(lst.begin(), lst.end())), lst.end());
+            }
+        } else if (count(lst.begin(), lst.end(), num) > 1) {
+            for (int i = 0; i < count(lst.begin(), lst.end(), num); i++) {
+                result.push_back(num);
+                lst.erase(remove(lst.begin(), lst.end(), num), lst.end());
+            }
+        } else {
             result.push_back(num);
             lst.erase(remove(lst.begin(), lst.end(), num), lst.end());
         }
@@ -19,5 +30,3 @@ vector<int> strange_sort_vector(vector<int> lst) {
 
     return result;
 }
-
-assert(strange_sort_vector({111111}) == vector<int>{111111});
