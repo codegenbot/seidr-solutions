3999e538c4691bcdd0e21668a024134a0da83cb6
beep boop <githubbot@vadim.me>
2024-07-06 07:47:45 +0200

    Solution 76 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/sj1ai82x Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4c08133e-bf6e-47f4-a974-818e9f507 d4d.py", line 5, in <module>     x = int(sys.stdin.readline())             ^^^ NameError: name 'sys' is not defined

--- Python_53.py
+++ Python_53.py
@@ -1,16 +1,14 @@
-import sys
-
 
 def add(x: int, y: int):
     return x + y
 
-
 # Accept user input for x and y
-x = input("Enter x: ")
-y = input("Enter y: ")
+import sys
+x = int(sys.stdin.readline())
+y = int(sys.stdin.readline())
 
 # Call the add function with the input values
-result = add(int(x), int(y))
+result = add(x, y)
 
 # Print the result to the console
 print("Result:", result)
