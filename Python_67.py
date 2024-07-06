d9b787cd8bc162d5864c360ded122992229c6783
beep boop <githubbot@vadim.me>
2024-07-06 05:51:06 +0200

    Solution 86 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/kh7c2g1m Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2a2a4a75-1588-40ac-8de2-9187f0981 e8e.py", line 11, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2a2a4a75-1588-40ac-8de2-9187f0981 e8e.py", line 8, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2a2a4a75-1588-40ac-8de2-9187f0981 e8e.py", line 2, in fruit_distribution     apples = int(re.search(r'\d+', s).group())                  ^^ NameError: name 're' is not defined

--- Python_67.py
+++ Python_67.py
@@ -1,6 +1,6 @@
-def fruit_distribution(s, total):
-    import re
 
-    apples = int(re.search(r"\d+", s).group())
-    oranges = int(re.search(r"\d+", s).group())
-    return apples + oranges + total
+def fruit_distribution(s):
+    apples = int(re.search(r'\d+', s).group())
+    oranges = int(re.search(r'\d+', s).group())
+    return (apples, oranges)
+    
