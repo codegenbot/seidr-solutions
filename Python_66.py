0e444dc5c1dd201ea0a9677b0860280b5776f312
beep boop <githubbot@vadim.me>
2024-07-06 21:50:29 +0200

    Solution 86 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/klh5nv8w Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f51f36fd-0e7b-4586-9879-0fc01119f ad1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_66.py
+++ Python_66.py
@@ -1,6 +1,11 @@
-def check(n: int) -> str:
-    digit_sum = sum(int(digit) for digit in str(n))
+def sum_of_digits(n):
+    return sum(int(digit) for digit in str(n))
+
+def check(digit_sum):
     if digit_sum % 3 == 0:
         return "YES"
     else:
         return "NO"
+
+input_value = int(input())
+print(check(sum_of_digits(input_value)))
