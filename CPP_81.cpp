7497e117c9df124de6c748ab5a6978471446317e
beep boop <githubbot@vadim.me>
2024-07-06 16:28:06 +0200

    Solution 43 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/n9sebtun Prompt: ng<char>*; _ForwardIterator = std::__cxx11::basic_string<char>*; _Tp = std::__cx x11::basic_string<char>]’ /usr/include/c++/11/bits/stl_vector.h:1585:33:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const std::__cxx11::basic_string<char >*; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11 ::basic_string<char> >]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = s td::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11::basic_str ing<char> >; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<std::__cx x11::basic_string<char> >]’ df2c8cb3-a0b4-4ac7-af81-39537f997efd.cpp:50:18:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -15,27 +15,27 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
     std::vector<std::string> result;
     for (float grade : grades) {
         std::string letterGrade;
-        if (grade >= 4.0) {
+        if (grade >= 4.0f) {
             letterGrade = "A+";
-        } else if (grade > 3.7) {
+        } else if (grade > 3.7f) {
             letterGrade = "A";
-        } else if (grade > 3.3) {
+        } else if (grade > 3.3f) {
             letterGrade = "A-";
-        } else if (grade > 3.0) {
+        } else if (grade > 3.0f) {
             letterGrade = "B+";
-        } else if (grade > 2.7) {
+        } else if (grade > 2.7f) {
             letterGrade = "B";
-        } else if (grade > 2.3) {
+        } else if (grade > 2.3f) {
             letterGrade = "B-";
-        } else if (grade > 2.0) {
+        } else if (grade > 2.0f) {
             letterGrade = "C+";
-        } else if (grade > 1.7) {
+        } else if (grade > 1.7f) {
             letterGrade = "C";
-        } else if (grade > 1.3) {
+        } else if (grade > 1.3f) {
             letterGrade = "C-";
-        } else if (grade > 1.0) {
+        } else if (grade > 1.0f) {
             letterGrade = "D+";
-        } else if (grade > 0.7) {
+        } else if (grade > 0.7f) {
             letterGrade = "D";
         } else {
             letterGrade = "E";
