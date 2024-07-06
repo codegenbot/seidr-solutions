5c05904f7ffdee684722cc06ca6e3460eff18262
beep boop <githubbot@vadim.me>
2024-07-06 18:16:37 +0200

    Solution 97 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qb239rlh Prompt: FizzBuzz Fizz Buzz Fizz Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3022decb-ddd8-4c4f-b4f0-24a1a7f77 bcc.py", line 18, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3022decb-ddd8-4c4f-b4f0-24a1a7f77 bcc.py", line 15, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,17 +1,17 @@
+```
 def fizz_buzz(n):
-    count = 0
+    out = []
     for i in range(1, n + 1):
-        out = ""
+        result = ""
         if i % 15 == 0:
-            out = "FizzBuzz"
-            count += 1
+            result = "FizzBuzz"
         elif i % 3 == 0:
-            out = "Fizz"
+            result = "Fizz"
         elif i % 5 == 0:
-            out = "Buzz"
+            result = "Buzz"
+        else:
+            result = str(i)
         
-        print(out)
+        out.append(result)
     
-    return count
-
-print(fizz_buzz(100000))
+    return '\n'.join(out)
