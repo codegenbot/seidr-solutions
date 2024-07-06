55c2f694d37bc3a9d98bfb6bc3775461d41cecaf
beep boop <githubbot@vadim.me>
2024-07-06 03:03:21 +0200

    Solution 79 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/brmn1pem Prompt: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ bf05620a-5660-4632-9abf-80a75908de56.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bf05620a-5660-4632-9abf-80a75908de56.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bf05620a-5660-4632-9abf-80a75908de56.cpp:18: bf05620a-5660-4632-9abf-80a75908de56.cpp: In function ‘int main()’: bf05620a-5660-4632-9abf-80a75908de56.cpp:28:35: error: no match for call to ‘(st d::vector<int>) (<brace-enclosed initializer list>)’    28 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~ bf05620a-5660-4632-9abf-80a75908de56.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,32 +1,8 @@
-```cpp
 #include <optional>
 #include <vector>
 
-bool std::operator==(const std::vector<int>& l, const std::vector<int>& r) {
-    if (l.size() != r.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < l.size(); ++i) {
-        if (l[i] != r[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int main() {
-    std::vector<int> result = filter_integers({3, 'c', 3, 3, 'a', 'b'});
-    assert(result == {3, 3, 3});
+    std::vector<int> result = filter_integers({std::make_optional(1), 'c', std::make_optional(3), std::make_optional(4)});
+    assert({1, 3} == result);
     return 0;
-}
-
-std::vector<int> filter_integers(std::vector<std::optional<int>> values) {
-    std::vector<int> result = {};
-    for (const auto& value : values) {
-        if(value.has_value()) {
-            int i = value.get();
-            result.push_back(i);
-        }
-    }
-    return result;
 }
