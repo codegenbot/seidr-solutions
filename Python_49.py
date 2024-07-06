aed2090251d7937d9b02d67a29d4a2c3157c12c0
beep boop <githubbot@vadim.me>
2024-07-06 17:54:07 +0200

    Solution 61 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5ux7p9qd Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/db0747f0-177c-4dc8-b7b2-68cb258ec 1eb.py", line 5     if f == lambda x, y: x % y:             ^^^^^^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,13 +1,15 @@
+import inspect
+
+
 def modp(x, y):
-    if y == 0:
-        return "Error: Division by zero"
-    else:
-        return x % y
+    return x % y
+
 
 def check(f):
-    if f == modp:
+    if str(inspect.getsource(f)) == "def modp(x, y): return x % y":
         return "Modular Polynomial Arithmetic Function is Correct"
     else:
         return "Error: Not a Modular Polynomial Arithmetic Function"
+
 
 print(check(modp))
