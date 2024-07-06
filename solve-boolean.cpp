2796931da08936deb9d3ba199671448e1f6591a7
beep boop <githubbot@vadim.me>
2024-07-06 13:52:43 +0200

    Solution 18 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/o9ngt94r Prompt: 961d7caf-9197-4c12-aca0-6352ff178db7.cpp: In function ‘int main()’: 961d7caf-9197-4c12-aca0-6352ff178db7.cpp:36:14: error: expected ‘}’ at end of in put    36 |     return 0;       |              ^ 961d7caf-9197-4c12-aca0-6352ff178db7.cpp:26:12: note: to match this ‘{’    26 | int main() {       |            ^

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,11 +1,43 @@
+```
+#include <string>
+#include <iostream> 
+using namespace std;
+
+bool solveBoolean(string s) {
+    bool t = true;
+    bool f = false;
+    bool res = t;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '&') {
+            res &= f;
+        } else if (s[i] == '|') {
+            res |= f;
+            f = t;
+            t = false;
+        } else if (s[i] == 't') {
+            t = true;
+            f = false;
+        } else if (s[i] == 'f') {
+            t = false;
+            f = true;
+        } else {
+            res &= (s[i] == 't');
+        }
+    }
+
+    return res;
+}
+
 int main() {
     string s;
     cout << "Enter a Boolean expression: ";
-    cin >> s;
+    getline(cin, s);
     bool result = solveBoolean(s);
     if (result) {
         cout << "True" << endl;
     } else {
         cout << "False" << endl;
     }
+    return 0;
 }
