ee3c35ffc4db5d022b5d60c4bcb4484e1a53bc11
beep boop <githubbot@vadim.me>
2024-07-06 09:04:25 +0200

    Solution 1 of Python/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/j4pdqfy9 Prompt: Complete the following code given the task description and function signature.

--- Python_155.py
+++ Python_155.py
@@ -1,3 +1,4 @@
 ```
 def even_odd_count(num):
-    return sum(1 for i in str(abs(num)) if int(i)%2==0), sum(1 for i in str(abs(num)) if int(i)%2!=0)
+    num = str(abs(num))
+    return (sum(digit % 2 == 0 for digit in num), len(num) - sum(digit % 2 == 0 for digit in num))
