baab6164ad3c273467a37783dcd8b8967ec4b6c4
beep boop <githubbot@vadim.me>
2024-07-06 16:48:55 +0200

    Solution 12 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/di5u7mn5 Prompt: 056f6ad3-a7e8-4784-b7f9-53b0ae173520.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int count_nums(vector<int> n) {       |                ^~~~~~ 056f6ad3-a7e8-4784-b7f9-53b0ae173520.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int count_nums(vector<int> n) {       |                       ^~~ In file included from 056f6ad3-a7e8-4784-b7f9-53b0ae173520.cpp:23: 056f6ad3-a7e8-4784-b7f9-53b0ae173520.cpp: In function ‘int main()’: 056f6ad3-a7e8-4784-b7f9-53b0ae173520.cpp:25:23: error: ‘count_nums’ cannot be us ed as a function    25 |     assert (count_nums({1}) == 1);       |             ~~~~~~~~~~^~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,28 +1,22 @@
 #include <vector>
-using namespace std;
 
-int count_nums(vector<int> nums) {
+int count_nums(vector<int> n) {
     int count = 0;
-    for (int num : nums) {
-        if (num >= 0) {
-            int sum = 0;
-            int abs_num = num;
-            while (abs_num > 0) {
-                int digit = abs_num % 10;
-                sum += digit;
-                abs_num /= 10;
+    for (int num : n) {
+        bool has_positive_sum = false;
+        if (num < 0) {
+            num = -num;
+        }
+        while (num > 0) {
+            int digit = num % 10;
+            num /= 10;
+            if (digit != 0) {
+                has_positive_sum = true;
+                break;
             }
-            if (sum > 0)
-                count++;
-        } else {
-            int sum = 0, i = 1;
-            do {
-                int digit = num / i % 10;
-                sum += (digit < 0) ? -digit : digit;
-                i *= 10;
-            } while (i <= abs(num));
-            if (sum > 0)
-                count++;
+        }
+        if (has_positive_sum) {
+            count++;
         }
     }
     return count;
