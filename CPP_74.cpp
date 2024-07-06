db878e8e8d8ae20033743e10e3adc791b1702a14
beep boop <githubbot@vadim.me>
2024-07-06 02:49:57 +0200

    Solution 8 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/op3ghdzk Prompt:       |                              ^~~~~~ 2c293844-371b-4c22-852b-34e0703f6041.cpp:18:37: error: ‘string’ was not declared  in this scope    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 2c293844-371b-4c22-852b-34e0703f6041.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2c293844-371b-4c22-852b-34e0703f6041.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2c293844-371b-4c22-852b-34e0703f6041.cpp:17: 2c293844-371b-4c22-852b-34e0703f6041.cpp: In function ‘int main()’: 2c293844-371b-4c22-852b-34e0703f6041.cpp:27:20: error: ‘total_match’ was not dec lared in this scope    27 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 2c293844-371b-4c22-852b-34e0703f6041.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -2,26 +2,21 @@
 #include <string>
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
+    return a == b;
 }
 
-vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<std::string> lst2) {
     int sum1 = 0;
-    for (const string& s : lst1) {
+    for (const std::string& s : lst1) {
         sum1 += s.length();
     }
     
     int sum2 = 0;
-    for (const string& s : lst2) {
+    for (const std::string& s : lst2) {
         sum2 += s.length();
     }
     
     if (sum1 < sum2) return lst1;
     else if (sum1 > sum2) return lst2;
-    else if (issame(lst1, lst2)) return lst1;
-    else return {"Equal lists are not same", "No solution found."};
+    else return lst1;
 }
