9fc31b12356147d8beded8f8405a75c0e55cacc6
beep boop <githubbot@vadim.me>
2024-07-06 16:33:27 +0200

    Solution 26 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/1dh2pvgq Prompt: red in this scope    10 |             operators.push('|');       |             ^~~~~~~~~ 9d57605c-cbd0-43fc-adf7-18e6d58cb443.cpp:12:13: error: ‘operators’ was not decla red in this scope    12 |             operators.push('&');       |             ^~~~~~~~~ 9d57605c-cbd0-43fc-adf7-18e6d58cb443.cpp:14:13: error: ‘operators’ was not decla red in this scope    14 |             operators.push('^');       |             ^~~~~~~~~ 9d57605c-cbd0-43fc-adf7-18e6d58cb443.cpp:17:20: error: ‘operators’ was not decla red in this scope    17 |         } else if (operators.empty()) {       |                    ^~~~~~~~~ 9d57605c-cbd0-43fc-adf7-18e6d58cb443.cpp:20:29: error: expected initializer befo re ‘operators’    20 |             char operator = operators.top();       |                             ^~~~~~~~~ 9d57605c-cbd0-43fc-adf7-18e6d58cb443.cpp:21:29: error: expected type-specifier b efore ‘)’ token    21 |             switch (operator) {       |                             ^

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,17 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 bool evaluate(const string &expression) {
     bool result = true;
-    int i = 0;
-    while (i < expression.length()) {
+    stack<char> operators;
+    for (int i = 0; i < expression.length(); i++) {
         if (expression[i] == '|') {
-            result |= expression[i + 1];
+            operators.push('|');
         } else if (expression[i] == '&') {
-            result &= expression[i + 1];
+            operators.push('&');
         } else if (expression[i] == '^') {
-            result ^= expression[i + 1];
+            operators.push('^');
         } else if (expression[i] == '~') {
             result = !result;
+        } else if (operators.empty()) {
+            result = true;
+        } else {
+            char operator = operators.top();
+            switch (operator) {
+                case '|':
+                    result |= expression[i];
+                    break;
+                case '&':
+                    result &= expression[i];
+                    break;
+                case '^':
+                    result ^= expression[i];
+                    break;
+            }
         }
-        i += 2;
     }
     return result && expression != "false";
 }
