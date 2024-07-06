f721348ba4de1a4ec2c820a38bf454e2a3f1e361
beep boop <githubbot@vadim.me>
2024-07-06 17:09:07 +0200

    Solution 69 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9of1w730 Prompt: Program did not receive expected input

--- Python_38.py
+++ Python_38.py
@@ -7,13 +7,8 @@ def decode_cyclic(s):
         group = s[i:i+3]
         if group[0] == group[1] and group[0] == group[2]:
             result += group[0]
-        elif group[0] != group[1]:
-            result += group[0] + group[1]
+            i += 2  
         else:
-            j = 2
-            while j > 0 and i+j < len(s):
-                if s[i+j] == group[0]:
-                    break
-                j -= 1
+            result += group
         i += 3
     return result
