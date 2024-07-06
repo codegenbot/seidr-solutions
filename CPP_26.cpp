042541fb8486a879ba051d14f9f273d01a1d4abe
beep boop <githubbot@vadim.me>
2024-07-06 18:49:20 +0200

    Solution 1 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/26lvi2h7 Prompt: Complete the following code given the task description and function signature.

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,9 +1,17 @@
-```
 vector<int> remove_duplicates(vector<int> numbers){
-    vector<int> result;
-    for(int num : numbers) {
-        if(find(result.begin(), result.end(), num) == result.end())
-            result.push_back(num);
+    vector<int> unique_numbers;
+    for(int num : numbers){
+        if(unique_index(num, unique_numbers) == unique_numbers.size()){
+            unique_numbers.push_back(num);
+        }
     }
-    return result;
-}```
+    return unique_numbers;
+}
+
+int unique_index(int num, vector<int>& vec){
+    for(int i = 0; i < vec.size(); i++){
+        if(vec[i] == num)
+            return i;
+    }
+    return -1;
+}
