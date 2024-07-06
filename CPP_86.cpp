72fa09161eb64091888b5f846567260e1a4bfd4f
beep boop <githubbot@vadim.me>
2024-07-06 15:12:00 +0200

    Solution 4 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gdej7gky Prompt: Program failed

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,39 +1,18 @@
-#include <iostream>
-#include <sstream>
-#include <algorithm>
-using namespace std;
-
 string anti_shuffle(string s) {
-    istringstream iss(s);
-    string word;
     string result = "";
-    
-    while (iss >> word) {
-        for (char c : word) {
-            result += c;
-        }
-        for (int i = 0; i < word.length(); ++i) {
-            if (word[i] != ' ') {
-                int j = i + 1;
-                while (j < word.length() && word[j] <= word[i]) {
-                    ++j;
-                }
-                for (; i < j; ++i) {
-                    result += word[i];
-                }
-                i = j - 1;
-            } else {
-                result += ' ';
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') {
+            result += " ";
+        } else {
+            string word;
+            while (i < s.length() && s[i] != ' ') {
+                word += s[i];
+                i++;
             }
+            sort(word.begin(), word.end());
+            result += word;
+            i--; // Reset i
         }
     }
-    
     return result;
-}
-
-int main() {
-    cout << anti_shuffle("Hi") << endl;
-    cout << anti_shuffle("hello") << endl;
-    cout << anti_shuffle("Hello World!!!") << endl;
-    return 0;
 }
