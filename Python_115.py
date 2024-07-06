e93623b5838476f9e0ab273f493b294f4ed596a2
beep boop <githubbot@vadim.me>
2024-07-06 03:00:24 +0200

    Solution 97 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/dx5s3j3u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f05ea6d4-7c4e-4619-b5f5-95cee2ac1 c83.py", line 18, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f05ea6d4-7c4e-4619-b5f5-95cee2ac1 c83.py", line 15, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f05ea6d4-7c4e-4619-b5f5-95cee2ac1 c83.py", line 11, in max_fill     return math.floor(water / capacity)            ^^^^ NameError: name 'math' is not defined

--- Python_115.py
+++ Python_115.py
@@ -5,9 +5,8 @@ def max_fill(grid, capacity):
     water = 0
     for row in grid:
         for cell in row:
-            if cell == 1:
+            if cell == 1 and water + 1 <= capacity:
                 water += 1
-    if capacity < water:
-        return -1
-    else:
-        return math.floor(water / capacity)
+            else:
+                break
+    return math.floor(water / capacity)
