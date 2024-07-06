2085a02344903666278c1d9910866d9b276bc43f
beep boop <githubbot@vadim.me>
2024-07-06 11:40:42 +0200

    Solution 53 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/05osbftj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/412a43b9-2bd5-4142-99d7-1f42a8c39 c44.py", line 26, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/412a43b9-2bd5-4142-99d7-1f42a8c39 c44.py", line 21, in check     sort_numbers("six five four three two one zero") AssertionError

--- Python_19.py
+++ Python_19.py
@@ -1,8 +1,6 @@
-
 def sort_numbers(numbers):
     if not re.match(r"^[a-zA-Z0-9]+$", numbers):
         return "Invalid input"
-    numbers = re.findall(r"\d+", numbers)
-    numbers = [int(num) for num in numbers]
+    numbers = [int(num) for num in re.findall(r"\d+", numbers)]
     sorted_numbers = sorted(numbers)
     return " ".join([str(num) for num in sorted_numbers])
