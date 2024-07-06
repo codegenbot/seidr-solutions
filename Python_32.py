7d71204cc2194e97491f3540e88fd837468bd60e
beep boop <githubbot@vadim.me>
2024-07-06 09:09:39 +0200

    Solution 27 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/qkdu2tv2 Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -1,9 +1,8 @@
 def find_zero(xs: list):
     if len(xs) % 2 == 1 or xs[0] != 0:
         raise ValueError("Invalid input")
-    for i in range(len(xs)):
-        if xs[i] != 0:
-            x = -xs[i - 1] / (2 * xs[i])
-            if poly(xs, x) == 0:
-                return x
+    for i, x in enumerate(xs):
+        y = -x / (2 * xs[i])
+        if poly(xs, y) == 0:
+            return y
     raise ValueError("No solution found")
