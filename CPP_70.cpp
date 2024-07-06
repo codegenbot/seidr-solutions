9b3cc733264fa1974b9b00acb99dc91fbd452be0
beep boop <githubbot@vadim.me>
2024-07-06 15:46:57 +0200

    Solution 6 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q9johqny Prompt:       |                    ^~~ 36bb74a2-86dd-4c6a-8251-40512bedac28.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 36bb74a2-86dd-4c6a-8251-40512bedac28.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 36bb74a2-86dd-4c6a-8251-40512bedac28.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 36bb74a2-86dd-4c6a-8251-40512bedac28.cpp:21: 36bb74a2-86dd-4c6a-8251-40512bedac28.cpp: In function ‘int main()’: 36bb74a2-86dd-4c6a-8251-40512bedac28.cpp:31:20: error: ‘strange_sort_list’ was n ot declared in this scope    31 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 36bb74a2-86dd-4c6a-8251-40512bedac28.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -9,21 +9,17 @@ vector<int> strange_sort_vector(vector<int> lst) {
 
     while (!lst.empty()) {
         int minVal = *min_element(lst.begin(), lst.end());
+        int maxVal = *max_element(lst.begin(), lst.end());
         result.push_back(minVal);
+        result.push_back(maxVal);
         lst.erase(std::remove(lst.begin(), lst.end(), minVal), lst.end());
-        
-        if (!lst.empty()) {
-            int maxVal = *max_element(lst.begin(), lst.end());
-            result.push_back(maxVal);
-            lst.erase(std::remove(lst.begin(), lst.end(), maxVal), lst.end());
-        }
+        lst.erase(std::remove(lst.begin(), lst.end(), maxVal), lst.end());
     }
 
     return result;
 }
 
 int main() {
-    vector<int> testVec = {111111};
-    assert(strange_sort_vector(testVec) == testVec);
+    assert(strange_sort_vector({111111}) == vector<int>({111111}));
     return 0;
 }
