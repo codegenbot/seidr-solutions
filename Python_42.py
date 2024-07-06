307f836bfe620e0f3fbc3970b2a6144b2e291de7
beep boop <githubbot@vadim.me>
2024-07-06 16:40:12 +0200

    Solution 1 of Python/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zzh9c5ge Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e9f111fd-1a0f-4630-bbd7-c3155442e 9fe.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_42.py
+++ Python_42.py
@@ -1,3 +1,6 @@
-```
-def incr_list(l: list):
-    return [i+1 for i in l]
+def incr_list():
+    n = int(input("Enter a number: "))
+    lst = list(range(1, n+1))
+    return [i+1 for i in lst]
+
+print(incr_list())
