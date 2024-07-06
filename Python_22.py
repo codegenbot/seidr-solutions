48fa3c48cc0fa19c2bd3f0b20bdd0f2dffbd7680
beep boop <githubbot@vadim.me>
2024-07-06 15:31:51 +0200

    Solution 17 of Python/22, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6f7dqd8q Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/895c945c-5469-4a20-97ac-b4caaebc1 36d.py", line 8, in <module>     check(filter_integers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/895c945c-5469-4a20-97ac-b4caaebc1 36d.py", line 5, in check     assert filter_integers([3, "c", 3, 3, "a", "b"]) == [3, 3, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/895c945c-5469-4a20-97ac-b4caaebc1 36d.py", line 3, in filter_integers     return [int(value) for value in values if isinstance(int(value), int)]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/895c945c-5469-4a20-97ac-b4caaebc1 36d.py", line 3, in <listcomp>     return [int(value) for value in values if isinstance(int(value), int)]                                                          ^^^^^^^^^^ ValueError: invalid literal for int() with base 10: 'c'

--- Python_22.py
+++ Python_22.py
@@ -1,8 +1,3 @@
-```
-def filter_integers(input_list=None):
-    if input_list is None:
-        input_str = input("Enter a comma-separated list of values: ")
-        values = [x.strip() for x in input_str.split(',')]
-    else:
-        values = [x.strip() for x in str(input_list).split(',')]
-    return [int(value) for value in values if isinstance(int(value), int)]
+def filter_integers(input_values):
+    values = [x.strip() for x in ','.join(map(str, input_values)).split(',')]
+    return [int(value) for value in values if value.isdigit()]
