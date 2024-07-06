1f0f1bb5e3daacd5c92509cf8bba4d5740119d35
beep boop <githubbot@vadim.me>
2024-07-06 11:59:52 +0200

    Solution 2 of Python/146, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/53sfgndr Prompt: Complete the following code given the task description and function signature.

--- Python_146.py
+++ Python_146.py
@@ -1,7 +1,6 @@
-```
 def specialFilter(nums):
     count = 0
     for num in nums:
-        if abs(num) > 10 and str(abs(num))[-1] in '13579' and str(abs(num))[0] in '13579':
+        if abs(num) > 10 and str(abs(num)).endswith('1') or str(abs(num)).endswith('3') or str(abs(num)).endswith('5') or str(abs(num)).endswith('7') or str(abs(num)).endswith('9'):
             count += 1
     return count
