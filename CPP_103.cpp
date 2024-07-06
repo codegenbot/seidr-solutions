c9223e9b33dccec54735e689201bbf111fbb14f6
beep boop <githubbot@vadim.me>
2024-07-06 17:15:50 +0200

    Solution 4 of CPP/103, ratio 0.8333333333333334 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/59l3tgie Prompt: 66b565e4-9a54-4fd0-b961-79795a8bc7a6.cpp: In function ‘std::string rounded_avg(i nt, int)’: 66b565e4-9a54-4fd0-b961-79795a8bc7a6.cpp:20:5: error: ‘reverse’ was not declared  in this scope    20 |     reverse(res.begin(), res.end());       |     ^~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,21 +1,23 @@
 #include <cmath>
+#include <string>
+#include <algorithm>
+using namespace std;
 
 string rounded_avg(int n, int m) {
-    if (n > m) return "-1";
+    if (n > m)
+        return "-1";
     double sum = 0;
-    for (int i = n; i <= m; i++) {
+    for (int i = n; i <= m; i++)
         sum += i;
-    }
-    double avg = round(sum / (m - n + 1));
+    int avg = lround(sum / (m - n + 1));
     string res = "";
-    while (avg > 0) {
-        if (avg >= 2) {
-            res += "1";
-            avg -= 2;
-        } else {
-            res += "0";
-            avg++;
-        }
+    while (avg) {
+        if (avg & 1)
+            res.push_back('1');
+        else
+            res.push_back('0');
+        avg >>= 1;
     }
+    reverse(res.begin(), res.end());
     return res;
 }
