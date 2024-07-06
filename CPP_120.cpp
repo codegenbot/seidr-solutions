cc4b0643453a3a9ced78d2013ff55fa9ff46dcc6
beep boop <githubbot@vadim.me>
2024-07-06 07:42:21 +0200

    Solution 8 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/a1tfr568 Prompt:       |                    ^~~ 0ac5d3ed-0e2d-4471-8183-ef194ceb27f0.cpp:8:27: error: ‘vector’ was not declared in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0ac5d3ed-0e2d-4471-8183-ef194ceb27f0.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0ac5d3ed-0e2d-4471-8183-ef194ceb27f0.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0ac5d3ed-0e2d-4471-8183-ef194ceb27f0.cpp:7: 0ac5d3ed-0e2d-4471-8183-ef194ceb27f0.cpp: In function ‘int main()’: 0ac5d3ed-0e2d-4471-8183-ef194ceb27f0.cpp:17:20: error: ‘maximum’ was not declare d in this scope    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 0ac5d3ed-0e2d-4471-8183-ef194ceb27f0.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,12 +1,24 @@
+#include <iostream>
 #include <vector>
-#include <algorithm>
+using namespace std;
 
-bool issame(vector<int> a,vector<int>b){
+bool isSame(vector<int> a, vector<int> b) {
+    if (a.empty() && b.empty()) {
+        return true;
+    }
+    if ((a.empty() || b.empty())) {
+        return false;
+    }
     return a == b;
 }
 
 vector<int> maximum(vector<int> arr, int k) {
-    vector<int> result(arr.begin(), arr.end());
-    sort(result.rbegin(), result.rend());
-    return vector<int>(result.begin(), result.begin() + k);
+    vector<int> result(arr.begin(), arr.begin() + k);
+    sort(result.begin(), result.end());
+    return result;
+}
+
+int main() {
+    assert(isSame(maximum({1, 2, 3, -23, 243, -400, 0}, 7), {})); 
+    return 0;
 }
