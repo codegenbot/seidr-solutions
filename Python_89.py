72a4b4e515b5edd1294343aeb244ba5d48229200
beep boop <githubbot@vadim.me>
2024-07-06 02:10:54 +0200

    Solution 91 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/4ip2inle Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/479c38a9-89de-4253-acb0-148cf3180 5fc.py", line 12, in <module>     check(encrypt)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/479c38a9-89de-4253-acb0-148cf3180 5fc.py", line 8, in check     encrypt("a") == "e"     ^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/479c38a9-89de-4253-acb0-148cf3180 5fc.py", line 2, in encrypt     table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxy z"[:len(s)])             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^ ValueError: the first two maketrans arguments must have equal length

--- Python_89.py
+++ Python_89.py
@@ -1,4 +1,6 @@
 def encrypt(s):
+    # Pad the shorter argument with spaces
+    s = s.rjust(len("abcdefghijklmnopqrstuvwxyz"), " ")
     table = str.maketrans(
         "abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyz"[: len(s)]
     )
