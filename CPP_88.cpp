031f4f3a4037bd651e28b7af8ef438fd5496895c
beep boop <githubbot@vadim.me>
2024-07-06 15:32:32 +0200

    Solution 13 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/e0wi7av0 Prompt:       |                    ^~~ 2dee8f86-36a4-4c78-980b-8f47fa33bcda.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2dee8f86-36a4-4c78-980b-8f47fa33bcda.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2dee8f86-36a4-4c78-980b-8f47fa33bcda.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2dee8f86-36a4-4c78-980b-8f47fa33bcda.cpp:14: 2dee8f86-36a4-4c78-980b-8f47fa33bcda.cpp: In function ‘int main()’: 2dee8f86-36a4-4c78-980b-8f47fa33bcda.cpp:24:20: error: ‘sort_array’ was not decl ared in this scope    24 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 2dee8f86-36a4-4c78-980b-8f47fa33bcda.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,19 +1,17 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> sort_array(std::vector<int> array){
+vector<int> sort_array(vector<int> array){
     int sum = array[0] + array[array.size()-1];
     if(sum % 2 == 0) {
-        std::vector<int> sortedArray = array;
-        std::sort(sortedArray.begin(), sortedArray.end(), std::greater<int>());
+        vector<int> sortedArray = array;
+        sort(sortedArray.begin(), sortedArray.end(), greater<int>());
         return sortedArray;
     } else {
-        std::vector<int> sortedArray = array;
-        std::sort(sortedArray.begin(), sortedArray.end());
+        vector<int> sortedArray = array;
+        sort(sortedArray.begin(), sortedArray.end());
         return sortedArray;
     }
 }
 
-int main() {
-    assert(sort_array({21, 14, 23, 11}) == std::vector<int>({23, 21, 14, 11}));
-}
+assert(sort_array({21, 14, 23, 11}) == vector<int>({23, 21, 14, 11}));
