a110e084e63d3dd890b3aa13f75eacc6d1663247
beep boop <githubbot@vadim.me>
2024-07-06 19:02:36 +0200

    Solution 70 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ygswh6dl Prompt: In file included from ec84ad29-49f1-4835-bdaf-7c3136a86366.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ec84ad29-49f1-4835-bdaf-7c3136a86366.cpp:18:37: error: ‘string’ was not declared  in this scope    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ec84ad29-49f1-4835-bdaf-7c3136a86366.cpp:18:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from ec84ad29-49f1-4835-bdaf-7c3136a86366.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from ec84ad29-49f1-4835-bdaf-7c3136a86366.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ ec84ad29-49f1-4835-bdaf-7c3136a86366.cpp:18:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,46 +1,13 @@
 #include <vector>
 #include <string>
-#include <algorithm>
 
 std::vector<std::string> bf(std::string a, std::string b) {
-    int i = 0;
-    while (i < a.size()) {
-        if (a[i] == 'B') {
-            for (int j = i; j < a.size() && a[j] != 'E'; j++) {
-                if (j > i && a[j] == ' ') {
-                    i = j + 1;
-                    break;
-                }
-            }
-            std::string word = "";
-            while (i < a.size() && a[i] != 'E') {
-                if (a[i] == ' ') {
-                    break;
-                } else {
-                    word += a[i];
-                    i++;
-                }
-            }
-            b += " " + word;
-        } else {
-            i++;
+    std::vector<std::string> result;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] == b[0]) {
+            result.push_back(a.substr(i));
         }
     }
-    return std::vector<std::string>(0).swap(b == "" ? std::vector<std::string>(1, "") : split(b));
-}
-
-std::vector<std::string> split(std::string str) {
-    size_t start = 0;
-    size_t end = str.find(' ');
-    std::vector<std::string> result;
-
-    while (end != std::string::npos) {
-        result.push_back(str.substr(start, end - start));
-        start = end + 1;
-        end = str.find(' ', start);
-    }
-
-    result.push_back(str.substr(start));
     return result;
 }
 
