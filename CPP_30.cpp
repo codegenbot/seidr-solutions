864cb39393d6537a1340ed79f4c697df11787824
beep boop <githubbot@vadim.me>
2024-07-06 11:38:19 +0200

    Solution 28 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/aikobq58 Prompt:       |                    ^~~~~ 48606900-faaf-4fda-918b-fd669bafbaff.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 48606900-faaf-4fda-918b-fd669bafbaff.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 48606900-faaf-4fda-918b-fd669bafbaff.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 48606900-faaf-4fda-918b-fd669bafbaff.cpp:11: 48606900-faaf-4fda-918b-fd669bafbaff.cpp: In function ‘int main()’: 48606900-faaf-4fda-918b-fd669bafbaff.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 48606900-faaf-4fda-918b-fd669bafbaff.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,17 +1,28 @@
-```cpp
 #include <vector>
 
-std::vector<float> get_positive(std::vector<float> l){
+std::vector<float> get_positive(std::vector<float> l) {
     std::vector<float> result;
-    for(float i : l) {
-        if(i > 0)
-            result.push_back(i);
+    for (float num : l) {
+        if (num > 0) {
+            result.push_back(num);
+        }
     }
     return result;
+}
 
+bool issame(std::vector<float> a, std::vector<float> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main() {
-    assert(get_positive({}) == std::vector<float>());
-    // Other code...
+    assert(issame(get_positive({}), {}));
+    return 0;
 }
