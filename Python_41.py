e6deac7a82b4d17d2c70e82fb353ba24d2bd4655
beep boop <githubbot@vadim.me>
2024-07-06 09:09:04 +0200

    Solution 43 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ion52a5b Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/044fc363-c31d-4dfa-81f9-6a3989e95 384.py", line 17, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/044fc363-c31d-4dfa-81f9-6a3989e95 384.py", line 14, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_41.py
+++ Python_41.py
@@ -1,4 +1,3 @@
-
 def car_race_collision(n: int):
     # Initialize a counter for the number of collisions
     collision_count = 0
@@ -9,8 +8,6 @@ def car_race_collision(n: int):
             # Check if the two cars are moving towards each other
             if (i + j) % 2 == 1:
                 collision_count += 1
-            
-            # If the number of cars is even and the sum of i and j is greater than or equal to half of n, increase the collision count by one.
             elif n % 2 == 0 and i + j >= n // 2:
                 collision_count += 1
 
