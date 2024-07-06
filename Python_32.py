371f2b2c71488e0df24dc84b8a19c2132193337f
beep boop <githubbot@vadim.me>
2024-07-06 02:01:20 +0200

    Solution 16 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/u4hjxqlv Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -1,7 +1,4 @@
-def find_zero(xs: list[float]) -> float:
-    if len(xs) == 1 or not any(x != 0 for x in xs):
-        raise ValueError("Input must contain at least one non-zero element")
-
+def find_zero(xs):
     for i in range(len(xs)):
         if xs[i] == 0:
             continue
