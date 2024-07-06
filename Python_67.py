2f9270847d2873bec1e7696cd9593a5c3c15a17d
beep boop <githubbot@vadim.me>
2024-07-07 01:21:01 +0200

    Solution 39 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/619yxeyy Prompt: Complete the following code given the task description and function signature.

--- Python_67.py
+++ Python_67.py
@@ -1,10 +1,4 @@
 def fruit_distribution(s, n):
-    s = s.replace("apples and ", "").replace("oranges and ", "")
-    n = int(n)
-    return (
-        n
-        - int(
-            s.split()[0].replace("apples ", ",").replace("oranges ", ",").split(",")[0]
-        )
-        - int(s.split()[2].replace("oranges ", ",").split(",")[1])
-    )
+    s = s.replace("apples and ", "")
+    s = s.replace("oranges", "")
+    return n - int(s.split()[0].replace("apples ", ""))
