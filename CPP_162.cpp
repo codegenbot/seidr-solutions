63a6f8e09754bf1a6126c7c1b6aa2120576180d1
beep boop <githubbot@vadim.me>
2024-07-06 14:38:43 +0200

    Solution 95 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zddpzc52 Prompt: 857b82ee-67b7-4c5f-ba2a-bc2583e5d5ab.cpp:1:10: fatal error: CryptoPP/MD5.h: No s uch file or directory     1 | #include <CryptoPP/MD5.h>       |          ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,17 +1,16 @@
+#include <iostream>
 #include <string>
-#include <algorithm>
 
 using namespace std;
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
-    unsigned char *buf = reinterpret_cast<unsigned char*>(text.c_str());
-    size_t len = text.length();
-
-    MD5 md;
-    md.Update(buf, len);
-    string result = hexEncoder().CalculateString(md.Digest());
+    CryptoPP::MD5 md;
+    byte *buf = (byte *)text.c_str();
+    size_t len = text.length() + 1; 
+    md.Update(buf, (int)len);
+    string result = CryptoPP::HexEncoder().CalculateString(md.Digest());
 
     return result;
 }
