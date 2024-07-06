70aed084b2afadd8934ea71d7d370c8d84469ebe
beep boop <githubbot@vadim.me>
2024-07-06 08:28:15 +0200

    Solution 67 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3l504wnm Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 9a0b5661-b908-4816-a42a-58f1a7d505d3.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,13 +1,14 @@
+```cpp
 #include <iostream>
 #include <vector>
 #include <string>
 
 bool same(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    if(a.size() != (std::size_t)b.size()) {
+    if(a.size() != (std::size_t) b.size()) {
         return false;
     }
     
-    for(std::size_t i = 0; i < a.size(); i++) {
+    for(int i = 0; i < (std::size_t) a.size(); i++) {
         if(a[i] != b[i]) {
             return false;
         }
@@ -17,7 +18,7 @@ bool same(const std::vector<std::string>& a, const std::vector<std::string>& b) 
 }
 
 std::vector<std::vector<std::string>> by_length(const std::vector<int>& arr) {
-    std::vector<std::vector<std::string>> result((std::size_t)10);
+    std::vector<std::vector<std::string>> result(10);
     
     for (int i : arr) {
         if(i >= 1 && i <= 9) {
@@ -26,34 +27,34 @@ std::vector<std::vector<std::string>> by_length(const std::vector<int>& arr) {
                     result[0].push_back("One");
                     break;
                 case 2:
-                    result[(std::size_t)1].push_back("Two");
+                    result[(std::size_t) 1].push_back("Two");
                     break;
                 case 3:
-                    result[(std::size_t)2].push_back("Three");
+                    result[(std::size_t) 2].push_back("Three");
                     break;
                 case 4:
-                    result[(std::size_t)3].push_back("Four");
+                    result[(std::size_t) 3].push_back("Four");
                     break;
                 case 5:
-                    result[(std::size_t)4].push_back("Five");
+                    result[(std::size_t) 4].push_back("Five");
                     break;
                 case 6:
-                    result[(std::size_t)5].push_back("Six");
+                    result[(std::size_t) 5].push_back("Six");
                     break;
                 case 7:
-                    result[(std::size_t)6].push_back("Seven");
+                    result[(std::size_t) 6].push_back("Seven");
                     break;
                 case 8:
-                    result[(std::size_t)7].push_back("Eight");
+                    result[(std::size_t) 7].push_back("Eight");
                     break;
                 case 9:
-                    result[(std::size_t)8].push_back("Nine");
+                    result[(std::size_t) 8].push_back("Nine");
                     break;
             }
         }
     }
 
-    return result;
+    return std::vector<std::vector<std::string>>(result);
 }
 
 int main() {
