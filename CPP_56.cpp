d7e297909dc05331776d20e0f849accf8cb7b966
beep boop <githubbot@vadim.me>
2024-07-06 04:41:06 +0200

    Solution 39 of CPP/56, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/p0gd0cm6 Prompt: 754b72b5-68d0-4729-b59d-b854b0d18814.cpp:1:30: error: ‘string’ is not a member o f ‘std’     1 | bool correct_bracketing(std::string brackets) {       |                              ^~~~~~ 754b72b5-68d0-4729-b59d-b854b0d18814.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | bool correct_bracketing(std::string brackets) { In file included from 754b72b5-68d0-4729-b59d-b854b0d18814.cpp:16: 754b72b5-68d0-4729-b59d-b854b0d18814.cpp: In function ‘int main()’: 754b72b5-68d0-4729-b59d-b854b0d18814.cpp:18:36: error: ‘correct_bracketing’ cann ot be used as a function    18 |     assert (not (correct_bracketing("<><><<><>><>>><>")));       |                  ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_56.cpp
+++ CPP_56.cpp
@@ -9,13 +9,13 @@ bool correct_bracketing(std::string brackets) {
             if (count > 0)
                 count--;
             else
-                return false; 
+                return false;
         }
     }
     return count == 0;
 }
 
 int main() {
-    assert(not (correct_bracketing("<><><<><>><>>><>")));
+    assert(not correct_bracketing("<><><<><>><>>><>"));
     return 0;
 }
