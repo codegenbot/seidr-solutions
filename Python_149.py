87411422e8670f517573e95223461f5a2ba4a7d9
beep boop <githubbot@vadim.me>
2024-07-06 21:23:47 +0200

    Solution 76 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/bn9yd81p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/075cabf8-a7b9-427d-8e66-96ae5d601 923.py", line 13, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/075cabf8-a7b9-427d-8e66-96ae5d601 923.py", line 10, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: sorted_list_sum() takes 0 positional arguments but 1 was given

--- Python_149.py
+++ Python_149.py
@@ -1,7 +1,5 @@
 def sorted_list_sum(input_list):
-    user_input = input("Enter a list of words separated by space: ").split()
-    
-    even_words = [word for word in user_input if "".join(filter(str.isdigit, word))]
-    odd_words = [word for word in user_input if word not in even_words]
+    even_words = [word for word in input_list if "".join(filter(str.isdigit, word))]
+    odd_words = [word for word in input_list if word not in even_words]
     
     return " ".join(sorted(even_words, key=lambda x: int("".join(filter(str.isdigit, x))))) + " " .join(sorted(odd_words))
