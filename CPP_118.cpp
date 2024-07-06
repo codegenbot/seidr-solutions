e997def0d8efa3ad9279406a57effe91df4c5c09
beep boop <githubbot@vadim.me>
2024-07-06 22:03:06 +0200

    Solution 16 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/76vytir9 Prompt: pe     1 | string get_closest_vowel(string word) {       | ^~~~~~ d3d37445-c44b-4365-b8e6-e972a5289bbf.cpp: In function ‘bool isvowel(char)’: d3d37445-c44b-4365-b8e6-e972a5289bbf.cpp:13:9: error: ‘tolower’ was not declared  in this scope    13 |     c = tolower(c);       |         ^~~~~~~ d3d37445-c44b-4365-b8e6-e972a5289bbf.cpp: In function ‘bool isconsonant(char)’: d3d37445-c44b-4365-b8e6-e972a5289bbf.cpp:18:9: error: ‘tolower’ was not declared  in this scope    18 |     c = tolower(c);       |         ^~~~~~~ d3d37445-c44b-4365-b8e6-e972a5289bbf.cpp:19:27: error: ‘isalpha’ was not declare d in this scope    19 |     return !isvowel(c) && isalpha(c);       |                           ^~~~~~~ In file included from d3d37445-c44b-4365-b8e6-e972a5289bbf.cpp:22: d3d37445-c44b-4365-b8e6-e972a5289bbf.cpp: In function ‘int main()’: d3d37445-c44b-4365-b8e6-e972a5289bbf.cpp:24:13: error: ‘get_closest_vowel’ was n ot declared in this scope    24 |     assert (get_closest_vowel("Above") == "o");       |             ^~~~~~~~~~~~~~~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,23 +1,22 @@
+using namespace std;
+
 string get_closest_vowel(string word) {
-    for (int i = word.length() - 1; i > 0; --i) {
-        if (isvowel(word[i])) {
-            int j;
-            for (j = i - 1; j >= 0; --j) {
-                if (!isvowel(word[j]) && !ispunctuation(word[j]))
-                    break;
-            }
-            return word.substr(j + 1, i - j);
-        }
+    int n = word.size();
+    for (int i = n - 1; i >= 0; --i) {
+        if (!isalpha(word[i])) continue;
+        if (isvowel(word[i])) return string(1, tolower(word[i]));
+        while (i > 0 && !isalpha(word[i-1])) --i;
+        if (i > 0 && isconsonant(word[i-1]) && isvowel(word[i])) return string(1, tolower(word[i]));
     }
     return "";
 }
 
 bool isvowel(char c) {
-    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+    c = tolower(c);
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
 }
 
-bool ispunctuation(char c) {
-    return (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' &&
-           c != 'A' && c != 'E' && c != 'I' && c != 'O' && c != 'U');
+bool isconsonant(char c) {
+    c = tolower(c);
+    return !isvowel(c) && isalpha(c);
 }
