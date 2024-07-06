eecfc74331adb3c84bfb351342bd3878abe11aa9
beep boop <githubbot@vadim.me>
2024-07-06 18:10:08 +0200

    Solution 97 of snow-day, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/yg65jg38 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/944b7d34-ef25-467d-99f1-f99956d79 c6c.py", line 3     return round((snow_fall_rate * hours) + (hours * snow_fall_rate)) * (1 - mel ting_proportion), 1)                    ^ SyntaxError: unmatched ')'

--- snow-day.py
+++ snow-day.py
@@ -1,3 +1,4 @@
+
 def snow_day(hours, snow_on_ground, snow_fall_rate, melting_proportion):
     if snow_on_ground == 0:
         return round((snow_fall_rate * hours) + (hours * snow_fall_rate)) * (1 - melting_proportion), 1)
