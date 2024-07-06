e1fe0ab41770497f27e48a61825ad2bb1572d446
beep boop <githubbot@vadim.me>
2024-07-06 04:34:42 +0200

    Solution 4 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/wbkh6ns2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1e5db76-bd3c-47fa-91b9-cee5ee201 40e.py", line 1, in <module>     def parse_music(music_string: str) -> List[int]:                                           ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_17.py
+++ Python_17.py
@@ -1,15 +1,19 @@
 ```
+from typing import List
+
 def parse_music(music_string: str) -> List[int]:
-    beats = []
+    music_list = []
     i = 0
     while i < len(music_string):
-        if music_string[i] == 'o':
-            beats.append(4)
+        if music_string[i] == "o":
+            music_list.append(4)
             i += 1
-        elif music_string[i:i+2] == 'o|':
-            beats.append(2)
+            while i < len(music_string) and music_string[i] != "|":
+                i += 1
+        elif music_string[i:i + 2] == "o|":
+            music_list.append(2)
             i += 2
-        elif music_string[i:i+3] == '.|__':
-            beats.append(1)
+        elif music_string[i:i + 3] == ".| ":
+            music_list.append(1)
             i += 3
-    return beats
+    return music_list
