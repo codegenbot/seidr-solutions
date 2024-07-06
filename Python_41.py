b8d00e8b475165a25b6656461cd841304eefa495
beep boop <githubbot@vadim.me>
2024-07-06 03:21:23 +0200

    Solution 1 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/izqipbz4 Prompt: Complete the following code given the task description and function signature.

--- Python_41.py
+++ Python_41.py
@@ -1,5 +1,2 @@
-
-def car_race_collision(n: int) -> int:
-    left_to_right = list(range(1, n + 1))
-    right_to_left = list(range(n, 0, -1))
-    return len([x for x in left_to_right if x in right_to_left])
+def car_race_collision(n: int):
+    return n * (n - 1) // 2
