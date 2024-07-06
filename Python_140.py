66fca5819c5c5e46013b22288e25f3411ffeb379
beep boop <githubbot@vadim.me>
2024-07-06 04:59:22 +0200

    Solution 66 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ynbb4504 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e374872b-dddd-4c0f-8c34-f4ebf96bb d67.py", line 13, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e374872b-dddd-4c0f-8c34-f4ebf96bb d67.py", line 9, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple"     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e374872b-dddd-4c0f-8c34-f4ebf96bb d67.py", line 3, in fix_spaces     text = re.sub(r"\s+", "_", text)            ^^ NameError: name 're' is not defined

--- Python_140.py
+++ Python_140.py
@@ -1,7 +1,5 @@
-import re
-
 
 def fix_spaces(text):
-    text = input("Enter some text to remove spaces and replace them with underscores: ")
+    import re
     text = re.sub(r"\s+", "_", text)
     return text.replace(" ", "")
