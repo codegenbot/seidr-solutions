7f67c2897eab3f3c0e4bf0ab7269f8f9178b607b
beep boop <githubbot@vadim.me>
2024-07-06 02:56:07 +0200

    Solution 81 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/al4be52p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6a3bc140-169c-4e5e-8b63-a53902012 7bd.py", line 11, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6a3bc140-169c-4e5e-8b63-a53902012 7bd.py", line 8, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_26.py
+++ Python_26.py
@@ -1,3 +1,4 @@
+```
 def remove_duplicates(input_str=None):
     if input_str is None:
         numbers = input("Enter comma-separated numbers: ")
@@ -6,4 +7,4 @@ def remove_duplicates(input_str=None):
     if input_str is None:
         return list(dict.fromkeys(map(int, filter(lambda x: x.isdigit(), numbers.split(",")))))
     else:
-        return list(dict.fromkeys(map(int, filter(lambda x: x.isdigit(), str(input_str).split(","))))))
+        return list(dict.fromkeys(map(int, filter(lambda x: x.isdigit(), str(input_str).split(",")))))
