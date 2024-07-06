60128a78cab3a5195a4362bc557356b8cb8c1b4d
beep boop <githubbot@vadim.me>
2024-07-06 15:44:22 +0200

    Solution 29 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7bimcvpy Prompt:                  from /usr/include/c++/11/algorithm:74,                  from e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp:16: e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp: In function ‘int main()’: e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp:26:20: error: ‘common’ was not declared  in this scope    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ e4a6de7f-ddf9-492b-8866-5dff1af50bf4.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -2,8 +2,7 @@
 #include <algorithm>
 
 bool issame(int x) {
-    // Code for the function goes here
-    return true;
+    return (x > 5);
 }
 
 vector<int> common(vector<int> l1, vector<int> l2) {
@@ -19,30 +18,15 @@ vector<int> common(vector<int> l1, vector<int> l2) {
 }
 
 int main() {
-    // Read input from user
-    int n;
-    cin >> n;
+    vector<int> v1 = {1, 2, 3, 4};
+    vector<int> v2 = {3, 4, 5, 6};
 
-    // Generate input arrays l1 and l2
-    vector<int> l1(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> l1[i];
+    vector<int> result = common(v1, v2);
+
+    for (auto x : result) {
+        if (!issame(x)) continue;
+        cout << x << endl;
     }
-
-    vector<int> l2(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> l2[i];
-    }
-
-    // Call the function
-    vector<int> result = common(l1, l2);
-
-    // Output result
-    cout << "Common elements: ";
-    for (int x : result) {
-        cout << x << " ";
-    }
-    cout << endl;
 
     return 0;
 }
