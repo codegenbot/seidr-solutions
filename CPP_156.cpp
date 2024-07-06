5702edd770e737e94b9f281d16e91f1d90647577
beep boop <githubbot@vadim.me>
2024-07-06 20:22:28 +0200

    Solution 96 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cge2wy3k Prompt: 2b9160be-7c7c-4450-bcd7-aa4b730d071b.cpp:1:6: error: ‘string’ in namespace ‘std’  does not name a type     1 | std::string intToMiniRoman(int number) {       |      ^~~~~~ 2b9160be-7c7c-4450-bcd7-aa4b730d071b.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | std::string intToMiniRoman(int number) {

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,34 +1,13 @@
+```cpp
 #include <string>
 
 std::string intToMiniRoman(int number) {
-    if (number > 3999) {
-        return "Invalid Roman Numeral";
-    }
-    
     std::string roman = "";
-    
-    while (number >= 1000) {
-        roman += "M";
-        number -= 1000;
-    }
-    
-    while (number >= 900) {
-        roman += "CM";
-        number -= 900;
-    }
-    
-    while (number >= 500) {
-        roman += "D";
-        number -= 500;
-    }
-    
-    while (number >= 400) {
-        roman += "CD";
-        number -= 400;
-    }
-    
-    while (number >= 100) {
-        if (number >= 900) {
+    while (number > 0) {
+        if (number >= 1000) {
+            roman += "M";
+            number -= 1000;
+        } else if (number >= 900) {
             roman += "CM";
             number -= 900;
         } else if (number >= 500) {
@@ -37,87 +16,39 @@ std::string intToMiniRoman(int number) {
         } else if (number >= 400) {
             roman += "CD";
             number -= 400;
-        } else {
-            while (number >= 100) {
+        } else if (number >= 100) {
+            if (number >= 90) {
+                roman += "XC";
+                number -= 90;
+            } else {
                 roman += "C";
                 number -= 100;
             }
-        }
-    }
-    
-    while (number >= 90) {
-        roman += "XC";
-        number -= 90;
-    }
-    
-    while (number >= 50) {
-        if (number >= 500) {
-            roman += "D";
-            number -= 500;
-        } else if (number >= 900) {
-            roman += "CM";
-            number -= 900;
-        } else {
-            roman += "L";
-            number -= 50;
-        }
-    }
-    
-    while (number >= 40) {
-        roman += "XL";
-        number -= 40;
-    }
-    
-    while (number >= 10) {
-        if (number >= 90) {
-            roman += "XC";
-            number -= 90;
         } else if (number >= 50) {
-            roman += "L";
-            number -= 50;
-        } else {
-            while (number >= 10) {
+            if (number == 50) {
+                roman += "L";
+                number = 0;
+            } else {
+                roman += "XL";
+                number -= 40;
+            }
+        } else if (number >= 10) {
+            if (number == 9) {
+                roman += "IX";
+                number = 0;
+            } else {
                 roman += "X";
                 number -= 10;
             }
-        }
-    }
-    
-    while (number >= 9) {
-        roman += "IX";
-        number -= 9;
-    }
-    
-    while (number >= 5) {
-        if (number >= 50) {
-            roman += "L";
-            number -= 50;
-        } else if (number >= 90) {
-            roman += "XC";
-            number -= 90;
         } else {
-            roman += "V";
-            number -= 5;
+            if (number == 5) {
+                roman += "V";
+                number = 0;
+            } else {
+                roman += "IV";
+                number -= 4;
+            }
         }
     }
-    
-    while (number >= 4) {
-        roman += "IV";
-        number -= 4;
-    }
-    
-    while (number >= 1) {
-        if (number >= 9) {
-            roman += "IX";
-            number -= 9;
-        } else if (number >= 5) {
-            roman += "V";
-            number -= 5;
-        } else {
-            roman += "I";
-            number -= 1;
-        }
-    }
-    
     return roman;
 }
