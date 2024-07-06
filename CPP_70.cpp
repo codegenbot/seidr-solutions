3e650c7d69e0438adc4920fc381eb06cb5aa0a8f
beep boop <githubbot@vadim.me>
2024-07-06 15:43:36 +0200

    Solution 2 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q9johqny Prompt: Complete the following code given the task description and function signature.

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,20 +1,26 @@
+#include <vector>
 #include <algorithm>
 using namespace std;
 
 vector<int> strange_sort_vector(vector<int> lst) {
     vector<int> result;
-    if (lst.empty()) return result;
-
-    vector<int> min_max;
-    for (int num : lst) {
-        if (num == *min_element(lst.begin(), lst.end())) {
-            result.push_back(num);
-            lst.erase(remove(lst.begin(), lst.end(), num), lst.end());
-        } else if (num == *max_element(lst.begin(), lst.end())) {
-            result.push_back(num);
-            lst.erase(remove(lst.begin(), lst.end(), num), lst.end());
+    while (!lst.empty()) {
+        int min_val = *min_element(lst.begin(), lst.end());
+        int max_val = *max_element(lst.begin(), lst.end());
+        result.push_back(min_val);
+        lst.erase-remove_one(lst.begin());
+        if (lst.size() > 0) {
+            result.push_back(max_val);
+            lst.erase(remove_one(lst.begin()), lst.end());
         }
     }
-
     return result;
+}
+
+int remove_one(const vector<int>::iterator& it) {
+    vector<int>::iterator newEnd = it + 1;
+    while (newEnd != lst.end() && *newEnd == *it) {
+        ++newEnd;
+    }
+    return newEnd - lst.begin();
 }
