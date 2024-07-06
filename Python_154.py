e714df0325e0bcc58baa2110054e8a99d09100d4
beep boop <githubbot@vadim.me>
2024-07-06 19:41:59 +0200

    Solution 17 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3f2yeli0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/23fadb58-1679-46f7-917b-4babc38ec 9c2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_154.py
+++ Python_154.py
@@ -1,4 +1,3 @@
-```
 def cycpattern_check(a, b):
     if len(b) == 0:
         return True
@@ -8,7 +7,7 @@ def cycpattern_check(a, b):
         (len(a) - 1) * len(b) <= len(a)
         and all(
             a[i:i+len(b)] == b or a[:i] + a[i+len(b):] == b
-            for i in range(len(a) - len(b) + 1)
+            for i in range(len(a)-len(b)+1)
         )
         or (a[:len(b)] == b and a[len(b):].join("") == b)
     )
