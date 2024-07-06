be8da48dcc895c9ae24b13df97a5b6006777cea5
beep boop <githubbot@vadim.me>
2024-07-06 20:36:12 +0200

    Solution 65 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/u2o18bue Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6b1c8ece-db9a-481d-baa9-b85a65d1d c43.py", line 1     return abs(first_half_sum - 2*second_half_sum)     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_151.py
+++ Python_151.py
@@ -1,5 +1,3 @@
 ```
-def double_the_difference(lst):
-    first_half_sum = sum(i for i in lst[:len(lst)//2])
-    second_half_sum = sum(i for i in lst[len(lst)//2:])
-    return [abs(sum(lst[:len(lst)//2]) - 2*sum(lst[len(lst)//2:]))]
+def solution(first_half_sum, second_half_sum):
+    return abs(first_half_sum - 2*second_half_sum)
