5e07cacaa7288c6e7f9de89e0ef74764610eaa09
beep boop <githubbot@vadim.me>
2024-07-06 18:28:44 +0200

    Solution 88 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5ux7p9qd Prompt:     return (x * modp(x, y-1)) % y                 ^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a45ce8d6-176d-499a-b080-9a2f3b43b 58c.py", line 5, in modp     return modp((x*x) % y, y//2)            ^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a45ce8d6-176d-499a-b080-9a2f3b43b 58c.py", line 5, in modp     return modp((x*x) % y, y//2)            ^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a45ce8d6-176d-499a-b080-9a2f3b43b 58c.py", line 7, in modp     return (x * modp(x, y-1)) % y                 ^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a45ce8d6-176d-499a-b080-9a2f3b43b 58c.py", line 3, in modp     return 1 if x % y == 0 else x                 ~~^~~ ZeroDivisionError: integer modulo by zero

--- Python_49.py
+++ Python_49.py
@@ -1,9 +1,7 @@
 def modp(x, y):
     if y == 0:
-        return 1 if x % y == 0 else x
-    elif y == 1: 
         return x
     elif y % 2 == 0: 
-        return modp((x*x) % (y//2), y//2)
+        return modp((x*x) % y, y//2)
     else:
         return (x * modp(x, y-1)) % y
