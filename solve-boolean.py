88ab8054de7d69f9de8ef41c0c57d46ddf0b073d
beep boop <githubbot@vadim.me>
2024-07-06 13:00:17 +0200

    Solution 50 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/iq6om4mn Prompt: it must return False for input t&t&f|f|t&t|f|t&t&f|f|f&t&f&t, but it returns .

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,18 +1,37 @@
 def solve_boolean(expression):
     bool_map = {'T': True, 'F': False}
-    expression = ''.join(bool_map.get(c, c) for c in expression)
-    
-    while '&' in expression:
-        start = 0
-        end = 0
-        for i in range(len(expression)-1, -1, -1):
-            if expression[i] == '&':
-                start = i
-            elif expression[i] == '|':
-                end = i
-                break
-        and_parts = expression[:end+1].split('&')
-        result &= all(eval(' '.join(map(str, part.split('|')))) for part in and_parts)
-        expression = expression[start + 1:]
-    
-    return not eval(' '.join(expression.split('|')))
+
+    def eval_expression(expr):
+        while '&' in expr:
+            start = 0
+            end = 0
+            for i in range(len(expr) - 1, -1, -1):
+                if expr[i] == '&':
+                    start = i
+                elif expr[i] == '|':
+                    end = i
+                    break
+            and_parts = expr[:end + 1].split('&')
+            expr = expr[start + 1:]
+            
+            result = all(eval_expression(part) for part in and_parts)
+            return 'T' if result else 'F'
+        
+        while '|' in expr:
+            start = 0
+            end = 0
+            for i in range(len(expr) - 1, -1, -1):
+                if expr[i] == '|':
+                    start = i
+                elif expr[i] == '&':
+                    end = i
+                    break
+            or_parts = expr[:end + 1].split('|')
+            expr = expr[start + 1:]
+            
+            result = any(eval_expression(part) for part in or_parts)
+            return 'T' if result else 'F'
+        
+        return bool_map[expr]
+
+    return eval_expression(expression) == 'T'
