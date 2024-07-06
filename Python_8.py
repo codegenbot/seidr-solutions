b9bbf01b08aeb64065e588f64677f0b72907c63e
beep boop <githubbot@vadim.me>
2024-07-06 09:49:25 +0200

    Solution 53 of Python/8, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/iy82mbk0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5eb4eeef-ee93-42a7-9d9d-f0c768ba0 a48.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -1,7 +1,5 @@
-def sum_product(numbers: List[int]) -> Tuple[int, int]:
-    total_sum = 0
-    product = 1
-    for num in numbers:
-        total_sum += num
-        product *= num
-    return total_sum, product
+def sum_product(numbers):
+    if len(numbers) == 0:
+        return 0, 1
+    else:
+        return sum(numbers), eval('*'.join(map(str, numbers)))
