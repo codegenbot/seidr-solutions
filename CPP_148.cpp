9cb27b2218aada210020f98df6813d025a103581
beep boop <githubbot@vadim.me>
2024-07-06 19:21:25 +0200

    Solution 90 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ygswh6dl Prompt: In file included from a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:19:37: error: ‘string’ was not declared  in this scope    19 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:19:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:19:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,36 +1,55 @@
+```cpp
 #include <vector>
 #include <string>
 #include <algorithm>
 
-std::vector<std::string> bf(std::string a, std::string b) {
-    int n = (int)a.length() + (int)b.length();
-    std::vector<char> res(n);
-    
-    for(int i = 0; i < a.length(); i++) {
-        res[i] = a[i];
-    }
-    
-    for(int i = 0, j = a.length(); i < b.length(); i++,j++) {
-        res[j] = b[i];
-    }
-    
-    std::string result;
-    for(char c : res) {
-        if(c != '\0') {
-            result.push_back(c);
-        } else {
-            break;
-        }
-    }
-    
-    return {result};
-}
-
 bool issame(std::vector<std::string> a, std::string b) {
     for (int i = 0; i < a.size(); i++) {
-        if (find(b.begin(),b.end(),a[i]) == b.end()) {
+        if (b.find(a[i]) == std::string::npos) {
             return false;
         }
     }
     return true;
+}
+
+std::vector<std::string> bf(std::string a, std::string b) {
+    int n = 0, m = 0;
+    for (char &c : a) {
+        if (c == '1') ++n;
+    }
+    for (char &c : b) {
+        if (c == '1') ++m;
+    }
+
+    std::vector<std::string> result;
+
+    if (n > m) {
+        int i = 0, j = n - m;
+        while (i < a.size() && j--) {
+            result.push_back(a.substr(i) + "0");
+            i++;
+        }
+        while (j--) {
+            result.push_back("0");
+        }
+    } else if (n < m) {
+        int i = 0, j = m - n;
+        while (i < b.size() && j--) {
+            result.push_back("1" + b.substr(i));
+            i++;
+        }
+        while (j--) {
+            result.push_back("1");
+        }
+    } else {
+        for (char &c : a) {
+            if (c == '1') {
+                result.push_back("1");
+            } else {
+                result.push_back("0");
+            }
+        }
+    }
+
+    return result;
 }
