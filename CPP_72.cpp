5c9c148944cf07d2b327cdaf9a3c00c23fc6dec8
beep boop <githubbot@vadim.me>
2024-07-06 02:40:36 +0200

    Solution 9 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/br4jenym Prompt: 8f81bc78-6241-42b9-b842-bb5afd6c56c4.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 8f81bc78-6241-42b9-b842-bb5afd6c56c4.cpp:16: 8f81bc78-6241-42b9-b842-bb5afd6c56c4.cpp: In function ‘int main()’: 8f81bc78-6241-42b9-b842-bb5afd6c56c4.cpp:18:13: error: ‘will_it_fly’ was not dec lared in this scope    18 |     assert (will_it_fly({5}, 5) == true);       |             ^~~~~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,21 +1,3 @@
-bool will_it_fly(vector<int> q, int w); 
-int main() {
-    vector<int> q;
-    int w;
-    cout << "Enter number of queues: ";
-    cin >> w;
-    for(int i = 0; i < w; ++i) {
-        cout << "Enter queue element: ";
-        int num;
-        cin >> num;
-        q.push_back(num);
-    }
-    if(will_it_fly(q, w))
-        cout << "The structure will fly";
-    else
-        cout << "The structure will not fly";
-    return 0;
-}
 bool will_it_fly(vector<int> q, int w) {
     string str = "";
     for (int i : q) {
