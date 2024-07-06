e11ebd19dc494870130d66e3d58bf9c386cfab79
beep boop <githubbot@vadim.me>
2024-07-06 10:04:49 +0200

    Solution 5 of Python/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/46oamsjz Prompt: Complete the following code given the task description and function signature.

--- Python_117.py
+++ Python_117.py
@@ -1,4 +1,7 @@
-
 def select_words(s, n):
     words = s.split()
-    return [word for word in words if len(word) == n and word.isalpha()]
+    result = []
+    for word in words:
+        if len([c for c in word if c.isalpha() and not c.isdigit()]) == n:
+            result.append(word)
+    return result
