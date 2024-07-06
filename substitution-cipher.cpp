5bc1f91208bace810821221336323dd384933b5c
beep boop <githubbot@vadim.me>
2024-07-06 18:58:37 +0200

    Solution 22 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/fh92ekhg Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 6f602b6b-d0d1-48e1-aa19-a1667c749c90.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -1,16 +1,12 @@
-#include <unordered_map>
 #include <iostream>
 #include <string>
 
 std::string decode(std::string cipher, std::string mapping) {
-    std::unordered_map<char, char> decryptMap;
-    for (int i = 0; i < mapping.size(); ++i) {
-        decryptMap[cipher[i]] = mapping[i];
-    }
     std::string result = "";
     for (char c : cipher) {
-        if (decryptMap.find(c) != decryptMap.end()) {
-            result += decryptMap[c];
+        int idx = mapping.find(c);
+        if (idx != std::string::npos) {
+            result += mapping[idx];
         } else {
             result += c;
         }
@@ -23,4 +19,3 @@ int main() {
     std::cin >> cipher >> mapping;
     std::cout << decode(cipher, mapping) << std::endl;
     return 0;
-}
