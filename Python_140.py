27d50263f4a7f75b8bd2af19df4e68a490e0f973
beep boop <githubbot@vadim.me>
2024-07-06 09:07:04 +0200

    Solution 36 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7zk9q7id Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/57a1b172-0fd5-4337-9b97-c58f6ef6a fd7.py", line 14, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/57a1b172-0fd5-4337-9b97-c58f6ef6a fd7.py", line 10, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,10 +1,6 @@
+```
 def fix_spaces(text):
-    new_text = " "
-    for char in text:
-        if char == " " and new_text[-1] != " ":
-            new_text += "_"
-        elif char == " " and new_text[-1] == " ":
-            new_text += "-"
-        else:
-            new_text += char
-    return new_text
+    text = text.replace("  ", " ")  
+    while "  " in text:  
+        text = text.replace("  ", " ")
+    return text.replace(" ", "_")
