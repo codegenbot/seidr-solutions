0736d3868732c5cde812b69ffa92740581e8e13d
beep boop <githubbot@vadim.me>
2024-07-06 09:01:10 +0200

    Solution 36 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/duae0477 Prompt:       |            ^ 35fd2b39-5049-45ef-8d51-d200e7fb8e43.cpp:21:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    21 | int main(){       |         ^~ 35fd2b39-5049-45ef-8d51-d200e7fb8e43.cpp:21:9: note: remove parentheses to defau lt-initialize a variable    21 | int main(){       |         ^~       |         -- 35fd2b39-5049-45ef-8d51-d200e7fb8e43.cpp:21:9: note: or replace parentheses with  braces to value-initialize a variable 35fd2b39-5049-45ef-8d51-d200e7fb8e43.cpp:21:11: error: a function-definition is not allowed here before ‘{’ token    21 | int main(){       |           ^ 35fd2b39-5049-45ef-8d51-d200e7fb8e43.cpp:23:2: error: expected ‘}’ at end of inp ut    23 | }       |  ^ 35fd2b39-5049-45ef-8d51-d200e7fb8e43.cpp:6:32: note: to match this ‘{’     6 | int solutions(vector<int> lst) {       |                                ^

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,3 +1,9 @@
+```
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
 int solutions(vector<int> lst) {
     int sum = 0;
     for(int i = 0; i < lst.size(); i++) {
@@ -7,7 +13,6 @@ int solutions(vector<int> lst) {
     }
     return sum;
 
-int main() {
+void main() {
     assert(solutions({3, 13, 2, 9}) == 3);
-    return 0;
-}
+};
