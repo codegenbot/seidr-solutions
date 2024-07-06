a5d5c532de24ca96a7a2d109834fb70d6a747dc8
beep boop <githubbot@vadim.me>
2024-07-06 18:47:17 +0200

    Solution 51 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7crd45z9 Prompt: r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <cmath>   +++ |+#include <cassert>     4 | 57bec5d1-6bd7-47dc-b784-beada2af9607.cpp: At global scope: 57bec5d1-6bd7-47dc-b784-beada2af9607.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 57bec5d1-6bd7-47dc-b784-beada2af9607.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from 57bec5d1-6bd7-47dc-b784-beada2af9607.cpp:28: 57bec5d1-6bd7-47dc-b784-beada2af9607.cpp: In function ‘int main()’: 57bec5d1-6bd7-47dc-b784-beada2af9607.cpp:30:35: error: ‘lst’ was not declared in  this scope    30 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 57bec5d1-6bd7-47dc-b784-beada2af9607.cpp:30:43: error: ‘odd_sum’ was not declare d in this scope    30 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -16,6 +16,7 @@ long long double_the_difference(std::vector<float> lst){
 int main() {
     std::vector<float> lst = {1,3,5,7};
     long long odd_sum = 0; 
+
     for(auto x : lst){
         if(int(x) > 0 && int(x) % 2 != 0)
             odd_sum += int(x);
