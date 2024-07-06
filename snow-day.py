9c08244e2f5c3b02d194b2b467d2d641ec528aa5
beep boop <githubbot@vadim.me>
2024-07-06 10:24:35 +0200

    Solution 46 of snow-day, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/yg65jg38 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8d6cc6bb-fd82-44b0-943c-c0b449dc2 6a8.py", line 1     def snow_day(hours, snow_on_ground, snow_fall_rate, melting_proportion): IndentationError: unexpected indent

--- snow-day.py
+++ snow-day.py
@@ -1,2 +1,2 @@
 def snow_day(hours, snow_on_ground, snow_fall_rate, melting_proportion):
-    return round(snow_on_ground + (hours * snow_fall_rate) - (melting_proportion * (snow_on_ground + (hours * snow_fall_rate))) * 10, 1)
+    return round((snow_on_ground + (hours * snow_fall_rate)) / (1 - melting_proportion), 1)
