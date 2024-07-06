9ebf573b1ca7941b0cda70815d5ae022e4750ab3
beep boop <githubbot@vadim.me>
2024-07-06 05:58:32 +0200

    Solution 93 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/8xdfryj1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b3fb060c-97a2-4982-883a-47b5d9b73 f34.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -9,6 +9,6 @@ def find_zero(xs: list):
     b = -sum(x for x in xs[:-1])
     c = sum(x * (i-1) for i, x in enumerate(xs[:-1], 2))
     d = -sum(x * math.factorial(i-1) for i, x in enumerate(xs[:-1], 2))
-
+    
     x = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
     return round(x, 2)
