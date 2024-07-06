5e7f17768880d00ed1874473963535d61bfbee34
beep boop <githubbot@vadim.me>
2024-07-06 02:06:02 +0200

    Solution 20 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/u4hjxqlv Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -1,10 +1,8 @@
-
 def find_zero(xs: list[int]) -> float:
     if len(xs) == 0:
         raise ValueError("Input list must not be empty.")
     for i in range(len(xs)):
         if xs[i] == 0:
             continue
-        x = -xs[i-1] / (xs[i] * xs[i])
+        x = -xs[i - 1] / (xs[i] * xs[i])
         return round(x, 2)
-```
