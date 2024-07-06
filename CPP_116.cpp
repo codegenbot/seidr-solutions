db7ecc98049de951e214627d751cc137c83dda9b
beep boop <githubbot@vadim.me>
2024-07-06 17:25:35 +0200

    Solution 4 of CPP/116, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0dhohf6g Prompt: Program failed

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 
 vector<int> sort_array(vector<int> arr) {
-    sort(arr.begin(), arr.end(), [](int a, int b) {
+    vector<int> arrCopy = arr;  
+    sort(arrCopy.begin(), arrCopy.end(), [](int a, int b) {
         int countA = __builtin_popcount(a);
         int countB = __builtin_popcount(b);
 
@@ -13,5 +14,5 @@ vector<int> sort_array(vector<int> arr) {
         else
             return countA < countB;
     });
-    return arr;
+    return arrCopy;  
 }
