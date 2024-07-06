63847b6362061c753271fad2cb5c5e4a688d6852
beep boop <githubbot@vadim.me>
2024-07-06 16:54:42 +0200

    Solution 12 of CPP/107, ratio 0.875 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ouct4po2 Prompt: Program failed

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,33 +1,40 @@
-#include <stdio.h>
+#include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 
 vector<int> even_odd_palindrome(int n) {
     vector<int> result(2);
-    int count_even = 0;
-    int count_odd = 0;
-
+    int count_even_pal = 0;
+    int count_odd_pal = 0;
+    
     for (int i = 1; i <= n; i++) {
         string str = to_string(i);
         bool is_palindrome = true;
-
-        for (int j = 0; j < str.length() / 2; j++) {
+        
+        for (int j = 0; j < str.length(); j++) {
             if (str[j] != str[str.length() - 1 - j]) {
                 is_palindrome = false;
                 break;
             }
         }
-
-        if (is_palindrome && i % 2 == 0) {
-            count_even++;
-        } else if (is_palindrome && i % 2 != 0) {
-            count_odd++;
+        
+        if (is_palindrome) {
+            if (i % 2 == 0)
+                count_even_pal++;
+            else
+                count_odd_pal++;
+        } 
+        // non-palindrome number
+        else {
+            if (i % 2 == 0)
+                result[0]++;
+            else
+                result[1]++;
         }
     }
-
-    result[0] = count_even;
-    result[1] = count_odd;
-
+    
+    result[0] = count_even_pal;
+    result[1] = count_odd_pal;
+    
     return result;
 }
