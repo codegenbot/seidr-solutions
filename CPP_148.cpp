b03b1ff0b68da30f6cc7809d44cb664241d187b5
beep boop <githubbot@vadim.me>
2024-07-06 10:16:48 +0200

    Solution 5 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gkv6k2hw Prompt:       |                              ^~~~~~ b7f157cd-18c4-46e2-8463-0d96b232c804.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b7f157cd-18c4-46e2-8463-0d96b232c804.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ b7f157cd-18c4-46e2-8463-0d96b232c804.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from b7f157cd-18c4-46e2-8463-0d96b232c804.cpp:29: b7f157cd-18c4-46e2-8463-0d96b232c804.cpp: In function ‘int main()’: b7f157cd-18c4-46e2-8463-0d96b232c804.cpp:39:20: error: ‘bf’ was not declared in this scope    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ b7f157cd-18c4-46e2-8463-0d96b232c804.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,12 +1,5 @@
-#include <vector>
-#include <algorithm>
-
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-
-std::vector<std::string> bf(std::string planet1, std::string planet2) {
-    std::vector<std::string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+vector<string> bf(string planet1, string planet2) {
+    vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
     int index1 = -1;
     int index2 = -1;
 
@@ -22,13 +15,14 @@ std::vector<std::string> bf(std::string planet1, std::string planet2) {
         return {};
     }
 
-    std::vector<std::string> result;
+    vector<string> result;
+
     for (int i = 0; i < planets.size(); i++) {
         if (i >= index1 && i <= index2) {
             result.push_back(planets[i]);
         }
     }
 
-    std::sort(result.begin(), result.end());
+    sort(result.begin(), result.end());
     return result;
 }
