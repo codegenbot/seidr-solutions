ad090b203cf6d8b6899a8d12b012f17f2b5524fc
beep boop <githubbot@vadim.me>
2024-07-06 17:58:58 +0200

    Solution 68 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5ux7p9qd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a624a1e7-67fb-4df3-b94a-0a7dda1a0 c76.py", line 13, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a624a1e7-67fb-4df3-b94a-0a7dda1a0 c76.py", line 10, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,8 +1,9 @@
+```Python
 def modp(x, y):
     return x % y
 
 def check(function, arguments):
-    if str(inspect.getsource(function)) == "def modp(x, y): return x % y":
-        return f"Modular Polynomial Arithmetic Function is Correct for {arguments}"
+    if function(*arguments) == modp(*arguments):
+        return f"{function.__name__} is Correct for {arguments}"
     else:
         return "Error: Not a Modular Polynomial Arithmetic Function"
