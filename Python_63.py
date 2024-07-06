c4562c15ca5dfb279a9235e964f5e27cf78ba5fb
beep boop <githubbot@vadim.me>
2024-07-06 17:58:57 +0200

    Solution 36 of Python/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0kwfpmzd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bafb0e36-37c5-475c-b7cd-f301adf3a 72f.py", line 15, in <module>     check(fibfib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bafb0e36-37c5-475c-b7cd-f301adf3a 72f.py", line 12, in check     assert fibfib(14) == 927            ^^^^^^^^^^^^^^^^^ AssertionError

--- Python_63.py
+++ Python_63.py
@@ -1,12 +1,7 @@
 def fibfib(n: int):
-    if n < 0:
-        return "Error: Input must be non-negative."
-    elif n == 0:
+    if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
-        a, b = 0, 1
-        for _ in range(2, n + 1):
-            a, b = b, a + b
-        return b
+        return fibfib(n - 1) + fibfib(n - 2)
