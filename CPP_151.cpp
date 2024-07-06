b8ec8330a4fa8f515b547c87e5c50cddeda40ec6
beep boop <githubbot@vadim.me>
2024-07-06 18:47:00 +0200

    Solution 49 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3z6pdsi9 Prompt: afaaafb2-15f2-4bef-bac2-1225e33fc72f.cpp:1:33: error: ‘vector’ was not declared in this scope     1 | long long double_the_difference(vector<float> lst) {       |                                 ^~~~~~ afaaafb2-15f2-4bef-bac2-1225e33fc72f.cpp:1:40: error: expected primary-expressio n before ‘float’     1 | long long double_the_difference(vector<float> lst) {       |                                        ^~~~~ In file included from afaaafb2-15f2-4bef-bac2-1225e33fc72f.cpp:13: afaaafb2-15f2-4bef-bac2-1225e33fc72f.cpp: In function ‘int main()’: afaaafb2-15f2-4bef-bac2-1225e33fc72f.cpp:15:35: error: ‘lst’ was not declared in  this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ afaaafb2-15f2-4bef-bac2-1225e33fc72f.cpp:15:38: error: ‘double_the_difference’ c annot be used as a function    15 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ afaaafb2-15f2-4bef-bac2-1225e33fc72f.cpp:15:43: error: ‘odd_sum’ was not declare d in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,16 +1,13 @@
 #include <vector>
 #include <cmath>
 
-long long doubleTheDifference(std::vector<float> lst) {
+long long double_the_difference(std::vector<float> numbers) {
     long long sum = 0;
-    for (float num : lst) {
-        if (num > 0 && floor(num) == num) { 
-            int integerPart = (int)num;
-            float fractionalPart = modf(num, &num);
+    for (float num : numbers) {
+        if (num > 0 && std::floor(num) == num) { 
+            auto fractionalPart = modf(sqrt(num), &num);
             if (fractionalPart != 0.0) { 
                 sum += pow(num, 2); 
-            } else {
-                sum += pow(integerPart, 2);
             }
         }
     }
