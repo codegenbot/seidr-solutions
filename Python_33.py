b4f5ee466c72c2b4edb678ae60ba19cfbce8420c
beep boop <githubbot@vadim.me>
2024-07-06 21:46:20 +0200

    Solution 23 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/skztahss Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/74343176-1361-4222-9247-5c5187493 c15.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/74343176-1361-4222-9247-5c5187493 c15.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -5,5 +5,5 @@ def sort_third(l: list):
         if i % 3 != 0:
             result.append(l[i])
         elif result and not isinstance(result[-1], list):
-            result.append(sorted([x for x in l if i % 3 == 0]))
+            result.append(sorted([x for x in l if i % 3 == 0])))
     return [item for sublist in result for item in sublist]
