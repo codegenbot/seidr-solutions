952febeb2ff3b17445005db298ea856c018dc1af
beep boop <githubbot@vadim.me>
2024-07-06 19:36:20 +0200

    Solution 48 of CPP/58, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0bs7rxb5 Prompt: 0ecf6046-c3b8-499d-8da0-b925ad63d15e.cpp:3:1: note: ‘std::set’ is defined in hea der ‘<set>’; did you forget to ‘#include <set>’?     2 | #include<algorithm>   +++ |+#include <set>     3 | using namespace std; 0ecf6046-c3b8-499d-8da0-b925ad63d15e.cpp:7:9: error: expected primary-expression  before ‘int’     7 |     set<int> s1(l1.begin(),l1.end());       |         ^~~ 0ecf6046-c3b8-499d-8da0-b925ad63d15e.cpp:8:9: error: expected primary-expression  before ‘int’     8 |     set<int> s2(l2.begin(),l2.end());       |         ^~~ 0ecf6046-c3b8-499d-8da0-b925ad63d15e.cpp:10:18: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    10 |     for(auto i : s1)       |                  ^~       |                  l1 0ecf6046-c3b8-499d-8da0-b925ad63d15e.cpp:11:12: error: ‘s2’ was not declared in this scope; did you mean ‘l2’?    11 |         if(s2.find(i) != s2.end())       |            ^~       |            l2

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,14 +1,17 @@
-#include <set>
+#include<vector>
+#include<set>
+#include<algorithm>
 using namespace std;
 
-vector<int> common(vector<int> l1, vector<int> l2) {
-    set<int> s1(l1.begin(), l1.end());
-    set<int> s2(l2.begin(), l2.end());
+vector<int> common(vector<int> l1,vector<int> l2){
+    vector<int> result;
+    set<int> s11(l1.begin(),l1.end());
+    set<int> s22(l2.begin(),l2.end());
 
-    set<int> intersection;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                      inserter(intersection, intersection.begin()));
+    for(auto i : s11)
+        if(s22.find(i) != s22.end())
+            result.push_back(i);
 
-    vector<int> result(intersection.begin(), intersection.end());
+    sort(result.begin(),result.end());
     return result;
 }
