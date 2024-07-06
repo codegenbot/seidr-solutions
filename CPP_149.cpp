44d495ee6849951eb075ebe070f09afdd0fcf9ac
beep boop <githubbot@vadim.me>
2024-07-06 19:10:56 +0200

    Solution 80 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ccu534fs Prompt:    45 | bool issame(vector<string> a,vector<string>b){       |                                            ^ b1eaa213-6f8a-4f66-8e53-aee32b1ad101.cpp:53:5: error: redefinition of ‘int main( )’    53 | int main(){       |     ^~~~ b1eaa213-6f8a-4f66-8e53-aee32b1ad101.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from b1eaa213-6f8a-4f66-8e53-aee32b1ad101.cpp:44: b1eaa213-6f8a-4f66-8e53-aee32b1ad101.cpp: In function ‘int main()’: b1eaa213-6f8a-4f66-8e53-aee32b1ad101.cpp:54:20: error: ‘sorted_list_sum’ was not  declared in this scope    54 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ b1eaa213-6f8a-4f66-8e53-aee32b1ad101.cpp:54:19: error: ‘issame’ cannot be used a s a function    54 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -15,4 +15,28 @@ bool issame(vector<string> a, vector<string> b) {
     return true;
 }
 
-cout << (issame(v1, v2)? "Same" : "Not Same") << endl;
+int main() {
+    int n;
+    cin >> n;
+
+    vector<string> v1, v2;
+
+    for(int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        v1.push_back(s);
+    }
+
+    for(int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        v2.push_back(s);
+    }
+
+    if(issame(v1, v2)) 
+        cout << "Same" << endl;
+    else
+        cout << "Not Same" << endl;
+
+    return 0;
+}
