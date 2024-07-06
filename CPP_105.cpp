b57c2a05ad3f8d645f25a62e2949c61a4ae97c9d
beep boop <githubbot@vadim.me>
2024-07-06 21:48:27 +0200

    Solution 57 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/za61nfn8 Prompt:       |                              ^~~~~~ 02f56fce-7ea0-434b-bbff-861737a7300e.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 02f56fce-7ea0-434b-bbff-861737a7300e.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 02f56fce-7ea0-434b-bbff-861737a7300e.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 02f56fce-7ea0-434b-bbff-861737a7300e.cpp:50: 02f56fce-7ea0-434b-bbff-861737a7300e.cpp: In function ‘int main()’: 02f56fce-7ea0-434b-bbff-861737a7300e.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 02f56fce-7ea0-434b-bbff-861737a7300e.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,15 +1,24 @@
 ```
 bool issame(int a, int b) {
-    if (a == b) {
+    if (a == b)
         return true;
-    } else {
+    else
         return false;
-    }
 }
 
 vector<string> by_length(vector<int> arr) {
-    vector<string> result;
+    vector<int> numbers;
     for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            numbers.push_back(num);
+        }
+    }
+
+    sort(numbers.begin(), numbers.end());
+    reverse(numbers.begin(), numbers.end());
+
+    vector<string> result;
+    for (int num : numbers) {
         string str = "";
         switch (num) {
             case 1:
