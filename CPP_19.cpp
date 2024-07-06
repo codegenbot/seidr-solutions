9664dfd735e693805de6987bfa4452a3a22e2d53
beep boop <githubbot@vadim.me>
2024-07-06 10:48:39 +0200

    Solution 3 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/w11sva6y Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,35 +1,35 @@
-#include <algorithm>
-using namespace std;
-
 string sort_numbers(string numbers) {
     map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
                                 {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
                                 {"eight", 8}, {"nine", 9}};
-
     vector<string> numVec;
     string temp;
-
-    for (int i = 0; i < numbers.length(); i++) {
-        if (numbers[i] == ' ') {
+    
+    for(int i = 0; i < numbers.length(); i++) {
+        if(numbers[i] == ' ') {
             continue;
         }
-        for (int j = i + 1; j <= numbers.length(); j++) {
-            if (j == numbers.length() || numbers[j] == ' ') {
-                temp = numbers.substr(i, j - i);
-                numVec.push_back(temp);
-                i = j;
+        temp += numbers[i];
+        
+        for(int j = i + 1; j <= numbers.length(); j++) {
+            if(j == numbers.length() || numbers[j] != ' ') {
                 break;
             }
+            temp += numbers[j];
+        }
+        
+        numVec.push_back(temp);
+        temp.clear();
+    }
+    
+    sort(numVec.begin(), numVec.end());
+    string result = "";
+    for(int i = 0; i < numVec.size(); i++) {
+        result += numMap[numVec[i]] < numMap[numVec[0]] ? "zero" : numVec[i];
+        if(i != numVec.size() - 1) {
+            result += " ";
         }
     }
-
-    sort(numVec.begin(), numVec.end(), [&numMap](string a, string b) {
-        return to_string(numMap[a]) < to_string(numMap[b]);
-    });
-
-    string result;
-    for (auto &num : numVec) {
-        result += num + " ";
-    }
-    return result.substr(0, result.length() - 1);
+    
+    return result;
 }
