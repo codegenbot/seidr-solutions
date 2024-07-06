469f38fd28b91ead117992680269858a4a369b00
beep boop <githubbot@vadim.me>
2024-07-06 11:51:46 +0200

    Solution 96 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ssvdmxr6 Prompt:    14 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ 2f8675bc-e6fe-4000-8733-231465cdb98c.cpp:14:37: error: ‘string’ was not declared  in this scope    14 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 2f8675bc-e6fe-4000-8733-231465cdb98c.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 2f8675bc-e6fe-4000-8733-231465cdb98c.cpp:13: 2f8675bc-e6fe-4000-8733-231465cdb98c.cpp: In function ‘int main()’: 2f8675bc-e6fe-4000-8733-231465cdb98c.cpp:23:20: error: ‘select_words’ was not de clared in this scope    23 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 2f8675bc-e6fe-4000-8733-231465cdb98c.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -6,7 +6,9 @@ bool issame(vector<std::string> a, vector<std::string> b) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) { 
+        if (a[i] < b[i]) { 
+            return true;
+        } else if (a[i] > b[i]) { 
             return false;
         }
     }
