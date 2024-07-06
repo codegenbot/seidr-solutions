07a37f8218994b85a6dcb60aeb744f8dd2bf715c
beep boop <githubbot@vadim.me>
2024-07-06 14:08:07 +0200

    Solution 78 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zddpzc52 Prompt: 7ac19024-507a-4c1d-8610-35cf2cdc5922.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 7ac19024-507a-4c1d-8610-35cf2cdc5922.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 7ac19024-507a-4c1d-8610-35cf2cdc5922.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 7ac19024-507a-4c1d-8610-35cf2cdc5922.cpp:2:10: fatal error: CryptoPP/MD5.h: No s uch file or directory     2 | #include <CryptoPP/MD5.h>       |          ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -8,9 +8,7 @@ string string_to_md5(string text) {
     if (text.empty()) return "";
 
     MD5 md;
-    byte *buf = (byte *)text.c_str();
-    size_t len = text.length() + 1; // need +1 for null-terminator
-    md.Update(buf, (unsigned long)len);
+    md.Update((const byte*)text.c_str(), (int)text.length());
     string result = HexEncoder().CalculateString(md.Digest());
 
     return result;
