d497ddfae3fa7585891b2d7e3c0bf3d2450ddd82
beep boop <githubbot@vadim.me>
2024-07-06 04:04:15 +0200

    Solution 74 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3vmnpvqv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/06878399-8c30-4d33-bae4-931745858 fd7.py", line 23, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/06878399-8c30-4d33-bae4-931745858 fd7.py", line 19, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,17 +1,4 @@
 def fix_spaces(text):
     if text[0].isspace():
-        result = '-' if text[0] == ' ' else '_'
-    else:
-        result = ''
-    prev_was_space = True
-    for char in text[1:]:
-        if char.isspace():
-            if not prev_was_space:
-                result += '-'
-            else:
-                result += '_'
-            prev_was_space = True
-        else:
-            result += char
-            prev_was_space = False
-    return result
+        return '_' + ''.join(['-' if c.isspace() else c for c in text[1:]])
+    return '-'.join(['_' if c.isspace() else c for c in text])
