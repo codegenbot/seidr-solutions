ac77cbcdb6a93040079b5ec38cd7a272f4e106f4
beep boop <githubbot@vadim.me>
2024-07-06 05:57:47 +0200

    Solution 11 of CPP/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nrlcvbn5 Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from c37b76f9-d13d-4f4d-8867-8fac6ed738a9.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ c37b76f9-d13d-4f4d-8867-8fac6ed738a9.cpp:36:44: error: ‘b’ was not declared in t his scope    36 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from c37b76f9-d13d-4f4d-8867-8fac6ed738a9.cpp:35: c37b76f9-d13d-4f4d-8867-8fac6ed738a9.cpp: In function ‘int main()’: c37b76f9-d13d-4f4d-8867-8fac6ed738a9.cpp:45:20: error: ‘words_string’ was not de clared in this scope    45 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |                    ^~~~~~~~~~~~ c37b76f9-d13d-4f4d-8867-8fac6ed738a9.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~

--- CPP_101.cpp
+++ CPP_101.cpp
@@ -33,6 +33,6 @@ std::vector<std::string> words_string(std::string s) {
 }
 
 int main() {
-    assert(issame(words_string("ahmed     , gamal"), std::vector<std::string>{ "ahmed", "gamal" }));
+    assert(issame(words_string("ahmed     , gamal"), std::vector<std::string>{"ahmed", "gamal"}));
     // ...
 }
