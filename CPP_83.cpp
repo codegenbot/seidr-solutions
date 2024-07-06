dc019853f4f2ed536b1c5e9658a026463999edde
beep boop <githubbot@vadim.me>
2024-07-06 05:43:00 +0200

    Solution 56 of CPP/83, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7jp0kji8 Prompt: 20277a2f-364a-40b1-bca9-e4fcc911277a.cpp: In function ‘bool starts_one_ends(int) ’: 20277a2f-364a-40b1-bca9-e4fcc911277a.cpp:14:42: error: invalid operands of types  ‘__gnu_cxx::__promote_2<long long int, int, double, double>::__type’ {aka ‘doub le’} and ‘int’ to binary ‘operator%’    14 |             if ((num / pow(10LL, n - 1)) % 10 == 1 || num % 10 == 1) {       |                 ~~~~~~~~~~~~~~~~~~~~~~~~ ^ ~~       |                      |                     |       |                      |                     int       |                      __gnu_cxx::__promote_2<long long int, int, double, double>::__type {aka double} 20277a2f-364a-40b1-bca9-e4fcc911277a.cpp: At global scope: 20277a2f-364a-40b1-bca9-e4fcc911277a.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 20277a2f-364a-40b1-bca9-e4fcc911277a.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -9,9 +9,9 @@ bool starts_one_ends(int n) {
         if (i == 1 || i % 10 == 1) {
             count++;
         }
-        for (int j = 1; j < pow(10, n); j++) {
-            int num = i * pow(10, n - 1) + j;
-            if ((num / pow(10, n - 1)) % 10 == 1 || num % 10 == 1) {
+        for (int j = 1; j < (long long)pow(10, n); j++) {
+            int num = i * (long long)pow(10, n - 1) + j;
+            if ((num / (long long)pow(10, n - 1)) % 10 == 1 || num % 10 == 1) {
                 count++;
             }
         }
