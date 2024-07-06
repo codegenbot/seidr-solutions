db41d6ad60e0ae45db07658efdb1ff41d8a2f668
beep boop <githubbot@vadim.me>
2024-07-06 20:19:36 +0200

    Solution 95 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cge2wy3k Prompt: 2b9160be-7c7c-4450-bcd7-aa4b730d071b.cpp:1:6: error: ‘string’ in namespace ‘std’  does not name a type     1 | std::string intToMiniRoman(int number) {       |      ^~~~~~ 2b9160be-7c7c-4450-bcd7-aa4b730d071b.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | std::string intToMiniRoman(int number) {

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,62 +1,123 @@
 #include <string>
 
 std::string intToMiniRoman(int number) {
-    std::string miniRoman = "";
+    if (number > 3999) {
+        return "Invalid Roman Numeral";
+    }
     
-    while (number > 0) {
-        if (number >= 1000) {
-            for (int i = 0; i < 3; i++) {
-                miniRoman += "M";
-                number -= 1000;
-            }
-        } else if (number >= 900) {
-            miniRoman += "CM";
+    std::string roman = "";
+    
+    while (number >= 1000) {
+        roman += "M";
+        number -= 1000;
+    }
+    
+    while (number >= 900) {
+        roman += "CM";
+        number -= 900;
+    }
+    
+    while (number >= 500) {
+        roman += "D";
+        number -= 500;
+    }
+    
+    while (number >= 400) {
+        roman += "CD";
+        number -= 400;
+    }
+    
+    while (number >= 100) {
+        if (number >= 900) {
+            roman += "CM";
             number -= 900;
         } else if (number >= 500) {
-            for (int i = 0; i < 2; i++) {
-                miniRoman += "D";
-                number -= 500;
-            }
+            roman += "D";
+            number -= 500;
         } else if (number >= 400) {
-            miniRoman += "CD";
+            roman += "CD";
             number -= 400;
-        } else if (number >= 100) {
-            for (int i = 0; i < number / 100; i++) {
-                miniRoman += "C";
+        } else {
+            while (number >= 100) {
+                roman += "C";
                 number -= 100;
             }
-        } else if (number >= 90) {
-            miniRoman += "XC";
+        }
+    }
+    
+    while (number >= 90) {
+        roman += "XC";
+        number -= 90;
+    }
+    
+    while (number >= 50) {
+        if (number >= 500) {
+            roman += "D";
+            number -= 500;
+        } else if (number >= 900) {
+            roman += "CM";
+            number -= 900;
+        } else {
+            roman += "L";
+            number -= 50;
+        }
+    }
+    
+    while (number >= 40) {
+        roman += "XL";
+        number -= 40;
+    }
+    
+    while (number >= 10) {
+        if (number >= 90) {
+            roman += "XC";
             number -= 90;
         } else if (number >= 50) {
-            for (int i = 0; i < number / 50; i++) {
-                miniRoman += "L";
-                number -= 50;
-            }
-        } else if (number >= 40) {
-            miniRoman += "XL";
-            number -= 40;
-        } else if (number >= 10) {
-            for (int i = 0; i < number / 10; i++) {
-                miniRoman += "X";
+            roman += "L";
+            number -= 50;
+        } else {
+            while (number >= 10) {
+                roman += "X";
                 number -= 10;
             }
-        } else if (number >= 9) {
-            miniRoman += "IX";
+        }
+    }
+    
+    while (number >= 9) {
+        roman += "IX";
+        number -= 9;
+    }
+    
+    while (number >= 5) {
+        if (number >= 50) {
+            roman += "L";
+            number -= 50;
+        } else if (number >= 90) {
+            roman += "XC";
+            number -= 90;
+        } else {
+            roman += "V";
+            number -= 5;
+        }
+    }
+    
+    while (number >= 4) {
+        roman += "IV";
+        number -= 4;
+    }
+    
+    while (number >= 1) {
+        if (number >= 9) {
+            roman += "IX";
             number -= 9;
         } else if (number >= 5) {
-            for (int i = 0; i < number / 5; i++) {
-                miniRoman += "V";
-                number -= 5;
-            }
-        } else if (number >= 4) {
-            miniRoman += "IV";
-            number -= 4;
+            roman += "V";
+            number -= 5;
         } else {
-            miniRoman += "I";
+            roman += "I";
             number -= 1;
         }
     }
     
-    return miniRoman;
+    return roman;
 }
