4100e89c0673e4beb5999653199d6356730db6d2
beep boop <githubbot@vadim.me>
2024-07-06 17:33:04 +0200

    Solution 1 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/rmvd3wj8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3664013f-b9da-460d-84a6-87ecdc9c2 744.py", line 10, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3664013f-b9da-460d-84a6-87ecdc9c2 744.py", line 7, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3664013f-b9da-460d-84a6-87ecdc9c2 744.py", line 3, in fruit_distribution     return n - int(s.split()[2].split(" ")[0])                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: 'and'

--- Python_67.py
+++ Python_67.py
@@ -1,3 +1,7 @@
 def fruit_distribution(s, n):
     s = s.replace("apples", "0").replace("oranges", "0")
-    return n - int(s.split()[2].split(" ")[0])
+    parts = s.split()
+    for i, part in enumerate(parts[2:]):
+        if part.isdigit():
+            return n - int(part)
+    return 0
