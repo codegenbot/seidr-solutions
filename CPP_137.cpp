edad462b5b0a5affaf0a839c81edebee87a754a5
beep boop <githubbot@vadim.me>
2024-07-06 19:09:24 +0200

    Solution 84 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/t0ko17pn Prompt:    40 | int main(){       |     ^~~~ a3c5e5ec-db49-4019-bdee-bf1f65656f09.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from a3c5e5ec-db49-4019-bdee-bf1f65656f09.cpp:39: a3c5e5ec-db49-4019-bdee-bf1f65656f09.cpp: In function ‘int main()’: a3c5e5ec-db49-4019-bdee-bf1f65656f09.cpp:41:13: error: ‘boost’ has not been decl ared    41 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ a3c5e5ec-db49-4019-bdee-bf1f65656f09.cpp:41:35: error: expected primary-expressi on before ‘>’ token    41 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ a3c5e5ec-db49-4019-bdee-bf1f65656f09.cpp:41:37: error: ‘compare_one’ was not dec lared in this scope; did you mean ‘compareOne’?    41 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,10 +1,20 @@
-```
 #include <iostream>
 #include <string>
 #include <algorithm>
-#include <sstream>
 
 using namespace std;
+
+int compareOne(string a, string b) {
+    int ai = stoi(a);
+    int bi = stoi(b);
+
+    if (ai > bi)
+        return 1;
+    else if (ai == bi)
+        return 0;
+    else
+        return -1;
+}
 
 int main() {
     cout << "Enter two numbers separated by space: ";
@@ -13,12 +23,14 @@ int main() {
         cin >> input[i];
     }
 
-    int ai = stoi(input[0]);
-    int bi = stoi(input[1]);
+    int result = compareOne(input[0], input[1]);
 
-    if (ai > bi) cout << "First is greater" << endl;
-    else if (ai == bi) cout << "Both are equal" << endl;
-    else cout << "Second is greater" << endl;
+    if (result > 0)
+        cout << "First is greater" << endl;
+    else if (result == 0)
+        cout << "Both are equal" << endl;
+    else
+        cout << "Second is greater" << endl;
 
     return 0;
 }
