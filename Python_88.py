e338cd064684e252ae9e92d2f1252e3f53faaafc
beep boop <githubbot@vadim.me>
2024-07-06 18:01:54 +0200

    Solution 9 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vtonw1kq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/70adcd96-8046-4f94-b52e-088500f26 f09.py", line 10, in <module>     check(sort_array)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/70adcd96-8046-4f94-b52e-088500f26 f09.py", line 7, in check     assert sort_array([21, 14, 23, 11]) == [23, 21, 14, 11], "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: sort_array() takes 0 positional arguments but 1 was given

--- Python_88.py
+++ Python_88.py
@@ -1,2 +1,6 @@
-def sort_array(array):
+def sort_array(input_array=None):
+    if input_array is None:
+        array = list(map(int, input("Enter numbers separated by space: ").split()))
+    else:
+        array = input_array
     return sorted(array)
