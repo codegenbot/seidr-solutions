afbf5a26c3419d3ec0355b89094915fd59862c69
beep boop <githubbot@vadim.me>
2024-07-06 09:51:48 +0200

    Solution 59 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/s5itb1ao Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -1,10 +1,3 @@
-def add(x, y):
+
+def add(x: int, y: int):
     return x + y
-
-
-print("Enter two numbers separated by a space: ")
-user_input = input().split()
-
-result = add(int(user_input[0]), int(user_input[1]))
-
-print("The sum is:", result)
