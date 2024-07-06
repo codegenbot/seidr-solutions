fe244f024328589084a1387efb75603290d0f456
beep boop <githubbot@vadim.me>
2024-07-06 13:17:23 +0200

    Solution 8 of CPP/43, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zyxeqll4 Prompt: 4f9351cb-da94-4b30-89c6-fbe7869d4a8b.cpp:1:24: error: ‘vector’ was not declared in this scope     1 | bool pairs_sum_to_zero(vector<int> l) {       |                        ^~~~~~ 4f9351cb-da94-4b30-89c6-fbe7869d4a8b.cpp:1:31: error: expected primary-expressio n before ‘int’     1 | bool pairs_sum_to_zero(vector<int> l) {       |                               ^~~ In file included from 4f9351cb-da94-4b30-89c6-fbe7869d4a8b.cpp:12: 4f9351cb-da94-4b30-89c6-fbe7869d4a8b.cpp: In function ‘int main()’: 4f9351cb-da94-4b30-89c6-fbe7869d4a8b.cpp:14:30: error: ‘pairs_sum_to_zero’ canno t be used as a function    14 |     assert (pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);       |             ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,9 +1,11 @@
+#include <iostream>
 #include <vector>
 #include <set>
-#include <unordered_set>
+
+using namespace std;
 
 bool pairs_sum_to_zero(vector<int> l) {
-    unordered_set<int> s(l.begin(), l.end());
+    set<int> s(l.begin(), l.end());
     for (int x : s) {
         int complement = -x;
         if (s.find(complement) != s.end() && x != complement) {
