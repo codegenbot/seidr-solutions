2266fa558ecaa8c6dd50979f38b418f2bd6ed1c4
beep boop <githubbot@vadim.me>
2024-07-06 11:30:14 +0200

    Solution 31 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ojdocb9l Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2cceb2cb-e7b6-48d2-a576-b1267a61e bb0.py", line 22, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2cceb2cb-e7b6-48d2-a576-b1267a61e bb0.py", line 18, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,16 +2,17 @@
 def encode(message):
     vowels = 'aeiou'
     encoded_message = ''
+    index = 0
     for char in message:
         if char.isalpha():
-            if char.lower() in vowels:
-                index = ord(char) - ord('a')
+            if char.lower() in 'aeiou':
                 if index % 2 == 0:
                     encoded_message += chr(ord('a') + (index) % 6)
                 else:
                     encoded_message += chr(ord('A') + (index) % 6)
             else:
                 encoded_message += char.swapcase()
+            index += 1
         else:
             encoded_message += char
     return encoded_message
