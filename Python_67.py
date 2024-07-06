a41017dbe0144131b3f807e7fba0bc0b10738466
beep boop <githubbot@vadim.me>
2024-07-06 04:10:53 +0200

    Solution 31 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/kh7c2g1m Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7cf13a4e-c54a-4024-b699-a9b901ce9 3bd.py", line 11, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7cf13a4e-c54a-4024-b699-a9b901ce9 3bd.py", line 8, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7cf13a4e-c54a-4024-b699-a9b901ce9 3bd.py", line 3, in fruit_distribution     apples, oranges = map(int, s.split())     ^^^^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: 'apples'

--- Python_67.py
+++ Python_67.py
@@ -1,5 +1,5 @@
 def fruit_distribution(s, n):
-    apples, oranges = map(int, s.split())
-    if not s.isdigit():
-        raise ValueError("Invalid input")
-    return n - apples - oranges
+   apples, oranges = map(int, s.split())
+   if not s.isdigit():
+      raise ValueError("Invalid input")
+   return n - apples - oranges
