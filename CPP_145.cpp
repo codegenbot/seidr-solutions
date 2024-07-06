079163964b67ded987873e766c6ee9c2a2238925
beep boop <githubbot@vadim.me>
2024-07-06 13:17:02 +0200

    Solution 74 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6h0099y8 Prompt: In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 2c81bdf5-3892-42d5-87b7-de5157408e2f.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 2c81bdf5-3892-42d5-87b7-de5157408e2f.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2c81bdf5-3892-42d5-87b7-de5157408e2f.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2c81bdf5-3892-42d5-87b7-de5157408e2f.cpp:20: 2c81bdf5-3892-42d5-87b7-de5157408e2f.cpp: In function ‘int main()’: 2c81bdf5-3892-42d5-87b7-de5157408e2f.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,24 +1,9 @@
+```cpp
 #include <algorithm>
 #include <vector>
 
-int main() {
-    std::vector<int> nums = {0,6,6,-76,-21,23,4};
-    std::vector<int> res = order_by_points(nums);
-    assert(std::equal(res.begin(), res.end(), {-76, -21, 0, 4, 23, 6, 6}));
-    return 0;
-}
-
 std::vector<int> order_by_points(std::vector<int> nums) {
-    auto compare = [&](int a, int b) {
-        if (a == b) return 0;
-        long sumA = 0, sumB = 0;
-        for (char c : std::to_string(a)) sumA += c - '0';
-        for (char c : std::to_string(b)) sumB += c - '0';
-        if (sumA != sumB) return sumA - sumB > 0 ? 1 : -1;
-        return distance(nums.begin(), find(nums.begin(), nums.end(), a)) -
-               distance(nums.begin(), find(nums.begin(), nums.end(), b));
-    };
     std::vector<int> res = nums;
-    sort(res.begin(), res.end(), compare);
+    sort(res.begin(), res.end());
     return res;
 }
