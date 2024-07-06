c21d7b9e5298c7b4263be7ce2d72798aa2abfe35
beep boop <githubbot@vadim.me>
2024-07-06 08:08:21 +0200

    Solution 45 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/fosqzw1v Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/28f412a3-1a52-4f12-921c-0ecd79d58 827.py", line 31, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/28f412a3-1a52-4f12-921c-0ecd79d58 827.py", line 28, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/28f412a3-1a52-4f12-921c-0ecd79d58 827.py", line 20, in minPath     for neighbor in get_neighbors(grid, cell):                     ^^^^^^^^^^^^^ NameError: name 'get_neighbors' is not defined

--- Python_129.py
+++ Python_129.py
@@ -1,12 +1,13 @@
+
 def get_neighbors(grid, cell):
     row, col = cell
     neighbors = []
     if row > 0:
-        neighbors.append((row - 1, col))
+        neighbors.append((row-1, col))
     if row < len(grid) - 1:
-        neighbors.append((row + 1, col))
+        neighbors.append((row+1, col))
     if col > 0:
-        neighbors.append((row, col - 1))
+        neighbors.append((row, col-1))
     if col < len(grid[0]) - 1:
-        neighbors.append((row, col + 1))
+        neighbors.append((row, col+1))
     return neighbors
