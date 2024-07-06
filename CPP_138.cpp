2a3bb0ea8e170b9049e32b5c0dabdc7cd5a9bc9f
beep boop <githubbot@vadim.me>
2024-07-06 22:40:57 +0200

    Solution 1 of CPP/138, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pvt1ovn8 Prompt: Complete the following code given the task description and function signature.

--- CPP_138.cpp
+++ CPP_138.cpp
@@ -1,13 +1,17 @@
-Here is the solution:
+#include <vector>
+
+using namespace std;
 
 bool is_equal_to_sum_even(int n) {
     for (int i = 2; i <= n; i += 2) {
+        vector<int> numbers;
         int sum = 0;
-        for (int j = 2; j <= n; j += 2) {
-            if (i + j > n) break;
+        for (int j = 2; j <= i; j += 2) {
+            if ((i - sum) < j) break;
+            numbers.push_back(j);
             sum += j;
-            if (sum == n) return true;
         }
+        if (sum == n) return true;
     }
     return false;
 }
