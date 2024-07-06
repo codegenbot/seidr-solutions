c8c67bb7e668810028b9fea3888b31904e30ed5e
beep boop <githubbot@vadim.me>
2024-07-06 11:44:39 +0200

    Solution 38 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/aikobq58 Prompt:       |           ^~~~~~ In file included from 42795831-064a-4ef6-9b63-38b9147cd0b4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 42795831-064a-4ef6-9b63-38b9147cd0b4.cpp:22:36: error: expected primary-expressi on before ‘float’    22 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 42795831-064a-4ef6-9b63-38b9147cd0b4.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 42795831-064a-4ef6-9b63-38b9147cd0b4.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~ In file included from 42795831-064a-4ef6-9b63-38b9147cd0b4.cpp:21: 42795831-064a-4ef6-9b63-38b9147cd0b4.cpp: In function ‘int main()’: 42795831-064a-4ef6-9b63-38b9147cd0b4.cpp:31:20: error: ‘get_positive’ was not de clared in this scope    31 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -12,14 +12,13 @@ std::vector<float> get_positive(std::vector<float> v) {
 
 int main() {
     assert(std::equal(get_positive({}), {}));
-    vector<float> v1 = get_positive({-1, 2, -3, 4, 5});
-    vector<float> v2 = get_positive({1, 2, 3, 4, 5});
+    std::vector<float> v1 = get_positive({-1, 2, -3, 4, 5});
+    std::vector<float> v2 = get_positive({1, 2, 3, 4, 5});
 
     if(issame(v1,v2))
         std::cout << "The two vectors are equal.\n";
     else
         std::cout << "The two vectors are not equal.\n";
 
-    
     return 0;
 }
