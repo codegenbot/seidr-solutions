0e2be09a16276b2013fd90bbc28651c12c97f1a1
beep boop <githubbot@vadim.me>
2024-07-06 17:44:37 +0200

    Solution 13 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/aagr2av9 Prompt:       |                    ^~~ 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp:9: 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp: In function ‘int main()’: 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp:19:20: error: ‘maximum’ was not declare d in this scope    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 1ee9c9f9-c967-4fb9-9525-982d77485752.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -2,36 +2,9 @@
 #include <algorithm>
 
 bool issame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    // your comparison logic here
 }
 
 int main() {
-    vector<int> arr;
-    int n, k;
-    cin >> n >> k;
-    for(int i = 0; i < n; i++) {
-        int val;
-        cin >> val;
-        arr.push_back(val);
-    }
-    vector<int> result(arr.begin(), arr.begin() + k);
-    sort(result.begin(), result.end());
-    if(k > n) {
-        cout << "Error: k cannot be greater than the size of the array." << endl;
-    } else {
-        cout << "The maximum of the first k elements is ";
-        for(int i = 0; i < k; i++) {
-            cout << result[i] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
+    // use issame function here
 }
