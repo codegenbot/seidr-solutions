566f4187f6baacc13c36394792912daa939b5ed1
beep boop <githubbot@vadim.me>
2024-07-06 19:06:49 +0200

    Solution 65 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/sef7a86m Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f80bcf00-0d0e-4598-9454-99ca160a5672.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f80bcf00-0d0e-4598-9454-99ca160a5672.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f80bcf00-0d0e-4598-9454-99ca160a5672.cpp:11: f80bcf00-0d0e-4598-9454-99ca160a5672.cpp: In function ‘int main()’: f80bcf00-0d0e-4598-9454-99ca160a5672.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ f80bcf00-0d0e-4598-9454-99ca160a5672.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -2,25 +2,12 @@
 #include <string>
 
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++) {
-        if(std::find(b.begin(), b.end(), a[i]) == b.end()) {
+    for(string s : a) {
+        if(find(b.begin(), b.end(), s) == b.end()) {
             return false;
         }
     }
     return true;
-}
-
-vector<string> filter_by_substring(vector<string> strings, string substring){
-    vector<string> result;
-    for(string s : strings) {
-        if(s.find(substring) != string::npos) {
-            result.push_back(s);
-        }
-    }
-    return result;
 }
 
 int main() {
