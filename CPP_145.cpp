bbf6ad244657d4bd80d4b47c2d9bdd0675d7a754
beep boop <githubbot@vadim.me>
2024-07-06 10:43:17 +0200

    Solution 20 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6h0099y8 Prompt:    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1dbafb70-e81b-41f3-b246-b9bf15a2afc1.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 1dbafb70-e81b-41f3-b246-b9bf15a2afc1.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main(){       |     ^~~~ In file included from 1dbafb70-e81b-41f3-b246-b9bf15a2afc1.cpp:30: 1dbafb70-e81b-41f3-b246-b9bf15a2afc1.cpp: In function ‘int main()’: 1dbafb70-e81b-41f3-b246-b9bf15a2afc1.cpp:40:20: error: ‘order_by_points’ was not  declared in this scope    40 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 1dbafb70-e81b-41f3-b246-b9bf15a2afc1.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,3 +1,4 @@
+```
 #include <algorithm>
 #include <vector>
 #include <string>
@@ -24,5 +25,5 @@ vector<int> order_by_points(vector<int> nums) {
 }
 
 int main(){
-    assert (order_by_points({0,6,6,-76,-21,23,4}) == vector<int>{-76, -21, 0, 4, 23, 6, 6});
+    assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0, 4, 23, 6, 6}));
 }
