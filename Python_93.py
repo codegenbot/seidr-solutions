7ea061a3da6b4ea782530c07ff8925376a951ea6
beep boop <githubbot@vadim.me>
2024-07-06 19:55:13 +0200

    Solution 94 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/26zr7o11 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d1e59de3-dcdb-41a0-8207-d5f16ab16 427.py", line 11, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d1e59de3-dcdb-41a0-8207-d5f16ab16 427.py", line 7, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg"     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: encode() missing 1 required positional argument: 'mapping'

--- Python_93.py
+++ Python_93.py
@@ -1,23 +1,2 @@
-def check():
-    encoding_mapping = {
-        "i": "k",
-        "I": "K",
-        "d": "d",
-        "o": "QnT",
-        "O": "q",
-        "n": "n",
-        "N": "N",
-        "t": "t",
-        "T": "T",
-        "k": "k",
-        "K": "K",
-        "w": "w",
-        "W": "W",
-        "h": "c",
-        "H": "C",
-        "a": "t",
-        "A": "T",
-        "s": "r",
-        "S": "R",
-    }
-    result = encode("I DoNt KnOw WhAt tO WrItE", encoding_mapping)
+def encode(text, mapping={}):
+    return "".join(mapping.get(i.lower(), i) for i in text)
