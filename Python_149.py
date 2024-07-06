6f6f876e3db459f2e68e03d1f52fcffd211bd319
beep boop <githubbot@vadim.me>
2024-07-06 05:28:14 +0200

    Solution 52 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/w2c7zzis Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/86cc45fd-dcd7-433d-980e-c1d03fbd4 f96.py", line 11, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/86cc45fd-dcd7-433d-980e-c1d03fbd4 f96.py", line 8, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,3 +1,4 @@
-
 def sorted_list_sum(lst):
-    return sorted(lst, key=len)
+    even_words = [word for word in lst]
+    odd_words = [word for word in lst if len(word) % 2 == 1]
+    return (sorted(even_words, key=len), sorted(odd_words, key=len))
