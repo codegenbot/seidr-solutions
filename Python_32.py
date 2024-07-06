6e8ad43a57afb45216c012420363189c44d874c0
beep boop <githubbot@vadim.me>
2024-07-06 02:13:13 +0200

    Solution 29 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/u4hjxqlv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2801d4a3-4983-40d5-9731-874fd69d3 fd3.py", line 1     if len(xs) == 0: IndentationError: unexpected indent

--- Python_32.py
+++ Python_32.py
@@ -1,10 +1,7 @@
-
-    if len(xs) == 0:
-        raise ValueError("Input list must not be empty.")
-    
-    for i in range(len(xs)):
-        if xs[i] == 0:
-            continue
-        
-        x = -xs[i-1] / (xs[i] * xs[i])
-        return round(x, 2)
+if len(xs) == 0:
+    raise ValueError("Input list must not be empty.")
+for i in range(len(xs)):
+    if xs[i] == 0:
+        continue
+    x = -xs[i - 1] / (xs[i] * xs[i])
+    return round(x, 2)
