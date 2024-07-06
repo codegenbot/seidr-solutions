715165dd4ff526fe8951e41cbac294ef41c8f961
beep boop <githubbot@vadim.me>
2024-07-06 20:59:32 +0200

    Solution 95 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7aum6t7i Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/64eeee58-0aa9-409b-b66d-de76e25e0 d0f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,20 +1,17 @@
 def sum_squares(lst):
     total_sum = 0
-    for element in lst:
-        if isinstance(element, dict):
-            for key, value in element.items():
-                if isinstance(value, (int, float)):
-                    total_sum += abs(value) ** 2
-                elif isinstance(value, list):
-                    total_sum += sum([x**2 for x in sum_squares(value)])
-        elif isinstance(element, list):
-            total_sum += sum([x**2 for x in element])
-        elif isinstance(element, (int, float)):
-            total_sum += abs(element) ** 2
-        else:
-            raise ValueError(
-                f"Invalid input '{element}' found. Input must contain only numbers."
-            )
+    if isinstance(lst, (int, float)):
+        total_sum += abs(lst) ** 2
+    elif isinstance(lst, list):
+        for element in lst:
+            if isinstance(element, dict):
+                total_sum += sum([value**2 for key, value in element.items()])
+            elif isinstance(element, list):
+                total_sum += sum([x**2 for x in element])
+            else:
+                raise ValueError(
+                    f"Invalid input '{element}' found. Input must contain only numbers."
+                )
     return total_sum
 
 
