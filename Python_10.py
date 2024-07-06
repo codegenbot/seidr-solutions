c917c77c5bc4d6d7f15657cf5d8182511ed311c7
beep boop <githubbot@vadim.me>
2024-07-06 08:20:21 +0200

    Solution 4 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/yoocl16w Prompt: Complete the following code given the task description and function signature.

--- Python_10.py
+++ Python_10.py
@@ -3,12 +3,8 @@ def is_palindrome(string: str) -> bool:
 
 
 def make_palindrome(string: str) -> str:
-    # Find the longest postfix of supplied string that is a palindrome.
-    suffix = ""
-    for i in range(len(string), 0, -1):
-        if is_palindrome(string[i:]):
-            suffix = string[i:]
-            break
-
-    # Append to the end of the string reverse of a string prefix that comes before the palindromic suffix.
-    return string + suffix[::-1]
+    suffix = string[::-1]
+    for i in range(len(suffix)):
+        if is_palindrome(suffix[: i + 1]):
+            return string + suffix[i + 1 :][::-1]
+    return string
