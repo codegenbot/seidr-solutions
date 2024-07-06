099addeeaefb09b6bc04c557ac71ec6b70f7e6db
beep boop <githubbot@vadim.me>
2024-07-06 02:23:14 +0200

    Solution 16 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ncp26xbv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/97863d4f-9ed4-4fbc-97ac-af5682152 e72.py", line 1     ```"result = """"                   ^ SyntaxError: unterminated triple-quoted string literal (detected at line 10)

--- Python_93.py
+++ Python_93.py
@@ -1,5 +1,1 @@
-def calculate_sum_and_average(input_dict):
-    total = sum(input_dict.values())
-    count = len(input_dict)
-    average = total / count
-    return {"sum": total, "average": average}
+result = """""""
