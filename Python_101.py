01a1600254c2ce0b04ee9e77654c29cace217f22
beep boop <githubbot@vadim.me>
2024-07-06 18:56:36 +0200

    Solution 17 of Python/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/f2mlu349 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e065589a-d872-4115-ba30-698e46f48 0d7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_101.py
+++ Python_101.py
@@ -1,14 +1,11 @@
-```
 import re
 
-def words_string():
-    s = input("Please enter a string with comma-separated values: ")
-    if not s:
-        return []  
-    try:
-        return re.split('?, |\s*,\s*|\s+|,\s*', s.strip())
-    except Exception as e:
-        print(f"Invalid input. Please enter valid text.")
-        return []
 
-print(words_string())
+def words_string():
+    while True:
+        try:
+            s = input("Please enter a string with comma-separated values: ")
+            result = re.split("?, |\s*,\s*|\s+|,\s*", s.strip())
+            return [x for x in result if x]
+        except Exception as e:
+            print(f"Invalid input. Please enter valid text.")
