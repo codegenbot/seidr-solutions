c321f459dd5583ba717b18194a024aef18b588ef
beep boop <githubbot@vadim.me>
2024-07-06 20:05:02 +0200

    Solution 43 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7aum6t7i Prompt: 14 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a761e28b-9c71-4fba-9d2c-aae9c0740 06f.py", line 60, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a761e28b-9c71-4fba-9d2c-aae9c0740 06f.py", line 30, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,16 +1,13 @@
 def sum_squares(lst):
     if isinstance(lst, dict):
-        return sum(map(lambda x: sum_squares(x), lst.values()))
+        return sum(sum_squares(value) for value in lst.values())
     elif not isinstance(lst, list) or len(lst) == 0:
         raise ValueError("Input must be a non-empty list")
 
     total_sum = 0
     for element in lst:
         if isinstance(element, dict):
-            if "data" not in element:
-                total_sum += sum_squares(list(element.values()))
-            else:
-                total_sum += abs(element["data"]) ** 2
+            total_sum += sum_squares(list(element.values()))
         elif not isinstance(element, (int, float)):
             raise ValueError(
                 f"Invalid input '{element}' found. Input must contain only numbers."
