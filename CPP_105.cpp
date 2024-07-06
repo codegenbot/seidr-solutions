cf06a9ea6b2d73424d109cf6ae46fca1649d3359
beep boop <githubbot@vadim.me>
2024-07-06 22:06:33 +0200

    Solution 82 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/za61nfn8 Prompt:       |                              ^~~~~~ a4f17343-359e-4658-bb9b-6e671fddb3ec.cpp:45:37: error: ‘string’ was not declared  in this scope    45 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a4f17343-359e-4658-bb9b-6e671fddb3ec.cpp:45:44: error: ‘b’ was not declared in t his scope    45 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a4f17343-359e-4658-bb9b-6e671fddb3ec.cpp:45:45: error: expression list treated a s compound expression in initializer [-fpermissive]    45 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a4f17343-359e-4658-bb9b-6e671fddb3ec.cpp:44: a4f17343-359e-4658-bb9b-6e671fddb3ec.cpp: In function ‘int main()’: a4f17343-359e-4658-bb9b-6e671fddb3ec.cpp:54:20: error: ‘by_length’ was not decla red in this scope    54 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ a4f17343-359e-4658-bb9b-6e671fddb3ec.cpp:54:19: error: ‘issame’ cannot be used a s a function    54 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size())
         return false;
@@ -18,37 +20,5 @@ vector<string> by_length(vector<int> arr) {
     }
     sort(numbers.begin(), numbers.end());
     reverse(numbers.begin(), numbers.end());
-    vector<string> result;
-    for (string num : numbers) {
-        switch (stoi(num)) {
-            case 1:
-                result.push_back("One");
-                break;
-            case 2:
-                result.push_back("Two");
-                break;
-            case 3:
-                result.push_back("Three");
-                break;
-            case 4:
-                result.push_back("Four");
-                break;
-            case 5:
-                result.push_back("Five");
-                break;
-            case 6:
-                result.push_back("Six");
-                break;
-            case 7:
-                result.push_back("Seven");
-                break;
-            case 8:
-                result.push_back("Eight");
-                break;
-            case 9:
-                result.push_back("Nine");
-                break;
-        }
-    }
-    return result;
+    return numbers;
 }
