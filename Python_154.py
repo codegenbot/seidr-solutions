f726a883cec4c79f11219f601bec1f57933da65b
beep boop <githubbot@vadim.me>
2024-07-06 03:33:02 +0200

    Solution 22 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/z4tucdrt Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f1c0042-0689-4c20-bc13-c164bf351 107.py", line 14, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f1c0042-0689-4c20-bc13-c164bf351 107.py", line 11, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,11 +1,7 @@
-```
 def cycpattern_check(a, b):
     if len(b) > len(a):
-        for i in range(len(b)-len(a)+1):
-            if b[i:i+len(a)] == a:
-                return True
-    else:
-        for i in range(len(a)-len(b)+1):
-            if a[i:i+len(b)] == b:
-                return True
+        return False
+    for i in range(len(a) - len(b) + 1):
+        if a[i : i + len(b)].lower() == b.lower():
+            return True
     return False
