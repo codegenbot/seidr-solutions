79b01c7080cf97ee801950d9f425fb7715a828bc
beep boop <githubbot@vadim.me>
2024-07-06 10:16:22 +0200

    Solution 32 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/rv1do6si Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90c7c2c5-e24b-4d91-bb37-488e34c4a 4f5.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90c7c2c5-e24b-4d91-bb37-488e34c4a 4f5.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,12 @@
 ```
-return [i if i % 3 != 0 else sorted([j for j in l if j % 3 == 0])[(i // 3) % len(sorted([j for j in l if j % 3 == 0]))]
-    for i in range(len(l))
-]
+def sort_third(l: list):
+    multiples_of_three = sorted([i for i in l if i % 3 == 0])
+    result = []
+    multiple_three_count = 0
+    for i in range(len(l)):
+        if i % 3 != 0:
+            result.append(i)
+        else:
+            result.append(multiples_of_three[multiple_three_count])
+            multiple_three_count += 1
+    return tuple(result)
