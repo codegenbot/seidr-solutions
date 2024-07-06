69eb92badf733e77892a47396967ad5a376005ab
beep boop <githubbot@vadim.me>
2024-07-06 11:43:10 +0200

    Solution 53 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/a1tfr568 Prompt:       |             ^~~~~~ a9b091f5-ca13-4354-b195-02a591c39967.cpp:32:34: error: expected primary-expressi on before ‘int’    32 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a9b091f5-ca13-4354-b195-02a591c39967.cpp:32:39: error: expression list treated a s compound expression in initializer [-fpermissive]    32 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a9b091f5-ca13-4354-b195-02a591c39967.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ a9b091f5-ca13-4354-b195-02a591c39967.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from a9b091f5-ca13-4354-b195-02a591c39967.cpp:31: a9b091f5-ca13-4354-b195-02a591c39967.cpp: In function ‘int main()’: a9b091f5-ca13-4354-b195-02a591c39967.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -12,10 +12,8 @@ bool isSame(std::vector<int> a, std::vector<int> b) {
 std::vector<int> maximum(std::vector<int> arr, int k) {
     std::vector<int> result;
     while(k--) {
-        int max_val = *std::max_element(arr.begin(), arr.end());
-        result.push_back(max_val);
-        auto it = std::remove(arr.begin(), arr.end(), max_val);
-        arr.erase(it, arr.end());
+        result.push_back(*std::max_element(arr.begin(), arr.end()));
+        arr.erase(std::remove(arr.begin(), arr.end(), *std::max_element(arr.begin(), arr.end())), arr.end());
     }
     return result;
 }
@@ -24,6 +22,12 @@ int main() {
     int arr[] = {1, 2, 3, -23, 243, -400, 0};
     int n = sizeof(arr)/sizeof(arr[0]);
     std::vector<int> myVector (arr, arr+n);
-    assert(isSame(maximum(myVector, 4), {0, 1, 2, 3}));
+    if(isSame(maximum(myVector, 4), {0, 1, 2, 3})) {
+        for(int i = 0; i < 4; ++i) {
+            std::cout << maximum(myVector, 4)[i] << " ";
+        }
+    } else {
+        std::cout << "Output is not as expected";
+    }
     return 0;
 }
