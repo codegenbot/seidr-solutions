b571d2de8495f6caeffe1e188b8fc2e8bda3993a
beep boop <githubbot@vadim.me>
2024-07-06 18:26:47 +0200

    Solution 13 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q061b090 Prompt: 25a26389-4723-4222-ae34-9def5e155dc8.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string Strongest_Extension(string class_name, vector<string> extensions)  {       | ^~~~~~ In file included from 25a26389-4723-4222-ae34-9def5e155dc8.cpp:19: 25a26389-4723-4222-ae34-9def5e155dc8.cpp: In function ‘int main()’: 25a26389-4723-4222-ae34-9def5e155dc8.cpp:21:13: error: ‘Strongest_Extension’ was  not declared in this scope    21 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 std::string Strongest_Extension(std::string class_name, std::vector<std::string> extensions) {
     int strongest = INT_MIN;
