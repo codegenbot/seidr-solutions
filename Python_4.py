822a61a129aa3aec264f645997709392a11f1963
beep boop <githubbot@vadim.me>
2024-07-06 05:45:35 +0200

    Solution 56 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/f3w5181t Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a8550111-99de-4a97-93f8-d56fe7306 486.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_4.py
+++ Python_4.py
@@ -1,9 +1,6 @@
+```
 from typing import List
 
 def mean_absolute_deviation(numbers: List[float]) -> float:
     mean = sum(numbers) / len(numbers)
     return sum(abs(num - mean) for num in numbers) / len(numbers)
-
-numbers_input = input("Enter space-separated floating point numbers: ")
-numbers = list(map(float, numbers_input.split()))
-print(mean_absolute_deviation(numbers))
