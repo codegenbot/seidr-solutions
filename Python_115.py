804ccf0488ea28cd3017e07695d379e72002a998
beep boop <githubbot@vadim.me>
2024-07-06 04:02:26 +0200

    Solution 82 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1h36up85 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5c6f1378-ac82-4fe5-a23b-7b2cfeeed 9cd.py", line 24, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5c6f1378-ac82-4fe5-a23b-7b2cfeeed 9cd.py", line 21, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_115.py
+++ Python_115.py
@@ -1,21 +1,21 @@
-```
 def max_fill(grid, capacity):
     total_amount = 0
-    left_to_right = [c for r in grid for c in r]
-    right_to_left = left_to_right[::-1]
+    left = [c for r in grid for c in r]
+    right = left[::-1]
+    left_ptr, right_ptr = 0, len(left) - 1
     
-    left_ptr, right_ptr = 0, len(left_to_right) - 1
-    
-    while left_ptr <= right_ptr:
-        min_capacity = min(capacity, int(left_to_right[left_ptr]), int(right_to_left[right_ptr]))
-        
-        total_amount += min_capacity
-        left_to_right[left_ptr] -= min_capacity
-        right_to_left[right_ptr] -= min_capacity
-        
-        if int(left_to_right[left_ptr]) == 0:
+    for i in range(min(left_ptr, right_ptr), max(left_ptr, right_ptr)):
+        if left[i] < capacity and right[len(left) - i - 1] < capacity:
+            total_amount += min(capacity, left[i]) // 2 * 2
+            left[i] -= (min(capacity, left[i])) // 2
+            right[len(left) - i - 1] -= (min(capacity, left[i])) // 2
+        elif left[left_ptr] < capacity:
+            total_amount += min(capacity, left[left_ptr]) // 2 * 2
+            left[left_ptr] -= (min(capacity, left[left_ptr])) // 2
             left_ptr += 1
-        if int(right_to_left[right_ptr]) == 0:
+        else:
+            total_amount += min(capacity, right[right_ptr]) // 2 * 2
+            right[right_ptr] -= (min(capacity, right[right_ptr])) // 2
             right_ptr -= 1
     
     return total_amount
