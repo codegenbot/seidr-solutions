8dbecf5ced3bcaea956d14844e34fc04602d549e
beep boop <githubbot@vadim.me>
2024-07-06 04:17:15 +0200

    Solution 94 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1h36up85 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/62a7bffa-650b-4b16-a6a2-98dfa3681 8db.py", line 28, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/62a7bffa-650b-4b16-a6a2-98dfa3681 8db.py", line 25, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_115.py
+++ Python_115.py
@@ -3,20 +3,24 @@ def max_fill(grid, capacity):
     left = [c for r in grid for c in r]
     right = left[::-1]
     left_ptr, right_ptr = 0, len(left) - 1
-
+    
     while left_ptr <= right_ptr:
         if left[left_ptr] < capacity and right[right_ptr] < capacity:
-            max_amount = min(capacity, left[left_ptr]) // 2 * 2
-            total_amount += max_amount
-            left[left_ptr] -= (min(capacity, left[left_ptr])) // 2
-            right[right_ptr] -= (min(capacity, left[left_ptr])) // 2
+            amount_to_fill = min(capacity, left[left_ptr]) // 2 * 2
+            total_amount += amount_to_fill
+            left[left_ptr] -= amount_to_fill
+            right[right_ptr] -= amount_to_fill
+            left_ptr += 1
+            right_ptr -= 1
         elif left[left_ptr] < capacity:
-            total_amount += min(capacity, left[left_ptr]) // 2 * 2
-            left[left_ptr] -= (min(capacity, left[left_ptr])) // 2
+            amount_to_fill = min(capacity, left[left_ptr]) // 2 * 2
+            total_amount += amount_to_fill
+            left[left_ptr] -= amount_to_fill
             left_ptr += 1
         else:
-            total_amount += min(capacity, right[right_ptr]) // 2 * 2
-            right[right_ptr] -= (min(capacity, right[right_ptr])) // 2
+            amount_to_fill = min(capacity, right[right_ptr]) // 2 * 2
+            total_amount += amount_to_fill
+            right[right_ptr] -= amount_to_fill
             right_ptr -= 1
-
+    
     return total_amount
