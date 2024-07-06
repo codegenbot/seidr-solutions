717bc5e2a2d4dbc38c99f5a1710d39781e382479
beep boop <githubbot@vadim.me>
2024-07-06 04:41:37 +0200

    Solution 45 of Python/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/0bn9vyfs Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/94af5509-4a51-48b6-845b-d55d327d5 363.py", line 39, in <module>     check(count_up_to)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/94af5509-4a51-48b6-845b-d55d327d5 363.py", line 10, in check     assert count_up_to(101) == [            ^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/94af5509-4a51-48b6-845b-d55d327d5 363.py", line 6, in count_up_to     if is_prime(i):        ^^^^^^^^ NameError: name 'is_prime' is not defined

--- Python_96.py
+++ Python_96.py
@@ -1,7 +1,7 @@
-def is_prime(n):
-    # Implement a function that checks if n is prime
-    return False  # Replace with your implementation
 
+def is_prime(n):
+   # Implement a function that checks if n is prime
+   return False  # Replace with your implementation
 
 def count_up_to(n):
     if n <= 2:
