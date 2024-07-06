f2b76e77337ea261f935a368fe6c719974c33adf
beep boop <githubbot@vadim.me>
2024-07-06 17:29:46 +0200

    Solution 63 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qb239rlh Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5a0c63d2-1a04-4d30-a8eb-5fb2b95b3 1f5.py", line 15, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5a0c63d2-1a04-4d30-a8eb-5fb2b95b3 1f5.py", line 12, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,14 +1,20 @@
-```
 def fizz_buzz(n):
-    count = 0
-    result = 0
+    fizzes = 0
+    buzzes = 0
+    nums = 0
+
     for i in range(1, n + 1):
         if i % 15 == 0:
-            count += 1
+            print("FizzBuzz")
+            buzzes += 1
         elif i % 5 == 0:
-            result += 1
+            print("Buzz")
+            buzzes += 1
         elif i % 3 == 0:
-            result += 1
+            print("Fizz")
+            fizzes += 1
         else:
-            pass
-    return count
+            print(i)
+            nums += 1
+
+    return fizzes, buzzes, nums + 1
