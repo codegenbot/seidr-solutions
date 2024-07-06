b6a4a024bc71a421cc22ea2f5061cda7d1e6576f
beep boop <githubbot@vadim.me>
2024-07-06 06:59:51 +0200

    Solution 93 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4dw7k43c Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b745c463-0ac3-4a52-a4f7-f4814b845 dd9.py", line 9, in <module>     check(sort_numbers)           ^^^^^^^^^^^^ NameError: name 'sort_numbers' is not defined

--- Python_19.py
+++ Python_19.py
@@ -1,4 +1,6 @@
-```
+Here's your Python code:
+
+```Python
 num_dict = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
 
 def check(input_str):
@@ -7,5 +9,5 @@ def check(input_str):
     numbers = [num_dict[word] for word in words if word in num_dict]
     return sorted(numbers)
 
-sort_numbers = lambda: input("Enter the numbers in English text format (separated by space), for example: one two three four five: ").split()
-print(check(' '.join(sort_numbers())))
+sort_numbers = lambda: input("Enter the numbers in English text format (separated by space), for example: one two three four five: ")
+print(check(sort_numbers()))
