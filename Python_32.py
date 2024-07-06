e5a9a069b2abdb716a420433877cb11faa0bf2ae
beep boop <githubbot@vadim.me>
2024-07-06 17:26:57 +0200

    Solution 97 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6qafnz8m Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f8401acf-3634-4220-ab7c-7aaeaee22 b76.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,6 +1,3 @@
-```
-print("This program calculates the roots of a quadratic equation.")
-print("Enter non-zero values for 'a', 'b', 'c': ")
 while True:
     try:
         a = float(input())
@@ -20,7 +17,7 @@ else:
     r1 = (-b + (b**2 - 4 * a * c) ** 0.5) / (2 * a)
     r2 = (-b - (b**2 - 4 * a * c) ** 0.5) / (2 * a)
 
-    if r1.imag != 0 or r2.imag != 0:
+    if complex(0).imag != 0 or complex(0).imag != 0:
         print(f"The equation has complex roots {r1} and {r2}.")
     else:
         print(f"Zeroes are approximately {r1:.2f} and {r2:.2f}")
