5ac5fdf12cc93cf60aaebb119aa95d86951e907e
beep boop <githubbot@vadim.me>
2024-07-06 13:49:09 +0200

    Solution 10 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/g0smtz44 Prompt: cb36dd33-a5a2-4da4-b8bc-e4c33c7df984.cpp:11:22: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    11 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                      ^~       |                      l1 cb36dd33-a5a2-4da4-b8bc-e4c33c7df984.cpp:11:44: error: ‘s2’ was not declared in this scope; did you mean ‘l2’?    11 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                                            ^~       |                                            l2 cb36dd33-a5a2-4da4-b8bc-e4c33c7df984.cpp:12:32: error: ‘intersection’ was not de clared in this scope    12 |                       inserter(intersection, intersection.begin()));       |                                ^~~~~~~~~~~~ cb36dd33-a5a2-4da4-b8bc-e4c33c7df984.cpp: At global scope: cb36dd33-a5a2-4da4-b8bc-e4c33c7df984.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ cb36dd33-a5a2-4da4-b8bc-e4c33c7df984.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,3 +1,4 @@
+```
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -5,12 +6,11 @@ using namespace std;
 
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
-    set<int> s2(l2.begin(), l2.end());
-    
+    set<int> s2(l2.begin(), l2.end()); 
     set<int> intersection;
-    set_difference(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                    inserter(intersection, intersection.begin()));
-                    
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                      inserter(intersection, intersection.begin()));
+    
     vector<int> result(intersection.begin(), intersection.end());
     return result;
 }
