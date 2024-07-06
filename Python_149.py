86cb65979143602bc74e01bd576c4e6a52fdb730
beep boop <githubbot@vadim.me>
2024-07-06 05:52:30 +0200

    Solution 92 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/w2c7zzis Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80298778-c957-433b-a02a-65f99375a 39c.py", line 11, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80298778-c957-433b-a02a-65f99375a 39c.py", line 8, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,6 +1,5 @@
 def sorted_list_sum(lst):
-if len(lst) < 2:
-return lst
-even_words = [word for word in lst if len(word) % 2 == 0]
-odd_words = [word for word in lst if len(word) % 2 != 0]
-return sorted(even_words, key=len) + sorted(odd_words, key=len)
+    all_words = sorted(lst, key=len)
+    even_words = [word for word in all_words if len(word) % 2 == 0]
+    odd_words = [word for word in all_words if len(word) % 2 != 0]
+    return sorted(even_words, key=len) + sorted(odd_words, key=len)
