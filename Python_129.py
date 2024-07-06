db79d97db44b4c5b3367fd8e21427cbe649715de
beep boop <githubbot@vadim.me>
2024-07-06 11:56:04 +0200

    Solution 12 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vcou2e8s Prompt: Complete the following code given the task description and function signature.

--- Python_129.py
+++ Python_129.py
@@ -1,27 +1,26 @@
-```
 def minPath(grid, k):
-    n = len(grid)
-    memo = {}
+    N = len(grid)
+    res = []
+    visited = [[False] * N for _ in range(N)]
 
     def dfs(i, j, path):
-        if (i, j) in memo:
-            return memo[(i, j)]
-        if sum(1 for x in set(path)) < k:
-            return []
+        if len(path) == k:
+            return path
+        visited[i][j] = True
+        path.append(grid[i][j])
+        min_val = grid[i][j]
+        min_path = path[:]
 
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        res = []
-        for d in directions:
-            ni, nj = i + d[0], j + d[1]
-            if 0 <= ni < n and 0 <= nj < n:
-                res += [grid[ni][nj]] + dfs(ni, nj, path + [grid[ni][nj]])
-        memo[(i, j)] = sorted(set(res))
-        return memo[(i, j)]
+        for ni, nj in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+            if 0 <= ni < N and 0 <= nj < N and not visited[ni][nj]:
+                new_path = dfs(ni, nj, path)
+                if len(new_path) == k:
+                    return new_path
+        visited[i][j] = False
+        return min_path
 
-    res = []
-    for i in range(n):
-        for j in range(n):
-            if not res or grid[i][j] < res[-1]:
-                res += dfs(i, j, [grid[i][j]])
+    for i in range(N):
+        for j in range(N):
+            res.append(dfs(i, j, []))
 
-    return min((res[0:i+1] for i in range(k+1) if len(res[:i+1]) == k), key=lambda x: (x.index(min(x)), len(x)))
+    return [x[0] for x in sorted(res)]
