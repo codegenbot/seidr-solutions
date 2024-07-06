59b6705665e2fc11d46446c89cf6fead882ba221
beep boop <githubbot@vadim.me>
2024-07-06 02:22:30 +0200

    Solution 12 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ynbb4504 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/da9b89b8-063f-4cbb-a982-1864f16cb ff8.py", line 12, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/da9b89b8-063f-4cbb-a982-1864f16cb ff8.py", line 8, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,6 +1,2 @@
 def fix_spaces(text):
-    # Check if the text has consecutive spaces
-    if "  " in text:
-        # Replace all consecutive spaces with a single underscore
-        text = text.replace("  ", "-")
-    return text.replace(" ", "_")
+    return text.replace("  ", "-", -1).replace(" ", "_")
