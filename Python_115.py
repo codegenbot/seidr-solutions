892946a730367a25f7f7ad0cccf1011cc3f16acf
beep boop <githubbot@vadim.me>
2024-07-06 02:26:01 +0200

    Solution 56 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/dx5s3j3u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/92fe368a-c402-4382-b612-453833b74 cec.py", line 19, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/92fe368a-c402-4382-b612-453833b74 cec.py", line 16, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_115.py
+++ Python_115.py
@@ -1,5 +1,6 @@
 import math
 
+
 def max_fill(grid, capacity):
     rows = len(grid)
     cols = len(grid[0])
@@ -8,4 +9,13 @@ def max_fill(grid, capacity):
         for j in range(cols):
             if grid[i][j] == 1:
                 water += 1
-    return math.ceil(water / capacity)
+    return math.floor(water / capacity)
+
+
+def main():
+    grid = [[1, 1, 1, 1], [1, 1, 1, 1]]
+    assert max_fill(grid, 9) == 2
+
+
+if __name__ == "__main__":
+    main()
