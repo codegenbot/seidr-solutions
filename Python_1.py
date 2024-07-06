0233757f20d453c749a69f1fdd49ad38b81df571
beep boop <githubbot@vadim.me>
2024-07-06 23:27:02 +0200

    Solution 39 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6we3j50k Prompt: Complete the following code given the task description and function signature.

--- Python_1.py
+++ Python_1.py
@@ -2,8 +2,8 @@
 from typing import List
 
 def separate_paren_groups(paren_string: str) -> List[str]:
-    groups = []
     stack = []
+    groups = []
     current_group = ''
     
     for char in paren_string:
