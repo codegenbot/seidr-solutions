932f16f9c09ef3be211e58d4559e6459292637bc
beep boop <githubbot@vadim.me>
2024-07-06 10:46:26 +0200

    Solution 30 of Python/6, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/u89qmsrm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/455de7b1-3295-4577-85f8-f209ec2aa 34f.py", line 17, in <module>     check(parse_nested_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/455de7b1-3295-4577-85f8-f209ec2aa 34f.py", line 14, in check     assert parse_nested_parens("(()(())((())))") == [4]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_6.py
+++ Python_6.py
@@ -1,11 +1,15 @@
-def parse_nested_parens(paren_string: str) -> List[int]:
+def parse_nested_parens(paren_string):
     result = []
+    max_level, level = 0, 0
     for group in paren_string.split():
-        level = 0
         for char in group:
             if char == "(":
                 level += 1
             elif char == ")":
                 level -= 1
-        result.append(level)
+                if level < 0:
+                    raise ValueError("Unbalanced parentheses")
+            if level > max_level:
+                max_level = level
+        result.append(max_level)
     return result
