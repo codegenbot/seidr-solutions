b7eb5a869a48f89aeaaa0f8f38ba10adab55a541
beep boop <githubbot@vadim.me>
2024-07-06 02:55:24 +0200

    Solution 46 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/c1gllatp Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c2833fd6-cc9e-49f8-9b3a-e1dbda8a4 44e.py", line 1, in <module>     def sum_product(numbers: list[int]) -> Tuple[int, int]:                                            ^^^^^ NameError: name 'Tuple' is not defined. Did you mean: 'tuple'?

--- Python_8.py
+++ Python_8.py
@@ -1,5 +1,6 @@
+
 def sum_product(numbers: list[int]) -> tuple[int, int]:
-    if not numbers:
-        return 0, 1
-    else:
-        return sum(numbers), math.prod(numbers)
+if not numbers:
+return 0, 1
+else:
+return sum(numbers), math.prod(numbers)
