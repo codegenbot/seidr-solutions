3eae7038d43cfe0a8c2dfb71d66d44c49afc02f5
beep boop <githubbot@vadim.me>
2024-07-06 09:21:49 +0200

    Solution 12 of CPP/126, ratio 0.9230769230769231 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/15gnsxut Prompt: in this scope     3 | bool is_sorted(vector<int> lst){       |                ^~~~~~ 23d92978-736e-4aee-8371-f53af5ccb39e.cpp:3:16: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 23d92978-736e-4aee-8371-f53af5ccb39e.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 23d92978-736e-4aee-8371-f53af5ccb39e.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 23d92978-736e-4aee-8371-f53af5ccb39e.cpp:3:23: error: expected primary-expressio n before ‘int’     3 | bool is_sorted(vector<int> lst){       |                       ^~~ In file included from 23d92978-736e-4aee-8371-f53af5ccb39e.cpp:12: 23d92978-736e-4aee-8371-f53af5ccb39e.cpp: In function ‘int main()’: 23d92978-736e-4aee-8371-f53af5ccb39e.cpp:14:22: error: ‘is_sorted’ cannot be use d as a function    14 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ~~~~~~~~~^~~~~~~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,16 +1,10 @@
-bool is_sorted(vector<int> lst){
+#include <vector>
+#include <algorithm>
+
+bool is_sorted(std::vector<int> lst) {
     for(int i = 0; i < lst.size() - 1; i++){
-        if(lst[i] >= lst[i+1]){
-            vector<int> temp;
-            for(int j = 0; j < lst.size(); j++){
-                int count = 0;
-                for(int k = 0; k < lst.size(); k++){
-                    if(lst[k] == lst[j])count++;
-                }
-                if(count > 1) return false;
-                temp.push_back(lst[j]);
-            }
-            lst = temp;
+        if(std::make_pair(lst[i],lst[i+1]) > std::make_pair(lst[i+1],lst[i])){
+            return false;
         }
     }
     return true;
