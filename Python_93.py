bbf11c80d101f580977d5906654faa8d390209aa
beep boop <githubbot@vadim.me>
2024-07-06 23:00:21 +0200

    Solution 84 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cvv0h3qj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b4ea9e67-1f3a-4c85-bc92-a7ca40113 a75.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -6,12 +6,12 @@ def encode(message):
                 if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
                     result += chr((ord(char) - 97 + 3) % 26 + 97)
                 else:
-                    result += chr((ord(char) - 97 + ord(char) - 97 + 1) % 26 + 97)
+                    result += chr((ord(char) - 97 + 1).bit_length() + 97)
             else:
                 if char == 'A' or char == 'E' or char == 'I' or char == 'O' or char == 'U':
                     result += chr((ord(char) - 65 + 3) % 26 + 65)
                 else:
-                    result += chr((ord(char) - 65 + ord(char) - 65 + 1) % 26 + 65)
+                    result += chr((ord(char) - 65 + 1).bit_length() + 65)
         else:
             result += char
     return result
