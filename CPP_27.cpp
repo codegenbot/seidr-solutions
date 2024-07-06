bea8c8c3c18139025362418fbd3b51233d6367e6
beep boop <githubbot@vadim.me>
2024-07-06 15:21:29 +0200

    Solution 98 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fizyj1bk Prompt: a80251df-c0e0-4266-9e8f-91ddc932070b.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string flip_case(std::string str){       | ^~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -2,12 +2,21 @@ using namespace std;
 
 string flip_case(string str){
     string result = "";
-    for (int i = 0; i < str.length(); i++) {
+    
+    for(int i=0; i<str.length(); i++){
         char c = str[i];
-        if (islower(c))
-            result += toupper(c);
+        
+        if(isalpha(c)){
+            if(isupper(c)) 
+                c = tolower(c);
+            else
+                c = toupper(c);
+            
+            result += c;
+        }
         else
-            result += tolower(c);
+            result += c;
     }
+    
     return result;
 }
