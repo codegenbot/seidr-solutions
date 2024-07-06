6c43363fa06cf857be02cf6b9f7c5c565a7849df
beep boop <githubbot@vadim.me>
2024-07-06 09:44:21 +0200

    Solution 10 of Python/94, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/n152llu1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4d4b8fe7-deed-41f8-880f-2f996fcab cc5.py", line 15, in <module>     check(skjkasdkd)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4d4b8fe7-deed-41f8-880f-2f996fcab cc5.py", line 11, in check     skjkasdkd([127, 97, 8192]) == 10     ^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4d4b8fe7-deed-41f8-880f-2f996fcab cc5.py", line 3, in skjkasdkd     largest_prime = max(filter(lambda x: is_prime(x), lst))                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4d4b8fe7-deed-41f8-880f-2f996fcab cc5.py", line 3, in <lambda>     largest_prime = max(filter(lambda x: is_prime(x), lst))                                          ^^^^^^^^ NameError: name 'is_prime' is not defined

--- Python_94.py
+++ Python_94.py
@@ -1,6 +1,6 @@
 def skjkasdkd(lst):
-    # Find the largest prime number in the list
     largest_prime = max(filter(lambda x: is_prime(x), lst))
-
-    # Return the sum of its digits
     return sum(map(int, str(largest_prime)))
+
+def is_prime(n):
+    return n > 1 and all(n % i for i in range(2, int(n**0.5) + 1))
