47e8c3ddff2f968ab6e810911a4871e516e296a1
beep boop <githubbot@vadim.me>
2024-07-06 16:34:37 +0200

    Solution 37 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1fv3kcsh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8ae9bd90-ebb6-4c50-8d88-db707a37e bd0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_37.py
+++ Python_37.py
@@ -1,14 +1,14 @@
-def sort_even(l: list):
+def sort_even(l: list) -> tuple:
     evens = sorted([x for x in l if x % 2 == 0])
     odds = sorted([x for x in l if x % 2 != 0])
-    
+
     result = []
-    while evens and odds:
+    while len(odds) >= len(evens):
         result.append(odds.pop(0))
         if evens:
             result.append(evens.pop(0))
-        
+
     result.extend(evens)
     result.extend(odds)
-    
+
     return tuple(result)
