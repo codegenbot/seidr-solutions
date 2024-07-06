3617c55fe100d73f1cdea54b07b3c7ab8c6fc2de
beep boop <githubbot@vadim.me>
2024-07-06 03:49:05 +0200

    Solution 74 of CPP/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/n8ravhsc Prompt:    42 |     return 0;       |     ^~~~~~ 6e221223-6b72-4b54-af0a-508015a59dd3.cpp:43:1: error: expected declaration befor e ‘}’ token    43 | }       | ^ 6e221223-6b72-4b54-af0a-508015a59dd3.cpp:46:10: error: ‘int main()’ redeclared a s different kind of entity    46 | int main(){       |          ^ 6e221223-6b72-4b54-af0a-508015a59dd3.cpp:15:5: note: previous declaration ‘int m ain’    15 | int main       |     ^~~~ In file included from 6e221223-6b72-4b54-af0a-508015a59dd3.cpp:45: 6e221223-6b72-4b54-af0a-508015a59dd3.cpp: In function ‘int main()’: 6e221223-6b72-4b54-af0a-508015a59dd3.cpp:47:23: error: could not convert ‘{1, -2 , 2, -2, 5, -5, 4, -4}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector <std::__cxx11::basic_string<char> >’    47 |     assert (below_zero({1, -2, 2, -2, 5, -5, 4, -4}) == true);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                       |       |                       <brace-enclosed initializer list>

--- CPP_3.cpp
+++ CPP_3.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <vector>
 
-bool below_zero(std::vector<std::string> operation) {
+bool below_zero(std::vector<std::string> operations) {
     int balance = 0;
-    for (const auto& operations : operation) {
-        int op = std::stoi(operations);
-        balance += op;
+    for (int i = 0; i < operations.size(); ++i) {
+        int operation = stoi(operations[i]);
+        balance += operation;
         if (balance < 0) return true;
     }
     return false;
+
+}
 
 int main() {
     std::vector<std::string> operations;
