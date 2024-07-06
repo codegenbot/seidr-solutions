58f75205543aba0207951c60e9789a859f6e58e0
beep boop <githubbot@vadim.me>
2024-07-06 11:16:30 +0200

    Solution 31 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6h0099y8 Prompt:    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d6587097-0ba1-48c9-a91c-243b760a5091.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ d6587097-0ba1-48c9-a91c-243b760a5091.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main(){       |     ^~~~ In file included from d6587097-0ba1-48c9-a91c-243b760a5091.cpp:30: d6587097-0ba1-48c9-a91c-243b760a5091.cpp: In function ‘int main()’: d6587097-0ba1-48c9-a91c-243b760a5091.cpp:40:20: error: ‘order_by_points’ was not  declared in this scope    40 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ d6587097-0ba1-48c9-a91c-243b760a5091.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -2,25 +2,15 @@
 #include <vector>
 #include <string>
 
-bool isSame(vector<int> a,vector<int>b){
+bool compare(int a, int b) {
+    if (!issame({a}, {b})) return (a > b) - (a < b);
     int sumA = 0, sumB = 0;
-    for (char c : to_string(a[0])) sumA += c - '0';
-    for (char c : to_string(b[0])) sumB += c - '0';
-    return sumA == sumB && a.size() == b.size();
+    for (char c : to_string(a)) sumA += c - '0';
+    for (char c : to_string(b)) sumB += c - '0';
+    return sumA - sumB > 0;
 }
 
 vector<int> order_by_points(vector<int> nums) {
-    auto compare = [&](int a, int b) {
-        if (!isSame({a},{b})) {
-            int sumA = 0, sumB = 0;
-            for (char c : to_string(a)) sumA += c - '0';
-            for (char c : to_string(b)) sumB += c - '0';
-            return sumA - sumB > 0 ? 1 : -1;
-        } else {
-            return distance(nums.begin(), find(nums.begin(), nums.end(), a)) -
-                   distance(nums.begin(), find(nums.begin(), nums.end(), b));
-        }
-    };
     sort(nums.begin(), nums.end(), compare);
     return nums;
 }
