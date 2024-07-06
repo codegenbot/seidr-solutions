8e04f95a425768f0d232e511265c989270edd012
beep boop <githubbot@vadim.me>
2024-07-06 06:47:42 +0200

    Solution 5 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/67p9mn8z Prompt: Complete the following code given the task description and function signature.

--- Python_67.py
+++ Python_67.py
@@ -1,6 +1,4 @@
 
 def fruit_distribution(s, n):
-    # get number of apples and oranges from input string
-    num_apples = int(s.split()[0])
-    num_oranges = int(s.split()[2])
-    return n - num_apples - num_oranges
+    apples, oranges = map(int, s.split())
+    return n - apples - oranges
