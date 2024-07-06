e068fc1d8bad809d017e19e6d8f22bdd180d03c3
beep boop <githubbot@vadim.me>
2024-07-06 18:09:57 +0200

    Solution 13 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xu191x85 Prompt: 87d51601-5ab4-4724-ae72-acdfb89da350.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | int sum_squares(vector<int> lst){       |                 ^~~~~~ 87d51601-5ab4-4724-ae72-acdfb89da350.cpp:1:24: error: expected primary-expressio n before ‘int’     1 | int sum_squares(vector<int> lst){       |                        ^~~ In file included from 87d51601-5ab4-4724-ae72-acdfb89da350.cpp:12: 87d51601-5ab4-4724-ae72-acdfb89da350.cpp: In function ‘int main()’: 87d51601-5ab4-4724-ae72-acdfb89da350.cpp:14:24: error: ‘sum_squares’ cannot be u sed as a function    14 |     assert (sum_squares({-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 14, -14, 6, 13, 11, 16, 16, 4, 10}) == -1448);       |             ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -1,12 +1,10 @@
-```cpp
 #include <vector>
-#include <cmath>
 
 int sum_squares(vector<int> lst){
     int total = 0;
     for(int i = 0; i < lst.size(); i++){
         if(i % 3 == 0 && i % 4 != 0)
-            total += pow(lst[i], 2);
+            total += lst[i] * lst[i];
         else if(i % 4 == 0 && i % 3 != 0)
             total += pow(lst[i], 3);
     }
