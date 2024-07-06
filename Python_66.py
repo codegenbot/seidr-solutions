70dbdbec68f376771ffc85d088938663fe94b295
beep boop <githubbot@vadim.me>
2024-07-06 21:52:27 +0200

    Solution 88 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/klh5nv8w Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/39ca6026-dd20-470d-a9c4-65e4d5b2d 9ca.py", line 13, in <module>     check(digitSum)           ^^^^^^^^ NameError: name 'digitSum' is not defined

--- Python_66.py
+++ Python_66.py
@@ -1,10 +1,6 @@
-```
 def check(n: int) -> str:
     digit_sum = sum(int(digit) for digit in str(n))
     if digit_sum % 3 == 0:
         return "YES"
     else:
         return "NO"
-
-n = int(input("Enter an integer: "))
-print(check(n))
