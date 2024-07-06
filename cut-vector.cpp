e57bf56be62f77761cdd2adfd250add61a5862e1
beep boop <githubbot@vadim.me>
2024-07-06 08:42:44 +0200

    Solution 18 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/a04ox21m Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from fe79e00d-fe9f-4979-bbf3-178ce2d92974.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,25 +1,15 @@
 ```cpp
 #include <vector>
 #include <iostream>
-
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int> vec) {
     int n = vec.size();
     vector<vector<int>> res(2);
     for (int i = 0; i < n; i++) {
-        if (i == 0 || vec[i] != vec[i - 1]) {
-            int mid = i;
-            while(mid > 0 && vec[mid] == vec[mid - 1])
-                mid--;
-            res[0].reserve(i);
-            for(int j=0; j<i; j++) {
-                res[0].push_back(vec[j]);
-            }
-            res[1].reserve(n-i);
-            for(int j=i; j<n; j++) {
-                res[1].push_back(vec[j]);
-            }
+        if (i == 0 || vec[i] - vec[i-1] >= 0) {
+            res[0].assign(vec.begin(), vec.begin() + i + 1);
+            res[1].assign(vec.begin() + i, vec.end());
             break;
         }
     }
