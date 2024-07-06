97ba8e9981878d116cee4526b54a5d13fb14d940
beep boop <githubbot@vadim.me>
2024-07-06 17:59:25 +0200

    Solution 88 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qb239rlh Prompt: FizzBuzz Fizz Buzz Fizz Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4619d363-f4f4-4c3b-b151-c73420f8d a4a.py", line 24, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4619d363-f4f4-4c3b-b151-c73420f8d a4a.py", line 21, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,19 +1,15 @@
 def fizz_buzz(n):
-    count = 0
     for i in range(1, n + 1):
         out = ""
         if i % 15 == 0:
             out = "FizzBuzz"
-            count += 1
-        elif i % 3 == 0 and i % 5 != 0:
+        elif i % 3 == 0:
             out = "Fizz"
-            count += 1
-        elif i % 5 == 0 and i % 3 != 0:
+        elif i % 5 == 0:
             out = "Buzz"
-            count += 1
         
         print(out)
     
-    return count
+    return sum(1 for _ in range(n) if (_ % 15 == 0))
 
 print(fizz_buzz(100000))
