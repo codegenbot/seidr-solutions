307e089c3f5fa07ef0aca37fd6990df1dc543dfe
beep boop <githubbot@vadim.me>
2024-07-06 07:34:56 +0200

    Solution 85 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fgj51epz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/28ec0495-9518-4c33-801e-0aab88f78 89c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,8 +1,6 @@
-```
 def modp(n: int, p: int):
     if p == 1:
         return n
     else:
-        input_str = f"Enter a number to perform power modulo operation (n={n}, p={p}): "
-        num = int(input())
-        return pow(num, p - 1, p) * num % p
+        result = pow(n, p - 1, p)
+        return (n * result) % p
