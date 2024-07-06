1652f8e03f63e002decab53e6865671ac2a677fe
beep boop <githubbot@vadim.me>
2024-07-06 03:26:52 +0200

    Solution 96 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/u4hjxqlv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5dd70ab0-b86e-47e6-b35b-38045190a 489.py", line 18     print(find_zero(x))                        ^ IndentationError: unindent does not match any outer indentation level

--- Python_32.py
+++ Python_32.py
@@ -1,10 +1,11 @@
+
 def find_zero(x):
-    if x > 0:
-        return -1
-    elif x < 0:
-        return 1
-    else:
-        return 0
+   if x > 0:
+       return -1
+   elif x < 0:
+       return 1
+   else:
+       return 0
 
 if __name__ == "__main__":
     while True:
