67fb74786654279c4803de504786b93139754ee0
beep boop <githubbot@vadim.me>
2024-07-06 05:22:15 +0200

    Solution 74 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r66k6rhr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c76bdcf7-9945-427f-a63b-90ffad56c 2da.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,2 +1,7 @@
 def make_palindrome(string: str) -> str:
-    return string + string[::-1]
+    reverse = string[::-1]
+    new_string = string + reverse
+    if len(string) % 2 != 0:
+        middle_index = len(string) // 2
+        return string[:middle_index] + string[middle_index] * 2 + string[:middle_index][::-1]
+    return new_string
