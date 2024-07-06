759033ff43bff3e51dfeced025b325695b97b1b5
beep boop <githubbot@vadim.me>
2024-07-06 21:59:30 +0200

    Solution 72 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/za61nfn8 Prompt:       |                              ^~~~~~ 02f56fce-7ea0-434b-bbff-861737a7300e.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 02f56fce-7ea0-434b-bbff-861737a7300e.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 02f56fce-7ea0-434b-bbff-861737a7300e.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 02f56fce-7ea0-434b-bbff-861737a7300e.cpp:50: 02f56fce-7ea0-434b-bbff-861737a7300e.cpp: In function ‘int main()’: 02f56fce-7ea0-434b-bbff-861737a7300e.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 02f56fce-7ea0-434b-bbff-861737a7300e.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,55 +1,54 @@
 #include <vector>
-#include <algorithm>
 #include <string>
 
 bool issame(int a, int b) {
-    if (a == 1 && b == 2 || a == 3 && b == 4 || a == 5 && b == 6 || a == 7 && b == 8 || a == 9 && b == 0)
-        return true;
-    return false;
+    return a == b;
 }
 
 vector<string> by_length(vector<int> arr) {
-    vector<string> result;
+    vector<int> numbers;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            string str = "";
-            switch (num) {
-                case 1:
-                    str = "One";
-                    break;
-                case 2:
-                    str = "Two";
-                    break;
-                case 3:
-                    str = "Three";
-                    break;
-                case 4:
-                    str = "Four";
-                    break;
-                case 5:
-                    str = "Five";
-                    break;
-                case 6:
-                    str = "Six";
-                    break;
-                case 7:
-                    str = "Seven";
-                    break;
-                case 8:
-                    str = "Eight";
-                    break;
-                case 9:
-                    str = "Nine";
-                    break;
-            }
-            for (int i = 0; i < arr.size(); i++) {
-                if (issame(num, arr[i])) {
-                    str += ", Same";
-                    break;
-                }
-            }
-            result.push_back(str);
+            numbers.push_back(num);
         }
+    }
+
+    sort(numbers.begin(), numbers.end());
+    reverse(numbers.begin(), numbers.end());
+
+    vector<string> result;
+    for (int num : numbers) {
+        string str = "";
+        switch (num) {
+            case 1:
+                str = "One";
+                break;
+            case 2:
+                str = "Two";
+                break;
+            case 3:
+                str = "Three";
+                break;
+            case 4:
+                str = "Four";
+                break;
+            case 5:
+                str = "Five";
+                break;
+            case 6:
+                str = "Six";
+                break;
+            case 7:
+                str = "Seven";
+                break;
+            case 8:
+                str = "Eight";
+                break;
+            case 9:
+                str = "Nine";
+                break;
+        }
+        result.push_back(str);
     }
 
     return result;
