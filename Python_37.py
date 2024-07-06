99dfe2f8028c1a73c9b0e2d4cc1843f050ebc7df
beep boop <githubbot@vadim.me>
2024-07-06 16:40:19 +0200

    Solution 41 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1fv3kcsh Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ac292e47-65de-4403-a3b2-3d58e3f69 14d.py", line 25, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ac292e47-65de-4403-a3b2-3d58e3f69 14d.py", line 20, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ac292e47-65de-4403-a3b2-3d58e3f69 14d.py", line 8, in sort_even     result.append(odds.pop(0))                   ^^^^^^^^^^^ IndexError: pop from empty list

--- Python_37.py
+++ Python_37.py
@@ -1,13 +1,18 @@
 def sort_even(l: list) -> tuple:
-    evens = sorted([x for x in l if x % 2 == 0])
-    odds = sorted([x for x in l if x % 2 != 0])
+    pos_evens = sorted([x for x in l if 0 < x and x % 2 == 0])
+    neg_evens = sorted([-x for x in l if x < 0 and x % 2 == 0])
+    odds = sorted([x for x in l if (x < 0 and x % 2 != 0) or (x > 0 and x % 2 != 0)])
 
     result = []
-    while len(evens) and len(odds):
-        result.append(evens.pop(0))
-        result.append(odds.pop(0))
+    while len(odds) >= len(pos_evens) and len(odds) >= len(neg_evens):
+        while pos_evens:
+            result.append(pos_evens.pop(0))
+        while neg_evens:
+            result.append(-neg_evens.pop(0))
+        if odds:
+            result.append(odds.pop(0))
 
-    result.extend(evens)
+    result.extend([-x for x in neg_evens])
     result.extend(odds)
-
+    
     return tuple(result)
