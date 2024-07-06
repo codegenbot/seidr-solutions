580ec1c8e7ef428d5df6aa9270f7a6ee280ad431
beep boop <githubbot@vadim.me>
2024-07-06 20:54:33 +0200

    Solution 70 of Python/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/f2mlu349 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f8c8e949-4e3f-4237-a02b-fbe4bdedb 193.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_101.py
+++ Python_101.py
@@ -1,6 +1,4 @@
 ```
-import re
-
 def words_string():
     while True:
         try:
@@ -8,7 +6,7 @@ def words_string():
             if not s: 
                 print("Input cannot be blank. Please enter valid text.")
                 continue
-            result = [x.strip() for x in re.split("[,\s]+", s) if x]
+            result = [x.strip() for x in re.split("[,\s]+", s.strip()) if x]
             return result
         except (ValueError, TypeError):
             print("Invalid input. Please try again with only letters and numbers separated by commas or spaces.")
