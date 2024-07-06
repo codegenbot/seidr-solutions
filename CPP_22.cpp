e0ded1552d6643df25b888db302b87e873f20302
beep boop <githubbot@vadim.me>
2024-07-06 02:51:31 +0200

    Solution 66 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/brmn1pem Prompt: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 31cdf3ac-aabb-4b73-9477-ea6deecb96e9.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 31cdf3ac-aabb-4b73-9477-ea6deecb96e9.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 31cdf3ac-aabb-4b73-9477-ea6deecb96e9.cpp:18: 31cdf3ac-aabb-4b73-9477-ea6deecb96e9.cpp: In function ‘int main()’: 31cdf3ac-aabb-4b73-9477-ea6deecb96e9.cpp:28:35: error: no match for call to ‘(st d::vector<int>) (<brace-enclosed initializer list>)’    28 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~ 31cdf3ac-aabb-4b73-9477-ea6deecb96e9.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,14 +1,14 @@
-#include <vector>
-#include <list>
 #include <optional>
-#include <any>
+#include <vector>
 
 std::vector<int> filter_integers(std::list<std::any> values) {
     std::vector<int> result;
     for (const auto& value : values) {
-        if (value.type() == typeid(int)) {
-            int i = std::any_cast<int>(value);
-            result.push_back(i);
+        if(value.type() == typeid(std::optional<int>)) {
+            auto optionalInt = boost::any_cast<std::optional<int>>(value);
+            if(optionalInt.has_value()) {
+                result.push_back(optionalInt.get());
+            }
         }
     }
     return result;
