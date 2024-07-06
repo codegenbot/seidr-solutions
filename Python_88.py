bad1ec2278a46aa941ab42d138cb400650314444
beep boop <githubbot@vadim.me>
2024-07-06 17:56:47 +0200

    Solution 3 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vtonw1kq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8018393f-417a-4c80-ac22-cd446d000 653.py", line 15, in <module>     check(sort_array)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8018393f-417a-4c80-ac22-cd446d000 653.py", line 12, in check     assert sort_array([21, 14, 23, 11]) == [23, 21, 14, 11], "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_88.py
+++ Python_88.py
@@ -1,9 +1,9 @@
 def sort_array(array):
     if len(array) == 0:
         return array
-    array.sort()
-    total = sum(array)
-    if total % 2 != 0:
-        return array
+    odd = [num for num in array if num % 2 != 0]
+    even = [num for num in array if num % 2 == 0]
+    if sum(array) % 2 != 0:
+        return sorted(odd + even)
     else:
-        return array[::-1]
+        return sorted(even, reverse=True) + sorted(odd)
