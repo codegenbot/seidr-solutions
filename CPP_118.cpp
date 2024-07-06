397cdbedd7a7c06420ab3dc3eb03ff2301aaf5bc
beep boop <githubbot@vadim.me>
2024-07-06 17:30:32 +0200

    Solution 6 of CPP/118, ratio 0.8461538461538461 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4tir31km Prompt: 0f68d41e-ea10-4fed-a2b8-272e7ff707e8.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string get_closest_vowel(string word) {       | ^~~~~~ In file included from 0f68d41e-ea10-4fed-a2b8-272e7ff707e8.cpp:13: 0f68d41e-ea10-4fed-a2b8-272e7ff707e8.cpp: In function ‘int main()’: 0f68d41e-ea10-4fed-a2b8-272e7ff707e8.cpp:15:13: error: ‘get_closest_vowel’ was n ot declared in this scope    15 |     assert (get_closest_vowel("Above") == "o");       |             ^~~~~~~~~~~~~~~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,9 +1,11 @@
-string get_closest_vowel(string word) {
-    string vowels = "aeiouAEIOU";
+#include<string>
+
+std::string get_closest_vowel(std::string word) {
+    std::string vowels = "aeiouAEIOU";
     int left = 0;
     for (int i = word.size() - 1; i >= 0; --i) {
         if (!ispunct(word[i]) && !isalpha(word[i])) break;
-        if (vowels.find(toupper(word[i])) != string::npos)
+        if (vowels.find(toupper(word[i])) != std::string::npos)
             return word.substr(left, i - left + 1);
         left = i + 1;
     }
