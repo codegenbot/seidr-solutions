511e2e52d88969f500c6e1088e6d699eed2c243d
beep boop <githubbot@vadim.me>
2024-07-06 12:02:08 +0200

    Solution 14 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lu58we93 Prompt: Complete the following code given the task description and function signature.

--- Python_140.py
+++ Python_140.py
@@ -1,3 +1,11 @@
 ```
 def fix_spaces(text):
-    return text.replace("  ", "-").replace(" ", "_")
+    result = ""
+    for char in text:
+        if char != " ":
+            result += char
+        elif len(result) < 1 or (result[-1] != " " and result[-1] != "_"):
+            result += "-"
+        else:
+            result += "_"
+    return result
