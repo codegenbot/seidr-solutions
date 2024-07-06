194146db7f02c14afddee80eaa3ff9b13f490c10
beep boop <githubbot@vadim.me>
2024-07-06 19:40:56 +0200

    Solution 20 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7aum6t7i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/97881930-d057-46c0-b39f-12de8626c 1ee.py", line 50, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/97881930-d057-46c0-b39f-12de8626c 1ee.py", line 20, in check     sum_squares(   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/97881930-d057-46c0-b39f-12de8626c 1ee.py", line 10, in sum_squares     raise ValueError(f"All numbers should be positive. Found a negative number: {num}") ValueError: All numbers should be positive. Found a negative number: -1

--- Python_142.py
+++ Python_142.py
@@ -7,7 +7,7 @@ def sum_squares(lst):
     for num in lst:
         if not isinstance(num, (int, float)):
             raise ValueError(f"Invalid input '{num}' found. Input must contain only numbers.")
-        if num <= 0:
+        if num < 0:
             raise ValueError(f"All numbers should be positive. Found a negative number: {num}")
         total_sum += num ** 2
     return total_sum
