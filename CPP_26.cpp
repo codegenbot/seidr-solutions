bf8c8f7925c6e1770a0b02b28386b04dfda1d633
beep boop <githubbot@vadim.me>
2024-07-06 15:10:17 +0200

    Solution 76 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/d0axb77d Prompt: s compound expression in initializer [-fpermissive]    29 | bool issame(vector<int> a,vector<int>b){       |                                       ^ e9c8506a-216c-45d9-9240-8f0ca2eb1659.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ e9c8506a-216c-45d9-9240-8f0ca2eb1659.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from e9c8506a-216c-45d9-9240-8f0ca2eb1659.cpp:28: e9c8506a-216c-45d9-9240-8f0ca2eb1659.cpp: In function ‘int main()’: e9c8506a-216c-45d9-9240-8f0ca2eb1659.cpp:38:20: error: ‘remove_duplicates’ was n ot declared in this scope; did you mean ‘removeDuplicates’?    38 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ e9c8506a-216c-45d9-9240-8f0ca2eb1659.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <set>
 
-bool sameVectors(std::vector<int> a, std::vector<int> b) {
+bool compareVectors(std::vector<int> a, std::vector<int> b) {
     if(a.size() != b.size()) return false;
     std::sort(a.begin(), a.end());
     std::sort(b.begin(), b.end());
@@ -23,5 +23,5 @@ std::vector<int> removeDuplicates(std::vector<int> numbers) {
 }
 
 int main() {
-    assert(sameVectors(removeDuplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5}));
+    assert(compareVectors(removeDuplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5}));
 }
