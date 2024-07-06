90a9f56db41d02c949332d08f966f7ff3e77b3d9
beep boop <githubbot@vadim.me>
2024-07-06 12:00:33 +0200

    Solution 63 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/aikobq58 Prompt:       |           ^~~~~~ In file included from a830f55f-d7e1-4206-a0ea-b5e79278997b.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a830f55f-d7e1-4206-a0ea-b5e79278997b.cpp:21:36: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ a830f55f-d7e1-4206-a0ea-b5e79278997b.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ a830f55f-d7e1-4206-a0ea-b5e79278997b.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~ In file included from a830f55f-d7e1-4206-a0ea-b5e79278997b.cpp:20: a830f55f-d7e1-4206-a0ea-b5e79278997b.cpp: In function ‘int main()’: a830f55f-d7e1-4206-a0ea-b5e79278997b.cpp:30:20: error: ‘get_positive’ was not de clared in this scope    30 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -6,18 +6,17 @@ float get_positive(float f) {
     return (f > 0) ? f : 0;
 }
 
-bool issame(const std::vector<float>& a,const std::vector<float>& b) {
-    return std::equal(a.begin(),a.end(),b.begin());
+bool issame(const std::vector<float>& a, const std::vector<float>& b) {
+    return std::equal(a.begin(), a.end(), b.begin());
 }
 
 int main() {
     assert(std::equal({get_positive(0)}, {}));
     std::vector<float> l1 = {get_positive(0), get_positive(2.5f), get_positive(-3), get_positive(4), get_positive(5)};
-    std::vector<float> l2 = {get_positive(2.5f), get_positive(4), get_position(5)};
-    if(issame(l1,l2)) {
+    std::vector<float> l2 = {get_positive(2.5f), get_positive(4), get_positive(5)};
+    if (issame(l1, l2)) {
         std::cout << "Vectors are same." << std::endl;
-    }
-    else {
+    } else {
         std::cout << "Vectors are not same." << std::endl;
     }
 }
