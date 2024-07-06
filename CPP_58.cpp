69e01df4a203fb4932d76f4b5808bb4215dee307
beep boop <githubbot@vadim.me>
2024-07-06 14:14:18 +0200

    Solution 28 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/g0smtz44 Prompt:       |                    ^~~ 342fa275-d1b0-4b16-a29a-9b0a8a53528d.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 342fa275-d1b0-4b16-a29a-9b0a8a53528d.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 342fa275-d1b0-4b16-a29a-9b0a8a53528d.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 342fa275-d1b0-4b16-a29a-9b0a8a53528d.cpp:19: 342fa275-d1b0-4b16-a29a-9b0a8a53528d.cpp: In function ‘int main()’: 342fa275-d1b0-4b16-a29a-9b0a8a53528d.cpp:29:20: error: ‘common’ was not declared  in this scope    29 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 342fa275-d1b0-4b16-a29a-9b0a8a53528d.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
 #include <vector>
 #include <set>
-using namespace std;
 
-vector<int> common(vector<int> l1, vector<int> l2) {
+std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
-    vector<int> result;
+    set<int> result;
     for (int i : s1) {
         if (s2.find(i) != s2.end()) {
-            result.push_back(i);
+            result.insert(i);
         }
     }
 
-    sort(result.begin(), result.end());
-    return result;
+    vector<int> res;
+    for (int i : result) {
+        res.push_back(i);
+    }
+    return res;
+
+}
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
 }
 
 int main() {
-    vector<int> l1 = {1, 4, 3, 34, 653, 2, 5};
-    vector<int> l2 = {5, 7, 1, 5, 9, 653, 121};
-
-    vector<int> result = common(l1, l2);
-    for (int i : result) {
-        cout << i << " ";
-    }
-    return 0;
+    assert(issame(common({4, 3, 2, 8}, {}), {}));
+    
 }
