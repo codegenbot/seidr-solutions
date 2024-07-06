d0a9ec01104136d5565909971e6c8538af7ecf29
beep boop <githubbot@vadim.me>
2024-07-06 07:36:37 +0200

    Solution 76 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/js6e9f34 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f5781dac-0089-4a42-a1c3-2a8689cd1 507.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -1,25 +1,31 @@
-```
-def minPath(grid, k):
+def findPaths(grid):
     n = len(grid)
     res = []
     for i in range(n):
         for j in range(n):
             if grid[i][j] == 1:
                 path = [grid[i][j]]
-                dfs(i, j, k - 1, grid, path, res)
+                dfs(i, j, 0, grid, path, res, 0)
     return res
 
-def dfs(i, j, k, grid, path, res):
+
+def dfs(i, j, k, grid, path, res, total):
     n = len(grid)
-    if k == 0:
-        res.append(path[:])
+    if k < 0 or i < 0 or i >= n or j < 0 or j >= n:
         return
-    for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        ni, nj = i + x, j + y
-        if 0 <= ni < n and 0 <= nj < n and grid[ni][nj] not in path:
-            temp = grid[ni][nj]
-            grid[ni][nj], path.append(temp)
-            dfs(ni, nj, k - 1, grid, path, res)
-            grid[ni][nj] = temp
-            if len(res) > 0 and len(path) + len(res[-1]) == k:
-                return
+    if grid[i][j] == -1:
+        res.append(
+            path
+            + [
+                grid[i][j],
+            ]
+        )
+        return
+    temp = grid[i][j]
+    grid[i][j] = -1
+    path.append(temp)
+    dfs(i - 1, j, k - 1, grid, path, res, total + 1 if i == 0 else total)
+    dfs(i + 1, j, k - 1, grid, path, res, total + 1 if n - 1 == i else total)
+    dfs(i, j - 1, k - 1, grid, path, res, total + 1 if 0 == j else total)
+    dfs(i, j + 1, k - 1, grid, path, res, total + 1 if n - 1 == j else total)
+    grid[i][j] = temp
