04ccbe421017d0a3e8f2c0803038fdbadca4ce57
beep boop <githubbot@vadim.me>
2024-07-06 10:13:00 +0200

    Solution 5 of Python/130, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/3znmsrol Prompt: Complete the following code given the task description and function signature.

--- Python_130.py
+++ Python_130.py
@@ -1,8 +1,8 @@
 
 def tri(n):
-    if n == 0:
-        return [1]
-    elif n % 2 == 0:
+    if n == 1:
         return [1, 3]
+    elif n % 2 == 0:
+        return [1, 3, 2 * (n // 2)]
     else:
-        return [1, 3, 2] + tri(n - 1)
+        return [1, 3, tri(n - 1) + tri(n - 2) + tri(n + 1)]
