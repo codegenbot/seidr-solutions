de5d4ee270c1ee56882746780805275c77640068
beep boop <githubbot@vadim.me>
2024-07-06 13:38:20 +0200

    Solution 84 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6h0099y8 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 2764132a-20bb-476f-afad-8e472697eaec.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,11 +1,8 @@
-```cpp
 #include <algorithm>
 #include <vector>
 
-using namespace std;
-
 std::vector<int> order_by_points(std::vector<int> nums) {
     std::vector<int> res = nums;
-    sort(res.begin(), res.end());
+    std::sort(res.begin(), res.end());
     return res;
 }
