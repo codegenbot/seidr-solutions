f09849bdd701be59acfc26887509cb373393d662
beep boop <githubbot@vadim.me>
2024-07-06 03:24:15 +0200

    Solution 15 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/yny9bc24 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/28c3897e-eacd-48b6-9f65-cc408146b f24.py", line 12, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/28c3897e-eacd-48b6-9f65-cc408146b f24.py", line 9, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_39.py
+++ Python_39.py
@@ -1,12 +1,14 @@
+from math import is_prime
+
 
 def prime_fib(n):
-   fib = [0, 1]
-   i = 2
-   while len(fib) <= n:
-       next_num = fib[i - 1] + fib[i - 2]
-       if is_prime(next_num):
-           fib.append(next_num)
-           i += 1
-       else:
-           break
-   return fib[-1]
+    fib = [0, 1]
+    i = 2
+    while len(fib) <= n:
+        next_num = fib[i - 1] + fib[i - 2]
+        if is_prime(next_num):
+            fib.append(next_num)
+            i += 1
+        else:
+            break
+    return fib[-1]
