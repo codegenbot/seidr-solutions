a9b81ed0784422b05ede7a7440f4a8924c686f49
beep boop <githubbot@vadim.me>
2024-07-06 08:40:33 +0200

    Solution 5 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/e5ji59uq Prompt: Complete the following code given the task description and function signature.

--- Python_26.py
+++ Python_26.py
@@ -1,8 +1,2 @@
 def remove_duplicates(numbers: List[int]) -> List[int]:
-    unique_nums = set()
-    result = []
-    for num in numbers:
-        if num not in unique_nums:
-            unique_nums.add(num)
-            result.append(num)
-    return result
+    return list(set(numbers))
