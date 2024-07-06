3af99988e3b9af96873d448f5b3138592742a019
beep boop <githubbot@vadim.me>
2024-07-06 22:16:22 +0200

    Solution 39 of CPP/111, ratio 0.875 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6wti9phk Prompt: Program failed

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,39 +1,37 @@
-#include<stdio.h>
-#include<string>
-#include<map>
+#include <string>
+#include <map>
 using namespace std;
 
-map<char,int> histogram(string test){
-    map<char,int> result;
-    string temp;
+map<char, int> histogram(string test) {
+    map<char, int> result;
+    string str = test;
+    for (char c : str) {
+        if (c != ' ') {
+            if (result.find(c) == result.end()) {
+                result[c] = 1;
+            } else {
+                result[c]++;
+            }
+        }
+    }
+    map<char, int> maxMap;
     int maxCount = 0;
-    
-    for (int i = 0; i < test.length(); i++) {
-        if (test[i] != ' ') {
-            temp += test[i];
-        } else {
-            if (!result.empty()) {
-                for (auto it = result.begin(); it != result.end(); it++) {
-                    if (it->second > maxCount) {
-                        maxCount = it->second;
-                    }
-                }
-            }
-            temp = "";
+    for (auto it = result.begin(); it != result.end(); ++it) {
+        if (it->second > maxCount) {
+            maxCount = it->second;
+            maxMap.clear();
+            maxMap[it->first] = it->second;
+        } else if (it->second == maxCount) {
+            maxMap[it->first] = it->second;
         }
     }
-
-    if (!temp.empty()) {
-        if (!result.empty()) {
-            for (auto it = result.begin(); it != result.end(); it++) {
-                if (it->second == maxCount) {
-                    result[temp[0]]++;
-                }
-            }
-        } else {
-            result[temp[0]] = 1;
+    auto maxIt = result.begin();
+    while (maxIt != result.end()) {
+        if (maxIt->second == maxCount) {
+            maxMap[maxIt->first] = maxIt->second;
+            break;
         }
+        ++maxIt;
     }
-
-    return result;
+    return maxMap;
 }
