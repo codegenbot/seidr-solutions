73fd7245fa098dee16d8aeabbcf67208cdab28d8
beep boop <githubbot@vadim.me>
2024-07-07 01:59:27 +0200

    Solution 3 of CPP/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4yogeywm Prompt: de1d37a1-cef6-4fc8-85ea-51ee4efadf24.cpp:3:1: error: ‘string’ does not name a ty pe     3 | string concatenate(vector<string> strings){       | ^~~~~~

--- CPP_28.cpp
+++ CPP_28.cpp
@@ -1,6 +1,6 @@
-#include <string>
+using namespace std;
 
-string concatenate(vector<string> strings) {
+std::string concatenate(vector<string> strings){
     string result = "";
     for (const auto& str : strings) {
         result += str;
