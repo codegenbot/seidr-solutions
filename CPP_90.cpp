059224c6c7e17d24f1edd757602b22fdecd7f811
beep boop <githubbot@vadim.me>
2024-07-06 05:14:21 +0200

    Solution 11 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3nr842s4 Prompt: e784e9f5-ce0b-44f9-b6bb-5e7c763422eb.cpp: In function ‘int main()’: e784e9f5-ce0b-44f9-b6bb-5e7c763422eb.cpp:13:5: error: ‘assert’ was not declared in this scope    13 |     assert(next_smallest({-35, 34, 12, -45}) == -35);       |     ^~~~~~ e784e9f5-ce0b-44f9-b6bb-5e7c763422eb.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <algorithm>   +++ |+#include <cassert>     4 | e784e9f5-ce0b-44f9-b6bb-5e7c763422eb.cpp: At global scope: e784e9f5-ce0b-44f9-b6bb-5e7c763422eb.cpp:18:5: error: redefinition of ‘int main( )’    18 | int main(){       |     ^~~~ e784e9f5-ce0b-44f9-b6bb-5e7c763422eb.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -8,7 +8,11 @@ int next_smallest(const std::vector<int>& lst) {
     std::vector<int> v = lst;
     sort(v.begin(), v.end());
     if (v.size() < 2) return -1; // None
-    return v[1];
+    for(int i=0; i<v.size(); i++){
+        if(find(lst.begin(), lst.end(), v[i]) != lst.end())
+            return v[i];
+    }
+    return -1;
 }
 
 int main_test() {
