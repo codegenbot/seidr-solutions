9728bf981d14c50b5ed8452c7ccbe820367fb4e8
beep boop <githubbot@vadim.me>
2024-07-06 12:35:11 +0200

    Solution 60 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6h0099y8 Prompt: In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 63269786-083c-4570-8753-c3dc1b9f2bd5.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 63269786-083c-4570-8753-c3dc1b9f2bd5.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 63269786-083c-4570-8753-c3dc1b9f2bd5.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 63269786-083c-4570-8753-c3dc1b9f2bd5.cpp:20: 63269786-083c-4570-8753-c3dc1b9f2bd5.cpp: In function ‘int main()’: 63269786-083c-4570-8753-c3dc1b9f2bd5.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,12 +1,14 @@
+```
 #include <algorithm>
 #include <vector>
 #include <string>
 
-int main() {
-    std::vector<int> nums = {0,6,6,-76,-21,23,4};
-    std::vector<int> res = order_by_points(nums);
-    assert(std::equal(res.begin(), res.end(), {-76, -21, 0, 4, 23, 6, 6}));
-    return 0;
+bool issame(vector<int> a,vector<int>b){
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i])
+            return false;
+    }
+    return true;
 }
 
 std::vector<int> order_by_points(std::vector<int> nums) {
