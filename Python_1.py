c01247b4ab0563ba17377079b41e875f0b9ab178
beep boop <githubbot@vadim.me>
2024-07-06 10:56:48 +0200

    Solution 56 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ov14dqn5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/067ac0d7-ec61-4169-8f54-c751b704a d4e.py", line 17, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/067ac0d7-ec61-4169-8f54-c751b704a d4e.py", line 14, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ AssertionError

--- Python_1.py
+++ Python_1.py
@@ -1,12 +1,14 @@
-def separate_paren_groups(input_string):
-    stack = []
-    result = []
-    for c in input_string:
-        if c == "(":
-            stack.append(c)
-        elif c == ")":
-            if len(stack) > 0 and stack[-1] == "(":
-                stack.pop()
-            else:
-                result.append(c)
-    return [group for group in result if group[0] == "(" and group[-1] == ")"]
+    def separate_paren_groups(paren_string):
+        stack = []
+        result = []
+        for c in paren_string:
+            if c == "(":
+                stack.append(c)
+            elif c == ")":
+                if len(stack) > 0 and stack[-1] == "(":
+                    stack.pop()
+                else:
+                    result.append(c)
+        while len(stack) > 0:
+            result.append(stack.pop())
+        return [group for group in result if group[0] == "(" and group[-1] == ")"]
