eafd0e258e74f1960def2a52157167e603e41900
beep boop <githubbot@vadim.me>
2024-07-06 18:12:27 +0200

    Solution 14 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fje7ztv1 Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d040846f-7cd5-487d-979a-3c11b8644f4a.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d040846f-7cd5-487d-979a-3c11b8644f4a.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d040846f-7cd5-487d-979a-3c11b8644f4a.cpp:25: d040846f-7cd5-487d-979a-3c11b8644f4a.cpp: In function ‘int main()’: d040846f-7cd5-487d-979a-3c11b8644f4a.cpp:35:20: error: ‘order_by_points’ was not  declared in this scope    35 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ d040846f-7cd5-487d-979a-3c11b8644f4a.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,9 +1,28 @@
+#include <algorithm>
+#include <vector>
+
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-int main() {
-    vector<int> nums = {5, 7, 10};
-    assert(issame(order_by_points(nums), vector<int>({5, 7, 10})));
-    return 0;
+vector<int> order_by_points(vector<int> nums) {
+    vector<pair<int, int>> pairs;
+    for (int i = 0; i < nums.size(); ++i) {
+        int sumOfDigits = 0;
+        int num = nums[i];
+        while (num != 0) {
+            sumOfDigits += num % 10;
+            num /= 10;
+        }
+        pairs.push_back({sumOfDigits, i});
+    }
+
+    sort(pairs.begin(), pairs.end());
+
+    vector<int> result;
+    for (int i = 0; i < nums.size(); ++i) {
+        result.push_back(nums[pairs[i].second]);
+    }
+
+    return result;
 }
