f90dd99afa837c1ba675f38d5c9e880156d47d8c
beep boop <githubbot@vadim.me>
2024-07-06 18:34:10 +0200

    Solution 32 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/wpnto8tb Prompt:       |                    ^~~ e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp:15: e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp: In function ‘int main()’: e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp:25:20: error: ‘compare’ was not declare d in this scope    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ e45ca786-e13a-4a90-919c-3ca9459d5c43.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,12 +1,18 @@
 #include <vector>
+#include <cmath>
 
 std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
-    std::vector<int> result;
+    vector<int> result;
     for (int i = 0; i < game.size(); i++) {
-        if (game[i] == guess[i]) {
+        if (issame(std::vector<int>({game[i]}), std::vector<int>(guess.begin(), guess.begin() + 1))) {
             result.push_back(0);
         } else {
             result.push_back(abs(game[i] - guess[i]));
         }
     }
     return result;
+}
+
+bool issame(std::vector<int> a, std::vector<int> b){
+    return a == b;
+}
