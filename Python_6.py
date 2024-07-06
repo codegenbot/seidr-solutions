ef554ca62a5448d462a783836181be70bc30c2a2
beep boop <githubbot@vadim.me>
2024-07-06 14:40:44 +0200

    Solution 32 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3ou52l0b Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d87f164e-f1e9-4b04-9163-d98cb4af5 997.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_6.py
+++ Python_6.py
@@ -1,10 +1,14 @@
-result = []
-nested_levels = 0
-for char in input():
-    if char == '(':
-        nested_levels += 1
-        result.append(nested_levels)
-    elif char == ')':
-        nested_levels -= 1
-        result.append(nested_levels)
-print([int(x) for x in result])
+def solution(input_str):
+    result = []
+    nested_levels = 0
+    for char in input_str:
+        if char == '(':
+            nested_levels += 1
+            result.append(nested_levels)
+        elif char == ')':
+            nested_levels -= 1
+            result.append(nested_levels)
+    print([int(x) for x in result])
+
+input_str = input()
+solution(input_str)
