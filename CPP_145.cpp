efd96b28d8aec5232811109760a78325a48e5102
beep boop <githubbot@vadim.me>
2024-07-06 18:45:52 +0200

    Solution 52 of CPP/145, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fje7ztv1 Prompt:    24 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 29579740-a0eb-471d-bede-f7aaeb25a7df.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 29579740-a0eb-471d-bede-f7aaeb25a7df.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 29579740-a0eb-471d-bede-f7aaeb25a7df.cpp:23: 29579740-a0eb-471d-bede-f7aaeb25a7df.cpp: In function ‘int main()’: 29579740-a0eb-471d-bede-f7aaeb25a7df.cpp:33:20: error: ‘order_by_points’ was not  declared in this scope    33 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 29579740-a0eb-471d-bede-f7aaeb25a7df.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,12 +1,21 @@
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
+bool issame(int a, int b) {
+    int sum1 = 0;
+    int num1 = abs(a);
+    while (num1 > 0) {
+        sum1 += num1 % 10;
+        num1 /= 10;
     }
-    return true;
+    int sum2 = 0;
+    int num2 = abs(b);
+    while (num2 > 0) {
+        sum2 += num2 % 10;
+        num2 /= 10;
+    }
+
+    return sum1 == sum2;
 }
 
 vector<int> order_by_points(vector<int> nums) {
@@ -29,9 +38,4 @@ vector<int> order_by_points(vector<int> nums) {
     }
 
     return result;
-}
-
-int main() {
-    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6}));
-    return 0;
 }
