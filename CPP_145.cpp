f3c7cbd81deb0d777e041cc6cc721d2bbec0083a
beep boop <githubbot@vadim.me>
2024-07-06 19:10:39 +0200

    Solution 96 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r0qsr2nm Prompt:    43 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 35dc800f-46ac-4c09-a674-e7816522c2cb.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ 35dc800f-46ac-4c09-a674-e7816522c2cb.cpp:37:5: note: ‘int main()’ previously def ined here    37 | int main() {       |     ^~~~ In file included from 35dc800f-46ac-4c09-a674-e7816522c2cb.cpp:42: 35dc800f-46ac-4c09-a674-e7816522c2cb.cpp: In function ‘int main()’: 35dc800f-46ac-4c09-a674-e7816522c2cb.cpp:52:20: error: ‘order_by_points’ was not  declared in this scope    52 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 35dc800f-46ac-4c09-a674-e7816522c2cb.cpp:52:19: error: ‘issame’ cannot be used a s a function    52 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,9 +1,29 @@
 #include <algorithm>
 #include <vector>
-#include <pair>
 
-bool issame(std::vector<int>, std::vector<int>);
-std::vector<int> order_by_points(std::vector<int>);
+bool issame(vector<int> a, vector<int>b) {
+    if(a.size() != b.size())
+        return false;
+    for(int i=0; i<a.size(); i++) {
+        if(a[i] != b[i])
+            return false;
+    }
+    return true;
+}
+
+vector<int> order_by_points(vector<int> nums) {
+    vector<pair<int, int>> pairs;
+    for (int i = 0; i < nums.size(); i++) {
+        pairs.push_back({get_sum_of_digits(nums[i]), i});
+    }
+    sort(pairs.begin(), pairs.end());
+    vector<int> result;
+    for (const auto& pair : pairs) {
+        result.push_back(nums[pair.second]);
+    }
+    return result;
+
+}
 
 int get_sum_of_digits(int num) {
     int sum = 0;
@@ -14,31 +34,7 @@ int get_sum_of_digits(int num) {
     return sum;
 }
 
-bool issame(std::vector<int> a, std::vector<int>b) {
-    if(a.size() != b.size())
-        return false;
-    for(int i=0; i<a.size(); i++) {
-        if(a[i] != b[i])
-            return false;
-    }
-    return true;
-}
-
-std::vector<int> order_by_points(std::vector<int> nums) {
-    std::vector<std::pair<int, int>> pairs;
-    for (int i = 0; i < nums.size(); i++) {
-        pairs.push_back({get_sum_of_digits(nums[i]), i});
-    }
-    std::sort(pairs.begin(), pairs.end());
-    std::vector<int> result;
-    for (const auto& pair : pairs) {
-        result.push_back(nums[pair.second]);
-    }
-    return result;
-
-}
-
 int main() {
-    assert(issame(order_by_points({0,6,6,-76,-21,23,4}), vector<int>{{(-76), (-21), (0), (4), (23), (6), (6)})));
+    assert(issame(vector<int>(order_by_points({0,6,6,-76,-21,23,4})), vector<int>({(-76), (-21), (0), (4), (23), (6), (6)})));
     return 0;
 }
