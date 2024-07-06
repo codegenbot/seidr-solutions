b5a70f0cd9215432bbd3c9a36bc7dc0311931c26
beep boop <githubbot@vadim.me>
2024-07-06 15:16:29 +0200

    Solution 23 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r2soyetl Prompt: 86ff5de1-e076-4697-9b00-b470263d39e0.cpp:1:21: error: ‘vector’ was not declared in this scope     1 | int smallest_change(vector<int> arr) {       |                     ^~~~~~ 86ff5de1-e076-4697-9b00-b470263d39e0.cpp:1:28: error: expected primary-expressio n before ‘int’     1 | int smallest_change(vector<int> arr) {       |                            ^~~ In file included from 86ff5de1-e076-4697-9b00-b470263d39e0.cpp:20: 86ff5de1-e076-4697-9b00-b470263d39e0.cpp: In function ‘int main()’: 86ff5de1-e076-4697-9b00-b470263d39e0.cpp:22:28: error: ‘smallest_change’ cannot be used as a function    22 |     assert (smallest_change({0, 1}) == 1);       |             ~~~~~~~~~~~~~~~^~~~~~~~

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
 #include <vector>
 #include <string>
 
-int smallest_change(std::vector<int> arr) {
+int smallest_change(vector<int> arr) {
     int n = arr.size();
-    std::string str = "";
+    string str = "";
     for (int i : arr) {
         str += to_string(i);
     }
