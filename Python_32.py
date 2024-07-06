638f86bf0350db9a2c0dcbe0dd7590cfacf83ca2
beep boop <githubbot@vadim.me>
2024-07-06 02:18:29 +0200

    Solution 34 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/u4hjxqlv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b0bc95fd-e05c-4c62-be27-9e529cdb5 98f.py", line 29, in <module>     check(find_zero)           ^^^^^^^^^ NameError: name 'find_zero' is not defined

--- Python_32.py
+++ Python_32.py
@@ -1,8 +1,8 @@
-def find_zero(xs: list[int]) -> float:
+def find_negative_ratio(xs):
     if len(xs) == 0:
         raise ValueError("Input list must not be empty.")
     for i in range(len(xs)):
         if xs[i] == 0:
             continue
         x = -xs[i - 1] / (xs[i] * xs[i])
-    return round(x, 2)
+        return round(x, 2)
