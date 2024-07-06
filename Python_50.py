4317591698f8e15ed3a841d7c9f4d91b1d2fdf33
beep boop <githubbot@vadim.me>
2024-07-06 10:02:55 +0200

    Solution 87 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/de8wywms Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -1,7 +1,3 @@
-def decode_shift(s):
-    return "".join([chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) for ch in s])
-
-
 user_input = input("Enter a string to decode: ")
 output = decode_shift(user_input)
 print("Decoded Text:", output)
