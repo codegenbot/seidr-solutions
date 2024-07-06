339eb42f9bd482fe8f67b727786d7a3f45e470a5
beep boop <githubbot@vadim.me>
2024-07-06 14:38:53 +0200

    Solution 70 of gcd, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/21blin5h Prompt: it must return 40 for input 893320 978960, but it returns .

--- gcd.py
+++ gcd.py
@@ -1,9 +1,10 @@
-
 def gcd(a, b):
-    while b != 0:
+    indices = []
+    while b != 0 or a_mod != 0:
         a_mod = a % b
         if a_mod == 0:
             break
+        indices.append(a_mod)
         a = b
         b = a_mod
-    return a
+    return indices
