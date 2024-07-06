a771265cf19f4aa9e03599881fa81c2f462d4e18
beep boop <githubbot@vadim.me>
2024-07-06 19:24:00 +0200

    Solution 5 of Python/145, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/b066cxvk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a77a527e-0ade-40d9-81af-e96441b3e c91.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_145.py
+++ Python_145.py
@@ -1,3 +1,5 @@
-```
-def order_by_points(nums):
-    return sorted((num, sum(map(int, str(abs(num))))) for num in nums)
+def order_by_points(numbers):
+    # Task description: Sort input numbers by points
+    # Function signature: def order_by_points(numbers)
+
+    return sorted((n, sum(map(int, str(abs(n))))) for n in numbers)
