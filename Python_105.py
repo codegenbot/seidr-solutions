a162ee0a3b07a981efe22c08a7d5007fa22f67b3
beep boop <githubbot@vadim.me>
2024-07-06 02:41:24 +0200

    Solution 90 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/tn9pfa00 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf29e042-1f02-49ae-bd21-b7cc3204f 2af.py", line 11, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf29e042-1f02-49ae-bd21-b7cc3204f 2af.py", line 8, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf29e042-1f02-49ae-bd21-b7cc3204f 2af.py", line 4, in by_length     return map(str, sorted(arr, key=len))                     ^^^^^^^^^^^^^^^^^^^^ TypeError: object of type 'int' has no len()

--- Python_105.py
+++ Python_105.py
@@ -2,4 +2,4 @@
 def by_length(arr):
     if not isinstance(arr, (list, tuple)):
         raise TypeError("by_length() only accepts lists or tuples")
-    return [str(x) for x in sorted(arr, key=lambda x: str(x))]
+    return list(map(lambda x: int(str(x)), sorted(arr, key=len(str(x)))))
