8d9bda367607b04681c1da854b2450899e0d4ad1
beep boop <githubbot@vadim.me>
2024-07-06 10:38:17 +0200

    Solution 10 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/u89qmsrm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a86bf9cd-3bf0-433f-a0f8-94786ac1a 437.py", line 16, in <module>     check(parse_nested_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a86bf9cd-3bf0-433f-a0f8-94786ac1a 437.py", line 13, in check     assert parse_nested_parens("(()(())((())))") == [4]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_6.py
+++ Python_6.py
@@ -1,11 +1,15 @@
-def parse_nested_parens(paren_string: str) -> list[int]:
+
+def parse_nested_parens(paren_string):
     result = []
+    level = 0
     for group in paren_string.split():
-        level = 0
+        level += 1
         for char in group:
-            if char == '(':
+            if char == "(":
                 level += 1
-            elif char == ')':
+            elif char == ")":
                 level -= 1
-        result.append(level)
+        if level == 0:
+            result.append(level)
     return result
+
