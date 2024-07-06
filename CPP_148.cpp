fbbc9a2edea63a152830b0b07f935647e13ee621
beep boop <githubbot@vadim.me>
2024-07-06 19:12:40 +0200

    Solution 80 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ygswh6dl Prompt: In file included from dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:19:37: error: ‘string’ was not declared  in this scope    19 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:19:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:19:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -2,8 +2,25 @@
 #include <string>
 #include <algorithm>
 
-std::string bf(std::string a, std::string b) {
-    // Define the function body here
+std::vector<std::string> bf(std::string a, std::string b) {
+    int m = a.size(), n = b.size();
+    std::vector<std::string> result;
+    
+    for (int i = 0; i <= m - b.size(); i++) {
+        bool isMatch = true;
+        for (int j = 0; j < b.size(); j++) {
+            if (a[i + j] != b[j]) {
+                isMatch = false;
+                break;
+            }
+        }
+        
+        if (isMatch) {
+            result.push_back(a.substr(i, b.size()));
+        }
+    }
+    
+    return result;
 }
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b){
