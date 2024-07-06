90ae4f65ca0ac247aa2473b5eccdd385c85a84b1
beep boop <githubbot@vadim.me>
2024-07-06 11:58:09 +0200

    Solution 43 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4htq3f01 Prompt:    31 | int main(){       |     ^~~~ a2f3e307-8300-465c-8809-4208a854cc62.cpp:2:5: note: ‘int main()’ previously defi ned here     2 | int main() {       |     ^~~~ In file included from a2f3e307-8300-465c-8809-4208a854cc62.cpp:30: a2f3e307-8300-465c-8809-4208a854cc62.cpp: In function ‘int main()’: a2f3e307-8300-465c-8809-4208a854cc62.cpp:32:13: error: ‘boost’ has not been decl ared    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ a2f3e307-8300-465c-8809-4208a854cc62.cpp:32:29: error: ‘string’ was not declared  in this scope    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ a2f3e307-8300-465c-8809-4208a854cc62.cpp:32:37: error: ‘compare_one’ was not dec lared in this scope    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <string>
 #include <limits>
@@ -15,12 +14,11 @@ int main() {
         cout << "Invalid input. Please enter an integer.\n";
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cin >> val1;
-        if (cin.fail()) {
-            cout << "Invalid input again. Please enter a valid number.\n";
-        }
-    }
+        cin >> val1;  
+    } 
 
+    cout << "The first number is: " << val1 << endl;
+    
     if (val1 > stoi(val2)) {
         cout << "The first number is bigger.\n";
     } else if (val1 < stoi(val2)) {
