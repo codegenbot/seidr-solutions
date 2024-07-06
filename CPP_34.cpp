a7c98172f628c5452cf677c784b5e8259ba0ddf7
beep boop <githubbot@vadim.me>
2024-07-06 12:05:54 +0200

    Solution 26 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tnxfnu72 Prompt:    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3b185407-d0da-4b6e-8f7a-afb5f4a9cca8.cpp:14: 3b185407-d0da-4b6e-8f7a-afb5f4a9cca8.cpp: In function ‘int main()’: 3b185407-d0da-4b6e-8f7a-afb5f4a9cca8.cpp:24:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    24 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 3b185407-d0da-4b6e-8f7a-afb5f4a9cca8.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ In file included from 3b185407-d0da-4b6e-8f7a-afb5f4a9cca8.cpp:14: 3b185407-d0da-4b6e-8f7a-afb5f4a9cca8.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) return false;
@@ -9,8 +9,8 @@ bool issame(vector<int> a, vector<int> b) {
     return true;
 }
 
-vector<int> unique(vector<int> l) {
-    vector<int> result(l.begin(), std::unique(l.begin(), l.end()).end());
+std::vector<int> unique(std::vector<int> l) {
+    std::vector<int> result(l.begin(), std::unique(l.begin(), l.end()).end());
     return result;
 }
 
