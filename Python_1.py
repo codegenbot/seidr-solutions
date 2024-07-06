86b46d345210ace787049bd4a4e785502b766304
beep boop <githubbot@vadim.me>
2024-07-06 11:13:49 +0200

    Solution 87 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ov14dqn5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c03acdc7-631b-44c2-85f1-608d6a277 9e9.py", line 26, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c03acdc7-631b-44c2-85f1-608d6a277 9e9.py", line 23, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ AssertionError

--- Python_1.py
+++ Python_1.py
@@ -1,21 +1,21 @@
-def separate_paren_groups(paren_string):
-    stack = []
-    result = []
-    count = 0
-    for c in paren_string:
-        if c == "(":
-            stack.append(c)
-            count += 1
-        elif c == ")":
-            if len(stack) > 0 and stack[-1] == "(":
-                stack.pop()
-                count -= 1
-            else:
-                while len(stack) > 0 and stack[-1] in ["(", ")"]:
+    def separate_paren_groups(paren_string):
+        stack = []
+        result = []
+        count = 0
+        for c in paren_string:
+            if c == "(":
+                stack.append(c)
+                count += 1
+            elif c == ")":
+                if len(stack) > 0 and stack[-1] == "(":
                     stack.pop()
-                result.append([c])
-    return [
-        group
-        for group in result
-        if len(group) % 2 == 0 and count == 0 and group[0] == "(" and group[-1] == ")"
-    ]
+                    count -= 1
+                else:
+                    while len(stack) > 0 and stack[-1] in ["(", ")"]:
+                        stack.pop()
+                    result.append([c])
+        return [
+            group
+            for group in result
+            if len(group) % 2 == 0 and count == 0 and group[0] == "(" and group[-1] == ")"
+        ]
