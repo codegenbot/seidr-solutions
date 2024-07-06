ae072b86d9991c0007f1de575da42b9bcafb719d
beep boop <githubbot@vadim.me>
2024-07-06 05:26:43 +0200

    Solution 48 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0n2q1vau Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6124b54d-1213-4e23-97d6-96ac3662a 5c1.py", line 20, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6124b54d-1213-4e23-97d6-96ac3662a 5c1.py", line 15, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,13 @@
-return [
-    (x if x % 3 != 0 else sorted([i for i in l if i % 3 == 0])[-1])
-    for x in sorted([i for i in l if i % 3 != 0], key=lambda x: (x, 2 * x))
-]
+def sort_third(l: list):
+    return [
+        (
+            i if isinstance(i, int) else 0
+            if isinstance(i, int) and i % 3 != 0
+            else (
+                sorted([x for x in l if isinstance(x, int) and x % 3 == 0])[-1]
+                if any(isinstance(x, int) and x % 3 == 0 for x in l)
+                else 0
+            )
+        )
+        for i in l
+    ]
