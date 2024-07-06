d1db765a32b3df80f13f5d4aa8b78da8268ef53f
beep boop <githubbot@vadim.me>
2024-07-06 03:13:24 +0200

    Solution 95 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/8987x0o8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a5a558f3-d499-426b-b834-3371d904d 9a4.py", line 23, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a5a558f3-d499-426b-b834-3371d904d 9a4.py", line 20, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a5a558f3-d499-426b-b834-3371d904d 9a4.py", line 17, in separate_paren_groups     result.append("".join(stack))                   ^^^^^^^^^^^^^^ TypeError: sequence item 0: expected str instance, list found

--- Python_1.py
+++ Python_1.py
@@ -1,18 +1,16 @@
 def separate_paren_groups(paren_string):
-    result = ""
-    stack = []
-    for char in paren_string:
-        if char == "(":
-            stack.append([])
-        elif char == ")":
-            if len(stack) > 0:
-                stack[-1].append(char)
-            else:
-                result += "".join(stack)
-                stack = []
-        else:
-            if len(stack) > 0:
-                stack[-1].append(char)
-    if len(stack) > 0:
-        result += "".join(stack)
-    return result
+result = ""
+stack = []
+for char in paren_string:
+if char == "(":
+stack.append([])
+elif char == ")":
+if len(stack) > 0:
+stack[-1].append(char)
+else:
+result += "".join(stack)
+stack = []
+else:
+if len(stack) > 0:
+stack[-1].append(char)
+return result
