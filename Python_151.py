e2b3efca16cd6288ea6ab7970585c47fa6bf730e
beep boop <githubbot@vadim.me>
2024-07-06 11:28:47 +0200

    Solution 70 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/v45sfc1y Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/edda073d-dd54-48f4-8a3a-83dbaf86d 960.py", line 16, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/edda073d-dd54-48f4-8a3a-83dbaf86d 960.py", line 12, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -2,11 +2,10 @@ def double_the_difference(lst):
     result = 0
     for num in lst:
         if num % 2 != 0 and num > 0:
-            result += num ** 2
+            result += num**2
     return result
 
-# Define the input list
-lst = [1, 2, 3, 4, 5]
 
-# Call the function with the defined list
-double_the_difference(lst)
+if __name__ == "__main__":
+    lst = list(map(int, input("Enter a list of numbers: ").split()))
+    print(double_the_difference(lst))
