c42190b3cf5f44e7d545a7310419cba637255323
beep boop <githubbot@vadim.me>
2024-07-06 19:21:52 +0200

    Solution 91 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ygswh6dl Prompt: In file included from a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:19:37: error: ‘string’ was not declared  in this scope    19 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:19:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:19:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -13,43 +13,15 @@ bool issame(std::vector<std::string> a, std::string b) {
 }
 
 std::vector<std::string> bf(std::string a, std::string b) {
-    int n = 0, m = 0;
-    for (char &c : a) {
-        if (c == '1') ++n;
-    }
-    for (char &c : b) {
-        if (c == '1') ++m;
-    }
-
     std::vector<std::string> result;
-
-    if (n > m) {
-        int i = 0, j = n - m;
-        while (i < a.size() && j--) {
-            result.push_back(a.substr(i) + "0");
-            i++;
+    int pos = 0;
+    while (pos < a.size()) {
+        int found = b.find(a.substr(pos));
+        if (found == std::string::npos) {
+            break;
         }
-        while (j--) {
-            result.push_back("0");
-        }
-    } else if (n < m) {
-        int i = 0, j = m - n;
-        while (i < b.size() && j--) {
-            result.push_back("1" + b.substr(i));
-            i++;
-        }
-        while (j--) {
-            result.push_back("1");
-        }
-    } else {
-        for (char &c : a) {
-            if (c == '1') {
-                result.push_back("1");
-            } else {
-                result.push_back("0");
-            }
-        }
+        result.push_back(b.substr(found, a.size() - pos));
+        pos += found + 1;
     }
-
     return result;
 }
