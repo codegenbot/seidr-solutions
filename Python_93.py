322a93867cdbce34fb91ec1373424a768ad4f4d1
beep boop <githubbot@vadim.me>
2024-07-06 07:25:52 +0200

    Solution 76 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gxmme9ma Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b609c0e2-e931-44fa-bf89-6a93990c5 84e.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b609c0e2-e931-44fa-bf89-6a93990c5 84e.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -5,7 +5,10 @@ def encode(message):
             if char.lower() in "aeiou":
                 result += chr((ord(char) - ord('a') + 2) % 26 + ord('a'))
             else:
-                result += char.swapcase()
+                if char.isupper():
+                    result += chr((ord(char) - ord('A') + 2) % 26 + ord('A'))
+                else:
+                    result += chr((ord(char) - ord('a') + 2) % 26 + ord('a'))
         else:
             result += char
     return result
