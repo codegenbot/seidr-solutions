f6a77135c00d3ab7dc4cd526e0bb4cf59d68f803
beep boop <githubbot@vadim.me>
2024-07-06 06:53:03 +0200

    Solution 83 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dh0r2tty Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -1,4 +1,4 @@
 def decode_shift(s: str):
     return "".join(
-        [chr((ord(ch.lower() - 97) % 26 + 97)) if ch.isalpha() else ch for ch in s]
+        [chr((ord((ch.lower() - 97) % 26 + 97))) if ch.isalpha() else ch for ch in s]
     )
