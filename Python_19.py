9eb32022f41209e84f852c20a2d7a137177063d6
beep boop <githubbot@vadim.me>
2024-07-06 11:41:06 +0200

    Solution 54 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/05osbftj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c67aaf78-ab9d-4e83-970c-475aafd9b 158.py", line 6     number_map = {                   ^ IndentationError: unindent does not match any outer indentation level

--- Python_19.py
+++ Python_19.py
@@ -1,6 +1,19 @@
+import re
 def sort_numbers(numbers):
-    if not re.match(r"^[a-zA-Z0-9]+$", numbers):
+    if not numbers.isdigit():
         return "Invalid input"
-    numbers = [int(num) for num in re.findall(r"\d+", numbers)]
+     number_map = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+    numbers = [int(number_map[num]) for num in re.findall(r"\d+", numbers)]
     sorted_numbers = sorted(numbers)
     return " ".join([str(num) for num in sorted_numbers])
