10df9e7607295f23d2a7bfe8dd3160c741f8049d
beep boop <githubbot@vadim.me>
2024-07-06 06:55:44 +0200

    Solution 13 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4t9e8soi Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7a11485f-59c2-4f0d-bcb3-c0aa4137d 039.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,4 +1,2 @@
 def solution(lst):
-    odd_sum = sum(i for i in lst[1::2] if i % 2 != 0)
-    even_count = len([i for i in lst if i % 2 == 0])
-    return f"The sum of odd numbers is {odd_sum} and the number of even numbers is {even_count}"
+    return sum(i for i in lst[1::2] if i % 2 != 0)
