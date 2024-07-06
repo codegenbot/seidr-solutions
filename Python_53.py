4d33778465693bd93b3b4b5c74a1d8577a2abd25
beep boop <githubbot@vadim.me>
2024-07-06 17:41:07 +0200

    Solution 17 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jaxdufv2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ee4a1061-863b-4d24-a52b-951170ea9 948.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,12 +1,15 @@
-```
 def add(x: int, y: int):
     return x + y
 
 while True:
     try:
         x = int(input("Enter the first number: "))
-        y = int(input("Enter the second number: "))
-        print(f"The sum is {add(x, y)}")
-        break
+        if -2**31 <= x <= 2**31 -1:
+            y = int(input("Enter the second number: "))
+            if -2**31 <= y <= 2**31 -1:
+                print(f"The sum is {add(x, y)}")
+                break
+        else:
+            print("Invalid input. Please enter a whole number within the range (-2,147,483,648 to 2,147,483,647).")
     except ValueError:
         print("Invalid input. Please enter a whole number.")
