1fa466a126b5d53d91aef2892da773a3671fb890
beep boop <githubbot@vadim.me>
2024-07-06 06:50:43 +0200

    Solution 12 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/67p9mn8z Prompt: Complete the following code given the task description and function signature.

--- Python_67.py
+++ Python_67.py
@@ -1,3 +1,4 @@
-def fruit_distribution(s, n):
-    apples, oranges = map(int, s.split(" "))
-    return n - apples - oranges
+
+def fruit_distribution(s,n):
+    total_fruits = int(s.split()[0]) + int(s.split()[-1])
+    return n - total_fruits
