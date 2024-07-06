d6de60639b4cf0fa8a0835c6246ce7656b2cd242
beep boop <githubbot@vadim.me>
2024-07-06 16:07:30 +0200

    Solution 13 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/18agfa5l Prompt: Program did not receive expected input

--- Python_28.py
+++ Python_28.py
@@ -1,15 +1,3 @@
-from typing import List
-
-
-def concatenate(strings: List[str]) -> str:
-    result = ""
-    for string in strings:
-        if len(result) > 0:
-            result += " "
-        result += string
-    return result
-
-strings = input().split()
-strings = [i for i in strings if i]  # remove empty strings
-
+strings = input("Please enter a list of strings (separated by any character): ")
+strings = [i for i in re.split("([^\w\s]+)", strings) if i]
 print(concatenate(strings))
