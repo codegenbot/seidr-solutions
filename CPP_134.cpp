2fe7a5bbc44d302fac540446419a5540784d9b05
beep boop <githubbot@vadim.me>
2024-07-06 23:17:47 +0200

    Solution 65 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1ydfzphe Prompt: 171e6ffe-2bde-4ba0-bdf4-8b7e45dbce68.cpp:1:37: error: ‘string’ was not declared in this scope     1 | bool check_if_last_char_is_a_letter(string txt) {       |                                     ^~~~~~ In file included from 171e6ffe-2bde-4ba0-bdf4-8b7e45dbce68.cpp:7: 171e6ffe-2bde-4ba0-bdf4-8b7e45dbce68.cpp: In function ‘int main()’: 171e6ffe-2bde-4ba0-bdf4-8b7e45dbce68.cpp:9:43: error: ‘check_if_last_char_is_a_l etter’ cannot be used as a function     9 |     assert (check_if_last_char_is_a_letter("apple pi e ") == false);       |             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -1,10 +1,9 @@
-```cpp
 #include <string>
 #include <cctype>
 #include <algorithm>
 
-bool check_if_last_char_is_a_letter(std::string txt) {
+bool check_if_last_char_is_a_letter(string txt) {
     if(txt.empty()) return false;
-    std::string lastChar = txt.substr(txt.length()-1, 1);
+    string lastChar = txt.substr(txt.length()-1, 1);
     return (isalpha(lastChar[0])) && (!any_of(istospace(), txt.begin(), txt.end()));
 }
