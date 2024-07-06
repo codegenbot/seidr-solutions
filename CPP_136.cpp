9ed111cbe3ba5cc5a81d9f13bb79a2076f3914f1
beep boop <githubbot@vadim.me>
2024-07-06 18:04:26 +0200

    Solution 15 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/co02erto Prompt:    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 82e5b3fa-9697-41ec-a890-20a09d753a6c.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 82e5b3fa-9697-41ec-a890-20a09d753a6c.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 82e5b3fa-9697-41ec-a890-20a09d753a6c.cpp:13: 82e5b3fa-9697-41ec-a890-20a09d753a6c.cpp: In function ‘int main()’: 82e5b3fa-9697-41ec-a890-20a09d753a6c.cpp:23:20: error: ‘largest_smallest_integer s’ was not declared in this scope    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 82e5b3fa-9697-41ec-a890-20a09d753a6c.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,18 +1,46 @@
+```cpp
 bool issame(vector<int> a, vector<int> b) {
     return (a.size() == 2 && b.size() == 2 && a[0] == b[0] && a[1] == b[1]);
 }
 
 int main() {
-    vector<int> lst;
-    int num;
-    while (true) {
-        cout << "Enter a number (-9999 to stop): ";
-        cin >> num;
-        if (num == -9999)
-            break;
-        lst.push_back(num);
+    vector<int> largest_smallest_integers(vector<int> lst) {
+        int a = 0, b = INT_MAX;
+        for (int x : lst) {
+            if (x < 0 && x > a) a = x;
+            else if (x > 0 && x < b) b = x;
+        }
+        return {a, b};
     }
-    vector<int> result = largest_smallest_integers(lst);
-    cout << "Largest: " << result[1] << endl;
-    cout << "Smallest: " << result[0] << endl;
+
+    vector<int> input;
+    cout << "Enter some integers separated by space: ";
+    while (true) {
+        int num;
+        cin >> num;
+        input.push_back(num);
+        char c;
+        cin >> c;
+        if (c == 'q') break;
+    }
+    
+    vector<int> result = largest_smallest_integers(input);
+    
+    cout << "Largest and smallest integers are: ";
+    for(int x : result) cout << x << " ";
+    cout << endl;
+
+    vector<vector<int>> pairs;
+    cin >> c;
+    while (c != 'q') {
+        vector<int> pair;
+        cin >> pair[0] >> pair[1];
+        pairs.push_back(pair);
+        cin >> c;
+    }
+
+    for(vector<int> p : pairs) {
+        if(issame(p, result)) cout << "Same as largest and smallest integers." << endl;
+        else cout << "Not same as largest and smallest integers." << endl;
+    }
 }
