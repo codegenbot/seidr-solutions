2b35eb32b11af36f6ae4e7c11ce20a52de003a91
beep boop <githubbot@vadim.me>
2024-07-06 08:00:50 +0200

    Solution 27 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ssvdmxr6 Prompt:    31 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 569240cd-8292-4e3a-9aac-ff5f3625ab59.cpp:31:44: error: ‘b’ was not declared in t his scope    31 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 569240cd-8292-4e3a-9aac-ff5f3625ab59.cpp:31:45: error: expression list treated a s compound expression in initializer [-fpermissive]    31 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 569240cd-8292-4e3a-9aac-ff5f3625ab59.cpp:30: 569240cd-8292-4e3a-9aac-ff5f3625ab59.cpp: In function ‘int main()’: 569240cd-8292-4e3a-9aac-ff5f3625ab59.cpp:40:20: error: ‘select_words’ was not de clared in this scope    40 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 569240cd-8292-4e3a-9aac-ff5f3625ab59.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,7 +1,7 @@
-bool issame(char ch1, char ch2) {
-    if (isalpha(ch1) && isalpha(ch2)) {
-        return tolower(ch1) == tolower(ch2);
+bool issame(char c1, char c2) {
+    if (isalpha(c1) && isalpha(c2)) {
+        return tolower(c1) == tolower(c2);
     } else {
-        return false;
+        return c1 == c2;
     }
 }
