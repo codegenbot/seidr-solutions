f0e05e801e9b04298a792162a698f2cef82b678a
beep boop <githubbot@vadim.me>
2024-07-06 11:09:53 +0200

    Solution 79 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ov14dqn5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2b329f2e-61a8-41a0-8169-53fd3cf99 fb4.py", line 27, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2b329f2e-61a8-41a0-8169-53fd3cf99 fb4.py", line 24, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ AssertionError

--- Python_1.py
+++ Python_1.py
@@ -15,8 +15,4 @@ def separate_paren_groups(paren_string):
                 while len(stack) > 0 and stack[-1] in ["(", ")"]:
                     stack.pop()
                 result.append(c)
-    return [
-        [group]
-        for group in result
-        if group[0] == "(" and group[-1] == ")" and len(group) % 2 == 0 and count == 0
-    ]
+    return [[group] for group in result if group[0] == "(" and group[-1] == ")" and len(group) % 2 == 0 and count == 0]
