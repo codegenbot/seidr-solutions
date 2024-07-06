17c6658d3eca7a81a1c1aec7d61ab16706cc5336
beep boop <githubbot@vadim.me>
2024-07-06 16:25:03 +0200

    Solution 11 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5j0y4o2g Prompt: a39f309b-c6cc-4e22-8fdc-0f46bb86f086.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<string> by_length(vector<int> arr){       | ^~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,16 +1,3 @@
 using namespace std;
 
 std::vector<std::string> by_length(std::vector<int> arr){
-    std::vector<std::string> temp;
-    for(int i: arr){
-        if(i >= 1 && i <= 9)temp.push_back(numToName[i]);
-    }
-    sort(temp.begin(), temp.end());
-    reverse(temp.begin(), temp.end());
-    std::vector<std::string> result;
-    std::map<int, std::string> numToName = {{1,"One"}, {2,"Two"}, {3,"Three"}, {4,"Four"}, {5,"Five"}, {6,"Six"}, {7,"Seven"}, {8,"Eight"}, {9,"Nine"}; 
-    for(int i: temp){
-        result.push_back(numToName[i]);
-    }
-    return result;
-}
