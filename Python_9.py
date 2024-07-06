b7ec8f2bbf3845f979acb5de78c68604501e767f
beep boop <githubbot@vadim.me>
2024-07-06 14:55:41 +0200

    Solution 9 of Python/9, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9qajdpid Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/81ade9dc-89dc-481c-9b2e-155a5758a ee6.py", line 1, in <module>     def rolling_max(numbers: List[int]) -> List[int]:                              ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_9.py
+++ Python_9.py
@@ -1,8 +1,7 @@
-def rolling_max(numbers: List[int]) -> List[int]:
+def rolling_max(numbers: list[int]) -> list[int]:
     result = []
-    max_so_far = numbers[0]
+    max_so_far = float('-inf')  
     for num in numbers:
-        if num > max_so_far:
-            max_so_far = num
+        max_so_far = max(num, max_so_far)
         result.append(max_so_far)
     return result
