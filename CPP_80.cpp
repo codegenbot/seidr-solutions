8990d570dbec3070eb01300a49c26b2e88556883
beep boop <githubbot@vadim.me>
2024-07-06 15:51:29 +0200

    Solution 41 of CPP/80, ratio 0.75 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yl3wmkze Prompt: f40fd171-8558-4348-99c7-d0733a7d9141.cpp:3:15: error: ‘string’ was not declared in this scope     3 | bool is_happy(string s) {       |               ^~~~~~ f40fd171-8558-4348-99c7-d0733a7d9141.cpp:3:15: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from f40fd171-8558-4348-99c7-d0733a7d9141.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from f40fd171-8558-4348-99c7-d0733a7d9141.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from f40fd171-8558-4348-99c7-d0733a7d9141.cpp:19: f40fd171-8558-4348-99c7-d0733a7d9141.cpp: In function ‘int main()’: f40fd171-8558-4348-99c7-d0733a7d9141.cpp:21:21: error: ‘is_happy’ cannot be used  as a function    21 |     assert (is_happy("iopaxioi") == false );       |             ~~~~~~~~^~~~~~~~~~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,15 +1,18 @@
-bool is_happy(string s){
-    if(s.length() < 3) return false;
+#include <string>
+#include <algorithm>
+
+int is_happy(std::string s) {
+    if(s.length() < 3) return 0;
     for(int i = 0; i < s.length()-2; i++){
         bool unique = true;
-        string substr = s.substr(i,3);
+        std::string substr = s.substr(i,3);
         for(char c : substr){
-            if(find(substr.begin(),substr.end(),c)-substr.end() >= 0){
+            if(std::find(substr.begin(),substr.end(),c)-substr.end() >= 0){
                 unique = false;
                 break;
             }
         }
-        if(unique) return true;
+        if(unique) return 1;
     }
-    return false;
+    return 0;
 }
