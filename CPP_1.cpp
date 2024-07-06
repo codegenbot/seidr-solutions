d7c2e5e862683e676ab50d44b310602f84750388
beep boop <githubbot@vadim.me>
2024-07-06 02:34:12 +0200

    Solution 44 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gbyn86lg Prompt: r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <string>   +++ |+#include <cassert>     3 | In file included from 859844a0-6513-4fc6-9ba8-2225f597b0cc.cpp:37: 859844a0-6513-4fc6-9ba8-2225f597b0cc.cpp: In function ‘int main()’: 859844a0-6513-4fc6-9ba8-2225f597b0cc.cpp:47:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    47 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~ 859844a0-6513-4fc6-9ba8-2225f597b0cc.cpp:6:6: note: candidate: ‘bool issame(cons t std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx1 1::basic_string<char> >&)’     6 | bool issame(const vector<string>& a, const vector<string>& b) {       |      ^~~~~~ 859844a0-6513-4fc6-9ba8-2225f597b0cc.cpp:38:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    38 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -5,17 +5,7 @@
 using namespace std;
 
 bool issame(const vector<string>& a, const vector<string>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-
-    return true;
+    return a == b;
 }
 
 vector<string> separate_paren_groups(string paren_string) {
@@ -41,6 +31,6 @@ vector<string> separate_paren_groups(string paren_string) {
 }
 
 int run_tests() {
-    assert(issame(separate_paren_groups("( ) (( ") , {"","",""}));
+    assert(issame(separate_paren_groups("( ) (( )"), vector<string>{{"()"}, {"()"}}));
     return 0;
 }
