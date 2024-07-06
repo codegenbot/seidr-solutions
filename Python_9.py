9f5aac24bae146517780c55c504cad9a60a43946
beep boop <githubbot@vadim.me>
2024-07-06 05:49:37 +0200

    Solution 31 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/osu2nq93 Prompt: Program did not receive expected input

--- Python_9.py
+++ Python_9.py
@@ -2,6 +2,5 @@ from typing import List
 
 
 def rolling_max(numbers: List[int]) -> List[int]:
-    max_sum = 0
-    result = [max(num for num in numbers[: i + 1]) for i in range(len(numbers))]
+    result = [max(sum(filter(None, numbers[: i + 1])) for i in range(len(numbers)))]
     return result
