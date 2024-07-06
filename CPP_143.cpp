8aedc17b623d2ba6a3188b6777b73e4698428c5c
beep boop <githubbot@vadim.me>
2024-07-06 10:26:19 +0200

    Solution 14 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/uv6ftgu4 Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ fcc0919d-655a-4364-a735-436a0daaa3a6.cpp:23:21:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,1 +1,42 @@
-int main
+```cpp
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::string words_in_sentence(std::string sentence){
+    std::string result = "";
+    for (const auto& word : split(sentence, ' ')) {
+        if (is_prime(word.length())) {
+            result += word + " ";
+        }
+    }
+    return result.substr(0, result.size() - 1);
+}
+
+std::vector<std::string> split(const std::string& str, char c) {
+    std::vector<std::string> tokens;
+    std::string temp = str;
+    size_t pos = 0;
+    while ((pos = temp.find(c)) != std::string::npos) {
+        tokens.push_back(temp.substr(0, pos));
+        temp.erase(0, pos + 1);
+    }
+    tokens.push_back(temp);
+    return tokens;
+}
+
+bool is_prime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+
+int main() {
+    std::cout << words_in_sentence("here is") << std::endl;
+    return 0;
+}
