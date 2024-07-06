c1b9ca7049e232981bf37f2a0d329b52b75aae56
beep boop <githubbot@vadim.me>
2024-07-06 14:31:43 +0200

    Solution 9 of Python/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9bklotpg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4175ff75-fefa-419b-a81e-bbb8bbd63 ce6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_7.py
+++ Python_7.py
@@ -1,7 +1,7 @@
 ```
-from typing import List
+from typing import list
 
-def filter_by_substring(strings: List[str], substring: str) -> List[str]:
+def filter_by_substring(strings: list, substring: str) -> list:
     return [s for s in strings if substring in s]
 
 if __name__ == "__main__":
