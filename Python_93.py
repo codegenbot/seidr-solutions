851ff1cdedb6daeab31033a7760a4454322dd730
beep boop <githubbot@vadim.me>
2024-07-06 08:29:22 +0200

    Solution 17 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3noqcgvu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ac37be99-f4f1-4c4e-b2b5-6ae0a257d 9ac.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -1,14 +1,15 @@
-```
 def encode(message):
-    vowels = 'aeiouAEIOU'
-    result = ''
+    vowels = "aeiouAEIOU"
+    result = ""
     for char in message:
         if char.isalpha():
             if char.lower() in vowels:
                 shift = 2
                 if char.isupper():
                     shift = -26
-                result += chr((ord(char) + shift) % 26 + ord('A') if char.isalpha() else ord('a'))
+                result += chr(
+                    (ord(char) + shift) % 26 + ord("A") if char.isupper() else ord("a")
+                )
             else:
                 result += char
         else:
