092ae158f93163a9f0399857d614b080e5cc5de8
beep boop <githubbot@vadim.me>
2024-07-06 04:04:29 +0200

    Solution 1 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/g5ej8jfl Prompt: Complete the following code given the task description and function signature.

--- Python_89.py
+++ Python_89.py
@@ -1,8 +1,6 @@
+
 def encrypt(s):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    rotated_alphabet = alphabet[2:] + alphabet[:2]
-    encrypted_string = ""
-    for char in s:
-        index = alphabet.index(char)
-        encrypted_string += rotated_alphabet[index]
-    return encrypted_string
+    new_alphabet = alphabet[2:] + alphabet[:2]
+    table = str.maketrans(alphabet, new_alphabet)
+    return s.translate(table)
