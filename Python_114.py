cff0adfbbb6bd1e20ca89ed0278bfd4e0812a60b
beep boop <githubbot@vadim.me>
2024-07-06 20:23:44 +0200

    Solution 42 of Python/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lpx7xrvd Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/49df82e2-fddc-4666-8901-336719bba 3a3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_114.py
+++ Python_114.py
@@ -1,7 +1,8 @@
+```
 input_num = input("Enter space-separated integers: ")
 nums = [int(x) for x in input_num.split()]
 print(minSubArraySum(nums))
-def minSubArraySum(*nums):
+def minSubArraySum(nums):
     total_sum = 0
     min_sum = float('inf')
     window_sum = 0
