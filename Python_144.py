d5eeb05d950bde6e9605c7e60c7bbd12bc21734a
beep boop <githubbot@vadim.me>
2024-07-06 02:56:25 +0200

    Solution 5 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lb1rqho5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c2d75b7f-3ddc-4002-927e-50050140e f52.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_144.py
+++ Python_144.py
@@ -1,20 +1,21 @@
+```
 def simplify(x, n):
-    x_num, x_denom = map(int, x.split("/"))
-    n_num, n_denom = map(int, n.split("/"))
+    x_num, x_denom = map(int, x.split('/'))
+    n_num, n_denom = map(int, n.split('/'))
 
     def gcd(a, b):
         while b:
             a, b = b, a % b
         return a
-
+    
     common = gcd(x_denom, n_denom)
-    new_x_num = x_num // common
-    new_n_num = n_num // common
-
-    simplified_x = str(new_x_num) + "/" + str(common)
-    simplified_n = str(new_n_num) + "/" + str(common)
-
-    if new_x_num * n_denom == new_n_num * x_denom:
-        return True, (simplified_x, simplified_n)
+    
+    simplified_x = str(x_num // common) + '/' + str(x_denom // common)
+    simplified_n = str(n_num // common) + '/' + str(n_denom // common)
+    
+    if x_num // common == 0:
+        return "0/1" if x_denom // common == 1 else "0/" + str(x_denom // common)
+    elif n_num // common == 0:
+        return "0/1"
     else:
-        return False, (x, n)
+        return simplified_x + ' / ' + simplified_n if simplified_x != simplified_n else simplified_x
