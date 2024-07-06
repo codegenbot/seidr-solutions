af442821b5f85e0cc1158802834b28ceca6a1add
beep boop <githubbot@vadim.me>
2024-07-06 06:16:18 +0200

    Solution 98 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l2mcvi3t Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7f303c95-15e4-4a62-bba9-b1b785baa 4ea.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_12.py
+++ Python_12.py
@@ -1,4 +1,3 @@
-```
 from typing import List, Optional
 
 def longest(strings: List[str]) -> Optional[str]:
@@ -7,8 +6,6 @@ def longest(strings: List[str]) -> Optional[str]:
     max_len = 0
     result = ''
     for s in strings:
-        if not s.isalnum():
-            continue
         if len(s) > max_len:
             max_len = len(s)
             result = s
