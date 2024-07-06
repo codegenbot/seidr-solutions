fd267542c0f83551aebe4619045c69d1cd0154e0
beep boop <githubbot@vadim.me>
2024-07-06 15:49:37 +0200

    Solution 54 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0wk0egm1 Prompt: In file included from 82291c38-67a1-4c2a-a0a1-4c4cf2ffcd58.cpp:21: 82291c38-67a1-4c2a-a0a1-4c4cf2ffcd58.cpp: In function ‘int main()’: 82291c38-67a1-4c2a-a0a1-4c4cf2ffcd58.cpp:31:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    31 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,12 +1,13 @@
-#include <vector>
-#include <algorithm>
+vector<int> strange_sort_vector(vector<int> lst) {
+    vector<int> result;
+    if (lst.empty()) return result;
 
-using namespace std;
-
-bool issame(vector<int> v1, vector<int> v2) {
-    if(v1.size() != v2.size()) return false;
-    for(int i = 0; i < v1.size(); i++) {
-        if(v1[i] != v2[i]) return false;
+    sort(lst.begin(), lst.end());
+    while (!lst.empty()) {
+        result.push_back(*lst.begin());
+        lst.erase(lst.begin());
+        if (!lst.empty())
+            sort(lst.begin(), lst.end());
     }
-    return true;
-}
+
+    return result;
