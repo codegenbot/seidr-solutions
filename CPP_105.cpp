fdb998cc5c67f3589850bb91876049202ce9a9bc
beep boop <githubbot@vadim.me>
2024-07-06 16:19:33 +0200

    Solution 5 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5j0y4o2g Prompt: a39f309b-c6cc-4e22-8fdc-0f46bb86f086.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<string> by_length(vector<int> arr){       | ^~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,16 +1,19 @@
 #include <vector>
 using namespace std;
 vector<pair<int, string>> by_length(vector<int> arr){
-    vector<int> temp;
+    vector<pair<int, string>> temp;
     for(int i: arr){
-        if(i >= 1 && i <= 9)temp.push_back(i);
+        if(i >= 1 && i <= 9)temp.push_back({i, to_string(i) == "1" ? "One": 
+            to_string(i) == "2" ? "Two":
+            to_string(i) == "3" ? "Three":
+            to_string(i) == "4" ? "Four":
+            to_string(i) == "5" ? "Five":
+            to_string(i) == "6" ? "Six":
+            to_string(i) == "7" ? "Seven":
+            to_string(i) == "8" ? "Eight":
+            "Nine"});
     }
     sort(temp.begin(), temp.end());
     reverse(temp.begin(), temp.end());
-    map<int, string> numToName = {{1,"One"}, {2,"Two"}, {3,"Three"}, {4,"Four"}, {5,"Five"}, {6,"Six"}, {7,"Seven"}, {8,"Eight"}, {9,"Nine"}; 
-    vector<pair<int, string>> result;
-    for(int i: temp){
-        result.push_back({i, numToName[i]});
-    }
-    return result;
+    return temp;
 }
