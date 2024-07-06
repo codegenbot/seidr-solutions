3c33cf7d0a889028003f8eaa86cf19d0693f4539
beep boop <githubbot@vadim.me>
2024-07-06 06:57:16 +0200

    Solution 58 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/d6ba9i14 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/79412647-a5d0-4d96-879d-e4cc05adb 2bf.py", line 11, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/79412647-a5d0-4d96-879d-e4cc05adb 2bf.py", line 7, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple"     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/79412647-a5d0-4d96-879d-e4cc05adb 2bf.py", line 2, in fix_spaces     return re.sub(r"\s+", "-", text)            ^^ NameError: name 're' is not defined

--- Python_140.py
+++ Python_140.py
@@ -1,8 +1,4 @@
-import re
-
-
 def fix_spaces(text):
-    text = re.sub(r"\s+", "-", text)
-    text = text.strip()
-    text = text.lower()
-    return text
+    import re
+
+    return re.sub(r"\s+", "-", text)
