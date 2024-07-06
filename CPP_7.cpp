5c9090d2cadbe59b3594d5373d47a3cec9c961ed
beep boop <githubbot@vadim.me>
2024-07-06 19:03:55 +0200

    Solution 58 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/sef7a86m Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f80bcf00-0d0e-4598-9454-99ca160a5672.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f80bcf00-0d0e-4598-9454-99ca160a5672.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f80bcf00-0d0e-4598-9454-99ca160a5672.cpp:11: f80bcf00-0d0e-4598-9454-99ca160a5672.cpp: In function ‘int main()’: f80bcf00-0d0e-4598-9454-99ca160a5672.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ f80bcf00-0d0e-4598-9454-99ca160a5672.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,3 +1,8 @@
 bool issame(vector<string> a,vector<string>b){
-    return a == b;
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
 }
