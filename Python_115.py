c742cbef667f28b902402da3720cf32f07b5ff63
beep boop <githubbot@vadim.me>
2024-07-06 04:18:57 +0200

    Solution 97 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1h36up85 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/62a7bffa-650b-4b16-a6a2-98dfa3681 8db.py", line 28, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/62a7bffa-650b-4b16-a6a2-98dfa3681 8db.py", line 25, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_115.py
+++ Python_115.py
@@ -1,4 +1,3 @@
-```
 def max_fill(grid, capacity):
     total_amount = 0
     left = [c for r in grid for c in r]
@@ -6,13 +5,11 @@ def max_fill(grid, capacity):
     left_ptr, right_ptr = 0, len(left) - 1
     
     while left_ptr <= right_ptr:
-        if left[left_ptr] < right[right_ptr]:
-            total_amount += min(capacity, left[left_ptr])
-            left[left_ptr] -= (min(capacity, left[left_ptr]))
+        if grid[0][left_ptr] < grid[0][right_ptr]:
+            total_amount += grid[0][left_ptr]
             left_ptr += 1
         else:
-            total_amount += min(capacity, right[right_ptr])
-            right[right_ptr] -= (min(capacity, right[right_ptr]))
+            total_amount += grid[0][right_ptr]
             right_ptr -= 1
     
     return total_amount
