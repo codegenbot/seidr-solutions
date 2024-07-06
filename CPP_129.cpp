5ee38ac98be31f48bf2bfe42cf835ae8b3ecddcd
beep boop <githubbot@vadim.me>
2024-07-06 18:09:04 +0200

    Solution 21 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1kalf9to Prompt:    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fc5365d9-72e3-4bc0-8348-b87f0cf0e55e.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fc5365d9-72e3-4bc0-8348-b87f0cf0e55e.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fc5365d9-72e3-4bc0-8348-b87f0cf0e55e.cpp:15: fc5365d9-72e3-4bc0-8348-b87f0cf0e55e.cpp: In function ‘int main()’: fc5365d9-72e3-4bc0-8348-b87f0cf0e55e.cpp:25:20: error: ‘minPath’ was not declare d in this scope    25 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ fc5365d9-72e3-4bc0-8348-b87f0cf0e55e.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,7 +1,11 @@
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
+bool issame(vector<vector<int>> grid1, vector<vector<int>> grid2) {
+    if(grid1.size() != grid2.size() || grid1[0].size() != grid2[0].size()) {
+        return false;
+    }
+    for(int i = 0; i < (int)grid1.size(); i++){
+        for(int j = 0; j < (int)grid1[0].size(); j++){
+            if(grid1[i][j] != grid2[i][j]) return false;
+        }
     }
     return true;
 }
