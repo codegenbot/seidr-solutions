c2bc41a529c248105cb9e6953bd29bb2321a89d3
beep boop <githubbot@vadim.me>
2024-07-06 02:16:52 +0200

    Solution 6 of Python/147, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/hl38wm9a Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/21a67d8d-5630-4fd1-88af-9c2708c68 2da.py", line 14, in <module>     check(get_max_triples)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/21a67d8d-5630-4fd1-88af-9c2708c68 2da.py", line 11, in check     assert get_max_triples(100) == 53361            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_147.py
+++ Python_147.py
@@ -1,9 +1,16 @@
+
 def get_max_triples(n):
-    a = [i * i - i + 1 for i in range(1, n + 1)]
-    count = 0
-    for i in range(len(a)):
-        for j in range(i + 1, len(a)):
-            for k in range(j + 1, len(a)):
-                if a[i] + a[j] + a[k] % 3 == 0:
-                    count += 1
-    return count
+    primes = []
+    for i in range(2, n+1):
+        is_prime = True
+        for j in range(2, int(i**0.5) + 1):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(i)
+    dp = [0] * (n+1)
+    for prime in primes:
+        for i in range(len(dp)):
+            dp[i] += dp[i-prime]
+    return dp[-1]
