dd65907e6b0757850025524c5f40118b8fb6f6b1
beep boop <githubbot@vadim.me>
2024-07-06 17:27:05 +0200

    Solution 97 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1fv3kcsh Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/54adc6be-e0f2-4d2f-a13e-f8be11933 685.py", line 17, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/54adc6be-e0f2-4d2f-a13e-f8be11933 685.py", line 12, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,11 +1,8 @@
-```
 def sort_even(l: list) -> tuple:
-    ints = [x for x in l if isinstance(x, int)]
-    negative_numbers = sorted([x for x in ints if x < 0])
-    zero_number = [x for x in ints if x == 0]
+    even_numbers = [x for x in l if isinstance(x, int) and x % 2 == 0]
+    sorted_negative = sorted([x for x in even_numbers if x < 0])
+    zeros = [x for x in even_numbers if x == 0]
     
-    positive_numbers = sorted([x for x in ints if x > 0])
-    even_positive_numbers = [x for x in positive_numbers if x % 2 == 0]
-    odd_positive_numbers = [x for x in positive_numbers if x % 2 != 0]
+    sorted_positive = sorted([x for x in even_numbers if x > 0])
     
-    return (tuple(negative_numbers + zero_number), tuple(even_positive_numbers + odd_positive_numbers))
+    return (tuple(sorted_negative + zeros), tuple(sorted_positive))
