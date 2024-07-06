f52be8e013f5f54f8db14f4030e986112f88cb4f
beep boop <githubbot@vadim.me>
2024-07-06 16:57:53 +0200

    Solution 72 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gashg63h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b56ce147-1919-43ab-8e07-92dcb3ad4 c89.py", line 2, in <module>     user_input = input(f"Enter string {i+1}: ")                                        ^ NameError: name 'i' is not defined. Did you mean: 'id'?

--- Python_12.py
+++ Python_12.py
@@ -1,9 +1,11 @@
+```Python
 strings = []
-i = 0
+i = 1
 while True:
-    user_input = input(f"Enter string {i+1}: ")
+    user_input = input(f"Enter string {i}: ")
     if len(user_input) > 0:
         strings.append(user_input)
         break
     else:
         print("Please enter a non-empty string.")
+        i += 1
