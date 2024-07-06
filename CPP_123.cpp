ca2fe972a617cb5b7548d4e9efba404dfac815c4
beep boop <githubbot@vadim.me>
2024-07-06 07:58:20 +0200

    Solution 5 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/o1mdw7cz Prompt:       |                    ^~~ 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp:17: 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp: In function ‘int main()’: 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp:27:20: error: ‘get_odd_collatz’ was not  declared in this scope    27 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 2f6e3caa-cfe2-49c5-adba-a278bb0ef4c6.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -5,15 +5,6 @@ bool issame(vector<int> a, vector<int> b) {
     return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
 }
 
-bool is_prime(int n) {
-    if (n <= 1)
-        return false;
-    for (int i = 2; i * i <= n; i++)
-        if (n % i == 0)
-            return false;
-    return true;
-}
-
 vector<int> get_odd_collatz(int n) {
     vector<int> result;
     while (n != 1) {
@@ -24,27 +15,5 @@ vector<int> get_odd_collatz(int n) {
         if (n % 2 != 0)
             result.push_back(n);
     }
+    sort(result.begin(), result.end());
     return result;
-}
-
-int main() {
-    int m, n;
-    cout << "Enter the first number: ";
-    cin >> m;
-    cout << "Enter the second number: ";
-    cin >> n;
-    
-    vector<int> a = get_odd_collatz(m);
-    vector<int> b = get_odd_collatz(n);
-
-    if (issame(a, b)) {
-        if (is_prime(m) && is_prime(n))
-            cout << "The numbers are prime and have the same Collatz sequence." << endl;
-        else
-            cout << "The numbers do not have the same Collatz sequence." << endl;
-    } else {
-        cout << "The numbers do not have the same Collatz sequence." << endl;
-    }
-    
-    return 0;
-}
