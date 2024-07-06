df7c43ad24940f6053d1643df2f8fd4011c741b4
beep boop <githubbot@vadim.me>
2024-07-06 14:28:32 +0200

    Solution 5 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0wk0egm1 Prompt: In file included from 9edbfcbc-0e47-4eb5-9c58-884c0d19efbb.cpp:21: 9edbfcbc-0e47-4eb5-9c58-884c0d19efbb.cpp: In function ‘int main()’: 9edbfcbc-0e47-4eb5-9c58-884c0d19efbb.cpp:31:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    31 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -17,27 +17,3 @@ vector<int> strange_sort_vector(vector<int> lst) {
 
     return result;
 }
-
-int main() {
-    vector<int> input;
-    int numElements;
-    cout << "Enter the number of elements in the list: ";
-    cin >> numElements;
-
-    for (int i = 0; i < numElements; i++) {
-        int n;
-        cout << "Enter element " << i + 1 << ": ";
-        cin >> n;
-        input.push_back(n);
-    }
-
-    vector<int> output = strange_sort_vector(input);
-
-    cout << "The sorted list is: ";
-    for (int x : output) {
-        cout << x << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
