e5a8410470c69a1dfb0738264fd0e1b8fbd7597b
beep boop <githubbot@vadim.me>
2024-07-06 05:25:51 +0200

    Solution 46 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0n2q1vau Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6124b54d-1213-4e23-97d6-96ac3662a 5c1.py", line 20, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6124b54d-1213-4e23-97d6-96ac3662a 5c1.py", line 15, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,9 @@
+```
 return [
-    (x if x % 3 != 0 else sorted([y for y in l if y % 3 == 0])[-1])
-    for x in sorted([i for i in l if i % 3 != 0], key=lambda x: (x, 2 * x))
+    (
+        x
+        if x % 3 != 0
+        else sorted([i for i in l if i % 3 == 0])[-1]
+    )
+    for x in sorted([i for i in l if i % 3 != 0], key=lambda x: (x, 2*x))
 ]
