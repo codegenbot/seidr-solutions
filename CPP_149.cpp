83d2bba4a58447f2922752d664bd9fab7e24a721
beep boop <githubbot@vadim.me>
2024-07-06 18:55:12 +0200

    Solution 64 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ccu534fs Prompt: In file included from a867ef73-8033-4386-90c1-5fc8bd4c2681.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ a867ef73-8033-4386-90c1-5fc8bd4c2681.cpp:39:44: error: ‘b’ was not declared in t his scope    39 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a867ef73-8033-4386-90c1-5fc8bd4c2681.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ a867ef73-8033-4386-90c1-5fc8bd4c2681.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main() {       |     ^~~~ In file included from a867ef73-8033-4386-90c1-5fc8bd4c2681.cpp:38: a867ef73-8033-4386-90c1-5fc8bd4c2681.cpp: In function ‘int main()’: a867ef73-8033-4386-90c1-5fc8bd4c2681.cpp:48:20: error: ‘sorted_list_sum’ was not  declared in this scope    48 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+bool isSame(std::vector<std::string> a, std::vector<std::string> b) {
     if(a.size() != b.size())
         return false;
     for(int i=0; i<a.size(); i++) {
@@ -12,7 +12,7 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return true;
 }
 
-std::vector<std::string> vector_sort(std::vector<std::string> lst) {
+std::vector<std::string> sortedListSum(std::vector<std::string> lst) {
     auto it = std::unique(lst.begin(), lst.end(),
         [](const std::string& a, const std::string& b){ 
             return (a.size() % 2 == 1 && b.size() % 2 == 0) || (a.size() % 2 == 0 && b.size() % 2 == 1);
@@ -32,5 +32,5 @@ std::vector<std::string> vector_sort(std::vector<std::string> lst) {
 }
 
 int main() {
-    assert(issame(vector_sort({"aaaa", "bbbb", "dd", "cc"}), {"ccc", "dd", "aaaa", "bbbb"}));
+    assert(isSame(sortedListSum({"aaaa", "bbbb", "dd", "cc"}), {"ccc", "dd", "aaaa", "bbbb"}));
 }
