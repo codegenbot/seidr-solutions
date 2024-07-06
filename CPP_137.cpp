6f5528b2c05ed10289eb934714ac015fecafcf6d
beep boop <githubbot@vadim.me>
2024-07-06 19:13:23 +0200

    Solution 99 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6jdkzc1w Prompt: 17c1a7c1-3175-419f-b9d1-c43868bea3fa.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 17c1a7c1-3175-419f-b9d1-c43868bea3fa.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 17c1a7c1-3175-419f-b9d1-c43868bea3fa.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 17c1a7c1-3175-419f-b9d1-c43868bea3fa.cpp:2:10: fatal error: boost/any.hpp: No su ch file or directory     2 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,7 +3,7 @@
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return (int)boost::any_cast<int>(a) > (double)boost::any_cast<double>(b) ? a : b;
+        return boost::any_cast<boost::any>(a) > boost::any_cast<boost::any>(b) ? a : b;
     }
     else if (a.type() == typeid(int) && b.type() == typeid(std::string)) {
         std::string str_a = boost::any_cast<std::string>(a);
