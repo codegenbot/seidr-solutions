557a61f87cf2f37b06dd18d8960c807748b9990a
beep boop <githubbot@vadim.me>
2024-07-06 12:04:10 +0200

    Solution 26 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dl2z8mmq Prompt:  compound expression in initializer [-fpermissive]     4 | int do_algebra(vector<char*> operators, vector<int> operands) {       |                                                             ^ a7c16f61-c57e-49cc-ad73-4e5bae22cbd4.cpp: In function ‘int main()’: a7c16f61-c57e-49cc-ad73-4e5bae22cbd4.cpp:23:28: error: ‘do_algebra’ cannot be us ed as a function    23 |     int result = do_algebra({"//", "*"}, {7, 3, 4});       |                  ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~ a7c16f61-c57e-49cc-ad73-4e5bae22cbd4.cpp: At global scope: a7c16f61-c57e-49cc-ad73-4e5bae22cbd4.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ a7c16f61-c57e-49cc-ad73-4e5bae22cbd4.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from a7c16f61-c57e-49cc-ad73-4e5bae22cbd4.cpp:27: a7c16f61-c57e-49cc-ad73-4e5bae22cbd4.cpp: In function ‘int main()’: a7c16f61-c57e-49cc-ad73-4e5bae22cbd4.cpp:29:23: error: ‘do_algebra’ cannot be us ed as a function    29 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,26 +1,33 @@
-#include <string>
 #include <vector>
 #include <cmath>
 
-int do_algebra(vector<string> operators, vector<int> operands) {
+int do_algebra(std::string operation, std::vector<int> operands) {
     int result = operands[0];
-    for (int i = 0; i < operators.size(); i++) {
-        if (operators[i] == "+") {
-            result += operands[i + 1];
-        } else if (operators[i] == "-") {
-            result -= operands[i + 1];
-        } else if (operators[i] == "*") {
-            result *= operands[i + 1];
-        } else if (operators[i] == "//") {
-            result = result / static_cast<int>(operands[i + 1]);
-        } else if (operators[i] == "**") {
-            result = pow(result, operands[i + 1]);
+    for (int i = 1; i < operands.size(); i++) {
+        if (operation == "*/") {
+            if (*operation.begin() == '*') {
+                result *= operands[i];
+            } else {
+                result /= operands[i];
+            }
+        } else if (operation == "+*") {
+            if (*operation.begin() == '+') {
+                result += operands[i];
+            } else {
+                result *= operands[i];
+            }
+        } else if (operation == "-+") {
+            if (*operation.begin() == '-') {
+                result -= operands[i];
+            } else {
+                result += operands[i];
+            }
         }
     }
     return result;
 }
 
 int main() {
-    int result = do_algebra({"//", "*"}, {7, 3, 4});
+    int result = do_algebra("//*", {7, 3, 4});
     return 0;
 }
