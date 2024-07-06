65bc5fb4bb4b8610333ae5fb5c07cbeb6a28a42f
beep boop <githubbot@vadim.me>
2024-07-06 03:37:51 +0200

    Solution 52 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/sateydpf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dcbf797f-24cf-4812-a277-87076f27d d7d.py", line 13, in <module>     check(rounded_avg)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dcbf797f-24cf-4812-a277-87076f27d d7d.py", line 10, in check     assert rounded_avg(5, 5) == "0b101"            ^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dcbf797f-24cf-4812-a277-87076f27d d7d.py", line 6, in rounded_avg     return bin(avg).replace("0b", "")            ^^^^^^^^ TypeError: 'float' object cannot be interpreted as an integer

--- Python_103.py
+++ Python_103.py
@@ -3,4 +3,4 @@ def rounded_avg(n, m):
         return "-1"
     total = sum(range(n, m + 1))
     avg = -(-total / (m - n + 1)) 
-    return bin(int(round(avg)))[2:]
+    return format(int(avg), 'b')
