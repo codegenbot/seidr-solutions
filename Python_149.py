59e9a04d82e9130ef08839ab1302c7bc6509f167
beep boop <githubbot@vadim.me>
2024-07-06 21:13:34 +0200

    Solution 68 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/bn9yd81p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fecca056-b357-48ba-80b0-59e1ef069 4b6.py", line 10, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fecca056-b357-48ba-80b0-59e1ef069 4b6.py", line 7, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,6 +1,4 @@
+```
 def sorted_list_sum(lst):
-    even_words = [word for word in lst if "".join(filter(str.isdigit, word))]
-    odd_words = [word for word in lst if word not in even_words]
-    
-    return " ".join(sorted(even_words, key=lambda x: int("".join(x))) + 
-                   sorted(odd_words))
+    even_words = [word for word in lst if any(c.isdigit() and int(c) % 2 == 0 for c in word)]
+    return " ".join(sorted(even_words, key=sum, reverse=True))
