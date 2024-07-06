f81def4ffb02e35f746646f3c680902e1c2c6b7e
beep boop <githubbot@vadim.me>
2024-07-06 18:26:42 +0200

    Solution 30 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r1a8ydno Prompt:    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 229c14f0-f023-46bb-ac96-206727f1fe24.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 229c14f0-f023-46bb-ac96-206727f1fe24.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 229c14f0-f023-46bb-ac96-206727f1fe24.cpp:17: 229c14f0-f023-46bb-ac96-206727f1fe24.cpp: In function ‘int main()’: 229c14f0-f023-46bb-ac96-206727f1fe24.cpp:27:20: error: ‘sorted_list_sum’ was not  declared in this scope    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 229c14f0-f023-46bb-ac96-206727f1fe24.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,12 +1,39 @@
-vector<string> sorted_list_sum(vector<string> lst) {
-    vector<string> new_list;
-    
-    for(auto s : lst){
-        if(s.length() % 2 == 0)
-            new_list.push_back(s);
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool issame(const string& a, const string& b) {
+    int sum_a = 0;
+    for (char c : a) {
+        if (c >= 'a' && c <= 'z')
+            sum_a += tolower(c) - 'a' + 1;
+        else
+            sum_a += toupper(c) - 'A' + 1;
     }
     
-    sort(new_list.begin(), new_list.end());
+    int sum_b = 0;
+    for (char c : b) {
+        if (c >= 'a' && c <= 'z')
+            sum_b += tolower(c) - 'a' + 1;
+        else
+            sum_b += toupper(c) - 'A' + 1;
+    }
     
-    return new_list;
+    return sum_a == sum_b;
+}
+
+vector<string> sorted_list_sum(vector<string> lst) {
+    auto it = unique(lst.begin(), lst.end(), 
+                     [](const string& a, const string& b) { return !issame(a, b); });
+    lst.erase(it, lst.end());
+    
+    sort(lst.begin(), lst.end(),
+         [](const string& a, const string& b) {
+             if (a.length() != b.length())
+                 return a.length() < b.length();
+             else
+                 return a < b;
+         });
+    
+    return lst;
 }
