b3aa69c55dee147ef162ea95f70b85391c69f0b4
beep boop <githubbot@vadim.me>
2024-07-06 23:09:53 +0200

    Solution 40 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/uttttzw3 Prompt:       |                    ^~~ 58b6b5ec-033e-4c3b-b0bb-90ad1dca24cc.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 58b6b5ec-033e-4c3b-b0bb-90ad1dca24cc.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 58b6b5ec-033e-4c3b-b0bb-90ad1dca24cc.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 58b6b5ec-033e-4c3b-b0bb-90ad1dca24cc.cpp:13: 58b6b5ec-033e-4c3b-b0bb-90ad1dca24cc.cpp: In function ‘int main()’: 58b6b5ec-033e-4c3b-b0bb-90ad1dca24cc.cpp:23:20: error: ‘compare’ was not declare d in this scope    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 58b6b5ec-033e-4c3b-b0bb-90ad1dca24cc.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,28 +1,18 @@
 #include <vector>
-
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
+bool issame(vector<int> a,vector<int>b);
 
 vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
     for (int i = 0; i < game.size(); i++) {
         if (game[i] == guess[i]) {
-            result.push_back(0);
+            result.push_back(2);
         } else {
             int diff = abs(game[i] - guess[i]);
-            if (!issame({game[i]}, {guess[i]})) {
-                diff++;
+            if (diff == game[i] || diff == guess[i]) {
+                result.push_back(1);
+            } else {
+                result.push_back(0);
             }
-            result.push_back(diff);
         }
     }
     return result;
