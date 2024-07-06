3117084716034e2447f5a9747d3475fe16cd0ffc
beep boop <githubbot@vadim.me>
2024-07-07 01:22:14 +0200

    Solution 37 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/8p2foubv Prompt: it must return 0U for input x0U-, but it returns .

--- middle-character.cpp
+++ middle-character.cpp
@@ -2,8 +2,8 @@
 std::string getMiddle(std::string str) {
     int len = str.length();
     if (len % 2 == 0) {
-        return str.substr((unsigned int)(len / 2) - 1, 2);
+        return str.substr((len / 2) - 1, 2);
     } else {
-        return str.substr((unsigned int)(len / 2), 1);
+        return str.substr(len / 2, 1);
     }
 }
