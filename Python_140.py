b948fc5c4fc218407b179ac294ba8ec5fa178f90
beep boop <githubbot@vadim.me>
2024-07-06 03:57:29 +0200

    Solution 48 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ynbb4504 Prompt: Program did not receive expected input

--- Python_140.py
+++ Python_140.py
@@ -1,6 +1,12 @@
-
 import re
+
 
 def fix_spaces(text):
     text = re.sub(r"\s+", "_", text)
     return text.replace(" ", "")
+
+
+if __name__ == "__main__":
+    user_input = input()
+    result = fix_spaces(user_input)
+    print(result)
