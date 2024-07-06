e663f83b05ad83a11c1c0beea1f4069e38613d3e
beep boop <githubbot@vadim.me>
2024-07-06 04:22:47 +0200

    Solution 97 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3vmnpvqv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7522b83d-f0f1-4e9d-8476-a58a07cf2 d51.py", line 13, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7522b83d-f0f1-4e9d-8476-a58a07cf2 d51.py", line 9, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,4 +1,5 @@
+```
 def fix_spaces(text):
-    if text.strip().lstrip().startswith(" "):
-        return "_" + "".join(["-" if c.isspace() else c for c in text.strip()[1:]])
-    return "-".join(["_" if c.isspace() else c for c in text])
+    if text.strip().lstrip().startswith(' '):  
+        return '_' + ''.join(['-' if c.isspace() else c for c in text.strip()[1:]])
+    return '-'.join(['_' if c.isspace() else c for c in text])
