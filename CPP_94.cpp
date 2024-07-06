77c1c9233c8e9ff1fd67f2a184122d4460f1af74
beep boop <githubbot@vadim.me>
2024-07-06 17:54:00 +0200

    Solution 79 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/shyup7qj Prompt: fc08e9d2-2c40-4b8a-8677-f7cfff107752.cpp:3:5: error: ‘assert’ was not declared i n this scope     3 |     assert(skjkasdkd(lst) == 10);       |     ^~~~~~ fc08e9d2-2c40-4b8a-8677-f7cfff107752.cpp:1:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?   +++ |+#include <cassert>     1 | int main() { fc08e9d2-2c40-4b8a-8677-f7cfff107752.cpp: At global scope: fc08e9d2-2c40-4b8a-8677-f7cfff107752.cpp:8:5: error: redefinition of ‘int main() ’     8 | int main(){       |     ^~~~ fc08e9d2-2c40-4b8a-8677-f7cfff107752.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from fc08e9d2-2c40-4b8a-8677-f7cfff107752.cpp:7: fc08e9d2-2c40-4b8a-8677-f7cfff107752.cpp: In function ‘int main()’: fc08e9d2-2c40-4b8a-8677-f7cfff107752.cpp:9:13: error: ‘skjkasdkd’ was not declar ed in this scope     9 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ^~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,7 +1,7 @@
 #include <cassert>
 #include <vector>
 
-int skjkasdkd(const std::vector<int>&);
+int skjkasdkd(const std::vector<int>& lst);
 
 int main() {
     std::vector<int> lst = {127, 97, 8192};
