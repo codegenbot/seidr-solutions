fc1655c962d79848e322a58eabd2d3a920fd77b0
beep boop <githubbot@vadim.me>
2024-07-06 08:22:52 +0200

    Solution 18 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/o1mdw7cz Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 91df499a-c1f7-4527-ace9-de19b8cedc64.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,8 +1,9 @@
-```cpp
 #include <vector>
 #include <algorithm>
+#include <initializer_list>
+#include <functional>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
@@ -13,27 +14,13 @@ std::vector<int> get_odd_collatz(int n) {
             n /= 2;
         else
             n = 3 * n + 1;
-        if (n % 2 != 0)
-            result.push_back(n);
+        result.push_back(n);
     }
     sort(result.begin(), result.end());
     return result;
 }
 
 int main() {
-    for(int i=1;i<=100;i++) {
-        vector<int> temp=get_odd_collatz(i);
-        bool unique=true;
-        for(auto x:temp) {
-            if(count(temp.begin(),temp.end(),x)>1) {
-                unique=false;
-                break;
-            }
-        }
-        if(unique) {
-            cout<<"Chain for "<<i<<" is unique"<<endl;
-        } else {
-            cout<<"Chain for "<<i<<" is not unique"<<endl;
-        }
-    }
+    assert(issame({get_odd_collatz(1)}, {1}));
+    return 0;
 }
