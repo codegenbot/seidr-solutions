6ef536ca306aa9bbe0a9b3842c172bafb019c604
beep boop <githubbot@vadim.me>
2024-07-06 05:37:09 +0200

    Solution 91 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/n8tffrp7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/17183931-6939-48ac-a347-052649a38 c1b.py", line 1     -lst = [int(x) for x in input("Enter numbers separated by spaces: ").split() ]     ^^^^ SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of ' ='?

--- Python_151.py
+++ Python_151.py
@@ -1,1 +1,8 @@
-lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+import sys
+def get_input(prompt):
+    if sys.version_info[0] == 3:
+        return input(prompt)
+    else:
+        return raw_input(prompt)
+lst = list(map(int, get_input("Enter numbers separated by spaces: ").split()))
