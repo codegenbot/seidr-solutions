cfb6cdd9b089644a4816e42f4f587199b4455135
beep boop <githubbot@vadim.me>
2024-07-06 18:58:07 +0200

    Solution 45 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/sef7a86m Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0ae87710-604d-4622-8db5-b0f30be9d824.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0ae87710-604d-4622-8db5-b0f30be9d824.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0ae87710-604d-4622-8db5-b0f30be9d824.cpp:10: 0ae87710-604d-4622-8db5-b0f30be9d824.cpp: In function ‘int main()’: 0ae87710-604d-4622-8db5-b0f30be9d824.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 0ae87710-604d-4622-8db5-b0f30be9d824.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -2,12 +2,10 @@
 #include <string>
 
 bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    return (a.size() == b.size()) && all_of(a.begin(), a.end(), [&](const auto& x) { 
-        return find(x, b[0]) != string::npos; });
+    return a == b;
 }
 
 int main() {
-    vector<string> output = filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run");
-    bool same = issame(output, {"grunt", "prune"});
+    assert(issame({filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run")}, {"grunt", "prune"} ) );
     return 0;
 }
