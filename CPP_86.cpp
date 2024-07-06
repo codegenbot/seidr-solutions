8c3a73935792faeb689649f452bf0bc85a683798
beep boop <githubbot@vadim.me>
2024-07-06 08:41:26 +0200

    Solution 84 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fhauqmuk Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ ae7c68af-cc8d-4a49-9fe9-b0ba756f369a.cpp:15:21:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,36 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
-
-vector<string> split(const string& str, const string& delimiter) {
-    size_t pos = 0;
-    vector<string> tokens;
-    while ((pos = str.find(delimiter)) != string::npos) {
-        if (pos > 0) { 
-            tokens.push_back(str.substr(0, pos));
-        }
-        str.erase(0, pos + delimiter.length());
-    }
-    tokens.push_back(str);
-    return tokens;
-}
-
+```cpp
 string anti_shuffle(string s){
     if(s.empty())
         return "";
+    
     string result = "";
     for (const auto& word : split(s, " ")) {
         result += string(word.begin(), word.end()) + " ";
     }
     return result.substr(0, result.size() - 1);
-}
-
-int main() {
-    string s;
-    cout << "Enter a sentence: ";
-    getline(cin, s);
-
-    cout << "Anti-Shuffle of input sentence is: " << anti_shuffle(s) << endl;
 }
