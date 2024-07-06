185664ffef7bc1d270f5e6802f107877a384afbe
beep boop <githubbot@vadim.me>
2024-07-06 16:51:15 +0200

    Solution 99 of Python/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9bklotpg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/57ffd9e6-ebae-4326-8e7a-0ac5cf853 060.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_7.py
+++ Python_7.py
@@ -1,14 +1,15 @@
-```
 def filter_by_substring(input_string):
-    substring = input("Enter a substring: ")
+    substring = "substring"
     if substring in input_string:
         return "The string contains the substring."
     else:
         return "The string does not contain the substring."
 
+
 def check(func, input_string):
     result = func(input_string)
     print(result)
+
 
 input_string = input("Enter a string: ")
 check(filter_by_substring, input_string)
