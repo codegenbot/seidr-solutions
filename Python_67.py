b26f37778212f32de39151e053200629c32ee694
beep boop <githubbot@vadim.me>
2024-07-06 06:28:28 +0200

    Solution 14 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/kzgckf1i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/21b4b504-0236-4cb2-8de6-a674f7462 3dc.py", line 23, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/21b4b504-0236-4cb2-8de6-a674f7462 3dc.py", line 20, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/21b4b504-0236-4cb2-8de6-a674f7462 3dc.py", line 8, in fruit_distribution     and int(word.replace(" apples", "").replace("apple", "")) < n         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: 's'

--- Python_67.py
+++ Python_67.py
@@ -1,11 +1,16 @@
-```
 def fruit_distribution(s, n):
     s = s.lower()
     words = s.split()
     count = 0
     for word in words:
-        if ("apples" in word and int(word.replace(" apples", "").replace("apple", "")) < n):
+        if (
+            "apples" in word
+            and int(word.replace(" apples", "").replace("apple", "")) < n
+        ):
             count += int(word.replace(" apples", "").replace("apple", ""))
-        elif ("oranges" in word and int(word.replace(" oranges", "").replace("orange", "")) < n):
+        elif (
+            "oranges" in word
+            and int(word.replace(" oranges", "").replace("orange", "")) < n
+        ):
             count += int(word.replace(" oranges", "").replace("orange", ""))
     return n - count
