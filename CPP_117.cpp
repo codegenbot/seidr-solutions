54100b5e9ebceab9d11be3b29184eae243cbd02e
beep boop <githubbot@vadim.me>
2024-07-06 08:59:52 +0200

    Solution 46 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ssvdmxr6 Prompt: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 139dea46-ce51-4943-af6d-855b09fdf8ba.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 139dea46-ce51-4943-af6d-855b09fdf8ba.cpp:19:44: error: ‘b’ was not declared in t his scope    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 139dea46-ce51-4943-af6d-855b09fdf8ba.cpp:19:45: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 139dea46-ce51-4943-af6d-855b09fdf8ba.cpp:18: 139dea46-ce51-4943-af6d-855b09fdf8ba.cpp: In function ‘int main()’: 139dea46-ce51-4943-af6d-855b09fdf8ba.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,41 +1,22 @@
+```cpp
 #include <vector>
 #include <string>
 #include <sstream>
-#include <cctype>
+
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+    return a == b;
+}
 
 std::vector<std::string> select_words(const std::string& str, int num) {
     std::vector<std::string> words;
-    std::stringstream s(str);
-    std::string word;
-
-    for(int i=0; i<num && s >> word; ++i)
-        words.push_back(word);
-
+    std::stringstream ss(str);
+    for(int i = 0; i < num; ++i){
+        std::getline(ss, ' ');
+        while (ss.peek() == ' ')
+            ss.ignore();
+        words.push_back(std::string());
+        words.back() = ss.str();
+        ss >> std::skipws;
+    }
     return words;
-}
-
-bool issame(char ch1, char ch2) {
-    if (isalpha(ch1) && isalpha(ch2)) {
-        return tolower(ch1) == tolower(ch2);
-    } else {
-        return false;
-    }
-}
-
-bool aresame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (!issame(a[i][0], b[i][0])) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int main() {
-    std::vector<std::string> b;
-    assert(aresame(select_words("a b c d e f", 4), b));
-    return 0;
 }
