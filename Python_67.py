72b772f6c549b4c35b9ce0fb1fd1bb50cfe96cc7
beep boop <githubbot@vadim.me>
2024-07-06 17:49:28 +0200

    Solution 13 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/rmvd3wj8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cd5b43a5-3dc6-4044-a260-44210299e 60a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,6 +1,7 @@
-```
+```Python
 import re
+
 def fruit_distribution(s, n):
     s = s.replace("apples", "0").replace("oranges", "0")
     parts = list(map(int, re.findall(r"\d+", s)))
-    return n - sum(parts)
+    return sum(parts) if len(parts) >= n else 0
