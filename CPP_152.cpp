6cff5fddab8c8c2e7abc50f56a165aafa7244634
beep boop <githubbot@vadim.me>
2024-07-06 23:38:27 +0200

    Solution 89 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/uttttzw3 Prompt:       |                    ^~~ 950d29cc-3d64-425e-8adb-bc92fc7aa5b9.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 950d29cc-3d64-425e-8adb-bc92fc7aa5b9.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 950d29cc-3d64-425e-8adb-bc92fc7aa5b9.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 950d29cc-3d64-425e-8adb-bc92fc7aa5b9.cpp:15: 950d29cc-3d64-425e-8adb-bc92fc7aa5b9.cpp: In function ‘int main()’: 950d29cc-3d64-425e-8adb-bc92fc7aa5b9.cpp:25:20: error: ‘compare’ was not declare d in this scope    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 950d29cc-3d64-425e-8adb-bc92fc7aa5b9.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -8,12 +8,14 @@ bool issame(vector<int> a,vector<int>b){
 vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
     for (int i = 0; i < game.size(); i++) {
-        if(issame({game[i],guess[i]},{{game[i],guess[i]}))){
-            result.push_back(2);
-        } else if(game[i] == guess[i]){
-            result.push_back(1);
-        } else {
+        if (issame({game[i]}, {guess[i]})) {
             result.push_back(0);
+        } else {
+            int diff = abs(guess[i] - game[i]);
+            if (diff > 9) {
+                diff = 10;
+            }
+            result.push_back(diff);
         }
     }
     return result;
