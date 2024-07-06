4ddaf7cf7271367749db6fccc99b13c0413786b3
beep boop <githubbot@vadim.me>
2024-07-06 02:47:52 +0200

    Solution 50 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/8987x0o8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/41157940-a92f-46a1-8474-b7aa82b89 8e2.py", line 24, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/41157940-a92f-46a1-8474-b7aa82b89 8e2.py", line 21, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/41157940-a92f-46a1-8474-b7aa82b89 8e2.py", line 18, in separate_paren_groups     result.append("".join(stack))                   ^^^^^^^^^^^^^^ TypeError: sequence item 0: expected str instance, list found

--- Python_1.py
+++ Python_1.py
@@ -1,18 +1,18 @@
-def separate_paren_groups(paren_string):
-result = []
-stack = []
-for char in paren_string:
-if char == "(":
-stack.append([])
-elif char == ")":
-if len(stack) > 0:
-stack[-1].append(char)
-else:
-result.append("".join(stack))
-stack = []
-else:
-if len(stack) > 0:
-stack[-1].append(char)
-if len(stack) > 0:
-result.append("".join(stack))
-return result
+def separate_paren_groups(paren_string: str) -> list[str]:
+    result = []
+    stack = []
+    for char in paren_string:
+        if char == "(":
+            stack.append([])
+        elif char == ")":
+            if len(stack) > 0:
+                stack[-1].append(char)
+            else:
+                result.append("".join(stack))
+                stack = []
+        else:
+            if len(stack) > 0:
+                stack[-1].append(char)
+    if len(stack) > 0:
+        result.append("".join(stack))
+    return result
