2694b3e4c9fb00ba0e2f709bbe9add0482391e98
beep boop <githubbot@vadim.me>
2024-07-06 19:52:47 +0200

    Solution 90 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/26zr7o11 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/38ade8d9-0591-4555-a163-7f6da4823 93e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -1,2 +1,5 @@
+```
 def encode(text, mapping):
-    return "".join(mapping[i] if i.isalpha() and i.lower() in mapping else i for i in text)
+    return "".join(mapping[i].lower() if i.isalpha() and i.lower() in mapping else i for i in text)
+
+print(encode("I DoNt KnOw WhAt tO WrItE", "k dQnT kNqW wHcT Tq wRkTg"))
