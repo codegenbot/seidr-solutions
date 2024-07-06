3d799488a67093f44dcf6401ee21ce1dd18891ee
beep boop <githubbot@vadim.me>
2024-07-06 20:06:39 +0200

    Solution 28 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/u2o18bue Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/671ca9cc-c76b-4a88-b39a-67acd1510 6f4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,13 +1,11 @@
+def double_the_difference(lst):
+    return [(x - min(lst)) * 2 + min(lst) for x in lst]
+
 def check(odd_sum):
-    lst = [1, 3, 5]
+    lst = [1,3,5]
     if double_the_difference(lst) == odd_sum:
         print("True")
     else:
         print("False")
-
-
-def double_the_difference(lst):
-    return [(x - min(lst)) * 2 + min(lst) for x in lst]
-
 
 check(9)
