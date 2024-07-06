e38743cc273082976d5cc8ff4216b9485803db2f
beep boop <githubbot@vadim.me>
2024-07-06 07:32:22 +0200

    Solution 76 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4eurhjdf Prompt: Program did not receive expected input

--- Python_22.py
+++ Python_22.py
@@ -1,5 +1,4 @@
-from typing import List
-import typing
+from typing import Any
 
-def filter_integers(values: List[typing.Any]) -> List[int]:
+def filter_integers(values: List[Any]) -> List[int]:
     return [value for value in values if isinstance(value, int)]
