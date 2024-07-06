7e7d210c1130b5e24fb6519ec896f3695744f245
beep boop <githubbot@vadim.me>
2024-07-06 12:40:51 +0200

    Solution 88 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/t0yv1tzr Prompt:    20 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8caca2bf-4443-446f-aa92-ebf32aec4722.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8caca2bf-4443-446f-aa92-ebf32aec4722.cpp:20:45: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8caca2bf-4443-446f-aa92-ebf32aec4722.cpp:19: 8caca2bf-4443-446f-aa92-ebf32aec4722.cpp: In function ‘int main()’: 8caca2bf-4443-446f-aa92-ebf32aec4722.cpp:29:20: error: ‘filter_by_prefix’ was no t declared in this scope    29 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 8caca2bf-4443-446f-aa92-ebf32aec4722.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,9 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
-vector<string> b;
-
+```
 bool issame(vector<string> a, vector<string> b) {
     if(a.size() != b.size()) return false;
     for(int i = 0; i < a.size(); ++i) {
@@ -21,8 +16,27 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
     return result;
 
 int main() {
-    vector<string> strings = {"xxx", "asd", "xxy", "john doe", "xxxA", "xxx"};
-    b = filter_by_prefix(strings, "xxx"); 
-    assert (issame(b , {"xxx", "xxxAAA", "xxx"}));
+    int n1, n2;
+    vector<string> strings1, strings2;
+    cin >> n1 >> n2;
+    for(int i = 0; i < n1; ++i) {
+        string s; 
+        cin >> s;
+        strings1.push_back(s);
+    }
+    for(int i = 0; i < n2; ++i) {
+        string s; 
+        cin >> s;
+        strings2.push_back(s);
+    }
+
+    if(!issame(strings1, strings2)) cout << "NO\n";
+    else{
+        vector<string> filtered_strings = filter_by_prefix(strings1, "abc");
+        for(string s : filtered_strings){
+            cout << s << endl;
+        }
+    }
+    
     return 0;
 }
