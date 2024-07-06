7ab46375d08eadead9387a9cc44e12756c35fe93
beep boop <githubbot@vadim.me>
2024-07-06 10:13:50 +0200

    Solution 70 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7zdlse8d Prompt: 407c3fe9-41a7-439a-9c53-be3be68b7afe.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 407c3fe9-41a7-439a-9c53-be3be68b7afe.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 407c3fe9-41a7-439a-9c53-be3be68b7afe.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 407c3fe9-41a7-439a-9c53-be3be68b7afe.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ 407c3fe9-41a7-439a-9c53-be3be68b7afe.cpp:18:5: error: redefinition of ‘int main( )’    18 | int main(){       |     ^~~~ 407c3fe9-41a7-439a-9c53-be3be68b7afe.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,3 +1,4 @@
+```
 #include <cassert>
 
 bool any_int(float a, float b, float c) {
@@ -11,3 +12,4 @@ bool any_int(float a, float b, float c) {
 int main() {
     assert(any_int(3.01, 4, 7) == false);
     return 0;
+}
