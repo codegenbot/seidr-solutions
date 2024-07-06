efd53867e2dcf8d2d9f0a7e94ff0baef7c8d09d2
beep boop <githubbot@vadim.me>
2024-07-06 03:42:34 +0200

    Solution 8 of Python/59, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/l40a9n71 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e6b89f6-1218-449e-a2b3-aaf335722 c3b.py", line 19, in <module>     check(largest_prime_factor)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e6b89f6-1218-449e-a2b3-aaf335722 c3b.py", line 16, in check     assert largest_prime_factor(13195) == 29            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_59.py
+++ Python_59.py
@@ -1,5 +1,7 @@
 def largest_prime_factor(n):
-    for i in range(2, n + 1):
+    if n <= 1:
+        return 1
+    for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0 and is_prime(i):
             return i
     return 1
