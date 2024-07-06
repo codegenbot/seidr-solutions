808ba20d95f44d936b8a218d3f66d4166d24e9ff
beep boop <githubbot@vadim.me>
2024-07-06 18:00:55 +0200

    Solution 20 of CPP/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gmdx32nh Prompt: 1b0254ea-ae95-4dc5-92ce-37cfa7f8f9e2.cpp:1:18: error: ‘vector’ was not declared in this scope     1 | int add_elements(vector<int> arr,int k){       |                  ^~~~~~ 1b0254ea-ae95-4dc5-92ce-37cfa7f8f9e2.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | int add_elements(vector<int> arr,int k){       |                         ^~~ 1b0254ea-ae95-4dc5-92ce-37cfa7f8f9e2.cpp:1:34: error: expected primary-expressio n before ‘int’     1 | int add_elements(vector<int> arr,int k){       |                                  ^~~ 1b0254ea-ae95-4dc5-92ce-37cfa7f8f9e2.cpp:1:39: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int add_elements(vector<int> arr,int k){       |                                       ^ In file included from 1b0254ea-ae95-4dc5-92ce-37cfa7f8f9e2.cpp:11: 1b0254ea-ae95-4dc5-92ce-37cfa7f8f9e2.cpp: In function ‘int main()’: 1b0254ea-ae95-4dc5-92ce-37cfa7f8f9e2.cpp:13:25: error: ‘add_elements’ cannot be used as a function    13 |     assert (add_elements({1}, 1) == 1);       |             ~~~~~~~~~~~~^~~~~~~~

--- CPP_122.cpp
+++ CPP_122.cpp
@@ -1,11 +1,20 @@
 #include <vector>
+#include <cmath>
 
-int add_elements(std::vector<int> arr, int k) {
+int add_elements(std::vector<int> arr, int k){
     int sum = 0;
-    for (int i = 0; i < k; i++) {
-        if (to_string(arr[i]).size() <= 2) {
+    for(int i=0; i<k; i++){
+        if(log10(arr[i]) + 1 <= 2) {
             sum += arr[i];
         }
     }
     return sum;
+
+}
+
+int main() {
+    std::vector<int> input = {1}; 
+    int k = input.size(); 
+    assert (add_elements(input, k) == 1); 
+    return 0;
 }
