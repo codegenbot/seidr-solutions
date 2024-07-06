b0a48dc0ec27b3692378fd144ec1803984680121
beep boop <githubbot@vadim.me>
2024-07-06 18:00:13 +0200

    Solution 70 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/bbi8kb69 Prompt:    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b951dec8-6152-45c0-905e-3d785cd8b35e.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b951dec8-6152-45c0-905e-3d785cd8b35e.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b951dec8-6152-45c0-905e-3d785cd8b35e.cpp:22: b951dec8-6152-45c0-905e-3d785cd8b35e.cpp: In function ‘int main()’: b951dec8-6152-45c0-905e-3d785cd8b35e.cpp:32:20: error: ‘count_up_to’ was not dec lared in this scope    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ b951dec8-6152-45c0-905e-3d785cd8b35e.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isEqualVector(vector<int> v1, vector<int> v2) {
+    if (v1.size() != v2.size())
+        return false;
+    for (int i = 0; i < v1.size(); ++i) {
+        if (v1[i] != v2[i])
+            return false;
+    }
+    return true;
+}
+
 vector<int> count_up_to(int n) {
     vector<int> primes;
     for (int i = 2; i <= n; ++i) {
@@ -17,4 +31,9 @@ vector<int> count_up_to(int n) {
         }
     }
     return primes;
+}
+
+int main() {
+    assert(isEqualVector(count_up_to(101), vector<int>({2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97})));
+    return 0;
 }
