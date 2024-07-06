f7d6e9cf51f95c27217798212a40c50502b6f47f
beep boop <githubbot@vadim.me>
2024-07-06 15:15:47 +0200

    Solution 28 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/491glfzb Prompt:    21 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ e2fd95b4-dbe2-40ae-aa16-b1898ffe740d.cpp:21:36: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ e2fd95b4-dbe2-40ae-aa16-b1898ffe740d.cpp:21:43: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from e2fd95b4-dbe2-40ae-aa16-b1898ffe740d.cpp:20: e2fd95b4-dbe2-40ae-aa16-b1898ffe740d.cpp: In function ‘int main()’: e2fd95b4-dbe2-40ae-aa16-b1898ffe740d.cpp:30:20: error: ‘sort_even’ was not decla red in this scope    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ e2fd95b4-dbe2-40ae-aa16-b1898ffe740d.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,22 +1,23 @@
-#include <vector>
-#include <algorithm>
-
+```cpp
 bool issame(vector<float> a, vector<float> b) {
-    return a == b;
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i)
+        if (abs(a[i] - b[i]) > 1e-6f) return false;
+    return true;
 }
 
-vector<float> sort_even(vector<float> l){
+vector<float> sort_even(vector<float> l) {
     vector<float> result(l.size());
-    for(int i = 0; i < l.size(); i++){
-        if(i % 2 == 0){
-            vector<float> even_indices;
-            for(int j = 0; j < l.size(); j++){
-                if(j % 2 == 0){
-                    even_indices.push_back(l[j]);
+    for (int i = 0; i < l.size(); ++i) {
+        if (i % 2 == 0) {
+            vector<float> evenVals;
+            for (float x : l) {
+                if (floor(x) == ceil(x)) { // checking if the number is even
+                    evenVals.push_back(x);
                 }
             }
-            sort(even_indices.begin(), even_indices.end());
-            result[i] = even_indices[0];
+            sort(evenVals.begin(), evenVals.end());
+            result[i] = evenVals[0];
         } else {
             result[i] = l[i];
         }
