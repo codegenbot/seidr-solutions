8def4784534ff0d52383d0c6abd4899c78dca8df
beep boop <githubbot@vadim.me>
2024-07-06 02:56:08 +0200

    Solution 50 of Python/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/hy53hw4z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a9dbaeb4-6f7b-48c2-926d-d396c82b6 012.py", line 21, in <module>     check(select_words)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a9dbaeb4-6f7b-48c2-926d-d396c82b6 012.py", line 13, in check     assert select_words("a b c d e f", 1) == [            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: 2nd edge test error: ['a', 'b', 'c', 'd', 'e', 'f']

--- Python_117.py
+++ Python_117.py
@@ -2,9 +2,9 @@ def select_words(s, n):
     words = s.split()
     if n <= 0:
         return []
-    elif n == 0:
-        return words
+    elif n == 1:
+        return list(filter(lambda x: x.islower(), words))
     else:
-        return [
-            word[1:] for word in words if len([c for c in word[1:] if c.islower()]) == n
-        ]
+        return list(
+            filter(lambda x: len([c for c in x[1:] if c.islower()]) == n, words)
+        )
