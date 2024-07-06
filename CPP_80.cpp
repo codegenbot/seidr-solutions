521f2b5309c9e3ed10076e4d3feb0dddd1d7577c
beep boop <githubbot@vadim.me>
2024-07-06 15:21:09 +0200

    Solution 20 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yl3wmkze Prompt: 8eb7e691-0788-4271-9ae6-a097bee0b63d.cpp:1:15: error: ‘string’ was not declared in this scope     1 | bool is_happy(string s) {       |               ^~~~~~ In file included from 8eb7e691-0788-4271-9ae6-a097bee0b63d.cpp:19: 8eb7e691-0788-4271-9ae6-a097bee0b63d.cpp: In function ‘int main()’: 8eb7e691-0788-4271-9ae6-a097bee0b63d.cpp:21:21: error: ‘is_happy’ cannot be used  as a function    21 |     assert (is_happy("iopaxioi") == false );       |             ~~~~~~~~^~~~~~~~~~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,26 +1,20 @@
 #include <string>
+using namespace std;
 
 bool is_happy(string s) {
-    if (s.length() < 3)
+    int n = s.length();
+    if(n < 3)
         return false;
-
-    for (int i = 0; i <= s.length() - 3; i++) {
-        string substr = s.substr(i, 3);
-        bool distinct = true;
-        for (char c : substr) {
-            int count = 0;
-            for (int j = 0; j < s.length(); j++) {
-                if (s[j] == c)
-                    count++;
-            }
-            if (count > 1) {
-                distinct = false;
+    for(int i=0; i<=n-3; i++){
+        bool unique = true;
+        string sub = s.substr(i, 3);
+        for(char c : sub){
+            if(find(sub.begin(), sub.end(), c) == sub.end())
+                unique = false;
                 break;
-            }
         }
-        if (!distinct)
-            return false;
+        if(unique)
+            return true;
     }
-
-    return true;
+    return false;
 }
