6201f6918b8994314e302f3b9d9de4472a2dafb2
beep boop <githubbot@vadim.me>
2024-07-06 08:29:07 +0200

    Solution 8 of Python/98, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nn6k73rq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/50208889-b40b-4491-b951-a306c93cf 70f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_98.py
+++ Python_98.py
@@ -1,6 +1,2 @@
-'''
-
 def count_upper(s):
-    return sum(1 for i, c in enumerate(s.upper()) if 'A' <= c <= 'E')
-
-'''
+    return sum(1 for i, c in enumerate(s) if 'A' <= c <= 'E' and i % 2 == 0)
