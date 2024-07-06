eb84b1690dcf9681564baccc8ed2cfd9fdee0719
beep boop <githubbot@vadim.me>
2024-07-06 19:45:47 +0200

    Solution 77 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/26zr7o11 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e7ab7e6f-039d-4a93-9c43-dc18ed6de d80.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -1,4 +1,3 @@
-```
 def encode(text, table):
     result = ""
     shifts = [int(x) for x in table.split() if x.isdigit()]
@@ -6,7 +5,7 @@ def encode(text, table):
     for char in text:
         if char.isalpha():
             ascii_val = ord(char) - 97
-            new_ascii_val = (ascii_val + int(shifts[0 % len(shifts)])) % 26
+            new_ascii_val = (ascii_val + shifts[0 % len(shifts)]) % 26
             result += chr(new_ascii_val + 97)
         else:
             result += char
