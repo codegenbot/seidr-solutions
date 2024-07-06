836aec248d12acec5665a2918dc2f149c21300ce
beep boop <githubbot@vadim.me>
2024-07-06 02:57:40 +0200

    Solution 61 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/a1rky2qb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6f2f5578-c602-4a7a-a04d-512fd7fdd aef.py", line 16, in <module>     check(parse_nested_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6f2f5578-c602-4a7a-a04d-512fd7fdd aef.py", line 13, in check     assert parse_nested_parens("(()(())((())))") == [4]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_6.py
+++ Python_6.py
@@ -1,7 +1,5 @@
-import itertools
 
-
-def parse_nested_parens(paren_string: str) -> list[int]:
+def parse_nested_parens(paren_string):
     result = []
     for group in paren_string.split():
         level = 0
