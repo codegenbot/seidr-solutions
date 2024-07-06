9c3d413849eb4a4158244803b03cc61fefa449e5
beep boop <githubbot@vadim.me>
2024-07-06 02:25:45 +0200

    Solution 22 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/uxnjd0wp Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a33d56f4-bf79-4b21-8a29-4d9d21b78 3c4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,13 +1,6 @@
 ```
 def add(x: int, y: int):
-    while x > y:
-        print(f"{x} is greater than {y}")
-        z = input("Press Enter to continue: ")
-        if z == "q":
-            break
-        x -= 1
-    else:
-        print(f"{x} is not greater than {y}")
+    return x + y
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
