4d6e9524885a107d4186e0e0be013db3c45057ef
beep boop <githubbot@vadim.me>
2024-07-06 08:19:38 +0200

    Solution 17 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/a1tfr568 Prompt: In file included from /usr/include/c++/11/vector:67,                  from e703d319-a9b5-4316-88a6-15b79756e27a.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from e703d319-a9b5-4316-88a6-15b79756e27a.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e703d319-a9b5-4316-88a6-15b79756e27a.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e703d319-a9b5-4316-88a6-15b79756e27a.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e703d319-a9b5-4316-88a6-15b79756e27a.cpp:14: e703d319-a9b5-4316-88a6-15b79756e27a.cpp: In function ‘int main()’: e703d319-a9b5-4316-88a6-15b79756e27a.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,15 +1,6 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> maximum(std::vector<int> arr, int k) {
-    std::vector<int> result;
-    for (int i = 0; i < k; ++i) {
-        int max_val = *std::max_element(arr.begin(), arr.end());
-        result.push_back(max_val);
-        arr.erase(std::remove(arr.begin(), arr.end(), max_val), arr.end());
-    }
-    return result;
-
 bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size())
         return false;
@@ -18,6 +9,17 @@ bool issame(std::vector<int> a, std::vector<int> b) {
             return false;
     }
     return true;
+}
+
+std::vector<int> maximum(std::vector<int> arr, int k) {
+    std::vector<int> result;
+    while (!issame(result, arr)) {
+        int max_val = *std::max_element(arr.begin(), arr.end());
+        result.push_back(max_val);
+        arr.erase(std::remove(arr.begin(), arr.end(), max_val), arr.end());
+    }
+    return result;
+}
 
 int main() {
     assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 4), {})); 
