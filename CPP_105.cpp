f2131d1c2c3d6373b107a805295416828fa53ac4
beep boop <githubbot@vadim.me>
2024-07-06 21:26:52 +0200

    Solution 33 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/za61nfn8 Prompt:       |                              ^~~~~~ fe5cde08-ad3d-4361-b683-9c566a9ddbeb.cpp:48:37: error: ‘string’ was not declared  in this scope    48 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ fe5cde08-ad3d-4361-b683-9c566a9ddbeb.cpp:48:44: error: ‘b’ was not declared in t his scope    48 | bool issame(vector<string> a,vector<string>b){       |                                            ^ fe5cde08-ad3d-4361-b683-9c566a9ddbeb.cpp:48:45: error: expression list treated a s compound expression in initializer [-fpermissive]    48 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from fe5cde08-ad3d-4361-b683-9c566a9ddbeb.cpp:47: fe5cde08-ad3d-4361-b683-9c566a9ddbeb.cpp: In function ‘int main()’: fe5cde08-ad3d-4361-b683-9c566a9ddbeb.cpp:57:20: error: ‘by_length’ was not decla red in this scope    57 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ fe5cde08-ad3d-4361-b683-9c566a9ddbeb.cpp:57:19: error: ‘issame’ cannot be used a s a function    57 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -3,193 +3,69 @@
 #include <algorithm>
 
 bool issame(vector<string>, vector<string>) {
-    // This function is not used anywhere in your code.
-    // It seems like it's supposed to check if two vectors are the same.
-    return true;
+    // implementation
 }
 
 int main() {
+    std::vector<int> arr;
     int n;
-    std::cout << "Enter number of elements: ";
+    std::cout << "Enter the number of elements: ";
     std::cin >> n;
-
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
+        int num;
         std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> arr[i];
+        std::cin >> num;
+        arr.push_back(num);
     }
-
-    vector<string> by_length(vector<int> arr) {
-        vector<int> temp;
-        for (int i : arr) {
-            if (i >= 10 && i <= 99) {
-                int tens = i / 10;
-                int ones = i % 10;
-                switch (tens) {
-                    case 1:
-                        str = "Ten";
-                        break;
-                    case 2:
-                        str = "Twenty";
-                        break;
-                    case 3:
-                        str = "Thirty";
-                        break;
-                    case 4:
-                        str = "Forty";
-                        break;
-                    case 5:
-                        str = "Fifty";
-                        break;
-                    case 6:
-                        str = "Sixty";
-                        break;
-                    case 7:
-                        str = "Seventy";
-                        break;
-                    case 8:
-                        str = "Eighty";
-                        break;
-                    case 9:
-                        str = "Ninety";
-                        break;
-                }
-                switch (ones) {
-                    case 1:
-                        str += " One";
-                        break;
-                    case 2:
-                        str += " Two";
-                        break;
-                    case 3:
-                        str += " Three";
-                        break;
-                    case 4:
-                        str += " Four";
-                        break;
-                    case 5:
-                        str += " Five";
-                        break;
-                    case 6:
-                        str += " Six";
-                        break;
-                    case 7:
-                        str += " Seven";
-                        break;
-                    case 8:
-                        str += " Eight";
-                        break;
-                    case 9:
-                        str += " Nine";
-                        break;
-                }
-            } else if (i >= 1 && i <= 9) {
-                switch (i) {
-                    case 1:
-                        str = "One";
-                        break;
-                    case 2:
-                        str = "Two";
-                        break;
-                    case 3:
-                        str = "Three";
-                        break;
-                    case 4:
-                        str = "Four";
-                        break;
-                    case 5:
-                        str = "Five";
-                        break;
-                    case 6:
-                        str = "Six";
-                        break;
-                    case 7:
-                        str = "Seven";
-                        break;
-                    case 8:
-                        str = "Eight";
-                        break;
-                    case 9:
-                        str = "Nine";
-                        break;
-                }
-            } else if (i == 0) {
-                str = "Zero";
-            } else if (i >= 10 && i <= 99) {
-                int tens = i / 10;
-                int ones = i % 10;
-                switch (tens) {
-                    case 1:
-                        str = "Ten";
-                        break;
-                    case 2:
-                        str = "Twenty";
-                        break;
-                    case 3:
-                        str = "Thirty";
-                        break;
-                    case 4:
-                        str = "Forty";
-                        break;
-                    case 5:
-                        str = "Fifty";
-                        break;
-                    case 6:
-                        str = "Sixty";
-                        break;
-                    case 7:
-                        str = "Seventy";
-                        break;
-                    case 8:
-                        str = "Eighty";
-                        break;
-                    case 9:
-                        str = "Ninety";
-                        break;
-                }
-                switch (ones) {
-                    case 1:
-                        str += " One";
-                        break;
-                    case 2:
-                        str += " Two";
-                        break;
-                    case 3:
-                        str += " Three";
-                        break;
-                    case 4:
-                        str += " Four";
-                        break;
-                    case 5:
-                        str += " Five";
-                        break;
-                    case 6:
-                        str += " Six";
-                        break;
-                    case 7:
-                        str += " Seven";
-                        break;
-                    case 8:
-                        str += " Eight";
-                        break;
-                    case 9:
-                        str += " Nine";
-                        break;
-                }
-            } else {
-                return "Invalid number";
-            }
-
-            result.push_back(str);
-        }
-        return result;
+    vector<string> result = by_length(arr);
+    for (string s : result) {
+        std::cout << s << " ";
     }
-
-    vector<string> output = by_length(arr);
-
-    for (string s : output) {
-        cout << s << endl;
-    }
-
     return 0;
+}
+
+vector<string> by_length(vector<int> arr) {
+    vector<int> temp;
+    for (int i : arr) {
+        if (i >= 1 && i <= 9) {
+            temp.push_back(i);
+        }
+    }
+    sort(temp.begin(), temp.end());
+    reverse(temp.begin(), temp.end());
+    vector<string> result;
+    for (int i : temp) {
+        string str = "";
+        switch (i) {
+            case 1:
+                str = "One";
+                break;
+            case 2:
+                str = "Two";
+                break;
+            case 3:
+                str = "Three";
+                break;
+            case 4:
+                str = "Four";
+                break;
+            case 5:
+                str = "Five";
+                break;
+            case 6:
+                str = "Six";
+                break;
+            case 7:
+                str = "Seven";
+                break;
+            case 8:
+                str = "Eight";
+                break;
+            case 9:
+                str = "Nine";
+                break;
+        }
+        result.push_back(str);
+    }
+    return result;
 }
