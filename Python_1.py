cf1a63942bc402a43c00254554860b46bba45c07
beep boop <githubbot@vadim.me>
2024-07-06 15:16:08 +0200

    Solution 51 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/48kvi39g Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d55ab3a9-0b55-43a7-b788-181c50089 02a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_1.py
+++ Python_1.py
@@ -4,14 +4,18 @@ stack = []
 group = ""
 result = []
 for char in paren_string:
-    if char == "(":
-        stack.append(char)
-        group += char
-    elif char == ")":
-        if not stack:
-            result.append(group)
-            group = ""
-        else:
+    if char == "(" or char == ")":
+        if char == "(":
+            stack.append(char)
+            group += char
+        elif char == ")":
             stack.pop()
             group += char
+            if not stack:
+                result.append(group)
+                group = ""
+    else:
+        group += char
+if stack:
+    result.append(group)
 print(result)
