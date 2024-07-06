29cbf664760e311baff065f4b8aef946f500e092
beep boop <githubbot@vadim.me>
2024-07-07 01:57:03 +0200

    Solution 20 of Python/125, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7ck8mnmj Prompt: Complete the following code given the task description and function signature.

--- Python_125.py
+++ Python_125.py
@@ -1,7 +1,8 @@
-```
 def split_words(txt):
     words = txt.split()
-    if len(words) > 0 and any(char.isalnum() for char in words[0]):
-        return words
+    if len(words) > 0 and all(char.isalnum() for char in words[0]):
+        return [words[0]]
     else:
-        return sum(1 for i in range(ord('a'), ord('z')+1) if txt.count(chr(i)) % 2 != 0)
+        return [word for word in words if "," in word] or sum(
+            1 for c in txt.lower() if ord(c) % 2 == 1
+        )
