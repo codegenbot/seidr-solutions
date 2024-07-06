f2b6103f33ada5faf098e9736452ef88b5534a05
beep boop <githubbot@vadim.me>
2024-07-06 07:12:08 +0200

    Solution 11 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/7x24i0jl Prompt: Complete the following code given the task description and function signature.

--- Python_89.py
+++ Python_89.py
@@ -1,6 +1,6 @@
 def encrypt(s):
-    result = ""
+    new_string = ""
     for char in s:
-        new_char = chr((ord(char) - 65) * 2 + 65)
-        result += new_char
-    return result
+        new_char = chr((ord(char) + 2) % 26)
+        new_string += new_char
+    return new_string
