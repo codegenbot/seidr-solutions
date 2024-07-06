dc167057b76618b7055089d447365c1ec14a7919
beep boop <githubbot@vadim.me>
2024-07-06 05:32:54 +0200

    Solution 90 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/n8tffrp7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/17183931-6939-48ac-a347-052649a38 c1b.py", line 1     -lst = [int(x) for x in input("Enter numbers separated by spaces: ").split() ]     ^^^^ SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of ' ='?

--- Python_151.py
+++ Python_151.py
@@ -1,17 +1,1 @@
-def double_the_difference(lst):
-    small = min(lst)
-    large = max(lst)
-    diff = large - small
-    return 2 * diff
-
-
-# Prompt user for input
-input_string = input("Enter numbers separated by spaces: ")
-try:
-    lst = [int(x) for x in input_string.split()]
-except ValueError:
-    print("Invalid input. Please enter a list of integers separated by spaces.")
-    return
-
-# Call function with input
-print(double_the_difference(lst))
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
