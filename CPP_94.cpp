27193823e7548775115dd35571e9b1638617d4db
beep boop <githubbot@vadim.me>
2024-07-06 09:09:22 +0200

    Solution 59 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ap5npprw Prompt: 2b21eb72-d466-4ccd-9070-df1824b5de0c.cpp:7:68: error: ‘calculateSumOfDigitsLarge stPrime’ was not declared in this scope     7 | ::cout << "The sum of digits for the largest prime is: " << calculateSum OfDigitsLargestPrime(lst) << std::endl;       |                                                             ^~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~ 2b21eb72-d466-4ccd-9070-df1824b5de0c.cpp:7:114: error: ‘endl’ is not a member of  ‘std’     7 | gest prime is: " << calculateSumOfDigitsLargestPrime(lst) << std::endl;       |                                                                   ^~~~ 2b21eb72-d466-4ccd-9070-df1824b5de0c.cpp:1:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?   +++ |+#include <ostream>     1 | int main_test() { In file included from 2b21eb72-d466-4ccd-9070-df1824b5de0c.cpp:11: 2b21eb72-d466-4ccd-9070-df1824b5de0c.cpp: In function ‘int main()’: 2b21eb72-d466-4ccd-9070-df1824b5de0c.cpp:13:13: error: ‘skjkasdkd’ was not decla red in this scope    13 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ^~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -2,34 +2,33 @@
 #include <iostream>
 #include <algorithm>
 
-int isPrime(int n) {
-    if (n <= 1)
-        return 0;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0)
-            return 0;
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
     }
-    return 1;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int calculateSumOfDigitsLargestPrime(std::vector<int> lst) {
-    int largestPrime = -1, sum = 0;
-
+    int maxNum = 0;
     for (int num : lst) {
-        for (int digit : {num}) {
-            if (isPrime(digit)) {
-                if (digit > largestPrime)
-                    largestPrime = digit;
-            }
+        if (num > maxNum) {
+            maxNum = num;
         }
     }
 
-    while (largestPrime > 0) {
-        sum += largestPrime % 10;
-        largestPrime /= 10;
+    int sumOfDigits = 0;
+    while (maxNum > 0) {
+        sumOfDigits += maxNum % 10;
+        maxNum /= 10;
     }
 
-    return sum;
+    return sumOfDigits;
 }
 
 int main_test() {
