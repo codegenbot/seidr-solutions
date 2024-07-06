34e9a5d6c7cdb93a6badfd4226e2bc5bcdc73564
beep boop <githubbot@vadim.me>
2024-07-06 21:22:37 +0200

    Solution 4 of CPP/113, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tb1og4b4 Prompt: Complete the following code given the task description and function signature.

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-vector<string> odd_count(vector<string> lst) {
+vector<string> odd_count(vector<string> lst){
     vector<string> result;
     for (int i = 0; i < lst.size(); i++) {
         int count = 0;
@@ -12,12 +12,12 @@ vector<string> odd_count(vector<string> lst) {
                 count++;
             }
         }
-        string s = "the number of odd elements ";
-        s += to_string(count);
-        s += " in the str";
-        s += to_string(i+1);
-        s += " of the input.";
-        result.push_back(s);
+        string temp = "the number of odd elements ";
+        temp += to_string(count);
+        temp += " in the str";
+        temp += to_string(i+1);
+        temp += " of the input.";
+        result.push_back(temp);
     }
     return result;
 }
