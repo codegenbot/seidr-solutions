037ff57b0da3b16bc741c8fb78ae95c65d08d539
beep boop <githubbot@vadim.me>
2024-07-06 03:03:10 +0200

    Solution 11 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3r9aab50 Prompt: std::__copy_move_backward<_IsMove, true, std::random_access_iterator_tag>::__cop y_move_b(const _Tp*, const _Tp*, _Tp*)’: /usr/include/c++/11/bits/stl_algobase.h:740:17: error: ‘ptrdiff_t’ does not name  a type   740 |           const ptrdiff_t _Num = __last - __first;       |                 ^~~~~~~~~ /usr/include/c++/11/bits/stl_algobase.h:740:17: note: ‘ptrdiff_t’ is defined in header ‘<cstddef>’; did you forget to ‘#include <cstddef>’? /usr/include/c++/11/bits/stl_algobase.h:741:15: error: ‘_Num’ was not declared i n this scope   741 |           if (_Num)       |               ^~~~ /usr/include/c++/11/bits/stl_algobase.h:743:29: error: ‘_Num’ was not declared i n this scope   743 |           return __result - _Num;       |                             ^~~~ /usr/include/c++/11/bits/stl_algobase.h: At global scope: /usr/include/c++/11/bits/stl_algobase.h:1349:16: error: ‘ptrdiff_t’ does not nam e a type  1349 |         static ptrdiff_t       |                ^~~~~~~~~ /usr/include/c++/11/bits/stl_algobase.h:1349:16: note: ‘ptrdiff_t’ is defined in  header ‘<cstddef>’; did you forget to ‘#include <cstddef>’?

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,10 +1,9 @@
 #include <cstddef>
-
 using namespace std;
 
 float triangle_area(float a, float b, float c) {
     if ((a + b > c) && (a + c > b) && (b + c > a))
-        return sqrt((c * (c-a)) * (c-b));
+        return sqrt(c * (c-a) * (c-b));
     else
         return -1.0f;
 }
