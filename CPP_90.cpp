408c524e8d23475cf2199fdcf7741c4ab2f3a416
beep boop <githubbot@vadim.me>
2024-07-06 16:34:46 +0200

    Solution 50 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/a8fzqhi0 Prompt: in this scope    17 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |     ^~~~~~ 5a52f277-d7a1-4b41-88bc-25d39bfb4069.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <algorithm>   +++ |+#include <cassert>     3 | 5a52f277-d7a1-4b41-88bc-25d39bfb4069.cpp: At global scope: 5a52f277-d7a1-4b41-88bc-25d39bfb4069.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 5a52f277-d7a1-4b41-88bc-25d39bfb4069.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 5a52f277-d7a1-4b41-88bc-25d39bfb4069.cpp:21: 5a52f277-d7a1-4b41-88bc-25d39bfb4069.cpp: In function ‘int main()’: 5a52f277-d7a1-4b41-88bc-25d39bfb4069.cpp:23:26: error: ‘next_smallest’ cannot be  used as a function    23 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -7,7 +7,7 @@ int next_smallest(vector<int> lst) {
     vector<int> sorted = lst;
     sort(sorted.begin(), sorted.end());
     for(int i=0; i<sorted.size()-1; i++) {
-        if(sorted[i] > sorted[i+1]) {
+        if(sorted[i] != sorted[i+1]) {
             return sorted[i];
         }
     }
