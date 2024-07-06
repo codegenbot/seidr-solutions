4046400adde42dbaec4fae81e7cfe441906bd88a
beep boop <githubbot@vadim.me>
2024-07-06 02:31:00 +0200

    Solution 27 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tkiemw9v Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2babf46b-baad-481e-b2b2-8adaeb165 747.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,8 +1,6 @@
 def solve(N):
-    result = ""
+    result = 0
     for i in str(bin(N)[2:]):
-        if i == "1":
-            result += "1"
-        else:
-            result += "0"
-    return result
+        if i == '1':
+            result += 2 ** (len(str(bin(N)[2:])) - 1)
+    return bin(result)[2:]
