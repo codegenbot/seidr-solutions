23040179e0db4e232ae815e5ca749366a6a2e452
beep boop <githubbot@vadim.me>
2024-07-06 10:04:57 +0200

    Solution 3 of Python/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ep7ps60o Prompt: Complete the following code given the task description and function signature.

--- Python_123.py
+++ Python_123.py
@@ -1,10 +1,11 @@
+
 def get_odd_collatz(n):
-    result = []
+    collatz = []
     while n > 1:
         if n % 2 == 0:
             n //= 2
         else:
             n = 3 * n + 1
-        if n % 2 == 1:
-            result.append(n)
-    return sorted(result)
+        if n not in collatz and n % 2 != 0:
+            collatz.append(n)
+    return sorted(collatz)
