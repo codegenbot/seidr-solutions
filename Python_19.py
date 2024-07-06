770c9cbaa0eda90e980be4438080d49458fdf582
beep boop <githubbot@vadim.me>
2024-07-06 06:44:49 +0200

    Solution 73 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4dw7k43c Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0f2e6895-85cb-4c71-9a8b-20590f6ec 8dc.py", line 9, in <module>     check(sort_numbers)           ^^^^^^^^^^^^ NameError: name 'sort_numbers' is not defined

--- Python_19.py
+++ Python_19.py
@@ -4,5 +4,5 @@ num_dict = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "si
 def sort_numbers(input_string):
     return sorted([num_dict[i] for i in input_string.split()])
 
-input_string = input("Enter a string of numbers (zero-nine) separated by spaces: ")
+input_string = input("Enter a string with numbers (zero-nine): ")
 print(sort_numbers(input_string))
