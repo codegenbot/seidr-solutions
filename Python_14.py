c6d12e53ef9d05888cfb2ed2cf425710a96984d2
beep boop <githubbot@vadim.me>
2024-07-06 10:48:12 +0200

    Solution 2 of Python/14, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/p60f2awn Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/789d92f4-3e39-46dc-a343-9863e4590 fbb.py", line 1, in <module>     def all_prefixes(string: str) -> List[str]:                                      ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_14.py
+++ Python_14.py
@@ -1,4 +1,4 @@
-def all_prefixes(string: str) -> List[str]:
+def all_prefixes(string: str) -> list[str]:
     prefixes = []
     for i in range(1, len(string) + 1):
         prefixes.append(string[:i])
