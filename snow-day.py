f2f78c83cf6fc0de89d5718f46ca7da1a2211a92
beep boop <githubbot@vadim.me>
2024-07-06 09:01:04 +0200

    Solution 41 of snow-day, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/yg65jg38 Prompt: it must return 17.509206714885256 for input 0 17.509206714885256 7.973435472779466 0.8439061849302216, but it returns .

--- snow-day.py
+++ snow-day.py
@@ -1,3 +1,7 @@
-
 def snow_day(hours, snow_on_ground, snow_fall_rate, melting_proportion):
-    return max(0, round((snow_on_ground + (hours * snow_fall_rate)) / (1 - melting_proportion), 1))
+    return max(
+        0,
+        round(
+            (snow_on_ground + (hours * snow_fall_rate)) / (1 - melting_proportion), 1
+        ),
+    )
