c9f617257ee15a1b5c201557671d4719cd36e908
beep boop <githubbot@vadim.me>
2024-07-06 03:07:10 +0200

    Solution 83 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/8987x0o8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9775fd08-7caf-467a-8f74-a4fc35bd2 e04.py", line 23, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9775fd08-7caf-467a-8f74-a4fc35bd2 e04.py", line 20, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9775fd08-7caf-467a-8f74-a4fc35bd2 e04.py", line 17, in separate_paren_groups     result.append("".join(stack))                   ^^^^^^^^^^^^^^ TypeError: sequence item 0: expected str instance, list found

--- Python_1.py
+++ Python_1.py
@@ -1,18 +1,18 @@
 def separate_paren_groups(paren_string):
-result = []
-stack = []
-for char in paren_string:
-if char == "(":
-stack.append([])
-elif char == ")":
-if len(stack) > 0:
-stack[-1].extend(char)
-else:
-result.append("".join(chain.from_iterable(stack)))
-stack = []
-else:
-if len(stack) > 0:
-stack[-1].append(char)
-if len(stack) > 0:
-result.append("".join(chain.from_iterable(stack)))
-return result
+    result = []
+    stack = []
+    for char in paren_string:
+        if char == "(":
+            stack.append([])
+        elif char == ")":
+            if len(stack) > 0:
+                stack[-1].extend(char)
+            else:
+                result.append("".join(chain.from_iterable(stack)))
+                stack = []
+        else:
+            if len(stack) > 0:
+                stack[-1].append(char)
+    if len(stack) > 0:
+        result.append("".join(chain.from_iterable(stack)))
+    return result
