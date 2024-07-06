4db8d48ba6b87e2760e75ba7927d47b0332b6e7c
beep boop <githubbot@vadim.me>
2024-07-06 20:33:29 +0200

    Solution 70 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7aum6t7i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3e41202a-0282-4264-b579-b44c3b464 e19.py", line 19, in <module>     main()   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3e41202a-0282-4264-b579-b44c3b464 e19.py", line 16, in main     print(sum_squares([{"x": [2]}, {"y": [4]}]))           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3e41202a-0282-4264-b579-b44c3b464 e19.py", line 5, in sum_squares     total_sum += sum_squares(list(element.values()))                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3e41202a-0282-4264-b579-b44c3b464 e19.py", line 9, in sum_squares     raise ValueError( ValueError: Invalid input '[2]' found. Input must contain only numbers.

--- Python_142.py
+++ Python_142.py
@@ -1,16 +1,14 @@
+```
 def sum_squares(lst):
     total_sum = 0
     for element in lst:
         if isinstance(element, dict):
-            total_sum += sum(
-                sum_squares(list(value)) for value in list(element.values())
-            )
+            for value in list(element.values()):
+                total_sum += sum((abs(x) ** 2 for x in value))
         elif isinstance(element, (int, float)):
             total_sum += abs(element) ** 2
         else:
-            raise ValueError(
-                f"Invalid input '{element}' found. Input must contain only numbers."
-            )
+            raise ValueError(f"Invalid input '{element}' found. Input must contain only numbers.")
     return total_sum
 
 
