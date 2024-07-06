acf91f0a9535455cf011ea825845f0ad65f0e0c1
beep boop <githubbot@vadim.me>
2024-07-06 10:25:48 +0200

    Solution 5 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pzni0fwe Prompt:       |                    ^~~ 5672ca64-e5d3-4c46-825c-0c6fa9f55033.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5672ca64-e5d3-4c46-825c-0c6fa9f55033.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5672ca64-e5d3-4c46-825c-0c6fa9f55033.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5672ca64-e5d3-4c46-825c-0c6fa9f55033.cpp:15: 5672ca64-e5d3-4c46-825c-0c6fa9f55033.cpp: In function ‘int main()’: 5672ca64-e5d3-4c46-825c-0c6fa9f55033.cpp:25:20: error: ‘compare’ was not declare d in this scope    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 5672ca64-e5d3-4c46-825c-0c6fa9f55033.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <algorithm>
 
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
@@ -9,5 +8,5 @@ int main() {
     assert(issame({1, 2, 3, 5}, {-1, 2, 3, 4}) == false);
     assert(issame({1, 2, 3, 5}, {1, 2, 3, 5}) == true);
     
-    // Your other code here...
+    return 0;
 }
