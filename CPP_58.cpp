c1369eddc0bf7c43bcd8bf9c48429e0ae0c14934
beep boop <githubbot@vadim.me>
2024-07-06 19:23:24 +0200

    Solution 16 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0bs7rxb5 Prompt:  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9b50b060-c281-472f-a62b-083706dc4b0a.cpp:17:27: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? 9b50b060-c281-472f-a62b-083706dc4b0a.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9b50b060-c281-472f-a62b-083706dc4b0a.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9b50b060-c281-472f-a62b-083706dc4b0a.cpp:16: 9b50b060-c281-472f-a62b-083706dc4b0a.cpp: In function ‘int main()’: 9b50b060-c281-472f-a62b-083706dc4b0a.cpp:26:20: error: ‘common’ was not declared  in this scope    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 9b50b060-c281-472f-a62b-083706dc4b0a.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,7 +1,16 @@
-set<int> s1(l1.begin(), l1.end());
-set<int> s2(l2.begin(), l2.end());
+#include <set>
+#include <vector>
 
-set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                 inplace_merge(s1.begin(), s1.end(), s1.end()));
-vector<int> res(s1.begin(), s1.end());
-return res;
+using namespace std;
+
+vector<int> common(vector<int> l1, vector<int> l2) {
+    set<int> s1(l1.begin(), l1.end());
+    set<int> s2(l2.begin(), l2.end());
+
+    set<int> intersection;
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                      inserter(intersection, intersection.begin()));
+
+    vector<int> result(intersection.begin(), intersection.end());
+    return result;
+}
