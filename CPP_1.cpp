9178ef0d5c1ea9755b4330217aa455b1615dc51d
beep boop <githubbot@vadim.me>
2024-07-06 02:46:51 +0200

    Solution 55 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gbyn86lg Prompt: In file included from 5376d8f6-f230-4bfc-9bbe-950e5a177d63.cpp:48: 5376d8f6-f230-4bfc-9bbe-950e5a177d63.cpp: In function ‘int main()’: 5376d8f6-f230-4bfc-9bbe-950e5a177d63.cpp:58:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    58 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~ 5376d8f6-f230-4bfc-9bbe-950e5a177d63.cpp:7:6: note: candidate: ‘bool issame(cons t std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx1 1::basic_string<char> >&)’     7 | bool issame(const vector<string>& a, const vector<string>& b) {       |      ^~~~~~ 5376d8f6-f230-4bfc-9bbe-950e5a177d63.cpp:49:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    49 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -18,17 +18,6 @@ bool issame(const vector<string>& a, const vector<string>& b) {
     return true;
 }
 
-vector<string> split(const string& s, const string& delimiter) {
-    vector<string> result;
-    size_t pos = 0, lastPos = 0;
-    while ((pos = s.find(delimiter, lastPos)) != std::string::npos) {
-        result.push_back(s.substr(lastPos, pos - lastPos));
-        lastPos = pos + delimiter.length();
-    }
-    result.push_back(s.substr(lastPos));
-    return result;
-}
-
 vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
     int open_count = 0;
@@ -51,9 +40,20 @@ vector<string> separate_paren_groups(string paren_string) {
     return result;
 }
 
+vector<string> split(const string& s, const string& delimiter) {
+    vector<string> result;
+    size_t pos = 0, lastPos = 0;
+    while ((pos = s.find(delimiter, lastPos)) != std::string::npos) {
+        result.push_back(s.substr(lastPos, pos - lastPos));
+        lastPos = pos + delimiter.length();
+    }
+    result.push_back(s.substr(lastPos));
+    return result;
+}
+
 int run_tests() {
-    string s1 = separate_paren_groups("( ) (( )) (( )( ))");
-    vector<string> v1 = vector<string>(split(s1, "))"));
-    assert(issame(v1, {"", "(()())",""}));
+    string s1 = separate_paren_groups("( ) (( )) (( )( )"));
+    vector<string> v1 = split(s1, "()");
+    assert(issame(v1, {"", "(()())","()" }));
     return 0;
 }
