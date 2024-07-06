bcff04433b6fd64d2a53832e971699ab7491b202
beep boop <githubbot@vadim.me>
2024-07-06 15:51:30 +0200

    Solution 70 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/iq6om4mn Prompt: it must return False for input t&t&f|f|t&t|f|t&t&f|f|f&t&f&t, but it returns .

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,38 +1,34 @@
 def solve_boolean(expression):
     bool_map = {'T': True, 'F': False}
-    op_stack = []
-    result = None
 
     def eval_expression(expr):
-        nonlocal result
-        while expr:
-            if expr[0] in ['|', '&']:
-                op_stack.append(expr[0])
-                expr = expr[1:]
-            elif op_stack and op_stack[-1] == '&' and expr[0] == '|':
-                result = True
-                break
-            else:
-                part = ''
-                while expr and expr[0] != '|' and expr[0] != '&':
-                    part += expr[0]
-                    expr = expr[1:]
-                if expr:
-                    op_stack.append(part)
-                else:
-                    return bool_map[part]
-        if op_stack:
-            if len(op_stack) == 2:
-                a, b = op_stack
-                result = (a == '&' and bool_map[a[1:]] and bool_map[b]) or (a == '|' and bool_map[a[1:]] or bool_map[b])
-            else:
-                for _ in range(len(op_stack)):
-                    op = op_stack.pop(0)
-                    if op == '|':
-                        result = any(eval_expression(part) for part in [op_stack.pop(0), op_stack.pop(0)])
-                    elif op == '&':
-                        result = all(eval_expression(part) for part in [op_stack.pop(0), op_stack.pop(0)])
-
-        return result
+        if '&' in expr:
+            start = 0
+            end = 0
+            for i in range(len(expr) - 1, -1, -1):
+                if expr[i] == '&':
+                    start = i
+                elif expr[i] == '|':
+                    end = i
+                    break
+            and_parts = expr[:end + 1].split('&')
+            expr = expr[start + 1:]
+            
+            return all(eval_expression(part) for part in and_parts)
+        elif '|' in expr:
+            start = 0
+            end = 0
+            for i in range(len(expr) - 1, -1, -1):
+                if expr[i] == '|':
+                    start = i
+                elif expr[i] == '&':
+                    end = i
+                    break
+            or_parts = expr[:end + 1].split('|')
+            expr = expr[start + 1:]
+            
+            return any(eval_expression(part) for part in or_parts)
+        else:
+            return bool_map[expr]
 
     return eval_expression(expression)
