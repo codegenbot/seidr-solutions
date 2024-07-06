0e86fa04fb45f0a7fa7d682aea1b7aedd3ccca37
beep boop <githubbot@vadim.me>
2024-07-06 20:31:23 +0200

    Solution 88 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3f2yeli0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d6d0d81-0b74-4330-889d-16d4ad33c e14.py", line 26, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d6d0d81-0b74-4330-889d-16d4ad33c e14.py", line 23, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,18 +1,6 @@
-```
-def cycpattern_check(a, b):
-    if not a.isnumeric() or not b.isnumeric():
-        return False
-
-    total = int(a) + int(b)
-    if total % 2 == 0:
-        return True
-    else:
-        return False
-
-
 def check(func):
     def wrapper(a, b):
-        return func(a, b)
+        return func(a, b) == "winten"
 
     return wrapper
 
