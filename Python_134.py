cb4ffa3a531e6b25f59b2adca6f8167ffd67e85c
beep boop <githubbot@vadim.me>
2024-07-06 02:02:47 +0200

    Solution 0 of Python/134, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/lkhgldn3 Prompt: Complete the following code given the task description and function signature.

---
+++ Python_134.py
@@ -0,0 +1,9 @@
+
+def check_if_last_char_is_a_letter(txt):
+    if len(txt) == 0:
+        return False
+    last_char = txt[-1]
+    if last_char.isalpha() and last_char not in " ":
+        return True
+    else:
+        return False
