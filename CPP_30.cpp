f7213922b00d26a3157f5f2304b173313b5ea8de
beep boop <githubbot@vadim.me>
2024-07-06 11:40:55 +0200

    Solution 32 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/aikobq58 Prompt:       |                    ^~~~~ 48606900-faaf-4fda-918b-fd669bafbaff.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 48606900-faaf-4fda-918b-fd669bafbaff.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 48606900-faaf-4fda-918b-fd669bafbaff.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 48606900-faaf-4fda-918b-fd669bafbaff.cpp:11: 48606900-faaf-4fda-918b-fd669bafbaff.cpp: In function ‘int main()’: 48606900-faaf-4fda-918b-fd669bafbaff.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 48606900-faaf-4fda-918b-fd669bafbaff.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,30 +1,25 @@
-```cpp
 #include <vector>
 #include <assert.h>
 
-std::vector<float> get_positive(std::vector<float> l) {
-    std::vector<float> result;
-    for (float num : l) {
-        if (num > 0) {
+bool issame(std::vector<float> a, std::vector<float> b) {
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(std::abs(a[i] - b[i]) > 1e-5) return false;
+    }
+    return true;
+}
+
+std::vector<float> get_positive(vector<float> l){
+    vector<float> result;
+    for(float num : l) {
+        if(num > 0) {
             result.push_back(num);
         }
     }
     return result;
 }
 
-bool issame(std::vector<float> a, std::vector<float> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int main() {
-    assert(issame(get_positive({}), {}));
+    assert(issame(get_positive({}), {}));  
     return 0;
 }
