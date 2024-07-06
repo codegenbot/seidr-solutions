e3e685f027591f65653aad36277528dae524edee
beep boop <githubbot@vadim.me>
2024-07-06 17:31:52 +0200

    Solution 78 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r6wjanum Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cbb50f79-9c3d-446d-b92f-11930c97d c1a.py", line 13, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cbb50f79-9c3d-446d-b92f-11930c97d c1a.py", line 8, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,6 +1,6 @@
 def sort_third(input_list):
-    result = [sorted([int(x) for x in input_list[i:i+3]], key=sum) for i in range(0, len(input_list), 3)]
-    remaining = input_list[len(input_list) // 3 * 3:]
+    first_part = [sorted(list(map(int, input_list[i:i+3]))) for i in range(0, len(input_list), 3)]
+    remaining = list(map(int, input_list[len(input_list) // 3 * 3:]))
     if remaining:
-        result.append(sorted([int(x) for x in remaining], key=sum))
-    return tuple(map(tuple, result))
+        first_part.append(sorted([x for x in remaining]))
+    return tuple(map(tuple, first_part))
