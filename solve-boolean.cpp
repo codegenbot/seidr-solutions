f643b96005863f6dfaa77a103b45ff48e67c7bcb
beep boop <githubbot@vadim.me>
2024-07-06 19:08:05 +0200

    Solution 41 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/o9ngt94r Prompt: 0653b545-2902-4f2c-b5b8-be8fb713dc7e.cpp: In function ‘int main()’: 0653b545-2902-4f2c-b5b8-be8fb713dc7e.cpp:48:14: error: expected ‘}’ at end of in put    48 |     return 0;       |              ^ 0653b545-2902-4f2c-b5b8-be8fb713dc7e.cpp:38:12: note: to match this ‘{’    38 | int main() {       |            ^

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,4 +1,40 @@
-```
+#include <string>
+#include <iostream> 
+using namespace std;
+
+bool solveBoolean(string s) {
+    bool t = true;
+    bool f = false;
+    bool res = t;
+    int i = 0;
+
+    while (i < s.length()) {
+        if (s[i] == 't') {
+            res = true;
+            i++;
+        } else if (s[i] == 'f') {
+            res = false;
+            i++;
+        } else if (s[i] == '&') {
+            res &= f;
+            i++;
+            if (i < s.length() && (s[i] == 't' || s[i] == 'f')) {
+                f = (s[i] == 't');
+                i++;
+            }
+        } else if (s[i] == '|') {
+            res |= f;
+            i++;
+            if (i < s.length() && (s[i] == 't' || s[i] == 'f')) {
+                f = (s[i] == 't');
+                i++;
+            }
+        }
+    }
+
+    return res;
+}
+
 int main() {
     string s;
     cout << "Enter a Boolean expression: ";
@@ -9,5 +45,4 @@ int main() {
     } else {
         cout << "False" << endl;
     }
-    return 0;
-}
+    return 0;}
