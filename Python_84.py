2ab545aec314f1dc2f03a74e1de6ed6e711c8da9
beep boop <githubbot@vadim.me>
2024-07-06 19:08:03 +0200

    Solution 33 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qeuhcwp8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6a6a3215-3011-4601-983a-aa616ba77 580.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,14 +1,12 @@
-```
 def main():
-    choice = int(input("Do you want to (1) Convert decimal to binary or (2) Convert binary to decimal? "))
-    solve = None
+    choice = int(
+        input(
+            "Do you want to (1) Convert decimal to binary or (2) Convert binary to decimal? "
+        )
+    )
     if choice == 1:
         N = int(input("Enter a number: "))
-        solve = bin(N)[2:]
+        print(bin(N)[2:])
     elif choice == 2:
         N = input("Enter a binary number: ")
-        solve = oct(int(N, 2))[2:]
-    else:
-        print("Invalid choice. Please choose (1) or (2).")
-    if solve is not None:
-        print(solve)
+        print(oct(int(N, 2))[2:])
