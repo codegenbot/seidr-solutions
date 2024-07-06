ea315743550c44b17f0331fe0d1d1dea4358dce5
beep boop <githubbot@vadim.me>
2024-07-06 12:42:40 +0200

    Solution 64 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6h0099y8 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 455c2e67-505a-4f58-8d90-da87aa46bfe4.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,24 +1,18 @@
 #include <algorithm>
 #include <vector>
 
-std::vector<int> order_by_points(std::vector<int> nums) {
+std::vector<int> order_by_points(int... nums) {
     auto compare = [&](int a, int b) {
         if (a == b) return 0;
         long sumA = 0, sumB = 0;
         for (char c : std::to_string(a)) sumA += c - '0';
         for (char c : std::to_string(b)) sumB += c - '0';
         if (sumA != sumB) return sumA - sumB > 0 ? 1 : -1;
-        return distance(nums.begin(), find(nums.begin(), nums.end(), a)) -
-               distance(nums.begin(), find(nums.begin(), nums.end(), b));
+        return distance(std::begin((int []){nums}), find(std::begin((int []){nums}), std::end((int []){nums}), a)) -
+               distance(std::begin((int []){nums}), find(std::begin((int []){nums}), std::end((int []){nums}), b));
     };
-    sort(nums.begin(), nums.end(), compare);
-    return nums;
-}
-
-int main(){
-    std::vector<int> result;
-    for (int i : {-76, -21, 0, 4, 23, 6, 6}) 
-        result.push_back(i); 
-    assert(std::equal(result.begin(), result.end(),{-76, -21, 0, 4, 23, 6, 6}));
-    return 0;
+    std::vector<int> v;
+    for (auto num : {nums}) v.push_back(num);
+    sort(v.begin(), v.end(), compare);
+    return v;
 }
