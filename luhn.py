04cf5ec5c65dcdab44841797a7f468d5a8c08b47
beep boop <githubbot@vadim.me>
2024-07-06 12:31:02 +0200

    Solution 40 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/3ywpqa0e Prompt: it must return 64 for input 16 7 3 6 6 2 0 9 2 5 0 2 3 9 4 7 6, but it returns .

--- luhn.py
+++ luhn.py
@@ -1,7 +1,6 @@
-
 def luhn_algorithm(digits):
     sum = 0
-    for i in range(1, 16, 2):
+    for i in range(2, 16, 2):
         digit = digits[i] * 2
         if digit > 9:
             sum += digit - 9
