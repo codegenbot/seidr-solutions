1eb8a0016a28a5054121810a47a79aaea380ad4e
beep boop <githubbot@vadim.me>
2024-07-06 02:21:47 +0200

    Solution 4 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/buu7n7i1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e691e441-7f82-49ed-ae9d-2c044b40d e3a.py", line 9, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e691e441-7f82-49ed-ae9d-2c044b40d e3a.py", line 6, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,2 +1,12 @@
 def cycpattern_check(a, b):
-    return any(x == y for x, y in zip(a[i:], b[: len(b)]))
+    # Forward loop
+    for i in range(len(a)):
+        if b in a[i:]:
+            return True
+
+    # Backward loop
+    for i in range(len(a)-1, -1, -1):
+        if b in a[i:]:
+            return True
+
+    return False
