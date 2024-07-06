7deede2a5ba41c8be4cb02930beecbfad63d63de
beep boop <githubbot@vadim.me>
2024-07-06 05:39:24 +0200

    Solution 57 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lynly39e Prompt: Program did not receive expected input

--- Python_22.py
+++ Python_22.py
@@ -1,5 +1,5 @@
-```
-def filter_integers(values: list[any]) -> list[int]:
-    if not all(isinstance(x, (int, str)) for x in values):
-        raise ValueError("Input should be a list of integers and strings")
+from typing import List
+
+
+def filter_integers(values: List) -> List[int]:
     return [value for value in values if isinstance(value, int)]
