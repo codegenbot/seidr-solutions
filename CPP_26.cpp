29e275e1a30dff097871ffb66f501dda26e8b341
beep boop <githubbot@vadim.me>
2024-07-06 14:16:29 +0200

    Solution 18 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/d0axb77d Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4df9e844-7461-4178-b6aa-fa0e1d6f9d4f.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4df9e844-7461-4178-b6aa-fa0e1d6f9d4f.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4df9e844-7461-4178-b6aa-fa0e1d6f9d4f.cpp:11: 4df9e844-7461-4178-b6aa-fa0e1d6f9d4f.cpp: In function ‘int main()’: 4df9e844-7461-4178-b6aa-fa0e1d6f9d4f.cpp:21:20: error: ‘remove_duplicates’ was n ot declared in this scope    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 4df9e844-7461-4178-b6aa-fa0e1d6f9d4f.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,26 +1,7 @@
-```cpp
 bool issame(vector<int> a, vector<int>b) {
-    if(a.size()!=b.size())
+    if(a.size() != b.size()) 
         return false;
-    for(int i=0;i<a.size();i++)
-        if(a[i]!=b[i])
-            return false;
-    return true;
-}
-
-int main() {
-    vector<int> numbers = {1, 2, 3, 4, 5, 6};
-    vector<int> result = remove_duplicates(numbers);
-    assert(issame(result, {1, 2, 3, 4, 5}) || issame(result, {2, 3, 4, 5}));
-    return 0;
-}
-
-vector<int> remove_duplicates(vector<int> numbers) {
-    vector<int> unique_numbers;
-    for (int num : numbers) {
-        if (find(unique_numbers.begin(), unique_numbers.end(), num) == unique_numbers.end()) {
-            unique_numbers.push_back(num);
-        }
-    }
-    return unique_numbers;
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    return a == b;
 }
