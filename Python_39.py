5b76b3659617b36b0c452a03bc4fab62c008a05b
beep boop <githubbot@vadim.me>
2024-07-06 16:24:36 +0200

    Solution 5 of Python/39, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/95uu9z4e Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/260abebd-cd3e-4dc4-acc3-54b64f1ab 74a.py", line 1     Here is the corrected code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_39.py
+++ Python_39.py
@@ -1,5 +1,4 @@
-```
-def prime_fib(n: int):
+def prime_fib(n):
     a, b = 0, 1
     count = 0
     while True:
@@ -9,7 +8,7 @@ def prime_fib(n: int):
                 return b
         a, b = b, a + b
 
-def is_prime(num: int):
+def is_prime(num):
     if num < 2:
         return False
     for i in range(2, int(num**0.5) + 1):
