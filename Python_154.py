bef39ea94bb9159949b90a860b22eb2dbf360213
beep boop <githubbot@vadim.me>
2024-07-06 20:29:03 +0200

    Solution 85 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3f2yeli0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3076e355-ccb0-45d7-9f22-0a3be519c fdc.py", line 26, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3076e355-ccb0-45d7-9f22-0a3be519c fdc.py", line 23, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,10 +1,9 @@
 def cycpattern_check(a, b):
-    if not (isinstance(a, int) and isinstance(b, int)):
-        return "not winten"
-    a = str(int(str(a).zfill(6)))[1:]
-    b = str(int(str(b).zfill(6)))[1:]
+    if not a.isnumeric() or not b.isnumeric():
+        return "Invalid input"
 
-    if int(str(a) + str(b)) % 2 == 0:
+    total = int(a) + int(b)
+    if total % 2 == 0:
         return "winten"
     else:
         return "not winten"
