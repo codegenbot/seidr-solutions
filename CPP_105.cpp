05206ce328acc3dc08f5302de55eedee1db71c81
beep boop <githubbot@vadim.me>
2024-07-06 16:57:18 +0200

    Solution 44 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5j0y4o2g Prompt:  in ‘a’, which is of non-class type ‘int’    24 |     for (int i=0;i<a.size();i++)       |                      ^~~~ 5656010c-a3e5-49e7-9dc6-778b5709c180.cpp:26:10: error: invalid types ‘int[int]’ for array subscript    26 |     if (a[i]!=b[i]) return false;       |          ^ 5656010c-a3e5-49e7-9dc6-778b5709c180.cpp:26:16: error: invalid types ‘int[int]’ for array subscript    26 |     if (a[i]!=b[i]) return false;       |                ^ In file included from 5656010c-a3e5-49e7-9dc6-778b5709c180.cpp:21: 5656010c-a3e5-49e7-9dc6-778b5709c180.cpp: In function ‘int main()’: 5656010c-a3e5-49e7-9dc6-778b5709c180.cpp:31:19: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    31 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                   |       |                   <brace-enclosed initializer list> 5656010c-a3e5-49e7-9dc6-778b5709c180.cpp:22:44: note:   initializing argument 2 of ‘bool issame(int, int)’    22 | bool issame(vector<string> a,vector<string>b){       |                              ~~~~~~~~~~~~~~^

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,17 +1,32 @@
-```
 #include <vector>
 using namespace std;
+
+bool is_same(vector<string> a,vector<string>b){
+    if(a.size()!=b.size()) return false;
+    for(int i = 0; i < a.size();i++)
+        if (a[i]!=b[i]) return false;
+    return true;
+}
+
 vector<pair<int, string>> by_length(vector<int> arr){
-    vector<int> temp;
-    for(int i: arr){
-        if(i >= 1 && i <= 9)temp.push_back(i);
+    vector<pair<int, string>> temp;
+    for(int i = 0; i < arr.size();i++){
+        if(i >= 1 && i <= 9)temp.push_back({i, to_string(i) == "1" ? "One": 
+            to_string(i) == "2" ? "Two":
+            to_string(i) == "3" ? "Three":
+            to_string(i) == "4" ? "Four":
+            to_string(i) == "5" ? "Five":
+            to_string(i) == "6" ? "Six":
+            to_string(i) == "7" ? "Seven":
+            to_string(i) == "8" ? "Eight":
+            "Nine"});
     }
     sort(temp.begin(), temp.end());
     reverse(temp.begin(), temp.end());
-    map<int, string> numToName = {{1,"One"}, {2,"Two"}, {3,"Three"}, {4,"Four"}, {5,"Five"}, {6,"Six"}, {7,"Seven"}, {8,"Eight"}, {9,"Nine"}; 
-    vector<pair<int, string>> result;
-    for(int i: temp){
-        result.push_back({i, numToName[i]});
-    }
-    return result;
+    return temp;
+}
+
+int main(){
+    assert(is_same(by_length({9, 4, 8}), vector<string>({"Nine", "Eight", "Four"})));
+    return 0;
 }
