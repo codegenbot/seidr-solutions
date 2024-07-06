1f03ce68b5bd2924f758d4b92a26c1ce352a7ceb
beep boop <githubbot@vadim.me>
2024-07-06 19:51:20 +0200

    Solution 87 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/26zr7o11 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/441ee852-3f6f-4e49-8537-47ddfca2e 6e1.py", line 1, in <module>     print(encode("I DoNt KnOw WhAt tO WrItE", "k dQnT kNqW wHcT Tq wRkTg"))           ^^^^^^ NameError: name 'encode' is not defined

--- Python_93.py
+++ Python_93.py
@@ -1,2 +1,5 @@
 ```
+def encode(text, mapping):
+    return "".join(mapping[i] if j < len(mapping) and i == j.lower() else i for i in text)
+
 print(encode("I DoNt KnOw WhAt tO WrItE", "k dQnT kNqW wHcT Tq wRkTg"))
