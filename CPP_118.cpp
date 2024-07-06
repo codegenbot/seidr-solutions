cba28d1a961161639c934c1b0fae279b3cdcbcc8
beep boop <githubbot@vadim.me>
2024-07-06 18:01:57 +0200

    Solution 32 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4tir31km Prompt: d931c80e-4fe8-41da-81fa-abea7ed329e8.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string get_closest_vowel(string word){       | ^~~~~~ d931c80e-4fe8-41da-81fa-abea7ed329e8.cpp: In function ‘bool isvowel(char)’: d931c80e-4fe8-41da-81fa-abea7ed329e8.cpp:19:10: error: ‘tolower’ was not declare d in this scope    19 |     ch = tolower(ch);       |          ^~~~~~~ In file included from d931c80e-4fe8-41da-81fa-abea7ed329e8.cpp:26: d931c80e-4fe8-41da-81fa-abea7ed329e8.cpp: In function ‘int main()’: d931c80e-4fe8-41da-81fa-abea7ed329e8.cpp:28:13: error: ‘get_closest_vowel’ was n ot declared in this scope    28 |     assert (get_closest_vowel("Above") == "o");       |             ^~~~~~~~~~~~~~~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,27 +1,24 @@
 #include <string>
 #include <cctype>
 
-string get_closest_vowel(string word){
-    int n = word.size();
-    for(int i=n-1; i>=0; i--){
-        if(word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || 
-           word[i] == 'o' || word[i] == 'u' || word[i] == 'A' || 
-           word[i] == 'E' || word[i] == 'I' || word[i] == 'O' || 
-           word[i] == 'U'){
-            for(int j=i-1; j>=0; j--){
-                if(!isvowel(word[j])){
-                    return (char)word[i];
-                }
-            }
-        }
-    }
-    return "";
-}
-
 bool isvowel(char ch){
-    ch = std::tolower(ch);
+    ch = tolower(ch);
     if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
         return true;
     }
     return false;
+}
+
+std::string get_closest_vowel(std::string word){
+    int n = word.size();
+    for(int i=n-1; i>=0; i--){
+        if((word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || 
+           word[i] == 'o' || word[i] == 'u' || word[i] == 'A' || 
+           word[i] == 'E' || word[i] == 'I' || word[i] == 'O' || 
+           word[i] == 'U') &&
+           (i == n-1 || !isvowel(word[i+1]))){
+            return (char)word[i];
+        }
+    }
+    return "";
 }
