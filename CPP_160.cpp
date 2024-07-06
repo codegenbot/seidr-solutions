d91fdedca973c1e979a4f5abd546434ba3fc4bd7
beep boop <githubbot@vadim.me>
2024-07-06 11:39:18 +0200

    Solution 5 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dl2z8mmq Prompt:       |                       ^~~~~~ 214f9df2-79c5-4870-a638-7f7a6eb33630.cpp:1:31: error: ‘operator_’ was not declar ed in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                               ^~~~~~~~~ 214f9df2-79c5-4870-a638-7f7a6eb33630.cpp:1:42: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                          ^~~~~~ 214f9df2-79c5-4870-a638-7f7a6eb33630.cpp:1:49: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                 ^~~ 214f9df2-79c5-4870-a638-7f7a6eb33630.cpp:1:61: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                             ^ In file included from 214f9df2-79c5-4870-a638-7f7a6eb33630.cpp:19: 214f9df2-79c5-4870-a638-7f7a6eb33630.cpp: In function ‘int main()’: 214f9df2-79c5-4870-a638-7f7a6eb33630.cpp:21:23: error: ‘do_algebra’ cannot be us ed as a function    21 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,6 +1,7 @@
 #include <vector>
+#include <cmath>
 
-int do_algebra(std::vector<std::string> operator_, std::vector<int> operand) {
+int do_algebra(vector<string> operator_, vector<int> operand) {
     int result = operand[0];
     for (int i = 0; i < operator_.size(); i++) {
         if (operator_[i] == "+") {
