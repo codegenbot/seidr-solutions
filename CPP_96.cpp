1308f1c9afa35c4eeb7af12353742f21a532e5cd
beep boop <githubbot@vadim.me>
2024-07-06 17:47:10 +0200

    Solution 54 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/bbi8kb69 Prompt:    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4701770b-edd4-4491-9fdb-36ee94e44f7f.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4701770b-edd4-4491-9fdb-36ee94e44f7f.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4701770b-edd4-4491-9fdb-36ee94e44f7f.cpp:22: 4701770b-edd4-4491-9fdb-36ee94e44f7f.cpp: In function ‘int main()’: 4701770b-edd4-4491-9fdb-36ee94e44f7f.cpp:32:20: error: ‘count_up_to’ was not dec lared in this scope    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 4701770b-edd4-4491-9fdb-36ee94e44f7f.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,28 +1,3 @@
-```cpp
-#include <vector>
-#include <algorithm>
-
-bool isEqual(const std::vector<int>& a, const std::vector<int>& b) {
-    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
-}
-
-std::vector<int> count_up_to(int n) {
-    std::vector<int> primes;
-    for (int i = 2; i <= n; ++i) {
-        bool isPrime = true;
-        if(i == 2)
-            primes.push_back(i);
-        else if(i > 2) {
-            for (int j = 2; j * j <= i; ++j) {
-                if (i % j == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime) {
-                primes.push_back(i);
-            }
-        }
-    }
-    return primes;
+bool isEqual(vector<int> a, vector<int> b) {
+    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
 }
