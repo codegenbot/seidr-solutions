9c3b8eac486c51877c1921afa9549051c791e6d4
beep boop <githubbot@vadim.me>
2024-07-06 03:23:26 +0200

    Solution 72 of Python/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/hy53hw4z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a8a0407f-e86e-4803-be07-8506a49a7 646.py", line 20, in <module>     check(select_words)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a8a0407f-e86e-4803-be07-8506a49a7 646.py", line 12, in check     assert select_words("a b c d e f", 1) == [            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: 2nd edge test error: ['a', 'b', 'c', 'd', 'e', 'f']

--- Python_117.py
+++ Python_117.py
@@ -1,9 +1,8 @@
-
 def select_words(s, n):
     words = s.split()
     if n <= 0:
-        return []
+        return ""
     elif n == 1:
-        return list(filter(lambda x: x.islower(), words))
+        return " ".join(filter(lambda x: x.islower(), words))
     else:
-        return list(filter(lambda x: all(c.islower() for c in x[1:]), words))[:n]
+        return " ".join(filter(lambda x: all(c.islower() for c in x[1:]), words[:n]))
