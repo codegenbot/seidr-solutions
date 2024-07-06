0991acfb45cc2d80dcde6d4469a4e698e8a62986
beep boop <githubbot@vadim.me>
2024-07-06 19:35:04 +0200

    Solution 15 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7aum6t7i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6755a605-cc56-4dbf-8cec-1cc4f257e f7e.py", line 45, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6755a605-cc56-4dbf-8cec-1cc4f257e f7e.py", line 15, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -5,6 +5,8 @@ def sum_squares(lst):
     total_sum = 0
     for num in lst:
         if not isinstance(num, (int, float)):
-            raise ValueError("All elements in the list must be integers or floats")
+            raise ValueError(f"Invalid input '{num}' found. Input must contain only numbers.")
+        if num <= 0:
+            raise ValueError(f"All numbers should be positive. Found a negative number: {num}")
         total_sum += num ** 2
     return total_sum
