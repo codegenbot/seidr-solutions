4f1b3b8bd6df708382dee6441c99a6616854a80b
beep boop <githubbot@vadim.me>
2024-07-06 09:05:20 +0200

    Solution 63 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jevrqdd0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f56e93da-ef36-4af0-b8cf-24b3c013e 98c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -8,11 +8,24 @@ def parse_music(music_string: str) -> List[int]:
     i = 0
     while i < len(music_string):
         if music_string[i] == '.':
-            i += 3
+            if i+3 <= len(music_string) and music_string[i+1:i+3].lower() in beats_per_note:
+                result.append(beats_per_note[music_string[i+1:i+3].lower()])
+                i += 3
+            else:
+                while i < len(music_string) and music_string[i].isalnum():
+                    note = ''
+                    while i < len(music_string) and music_string[i].isalnum():
+                        note += music_string[i]
+                        i += 1
+                    result.append(beats_per_note[note])
         else:
-            note = ''
             while i < len(music_string) and music_string[i].isalnum():
-                note += music_string[i]
-                i += 1
-            result.append(beats_per_note[note])
+                note = ''
+                while i < len(music_string) and music_string[i].isalnum():
+                    note += music_string[i]
+                    i += 1
+                if note.lower() in beats_per_note:
+                    result.append(beats_per_note[note.lower()])
+                else:
+                    return []
     return result
