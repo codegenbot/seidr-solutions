5894767ce4da5c5431052c1be337a56840bd0d68
beep boop <githubbot@vadim.me>
2024-07-06 17:07:15 +0200

    Solution 71 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1fv3kcsh Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/27bb20c7-3129-4069-8e11-189fca5a8 ade.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/27bb20c7-3129-4069-8e11-189fca5a8 ade.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,4 +1,5 @@
+```
 def sort_even(l: list) -> tuple:
-    even_numbers = sorted([x for x in l if isinstance(x, int) and x % 2 == 0])
-    odd_numbers = sorted([x for x in l if isinstance(x, int) and x % 2 != 0])
-    return tuple(even_numbers), tuple(odd_numbers)
+    even_numbers = [x for x in l if isinstance(x, int) and x % 2 == 0]
+    odd_numbers = [x for x in l if isinstance(x, int) and x % 2 != 0]
+    return (tuple(sorted(even_numbers)), tuple(sorted(odd_numbers)))
