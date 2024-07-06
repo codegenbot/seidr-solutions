b36b1bccf66b8674a91eb166c5314fd5030b97b3
beep boop <githubbot@vadim.me>
2024-07-06 18:16:37 +0200

    Solution 43 of CPP/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/w9yoob9j Prompt: in this scope    17 |     assert(minSubArraySum({1, -1}) == -1);       |     ^~~~~~ 07d61dcb-3fd3-4a05-90f4-495526ba206f.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <limits>   +++ |+#include <cassert>     3 | 07d61dcb-3fd3-4a05-90f4-495526ba206f.cpp: At global scope: 07d61dcb-3fd3-4a05-90f4-495526ba206f.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 07d61dcb-3fd3-4a05-90f4-495526ba206f.cpp:4:5: note: ‘int main()’ previously defi ned here     4 | int main() {       |     ^~~~ In file included from 07d61dcb-3fd3-4a05-90f4-495526ba206f.cpp:20: 07d61dcb-3fd3-4a05-90f4-495526ba206f.cpp: In function ‘int main()’: 07d61dcb-3fd3-4a05-90f4-495526ba206f.cpp:22:13: error: ‘minSubArraySum’ was not declared in this scope    22 |     assert (minSubArraySum({1, -1}) == -1);       |             ^~~~~~~~~~~~~~

--- CPP_114.cpp
+++ CPP_114.cpp
@@ -6,10 +6,10 @@ long long minSubArraySum(std::vector<long long> nums) {
     long long sum = 0, min_sum = LLONG_MAX;
     for (int i = 0; i < nums.size(); i++) {
         sum += nums[i];
-        if(sum > 0)
-            min_sum = std::min(min_sum, sum);
-        else 
+        min_sum = std::min(min_sum, sum);
+        if (sum > 0) {
             sum = 0;
+        }
     }
     return min_sum;
 }
