ee0d2b38bf173a379901401bb5e6c681558e4908
beep boop <githubbot@vadim.me>
2024-07-06 03:58:00 +0200

    Solution 48 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/bl4f4nrb Prompt: Program did not receive expected input

--- Python_38.py
+++ Python_38.py
@@ -1,8 +1,9 @@
-def decode_cyclic(s):
+
+def decode_cyclic(s: str):
     if len(s) % 3 != 0:
         print("Input string must have a length that is a multiple of 3")
         return None
-    groups = [s[(3 * i) : min((3 * i + 3), len(s))] for i in range((len(s) + 2) // 3)]
+    groups = [s[i: i+3] for i in range(0, len(s), 3)]
     decoded_groups = [
         (group[-1:] + group[:-1]) if len(group) == 3 else group for group in groups
     ]
