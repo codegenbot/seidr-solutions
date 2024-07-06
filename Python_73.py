3e0b275881044f8fd3de291c13c5655f56b5b04d
beep boop <githubbot@vadim.me>
2024-07-06 08:10:46 +0200

    Solution 10 of Python/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jv9360to Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8e8dc434-46c2-4bc0-aebf-fa884c4b7 3cb.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_73.py
+++ Python_73.py
@@ -1,9 +1,12 @@
 def smallest_change(arr):
+    if len(arr) != 16:
+        raise ValueError("Array length should be 16 bits")
+
     n = len(arr)
     forward = arr[:]
     backward = arr[::-1]
     
     for i in range(n//2):
         if forward[i] != backward[n-i-1]:
-            return 0
+            return 1
     return 0
