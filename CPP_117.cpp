a2d2c4bc48e3610f0a2a27844dcb7ecc45e88e80
beep boop <githubbot@vadim.me>
2024-07-06 08:53:11 +0200

    Solution 43 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ssvdmxr6 Prompt:       |                                     ^~~~~~ 5e7f513e-0120-40b8-ab98-2b1de83e59fc.cpp:41:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 5e7f513e-0120-40b8-ab98-2b1de83e59fc.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 5e7f513e-0120-40b8-ab98-2b1de83e59fc.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 5e7f513e-0120-40b8-ab98-2b1de83e59fc.cpp:41:44: error: ‘b’ was not declared in t his scope    41 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5e7f513e-0120-40b8-ab98-2b1de83e59fc.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 5e7f513e-0120-40b8-ab98-2b1de83e59fc.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -14,7 +14,7 @@ std::vector<std::string> select_words(const std::string& str, int num) {
     return words;
 }
 
-bool is_same(char ch1, char ch2) {
+bool areSame(char ch1, char ch2) {
     if (isalpha(ch1) && isalpha(ch2)) {
         return tolower(ch1) == tolower(ch2);
     } else {
@@ -22,19 +22,18 @@ bool is_same(char ch1, char ch2) {
     }
 }
 
-bool are_words_the_same(std::vector<std::string> a, std::vector<std::string> b) {
+bool areWordsTheSame(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size()) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
-        if (!is_same(a[i][0], b[i][0])) {
+        if (!areSame(a[i][0], b[i][0])) {
             return false;
         }
     }
     return true;
 }
 
-int test_main() {
-    assert(are_words_the_same(select_words("a b c d e f", 4), {"b", "c", "d", "f"}));
+int main2() {
+    assert(issame(select_words("a b c d e f", 4), {"b", "c", "d", "f"}));
     return 0;
-}
