21d3d13f2423b23766cfb0f47faef873912cbf1a
beep boop <githubbot@vadim.me>
2024-07-06 15:53:04 +0200

    Solution 56 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0wk0egm1 Prompt: In file included from 82291c38-67a1-4c2a-a0a1-4c4cf2ffcd58.cpp:21: 82291c38-67a1-4c2a-a0a1-4c4cf2ffcd58.cpp: In function ‘int main()’: 82291c38-67a1-4c2a-a0a1-4c4cf2ffcd58.cpp:31:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    31 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -3,20 +3,6 @@
 
 using namespace std;
 
-bool issame(vector<int> v1, vector<int> v2) {
-    if(v1.size() != v2.size()) return false;
-    for(int i=0; i<v1.size(); i++){
-        if(v1[i] != v2[i]) return false;
-    }
-    return true;
-}
-
-bool isequal(vector<int> v1, vector<int> v2) {
-    sort(v1.begin(), v1.end());
-    sort(v2.begin(), v2.end());
-    return (v1==v2);
-}
-
 vector<int> strange_sort_vector(vector<int> lst) {
     vector<int> result;
     if (lst.empty()) return result;
@@ -29,6 +15,10 @@ vector<int> strange_sort_vector(vector<int> lst) {
             sort(lst.begin(), lst.end());
     }
 
-    assert(issame(result, vector<int>(vector<int>(1,result.size()).begin(), vector<int>(1,result.size()).begin()+result.size())));
     return result;
+}
+
+int main() {
+    assert(equal(strange_sort_vector({1, 2, 3}).begin(), strange_sort_vector({1, 2, 3}).end(), {1, 2, 3}.begin(), {1, 2, 3}.end()));
+    return 0;
 }
