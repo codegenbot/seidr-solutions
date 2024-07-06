843efb6178dc12af7a055991c4d5d5320325f75a
beep boop <githubbot@vadim.me>
2024-07-06 10:04:34 +0200

    Solution 17 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/rv1do6si Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90c7c2c5-e24b-4d91-bb37-488e34c4a 4f5.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90c7c2c5-e24b-4d91-bb37-488e34c4a 4f5.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,1 @@
-```
-return [i if i % 3 != 0 else sorted([j for j in l if j % 3 == 0])[(i // 3) % len(sorted([j for j in l if j % 3 == 0]))]
-    for i in range(len(l))
-]
+return [i if i % 3 != 0 else sorted([j for j in l if j % 3 == 0])[(i // 3) % len(sorted([j for j in l if j % 3 == 0]))] for i in range(len(l))]
