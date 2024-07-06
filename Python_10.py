c6968e175d15f5ee31cbd4f41c40d563253b2895
beep boop <githubbot@vadim.me>
2024-07-06 02:49:39 +0200

    Solution 32 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/zqqswxr4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5d1b0901-4f28-4280-b8dd-9b0af613f c55.py", line 15, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5d1b0901-4f28-4280-b8dd-9b0af613f c55.py", line 12, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,13 +1,10 @@
 def make_palindrome(string: str) -> str:
+    # Find the longest postfix of supplied string that is a palindrome
     i = len(string) - 1
     while i > 0 and string[i] != string[i - 1]:
         i -= 1
     if i == 0:
         return string + string[::-1]
-
-    # Check if the input string already ends with a palindrome
-    if string[-i:] == string[:i][::-1]:
-        return string
 
     # Append to the end of the string reverse of a string prefix that comes before the palindromic suffix
     return string + string[:i][::-1]
