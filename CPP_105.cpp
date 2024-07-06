dff9d267d2323a53e24233b2ea82f372f99a0897
beep boop <githubbot@vadim.me>
2024-07-06 17:38:13 +0200

    Solution 34 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/horaq1dq Prompt:       |                              ^~~~~~ 44db57d3-1ec4-46e8-bafc-e35c26ad6294.cpp:46:37: error: ‘string’ was not declared  in this scope    46 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 44db57d3-1ec4-46e8-bafc-e35c26ad6294.cpp:46:44: error: ‘b’ was not declared in t his scope    46 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 44db57d3-1ec4-46e8-bafc-e35c26ad6294.cpp:46:45: error: expression list treated a s compound expression in initializer [-fpermissive]    46 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 44db57d3-1ec4-46e8-bafc-e35c26ad6294.cpp:45: 44db57d3-1ec4-46e8-bafc-e35c26ad6294.cpp: In function ‘int main()’: 44db57d3-1ec4-46e8-bafc-e35c26ad6294.cpp:55:20: error: ‘by_length’ was not decla red in this scope    55 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 44db57d3-1ec4-46e8-bafc-e35c26ad6294.cpp:55:19: error: ‘issame’ cannot be used a s a function    55 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <algorithm>
 #include <string>
@@ -16,13 +15,37 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
 }
 
 std::vector<std::string> by_length(std::vector<int> arr) {
-    vector<string> numVec;
+    vector<string> result;
     for (int i : arr) {
-        if (i >= 1 && i <= 9) {
-            numVec.push_back(i < 5 ? "One" : (i < 8 ? "Two" : "Three"));
+        switch (i) {
+            case 1:
+                result.push_back("One");
+                break;
+            case 2:
+                result.push_back("Two");
+                break;
+            case 3:
+                result.push_back("Three");
+                break;
+            case 4:
+                result.push_back("Four");
+                break;
+            case 5:
+                result.push_back("Five");
+                break;
+            case 6:
+                result.push_back("Six");
+                break;
+            case 7:
+                result.push_back("Seven");
+                break;
+            case 8:
+                result.push_back("Eight");
+                break;
+            case 9:
+                result.push_back("Nine");
+                break;
         }
     }
-    sort(numVec.begin(), numVec.end());
-    reverse(numVec.begin(), numVec.end());
-    return numVec;
+    return result;
 }
