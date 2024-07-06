e55d9b05605cb59d5ec2799dafdf8643bd08f988
beep boop <githubbot@vadim.me>
2024-07-06 18:10:00 +0200

    Solution 34 of CPP/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/w9yoob9j Prompt: 78fe43dc-0663-4cc6-8a08-c55481eca812.cpp:1:26: error: ‘vector’ was not declared in this scope     1 | long long minSubArraySum(vector<long long> nums) {       |                          ^~~~~~ 78fe43dc-0663-4cc6-8a08-c55481eca812.cpp:1:33: error: expected primary-expressio n before ‘long’     1 | long long minSubArraySum(vector<long long> nums) {       |                                 ^~~~ In file included from 78fe43dc-0663-4cc6-8a08-c55481eca812.cpp:13: 78fe43dc-0663-4cc6-8a08-c55481eca812.cpp: In function ‘int main()’: 78fe43dc-0663-4cc6-8a08-c55481eca812.cpp:15:27: error: ‘minSubArraySum’ cannot b e used as a function    15 |     assert (minSubArraySum({1, -1}) == -1);       |             ~~~~~~~~~~~~~~^~~~~~~~~

--- CPP_114.cpp
+++ CPP_114.cpp
@@ -1,12 +1,2 @@
 #include <vector>
-long long minSubArraySum(vector<long long> nums) { 
-    long long total = 0; 
-    long long min_sum = LLONG_MAX; 
-
-    for (int i = 0; i < nums.size(); i++) { 
-        total += nums[i]; 
-        min_sum = min(min_sum, total); 
-    } 
-
-    return min_sum; 
-}
+long long minSubArraySum(vector<long long> nums) {
