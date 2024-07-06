6cafdd00736d9d58d2995e009f170f248cadc26a
beep boop <githubbot@vadim.me>
2024-07-06 19:09:51 +0200

    Solution 78 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ygswh6dl Prompt: In file included from dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:19:37: error: ‘string’ was not declared  in this scope    19 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:19:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ dd5558a4-fd26-4b3e-894e-5506361d1919.cpp:19:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -2,10 +2,6 @@
 #include <string>
 #include <algorithm>
 
-std::string bf(std::string a, std::string b) {
-    // Define the function body here
-}
-
 bool issame(std::vector<std::string> a, std::vector<std::string> b){
     for (int i = 0; i < a.size(); i++) {
         if (find(b.begin(),b.end(),a[i]) == b.end()) {
@@ -13,4 +9,7 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b){
         }
     }
     return true;
+}
+
+std::string bf(std::string a, std::string b) {
 }
