c03a42e3cf8543b5ec265fb30fe0f1c21b03eeda
beep boop <githubbot@vadim.me>
2024-07-06 19:53:38 +0200

    Solution 91 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/26zr7o11 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1d3c25ef-a155-4b04-86d8-a7c224449 d73.py", line 11, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1d3c25ef-a155-4b04-86d8-a7c224449 d73.py", line 7, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg"     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: encode() missing 1 required positional argument: 'mapping'

--- Python_93.py
+++ Python_93.py
@@ -1,5 +1,2 @@
-```
 def encode(text, mapping):
-    return "".join(mapping[i].lower() if i.isalpha() and i.lower() in mapping else i for i in text)
-
-print(encode("I DoNt KnOw WhAt tO WrItE", "k dQnT kNqW wHcT Tq wRkTg"))
+    return "".join(mapping[i] if i.isalpha() and i.lower() in mapping else i for i in text)
