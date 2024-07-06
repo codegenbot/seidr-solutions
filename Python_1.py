dbac0464ca6065d9588afd5a868f6fbb108e1895
beep boop <githubbot@vadim.me>
2024-07-06 05:35:30 +0200

    Solution 16 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/n84dqih8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f7246f40-28fb-48f7-902b-dfee0a934 7c0.py", line 1, in <module>     def separate_paren_groups(paren_string: str) -> List[str]:                                                     ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_1.py
+++ Python_1.py
@@ -1,12 +1,17 @@
-def separate_paren_groups(paren_string: str) -> List[str]:
+def separate_paren_groups(paren_string: str) -> list[str]:
     result = []
     stack = []
     for char in paren_string:
         if char == "(":
             stack.append([])
         elif char == ")":
-            group = stack.pop()
-            result.append(group)
+            if len(stack) > 0:
+                stack[-1].append(char)
+            else:
+                result.append(char)
         else:
-            continue
+            if len(stack) > 0:
+                stack[-1].append(char)
+    for group in stack:
+        result.append("".join(group))
     return result
