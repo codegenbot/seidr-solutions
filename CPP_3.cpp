da8d259f766c8e4ba8c3c2819a5afab491985f0e
beep boop <githubbot@vadim.me>
2024-07-07 01:41:08 +0200

    Solution 2 of CPP/3, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zvxcs5tp Prompt: a1bb7003-8ddc-40f8-8237-470edf5c4763.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | bool below_zero(vector<int> operations){       |                 ^~~~~~ a1bb7003-8ddc-40f8-8237-470edf5c4763.cpp:1:24: error: expected primary-expressio n before ‘int’     1 | bool below_zero(vector<int> operations){       |                        ^~~ In file included from a1bb7003-8ddc-40f8-8237-470edf5c4763.cpp:10: a1bb7003-8ddc-40f8-8237-470edf5c4763.cpp: In function ‘int main()’: a1bb7003-8ddc-40f8-8237-470edf5c4763.cpp:12:23: error: ‘below_zero’ cannot be us ed as a function    12 |     assert (below_zero({1, -2, 2, -2, 5, -5, 4, -4}) == true);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_3.cpp
+++ CPP_3.cpp
@@ -1,6 +1,8 @@
-bool below_zero(vector<int> operations){
+#include <vector>
+
+bool below_zero(std::vector<int> operations){
     int balance = 0;
-    for(int operation : operations){
+    for(int &operation : operations){
         balance += operation;
         if(balance < 0) return true;
     }
