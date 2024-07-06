6c908963b5a6f8bfec820527b836c7c923f72c9e
beep boop <githubbot@vadim.me>
2024-07-06 14:59:13 +0200

    Solution 17 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ny2a3etd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4999dd1a-537e-4dab-a003-d2a4683e6 d9f.py", line 14, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4999dd1a-537e-4dab-a003-d2a4683e6 d9f.py", line 11, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,9 +1,23 @@
+```
 def make_palindrome(s: str) -> str:
     if s == s[::-1]:
         return s
 
     for char in "abcdefghijklmnopqrstuvwxyz":
-        for i in range(len(s)):
-            new_string = s[:i] + char + s[i:]
-            if new_string == new_string[::-1]:
-                return new_string
+        left_to_right = ""
+        right_to_left = list(s)
+        i, j = 0, len(s) - 1
+        while i <= j:
+            left_to_right += s[i]
+            right_to_left[j] = ''
+            i += 1
+            j -= 1
+        right_to_left.append(char)
+        for k in range(len(left_to_right)):
+            if right_to_left[k] == '':
+                left_to_right = left_to_right[:k] + char + left_to_right[k:]
+                break
+
+        new_string = left_to_right + "".join(reversed(right_to_left))
+        if new_string == new_string[::-1]:
+            return new_string
