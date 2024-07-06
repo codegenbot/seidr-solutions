f201dc595b18ff19f703fb5e4ed294fcd8126043
beep boop <githubbot@vadim.me>
2024-07-06 10:02:05 +0200

    Solution 14 of CPP/132, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/z1g8mvmj Prompt: 7e72ab87-f7a0-4d1c-9066-04ea44121c96.cpp:15:9: error: ‘cout’ was not declared in  this scope    15 |         cout << "The string is nested.\n";       |         ^~~~ 7e72ab87-f7a0-4d1c-9066-04ea44121c96.cpp:17:9: error: ‘cout’ was not declared in  this scope    17 |         cout << "The string is not nested.\n";       |         ^~~~ 7e72ab87-f7a0-4d1c-9066-04ea44121c96.cpp: At global scope: 7e72ab87-f7a0-4d1c-9066-04ea44121c96.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 7e72ab87-f7a0-4d1c-9066-04ea44121c96.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~ In file included from 7e72ab87-f7a0-4d1c-9066-04ea44121c96.cpp:22: 7e72ab87-f7a0-4d1c-9066-04ea44121c96.cpp: In function ‘int main()’: 7e72ab87-f7a0-4d1c-9066-04ea44121c96.cpp:24:13: error: ‘is_nested’ was not decla red in this scope    24 |     assert (is_nested("]]]]]]]]") == false);       |             ^~~~~~~~~

--- CPP_132.cpp
+++ CPP_132.cpp
@@ -11,13 +11,12 @@ bool is_nested(std::string str) {
 }
 
 int main() {
-    std::string str;
-    std::cout << "Enter a string: ";
-    std::cin >> str;
-    bool result = is_nested(str);
-    if(result)
-        std::cout << "The string is nested";
-    else
-        std::cout << "The string is not nested";
+    std::string s;
+    std::cin >> s;
+    if (is_nested(s)) {
+        std::cout << "The string is nested.\n";
+    } else {
+        std::cout << "The string is not nested.\n";
+    }
     return 0;
 }
