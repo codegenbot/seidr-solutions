c91b72747188e51f2e50eb807324f4e972347c09
beep boop <githubbot@vadim.me>
2024-07-06 13:14:58 +0200

    Solution 72 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vxyizaz8 Prompt:       |                  ^~~~~~ 25d7b957-95eb-40b4-b3ec-2922ef921c63.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | bool isSame(std::vector<float> a, std::vector<float> b) { 25d7b957-95eb-40b4-b3ec-2922ef921c63.cpp:1:25: error: expected primary-expressio n before ‘float’     1 | bool isSame(std::vector<float> a, std::vector<float> b) {       |                         ^~~~~ 25d7b957-95eb-40b4-b3ec-2922ef921c63.cpp:1:40: error: ‘vector’ is not a member o f ‘std’     1 | bool isSame(std::vector<float> a, std::vector<float> b) {       |                                        ^~~~~~ 25d7b957-95eb-40b4-b3ec-2922ef921c63.cpp:1:40: note: ‘std::vector’ is defined in  header ‘<vector>’; did you forget to ‘#include <vector>’? 25d7b957-95eb-40b4-b3ec-2922ef921c63.cpp:1:47: error: expected primary-expressio n before ‘float’     1 | bool isSame(std::vector<float> a, std::vector<float> b) {       |                                               ^~~~~ 25d7b957-95eb-40b4-b3ec-2922ef921c63.cpp:1:55: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool isSame(std::vector<float> a, std::vector<float> b) {       |                                                       ^

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -1,8 +1,9 @@
 #include <vector>
+#include <cmath>
 
 bool isSame(std::vector<float> a, std::vector<float> b) {
     if(a.size() != b.size()) return false;
     for(int i = 0; i < a.size(); ++i) {
-        if(abs(a[i] - b[i]) > 1e-5f) return false;
+        if(std::abs(a[i] - b[i]) > 1e-5f) return false;
     }
     return true;
