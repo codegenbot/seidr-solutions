d692e83c09d636993b2c8bac6619ded8a3e9620e
beep boop <githubbot@vadim.me>
2024-07-06 23:14:18 +0200

    Solution 11 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/p9zd3p1w Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4a60c4c6-b038-4d38-adf2-28db3c3b5 733.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,6 +1,5 @@
 def double_the_difference():
-    user_input = input("Enter a list of integers: ")
-    lst = [int(x) for x in user_input.split()]
+    lst = [int(x) for x in input("Enter a list of integers: ").split()]
     return sum(i ** 2 for i in lst if isinstance(i, int) and i > 0)
 
 print(double_the_difference())
