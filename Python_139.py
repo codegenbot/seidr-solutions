c618cbe80804d031364c3af79b667e23358117d5
beep boop <githubbot@vadim.me>
2024-07-06 06:59:52 +0200

    Solution 2 of Python/139, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/105qa4f9 Prompt: Complete the following code given the task description and function signature.

--- Python_139.py
+++ Python_139.py
@@ -2,6 +2,6 @@ def special_factorial(n):
     if n < 1:
         return 0
     result = 1
-    for i in range(n, 0, -1):
+    for i in range(1, n + 1):
         result *= math.factorial(i)
     return result
