efdc3b2a9dff9f2c03ac4c1dd567fe615017bd34
beep boop <githubbot@vadim.me>
2024-07-06 10:38:03 +0200

    Solution 61 of Python/75, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/4uhps6fc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a6bcb769-32bb-4d9e-8319-4412337f8 082.py", line 19, in <module>     check(is_multiply_prime)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a6bcb769-32bb-4d9e-8319-4412337f8 082.py", line 16, in check     assert is_multiply_prime(11 * 13 * 7) == True            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_75.py
+++ Python_75.py
@@ -4,3 +4,10 @@ def is_multiply_prime(a):
             if not (i * (a // i) == a):
                 return False
     return True
+def is_prime(n):
+    if n <= 1 or n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
