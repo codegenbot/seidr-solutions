83f25b1c0f6cdc6234a41e1cd91e254e59b1ec2e
beep boop <githubbot@vadim.me>
2024-07-06 20:41:37 +0200

    Solution 78 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7aum6t7i Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6b31cfea-0b41-4443-9714-a04c2c990 030.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -3,24 +3,16 @@ def sum_squares(lst):
     total_sum = 0
     for element in lst:
         if isinstance(element, dict):
-            total_sum += sum(
-                value**2 
-                for key, value in element.items() 
-                if isinstance(value, (int, float))
-            )
+            total_sum += sum(value**2 for value in element.values() if isinstance(value, (int, float)))
         elif isinstance(element, list):
             total_sum += sum_squares([element])
         elif isinstance(element, (int, float)):
             total_sum += abs(element) ** 2
         else:
-            raise ValueError(
-                f"Invalid input '{element}' found. Input must contain only numbers."
-            )
+            raise ValueError(f"Invalid input '{element}' found. Input must contain only numbers.")
     return total_sum
-
 
 def main():
     print(sum_squares([{"x": [2]}, {"y": [4]}]))
-
 
 main()
