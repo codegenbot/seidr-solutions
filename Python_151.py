36f9a4d7906ebe2ebb81b021bd4c7968ea71bea6
beep boop <githubbot@vadim.me>
2024-07-06 04:24:34 +0200

    Solution 94 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/61mfjeit Prompt: 12 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e777ad6c-f0a7-458a-882f-8b576bb91 47d.py", line 15, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e777ad6c-f0a7-458a-882f-8b576bb91 47d.py", line 11, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,4 +1,7 @@
-def double_the_difference(lst):
-    odd_sum = sum(i for i in lst if isinstance(i, int) and i % 2 != 0)
-    even_sum = sum(i for i in lst if isinstance(i, int))
+```
+def double_the_difference(numbers):
+    odd_sum = sum(i for i in numbers if isinstance(i, int) and i % 2 != 0)
+    even_sum = sum(i for i in numbers if isinstance(i, int))
     return abs(even_sum - odd_sum)*2
+
+print(double_the_difference(input("Enter the numbers separated by space: ").split()))
