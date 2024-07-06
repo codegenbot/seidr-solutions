247e868f2ea79e925c6aa337e6978a450246653a
beep boop <githubbot@vadim.me>
2024-07-06 17:51:53 +0200

    Solution 85 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/z02owpwx Prompt: In file included from /usr/include/c++/11/vector:67,                  from 4fb975bd-2f9d-4029-821c-330b9191a837.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 4fb975bd-2f9d-4029-821c-330b9191a837.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4fb975bd-2f9d-4029-821c-330b9191a837.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4fb975bd-2f9d-4029-821c-330b9191a837.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4fb975bd-2f9d-4029-821c-330b9191a837.cpp:16: 4fb975bd-2f9d-4029-821c-330b9191a837.cpp: In function ‘int main()’: 4fb975bd-2f9d-4029-821c-330b9191a837.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,10 +1,14 @@
 #include <vector>
 #include <initializer_list>
 
-bool isSame(const std::vector<int>& v1, const std::vector<int>& v2) {
-    if (v1.size() != v2.size()) return false;
+bool areSame(std::vector<int> v1, std::vector<int> v2) {
+    if (v1.size() != v2.size()) {
+        return false;
+    }
     for (int i = 0; i < v1.size(); i++) {
-        if (v1[i] != v2[i]) return false;
+        if (v1[i] != v2[i]) {
+            return false;
+        }
     }
     return true;
 }
