d6f2357ce8dc119e059b4fa2e7480cacd8a17a66
beep boop <githubbot@vadim.me>
2024-07-07 00:51:09 +0200

    Solution 32 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/oh33d05g Prompt: Complete the following code given the task description and function signature.

--- Python_38.py
+++ Python_38.py
@@ -1,11 +1,43 @@
+Here is the completed code:
+
 def decode_cyclic(s: str):
-    result = []
-    i = 0
-    while i < len(s):
-        if i + 3 <= len(s):
-            group = s[i : i + 3]
-        else:
-            group = s[i:]
-        result.append(group[1:] + (group[0] if group else ""))
-        i += 3
-    return "".join(result)
+    result = ''
+    temp = ''
+    for char in s:
+        temp += char
+        if len(temp) == 3:
+            if temp[0] != temp[2]:
+                while True:
+                    if temp[0] == temp[1]:
+                        result += temp[0]
+                        temp = ''
+                        break
+                    elif temp[0] == temp[2]:
+                        result += temp[0]
+                        temp = ''
+                        break
+                    else:
+                        temp = temp[1:] + temp[0]
+            else:
+                result += temp[0]
+                temp = ''
+        if len(s) - 1 == i:
+            if len(temp) > 0:
+                if temp[0] != temp[2]:
+                    while True:
+                        if temp[0] == temp[1]:
+                            result += temp[0]
+                            temp = ''
+                            break
+                        elif temp[0] == temp[2]:
+                            result += temp[0]
+                            temp = ''
+                            break
+                        else:
+                            temp = temp[1:] + temp[0]
+                    if len(temp) > 0:
+                        result += temp[0]
+                else:
+                    result += temp[0]
+        i += 1
+    return result
