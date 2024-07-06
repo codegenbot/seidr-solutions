78c53adc47ca4f69e0feea084bc9839f1181cbaf
beep boop <githubbot@vadim.me>
2024-07-06 22:04:11 +0200

    Solution 32 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/n59dgzor Prompt:       |                    ^~~ 3fcafa37-c65b-4d10-9c97-7f37ba66ec17.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3fcafa37-c65b-4d10-9c97-7f37ba66ec17.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3fcafa37-c65b-4d10-9c97-7f37ba66ec17.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3fcafa37-c65b-4d10-9c97-7f37ba66ec17.cpp:17: 3fcafa37-c65b-4d10-9c97-7f37ba66ec17.cpp: In function ‘int main()’: 3fcafa37-c65b-4d10-9c97-7f37ba66ec17.cpp:27:20: error: ‘sort_array’ was not decl ared in this scope    27 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 3fcafa37-c65b-4d10-9c97-7f37ba66ec17.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,27 +1,20 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
 vector<int> sort_array(vector<int> arr) {
     sort(arr.begin(), arr.end(), [](int a, int b) {
-        if (count(bitset<32>(a).to_string().begin(), bitset<32>(a).to_string().end(), '1') <
-            count(bitset<32>(b).to_string().begin(), bitset<32>(b).to_string().end(), '1')) {
+        if (count(binary(a).begin(), binary(a).end(), '1') <
+            count(binary(b).begin(), binary(b).end(), '1')) {
             return true;
-        } else if (count(bitset<32>(a).to_string().begin(), bitset<32>(a).to_string().end(), '1') ==
-                   count(bitset<32>(b).to_string().begin(), bitset<32>(b).to_string().end(), '1')) {
+        } else if (count(binary(a).begin(), binary(a).end(), '1') ==
+                   count(binary(b).begin(), binary(b).end(), '1')) {
             return a < b;
         }
         return false;
     });
     return arr;
-}
-
-int main() {
-    assert(issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));
-    return 0;
 }
