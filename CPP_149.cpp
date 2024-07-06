294ad51ec31d0383187f668b35bfa7e6ee26759f
beep boop <githubbot@vadim.me>
2024-07-06 19:16:09 +0200

    Solution 84 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ccu534fs Prompt:    38 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1b90412e-7961-4c7f-8044-dc203739dc2f.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ 1b90412e-7961-4c7f-8044-dc203739dc2f.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 1b90412e-7961-4c7f-8044-dc203739dc2f.cpp:37: 1b90412e-7961-4c7f-8044-dc203739dc2f.cpp: In function ‘int main()’: 1b90412e-7961-4c7f-8044-dc203739dc2f.cpp:47:20: error: ‘sorted_list_sum’ was not  declared in this scope    47 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 1b90412e-7961-4c7f-8044-dc203739dc2f.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -7,36 +7,28 @@ bool issame(vector<string> a, vector<string> b) {
         return false;
     
     for(int i = 0; i < a.size(); i++) {
-        if(a[i].size() % 2 == 1 && b[i].size() % 2 == 0 || 
-           a[i].size() % 2 == 0 && b[i].size() % 2 == 1) 
+        if((a[i].size() % 2 == 1 && b[i].size() % 2 == 0) || 
+           (a[i].size() % 2 == 0 && b[i].size() % 2 == 1)) 
             return false;
     }
     
     return true;
 }
 
-int main() {
+int main_program() {
     int n;
+    vector<string> a, b;
     cin >> n;
-
-    vector<string> v1, v2;
-
     for(int i = 0; i < n; i++) {
         string s;
         cin >> s;
-        v1.push_back(s);
-    }
-
-    for(int i = 0; i < n; i++) {
-        string s;
-        cin >> s;
-        v2.push_back(s);
-    }
-
-    if(issame(v1, v2)) 
-        cout << "Same" << endl;
-    else
-        cout << "Not Same" << endl;
-
+        if(i % 2 == 0) {
+            a.push_back(s);
+        } else {
+            b.push_back(s);
+        }
+    
+    cout << (issame(a, b) ? "Same" : "Not Same");
+    
     return 0;
 }
