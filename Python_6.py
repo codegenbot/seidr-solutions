71263be37a7411f28023ade76b1249217b2a916b
beep boop <githubbot@vadim.me>
2024-07-06 14:31:53 +0200

    Solution 21 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3ou52l0b Prompt: [1, 2, 3, 2, 1, 0] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b3fab0a-7151-41e1-999c-fc1527a8a b23.py", line 19, in <module>     check(parse_nested_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b3fab0a-7151-41e1-999c-fc1527a8a b23.py", line 16, in check     assert parse_nested_parens("(()(())((())))") == [4]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_6.py
+++ Python_6.py
@@ -1,11 +1,11 @@
-def parse_nested_parens(paren_string: str) -> List[int]:
+def parse_nested_parens(paren_string: str) -> list[int]:
     result = []
     level = 0
+    levels = []
     for char in paren_string:
         if char == '(':
             level += 1
-            result.append(level)
+            levels.append(level)
         elif char == ')':
-            level -= 1
-            result.append(level)
-    return result
+            levels.pop()
+    return [levels[-1]] if levels else []
