ccd3a68be5e28c0d75587d1676889844e2f70078
beep boop <githubbot@vadim.me>
2024-07-06 02:25:52 +0200

    Solution 18 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ox8j8dw4 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 4526f72f-7f6a-4594-9a8a-180b1dd88712.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,41 +1,37 @@
+```cpp
 #include <vector>
 #include <algorithm>
+#include <initializer_list>
 
 bool issame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()) return false;
-    return std::equal(a.begin(), a.end(), b.begin());
+    if(a.size()!=b.size())
+        return false;
+    return std::equal(a.begin(),a.end(),b.begin());
 }
 
 vector<int> sort_third(vector<int> l) {
-    vector<int> result;
-    for(int i = 0; i < l.size(); i++) {
-        if(i % 3 == 0) {
-            vector<int> temp;
-            for(int j = 0; j < l.size(); j++) {
-                if(j % 3 == 0) {
-                    temp.push_back(l[j]);
-                }
-            }
-            sort(temp.begin(), temp.end());
-            int idx = 0;
-            for(int k = 0; k < temp.size(); k++) {
-                if(k % 3 == 0) {
-                    result.push_back(temp[k]);
-                    idx++;
-                }
-                if(idx > 1) break;
-            }
-        } else {
-            result.push_back(l[i]);
+    vector<int> result(l);
+    for (int i = 0; i < l.size(); i++) {
+        if (i % 3 == 1 || i % 3 == 2) {
+            std::partial_sort(&result[i], &result[i], &result.back());
         }
     }
     return result;
 }
 
 int main() {
-    // Test code
-    vector<int> l = {4, 2, 9, 6, 23, 12, 34, 11};
-    vector<int> res = sort_third(l);
-    for(int i : res) cout << i << " ";
+    vector<int> a = {10,40,70,20,50};
+    vector<int> b = {40,20,50,10,70};
+    if(issame(a,b))
+        cout<<"True";
+    else
+        cout<<"False";
+
+    vector<int> l = {1,2,3,4,5,6,7,8,9};
+    l = sort_third(l);
+    
+    for(int i=0;i<l.size();i++){
+        cout<<l[i]<<" ";
+    }
     return 0;
 }
