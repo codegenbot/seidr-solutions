b4e218bf1054c85fd439fe603968cfe9dd5961a8
beep boop <githubbot@vadim.me>
2024-07-07 01:42:05 +0200

    Solution 53 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dkhymkit Prompt: Complete the following code given the task description and function signature.

--- Python_84.py
+++ Python_84.py
@@ -1,3 +1,3 @@
+```
 def solve(N):
-    total_sum = sum(int(bit) for bit in bin(N)[2:].replace('0b', '')) 
-    return format(total_sum, 'b')
+    return bin(sum(int(i) for i in str(bin(N)[2:])))```
