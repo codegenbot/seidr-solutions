aaccbfe951028f7afdbcf8c4fbdb43f06009b59f
beep boop <githubbot@vadim.me>
2024-07-06 07:18:12 +0200

    Solution 77 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/42l0jg6c Prompt: Program did not receive expected input

--- Python_38.py
+++ Python_38.py
@@ -1,9 +1,6 @@
 def decode_cyclic(input_string: str) -> str:
     try:
-        groups = [
-            input_string[(3 * i) : min((3 * i + 3), len(input_string))]
-            for i in range((len(input_string) + 2) // 3)
-        ]
+        groups = [input_string[(3 * i):min((3 * i + 3), len(input_string))] for i in range((len(input_string) + 2) // 3)]
         return "".join([group[0] + group[1:] for group in groups])
     except:
         print("Invalid input format. Expected a string.")
