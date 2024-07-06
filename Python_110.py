f6213ab3c9503ce840f19b9513de8ff10acf64c9
beep boop <githubbot@vadim.me>
2024-07-06 09:56:53 +0200

    Solution 0 of Python/110, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/3ihapr3d Prompt: Complete the following code given the task description and function signature.

---
+++ Python_110.py
@@ -0,0 +1,10 @@
+
+def exchange(lst1, lst2):
+    even_nums = []
+    for num in lst1:
+        if num % 2 == 0:
+            even_nums.append(num)
+    for num in lst2:
+        if num not in even_nums:
+            return "NO"
+    return "YES"
