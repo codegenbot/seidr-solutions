c8a3119570dff96c5719f44cd3b5e2975f367067
beep boop <githubbot@vadim.me>
2024-07-06 16:29:01 +0200

    Solution 48 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/i0tpovve Prompt: ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | 7b1a743a-0084-4f97-8927-e4f48e39bd2f.cpp:19:68: error: ‘endl’ is not a member of  ‘std’    19 |    std::cout << smallest_change(std::vector<int>({0, 1})) << std::endl;       |                                                                   ^~~~ 7b1a743a-0084-4f97-8927-e4f48e39bd2f.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | 7b1a743a-0084-4f97-8927-e4f48e39bd2f.cpp: At global scope: 7b1a743a-0084-4f97-8927-e4f48e39bd2f.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ 7b1a743a-0084-4f97-8927-e4f48e39bd2f.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,5 +1,5 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 
 int smallest_change(std::vector<int> arr) {
     int left = 0, right = arr.size() - 1;
@@ -16,4 +16,6 @@ int smallest_change(std::vector<int> arr) {
     return changes;
 }
 
-std::cout << smallest_change(std::vector<int>({0, 1})) << std::endl;
+int main() {
+    std::cout << smallest_change(std::vector<int>({0, 1})) << std::endl;
+}
