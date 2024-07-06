d77e32228c9d8e5d79141198193134bc26d70f92
beep boop <githubbot@vadim.me>
2024-07-06 04:26:17 +0200

    Solution 36 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/kh7c2g1m Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4b4cac06-f3fe-4a65-8c8c-b06e36608 647.py", line 12, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4b4cac06-f3fe-4a65-8c8c-b06e36608 647.py", line 9, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: fruit_distribution() takes 1 positional argument but 2 were given

--- Python_67.py
+++ Python_67.py
@@ -1,11 +1,5 @@
-import re
 
-
-def fruit_distribution(s):
-    # Use a regular expression to extract the numbers from the input string
-    match = re.search(r"\d+", s)
-    if match:
-        apples, oranges = map(int, match.groups())
-        return apples + oranges
-    else:
-        return 0
+def fruit_distribution(s, total):
+    apples = int(re.search(r'\d+', s).group())
+    oranges = int(re.search(r'\d+', s).group())
+    return apples + oranges + total
