de2c8df808aaa04b6ee170833d3df75e27f080be
beep boop <githubbot@vadim.me>
2024-07-06 16:14:00 +0200

    Solution 15 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qb239rlh Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/183048cf-3f2f-4b3b-992d-c0bf50a89 f07.py", line 15, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/183048cf-3f2f-4b3b-992d-c0bf50a89 f07.py", line 12, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,12 +1,12 @@
 def fizz_buzz(n: int):
-    result = []
+    count = 0
     for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            result.append('FizzBuzz')
-        elif i % 3 == 0:
-            result.append('Fizz')
+        if i % 15 == 0:
+            count += 1
         elif i % 5 == 0:
-            result.append('Buzz')
+            pass
+        elif i % 3 == 0:
+            pass
         else:
-            result.append(str(i))
-    return '\n'.join(result)
+            pass
+    return count
