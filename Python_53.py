e554bfee07684483a0177973e457eb7b88c76c8b
beep boop <githubbot@vadim.me>
2024-07-06 19:27:05 +0200

    Solution 52 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jaxdufv2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/18d7da0c-e4c7-46fd-b589-a91da5482 4e3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,4 +1,3 @@
-```
 def add(x: int, y: int):
     return x + y
 
@@ -11,20 +10,17 @@ while True:
             elif x.replace('-', '',).replace('.', '').isnumeric():
                 x = int(x)
                 if -2**31 <= x <= 2**31 -1:
-                    while True:
-                        y = input("Enter the second number (or 'q' to quit): ")
-                        if y == "q":
-                            break
-                        elif y.replace('-', '',).replace('.', '').isnumeric():
-                            y = int(y)
-                            if -2**31 <= y <= 2**31 -1:
-                                print(f"The sum is {add(x, y)}")
-                                break
-                            else:
-                                print("Invalid input. Please enter a whole number.")
+                    y = input("Enter the second number (or 'q' to quit): ")
+                    if y == "q":
+                        break
+                    elif y.replace('-', '',).replace('.', '').isnumeric():
+                        y = int(y)
+                        if -2**31 <= y <= 2**31 -1:
+                            print(f"The sum is {add(x, y)}")
                         else:
                             print("Invalid input. Please enter a whole number.")
-                    break
+                    else:
+                        print("Invalid input. Please enter a whole number.")
                 else:
                     print("Invalid input. Please enter a whole number.")
             else:
