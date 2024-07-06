9d9529bc85dd0254ddc585ee660c977b25e69479
beep boop <githubbot@vadim.me>
2024-07-06 15:03:47 +0200

    Solution 9 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/491glfzb Prompt:    21 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 3c8e0c3c-6dfc-4252-a741-060097e74e4d.cpp:21:36: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 3c8e0c3c-6dfc-4252-a741-060097e74e4d.cpp:21:43: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 3c8e0c3c-6dfc-4252-a741-060097e74e4d.cpp:20: 3c8e0c3c-6dfc-4252-a741-060097e74e4d.cpp: In function ‘int main()’: 3c8e0c3c-6dfc-4252-a741-060097e74e4d.cpp:30:20: error: ‘sort_even’ was not decla red in this scope    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 3c8e0c3c-6dfc-4252-a741-060097e74e4d.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -6,36 +6,23 @@ bool issame(vector<float> a, vector<float> b) {
 }
 
 vector<float> sort_even(vector<float> l) {
-    vector<float> result;
-    
-    // separate even and odd numbers
-    vector<float> evens;
-    vector<float> odds;
-    for (float x : l) {
-        if (std::abs(x) % 2 == 0)
-            evens.push_back(x);
-        else
-            odds.push_back(x);
-    }
+    auto evenVals = l;
+    auto oddVals = l;
 
-    // sort the even numbers
-    std::sort(evens.begin(), evens.end());
+    // Sort all values
+    std::sort(evenVals.begin(), evenVals.end());
+    std::sort(oddVals.begin(), oddVals.end());
 
-    // alternate between even and odd numbers in the result
+    // Copy sorted even and odd values to result array
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            if (!evens.empty())
-                result.push_back(evens.front());
-            else
-                result.push_back(odds.front());
-            if (!evens.empty()) evens.pop_front();
-            if (!odds.empty()) odds.pop_front();
+            l[i] = evenVals[0];
+            evenVals.erase(evenVals.begin());
         } else {
-            if (!odds.empty())
-                result.push_back(odds.back());
-            if (!odds.empty()) odds.pop_back();
+            l[i] = oddVals[0];
+            oddVals.erase(oddVals.begin());
         }
     }
 
-    return result;
+    return l;
 }
