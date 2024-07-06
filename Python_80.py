b1ab7ae67c36a84af4aaf17c8394a2ec1573dd96
beep boop <githubbot@vadim.me>
2024-07-06 02:03:52 +0200

    Solution 3 of Python/80, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ow1vco5g Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e5e15a60-b985-4cd4-a6d5-44e64774d 3fc.py", line 14, in <module>     check(is_happy)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e5e15a60-b985-4cd4-a6d5-44e64774d 3fc.py", line 11, in check     assert is_happy("iopaxioi") == False, "iopaxioi"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: iopaxioi

--- Python_80.py
+++ Python_80.py
@@ -4,4 +4,6 @@ def is_happy(s):
     for i in range(0, len(s) - 2, 3):
         if len(set(s[i : i + 3])) != 3:
             return False
+    if len(set(s[-2:])) == 1 or len(set(s[-1:])) == 1:
+        return False
     return True
