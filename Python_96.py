b0d451e76880a6dfabb1ec1ba287b789366a6d73
beep boop <githubbot@vadim.me>
2024-07-06 10:12:28 +0200

    Solution 68 of Python/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/78684uxb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/46924ad2-2bed-497b-b4b6-f6da8728d 6bd.py", line 38, in <module>     check(count_up_to)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/46924ad2-2bed-497b-b4b6-f6da8728d 6bd.py", line 9, in check     assert count_up_to(101) == [            ^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_96.py
+++ Python_96.py
@@ -1,6 +1,7 @@
+
 def count_up_to(n):
     prime_numbers = []
-    for i in range(2, n + 1):
+    for i in range(2, n+1):
         if all(i % num != 0 for num in range(2, int(i**0.5) + 1)):
             prime_numbers.append(i)
     return prime_numbers
