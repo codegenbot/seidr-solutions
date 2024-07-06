f8e29ddf0a442d01ba8219f40d46920cdd9fe98f
beep boop <githubbot@vadim.me>
2024-07-06 19:28:36 +0200

    Solution 51 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/26zr7o11 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2bef3010-43b6-4fdf-a3c0-3dae0a8af f39.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -1,7 +1,6 @@
 def encode(input_string, output_string):
     result = ""
-    for i in range(len(input_string)):
-        char = input_string[i]
+    for char in input_string:
         if char.isalpha():
             ascii_value = ord(char.lower()) - 97
             if char.isupper():
