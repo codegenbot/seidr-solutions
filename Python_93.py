4016078c5af4f3d17dcfbf2546a110ec063054b5
beep boop <githubbot@vadim.me>
2024-07-06 19:33:15 +0200

    Solution 57 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/26zr7o11 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/92cdcc26-201b-40a9-9676-21a85007b f29.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -1,1 +1,16 @@
+def encode(text, table):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            ascii_val = ord(char.upper()) - 65
+            new_ascii_val = (ascii_val + int(table[0])) % 26
+            if char.islower():
+                result += chr(new_ascii_val + 97)
+            else:
+                result += chr(new_ascii_val + 65)
+        else:
+            result += char
+    return result
+
+
 print(encode("I DoNt KnOw WhAt tO WrItE", "k dQnT kNqW wHcT Tq wRkTg"))
