3d4174874e1fe9cd3f71e7e2bd41385210e46a28
beep boop <githubbot@vadim.me>
2024-07-06 22:05:42 +0200

    Solution 45 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/oj780jt4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf4cd8ce-5aaa-4ed7-a73a-6b550e836 986.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -1,8 +1,8 @@
 from typing import List
 
+
 def concatenate(strings: List[str]) -> str:
-    result = ''
-    for string in strings:
-        if string != '':
-            result += string
-    return result
+    if not strings:
+        return "No strings provided"
+    else:
+        return "".join(strings)
