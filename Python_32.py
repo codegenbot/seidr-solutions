56ad8fcbdf30884f13c5b640e1e3015d14ba133d
beep boop <githubbot@vadim.me>
2024-07-06 03:45:35 +0200

    Solution 43 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/zo3m29uf Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -1,8 +1,8 @@
 def find_zero(xs):
-    try:
-        for i in range(len(xs)):
-            if xs[i] == 0:
-                return i
+   try:
+       for i in range(len(xs)):
+           if xs[i] == 0:
+               return i
         return -1
     except IndexError:
         return None
