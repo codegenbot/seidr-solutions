0f83516c4b51fed7242952653da304c95ca6fbe6
beep boop <githubbot@vadim.me>
2024-07-06 14:45:03 +0200

    Solution 37 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3ou52l0b Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9f77388c-d55a-4db0-8c90-88b4d2e24 4fd.py", line 16, in <module>     check(parse_nested_parens)           ^^^^^^^^^^^^^^^^^^^ NameError: name 'parse_nested_parens' is not defined

--- Python_6.py
+++ Python_6.py
@@ -1,10 +1,11 @@
-result = []
-nested_levels = 0
-for char in input():
-    if char == "(":
-        nested_levels += 1
-        result.append(str(nested_levels))
-    elif char == ")":
-        nested_levels -= 1
-        result.append(str(nested_levels))
-print(result)
+def nested_levels(s):
+    result = []
+    nested_levels = 0
+    for char in s:
+        if char == "(":
+            nested_levels += 1
+            result.append(str(nested_levels))
+        elif char == ")":
+            nested_levels -= 1
+            result.append(str(nested_levels))
+    print(result)
