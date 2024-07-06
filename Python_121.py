334eaa0ff574350ab4786048a21e36358253608e
beep boop <githubbot@vadim.me>
2024-07-06 04:23:22 +0200

    Solution 94 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/23sd7ljf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8b080adc-d5fd-4161-b3e5-f39c52b4a 6a2.py", line 14, in <module>     check(solution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8b080adc-d5fd-4161-b3e5-f39c52b4a 6a2.py", line 9, in check     assert solution([3, 13, 2, 9]) == 3            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_121.py
+++ Python_121.py
@@ -1,23 +1,6 @@
-def wordLadder(inputString):
-    words = inputString.split(' ')
-    result = []
-    for i in range(1, len(words)):
-        if len(set(word[i-1]) ^ set(word[i])) == 1:
-            if len(result) < i - 1 or len(result[-1].get('words', [])) > 0:
-                result.append({'length': i, 'words': [word[0]]})
-            while True:
-                temp_words = word[i-1]
-                for j in range(len(word[i])):
-                    for k in string.ascii_lowercase:
-                        new_word = temp_words[:j] + k + temp_words[j+1:]
-                        if new_word not in result[-1]['words'] and new_word != word[i]:
-                            result.append({'length': i+1, 'words': result[-1]['words'] + [new_word]})
-                            break
-                    else:
-                        continue
-                    break
-                if len(result[-1]['words']) == i:
-                    break
-        elif len(set(word[i-1]) ^ set(word[i])) > 1 or i - 1 < 0:
-            return []
-    return result
+```
+def solution(lst):
+    if not isinstance(lst, list) or any(not isinstance(i, str) for i in lst):
+        return 0
+    total = sum(int(i) for i in lst[::] if i.isdigit() and int(i) > 1)
+    return total * int(lst[0]) if total else 0
