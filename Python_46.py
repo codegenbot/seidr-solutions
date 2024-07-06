96aa1e6d7581ce2b953baf13c443aa6933ce820d
beep boop <githubbot@vadim.me>
2024-07-06 08:59:34 +0200

    Solution 1 of Python/46, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/qkkr19jr Prompt: Complete the following code given the task description and function signature.

--- Python_46.py
+++ Python_46.py
@@ -1,11 +1,13 @@
 
-def fib4(n):
+def fib4(n: int):
     if n <= 3:
         return 0
     elif n == 4:
         return 2
     else:
-        a, b, c, d = 0, 0, 2, 0
+        a = 0
+        b = 0
+        c = 2
         for i in range(5, n+1):
-            a, b, c, d = b, c, d, a + b + c + d
-        return a
+            a, b, c = b, c, a + b + c
+        return c
