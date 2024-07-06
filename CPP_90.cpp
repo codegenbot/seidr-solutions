f04e35f0698ef52689022d08a9aa09b67b96de87
beep boop <githubbot@vadim.me>
2024-07-06 17:12:22 +0200

    Solution 80 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/a8fzqhi0 Prompt: ac17d596-0ffc-4f9f-96a5-13dcc4517145.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ ac17d596-0ffc-4f9f-96a5-13dcc4517145.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ ac17d596-0ffc-4f9f-96a5-13dcc4517145.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ ac17d596-0ffc-4f9f-96a5-13dcc4517145.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ In file included from ac17d596-0ffc-4f9f-96a5-13dcc4517145.cpp:15: ac17d596-0ffc-4f9f-96a5-13dcc4517145.cpp: In function ‘int main()’: ac17d596-0ffc-4f9f-96a5-13dcc4517145.cpp:17:13: error: ‘next_smallest’ was not d eclared in this scope    17 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ^~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,15 +1,15 @@
 int next_larger(std::vector<int> lst) {
-    int result = -1;
-    for(int i = 0; i < lst.size(); i++){
-        bool isLargerFound = false;
-        for(int j = i + 1; j < lst.size(); j++){
-            if(lst[j] > lst[i]){
-                isLargerFound = true;
-                result = lst[i];
+    if(lst.size() < 2) return -1; 
+    std::vector<int> result;
+    int max = -1;
+    for(int i = lst.size()-1; i >=0 ; i--){
+        for(int j = i-1; j>=0; j--) {
+            if (lst[j] > lst[i]) {
+                max = lst[i];
                 break;
             }
         }
-        if(isLargerFound) break;
+        if(max != -1) break;
     }
-    return result;
+    return max; 
 }
