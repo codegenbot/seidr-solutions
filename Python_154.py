9e37f7f4a83d6d96377c566d177ae3577cc12642
beep boop <githubbot@vadim.me>
2024-07-06 20:34:25 +0200

    Solution 92 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3f2yeli0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0021b528-1cea-4314-97a1-3bd50a065 8bc.py", line 25, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0021b528-1cea-4314-97a1-3bd50a065 8bc.py", line 22, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,5 +1,5 @@
 def cycpattern_check(a, b):
-    if not a.isnumeric() or not b.isnumeric():
+    if not a.isdigit() or not b.isdigit():
         return "Invalid input"
     total = int(a) + int(b)
     if total % 2 == 0:
