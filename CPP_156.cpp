d977d81c306e968f6497754d147db94f087d1fd1
beep boop <githubbot@vadim.me>
2024-07-06 14:55:41 +0200

    Solution 42 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/hjcrazap Prompt: x11::basic_string<char> >*; _ForwardIterator = std::pair<int, std::__cxx11::basi c_string<char> >*; _Tp = std::pair<int, std::__cxx11::basic_string<char> >]’ /usr/include/c++/11/bits/stl_vector.h:1585:33:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const std::pair<int, std::__cxx11::ba sic_string<char> >*; _Tp = std::pair<int, std::__cxx11::basic_string<char> >; _A lloc = std::allocator<std::pair<int, std::__cxx11::basic_string<char> > >]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = s td::pair<int, std::__cxx11::basic_string<char> >; _Alloc = std::allocator<std::p air<int, std::__cxx11::basic_string<char> > >; std::vector<_Tp, _Alloc>::allocat or_type = std::allocator<std::pair<int, std::__cxx11::basic_string<char> > >]’ b0d5dca8-d0a3-4c25-a43c-782ecbb09588.cpp:9:96:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,25 +1,3 @@
-#include <iostream>
-#include <string>
-#include <vector>
-using namespace std;
-
-string int_to_mini_roman(int num) {
-    vector<pair<int, string>> roman_map = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+vector<pair<int, const char*>> roman_map = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
                                             {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
                                             {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
-    string roman = "";
-    
-    for (const auto& pair : roman_map) {
-        while (num >= pair.first) {
-            num -= pair.first;
-            roman += pair.second;
-        }
-    }
-    
-    return roman;
-}
-
-int main() {
-    cout << int_to_mini_roman(1000) << endl;
-    return 0;
-}
