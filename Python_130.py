425f5b8bbd65e7e5f69d5929620c14105d7c9003
beep boop <githubbot@vadim.me>
2024-07-06 05:05:04 +0200

    Solution 12 of Python/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/n5cgh0lb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fcb153fb-84a7-4202-b33f-963fa8261 a17.py", line 15, in <module>     check(tri)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fcb153fb-84a7-4202-b33f-963fa8261 a17.py", line 12, in check     assert tri(1) == [1, 3]            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_130.py
+++ Python_130.py
@@ -1,11 +1,7 @@
-
 def tri(n):
-    if n == 0:
-        return [1]
-    elif n == 1:
-        return [3]
+    if n == 1:
+        return [1] * n
+    elif n % 2 == 0:
+        return [1, int(n / 2)]
     else:
-        # Handle case where n > 2
-        if n > 2:
-            result = [1, 3] + [result[i-1] + result[i-2] + result[i+1] for i in range(2, n+1)]
-        return result
+        return [1, tri(n - 1)[-1], tri(n - 2)[-1], tri(n + 1)[-1]]
