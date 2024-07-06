bca2037b7e152df4de62c3c8503df038a8c3d2cb
beep boop <githubbot@vadim.me>
2024-07-06 03:37:39 +0200

    Solution 80 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/h77vs2tk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/131b5655-7c12-40e9-9b26-6d23ee372 cc0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -1,11 +1,11 @@
-```
 from typing import List
 
 
 def concatenate(strings: List[str]) -> str:
+    strings = []
     while True:
         s = input("Enter a string (or 'stop' if finished): ")
         if s.lower() == "stop":
             break
         strings.append(s)
-    return ' '.join([s.strip() for s in strings])
+    return " ".join([s.strip() for s in strings])
