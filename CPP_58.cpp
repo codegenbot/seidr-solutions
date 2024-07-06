6b8719158af712fbb3c93dc670c82d1089ebb399
beep boop <githubbot@vadim.me>
2024-07-06 13:57:08 +0200

    Solution 16 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/g0smtz44 Prompt:       |         ^~~ e5504c44-fde0-4761-a167-350e3fae0c00.cpp:7:9: error: expected primary-expression  before ‘int’     7 |     set<int> s2(l2.begin(), l2.end());       |         ^~~ e5504c44-fde0-4761-a167-350e3fae0c00.cpp:9:9: error: expected primary-expression  before ‘int’     9 |     set<int> intersection;       |         ^~~ e5504c44-fde0-4761-a167-350e3fae0c00.cpp:10:22: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    10 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                      ^~       |                      l1 e5504c44-fde0-4761-a167-350e3fae0c00.cpp:10:44: error: ‘s2’ was not declared in this scope; did you mean ‘l2’?    10 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                                            ^~       |                                            l2 e5504c44-fde0-4761-a167-350e3fae0c00.cpp:11:33: error: ‘intersection’ was not de clared in this scope    11 |                        inserter(intersection, intersection.begin()));       |                                 ^~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
-    set<int> intersection;
+    set<int> s2;
+    for(int x : l2) {s2.insert(x);}
     
-    set_intersection(s1.begin(), s1.end(), l2.begin(), l2.end(),
-                      inserter(intersection, intersection.begin()));
+    set<int> intersection;
+    set_difference(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                       inserter(intersection, intersection.begin()));
     
     vector<int> result(intersection.begin(), intersection.end());
     return result;
