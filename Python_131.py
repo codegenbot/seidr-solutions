ed08f1745a343b41804bf9350e82c294c85b3949
beep boop <githubbot@vadim.me>
2024-07-06 03:44:04 +0200

    Solution 80 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/kwapk43d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8ebd6afd-0afa-4808-af58-25529a631 d4f.py", line 12, in <module>     check(digits)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8ebd6afd-0afa-4808-af58-25529a631 d4f.py", line 9, in check     assert digits(2468) == 0            ^^^^^^^^^^^^^^^^^ AssertionError

--- Python_131.py
+++ Python_131.py
@@ -1,5 +1,6 @@
+
 def digits(n):
     if n < 0:
-        raise ValueError("Invalid input: must be a positive integer")
+        return 0
     else:
         return max([int(digit) for digit in str(n)])
