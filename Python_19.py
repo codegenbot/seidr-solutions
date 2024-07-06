2edcda3822331044a831114cbe6c9d2f5b9998c2
beep boop <githubbot@vadim.me>
2024-07-06 11:25:38 +0200

    Solution 38 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/05osbftj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/50a1fa9e-0b69-4275-b48f-8c12e4601 9fe.py", line 25, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/50a1fa9e-0b69-4275-b48f-8c12e4601 9fe.py", line 20, in check     sort_numbers("six five four three two one zero") AssertionError

--- Python_19.py
+++ Python_19.py
@@ -1,17 +1,5 @@
 
 def sort_numbers(numbers: str) -> str:
-    number_map = {
-        "zero": 0,
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9,
-    }
-    numbers = [int(number_map[num]) for num in numbers.strip().split()]
-    sorted_numbers = sorted(numbers)
-    return " ".join([str(num) for num in sorted_numbers])
+    number_list = numbers.split()
+    sorted_numbers = sorted(number_list)
+    return " ".join(sorted_numbers)
