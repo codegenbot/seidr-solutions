0316d0df3009b016a6b1d095db5473d603c91852
beep boop <githubbot@vadim.me>
2024-07-06 12:07:32 +0200

    Solution 25 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vcou2e8s Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/01d1bb09-94f6-4d8f-a632-655704392 42a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -6,9 +6,7 @@ def minPath(grid, k):
     while queue:
         row, col, path = queue.pop(0)
         if len(path) == k:
-            if not min_path or sum(int(x) for x in path) < sum(
-                int(x) for x in min_path
-            ):
+            if not min_path or "".join(path) < "".join(min_path):
                 min_path = path
         else:
             for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
