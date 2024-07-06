a055e10a885edf8ed6d015fa1cf80d195173df09
beep boop <githubbot@vadim.me>
2024-07-06 10:51:05 +0200

    Solution 44 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ov14dqn5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5cffb436-7ba5-44d3-add4-cdd2e983e 3e5.py", line 21, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5cffb436-7ba5-44d3-add4-cdd2e983e 3e5.py", line 18, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ AssertionError

--- Python_1.py
+++ Python_1.py
@@ -9,4 +9,8 @@ def separate_paren_groups(paren_string):
                 stack.pop()
             else:
                 result.append(c)
+    # Check for unmatched parentheses
+    if len(stack) != 0:
+        return None
+    # Return the result list with all complete parenthesis groups
     return [group for group in result if group[0] == "(" and group[-1] == ")"]
