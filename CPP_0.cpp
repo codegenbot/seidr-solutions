12de1f6475e4f1f4d3b47e096440936e4891e4df
beep boop <githubbot@vadim.me>
2024-07-06 12:59:17 +0200

    Solution 69 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/oum7agu8 Prompt: 70dbff0d-f7f0-4c3b-a405-0bc8a9cf4f3d.cpp:6:22: error: ‘abs’ is not a member of ‘ std’     6 |             if (std::abs(numbers[i] - numbers[j]) <= threshold)       |                      ^~~ 70dbff0d-f7f0-4c3b-a405-0bc8a9cf4f3d.cpp: In function ‘int main()’: 70dbff0d-f7f0-4c3b-a405-0bc8a9cf4f3d.cpp:15:5: error: ‘vector’ was not declared in this scope    15 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 70dbff0d-f7f0-4c3b-a405-0bc8a9cf4f3d.cpp:15:5: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 70dbff0d-f7f0-4c3b-a405-0bc8a9cf4f3d.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 70dbff0d-f7f0-4c3b-a405-0bc8a9cf4f3d.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 70dbff0d-f7f0-4c3b-a405-0bc8a9cf4f3d.cpp:15:12: error: expected primary-expressi on before ‘float’    15 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -14,9 +14,10 @@ bool has_close_elements(const std::vector<float>& numbers, float threshold) {
 
 int main() {
     std::vector<float> a = {1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
-    if (has_close_elements(a, 0.1f)) {
-        std::cout << "The numbers contain close elements." << std::endl;
+    if(has_close_elements(a, 0.1f)) {
+        std::cout << "The array contains close elements." << std::endl;
     } else {
-        std::cout << "The numbers do not contain close elements." << std::endl;
+        std::cout << "The array does not contain close elements." << std::endl;
     }
+    return 0;
 }
