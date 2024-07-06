89c2fc40e3c64d1819facb8f0007300919b7df27
beep boop <githubbot@vadim.me>
2024-07-06 13:52:02 +0200

    Solution 23 of CPP/48, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/k8yhriwg Prompt: 25b6d5e7-3287-482e-8fdd-bd9adc09ac69.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ 25b6d5e7-3287-482e-8fdd-bd9adc09ac69.cpp:3:1: error: expected unqualified-id bef ore ‘for’     3 | for(int i=0; i<len/2; i++){       | ^~~ 25b6d5e7-3287-482e-8fdd-bd9adc09ac69.cpp:3:14: error: ‘i’ does not name a type     3 | for(int i=0; i<len/2; i++){       |              ^ 25b6d5e7-3287-482e-8fdd-bd9adc09ac69.cpp:3:23: error: ‘i’ does not name a type     3 | for(int i=0; i<len/2; i++){       |                       ^ 25b6d5e7-3287-482e-8fdd-bd9adc09ac69.cpp:7:1: error: expected unqualified-id bef ore ‘return’     7 | return true;```       | ^~~~~~ In file included from 25b6d5e7-3287-482e-8fdd-bd9adc09ac69.cpp:9: 25b6d5e7-3287-482e-8fdd-bd9adc09ac69.cpp: In function ‘int main()’: 25b6d5e7-3287-482e-8fdd-bd9adc09ac69.cpp:11:13: error: ‘is_palindrome’ was not d eclared in this scope    11 |     assert (is_palindrome("xywzx") == false);       |             ^~~~~~~~~~~~~

--- CPP_48.cpp
+++ CPP_48.cpp
@@ -8,17 +8,18 @@ bool is_palindrome(const std::string& text) {
             return false;
     }
     return true;
+
 }
 
 int main() {
     assert(is_palindrome("xywzx") == false);
-    std::string input;
+    std::string str;
     std::cout << "Enter a string: ";
-    std::cin >> input;
-    if (is_palindrome(input))
-        std::cout << "The entered string is palindrome.";
-    else
-        std::cout << "The entered string is not palindrome.";
-    
+    std::getline(std::cin, str);
+    if (is_palindrome(str)) {
+        std::cout << str << " is a palindrome.\n";
+    } else {
+        std::cout << str << " is not a palindrome.\n";
+    }
     return 0;
 }
