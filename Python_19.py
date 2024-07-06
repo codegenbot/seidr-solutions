79c00eac45ab4e578f828b7809c4c4c54ce91d17
beep boop <githubbot@vadim.me>
2024-07-06 11:23:41 +0200

    Solution 34 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/05osbftj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bc559564-b482-497a-a5ef-bef41cc98 b9a.py", line 24, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bc559564-b482-497a-a5ef-bef41cc98 b9a.py", line 19, in check     sort_numbers("six five four three two one zero") AssertionError

--- Python_19.py
+++ Python_19.py
@@ -1,7 +1,19 @@
 import re
 
 
-def sort_numbers(numbers: str) -> str:
-    numbers = [int(re.search(r"\d+", num).group()) for num in numbers.split()]
+def sort_numbers(numbers):
+    number_map = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+    numbers = [int(number_map[num]) for num in re.findall(r"\d+", numbers)]
     sorted_numbers = sorted(numbers)
     return " ".join([str(num) for num in sorted_numbers])
