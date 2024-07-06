5ba0d6c28052b361f6f83d7fd56c231a908bd9db
beep boop <githubbot@vadim.me>
2024-07-06 04:19:42 +0200

    Solution 90 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/23sd7ljf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9f82838d-b3aa-401e-89fb-f315d5536 3d1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,3 +1,8 @@
-def solution(lst):
-    total = sum(int(i) for i in lst if int(i) > 1)
-    return total * int(lst[0]) if total else 0
+```
+def beautifulDays(i, j, k):
+    count = 0
+    for day in range(int(i), int(j)+1):
+        rev_day = str(day)[::-1]
+        if abs(int(day) - int(rev_day)) <= k:
+            count += 1
+    return count
