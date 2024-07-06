be4bce5c2331af6bee9099ef7e0089cb91d76df2
beep boop <githubbot@vadim.me>
2024-07-06 03:55:14 +0200

    Solution 15 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6didusaq Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 0e520e42-a13c-4045-bc02-00208b590a71.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -7,8 +7,7 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
 
 std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
     std::vector<std::string> letter_grades;
-    for (int i = 0; i < grades.size(); i++) {
-        float grade = grades[i];
+    for (float grade : grades) {
         std::string letter_grade;
         if (grade >= 4.0) {
             letter_grade = "A+";
@@ -35,7 +34,7 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
         } else if (grade > 0.0) {
             letter_grade = "D-";
         } else {
-            letter_grade = "E";
+            letter_grade = "F";
         }
         letter_grades.push_back(letter_grade);
     }
