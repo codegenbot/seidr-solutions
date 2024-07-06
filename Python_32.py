300f848437cbebffa5a58ca3eaa9b43565c59b1a
beep boop <githubbot@vadim.me>
2024-07-06 06:28:04 +0200

    Solution 13 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/z2r9fz9l Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,5 +1,8 @@
 def find_zero(xs: list):
-    if len(xs) % 2 == 0 and xs[0]:
-        for i in range(len(xs)):
-            if xs[i] != 0:
-                return -xs[i-1] / (2 * xs[i])
+    if len(xs) % 2 == 0 and xs[-1] != 0:
+        return None
+    for i in range(len(xs)):
+        x = -xs[i] / xs[i + 1]
+        if poly(xs, x) < 0.0001:
+            return x
+    return None
