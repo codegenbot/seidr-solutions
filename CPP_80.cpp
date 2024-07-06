d785e110f04fd23930fa8bc9e253381d38dd273f
beep boop <githubbot@vadim.me>
2024-07-06 15:26:19 +0200

    Solution 24 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yl3wmkze Prompt: 8eb7e691-0788-4271-9ae6-a097bee0b63d.cpp:1:15: error: ‘string’ was not declared in this scope     1 | bool is_happy(string s) {       |               ^~~~~~ In file included from 8eb7e691-0788-4271-9ae6-a097bee0b63d.cpp:19: 8eb7e691-0788-4271-9ae6-a097bee0b63d.cpp: In function ‘int main()’: 8eb7e691-0788-4271-9ae6-a097bee0b63d.cpp:21:21: error: ‘is_happy’ cannot be used  as a function    21 |     assert (is_happy("iopaxioi") == false );       |             ~~~~~~~~^~~~~~~~~~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,33 +1,26 @@
-#include <iostream>
 #include <string>
-
-using namespace std;
-
-bool is_happy(string s) {
-    int n = s.length();
-    if(n < 3)
-        return false;
-    for(int i=0; i<=n-3; i++){
-        bool unique = true;
-        string sub = s.substr(i, 3);
-        for(char c : sub){
-            if(find(sub.begin(), sub.end(), c) == sub.end())
-                unique = false;
-        }
-        if(unique)
-            return true;
-    }
-    return false;
-}
+#include <algorithm>
 
 int main() {
-    string str;
-    cout << "Enter a string: ";
-    cin >> str;
-    if(is_happy(str))
-        cout << "Happy" << endl;
-    else
-        cout << "Not Happy" << endl;
-    
+    bool is_happy(string s) {
+        int n = s.length();
+        if(n < 3)
+            return false;
+        for(int i=0; i<=n-3; i++){
+            bool unique = true;
+            string sub = s.substr(i, 3);
+            for(char c : sub){
+                if(find(sub.begin(), sub.end(), c) == sub.end())
+                    unique = false;
+                    break;
+            }
+            if(unique)
+                return true;
+        }
+        return false;
+    }
+
+    assert (is_happy("iopaxioi") == false );
+
     return 0;
 }
