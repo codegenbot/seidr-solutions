2d01f72705c7b2df6689324867d7496e6e467eda
beep boop <githubbot@vadim.me>
2024-07-06 03:05:03 +0200

    Solution 39 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/92jdlk6h Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0706c257-1f11-440c-9ec6-2020cc4b5 6bd.py", line 5     +    if x + y > sys.maxsize:          ^^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -2,7 +2,6 @@ def add(x: int or float, y: int or float):
     # Check if inputs are valid
     if not (isinstance(x, int) and isinstance(y, int)):
         raise ValueError("Inputs must be integers")
-
     if x + y > sys.maxsize:
         raise OverflowError()
 
