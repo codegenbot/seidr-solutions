9427ba6e3bc9929d87bf03dedd5d64e7e47132c2
beep boop <githubbot@vadim.me>
2024-07-07 01:43:04 +0200

    Solution 5 of Python/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l7wi4gin Prompt: Complete the following code given the task description and function signature.

--- Python_106.py
+++ Python_106.py
@@ -8,6 +8,8 @@ def f(n):
             result.append(sum(range(1, i+1)))
     return result
 
-import math
 def factorial(x):
-    return math.factorial(x)
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return x * factorial(x-1)
