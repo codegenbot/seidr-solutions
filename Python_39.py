871e327ef568a7bc069fed7f0a608c0229e7bce7
beep boop <githubbot@vadim.me>
2024-07-06 07:40:28 +0200

    Solution 74 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/oadsq5yd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5d2f4329-527a-4f81-ab77-0b92e1c77 f34.py", line 13, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5d2f4329-527a-4f81-ab77-0b92e1c77 f34.py", line 10, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_39.py
+++ Python_39.py
@@ -1,16 +1,17 @@
+
 def prime_fib(n):
     def is_prime(num):
         if num < 2:
             return False
-        for i in range(2, int(num**0.5) + 1):
+        for i in range(2, int(num ** 0.5) + 1):
             if num % i == 0:
                 return False
         return True
-
+    
     fib = [0, 1]
     i = 2
     while len(fib) <= n:
-        fib.append(fib[i - 1] + fib[i - 2])
+        fib.append(fib[i-1] + fib[i-2])
         if is_prime(fib[-1]):
             i += 1
     return fib[-1]
