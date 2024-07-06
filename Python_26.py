6043d5ea117f89f2ddf5636129fef052697a53b6
beep boop <githubbot@vadim.me>
2024-07-06 02:10:32 +0200

    Solution 29 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/al4be52p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4228bd8e-7bbb-44c6-b159-c55b60d46 676.py", line 8, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4228bd8e-7bbb-44c6-b159-c55b60d46 676.py", line 5, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: remove_duplicates() takes 0 positional arguments but 1 was given

--- Python_26.py
+++ Python_26.py
@@ -1,4 +1,3 @@
-```
-def remove_duplicates(input_str=""):
+def remove_duplicates(input_str=""): 
     numbers = input(input_str + "Enter comma-separated numbers: ") or input_str
     return list(set(map(int, numbers.split(","))))
