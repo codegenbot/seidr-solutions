d6aff12966967d610e20c5c523126d036920be14
beep boop <githubbot@vadim.me>
2024-07-06 03:25:02 +0200

    Solution 24 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/61mfjeit Prompt: There is a problem with the function Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9fb309cb-1343-40fe-9ebc-3c62a22b7 8d0.py", line 20, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9fb309cb-1343-40fe-9ebc-3c62a22b7 8d0.py", line 16, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,5 +1,8 @@
 ```
-if double_the_difference([1, 2, 3]) == 18:
-    print("The function works correctly")
-else:
-    print("There is a problem with the function")
+def double_the_difference(lst):
+    odd_sum = sum(i**2 for i in lst if isinstance(i, int) and i % 2 != 0)
+    return odd_sum*2
+
+
+input_list = [1, 2, 3]
+print(double_the_difference(input_list))
