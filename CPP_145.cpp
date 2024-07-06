0e7ac30dc7c3e4520edbc3a2418e4c856f7de279
beep boop <githubbot@vadim.me>
2024-07-06 13:41:51 +0200

    Solution 86 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6h0099y8 Prompt: In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from f717cf6f-b0f3-4d5f-8e35-67f613654628.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ f717cf6f-b0f3-4d5f-8e35-67f613654628.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f717cf6f-b0f3-4d5f-8e35-67f613654628.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f717cf6f-b0f3-4d5f-8e35-67f613654628.cpp:10: f717cf6f-b0f3-4d5f-8e35-67f613654628.cpp: In function ‘int main()’: f717cf6f-b0f3-4d5f-8e35-67f613654628.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,8 +1,12 @@
 #include <algorithm>
 #include <vector>
 
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
+}
+
 std::vector<int> order_by_points(std::vector<int> nums) {
     std::vector<int> res = nums;
-    std::sort(res.begin(), res.end());
+    sort(res.begin(), res.end());
     return res;
 }
