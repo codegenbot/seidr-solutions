c3cca5d7a46edf71c7e5d3245565455c7fc00dd8
beep boop <githubbot@vadim.me>
2024-07-06 18:10:17 +0200

    Solution 10 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/rhtz8oce Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1cca3930-1798-498c-b159-142b99bba7d3.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1cca3930-1798-498c-b159-142b99bba7d3.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1cca3930-1798-498c-b159-142b99bba7d3.cpp:16: 1cca3930-1798-498c-b159-142b99bba7d3.cpp: In function ‘int main()’: 1cca3930-1798-498c-b159-142b99bba7d3.cpp:26:20: error: ‘largest_smallest_integer s’ was not declared in this scope    26 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 1cca3930-1798-498c-b159-142b99bba7d3.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,19 +1,6 @@
-```cpp
-#include <vector>
-#include <algorithm>
-
-bool same(vector<int> a,vector<int> b){
-    return (a.size() == b.size()) && (std::all_of(a.begin(), a.end(), 
-      [&b](int x){return std::find(b.begin(), b.end(), x) != b.end();}));
-}
-
-int main(){
-    assert(same(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
-    return 0;
-}
-
 vector<int> largest_smallest_integers(vector<int> lst) {
     int max_negative = 0, min_positive = INT_MAX;
+    vector<int> result;
     for (int num : lst) {
         if (num < 0 && num > max_negative) {
             max_negative = num;
