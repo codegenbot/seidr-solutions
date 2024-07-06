0b62cb89139601c31ef116450fead29b65c8e7a6
beep boop <githubbot@vadim.me>
2024-07-06 08:25:12 +0200

    Solution 52 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9vokdxzg Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -1,6 +1,3 @@
-```Python
+```
 def add(x: int, y: int):
-    x = int(input("Enter the first number: "))
-    return x + int(input("Enter the second number: "))
-
-print(add(0, 0))
+    return x + y
