143c2ba793d1598a256acc5d40ad586688004284
beep boop <githubbot@vadim.me>
2024-07-06 18:18:12 +0200

    Solution 5 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ccu534fs Prompt:    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d817bfe5-add1-48d1-bb63-f99f7d8af689.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d817bfe5-add1-48d1-bb63-f99f7d8af689.cpp:21:45: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d817bfe5-add1-48d1-bb63-f99f7d8af689.cpp:20: d817bfe5-add1-48d1-bb63-f99f7d8af689.cpp: In function ‘int main()’: d817bfe5-add1-48d1-bb63-f99f7d8af689.cpp:30:20: error: ‘sorted_list_sum’ was not  declared in this scope    30 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ d817bfe5-add1-48d1-bb63-f99f7d8af689.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,23 +1,23 @@
 #include <algorithm>
-#include <vector>
-#include <string>
+#include <assert.h>
 
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size())
+    if(a.size() != b.size()) 
         return false;
-    
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i].size() % 2 == 1 && b[i].size() % 2 == 0 || 
-           a[i].size() % 2 == 0 && b[i].size() % 2 == 1) 
-            return false;
+    else {
+        for(int i = 0; i < a.size(); ++i) {
+            if(a[i].size() % 2 == 1 && b[i].size() % 2 == 0 || a[i].size() % 2 == 0 && b[i].size() % 2 == 1)
+                return false;
+        }
     }
-    
     return true;
 }
 
 vector<string> vector_sort(vector<string> lst) {
     auto it = unique(lst.begin(), lst.end(),
-        [](const string& a, const string& b){ return (a.size() % 2 == 1 && b.size() % 2 == 0) || (a.size() % 2 == 0 && b.size() % 2 == 1); });
+        [](const string& a, const string& b){ 
+            return (a.size() % 2 == 1 && b.size() % 2 == 0) || (a.size() % 2 == 0 && b.size() % 2 == 1);
+    });
     lst.erase(unique(it, lst.end()), lst.end());
     
     sort(lst.begin(), lst.end(),
