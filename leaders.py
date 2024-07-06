9a3bf0a1b4ce54861ed97202c4a91feb9f0162d7
beep boop <githubbot@vadim.me>
2024-07-06 02:57:24 +0200

    Solution 14 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/96bhn0nl Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/67e5e35f-2394-44d5-8407-0a5fe5e73 b38.py", line 11     [/PYTHON]      ^ SyntaxError: invalid syntax

--- leaders.py
+++ leaders.py
@@ -3,6 +3,5 @@ def leaders(vector):
     current_leader = vector[-1]
     for i in range(len(vector) - 2, -1, -1):
         if vector[i] >= current_leader:
-            result.append(current_leader)
             current_leader = vector[i]
-    return [vector[-1]] + result
+    return [current_leader] + result
