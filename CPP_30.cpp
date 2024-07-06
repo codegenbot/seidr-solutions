fc28bc5a4852b7200899f11193f4746c5dc62b86
beep boop <githubbot@vadim.me>
2024-07-06 03:37:25 +0200

    Solution 87 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/p3so980n Prompt:       |             ^~~~~~ e28dd1fe-1a6d-4593-9ed4-f5e0dc5fd5bb.cpp:49:36: error: expected primary-expressi on before ‘float’    49 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ e28dd1fe-1a6d-4593-9ed4-f5e0dc5fd5bb.cpp:57:5: error: redefinition of ‘int main( )’    57 | int main(){       |     ^~~~ e28dd1fe-1a6d-4593-9ed4-f5e0dc5fd5bb.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~ In file included from e28dd1fe-1a6d-4593-9ed4-f5e0dc5fd5bb.cpp:48: e28dd1fe-1a6d-4593-9ed4-f5e0dc5fd5bb.cpp: In function ‘int main()’: e28dd1fe-1a6d-4593-9ed4-f5e0dc5fd5bb.cpp:58:32: error: too few arguments to func tion ‘std::vector<float> get_positive(std::vector<float>, std::vector<float>)’    58 |     assert (issame(get_positive({}) , {}));       |                    ~~~~~~~~~~~~^~~~ e28dd1fe-1a6d-4593-9ed4-f5e0dc5fd5bb.cpp:18:20: note: declared here    18 | std::vector<float> get_positive(std::vector<float> l, std::vector<float>  v) {       |                    ^~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,3 +1,4 @@
+```
 #include <initializer_list>
 #include <vector>
 #include <iostream>
@@ -29,17 +30,15 @@ std::vector<float> get_positive(std::vector<float> l, std::vector<float> v) {
     return result;
 }
 
-int main() {
-    std::vector<float> vec1 = {1, 2, 3}; 
-    std::vector<float> vec2 = {-4, -5, 6};
-    if (issame(vec1,vec2)) {
-        std::cout << "Vectors are same.\n";
-    } else {
-        std::cout << "Vectors are not same.\n";
-    }
-    std::cout << "Only positive numbers are: ";
-    for (float num : get_positive(vec1,vec2)) {
-        std::cout << num << " ";
-    }
-    std::cout << std::endl;
+std::vector<float> vec1 = {1, 2, 3}; 
+std::vector<float> vec2 = {-4, -5, 6};
+if (issame(vec1,vec2)) {
+    std::cout << "Vectors are same.\n";
+} else {
+    std::cout << "Vectors are not same.\n";
 }
+std::cout << "Only positive numbers are: ";
+for (float num : get_positive(vec1,vec2)) {
+    std::cout << num << " ";
+}
+std::cout << std::endl;
