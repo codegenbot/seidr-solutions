862d8dde66404d8cee792840a8603457aa5c5eb1
beep boop <githubbot@vadim.me>
2024-07-06 05:25:59 +0200

    Solution 80 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r66k6rhr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/232258f8-5d47-41c7-b0f1-0a525237f d4a.py", line 14, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/232258f8-5d47-41c7-b0f1-0a525237f d4a.py", line 11, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,9 +1,9 @@
+```
 def make_palindrome(string: str) -> str:
     reverse = string[::-1]
-    new_string = ""
+    new_string = string
     for i in range(len(string)):
-        if len(string) % 2 == 0 or i < len(string) // 2:
-            new_string += string[i]
-        else:
-            new_string += string[i] + reverse[len(reverse) - 1 - i]
+        if i == len(string) // 2 and len(string) % 2 != 0:
+            continue
+        new_string += reverse[len(reverse) - 1 - i]
     return new_string
