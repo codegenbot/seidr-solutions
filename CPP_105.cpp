06141dbf556a059568c215fdd78f2604dd97bb15
beep boop <githubbot@vadim.me>
2024-07-06 21:09:53 +0200

    Solution 15 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/za61nfn8 Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,20 +1,24 @@
+#include <string>
+#include <map>
+
 vector<string> by_length(vector<int> arr) {
-    vector<int> numArr;
-    for (int num : arr) {
-        if (num >= 1 && num <= 9) {
-            numArr.push_back(num);
+    vector<int> toSort;
+    for (int i : arr) {
+        if (i >= 1 && i <= 9) {
+            toSort.push_back(i);
         }
     }
-
-    sort(numArr.begin(), numArr.end());
-    reverse(numArr.begin(), numArr.end());
-
+    
+    sort(toSort.begin(), toSort.end());
+    reverse(toSort.begin(), toSort.end());
+    
     vector<string> result;
-    map<int, string> numName = {{1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
-                                 {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-    for (int num : numArr) {
-        result.push_back(numName[num]);
+    map<int, string> intToName = {{1, "One"}, {2, "Two"}, {3, "Three"}, 
+                                   {4, "Four"}, {5, "Five"}, {6, "Six"},
+                                   {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
+    for (int i : toSort) {
+        result.push_back(intToName[i]);
     }
-
+    
     return result;
 }
