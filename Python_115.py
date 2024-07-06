787427b7586558f76e3a3894d66764b608ca4768
beep boop <githubbot@vadim.me>
2024-07-06 08:27:58 +0200

    Solution 68 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ve9f25ft Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48dd62cd-9c97-4ee4-9a77-8fe3bddd5 553.py", line 30, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48dd62cd-9c97-4ee4-9a77-8fe3bddd5 553.py", line 27, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_115.py
+++ Python_115.py
@@ -5,15 +5,10 @@ def max_fill(grid, capacity):
 
     count = 0
 
-    while True:
-        can_fill = False
-        for i in range(rows):
-            for j in range(cols):
-                if water[i][j] < capacity and grid[i][j] == 1:
-                    water[i][j] += 1
-                    count += 1
-                    can_fill = True
-        if not can_fill:
-            break
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1 and water[i][j] >= capacity:
+                water[i][j] += 1
+                count += 1
 
     return count
