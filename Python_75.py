34339044d0d07049d05ac204eb5fcc53cb582bd1
beep boop <githubbot@vadim.me>
2024-07-06 07:09:53 +0200

    Solution 26 of Python/75, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/3jau7312 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e97d5e4-c65c-44d5-8113-45a7b8c2b 84b.py", line 13, in <module>     check(is_multiply_prime)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e97d5e4-c65c-44d5-8113-45a7b8c2b 84b.py", line 10, in check     assert is_multiply_prime(11 * 13 * 7) == True            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e97d5e4-c65c-44d5-8113-45a7b8c2b 84b.py", line 6, in is_multiply_prime     if a % i == 0 and is_prime(i) and is_prime(a // i):                       ^^^^^^^^ NameError: name 'is_prime' is not defined

--- Python_75.py
+++ Python_75.py
@@ -1,3 +1,4 @@
+
 def is_multiply_prime(a):
     if a < 1:
         return False
@@ -5,10 +6,4 @@ def is_multiply_prime(a):
         if a % i == 0 and is_prime(i) and is_prime(a // i):
             return True
     return False
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+```
