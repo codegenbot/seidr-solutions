ccc7acc4fbac5fa39da2d8c63a66ceece7c85077
beep boop <githubbot@vadim.me>
2024-07-07 00:09:24 +0200

    Solution 72 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xypmlusu Prompt: 7d8e6c2d-eded-4862-ac3b-aa44a867fc4f.cpp:1:10: fatal error: digest.h: No such fi le or directory     1 | #include <digest.h>       |          ^~~~~~~~~~ compilation terminated.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,12 +1,10 @@
 #include <openssl/md5.h>
-#include <sstream>
-#include <string>
 
-std::string string_to_md5(std::string text) {
+string string_to_md5(string text) {
     if (text.empty()) return "";
     unsigned char md5[MD5_DIGEST_LENGTH];
     MD5((unsigned char*)text.c_str(), text.size(), md5);
-    std::stringstream ss;
+    stringstream ss;
     for(int i = 0 ; i < MD5_DIGEST_LENGTH; i++) {
         ss << setfill('0') << setw(2) << hex << (int)md5[i];
     }
