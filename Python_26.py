01a7915c81a10ebb7e3f812e3e142dde312ee63a
beep boop <githubbot@vadim.me>
2024-07-06 03:09:25 +0200

    Solution 96 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/al4be52p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b2893281-a8ff-41a7-a38c-19a2a2b80 704.py", line 11, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b2893281-a8ff-41a7-a38c-19a2a2b80 704.py", line 8, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b2893281-a8ff-41a7-a38c-19a2a2b80 704.py", line 6, in remove_duplicates     return list(map(int, set(numbers.replace(" ", "").split(","))))            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: '5]'

--- Python_26.py
+++ Python_26.py
@@ -1,7 +1,6 @@
-```
 def remove_duplicates(input_str=None):
     if input_str is None:
         numbers = input("Enter comma-separated numbers: ")
     else:
         numbers = str(input_str)
-    return list(map(str, set(map(int, numbers.replace(" ", "").split(",")))))
+    return list(map(int, set(numbers.replace(" ", "").split(",") + numbers.replace(" ", "").split()))
