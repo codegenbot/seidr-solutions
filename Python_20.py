158f3dd85bc5e21550408ce28b9a311ee9ed6637
beep boop <githubbot@vadim.me>
2024-07-06 08:29:45 +0200

    Solution 8 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ltdfq1zn Prompt: Complete the following code given the task description and function signature.

--- Python_20.py
+++ Python_20.py
@@ -1,8 +1,7 @@
-
 def find_closest_elements(numbers: List[float]) -> Tuple[float, float]:
     numbers.sort()
-    closest_pair = (numbers[0], numbers[1])
-    for i in range(len(numbers)-1):
-        if abs(numbers[i] - numbers[i+1]) < abs(closest_pair[0] - closest_pair[1]):
-            closest_pair = (numbers[i], numbers[i+1])
-    return closest_pair
+    closest = (numbers[0], numbers[1])
+    for i in range(len(numbers) - 1):
+        if abs(numbers[i] - numbers[i + 1]) < abs(closest[0] - closest[1]):
+            closest = (numbers[i], numbers[i + 1])
+    return closest
