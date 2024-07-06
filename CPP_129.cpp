62230b47958fb92a84c45db4afe69be654e53819
beep boop <githubbot@vadim.me>
2024-07-06 18:40:13 +0200

    Solution 53 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1kalf9to Prompt: /usr/include/c++/11/bits/stl_uninitialized.h:151:15:   required from ‘_ForwardIt erator std::uninitialized_copy(_InputIterator, _InputIterator, _ForwardIterator)  [with _InputIterator = __gnu_cxx::__normal_iterator<const std::vector<int>*, st d::vector<std::vector<int> > >; _ForwardIterator = std::vector<int>*]’ /usr/include/c++/11/bits/stl_uninitialized.h:333:37:   required from ‘_ForwardIt erator std::__uninitialized_copy_a(_InputIterator, _InputIterator, _ForwardItera tor, std::allocator<_Tp>&) [with _InputIterator = __gnu_cxx::__normal_iterator<c onst std::vector<int>*, std::vector<std::vector<int> > >; _ForwardIterator = std ::vector<int>*; _Tp = std::vector<int>]’ /usr/include/c++/11/bits/stl_vector.h:558:31:   required from ‘std::vector<_Tp, _Alloc>::vector(const std::vector<_Tp, _Alloc>&) [with _Tp = std::vector<int>; _ Alloc = std::allocator<std::vector<int> >]’ 09875ae2-c0f5-404c-ae94-3858387dffd3.cpp:34:33:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,9 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <climits>
-
-using namespace std;
-
 vector<vector<int>> minPath(vector<vector<int>> grid, int k) {
     vector<int> res;
     for (int i = 0; i < k; ++i) {
@@ -25,15 +19,4 @@ vector<vector<int>> minPath(vector<vector<int>> grid, int k) {
         }
     }
     return res;
-}
-
-int main() {
-    vector<vector<int>> grid = {{1,2,3}, {4,5,6}, {7,8,9}};
-    int k = 3;
-    vector<int> result = minPath(grid, k);
-    for (auto val : result) {
-        cout << val << " ";
-    }
-    cout << endl;
-    return 0;
 }
