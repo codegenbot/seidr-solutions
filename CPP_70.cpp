11177a6803b393ac1e6507f90226842f1ba253d5
beep boop <githubbot@vadim.me>
2024-07-06 14:26:29 +0200

    Solution 4 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0wk0egm1 Prompt: In file included from 9edbfcbc-0e47-4eb5-9c58-884c0d19efbb.cpp:21: 9edbfcbc-0e47-4eb5-9c58-884c0d19efbb.cpp: In function ‘int main()’: 9edbfcbc-0e47-4eb5-9c58-884c0d19efbb.cpp:31:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    31 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,15 +1,43 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 vector<int> strange_sort_vector(vector<int> lst) {
     vector<int> result;
+    if (lst.empty()) return result;
+
+    sort(lst.begin(), lst.end());
     while (!lst.empty()) {
-        int min_val = *min_element(lst.begin(), lst.end());
-        result.push_back(min_val);
-        lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
-        
-        if (!lst.empty()) {
-            int max_val = *max_element(lst.begin(), lst.end());
-            result.push_back(max_val);
-            lst.erase(remove(lst.begin(), lst.end(), max_val), lst.end());
-        }
+        result.push_back(*lst.begin());
+        lst.erase(lst.begin());
+        if (!lst.empty())
+            sort(lst.begin(), lst.end());
     }
+
     return result;
+}
+
+int main() {
+    vector<int> input;
+    int numElements;
+    cout << "Enter the number of elements in the list: ";
+    cin >> numElements;
+
+    for (int i = 0; i < numElements; i++) {
+        int n;
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> n;
+        input.push_back(n);
+    }
+
+    vector<int> output = strange_sort_vector(input);
+
+    cout << "The sorted list is: ";
+    for (int x : output) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
