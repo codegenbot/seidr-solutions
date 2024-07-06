9d48a98fdea9671247ad8d4576a2a5161fbaa88f
beep boop <githubbot@vadim.me>
2024-07-06 03:33:12 +0200

    Solution 54 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/23sd7ljf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eccf871c-a37b-4341-8ebf-05c81153b e68.py", line 11, in <module>     check(solution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eccf871c-a37b-4341-8ebf-05c81153b e68.py", line 6, in check     assert solution([3, 13, 2, 9]) == 3            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_121.py
+++ Python_121.py
@@ -1,3 +1,2 @@
-```
 def solution(lst):
-    return sum(int(i) for i in lst[1::2] if int(i) % 2 != 0 and int(i) > 1)
+    return sum(int(i) for i in lst[1::2] if int(i) > 1 and int(i) % 2 != 0) or 0
