ef198dc5588efa354599297428341595035c78b0
beep boop <githubbot@vadim.me>
2024-07-06 17:11:50 +0200

    Solution 80 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/n9sebtun Prompt: ng<char>*; _ForwardIterator = std::__cxx11::basic_string<char>*; _Tp = std::__cx x11::basic_string<char>]’ /usr/include/c++/11/bits/stl_vector.h:1585:33:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const std::__cxx11::basic_string<char >*; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11 ::basic_string<char> >]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = s td::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11::basic_str ing<char> >; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<std::__cx x11::basic_string<char> >]’ 304d00cf-660d-49c9-9fd6-8dc4ae789a4e.cpp:64:5:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -38,7 +38,7 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
         } else if (grade > 0.7) {
             letterGrade = "D";
         } else {
-            letterGrade = "F";
+            letterGrade = "E";
         }
         result.push_back(letterGrade);
     }
@@ -46,8 +46,8 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
 }
 
 int main() {
-    std::vector<float> testGrades = {0, 0.7};
-    std::vector<std::string> letterGrades = numerical_letter_grade(testGrades); 
-    assert(issame({letterGrades[0], letterGrades[1]}, {"E", "D"})); 
+    std::vector<float> testGrades = {0.0, 0.7};
+    std::vector<std::string> letterGrades = numerical_letter_grade(testGrades);
+    assert(issame({letterGrades[0], letterGrades[1]}, {"E", "D-"})); 
     return 0;
 }
