41c2a856bfd37a529c6d49bad9807ce9a4c407c1
beep boop <githubbot@vadim.me>
2024-07-06 20:04:30 +0200

    Solution 25 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/u2o18bue Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c90a2844-88b3-4cb5-983d-034122fe4 78e.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c90a2844-88b3-4cb5-983d-034122fe4 78e.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,13 +1,12 @@
 ```
+def check(odd_sum):
+    lst = [1,3,5]
+    if double_the_difference(lst) == odd_sum:
+        print("True")
+    else:
+        print("False")
+
 def double_the_difference(lst):
     return [(x - min(lst)) * 2 + min(lst) for x in lst]
 
-
-while True:
-    lst = input("Enter a list of numbers separated by spaces: ").split()
-    if all(x.isdigit() for x in lst):  
-        lst = [int(x) for x in lst]
-        while "" in lst:
-            lst.remove("")
-        break
-print(double_the_difference(lst))
+check(9)
