42bf8974d939c70ea876929b3040ad7f9e9d1c95
beep boop <githubbot@vadim.me>
2024-07-06 15:39:43 +0200

    Solution 41 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0yee0l13 Prompt:       |                            ^ 83ad0f7d-ff41-4a19-aa0c-d44367ca3dcf.cpp:10:30: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ 83ad0f7d-ff41-4a19-aa0c-d44367ca3dcf.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 83ad0f7d-ff41-4a19-aa0c-d44367ca3dcf.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 83ad0f7d-ff41-4a19-aa0c-d44367ca3dcf.cpp:9: 83ad0f7d-ff41-4a19-aa0c-d44367ca3dcf.cpp: In function ‘int main()’: 83ad0f7d-ff41-4a19-aa0c-d44367ca3dcf.cpp:19:20: error: ‘numerical_letter_grade’ was not declared in this scope    19 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 83ad0f7d-ff41-4a19-aa0c-d44367ca3dcf.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -2,27 +2,10 @@
 #include <vector>
 #include <string>
 
-double numerical_letter_grade(int score) {
-    double grade = 0;
-
-    if (score >= 90)
-        grade = 4.00;
-    else if (score >= 80)
-        grade = 3.50;
-    else if (score >= 70)
-        grade = 3.00;
-    else if (score >= 60)
-        grade = 2.50;
-    else
-        grade = 0.00;
-
-    return grade;
-}
-
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) return false;
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]) return false;
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
