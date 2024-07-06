e366998b23c605df9fb6e64c023716abc38889b1
beep boop <githubbot@vadim.me>
2024-07-06 11:35:36 +0200

    Solution 48 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/t5qd6gs6 Prompt: it must return GI for input %.gD/]&+f(E&pgcoooM52N</dqS6FQ9IBS\5>|b^vqUMiGI{)V;'I{6+GA+x}08IxBE4hgR2pOmS^OW~>=mCB|W{^nuD, but it returns .

--- middle-character.py
+++ middle-character.py
@@ -1,6 +1,5 @@
-
 def get_middle(s):
     if len(s) % 2 == 0:
         return s[len(s) // 2 - 1 : len(s) // 2 + 1]
     else:
-        return s.translate({ord(c): '' for c in string.punctuation})[len(s) // 2]
+        return s[len(s) // 2]
