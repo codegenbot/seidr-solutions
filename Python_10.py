35ba023c9dc1c30dd6606b4e478489c83f0b3e1a
beep boop <githubbot@vadim.me>
2024-07-06 04:57:14 +0200

    Solution 18 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tdbrvss0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f40d7b47-f8ce-4464-80c9-543417ecc 3fa.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,4 +1,4 @@
-"""
+```
 def is_palindrome(string: str) -> bool:
     return string == string[::-1]
 
@@ -9,4 +9,3 @@ def make_palindrome(string: str) -> str:
         if string[:i+1] == string[:i+1][::-1]:
             return string[:i+1] + string[i:][::-1]
     return string + string[::-1]
-"""
