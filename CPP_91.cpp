00705535db03aaf1ade7165f51fcd9c26c276e94
beep boop <githubbot@vadim.me>
2024-07-06 16:15:10 +0200

    Solution 12 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9hrt0837 Prompt: 37f3f008-ced3-4035-bfb3-2f44809fee0e.cpp:1:14: error: ‘string’ was not declared in this scope     1 | int is_bored(string S){       |              ^~~~~~ In file included from 37f3f008-ced3-4035-bfb3-2f44809fee0e.cpp:15: 37f3f008-ced3-4035-bfb3-2f44809fee0e.cpp: In function ‘int main()’: 37f3f008-ced3-4035-bfb3-2f44809fee0e.cpp:17:21: error: ‘is_bored’ cannot be used  as a function    17 |     assert (is_bored("You and I are going for a walk") == 0);       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,12 +1,23 @@
+#include <iostream>
 #include <string>
 
-int is_bored(string S){
+using namespace std;
+
+int is_bored(string S) {
     int count = 0;
+    string boredom = "I";
     size_t pos = 0;
-    while((pos = S.find("I", pos)) != string::npos) {
-        if (S[pos] == 'I' && (S.find(".", pos) != string::npos || S.find("?", pos) != string::npos || S.find("!", pos) != string::npos)) {
+    while ((pos = S.find(" ", pos)) != string::npos) {
+        if (S.substr(0, pos).compare(boredom) == 0) {
             count++;
+            break;
         }
-        pos = S.find(" ", pos) + 1;
+        pos++;
     }
     return count;
+}
+
+int main() {
+    assert(is_bored("You and I are going for a walk") == 0);
+    return 0;
+}
