cd9acc0cdc027d2b914e5a3c50ee74f5fcf8da50
beep boop <githubbot@vadim.me>
2024-07-06 16:19:24 +0200

    Solution 28 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6qafnz8m Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/75f546bd-67c0-4f72-b539-bf370b1de 9bc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,12 +1,11 @@
+```
 import math
 
-
 def find_zero(a, b, c):
-    d = math.sqrt(b**2 - 4 * a * c)
-    x1 = (-b + d) / (2 * a)
-    x2 = (-b - d) / (2 * a)
+    d = math.sqrt(b**2 - 4*a*c)
+    x1 = (-b + d) / (2*a)
+    x2 = (-b - d) / (2*a)
     return round(min(x1, x2), 2)
-
 
 if __name__ == "__main__":
     print("Enter a: ")
