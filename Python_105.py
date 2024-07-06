2e5b0692b0a479c3bb1e57254c071a1aae47e215
beep boop <githubbot@vadim.me>
2024-07-06 02:13:56 +0200

    Solution 2 of Python/105, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zgcpokd2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b3987a64-b99d-4690-b725-01daac096 f68.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_105.py
+++ Python_105.py
@@ -1,6 +1,9 @@
 def by_length(arr):
-    sorted_arr = sorted([x for x in arr if 1 <= x <= 9])
-    return [names[x - 1] for x in reversed(sorted_arr)]
+    result = sorted([str(i) for i in arr if 1 <= i <= 9])
+    result.reverse()
+    return [get_name(int(num)) for num in result]
 
-
-names = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+def get_name(n):
+    names = {"1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five",
+             "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine"}
+    return names.get(str(n), "")
