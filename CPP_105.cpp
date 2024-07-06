1e234594f67f46b255628d37e756109712873fca
beep boop <githubbot@vadim.me>
2024-07-06 17:32:24 +0200

    Solution 26 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/horaq1dq Prompt:       |                              ^~~~~~ d465b2a9-d911-4038-9ebe-1f63d52f4dc5.cpp:25:37: error: ‘string’ was not declared  in this scope    25 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d465b2a9-d911-4038-9ebe-1f63d52f4dc5.cpp:25:44: error: ‘b’ was not declared in t his scope    25 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d465b2a9-d911-4038-9ebe-1f63d52f4dc5.cpp:25:45: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d465b2a9-d911-4038-9ebe-1f63d52f4dc5.cpp:24: d465b2a9-d911-4038-9ebe-1f63d52f4dc5.cpp: In function ‘int main()’: d465b2a9-d911-4038-9ebe-1f63d52f4dc5.cpp:34:20: error: ‘by_length’ was not decla red in this scope    34 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ d465b2a9-d911-4038-9ebe-1f63d52f4dc5.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,22 +1,26 @@
-```cpp
 bool issame(vector<string> a,vector<string>b){
     return a == b;
 }
 
 vector<string> by_length(vector<int> arr) {
+    vector<string> sortedArr;
+    for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            sortedArr.push_back(to_string(num));
+        }
+    }
+
+    sort(sortedArr.begin(), sortedArr.end());
+
+    reverse(sortedArr.begin(), sortedArr.end());
+
     vector<string> result;
     map<int, string> numToName = {{1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
                                    {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
 
-    for (int num : arr) {
-        if (num >= 1 && num <= 9) {
-            result.push_back(numToName[num]);
-        }
+    for (int num : sortedArr) {
+        result.push_back(numToName[num]);
     }
-
-    sort(result.begin(), result.end());
-
-    reverse(result.begin(), result.end());
 
     return result;
 }
