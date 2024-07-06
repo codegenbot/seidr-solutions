79aefd6a0c84f22f3ea122dcbe8077868b541dcb
beep boop <githubbot@vadim.me>
2024-07-06 17:15:15 +0200

    Solution 82 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1fv3kcsh Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa561f5e-6519-4064-a201-cf372186c 067.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa561f5e-6519-4064-a201-cf372186c 067.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,8 @@
 def sort_even(l: list) -> tuple:
     even_numbers = [x for x in l if isinstance(x, int) and x % 2 == 0]
-    odd_numbers = [x for x in l if isinstance(x, int) and x % 2 != 0]
+    try:
+        odd_numbers = [x for x in l if isinstance(x, int) and x % 2 != 0]
+    except TypeError:
+        raise ValueError("List contains non-integer values")
     
-    return (tuple(even_numbers), tuple(odd_numbers))
+    return (tuple(sorted(even_numbers)), tuple(sorted(odd_numbers)))
