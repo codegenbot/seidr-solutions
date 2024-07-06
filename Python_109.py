24e7ce815fe8e74b163ef9ce655ba0814b2b7dfb
beep boop <githubbot@vadim.me>
2024-07-06 08:28:52 +0200

    Solution 1 of Python/109, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4zk22p7i Prompt: Complete the following code given the task description and function signature.

--- Python_109.py
+++ Python_109.py
@@ -1,11 +1,11 @@
-```
 def move_one_ball(arr):
     if len(arr) == 0:
         return True
-    
-    arr = [x for _, x in sorted((i, y) for i, y in enumerate(arr))]
-    
-    for i in range(1, len(arr)):
-        if arr[i] < arr[0]:
+
+    left = 0
+    for right in range(len(arr)):
+        if arr[right] < arr[left]:
             return False
+        left += 1
+
     return True
