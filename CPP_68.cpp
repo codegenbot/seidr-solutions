e63720251aed747c8f76402e5829605a3a39e4ad
beep boop <githubbot@vadim.me>
2024-07-06 19:35:11 +0200

    Solution 25 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gxyqmqlh Prompt:       |                    ^~~ 14037e6e-1afc-4621-ae23-320b976e1107.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 14037e6e-1afc-4621-ae23-320b976e1107.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 14037e6e-1afc-4621-ae23-320b976e1107.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 14037e6e-1afc-4621-ae23-320b976e1107.cpp:21: 14037e6e-1afc-4621-ae23-320b976e1107.cpp: In function ‘int main()’: 14037e6e-1afc-4621-ae23-320b976e1107.cpp:31:20: error: ‘pluck’ was not declared in this scope    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 14037e6e-1afc-4621-ae23-320b976e1107.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,8 +1,7 @@
 #include <vector>
-#include <stdbool.h>
 
-vector<int> pluck(vector<int> arr) {
-    vector<int> result = {};
+std::vector<int> pluck(std::vector<int> arr) {
+    std::vector<int> result = {};
     int minEvenValueIndex = -1;
     int minEvenValue = INT_MAX;
 
@@ -14,15 +13,6 @@ vector<int> pluck(vector<int> arr) {
     }
 
     if (minEvenValue != INT_MAX) {
-        result.push_back(minEvenValue);
-        result.push_back(minEvenValueIndex);
-    } else {
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] % 2 == 1 && minEvenValue > arr[i]) {
-                minEvenValue = arr[i];
-                minEvenValueIndex = i;
-            }
-        }
         result.push_back(minEvenValue);
         result.push_back(minEvenValueIndex);
     }
