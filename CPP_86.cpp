febf42fccfcfcc7ef0048122922846da35dea9ec
beep boop <githubbot@vadim.me>
2024-07-06 19:49:46 +0200

    Solution 16 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/t1eho5tt Prompt: 53041012-0674-4b70-937d-c8a92b0101e3.cpp:4:1: error: ‘string’ does not name a ty pe     4 | string anti_shuffle(string s) {       | ^~~~~~ In file included from 53041012-0674-4b70-937d-c8a92b0101e3.cpp:23: 53041012-0674-4b70-937d-c8a92b0101e3.cpp: In function ‘int main()’: 53041012-0674-4b70-937d-c8a92b0101e3.cpp:25:13: error: ‘anti_shuffle’ was not de clared in this scope    25 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,45 +1,20 @@
 #include <algorithm>
-#include <string>
-
 using namespace std;
 
 string anti_shuffle(string s) {
     string result = "";
-    for (const auto& word : split(s, ' ')) {
-        result += reorder(word);
-        if (!result.empty()) {
+    size_t pos = 0;
+    while (pos < s.length()) {
+        size_t nextSpace = s.find(' ', pos);
+        if (nextSpace == string::npos) {
+            nextSpace = s.length();
+        }
+        string word = s.substr(pos, nextSpace - pos);
+        sort(word.begin(), word.end());
+        result += word;
+        if (nextSpace < s.length()) {
             result += " ";
         }
+        pos = nextSpace + 1;
     }
     return result;
-}
-
-string split(const string& str, char delimeter) {
-    string result[100];
-    int i = 0;
-    for (const auto& c : str) {
-        if (c != delimeter) {
-            result[i] += c;
-        } else {
-            i++;
-        }
-    }
-    return "";
-}
-
-string reorder(const string& word) {
-    string result = "";
-    for (char c : word) {
-        bool isAdded = false;
-        for (const auto& charInWord : word) {
-            if (!isAdded && c > charInWord) {
-                result += charInWord;
-                isAdded = true;
-            }
-        }
-        if (!isAdded) {
-            result += c;
-        }
-    }
-    return result;
-}
