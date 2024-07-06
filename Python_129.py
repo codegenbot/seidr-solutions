bf7abf8b45e6958e707052d9be0a4fd56fba1275
beep boop <githubbot@vadim.me>
2024-07-06 23:17:14 +0200

    Solution 93 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/flfbd0f2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4adb6bea-68e7-4b78-9090-099386185 6b7.py", line 34, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4adb6bea-68e7-4b78-9090-099386185 6b7.py", line 31, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -6,7 +6,7 @@ def minPath(grid, k):
             if grid[i][j] == 1:
                 path = [grid[i][j]]
                 dfs(grid, i, j, k - 1, path, res)
-    return res
+    return sorted(set(res))
 
 def dfs(grid, x, y, k, path, res):
     n = len(grid)
@@ -19,5 +19,5 @@ def dfs(grid, x, y, k, path, res):
                 path.append(grid[nx][ny])
                 dfs(grid, nx, ny, k - 1, path, res)
                 if len(path) == k + 1:
-                    res.append(sorted(path[:]))
+                    res.append(path[:])
                 path.pop()
