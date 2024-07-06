e593e1248607608fcfdba4f819689fa9a09ab542
beep boop <githubbot@vadim.me>
2024-07-06 03:03:37 +0200

    Solution 75 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/a1rky2qb Prompt:                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8e871096-99de-468a-a7cc-e300aa53a b96.py", line 17, in parse_nested_parens     level = 1 + parse_nested_parens(group[i + 1 :])[0]                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8e871096-99de-468a-a7cc-e300aa53a b96.py", line 19, in parse_nested_parens     level = -1 + parse_nested_parens(group[: i - 1])[0]                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8e871096-99de-468a-a7cc-e300aa53a b96.py", line 19, in parse_nested_parens     level = -1 + parse_nested_parens(group[: i - 1])[0]                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8e871096-99de-468a-a7cc-e300aa53a b96.py", line 19, in parse_nested_parens     level = -1 + parse_nested_parens(group[: i - 1])[0]                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   [Previous line repeated 9 more times] IndexError: list index out of range

--- Python_6.py
+++ Python_6.py
@@ -1,13 +1,13 @@
 def parse_nested_parens(paren_string):
     result = []
     for group in paren_string.split():
-        levels = [] # Initialize the levels list
+        levels = []
         level = 0
         for i, char in enumerate(group):
             if char == "(":
                 level += 1
             elif char == ")":
                 level -= 1
-            levels.append(level) # Append to the levels list
+            levels.append(level)
         result.extend(levels)
     return result
