7c1c0e6b2ac0fb174acac2274f3b9ff42449e95b
beep boop <githubbot@vadim.me>
2024-07-06 16:48:57 +0200

    Solution 65 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/i0tpovve Prompt: f7c76757-19e4-4db4-8397-b285dcc70185.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ f7c76757-19e4-4db4-8397-b285dcc70185.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -4,8 +4,8 @@
 int smallest_change(std::vector<int> arr) {
     int changes = 0;
 
-    for (int i = 1; i < arr.size(); i++) {
-        if (arr[i] != arr[i-1]) {
+    for (int i = 1; i < arr.size(); ++i) {
+        if (arr[i] != arr[i - 1]) {
             changes++;
         }
     }
