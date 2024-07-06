31679045a7858e9434a32b33c8a3c3be1e617c0a
beep boop <githubbot@vadim.me>
2024-07-06 10:36:59 +0200

    Solution 38 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/64bgbtjk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b3416156-c3f1-4b75-a10a-f67f560a3 b68.py", line 7, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b3416156-c3f1-4b75-a10a-f67f560a3 b68.py", line 4, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_41.py
+++ Python_41.py
@@ -1,7 +1,5 @@
 def car_race_collision(n: int):
-    if n < 1:
-        return "Please enter a positive integer."
-    elif n == 1:
-        return "No collisions will occur because there is only one car."
-    else:
-        return n * (n - 1) // 2
+    total_cars = 0
+    for i in range(1, n + 1):
+        total_cars += i
+    return total_cars
