86525f4de8ffdfdb3a2f19edd486892d50748bc2
beep boop <githubbot@vadim.me>
2024-07-06 15:27:18 +0200

    Solution 49 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/90xzm5ge Prompt: e8084437-eaa6-4e03-99be-5b54b06e1206.cpp:11:5: error: ‘assert’ was not declared in this scope    11 |     assert (will_it_fly({5}, 5) == true);       |     ^~~~~~ e8084437-eaa6-4e03-99be-5b54b06e1206.cpp:1:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?   +++ |+#include <cassert>     1 | bool will_it_fly(vector<int> q,int w){ e8084437-eaa6-4e03-99be-5b54b06e1206.cpp: At global scope: e8084437-eaa6-4e03-99be-5b54b06e1206.cpp:17:5: error: redefinition of ‘int main( )’    17 | int main(){       |     ^~~~ e8084437-eaa6-4e03-99be-5b54b06e1206.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~ In file included from e8084437-eaa6-4e03-99be-5b54b06e1206.cpp:16: e8084437-eaa6-4e03-99be-5b54b06e1206.cpp: In function ‘int main()’: e8084437-eaa6-4e03-99be-5b54b06e1206.cpp:18:24: error: ‘will_it_fly’ cannot be u sed as a function    18 |     assert (will_it_fly({5}, 5) == true);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,11 +1,10 @@
 #include <cassert>
-#include <vector>
 #include <string>
 
-bool will_it_fly(std::vector<int> q,int w){
-    std::string str = "";
-    for(int i:q)str+=std::to_string(i);
-    if(str!=std::string(rbegin(str),rend(str)))return false;
+bool will_it_fly(vector<int> q,int w){
+    string str = "";
+    for(int i:q)str+=to_string(i);
+    if(str!=string(rbegin(str),rend(str)))return false;
     int sum=0;
     for(int i:q)sum+=i;
     return sum<=w;
