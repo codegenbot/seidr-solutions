21a9678ed680a123780afa0b57f88986bc36e9e7
beep boop <githubbot@vadim.me>
2024-07-06 06:17:56 +0200

    Solution 68 of CPP/83, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7jp0kji8 Prompt: 8a66830c-871d-429e-b767-090e40e90db2.cpp: In function ‘bool starts_one_ends(int) ’: 8a66830c-871d-429e-b767-090e40e90db2.cpp:14:40: error: invalid operands of types  ‘__gnu_cxx::__promote_2<int, int, double, double>::__type’ {aka ‘double’} and ‘ int’ to binary ‘operator%’    14 |             if ((num / pow(10, n - 1)) % 10 == 1 || num % 10 == 1) {       |                 ~~~~~~~~~~~~~~~~~~~~~~ ^ ~~       |                      |                   |       |                      |                   int       |                      __gnu_cxx::__promote_2<int, int, double, double>::_ _type {aka double} 8a66830c-871d-429e-b767-090e40e90db2.cpp: At global scope: 8a66830c-871d-429e-b767-090e40e90db2.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 8a66830c-871d-429e-b767-090e40e90db2.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -1,3 +1,4 @@
+```
 #include <iostream>
 #include <cmath>
 
@@ -9,9 +10,9 @@ bool starts_one_ends(int n) {
         if (i == 1 || i % 10 == 1) {
             count++;
         }
-        for (long long j = 1; j < pow(10, n); j++) {
-            long long num = i * pow(10, n - 1) + j;
-            if ((num / pow(10, n - 1)) % 10 == 1 || num % 10 == 1) {
+        for (int j = 1; j < static_cast<int>(pow(10, n)); j++) {
+            int num = i * pow(10, n - 1) + j;
+            if ((num / static_cast<int>(pow(10, n - 1))) % 10 == 1 || num % 10 == 1) {
                 count++;
             }
         }
