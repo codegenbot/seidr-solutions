f207c604fa94e3ac46c387656bfd8db260407893
beep boop <githubbot@vadim.me>
2024-07-06 18:30:43 +0200

    Solution 61 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/d9qzivqw Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/314c98b0-de8b-489b-a65b-bfbc52d95 355.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_41.py
+++ Python_41.py
@@ -7,17 +7,22 @@ def car_race_collision():
             print("Invalid input. Please enter an integer.")
     
     if n < 1:
-        return str(0) 
+        return None
+    
     total_damage = 0
     for i in range(n):
         while True:
             try:
-                speed, damage = map(int, input(f"Enter speed and damage for car {i+1}: ").split())
-                break
+                values = input(f"Enter speed and damage for car {i+1}: ")
+                if len(values.split()) != 2:
+                    print("Invalid input. Please enter two integers separated by a space.")
+                else:
+                    speed, damage = map(int, values.split())
+                    break
             except ValueError:
                 print("Invalid input. Please enter two integers separated by a space.")
         total_damage += damage * speed
     
     return str(total_damage)
 
-print(car_race_collision())
+car_race_collision()
