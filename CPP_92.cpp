2869b76ff4d1737a04f5a2ea154b18fd67b03629
beep boop <githubbot@vadim.me>
2024-07-06 17:15:21 +0200

    Solution 36 of CPP/92, ratio 0.7 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/skeuxh7i Prompt: Program failed

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,14 +1,6 @@
-#include <cassert>
-
-bool any_int(float a, float b, float c) {
+bool any_int(int a, int b, int c) {
     if (a == int(a) && b == int(b) && c == int(c)) {
         return (a == b + c || a == c + b || b == a + c || b == c + a || c == a + b || c == b + a);
     }
     return false;
-}
-
-int main() {
-    assert(any_int(3.01, 4, 7) == false); 
-    assert(any_int(1,2,3) == true); // Test case for three integers
-    return 0;
 }
