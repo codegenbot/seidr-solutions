055bf1985329ab7e585d5a3f18f162b1ac1d72ee
beep boop <githubbot@vadim.me>
2024-07-06 18:01:02 +0200

    Solution 15 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xkhiaef7 Prompt:       |                    ^~~ 85d8b4d9-a307-4335-ac7d-404a4cf17633.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 85d8b4d9-a307-4335-ac7d-404a4cf17633.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 85d8b4d9-a307-4335-ac7d-404a4cf17633.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 85d8b4d9-a307-4335-ac7d-404a4cf17633.cpp:10: 85d8b4d9-a307-4335-ac7d-404a4cf17633.cpp: In function ‘int main()’: 85d8b4d9-a307-4335-ac7d-404a4cf17633.cpp:20:20: error: ‘maximum’ was not declare d in this scope    20 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 85d8b4d9-a307-4335-ac7d-404a4cf17633.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -2,37 +2,42 @@
 #include <algorithm>
 
 bool issame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()) {
+    if(a.size() != b.size())
         return false;
-    }
     for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
+        if(a[i] != b[i])
             return false;
-        }
     }
     return true;
 }
 
 int main() {
     int k;
-    cin >> k;
+    std::cin >> k;
 
-    vector<int> maximum(vector<int> arr, int k) {
-        vector<int> res(k);
-        partial_sort(arr.begin(), arr.end() - k + 1, arr.end());
-        for (int i = 0; i < k; i++) {
-            res[i] = arr[arr.size() - k + i];
-        }
-        return res;
-    }
+    vector<int> arr;
+    for(int i = 0; i < 7; i++)
+        arr.push_back(i - 3); // Example array, replace with your input
 
-    if (k <= 0) {
+    if(k <= 0) {
         cout << "Invalid value of k" << endl;
         return -1;
     } else {
-        vector<int> res = maximum({1, 2, 3, -23, 243, -400, 0}, k);
-        // Do something with the result
+        vector<int> res = maximum(arr, k);
+        for(int i = 0; i < k; i++) {
+            cout << res[i] << " ";
+        }
+        cout << endl;
     }
 
     return 0;
+}
+
+vector<int> maximum(vector<int> arr, int k) {
+    vector<int> res(k);
+    partial_sort(arr.begin(), arr.end() - k + 1, arr.end());
+    for (int i = 0; i < k; i++) {
+        res[i] = arr[arr.size() - k + i];
+    }
+    return res;
 }
