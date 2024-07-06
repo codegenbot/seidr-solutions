cfc26e8119b7b9ffd9923e85ec81ad019b9d55a5
beep boop <githubbot@vadim.me>
2024-07-06 17:18:55 +0200

    Solution 64 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r6wjanum Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/37993e5e-3a78-40b1-a246-c6bdb5089 4f5.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/37993e5e-3a78-40b1-a246-c6bdb5089 4f5.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/37993e5e-3a78-40b1-a246-c6bdb5089 4f5.py", line 2, in sort_third     return [sorted(input_list[i:i+3]) for i in range(0, len(input_list), 3)] + ( [sorted(input_list[j:j+2])] if len(input_list) % 3 != 0 else [])                    ^ NameError: name 'j' is not defined

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,3 @@
+```
 def sort_third(input_list):
-    return [sorted(input_list[k:k+3]) for k in range(0, len(input_list), 3)] + (
-        [sorted(input_list[j:j+2])] if len(input_list) % 3 != 0 else []
-    )
+    return [sorted(input_list[i:i+3]) for i in range(0, len(input_list), 3)] + ([input_list[-1]] if len(input_list) % 3 != 0 else [])
