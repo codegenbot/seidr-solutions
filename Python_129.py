8e56c2166ae39fe629f418496037e04ba4d6716f
beep boop <githubbot@vadim.me>
2024-07-06 23:05:46 +0200

    Solution 66 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/flfbd0f2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/573f72cc-dfd2-414c-8fd4-501ecc125 1c6.py", line 33, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/573f72cc-dfd2-414c-8fd4-501ecc125 1c6.py", line 30, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -7,7 +7,6 @@ def minPath(grid, k):
     def dfs(i, j, path, count):
         nonlocal result
         if count == k:
-            nonlocal result
             result.append(sorted(path))
             return True
 
