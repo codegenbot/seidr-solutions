cead21cc8f046b96ca18704e983fbf3d971bf857
beep boop <githubbot@vadim.me>
2024-07-06 05:46:44 +0200

    Solution 84 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/w2c7zzis Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3f8adb2f-d36e-4869-9162-9308118a3 263.py", line 11, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3f8adb2f-d36e-4869-9162-9308118a3 263.py", line 8, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,4 +1,4 @@
 def sorted_list_sum(lst):
     even_words = [word for word in lst if len(word) % 2 == 0]
     odd_words = [word for word in lst if len(word) % 2 != 0]
-    return sorted(even_words, key=lambda x: len(x)) + sorted(odd_words, key=lambda x: len(x))
+    return sorted(even_words, key=len) + sorted(odd_words, key=len)
