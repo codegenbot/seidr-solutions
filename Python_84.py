01ec610b962b815f0ea9e2e9c6d096bf5d2d7c39
beep boop <githubbot@vadim.me>
2024-07-06 21:56:01 +0200

    Solution 98 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qeuhcwp8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cdab427d-57fd-4d2e-900c-ba87cc998 5d9.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,12 +1,12 @@
+# Blank line to satisfy Python syntax
+
 def convert():
     while True:
-        choice = input(
-            "Do you want to (1) Convert decimal to binary or (2) Convert binary to decimal? "
-        )
+        choice = input("Do you want to (1) Convert decimal to binary or (2) Convert binary to decimal? ")
         if choice in ["1", "2"]:
             break
         else:
-            print("Invalid input. Please choose a number between 1 and 2.")
+            print(("Invalid input. Please choose a number between 1 and 2."))
 
     if choice == "1":
         while True:
@@ -15,7 +15,7 @@ def convert():
                 print(bin(N)[2:])
                 break
             except ValueError:
-                print("That's not a valid number! Try again.")
+                print(("That's not a valid number! Try again."))
     elif choice == "2":
         while True:
             bin_input = input("Enter a binary number in base 10 (e.g., 101): ")
@@ -24,7 +24,7 @@ def convert():
                 print(N)
                 break
             except ValueError:
-                print("That's not a valid binary number! Try again.")
+                print(("That's not a valid binary number! Try again."))
 
 
 if __name__ == "__main__":
