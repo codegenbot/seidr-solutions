1498b5bde18ff5efe4d1b11667d827505fc9ec6c
beep boop <githubbot@vadim.me>
2024-07-06 12:47:10 +0200

    Solution 71 of Python/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xfaq9o17 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a5a9d6bd-4693-49aa-9763-42cca8627 31a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_73.py
+++ Python_73.py
@@ -1,11 +1,20 @@
 ```
 def smallest_change(arr):
-    arr = sorted(arr)
-    n = len(arr) - 1
+    n = len(arr)
+    arr.sort()
+    left, right = 0, n - 1
     changes = 0
     
-    for i in range(n):
-        if arr[i] != arr[i+1]:
-            changes += 1
+    while left < right:
+        if arr[left] != arr[right]:
+            for i in range(n-1):
+                if arr[i] == arr[right]:
+                    arr[i], arr[left] = arr[left], arr[i]
+                    changes += 1
+                    break
+            left += 1
+        else:
+            left += 1
+            right -= 1
     
     return changes
