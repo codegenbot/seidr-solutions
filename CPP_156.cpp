0ee68f3c21a8c4f24d070990b01c21d36d45b42e
beep boop <githubbot@vadim.me>
2024-07-06 19:54:54 +0200

    Solution 81 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cge2wy3k Prompt: f5332959-305f-48f8-b3a4-acb9558cb071.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ f5332959-305f-48f8-b3a4-acb9558cb071.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ f5332959-305f-48f8-b3a4-acb9558cb071.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ f5332959-305f-48f8-b3a4-acb9558cb071.cpp:2:6: error: ‘string’ in namespace ‘std’  does not name a type     2 | std::string intToMiniRoman(int number) {       |      ^~~~~~ f5332959-305f-48f8-b3a4-acb9558cb071.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | ``` In file included from f5332959-305f-48f8-b3a4-acb9558cb071.cpp:19: f5332959-305f-48f8-b3a4-acb9558cb071.cpp: In function ‘int main()’: f5332959-305f-48f8-b3a4-acb9558cb071.cpp:21:13: error: ‘int_to_mini_romank’ was not declared in this scope    21 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-std::string intToMiniRoman(int number) {
+string intToMiniRoman(int number) {
     vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {500, "D"},
                                          {400, "CD"}, {100, "C"}, {90, "XC"},
                                          {50, "L"}, {40, "XL"}, {10, "X"},
                                          {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
-    std::string result = "";
+    string result = "";
     
     for (const auto& pair : roman) {
         while (number >= pair.first) {
