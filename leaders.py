ebb0b271addf2b4b3ae7a69cf1ea2dc8d3f22cb8
beep boop <githubbot@vadim.me>
2024-07-06 09:52:56 +0200

    Solution 42 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/96bhn0nl Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/96b6c9d5-7f00-430d-816b-86d5388ed 59c.py", line 1     def leaders(vector): IndentationError: unexpected indent

--- leaders.py
+++ leaders.py
@@ -1,8 +1,6 @@
 def leaders(vector):
-    max_value = -1
     result = []
-    for i in range(len(vector)-1, -1, -1):
-        if vector[i] >= max_value:
-            max_value = vector[i]
-            result.append(max_value)
-    return result[::-1]
+    for i in range(len(vector)):
+        if vector[i] >= all(vector[i + 1 :]):
+            result.append(vector[i])
+    return result
