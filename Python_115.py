658b174608432263e53a682acd9497abd9fe00db
beep boop <githubbot@vadim.me>
2024-07-06 04:07:08 +0200

    Solution 89 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1h36up85 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ebd1f80e-c435-4213-a337-fb28b36fd e82.py", line 1     ````     ^ SyntaxError: invalid syntax

--- Python_115.py
+++ Python_115.py
@@ -1,18 +1,18 @@
-```python
+```
 def max_fill(grid, capacity):
     total_amount = 0
-    left = [str(c) for r in grid for c in r]
+    left = [c for r in grid for c in r]
     right = left[::-1]
     left_ptr, right_ptr = 0, len(left) - 1
     
     while left_ptr <= right_ptr:
         h = min(capacity, int(left[left_ptr]), int(right[right_ptr]))
         total_amount += h
-        left[left_ptr] -= str(h)
-        right[right_ptr] -= str(h)
-        if left[left_ptr].endswith('0'):
-            left_ptr += 1
-        if right[right_ptr].endswith('0'):
-            right_ptr -= 1
+        left[left_ptr] -= h
+        right[right_ptr] -&#x3d; h
+        if left[left_ptr] &#x3d; 0:
+            left_ptr +&#x3d; 1
+        if right[right_ptr] &#x3d; 0:
+            right_ptr -&#x3d; 1
     
-    return int(total_amount)
+    return total_amount
