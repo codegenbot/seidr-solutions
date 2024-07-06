ed8f786ab501c5df05689d591a4310026f37e8f2
beep boop <githubbot@vadim.me>
2024-07-06 08:07:50 +0200

    Solution 11 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/o1mdw7cz Prompt:       |             ^~~~~~ 40094a4f-a3eb-4372-a07b-0403846b6cfc.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 40094a4f-a3eb-4372-a07b-0403846b6cfc.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 40094a4f-a3eb-4372-a07b-0403846b6cfc.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 40094a4f-a3eb-4372-a07b-0403846b6cfc.cpp:27: 40094a4f-a3eb-4372-a07b-0403846b6cfc.cpp: In function ‘int main()’: 40094a4f-a3eb-4372-a07b-0403846b6cfc.cpp:37:20: error: ‘get_odd_collatz’ was not  declared in this scope    37 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 40094a4f-a3eb-4372-a07b-0403846b6cfc.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -13,9 +13,9 @@ vector<int> get_odd_collatz(int n) {
             n /= 2;
         else
             n = 3 * n + 1;
-        result.push_back(n);
+        if (n % 2 != 0)
+            result.push_back(n);
     }
-    sort(result.begin(), result.end());
     return result;
 }
 
