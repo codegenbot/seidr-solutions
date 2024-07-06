2b90136e63d265a8ee23486a542aff7a879e6d7c
beep boop <githubbot@vadim.me>
2024-07-06 12:15:15 +0200

    Solution 45 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tnxfnu72 Prompt: b4841fbe-ba42-4c4a-b1ec-87358ea0c751.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ b4841fbe-ba42-4c4a-b1ec-87358ea0c751.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 vector<int> unique(vector<int> l) {
-    vector<int> result(l.begin(), unique_copy(l.begin(), l.end()));
-    sort(result.begin(), result.end());
+    vector<int> result(l.begin(), l.end());
+    sort(result.begin(), unique(result.begin(), result.end()));
     return result;
 }
 
