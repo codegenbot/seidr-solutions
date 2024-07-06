1450ae683e597f1b795910c5b8d8432953b73bfb
beep boop <githubbot@vadim.me>
2024-07-06 20:22:50 +0200

    Solution 75 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3f2yeli0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f29f1b14-f594-4b38-889e-9708dbf58 fb7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_154.py
+++ Python_154.py
@@ -1,13 +1,13 @@
 def cycpattern_check(a, b):
     if int(a) % 2 == 0 and int(b) % 2 != 0:
-        return "winten"
+        return ("winten")
     else:
         return "not winten"
 
 
 def check(func):
     def wrapper(a, b):
-        return func(a, b) == "winten"
+        return func(int(a), int(b)) == "winten"
 
     return wrapper
 
