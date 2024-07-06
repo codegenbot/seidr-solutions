e6f45f19257085c3bb423bfbe357a5f2541e736a
beep boop <githubbot@vadim.me>
2024-07-06 16:53:58 +0200

    Solution 12 of CPP/109, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4wisdxmw Prompt:                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 8b04bde5-1258-4fdf-973f-e05b761b0792.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 8b04bde5-1258-4fdf-973f-e05b761b0792.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8b04bde5-1258-4fdf-973f-e05b761b0792.cpp:3:27: error: expected primary-expressio n before ‘int’     3 | bool move_one_ball(vector<int> arr) {       |                           ^~~ In file included from 8b04bde5-1258-4fdf-973f-e05b761b0792.cpp:21: 8b04bde5-1258-4fdf-973f-e05b761b0792.cpp: In function ‘int main()’: 8b04bde5-1258-4fdf-973f-e05b761b0792.cpp:23:26: error: ‘move_one_ball’ cannot be  used as a function    23 |     assert (move_one_ball({})==true);       |             ~~~~~~~~~~~~~^~~~

--- CPP_109.cpp
+++ CPP_109.cpp
@@ -1,17 +1,20 @@
+#include <algorithm>
 #include <vector>
-using namespace std;
 
-bool move_one_ball(vector<int> arr) {
+bool move_one_ball(std::vector<int> arr) {
     int n = arr.size();
-    if (n == 0)
-        return true;
+    if (n == 0) return true;
     
-    sort(arr.begin(), arr.end());
-    for (int i = 1; i < n; i++) {
-        if (arr[i] <= arr[0]) {
-            return false;
-        }
+    std::vector<int> temp(arr);
+    std::sort(temp.begin(), temp.end());
+    
+    for (int i = 0; i < n; i++) {
+        arr = temp;
+        temp = std::vector<int>(arr.begin() + 1, arr.end());
+        temp.push_back(arr[0]);
+        
+        if (temp == arr) return true;
     }
     
-    return true;
+    return false;
 }
