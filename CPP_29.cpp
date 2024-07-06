bcf2ae9a93c9f8cab013bf9f501ce471c8719935
beep boop <githubbot@vadim.me>
2024-07-07 01:58:44 +0200

    Solution 1 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/uty50v51 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ee390bb6-06c1-400a-b762-6cf95022b65b.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ee390bb6-06c1-400a-b762-6cf95022b65b.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ee390bb6-06c1-400a-b762-6cf95022b65b.cpp:10: ee390bb6-06c1-400a-b762-6cf95022b65b.cpp: In function ‘int main()’: ee390bb6-06c1-400a-b762-6cf95022b65b.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ ee390bb6-06c1-400a-b762-6cf95022b65b.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,8 +1,12 @@
-vector<string> filter_by_prefix(vector<string> strings, string prefix){
-    vector<string> result;
-    for(string s : strings){
-        if(s.find(prefix) == 0)
-            result.push_back(s);
+bool issame(vector<string> a, vector<string> b) {
+    for(string s : a) {
+        if(find(b.begin(), b.end(), s) == b.end()) 
+            return false;
     }
-    return result;
+    return true;
+}
+
+int main() {
+    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxxAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));
+    return 0;
 }
