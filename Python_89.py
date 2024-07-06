26021aa2814f0d2171bc1c20b64db14f7e7babed
beep boop <githubbot@vadim.me>
2024-07-06 04:56:15 +0200

    Solution 81 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/g5ej8jfl Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4235c027-dce5-401d-8620-6858821c0 555.py", line 24, in <module>     check(encrypt)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4235c027-dce5-401d-8620-6858821c0 555.py", line 20, in check     encrypt("a") == "e" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_89.py
+++ Python_89.py
@@ -1,6 +1,5 @@
-
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-rotation_dict = {char: alphabet[(alphabet.index(char) + 2) % len(alphabet)] for char in alphabet}
-
 def encrypt(s):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    rotation_dict = {char: alphabet[(alphabet.index(char) + 2) % len(alphabet)] for char in alphabet}
+
     return ''.join([rotation_dict[c] for c in s])
