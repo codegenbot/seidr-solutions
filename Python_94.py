6b3c4585d3b737eb545d1947c1f63912d5bb6875
beep boop <githubbot@vadim.me>
2024-07-06 04:18:25 +0200

    Solution 6 of Python/94, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/vubnro2p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5c3b9e40-114f-4d16-b8b4-592dc0b9a 11c.py", line 21, in <module>     check(skjkasdkd)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5c3b9e40-114f-4d16-b8b4-592dc0b9a 11c.py", line 17, in check     skjkasdkd([127, 97, 8192]) == 10     ^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5c3b9e40-114f-4d16-b8b4-592dc0b9a 11c.py", line 5, in skjkasdkd     if is_prime(num) and (max_prime is None or num > max_prime):        ^^^^^^^^ NameError: name 'is_prime' is not defined. Did you mean: 'max_prime'?

--- Python_94.py
+++ Python_94.py
@@ -11,3 +11,11 @@ def skjkasdkd(lst):
         return sum(map(int, str(max_prime)))
     else:
         return 0
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
