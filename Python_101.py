21f309cf262680213e648c13e9e63172d4ca809c
beep boop <githubbot@vadim.me>
2024-07-06 04:06:24 +0200

    Solution 96 of Python/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/tbrkevyf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9602321b-edfd-4611-875f-138a66b85 844.py", line 14, in <module>     check(words_string)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9602321b-edfd-4611-875f-138a66b85 844.py", line 11, in check     assert words_string("ahmed     , gamal") == ["ahmed", "gamal"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9602321b-edfd-4611-875f-138a66b85 844.py", line 2, in words_string     inp = sys.stdin.readline()           ^^^ NameError: name 'sys' is not defined

--- Python_101.py
+++ Python_101.py
@@ -1,7 +1,11 @@
+import sys
+import re
+
+
 def words_string(s):
     inp = sys.stdin.readline()
     words = re.split(r"\s+", inp)
     if len(words) == 1:
-        return "The string contains one word."
+        return ["The string contains one word."]
     else:
-        return f"The string contains {len(words)} words."
+        return [f"The string contains {len(words)} words."]
