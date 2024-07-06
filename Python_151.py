0e566da05c03f8aac25b6a148555ca062190a9f3
beep boop <githubbot@vadim.me>
2024-07-06 12:09:52 +0200

    Solution 14 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1hc8uxc8 Prompt: Complete the following code given the task description and function signature.

--- Python_151.py
+++ Python_151.py
@@ -1,3 +1,3 @@
 def double_the_difference(lst):
-    result = sum(i**2 for i in lst if isinstance(i, int) and i > 0)
-    return result * 2
+    total = sum(i**2 for i in lst if isinstance(i, int) and i > 0)
+    return total * 2
