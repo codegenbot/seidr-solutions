cea5503fbfe3a3fc8fcc15c9ffd988566d4e042c
beep boop <githubbot@vadim.me>
2024-07-06 17:26:24 +0200

    Solution 59 of CPP/98, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zxts84mo Prompt: Program failed

--- CPP_98.cpp
+++ CPP_98.cpp
@@ -3,7 +3,8 @@
 int count_upper(std::string s){
     int count = 0;
     for(int i = 0; i < s.length(); i++){
-        if((i % 2 == 0) && (s[i] >= 'A' && s[i] <= 'Z')){
+        if(i % 2 == 0 && (s[i] >= 'A' && s[i] <= 'U') || 
+           (s[i] >= 'a' && s[i] <= 'u')){
             count++;
         }
     }
