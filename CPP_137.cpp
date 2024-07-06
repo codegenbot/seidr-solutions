4235a43393cb47451109c84fc5f7d08d3baecdc4
beep boop <githubbot@vadim.me>
2024-07-06 18:17:46 +0200

    Solution 32 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/t0ko17pn Prompt: 6a33422c-c7ac-4aaf-9d4e-7c450cdd5ed7.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,9 +1,10 @@
 #include <boost/any.hpp>
+#include <boost/lexical_cast.hpp>
 
 using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_any_of<a>(numeric()) && is_any_of<b>(numeric())) {
+    if (is_any_of<a>(numeric_floating_point())) {
         double da = get<double>(a);
         double db = get<double>(b);
 
@@ -12,19 +13,29 @@ boost::any compare_one(boost::any a, boost::any b) {
         else if (db > da)
             return b;
 
-        return boost::any("None");
-    } else {
-        string sa = boost::any_cast<string>(a);
-        string sb = boost::any_cast<string>(b);
-
-        double da = stod(sa);
-        double db = stod(sb);
+        return "None";
+    } else if (is_any_of<a>(integer<>)) {
+        int da = boost::any_cast<int>(a);
+        int db = boost::any_cast<int>(b);
 
         if (da > db)
             return a;
         else if (db > da)
             return b;
 
-        return boost::any("None");
+        return "None";
+    } else {
+        string sa = boost::any_cast<string>(a);
+        string sb = boost::any_cast<string>(b);
+
+        double da = lexical_cast<double>(sa);
+        double db = lexical_cast<double>(sb);
+
+        if (da > db)
+            return a;
+        else if (db > da)
+            return b;
+
+        return "None";
     }
 }
