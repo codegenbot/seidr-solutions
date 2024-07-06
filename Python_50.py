d6b0d265a6cb2027f32d73a58c7c6324ccffa972
beep boop <githubbot@vadim.me>
2024-07-06 02:10:04 +0200

    Solution 87 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/t2jqbixf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/07319d85-596b-4663-b3b9-155dc0a9f e1b.py", line 1, in <module>     @click.command()      ^^^^^ NameError: name 'click' is not defined. Did you mean: 'slice'?

--- Python_50.py
+++ Python_50.py
@@ -1,7 +1,5 @@
-@click.command()
-@click.option("--shift", type=int, help="The number of characters to shift.")
+
 def decode_shift(message):
-    message = click.prompt("Enter the message to be decoded", default="")
-    return "".join(
-        [chr((ord(ch) - shift - ord("a")) % 26 + ord("a")) for ch in message]
-    )
+    message = int(click.prompt("Enter the message to be decoded", default=""))
+    shift = int(input("Enter the number of characters to shift: "))
+    return "".join([chr((ord(ch) - shift - ord("a")) % 26 + ord("a")) for ch in message])
