a9de49499fca065c52d0cb7d5dd055b2008784f0
beep boop <githubbot@vadim.me>
2024-07-06 21:42:54 +0200

    Solution 15 of CPP/111, ratio 0.75 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6wti9phk Prompt: Complete the following code given the task description and function signature.

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,45 +1,39 @@
-#include <string>
-#include <map>
+#include<stdio.h>
+#include<string>
+#include<map>
 using namespace std;
 
-map<char, int> histogram(string test) {
-    map<char, int> result;
-    if (test.empty()) return result;
+map<char,int> histogram(string test){
+    map<char,int> result;
+    string temp;
+    int maxCount = 0;
+    
+    for (int i = 0; i < test.length(); i++) {
+        if (test[i] != ' ') {
+            temp += test[i];
+        } else {
+            if (!result.empty()) {
+                for (auto it = result.begin(); it != result.end(); it++) {
+                    if (it->second > maxCount) {
+                        maxCount = it->second;
+                    }
+                }
+            }
+            temp = "";
+        }
+    }
 
-    string word;
-    for (int i = 0; i < test.size(); i++) {
-        char c = test[i];
-        if (c == ' ') {
-            if (!word.empty()) {
-                if (result.find(word[0]) != result.end())
-                    result[word[0]]++;
-                else
-                    result[word[0]] = 1;
-                word.clear();
+    if (!temp.empty()) {
+        if (!result.empty()) {
+            for (auto it = result.begin(); it != result.end(); it++) {
+                if (it->second == maxCount) {
+                    result[temp[0]]++;
+                }
             }
         } else {
-            word += c;
+            result[temp[0]] = 1;
         }
     }
 
-    if (!word.empty()) {
-        if (result.find(word[0]) != result.end())
-            result[word[0]]++;
-        else
-            result[word[0]] = 1;
-    }
-
-    int maxCount = 0;
-    for (auto it = result.begin(); it != result.end(); it++) {
-        maxCount = max(maxCount, it->second);
-    }
-
-    map<char, int> maxMap;
-    for (auto it = result.begin(); it != result.end(); it++) {
-        if (it->second == maxCount) {
-            maxMap[it->first] = it->second;
-        }
-    }
-
-    return maxMap;
+    return result;
 }
