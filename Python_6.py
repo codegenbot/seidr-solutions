14adec8cabd172602b180ccfe93bd045c16433e4
beep boop <githubbot@vadim.me>
2024-07-06 15:05:09 +0200

    Solution 59 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3ou52l0b Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/97d5c14a-69c4-4a30-8089-616812924 381.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_6.py
+++ Python_6.py
@@ -1,9 +1,10 @@
-```
 def parse_nested_parens(s):
-    while '{}{}' in s:
+    while '{}' in s:
         start = s.index('{')
         end = s.index('}')
-        s = s[:start] + input().strip() + s[end+1:]
+        user_input = str(input()).strip()
+        s = s[:start] + user_input + s[end+1:]
     return s
 
-print(parse_nested_parens(input().strip()))
+main_input = input().strip()
+print(parse_nested_parens(main_input))
