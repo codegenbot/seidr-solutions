846347f7c3853b7dca6119bcb854faea8f9e8878
beep boop <githubbot@vadim.me>
2024-07-06 16:43:14 +0200

    Solution 82 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/ltcqd9ic Prompt: it must return 5 for input 139113443, but it returns .

--- paired-digits.py
+++ paired-digits.py
@@ -1,6 +1,6 @@
 def paired_digits(s):
     total = 0
-    for i in range(len(s)):
-        if s[i] == s[i+1]:
-            total += int(s[i])
+    for i, j in zip(s, s[1:]):
+        if i == j:
+            total += int(i)
     return total
