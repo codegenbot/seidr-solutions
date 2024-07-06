ab5e3182704e771ba132cb6e475acfa7bb290d04
beep boop <githubbot@vadim.me>
2024-07-06 07:05:24 +0200

    Solution 2 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qm1etz17 Prompt: ea529f35-45b7-4ecf-bc5b-6b0f8ef7d902.cpp:23:18: error: ‘string’ was not declared  in this scope    23 | bool isconsonant(string word, int i) {       |                  ^~~~~~ ea529f35-45b7-4ecf-bc5b-6b0f8ef7d902.cpp:2:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?     1 | #include <algorithm>   +++ |+#include <string>     2 | using namespace std; ea529f35-45b7-4ecf-bc5b-6b0f8ef7d902.cpp:23:31: error: expected primary-expressi on before ‘int’    23 | bool isconsonant(string word, int i) {       |                               ^~~ ea529f35-45b7-4ecf-bc5b-6b0f8ef7d902.cpp:23:36: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool isconsonant(string word, int i) {       |                                    ^ In file included from ea529f35-45b7-4ecf-bc5b-6b0f8ef7d902.cpp:28: ea529f35-45b7-4ecf-bc5b-6b0f8ef7d902.cpp: In function ‘int main()’: ea529f35-45b7-4ecf-bc5b-6b0f8ef7d902.cpp:30:13: error: ‘get_closest_vowel’ was n ot declared in this scope    30 |     assert (get_closest_vowel("Above") == "o");       |             ^~~~~~~~~~~~~~~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,12 +1,27 @@
+#include <algorithm>
+#include <string>
+using namespace std;
+
+bool isconsonant(string word, int i) {
+    char c = tolower(word[i]);
+    return !(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+}
+
 string get_closest_vowel(string word) {
-    for (int i = word.length() - 1; i > 0; i--) {
-        if ("aeiouAEIOU".find(word[i]) != string::npos) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (!("aeiouAEIOU".find(word[j]) == string::npos)) {
-                    return word.substr(j, i - j);
-                }
+    int n = word.length();
+    for (int i = n - 1; i > 0; --i) {
+        if (isvowel(word[i])) {
+            for (int j = i - 1; j >= 0; --j) {
+                if (!isvowel(word[j]) && !isconsonant(word, j)) break;
+                else if (!isvowel(word[j]) && isconsonant(word, j)) continue;
+                else return string(1, word[i]);
             }
         }
     }
     return "";
+}
+
+bool isvowel(char c) {
+    c = tolower(c);
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 }
