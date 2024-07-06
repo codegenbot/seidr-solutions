6686b518c8eee08d360cf8ce7bfa6a45aa9cde07
beep boop <githubbot@vadim.me>
2024-07-06 11:18:29 +0200

    Solution 57 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3gvl0s3q Prompt:       |                           ^~~~~~ e75165aa-aa4d-4dbd-bfc4-236fe4916dc6.cpp:26:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from e75165aa-aa4d-4dbd-bfc4-236fe4916dc6.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from e75165aa-aa4d-4dbd-bfc4-236fe4916dc6.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e75165aa-aa4d-4dbd-bfc4-236fe4916dc6.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e75165aa-aa4d-4dbd-bfc4-236fe4916dc6.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ e75165aa-aa4d-4dbd-bfc4-236fe4916dc6.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <list>
 #include <any>
@@ -20,6 +19,6 @@ std::vector<int> filter_integers(std::list<std::any> values) {
 }
 
 int main() {
-    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), std::vector<int>{3, 3, 3}));
+    assert(std::issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
     return 0;
 }
