5fd1b06f0c42a332961c2474cc1d7fe91872f717
beep boop <githubbot@vadim.me>
2024-07-06 10:40:20 +0200

    Solution 16 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/u89qmsrm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/61137977-1589-4fca-aefb-31827d054 16c.py", line 16, in <module>     check(parse_nested_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/61137977-1589-4fca-aefb-31827d054 16c.py", line 13, in check     assert parse_nested_parens("(()(())((())))") == [4]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_6.py
+++ Python_6.py
@@ -1,11 +1,13 @@
-def parse_nested_parens(paren_string):
+def parse_nested_parens(paren_string: str) -> list[int]:
     stack = []
     result = []
     for char in paren_string:
         if char == "(":
             stack.append(char)
         elif char == ")":
-            stack.pop()
-            level = len(stack) - 1
+            level = 0
+            while len(stack) > 0 and stack[-1] != ")":
+                level += 1
+                stack.pop()
             result.append(level)
     return result
