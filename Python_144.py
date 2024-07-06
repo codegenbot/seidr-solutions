4b92ecd2d8206f1a92475db6f9ee2d717af18517
beep boop <githubbot@vadim.me>
2024-07-06 02:11:12 +0200

    Solution 0 of Python/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/llzwgpxo Prompt: Complete the following code given the task description and function signature.

---
+++ Python_144.py
@@ -0,0 +1,9 @@
+
+def simplify(x, n):
+    x_num, x_denom = map(int, x.split('/'))
+    n_num, n_denom = map(int, n.split('/'))
+    result = x_num * n_num
+    if result % (x_denom * n_denom) == 0:
+        return True
+    else:
+        return False
