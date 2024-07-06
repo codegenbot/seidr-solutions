5c546fd3d5c08b88986b942ca2629d2c568f65d5
beep boop <githubbot@vadim.me>
2024-07-06 04:15:49 +0200

    Solution 12 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/sj1ai82x Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -1,4 +1,13 @@
 def add(x: int, y: int):
-    x = int(input("Enter first number: "))
-    y = int(input("Enter second number: "))
+    """
+    This function takes two arguments of type `int` and returns the sum of those numbers.
+
+    :param x: first number to be added
+    :type x: int
+    :param y: second number to be added
+    :type y: int
+
+    :return: The sum of x and y
+    :rtype: int
+    """
     return x + y
