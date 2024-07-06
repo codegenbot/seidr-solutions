a7b1b38db33925a8a72b01316d9baaa0f5ff2575
beep boop <githubbot@vadim.me>
2024-07-06 10:16:25 +0200

    Solution 1 of CPP/8, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/amhsuj9f Prompt: Complete the following code given the task description and function signature.

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,13 +1,14 @@
 #include <vector>
+
 using namespace std;
 
 vector<int> sum_product(vector<int> numbers) {
     int sum = 0;
     int product = 1;
     
-    for (int number : numbers) {
-        sum += number;
-        product *= number;
+    for (int num : numbers) {
+        sum += num;
+        product *= num;
     }
     
     return {sum, product};
