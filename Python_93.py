f358f6550d55381905d08576870504c553bdefe8
beep boop <githubbot@vadim.me>
2024-07-06 03:05:04 +0200

    Solution 57 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ncp26xbv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/066920ed-04ac-42ae-bf8a-12760c6c0 78e.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/066920ed-04ac-42ae-bf8a-12760c6c0 78e.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,12 +2,10 @@ def encode(message):
     result = ""
     for char in message:
         if char.isalpha():
-            if char.lower() in "aeiou":
-                char_code = {"a": "c", "e": "g", "i": "k", "o": "q", "u": "y"}[
-                    char.lower()
-                ]
+            if char.lower() in 'aeiou':
+                char_code = {'a': 'c', 'e': 'g', 'i': 'k', 'o': 'q', 'u': 'y'}[char.lower()]
                 result += char_code.upper() if char.isupper() else char_code
             else:
-                result += char.swapcase() if char.isalpha() else char
+                result += (char.swapcase() if char.isalpha() else char)
         else:
             result += char
