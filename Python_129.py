32e5e163dab562499f5ab45f5597154b8e81786e
beep boop <githubbot@vadim.me>
2024-07-06 02:53:51 +0200

    Solution 19 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/j2ptses6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/468c92b5-7dc4-4179-ac4f-f0b7e1eca ce0.py", line 33, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/468c92b5-7dc4-4179-ac4f-f0b7e1eca ce0.py", line 30, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -1,11 +1,11 @@
 def minPath(grid, k):
     n = len(grid)
-
+    
     queue = [(0, 0, [])]
     visited = set((0, 0))
     path_lengths = {}
     shortest_paths = {}
-
+    
     while queue:
         x, y, path = queue.pop(0)
 
@@ -17,11 +17,8 @@ def minPath(grid, k):
 
             if (nx, ny) not in visited and 0 <= nx < n and 0 <= ny < n:
                 new_path = path + [grid[nx][ny]]
-
-                if (
-                    len(new_path) not in shortest_paths
-                    or len(new_path) < shortest_paths[len(new_path)]
-                ):
+                
+                if len(new_path) not in shortest_paths or len(new_path) < shortest_paths[len(new_path)]:
                     queue.append((nx, ny, new_path))
                     visited.add((nx, ny))
                     shortest_paths[len(new_path)] = len(new_path)
