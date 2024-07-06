bdb8a2c67682b736441277e742ee87002630f0a4
beep boop <githubbot@vadim.me>
2024-07-06 03:40:21 +0200

    Solution 43 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ynbb4504 Prompt: Program did not receive expected input

--- Python_140.py
+++ Python_140.py
@@ -1,6 +1,7 @@
 import re
-
-
 def fix_spaces(text):
+    # Replace multiple spaces with single underscore
     text = re.sub(r"\s+", "_", text)
+    
+    # Remove all remaining spaces
     return text.replace(" ", "")
