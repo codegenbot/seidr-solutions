fc4e50f2abba3f7d5bac0e25ce81ca17199069b1
beep boop <githubbot@vadim.me>
2024-07-06 17:48:54 +0200

    Solution 80 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/z02owpwx Prompt:       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from /usr/include/x86_64-linux-gnu/c++/11/bits/stdc++.h:65,                  from c13bdf2b-33fa-4623-92f0-21a5a53c3b5d.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c13bdf2b-33fa-4623-92f0-21a5a53c3b5d.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c13bdf2b-33fa-4623-92f0-21a5a53c3b5d.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c13bdf2b-33fa-4623-92f0-21a5a53c3b5d.cpp:16: c13bdf2b-33fa-4623-92f0-21a5a53c3b5d.cpp: In function ‘int main()’: c13bdf2b-33fa-4623-92f0-21a5a53c3b5d.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,14 +1,22 @@
+#include <initializer_list>
 #include <bits/stdc++.h>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
 
-int main() {
-    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
-    return 0;
+std::vector<int> make_a_pile(int n) {
+    std::vector<int> pile;
+    for (int i = 1; i <= n; i++) {
+        if(i % 2 == 0) {
+            pile.push_back(2*i-1);
+        } else {
+            pile.push_back(2*i);
+        }
+    }
+    return pile;
 }
