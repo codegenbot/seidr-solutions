130f8772fd9535f5dca7f7aff47a1b170781b28e
beep boop <githubbot@vadim.me>
2024-07-06 23:17:47 +0200

    Solution 24 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xypmlusu Prompt:       |         ~~~~~~~~~~^~~~~~~~~~~~~~ In file included from d54ce1ae-45a6-4eb1-9483-2f083ceefd4e.cpp:3: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ d54ce1ae-45a6-4eb1-9483-2f083ceefd4e.cpp:18:14: warning: ‘int MD5_Final(unsigned  char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]    18 |     MD5_Final(md5, &ctx);       |     ~~~~~~~~~^~~~~~~~~~~ In file included from d54ce1ae-45a6-4eb1-9483-2f083ceefd4e.cpp:3: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ /usr/bin/ld: /tmp/ccW3noLf.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> >)': d54ce1ae-45a6-4eb1-9483-2f083ceefd4e.cpp:(.text+0x8e): undefined reference to `M D5_Init' /usr/bin/ld: d54ce1ae-45a6-4eb1-9483-2f083ceefd4e.cpp:(.text+0xc4): undefined re ference to `MD5_Update' /usr/bin/ld: d54ce1ae-45a6-4eb1-9483-2f083ceefd4e.cpp:(.text+0xf0): undefined re ference to `MD5_Final' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <openssl/evp.h>
 #include <openssl/md5.h>
 
 using namespace std;
@@ -9,14 +8,14 @@ string string_to_md5(string text) {
     if (text.empty()) return "";
 
     unsigned char md5[MD5_DIGEST_LENGTH];
-    EVP_MD_CTX ctx;
-    EVP_MD_CTX_init(&ctx);
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
     const char* ptr = text.c_str();
     while (*ptr) {
-        EVP_DigestUpdate(&ctx, ptr, 1);
+        MD5_Update(&ctx, ptr, 1);
         ptr++;
     }
-    EVP_DigestFinal(&ctx, md5, nullptr);
+    MD5_Final(md5, &ctx);
 
     string result;
     for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
