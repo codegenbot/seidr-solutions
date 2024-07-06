422f68cf970807f1c99da331826932800f3e1289
beep boop <githubbot@vadim.me>
2024-07-06 02:56:54 +0200

    Solution 46 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/zqqswxr4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f8ff7aa8-3600-4ac5-888e-8c36ba0d9 6e8.py", line 15, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f8ff7aa8-3600-4ac5-888e-8c36ba0d9 6e8.py", line 12, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,8 +1,6 @@
 def make_palindrome(string):
-    def is_palindrome(s):
-        return s == s[::-1]
-
-    if is_palindrome(string):
-        return string
-    else:
-        return "".join(reversed(string))
+    rev = list(reversed(string))
+    for i in range(len(rev)):
+        if rev[i] != string[i]:
+            return False
+    return True
