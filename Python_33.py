d30762d86fba3082358d1babba62ef4f15ba12d7
beep boop <githubbot@vadim.me>
2024-07-06 17:36:10 +0200

    Solution 84 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r6wjanum Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4847c30e-0580-41a4-b180-98524a7e1 1ae.py", line 20, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4847c30e-0580-41a4-b180-98524a7e1 1ae.py", line 15, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,13 +1,13 @@
 def sort_third(input_list):
     result = []
     current_group = []
-    for num in input_list:
-        current_group.append(int(num))
+    for num in sorted(map(int, input_list)):
+        current_group.append(num)
         if len(current_group) == 3:
             result.append(tuple(sorted(current_group)))
             current_group = []
 
-    if current_group:  
+    if current_group:
         result.append(tuple(sorted(current_group)))
 
     return tuple(result)
