331716f283f37530ad4ac21b4a7daba8f2d00d89
beep boop <githubbot@vadim.me>
2024-07-06 19:43:25 +0200

    Solution 75 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cge2wy3k Prompt:                  from /usr/include/c++/11/vector:60,                  from 6d11910f-856e-4a70-a86c-5c196773f68d.cpp:1: /usr/include/c++/11/bits/stl_pair.h:211:12: note: ‘std::pair’ declared here   211 |     struct pair       |            ^~~~ 6d11910f-856e-4a70-a86c-5c196773f68d.cpp:6:33: error: template argument 1 is inv alid     6 |     std::vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {5 00, "D"},       |                                 ^~ 6d11910f-856e-4a70-a86c-5c196773f68d.cpp:6:33: error: template argument 2 is inv alid 6d11910f-856e-4a70-a86c-5c196773f68d.cpp:6:36: error: scalar object ‘roman’ requ ires one element in initializer     6 |     std::vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {5 00, "D"},       |                                    ^~~~~ In file included from 6d11910f-856e-4a70-a86c-5c196773f68d.cpp:22: 6d11910f-856e-4a70-a86c-5c196773f68d.cpp: In function ‘int main()’: 6d11910f-856e-4a70-a86c-5c196773f68d.cpp:24:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘intToMiniRoman’?    24 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -2,11 +2,13 @@
 #include <map>
 #include <string>
 
+std::vector<std::pair<int, std::string>> romanPair;
+
 std::string intToMiniRoman(int number) {
-    std::vector<std::pair<int, std::string>> romanPair = {{1000, "M"}, {900, "CM"}, {500, "D"},
-                                                           {400, "CD"}, {100, "C"}, {90, "XC"},
-                                                           {50, "L"}, {40, "XL"}, {10, "X"},
-                                                           {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+    romanPair = {{1000, "M"}, {900, "CM"}, {500, "D"},
+                 {400, "CD"}, {100, "C"}, {90, "XC"},
+                 {50, "L"}, {40, "XL"}, {10, "X"},
+                 {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
     std::string result = "";
     
     for (const auto& pair : romanPair) {
