b6221cbf9a93efe58a303102e3389e2091c17bdc
beep boop <githubbot@vadim.me>
2024-07-06 06:50:32 +0200

    Solution 9 of Python/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9tvtvwhx Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6fb3ae0b-d4f5-4dd0-9fe3-d405e5820 ee6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_128.py
+++ Python_128.py
@@ -6,8 +6,5 @@ def prod_signs(arr):
     magnitude_sum = sum(abs(x) for x in arr)
     sign_product = 1
     for num in arr:
-        if num > 0:
-            product *= 1
-        elif num < 0:
-            product *=-1
+        product *= num / abs(num)
     return product * magnitude_sum
