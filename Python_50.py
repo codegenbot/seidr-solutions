8abd312614441394548de7661fc0869d0a09434d
beep boop <githubbot@vadim.me>
2024-07-06 03:03:20 +0200

    Solution 95 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/134n4wi8 Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -1,23 +1,13 @@
 def decode_shift():
     while True:
         shift = input("Enter 'encode' or 'decode': ")
-
+        
         if shift == "encode" or shift == "decode":
             break
         else:
             raise Exception("Invalid input. Please enter 'encode' or 'decode'.")
-
+            
     if shift == "encode":
-        return "".join(
-            [
-                chr((ord(ch) - ord("a") + 3) % 26 + ord("a"))
-                for ch in input("Enter the string: ").lower()
-            ]
-        )
+        return "".join([chr((ord(ch) - ord("a") + 3) % 26 + ord("a")) for ch in input("Enter the string: ").lower()])
     elif shift == "decode":
-        return "".join(
-            [
-                chr((ord(ch) - ord("a") - 3) % 26 + ord("a"))
-                for ch in input("Enter the string: ").lower()
-            ]
-        )
+        return "".join([chr((ord(ch) - ord("a") - 3) % 26 + ord("a")) for ch in input("Enter the string: ").lower()])
