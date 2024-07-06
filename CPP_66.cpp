1508830dfa25e85ca94c37073173e0c44c8bf2e5
beep boop <githubbot@vadim.me>
2024-07-06 16:34:20 +0200

    Solution 54 of CPP/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/uftugksb Prompt: c0ba31bd-a14b-4347-8381-8ebb4e09d47a.cpp: In function ‘int main()’: c0ba31bd-a14b-4347-8381-8ebb4e09d47a.cpp:16:5: error: ‘assert’ was not declared in this scope    16 |     assert(digitSum("You arE Very Smart") == 327);       |     ^~~~~~ c0ba31bd-a14b-4347-8381-8ebb4e09d47a.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <iostream>   +++ |+#include <cassert>     2 | #include <string> c0ba31bd-a14b-4347-8381-8ebb4e09d47a.cpp: At global scope: c0ba31bd-a14b-4347-8381-8ebb4e09d47a.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ c0ba31bd-a14b-4347-8381-8ebb4e09d47a.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_66.cpp
+++ CPP_66.cpp
@@ -12,9 +12,9 @@ int digitSum(string s) {
         }
     }
     return sum;
-
 }
 
-assert(digitSum("You arE Very Smart") == 327);
-
-return 0;
+int main() {
+    assert(digitSum("You arE Very Smart") == 327);
+    return 0;
+}
