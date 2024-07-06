2fe50247adae91169eb9b10f6285ff95919b713e
beep boop <githubbot@vadim.me>
2024-07-06 18:37:07 +0200

    Solution 52 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cdq02h36 Prompt: 005accf1-73be-46bd-8c37-85e06e0b6c67.cpp: In function ‘std::vector<std::__cxx11: :basic_string<char> > split(const string&, const string&)’: 005accf1-73be-46bd-8c37-85e06e0b6c67.cpp:10:16: error: passing ‘const string’ {a ka ‘const std::__cxx11::basic_string<char>’} as ‘this’ argument discards qualifi ers [-fpermissive]    10 |         s.erase(0, pos + delimiter.length());       |         ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 005accf1-73be-46bd-8c37-85e06e0b6c67.cpp:1: /usr/include/c++/11/bits/basic_string.h:1827:7: note:   in call to ‘std::__cxx11 ::basic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Tra its, _Alloc>::erase(std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_ty pe, std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type) [with _CharT  = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>; std::_ _cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type = long unsigned int]’  1827 |       erase(size_type __pos = 0, size_type __n = npos)       |       ^~~~~

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,5 +1,19 @@
+```cpp
+#include <iostream>
 #include <string>
 #include <vector>
+
+std::vector<std::string> split(const std::string& s, const std::string& delimiter) {
+    std::vector<std::string> result;
+    size_t pos = 0;
+    std::string temp = s; // Make a copy of s
+    while ((pos = temp.find(delimiter)) != std::string::npos) {
+        result.push_back(temp.substr(0, pos));
+        temp.erase(0, pos + delimiter.length());
+    }
+    result.push_back(temp);
+    return result;
+}
 
 bool is_prime(int n) {
     if (n <= 1)
@@ -11,23 +25,12 @@ bool is_prime(int n) {
     return true;
 }
 
-std::string words_in_sentence(std::string sentence) {
+std::string words_in_sentence(std::string sentence){
     std::string result = "";
     for (const auto& word : split(sentence, " ")) {
         if(is_prime(word.length())){
             result += word + " ";
         }
     }
-    return result;
-}
-
-std::vector<std::string> split(const std::string& s, const std::string& delimiter) {
-    std::vector<std::string> result;
-    size_t pos = 0;
-    while ((pos = s.find(delimiter)) != std::string::npos) {
-        result.push_back(s.substr(0, pos));
-        s = s.substr(pos + delimiter.length());
-    }
-    result.push_back(s);
     return result;
 }
