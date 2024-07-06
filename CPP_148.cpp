00ef2fd1a0bb6cc00eb5895a23053c4cd967dfd2
beep boop <githubbot@vadim.me>
2024-07-06 18:56:22 +0200

    Solution 65 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ygswh6dl Prompt:       |                            ^ 92d052bb-1e8e-4fda-b7a0-aaa6fcecfa28.cpp:12:30: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ 92d052bb-1e8e-4fda-b7a0-aaa6fcecfa28.cpp:12:37: error: ‘string’ was not declared  in this scope    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 92d052bb-1e8e-4fda-b7a0-aaa6fcecfa28.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 92d052bb-1e8e-4fda-b7a0-aaa6fcecfa28.cpp:11: 92d052bb-1e8e-4fda-b7a0-aaa6fcecfa28.cpp: In function ‘int main()’: 92d052bb-1e8e-4fda-b7a0-aaa6fcecfa28.cpp:21:20: error: ‘bf’ was not declared in this scope    21 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 92d052bb-1e8e-4fda-b7a0-aaa6fcecfa28.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -2,23 +2,12 @@
 #include <algorithm>
 using namespace std;
 
-string bf(string s) {
-    vector<string> vs;
-    for(int i = 0; i < s.length();i+=3){
-        string temp = "";
-        for(int j = i; j <= i+2 && j < s.length() ;j++){
-            temp += s[j];
-        }
-        vs.push_back(temp);
+bool issame(vector<string> a,vector<string>b){
+    if(a.size() != b.size())
+        return false;
+    for(int i = 0; i < a.size(); i++){
+        if(find(b.begin(),b.end(),a[i]) == b.end())
+            return false;
     }
-    return "(" + s + " , {" + join(vs) + "})";
-}
-
-string join(vector<string> v){
-    string ret = "";
-    for(int i=0; i<v.size()-1;i++)
-        ret+=v[i] +", ";
-    if(v.size()>0)
-        ret += v.back();
-    return "("+ret+")";
+    return true;
 }
