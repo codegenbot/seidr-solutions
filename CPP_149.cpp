68de13d4410cb6e07db4f1b6aaba3450c76836fa
beep boop <githubbot@vadim.me>
2024-07-06 18:39:00 +0200

    Solution 44 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r1a8ydno Prompt:    20 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c775dd63-61ef-4619-8582-2428c5755a0d.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c775dd63-61ef-4619-8582-2428c5755a0d.cpp:20:45: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c775dd63-61ef-4619-8582-2428c5755a0d.cpp:19: c775dd63-61ef-4619-8582-2428c5755a0d.cpp: In function ‘int main()’: c775dd63-61ef-4619-8582-2428c5755a0d.cpp:29:20: error: ‘sorted_list_sum’ was not  declared in this scope    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ c775dd63-61ef-4619-8582-2428c5755a0d.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,32 +1,25 @@
-```cpp
-#include <vector>
-#include <string>
-
-bool issame(const std::string& s1, const std::string& s2) {
-    if (s1.length() != s2.length()) {
-        return false;
-    }
-    for (int i = 0; i < s1.length(); ++i) {
-        if (s1[i] != s2[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-std::vector<std::string> sorted_list_sum(std::vector<std::string> lst) {
-    std::vector<std::string> result;
+vector<string> sorted_list_sum(vector<string> lst) {
+    vector<string> result;
     for (const auto& str : lst) {
-        if (str.length() % 2 == 0 || issame(str, "sum")) {
+        if (str.length() % 2 == 0) {
             result.push_back(str);
         }
     }
-    std::sort(result.begin(), result.end());
+    sort(result.begin(), result.end(),
+         [](const string& a, const string& b) {
+             if (a.length() != b.length()) {
+                 return a.length() < b.length();
+             } else {
+                 return a < b;
+             }
+         });
     return result;
 }
 
-int main() {
-    std::vector<std::string> lst = {"aaaa", "bbbb", "dd", "cc"};
-    assert(issame(sorted_list_sum(lst), lst));
-    return 0;
+bool issame(vector<string> a, vector<string> b) {
+    if (a != b) {
+        return false;
+    } else {
+        return true;
+    }
 }
