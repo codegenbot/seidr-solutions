f4234ce665d9729ad11142e9684f07dddffc8d0e
beep boop <githubbot@vadim.me>
2024-07-06 21:56:33 +0200

    Solution 23 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/kri7p7c0 Prompt: Program did not receive expected input

--- Python_25.py
+++ Python_25.py
@@ -10,4 +10,4 @@ def factorize(n: int) -> List[int]:
             n //= i
     if n > 1:
         factors.append(n)
-    return [factor ** factors.count(factor) for factor in set(factors)]
+    return [factor ** (factors.count(factor)) for factor in set(factors)]
