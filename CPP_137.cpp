d932e6d2f682edc06ee5f732bff52e44f0966af5
beep boop <githubbot@vadim.me>
2024-07-06 18:18:22 +0200

    Solution 33 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/t0ko17pn Prompt: 6a33422c-c7ac-4aaf-9d4e-7c450cdd5ed7.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,35 +1,50 @@
 #include <boost/any.hpp>
-#include <boost/lexical_cast.hpp>
+#include <string>
+#include <sstream>
 
 using namespace boost;
 
+bool is_numeric(boost::any a) {
+    try {
+        double tmp = get<double>(a);
+        return true;
+    } catch (...) {
+        return false;
+    }
+}
+
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_any_of<a>(numeric_floating_point())) {
-        double da = get<double>(a);
-        double db = get<double>(b);
+    if (is_numeric(a)) {
+        if (is_numeric(b)) {
+            double da = boost::any_cast<double>(a);
+            double db = boost::any_cast<double>(b);
 
-        if (da > db)
-            return a;
-        else if (db > da)
-            return b;
+            if (da > db)
+                return a;
+            else if (db > da)
+                return b;
 
-        return "None";
-    } else if (is_any_of<a>(integer<>)) {
-        int da = boost::any_cast<int>(a);
-        int db = boost::any_cast<int>(b);
+            return "None";
+        } else {
+            std::string sa = boost::any_cast<std::string>(a);
+            std::string sb = boost::any_cast<std::string>(b);
 
-        if (da > db)
-            return a;
-        else if (db > da)
-            return b;
+            double da = std::stod(sa);
+            double db = std::stod(sb);
 
-        return "None";
+            if (da > db)
+                return a;
+            else if (db > da)
+                return b;
+
+            return "None";
+        }
     } else {
-        string sa = boost::any_cast<string>(a);
-        string sb = boost::any_cast<string>(b);
+        std::string sa = boost::any_cast<std::string>(a);
+        std::string sb = boost::any_cast<std::string>(b);
 
-        double da = lexical_cast<double>(sa);
-        double db = lexical_cast<double>(sb);
+        double da = std::stod(sa);
+        double db = std::stod(sb);
 
         if (da > db)
             return a;
