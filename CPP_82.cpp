2209295a63627e4aa7c446361061c8306fff4ebe
beep boop <githubbot@vadim.me>
2024-07-06 04:39:22 +0200

    Solution 12 of CPP/82, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/214qdpe5 Prompt: Program failed

--- CPP_82.cpp
+++ CPP_82.cpp
@@ -3,7 +3,8 @@
 bool prime_length(std::string str){
     int len = str.length();
     if(len <= 1) return false;
-    for(int i=2; i*i<=len; i++){
+    for(int i=2; i<=len; i++){
+        if(i==len) continue; 
         if(len%i==0) return false;
     }
     return true;
