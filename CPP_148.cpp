ecae491821416e2998fdc45bf499fe1c142e87a0
beep boop <githubbot@vadim.me>
2024-07-06 19:20:45 +0200

    Solution 88 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ygswh6dl Prompt: In file included from a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:19:37: error: ‘string’ was not declared  in this scope    19 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:19:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ a22b66c3-e4b7-448d-b53a-248ea22678ed.cpp:19:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,34 +1,17 @@
+```cpp
 #include <vector>
 #include <string>
 #include <algorithm>
-using namespace std;
 
-std::vector<std::string> bf(std::string a, std::string second) {
-    int j = 0;
-    for (int i = 0; i < a.length(); i++) {
-        if (a[i] == 'B' || a[i] == 'b') {
-            a.insert(i++, "first");
-        }
-        else if (a[i] == 'F' || a[i] == 'f') {
-            a.replace(i, 1, "first");
-        }
-    }
-    for (int i = 0; i < second.length(); i++) {
-        if (second[i] == 'B' || second[i] == 'b') {
-            second.insert(i++, "second");
-        }
-        else if (second[i] == 'F' || second[i] == 'f') {
-            second.replace(i, 1, "second");
-        }
-    }
-    return {a, second};
-}
-
-bool issame(vector<string> a, vector<string> second){
+bool issame(std::vector<std::string> a, std::string b) {
     for (int i = 0; i < a.size(); i++) {
-        if (find(second.begin(),second.end(),a[i]) == second.end()) {
+        if (find(b.begin(),b.end(),a[i]) == b.end()) {
             return false;
         }
     }
     return true;
+}
+
+std::vector<std::string> bf(std::string a, std::string b) {
+    // Define the function body here
 }
