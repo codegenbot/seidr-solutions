305007b7ad1135d668a898eff85b1e3054bcb15c
beep boop <githubbot@vadim.me>
2024-07-06 09:02:22 +0200

    Solution 28 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ion52a5b Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/afb25846-0c5f-4ad1-8339-ae6f6f97b e8e.py", line 19, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/afb25846-0c5f-4ad1-8339-ae6f6f97b e8e.py", line 16, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_41.py
+++ Python_41.py
@@ -4,10 +4,10 @@ def car_race_collision(n: int):
     collisions = 0
 
     while left_to_right < n and right_to_left >= 0:
-        if left_to_right < right_to_left:
+        if left_to_right < right_to_left or (left_to_right == right_to_left):
             left_to_right += 1
         else:
             right_to_left -= 1
         collisions += 1
 
-    return collisions - 1
+    return collisions
