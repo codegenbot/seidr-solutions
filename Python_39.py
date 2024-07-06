8968aee316c1a0dca5afadc8510490c62a9241df
beep boop <githubbot@vadim.me>
2024-07-06 09:36:22 +0200

    Solution 85 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/51amy07t Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/18d4714a-b7f7-48ab-8ff3-71453aa45 d9b.py", line 11, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/18d4714a-b7f7-48ab-8ff3-71453aa45 d9b.py", line 8, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/18d4714a-b7f7-48ab-8ff3-71453aa45 d9b.py", line 6, in prime_fib     return next(x for x in fibs if is_prime(x))            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/18d4714a-b7f7-48ab-8ff3-71453aa45 d9b.py", line 6, in <genexpr>     return next(x for x in fibs if is_prime(x))                                    ^^^^^^^^ NameError: name 'is_prime' is not defined

--- Python_39.py
+++ Python_39.py
@@ -1,13 +1,13 @@
 
 def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+   if n < 2:
+       return False
+   for i in range(2, int(n ** 0.5) + 1):
+       if n % i == 0:
+           return False
+   return True
 
-def prime_fib(n):
+def prime_fib(n: int):
     fibs = [0, 1]
     while len(fibs) <= n:
         fibs.append(fibs[-1] + fibs[-2])
