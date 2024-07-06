1ee82a8962a885831aabe27020e6f3e707724657
beep boop <githubbot@vadim.me>
2024-07-06 10:43:18 +0200

    Solution 22 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/u89qmsrm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/df87b37f-4a4a-4929-b90f-7ff900427 e6a.py", line 18, in <module>     check(parse_nested_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/df87b37f-4a4a-4929-b90f-7ff900427 e6a.py", line 15, in check     assert parse_nested_parens("(()(())((())))") == [4]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_6.py
+++ Python_6.py
@@ -1,17 +1,12 @@
 def parse_nested_parens(paren_string):
     result = []
     level = 0
-    stack = []
     for group in paren_string.split():
         level += 1
         for char in group:
             if char == "(":
-                stack.append(level)
+                level += 1
             elif char == ")":
-                if not stack:
-                    raise ValueError("Unbalanced parentheses")
                 level -= 1
-                result.append(stack.pop())
-        if level == 0:
-            result.append(level)
+        result.append(level)
     return result
