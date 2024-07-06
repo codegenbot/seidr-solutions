81baccd36de99a95f4fbaedce9c28e046b3d0088
beep boop <githubbot@vadim.me>
2024-07-06 19:00:33 +0200

    Solution 81 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xkhiaef7 Prompt:       |             ^~~~~~ 9f90b29d-2f95-4974-8099-fc722ea21e1b.cpp:26:20: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 9f90b29d-2f95-4974-8099-fc722ea21e1b.cpp:26:27: error: ‘vector’ was not declared  in this scope    26 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9f90b29d-2f95-4974-8099-fc722ea21e1b.cpp:26:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 9f90b29d-2f95-4974-8099-fc722ea21e1b.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 9f90b29d-2f95-4974-8099-fc722ea21e1b.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9f90b29d-2f95-4974-8099-fc722ea21e1b.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -11,7 +11,7 @@ bool std::vector<int> a, std::vector<int> b) {
 }
 
 std::vector<int> maximum(std::vector<int> arr,int k){
-    std::priority_queue<int> max_heap;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> max_heap;
     for(int i:arr){
         max_heap.push(i);
         if(max_heap.size()>k)
