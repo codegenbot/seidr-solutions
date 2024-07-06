9d4201733a7b94679c965d5d8e33cb9c7daf4cef
beep boop <githubbot@vadim.me>
2024-07-06 10:25:54 +0200

    Solution 11 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/rxbdq4ku Prompt:       |           ^~~~~~ In file included from 8f224698-d644-450d-bb20-5b80add67c40.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8f224698-d644-450d-bb20-5b80add67c40.cpp:9:26: error: expected primary-expressio n before ‘int’     9 | int specialFilter(vector<int> nums) {       |                          ^~~ 8f224698-d644-450d-bb20-5b80add67c40.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ 8f224698-d644-450d-bb20-5b80add67c40.cpp:3:5: note: ‘int main()’ previously defi ned here     3 | int main() {       |     ^~~~ In file included from 8f224698-d644-450d-bb20-5b80add67c40.cpp:19: 8f224698-d644-450d-bb20-5b80add67c40.cpp: In function ‘int main()’: 8f224698-d644-450d-bb20-5b80add67c40.cpp:21:26: error: ‘specialFilter’ cannot be  used as a function    21 |     assert (specialFilter({}) == 0                   );       |             ~~~~~~~~~~~~~^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <vector>
 
-int specialFilter(vector<int> nums) {
+int specialFilter(std::vector<int> nums) {
     int count = 0;
     for (int num : nums) {
-        if (abs(num) > 10 && (num % 10) % 2 != 0 && (num / 10) % 10 % 2 != 0) {
+        if (std::abs(num) > 10 && (num % 10) % 2 != 0 && (num / 10) % 10 % 2 != 0) {
             count++;
         }
     }
@@ -12,19 +12,20 @@ int specialFilter(vector<int> nums) {
 }
 
 int main() {
-    vector<int> nums;
-    int n;
-    cout << "Enter number of elements: ";
-    cin >> n;
-    nums.resize(n);
-    
-    for (int i = 0; i < n; i++) {
-        cout << "Enter element " << i+1 << ": ";
-        cin >> nums[i];
+    std::vector<int> nums;
+    int num;
+
+    cout << "Enter numbers (0 to stop):" << endl;
+
+    while (true) {
+        cin >> num;
+        if (num == 0)
+            break;
+        nums.push_back(num);
     }
-    
+
     int count = specialFilter(nums);
-    
+
     cout << "Count: " << count << endl;
     return 0;
 }
