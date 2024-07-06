3f1b34fa44c82c02db76492f9ccae9b2091ddf25
beep boop <githubbot@vadim.me>
2024-07-06 19:29:15 +0200

    Solution 6 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/bn9yd81p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9ecd5541-c169-4938-8472-f00ad3ecc 997.py", line 9, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9ecd5541-c169-4938-8472-f00ad3ecc 997.py", line 6, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,3 +1,5 @@
 ```
 def sorted_list_sum(lst):
-    return sorted([word for word in lst if len(word) % 2 == 0], key=lambda x: (len(x), x))
+    evens = [word for word in lst if len(word) % 2 == 0]
+    odd = [word for word in lst if len(word) % 2 != 0]
+    return [word for word in sorted(evens, key=lambda x: (len(x), x)) + odd]
