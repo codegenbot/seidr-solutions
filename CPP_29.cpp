e2a7119ca587a235945bfde7b3800490369eed1f
beep boop <githubbot@vadim.me>
2024-07-06 12:39:16 +0200

    Solution 86 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/t0yv1tzr Prompt:    20 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8caca2bf-4443-446f-aa92-ebf32aec4722.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8caca2bf-4443-446f-aa92-ebf32aec4722.cpp:20:45: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8caca2bf-4443-446f-aa92-ebf32aec4722.cpp:19: 8caca2bf-4443-446f-aa92-ebf32aec4722.cpp: In function ‘int main()’: 8caca2bf-4443-446f-aa92-ebf32aec4722.cpp:29:20: error: ‘filter_by_prefix’ was no t declared in this scope    29 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 8caca2bf-4443-446f-aa92-ebf32aec4722.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -4,7 +4,6 @@
 #include <string>
 
 vector<string> b;
-
 bool issame(vector<string> a, vector<string> b) {
     if(a.size() != b.size()) return false;
     for(int i = 0; i < a.size(); ++i) {
@@ -20,6 +19,7 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
             result.push_back(s);
     }
     return result;
+}
 
 int main() {
     vector<string> strings = {"xxx", "asd", "xxy", "john doe", "xxxA", "xxx"};
