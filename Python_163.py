0ca758a85d7390ee73ce3bc39eecbeab2471810e
beep boop <githubbot@vadim.me>
2024-07-06 19:37:20 +0200

    Solution 1 of Python/163, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4h0rkn7c Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0cec9719-c817-4bd3-984f-5aafd640d dd1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_163.py
+++ Python_163.py
@@ -1,3 +1,4 @@
-```
-def generate_integers(a, b):
+def generate_integers():
+    a = int(input("Enter the first integer: "))
+    b = int(input("Enter the second integer: "))
     return sorted([i for i in range(min(a,b), max(a,b)+1) if int(str(i)) % 2 == 0])
