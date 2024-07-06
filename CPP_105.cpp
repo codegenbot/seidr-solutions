408df08c968e494af6bf2660629c04d5279c59be
beep boop <githubbot@vadim.me>
2024-07-06 17:44:21 +0200

    Solution 42 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/horaq1dq Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ a29c2fdc-eaa8-4ac0-a06a-ff9b5036b039.cpp:19:25:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -4,18 +4,18 @@
 #include <algorithm>
 
 std::vector<std::string> by_length(std::vector<std::string> arr) {
-    std::vector<std::string> sortedArr;
-    for (const auto& str : arr){
-        if(str.length() >= 1 && str.length() <= 9)
-            sortedArr.push_back(str);
+    std::vector<int> sortedArr;
+    for(auto i : arr){
+        if(i.length() >= 1 && i.length() <= 9)
+            sortedArr.push_back(i.length());
     }
     sort(sortedArr.begin(), sortedArr.end());
     reverse(sortedArr.begin(), sortedArr.end());
+    std::vector<std::string> result;
     std::map<int, std::string> numToName = {{1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, 
                                              {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-    for (const auto& str : sortedArr){
-        int len = std::stoi(str);
-        result.push_back(std::to_string(len) + " " + numToName[len] + "!"); 
+    for(int i : sortedArr){
+        result.push_back(std::to_string(i) + " " + numToName[i] + "!");
     }
     return result;
 }
