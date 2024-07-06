9d726ae826de6ddf69e0f2002d8acf467d48b65e
beep boop <githubbot@vadim.me>
2024-07-06 23:54:06 +0200

    Solution 54 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3xk2tfuk Prompt: Complete the following code given the task description and function signature.

--- Python_10.py
+++ Python_10.py
@@ -1,11 +1,13 @@
-Here is the completed code:
-
+```
 def is_palindrome(string: str) -> bool:
+    """ Test if given string is a palindrome """
     return string == string[::-1]
 
+
 def make_palindrome(string: str) -> str:
-    if string == string[::-1]:
-        return string + string[::-1]
-    for i in range(len(string)):
-        if string[i:] == string[i:] + (string[:i][::-1]):
-            return string[:i] + string[i:][::-1] + string[i:]
+    s = string.lower()
+    j = len(s) - 1
+    for i in range(len(s) - 1, -1, -1):
+        if s[i] != s[j]:
+            return string + s[j+1::-1]
+        j -= 1
