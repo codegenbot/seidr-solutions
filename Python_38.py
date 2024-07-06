708634e5593442952f8eb81a32d5bc465671e257
beep boop <githubbot@vadim.me>
2024-07-06 03:45:29 +0200

    Solution 35 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/bl4f4nrb Prompt: Program did not receive expected input

--- Python_38.py
+++ Python_38.py
@@ -1,4 +1,4 @@
-def decode_cyclic(s):
+def decode_cyclic(s: str):
     if len(s) % 3 != 0:
         raise ValueError("Input string length must be divisible by 3")
     groups = [s[(3 * i) : min((3 * i + 3), len(s))] for i in range((len(s) + 2) // 3)]
