a992b517469ea11bdebf76a6e99aac3dd0da5908
beep boop <githubbot@vadim.me>
2024-07-06 10:40:14 +0200

    Solution 22 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/ov14dqn5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/010ff385-9647-48aa-8688-4545b0fe3 bd2.py", line 18, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/010ff385-9647-48aa-8688-4545b0fe3 bd2.py", line 15, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ AssertionError

--- Python_1.py
+++ Python_1.py
@@ -1,4 +1,5 @@
-def separate_paren_groups(paren_string):
+
+def separate_paren_groups(paren_string: str) -> list[str]:
     result = []
     depth = 0
     start = 0
@@ -7,7 +8,7 @@ def separate_paren_groups(paren_string):
             depth += 1
         elif paren_string[i] == ")":
             depth -= 1
-            if depth == 0 and start < i + 1:
+            if depth == 0 and start < len(paren_string):
                 result.append(paren_string[start : i + 1])
                 start = i + 1
     return result
