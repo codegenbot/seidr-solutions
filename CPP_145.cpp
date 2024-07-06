7d1eba42dde049df77d18c917b2009c8b7559e37
beep boop <githubbot@vadim.me>
2024-07-06 13:03:48 +0200

    Solution 71 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6h0099y8 Prompt: on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3a06345e-5664-4946-9516-629a862d9053.cpp:28:39: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 3a06345e-5664-4946-9516-629a862d9053.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 3a06345e-5664-4946-9516-629a862d9053.cpp:5:5: note: ‘int main()’ previously defi ned here     5 | int main() {       |     ^~~~ In file included from 3a06345e-5664-4946-9516-629a862d9053.cpp:27: 3a06345e-5664-4946-9516-629a862d9053.cpp: In function ‘int main()’: 3a06345e-5664-4946-9516-629a862d9053.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -10,8 +10,7 @@ int main() {
 }
 
 std::vector<int> order_by_points(std::vector<int> nums) {
-    std::vector<int> res = nums;
-    sort(res.begin(), res.end(), [&](int a, int b) {
+    auto compare = [&](int a, int b) {
         if (a == b) return 0;
         long sumA = 0, sumB = 0;
         for (char c : std::to_string(a)) sumA += c - '0';
@@ -19,6 +18,8 @@ std::vector<int> order_by_points(std::vector<int> nums) {
         if (sumA != sumB) return sumA - sumB > 0 ? 1 : -1;
         return distance(nums.begin(), find(nums.begin(), nums.end(), a)) -
                distance(nums.begin(), find(nums.begin(), nums.end(), b));
-    });
+    };
+    std::vector<int> res = nums;
+    sort(res.begin(), res.end(), compare);
     return res;
 }
