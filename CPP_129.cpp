7ee574bd3c6b386487ef603a316860f2c9bb753a
beep boop <githubbot@vadim.me>
2024-07-06 19:24:14 +0200

    Solution 89 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1kalf9to Prompt:    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ e78be36a-5a17-41df-903d-022da9ceba25.cpp:21:10: error: ‘int main()’ redeclared a s different kind of entity    21 | int main(){       |          ^ e78be36a-5a17-41df-903d-022da9ceba25.cpp:1:5: note: previous declaration ‘int ma in’     1 | int main {       |     ^~~~ In file included from e78be36a-5a17-41df-903d-022da9ceba25.cpp:12: e78be36a-5a17-41df-903d-022da9ceba25.cpp: In function ‘int main()’: e78be36a-5a17-41df-903d-022da9ceba25.cpp:22:20: error: ‘minPath’ was not declare d in this scope    22 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ e78be36a-5a17-41df-903d-022da9ceba25.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -10,38 +10,5 @@ int main() {
 }
 
 vector<int> minPath(vector<vector<int>>& grid, int k) {
-    int n = grid.size();
-    if(n == 1)
-        return vector<int>(1, grid[0][0]);
     
-    if(k > n*n)
-        return {};
-
-    vector<bool> visited(n*n+1, false);
-    vector<int> path;
-    minPathHelper(grid, k, 0, path, visited);
-    return path;
-}
-
-void minPathHelper(vector<vector<int>>& grid, int k, int pos, vector<int>& path, vector<bool>& visited) {
-    if(path.size() == k) {
-        cout << "Path: ";
-        for (auto val : path)
-            cout << val << " ";
-        cout << endl;
-        return;
-    }
-    
-    for(int i = 0; i < grid.size(); i++) {
-        for(int j = 0; j < grid[0].size(); j++) {
-            int idx = i*grid[0].size() + j;
-            if(!visited[idx] && path.empty() || grid[i][j] == path.back()+1) {
-                visited[idx] = true;
-                path.push_back(grid[i][j]);
-                minPathHelper(grid, k, idx+1, path, visited);
-                path.pop_back();
-                visited[idx] = false;
-            }
-        }
-    }
 }
