34980bc103ae659b30b122c5c38d3e994528a8fd
beep boop <githubbot@vadim.me>
2024-07-06 06:43:32 +0200

    Solution 1 of Python/63, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/tiyowbvl Prompt: Complete the following code given the task description and function signature.

--- Python_63.py
+++ Python_63.py
@@ -3,11 +3,12 @@ def fibfib(n: int):
     if n <= 2:
         return 0
     else:
-        a = 1
+        a = 0
         b = 0
-        c = 0
+        c = 1
         for i in range(3, n+1):
-            c = a + b + fibfib(i-3)
+            d = a + b + c
             a = b
             b = c
+            c = d
         return c
