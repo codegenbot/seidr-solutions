5a82e29cf66a3a45ad98de2e400fc1088a425312
beep boop <githubbot@vadim.me>
2024-07-06 16:29:32 +0200

    Solution 15 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5j0y4o2g Prompt:       |                              ^~~~~~ 6529f765-2642-4b48-9012-79416959da5a.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 6529f765-2642-4b48-9012-79416959da5a.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6529f765-2642-4b48-9012-79416959da5a.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 6529f765-2642-4b48-9012-79416959da5a.cpp:50: 6529f765-2642-4b48-9012-79416959da5a.cpp: In function ‘int main()’: 6529f765-2642-4b48-9012-79416959da5a.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 6529f765-2642-4b48-9012-79416959da5a.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,21 +1,7 @@
-vector<string> by_length(vector<int> arr) {
-    vector<int> nums;
-    for (int num : arr) {
-        if (num >= 1 && num <= 9) {
-            nums.push_back(num);
-        }
-    }
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
+}
 
-    sort(nums.begin(), nums.end());
-    reverse(nums.begin(), nums.end());
-
-    vector<string> result;
-    map<int, string> numStrMap = {{1, "One"}, {2, "Two"}, {3, "Three"}, 
-                                  {4, "Four"}, {5, "Five"}, {6, "Six"}, 
-                                  {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-    for (int num : nums) {
-        result.push_back(numStrMap[num]);
-    }
-
-    return result;
+int main() {
+    assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
 }
