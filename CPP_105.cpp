44878b35ff466182c348216c8ad51da5c52f6029
beep boop <githubbot@vadim.me>
2024-07-06 06:08:12 +0200

    Solution 13 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3l504wnm Prompt: In file included from /usr/include/c++/11/string:39,                  from ad11e244-b5b3-4d3d-a728-989b1c97e179.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from ad11e244-b5b3-4d3d-a728-989b1c97e179.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ ad11e244-b5b3-4d3d-a728-989b1c97e179.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from ad11e244-b5b3-4d3d-a728-989b1c97e179.cpp:50: ad11e244-b5b3-4d3d-a728-989b1c97e179.cpp: In function ‘int main()’: ad11e244-b5b3-4d3d-a728-989b1c97e179.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ ad11e244-b5b3-4d3d-a728-989b1c97e179.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -43,15 +43,9 @@ vector<string> by_length(vector<int> arr) {
 }
 
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) {
-        return false;
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
-    
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-
     return true;
 }
