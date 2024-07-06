85ab8519773f067c2698a4bcf8594195d0aa0b9a
beep boop <githubbot@vadim.me>
2024-07-06 19:39:45 +0200

    Solution 14 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3f2yeli0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8c575602-cbc4-4e95-8bdb-13ecde31d b4e.py", line 18, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8c575602-cbc4-4e95-8bdb-13ecde31d b4e.py", line 15, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,14 +1,7 @@
-```
 def cycpattern_check(a, b):
     if len(b) == 0:
         return True
-    if a == b:
-        return True
     return (a == b) or (
         (len(a) - 1) * len(b) <= len(a)
-        and all(
-            a[i : i + len(b)] == b or a[:i] + a[i + len(b) :] == b
-            for i in range(len(a) - len(b) + 1)
-        )
-        or (a[: len(b)] == b and a[len(b) :].join("") == b)
+        and all(a[i:i+len(b)] == b for i in range(len(a)-len(b)+1))
     )
