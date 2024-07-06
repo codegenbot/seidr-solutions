d01eb651fdcd722b813b8810b6d4470b4681f7f0
beep boop <githubbot@vadim.me>
2024-07-06 03:31:57 +0200

    Solution 17 of CPP/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/f8rzrsvw Prompt: 50df4027-7ff5-4065-86da-dfe65f8f6fa9.cpp:1:30: error: ‘string’ is not a member o f ‘std’     1 | bool correct_bracketing(std::string brackets) {       |                              ^~~~~~ 50df4027-7ff5-4065-86da-dfe65f8f6fa9.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | bool correct_bracketing(std::string brackets) { In file included from 50df4027-7ff5-4065-86da-dfe65f8f6fa9.cpp:11: 50df4027-7ff5-4065-86da-dfe65f8f6fa9.cpp: In function ‘int main()’: 50df4027-7ff5-4065-86da-dfe65f8f6fa9.cpp:13:36: error: ‘correct_bracketing’ cann ot be used as a function    13 |     assert (not (correct_bracketing("()()(()())()))()")));       |                  ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -1,10 +1,16 @@
 #include <string>
+#include <assert.h>
 
-int correct_bracketing(std::string brackets) {
+bool correct_bracketing(std::string brackets) {
     int count = 0;
     for (char c : brackets) {
         if (c == '(') count++;
         else if (count > 0) count--;
     }
-    return count;
+    return count == 0;
+}
+
+int main() {
+    assert(!correct_bracketing("()()(()())()))()"));
+    return 0;
 }
