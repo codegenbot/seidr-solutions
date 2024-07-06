1632c1ad0e90c84e5688d9ff6ee4fd024d8ca635
beep boop <githubbot@vadim.me>
2024-07-06 08:13:22 +0200

    Solution 92 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/9f3y63v2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b155ef14-4e11-4f4e-adb2-440e0166f 11a.py", line 29, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b155ef14-4e11-4f4e-adb2-440e0166f 11a.py", line 26, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b155ef14-4e11-4f4e-adb2-440e0166f 11a.py", line 9, in minPath     dp = [[float("inf") for _ in range(m)] for _ in range(n)]                                                           ^ NameError: name 'n' is not defined

--- Python_129.py
+++ Python_129.py
@@ -1,13 +1,9 @@
-def minPath(grid, k):
-    n = len(grid)
-    m = len(grid[0])
-    dp = [[float("inf") for _ in range(m)] for _ in range(n)]
+def minPath(grid, k, n):
+    dp = [[float("inf") for _ in range(n)] for _ in range(k)]
     dp[0][0] = grid[0][0]
-
-    for i in range(1, n):
-        for j in range(m):
+    for i in range(1, k):
+        for j in range(n):
             if i == 0 and j == 0:
                 continue
             dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
-
     return dp[-1][-1]
