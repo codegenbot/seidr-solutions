bfe96d8fcce790678c74d35f728dd70900889e34
beep boop <githubbot@vadim.me>
2024-07-06 16:13:38 +0200

    Solution 18 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1fv3kcsh Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/489e0c38-c2a8-422d-a46f-5e6cb1254 dfa.py", line 21, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/489e0c38-c2a8-422d-a46f-5e6cb1254 dfa.py", line 16, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,12 +1,12 @@
 def sort_even(l: list):
-    even_idx = [i for i, x in enumerate(l) if x % 2 == 0]
-    sorted_evens = sorted([x for x in l if x % 2 == 0])
+    even_dict = {i: x for i, x in enumerate([x for x in l if x % 2 == 0])}
+    even_idx = [k for k in even_dict.keys()]
     
     result = []
     even_index = 0
     for i, x in enumerate(l):
         if i in even_idx:
-            result.append(sorted_evens[even_index % len(sorted_evens)])
+            result.append(even_dict[i])
             even_index += 1
         else:
             result.append(x)
