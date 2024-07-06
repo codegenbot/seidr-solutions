c6c8785ed98508046d97542b52f5daf81502414d
beep boop <githubbot@vadim.me>
2024-07-06 15:51:16 +0200

    Solution 50 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0yee0l13 Prompt:       |                              ^~~~~~ b47241b7-9e2c-4cf1-b2aa-45a8cbcb7788.cpp:11:37: error: ‘string’ was not declared  in this scope    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b47241b7-9e2c-4cf1-b2aa-45a8cbcb7788.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ b47241b7-9e2c-4cf1-b2aa-45a8cbcb7788.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from b47241b7-9e2c-4cf1-b2aa-45a8cbcb7788.cpp:10: b47241b7-9e2c-4cf1-b2aa-45a8cbcb7788.cpp: In function ‘int main()’: b47241b7-9e2c-4cf1-b2aa-45a8cbcb7788.cpp:20:20: error: ‘numerical_letter_grade’ was not declared in this scope    20 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ b47241b7-9e2c-4cf1-b2aa-45a8cbcb7788.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -10,27 +10,27 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return true;
 }
 
-std::vector<std::string> numerical_letter_grade(const std::vector<int>& scores) {
+std::vector<std::string> numerical_letter_grade(std::vector<int> scores){
     std::vector<std::string> grades;
-    
-    for(auto score : scores){
-        if(score >= 90.0) 
+    for(int i=0; i<scores.size(); i++){
+        int score = scores[i];
+        if(score < 60)
+            grades.push_back("F");
+        else if(score >= 90) 
             grades.push_back("A");
-        else if(score >= 80.0) 
+        else if(score >=80)
             grades.push_back("B");
-        else if(score >= 70.0) 
+        else if(score >=70) 
             grades.push_back("C");
-        else if(score >= 60.0) 
+        else
             grades.push_back("D");
-        else 
-            grades.push_back("E");
     }
-    
     return grades;
 }
 
 int main() {
-    std::vector<int> scores = {90, 80};
-    assert(issame(numerical_letter_grade(scores), {"A", "B"}));
+    std::vector<int> scores = {0, 0.7};
+    std::vector<std::string> grades = {"E", "D-" };
+    assert(issame(numerical_letter_grade(scores), grades));
     return 0;
 }
