0564766a77c09a7a32aa5f4a8a4fcfe60edee295
beep boop <githubbot@vadim.me>
2024-07-06 16:17:00 +0200

    Solution 15 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l4ahvohl Prompt:       |                    ^~~ 458a7280-fdab-415c-bc1b-7c9685ca8a6d.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 458a7280-fdab-415c-bc1b-7c9685ca8a6d.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 458a7280-fdab-415c-bc1b-7c9685ca8a6d.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 458a7280-fdab-415c-bc1b-7c9685ca8a6d.cpp:15: 458a7280-fdab-415c-bc1b-7c9685ca8a6d.cpp: In function ‘int main()’: 458a7280-fdab-415c-bc1b-7c9685ca8a6d.cpp:25:20: error: ‘make_a_pile’ was not dec lared in this scope    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 458a7280-fdab-415c-bc1b-7c9685ca8a6d.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,18 +1,14 @@
-#include <iostream>
 #include <vector>
+#include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-std::vector<std::int32_t> make_a_pile(int n) {
-    std::vector<std::int32_t> pile;
+std::vector<int> make_a_pile(int n) {
+    std::vector<int> pile;
     int numStones = 1;
     for (int i = 0; i < n; i++) {
         if (i % 2 == 0) {
             numStones += 2;
         } else {
-            numStones += 1;
+            numStones++;
         }
         pile.push_back(numStones);
     }
