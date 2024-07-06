f510fcb47823bd82d6ebdd906e4ea95748cf59cb
beep boop <githubbot@vadim.me>
2024-07-06 21:01:57 +0200

    Solution 97 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7aum6t7i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26e7ffe4-dccb-467e-b943-4b1c7fcd6 06b.py", line 22, in <module>     main()   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26e7ffe4-dccb-467e-b943-4b1c7fcd6 06b.py", line 19, in main     print(sum_squares([{"x": [2]}, {"y": [4]}]))           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26e7ffe4-dccb-467e-b943-4b1c7fcd6 06b.py", line 8, in sum_squares     total_sum += sum([value**2 for key, value in element.items()])                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26e7ffe4-dccb-467e-b943-4b1c7fcd6 06b.py", line 8, in <listcomp>     total_sum += sum([value**2 for key, value in element.items()])                       ~~~~~^^~ TypeError: unsupported operand type(s) for ** or pow(): 'list' and 'int'

--- Python_142.py
+++ Python_142.py
@@ -1,23 +1,22 @@
 def sum_squares(lst):
     total_sum = 0
-    for element in lst:
-        if isinstance(element, dict):
-            for key, value in element.items():
-                if isinstance(value, (int, float)):
-                    total_sum += abs(value) ** 2
-                elif (
-                    isinstance(value, list) and not isinstance(value[0], list) and value
-                ):
-                    total_sum += sum([x**2 for x in value])
-        elif isinstance(element, list) and not isinstance(element[0], list) and element:
-            total_sum += sum([x**2 for x in element])
-        elif isinstance(element, (int, float)):
-            total_sum += abs(element) ** 2
+    if isinstance(lst, (int, float)):
+        total_sum += abs(lst) ** 2
+    elif isinstance(lst, list):
+        for element in lst:
+            if isinstance(element, dict):
+                total_sum += sum([v**2 for v in element.values()])
+            elif isinstance(element, list):
+                total_sum += sum([x**2 for x in element])
+            else:
+                raise ValueError(
+                    f"Invalid input '{element}' found. Input must contain only numbers."
+                )
     return total_sum
 
 
 def main():
-    print(sum_squares([1, 2, 3]))
+    print(sum_squares([{"x": [2]}, {"y": [4]}]))
 
 
 main()
