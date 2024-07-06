052ad9eb43420b9e647e11494a12bf1af40619b0
beep boop <githubbot@vadim.me>
2024-07-06 21:18:49 +0200

    Solution 83 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/u2o18bue Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4936e9d0-5068-48dc-a696-ff0d75af8 a68.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,9 +1,11 @@
-def solution():
-    while True:
-        try:
-            first_half_sum = int(input("Enter first half sum: "))
-            second_half_sum = int(input("Enter second half sum: "))
-            print(abs(first_half_sum - 2*second_half_sum))
-        except ValueError:
-            print("Invalid input. Please enter a whole number for both sums.")
-        solution()
+while True:
+    def solution():
+        while True:
+            try:
+                first_half_sum = int(input("Enter first half sum: "))
+                second_half_sum = int(input("Enter second half sum: "))
+                return abs(first_half_sum - 2*second_half_sum)
+            except ValueError:
+                print("Invalid input. Please enter a whole number for both sums.")
+
+    print(solution())
