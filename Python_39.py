f6a53110cb64523d7378dd778ad09858c3cbcca8
beep boop <githubbot@vadim.me>
2024-07-06 08:54:58 +0200

    Solution 2 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/51amy07t Prompt: Complete the following code given the task description and function signature.

--- Python_39.py
+++ Python_39.py
@@ -1,10 +1,6 @@
 
 def prime_fib(n: int):
     fib = [0, 1]
-    i = 2
-    while len(fib) <= n:
-        fib.append(fib[i-2] + fib[i-1])
-        if fib[-1] % 2 == 0 and fib[-1] != 2:
-            fib.pop()
-        i += 1
-    return fib[-1]
+    for i in range(2, n+1):
+        fib.append(fib[i-1] + fib[i-2])
+    return fib[n]
