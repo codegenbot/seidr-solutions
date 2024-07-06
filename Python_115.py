dd7791ce303a37f346143a2fac3cc30e96df9fbc
beep boop <githubbot@vadim.me>
2024-07-06 10:05:31 +0200

    Solution 7 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/9e49uhco Prompt: Complete the following code given the task description and function signature.

--- Python_115.py
+++ Python_115.py
@@ -1,8 +1,8 @@
+
 def max_fill(grid, capacity):
     count = 0
     for row in grid:
-        for i in range(len(row)):
-            if row[i] == 1 and capacity > 0:
-                count += 1
-                capacity -= 1
+        water = sum(row)
+        if water > 0:
+            count += math.ceil(water / capacity)
     return count
