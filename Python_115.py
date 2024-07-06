1de05fbcdd24c1c234a480dd5e4c68b438cfab9c
beep boop <githubbot@vadim.me>
2024-07-06 02:47:03 +0200

    Solution 81 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/dx5s3j3u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6257fae7-0d40-4c5f-972b-4abb20f1e 6e1.py", line 1     def max_fill(grid, capacity): IndentationError: unexpected indent

--- Python_115.py
+++ Python_115.py
@@ -1,12 +1,12 @@
-def max_fill(grid, capacity):
-    rows = len(grid)
-    cols = len(grid[0])
-    water = 0
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                water += 1
-    if capacity < water:
-        return -1
-    else:
-        return math.floor(water / capacity)
+    def max_fill(grid, capacity):
+        rows = len(grid)
+        cols = len(grid[0])
+        water = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    water += 1
+        if capacity < water:
+            return -1
+        else:
+            return math.floor(water / capacity)
