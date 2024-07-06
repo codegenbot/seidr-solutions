b2f18d61701122ed6aa643de4a1be4377ca3ef79
beep boop <githubbot@vadim.me>
2024-07-06 11:10:27 +0200

    Solution 63 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/i9liy1qo Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3b0cd5f-c72c-4f27-9001-6d30ee511 44c.py", line 14, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3b0cd5f-c72c-4f27-9001-6d30ee511 44c.py", line 11, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3b0cd5f-c72c-4f27-9001-6d30ee511 44c.py", line 3, in fruit_distribution     count_apples = int(                    ^^^^ ValueError: invalid literal for int() with base 10: ''

--- Python_67.py
+++ Python_67.py
@@ -1,7 +1,8 @@
+```
 count_apples = 0
 for word in words:
     if "apples" in word:
-        count_apples += int("".join(filter(str.isdigit, word)))
+        count_apples += int(''.join(filter(str.isdigit, word)))
 if not count_apples:
     return n - 0
 return n - count_apples
