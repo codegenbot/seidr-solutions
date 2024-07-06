9cf33853f32c10b32e93161456f1293792e10767
beep boop <githubbot@vadim.me>
2024-07-06 14:07:49 +0200

    Solution 24 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/g0smtz44 Prompt: 5489c8e9-c760-49d1-8f97-0de757c41437.cpp:9:9: error: expected primary-expression  before ‘int’     9 |     set<int> s2(l2.begin(), l2.end());       |         ^~~ 5489c8e9-c760-49d1-8f97-0de757c41437.cpp:11:18: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    11 |     for (int i : s1) {       |                  ^~       |                  l1 5489c8e9-c760-49d1-8f97-0de757c41437.cpp:12:13: error: ‘s2’ was not declared in this scope; did you mean ‘l2’?    12 |         if (s2.find(i) != s2.end()) {       |             ^~       |             l2 5489c8e9-c760-49d1-8f97-0de757c41437.cpp: At global scope: 5489c8e9-c760-49d1-8f97-0de757c41437.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ 5489c8e9-c760-49d1-8f97-0de757c41437.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,13 +1,14 @@
+```cpp
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <set>
 using namespace std;
 
 vector<int> common(vector<int> l1, vector<int> l2) {
-    vector<int> result;
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
+    vector<int> result;
     for (int i : s1) {
         if (s2.find(i) != s2.end()) {
             result.push_back(i);
