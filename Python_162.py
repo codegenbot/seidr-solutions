da6f47cf9df2466cc97f8889fe2c6dd1077c6e18
beep boop <githubbot@vadim.me>
2024-07-06 20:38:03 +0200

    Solution 38 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/p1m4pdj1 Prompt: Program did not receive expected input

--- Python_162.py
+++ Python_162.py
@@ -2,13 +2,13 @@ import hashlib
 
 
 def string_to_md5(text):
-    return hashlib.md5(text).hexdigest()
+    return hashlib.md5(text.encode("utf-8")).hexdigest()
 
 
 def main():
     while True:
-        text = input("Please enter a string (or 'q' to quit): ").encode("utf-8")
-        if text.decode().lower() == "q":
+        text = input("Please enter a string (or 'q' to quit): ")
+        if text.lower() == "q":
             break
         try:
             result = string_to_md5(text)
