41c4458103686ec41137a99c94097c5db099d91c
beep boop <githubbot@vadim.me>
2024-07-06 09:11:06 +0200

    Solution 2 of CPP/136, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ccmd7gcc Prompt: d62aa9fb-d1df-403a-8a9f-764a15c08ab9.cpp: In function ‘std::vector<int> largest_ smallest_integers(std::vector<int>)’: d62aa9fb-d1df-403a-8a9f-764a15c08ab9.cpp:7:24: error: ‘INT_MAX’ was not declared  in this scope     7 |     int min_positive = INT_MAX;       |                        ^~~~~~~ d62aa9fb-d1df-403a-8a9f-764a15c08ab9.cpp:3:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     2 | #include <vector>   +++ |+#include <climits>     3 | using namespace std; d62aa9fb-d1df-403a-8a9f-764a15c08ab9.cpp: At global scope: d62aa9fb-d1df-403a-8a9f-764a15c08ab9.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ d62aa9fb-d1df-403a-8a9f-764a15c08ab9.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,10 +1,35 @@
-Here is the completed code:
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
 
 vector<int> largest_smallest_integers(vector<int> lst) {
-    int maxNeg = 0, minPos = INT_MAX;
+    int max_negative = 0;
+    int min_positive = INT_MAX;
+
     for (int num : lst) {
-        if (num < 0 && num > maxNeg) maxNeg = num;
-        else if (num > 0 && num < minPos) minPos = num;
+        if (num < 0 && num > max_negative) {
+            max_negative = num;
+        } else if (num > 0 && num < min_positive) {
+            min_positive = num;
+        }
     }
-    return {(maxNeg >= 0 ? 0 : maxNeg), (minPos <= 0 ? 0 : minPos)};
+
+    return {(max_negative >= 0 ? 0 : max_negative), (min_positive <= 0 ? 0 : min_positive)};
+}
+
+int main1() {
+    vector<int> lst1 = {2, 4, 1, 3, 5, 7};
+    vector<int> result1 = largest_smallest_integers(lst1);
+    cout << "Largest negative integer: " << result1[0] << ", Smallest positive integer: " << result1[1] << endl;
+
+    vector<int> lst2 = {};
+    vector<int> result2 = largest_smallest_integers(lst2);
+    cout << "Largest negative integer: " << result2[0] << ", Smallest positive integer: " << result2[1] << endl;
+
+    vector<int> lst3 = {0};
+    vector<int> result3 = largest_smallest_integers(lst3);
+    cout << "Largest negative integer: " << result3[0] << ", Smallest positive integer: " << result3[1] << endl;
+    
+    return 0;
 }
