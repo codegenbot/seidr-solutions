21e66260da30d40a2e8b2da762933c83d8b8776a
beep boop <githubbot@vadim.me>
2024-07-06 04:13:21 +0200

    Solution 80 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/yny9bc24 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/00c75078-1183-44d9-b91b-c3ab9d969 e8b.py", line 10, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/00c75078-1183-44d9-b91b-c3ab9d969 e8b.py", line 7, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/00c75078-1183-44d9-b91b-c3ab9d969 e8b.py", line 5, in prime_fib     return fib[-1] if is_prime(fib[-1]) else None                       ^^^^^^^^ NameError: name 'is_prime' is not defined

--- Python_39.py
+++ Python_39.py
@@ -11,4 +11,7 @@ def prime_fib(n):
     fib = [0, 1]
     for i in range(2, n + 1):
         fib.append(fib[i - 1] + fib[i - 2])
-    return fib[-1] if is_prime(fib[-1]) else None
+    if is_prime(fib[-1]):
+        return fib[-1]
+    else:
+        return None
