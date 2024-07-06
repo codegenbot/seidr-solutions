2de45d8bf2735e10b1f0538f9b657db8d7978fc6
beep boop <githubbot@vadim.me>
2024-07-06 19:32:13 +0200

    Solution 97 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1kalf9to Prompt:    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 95a1c70f-795f-4915-9837-356826cf39b9.cpp:21:10: error: ‘int main()’ redeclared a s different kind of entity    21 | int main(){       |          ^ 95a1c70f-795f-4915-9837-356826cf39b9.cpp:1:5: note: previous declaration ‘int ma in’     1 | int main {       |     ^~~~ In file included from 95a1c70f-795f-4915-9837-356826cf39b9.cpp:12: 95a1c70f-795f-4915-9837-356826cf39b9.cpp: In function ‘int main()’: 95a1c70f-795f-4915-9837-356826cf39b9.cpp:22:20: error: ‘minPath’ was not declare d in this scope    22 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 95a1c70f-795f-4915-9837-356826cf39b9.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,14 +1,3 @@
-int main() {
-    vector<vector<int>> grid = {{1,2,3}, {4,5,6}, {7,8,9}};
-    int k = 3;
-    vector<int> result = minPath(grid, k);
-    for (auto val : result) {
-        cout << val << " ";
-    }
-    cout << endl;
-    return 0;
-}
-
 #include <vector>
 #include <algorithm>
 
@@ -18,4 +7,15 @@ bool issame(vector<int> a, vector<int> b) {
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
     // implementation of minPath function
+}
+
+int main() {
+    vector<vector<int>> grid = {{1,2,3}, {4,5,6}, {7,8,9}};
+    int k = 3;
+    vector<int> result = minPath(grid, k);
+    for (auto val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+    return 0;
 }
