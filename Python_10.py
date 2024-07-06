5956864a8c974a6d98fdd3e6d2078206aff6b4bc
beep boop <githubbot@vadim.me>
2024-07-06 02:54:15 +0200

    Solution 41 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/zqqswxr4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/282584df-48e4-44ce-b592-533b97137 832.py", line 17, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/282584df-48e4-44ce-b592-533b97137 832.py", line 14, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,10 +1,7 @@
+
 def make_palindrome(string):
-    palindrome = []
-    for i, c in enumerate(string):
-        if len(string) % 2 == 0:
-            if string[i] != string[-i - 1]:
-                palindrome.append(c)
-                palindrome.append(string[-i - 1])
-        else:
-            palindrome.append(c)
-    return "".join(palindrome)
+    if len(string) % 2 == 1:
+        return "".join(reversed(string))
+    else:
+        mid = len(string) // 2
+        return string[:mid] + reversed(string[mid:])
