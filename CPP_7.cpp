c63162ad947b47428908e2e37c11853ff8060a78
beep boop <githubbot@vadim.me>
2024-07-06 10:30:31 +0200

    Solution 35 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q0l1s9e1 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ff1051f3-6bf9-40ab-9373-cb8c0f6fe4d2.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ff1051f3-6bf9-40ab-9373-cb8c0f6fe4d2.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ff1051f3-6bf9-40ab-9373-cb8c0f6fe4d2.cpp:10: ff1051f3-6bf9-40ab-9373-cb8c0f6fe4d2.cpp: In function ‘int main()’: ff1051f3-6bf9-40ab-9373-cb8c0f6fe4d2.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ ff1051f3-6bf9-40ab-9373-cb8c0f6fe4d2.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,12 +1,24 @@
 ```cpp
-vector<string> filter_by_substring(vector<string> strings, string substring){
-    vector<string> result;
-    for(string s : strings){
-        if(s.find(substring) != string::npos)
-            result.push_back(s);
-    }
-    return result;
-}
 bool issame(vector<string> a, vector<string> b){
     return a == b;
+}
+vector<vector<string>> filter_by_substring(vector<string> strings, string substring){
+    vector<vector<string>> result;
+    for(string s : strings){
+        bool is_same = true;
+        vector<string> temp;
+        temp.push_back(s);
+        for(int i = 0; i < 2; i++){
+            if(i == 1)
+                temp[0] = substring;
+            if(!issame(temp, strings)){
+                is_same = false;
+                break;
+            }
+            strings.pop_back();
+        }
+        if(is_same)    
+            result.push_back(temp);
+    }
+    return result;
 }
