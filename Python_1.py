8f6b4be119e79d6e7c636b01285057d3e1ba11ba
beep boop <githubbot@vadim.me>
2024-07-06 05:41:36 +0200

    Solution 52 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3ci03y9r Prompt: Program did not receive expected input

--- Python_1.py
+++ Python_1.py
@@ -1,21 +1,24 @@
 from typing import List
 
+
 def separate_paren_groups(paren_string: str) -> List[str]:
     result = []
     stack = []
-    temp_str = ''
-    
+    temp_str = ""
+
     for char in paren_string:
-        if char == ' ':
+        if char == " ":
             continue
-        if char == '(':
+        if char == "(":
             stack.append(char)
             temp_str += char
-        elif char == ')':
+        elif char == ")":
             stack.pop()
             temp_str += char
             if not stack:
                 result.append(temp_str)
-                temp_str = ''
-    
+                temp_str = ""
+    if temp_str and stack:
+        result.append("(" + temp_str)
+
     return result
