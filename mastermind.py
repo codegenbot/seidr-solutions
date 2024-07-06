b323b1ca9ccef89e999c5fcf0efe3a51295272e8
beep boop <githubbot@vadim.me>
2024-07-06 03:34:39 +0200

    Solution 16 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/c6jx08ft Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3059ff25-7997-47e0-a5c5-933e1817a 837.py", line 1     ```     ^ SyntaxError: invalid syntax

--- mastermind.py
+++ mastermind.py
@@ -1,3 +1,4 @@
+```
 def mastermind(code, guess):
     black = 0
     white = 0
@@ -6,11 +7,10 @@ def mastermind(code, guess):
     for i in range(4):
         if code[i] == guess[i]:
             black += 1
+        elif code_count[ord(code[i])-55] < guess_count[ord(code[i])-55]:
+            white += 1
         else:
-            code_char = code[i]
-            guess_char = guess[i]
-            code_count[ord(code_char)-55]++;
-            guess_count[ord(guess_char)-55]++;
+            code_count[ord(code[i])-55]++;
     for i in range(6):
         white += min(code_count[i], guess_count[i])
     return black, white
