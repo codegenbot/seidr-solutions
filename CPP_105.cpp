154f50137bd8f5d7793633ce0389b56ed49eea3a
beep boop <githubbot@vadim.me>
2024-07-06 17:14:53 +0200

    Solution 66 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5j0y4o2g Prompt:       |         ^~ 2cf93355-eed5-48bb-9075-34a28e07ca09.cpp:24:9: note: remove parentheses to defau lt-initialize a variable    24 | int main(){       |         ^~       |         -- 2cf93355-eed5-48bb-9075-34a28e07ca09.cpp:24:9: note: or replace parentheses with  braces to value-initialize a variable 2cf93355-eed5-48bb-9075-34a28e07ca09.cpp:24:11: error: a function-definition is not allowed here before ‘{’ token    24 | int main(){       |           ^ 2cf93355-eed5-48bb-9075-34a28e07ca09.cpp:26:2: error: expected ‘}’ at end of inp ut    26 | }       |  ^ 2cf93355-eed5-48bb-9075-34a28e07ca09.cpp:4:54: note: to match this ‘{’     4 | vector<pair<int, string>> by_length(vector<int> arr) {       |                                                      ^ 2cf93355-eed5-48bb-9075-34a28e07ca09.cpp:26:2: warning: no return statement in f unction returning non-void [-Wreturn-type]    26 | }       |  ^

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -2,7 +2,7 @@
 #include <map>
 using namespace std;
 
-vector<pair<int, string>> by_length(vector<int> arr) {
+void by_length(vector<int> arr) {
     vector<pair<int, string>> result;
     map<int, string> numToName = {{1,"One"}, {2,"Two"}, {3,"Three"}, {4,"Four"}, {5,"Five"}, {6,"Six"}, {7,"Seven"}, {8,"Eight"}, {9,"Nine"}; 
     for(int i: arr){
@@ -10,13 +10,10 @@ vector<pair<int, string>> by_length(vector<int> arr) {
     }
     sort(result.begin(), result.end());
     reverse(result.begin(), result.end());
-    return result;
+    return;
 }
 
 int main(){
     vector<int> input = {2,3,8,5};
-    vector<pair<int, string>> output = by_length(input);
-    for(auto i : output) {
-        cout << "Length: " << i.first << ", Name: " << i.second << endl;
-    }
+    by_length(input);
 }
