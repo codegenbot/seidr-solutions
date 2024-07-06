e51f9643075d59a9a7bfb590e67edd3de6863caf
beep boop <githubbot@vadim.me>
2024-07-06 20:23:14 +0200

    Solution 72 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/rmvd3wj8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3324518-da79-4839-aa93-9a9e85808 613.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,4 +1,3 @@
-```
 def fruit_distribution(fruits, amount):
     fruit_dict = {}
     for fruit in fruits.split(','):
@@ -6,5 +5,3 @@ def fruit_distribution(fruits, amount):
             fruit_dict[fruit.strip()] = 0
         fruit_dict[fruit.strip()] += 1
     return {k: v / amount * 100 for k, v in fruit_dict.items()}
-
-print(f"{fruit_distribution(input('Enter the list of fruits: '), int(input('Enter the amount: ')))}")
