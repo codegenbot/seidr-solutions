73ab8a22059669581b2f55be86a4dfe1a204b852
beep boop <githubbot@vadim.me>
2024-07-06 08:04:06 +0200

    Solution 26 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l2o5fvxv Prompt: Program failed

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,22 +1,13 @@
-#include <iostream>
-#include <string>
-#include <cassert>
-#include <cmath>
-
 std::string rounded_avg(int n, int m) {
     if (n > m)
         return "-1";
     double sum = 0;
     for (int i = n; i <= m; i++)
         sum += i;
-    double avg = std::round(sum / (double)(m - n + 1));
+    double avg = round(sum / (double)(m - n + 1));
     std::string res;
-    while (avg > 0) {
-        if (avg % 2 == 1)
-            res.push_back('1');
-        else
-            res.push_back('0');
+    while (avg >= 1) {
+        res.push_back((int)(avg)%2+'0');
         avg /= 2;
     }
     return res;
-}
