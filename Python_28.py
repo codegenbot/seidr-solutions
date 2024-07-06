3814fcca227974fd9049867358c28651a5b343b2
beep boop <githubbot@vadim.me>
2024-07-06 02:40:00 +0200

    Solution 41 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/h77vs2tk Prompt: Program did not receive expected input

--- Python_28.py
+++ Python_28.py
@@ -1,9 +1,8 @@
 from typing import List
 
-
 def concatenate(strings: List[str]) -> str:
-    result = "".join([s.strip() for s in strings]).strip()
-
+    result = ''.join([s.strip() for s in strings]).strip()
+    
     if len(result) == 0:
         return "Invalid input. Please enter some values."
     else:
