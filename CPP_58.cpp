748c5a9d29e026d6a35ad17ee772fca5af638d29
beep boop <githubbot@vadim.me>
2024-07-06 16:27:09 +0200

    Solution 89 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7bimcvpy Prompt: 0e48e037-c737-4569-aac9-a25a44878308.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ 0e48e037-c737-4569-aac9-a25a44878308.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -10,7 +10,7 @@ vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s2(l2.begin(), l2.end());
 
     set<int> intersection;
-    set_difference(s2.begin(), s2.end(), s1.begin(), s1.end(),
+    set_intersection(s2.begin(), s2.end(), s1.begin(), s1.end(),
                     inserter(intersection, intersection.begin()));
 
     vector<int> result;
