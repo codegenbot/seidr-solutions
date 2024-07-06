dba670a62091b5a4b0fb3c253ce8aeac6d32a75b
beep boop <githubbot@vadim.me>
2024-07-06 10:48:41 +0200

    Solution 76 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ssvdmxr6 Prompt:    15 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c20fef1a-d989-412e-9833-59c1996d4a35.cpp:15:44: error: ‘b’ was not declared in t his scope    15 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c20fef1a-d989-412e-9833-59c1996d4a35.cpp:15:45: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c20fef1a-d989-412e-9833-59c1996d4a35.cpp:14: c20fef1a-d989-412e-9833-59c1996d4a35.cpp: In function ‘int main()’: c20fef1a-d989-412e-9833-59c1996d4a35.cpp:24:20: error: ‘select_words’ was not de clared in this scope    24 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ c20fef1a-d989-412e-9833-59c1996d4a35.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,10 +1,9 @@
-```cpp
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+bool sameWords(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size()) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
-        if (!a[i][0].compare(0,1,b[i][0],0,1)) {
+        if (!same(a[i][0], b[i][0])) {
             return false;
         }
     }
