34009ae60f6701dacc9d4fccc0a00661abfc2afa
beep boop <githubbot@vadim.me>
2024-07-06 02:13:43 +0200

    Solution 40 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dsmr51i9 Prompt: 5765ebb2-5835-4e9b-977e-4b2bfe0910ad.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 5765ebb2-5835-4e9b-977e-4b2bfe0910ad.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <string>
-#include <cassert>
 
 using namespace std;
 
@@ -20,15 +19,14 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
         }
     }
     return result;
-}
 
+}
 int main() {
     vector<string> strings = {"xxx", "asd", "xxy", "john doe", "xxxxAAA", "xxx"};
     string prefix = "xxx";
-    
-    vector<string> expected_result = {"xxx", "xxxAAA", "xxx"};
-    vector<string> actual_result = filter_by_prefix(strings, prefix);
-    
-    assert(same(actual_result, expected_result));
+    vector<string> expectedResult = {"xxx", "xxxAAA", "xxx"};
+
+    assert(same(filter_by_prefix(strings, prefix), expectedResult));
+
     return 0;
 }
