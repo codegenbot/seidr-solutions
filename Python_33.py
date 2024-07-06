ecaa817d8a8c83fbfb8c72a73b96c2fd7ccb19f7
beep boop <githubbot@vadim.me>
2024-07-06 16:53:51 +0200

    Solution 39 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r6wjanum Prompt: [3, 5, 6, 4, 8, 9, 1, 2] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/13ae2783-d4f6-4c4b-a719-1fa65436a 13e.py", line 21, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/13ae2783-d4f6-4c4b-a719-1fa65436a 13e.py", line 16, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -3,9 +3,12 @@ def sort_third(l):
     result = []
     for i in range(0, len(l), 3):
         if i + 2 < len(l):
-            result.append([*sorted([l[i], l[i+1], l[i+2]])])
+            result.append(sorted([l[i], l[i+1], l[i+2]]))
         else:
-            result.append([l[i]])
+            if len(l) == i + 1:
+                result.append([l[i]])
+            elif len(l) == i + 2:
+                result.append(sorted([l[i], l[i+1]]))
     return [item for sublist in result for item in sublist]
 
 input_list = [5, 6, 3, 4, 8, 9, 2, 1]
