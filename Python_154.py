0b8a7a2d632584d906817e8d285af3bacfe783e2
beep boop <githubbot@vadim.me>
2024-07-06 20:10:52 +0200

    Solution 59 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3f2yeli0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2d38fa7f-7154-4e1e-981a-ac9c0ab46 e88.py", line 13, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2d38fa7f-7154-4e1e-981a-ac9c0ab46 e88.py", line 10, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,7 +1,10 @@
+```
 def cycpattern_check(a, b):
     if len(a) != len(b):
         return False
-    for i in range(len(a)):
-        if a[i:] + a[:i] != b:
-            return False
-    return True
+    a = sorted([c for c in a.lower()])
+    for i in range(len(b)):
+        b_rotated = [c for c in (b[i:] + b[:i]).lower()]
+        if str(a) == str(sorted(b_rotated)):
+            return True
+    return False
