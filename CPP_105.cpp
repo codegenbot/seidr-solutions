a0ef395f47dcf5045056e71b21b797fd1c0ebda2
beep boop <githubbot@vadim.me>
2024-07-06 18:13:52 +0200

    Solution 93 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/horaq1dq Prompt:       |                              ^~~~~~ 533ecd54-bd18-4923-8640-21ec1c0350ff.cpp:15:37: error: ‘string’ was not declared  in this scope    15 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 533ecd54-bd18-4923-8640-21ec1c0350ff.cpp:15:44: error: ‘b’ was not declared in t his scope    15 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 533ecd54-bd18-4923-8640-21ec1c0350ff.cpp:15:45: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 533ecd54-bd18-4923-8640-21ec1c0350ff.cpp:14: 533ecd54-bd18-4923-8640-21ec1c0350ff.cpp: In function ‘int main()’: 533ecd54-bd18-4923-8640-21ec1c0350ff.cpp:24:20: error: ‘by_length’ was not decla red in this scope    24 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 533ecd54-bd18-4923-8640-21ec1c0350ff.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,12 +1,27 @@
-#include <map>
-#include <vector>
 #include <string>
+#include <vector>
 
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+using namespace std;
+
+bool by_length(const vector<string>& a, const vector<string>& b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i < a.size(); ++i)
-        if (a[i] != b[i])
+        if (a[i].size() != b[i].size())
             return false;
     return true;
+}
+
+int main() {
+    vector<int> arr1 = {3, 2, 9};
+    vector<int> arr2 = {4, 7, 5};
+    vector<string> result1 = by_length(arr1);
+    vector<string> result2 = by_length(arr2);
+
+    if (by_length(result1, result2))
+        cout << "Arrays are same in terms of length" << endl;
+    else
+        cout << "Arrays are not same in terms of length" << endl;
+
+    return 0;
 }
