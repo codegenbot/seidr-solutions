d61771c8a1f623b94864648c72e08585628bdbfb
beep boop <githubbot@vadim.me>
2024-07-06 17:46:43 +0200

    Solution 58 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5ux7p9qd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0304b194-bb95-4326-b375-ba4a845b0 25d.py", line 8, in <module>     print(check(modp))           ^^^^^ NameError: name 'check' is not defined

--- Python_49.py
+++ Python_49.py
@@ -1,14 +1,7 @@
-```
-def check(f):
-    if f == modp:
-        return "Modular Polynomial Arithmetic Function is Correct"
-    else:
-        return "Error: Not a Modular Polynomial Arithmetic Function"
-
 def modp(x, y):
     if y == 0:
         return "Error: Division by zero"
     else:
         return x % y
 
-print(check(modp))
+print(modp(10,3))
