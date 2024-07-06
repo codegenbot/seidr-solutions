eb7d1ed41c10cf395518ff7c8f34f78407b1828d
beep boop <githubbot@vadim.me>
2024-07-06 20:05:07 +0200

    Solution 51 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3f2yeli0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d117cfee-3b47-4106-8e79-1e09c6202 259.py", line 17, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d117cfee-3b47-4106-8e79-1e09c6202 259.py", line 14, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,14 +1,6 @@
-```
 def cycpattern_check(a, b):
-    if len(b) == 0:
-        return True
-    if a == b:
-        return True
-    for i in range(len(a)):
-        rotated_a = a[i:] + a[:i]
-        if rotated_a == b or b in rotated_a:
+    min_len = min(len(a), len(b))
+    for i in range(min_len):
+        if a[i:] == b[:i]:
             return True
-    rotated_a = a + a
-    if rotated_a == b or b in rotated_a:
-        return True
     return False
