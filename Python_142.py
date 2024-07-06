ad74a53cb6f988dc0ddf5b8acbfaabff83a0ab64
beep boop <githubbot@vadim.me>
2024-07-06 20:30:19 +0200

    Solution 68 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7aum6t7i Prompt: 20 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7734700e-c14f-48c1-8265-d33b00f98 f1f.py", line 54, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7734700e-c14f-48c1-8265-d33b00f98 f1f.py", line 24, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,3 +1,4 @@
+```
 def sum_squares(lst):
     total_sum = 0
     for element in lst:
@@ -6,14 +7,12 @@ def sum_squares(lst):
         elif isinstance(element, (int, float)):
             total_sum += abs(element) ** 2
         else:
-            raise ValueError(
-                f"Invalid input '{element}' found. Input must contain only numbers."
-            )
+            raise ValueError(f"Invalid input '{element}' found. Input must contain only numbers.")
     return total_sum
 
 
 def main():
-    print(sum_squares([{"x": [2]}, {"y": [4]}]))
+    print(sum_squares([{"x": 2}, {"y": 4}]))
 
 
 main()
