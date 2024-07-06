c0a1fddb2617928100f1d9c2e6c56a627e4f256d
beep boop <githubbot@vadim.me>
2024-07-06 03:38:30 +0200

    Solution 36 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/yny9bc24 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cdfd9b13-5e81-46ab-bc50-b6d8e0a5e 005.py", line 26, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cdfd9b13-5e81-46ab-bc50-b6d8e0a5e 005.py", line 23, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_39.py
+++ Python_39.py
@@ -1,18 +1,5 @@
-import math
-
 def prime_fib(n):
     fib = [0, 1]
-    while len(fib) <= n:
-        fib.append(fib[-1] + fib[-2])
-    return fib[n]
-
-def is_prime(n):
-    if n < 2 or (math.isqrt(n) ** 2) != n:
-        return False
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-def check(n):
-    assert is_prime(prime_fib(n))
+    for i in range(2, n + 1):
+        fib.append(fib[i - 1] + fib[i - 2])
+    return fib[-1] if is_prime(fib[-1]) else None
