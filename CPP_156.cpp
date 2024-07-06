65f6c8ca74139bf40c55010e6b27c5ce097a8357
beep boop <githubbot@vadim.me>
2024-07-06 20:09:11 +0200

    Solution 88 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cge2wy3k Prompt: 94a30e88-399b-4940-a438-99922bef4c9f.cpp:1:6: error: ‘string’ in namespace ‘std’  does not name a type     1 | std::string intToMiniRoman(int number) {       |      ^~~~~~ 94a30e88-399b-4940-a438-99922bef4c9f.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | std::string intToMiniRoman(int number) { In file included from 94a30e88-399b-4940-a438-99922bef4c9f.cpp:18: 94a30e88-399b-4940-a438-99922bef4c9f.cpp: In function ‘int main()’: 94a30e88-399b-4940-a438-99922bef4c9f.cpp:20:13: error: ‘int_to_mini_romank’ was not declared in this scope    20 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,8 +1,7 @@
 #include <string>
-#include <vector>
 
 std::string intToMiniRoman(int number) {
-    std::vector<std::pair<int, std::string>> roman = {{1000, "M"}, {900, "CM"}, {500, "D"},
+    vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {500, "D"},
                                          {400, "CD"}, {100, "C"}, {90, "XC"},
                                          {50, "L"}, {40, "XL"}, {10, "X"},
                                          {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
