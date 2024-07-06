ba6cefbc1f0f9f97608338b3c20cd1de6c07a054
beep boop <githubbot@vadim.me>
2024-07-06 06:27:01 +0200

    Solution 75 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/f3w5181t Prompt: Program did not receive expected input

--- Python_4.py
+++ Python_4.py
@@ -3,6 +3,6 @@ from typing import List
 
 def mean_absolute_deviation(numbers: List[float]) -> float:
     if len([num for num in numbers if num != 0]) == 0:
-        return 
+        return
     mean = sum(numbers) / len(numbers)
     return sum(abs(num - mean) for num in numbers) / len(numbers)
