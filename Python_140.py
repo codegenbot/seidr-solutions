1e138ab1b91970ec83fb65a28ea45fb019c57116
beep boop <githubbot@vadim.me>
2024-07-06 23:44:14 +0200

    Solution 70 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/8dw5h2zl Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/096be730-d97e-4d47-954c-3bc5fc998 ad7.py", line 14, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/096be730-d97e-4d47-954c-3bc5fc998 ad7.py", line 10, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,11 +1,5 @@
-def compress_string(s):
-    compressed = ""
-    i = 0
-    while i < len(s):
-        count = 1
-        while i + 1 < len(s) and s[i] == s[i+1]:
-            i += 1
-            count += 1
-        compressed += str(count) + s[i] if count > 1 else s[i]
-        i += 1
-    return compressed
+def fix_spaces(text):
+    text = text.strip().replace(" ", "_")
+    while "__" in text:
+        text = text.replace("__", "_")
+    return text
