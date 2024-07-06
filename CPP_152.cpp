41915d2ec379ee0b9669042857faac75604b919c
beep boop <githubbot@vadim.me>
2024-07-06 10:39:57 +0200

    Solution 10 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pzni0fwe Prompt:       |           ^~~~~~ In file included from f00ad2fc-a553-4e88-b39c-bea80d1fb60b.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ f00ad2fc-a553-4e88-b39c-bea80d1fb60b.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f00ad2fc-a553-4e88-b39c-bea80d1fb60b.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f00ad2fc-a553-4e88-b39c-bea80d1fb60b.cpp:15: f00ad2fc-a553-4e88-b39c-bea80d1fb60b.cpp: In function ‘int main()’: f00ad2fc-a553-4e88-b39c-bea80d1fb60b.cpp:25:20: error: ‘compare’ was not declare d in this scope    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ f00ad2fc-a553-4e88-b39c-bea80d1fb60b.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,35 +1,14 @@
-```
-#include <iostream>
 #include <vector>
-using namespace std;
+#include <cassert>
 
-bool issame(int a, int b) {
-    if (a == b)
-        return true;
-    else
-        return false;
-}
-
-std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
-    std::vector<int> result;
-    for (int i = 0; i < game.size(); i++) {
-        if (game[i] == guess[i]) {
-            result.push_back(0);
-        } else {
-            result.push_back(abs(game[i] - guess[i]));
-        }
+bool issame(vector<int> a, vector<int> b) {
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
-    return result;
+    return true;
 }
 
 int main() {
-    std::vector<int> g1 = {2, 7, 11};
-    std::vector<int> g2 = {3, 8, 12};
-
-    std::vector<int> res = compare(g1, g2);
-
-    for (auto i : res) {
-        cout << i << " ";
-    }
+    assert(compare({1,2,3,5},{-1,2,3,4}) == ({2,0,0,1})); 
     return 0;
 }
