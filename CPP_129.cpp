8b266f1eb5621bb7d9961df576969e8ee66cbcae
beep boop <githubbot@vadim.me>
2024-07-06 18:04:09 +0200

    Solution 16 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1kalf9to Prompt:    32 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b4ebeee3-4afa-40c1-ab0f-882ed7ffb56f.cpp:32:34: error: expected primary-expressi on before ‘int’    32 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b4ebeee3-4afa-40c1-ab0f-882ed7ffb56f.cpp:32:39: error: expression list treated a s compound expression in initializer [-fpermissive]    32 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b4ebeee3-4afa-40c1-ab0f-882ed7ffb56f.cpp:31: b4ebeee3-4afa-40c1-ab0f-882ed7ffb56f.cpp: In function ‘int main()’: b4ebeee3-4afa-40c1-ab0f-882ed7ffb56f.cpp:41:20: error: ‘minPath’ was not declare d in this scope    41 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ b4ebeee3-4afa-40c1-ab0f-882ed7ffb56f.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,18 +1,25 @@
-vector<int> minPath(vector<vector<int>> grid, int k) {
+```cpp
+#include <vector>
+#include <algorithm>
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+std::vector<int> minPath(std::vector<std::vector<int>> grid, int k) {
     int n = grid.size();
-    vector<vector<bool>> visited(n, vector<bool>(n));
-    vector<int> res;
+    std::vector<std::vector<bool>> visited(n, std::vector<bool>(n));
+    std::vector<int> res;
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
             if (grid[i][j] == 1) {
                 dfs(grid, visited, i, j, k, &res);
-                break;
+                return res;
             }
     return res;
-
 }
 
-void dfs(vector<vector<int>>& grid, vector<vector<bool>>& visited, int x, int y, int k, vector<int>* res) {
+void dfs(std::vector<std::vector<int>>& grid, std::vector<std::vector<bool>>& visited, int x, int y, int k, std::vector<int>* res) {
     if (k == 0)
         return;
     visited[x][y] = true;
@@ -27,15 +34,9 @@ void dfs(vector<vector<int>>& grid, vector<vector<bool>>& visited, int x, int y,
             }
         }
     visited[x][y] = false;
-
-}
-
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-
 }
 
 int main() {
-    assert(issame(minPath({{1, 3}, {3, 2}}, 10), vector<int>(1,3,1,3,1,3,1,3,1,3)));
+    assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
     return 0;
 }
