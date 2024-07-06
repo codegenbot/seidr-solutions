30a7c195341de1630207b12c86e721ed17322308
beep boop <githubbot@vadim.me>
2024-07-06 05:21:15 +0200

    Solution 3 of CPP/98, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/j3ciqgiz Prompt: cbbf3810-aea7-4ce4-9e36-9b1196cdaad6.cpp:1:17: error: ‘string’ was not declared in this scope     1 | int count_upper(string s){       |                 ^~~~~~ In file included from cbbf3810-aea7-4ce4-9e36-9b1196cdaad6.cpp:11: cbbf3810-aea7-4ce4-9e36-9b1196cdaad6.cpp: In function ‘int main()’: cbbf3810-aea7-4ce4-9e36-9b1196cdaad6.cpp:13:24: error: ‘count_upper’ cannot be u sed as a function    13 |     assert (count_upper("EEEE") == 2);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_98.cpp
+++ CPP_98.cpp
@@ -6,10 +6,14 @@ using namespace std;
 
 int count_upper(string s){
     int count = 0;
-    for(int i=0; i<s.length(); i=i+1){
+    for(int i=1; i<s.length(); i=i+2){
         if(isupper(s[i])){
             count++;
         }
     }
     return count;
+}
+
+int main() {
+    assert (count_upper("EEEE") == 2);
 }
