f2d87bfe2b470f54447645b880d7159a4ea68fd0
beep boop <githubbot@vadim.me>
2024-07-06 14:40:17 +0200

    Solution 4 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/t2kt1dve Prompt:       |                              ^~~~~~ 5c59ba8a-835e-4442-946f-2f53e515f704.cpp:16:37: error: ‘string’ was not declared  in this scope    16 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5c59ba8a-835e-4442-946f-2f53e515f704.cpp:16:44: error: ‘b’ was not declared in t his scope    16 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5c59ba8a-835e-4442-946f-2f53e515f704.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 5c59ba8a-835e-4442-946f-2f53e515f704.cpp:15: 5c59ba8a-835e-4442-946f-2f53e515f704.cpp: In function ‘int main()’: 5c59ba8a-835e-4442-946f-2f53e515f704.cpp:25:20: error: ‘total_match’ was not dec lared in this scope    25 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 5c59ba8a-835e-4442-946f-2f53e515f704.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,19 +1,29 @@
+#include <vector>
+#include <string>
+
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
+
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     int sum1 = 0;
     for (const string& s : lst1) {
         sum1 += s.size();
     }
-    
     int sum2 = 0;
     for (const string& s : lst2) {
         sum2 += s.size();
     }
+    if (sum1 < sum2) return lst1;
+    else if (sum1 > sum2) return lst2;
+    else return lst1;
+}
 
-    if (sum1 < sum2) {
-        return lst1;
-    } else if (sum1 > sum2) {
-        return lst2;
-    } else {
-        return lst1;
-    }
+int main() {
+    assert(issame(total_match({"this"}, {}), {}));
+    return 0;
 }
