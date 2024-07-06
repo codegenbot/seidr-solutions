86ec53f325ebe5a8cdfdb73fa70f01572aa0ae55
beep boop <githubbot@vadim.me>
2024-07-06 17:52:46 +0200

    Solution 99 of CPP/95, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4nob0eea Prompt: Program failed

--- CPP_95.cpp
+++ CPP_95.cpp
@@ -1,6 +1,5 @@
-```cpp
-int test_main() {
+```int test_main() {
     assert(check_dict_case({}) == false);
     assert(check_dict_case({{"hello", "world"}, {"HELLO", "WORLD"}}) == true);
     return 0;
-}
+}```
