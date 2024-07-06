6a6b1a4770f1ba543718e60d6dfc0dddb1b8e76b
beep boop <githubbot@vadim.me>
2024-07-06 18:32:46 +0200

    Solution 34 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ccu534fs Prompt:    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8eb1dcf3-c7c2-44aa-a784-8df6221a02ec.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8eb1dcf3-c7c2-44aa-a784-8df6221a02ec.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8eb1dcf3-c7c2-44aa-a784-8df6221a02ec.cpp:17: 8eb1dcf3-c7c2-44aa-a784-8df6221a02ec.cpp: In function ‘int main()’: 8eb1dcf3-c7c2-44aa-a784-8df6221a02ec.cpp:27:20: error: ‘sorted_list_sum’ was not  declared in this scope    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 8eb1dcf3-c7c2-44aa-a784-8df6221a02ec.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,12 +1,12 @@
 vector<string> sorted_list_sum(vector<string> lst) {
     auto it = unique(lst.begin(), lst.end(),
-                     [](const string& a, const string& b) { return a.length() % 2 == 1; });
+                     [](const string& a, const string& b) { return (stoi(a.substr(4)) + stoi(b.substr(4))) % 2 == 1; });
     lst.erase(it, lst.end());
     
     sort(lst.begin(), lst.end(),
          [](const string& a, const string& b) {
-             if (a.length() != b.length()) 
-                 return a.length() < b.length();
+             if (stoi(a.substr(4)) != stoi(b.substr(4)))
+                 return stoi(a.substr(4)) < stoi(b.substr(4));
              else
                  return a < b;
          });
