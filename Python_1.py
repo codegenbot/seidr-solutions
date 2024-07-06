124076aaa0e7e086560935fc4187104bcc61c0db
beep boop <githubbot@vadim.me>
2024-07-06 15:14:58 +0200

    Solution 49 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/48kvi39g Prompt: Program did not receive expected input

--- Python_1.py
+++ Python_1.py
@@ -4,12 +4,16 @@ stack = []
 group = ""
 result = []
 for char in paren_string:
-    if char == "(":
-        stack.append(char)
+    if char == "(" or char == ")":
+        if char == "(":
+            stack.append(char)
+            group += char
+        elif char == ")":
+            stack.pop()
+            group += char
+            if not stack:
+                result.append(group)
+                group = ""
+    else:
         group += char
-    elif char == ")":
-        stack.pop()
-        group += char
-        if not stack:
-            result.append(group)
-            group = ""
+print(result)
