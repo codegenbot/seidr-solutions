bd88c838055dc21d26d1969cb1d94a6895cb6ad8
beep boop <githubbot@vadim.me>
2024-07-06 09:41:15 +0200

    Solution 69 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/qkdu2tv2 Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -1,6 +1,3 @@
 def find_zero(xs: list):
     # Your code here
-    if len(xs) == 0:
-        return None
-    else:
-        return math.pow(xs[0], 1 / len(xs))
+    return math.pow(xs[0], 1 / len(xs)) if xs else None
