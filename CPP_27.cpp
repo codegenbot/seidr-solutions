1a2380a663bc3e377727fa44b18389b0f44e8d9b
beep boop <githubbot@vadim.me>
2024-07-06 11:21:39 +0200

    Solution 9 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ik1353gi Prompt: In file included from d5062f5d-13ae-4fc1-b1f8-6e6914fa2108.cpp:19: d5062f5d-13ae-4fc1-b1f8-6e6914fa2108.cpp: In function ‘int main()’: d5062f5d-13ae-4fc1-b1f8-6e6914fa2108.cpp:21:13: error: ‘filp_case’ was not decla red in this scope; did you mean ‘flip_case’?    21 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,19 +1,2 @@
-#include <string>
-
-using namespace std;
-
-string flip_case(string str) {
-    string result = "";
-    for (char c : str) {
-        if (isupper(c)) {
-            result += tolower(c);
-        } else if (islower(c)) {
-            result += toupper(c);
-        } else {
-            result += c;
-        }
-    }
-    return result;
-}
-
-assert(flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
+```c++
+assert (flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
