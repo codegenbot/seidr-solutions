c431266211d4ad97009a58e6f83fb5638cb2d048
beep boop <githubbot@vadim.me>
2024-07-06 16:12:54 +0200

    Solution 17 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/18agfa5l Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6260b13d-5229-454a-afff-2a0ba5ba7 770.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -1,5 +1,2 @@
-```
-import re
-strings = input("Please enter a list of strings (separated by any character): ", encoding='utf-8', errors='replace')
-strings = [i for i in re.split("([^\w\s]+)", strings) if i]
-print("".join(strings))
+strings = input("Please enter a list of strings (separated by any character): ")
+print(''.join(strings.split()))
