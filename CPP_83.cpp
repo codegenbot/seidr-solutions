70fd8decd7a40f8a078b4ea5972222c1d9e59658
beep boop <githubbot@vadim.me>
2024-07-06 05:05:56 +0200

    Solution 44 of CPP/83, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7jp0kji8 Prompt: __future_base::_State_baseV2>; _Args = {}; __gnu_cxx::_Lock_policy _Lp = __gnu_c xx::_S_atomic]’ /usr/include/c++/11/bits/shared_ptr_base.h:1342:14:   required from ‘std::__shar ed_ptr<_Tp, _Lp>::__shared_ptr(std::_Sp_alloc_shared_tag<_Tp>, _Args&& ...) [wit h _Alloc = std::allocator<std::__future_base::_State_baseV2>; _Args = {}; _Tp = std::__future_base::_State_baseV2; __gnu_cxx::_Lock_policy _Lp = __gnu_cxx::_S_a tomic]’ /usr/include/c++/11/bits/shared_ptr.h:409:59:   required from ‘std::shared_ptr<_ Tp>::shared_ptr(std::_Sp_alloc_shared_tag<_Tp>, _Args&& ...) [with _Alloc = std: :allocator<std::__future_base::_State_baseV2>; _Args = {}; _Tp = std::__future_b ase::_State_baseV2]’ /usr/include/c++/11/bits/shared_ptr.h:862:14:   required from ‘std::shared_ptr<_ Tp> std::allocate_shared(const _Alloc&, _Args&& ...) [with _Tp = std::__future_b ase::_State_baseV2; _Alloc = std::allocator<std::__future_base::_State_baseV2>; _Args = {}]’ /usr/include/c++/11/bits/shared_ptr.h:878:39:   required from ‘std::shared_ptr<_ Tp> std::make_shared(_Args&& ...) [with _Tp = std::__future_base::_State_baseV2;  _Args = {}]’ /usr/include/c++/11/future:1296:43:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::_Sp_counted_ptr_inplace<std::__future_base::_State_baseV2, std::allo cator<std::__future_base::_State_baseV2>, __gnu_cxx::_S_atomic> >’ has no member  named ‘_M_max_size’

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -1,7 +1,5 @@
 ```cpp
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
 bool starts_one_ends(int n) {
@@ -10,15 +8,14 @@ bool starts_one_ends(int n) {
         if (i == 1 || i % 10 == 1) {
             count++;
         }
-        for (int j = 1; j < pow(10, n); j++) {
-            long long num = i * pow(10, n - 1) + j;
-            if ((num / pow(10, n - 1)) % 10 == 1 || num % 10 == 1) {
+        for (long long j = 1; j < pow(10LL, n); j++) {
+            long long num = i * pow(10LL, n - 1) + j;
+            if ((num / pow(10LL, n - 1)) % 10 == 1 || num % 10 == 1) {
                 count++;
             }
         }
     }
     return count;
-
 }
 
 int main() {
