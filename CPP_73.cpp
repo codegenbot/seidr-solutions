3eb56ed27913683b1212e95027055b20d79d947c
beep boop <githubbot@vadim.me>
2024-07-06 15:42:18 +0200

    Solution 0 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/i0tpovve Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_73.cpp
@@ -0,0 +1,14 @@
+int smallest_change(vector<int> arr) {
+    int left = 0, right = arr.size() - 1;
+    int changes = 0;
+
+    while (left < right) {
+        if (arr[left] != arr[right]) {
+            changes++;
+        }
+        left++;
+        right--;
+    }
+
+    return changes;
+}
