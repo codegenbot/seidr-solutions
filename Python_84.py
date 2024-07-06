649f18bfcd7a9c949af9ec163f481d09ec71b882
beep boop <githubbot@vadim.me>
2024-07-06 19:22:39 +0200

    Solution 37 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qeuhcwp8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/08e669ef-5a83-4560-8ad3-38a87beae fe7.py", line 19, in <module>     check(solve)           ^^^^^ NameError: name 'solve' is not defined

--- Python_84.py
+++ Python_84.py
@@ -1,9 +1,7 @@
 ```
 def solve():
     choice = int(
-        input(
-            "Do you want to (1) Convert decimal to binary or (2) Convert binary to decimal? "
-        )
+        input("Do you want to (1) Convert decimal to binary or (2) Convert binary to decimal? ")
     )
     if choice == 1:
         N = int(input("Enter a number: "))
