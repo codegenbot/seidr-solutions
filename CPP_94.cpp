ac7f0f5fe5ae7f7b86a8629b6fcb20a443971313
beep boop <githubbot@vadim.me>
2024-07-06 17:50:34 +0200

    Solution 75 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ngcx7x2q Prompt: 97b8f9f2-140d-466a-a9bc-165ca5e97625.cpp:1:15: error: ‘vector’ was not declared in this scope     1 | int skjkasdkd(vector<int> lst){       |               ^~~~~~ 97b8f9f2-140d-466a-a9bc-165ca5e97625.cpp:1:22: error: expected primary-expressio n before ‘int’     1 | int skjkasdkd(vector<int> lst){       |                      ^~~ In file included from 97b8f9f2-140d-466a-a9bc-165ca5e97625.cpp:17: 97b8f9f2-140d-466a-a9bc-165ca5e97625.cpp: In function ‘int main()’: 97b8f9f2-140d-466a-a9bc-165ca5e97625.cpp:19:22: error: ‘skjkasdkd’ cannot be use d as a function    19 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ~~~~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,16 +1,2 @@
 #include <vector>
 int skjkasdkd(std::vector<int> lst) {
-    int sum = 0;
-    std::vector<int>::iterator it;
-    for(it=lst.begin();it!=lst.end();++it)
-    {
-        if(!isPrime(*it)) continue; 
-        int n = *it;
-        while(n > 0) 
-        {
-            sum += n % 10;
-            n /= 10;
-        }
-    }
-    return sum;
-}
