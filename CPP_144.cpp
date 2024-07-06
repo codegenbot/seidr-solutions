1662870a901ec6b7cd9ed70c46feec1a5e6bf457
beep boop <githubbot@vadim.me>
2024-07-06 18:03:33 +0200

    Solution 1 of CPP/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l7yc6ryu Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,31 +1,32 @@
 #include <string>
-#include <vector>
-
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = 0, b = 0, c = 0, d = 0;
-    for (int i = 0; i < x.length(); i++) {
-        if (x[i] == '/') break;
-        if (isdigit(x[i])) a = a * 10 + (x[i] - '0');
-    }
-    for (int i = x.find('/') + 1; i < x.length(); i++) {
-        if (isdigit(x[i])) b = b * 10 + (x[i] - '0');
+    int a = 1, b = 1, c = 1, d = 1;
+    int numeratorX = 0, denominatorX = 0;
+    int numeratorN = 0, denominatorN = 0;
+
+    // convert fractions to numerators and denominators
+    int i = x.find('/');
+    a = stoi(x.substr(0, i));
+    c = stoi(x.substr(i + 1));
+    i = n.find('/');
+    b = stoi(n.substr(0, i));
+    d = stoi(n.substr(i + 1));
+
+    // calculate the greatest common divisor of a and b
+    numeratorX = a * b;
+    denominatorX = c * d;
+
+    while (denominatorX % c != 0) {
+        int temp = c;
+        c = denominatorX % c;
+        denominatorX = temp;
     }
 
-    for (int i = 0; i < n.length(); i++) {
-        if (n[i] == '/') break;
-        if (isdigit(n[i])) c = c * 10 + (n[i] - '0');
+    if ((numeratorX / c) == (b / d)) {
+        return true;
+    } else {
+        return false;
     }
-    for (int i = n.find('/') + 1; i < n.length(); i++) {
-        if (isdigit(n[i])) d = d * 10 + (n[i] - '0');
-    }
-
-    int commonDivisor = gcd(abs(a), abs(c));
-    return (a / commonDivisor) * b == c / commonDivisor;
-}
-
-int gcd(int a, int b) {
-    if (b == 0) return a;
-    return gcd(b, a % b);
 }
