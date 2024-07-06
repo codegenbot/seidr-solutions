33a27fd0e6e31da3f72b11419760432273e6005b
beep boop <githubbot@vadim.me>
2024-07-06 02:00:27 +0200

    Solution 2 of Python/74, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q5hxpw3t Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/95797da4-3431-43c2-8141-522fd7221 565.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_74.py
+++ Python_74.py
@@ -1,4 +1,4 @@
-```
+# Your task is to compare the total number of characters in two lists of strings. If one list has fewer characters than the other, return that list. If they have the same number of characters, return the first list.
 def total_match(lst1, lst2):
     total_chars1 = sum(len(s) for s in lst1)
     total_chars2 = sum(len(s) for s in lst2)
