ed8914648c2d0bdc52dfb241d0a5eba736093373
beep boop <githubbot@vadim.me>
2024-07-06 08:09:54 +0200

    Solution 0 of Python/78, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1ppu66t9 Prompt: Complete the following code given the task description and function signature.

---
+++ Python_78.py
@@ -0,0 +1,8 @@
+```
+def hex_key(num):
+    primes = {'2', '3', '5', '7', 'B', 'D'}
+    count = 0
+    for char in num:
+        if char.upper() in primes:
+            count += 1
+    return count
