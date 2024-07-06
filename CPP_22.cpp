b1a27a76c4891febe0ebe9b33e102e7c885c71bb
beep boop <githubbot@vadim.me>
2024-07-06 18:47:37 +0200

    Solution 6 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6qspcpr6 Prompt: Complete the following code given the task description and function signature.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,15 +1,7 @@
-vector<int> filter_integers(list_any values){
+vector<int> filter_integers(list_any values) {
     vector<int> result;
-    for (const auto& value : values) {
-        boost::any_cast<int>(value);
-        bool is_integer = false;
-        try {
-            int i = boost::any_cast<int>(value);
-            is_integer = true;
-        } catch (...) {
-        }
-        if (is_integer)
-            result.push_back(boost::any_cast<int>(value));
+    for (auto& value : values) {
+        boost::any_cast<int>(&value) ? result.push_back(boost::any_cast<int>(value)) : 0;
     }
     return result;
 }
