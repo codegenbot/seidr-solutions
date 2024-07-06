a09161fd56735de02bcc73e03d61b2e4ea558e77
beep boop <githubbot@vadim.me>
2024-07-06 14:55:10 +0200

    Solution 11 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ny2a3etd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/52290737-f3e3-464b-a9ff-d8f02990b a39.py", line 13, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/52290737-f3e3-464b-a9ff-d8f02990b a39.py", line 10, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,9 +1,9 @@
-```
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
         return string
     for i in range(len(string)):
         middle = len(string) // 2
-        if i < middle and string[i:] == string[i:][::-1]:
-            return string[:i] + string[i:i+1] + string[i:][::-1]
-    return string + string[-1] + string[::-1].lstrip()
+        for j in reversed(range(i, len(string))):
+            new_string = string[:i] + string[i:j][::-1] + string[j:]
+            if new_string == new_string[::-1]:
+                return new_string
