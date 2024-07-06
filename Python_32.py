86102a39b1359b3743588d10d94672dfa589a409
beep boop <githubbot@vadim.me>
2024-07-06 17:27:42 +0200

    Solution 98 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6qafnz8m Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f8401acf-3634-4220-ab7c-7aaeaee22 b76.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,6 +1,6 @@
 while True:
     try:
-        a = float(input())
+        a = float(input("Enter 'a' value: "))
         if a == 0:
             print("Please enter non-zero value for 'a'.")
         else:
@@ -17,7 +17,7 @@ else:
     r1 = (-b + (b**2 - 4 * a * c) ** 0.5) / (2 * a)
     r2 = (-b - (b**2 - 4 * a * c) ** 0.5) / (2 * a)
 
-    if complex(0).imag != 0 or complex(0).imag != 0:
+    if r1.imag != 0 or r2.imag != 0:
         print(f"The equation has complex roots {r1} and {r2}.")
     else:
         print(f"Zeroes are approximately {r1:.2f} and {r2:.2f}")
