6a709220e48073334e7befd3f38a87d8d44cc7ca
beep boop <githubbot@vadim.me>
2024-07-06 12:29:15 +0200

    Solution 11 of CPP/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xrs0ecjn Prompt: n before ‘float’     1 | float mean_absolute_deviation(vector<float> numbers) {       |                                      ^~~~~ 0ce0d455-0715-4e7e-ac85-3c1a771360b3.cpp:9:12: error: ‘vector’ was not declared in this scope     9 | float mean(vector<float> numbers) {       |            ^~~~~~ 0ce0d455-0715-4e7e-ac85-3c1a771360b3.cpp:9:19: error: expected primary-expressio n before ‘float’     9 | float mean(vector<float> numbers) {       |                   ^~~~~ In file included from 0ce0d455-0715-4e7e-ac85-3c1a771360b3.cpp:13: 0ce0d455-0715-4e7e-ac85-3c1a771360b3.cpp: In function ‘int main()’: 0ce0d455-0715-4e7e-ac85-3c1a771360b3.cpp:15:40: error: ‘mean_absolute_deviation’  cannot be used as a function    15 |     assert (abs(mean_absolute_deviation({1.0, 2.0, 3.0, 4.0, 5.0}) - 6.0 /5.0) < 1e-4);       |                 ~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~ 0ce0d455-0715-4e7e-ac85-3c1a771360b3.cpp:15:13: error: ‘abs’ was not declared in  this scope    15 |     assert (abs(mean_absolute_deviation({1.0, 2.0, 3.0, 4.0, 5.0}) - 6.0 /5.0) < 1e-4);       |             ^~~

--- CPP_4.cpp
+++ CPP_4.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 #include <numeric>
 
+float mean(vector<float> numbers) {
+    return static_cast<float>(std::accumulate(numbers.begin(), numbers.end(), 0.0)) / numbers.size();
+}
+
 float mean_absolute_deviation(vector<float> numbers) {
     float sum = 0.0;
     for (float num : numbers) {
         sum += abs(num - mean(numbers));
     }
     return sum / numbers.size();
-}
-
-float mean(vector<float> numbers) {
-    return static_cast<float>(std::accumulate(numbers.begin(), numbers.end(), 0.0)) / numbers.size();
 }
