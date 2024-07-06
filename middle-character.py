10120a8bf9ac577b911680085a1697f3dfd63423
beep boop <githubbot@vadim.me>
2024-07-06 12:23:14 +0200

    Solution 51 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/t5qd6gs6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/042fcb2a-b2b9-4fcf-8afd-77f9cf2cb 971.py", line 2     if len(s) % 2 == 0:  # if input is even length     ^ IndentationError: expected an indented block after function definition on line 1

--- middle-character.py
+++ middle-character.py
@@ -1,5 +1,5 @@
-def get_middle(s):
-    if len(s) % 2 == 0:  # if input is even length
-        return s[len(s) // 2 - 1 : len(s) // 2 + 1]
-    else:  # if input is odd length
-        return s[len(s) // 2]
+    def get_middle(s):
+        if len(s) % 2 == 0: # if input is even length
+            return s[len(s) // 2 - 1 : len(s) // 2 + 1]
+        else: # if input is odd length
+            return s[len(s) // 2]
