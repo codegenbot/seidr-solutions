ce742b081e9d818172da62de0e9bd158e99f42bf
beep boop <githubbot@vadim.me>
2024-07-06 17:12:10 +0200

    Solution 10 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lbvzeknb Prompt: fb502cb7-463b-47fc-9685-e5f78af4a121.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string exchange(vector<int> lst1, vector<int> lst2) {       | ^~~~~~ In file included from fb502cb7-463b-47fc-9685-e5f78af4a121.cpp:18: fb502cb7-463b-47fc-9685-e5f78af4a121.cpp: In function ‘int main()’: fb502cb7-463b-47fc-9685-e5f78af4a121.cpp:20:13: error: ‘exchange’ was not declar ed in this scope    20 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
 
 std::string exchange(std::vector<int> lst1, std::vector<int> lst2) {
     int oddCount = 0;
@@ -13,29 +12,34 @@ std::string exchange(std::vector<int> lst1, std::vector<int> lst2) {
     for (int i = 0; i < lst2.size(); i++) {
         if (lst2[i] % 2 != 0 && oddCount > 0) {
             lst1.push_back(lst2[i]);
-            lst2.erase(std::remove(lst2.begin(), lst2.end(), lst2[i]), lst2.end());
+            lst2.erase(lst2.begin() + i);
             i--;
             oddCount--;
         }
     }
-    return oddCount == 0 ? "YES" : "NO";
+    return (oddCount == 0) ? "YES" : "NO";
 }
 
 int main() {
     std::vector<int> lst1, lst2;
     int n1, n2;
-    std::cout << "Enter the number of elements in list 1: ";
+
+    // Get input
+    std::cout << "Enter the size of lst1: ";
     std::cin >> n1;
-    for (int i = 0; i < n1; i++) {
-        std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> lst1.push_back(i);
+    for (int i = 0; i < n1; ++i) {
+        std::cout << "Enter element " << i + 1 << " of lst1: ";
+        std::cin >> (lst1).push_back(i);
     }
-    std::cout << "Enter the number of elements in list 2: ";
+    std::cout << "Enter the size of lst2: ";
     std::cin >> n2;
-    for (int i = 0; i < n2; i++) {
-        std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> lst2.push_back(i);
+    for (int i = 0; i < n2; ++i) {
+        std::cout << "Enter element " << i + 1 << " of lst2: ";
+        std::cin >> (lst2).push_back(i);
     }
-    std::cout << exchange(lst1, lst2) << std::endl;
+
+    // Call the function
+    std::cout << exchange(lst1, lst2);
+
     return 0;
 }
