ac9e4dd40f8e56f0822cd34c70631e96252690fc
beep boop <githubbot@vadim.me>
2024-07-06 02:50:32 +0200

    Solution 44 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/a1rky2qb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9fecce48-eaff-46dd-9734-40f15f5ce 57c.py", line 16, in <module>     check(parse_nested_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9fecce48-eaff-46dd-9734-40f15f5ce 57c.py", line 13, in check     assert parse_nested_parens("(()(())((())))") == [4]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_6.py
+++ Python_6.py
@@ -1,5 +1,5 @@
-def parse_nested_parens(paren_string: str) -> int:
-    max_level = 0
+def parse_nested_parens(paren_string: str) -> list[int]:
+    result = []
     for group in paren_string.split():
         level = 0
         for char in group:
@@ -7,5 +7,5 @@ def parse_nested_parens(paren_string: str) -> int:
                 level += 1
             elif char == ")":
                 level -= 1
-        max_level = max(max_level, level)
-    return max_level
+        result.append(level)
+    return result
