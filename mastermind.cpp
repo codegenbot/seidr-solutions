0dc4f21d666dee4b9a1cd385ff979dd3b4b3cdd6
beep boop <githubbot@vadim.me>
2024-07-06 21:55:26 +0200

    Solution 20 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/uzdkq0v8 Prompt: b8d66bbc-73b2-4d18-9a0d-f631745bf78e.cpp: In function ‘int main()’: b8d66bbc-73b2-4d18-9a0d-f631745bf78e.cpp:15:73: error: invalid types ‘int[__gnu_ cxx::__alloc_traits<std::allocator<char>, char>::value_type {aka char}]’ for arr ay subscript    15 | } else if (code.find(guess[i]) != string::npos && !whitePegs[guess[i]]) { // added check to make sure the guessed character is not already used as a whi te peg       |                                                             ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main() {
     string code, guess;
     int whitePegs = 0, blackPegs = 0;
+
     while (getline(cin, code)) {
         getline(cin, guess);
+
         for (int i = 0; i < code.length(); i++) {
             if (code[i] == guess[i]) {
                 blackPegs++;
-            } else if (code.find(guess[i]) != string::npos && !whitePegs) {
-                whitePegs++;
+            } else if (code.find(guess[i]) != string::npos && !whitePegs[guess[i] - '0']) { // added check to make sure the guessed character is not already used as a white peg
+                whitePegs[guess[i] - '0']++; // increment the corresponding element in the whitePegs array
             }
         }
+
         cout << whitePegs << " " << blackPegs << endl;
     }
+
     return 0;
 }
