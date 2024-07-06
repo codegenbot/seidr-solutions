c9797a4fea23a41a21ad8285474e1610afaae400
beep boop <githubbot@vadim.me>
2024-07-06 02:24:30 +0200

    Solution 43 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/al4be52p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d42592c1-8e24-4e60-8cdd-19fb2632a ae4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_26.py
+++ Python_26.py
@@ -1,7 +1,9 @@
-``` 
+```
+"""
 def remove_duplicates(input_str=None):
     if input_str is None:
         numbers = input("Enter comma-separated numbers: ")
     else:
         numbers = ",".join(map(str, input_str))
     return list(dict.fromkeys(map(int, numbers.split(","))))
+"""
