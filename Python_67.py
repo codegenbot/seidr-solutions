a15197fd992a718234b386c2bdc2097bff6daff1
beep boop <githubbot@vadim.me>
2024-07-06 05:37:19 +0200

    Solution 79 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/kh7c2g1m Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/069b6886-9376-45be-9d3a-0f11c464c d85.py", line 11, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/069b6886-9376-45be-9d3a-0f11c464c d85.py", line 8, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/069b6886-9376-45be-9d3a-0f11c464c d85.py", line 2, in fruit_distribution     apples = int(re.search(r"\d+", s).group())                  ^^ NameError: name 're' is not defined

--- Python_67.py
+++ Python_67.py
@@ -1,9 +1,6 @@
 
-# Write a function that returns the total number of fruits based on the given input string
 def fruit_distribution(s):
-    # Use regular expressions to extract the numbers from the input string
     apples = int(re.search(r"\d+", s).group())
     oranges = int(re.search(r"\d+", s).group())
-    
-    # Return the sum of the two numbers
     return apples + oranges
+import re
