fab23874cae9d44ef758698f9a8ca3ff36de1410
beep boop <githubbot@vadim.me>
2024-07-06 06:30:33 +0200

    Solution 12 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/42l0jg6c Prompt: Complete the following code given the task description and function signature.

--- Python_38.py
+++ Python_38.py
@@ -1,3 +1,9 @@
-def decode_cyclic(s: str):
-    groups = [(group[2:] + group[:2]) for group in s.split()]
+def decode_cyclic(s):
+    groups = []
+    for i in range((len(s) + 2) // 3):
+        group = s[(3 * i) : min((3 * i + 3), len(s))]
+        if len(group) == 3:
+            groups.append(group[1:] + group[0])
+        else:
+            groups.append(group)
     return "".join(groups)
