ba3a6806f2b44cc80d8c82a5fdc5acc81fa799ab
beep boop <githubbot@vadim.me>
2024-07-06 13:31:14 +0200

    Solution 1 of CPP/55, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/li7jy0n4 Prompt: Complete the following code given the task description and function signature.

--- CPP_55.cpp
+++ CPP_55.cpp
@@ -1,9 +1,11 @@
+#include <stdio.h>
+using namespace std;
+
 int fib(int n) {
-    if (n <= 1) {
+    if (n <= 1)
         return n;
-    }
     int a = 0, b = 1, result = 1;
-    for (int i = 2; i <= n; ++i) {
+    for (int i = 2; i <= n; i++) {
         result = a + b;
         a = b;
         b = result;
