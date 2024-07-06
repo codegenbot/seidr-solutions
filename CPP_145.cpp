a40edc52ca96015685c53db744800af750b3768b
beep boop <githubbot@vadim.me>
2024-07-06 18:19:14 +0200

    Solution 22 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r0qsr2nm Prompt:    24 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 23b8f31f-9990-4d24-b028-e9a2f6e74ffb.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 23b8f31f-9990-4d24-b028-e9a2f6e74ffb.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 23b8f31f-9990-4d24-b028-e9a2f6e74ffb.cpp:23: 23b8f31f-9990-4d24-b028-e9a2f6e74ffb.cpp: In function ‘int main()’: 23b8f31f-9990-4d24-b028-e9a2f6e74ffb.cpp:33:20: error: ‘order_by_points’ was not  declared in this scope    33 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 23b8f31f-9990-4d24-b028-e9a2f6e74ffb.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,7 +1,4 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
+```c++
 bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); i++) {
