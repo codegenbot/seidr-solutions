a59a02c5b2dae1d35a4ec73dae3a6102a2a18442
beep boop <githubbot@vadim.me>
2024-07-06 09:06:22 +0200

    Solution 8 of Python/141, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ihqubshq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a21f3602-cc8a-49d2-84b6-b8bd0749e 475.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_141.py
+++ Python_141.py
@@ -1,15 +1,12 @@
-```
 def file_name_check(file_name):
     if len([c for c in file_name if c.isdigit()]) > 3:
         return 'No'
-    elif '.' not in file_name or file_name.count('.') > 1:
+    if '.' not in file_name or file_name.count('.') > 1:
         return 'No'
-    else:
-        before_dot = file_name.split('.')[0]
-        after_dot = file_name.split('.')[1].lower()
-        if not before_dot or not before_dot[0].isalpha():
-            return 'No'
-        elif after_dot not in ['txt', 'exe', 'dll']:
-            return 'No'
-        else:
-            return 'Yes'
+    before_dot = file_name.split('.')[0]
+    after_dot = file_name.split('.')[1].lower()
+    if not before_dot or not before_dot[0].isalpha():
+        return 'No'
+    if after_dot not in ['txt', 'exe', 'dll']:
+        return 'No'
+    return 'Yes'
