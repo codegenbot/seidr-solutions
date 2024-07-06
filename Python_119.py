bfebe50da47bbc90789013969f3956588bc30b34
beep boop <githubbot@vadim.me>
2024-07-06 10:05:13 +0200

    Solution 7 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/1inrvcki Prompt: Complete the following code given the task description and function signature.

--- Python_119.py
+++ Python_119.py
@@ -1,14 +1,14 @@
 def match_parens(lst):
-    stack = []
+    left = 0
+    right = 0
     for char in lst[0]:
         if char == "(":
-            stack.append(char)
-        elif char == ")":
-            if len(stack) == 0:
-                return "No"
-            else:
-                stack.pop()
-    if len(stack) != 0:
-        return "No"
-    else:
-        return "Yes"
+            left += 1
+        else:
+            right += 1
+    for char in lst[1]:
+        if char == "(":
+            left -= 1
+        else:
+            right -= 1
+    return "Yes" if left == 0 and right == 0 else "No"
