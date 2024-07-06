cd968061317d8c60152870b5c703328f55fa0357
beep boop <githubbot@vadim.me>
2024-07-06 04:54:36 +0200

    Solution 78 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/g5ej8jfl Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4235c027-dce5-401d-8620-6858821c0 555.py", line 24, in <module>     check(encrypt)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4235c027-dce5-401d-8620-6858821c0 555.py", line 20, in check     encrypt("a") == "e" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_89.py
+++ Python_89.py
@@ -5,10 +5,7 @@ def encrypt(s):
     for char in s:
         index = alphabet.index(char)
         if index < 2:
-            # If the character is one of the first two characters in the alphabet,
-            # return the encrypted string without rotating the character.
             return encrypted_string + char
         else:
-            # Otherwise, rotate the character by 2 positions and add it to the encrypted string.
             encrypted_string += rotated_alphabet[index - 2]
     return encrypted_string
