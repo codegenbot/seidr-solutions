e97d2a0deed2ea751465fd16ba3e1e93097982f7
beep boop <githubbot@vadim.me>
2024-07-06 17:17:25 +0200

    Solution 74 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/isn8czbm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c8998110-00f4-478c-a756-d71a1b3d1 5ee.py", line 20, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c8998110-00f4-478c-a756-d71a1b3d1 5ee.py", line 17, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c8998110-00f4-478c-a756-d71a1b3d1 5ee.py", line 6, in remove_duplicates     numbers = [int(num) for num in numbers.split()]                                    ^^^^^^^^^^^^^ AttributeError: 'list' object has no attribute 'split'

--- Python_26.py
+++ Python_26.py
@@ -1,5 +1,11 @@
-def remove_duplicates(numbers: list[int]) -> list[int]:
+def remove_duplicates(numbers: str) -> list[int]:
     if not numbers:
         return []
-    
-    return sorted(list(set(numbers)))
+    numbers = [int(num) for num in numbers.split()]
+    seen = set()
+    result = []
+    for num in numbers:
+        if num not in seen:
+            result.append(num)
+            seen.add(num)
+    return result
