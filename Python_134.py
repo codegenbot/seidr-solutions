8eeae3d5946b1a9d079553922210c8572ef9ced3
beep boop <githubbot@vadim.me>
2024-07-06 22:46:21 +0200

    Solution 1 of Python/134, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9oql5rue Prompt: Complete the following code given the task description and function signature.

--- Python_134.py
+++ Python_134.py
@@ -1,10 +1,5 @@
-```
 def check_if_last_char_is_a_letter(txt):
     if len(txt) == 0:
         return False
-    last_char = txt[-1].lower()
-    prev_chars = txt[:-1].split()
-    for char in prev_chars:
-        if last_char in char.lower():
-            return False
-    return last_char.isalpha()
+    last_char = txt[-1]
+    return last_char.isalpha() and not txt.rstrip().endswith(txt)
