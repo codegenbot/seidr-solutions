cf10450b1493a6c2ea915a8c5ebd13b458ba145b
beep boop <githubbot@vadim.me>
2024-07-06 10:41:28 +0200

    Solution 18 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/u89qmsrm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3168b8f9-9bd3-4ddf-bfbc-6f8328c87 f9e.py", line 21, in <module>     check(parse_nested_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3168b8f9-9bd3-4ddf-bfbc-6f8328c87 f9e.py", line 18, in check     assert parse_nested_parens("(()(())((())))") == [4]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_6.py
+++ Python_6.py
@@ -1,13 +1,12 @@
 def parse_nested_parens(paren_string):
-    stack = []
     result = []
-    for char in paren_string:
-        if char == "(":
-            stack.append(char)
-        elif char == ")":
-            level = 0
-            while len(stack) > 0 and stack[-1] != ")":
+    level = 0
+    for group in paren_string.split():
+        for char in group:
+            if char == "(":
                 level += 1
-                stack.pop()
-            result.append(level)
+            elif char == ")":
+                level -= 1
+                if level == 0:
+                    result.append(level)
     return result
