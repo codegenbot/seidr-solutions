0777ecea15a2a28193a401010213a18d5bf31bf7
beep boop <githubbot@vadim.me>
2024-07-06 04:28:20 +0200

    Solution 47 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/qwgfrn7h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/755444b8-9ab5-462c-bd4a-95b4c757c 948.py", line 22, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/755444b8-9ab5-462c-bd4a-95b4c757c 948.py", line 18, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,14 +1,11 @@
 def encode(message):
     encoded_message = ""
     for char in message:
-        # Check if the character is alphabetic
         if char.isalpha():
-            # Apply the encoding to alphabetic characters
             if char.islower():
                 encoded_char = chr(ord("A") + (ord(char) - ord("a")) % 26)
             elif char.isupper():
                 encoded_char = chr(ord("A") + (ord(char) - ord("A")) % 26)
-        # If the character is not alphabetic, use it as is
         else:
             encoded_char = char
         encoded_message += encoded_char
