abfa1954fb8432bf22681a66a14189d6ad2db902
beep boop <githubbot@vadim.me>
2024-07-06 02:04:21 +0200

    Solution 84 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/4ip2inle Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/08ac5fcc-60dc-45aa-b725-ac30c8aab e45.py", line 13, in <module>     check(encrypt)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/08ac5fcc-60dc-45aa-b725-ac30c8aab e45.py", line 9, in check     encrypt("a") == "e"     ^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/08ac5fcc-60dc-45aa-b725-ac30c8aab e45.py", line 3, in encrypt     table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxy za")             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^ ValueError: the first two maketrans arguments must have equal length

--- Python_89.py
+++ Python_89.py
@@ -1,8 +1,3 @@
-
 def encrypt(s):
-    table = {
-        "a": "e",
-        "b": "f",
-        ...
-    }
-    return "".join([table.get(c, c) for c in s])
+    table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab")
+    return s.translate(table)
