13f2c799cc3f22cbdb29e90cfaf92ccc077554d2
beep boop <githubbot@vadim.me>
2024-07-06 15:34:41 +0200

    Solution 36 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/t2kt1dve Prompt:       |           ^~~~~~ 12c9c818-079c-42a8-8ae0-8b8498c87d4c.cpp:32:44: error: ‘b’ was not declared in t his scope    32 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 12c9c818-079c-42a8-8ae0-8b8498c87d4c.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 12c9c818-079c-42a8-8ae0-8b8498c87d4c.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~ In file included from 12c9c818-079c-42a8-8ae0-8b8498c87d4c.cpp:31: 12c9c818-079c-42a8-8ae0-8b8498c87d4c.cpp: In function ‘int main()’: 12c9c818-079c-42a8-8ae0-8b8498c87d4c.cpp:41:20: error: ‘total_match’ was not dec lared in this scope    41 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 12c9c818-079c-42a8-8ae0-8b8498c87d4c.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,63 +1,29 @@
 #include <vector>
 #include <string>
 
-bool issame(const string& s1, const string& s2) {
-    if (s1.size() != s2.size()) return false;
-    for (int i = 0; i < s1.size(); ++i) {
-        if (tolower(s1[i]) != tolower(s2[i])) return false;
+bool issame(std::vector<std::string> a) {
+    if (a.size() == 0) return true;
+    for (int i = 1; i < a.size(); i++) {
+        if (a[i] != a[0]) return false;
     }
     return true;
 }
 
-vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<std::string> lst2) {
     int sum1 = 0;
     for (const string& s : lst1) {
         sum1 += s.size();
     }
-    
     int sum2 = 0;
     for (const string& s : lst2) {
         sum2 += s.size();
     }
+    if (sum1 < sum2) return lst1;
+    else if (sum1 > sum2) return lst2;
+    else return lst1;
+}
 
-    if (sum1 < sum2) {
-        vector<string> result;
-        for (const string& s : lst1) {
-            bool match = false;
-            for (const string& t : lst2) {
-                if (issame(s, t)) {
-                    match = true;
-                    break;
-                }
-            }
-            if (!match) return lst1;
-        }
-        return lst1;
-    } else if (sum1 > sum2) {
-        vector<string> result;
-        for (const string& s : lst2) {
-            bool match = false;
-            for (const string& t : lst1) {
-                if (issame(s, t)) {
-                    match = true;
-                    break;
-                }
-            }
-            if (!match) return lst2;
-        }
-        return lst2;
-    } else {
-        vector<string> result;
-        bool match = false;
-        for (const string& s : lst1) {
-            for (const string& t : lst2) {
-                if (issame(s, t)) {
-                    match = true;
-                    break;
-                }
-            }
-            if (!match) return lst1;
-        }
-        return lst1;
-    }
+int main() {
+    assert(issame(total_match({"this"}, {})));
+    return 0;
 }
