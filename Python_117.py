1bbb5af8384f500c1dd44f535dc4df8b87e23d79
beep boop <githubbot@vadim.me>
2024-07-06 04:44:50 +0200

    Solution 24 of Python/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/t5mp7x1v Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bbde03ff-6e05-4389-a411-81af89ceb 347.py", line 20, in <module>     check(select_words)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bbde03ff-6e05-4389-a411-81af89ceb 347.py", line 12, in check     assert select_words("a b c d e f", 1) == [            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: 2nd edge test error: ['a', 'b', 'c', 'd', 'e', 'f']

--- Python_117.py
+++ Python_117.py
@@ -2,6 +2,6 @@ def select_words(s, n):
     words = s.split()
     selected_words = []
     for word in words:
-        if len([c for c in word if c.isalpha() and not c.isdigit()]) == n:
+        if len(word) == n:
             selected_words.append(word)
     return selected_words
