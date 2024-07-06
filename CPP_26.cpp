968fc7ed4941bb7f7e7c8a21f1b53539290519bd
beep boop <githubbot@vadim.me>
2024-07-06 13:05:22 +0200

    Solution 88 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ocp7lybz Prompt:                  from 45aa1bee-d583-41ce-8bd3-027a1d8a21bd.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 45aa1bee-d583-41ce-8bd3-027a1d8a21bd.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 45aa1bee-d583-41ce-8bd3-027a1d8a21bd.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 45aa1bee-d583-41ce-8bd3-027a1d8a21bd.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 45aa1bee-d583-41ce-8bd3-027a1d8a21bd.cpp:25: 45aa1bee-d583-41ce-8bd3-027a1d8a21bd.cpp: In function ‘int main()’: 45aa1bee-d583-41ce-8bd3-027a1d8a21bd.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,32 +1,24 @@
 #include <vector>
 #include <algorithm>
 
-bool sameVectors(const std::vector<int>& a, const std::vector<int>& b) {
+bool compareVectors(const std::vector<int>& a, const std::vector<int>& b) {
     if(a.size() != b.size())
         return false;
     for(int i = 0; i < a.size(); ++i)
         if(a[i] != b[i])
             return false;
     return true;
+
 }
 
 std::vector<int> remove_duplicates(std::vector<int> numbers){
     std::vector<int> result;
-    
+
     for(int i : numbers) {
-        bool already_exists = false;
-        
-        for(int j : result) {
-            if(i == j) {
-                already_exists = true;
-                break;
-            }
-        }
-        
-        if(!already_exists) {
+        if(find(result.begin(), result.end(), i) == result.end()) {
             result.push_back(i);
         }
     }
-    
+
     return result;
 }
