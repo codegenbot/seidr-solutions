cada8ffe4968696bcc015a7a01c21dc497c2a9fd
beep boop <githubbot@vadim.me>
2024-07-06 15:07:48 +0200

    Solution 45 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/48kvi39g Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9b07d29d-7ca5-4e12-8634-761b6adf4 56c.py", line 1, in <module>     for char in paren_string:                 ^^^^^^^^^^^^ NameError: name 'paren_string' is not defined

--- Python_1.py
+++ Python_1.py
@@ -1,10 +1,15 @@
+```
+paren_string = input("Enter the string: ")
+stack = []
+group = ""
+result = []
 for char in paren_string:
     if char == "(":
         stack.append(char)
         group += char
-    elif     char == ")":
+    elif char == ")":
         stack.pop()
-        group += char  
-        if not stack:  
+        group += char
+        if not stack:
             result.append(group)
             group = ""
