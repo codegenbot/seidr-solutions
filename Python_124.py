f152073612cb66d40aada7f7d50f88291acb9740
beep boop <githubbot@vadim.me>
2024-07-06 04:43:15 +0200

    Solution 4 of Python/124, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/kbejf6ju Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d87a0029-dbad-46b5-8855-c1674495f eab.py", line 18, in <module>     check(valid_date)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d87a0029-dbad-46b5-8855-c1674495f eab.py", line 15, in check     assert valid_date("04-2003") == False            ^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d87a0029-dbad-46b5-8855-c1674495f eab.py", line 2, in valid_date     month, day, year = map(int, date.split("-"))     ^^^^^^^^^^^^^^^^ ValueError: not enough values to unpack (expected 3, got 2)

--- Python_124.py
+++ Python_124.py
@@ -1,11 +1,13 @@
 def valid_date(date):
-    month, day, year = date.split("-")
-    if len(month) != 2 or len(day) != 2 or len(year) != 4:
+    if len(date.split("-")) < 3:
         return False
-    if int(month) not in range(1, 13) or int(day) not in range(1, 32):
+    month, day, year = map(int, date.split("-"))
+    if month < 1 or month > 12:
         return False
-    if int(month) in [4, 6, 9, 11] and int(day) > 30:
+    if day < 1 or day > 31:
         return False
-    if int(month) == 2 and (int(year) % 4 == 0 and int(day) > 29 or int(day) > 28):
+    if month in [4, 6, 9, 11] and day == 31:
+        return False
+    if month == 2 and (day > 28 or (day == 29 and year % 4 != 0)):
         return False
     return True
