9c983852260a5b0a706c743d55e78bb86491f3c7
beep boop <githubbot@vadim.me>
2024-07-06 21:28:46 +0200

    Solution 84 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qeuhcwp8 Prompt: Program did not receive expected input

--- Python_84.py
+++ Python_84.py
@@ -18,18 +18,16 @@ def solve():
                     choice = input(
                         "Do you want to (1) Convert decimal to binary or (2) Convert binary to decimal? "
                     )
-                    break
                 except ValueError:
                     print("That's not a valid number! Try again.")
         elif choice == "2":
             while True:
                 try:
-                    N = int(input("Enter a binary number in base 10: "), 2)
-                    print(N)
+                    N = input("Enter a binary number: ")
+                    print(int(N, 2))
                     choice = input(
                         "Do you want to (1) Convert decimal to binary or (2) Convert binary to decimal? "
                     )
-                    break
                 except ValueError:
                     print("That's not a valid binary number! Try again.")
 
