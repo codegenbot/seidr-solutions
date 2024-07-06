eb54a2ac9eb5ad3dfde046800fede317554314c7
beep boop <githubbot@vadim.me>
2024-07-06 17:25:03 +0200

    Solution 63 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/18agfa5l Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/09aba287-70f6-41a9-80fa-642c00ad2 cff.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -1,7 +1,11 @@
-num_strings = int(input("Enter the number of strings: "))
-strings_list = []
+while True:
+    try:
+        num_strings = int(input("Enter the number of strings: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a positive integer.")
+
 for _ in range(num_strings):
     strings = input("Enter a string: ")
     reversed_string = strings[::-1]
-    strings_list.append(reversed_string)
-print(strings_list)
+    print(reversed_string)
