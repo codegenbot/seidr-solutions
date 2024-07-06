46a764c3c0ed7d01312a87ebb0c7b106018589f3
beep boop <githubbot@vadim.me>
2024-07-06 06:56:52 +0200

    Solution 7 of Python/75, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-codellama_34b-instruct-run1/runs/3jau7312 Prompt: Complete the following code given the task description and function signature.

--- Python_75.py
+++ Python_75.py
@@ -1,14 +1,6 @@
+
 def is_multiply_prime(a):
-    for i in range(1, a + 1):
+    for i in range(2, int(a**0.5) + 1):
         if a % i == 0 and is_prime(i) and is_prime(a // i):
             return True
     return False
-
-
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
