4c056cae2a5d72c702706d7e31a772c8f3dc5e2b
beep boop <githubbot@vadim.me>
2024-07-06 19:24:49 +0200

    Solution 25 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/b3zlor4d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ce8098bf-91ee-4e67-9708-a7c73a85d d59.py", line 13, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ce8098bf-91ee-4e67-9708-a7c73a85d d59.py", line 10, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ce8098bf-91ee-4e67-9708-a7c73a85d d59.py", line 6, in max_fill     return math.ceil(sum(row) / capacity for row in rows).count(capacity)            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: must be real number, not generator

--- Python_115.py
+++ Python_115.py
@@ -3,5 +3,4 @@ import math
 
 def max_fill(grid, capacity):
     rows = [min(c for c in row) for row in grid]
-    avg_rows = [(sum(row) / capacity) for row in rows]
-    return math.ceil(sum(r / capacity) for r in avg_rows)
+    return sum(1 for _ in (math.ceil(sum(row) / capacity) for row in rows)) // capacity
