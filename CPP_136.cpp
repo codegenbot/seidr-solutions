9f99a28dac2ea7e32b725c29f48b1ec59920ef14
beep boop <githubbot@vadim.me>
2024-07-06 18:13:51 +0200

    Solution 26 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/co02erto Prompt:    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9114804e-a1ea-40ee-b8f4-076b36ba5efe.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9114804e-a1ea-40ee-b8f4-076b36ba5efe.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9114804e-a1ea-40ee-b8f4-076b36ba5efe.cpp:15: 9114804e-a1ea-40ee-b8f4-076b36ba5efe.cpp: In function ‘int main()’: 9114804e-a1ea-40ee-b8f4-076b36ba5efe.cpp:25:20: error: ‘largest_smallest_integer s’ was not declared in this scope    25 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 9114804e-a1ea-40ee-b8f4-076b36ba5efe.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,11 +1,3 @@
-vector<int> largest_smallest_integers(vector<int> lst){
-    int max_negative = 0;
-    int min_positive = INT_MAX;
-
-    for(int i : lst){
-        if(i < 0 && i > max_negative) max_negative = i;
-        else if(i > 0 && i < min_positive) min_positive = i;
-    }
-
-    return {(max_negative >= 0) ? 0 : max_negative, (min_positive <= 0) ? 0 : min_positive};
+bool issame(vector<int> a, vector<int> b) {
+    return (a.size() == b.size()) && (equal(a.begin(), a.end(), b.begin()));
 }
