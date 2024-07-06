37c906ddd645f7420da993bac68d7b5049593548
beep boop <githubbot@vadim.me>
2024-07-06 14:57:20 +0200

    Solution 12 of CPP/79, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/snvr445e Prompt: 77477c8c-7bec-41f7-ba79-e8870c8fd1f4.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the solution:       | ^~~~ In file included from 77477c8c-7bec-41f7-ba79-e8870c8fd1f4.cpp:15: 77477c8c-7bec-41f7-ba79-e8870c8fd1f4.cpp: In function ‘int main()’: 77477c8c-7bec-41f7-ba79-e8870c8fd1f4.cpp:17:13: error: ‘decimal_to_binary’ was n ot declared in this scope    17 |     assert (decimal_to_binary(15) == "db1111db");       |             ^~~~~~~~~~~~~~~~~

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -1,20 +1,13 @@
-#include <iostream>
 #include <string>
 
 std::string decimal_to_binary(int decimal){
-    std::string binary = "";
-    while(decimal > 0){
-        if(decimal % 2 == 0)
-            binary.insert(0,"0");
+    string binary = "";
+    while(decimal > 0) {
+        if (decimal % 2 == 1)
+            binary = "1" + binary;
         else
-            binary.insert(0,"1");
+            binary = "0" + binary;
         decimal /= 2;
     }
     return "db" + binary + "db";
-}
-
-int main(){
-    std::cout << decimal_to_binary(15) << std::endl;
-    assert (decimal_to_binary(15) == "db1111db");
-    return 0;
 }
