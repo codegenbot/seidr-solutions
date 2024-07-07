c8fcafc2cf82b4a5cb6e3f4fa7b5790c8fa4e14d
beep boop <githubbot@vadim.me>
2024-07-07 05:59:05 +0200

    Solution 93 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3iwdkljd Prompt:       |           ^~~~ 9d97e9df-44ec-42c3-a9f0-87bb3334a0ca.cpp:45:21: error: request for member ‘size’  in ‘b’, which is of non-class type ‘int’    45 |     if (a.size()!=b.size()) return false;       |                     ^~~~ 9d97e9df-44ec-42c3-a9f0-87bb3334a0ca.cpp:46:22: error: request for member ‘size’  in ‘a’, which is of non-class type ‘int’    46 |     for (int i=0;i<a.size();i++)       |                      ^~~~ 9d97e9df-44ec-42c3-a9f0-87bb3334a0ca.cpp:48:10: error: invalid types ‘int[int]’ for array subscript    48 |     if (a[i]!=b[i]) return false;       |          ^ 9d97e9df-44ec-42c3-a9f0-87bb3334a0ca.cpp:48:16: error: invalid types ‘int[int]’ for array subscript    48 |     if (a[i]!=b[i]) return false;       |                ^ In file included from 9d97e9df-44ec-42c3-a9f0-87bb3334a0ca.cpp:43: 9d97e9df-44ec-42c3-a9f0-87bb3334a0ca.cpp: In function ‘int main()’: 9d97e9df-44ec-42c3-a9f0-87bb3334a0ca.cpp:53:20: error: ‘reverse_delete’ was not declared in this scope; did you mean ‘reverseDelete’?    53 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -6,7 +6,7 @@ struct vector<_Ty>::size_type { };
 
 using namespace std;
 
-vector<string> reverse_delete(vector<string>& v) {
+vector<string> reverseDelete(vector<string>& v) {
     for (int i = v.size() - 1; i >= 0; --i) {
         if (!v[i].empty()) {
             break;
@@ -30,17 +30,12 @@ bool issame(const vector<string>& b, const vector<string>& a) {
 
 void test() {
     vector<string> v1 = {"mamma"};
-    vector<string> result1 = reverse_delete(v1);
+    vector<string> result1 = reverseDelete(v1);
     
     assert(issame(vector<string>({"", "True"}), result1));
     
     vector<string> v2 = {"mia"};
-    vector<string> result2 = reverse_delete(v2);
+    vector<string> result2 = reverseDelete(v2);
     
     assert(issame(vector<string>({"True"}), result2));
-}
-
-int main() {
-    test();
-    return 0;
 }
