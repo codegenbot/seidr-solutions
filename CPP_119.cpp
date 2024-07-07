3b9a48af994282d2c4f0928490504128bc2ca629
beep boop <githubbot@vadim.me>
2024-07-07 10:52:41 +0200

    Solution 96 of CPP/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/arqn13qk Prompt: Complete the following code given the task description and function signature.

--- CPP_119.cpp
+++ CPP_119.cpp
@@ -1,12 +1,10 @@
-string match_parens(vector<string> lst){
+string match_parens(vector<string> lst) {
     int open = 0, close = 0;
-
-    for(string s : lst){
-        if(s[0] == '(') open++;
-        else if(s[0] == ')') close++;
-
-        if(close > open) return "No";
+    for (auto s : lst) {
+        for (char c : s) {
+            if (c == '(') open++;
+            else close++;
+        }
     }
-
     return (open == close) ? "Yes" : "No";
 }
