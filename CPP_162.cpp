ebfd0785843f007943456f8962d28e16c90bd53e
beep boop <githubbot@vadim.me>
2024-07-07 11:28:51 +0200

    Solution 91 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mxo8yxx5 Prompt: Complete the following code given the task description and function signature.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,30 +1,32 @@
-MD5_CTX md5Ctx;
-unsigned char md5Buffer[16];
-size_t len = text.length();
-char *strText = text.c_str();
+#include <string>
+#include <openssl/evp.h>
+#include <openssl/hmac.h>
 
-if (len == 0) {
-    return "";
+using namespace std;
+
+string string_to_md5(string text) {
+    if(text.empty()) return "";
+
+    EVP_MD_CTX md_ctx;
+    EVP_PKEY *pkey = EVP_pkey_new();
+    EVP_MD *md = EVP_get_md5();
+
+    EVP_DigestInit_ex(&md_ctx, md, NULL);
+    EVP_DigestUpdate(&md_ctx, text.c_str(), text.size());
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    int md_length;
+
+    EVP_DigestFinal_ex(&md_ctx, md_value, &md_length);
+
+    string result = "";
+    for(int i=0; i<md_length; ++i) {
+        char buff[3];
+        sprintf(buff, "%02x", (unsigned char)md_value[i]);
+        result.append(buff);
+    }
+
+    EVP_PKEY_free(pkey);
+    EVP_MD_CTX_destroy(&md_ctx);
+
+    return result;
 }
-
-MD5Init(&md5Ctx);
-MD5Update(&md5Ctx, strText, len);
-MD5Final(md5Buffer, &md5Ctx);
-
-stringstream ss;
-for(int i=0; i<16; i++){
-    char c = (char)(md5Buffer[i] >> 4);
-    if(c > '9')
-        c = ((c - 'a' + 10) % 16);
-    else
-        c += '0';
-    
-    ss << c;
-    c = (char)(md5Buffer[i] & 0xF);
-    if(c > '9')
-        c = ((c - 'a' + 10) % 16);
-    else
-        c += '0';
-    ss << c;
-}
-return ss.str();
