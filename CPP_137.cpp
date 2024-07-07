a5f630175cef57dc1094bd66a08c00c530795d1a
beep boop <githubbot@vadim.me>
2024-07-07 06:55:08 +0200

    Solution 79 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lxy8b22d Prompt: 5f8959b9-6705-47b9-9e6f-d6a10d6980f1.cpp:23:29: error: ‘string’ was not declared  in this scope    23 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 5f8959b9-6705-47b9-9e6f-d6a10d6980f1.cpp:23:29: note: suggested alternatives: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 5f8959b9-6705-47b9-9e6f-d6a10d6980f1.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 5f8959b9-6705-47b9-9e6f-d6a10d6980f1.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,21 +1,18 @@
+#include <boost/any.hpp>
 #include <iostream>
 #include <string>
 #include <limits>
 
-using namespace std;
-
-string compare_one(string s1, int s2) {
-    double d1 = stod(s1);
+std::string compare_one(std::string s1, int s2) {
+    double d1 = std::stod(s1);
     if (d1 >= s2) return s1;
     else if (d1 == s2) return "None";
-    else return to_string(s2);
+    else return std::to_string(s2);
 }
 
 int main() {
-    cout << compare_one("123.45", 6789) << endl; 
-    string s1 = "1";  
-    int s2 = 2;  
-    if(compare_one(s1, s2) == s1) cout << s1 << " is greater than or equal to " << s2 << endl;
-    else cout << s2 << " is greater than " << s1 << endl;
+    std::cout << compare_one("123.45", 6789) << std::endl; 
+    if(compare_one("1", 2) == "1") std::cout << "1 is greater than or equal to 2" << std::endl;
+    else std::cout << "2 is greater than 1" << std::endl;
     return 0;
 }
