ed6651cc15190cd2a17444c49c9f63f997abe719
beep boop <githubbot@vadim.me>
2024-07-07 02:15:43 +0200

    Solution 28 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vcnvivvc Prompt: 191a3cfc-feca-4957-baea-22c18e4df0bc.cpp: In function ‘int main()’: 191a3cfc-feca-4957-baea-22c18e4df0bc.cpp:26:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    26 | int main(){       |         ^~ 191a3cfc-feca-4957-baea-22c18e4df0bc.cpp:26:9: note: remove parentheses to defau lt-initialize a variable    26 | int main(){       |         ^~       |         -- 191a3cfc-feca-4957-baea-22c18e4df0bc.cpp:26:9: note: or replace parentheses with  braces to value-initialize a variable 191a3cfc-feca-4957-baea-22c18e4df0bc.cpp:26:11: error: a function-definition is not allowed here before ‘{’ token    26 | int main(){       |           ^ 191a3cfc-feca-4957-baea-22c18e4df0bc.cpp:30:2: error: expected ‘}’ at end of inp ut    30 | }       |  ^ 191a3cfc-feca-4957-baea-22c18e4df0bc.cpp:16:12: note: to match this ‘{’    16 | int main() {       |            ^

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,8 +1,24 @@
-int main() {
+``` 
+#include <vector>
+#include <cmath> 
+#include <cassert>
+
+bool has_close_elements(std::vector<double> numbers, double threshold){
+    for(int i = 0; i < numbers.size(); i++){
+        for(int j = i + 1; j < numbers.size(); j++){
+            if(std::abs(numbers[i] - numbers[j]) <= threshold)
+                return true;
+        }
+    }
+    return false;
+
+}
+
+int main {
     double a[] = {1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
     std::vector<double> b;
     for (double num : a) {
         b.push_back(num);
     }
-    assert(has_close_elements(b, 0.5) == false);
-    return 0;
+    assert (has_close_elements(b, 0.5) == false);
+}
