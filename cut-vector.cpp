ca9ca7f05e45b45db3eb3e1e281191cfd3fc8015
beep boop <githubbot@vadim.me>
2024-07-07 06:28:08 +0200

    Solution 44 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/1ro3j5ba Prompt: bbe6d500-8526-4b8d-84e3-501704058f90.cpp:23:90: note:   ‘std::vector<int>’ is no t derived from ‘const std::move_iterator<_IteratorL>’    23 | t>(left.begin(), left.end()) + vector<int>(right.begin(), right.end());       |                                                                      ^ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/vector:60,                  from bbe6d500-8526-4b8d-84e3-501704058f90.cpp:1: /usr/include/c++/11/bits/stl_iterator.h:1276:5: note: candidate: ‘template<class  _Iterator, class _Container> __gnu_cxx::__normal_iterator<_Iterator, _Container > __gnu_cxx::operator+(typename __gnu_cxx::__normal_iterator<_Iterator, _Contain er>::difference_type, const __gnu_cxx::__normal_iterator<_Iterator, _Container>& )’  1276 |     operator+(typename __normal_iterator<_Iterator, _Container>::differe nce_type       |     ^~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h:1276:5: note:   template argument deduct ion/substitution failed: bbe6d500-8526-4b8d-84e3-501704058f90.cpp:23:90: note:   ‘std::vector<int>’ is no t derived from ‘const __gnu_cxx::__normal_iterator<_Iterator, _Container>’    23 | t>(left.begin(), left.end()) + vector<int>(right.begin(), right.end());       |                                                                      ^

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,9 +1,3 @@
-```
-#include <vector>
-#include <climits>
-#include <cmath>
-using namespace std;
-
 vector<int> cutVector(vector<int>& v) {
     int n = v.size();
     int minDiff = INT_MAX;
@@ -20,5 +14,7 @@ vector<int> cutVector(vector<int>& v) {
     vector<int> left(v.begin(), v.begin() + pos);
     vector<int> right(v.begin() + pos, v.end());
     
-    return vector<int>(left.begin(), left.end()) + right;
-}
+    vector<int> result;
+    result.insert(result.end(), left.begin(), left.end());
+    result.insert(result.end(), right.begin(), right.end());
+    return result;
