b04210dc44381befaa21ed813023598007e03907
beep boop <githubbot@vadim.me>
2024-07-07 06:33:23 +0200

    Solution 27 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jdhym3pn Prompt: 9c5d9697-5978-484e-bd30-180b3e75e305.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 9c5d9697-5978-484e-bd30-180b3e75e305.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 9c5d9697-5978-484e-bd30-180b3e75e305.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ 9c5d9697-5978-484e-bd30-180b3e75e305.cpp:38:6: error: ‘string’ in namespace ‘std ’ does not name a type    38 | std::string toMiniRomanHelper(int num, std::string s1, std::string s2) {       |      ^~~~~~ 9c5d9697-5978-484e-bd30-180b3e75e305.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | ```cpp In file included from 9c5d9697-5978-484e-bd30-180b3e75e305.cpp:51: 9c5d9697-5978-484e-bd30-180b3e75e305.cpp: In function ‘int main()’: 9c5d9697-5978-484e-bd30-180b3e75e305.cpp:53:13: error: ‘int_to_mini_romank’ was not declared in this scope    53 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,33 +1,56 @@
 ```cpp
-#include <iostream>
 #include <string>
 
-std::string toMiniRoman(int num);
-
-int main() {
-    int num;
-    std::cout << "Enter a number: ";
-    std::cin >> num;
-    std::cout << "The mini Roman numeral is: " << toMiniRoman(num) << "\n";
-    return 0;
-}
-
 std::string toMiniRoman(int num) {
-    if (num < 1) {
-        return "";
+    if (num < 1 || num > 3999) {
+        return "Invalid input";
     }
-    
-    int roman[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    std::string romanStr[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    
-    std::string result;
-    
-    for (int i = 0; num > 0 && i < 13; ++i) {
-        while (num >= roman[i]) {
-            num -= roman[i];
-            result += romanStr[i];
+    std::string roman = "";
+    int thousands = num / 1000;
+    for (int i = 0; i < thousands; i++) {
+        roman += "M";
+    }
+    num %= 1000;
+    int hundreds = num / 100;
+    if (hundreds > 0) {
+        roman += toMiniRomanHelper(hundreds, "C", "D");
+    }
+    num %= 100;
+    int tens = num / 10;
+    if (tens > 3) {
+        roman += "CM";
+        num -= 900;
+    } else if (tens > 0) {
+        roman += toMiniRomanHelper(tens, "X", "L");
+    }
+    num %= 10;
+    int ones = num;
+    if (ones == 9) {
+        roman += "IX";
+    } else if (ones >= 5) {
+        roman += toMiniRomanHelper(ones, "V", "IV");
+    } else {
+        for (int i = 0; i < ones; i++) {
+            roman += "I";
         }
     }
-    
-    return result;
+    return roman;
+}
+
+std::string toMiniRomanHelper(int num, std::string s1, std::string s2) {
+    if (num == 4) {
+        return s2;
+    } else if (num == 9) {
+        return s1 + "I";
+    } else {
+        for (int i = 0; i < num / 5; i++) {
+            roman += s1;
+        }
+        return toMiniRomanHelper(num % 5, s1, s2);
+    }
+}
+
+int main() {
+    assert(toMiniRoman(1000) == "M");
+    return 0;
 }
