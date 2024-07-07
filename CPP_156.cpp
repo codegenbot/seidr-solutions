2423e0367ff2967760914f36305c3386f4d01255
beep boop <githubbot@vadim.me>
2024-07-07 09:02:21 +0200

    Solution 90 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jdhym3pn Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: e3ee5dec-c0a3-471c-b8cf-3f9f8795ac33.cpp:45:75:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -34,34 +34,8 @@ std::string toMiniRomanHelper(int num, std::string str, std::string roman) {
 
 std::string int_to_mini_roman(int num) {
     if (num <= 0)
-        return "Nulla";
-    if (num == 1)
-        return "I";
-    if (num == 4)
-        return "IV";
-    if (num == 5)
-        return "V";
-    if (num == 9)
-        return "IX";
-    if (num == 10)
-        return "X";
-    if (num == 40)
-        return "XL";
-    if (num == 50)
-        return "L";
-    if (num == 90)
-        return "XC";
-    if (num == 100)
-        return "C";
-    if (num == 400)
-        return "CD";
-    if (num == 500)
-        return "D";
-    if (num == 900)
-        return "CM";
-    if (num == 1000)
-        return "M";
-    return toMiniRomanHelper(num, "", "MDC");
+        return "";
+    return toMiniRomanHelper(num, "", std::string("MD"));
 }
 
 int main() {
