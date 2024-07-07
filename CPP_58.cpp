dfa187dad8d6d9b59bed999ed90742ba77430b0b
beep boop <githubbot@vadim.me>
2024-07-07 04:27:30 +0200

    Solution 99 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/io7n5spc Prompt: eff05667-43fa-4ddc-8b02-8d20d1d3dd94.cpp:70:34: error: expected primary-expressi on before ‘int’    70 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ eff05667-43fa-4ddc-8b02-8d20d1d3dd94.cpp:70:39: error: expression list treated a s compound expression in initializer [-fpermissive]    70 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from eff05667-43fa-4ddc-8b02-8d20d1d3dd94.cpp:69: eff05667-43fa-4ddc-8b02-8d20d1d3dd94.cpp: In function ‘int main()’: eff05667-43fa-4ddc-8b02-8d20d1d3dd94.cpp:79:20: error: ‘common’ was not declared  in this scope; did you mean ‘std::common’?    79 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ eff05667-43fa-4ddc-8b02-8d20d1d3dd94.cpp:17:22: note: ‘std::common’ declared her e    17 |     std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {       |                      ^~~~~~ In file included from eff05667-43fa-4ddc-8b02-8d20d1d3dd94.cpp:69: eff05667-43fa-4ddc-8b02-8d20d1d3dd94.cpp:79:19: error: ‘issame’ cannot be used a s a function    79 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -5,65 +5,67 @@
 #include <initializer_list>
 #include <iostream>
 
-bool same(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
+namespace std {
+    bool same(std::vector<int> a, std::vector<int> b) {
+        if (a.size() != b.size()) return false;
+        for (int i = 0; i < a.size(); i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
     }
-    return true;
+
+    std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
+        std::set<int> set1(l1.begin(), l1.end());
+        std::set<int> set2(l2.begin(), l2.end());
+        std::vector<int> intersection;
+        for (int i = 0; i < set1.size(); i++) {
+            int element = *set1.begin() + i;
+            if(set2.find(element) != set2.end()){
+                intersection.push_back(element);
+            }
+        }
+        return intersection;
+    }
 }
 
-std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
-    std::set<int> set1(l1.begin(), l1.end());
-    std::set<int> set2(l2.begin(), l2.end());
-    std::vector<int> intersection;
-    for (int i = 0; i < set1.size(); i++) {
-        int element = *set1.begin() + i;
-        if(set2.find(element) != set2.end()){
-            intersection.push_back(element);
-        }
-    }
-    return intersection;
+bool issame(vector<int> a, vector<int> b) {
+    return same(a, b);
 }
 
-int main() {
-    std::vector<int> l1;
-    int n1;
-    std::cout << "Enter the number of elements in list 1: ";
-    std::cin >> n1;
-    for(int i = 0; i < n1; i++) {
-        int x;
-        std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> x;
-        l1.push_back(x);
-    }
+std::vector<int> l1;
+int n1;
+std::cout << "Enter the number of elements in list 1: ";
+std::cin >> n1;
+for(int i = 0; i < n1; i++) {
+    int x;
+    std::cout << "Enter element " << i + 1 << ": ";
+    std::cin >> x;
+    l1.push_back(x);
+}
 
-    std::vector<int> l2;
-    int n2;
-    std::cout << "Enter the number of elements in list 2: ";
-    std::cin >> n2;
-    for(int i = 0; i < n2; i++) {
-        int x;
-        std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> x;
-        l2.push_back(x);
-    }
+std::vector<int> l2;
+int n2;
+std::cout << "Enter the number of elements in list 2: ";
+std::cin >> n2;
+for(int i = 0; i < n2; i++) {
+    int x;
+    std::cout << "Enter element " << i + 1 << ": ";
+    std::cin >> x;
+    l2.push_back(x);
+}
 
-    if(same(l1, l2)) { 
-        std::cout << "The two lists are the same." << std::endl;
-    } else {
-        std::cout << "The two lists are different." << std::endl;
-    }
+if(issame(l1, l2)) { 
+    std::cout << "The two lists are the same." << std::endl;
+} else {
+    std::cout << "The two lists are different." << std::endl;
+}
 
-    std::vector<int> intersection = common(l1, l2);
-    if(intersection.size() != 0) {
-        for(int i = 0; i < intersection.size(); i++) {
-            std::cout << intersection[i] << " ";
-        }
-        std::cout << std::endl;
-    } else {
-        std::cout << "No common elements." << std::endl;
+std::vector<int> intersection = common(l1, l2);
+if(intersection.size() != 0) {
+    for(int i = 0; i < intersection.size(); i++) {
+        std::cout << intersection[i] << " ";
     }
-
-    return 0;
+    std::cout << std::endl;
+} else {
+    std::cout << "No common elements." << std::endl;
 }
