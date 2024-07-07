c8d2d73d9b79032f8e3acbe4f3d4a452d6fcfc2b
beep boop <githubbot@vadim.me>
2024-07-07 08:47:00 +0200

    Solution 29 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/u0n0gq9b Prompt:       |     ^~~       |     std::cin In file included from f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:2: /usr/include/c++/11/iostream:60:18: note: ‘std::cin’ declared here    60 |   extern istream cin;           /// Linked to standard input       |                  ^~~ f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:27:12: error: ‘s’ was not declared in t his scope    27 |     cin >> s;       |            ^ f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:28:5: error: ‘cout’ was not declared in  this scope; did you mean ‘std::cout’?    28 |     cout << (solveBoolean(s) ? "True" : "False");       |     ^~~~       |     std::cout In file included from f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:2: /usr/include/c++/11/iostream:61:18: note: ‘std::cout’ declared here    61 |   extern ostream cout;          /// Linked to standard output       |                  ^~~~ f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:28:26: error: ‘solveBoolean’ cannot be used as a function    28 |     cout << (solveBoolean(s) ? "True" : "False");       |              ~~~~~~~~~~~~^~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -4,26 +4,16 @@
 using namespace std;
 
 bool solveBoolean(string s) {
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '&') {
-            while (i + 1 < s.length() && s[i+1] == '&') i++;
-            s.erase(i, 1);
-            i--;
-        } else if (s[i] == '|') {
-            while (i + 1 < s.length() && s[i+1] == '|') i++;
-            s.erase(i, 1);
-            i--;
-        }
-    }
-
-    string truthTable[2][2] = {{"F", "F"}, {"T", "T"}};
-    int value = (s == "TT" || s == "FF") ? 0 : (s == "TF" || s == "FT") ? 1 : -1;
-    return value != -1 && truthTable[!!stoi(s)][value][0] == 'T';
+    int result = s.find("T");
+    if (result != string::npos)
+        return true;
+    else
+        return false;
 }
 
 int main() {
     string s;
     cin >> s;
-    cout << (solveBoolean(s) ? "True" : "False");
+    cout << (solveBoolean(s) ? "True" : "False") << endl;
     return 0;
 }
