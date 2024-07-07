0dde3e6ee7dc75b553de64fb55f229d9b2bcdea2
beep boop <githubbot@vadim.me>
2024-07-07 03:34:30 +0200

    Solution 4 of CPP/83, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cdytf632 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -1,20 +1,25 @@
-```cpp
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int starts_one_ends(int n) {
     int count = 0;
-    for (int i = 1; i <= 9; i++) {
-        if (i == 1 || i % 10 == 1) {
+    for (int i = 1; i <= pow(10, n-1); i++) {
+        if (i == 1 || to_string(i).back() == '1') {
             count++;
         }
-        for (int j = 1; j < i; j++) {
-            if (j % 10 != 1 && j / 10 != 1) {
+        string str = to_string(i);
+        bool flag = true;
+        for (char c : str) {
+            if (c != '1' && c != '0') {
+                flag = false;
                 break;
             }
         }
+        if (!flag) {
+            continue;
+        }
+        count++;
     }
     return count;
 }
@@ -23,8 +28,5 @@ int main() {
     int n;
     cout << "Enter a positive integer: ";
     cin >> n;
-
-    string str = to_string(n);
-    cout << "The count of the numbers of " << str << "-digit positive integers that start or end with 1 is: " << starts_one_ends(stoi(str)) << endl;
-    return 0;
+    cout << "The count of the numbers of " << n << "-digit positive integers that start or end with 1 is: " << starts_one_ends(n) << endl;
 }
