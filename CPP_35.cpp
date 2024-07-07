752e397298ff9f819600fc1174250f90801c4866
beep boop <githubbot@vadim.me>
2024-07-07 02:53:11 +0200

    Solution 46 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nfgpe593 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from e86ecd65-1902-458d-b8b5-61fadf72fa23.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -3,7 +3,11 @@
 #include <vector>
 
 int main() {
-    std::vector<int> vec({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10});
+    std::vector<int> vec;
+    int num;
+    while (std::cin >> num) {
+        vec.push_back(num);
+    }
     int maxVal = *std::max_element(vec.begin(), vec.end());
     return 0;
 }
