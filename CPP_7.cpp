01d0338b8456b4d2f49b57b38c85f3b4938f88c9
beep boop <githubbot@vadim.me>
2024-07-07 06:26:38 +0200

    Solution 86 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zx302vzt Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 2836bcf0-18a5-4638-b707-ac78b9d7eb19.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2836bcf0-18a5-4638-b707-ac78b9d7eb19.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2836bcf0-18a5-4638-b707-ac78b9d7eb19.cpp:10: 2836bcf0-18a5-4638-b707-ac78b9d7eb19.cpp: In function ‘int main()’: 2836bcf0-18a5-4638-b707-ac78b9d7eb19.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 2836bcf0-18a5-4638-b707-ac78b9d7eb19.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,11 +1,11 @@
 #include <vector>
-#include <algorithm>
+#include <string>
 
-bool issame(vector<string> a,vector<string>b){
-    if(a.size() != b.size())
+bool issame(vector<string> a, vector<string> b) {
+    if(a.size() != b.size()) 
         return false;
-    for(int i = 0; i<a.size(); i++){
-        if(find(b.begin(),b.end(),a[i]) == b.end())
+    for(int i=0; i<a.size(); i++) {
+        if(find(b.begin(), b.end(), a[i]) == b.end())
             return false;
     }
     return true;
@@ -14,19 +14,9 @@ bool issame(vector<string> a,vector<string>b){
 vector<string> filter_by_substring(vector<string> strings, string substring){
     vector<string> result;
     for(string s : strings){
-        if(issame(split(s, substring), split(substring, substring)))
+        if(s.find(substring) != string::npos && issame({s}, {substring})){
             result.push_back(s);
+        }
     }
     return result;
-}
-
-vector<string> split(string s, string substr){
-    vector<string> res;
-    size_t pos = 0;
-    while((pos = s.find(substr)) != string::npos){
-        res.push_back(s.substr(0, pos));
-        s = s.substr(pos + substr.size());
-    }
-    res.push_back(s);
-    return res;
 }
