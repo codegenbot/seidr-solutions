fefd0a7964171f745dd7b22f5fef5e88c204c356
beep boop <githubbot@vadim.me>
2024-07-07 06:35:31 +0200

    Solution 10 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/d7j5f4qo Prompt: /usr/include/c++/11/pstl/glue_algorithm_defs.h:243:1: note: candidate: ‘template <class _ExecutionPolicy, class _ForwardIterator1, class _ForwardIterator2> __pst l::__internal::__enable_if_execution_policy<_ExecutionPolicy, _ForwardIterator2>  std::unique_copy(_ExecutionPolicy&&, _ForwardIterator1, _ForwardIterator1, _For wardIterator2)’   243 | unique_copy(_ExecutionPolicy&& __exec, _ForwardIterator1 __first, _Forwa rdIterator1 __last, _ForwardIterator2 __result);       | ^~~~~~~~~~~ /usr/include/c++/11/pstl/glue_algorithm_defs.h:243:1: note:   template argument deduction/substitution failed: 1811ad19-601d-4643-b102-ac5104c40d8f.cpp:8:46: note:   candidate expects 4 argum ents, 2 provided     8 |     vector<int> result(l.begin(), unique_copy(l.begin(), l.end()));       |                                   ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~ 1811ad19-601d-4643-b102-ac5104c40d8f.cpp: At global scope: 1811ad19-601d-4643-b102-ac5104c40d8f.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 1811ad19-601d-4643-b102-ac5104c40d8f.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,9 +1,21 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 vector<int> unique(vector<int> l) {
-    vector<int> result(l.begin(), unique_copy(l.begin(), l.end()));
+    vector<int> result;
+    copy(unique_copy(l.begin(), l.end()), back_inserter(result));
     sort(result.begin(), result.end());
     return result;
+}
+
+int main() {
+    vector<int> input = {5, 3, 5, 2, 3, 3, 9, 0, 123};
+    vector<int> output = unique(input);
+    for (auto i : output) {
+        cout << i << " ";
+    }
+    return 0;
 }
