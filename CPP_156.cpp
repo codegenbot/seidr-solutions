e3166b134af7d416e9218e47dd47678a9119ad5c
beep boop <githubbot@vadim.me>
2024-07-07 09:05:13 +0200

    Solution 91 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jdhym3pn Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: a1f15049-ec0b-4d7f-bde1-6424c8bc0e0e.cpp:71:75:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,41 +1,66 @@
-```cpp
 #include <iostream>
 #include <string>
 
-int getVal(std::string s) {
-    int val = 0;
-    if (s == "M")
-        val = 1000;
-    else if (s == "D")
-        val = 500;
-    else if (s == "C")
-        val = 100;
-    else if (s == "L")
-        val = 50;
-    else if (s == "X")
-        val = 10;
-    else if (s == "V")
-        val = 5;
-    else if (s == "I")
-        val = 1;
-    return val;
-}
-
-std::string toMiniRomanHelper(int num, std::string str, std::string roman) {
-    if (num == 0)
-        return str;
-    int val = getVal(std::string(1, roman[0]));
-    if (num >= val) {
-        return toMiniRomanHelper(num - val, str + roman[0], roman.substr(1));
-    } else {
-        return toMiniRomanHelper(num, str, roman.substr(1));
-    }
-}
-
 std::string int_to_mini_roman(int num) {
-    if (num <= 0)
-        return "";
-    return toMiniRomanHelper(num, "", std::string("MD"));
+    std::string roman = "";
+    if (num >= 1000)
+        return "M" + int_to_mini_roman(num - 1000);
+    else if (num >= 900)
+        return "CM" + int_to_mini_roman(num - 900);
+    else if (num >= 500)
+        return "D" + int_to_mini_roman(num - 500);
+    else if (num >= 400)
+        return "CD" + int_to_mini_roman(num - 400);
+    else if (num >= 100)
+        return toRoman(num % 100) + int_to_mini_roman(num / 100);
+    else if (num >= 90)
+        return "XC" + int_to_mini_roman(num - 90);
+    else if (num >= 50)
+        return "L" + int_to_mini_roman(num - 50);
+    else if (num >= 40)
+        return "XL" + int_to_mini_roman(num - 40);
+    else if (num >= 10)
+        return toRoman(num % 10) + int_to_mini_roman(num / 10);
+    else if (num >= 9)
+        return "IX" + int_to_mini_roman(num - 9);
+    else if (num >= 5)
+        return "V" + int_to_mini_roman(num - 5);
+    else if (num >= 4)
+        return "IV" + int_to_mini_roman(num - 4);
+    else
+        return toRoman(num);
+
+}
+
+std::string toRoman(int num) {
+    switch (num) {
+        case 1:
+            return "I";
+        case 2:
+            return "II";
+        case 3:
+            return "III";
+        case 4:
+            return "IV";
+        case 5:
+            return "V";
+        case 6:
+            return "VI";
+        case 7:
+            return "VII";
+        case 8:
+            return "VIII";
+        case 9:
+            return "IX";
+        default:
+            if (num >= 10) {
+                if (num % 10 == 0)
+                    return std::string(num / 10, 'X');
+                else
+                    return toRoman(num / 10) + "X" + toRoman(num % 10);
+            } else
+                return "";
+    }
 }
 
 int main() {
