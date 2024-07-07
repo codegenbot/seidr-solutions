94fa8ec60e1d50b215290f1d65669372494205f1
beep boop <githubbot@vadim.me>
2024-07-07 04:52:23 +0200

    Solution 69 of square-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/b8diafrk Prompt: it must return 4649491636 for input 283746, but it returns .

--- square-digits.py
+++ square-digits.py
@@ -1,8 +1,2 @@
 def square_digits(n):
     return "".join([str(int(x) ** 2) for x in reversed(map(int, str(n)))])
-
-OR
-
-from functools import reduce
-def square_digits(n):
-    return reduce(lambda x, y: str(int(x) ** 2) + str(int(y) ** 2), map(int, str(n)))
