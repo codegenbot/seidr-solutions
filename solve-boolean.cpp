700f2eb06af2a9016d688edd2772847d0d1ef30d
beep boop <githubbot@vadim.me>
2024-07-07 08:53:27 +0200

    Solution 32 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/u0n0gq9b Prompt:       |     ^~~       |     std::cin In file included from f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:2: /usr/include/c++/11/iostream:60:18: note: ‘std::cin’ declared here    60 |   extern istream cin;           /// Linked to standard input       |                  ^~~ f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:27:12: error: ‘s’ was not declared in t his scope    27 |     cin >> s;       |            ^ f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:28:5: error: ‘cout’ was not declared in  this scope; did you mean ‘std::cout’?    28 |     cout << (solveBoolean(s) ? "True" : "False");       |     ^~~~       |     std::cout In file included from f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:2: /usr/include/c++/11/iostream:61:18: note: ‘std::cout’ declared here    61 |   extern ostream cout;          /// Linked to standard output       |                  ^~~~ f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:28:26: error: ‘solveBoolean’ cannot be used as a function    28 |     cout << (solveBoolean(s) ? "True" : "False");       |              ~~~~~~~~~~~~^~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,38 +1,25 @@
-```cpp
-#include <vector>
 #include <iostream>
+#include <stack>
 #include <string>
-using namespace std;
 
 bool solveBoolean(string s) {
-    bool result = true;
+    stack<char> st;
     for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'T') {
-            result = true;
-        } else if (s[i] == 'F') {
-            result = false;
+        if (s[i] == '&') {
+            while (!st.empty() && st.top() == '&') {
+                st.pop();
+            }
+            st.push('&');
         } else if (s[i] == '|') {
-            bool temp = result;
-            result = !result;
-            result = result && temp;
-        } else if (s[i] == '&') {
-            bool temp = true;
-            while (i < s.length() && s[i] != ')') {
-                if (s[i] == 'T') {
-                    temp = true;
-                } else if (s[i] == 'F') {
-                    temp = false;
-                    break;
-                }
-                i++;
+            while (!st.empty()) {
+                st.pop();
             }
-            if (!temp) {
-                i--;
-            }
-            result &= temp;
+            st.push('|');
+        } else {
+            st.push(s[i]);
         }
     }
-    return result;
+    return st.top() == 'T';
 }
 
 int main() {
