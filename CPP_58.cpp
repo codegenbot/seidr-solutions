c045019cb1aab031af54a026859b7b219f608f63
beep boop <githubbot@vadim.me>
2024-07-07 04:16:43 +0200

    Solution 89 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/io7n5spc Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 51f3bc62-1ec7-4ac1-8ddd-fc90c630f103.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -3,67 +3,69 @@
 #include <set>
 #include <algorithm>
 #include <sstream>
-using namespace std;
+#include <initializer_list>
 
-bool same(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
-}
-
-vector<int> common(vector<int> l1, vector<int> l2) {
-    set<int> set1(l1.begin(), l1.end());
-    set<int> set2(l2.begin(), l2.end());
-    vector<int> intersection;
-    for (int i = 0; i < set1.size(); i++) {
-        int element = *set1.begin() + i;
-        if(set2.find(element) != set2.end()){
-            intersection.push_back(element);
+namespace std {
+    bool same(std::vector<int> a, std::vector<int> b) {
+        if (a.size() != b.size()) return false;
+        for (int i = 0; i < a.size(); i++) {
+            if (a[i] != b[i]) return false;
         }
-    }
-    return intersection;
-}
-
-int main() {
-    vector<int> l1;
-    int n1;
-    cout << "Enter the number of elements in list 1: ";
-    cin >> n1;
-    for(int i = 0; i < n1; i++) {
-        int x;
-        cout << "Enter element " << i + 1 << ": ";
-        cin >> x;
-        l1.push_back(x);
+        return true;
     }
 
-    vector<int> l2;
-    int n2;
-    cout << "Enter the number of elements in list 2: ";
-    cin >> n2;
-    for(int i = 0; i < n2; i++) {
-        int x;
-        cout << "Enter element " << i + 1 << ": ";
-        cin >> x;
-        l2.push_back(x);
-    }
-
-    if(same(l1, l2)) {
-        cout << "The two lists are the same." << endl;
-    } else {
-        cout << "The two lists are different." << endl;
-    }
-
-    vector<int> intersection = common(l1, l2);
-    if(intersection.size() != 0) {
-        for(int i = 0; i < intersection.size(); i++) {
-            cout << intersection[i] << " ";
+    std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
+        std::set<int> set1(l1.begin(), l1.end());
+        std::set<int> set2(l2.begin(), l2.end());
+        std::vector<int> intersection;
+        for (int i = 0; i < set1.size(); i++) {
+            int element = *set1.begin() + i;
+            if(set2.find(element) != set2.end()){
+                intersection.push_back(element);
+            }
         }
-        cout << endl;
-    } else {
-        cout << "No common elements." << endl;
+        return intersection;
     }
 
-    return 0;
+    int main() {
+        std::vector<int> l1;
+        int n1;
+        std::cout << "Enter the number of elements in list 1: ";
+        std::cin >> n1;
+        for(int i = 0; i < n1; i++) {
+            int x;
+            std::cout << "Enter element " << i + 1 << ": ";
+            std::cin >> x;
+            l1.push_back(x);
+        }
+
+        std::vector<int> l2;
+        int n2;
+        std::cout << "Enter the number of elements in list 2: ";
+        std::cin >> n2;
+        for(int i = 0; i < n2; i++) {
+            int x;
+            std::cout << "Enter element " << i + 1 << ": ";
+            std::cin >> x;
+            l2.push_back(x);
+        }
+
+        if(std::same(l1, l2)) { 
+            std::cout << "The two lists are the same." << std::endl;
+        } else {
+            std::cout << "The two lists are different." << std::endl;
+        }
+
+        std::vector<int> intersection = common(l1, l2);
+        if(intersection.size() != 0) {
+            for(int i = 0; i < intersection.size(); i++) {
+                std::cout << intersection[i] << " ";
+            }
+            std::cout << std::endl;
+        } else {
+            std::cout << "No common elements." << std::endl;
+        }
+
+        return 0;
+    }
 }
