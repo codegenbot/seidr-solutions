7cca299745f64b525fe67f805ca8fbb0ca8495fb
beep boop <githubbot@vadim.me>
2024-07-07 05:18:05 +0200

    Solution 77 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tfufwup9 Prompt: _Tp>&) [with _ForwardIterator = std::__cxx11::basic_string<char>*; _Size = long unsigned int; _Tp = std::__cxx11::basic_string<char>]’ /usr/include/c++/11/bits/stl_vector.h:1606:36:   required from ‘void std::vector <_Tp, _Alloc>::_M_default_initialize(std::vector<_Tp, _Alloc>::size_type) [with _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11::ba sic_string<char> >; std::vector<_Tp, _Alloc>::size_type = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:512:9:   required from ‘std::vector<_Tp, _ Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocator_type&) [with  _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11::b asic_string<char> >; std::vector<_Tp, _Alloc>::size_type = long unsigned int; st d::vector<_Tp, _Alloc>::allocator_type = std::allocator<std::__cxx11::basic_stri ng<char> >]’ 57aeaed6-9b0b-4606-83e4-86013627c08e.cpp:50:25:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -47,12 +47,12 @@ int main() {
     int n;
     cout << "Enter the number of integers: ";
     cin >> n;
-    vector<string> arr;
+    vector<string> arr(std::vector<string>(n));
     for(int i=0; i<n; i++) {
         string num;
         cout << "Enter integer " << i+1 << ": ";
         cin >> num;
-        arr.push_back(num);
+        arr[i] = num;
     }
     vector<string> output = by_length(arr);
     cout << "The numbers in the order of their lengths are: ";
