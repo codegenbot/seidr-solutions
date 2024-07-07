83b311cabd0231d765a367b5f0281d8f255ae8d9
beep boop <githubbot@vadim.me>
2024-07-07 05:34:06 +0200

    Solution 5 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/01ftl4ye Prompt:  6579 |     getline(basic_istream<_CharT, _Traits>&& __is,       |     ^~~~~~~ /usr/include/c++/11/bits/basic_string.h:6579:5: note:   template argument deduct ion/substitution failed: d85502fb-2f1e-401e-be29-3292c8886082.cpp:14:43: note:   candidate expects 3 argu ments, 2 provided    14 |     if (!(s2 >> c) || !(s2 >> std::getline(s2, string()) >> d)) {       |                               ~~~~~~~~~~~~^~~~~~~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from d85502fb-2f1e-401e-be29-3292c8886082.cpp:1: /usr/include/c++/11/bits/basic_string.h:6586:5: note: candidate: ‘std::basic_ist ream<_CharT, _Traits>& std::getline(std::basic_istream<_CharT, _Traits>&&, std:: __cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ (near match)  6586 |     getline(basic_istream<_CharT, _Traits>&& __is,       |     ^~~~~~~ /usr/include/c++/11/bits/basic_string.h:6586:5: note:   conversion of argument 2  would be ill-formed: d85502fb-2f1e-401e-be29-3292c8886082.cpp:14:48: error: cannot bind non-const lva lue reference of type ‘std::__cxx11::basic_string<char>&’ to an rvalue of type ‘ std::string’ {aka ‘std::__cxx11::basic_string<char>’}    14 |     if (!(s2 >> c) || !(s2 >> std::getline(s2, string()) >> d)) {       |                                                ^~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -5,19 +5,15 @@ using namespace std;
 
 bool simplify(string x, string n) {
     int a = 0, b = 1, c = 0, d = 1;
-    string cstr;
     stringstream s1(x), s2(n);
     
     if (!(s1 >> a) || !(s1 >> '/' >> b)) { 
         return false; 
     }
     
-    if (!(s2 >> cstr) || (cstr != "/" && !((s2 >> c) || (s2 >> d))) ) { 
-        return false; 
-    }
-    
-    if (s2.peek() == '/') {
-        s2 >> d;
+    string n_str;
+    if (!(s2 >> c) || !(getline(s2, n_str, '/') && s2 >> d)) {
+        return false;
     }
     
     return (a*d == b*c);
