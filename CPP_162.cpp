1b0a6f0589929818a77aa4fdcd5bd6589f836f8e
beep boop <githubbot@vadim.me>
2024-07-07 06:06:05 +0200

    Solution 6 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/d64c00go Prompt: 4e956322-a705-47c4-a9fe-928db2a3408b.cpp: In function ‘std::string string_to_md5 (std::string)’: 4e956322-a705-47c4-a9fe-928db2a3408b.cpp:10:8: warning: ‘unsigned char* MD5(cons t unsigned char*, size_t, unsigned char*)’ is deprecated: Since OpenSSL 3.0 [-Wd eprecated-declarations]    10 |     MD5((const unsigned char*)text.c_str(), text.size(), md5);       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 4e956322-a705-47c4-a9fe-928db2a3408b.cpp:2: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ /usr/bin/ld: /tmp/ccGGxkSy.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> >)': 4e956322-a705-47c4-a9fe-928db2a3408b.cpp:(.text+0x97): undefined reference to `M D5' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,18 +1,24 @@
 #include <string>
 #include <openssl/md5.h>
+#include <openssl/evp.h>
 
 using namespace std;
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
-    unsigned char md5[16];
-    MD5((const unsigned char*)text.c_str(), text.size(), md5);
+    EVP_MD_CTX mdctx;
+    EVP_MD_CTX_init(&mdctx);
+    EVP_DigestInit_ex(&mdctx, EVP_md5(), NULL);
+    EVP_DigestUpdate(&mdctx, (const unsigned char*)text.c_str(), text.size());
+    unsigned char md[16];
+    unsigned int len;
+    EVP_DigestFinal_ex(&mdctx, md, &len);
 
     string result;
     for (int i = 0; i < 16; ++i) {
         char buffer[3];
-        sprintf(buffer, "%02x", md5[i]);
+        sprintf(buffer, "%02x", md[i]);
         result += buffer;
     }
 
