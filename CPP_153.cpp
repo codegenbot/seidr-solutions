afa75463144ef6a10856bbcc37420516ab71eb48
beep boop <githubbot@vadim.me>
2024-07-07 08:12:51 +0200

    Solution 18 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2y6mujl6 Prompt:       |                         ^~~~~~ 979ae723-b5f1-4cdf-bc66-54203d9400b8.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ 979ae723-b5f1-4cdf-bc66-54203d9400b8.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ 979ae723-b5f1-4cdf-bc66-54203d9400b8.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ 979ae723-b5f1-4cdf-bc66-54203d9400b8.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from 979ae723-b5f1-4cdf-bc66-54203d9400b8.cpp:19: 979ae723-b5f1-4cdf-bc66-54203d9400b8.cpp: In function ‘int main()’: 979ae723-b5f1-4cdf-bc66-54203d9400b8.cpp:21:32: error: ‘Strongest_Extension’ can not be used as a function    21 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <cctype>
 
 std::string Strongest_Extension(std::string class_name, std::vector<std::string> extensions) {
     int strongest_strength = 0;
@@ -17,13 +18,4 @@ std::string Strongest_Extension(std::string class_name, std::vector<std::string>
     }
 
     return class_name + "." + strongest_extension;
-}
-
-int main() {
-    std::string class_name = "MyClass";
-    std::vector<std::string> extensions = {"cpp", "java", "python", "swift"};
-
-    std::cout << Strongest_Extension(class_name, extensions) << std::endl;
-
-    return 0;
 }
