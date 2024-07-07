e727d2835d28d0f484d9edae655ba8ee2db23ea7
beep boop <githubbot@vadim.me>
2024-07-07 05:47:59 +0200

    Solution 8 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/017rk2t6 Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 51616882-0a9e-423a-9012-b7368495e943.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 51616882-0a9e-423a-9012-b7368495e943.cpp:34:44: error: ‘b’ was not declared in t his scope    34 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 51616882-0a9e-423a-9012-b7368495e943.cpp:33: 51616882-0a9e-423a-9012-b7368495e943.cpp: In function ‘int main()’: 51616882-0a9e-423a-9012-b7368495e943.cpp:43:20: error: ‘sorted_list_sum’ was not  declared in this scope    43 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 51616882-0a9e-423a-9012-b7368495e943.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <sstream>
 
-using string = std::string;
+using namespace std;
 
 bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) return false;
@@ -33,5 +33,6 @@ vector<string> sorted_list_sum(vector<string> lst) {
 }
 
 int main() {
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), sorted_list_sum({"cc", "dd", "aaaa", "bbbb"})));
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+    return 0;
 }
