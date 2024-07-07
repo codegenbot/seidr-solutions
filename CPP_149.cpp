8b1933931d8629a991d1fb58f9619f69a4da6dad
beep boop <githubbot@vadim.me>
2024-07-07 08:33:52 +0200

    Solution 78 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mwz9wfa4 Prompt:    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 01a9928a-b41d-499c-9588-c3a1a0e282c0.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 01a9928a-b41d-499c-9588-c3a1a0e282c0.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 01a9928a-b41d-499c-9588-c3a1a0e282c0.cpp:23: 01a9928a-b41d-499c-9588-c3a1a0e282c0.cpp: In function ‘int main()’: 01a9928a-b41d-499c-9588-c3a1a0e282c0.cpp:33:20: error: ‘sorted_list_sum’ was not  declared in this scope    33 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 01a9928a-b41d-499c-9588-c3a1a0e282c0.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,27 +1,7 @@
-std::vector<string> sorted_list_sum(vector<string> lst) {
-    vector<int> sum;
-    for (const auto& str : lst) {
-        int total = 0;
-        for (char c : str) {
-            if (islower(c)) {
-                total += tolower(c) - 'a' + 1;
-            } else if (isupper(c)) {
-                total += toupper(c) - 'A' + 27;
-            }
-        }
-        sum.push_back(total);
+long sorted_list_sum(vector<string> lst) {
+    long sum = 0;
+    for (const string& str : lst) {
+        sum += stoll(str);
     }
-
-    vector<string> sorted_lst = lst;
-
-    std::sort(sorted_lst.begin(), sorted_lst.end(),
-              [](const string& a, const string& b) {
-                  if (stoi(a.substr(1)) != stoi(b.substr(1))) {
-                      return stoi(a.substr(1)) < stoi(b.substr(1));
-                  } else {
-                      return a < b;
-                  }
-              });
-
-    return sorted_lst;
+    return sum;
 }
