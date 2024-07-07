211df1bc38d4a92776e38660e1296c303baa219c
beep boop <githubbot@vadim.me>
2024-07-07 07:21:10 +0200

    Solution 25 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/a1h9xy5b Prompt: 2b701902-ea07-4f99-9ce8-53efa1548433.cpp:1:15: error: ‘vector’ was not declared in this scope     1 | int skjkasdkd(vector<int> lst) {       |               ^~~~~~ 2b701902-ea07-4f99-9ce8-53efa1548433.cpp:1:22: error: expected primary-expressio n before ‘int’     1 | int skjkasdkd(vector<int> lst) {       |                      ^~~ In file included from 2b701902-ea07-4f99-9ce8-53efa1548433.cpp:30: 2b701902-ea07-4f99-9ce8-53efa1548433.cpp: In function ‘int main()’: 2b701902-ea07-4f99-9ce8-53efa1548433.cpp:32:22: error: ‘skjkasdkd’ cannot be use d as a function    32 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ~~~~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,40 +1,49 @@
 #include <vector>
+#include <iostream>
 
-class Solution {
-public:
-    int skjkasdkd(vector<int> lst) {
-        int maxPrime = 0;
-        for (int num : lst) {
-            if (isPrime(num)) {
-                if (num > maxPrime) {
-                    maxPrime = num;
-                }
+int skjkasdkd(std::vector<int> lst) {
+    int maxPrime = 0;
+    for (int num : lst) {
+        if (isPrime(num)) {
+            if (num > maxPrime) {
+                maxPrime = num;
             }
         }
-        int sumOfDigits = 0;
-        while (maxPrime > 0) {
-            sumOfDigits += maxPrime % 10;
-            maxPrime /= 10;
-        }
-        return sumOfDigits;
     }
+    int sumOfDigits = 0;
+    while (maxPrime > 0) {
+        sumOfDigits += maxPrime % 10;
+        maxPrime /= 10;
+    }
+    return sumOfDigits;
+}
 
-    bool isPrime(int num) {
-        if (num <= 1) {
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
             return false;
         }
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
-};
+    return true;
+}
 
 int main() {
-    Solution solution;
-    vector<int> lst = {1, 2, 3, 4, 5};
-    cout << solution.skjkasdkd(lst);
+    std::vector<int> lst;
+    int n;
+    std::cout << "Enter the number of elements in the list: ";
+    std::cin >> n;
+    
+    for(int i = 1; i <= n; i++) {
+        int num;
+        std::cout << "Enter element " << i << ": ";
+        std::cin >> num;
+        lst.push_back(num);
+    }
+    
+    std::cout << "Sum of digits of the largest prime number is: " << skjkasdkd(lst) << std::endl;
+    
     return 0;
 }
