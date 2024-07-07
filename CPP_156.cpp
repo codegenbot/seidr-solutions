551fa319c4d1cc1afce5570d074e26bc4c0a9923
beep boop <githubbot@vadim.me>
2024-07-07 08:29:28 +0200

    Solution 71 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jdhym3pn Prompt: In file included from b21cc872-0253-4e7c-b948-771df9171f66.cpp:39: b21cc872-0253-4e7c-b948-771df9171f66.cpp: In function ‘int main()’: b21cc872-0253-4e7c-b948-771df9171f66.cpp:41:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    41 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,41 +1,4 @@
-#include <string>
-#include <cassert>
-
-std::string toMiniRomanHelper(int num, std::string roman, std::string symbols) {
-    if (num == 0)
-        return roman;
-
-    size_t i = symbols.find_first_of("IVXL");
-    if (i != std::string::npos) {
-        int count = 1;
-        while ((i + 1 < symbols.size()) && (symbols[i+1] == symbols[i])) {
-            ++i; ++count;
-        }
-        if (count == 3)
-            roman += symbols[i];
-        else
-            for (int j = 0; j < count; ++j) 
-                roman += symbols[i];
-
-        num -= count * (symbols[i] - 'I');
-    } else {
-        int value = std::stoi(symbols.substr(0,1));
-        if (value <= num) {
-            roman += symbols[0];
-            num -= value;
-            return toMiniRomanHelper(num, roman, symbols);
-        }
-    }
-
-    return roman;
-}
-
 std::string int_to_mini_roman(int num) {
     if (num <= 0)
         return "";
     return toMiniRomanHelper(num, "", "MDC");
-}
-
-int main() {
-    assert(int_to_mini_roman(1000).compare("M") == 0);
-}
