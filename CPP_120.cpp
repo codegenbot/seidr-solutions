e30a0d535f1a72e4a0f0bb38fd6d8bab0329c5ef
beep boop <githubbot@vadim.me>
2024-07-07 07:55:46 +0200

    Solution 57 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/x19lori4 Prompt:       |                    ^~~ 2aa92148-04f9-4158-a976-946b4de45a91.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2aa92148-04f9-4158-a976-946b4de45a91.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2aa92148-04f9-4158-a976-946b4de45a91.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2aa92148-04f9-4158-a976-946b4de45a91.cpp:16: 2aa92148-04f9-4158-a976-946b4de45a91.cpp: In function ‘int main()’: 2aa92148-04f9-4158-a976-946b4de45a91.cpp:26:20: error: ‘maximum’ was not declare d in this scope    26 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 2aa92148-04f9-4158-a976-946b4de45a91.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,10 +1,7 @@
-```cpp
 #include <queue>
 #include <vector>
 
-using namespace std;
-
-bool issame(vector<int> a,vector<int>b){
+bool issame(std::vector<int> a,std::vector<int>b){
     if(a.size() != b.size()) return false;
     for(int i = 0; i < a.size(); i++){
         if(a[i] != b[i]) return false;
@@ -12,14 +9,14 @@ bool issame(vector<int> a,vector<int>b){
     return true;
 }
 
-vector<int> maximum(vector<int> arr, int k) {
-    priority_queue<int> pq;
+std::vector<int> maximum(std::vector<int> arr, int k) {
+    std::priority_queue<int> pq;
     for (int i : arr) {
         pq.push(i);
         if (pq.size() > k)
             pq.pop();
     }
-    vector<int> result(k);
+    std::vector<int> result(k);
     for (int i = k - 1; i >= 0; --i)
         result[i] = pq.top(), pq.pop();
     return result;
