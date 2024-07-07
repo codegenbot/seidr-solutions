553c864772b048bdb9f3f9ee85c86b09c60734af
beep boop <githubbot@vadim.me>
2024-07-07 06:35:06 +0200

    Solution 75 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qyjulrnb Prompt: b466f317-5a0e-47aa-96a6-55f1a528221f.cpp:3:9: fatal error: boost/any.hpp: No suc h file or directory     3 | #include<boost/any.hpp>       |         ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,14 +1,17 @@
+#include <vector>
+#include <string>
 #include <boost/any.hpp>
-
+#include <list>
+typedef std::list<boost::any> list_any;
 using namespace std;
 
-vector<int> filter_integers(list<any> values) {
+vector<int> filter_integers(list_any values){
     vector<int> result;
     for(auto value : values){
-        if(value.type() == typeid(int)){
-            int integer_value = boost::any_cast<int>(value);
+        boost::any any_value = value;
+        if(any_value.type() == typeid(int)){
+            int integer_value = boost::any_cast<int>(any_value);
             result.push_back(integer_value);
         }
     }
     return result;
-}
