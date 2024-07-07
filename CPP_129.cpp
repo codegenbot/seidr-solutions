54e4d0aa8578887148291efa86656273ef16dcfb
beep boop <githubbot@vadim.me>
2024-07-07 10:21:34 +0200

    Solution 6 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qxktwr7o Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,41 +1,34 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 vector<int> minPath(vector<vector<int>> grid, int k) {
     int n = grid.size();
     vector<vector<int>> dp(n, vector<int>(n));
-    
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (i == 0 && j == 0) {
                 dp[i][j] = grid[i][j];
             } else if (i > 0) {
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+                dp[i][j] = min({grid[i][j], dp[i-1][j]});
+            } else if (j > 0) {
+                dp[i][j] = min({grid[i][j], dp[i][j-1]});
+            }
+        }
+    }
+
+    vector<int> res;
+    int i = n - 1, j = n - 1;
+    for (int l = 0; l < k; l++) {
+        res.push_back(grid[i][j]);
+        if (i > 0 && j > 0) {
+            if (grid[i-1][j] < grid[i][j-1]) {
+                i--;
             } else {
-                dp[i][j] = min(dp[i][j-1], dp[i-1][j]) + grid[i][j];
+                j--;
             }
+        } else if (i > 0) {
+            i--;
+        } else {
+            j--;
         }
     }
-    
-    int min_val = INT_MAX;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (dp[i][j] == dp[n-1][n-1]) {
-                vector<int> path;
-                int val = grid[i][j];
-                for (int l = 0; l <= k; l++) {
-                    path.push_back(val);
-                    if (l < k) {
-                        if (i > 0) i--;
-                        else if (j > 0) j--;
-                        else break;
-                    }
-                }
-                return path;
-            }
-        }
-    }
-    
-    return {};
+
+    return res;
 }
