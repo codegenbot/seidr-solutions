7264e733f81333c311ce1d8a22b6d898be537d5d
beep boop <githubbot@vadim.me>
2024-07-07 03:48:04 +0200

    Solution 93 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4otdz96k Prompt:    17 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ f9a29df3-e9c4-4be9-991f-456da7f0ed98.cpp:17:36: error: expected primary-expressi on before ‘float’    17 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ f9a29df3-e9c4-4be9-991f-456da7f0ed98.cpp:17:43: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from f9a29df3-e9c4-4be9-991f-456da7f0ed98.cpp:16: f9a29df3-e9c4-4be9-991f-456da7f0ed98.cpp: In function ‘int main()’: f9a29df3-e9c4-4be9-991f-456da7f0ed98.cpp:26:20: error: ‘sort_even’ was not decla red in this scope    26 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ f9a29df3-e9c4-4be9-991f-456da7f0ed98.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,43 +1,23 @@
 ```cpp
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include <algorithm>
 
-bool compareVectors(const std::vector<float>& a, const std::vector<float>& b) {
+bool issame(std::vector<float> a, std::vector<float> b) {
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); i++) {
-        if (!compareFloats(a[i], b[i])) return false;
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
 
-bool compareFloats(float a, float b) {
-    return std::abs(a - b) > 1e-5;
-}
-
-std::vector<float> sortEven(std::vector<float> input) {
-    std::vector<float> result;
-    for (int i = 0; i < input.size(); i++) {
-        if (i % 2 == 0) {
-            float minVal = input[0];
-            for (float val : input) {
-                if (!compareFloats(val, minVal)) {
-                    minVal = val;
-                }
-            }
-            result.push_back(minVal);
-        } else {
-            auto it = std::stable_partition(input.begin(), input.end(),
-                                             [back = &result.back()](const float& val) {
-                                                 return !compareFloats(val, *back);
-                                             });
-            if (it != input.end()) {
-                result.push_back(*it);
-            }
-        }
+std::vector<float> sort_even(std::vector<float> input) {
+    std::vector<float> even;
+    for (float val : input) {
+        if (val % 2 == 0) even.push_back(val);
     }
-    return result;
+    std::sort(even.begin(), even.end());
+    return even;
 }
 
 int main() { 
@@ -49,7 +29,7 @@ int main() {
         input.push_back(num);
     }
     std::cin.ignore();
-    for (float val : sortEven(input)) {
+    for (float val : sort_even(input)) {
         std::cout << val << " ";
     }
 }
