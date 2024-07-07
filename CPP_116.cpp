fcade54ffad078e39e40687203d43101a73c1fbc
beep boop <githubbot@vadim.me>
2024-07-07 07:34:26 +0200

    Solution 4 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/v0gzscpn Prompt: Complete the following code given the task description and function signature.

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,3 +1,16 @@
+#include <vector>
+#include <algorithm>
+
 vector<int> sort_array(vector<int> arr) {
-    return vector<int>(arr.begin(), arr.end());
+    vector<int> result;
+    for (int i = 0; i < arr.size(); i++) {
+        int ones = __builtin_popcount(arr[i]);
+        result.push_back({ones, arr[i]});
+    }
+    std::sort(result.begin(), result.end());
+    vector<int> final_result;
+    for (auto& x : result) {
+        final_result.push_back(x.second);
+    }
+    return final_result;
 }
