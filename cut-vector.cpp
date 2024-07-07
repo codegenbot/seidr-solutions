034d79b0a38c414449ed139da0b1f324d58ce2ef
beep boop <githubbot@vadim.me>
2024-07-07 05:10:59 +0200

    Solution 28 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/1ro3j5ba Prompt:     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; 3fccedb5-7da7-478e-9c2e-ada50517475b.cpp:9:20: error: ‘abs’ was not declared in this scope     9 |         int diff = abs(v[i] - v[0]);       |                    ^~~ 3fccedb5-7da7-478e-9c2e-ada50517475b.cpp: In function ‘int main()’: 3fccedb5-7da7-478e-9c2e-ada50517475b.cpp:21:5: error: ‘cin’ was not declared in this scope    21 |     cin >> n;       |     ^~~ 3fccedb5-7da7-478e-9c2e-ada50517475b.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; 3fccedb5-7da7-478e-9c2e-ada50517475b.cpp:29:5: error: ‘cout’ was not declared in  this scope    29 |     cout << "[";       |     ^~~~ 3fccedb5-7da7-478e-9c2e-ada50517475b.cpp:29:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’?

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,23 +1,22 @@
-#include <iostream>
 #include <vector>
-#include <limits>
+#include <climits>
+#include <iostream>
 
 using namespace std;
 
 pair<vector<int>, vector<int>> cutVector(vector<int> v) {
-    int minDiff = INT_MAX;
-    pair<vector<int>, vector<int>> res;
+    int min_diff = INT_MAX;
+    int idx = 0;
     
     for (int i = 1; i < v.size(); i++) {
-        int diff = abs(v[i-1] - v[i]);
-        if (diff <= minDiff) {
-            minDiff = diff;
-            res.first = vector<int>(v.begin(), v.begin() + i);
-            res.second = vector<int>(v.begin() + i, v.end());
+        int diff = abs(v[i] - v[0]);
+        if (diff <= min_diff) {
+            min_diff = diff;
+            idx = i;
         }
     }
     
-    return res;
+    return {vector<int>(v.begin(), v.begin() + idx), vector<int>(v.begin() + idx, v.end())};
 }
 
 int main() {
@@ -27,14 +26,19 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> v[i];
     }
+    
     pair<vector<int>, vector<int>> result = cutVector(v);
+    
     cout << "[";
-    for (int num : result.first) {
-        cout << num << " ";
+    for (int i = 0; i < result.first.size() - 1; i++) {
+        cout << result.first[i] << " ";
     }
-    cout << "] [";
-    for (int num : result.second) {
-        cout << num << " ";
+    cout << result.first.back() << "]\n";
+    cout << "[";
+    for (int i = 0; i < result.second.size() - 1; i++) {
+        cout << result.second[i] << " ";
     }
-    cout << "]" << endl;
+    cout << result.second.back() << "]\n";
+    
     return 0;
+}
