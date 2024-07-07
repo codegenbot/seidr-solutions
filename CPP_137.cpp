ed040289fce2b9e098971f96d3471c05b7c5486f
beep boop <githubbot@vadim.me>
2024-07-07 10:45:07 +0200

    Solution 30 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6mhsqfmo Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,46 +1,39 @@
 #include <boost/any.hpp>
-#include <string>
-#include <algorithm>
 
-using namespace std;
+using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return b;
-    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return b;
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = any_cast<string>(a);
-        string str2 = any_cast<string>(b);
-        istringstream iss(str1);
-        double num1;
-        iss >> num1;
-        istringstream iss2(str2);
-        double num2;
-        iss2 >> num2;
-        if (num1 > num2) {
-            return a;
-        } else if (num1 < num2) {
-            return b;
-        } else {
-            return boost::any("None");
-        }
-    } else if (a.type() == typeid(string) && (b.type() == typeid(int) || b.type() == typeid(double))) {
-        string str = any_cast<string>(a);
-        istringstream iss(str);
-        double num;
-        iss >> num;
-        return b;
-    } else if ((a.type() == typeid(int) || a.type() == typeid(double)) && b.type() == typeid(string)) {
-        boost::any temp = a;
-        a = b;
-        b = temp;
-    }
-    if (a > b) {
-        return a;
-    } else if (a < b) {
-        return b;
-    } else {
+    if (is_none(a) && is_none(b)) {
         return boost::any("None");
+    }
+
+    if (!is_none(a)) {
+        if (!is_none(b)) {
+            if (get<double>(a) > get<double>(b))
+                return a;
+            else if (get<double>(a) < get<double>(b))
+                return b;
+            else
+                return boost::any("None");
+        } else {
+            if (get<int>(a) > 0)
+                return a;
+            else
+                return b;
+        }
+    } else {
+        if (!is_none(b)) {
+            if (get<int>(b) > 0)
+                return b;
+            else
+                return a;
+        } else {
+            if (get<string>(a) > get<string>(b))
+                return a;
+            else if (get<string>(a) < get<string>(b))
+                return b;
+            else
+                return boost::any("None");
+        }
     }
 }
