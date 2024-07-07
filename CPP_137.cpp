4bed9a9c6a6a9e641b3db88798532ea6e4dfa0a4
beep boop <githubbot@vadim.me>
2024-07-07 05:25:25 +0200

    Solution 12 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lxy8b22d Prompt: ined here    12 | int main() {       |     ^~~~ In file included from 48aac72d-56c0-4ee4-a515-177c2ef2edab.cpp:17: 48aac72d-56c0-4ee4-a515-177c2ef2edab.cpp: In function ‘int main()’: 48aac72d-56c0-4ee4-a515-177c2ef2edab.cpp:19:13: error: ‘boost’ has not been decl ared    19 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 48aac72d-56c0-4ee4-a515-177c2ef2edab.cpp:19:35: error: expected primary-expressi on before ‘>’ token    19 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 48aac72d-56c0-4ee4-a515-177c2ef2edab.cpp:19:62: error: could not convert ‘1’ fro m ‘int’ to ‘std::string’ {aka ‘std::__cxx11::basic_string<char>’}    19 |    assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "None ");       |                                                             ^       |                                                             |       |                                                             int

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -4,14 +4,15 @@
 using namespace std;
 
 string compare_one(string s1, string s2) {
-    if (stod(s1) > stod(s2)) return s1;
-    else if (stod(s1) == stod(s2)) return "None";
+    double d1 = stod(s1);
+    double d2 = stod(s2);
+
+    if (d1 > d2) return s1;
+    else if (d1 == d2) return "None";
     else return s2;
 }
 
 int main() {
     cout << compare_one("123.45", "6789") << endl; 
-    if(compare_one("1", "2") == "1") cout << "1 is greater than or equal to 2" << endl;
-    else cout << "2 is greater than 1" << endl;
     return 0;
 }
