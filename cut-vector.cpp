f3ea6d853b190097116ea1c046ce76fd7c2cf9f3
beep boop <githubbot@vadim.me>
2024-07-07 03:35:21 +0200

    Solution 90 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/a04ox21m Prompt:   +++ |+#include <vector>     2 | using namespace std; 221a143f-1865-4fbc-acce-f0a5e2529035.cpp:7:12: error: expected primary-expressio n before ‘int’     7 |     vector<int> vec(n);       |            ^~~ 221a143f-1865-4fbc-acce-f0a5e2529035.cpp:9:16: error: ‘vec’ was not declared in this scope     9 |         cin >> vec[i];       |                ^~~ 221a143f-1865-4fbc-acce-f0a5e2529035.cpp:11:19: error: expected primary-expressi on before ‘int’    11 |     vector<vector<int>> res = cutVector(vec);       |                   ^~~ 221a143f-1865-4fbc-acce-f0a5e2529035.cpp:14:25: error: ‘res’ was not declared in  this scope    14 |     for (int i = 0; i < res.size(); i++) {       |                         ^~~ 221a143f-1865-4fbc-acce-f0a5e2529035.cpp: At global scope: 221a143f-1865-4fbc-acce-f0a5e2529035.cpp:30:1: error: ‘vector’ does not name a t ype    30 | vector<vector<int>> cutVector(vector<int> vec) {       | ^~~~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,5 +1,5 @@
-```cpp
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
@@ -11,15 +11,18 @@ int main() {
     }
     vector<vector<int>> res = cutVector(vec);
     cout << "[";
+    bool first = true;
     for (int i = 0; i < res.size(); i++) {
         cout << "[";
-        for(int j = 0; j < res[i].size(); j++){
-            for(auto num:res[i]){
-                cout<<num<<" ";
-            }
-            if(i==res.size()-1 && j==res[i].size()-1)cout<<"]]";
-            else cout << "], [";
+        string str = "";
+        bool firstElement = true;
+        for(auto num:res[i]){
+            if(firstElement)firstElement=false;else str+=", ";
+            str += to_string(num);
         }
+        cout<<str<<"] ";
+        if(i < res.size() - 1) cout << "], [";
+        else cout << "]";
     }
     cout << "]";
     return 0;
@@ -28,10 +31,10 @@ int main() {
 vector<vector<int>> cutVector(vector<int> vec) {
     int n = vec.size();
     vector<vector<int>> res(2);
-    for (int i = 0; i < n-1; i++) {
-        if (vec[i] - vec[0] >= 0) {
-            res[0].assign(vec.begin(), vec.begin() + i+1);
-            res[1].assign(vec.begin()+i, vec.end());
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || vec[i] - vec[i-1] >= 0) {
+            res[0].assign(vec.begin(), vec.begin() + i + 1);
+            res[1].assign(vec.begin() + i, vec.end());
             break;
         }
     }
