772baed261e6ce3bee12a663cffb01ad427d2ca5
beep boop <githubbot@vadim.me>
2024-07-07 04:49:34 +0200

    Solution 91 of CPP/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nmc0p4k8 Prompt: 817f278d-adf0-450e-8e8b-e0a5ea868e64.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 817f278d-adf0-450e-8e8b-e0a5ea868e64.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_66.cpp
+++ CPP_66.cpp
@@ -1,3 +1,4 @@
+```
 #include <string>
 #include <cassert>
 
@@ -11,11 +12,4 @@ int digitSum(string s) {
         }
     }
     return sum;
-
-}
-
-int main() {
-    assert(digitSum("You arE Very Smart") == 15);
-    cout << "The digit sum is: " << digitSum("You arE Very Smart") << endl;
-    return 0;
 }
