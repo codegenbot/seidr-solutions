f9b53943ae15ca09593b37eb97a0ca67eb7a2f0a
beep boop <githubbot@vadim.me>
2024-07-07 05:49:35 +0200

    Solution 6 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/kf4a2rzo Prompt: w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const float*; _Tp = float; _Alloc = s td::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = f loat; _Alloc = std::allocator<float>; std::vector<_Tp, _Alloc>::allocator_type =  std::allocator<float>]’ 1356539a-b850-49ae-b47f-79790233c766.cpp:19:54:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -3,12 +3,12 @@
 #include <vector>
 #include <cmath>
 
-long long double_the_difference(std::vector<double> lst) {
+long long double_the_difference(std::vector<const float*> lst) {
     long long sum = 0;
-    for (double num : lst) {
-        if (num > 0 && std::floor(num) == num) { 
-            if (std::fmod(num, 2.0) != 0.0) { 
-                sum += std::pow(num, 2);
+    for (const float* num : lst) {
+        if (*num > 0 && std::floor(*num) == *num) { 
+            if (std::fmod(*num, 2.0) != 0.0) { 
+                sum += std::pow(*num, 2);
             }
         }
     }
@@ -16,7 +16,7 @@ long long double_the_difference(std::vector<double> lst) {
 }
 
 int main() {
-    std::vector<double> lst = {1.5, 3.0, 4.25, 6.0};
+    std::vector<const float*> lst = {(float)1.5, (float)3.0, (float)4.25, (float)6.0};
     long long odd_sum = double_the_difference(lst);
 
     return 0;
