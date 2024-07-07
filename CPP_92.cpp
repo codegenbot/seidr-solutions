47a6780ff1663d4fb9592445129b11258ef63455
beep boop <githubbot@vadim.me>
2024-07-07 07:15:12 +0200

    Solution 20 of CPP/92, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mtdz9fgk Prompt: 726d3608-afe5-477d-ad8c-1eb704b1ada9.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 726d3608-afe5-477d-ad8c-1eb704b1ada9.cpp:10: 726d3608-afe5-477d-ad8c-1eb704b1ada9.cpp: In function ‘int main()’: 726d3608-afe5-477d-ad8c-1eb704b1ada9.cpp:12:13: error: ‘any_int’ was not declare d in this scope    12 |     assert (any_int(3.01,4,7)==false);       |             ^~~~~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,8 +1,8 @@
-Here is the completed code:
-
 bool any_int(float a, float b, float c) {
+    bool any = false;
     if (a == int(a) && b == int(b) && c == int(c)) {
+        any = true;
         return (a == b + c || a == c + b || b == a + c || c == a + b);
     }
-    return false;
+    return any;
 }
