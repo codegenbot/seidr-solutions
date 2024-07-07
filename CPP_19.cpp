d42257bdd7c8093e24f0bb859511e88160b4776f
beep boop <githubbot@vadim.me>
2024-07-07 06:14:20 +0200

    Solution 5 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/c8w4r42z Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,29 +1,29 @@
-#include <algorithm>
+map<string,int> numMap = {{"zero",0}, {"one",1}, {"two",2}, {"three",3}, 
+                          {"four",4}, {"five",5}, {"six",6}, {"seven",7},
+                          {"eight",8}, {"nine",9}};
 
-string sort_numbers(string numbers) {
-    map<string, int> numberMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                                   {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                                   {"eight", 8}, {"nine", 9}};
-    vector<string> numberVector;
-    string num;
+vector<string> nums;
+string temp;
 
-    for (char &c : numbers) {
-        if (isdigit(c)) continue; // skip digits
-        num = c == ' ' ? "" : c + "";
-        while ((c = numbers[numbers.find(num) + 1]) != ' ') num += c;
-        numberVector.push_back(num);
+for (char c : numbers) {
+    if (c == ' ') {
+        nums.push_back(temp);
+        temp = "";
+    } else {
+        temp += c;
     }
-
-    sort(numberVector.begin(), numberVector.end());
-    return string(numberVector.size(), ' ') + join(numberVector, " ");
 }
+nums.push_back(temp);
 
-string join(vector<string> v, string sep) {
-    string result = "";
-    for (int i = 0; i < v.size(); i++) {
-        result += v[i];
-        if (i < v.size() - 1)
-            result += sep;
+sort(nums.begin(), nums.end());
+
+string result = "";
+for (const string& num : nums) {
+    for (auto it = numMap.begin(); it != numMap.end(); ++it) {
+        if (num == it->first) {
+            result += num + " ";
+            break;
+        }
     }
-    return result;
 }
+return result.substr(0, result.size() - 1);
