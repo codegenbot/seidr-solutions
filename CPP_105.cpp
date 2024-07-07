bf5b11f31dcdb9ec10011d4d1ce2427810742974
beep boop <githubbot@vadim.me>
2024-07-07 07:26:26 +0200

    Solution 5 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/kj8nj2v2 Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -13,35 +13,37 @@ vector<string> by_length(vector<int> arr) {
 
     vector<string> result;
     for (int i : temp) {
+        string str = "";
         switch (i) {
             case 1:
-                result.push_back("One");
+                str = "One";
                 break;
             case 2:
-                result.push_back("Two");
+                str = "Two";
                 break;
             case 3:
-                result.push_back("Three");
+                str = "Three";
                 break;
             case 4:
-                result.push_back("Four");
+                str = "Four";
                 break;
             case 5:
-                result.push_back("Five");
+                str = "Five";
                 break;
             case 6:
-                result.push_back("Six");
+                str = "Six";
                 break;
             case 7:
-                result.push_back("Seven");
+                str = "Seven";
                 break;
             case 8:
-                result.push_back("Eight");
+                str = "Eight";
                 break;
             case 9:
-                result.push_back("Nine");
+                str = "Nine";
                 break;
         }
+        result.push_back(str);
     }
 
     return result;
