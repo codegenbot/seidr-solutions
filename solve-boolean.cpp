d315af5aafa2668d8ae0cd3464aa17fff84d7f3e
beep boop <githubbot@vadim.me>
2024-07-07 06:47:29 +0200

    Solution 95 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/o9ngt94r Prompt: it must return True for input t|t&t&t|f&t&t|f|t|t, but it returns Enter a Boolean expression: True.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,56 +1,44 @@
-#include <string>
-#include <iostream>
-
-using namespace std;
-
-bool evaluateTerm(string term);
+```
 bool solveBoolean(string s) {
-    bool res = true;
+    bool res = false;
     string term;
     for (int i = 0; i < s.length(); i++) {
         char c = s[i];
         if (c == '|') {
             bool subRes = evaluateTerm(term);
-            if (!subRes) return false;
+            res = res || subRes;
             term = "";
         } 
-        else if (c == '&') {
-            if (term != "t") res = false;
-            term = "";
-        }
         else if (c == 't' || c == 'f') {
             if (c == 't') term += 't';
             else if (c == 'f') term += 'f';
         }
+        else if (c == '&') {
+            bool subRes = evaluateTerm(term);
+            res = res && subRes;
+            term = "";
+        }
     }
     bool subRes = evaluateTerm(term);
-    if (!subRes) return false;
-    return true;
+    res = res || subRes;
+
+    return res;
 }
 
 bool evaluateTerm(string term) {
-    if (term == "t") return true;
-    if (term == "f") return false;
-
+    bool res = true;
     for (int i = 0; i < term.length(); i++) {
         char c = term[i];
         if (c == '&') {
-            if (i % 2 == 0) return (term.substr(0, i) == "tt");
-            else return (term.substr(i + 1) == "ff");
+            if (term[i+1] == 't' && term[i+2] == 't') res = true;
+            else if ((term[i+1] == 't' && term[i+2] == 'f') || 
+                     (term[i+1] == 'f' && term[i+2] == 't') || 
+                     (term[i+1] == 'f' && term[i+2] == 'f')) res = false;
+            i++;
+        } 
+        else if (c == 't' || c == 'f') {
+            continue;
         }
     }
-    return true;
-}
-
-int main() {
-    string s;
-    cout << "Enter a Boolean expression: ";
-    getline(cin, s);
-    bool result = solveBoolean(s);
-    if (result) {
-        cout << "True" << endl;
-    } else {
-        cout << "False" << endl;
-    }
-    return 0;
+    return res;
 }
