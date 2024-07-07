37e7c23792ce91806d1d96a21e81808ceb0530b4
beep boop <githubbot@vadim.me>
2024-07-07 07:49:05 +0200

    Solution 17 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/j8xyzuq3 Prompt:    27 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 388dd42b-731a-45f3-9de4-474f873c070f.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 388dd42b-731a-45f3-9de4-474f873c070f.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 388dd42b-731a-45f3-9de4-474f873c070f.cpp:26: 388dd42b-731a-45f3-9de4-474f873c070f.cpp: In function ‘int main()’: 388dd42b-731a-45f3-9de4-474f873c070f.cpp:36:20: error: ‘minPath’ was not declare d in this scope    36 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 388dd42b-731a-45f3-9de4-474f873c070f.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,7 +1,35 @@
-bool issame(int a, int b) {
-    return a == b;
+```
+bool issame(int i1,int j1,int i2,int j2,vector<vector<int>>& grid){
+    int n = grid.size();
+    for(int x = min(i1,i2);x<=max(i1,i2);x++){
+        for(int y = min(j1,j2);y<=max(j1,j2);y++){
+            if(grid[x][y] != 0) return false;
+        }
+    }
+    return true;
 }
 
-int main() {
-    // Your code here
+vector<int> minPath(vector<vector<int>>& grid, int k){
+    int n = grid.size();
+    vector<vector<int>> dp(n, vector<int>(n));
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 && j==0) dp[i][j] = grid[i][j];
+            else{
+                int val = INT_MAX;
+                if(i>0) val = min(val, dp[i-1][j]);
+                if(j>0) val = min(val, dp[i][j-1]);
+                dp[i][j] = val + grid[i][j];
+            }
+        }
+    }
+    vector<int> res;
+    int i = n-1, j = n-1;
+    for(int l=0; l<k; l++){
+        res.push_back(grid[i][j]);
+        if(i>0 && dp[i-1][j] < dp[i][j-1]) i--;
+        else j--;
+    }
+    reverse(res.begin(), res.end());
+    return res;
 }
