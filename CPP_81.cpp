2ad29efbb96225d9a3468ac041aea145347a0621
beep boop <githubbot@vadim.me>
2024-07-07 05:12:42 +0200

    Solution 94 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vsibqb1o Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ c09d8def-1e58-4d4c-ad10-9e7669f66324.cpp:43:25:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -4,6 +4,7 @@
 #include <string>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a.empty() && b.empty()) return true;
     return (a.size() == b.size()) && std::all_of(a.begin(), a.end(), [b](const auto& x) { return x == *(b.begin()); });
 }
 
@@ -37,10 +38,10 @@ std::string letterGrade(float grade) {
 }
 
 std::vector<std::string> numericalLetterGrade(std::vector<float> grades) {
-    std::vector<std::string> result(std::allocator<std::string>());
+    std::vector<std::string> result;
     for (float grade : grades) {
         std::string letter = letterGrade(grade);
-        result.emplace_back(letter); 
+        result.push_back(letter);
     }
     return result;
 }
