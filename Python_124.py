a71b7aa6d6f7f72456959342bf31156508b59a94
beep boop <githubbot@vadim.me>
2024-07-07 02:05:06 +0200

    Solution 34 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/oztmjazh Prompt: Complete the following code given the task description and function signature.

--- Python_124.py
+++ Python_124.py
@@ -8,6 +8,6 @@ def valid_date(date):
         return False
     if (month in [4,6,9,11] and day > 30) or (month == 2 and day > 29):
         return False
-    if day < 1 or day > 31:
+    if int(day) < 1 or int(day) > 31:
         return False
     return True
