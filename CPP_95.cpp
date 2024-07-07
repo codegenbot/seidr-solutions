66921456edfac197b80f53ccab15273a1bbcc5c6
beep boop <githubbot@vadim.me>
2024-07-07 09:35:01 +0200

    Solution 30 of CPP/95, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/x78a7we2 Prompt: Complete the following code given the task description and function signature.

--- CPP_95.cpp
+++ CPP_95.cpp
@@ -5,14 +5,14 @@ bool check_dict_case(map<string, string> dict) {
     bool allUpper = true;
 
     for (auto& pair : dict) {
-        string key = pair.first;
-        if (!islower(key[0])) {
+        if (!islower(pair.first[0]) && !isupper(pair.first[0])) {
             allLower = false;
-            break;
-        }
-        if (!isupper(key[0])) {
             allUpper = false;
             break;
+        } else if (islower(pair.first[0])) {
+            allUpper = false;
+        } else {
+            allLower = false;
         }
     }
 
