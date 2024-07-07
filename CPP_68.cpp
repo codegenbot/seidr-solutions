8bea7a61d3d592060a04d2c217569b307c2ba292
beep boop <githubbot@vadim.me>
2024-07-07 07:05:04 +0200

    Solution 37 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r44jutti Prompt:       |                    ^~~ 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp:17: 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp: In function ‘int main()’: 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp:27:20: error: ‘pluck’ was not declared in this scope    27 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,21 +1,24 @@
 #include <vector>
 #include <algorithm>
 
+std::vector<int> pluck(std::vector<int> arr) {
+    std::vector<pair<int, int>> nodes;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] % 2 == 0) {
+            nodes.emplace_back(arr[i], i);
+        }
+    }
+    if (nodes.empty()) {
+        return {};
+    }
+    sort(nodes.begin(), nodes.end());
+    return {nodes[0].first, nodes[0].second};
+}
+
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
-std::vector<int> pluck(std::vector<int> arr) {
-    vector<int> result;
-    for (int i = 0; i < arr.size(); i++) {
-        if (arr[i] % 2 == 0) {
-            result.push_back(arr[i]);
-        }
-    }
-    sort(result.begin(), result.end());
-    return result;
-}
-
 int main() {
-    assert(issame(pluck({7, 9, 7, 1}), std::vector<int>{}));  
+    assert(issame(pluck({7, 9, 7, 1}), {}));  
 }
