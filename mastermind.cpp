4ec9093d525960da60e2708a540c40515090a6c1
beep boop <githubbot@vadim.me>
2024-07-07 10:56:35 +0200

    Solution 38 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/uzdkq0v8 Prompt: Program did not receive expected input

--- mastermind.cpp
+++ mastermind.cpp
@@ -6,17 +6,19 @@ int main() {
     string code, guess;
     int whitePegs = 0, blackPegs = 0;
 
-    cin >> code >> guess;
+    while (getline(cin, code)) {
+        getline(cin, guess);
 
-    for (int i = 0; i < code.length(); i++) {
-        if (code[i] == guess[i]) {
-            blackPegs++;
-        } else if (code.find(guess[i]) != string::npos && !whitePegs) { // added check to make sure the guessed character is not already used as a white peg
-            whitePegs++;
+        for (int i = 0; i < code.length(); i++) {
+            if (code[i] == guess[i]) {
+                blackPegs++;
+            } else if (code.find(guess[i]) != string::npos && !whitePegs) { // added check to make sure the guessed character is not already used as a white peg
+                whitePegs++;
+            }
         }
-    }
 
-    cout << whitePegs << " " << blackPegs << endl;
+        cout << whitePegs << " " << blackPegs << endl;
+    }
 
     return 0;
 }
