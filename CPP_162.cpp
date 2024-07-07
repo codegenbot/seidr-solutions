2f3b820fb3b9188b12181bfd9c7c1129e1df7720
beep boop <githubbot@vadim.me>
2024-07-07 11:17:52 +0200

    Solution 67 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mxo8yxx5 Prompt: Complete the following code given the task description and function signature.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,15 +1,32 @@
-#include <openssl/ssl.h>
+```c++
+#include <string>
+#include <sstream>
+
+using namespace std;
 
 string string_to_md5(string text) {
-    if (text.empty()) return "None";
-
-    unsigned char md5[MD5_DIGEST_LENGTH];
-    MD5((unsigned char*)text.c_str(), text.length(), md5);
-
-    stringstream ss;
-    for(int i = 0 ; i < MD5_DIGEST_LENGTH; i++) {
-        ss << hex << setfill('0') << fixed << setw(2) << (int)md5[i];
+    if (text.empty()) {
+        return "";
     }
 
-    return ss.str();
+    MD5_CTX md5;
+    unsigned char md5Buffer[16];
+    MD5_Init(&md5);
+
+    stringstream ss(text);
+    string str;
+    int len = 0;
+    while (getline(ss, str, '\0')) {
+        len += str.length();
+        const char* cstr = str.c_str();
+        MD5_Update(&md5, cstr, str.length());
+    }
+
+    MD5_Final(md5Buffer, &md5);
+
+    stringstream ssOut;
+    for (int i = 0; i < 16; ++i) {
+        ssOut << hex << setfill('0') << setw(2) << (int)md5Buffer[i];
+    }
+    return ssOut.str();
 }
