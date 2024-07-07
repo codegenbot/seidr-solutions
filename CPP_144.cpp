f88f9ccd78ab3e7294956bd682a75cd9aa2a68ef
beep boop <githubbot@vadim.me>
2024-07-07 05:31:37 +0200

    Solution 4 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/01ftl4ye Prompt: >&, signed char*)’   852 |     operator>>(basic_istream<char, _Traits>& __in, signed char* __s)       |     ^~~~~~~~ /usr/include/c++/11/istream:852:5: note:   template argument deduction/substitut ion failed: 187ae851-447d-4ab7-89a0-373279b7c9ea.cpp:14:31: note:   cannot convert ‘'/'’ (ty pe ‘char’) to type ‘signed char*’    14 |     if (!(s2 >> c) || !(s2 >> '/' >> d)) {       |                               ^~~ In file included from /usr/include/c++/11/sstream:38,                  from 187ae851-447d-4ab7-89a0-373279b7c9ea.cpp:2: /usr/include/c++/11/istream:1006:5: note: candidate: ‘template<class _Istream, c lass _Tp> _Istream&& std::operator>>(_Istream&&, _Tp&&)’  1006 |     operator>>(_Istream&& __is, _Tp&& __x)       |     ^~~~~~~~ /usr/include/c++/11/istream:1006:5: note:   template argument deduction/substitu tion failed: /usr/include/c++/11/istream: In substitution of ‘template<class _Istream, class _Tp> _Istream&& std::operator>>(_Istream&&, _Tp&&) [with _Istream = std::__cxx11 ::basic_stringstream<char>&; _Tp = char]’: 187ae851-447d-4ab7-89a0-373279b7c9ea.cpp:14:31:   required from here /usr/include/c++/11/istream:1006:5: error: no type named ‘type’ in ‘struct std:: enable_if<false, void>’

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -5,14 +5,19 @@ using namespace std;
 
 bool simplify(string x, string n) {
     int a = 0, b = 1, c = 0, d = 1;
+    string cstr;
     stringstream s1(x), s2(n);
     
     if (!(s1 >> a) || !(s1 >> '/' >> b)) { 
         return false; 
     }
     
-    if (!(s2 >> c) || !(s2 >> std::getline(s2, string()) >> d)) { 
+    if (!(s2 >> cstr) || (cstr != "/" && !((s2 >> c) || (s2 >> d))) ) { 
         return false; 
+    }
+    
+    if (s2.peek() == '/') {
+        s2 >> d;
     }
     
     return (a*d == b*c);
