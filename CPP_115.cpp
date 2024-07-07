e625909c409a4c4954344059f8720a8470c2a37d
beep boop <githubbot@vadim.me>
2024-07-07 10:22:20 +0200

    Solution 98 of CPP/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/peyybo0b Prompt: Complete the following code given the task description and function signature.

--- CPP_115.cpp
+++ CPP_115.cpp
@@ -1,21 +1,7 @@
 int max_fill(vector<vector<int>> grid, int capacity) {
-    int rows = grid.size();
-    int wells_per_row = grid[0].size();
-
-    int buckets_needed = 0;
-    for (int i = 0; i < rows; i++) {
-        int water_in_well = 0;
-        for (int j = 0; j < wells_per_row; j++) {
-            if (grid[i][j] == 1) {
-                water_in_well += 1;
-            }
-        }
-
-        while (water_in_well > capacity) {
-            water_in_well -= capacity;
-            buckets_needed++;
-        }
+    int total_water = 0;
+    for (vector<int> well : grid) {
+        total_water += accumulate(well.begin(), well.end(), 0);
     }
-
-    return buckets_needed;
+    return ceil((double)total_water / capacity);
 }
