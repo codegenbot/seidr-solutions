a08dfb72560d8783d687a0effcb94dc0a80e5de9
beep boop <githubbot@vadim.me>
2024-07-07 09:27:19 +0200

    Solution 57 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2iqkzfom Prompt: Complete the following code given the task description and function signature.

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,20 +1,20 @@
 vector<string> numerical_letter_grade(vector<float> grades){
-    vector<string> letter_grades;
-    for(float g:grades){
+    vector<string> result;
+    for(float g: grades){
         string letter;
-        if(g>=4.0)letter="A+";
-        else if(g>3.7)letter="A";
-        else if(g>3.3)letter="A-";
-        else if(g>3.0)letter="B+";
-        else if(g>2.7)letter="B";
-        else if(g>2.3)letter="B-";
-        else if(g>2.0)letter="C+";
-        else if(g>1.7)letter="C";
-        else if(g>1.3)letter="C-";
-        else if(g>1.0)letter="D+";
-        else if(g>0.7)letter="D";
-        else letter="E";
-        letter_grades.push_back(letter);
+        if(g >= 4.0) letter = "A+";
+        else if(g > 3.7) letter = "A";
+        else if(g > 3.3) letter = "A-";
+        else if(g > 3.0) letter = "B+";
+        else if(g > 2.7) letter = "B";
+        else if(g > 2.3) letter = "B-";
+        else if(g > 2.0) letter = "C+";
+        else if(g > 1.7) letter = "C";
+        else if(g > 1.3) letter = "C-";
+        else if(g > 1.0) letter = "D+";
+        else if(g > 0.7) letter = "D";
+        else letter = "E";
+        result.push_back(letter);
     }
-    return letter_grades;
+    return result;
 }
