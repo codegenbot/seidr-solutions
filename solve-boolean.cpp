17c0274e347ac0c119513f1d9dadac51497e054e
beep boop <githubbot@vadim.me>
2024-07-07 02:16:41 +0200

    Solution 60 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/1dh2pvgq Prompt: red in this scope    10 |             operators.push('|');       |             ^~~~~~~~~ 0767c3c9-f382-4dbe-8a1a-f4ab870a2fff.cpp:12:13: error: ‘operators’ was not decla red in this scope    12 |             operators.push('&');       |             ^~~~~~~~~ 0767c3c9-f382-4dbe-8a1a-f4ab870a2fff.cpp:14:13: error: ‘operators’ was not decla red in this scope    14 |             operators.push('^');       |             ^~~~~~~~~ 0767c3c9-f382-4dbe-8a1a-f4ab870a2fff.cpp:17:20: error: ‘operators’ was not decla red in this scope    17 |         } else if (operators.empty()) {       |                    ^~~~~~~~~ 0767c3c9-f382-4dbe-8a1a-f4ab870a2fff.cpp:20:29: error: expected initializer befo re ‘operators’    20 |             char operator = operators.top();       |                             ^~~~~~~~~ 0767c3c9-f382-4dbe-8a1a-f4ab870a2fff.cpp:21:29: error: expected type-specifier b efore ‘)’ token    21 |             switch (operator) {       |                             ^

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
 bool evaluate(const string &expression) {
-    stack<char> operators;
-    bool result = true;
-    for (int i = 0; i < expression.length(); i++) {
-        if (expression[i] == '|') {
-            operators.push('|');
-        } else if (expression[i] == '&') {
-            operators.push('&');
-        } else if (expression[i] == '^') {
-            operators.push('^');
-        } else if (operators.empty()) {
-            result = true;
-        } else {
-            char operator = operators.top();
-            switch (operator) {
-                case '|':
-                    result |= expression[i];
-                    break;
-                case '&':
-                    result &= expression[i];
-                    break;
-                case '^':
-                    result ^= expression[i];
-                    break;
-            }
-        }
-    }
-    return result && expression != "false";
+stack<char> operators; // initialize the stack
+bool result = true;
+for (int i = 0; i < expression.length(); i++) {
+if (expression[i] == '|') {
+operators.push('|');
+} else if (expression[i] == '&') {
+operators.push('&');
+} else if (expression[i] == '^') {
+operators.push('^');
+} else if (expression[i] == '~') {
+result = !result;
+} else if (operators.empty()) {
+result = true;
+} else {
+char operator = operators.top();
+switch (operator) {
+case '|':
+result |= expression[i];
+break;
+case '&':
+result &= expression[i];
+break;
+case '^':
+result ^= expression[i];
+break;
+}
+}
+}
+return result && expression != "false";
 }
