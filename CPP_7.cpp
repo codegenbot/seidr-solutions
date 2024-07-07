c30bfc912b58539b5aac94d607ef636bf2b72b21
beep boop <githubbot@vadim.me>
2024-07-07 06:14:46 +0200

    Solution 30 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zx302vzt Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b2be664e-189d-4f65-81b7-05963a39f736.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ b2be664e-189d-4f65-81b7-05963a39f736.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from b2be664e-189d-4f65-81b7-05963a39f736.cpp:11: b2be664e-189d-4f65-81b7-05963a39f736.cpp: In function ‘int main()’: b2be664e-189d-4f65-81b7-05963a39f736.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ b2be664e-189d-4f65-81b7-05963a39f736.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,21 +1,9 @@
-bool issame(vector<string> a, vector<vector<string>> b) {
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(!issame(a[i], b[i])) return false;
-    }
-    return true;
-}
-
-vector<vector<string>> filter_by_substring(vector<vector<string>> strings, string substring){
-    vector<vector<string>> result;
-    for(auto s : strings){
-        vector<string> temp;
-        for(auto str : s) {
-            if(str.find(substring) != string::npos)
-                temp.push_back(str);
+vector<string> filter_by_substring(vector<string> strings, string substring){
+    vector<string> result;
+    for(auto& s : strings) {
+        if(s.find(substring) != string::npos) {
+            result.push_back(s);
         }
-        if(!temp.empty()) 
-            result.push_back(temp);
     }
     return result;
 }
