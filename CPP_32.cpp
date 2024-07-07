5f87d0f51a8fdb5fffd5e76ea7be88e3c7de422b
beep boop <githubbot@vadim.me>
2024-07-07 03:03:02 +0200

    Solution 69 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/piua4g83 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = double]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = double; std::allocator_t raits<std::allocator<_CharT> >::pointer = double*; std::allocator_traits<std::al locator<_CharT> >::allocator_type = std::allocator<double>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = double; _Alloc = std::allocator<double>; std::_Vector_base<_Tp, _Alloc>:: pointer = double*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const double&}; _Tp = double; _Alloc = std::allocator<double>; std ::vector<_Tp, _Alloc>::iterator = std::vector<double>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = double; _Alloc = std::al locator<double>; std::vector<_Tp, _Alloc>::value_type = double]’ 1a62dd3c-cc88-4de0-be98-edec37e51879.cpp:17:25:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<double>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -5,42 +5,30 @@
 int main() {
     int ncoeff; 
     std::vector<double> coeffs;
-    
+
     std::cout << "Enter the number of coefficients: ";
     std::cin >> ncoeff;
     
-    coeffs.resize(ncoeff);
-    
     for(int i = 0; i < ncoeff; i++) {
-        std::cout << "Enter coefficient " << i+1 << ": ";
         double coeff;
+        std::cout << "Enter coefficient " << i+1 << ": ";
         std::cin >> coeff;
-        coeffs[i] = coeff;
+        coeffs.push_back(coeff);
     }
     
     double x, y;
     std::cout << "Enter the value of x: ";
     std::cin >> x;
-    
-    find_zero(coeffs, ncoeff, x, y);
-    
+
+    for(int i = 0; i < ncoeff; i++) {
+        double poly = 0.0;
+        for(int j = 0; j <= i; j++) {
+            poly += coeffs[j] * pow(x, j);
+        }
+        y = poly;
+    }
+
     std::cout << "The value of y is: " << y << std::endl;
-    
+
     return 0;
-}
-
-void find_zero(std::vector<double> coeffs, int ncoeff, double x, double& y) {
-    double poly = 0.0;
-    for(int i = 0; i < ncoeff; i++) {
-        poly += coeffs[i] * pow(x, i);
-    }
-    y = poly;
-}
-
-double poly(std::vector<double> coeffs, int ncoeff, double x) {
-    double poly = 0.0;
-    for(int i = 0; i < ncoeff; i++) {
-        poly += coeffs[i] * pow(x, i);
-    }
-    return poly;
 }
