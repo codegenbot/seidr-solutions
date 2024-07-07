2dfed94b7f8ef1daea95f3d5c7342d7ef4012c4b
beep boop <githubbot@vadim.me>
2024-07-07 02:28:29 +0200

    Solution 33 of CPP/0, ratio 0.8571428571428571 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vcnvivvc Prompt: 604893cd-8f5d-4f92-8f75-2d8d3fd6a65d.cpp: In function ‘int mainProblem()’: 604893cd-8f5d-4f92-8f75-2d8d3fd6a65d.cpp:13:5: error: ‘assert’ was not declared in this scope    13 |     assert(has_close_elements(b, 0.5) == false);       |     ^~~~~~ 604893cd-8f5d-4f92-8f75-2d8d3fd6a65d.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | using namespace std; 604893cd-8f5d-4f92-8f75-2d8d3fd6a65d.cpp:14:1: warning: no return statement in f unction returning non-void [-Wreturn-type]    14 | }       | ^

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,9 +1,27 @@
-bool has_close_elements(vector<float> numbers, float threshold){
-    for(int i = 0; i < numbers.size(); i++){
-        for(int j = i + 1; j < numbers.size(); j++){
-            if(abs(numbers[i] - numbers[j]) <= threshold)
-                return true;
+#include <iostream>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+bool has_close_elements(vector<double>, double);
+
+int mainProblem() {
+    vector<double> a = {1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
+    vector<double> b;
+    for (double num : a) {
+        b.push_back(num);
+    }
+    assert(has_close_elements(b, 0.5) == false);
+    return 0;
+}
+
+bool has_close_elements(vector<double> v, double tol) {
+    bool result = true;
+    for(int i=1; i<v.size(); i++) {
+        if(abs(v[i] - v[i-1]) < tol) {
+            result = false;
+            break;
         }
     }
-    return false;
+    return result;
 }
