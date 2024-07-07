85c491badc25b8378063e55ca31423785931239e
beep boop <githubbot@vadim.me>
2024-07-07 08:01:48 +0200

    Solution 97 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ghi1xlnr Prompt: 777e8e09-8fa5-4cb1-9081-142067557290.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 777e8e09-8fa5-4cb1-9081-142067557290.cpp:19: 777e8e09-8fa5-4cb1-9081-142067557290.cpp: In function ‘int main()’: 777e8e09-8fa5-4cb1-9081-142067557290.cpp:21:13: error: ‘rounded_avg’ was not dec lared in this scope    21 |     assert (rounded_avg(5, 5) == "101");       |             ^~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,19 +1,18 @@
+```c++
 int main(){
-    int n,m;
+    int n, m;
     cin >> n >> m;
-    if(n > m) cout << "-1";
-    else {
-        int sum = 0;
-        for(int i=n; i<=m; i++) sum += i;
-        double avg = (double)sum / (m - n + 1);
-        int rounded = lround(avg);
-        string res = "";
-        while(rounded > 0){
-            if(rounded & 1) res.push_back('1');
-            else res.push_back('0');
-            rounded >>= 1;
-        }
-        reverse(res.begin(), res.end());
-        cout << res;
+    if(n > m) return -1;
+    int sum = 0;
+    for(int i=n; i<=m; i++) sum += i;
+    double avg = (double)sum / (m - n + 1);
+    int rounded = lround(avg);
+    string res = "";
+    while(rounded > 0){
+        if(rounded & 1) res.push_back('1');
+        else res.push_back('0');
+        rounded >>= 1;
     }
+    reverse(res.begin(), res.end());
+    cout << res << endl;
 }
