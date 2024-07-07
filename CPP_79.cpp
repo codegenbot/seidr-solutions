ded827fb21e7d21975fe8f28dfc273b17c605a25
beep boop <githubbot@vadim.me>
2024-07-07 09:26:35 +0200

    Solution 74 of CPP/79, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/hrva9b7c Prompt: Complete the following code given the task description and function signature.

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -3,11 +3,11 @@ Here is the completed code:
 string decimal_to_binary(int decimal){
     string binary = "";
     while(decimal > 0){
-        if((decimal & 1) == 1)
-            binary = "1" + binary;
+        if((decimal % 2) == 1)
+            binary.insert(0,"1");
         else
-            binary = "0" + binary;
-        decimal >>= 1;
+            binary.insert(0,"0");
+        decimal /= 2;
     }
     return "db" + binary + "db";
 }
