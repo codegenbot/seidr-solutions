eddd600255d554218c528884bf00a1d7d91530b9
beep boop <githubbot@vadim.me>
2024-07-07 04:44:47 +0200

    Solution 3 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/32lqhmr7 Prompt: std::vector<int>, std::vector<int>)’: 8943c062-c7e5-4b00-a237-04e6fce20044.cpp:5:17: error: ‘max’ was not declared in this scope; did you mean ‘std::max’?     5 |     int start = max(interval1[0], interval2[0]);       |                 ^~~       |                 std::max In file included from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 8943c062-c7e5-4b00-a237-04e6fce20044.cpp:1: /usr/include/c++/11/bits/stl_algobase.h:300:5: note: ‘std::max’ declared here   300 |     max(const _Tp& __a, const _Tp& __b, _Compare __comp)       |     ^~~ 8943c062-c7e5-4b00-a237-04e6fce20044.cpp:6:15: error: ‘min’ was not declared in this scope; did you mean ‘std::min’?     6 |     int end = min(interval1[1], interval2[1]);       |               ^~~       |               std::min In file included from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 8943c062-c7e5-4b00-a237-04e6fce20044.cpp:1: /usr/include/c++/11/bits/stl_algobase.h:278:5: note: ‘std::min’ declared here   278 |     min(const _Tp& __a, const _Tp& __b, _Compare __comp)       |     ^~~

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -1,21 +1,2 @@
-#include <string>
-#include <vector>
-
-int intersection(std::vector<int> interval1, std::vector<int> interval2) {
-    int start = std::max(interval1[0], interval2[0]);
-    int end = std::min(interval1[1], interval2[1]);
-
-    if (start > end) return 0;
-
-    int length = end - start + 1;
-
-    bool isPrime = true;
-    for (int i = 2; i * i <= length; i++) {
-        if (length % i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
-
-    return isPrime ? 1 : 0;
-}
+int start = std::max(interval1[0], interval2[0]);
+int end = std::min(interval1[1], interval2[1]);
