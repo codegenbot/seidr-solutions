9241afca1f65f6d4f2ce4149ae537a2e787fbc22
beep boop <githubbot@vadim.me>
2024-07-07 06:42:55 +0200

    Solution 60 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/x3k5z0wa Prompt: 1eae4731-cb0c-4e1f-bda4-6352beac6f3d.cpp: In function ‘double vectorDistance(int )’: 1eae4731-cb0c-4e1f-bda4-6352beac6f3d.cpp:4:9: error: ‘cin’ was not declared in t his scope     4 |         cin >> x1[i] >> x2[i];       |         ^~~ 1eae4731-cb0c-4e1f-bda4-6352beac6f3d.cpp:8:16: error: ‘pow’ was not declared in this scope     8 |         sum += pow(x2[i] - x1[i], 2.0);       |                ^~~ 1eae4731-cb0c-4e1f-bda4-6352beac6f3d.cpp:10:12: error: ‘sqrt’ was not declared i n this scope    10 |     return sqrt(sum);       |            ^~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,15 +1,15 @@
-#include <cmath>
 #include <iostream>
-
-using namespace std;
+#include <cmath>
 
 double vectorDistance(int n) {
     double x1[n], x2[n];
-    for (int i = 0; i < n; i++) {
-        cin >> x1[i] >> x2[i];
+    std::cin >> x1[0]; // Read first element of x1
+    for (int i = 1; i < n; i++) {
+        std::cin >> x1[i] >> x2[i];
     }
     double sum = 0.0;
     for (int i = 0; i < n; i++) {
         sum += pow(x2[i] - x1[i], 2.0);
     }
-    return sqrt(sum);
+    return sqrt(sum); 
+}
