aa05fa977e459376f7dc92e27d6fff8155488ce9
beep boop <githubbot@vadim.me>
2024-07-07 09:09:50 +0200

    Solution 94 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jdhym3pn Prompt:    23 |         return toRoman(num % 10) + int_to_mini_roman(num / 10);       |                ^~~~~~~       |                roman 2eeaff79-e7b0-44b8-9416-9afb056ed579.cpp:31:16: error: ‘toRoman’ was not declare d in this scope; did you mean ‘roman’?    31 |         return toRoman(num);       |                ^~~~~~~       |                roman 2eeaff79-e7b0-44b8-9416-9afb056ed579.cpp: At global scope: 2eeaff79-e7b0-44b8-9416-9afb056ed579.cpp:75:5: error: redefinition of ‘int main( )’    75 | int main(){       |     ^~~~ 2eeaff79-e7b0-44b8-9416-9afb056ed579.cpp:66:5: note: ‘int main()’ previously def ined here    66 | int main() {       |     ^~~~ In file included from 2eeaff79-e7b0-44b8-9416-9afb056ed579.cpp:74: 2eeaff79-e7b0-44b8-9416-9afb056ed579.cpp: In function ‘int main()’: 2eeaff79-e7b0-44b8-9416-9afb056ed579.cpp:76:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    76 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,37 +1,6 @@
 #include <iostream>
 #include <string>
 
-std::string int_to_mini_roman(int num) {
-    std::string roman = "";
-    if (num >= 1000)
-        return "M" + int_to_mini_roman(num - 1000);
-    else if (num >= 900)
-        return "CM" + int_to_mini_roman(num - 900);
-    else if (num >= 500)
-        return "D" + int_to_mini_roman(num - 500);
-    else if (num >= 400)
-        return "CD" + int_to_mini_roman(num - 400);
-    else if (num >= 100)
-        return toRoman(num % 10) + int_to_mini_roman(num / 10);
-    else if (num >= 90)
-        return "XC" + int_to_mini_roman(num - 90);
-    else if (num >= 50)
-        return "L" + int_to_mini_roman(num - 50);
-    else if (num >= 40)
-        return "XL" + int_to_mini_roman(num - 40);
-    else if (num >= 10)
-        return toRoman(num % 10) + int_to_mini_roman(num / 10);
-    else if (num >= 9)
-        return "IX" + int_to_mini_roman(num - 9);
-    else if (num >= 5)
-        return "V" + int_to_mini_roman(num - 5);
-    else if (num >= 4)
-        return "IV" + int_to_mini_roman(num - 4);
-    else
-        return toRoman(num);
-
-}
-
 std::string toRoman(int num) {
     switch (num) {
         case 1:
@@ -63,7 +32,36 @@ std::string toRoman(int num) {
     }
 }
 
-int main_func() {
+std::string int_to_mini_roman(int num) {
+    if (num >= 1000)
+        return "M" + int_to_mini_roman(num - 1000);
+    else if (num >= 900)
+        return "CM" + int_to_mini_roman(num - 900);
+    else if (num >= 500)
+        return "D" + int_to_mini_roman(num - 500);
+    else if (num >= 400)
+        return "CD" + int_to_mini_roman(num - 400);
+    else if (num >= 100)
+        return toRoman(num % 100) + int_to_mini_roman(num / 100);
+    else if (num >= 90)
+        return "XC" + int_to_mini_roman(num - 90);
+    else if (num >= 50)
+        return "L" + int_to_mini_roman(num - 50);
+    else if (num >= 40)
+        return "XL" + int_to_mini_roman(num - 40);
+    else if (num >= 10)
+        return toRoman(num % 10) + int_to_mini_roman(num / 10);
+    else if (num >= 9)
+        return "IX" + int_to_mini_roman(num - 9);
+    else if (num >= 5)
+        return "V" + int_to_mini_roman(num - 5);
+    else if (num >= 4)
+        return "IV" + int_to_mini_roman(num - 4);
+    else
+        return toRoman(num);
+}
+
+int main() {
     int num; // input
     std::cout << "Enter a number: ";
     std::cin >> num;
