99f4002fb7b1284273eeb75df65974732754648f
beep boop <githubbot@vadim.me>
2024-07-07 10:35:49 +0200

    Solution 63 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qsjwobvp Prompt: Complete the following code given the task description and function signature.

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,18 +1,24 @@
+#include<stdio.h>
+#include<string>
+#include<algorithm>
+using namespace std;
+
 string get_closest_vowel(string word) {
     int n = word.size();
-    for (int i = n - 1; i > 0; --i) {
+    string result;
+    for (int i = 2; i < n; ++i) {
         if (!isalpha(word[i])) continue;
-        char c = tolower(word[i]);
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-            return word.substr(i);
+        if (word[i] == 'Y' || word[i] == 'y') continue;
+        if (isvowel(word[i])) {
+            if (result.empty()) result = tolower(word[i]);
+            else if (abs((int)(word[i] - 'a') - abs((int)(result[0] - 'a'))) > 1) return "";
+            break;
         }
     }
-    for (int i = 0; i < n - 1; ++i) {
-        if (!isalpha(word[i])) continue;
-        char c = tolower(word[i]);
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-            return word.substr(0, i);
-        }
-    }
-    return "";
+    return result;
+}
+
+bool isvowel(char c) {
+    c = tolower(c);
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
 }
