5fa2a6a73776d62f09381fe98826edc9f8571585
beep boop <githubbot@vadim.me>
2024-07-07 10:48:43 +0200

    Solution 83 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qsjwobvp Prompt: Complete the following code given the task description and function signature.

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,12 +1,11 @@
-#include <algorithm>
-#include <string>
-
-using namespace std;
-
 string get_closest_vowel(string word) {
-    for (int i = word.size() - 1; i > 0; i--) {
-        if ("aeiouAEIOU".find(word[i]) != string::npos) {
-            return word.substr(i);
+    for(int i = word.size() - 1; i > 0; --i) {
+        if("aeiouAEIOU".find(word[i]) != string::npos) {
+            int left = i;
+            while(left > 0 && "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".find(word[left-1]) == string::npos)
+                --left;
+            if(i - left < word.size() - left + 1)
+                return &word[i];
         }
     }
     return "";
