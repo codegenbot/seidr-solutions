c7d576cd739f62c6ea8be63fe227de5f88272de4
beep boop <githubbot@vadim.me>
2024-07-07 05:57:38 +0200

    Solution 98 of CPP/102, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/oet7vwoz Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: 9677556d-c922-4958-93e4-eaa2aa2f56dd.cpp:31:83:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_102.cpp
+++ CPP_102.cpp
@@ -7,25 +7,24 @@ int choose_num(int num1, int num2) {
 }
 
 int main() { 
-    std::string input;
+    std::string line;
     std::cout << "Enter two numbers (separated by a space): ";
-    if (!(std::getline(std::cin, input))) {  
+    if (!(std::getline(std::cin, line))) {  
         std::cout << "Invalid input. Please enter two integers." << std::endl;
         return 1; 
     }
 
-    size_t pos = input.find(' ');
+    size_t pos = line.find(' ');
     if(pos == std::string::npos) {
         std::cout << "Invalid input. Please enter two integers." << std::endl;
         return 1;
     }
     
-    std::string s1 = input.substr(0, pos);
-    input.erase(0, pos + 1); 
-    std::string s2 = input;
+    std::string s1 = line.substr(0, pos);
+    line.erase(0, pos + 1); 
 
     int num1 = std::stoi(s1);
-    int num2 = (s2.empty()) ? num1 : std::stoi(s2);
+    int num2 = std::stoi(line);
 
     if(num1 > num2)
         std::cout << "Greater number is " << std::to_string(choose_num(num1, num2)) << std::endl;
