7dcc831e7191b2368835fa9c6b35b69bf79b0bcb
beep boop <githubbot@vadim.me>
2024-07-07 06:46:50 +0200

    Solution 70 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/p10iv3ay Prompt:       |                    ^~~~~ 923e1e8d-179d-451e-aeb5-6f7352caa49e.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 923e1e8d-179d-451e-aeb5-6f7352caa49e.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 923e1e8d-179d-451e-aeb5-6f7352caa49e.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 923e1e8d-179d-451e-aeb5-6f7352caa49e.cpp:11: 923e1e8d-179d-451e-aeb5-6f7352caa49e.cpp: In function ‘int main()’: 923e1e8d-179d-451e-aeb5-6f7352caa49e.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 923e1e8d-179d-451e-aeb5-6f7352caa49e.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,25 +1,14 @@
 #include <vector>
-using namespace std;
+#include <algorithm>
 
-bool isSame(vector<float> a, vector<float> b) {
-    if(a.size() != b.size())
+bool issame(vector<float> a, vector<float> b) {
+    if (a.size() != b.size()) {
         return false;
-    for(int i = 0; i<a.size(); i++)
-        if(a[i] != b[i])
-            return false;
-    return true;
-}
-
-vector<float> get_positive(vector<float> l){
-    vector<float> result;
-    for(float i : l) {
-        if(i > 0)
-            result.push_back(i);
     }
-    return result;
-}
-
-int main() {
-    assert(isSame(get_positive({}), {}));
-    return 0;
+    for (int i = 0; i < a.size(); i++) {
+        if (std::abs(a[i] - b[i]) > 1e-9) {
+            return false;
+        }
+    }
+    return true;
 }
