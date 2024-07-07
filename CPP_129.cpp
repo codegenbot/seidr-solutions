cefd5c73ae0af7c8416c8d0f673af37fbb51a0ab
beep boop <githubbot@vadim.me>
2024-07-07 06:14:24 +0200

    Solution 60 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/w9gmztj5 Prompt:    85 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c33722e7-d4ed-4c99-9ebe-397ae9429a3f.cpp:93:5: error: redefinition of ‘int main( )’    93 | int main(){       |     ^~~~ c33722e7-d4ed-4c99-9ebe-397ae9429a3f.cpp:67:5: note: ‘int main()’ previously def ined here    67 | int main()       |     ^~~~ In file included from c33722e7-d4ed-4c99-9ebe-397ae9429a3f.cpp:84: c33722e7-d4ed-4c99-9ebe-397ae9429a3f.cpp: In function ‘int main()’: c33722e7-d4ed-4c99-9ebe-397ae9429a3f.cpp:94:20: error: ‘minPath’ was not declare d in this scope    94 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ c33722e7-d4ed-4c99-9ebe-397ae9429a3f.cpp:94:19: error: ‘issame’ cannot be used a s a function    94 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,6 +1,6 @@
 using namespace std;
 
-bool operator>(const pair<int, int> &a, const pair<int, int> &b) {
+bool operator>(const pair<int> &a, const pair<int> &b) {
     return a.first > b.first;
 }
 
@@ -11,6 +11,7 @@ struct pair_int {
 };
 
 vector<int> minPath(vector<vector<int>> grid, int k);
+
 bool issame(vector<int>, vector<int>);
 
 bool issame(vector<int> a, vector<int> b) {
@@ -19,53 +20,6 @@ bool issame(vector<int> a, vector<int> b) {
         if (a[i] != b[i]) return false;
     }
     return true;
-}
-
-bool operator>(const pair_int &a, const pair_int &b) {
-    return a.val > b.val;
-}
-
-vector<int> minPath(vector<vector<int>> grid, int k) {
-    int n = grid.size();
-    priority_queue<pair_int, vector<pair_int>, greater<pair_int>> pq; 
-    vector<int> res;
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            pair_int p = {grid[i][j], i, j};
-            pq.push(p);
-        }
-    }
-
-    while (!pq.empty()) {
-        int val = pq.top().val;
-        int pos_i = pq.top().i;
-        int pos_j = pq.top().j;
-        pq.pop();
-
-        res.push_back(val);
-
-        if (k > 0) {
-            --k;
-
-            // Check all neighbors
-            for (int i = -1; i <= 1; ++i) {
-                for (int j = -1; j <= 1; ++j) {
-                    int ni = pos_i + i;
-                    int nj = pos_j + j;
-
-                    if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
-                        pair_int p = {grid[ni][nj], ni, nj};
-                        pq.push(p);
-                    }
-                }
-            }
-        } else {
-            break;
-        }
-    }
-
-    return res;
 }
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
