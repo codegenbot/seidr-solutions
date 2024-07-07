beb9d1f316fa67aa60aa11ab3e7d965f6e156124
beep boop <githubbot@vadim.me>
2024-07-07 02:34:36 +0200

    Solution 29 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/uty50v51 Prompt:       |     ^~~~ In file included from e1712ade-89db-4296-9125-24078f25c854.cpp:28: e1712ade-89db-4296-9125-24078f25c854.cpp: In function ‘int main()’: e1712ade-89db-4296-9125-24078f25c854.cpp:38:36: error: invalid initialization of  reference of type ‘const int&’ from expression of type ‘<brace-enclosed initial izer list>’    38 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~ e1712ade-89db-4296-9125-24078f25c854.cpp:3:75: note: in passing argument 1 of ‘i nt filter_by_prefix(const int&, const int&)’     3 | d::string> filter_by_prefix(const std::vector<std::string>& strings, con st std::string& prefix) {       |                             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~ In file included from e1712ade-89db-4296-9125-24078f25c854.cpp:28: e1712ade-89db-4296-9125-24078f25c854.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,13 +1,6 @@
 int main() {
-    std::vector<std::string> strings;
-    strings.push_back("xxx");
-    strings.push_back("asd");
-    strings.push_back("xxy");
-    strings.push_back("john doe");
-    strings.push_back("xxxxAA");
-    strings.push_back("xxx");
-
+    std::vector<std::string> strings({"xxx", "asd", "xxy", "john doe", "xxxxAA", "xxx"});
     std::vector<std::string> result = filter_by_prefix(strings, "xxx");
-    assert(std::operator==(result, std::vector<std::string>{"xxx", "xxxAAA", "xxx"}));
+    assert(result == std::vector<std::string>{"xxx", "xxx", "xxx"}); 
     return 0;
 }
