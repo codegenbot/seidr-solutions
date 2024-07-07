0397328cf5a548f13b763f3933d8caa6856eaae9
beep boop <githubbot@vadim.me>
2024-07-07 05:26:48 +0200

    Solution 13 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lxy8b22d Prompt: ined here    12 | int main() {       |     ^~~~ In file included from c106e5f7-28bb-47cd-b24e-20dbc99eee91.cpp:19: c106e5f7-28bb-47cd-b24e-20dbc99eee91.cpp: In function ‘int main()’: c106e5f7-28bb-47cd-b24e-20dbc99eee91.cpp:21:13: error: ‘boost’ has not been decl ared    21 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ c106e5f7-28bb-47cd-b24e-20dbc99eee91.cpp:21:35: error: expected primary-expressi on before ‘>’ token    21 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ c106e5f7-28bb-47cd-b24e-20dbc99eee91.cpp:21:62: error: could not convert ‘1’ fro m ‘int’ to ‘std::string’ {aka ‘std::__cxx11::basic_string<char>’}    21 |    assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "None ");       |                                                             ^       |                                                             |       |                                                             int

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <string>
+#include <boost/any.hpp>
 
 using namespace std;
 
 string compare_one(string s1, string s2) {
-    double d1 = stod(s1);
-    double d2 = stod(s2);
-
-    if (d1 > d2) return s1;
-    else if (d1 == d2) return "None";
+    if (stod(s1) > stod(s2)) return s1;
+    else if (stod(s1) == stod(s2)) return "None";
     else return s2;
 }
 
 int main() {
     cout << compare_one("123.45", "6789") << endl; 
+    if(compare_one("1", "2") == "1") cout << "1 is greater than or equal to 2" << endl;
+    else cout << "2 is greater than 1" << endl;
     return 0;
 }
