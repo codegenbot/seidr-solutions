5fdc3bdacdd6492c82e7069a712a770e7c26d536
beep boop <githubbot@vadim.me>
2024-07-07 04:00:26 +0200

    Solution 3 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tfufwup9 Prompt: In file included from /usr/include/c++/11/string:39,                  from 2ed06d66-9202-40e5-a19e-c692d15bde7d.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 2ed06d66-9202-40e5-a19e-c692d15bde7d.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 2ed06d66-9202-40e5-a19e-c692d15bde7d.cpp:58:44: error: ‘b’ was not declared in t his scope    58 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 2ed06d66-9202-40e5-a19e-c692d15bde7d.cpp:57: 2ed06d66-9202-40e5-a19e-c692d15bde7d.cpp: In function ‘int main()’: 2ed06d66-9202-40e5-a19e-c692d15bde7d.cpp:67:20: error: ‘by_length’ was not decla red in this scope    67 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 2ed06d66-9202-40e5-a19e-c692d15bde7d.cpp:67:19: error: ‘issame’ cannot be used a s a function    67 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,31 +1,25 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <string>
 
-using namespace std;
-
-bool issame(int a, int b) {
-    if (a == b)
-        return true;
-    else
-        return false;
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
 }
 
-vector<string> by_length(vector<int> arr) {
-    vector<int> nums;
+std::vector<std::string> by_length(std::vector<int> arr) {
+    std::vector<std::string> nums;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            nums.push_back(num);
+            nums.push_back(std::to_string(num));
         }
     }
 
-    sort(nums.begin(), nums.end());
-    reverse(nums.begin(), nums.end());
+    std::sort(nums.begin(), nums.end());
+    std::reverse(nums.begin(), nums.end());
 
-    vector<string> result;
-    for (int num : nums) {
-        string str = "";
-        switch (num) {
+    std::vector<std::string> result;
+    for (std::string str : nums) {
+        switch (std::stoi(str)) {
             case 1:
                 str = "One";
                 break;
@@ -58,15 +52,4 @@ vector<string> by_length(vector<int> arr) {
     }
 
     return result;
-}
-
-int main() {
-    vector<int> arr = {1, 2, 3, 4, 5};
-    vector<string> result = by_length(arr);
-
-    for (string s : result) {
-        cout << s << endl;
-    }
-    
-    return 0;
 }
