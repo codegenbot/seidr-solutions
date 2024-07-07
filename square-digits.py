34a43f176b913e5ea85483accf3af401e4ebb634
beep boop <githubbot@vadim.me>
2024-07-07 04:32:51 +0200

    Solution 66 of square-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/b8diafrk Prompt: it must return 4649491636 for input 283746, but it returns .

--- square-digits.py
+++ square-digits.py
@@ -1,5 +1,2 @@
 def square_digits(n):
-    if str(n)[0] == '0':
-        return "0"
-    else:
-        return "".join([str(int(x) ** 2) for x in str(n)])
+    return "".join([str(int(x) ** 2) for x in map(int, str(n))])
