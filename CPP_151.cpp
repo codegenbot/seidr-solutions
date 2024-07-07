8cfbb53203eb92a2f06bb5d7da4e212c7b4a1b43
beep boop <githubbot@vadim.me>
2024-07-07 06:06:16 +0200

    Solution 20 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/kf4a2rzo Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = double]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_Tp1> >::allocate(std::allocator_traits<std::a llocator<_Tp1> >::allocator_type&, int) [with _Tp = double; std::allocator_trait s<std::allocator<_Tp1> >::pointer = double*; std::allocator_traits<std::allocato r<_Tp1> >::allocator_type = std::allocator<double>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = double; _Alloc = std::allocator<double>; std::_Vector_base<_Tp, _Alloc>:: pointer = double*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const double&}; _Tp = double; _Alloc = std::allocator<double>; std ::vector<_Tp, _Alloc>::iterator = std::vector<double>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = double; _Alloc = std::al locator<double>; std::vector<_Tp, _Alloc>::value_type = double]’ 5fb5c2cb-bb10-4381-84f4-eaa2e08678be.cpp:43:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<double>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -6,15 +6,15 @@
 #include <iostream>
 
 double double_the_difference(std::vector<double> lst) {
-    double odd_sum = 0;
-    for(double y : lst) {
-        if (y > 0 && std::floor(y) == y) { 
-            if (std::fmod(y, 2.0) != 0.0) { 
-                odd_sum += std::pow(y, 2);
+    return std::sqrt(std::accumulate(lst.begin(), lst.end(), 0.0,
+        [y](double s, double y) {
+            if (y > 0 && std::floor(y) == y) { 
+                if (std::fmod(y, 2.0) != 0.0) { 
+                    return s + std::pow(y, 2);
+                }
             }
-        }
-    }
-    return odd_sum;
+            return s;
+        }));
 }
 
 void calculateOddSums(std::vector<double> lst) {
@@ -26,7 +26,10 @@ void calculateOddSums(std::vector<double> lst) {
             }
         }
     }
-    double average = total_sum / std::count_if(lst.begin(), lst.end(), [](double x){ return x > 0 && std::floor(x) == x && std::fmod(x, 2.0) != 0.0; });
+    double average = (total_sum / std::count_if(lst.begin(), lst.end(),
+        [](double x) { 
+            return x > 0 && std::floor(x) == x && std::fmod(x, 2.0) != 0.0; 
+        }));
     std::cout << "Average of odd numbers: " << average << std::endl;
 }
 
@@ -35,14 +38,17 @@ int main() {
     std::cout << "Enter the number of elements: ";
     std::cin >> n;
     
+    if(n < 0) {
+        std::cout << "Please enter a non-negative number." << std::endl;
+        return -1;
+    }
+    
     std::vector<double> lst;
     for(int i = 0; i < n; i++) {
         double num;
         std::cout << "Enter element " << i+1 << ": ";
         std::cin >> num;
-        if (num > 0) {
-            lst.push_back(num);
-        }
+        lst.push_back(num);
     }
     
     double odd_sum = double_the_difference(lst);
