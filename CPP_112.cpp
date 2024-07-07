0eb81c4aa8a8c3d7dd620c5559fec29b716d0dd4
beep boop <githubbot@vadim.me>
2024-07-07 10:28:19 +0200

    Solution 71 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dbjtk1yu Prompt: Complete the following code given the task description and function signature.

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -4,20 +4,21 @@ vector<string> reverse_delete(string s, string c) {
     vector<string> result;
     string temp = "";
     for (int i = 0; i < s.length(); i++) {
-        if (!find(c.begin(), c.end(), s[i]) != c.end()) {
+        if (find(c.begin(), c.end(), s[i]) == c.end()) {
             temp += s[i];
         }
     }
-    bool check = true;
-    int left = 0, right = temp.length() - 1;
-    while (left < right) {
-        if (temp[left] != temp[right]) {
-            check = false;
+    bool isPalindrome = true;
+    int start = 0, end = temp.length() - 1;
+    while (start < end) {
+        if (temp[start] != temp[end]) {
+            isPalindrome = false;
             break;
         }
-        left++;
-        right--;
+        start++;
+        end--;
     }
     result.push_back(temp);
-    result.push_back((check ? "True" : "False"));
+    result.push_back((isPalindrome) ? "True" : "False");
     return result;
+}
