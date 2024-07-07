74c1020893756a80d3aca695a5a1013afaf19265
beep boop <githubbot@vadim.me>
2024-07-07 06:33:19 +0200

    Solution 71 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/w9gmztj5 Prompt: ()’   106 | int main(){       |     ^~~~ 7bd57603-03c4-4272-8ca8-57781b36759f.cpp:80:5: note: ‘int main()’ previously def ined here    80 | int main()       |     ^~~~ In file included from 7bd57603-03c4-4272-8ca8-57781b36759f.cpp:97: 7bd57603-03c4-4272-8ca8-57781b36759f.cpp: In function ‘int main()’: 7bd57603-03c4-4272-8ca8-57781b36759f.cpp:107:19: error: call of overloaded ‘issa me(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous   107 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~ 7bd57603-03c4-4272-8ca8-57781b36759f.cpp:23:6: note: candidate: ‘bool issame(con st std::vector<int>&, const std::vector<int>&)’    23 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ 7bd57603-03c4-4272-8ca8-57781b36759f.cpp:98:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    98 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <vector>
 #include <queue>
 
@@ -30,49 +31,6 @@ bool issame(const vector<int>& a, const vector<int>& b) {
 
 bool operator>(const pair_int &a, const pair_int &b) {
     return a.val > b.val;
-}
-
-vector<int> minPath(vector<vector<int>> grid, int k) {
-    int n = grid.size();
-    priority_queue<pair_int, vector<pair_int>, greater<pair_int>> pq; 
-    vector<int> res;
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            pair_int p = {grid[i][j], i, j};
-            pq.push(p);
-        }
-    }
-
-    while (!pq.empty()) {
-        int val = pq.top().val;
-        int pos_i = pq.top().i;
-        int pos_j = pq.top().j;
-        pq.pop();
-
-        res.push_back(val);
-
-        if (k > 0) {
-            --k;
-
-            // Check all neighbors
-            for (int i = -1; i <= 1; ++i) {
-                for (int j = -1; j <= 1; ++j) {
-                    int ni = pos_i + i;
-                    int nj = pos_j + j;
-
-                    if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
-                        pair_int p = {grid[ni][nj], ni, nj};
-                        pq.push(p);
-                    }
-                }
-            }
-        } else {
-            break;
-        }
-    }
-
-    return res;
 }
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
