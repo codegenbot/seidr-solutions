7a66ca08418c40cab05e4c168c1aba2daf8e58db
beep boop <githubbot@vadim.me>
2024-07-07 07:59:43 +0200

    Solution 67 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/e1d4oson Prompt: 579febf3-e881-4051-a627-0496a7309021.cpp:1:14: error: ‘vector’ was not declared in this scope     1 | int solution(vector<int> lst) {       |              ^~~~~~ 579febf3-e881-4051-a627-0496a7309021.cpp:1:21: error: expected primary-expressio n before ‘int’     1 | int solution(vector<int> lst) {       |                     ^~~ In file included from 579febf3-e881-4051-a627-0496a7309021.cpp:11: 579febf3-e881-4051-a627-0496a7309021.cpp: In function ‘int main()’: 579febf3-e881-4051-a627-0496a7309021.cpp:13:13: error: ‘solutions’ was not decla red in this scope; did you mean ‘solution’?    13 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <vector>
 
 int solution(vector<int> lst) {
@@ -12,5 +13,4 @@ int solution(vector<int> lst) {
 
 int main() {
     assert (solution({3, 13, 2, 9}) == 3);
-    // your code here
 }
