79b0e327811f4db370e97f4b0d0d5da419a15520
beep boop <githubbot@vadim.me>
2024-07-07 05:50:56 +0200

    Solution 84 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/wws4u5em Prompt:       |                           ^~~~~~ 9b15e807-728b-4316-be62-6aaaee5040b3.cpp:42:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 9b15e807-728b-4316-be62-6aaaee5040b3.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 9b15e807-728b-4316-be62-6aaaee5040b3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9b15e807-728b-4316-be62-6aaaee5040b3.cpp:42:34: error: expected primary-expressi on before ‘int’    42 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9b15e807-728b-4316-be62-6aaaee5040b3.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ 9b15e807-728b-4316-be62-6aaaee5040b3.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,6 +1,14 @@
 #include <vector>
 #include <iostream>
 
+bool issame(std::vector<int> v1, std::vector<int> v2) {
+    if(v1.size() != v2.size()) return false;
+    for(int i=0; i<v1.size(); i++) {
+        if(v1[i] != v2[i]) return false;
+    }
+    return true;
+}
+
 std::vector<int> get_odd_collatz(int n) {
     std::vector<int> sequence;
     while (n > 1) {
@@ -10,14 +18,6 @@ std::vector<int> get_odd_collatz(int n) {
         n = (n + 1) / 2; 
     }
     return sequence;
-}
-
-bool same(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) return false;
-    for(int i=0; i<a.size(); i++) {
-        if(a[i] != b[i]) return false;
-    }
-    return true;
 }
 
 int main() {
@@ -32,7 +32,7 @@ int main() {
     for (int i : output) {
         std::cout << i << " ";
     }
-    if (!same({1}, {1})) {
+    if (!issame({1}, {output})) { 
         std::cout << "Assertion failed." << std::endl;
     }
     return 0;
