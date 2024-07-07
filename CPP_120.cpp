1853de12e875db42b9852b6b7233f1a8887954de
beep boop <githubbot@vadim.me>
2024-07-07 07:55:22 +0200

    Solution 54 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/x19lori4 Prompt:       |                    ^~~ 2aa92148-04f9-4158-a976-946b4de45a91.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2aa92148-04f9-4158-a976-946b4de45a91.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2aa92148-04f9-4158-a976-946b4de45a91.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2aa92148-04f9-4158-a976-946b4de45a91.cpp:16: 2aa92148-04f9-4158-a976-946b4de45a91.cpp: In function ‘int main()’: 2aa92148-04f9-4158-a976-946b4de45a91.cpp:26:20: error: ‘maximum’ was not declare d in this scope    26 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 2aa92148-04f9-4158-a976-946b4de45a91.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,48 +1,24 @@
-#include <iostream>
-#include <vector>
+```cpp
 #include <queue>
+#include <vector>
 
-using namespace std;
-
-bool issame(vector<int> a,vector<int>b){
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]) return false;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
 
-vector<int> maximum(vector<int> arr, int k) {
-    priority_queue<int> pq;
+std::vector<int> maximum(std::vector<int> arr, int k) {
+    std::priority_queue<int> pq;
     for (int i : arr) {
         pq.push(i);
         if (pq.size() > k)
             pq.pop();
     }
-    vector<int> result(k);
+    std::vector<int> result(k);
     for (int i = k - 1; i >= 0; --i)
         result[i] = pq.top(), pq.pop();
     return result;
-}
-
-int main() {
-    int n, k;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-    vector<int> arr(n);
-    cout << "Enter the array elements: ";
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
-    cout << "Enter k: ";
-    cin >> k;
-    vector<int> res = maximum(arr, k);
-    if(res.size() == k) {
-        cout << "The maximum of every k elements is: ";
-        for (int i : res)
-            cout << i << ' ';
-        cout << endl;
-    } else
-        cout << "Array size less than or equal to k." << endl;
-    return 0;
 }
