88613d6be8539e4ac5d90b027741844c70b55407
beep boop <githubbot@vadim.me>
2024-07-07 03:39:25 +0200

    Solution 12 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vsibqb1o Prompt:       |           ^~~~~~ 216688b8-b9f3-4ad2-b137-eb14d8d4a7cd.cpp:54:44: error: ‘b’ was not declared in t his scope    54 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 216688b8-b9f3-4ad2-b137-eb14d8d4a7cd.cpp:62:5: error: redefinition of ‘int main( )’    62 | int main(){       |     ^~~~ 216688b8-b9f3-4ad2-b137-eb14d8d4a7cd.cpp:47:5: note: ‘int main()’ previously def ined here    47 | int main() {       |     ^~~~ In file included from 216688b8-b9f3-4ad2-b137-eb14d8d4a7cd.cpp:53: 216688b8-b9f3-4ad2-b137-eb14d8d4a7cd.cpp: In function ‘int main()’: 216688b8-b9f3-4ad2-b137-eb14d8d4a7cd.cpp:63:20: error: ‘numerical_letter_grade’ was not declared in this scope    63 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 216688b8-b9f3-4ad2-b137-eb14d8d4a7cd.cpp:63:19: error: ‘issame’ cannot be used a s a function    63 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -44,7 +44,7 @@ vector<string> numerical_letter_grade(vector<float> grades) {
 }
 
 int main() {
-    vector<float> fgrades = {0, 0.7}; 
-    assert(issame(numerical_letter_grade(fgrades), {"E", "D-"}));
+    vector<string> fgrades = {"0", "0.7"}; 
+    assert(issame(numerical_letter_grade({stof(fgrades[0]), stof(fgrades[1])}), {"E", "D-"}));
     return 0;
 }
