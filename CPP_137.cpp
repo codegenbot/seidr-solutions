73c74c611e9fe5f09d10a55c652031730bba154e
beep boop <githubbot@vadim.me>
2024-07-07 11:10:24 +0200

    Solution 96 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6mhsqfmo Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,57 +1,16 @@
+```cpp
 #include <string>
 #include <boost/any.hpp>
 
-using namespace std;
+using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return max(a, b);
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        return max(a, b);
-    }
-    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return boost::any(b);
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        return max(a, b);
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if (((string)a) > ((string)b))
-            return a;
-        else
-            return b;
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        if (stoi((string)b) > (int)a)
-            return boost::any(b);
-        else if (stoi((string)b) < (int)a)
-            return a;
-        else
-            return boost::any("None");
-    }
-    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        if (stod((string)b) > (double)a)
-            return boost::any(b);
-        else if (stod((string)b) < (double)a)
-            return a;
-        else
-            return boost::any("None");
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        if ((int)b > (int)a)
-            return boost::any(b);
-        else if ((int)b < (int)a)
-            return a;
-        else
-            return boost::any("None");
-    }
-    else {
-        if ((double)a > (double)b)
-            return a;
-        else if ((double)a < (double)b)
-            return b;
-        else
-            return boost::any("None");
-    }
+    if (is_none(a) || is_none(b))
+        return "None";
+
+    any_cast<double>(a) > any_cast<double>(b)
+        ? return a
+        : any_cast<std::string>(a) > any_cast<std::string>(b)
+            ? return a
+            : b;
 }
