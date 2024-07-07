e83b7999045802472e5e43353e0a29f40dc3d0ea
beep boop <githubbot@vadim.me>
2024-07-07 07:55:02 +0200

    Solution 47 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/exzsclts Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2c481e33-5806-4eeb-aa84-4e6fea4e9 df4.py", line 3     return str(sum((i * 2 if i % 2 != 0 else i) for i in (j if j > 9 then j - 9 else j for j in ((i * 2) for i in card)))) % 10                                                           ^^^^^^^^^^ SyntaxError: expected 'else' after 'if' expression

--- luhn.py
+++ luhn.py
@@ -1,3 +1,3 @@
 def luhn(card):
     card = list(map(int, card.replace(" ", "")))
-    return str(sum((i * 2 if i % 2 != 0 else i) for i in (j if j > 9 else j-9 for j in ((i * 2) for i in card)))) % 10
+    return str(sum(i if i % 2 == 0 else 2 * i if 9 < 2 * i else 2 * i - 9 for i in card)) % 10
