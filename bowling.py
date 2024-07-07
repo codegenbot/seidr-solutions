7219aa686176e08f75f4e1204455379893b43b01
beep boop <githubbot@vadim.me>
2024-07-08 00:05:44 +0200

    Solution 51 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/n1u5dqj1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dad21802-2595-4a50-923c-f8e072227 e29.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -2,31 +2,29 @@
 def bowling_score(frames):
     score = 0
     frame = 0
-    i = 0
-    while i < len(frames):
+    for i in range(len(frames)):
         if frames[i].isdigit():
             if len(frames[i]) > 1:
-                if int(frames[i:i+2]) == 10: 
-                    score += 10 + (10 - int(frames[i-1:i])) if frame < 9 else 10
+                if int(frames[i]) == 10: 
+                    score += 10 + (10 - int(frames[i-1:i+1])) if frame < 9 else 10
                     frame += 1
-                    i += 1
                 elif int(frames[i]) < 10:
                     score += int(frames[i])
                     frame += 1
-                    i += 1
             else:
                 score += int(frames[i])
-                i += 1
         else:
             if frames[i] == 'X':
-                score += 10 + (10 - int(frames[i-2:i])) if frame < 9 else 10
+                if frame < 9:
+                    score += 10 + (10 - int(frames[i-1])) + 10
+                else:
+                    score += 10
                 frame += 1
-                i += 1
             elif frames[i] == '/':
-                a, b = map(int, frames[i+1:].split('/'))
-                score += a + b
+                a, b = map(int, frames[i+1].split('/'))
+                if int(frames[i-1]) + a >= 10:
+                    score += 10 + b
+                else:
+                    score += a + b
                 frame += 1
-                i += len(frames[i+1:].split('/')) + 1
-            else:
-                continue
     return score
