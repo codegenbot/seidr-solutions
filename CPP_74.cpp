9111bba7e90cb7b0bd8db630839dac1475acfff5
beep boop <githubbot@vadim.me>
2024-07-07 03:09:37 +0200

    Solution 2 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1zjwyqt3 Prompt:       |                              ^~~~~~ 040cb2b4-0977-4339-97aa-44246dbad214.cpp:16:37: error: ‘string’ was not declared  in this scope    16 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 040cb2b4-0977-4339-97aa-44246dbad214.cpp:16:44: error: ‘b’ was not declared in t his scope    16 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 040cb2b4-0977-4339-97aa-44246dbad214.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 040cb2b4-0977-4339-97aa-44246dbad214.cpp:15: 040cb2b4-0977-4339-97aa-44246dbad214.cpp: In function ‘int main()’: 040cb2b4-0977-4339-97aa-44246dbad214.cpp:25:20: error: ‘total_match’ was not dec lared in this scope    25 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 040cb2b4-0977-4339-97aa-44246dbad214.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,13 +1,10 @@
 ```cpp
+#include <iostream>
 #include <vector>
 #include <string>
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
+    return (a == b);
 }
 
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
@@ -26,5 +23,5 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
 
 int main() {
     assert(issame(total_match({"this"}, {}), {}));
-    // Your other code here
+    return 0;
 }
