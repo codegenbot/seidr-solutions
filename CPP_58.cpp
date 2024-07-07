daa283a8b94ddaef6f237d0f2f68f12409a497e6
beep boop <githubbot@vadim.me>
2024-07-07 03:01:16 +0200

    Solution 16 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/io7n5spc Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 7af904e4-33de-4ede-8aad-815745c84177.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <algorithm>
 #include <set>
@@ -9,14 +8,14 @@ vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> set1(l1.begin(), l1.end());
     set<int> set2(l2.begin(), l2.end());
     vector<int> intersection;
-    intersection.insert(intersection.end(), set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(),
-        back_inserter(intersection)));
+    copy(set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end()),
+        set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end()), back_inserter(intersection));
     return intersection;
 }
 
 int main() {
-    vector<int> l1 = vector<int>({1, 2, 3, 4});
-    vector<int> l2 = vector<int>({3, 4, 5, 6});
+    vector<int> l1 = {1,2,3,4};
+    vector<int> l2 = {3,4,5,6};
     vector<int> result = common(l1, l2);
     for(int i : result) {
         cout << i << " ";
