0dd39cfbc01869fa6c612cb6a00e50fd814fb697
beep boop <githubbot@vadim.me>
2024-07-07 06:47:25 +0200

    Solution 53 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qqmyywae Prompt: 3367bc1b-6552-4907-ad52-bbd454f134d5.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<float>::iterator it = max(l.begin(), l.end());       | ^~~~~~ 3367bc1b-6552-4907-ad52-bbd454f134d5.cpp:2:1: error: expected unqualified-id bef ore ‘return’     2 | return *it;       | ^~~~~~ In file included from 3367bc1b-6552-4907-ad52-bbd454f134d5.cpp:4: 3367bc1b-6552-4907-ad52-bbd454f134d5.cpp: In function ‘int main()’: 3367bc1b-6552-4907-ad52-bbd454f134d5.cpp:6:17: error: ‘max_element’ was not decl ared in this scope     6 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ 3367bc1b-6552-4907-ad52-bbd454f134d5.cpp:6:13: error: ‘abs’ was not declared in this scope     6 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -4,5 +4,5 @@
 int main() {
     std::vector<float> l = {5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10};
     auto it = std::max_element(l.begin(), l.end());
-    return abs(*it);
+    return static_cast<int>(*it);
 }
