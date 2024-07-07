74f1df74701098029a26d7a1f82a7786cfce7e47
beep boop <githubbot@vadim.me>
2024-07-07 05:28:01 +0200

    Solution 79 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vzoz9gns Prompt:    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 01f31a72-97f1-46d3-81b8-027ee2ad3b04.cpp:24:10: error: ‘int main()’ redeclared a s different kind of entity    24 | int main(){       |          ^ 01f31a72-97f1-46d3-81b8-027ee2ad3b04.cpp:1:5: note: previous declaration ‘int ma in’     1 | int main       |     ^~~~ In file included from 01f31a72-97f1-46d3-81b8-027ee2ad3b04.cpp:15: 01f31a72-97f1-46d3-81b8-027ee2ad3b04.cpp: In function ‘int main()’: 01f31a72-97f1-46d3-81b8-027ee2ad3b04.cpp:25:20: error: ‘count_up_to’ was not dec lared in this scope    25 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 01f31a72-97f1-46d3-81b8-027ee2ad3b04.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,11 +1,44 @@
-int main {
+```cpp
+#include <vector>
+#include <algorithm>
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::vector<int> count_up_to(int n) {
+    std::vector<int> primes;
+    for (int i = 2; i <= n; i++) {
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            primes.push_back(i);
+        }
+    }
+    return primes;
+}
+
+int main() {
     int n;
     std::cout << "Enter a number: ";
     std::cin >> n;
-    std::vector<int> primes = countUpTo(n);
+    std::vector<int> primes = count_up_to(n);
     for (int prime : primes) {
         if (prime != primes[0]) {
             std::cout << "Prime number: " << prime << std::endl;
         }
     }
-    return 0;
+}
