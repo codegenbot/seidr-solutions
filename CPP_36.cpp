063126fae5f75cf9301997f4b997dd6a0e2fd05c
beep boop <githubbot@vadim.me>
2024-07-07 06:35:43 +0200

    Solution 1 of CPP/36, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ixec7tqn Prompt: Complete the following code given the task description and function signature.

--- CPP_36.cpp
+++ CPP_36.cpp
@@ -1,19 +1,12 @@
+#include <stdio.h>
+using namespace std;
+
 int fizz_buzz(int n) {
     int count = 0;
     for (int i = 1; i < n; i++) {
-        if ((i % 11 == 0 || i % 13 == 0) && count_7(i)) {
+        if ((i % 11 == 0 || i % 13 == 0) && (i / 10 % 10 == 7 || i % 10 == 7)) {
             count++;
         }
     }
     return count;
-}
-
-bool count_7(int num) {
-    while (num > 0) {
-        int digit = num % 10;
-        if (digit == 7)
-            return true;
-        num /= 10;
-    }
-    return false;
 }
