87b33090504eb8b3589b7fc57529823e17a87b56
beep boop <githubbot@vadim.me>
2024-07-07 15:29:38 +0200

    Solution 90 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/md5rp5pb Prompt: Program did not receive expected input

--- twitter.cpp
+++ twitter.cpp
@@ -25,8 +25,21 @@ string validateTweet(string tweet) {
 int main() {
     string tweet;
     cout << "Enter a tweet: ";
-    cin.ignore(); 
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
     getline(cin, tweet);
+    
+    size_t firstNotSpace = tweet.find_first_not_of(' ');
+    if (firstNotSpace != string::npos) {
+        tweet.erase(0, firstNotSpace);
+    } else {
+        tweet = "";
+    }
+    
+    size_t lastNotSpace = tweet.find_last_not_of(" \t\r\n");
+    if (lastNotSpace != string::npos) {
+        tweet.erase(lastNotSpace + 1);
+    }
+    
     cout << validateTweet(tweet) << endl;
     return 0;
 }
