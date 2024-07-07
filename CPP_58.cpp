53f9d92638e22ac6d506a322d9637703e1e68444
beep boop <githubbot@vadim.me>
2024-07-07 04:25:20 +0200

    Solution 97 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/io7n5spc Prompt: ef4e35fc-03c8-4632-84c5-1f4fbbaebbbb.cpp:82:5: error: redefinition of ‘int main( )’    82 | int main(){       |     ^~~~ ef4e35fc-03c8-4632-84c5-1f4fbbaebbbb.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from ef4e35fc-03c8-4632-84c5-1f4fbbaebbbb.cpp:73: ef4e35fc-03c8-4632-84c5-1f4fbbaebbbb.cpp: In function ‘int main()’: ef4e35fc-03c8-4632-84c5-1f4fbbaebbbb.cpp:83:20: error: ‘common’ was not declared  in this scope; did you mean ‘std::common’?    83 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ ef4e35fc-03c8-4632-84c5-1f4fbbaebbbb.cpp:17:22: note: ‘std::common’ declared her e    17 |     std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {       |                      ^~~~~~ In file included from ef4e35fc-03c8-4632-84c5-1f4fbbaebbbb.cpp:73: ef4e35fc-03c8-4632-84c5-1f4fbbaebbbb.cpp:83:19: error: ‘issame’ cannot be used a s a function    83 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,4 +1,3 @@
-```
 #include <vector>
 #include <set>
 #include <algorithm>
@@ -29,44 +28,40 @@ namespace std {
     }
 }
 
-int main() {
-    std::vector<int> l1;
-    int n1;
-    std::cout << "Enter the number of elements in list 1: ";
-    std::cin >> n1;
-    for(int i = 0; i < n1; i++) {
-        int x;
-        std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> x;
-        l1.push_back(x);
-    }
+std::vector<int> l1;
+int n1;
+std::cout << "Enter the number of elements in list 1: ";
+std::cin >> n1;
+for(int i = 0; i < n1; i++) {
+    int x;
+    std::cout << "Enter element " << i + 1 << ": ";
+    std::cin >> x;
+    l1.push_back(x);
+}
 
-    std::vector<int> l2;
-    int n2;
-    std::cout << "Enter the number of elements in list 2: ";
-    std::cin >> n2;
-    for(int i = 0; i < n2; i++) {
-        int x;
-        std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> x;
-        l2.push_back(x);
-    }
+std::vector<int> l2;
+int n2;
+std::cout << "Enter the number of elements in list 2: ";
+std::cin >> n2;
+for(int i = 0; i < n2; i++) {
+    int x;
+    std::cout << "Enter element " << i + 1 << ": ";
+    std::cin >> x;
+    l2.push_back(x);
+}
 
-    if(std::same(l1, l2)) { 
-        std::cout << "The two lists are the same." << std::endl;
-    } else {
-        std::cout << "The two lists are different." << std::endl;
-    }
+if(std::same(l1, l2)) { 
+    std::cout << "The two lists are the same." << std::endl;
+} else {
+    std::cout << "The two lists are different." << std::endl;
+}
 
-    std::vector<int> intersection = common(l1, l2);
-    if(intersection.size() != 0) {
-        for(int i = 0; i < intersection.size(); i++) {
-            std::cout << intersection[i] << " ";
-        }
-        std::cout << std::endl;
-    } else {
-        std::cout << "No common elements." << std::endl;
+std::vector<int> intersection = common(l1, l2);
+if(intersection.size() != 0) {
+    for(int i = 0; i < intersection.size(); i++) {
+        std::cout << intersection[i] << " ";
     }
-
-    return 0;
+    std::cout << std::endl;
+} else {
+    std::cout << "No common elements." << std::endl;
 }
