81974b6cb3634a63becace94a16cc50b8b3b3347
beep boop <githubbot@vadim.me>
2024-07-07 23:59:12 +0200

    Solution 32 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/z3x1q7vo Prompt: 92d3105e-c8ac-418b-8711-62fd158a36ad.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,33 +1,23 @@
-#include <iostream>
 #include <string>
 
 int bowlingScore(string s) {
     int score = 0;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '/') {
-            if (i > 1 && s[i-2] == 'X') {
-                score += 30;
-            } else if (i > 0) {
-                int prevRoll = stoi(s.substr(i - 1, 1));
-                if (prevRoll < 10) {
-                    score += 10 + prevRoll;
-                } else {
-                    score += 10;
-                }
+    int roll = 0;
+    for (char c : s) {
+        if (c == '/') {
+            if (roll < 2) {
+                score += 10 - (10 - stoi(s.substr(0, s.find('/'))));
             }
-            i++;
-        } else if (isdigit(s[i])) {
-            int roll = stoi(s.substr(i, 1));
-            while (i < s.length() && isdigit(s[i])) {
-                roll = roll * 10 + stoi(s.substr(i, 1)) - '0';
-                i++;
-            }
-            score += roll;
-        } else if (s[i] == 'X') {
-            score += 10;
-            if (i < s.length() - 1 && s[i+1] == '/') {
+            roll = 0;
+        } else if (isdigit(c)) {
+            roll++;
+        } else { 
+            if (roll == 1) {
                 score += 10;
+            } else {
+                score += 10 + (stoi(s.substr(s.find('X') - 1, 2)) - 10);
             }
+            roll = 0;
         }
     }
     return score;
