b08a41d0a1fbf7e541a3d04d7581e5dd0edfd05c
beep boop <githubbot@vadim.me>
2024-07-07 08:21:20 +0200

    Solution 45 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mwz9wfa4 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 6438154f-049e-4741-95fb-439d7abcf677.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6438154f-049e-4741-95fb-439d7abcf677.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 6438154f-049e-4741-95fb-439d7abcf677.cpp:10: 6438154f-049e-4741-95fb-439d7abcf677.cpp: In function ‘int main()’: 6438154f-049e-4741-95fb-439d7abcf677.cpp:20:20: error: ‘sorted_list_sum’ was not  declared in this scope    20 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 6438154f-049e-4741-95fb-439d7abcf677.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,8 +1,8 @@
 vector<string> sorted_list_sum(vector<string> lst) {
-    auto it = unique(lst.begin(), lst.end(), 
-        [](const string& a){ return a.size() % 2 == 1; });
+    auto it = unique(lst.begin(), lst.end(),
+        [](const string& a, const string& b){ return (a.size() % 2 == 1 && b.size() % 2 == 0) || (a.size() % 2 == 0 && b.size() % 2 == 1); });
     lst.erase(unique(lst.begin(), it), lst.end());
-    sort(lst.begin(), lst.end(), 
+    sort(lst.begin(), lst.end(),
         [](const string& a, const string& b){ if (a.size() != b.size()) return a.size() < b.size(); else return a < b; });
     return lst;
 }
