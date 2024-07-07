e61a87feac9652bbb6c365e86ee0166885006db9
beep boop <githubbot@vadim.me>
2024-07-08 01:46:07 +0200

    Solution 53 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/192v2308 Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,17 +1,24 @@
+```cpp
 #include <string>
 using namespace std;
 
 class Solution {
 public:
     string numberToWords(int num) {
-        if (num % 15 == 0)
-            return "FizzBuzz";
-        else if (num % 3 == 0)
-            return "Fizz";
-        else if (num % 5 == 0)
-            return "Buzz";
-        else
+        if (num == 0)
+            return "0";
+        
+        if (num % 3 != 0 && num % 5 != 0) 
             return to_string(num);
+            
+        if (num % 15 == 0)  
+            return "FizzBuzz";
+            
+        if (num % 3 == 0)   
+            return "Fizz";
+            
+        if (num % 5 == 0)
+            return "Buzz";
     }
 };
 
