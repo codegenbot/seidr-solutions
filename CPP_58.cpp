389b072862552b5f20386c835ec77218798bdd07
beep boop <githubbot@vadim.me>
2024-07-07 02:56:26 +0200

    Solution 11 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/io7n5spc Prompt: ent 1 from ‘std::insert_iterator<std::set<int> >’ to ‘std::initializer_list<int> ’   316 |       operator=(initializer_list<value_type> __l)       |                 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~ 755227c3-7d63-4de0-a798-6bd4a70d9840.cpp: In function ‘int main()’: 755227c3-7d63-4de0-a798-6bd4a70d9840.cpp:23:9: error: ‘cout’ was not declared in  this scope    23 |         cout << i << " ";       |         ^~~~ 755227c3-7d63-4de0-a798-6bd4a70d9840.cpp:6:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     5 | #include <set>   +++ |+#include <iostream>     6 | using namespace std; 755227c3-7d63-4de0-a798-6bd4a70d9840.cpp: At global scope: 755227c3-7d63-4de0-a798-6bd4a70d9840.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 755227c3-7d63-4de0-a798-6bd4a70d9840.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,7 +1,9 @@
+```cpp
+#pragma message "This program requires at least C++14 for initializer_list support."
 #include <vector>
 #include <algorithm>
 #include <set>
-#include <initializer_list>
+#include <iostream>
 using namespace std;
 
 vector<int> common(vector<int> l1, vector<int> l2) {
