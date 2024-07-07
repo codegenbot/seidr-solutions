1bc0a4c3e1eadab43d3638c9c8f2b0acc34fd545
beep boop <githubbot@vadim.me>
2024-07-07 03:21:48 +0200

    Solution 78 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3wb4tky5 Prompt:                  from d8e7c903-9b4d-4360-a630-893dfa3b3db5.cpp:1: d8e7c903-9b4d-4360-a630-893dfa3b3db5.cpp: In function ‘int main()’: d8e7c903-9b4d-4360-a630-893dfa3b3db5.cpp:19:12: error: ‘filp_case’ was not decla red in this scope    19 |     assert(filp_case("These violent delights have violent ends") == "tHE SE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |            ^~~~~~~~~ d8e7c903-9b4d-4360-a630-893dfa3b3db5.cpp: At global scope: d8e7c903-9b4d-4360-a630-893dfa3b3db5.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ d8e7c903-9b4d-4360-a630-893dfa3b3db5.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from d8e7c903-9b4d-4360-a630-893dfa3b3db5.cpp:23: d8e7c903-9b4d-4360-a630-893dfa3b3db5.cpp: In function ‘int main()’: d8e7c903-9b4d-4360-a630-893dfa3b3db5.cpp:25:13: error: ‘filp_case’ was not decla red in this scope    25 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -18,4 +18,5 @@ std::string filp_case(const std::string& str) -> const std::string {
 
 int main() {
     assert(filp_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
+    return 0;
 }
