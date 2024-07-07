6b1c5e3079df55fca61549cf914df798c0f496d5
beep boop <githubbot@vadim.me>
2024-07-07 07:40:47 +0200

    Solution 29 of CPP/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r926t5cc Prompt:       |              ^~~~~~ cd53601e-5065-4c62-9ae2-1fb95d00a9ac.cpp:1:21: error: ‘vector’ was not declared in this scope     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                     ^~~~~~ cd53601e-5065-4c62-9ae2-1fb95d00a9ac.cpp:1:28: error: expected primary-expressio n before ‘int’     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                            ^~~ cd53601e-5065-4c62-9ae2-1fb95d00a9ac.cpp:1:40: error: expected primary-expressio n before ‘int’     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                                        ^~~ cd53601e-5065-4c62-9ae2-1fb95d00a9ac.cpp:1:52: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                                                    ^ In file included from cd53601e-5065-4c62-9ae2-1fb95d00a9ac.cpp:13: cd53601e-5065-4c62-9ae2-1fb95d00a9ac.cpp: In function ‘int main()’: cd53601e-5065-4c62-9ae2-1fb95d00a9ac.cpp:15:21: error: ‘max_fill’ cannot be used  as a function    15 |     assert (max_fill({{1,1,1,1}, {1,1,1,1}}, 9) == 2);       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_115.cpp
+++ CPP_115.cpp
@@ -13,8 +13,6 @@ int max_fill(std::vector<std::vector<int>> grid, int capacity) {
     }
     return buckets_needed;
 
-}
-
 int main() {
     assert(max_fill({{1,1,1,1}, {1,1,1,1}}, 9) == 2);
     // Test the function
