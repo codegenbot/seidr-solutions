bfbddaf0416e78d0a6031aa1ff52f6988dd5760c
beep boop <githubbot@vadim.me>
2024-07-07 07:14:03 +0200

    Solution 98 of snow-day, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ov9pj3bq Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- snow-day.cpp
+++ snow-day.cpp
@@ -1,15 +1,37 @@
-#include <algorithm>
+#include <iostream>
+using namespace std;
 
 double snowDay(int hours, float initialSnow, float rateOfSnowfall, float proportionMeltingPerHour) {
     double totalSnow = initialSnow; 
 
     for (int i = 0; i < hours; i++) {
         double netSnowfall = rateOfSnowfall - (totalSnow * proportionMeltingPerHour);
-        if (netSnowfall > 0) {
+        if (netSnowfall < 0) {
+            totalSnow = 0;
+        } else {
             totalSnow += netSnowfall;
-        } else if (netSnowfall < 0) {
-            totalSnow = std::max(0.0, totalSnow + netSnowfall);
         }
     }
     
     return totalSnow;
+}
+
+int main() {
+    float hours;
+    float initialSnow, rateOfSnowfall, proportionMeltingPerHour;
+
+    cout << "Enter number of hours: ";
+    cin >> hours;
+    cout << "Enter initial snow: ";
+    cin >> initialSnow;
+    cout << "Enter rate of snow fall: ";
+    cin >> rateOfSnowfall;
+    cout << "Enter proportion of snow melting per hour: ";
+    cin >> proportionMeltingPerHour;
+
+    double result = snowDay((int)hours, initialSnow, rateOfSnowfall, proportionMeltingPerHour);
+    
+    cout << "Amount of snow on the ground after " << hours << " hours is: " << result << endl;
+
+    return 0;
+}
