d3273254ef00a1a5b646e9a5906ba23b263aabdf
beep boop <githubbot@vadim.me>
2024-07-07 10:57:43 +0200

    Solution 63 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6mhsqfmo Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,36 +1,23 @@
-#include <string>
-#include <algorithm>
 #include <boost/any.hpp>
-#include <boost/lexical_cast.hpp>
 
-using namespace std;
+using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return max(a.convert_to<int>(), b.convert_to<float>());
+    if (is_double(a) && is_double(b)) {
+        return (get<double>(a) > get<double>(b)) ? a : b;
+    } else if (is_double(a) || is_double(b)) {
+        return "None";
+    } else if (is_numeric_string(a) && is_numeric_string(b)) {
+        string strA = get<string>(a);
+        string strB = get<string>(b);
+
+        if (strA > strB)
+            return a;
+        else if (strA < strB)
+            return b;
+        else
+            return "None";
+    } else {
+        return "None";
     }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        string str = boost::lexical_cast<string>(a.convert_to<int>());
-        return (boost::lexical_cast<double>(str) > boost::lexical_cast<double>(b.convert_to<string()))) ? a : b;
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return max(a, boost::any(b.convert_to<int>()));
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        string str = a.convert_to<string>();
-        double num = boost::lexical_cast<double>(str);
-        if (num > b.convert_to<float>()) return a;
-        else if (num < b.convert_to<float>()) return b;
-        else return boost::any("None");
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = a.convert_to<string>();
-        string str2 = b.convert_to<string>();
-        double num1 = boost::lexical_cast<double>(str1);
-        double num2 = boost::lexical_cast<double>(str2);
-        if (num1 > num2) return a;
-        else if (num1 < num2) return b;
-        else return boost::any("None");
-    }
-    return a;
 }
