4ae4ad0ccc26de570efbd29d6a9c43cc383709e5
beep boop <githubbot@vadim.me>
2024-07-07 02:57:36 +0200

    Solution 52 of coin-sums, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/0szvkex8 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- coin-sums.cpp
+++ coin-sums.cpp
@@ -1,47 +1,38 @@
 #include <iostream>
-#include <tuple>
-
 using namespace std;
-
-tuple<int, int, int, int> coinSum(int cents) {
-    int quarterCount = 0;
-    int dimeCount = 0;
-    int nickelCount = 0;
-    int pennyCount = 0;
-
-    while (cents >= 25) {
-        quarterCount++;
-        cents -= 25;
-    }
-    
-    while (cents >= 10) {
-        dimeCount++;
-        cents -= 10;
-    }
-
-    while (cents >= 5) {
-        nickelCount++;
-        cents -= 5;
-    }
-
-    while (cents > 0) {
-        pennyCount++;
-        cents--;
-    }
-
-    return make_tuple(quarterCount, dimeCount, nickelCount, pennyCount);
-}
 
 int main() {
     int cents;
     cin >> cents;
 
-    auto counts = coinSum(cents);
-    
-    cout << get<0>(counts) << endl;
-    cout << get<1>(counts) << endl;
-    cout << get<2>(counts) << endl;
-    cout << get<3>(counts) << endl;
+    int pennies = 0;
+    while (cents > 4) { 
+        pennies++;
+        cents -= 1; 
+    }
+
+    int quarters = 0;
+    while (cents >= 25) {
+        quarters++;
+        cents -= 25;
+    }
+
+    int dimes = 0;
+    while (cents >= 10) {
+        dimes++;
+        cents -= 10;
+    }
+
+    int nickles = 0;
+    while (cents >= 5) {
+        nickles++;
+        cents -= 5;
+    }
+
+    cout << quarters << " quarters, ";
+    cout << dimes << " dimes, ";
+    cout << nickles << " nickles, ";
+    cout << pennies << " pennies." << endl;
 
     return 0;
 }
