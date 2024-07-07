8b21b0831970e455ad62d83e2353ec2aac8fa843
beep boop <githubbot@vadim.me>
2024-07-07 11:28:31 +0200

    Solution 90 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mxo8yxx5 Prompt: Complete the following code given the task description and function signature.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,23 +1,30 @@
-#include <openssl/evp.h>
-#include <string>
+MD5_CTX md5Ctx;
+unsigned char md5Buffer[16];
+size_t len = text.length();
+char *strText = text.c_str();
 
-using namespace std;
-
-string string_to_md5(string text) {
-    if (text.empty()) return "";
-
-    unsigned char hash[MD5_DIGEST_LENGTH];
-    MD5_CTX md5;
-    MD5_Init(&md5);
-    const char *ptr = text.c_str();
-    size_t len = text.size();
-    while(len--) MD5_Update(&md5, ptr++, 1);
-    MD5_Final(hash,&md5);
-
-    ostringstream oss;
-    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        oss << setfill('0') << setw(2) << hex << (int)hash[i];
-    }
-
-    return oss.str();
+if (len == 0) {
+    return "";
 }
+
+MD5Init(&md5Ctx);
+MD5Update(&md5Ctx, strText, len);
+MD5Final(md5Buffer, &md5Ctx);
+
+stringstream ss;
+for(int i=0; i<16; i++){
+    char c = (char)(md5Buffer[i] >> 4);
+    if(c > '9')
+        c = ((c - 'a' + 10) % 16);
+    else
+        c += '0';
+    
+    ss << c;
+    c = (char)(md5Buffer[i] & 0xF);
+    if(c > '9')
+        c = ((c - 'a' + 10) % 16);
+    else
+        c += '0';
+    ss << c;
+}
+return ss.str();
