32331fae652fee247ac668501e354c3e487b1872
beep boop <githubbot@vadim.me>
2024-07-07 08:11:14 +0200

    Solution 60 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/p9keohfq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6527eefe-3113-490a-b7f5-092c24711 74d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- mastermind.py
+++ mastermind.py
@@ -2,18 +2,13 @@ def mastermind(code, guess):
     white_peg = 0
     black_peg = 0
     code_counts = [0] * 6
-    guess_counts = [0] * 6
-
     for i in range(4):
-        code_counts[ord(code[i]) - ord("B")] += 1
-        guess_counts[ord(guess[i]) - ord("B")] += 1
+        if code[i] == guess[i]:
+            black_peg += 1
+        elif code_counts[ord(guess[i]) - ord('B')] > 0:
+            white_peg += 1
+            code_counts[ord(guess[i]) - ord('B')] -= 1
+        else:
+            code_counts[ord(guess[i]) - ord('B')] += 1
 
-    for i in range(6):
-        if code_counts[i] > 0 and guess_counts[i] > 0:
-            black_peg += min(code_counts[i], guess_counts[i])
-        elif code_counts[i] > 0:
-            white_peg += code_counts[i] - guess_counts[i]
-        elif guess_counts[i] > 0:
-            white_peg += guess_counts[i]
-
-    return str(white_peg) + "\n" + str(black_peg)
+    return str(white_peg) + '\n' + str(black_peg)
