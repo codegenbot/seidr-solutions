32f639a559a57f725c37fb5330b82d303a1642a9
beep boop <githubbot@vadim.me>
2024-07-07 06:21:34 +0200

    Solution 65 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zx302vzt Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ fb4d4a93-6bf1-4157-bb4a-51498355e0bb.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ fb4d4a93-6bf1-4157-bb4a-51498355e0bb.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from fb4d4a93-6bf1-4157-bb4a-51498355e0bb.cpp:10: fb4d4a93-6bf1-4157-bb4a-51498355e0bb.cpp: In function ‘int main()’: fb4d4a93-6bf1-4157-bb4a-51498355e0bb.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ fb4d4a93-6bf1-4157-bb4a-51498355e0bb.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,24 +1,22 @@
-```cpp
 #include <vector>
 #include <string>
-#include <algorithm>
 
 bool issame(vector<string> a,vector<string> b){
-    vector<string> a_copy(a);
-    vector<string> b_copy(b);
-
-    transform(a_copy.begin(), a_copy.end(), a_copy.begin(),
-               (unsigned int(*)(unsigned int))std::tolower);
-    transform(b_copy.begin(), b_copy.end(), b_copy.begin(),
-               (unsigned int(*)(unsigned int))std::tolower);
-
-    return a_copy == b_copy;
+    vector<string> strings1;
+    for(string s : a){
+        strings1.push_back(std::tolower(s));
+    }
+    vector<string> strings2;
+    for(string s : b){
+        strings2.push_back(std::tolower(s));
+    }
+    return filter_by_substring(strings1, std::tolower(substring)) == filter_by_substring(strings2, std::tolower(substring));
 }
 
 vector<string> filter_by_substring(vector<string> strings, string substring){
     vector<string> result;
     for(string s : strings){
-        if(s.find(substring) != string::npos)
+        if(s.find(std::tolower(substring)) != string::npos)
             result.push_back(s);
     }
     return result;
