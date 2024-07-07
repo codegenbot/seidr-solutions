0c4b5e111f1a8e47e05a01646f84f9dbbf5279ff
beep boop <githubbot@vadim.me>
2024-07-07 03:13:28 +0200

    Solution 14 of CPP/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nmc0p4k8 Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from a8435bc4-8a58-4496-8481-df3e29335f2d.cpp:3: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<const char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ a8435bc4-8a58-4496-8481-df3e29335f2d.cpp:18:5:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_66.cpp
+++ CPP_66.cpp
@@ -1,9 +1,18 @@
+#include <cassert>
+#include <string>
+#include <cctype>
+
 int digitSum(std::string s) {
     int sum = 0;
-    for (int i = 0; i < s.size(); ++i) {
-        if (isupper(s[i])) {
-            sum += tolower(s[i]) - 'a' + 1;
+    for (auto c : s) {
+        if (isupper(c)) {
+            sum += tolower(c) - 'a' + 1;
         }
     }
     return sum % 26;
+
+}
+
+int main() {
+    assert(digitSum("You arE Very Smart") == 14);
 }
