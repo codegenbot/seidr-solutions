b80a5395284a541b056ffebd44308a67810f4aae
beep boop <githubbot@vadim.me>
2024-07-07 02:31:34 +0200

    Solution 18 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nfgpe593 Prompt: 67bdec06-70c2-45ca-b95e-7d045f1440b5.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 67bdec06-70c2-45ca-b95e-7d045f1440b5.cpp:4:5: note: ‘int main()’ previously defi ned here     4 | int main() {       |     ^~~~ In file included from 67bdec06-70c2-45ca-b95e-7d045f1440b5.cpp:18: 67bdec06-70c2-45ca-b95e-7d045f1440b5.cpp: In function ‘int main()’: 67bdec06-70c2-45ca-b95e-7d045f1440b5.cpp:20:17: error: ‘max_element’ was not dec lared in this scope; did you mean ‘std::max_element’?    20 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 67bdec06-70c2-45ca-b95e-7d045f1440b5.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:528:1: note: ‘std::max_element’ d eclared here   528 | max_element(_ExecutionPolicy&& __exec, _ForwardIterator __first, _Forwar dIterator __last);       | ^~~~~~~~~~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,14 +1,8 @@
+```cpp
 #include <algorithm>
 #include <vector>
 
-long long maxVal = 0;
+int maxVal = *std::max_element({-5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10});
 int main() {
-    int l[] = {-5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10};
-    std::vector<int> v;
-    for (int i : l) {
-        v.push_back(i);
-    }
-    
-    long long maxVal = *std::max_element(v.begin(), v.end());
     return std::abs(maxVal);
 }
