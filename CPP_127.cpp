88f6cb079ad6fe2ec997098b8c762df5304ba8df
beep boop <githubbot@vadim.me>
2024-07-07 04:57:26 +0200

    Solution 14 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/32lqhmr7 Prompt: b4b98b09-fc1d-4447-8740-2bf8c675ed45.cpp: In function ‘int main()’: b4b98b09-fc1d-4447-8740-2bf8c675ed45.cpp:16:5: error: ‘assert’ was not declared in this scope    16 |     assert(intersection({-2, -2}, {-3, -2}) == "NO");       |     ^~~~~~ b4b98b09-fc1d-4447-8740-2bf8c675ed45.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <string>   +++ |+#include <cassert>     3 | #include <initializer_list> b4b98b09-fc1d-4447-8740-2bf8c675ed45.cpp: At global scope: b4b98b09-fc1d-4447-8740-2bf8c675ed45.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ b4b98b09-fc1d-4447-8740-2bf8c675ed45.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -1,15 +1,16 @@
-#include <cassert>
+#include <algorithm>
 #include <string>
 #include <initializer_list>
+#include <cassert>
 
 std::string intersection(std::pair<int, int> interval1, std::pair<int, int> interval2) {
     int start = std::max(interval1.first, interval2.first);
     int end = std::min(interval1.second, interval2.second);
 
-    if (start <= end)
-        return "YES";
-    else
+    if (start >= end)
         return "NO";
+    else
+        return "YES";
 }
 
 int main() {
