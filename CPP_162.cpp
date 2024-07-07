246f7328eb177a8270e92bc5e27a99cec56a39ac
beep boop <githubbot@vadim.me>
2024-07-07 06:28:40 +0200

    Solution 22 of CPP/162, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/d64c00go Prompt: 26430268-3d2c-4fc6-b012-c5240334da14.cpp:6:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     6 | string string_to_md5(string text) {       | ^~~~~~       | stdin In file included from 26430268-3d2c-4fc6-b012-c5240334da14.cpp:27: 26430268-3d2c-4fc6-b012-c5240334da14.cpp: In function ‘int main()’: 26430268-3d2c-4fc6-b012-c5240334da14.cpp:29:13: error: ‘string_to_md5’ was not d eclared in this scope    29 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,21 +1,19 @@
 #include <string>
-#include <openssl/ssl.h>
-#include <openssl/x509v3.h>
+#include <algorithm>
+#include <iomanip>
 
 using namespace std;
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
-    unsigned char md5[16];
-    MD5((const unsigned char*)text.c_str(), text.size(), md5);
-
-    string result;
-    for (int i = 0; i < 16; ++i) {
-        char buffer[3];
-        sprintf(buffer, "%02x", md5[i]);
-        result += buffer;
+    unsigned int hash = 0;
+    for (char c : text) {
+        hash = ((hash << 4) + hash) ^ (unsigned int)c;
     }
 
-    return result;
+    ostringstream oss;
+    oss << setfill('0') << setw(8) << hex << hash;
+
+    return oss.str();
 }
