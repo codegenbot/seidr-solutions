7df7c4f96ed60d09eff5934fb529151d33b57e1e
beep boop <githubbot@vadim.me>
2024-07-08 01:14:48 +0200

    Solution 40 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/w53jnnyg Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -1,4 +1,3 @@
-```
 #include <vector>
 using namespace std;
 
@@ -12,24 +11,8 @@ int fuelCost(vector<int> nums) {
 }
 
 int main() {
-    vector<int> nums;
-    cout << "Enter numbers separated by space: ";
-    string str;
-    getline(cin, str);
-    for (const auto& s : split(str, ' ')) {
-        nums.push_back(stoi(s));
-    }
-    cout << "Fuel cost is: " << fuelCost(nums) << endl;
+    vector<int> nums = {1, 2, 3};
+    int result = fuelCost(nums);
+    cout << "Fuel cost: " << result << endl;
     return 0;
-}
-
-vector<string> split(const string& str, char c) {
-    vector<string> tokens;
-    size_t pos = 0;
-    while ((pos = str.find(c)) != string::npos) {
-        tokens.push_back(str.substr(0, pos));
-        str.erase(0, pos + 1);
-    }
-    tokens.push_back(str);
-    return tokens;
 }
