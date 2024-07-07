cf45a65b3453b42748f7ce285e1319e24d0da754
beep boop <githubbot@vadim.me>
2024-07-07 07:14:00 +0200

    Solution 82 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/x3k5z0wa Prompt: 9f2f3adc-3d73-4c2c-8321-5230b29d97aa.cpp:1:25: error: ‘n’ was not declared in th is scope     1 | double* x1 = new double[n], *x2 = new double[n];       |                         ^ 9f2f3adc-3d73-4c2c-8321-5230b29d97aa.cpp:1:46: error: ‘n’ was not declared in th is scope     1 | double* x1 = new double[n], *x2 = new double[n];       |                                              ^

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,24 +1,23 @@
-```
+```c
+#include <cstdio>
 #include <cmath>
-#include <iostream>
-
-using namespace std;
-
-double vectorDistance(int n) {
-    double x1[n], x2[n];
-    for (int i = 0; i < n; i++) {
-        cin >> x1[i] >> x2[i];
-    }
-    double sum = 0.0;
-    for (int i = 0; i < n; i++) {
-        sum += pow(x2[i] - x1[i], 2.0);
-    }
-    return sqrt(sum); 
-}
 
 int main() {
     int n;
-    cin >> n;
-    double distance = vectorDistance(n);
-    cout << "Euclidean distance is: " << distance << endl;
-    return 0; }
+    scanf("%d", &n);
+    double* x1 = new double[n], *x2 = new double[n];
+
+    for(int i=0; i<n; i++) {
+        scanf("%lf", &x1[i]);
+        scanf("%lf", &x2[i]);
+    }
+
+    double sum = 0.0;
+    for(int i=0; i<n; i++) {
+        sum += pow(x1[i] - x2[i], 2);
+    }
+
+    printf("%.4f\n", sqrt(sum));
+
+    return 0;
+}
