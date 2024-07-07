093f0113f3f4d48a8e5f098298e40b44cc3c45a5
beep boop <githubbot@vadim.me>
2024-07-08 00:34:01 +0200

    Solution 80 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/z3x1q7vo Prompt: 1a4c9776-4ca4-4b07-b57f-1a9cf4d61979.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,30 +1,28 @@
-```
 #include <string>
-
-int bowlingScore(std::string s) {
+int bowlingScore(string s) {
     int score = 0;
     int roll1, roll2;
-    bool spare = false;
 
     for (int i = 0; i < 10; i++) {
         if (s[i] == 'X') {
-            score += 10 + (i < 8 ? bowlingScore(s.substr(i+1)) : 0);
-            continue;
-        }
-        roll1 = s[i] - '0';
-        if (i < 8) {
-            if (s[i+1] == '/') {
-                spare = true;
-                i++;
-                continue;
+            score += 10;
+            if (i < 9) {
+                score += bowlingScore(s.substr(i+1));
             }
-            roll2 = s[i+1] - '0';
-            score += roll1 + roll2;
-            if (roll1 + roll2 == 10) {
-                score += bowlingScore(s.substr(i+2));
-            }
+        } else if (s[i] == '/') {
+            spare = true;
+            i++;
         } else {
-            score += roll1;
+            roll1 = s[i] - '0';
+            if (i < 8) {
+                roll2 = s[i+1] - '0';
+                score += roll1 + roll2;
+                if (roll1 + roll2 == 10) {
+                    spare = true;
+                }
+            } else {
+                score += roll1;
+            }
         }
     }
 
