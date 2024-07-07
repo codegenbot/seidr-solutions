abaca09d6e9f96a78ed14212039ebf0d636b0504
beep boop <githubbot@vadim.me>
2024-07-07 04:41:43 +0200

    Solution 52 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/bkm6uark Prompt: Complete the following code given the task description and function signature.

--- Python_162.py
+++ Python_162.py
@@ -1,8 +1,7 @@
 import hashlib
 
 def string_to_md5(text):
-    if not text:
+    if text:
+        return hashlib.md5(text.encode()).hexdigest()
+    else:
         return None
-    md5_hash = hashlib.md5()
-    md5_hash.update(text.encode())
-    return md5_hash.hexdigest()
