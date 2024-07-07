0fb0150cc08d70cb014956d49c2cb6b567caf0d3
beep boop <githubbot@vadim.me>
2024-07-07 07:55:37 +0200

    Solution 25 of CPP/135, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7p0dtrjc Prompt: 9a2271c5-b4ab-4d1c-88f6-352970fd09c1.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the solution:       | ^~~~ In file included from 9a2271c5-b4ab-4d1c-88f6-352970fd09c1.cpp:12: 9a2271c5-b4ab-4d1c-88f6-352970fd09c1.cpp: In function ‘int main()’: 9a2271c5-b4ab-4d1c-88f6-352970fd09c1.cpp:14:13: error: ‘can_arrange’ was not dec lared in this scope    14 |     assert (can_arrange({})==-1);       |             ^~~~~~~~~~~

--- CPP_135.cpp
+++ CPP_135.cpp
@@ -1,18 +1,13 @@
+Here is the modified code:
+
 #include <vector>
 using namespace std;
 
 int can_arrange(vector<int> arr) {
-    int i = 1;
-    while (i < arr.size()) {
+    for (int i = 1; i < arr.size(); i++) {
         if (arr[i] <= arr[i - 1]) {
-            return i - 1;
+            return i;
         }
-        i++;
     }
     return -1;
-}
-
-int main() {
-    assert(can_arrange({}) == -1);
-    return 0;
 }
