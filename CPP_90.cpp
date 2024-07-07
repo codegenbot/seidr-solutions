90cf506076d22ce31137941fcc4c3f1bb7ef5ca1
beep boop <githubbot@vadim.me>
2024-07-07 07:12:19 +0200

    Solution 15 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5ue9xw48 Prompt: 0435b2d9-69c1-4200-8b19-f88226797aba.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int next_smallest(vector<int> lst){       |                   ^~~~~~ 0435b2d9-69c1-4200-8b19-f88226797aba.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int next_smallest(vector<int> lst){       |                          ^~~ In file included from 0435b2d9-69c1-4200-8b19-f88226797aba.cpp:13: 0435b2d9-69c1-4200-8b19-f88226797aba.cpp: In function ‘int main()’: 0435b2d9-69c1-4200-8b19-f88226797aba.cpp:15:26: error: ‘next_smallest’ cannot be  used as a function    15 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -4,7 +4,7 @@
 
 int next_smallest(std::vector<int> lst){
     std::vector<int> sorted = lst;
-    sort(sorted.begin(), sorted.end());
+    std::sort(sorted.begin(), sorted.end());
     if (sorted.size() < 2) return -1; 
     for (int i = 0; i < sorted.size() - 1; i++) {
         if (sorted[i] != sorted[i + 1]) {
