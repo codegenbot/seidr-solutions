1eb9558424ac5c732467b25f3c7fbf7793ad5dcd
beep boop <githubbot@vadim.me>
2024-07-07 05:21:24 +0200

    Solution 86 of CPP/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dz59c59a Prompt: 0467f325-8f2b-48d9-9e48-92d2cefc2b46.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string closest_integer(double num) {       | ^~~~~~ In file included from 0467f325-8f2b-48d9-9e48-92d2cefc2b46.cpp:10: 0467f325-8f2b-48d9-9e48-92d2cefc2b46.cpp: In function ‘int main()’: 0467f325-8f2b-48d9-9e48-92d2cefc2b46.cpp:12:13: error: ‘closest_integer’ was not  declared in this scope    12 |     assert (closest_integer("0") == 0);       |             ^~~~~~~~~~~~~~~

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,10 +1,8 @@
-#include <string>
-
-string closest_integer(double num) {
+std::string closest_integer(double num) {
     int integer = static_cast<int>(num);
     if (abs(num - integer) > abs(num - (integer + 1))) {
-        return to_string(integer + 1);
+        return std::to_string(integer + 1);
     } else {
-        return to_string(integer);
+        return std::to_string(integer);
     }
 }
