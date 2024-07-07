676a79d90cda9afc8592c8a097601498726c6bbf
beep boop <githubbot@vadim.me>
2024-07-07 03:41:43 +0200

    Solution 1 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/b0nazjdk Prompt: 624866f5-a6d0-428a-89de-d262ddc0face.cpp:1:14: error: ‘string’ was not declared in this scope     1 | int is_bored(string S){       |              ^~~~~~ In file included from 624866f5-a6d0-428a-89de-d262ddc0face.cpp:15: 624866f5-a6d0-428a-89de-d262ddc0face.cpp: In function ‘int main()’: 624866f5-a6d0-428a-89de-d262ddc0face.cpp:17:21: error: ‘is_bored’ cannot be used  as a function    17 |     assert (is_bored("You and I are going for a walk") == 0);       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,8 +1,11 @@
-int is_bored(string S){
+```
+#include <string>
+
+int is_bored(std::string S) {
     int count = 0;
-    string boredom = "I";
+    std::string boredom = "I am bored";
     size_t pos = 0;
-    while ((pos = S.find(" ", pos)) != string::npos) {
+    while ((pos = S.find(" ", pos)) != std::string::npos) {
         if (S.substr(0, pos).compare(boredom) == 0) {
             count++;
             break;
