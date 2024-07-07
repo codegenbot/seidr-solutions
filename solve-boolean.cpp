a1521f3321566528c97e7f0132c83d1f5cf57d47
beep boop <githubbot@vadim.me>
2024-07-07 02:53:58 +0200

    Solution 63 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/o9ngt94r Prompt: it must return True for input t|t&t&t|f&t&t|f|t|t, but it returns Enter a Boolean expression: True.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,44 +1,18 @@
-#include <string>
-#include <iostream>
-
-using namespace std;
-
 bool solveBoolean(string s) {
-    bool t = true;
-    bool f = false;
-    bool res = t;
+    bool res = true;
+    int i = 0;
 
-    for (int i = 0; i < s.length(); i++) {
-        char c = s[i];
-        if (c == 't') {
-            if (s[i + 1] == '&') {
-                res &= t;
-                i += 2;
-            } else if (s[i + 1] == '|') {
-                return true;
-            }
-        } else if (c == 'f') {
-            if (s[i + 1] == '&') {
-                res &= f;
-                i += 2;
-            } else if (s[i + 1] == '|') {
-                return false;
-            }
+    while (i < s.length()) {
+        if (s[i] == 't') {
+            i += (s[i + 1] == '&') ? 2 : 1;
+            res = (s[i + 1] == '&') ? (res & true) : (res | true);
+        } else if (s[i] == 'f') {
+            i += (s[i + 1] == '&') ? 2 : 1;
+            res = (s[i + 1] == '&') ? (res & false) : (res | false);
+        } else if (s[i] == '|') {
+            return s.substr(0, i).compare("t") == 0;
         }
     }
 
     return res;
-}
-
-int main() {
-    string s;
-    cout << "Enter a Boolean expression: ";
-    getline(cin, s);
-    bool result = solveBoolean(s);
-    if (result) {
-        cout << "True" << endl;
-    } else {
-        cout << "False" << endl;
-    }
-    return 0;
 }
