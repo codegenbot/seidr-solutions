b7e2e770f887ed4badb42eb990ca6ef3c8557128
beep boop <githubbot@vadim.me>
2024-07-07 04:41:43 +0200

    Solution 10 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/wws4u5em Prompt:       |                           ^~~~~~ 76085e11-44c4-4532-bef8-132370ff849b.cpp:41:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 76085e11-44c4-4532-bef8-132370ff849b.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 76085e11-44c4-4532-bef8-132370ff849b.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 76085e11-44c4-4532-bef8-132370ff849b.cpp:41:34: error: expected primary-expressi on before ‘int’    41 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 76085e11-44c4-4532-bef8-132370ff849b.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 76085e11-44c4-4532-bef8-132370ff849b.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -4,33 +4,7 @@
 // Prototype declaration
 std::vector<int> get_odd_collatz(int n);
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-// Function implementation
-std::vector<int> get_odd_collatz(int n) {
-    std::vector<int> result;
-    while (n != 1) {
-        if (n % 2 == 0) {
-            n /= 2;
-        } else {
-            n = 3 * n + 1;
-        }
-        if (n % 2 != 0) { 
-            result.push_back(n);
-        }
-    }
-    return result;
-}
+bool issame(std::vector<int>, std::vector<int>); 
 
 std::vector<int> get_odd_collatz(int n) {
     std::vector<int> result;
@@ -49,12 +23,5 @@ std::vector<int> get_odd_collatz(int n) {
 
 int main() {
     assert(issame(get_odd_collatz(1), std::vector<int>({1})));  
-    int input;
-    std::cout << "Enter a number: ";
-    std::cin >> input;
-    std::vector<int> output = get_odd_collatz(input);
-    for (int num : output) {
-        std::cout << num << " ";
-    }
     return 0;
 }
