1ccd31f8d2a512ca2af309823583b85c3b956476
beep boop <githubbot@vadim.me>
2024-07-07 06:34:02 +0200

    Solution 90 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/o9ngt94r Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <string>
 #include <iostream>
 
@@ -13,10 +14,9 @@ bool solveBoolean(string s) {
             bool subRes = evaluateTerm(term);
             res = res || subRes;
             term = "";
-        } else if (c == 't' || c == 'f') {
-            if (c == 't') term += 't';
-            else term += 'f';
-            if (i < s.length() - 1 && s[i + 1] == '&') i++; 
+        } 
+        else if (c == 't' || c == 'f') {
+            term += (c == 't') ? "t" : "f";
         }
     }
     bool subRes = evaluateTerm(term);
@@ -31,17 +31,16 @@ bool evaluateTerm(string term) {
     for (int i = 0; i < term.length(); i++) {
         char c = term[i];
         if (c == '&') {
-            bool subRes = (subTerm == "t") ? true : false;
-            res = res && subRes;
+            if (subTerm == "tt") res = true;
+            else if (subTerm == "tf" || subTerm == "ft" || subTerm == "ff") res = false;
             subTerm = "";
-        } else if (c == 't' || c == 'f') {
-            if (c == 't') subTerm += 't';
-            else subTerm += 'f';
-            if (i < term.length() - 1 && term[i + 1] == '&') i++; 
+        } 
+        else if (c == 't' || c == 'f') {
+            subTerm += (c == 't') ? "tt" : "tf";
         }
     }
-    bool subRes = (subTerm == "t") ? true : false;
-    res = res && subRes;
+    if (subTerm == "tt") res = true;
+    else if (subTerm == "tf" || subTerm == "ft" || subTerm == "ff") res = false;
 
     return res;
 }
@@ -57,3 +56,4 @@ int main() {
         cout << "False" << endl;
     }
     return 0;
+}
