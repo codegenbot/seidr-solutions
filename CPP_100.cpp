20fcbbbd60e55bf4bfcb5f8ee23ec49620fdbec9
beep boop <githubbot@vadim.me>
2024-07-07 04:25:05 +0200

    Solution 33 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ajvrnv63 Prompt:       |             ^~~~~~ 4329aad3-eef4-4efa-b79b-e357657c7a7d.cpp:36:34: error: expected primary-expressi on before ‘int’    36 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4329aad3-eef4-4efa-b79b-e357657c7a7d.cpp:36:39: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 4329aad3-eef4-4efa-b79b-e357657c7a7d.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ 4329aad3-eef4-4efa-b79b-e357657c7a7d.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 4329aad3-eef4-4efa-b79b-e357657c7a7d.cpp:35: 4329aad3-eef4-4efa-b79b-e357657c7a7d.cpp: In function ‘int main()’: 4329aad3-eef4-4efa-b79b-e357657c7a7d.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,33 +1,8 @@
-#include <vector>
-#include <algorithm>
-
-std::vector<int> make_a_pile(int n) {
-    std::vector<int> pile;
-    for (int i = 2; i <= n; i += 2) {
-        pile.push_back(i);
-    }
-    return pile;
-}
-
-bool areSame(const std::vector<int>& a, const std::vector<int>& b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
-            return false;
-    }
-    return true;
-}
-
 int main() {
-    std::vector<int> pile = make_a_pile(20);
+    std::vector<int> pile = make_a_pile(8);
     std::sort(pile.begin(), pile.end());
     for (auto it = pile.begin(); it != pile.end(); ++it) {
         *it -= 1;
     }
-    if (!areSame(make_a_pile(8), pile)) {
-        std::cout << "Test failed" << std::endl;
-    } else {
-        std::cout << "Test passed" << std::endl;
-    }
+    assert(sameCheck(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
 }
