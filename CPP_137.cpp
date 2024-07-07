d8f8a9943407042ae4fa7e912e88e6d4deb95727
beep boop <githubbot@vadim.me>
2024-07-07 11:02:09 +0200

    Solution 73 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6mhsqfmo Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,55 +1,28 @@
 #include <boost/any.hpp>
 #include <string>
-#include <iostream>
 
-using namespace std;
+using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return max((int)a.to_ulong(), (float)b.to_double());
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string str = (string)b.convert_to<string>();
-        istringstream iss(str);
-        float num;
-        iss >> num;
-        return max(a, boost::any(num));
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if ((string)a.convert_to<string>() > (string)b.convert_to<string>())
-            return a;
-        else if ((string)a.convert_to<string>() < (string)b.convert_to<string>())
+    if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return b;
+    } else if (a.type() == typeid(double) && b.type() == typeid(string)) {
+        return b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if (stod(b.convert_to<string>().c_str()) >= stod(a.convert_to<string>().c_str())) {
             return b;
-        else
-            return boost::any("None");
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        string str = (string)b.convert_to<string>();
-        istringstream iss(str);
-        int num;
-        iss >> num;
-        if ((int)a.to_ulong() > num)
+        } else {
             return a;
-        else if ((int)a.to_ulong() < num)
-            return b;
-        else
-            return boost::any("None");
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        int num = (int)a.convert_to<int>();
-        if ((string)b.convert_to<string>() > to_string(num))
-            return b;
-        else if ((string)b.convert_to<string>() < to_string(num))
+        }
+    } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
+        if (get<double>(a) > get<double>(b)) {
             return a;
-        else
-            return boost::any("None");
-    }
-    else {
-        if ((float)a.to_double() > (float)b.to_double())
-            return a;
-        else if ((float)a.to_double() < (float)b.to_double())
+        } else if (get<double>(a) < get<double>(b)) {
             return b;
-        else
+        } else {
             return boost::any("None");
+        }
+    } else {
+        return boost::any("None");
     }
 }
