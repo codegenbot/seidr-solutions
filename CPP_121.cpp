d67df1a796c5b963a9c25cd08b5d56b63f71c674
beep boop <githubbot@vadim.me>
2024-07-07 05:26:34 +0200

    Solution 53 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0h3w6dr4 Prompt: w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const int*; _Tp = int; _Alloc = std:: allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = i nt; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::allocator_type = std ::allocator<int>]’ 83387d8f-c796-4487-9de3-429165921dcb.cpp:14:23:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,17 +1,6 @@
-#include <iostream>
-#include <vector>
-
-int solution(std::vector<int> arr) {
-    int result = 0;
-    for (int i : arr) {
-        result += i % 2;
-    }
-    return result;
-}
-
 int main() {
-    using namespace std;
-    int sum = solution({3, 13, 2, 9});
-    cout << "Result: " << sum << endl;
+    std::vector<int> arr = {3, 13, 2, 9};
+    int sum = solution(arr);
+    std::cout << "Result: " << sum << std::endl;
     return 0;
 }
