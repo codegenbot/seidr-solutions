007e1dd527a75360a9045795c31157e8945beadb
beep boop <githubbot@vadim.me>
2024-07-07 02:05:49 +0200

    Solution 80 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/a04ox21m Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -2,30 +2,6 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) {
-        cin >> vec[i];
-    }
-    vector<vector<int>> res = cutVector(vec);
-    cout << "[";
-    for (int i = 0; i < res.size(); i++) {
-        cout << "[";
-        for(int j = 0; j < res[i].size(); j++){
-            for(auto num:res[i][j]){
-                cout<<num<<" ";
-                if(j+1<res[i].size())cout<<",";
-            }
-        }
-        if(i < res.size() - 1) cout << "], [";
-        else cout << "]";
-    }
-    cout << "]";
-    return 0;
-}
-
 vector<vector<int>> cutVector(vector<int> vec) {
     int n = vec.size();
     vector<vector<int>> res(2);
@@ -37,4 +13,23 @@ vector<vector<int>> cutVector(vector<int> vec) {
         }
     }
     return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
+    }
+    auto res = cutVector(vec);
+    cout << "[";
+    for (int i = 0; i < res.size(); i++) {
+        cout << "[";
+        for(auto num:res[i])cout<<num<<" ";
+        if(i < res.size() - 1) cout << "], [";
+        else cout << "]";
+    }
+    cout << "]";
+    return 0;
 }
