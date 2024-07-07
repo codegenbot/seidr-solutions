9298e2f503da86868fd50d6ceec07f690204313f
beep boop <githubbot@vadim.me>
2024-07-07 07:43:16 +0200

    Solution 0 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/j8xyzuq3 Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_129.cpp
@@ -0,0 +1,24 @@
+vector<int> minPath(vector<vector<int>>& grid, int k){
+    int n = grid.size();
+    vector<vector<int>> dp(n, vector<int>(n));
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 && j==0) dp[i][j] = grid[i][j];
+            else{
+                int val = INT_MAX;
+                if(i>0) val = min(val, dp[i-1][j]);
+                if(j>0) val = min(val, dp[i][j-1]);
+                dp[i][j] = val + grid[i][j];
+            }
+        }
+    }
+    vector<int> res;
+    int i = n-1, j = n-1;
+    for(int l=0; l<k; l++){
+        res.push_back(grid[i][j]);
+        if(i>0 && dp[i-1][j] < dp[i][j-1]) i--;
+        else j--;
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
