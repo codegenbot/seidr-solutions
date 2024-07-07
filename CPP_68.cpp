64b4f6cf1bd59d0f278aebeebc46f77ed85fdf70
beep boop <githubbot@vadim.me>
2024-07-07 04:50:26 +0200

    Solution 79 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/czri1ggi Prompt: In file included from /usr/include/c++/11/vector:67,                  from fb7cc24b-7d37-44c5-9428-7f5a1dd494cd.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from fb7cc24b-7d37-44c5-9428-7f5a1dd494cd.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ fb7cc24b-7d37-44c5-9428-7f5a1dd494cd.cpp:69:34: error: expected primary-expressi on before ‘int’    69 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fb7cc24b-7d37-44c5-9428-7f5a1dd494cd.cpp:69:39: error: expression list treated a s compound expression in initializer [-fpermissive]    69 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fb7cc24b-7d37-44c5-9428-7f5a1dd494cd.cpp:68: fb7cc24b-7d37-44c5-9428-7f5a1dd494cd.cpp: In function ‘int main()’: fb7cc24b-7d37-44c5-9428-7f5a1dd494cd.cpp:78:19: error: ‘issame’ cannot be used a s a function    78 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -35,7 +35,7 @@ int mainFunction() {
     // Read input
     std::cout << "Enter the number of elements: ";
     std::cin >> n;  
-    std::vector<int> input(input); 
+    std::vector<int> input(n, 0); 
     for (int i = 0; i < n; i++) {
         std::cout << "Enter element " << i + 1 << ": ";
         std::cin >> input[i];
@@ -58,7 +58,10 @@ int mainFunction() {
     if (output.empty()) {
         std::cout << "No even numbers found." << std::endl;
     } else {
-        assert(sameVector(pluck({7, 9, 7, 1}), {}));
+        std::vector<int> test = {1,2,3};
+        std::vector<int> expected = {7, 9, 7, 1};
+        std::vector<int> actual = pluck(test);
+        assert(sameVector(actual, expected));
         std::cout << "Minimum even number: " << output[0] << ", Index: " << output[1]
                   << std::endl;
     }
