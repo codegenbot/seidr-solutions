d4ca57dd1aa605ecaa00dec31c2b462d8c95289a
beep boop <githubbot@vadim.me>
2024-07-07 03:19:16 +0200

    Solution 3 of CPP/76, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/52ibulto Prompt: 03c29fd7-d614-4099-8314-1ee95e5f7bb6.cpp: In function ‘bool is_simple_power(int,  int)’: 03c29fd7-d614-4099-8314-1ee95e5f7bb6.cpp:2:23: error: ‘log’ was not declared in this scope; did you mean ‘long’?     2 |     double y = pow(n, log(x) / log(n));       |                       ^~~       |                       long 03c29fd7-d614-4099-8314-1ee95e5f7bb6.cpp:2:16: error: ‘pow’ was not declared in this scope     2 |     double y = pow(n, log(x) / log(n));       |                ^~~

--- CPP_76.cpp
+++ CPP_76.cpp
@@ -1,6 +1,6 @@
-Here is the completed code:
+#include <cmath>
 
 bool is_simple_power(int x, int n) {
-    double y = pow(n, log(x) / log(n));
+    double y = pow(n, std::log(x) / std::log(n));
     return (y == x);
 }
