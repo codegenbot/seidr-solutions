a99fb7d6aa26e846a9228000787ad1f508b308e0
beep boop <githubbot@vadim.me>
2024-07-08 00:17:28 +0200

    Solution 53 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/z3x1q7vo Prompt: a3c2952e-e87d-4e72-9322-de5c9ecc989d.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string str) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,19 +1,19 @@
-using namespace std;
+#include <string>
 
 int bowlingScore(string str) {
     int score = 0;
-    for (char c : str) {
-        if (c == '/') {
-            if (score % 2 != 1) {
-                score += 10 - (str[str.find(c) - 1] - '0');
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == '/') {
+            if (i + 2 <= str.length()) {
+                if (str[i+1] != '/' && str[i+2] != '/') {
+                    score += 10 - ((str[i-1] - '0') + (str[i] - '0'));
+                } else {
+                    score += 10;
+                }
             }
-            else {
-                score += 10;
-            }
-        } 
-        else if (isdigit(c)) {
-            score += c - '0';
-        } 
+        } else if (isdigit(str[i])) {
+            score += str[i] - '0';
+        }
     }
     return score;
 }
