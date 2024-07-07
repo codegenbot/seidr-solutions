9666d1cb06fa83b94682ec88e25a3d011716e470
beep boop <githubbot@vadim.me>
2024-07-07 07:02:06 +0200

    Solution 41 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jdhym3pn Prompt:  does not name a type     3 | std::string toMiniRomanHelper(int num, std::string str, std::string roma n) {       |      ^~~~~~ 3d9c23b0-662b-40d7-b97f-ee65e2343839.cpp:2:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?     1 | #include <cassert>   +++ |+#include <string>     2 | 3d9c23b0-662b-40d7-b97f-ee65e2343839.cpp:48:6: error: ‘string’ in namespace ‘std ’ does not name a type    48 | std::string int_to_mini_roman(int num) {       |      ^~~~~~ 3d9c23b0-662b-40d7-b97f-ee65e2343839.cpp:48:1: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’?    48 | std::string int_to_mini_roman(int num) {       | ^~~ In file included from 3d9c23b0-662b-40d7-b97f-ee65e2343839.cpp:52: 3d9c23b0-662b-40d7-b97f-ee65e2343839.cpp: In function ‘int main()’: 3d9c23b0-662b-40d7-b97f-ee65e2343839.cpp:54:13: error: ‘int_to_mini_romank’ was not declared in this scope    54 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,9 +1,50 @@
-#include <cassert>
+#include <string>
 
-std::string int_to_mini_roman(int num) {
-    return toMiniRomanHelper(num, "", "M");
+std::string toMiniRomanHelper(int num, std::string str, std::string roman) {
+    if (num == 0)
+        return str;
+    if (num >= 4000)
+        return "IV";
+    if (num >= 1000) {
+        str += roman[0];
+        return toMiniRomanHelper(num - 1000, str, roman);
+    }
+    if (num >= 900)
+        return "CM";
+    if (num >= 500) {
+        str += roman[0];
+        return toMiniRomanHelper(num - 500, str, roman.substr(1));
+    }
+    if (num >= 400)
+        return "CD";
+    if (num >= 100) {
+        str += roman[0];
+        return toMiniRomanHelper(num - 100, str, roman.substr(1));
+    }
+    if (num >= 90)
+        return "XC";
+    if (num >= 50) {
+        str += roman[0];
+        return toMiniRomanHelper(num - 50, str, roman.substr(1));
+    }
+    if (num >= 40)
+        return "XL";
+    if (num >= 10) {
+        str += roman[0];
+        return toMiniRomanHelper(num - 10, str, roman.substr(1));
+    }
+    if (num >= 9)
+        return "IX";
+    if (num >= 5) {
+        str += roman[0];
+        return toMiniRomanHelper(num - 5, str, roman.substr(1));
+    }
+    if (num >= 4)
+        return "IV";
+    str += roman[0];
+    return str + roman[1];
 }
 
-int main() {
-    assert (int_to_mini_roman(1000) == "M");
+std::string int_to_mini_roman(int num) {
+    return toMiniRomanHelper(num, "", "MDCLXVI");
 }
