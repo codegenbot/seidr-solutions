7a059267f09e2d0ba390a5bfcaa401d300d0ad4f
beep boop <githubbot@vadim.me>
2024-07-07 06:55:24 +0200

    Solution 38 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jdhym3pn Prompt: In file included from 7c5865ec-d575-4591-b6f5-d8188d598a93.cpp:22: 7c5865ec-d575-4591-b6f5-d8188d598a93.cpp: In function ‘int main()’: 7c5865ec-d575-4591-b6f5-d8188d598a93.cpp:24:13: error: ‘int_to_mini_romank’ was not declared in this scope    24 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,9 +1,30 @@
 ```cpp
-std::string int_to_mini_roman(int num) {
-    return toMiniRomanHelper(num, "", "M");
+#include <string>
+
+std::string toMiniRomanHelper(int num, std::string s1, std::string s2) {
+    if (num == 4) {
+        return s2;
+    } else if (num == 9) {
+        return s1 + "I";
+    } else {
+        std::string temp = "";
+        for (int i = 0; i < num / 5; i++) {
+            temp += s1[0];
+        }
+        if (!temp.empty()) {
+            temp += toMiniRomanHelper(num % 5, "", s2);
+        } else {
+            temp += toMiniRomanHelper(num, s1, s2);
+        }
+        return temp;
+    }
+}
+
+std::string int_to_mini_romank(int n) {
+    return toMiniRomanHelper(n, "M", "IV");
 }
 
 int main() {
-    assert (int_to_mini_roman(1000) == "M");
-    //...
+    assert(int_to_mini_romank(1000) == "M");  
+    return 0;
 }
