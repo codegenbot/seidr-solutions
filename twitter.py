1e44700b5109c66bfa04a831a8ccaf51686d6482
beep boop <githubbot@vadim.me>
2024-07-08 01:22:15 +0200

    Solution 39 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-codellama_34b-instruct-run1/runs/i8j8myp4 Prompt: it must return Your tweet has 16 characters for input d|iR$lGSxnu,5DZ*, but it returns .

--- twitter.py
+++ twitter.py
@@ -1,8 +1,5 @@
-
 def validate_tweet(tweet):
     if len(tweet) > 140:
         return "Too many characters"
     else:
         return f"Your tweet has {len(tweet)} characters"
-    else:
-        return "You didn't type anything"
