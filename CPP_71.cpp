e37ae5e22f42f23c0c72154058832af655186200
beep boop <githubbot@vadim.me>
2024-07-07 06:55:54 +0200

    Solution 4 of CPP/71, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tmveg6rm Prompt: Complete the following code given the task description and function signature.

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,10 +1,10 @@
-Here is the solution:
+#include <cmath>
+
+using namespace std;
 
 float triangle_area(float a, float b, float c) {
-    if ((a + b > c) && (a + c > b) && (b + c > a)) {
-        float s = (a + b + c) / 2;
-        return round((sqrt(s * (s - a) * (s - b) * (s - c))) * 100.0) / 100.0;
-    } else {
-        return -1.0f;
-    }
+    if (a + b > c && a + c > b && b + c > a)
+        return 0.5f * sqrt(pow(c, 2) - pow(a, 2) - pow(b, 2));
+    else
+        return -1;
 }
