cb80d81bd727fd65c66027add3842184c5c69329
beep boop <githubbot@vadim.me>
2024-07-07 03:05:50 +0200

    Solution 23 of CPP/51, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tg52cs35 Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 6d48d20a-9d15-42c8-98fb-4dac5e063b9b.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<const char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 6d48d20a-9d15-42c8-98fb-4dac5e063b9b.cpp:5:26:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_51.cpp
+++ CPP_51.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 std::string remove_vowels(std::string text){
-    std::string result = "";
+    std::string result;
     for(char c : text){
         if(tolower(c) != 'a' && tolower(c) != 'e' && tolower(c) != 'i' && 
            tolower(c) != 'o' && tolower(c) != 'u'){
