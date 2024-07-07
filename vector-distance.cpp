37f098544adc5d591c351ac459a951099c6f8d9b
beep boop <githubbot@vadim.me>
2024-07-07 07:18:03 +0200

    Solution 90 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/x3k5z0wa Prompt: std::__copy_move_backward<_IsMove, true, std::random_access_iterator_tag>::__cop y_move_b(const _Tp*, const _Tp*, _Tp*)’: /usr/include/c++/11/bits/stl_algobase.h:740:17: error: ‘ptrdiff_t’ does not name  a type   740 |           const ptrdiff_t _Num = __last - __first;       |                 ^~~~~~~~~ /usr/include/c++/11/bits/stl_algobase.h:740:17: note: ‘ptrdiff_t’ is defined in header ‘<cstddef>’; did you forget to ‘#include <cstddef>’? /usr/include/c++/11/bits/stl_algobase.h:741:15: error: ‘_Num’ was not declared i n this scope   741 |           if (_Num)       |               ^~~~ /usr/include/c++/11/bits/stl_algobase.h:743:29: error: ‘_Num’ was not declared i n this scope   743 |           return __result - _Num;       |                             ^~~~ /usr/include/c++/11/bits/stl_algobase.h: At global scope: /usr/include/c++/11/bits/stl_algobase.h:1349:16: error: ‘ptrdiff_t’ does not nam e a type  1349 |         static ptrdiff_t       |                ^~~~~~~~~ /usr/include/c++/11/bits/stl_algobase.h:1349:16: note: ‘ptrdiff_t’ is defined in  header ‘<cstddef>’; did you forget to ‘#include <cstddef>’?

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,5 +1,23 @@
-```cpp
-int n;
-cin >> n;
+#include <cstdio>
+#include <cmath>
+#include <cstddef>
 
-double* x1 = new double[n], *x2 = new double[n];
+int main() {
+    int n;
+    scanf("%d", &n);
+    double* x1 = new double[n], *x2 = new double[n];
+
+    for(int i=0; i<n; i++) {
+        scanf("%lf", &x1[i]);
+        scanf("%lf", &x2[i]);
+    }
+
+    double sum = 0.0;
+    for(int i=0; i<n; i++) {
+        sum += pow(x1[i] - x2[i], 2);
+    }
+
+    printf("%.4f\n", sqrt(sum));
+
+    return 0;
+}
