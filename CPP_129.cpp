0419ba35a7ef37ec628debb2cb023c53691eb05e
beep boop <githubbot@vadim.me>
2024-07-07 10:31:04 +0200

    Solution 20 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qxktwr7o Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,40 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
     int n = grid.size();
-    vector<vector<int>> dp(n, vector<int>(n));
+    vector<vector<int>> visited(n, vector<int>(n));
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>> > pq;
+    vector<int> res;
     
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == 0 && j == 0) {
-                dp[i][j] = grid[i][j];
-            } else if (i > 0) {
-                dp[i][j] = min({dp[i-1][j], dp[i-1][j-1], dp[i-1][j+1]}) + grid[i][j];
-            } else if (j > 0) {
-                dp[i][j] = min({dp[i][j-1], dp[i-1][j-1], dp[i+1][j-1]}) + grid[i][j];
-            } else {
-                dp[i][j] = min({dp[i-1][j], dp[i-1][j+1]}) + grid[i][j];
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (grid[i][j] == 1) {
+                pq.push({0, {i, j}});
+                visited[i][j] = 1;
             }
         }
     }
-    
-    vector<int> res;
-    int i = n - 1, j = n - 1;
-    for (int l = 0; l < k; l++) {
-        res.push_back(grid[i][j]);
-        if (i > 0 && j > 0) {
-            if (dp[i-1][j] == dp[i][j]) i--;
-            else if (dp[i][j-1] == dp[i][j]) j--;
-            else i--;
-        } else if (i > 0) {
-            i--;
-        } else {
-            j--;
+
+    while (!pq.empty()) {
+        int val = pq.top().first;
+        pair<int, int> pos = pq.top().second;
+        pq.pop();
+
+        if (k > 0) {
+            res.push_back(val);
+            k--;
+        }
+
+        vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (auto dir : dirs) {
+            int ni = pos.first + dir.first;
+            int nj = pos.second + dir.second;
+
+            if (ni >= 0 && ni < n && nj >= 0 && nj < n && !visited[ni][nj]) {
+                visited[ni][nj] = 1;
+                pq.push({val, {ni, nj}});
+            }
         }
     }
-    
+
     return res;
 }
 
@@ -42,8 +47,8 @@ int main() {
     vector<vector<int>> grid = {{1,2,3}, {4,5,6}, {7,8,9}};
     int k = 3;
     vector<int> result = minPath(grid, k);
-    for (int i : result) {
-        cout << i << " ";
+    for (auto val : result) {
+        cout << val << " ";
     }
     return 0;
 }
