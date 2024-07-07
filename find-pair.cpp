e8e7842b89cf0abde7bd113c16dd768cf343f837
beep boop <githubbot@vadim.me>
2024-07-07 05:52:05 +0200

    Solution 12 of find-pair, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/iz1rxrhj Prompt:  in this scope    10 |         if (numMap.find(complement) != numMap.end()) {       |             ^~~~~~ 17ab7f8e-c584-43d3-8172-bac3ee99f2fe.cpp:14:9: error: ‘numMap’ was not declared in this scope    14 |         numMap[vec[i]] = i;       |         ^~~~~~ 17ab7f8e-c584-43d3-8172-bac3ee99f2fe.cpp: In function ‘int main()’: 17ab7f8e-c584-43d3-8172-bac3ee99f2fe.cpp:22:5: error: ‘cin’ was not declared in this scope    22 |     cin >> n;       |     ^~~ 17ab7f8e-c584-43d3-8172-bac3ee99f2fe.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; 17ab7f8e-c584-43d3-8172-bac3ee99f2fe.cpp:33:5: error: ‘cout’ was not declared in  this scope    33 |     cout << result.first << "\n";       |     ^~~~ 17ab7f8e-c584-43d3-8172-bac3ee99f2fe.cpp:33:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’?

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,39 +1,40 @@
 #include <vector>
+#include <unordered_map>
+#include <iostream>
+
 using namespace std;
 
-vector<int> findPair(vector<int>& nums, int target) {
+pair<int, int> findPair(vector<int>& vec, int target) {
     unordered_map<int, int> numMap;
-    vector<int> result;
-
-    for (int i = 0; i < nums.size(); i++) {
-        int complement = target - nums[i];
+    
+    for (int i = 0; i < vec.size(); i++) {
+        int complement = target - vec[i];
+        
         if (numMap.find(complement) != numMap.end()) {
-            result.push_back(nums[i]);
-            result.push_back(complement);
-            break;
+            return {complement, vec[i]};
         }
-        numMap[nums[i]] = i;
+        
+        numMap[vec[i]] = i;
     }
-
-    return result;
+    
+    return {-1, -1}; // Return a default pair if no such pair is found
 }
 
 int main() {
     int n;
     cin >> n;
-
-    vector<int> nums(n);
+    vector<int> vec(n);
     for (int i = 0; i < n; i++) {
-        cin >> nums[i];
+        cin >> vec[i];
     }
-
+    
     int target;
     cin >> target;
-
-    vector<int> pair = findPair(nums, target);
-
-    cout << pair[0] << endl;
-    cout << pair[1] << endl;
-
+    
+    pair<int, int> result = findPair(vec, target);
+    
+    cout << result.first << "\n";
+    cout << result.second << "\n";
+    
     return 0;
 }
