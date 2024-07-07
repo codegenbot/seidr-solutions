ffbefd380434107c493b3dd736a3295f5dc6d3db
beep boop <githubbot@vadim.me>
2024-07-07 05:37:57 +0200

    Solution 8 of CPP/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/01ftl4ye Prompt: >&, signed char*)’   852 |     operator>>(basic_istream<char, _Traits>& __in, signed char* __s)       |     ^~~~~~~~ /usr/include/c++/11/istream:852:5: note:   template argument deduction/substitut ion failed: 4ca046a0-24e5-4abe-9a6d-041588e2f121.cpp:10:31: note:   cannot convert ‘'/'’ (ty pe ‘char’) to type ‘signed char*’    10 |     if (!(s1 >> a) || !(s1 >> '/' >> b)) {       |                               ^~~ In file included from /usr/include/c++/11/sstream:38,                  from 4ca046a0-24e5-4abe-9a6d-041588e2f121.cpp:2: /usr/include/c++/11/istream:1006:5: note: candidate: ‘template<class _Istream, c lass _Tp> _Istream&& std::operator>>(_Istream&&, _Tp&&)’  1006 |     operator>>(_Istream&& __is, _Tp&& __x)       |     ^~~~~~~~ /usr/include/c++/11/istream:1006:5: note:   template argument deduction/substitu tion failed: /usr/include/c++/11/istream: In substitution of ‘template<class _Istream, class _Tp> _Istream&& std::operator>>(_Istream&&, _Tp&&) [with _Istream = std::__cxx11 ::basic_stringstream<char>&; _Tp = char]’: 4ca046a0-24e5-4abe-9a6d-041588e2f121.cpp:10:31:   required from here /usr/include/c++/11/istream:1006:5: error: no type named ‘type’ in ‘struct std:: enable_if<false, void>’

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -4,10 +4,22 @@
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = 0, b = 0, c = 0, d = 0;
+    int a = 0, b = 1, c = 0, d = 1;
     stringstream s1(x), s2(n);
-    s1 >> a >> '/' >> b;
-    s2 >> c >> '/' >> d;
+    
+    std::string num_str;
+    s1 >> num_str;
+    size_t pos = num_str.find('/');
+    if (pos == string::npos) {
+        return false;
+    }
+    a = stoi(num_str.substr(0, pos));
+    b = stoi(num_str.substr(pos + 1));
+
+    string n_str;
+    if (!(s2 >> c) || !(getline(s2, n_str, '/') && s2 >> d)) {
+        return false;
+    }
     
     return (a*d == b*c);
 }
