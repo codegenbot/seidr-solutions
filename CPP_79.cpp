7fb231e5a7603c588593c8a38a9d4935d29878dc
beep boop <githubbot@vadim.me>
2024-07-07 07:27:02 +0200

    Solution 86 of CPP/79, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/j7dnhi1j Prompt: 1d0329bd-3a64-4a38-b29e-641bacfa3666.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the solution:       | ^~~~

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -1,9 +1,9 @@
-int decimalToBinary(int n) {
+string decimal_to_binary(int decimal){
     string binary = "";
-    while(n > 0){
-        if(n % 2 == 0) binary.push_back('0');
+    while(decimal > 0){
+        if(decimal % 2 == 0) binary.push_back('0');
         else binary.push_back('1');
-        n /= 2;
+        decimal /= 2;
     }
     string result = "db" + binary + "db";
     return result;
