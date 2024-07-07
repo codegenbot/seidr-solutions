aab68943fc3e106735a0c75e11aef420e56b5224
beep boop <githubbot@vadim.me>
2024-07-07 10:25:25 +0200

    Solution 68 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dbjtk1yu Prompt: Complete the following code given the task description and function signature.

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,26 +1,30 @@
-Here is the completed code:
+Here is the solution:
 
 vector<string> reverse_delete(string s, string c) {
-    vector<string> result;
-    string temp = "";
-    for (char x : s) {
-        bool flag = false;
-        for (char y : c) {
-            if (x == y) {
-                flag = true;
-                break;
-            }
-        }
-        if (!flag) {
-            temp += x;
+    vector<char> temp(s.begin(), s.end());
+    for (char i : c) {
+        auto it = find(temp.begin(), temp.end(), i);
+        while (it != temp.end()) {
+            temp.erase(it);
+            break;
         }
     }
-    result.push_back(temp);
-    string rev = temp;
-    reverse(rev.begin(), rev.end());
-    if (temp == rev) {
-        result.push_back("True");
-    } else {
-        result.push_back("False");
+    string result_str = "";
+    for (auto i : temp) {
+        result_str += i;
     }
+    
+    bool is_palindrome = true;
+    int left = 0, right = result_str.length() - 1;
+    while (left < right) {
+        if (result_str[left] != result_str[right]) {
+            is_palindrome = false;
+            break;
+        }
+        left++;
+        right--;
+    }
+    
+    vector<string> result({result_str, (is_palindrome ? "True" : "False")});
     return result;
+}
