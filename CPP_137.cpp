d37a24d4a446cea3d10bcf74568e4f39673dc36c
beep boop <githubbot@vadim.me>
2024-07-07 11:10:08 +0200

    Solution 95 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6mhsqfmo Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,38 +1,57 @@
-if(a.type() == boost::any::typeless_type || a.type() == typeid(int) || a.type() == typeid(float)) {
-    if(b.type() == boost::any::typeless_type || b.type() == typeid(int) || b.type() == typeid(float)) {
-        boost::any max;
-        if(a.convert_to(max) && b.convert_to(max)) {
-            return max;
-        } else if(a.convert_to(max)) {
-            return max;
-        } else if(b.convert_to(max)) {
-            return max;
-        }
-    } else {
-        string a_str = a.convert_to<string>();
-        string b_str = b.convert_to<string>();
-        if(a_str.size() > 0 && (a_str[0] == '.' || a_str[0] == ',')) {
-            a_str = a_str.substr(1);
-        }
-        if(b_str.size() > 0 && (b_str[0] == '.' || b_str[0] == ',')) {
-            b_str = b_str.substr(1);
-        }
-        if(stof(a_str) > stof(b_str)) {
-            return a;
-        } else if(stof(b_str) > stof(a_str)) {
-            return b;
-        } else {
-            return "None";
-        }
+#include <string>
+#include <boost/any.hpp>
+
+using namespace std;
+
+boost::any compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return max(a, b);
     }
-} else {
-    string a_str = a.convert_to<string>();
-    string b_str = b.convert_to<string>();
-    if(stof(a_str) > stof(b_str)) {
-        return a;
-    } else if(stof(b_str) > stof(a_str)) {
-        return b;
-    } else {
-        return "None";
+    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        return max(a, b);
+    }
+    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return boost::any(b);
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        return max(a, b);
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if (((string)a) > ((string)b))
+            return a;
+        else
+            return b;
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        if (stoi((string)b) > (int)a)
+            return boost::any(b);
+        else if (stoi((string)b) < (int)a)
+            return a;
+        else
+            return boost::any("None");
+    }
+    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
+        if (stod((string)b) > (double)a)
+            return boost::any(b);
+        else if (stod((string)b) < (double)a)
+            return a;
+        else
+            return boost::any("None");
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        if ((int)b > (int)a)
+            return boost::any(b);
+        else if ((int)b < (int)a)
+            return a;
+        else
+            return boost::any("None");
+    }
+    else {
+        if ((double)a > (double)b)
+            return a;
+        else if ((double)a < (double)b)
+            return b;
+        else
+            return boost::any("None");
     }
 }
