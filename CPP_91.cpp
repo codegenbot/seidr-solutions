b97924e83dd74f7d8ae7923f6c5e5873177b00a3
beep boop <githubbot@vadim.me>
2024-07-07 07:08:52 +0200

    Solution 9 of CPP/91, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/og630ey8 Prompt: Complete the following code given the task description and function signature.

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,14 +1,20 @@
+#include <string>
+
+using namespace std;
+
 int is_bored(string S) {
     int count = 0;
     string boredom = "I";
+    
     size_t pos = 0;
-
-    while ((pos = S.find('.', pos)) != string::npos || (pos = S.find('!', pos)) != string::npos || (pos = S.find('?')) != string::npos) {
+    while ((pos = S.find('.', pos)) != string::npos || 
+           (pos = S.find('?', pos)) != string::npos || 
+           (pos = S.find('!', pos)) != string::npos) {
         if (S.substr(0, boredom.size()).compare(boredom) == 0) {
             count++;
         }
-        pos++;
+        pos = max(pos + 1, S.find('.', pos));
     }
-
+    
     return count;
 }
