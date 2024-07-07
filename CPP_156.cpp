af4371b56149a10e93efd66dc511d9c6a8aa6038
beep boop <githubbot@vadim.me>
2024-07-07 07:09:26 +0200

    Solution 44 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jdhym3pn Prompt: In file included from 619d491b-b2ba-4f90-ad4c-22ad1a649f07.cpp:52: 619d491b-b2ba-4f90-ad4c-22ad1a649f07.cpp: In function ‘int main()’: 619d491b-b2ba-4f90-ad4c-22ad1a649f07.cpp:54:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    54 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,3 +1,51 @@
+```
+#include <string>
+
+std::string toMiniRomanHelper(int num, std::string str, std::string roman) {
+    if (num == 0)
+        return str;
+    if (num >= 4000)
+        return "IV";
+    if (num >= 1000) {
+        str += roman[0];
+        return toMiniRomanHelper(num - 1000, str, roman);
+    }
+    if (num >= 900)
+        return "CM";
+    if (num >= 500) {
+        str += roman[0];
+        return toMiniRomanHelper(num - 500, str, roman.substr(1));
+    }
+    if (num >= 400)
+        return "CD";
+    if (num >= 100) {
+        str += roman[0];
+        return toMiniRomanHelper(num - 100, str, roman.substr(1));
+    }
+    if (num >= 90)
+        return "XC";
+    if (num >= 50) {
+        str += roman[0];
+        return toMiniRomanHelper(num - 50, str, roman.substr(1));
+    }
+    if (num >= 40)
+        return "XL";
+    if (num >= 10) {
+        str += roman[0];
+        return toMiniRomanHelper(num - 10, str, roman.substr(1));
+    }
+    if (num >= 9)
+        return "IX";
+    if (num >= 5) {
+        str += roman[0];
+        return toMiniRomanHelper(num - 5, str, roman.substr(1));
+    }
+    if (num >= 4)
+        return "IV";
+    str += roman[0];
+    return str + roman[1];
+}
+
 std::string int_to_mini_roman(int num) {
     return toMiniRomanHelper(num, "", "MDCLXVI");
 }
