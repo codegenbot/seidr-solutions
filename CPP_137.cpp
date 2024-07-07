362aea420d54e31b28853398dd63848b41550897
beep boop <githubbot@vadim.me>
2024-07-07 10:45:32 +0200

    Solution 31 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6mhsqfmo Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,39 +1,46 @@
+Here is the completed code:
+
+```cpp
 #include <boost/any.hpp>
 
-using namespace boost;
-
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_none(a) && is_none(b)) {
-        return boost::any("None");
-    }
-
-    if (!is_none(a)) {
-        if (!is_none(b)) {
-            if (get<double>(a) > get<double>(b))
-                return a;
-            else if (get<double>(a) < get<double>(b))
-                return b;
-            else
-                return boost::any("None");
-        } else {
-            if (get<int>(a) > 0)
-                return a;
-            else
-                return b;
-        }
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        int x = boost::any_cast<int>(a);
+        int y = boost::any_cast<int>(b);
+        return (x > y) ? a : ((x < y) ? b : boost::any("None"));
+    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        float x = boost::any_cast<float>(a);
+        float y = boost::any_cast<float>(b);
+        return (x > y) ? a : ((x < y) ? b : boost::any("None"));
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string x = boost::any_cast<string>(a);
+        string y = boost::any_cast<string>(b);
+        return (stof(x) > stof(y)) ? a : ((stof(x) < stof(y)) ? b : boost::any("None"));
+    } else if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        int x = boost::any_cast<int>(a);
+        float y = boost::any_cast<float>(b);
+        return (x > y) ? a : ((x < y) ? b : boost::any("None"));
+    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        int x = boost::any_cast<int>(a);
+        string y = boost::any_cast<string>(b);
+        return (x > stof(y)) ? a : ((x < stof(y)) ? b : boost::any("None"));
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        float x = boost::any_cast<float>(a);
+        int y = boost::any_cast<int>(b);
+        return (x > y) ? a : ((x < y) ? b : boost::any("None"));
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        float x = boost::any_cast<float>(a);
+        string y = boost::any_cast<string>(b);
+        return (x > stof(y)) ? a : ((x < stof(y)) ? b : boost::any("None"));
+    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        string x = boost::any_cast<string>(a);
+        int y = boost::any_cast<int>(b);
+        return (stof(x) > y) ? a : ((stof(x) < y) ? b : boost::any("None"));
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        string x = boost::any_cast<string>(a);
+        float y = boost::any_cast<float>(b);
+        return (stof(x) > y) ? a : ((stof(x) < y) ? b : boost::any("None"));
     } else {
-        if (!is_none(b)) {
-            if (get<int>(b) > 0)
-                return b;
-            else
-                return a;
-        } else {
-            if (get<string>(a) > get<string>(b))
-                return a;
-            else if (get<string>(a) < get<string>(b))
-                return b;
-            else
-                return boost::any("None");
-        }
+        return boost::any("None");
     }
 }
