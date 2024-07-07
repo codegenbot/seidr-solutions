fc8853e4b19fdf0eb6d81140afe127c9cced3e40
beep boop <githubbot@vadim.me>
2024-07-07 09:23:30 +0200

    Solution 67 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/sa305lxd Prompt: Complete the following code given the task description and function signature.

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -3,13 +3,11 @@ int smallest_change(vector<int> arr) {
     vector<vector<int>> dp(n, vector<int>(n));
     
     for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
+        for (int j = i; j >= 0; j--) {
             if (i == j)
                 dp[i][j] = 0;
-            else if (arr[i] == arr[j])
-                dp[i][j] = dp[i][j - 1];
             else
-                dp[i][j] = min(1 + dp[i + 1][j - 1], dp[i][j - 1]) + 1;
+                dp[j][i] = abs(arr[i] - arr[j]) + (dp[(j + 1)][(i - 1)]);
         }
     }
     
