dc0d0cd8839a75f6f2b2511a4310381b1c3b4e82
beep boop <githubbot@vadim.me>
2024-07-07 09:06:23 +0200

    Solution 35 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/u0n0gq9b Prompt:       |     ^~~       |     std::cin In file included from f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:2: /usr/include/c++/11/iostream:60:18: note: ‘std::cin’ declared here    60 |   extern istream cin;           /// Linked to standard input       |                  ^~~ f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:27:12: error: ‘s’ was not declared in t his scope    27 |     cin >> s;       |            ^ f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:28:5: error: ‘cout’ was not declared in  this scope; did you mean ‘std::cout’?    28 |     cout << (solveBoolean(s) ? "True" : "False");       |     ^~~~       |     std::cout In file included from f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:2: /usr/include/c++/11/iostream:61:18: note: ‘std::cout’ declared here    61 |   extern ostream cout;          /// Linked to standard output       |                  ^~~~ f6e1df8a-fedc-45df-a4b0-eb23e49c3430.cpp:28:26: error: ‘solveBoolean’ cannot be used as a function    28 |     cout << (solveBoolean(s) ? "True" : "False");       |              ~~~~~~~~~~~~^~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -2,37 +2,23 @@
 #include <string>
 
 bool solveBoolean(string s) {
-    bool res = true;
+    stack<char> st;
     for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'T') {
-            res = true;
-        } else if (s[i] == 'F') {
-            res = false;
-        } else if (s[i] == '|') {
-            bool temp = res;
-            res = !res;
-            res = res && temp;
-        } else if (s[i] == '&') {
-            bool temp = res;
-            res = !res;
-            while (res && i < s.length() - 1) {
-                if (s[i + 1] == 'T')
-                    return true;
-                else if (s[i + 1] == 'F')
-                    break;
-                else if (s[i + 1] == '&') {
-                    res = false;
-                    while (i < s.length() - 2 && s[i + 1] == '&') {
-                        i++;
-                    }
-                } else if (s[i + 1] == '|') {
-                    return true;
-                }
+        if (s[i] == '&') {
+            while (!st.empty() && st.top() == '&') {
+                st.pop();
             }
-            res = temp;
+            st.push('&');
+        } else if (s[i] == '|') {
+            while (!st.empty()) {
+                st.pop();
+            }
+            st.push('|');
+        } else {
+            st.push(s[i]);
         }
     }
-    return res;
+    return st.top() == 'T';
 }
 
 int main() {
