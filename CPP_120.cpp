799fbeeff6d8f47b4d901e8b24b1e32d1efcf284
beep boop <githubbot@vadim.me>
2024-07-07 07:39:54 +0200

    Solution 17 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/x19lori4 Prompt:       |                    ^~~ 8800238a-e729-4262-809f-32ce486fea71.cpp:8:27: error: ‘vector’ was not declared in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8800238a-e729-4262-809f-32ce486fea71.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8800238a-e729-4262-809f-32ce486fea71.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8800238a-e729-4262-809f-32ce486fea71.cpp:7: 8800238a-e729-4262-809f-32ce486fea71.cpp: In function ‘int main()’: 8800238a-e729-4262-809f-32ce486fea71.cpp:17:20: error: ‘maximum’ was not declare d in this scope    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 8800238a-e729-4262-809f-32ce486fea71.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 
 vector<int> maximum(vector<int> arr,int k){
-    vector<int> result(arr.begin(),arr.begin()+k);
+    vector<int> result(arr.begin(),arr.end());
     sort(result.begin(),result.end());
     return result;
 }
