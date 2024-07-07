0e1ea43de58e604c3a7a3269e2ac426f22141967
beep boop <githubbot@vadim.me>
2024-07-07 09:27:37 +0200

    Solution 91 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/kin9cpoq Prompt: Complete the following code given the task description and function signature.

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,12 +1,18 @@
 bool will_it_fly(vector<int> q, int w) {
-    vector<int> qCopy = q;
-    reverse(qCopy.begin(), qCopy.end());
-    if (q != qCopy)
-        return false;
-
+    string str = "";
+    for (int i : q) {
+        str += to_string(i);
+    }
+    if (!isPalindrome(str)) return false;
     int sum = 0;
     for (int i : q) {
         sum += i;
     }
     return sum <= w;
+}
+
+bool isPalindrome(string s) {
+    string rev = s;
+    reverse(rev.begin(), rev.end());
+    return s == rev;
 }
