f00550544a4172165cd692f2c9acd3677d0b1da9
beep boop <githubbot@vadim.me>
2024-07-07 08:34:20 +0200

    Solution 73 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jdhym3pn Prompt: aaf1dbb2-b329-4cbf-a7f6-8578f7271b8c.cpp:1:6: error: ‘string’ in namespace ‘std’  does not name a type     1 | std::string int_to_mini_roman(int num) {       |      ^~~~~~ aaf1dbb2-b329-4cbf-a7f6-8578f7271b8c.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | std::string int_to_mini_roman(int num) {

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,4 +1,37 @@
+#include <string>
+
 std::string int_to_mini_roman(int num) {
     if (num <= 0)
         return "";
     return toMiniRomanHelper(num, "", "MDC");
+}
+
+std::string toMiniRomanHelper(int num, std::string str, std::string roman) {
+    if (num == 0)
+        return str;
+    int val = getVal(roman);
+    if (num >= val) {
+        return toMiniRomanHelper(num - val, str + roman[0], roman.substr(1));
+    } else {
+        return toMiniRomanHelper(num, str, roman.substr(1));
+    }
+}
+
+int getVal(std::string s) {
+    int val = 0;
+    if (s == "M")
+        val = 1000;
+    else if (s == "D")
+        val = 500;
+    else if (s == "C")
+        val = 100;
+    else if (s == "L")
+        val = 50;
+    else if (s == "X")
+        val = 10;
+    else if (s == "V")
+        val = 5;
+    else if (s == "I")
+        val = 1;
+    return val;
+}
