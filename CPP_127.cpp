faf64f0e60f027cbe6690c95d2a142ddbe2a5168
beep boop <githubbot@vadim.me>
2024-07-07 06:53:32 +0200

    Solution 98 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/32lqhmr7 Prompt: 740d206c-bfea-4082-a304-0098300ab889.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 740d206c-bfea-4082-a304-0098300ab889.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 740d206c-bfea-4082-a304-0098300ab889.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 740d206c-bfea-4082-a304-0098300ab889.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -2,4 +2,4 @@ std::string intersection(std::pair<int, int> interval1, std::pair<int, int> inte
     if (interval1.first > interval2.second || interval2.first > interval1.second)
         return "no";
     else
-        return std::to_string(interval1.first) + " " + std::to_string(std::min({interval1.second, interval2.second}));
+        return std::to_string(std::max({interval1.first, interval2.first})) + " " + std::to_string(std::min({interval1.second, interval2.second}));
