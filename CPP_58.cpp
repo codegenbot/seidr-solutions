8083a3cd21d2bcf21aaede4b22fd890b6bb87850
beep boop <githubbot@vadim.me>
2024-07-07 03:16:03 +0200

    Solution 30 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/io7n5spc Prompt:       |             ^~~~~~ 34270094-f08f-4d56-a637-650cc6580c22.cpp:36:34: error: expected primary-expressi on before ‘int’    36 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 34270094-f08f-4d56-a637-650cc6580c22.cpp:36:39: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 34270094-f08f-4d56-a637-650cc6580c22.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ 34270094-f08f-4d56-a637-650cc6580c22.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 34270094-f08f-4d56-a637-650cc6580c22.cpp:35: 34270094-f08f-4d56-a637-650cc6580c22.cpp: In function ‘int main()’: 34270094-f08f-4d56-a637-650cc6580c22.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -3,28 +3,27 @@
 #include <set>
 #include <iostream>
 
-std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
-    std::set<int> set1(l1.begin(), l1.end());
-    std::set<int> set2(l2.begin(), l2.end());
-    std::vector<int> intersection;
-    intersection.insert(intersection.end(), std::set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(),
-        std::back_inserter(intersection)));
-    return intersection;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return (common(a, b).size() == 0) ? true : false;
 }
 
-int main() {
+int main(){
     std::vector<int> l1 = {1, 2, 3, 4};
     std::vector<int> l2 = {3, 4, 5, 6};
 
-    std::set<int> set1(l1.begin(), l1.end());
-    std::set<int> set2(l2.begin(), l2.end());
-
-    if(set2.size() == std::set_difference(set1.begin(), set1.end(), set2.begin(), set2.end(),
-        std::inserter(set2, set2.end())).size()) {
-        std::cout << "All elements of one list are present in another." << std::endl;
+    if(l1.size() > l2.size()) {
+        for(int i : l1) {
+            if(std::find(l2.begin(), l2.end(), i) == l2.end())
+                break;
+        }
     } else {
-        std::cout << "Not all elements of one list are present in another." << std::endl;
+        for(int i : l2) {
+            if(std::find(l1.begin(), l1.end(), i) == l1.end())
+                break;
+        }
     }
+
+    std::cout << "All elements of one list are present in another." << std::endl;
 
     return 0;
 }
