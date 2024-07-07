e4a636408c4fceb4aba4ab89dfd9b70e7441dfd0
beep boop <githubbot@vadim.me>
2024-07-07 08:04:07 +0200

    Solution 49 of CPP/129, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/j8xyzuq3 Prompt: 1e47e91e-fe8b-40f9-8bd9-1a586a60882c.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 1e47e91e-fe8b-40f9-8bd9-1a586a60882c.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,24 +1,19 @@
-vector<int> minPath(vector<vector<int>>& grid, int k){
-    int n = grid.size();
-    vector<vector<int>> dp(n, vector<int>(n));
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(i==0 && j==0) dp[i][j] = grid[i][j];
-            else{
-                int val = INT_MAX;
-                if(i>0) val = min(val, dp[i-1][j]);
-                if(j>0) val = min(val, dp[i][j-1]);
-                dp[i][j] = val + grid[i][j];
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> minPath(vector<vector<int>> grid, int k) {
+    vector<int> result;
+    for (int i = 0; i < k; i++) {
+        int maxRow = -1, maxValue = 0;
+        for (int j = 0; j < grid.size(); j++) {
+            if (grid[j][i] > maxValue) {
+                maxValue = grid[j][i];
+                maxRow = j;
             }
         }
+        result.push_back(maxValue);
+        grid[maxRow].erase(grid[maxRow].begin());
     }
-    vector<int> res;
-    int i = n-1, j = n-1;
-    for(int l=0; l<k; l++){
-        res.push_back(grid[i][j]);
-        if(i>0 && dp[i-1][j] < dp[i][j-1]) i--;
-        else j--;
-    }
-    reverse(res.begin(), res.end());
-    return res;
+    return result;
 }
