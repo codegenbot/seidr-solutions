694ffc4f37dbec13605c617979629a3aaf9dd17d
beep boop <githubbot@vadim.me>
2024-07-07 02:23:00 +0200

    Solution 31 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vcnvivvc Prompt: e63a3f09-da8e-47ed-9a90-998d5f2064ab.cpp:11:5: error: redefinition of ‘int main( )’    11 | int main(){       |     ^~~~ e63a3f09-da8e-47ed-9a90-998d5f2064ab.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ e63a3f09-da8e-47ed-9a90-998d5f2064ab.cpp: In function ‘int main()’: e63a3f09-da8e-47ed-9a90-998d5f2064ab.cpp:12:5: error: ‘vector’ was not declared in this scope    12 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ e63a3f09-da8e-47ed-9a90-998d5f2064ab.cpp:12:12: error: expected primary-expressi on before ‘float’    12 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~ In file included from e63a3f09-da8e-47ed-9a90-998d5f2064ab.cpp:10: e63a3f09-da8e-47ed-9a90-998d5f2064ab.cpp:13:13: error: ‘has_close_elements’ was not declared in this scope    13 |     assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false) ;       |             ^~~~~~~~~~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,10 +1,25 @@
-int main(); 
-{
-    double a[] = {1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
-    std::vector<double> b;
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool has_close_elements(vector<double>, double);
+
+int mainProblem() {
+    vector<double> a = {1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
+    vector<double> b;
     for (double num : a) {
         b.push_back(num);
     }
     assert(has_close_elements(b, 0.5) == false);
-    return 0;
+}
+
+bool has_close_elements(vector<double> v, double tol) {
+    bool result = true;
+    for(int i=1; i<v.size(); i++) {
+        if(abs(v[i] - v[i-1]) < tol) {
+            result = false;
+            break;
+        }
+    }
+    return result;
 }
