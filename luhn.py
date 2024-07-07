4f13b0b054ceae7ad17e40055b3a8cc0d3f496b3
beep boop <githubbot@vadim.me>
2024-07-07 08:19:40 +0200

    Solution 95 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/exzsclts Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1cc75808-7b9d-4dff-84cb-e76a02122 f45.py", line 1     return str(sum((i * 2 if i % 2 != 0 else i) if (i * 2) > 9 else (i * 2 - 9) for i in card) % 10)     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- luhn.py
+++ luhn.py
@@ -1,2 +1,2 @@
 def luhn(card):
-    return str(sum((int(i) * 2 if int(i) % 2 != 0 else int(i)) if (int(i) * 2) > 9 else (int(i) * 2 - 9) for i in card) % 10)
+    return str(sum((int(digit) * 2 if index % 2 != 0 else int(digit)) if (int(digit) * 2) > 9 else ((int(digit) * 2) - 9) for index, digit in enumerate(card[::-1])) % 10)
