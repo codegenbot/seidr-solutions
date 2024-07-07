6ce15180f94233d501f734a75abee4a65d53eda5
beep boop <githubbot@vadim.me>
2024-07-07 02:40:19 +0200

    Solution 36 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/g2m11uph Prompt:    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 115dacdd-243e-4d07-abce-56a15522dfa7.cpp:17: 115dacdd-243e-4d07-abce-56a15522dfa7.cpp: In function ‘int main()’: 115dacdd-243e-4d07-abce-56a15522dfa7.cpp:27:13: error: reference to ‘issame’ is ambiguous    27 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ^~~~~~ 115dacdd-243e-4d07-abce-56a15522dfa7.cpp:4:6: note: candidates are: ‘bool std::i ssame’     4 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 115dacdd-243e-4d07-abce-56a15522dfa7.cpp:18:6: note:                 ‘bool issam e’    18 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ In file included from 115dacdd-243e-4d07-abce-56a15522dfa7.cpp:17: 115dacdd-243e-4d07-abce-56a15522dfa7.cpp:27:20: error: ‘remove_duplicates’ was n ot declared in this scope    27 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,9 +1,7 @@
-#include <vector>
-#include <algorithm>
-
+```cpp
 using namespace std;
 
-bool myIssame(vector<int> a, vector<int> b) {
+bool issame(vector<int> a, vector<int> b) {
     return a.size() == b.size() && a == b;
 }
 
@@ -11,11 +9,6 @@ vector<int> remove_duplicates(vector<int> numbers) {
     vector<int> result(numbers.begin(), numbers.end());
     sort(result.begin(), result.end());
     auto it = unique(result.begin(), result.end());
-    it = erase(it, result.end()); 
-    return vector<int>(result.begin(), it);
-}
-
-int main() {
-    assert(myIssame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}));
-    return 0;
+    result.erase(it, result.end()); 
+    return result;
 }
