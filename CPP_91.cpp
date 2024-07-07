febf9266b6d5ea0a74f79ad3fbe79cd501d77003
beep boop <githubbot@vadim.me>
2024-07-07 03:57:18 +0200

    Solution 14 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/b0nazjdk Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,16 +1,29 @@
+```cpp
 #include <iostream>
 #include <string>
+
+int is_bored(std::string S) {
+    int count = 0;
+    std::string boredom = "I am bored";
+    size_t pos = S.find(boredom);
+    while (pos != std::string::npos) {
+        count++;
+        auto boredom_iter = S.begin() + pos;
+        while (*boredom_iter++ == boredom[0]) {
+            if (std::string(borrow_iter, boredom.length()) != boredom) break;
+            count++;
+            pos += boredom.length();
+            boredom_iter += boredom.length();
+        }
+    }
+    return count;
+}
 
 int main() {
     std::string input;
     std::cout << "Enter a string: ";
     std::getline(std::cin, input);
-    int count = 0;
-    size_t pos = 0;
-    while ((pos = input.find("I am bored", pos)) != std::string::npos) {
-        count++;
-        pos += 7; // The length of the substring "I am bored"
-    }
-    std::cout << "The count of 'I am bored' in your string is: " << count << std::endl;
+    int result = is_bored(input);
+    std::cout << "The count of 'I am bored' in your string is: " << result << std::endl;
     return 0;
 }
