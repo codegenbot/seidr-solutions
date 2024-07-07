9debc0449c54398137ef539a78741a4c1cf049b1
beep boop <githubbot@vadim.me>
2024-07-07 05:49:34 +0200

    Solution 34 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lxy8b22d Prompt: ined here    15 | int main() {       |     ^~~~ In file included from a3b8d60c-5934-4492-95b4-43dd7d899e11.cpp:23: a3b8d60c-5934-4492-95b4-43dd7d899e11.cpp: In function ‘int main()’: a3b8d60c-5934-4492-95b4-43dd7d899e11.cpp:25:13: error: ‘boost’ has not been decl ared    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ a3b8d60c-5934-4492-95b4-43dd7d899e11.cpp:25:35: error: expected primary-expressi on before ‘>’ token    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ a3b8d60c-5934-4492-95b4-43dd7d899e11.cpp:25:62: error: could not convert ‘1’ fro m ‘int’ to ‘std::string’ {aka ‘std::__cxx11::basic_string<char>’}    25 |    assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "None ");       |                                                             ^       |                                                             |       |                                                             int

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,17 +3,18 @@
 
 using namespace std;
 
-string compare_one(string s1, int s2) {
+string compare_one(string s1, string s2) {
     double d1 = stod(s1);
-    if (d1 > s2) return s1;
-    else if (d1 == s2) return "None";
-    else return to_string(s2);
+    double d2 = stod(s2);
+
+    if (d1 > d2) return s1;
+    else if (d1 == d2) return "None";
+    else return s2;
 }
 
 int main() {
-    cout << compare_one("123.45", 6789) << endl; 
-    string s1 = "1";  
-    int s2 = 2;  
+    cout << compare_one("123.45", "6789") << endl; 
+    string s1 = to_string(1), s2 = to_string(2);  
     if(compare_one(s1, s2) == s1) cout << s1 << " is greater than or equal to " << s2 << endl;
     else cout << s2 << " is greater than " << s1 << endl;
     return 0;
