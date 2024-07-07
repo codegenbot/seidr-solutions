7ff434035b2e2b5be4a39e053375b49d0dfd3e43
beep boop <githubbot@vadim.me>
2024-07-07 05:21:22 +0200

    Solution 21 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/w9gmztj5 Prompt: ned int; _Tp = std::vector<bool>; _Tp2 = std::vector<bool>]’ /usr/include/c++/11/bits/stl_vector.h:1596:33:   required from ‘void std::vector <_Tp, _Alloc>::_M_fill_initialize(std::vector<_Tp, _Alloc>::size_type, const val ue_type&) [with _Tp = std::vector<bool>; _Alloc = std::allocator<std::vector<boo l> >; std::vector<_Tp, _Alloc>::size_type = long unsigned int; std::vector<_Tp, _Alloc>::value_type = std::vector<bool>]’ /usr/include/c++/11/bits/stl_vector.h:525:9:   required from ‘std::vector<_Tp, _ Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const value_type&, const all ocator_type&) [with _Tp = std::vector<bool>; _Alloc = std::allocator<std::vector <bool> >; std::vector<_Tp, _Alloc>::size_type = long unsigned int; std::vector<_ Tp, _Alloc>::value_type = std::vector<bool>; std::vector<_Tp, _Alloc>::allocator _type = std::allocator<std::vector<bool> >]’ 8e278c80-6f25-497a-9a8d-082697e3c390.cpp:12:40:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -8,7 +8,8 @@ using namespace std;
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
     int n = grid.size();
-    vector<vector<bool>> visited(n, vector<bool>(n, false));
+    vector<bool> row(n, false);
+    vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
     vector<int> res;
 
