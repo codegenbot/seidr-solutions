558dd3ae2e508857f28a8ff0d356b03948ff8d87
beep boop <githubbot@vadim.me>
2024-07-07 05:34:05 +0200

    Solution 44 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/32lqhmr7 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -6,7 +6,7 @@ std::string intersection(std::pair<int, int> interval1, std::pair<int, int> inte
     int start = std::max(interval1.first, interval2.first);
     int end = std::min(interval1.second, interval2.second);
 
-    return (start <= end ? "YES" : "NO");
+    return start <= end ? "YES" : "NO";
 }
 
 int main() {
@@ -16,22 +16,26 @@ int main() {
     std::cout << "Enter the first interval's start: ";
     int temp;
     std::cin >> temp; 
-    interval1.first = temp;
+    std::string str1 = std::to_string(temp);
+    interval1.first = atoi(str1.c_str());
 
     std::cout << "Enter the first interval's end: ";
     std::cin >> temp; 
-    interval1.second = temp;
+    str1 = std::to_string(temp);
+    interval1.second = atoi(str1.c_str());
 
     std::pair<int, int> interval2;
     std::cout << "Enter the second interval's start: ";
     std::cin >> temp; 
-    interval2.first = temp;
+    str1 = std::to_string(temp);
+    interval2.first = atoi(str1.c_str());
 
     std::cout << "Enter the second interval's end: ";
     std::cin >> temp; 
-    interval2.second = temp;
+    str1 = std::to_string(temp);
+    interval2.second = atoi(str1.c_str());
 
-    if (intersection(interval1, interval2) == "YES")
+    if (std::string(intersection(interval1, interval2)) == "YES")
         std::cout << "The intervals intersect.\n";
     else
         std::cout << "The intervals do not intersect.\n";
