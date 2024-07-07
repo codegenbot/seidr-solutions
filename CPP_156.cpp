6afc05b679549be079490de829bc36e3ee7b2fd4
beep boop <githubbot@vadim.me>
2024-07-07 06:46:30 +0200

    Solution 33 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jdhym3pn Prompt: _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 74ddd436-be01-4ab0-958e-14702a43d0c3.cpp:6:16:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 74ddd436-be01-4ab0-958e-14702a43d0c3.cpp:6:16:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,56 +1,18 @@
-```cpp
-#include <string>
-
-std::string toMiniRoman(int num) {
-    if (num < 1 || num > 3999) {
-        return "Invalid input";
-    }
-    std::string roman = "";
-    int thousands = num / 1000;
-    for (int i = 0; i < thousands; i++) {
-        roman += std::string("M");
-    }
-    num %= 1000;
-    int hundreds = num / 100;
-    if (hundreds > 0) {
-        roman += toMiniRomanHelper(hundreds, "C", "D");
-    }
-    num %= 100;
-    int tens = num / 10;
-    if (tens > 3) {
-        roman += "CM";
-        num -= 900;
-    } else if (tens > 0) {
-        roman += toMiniRomanHelper(tens, "X", "L");
-    }
-    num %= 10;
-    int ones = num;
-    if (ones == 9) {
-        roman += "IX";
-    } else if (ones >= 5) {
-        roman += std::string(toMiniRomanHelper(ones, "V", "IV"));
-    } else {
-        for (int i = 0; i < ones; i++) {
-            roman += "I";
-        }
-    }
-    return roman;
-}
-
 std::string toMiniRomanHelper(int num, std::string s1, std::string s2) {
     if (num == 4) {
         return s2;
     } else if (num == 9) {
         return s1 + "I";
     } else {
+        std::string temp = "";
         for (int i = 0; i < num / 5; i++) {
-            roman += std::string(1, s1);
+            temp += s1[0];
         }
-        return toMiniRomanHelper(num % 5, s1, s2);
+        if (!temp.empty()) {
+            temp += toMiniRomanHelper(num % 5, "", s2);
+        } else {
+            temp += toMiniRomanHelper(num, s1, s2);
+        }
+        return temp;
     }
-}
-
-int main() {
-    assert(toMiniRoman(1000) == "M");
-    return 0;
 }
