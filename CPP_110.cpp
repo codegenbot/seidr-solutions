2d68c8d91ffd2721d657bbe766cc13c6b881d045
beep boop <githubbot@vadim.me>
2024-07-07 10:05:55 +0200

    Solution 52 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2ayxpfoe Prompt: Complete the following code given the task description and function signature.

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,24 +1,24 @@
+#include <vector>
+#include <set>
+using namespace std;
+
 string exchange(vector<int> lst1, vector<int> lst2) {
+    set<int> even;
+    for (int num : lst2) {
+        if (num % 2 == 0) {
+            even.insert(num);
+        }
+    }
+
     for (int num : lst1) {
-        if (num % 2 != 0 && !binary_search(lst2.begin(), lst2.end(), num)) {
+        if (even.count(num)) {
+            continue;
+        } else if (even.find(num) != even.end()) {
+            even.erase(even.find(num));
+        } else {
             return "NO";
         }
     }
+
     return "YES";
-}
-
-bool binary_search(vector<int>& vec, int target) {
-    int left = 0;
-    int right = vec.size() - 1;
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (vec[mid] == target)
-            return true;
-        else if (vec[mid] < target)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-    return false;
 }
