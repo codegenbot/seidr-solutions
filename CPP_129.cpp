d3fcd6a8aa2ce60f0bd564b565b7513f23fe34a0
beep boop <githubbot@vadim.me>
2024-07-07 05:08:08 +0200

    Solution 13 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/w9gmztj5 Prompt:  std::vector<_Tp, _Alloc>::iterator = std::vector<std::pair<int, std::pair<int, int> > >::iterator]’ /usr/include/c++/11/bits/vector.tcc:121:21:   required from ‘std::vector<_Tp, _A lloc>::reference std::vector<_Tp, _Alloc>::emplace_back(_Args&& ...) [with _Args  = {std::pair<int, std::pair<int, int> >}; _Tp = std::pair<int, std::pair<int, i nt> >; _Alloc = std::allocator<std::pair<int, std::pair<int, int> > >; std::vect or<_Tp, _Alloc>::reference = std::pair<int, std::pair<int, int> >&]’ /usr/include/c++/11/bits/stl_vector.h:1204:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(std::vector<_Tp, _Alloc>::value_type&&) [with _Tp = std ::pair<int, std::pair<int, int> >; _Alloc = std::allocator<std::pair<int, std::p air<int, int> > >; std::vector<_Tp, _Alloc>::value_type = std::pair<int, std::pa ir<int, int> >]’ /usr/include/c++/11/bits/stl_queue.h:650:13:   required from ‘void std::priority _queue<_Tp, _Sequence, _Compare>::push(std::priority_queue<_Tp, _Sequence, _Comp are>::value_type&&) [with _Tp = std::pair<int, std::pair<int, int> >; _Sequence = std::vector<std::pair<int, std::pair<int, int> > >; _Compare = std::less<std:: pair<int, std::pair<int, int> > >; std::priority_queue<_Tp, _Sequence, _Compare> ::value_type = std::pair<int, std::pair<int, int> >]’ ecb3bd8b-48bb-4082-b96c-d6edbe047335.cpp:18:24:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::pair<int, std::pair<int, int> > >’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,4 +1,4 @@
-```
+```cpp
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -9,13 +9,13 @@ using namespace std;
 vector<int> minPath(vector<vector<int>> grid, int k) {
     int n = grid.size();
     vector<vector<bool>> visited(n, vector<bool>(n));
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>> > pq;
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
     vector<int> res;
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             if (!visited[i][j]) {
-                pq.push({grid[i][j], {i, j}});
+                pq.push({grid[i][j], i, j});
                 visited[i][j] = true;
             }
         }
@@ -23,7 +23,7 @@ vector<int> minPath(vector<vector<int>> grid, int k) {
 
     while (!pq.empty()) {
         int val = pq.top().first;
-        pair<int, int> pos = pq.top().second;
+        pair<int,int> pos = {pq.top().second, pq.top().second};
         pq.pop();
 
         res.push_back(val);
@@ -31,6 +31,7 @@ vector<int> minPath(vector<vector<int>> grid, int k) {
         if (k > 0) {
             --k;
 
+            // Check all neighbors
             for (int i = -1; i <= 1; ++i) {
                 for (int j = -1; j <= 1; ++j) {
                     int ni = pos.first + i;
@@ -38,7 +39,7 @@ vector<int> minPath(vector<vector<int>> grid, int k) {
 
                     if (ni >= 0 && ni < n && nj >= 0 && nj < n && !visited[ni][nj]) {
                         visited[ni][nj] = true;
-                        pq.push({grid[ni][nj], {ni, nj}});
+                        pq.push({grid[ni][nj], ni, nj});
                     }
                 }
             }
