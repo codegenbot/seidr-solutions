e39f15953a393d5d98935cd7de68c4664e5b4faf
beep boop <githubbot@vadim.me>
2024-07-07 04:04:31 +0200

    Solution 14 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vzoz9gns Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9d7f257a-efcf-4876-9c40-87498b2f8ee6.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9d7f257a-efcf-4876-9c40-87498b2f8ee6.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9d7f257a-efcf-4876-9c40-87498b2f8ee6.cpp:19: 9d7f257a-efcf-4876-9c40-87498b2f8ee6.cpp: In function ‘int main()’: 9d7f257a-efcf-4876-9c40-87498b2f8ee6.cpp:29:20: error: ‘count_up_to’ was not dec lared in this scope    29 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 9d7f257a-efcf-4876-9c40-87498b2f8ee6.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,6 +1,33 @@
-#include <vector>
-#include <algorithm>
+bool isSame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
-bool equal(vector<int> a, vector<int> b) {
-    return a == b;
+vector<int> count_up_to(int n) {
+    vector<int> primes;
+    for (int i = 2; i <= n; ++i) {
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; ++j) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            primes.push_back(i);
+        }
+    }
+    return primes;
+}
+
+int main() {
+    assert(isSame(count_up_to(101), vector<int>({2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97})));
+    return 0;
 }
