a21e4d6472420fe029a79dc2f0ccc500868e947a
beep boop <githubbot@vadim.me>
2024-07-07 08:33:51 +0200

    Solution 9 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/85t3a2na Prompt: Complete the following code given the task description and function signature.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,11 +1,7 @@
 double find_zero(vector<double> xs){
     double sum = 0;
-    int i;
-    for (i=0;i<xs.size();i++)
-    {
-        if(i%2==1) // only consider odd powers of x
-            continue;
-        sum+=xs[i]/(double)i; // divide by the power to find the root
+    for(int i=1; i<xs.size();i++){
+        if(i%2==0)sum+=xs[i]/(double)i;
     }
-    return -sum; // since we are finding the root where poly(x)=0, it's the negative of what we calculated
+    return -sum;
 }
