d61e8443240e0d851b4abdb2be6f8bc77f6fcdbe
beep boop <githubbot@vadim.me>
2024-07-07 08:31:27 +0200

    Solution 76 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/kf4a2rzo Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = double]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_Tp1> >::allocate(std::allocator_traits<std::a llocator<_Tp1> >::allocator_type&, int) [with _Tp = double; std::allocator_trait s<std::allocator<_Tp1> >::pointer = double*; std::allocator_traits<std::allocato r<_Tp1> >::allocator_type = std::allocator<double>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = double; _Alloc = std::allocator<double>; std::_Vector_base<_Tp, _Alloc>:: pointer = double*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const double&}; _Tp = double; _Alloc = std::allocator<double>; std ::vector<_Tp, _Alloc>::iterator = std::vector<double>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = double; _Alloc = std::al locator<double>; std::vector<_Tp, _Alloc>::value_type = double]’ 05e6c03d-67ab-4370-9005-0caee6fd53a5.cpp:41:26:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<double>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -37,8 +37,8 @@ void calculateAndPrint() {
         std::cout << "Enter element " << i+1 << ": ";
         std::cin >> num;
 
-        if (num > 0.0 && std::fmod(num,2.0) != 0.0) { 
-            lst.push_back(num); 
+        if (num >= 0.0 && std::fmod(num,2.0) != 0.0) { 
+            lst.push_back(num);
         }
     }
 
