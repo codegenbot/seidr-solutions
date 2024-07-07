79a98551e7ebdf7d265c316eb3d7ac7451e82916
beep boop <githubbot@vadim.me>
2024-07-07 06:00:24 +0200

    Solution 67 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dwe5rvx9 Prompt: In file included from /usr/include/c++/11/vector:67,                  from 0636d6e4-c77f-4b62-afcc-86263f7435ae.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 0636d6e4-c77f-4b62-afcc-86263f7435ae.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 0636d6e4-c77f-4b62-afcc-86263f7435ae.cpp:42:34: error: expected primary-expressi on before ‘int’    42 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0636d6e4-c77f-4b62-afcc-86263f7435ae.cpp:42:39: error: expression list treated a s compound expression in initializer [-fpermissive]    42 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0636d6e4-c77f-4b62-afcc-86263f7435ae.cpp:41: 0636d6e4-c77f-4b62-afcc-86263f7435ae.cpp: In function ‘int main()’: 0636d6e4-c77f-4b62-afcc-86263f7435ae.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,14 +1,9 @@
-```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 bool compareVectors(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
+    return a == b;
 }
 
 std::vector<int> maximum(std::vector<int> arr, int k) {
