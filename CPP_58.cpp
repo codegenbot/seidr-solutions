3986b2714de7e32b807ef85a81c7303142e8a772
beep boop <githubbot@vadim.me>
2024-07-07 03:02:58 +0200

    Solution 18 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/io7n5spc Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 5adaedd8-717a-45df-82cf-0c8820adfc35.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -3,22 +3,23 @@
 #include <algorithm>
 #include <set>
 #include <iostream>
+using namespace std;
 
-std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
-    std::set<int> set1(l1.begin(), l1.end());
-    std::set<int> set2(l2.begin(), l2.end());
-    std::vector<int> intersection;
-    intersection.insert(intersection.end(), std::set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(),
-        std::back_inserter(intersection)));
+vector<int> common(vector<int> l1, vector<int> l2) {
+    set<int> set1(l1.begin(), l1.end());
+    set<int> set2(l2.begin(), l2.end());
+    vector<int> intersection;
+    intersection.insert(intersection.end(), set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(),
+        back_inserter(intersection)));
     return intersection;
 }
 
 int main() {
-    std::vector<int> l1 = {1, 2, 3, 4};
-    std::vector<int> l2 = {3, 4, 5, 6};
-    std::vector<int> result = common(l1, l2);
+    vector<int> l1 = vector<int>({1, 2, 3, 4});
+    vector<int> l2 = vector<int>({3, 4, 5, 6});
+    vector<int> result = common(l1, l2);
     for(int i : result) {
-        std::cout << i << " ";
+        cout << i << " ";
     }
     return 0;
 }
