cef9924aacd25fa260ddaa9681ace6cf7d4480da
beep boop <githubbot@vadim.me>
2024-07-07 10:32:19 +0200

    Solution 98 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/osmmpzaz Prompt: Complete the following code given the task description and function signature.

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,15 +1,20 @@
 int count_nums(vector<int> v) {
     int count = 0;
     for (int num : v) {
+        int sum = 0;
+        bool hasNegativeSign = false;
         if (num < 0) {
+            hasNegativeSign = true;
             num = -num;
         }
-        int sum = 0;
         while (num > 0) {
-            sum += num % 10;
+            int digit = num % 10;
+            if (digit != 0) {
+                sum += digit;
+            }
             num /= 10;
         }
-        if (sum > 0) {
+        if (!hasNegativeSign && sum > 0 || hasNegativeSign && sum >= 1) {
             count++;
         }
     }
