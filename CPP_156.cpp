24a15cc7c06bb20a434561a580b8dcd8056e3186
beep boop <githubbot@vadim.me>
2024-07-07 06:29:19 +0200

    Solution 25 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jdhym3pn Prompt: a02f84c4-38ed-47ce-9a74-6c93aae8d789.cpp:15:5: error: redefinition of ‘int main( )’    15 | int main(){       |     ^~~~ a02f84c4-38ed-47ce-9a74-6c93aae8d789.cpp:6:5: note: ‘int main()’ previously defi ned here     6 | int main() {       |     ^~~~ In file included from a02f84c4-38ed-47ce-9a74-6c93aae8d789.cpp:14: a02f84c4-38ed-47ce-9a74-6c93aae8d789.cpp: In function ‘int main()’: a02f84c4-38ed-47ce-9a74-6c93aae8d789.cpp:16:13: error: ‘int_to_mini_romank’ was not declared in this scope    16 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,41 +1,49 @@
 ```cpp
-#include <iostream>
-#include <string>
-
 std::string toMiniRoman(int num) {
-    if (num == 0)
-        return "0";
+    if (num < 1 || num > 3999) {
+        return "Invalid input";
+    }
     std::string roman = "";
     int thousands = num / 1000;
-    for (int i = 0; i < thousands; i++)
+    for (int i = 0; i < thousands; i++) {
         roman += "M";
+    }
     num %= 1000;
     int hundreds = num / 100;
-    if (hundreds > 3)
-        roman += "CM" + toMiniRoman(hundreds - 300);
-    else {
-        for (int i = 0; i < hundreds; i++)
-            roman += "C";
+    if (hundreds > 0) {
+        roman += toMiniRomanHelper(hundreds, "C", "D");
     }
     num %= 100;
     int tens = num / 10;
-    if (tens == 9)
-        roman += "IX";
-    else if (tens >= 5) {
-        roman += "D" + toMiniRoman(tens - 5);
-    } else {
-        for (int i = 0; i < tens; i++)
-            roman += "X";
+    if (tens > 3) {
+        roman += "CM";
+        num -= 900;
+    } else if (tens > 0) {
+        roman += toMiniRomanHelper(tens, "X", "L");
     }
     num %= 10;
     int ones = num;
-    if (ones == 9)
+    if (ones == 9) {
         roman += "IX";
-    else if (ones >= 5) {
-        roman += "V" + toMiniRoman(ones - 5);
+    } else if (ones >= 5) {
+        roman += toMiniRomanHelper(ones, "V", "IV");
     } else {
-        for (int i = 0; i < ones; i++)
+        for (int i = 0; i < ones; i++) {
             roman += "I";
+        }
     }
     return roman;
+}
+
+std::string toMiniRomanHelper(int num, std::string s1, std::string s2) {
+    if (num == 4) {
+        return s2;
+    } else if (num == 9) {
+        return s1 + "I";
+    } else {
+        for (int i = 0; i < num / 5; i++) {
+            roman += s1;
+        }
+        roman += toMiniRomanHelper(num % 5, s1, s2);
+    }
 }
