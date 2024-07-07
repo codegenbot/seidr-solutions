9ba22b0d4ac1a15685e87262dd8027c08c60222b
beep boop <githubbot@vadim.me>
2024-07-07 09:35:34 +0200

    Solution 14 of CPP/100, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/bwvv33vt Prompt: Complete the following code given the task description and function signature.

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,14 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 vector<int> make_a_pile(int n) {
     vector<int> pile;
     int stones = 1;
     for (int i = 0; i < n; i++) {
-        if ((i + 1) % 2 == 0) {
-            pile.push_back(stones);
+        if (i % 2 == 0) {
             stones += 2;
         } else {
-            pile.push_back(stones);
             stones += 1;
         }
+        pile.push_back(stones);
     }
     return pile;
 }
