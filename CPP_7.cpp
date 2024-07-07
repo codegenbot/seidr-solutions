c1ddb3e50e9ed51557f3d3d8bf7fd04dd3e3cc7b
beep boop <githubbot@vadim.me>
2024-07-07 06:12:06 +0200

    Solution 20 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zx302vzt Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 76896c53-ec7a-41b9-a49f-03ed27dccaa0.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 76896c53-ec7a-41b9-a49f-03ed27dccaa0.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 76896c53-ec7a-41b9-a49f-03ed27dccaa0.cpp:10: 76896c53-ec7a-41b9-a49f-03ed27dccaa0.cpp: In function ‘int main()’: 76896c53-ec7a-41b9-a49f-03ed27dccaa0.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 76896c53-ec7a-41b9-a49f-03ed27dccaa0.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,11 +1,12 @@
-#include <vector>
-#include <string>
-
-bool issame(vector<string> a,vector<string> b){
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-int main() {
-    assert(issame({filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run")}, {"grunt", "prune"}));
-    return 0;
+vector<string> filter_by_substring(vector<string> strings, string substring){
+    vector<string> result;
+    for(auto s : strings){
+        if(s.find(substring) != string::npos)
+            result.push_back(s);
+    }
+    return result;
 }
