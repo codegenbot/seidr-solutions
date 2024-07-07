3ab98af97dba918151dd8b8708884252b7bc3556
beep boop <githubbot@vadim.me>
2024-07-07 07:18:21 +0200

    Solution 84 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6bguuwt2 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = double]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = double; std::allocator_t raits<std::allocator<_CharT> >::pointer = double*; std::allocator_traits<std::al locator<_CharT> >::allocator_type = std::allocator<double>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = double; _Alloc = std::allocator<double>; std::_Vector_base<_Tp, _Alloc>:: pointer = double*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const double&}; _Tp = double; _Alloc = std::allocator<double>; std ::vector<_Tp, _Alloc>::iterator = std::vector<double>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = double; _Alloc = std::al locator<double>; std::vector<_Tp, _Alloc>::value_type = double]’ 572670eb-e4b3-4bdf-8239-c85fc2bf4207.cpp:21:20:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<double>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -18,9 +18,9 @@ int main() {
         return 1;
     }
     std::vector<double> input;
-    input.reserve(2); // Reserve space for 2 elements
-    input.push_back(num1);
-    input.push_back(num2);
+    input.resize(2);
+    input[0] = num1;
+    input[1] = num2;
     std::cout << "Sum of squares is: " << sum_squares(input) << std::endl;
     return 0;
 }
