f958021ed3bbcccdc6e3f389be0d5ecfba633ca9
beep boop <githubbot@vadim.me>
2024-07-07 17:21:13 +0200

    Solution 31 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/9on4t4oz Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 15796310-032f-4079-b10b-f387e28032cb.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,1 +1,35 @@
-```
+```#
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string camelCase(string s) {
+    string result = "";
+    for (int i = 0; i <= s.size(); i++) {
+        if (i == s.size() || s[i] == '-') {
+            if (result != "") {
+                result[0] = toupper(result[0]);
+            }
+            int j = i + 1;
+            while (j < s.size() && s[j] == '-') {
+                j++;
+            }
+            string word = s.substr(i, j - i);
+            result += word;
+            i = j;
+        } else if (s[i] != ' ') {
+            result += s[i];
+        }
+    }
+    return result;
+}
+
+int main() {
+    string input;
+    cout << "Enter a string in kebab-case: ";
+    cin >> input;
+    cout << "The camelCase conversion is: " << camelCase(input) << endl;
+    return 0;
+}
