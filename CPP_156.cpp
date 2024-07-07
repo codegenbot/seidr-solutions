17d57c6e869b32b6e865a444daf395dd920a4ad0
beep boop <githubbot@vadim.me>
2024-07-07 12:01:57 +0200

    Solution 80 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xiinw570 Prompt: Complete the following code given the task description and function signature.

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,37 +1,17 @@
 #include <vector>
-#include <string>
-
 using namespace std;
 
 string int_to_mini_roman(int number) {
-    vector<string> roman[1000] = {
-        {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
-        {"", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX"},
-        {"", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX"},
-        {"", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX"},
-        {"", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX"},
-        {"L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX"},
-        {"", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX"},
-        {"", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX"},
-        {"", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC"},
-        {"XC", "XCI", "XCII", "XCIII", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"}
-    };
-
-    string result = "";
-
-    while (number >= 1000) {
-        result += "M";
-        number -= 1000;
-    }
-
-    for (int i = 0; i < roman[number].size(); i++) {
-        if (i > 0 && roman[number][i] == roman[number - 1][i]) {
-            result += roman[number][i];
-        } else {
-            result += roman[number][i];
+    vector<pair<int, string>> romanMap = {{1000, "M"}, {900, "CM"}, {500, "D"}, 
+                                            {400, "CD"}, {100, "C"}, {90, "XC"},
+                                            {50, "L"}, {40, "XL"}, {10, "X"}, 
+                                            {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+    string roman = "";
+    for (const auto &p : romanMap) {
+        while (number >= p.first) {
+            roman += p.second;
+            number -= p.first;
         }
-        number -= roman[0][i+1].find(roman[number][i]) + 1;
     }
-
-    return result;
+    return roman;
 }
