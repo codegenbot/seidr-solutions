9fcb2d78475aaf5026f23dbf7af333e55ffa1cd5
beep boop <githubbot@vadim.me>
2024-07-07 05:48:30 +0200

    Solution 84 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3iwdkljd Prompt:       |                      ^~~~ 3fe86f52-28ee-4d6b-a952-b52b07a35897.cpp:45:10: error: invalid types ‘int[int]’ for array subscript    45 |     if (a[i]!=b[i]) return false;       |          ^ 3fe86f52-28ee-4d6b-a952-b52b07a35897.cpp:45:16: error: invalid types ‘int[int]’ for array subscript    45 |     if (a[i]!=b[i]) return false;       |                ^ In file included from 3fe86f52-28ee-4d6b-a952-b52b07a35897.cpp:40: 3fe86f52-28ee-4d6b-a952-b52b07a35897.cpp: In function ‘int main()’: 3fe86f52-28ee-4d6b-a952-b52b07a35897.cpp:50:35: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    50 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                                   ^~~~~~~       |                                   |       |                                   const char* 3fe86f52-28ee-4d6b-a952-b52b07a35897.cpp:50:35: error: cannot bind rvalue ‘(int) ((const char*)"mamma")’ to ‘int&’ 3fe86f52-28ee-4d6b-a952-b52b07a35897.cpp:9:47: note: in passing argument 1 of ‘i nt reverse_delete(int&)’     9 | vector<string> reverse_delete(vector<string>& v) {       |                               ~~~~~~~~~~~~~~~~^

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -6,14 +6,17 @@ struct vector<_Ty>::size_type { };
 
 using namespace std;
 
-vector<string> reverse_delete(vector<string>& v) {
-    for (int i = v.size() - 1; i >= 0; --i) {
-        if (!v[i].empty()) {
-            break;
+vector<string> reverse_delete(vector<string>& v1, vector<string>& v2) {
+    vector<string> result;
+    int minLength = min(v1.size(), v2.size());
+    for (int i = 0; i < minLength; ++i) {
+        if (!v1[i].empty() || !v2[i].empty()) {
+            result.push_back((v1[i] + " " + v2[i]).empty() ? "" : (v1[i] + " " + v2[i]));
+        } else {
+            result.push_back("");
         }
-        v.erase(v.begin() + i);
     }
-    return v;
+    return result;
 }
 
 bool issame(vector<string> b, vector<string> a) {
@@ -21,17 +24,17 @@ bool issame(vector<string> b, vector<string> a) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
-        if (a.at(i) != b.at(i)) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
     return true;
 }
 
-int main() {
+int mainFunction() {
     vector<string> v = {"mamma", "mia"};
     vector<string> temp = {"mamma", "mia"};
-    v = reverse_delete(temp);
+    v = reverse_delete(temp, v);
     vector<string> result = {{"", "True"}};
     assert(issame(result, v));
     return 0;
