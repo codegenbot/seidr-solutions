b4549438d9a88204b3717e47d4f390cac8719c94
beep boop <githubbot@vadim.me>
2024-07-07 03:27:52 +0200

    Solution 96 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/piua4g83 Prompt: ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = double; std::allocator_t raits<std::allocator<_CharT> >::pointer = double*; std::allocator_traits<std::al locator<_CharT> >::allocator_type = std::allocator<double>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = double; _Alloc = std::allocator<double>; std::_Vector_base<_Tp, _Alloc>:: pointer = double*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:361:33:   required from ‘void std::_Vector _base<_Tp, _Alloc>::_M_create_storage(size_t) [with _Tp = double; _Alloc = std:: allocator<double>; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:305:9:   required from ‘std::_Vector_base< _Tp, _Alloc>::_Vector_base(size_t, const allocator_type&) [with _Tp = double; _A lloc = std::allocator<double>; size_t = long unsigned int; std::_Vector_base<_Tp , _Alloc>::allocator_type = std::allocator<double>]’ /usr/include/c++/11/bits/stl_vector.h:555:61:   required from ‘std::vector<_Tp, _Alloc>::vector(const std::vector<_Tp, _Alloc>&) [with _Tp = double; _Alloc = st d::allocator<double>]’ 6f444dff-6367-4511-bf84-57d28a40c0eb.cpp:50:24:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<double>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -10,13 +10,13 @@ int main() {
     std::cout << "Enter the number of coefficients: ";
     std::cin >> ncoeff;
     
-    std::vector<double>(ncoeff, std::vector<double>::allocator_type()); 
+    std::vector<double> coeffs(ncoeff); 
 
     for(int i = 0; i < ncoeff; i++) {
         double coeff;
         std::cout << "Enter coefficient " << i+1 << ": ";
         std::cin >> coeff;
-        coeffs.push_back(coeff);
+        coeffs[i] = coeff;
    }
     
     double x, y;
