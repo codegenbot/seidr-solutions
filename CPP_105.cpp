b56a4488b31766ba74c79e89ec3baab109a07a8f
beep boop <githubbot@vadim.me>
2024-07-07 07:32:37 +0200

    Solution 21 of CPP/105, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/kj8nj2v2 Prompt:       |                              ^~~~~~ cba620d1-0aaf-40c4-b9e2-f594089fa0be.cpp:48:37: error: ‘string’ was not declared  in this scope    48 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ cba620d1-0aaf-40c4-b9e2-f594089fa0be.cpp:48:44: error: ‘b’ was not declared in t his scope    48 | bool issame(vector<string> a,vector<string>b){       |                                            ^ cba620d1-0aaf-40c4-b9e2-f594089fa0be.cpp:48:45: error: expression list treated a s compound expression in initializer [-fpermissive]    48 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from cba620d1-0aaf-40c4-b9e2-f594089fa0be.cpp:47: cba620d1-0aaf-40c4-b9e2-f594089fa0be.cpp: In function ‘int main()’: cba620d1-0aaf-40c4-b9e2-f594089fa0be.cpp:57:20: error: ‘by_length’ was not decla red in this scope    57 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ cba620d1-0aaf-40c4-b9e2-f594089fa0be.cpp:57:19: error: ‘issame’ cannot be used a s a function    57 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,39 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 vector<string> by_length(vector<int> arr) {
-    vector<string> result;
+    vector<int> sortedArr;
     for (int num : arr) {
-        string str;
+        if (num >= 1 && num <= 9) {
+            sortedArr.push_back(num);
+        }
+    }
+    
+    reverse(sortedArr.begin(), sortedArr.end());
+    
+    vector<string> result;
+    for (int num : sortedArr) {
         switch (num) {
             case 1:
-                str = "One";
+                result.push_back("One");
                 break;
             case 2:
-                str = "Two";
+                result.push_back("Two");
                 break;
             case 3:
-                str = "Three";
+                result.push_back("Three");
                 break;
             case 4:
-                str = "Four";
+                result.push_back("Four");
                 break;
             case 5:
-                str = "Five";
+                result.push_back("Five");
                 break;
             case 6:
-                str = "Six";
+                result.push_back("Six");
                 break;
             case 7:
-                str = "Seven";
+                result.push_back("Seven");
                 break;
             case 8:
-                str = "Eight";
+                result.push_back("Eight");
                 break;
             case 9:
-                str = "Nine";
+                result.push_back("Nine");
                 break;
         }
-        result.push_back(str);
     }
     
     return result;
