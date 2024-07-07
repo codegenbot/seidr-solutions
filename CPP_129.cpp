d2ee9a8a9f977910bba99422e08d1b433d9035b0
beep boop <githubbot@vadim.me>
2024-07-07 06:31:00 +0200

    Solution 70 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/w9gmztj5 Prompt:    93 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c13b89af-394f-4df5-a8fa-d877d4e22013.cpp:93:27: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? c13b89af-394f-4df5-a8fa-d877d4e22013.cpp:93:34: error: expected primary-expressi on before ‘int’    93 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c13b89af-394f-4df5-a8fa-d877d4e22013.cpp:101:5: error: redefinition of ‘int main ()’   101 | int main(){       |     ^~~~ c13b89af-394f-4df5-a8fa-d877d4e22013.cpp:75:5: note: ‘int main()’ previously def ined here    75 | int main()       |     ^~~~ In file included from c13b89af-394f-4df5-a8fa-d877d4e22013.cpp:92: c13b89af-394f-4df5-a8fa-d877d4e22013.cpp: In function ‘int main()’: c13b89af-394f-4df5-a8fa-d877d4e22013.cpp:102:20: error: ‘minPath’ was not declar ed in this scope   102 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -75,7 +75,48 @@ vector<int> minPath(vector<vector<int>> grid, int k) {
     return res;
 }
 
-#include <queue>
+vector<int> minPath(vector<vector<int>> grid, int k) {
+    int n = grid.size();
+    priority_queue<pair_int, vector<pair_int>, greater<pair_int>> pq; 
+    vector<int> res;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            pair_int p = {grid[i][j], i, j};
+            pq.push(p);
+        }
+    }
+
+    while (!pq.empty()) {
+        int val = pq.top().val;
+        int pos_i = pq.top().i;
+        int pos_j = pq.top().j;
+        pq.pop();
+
+        res.push_back(val);
+
+        if (k > 0) {
+            --k;
+
+            // Check all neighbors
+            for (int i = -1; i <= 1; ++i) {
+                for (int j = -1; j <= 1; ++j) {
+                    int ni = pos_i + i;
+                    int nj = pos_j + j;
+
+                    if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                        pair_int p = {grid[ni][nj], ni, nj};
+                        pq.push(p);
+                    }
+                }
+            }
+        } else {
+            break;
+        }
+    }
+
+    return res;
+}
 
 int main() 
 {
