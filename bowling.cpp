436057b3e71936520748e693186fb02e7aa8b938
beep boop <githubbot@vadim.me>
2024-07-07 03:25:31 +0200

    Solution 51 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/x8mq9nzp Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,4 +1,3 @@
-```cpp
 int bowlingScore(string s) {
     int score = 0;
     int currentRoll = 0;
@@ -6,8 +5,8 @@ int bowlingScore(string s) {
 
     for(int i = 0; i < s.length(); i++) {
         char c = s[i];
-        if(c == '/') {
-            if(currentRoll + max(0, min(10 - rollCount, currentRoll)) > 10)
+        if (c == '/') {
+            if (currentRoll + max(0, min(10 - rollCount, currentRoll)) > 10)
                 score += 10 + max(0, currentRoll - 10);
             else
                 score += currentRoll + max(0, min(10 - rollCount, currentRoll));
@@ -19,7 +18,7 @@ int bowlingScore(string s) {
         }
     }
 
-    if(currentRoll > 0) score += currentRoll;
+    if (currentRoll > 0) score += currentRoll;
 
     return score;
 }
