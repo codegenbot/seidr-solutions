698f6144a4e1dde5ef9170d2ebf406938a6ecbbf
beep boop <githubbot@vadim.me>
2024-07-07 10:06:51 +0200

    Solution 53 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2ayxpfoe Prompt: Complete the following code given the task description and function signature.

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,24 +1,9 @@
-#include <vector>
-#include <set>
-using namespace std;
-
 string exchange(vector<int> lst1, vector<int> lst2) {
-    set<int> even;
-    for (int num : lst2) {
-        if (num % 2 == 0) {
-            even.insert(num);
-        }
-    }
-
+    int oddCount = 0;
     for (int num : lst1) {
-        if (even.count(num)) {
-            continue;
-        } else if (even.find(num) != even.end()) {
-            even.erase(even.find(num));
-        } else {
-            return "NO";
+        if (num % 2 != 0) {
+            oddCount++;
         }
     }
-
-    return "YES";
+    return oddCount == 0 ? "YES" : "NO";
 }
