9dc85937e510ad5aaf7cb4838aa505e8750364b9
beep boop <githubbot@vadim.me>
2024-07-07 10:02:38 +0200

    Solution 47 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2ayxpfoe Prompt: Complete the following code given the task description and function signature.

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,17 +1,14 @@
+#include <vector>
+#include <set>
+using namespace std;
+
 string exchange(vector<int> lst1, vector<int> lst2) {
-    for (int num : lst1) {
-        if (num % 2 != 0) return "NO";
-    }
-    for (int i = 0; i < lst2.size(); i++) {
-        int found = 0;
-        for (int j = 0; j < lst1.size(); j++) {
-            if (lst2[i] == lst1[j]) {
-                found = 1;
-                break;
-            }
+    set<int> s(lst1.begin(), lst1.end());
+    for (int num : lst2) {
+        if (s.find(num) != s.end()) {
+            s.erase(s.find(num));
+            return "YES";
         }
-        if (found) continue;
-        if (lst2[i] % 2 != 0) return "NO";
     }
-    return "YES";
+    return "NO";
 }
