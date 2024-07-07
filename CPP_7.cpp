9545206bc1e4065bc2c57c9a036c9d486badc9c8
beep boop <githubbot@vadim.me>
2024-07-07 03:04:43 +0200

    Solution 72 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/oqfi9zp7 Prompt:       |                                             ^ f4247192-dfe7-4d6c-97f6-cd987c1d5574.cpp:32:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    32 | int main(){       |         ^~ f4247192-dfe7-4d6c-97f6-cd987c1d5574.cpp:32:9: note: remove parentheses to defau lt-initialize a variable    32 | int main(){       |         ^~       |         -- f4247192-dfe7-4d6c-97f6-cd987c1d5574.cpp:32:9: note: or replace parentheses with  braces to value-initialize a variable f4247192-dfe7-4d6c-97f6-cd987c1d5574.cpp:32:11: error: a function-definition is not allowed here before ‘{’ token    32 | int main(){       |           ^ f4247192-dfe7-4d6c-97f6-cd987c1d5574.cpp:34:2: error: expected ‘}’ at end of inp ut    34 | }       |  ^ f4247192-dfe7-4d6c-97f6-cd987c1d5574.cpp:18:12: note: to match this ‘{’    18 | int main() {       |            ^

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,9 +1,6 @@
-int main() {
+int main 
+{ 
     std::vector<std::string> str = {"grunt", "trumpet", "prune", "gruesome"};
-    std::vector<std::string> result = filterBySubstring(str, "run");
-    for (const auto& s : result) {
-        if (!issame({s}, {"grunt", "prune"})) {
-            return 1;
-        }
-    }
+    assert(issame(filterBySubstring(str, "run"), {"grunt", "prune"}));
+    return 0;
 }
