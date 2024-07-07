b015c0550358c91154249dcb208c52046660539e
beep boop <githubbot@vadim.me>
2024-07-07 10:06:31 +0200

    Solution 65 of CPP/92, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/v604ap9j Prompt: Complete the following code given the task description and function signature.

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,8 +1,7 @@
 bool any_int(float a, float b, float c) {
-    if (a == static_cast<int>(a) && b == static_cast<int>(b) && c == static_cast<int>(c)) {
-        if (a == b + c || b == a + c || c == a + b) {
-            return true;
-        }
-    }
-    return false;
+    int ai = (int)a;
+    int bi = (int)b;
+    int ci = (int)c;
+
+    return (ai == bi + ci) || (bi == ai + ci) || (ci == ai + bi);
 }
