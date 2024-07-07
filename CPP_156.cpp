075b95edc3eca19adf7554bf55e4fd736344c46a
beep boop <githubbot@vadim.me>
2024-07-07 08:52:50 +0200

    Solution 95 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zearthqu Prompt: eb5c1ee7-869b-4d6a-a96c-447864b17478.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<pair<int, string>> roman_numerals = {{1000, "M"}, {900, "CM"}, {5 00, "D"}, {400, "CD"},       | ^~~~~~ eb5c1ee7-869b-4d6a-a96c-447864b17478.cpp:5:1: error: ‘string’ does not name a ty pe     5 | string int_to_mini_roman(int number) {       | ^~~~~~ In file included from eb5c1ee7-869b-4d6a-a96c-447864b17478.cpp:16: eb5c1ee7-869b-4d6a-a96c-447864b17478.cpp: In function ‘int main()’: eb5c1ee7-869b-4d6a-a96c-447864b17478.cpp:18:13: error: ‘int_to_mini_romank’ was not declared in this scope    18 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,5 +1,9 @@
 using namespace std;
 
+#include <iostream>
+#include <vector>
+#include <string>
+
 vector<pair<int, string>> roman_numerals = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
                                               {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
                                               {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
