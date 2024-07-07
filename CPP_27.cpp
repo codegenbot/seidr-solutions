fe710941b43908cfd38b4c99537eb56f5baa6331
beep boop <githubbot@vadim.me>
2024-07-07 08:43:36 +0200

    Solution 52 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ii9y1ycb Prompt: Complete the following code given the task description and function signature.

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,8 +1,23 @@
-for(int i=0; i<str.length(); i++){
-    if(str[i] >= 'a' && str[i] <= 'z'){
-        str[i] = toupper(str[i]);
+char temp;
+for(int i=0; i<str.length();i++){
+    if(islower(str[i])){
+        temp=str[i];
+        for(int j='a';j<='z';j++){
+            if(j==temp+32)break;
+            else if(j==temp-32){
+                str.replace(i,1,&'A');
+                break;
+            }
+        }
     }else{
-        str[i] = tolower(str[i]);
+        temp=str[i];
+        for(int j='A';j<='Z';j++){
+            if(j==temp+32)break;
+            else if(j==temp-32){
+                str.replace(i,1,&'a');
+                break;
+            }
+        }
     }
 }
 return str;
