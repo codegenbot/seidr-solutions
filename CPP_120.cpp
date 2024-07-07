c80ea5c96fef356c8403bfdcc23813f5549f5e42
beep boop <githubbot@vadim.me>
2024-07-07 05:22:14 +0200

    Solution 40 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dwe5rvx9 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ a25d8747-a98d-4c61-bd3b-849acaeb1380.cpp:19:25:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -15,14 +15,10 @@ std::vector<int> maximum(std::vector<int> arr, int k) {
     std::vector<int> arrCopy = arr; 
     std::sort(arrCopy.begin(), arrCopy.end());
     std::vector<int> result;
-    for (int i = 0; i < k && i < arrCopy.size(); i++) {
-        if (arrCopy.size() > 1) {
-            result.push_back(arrCopy[arrCopy.size() - 1 - i]);
-            arrCopy.pop_back();
-        } else {
-            result.push_back(arrCopy[0]);
-            break;
-        }
+    if (k > arrCopy.size()) k = arrCopy.size();
+    for (int i = 0; i < k; i++) {
+        result.push_back(arrCopy[arrCopy.size() - 1 - i]);
+        arrCopy.pop_back();
     }
     return result;
 }
