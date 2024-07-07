b6cabaff1e5177b9197cb93ecd5aa066ee32c635
beep boop <githubbot@vadim.me>
2024-07-07 07:34:19 +0200

    Solution 3 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/v0gzscpn Prompt: Complete the following code given the task description and function signature.

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,21 +1,3 @@
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 vector<int> sort_array(vector<int> arr) {
-    vector<int> result = arr;
-    sort(result.begin(), result.end(),
-        [](int a, int b) {
-            if (count(binary(a).begin(), binary(a).end(), '1') <
-                count(binary(b).begin(), binary(b).end(), '1')) {
-                return true;
-            }
-            if (count(binary(a).begin(), binary(a).end(), '1') ==
-                count(binary(b).begin(), binary(b).end(), '1')) {
-                return a < b;
-            }
-            return false;
-        });
-    return result;
+    return vector<int>(arr.begin(), arr.end());
 }
