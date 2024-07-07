fa7e2c316a008a23d2cedc0e1b96eaf0eedf46c4
beep boop <githubbot@vadim.me>
2024-07-07 07:07:49 +0200

    Solution 43 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r44jutti Prompt:       |                    ^~~ 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp:17: 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp: In function ‘int main()’: 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp:27:20: error: ‘pluck’ was not declared in this scope    27 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 3bb4cd30-b916-4e9b-a2dd-1f6fbffc9aa1.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -9,3 +9,26 @@ int main() {
     assert(issame(pluck({7, 9, 7, 1}) , {}));
     return 0;
 }
+
+std::vector<int> pluck(std::vector<int> arr) {
+    vector<pair<int, int>> nodes;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] % 2 == 0) {
+            nodes.emplace_back(arr[i], i);
+        }
+    }
+    if (nodes.empty()) {
+        return {};
+    }
+    sort(nodes.begin(), nodes.end());
+    vector<int> result;
+    for(int i = 1; i < nodes.size(); i++) {
+        if(nodes[i-1].first != nodes[i].first) {
+            result.push_back(nodes[i-1].second);
+            break;
+        } else if(i == nodes.size() - 1) {
+            result.push_back(nodes.back().second);
+        }
+    }
+    return result;
+}
