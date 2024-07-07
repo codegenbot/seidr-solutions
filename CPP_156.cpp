89a546a63677be3a37141f8e37b642023932f871
beep boop <githubbot@vadim.me>
2024-07-07 08:27:26 +0200

    Solution 36 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/zearthqu Prompt: 5aeeca1e-df83-46f8-aed7-b64b8e8219a3.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int k) {       | ^~~~~~ In file included from 5aeeca1e-df83-46f8-aed7-b64b8e8219a3.cpp:14: 5aeeca1e-df83-46f8-aed7-b64b8e8219a3.cpp: In function ‘int main()’: 5aeeca1e-df83-46f8-aed7-b64b8e8219a3.cpp:16:13: error: ‘int_to_mini_romank’ was not declared in this scope    16 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 string int_to_roman(int k) {
     vector<pair<int, string>> romanMap = {{1000,"M"},{900,"CM"},{500,"D"},{400,"CD"},{100,"C"},{90,"XC"},
                                          {50,"L"},{40,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}};
