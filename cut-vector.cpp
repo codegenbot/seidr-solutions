3c734609930d580dd698c3757bff7c91534e43ae
beep boop <githubbot@vadim.me>
2024-07-08 00:17:56 +0200

    Solution 9 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/c4nqmnc9 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,63 +1,18 @@
 #include <vector>
-#include <iostream>
-
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int> vec) {
+pair<vector<int>, vector<int>> cutVector(vector<int> nums) {
     int min_diff = INT_MAX;
-    int cut_index = 0;
-
-    for (int i = 1; i <= vec.size() - 1; i++) {
-        int left_sum = 0, right_sum = 0;
-        for (int j = 0; j < i; j++) {
-            left_sum += vec[j];
-        }
-        for (int j = i; j < vec.size(); j++) {
-            right_sum += vec[j];
-        }
-
-        int diff = abs(left_sum - right_sum);
-        if (diff < min_diff) {
+    int split_index = 0;
+    
+    for (int i = 1; i < nums.size(); i++) {
+        int diff = nums[i] - nums[0];
+        if (diff <= min_diff) {
             min_diff = diff;
-            cut_index = i;
+            split_index = i;
         }
     }
-
-    vector<vector<int>> result(2);
-    for (int i = 0; i < cut_index; i++) {
-        result[0].push_back(vec[i]);
-    }
-    for (int i = cut_index; i < vec.size(); i++) {
-        result[1].push_back(vec[i]);
-    }
-
-    return result;
-}
-
-int main() {
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) {
-        cout << "Enter element " << i + 1 << ": ";
-        cin >> vec[i];
-    }
-
-    vector<vector<int>> result = cutVector(vec);
-
-    cout << "Left subvector: ";
-    for (int num : result[0]) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    cout << "Right subvector: ";
-    for (int num : result[1]) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    return 0;
+    
+    return {vector<int>(nums.begin(), nums.begin() + split_index), 
+            vector<int>(nums.begin() + split_index, nums.end())};
 }
