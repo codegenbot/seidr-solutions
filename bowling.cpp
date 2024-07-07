6cbf7b32fee28183aaad32bc75bae9545b274940
beep boop <githubbot@vadim.me>
2024-07-07 04:35:26 +0200

    Solution 56 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/x8mq9nzp Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- bowling.cpp
+++ bowling.cpp
@@ -4,39 +4,23 @@ using namespace std;
 
 int bowlingScore(string s) {
     int score = 0;
-    int currentRoll = 0;
-    int rollCount = 0;
-
-    for (char c : s) {
-        if (c == '/') {
-            if (rollCount < 9) {
-                score += max(10, currentRoll);
-            } else {
-                score += currentRoll + max(10, currentRoll);
-            }
-            currentRoll = 0;
-            rollCount++;
-        } else if (isdigit(c)) {
-            currentRoll *= 10;
-            currentRoll += c - '0';
+    string rolls[] = {"0", "1/2", "3-4", "5/6", "7", "8/9"};
+    for (string roll : rolls) {
+        if (roll.find('/') != string::npos) {
+            int firstRoll = stoi(roll.substr(0, roll.find('/')));
+            int secondRoll = 10 - firstRoll;
+            score += firstRoll + secondRoll;
+        } else if (isdigit(roll[0])) {
+            int frameScore = stoi(roll);
+            score += frameScore;
         }
     }
-
-    if (rollCount == 10) {
-        score += max(10, currentRoll) + max(10, currentRoll);
-    } else if (rollCount == 9 && currentRoll > 8) {
-        score += max(10, currentRoll) * 2;
-    } else {
-        score += currentRoll;
-    }
-
     return score;
 }
 
 int main() {
-    string s;
-    cout << "Enter the bowling scores: ";
-    cin >> s;
-    cout << "The total score is: " << bowlingScore(s);
+    string s = "1/2 3-4 5/6 /7 8/9";
+    int score = bowlingScore(s);
+    cout << "Score: " << score << endl;
     return 0;
 }
