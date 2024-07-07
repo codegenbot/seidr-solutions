12bd5fcaefa188aed5cb35f04a98d0940b3ea07c
beep boop <githubbot@vadim.me>
2024-07-07 03:54:20 +0200

    Solution 98 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/rhdzj1hd Prompt:       |             ^~~~~~ ae601b74-8221-44fa-9a77-f31a5418980c.cpp:38:36: error: expected primary-expressi on before ‘float’    38 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ ae601b74-8221-44fa-9a77-f31a5418980c.cpp:38:43: error: expression list treated a s compound expression in initializer [-fpermissive]    38 | bool issame(vector<float> a,vector<float>b){       |                                           ^ ae601b74-8221-44fa-9a77-f31a5418980c.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ ae601b74-8221-44fa-9a77-f31a5418980c.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from ae601b74-8221-44fa-9a77-f31a5418980c.cpp:37: ae601b74-8221-44fa-9a77-f31a5418980c.cpp: In function ‘int main()’: ae601b74-8221-44fa-9a77-f31a5418980c.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -32,7 +32,5 @@ int main() {
     float maxV1 = get_positive(v1);
     float maxV2 = get_positive(v2);
 
-    bool areSetsTheSame = same(v1, v2);
-
-    std::cout << "The two sets of numbers are " << (v1.empty() ? "empty" : (areSetsTheSame ? "" : "not ")) << " the same.\n";
+    std::cout << "The two sets of numbers are " << (v1.empty() ? "empty" : (same(v1, v2) ? "" : "not ")) << " the same.\n";
 }
