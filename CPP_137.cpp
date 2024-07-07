e5a0c212eddd3aa9ed2f16d5bdc8876430f81c06
beep boop <githubbot@vadim.me>
2024-07-07 06:03:33 +0200

    Solution 47 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/lxy8b22d Prompt: 54c3da00-a44e-4d24-9162-ee7c274ab149.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ 54c3da00-a44e-4d24-9162-ee7c274ab149.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from 54c3da00-a44e-4d24-9162-ee7c274ab149.cpp:23: 54c3da00-a44e-4d24-9162-ee7c274ab149.cpp: In function ‘int main()’: 54c3da00-a44e-4d24-9162-ee7c274ab149.cpp:25:13: error: ‘boost’ has not been decl ared    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 54c3da00-a44e-4d24-9162-ee7c274ab149.cpp:25:35: error: expected primary-expressi on before ‘>’ token    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -12,15 +12,10 @@ string compare_one(string s1, int s2) {
 }
 
 int main() {
-    cout << "Enter the string: ";
-    string s1; cin >> s1;
-    cout << "Enter an integer: ";
-    int s2; cin >> s2;
-
-    cout << compare_one(s1, s2) << endl; 
-
+    cout << compare_one("123.45", 6789) << endl; 
+    string s1 = "1";  
+    int s2 = 2;  
     if(compare_one(s1, s2) == s1) cout << s1 << " is greater than or equal to " << s2 << endl;
     else cout << s2 << " is greater than " << s1 << endl;
-
     return 0;
 }
