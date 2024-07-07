c64660ba7e9ddcebe229542f9946fe3104f019b7
beep boop <githubbot@vadim.me>
2024-07-07 04:54:28 +0200

    Solution 58 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tfufwup9 Prompt: 81b0978b-846a-4e20-b578-2ca7d0bf4f94.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 81b0978b-846a-4e20-b578-2ca7d0bf4f94.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 81b0978b-846a-4e20-b578-2ca7d0bf4f94.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 81b0978b-846a-4e20-b578-2ca7d0bf4f94.cpp:84:5: error: redefinition of ‘int main( )’    84 | int main(){       |     ^~~~ 81b0978b-846a-4e20-b578-2ca7d0bf4f94.cpp:56:5: note: ‘int main()’ previously def ined here    56 | int main() {       |     ^~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,3 +1,4 @@
+```
 #include <iostream>
 #include <vector>
 #include <map>
@@ -6,8 +7,7 @@
 using namespace std;
 
 vector<string> by_length(vector<int> arr) {
-    vector<string> output;
-    map<int, int> lengthCount;
+    vector<int> nums;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
             string str = to_string(num);
@@ -19,11 +19,13 @@ vector<string> by_length(vector<int> arr) {
             else if (str == "6") str = "Six";
             else if (str == "7") str = "Seven";
             else if (str == "8") str = "Eight";
-            else output.push_back("Nine");
+            else if (str == "9") str = "Nine";
+            nums.push_back(stoi(str));
         }
     }
 
-    for (int num : arr) {
+    map<int, int> lengthCount;
+    for (int num : nums) {
         int len = to_string(num).length();
         if (lengthCount.find(len) != lengthCount.end()) {
             lengthCount[len]++;
@@ -32,15 +34,19 @@ vector<string> by_length(vector<int> arr) {
         }
     }
 
+    vector<string> output;
     for (auto it = lengthCount.rbegin(); it != lengthCount.rend(); ++it) {
         while (it->second > 0) {
-            for(int i=1; i<=9; i++) {
-                if(to_string(i).length() == it->first) {
-                    output.push_back((i==1?"One":(i==2?"Two":(i==3?"Three):(i==4?"Four":"Five"))));
-                    it->second--;
-                    if(it->second == 0) break;
-                }
-            }
+            if(it->first == 1) output.push_back("One");
+            else if(it->first == 2) output.push_back("Two");
+            else if(it->first == 3) output.push_back("Three");
+            else if(it->first == 4) output.push_back("Four");
+            else if(it->first == 5) output.push_back("Five");
+            else if(it->first == 6) output.push_back("Six");
+            else if(it->first == 7) output.push_back("Seven");
+            else if(it->first == 8) output.push_back("Eight");
+            else output.push_back("Nine");
+            it->second--;
         }
     }
 
