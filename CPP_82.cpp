5e43cf15a46d757d802fe0b173d2b063046560d5
beep boop <githubbot@vadim.me>
2024-07-07 07:09:53 +0200

    Solution 23 of CPP/82, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7nicx7d9 Prompt: ca0d31db-6763-40ea-afd3-f0c3d885fed7.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from ca0d31db-6763-40ea-afd3-f0c3d885fed7.cpp:11: ca0d31db-6763-40ea-afd3-f0c3d885fed7.cpp: In function ‘int main()’: ca0d31db-6763-40ea-afd3-f0c3d885fed7.cpp:13:13: error: ‘prime_length’ was not de clared in this scope    13 |     assert (prime_length("0") == false);       |             ^~~~~~~~~~~~

--- CPP_82.cpp
+++ CPP_82.cpp
@@ -1,7 +1,6 @@
-bool prime_length(string str){
-    int len = str.length();
+bool prime_length(string str){ 
+    int len = str.length(); 
     for(int i=2; i*i<=len; i++){
         if(len%i==0) return false;
     }
     return true;
-}
