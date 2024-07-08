0b1fadc1f438fb57081c813c85859a1c73efffe5
beep boop <githubbot@vadim.me>
2024-07-09 01:33:54 +0200

    Solution 6 of CPP/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/vmm4yi2z Prompt:    23 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 7f6d82af-24f8-4bcb-a4c8-888c1aec4754.cpp:23:44: error: ‘b’ was not declared in t his scope    23 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7f6d82af-24f8-4bcb-a4c8-888c1aec4754.cpp:23:45: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 7f6d82af-24f8-4bcb-a4c8-888c1aec4754.cpp:22: 7f6d82af-24f8-4bcb-a4c8-888c1aec4754.cpp: In function ‘int main()’: 7f6d82af-24f8-4bcb-a4c8-888c1aec4754.cpp:32:20: error: ‘words_string’ was not de clared in this scope    32 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |                    ^~~~~~~~~~~~ 7f6d82af-24f8-4bcb-a4c8-888c1aec4754.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~

--- CPP_101.cpp
+++ CPP_101.cpp
@@ -1,11 +1,10 @@
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(vector<string> a,vector<string> b) {
+    return (a.size() == b.size()) && all_of(a.begin(), a.end(), [&](string s) { 
+        auto it = find_if(b.begin(),b.end(),[&](string t) { return s==t; }); 
+        return it != b.end(); }) ; 
+} 
+
+int main() {
+    assert(issame(words_string("ahmed     , gamal") , {"ahmed", "gamal"}));
+    return 0;
 }
