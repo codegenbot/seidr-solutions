b0708725f7e8f223c34a5fcef653386aa11acaee
beep boop <githubbot@vadim.me>
2024-07-09 01:47:29 +0200

    Solution 21 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/w213mml2 Prompt:       |     ^~~~~~ 6a07078d-4f3c-482d-90dd-386e592efabe.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | using namespace std; 6a07078d-4f3c-482d-90dd-386e592efabe.cpp: At global scope: 6a07078d-4f3c-482d-90dd-386e592efabe.cpp:35:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    35 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 6a07078d-4f3c-482d-90dd-386e592efabe.cpp:4:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     4 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 6a07078d-4f3c-482d-90dd-386e592efabe.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 6a07078d-4f3c-482d-90dd-386e592efabe.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -19,14 +19,11 @@ vector<int> minPath(vector<vector<int>> grid, int k) {
     for (int i = 0; i < grid.size(); ++i) {
         for (int j = 0; j < grid[0].size(); ++j) {
             if (grid[i][j] == k) {
-                for (int x = 0; x < grid.size(); ++x) {
-                    for (int y = 0; y < grid[0].size(); ++y) {
-                        if (grid[x][y] == k && (i != x || j != y)) {
-                            result.push_back(i);
-                            result.push_back(j);
-                            return minPath({{1, 3}, {3, 2}}, 10);
-                        }
-                    }
+                while(i + 1 < grid.size() && j + 1 < grid[0].size()) {
+                    i++;
+                    j++;
+                    result.push_back(i);
+                    result.push_back(j);
                 }
             }
         }
@@ -35,6 +32,6 @@ vector<int> minPath(vector<vector<int>> grid, int k) {
 }
 
 int main() {
-    assert(issame(minPath({{1, 3}, {3, 2}}, 10), vector<int>(10,1));
+    assert(issame(minPath({{1, 3}, {3, 2}}, 10), vector<int>({1, 1, 1, 1, 1, 1, 1, 1, 1, 1}));
     return 0;
 }
