2f7c5371c9d929fc62f023df552ff0b641036294
beep boop <githubbot@vadim.me>
2024-07-09 00:31:28 +0200

    Solution 30 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/gewsiiq5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/102456a5-df40-4e4c-bf39-cd141a5b7 404.py", line 15, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/102456a5-df40-4e4c-bf39-cd141a5b7 404.py", line 12, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/102456a5-df40-4e4c-bf39-cd141a5b7 404.py", line 6, in make_palindrome     while len(string) > 0 and not is_palindrome(prefix + string[0]):                                   ^^^^^^^^^^^^^ NameError: name 'is_palindrome' is not defined. Did you mean: 'make_palindrome'?

--- Python_10.py
+++ Python_10.py
@@ -1,12 +1,14 @@
-```
-def make_palindrome(string: str) -> str:
-    if len(string) == 0:
-        return ""
-    prefix = ""
-    while len(string) > 1 and not is_palindrome(prefix + string[0] + string[-1]):
-        prefix += string[0]
-        string = string[1:-1]
-    return prefix + string[0] + string[-1] + prefix[::-1]
-
 def is_palindrome(s: str) -> bool:
     return s == s[::-1]
+
+
+def make_palindrome(string: str) -> str:
+    if not string:
+        return ""
+
+    prefix = ""
+    while len(string) > 0 and not is_palindrome(prefix + string[0]):
+        prefix += string[0]
+        string = string[:-1] if string[0] == string[-1] else string[1:]
+
+    return prefix + string + prefix[::-1]
