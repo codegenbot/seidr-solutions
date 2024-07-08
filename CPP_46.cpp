183a32133682d74bb8f4a82a1fd1dcb8aa59c93d
beep boop <githubbot@vadim.me>
2024-07-09 00:22:16 +0200

    Solution 6 of CPP/46, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/8iwckk56 Prompt: 7f571a7b-4ebb-4ee4-a932-a148501e34fe.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 7f571a7b-4ebb-4ee4-a932-a148501e34fe.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_46.cpp
+++ CPP_46.cpp
@@ -1,26 +1,23 @@
-```
 #include <stdio.h>
-
 using namespace std;
 
 int fib4(int n) {
-    if (n <= 1)
-        return 0;
-    int a = 0, b = 2, c = 0, d = 2;
-    for (int i = 3; i <= n; i++) {
-        int temp = a + b + c + d;
-        a = b;
-        b = c;
-        c = d;
-        d = temp;
+    if (n <= 3) return 0;
+    int a = 2, b = 0, c = 0, d = 0;
+    for (int i = 4; i <= n; i++) {
+        int temp = a + b + c;
+        d = c;
+        c = b;
+        b = temp;
+        a = d;
     }
-    return d;
+    return a;
 }
 
 int main() {
     int n;
-    printf("Enter the value of n: ");
+    printf("Enter the number: ");
     scanf("%d", &n);
-    printf("The %d-th element of the fib4 number sequence is: %d\n", n, fib4(n));
+    printf("The %d-th element of the fib4 sequence is: %d\n", n, fib4(n));
     return 0;
 }
