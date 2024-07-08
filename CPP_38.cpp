f6c1afe8c645e55b04e4c8c0fb1c28cbab166408
beep boop <githubbot@vadim.me>
2024-07-09 00:54:05 +0200

    Solution 63 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/qlvqiwhq Prompt:                  from d89cb490-8174-458d-9251-89f7d66d4e61.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ d89cb490-8174-458d-9251-89f7d66d4e61.cpp:52:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    52 |             str+=chr;       |             ^~~       |             std d89cb490-8174-458d-9251-89f7d66d4e61.cpp:55:15: error: expected ‘;’ before ‘enco ded_str’    55 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from d89cb490-8174-458d-9251-89f7d66d4e61.cpp:42: d89cb490-8174-458d-9251-89f7d66d4e61.cpp:56:31: error: ‘encoded_str’ was not dec lared in this scope    56 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ d89cb490-8174-458d-9251-89f7d66d4e61.cpp:56:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    56 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -2,11 +2,29 @@
 #include <string> // Include the <string> library
 
 std::string encode_cyclic(std::string s) {
-    // Function remains the same
+    int l = s.length();
+    std::string encoded_str;
+    for (int i = 0; i * 3 < l; i++) {
+        std::string x = s.substr(i * 3, 3);
+        if (x.length() == 3) {
+            x = x[1] + x[2] + x[0];
+        }
+        encoded_str.append(x);
+    }
+    return encoded_str;
 }
 
 std::string decode_cyclic(std::string s) {
-    // Function remains the same
+    int l = s.length();
+    std::string decoded_str;
+    for (int i = 0; i * 3 < l; i++) {
+        std::string x = s.substr(i * 3, 3);
+        if (x.length() == 3) {
+            x = x[2] + x[0] + x[1];
+        }
+        decoded_str.append(x);
+    }
+    return decoded_str;
 }
 
 int main() {
