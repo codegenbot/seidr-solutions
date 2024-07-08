84ddd4a3ad3dcda7057babcad823516b529c1e89
beep boop <githubbot@vadim.me>
2024-07-09 00:42:52 +0200

    Solution 57 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nrmo6kgd Prompt: In file included from /usr/include/c++/11/cassert:44,                  from 62ffb931-87c0-47b4-a87d-0cf867ff075e.cpp:4: 62ffb931-87c0-47b4-a87d-0cf867ff075e.cpp:10:1: error: expected unqualified-id be fore ‘static_cast’    10 | assert(my_strlen(std::string("asdasnakj")) == 13);       | ^~~~~~ 62ffb931-87c0-47b4-a87d-0cf867ff075e.cpp:10:1: error: expected ‘)’ before ‘stati c_cast’    10 | assert(my_strlen(std::string("asdasnakj")) == 13);       | ^~~~~~ 62ffb931-87c0-47b4-a87d-0cf867ff075e.cpp:11:6: error: ‘cout’ in namespace ‘std’ does not name a type    11 | std::cout << "The length of 'asdasnakj' is: " << my_strlen(std::string(" asdasnakj")) << "\n";       |      ^~~~ In file included from 62ffb931-87c0-47b4-a87d-0cf867ff075e.cpp:1: /usr/include/c++/11/iostream:61:18: note: ‘std::cout’ declared here    61 |   extern ostream cout;          /// Linked to standard output       |                  ^~~~ 62ffb931-87c0-47b4-a87d-0cf867ff075e.cpp:12:1: error: expected unqualified-id be fore ‘return’    12 | return 0;       | ^~~~~~

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -6,7 +6,7 @@ int my_strlen(std::string str) {
 }
 
 int main() {
-    assert(my_strlen(std::string("asdasnakj")) == 13);
-    std::cout << "The length of 'asdasnakj' is: " << my_strlen(std::string("asdasnakj")) << "\n";
+    assert(my_strlen("asdasnakj") == 13);
+    std::cout << "The length of 'asdasnakj' is: " << my_strlen("asdasnakj") << "\n";
     return 0;
 }
