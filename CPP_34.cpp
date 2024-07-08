b197df5a97061a788cdc8e93f4f70a84b548cba4
beep boop <githubbot@vadim.me>
2024-07-09 00:42:42 +0200

    Solution 37 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/p3mcjouk Prompt:       |                                  ^~~ dce26940-44cc-4d32-8301-fae21caa57a5.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ dce26940-44cc-4d32-8301-fae21caa57a5.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~ In file included from dce26940-44cc-4d32-8301-fae21caa57a5.cpp:17: dce26940-44cc-4d32-8301-fae21caa57a5.cpp: In function ‘int main()’: dce26940-44cc-4d32-8301-fae21caa57a5.cpp:27:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    27 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from dce26940-44cc-4d32-8301-fae21caa57a5.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -9,8 +9,8 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 
 int main() {
     auto v = std::vector<int>({5, 3, 5, 2, 3, 3, 9, 0, 123});
-    v.erase(std::unique(v.begin(), v.end()), v.end());
-    assert(issame(v, {5, 3, 2, 9, 0, 123}));
-
+    v.erase(std::std::unique(v.begin(), v.end()), v.end());
+    assert(issame(v, {5, 3, 2 ,9, 0, 123}));
+    
     return 0;
 }
