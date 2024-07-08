1f3df8ab3eef2488060fb7c3fdc78e35a1434c70
beep boop <githubbot@vadim.me>
2024-07-09 01:03:34 +0200

    Solution 93 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dib3n468 Prompt:       |                                 ^~~~~~ In file included from 9352ae4a-f7b2-4885-b6e6-0872573e4214.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 9352ae4a-f7b2-4885-b6e6-0872573e4214.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 9352ae4a-f7b2-4885-b6e6-0872573e4214.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 9352ae4a-f7b2-4885-b6e6-0872573e4214.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 9352ae4a-f7b2-4885-b6e6-0872573e4214.cpp:27: 9352ae4a-f7b2-4885-b6e6-0872573e4214.cpp: In function ‘int main()’: 9352ae4a-f7b2-4885-b6e6-0872573e4214.cpp:37:20: error: ‘all_prefixes’ was not de clared in this scope    37 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -2,10 +2,10 @@
 #include <string>
 
 bool isSame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    if (a.size() != b.size())
+    if(a.size() != b.size())
         return false;
     for(int i = 0; i < a.size(); i++)
-        if (a[i] != b[i])
+        if(a[i] != b[i])
             return false;
     return true;
 }
