a2c748c292e5a43bf052d4803425642560f073bc
beep boop <githubbot@vadim.me>
2024-07-09 01:01:57 +0200

    Solution 94 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/p3mcjouk Prompt:       |                                  ^~~ 16b4e150-78d3-46e8-a2b2-a729f960739f.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 16b4e150-78d3-46e8-a2b2-a729f960739f.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~ In file included from 16b4e150-78d3-46e8-a2b2-a729f960739f.cpp:24: 16b4e150-78d3-46e8-a2b2-a729f960739f.cpp: In function ‘int main()’: 16b4e150-78d3-46e8-a2b2-a729f960739f.cpp:34:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    34 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 16b4e150-78d3-46e8-a2b2-a729f960739f.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -4,19 +4,21 @@
 #include <cassert>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return std::is_permutation(a.begin(), a.end(), b.begin(), b.end());
+    return a == b;
 }
 
-int main() {
-    std::vector<int> a = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    std::vector<int> b = {2, 3, 5, 9, 123, 0};
+std::vector<int> a = {5, 3, 5, 2, 3, 3, 9, 0, 123};
+std::vector<int> b = {2, 3, 5, 9, 123, 0};
 
-    std::vector<int> c(a);
-    std::sort(c.begin(), c.end());
-    std::sort(b.begin(), b.end());
+std::vector<int> c(a);
+std::sort(c.begin(), c.end());
+std::sort(b.begin(), b.end());
 
-    assert(issame(b, c));
-    assert(issame(std::vector<int>(std::make_move_iterator(a.begin()), std::make_move_iterator(a.end())), std::vector<int>{0, 2, 3, 5, 9, 123}));
+assert(issame(c, b));
+assert(issame(std::vector<int>(std::make_move_iterator(a.begin()), std::make_move_iterator(std::unique(a.begin(), a.end()))), std::vector<int>{0, 2, 3, 5, 9, 123}));
 
-    return 0;
-}
+std::vector<int> c(a);
+std::sort(c.begin(), c.end());
+std::sort(b.begin(), b.end());
+
+assert(issame(c, b));
