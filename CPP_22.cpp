7b31745e3a022af5b4a90f164245dc869b97a674
beep boop <githubbot@vadim.me>
2024-07-09 00:17:31 +0200

    Solution 5 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/80df1iq3 Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 92b0c6d9-709f-473f-aedb-690d8e874ad8.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 92b0c6d9-709f-473f-aedb-690d8e874ad8.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 92b0c6d9-709f-473f-aedb-690d8e874ad8.cpp:11: 92b0c6d9-709f-473f-aedb-690d8e874ad8.cpp: In function ‘int main()’: 92b0c6d9-709f-473f-aedb-690d8e874ad8.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 92b0c6d9-709f-473f-aedb-690d8e874ad8.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,7 +1,17 @@
 bool issame(vector<int> a, vector<int> b) {
-    if(a.size()!=b.size())return false;
-    for(int i=0;i<a.size();i++){
-        if(a[i]!=b[i]) return false;
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) return false;
     }
     return true;
+}
+
+vector<int> filter_integers(list_any values){
+    vector<int> result;
+    for (const auto& value : values) {
+        boost::any_cast<int>(value, 0);
+        if (!boost::any_cast<bool>(value)) continue;
+        result.push_back(boost::any_cast<int>(value));
+    }
+    return result;
 }
