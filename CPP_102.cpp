e40c5a6a3cf70328af0e09b88dc0fd99bf40ed19
beep boop <githubbot@vadim.me>
2024-07-09 01:15:45 +0200

    Solution 26 of CPP/102, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/einmeurw Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_102.cpp
+++ CPP_102.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
 int choose_num(int x, int y) {
-    if (x % 2 == 0 && y >= x)
-        return y;
-    else if (y % 2 != 0 || x > y)
-        return -1;
-    else if (x % 2 == 0)
-        return x;
-    else 
-        for (int i = x; i <= y; i++) {
-            if (i % 2 == 0) 
-                return i;
-        }
-    return -1;
+    string result = "";
+    for (int i = x; i <= y; i++) {
+        if (i % 2 == 0)
+            result += to_string(i) + " ";
+    }
+    return stoi(result);
 }
 
 int main() {
