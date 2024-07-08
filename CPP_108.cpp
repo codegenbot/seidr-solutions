c23b28ed8ca6200cb9dd6bceb955332e5f4f814e
beep boop <githubbot@vadim.me>
2024-07-09 01:54:44 +0200

    Solution 35 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5hsg63q3 Prompt: bb22f926-1bb9-48b5-8a95-80cd4074ae82.cpp: In function ‘int main()’: bb22f926-1bb9-48b5-8a95-80cd4074ae82.cpp:33:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    33 | int main(){       |         ^~ bb22f926-1bb9-48b5-8a95-80cd4074ae82.cpp:33:9: note: remove parentheses to defau lt-initialize a variable    33 | int main(){       |         ^~       |         -- bb22f926-1bb9-48b5-8a95-80cd4074ae82.cpp:33:9: note: or replace parentheses with  braces to value-initialize a variable bb22f926-1bb9-48b5-8a95-80cd4074ae82.cpp:33:11: error: a function-definition is not allowed here before ‘{’ token    33 | int main(){       |           ^ bb22f926-1bb9-48b5-8a95-80cd4074ae82.cpp:35:2: error: expected ‘}’ at end of inp ut    35 | }       |  ^ bb22f926-1bb9-48b5-8a95-80cd4074ae82.cpp:27:12: note: to match this ‘{’    27 | int main() {       |            ^

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,20 +1,21 @@
-```
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int count_nums(vector<int> nums) {
     int sign = 1;
     int count = 0;
     for (int num : nums) {
+        bool negativeFound = false;
         if(num >= 0)
             sign = 1;
         else
-            sign = -1;
+            sign = -1, negativeFound = true;
 
-        while(abs(num) > 0) {
+        while(abs(num) > 0 || negativeFound) {
             int digit = abs(num) % 10 * sign;
-            if(digit > 0 || (digit == 1 && sign == 1))
+            if(digit > 0 || (digit == 1 && !negativeFound))
                 count++;
             num /= 10;
         }
