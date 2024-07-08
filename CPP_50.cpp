be61ab29967fd04863d7d4246b70a5edf5ebce3f
beep boop <githubbot@vadim.me>
2024-07-09 00:20:06 +0200

    Solution 1 of CPP/50, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zen6ocmy Prompt: Complete the following code given the task description and function signature.

--- CPP_50.cpp
+++ CPP_50.cpp
@@ -4,20 +4,18 @@ using namespace std;
 
 string encode_shift(string s) {
     string out;
-    int i;
-    for (i=0;i<s.length();i++) {
-        int w=((int)s[i]+5-(int)'a')%26+(int)'a';   
-        out=out+(char)w;
+    for (int i = 0; i < s.length(); i++) {
+        int w = ((int)s[i] + 5 - (int)'a') % 26 + (int)'a';
+        out += (char)w;
     }
     return out;
 }
 
 string decode_shift(string s) {
     string out;
-    int i;
-    for (i=0;i<s.length();i++) {
-        int w(((int)s[i])+'a'-5)%26+(int)'a';
-        out=out+(char)w;
+    for (int i = 0; i < s.length(); i++) {
+        int w = ((int)s[i] - (int)'a' + 26) % 26 + (int)'a';
+        out += (char)w;
     }
     return out;
 }
