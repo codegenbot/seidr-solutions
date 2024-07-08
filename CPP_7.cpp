2d30236a5b6bfa0f898c533269b92c8b49fdc0da
beep boop <githubbot@vadim.me>
2024-07-09 00:27:17 +0200

    Solution 34 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9s6po2bb Prompt:     7 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 7ae47df6-ab52-4bb9-a290-90ef1c987131.cpp:15:10: error: ‘int main()’ redeclared a s different kind of entity    15 | int main(){       |          ^ 7ae47df6-ab52-4bb9-a290-90ef1c987131.cpp:1:5: note: previous declaration ‘int ma in’     1 | int main {       |     ^~~~ In file included from 7ae47df6-ab52-4bb9-a290-90ef1c987131.cpp:6: 7ae47df6-ab52-4bb9-a290-90ef1c987131.cpp: In function ‘int main()’: 7ae47df6-ab52-4bb9-a290-90ef1c987131.cpp:16:20: error: ‘filter_by_substring’ was  not declared in this scope    16 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 7ae47df6-ab52-4bb9-a290-90ef1c987131.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,33 +1,29 @@
 #include <vector>
 #include <string>
 
-std::vector<std::string> filter_by_substring(std::vector<std::string> strings, std::string substring) {
-    std::vector<std::string> result;
-    for(auto str : strings){
-        if(str.find(substring) != std::string::npos){
-            result.push_back(str);
-        }
+bool issame(std::vector<std::string> v1, std::vector<std::string> v2) {
+    if(v1.size() != v2.size())
+        return false;
+    
+    for(int i=0; i<v1.size(); i++) {
+        if(v1[i] != v2[i])
+            return false;
     }
+    
+    return true;
+}
+
+std::vector<std::string> filter_by_substring(std::vector<std::string> vec, std::string sub) {
+    std::vector<std::string> result;
+    for(const auto& str : vec) {
+        if(str.find(sub) != std::string::npos)
+            result.push_back(str);
+    }
+    
     return result;
 }
 
-bool is_same(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    bool flag = true;
-    size_t min_len = std::min(a.size(), b.size());
-    for(size_t i = 0; i < min_len; i++){
-        if(!is_same_helper(a[i], b[i])){
-            flag = false;
-            break;
-        }
-    }
-    return flag;
-}
-
-bool is_same_helper(const std::string& a, const std::string& b) {
-    return a == b;
-}
-
 int main() {
-    assert(is_same({filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run")}, {"grunt", "prune"}));
+    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
     return 0;
 }
