402d70f91a85bdef54978b23efb73e4a9a7e397e
beep boop <githubbot@vadim.me>
2024-07-09 01:55:15 +0200

    Solution 30 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c6pooyd5 Prompt:       |              ^~~~~~ 0d1beb51-f60a-4e2e-b19d-ee9c8c0fd4ec.cpp:1:21: error: expected primary-expressio n before ‘int’     1 | int exchange(vector<int> lst1, vector<int> lst2) {       |                     ^~~ 0d1beb51-f60a-4e2e-b19d-ee9c8c0fd4ec.cpp:1:32: error: ‘vector’ was not declared in this scope     1 | int exchange(vector<int> lst1, vector<int> lst2) {       |                                ^~~~~~ 0d1beb51-f60a-4e2e-b19d-ee9c8c0fd4ec.cpp:1:39: error: expected primary-expressio n before ‘int’     1 | int exchange(vector<int> lst1, vector<int> lst2) {       |                                       ^~~ 0d1beb51-f60a-4e2e-b19d-ee9c8c0fd4ec.cpp:1:48: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int exchange(vector<int> lst1, vector<int> lst2) {       |                                                ^ In file included from 0d1beb51-f60a-4e2e-b19d-ee9c8c0fd4ec.cpp:18: 0d1beb51-f60a-4e2e-b19d-ee9c8c0fd4ec.cpp: In function ‘int main()’: 0d1beb51-f60a-4e2e-b19d-ee9c8c0fd4ec.cpp:20:21: error: ‘exchange’ cannot be used  as a function    20 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,5 +1,3 @@
-```
-#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -7,10 +5,11 @@ int exchange(vector<int> lst1, vector<int> lst2) {
     for (int num : lst1) {
         if (num % 2 != 0) {
             bool found = false;
-            for (int i = lst2.size() - 1; !found && i >= 0; --i) {
-                if (lst2[i] % 2 == 0) {
-                    swap(lst1.back(), lst2[i]);
+            for (int num2 : lst2) {
+                if (num2 % 2 == 0) {
+                    swap(lst1.back(), lst2.back());
                     found = true;
+                    break;
                 }
             }
             if (!found) return 0;
@@ -20,8 +19,6 @@ int exchange(vector<int> lst1, vector<int> lst2) {
 }
 
 int main() {
-    vector<int> lst1 = {1, 3, 4};
-    vector<int> lst2 = {2, 4, 6, 8};
-    cout << exchange(lst1, lst2);
+    assert(exchange({100, 200}, {200, 200}) == 1);
     return 0;
 }
