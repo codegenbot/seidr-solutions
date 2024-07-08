ad2e8cd56ef018425083f69678ba3bc530907175
beep boop <githubbot@vadim.me>
2024-07-09 01:04:07 +0200

    Solution 84 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/8d7vasla Prompt: oat>’    45 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from e27db114-c452-437b-a5b8-ada1bb9408c7.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<float>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ e27db114-c452-437b-a5b8-ada1bb9408c7.cpp:19:42: note:   initializing argument 1 of ‘void rescale_to_unit(std::vector<float>&)’    19 | void rescale_to_unit(std::vector<float>& values) {       |                      ~~~~~~~~~~~~~~~~~~~~^~~~~~ In file included from e27db114-c452-437b-a5b8-ada1bb9408c7.cpp:35: e27db114-c452-437b-a5b8-ada1bb9408c7.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,30 +1,3 @@
-#include <vector>
-#include <algorithm>
-#include <cassert>
-
-bool is_same(std::vector<float> a, std::vector<float> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-void rescale_to_unit(std::vector<float>& values) {
-    float min_val = *std::min_element(values.begin(), values.end());
-    float max_val = *std::max_element(values.begin(), values.end());
-
-    for (size_t i = 0; i < values.size(); ++i) {
-        values[i] = (values[i] - min_val) / (max_val - min_val);
-    }
-}
-
 int main() {
     std::vector<float> input = {12.0, 11.0, 15.0, 13.0, 14.0};
     rescale_to_unit(input);
