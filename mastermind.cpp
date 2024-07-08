a734c1877143e4254fad2b4f92291cc6f5fb889a
beep boop <githubbot@vadim.me>
2024-07-08 02:27:25 +0200

    Solution 32 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/81fc8mpf Prompt: 531d1ac4-56ad-4dfb-a529-767c10427335.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ 531d1ac4-56ad-4dfb-a529-767c10427335.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ 531d1ac4-56ad-4dfb-a529-767c10427335.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,3 +1,4 @@
+```c
 #include <string>
 #include <unordered_map>
 
@@ -5,22 +6,22 @@ int mastermind(string code, string guess) {
     int white = 0;
     int black = 0;
 
-    unordered_map<char,int> codeMap;
+    unordered_map<char, int> code_count;
     for(int i=0; i<4; i++) {
-        codeMap[code[i]]++;
+        code_count[code[i]]++;
     }
 
     for(int i=0; i<4; i++) {
         if(code[i] == guess[i]) {
             black++;
-            codeMap[code[i]]--;
+            code_count[code[i]]--;
         }
     }
 
     for(int i=0; i<4; i++) {
-        if(codeMap.find(guess[i]) != codeMap.end() && codeMap[guess[i]] > 0) {
+        if(code_count.find(guess[i]) != code_count.end() && code_count[guess[i]] > 0) {
             white++;
-            codeMap[guess[i]]--;
+            code_count[guess[i]]--;
         }
     }
 
