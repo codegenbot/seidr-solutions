5549f42758b231df6c3d0bd764ca4c50d4d24ab3
beep boop <githubbot@vadim.me>
2024-07-09 00:26:45 +0200

    Solution 4 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/emspjx0x Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 3cf371a4-ee84-4cf7-9ef1-07c6af2fd730.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3cf371a4-ee84-4cf7-9ef1-07c6af2fd730.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3cf371a4-ee84-4cf7-9ef1-07c6af2fd730.cpp:25: 3cf371a4-ee84-4cf7-9ef1-07c6af2fd730.cpp: In function ‘int main()’: 3cf371a4-ee84-4cf7-9ef1-07c6af2fd730.cpp:35:20: error: ‘separate_paren_groups’ w as not declared in this scope    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 3cf371a4-ee84-4cf7-9ef1-07c6af2fd730.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,13 +1,30 @@
-bool issame(vector<string> a, vector<string> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
+bool issame(vector<string> a, vector<string> b); 
 
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
+vector<string> separate_paren_groups(string paren_string) {
+    vector<string> result;
+    string current_group;
+    int open_braces = 0;
+  
+    for (char c : paren_string) {
+        if (c == '(') {
+            if (open_braces > 0) {
+                current_group += c;
+            }
+            open_braces++;
+        } else if (c == ')') {
+            open_braces--;
+            if (open_braces > 0) {
+                current_group += c;
+            } else if (open_braces == 0) {
+                result.push_back(current_group);
+                current_group = "";
+            }
         }
     }
 
-    return true;
+    return result;
+}
+
+bool issame(vector<string> a, vector<string> b) {
+    // Function implementation to check if two vectors of strings are the same or not
 }
