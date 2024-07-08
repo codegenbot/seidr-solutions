56341f19e341c88636f9d7649f7b66ff0240ee72
beep boop <githubbot@vadim.me>
2024-07-09 00:43:32 +0200

    Solution 30 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/4lr1ive3 Prompt:    40 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 2d1b39eb-28f2-442c-b84f-c023b3ab975e.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ 2d1b39eb-28f2-442c-b84f-c023b3ab975e.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from 2d1b39eb-28f2-442c-b84f-c023b3ab975e.cpp:39: 2d1b39eb-28f2-442c-b84f-c023b3ab975e.cpp: In function ‘int main()’: 2d1b39eb-28f2-442c-b84f-c023b3ab975e.cpp:49:20: error: ‘sort_even’ was not decla red in this scope    49 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 2d1b39eb-28f2-442c-b84f-c023b3ab975e.cpp:49:19: error: ‘issame’ cannot be used a s a function    49 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -2,13 +2,16 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(vector<float> a, vector<float> b) {
-    return a == b;  // Check if two vectors are the same
+// Function declaration
+void sort_even(std::vector<float>& a);
+
+bool issame(std::vector<float> a, std::vector<float> b) {
+    return a == b;
 }
 
-void sort_even(vector<float>& a) {
-    vector<float> even_indices;
-    vector<float> sorted_even_indices;
+void sort_even(std::vector<float>& a) {
+    std::vector<float> even_indices;
+    std::vector<float> sorted_even_indices;
 
     for (int i = 0; i < a.size(); i++) {
         if (i % 2 == 0) {
@@ -17,7 +20,7 @@ void sort_even(vector<float>& a) {
         }
     }
 
-    sort(sorted_even_indices.begin(), sorted_even_indices.end());
+    std::sort(sorted_even_indices.begin(), sorted_even_indices.end());
 
     int sorted_even_index = 0;
     for (int i = 0; i < a.size(); i++) {
@@ -29,7 +32,7 @@ void sort_even(vector<float>& a) {
 }
 
 int main() {
-    vector<float> input = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
+    std::vector<float> input = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
     sort_even(input);
     assert(issame(input, {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
 
