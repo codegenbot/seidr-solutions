d13e03fdf32331a87c37c69e8adbe082b3f691a5
beep boop <githubbot@vadim.me>
2024-07-09 01:30:55 +0200

    Solution 8 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ltjr7ccv Prompt:       |                    ^~~ e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp:16: e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp: In function ‘int main()’: e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp:26:20: error: ‘strange_sort_list’ was n ot declared in this scope    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,8 +1,10 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b);
+bool is_permutation(const std::vector<int>& a, const std::vector<int>& b) {
+    return std::is_permutation(a.begin(), a.end(), b.begin());
+}
 
-std::vector<int> strange_sort_list(std::vector<int> lst){
+std::vector<int> strange_sort_list(std::vector<int> lst) {
     sort(lst.begin(), lst.end());
     std::vector<int> result;
     int i = 0, j = lst.size() - 1;
@@ -15,4 +17,10 @@ std::vector<int> strange_sort_list(std::vector<int> lst){
         j--;
     }
     return result;
+}
+
+int main() {
+    assert(is_permutation(strange_sort_list({111111}), {111111}));
+    // Additional test cases
+    return 0;
 }
