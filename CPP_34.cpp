3f932a13d2c008472a735f336fc5ebab3f551c3e
beep boop <githubbot@vadim.me>
2024-07-09 00:52:19 +0200

    Solution 68 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/p3mcjouk Prompt:       |                                  ^~~ 9190ad5f-8447-414b-b010-32914458eb05.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 9190ad5f-8447-414b-b010-32914458eb05.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~ In file included from 9190ad5f-8447-414b-b010-32914458eb05.cpp:19: 9190ad5f-8447-414b-b010-32914458eb05.cpp: In function ‘int main()’: 9190ad5f-8447-414b-b010-32914458eb05.cpp:29:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    29 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 9190ad5f-8447-414b-b010-32914458eb05.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -11,11 +11,7 @@ int main() {
     std::vector<int> a = {5, 3, 5, 2, 3, 3, 9, 0, 123};
     std::vector<int> b = {2, 3, 5, 9, 123, 0};
 
-    std::vector<int> c(a);
-    std::sort(c.begin(), c.end());
-    std::sort(b.begin(), b.end());
-
-    assert(issame(c, b));
+    assert(issame(std::vector<int>(a.begin(), a.end()), std::vector<int>(b.begin(), b.end())));
 
     return 0;
 }
