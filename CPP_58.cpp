dc8b231f189362ed3b2bac6a0f5755b77730c2e2
beep boop <githubbot@vadim.me>
2024-07-09 00:34:47 +0200

    Solution 8 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xxoam8gu Prompt:       |                    ^~~ 7e893d23-8027-47e7-b465-b6e392436562.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7e893d23-8027-47e7-b465-b6e392436562.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7e893d23-8027-47e7-b465-b6e392436562.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7e893d23-8027-47e7-b465-b6e392436562.cpp:12: 7e893d23-8027-47e7-b465-b6e392436562.cpp: In function ‘int main()’: 7e893d23-8027-47e7-b465-b6e392436562.cpp:22:20: error: ‘common’ was not declared  in this scope    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 7e893d23-8027-47e7-b465-b6e392436562.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,9 +1,19 @@
+#include <iostream>
 #include <vector>
 #include <set>
+
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return (a.size() == b.size()) && equal(a.begin(), a.end(), b.begin());
+bool issame(vector<int> l1, vector<int> l2) {
+    if(l1.size() != l2.size()) 
+        return false;
+    
+    for(int i=0; i<l1.size(); i++) {
+        if(l1[i] != l2[i]) 
+            return false;
+    }
+    
+    return true;
 }
 
 vector<int> common(vector<int> l1, vector<int> l2) {
@@ -15,4 +25,29 @@ vector<int> common(vector<int> l1, vector<int> l2) {
         inserter(intersection, intersection.begin()));
 
     return vector<int>(intersection.begin(), intersection.end());
+}
+
+int main() {
+    int n,m;
+    cout << "Enter size of first array: ";
+    cin >> n;
+    cout << "Enter elements of first array: ";
+    vector<int> l1(n);
+    for(int i=0; i<n; i++) 
+        cin >> l1[i];
+    
+    cout << "Enter size of second array: ";
+    cin >> m;
+    cout << "Enter elements of second array: ";
+    vector<int> l2(m);
+    for(int i=0; i<m; i++) 
+        cin >> l2[i];
+
+    if(issame(l1, l2)) {
+        cout << "Two arrays are same.\n";
+    } else {
+        cout << "Two arrays are not same.\n";
+    }
+
+    return 0;
 }
