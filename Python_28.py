1f9de5947475e81f5da2a98ad11b5af09b8accc9
beep boop <githubbot@vadim.me>
2024-07-09 01:29:00 +0200

    Solution 27 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/drbj1lci Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1d11adc7-35e6-41ce-b232-d32a536d7 fc6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -1,21 +1,13 @@
-from typing import List
-
-
-def concatenate(strings: List[str]) -> str:
+def concatenate():
     user_strings = []
 
     while True:
-        user_input = input(
-            "Enter a list of strings separated by commas or 'q' to quit: "
-        )
-
-        if user_input.lower() == "q":
-            return "".join(user_strings)
-
         try:
+            user_input = input(
+                "Enter a list of strings separated by commas or 'q' to quit: "
+            )
+            if user_input.lower() == "q":
+                return
             user_strings = [s.strip() for s in user_input.split(",")]
-            break
         except ValueError:
             print("Invalid input. Please enter a list of strings separated by commas.")
-
-    return "".join(user_strings)
