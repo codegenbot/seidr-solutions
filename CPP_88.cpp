1943331d0197ed7b215d800b05582ba54d4344c7
beep boop <githubbot@vadim.me>
2024-07-09 00:40:45 +0200

    Solution 1 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/qhscq9va Prompt: Complete the following code given the task description and function signature.

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,10 +1,9 @@
-#include <algorithm>
-
 vector<int> sort_array(vector<int> array) {
-    int sum = array[0] + array.back();
+    int sum = array[0] + array[array.size() - 1];
     if (sum % 2 == 1) {
-        return vector<int>(array.begin(), array.end());
+        sort(array.begin(), array.end());
     } else {
-        return vector<int>(array.rbegin(), array.rend());
+        sort(array.rbegin(), array.rend());
     }
+    return array;
 }
