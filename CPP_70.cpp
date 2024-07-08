7b533b2a2e799cb3b4d5fa3be693f59c5286e54b
beep boop <githubbot@vadim.me>
2024-07-09 01:28:36 +0200

    Solution 6 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ltjr7ccv Prompt:       |                    ^~~ e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp:16: e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp: In function ‘int main()’: e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp:26:20: error: ‘strange_sort_list’ was n ot declared in this scope    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ e682ce8a-30bf-4d71-8b54-8b6678536bf4.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,24 +1,5 @@
 #include <vector>
-#include <algorithm>
-#include <cassert>
 
-using namespace std;
+bool issame(std::vector<int> a, std::vector<int> b){
 
-vector<int> strange_sort_list(vector<int> lst) {
-    sort(lst.begin(), lst.end());
-    vector<int> result;
-    int left = 0, right = lst.size()-1;
-    while(left <= right){
-        result.push_back(lst[left]);
-        if(left != right)
-            result.push_back(lst[right]);
-        left++;
-        right--;
-    }
-    return result;
-}
-
-int main() {
-    assert((strange_sort_list({111111}) == vector<int>{111111}));
-    return 0;
-}
+std::vector<int> strange_sort_list(std::vector<int> lst);
