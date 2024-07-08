dac4e3287203cf3539c10126d6922e1d6931aa6f
beep boop <githubbot@vadim.me>
2024-07-09 00:59:02 +0200

    Solution 39 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3x14wabr Prompt: In file included from /usr/include/c++/11/string:39,                  from 4d46268a-f54c-4011-8494-4353cabe54a7.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 4d46268a-f54c-4011-8494-4353cabe54a7.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 4d46268a-f54c-4011-8494-4353cabe54a7.cpp:16:44: error: ‘b’ was not declared in t his scope    16 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 4d46268a-f54c-4011-8494-4353cabe54a7.cpp:15: 4d46268a-f54c-4011-8494-4353cabe54a7.cpp: In function ‘int main()’: 4d46268a-f54c-4011-8494-4353cabe54a7.cpp:25:20: error: ‘total_match’ was not dec lared in this scope    25 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 4d46268a-f54c-4011-8494-4353cabe54a7.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,5 +1,8 @@
 bool issame(vector<string> a, string b) {
-    if(a.size()!=1 || a[0].compare(b)!=0)
+    if(a.size()!=1)
         return false;
+    for(int i=0;i<a.size();i++){
+        if(a[i].compare(b) != 0)
+            return false;
+    }
     return true;
-}
