6b651e5d48dbaf8bfa782fbf7f3277819d3a78df
beep boop <githubbot@vadim.me>
2024-07-09 00:35:05 +0200

    Solution 15 of CPP/51, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/rs3ij7r7 Prompt: cdfa845e-6352-4519-9b7e-866a426164cc.cpp:3:1: error: ‘string’ does not name a ty pe     3 | string remove_vowels(string text){       | ^~~~~~

--- CPP_51.cpp
+++ CPP_51.cpp
@@ -1,9 +1,7 @@
-using namespace std;
-
 std::string remove_vowels(std::string text){
     std::string result = "";
     for(int i=0; i < text.length(); i++){
-        if(text[i] != 'a' && text[i] != 'e' && text[i] != 'i' && text[i] != 'o' && text[i] != 'u'
+        if(text[i] != 'a' && text[i] != 'e' && text[i] != 'i' && text[i] != 'o' && text[i] != 'u' 
            && text[i] != 'A' && text[i] != 'E' && text[i] != 'I' && text[i] != 'O' && text[i] != 'U')
             result += text[i];
     }
