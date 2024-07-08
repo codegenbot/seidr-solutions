6d21f45c649bfb3f884da8d2deae3abf1a384c2c
beep boop <githubbot@vadim.me>
2024-07-09 01:27:20 +0200

    Solution 66 of CPP/65, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/m5awezmi Prompt: 22411c2c-45e6-40c1-a39d-2f94319070b9.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 22411c2c-45e6-40c1-a39d-2f94319070b9.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~

--- CPP_65.cpp
+++ CPP_65.cpp
@@ -3,19 +3,22 @@
 using namespace std;
 
 string circular_shift(int x, int shift) {
-    string str = std::move(to_string(x));
+    string str = to_string(x);
     int n = str.length();
     if (shift >= n)
         return str;
     else {
-        string result = "";
-        for (int i = 0; i < n; i++) {
-            if (i < shift || i >= n - shift)
-                result += str[i];
+        char buffer[n];  
+        int i;
+        for (i = 0; i < n - shift; i++)
+            buffer[i] = str[shift];
+        for (int j = 0; j < shift; j++) {
+            if (i < n)
+                buffer[i++] = str[j];
             else
-                result = str[i] + result;
+                break;
         }
-        return result;
+        return string(buffer);
     }
 }
 
