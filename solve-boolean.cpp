30f0cb952bdf65f61c6170ecab7cfb1cdf0c73d7
beep boop <githubbot@vadim.me>
2024-07-08 12:22:48 +0200

    Solution 85 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/2kwhzuet Prompt: ared in this scope    24 |     cin >> expression;       |            ^~~~~~~~~~ 0ae1dff9-cb02-4f04-806e-40c5647653a5.cpp:27:27: error: ‘endl’ was not declared i n this scope; did you mean ‘std::endl’?    27 |         cout << "True" << endl;       |                           ^~~~       |                           std::endl In file included from /usr/include/c++/11/iostream:39,                  from 0ae1dff9-cb02-4f04-806e-40c5647653a5.cpp:2: /usr/include/c++/11/ostream:684:5: note: ‘std::endl’ declared here   684 |     endl(basic_ostream<_CharT, _Traits>& __os)       |     ^~~~ 0ae1dff9-cb02-4f04-806e-40c5647653a5.cpp:29:28: error: ‘endl’ was not declared i n this scope; did you mean ‘std::endl’?    29 |         cout << "False" << endl;       |                            ^~~~       |                            std::endl In file included from /usr/include/c++/11/iostream:39,                  from 0ae1dff9-cb02-4f04-806e-40c5647653a5.cpp:2: /usr/include/c++/11/ostream:684:5: note: ‘std::endl’ declared here   684 |     endl(basic_ostream<_CharT, _Traits>& __os)       |     ^~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,33 +1,3 @@
-```
-#include <vector>
-#include <iostream>
-#include <string>
-
-bool evaluateBooleanExpression(const std::string& expression) {
-    bool result = true;
-    for (char c : expression) {
-        if (c == '&') {
-            result &= false;
-        } else if (c == '|') {
-            result |= false;
-        } else if (c == 'T' || c == 't') {
-            result = true;
-        } else if (c == 'F' || c == 'f') {
-            result = false;
-        }
-    }
-    return result;
-}
-
-int main() {
-    std::string expression;
-    std::cout << "Enter a Boolean expression: ";
-    std::cin >> expression;
-    bool result = evaluateBooleanExpression(expression);
-    if (result) {
-        std::cout << "True" << std::endl;
+cout << "True" << std::endl;
     } else {
-        std::cout << "False" << std::endl;
-    }
-    return 0;
-}
+        cout << "False" << std::endl;
