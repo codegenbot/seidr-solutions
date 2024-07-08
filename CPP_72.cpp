456787f89f1f4966af31e82f1ccbb08447d28b0f
beep boop <githubbot@vadim.me>
2024-07-09 01:23:51 +0200

    Solution 2 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/f3ip0hd4 Prompt: ore ‘for’     8 |     for (int i = 0; i < q.size() / 2; i++) {       |     ^~~ 2c6ee9df-7fbd-4cfe-b763-bc8cbd32896e.cpp:8:21: error: ‘i’ does not name a type     8 |     for (int i = 0; i < q.size() / 2; i++) {       |                     ^ 2c6ee9df-7fbd-4cfe-b763-bc8cbd32896e.cpp:8:39: error: ‘i’ does not name a type     8 |     for (int i = 0; i < q.size() / 2; i++) {       |                                       ^ 2c6ee9df-7fbd-4cfe-b763-bc8cbd32896e.cpp:13:5: error: expected unqualified-id be fore ‘return’    13 |     return true;       |     ^~~~~~ 2c6ee9df-7fbd-4cfe-b763-bc8cbd32896e.cpp:14:1: error: expected declaration befor e ‘}’ token    14 | }       | ^ In file included from 2c6ee9df-7fbd-4cfe-b763-bc8cbd32896e.cpp:16: 2c6ee9df-7fbd-4cfe-b763-bc8cbd32896e.cpp: In function ‘int main()’: 2c6ee9df-7fbd-4cfe-b763-bc8cbd32896e.cpp:18:13: error: ‘will_it_fly’ was not dec lared in this scope    18 |     assert (will_it_fly({5}, 5) == true);       |             ^~~~~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,4 +1,5 @@
-int sum = 0;
+bool will_it_fly(const vector<int>& q, int w) {
+    int sum = 0;
     for (int i = 0; i < q.size(); i++) {
         sum += q[i];
     }
@@ -6,7 +7,7 @@ int sum = 0;
         return false;
     }
     for (int i = 0; i < q.size() / 2; i++) {
-        if (q[i] != q[q.size() - 1 - i]) {
+        if (q[i] != q[q.size() - i - 1]) {
             return false;
         }
     }
