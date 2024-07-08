4726e20e6f90836a4b24afa865cd857fbc4b4730
beep boop <githubbot@vadim.me>
2024-07-09 00:58:19 +0200

    Solution 35 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/2tagw1p7 Prompt: /usr/include/c++/11/bits/stl_uninitialized.h:704:44:   required from ‘_ForwardIt erator std::__uninitialized_default_n_a(_ForwardIterator, _Size, std::allocator< _Tp>&) [with _ForwardIterator = int*; _Size = long unsigned int; _Tp = int]’ /usr/include/c++/11/bits/stl_vector.h:1606:36:   required from ‘void std::vector <_Tp, _Alloc>::_M_default_initialize(std::vector<_Tp, _Alloc>::size_type) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = l ong unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:512:9:   required from ‘std::vector<_Tp, _ Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocator_type&) [with  _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = long unsigned int; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<int >]’ ccc7d69d-f64e-48c0-a7fd-b3566d234415.cpp:26:23:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <algorithm>
 
 using namespace std;
 
 vector<int> findPair(vector<int>& nums, int target) {
-    sort(nums.begin(), nums.end());
     unordered_map<int, int> numMap;
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
