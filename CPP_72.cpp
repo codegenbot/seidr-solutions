1667299a6900663c1b2e08687061bea9bc47e8f2
beep boop <githubbot@vadim.me>
2024-07-09 01:08:25 +0200

    Solution 38 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zqffj6s3 Prompt:                  from /usr/include/c++/11/bits/stl_algobase.h:64,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 1d7543b1-67de-4370-828f-2dda9bd46768.cpp:3: /usr/include/c++/11/type_traits: In instantiation of ‘struct std::is_destructibl e<int>’: /usr/include/c++/11/bits/stl_construct.h:188:51:   required from ‘void std::_Des troy(_ForwardIterator, _ForwardIterator) [with _ForwardIterator = int*]’ /usr/include/c++/11/bits/alloc_traits.h:848:15:   required from ‘void std::_Dest roy(_ForwardIterator, _ForwardIterator, std::allocator<_T2>&) [with _ForwardIter ator = int*; _Tp = int]’ /usr/include/c++/11/bits/stl_vector.h:680:15:   required from ‘std::vector<_Tp, _Alloc>::~vector() [with _Tp = int; _Alloc = std::allocator<int>]’ 1d7543b1-67de-4370-828f-2dda9bd46768.cpp:22:5:   required from here /usr/include/c++/11/type_traits:885:52: error: static assertion failed: template  argument must be a complete class or an unbounded array   885 |       static_assert(std::__is_complete_or_unbounded(__type_identity<_Tp> {}),       |                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~ ~~~ /usr/include/c++/11/type_traits:885:52: note: ‘std::__is_complete_or_unbounded<s td::__type_identity<int> >((std::__type_identity<int>{}, std::__type_identity<in t>()))’ evaluates to false

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -5,15 +5,15 @@
 
 bool will_it_fly(std::vector<int> q, int w) {
     std::string str = "";
+    for(int i : q) {
+        str += std::to_string(i);
+    }
+    if(std::accumulate(q.begin(), q.end(), 0) != std::accumulate(q.rbegin(), q.rend(), 0))
+        return false;
     int sum = 0;
     for(int i : q) {
         sum += i;
     }
-    for(int i : q) {
-        str += std::to_string(i);
-    }
-    if(str != std::string(str.rbegin(), str.rend()))
-        return false;
     return sum <= w;
 }
 
@@ -40,6 +40,6 @@ int main() {
     if(will_it_fly(weights, total_weight)) {
         std::cout << "The package will fly.\n";
     } else {
-        std::cout << "The package won't fly due to incorrect weight sequence or exceeding the total weight limit.\n";
+        std::cout << "The package won't fly.\n";
     }
 }
