b8ac45e41fee85d18bb3ff5bb5e896bd37cee63d
beep boop <githubbot@vadim.me>
2024-07-09 00:36:49 +0200

    Solution 40 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/80df1iq3 Prompt: 45d94bf1-b5c6-453d-848c-4dcd34cf5ce0.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,22 +1,23 @@
 #include <vector>
 #include <any>
-#include <boost-any.hpp>
-#include <boost/type_id.hpp>
+#include <boost/variant.hpp>
+using namespace boost;
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> filter_integers(std::vector<std::any> values) {
+std::vector<int> filter_integers(std::vector<any> values) {
     std::vector<int> result;
     for (const auto& value : values) {
-        if (value.type() == typeid(int)) {
-            result.push_back(boost::any_cast<int>(value));
+        if (holds_alternative<int>(value)) {
+            result.push_back(hold_alternative<int>(value));
         }
     }
     return result;
 }
 
 int main() {
-    assert(issame(filter_integers({3, std::any('c'), 3, 3, std::any('a'), std::any('b')}), {3, 3, 3}));
+    assert(issame(filter_integers({3, any('c'), 3, 3, any('a'), any('b')}), {3, 3, 3}));
     return 0;
+}
