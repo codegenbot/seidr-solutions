b2f505fdf9e362b43d47947d30e7b58334c4e930
beep boop <githubbot@vadim.me>
2024-07-09 01:41:24 +0200

    Solution 17 of Python/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/jor3l590 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3a7dfa06-644e-4145-8cd4-8025d9bff 2ca.py", line 7, in <module>     check(incr_list)           ^^^^^^^^^ NameError: name 'incr_list' is not defined

--- Python_42.py
+++ Python_42.py
@@ -2,9 +2,10 @@ def increment_list(l):
     return [x + 1 for x in l]
 
 def check(increment_list):
-    n = [1, 2, 3]
-    result = increment_list(n)
-    assert result == [2, 3, 4]
-    print("Test passed!")
+    l = [1, 2, 3]
+    if increment_list(l) == [2, 3, 4]:
+        print("Pass")
+    else:
+        print("Fail")
 
 check(increment_list)
