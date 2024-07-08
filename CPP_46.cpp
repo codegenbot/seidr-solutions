c977ca848cd4c61edfa43636c0d66bd9a9a55704
beep boop <githubbot@vadim.me>
2024-07-09 00:57:36 +0200

    Solution 64 of CPP/46, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/8iwckk56 Prompt:       | ^~~~~~~~~~~~~ 57d229e8-ad12-4c79-914e-79fa1c17709c.cpp:9:9: warning: empty parentheses were di sambiguated as a function declaration [-Wvexing-parse]     9 | int main(){       |         ^~ 57d229e8-ad12-4c79-914e-79fa1c17709c.cpp:9:9: note: remove parentheses to defaul t-initialize a variable     9 | int main(){       |         ^~       |         -- 57d229e8-ad12-4c79-914e-79fa1c17709c.cpp:9:9: note: or replace parentheses with braces to value-initialize a variable 57d229e8-ad12-4c79-914e-79fa1c17709c.cpp:9:11: error: a function-definition is n ot allowed here before ‘{’ token     9 | int main(){       |           ^ 57d229e8-ad12-4c79-914e-79fa1c17709c.cpp:11:2: error: expected ‘}’ at end of inp ut    11 | }       |  ^ 57d229e8-ad12-4c79-914e-79fa1c17709c.cpp:1:12: note: to match this ‘{’     1 | int main() {       |            ^

--- CPP_46.cpp
+++ CPP_46.cpp
@@ -1,9 +1,6 @@
-```
-int main 
-{
+int main {
     int n;
     std::cout << "Enter the value of n: ";
     std::cin >> n;
     std::cout << "The " << n << "-th element of the fib4 number sequence is: " << fib4(n) << std::endl;
     return 0;
-}
