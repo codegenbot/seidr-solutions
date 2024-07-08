7c495daf238c914f60cb3a06b7a4a7d2d88949c4
beep boop <githubbot@vadim.me>
2024-07-09 00:40:33 +0200

    Solution 60 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/emspjx0x Prompt:       |           ^~~~~~ In file included from ea005a5f-1787-4e51-b368-4ec6659bd3d3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ea005a5f-1787-4e51-b368-4ec6659bd3d3.cpp:46:37: error: ‘string’ was not declared  in this scope    46 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ea005a5f-1787-4e51-b368-4ec6659bd3d3.cpp:46:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from ea005a5f-1787-4e51-b368-4ec6659bd3d3.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from ea005a5f-1787-4e51-b368-4ec6659bd3d3.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ ea005a5f-1787-4e51-b368-4ec6659bd3d3.cpp:46:44: error: ‘b’ was not declared in t his scope    46 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,45 +1,4 @@
 #include <vector>
 #include <string>
-#include <cassert>
 
-using namespace std;
-
-std::vector<std::string> separate_paren_groups(const std::string& s) {
-    std::vector<std::string> groups;
-    int count = 0;
-    std::string current_group = "";
-    
-    for(char c : s) {
-        if(c == '(') {
-            count++;
-        } else if(c == ')') {
-            count--;
-        }
-        
-        current_group += c;
-        
-        if(count == 0) {
-            groups.push_back(current_group);
-            current_group = "";
-        }
-    }
-    
-    return groups;
-}
-
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), std::vector<std::string>{"()", "(())", "(()())"}));
-return 0;
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b){
