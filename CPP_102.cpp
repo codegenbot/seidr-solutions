92380c852fdc7cd621aff5e19254be5c71633a3b
beep boop <githubbot@vadim.me>
2024-07-09 01:51:53 +0200

    Solution 56 of CPP/102, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/einmeurw Prompt: x11::basic_string<_CharT, _Traits, _Alloc>::const_iterator) [with _CharT = char;  _Traits = std::char_traits<char>; _Alloc = std::allocator<char>; std::__cxx11:: basic_string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_st ring<char>::const_iterator; std::__cxx11::basic_string<_CharT, _Traits, _Alloc>: :const_iterator = std::__cxx11::basic_string<char>::const_iterator]’  2176 |       replace(__const_iterator __i1, __const_iterator __i2,       |       ^~~~~~~ /usr/include/c++/11/bits/basic_string.h:2176:32: note:   no known conversion for  argument 1 from ‘long int’ to ‘std::__cxx11::basic_string<char>::__const_iterat or’ {aka ‘std::__cxx11::basic_string<char>::const_iterator’}  2176 |       replace(__const_iterator __i1, __const_iterator __i2,       |               ~~~~~~~~~~~~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h:2201:21: note: candidate: ‘std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Trai ts, _Alloc>::replace(std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::const_ iterator, std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::const_iterator, s td::initializer_list<_Tp>) [with _CharT = char; _Traits = std::char_traits<char> ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::const_iterator = std::__cxx11::basic_string<char>::const_iterator]’  2201 |       basic_string& replace(const_iterator __i1, const_iterator __i2,       |                     ^~~~~~~ /usr/include/c++/11/bits/basic_string.h:2201:21: note:   candidate expects 3 arg uments, 4 provided

--- CPP_102.cpp
+++ CPP_102.cpp
@@ -1,36 +1,8 @@
-#include <iostream>
-#include <sstream>
-#include <vector>
-using namespace std;
-
 int choose_num(int x, int y) {
-    vector<int> result;
-    stringstream ss;
+    int sum = 0;
     for (int i = x; i <= y; i++) {
         if (i % 2 == 0)
-            ss << i << " ";
+            sum += i;
     }
-    string str = ss.str();
-    size_t pos = 0;
-    while ((pos = str.find(" ")) != string::npos) {
-        result.push_back(stoi(str.substr(0, pos)));
-        str.erase(0, pos + 1);
-    }
-    if (!str.empty())
-        result.push_back(stoi(str));
-    
-    int max = INT_MIN;
-    for (int i : result)
-        if (i > max)
-            max = i;
-    return max;
-}
-
-int main() {
-    int x, y;
-    cout << "Enter value of x: ";
-    cin >> x;
-    cout << "Enter value of y: ";
-    cin >> y;
-    cout << "The biggest even integer is: " << choose_num(x,y) << endl;
+    return sum;
 }
