5b394682949723618e20ecd0d887895b31058d75
beep boop <githubbot@vadim.me>
2024-07-09 00:23:24 +0200

    Solution 28 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/a3jqkatw Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from fd42c89e-19ac-47e7-8f3d-1b2ff7e5c292.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<const char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ fd42c89e-19ac-47e7-8f3d-1b2ff7e5c292.cpp:33:21:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,30 +1,31 @@
 ```cpp
+#include <string>
+#include <vector>
+
 int bowlingScore(std::string s) {
     int score = 0;
     bool lastRollWasStrike = false;
     bool lastRollWasSpare = false;
 
-    for (char c : s) {
-        if (c == 'X') {
+    for (size_t i = 0; i < s.size(); ) {
+        if (s[i] == 'X') { 
             score += 10 + (lastRollWasStrike ? 10 : (lastRollWasSpare ? 5 : 0));
             lastRollWasStrike = true;
             lastRollWasSpare = false;
-        } else if (c == '/') {
-            size_t pos = s.find('/');
-            if (pos != std::string::npos) {
-                int nextOneRoll = stoi(s.substr(0, pos));
-                score += nextOneRoll;
-            } else {
-                int nextTwoRolls = stoi(s);
-                score += nextTwoRolls / 2;
-            }
+            i++; // skip the rest of the frame
+        } else if (s[i] == '/') { 
+            int nextTwoRolls = std::stoi(s.substr(i + 1, 2)) * 10 / 2;
+            score += (lastRollWasStrike ? 10 : (lastRollWasSpare ? 5 : 0));
+            score += nextTwoRolls;
             lastRollWasStrike = false;
             lastRollWasSpare = true;
-        } else {
-            int roll = c - '0';
-            if (c == '9') lastRollWasSpare = true;
+            i += 3; // skip the rest of the frame
+        } else { 
+            int roll = s[i] - '0';
+            if (roll == 9) lastRollWasSpare = true;
             else lastRollWasStrike = true;
             score += roll;
+            i++; // move to the next character
         }
     }
 
