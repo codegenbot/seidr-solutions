fe5e489bc702e708c936e38f92209844f013f587
beep boop <githubbot@vadim.me>
2024-07-09 00:50:28 +0200

    Solution 34 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/d8r3nlix Prompt: w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const int*; _Tp = int; _Alloc = std:: allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = i nt; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::allocator_type = std ::allocator<int>]’ 085d2af1-3919-4fb2-8364-44ee1a472f34.cpp:16:5:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,4 +1,3 @@
-```
 #include <iostream>
 #include <vector>
 
@@ -9,6 +8,9 @@ bool below_threshold(std::vector<int> l, int t) {
         }
     }
     return true;
-
 }
-assert(not(below_threshold({1, 8, 4, 10}, 10)));
+
+int main() {
+    assert(not(below_threshold(std::vector<int>(1, 8, 4, 10), 10)));
+    return 0;
+}
