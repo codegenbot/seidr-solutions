6546878cf5cb68f17584aee20944a3f2ebcec3cc
beep boop <githubbot@vadim.me>
2024-07-09 00:48:12 +0200

    Solution 13 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/hon28pej Prompt:  in this scope    21 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 91e247ed-2f1c-4ffa-b98d-7b887b852d7f.cpp:21:42: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 91e247ed-2f1c-4ffa-b98d-7b887b852d7f.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 91e247ed-2f1c-4ffa-b98d-7b887b852d7f.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 91e247ed-2f1c-4ffa-b98d-7b887b852d7f.cpp:21:49: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ In file included from 91e247ed-2f1c-4ffa-b98d-7b887b852d7f.cpp:20: 91e247ed-2f1c-4ffa-b98d-7b887b852d7f.cpp: In function ‘int main()’: 91e247ed-2f1c-4ffa-b98d-7b887b852d7f.cpp:33:20: error: ‘get_row’ was not declare d in this scope    33 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 
 std::vector<std::vector<int>> get_row(std::vector<std::vector<int>>& matrix, int row) {
-    std::vector<int> row_data;
+    std::vector<int> temp;
     for(int i = 0; i < matrix[0].size(); i++) {
-        row_data.push_back(matrix[row][i]);
+        temp.push_back(matrix[row][i]);
     }
-    return row_data;
+    return temp;
 }
 
 bool issame(const std::vector<std::vector<int>>& a, const std::vector<std::vector<int>>& b) {
@@ -26,5 +26,5 @@ bool issame(const std::vector<std::vector<int>>& a, const std::vector<std::vecto
 }
 
 int main() {
-    assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));
+    assert (issame(get_row({},{1},{1,2,3}),{{2,2}}));
 }
