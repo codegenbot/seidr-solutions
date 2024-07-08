b4666fd2b9e4534af13523f6bfa5ddeb149a26c1
beep boop <githubbot@vadim.me>
2024-07-09 00:44:28 +0200

    Solution 18 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3x14wabr Prompt: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::__cxx11::basi c_string<_CharT, _Traits, _Alloc>::basic_string(std::__cxx11::basic_string<_Char T, _Traits, _Alloc>&&) [with _CharT = char; _Traits = std::char_traits<char>; _A lloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:6667:12:   required from here /usr/include/c++/11/bits/basic_string.h:576:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   576 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,33 +1,9 @@
-#include <vector>
-#include <string>
-
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if (a.size() != b.size()) 
+bool issame(vector<string> a,vector<string>b){
+    if(a.size()!=b.size())
         return false;
-        
-    for (int i = 0; i < a.size(); i++) 
-        if (a[i] != b[i]) 
+    for(int i=0;i<a.size();i++){
+        if(strcmp(a[i].c_str(), b[i].c_str()))
             return false;
-
-    return true;
-}
-
-std::vector<std::string> total_match(std::vector<std::string> input, std::vector<std::string> pattern) {
-    for(int i = 0; i < input.size(); i++) {
-        if(pattern[i] == "") {
-            continue;
-        } else {
-            if(input[i].find(pattern[i]) == std::string::npos) {
-                return {};
-            }
-        }
     }
-    return input;
-}
-
-int main() {
-    // Test the function
-    assert(issame(total_match({"this"}, {}), {}));
-    
-    return 0;
+    return true;
 }
