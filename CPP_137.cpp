f5f8ce99e74242607a4b09d82718e677829a15e3
beep boop <githubbot@vadim.me>
2024-07-09 01:40:26 +0200

    Solution 8 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhnk3fn Prompt:    19 | } else if (a.type() == boost::any::typeclass<string> &&       |   ^~~~ f3ac71ab-33c6-4deb-8227-91ce132a7b8b.cpp:28:3: error: expected unqualified-id be fore ‘else’    28 | } else {       |   ^~~~ In file included from f3ac71ab-33c6-4deb-8227-91ce132a7b8b.cpp:32: f3ac71ab-33c6-4deb-8227-91ce132a7b8b.cpp: In function ‘int main()’: f3ac71ab-33c6-4deb-8227-91ce132a7b8b.cpp:34:13: error: ‘boost’ has not been decl ared    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ f3ac71ab-33c6-4deb-8227-91ce132a7b8b.cpp:34:29: error: ‘string’ was not declared  in this scope    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ f3ac71ab-33c6-4deb-8227-91ce132a7b8b.cpp:34:37: error: ‘compare_one’ was not dec lared in this scope    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,33 +1,22 @@
-#include <boost/any.hpp>
-#include <string>
-
-if (a.type() == boost::any::typeclass<int> && 
-    b.type() == boost::any::typeclass<int>) {
-    if (get<int>(a) > get<int>(b)) {
-        return a;
-    } else if (get<int>(a) < get<int>(b)) {
-        return b;
-    } else {
-        return a;
+```cpp
+int main() {
+    boost::any a = boost::any(1);
+    boost::any b = boost::any("2");
+    try {
+        if (a.type() == boost::any::typeclass<int> && 
+            b.type() == boost::any::typeclass<string>) {
+            if (boost::get<int>(a) > boost::get<string>(b)) {
+                return a;
+            } else if (boost::get<int>(a) < boost::get<string>(b)) {
+                return b;
+            } else {
+                throw invalid_argument("Invalid types");
+            }
+        } else {
+            throw invalid_argument("Invalid types");
+        }
+    } catch (invalid_argument &e) {
+        cerr << e.what() << endl;
+        return 1;
     }
-} else if (a.type() == boost::any::typeclass<float> && 
-           b.type() == boost::any::typeclass<float>) {
-    if (get<float>(a) > get<float>(b)) {
-        return a;
-    } else if (get<float>(a) < get<float>(b)) {
-        return b;
-    } else {
-        return a;
-    }
-} else if (a.type() == boost::any::typeclass<string> && 
-           b.type() == boost::any::typeclass<string>) {
-    if (get<string>(a) > get<string>(b)) {
-        return a;
-    } else if (get<string>(a) < get<string>(b)) {
-        return b;
-    } else {
-        return a;
-    }
-} else {
-    throw invalid_argument("Invalid types");
 }
