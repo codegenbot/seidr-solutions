b5efb90681957590bed9002b78543b021ab11c80
beep boop <githubbot@vadim.me>
2024-07-09 00:20:30 +0200

    Solution 20 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/go1wwxqh Prompt: 936e09c9-dc75-4c77-ae34-872841336ef3.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ 936e09c9-dc75-4c77-ae34-872841336ef3.cpp: In function ‘int main()’: 936e09c9-dc75-4c77-ae34-872841336ef3.cpp:26:5: error: ‘vector’ was not declared in this scope    26 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 936e09c9-dc75-4c77-ae34-872841336ef3.cpp:26:5: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 936e09c9-dc75-4c77-ae34-872841336ef3.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 936e09c9-dc75-4c77-ae34-872841336ef3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 936e09c9-dc75-4c77-ae34-872841336ef3.cpp:26:12: error: expected primary-expressi on before ‘float’    26 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <cmath>
 
-bool has_close_elements(std::vector<std::float_t> numbers, std::float_t threshold) {
+bool has_close_elements(std::vector<float> numbers, float threshold) {
     for (int i = 0; i < numbers.size(); i++) {
         for (int j = i + 1; j < numbers.size(); j++) {
             if (std::abs(numbers[i] - numbers[j]) <= threshold) {
@@ -14,7 +14,7 @@ bool has_close_elements(std::vector<std::float_t> numbers, std::float_t threshol
 }
 
 int main() {
-    std::vector<std::float_t> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
+    std::vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
     if (has_close_elements(a, 0.1f)) {
         std::cout << "The array contains close elements." << std::endl;
     } else {
