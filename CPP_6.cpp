6270952e31e1ced6852243214ac3265de17a166f
beep boop <githubbot@vadim.me>
2024-07-09 00:27:30 +0200

    Solution 4 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/a4if19d8 Prompt:       |                    ^~~ ad485918-a03d-4291-86d5-800db484997e.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ad485918-a03d-4291-86d5-800db484997e.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ad485918-a03d-4291-86d5-800db484997e.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ad485918-a03d-4291-86d5-800db484997e.cpp:24: ad485918-a03d-4291-86d5-800db484997e.cpp: In function ‘int main()’: ad485918-a03d-4291-86d5-800db484997e.cpp:34:20: error: ‘parse_nested_parens’ was  not declared in this scope    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ ad485918-a03d-4291-86d5-800db484997e.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-vector<int> parse_nested_parens(string paren_string);
+bool issame(vector<int> a, vector<int> b);
 
 vector<int> parse_nested_parens(string paren_string) {
     vector<int> depths;
@@ -23,4 +23,24 @@ vector<int> parse_nested_parens(string paren_string) {
     depths.push_back(max_depth);
     
     return depths;
+}
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int main() {
+    assert(issame(parse_nested_parens("(()(())((())))"), {4}));
+  
+    return 0;
 }
