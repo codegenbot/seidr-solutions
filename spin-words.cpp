92e8050daca81552ef774c501ae816fc21fa4c83
beep boop <githubbot@vadim.me>
2024-07-08 10:04:03 +0200

    Solution 50 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/5399zpdp Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,44 +1,40 @@
-#include <vector>
-#include <iostream>
 #include <string>
+using namespace std;
 
-std::string spinWords(std::string str) {
-    std::string result = "";
-    int wordLength = 0;
-    
-    for (int i = 0; i < str.length(); i++) {
-        if (str[i] == ' ') {
-            if (wordLength >= 5) {
-                for (int j = i - 1; j >= i - wordLength; j--) {
-                    result += str[j];
-                }
-            } else {
-                result += str.substr(i, 1); 
-            }
-            wordLength = 0;
+string spinWords(string str) {
+    string result = "";
+    string word = "";
+
+    for (char c : str) {
+        if (c == ' ') {
+            if (word.length() >= 5) {
+                for (int i = word.length() - 1; i >= 0; --i)
+                    result += word[i];
+            } else
+                result += word;
+            word = "";
         } else {
-            wordLength++;
+            word += c;
         }
     }
 
-    if (wordLength >= 5) {
-        std::string reversedWord;
-        for (int i = str.length() - 1; i >= str.length() - wordLength; i--) {
-            reversedWord += str[i];
-        }
-        result += reversedWord + " ";
-    } else {
-        result = str;
-    }
-    
+    if (word.length() >= 5) {
+        for (int i = word.length() - 1; i >= 0; --i)
+            result += word[i];
+    } else
+        result += word;
+
     return result;
 }
 
 int main() {
-    std::cout << spinWords("a") << std::endl;
-    std::cout << spinWords("this is a test") << std::endl;
-    std::cout << spinWords("this is another test") << std::endl;
-    std::cout << spinWords("hi") << std::endl;
-    
+    string str = "Hello World";
+    cout << spinWords(str) << endl;
+    str = "This is a test";
+    cout << spinWords(str) << endl;
+    str = "Short";
+    cout << spinWords(str) << endl;
+    str = "";
+    cout << spinWords(str) << endl;
     return 0;
 }
