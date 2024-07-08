132026c0003efb397a84fd536ada829895516dd1
beep boop <githubbot@vadim.me>
2024-07-09 00:32:09 +0200

    Solution 33 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/80df1iq3 Prompt: 5c145ea7-5a26-446a-8ad6-4b0f18a38368.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 5c145ea7-5a26-446a-8ad6-4b0f18a38368.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 5c145ea7-5a26-446a-8ad6-4b0f18a38368.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 5c145ea7-5a26-446a-8ad6-4b0f18a38368.cpp:4:10: fatal error: boost/any.hpp: No su ch file or directory     4 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,6 +1,6 @@
+```cpp
 #include <vector>
 #include <any>
-#include <boost/any.hpp>
 #include <boost/optional.hpp>
 #include <boost/type_index.hpp>
 
@@ -19,6 +19,6 @@ std::vector<int> filter_integers(std::vector<std::any> values) {
 }
 
 int main() {
-    assert(issame(filter_integers({3, boost::any('c'), 3, 3, boost::any('a'), boost::any('b')}), {3, 3, 3}));
+    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
     return 0;
 }
