87f4332a089bfa87bdab92bccdc4427e28b55500
beep boop <githubbot@vadim.me>
2024-07-09 00:29:29 +0200

    Solution 8 of CPP/19, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/poiv4yw7 Prompt: 8bf49f1b-29d1-487d-be2d-1a373210f113.cpp:1:1: error: ‘map’ does not name a type     1 | map<string, int> numMap = {       | ^~~ 8bf49f1b-29d1-487d-be2d-1a373210f113.cpp:14:1: error: ‘string’ does not name a t ype    14 | string sort_numbers(string numbers){       | ^~~~~~ In file included from 8bf49f1b-29d1-487d-be2d-1a373210f113.cpp:36: 8bf49f1b-29d1-487d-be2d-1a373210f113.cpp: In function ‘int main()’: 8bf49f1b-29d1-487d-be2d-1a373210f113.cpp:38:13: error: ‘sort_numbers’ was not de clared in this scope    38 |     assert (sort_numbers("six five four three two one zero") == "zero on e two three four five six");       |             ^~~~~~~~~~~~

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,20 +1,43 @@
-map<string, int> num_map = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
-    map<int, string> rev_num_map;
-    for (auto const& pair : num_map) {
-        rev_num_map[pair.second] = pair.first;
-    }
+#include <iostream>
+#include <map>
+#include <string>
+#include <cassert>
 
-    vector<int> sorted_numbers;
-    stringstream ss(numbers);
-    string token;
-    while (ss >> token) {
-        sorted_numbers.push_back(num_map[token]);
-    }
-    sort(sorted_numbers.begin(), sorted_numbers.end());
+using namespace std;
 
+string sort_numbers(string numbers);
+
+map<string, int> numMap = {
+    {"zero", 0},
+    {"one", 1},
+    {"two", 2},
+    {"three", 3},
+    {"four", 4},
+    {"five", 5},
+    {"six", 6},
+    {"seven", 7},
+    {"eight", 8},
+    {"nine", 9}
+};
+
+string sort_numbers(string numbers){
     string result;
-    for (int num : sorted_numbers) {
-        result += rev_num_map[num] + " ";
+    map<int, string> sortedNums;
+    
+    size_t pos = 0;
+    while ((pos = numbers.find(" ")) != string::npos) {
+        string numStr = numbers.substr(0, pos);
+        int num = numMap[numStr];
+        sortedNums[num] = numStr;
+        numbers.erase(0, pos + 1);
     }
-    result.pop_back(); // Remove the extra space at the end
+    
+    sortedNums[numMap[numbers]] = numbers;
+    
+    for (const auto& pair : sortedNums) {
+        result += pair.second + " ";
+    }
+    
+    result.pop_back(); // Remove extra space at the end
     return result;
+}
