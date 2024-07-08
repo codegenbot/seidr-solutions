8c6d81fa8967816ba0a830679b8f30ab22ac3ccf
beep boop <githubbot@vadim.me>
2024-07-09 00:16:24 +0200

    Solution 9 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/af0uic6z Prompt: 47c6db47-8bb3-4710-8f18-719205c6e1a3.cpp:3:9: fatal error: boost/any.hpp: No suc h file or directory     3 | #include<boost/any.hpp>       |         ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -8,7 +8,7 @@ using namespace std;
 vector<int> filter_integers(list_any values) {
     vector<int> result;
     for (const auto& value : values) {
-        if (boost::any_cast<int>(value, 0) != 0) {
+        if (boost::any_cast<int>(value) != 0) {
             result.push_back(boost::any_cast<int>(value));
         }
     }
