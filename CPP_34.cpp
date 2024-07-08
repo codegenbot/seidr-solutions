2e06434705ec1147330b408d1d4c9c1bd6c04002
beep boop <githubbot@vadim.me>
2024-07-09 00:19:43 +0200

    Solution 4 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/sh1nutg3 Prompt:    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 466f66d0-f0d4-4fd1-9d93-cb90dd302433.cpp:15: 466f66d0-f0d4-4fd1-9d93-cb90dd302433.cpp: In function ‘int main()’: 466f66d0-f0d4-4fd1-9d93-cb90dd302433.cpp:25:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    25 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 466f66d0-f0d4-4fd1-9d93-cb90dd302433.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ In file included from 466f66d0-f0d4-4fd1-9d93-cb90dd302433.cpp:15: 466f66d0-f0d4-4fd1-9d93-cb90dd302433.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,15 +1,12 @@
 #include <algorithm>
 #include <vector>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); ++i)
-        if (a[i] != b[i])
-            return false;
-    return true;
-}
-
 std::vector<int> unique(std::vector<int> l) {
-    return std::vector<int>(std::unique_copy(l.begin(), std::unique_end(l.end()), l.begin()));
+    std::vector<int> result;
+    for (int i : l) {
+        if (std::find_if(result.begin(), result.end(),
+                         [&](int x) { return x == i; }) == result.end())
+            result.push_back(i);
+    }
+    return result;
 }
