b2b314241aa7b8b9785f05e6203acb257ce374c6
beep boop <githubbot@vadim.me>
2024-07-09 00:28:16 +0200

    Solution 44 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/i86t0gnh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d551b18f-d941-480a-84ae-25e4ba369 f12.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,18 +1,18 @@
 def bowling_score(frames):
     score = 0
-    frames = frames.split("\n")
+    frames = frames.split('\n')
     for i, frame in enumerate(frames):
         if len(frame) == 2:
-            if frame[0] == "X":
+            if frame[0] == 'X':
                 if i < 9:
                     score += 30
                 else:
-                    score += 30 + int(list("X-" + frames[i + 1])[3])
-            elif frame[1] == "/":
+                    score += 30 + int(list('X-' + frames[i+1])[3])
+            elif frame[1] == '/':
                 score += 10 + (10 - int(frame[0]))
         elif len(frame) == 1:
-            if i < 9 and int(frame) + int(frames[i + 1].split()[0]) >= 10:
+            if i < 9 and int(frame) + int(frames[i+1].split()[0]) >= 10:
                 score += 10 + int(frame)
             else:
-                score += int(frame) + int(frames[i + 1].split()[0])
+                score += int(frame) + int(frames[i+1].split()[0])
     return score
