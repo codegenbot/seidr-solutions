ebddbcf2fcc1176e88ed823ec8785414de6de902
beep boop <githubbot@vadim.me>
2024-07-09 01:49:17 +0200

    Solution 20 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c6pooyd5 Prompt: 5124be4f-3082-456f-a53f-7563452763be.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 5124be4f-3082-456f-a53f-7563452763be.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 5124be4f-3082-456f-a53f-7563452763be.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 5124be4f-3082-456f-a53f-7563452763be.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ In file included from 5124be4f-3082-456f-a53f-7563452763be.cpp:19: 5124be4f-3082-456f-a53f-7563452763be.cpp: In function ‘int main()’: 5124be4f-3082-456f-a53f-7563452763be.cpp:21:13: error: ‘exchange’ was not declar ed in this scope    21 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 int exchange(vector<int> lst1, vector<int> lst2) {
     for (int num : lst1) {
         if (num % 2 != 0) {
@@ -14,3 +18,13 @@ int exchange(vector<int> lst1, vector<int> lst2) {
     }
     return 1;
 }
+
+int main() {
+    vector<int> list1 = {100, 200};
+    vector<int> list2 = {200, 200};
+    int result = exchange(list1, list2);
+    if (result == 1)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
