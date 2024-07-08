a2f95e4f944f20a0fc53e61af5e9b18099c99bae
beep boop <githubbot@vadim.me>
2024-07-09 00:30:04 +0200

    Solution 30 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/af0uic6z Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 83afbf9c-56c5-49d6-a550-5c23758fb30a.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 83afbf9c-56c5-49d6-a550-5c23758fb30a.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 83afbf9c-56c5-49d6-a550-5c23758fb30a.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 83afbf9c-56c5-49d6-a550-5c23758fb30a.cpp:25: 83afbf9c-56c5-49d6-a550-5c23758fb30a.cpp: In function ‘int main()’: 83afbf9c-56c5-49d6-a550-5c23758fb30a.cpp:35:35: error: could not convert ‘{3, 'c ', 3, 3, 'a', 'b'}’ from ‘<brace-enclosed initializer list>’ to ‘std::any’    35 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~       |                                   |       |                                   <brace-enclosed initializer list>

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,13 +1,11 @@
 #include <any>
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return std::equal(a.begin(), a.end(), b.begin());
-}
+bool issame(std::any a, std::any b) { return (std::any_cast<int>(a) == std::any_cast<int>(b)); }
 
 std::vector<int> filter_integers(std::any values) {
     std::vector<int> result;
-    for (auto& value : std::any_cast<std::vector<int>>(values)) {
+    for (auto& value : std::any_cast<std::any>(values)) {
         if (std::holds_alternative<int>(value)) {
             int val = std::get<int>(value);
             result.push_back(val);
@@ -17,8 +15,8 @@ std::vector<int> filter_integers(std::any values) {
 }
 
 int main() {
-    std::any values = std::make_any(std::vector<int>{{3, 3, 3}});
+    auto values = std::any({3, 50, 3, 3, 1, 2});
     auto output = filter_integers(values);
-    assert(issame({3, 3, 3}, output));
+    assert(issame(std::any(output), std::any({3, 3, 3})));
     return 0;
 }
