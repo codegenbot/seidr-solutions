f2fe06c338de33d13e28c9450021db803ead81e9
beep boop <githubbot@vadim.me>
2024-07-09 01:17:09 +0200

    Solution 13 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/fk6tmp91 Prompt:       |     ^~~~~~ 64a21d8c-756f-4fd7-b53c-4daa2deec5dc.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | 64a21d8c-756f-4fd7-b53c-4daa2deec5dc.cpp: At global scope: 64a21d8c-756f-4fd7-b53c-4daa2deec5dc.cpp:24:6: error: redefinition of ‘bool issa me(std::vector<float>, std::vector<float>)’    24 | bool issame(vector<float> a,vector<float>b){       |      ^~~~~~ 64a21d8c-756f-4fd7-b53c-4daa2deec5dc.cpp:14:6: note: ‘bool issame(std::vector<fl oat>, std::vector<float>)’ previously defined here    14 | bool issame(vector<float> a, vector<float> b){       |      ^~~~~~ 64a21d8c-756f-4fd7-b53c-4daa2deec5dc.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 64a21d8c-756f-4fd7-b53c-4daa2deec5dc.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main(){       |     ^~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -12,10 +12,6 @@ vector<float> derivative(vector<float> xs){
     return result;
 }
 
-//bool issame(vector<float> a, vector<float> b){
-//    return a == b;
-//}
-
 int main(){
     assert(issame(derivative({1}), {}));
     return 0;
