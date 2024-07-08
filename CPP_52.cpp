cceff7af47aa94ae9d4801483cde3447f21f6572
beep boop <githubbot@vadim.me>
2024-07-09 01:05:21 +0200

    Solution 9 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/i9q2p3si Prompt: 8ce6a11b-ee71-46ae-88b5-96872b9250f5.cpp:1:1: error: expected unqualified-id bef ore ‘for’     1 | for(int num : l){       | ^~~ 8ce6a11b-ee71-46ae-88b5-96872b9250f5.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return true;       |     ^~~~~~ 8ce6a11b-ee71-46ae-88b5-96872b9250f5.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from 8ce6a11b-ee71-46ae-88b5-96872b9250f5.cpp:9: 8ce6a11b-ee71-46ae-88b5-96872b9250f5.cpp: In function ‘int main()’: 8ce6a11b-ee71-46ae-88b5-96872b9250f5.cpp:11:17: error: ‘below_threshold’ was not  declared in this scope    11 |     assert (not(below_threshold({1, 8, 4, 10}, 10)));       |                 ^~~~~~~~~~~~~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -2,8 +2,8 @@
 #include <cassert>
 
 bool below_threshold(std::vector<int> l, int t) {
-    for (int num : l) {
-        if (num >= t) {
+    for(int num : l){
+        if(num >= t){
             return false;
         }
     }
