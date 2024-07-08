5d39a1bb6f8c57249962087d9a136fd1c09998b7
beep boop <githubbot@vadim.me>
2024-07-09 01:28:40 +0200

    Solution 2 of CPP/95, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xlgr9s8v Prompt:       |                     ^~~ 3dba1acb-5fc1-45ae-bafc-334f9a09f669.cpp:1:25: error: ‘string’ was not declared in this scope     1 | bool check_map_case(map<string, string> dict) {       |                         ^~~~~~ 3dba1acb-5fc1-45ae-bafc-334f9a09f669.cpp:1:33: error: ‘string’ was not declared in this scope     1 | bool check_map_case(map<string, string> dict) {       |                                 ^~~~~~ 3dba1acb-5fc1-45ae-bafc-334f9a09f669.cpp:1:41: error: ‘dict’ was not declared in  this scope     1 | bool check_map_case(map<string, string> dict) {       |                                         ^~~~ 3dba1acb-5fc1-45ae-bafc-334f9a09f669.cpp:1:45: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool check_map_case(map<string, string> dict) {       |                                             ^ In file included from 3dba1acb-5fc1-45ae-bafc-334f9a09f669.cpp:22: 3dba1acb-5fc1-45ae-bafc-334f9a09f669.cpp: In function ‘int main()’: 3dba1acb-5fc1-45ae-bafc-334f9a09f669.cpp:24:13: error: ‘check_dict_case’ was not  declared in this scope; did you mean ‘check_map_case’?    24 |     assert (check_dict_case({}) == false);       |             ^~~~~~~~~~~~~~~

--- CPP_95.cpp
+++ CPP_95.cpp
@@ -1,5 +1,8 @@
-bool check_dict_case(map<string, string> dict) {
-    if (dict.empty()) return false;
+#include <string>
+
+bool check_dict_case(std::map<std::string, std::string> dict) {
+    if (dict.empty())
+        return false;
 
     bool allLower = true;
     bool allUpper = true;
@@ -9,13 +12,10 @@ bool check_dict_case(map<string, string> dict) {
             allLower = false;
             allUpper = false;
             break;
-        }
-        else if (!allLower && islower(pair.first[0])) {
-            allUpper = false;
-        }
-        else if (!allUpper && isupper(pair.first[0])) {
+        } else if (allLower && !islower(pair.first[0]))
             allLower = false;
-        }
+        else if (allUpper && !isupper(pair.first[0]))
+            allUpper = false;
     }
 
     return allLower || allUpper;
