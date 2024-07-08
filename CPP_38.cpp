0efb30d0c38578cc5991205a746fea5cb939bb89
beep boop <githubbot@vadim.me>
2024-07-09 00:37:33 +0200

    Solution 7 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/qlvqiwhq Prompt: f2cb06f6-f391-4492-adb4-ca60427aa6e5.cpp:25:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    25 |             str+=chr;       |             ^~~       |             std f2cb06f6-f391-4492-adb4-ca60427aa6e5.cpp:28:15: error: expected ‘;’ before ‘enco ded_str’    28 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from f2cb06f6-f391-4492-adb4-ca60427aa6e5.cpp:15: f2cb06f6-f391-4492-adb4-ca60427aa6e5.cpp:29:31: error: ‘encoded_str’ was not dec lared in this scope    29 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ f2cb06f6-f391-4492-adb4-ca60427aa6e5.cpp:29:17: error: ‘decode_cyclic’ was not d eclared in this scope    29 |         assert (decode_cyclic(encoded_str) == str);       |                 ^~~~~~~~~~~~~ f2cb06f6-f391-4492-adb4-ca60427aa6e5.cpp:29:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    29 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
 #include <string>
 
-string encode_cyclic(string s);
-string decode_cyclic(string s);
-
 string encode_cyclic(string s){
-    int l=s.length();
-    s+=string(-l%3,' ');
-    string x,output;
-    int i;
-    for (i=0;i*3<l;i++)
-    {
-        x=s.substr(i*3,3);
-        if (x.length()==3) x=x[1]+x[2]+x[0];
-        output=output+x;
+    int l = s.length();
+    string output;
+    
+    for (int i = 0; i < l; i++) {
+        string x = s.substr(i, 1);
+        if (x.length() == 1) x = x + s.substr(0, i);
+        
+        output = output + x;
     }
     return output;
 }
 
 string decode_cyclic(string s){
-    int l=s.length();
-    int num=(l+2)/3;
-    string x,output;
+    int l = s.length();
+    int num = (l + 2) / 3;
+    string x, output;
     int i;
-    for (i=0;i*3<l;i++)
-    {
-        x=s.substr(i*3,3);
-        if (x.length()==3) x=x[2]+x.substr(0,2);
-        output=output+x;
+    
+    for (i = 0; i * 3 < l; i++) {
+        x = s.substr(i * 3, 3);
+        if (x.length() == 3) x = x[2] + x.substr(0, 2);
+        output = output + x;
     }
     return output;
-}
+} 
