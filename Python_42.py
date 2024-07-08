61cdf759081cbc9e6b6ef639eeec50705d63b8a3
beep boop <githubbot@vadim.me>
2024-07-09 01:43:23 +0200

    Solution 18 of Python/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/jor3l590 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/40f00bb7-f048-463f-ab2d-0c7428dab 4bf.py", line 5, in <module>     check(increment_list)     ^^^^^ NameError: name 'check' is not defined

--- Python_42.py
+++ Python_42.py
@@ -1,11 +1,11 @@
 def increment_list(l):
     return [x + 1 for x in l]
 
-def check(increment_list):
+def check(func):
     l = [1, 2, 3]
-    if increment_list(l) == [2, 3, 4]:
-        print("Pass")
+    if func(l) == [2, 3, 4]:
+        print("Test passed")
     else:
-        print("Fail")
+        print("Test failed")
 
 check(increment_list)
