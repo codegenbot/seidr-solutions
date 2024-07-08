8b052baa8d19bc8ada73046ec591bbf09f57bc5d
beep boop <githubbot@vadim.me>
2024-07-09 01:32:49 +0200

    Solution 15 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7yh14zdh Prompt: 5aa3dbf9-021e-4127-b8df-09248ce763a2.cpp:3:14: error: ‘string’ was not declared in this scope     3 | int is_bored(string S){       |              ^~~~~~ 5aa3dbf9-021e-4127-b8df-09248ce763a2.cpp:3:14: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 5aa3dbf9-021e-4127-b8df-09248ce763a2.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 5aa3dbf9-021e-4127-b8df-09248ce763a2.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from 5aa3dbf9-021e-4127-b8df-09248ce763a2.cpp:14: 5aa3dbf9-021e-4127-b8df-09248ce763a2.cpp: In function ‘int main()’: 5aa3dbf9-021e-4127-b8df-09248ce763a2.cpp:16:21: error: ‘is_bored’ cannot be used  as a function    16 |     assert (is_bored("You and I are going for a walk") == 0);       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,1 +1,9 @@
 int is_bored(std::string S){
+    int count = 0;
+    size_t pos = 0;
+    while((pos = S.find("I", pos)) != std::string::npos) {
+        if (S[pos] == '.' || S[pos] == '?' || S[pos] == '!') 
+            count++;
+        pos++;
+    }
+    return count;
