64a9eca66396ffed4f80edd882f9d1942d0c9ba8
beep boop <githubbot@vadim.me>
2024-07-09 00:33:54 +0200

    Solution 5 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/y3orfb0a Prompt: 553e1625-7a55-495d-b1b1-db4dbc120210.cpp:20:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    20 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 553e1625-7a55-495d-b1b1-db4dbc120210.cpp:23:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    23 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 553e1625-7a55-495d-b1b1-db4dbc120210.cpp:23:36: error: ‘find_zero’ cannot be use d as a function    23 |         solution = find_zero(coeffs);       |                                    ^ In file included from 553e1625-7a55-495d-b1b1-db4dbc120210.cpp:7: 553e1625-7a55-495d-b1b1-db4dbc120210.cpp:24:21: error: ‘poly’ was not declared i n this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 553e1625-7a55-495d-b1b1-db4dbc120210.cpp:24:17: error: ‘abs’ was not declared in  this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,16 +1,20 @@
-#include <vector>
 #include <cmath>
 
-double coeffs(vector<double> xs){
-    return xs[0];
+vector<double> coeffs() {
+    vector<double> coefficients = {1.0, -3.0, 2.0}; // Example coefficients
+    return coefficients;
 }
 
-double poly(vector<double> xs){
-    return xs[1];
+double poly(vector<double> coeffs, double x) {
+    double result = 0.0;
+    for (int i = 0; i < coeffs.size(); i++) {
+        result += coeffs[i] * pow(x, i);
+    }
+    return result;
 }
 
-double find_zero(vector<double> xs){
-    double a = coeffs(xs);
-    double b = poly(xs);
-    return -b/a;
+double find_zero(vector<double> xs) {
+    double a = xs[0];
+    double b = xs[1];
+    return -b / a;
 }
