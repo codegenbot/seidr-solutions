541e51f2eac454ffd22d9a5951501180d313407b
beep boop <githubbot@vadim.me>
2024-07-09 01:44:13 +0200

    Solution 10 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c6pooyd5 Prompt: 7630d418-19aa-4604-887a-38c01c1a13dc.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 7630d418-19aa-4604-887a-38c01c1a13dc.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 7630d418-19aa-4604-887a-38c01c1a13dc.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 7630d418-19aa-4604-887a-38c01c1a13dc.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ In file included from 7630d418-19aa-4604-887a-38c01c1a13dc.cpp:19: 7630d418-19aa-4604-887a-38c01c1a13dc.cpp: In function ‘int main()’: 7630d418-19aa-4604-887a-38c01c1a13dc.cpp:21:13: error: ‘exchange’ was not declar ed in this scope    21 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,49 +1,17 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-string exchange(vector<int> lst1, vector<int> lst2) {
-    int sum = 0;
-    for (int num : lst2) {
+```cpp
+int exchange(vector<int> lst1, vector<int> lst2) {
+    for (int num : lst1) {
         if (num % 2 != 0) {
-            return "NO";
-        }
-        sum += num;
-    }
-
-    if(sum%4!=0){
-        return "NO";
-    }
-
-    for (int i = 0; i < lst1.size(); i++) {
-        if (std::find(lst2.begin(), lst2.end(), lst1[i]) == lst2.end()) {
-            return "NO";
+            bool found = false;
+            for (int num2 : lst2) {
+                if (num2 % 2 == 0) {
+                    swap(lst1.back(), lst2.back());
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) return 0;
         }
     }
-
-    return "YES";
-}
-
-int main() {
-    vector<int> lst1;
-    vector<int> lst2;
-
-    int n, m;
-    cin >> n >> m;
-
-    for (int i = 0; i < n; i++) {
-        int num;
-        cin >> num;
-        lst1.push_back(num);
-    }
-
-    for (int i = 0; i < m; i++) {
-        int num;
-        cin >> num;
-        lst2.push_back(num);
-    }
-
-    cout << exchange(lst1, lst2) << endl;
-
-    return 0;
+    return 1;
 }
