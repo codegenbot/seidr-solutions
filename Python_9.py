74d6185aaf316680dd9de99e0fdd37b897a19808
beep boop <githubbot@vadim.me>
2024-07-09 00:41:52 +0200

    Solution 28 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/wermr37h Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b3010715-73fc-4510-b8a2-755763669 253.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_9.py
+++ Python_9.py
@@ -1,11 +1,8 @@
 ```
-from typing import List
-
-
 def rolling_max(numbers: List[int]) -> List[int]:
-    max_rolling_sum = []
-    current_sum = 0
+    current_max = float('-inf')
+    max_rolling_sum = [current_max]
     for num in numbers:
-        current_sum = max(0, current_sum + num)
-        max_rolling_sum.append(current_sum)
+        current_max = max(current_max, num)
+        max_rolling_sum.append(current_max)
     return max_rolling_sum
