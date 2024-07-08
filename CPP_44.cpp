d26d337eb87b26448b8c1552226c07104000318b
beep boop <githubbot@vadim.me>
2024-07-09 00:44:41 +0200

    Solution 7 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/fcucnqjl Prompt: c0abbdb8-c113-4b21-bff5-20c364fa94aa.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string change_base(int x, int base){       | ^~~~~~ In file included from c0abbdb8-c113-4b21-bff5-20c364fa94aa.cpp:10: c0abbdb8-c113-4b21-bff5-20c364fa94aa.cpp: In function ‘int main()’: c0abbdb8-c113-4b21-bff5-20c364fa94aa.cpp:14:17: error: ‘change_base’ was not dec lared in this scope    14 |         assert (change_base(x, x + 1) == to_string(x));       |                 ^~~~~~~~~~~ c0abbdb8-c113-4b21-bff5-20c364fa94aa.cpp:14:42: error: ‘to_string’ was not decla red in this scope    14 |         assert (change_base(x, x + 1) == to_string(x));       |                                          ^~~~~~~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -7,8 +7,8 @@ using namespace std;
 string change_base(int x, int base){
     string result = "";
     while(x > 0){
-        result = to_string(x % base) + result;
-        x /= base;
+    result = to_string(x % base) + result;
+    x /= base;
     }
     return result;
 }
