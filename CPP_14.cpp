f3a3a3c0c88e4ff8b8d72504834186ac3db0366e
beep boop <githubbot@vadim.me>
2024-07-09 00:43:02 +0200

    Solution 58 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dib3n468 Prompt:       |           ^~~~~~ 7f2f2803-5719-4b55-9f6f-91483838fbf8.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7f2f2803-5719-4b55-9f6f-91483838fbf8.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 7f2f2803-5719-4b55-9f6f-91483838fbf8.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 7f2f2803-5719-4b55-9f6f-91483838fbf8.cpp:28: 7f2f2803-5719-4b55-9f6f-91483838fbf8.cpp: In function ‘int main()’: 7f2f2803-5719-4b55-9f6f-91483838fbf8.cpp:38:20: error: ‘all_prefixes’ was not de clared in this scope    38 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ 7f2f2803-5719-4b55-9f6f-91483838fbf8.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,8 +1,5 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
 #include <vector>
-#include <initializer_list>
+#include <string>
 
 bool issame(vector<string> a, vector<string> b) {
     if(a.size() != b.size())
@@ -14,14 +11,14 @@ bool issame(vector<string> a, vector<string> b) {
 }
 
 vector<string> all_prefixes(const string& str) {
-    vector<string> result({""});
+    vector<string> result;
     for (int i = 1; i <= str.length(); i++) {
         result.push_back(str.substr(0, i));
     }
     return result;
 }
 
-int main() {
-    assert(issame(all_prefixes("WWW"), vector<string>{"W", "WW", "WWW"}));
+bool main() {
+    assert(issame(all_prefixes("WWW"), vector<string>({"", "W", "WW", "WWW"})));
     return 0;
 }
