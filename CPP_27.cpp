389b2ef456b60576d0fdeb3154a01cd46dff9980
beep boop <githubbot@vadim.me>
2024-07-09 00:56:50 +0200

    Solution 62 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/471qj8n6 Prompt: a4896637-a9e8-4a78-a172-6aaa119fbc4d.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ a4896637-a9e8-4a78-a172-6aaa119fbc4d.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cassert>
 
-std::string filp_case(std::string str){
+std::string flip_case(std::string str){
     std::string result = "";
     for(int i=0; i<str.length(); i++){
         if(str[i] >= 'a' && str[i] <= 'z'){
@@ -15,6 +15,7 @@ std::string filp_case(std::string str){
 }
 
 int main() {
-    assert(filp_case("These violent delights have violent ends") == "these violent delights have violent ends");
-    std::cout << filp_case("Hello, world!") << std::endl;
+    assert(flip_case("These violent delights have violent ends") == "these violent delights have violent ends");
+    std::cout << flip_case("Hello, world!") << std::endl;
     return 0;
+}
