b18505e043b07df22aca32e92f1c873a75d41454
beep boop <githubbot@vadim.me>
2024-07-09 01:03:53 +0200

    Solution 31 of CPP/89, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/6mywfhb5 Prompt: a889abbd-1437-482a-8aaf-bf13c9c2b035.cpp:4:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     3 | #include <cctype>   +++ |+#include <iostream>     4 | a889abbd-1437-482a-8aaf-bf13c9c2b035.cpp:9:50: error: ‘endl’ is not a member of ‘std’     9 |     std::cout << encrypt("Hello World!") << std::endl;       |                                                  ^~~~ a889abbd-1437-482a-8aaf-bf13c9c2b035.cpp:4:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     3 | #include <cctype>   +++ |+#include <ostream>     4 | a889abbd-1437-482a-8aaf-bf13c9c2b035.cpp: At global scope: a889abbd-1437-482a-8aaf-bf13c9c2b035.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ a889abbd-1437-482a-8aaf-bf13c9c2b035.cpp:7:5: note: ‘int main()’ previously defi ned here     7 | int main() {       |     ^~~~

--- CPP_89.cpp
+++ CPP_89.cpp
@@ -1,7 +1,17 @@
+#include <cassert>
+#include <string>
 #include <cctype>
+#include <iostream>
 
-string encrypt(string s) {
-    string result = "";
+std::string encrypt(std::string s);
+
+int test_main() {
+    assert(encrypt("a") == "e");
+    std::cout << encrypt("Hello World!") << std::endl;
+}
+
+std::string encrypt(std::string s) {
+    std::string result = "";
     for (char c : s) {
         if (isalpha(c)) {
             char base = isupper(c) ? 'A' : 'a';
