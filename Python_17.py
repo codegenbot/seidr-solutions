afa329c0f9ced6c145c3c294f33c88488c4039ef
beep boop <githubbot@vadim.me>
2024-07-09 00:47:47 +0200

    Solution 28 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kcw3rfr5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ba7d9c5b-f3e7-42f6-add0-982229ca4 60c.py", line 2     def check(parse_music):     ^ IndentationError: expected an indented block after function definition on line 1

--- Python_17.py
+++ Python_17.py
@@ -1,20 +1,3 @@
-```
-def check(temp: str) -> bool:
-    for char in temp:
-        if not char.isalpha() and not char.isdigit():
-            return False
-    return True
-
-
 def parse_music(music_string: str) -> List[int]:
-    result = []
-    notes = music_string.split(',')
-
-    for note in notes:
-        if check(note):
-            note_value = int(note.strip().split(' ')[1])
-            result.append(note_value)
-        else:
-            break
-
-    return result
+    parts = music_string.split(";")
+    return [int(part.strip().split()[1]) for part in parts]
