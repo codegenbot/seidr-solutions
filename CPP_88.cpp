71f3de357ef325d1276af12463e96012eabad38f
beep boop <githubbot@vadim.me>
2024-07-09 00:52:59 +0200

    Solution 19 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/qhscq9va Prompt:       |             ^~~~~~ a6d50d76-1fab-418a-8cd1-6d9a56eff9b8.cpp:41:34: error: expected primary-expressi on before ‘int’    41 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a6d50d76-1fab-418a-8cd1-6d9a56eff9b8.cpp:41:39: error: expression list treated a s compound expression in initializer [-fpermissive]    41 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a6d50d76-1fab-418a-8cd1-6d9a56eff9b8.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ a6d50d76-1fab-418a-8cd1-6d9a56eff9b8.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from a6d50d76-1fab-418a-8cd1-6d9a56eff9b8.cpp:40: a6d50d76-1fab-418a-8cd1-6d9a56eff9b8.cpp: In function ‘int main()’: a6d50d76-1fab-418a-8cd1-6d9a56eff9b8.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,6 +1,14 @@
 #include <algorithm>
 #include <vector>
 
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) return false;
+    }
+    return true;
+}
+
 std::vector<int> sort_array(std::vector<int> array) {
     if (array.empty()) return array;
 
@@ -23,14 +31,6 @@ std::vector<int> sort_array(std::vector<int> array) {
     sortedArray.insert(sortedArray.end(), left, middle);
 
     return sortedArray;
-}
-
-bool issame(vector<int> a, vector<int>b) {
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
-    }
-    return true;
 }
 
 int main() {
