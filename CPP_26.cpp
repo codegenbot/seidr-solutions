47ebfdd38c3f24e7869d2eded61b1a2c9839bdc5
beep boop <githubbot@vadim.me>
2024-07-09 00:32:47 +0200

    Solution 11 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/hy8cse5r Prompt:                  from /usr/include/c++/11/algorithm:74,                  from 22dfbdd6-8ab4-43b4-a312-92c17a485872.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 22dfbdd6-8ab4-43b4-a312-92c17a485872.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 22dfbdd6-8ab4-43b4-a312-92c17a485872.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 22dfbdd6-8ab4-43b4-a312-92c17a485872.cpp:15: 22dfbdd6-8ab4-43b4-a312-92c17a485872.cpp: In function ‘int main()’: 22dfbdd6-8ab4-43b4-a312-92c17a485872.cpp:25:20: error: ‘remove_duplicates’ was n ot declared in this scope    25 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,12 +1,13 @@
 #include <algorithm>
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
 vector<int> remove_duplicates(const vector<int>& numbers);
 
 bool issame(const vector<int>& a, const vector<int>& b) {
-    return (std::equal(a.begin(), a.end(), b.begin(), b.end()));
+    return (a == b);
 }
 
 vector<int> remove_duplicates(const vector<int>& numbers) {
