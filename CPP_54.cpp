b649450ef3f1992a4e6d37f7bd4a3aff4f9cace7
beep boop <githubbot@vadim.me>
2024-07-09 00:55:08 +0200

    Solution 50 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9itap37f Prompt: 9c79892f-a6c6-4031-9dbd-80226a2726a2.cpp: In function ‘int main()’: 9c79892f-a6c6-4031-9dbd-80226a2726a2.cpp:12:5: error: ‘assert’ was not declared in this scope    12 |     assert(same_chars("aabb", "aaccc") == false);       |     ^~~~~~ 9c79892f-a6c6-4031-9dbd-80226a2726a2.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <string>   +++ |+#include <cassert>     3 | #include <initializer_list> 9c79892f-a6c6-4031-9dbd-80226a2726a2.cpp: At global scope: 9c79892f-a6c6-4031-9dbd-80226a2726a2.cpp:16:5: error: redefinition of ‘int main( )’    16 | int main(){       |     ^~~~ 9c79892f-a6c6-4031-9dbd-80226a2726a2.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -1,6 +1,5 @@
-Here is the modified code:
-
 ```cpp
+#include <cassert>
 #include <algorithm>
 #include <string>
 #include <initializer_list>
@@ -9,10 +8,8 @@ bool same_chars(std::string s0, std::string s1) {
     sort(s0.begin(), s0.end());
     sort(s1.begin(), s1.end());
     return (s0 == s1);
-
 }
 
 int main() {
     assert(same_chars("aabb", "aaccc") == false);
-    
 }
