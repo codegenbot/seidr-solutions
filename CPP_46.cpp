aa43998bc282e72354a9460e96477a37851fdddd
beep boop <githubbot@vadim.me>
2024-07-09 00:51:36 +0200

    Solution 55 of CPP/46, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/8iwckk56 Prompt:       | ^~~~~~~~~~~~~ 1dd05220-e852-4a97-b5e0-de80f0062bde.cpp:26:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    26 | int main(){       |         ^~ 1dd05220-e852-4a97-b5e0-de80f0062bde.cpp:26:9: note: remove parentheses to defau lt-initialize a variable    26 | int main(){       |         ^~       |         -- 1dd05220-e852-4a97-b5e0-de80f0062bde.cpp:26:9: note: or replace parentheses with  braces to value-initialize a variable 1dd05220-e852-4a97-b5e0-de80f0062bde.cpp:26:11: error: a function-definition is not allowed here before ‘{’ token    26 | int main(){       |           ^ 1dd05220-e852-4a97-b5e0-de80f0062bde.cpp:28:2: error: expected ‘}’ at end of inp ut    28 | }       |  ^ 1dd05220-e852-4a97-b5e0-de80f0062bde.cpp:18:12: note: to match this ‘{’    18 | int main() {       |            ^

--- CPP_46.cpp
+++ CPP_46.cpp
@@ -1,3 +1,4 @@
+```
 #include <iostream>
 #include <cstdio>
 
@@ -15,9 +16,10 @@ int fib4(int n) {
     return d;
 }
 
-int main() {
+int main() 
+{
     int n;
     std::cout << "Enter the value of n: ";
     std::cin >> n;
     std::cout << "The " << n << "-th element of the fib4 number sequence is: " << fib4(n) << std::endl;
-}
+};
