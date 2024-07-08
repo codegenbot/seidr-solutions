7346ccffb4ef39b4a7dc4326a15077dda132aca7
beep boop <githubbot@vadim.me>
2024-07-09 00:54:00 +0200

    Solution 63 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/80df1iq3 Prompt: 10f0faae-c292-4261-a613-8640d549c102.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 10f0faae-c292-4261-a613-8640d549c102.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 10f0faae-c292-4261-a613-8640d549c102.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 10f0faae-c292-4261-a613-8640d549c102.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 10f0faae-c292-4261-a613-8640d549c102.cpp:24: 10f0faae-c292-4261-a613-8640d549c102.cpp: In function ‘int main()’: 10f0faae-c292-4261-a613-8640d549c102.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -16,6 +16,6 @@ std::vector<int> filter_integers(const std::vector<std::any>& values) {
 }
 
 int main() {
-    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), std::vector<int>{3, 3, 3}));
+    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
     return 0;
 }
