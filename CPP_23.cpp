8ce0e77d26de60fd0de1a5a011d447dbc7e25eff
beep boop <githubbot@vadim.me>
2024-07-09 00:36:57 +0200

    Solution 47 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nrmo6kgd Prompt: 61ff8170-9302-4f02-bc4f-59ba89b7c8e9.cpp: In function ‘int main()’: 61ff8170-9302-4f02-bc4f-59ba89b7c8e9.cpp:10:5: error: ‘assert’ was not declared in this scope    10 |     assert(my_strlen(std::string("asdasnakj")) == 13);       |     ^~~~~~ 61ff8170-9302-4f02-bc4f-59ba89b7c8e9.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <cstring>   +++ |+#include <cassert>     4 | 61ff8170-9302-4f02-bc4f-59ba89b7c8e9.cpp: At global scope: 61ff8170-9302-4f02-bc4f-59ba89b7c8e9.cpp:16:5: error: redefinition of ‘int main( )’    16 | int main(){       |     ^~~~ 61ff8170-9302-4f02-bc4f-59ba89b7c8e9.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,4 +1,3 @@
-```c
 #include <cassert>
 #include <iostream>
 #include <string>
@@ -9,7 +8,7 @@ int my_strlen(std::string str) {
 }
 
 int main() {
-    assert(my_strlen(std::string("asdasnakj")) == 13);
-    std::cout << "The length of 'asdasnakj' is: " << my_strlen(std::string("asdasnakj")) << "\n";
+    assert(my_strlen("asdasnakj") == 13);
+    std::cout << "The length of 'asdasnakj' is: " << my_strlen("asdasnakj") << "\n";
     return 0;
 }
