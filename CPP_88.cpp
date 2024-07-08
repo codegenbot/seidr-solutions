8a134554894017650a75f0b24a0d9dc5272bea97
beep boop <githubbot@vadim.me>
2024-07-09 01:42:48 +0200

    Solution 78 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/qhscq9va Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 07a7be31-f205-42bf-b80c-bbc16207d9f7.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -35,5 +35,5 @@ int main() {
     }
     std::cout << array[0] + array.back() << std::endl;
     
-    return (array != std::vector<int>( {23, 21, 14, 11} ) ? 1 : 0);
+    return (array != std::vector<int>{23, 21, 14, 11}) ? 1 : 0;
 }
