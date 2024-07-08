9d1a20230af1b0469d2731b47e193d2dc2574b96
beep boop <githubbot@vadim.me>
2024-07-09 00:29:00 +0200

    Solution 8 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/qbblnf5y Prompt:       |                              ^~~~~~ a13d7a9c-d203-4835-9626-7d92b92c3507.cpp:12:37: error: ‘string’ was not declared  in this scope    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a13d7a9c-d203-4835-9626-7d92b92c3507.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a13d7a9c-d203-4835-9626-7d92b92c3507.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a13d7a9c-d203-4835-9626-7d92b92c3507.cpp:11: a13d7a9c-d203-4835-9626-7d92b92c3507.cpp: In function ‘int main()’: a13d7a9c-d203-4835-9626-7d92b92c3507.cpp:21:20: error: ‘all_prefixes’ was not de clared in this scope    21 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ a13d7a9c-d203-4835-9626-7d92b92c3507.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -5,21 +5,21 @@
 
 using namespace std;
 
-vector<string> all_prefixes(string str){
+vector<string> all_prefixes(string str) {
     vector<string> result;
     string prefix = "";
-    for(char c : str){
+    for (char c : str) {
         prefix += c;
         result.push_back(prefix);
     }
     return result;
 }
 
-bool issame(vector<string> a, vector<string> b){
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-int main(){
+int main() {
     assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
     
     return 0;
