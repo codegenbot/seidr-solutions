ede703a33f44d3cabedd9ce5d40730eefca133ee
beep boop <githubbot@vadim.me>
2024-07-09 01:59:19 +0200

    Solution 42 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5hsg63q3 Prompt: e    28 |     cout << "Count: " << count_nums({-1234, 5678, -9012}) << endl;       |     ^~~~ 9e04d9b1-a5b4-4dd1-a1b2-20d40c82cb68.cpp:28:57: error: expected unqualified-id b efore ‘)’ token    28 |     cout << "Count: " << count_nums({-1234, 5678, -9012}) << endl;       |                                                         ^ 9e04d9b1-a5b4-4dd1-a1b2-20d40c82cb68.cpp:29:5: error: expected unqualified-id be fore ‘return’    29 |     return 0;       |     ^~~~~~ 9e04d9b1-a5b4-4dd1-a1b2-20d40c82cb68.cpp:30:1: error: expected declaration befor e ‘}’ token    30 | }       | ^ 9e04d9b1-a5b4-4dd1-a1b2-20d40c82cb68.cpp:33:10: error: ‘int main()’ redeclared a s different kind of entity    33 | int main(){       |          ^ 9e04d9b1-a5b4-4dd1-a1b2-20d40c82cb68.cpp:25:5: note: previous declaration ‘int m ain’    25 | int main       |     ^~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,5 +1,27 @@
-int main
-{
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int count_nums(vector<int> nums) {
+    int sign = 1;
+    int count = 0;
+    for (int num : nums) {
+        if(num >= 0)
+            sign = 1;
+        else
+            sign = -1;
+
+        while(abs(num) > 0) {
+            int digit = abs(num) % 10 * sign;
+            if(digit > 0 || (digit == 1 && sign == 1))
+                count++;
+            num /= 10;
+        }
+    }
+    return count;
+}
+
+int main() {
     assert(count_nums({1}) == 1);
     cout << "Count: " << count_nums({-1234, 5678, -9012}) << endl;
     return 0;
