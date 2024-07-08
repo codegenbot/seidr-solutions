89ceaba7f5517e1f219dd512f833da21eb571d3b
beep boop <githubbot@vadim.me>
2024-07-09 01:17:29 +0200

    Solution 4 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/p33wwvjw Prompt: d4323aa2-2d61-42cc-9c8a-84901f10846b.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ d4323aa2-2d61-42cc-9c8a-84901f10846b.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ d4323aa2-2d61-42cc-9c8a-84901f10846b.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,11 +1,8 @@
-#include <map>
 #include <string>
-
-int mastermind(string code, string guess) {
+int mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
 
-    // Count the number of correct colors in incorrect positions
     map<char, int> codeCount;
     map<char, int> guessCount;
     for (int i = 0; i < 4; ++i) {
@@ -18,7 +15,6 @@ int mastermind(string code, string guess) {
         }
     }
 
-    // Count the number of correct colors in correct positions
     int codeIndex = 0;
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
@@ -27,5 +23,4 @@ int mastermind(string code, string guess) {
         }
     }
 
-    return white + black;
-}
+    return black;
