0e6ea1019b43871b831a4326bf2c74b865ebb826
beep boop <githubbot@vadim.me>
2024-07-09 01:06:19 +0200

    Solution 66 of CPP/54, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9itap37f Prompt: 0e5c0e05-55d7-45c9-ad8f-f2d22006907a.cpp:17:5: error: redefinition of ‘int main( )’    17 | int main(){       |     ^~~~ 0e5c0e05-55d7-45c9-ad8f-f2d22006907a.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <cassert>
 #include <algorithm>
 #include <string>
@@ -7,8 +6,4 @@ bool same_chars(std::string s0, std::string s1) {
     sort(s0.begin(), s0.end());
     sort(s1.begin(), s1.end());
     return (s0 == s1);
-}
-
-int main() {
-    assert(same_chars("aabb", "aaccc") == false);
 }
