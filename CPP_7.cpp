327636b31589da5a49e68eb84da419b17612e9d2
beep boop <githubbot@vadim.me>
2024-07-09 00:12:56 +0200

    Solution 3 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/z16tspui Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 269903d4-bd40-468f-837c-3c7e8e9756f5.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 269903d4-bd40-468f-837c-3c7e8e9756f5.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 269903d4-bd40-468f-837c-3c7e8e9756f5.cpp:11: 269903d4-bd40-468f-837c-3c7e8e9756f5.cpp: In function ‘int main()’: 269903d4-bd40-468f-837c-3c7e8e9756f5.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 269903d4-bd40-468f-837c-3c7e8e9756f5.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -7,9 +7,10 @@ vector<string> filter_by_substring(vector<string> strings, string substring){
     }
     return result;
 
-    bool issame(vector<string> a, vector<string> b){
-        if(a.size() != b.size()) return false;
-        for(auto s : a) if(find(b.begin(),b.end(),s) == b.end()) return false;
-        return true;
-    }
+}
+
+bool issame(vector<string> a, vector<string> b){
+    if(a.size() != b.size()) return false;
+    for(auto s : a) if(find(b.begin(),b.end(),s) == b.end()) return false;
+    return true;
 }
