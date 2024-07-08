ec6b203284d0b9acab388fcab900af5cdfc89853
beep boop <githubbot@vadim.me>
2024-07-09 00:39:19 +0200

    Solution 15 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/qlvqiwhq Prompt: f50f216e-2487-485f-85f4-cc7d73fd71e1.cpp:24:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    24 |             str+=chr;       |             ^~~       |             std f50f216e-2487-485f-85f4-cc7d73fd71e1.cpp:27:15: error: expected ‘;’ before ‘enco ded_str’    27 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from f50f216e-2487-485f-85f4-cc7d73fd71e1.cpp:14: f50f216e-2487-485f-85f4-cc7d73fd71e1.cpp:28:31: error: ‘encoded_str’ was not dec lared in this scope    28 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ f50f216e-2487-485f-85f4-cc7d73fd71e1.cpp:28:17: error: ‘decode_cyclic’ was not d eclared in this scope    28 |         assert (decode_cyclic(encoded_str) == str);       |                 ^~~~~~~~~~~~~ f50f216e-2487-485f-85f4-cc7d73fd71e1.cpp:28:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    28 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -1,1 +1,27 @@
-string decode_cyclic(string s);
+#include <string>
+
+string encode_cyclic(string s) {
+    string encoded_str;
+    int l = s.length();
+    for (int i = 0; i * 3 < l; i++) {
+        string x = s.substr(i * 3, 3);
+        if (x.length() == 3) {
+            x = x.substr(1) + x[0];
+        }
+        encoded_str += x;
+    }
+    return encoded_str;
+}
+
+string decode_cyclic(string s) {
+    string decoded_str;
+    int l = s.length();
+    for (int i = 0; i < l; i++) {
+        string x = s.substr(i, 1);
+        if (i % 3 == 2) {
+            x = x[x.length() - 1] + x.substr(0, 1);
+        }
+        decoded_str += x;
+    }
+    return decoded_str;
+}
