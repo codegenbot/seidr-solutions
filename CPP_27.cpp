0e08ef136cf873deb214d89b7a69a1567371244b
beep boop <githubbot@vadim.me>
2024-07-09 01:04:27 +0200

    Solution 72 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/471qj8n6 Prompt: 7d42f6b1-ca34-4b24-97d7-ccd8877b9977.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 7d42f6b1-ca34-4b24-97d7-ccd8877b9977.cpp: In function ‘int main()’: 7d42f6b1-ca34-4b24-97d7-ccd8877b9977.cpp:19:5: error: ‘assert’ was not declared in this scope    19 |     assert(filp_case("These violent delights have violent ends") == "the se violent delights have violent ends");       |     ^~~~~~ 7d42f6b1-ca34-4b24-97d7-ccd8877b9977.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <cctype>   +++ |+#include <cassert>     5 | 7d42f6b1-ca34-4b24-97d7-ccd8877b9977.cpp: At global scope: 7d42f6b1-ca34-4b24-97d7-ccd8877b9977.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ 7d42f6b1-ca34-4b24-97d7-ccd8877b9977.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -7,10 +7,14 @@
 std::string filp_case(std::string str){
     std::string result = "";
     for(int i=0; i<str.length(); i++){
-        if(str[i] >= 'a' && str[i] <= 'z'){
-            result += std::toupper((char)str[i]);
-        } else {
-            result += std::tolower((char)str[i]);
+        if(std::isalpha(str[i])) {
+            if(std::islower(str[i]))
+                result += std::toupper((char)str[i]);
+            else
+                result += std::tolower((char)str[i]);
+        } 
+        else {
+            result += str[i];
         }
     }
     return result;
