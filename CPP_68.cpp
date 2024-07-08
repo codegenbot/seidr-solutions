d78fbfde22c23e5ca5bafbb4a0533afe3ec6a247
beep boop <githubbot@vadim.me>
2024-07-09 01:00:40 +0200

    Solution 34 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/055ljvw3 Prompt:       |                                  ^~~ c32d1825-a7e4-4cd0-b9a3-cb47723b2b36.cpp:36:39: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<int> a,vector<int>b){       |                                       ^ c32d1825-a7e4-4cd0-b9a3-cb47723b2b36.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ c32d1825-a7e4-4cd0-b9a3-cb47723b2b36.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from c32d1825-a7e4-4cd0-b9a3-cb47723b2b36.cpp:35: c32d1825-a7e4-4cd0-b9a3-cb47723b2b36.cpp: In function ‘int main()’: c32d1825-a7e4-4cd0-b9a3-cb47723b2b36.cpp:45:20: error: ‘pluck’ was not declared in this scope    45 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ c32d1825-a7e4-4cd0-b9a3-cb47723b2b36.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,37 +1,30 @@
 ```cpp
-namespace Solution {
-    std::vector<std::pair<int, int>> pluck(std::vector<int> arr) {
-        std::vector<std::pair<int, int>> evenNodes;
+std::vector<std::pair<int, int>> findMinEvenNode(std::vector<int> arr) {
+    std::vector<std::pair<int, int>> evenNodes;
 
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] % 2 == 0) {
-                evenNodes.push_back({arr[i], i});
-            }
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] % 2 == 0) {
+            evenNodes.push_back({arr[i], i});
         }
-
-        if (evenNodes.empty()) {
-            return {};
-        }
-
-        int minEvenValue = evenNodes[0].first;
-        int minIndex = evenNodes[0].second;
-
-        for (int i = 1; i < evenNodes.size(); i++) {
-            if (evenNodes[i].first < minEvenValue) {
-                minEvenValue = evenNodes[i].first;
-                minIndex = evenNodes[i].second;
-            } else if (evenNodes[i].first == minEvenValue) {
-                minIndex = std::min(minIndex, evenNodes[i].second);
-            }
-        }
-
-        std::vector<std::pair<int, int>> result;
-        result.push_back({minEvenValue, minIndex});
-        return result;
     }
 
-    int main() {
-        assert(pluck({7, 9, 7, 1}) == {});
-        return 0;
+    if (evenNodes.empty()) {
+        return {};
     }
+
+    int minEvenValue = evenNodes[0].first;
+    int minIndex = evenNodes[0].second;
+
+    for (int i = 1; i < evenNodes.size(); i++) {
+        if (evenNodes[i].first < minEvenValue) {
+            minEvenValue = evenNodes[i].first;
+            minIndex = evenNodes[i].second;
+        } else if (evenNodes[i].first == minEvenValue) {
+            minIndex = std::min(minIndex, evenNodes[i].second);
+        }
+    }
+
+    std::vector<std::pair<int, int>> result;
+    result.push_back({minEvenValue, minIndex});
+    return result;
 }
