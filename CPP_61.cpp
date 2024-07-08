c54193ec3739cec78771e5d5daef1a88b9e70569
beep boop <githubbot@vadim.me>
2024-07-09 01:07:03 +0200

    Solution 40 of CPP/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/64crz455 Prompt: its<std::allocator<_CharT> >::pointer = char*; std::allocator_traits<std::alloca tor<_CharT> >::allocator_type = std::allocator<char>]’ /usr/include/c++/11/bits/stl_deque.h:562:26:   required from ‘std::_Deque_base<_ Tp, _Alloc>::_Ptr std::_Deque_base<_Tp, _Alloc>::_M_allocate_node() [with _Tp = char; _Alloc = std::allocator<char>; std::_Deque_base<_Tp, _Alloc>::_Ptr = char* ]’ /usr/include/c++/11/bits/stl_deque.h:663:21:   required from ‘void std::_Deque_b ase<_Tp, _Alloc>::_M_create_nodes(std::_Deque_base<_Tp, _Alloc>::_Map_pointer, s td::_Deque_base<_Tp, _Alloc>::_Map_pointer) [with _Tp = char; _Alloc = std::allo cator<char>; std::_Deque_base<_Tp, _Alloc>::_Map_pointer = char**]’ /usr/include/c++/11/bits/stl_deque.h:637:4:   required from ‘void std::_Deque_ba se<_Tp, _Alloc>::_M_initialize_map(size_t) [with _Tp = char; _Alloc = std::alloc ator<char>; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_deque.h:439:9:   required from ‘std::_Deque_base<_T p, _Alloc>::_Deque_base() [with _Tp = char; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/stl_deque.h:834:7:   required from ‘std::stack<_Tp, _Se quence>::stack() [with _Seq = std::deque<char, std::allocator<char> >; _Requires  = void; _Tp = char; _Sequence = std::deque<char, std::allocator<char> >]’ 93c31d22-b57a-40da-8147-3c424441ac67.cpp:7:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<char>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -1,9 +1,12 @@
+Here is the modified code:
+
+```cpp
 #include <iostream>
 #include <stack>
 #include <string>
 
 bool correct_bracketing(std::string s) {
-    std::stack<char> stack(std::stack<char>::c_type());
+    std::stack<char, std::vector<char>> stack;
     for(int i=0;i<s.length();i++){
         if(s[i] == '(')stack.push('(');
         else if(s[i] == ')'){
