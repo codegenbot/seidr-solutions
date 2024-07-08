d51f76786765db08146901964ca78fb9ee0375b2
beep boop <githubbot@vadim.me>
2024-07-09 00:15:01 +0200

    Solution 10 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7x526egu Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from bd8b8cbf-ae24-444b-8627-e21cc5048c24.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,27 +1,20 @@
 ```cpp
 #include <vector>
 #include <string>
+#include <initializer_list>
 
-bool isSame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
 }
 
 vector<string> all_prefixes(string str) {
     vector<string> result;
-    for (int i = 1; i <= str.length(); ++i) {
+    for (int i = 1; i <= str.length(); i++) {
         result.push_back(str.substr(0, i));
     }
     return result;
 }
 
 int main() {
-    assert(isSame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    assert(issame(all_prefixes("WWW") , {"W", "WW", "WWW"}));
 }
