907e644119fd4ab3e169441d0df2669c1fee27af
beep boop <githubbot@vadim.me>
2024-07-09 01:37:55 +0200

    Solution 46 of CPP/102, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/einmeurw Prompt: 24654a18-63d3-4fac-ae01-128c69d0c872.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 24654a18-63d3-4fac-ae01-128c69d0c872.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_102.cpp
+++ CPP_102.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 
 int choose_num(int x, int y) {
-    stringstream result;
-    string res = "";
+    string result = "";
     for (int i = x; i <= y; i++) {
         if (i % 2 == 0)
-            res += to_string(i) + " ";
+            result += to_string(i) + " ";
     }
-    return stoi(res);
+    return stoi(result);
 }
 
 int main() {
@@ -17,5 +16,5 @@ int main() {
     cin >> x;
     cout << "Enter value of y: ";
     cin >> y;
-    cout << "The biggest even integer is: " << choose_num(x,y) - 1 << endl;
+    cout << "The biggest even integer is: " << choose_num(x,y) << endl;
 }
