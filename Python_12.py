acb369da36a3ab05446d1e56df362ee525297964
beep boop <githubbot@vadim.me>
2024-07-09 00:24:05 +0200

    Solution 11 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/odg1tedz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/836cfb7d-6cab-492d-a81a-4ce421704 f02.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_12.py
+++ Python_12.py
@@ -3,7 +3,7 @@ def longest(strings: List[str]) -> Optional[str]:
     if not strings:
         return None
     max_length = 0
-    longest_str = ""
+    longest_str = None
     for s in strings:
         if len(s) > max_length:
             max_length = len(s)
