94ea3a432f095fa0b36882892fbd9e87bf7c4954
beep boop <githubbot@vadim.me>
2024-07-09 01:38:59 +0200

    Solution 15 of CPP/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/vmm4yi2z Prompt:     9 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ bf9bdd2f-ac6f-4d55-9602-d01f3cbc645a.cpp:9:37: error: ‘string’ was not declared in this scope     9 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bf9bdd2f-ac6f-4d55-9602-d01f3cbc645a.cpp:9:44: error: ‘b’ was not declared in th is scope     9 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from bf9bdd2f-ac6f-4d55-9602-d01f3cbc645a.cpp:8: bf9bdd2f-ac6f-4d55-9602-d01f3cbc645a.cpp: In function ‘int main()’: bf9bdd2f-ac6f-4d55-9602-d01f3cbc645a.cpp:18:20: error: ‘words_string’ was not de clared in this scope    18 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |                    ^~~~~~~~~~~~ bf9bdd2f-ac6f-4d55-9602-d01f3cbc645a.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~

--- CPP_101.cpp
+++ CPP_101.cpp
@@ -1,6 +1,11 @@
-```cpp
+#include <vector>
+using namespace std;
+
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) return false;
-    else for(int i = 0; i < a.size(); i++) if(a[i] != b[i]) return false;
+    if(a.size() != b.size()) 
+        return false;
+    else for(int i = 0; i < a.size(); i++) 
+        if(a[i] != b[i]) 
+            return false;
     return true;
 }
