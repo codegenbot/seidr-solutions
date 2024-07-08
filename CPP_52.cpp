e026b772822848599673245eacd5e5c7910bd03e
beep boop <githubbot@vadim.me>
2024-07-09 00:47:19 +0200

    Solution 30 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/d8r3nlix Prompt: 33343609-580f-4a63-b2c9-1a0688a5d968.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 33343609-580f-4a63-b2c9-1a0688a5d968.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <initializer_list>
 #include <cassert>
 
 bool below_threshold(std::vector<int> l, int t) {
@@ -10,10 +9,10 @@ bool below_threshold(std::vector<int> l, int t) {
         }
     }
     return true;
+
 }
 
 int main() {
     assert(not(below_threshold({1, 8, 4, 10}, 10)));
-    std::cout << "Error: All numbers in the list are below or equal to the threshold.\n";
     return 0;
 }
