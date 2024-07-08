f02425e0c4380fdc2de62f6f1731f95063eb95f0
beep boop <githubbot@vadim.me>
2024-07-09 00:40:02 +0200

    Solution 11 of CPP/79, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/e4bzttrq Prompt: f6568134-026c-4b5b-8ae5-cfb237326855.cpp: In function ‘int main()’: f6568134-026c-4b5b-8ae5-cfb237326855.cpp:6:5: error: ‘assert’ was not declared i n this scope     6 |     assert(decimal_to_binary(15) == "db1111db");       |     ^~~~~~ f6568134-026c-4b5b-8ae5-cfb237326855.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <string>   +++ |+#include <cassert>     2 | f6568134-026c-4b5b-8ae5-cfb237326855.cpp: At global scope: f6568134-026c-4b5b-8ae5-cfb237326855.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ f6568134-026c-4b5b-8ae5-cfb237326855.cpp:5:5: note: ‘int main()’ previously defi ned here     5 | int main() {       |     ^~~~

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -4,7 +4,7 @@
 std::string decimal_to_binary(int decimal);
 
 int main() {
-    assert(decimal_to_binary(15) == "1111db");
+    assert(decimal_to_binary(15) == "db1111db");
 }
 
 std::string decimal_to_binary(int decimal){
@@ -13,5 +13,5 @@ std::string decimal_to_binary(int decimal){
         binary = (decimal & 1) ? "1" + binary : "0" + binary;
         decimal >>= 1;
     }
-    return "db" + binary + "b";
+    return "db" + binary + "db";
 }
