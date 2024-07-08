cd48bddd8340cb5516e734ccb7ed409ee296106a
beep boop <githubbot@vadim.me>
2024-07-09 00:23:30 +0200

    Solution 14 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ezy5hwlp Prompt: 4f798ee3-af50-49de-9b9e-c04139703649.cpp:1:15: error: ‘string’ was not declared in this scope     1 | int my_strlen(string str){       |               ^~~~~~ In file included from 4f798ee3-af50-49de-9b9e-c04139703649.cpp:5: 4f798ee3-af50-49de-9b9e-c04139703649.cpp: In function ‘int main()’: 4f798ee3-af50-49de-9b9e-c04139703649.cpp:7:13: error: ‘strlen’ was not declared in this scope     7 |     assert (strlen("asdasnakj") == 9);       |             ^~~~~~ 4f798ee3-af50-49de-9b9e-c04139703649.cpp:6:1: note: ‘strlen’ is defined in heade r ‘<cstring>’; did you forget to ‘#include <cstring>’?     5 | #include<assert.h>   +++ |+#include <cstring>     6 | int main(){

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,11 +1,8 @@
+```
 #include <string>
-#include <assert.h>
+#include <cassert>
+#include <cstring>
 
 int my_strlen(string str){
     return str.length();
-}
-
-int main(){
-    assert (my_strlen("asdasnakj") == 13);
-    return 0;
 }
