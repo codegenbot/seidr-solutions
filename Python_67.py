a864bac40e3f85e7b532879551c55dcae51353bd
beep boop <githubbot@vadim.me>
2024-07-09 01:48:17 +0200

    Solution 16 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/1k3t89pf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f0eaa79a-c4ac-42ae-be64-bd65a6344 920.py", line 14, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f0eaa79a-c4ac-42ae-be64-bd65a6344 920.py", line 11, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -1,8 +1,8 @@
+```
 def fruit_distribution(s, n):
     s = s.replace("apples and ", "")
     if "oranges" in s:
         number_of_apples = int(s.split()[0].replace("apples ", ""))
-        return n - number_of_apples - 1
+        return n - (number_of_apples + int(s.split()[1])) - 1
     else:
-        integers = [int(x) for x in s.split() if x.isdigit()]
-        return n - sum(integers)
+        return n - int(s) - 1
