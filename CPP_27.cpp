5cbeff5ffa3b2e27ff1c754c48113c5b94941bc0
beep boop <githubbot@vadim.me>
2024-07-09 00:25:32 +0200

    Solution 17 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/471qj8n6 Prompt: SE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |     ^~~~~~ 5dabc465-1e7e-4bce-99e3-bc2274800082.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <iostream>   +++ |+#include <cassert>     2 | #include <string> 5dabc465-1e7e-4bce-99e3-bc2274800082.cpp: At global scope: 5dabc465-1e7e-4bce-99e3-bc2274800082.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 5dabc465-1e7e-4bce-99e3-bc2274800082.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 5dabc465-1e7e-4bce-99e3-bc2274800082.cpp:22: 5dabc465-1e7e-4bce-99e3-bc2274800082.cpp: In function ‘int main()’: 5dabc465-1e7e-4bce-99e3-bc2274800082.cpp:24:13: error: ‘filp_case’ was not decla red in this scope; did you mean ‘flip_case’?    24 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -14,7 +14,7 @@ std::string flip_case(std::string str){
     return result;
 }
 
-int mainFunc() {
+int initial_main() {
     assert(flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
     std::cout << flip_case("Hello, world!") << std::endl;
     return 0;
