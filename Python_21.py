2e24e22b4f5d3fa36900bef0dfa09a528c400ad7
beep boop <githubbot@vadim.me>
2024-07-09 01:07:30 +0200

    Solution 23 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/6suu5fvj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1719c767-e081-4708-bece-678605493 800.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_21.py
+++ Python_21.py
@@ -1,10 +1,11 @@
 from typing import List
 
+
 def rescale_to_unit(numbers: List[float]) -> List[float]:
     min_val = min(numbers)
     max_val = max(numbers)
-    
+
     if max_val - min_val == 0:
         return [0] * len(numbers)
-        
+
     return [(x - min_val) / (max_val - min_val) for x in numbers]
