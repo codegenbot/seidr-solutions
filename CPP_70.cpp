20bea4053adc4de65a315e3853d0477166b10d98
beep boop <githubbot@vadim.me>
2024-07-09 01:39:44 +0200

    Solution 14 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ltjr7ccv Prompt: In file included from /usr/include/c++/11/vector:67,                  from a4bc33c9-8b11-4100-b1ea-a9242a9ad8a3.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from a4bc33c9-8b11-4100-b1ea-a9242a9ad8a3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a4bc33c9-8b11-4100-b1ea-a9242a9ad8a3.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a4bc33c9-8b11-4100-b1ea-a9242a9ad8a3.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a4bc33c9-8b11-4100-b1ea-a9242a9ad8a3.cpp:18: a4bc33c9-8b11-4100-b1ea-a9242a9ad8a3.cpp: In function ‘int main()’: a4bc33c9-8b11-4100-b1ea-a9242a9ad8a3.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,14 +1,9 @@
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b){
-    if(a.size() != b.size())
-        return false;
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i])
-            return false;
-    }
-    return true;
+    return a == b;
 }
 
 std::vector<int> strange_sort_list(std::vector<int> lst){
@@ -23,4 +18,10 @@ std::vector<int> strange_sort_list(std::vector<int> lst){
         right--;
     }
     return result;
+}
+
+int main() {
+    assert(issame(strange_sort_list({111111}), std::vector<int>({111111})));
+    
+    return 0;
 }
