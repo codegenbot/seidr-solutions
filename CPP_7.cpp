ead143808ce8f5ca12dfffb07af262c5b3939056
beep boop <githubbot@vadim.me>
2024-07-09 00:27:30 +0200

    Solution 35 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9s6po2bb Prompt:     7 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 7ae47df6-ab52-4bb9-a290-90ef1c987131.cpp:15:10: error: ‘int main()’ redeclared a s different kind of entity    15 | int main(){       |          ^ 7ae47df6-ab52-4bb9-a290-90ef1c987131.cpp:1:5: note: previous declaration ‘int ma in’     1 | int main {       |     ^~~~ In file included from 7ae47df6-ab52-4bb9-a290-90ef1c987131.cpp:6: 7ae47df6-ab52-4bb9-a290-90ef1c987131.cpp: In function ‘int main()’: 7ae47df6-ab52-4bb9-a290-90ef1c987131.cpp:16:20: error: ‘filter_by_substring’ was  not declared in this scope    16 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 7ae47df6-ab52-4bb9-a290-90ef1c987131.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -2,24 +2,18 @@
 #include <string>
 
 bool issame(std::vector<std::string> v1, std::vector<std::string> v2) {
-    if(v1.size() != v2.size())
-        return false;
-    
-    for(int i=0; i<v1.size(); i++) {
-        if(v1[i] != v2[i])
-            return false;
+    if(v1.size() != v2.size()) return false;
+    for(int i = 0; i < v1.size(); i++) {
+        if(v1[i] != v2[i]) return false;
     }
-    
     return true;
 }
 
-std::vector<std::string> filter_by_substring(std::vector<std::string> vec, std::string sub) {
+std::vector<std::string> filter_by_substring(std::vector<std::string> str, std::string sub) {
     std::vector<std::string> result;
-    for(const auto& str : vec) {
-        if(str.find(sub) != std::string::npos)
-            result.push_back(str);
+    for(const auto& s : str) {
+        if(s.find(sub) != std::string::npos) result.push_back(s);
     }
-    
     return result;
 }
 
