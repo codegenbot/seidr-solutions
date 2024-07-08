e2ab40e5f62354fae71e112616752bb4d76d1bd7
beep boop <githubbot@vadim.me>
2024-07-09 00:52:01 +0200

    Solution 9 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8gpct13x Prompt:       |                              ^~~~~~ 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp:16:37: error: ‘string’ was not declared  in this scope    16 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp:16:44: error: ‘b’ was not declared in t his scope    16 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp:15: 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp: In function ‘int main()’: 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp:25:20: error: ‘total_match’ was not dec lared in this scope    25 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,9 +1,10 @@
+```cpp
 #include <vector>
 #include <string>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size()) return false;
-    for (size_t i = 0; i < a.size(); ++i) {
+    for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) return false;
     }
     return true;
@@ -18,18 +19,10 @@ std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<
     for (const auto &s : lst2) {
         sum2 += s.length();
     }
-    if (sum1 < sum2) return lst1;
+    if (sum1 < sum2 && issame(lst1, lst2)) return lst2;
+    else if (sum1 > sum2 && issame(lst1, lst2)) return lst1;
+    else if (issame(lst1, lst2)) return lst1; // or return lst2
+    else if (sum1 < sum2) return lst1;
     else if (sum1 > sum2) return lst2;
-    else if (!issame(lst1, lst2)) {
-        int sum3 = 0, sum4 = 0;
-        for (const auto &s : lst1) {
-            sum3 += s.length();
-        }
-        for (const auto &s : lst2) {
-            sum4 += s.length();
-        }
-        if (sum3 < sum4) return lst1;
-        else if (sum3 > sum4) return lst2;
-        else return lst1;
-    } else return lst1;
+    else return lst1;
 }
