a94030c3566c7da3a9f60fe3d548a8156c857c20
beep boop <githubbot@vadim.me>
2024-07-09 00:55:44 +0200

    Solution 81 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9s6po2bb Prompt:    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ dedafd0f-1db5-4ab1-9a74-8f7b5b16364e.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ dedafd0f-1db5-4ab1-9a74-8f7b5b16364e.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from dedafd0f-1db5-4ab1-9a74-8f7b5b16364e.cpp:28: dedafd0f-1db5-4ab1-9a74-8f7b5b16364e.cpp: In function ‘int main()’: dedafd0f-1db5-4ab1-9a74-8f7b5b16364e.cpp:38:20: error: ‘filter_by_substring’ was  not declared in this scope    38 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ dedafd0f-1db5-4ab1-9a74-8f7b5b16364e.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -8,12 +8,3 @@ std::vector<std::string> filter_by_substring(std::vector<std::string> vec, std::
 bool issame(std::vector<std::string> v1, std::vector<std::string> v2);
 
 #endif // FUNCTIONS_H
-
-#include "functions.h"  // Include the header file
-#include <iostream>
-#include <cassert>
-
-int main() {
-    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
-    return 0;
-}
