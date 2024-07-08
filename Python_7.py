3e266c46a14b627023e5561040efb6410a03f956
beep boop <githubbot@vadim.me>
2024-07-09 01:23:54 +0200

    Solution 11 of Python/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/sd0u350t Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/afe260d6-f2e4-41f8-9f21-c3db0df8a 30a.py", line 3, in <module>     filter_strings(strings, substring)                    ^^^^^^^ NameError: name 'strings' is not defined

--- Python_7.py
+++ Python_7.py
@@ -1,7 +1,6 @@
 def filter_strings(strings, substring):
     return [s for s in strings if substring in s]
 
-
 strings = input().split()
 substring = input()
 print(filter_strings(strings, substring))
