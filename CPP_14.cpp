a62171a58a29facc142d78376e017ef71b109563
beep boop <githubbot@vadim.me>
2024-07-09 00:33:57 +0200

    Solution 44 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dib3n468 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 4b79d40c-21f7-4569-8760-095c5b4c664f.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,6 +1,7 @@
 ```cpp
 #include <vector>
 #include <string>
+#include <iterator>
 
 bool issame(vector<string> a, vector<string> b) {
     if(a.size() != b.size())
@@ -12,7 +13,7 @@ bool issame(vector<string> a, vector<string> b) {
 }
 
 vector<string> all_prefixes(const string& str) {
-    vector<string> result({""});
+    vector<string> result;
     for (int i = 1; i <= str.length(); i++) {
         result.push_back(str.substr(0, i));
     }
