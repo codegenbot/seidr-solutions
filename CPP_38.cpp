2f5f2719107aa3d85e8915f350e473bc4ac307c9
beep boop <githubbot@vadim.me>
2024-07-09 00:58:43 +0200

    Solution 73 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/qlvqiwhq Prompt:                  from 6963c871-fd3b-4f46-8755-a43e4ed6772a.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 6963c871-fd3b-4f46-8755-a43e4ed6772a.cpp:53:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    53 |             str+=chr;       |             ^~~       |             std 6963c871-fd3b-4f46-8755-a43e4ed6772a.cpp:56:15: error: expected ‘;’ before ‘enco ded_str’    56 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from 6963c871-fd3b-4f46-8755-a43e4ed6772a.cpp:43: 6963c871-fd3b-4f46-8755-a43e4ed6772a.cpp:57:31: error: ‘encoded_str’ was not dec lared in this scope    57 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ 6963c871-fd3b-4f46-8755-a43e4ed6772a.cpp:57:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    57 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -5,26 +5,34 @@
 std::string encode_cyclic(std::string s) {
     int l = s.length();
     std::string encoded_str;
+    
     for (int i = 0; i * 3 < l; i++) {
         std::string x = s.substr(i * 3, 3);
+        
         if (x.length() == 3) {
             x = x[1] + x[2] + x[0];
         }
+        
         encoded_str.append(x);
     }
+    
     return encoded_str;
 }
 
 std::string decode_cyclic(std::string s) {
     int l = s.length();
     std::string decoded_str;
+    
     for (int i = 0; i * 3 < l; i++) {
         std::string x = s.substr(i * 3, 3);
+        
         if (x.length() == 3) {
             x = x[2] + x[0] + x[1];
         }
+        
         decoded_str.append(x);
     }
+    
     return decoded_str;
 }
 
