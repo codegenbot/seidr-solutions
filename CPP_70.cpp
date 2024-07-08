4f439d7f17961fefd3a9ed62705bedc805277f25
beep boop <githubbot@vadim.me>
2024-07-09 01:33:09 +0200

    Solution 72 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/alkpylw8 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 67d6bbbd-d627-4fab-bef5-8a9d49b5a0e9.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 #include <algorithm>
-#include <initializer_list> >
+#include <initializer_list>
 #include <iostream>
 
 bool issame(vector<int> a, vector<int> b) {
@@ -27,8 +27,8 @@ std::vector<int> strange_sort_list(initializer_list<int> lst) {
 }
 
 int main1() {
-    vector<int> result = strange_sort_list({111111});
-    if (issame(result, {11111})) {
+    vector<int> result = strange_sort_list({11111, 111});
+    if (issame(result, {11111, 111})) {
         cout << "Output is correct.\n";
     } else {
         cout << "Output is incorrect.\n";
