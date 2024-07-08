2af812a77fd53205afabcb5107596f97d633feb9
beep boop <githubbot@vadim.me>
2024-07-09 00:42:24 +0200

    Solution 5 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/hon28pej Prompt:       |                                   ^~~~~~ bd382f84-cf80-4f6e-8af9-e15f637a111f.cpp:14:42: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ bd382f84-cf80-4f6e-8af9-e15f637a111f.cpp:14:49: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ bd382f84-cf80-4f6e-8af9-e15f637a111f.cpp:14:56: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from bd382f84-cf80-4f6e-8af9-e15f637a111f.cpp:13: bd382f84-cf80-4f6e-8af9-e15f637a111f.cpp: In function ‘int main()’: bd382f84-cf80-4f6e-8af9-e15f637a111f.cpp:26:20: error: ‘get_row’ was not declare d in this scope    26 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ bd382f84-cf80-4f6e-8af9-e15f637a111f.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -3,18 +3,12 @@
 
 std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x) {
     std::vector<std::vector<int>> result;
+    
     for (int i = 0; i < lst.size(); i++) {
         if (i == x) {
-            for (auto j : lst[i]) {
-                result.push_back({j});
-            }
+            result.push_back(lst[i]);
         }
     }
+    
     return result;
-}
-
-int main() {
-    std::vector<std::vector<int>> lst = {{}, {1}, {1, 2, 3}};
-    std::vector<std::vector<int>> expected = {{1, 2, 3}};
-    assert(std::equal(get_row(lst, 2).begin(), get_row(lst, 2).end(), expected.begin()));
 }
