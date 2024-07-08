57b3f113e8ace52684cb5ef23d2197f8be40ba6a
beep boop <githubbot@vadim.me>
2024-07-09 01:25:44 +0200

    Solution 9 of CPP/82, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/d71hpa6h Prompt: af2bb912-2b7b-48b9-8fd5-c2f13839ed48.cpp:1:19: error: ‘string’ was not declared in this scope     1 | bool prime_length(string str){       |                   ^~~~~~ In file included from af2bb912-2b7b-48b9-8fd5-c2f13839ed48.cpp:9: af2bb912-2b7b-48b9-8fd5-c2f13839ed48.cpp: In function ‘int main()’: af2bb912-2b7b-48b9-8fd5-c2f13839ed48.cpp:11:25: error: ‘prime_length’ cannot be used as a function    11 |     assert (prime_length("0") == false);       |             ~~~~~~~~~~~~^~~~~

--- CPP_82.cpp
+++ CPP_82.cpp
@@ -1,8 +1,8 @@
 #include <string>
 
-bool prime_length(string str) {
+bool prime_length(string str){
     int len = str.length();
-    if(len<2) return false;
+    if(len <=1) return false;
     for(int i=2; i*i<=len; i++){
         if(len%i==0) return false;
     }
