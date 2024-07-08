eeaf2c839105cc292c4a7317ffdd7b46a6d4103b
beep boop <githubbot@vadim.me>
2024-07-09 00:30:26 +0200

    Solution 14 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/asyyr8p8 Prompt:       |                           ^~~~~~ db744106-e7ec-41dc-928c-3b81a02f3693.cpp:26:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from db744106-e7ec-41dc-928c-3b81a02f3693.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from db744106-e7ec-41dc-928c-3b81a02f3693.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ db744106-e7ec-41dc-928c-3b81a02f3693.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ db744106-e7ec-41dc-928c-3b81a02f3693.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ db744106-e7ec-41dc-928c-3b81a02f3693.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -18,7 +18,7 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 }
 
 int main() {
-    assert(issame({10, 10}, sum_product({10, 10})));
+    assert(issame({10, 10}, sum_product({10})));
     
     return 0;
 }
