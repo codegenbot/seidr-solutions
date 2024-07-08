823a4d5de998d4f65ac4edc0b44bc26c8adbcd59
beep boop <githubbot@vadim.me>
2024-07-09 01:46:29 +0200

    Solution 69 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/84cndn9h Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6a55564b-c2d5-43c5-a062-79d8affb1 693.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_14.py
+++ Python_14.py
@@ -1,9 +1,9 @@
+```python
 def all_prefixes_and_suffixes(string: str) -> List[str]:
     result = []
     for i in range(1, len(string) + 1):
         prefix = string[:i]
         result.append(prefix)
         if i < len(string):
-            suffix = string[i:]
-            result.append(suffix)
+            result.append(string[i:])
     return result
