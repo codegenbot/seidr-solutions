05964fbed0047a01e027c76e250888e7e76a6a77
beep boop <githubbot@vadim.me>
2024-07-09 00:54:39 +0200

    Solution 78 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dib3n468 Prompt:       |                                     ^~~~~~ 9f42fd8d-5ea6-45c4-bdcf-49a53e74b20f.cpp:27:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 9f42fd8d-5ea6-45c4-bdcf-49a53e74b20f.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 9f42fd8d-5ea6-45c4-bdcf-49a53e74b20f.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 9f42fd8d-5ea6-45c4-bdcf-49a53e74b20f.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 9f42fd8d-5ea6-45c4-bdcf-49a53e74b20f.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 9f42fd8d-5ea6-45c4-bdcf-49a53e74b20f.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,17 +1,17 @@
 #include <vector>
 #include <string>
 
-bool isSame(const vector<string>& a, const vector<string>& c) {
-    if(a.size() != c.size())
+bool issame(std::vector<std::string> v1, std::vector<std::string> v2) {
+    if(v1.size() != v2.size())
         return false;
-    for(int i = 0; i < a.size(); i++)
-        if(a[i] != c[i])
+    for(int i = 0; i < v1.size(); i++)
+        if(v1[i] != v2[i])
             return false;
     return true;
 }
 
-vector<string> allPrefixes(const string& str) {
-    vector<string> result;
+std::vector<std::string> all_prefixes(const std::string& str) {
+    std::vector<std::string> result;
     for (int i = 1; i <= str.length(); i++) {
         result.push_back(str.substr(0, i));
     }
@@ -19,7 +19,6 @@ vector<string> allPrefixes(const string& str) {
 }
 
 int main() {
-    vector<string> prefixes = allPrefixes("WWW"); 
-    assert(isSame(prefixes, {"", "W", "WW", "WWW"}));
+    assert(issame(all_prefixes("WWW"), std::vector<std::string>({"", "W", "WW", "WWW"})));
     return 0;
 }
