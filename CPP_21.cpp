a2ff0a100349a3e74def9b574ae47af7ad95d5db
beep boop <githubbot@vadim.me>
2024-07-09 00:31:34 +0200

    Solution 8 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/8d7vasla Prompt:    14 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ c88356b9-0550-4a68-9c71-b7b7dc901644.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ c88356b9-0550-4a68-9c71-b7b7dc901644.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from c88356b9-0550-4a68-9c71-b7b7dc901644.cpp:13: c88356b9-0550-4a68-9c71-b7b7dc901644.cpp: In function ‘int main()’: c88356b9-0550-4a68-9c71-b7b7dc901644.cpp:23:20: error: ‘rescale_to_unit’ was not  declared in this scope    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ c88356b9-0550-4a68-9c71-b7b7dc901644.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,4 +1,4 @@
-vector<float> rescale_to_unit(vector<float> numbers) {
+vector<float> rescale_to_unit(vector<float> numbers) {    
     float min_num = *min_element(numbers.begin(), numbers.end());
     float max_num = *max_element(numbers.begin(), numbers.end());
     
@@ -9,10 +9,4 @@ vector<float> rescale_to_unit(vector<float> numbers) {
     }
     
     return rescaled_numbers;
-}
-
-int main() {
-    assert(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) == vector<float>{0.25, 0.0, 1.0, 0.5, 0.75});
-
-    return 0;
 }
