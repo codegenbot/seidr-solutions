acd12d78441a2160296051998beff06b15b365d9
beep boop <githubbot@vadim.me>
2024-07-09 01:00:33 +0200

    Solution 6 of CPP/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/hikmraiq Prompt: 1aaf1c84-8a96-460a-89da-74b3ef36efb4.cpp:22:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    22 |             str+=chr;       |             ^~~       |             std 1aaf1c84-8a96-460a-89da-74b3ef36efb4.cpp:25:15: error: expected ‘;’ before ‘enco ded_str’    25 |         string encoded_str = encode_shift(str);       |               ^~~~~~~~~~~~       |               ; In file included from 1aaf1c84-8a96-460a-89da-74b3ef36efb4.cpp:12: 1aaf1c84-8a96-460a-89da-74b3ef36efb4.cpp:26:30: error: ‘encoded_str’ was not dec lared in this scope    26 |         assert (decode_shift(encoded_str) == str);       |                              ^~~~~~~~~~~ 1aaf1c84-8a96-460a-89da-74b3ef36efb4.cpp:26:17: error: ‘decode_shift’ was not de clared in this scope    26 |         assert (decode_shift(encoded_str) == str);       |                 ^~~~~~~~~~~~ 1aaf1c84-8a96-460a-89da-74b3ef36efb4.cpp:26:46: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    26 |         assert (decode_shift(encoded_str) == str);       |                                              ^~~

--- CPP_50.cpp
+++ CPP_50.cpp
@@ -2,31 +2,30 @@
 #include <string>
 #include <cassert>
 
-std::string encode_shift(std::string s){
-    std::string out;
-    int i;
-    for (i=0; i<s.length(); i++)
-    {
-        int w = (((int)s[i] + 5 - (int)'a' + 26 ) % 26) + (int)'a';   
+using namespace std;
+
+string encode_shift(string s){
+    string out;
+    for (int i = 0; i < s.length(); i++){
+        int w = ((int)s[i] + 5 - (int)'a') % 26 + (int)'a';   
         out += (char)w;
     }
     return out;
 }
 
-std::string decode_shift(std::string s){
-    std::string out;
-    int i;
-    for (i=0;i<s.length();i++)
-    {
+string decode_shift(string s){
+    string out;
+    for (int i = 0; i < s.length(); i++){
         int w = ((int)s[i] - 5 - (int)'a' + 26) % 26 + (int)'a';   
         out += (char)w;
     }
     return out;
 }
 
-int main() {
-    std::string str = "hello";
-    std::string encoded_str = encode_shift(str);
+int main(){
+    string str;
+    cin >> str;
+    string encoded_str = encode_shift(str);
     assert(decode_shift(encoded_str) == str);
     return 0;
 }
