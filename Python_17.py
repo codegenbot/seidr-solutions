c8e2f9f0643b6e3de75f839b2702e1ec9499e2b5
beep boop <githubbot@vadim.me>
2024-07-09 01:05:26 +0200

    Solution 48 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kcw3rfr5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/572b2c1d-65a4-4cc5-a5ca-00e537ffb f88.py", line 1     ````     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,6 +1,6 @@
-note_map = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6}
-
+```
 def check(note: str) -> int:
+    note_map = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6}
     return note_map.get(note.upper(), -1)
 
 def parse_music(music_string: str) -> list:
