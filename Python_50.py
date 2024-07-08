6c5933f91fd2c54ed49a4bdb47deaf9b03f50b2f
beep boop <githubbot@vadim.me>
2024-07-09 01:44:39 +0200

    Solution 38 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pnyb5n2l Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/abb31eb3-18ca-4f64-ad1b-22caa2454 8d9.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_50.py
+++ Python_50.py
@@ -1,9 +1,6 @@
-```
 def decode_shift(s: str):
-    if s[:7] != 'decode ':
-        return "Invalid input. Please start with 'decode '"
     decoded_s = ""
-    for ch in s[7:]:
+    for ch in s:
         if ch.isalpha():
             ascii_offset = ord('a') if ch.lower() == ch else ord('A')
             decoded_s += chr((ord(ch) - ascii_offset - 5) % 26 + ascii_offset)
