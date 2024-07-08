a01180700170b76d4465b5903cacb08afbca6fb7
beep boop <githubbot@vadim.me>
2024-07-09 00:14:29 +0200

    Solution 6 of CPP/18, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/v81ygri0 Prompt: df23aa62-77fc-4f9c-8d59-8cacc7e5eb1b.cpp:1:20: error: ‘string’ was not declared in this scope     1 | int how_many_times(string str, string substring) {       |                    ^~~~~~ df23aa62-77fc-4f9c-8d59-8cacc7e5eb1b.cpp:1:32: error: ‘string’ was not declared in this scope     1 | int how_many_times(string str, string substring) {       |                                ^~~~~~ df23aa62-77fc-4f9c-8d59-8cacc7e5eb1b.cpp:1:48: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int how_many_times(string str, string substring) {       |                                                ^ In file included from df23aa62-77fc-4f9c-8d59-8cacc7e5eb1b.cpp:11: df23aa62-77fc-4f9c-8d59-8cacc7e5eb1b.cpp: In function ‘int main()’: df23aa62-77fc-4f9c-8d59-8cacc7e5eb1b.cpp:13:27: error: ‘how_many_times’ cannot b e used as a function    13 |     assert (how_many_times("john doe", "john") == 1);       |             ~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -1,10 +1,24 @@
-#include <string>
+#include<string>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter the string: ";
+    cin >> str;
+    string substring;
+    cout << "Enter the substring: ";
+    cin >> substring;
+    int count = how_many_times(str, substring);
+    cout << "The substring appears " << count << " times." << endl;
+    return 0;
+}
 
 int how_many_times(string str, string substring) {
     int count = 0;
     size_t pos = 0;
-    while ((pos = str.find(substring)) != std::string::npos) {
-        count++;
+    while ((pos = str.find(substring, pos)) != string::npos) {
+        ++count;
         pos += substring.size();
     }
     return count;
+}
