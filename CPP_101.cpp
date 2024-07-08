3ef4340209c54f34a42eca07b226dca3140128ee
beep boop <githubbot@vadim.me>
2024-07-09 01:32:07 +0200

    Solution 3 of CPP/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/vmm4yi2z Prompt:    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 985eab9d-b38b-420f-a9ad-616589d33db0.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 985eab9d-b38b-420f-a9ad-616589d33db0.cpp:21:45: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 985eab9d-b38b-420f-a9ad-616589d33db0.cpp:20: 985eab9d-b38b-420f-a9ad-616589d33db0.cpp: In function ‘int main()’: 985eab9d-b38b-420f-a9ad-616589d33db0.cpp:30:20: error: ‘words_string’ was not de clared in this scope    30 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |                    ^~~~~~~~~~~~ 985eab9d-b38b-420f-a9ad-616589d33db0.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~

--- CPP_101.cpp
+++ CPP_101.cpp
@@ -1,11 +1,6 @@
-bool issame(vector<string> a, vector<string> b) { 
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(vector<string> a, vector<string> b) {
+    bool same = true;
+    if(a.size() != b.size()) same = false;
+    else for(int i = 0; i < a.size(); i++) if(a[i] != b[i]) same = false;
+    return same;
 }
