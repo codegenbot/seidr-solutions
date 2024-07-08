69b5f026a763abd05aa8e22dff4ca78be330cc17
beep boop <githubbot@vadim.me>
2024-07-09 00:17:30 +0200

    Solution 2 of CPP/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/73cn7090 Prompt: b35e32dc-10a8-4faf-88cd-bea6a1d900cd.cpp: In function ‘int fizz_buzz(int)’: b35e32dc-10a8-4faf-88cd-bea6a1d900cd.cpp:4:45: error: ‘to_string’ was not declar ed in this scope     4 |         if ((i % 11 == 0 || i % 13 == 0) && to_string(i).find('7') != st ring::npos) {       |                                             ^~~~~~~~~ b35e32dc-10a8-4faf-88cd-bea6a1d900cd.cpp:4:71: error: ‘string’ has not been decl ared     4 |  ((i % 11 == 0 || i % 13 == 0) && to_string(i).find('7') != string::npos ) {       |                                                             ^~~~~~

--- CPP_36.cpp
+++ CPP_36.cpp
@@ -1,24 +1,11 @@
-#include <cstdio>
-
-using namespace std;
+#include <string>
 
 int fizz_buzz(int n) {
     int count = 0;
-    for (int i = 1; i < n; i++) {
-        if ((i % 11 == 0 || i % 13 == 0) && count(i)) {
-            count++;
+    for (int i = 1; i < n; ++i) {
+        if ((i % 11 == 0 || i % 13 == 0) && to_string(i).find("7") != string::npos) {
+            ++count;
         }
     }
     return count;
-}
-
-bool count(int num) {
-    int count7 = 0;
-    while (num > 0) {
-        if (num % 10 == 7) {
-            count7++;
-        }
-        num /= 10;
-    }
-    return count7 > 0;
 }
