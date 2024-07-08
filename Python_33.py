58961818e34ee3f8483750c4d596a852e35bdda8
beep boop <githubbot@vadim.me>
2024-07-09 01:15:45 +0200

    Solution 40 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/35p9hhy0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9c09d814-5226-45e1-b224-05574b698 83c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,6 +1,3 @@
-def sort_third(lst):
-    third_max = max([x for x in lst if x % 3 == 0], default=None)
-    return [
-        val if i % 3 != 0 else (third_max if val > third_max else val)
-        for i, val in enumerate(lst)
-    ]
+def sort_third(l: list):
+    max_val = next((val for val in l if val % 3 == 0), None)
+    return [val if i % 3 != 0 else max_val for i, val in enumerate(l)]
