09050ceb3b4044ca2e1696a2dc582b1cba522a81
beep boop <githubbot@vadim.me>
2024-07-09 01:54:16 +0200

    Solution 21 of CPP/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/f83613l2 Prompt: in this scope     4 | long long minSubArraySum(vector<long long> nums) {       |                          ^~~~~~ 44788e28-17aa-47e1-b27e-a861f8079fc9.cpp:4:26: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 44788e28-17aa-47e1-b27e-a861f8079fc9.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 44788e28-17aa-47e1-b27e-a861f8079fc9.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 44788e28-17aa-47e1-b27e-a861f8079fc9.cpp:4:33: error: expected primary-expressio n before ‘long’     4 | long long minSubArraySum(vector<long long> nums) {       |                                 ^~~~ In file included from 44788e28-17aa-47e1-b27e-a861f8079fc9.cpp:18: 44788e28-17aa-47e1-b27e-a861f8079fc9.cpp: In function ‘int main()’: 44788e28-17aa-47e1-b27e-a861f8079fc9.cpp:20:27: error: ‘minSubArraySum’ cannot b e used as a function    20 |     assert (minSubArraySum({1, -1}) == -1);       |             ~~~~~~~~~~~~~~^~~~~~~~~

--- CPP_114.cpp
+++ CPP_114.cpp
@@ -1,4 +1,4 @@
-long long minSubArraySum(vector<long long>& nums) {
+long long minSubArraySum(vector<long long> &nums) {
     long long sum = 0, min_sum = LLONG_MAX;
     for (int i = 0; i < nums.size(); i++) {
         sum += nums[i];
