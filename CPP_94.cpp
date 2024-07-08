7de13baa05e056b09e2c2cc1345cd4b6dc18cdc8
beep boop <githubbot@vadim.me>
2024-07-09 01:34:40 +0200

    Solution 14 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jroorilg Prompt:     8 |         cout << "Enter number: ";       |         ^~~~ 5a017c86-f2b7-4f79-b8d4-9efadd8b8e04.cpp:9:9: error: ‘cin’ was not declared in t his scope     9 |         cin >> num;       |         ^~~ 5a017c86-f2b7-4f79-b8d4-9efadd8b8e04.cpp:10:9: error: ‘lst’ was not declared in this scope    10 |         lst.push_back(num);       |         ^~~ 5a017c86-f2b7-4f79-b8d4-9efadd8b8e04.cpp:11:12: error: ‘isPrime’ was not declare d in this scope    11 |         if(isPrime(num)){       |            ^~~~~~~ In file included from 5a017c86-f2b7-4f79-b8d4-9efadd8b8e04.cpp:33: 5a017c86-f2b7-4f79-b8d4-9efadd8b8e04.cpp: In function ‘int main()’: 5a017c86-f2b7-4f79-b8d4-9efadd8b8e04.cpp:35:22: error: too many arguments to fun ction ‘int skjkasdkd()’    35 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ~~~~~~~~~^~~~~~~~~~~~~~~~~ 5a017c86-f2b7-4f79-b8d4-9efadd8b8e04.cpp:3:5: note: declared here     3 | int skjkasdkd() {       |     ^~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,25 +1,32 @@
-int main{
-    int skjkasdkd(std::vector<int> lst){
-        int maxPrime = 0;
-        for(int i=0; i<lst.size(); i++){
-            if(isPrime(lst[i])){
-                if(maxPrime < lst[i]){
-                    maxPrime = lst[i];
-                }
+#include <iostream>
+#include <vector>
+
+int skjkasdkd(int num) {
+    std::vector<int> lst;
+    int maxPrime = 0;
+    for(int i=0; i<num; i++){
+        int n;
+        std::cout << "Enter number: ";
+        std::cin >> n;
+        lst.push_back(n);
+        if(isPrime(n)){
+            if(maxPrime < n){
+                maxPrime = n;
             }
         }
-        int sumOfDigits = 0;
-        while(maxPrime > 0){
-            sumOfDigits += maxPrime % 10;
-            maxPrime /= 10;
-        }
-        return sumOfDigits;
     }
+    int sumOfDigits = 0;
+    while(maxPrime > 0){
+        sumOfDigits += maxPrime % 10;
+        maxPrime /= 10;
+    }
+    return sumOfDigits;
+}
 
-    bool isPrime(int n) {
-        if (n <= 1) return false;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
-        }
-        return true;
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
     }
+    return true;
+}
