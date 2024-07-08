469a9a296e8fce935bba21b178dfd59009114a2a
beep boop <githubbot@vadim.me>
2024-07-09 01:41:36 +0200

    Solution 83 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/2tagw1p7 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 453ae38d-75bb-4d03-8e8d-3dc4d520fc74.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- find-pair.cpp
+++ find-pair.cpp
@@ -2,18 +2,17 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> findPair(std::vector<int>& nums, int target) {
+std::pair<int, int> findPair(std::vector<int>& nums, int target) {
     std::sort(nums.begin(), nums.end());
     for (int i = 0; i < nums.size() - 1; i++) {
         int complement = target - nums[i];
         int j = i + 1;
         while (j < nums.size() && nums[j] < complement) {
             if (nums[j] + nums[i] == target) {
-                int result[2] = {nums[i], nums[j]};
-                return std::vector<int>(result, result+2);
+                return std::make_pair(nums[i], nums[j]);
             }
             j++;
         }
     }
-    return {-1, -1};
+    return std::make_pair(-1, -1);
 }
