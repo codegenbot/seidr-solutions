24c4278e100dbb253d9149954844270c65b41551
beep boop <githubbot@vadim.me>
2024-07-09 00:27:36 +0200

    Solution 4 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/vnmlm7oy Prompt:    29 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bd8d4a8d-f61b-407c-a27a-124f74e43d60.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bd8d4a8d-f61b-407c-a27a-124f74e43d60.cpp:29:39: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bd8d4a8d-f61b-407c-a27a-124f74e43d60.cpp:28: bd8d4a8d-f61b-407c-a27a-124f74e43d60.cpp: In function ‘int main()’: bd8d4a8d-f61b-407c-a27a-124f74e43d60.cpp:38:20: error: ‘parse_music’ was not dec lared in this scope    38 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ bd8d4a8d-f61b-407c-a27a-124f74e43d60.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,14 +1,32 @@
-vector<int> parse_music(string music_string);
-
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
+bool issame(string s1, string s2) {
+    return s1 == s2;
 }
 
-int main(){
-    string music_string;
-    cin >> music_string;
-    
-    vector<int> beats = parse_music(music_string);
+vector<int> parse_music(string music_string);
 
-    return 0;
+vector<int> parse_music(string music_string) {
+    vector<int> beats;
+    string note = "";
+    for (char c : music_string) {
+        if (c == 'o') {
+            if (issame(note, "o|")) {
+                beats.push_back(2);
+            } else if (issame(note, ".|")) {
+                beats.push_back(1);
+            } else {
+                beats.push_back(4);
+            }
+            note = "";
+        } else {
+            note += c;
+        }
+    }
+    if (!note.empty()) {
+        if (issame(note, "o|")) {
+            beats.push_back(2);
+        } else if (issame(note, ".|")) {
+            beats.push_back(1);
+        }
+    }
+    return beats;
 }
