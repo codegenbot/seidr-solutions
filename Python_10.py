69ea7bededdc0f3ceac7eb0cbede68a390502d87
beep boop <githubbot@vadim.me>
2024-07-09 01:27:54 +0200

    Solution 71 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/gewsiiq5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5b25dbaf-d426-493c-b50a-4e06e7f5a 722.py", line 21, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5b25dbaf-d426-493c-b50a-4e06e7f5a 722.py", line 18, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -4,15 +4,17 @@ def make_palindrome(s):
 
     s = list(s)
     i, j = 0, len(s) - 1
+    palindrome = []
 
     while i <= j:
         if s[i] == s[j]:
-            palindrome = s[i] + "".join(s[i + 1 : j])
+            palindrome.append(s[i])
             i += 1
             j -= 1
         else:
-            palindrome = s[: i + 1] + "".join(reversed(s[i:j])) + s[j:]
+            palindrome = s[:i+1] + ''.join(reversed(s[i:j])) + s[j:]
             break
     if i <= j:
-        palindrome += "".join(reversed(s[i : j + 1]))
-    return palindrome
+        palindrome += ''.join(reversed(s[i:j+1]))
+
+    return ''.join(palindrome)
