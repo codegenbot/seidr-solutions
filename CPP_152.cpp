4651181321f0f6f3559446c45443c9c1c352bc18
beep boop <githubbot@vadim.me>
2024-07-09 01:59:37 +0200

    Solution 5 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/q7d47p24 Prompt:       |                    ^~~ 3a2b8616-1d1b-42c2-a7a7-196de9e1cf9a.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3a2b8616-1d1b-42c2-a7a7-196de9e1cf9a.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3a2b8616-1d1b-42c2-a7a7-196de9e1cf9a.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3a2b8616-1d1b-42c2-a7a7-196de9e1cf9a.cpp:13: 3a2b8616-1d1b-42c2-a7a7-196de9e1cf9a.cpp: In function ‘int main()’: 3a2b8616-1d1b-42c2-a7a7-196de9e1cf9a.cpp:23:20: error: ‘compare’ was not declare d in this scope    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 3a2b8616-1d1b-42c2-a7a7-196de9e1cf9a.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -4,12 +4,9 @@ vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
     for (int i = 0; i < game.size(); i++) {
         if (game[i] == guess[i]) {
-            result.push_back(2);
+            result.push_back(0);
         } else {
-            for (int j = abs(guess[i] - game[i]); j > 0; --j)
-                result.push_back(1);
-            for (int k = abs(guess[i] - game[i]); k > 0; --k)
-                result.push_back(0);
+            result.push_back(abs(guess[i] - game[i]));
         }
     }
     return result;
