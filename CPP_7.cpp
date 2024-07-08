9107309754a87640347e8bde5cf83451bfc4a0e6
beep boop <githubbot@vadim.me>
2024-07-09 00:45:41 +0200

    Solution 62 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/z16tspui Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from cffc7525-3f7e-4756-a4db-6c21d151327a.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -19,6 +19,7 @@ vector<string> filter_by_substring(vector<string> strings, string substring){
 
 }
 int main() {
-    assert(issame(filter_by_substring({{"grunt"}, {"trumpet"}, {"prune"}, {"gruesome"}}, "run") ,{{"grunt"}, {"prune"}}));
+    vector<string> strings = {"grunt", "trumpet", "prune", "gruesome"};
+    assert(issame(filter_by_substring(strings, "run"),{"grunt","prune"}));
     return 0;
 }
