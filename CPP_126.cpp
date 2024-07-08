ff65ee56c85225e8890c418c3463566a38eb63f4
beep boop <githubbot@vadim.me>
2024-07-09 01:29:21 +0200

    Solution 8 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/6wky7084 Prompt: 5f88ac17-b9ce-4f92-b6fc-74ea9d5f5a02.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | bool is_sorted(vector<int> lst){       |                ^~~~~~ 5f88ac17-b9ce-4f92-b6fc-74ea9d5f5a02.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | bool is_sorted(vector<int> lst){       |                       ^~~ In file included from 5f88ac17-b9ce-4f92-b6fc-74ea9d5f5a02.cpp:15: 5f88ac17-b9ce-4f92-b6fc-74ea9d5f5a02.cpp: In function ‘int main()’: 5f88ac17-b9ce-4f92-b6fc-74ea9d5f5a02.cpp:17:22: error: ‘is_sorted’ cannot be use d as a function    17 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ~~~~~~~~~^~~~~~~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,15 +1,6 @@
 #include <vector>
+#include <algorithm>
 
 bool is_sorted(vector<int> lst){
-    for(int i=1; i<lst.size();i++){
-        if(lst[i-1] >= lst[i]){
-            return false;
-        }
-    }
-    vector<int> v(lst.begin(), lst.end());
-    sort(v.begin(), v.end());
-    if(count(v.begin(), v.end(), v[0]) > 1){
-        return false;
-    }
-    return true;
+    return std::is_permutation(lst.begin(), lst.end());
 }
