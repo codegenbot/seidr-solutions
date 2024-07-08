3c1cede87b7f80a6be876a3c2c74dafc7f34161d
beep boop <githubbot@vadim.me>
2024-07-09 01:02:50 +0200

    Solution 15 of CPP/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/hikmraiq Prompt: b6ccd42b-0561-46be-9a23-5ebb39cd1c30.cpp:27:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    27 |             str+=chr;       |             ^~~       |             std b6ccd42b-0561-46be-9a23-5ebb39cd1c30.cpp:30:15: error: expected ‘;’ before ‘enco ded_str’    30 |         string encoded_str = encode_shift(str);       |               ^~~~~~~~~~~~       |               ; In file included from b6ccd42b-0561-46be-9a23-5ebb39cd1c30.cpp:17: b6ccd42b-0561-46be-9a23-5ebb39cd1c30.cpp:31:30: error: ‘encoded_str’ was not dec lared in this scope    31 |         assert (decode_shift(encoded_str) == str);       |                              ^~~~~~~~~~~ b6ccd42b-0561-46be-9a23-5ebb39cd1c30.cpp:31:17: error: ‘decode_shift’ was not de clared in this scope    31 |         assert (decode_shift(encoded_str) == str);       |                 ^~~~~~~~~~~~ b6ccd42b-0561-46be-9a23-5ebb39cd1c30.cpp:31:46: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    31 |         assert (decode_shift(encoded_str) == str);       |                                              ^~~

--- CPP_50.cpp
+++ CPP_50.cpp
@@ -1,23 +1,15 @@
-
 #include <string>
 #include <cassert>
 
-string encode_shift(string s) {
-    string out;
-    int i;
-    for (i = 0; i < s.length(); i++) {
-        int w = ((int)s[i] - 5 - (int)'a' + 26) % 26 + (int)'a';
-        out = out + (char)w;
-    }
-    return out;
-}
+string encode_shift(string s);
 
-string decode_shift(string s) {
+string decode_shift(string s){
     string out;
     int i;
-    for (i = 0; i < s.length(); i++) {
-        int w = ((int)s[i] + 5 - (int)'a' + 26) % 26 + (int)'a';
-        out = out + (char)w;
+    for (i=0;i<s.length();i++)
+    {
+        int w=((int)s[i]-5-(int)'a'+26)%26+(int)'a';   
+        out=out+(char)w;
     }
     return out;
 }
