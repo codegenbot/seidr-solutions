59c2e5c41a96b00d9dc152241e00d64716af7bb1
beep boop <githubbot@vadim.me>
2024-07-09 00:22:07 +0200

    Solution 8 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/458jbkxn Prompt: Program did not receive expected input

--- Python_1.py
+++ Python_1.py
@@ -20,4 +20,11 @@ def separate_paren_groups(paren_string: str) -> List[str]:
                 groups.append(current_group + ")")
                 current_group = ""
 
-    return [group[1:-1].strip() for group in groups]
+    if stack:
+        for _ in range(stack.__len__()):
+            current_group += ")"
+        groups.append(current_group)
+    elif current_group:
+        groups.append(current_group)
+
+    return [group[1:-1] for group in groups]
