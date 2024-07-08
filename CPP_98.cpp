0e788b30cbaca456c7c5521b2b87f9f0c4b8cffa
beep boop <githubbot@vadim.me>
2024-07-09 00:59:48 +0200

    Solution 10 of CPP/98, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/o4twcqi7 Prompt:                  from /usr/include/x86_64-linux-gnu/c++/11/bits/stdc++.h:54,                  from 158cc746-36fb-4987-97aa-99925e7ec41c.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/istream:38,                  from /usr/include/c++/11/sstream:38,                  from /usr/include/c++/11/complex:45,                  from /usr/include/c++/11/ccomplex:39,                  from /usr/include/x86_64-linux-gnu/c++/11/bits/stdc++.h:54,                  from 158cc746-36fb-4987-97aa-99925e7ec41c.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from 158cc746-36fb-4987-97aa-99925e7ec41c.cpp:13: 158cc746-36fb-4987-97aa-99925e7ec41c.cpp: In function ‘int main()’: 158cc746-36fb-4987-97aa-99925e7ec41c.cpp:15:24: error: ‘count_upper’ cannot be u sed as a function    15 |     assert (count_upper("EEEE") == 2);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_98.cpp
+++ CPP_98.cpp
@@ -1,9 +1,10 @@
 #include <string>
+#include <cctype>
 
 int count_upper(string s) {
     int count = 0;
-    for(int i=0; i<s.length(); i++) {
-        if((s[i]>='A' && s[i]<='E') || (s[i]>='I' && s[i]<='O') || (s[i]>='U')) {
+    for (int i = 0; i < s.length(); i++) {
+        if (isupper(s[i])) {
             count++;
         }
     }
