efd54afa9a31d3e2080c241a651e2a433c74763e
beep boop <githubbot@vadim.me>
2024-07-09 00:34:06 +0200

    Solution 34 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/t4rebmmc Prompt:       |                           ^~~~~~ 1c4db0fc-21ad-43aa-9ddf-8d41b4d7f346.cpp:41:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 1c4db0fc-21ad-43aa-9ddf-8d41b4d7f346.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1c4db0fc-21ad-43aa-9ddf-8d41b4d7f346.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1c4db0fc-21ad-43aa-9ddf-8d41b4d7f346.cpp:41:34: error: expected primary-expressi on before ‘int’    41 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1c4db0fc-21ad-43aa-9ddf-8d41b4d7f346.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 1c4db0fc-21ad-43aa-9ddf-8d41b4d7f346.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main() {       |     ^~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -1,21 +1,7 @@
 #include <vector>
 #include <cassert>
 
-std::vector<int> intersperse(std::vector<int> numbers, int delimeter);
-
-std::vector<int> intersperse(std::vector<int> numbers, int delimeter) {
-    std::vector<int> result;
-    
-    for(int i = 0; i < numbers.size(); i++) {
-        result.push_back(numbers[i]);
-        
-        if(i < numbers.size() - 1) {
-            result.push_back(delimeter);
-        }
-    }
-    
-    return result;
-}
+std::vector<int> intersperse(std::vector<int> numbers, int delimiter);
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     if(a.size() != b.size()) {
@@ -31,8 +17,16 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return true;
 }
 
-int main() {
-    assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
+std::vector<int> intersperse(std::vector<int> numbers, int delimiter) {
+    std::vector<int> result;
     
-    return 0;
+    for(int i = 0; i < numbers.size(); i++) {
+        result.push_back(numbers[i]);
+        
+        if(i < numbers.size() - 1) {
+            result.push_back(delimiter);
+        }
+    }
+    
+    return result;
 }
