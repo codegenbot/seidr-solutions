a00784f7f1cce7e95946c6b379f7fe05de2e5704
beep boop <githubbot@vadim.me>
2024-07-09 00:33:22 +0200

    Solution 26 of CPP/46, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/8iwckk56 Prompt: 5f4780d0-a087-412f-91c0-8deffa6ad5d2.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 5f4780d0-a087-412f-91c0-8deffa6ad5d2.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_46.cpp
+++ CPP_46.cpp
@@ -14,10 +14,6 @@ int fib4(int n) {
     return d;
 }
 
-int main() {
-    int n;
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
-    printf("The %d-th element of the fib4 number sequence is: %d\n", n, fib4(n));
-    return 0;
-}
+printf("Enter the value of n: ");
+scanf("%d", &n);
+printf("The %d-th element of the fib4 number sequence is: %d\n", n, fib4(n));
