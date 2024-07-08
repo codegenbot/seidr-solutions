204255c65cacfa3dbee306b36edc4e3b2f079567
beep boop <githubbot@vadim.me>
2024-07-09 01:01:21 +0200

    Solution 10 of CPP/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/hikmraiq Prompt: 10fd647f-f46c-4ca6-a86b-b70bed3a25fb.cpp:27:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    27 |             str+=chr;       |             ^~~       |             std 10fd647f-f46c-4ca6-a86b-b70bed3a25fb.cpp:30:15: error: expected ‘;’ before ‘enco ded_str’    30 |         string encoded_str = encode_shift(str);       |               ^~~~~~~~~~~~       |               ; In file included from 10fd647f-f46c-4ca6-a86b-b70bed3a25fb.cpp:17: 10fd647f-f46c-4ca6-a86b-b70bed3a25fb.cpp:31:30: error: ‘encoded_str’ was not dec lared in this scope    31 |         assert (decode_shift(encoded_str) == str);       |                              ^~~~~~~~~~~ 10fd647f-f46c-4ca6-a86b-b70bed3a25fb.cpp:31:17: error: ‘decode_shift’ was not de clared in this scope    31 |         assert (decode_shift(encoded_str) == str);       |                 ^~~~~~~~~~~~ 10fd647f-f46c-4ca6-a86b-b70bed3a25fb.cpp:31:46: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    31 |         assert (decode_shift(encoded_str) == str);       |                                              ^~~

--- CPP_50.cpp
+++ CPP_50.cpp
@@ -1,31 +1,9 @@
-#include <iostream>
 #include <string>
-#include <cassert>
 
-std::string encode_shift(std::string s) {
-    std::string out;
-    for (int i = 0; i < s.length(); i++) {
-        char w = ((int)s[i] + 5 - (int)'a') % 26 + (int)'a';
-        out += w;
-    }
-    return out;
-}
+string encode_shift(string s);
 
-std::string decode_shift(std::string s) {
-    std::string out;
-    for (int i = 0; i < s.length(); i++) {
-        char w = ((int)s[i] - 5 - (int)'a' + 26) % 26 + (int)'a';
-        out += w;
-    }
-    return out;
-}
-
-int main() {
-    std::string input_str = "hello";
-    std::string encoded_str = encode_shift(input_str);
-    std::string decoded_str = decode_shift(encoded_str);
-
-    assert(decoded_str == input_str);
-
-    return 0;
+void test_code() {
+    string str = "input string";
+    string encoded_str = encode_shift(str);
+    assert(decode_shift(encoded_str) == str);
 }
