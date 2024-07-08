da30669a09d786bc9ae18b432897ad292779f977
beep boop <githubbot@vadim.me>
2024-07-09 00:56:25 +0200

    Solution 24 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/qhscq9va Prompt:       |             ^~~~~~ 30269417-0606-4c14-a77d-989d1b4d2865.cpp:41:34: error: expected primary-expressi on before ‘int’    41 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 30269417-0606-4c14-a77d-989d1b4d2865.cpp:41:39: error: expression list treated a s compound expression in initializer [-fpermissive]    41 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 30269417-0606-4c14-a77d-989d1b4d2865.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 30269417-0606-4c14-a77d-989d1b4d2865.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from 30269417-0606-4c14-a77d-989d1b4d2865.cpp:40: 30269417-0606-4c14-a77d-989d1b4d2865.cpp: In function ‘int main()’: 30269417-0606-4c14-a77d-989d1b4d2865.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -28,10 +28,10 @@ std::vector<int> sort_array(std::vector<int> array) {
 int main() {
     std::vector<int> array = {21, 14, 23, 11};
     array = sort_array(array);
-    if (array.size() % 2 == 0) {
-        std::cout << "Sum of first and last elements is even: ";
-    } else {
+    if (array.size() % 2 == 1) {
         std::cout << "Sum of first and last elements is odd: ";
+    } else {
+        std::cout << "Sum of first and last elements is even: ";
     }
     std::cout << array[0] + array.back() << std::endl;
     return 0;
