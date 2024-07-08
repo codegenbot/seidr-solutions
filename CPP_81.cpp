7a7df1cb1aed9759e8104e062b4cb5665d7bbe3e
beep boop <githubbot@vadim.me>
2024-07-09 01:24:52 +0200

    Solution 11 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/60hwnehq Prompt: In file included from /usr/include/c++/11/string:39,                  from ae9d2dd3-6a76-4419-9909-77a7435acccf.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from ae9d2dd3-6a76-4419-9909-77a7435acccf.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ ae9d2dd3-6a76-4419-9909-77a7435acccf.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from ae9d2dd3-6a76-4419-9909-77a7435acccf.cpp:50: ae9d2dd3-6a76-4419-9909-77a7435acccf.cpp: In function ‘int main()’: ae9d2dd3-6a76-4419-9909-77a7435acccf.cpp:60:20: error: ‘numerical_letter_grade’ was not declared in this scope    60 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ ae9d2dd3-6a76-4419-9909-77a7435acccf.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -48,7 +48,7 @@ vector<string> numerical_letter_grade(vector<float> grades) {
 }
 
 int main() {
-    vector<float> grades = {3.8, 2.5};
-    assert(issame(numerical_letter_grade(grades), {"A", "C-"}));
+    vector<float> grades = {0, 0.7};
+    cout << issame(numerical_letter_grade(grades), {"E", "D-" }) << endl;
     return 0;
 }
