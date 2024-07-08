6c622013f93d65527e80df67548c5d182cef1997
beep boop <githubbot@vadim.me>
2024-07-09 00:30:19 +0200

    Solution 6 of CPP/61, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/91uo6vna Prompt: deb906de-ad83-4258-ba67-c4b054401b8b.cpp:1:25: error: ‘string’ was not declared in this scope     1 | bool correct_bracketing(string brackets){       |                         ^~~~~~ In file included from deb906de-ad83-4258-ba67-c4b054401b8b.cpp:14: deb906de-ad83-4258-ba67-c4b054401b8b.cpp: In function ‘int main()’: deb906de-ad83-4258-ba67-c4b054401b8b.cpp:16:36: error: ‘correct_bracketing’ cann ot be used as a function    16 |     assert (not (correct_bracketing("()()(()())()))()")));       |                  ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -1,11 +1,14 @@
-bool correct_bracketing(string brackets){
+#include <string>
+
+bool correct_bracketing(std::string brackets){
     int count = 0;
-    for(int i=0; i<brackets.length(); i++){
-        if(brackets[i] == '(')
+    for(int i=0; i<brackets.size(); i++){
+        if(brackets[i] == '(') {
             count++;
-        else if(brackets[i] == ')')
+        } else if (brackets[i] == ')') {
+            if(count <= 0) return false;
             count--;
-        if(count < 0) return false;
+        }
     }
-    return (count == 0);
+    return count == 0;
 }
