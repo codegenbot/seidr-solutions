81dd3e578129532f6126737a75d88d4e105001e8
beep boop <githubbot@vadim.me>
2024-07-09 00:30:43 +0200

    Solution 16 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/t4rebmmc Prompt: In file included from /usr/include/c++/11/vector:67,                  from 8f76cdc6-e6df-4767-a86b-b7910dcbef84.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 8f76cdc6-e6df-4767-a86b-b7910dcbef84.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8f76cdc6-e6df-4767-a86b-b7910dcbef84.cpp:31:34: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 8f76cdc6-e6df-4767-a86b-b7910dcbef84.cpp:30: 8f76cdc6-e6df-4767-a86b-b7910dcbef84.cpp: In function ‘int main()’: 8f76cdc6-e6df-4767-a86b-b7910dcbef84.cpp:40:20: error: ‘intersperse’ was not dec lared in this scope    40 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ 8f76cdc6-e6df-4767-a86b-b7910dcbef84.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -13,18 +13,19 @@ bool issame(std::vector<int> a, std::vector<int> b){
     return true;
 }
 
-std::vector<int> intersperse(std::vector<int> numbers, int delimiter){
+std::vector<int> intersperse(std::vector<int> numbers, int delimeter){
     std::vector<int> result;
     for(int i = 0; i < numbers.size(); i++){
         result.push_back(numbers[i]);
         if(i < numbers.size() - 1){
-            result.push_back(delimiter);
+            result.push_back(delimeter);
         }
     }
     return result;
 }
 
-int main() {
+int main(){
     assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
+    
     return 0;
 }
