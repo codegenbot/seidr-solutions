1de6ed210e29ec195790441f26f01c787084841f
beep boop <githubbot@vadim.me>
2024-07-09 00:55:24 +0200

    Solution 12 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0e61qwl9 Prompt: in this scope     4 | int smallest_change(vector<int> arr) {       |                     ^~~~~~ 01759a53-f5bd-426a-aade-ace3e6a1b4a1.cpp:4:21: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 01759a53-f5bd-426a-aade-ace3e6a1b4a1.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 01759a53-f5bd-426a-aade-ace3e6a1b4a1.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 01759a53-f5bd-426a-aade-ace3e6a1b4a1.cpp:4:28: error: expected primary-expressio n before ‘int’     4 | int smallest_change(vector<int> arr) {       |                            ^~~ In file included from 01759a53-f5bd-426a-aade-ace3e6a1b4a1.cpp:25: 01759a53-f5bd-426a-aade-ace3e6a1b4a1.cpp: In function ‘int main()’: 01759a53-f5bd-426a-aade-ace3e6a1b4a1.cpp:27:28: error: ‘smallest_change’ cannot be used as a function    27 |     assert (smallest_change({0, 1}) == 1);       |             ~~~~~~~~~~~~~~~^~~~~~~~

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -5,7 +5,7 @@ int smallest_change(std::vector<int> arr) {
     int n = arr.size();
     std::string s = "";
     for (int i : arr) {
-        s += to_string(i);
+        s += std::to_string(i);
     }
     int left = 0, right = s.length() - 1;
     int changes = 0;
@@ -20,7 +20,6 @@ int smallest_change(std::vector<int> arr) {
         }
     }
     return changes;
-}
 
 int main() {
     std::vector<int> arr = {0, 1};
