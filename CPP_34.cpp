c5fd2939925ae6bcef13127c4f97e170f53e5e2c
beep boop <githubbot@vadim.me>
2024-07-09 00:39:59 +0200

    Solution 28 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/p3mcjouk Prompt:     4 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 14ce2de7-5a3c-41d1-8f46-6880ad3c4a90.cpp:3: 14ce2de7-5a3c-41d1-8f46-6880ad3c4a90.cpp: In function ‘int main()’: 14ce2de7-5a3c-41d1-8f46-6880ad3c4a90.cpp:13:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    13 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 14ce2de7-5a3c-41d1-8f46-6880ad3c4a90.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ In file included from 14ce2de7-5a3c-41d1-8f46-6880ad3c4a90.cpp:3: 14ce2de7-5a3c-41d1-8f46-6880ad3c4a90.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,15 +1,14 @@
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b){
+    std::sort(a.begin(), a.end());
+    std::sort(b.begin(), b.end());
     return a == b;
 }
 
 int main() {
-    auto v = std::vector<int>({5, 3, 5, 2, 3, 3, 9, 0, 123});
-    v.erase(std::unique(v.begin(), v.end()), v.end());
-    assert (issame(v, {5, 3, 2, 9, 0, 123}));
-    
-    return 0;
+    assert (issame(std::vector<int>({5, 3, 5, 2, 3, 3, 9, 0, 123}), 
+                   std::vector<int>({0, 2, 3, 5, 9, 123}));   
 }
