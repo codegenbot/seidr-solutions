6403e3871dee70b1165f596f5df0b09484a81193
beep boop <githubbot@vadim.me>
2024-07-09 01:34:14 +0200

    Solution 26 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/fk6tmp91 Prompt: 1741992c-c149-4b82-b4ba-7060236a718b.cpp:25:6: error: redefinition of ‘bool issa me(std::vector<float>, std::vector<float>)’    25 | bool issame(vector<float> a,vector<float>b){       |      ^~~~~~ 1741992c-c149-4b82-b4ba-7060236a718b.cpp:15:6: note: ‘bool issame(std::vector<fl oat>, std::vector<float>)’ previously defined here    15 | bool issame(vector<float> a, vector<float> b) {       |      ^~~~~~ 1741992c-c149-4b82-b4ba-7060236a718b.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 1741992c-c149-4b82-b4ba-7060236a718b.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -13,14 +13,7 @@ vector<float> derivative(vector<float> xs) {
 }
 
 int main() {
-    // Test case 1
-    assert(derivative({1}) == vector<float>{});
-
-    // Add more test cases here
-    assert(derivative({1, 2, 3}) == vector<float>{2, 6});
-    assert(derivative({3, 5, 7, 9}) == vector<float>{5, 14, 27});
-    
-    cout << "All test cases pass." << endl;
-
+    vector<float> res = derivative({1});
+    assert(res.empty());
     return 0;
 }
