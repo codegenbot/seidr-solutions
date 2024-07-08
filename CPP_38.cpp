fbe2821d8a8affdc419253050d6c7af0930b062d
beep boop <githubbot@vadim.me>
2024-07-09 01:03:35 +0200

    Solution 84 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/qlvqiwhq Prompt:                  from 2f5e1c73-febb-4a29-93c9-35731dc26b8a.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 2f5e1c73-febb-4a29-93c9-35731dc26b8a.cpp:48:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    48 |             str+=chr;       |             ^~~       |             std 2f5e1c73-febb-4a29-93c9-35731dc26b8a.cpp:51:15: error: expected ‘;’ before ‘enco ded_str’    51 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from 2f5e1c73-febb-4a29-93c9-35731dc26b8a.cpp:38: 2f5e1c73-febb-4a29-93c9-35731dc26b8a.cpp:52:31: error: ‘encoded_str’ was not dec lared in this scope    52 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ 2f5e1c73-febb-4a29-93c9-35731dc26b8a.cpp:52:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    52 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -1,34 +1,9 @@
-#include <iostream>
-#include <string>
-#include <cassert>
-
-std::string encode_cyclic(const std::string& s) {
-    std::string encoded_str;
-    for (int i = 0; i < s.length(); i += 3) {
-        std::string x = s.substr(i, 3);
-        x = x.substr(1) + x[0];
-        encoded_str += x;
-    }
-    return encoded_str;
-}
-
-std::string decode_cyclic(const std::string& s) {
-    std::string decoded_str;
-    for (int i = 0; i < s.length(); i += 3) {
-        std::string x = s.substr(i, 3);
-        x = x[2] + x.substr(0, 2);
-        decoded_str += x;
-    }
-    return decoded_str;
-}
-
 int main() {
-    std::string str = "abcde";
+    std::string str; // Declare the str variable here
+    str = "abcde";
     std::string encoded_str = encode_cyclic(str);
     std::string decoded_str = decode_cyclic(encoded_str);
-
     assert(decoded_str == str);
-
     std::cout << "Original: " << str << std::endl;
     std::cout << "Encoded: " << encoded_str << std::endl;
     std::cout << "Decoded: " << decoded_str << std::endl;
