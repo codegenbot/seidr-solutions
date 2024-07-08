e38d818cb82e821fc9855de23b4028e55199416f
beep boop <githubbot@vadim.me>
2024-07-09 01:33:49 +0200

    Solution 62 of indices-of-substring, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/x0mblekh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/119e2568-e4c7-4593-a4b0-f6c18344a 44e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -1,10 +1,12 @@
+```
 def indices_of_substring(text, target):
-    """Returns a list of integers of the indices at which the target appears in the text. Targets may overlap."""
-    result = []
-    index = 0
-    while True:
-        index = text.find(target, index)
-        if index == -1:
-            break
-        result.append(index)
-        index += len(target)
+"""Return a list of integers of the indices at which the target appears in the text."""
+result = []
+index = 0
+while True:
+    index = text.find(target, index)
+    if index == -1:
+        break
+    result.append(index)
+    index += len(target)
+return result
