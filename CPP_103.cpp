b208b17f8435fdb7e3cca313c358f6673694c962
beep boop <githubbot@vadim.me>
2024-07-09 00:58:03 +0200

    Solution 2 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vwmlwom9 Prompt: c95b993c-3945-4801-a75a-ab38965ac574.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string rounded_avg(int n,int m){       | ^~~~~~ In file included from c95b993c-3945-4801-a75a-ab38965ac574.cpp:19: c95b993c-3945-4801-a75a-ab38965ac574.cpp: In function ‘int main()’: c95b993c-3945-4801-a75a-ab38965ac574.cpp:21:13: error: ‘rounded_avg’ was not dec lared in this scope    21 |     assert (rounded_avg(5, 5) == "101");       |             ^~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,15 +1,19 @@
-string rounded_avg(int n,int m){
+#include <string>
+
+std::string rounded_avg(int n,int m){
     if(n > m) return "-1";
     int sum = 0;
-    for(int i=n; i<=m; i++) sum += i;
-    double avg = (double)sum / (m - n + 1);
-    avg = floor(avg);
-    string res = "";
+    for(int i=n; i<=m; i++){
+        sum += i;
+    }
+    double avg = (double)sum/(m-n+1);
+    avg = floor(avg + 0.5); 
+    std::string binary = "";
     while(avg > 0){
-        if(avg & 1) res.push_back('1');
-        else res.push_back('0');
+        if(avg & 1) binary += "1";
+        else binary += "0";
         avg >>= 1;
     }
-    reverse(res.begin(), res.end());
-    return res;
+    reverse(binary.begin(),binary.end());
+    return binary;
 }
