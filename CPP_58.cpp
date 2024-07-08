dd95b5fad9d1955c5c1334f06fc9538b910bb955
beep boop <githubbot@vadim.me>
2024-07-09 01:09:27 +0200

    Solution 8 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/3qobjomi Prompt:       |                    ^~~ e9fd1948-b13c-4c4f-af0b-01df14e81bba.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e9fd1948-b13c-4c4f-af0b-01df14e81bba.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e9fd1948-b13c-4c4f-af0b-01df14e81bba.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e9fd1948-b13c-4c4f-af0b-01df14e81bba.cpp:11: e9fd1948-b13c-4c4f-af0b-01df14e81bba.cpp: In function ‘int main()’: e9fd1948-b13c-4c4f-af0b-01df14e81bba.cpp:21:20: error: ‘common’ was not declared  in this scope    21 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ e9fd1948-b13c-4c4f-af0b-01df14e81bba.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,16 +1,17 @@
 #include <vector>
+#include <algorithm>
 
 bool issame(int a, int b) {
     return a == b;
 }
 
-vector<int> findCommonElements(vector<int>& l1, vector<int>& l2) {
-    sort(l1.begin(), l1.end());
-    sort(l2.begin(), l2.end());
+std::vector<int> common(std::vector<int>& l1, std::vector<int>& l2) {
+    std::sort(l1.begin(), l1.end());
+    std::sort(l2.begin(), l2.end());
     
-    vector<int> result;
-    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result), issame);
-    result.erase(unique(result.begin(), result.end()), result.end());
+    std::vector<int> result;
+    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), std::back_inserter(result), issame);
+    result.erase(std::unique(result.begin(), result.end()), result.end());
     
     return result;
 }
