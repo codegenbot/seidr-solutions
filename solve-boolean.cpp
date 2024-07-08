fb6d98c0f51be9ba4a42743f2c81ce0384c4eb82
beep boop <githubbot@vadim.me>
2024-07-08 04:57:07 +0200

    Solution 29 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/2kwhzuet Prompt: 4859df85-4bd5-4a62-99b9-a37097ee7144.cpp:1:19: error: ‘string’ was not declared in this scope     1 | bool solveBoolean(string expression) {       |                   ^~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -3,12 +3,22 @@
 bool solveBoolean(string expression) {
     bool res = true;
     for (int i = 0; i < expression.size(); i++) {
-        if (expression[i] == '&') {
-            res &= (expression[i+1] == 'T');
-            i++;
-        } else if (expression[i] == '|') {
-            res |= (expression[i+1] == 'T');
-            i++;
+        if (expression[i] == '|') {
+            if (expression[i+1] == 'T')
+                res = true;
+            else
+                res = false;
+            while (i + 1 < expression.size() && expression[i+1] != '&' && expression[i+1] != '|') {
+                i++;
+            }
+        } else if (expression[i] == '&') {
+            if (expression[i+1] == 'T')
+                res &= true;
+            else
+                res = false;
+            while (i + 1 < expression.size() && expression[i+1] != '&' && expression[i+1] != '|') {
+                i++;
+            }
         }
     }
     return res;
