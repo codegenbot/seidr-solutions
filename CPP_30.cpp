c14b6bbec37ecc265d10fb7a75777791cebee4be
beep boop <githubbot@vadim.me>
2024-07-09 01:49:43 +0200

    Solution 56 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3k02qe59 Prompt:       |                             ^~~~~~ 2e876692-0b1a-4aaf-a580-c69478d852a2.cpp:19:36: error: expected primary-expressi on before ‘float’    19 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 2e876692-0b1a-4aaf-a580-c69478d852a2.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 2e876692-0b1a-4aaf-a580-c69478d852a2.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 2e876692-0b1a-4aaf-a580-c69478d852a2.cpp:18: 2e876692-0b1a-4aaf-a580-c69478d852a2.cpp: In function ‘int main()’: 2e876692-0b1a-4aaf-a580-c69478d852a2.cpp:28:20: error: ‘get_positive’ was not de clared in this scope    28 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 2e876692-0b1a-4aaf-a580-c69478d852a2.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -7,18 +7,5 @@ vector<float> get_positive(vector<float> v) {
 }
 
 bool issame(vector<float> a, vector<float> b) {
-    return (a == b);
-}
-
-vector<float> get_positive(vector<float> v) {
-    vector<float> result;
-    for (float x : v) {
-        if (x > 0) result.push_back(x);
-    }
-    return result;
-}
-
-int main() {
-    assert(issame(get_positive({}), {})); 
-    return 0;
+    return a == b;
 }
