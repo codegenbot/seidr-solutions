ea7d84ab7f5d283a37f11b41d65024a07974b281
beep boop <githubbot@vadim.me>
2024-07-09 00:10:16 +0200

    Solution 2 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9s6po2bb Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 66e6a699-afd0-4822-acb7-cee88de0fec1.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 66e6a699-afd0-4822-acb7-cee88de0fec1.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 66e6a699-afd0-4822-acb7-cee88de0fec1.cpp:11: 66e6a699-afd0-4822-acb7-cee88de0fec1.cpp: In function ‘int main()’: 66e6a699-afd0-4822-acb7-cee88de0fec1.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 66e6a699-afd0-4822-acb7-cee88de0fec1.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,9 +1,20 @@
 vector<string> filter_by_substring(vector<string> strings, string substring){
     vector<string> result;
-    for(string s : strings) {
-        if(s.find(substring) != string::npos) {
-            result.push_back(s);
+    for(auto str : strings){
+        if(str.find(substring) != string::npos){
+            result.push_back(str);
         }
     }
     return result;
+}
+
+bool issame(string s1, string s2){
+    bool flag = true;
+    for(int i = 0; i < s1.length(); i++){
+        if(s1[i] != s2[i]){
+            flag = false;
+            break;
+        }
+    }
+    return flag;
 }
