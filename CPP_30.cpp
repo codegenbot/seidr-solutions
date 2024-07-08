f44719cfc96b33121d24f232a48aa22bcb5e7668
beep boop <githubbot@vadim.me>
2024-07-09 00:21:07 +0200

    Solution 9 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/0uc633i4 Prompt:       |                    ^~~~~ 562af7e7-6562-4349-bc5e-ba503102d3d1.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 562af7e7-6562-4349-bc5e-ba503102d3d1.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 562af7e7-6562-4349-bc5e-ba503102d3d1.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 562af7e7-6562-4349-bc5e-ba503102d3d1.cpp:11: 562af7e7-6562-4349-bc5e-ba503102d3d1.cpp: In function ‘int main()’: 562af7e7-6562-4349-bc5e-ba503102d3d1.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 562af7e7-6562-4349-bc5e-ba503102d3d1.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,6 +1,11 @@
 #include <vector>
+#include <assert.h>
 
-std::vector<float> get_positive(std::vector<float> l) {
+bool issame(std::vector<float> a, std::vector<float> b) {
+    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
+}
+
+std::vector<float> get_positive(std::vector<float> l){
     std::vector<float> result;
     for(float num : l) {
         if(num > 0) {
@@ -8,13 +13,4 @@ std::vector<float> get_positive(std::vector<float> l) {
         }
     }
     return result;
-}
-
-bool issame(std::vector<float> a, std::vector<float> b) {
-    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
-}
-
-int main() {
-    assert(issame(get_positive({}), {}));  
-    return 0;
 }
