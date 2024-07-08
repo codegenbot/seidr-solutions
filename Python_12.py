5d4f7c1160efd3bb07bb7b6a805ba3ce3dd5a5f8
beep boop <githubbot@vadim.me>
2024-07-09 00:45:08 +0200

    Solution 30 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/odg1tedz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b9cb9f6-f8af-43f0-8983-3ebb3ff35 45f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_12.py
+++ Python_12.py
@@ -1,6 +1,5 @@
 from typing import List, Optional
 
-
 def longest(strings: List[str]) -> Optional[str]:
     if not strings:
         return None
@@ -11,6 +10,5 @@ def longest(strings: List[str]) -> Optional[str]:
             max_length = len(s)
             longest_str = s
     return longest_str
-
 
 print(longest(["apple", "banana", "cherry"]))
