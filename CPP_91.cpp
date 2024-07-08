741822ddb57bdb24b83ca07351c927bed23f3096
beep boop <githubbot@vadim.me>
2024-07-09 01:38:06 +0200

    Solution 22 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7yh14zdh Prompt: 75550de3-ebb8-4e0a-b075-2bc38a0b97c7.cpp:3:14: error: ‘string’ was not declared in this scope     3 | int is_bored(string S){       |              ^~~~~~ 75550de3-ebb8-4e0a-b075-2bc38a0b97c7.cpp:3:14: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 75550de3-ebb8-4e0a-b075-2bc38a0b97c7.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 75550de3-ebb8-4e0a-b075-2bc38a0b97c7.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,7 +1,4 @@
-```cpp
-#include <string>
-
-int is_bored(std::string S){
+int is_bored(std::string S) {
     int count = 0;
     size_t pos = 0;
     while((pos = S.find("I", pos)) != std::string::npos) {
