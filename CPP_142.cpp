52e33e7932864bf33c405fe11c10550ea759e6b4
beep boop <githubbot@vadim.me>
2024-07-09 01:51:46 +0200

    Solution 10 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7gcdsa33 Prompt: 15df21d7-bfcb-4c6d-bf44-8508ffd8aa0e.cpp: In function ‘int main()’: 15df21d7-bfcb-4c6d-bf44-8508ffd8aa0e.cpp:17:5: error: ‘assert’ was not declared in this scope    17 |     assert(sum_squares({-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5,       |     ^~~~~~ 15df21d7-bfcb-4c6d-bf44-8508ffd8aa0e.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <cmath>   +++ |+#include <cassert>     3 | 15df21d7-bfcb-4c6d-bf44-8508ffd8aa0e.cpp: At global scope: 15df21d7-bfcb-4c6d-bf44-8508ffd8aa0e.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 15df21d7-bfcb-4c6d-bf44-8508ffd8aa0e.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -1,16 +1,21 @@
+#include <cassert>
+#include <cmath>
 #include <vector>
-using namespace std;
 
-int sum_squares(vector<int> lst){
-    int total = 0;
+int sum_squares(std::vector<int> lst){
+    int result = 0;
     for(int i = 0; i < lst.size(); i++){
-        if(i % 3 == 0 && i % 4 != 0) total += lst[i] * lst[i];
-        else if(i % 4 == 0 && i % 3 != 0) total += lst[i] * lst[i] * lst[i];
+        if(i % 3 == 0 && i % 4 != 0){
+            result += std::pow(lst[i], 2);
+        } else if(i % 4 == 0 && i % 3 != 0){
+            result += std::pow(lst[i], 3);
+        }
     }
-    return total;
+    return result;
 }
 
-int main(){
-    vector<int> input = {-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 14, -14, 6, 13, 11, 16, 16, 4, 10};
-    cout << "sum_squares of input is: " << sum_squares(input) << endl;
+int main() {
+    assert(sum_squares({-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5,
+                        14, -14, 6, 13, 11, 16, 16, 4, 10}) == -1448);
+    return 0;
 }
