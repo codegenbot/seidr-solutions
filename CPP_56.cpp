c239272603396a131a7fb8664acb4f19b864cbcd
beep boop <githubbot@vadim.me>
2024-07-09 00:33:10 +0200

    Solution 6 of CPP/56, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/o57jkbfm Prompt: cf980ce3-2f5f-4f62-b2c1-7c4d17d98ea4.cpp:1:25: error: ‘string’ was not declared in this scope     1 | bool correct_bracketing(string brackets){       |                         ^~~~~~ In file included from cf980ce3-2f5f-4f62-b2c1-7c4d17d98ea4.cpp:14: cf980ce3-2f5f-4f62-b2c1-7c4d17d98ea4.cpp: In function ‘int main()’: cf980ce3-2f5f-4f62-b2c1-7c4d17d98ea4.cpp:16:36: error: ‘correct_bracketing’ cann ot be used as a function    16 |     assert (not (correct_bracketing("<><><<><>><>>><>")));       |                  ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_56.cpp
+++ CPP_56.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
-bool correct_bracketing(std::string brackets){
+bool correct_bracketing(string brackets){
     int count = 0;
     for(int i=0; i<brackets.length(); i++){
         if(brackets[i] == '<'){
             count++;
-        }
-        else if(brackets[i] == '>'){
+        } else if(brackets[i] == '>'){
             if(count <= 0) return false;
             count--;
         }
     }
     return count == 0;
-}
-
-int main() {
-    assert(correct_bracketing("<><><<><>><>>><>"));
-    std::cout << "Correct bracketing" << std::endl;
-    return 0;
 }
