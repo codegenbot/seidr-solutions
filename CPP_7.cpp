201e530caf96558f3eea38af4f63dfdc277b572a
beep boop <githubbot@vadim.me>
2024-07-09 00:23:23 +0200

    Solution 28 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9s6po2bb Prompt: 1af38db9-4c22-4234-8a2d-efa05c5a7401.cpp:39:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    39 | int main(){       |         ^~ 1af38db9-4c22-4234-8a2d-efa05c5a7401.cpp:39:9: note: remove parentheses to defau lt-initialize a variable    39 | int main(){       |         ^~       |         -- 1af38db9-4c22-4234-8a2d-efa05c5a7401.cpp:39:9: note: or replace parentheses with  braces to value-initialize a variable 1af38db9-4c22-4234-8a2d-efa05c5a7401.cpp:39:11: error: a function-definition is not allowed here before ‘{’ token    39 | int main(){       |           ^ 1af38db9-4c22-4234-8a2d-efa05c5a7401.cpp:41:2: error: expected ‘}’ at end of inp ut    41 | }       |  ^ 1af38db9-4c22-4234-8a2d-efa05c5a7401.cpp:16:102: note: to match this ‘{’    16 | _by_substring(std::vector<std::string> strings, std::string substring){       |                                                                       ^

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,6 +1,16 @@
 #include <vector>
 #include <string>
 
+bool issame(const std::string& a, const std::string& b) {
+    if (a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     bool flag = true;
     if (a.size() != b.size()) return false;
@@ -22,8 +32,7 @@ std::vector<std::string> filter_by_substring(std::vector<std::string> strings, s
     }
     return result;
 
-{
-    int main() {
-        assert(issame({filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run")}, {"grunt", "prune"}));
-        return 0;
+int main() {
+    assert(issame({filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run")}, {"grunt", "prune"}));
+    return 0;
 }
