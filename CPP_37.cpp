2085d027a8fac4f5ee75221c0a3d2f1b3554048e
beep boop <githubbot@vadim.me>
2024-07-09 00:52:56 +0200

    Solution 54 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/4lr1ive3 Prompt: ined here    33 | int main() {       |     ^~~~ In file included from 4e304b8d-d085-4d44-b96d-771394279af6.cpp:39: 4e304b8d-d085-4d44-b96d-771394279af6.cpp: In function ‘int main()’: 4e304b8d-d085-4d44-b96d-771394279af6.cpp:49:29: error: cannot bind non-const lva lue reference of type ‘std::vector<float>&’ to an rvalue of type ‘std::vector<fl oat>’    49 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 4e304b8d-d085-4d44-b96d-771394279af6.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<float>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 4e304b8d-d085-4d44-b96d-771394279af6.cpp:11:31: note:   initializing argument 1 of ‘void sort_even(std::vector<float>&)’    11 | void sort_even(vector<float>& a) {       |                ~~~~~~~~~~~~~~~^

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -9,18 +9,24 @@ bool issame(vector<float> a, vector<float> b) {
 }
 
 void sort_even(vector<float>& a) {
-    vector<pair<float, int>> even_indices;
+    vector<float> even_indices;
+    vector<float> sorted_even_indices;
 
     for (int i = 0; i < a.size(); i++) {
         if (i % 2 == 0) {
-            even_indices.push_back(make_pair(a[i], i));
+            even_indices.push_back(a[i]);
+            sorted_even_indices.push_back(a[i]);
         }
     }
 
-    sort(even_indices.begin(), even_indices.end());
+    sort(sorted_even_indices.begin(), sorted_even_indices.end());
 
-    for (int i = 0; i < even_indices.size(); i++) {
-        a[even_indices[i].second] = even_indices[i].first;
+    int sorted_even_index = 0;
+    for (int i = 0; i < a.size(); i++) {
+        if (i % 2 == 0) {
+            a[i] = sorted_even_indices[sorted_even_index];
+            sorted_even_index++;
+        }
     }
 }
 
