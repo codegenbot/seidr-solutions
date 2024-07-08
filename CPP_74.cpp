909a56326ee653932766a8abb84b3f1afa844236
beep boop <githubbot@vadim.me>
2024-07-09 00:57:22 +0200

    Solution 36 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3x14wabr Prompt: In file included from /usr/include/c++/11/string:39,                  from 3490a58e-d7ce-4ed8-b568-f1d77265b903.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 3490a58e-d7ce-4ed8-b568-f1d77265b903.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 3490a58e-d7ce-4ed8-b568-f1d77265b903.cpp:15:44: error: ‘b’ was not declared in t his scope    15 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 3490a58e-d7ce-4ed8-b568-f1d77265b903.cpp:14: 3490a58e-d7ce-4ed8-b568-f1d77265b903.cpp: In function ‘int main()’: 3490a58e-d7ce-4ed8-b568-f1d77265b903.cpp:24:20: error: ‘total_match’ was not dec lared in this scope    24 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 3490a58e-d7ce-4ed8-b568-f1d77265b903.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,36 +1,13 @@
 ```cpp
 #include <vector>
-#include <string>
+#include <algorithm>
 
-bool issame(vector<string> a, vector<string> b) {
-    if(a.size()!=b.size())
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+    if (a.size() != b.size())
         return false;
-    for(int i=0;i<a.size();i++){
-        if(std::strcmp(a[i].c_str(), b[i].c_str())!=0)
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i].compare(b[i]) != 0)
             return false;
     }
     return true;
-}
-
-int main() {
-    int n;
-    std::cin >> n;
-    vector<string> a(n);
-    for (string &s : a) {
-        cin >> s;
-    }
-
-    vector<string> b(n);
-    for (string &s : b) {
-        cin >> s;
-    }
-
-    bool total_match = issame(a, b);
-
-    if(total_match){
-        cout << "True";
-    }else{
-        cout << "False";
-    }
-    
 }
