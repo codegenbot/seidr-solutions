57d1de4eb0ff2d512b4625939b5c36fd9f9216ec
beep boop <githubbot@vadim.me>
2024-07-09 00:47:35 +0200

    Solution 16 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/fcucnqjl Prompt: In file included from 1b0712be-0cde-4143-bfa9-6ad3508ac965.cpp:13: 1b0712be-0cde-4143-bfa9-6ad3508ac965.cpp: In function ‘int main()’: 1b0712be-0cde-4143-bfa9-6ad3508ac965.cpp:17:42: error: ‘to_string’ was not decla red in this scope; did you mean ‘std::__cxx11::to_string’?    17 |         assert (change_base(x, x + 1) == to_string(x));       |                                          ^~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from 1b0712be-0cde-4143-bfa9-6ad3508ac965.cpp:1: /usr/include/c++/11/bits/basic_string.h:6739:3: note: ‘std::__cxx11::to_string’ declared here  6739 |   to_string(long double __val)       |   ^~~~~~~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -11,7 +11,8 @@ std::string change_base(int x, int base) {
 }
 
 int main() {
-    int x = 10;
-    assert(change_base(x, x + 1) == std::to_string(x));
+    int x = 10;    
+    assert (change_base(x, x + 1) == std::to_string(x));
+    
     return 0;
 }
