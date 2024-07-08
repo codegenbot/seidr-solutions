935695a60bbd3b266365d77272cd48ec222d615f
beep boop <githubbot@vadim.me>
2024-07-09 00:28:13 +0200

    Solution 5 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/swe0u9x3 Prompt: 7710f87e-d0c7-4d1e-9765-fcb7817397f3.cpp:12:22: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    12 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                      ^~       |                      l1 7710f87e-d0c7-4d1e-9765-fcb7817397f3.cpp:12:44: error: ‘s2’ was not declared in this scope; did you mean ‘l2’?    12 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                                            ^~       |                                            l2 7710f87e-d0c7-4d1e-9765-fcb7817397f3.cpp:13:32: error: ‘intersection’ was not de clared in this scope    13 |                       inserter(intersection, intersection.begin()));       |                                ^~~~~~~~~~~~ 7710f87e-d0c7-4d1e-9765-fcb7817397f3.cpp: At global scope: 7710f87e-d0c7-4d1e-9765-fcb7817397f3.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ 7710f87e-d0c7-4d1e-9765-fcb7817397f3.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -4,22 +4,22 @@
 
 using namespace std;
 
-vector<int> common(vector<int> l1, vector<int> l2) {
+set<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
-    set<int> intersection;
-    intersection.insert(intersection.begin(), s1.begin(), s1.end());
-    intersection.insert(intersection.end(), s2.begin(), s2.end());
 
-    vector<int> result(intersection.begin(), intersection.end());
-    return result;
+    set<int> intersection;
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                      inserter(intersection, intersection.begin()));
+
+    return intersection;
 }
 
 int main() {
     vector<int> l1 = {1, 4, 3, 34, 653, 2, 5};
     vector<int> l2 = {5, 7, 1, 5, 9, 653, 121};
 
-    vector<int> result = common(l1, l2);
+    set<int> result = common(l1, l2);
 
     for (int i : result) {
         cout << i << " ";
