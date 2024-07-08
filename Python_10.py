fe86d0e5681ee183294a1e13d7ed26bfef694baf
beep boop <githubbot@vadim.me>
2024-07-09 01:50:32 +0200

    Solution 92 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/gewsiiq5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8bb216b5-334d-414c-b62d-c25c3831d 92e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,3 +1,6 @@
+from operator import reversed
+
+
 def make_palindrome(s):
     if len(s) == 0:
         return ""
@@ -6,10 +9,10 @@ def make_palindrome(s):
     i, j = 0, len(s) - 1
 
     while i <= j:
-        left_half = s[:i+1]
+        left_half = s[: i + 1]
         right_half = s[j:]
         middle = "".join(reversed(left_half[1:]))
-        palindrome = str(left_half[0]) + ''.join(reversed(middle)) + str(right_half)
+        palindrome = str(left_half[0]) + "".join(reversed(middle)) + str(right_half)
         i += 1
         j -= 1
 
