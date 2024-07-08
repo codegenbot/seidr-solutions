e476bae2c67254d1f173127f705c0a4951baa934
beep boop <githubbot@vadim.me>
2024-07-09 00:42:29 +0200

    Solution 21 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3k02qe59 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -6,7 +6,7 @@ bool issame(std::vector<float> a, std::vector<float> b) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
-        if ((a[i] - b[i]) > 1e-9 || (b[i] - a[i]) > 1e-9) {
+        if ((a[i] != b[i]) && (a[i] > 0 || b[i] > 0)) {
             return false;
         }
     }
@@ -21,23 +21,4 @@ float get_positive(std::vector<float> l) {
         }
     }
     return sum;
-}
-
-int main() {
-    int n;
-    std::cin >> n;
-    
-    std::vector<float> numbers(n);
-    for(int i = 0; i < n; i++) {
-        std::cin >> numbers[i];
-    }
-
-    if(issame(numbers, {1.0f})) {
-        float sum = get_positive(numbers);
-        std::cout << "Yes " << sum << "\n";
-    } else {
-        std::cout << "No\n";
-    }
-    
-    return 0;
 }
