9374ea5d160bd75f5f4123b2aef6b290eb8af9d1
beep boop <githubbot@vadim.me>
2024-07-09 00:34:17 +0200

    Solution 8 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/y3orfb0a Prompt: abf8ecd2-2a3d-4720-a665-01d3aba4e56b.cpp:20:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    20 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff abf8ecd2-2a3d-4720-a665-01d3aba4e56b.cpp:23:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    23 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff abf8ecd2-2a3d-4720-a665-01d3aba4e56b.cpp:23:36: error: ‘find_zero’ cannot be use d as a function    23 |         solution = find_zero(coeffs);       |                                    ^ In file included from abf8ecd2-2a3d-4720-a665-01d3aba4e56b.cpp:7: abf8ecd2-2a3d-4720-a665-01d3aba4e56b.cpp:24:21: error: ‘poly’ was not declared i n this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ abf8ecd2-2a3d-4720-a665-01d3aba4e56b.cpp:24:17: error: ‘abs’ was not declared in  this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
 #include <vector>
 #include <cmath>
 #include <cassert>
 
-double poly(const std::vector<double>& coeffs, double x) {
+vector<double> poly(vector<double> coeffs, double x){
     double result = 0.0;
-    for (size_t i = 0; i < coeffs.size(); ++i) {
-        result += coeffs[i] * std::pow(x, i);
+    for (int i = 0; i < coeffs.size(); i++){
+        result += coeffs[i] * pow(x, i);
     }
     return result;
 }
 
-double find_zero(const std::vector<double>& xs) {
+double find_zero(vector<double> xs){
+    assert(xs.size() >= 2);
     double a = xs[0];
     double b = xs[1];
     return -b/a;
