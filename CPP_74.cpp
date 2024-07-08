0e095a26d461254c84fa1f789e330b1511edadb4
beep boop <githubbot@vadim.me>
2024-07-09 01:00:13 +0200

    Solution 41 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3x14wabr Prompt: In file included from /usr/include/c++/11/string:39,                  from 4d46268a-f54c-4011-8494-4353cabe54a7.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 4d46268a-f54c-4011-8494-4353cabe54a7.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 4d46268a-f54c-4011-8494-4353cabe54a7.cpp:16:44: error: ‘b’ was not declared in t his scope    16 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 4d46268a-f54c-4011-8494-4353cabe54a7.cpp:15: 4d46268a-f54c-4011-8494-4353cabe54a7.cpp: In function ‘int main()’: 4d46268a-f54c-4011-8494-4353cabe54a7.cpp:25:20: error: ‘total_match’ was not dec lared in this scope    25 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 4d46268a-f54c-4011-8494-4353cabe54a7.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,7 +1,8 @@
+```cpp
 #include <vector>
 #include <string>
 
-bool issame(vector<string> a,vector<string>b){
+bool total_match(vector<string> a,vector<string>b){
     if(a.size()!=b.size())
         return false;
     for(int i=0;i<a.size();i++){
@@ -9,4 +10,23 @@ bool issame(vector<string> a,vector<string>b){
             return false;
     }
     return true;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<string> a(n);
+    for(auto &x: a){
+        cin >> x;
+    }
+    vector<string> b(n);
+    for(auto &x: b){
+        cin >> x;
+    }
+    
+    if(total_match(a,b))
+        cout << "Is same";
+    else
+        cout << "Not same";
+    
 }
