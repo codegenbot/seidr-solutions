94b08c22c28201e663b67cb9a509bf7559c2ab8d
beep boop <githubbot@vadim.me>
2024-07-09 00:27:27 +0200

    Solution 6 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/emspjx0x Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 13fb3378-604c-43ac-925d-fa9cc0a5b37a.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 13fb3378-604c-43ac-925d-fa9cc0a5b37a.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 13fb3378-604c-43ac-925d-fa9cc0a5b37a.cpp:25: 13fb3378-604c-43ac-925d-fa9cc0a5b37a.cpp: In function ‘int main()’: 13fb3378-604c-43ac-925d-fa9cc0a5b37a.cpp:35:20: error: ‘separate_paren_groups’ w as not declared in this scope    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 13fb3378-604c-43ac-925d-fa9cc0a5b37a.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,38 +1,27 @@
-bool issame(vector<string> a, vector<string> b){
-    if(a.size() != b.size()) {
-        return false;
-    }
-
-    for(int i = 0; i < a.size(); ++i) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-vector<string> separate_paren_groups(string paren_string) {
+vector<string> issame(vector<string> a, vector<string> b) {
     vector<string> result;
     string current_group;
-    int open_braces = 0;
-
+    int balance = 0;
+    
     for (char c : paren_string) {
         if (c == '(') {
-            if (open_braces > 0) {
+            if (balance > 0) {
                 current_group += c;
             }
-            open_braces++;
+            balance++;
         } else if (c == ')') {
-            open_braces--;
-            if (open_braces > 0) {
+            balance--;
+            if (balance > 0) {
                 current_group += c;
-            } else if (open_braces == 0) {
+            } else if (balance == 0) {
                 result.push_back(current_group);
                 current_group = "";
             }
         }
     }
-
+    
     return result;
+}
+int main() {
+    // Your main function code here
 }
