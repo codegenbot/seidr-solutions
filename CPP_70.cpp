e3569d632dee99509d931487facea873a4a4d048
beep boop <githubbot@vadim.me>
2024-07-09 00:48:49 +0200

    Solution 16 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/alkpylw8 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ eb9b3380-c4a9-46d6-bc31-08c77c696a81.cpp:17:25:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,18 +1,23 @@
+```cpp
 #include <iostream>
 #include <vector>
 using namespace std;
 
-bool issame(vector<int> a,vector<int> b){
-    return a==b;
+bool isSame(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
 vector<int> strange_sort_vector(vector<int> lst) {
     vector<int> result;
     if (lst.empty()) return result;
 
+    sort(lst.begin(), lst.end());
+
     while (!lst.empty()) {
-        result.push_back(*min_element(lst.begin(), lst.end()));
-        lst.erase(distance(lst.begin(), min_element(lst.begin(), lst.end())), lst.end());
+        result.push_back(*lst.begin());
+        lst.erase(lst.begin());
+        if (!lst.empty())
+            sort(lst.begin(), lst.end());
     }
 
     return result;
@@ -20,6 +25,6 @@ vector<int> strange_sort_vector(vector<int> lst) {
 
 int main() {
     vector<int> test = strange_sort_vector({111111});
-    for(int i: test) cout << i << " ";
+    for (int i : test) cout << i << " ";
     return 0;
 }
