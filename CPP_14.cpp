e2fddcc22492748febf29513b251c78b32888188
beep boop <githubbot@vadim.me>
2024-07-09 00:57:20 +0200

    Solution 82 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dib3n468 Prompt:       |                                 ^~~~~~ In file included from 37ef0a64-6d26-47e8-9e77-ef9cbf517b1b.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 37ef0a64-6d26-47e8-9e77-ef9cbf517b1b.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 37ef0a64-6d26-47e8-9e77-ef9cbf517b1b.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 37ef0a64-6d26-47e8-9e77-ef9cbf517b1b.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from 37ef0a64-6d26-47e8-9e77-ef9cbf517b1b.cpp:26: 37ef0a64-6d26-47e8-9e77-ef9cbf517b1b.cpp: In function ‘int main()’: 37ef0a64-6d26-47e8-9e77-ef9cbf517b1b.cpp:36:20: error: ‘all_prefixes’ was not de clared in this scope    36 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,19 +1,25 @@
 #include <vector>
 #include <string>
+#include <assert.h>
 
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    if(a.size() != b.size())
+bool issame(const vector<string>& a, const vector<string>& c) {
+    if(a.size() != c.size())
         return false;
     for(int i = 0; i < a.size(); i++)
-        if(a[i] != b[i])
+        if(a[i] != c[i])
             return false;
     return true;
 }
 
-std::vector<std::string> all_prefixes(const std::string& str) {
-    std::vector<std::string> result;
+vector<string> all_prefixes(const string& str) {
+    vector<string> result;
     for (int i = 1; i <= str.length(); i++) {
         result.push_back(str.substr(0, i));
     }
     return result;
+}
+
+int main() {
+    assert(issame(all_prefixes("WWW"), vector<string>({"", "W", "WW", "WWW"})));
+    return 0;
 }
