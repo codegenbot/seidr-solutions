b50c2cd534e6a3f746b41813908c2ab4b2fb9cee
beep boop <githubbot@vadim.me>
2024-07-09 00:50:31 +0200

    Solution 7 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8gpct13x Prompt:       |                              ^~~~~~ 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp:16:37: error: ‘string’ was not declared  in this scope    16 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp:16:44: error: ‘b’ was not declared in t his scope    16 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp:15: 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp: In function ‘int main()’: 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp:25:20: error: ‘total_match’ was not dec lared in this scope    25 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 2b0859c3-4d29-4dd5-b4cd-ea686f488f3d.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,15 +1,25 @@
+```cpp
 #include <vector>
 #include <string>
 
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) return false;
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]) return false;
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
 
-int main() {
-    assert(issame(total_match({"this"}, {}), {}));
-    return 0;
+vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+    int sum1 = 0;
+    for (string s : lst1) {
+        sum1 += s.length();
+    }
+    int sum2 = 0;
+    for (string s : lst2) {
+        sum2 += s.length();
+    }
+    if (sum1 < sum2) return lst1;
+    else if (sum1 > sum2) return lst2;
+    else return issame(lst1, lst2) ? lst1 : lst2;
 }
