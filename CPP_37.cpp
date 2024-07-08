0dbffc45f46089053c494d778ecb0f7e9a3715eb
beep boop <githubbot@vadim.me>
2024-07-09 01:08:50 +0200

    Solution 84 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/4lr1ive3 Prompt: ined here    29 | int main() {       |     ^~~~ In file included from 41d1540f-a681-4912-a2d4-ef9150278023.cpp:36: 41d1540f-a681-4912-a2d4-ef9150278023.cpp: In function ‘int main()’: 41d1540f-a681-4912-a2d4-ef9150278023.cpp:46:29: error: cannot bind non-const lva lue reference of type ‘std::vector<float>&’ to an rvalue of type ‘std::vector<fl oat>’    46 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 41d1540f-a681-4912-a2d4-ef9150278023.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<float>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 41d1540f-a681-4912-a2d4-ef9150278023.cpp:11:31: note:   initializing argument 1 of ‘void sort_even(std::vector<float>&)’    11 | void sort_even(vector<float>& a) {       |                ~~~~~~~~~~~~~~~^

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -22,15 +22,12 @@ void sort_even(vector<float>& a) {
     for (int i = 0; i < even_indices.size(); i++) {
         a[even_indices[i].second] = even_indices[i].first;
     }
-
-    assert(issame(a, {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
 }
 
 int main() {
     vector<float> input = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
     sort_even(input);
-
-    assert(issame(input, {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    assert (issame(input, {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
 
     return 0;
 }
