2a420e7691446d48c871a906c68a81f2ab1de214
beep boop <githubbot@vadim.me>
2024-07-09 14:00:56 +0200

    Solution 55 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ujx0k24n Prompt: c96455c1-dbbe-4d7f-aef8-e2a2503882f3.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~ c96455c1-dbbe-4d7f-aef8-e2a2503882f3.cpp: In function ‘int main()’: c96455c1-dbbe-4d7f-aef8-e2a2503882f3.cpp:51:9: error: ‘vector’ was not declared in this scope    51 |         vector<double> coeffs = {};       |         ^~~~~~ c96455c1-dbbe-4d7f-aef8-e2a2503882f3.cpp:51:9: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from c96455c1-dbbe-4d7f-aef8-e2a2503882f3.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from c96455c1-dbbe-4d7f-aef8-e2a2503882f3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c96455c1-dbbe-4d7f-aef8-e2a2503882f3.cpp:51:16: error: expected primary-expressi on before ‘double’    51 |         vector<double> coeffs = {};       |                ^~~~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,29 +1,30 @@
 #include <vector>
 #include <cmath>
+using namespace std;
 
-std::vector<double> coeffs = {};
+vector<double> coeffs;
 double coeff;
 int n;
 
-double find_zero(std::vector<double> coeffs);
-double poly(std::vector<double> coeffs, double solution);
+double find_zero(vector<double> coeffs);
+double poly(vector<double> coeffs, double solution);
 
 int main() {
-    std::cout << "Enter the number of coefficients: ";
-    std::cin >> n;
+    cout << "Enter the number of coefficients: ";
+    cin >> n;
 
     for (int i = 0; i < n; ++i) {
-        std::cout << "Enter coefficient " << i+1 << ": ";
-        std::cin >> coeff;
+        cout << "Enter coefficient " << i+1 << ": ";
+        cin >> coeff;
         coeffs.push_back(coeff);
     }
 
     double solution = find_zero(coeffs); 
-    assert(std::abs(poly(coeffs, solution)) < 1e-3); 
+    assert(abs(poly(coeffs, solution)) < 1e-3); 
     return 0;
 }
 
-double find_zero(std::vector<double> coeffs) {
+double find_zero(vector<double> coeffs) {
     double sum = 0.0;
     for (int i = 0; i < coeffs.size(); ++i) {
         sum += coeffs[i] / pow(2, i);
@@ -31,7 +32,7 @@ double find_zero(std::vector<double> coeffs) {
     return -sum;
 }
 
-double poly(std::vector<double> coeffs, double solution) {
+double poly(vector<double> coeffs, double solution) {
     double result = 0.0;
     for (int i = 0; i < coeffs.size(); ++i) {
         result += coeffs[i] * pow(solution, i);
