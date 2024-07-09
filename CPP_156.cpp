f8abb12a09621e0acd55446132f02fffbc96aae1
beep boop <githubbot@vadim.me>
2024-07-09 05:03:24 +0200

    Solution 43 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/z1pfhlvw Prompt: b337a006-af91-49ef-8636-6a372abce7c5.cpp:3:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     3 | string int_to_mini_roman(int number) {       | ^~~~~~       | stdin In file included from b337a006-af91-49ef-8636-6a372abce7c5.cpp:19: b337a006-af91-49ef-8636-6a372abce7c5.cpp: In function ‘int main()’: b337a006-af91-49ef-8636-6a372abce7c5.cpp:21:13: error: ‘int_to_mini_romank’ was not declared in this scope    21 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-string int_to_roman(int number) {
+string int_to_mini_roman(int number) {
     vector<pair<int, string>> romanNumerals = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
         {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
 
@@ -19,13 +19,10 @@ string int_to_roman(int number) {
 }
 
 int main() {
-    int num;
-    cout << "Enter a number between 1 and 3999: ";
-    cin >> num;
-    if (num < 1 || num > 3999) {
-        cout << "Number out of range. Please enter a number between 1 and 3999.";
-    } else {
-        cout << "The Roman numeral for the entered number is: " << int_to_roman(num);
-    }
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    string mini_roman = int_to_mini_roman(number);
+    cout << "The mini Roman numeral is: " << mini_roman << endl;
     return 0;
 }
