c50844871cdfdcdbb19ad3b0ff8a4ecd82d25872
beep boop <githubbot@vadim.me>
2024-07-09 04:21:02 +0200

    Solution 16 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fv53j2kl Prompt: acb7e14f-56ce-48c2-9c84-c1fb943f77d6.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,16 +1,13 @@
 #include <boost/any.hpp>
-#include <string>
-#include <iostream>
 
-using namespace std;
 using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return b;
+        return a;
     }
     else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return b;
+        return a;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
         string str1 = boost::any_cast<string>(a);
@@ -19,17 +16,14 @@ boost::any compare_one(boost::any a, boost::any b) {
             return a;
         else if (str1 < str2)
             return b;
-        else
-            return boost::any(string("None"));
     }
-    else {
-        double val1 = boost::any_cast<double>(a);
-        double val2 = boost::any_cast<double>(b);
-        if (val1 > val2)
+    else if ((a.type() == typeid(int) && b.type() == typeid(double)) || 
+             (a.type() == typeid(double) && b.type() == typeid(int))) {
+        double d1 = boost::any_cast<double>(a);
+        double d2 = boost::any_cast<double>(b);
+        if (d1 > d2)
             return a;
-        else if (val1 < val2)
-            return b;
         else
-            return boost::any(string("None"));
+            return b;
     }
 }
