bef5a5189dd4b608780b28faa18e2a0528b94163
beep boop <githubbot@vadim.me>
2024-07-09 10:16:54 +0200

    Solution 33 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/c8pse80p Prompt:    27 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 38e671fe-3024-445a-bc08-e13a7a24ebb8.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 38e671fe-3024-445a-bc08-e13a7a24ebb8.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 38e671fe-3024-445a-bc08-e13a7a24ebb8.cpp:26: 38e671fe-3024-445a-bc08-e13a7a24ebb8.cpp: In function ‘int main()’: 38e671fe-3024-445a-bc08-e13a7a24ebb8.cpp:36:20: error: ‘count_up_to’ was not dec lared in this scope    36 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 38e671fe-3024-445a-bc08-e13a7a24ebb8.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,7 +1,17 @@
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
+vector<int> issame(vector<int> a, vector<int> b) {
+    vector<int> common_elements;
+    unordered_set<int> setA(a.begin(), a.end());
+    unordered_set<int> setB(b.begin(), b.end());
     
+    for (int num : setA) {
+        if (setB.count(num)) {
+            common_elements.push_back(num);
+        }
+    }
+    
+    return common_elements;
+}
+
 vector<int> count_up_to(int n) {
     vector<int> result;
     if (n <= 1) {
