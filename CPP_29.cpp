87f4cd822f0bb7cd8791f38e627525ccf01431f2
beep boop <githubbot@vadim.me>
2024-07-09 03:29:38 +0200

    Solution 52 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/o5dcqdjt Prompt:    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ ec50db17-b022-47ea-b827-40e1d6830a5e.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ ec50db17-b022-47ea-b827-40e1d6830a5e.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from ec50db17-b022-47ea-b827-40e1d6830a5e.cpp:23: ec50db17-b022-47ea-b827-40e1d6830a5e.cpp: In function ‘int main()’: ec50db17-b022-47ea-b827-40e1d6830a5e.cpp:33:20: error: ‘filter_by_prefix’ was no t declared in this scope    33 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ ec50db17-b022-47ea-b827-40e1d6830a5e.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,7 +1,4 @@
 ```cpp
-vector<string> filter_by_prefix(vector<string> strings, string prefix);
-bool issame(vector<string> a, vector<string> b);
-
 vector<string> filter_by_prefix(vector<string> strings, string prefix){
     vector<string> result;
     for(auto s : strings){
@@ -12,14 +9,13 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
 }
 
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size())
-        return false;
-    for(int i = 0; i < a.size(); i++)
-        if(find(b.begin(), b.end(), a[i]) == b.end())
-            return false;
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]) return false;
+    }
     return true;
 }
 
 int main() {
-    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAC", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));
+    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAC", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
 }
