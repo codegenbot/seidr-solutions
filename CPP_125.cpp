83e1495ea96972051f6190ab1151bdf05889eec0
beep boop <githubbot@vadim.me>
2024-07-09 22:07:16 +0200

    Solution 8 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/wtp5skh7 Prompt:       |                              ^~~~~~ 27a5989b-f605-45b8-ab92-192df46312c9.cpp:37:37: error: ‘string’ was not declared  in this scope    37 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 27a5989b-f605-45b8-ab92-192df46312c9.cpp:37:44: error: ‘b’ was not declared in t his scope    37 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 27a5989b-f605-45b8-ab92-192df46312c9.cpp:37:45: error: expression list treated a s compound expression in initializer [-fpermissive]    37 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 27a5989b-f605-45b8-ab92-192df46312c9.cpp:36: 27a5989b-f605-45b8-ab92-192df46312c9.cpp: In function ‘int main()’: 27a5989b-f605-45b8-ab92-192df46312c9.cpp:46:20: error: ‘split_words’ was not dec lared in this scope    46 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 27a5989b-f605-45b8-ab92-192df46312c9.cpp:46:19: error: ‘issame’ cannot be used a s a function    46 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,56 +1,13 @@
-#include <vector>
-#include <string>
-
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if (a.size() != b.size()) {
+bool issame(vector<string> a,vector<string>b){
+    if(a.size() != b.size())
         return false;
-    }
-    
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
+    for(int i = 0; i < a.size(); i++)
+        if(a[i] != b[i])
             return false;
-        }
-    }
-    
     return true;
 }
 
-std::vector<std::string> split_words(string txt) {
-    vector<string> result;
-    string word = "";
-    
-    for (char c : txt) {
-        if (isspace(c)) {
-            if (!word.empty()) {
-                result.push_back(word);
-                word = "";
-            }
-        } else if (c == ',') {
-            if (!word.empty()) {
-                result.push_back(word);
-                word = "";
-            }
-        } else {
-            word += c;
-        }
-    }
-    
-    if (!word.empty()) {
-        result.push_back(word);
-    } else {
-        int count = 0;
-        for (char c = 'a'; c <= 'z'; ++c) {
-            if (txt.find(c) != string::npos) {
-                count++;
-            }
-        }
-        result.push_back(to_string(count));
-    }
-    
-    return result;
-}
-
 int main() {
-    assert(issame(split_words(""), {"0"}));
+    assert (issame(split_words(""), {"0"}));
     return 0;
 }
