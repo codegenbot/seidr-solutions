43eb016e064261655688816d639ad8a44e899538
beep boop <githubbot@vadim.me>
2024-07-10 00:26:41 +0200

    Solution 48 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/4ovdgu6m Prompt: 22850bc2-6de3-412a-8cc4-3001a6f005d4.cpp:1:9: error: ‘string’ was not declared i n this scope     1 | int CAP(string s) { int cnt = 0; for (char c : s) if (isupper(c)) cnt++;  return cnt; }       |         ^~~~~~ 22850bc2-6de3-412a-8cc4-3001a6f005d4.cpp:2:8: error: ‘string’ was not declared i n this scope     2 | int SM(string s) { int cnt = 0; for (char c : s) if (islower(c)) cnt++; return cnt; }       |        ^~~~~~ 22850bc2-6de3-412a-8cc4-3001a6f005d4.cpp:4:1: error: ‘string’ does not name a ty pe     4 | string Strongest_Extension(string class_name, vector<string> extensions)  {       | ^~~~~~ In file included from 22850bc2-6de3-412a-8cc4-3001a6f005d4.cpp:19: 22850bc2-6de3-412a-8cc4-3001a6f005d4.cpp: In function ‘int main()’: 22850bc2-6de3-412a-8cc4-3001a6f005d4.cpp:21:13: error: ‘Strongest_Extension’ was  not declared in this scope    21 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,35 +1,21 @@
-#include <string>
+#include <iostream>
 #include <vector>
-#include <cassert>
+#include <string>
 
-using namespace std;
+int CAP(string s) { int cnt = 0; for (char c : s) if (isupper(c)) cnt++; return cnt; }
+int SM(string s) { int cnt = 0; for (char c : s) if (islower(c)) cnt++; return cnt; }
 
 string Strongest_Extension(string class_name, vector<string> extensions) {
-    int max_strength = INT_MIN;
     string strongest_extension = "";
-    
-    for (const auto& ext : extensions) {
-        int cap_count = 0, sm_count = 0;
-        for (char c : ext) {
-            if (isupper(c)) {
-                cap_count++;
-            } else if (islower(c)) {
-                sm_count++;
-            }
-        }
-        
-        int strength = cap_count - sm_count;
+    int max_strength = INT_MIN;
+
+    for (string ext : extensions) {
+        int strength = CAP(ext) - SM(ext);
         if (strength > max_strength || (strength == max_strength && ext < strongest_extension)) {
             max_strength = strength;
             strongest_extension = ext;
         }
     }
-    
-    return class_name + "." + strongest_extension;
-}
 
-int main() {
-    assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
-    
-    return 0;
+    return class_name + "." + strongest_extension;
 }
