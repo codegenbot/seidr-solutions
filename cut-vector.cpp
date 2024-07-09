d73c7b95e8f4320ea8d04a6a5ef228739c9cc8b2
beep boop <githubbot@vadim.me>
2024-07-09 05:57:29 +0200

    Solution 17 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/eb053e6f Prompt: >, std::vector<int> > cutVector(std::vector<int>&)’: 2ee60fa9-3c4a-4f31-a9a5-dc67e01ac73b.cpp:5:19: error: ‘INT_MAX’ was not declared  in this scope     5 |     int minDiff = INT_MAX;       |                   ^~~~~~~ 2ee60fa9-3c4a-4f31-a9a5-dc67e01ac73b.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; 2ee60fa9-3c4a-4f31-a9a5-dc67e01ac73b.cpp:8:20: error: ‘abs’ was not declared in this scope     8 |         int diff = abs(nums[i] - nums[i-1]);       |                    ^~~ 2ee60fa9-3c4a-4f31-a9a5-dc67e01ac73b.cpp:14:97: error: expected ‘}’ at end of in put    14 |     return {{nums.begin(), nums.begin() + splitIndex}, {nums.begin() + s plitIndex, nums.end()}};       |                         ^ 2ee60fa9-3c4a-4f31-a9a5-dc67e01ac73b.cpp:4:61: note: to match this ‘{’     4 | pair<vector<int>, vector<int>> cutVector(vector<int>& nums) {       |                                                             ^

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -5,9 +5,6 @@ using namespace std;
 pair<vector<int>, vector<int>> cutVector(vector<int>& nums) {
     int minDiff = INT_MAX;
     int splitIndex = 0;
-    if(nums.size() == 1) {
-        return {{},{}};
-    }
     for (int i = 1; i < nums.size(); i++) {
         int diff = abs(nums[i] - nums[i-1]);
         if (diff < minDiff) {
@@ -15,5 +12,15 @@ pair<vector<int>, vector<int>> cutVector(vector<int>& nums) {
             splitIndex = i;
         }
     }
-    return {{nums.begin(), nums.begin() + splitIndex}, {nums.begin() + splitIndex, nums.end()}};
+
+    vector<int> left, right;
+    if (nums.empty()) {
+        return make_pair(left, right);
+    } else if (nums.size() == 1) {
+        return make_pair({nums[0]}, {});
+    } else {
+        left.assign(nums.begin(), nums.begin() + splitIndex);
+        right.assign(nums.begin() + splitIndex, nums.end());
+        return make_pair(left, right);
+    }
 }
