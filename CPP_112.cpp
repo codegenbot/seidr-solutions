ef8445aa684aa120d092690c2b1d213390400a0e
beep boop <githubbot@vadim.me>
2024-07-09 10:25:25 +0200

    Solution 4 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/owo9prrk Prompt:       |                              ^~~~~~ 43fdf1ea-f8ea-46ef-93a8-4ce897959a9b.cpp:14:37: error: ‘string’ was not declared  in this scope    14 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 43fdf1ea-f8ea-46ef-93a8-4ce897959a9b.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 43fdf1ea-f8ea-46ef-93a8-4ce897959a9b.cpp:14:45: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 43fdf1ea-f8ea-46ef-93a8-4ce897959a9b.cpp:13: 43fdf1ea-f8ea-46ef-93a8-4ce897959a9b.cpp: In function ‘int main()’: 43fdf1ea-f8ea-46ef-93a8-4ce897959a9b.cpp:23:20: error: ‘reverse_delete’ was not declared in this scope    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 43fdf1ea-f8ea-46ef-93a8-4ce897959a9b.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,11 +1,25 @@
-vector<string> reverse_delete(string s, string c) {
-        string result = "";
-        for (char ch : s) {
-            if (c.find(ch) == string::npos) {
-                result += ch;
-            }
+#include <string>
+
+bool issame(vector<string> a, vector<string> b);
+
+vector<string> reverse_delete(string s, string c);
+
+int main() {
+    // Add your code here
+}
+
+vector<string> reverse_delete(string s, string c){
+    string result = "";
+    for(char ch : s){
+        if(c.find(ch) == string::npos){
+            result += ch;
         }
-        string reversed = result;
-        reverse(reversed.begin(), reversed.end());
-        return {result, result == reversed ? "True" : "False"};
     }
+    string result_reversed = result;
+    reverse(result_reversed.begin(), result_reversed.end());
+    return {result, issame(result, result_reversed) ? "True" : "False"};
+}
+
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
+}
