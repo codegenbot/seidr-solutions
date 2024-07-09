3c9c6bcd4ca7834411739d3e39e6252e03fd4af3
beep boop <githubbot@vadim.me>
2024-07-09 03:15:26 +0200

    Solution 72 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zlpto1aq Prompt: L GrQw NqRz ZkDw wR ZuLwH Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6d3ca285-9fe5-4415-9a22-57e21c98c d87.py", line 22, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6d3ca285-9fe5-4415-9a22-57e21c98c d87.py", line 18, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -5,10 +5,10 @@ def encode(message):
         if char.isalpha():
             ascii_offset = 65 if char.isupper() else 97
             result += chr((ord(char) - ascii_offset + 3) % 26 + ascii_offset)
-        elif char.isspace():
-            result += ' '
-        else:
+        elif not char.isalnum() and char != ' ':
             result += '*'
+        else:
+            result += char
     return result
 
 print(encode("I DoNt KnOw WhAt tO WrItE"))
