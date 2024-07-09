e7715c8a44d798c94ce00a315444a740390bb5f4
beep boop <githubbot@vadim.me>
2024-07-09 05:23:26 +0200

    Solution 41 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/v59n641y Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/349b2645-9ef9-4157-89f6-b8f3278dd 907.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,5 +1,5 @@
 from typing import List, Any
 
 def filter_integers(values: List[Any]) -> List[int]:
-    result = [i*2 for i in values if isinstance(i, int)]
+    result = [value for value in values if isinstance(value, int) and value > 10]
     return result
