b4aad818d84167c71434ba9d975133b3ad294a33
beep boop <githubbot@vadim.me>
2024-07-09 03:48:30 +0200

    Solution 70 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jvy931jd Prompt:       |             ^~~~~~ a55a2e08-cdd5-4e51-a4e6-3de9feb51f53.cpp:23:36: error: expected primary-expressi on before ‘float’    23 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ a55a2e08-cdd5-4e51-a4e6-3de9feb51f53.cpp:23:43: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<float> a,vector<float>b){       |                                           ^ a55a2e08-cdd5-4e51-a4e6-3de9feb51f53.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ a55a2e08-cdd5-4e51-a4e6-3de9feb51f53.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from a55a2e08-cdd5-4e51-a4e6-3de9feb51f53.cpp:22: a55a2e08-cdd5-4e51-a4e6-3de9feb51f53.cpp: In function ‘int main()’: a55a2e08-cdd5-4e51-a4e6-3de9feb51f53.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -15,20 +15,6 @@ bool are_equal(std::vector<float> a, std::vector<float> b){
     return std::equal(a.begin(),a.end(),b.begin());
 }
 
-std::vector<float> get_positive(std::vector<float> l){
-    std::vector<float> result;
-    for(float num : l) {
-        if(num > 0) {
-            result.push_back(num);
-        }
-    }
-    return result;
-}
-
-bool are_equal(std::vector<float> a, std::vector<float> b){
-    return std::equal(a.begin(),a.end(),b.begin());
-}
-
 int main() {
     assert (are_equal(get_positive({}), {}) );
     return 0;
