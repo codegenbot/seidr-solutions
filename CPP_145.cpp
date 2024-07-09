fce7d94112437645b38de2d65b500f5f355f5dfe
beep boop <githubbot@vadim.me>
2024-07-09 11:54:42 +0200

    Solution 22 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/q733fkvg Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ce63df29-e68c-4c65-962c-bc6218cfa8af.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ce63df29-e68c-4c65-962c-bc6218cfa8af.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ce63df29-e68c-4c65-962c-bc6218cfa8af.cpp:19: ce63df29-e68c-4c65-962c-bc6218cfa8af.cpp: In function ‘int main()’: ce63df29-e68c-4c65-962c-bc6218cfa8af.cpp:29:20: error: ‘order_by_points’ was not  declared in this scope    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ ce63df29-e68c-4c65-962c-bc6218cfa8af.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,22 +1,29 @@
-bool issame(vector<int> a, vector<int> b){
-    int sum_a = accumulate(a.begin(), a.end(), 0);
-    int sum_b = accumulate(b.begin(), b.end(), 0);
+bool issame(vector<int> a, vector<int> b) {
+    int sum_a = 0, sum_b = 0;
+    for (int num : a) {
+        sum_a += abs(num);
+    }
+    for (int num : b) {
+        sum_b += abs(num);
+    }
     return sum_a == sum_b;
 }
 
-void order_by_points(vector<int>& nums){
-    sort(nums.begin(), nums.end(), [](int a, int b){
-        int sum_a = accumulate(to_string(abs(a)).begin(), to_string(abs(a)).end(), 0, 
-                               [](int sum, char c){return sum + (c - '0');});
-        int sum_b = accumulate(to_string(abs(b)).begin(), to_string(abs(b)).end(), 0, 
-                               [](int sum, char c){return sum + (c - '0');});
-        
-        if(sum_a == sum_b){
-            return find(nums.begin(), nums.end(), a) < find(nums.begin(), nums.end(), b);
+void order_by_points(vector<int>& nums) {
+    sort(nums.begin(), nums.end(), [](int a, int b) {
+        int sum_a = 0, sum_b = 0;
+        int temp_a = abs(a), temp_b = abs(b);
+        while(temp_a > 0){
+            sum_a += temp_a % 10;
+            temp_a /= 10;
+        }
+        while(temp_b > 0){
+            sum_b += temp_b % 10;
+            temp_b /= 10;
+        }
+        if (sum_a == sum_b) {
+            return a < b;
         }
         return sum_a < sum_b;
     });
 }
-
-order_by_points(nums);
-return nums;
