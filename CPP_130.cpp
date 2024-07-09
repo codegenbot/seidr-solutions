f637611b5d4488bb7e69dd9e58c2506166cfa10d
beep boop <githubbot@vadim.me>
2024-07-09 11:23:42 +0200

    Solution 21 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/7winz8yi Prompt:       |                    ^~~ ddd232a5-df66-4d5d-a237-79f6b01c9bb1.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ddd232a5-df66-4d5d-a237-79f6b01c9bb1.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ddd232a5-df66-4d5d-a237-79f6b01c9bb1.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ddd232a5-df66-4d5d-a237-79f6b01c9bb1.cpp:18: ddd232a5-df66-4d5d-a237-79f6b01c9bb1.cpp: In function ‘int main()’: ddd232a5-df66-4d5d-a237-79f6b01c9bb1.cpp:28:20: error: ‘tri’ was not declared in  this scope    28 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ ddd232a5-df66-4d5d-a237-79f6b01c9bb1.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,9 +1,6 @@
 #include <vector>
-#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
+bool issame(std::vector<int> a, std::vector<int> b);
 
 std::vector<int> tri(int n) {
     std::vector<int> result;
@@ -15,7 +12,7 @@ std::vector<int> tri(int n) {
                 if (i % 2 == 0) {
                     result.push_back(1 + i / 2);
                 } else {
-                    result.push_back(result[i - 1] + result[i - 2] + result[i - 1]);
+                    result.push_back(result[i - 1] + result[i - 2] + result[i + 1]);
                 }
             }
         }
@@ -23,6 +20,6 @@ std::vector<int> tri(int n) {
     return result;
 }
 
-// assert(issame(tri(0), {3}));
-// assert(issame(tri(1), {3, 1}));
-// assert(issame(tri(5), {3, 1, 1, 5, 8, 13}));
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
