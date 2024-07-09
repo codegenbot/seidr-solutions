18d14d8fb324246fd00ef95ccaa73cea39b9fa89
beep boop <githubbot@vadim.me>
2024-07-10 00:59:28 +0200

    Solution 32 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8mlwntfv Prompt: b911aa9a-34dc-43ea-a9ea-7a1737faf43f.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<pair<int, string>> roman = {{1000,"M"},{900,"CM"},{500,"D"},{400, "CD"},{100,"C"},{90,"XC"},{50,"L"},{40,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"}, {1,"I"}};       | ^~~~~~ b911aa9a-34dc-43ea-a9ea-7a1737faf43f.cpp:2:1: error: ‘string’ does not name a ty pe     2 | string int_to_mini_roman(int number){       | ^~~~~~ In file included from b911aa9a-34dc-43ea-a9ea-7a1737faf43f.cpp:13: b911aa9a-34dc-43ea-a9ea-7a1737faf43f.cpp: In function ‘int main()’: b911aa9a-34dc-43ea-a9ea-7a1737faf43f.cpp:15:13: error: ‘int_to_mini_romank’ was not declared in this scope    15 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,9 +1,7 @@
-#include <iostream>
 #include <vector>
 #include <string>
 
 std::vector<std::pair<int, std::string>> roman = {{1000,"M"},{900,"CM"},{500,"D"},{400,"CD"},{100,"C"},{90,"XC"},{50,"L"},{40,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}};
-
 std::string int_to_mini_roman(int number){
     std::string romanNumeral = "";
     for(auto p : roman){
@@ -13,9 +11,4 @@ std::string int_to_mini_roman(int number){
         }
     }
     return romanNumeral;
-}
-
-int main() {
-    assert(int_to_mini_roman(1000) == "M");
-    return 0;
 }
