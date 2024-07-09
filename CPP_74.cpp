ac4480b9ee5f300d8f81dd385ba2aa928b5ce997
beep boop <githubbot@vadim.me>
2024-07-09 04:30:21 +0200

    Solution 81 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8ahd9aop Prompt:  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 90896511-7dfb-4c8f-89a2-01ddbdebfb53.cpp:26:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 90896511-7dfb-4c8f-89a2-01ddbdebfb53.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 90896511-7dfb-4c8f-89a2-01ddbdebfb53.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 90896511-7dfb-4c8f-89a2-01ddbdebfb53.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 90896511-7dfb-4c8f-89a2-01ddbdebfb53.cpp:25: 90896511-7dfb-4c8f-89a2-01ddbdebfb53.cpp: In function ‘int main()’: 90896511-7dfb-4c8f-89a2-01ddbdebfb53.cpp:35:20: error: ‘total_match’ was not dec lared in this scope    35 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -24,6 +24,30 @@ std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<
 
 int main() {
     assert(issame(total_match({"this"}, {}), {}));
-    // ... rest of your code ...
+    std::vector<std::string> v1, v2;
+    std::cout << "Enter the strings for list 1. Enter 'stop' to stop entering.\n";
+    while (true) {
+        std::string s;
+        std::cin >> s;
+        if (s == "stop") break;
+        v1.push_back(s);
+    }
+    
+    std::cout << "\nEnter the strings for list 2. Enter 'stop' to stop entering.\n";
+    while (true) {
+        std::string s;
+        std::cin >> s;
+        if (s == "stop") break;
+        v2.push_back(s);
+    }
+
+    std::vector<std::string> result = total_match(v1, v2);
+
+    std::cout << "\nThe total match is: ";
+    for (const auto& s : result) {
+        std::cout << s << " ";
+    }
+    std::cout << '\n';
+    
     return 0;
 }
