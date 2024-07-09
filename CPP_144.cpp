f63477e0d236a942f75f73a64fb63de81aeea06c
beep boop <githubbot@vadim.me>
2024-07-09 17:18:55 +0200

    Solution 2 of CPP/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j6ijrqwh Prompt: >&, signed char*)’   852 |     operator>>(basic_istream<char, _Traits>& __in, signed char* __s)       |     ^~~~~~~~ /usr/include/c++/11/istream:852:5: note:   template argument deduction/substitut ion failed: 7b187331-b451-43e1-aa5d-c99917809096.cpp:10:16: note:   cannot convert ‘'/'’ (ty pe ‘char’) to type ‘signed char*’    10 |     s2 >> c >> '/' >> d;       |                ^~~ In file included from /usr/include/c++/11/sstream:38,                  from 7b187331-b451-43e1-aa5d-c99917809096.cpp:2: /usr/include/c++/11/istream:1006:5: note: candidate: ‘template<class _Istream, c lass _Tp> _Istream&& std::operator>>(_Istream&&, _Tp&&)’  1006 |     operator>>(_Istream&& __is, _Tp&& __x)       |     ^~~~~~~~ /usr/include/c++/11/istream:1006:5: note:   template argument deduction/substitu tion failed: /usr/include/c++/11/istream: In substitution of ‘template<class _Istream, class _Tp> _Istream&& std::operator>>(_Istream&&, _Tp&&) [with _Istream = std::basic_i stream<char>&; _Tp = char]’: 7b187331-b451-43e1-aa5d-c99917809096.cpp:10:16:   required from here /usr/include/c++/11/istream:1006:5: error: no type named ‘type’ in ‘struct std:: enable_if<false, void>’

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -6,8 +6,23 @@ using namespace std;
 bool simplify(string x, string n) {
     int a = 0, b = 0, c = 0, d = 0;
     stringstream s1(x), s2(n);
-    s1 >> a >> '/' >> b;
-    s2 >> c >> '/' >> d;
+    
+    // Remove '/' from the strings
+    char temp;
+    while(s2 >> temp) {
+        if(temp == '/')
+            break;
+        c *= 10;
+        c += temp - '0';
+    }
+    
+    // Remove '/' from the strings
+    while(s1 >> temp) {
+        if(temp == '/')
+            break;
+        d *= 10;
+        d += temp - '0';
+    }
     
     return (a*d == b*c);
 }
