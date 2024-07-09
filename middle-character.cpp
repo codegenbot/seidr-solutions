c0c9e67c6771d3c8c1f43f5debcb6b8b2706d0c1
beep boop <githubbot@vadim.me>
2024-07-09 03:37:02 +0200

    Solution 30 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/ux0vjpor Prompt: d467c8fe-b680-4f00-91ce-fda4818e4606.cpp: In function ‘std::string middleCharact er(std::string)’: d467c8fe-b680-4f00-91ce-fda4818e4606.cpp:5:23: error: could not convert ‘(((n % 2) == 1) ? ((int)s.std::__cxx11::basic_string<char>::operator[](((std::__cxx11:: basic_string<char>::size_type)(n / 2)))) : (((int)s.std::__cxx11::basic_string<c har>::operator[](((std::__cxx11::basic_string<char>::size_type)((n - 1) / 2)))) + ((int)s.std::__cxx11::basic_string<char>::operator[](((std::__cxx11::basic_str ing<char>::size_type)(n / 2))))))’ from ‘int’ to ‘std::string’ {aka ‘std::__cxx1 1::basic_string<char>’}     5 |     return n % 2 == 1 ? s[n / 2] : (s[(n - 1) / 2] + s[n / 2]);       |            ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                       |       |                       int

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,7 +1,4 @@
-```cpp
-#include <string>
-
 std::string middleCharacter(std::string s) {
     int n = s.length();
-    return n % 2 == 1 ? std::string{1, s[n / 2]} : s.substr((n - 1) / 2, 2);
+    return n % 2 == 1 ? s.substr(n / 2, 1) : s.substr((n - 1) / 2, 2);
 }
