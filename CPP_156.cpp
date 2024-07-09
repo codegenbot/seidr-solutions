9200d296c964a39087c254398b64756d8ac43414
beep boop <githubbot@vadim.me>
2024-07-09 12:35:13 +0200

    Solution 27 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/eh45pa3v Prompt: In file included from 602c191f-f8a3-41bf-bf15-4b2606cbac6f.cpp:20: 602c191f-f8a3-41bf-bf15-4b2606cbac6f.cpp: In function ‘int main()’: 602c191f-f8a3-41bf-bf15-4b2606cbac6f.cpp:22:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    22 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,14 +1,1 @@
-string int_to_mini_roman(int number) {
-    vector<string> roman_numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    vector<int> decimal_values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-
-    string roman = "";
-    for (int i = 0; i < decimal_values.size(); i++) {
-        while (number >= decimal_values[i]) {
-            number -= decimal_values[i];
-            roman += roman_numerals[i];
-        }
-    }
-
-    return roman;
-}
+```string int_to_mini_roman(int number)```
