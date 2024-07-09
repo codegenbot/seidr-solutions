af6cfbf67eee1343d6613b82d55786132473ac98
beep boop <githubbot@vadim.me>
2024-07-09 07:02:45 +0200

    Solution 1 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n7aimvu6 Prompt: Complete the following code given the task description and function signature.

--- Python_67.py
+++ Python_67.py
@@ -1,4 +1,4 @@
 def fruit_distribution(s, n):
-    s = s.lower()
-    num_apples = int("".join(filter(str.isdigit, s))[:-7])
-    return n - num_apples
+    s = s.replace("apples ", "")
+    s = s.replace("oranges ", "")
+    return n - int(s.split()[0].replace("and", "").split()[0]) - 1
