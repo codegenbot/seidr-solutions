3a5f478bfd1892a4510e3396b41b31b0e292c31f
beep boop <githubbot@vadim.me>
2024-07-09 08:19:23 +0200

    Solution 24 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/8ou4ga8l Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6f95d05b-0863-4c33-a6f0-bc2b66211 abc.py", line 37, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6f95d05b-0863-4c33-a6f0-bc2b66211 abc.py", line 7, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,5 +1,4 @@
 def sum_squares(lst):
-    result = 0
-    for i in lst:
-        result += (i ** 3 if i % 12 == 0 or i % 4 == 0 else i ** 2)
-    return result
+    if not all(1 <= i <= 10 for i in lst):
+        return "Error: List must contain only numbers between 1 and 10."
+    return sum(i**2 if i % 4 == 0 or i % 12 == 0 else i for i in lst)
