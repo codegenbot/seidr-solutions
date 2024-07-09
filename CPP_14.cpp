ab4bd827032786d25ff524d3cdf42a6da209966d
beep boop <githubbot@vadim.me>
2024-07-09 07:06:30 +0200

    Solution 35 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/g3dq5xin Prompt:       |                              ^~~~~~ c69d91af-17f2-4815-ab06-d1ff99ab6ac2.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c69d91af-17f2-4815-ab06-d1ff99ab6ac2.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c69d91af-17f2-4815-ab06-d1ff99ab6ac2.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c69d91af-17f2-4815-ab06-d1ff99ab6ac2.cpp:9: c69d91af-17f2-4815-ab06-d1ff99ab6ac2.cpp: In function ‘int main()’: c69d91af-17f2-4815-ab06-d1ff99ab6ac2.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ c69d91af-17f2-4815-ab06-d1ff99ab6ac2.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,17 +1,12 @@
 #include <vector>
 #include <string>
 
-bool issame(const vector<string>& prefixes){
-    for(int i = 1; i < prefixes.size(); ++i){
-        if(prefixes[i] != prefixes[i-1]){
-            return false;
-        }
-    }
-    return true;
+bool issame(const std::string& s1, const std::string& s2) {
+    return s1 == s2;
 }
 
-vector<string> all_prefixes(string str){
-    vector<string> result;
+std::vector<std::string> all_prefixes(std::string str) {
+    std::vector<std::string> result;
     for (int i = 1; i <= str.size(); ++i) {
         result.push_back(str.substr(0, i));
     }
