157e5d7204f9ed6bf1e06c6f834abf4c43e5fe8e
beep boop <githubbot@vadim.me>
2024-07-09 03:48:08 +0200

    Solution 30 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tc8gxfw7 Prompt: 76204235-c016-4c9e-9fea-e782172c3547.cpp: In function ‘std::vector<std::pair<int , int> > pluck(std::vector<int>)’: 76204235-c016-4c9e-9fea-e782172c3547.cpp:9:24: error: ‘INT_MAX’ was not declared  in this scope     9 |     int smallestEven = INT_MAX;       |                        ^~~~~~~ 76204235-c016-4c9e-9fea-e782172c3547.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | In file included from 76204235-c016-4c9e-9fea-e782172c3547.cpp:25: 76204235-c016-4c9e-9fea-e782172c3547.cpp: In function ‘int main()’: 76204235-c016-4c9e-9fea-e782172c3547.cpp:35:25: error: could not convert ‘pluck( std::vector<int>)()’ from ‘vector<std::pair<int, int>>’ to ‘vector<int>’    35 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ~~~~~^~~~~~~~~~~~~~       |                         |       |                         vector<std::pair<int, int>>

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,35 +1,16 @@
-```cpp
 #include <climits>
 #include <vector>
 
 using namespace std;
 
-vector<pair<int, int>>() pluck(vector<int> arr) {
+vector<pair<int, int>> pluck(vector<int> arr) {
     vector<pair<int, int>> result;
+    
     if (arr.empty()) return {};
 
     for (int i = 0; i < arr.size(); i++) {
-        bool isEven = false;
-        int smallestIndex = -1;
-        for (int j = 0; j < arr.size(); j++) {
-            if (arr[j] % 2 == 0) {
-                isEven = true;
-                if (arr[j] < arr[i]) {
-                    smallestIndex = i;
-                }
-            }
-        }
-
-        if (isEven) {
-            result.push_back({arr[smallestIndex], smallestIndex});
-            for(int j = 0; j < arr.size(); j++) {
-                arr.erase(arr.begin() + smallestIndex);
-                break;
-            }
-        } else {
+        if (arr[i] % 2 == 0) {
             result.push_back({arr[i], i});
-            arr.erase(arr.begin() + i);
-            break;
         }
     }
 
