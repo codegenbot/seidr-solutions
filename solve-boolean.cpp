53180236519647b54bc72ecafbf99f3dab507c52
beep boop <githubbot@vadim.me>
2024-07-09 07:28:53 +0200

    Solution 6 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/jl3bt86n Prompt:       |            ^~~~~~~~~~ 52f12a10-35f4-41c3-8f59-14480d3513bd.cpp:21:9: error: ‘cout’ was not declared in  this scope    21 |         cout << "True" << endl;       |         ^~~~ 52f12a10-35f4-41c3-8f59-14480d3513bd.cpp:21:27: error: ‘endl’ was not declared i n this scope    21 |         cout << "True" << endl;       |                           ^~~~ 52f12a10-35f4-41c3-8f59-14480d3513bd.cpp:23:9: error: ‘cout’ was not declared in  this scope    23 |         cout << "False" << endl;       |         ^~~~ 52f12a10-35f4-41c3-8f59-14480d3513bd.cpp:23:28: error: ‘endl’ was not declared i n this scope    23 |         cout << "False" << endl;       |                            ^~~~ 52f12a10-35f4-41c3-8f59-14480d3513bd.cpp: In function ‘bool evaluateBooleanExpre ssion(const int&)’: 52f12a10-35f4-41c3-8f59-14480d3513bd.cpp:15:1: warning: control reaches end of n on-void function [-Wreturn-type]    15 | }       | ^

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
-#include <string>
+
+bool evaluateBooleanExpression(const std::string& expression) {
+    if (expression == "t") {
+        return true;
+    } else if (expression == "f") {
+        return false;
+    } else {
+        bool left = evaluateBooleanExpression(expression.substr(0, 1));
+        bool right = evaluateBooleanExpression(expression.substr(2));
+        if (expression[1] == '&') {
+            return left && right;
+        } else {
+            return left || right;
+        }
+    }
+}
 
 int main() {
-    std::string input;
-    std::cin >> input;
-
-    if (input == "T") {
+    std::string expression;
+    std::cin >> expression;
+    if (evaluateBooleanExpression(expression)) {
         std::cout << "True" << std::endl;
     } else {
         std::cout << "False" << std::endl;
     }
-
     return 0;
 }
