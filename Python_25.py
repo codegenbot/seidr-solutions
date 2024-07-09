aac061fcbb436ab1927d60ef0c571f78f6a11577
beep boop <githubbot@vadim.me>
2024-07-09 18:58:32 +0200

    Solution 94 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/jnvpstjj Prompt: Program did not receive expected input

--- Python_25.py
+++ Python_25.py
@@ -1,17 +1,20 @@
 ```
 from typing import List
-import math
 
 
 def factorize(n: int) -> List[int]:
     factors = []
     i = n
-    while i > 1:
-        count = 0
-        while i % i == 0:
-            i //= i
-            count += 1
-        if count > 0:
+    while i * i <= n:
+        if n % i:
+            i -= 1
+        else:
+            count = 0
+            while n % i == 0:
+                n //= i
+                count += 1
             factors.append(i) * count
-        i = 2
+            i = n
+    if n > 1:
+        factors.append(n)
     return [int(factor) for factor in set(factors)]
