55e6624deda9c7d20cada6cc0a61470559cc1d7b
beep boop <githubbot@vadim.me>
2024-07-09 03:18:15 +0200

    Solution 39 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lqyq253s Prompt:       |                                     ^~~~~~ e09cc4e0-13e6-4a97-8f42-12b115dddbb6.cpp:33:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from e09cc4e0-13e6-4a97-8f42-12b115dddbb6.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from e09cc4e0-13e6-4a97-8f42-12b115dddbb6.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e09cc4e0-13e6-4a97-8f42-12b115dddbb6.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e09cc4e0-13e6-4a97-8f42-12b115dddbb6.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ e09cc4e0-13e6-4a97-8f42-12b115dddbb6.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <string>
 
@@ -6,8 +5,9 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if(a.size() != b.size()) {
         return false;
     }
+    vector<string> c = b;
     for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
+        if(a[i] != c[i]) {
             return false;
         }
     }
@@ -25,7 +25,6 @@ std::vector<std::string> filter_by_prefix(std::vector<std::string> strings, std:
 }
 
 int main() {
-    std::vector<std::string> b = filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAffAA", "xxx"}, "xxx");
-    assert(issame(b, {"xxx", "xxxAAA", "xxx"}));
+    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAffAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));
     return 0;
 }
