e44a0319c3f5041e34b4af2a33553462ca5833c6
beep boop <githubbot@vadim.me>
2024-07-10 00:40:22 +0200

    Solution 26 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fhsevhcx Prompt: 3d3b714b-9956-4596-9038-b13f5da2dcd2.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,26 +1,26 @@
 ```cpp
 #include <boost/any.hpp>
-#include <boost/lexical_cast.hpp>
+#include <boost/type_index.hpp>
 
 using namespace boost;
 
-boost::any compare_one(boost::any a, boost::any b) {
+bool is_string(any a) {
+    return get_type(a).type() == typeid(string);
+}
+
+string compare_one(any a, any b) {
     if (is_same<any_tag, get_type(a)>() && is_same<any_tag, get_type(b)>()) {
-        return a > b ? a : b;
-    } else if (is_same<string_any_tag, get_type(a)>() && is_same<string_any_tag, get_type(b)>()) {
+        int val1 = any_cast<int>(a);
+        int val2 = any_cast<int>(b);
+        return to_string(val1 > val2 ? val1 : val2);
+    } else if (is_string(a) && is_string(b)) {
         string str1 = any_cast<string>(a);
         string str2 = any_cast<string>(b);
+        return str1 > str2 ? str1 : str2;
+    } else if ((is_string(a) || is_string(b))) {
+        string str1 = is_string(a) ? any_cast<string>(a) : any_cast<string>(b);
+        string str2 = is_string(b) ? any_cast<string>(b) : any_cast<string>(a);
         return str1 > str2 ? a : b;
-    } else if (is_same<string_any_tag, get_type(a)>() || is_same<string_any_tag, get_type(b)>()) {
-        string str1 = any_cast<string>(a);
-        string str2 = any_cast<string>(b);
-        if (str1 == str2) {
-            return boost::any(lexical_cast<std::string>("None"));
-        } else if (str1 > str2) {
-            return a;
-        } else {
-            return b;
-        }
     } else {
         throw invalid_argument("Invalid types");
     }
