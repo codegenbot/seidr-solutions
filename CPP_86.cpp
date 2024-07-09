23718080edd68abfd9b4b07de87bc59d6907440b
beep boop <githubbot@vadim.me>
2024-07-09 18:39:29 +0200

    Solution 10 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/n9bn7yqf Prompt:     8 |     for (const auto& word : split(s, ' ')) {       |                             ^~~~~ 1c0f9f55-2224-4a81-9b77-802da92bf482.cpp: In function ‘std::string split(const s tring&, char)’: 1c0f9f55-2224-4a81-9b77-802da92bf482.cpp:21:18: error: passing ‘const string’ {a ka ‘const std::__cxx11::basic_string<char>’} as ‘this’ argument discards qualifi ers [-fpermissive]    21 |         str.erase(0, pos + 1);       |         ~~~~~~~~~^~~~~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from 1c0f9f55-2224-4a81-9b77-802da92bf482.cpp:2: /usr/include/c++/11/bits/basic_string.h:1827:7: note:   in call to ‘std::__cxx11 ::basic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Tra its, _Alloc>::erase(std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_ty pe, std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type) [with _CharT  = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>; std::_ _cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type = long unsigned int]’  1827 |       erase(size_type __pos = 0, size_type __n = npos)       |       ^~~~~ 1c0f9f55-2224-4a81-9b77-802da92bf482.cpp:24:12: error: ‘join’ was not declared i n this scope    24 |     return join(tokens, " ");       |            ^~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,21 +1,34 @@
+```cpp
 #include <algorithm>
 #include <string>
+#include <vector>
 using namespace std;
 
-int anti_shuffle(string s){
-    string result = "";
-    for(size_t i=0; i<s.size(); i++){
-        if(s[i] == ' '){
-            result += ' ';
-            continue;
-        }
-        string word = "";
-        for(size_t j=i; j<s.size() && s[j] != ' '; j++){
-            word += s[j];
-        }
-        i = j - 1;
-        sort(word.begin(), word.end());
-        result += word + " ";
+string join(const vector<string>& vec, const string& sep) {
+    string result;
+    for (const auto& s : vec) {
+        result += s + sep;
     }
-    return 0;
+    return result.substr(0, result.size() - 1);
+}
+
+string split(const string& str, char sep) {
+    vector<string> tokens;
+    size_t pos = 0;
+    while ((pos = str.find(sep)) != string::npos) {
+        tokens.push_back(str.substr(0, pos));
+        str.erase(0, pos + 1);
+    }
+    tokens.push_back(str);
+    return join(tokens, " ");
+}
+
+string anti_shuffle(string s) {
+    string result = "";
+    for (const auto& word : split(s, ' ')) {
+        string temp = word;
+        sort(temp.begin(), temp.end());
+        result += temp + " ";
+    }
+    return result.substr(0, result.size() - 1);
 }
