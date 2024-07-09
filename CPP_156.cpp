d80468557b12e74b75ac002c6fb1e781e190b2b2
beep boop <githubbot@vadim.me>
2024-07-09 03:07:39 +0200

    Solution 64 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/o8358e7c Prompt: e<std::pair<int, std::__cxx11::basic_string<char> > >’: /usr/include/c++/11/bits/stl_construct.h:188:51:   required from ‘void std::_Des troy(_ForwardIterator, _ForwardIterator) [with _ForwardIterator = std::pair<int,  std::__cxx11::basic_string<char> >*]’ /usr/include/c++/11/bits/alloc_traits.h:848:15:   required from ‘void std::_Dest roy(_ForwardIterator, _ForwardIterator, std::allocator<_T2>&) [with _ForwardIter ator = std::pair<int, std::__cxx11::basic_string<char> >*; _Tp = std::pair<int, std::__cxx11::basic_string<char> >]’ /usr/include/c++/11/bits/stl_vector.h:680:15:   required from ‘std::vector<_Tp, _Alloc>::~vector() [with _Tp = std::pair<int, std::__cxx11::basic_string<char> > ; _Alloc = std::allocator<std::pair<int, std::__cxx11::basic_string<char> > >]’ c96dd44f-b614-4bb2-929a-3bf7ca8e5a72.cpp:6:42:   required from here /usr/include/c++/11/type_traits:885:52: error: static assertion failed: template  argument must be a complete class or an unbounded array   885 |       static_assert(std::__is_complete_or_unbounded(__type_identity<_Tp> {}),       |                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~ ~~~ /usr/include/c++/11/type_traits:885:52: note: ‘std::__is_complete_or_unbounded<s td::__type_identity<std::pair<int, std::__cxx11::basic_string<char> > > >((std:: __type_identity<std::pair<int, std::__cxx11::basic_string<char> > >{}, std::__ty pe_identity<std::pair<int, std::__cxx11::basic_string<char> > >()))’ evaluates t o false

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,1 +1,37 @@
-std::vector<std::pair<int, std::string>> roman = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+```cpp
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct pair {
+    int first;
+    string second;
+};
+
+vector<pair> roman = {{1000, "M"}, {900, "CM"}, {500, "D"},
+                        {400, "CD"}, {100, "C"}, {90, "XC"},
+                        {50, "L"}, {40, "XL"}, {10, "X"},
+                        {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+
+string int_to_mini_roman(int number) {
+    string result = "";
+    for (const auto &romanNumber : roman) {
+        while (number >= romanNumber.first) {
+            number -= romanNumber.first;
+            result += romanNumber.second;
+        }
+    }
+    if(number > 0)
+        result.append(to_string(number)); 
+    return result;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Mini Roman representation of the number is: " << int_to_mini_roman(num) << endl;
+    return 0;
+}
