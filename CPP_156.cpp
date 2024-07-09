b4bd2f8aa06ec9d46caa8f07ab9fe05a2cb7542d
beep boop <githubbot@vadim.me>
2024-07-09 02:28:27 +0200

    Solution 28 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/noyz23n0 Prompt: 931fb3bf-9ad8-4b6f-a028-8f24822b8be1.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int number) {       | ^~~~~~ In file included from 931fb3bf-9ad8-4b6f-a028-8f24822b8be1.cpp:14: 931fb3bf-9ad8-4b6f-a028-8f24822b8be1.cpp: In function ‘int main()’: 931fb3bf-9ad8-4b6f-a028-8f24822b8be1.cpp:16:13: error: ‘int_to_mini_romank’ was not declared in this scope    16 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,9 +1,8 @@
-#include <vector>
 #include <string>
-using namespace std;
+#include <vector>
 
 std::string int_to_mini_roman(int number) {
-    vector< pair<int, string> > romans = {{1000,"M"},{900,"CM"},{500,"D"},{400,"CD"},{100,"C"},{90,"XC"},
+    std::vector< pair<int, std::string> > romans = {{1000,"M"},{900,"CM"},{500,"D"},{400,"CD"},{100,"C"},{90,"XC"},
                                          {50,"L"},{40,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}};
     std::string roman = "";
     for (auto p : romans) {
@@ -13,9 +12,4 @@ std::string int_to_mini_roman(int number) {
         }
     }
     return roman;
-}
-
-int main() {
-    assert(int_to_mini_roman(1000) == "M");
-    return 0;
 }
