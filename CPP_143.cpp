2ab65c0f45bf7c2a8f0cd4069a55bd63db00c679
beep boop <githubbot@vadim.me>
2024-07-09 11:01:55 +0200

    Solution 14 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4icgo1mz Prompt: Complete the following code given the task description and function signature.

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,32 +1,43 @@
-#include <cmath>
-using namespace std;
-
-string words_in_sentence(string sentence) {
+string words_in_sentence(string sentence){
+    vector<string> words = split(sentence, ' ');
     string result = "";
-    bool isPrime[100] = {false};
-    
-    for (int i = 2; i * i <= 100; i++) {
-        if (!isPrime[i]) {
-            for (int j = i; j * i < 100; j++) {
-                isPrime[j * i] = true;
+    for(int i = 0; i < words.size(); i++){
+        if(is_prime(word_length(words[i]))){
+            result += words[i] + " ";
+        }
+    }
+    return result;
+}
+
+int word_length(string s){
+    int len = 0;
+    for(char c : s){
+        len++;
+    }
+    return len;
+}
+
+bool is_prime(int n){
+    if(n <= 1) return false;
+    for(int i = 2; i*i <= n; i++){
+        if(n%i == 0) return false;
+    }
+    return true;
+}
+
+vector<string> split(string s, char c){
+    vector<string> result;
+    string temp = "";
+    for(char ch : s){
+        if(ch != c){
+            temp += ch;
+        }else{
+            if(!temp.empty()){
+                result.push_back(temp);
+                temp = "";
             }
         }
     }
-
-    int len = sentence.size();
-    int spaceIndex = 0;
-
-    while (spaceIndex < len) {
-        spaceIndex = sentence.find(' ', spaceIndex);
-        if (spaceIndex == -1)
-            break;
-        
-        string word = sentence.substr(0, spaceIndex);
-        if (!isPrime[word.size()]) 
-            continue;
-
-        result += word + " ";
-    }
-    
+    if(!temp.empty()) result.push_back(temp);
     return result;
 }
