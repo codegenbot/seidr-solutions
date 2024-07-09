5cbd0aa1fa5a8ef89847802ab32509b7c95be573
beep boop <githubbot@vadim.me>
2024-07-10 00:51:15 +0200

    Solution 47 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/9kcfsxlz Prompt: Program did not receive expected input

--- twitter.py
+++ twitter.py
@@ -1,29 +1,13 @@
-import re
-
-
 def validate_tweet(tweet):
-    if len(tweet) > 140:
+    if len(tweet.encode('ascii', 'ignore').decode()) > 140:
         return "Too many characters"
     elif not tweet.strip():
         return "You didn't type anything"
     else:
         return f"Your tweet has {len(tweet)} characters"
 
-
-print(validate_tweet(""))
-print(validate_tweet("1"))
-print(
-    validate_tweet(
-        "max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it"
-    )
-)
-print(
-    validate_tweet(
-        "*(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234"
-    )
-)
-print(
-    validate_tweet(
-        "Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1"
-    )
-)
+print(validate_tweet("")) 
+print(validate_tweet("1")) 
+print(validate_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it"))
+print(validate_tweet("*(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~```)"))
+print(validate_tweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1"))
