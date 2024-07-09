93138072f06e192ea7921843c420eb55bc6a0e96
beep boop <githubbot@vadim.me>
2024-07-09 04:12:09 +0200

    Solution 1 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/goc259cy Prompt: Complete the following code given the task description and function signature.

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -3,16 +3,42 @@
 using namespace std;
 
 vector<int> largest_smallest_integers(vector<int> lst) {
-    int maxNeg = 0;
-    int minPos = INT_MAX;
+    int maxNegative = 0;
+    int minPositive = INT_MAX;
 
     for (int num : lst) {
-        if (num < 0 && num > maxNeg) {
-            maxNeg = num;
-        } else if (num > 0 && num < minPos) {
-            minPos = num;
+        if (num < 0 && num > maxNegative) {
+            maxNegative = num;
+        } else if (num > 0 && num < minPositive) {
+            minPositive = num;
         }
     }
 
-    return {(maxNeg > 0) ? 0 : maxNeg, (minPos < 1) ? 0 : minPos};
+    return {(maxNegative >= 0) ? 0 : maxNegative, (minPositive <= 0) ? 0 : minPositive};
+}
+
+int main() {
+    vector<int> test1 = {2, 4, 1, 3, 5, 7};
+    vector<int> test2 = {};
+    vector<int> test3 = {0};
+
+    cout << "Test1: " << endl;
+    for (int num : largest_smallest_integers(test1)) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    cout << "Test2: " << endl;
+    for (int num : largest_smallest_integers(test2)) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    cout << "Test3: " << endl;
+    for (int num : largest_smallest_integers(test3)) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
