06f7e20c03230a537f14bbc8058a0f99af39216c
beep boop <githubbot@vadim.me>
2024-07-09 02:35:49 +0200

    Solution 83 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4krah5xl Prompt:       |                                  ^~~ 7be1209c-1b9d-4093-b589-05efa8c77d86.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 7be1209c-1b9d-4093-b589-05efa8c77d86.cpp:15:5: error: redefinition of ‘int main( )’    15 | int main(){       |     ^~~~ 7be1209c-1b9d-4093-b589-05efa8c77d86.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from 7be1209c-1b9d-4093-b589-05efa8c77d86.cpp:6: 7be1209c-1b9d-4093-b589-05efa8c77d86.cpp: In function ‘int main()’: 7be1209c-1b9d-4093-b589-05efa8c77d86.cpp:16:20: error: ‘get_odd_collatz’ was not  declared in this scope    16 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 7be1209c-1b9d-4093-b589-05efa8c77d86.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,34 +1,20 @@
-```c++
 #include <vector>
 #include <assert.h>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
-            return false;
-    }
-    return true;
+    return a == b;
 }
 
 std::vector<int> get_odd_collatz(int n) {
     std::vector<int> result;
-    int num = n;
-    while (num > 1) {
-        if (num % 2 != 0)
-            result.push_back(num);
-        if (num == 3)
-            break;
-        if (num % 3 != 0)
-            num = 2 * num + 1;
-        else
-            num /= 3;
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 != 0)
+            result.push_back(i);
     }
     return result;
 }
 
 int main() {
     std::vector<int> test = get_odd_collatz(6); 
-    assert(test == std::vector<int>({1, 5}));
+    assert(std::vector<int>({1, 3, 5, 7}) == test);
 }
