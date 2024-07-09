a769ca356852defda66e6ed6330c1fdf11b66198
beep boop <githubbot@vadim.me>
2024-07-09 09:20:49 +0200

    Solution 70 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5av973i7 Prompt:       |                                  ^~~ ebdcadf2-f227-4e01-8f9e-b4b681f71039.cpp:71:39: error: expression list treated a s compound expression in initializer [-fpermissive]    71 | bool issame(vector<int> a,vector<int>b){       |                                       ^ ebdcadf2-f227-4e01-8f9e-b4b681f71039.cpp:79:5: error: redefinition of ‘int main( )’    79 | int main(){       |     ^~~~ ebdcadf2-f227-4e01-8f9e-b4b681f71039.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~ In file included from ebdcadf2-f227-4e01-8f9e-b4b681f71039.cpp:70: ebdcadf2-f227-4e01-8f9e-b4b681f71039.cpp: In function ‘int main()’: ebdcadf2-f227-4e01-8f9e-b4b681f71039.cpp:80:20: error: ‘common’ was not declared  in this scope    80 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ ebdcadf2-f227-4e01-8f9e-b4b681f71039.cpp:80:19: error: ‘issame’ cannot be used a s a function    80 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <algorithm>
 
-bool isSame(const std::set<std::string>& s1, const std::set<std::string>& s2) {
+bool checkSets(const std::set<std::string>& s1, const std::set<std::string>& s2) {
     return s1 == s2;
 }
 
@@ -58,7 +58,7 @@ int main() {
     }
     std::cout << "\n";
 
-    if(isSame(s1, s2)) {
+    if(checkSets(s1, s2)) {
         std::cout << "Sets are same.\n";
     } else {
         std::cout << "Sets are not same.\n";
