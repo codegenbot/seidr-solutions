5d07b0863eb8b21e447399fa548efd95fa8c4edc
beep boop <githubbot@vadim.me>
2024-07-10 00:48:21 +0200

    Solution 22 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/szteghpq Prompt:       |                    ^~~ 0e6c681e-f5da-4248-aa47-030fb58a94a5.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0e6c681e-f5da-4248-aa47-030fb58a94a5.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0e6c681e-f5da-4248-aa47-030fb58a94a5.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0e6c681e-f5da-4248-aa47-030fb58a94a5.cpp:15: 0e6c681e-f5da-4248-aa47-030fb58a94a5.cpp: In function ‘int main()’: 0e6c681e-f5da-4248-aa47-030fb58a94a5.cpp:25:20: error: ‘compare’ was not declare d in this scope    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 0e6c681e-f5da-4248-aa47-030fb58a94a5.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
-int main() {
-    std::vector<int> game = {1,2,3,5};
-    std::vector<int> guess = {-1,2,3,4};
-    
-    std::vector<int> result = compare(game, guess);
-    for(int i: result) {
-        std::cout << i << " ";
-    }
-    return 0;
-}
 
 std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
     std::vector<int> result;
@@ -23,4 +11,8 @@ std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
         }
     }
     return result;
+}
+
+int main() {
+    assert(std::equal(compare({1,2,3,5},{-1,2,3,4}),{0,0,0,1}));
 }
