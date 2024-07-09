5cd90f4faf2768c087bf6901e16d2beed0bbe42a
beep boop <githubbot@vadim.me>
2024-07-09 06:55:02 +0200

    Solution 42 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/50q1gn5t Prompt: 1ff9f2b8-7d97-4a19-ad5a-e94de5d35e08.cpp:2:10: fatal error: boost/any.hpp: No su ch file or directory     2 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,13 +1,14 @@
-#include <boost/any.hpp>
 #include <string>
+#include <boost/any.hpp>
 
 using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return b;
+        return boost::any_cast<float>(b);
     } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        return a;
+        string s = boost::any_cast<string>(b);
+        return topper(s);
     } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
         return a;
     } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
@@ -33,5 +34,12 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
     } else {
         return "None";
+    }
+
+    string topper(string s) {
+        for (char &c : s) {
+            c = toupper(c);
+        }
+        return s;
     }
 }
