2c9d0cd0a2e7ccb54345a7feff95f7febae25ba7
beep boop <githubbot@vadim.me>
2024-07-09 08:35:22 +0200

    Solution 76 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jicx7wv6 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from c5270db0-ecde-4588-a54a-c66a9801932d.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
-#include <initializer_list>
 using namespace std;
 
 double poly(vector<double> coeffs, double solution) {
@@ -16,12 +15,14 @@ double poly(vector<double> coeffs, double solution) {
 double find_zero(vector<double> xs){
     double sum = 0;
     double coeff = xs[0];
-    vector<vector<double>> temp;
+    vector<vector<double>> temp(xs.begin() + 1, xs.end());
     for (int i = 1; i < xs.size(); i++) {
         if (i % 2 == 0) {
             sum += xs[i] / coeff;
-            vector<double> temp1(xs.begin() + i, xs.begin() + i+1);
-            temp.push_back(temp1);
+            for(int j = 0; j < temp[i-1].size(); j++) {
+                temp[i-1][j] /= coeff;
+            }
+            coeff = xs[i];
         }
         else{
             coeff = xs[i];
