8e2dc896eea951cc711004899061688ff9a4dc1e
beep boop <githubbot@vadim.me>
2024-07-09 04:20:19 +0200

    Solution 4 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/o6zsgg2s Prompt:       |                    ^~~ 40663f16-f8ac-43e1-b851-02839610f886.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 40663f16-f8ac-43e1-b851-02839610f886.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 40663f16-f8ac-43e1-b851-02839610f886.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 40663f16-f8ac-43e1-b851-02839610f886.cpp:15: 40663f16-f8ac-43e1-b851-02839610f886.cpp: In function ‘int main()’: 40663f16-f8ac-43e1-b851-02839610f886.cpp:25:20: error: ‘tri’ was not declared in  this scope    25 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ 40663f16-f8ac-43e1-b851-02839610f886.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,12 +1,8 @@
 #include <vector>
-#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-std::vector<int> completeCode(int n) {
-    std::vector<int> result;
+vector<int> tri(int n)
+{
+    vector<int> result;
     if (n >= 0) {
         result.push_back(3);
         for (int i = 1; i <= n; ++i) {
@@ -20,7 +16,24 @@ std::vector<int> completeCode(int n) {
     return result;
 }
 
+bool issame(vector<int>& a, vector<int>& b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
-    assert(issame(completeCode(1), {1, 3}));
+    vector<int> a = tri(1);
+    if (issame(a, {1, 3})) {
+        cout << "Vectors are the same" << endl;
+    } else {
+        cout << "Vectors are different" << endl;
+    }
     return 0;
 }
