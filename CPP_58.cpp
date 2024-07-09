67ffb161681197ab3d961f928d18f5d97c1c0c54
beep boop <githubbot@vadim.me>
2024-07-09 21:54:11 +0200

    Solution 84 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g34sr5ik Prompt:       |     ^~~~ 64f4602a-f788-42fc-b2d6-d90172f08abe.cpp:3:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     2 | #include <cassert>   +++ |+#include <iostream>     3 | 64f4602a-f788-42fc-b2d6-d90172f08abe.cpp: At global scope: 64f4602a-f788-42fc-b2d6-d90172f08abe.cpp:34:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    34 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 64f4602a-f788-42fc-b2d6-d90172f08abe.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a,vector<int> b){       |      ^~~~~~ 64f4602a-f788-42fc-b2d6-d90172f08abe.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 64f4602a-f788-42fc-b2d6-d90172f08abe.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,17 +1,53 @@
 #include <vector>
 #include <assert>
+#include <iostream>
 #include <set>
-#include <algorithm>
 
 using namespace std;
 
-vector<int> common(vector<int> l1, vector<int> b2) {
+bool issame(vector<int> a,vector<int> b){
+    return a == b;
+}
+
+vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
-    set<int> s2(b2.begin(), b2.end());
+    set<int> s2(l2.begin(), l2.end());
     set<int> intersection;
     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
         inserter(intersection, intersection.begin()));
     vector<int> result(intersection.begin(), intersection.end());
-    sort(result.begin(), result.end()); 
+    sort(result.begin(), result.end());
     return result;
+}
+
+int main() {
+    vector<int> l1;
+    int n;
+    cout << "Enter the number of elements in list 1: ";
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        int x;
+        cout << "Enter element " << (i+1) << ": ";
+        cin >> x;
+        l1.push_back(x);
+    }
+    
+    vector<int> l2;
+    cout << "Enter the number of elements in list 2: ";
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        int x;
+        cout << "Enter element " << (i+1) << ": ";
+        cin >> x;
+        l2.push_back(x);
+    }
+    
+    vector<int> commonElements = common(l1, l2);
+    cout << "Common elements are: ";
+    for(int i : commonElements) {
+        cout << i << " ";
+    }
+    return 0;
 }
