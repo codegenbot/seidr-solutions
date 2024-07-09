29aff394a29693bc3a194dc505c3e13983ee3d7e
beep boop <githubbot@vadim.me>
2024-07-09 21:52:52 +0200

    Solution 65 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/oje19k94 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from ad24d389-e9f0-4653-871a-5d77e205699d.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -15,10 +15,19 @@ bool issame(vector<string> a,vector<string> b) {
 }
 
 int main() {
-    assert(issame({"this"}, {}));
+    assert(issame({"this"}, {})); 
     return 0;
 }
 
-vector<string> total_match(vector<string>, initializer_list<string>) {
-    // Your code here
+vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+    int sum1 = 0, sum2 = 0;
+    for (const string& s : lst1) sum1 += s.size();
+    for (const string& s : lst2) sum2 += s.size();
+
+    if (sum1 < sum2)
+        return lst1;
+    else if (sum1 > sum2)
+        return lst2;
+    else
+        return issame(lst1, lst2) ? lst1 : lst2;
 }
