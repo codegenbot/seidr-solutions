f3b4419a36a7fb8248464b0613f5e4166a08cbbe
beep boop <githubbot@vadim.me>
2024-07-10 01:22:30 +0200

    Solution 4 of CPP/0, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/wpubjypd Prompt: f82c4746-7023-4805-b281-8a1c89cfcb3a.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ f82c4746-7023-4805-b281-8a1c89cfcb3a.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -12,9 +12,3 @@ bool has_close_elements(vector<double> numbers, double threshold) {
     }
     return false;
 }
-
-int main() {
-    vector<double> a = {1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
-    assert(has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false);
-    return 0;
-}
