b88687b49aba3940c441a82e7936a88ac9a8b567
beep boop <githubbot@vadim.me>
2024-07-09 23:52:36 +0200

    Solution 13 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/2ghk70sf Prompt: Complete the following code given the task description and function signature.

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,13 +1,16 @@
+bool hasOdd(int x) {
+    return (x & 1);
+}
+
 string exchange(vector<int> lst1, vector<int> lst2) {
     int oddCount = 0;
-    
-    for (int num : lst1) {
-        if (num % 2 != 0) {
+    for (int x : lst1) {
+        if (hasOdd(x)) {
             oddCount++;
         }
     }
-    
-    if (oddCount > (lst2.size() - oddCount)) {
+
+    if (oddCount > (lst2.size() / 2)) {
         return "NO";
     } else {
         return "YES";
