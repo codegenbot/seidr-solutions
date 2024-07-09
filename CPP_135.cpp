e500e93c854278e37946743c56a8666db02d7063
beep boop <githubbot@vadim.me>
2024-07-09 13:42:53 +0200

    Solution 50 of CPP/135, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/bvib8907 Prompt: 3733b634-858c-464b-b61e-51308f68407c.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 3733b634-858c-464b-b61e-51308f68407c.cpp:11: 3733b634-858c-464b-b61e-51308f68407c.cpp: In function ‘int main()’: 3733b634-858c-464b-b61e-51308f68407c.cpp:13:13: error: ‘can_arrange’ was not dec lared in this scope    13 |     assert (can_arrange({})==-1);       |             ^~~~~~~~~~~

--- CPP_135.cpp
+++ CPP_135.cpp
@@ -1,14 +1,7 @@
-#include<vector>
-
 int can_arrange(vector<int> arr){
     for(int i = 1; i < arr.size(); i++){
         if(arr[i] <= arr[i-1])
             return i - 1;
     }
     return -1;
-}
-
-int main(){
-   assert (can_arrange({})==-1);
-   return 0;
 }
