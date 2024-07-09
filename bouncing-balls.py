4289b8f8463d582a6ff743407f56f667ab4d0dad
beep boop <githubbot@vadim.me>
2024-07-09 17:15:04 +0200

    Solution 31 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/51crb25a Prompt: it must return 49.02666621212136 for input 35.416812375335844 5.783412047121341 3, but it returns .

--- bouncing-balls.py
+++ bouncing-balls.py
@@ -9,6 +9,6 @@ def bouncing_balls():
 
     for i in range(1, num_bounces + 1):
         start_height *= bounciness_index
-        total_distance += 2 * start_height
-    
+        total_distance += 4 * start_height
+
     print(total_distance)
