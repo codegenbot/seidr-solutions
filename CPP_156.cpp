6f00cc961550547fa0b4d0b4c619bcfdf8b2e381
beep boop <githubbot@vadim.me>
2024-07-09 02:45:11 +0200

    Solution 35 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/o8358e7c Prompt: D"},       |     ^~~~~~ 432e1396-7bdc-4438-97c5-bfcfb9900788.cpp:2:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?     1 | #include <string>   +++ |+#include <vector>     2 | using namespace std; 432e1396-7bdc-4438-97c5-bfcfb9900788.cpp:5:28: error: expected primary-expressio n before ‘>’ token     5 |     vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {500, " D"},       |                            ^~ 432e1396-7bdc-4438-97c5-bfcfb9900788.cpp:5:31: error: ‘roman’ was not declared i n this scope     5 |     vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {500, " D"},       |                               ^~~~~ In file included from 432e1396-7bdc-4438-97c5-bfcfb9900788.cpp:21: 432e1396-7bdc-4438-97c5-bfcfb9900788.cpp: In function ‘int main()’: 432e1396-7bdc-4438-97c5-bfcfb9900788.cpp:23:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    23 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,14 +1,12 @@
-#include <string>
 #include <vector>
-
 using namespace std;
 
-string int_to_mini_roman(int number) {
+std::string int_to_mini_roman(int number) {
     vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {500, "D"},
                                          {400, "CD"}, {100, "C"}, {90, "XC"},
                                          {50, "L"}, {40, "XL"}, {10, "X"},
                                          {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
-    string result = "";
+    std::string result = "";
     for (const auto &romanNumber : roman) {
         while (number >= romanNumber.first) {
             number -= romanNumber.first;
@@ -18,12 +16,4 @@ string int_to_mini_roman(int number) {
     if(number > 0)
         result.append(to_string(number)); 
     return result;
-}
-
-int main() {
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    cout << "Mini Roman representation of the number is: " << int_to_mini_roman(num) << endl;
-    return 0;
 }
