d3a91f3cd66376a0a3534135f786bf2561378a70
beep boop <githubbot@vadim.me>
2024-07-09 21:25:16 +0200

    Solution 91 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/h6h6rl4f Prompt: In file included from /usr/include/c++/11/iostream:39,                  from 6160c53f-ee01-4bf7-a417-f913afb8c45e.cpp:1: /usr/include/c++/11/ostream:750:5: note: candidate: ‘template<class _Ostream, cl ass _Tp> _Ostream&& std::operator<<(_Ostream&&, const _Tp&)’   750 |     operator<<(_Ostream&& __os, const _Tp& __x)       |     ^~~~~~~~ /usr/include/c++/11/ostream:750:5: note:   template argument deduction/substitut ion failed: /usr/include/c++/11/ostream: In substitution of ‘template<class _Ostream, class _Tp> _Ostream&& std::operator<<(_Ostream&&, const _Tp&) [with _Ostream = std::ba sic_ostream<char>&; _Tp = std::vector<std::__cxx11::basic_string<char> >]’: 6160c53f-ee01-4bf7-a417-f913afb8c45e.cpp:29:52:   required from here /usr/include/c++/11/ostream:750:5: error: no type named ‘type’ in ‘struct std::e nable_if<false, void>’ 6160c53f-ee01-4bf7-a417-f913afb8c45e.cpp: At global scope: 6160c53f-ee01-4bf7-a417-f913afb8c45e.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ 6160c53f-ee01-4bf7-a417-f913afb8c45e.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,35 +1,6 @@
 ```cpp
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-vector<string> sorted_list_sum(vector<string> lst) {
-    vector<string> evenLengthStrings;
-    for (const string& str : lst) {
-        if (str.length() % 2 == 0) {
-            evenLengthStrings.push_back(str);
-        }
-    }
-
-    sort(evenLengthStrings.begin(), evenLengthStrings.end(),
-         [](const string& a, const string& b) {
-             if (a.length() != b.length()) {
-                 return a.length() < b.length();
-             } else {
-                 return a < b;
-             }
-         });
-
-    return evenLengthStrings;
+cout << "The lists are the same: actual - ";
+for(const string& str : result) {
+    cout << str << ", ";
 }
-
-int main() {
-    vector<string> input = {"aaaa", "bbbb", "dd", "cc"};
-    vector<string> result = sorted_list_sum(input);
-    cout << "The lists are the same: actual - ";
-    for(const string& str : result) {
-        cout << str << ", ";
-    }
-    cout << "\nexpected - { cc, dd, aaaa, bbbbb }\n";
-}
+cout << "\nexpected - {" << "cc" << ", " << "dd" << ", " << "aaaa" << ", " << "bbbb" << "}\n";
