4cb9cfce57f435db3d4e02f19e996ac0dc7e304a
beep boop <githubbot@vadim.me>
2024-07-09 13:23:40 +0200

    Solution 4 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ujx0k24n Prompt: 2797c6b9-ae29-4dc3-b3ac-856ec7b6e56b.cpp:35:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    35 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 2797c6b9-ae29-4dc3-b3ac-856ec7b6e56b.cpp:38:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    38 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 2797c6b9-ae29-4dc3-b3ac-856ec7b6e56b.cpp:38:36: error: ‘find_zero’ cannot be use d as a function    38 |         solution = find_zero(coeffs);       |                                    ^ In file included from 2797c6b9-ae29-4dc3-b3ac-856ec7b6e56b.cpp:22: 2797c6b9-ae29-4dc3-b3ac-856ec7b6e56b.cpp:39:25: error: ‘poly’ cannot be used as a function    39 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ~~~~^~~~~~~~~~~~~~~~~~ 2797c6b9-ae29-4dc3-b3ac-856ec7b6e56b.cpp:39:17: error: ‘abs’ was not declared in  this scope    39 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,22 +1,20 @@
-```cpp
-double poly(vector<double> coeffs, double x) {
+double poly(vector<double> a, double x) {
     double sum = 0;
-    for (int i = 0; i < coeffs.size(); i++) {
-        sum += coeffs[i] * pow(x, i);
+    for (int i = 0; i < a.size(); i++) {
+        sum += a[i] * pow(x, i);
     }
     return sum;
 }
 
 double find_zero(vector<double> coeffs){
-    vector<double> solution_coeffs(coeffs); 
     double root = 1;
     double precision = 0.00001;
 
     while (true) {
-        double poly_val = poly(solution_coeffs, root);
+        double poly_val = poly(coeffs, root);
         if (abs(poly_val) < precision)
             break;
-        root -= poly_val / poly(solution_coeffs, root);
+        root -= poly_val / poly(coeffs, root);
     }
     return root;
 }
