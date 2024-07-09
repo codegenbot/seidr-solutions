dd99b4e901ebb7f7b303fc366b431631ff4b2f7e
beep boop <githubbot@vadim.me>
2024-07-09 12:37:28 +0200

    Solution 74 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/tkok7ny4 Prompt: 6c1444e5-5656-44bb-b743-cd013b4e3b5b.cpp: In function ‘std::vector<std::pair<int , int> > pluck(std::vector<int>)’: 6c1444e5-5656-44bb-b743-cd013b4e3b5b.cpp:8:19: error: ‘INT_MAX’ was not declared  in this scope     8 |     int minEven = INT_MAX;       |                   ^~~~~~~ 6c1444e5-5656-44bb-b743-cd013b4e3b5b.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; In file included from 6c1444e5-5656-44bb-b743-cd013b4e3b5b.cpp:23: 6c1444e5-5656-44bb-b743-cd013b4e3b5b.cpp: In function ‘int main()’: 6c1444e5-5656-44bb-b743-cd013b4e3b5b.cpp:33:25: error: could not convert ‘pluck( std::vector<int>)()’ from ‘vector<std::pair<int, int>>’ to ‘vector<int>’    33 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ~~~~~^~~~~~~~~~~~~~       |                         |       |                         vector<std::pair<int, int>>

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,5 +1,4 @@
-```#include <climits>
-
+#include <climits>
 using namespace std;
 
 vector<pair<int, int>> pluck(vector<int> arr) {
@@ -19,4 +18,4 @@ vector<pair<int, int>> pluck(vector<int> arr) {
     result.push_back({minEven, minIndex});
     
     return result;
-}```
+}
