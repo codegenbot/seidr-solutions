2953614b3592559aceb4dacec86a8fda350a541b
beep boop <githubbot@vadim.me>
2024-07-09 06:40:14 +0200

    Solution 16 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/50q1gn5t Prompt: 1ff9f2b8-7d97-4a19-ad5a-e94de5d35e08.cpp:2:10: fatal error: boost/any.hpp: No su ch file or directory     2 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,16 +1,37 @@
-using namespace boost;
+#include <string>
+#include <boost/any.hpp>
+
+using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_same<any_type, float>(a.type()) && is_same<any_type, float>(b.type())) {
-        return a > b ? a : b;
-    } else if (is_same<any_type, double>(a.type()) && is_same<any_type, double>(b.type())) {
-        return a > b ? a : b;
-    } else if (is_same<any_type, string>(a.type()) && is_same<any_type, string>(b.type())) {
-        string str_a = boost::any_cast<string>(a);
-        string str_b = boost::any_cast<string>(b);
-        return str_a > str_b ? a : b;
-    } else if (is_same<any_type, int>(a.type()) && is_same<any_type, int>(b.type())) {
-        return a > b ? a : b;
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        return a;
+    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        return a;
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string x = boost::any_cast<string>(a);
+        string y = boost::any_cast<string>(b);
+        if (x >= y)
+            return a;
+        else
+            return b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        int x = boost::any_cast<int>(a);
+        int y = boost::any_cast<int>(b);
+        if (x >= y)
+            return a;
+        else
+            return b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        float x = boost::any_cast<float>(a);
+        float y = boost::any_cast<float>(b);
+        if (x >= y)
+            return a;
+        else
+            return b;
+    } else {
+        return boost::any("None");
     }
-    return any("None");
 }
