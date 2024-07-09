b14919c4f784dd765fe604d14bf9adc0d3299e3b
beep boop <githubbot@vadim.me>
2024-07-09 09:47:18 +0200

    Solution 4 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/c8pse80p Prompt:    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f1f23899-8acb-494c-b8b6-f7bfb1b0c2b9.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f1f23899-8acb-494c-b8b6-f7bfb1b0c2b9.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f1f23899-8acb-494c-b8b6-f7bfb1b0c2b9.cpp:21: f1f23899-8acb-494c-b8b6-f7bfb1b0c2b9.cpp: In function ‘int main()’: f1f23899-8acb-494c-b8b6-f7bfb1b0c2b9.cpp:31:20: error: ‘count_up_to’ was not dec lared in this scope    31 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ f1f23899-8acb-494c-b8b6-f7bfb1b0c2b9.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,4 +1,5 @@
-vector<int> result;
+vector<int> count_up_to(int n) {
+    vector<int> result;
     if (n <= 1) {
         return result;
     }
@@ -6,17 +7,12 @@ vector<int> result;
     vector<bool> isPrime(n, true);
     isPrime[0] = isPrime[1] = false;
     
-    for (int i = 2; i * i < n; ++i) {
-        if (isPrime[i]) {
-            for (int j = i * i; j < n; j += i) {
-                isPrime[j] = false;
-            }
-        }
-    }
-    
     for (int i = 2; i < n; ++i) {
         if (isPrime[i]) {
             result.push_back(i);
+            for (int j = i * 2; j < n; j += i) {
+                isPrime[j] = false;
+            }
         }
     }
     
