1d21ac38a25e52f6e4f6ccc4cceec63d21f2551a
beep boop <githubbot@vadim.me>
2024-07-09 13:41:48 +0200

    Solution 8 of CPP/31, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/1dwzrait Prompt: d1c81553-5a72-4a1a-b73c-8b33257fbe06.cpp: In function ‘int main()’: d1c81553-5a72-4a1a-b73c-8b33257fbe06.cpp:3:5: error: ‘assert’ was not declared i n this scope     3 |     assert(is_prime(13441 * 19) == false);       |     ^~~~~~ d1c81553-5a72-4a1a-b73c-8b33257fbe06.cpp:1:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?   +++ |+#include <cassert>     1 | bool is_prime(int); d1c81553-5a72-4a1a-b73c-8b33257fbe06.cpp: At global scope: d1c81553-5a72-4a1a-b73c-8b33257fbe06.cpp:7:5: error: redefinition of ‘int main() ’     7 | int main(){       |     ^~~~ d1c81553-5a72-4a1a-b73c-8b33257fbe06.cpp:2:5: note: ‘int main()’ previously defi ned here     2 | int main() {       |     ^~~~

--- CPP_31.cpp
+++ CPP_31.cpp
@@ -1,13 +1,11 @@
 #include <cassert>
-#include <cmath>
 
 bool is_prime(int n) {
     if (n <= 1)
         return false;
-    for (int i = 2; i <= sqrt(n); i++) {
+    for (int i = 2; i * i <= n; i++)
         if (n % i == 0)
             return false;
-    }
     return true;
 }
 
