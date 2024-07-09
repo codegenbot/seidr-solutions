38237a675c27452260271cd2d267a97881b54cf0
beep boop <githubbot@vadim.me>
2024-07-09 02:20:47 +0200

    Solution 18 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/noyz23n0 Prompt: ed496a96-164a-45a3-b10b-4857f8e27797.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_roman(int number) {       | ^~~~~~ In file included from ed496a96-164a-45a3-b10b-4857f8e27797.cpp:14: ed496a96-164a-45a3-b10b-4857f8e27797.cpp: In function ‘int main()’: ed496a96-164a-45a3-b10b-4857f8e27797.cpp:16:13: error: ‘int_to_mini_romank’ was not declared in this scope    16 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,11 +1,7 @@
-```cpp
-#include <vector>
-#include <string>
-
-std::string int_to_mini_romank(int number) {
-    std::vector<std::pair<int, char>> romans = {{1000,'M'},{900,'C'},{500,'D'},{400,'C'},{100,'C'},{90,'X'},
-                                         {50,'L'},{40,'X'},{10,'X'},{9,'I'},{5,'V'},{4,'I'},{1,'I'}};
-    std::string roman = "";
+string int_to_mini_roman(int number) {
+    vector< pair<int, string> > romans = {{1000,"M"},{900,"CM"},{500,"D"},{400,"CD"},{100,"C"},{90,"XC"},
+                                         {50,"L"},{40,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}};
+    string roman = "";
     for (auto p : romans) {
         while (number >= p.first) {
             number -= p.first;
@@ -13,8 +9,4 @@ std::string int_to_mini_romank(int number) {
         }
     }
     return roman;
-}
-
-int main() {
-    assert (int_to_mini_romank(1000) == "M");
 }
