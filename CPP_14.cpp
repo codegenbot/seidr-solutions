b5f7441eb3c07e8b6aa937f5b32db7e190cd6028
beep boop <githubbot@vadim.me>
2024-07-09 06:57:54 +0200

    Solution 16 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/g3dq5xin Prompt:       |                              ^~~~~~ 605dc977-6406-46e3-883c-12d6f356249e.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 605dc977-6406-46e3-883c-12d6f356249e.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 605dc977-6406-46e3-883c-12d6f356249e.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 605dc977-6406-46e3-883c-12d6f356249e.cpp:9: 605dc977-6406-46e3-883c-12d6f356249e.cpp: In function ‘int main()’: 605dc977-6406-46e3-883c-12d6f356249e.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ 605dc977-6406-46e3-883c-12d6f356249e.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -3,22 +3,19 @@
 #include <string>
 #include <cassert>
 
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b){
-    return a == b;
-}
-
-vector<string> all_prefixes(string str){
-    vector<string> result;
+std::vector<std::string> all_prefixes(std::string str) {
+    std::vector<std::string> result;
     for (int i = 1; i <= str.size(); ++i) {
         result.push_back(str.substr(0, i));
     }
     return result;
 }
 
-int main(){
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    return a == b;
+}
+
+int main() {
     assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
-    cout << "Test passed!" << endl;
     return 0;
 }
