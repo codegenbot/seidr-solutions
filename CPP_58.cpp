b64955bd0f21f2000888e6a5fca65576475cac00
beep boop <githubbot@vadim.me>
2024-07-09 08:43:23 +0200

    Solution 40 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5av973i7 Prompt: /usr/include/c++/11/bits/stl_set.h:512:25:   required from ‘std::pair<typename s td::_Rb_tree<_Key, _Key, std::_Identity<_Tp>, _Compare, typename __gnu_cxx::__al loc_traits<_Alloc>::rebind<_Key>::other>::const_iterator, bool> std::set<_Key, _ Compare, _Alloc>::insert(const value_type&) [with _Key = std::__cxx11::basic_str ing<char>; _Compare = std::less<std::__cxx11::basic_string<char> >; _Alloc = std ::allocator<std::__cxx11::basic_string<char> >; typename std::_Rb_tree<_Key, _Ke y, std::_Identity<_Tp>, _Compare, typename __gnu_cxx::__alloc_traits<_Alloc>::re bind<_Key>::other>::const_iterator = std::_Rb_tree<std::__cxx11::basic_string<ch ar>, std::__cxx11::basic_string<char>, std::_Identity<std::__cxx11::basic_string <char> >, std::less<std::__cxx11::basic_string<char> >, std::allocator<std::__cx x11::basic_string<char> > >::const_iterator; typename __gnu_cxx::__alloc_traits< _Alloc>::rebind<_Key>::other = std::allocator<std::__cxx11::basic_string<char> > ; typename __gnu_cxx::__alloc_traits<_Alloc>::rebind<_Key> = __gnu_cxx::__alloc_ traits<std::allocator<std::__cxx11::basic_string<char> >, std::__cxx11::basic_st ring<char> >::rebind<std::__cxx11::basic_string<char> >; typename _Alloc::value_ type = std::__cxx11::basic_string<char>; std::set<_Key, _Compare, _Alloc>::value _type = std::__cxx11::basic_string<char>]’ 31044dec-10b9-40de-b9f6-9bbe7f07304d.cpp:22:21:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::_Rb_tree_node<std::__cxx11::basic_string<char> > >’ has no member na med ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -11,29 +11,19 @@ int main() {
     std::cin >> n1 >> m1 >> n2 >> m2;
 
     std::vector<std::string> s1Vec;
-    s1Vec.resize(n1);
+    s1Vec.reserve(n1);
     for(int i = 0; i < n1; i++) {
         std::string x;
         std::cin >> x;
-        s1Vec[i] = x; 
-    }
-
-    for(auto &i : s1Vec) {
-        if(s1.insert(i).second)  
-            continue; 
+        s1.insert(x);
     }
 
     std::vector<std::string> s2Vec;
-    s2Vec.resize(m1);
+    s2Vec.reserve(m1);
     for(int i = 0; i < m1; i++) {
         std::string y;
         std::cin >> y;
-        s2Vec[i] = y; 
-    }
-
-    for(auto &i : s2Vec) {
-        if(s2.insert(i).second)  
-            continue; 
+        s2.insert(y);
     }
 
     std::vector<std::string> diff(s1.begin(), s1.end());
@@ -43,7 +33,7 @@ int main() {
     auto intersection = s1;
     s1.clear();
     std::vector<std::string> intersect;
-    std::set_intersection(intersection.begin(), intersection.end(), s2.begin(), s2.end(),
+    std::set_intersection(s2.begin(), s2.end(), s1.begin(), s1.end(),
                            std::back_inserter(intersect));
 
     std::cout << "Difference: ";
