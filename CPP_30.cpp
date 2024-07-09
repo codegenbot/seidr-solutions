3da50cb8c68fd3d24f07c8f7843430d5080b6bea
beep boop <githubbot@vadim.me>
2024-07-09 03:45:01 +0200

    Solution 64 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jvy931jd Prompt:       |           ^~~~~~ In file included from c291526e-d889-4f5b-9b9e-d9b5c3e90bc9.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c291526e-d889-4f5b-9b9e-d9b5c3e90bc9.cpp:15:36: error: expected primary-expressi on before ‘float’    15 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ c291526e-d889-4f5b-9b9e-d9b5c3e90bc9.cpp:15:43: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from c291526e-d889-4f5b-9b9e-d9b5c3e90bc9.cpp:14: c291526e-d889-4f5b-9b9e-d9b5c3e90bc9.cpp: In function ‘int main()’: c291526e-d889-4f5b-9b9e-d9b5c3e90bc9.cpp:24:20: error: ‘get_positive’ was not de clared in this scope    24 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ c291526e-d889-4f5b-9b9e-d9b5c3e90bc9.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,16 +1,11 @@
 #include <vector>
-#include <float.h>
+#include <memory>
 
 bool issame(vector<float> a, vector<float> b) {
-    if(a.size() != b.size()) 
-        return false;
-    for(int i = 0; i<a.size(); i++) 
-        if(fabs(a[i] - b[i]) > FLT_EPSILON) 
-            return false;
-    return true;
+    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
 }
 
-vector<float> get_positive(vector<float> l){
+vector<float> get_positive(const vector<float>& l) {
     vector<float> result;
     for(float num : l) {
         if(num > 0) {
