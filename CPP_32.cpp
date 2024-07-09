81895c99a18714e67f04683d15f46c5cf72dbf87
beep boop <githubbot@vadim.me>
2024-07-09 03:31:58 +0200

    Solution 51 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h5y6hrvk Prompt: 9d29ec62-83e9-43eb-a906-0228a505e58d.cpp:37:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    37 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 9d29ec62-83e9-43eb-a906-0228a505e58d.cpp:40:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    40 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 9d29ec62-83e9-43eb-a906-0228a505e58d.cpp:40:20: error: ‘find_zero’ was not decla red in this scope    40 |         solution = find_zero(coeffs);       |                    ^~~~~~~~~ In file included from 9d29ec62-83e9-43eb-a906-0228a505e58d.cpp:24: 9d29ec62-83e9-43eb-a906-0228a505e58d.cpp:41:21: error: ‘poly’ was not declared i n this scope    41 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 9d29ec62-83e9-43eb-a906-0228a505e58d.cpp:41:17: error: ‘abs’ was not declared in  this scope    41 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,12 +1,21 @@
 double find_zero(vector<double> xs){
-    vector<double> coeff;
-    for (int i = 0; i < xs.size(); i += 2) {
+    vector<double> poly;
+    double coeff = 0;
+    for (int i = 1; i < xs.size(); i += 2) {
         double x = xs[i];
         int power = (i - 1) / 2;
-        if (coeff.size() <= power) {
-            coeff.resize(power + 1);
+        if (poly.size() <= power) {
+            poly.resize(power + 1);
         }
-        coeff[power] -= x;
+        poly[power].push_back(x);
     }
-    return -coeff[0] / xs[0];
+    for (int i = 0; i < poly.size(); i++) {
+        coeff = 0;
+        for (int j = 0; j <= i; j++) {
+            if (j < poly[i].size()) {
+                coeff -= poly[i][j];
+            }
+        }
+    }
+    return -coeff / xs[0];
 }
