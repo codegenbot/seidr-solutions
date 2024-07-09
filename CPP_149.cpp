ccb8e544f7a93c64d2432abe23315b9776f7a77c
beep boop <githubbot@vadim.me>
2024-07-09 04:32:04 +0200

    Solution 16 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/3gtchhci Prompt:    23 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 3049bc68-1e0a-49cf-912c-289dae5fe182.cpp:23:44: error: ‘b’ was not declared in t his scope    23 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3049bc68-1e0a-49cf-912c-289dae5fe182.cpp:23:45: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3049bc68-1e0a-49cf-912c-289dae5fe182.cpp:22: 3049bc68-1e0a-49cf-912c-289dae5fe182.cpp: In function ‘int main()’: 3049bc68-1e0a-49cf-912c-289dae5fe182.cpp:32:20: error: ‘sorted_list_sum’ was not  declared in this scope    32 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 3049bc68-1e0a-49cf-912c-289dae5fe182.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -2,14 +2,38 @@ bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i].size() != b[i].size()) {
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
     return true;
 }
 
-int main() {
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+vector<string> sorted_list_sum(vector<string> lst) {
+    vector<string> evens, odds;
+
+    for (const auto& str : lst) {
+        if (str.length() % 2 == 0) {
+            evens.push_back(str);
+        } else {
+            odds.push_back(str);
+        }
+    }
+
+    sort(evens.begin(), evens.end());
+    sort(odds.begin(), odds.end());
+
+    vector<string> result;
+    for (const auto& str : lst) {
+        if (str.length() % 2 == 0) {
+            result.push_back(evens.front());
+            evens.pop_front();
+        } else {
+            result.push_back(odds.front());
+            odds.pop_front();
+        }
+    }
+
+    return result;
 }
