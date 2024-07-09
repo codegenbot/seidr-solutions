6df3f5364b6c9d365e84a65cc1496135697357a6
beep boop <githubbot@vadim.me>
2024-07-09 04:41:22 +0200

    Solution 0 of CPP/158, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/2pmfkefm Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_158.cpp
@@ -0,0 +1,16 @@
+string find_max(vector<string> words){
+    string max_word = *words.begin();
+    int max_unique_chars = 0;
+    
+    for(auto word : words){
+        int unique_chars = 0;
+        set<char> char_set(word.begin(), word.end());
+        
+        if(char_set.size() > max_unique_chars){
+            max_unique_chars = char_set.size();
+            max_word = word;
+        }
+    }
+    
+    return max_word;
+}
