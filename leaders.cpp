fd2ecdf94f42bbeb3e716e425c9b30141c9da626
beep boop <githubbot@vadim.me>
2024-07-09 08:10:01 +0200

    Solution 72 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/rxikrfz5 Prompt: f2afba4f-2c8b-4f4d-aa86-ca77bb0d9064.cpp: In function ‘int main()’: f2afba4f-2c8b-4f4d-aa86-ca77bb0d9064.cpp:28:9: error: ‘cout’ was not declared in  this scope    28 |         cout << i << endl;       |         ^~~~ f2afba4f-2c8b-4f4d-aa86-ca77bb0d9064.cpp:3:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     2 | #include <algorithm>   +++ |+#include <iostream>     3 | using namespace std; f2afba4f-2c8b-4f4d-aa86-ca77bb0d9064.cpp:28:22: error: ‘endl’ was not declared i n this scope    28 |         cout << i << endl;       |                      ^~~~ f2afba4f-2c8b-4f4d-aa86-ca77bb0d9064.cpp:3:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     2 | #include <algorithm>   +++ |+#include <ostream>     3 | using namespace std;

--- leaders.cpp
+++ leaders.cpp
@@ -1,33 +1,19 @@
-```cpp
 #include <vector>
 #include <algorithm>
+#include <iostream>
+
 using namespace std;
 
 vector<int> leaders(vector<int>& arr) {
     int n = arr.size();
     vector<int> result;
-    int max_right = arr[n-1];
+    int maxright = arr[n - 1];
     for (int i = n - 1; i >= 0; i--) {
-        if (arr[i] >= max_right) {
-            max_right = arr[i];
-            result.push_back(max_right);
+        if (arr[i] >= maxright) {
+            result.push_back(arr[i]);
+            maxright = arr[i];
         }
     }
-    reverse(result.begin(), result.end());
-    return result;
-}
-
-vector<int> leaders(vector<int>& arr) {
-    int n = arr.size();
-    vector<int> result;
-    int max_right = arr[n-1];
-    for (int i = n - 1; i >= 0; i--) {
-        if (arr[i] >= max_right) {
-            max_right = arr[i];
-            result.push_back(max_right);
-        }
-    }
-    reverse(result.begin(), result.end());
     return result;
 }
 
