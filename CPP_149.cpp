5673e28bb0ba8767eb3ab7f2709d43e1c12ba6d2
beep boop <githubbot@vadim.me>
2024-07-09 20:10:27 +0200

    Solution 17 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/h6h6rl4f Prompt:    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 885b18b4-1b2f-4094-8b2f-bacda45732eb.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 885b18b4-1b2f-4094-8b2f-bacda45732eb.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 885b18b4-1b2f-4094-8b2f-bacda45732eb.cpp:23: 885b18b4-1b2f-4094-8b2f-bacda45732eb.cpp: In function ‘int main()’: 885b18b4-1b2f-4094-8b2f-bacda45732eb.cpp:33:20: error: ‘sorted_list_sum’ was not  declared in this scope    33 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 885b18b4-1b2f-4094-8b2f-bacda45732eb.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,14 +1,10 @@
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    return a == b;
-}
-
-std::vector<string> sorted_list_sum(vector<string> lst) {
+std::vector<std::string> sorted_list_sum(std::vector<std::string> lst) {
     // Filter out strings with odd lengths
-    vector<string> evenLengthStrings;
-    for (const string& str : lst) {
+    std::vector<std::string> evenLengthStrings;
+    for (const std::string& str : lst) {
         if (str.length() % 2 == 0) {
             evenLengthStrings.push_back(str);
         }
@@ -16,13 +12,13 @@ std::vector<string> sorted_list_sum(vector<string> lst) {
 
     // Sort the remaining strings by length and then alphabetically
     sort(evenLengthStrings.begin(), evenLengthStrings.end(),
-          [](const string& a, const string& b) {
-              if (a.length() != b.length()) {
-                  return a.length() < b.length();
-              } else {
-                  return a < b;
-              }
-          });
+            [](const std::string& a, const std::string& b) {
+                if (a.length() != b.length()) {
+                    return a.length() < b.length();
+                } else {
+                    return a < b;
+                }
+            });
 
     return evenLengthStrings;
 }
