371d2b7b34a1e235250acfa0fb9f4728d5f0652a
beep boop <githubbot@vadim.me>
2024-07-09 07:54:30 +0200

    Solution 5 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5av973i7 Prompt:       |     ^~~ ee43df40-d7a5-432b-9f2c-806edd31b2ce.cpp:3:1: note: ‘std::set’ is defined in hea der ‘<set>’; did you forget to ‘#include <set>’?     2 | #include <algorithm>   +++ |+#include <set>     3 | using namespace std; ee43df40-d7a5-432b-9f2c-806edd31b2ce.cpp:6:9: error: expected primary-expression  before ‘int’     6 |     set<int> s1(l1.begin(), l1.end());       |         ^~~ ee43df40-d7a5-432b-9f2c-806edd31b2ce.cpp:7:9: error: expected primary-expression  before ‘int’     7 |     set<int> s2(l2.begin(), l2.end());       |         ^~~ ee43df40-d7a5-432b-9f2c-806edd31b2ce.cpp:9:9: error: expected primary-expression  before ‘int’     9 |     set<int> intersection(set_difference(s1.begin(), s1.end(), s2.begin( ), s2.end(),       |         ^~~ ee43df40-d7a5-432b-9f2c-806edd31b2ce.cpp:12:24: error: ‘intersection’ was not de clared in this scope    12 |     vector<int> result(intersection.begin(), intersection.end());       |                        ^~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,16 +1,18 @@
-```cpp
 #include <vector>
 #include <algorithm>
+#include <set>
+
 using namespace std;
 
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
-    set<int> intersection;
 
-    set_difference(s1.begin(), s1.end(), s2.begin(), s2.end(),
-        inserter(intersection, intersection.begin()));
-
+    set<int> intersection; 
+    intersection.insert(set_difference(s1.begin(), s1.end(), s2.begin(), s2.end(),
+        inserter(intersection, intersection.begin())).end(), 
+        set_difference(s1.begin(), s1.end(), s2.begin(), s2.end()).end());
+    
     vector<int> result(intersection.begin(), intersection.end());
     return result;
 }
