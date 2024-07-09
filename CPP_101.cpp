b0a9387b8bd049bdb5fc8d3cb9e601f5ddb6fd01
beep boop <githubbot@vadim.me>
2024-07-09 02:46:55 +0200

    Solution 8 of CPP/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ryo1pk4z Prompt:    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ea37f7c3-d046-4d08-9d5e-3706d8bf7dca.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ea37f7c3-d046-4d08-9d5e-3706d8bf7dca.cpp:21:45: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ea37f7c3-d046-4d08-9d5e-3706d8bf7dca.cpp:20: ea37f7c3-d046-4d08-9d5e-3706d8bf7dca.cpp: In function ‘int main()’: ea37f7c3-d046-4d08-9d5e-3706d8bf7dca.cpp:30:20: error: ‘words_string’ was not de clared in this scope    30 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |                    ^~~~~~~~~~~~ ea37f7c3-d046-4d08-9d5e-3706d8bf7dca.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~

--- CPP_101.cpp
+++ CPP_101.cpp
@@ -1,12 +1,22 @@
-vector<string> words_string(string s);
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+using namespace std;
 
 bool issame(vector<string> a, vector<string> b){
-    return a == b;
-}
-
-int main(){
-    assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal"}));
-    return 0;
+    if(a.size() != b.size()){
+        return false;
+    }
+    
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    
+    return true;
 }
 
 vector<string> words_string(string s){
@@ -26,4 +36,10 @@ vector<string> words_string(string s){
         words.push_back(word);
     }
     return words;
+}
+
+int main(){
+    assert(issame(words_string("ahmed     , gamal"), {"ahmed", "gamal"}));
+    
+    return 0;
 }
