8b8f9424579a466009e56508585fc19b2e95cfa7
beep boop <githubbot@vadim.me>
2024-07-09 02:56:49 +0200

    Solution 66 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/q5aay22w Prompt: in this scope    21 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 3809088d-51c7-4de5-a923-893b3dd6d865.cpp:21:5: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 3809088d-51c7-4de5-a923-893b3dd6d865.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 3809088d-51c7-4de5-a923-893b3dd6d865.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 3809088d-51c7-4de5-a923-893b3dd6d865.cpp:21:12: error: expected primary-expressi on before ‘float’    21 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~ In file included from 3809088d-51c7-4de5-a923-893b3dd6d865.cpp:19: 3809088d-51c7-4de5-a923-893b3dd6d865.cpp:22:31: error: ‘has_close_elements’ cann ot be used as a function    22 |     assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false) ;       |             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -12,7 +12,7 @@ bool has_close_elements(std::vector<float> numbers, float threshold) {
 }
 
 int main() {
-    std::vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
+    std::vector<float> a = {1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
     bool result = has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5);
     assert(!result); 
 }
