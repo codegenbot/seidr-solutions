6ba6ca05dd242237682f3d7f0ab8cf5415a437dc
beep boop <githubbot@vadim.me>
2024-07-09 03:01:09 +0200

    Solution 13 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/x5ov6o3d Prompt: 75d62358-9eb7-4f83-8a6a-264a6f273240.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string flip_case(string str){       | ^~~~~~ In file included from 75d62358-9eb7-4f83-8a6a-264a6f273240.cpp:14: 75d62358-9eb7-4f83-8a6a-264a6f273240.cpp: In function ‘int main()’: 75d62358-9eb7-4f83-8a6a-264a6f273240.cpp:16:13: error: ‘filp_case’ was not decla red in this scope    16 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,12 +1,11 @@
-#include <string>
-
 std::string flip_case(std::string str){
+#include <string>
     std::string result = "";
     for(int i=0; i<str.length(); i++){
         if(str[i] >= 'a' && str[i] <= 'z')
-            result += toupper((char)str[i]);
+            result += toupper(str[i]);
         else if(str[i] >= 'A' && str[i] <= 'Z')
-            result += tolower((char)str[i]);
+            result += tolower(str[i]);
         else
             result += str[i];
     }
