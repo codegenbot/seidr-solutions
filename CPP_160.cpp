e95bdcc4fa09897751dc819a573c4775975abe08
beep boop <githubbot@vadim.me>
2024-07-09 20:28:11 +0200

    Solution 23 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8tmp81k3 Prompt: tor std::uninitialized_copy(_InputIterator, _InputIterator, _ForwardIterator) [w ith _InputIterator = const char*; _ForwardIterator = std::pair<char, int>*]’: /usr/include/c++/11/bits/stl_uninitialized.h:333:37:   required from ‘_ForwardIt erator std::__uninitialized_copy_a(_InputIterator, _InputIterator, _ForwardItera tor, std::allocator<_Tp>&) [with _InputIterator = const char*; _ForwardIterator = std::pair<char, int>*; _Tp = std::pair<char, int>]’ /usr/include/c++/11/bits/stl_vector.h:1585:33:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const char*; _Tp = std::pair<char, in t>; _Alloc = std::allocator<std::pair<char, int> >]’ /usr/include/c++/11/bits/stl_vector.h:657:23:   required from ‘std::vector<_Tp, _Alloc>::vector(_InputIterator, _InputIterator, const allocator_type&) [with _In putIterator = const char*; <template-parameter-2-2> = void; _Tp = std::pair<char , int>; _Alloc = std::allocator<std::pair<char, int> >; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<std::pair<char, int> >]’ 411728df-e891-424f-9ed8-6bbe4bc755ae.cpp:25:5:   required from here /usr/include/c++/11/bits/stl_uninitialized.h:138:72: error: static assertion fai led: result type must be constructible from value type of input range   138 | static_assert(is_constructible<_ValueType2, decltype(*__first)>::value,       |                                                                  ^~~~~ /usr/include/c++/11/bits/stl_uninitialized.h:138:72: note: ‘std::integral_consta nt<bool, false>::value’ evaluates to false

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,8 +1,7 @@
 #include <vector>
 #include <cmath>
-#include <utility>
 
-int do_algebra() {
+std::vector<std::pair<char, int>> get_input() {
     int numOperators;
     std::cin >> numOperators;
 
@@ -15,6 +14,10 @@ int do_algebra() {
         operators_and_operands.push_back({op, operand});
     }
 
+    return operators_and_operands;
+}
+
+int do_algebra(std::vector<std::pair<char, int>> operators_and_operands) {
     int result = operators_and_operands[0].second;
     for (const auto& pair : operators_and_operands) {
         if (pair.first == '+') {
@@ -26,8 +29,15 @@ int do_algebra() {
         } else if (pair.first == '/') {
             result = result / pair.second;
         } else if (pair.first == '**') {
-            result = pow(result, pair.second);
+            result = std::pow(result, pair.second);
         }
     }
     return result;
+}
+
+int main() {
+    auto operators_and_operands = get_input();
+    int output = do_algebra(operators_and_operands);
+    std::cout << output << std::endl;
+    return 0;
 }
