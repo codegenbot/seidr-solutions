a76e7bbbf91d030d6287799d8a6875ab373886a4
beep boop <githubbot@vadim.me>
2024-07-09 05:18:34 +0200

    Solution 38 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/d9xvhjel Prompt: d48ed144-4ccf-4383-ae67-6d7435198052.cpp: In function ‘std::string string_to_md5 (std::string)’: d48ed144-4ccf-4383-ae67-6d7435198052.cpp:11:5: error: ‘MD5’ was not declared in this scope; did you mean ‘md5’?    11 |     MD5((const unsigned char*)text.c_str(), text.size(), md5);       |     ^~~       |     md5 d48ed144-4ccf-4383-ae67-6d7435198052.cpp:15:22: error: aggregate ‘std::stringstr eam ss’ has incomplete type and cannot be defined    15 |         stringstream ss;       |                      ^~ d48ed144-4ccf-4383-ae67-6d7435198052.cpp:16:15: error: ‘hex’ was not declared in  this scope    16 |         ss << hex << (int)md5[i];       |               ^~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -10,16 +10,16 @@ string string_to_md5(string text) {
     
     unsigned char md5[16];
     EVP_MD_CTX mdctx;
-    const EVP_MD *md = EVP_md5();
-    unsigned char* d;
-    int len;
-    HMAC(EVP_md5(), NULL, 0, &text[0], text.size(), &mdctx);
-    EVP_MD_CTX_cleanup(&mdctx);
-
+    EVP_MD *md = EVP_md5();
+    if (!EVP_DigestInit_ex(&mdctx, md, NULL)) 
+        return "Error in EVP_DigestInit_ex";
+    EVP_DigestUpdate(&mdctx, (const unsigned char*)text.c_str(), text.size());
+    EVP_DigestFinal_ex(&mdctx, md5, NULL);
+    
     string result;
-    for(int i = 0; i < md5_size(md); ++i){
-        stringstream ss;
-        ss << hex << (int)EVP_get_block56(EVP_CIPHER_CTX_get_block_size((EVP_CIPHER_CTX *)NULL), &md[i]);
+    for(int i = 0; i < 16; ++i){
+        ostringstream ss;
+        ss << hex << setfill('0') << setw(2) << (int)md5[i];
         result += ss.str();
     }
     
