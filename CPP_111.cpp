2939c49b0fc49c3e5e3120a958ae493dcf1bb68e
beep boop <githubbot@vadim.me>
2024-07-09 02:36:12 +0200

    Solution 64 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zbah5c5m Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_map.h:62,                  from /usr/include/c++/11/map:61,                  from ca048222-9d0b-4293-a1fe-77c7f38924c9.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,4 +1,4 @@
-```
+```c++
 #include <map>
 #include <string>
 
@@ -19,6 +19,6 @@ bool issame(std::map<char, int> a, std::map<char, int> b) {
 }
 
 int main() {
-    assert(issame(histogram("a"), std::map<char, int>({std::pair<char, int>('a', 1)})));
+    assert(histogram("a") == std::map<char, int>{{'a', 1}});
     return 0;
 }
