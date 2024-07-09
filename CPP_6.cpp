d8d588f6c19eadab4a4688828048ac1c39c424da
beep boop <githubbot@vadim.me>
2024-07-09 20:24:43 +0200

    Solution 8 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g440czyz Prompt:  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f60796f2-1fe3-40a5-b6f4-a30d4f5f08c4.cpp:25:27: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? f60796f2-1fe3-40a5-b6f4-a30d4f5f08c4.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f60796f2-1fe3-40a5-b6f4-a30d4f5f08c4.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f60796f2-1fe3-40a5-b6f4-a30d4f5f08c4.cpp:24: f60796f2-1fe3-40a5-b6f4-a30d4f5f08c4.cpp: In function ‘int main()’: f60796f2-1fe3-40a5-b6f4-a30d4f5f08c4.cpp:34:20: error: ‘parse_nested_parens’ was  not declared in this scope    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ f60796f2-1fe3-40a5-b6f4-a30d4f5f08c4.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,14 +1,11 @@
-#include <stack>
 #include <vector>
-
+#include <stack>
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
-            return false;
+bool issame(vector<int> a,vector<int>b){
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i<a.size(); i++){
+        if(a[i] != b[i]) return false;
     }
     return true;
 }
@@ -17,7 +14,7 @@ vector<int> parse_nested_parens(string paren_string) {
     vector<int> result;
     stack<int> nesting_levels;
     int max_nesting = 0;
-
+    
     for (char c : paren_string) {
         if (c == '(') {
             nesting_levels.push(1);
@@ -29,11 +26,11 @@ vector<int> parse_nested_parens(string paren_string) {
             max_nesting = max(max_nesting, current_level);
         }
     }
-
+    
     return result;
 }
 
 int main() {
-    assert(issame(parse_nested_parens("(()(())((()))"), vector<int>({{4}})));
+    assert(issame(parse_nested_parens("(()(())((()))") ,{4}));
     return 0;
 }
