2305a1d051c776ae867ba4b8e9ed43a6ac159e90
beep boop <githubbot@vadim.me>
2024-07-09 04:44:42 +0200

    Solution 82 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/i28o72ia Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eb3d4d58-8669-4434-a591-e106a2009 e22.py", line 10     encoded_message += chr((ord(char) - ord("A") + 4) % 26 + ord("A")) if char.i supper() else chr((ord(char) - ord("a") + 4) % 26 + ord("a")                  ^ SyntaxError: '(' was never closed

--- Python_93.py
+++ Python_93.py
@@ -4,10 +4,10 @@ def encode(message):
 
     for char in message:
         if char.isalpha():
-            if char.upper() in vowels:
-                encoded_message += chr((ord(char.upper()) - ord("A") + 3) % 26 + ord("A"))
+            if char.lower() in vowels:
+                encoded_message += chr((ord(char) - ord("A") + 3) % 26 + ord("A")) if char.isupper() else chr((ord(char) - ord("a") + 3) % 26 + ord("a"))
             else:
-                encoded_message += chr((ord(char.upper()) - ord("A") + 4) % 26 + ord("A"))
+                encoded_message += chr((ord(char) - ord("A") + 4) % 26 + ord("A")) if char.isupper() else chr((ord(char) - ord("a") + 4) % 26 + ord("a"))
         else:
             encoded_message += char
 
