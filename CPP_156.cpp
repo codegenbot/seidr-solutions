017b5ee76d71174b61eff99edb1ad02b094a1634
beep boop <githubbot@vadim.me>
2024-07-09 05:19:38 +0200

    Solution 46 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/urrbvh7e Prompt: ea1af8b7-1eee-4f24-80a2-d907da8bd100.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int num) {       | ^~~~~~ In file included from ea1af8b7-1eee-4f24-80a2-d907da8bd100.cpp:71: ea1af8b7-1eee-4f24-80a2-d907da8bd100.cpp: In function ‘int main()’: ea1af8b7-1eee-4f24-80a2-d907da8bd100.cpp:73:13: error: ‘int_to_mini_romank’ was not declared in this scope    73 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,6 +1,5 @@
-```
-string miniRomanToInteger(string roman) {
-    int num = 0;
+```cpp
+string intToMiniRoman(int num) {
     string roman[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",
                       "", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
                       "", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
@@ -15,77 +14,59 @@ string miniRomanToInteger(string roman) {
                       "", "CXLI", "CXLII", "CXLIII", "CXLIV", "CXLV", "CXLVI", "CXLVII", "CXLVIII", "CXlix", "CL",
                       "", "CLI", "CII", "CC", "CCI", "CCC", "CCCI", "CCCII", "CCCIII", "CCCIV", "CCCV", "CCCVI", "CCCvii", "CCCIX", "CCCX"};
 
-    for (int i = 0; i < sizeof(roman) / sizeof(string); i++) {
-        if (roman[i].length() > 1 && roman[i].substr(0, 2) == roman[i]) {
-            while (num >= roman_to_int(roman[i])) {
-                num -= roman_to_int(roman[i]);
-                result += roman[i];
+    string result = "";
+
+    while (num > 0) {
+        if (num >= 1000) {
+            num -= 1000;
+            result += "M";
+        } else if (num >= 900) {
+            num -= 900;
+            result += "CM";
+        } else if (num >= 500) {
+            num -= 500;
+            result += "D";
+        } else if (num >= 400) {
+            num -= 400;
+            result += "CD";
+        } else if (num >= 100) {
+            int count = num / 100;
+            for (int i = 0; i < count; i++) {
+                num -= 100;
+                result += "C";
             }
+        } else if (num >= 90) {
+            num -= 90;
+            result += "XC";
+        } else if (num >= 50) {
+            num -= 50;
+            result += "L";
+        } else if (num == 40) {
+            result += "XL";
+            return result;
+        } else if (num >= 10) {
+            int count = num / 10;
+            for (int i = 0; i < count; i++) {
+                num -= 10;
+                result += "X";
+            }
+        } else if (num == 9) {
+            result += "IX";
+            return result;
+        } else if (num >= 5) {
+            int count = num / 5;
+            for (int i = 0; i < count; i++) {
+                num -= 5;
+                result += "V";
+            }
+        } else if (num == 4) {
+            result += "IV";
+            return result;
         } else {
-            while (num >= roman_to_int(roman[i])) {
-                num -= roman_to_int(roman[i]);
-                result += roman[i];
-            }
+            num--;
+            result += "I";
         }
     }
 
     return result;
 }
-
-int roman_to_int(string roman) {
-    int num = 0;
-    string roman[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",
-                      "", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
-                      "", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
-                      "", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
-                      "", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
-                      "", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
-                      "", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
-                      "", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
-                      "", "C", "CI", "CII", "CIII", "CIV", "CV", "CVI", "CVII", "CVIII", "CX", "CXI", "CXII", "CXIII", "CXIV", "CXV", "CXVI", "CXVII", "CXVIII", "CXIX", "CXX",
-                      "", "CXXI", "CXXII", "CXXIII", "CXXIV", "CXXV", "CXXVI", "CXXVII", "CXXVIII", "CXXIX", "CXXX",
-                      "", "CXXXI", "CXXXII", "CXXXIII", "CXXXIV", "CXXXV", "CXXXVI", "CXXXVII", "CXXXVIII", "CXXXIX", "CXL",
-                      "", "CXLI", "CXLII", "CXLIII", "CXLIV", "CXLV", "CXLVI", "CXLVII", "CXLVIII", "CXlix", "CL",
-                      "", "CLI", "CII", "CC", "CCI", "CCC", "CCCI", "CCCII", "CCCIII", "CCCIV", "CCCV", "CCCVI", "CCCvii", "CCCIX", "CCCX"};
-
-    if (roman.length() == 1) {
-        for (int i = 0; i < sizeof(roman) / sizeof(string); i++) {
-            if (roman[i].length() > 0 && roman[i] == roman[0]) {
-                return roman_to_int_roman(i);
-            }
-        }
-    } else if (roman.length() > 1 && roman.substr(0, 2) == "IV") {
-        return 4;
-    } else if (roman.length() > 1 && roman.substr(0, 2) == "IX") {
-        return 9;
-    } else if (roman.length() > 1 && roman.substr(0, 2) == "XL") {
-        return 40;
-    } else if (roman.length() > 1 && roman.substr(0, 2) == "XC") {
-        return 90;
-    } else if (roman.length() > 1 && roman.substr(0, 3) == "CD") {
-        return 400;
-    } else if (roman.length() > 1 && roman.substr(0, 3) == "CM") {
-        return 900;
-    }
-
-    for (int i = 0; i < sizeof(roman) / sizeof(string); i++) {
-        if (roman[i].length() > 1 && roman[i] == roman[0]) {
-            while (num >= roman_to_int_roman(i)) {
-                num -= roman_to_int_roman(i);
-                result += roman[i];
-            }
-        } else {
-            for (int j = 0; j < sizeof(roman) / sizeof(string); j++) {
-                if (roman[j].length() > 1 && roman[j] == roman[0]) {
-                    while (num >= roman_to_int_roman(j)) {
-                        num -= roman_to_int_roman(j);
-                        result += roman[j];
-                    }
-                } else if (roman[j].length() == 1) {
-                    for (int k = 0; k < sizeof(roman) / sizeof(string); k++) {
-                        if (roman[k].length() > 0 && roman[k] == roman[j]) {
-                            while (num >= roman_to_int_roman(k)) {
-                                num -= roman_to_int_roman(k);
-                                result += roman[k];
-                            }
-                        }
