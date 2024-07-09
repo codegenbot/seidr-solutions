2d39e557ce4c38fc2f9cf1f02fcddd65823b0e97
beep boop <githubbot@vadim.me>
2024-07-09 16:52:31 +0200

    Solution 20 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0bwgh28f Prompt:                  from /usr/include/c++/11/iostream:39,                  from 4aed12b8-82c9-498d-af78-11e27a83620d.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 4aed12b8-82c9-498d-af78-11e27a83620d.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4aed12b8-82c9-498d-af78-11e27a83620d.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 4aed12b8-82c9-498d-af78-11e27a83620d.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 4aed12b8-82c9-498d-af78-11e27a83620d.cpp:29: 4aed12b8-82c9-498d-af78-11e27a83620d.cpp: In function ‘int main()’: 4aed12b8-82c9-498d-af78-11e27a83620d.cpp:39:20: error: ‘by_length’ was not decla red in this scope    39 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,4 +1,4 @@
-```cpp
+```
 #include <iostream>
 #include <vector>
 #include <string>
@@ -16,13 +16,12 @@ int main() {
     std::vector<int> intArray(numArray, numArray + sizeof(numArray) / sizeof(numArray[0]));
     std::vector<std::string> stringArray;
 
-    for (int i = 0; i < intArray.size(); i++) {
-        if (intArray[i] == 9) stringArray.push_back("Nine");
-        else if (intArray[i] == 4) stringArray.push_back("Four");
-        else if (intArray[i] == 8) stringArray.push_back("Eight");
+    for (int i : intArray) {
+        if (i == 9) stringArray.push_back("Nine");
+        else if (i == 4) stringArray.push_back("Four");
+        else if (i == 8) stringArray.push_back("Eight");
     }
 
-    std::vector<std::string> strArr = {"Nine", "Four", "Eight"};
+    std::vector<std::string> strArr = {"" + std::to_string(i) for i in intArray};
     assert(issame(stringArray, strArr));
-    return 0;
 }
