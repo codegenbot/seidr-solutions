2e15372e1cf4b519710accbabc185fa1f11c6af1
beep boop <githubbot@vadim.me>
2024-07-09 05:51:54 +0200

    Solution 77 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/d9xvhjel Prompt:       |     ^~~~~~~~~~~~~~~~~~       |     EVP_MD_CTX_create 66c8f93a-aed9-41cc-8008-b88e1724dc08.cpp:20:24: error: ‘md5_size’ was not declar ed in this scope; did you mean ‘dysize’?    20 |     for(int i = 0; i < md5_size(md); ++i){       |                        ^~~~~~~~       |                        dysize 66c8f93a-aed9-41cc-8008-b88e1724dc08.cpp:22:103: error: invalid use of incomplet e type ‘struct evp_md_st’    22 | block56(EVP_CIPHER_CTX_get_block_size((EVP_CIPHER_CTX *)NULL), &md[i]);       |                                                                     ^ In file included from /usr/include/openssl/evp.h:26,                  from 66c8f93a-aed9-41cc-8008-b88e1724dc08.cpp:2: /usr/include/openssl/types.h:103:16: note: forward declaration of ‘struct evp_md _st’   103 | typedef struct evp_md_st EVP_MD;       |                ^~~~~~~~~ 66c8f93a-aed9-41cc-8008-b88e1724dc08.cpp:22:27: error: ‘EVP_get_block56’ was not  declared in this scope    22 |         ss << hex << (int)EVP_get_block56(EVP_CIPHER_CTX_get_block_size( (EVP_CIPHER_CTX *)NULL), &md[i]);       |                           ^~~~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,15 +1,12 @@
-#include <string>
-#include <openssl/evp.h>
+#include <openssl/ssl.h>
 #include <openssl/crypto.h>
-#include <sstream>
-#include <iomanip>
 
 using namespace std;
 
 string string_to_md5(string text) {
     if (text.empty()) return "None";
     
-    unsigned char md[16];
+    unsigned char md5[16];
     EVP_MD_CTX mdctx;
     const EVP_MD *md = EVP_md5();
     unsigned char* d;
@@ -20,7 +17,7 @@ string string_to_md5(string text) {
     string result;
     for(int i = 0; i < sizeof(md) / sizeof(md[0]); ++i){
         stringstream ss;
-        ss << hex << (int)md[i];
+        ss << hex << (int) md[i];
         result += ss.str();
     }
     
