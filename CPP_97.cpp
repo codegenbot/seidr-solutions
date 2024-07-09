27cfc8e0118f2b21c959985707655f613be97208
beep boop <githubbot@vadim.me>
2024-07-09 18:49:36 +0200

    Solution 1 of CPP/97, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/f6a8mr3k Prompt: Complete the following code given the task description and function signature.

--- CPP_97.cpp
+++ CPP_97.cpp
@@ -1,7 +1,7 @@
 Here is the completed code:
 
-int multiply(int a, int b) {
+int multiply(int a, int b){
     int unit_a = a % 10;
-    int unit_b = b % 10;
+    int unit_b = abs(b) % 10; // take absolute value
     return unit_a * unit_b;
 }
