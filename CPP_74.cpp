c9d73e6238b8f15ecd5bae9c08cfba62f986c592
beep boop <githubbot@vadim.me>
2024-07-09 04:32:29 +0200

    Solution 84 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8ahd9aop Prompt:  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 731351f2-41a5-46f5-9b26-855355ce1430.cpp:26:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 731351f2-41a5-46f5-9b26-855355ce1430.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 731351f2-41a5-46f5-9b26-855355ce1430.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 731351f2-41a5-46f5-9b26-855355ce1430.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 731351f2-41a5-46f5-9b26-855355ce1430.cpp:25: 731351f2-41a5-46f5-9b26-855355ce1430.cpp: In function ‘int main()’: 731351f2-41a5-46f5-9b26-855355ce1430.cpp:35:20: error: ‘total_match’ was not dec lared in this scope    35 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -9,7 +9,7 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return true;
 }
 
-std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<std::string> lst2) {
+vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     int sum1 = 0, sum2 = 0;
     for (const auto& s : lst1) {
         sum1 += s.length();
@@ -24,38 +24,6 @@ std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<
 
 int main() {
     assert(issame(total_match({"this"}, {}), {}));
-    std::vector<std::string> lst1;
-    std::cout << "Enter the strings for list 1: ";
-    int n1 = 0;
-    while (std::cin >> lst1[n1]) {
-        if (++n1 == 10) break;
-    }
-    std::vector<std::string> lst2;
-    std::cout << "Enter the strings for list 2: ";
-    int n2 = 0;
-    while (std::cin >> lst2[n2]) {
-        if (++n2 == 10) break;
-    }
-    
-    std::cout << "List 1: ";
-    for (const auto& s : lst1) {
-        std::cout << s << " ";
-    }
-    std::cout << "\n";
-    std::cout << "List 2: ";
-    for (const auto& s : lst2) {
-        std::cout << s << " ";
-    }
-    std::cout << "\n";
-    
-    if (issame(lst1, lst2)) 
-        std::cout << "The two lists are the same.\n"; 
-    else 
-        std::cout << "total_match: ";
-    for (const auto& s : total_match(lst1, lst2)) {
-        std::cout << s << " ";
-    }
-    std::cout << "\n";
-    
+    // And so on...
     return 0;
 }
