e6576fff2d46ed1b3ea4341d589c9f0f3bccc8ec
beep boop <githubbot@vadim.me>
2024-07-09 08:24:59 +0200

    Solution 34 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kd9i1j85 Prompt:       |                    ^~~ b7057955-78d7-42c9-8075-8335d1d671d1.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b7057955-78d7-42c9-8075-8335d1d671d1.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b7057955-78d7-42c9-8075-8335d1d671d1.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b7057955-78d7-42c9-8075-8335d1d671d1.cpp:19: b7057955-78d7-42c9-8075-8335d1d671d1.cpp: In function ‘int main()’: b7057955-78d7-42c9-8075-8335d1d671d1.cpp:29:20: error: ‘get_odd_collatz’ was not  declared in this scope    29 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ b7057955-78d7-42c9-8075-8335d1d671d1.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,23 +1,16 @@
-```cpp
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> get_odd_collatz(int n) {
-    vector<int> result;
-    while (n != 1) {
-        if (n % 2 == 0) {
-            n /= 2;
-        } else {
-            n = 3 * n + 1;
-        }
-        if (n % 2 != 0) {
-            result.push_back(n);
-        }
+bool issame(vector<int> v1, vector<int> v2) {
+    if(v1.size() != v2.size()) 
+        return false;
+    for(int i = 0; i < v1.size(); i++) {
+        if(v1[i] != v2[i]) 
+            return false;
     }
-    sort(result.begin(), result.end());
-    return result;
+    return true;
 }
 
 int main() {
