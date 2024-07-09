a442fb7f2fdeff489b723d4763006ae97833aea2
beep boop <githubbot@vadim.me>
2024-07-09 12:32:12 +0200

    Solution 77 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7113sxs1 Prompt: r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | 63f2feb4-2e8f-46c6-a045-b8c747f9b679.cpp: At global scope: 63f2feb4-2e8f-46c6-a045-b8c747f9b679.cpp:17:5: error: redefinition of ‘int main( )’    17 | int main(){       |     ^~~~ 63f2feb4-2e8f-46c6-a045-b8c747f9b679.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~ In file included from 63f2feb4-2e8f-46c6-a045-b8c747f9b679.cpp:16: 63f2feb4-2e8f-46c6-a045-b8c747f9b679.cpp: In function ‘int main()’: 63f2feb4-2e8f-46c6-a045-b8c747f9b679.cpp:18:35: error: ‘lst’ was not declared in  this scope    18 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 63f2feb4-2e8f-46c6-a045-b8c747f9b679.cpp:18:43: error: ‘odd_sum’ was not declare d in this scope    18 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,7 +1,16 @@
+#include <cassert>
+#include <vector>
+
 long double_the_difference(const std::vector<int>& input) {
     long average = 0;
     for (int num : input) {
         average += num;
     }
-    return static_cast<long>(average);
+    return static_cast<long>(average * average);
+}
+
+int main() {
+    std::vector<int> lst = {1,2,3};
+    int expected_result = double_the_difference(lst); 
+    assert(expected_result == 14); 
 }
