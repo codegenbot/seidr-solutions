8a0f487fcb299564492554f47012054a4cad9952
beep boop <githubbot@vadim.me>
2024-07-09 16:44:28 +0200

    Solution 61 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7gxeyycu Prompt:       |             ^~~~~~ e1e737ac-5151-4033-ad18-538ddadf9d4a.cpp:63:34: error: expected primary-expressi on before ‘int’    63 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e1e737ac-5151-4033-ad18-538ddadf9d4a.cpp:63:39: error: expression list treated a s compound expression in initializer [-fpermissive]    63 | bool issame(vector<int> a,vector<int>b){       |                                       ^ e1e737ac-5151-4033-ad18-538ddadf9d4a.cpp:71:5: error: redefinition of ‘int main( )’    71 | int main(){       |     ^~~~ e1e737ac-5151-4033-ad18-538ddadf9d4a.cpp:43:5: note: ‘int main()’ previously def ined here    43 | int main() {       |     ^~~~ In file included from e1e737ac-5151-4033-ad18-538ddadf9d4a.cpp:62: e1e737ac-5151-4033-ad18-538ddadf9d4a.cpp: In function ‘int main()’: e1e737ac-5151-4033-ad18-538ddadf9d4a.cpp:72:19: error: ‘issame’ cannot be used a s a function    72 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-bool compareVectors(const std::vector<int>& a, const std::vector<int>& b) {
+bool areEqual(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
@@ -50,7 +50,7 @@ int main() {
 
     std::vector<int> output = strange_sort_list(input);
 
-    if (compareVectors(output, {1,2,3,4,5,6})) {
+    if (areEqual(output, {1,2,3,4,5,6})) {
         std::cout << "True" << std::endl;
     } else {
         std::cout << "False" << std::endl;
