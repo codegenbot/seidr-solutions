6b9e9b01ec8e01707f55eb295c67e0d499ddf083
beep boop <githubbot@vadim.me>
2024-07-09 02:15:54 +0200

    Solution 18 of CPP/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/namiduv7 Prompt: ore ‘for’     1 | for(int i = 0; i < a.length(); i++) {       | ^~~ 7f6b8e46-f8a2-497b-814d-497bf0d6a8b1.cpp:1:16: error: ‘i’ does not name a type     1 | for(int i = 0; i < a.length(); i++) {       |                ^ 7f6b8e46-f8a2-497b-814d-497bf0d6a8b1.cpp:1:32: error: ‘i’ does not name a type     1 | for(int i = 0; i < a.length(); i++) {       |                                ^ 7f6b8e46-f8a2-497b-814d-497bf0d6a8b1.cpp:15:5: error: expected unqualified-id be fore ‘return’    15 |     return false;       |     ^~~~~~ 7f6b8e46-f8a2-497b-814d-497bf0d6a8b1.cpp:16:1: error: expected declaration befor e ‘}’ token    16 | }       | ^ In file included from 7f6b8e46-f8a2-497b-814d-497bf0d6a8b1.cpp:18: 7f6b8e46-f8a2-497b-814d-497bf0d6a8b1.cpp: In function ‘int main()’: 7f6b8e46-f8a2-497b-814d-497bf0d6a8b1.cpp:20:14: error: ‘cycpattern_check’ was no t declared in this scope    20 |     assert  (cycpattern_check("winemtt","tinem") == true );       |              ^~~~~~~~~~~~~~~~

--- CPP_154.cpp
+++ CPP_154.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <string>
-
 bool cycpattern_check(string a, string b) {
     for(int i = 0; i < a.length(); i++) {
         string temp = a.substr(i);
@@ -17,20 +14,4 @@ bool cycpattern_check(string a, string b) {
         }
     }
     return false;
-}
-
-int main() {
-    string a, b;
-    cout << "Enter the first string: ";
-    cin >> a;
-    cout << "Enter the second string: ";
-    cin >> b;
-    
-    if(cycpattern_check(a, b)) {
-        cout << "The pattern is cyclically present in the given strings." << endl;
-    } else {
-        cout << "The pattern is not cyclically present in the given strings." << endl;
-    }
-    
-    return 0;
 }
