8dbb2bf541d068f554a83b52a5bed357b8e6c0a1
beep boop <githubbot@vadim.me>
2024-07-09 03:10:28 +0200

    Solution 15 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/du4l4ndj Prompt:       |           ^~~~~~ In file included from 062259d8-f05a-464a-8e12-b9cca67c9752.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 062259d8-f05a-464a-8e12-b9cca67c9752.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 062259d8-f05a-464a-8e12-b9cca67c9752.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 062259d8-f05a-464a-8e12-b9cca67c9752.cpp:13: 062259d8-f05a-464a-8e12-b9cca67c9752.cpp: In function ‘int main()’: 062259d8-f05a-464a-8e12-b9cca67c9752.cpp:23:20: error: ‘get_positive’ was not de clared in this scope    23 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 062259d8-f05a-464a-8e12-b9cca67c9752.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -17,19 +17,6 @@ std::vector<float> get_positive(std::vector<float> l) {
 }
 
 int main() {
-    assert(issame({}, {}) == true);
-    assert(get_positive({}) == {});
-    vector<float> vec1 = {1, -2, 3};
-    vector<float> vec2 = {-4, 5, -6};
-    vector<float> posVec1 = get_positive(vec1);
-    vector<float> posVec2 = get_positive(vec2);
-    assert(issame(posVec1, {}) == false);
-    assert(issame(posVec2, {}) == true);
-    for (float num : posVec1) {
-        if (num > 0) {
-            std::cout << num << " ";
-        }
-    }
-    std::cout << '\n';
+    assert(issame(get_positive({}), {}));
     return 0;
 }
