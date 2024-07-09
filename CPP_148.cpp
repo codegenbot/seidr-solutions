19b0ada31039f70d32890a041ec0df53076ba82f
beep boop <githubbot@vadim.me>
2024-07-09 03:17:03 +0200

    Solution 94 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/yzjvcls6 Prompt: In file included from 0ee3d263-d2fa-4591-bbd0-049b50c9f5da.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 0ee3d263-d2fa-4591-bbd0-049b50c9f5da.cpp:21:37: error: ‘string’ was not declared  in this scope    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0ee3d263-d2fa-4591-bbd0-049b50c9f5da.cpp:21:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 0ee3d263-d2fa-4591-bbd0-049b50c9f5da.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 0ee3d263-d2fa-4591-bbd0-049b50c9f5da.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 0ee3d263-d2fa-4591-bbd0-049b50c9f5da.cpp:21:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,13 +1,6 @@
 #include <vector>
 #include <string>
 
-std::vector<std::string> bf(const std::string& str1, const std::string& str2) {
-    if(str1 == "Jupiter" && str2 == "Makemake")
-        return {{str1}, {str2}};
-    else
-        return {};
-}
-
 bool issame(const std::vector<std::string>& vec1, const std::vector<std::string>& vec2) {
     return vec1 == vec2;
 }
@@ -15,7 +8,6 @@ bool issame(const std::vector<std::string>& vec1, const std::vector<std::string>
 int main() {
     if (!issame(bf("Jupiter", "Makemake"), {{"Jupiter"}, {"Makemake"}})) {
         return 1; 
-    } else {
-        return 0;
     }
+    return 0;
 }
