4b356031471c535a9a0882e871b3339dcbff3c02
beep boop <githubbot@vadim.me>
2024-07-09 03:19:09 +0200

    Solution 20 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nvqm4xhw Prompt: 30ef11c5-026b-4000-ac2e-f6994972c119.cpp:24:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    24 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 30ef11c5-026b-4000-ac2e-f6994972c119.cpp:27:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    27 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 30ef11c5-026b-4000-ac2e-f6994972c119.cpp:27:36: error: ‘find_zero’ cannot be use d as a function    27 |         solution = find_zero(coeffs);       |                                    ^ In file included from 30ef11c5-026b-4000-ac2e-f6994972c119.cpp:11: 30ef11c5-026b-4000-ac2e-f6994972c119.cpp:28:21: error: ‘poly’ was not declared i n this scope    28 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 30ef11c5-026b-4000-ac2e-f6994972c119.cpp:28:17: error: ‘abs’ was not declared in  this scope    28 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,27 +1,10 @@
-```
-#include<stdio.h>
-#include<math.h>
-#include<vector>
-using namespace std;
-
-double poly(vector<double> xs, double x){
-    double sum=0;
-    int i;
-    for (i=0;i<xs.size();i++)
-    {
-        sum+=xs[i]*pow(x,i);
+```cpp
+double find_zero(vector<double> coeffs, double& coeff){
+    int n = coeffs.size();
+    double a = coeff;
+    double b = 0;
+    for (int i = 0; i < n/2; i++) {
+        b -= coeffs[i] / a * pow(a, 2*i);
     }
-    return sum;
-}
-
-void find_zero(vector<double> xs){
-    if(xs.size()%2 != 0) printf("-1"); // Print -1 if the number of coefficients is odd
-    else{
-        double x=-1*fmax(-1*min(xs),-1*(xs[0]-1.0));
-        while(abs(poly(xs,x)) > 1e-5)
-        {
-            x+=0.1;
-        }
-        std::fixed; std::cout << std::setprecision(2) << x << std::endl;
-    }
+    return -b / (2*a);
 }
