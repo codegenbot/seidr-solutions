2cd984c2660d8c8a527bf3fe69ef43bf3699bd0b
beep boop <githubbot@vadim.me>
2024-07-09 08:19:35 +0200

    Solution 88 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n7aimvu6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7a3a90fc-28fb-4446-b3cd-db7e41401 372.py", line 11, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7a3a90fc-28fb-4446-b3cd-db7e41401 372.py", line 8, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -1,4 +1,7 @@
 def fruit_distribution(s, n):
     s = s.lower()
-    num_apples = int(''.join(map(str, filter(int, re.finditer(r'\d+', s)))).lstrip('0') or 0)
+    num_apples = 0
+    for word in s.split():
+        if word.startswith('apple'):
+            num_apples += int(next((i for i in iter(s.split()[s.split().index(word)+1:]) if i.isdigit()), '0'))
     return n - num_apples
