97f724bec9163862ba88e12ab402433cb9bef7a8
beep boop <githubbot@vadim.me>
2024-07-09 07:06:14 +0200

    Solution 1 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/amzk30xe Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 01b93b17-eaf0-46a3-83eb-bc28c2f86fa1.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 01b93b17-eaf0-46a3-83eb-bc28c2f86fa1.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 01b93b17-eaf0-46a3-83eb-bc28c2f86fa1.cpp:10: 01b93b17-eaf0-46a3-83eb-bc28c2f86fa1.cpp: In function ‘int main()’: 01b93b17-eaf0-46a3-83eb-bc28c2f86fa1.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 01b93b17-eaf0-46a3-83eb-bc28c2f86fa1.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,8 +1,8 @@
-vector<string> filter_by_prefix(vector<string> strings, string prefix){
-    vector<string> result;
-    for(string s : strings){
-        if(s.find(prefix) == 0)
-            result.push_back(s);
-    }
-    return result;
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
+}
+
+int main() {
+    assert(std::equal(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxxAA", "xxx"}, "xxx").begin(), filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxxAA", "xxx"}, "xxx").end(), {"xxx", "xxxAAA", "xxx"}));
+    return 0;
 }
