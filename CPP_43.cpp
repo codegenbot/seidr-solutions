3111eba68b61391510e182cf25acebfe2a7fd0d1
beep boop <githubbot@vadim.me>
2024-07-10 01:40:55 +0200

    Solution 9 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ciba3k4d Prompt: /usr/include/c++/11/bits/stl_iterator.h:585:5: note:   template argument deducti on/substitution failed: f082d35b-870e-4b05-ba22-d30cbbf49295.cpp:11:30: note:   mismatched types ‘const std::reverse_iterator<_Iterator>’ and ‘int’    11 |         if (find(s.begin() + i + 1, s.upper_bound(complement), complemen t) != s.end()) {       |                              ^ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/algorithm:61,                  from f082d35b-870e-4b05-ba22-d30cbbf49295.cpp:1: /usr/include/c++/11/bits/stl_iterator.h:1700:5: note: candidate: ‘template<class  _Iterator> constexpr std::move_iterator<_IteratorL> std::operator+(typename std ::move_iterator<_IteratorL>::difference_type, const std::move_iterator<_Iterator L>&)’  1700 |     operator+(typename move_iterator<_Iterator>::difference_type __n,       |     ^~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h:1700:5: note:   template argument deduct ion/substitution failed: f082d35b-870e-4b05-ba22-d30cbbf49295.cpp:11:30: note:   mismatched types ‘const std::move_iterator<_IteratorL>’ and ‘int’    11 |         if (find(s.begin() + i + 1, s.upper_bound(complement), complemen t) != s.end()) {       |                              ^

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,13 +1,9 @@
-#include <vector>
-#include <set>
-
-using namespace std;
-
 bool pairs_sum_to_zero(vector<int> l) {
     set<int> s = set<int>(l.begin(), l.end()); 
     for (int i = 0; i < s.size(); i++) { 
         int complement = -s[i];
-        if (find(s.begin(), s.end(), complement) != s.end()) {
+        auto it = s.find(complement);
+        if (it != s.end() && it != s.begin()) {
             return true;
         }
     }
