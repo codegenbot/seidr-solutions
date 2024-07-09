9704daf08b7ad576c998cce92035c7bac1f6f317
beep boop <githubbot@vadim.me>
2024-07-09 04:28:36 +0200

    Solution 4 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/miacs3lq Prompt:       |                         ^~~~~~ d4f475c1-7bf7-4517-80a1-ee620ba8acc9.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ d4f475c1-7bf7-4517-80a1-ee620ba8acc9.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ d4f475c1-7bf7-4517-80a1-ee620ba8acc9.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ d4f475c1-7bf7-4517-80a1-ee620ba8acc9.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from d4f475c1-7bf7-4517-80a1-ee620ba8acc9.cpp:21: d4f475c1-7bf7-4517-80a1-ee620ba8acc9.cpp: In function ‘int main()’: d4f475c1-7bf7-4517-80a1-ee620ba8acc9.cpp:23:32: error: ‘Strongest_Extension’ can not be used as a function    23 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,17 +1,19 @@
+#include <vector>
+#include <string>
+
 int Strongest_Extension(string class_name,vector<string> extensions){
-    int max_strength = 0;
+    int strongest = 0;
     string strongest_extension;
 
-    for (string extension : extensions) {
-        int cap = 0;
-        int sm = 0;
-        for (char c : extension) {
-            if (isupper(c)) cap++;
-            else if (islower(c)) sm++;
+    for(auto extension : extensions){
+        int cap = 0, sm = 0;
+        for(char c : extension){
+            if(isupper(c)) cap++;
+            else if(islower(c)) sm++;
         }
         int strength = cap - sm;
-        if (strength > max_strength) {
-            max_strength = strength;
+        if(strength > strongest){
+            strongest = strength;
             strongest_extension = extension;
         }
     }
