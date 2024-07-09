04607a6d238d72b2a7c370663210058000d6d19e
beep boop <githubbot@vadim.me>
2024-07-09 09:05:48 +0200

    Solution 82 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n98cndzy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c3f7843b-2df7-4e2e-b074-3413809e5 56a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -1,32 +1,15 @@
-```
 def match_parens(lst):
     """stack = []
-    for s in lst:
-        stack_s = []
-        for char in s:
-            if char == '(':
-                stack_s.append(char)
-            elif char == ')':
-                if len(stack_s) == 0:
-                    return 'No'
-                else:
-                    stack_s.pop()
-        while stack_s and stack:
-            if stack_s.pop() != stack.pop():
-                return 'No'
-    return 'Yes'"""
-stack = []
 for s in lst:
-    stack_s = []
     for char in s:
         if char == '(':
-            stack_s.append(char)
+            stack.append(char)
         elif char == ')':
-            if len(stack_s) == 0:
+            if len(stack) == 0:
                 return 'No'
             else:
-                stack_s.pop()
-    while stack_s and stack:
-        if stack_s.pop() != stack.pop():
-            return 'No'
-return 'Yes''
+                stack.pop()
+if len(stack) == 0:
+    return 'Yes'
+else:
+    return 'No''"
