3ac08d498f2ddd4e2241f0bac411fd2875206f5a
beep boop <githubbot@vadim.me>
2024-07-09 06:10:14 +0200

    Solution 71 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/grqmdkca Prompt: dcc73a67-ae39-4622-a673-23066e4f53ba.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from dcc73a67-ae39-4622-a673-23066e4f53ba.cpp:12: dcc73a67-ae39-4622-a673-23066e4f53ba.cpp: In function ‘int main()’: dcc73a67-ae39-4622-a673-23066e4f53ba.cpp:14:17: error: ‘max_element’ was not dec lared in this scope    14 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ dcc73a67-ae39-4622-a673-23066e4f53ba.cpp:14:13: error: ‘abs’ was not declared in  this scope    14 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -2,10 +2,10 @@ float max_element(vector<float> l);
 #include <cmath>
 
 float max_element(vector<float> l){
-    float max = abs(l[0]);
+    float max = l[0];
     for(int i=1; i<l.size(); i++){
-        if(abs(l[i]) > max)
-            max = abs(l[i]);
+        if(l[i] > max)
+            max = l[i];
     }
     return max;
 }
