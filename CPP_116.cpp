f0d53890257084cbc82e3e1ac343fe0c9b0415ce
beep boop <githubbot@vadim.me>
2024-07-09 19:53:31 +0200

    Solution 38 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lvdfcyfj Prompt:       |           ^~~~~~ In file included from bf43a465-b25b-4782-9d01-d237329db4a1.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ bf43a465-b25b-4782-9d01-d237329db4a1.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bf43a465-b25b-4782-9d01-d237329db4a1.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ bf43a465-b25b-4782-9d01-d237329db4a1.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from bf43a465-b25b-4782-9d01-d237329db4a1.cpp:25: bf43a465-b25b-4782-9d01-d237329db4a1.cpp: In function ‘int main()’: bf43a465-b25b-4782-9d01-d237329db4a1.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -2,20 +2,20 @@
 #include <algorithm>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size()!=b.size()) return false;
-    for(int i=0;i<a.size();i++){
-        if(a[i]!=b[i]) return false;
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
 
-std::vector<int> sort_array(std::vector<int>& arr) {
+std::vector<int> sort_array(int num) {
+    std::vector<int> arr({num});
     std::sort(arr.begin(), arr.end());
     return arr;
 }
 
 int main() {
-    std::vector<int> test = {2, 4, 8, 16, 32};
-    assert(issame(sort_array(test), test));
+    assert(issame(sort_array(2, 4, 8, 16, 32), {2, 4, 8, 16, 32}));
     return 0;
 }
