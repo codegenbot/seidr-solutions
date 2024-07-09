35c758d14fef9668d6c5296528aa4cbf981e24c3
beep boop <githubbot@vadim.me>
2024-07-09 21:32:59 +0200

    Solution 11 of CPP/83, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/i13ngl1d Prompt: 63c9e9b1-cfb3-4239-a724-272c50bf55bc.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ 63c9e9b1-cfb3-4239-a724-272c50bf55bc.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -1,4 +1,4 @@
-```cpp
+```c++
 #include <iostream>
 
 using namespace std;
@@ -20,4 +20,3 @@ int main() {
     cin >> n;
     cout << "The count of the numbers of " << n << "-digit positive integers that start or end with 1 is: " << starts_one_ends(n) << endl;
     return 0;
-}
