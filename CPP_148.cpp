d382d5317316e38fc5165cf6cc00abbf5dba61bb
beep boop <githubbot@vadim.me>
2024-07-09 02:33:18 +0200

    Solution 29 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/yzjvcls6 Prompt:       |           ^~~~ b938a7d1-2940-414f-b2b1-7ba949af06ca.cpp:64:21: error: request for member ‘size’  in ‘b’, which is of non-class type ‘int’    64 |     if (a.size()!=b.size()) return false;       |                     ^~~~ b938a7d1-2940-414f-b2b1-7ba949af06ca.cpp:65:22: error: request for member ‘size’  in ‘a’, which is of non-class type ‘int’    65 |     for (int i=0;i<a.size();i++)       |                      ^~~~ b938a7d1-2940-414f-b2b1-7ba949af06ca.cpp:67:10: error: invalid types ‘int[int]’ for array subscript    67 |     if (a[i]!=b[i]) return false;       |          ^ b938a7d1-2940-414f-b2b1-7ba949af06ca.cpp:67:16: error: invalid types ‘int[int]’ for array subscript    67 |     if (a[i]!=b[i]) return false;       |                ^ In file included from b938a7d1-2940-414f-b2b1-7ba949af06ca.cpp:62: b938a7d1-2940-414f-b2b1-7ba949af06ca.cpp: In function ‘int main()’: b938a7d1-2940-414f-b2b1-7ba949af06ca.cpp:72:22: error: ‘bf’ cannot be used as a function    72 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -5,11 +5,17 @@ using namespace std;
 vector<string> bf(string planet1, string planet2) {
     vector<string> result;
     
+    int mercury = 57.9, venus = 67.2, earth = 0, mars = 225, 
+        jupiter = 778, saturn = 1433, uranus = 2870, neptune = 4495;
+
     string planets[] = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+    
     sort({planet1, planet2});
     
-    if (planet1 != "Mercury" && planet2 != "Mercury")
+    if (planet1 != "Mercury" && planet2 != "Mercury") {
         result.push_back(planet1);
+        result.push_back(planet2);
+    }
     else if ((planet1 == "Mercury" || planet2 == "Mercury")) {
         if (planet1 == "Mercury")
             result.push_back(planet1);
