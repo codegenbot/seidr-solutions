5d1ce43bd15478707f2b97b0c18c5a27ed6dec10
beep boop <githubbot@vadim.me>
2024-07-10 01:57:43 +0200

    Solution 41 of CPP/23, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/i7d6tqpk Prompt: 6cd6fed9-f99c-43df-9850-dd5e1ae11456.cpp:15:5: error: redefinition of ‘int main( )’    15 | int main(){       |     ^~~~ 6cd6fed9-f99c-43df-9850-dd5e1ae11456.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,3 +1,7 @@
-int strlen(string str){
-    return str.length();
+#include <cassert>
+#include <string>
+#include <cstring>
+
+int myStringLen(const char* str) {
+    return std::string(str).length();
 }
