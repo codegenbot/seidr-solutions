842afd7ceee68ec906904022770a704905b0821f
beep boop <githubbot@vadim.me>
2024-07-09 02:17:10 +0200

    Solution 10 of CPP/142, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/1a0qj9ht Prompt:       |                 ^~~~~~ 602cd588-84f9-42c3-b8c9-885b67c879a2.cpp:4:17: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 602cd588-84f9-42c3-b8c9-885b67c879a2.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 602cd588-84f9-42c3-b8c9-885b67c879a2.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 602cd588-84f9-42c3-b8c9-885b67c879a2.cpp:4:24: error: expected primary-expressio n before ‘int’     4 | int sum_squares(vector<int> lst) {       |                        ^~~ In file included from 602cd588-84f9-42c3-b8c9-885b67c879a2.cpp:16: 602cd588-84f9-42c3-b8c9-885b67c879a2.cpp: In function ‘int main()’: 602cd588-84f9-42c3-b8c9-885b67c879a2.cpp:18:24: error: ‘sum_squares’ cannot be u sed as a function    18 |     assert (sum_squares({-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 14, -14, 6, 13, 11, 16, 16, 4, 10}) == -1448);       |             ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -1,10 +1,13 @@
-int sum_squares(vector<int> lst) {
+#include <vector>
+#include <cmath>
+
+int sum_squares(std::vector<int> lst) {
     int result = 0;
     for (int i = 0; i < lst.size(); i++) {
         if (i % 3 == 0 && i % 4 != 0) {
             result += lst[i] * lst[i];
         } else if (i % 4 == 0 && i % 3 != 0) {
-            result += pow(lst[i], 3);
+            result += std::pow(lst[i], 3);
         }
     }
     return result;
