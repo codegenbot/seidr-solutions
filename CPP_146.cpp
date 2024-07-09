91757bb4dc223cb4eca3b0f05daf912109d94aa6
beep boop <githubbot@vadim.me>
2024-07-09 11:08:53 +0200

    Solution 13 of CPP/146, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7ujc9dvb Prompt: Complete the following code given the task description and function signature.

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,9 +1,13 @@
-int specialFilter(vector<int> nums) {
+#include<stdio.h>
+#include<vector>
+#include<string>
+using namespace std;
+
+int specialFilter(vector<int> nums){
     int count = 0;
-    for (int num : nums) {
-        if (abs(num) > 10 && (num % 10) % 2 != 0 && ((num / 10) % 10) % 2 != 0) {
+    for(int num : nums){
+        if(abs(num) > 10 && (abs(num)%10)%2!=0 && ((abs(num)/10)%10)%2!=0)
             count++;
-        }
     }
     return count;
 }
