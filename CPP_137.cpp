06d2c07027327f9d34d02b04d36594280786017d
beep boop <githubbot@vadim.me>
2024-07-10 00:44:30 +0200

    Solution 39 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fhsevhcx Prompt: 3d3b714b-9956-4596-9038-b13f5da2dcd2.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,18 +1,18 @@
 #include <boost/any.hpp>
-#include <boost/type_index.hpp>
+#include <boost/type_traits.hpp>
 
 using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_same<boost::type_index::type<void>, get_type(a)>() && is_same<boost::type_index::type<int>, get_type(b)>()) {
-        return any_cast<int>(a) > any_cast<int>(b) ? a : b;
-    } else if (is_same<boost::type_index::type<std::string>, get_type(a)>() && is_same<boost::type_index::type<std::string>, get_type(b)>()) {
-        std::string str1 = any_cast<std::string>(a);
-        std::string str2 = any_cast<std::string>(b);
+    if (is_same<any_tag, get_type(a)>() && is_same<any_tag, get_type(b)>()) {
+        return a > b ? a : b;
+    } else if (is_same<string_any_tag, get_type(a)>() && is_same<string_any_tag, get_type(b)>()) {
+        string str1 = any_cast<string>(a);
+        string str2 = any_cast<string>(b);
         return str1 > str2 ? a : b;
-    } else if (is_same<boost::type_index::type<std::string>, get_type(a)>() || is_same<boost::type_index::type<std::string>, get_type(b)>()) {
-        std::string str1 = any_cast<std::string>(a);
-        std::string str2 = any_cast<std::string>(b);
+    } else if (is_same<string_any_tag, get_type(a)>() || is_same<string_any_tag, get_type(b)>()) {
+        string str1 = any_cast<string>(a);
+        string str2 = any_cast<string>(b);
         if (str1 == str2) {
             return boost::any("None");
         } else if (str1 > str2) {
@@ -21,6 +21,6 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         }
     } else {
-        throw std::invalid_argument("Invalid types");
+        throw invalid_argument("Invalid types");
     }
 }
