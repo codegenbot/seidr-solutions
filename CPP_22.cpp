4dc5f4a9578c431a2602b739ad2833bd23980dc1
beep boop <githubbot@vadim.me>
2024-07-09 12:49:05 +0200

    Solution 5 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/14drhf0m Prompt: ed81ae7b-9b9a-4d5b-bfed-90c601547810.cpp:31:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    31 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ ed81ae7b-9b9a-4d5b-bfed-90c601547810.cpp:21:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    21 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ ed81ae7b-9b9a-4d5b-bfed-90c601547810.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ ed81ae7b-9b9a-4d5b-bfed-90c601547810.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from ed81ae7b-9b9a-4d5b-bfed-90c601547810.cpp:30: ed81ae7b-9b9a-4d5b-bfed-90c601547810.cpp: In function ‘int main()’: ed81ae7b-9b9a-4d5b-bfed-90c601547810.cpp:40:35: error: no match for call to ‘(st d::vector<int>) (<brace-enclosed initializer list>)’    40 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,25 +1,17 @@
-#define BOOST_ALL_DYN_LINK 1
-#include <boost/config.hpp>
-#include <boost/any.hpp>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
 
 using namespace std;
-namespace boost {
-namespace any {
-class bad_any_cast : public bad_cast { };
-template<class T> struct any_cast {
-    const any& a;
-    explicit any_cast(any& a) : a(a) {}
-    operator T() const { if (!a.good()) throw bad_any_cast(); return any_cast<T>(a).get();
-};
-} // namespace any
-} // namespace boost
 
 vector<int> filter_integers(list<boost::any> values) {
     vector<int> result;
     for (const auto& value : values) {
         try {
             int val = boost::any_cast<int>(value);
-            result.push_back(val);
+            if(find(result.begin(), result.end(), val) == result.end())
+                result.push_back(val);
         } catch (...) {
             // ignore non-integer values
         }
@@ -27,11 +19,6 @@ vector<int> filter_integers(list<boost::any> values) {
     return result;
 }
 
-bool issame(vector<int> a,vector<int>b){
+bool issame(vector<int> a, vector<int> b) {
     return (a.size() == b.size()) && equal(a.begin(), a.end(), b.begin());
-}
-
-int main() {
-    assert(issame(filter_integers({3, boost::any("c"), 3, 3, boost::any("a"), boost::any("b")}) ,{3, 3, 3}));
-    return 0;
 }
