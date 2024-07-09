27bfade8c1b7671bbc8c23f58d171f0567ab61cb
beep boop <githubbot@vadim.me>
2024-07-09 17:09:20 +0200

    Solution 36 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/2505ch6v Prompt:       |                              ^~~~~~ c74623a0-173e-4066-9597-b254af3e9e77.cpp:22:37: error: ‘string’ was not declared  in this scope    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c74623a0-173e-4066-9597-b254af3e9e77.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c74623a0-173e-4066-9597-b254af3e9e77.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c74623a0-173e-4066-9597-b254af3e9e77.cpp:21: c74623a0-173e-4066-9597-b254af3e9e77.cpp: In function ‘int main()’: c74623a0-173e-4066-9597-b254af3e9e77.cpp:31:20: error: ‘numerical_letter_grade’ was not declared in this scope    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ c74623a0-173e-4066-9597-b254af3e9e77.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -2,11 +2,7 @@
 #include <string>
 
 bool issame(const vector<string> &a, const vector<string> &b){
-    if(a.size() != b.size()) return false;
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]) return false;
-    }
-    return true;
+    return a == b;
 }
 
 vector<string> numerical_letter_grade(vector<float> grades){
