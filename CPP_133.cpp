6cb101d314d32496f25ac6accbb27498764ce49f
beep boop <githubbot@vadim.me>
2024-07-09 19:41:31 +0200

    Solution 6 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/llf1ay19 Prompt: af3ee21c-1bab-4616-a6ba-cce2d38e84d9.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the solution:       | ^~~~ In file included from af3ee21c-1bab-4616-a6ba-cce2d38e84d9.cpp:12: af3ee21c-1bab-4616-a6ba-cce2d38e84d9.cpp: In function ‘int main()’: af3ee21c-1bab-4616-a6ba-cce2d38e84d9.cpp:14:13: error: ‘sum_squares’ was not dec lared in this scope    14 |     assert (sum_squares({-1,1,0})==2);       |             ^~~~~~~~~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,12 +1,19 @@
 #include <vector>
 #include <cmath>
+#include <assert.h>
+
+using namespace std;
 
 int sum_squares(vector<float> lst) {
-    int sum = 0;
-    
-    for(float num : lst){
-        sum += ceil(num) * ceil(num);
+    int total = 0;
+    for (float num : lst) {
+        double roundedNum = ceil(num);
+        total += pow(roundedNum, 2);
     }
-    
-    return sum;
+    return total;
+}
+
+int main() {
+    assert(sum_squares({-1,1,0})==2);
+    return 0;
 }
