f621305312ead98e773c57d99c2230d2bfa38f87
beep boop <githubbot@vadim.me>
2024-07-09 19:04:22 +0200

    Solution 51 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g4d79e6v Prompt: ebdbbda4-ee85-42bf-b57b-a4d7d9368d5a.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ ebdbbda4-ee85-42bf-b57b-a4d7d9368d5a.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from ebdbbda4-ee85-42bf-b57b-a4d7d9368d5a.cpp:33: ebdbbda4-ee85-42bf-b57b-a4d7d9368d5a.cpp: In function ‘int main()’: ebdbbda4-ee85-42bf-b57b-a4d7d9368d5a.cpp:35:22: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    35 |     assert (is_happy("iopaxioi") == false );       |                      ^~~~~~~~~~       |                      |       |                      const char* ebdbbda4-ee85-42bf-b57b-a4d7d9368d5a.cpp:6:19: note:   initializing argument 1 o f ‘bool is_happy(int)’     6 | bool is_happy(int n) {       |               ~~~~^

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -4,8 +4,8 @@
 bool is_happy(int n);
 
 int main() {
-    int num;
     std::cout << "Enter a number: ";
+    int num;
     std::cin >> num;  
     if (is_happy(num)) {
         std::cout << num << " is a happy number.\n";
