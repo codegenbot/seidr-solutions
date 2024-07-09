496eaaa71d635e9c78da9c0a5476b9824e7d0642
beep boop <githubbot@vadim.me>
2024-07-09 09:16:33 +0200

    Solution 94 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/3b0ln6hy Prompt:    64 | bool issame(vector<float> a,vector<float>b){       |                                           ^ 321baec9-522d-4c60-9e5a-f55e7b176986.cpp:72:5: error: redefinition of ‘int main( )’    72 | int main(){       |     ^~~~ 321baec9-522d-4c60-9e5a-f55e7b176986.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from 321baec9-522d-4c60-9e5a-f55e7b176986.cpp:63: 321baec9-522d-4c60-9e5a-f55e7b176986.cpp: In function ‘int main()’: 321baec9-522d-4c60-9e5a-f55e7b176986.cpp:73:20: error: ‘sort_even’ was not decla red in this scope; did you mean ‘sortEven’?    73 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 321baec9-522d-4c60-9e5a-f55e7b176986.cpp:73:19: error: ‘issame’ cannot be used a s a function    73 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <iostream>
 
-bool isSame(std::vector<float> a, std::vector<float> b) {
+bool areEqual(std::vector<float> a, std::vector<float> b) {
     return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
 }
 
@@ -56,6 +56,6 @@ int main() {
     }
     std::sort(expected.begin(), expected.end());
     
-    bool same = isSame(result, expected);
+    bool areEqual = areEqual(result, expected);
     return 0;
 }
