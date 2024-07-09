32a1c2c4c952e1b8968237b4d781022a14f11101
beep boop <githubbot@vadim.me>
2024-07-09 03:29:32 +0200

    Solution 35 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jvy931jd Prompt:       |                    ^~~~~ 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:11: 5d830cd4-1162-4af1-ae79-c42923590cad.cpp: In function ‘int main()’: 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,17 +1,43 @@
-```cpp
 #include <vector>
-#include <algorithm>
+#include <iostream>
 
 bool isSame(vector<float> a, vector<float> b) {
-    return (a == b);
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
+            return false;
+    }
+    return true;
 }
 
 int main() {
-    assert(isSame(get_positive({}), {}));
-    // ...
+    vector<float> l1 = { -1.0f, 2.0f, -3.0f };
+    vector<float> l2 = { -4.0f, 5.0f, -6.0f };
+    vector<float> result1 = get_positive(l1);
+    vector<float> result2 = get_positive(l2);
+
+    if (isSame(result1, vector<float>())) {
+        std::cout << "result1 is empty" << std::endl;
+    }
+    else
+        for (float i : result1)
+            std::cout << i << " ";
+    
+    std::cout << std::endl;
+
+    if (isSame(result2, vector<float>())) {
+        std::cout << "result2 is empty" << std::endl;
+    }
+    else
+        for (float i : result2)
+            std::cout << i << " ";
+    
+    std::cout << std::endl;
+    return 0;
 }
 
-vector<float> get_positive(vector<float> l){
+vector<float> get_positive(vector<float> l) {
     vector<float> result;
     for(float i : l){
         if(i > 0) {
