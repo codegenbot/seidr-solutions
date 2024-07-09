5be9dfc758c11300025e0bc11d83424bbd35de6f
beep boop <githubbot@vadim.me>
2024-07-09 15:18:22 +0200

    Solution 16 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4eb0byil Prompt:       |                    ^~~ 8db9e16b-9556-4023-90ac-1fd37c3fdcc5.cpp:6:27: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8db9e16b-9556-4023-90ac-1fd37c3fdcc5.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8db9e16b-9556-4023-90ac-1fd37c3fdcc5.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8db9e16b-9556-4023-90ac-1fd37c3fdcc5.cpp:5: 8db9e16b-9556-4023-90ac-1fd37c3fdcc5.cpp: In function ‘int main()’: 8db9e16b-9556-4023-90ac-1fd37c3fdcc5.cpp:15:20: error: ‘common’ was not declared  in this scope    15 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 8db9e16b-9556-4023-90ac-1fd37c3fdcc5.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,15 +1,23 @@
-#include <vector>
+```cpp
+#include <iostream>
 #include <set>
+#include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(int a, int b) {
     return a == b;
 }
 
 int main() {
-    std::vector<int> l1 = {1,2,3};
-    std::vector<int> v2 = {4,5,6};
+    std::vector<int> l1 = {1, 2, 3};
+    std::vector<int> v2 = {4, 5, 6};
+
     std::set<int> common_result;
-    common_result = set_intersection(l1.begin(), l1.end(), v2.begin(), v2.end(),
-        inserter(common_result, common_result.begin()));
+
+    common_result = std::set<int>(std::set_intersection(l1.begin(), l1.end(), v2.begin(), v2.end(),
+        std::inserter(common_result, common_result.begin())));
+
+    for (int i : common_result) {
+        std::cout << i << " ";
+    }
     return 0;
 }
