cff5e5937b16fe438041a119ca4a2c6ef1f47541
beep boop <githubbot@vadim.me>
2024-07-09 02:30:34 +0200

    Solution 1 of CPP/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/p03v9iz5 Prompt: Complete the following code given the task description and function signature.

--- CPP_3.cpp
+++ CPP_3.cpp
@@ -1,7 +1,7 @@
 bool below_zero(vector<int> operations){
     int balance = 0;
-    for(int i=0; i<operations.size(); i++){
-        balance += operations[i];
+    for(int operation : operations){
+        balance += operation;
         if(balance < 0) return true;
     }
     return false;
