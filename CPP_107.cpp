b0c42e293676baed497c58d1728ac2860f541e82
beep boop <githubbot@vadim.me>
2024-07-09 18:18:30 +0200

    Solution 80 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/6w4aoj8e Prompt:       |                    ^~~ cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp:18: cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp: In function ‘int main()’: cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp:28:20: error: ‘even_odd_palindrome’ was  not declared in this scope    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,30 +1,18 @@
-#include <vector> // Include the necessary header file
+#include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b); // Function prototype declaration
-
-std::vector<int> even_odd_palindrome(int n) {
-    std::vector<int> result = {0, 0};
+vector<int> even_odd_palindrome(int n) {
+    vector<int> res(2, 0);
     for (int i = 1; i <= n; ++i) {
-        std::string num = std::to_string(i);
-        std::string rev = num;
-        std::reverse(rev.begin(), rev.end());
-        if (num == rev) {
-            if ((num.back() - '0') % 2 == 0) {
-                result[0]++;
+        string s = to_string(i);
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        if (s == rev) {
+            if (i % 2 == 0) {
+                res[0]++;
             } else {
-                result[1]++;
+                res[1]++;
             }
         }
     }
-    return result;
-}
-
-int main() {
-    // Your code here
-
-    assert(issame(even_odd_palindrome(1), {0, 1})); // Correct function call
-}
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    // Your function implementation
+    return res;
 }
