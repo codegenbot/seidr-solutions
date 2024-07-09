6fc077c9b576f5b81d9f8cf1f51678c5d60b1ae2
beep boop <githubbot@vadim.me>
2024-07-09 13:36:41 +0200

    Solution 25 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ujx0k24n Prompt: /usr/include/c++/11/bits/stl_uninitialized.h:704:44:   required from ‘_ForwardIt erator std::__uninitialized_default_n_a(_ForwardIterator, _Size, std::allocator< _Tp>&) [with _ForwardIterator = double*; _Size = long unsigned int; _Tp = double ]’ /usr/include/c++/11/bits/vector.tcc:627:35:   required from ‘void std::vector<_T p, _Alloc>::_M_default_append(std::vector<_Tp, _Alloc>::size_type) [with _Tp = d ouble; _Alloc = std::allocator<double>; std::vector<_Tp, _Alloc>::size_type = lo ng unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:940:4:   required from ‘void std::vector<_ Tp, _Alloc>::resize(std::vector<_Tp, _Alloc>::size_type) [with _Tp = double; _Al loc = std::allocator<double>; std::vector<_Tp, _Alloc>::size_type = long unsigne d int]’ 27fffcc8-0e73-4c25-b5f0-3b67aec71d29.cpp:29:18:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -26,6 +26,7 @@ int main() {
     std::cin >> n;
 
     std::vector<double> coeffs(n);
+
     for (int i = 0; i < n; i++) {
         double coeff;
         std::cin >> coeff;
@@ -36,11 +37,11 @@ int main() {
     std::cin >> x;
     
     // Call poly function with input values
-    double sum = poly(coeffs, x);
+    double sum = poly({coeffs[0], coeffs[1], coeffs[2]}, x);
     std::cout << "The polynomial evaluation at x = " << x << " is: " << sum << std::endl;
 
     // Calculate the root using find_zero function
-    double solution = find_zero(coeffs);
+    double solution = find_zero({coeffs[0], coeffs[1], coeffs[2]});
 
     return 0;
 }
