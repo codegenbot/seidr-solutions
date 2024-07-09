811a252405396541c551f979d9eb7995afe138f3
beep boop <githubbot@vadim.me>
2024-07-09 20:48:41 +0200

    Solution 5 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/jy7fbynr Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4cfc911f-be39-412f-841f-e7ee425fb508.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4cfc911f-be39-412f-841f-e7ee425fb508.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4cfc911f-be39-412f-841f-e7ee425fb508.cpp:10: 4cfc911f-be39-412f-841f-e7ee425fb508.cpp: In function ‘int main()’: 4cfc911f-be39-412f-841f-e7ee425fb508.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 4cfc911f-be39-412f-841f-e7ee425fb508.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,9 +1,10 @@
+```cpp
 bool issame(vector<string> a, vector<string>b) {
     if(a.size() != b.size()) {
         return false;
     }
     for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
+        if(find(b.begin(), b.end(), a[i]) == b.end()) {
             return false;
         }
     }
