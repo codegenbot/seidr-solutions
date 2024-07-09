cb232705d30cb8471d21fbd51bd3062da34200dd
beep boop <githubbot@vadim.me>
2024-07-09 20:54:32 +0200

    Solution 54 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/owmm65gu Prompt: In file included from b5487d4e-076b-4bbc-b7b2-a7ec2e9f1b80.cpp:3: b5487d4e-076b-4bbc-b7b2-a7ec2e9f1b80.cpp: In function ‘int main()’: b5487d4e-076b-4bbc-b7b2-a7ec2e9f1b80.cpp:5:13: error: ‘int_to_mini_romank’ was n ot declared in this scope     5 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,22 +1,13 @@
 ```cpp
 const char* roman[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-int int_to_mini_romank(int n) {
-    static const int value[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    static const char* romanl[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-
-    int i;
-    for (i = 0; n > 0; ++i) {
-        if (n >= value[i]) {
-            n -= value[i];
-            printf("%s", romanl[i]);
-        } else
-            break;
+string int_to_mini_roman(int num) {
+    string result;
+    for (int i = 0; i < 13; ++i) {
+        while (num >= roman[i][1] - '0' * (roman[i][1] == 'X' || roman[i][1] == 'V')) {
+            result += roman[i];
+            num -= roman[i][1] - '0';
+        }
     }
-    return 0;
-}
-
-int main() {
-    assert(int_to_mini_romank(1000) == "M"); 
-    return 0;
+    return result;
 }
