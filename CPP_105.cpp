1e268902c208fe0e447389cd8089aacfe90fffc7
beep boop <githubbot@vadim.me>
2024-07-09 06:53:32 +0200

    Solution 94 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/gekczo0y Prompt:       |                              ^~~~~~ c81d0175-f4ef-4281-944f-287c54240664.cpp:44:37: error: ‘string’ was not declared  in this scope    44 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c81d0175-f4ef-4281-944f-287c54240664.cpp:44:44: error: ‘b’ was not declared in t his scope    44 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c81d0175-f4ef-4281-944f-287c54240664.cpp:44:45: error: expression list treated a s compound expression in initializer [-fpermissive]    44 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c81d0175-f4ef-4281-944f-287c54240664.cpp:43: c81d0175-f4ef-4281-944f-287c54240664.cpp: In function ‘int main()’: c81d0175-f4ef-4281-944f-287c54240664.cpp:53:20: error: ‘by_length’ was not decla red in this scope    53 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ c81d0175-f4ef-4281-944f-287c54240664.cpp:53:19: error: ‘issame’ cannot be used a s a function    53 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,8 +1,7 @@
 #include <vector>
 #include <algorithm>
-using namespace std;
 
-bool issame(vector<string> a, vector<string>b){
+bool issame(vector<string> a,vector<string>b){
     return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
 }
 
