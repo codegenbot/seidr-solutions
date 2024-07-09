29025eebae639a6c5bcbc345d01a782e7a45501f
beep boop <githubbot@vadim.me>
2024-07-09 20:42:59 +0200

    Solution 4 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/v0dpr46q Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ae92b266-c947-4d58-9282-2cee3d6f9227.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ae92b266-c947-4d58-9282-2cee3d6f9227.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ae92b266-c947-4d58-9282-2cee3d6f9227.cpp:11: ae92b266-c947-4d58-9282-2cee3d6f9227.cpp: In function ‘int main()’: ae92b266-c947-4d58-9282-2cee3d6f9227.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ ae92b266-c947-4d58-9282-2cee3d6f9227.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,8 +1,29 @@
-vector<int> filter_integers(list_any values) {
+#include <iostream>
+#include <vector>
+#include <list>
+#include <any>
+#include <algorithm>
+
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
+    return a.size() == b.size() && all_of(a.begin(), a.end(), [&b](int x) {return find(b.begin(), b.end(), x) != b.end();});
+}
+
+vector<int> filter_integers(list<any> values) {
     vector<int> result;
     for (const auto& value : values) {
-        if (boost::any_cast<int>(value).good()) {
-            result.push_back(boost::any_cast<int>(value));
+        if (any_cast<int>(value).type() == typeid(int)) {
+            int val = any_cast<int>(value);
+            bool same = false;
+            for (int num : result) {
+                if (issame({num}, {val})) {
+                    same = true;
+                    break;
+                }
+            }
+            if (!same)
+                result.push_back(val);
         }
     }
     return result;
