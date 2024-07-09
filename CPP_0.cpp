8f86d61d6e73dc0eac9d1a189b8d225c80465fc2
beep boop <githubbot@vadim.me>
2024-07-09 07:37:23 +0200

    Solution 37 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/i5yuyk8j Prompt: 7a2214fe-2fc3-4944-9d48-5597d36a9217.cpp: In function ‘int main()’: 7a2214fe-2fc3-4944-9d48-5597d36a9217.cpp:18:5: error: ‘assert’ was not declared in this scope    18 |     assert(has_close_elements(numbers, 0.5) == false);       |     ^~~~~~ 7a2214fe-2fc3-4944-9d48-5597d36a9217.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | using namespace std; 7a2214fe-2fc3-4944-9d48-5597d36a9217.cpp: At global scope: 7a2214fe-2fc3-4944-9d48-5597d36a9217.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 7a2214fe-2fc3-4944-9d48-5597d36a9217.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,4 +1,20 @@
-int main{
+#include <iostream>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+bool has_close_elements(vector<float> a, float tol) {
+    for (int i = 0; i < a.size() - 1; i++) {
+        for (int j = i + 1; j < a.size(); j++) {
+            if (abs(a[i] - a[j]) <= tol) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
     vector<float> numbers = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f, 0};
     assert(has_close_elements(numbers, 0.5) == false);
     
