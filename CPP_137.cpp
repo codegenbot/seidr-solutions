fd2835131b8ecd70bee36db90529fa0eb5007da9
beep boop <githubbot@vadim.me>
2024-07-09 16:03:21 +0200

    Solution 56 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/40ydl3g4 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,23 +1,31 @@
+#include <string>
+#include <algorithm>
+#include <boost/any.hpp>
+#include <boost/lexical_cast.hpp>
+
+using namespace std;
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int x = boost::get<int>(a);
-        int y = boost::get<int>(b);
-        return (x > y) ? a : ((x < y) ? b : boost::any("None"));
-    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        float x = boost::get<float>(a);
-        float y = boost::get<float>(b);
-        return (x > y) ? a : ((x < y) ? b : boost::any("None"));
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return boost::any(b);
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return boost::any(a);
     } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string x = boost::get<string>(a);
-        string y = boost::get<string>(b);
-        return (stof(x) > stof(y)) ? a : ((stof(x) < stof(y)) ? b : boost::any("None"));
-    } else if (a.type() == typeid(string) && b.type() != typeid(string)) {
-        string x = boost::get<string>(a);
-        return (stof(x) > boost::get<float>(b)) ? a : ((stof(x) < boost::get<float>(b)) ? b : boost::any("None"));
-    } else if (b.type() == typeid(string) && a.type() != typeid(string)) {
-        string y = boost::get<string>(b);
-        return (stof(y) > boost::get<float>(a)) ? b : ((stof(y) < boost::get<float>(a)) ? a : boost::any("None"));
+        string strA = boost::any_cast<string>(a);
+        string strB = boost::any_cast<string>(b);
+        if (stod(strA) > stod(strB))
+            return a;
+        else if (stod(strA) < stod(strB))
+            return b;
+        else
+            return boost::any("None");
     } else {
-        return boost::any("None");
+        double numA = boost::any_cast<double>(a);
+        double numB = boost::any_cast<double>(b);
+        if (numA > numB)
+            return a;
+        else if (numA < numB)
+            return b;
+        else
+            return boost::any("None");
     }
 }
