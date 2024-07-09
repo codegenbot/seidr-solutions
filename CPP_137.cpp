4a4951edbdc0d1219448bf39767249b94a22aec9
beep boop <githubbot@vadim.me>
2024-07-09 15:58:46 +0200

    Solution 43 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/40ydl3g4 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,38 +1,46 @@
-#include <iostream>
-#include <string>
-#include <boost/any.hpp>
-
-using namespace std;
-
+```
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        int ai = boost::any_cast<int>(a);
-        float bf = boost::any_cast<float>(b);
-        return bf > (float)ai ? b : a;
-    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        float af = boost::any_cast<float>(a);
-        int bi = boost::any_cast<int>(b);
-        return af > (float)bi ? a : b;
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string as = boost::any_cast<string>(a);
-        string bs = boost::any_cast<string>(b);
-        return as.compare(bs) > 0 ? a : b;
+        return boost::any(b);
+    } else if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return boost::any(b);
     } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int ai = boost::any_cast<int>(a);
-        string bs = boost::any_cast<string>(b);
-        return stoi(bs) > ai ? b : a;
+        if ((boost::any_cast<string>(b)).find(',') != string::npos || (boost::any_cast<string>(b)).find('.') != string::npos) {
+            return b;
+        } else {
+            return boost::any("None");
+        }
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return a;
+    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return a;
     } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        string as = boost::any_cast<string>(a);
-        int bi = boost::any_cast<int>(b);
-        return stoi(as) > bi ? a : b;
-    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        float af = boost::any_cast<float>(a);
-        string bs = boost::any_cast<string>(b);
-        return stof(bs) > af ? b : a;
+        if ((boost::any_cast<string>(a)).find(',') != string::npos || (boost::any_cast<string>(a)).find('.') != string::npos) {
+            return a;
+        } else {
+            return boost::any("None");
+        }
     } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        string as = boost::any_cast<string>(a);
-        float bf = boost::any_cast<float>(b);
-        return stof(as) > bf ? a : b;
+        if ((boost::any_cast<string>(b)).find(',') != string::npos || (boost::any_cast<string>(b)).find('.') != string::npos) {
+            return boost::any("None");
+        } else {
+            return a;
+        }
+    } else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        if ((boost::any_cast<string>(a)).find(',') != string::npos || (boost::any_cast<string>(a)).find('.') != string::npos) {
+            return boost::any("None");
+        } else {
+            return a;
+        }
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if ((boost::any_cast<string>(a)).compare((boost::any_cast<string>(b))) > 0) {
+            return a;
+        } else if ((boost::any_cast<string>(a)).compare((boost::any_cast<string>(b)))) < 0) {
+            return b;
+        } else {
+            return boost::any("None");
+        }
+    } else {
+        return boost::any("None");
     }
-    return "None";
 }
