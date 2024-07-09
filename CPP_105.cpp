b8653d7c711624e498cead62b5df54edc6f044ab
beep boop <githubbot@vadim.me>
2024-07-09 12:52:58 +0200

    Solution 13 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/6g1exte2 Prompt:       |                              ^~~~~~ 4c5b11b0-1f7a-45de-b100-8e36eb9ff0a4.cpp:27:37: error: ‘string’ was not declared  in this scope    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4c5b11b0-1f7a-45de-b100-8e36eb9ff0a4.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4c5b11b0-1f7a-45de-b100-8e36eb9ff0a4.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4c5b11b0-1f7a-45de-b100-8e36eb9ff0a4.cpp:26: 4c5b11b0-1f7a-45de-b100-8e36eb9ff0a4.cpp: In function ‘int main()’: 4c5b11b0-1f7a-45de-b100-8e36eb9ff0a4.cpp:36:20: error: ‘by_length’ was not decla red in this scope    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 4c5b11b0-1f7a-45de-b100-8e36eb9ff0a4.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<string> a, vector<string>b){
+bool issame(vector<string> a, vector<string> b){
     if(a.size() != b.size())
         return false;
     for(int i=0; i<a.size(); i++){
@@ -12,39 +12,33 @@ bool issame(vector<string> a, vector<string>b){
     return true;
 }
 
-std::vector<std::string> by_length(std::vector<int> arr) {
-    std::vector<int> temp;
+vector<vector<string>> by_length(vector<int> arr) {
+    vector<string> temp;
     for (int i : arr) {
         if (i >= 1 && i <= 9)
-            temp.push_back(i);
+            temp.push_back(to_string(i));
     }
     sort(temp.begin(), temp.end());
     reverse(temp.begin(), temp.end());
-    std::vector<std::string> result;
-    for (int i : temp) {
-        switch (i) {
-            case 1: result.push_back("One"); break;
-            case 2: result.push_back("Two"); break;
-            case 3: result.push_back("Three"); break;
-            case 4: result.push_back("Four"); break;
-            case 5: result.push_back("Five"); break;
-            case 6: result.push_back("Six"); break;
-            case 7: result.push_back("Seven"); break;
-            case 8: result.push_back("Eight"); break;
-            case 9: result.push_back("Nine"); break;
-        }
+    vector<vector<string>> result;
+    for (int i = 0; i < temp.size(); i += 3) {
+        if(i+2 < temp.size())
+            result.push_back({temp[i], temp[i+1], temp[i+2]});
+        else
+            result.push_back({temp[i]});
     }
     return result;
 }
 
 int main() {
-    std::vector<int> numbers = {1, 2, 3, 4, 5};
-    std::vector<std::string> output = by_length(numbers);
+    vector<int> arr = {8,9,5,3,7,4};
+    vector<vector<string>> output = by_length(arr);
     
-    if(issame({output[0], output[1], output[2]}, {"One", "Two", "Three"}))
-        std::cout << "The numbers are in the correct order.\n";
-    else
-        std::cout << "The numbers are not in the correct order.\n";
+    for(auto i : output) {
+        for(auto j : i)
+            std::cout << j << " ";
+        std::cout << std::endl;
+    }
     
     return 0;
 }
