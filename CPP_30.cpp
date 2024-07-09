ddd450b9191752126ea636367879f23ac11f3673
beep boop <githubbot@vadim.me>
2024-07-09 07:35:03 +0200

    Solution 32 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/cjdhtx4b Prompt:       |                    ^~~~~ 29e2fa44-4382-4b14-8bb2-5b094f1bcdd9.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 29e2fa44-4382-4b14-8bb2-5b094f1bcdd9.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 29e2fa44-4382-4b14-8bb2-5b094f1bcdd9.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 29e2fa44-4382-4b14-8bb2-5b094f1bcdd9.cpp:11: 29e2fa44-4382-4b14-8bb2-5b094f1bcdd9.cpp: In function ‘int main()’: 29e2fa44-4382-4b14-8bb2-5b094f1bcdd9.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 29e2fa44-4382-4b14-8bb2-5b094f1bcdd9.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,11 +1,28 @@
 #include <vector>
-#include <cassert>
 
-bool issame(const std::vector<float>& a, const std::vector<float>& b){
-    return a == b;
+bool issame(std::vector<float> a, std::vector<float> b){
+    if(a.size() != b.size()) {
+        return false;
+    } else {
+        for(int i = 0; i < a.size(); i++){
+            if(a[i] != b[i]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+std::vector<float> get_positive(std::vector<float> l){
+    std::vector<float> positive_numbers;
+    for (float num : l) {
+        if (num > 0) {
+            positive_numbers.push_back(num);
+        }
+    }
+    return positive_numbers;
 }
 
 int main(){
-    assert (issame(get_positive({}) , {}));
-    return 0;
+    assert(issame(get_positive({}), {}));
 }
