77c988f756baf51d206fb8c849da357c3c9e23de
beep boop <githubbot@vadim.me>
2024-07-09 16:22:16 +0200

    Solution 99 of CPP/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/525nbi63 Prompt:       |   ^~~~~~~~ /usr/include/c++/11/system_error:408:37: note:   no known conversion for argumen t 1 from ‘std::string’ {aka ‘std::__cxx11::basic_string<char>’} to ‘const std::e rror_condition&’   408 |   operator==(const error_condition& __lhs, const error_code& __rhs) noex cept       |              ~~~~~~~~~~~~~~~~~~~~~~~^~~~~ In file included from /usr/include/c++/11/string:41,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 56407fc6-0af5-4c8a-8c38-88632855fa1a.cpp:1: /usr/include/c++/11/bits/allocator.h:204:7: note: candidate: ‘bool std::operator ==(const std::allocator<char>&, const std::allocator<char>&)’   204 |       operator==(const allocator&, const allocator&) _GLIBCXX_NOTHROW       |       ^~~~~~~~ /usr/include/c++/11/bits/allocator.h:204:18: note:   no known conversion for arg ument 1 from ‘std::string’ {aka ‘std::__cxx11::basic_string<char>’} to ‘const st d::allocator<char>&’   204 |       operator==(const allocator&, const allocator&) _GLIBCXX_NOTHROW       |                  ^~~~~~~~~~~~~~~~

--- CPP_39.cpp
+++ CPP_39.cpp
@@ -1,10 +1,30 @@
-std::string prime_fib(int num) {
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+string prime_fib(int num) {
     int a = 0, b = 1, fibNum = 0;
     while (true) {
         fibNum = a + b;
         a = b;
         b = fibNum;
         if (isPrime(fibNum)) {
-            if (fibNum > num) return std::to_string(fibNum);
+            if (fibNum > num) return to_string(fibNum);
         }
     }
+}
+
+void solveProblem() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "The first prime Fibonacci number greater than " << num << " is " << prime_fib(num) << endl;
+}
