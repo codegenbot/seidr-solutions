6362d3779d43b7f3a14e5ba5af08e4d26c2c5c48
beep boop <githubbot@vadim.me>
2024-07-09 18:36:48 +0200

    Solution 8 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/2rgfzmuz Prompt: 01fff74c-bf10-48ed-82d4-0aeaa0ac3c7f.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string solve(int N) {       | ^~~~~~ In file included from 01fff74c-bf10-48ed-82d4-0aeaa0ac3c7f.cpp:19: 01fff74c-bf10-48ed-82d4-0aeaa0ac3c7f.cpp: In function ‘int main()’: 01fff74c-bf10-48ed-82d4-0aeaa0ac3c7f.cpp:21:13: error: ‘solve’ was not declared in this scope    21 |     assert (solve(963) == "10010");       |             ^~~~~

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
 #include <string>
 
-std::string solve(int N) {
+string solve(int N) {
     int sum = 0;
     while (N > 0) {
         sum += N % 2;
         N /= 2;
     }
-    std::string result = "";
+    string result = "";
     while (sum > 0) {
         if (sum & 1)
             result.push_back('1');
@@ -17,3 +16,4 @@ std::string solve(int N) {
     }
     reverse(result.begin(), result.end());
     return result;
+}
