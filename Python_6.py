194db8d15fcc5461a9e7ee54bcd0d785554c7043
beep boop <githubbot@vadim.me>
2024-07-09 02:00:17 +0200

    Solution 37 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/xsmwblrg Prompt: Program did not receive expected input

--- Python_6.py
+++ Python_6.py
@@ -4,6 +4,6 @@ def parse_nested_parens(paren_string: str) -> List[int]:
     return [
         max([paren.count("(" * i) for i in range(1, len(paren) // 2 + 1)])
         for paren in paren_string.split()
-    ] 
+    ]
 
 parse_nested_parens("((())() () ())")
