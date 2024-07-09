c8ef256b532fdcaf438d9e3db45f2568ee5d94d6
beep boop <githubbot@vadim.me>
2024-07-09 17:50:34 +0200

    Solution 16 of CPP/99, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/mpq4a44g Prompt:       |              std d0d2b76d-682f-4871-99dd-ffbe694fd004.cpp:2:17: error: ‘floor’ was not declared i n this scope     2 |     int lower = floor(num);       |                 ^~~~~ d0d2b76d-682f-4871-99dd-ffbe694fd004.cpp:3:17: error: ‘ceil’ was not declared in  this scope     3 |     int upper = ceil(num);       |                 ^~~~ d0d2b76d-682f-4871-99dd-ffbe694fd004.cpp:4:5: error: expected unqualified-id bef ore ‘return’     4 |     return (num - lower < upper - num) ? lower : upper;       |     ^~~~~~ d0d2b76d-682f-4871-99dd-ffbe694fd004.cpp:5:1: error: expected declaration before  ‘}’ token     5 | }       | ^ In file included from d0d2b76d-682f-4871-99dd-ffbe694fd004.cpp:7: d0d2b76d-682f-4871-99dd-ffbe694fd004.cpp: In function ‘int main()’: d0d2b76d-682f-4871-99dd-ffbe694fd004.cpp:9:13: error: ‘closest_integer’ was not declared in this scope     9 |     assert (closest_integer("0") == 0);       |             ^~~~~~~~~~~~~~~

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,4 +1,9 @@
-double num = stod(value);
+#include <string>
+#include <cmath>
+using namespace std;
+
+int closest_integer(string value) {
+    double num = stod(value);
     int lower = floor(num);
     int upper = ceil(num);
     return (num - lower < upper - num) ? lower : upper;
