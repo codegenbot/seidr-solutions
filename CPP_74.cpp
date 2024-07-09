598bea426a20f7e3dc0db801a1b232eed30a1160
beep boop <githubbot@vadim.me>
2024-07-09 17:05:42 +0200

    Solution 48 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ok8wkyae Prompt: c23eae47-5035-4f93-a1e8-fe947c02ea0f.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c23eae47-5035-4f93-a1e8-fe947c02ea0f.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c23eae47-5035-4f93-a1e8-fe947c02ea0f.cpp:16: c23eae47-5035-4f93-a1e8-fe947c02ea0f.cpp: In function ‘int main()’: c23eae47-5035-4f93-a1e8-fe947c02ea0f.cpp:26:20: error: ‘total_match’ was not dec lared in this scope    26 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ c23eae47-5035-4f93-a1e8-fe947c02ea0f.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ c23eae47-5035-4f93-a1e8-fe947c02ea0f.cpp: In function ‘int total_chars(int)’: c23eae47-5035-4f93-a1e8-fe947c02ea0f.cpp:7:1: warning: control reaches end of no n-void function [-Wreturn-type]     7 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,35 +1,20 @@
-#include <string>
+bool issame(vector<string> a, vector<string> b) {
+    return total_chars(a) == total_chars(b);
+}
 
-bool issame(string s1, string s2) {
-    if (s1.size() != s2.size()) {
-        return false;
+int total_chars(const vector<string>& lst) {
+    int total = 0;
+    for (const string& str : lst) {
+        total += str.size();
     }
-    for (int i = 0; i < s1.size(); i++) {
-        if (s1[i] != s2[i]) {
-            return false;
-        }
-    }
-    return true;
+    return total;
 }
 
 vector<string> total_match(vector<string> lst1, vector<string> lst2);
 
-int sum_chars(vector<string> lst) {
-    int sum = 0;
-    for (string s : lst) {
-        sum += s.size();
-    }
-    return sum;
-}
-
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    if (sum_chars(lst1) < sum_chars(lst2)) {
+    if (total_chars(lst1) < total_chars(lst2)) {
         return lst1;
     }
     return lst2;
-}
-
-int main() {
-    // Main function code goes here
-    return 0;
 }
