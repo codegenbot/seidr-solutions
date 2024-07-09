f7a0903eba5f975c1ffd6b96c8d80c079117cfbc
beep boop <githubbot@vadim.me>
2024-07-09 05:10:53 +0200

    Solution 38 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/urrbvh7e Prompt: 0598ce3e-5a50-4f7e-8ab4-6265ac625ab5.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int num) {       | ^~~~~~ In file included from 0598ce3e-5a50-4f7e-8ab4-6265ac625ab5.cpp:80: 0598ce3e-5a50-4f7e-8ab4-6265ac625ab5.cpp: In function ‘int main()’: 0598ce3e-5a50-4f7e-8ab4-6265ac625ab5.cpp:82:13: error: ‘int_to_mini_romank’ was not declared in this scope    82 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,3 +1,4 @@
+```cpp
 string int_to_mini_roman(int num) {
     string roman[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",
                       "", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
@@ -30,25 +31,46 @@ string int_to_mini_roman(int num) {
             num -= 400;
             result += "CD";
         } else if (num >= 100) {
-            for(int i = 0; i < (num / 100); ++i) {
-                num -= 100;
+            for(int i = 0; (i * 100) <= num; ++i) {
+                num -= i * 100;
                 result += "C";
+                for(int j = 0; j < i - 1; ++j) {
+                    result += "X";
+                }
             }
         } else if (num >= 90) {
             num -= 90;
             result += "XC";
         } else if (num >= 50) {
-            num -= 50;
-            result += "L";
+            for(int i = 0; (i * 50) <= num; ++i) {
+                num -= i * 50;
+                result += "L";
+                for(int j = 0; j < i - 1; ++j) {
+                    result += "X";
+                }
+            }
         } else if (num >= 40) {
             num -= 40;
             result += "XL";
-        } else {
+        } else if (num >= 10) {
             for(int i = 0; ; ++i) {
                 if((i * 10) > num) break;
                 num -= i * 10;
                 result += "X";
                 for(int j = 0; j < i - 1; ++j) {
+                    result += "I";
+                }
+            }
+        } else {
+            while(num > 0) {
+                if(num >= 3) {
+                    num -= 3;
+                    result += "I";
+                } else if (num == 2) {
+                    result += "II";
+                    return result;
+                } else {
+                    num--;
                     result += "I";
                 }
             }
