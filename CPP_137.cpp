c65fa2f7ed0911a297e93bcaa7aaa5102b2fab90
beep boop <githubbot@vadim.me>
2024-07-09 05:09:39 +0200

    Solution 60 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/chasion3 Prompt: 226bbb03-f8a1-4aa7-bf98-3ddafe0181b1.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,21 +1,24 @@
-#include <boost/any.hpp>
+#include <iostream>
 #include <string>
+#include <boost/any.hpp>
+#include <boost/any_cast.hpp>
 
 using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_float(a) || is_float(b)) {
-        float fa = get<float>(a);
-        float fb = get<float>(b);
-        return boost::any(fa > fb ? get<boost::any>(a) : get<boost::any>(b));
-    } else if (is_string(a) && is_string(b)) {
-        string sa = get<string>(a);
-        string sb = get<string>(b);
-        float f1 = stof(sa), f2 = stof(sb);
-        if(f1 > f2) return a;
-        else if(f1 < f2) return b;
-        else return boost::any("None");
-    } else {
-        return boost::any("None");
+    if (is_any_of<float>(a) || is_any_of<float>(b)) {
+        float fa = boost::any_cast<float>(a);
+        float fb = boost::any_cast<float>(b);
+        return fa > fb ? a : b;
+    } else if (is_any_of<string>(a) && is_any_of<string>(b)) {
+        string sa = boost::any_cast<string>(a);
+        string sb = boost::any_cast<string>(b);
+        double da = stod(sa), db = stod(sb);
+        if(da > db)
+            return a;
+        else
+            return b;
+    } else if (!is_any_of<float>(a) && !is_any_of<float>(b)) {
+        return "None";
     }
 }
