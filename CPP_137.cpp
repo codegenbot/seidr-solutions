a40c140e96ea55436d0ca4f0c37d39728f03ad12
beep boop <githubbot@vadim.me>
2024-07-09 04:21:31 +0200

    Solution 19 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fv53j2kl Prompt: acb7e14f-56ce-48c2-9c84-c1fb943f77d6.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,32 +1,29 @@
 #include <boost/any.hpp>
-
 using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return boost::any(boost::any_cast<double>(b));
+        return b;
     }
     else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return boost::any(boost::any_cast<int>(a));
+        return a;
     }
-    else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        std::string str1 = boost::any_cast<std::string>(a);
-        std::string str2 = boost::any_cast<std::string>(b);
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = boost::any_cast<string>(a);
+        string str2 = boost::any_cast<string>(b);
         if (str1 > str2)
             return a;
         else if (str1 < str2)
             return b;
         else
-            return boost::any("None");
+            return any((string)"None");
     }
     else {
-        double d1 = boost::any_cast<double>(a);
-        double d2 = boost::any_cast<double>(b);
-        if (d1 > d2)
+        if (boost::any_cast<double>(a) > boost::any_cast<double>(b))
             return a;
-        else if (d1 < d2)
+        else if (boost::any_cast<double>(a) < boost::any_cast<double>(b))
             return b;
         else
-            return boost::any("None");
+            return any((string)"None");
     }
 }
