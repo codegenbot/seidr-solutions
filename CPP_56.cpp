6ae4fab4e4097392467426dcbc022757774a0f41
beep boop <githubbot@vadim.me>
2024-07-09 16:06:11 +0200

    Solution 44 of CPP/56, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5xo5ju7j Prompt:       |            ^ 0afffb40-5a89-4767-b8aa-008e3768b719.cpp:36:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    36 | int main(){       |         ^~ 0afffb40-5a89-4767-b8aa-008e3768b719.cpp:36:9: note: remove parentheses to defau lt-initialize a variable    36 | int main(){       |         ^~       |         -- 0afffb40-5a89-4767-b8aa-008e3768b719.cpp:36:9: note: or replace parentheses with  braces to value-initialize a variable 0afffb40-5a89-4767-b8aa-008e3768b719.cpp:36:11: error: a function-definition is not allowed here before ‘{’ token    36 | int main(){       |           ^ 0afffb40-5a89-4767-b8aa-008e3768b719.cpp:38:2: error: expected ‘}’ at end of inp ut    38 | }       |  ^ 0afffb40-5a89-4767-b8aa-008e3768b719.cpp:5:52: note: to match this ‘{’     5 | bool correct_bracketing(const char* bracket_string){       |                                                    ^

--- CPP_56.cpp
+++ CPP_56.cpp
@@ -1,8 +1,8 @@
-int main {
+```int main() {
     std::string bracket_string = "<><><<><>><>>><>";
     assert(!correct_bracketing(bracket_string.c_str()));
     std::string brackets = "<<>";
     if(correct_bracketing(brackets.c_str())) std::cout << "Correct" << std::endl;
     else std::cout << "Incorrect" << std::endl;
     return 0;
-}
+}```
