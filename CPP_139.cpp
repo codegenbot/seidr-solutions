c8fbea431e85bbc518bfb1eeb5aa5591374df6ee
beep boop <githubbot@vadim.me>
2024-07-10 00:26:25 +0200

    Solution 0 of CPP/139, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mm33nwh0 Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_139.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+using namespace std;
+long long special_factorial(int n){
+    long long result = 1;
+    for(int i=n; i>0; i--){
+        result *= factorial(i);
+    }
+    return result;
+}
+long long factorial(int n){
+    if(n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n-1);
+}
