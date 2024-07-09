e7ffa0f319208445b02f6f25f6ce85d439db4c92
beep boop <githubbot@vadim.me>
2024-07-09 03:03:29 +0200

    Solution 58 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/o8358e7c Prompt: eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 7a904803-f5bb-4b34-9660-c670a25f4271.cpp:28:10: error: ‘cout’ is not a member of  ‘std’    28 |     std::cout << "Mini Roman representation of the number is: " << int_t o_mini_roman(num) << std::endl;       |          ^~~~ 7a904803-f5bb-4b34-9660-c670a25f4271.cpp:28:10: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’? 7a904803-f5bb-4b34-9660-c670a25f4271.cpp: At global scope: 7a904803-f5bb-4b34-9660-c670a25f4271.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 7a904803-f5bb-4b34-9660-c670a25f4271.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from 7a904803-f5bb-4b34-9660-c670a25f4271.cpp:32: 7a904803-f5bb-4b34-9660-c670a25f4271.cpp: In function ‘int main()’: 7a904803-f5bb-4b34-9660-c670a25f4271.cpp:34:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    34 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,16 +1,15 @@
-```cpp
 #include <string>
 #include <vector>
+#include <ostream>
 #include <iostream>
 
-using namespace std;
+std::vector<std::pair<int, std::string>> roman = {{1000, "M"}, {900, "CM"}, {500, "D"},
+                                                    {400, "CD"}, {100, "C"}, {90, "XC"},
+                                                    {50, "L"}, {40, "XL"}, {10, "X"},
+                                                    {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
 
-string int_to_mini_roman(int number) {
-    vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {500, "D"},
-                                         {400, "CD"}, {100, "C"}, {90, "XC"},
-                                         {50, "L"}, {40, "XL"}, {10, "X"},
-                                         {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
-    string result = "";
+std::string toMiniRoman(int number) {
+    std::string result = "";
     for (const auto &romanNumber : roman) {
         while (number >= romanNumber.first) {
             number -= romanNumber.first;
@@ -18,14 +17,14 @@ string int_to_mini_roman(int number) {
         }
     }
     if(number > 0)
-        result.append(to_string(number)); 
+        result.append(std::to_string(number)); 
     return result;
 }
 
 int main() {
     int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    cout << "Mini Roman representation of the number is: " << int_to_mini_roman(num) << endl;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+    std::cout << "Mini Roman representation of the number is: " << toMiniRoman(num) << std::endl;
     return 0;
 }
