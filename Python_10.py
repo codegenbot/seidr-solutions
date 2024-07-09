57a22034fa9f6daa6c75ebd6d54bc542dea89fff
beep boop <githubbot@vadim.me>
2024-07-09 04:07:31 +0200

    Solution 21 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9n0jmv07 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5c19a2f5-a37c-41cf-9ade-a618a6416 d70.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,17 +1,11 @@
-def check_palindrome(string: str) -> None:
-    input_string = input("Enter a string to check if it's a palindrome: ")
-    if is_palindrome(input_string):
-        print("The string is a palindrome.")
-    else:
-        print("The string is not a palindrome.")
-
 def is_palindrome(string: str) -> bool:
     return string == string[::-1]
+
 
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
         return string + string
     for i in range(len(string)):
-        if string[:i+1] == string[:i+1][::-1]:
-            return string[:i+1] + string[i:][::-1]
+        if string[: i + 1] == string[: i + 1][::-1]:
+            return string[: i + 1] + string[i:][::-1]
     return string + string[::-1]
