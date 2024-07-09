d028cccfbe94be946ecb1fef55797cee205d63cc
beep boop <githubbot@vadim.me>
2024-07-09 16:05:31 +0200

    Solution 53 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/n6g4p4ab Prompt: Complete the following code given the task description and function signature.

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -1,13 +1,5 @@
 bool check_if_last_char_is_a_letter(string txt){
     if(txt.empty()) return false;
-    string lastChar = txt.substr(txt.length()-1, 1);
-    for(int i=0; i<txt.length(); i++){
-        if(txt[i] == ' ' || txt[i] == '\0') {
-            if(i != txt.length() - 1 && txt.substr(i+1).find(lastChar) == string::npos)
-                return true;
-            else
-                return false;
-        }
-    }
-    return false;
+    char lastChar = txt.back();
+    return isalpha(lastChar) && !isalnum(lastChar);
 }
