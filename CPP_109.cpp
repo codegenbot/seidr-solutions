6c64f4afccd61ed85c5a9c81d48045e0196481c8
beep boop <githubbot@vadim.me>
2024-07-09 19:15:56 +0200

    Solution 5 of CPP/109, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/msoy7j9q Prompt: 47c4ade6-9121-436e-98c8-76582107c1f8.cpp:1:20: error: ‘vector’ was not declared in this scope     1 | bool move_one_ball(vector<int> arr) {       |                    ^~~~~~ 47c4ade6-9121-436e-98c8-76582107c1f8.cpp:1:27: error: expected primary-expressio n before ‘int’     1 | bool move_one_ball(vector<int> arr) {       |                           ^~~ In file included from 47c4ade6-9121-436e-98c8-76582107c1f8.cpp:11: 47c4ade6-9121-436e-98c8-76582107c1f8.cpp: In function ‘int main()’: 47c4ade6-9121-436e-98c8-76582107c1f8.cpp:13:26: error: ‘move_one_ball’ cannot be  used as a function    13 |     assert (move_one_ball({})==true);       |             ~~~~~~~~~~~~~^~~~

--- CPP_109.cpp
+++ CPP_109.cpp
@@ -3,7 +3,9 @@
 bool move_one_ball(vector<int> arr) {
     int max = *max_element(arr.begin(), arr.end());
     for (int i = 0; i < arr.size(); i++) {
-        if (arr[i] > max - arr.size() + i) return false;
+        if (arr[i] > max - arr.size() + i) {
+            return false;
+        }
     }
     return true;
 }
