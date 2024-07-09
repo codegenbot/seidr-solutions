eaff267621999d37d993499ed2696584de802724
beep boop <githubbot@vadim.me>
2024-07-09 14:38:30 +0200

    Solution 42 of CPP/51, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ckuuy5av Prompt: Complete the following code given the task description and function signature.

--- CPP_51.cpp
+++ CPP_51.cpp
@@ -1,8 +1,9 @@
-string remove_vowels(string text){
+string remove_vowels(string text) {
     string result = "";
-    for(char c : text){
-        if(strchr("aeiouAEIOU",c) == NULL)
+    for (char c : text) {
+        if (!strchr("aeiouAEIOU", c)) {
             result += c;
+        }
     }
     return result;
 }
