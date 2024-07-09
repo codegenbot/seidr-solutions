91f60ffcadcbf3c534c588afb84c147a27ef0f98
beep boop <githubbot@vadim.me>
2024-07-09 21:40:52 +0200

    Solution 28 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/1olnobwn Prompt:       |                              ^~~~~~ 1f554bd7-3869-45a1-98ee-42a7b8a957bf.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 1f554bd7-3869-45a1-98ee-42a7b8a957bf.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1f554bd7-3869-45a1-98ee-42a7b8a957bf.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1f554bd7-3869-45a1-98ee-42a7b8a957bf.cpp:32: 1f554bd7-3869-45a1-98ee-42a7b8a957bf.cpp: In function ‘int main()’: 1f554bd7-3869-45a1-98ee-42a7b8a957bf.cpp:42:20: error: ‘numerical_letter_grade’ was not declared in this scope    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 1f554bd7-3869-45a1-98ee-42a7b8a957bf.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,11 +1,34 @@
+```
 #include <vector>
 #include <string>
 
-bool same(vector<string> a,vector<string>b){
-    return a == b;
-}
-
-int main() {
-    assert(same(numerical_letter_grade({4.0}), {"A+"}));
-    return 0;
+std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
+    std::vector<std::string> letter_grades;
+    for (float grade : grades) {
+        if (grade >= 4.0)
+            letter_grades.push_back("A+");
+        else if (grade > 3.7)
+            letter_grades.push_back("A");
+        else if (grade > 3.3)
+            letter_grades.push_back("A-");
+        else if (grade > 3.0)
+            letter_grades.push_back("B+");
+        else if (grade > 2.7)
+            letter_grades.push_back("B");
+        else if (grade > 2.3)
+            letter_grades.push_back("B-");
+        else if (grade > 2.0)
+            letter_grades.push_back("C+");
+        else if (grade > 1.7)
+            letter_grades.push_back("C");
+        else if (grade > 1.3)
+            letter_grades.push_back("C-");
+        else if (grade > 1.0)
+            letter_grades.push_back("D+");
+        else if (grade > 0.7)
+            letter_grades.push_back("D");
+        else
+            letter_grades.push_back("F");
+    }
+    return letter_grades;
 }
