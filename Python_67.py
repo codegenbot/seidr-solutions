3ccc1b2dbb502a1c2cbe54815bc0ff085941c377
beep boop <githubbot@vadim.me>
2024-07-09 07:36:22 +0200

    Solution 52 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n7aimvu6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7ce9d905-288c-407f-9c52-b1181f1d0 c5b.py", line 3, in <module>     if "".join(filter(str.isdigit, s))                                    ^ NameError: name 's' is not defined

--- Python_67.py
+++ Python_67.py
@@ -1,4 +1,6 @@
-def fruit_distribution(s, n):
-    s = s.lower()
-    num_apples = s.count("apple")
-    return n - num_apples
+def num_apples(s):
+    return (
+        int("".join(filter(str.isdigit, s)).lstrip("0") or 0)
+        if "".join(filter(str.isdigit, s))
+        else 0
+    )
