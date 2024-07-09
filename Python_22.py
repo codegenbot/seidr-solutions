8c961a1ec30c04771d6c33d9aed2e07cb9d19669
beep boop <githubbot@vadim.me>
2024-07-10 01:17:39 +0200

    Solution 1 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5oj21un6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e3c07cb5-e14d-4f70-907d-7bdd34a47 d3e.py", line 1, in <module>     def filter_integers(values: List[Any]) -> List[int]:                                 ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_22.py
+++ Python_22.py
@@ -1,2 +1,4 @@
-def filter_integers(values: List[Any]) -> List[int]:
+from typing import Any, list
+
+def filter_integers(values: list[Any]) -> list[int]:
     return [value for value in values if isinstance(value, int)]
