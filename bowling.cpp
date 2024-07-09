eed762b4d8f26e902c734bba3ba92f0a6f4e710b
beep boop <githubbot@vadim.me>
2024-07-09 17:12:49 +0200

    Solution 15 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/acc9guyz Prompt: 46e46bc0-d83b-4865-a23b-d0a4d7c2b138.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int bowlingScore(std::string s) {       |                       ^~~~~~ 46e46bc0-d83b-4865-a23b-d0a4d7c2b138.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int bowlingScore(std::string s) {

--- bowling.cpp
+++ bowling.cpp
@@ -1,27 +1,54 @@
+#include <string>
+
 int bowlingScore(std::string s) {
     int score = 0;
-    int currentRoll = 0;
     std::istringstream iss(s);
     char c;
+
+    int currentRoll1 = 0;
+    int currentRoll2 = 0;
     while (iss >> c) {
         if (c == 'X') {
             score += 30;
-            currentRoll = 10;
+            if (currentRoll1 > 0 || currentRoll2 > 0) { 
+                score -= 10; 
+            }
         } else if (c == '/') {
-            score += currentRoll + 10;
-            currentRoll = 0;
+            score += currentRoll1 + 10;
+            currentRoll1 = 0;
+            currentRoll2 = 0;
         } else {
             int roll = c - '0';
-            currentRoll += roll;
+            if (currentRoll1 > 0) { 
+                currentRoll2 = roll;
+            } else { 
+                currentRoll1 = roll;
+            }
             if (iss.peek() == '/' || iss.peek() == 'X') {
-                if (currentRoll < 10) {
-                    score += roll * 2;
-                } else {
-                    score += roll;
-                }
-                currentRoll = 0;
+                score += roll * 2; 
+            } else {
+                score += roll;
             }
         }
     }
+
+    if (currentRoll1 > 0) {
+        if (currentRoll1 + currentRoll2 >= 10) { 
+            score += 10;
+            if (currentRoll1 + currentRoll2 == 10) {
+                score += currentRoll1; 
+            } else {
+                score += currentRoll1 + currentRoll2; 
+            }
+        } else { 
+            score += 10;
+            if (currentRoll1 == 9) {
+                score += 1 * 2; 
+            } else {
+                score += 10 + currentRoll1 * 2; 
+            }
+        }
+    }
+
     return score;
 }
