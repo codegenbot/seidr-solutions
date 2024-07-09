d17317e73f6bde14f2beb930f966afc3687195a4
beep boop <githubbot@vadim.me>
2024-07-09 11:04:41 +0200

    Solution 8 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j8w2522w Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,23 +1,25 @@
 #include <string>
-#include <vector>
-
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = stoi(getNumerator(x));
-    int b = stoi(getDenominator(x));
-    int c = stoi(getNumerator(n));
-    int d = stoi(getDenominator(n));
-
-    return (a * d) == (b * c);
-}
-
-string getNumerator(string s) {
-    size_t pos = s.find('/');
-    return s.substr(0, pos);
-}
-
-string getDenominator(string s) {
-    size_t pos = s.find('/');
-    return s.substr(pos + 1);
+    int a = 0, b = 0, c = 0, d = 0;
+    string temp1, temp2;
+    // Convert the fractions to integers.
+    for(int i=1; i<x.size(); i++) {
+        if(x[i]=='/') break;
+        if(x[i-1]!='/') a = a*10 + (x[i]-'0');
+    }
+    for(int i=x.size()-1; i>0; i--) {
+        if(x[i-1]!='/') b = b*10 + (x[i]-'0');
+    }
+    for(int i=1; i<n.size(); i++) {
+        if(n[i]=='/') break;
+        if(n[i-1]!='/') c = c*10 + (n[i]-'0');
+    }
+    for(int i=n.size()-1; i>0; i--) {
+        if(n[i-1]!='/') d = d*10 + (n[i]-'0');
+    }
+    // Calculate the greatest common divisor of a, b, c and d.
+    int g = __gcd(__gcd(a, b), __gcd(c, d));
+    return (a/g) == (c/g);
 }
