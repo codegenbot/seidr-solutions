e1f70915103a36410bd5ec875966d09a1c7c122b
beep boop <githubbot@vadim.me>
2024-07-09 18:58:03 +0200

    Solution 2 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/t1wz5kz8 Prompt:       |                    ^~~ 5d8f48a3-2616-43ba-b016-b3be1ec70697.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5d8f48a3-2616-43ba-b016-b3be1ec70697.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5d8f48a3-2616-43ba-b016-b3be1ec70697.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5d8f48a3-2616-43ba-b016-b3be1ec70697.cpp:17: 5d8f48a3-2616-43ba-b016-b3be1ec70697.cpp: In function ‘int main()’: 5d8f48a3-2616-43ba-b016-b3be1ec70697.cpp:27:20: error: ‘make_a_pile’ was not dec lared in this scope    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 5d8f48a3-2616-43ba-b016-b3be1ec70697.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,15 +1,24 @@
-Here is the completed code:
+#include <vector>
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
 
 vector<int> make_a_pile(int n) {
     vector<int> pile;
-    int stones = 1;
-    for (int i = 0; i < n; i++) {
-        if ((i + 1) % 2 == 0) {
-            pile.push_back(stones);
-            stones += 2;
+    int stones = n;
+    while (stones > 0) {
+        pile.push_back(stones);
+        if (n % 2 == 1) {
+            n++;
+            stones = n;
         } else {
-            pile.push_back(stones);
-            stones += 1;
+            n++;
+            stones = n * 2;
         }
     }
     return pile;
