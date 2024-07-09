e73ca6cc427f79dfdd6d6b45fe3d1b16b5a38522
beep boop <githubbot@vadim.me>
2024-07-09 08:25:11 +0200

    Solution 22 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/x8fwogdg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a8cca490-1d15-4930-ae2a-e2d6691c0 a12.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -1,6 +1,8 @@
 ```
 def minPath(grid, k):
     n = len(grid)
+    m = [[i * j for j in range(1, n + 1)] for i in range(1, n + 1)]
+    
     memo = {}
     
     def dfs(i, j, path, cnt):
@@ -8,26 +10,24 @@ def minPath(grid, k):
             return memo[(i, j)]
         
         if cnt == k:
-            return path
+            return [path]
         
         res = []
-        for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             ni, nj = i + x, j + y
             if 1 <= ni < n and 1 <= nj < n and (ni, nj) not in memo:
-                res.append(dfs(ni, nj, path + [grid[ni][nj]], cnt + 1))
+                for p in dfs(ni, nj, path + [m[ni][nj]], cnt + 1):
+                    res.append(p)
         
         res.sort()
-        memo[(i, j)] = res[0]
-        return memo[(i, j)]
+        memo[(i, j)] = res
+        return res
     
     min_path = []
-    for i in range(1, len(grid)):
-        for j in range(len(grid[i])):
+    for i in range(1, n):
+        for j in range(n):
             if (i, j) not in memo:
-                min_path = dfs(i, j, [], 0)
-                break
-        else:
-            continue
-        break
-    
+                for path in dfs(i, j, [], 0):
+                    if len(path) == k and grid[i-1][j] != 1 and grid[i][j-1] != 1 and all(0 < i2 < n and 0 < j2 < n and (grid[i2-1][j2] != 1 or path[m2].index(minPath(m, k)[m2])) for m2, (i2, j2) in enumerate(path)):
+                        min_path = path
     return min_path
