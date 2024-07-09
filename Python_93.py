516a8483d25a3a4e34500f03ae09545d01f44cd9
beep boop <githubbot@vadim.me>
2024-07-09 19:54:16 +0200

    Solution 82 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ifpy1f4m Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/001aa9ce-e945-4b54-bd97-852f60d51 be0.py", line 9     encoded_message += chr((ord(char) - (ord('A') if char.isupper() else ord('a' )) + 2) % 26 + (ord('A') if char.isupper() else ord('a'))                           ^ SyntaxError: '(' was never closed

--- Python_93.py
+++ Python_93.py
@@ -1,1 +1,4 @@
-encoded_message += chr((ord(char) - (ord('A') if char.isupper() else ord('a')) + 2) % 26 + (ord('A') if char.isupper() else ord('a')))
+encoded_message += chr(
+    (ord(char) - (ord("A") if char.isupper() else ord("a")) + 2) % 26
+    + (ord("A") if char.isupper() else ord("a"))
+)
