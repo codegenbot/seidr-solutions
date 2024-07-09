0eaafd25cd781ae418e9bde8298a232fbfcac058
beep boop <githubbot@vadim.me>
2024-07-09 02:57:44 +0200

    Solution 53 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ca85xkll Prompt: In file included from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 341a5d22-1c0e-4abc-80f6-50f541c6457d.cpp:1: /usr/include/c++/11/bits/stl_algobase.h:300:5: note: candidate: ‘template<class _Tp, class _Compare> constexpr const _Tp& std::max(const _Tp&, const _Tp&, _Comp are)’   300 |     max(const _Tp& __a, const _Tp& __b, _Compare __comp)       |     ^~~ /usr/include/c++/11/bits/stl_algobase.h:300:5: note:   template argument deducti on/substitution failed: 341a5d22-1c0e-4abc-80f6-50f541c6457d.cpp:39:51: note:   candidate expects 3 argu ments, 0 provided    39 |         cin.ignore(numeric_limits<streamsize>::max(), '\n');       |                                              ~~~~~^~ 341a5d22-1c0e-4abc-80f6-50f541c6457d.cpp: At global scope: 341a5d22-1c0e-4abc-80f6-50f541c6457d.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ 341a5d22-1c0e-4abc-80f6-50f541c6457d.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -8,19 +8,21 @@ string string_to_md5(string text) {
     if (text.empty()) return "None";
     
     unsigned char result[16];
-    // EVP_MD_CTX md_ctx;
-    // EVP_PseudoRandomBytes(result, 0, &md_ctx);
-    // unsigned char* d = nullptr;
-    // size_t len = 0;
-    // int ret = EVP_Digest(text.c_str(), text.size(), &d, &len, EVP_md_md5(), &md_ctx);
-
+    //EVP_MD_CTX md_ctx;
+    //EVP_PseudoRandomBytes(result, 0, &md_ctx);
+    unsigned char* d = nullptr;
+    size_t len = 0;
+    //int ret = EVP_Digest(text.c_str(), text.size(), &d, &len, EVP_md_md5(), &md_ctx);
+    
     string md5_hash;
-
     for (int i = 0; i < 16; ++i) {
         ostringstream oss;
-        oss << hex << setfill('0') << setw(2) << static_cast<unsigned int>(rand() % 256);
+        oss << hex << setfill('0') << setw(2) << static_cast<unsigned int>(result[i]);
         md5_hash += oss.str();
     }
+    
+    //free(d);
+    //EVP_MD_CTX_free(&md_ctx);
     
     return md5_hash;
 }
