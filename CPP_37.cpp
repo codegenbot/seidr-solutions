70390d1159c2e505953c329db1f8b486fe2c49b4
beep boop <githubbot@vadim.me>
2024-07-09 14:20:04 +0200

    Solution 25 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7dqgrwjn Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 3a4f83a3-206b-4931-b5f7-b539d40d387c.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -36,10 +36,9 @@ vector<float> sort_even(vector<float> l) {
     return result;
 }
 
-int main() {
+int main() { 
     vector<float> input = make_vector({5, 8, -12, 4, 23, 2, 3, 11, 12, -10});
     vector<float> expected = { -12, 8, 3, 4, 5, 2, 12, 11, 23, -10};
     assert(isSame(sort_even(input), expected));
-    
     return 0;
 }
