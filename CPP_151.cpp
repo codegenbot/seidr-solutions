f402d1e55edfc0cb5bd5a12f3e1b5fce6c5bdf08
beep boop <githubbot@vadim.me>
2024-07-09 04:57:30 +0200

    Solution 48 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zxwtfg96 Prompt: __future_base::_State_baseV2>; _Args = {}; __gnu_cxx::_Lock_policy _Lp = __gnu_c xx::_S_atomic]’ /usr/include/c++/11/bits/shared_ptr_base.h:1342:14:   required from ‘std::__shar ed_ptr<_Tp, _Lp>::__shared_ptr(std::_Sp_alloc_shared_tag<_Tp>, _Args&& ...) [wit h _Alloc = std::allocator<std::__future_base::_State_baseV2>; _Args = {}; _Tp = std::__future_base::_State_baseV2; __gnu_cxx::_Lock_policy _Lp = __gnu_cxx::_S_a tomic]’ /usr/include/c++/11/bits/shared_ptr.h:409:59:   required from ‘std::shared_ptr<_ Tp>::shared_ptr(std::_Sp_alloc_shared_tag<_Tp>, _Args&& ...) [with _Alloc = std: :allocator<std::__future_base::_State_baseV2>; _Args = {}; _Tp = std::__future_b ase::_State_baseV2]’ /usr/include/c++/11/bits/shared_ptr.h:862:14:   required from ‘std::shared_ptr<_ Tp> std::allocate_shared(const _Alloc&, _Args&& ...) [with _Tp = std::__future_b ase::_State_baseV2; _Alloc = std::allocator<std::__future_base::_State_baseV2>; _Args = {}]’ /usr/include/c++/11/bits/shared_ptr.h:878:39:   required from ‘std::shared_ptr<_ Tp> std::make_shared(_Args&& ...) [with _Tp = std::__future_base::_State_baseV2;  _Args = {}]’ /usr/include/c++/11/future:1296:43:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::_Sp_counted_ptr_inplace<std::__future_base::_State_baseV2, std::allo cator<std::__future_base::_State_baseV2>, __gnu_cxx::_S_atomic> >’ has no member  named ‘_M_max_size’

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,8 +1,9 @@
 ```cpp
 #include <vector>
-using namespace std;
+#include <cmath>
+#include <iostream>
 
-long long double_the_difference(vector<float> lst) {
+long long double_the_difference(std::vector<float> lst) {
     long long sum = 0;
     for (float num : lst) {
         if (num > 0 && modf(num, &num) == 0) { 
@@ -13,11 +14,11 @@ long long double_the_difference(vector<float> lst) {
 }
 
 int main() {
-    vector<float> lst = {1.5, 2.3, 4.0, -3.7, 6.0};
+    std::vector<float> lst = {1.5, 2.3, 4.0, -3.7, 6.0};
     float odd_sum;
-    
+
     long long double result = double_the_difference(lst);
-    cout << "The difference is: " << result << endl;
+    std::cout << "The difference is: " << result << std::endl;
 
     return 0;
 }
