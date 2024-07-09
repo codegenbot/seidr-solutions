4a09518b51ad50e24204a1ce045c440abb6359b5
beep boop <githubbot@vadim.me>
2024-07-09 04:56:04 +0200

    Solution 93 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/d66di5pm Prompt:       |             ^~~~~~ 34062393-ba2c-4d91-8d28-0b9710b834db.cpp:22:20: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 34062393-ba2c-4d91-8d28-0b9710b834db.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 34062393-ba2c-4d91-8d28-0b9710b834db.cpp:22:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 34062393-ba2c-4d91-8d28-0b9710b834db.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 34062393-ba2c-4d91-8d28-0b9710b834db.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 34062393-ba2c-4d91-8d28-0b9710b834db.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,19 +1,1 @@
-#include <vector>
-#include <algorithm>
-#include <bitset>
-
 bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
-}
-
-std::vector<int> sort_array(std::vector<int> arr){
-    std::sort(arr.begin(), arr.end(), [](int a, int b){
-        int countA = std::bitset<32>(a).count();
-        int countB = std::bitset<32>(b).count();
-        if (countA == countB) {
-            return a < b;
-        }
-        return countA < countB;
-    });
-    return arr;
-}
