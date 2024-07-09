cbb935d3d237a43277b9e4338af83de7d0ea92e5
beep boop <githubbot@vadim.me>
2024-07-09 02:36:41 +0200

    Solution 69 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhnk3fn Prompt: 84072018-a955-4248-9671-34db4d66bfde.cpp:1:10: fatal error: boost/variant.hpp: N o such file or directory     1 | #include <boost/variant.hpp>       |          ^~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,6 +1,5 @@
+```cpp
 #include <boost/variant.hpp>
-#include <boost/none.hpp>
-
 using namespace boost;
 
 variant<string, int, double, float> compare_one(variant<string, int, double, float> a, variant<string, int, double, float> b) {
@@ -10,26 +9,22 @@ variant<string, int, double, float> compare_one(variant<string, int, double, flo
         return strA > strB ? a : b;
     }
     else if ((holds_alternation<string>(a) && holds_alternation<int>(b)) || (holds_alternation<string>(b) && holds_alternation<int>(a))) {
-        int val1 = get<int>(get_or(a, boost::none, [](auto& v) { return boost::none; }));
-        string str2 = get<string>(get_or(b, boost::none, [](auto& v) { return boost::none; }));
+        int val1 = get<int>(get_or(a, boost::none));
+        string str2 = get<string>(get_or(b, boost::none));
         return val1 > 0 ? a : b;
     }
     else if ((holds_alternation<int>(a) && holds_alternation<string>(b)) || (holds_alternation<int>(b) && holds_alternation<string>(a))) {
-        int val1 = get<int>(a), val2 = get<int>(get_or(b, boost::none, [](auto& v) { return boost::none; }));
+        int val1 = get<int>(a), val2 = get<int>(get_or(b, boost::none));
         return val1 > val2 ? a : b;
     }
     else if ((holds_alternation<double>(a) && holds_alternation<string>(b)) || (holds_alternation<string>(a) && holds_alternation<double>(b))) {
-        double val1 = get<double>(a), val2 = get<double>(get_or(b, boost::none, [](auto& v) { return boost::none; }));
+        double val1 = get<double>(a), val2 = get<double>(get_or(b, boost::none));
         return val1 > val2 ? a : b;
     }
     else if ((holds_alternation<float>(a) && holds_alternation<string>(b)) || (holds_alternation<string>(a) && holds_alternation<float>(b))) {
-        float val1 = get<float>(a), val2 = get<float>(get_or(b, boost::none, [](auto& v) { return boost::none; }));
+        float val1 = get<float>(a), val2 = get<float>(get_or(b, boost::none));
         return val1 > val2 ? a : b;
     }
     else
         return boost::none;
-}
-
-int main() {
-    return 0;
 }
