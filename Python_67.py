7dcf064e3564b67c6b2fc8cec0525445afc0a6b2
beep boop <githubbot@vadim.me>
2024-07-09 08:22:30 +0200

    Solution 93 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n7aimvu6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/abcb85f5-d70b-4044-a53a-677ed7c30 1d0.py", line 11, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/abcb85f5-d70b-4044-a53a-677ed7c30 1d0.py", line 8, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -2,5 +2,5 @@
 import re
 def fruit_distribution(s, n):
     s = s.lower()
-    num_apples = sum(int(i) for i in re.findall('\d+', s)) if re.search('\d+', s) else 0
+    num_apples = sum(int(i) for i in re.findall('\d+', s))
     return n - num_apples
