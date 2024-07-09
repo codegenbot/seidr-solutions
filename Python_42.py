1b5f0e09c549dcfa2f7725fe8019f1a0e6968cbf
beep boop <githubbot@vadim.me>
2024-07-09 22:27:21 +0200

    Solution 88 of Python/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/eem7e9nl Prompt: [2, 3, 4, 5, 6] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d60484a3-1bc7-4290-b543-8fc53cd2c 681.py", line 12, in <module>     check(incr_list)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d60484a3-1bc7-4290-b543-8fc53cd2c 681.py", line 9, in check     assert incr_list([5, 2, 5, 2, 3, 3, 9, 0, 123]) == [6, 3, 6, 3, 4, 4, 10, 1,  124]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: 'list' object is not callable

--- Python_42.py
+++ Python_42.py
@@ -1,10 +1,11 @@
 def increment_numbers(l):
     return [num + 1 for num in l]
 
-incr_list = [1, 2, 3, 4, 5]
+def check(incr_list):
+    assert increment_numbers(incr_list) == [6, 3, 6, 3, 4, 4, 10, 1, 124]
+
+incr_list = [5, 2, 5, 2, 3, 3, 9, 0, 123]
 
 result = increment_numbers(incr_list)
 print(result)
-
-# Fix the assertion call
-assert increment_numbers([5, 2, 5, 2, 3, 3, 9, 0, 123]) == [6, 3, 6, 3, 4, 4, 10, 1, 124]
+check(incr_list)
