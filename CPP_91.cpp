0b39f9d4e28147ff3c1305ad29b887a2f5d02cad
beep boop <githubbot@vadim.me>
2024-07-09 16:02:13 +0200

    Solution 3 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/8rtr04vk Prompt:       |   ^ 1bb83792-8f41-4861-bd5d-938f118389bb.cpp:1:4: error: stray ‘#’ in program     1 | ```#include <string>       |    ^ 1bb83792-8f41-4861-bd5d-938f118389bb.cpp:15:2: error: stray ‘`’ in program    15 | }```       |  ^ 1bb83792-8f41-4861-bd5d-938f118389bb.cpp:15:3: error: stray ‘`’ in program    15 | }```       |   ^ 1bb83792-8f41-4861-bd5d-938f118389bb.cpp:15:4: error: stray ‘`’ in program    15 | }```       |    ^ 1bb83792-8f41-4861-bd5d-938f118389bb.cpp:1:5: error: ‘include’ does not name a t ype     1 | ```#include <string>       |     ^~~~~~~ In file included from 1bb83792-8f41-4861-bd5d-938f118389bb.cpp:17: 1bb83792-8f41-4861-bd5d-938f118389bb.cpp: In function ‘int main()’: 1bb83792-8f41-4861-bd5d-938f118389bb.cpp:19:13: error: ‘is_bored’ was not declar ed in this scope    19 |     assert (is_bored("You and I are going for a walk") == 0);       |             ^~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,5 +1,26 @@
 #include <string>
 
+int main() {
+    int count = 0;
+    string boredom = "I";
+    string S;
+    cout << "Enter a sentence: ";
+    cin >> S;
+
+    size_t pos = 0;
+    while ((pos = S.find(" ", pos)) != string::npos) {
+        if (S.substr(0, pos).compare(boredom) == 0) {
+            count++;
+            break;
+        }
+        pos++;
+    }
+
+    cout << "Count: " << count << endl;
+
+    return is_bored(S);
+}
+
 int is_bored(string S){
     int count = 0;
     string boredom = "I";
@@ -12,3 +33,4 @@ int is_bored(string S){
         pos++;
     }
     return count;
+}
