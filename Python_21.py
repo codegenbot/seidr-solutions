c29810123a4d1bfaaea222c17887d97341edbb3d
beep boop <githubbot@vadim.me>
2024-07-09 03:46:00 +0200

    Solution 91 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/6suu5fvj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/59da25c9-d72d-43df-a1aa-4e3121615 da3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_21.py
+++ Python_21.py
@@ -1,7 +1,3 @@
-print(
-    """Enter a list of floating point numbers separated by space, and the function will rescale them to have values between 0 and 1.
+print('''Enter a list of floating point numbers separated by space, and the function will rescale them to have values between 0 and 1.
 {}
-""".format(
-        rescale_to_unit()
-    )
-)
+'''.format(rescale_to_unit()))
