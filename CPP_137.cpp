039887efefe8f471ddcabc7c7b6a8bb1977321cf
beep boop <githubbot@vadim.me>
2024-07-09 04:35:23 +0200

    Solution 36 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fv53j2kl Prompt: a89ceabd-cf9a-4838-a9dd-44bb49b6cd43.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,46 +1,30 @@
 #include <boost/any.hpp>
-
+#include <iostream>
 #include <string>
-using namespace std;
+#include <stdexcept>
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int x = boost::any_cast<int>(a);
-        int y = boost::any_cast<int>(b);
-        return (x > y) ? a : ((x < y) ? b : boost::any(typeid(string)));
-    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        float x = boost::any_cast<float>(a);
-        float y = boost::any_cast<float>(b);
-        return (x > y) ? a : ((x < y) ? b : boost::any(typeid(string)));
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string x = boost::any_cast<string>(a);
-        string y = boost::any_cast<string>(b);
-        return (stof(x) > stof(y)) ? a : ((stof(x) < stof(y)) ? b : boost::any(typeid(int)));
-    } else if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        int x = boost::any_cast<int>(a);
-        float y = boost::any_cast<float>(b);
-        return (x > y) ? a : ((x < y) ? b : boost::any(typeid(string)));
-    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int x = boost::any_cast<int>(a);
-        string y = boost::any_cast<string>(b);
-        return (x > stof(y)) ? a : ((x < stof(y)) ? b : boost::any(typeid(float)));
-    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        float x = boost::any_cast<float>(a);
-        int y = boost::any_cast<int>(b);
-        return (x > y) ? a : ((x < y) ? b : boost::any(typeid(string)));
-    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        float x = boost::any_cast<float>(a);
-        string y = boost::any_cast<string>(b);
-        return (x > stof(y)) ? a : ((x < stof(y)) ? b : boost::any(typeid(int)));
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return ((int)boost::any_cast<int>(a) > boost::any_cast<float>(b)) ? a : b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return ((int)boost::any_cast<int>(a) > boost::any_cast<double>(b)) ? a : b;
     } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        string x = boost::any_cast<string>(a);
-        int y = boost::any_cast<int>(b);
-        return (stof(x) > y) ? a : ((stof(x) < y) ? b : boost::any(typeid(float)));
+        std::string str = boost::any_cast<std::string>(a);
+        int num = boost::any_cast<int>(b);
+        return (std::stoi(str) > num) ? a : b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return (boost::any_cast<float>(a) > static_cast<float>(boost::any_cast<int>(b))) ? a : b;
+    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return (boost::any_cast<double>(a) > static_cast<double>(boost::any_cast<int>(b))) ? a : b;
     } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        string x = boost::any_cast<string>(a);
-        float y = boost::any_cast<float>(b);
-        return (stof(x) > y) ? a : ((stof(x) < y) ? b : boost::any(typeid(int)));
+        std::string str = boost::any_cast<std::string>(a);
+        float num = boost::any_cast<float>(b);
+        return (std::stof(str) > num) ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        std::string str = boost::any_cast<std::string>(a);
+        double num = boost::any_cast<double>(b);
+        return (std::stod(str) > num) ? a : b;
     } else {
-        return boost::any(typeid(string));
+        return "None";
     }
 }
