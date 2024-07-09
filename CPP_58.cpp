606f02074c6571ebc6b5e554cdd6b25d7a6c0e6f
beep boop <githubbot@vadim.me>
2024-07-09 04:03:12 +0200

    Solution 57 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tzm0q5ti Prompt:       |                    ^~~ 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp:14: 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp: In function ‘int main()’: 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp:24:20: error: ‘common’ was not declared  in this scope    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -2,30 +2,11 @@
 #include <vector>
 #include <set>
 
-int issame(vector<int> v) {
-    int count = 0;
-    for (int i : v) {
-        if (i % 2 == 0)
-            count++;
-    }
-    return count;
+bool issame(int a, int b) {
+    return (a == b);
 }
 
-int main() {
-    vector<int> l1, l2;
-    cout << "Enter the elements of list 1: ";
-    while (cin >> l1.back()) {}
-    
-    cout << "Enter the elements of list 2: ";
-    while (cin >> l2.back()) {}
-
-    vector<int> result = common(l1, l2);
-    int same = issame(result);
-
-    return 0;
-}
-
-vector<int> common(vector<int> l1, vector<int> l2) {
+int common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
@@ -34,5 +15,25 @@ vector<int> common(vector<int> l1, vector<int> l2) {
                       inserter(intersection, intersection.begin()));
 
     vector<int> result(intersection.begin(), intersection.end());
-    return result;
+    return (result.size());
+}
+
+int main() {
+    int n1, m1, n2, m2;
+    cin >> n1 >> m1;
+    vector<int> l1(n1);
+    for (int i = 0; i < n1; i++) {
+        cin >> l1[i];
+    }
+    cin >> n2 >> m2;
+    vector<int> l2(n2);
+    for (int i = 0; i < n2; i++) {
+        cin >> l2[i];
+    }
+
+    int k = common(l1, l2);
+
+    cout << k << endl;
+
+    return 0;
 }
