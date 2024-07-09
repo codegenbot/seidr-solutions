ea71479bf06f20b7379d47109fb31b877b798e6a
beep boop <githubbot@vadim.me>
2024-07-09 03:51:12 +0200

    Solution 15 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/g6w1o3fa Prompt:       |                    ^~~ 72bb679f-8777-4d3c-930f-da77f8084acf.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 72bb679f-8777-4d3c-930f-da77f8084acf.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 72bb679f-8777-4d3c-930f-da77f8084acf.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 72bb679f-8777-4d3c-930f-da77f8084acf.cpp:13: 72bb679f-8777-4d3c-930f-da77f8084acf.cpp: In function ‘int main()’: 72bb679f-8777-4d3c-930f-da77f8084acf.cpp:23:20: error: ‘sort_array’ was not decl ared in this scope    23 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 72bb679f-8777-4d3c-930f-da77f8084acf.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,39 +1,22 @@
-#include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
 bool issame(int a, int b) {
-    return (a == b);
+    return a == b;
 }
 
 std::vector<int> sort_array(std::vector<int> array) {
-    if(array.empty()) {
-        return array;
+    std::int64_t sum = 0;
+    for (int i : array) {
+        sum += i;
     }
-
-    std::sort(array.begin(), array.end(), [](int a, int b) {
-        return (!issame(a,b));
-    });
-
+    if (sum % 2 == 0) {
+        std::sort(array.begin(), array.end(), [](int a, int b){ return a > b; });
+    } else {
+        std::sort(array.begin(), array.end());
+    }
+    for (auto it = array.begin(); it != array.end(); ++it) {
+        *it = *it;
+    }
     return array;
-}
-
-int main() {
-    int n;
-    std::cout << "Enter the number of elements: ";
-    std::cin >> n;
-
-    std::vector<int> array(n);
-    for (int i = 0; i < n; i++) {
-        std::cout << "Enter element " << i+1 << ": ";
-        std::cin >> array[i];
-    }
-
-    sort_array(array);
-
-    for (int x : array) {
-        std::cout << x << " ";
-    }
-    
-    return 0;
 }
