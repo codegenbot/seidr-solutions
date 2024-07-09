5c9f554feb1418eb0e0be19c649275f20d887a6e
beep boop <githubbot@vadim.me>
2024-07-09 02:33:59 +0200

    Solution 30 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/yzjvcls6 Prompt:       |           ^~~~ 0e0504fe-7786-42ef-80b0-01973173c659.cpp:70:21: error: request for member ‘size’  in ‘b’, which is of non-class type ‘int’    70 |     if (a.size()!=b.size()) return false;       |                     ^~~~ 0e0504fe-7786-42ef-80b0-01973173c659.cpp:71:22: error: request for member ‘size’  in ‘a’, which is of non-class type ‘int’    71 |     for (int i=0;i<a.size();i++)       |                      ^~~~ 0e0504fe-7786-42ef-80b0-01973173c659.cpp:73:10: error: invalid types ‘int[int]’ for array subscript    73 |     if (a[i]!=b[i]) return false;       |          ^ 0e0504fe-7786-42ef-80b0-01973173c659.cpp:73:16: error: invalid types ‘int[int]’ for array subscript    73 |     if (a[i]!=b[i]) return false;       |                ^ In file included from 0e0504fe-7786-42ef-80b0-01973173c659.cpp:68: 0e0504fe-7786-42ef-80b0-01973173c659.cpp: In function ‘int main()’: 0e0504fe-7786-42ef-80b0-01973173c659.cpp:78:22: error: ‘bf’ cannot be used as a function    78 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,27 +1,66 @@
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 vector<string> bf(string planet1, string planet2) {
     vector<string> result;
     
-    int mercury = 57.9, venus = 67.2, earth = 0, mars = 225, 
-        jupiter = 778, saturn = 1433, uranus = 2870, neptune = 4495;
+    int distance1 = -1;
+    int distance2 = -1;
+    double mercuryDistance = 57.9, venusDistance = 67.2, earthDistance = 0, marsDistance = 225, jupiterDistance = 778, saturnDistance = 1433, uranusDistance = 2870, neptuneDistance = 4495;
 
-    string planets[] = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
-    
-    sort({planet1, planet2});
-    
-    if (planet1 != "Mercury" && planet2 != "Mercury") {
-        result.push_back(planet1);
-        result.push_back(planet2);
+    if (planet1 == "Mercury") {
+        distance1 = mercuryDistance;
     }
-    else if ((planet1 == "Mercury" || planet2 == "Mercury")) {
-        if (planet1 == "Mercury")
-            result.push_back(planet1);
-        else
-            result.push_back(planet2);
+    else if (planet1 == "Venus") {
+        distance1 = venusDistance;
     }
-    
+    else if (planet1 == "Earth") {
+        distance1 = earthDistance;
+    }
+    else if (planet1 == "Mars") {
+        distance1 = marsDistance;
+    }
+    else if (planet1 == "Jupiter") {
+        distance1 = jupiterDistance;
+    }
+    else if (planet1 == "Saturn") {
+        distance1 = saturnDistance;
+    }
+    else if (planet1 == "Uranus") {
+        distance1 = uranusDistance;
+    }
+    else if (planet1 == "Neptune") {
+        distance1 = neptuneDistance;
+    }
+
+    if (planet2 == "Mercury") {
+        distance2 = mercuryDistance;
+    }
+    else if (planet2 == "Venus") {
+        distance2 = venusDistance;
+    }
+    else if (planet2 == "Earth") {
+        distance2 = earthDistance;
+    }
+    else if (planet2 == "Mars") {
+        distance2 = marsDistance;
+    }
+    else if (planet2 == "Jupiter") {
+        distance2 = jupiterDistance;
+    }
+    else if (planet2 == "Saturn") {
+        distance2 = saturnDistance;
+    }
+    else if (planet2 == "Uranus") {
+        distance2 = uranusDistance;
+    }
+    else if (planet2 == "Neptune") {
+        distance2 = neptuneDistance;
+    }
+
+    if (distance1 != -1 && distance2 != -1) {
+        result.push_back((distance1 < distance2 ? planet1 : planet2));
+    }
+
     return result;
 }
