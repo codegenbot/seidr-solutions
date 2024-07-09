9da6e5654832bcc85017da0c94920ac1a412cbb5
beep boop <githubbot@vadim.me>
2024-07-09 03:13:21 +0200

    Solution 27 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lqyq253s Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e11895a7-8624-40ce-9a71-0c4ca3448f72.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e11895a7-8624-40ce-9a71-0c4ca3448f72.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e11895a7-8624-40ce-9a71-0c4ca3448f72.cpp:10: e11895a7-8624-40ce-9a71-0c4ca3448f72.cpp: In function ‘int main()’: e11895a7-8624-40ce-9a71-0c4ca3448f72.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ e11895a7-8624-40ce-9a71-0c4ca3448f72.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,11 +1,16 @@
+```cpp
 #include <iostream>
 #include <vector>
 #include <string>
 
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) return false;
+    if(a.size() != b.size())
+        return false;
     for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
+        for(int j = 0; j < a[i].size(); j++) {
+            if(a[i][j] != b[i][j])
+                return false;
+        }
     }
     return true;
 }
