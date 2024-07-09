0eb5f492c95125b28b9955e739e14f194f5c36e3
beep boop <githubbot@vadim.me>
2024-07-09 15:10:36 +0200

    Solution 10 of CPP/51, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/oemirqy8 Prompt: Program failed

--- CPP_51.cpp
+++ CPP_51.cpp
@@ -2,12 +2,10 @@ string remove_vowels(string text){
     string result = "";
     for(char c : text){
         if(toupper(c) != 'A' && toupper(c) != 'E' && toupper(c) != 'I' 
-           && toupper(c) != 'O' && toupper(c) != 'U'){
-            if(islower(c)) {
-                result += c;
-            } else {
-                result += tolower(c);
-            }
+           && toupper(c) != 'O' && toupper(c) != 'U' && 
+           tolower(c) != 'a' && tolower(c) != 'e' && tolower(c) != 'i' 
+           && tolower(c) != 'o' && tolower(c) != 'u'){
+            result += c; 
         }
     }
     return result;
