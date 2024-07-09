8eca31c10009ec99e99a69a25573f0be40f8992b
beep boop <githubbot@vadim.me>
2024-07-09 09:35:04 +0200

    Solution 16 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/jrle2ghq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/98729ae4-64bc-474c-bc98-a745de74b 53e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,18 +1,19 @@
-from typing import List
-
-
+```
 def parse_music(music_string: str) -> List[int]:
-    beats_per_note = {"o": 4, "o|": 2, ".|": 1}
-    result = []
-    note_index = 0
-    while note_index < len(music_string):
-        if music_string[note_index] == ".":
-            result.append(beats_per_note[".|"])
-            note_index += 3
-        elif music_string[note_index : note_index + 2] == "o|":
-            result.append(beats_per_note["o|"])
-            note_index += 2
+    notes = {'o': 4, 'o|': 2, '.|': 1}
+    beats = []
+    note = ''
+    
+    for char in music_string:
+        if char not in notes:
+            continue
+        if char != note:
+            if note:
+                beats.append(notes[note])
+            note = char
         else:
-            result.append(beats_per_note["o"])
-            note_index += 1
-    return result
+            note += char
+    
+    beats.append(notes[note])
+    
+    return beats
