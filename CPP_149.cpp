7ef43d116aadfaa9cc248bf1c4b2d33a46482e26
beep boop <githubbot@vadim.me>
2024-07-09 22:25:57 +0200

    Solution 8 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/b0l0pzf4 Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4aa49a65-825a-4911-b6bd-130a887e09e7.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4aa49a65-825a-4911-b6bd-130a887e09e7.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4aa49a65-825a-4911-b6bd-130a887e09e7.cpp:26: 4aa49a65-825a-4911-b6bd-130a887e09e7.cpp: In function ‘int main()’: 4aa49a65-825a-4911-b6bd-130a887e09e7.cpp:36:20: error: ‘sorted_list_sum’ was not  declared in this scope    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 4aa49a65-825a-4911-b6bd-130a887e09e7.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,6 +1,3 @@
-```cpp
-bool issame(vector<string>, vector<string>);
-
 vector<string> sorted_list_sum(vector<string> lst) {
     vector<string> result;
 
@@ -12,25 +9,14 @@ vector<string> sorted_list_sum(vector<string> lst) {
     }
 
     // Sort the resulted vector by length and then alphabetically
-    std::sort(result.begin(), result.end(),
-              [](const string& a, const string& b) {
-                  if (a.length() != b.length()) {
-                      return a.length() < b.length();
-                  } else {
-                      return a < b;
-                  }
-              });
+    sort(result.begin(), result.end(),
+         [](const string& a, const string& b) {
+             if (a.length() != b.length()) {
+                 return a.length() < b.length();
+             } else {
+                 return a < b;
+             }
+         });
 
     return result;
-}
-
-bool issame(vector<string> v1, vector<string> v2) {
-    if(v1.size()!=v2.size())
-        return false;
-    
-    for(int i = 0; i<v1.size(); i++) {
-        if( v1[i] != v2[i])
-            return false;
-    }
-    return true;
 }
