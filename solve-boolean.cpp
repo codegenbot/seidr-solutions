a1bfbf55bd9395a9c64289c4fbfca308ea5722f6
beep boop <githubbot@vadim.me>
2024-07-09 09:07:23 +0200

    Solution 32 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/y9p8yv78 Prompt: e77eb014-458e-4e70-9eed-aed59480fadb.cpp:1:19: error: ‘string’ was not declared in this scope     1 | bool solveBoolean(string s) {       |                   ^~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,4 +1,5 @@
 #include <string>
+using namespace std;
 
 bool solveBoolean(string s) {
     bool res = true;
@@ -8,24 +9,26 @@ bool solveBoolean(string s) {
             break;
         }
         else if (c == '&') {
-            while(res && s.find('&') != string::npos){
-                size_t pos = s.find('&');
-                if(s[pos+1] == 'T'){
-                    s.replace(pos, 2, "FT");
-                }else{
-                    s.replace(pos, 2, "FF");
-                }
+            bool temp = true;
+            for (; s.size() && s[0] != '&' && s[0] != '|'; ++s) {
+                if (s[0] == 'T')
+                    temp = true;
+                else 
+                    temp = false;
+                s = s.substr(1);
             }
+            res &= temp;
         }
         else if (c == '|') {
-            while(res || s.find('|') != string::npos){
-                size_t pos = s.find('|');
-                if(s[pos+1] == 'T'){
-                    s.replace(pos, 2, "FT");
-                }else{
-                    s.replace(pos, 2, "FF");
-                }
+            bool temp = false;
+            for (; s.size() && s[0] != '&' && s[0] != '|'; ++s) {
+                if (s[0] == 'T')
+                    temp = true;
+                else 
+                    temp = false;
+                s = s.substr(1);
             }
+            res |= temp;
         }
     }
     return res;
