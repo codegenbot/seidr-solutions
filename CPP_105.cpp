28eb51228e593fba4b0a455c8d5fe30955b56c26
beep boop <githubbot@vadim.me>
2024-07-09 13:12:28 +0200

    Solution 63 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/6g1exte2 Prompt:       |                              ^~~~~~ f1d5b1be-9e93-4f0c-97ca-50e97f0e54b3.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f1d5b1be-9e93-4f0c-97ca-50e97f0e54b3.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f1d5b1be-9e93-4f0c-97ca-50e97f0e54b3.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f1d5b1be-9e93-4f0c-97ca-50e97f0e54b3.cpp:29: f1d5b1be-9e93-4f0c-97ca-50e97f0e54b3.cpp: In function ‘int main()’: f1d5b1be-9e93-4f0c-97ca-50e97f0e54b3.cpp:39:20: error: ‘by_length’ was not decla red in this scope    39 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ f1d5b1be-9e93-4f0c-97ca-50e97f0e54b3.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -5,17 +5,29 @@
 bool issame(vector<string>, vector<string>);
 
 vector<string> by_length(vector<int> arr) {
-    vector<string> numbers;
+    vector<int> numbers;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            numbers.push_back(to_string(num));
+            numbers.push_back(num);
         }
     }
     sort(numbers.begin(), numbers.end());
     reverse(numbers.begin(), numbers.end());
-    return numbers;
-}
-
-bool issame(vector<string>, vector<string>) {
-    // implementation
+    vector<string> result;
+    for (int num : numbers) {
+        std::string str = "";
+        switch (num) {
+            case 1: str = "One"; break;
+            case 2: str = "Two"; break;
+            case 3: str = "Three"; break;
+            case 4: str = "Four"; break;
+            case 5: str = "Five"; break;
+            case 6: str = "Six"; break;
+            case 7: str = "Seven"; break;
+            case 8: str = "Eight"; break;
+            case 9: str = "Nine"; break;
+        }
+        result.push_back(str);
+    }
+    return result;
 }
