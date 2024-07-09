354a681f95485a76a398e2e00b3087d8924b2436
beep boop <githubbot@vadim.me>
2024-07-09 14:35:09 +0200

    Solution 2 of CPP/44, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/102vjbqs Prompt: Program failed

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -4,13 +4,26 @@ using namespace std;
 
 string change_base(int x, int base) {
     string res = "";
+    bool is_negative = false;
+    
+    if(x < 0) {
+        is_negative = true;
+        x *= -1;
+    }
+    
     while (x > 0) {
         int rem = x % base;
-        if (rem >= 10)
-            res.insert(0, 1, 'A' + rem - 10);
+        
+        if(rem >= 10 && base <= 36)
+            res.insert(0, 1, ('A' + rem - 10));
         else
             res.insert(0, 1, '0' + rem);
+        
         x /= base;
     }
-    return res;
+    
+    if(is_negative) 
+        res.insert(0, 1, '-');
+    
+    return (x == 0)? "0":res;
 }
