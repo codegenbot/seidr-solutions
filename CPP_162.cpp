6379890523c359c0076377f8e122e87a69d337be
beep boop <githubbot@vadim.me>
2024-07-09 03:22:25 +0200

    Solution 87 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ca85xkll Prompt: 1e4aefd7-253e-467e-9e96-2fc23e438e31.cpp: In function ‘std::string string_to_md5 (std::string)’: 1e4aefd7-253e-467e-9e96-2fc23e438e31.cpp:15:16: error: aggregate ‘EVP_MD_CTX md_ ctx’ has incomplete type and cannot be defined    15 |     EVP_MD_CTX md_ctx;       |                ^~~~~~ 1e4aefd7-253e-467e-9e96-2fc23e438e31.cpp: At global scope: 1e4aefd7-253e-467e-9e96-2fc23e438e31.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ 1e4aefd7-253e-467e-9e96-2fc23e438e31.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -15,14 +15,17 @@ string string_to_md5(string text) {
     size_t len = 0;
     int ret = EVP_DigestInit_ex(md_ctx, EVP_sha1(), NULL);
     ret = EVP_DigestUpdate(md_ctx, text.c_str(), text.size());
-    EVP_MD_CTX_destruct(&md_ctx);
-
+    EVP_DigestFinal_ex(md_ctx, &d, &len);
+    
     string md5_hash;
     for (int i = 0; i < 16; ++i) {
         ostringstream oss;
         oss << hex << setfill('0') << setw(2) << static_cast<unsigned int>(d[i]);
         md5_hash += oss.str();
     }
+    
+    free(d);
+    EVP_MD_CTX_free(md_ctx);
     
     return md5_hash;
 }
