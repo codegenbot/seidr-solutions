5a67acce094aad289b548e0413c7b7dc836f2231
beep boop <githubbot@vadim.me>
2024-07-09 05:01:31 +0200

    Solution 28 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sqlpuqyj Prompt:       |                    ^~~ fc98ddfc-73cf-4377-a7fe-7aadc7286e4e.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fc98ddfc-73cf-4377-a7fe-7aadc7286e4e.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fc98ddfc-73cf-4377-a7fe-7aadc7286e4e.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fc98ddfc-73cf-4377-a7fe-7aadc7286e4e.cpp:14: fc98ddfc-73cf-4377-a7fe-7aadc7286e4e.cpp: In function ‘int main()’: fc98ddfc-73cf-4377-a7fe-7aadc7286e4e.cpp:24:20: error: ‘even_odd_count’ was not declared in this scope    24 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ fc98ddfc-73cf-4377-a7fe-7aadc7286e4e.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,25 +1,23 @@
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
 }
 
-vector<int> even_odd_count(int num) {
-    int count_even = 0;
-    int count_odd = 0;
-
-    if (num < 0)
-        num = -num;
-
+std::vector<int> even_odd_count(int num) {
+    int count_even = 0, count_odd = 0;
     while (num != 0) {
         int digit = num % 10;
         if (digit % 2 == 0)
             count_even++;
         else
             count_odd++;
-
         num /= 10;
     }
-
     return {count_even, count_odd};
+}
+
+int main() {
+    assert(even_odd_count(0) == std::vector<int>(1, 0));
+    return 0;
 }
