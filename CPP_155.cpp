cbfec6de6b2d88d5ebd8412f63df77cabb3bd528
beep boop <githubbot@vadim.me>
2024-07-09 04:54:51 +0200

    Solution 22 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sqlpuqyj Prompt:       |                    ^~~ fa85baa6-a4ca-4464-b0eb-49582a9f7a28.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fa85baa6-a4ca-4464-b0eb-49582a9f7a28.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fa85baa6-a4ca-4464-b0eb-49582a9f7a28.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fa85baa6-a4ca-4464-b0eb-49582a9f7a28.cpp:21: fa85baa6-a4ca-4464-b0eb-49582a9f7a28.cpp: In function ‘int main()’: fa85baa6-a4ca-4464-b0eb-49582a9f7a28.cpp:31:20: error: ‘even_odd_count’ was not declared in this scope    31 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ fa85baa6-a4ca-4464-b0eb-49582a9f7a28.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -4,7 +4,22 @@ bool issame(vector<int> a, vector<int> b) {
     return (a == b);
 }
 
-int main() {
-    // Your main function here
-    return 0;
+vector<int> even_odd_count(int num) {
+    int count_even = 0;
+    int count_odd = 0;
+
+    if (num < 0)
+        num = -num;
+
+    while (num != 0) {
+        int digit = num % 10;
+        if (digit % 2 == 0)
+            count_even++;
+        else
+            count_odd++;
+
+        num /= 10;
+    }
+
+    return {count_even, count_odd};
 }
