dc7631d1dc981e75bccd05e051c0f0973b7c773d
beep boop <githubbot@vadim.me>
2024-07-09 21:51:32 +0200

    Solution 80 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g34sr5ik Prompt:       |     ^~~~~~~~~~~~~~~~ 386e11cf-c65e-4f33-b099-be3a08ab0117.cpp:17:5: error: ‘sort’ was not declared in  this scope; did you mean ‘short’?    17 |     sort(result.begin(), result.end());       |     ^~~~       |     short 386e11cf-c65e-4f33-b099-be3a08ab0117.cpp: At global scope: 386e11cf-c65e-4f33-b099-be3a08ab0117.cpp:31:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    31 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 386e11cf-c65e-4f33-b099-be3a08ab0117.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 386e11cf-c65e-4f33-b099-be3a08ab0117.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 386e11cf-c65e-4f33-b099-be3a08ab0117.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,10 +1,11 @@
 #include <vector>
+#include <assert>
 #include <set>
 #include <algorithm>
 
 using namespace std;
 
-bool isSame(vector<int> a, vector<int> b) {
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
@@ -15,12 +16,12 @@ vector<int> common(vector<int> l1, vector<int> l2) {
     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
         inserter(intersection, intersection.begin()));
     vector<int> result(intersection.begin(), intersection.end());
-    sort(result.begin(), result.end());
+    sort(result.begin(), result.end()); 
     return result;
 }
 
 int main() {
-    assert(isSame(common({4, 3, 2, 8}, {}), {}));
+    assert(issame(common({4, 3, 2, 8}, {}), {}));
     vector<int> list1 = {1, 2, 3};
     vector<int> list2 = {1, 2, 4};
     
