e882069817de624749e339ad60893fbeab6220eb
beep boop <githubbot@vadim.me>
2024-07-09 22:25:49 +0200

    Solution 24 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/j0a1ba12 Prompt: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 5cc6f3bf-a5ce-4fbd-afb8-481178e402cf.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 5cc6f3bf-a5ce-4fbd-afb8-481178e402cf.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from 5cc6f3bf-a5ce-4fbd-afb8-481178e402cf.cpp:41: 5cc6f3bf-a5ce-4fbd-afb8-481178e402cf.cpp:43:37: error: ‘compare_one’ was not dec lared in this scope    43 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,41 +1,41 @@
+#include <iostream>
 #include <boost/any.hpp>
-#include <boost/type_index.hpp>
+#include <boost/convert.hpp>
+#include <string>
 
-using namespace boost;
-
-boost::any compare_one(boost::any a, boost::any b) {
+bool compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return (int) a > (double) b ? a : b;
+        return (int) a > (double) b;
     }
     else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return (double) a > (int) b ? a : b;
+        return (double) a > (int) b;
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string s1 = any_cast<string>(a);
-        string s2 = any_cast<string>(b);
-        double n1 = stod(s1);
-        double n2 = stod(s2);
-        return n1 > n2 ? a : b;
+    else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+        std::string s1 = boost::any_cast<std::string>(a);
+        std::string s2 = boost::any_cast<std::string>(b);
+        double n1 = std::stod(s1);
+        double n2 = std::stod(s2);
+        return n1 > n2;
     }
-    else if (a.type() == typeid(string) && (b.type() == typeid(int) || b.type() == typeid(double))) {
-        string s = any_cast<string>(a);
-        double n = stod(s);
-        return n > boost::any_cast<int>(b) ? a : b;
+    else if (a.type() == typeid(std::string) && (b.type() == typeid(int) || b.type() == typeid(double))) {
+        std::string s = boost::any_cast<std::string>(a);
+        double n = std::stod(s);
+        return n > boost::any_cast<int>(b);
     }
-    else if ((a.type() == typeid(int) || a.type() == typeid(double)) && b.type() == typeid(string)) {
+    else if ((a.type() == typeid(int) || a.type() == typeid(double)) && b.type() == typeid(std::string)) {
         double n1 = boost::any_cast<double>(a);
-        string s2 = any_cast<string>(b);
-        double n2 = stod(s2);
-        return n1 > n2 ? a : b;
+        std::string s2 = boost::any_cast<std::string>(b);
+        double n2 = std::stod(s2);
+        return n1 > n2;
     }
     else if (a.type() == typeid(int) && a.convert_to<int>() == a.convert_to<double>()) {
-        return type_id<decltype("None")>();
+        return true;
     }
     else if (a.type() == typeid(double) && a.convert_to<double>() == a.convert_to<int>()) {
-        return type_id<decltype("None")>();
+        return true;
     }
-    else if (a.type() == typeid(string) && any_cast<string>(a).find('.') == string::npos) {
-        return type_id<decltype("None")>();
+    else if (a.type() == typeid(std::string) && boost::any_cast<std::string>(a).find('.') == std::string::npos) {
+        return true;
     }
-    return a;
+    return false;
 }
