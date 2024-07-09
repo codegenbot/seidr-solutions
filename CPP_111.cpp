cd6b629f892b67f8da5145677a4118f7e9c84e9d
beep boop <githubbot@vadim.me>
2024-07-09 10:45:52 +0200

    Solution 35 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/gop9ghvx Prompt:       |                             ^~~ c60b51df-29ff-453f-a94a-90e613bb5cab.cpp:26:33: error: expected primary-expressi on before ‘char’    26 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ c60b51df-29ff-453f-a94a-90e613bb5cab.cpp:26:38: error: expected primary-expressi on before ‘int’    26 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ c60b51df-29ff-453f-a94a-90e613bb5cab.cpp:26:44: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from c60b51df-29ff-453f-a94a-90e613bb5cab.cpp:25: c60b51df-29ff-453f-a94a-90e613bb5cab.cpp: In function ‘int main()’: c60b51df-29ff-453f-a94a-90e613bb5cab.cpp:40:20: error: ‘histogram’ was not decla red in this scope    40 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ c60b51df-29ff-453f-a94a-90e613bb5cab.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,28 +1,6 @@
-#include <iostream>
-#include <sstream>
-#include <map>
-#include <string>
-
-using namespace std;
-
 map<char, int> histogram(string test);
-
 bool issame(const map<char, int>& a, const map<char, int>& b) {
     return a == b;
-}
-
-int main() {
-    string test = "hello world";
-    map<char, int> result = histogram(test);
-    
-    for (const auto& entry : result) {
-        cout << entry.first << ": " << entry.second << endl;
-    }
-    
-    map<char, int> testMap = {{'a', 1}, {'b', 2}};
-    cout << issame(testMap, testMap) << endl;
-    
-    return 0;
 }
 
 map<char, int> histogram(string test) {
