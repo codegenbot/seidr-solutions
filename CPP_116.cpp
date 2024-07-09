8bfbcb8cead1e69ee135b48fc3a9bd8cf08b63ee
beep boop <githubbot@vadim.me>
2024-07-09 04:40:55 +0200

    Solution 60 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/hvdy68h7 Prompt: In file included from /usr/include/c++/11/vector:67,                  from b238895d-a41e-4a2e-b50e-d33ca513aaea.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from b238895d-a41e-4a2e-b50e-d33ca513aaea.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b238895d-a41e-4a2e-b50e-d33ca513aaea.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b238895d-a41e-4a2e-b50e-d33ca513aaea.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b238895d-a41e-4a2e-b50e-d33ca513aaea.cpp:17: b238895d-a41e-4a2e-b50e-d33ca513aaea.cpp: In function ‘int main()’: b238895d-a41e-4a2e-b50e-d33ca513aaea.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,9 +1,12 @@
 #include <vector>
 #include <algorithm>
 
+bool issame(std::vector<int> a,std::vector<int> b){
+    return std::equal(a.begin(),a.end(),b.begin(),b.end());
+}
+
 std::vector<int> sort_array(std::vector<int> arr) {
-    std::vector<int> arrCopy = arr; 
-    std::sort(arrCopy.begin(), arrCopy.end(), [](int a, int b) {
+    std::sort(arr.begin(), arr.end(), [](int a, int b) {
         int ones_a = __builtin_popcount(a);
         int ones_b = __builtin_popcount(b);
 
@@ -12,5 +15,10 @@ std::vector<int> sort_array(std::vector<int> arr) {
         else
             return ones_a < ones_b;
     });
-    return arrCopy;
+    return arr;
+}
+
+int main() {
+    assert(issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));
+    return 0;
 }
