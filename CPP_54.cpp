2413bd6c2a0a5142e489af6bb41a31324b186f6b
beep boop <githubbot@vadim.me>
2024-07-09 16:22:37 +0200

    Solution 73 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5uknokbc Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <iostream>
 using namespace std;
 
@@ -7,10 +8,10 @@ bool same_chars(string s1, string s2) {
     }
     for (int i = 0; i < s1.size(); i++) {
         int count = 0;
-        while (i + count <= s1.size() && i + count < s1.size() && s1[i] == s1[i + count]) {
+        while (i + count <= s1.size() && i < s1.size() && s1[i] == s1[i + count]) {
             count++;
         }
-        if (count > 1) {
+        if (s1[i] != s1[i + count - 1]) {
             return false;
         }
     }
@@ -24,7 +25,7 @@ int main() {
     cin >> s1;
     cout << "Enter the second string: ";
     cin >> s2;
-    if ((same_chars(s1, s2))) {
+    if (same_chars(s1, s2)) {
         cout << "True";
     } else {
         cout << "False";
