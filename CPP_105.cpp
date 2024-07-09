e07249b7a6592cdb83d9ed02707d9fa386c74970
beep boop <githubbot@vadim.me>
2024-07-10 00:38:44 +0200

    Solution 95 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g8hfxvnh Prompt:       |                              ^~~~~~ d58f979c-54d1-4138-a90e-f9148fafa34e.cpp:47:37: error: ‘string’ was not declared  in this scope    47 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d58f979c-54d1-4138-a90e-f9148fafa34e.cpp:47:44: error: ‘b’ was not declared in t his scope    47 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d58f979c-54d1-4138-a90e-f9148fafa34e.cpp:47:45: error: expression list treated a s compound expression in initializer [-fpermissive]    47 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d58f979c-54d1-4138-a90e-f9148fafa34e.cpp:46: d58f979c-54d1-4138-a90e-f9148fafa34e.cpp: In function ‘int main()’: d58f979c-54d1-4138-a90e-f9148fafa34e.cpp:56:20: error: ‘by_length’ was not decla red in this scope    56 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ d58f979c-54d1-4138-a90e-f9148fafa34e.cpp:56:19: error: ‘issame’ cannot be used a s a function    56 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,47 +1,53 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+vector<string> by_length(vector<int> arr) {
+    vector<int> sortedArr;
+    for (int i : arr) {
+        if (i >= 1 && i <= 9)
+            sortedArr.push_back(i);
+    }
+    sort(sortedArr.begin(), sortedArr.end());
+    reverse(sortedArr.begin(), sortedArr.end());
+    vector<string> result;
+    for (int i : sortedArr) {
+        string str = "";
+        switch (i) {
+            case 1:
+                str = "One";
+                break;
+            case 2:
+                str = "Two";
+                break;
+            case 3:
+                str = "Three";
+                break;
+            case 4:
+                str = "Four";
+                break;
+            case 5:
+                str = "Five";
+                break;
+            case 6:
+                str = "Six";
+                break;
+            case 7:
+                str = "Seven";
+                break;
+            case 8:
+                str = "Eight";
+                break;
+            case 9:
+                str = "Nine";
+                break;
+        }
+        result.push_back(str);
+    }
+    return result;
+}
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size())
+    if (a.size() != b.size())
         return false;
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i])
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
             return false;
     }
     return true;
-}
-
-vector<string> by_length(vector<int> arr) {
-    vector<string> result;
-    map<int, string> numStrMap = {{1, "One"}, {2, "Two"}, {3, "Three"}, 
-                                   {4, "Four"}, {5, "Five"}, {6, "Six"}, 
-                                   {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-    for (int i : arr) {
-        if (i >= 1 && i <= 9)
-            result.push_back(numStrMap[i]);
-    }
-    sort(result.begin(), result.end());
-    reverse(result.begin(), result.end());
-    return result;
-}
-
-int main() {
-    vector<int> input;
-    int n;
-    std::cout << "Enter number of elements: ";
-    std::cin >> n;
-    for(int i = 0; i < n; i++) {
-        std::cout << "Enter the number: ";
-        int num;
-        std::cin >> num;
-        input.push_back(num);
-    }
-    
-    vector<string> result = by_length(input);
-    std::cout << "Sorted string array: ";
-    for(string s : result) {
-        cout << s << " ";
-    }
-    return 0;
 }
