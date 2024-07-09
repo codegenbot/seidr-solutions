3793d0469f74695f5f101205c682325a506e855e
beep boop <githubbot@vadim.me>
2024-07-09 02:12:31 +0200

    Solution 14 of CPP/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/namiduv7 Prompt:     1 | ```       |  ^ fd511571-7614-4500-a36d-2849ac35bde4.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ fd511571-7614-4500-a36d-2849ac35bde4.cpp:2:23: error: ‘string’ was not declared in this scope     2 | bool cycpattern_check(string a, string b) {       |                       ^~~~~~ fd511571-7614-4500-a36d-2849ac35bde4.cpp:2:33: error: ‘string’ was not declared in this scope     2 | bool cycpattern_check(string a, string b) {       |                                 ^~~~~~ fd511571-7614-4500-a36d-2849ac35bde4.cpp:2:41: error: expression list treated as  compound expression in initializer [-fpermissive]     2 | bool cycpattern_check(string a, string b) {       |                                         ^ In file included from fd511571-7614-4500-a36d-2849ac35bde4.cpp:20: fd511571-7614-4500-a36d-2849ac35bde4.cpp: In function ‘int main()’: fd511571-7614-4500-a36d-2849ac35bde4.cpp:22:30: error: ‘cycpattern_check’ cannot  be used as a function    22 |     assert  (cycpattern_check("winemtt","tinem") == true );       |              ~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_154.cpp
+++ CPP_154.cpp
@@ -2,20 +2,16 @@
 using namespace std;
 
 bool cycpattern_check(string a, string b) {
-    if(a.length() < b.length()) return false;
     for(int i = 0; i < a.length(); i++) {
-        int j = 0;
         string temp = a.substr(i);
-        while(temp.length() >= b.length()) {
-            int k = 0;
+        if(temp.length() >= b.length()) {
             bool flag = true;
-            while(k < b.length()) {
-                if(temp.find(b[k]) == -1) {
+            for(int j = 0; j < b.length(); j++) {
+                if(temp.find(b[j]) == -1) {
                     flag = false;
                     break;
                 }
                 temp = temp.substr(1) + temp[0];
-                k++;
             }
             if(flag) return true;
         }
