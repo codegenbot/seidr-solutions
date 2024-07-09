3cf5774404062b1e371e889faf9bca511ccfeefd
beep boop <githubbot@vadim.me>
2024-07-09 05:44:25 +0200

    Solution 75 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/urrbvh7e Prompt: 12c9414f-e8e1-462a-8b4b-6461c417de62.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int num) {       | ^~~~~~ In file included from 12c9414f-e8e1-462a-8b4b-6461c417de62.cpp:87: 12c9414f-e8e1-462a-8b4b-6461c417de62.cpp: In function ‘int main()’: 12c9414f-e8e1-462a-8b4b-6461c417de62.cpp:89:13: error: ‘int_to_mini_romank’ was not declared in this scope    89 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -32,55 +32,54 @@ string int_to_mini_roman(int num) {
             num -= 400;
             result += "CD";
         } else if (num >= 100) {
-            while(num >= 100) {
-                if(num >= 900) {
-                    num -= 900;
-                    result += "CM";
-                } else if(num >= 500) {
-                    num -= 500;
-                    result += "D";
-                } else {
-                    num -= 100;
-                    result += "C";
-                }
-            }
+            for(int i=0; i<num/100; ++i)
+                result += "C";
+            num %= 100;
         } else if (num >= 90) {
-            num -= 90;
-            result += "XC";
+            while(num>=90) {
+                num -= 90;
+                result += "XC";
+            }
         } else if (num >= 50) {
-            while(num >= 50) {
-                if(num >= 40) {
-                    num -= 40;
-                    result += "XL";
-                } else {
-                    num -= 50;
-                    result += "L";
-                }
+            for(int i=0; i<num/50; ++i)
+                result += "L";
+            num %= 50;
+        } else if (num >= 40) {
+            while(num>=40) {
+                num -= 40;
+                result += "XL";
             }
+        } else if (num >= 10) {
+            for(int i=0; i<num/10; ++i)
+                result += "X";
+            num %= 10;
         } else if (num >= 9) {
-            num -= 9;
-            result += "IX";
+            while(num>=9) {
+                num -= 9;
+                result += "IX";
+            }
         } else if (num >= 5) {
-            while(num >= 5) {
-                if(num >= 4) {
-                    num -= 4;
-                    result += "IV";
-                } else {
-                    num -= 5;
-                    result += "V";
-                }
-            }
-        } else if (num >= 3) {
-            while(num > 2) {
-                num--;
-                result += "I";
-            }
-            if(num == 2) {
-                return "II";
+            for(int i=0; i<num/5; ++i)
+                result += "V";
+            num %= 5;
+        } else if (num >= 4) {
+            while(num>=4) {
+                num -= 4;
+                result += "IV";
             }
         } else {
-            num--;
-            result += "I";
+            while(num>0) {
+                if(num>=3) {
+                    num -= 3;
+                    result += "III";
+                } else if (num == 2) {
+                    result += "II";
+                    return result;
+                } else {
+                    num--;
+                    result += "I";
+                }
+            }
         }
     }
 
