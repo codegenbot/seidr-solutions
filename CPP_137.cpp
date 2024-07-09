1ecb4d54b450a59210d0c6759f3784d3319094db
beep boop <githubbot@vadim.me>
2024-07-09 02:44:25 +0200

    Solution 44 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/pf8nwdde Prompt: a6297141-4e83-498f-aacd-5336c822d452.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,3 @@
-```
 #include <boost/any.hpp>
 #include <string>
 
@@ -15,20 +14,7 @@ int main() {
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return "Not comparable";
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(std::string)) {
-        return "Not comparable";
-    }
-    else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        std::string strA = boost::any_cast<std::string>(a);
-        std::string strB = boost::any_cast<std::string>(b);
-        if (strA > strB)
-            return a;
-        else if (strA < strB)
-            return b;
-        else
-            return "Equal";
+        return a;
     }
     else if (a.type() == typeid(int) && b.type() == typeid(int)) {
         int iA = boost::any_cast<int>(a);
@@ -40,7 +26,46 @@ boost::any compare_one(boost::any a, boost::any b) {
         else
             return "Equal";
     }
+    else if (a.type() == typeid(int) && b.type() == typeid(std::string)) {
+        return a;
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        int iB = boost::any_cast<int>(b);
+        if ((boost::any_cast<float>(a)) > iB)
+            return a;
+        else
+            return "Equal";
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(std::string)) {
+        return "Equal";
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        float fA = boost::any_cast<float>(a);
+        float fB = boost::any_cast<float>(b);
+        if (fA > fB)
+            return a;
+        else if (fA < fB)
+            return b;
+        else
+            return "Equal";
+    }
+    else if (a.type() == typeid(std::string) && b.type() == typeid(int)) {
+        return "None";
+    }
+    else if (a.type() == typeid(std::string) && b.type() == typeid(float)) {
+        return "None";
+    }
+    else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+        std::string strA = boost::any_cast<std::string>(a);
+        std::string strB = boost::any_cast<std::string>(b);
+        if (strA > strB)
+            return a;
+        else if (strA < strB)
+            return b;
+        else
+            return "Equal";
+    }
     else {
-        return "Not comparable";
+        return "None";
     }
 }
