9974e34ec6f6a096ee09bd8fabadbab3fcf0d48e
beep boop <githubbot@vadim.me>
2024-07-09 17:44:29 +0200

    Solution 5 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/hfjhv8f5 Prompt: In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 41aab248-4d45-4281-a1c5-95b4aef7f300.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 41aab248-4d45-4281-a1c5-95b4aef7f300.cpp:5:59: error: ‘extensions’ was not decla red in this scope     5 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                                           ^~~~~~~~~~ 41aab248-4d45-4281-a1c5-95b4aef7f300.cpp:5:69: error: expression list treated as  compound expression in initializer [-fpermissive]     5 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                                                     ^ In file included from 41aab248-4d45-4281-a1c5-95b4aef7f300.cpp:25: 41aab248-4d45-4281-a1c5-95b4aef7f300.cpp: In function ‘int main()’: 41aab248-4d45-4281-a1c5-95b4aef7f300.cpp:27:32: error: ‘Strongest_Extension’ can not be used as a function    27 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,15 +1,28 @@
-```cpp
-int Strongest_Extension(string class_name, vector<string> extensions){
+#include <iostream>
+#include <vector>
+#include <string>
+
+int Strongest_Extension(std::string class_name, std::vector<std::string> extensions){
     int max_strength = 0;
-    string strongest_extension;
+    std::string strongest_extension;
 
     for(auto extension : extensions){
-        int length = extension.length();
-        if(length > max_strength || (length == max_strength && extension.compare(0, length, strongest_extension) < 0)){
-            max_strength = length;
+        std::string strong_ext;
+        for(auto c : extension){
+            if(std::isupper(c)) strong_ext += std::to_string(c);
+            else if(std::islower(c)) strong_ext += std::to_string(c);
+        }
+        int strength = strong_ext.length() - (int)strong_ext.convert_to_lower().length();
+        if(strength > max_strength || (strength == max_strength && extension.compare(0, extension.size(), strongest_extension) < 0)){
+            max_strength = strength;
             strongest_extension = extension;
         }
     }
 
     return class_name + "." + strongest_extension;
+}
+
+int main() {
+    assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
+    return 0;
 }
