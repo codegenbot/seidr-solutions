6c8246fd60900f630644622b7e468b81b5718340
beep boop <githubbot@vadim.me>
2024-07-09 23:13:24 +0200

    Solution 58 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/zt4qvmez Prompt: d35b5edb-06d4-47fd-a98a-9e078fb5fb66.cpp: In function ‘std::string string_to_md5 (std::string)’: d35b5edb-06d4-47fd-a98a-9e078fb5fb66.cpp:12:16: error: aggregate ‘EVP_MD_CTX md’  has incomplete type and cannot be defined    12 |     EVP_MD_CTX md;       |                ^~ d35b5edb-06d4-47fd-a98a-9e078fb5fb66.cpp:14:26: error: ‘MD5_DIGEST_LENGTH’ was n ot declared in this scope    14 |     unsigned char result[MD5_DIGEST_LENGTH];       |                          ^~~~~~~~~~~~~~~~~ d35b5edb-06d4-47fd-a98a-9e078fb5fb66.cpp:17:30: error: ‘result’ was not declared  in this scope    17 |     EVP_DigestFinal_CTX(&md, result, NULL);       |                              ^~~~~~ d35b5edb-06d4-47fd-a98a-9e078fb5fb66.cpp:17:5: error: ‘EVP_DigestFinal_CTX’ was not declared in this scope; did you mean ‘EVP_DigestFinal_ex’?    17 |     EVP_DigestFinal_CTX(&md, result, NULL);       |     ^~~~~~~~~~~~~~~~~~~       |     EVP_DigestFinal_ex

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -14,14 +14,14 @@ string string_to_md5(string text) {
     EVP_MD_CTX_init(&md);
     unsigned char result[MD5_DIGEST_LENGTH];
 
-    EVP_DigestInit_ex(&md, EVP_md5(), NULL);
-    EVP_DigestUpdate(&md, text.c_str(), text.size());
-    unsigned char* md5 = (unsigned char*)malloc(MD5_DIGEST_LENGTH);
-    EVP_DigestFinal_CTX(&md, md5, NULL);
-    string output;
+    EVP_DigestInit_ex(EVP_get_builtin_context(), EVP_md5(), NULL);
+    EVP_DigestUpdate(&md, (unsigned char*)text.c_str(), text.size());
+    EVP_DigestFinal_CTX(&md, result, NULL);
+
+    stringstream ss;
     for(int i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        output += setfill('0') << setw(2) << hex << std::setw(2) << std::setfill('0') << (int)md5[i];
+        ss << setfill('0') << setw(2) << hex << (int)result[i];
     }
-    free(md5);
-    return output;
+
+    return ss.str();
 }
