c1199a5d21f0ca3df6c31466d6872860ec43e537
beep boop <githubbot@vadim.me>
2024-07-09 07:39:31 +0200

    Solution 8 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/x8c0rtpg Prompt: In file included from 029e8e30-01dd-43be-a924-b8b15eb9844d.cpp:22: 029e8e30-01dd-43be-a924-b8b15eb9844d.cpp: In function ‘int main()’: 029e8e30-01dd-43be-a924-b8b15eb9844d.cpp:24:30: error: could not convert ‘{-3, 9 , -1, 4, 2, 31}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector<std::v ector<int> >’    24 |     assert (pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);       |             ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~       |                              |       |                              <brace-enclosed initializer list>

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,15 +1,2 @@
-bool pairs_sum_to_zero(vector<vector<int>> l) {
-    set<int> s;
-    for (const auto& v : l) {
-        for (int i : v) {
-            s.insert(i);
-        }
-    }
-    for (auto it = s.begin(); it != s.end(); ++it) { 
-        int complement = -(*it);
-        if (s.find(complement) != s.end()) {
-            return true;
-        }
-    }
-    return false;
-}
+```cpp
+assert(pairs_sum_to_zero({{1, 2}, {3, 4}, {-5, 6}}) == false);
