b5ca09eca1573d97c7060202c683a09649a4367d
beep boop <githubbot@vadim.me>
2024-07-09 04:49:29 +0200

    Solution 24 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xuigdtqp Prompt:       |                       ^~~~~~ a4895c18-a430-4d23-8692-6e7705bf163c.cpp:1:31: error: ‘operator_’ was not declar ed in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                               ^~~~~~~~~ a4895c18-a430-4d23-8692-6e7705bf163c.cpp:1:42: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                          ^~~~~~ a4895c18-a430-4d23-8692-6e7705bf163c.cpp:1:49: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                 ^~~ a4895c18-a430-4d23-8692-6e7705bf163c.cpp:1:61: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                             ^ In file included from a4895c18-a430-4d23-8692-6e7705bf163c.cpp:19: a4895c18-a430-4d23-8692-6e7705bf163c.cpp: In function ‘int main()’: a4895c18-a430-4d23-8692-6e7705bf163c.cpp:21:23: error: ‘do_algebra’ cannot be us ed as a function    21 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -10,9 +10,12 @@ int do_algebra(vector<string> operator_, vector<int> operand) {
             result -= operand[i + 1];
         } else if (operator_[i] == "*") {
             result *= operand[i + 1];
-        } else if (operator_[i] == "//") {
-            result = result / static_cast<int>(operand[i + 1]);
-        } else if (operator_[i] == "**") {
+        } else if (operator_[i] == "//" || operator_[i] == "÷") {
+            if (operand[i + 1] != 0)
+                result = result / static_cast<int>(operand[i + 1]);
+            else
+                return -1;  // handle division by zero error
+        } else if (operator_[i] == "**" || operator_[i] == "^") {
             result = pow(result, operand[i + 1]);
         }
     }
