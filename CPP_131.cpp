189c0f62f2215bc3378080571fe6e09290862984
beep boop <githubbot@vadim.me>
2024-07-09 09:50:25 +0200

    Solution 8 of CPP/131, ratio 0.5714285714285714 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ufivtr8m Prompt: Complete the following code given the task description and function signature.

--- CPP_131.cpp
+++ CPP_131.cpp
@@ -1,11 +1,8 @@
-Here is the solution:
-
-int digits(int n) {
+int digits(int n){
     int product = 1;
-    while (n > 0) {
+    while(n > 0){
         int digit = n % 10;
-        if (digit % 2 != 0)
-            product *= digit;
+        if(digit % 2 != 0) product *= digit;
         n /= 10;
     }
     return product;
