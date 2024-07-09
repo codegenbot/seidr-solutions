9767aae46f08f25147e88349bdae2a44c462f1f0
beep boop <githubbot@vadim.me>
2024-07-09 18:46:37 +0200

    Solution 4 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/pudv0wlu Prompt: da6ab7ae-a1e0-4058-9d78-ee333b5f804f.cpp:1:14: error: ‘string’ was not declared in this scope     1 | int is_bored(string S){       |              ^~~~~~ In file included from da6ab7ae-a1e0-4058-9d78-ee333b5f804f.cpp:16: da6ab7ae-a1e0-4058-9d78-ee333b5f804f.cpp: In function ‘int main()’: da6ab7ae-a1e0-4058-9d78-ee333b5f804f.cpp:18:21: error: ‘is_bored’ cannot be used  as a function    18 |     assert (is_bored("You and I are going for a walk") == 0);       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,12 +1,13 @@
+```
 #include <string>
 
-int is_bored(string S) {
+int is_bored(string S){
     int count = 0;
     string boredom = "I";
     size_t pos = 0;
-    while ((pos = S.find('.', pos)) != string::npos || 
-           (pos = S.find('?', pos)) != string::npos || 
-           (pos = S.find('!', pos)) != string::npos) {
+    while ((pos = S.find('.')). != string::npos || 
+           (pos = S.find('?')). != string::npos || 
+           (pos = S.find('!')). != string::npos) {
         if (S.substr(0, boredom.size()).compare(boredom) == 0) {
             count++;
         }
