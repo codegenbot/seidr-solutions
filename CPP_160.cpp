dfa2e53baec34553edc03fdb18cbd8de5abaf577
beep boop <githubbot@vadim.me>
2024-07-09 17:52:48 +0200

    Solution 2 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5a7qtuzk Prompt:       |                       ^~~~~~ d10fc3ab-579c-419c-be8e-fdcb509f5c3a.cpp:1:31: error: ‘operator_’ was not declar ed in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                               ^~~~~~~~~ d10fc3ab-579c-419c-be8e-fdcb509f5c3a.cpp:1:42: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                          ^~~~~~ d10fc3ab-579c-419c-be8e-fdcb509f5c3a.cpp:1:49: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                 ^~~ d10fc3ab-579c-419c-be8e-fdcb509f5c3a.cpp:1:61: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                             ^ In file included from d10fc3ab-579c-419c-be8e-fdcb509f5c3a.cpp:43: d10fc3ab-579c-419c-be8e-fdcb509f5c3a.cpp: In function ‘int main()’: d10fc3ab-579c-419c-be8e-fdcb509f5c3a.cpp:45:23: error: ‘do_algebra’ cannot be us ed as a function    45 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,39 +1,40 @@
+```cpp
 #include <vector>
 #include <cmath>
 
-int do_algebra(vector<string> operators, vector<int> operands) {
+int do_algebra(vector<string> ops, vector<int> operands) {
     int result = 0;
-    for (int i = 0; i < operators.size(); i++) {
-        if (operators[i] == "+") {
+    for (int i = 0; i < ops.size(); i++) {
+        if (ops[i] == "+") {
             result += operands[i];
-        } else if (operators[i] == "-") {
+        } else if (ops[i] == "-") {
             result -= operands[i];
-        } else if (operators[i] == "*") {
-            int temp = 1;
-            for (int j = i; j < operators.size(); j++) {
-                if (operators[j] == "*") {
+        } else if (ops[i] == "*") {
+            int temp = 0;
+            for (int j = i; j < ops.size(); j++) {
+                if (ops[j] == "*") {
                     temp *= operands[j + 1];
-                } else if (operators[j] == "+") {
+                } else if (ops[j] == "+") {
                     result += temp * operands[j + 1];
                     break;
                 }
             }
-        } else if (operators[i] == "//") {
+        } else if (ops[i] == "//") {
             int temp = operands[i + 1];
-            for (int j = i + 1; j < operators.size(); j++) {
-                if (operators[j] == "//") {
+            for (int j = i + 1; j < ops.size(); j++) {
+                if (ops[j] == "//") {
                     temp /= operands[j + 1];
-                } else if (operators[j] == "+") {
+                } else if (ops[j] == "+") {
                     result += temp / operands[j + 1];
                     break;
                 }
             }
-        } else if (operators[i] == "**") {
+        } else if (ops[i] == "**") {
             int temp = pow(operands[i + 1], 1);
-            for (int j = i + 1; j < operators.size(); j++) {
-                if (operators[j] == "**") {
+            for (int j = i + 1; j < ops.size(); j++) {
+                if (ops[j] == "**") {
                     temp = pow(temp, operands[j + 1]);
-                } else if (operators[j] == "+") {
+                } else if (ops[j] == "+") {
                     result += temp;
                     break;
                 }
