fd0597058b201c76da0fffc4b5d0a3ae1008f7b5
beep boop <githubbot@vadim.me>
2024-07-09 15:10:59 +0200

    Solution 4 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4eb0byil Prompt:       |         ^~~ 0a3e8b46-28d2-4b2c-a5a4-8142468d129e.cpp:7:9: error: expected primary-expression  before ‘int’     7 |     set<int> s1(l1.begin(), l1.end());       |         ^~~ 0a3e8b46-28d2-4b2c-a5a4-8142468d129e.cpp:8:9: error: expected primary-expression  before ‘int’     8 |     set<int> s2(l2.begin(), l2.end());       |         ^~~ 0a3e8b46-28d2-4b2c-a5a4-8142468d129e.cpp:10:22: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    10 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                      ^~       |                      l1 0a3e8b46-28d2-4b2c-a5a4-8142468d129e.cpp:10:44: error: ‘s2’ was not declared in this scope; did you mean ‘l2’?    10 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                                            ^~       |                                            l2 0a3e8b46-28d2-4b2c-a5a4-8142468d129e.cpp:11:18: error: ‘intersection’ was not de clared in this scope    11 |         inserter(intersection, intersection.begin()));       |                  ^~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -2,14 +2,14 @@
 #include <algorithm>
 using namespace std;
 
-vector<int> common(vector<int> l1, vector<int> v2) {
-    set<int> common_result;
+vector<int> common(vector<int> l1, vector<int> l2) {
+    set<int> intersection;
     set<int> s1(l1.begin(), l1.end());
-    set<int> s2(v2.begin(), v2.end());
+    set<int> s2(l2.begin(), l2.end());
 
     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-        inserter(common_result, common_result.begin()));
+        inserter(intersection, intersection.begin()));
 
-    vector<int> result(common_result.begin(), common_result.end());
+    vector<int> result(intersection.begin(), intersection.end());
     return result;
 }
