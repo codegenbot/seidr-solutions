78eacc2f627a78e869b1407123f61ed5249910af
beep boop <githubbot@vadim.me>
2024-07-09 04:37:24 +0200

    Solution 50 of Python/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/6qoso50g Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5ce53559-d7d6-4e35-843d-e90976003 c08.py", line 21, in <module>     check(search)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5ce53559-d7d6-4e35-843d-e90976003 c08.py", line 18, in check     assert search([3, 10, 10, 9, 2]) == -1            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_69.py
+++ Python_69.py
@@ -1,16 +1,12 @@
-def search(lst=None):
+def search(lst):
     freq_dict = {}
-    if lst:
-        for num in lst:
-            if num > 0:
-                if num in freq_dict:
-                    freq_dict[num] += 1
-                else:
-                    freq_dict[num] = 1
-    max_num = -1
-    if freq_dict:
-        for key, value in freq_dict.items():
-            if key >= value and key > 0:
-                max_num = key
-                break
-    return max_num if max_num > 0 else 0
+    for num in lst:
+        if num > 0:
+            if num in freq_dict:
+                freq_dict[num] += 1
+            else:
+                freq_dict[num] = 1
+    if not freq_dict:
+        return -1
+    max_num = max(freq_dict.keys())
+    return max_num if max_num > 0 else -1
