40696ddb2d1079bf5ae53538c213dd7373e7705d
beep boop <githubbot@vadim.me>
2024-07-09 15:38:12 +0200

    Solution 21 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/a8totsqj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5cee9ad5-b364-4123-a3b1-b5974858b 8f8.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5cee9ad5-b364-4123-a3b1-b5974858b 8f8.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5cee9ad5-b364-4123-a3b1-b5974858b 8f8.py", line 2, in sort_third     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5cee9ad5-b364-4123-a3b1-b5974858b 8f8.py", line 3, in <listcomp>     x if i % 3 != 0 else sorted([i for i in l if i % 3 == 0])[k]          ^ NameError: name 'i' is not defined. Did you mean: 'id'?

--- Python_33.py
+++ Python_33.py
@@ -1,5 +1,11 @@
 def sort_third(l: list):
     return [
-        sorted([i for i in l if i % 3 == 0])[(len(sorted([i for i in l if i % 3 == 0], reverse=True)) - 1) if x % 3 != 0 else k]
+        sorted([i for i in l if i % 3 == 0])[
+            (
+                (len(sorted([i for i in l if i % 3 == 0], reverse=True)) - 1)
+                if x % 3 != 0
+                else k
+            )
+        ]
         for k, x in enumerate(sorted([x for x in l if x % 3 == 0], reverse=True))
     ]
