a9110dbd220edbf736dbc72ddb1f18a929ecdbed
beep boop <githubbot@vadim.me>
2024-07-09 12:47:09 +0200

    Solution 73 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ejnsoy1t Prompt:       |                              ^~~~~~ 51e93314-2c13-4b42-b9f9-9a02e9cb5cc4.cpp:38:37: error: ‘string’ was not declared  in this scope    38 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 51e93314-2c13-4b42-b9f9-9a02e9cb5cc4.cpp:38:44: error: ‘b’ was not declared in t his scope    38 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 51e93314-2c13-4b42-b9f9-9a02e9cb5cc4.cpp:38:45: error: expression list treated a s compound expression in initializer [-fpermissive]    38 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 51e93314-2c13-4b42-b9f9-9a02e9cb5cc4.cpp:37: 51e93314-2c13-4b42-b9f9-9a02e9cb5cc4.cpp: In function ‘int main()’: 51e93314-2c13-4b42-b9f9-9a02e9cb5cc4.cpp:47:20: error: ‘numerical_letter_grade’ was not declared in this scope    47 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 51e93314-2c13-4b42-b9f9-9a02e9cb5cc4.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,13 +1,12 @@
-```cpp
 #include <vector>
 #include <string>
 
-bool issame(vector<string> a,vector<string>b){
+bool issame(std::vector<std::string> a, std::vector<std::string> b){
     return a==b;
 }
 
-vector<string> numerical_letter_grade(vector<float> grades){
-    vector<string> result;
+std::vector<std::string> numerical_letter_grade(std::vector<float> grades){
+    std::vector<string> result;
     for(float g:grades){
         string letterGrade = "";
         if(g >= 4.0) {
@@ -40,10 +39,4 @@ vector<string> numerical_letter_grade(vector<float> grades){
         result.push_back(letterGrade);
     }
     return result;
-}
-
-int main() {
-    vector<float> grades = {0, 0.7};
-    assert(numerical_letter_grade(grades) == vector<string>({"E", "D-" }));
-    // ...
 }
