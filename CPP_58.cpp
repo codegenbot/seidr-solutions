2aa25212a52faa0b19ba9d5a3cd227d2cdbb4e2a
beep boop <githubbot@vadim.me>
2024-07-10 01:58:16 +0200

    Solution 5 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0hw5ynmd Prompt:       |                    ^~~ 4806eeb1-ccd0-44c7-8fb6-78e83c0d2b8a.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4806eeb1-ccd0-44c7-8fb6-78e83c0d2b8a.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4806eeb1-ccd0-44c7-8fb6-78e83c0d2b8a.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4806eeb1-ccd0-44c7-8fb6-78e83c0d2b8a.cpp:11: 4806eeb1-ccd0-44c7-8fb6-78e83c0d2b8a.cpp: In function ‘int main()’: 4806eeb1-ccd0-44c7-8fb6-78e83c0d2b8a.cpp:21:20: error: ‘common’ was not declared  in this scope    21 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 4806eeb1-ccd0-44c7-8fb6-78e83c0d2b8a.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,11 +1,32 @@
+#include <iostream>
 #include <vector>
-#include <algorithm>
 #include <set>
+
 using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
+    if(a.size() != b.size()) {
+        return false;
+    }
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
-    return vector<int>(set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end()), set_intersection(s1.end()-1, s1.end(), s2.end()-1, s2.end()));
+    vector<int> result;
+    copy(std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), back_inserter(result)));
+    
+    return result;
+}
+
+int main() {
+    assert(issame(common({4, 3, 2, 8}, {}), {}));
+    // ... rest of your code
 }
