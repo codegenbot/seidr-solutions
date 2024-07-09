83efd08c75947c6178f58f7461516740572d61f3
beep boop <githubbot@vadim.me>
2024-07-09 19:25:31 +0200

    Solution 76 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/n9bn7yqf Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ 311552fd-b2d7-4155-8721-cc962123740b.cpp:27:21:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,32 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-
-using namespace std;
-
-string join(const vector<string>& vec, const string& sep) {
-    string result;
-    for (const auto& s : vec) {
-        if (!result.empty()) {
-            result += sep;
-        }
-        result += s;
-    }
-    return result;
-}
-
-string split(const string& str, char sep) {
-    vector<string> tokens;
-    size_t pos = 0;
-    while ((pos = str.find(sep)) != string::npos) {
-        tokens.push_back(str.substr(0, pos));
-        str = str.substr(pos + 1);
-    }
-    tokens.push_back(str);
-    return join(tokens, " ");
-}
-
+```cpp
 string anti_shuffle(string s) {
     vector<string> words;
     string word = "";
@@ -50,4 +22,3 @@ string anti_shuffle(string s) {
         result += w;
     }
     return result;
-}
