c1a9a3cbb865baadccee21b50c0d5b44a5a39ecc
beep boop <githubbot@vadim.me>
2024-07-09 06:05:46 +0200

    Solution 98 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0ywi65gh Prompt:       |                                             ^ afcc6033-ce45-4937-986b-4ee91ed1a990.cpp:44:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    44 | int main(){       |         ^~ afcc6033-ce45-4937-986b-4ee91ed1a990.cpp:44:9: note: remove parentheses to defau lt-initialize a variable    44 | int main(){       |         ^~       |         -- afcc6033-ce45-4937-986b-4ee91ed1a990.cpp:44:9: note: or replace parentheses with  braces to value-initialize a variable afcc6033-ce45-4937-986b-4ee91ed1a990.cpp:44:11: error: a function-definition is not allowed here before ‘{’ token    44 | int main(){       |           ^ afcc6033-ce45-4937-986b-4ee91ed1a990.cpp:46:2: error: expected ‘}’ at end of inp ut    46 | }       |  ^ afcc6033-ce45-4937-986b-4ee91ed1a990.cpp:27:12: note: to match this ‘{’    27 | int main() {       |            ^

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,8 +1,2 @@
 int main 
-{ 
-    vector<string> lst = {"hello", "world", "abc", "abcd"}; 
-    vector<string> result = sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}); 
-
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"})); 
-
-    return 0;
+{
