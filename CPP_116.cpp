413c273ff7111a5a82e46aad3ab2175db0cbfa47
beep boop <githubbot@vadim.me>
2024-07-09 04:05:38 +0200

    Solution 12 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/hvdy68h7 Prompt:       |           ^~~~~~ In file included from c76e5a19-9776-4b85-8b01-d7a38f4b28b9.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c76e5a19-9776-4b85-8b01-d7a38f4b28b9.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c76e5a19-9776-4b85-8b01-d7a38f4b28b9.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c76e5a19-9776-4b85-8b01-d7a38f4b28b9.cpp:19: c76e5a19-9776-4b85-8b01-d7a38f4b28b9.cpp: In function ‘int main()’: c76e5a19-9776-4b85-8b01-d7a38f4b28b9.cpp:29:20: error: ‘sort_array’ was not decl ared in this scope    29 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ c76e5a19-9776-4b85-8b01-d7a38f4b28b9.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,14 +1,22 @@
+```cpp
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(vector<int> a,vector<int>b) {
+    return a == b;
+}
+
+vector<int> sort_vector(vector<int> arr) {
+    auto comp = [&] (int a, int b) {
+        int ones_a = __builtin_popcount(a);
+        int ones_b = __builtin_popcount(b);
+
+        if (ones_a == ones_b)
+            return a - b;
+        else
+            return ones_a - ones_b;
+    };
+
+    sort(arr.begin(), arr.end(), comp);
+    return arr;
 }
