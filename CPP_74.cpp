ca6e6414ae7629321976b4b5eb346fc1bda39d71
beep boop <githubbot@vadim.me>
2024-07-09 03:47:08 +0200

    Solution 22 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8ahd9aop Prompt:       |                              ^~~~~~ 870201ab-d2bf-4fea-b0d9-2eaa7bf0e785.cpp:22:37: error: ‘string’ was not declared  in this scope    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 870201ab-d2bf-4fea-b0d9-2eaa7bf0e785.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 870201ab-d2bf-4fea-b0d9-2eaa7bf0e785.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 870201ab-d2bf-4fea-b0d9-2eaa7bf0e785.cpp:21: 870201ab-d2bf-4fea-b0d9-2eaa7bf0e785.cpp: In function ‘int main()’: 870201ab-d2bf-4fea-b0d9-2eaa7bf0e785.cpp:31:20: error: ‘total_match’ was not dec lared in this scope    31 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 870201ab-d2bf-4fea-b0d9-2eaa7bf0e785.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -2,37 +2,31 @@
 #include <string>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if(a.size() != b.size()) {
+    if (a.size() != b.size()) {
         return false;
     }
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
     return true;
 }
 
-int main() {
-    assert(issame(total_match({"this"}, {}), {}));
-    return 0;
-}
-
 std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<std::string> lst2) {
-    int total_chars1 = 0, total_chars2 = 0;
+    vector<string> result;
 
     for (const auto& str : lst1) {
-        total_chars1 += str.length();
-    }
-    for (const auto& str : lst2) {
-        total_chars2 += str.length();
+        if (!issame(lst1, lst2)) {
+            return lst1;
+        }
     }
 
-    if (total_chars1 < total_chars2) {
-        return lst1;
-    } else if (total_chars1 > total_chars2) {
-        return lst2;
-    } else {
-        return lst1.size() == 0 ? lst1 : lst2;
+    for (const auto& str : lst2) {
+        if (!issame(lst1, lst2)) {
+            return lst2;
+        }
     }
+
+    return lst1;
 }
