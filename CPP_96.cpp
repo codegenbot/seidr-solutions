6b49a0e7b9b9ba8fbb47ac92c33e156f9f670bdd
beep boop <githubbot@vadim.me>
2024-07-09 10:29:38 +0200

    Solution 47 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/c8pse80p Prompt:    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1344f336-8c5b-462d-951b-a65a2bb2d2bb.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1344f336-8c5b-462d-951b-a65a2bb2d2bb.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1344f336-8c5b-462d-951b-a65a2bb2d2bb.cpp:22: 1344f336-8c5b-462d-951b-a65a2bb2d2bb.cpp: In function ‘int main()’: 1344f336-8c5b-462d-951b-a65a2bb2d2bb.cpp:32:20: error: ‘count_up_to’ was not dec lared in this scope    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 1344f336-8c5b-462d-951b-a65a2bb2d2bb.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
@@ -15,30 +17,29 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return true;
 }
 
-std::vector<int> count_up_to(int n);
+vector<int> count_up_to(int n);
 
 int main() {
     int n;
-    std::cin >> n;
-    
-    std::vector<int> primes = count_up_to(n);
-    
+    cin >> n;
+
+    vector<int> primes = count_up_to(n);
+
     for (int prime : primes) {
-        std::cout << prime << " ";
+        cout << prime << " ";
     }
-    
-    std::cout << std::endl;
-    
+    cout << endl;
+
     return 0;
 }
 
-std::vector<int> count_up_to(int n) {
-    std::vector<int> result;
+vector<int> count_up_to(int n) {
+    vector<int> result;
     if (n <= 1) {
         return result;
     }
     
-    std::vector<bool> isPrime(n, true);
+    vector<bool> isPrime(n, true);
     isPrime[0] = isPrime[1] = false;
     
     for (int i = 2; i < n; ++i) {
