fdc507edca5b0ec4ee0912d2d58a32e2b73f27d2
beep boop <githubbot@vadim.me>
2024-07-09 06:52:32 +0200

    Solution 15 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/69phywk9 Prompt: Complete the following code given the task description and function signature.

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,18 +1,19 @@
+#include <vector>
 #include <string>
+
 using namespace std;
 
-string int_to_mini_romank(int number) {
-    const vector<pair<int, string>> romans = {{1000, "M"}, {900, "CM"}, {500, "D"},
-        {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
-    string result;
+string int_to_mini_roman(int num) {
+    vector<pair<int, string>> romanNumeral = {{1000, "M"}, {900, "CM"}, {500, "D"},
+            {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"},
+            {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
 
-    for (const auto& roman : romans) {
-        while (number >= roman.first) {
-            number -= roman.first;
+    string result;
+    for (auto roman : romanNumeral) {
+        while (num >= roman.first) {
+            num -= roman.first;
             result += roman.second;
         }
     }
-
     return result;
 }
