7c5326c2c3ea8dfe03dacf424f9ca790e9a365cc
beep boop <githubbot@vadim.me>
2024-07-09 06:07:38 +0200

    Solution 57 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/a0u5q4g6 Prompt:       |                                     ^~~~~~ 38a5dc95-11bf-4c83-865f-17ef44b49e89.cpp:32:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 38a5dc95-11bf-4c83-865f-17ef44b49e89.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 38a5dc95-11bf-4c83-865f-17ef44b49e89.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 38a5dc95-11bf-4c83-865f-17ef44b49e89.cpp:32:44: error: ‘b’ was not declared in t his scope    32 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 38a5dc95-11bf-4c83-865f-17ef44b49e89.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 38a5dc95-11bf-4c83-865f-17ef44b49e89.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -21,3 +21,8 @@ std::vector<std::string> sorted_list_sum(std::vector<std::string> list) {
     std::sort(list.begin(), list.end());
     return list;
 }
+
+int main() {
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+    return 0;
+}
