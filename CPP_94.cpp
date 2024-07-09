0ca83d93b5472824c3b322b66e93fa51c58283a4
beep boop <githubbot@vadim.me>
2024-07-09 18:50:04 +0200

    Solution 9 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/i7z6yt3c Prompt: d33f9fab-f70b-4c9c-9b6a-222761bd8dd6.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from d33f9fab-f70b-4c9c-9b6a-222761bd8dd6.cpp:27: d33f9fab-f70b-4c9c-9b6a-222761bd8dd6.cpp: In function ‘int main()’: d33f9fab-f70b-4c9c-9b6a-222761bd8dd6.cpp:29:13: error: ‘skjkasdkd’ was not decla red in this scope    29 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ^~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,41 +1,35 @@
-```
 int main() {
-    int skjkasdkd(vector<int> lst){
-        int max_prime = 0;
-        for(int i : lst){
-            if(isPrime(i) && i > max_prime)
-                max_prime = i;
-        }
-        int sum_of_digits = 0;
-        while(max_prime > 0){
-            sum_of_digits += (max_prime % 10);
-            max_prime /= 10;
-        }
-        return sum_of_digits;
-    }
-
-    bool isPrime(int n) {
-        if (n <= 1)
-            return false;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0)
-                return false;
-        }
-        return true;
-    }
-
+    int max_prime = 0;
     vector<int> lst;
-    int num;
-    cout << "Enter the number of elements: ";
-    cin >> num;
-
-    for(int i=0; i<num; i++){
-        cout << "Enter element " << (i+1) << ": ";
-        cin >> lst.push_back();
+    cout << "Enter numbers separated by spaces: ";
+    for(string s; cin >> s;){
+        lst.push_back(stoi(s));
     }
-
-    int result = skjkasdkd(lst);
-
-    cout << "The sum of the digits is: " << result;
+    int sum_of_digits = skjkasdkd(lst);
+    cout << "Sum of digits is: " << sum_of_digits << endl;
     return 0;
+}
+
+int skjkasdkd(vector<int> lst){
+    int max_prime = 0;
+    for(int i : lst){
+        if(isPrime(i) && i > max_prime)
+            max_prime = i;
+    }
+    int sum_of_digits = 0;
+    while(max_prime > 0){
+        sum_of_digits += (max_prime % 10);
+        max_prime /= 10;
+    }
+    return sum_of_digits;
+}
+
+bool isPrime(int n) {
+    if (n <= 1)
+        return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
 }
