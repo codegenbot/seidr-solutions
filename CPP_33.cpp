db9e9ef97103f52c0e9a4aa7513db4d926d92a6a
beep boop <githubbot@vadim.me>
2024-07-09 14:41:56 +0200

    Solution 31 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/fm3jbbbw Prompt: <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’ /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:78:33:   required from ‘void std::vector<_Tp , _Alloc>::reserve(std::vector<_Tp, _Alloc>::size_type) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = long unsigned int]’ 6ebc626c-274d-4a06-b2f0-86a887aeab2f.cpp:21:31:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -6,26 +6,31 @@
 using namespace std;
 
 vector<int> sort_third(vector<int> l) {
-    vector<int> result;
+    vector<int> result(10); // Initialize with some value
     int groupCount = 0;
 
     for (int i : l) {
         if(groupCount % 3 == 0) { 
-            if(result.size() > 0) {
-                sort(result.begin(), result.end());
-                reverse(result.begin(), result.end());
-            }
-            result.push_back(i);
+            result[groupCount] = i;
             groupCount++;
+            if(groupCount % 3 != 0) { 
+                // Sort and reverse the current group
+                sort(result.begin() + groupCount - (groupCount % 3), result.begin() + groupCount);
+                vector<int> temp = result;
+                result.clear(); 
+                result.reserve(temp.size()); 
+                for (int j = temp.size() - 1; j >= 0; j--) {
+                    result.push_back(temp[j]);
+                }
+            }
         } else {
-            result.push_back(i);
+            result[groupCount] = i;
             groupCount++;
         }
     }
 
-    if(groupCount % 3 == 0 && result.size() > 0) {
+    if(groupCount % 3 == 0) {
         sort(result.begin(), result.end());
-        reverse(result.begin(), result.end());
     }
 
     return result;
