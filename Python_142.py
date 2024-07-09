cdcaabfbfa5054ddce9c3a216700a1509771cff1
beep boop <githubbot@vadim.me>
2024-07-09 05:29:53 +0200

    Solution 44 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/22jclxwk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a56deced-f6bb-430a-b371-1840260db d5a.py", line 48, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a56deced-f6bb-430a-b371-1840260db d5a.py", line 18, in check     sum_squares( TypeError: sum_squares() takes 0 positional arguments but 1 was given

--- Python_142.py
+++ Python_142.py
@@ -1,13 +1,1 @@
-def sum_squares():
-    while True:
-        try:
-            lst = list(map(int, input("Enter numbers separated by space: ").split()))
-            break
-        except ValueError:
-            print("Invalid input. Please enter numbers separated by spaces.")
-
-    result = sum(x**2 if i % 3 == 0 and i < len(lst) - 1 and (i + 1) % 4 != 0 \
-                 else x**3 if i % 4 == 0 and i < len(lst) - 2 and (i + 2) % 3 != 0 \
-                 else x for i, x in enumerate(lst))
-
-    return result
+def sum_squares(input_val=None):
