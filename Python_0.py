0caa533e6c5ea99a19bba9663dae7c9603837285
beep boop <githubbot@vadim.me>
2024-07-09 19:45:28 +0200

    Solution 22 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fq8zgi3h Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f92a2d1-a9b6-4b26-b2c6-5b8155838 f9d.py", line 2     for i in range(len(numbers) - 1): IndentationError: unexpected indent

--- Python_0.py
+++ Python_0.py
@@ -1,3 +1,5 @@
 def is_difference_within_threshold(numbers, threshold):
     numbers.sort()
-    return any(abs(numbers[i] - numbers[i+1]) < threshold for i in range(len(numbers) - 1)
+    return any(
+        abs(numbers[i] - numbers[i + 1]) < threshold for i in range(len(numbers) - 1)
+    )
