5e03abfd373e6bc474c9c5ce2d72b543cb0da776
beep boop <githubbot@vadim.me>
2024-07-09 02:06:59 +0200

    Solution 55 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5hsg63q3 Prompt: w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const int*; _Tp = int; _Alloc = std:: allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = i nt; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::allocator_type = std ::allocator<int>]’ 8272bdbe-aaa1-40f1-9794-851ef5f2514d.cpp:26:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,4 +1,4 @@
-int count_nums(vector<int>(nums)) {
+int count_nums(std::vector<int>(nums)) {
     int sign = 1;
     int count = 0;
     for (int num : nums) {
@@ -16,3 +16,8 @@ int count_nums(vector<int>(nums)) {
     }
     return count;
 }
+
+int main() {
+    assert(count_nums({1}) == 1);
+    cout << "Count: " << count_nums({-1234, 5678, -9012}) << endl;
+    return 0;
