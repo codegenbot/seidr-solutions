0c293f20f8529531fec8752d5ac67b3eb81e8440
beep boop <githubbot@vadim.me>
2024-07-09 23:28:29 +0200

    Solution 0 of CPP/75, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nfh5qrue Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_75.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+
+bool is_multiply_prime(int a) {
+    for (int i = 2; i <= sqrt(a); i++) {
+        int remainder = a % i;
+        if (remainder == 0) {
+            int count = 0;
+            for (int j = i; j <= a / i; j++) {
+                if (a % j == 0) {
+                    count++;
+                }
+            }
+            if (count >= 3) return true;
+        }
+    }
+    return false;
+}
