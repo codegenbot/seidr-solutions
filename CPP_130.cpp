99995df29532696bc5b1209901681323e2bff513
beep boop <githubbot@vadim.me>
2024-07-09 08:51:41 +0200

    Solution 4 of CPP/130, ratio 0.8 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/yxxraarm Prompt: Complete the following code given the task description and function signature.

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -3,17 +3,16 @@
 using namespace std;
 
 vector<int> tri(int n) {
-    vector<int> result(0);
-    if (n == 0) return result;
-    result.push_back(3);
-    if (n == 1) return result;
-    result.push_back(1);
-    if (n == 2) return result;
-    for (int i = 3; i <= n; ++i) {
-        int val = 0;
-        if (i % 2 == 0) val = 1 + i / 2;
-        else val = result[i-1] + result[i-2] + result[i-3];
-        result.push_back(val);
+    vector<int> result(1, 3);
+    if (n > 0) {
+        for (int i = 2; i <= n; i++) {
+            int val;
+            if (i % 2 == 0)
+                val = 1 + i / 2;
+            else
+                val = result[i - 1] + result[i - 2] + result[i - 3];
+            result.push_back(val);
+        }
     }
     return result;
 }
