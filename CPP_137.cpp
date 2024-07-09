5ea620583ba35ca77ce8307943aa4b6b5cae4ec9
beep boop <githubbot@vadim.me>
2024-07-09 13:25:33 +0200

    Solution 3 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/z1i35unk Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,55 +1,37 @@
-Here is the completed code:
-
-```cpp
+#include <iostream>
 #include <string>
-#include <algorithm>
 #include <boost/any.hpp>
 
 using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return boost::any_cast<int>(a) > boost::any_cast<int>(b)
-            ? a
-            : (boost::any_cast<int>(a) < boost::any_cast<int>(b) ? b : boost::any("None"));
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        return boost::any_cast<float>(a) > boost::any_cast<float>(b)
-            ? a
-            : (boost::any_cast<float>(a) < boost::any_cast<float>(b) ? b : boost::any("None"));
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return boost::any_cast<string>(a) > boost::any_cast<string>(b)
-            ? a
-            : (boost::any_cast<string>(a) < boost::any_cast<string>(b) ? b : boost::any("None"));
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return boost::any_cast<float>(b) > boost::any_cast<int>(a)
-            ? a
-            : (boost::any_cast<float>(b) < boost::any_cast<int>(a) ? b : boost::any("None"));
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int a_int = boost::any_cast<int>(a);
-        return a_int > std::stof(boost::any_cast<string>(b))
-            ? a
-            : (a_int < std::stof(boost::any_cast<string>(b)) ? b : boost::any("None"));
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        return boost::any_cast<float>(a) > std::stof(boost::any_cast<string>(b))
-            ? a
-            : (boost::any_cast<float>(a) < std::stof(boost::any_cast<string>(b)) ? b : boost::any("None"));
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        return std::stof(a.cast<std::string>()) > boost::any_cast<int>(b)
-            ? a
-            : (std::stof(a.cast<std::string>()) < boost::any_cast<int>(b) ? b : boost::any("None"));
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        return std::stof(a.cast<std::string>()) > boost::any_cast<float>(b)
-            ? a
-            : (std::stof(a.cast<std::string>()) < boost::any_cast<float>(b) ? b : boost::any("None"));
-    }
-    else {
+        int x = boost::any_cast<int>(a);
+        int y = boost::any_cast<int>(b);
+        return (x > y) ? a : ((x < y) ? b : boost::any("None"));
+    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        float x = boost::any_cast<float>(a);
+        float y = boost::any_cast<float>(b);
+        return (x > y) ? a : ((x < y) ? b : boost::any("None"));
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string x = boost::any_cast<string>(a);
+        string y = boost::any_cast<string>(b);
+        return (stof(x) > stof(y)) ? a : ((stof(x) < stof(y)) ? b : boost::any("None"));
+    } else if (a.type() == typeid(string) && b.type() != typeid(string)) {
+        string x = boost::any_cast<string>(a);
+        return (stof(x) > boost::any_cast<float>(b)) ? a : ((stof(x) < boost::any_cast<float>(b)) ? b : boost::any("None"));
+    } else if (b.type() == typeid(string) && a.type() != typeid(string)) {
+        string y = boost::any_cast<string>(b);
+        return (stof(y) > boost::any_cast<float>(a)) ? b : ((stof(y) < boost::any_cast<float>(a)) ? a : boost::any("None"));
+    } else {
         return boost::any("None");
     }
+}
+
+int main() {
+    cout << boost::any_cast<string>(compare_one(1, 2.5)) << endl;
+    cout << boost::any_cast<string>(compare_one(1, "2,3")) << endl;
+    cout << boost::any_cast<string>(compare_one("5,1", "6")) << endl;
+    cout << boost::any_cast<string>(compare_one("1", 1)) << endl;
+    return 0;
 }
