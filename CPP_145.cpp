913c370af81c15171a004cb1fe6c277649e4bb09
beep boop <githubbot@vadim.me>
2024-07-09 22:53:31 +0200

    Solution 59 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/aeycljm6 Prompt:    28 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ ce065964-d7af-483a-a640-5ed4dc0a9c2c.cpp:28:27: error: ‘vector’ was not declared  in this scope    28 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ce065964-d7af-483a-a640-5ed4dc0a9c2c.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from ce065964-d7af-483a-a640-5ed4dc0a9c2c.cpp:27: ce065964-d7af-483a-a640-5ed4dc0a9c2c.cpp: In function ‘int main()’: ce065964-d7af-483a-a640-5ed4dc0a9c2c.cpp:37:20: error: ‘order_by_points’ was not  declared in this scope    37 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ ce065964-d7af-483a-a640-5ed4dc0a9c2c.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -2,15 +2,7 @@
 #include <algorithm>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
 std::vector<int> order_by_points(std::vector<int> nums) {
