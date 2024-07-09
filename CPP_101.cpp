3b1d8a8a58b19eed745eb0d68fce23a6e03dfa1f
beep boop <githubbot@vadim.me>
2024-07-09 19:31:18 +0200

    Solution 45 of CPP/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/4szsaqcj Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ 455155cd-d610-4fff-a130-e7017a652c74.cpp:20:33:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_101.cpp
+++ CPP_101.cpp
@@ -13,32 +13,22 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b){
 
 std::vector<std::string> words_string(std::string s){
     std::vector<std::string> result;
-    int size = 2;
-    std::vector<std::string>().swap(result);
-    result.reserve(size);
-
     std::string word = "";
     for(int i=0; i<s.length(); i++){
         if(s[i] == ' ' || s[i] == ','){
             if(!word.empty()){
-                if(result.size() >= size) {
-                    result.resize(result.capacity()*2 + 1);
-                    size *= 2;
-                }
                 result.push_back(word);
                 word = "";
+                if(result.capacity() < result.size() + 1) {
+                    result.reserve(result.size() * 2 + 1);
+                }
             }
         }else{
             word += s[i];
         }
     }
     if(!word.empty())
-        if(result.size() >= size) {
-            result.resize(result.capacity()*2 + 1);
-            size *= 2;
-        }
-        else result.push_back(word);
-
+        result.push_back(word);
     return result;
 }
 
