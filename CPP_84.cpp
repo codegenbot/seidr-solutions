423a0eee7e9c9aa2d7f385c209ade0ce71a802e1
beep boop <githubbot@vadim.me>
2024-07-09 17:28:00 +0200

    Solution 87 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/155pyajg Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <string>
-#include <cassert>
 
 long long solve(int n) {
     std::string binary;
-    while(n > 0) {
-        if(n % 2 == 0)
-            binary.insert(0, "0");
-        else
-            binary.insert(0, "1");
+    std::string str = (n < 0 ? "-" : "");
+    do {
+        str.insert(0, n % 2 == 0 ? "0" : "1");
         n /= 2;
-    }
+    } while(n > 0);
+    binary = str;
     return (binary.length() == 0 ? -1 : std::stoll(binary.c_str(), nullptr, 2));
 }
 
 int main() {
-    assert(solve(963) == 1010LL);
+    assert(solve(963) == 1010);
+    return 0;
 }
