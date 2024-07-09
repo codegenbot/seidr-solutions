b21a7b59c919edf087f8c31ad7a012bcdd3e78d0
beep boop <githubbot@vadim.me>
2024-07-09 10:58:21 +0200

    Solution 10 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/m7uvjh8o Prompt: _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 5b3c99ab-d078-4ad0-9f4c-0962b923b53b.cpp:9:30:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 5b3c99ab-d078-4ad0-9f4c-0962b923b53b.cpp:9:30:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,6 +1,5 @@
+```cpp
 #include <string>
-#include <openssl/ssl.h>
-#include <openssl/crypto.h>
 #include <openssl/md5.h>
 #include <openssl/evp.h>
 
@@ -9,14 +8,18 @@ using namespace std;
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
-    unsigned char* data = (unsigned char*)text.c_str();
-    unsigned char md5[16];
-    EVP_MD5((const unsigned char*)data, text.size(), &md5);
+    unsigned char data[text.size()];
+    memcpy(data, text.c_str(), text.size());
+    EVP_MD5_CTX md5_ctx;
+    unsigned char md5_sum[16];
+    EVP_MD5Init(&md5_ctx);
+    EVP_MD5Update(&md5_ctx, &data[0], text.size());
+    EVP_MD5Final(md5_sum, &md5_ctx);
 
     string result;
     for (int i = 0; i < 16; ++i) {
         char buffer[3];
-        sprintf(buffer, "%02x", md5[i]);
+        sprintf(buffer, "%02x", md5_sum[i]);
         result += buffer;
     }
 
