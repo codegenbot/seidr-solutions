30e9929530caf747f0c45d181e8728b936c30c5d
beep boop <githubbot@vadim.me>
2024-07-09 12:56:48 +0200

    Solution 26 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/gyjrac6n Prompt: Program did not receive expected input

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -7,7 +7,8 @@ int main() {
     vector<int> nums;
     int num;
     
-    while (cin >> num && !cin.eof()) {
+    while (!cin.eof()) {
+        cin >> num;
         nums.push_back(num);
     }
     
@@ -20,7 +21,7 @@ int main() {
     int prefixSum = 0;
     int index = 0;
     
-    while (prefixSum < target && index < nums.size()) {
+    while (prefixSum < target) {
         prefixSum += nums[index];
         ++index;
     }
@@ -29,32 +30,11 @@ int main() {
         for (int i = 0; i < index; ++i) {
             cout << nums[i] << endl;
         }
-        cout << endl;
-        for (int i = index; i < nums.size(); ++i) {
+    } else {
+        for (int i = 0; i < index; ++i) {
             cout << nums[i] << endl;
         }
-    } else {
-        int diff1 = target - prefixSum;
-        int diff2 = prefixSum - nums[index - 1];
-        if (diff1 <= diff2) {
-            for (int i = 0; i < index; ++i) {
-                cout << nums[i] << endl;
-            }
-            cout << endl;
-            for (int i = index; i < nums.size(); ++i) {
-                cout << nums[i] << endl;
-            }
-        } else {
-            for (int i = 0; i < index - 1; ++i) {
-                cout << nums[i] << endl;
-            }
-            cout << endl;
-            cout << nums[index - 1] << endl;
-            for (int i = index; i < nums.size(); ++i) {
-                cout << nums[i] << endl;
-            }
-        }
     }
-    
+
     return 0;
 }
