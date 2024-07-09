0b4ca2dc72de1df6f5f65828b0b0f3c7797527e3
beep boop <githubbot@vadim.me>
2024-07-09 03:43:40 +0200

    Solution 96 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/1yy7p033 Prompt: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 71b5b413-2d77-4b30-a2c4-6e2c2efd3e95.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from /usr/include/c++/11/cassert:44,                  from 71b5b413-2d77-4b30-a2c4-6e2c2efd3e95.cpp:2: 71b5b413-2d77-4b30-a2c4-6e2c2efd3e95.cpp: In function ‘int main()’: 71b5b413-2d77-4b30-a2c4-6e2c2efd3e95.cpp:10:21: error: ‘my_strlen’ cannot be use d as a function    10 |     assert(my_strlen("asdasnakj") == 12);       |            ~~~~~~~~~^~~~~~~~~~~~~ 71b5b413-2d77-4b30-a2c4-6e2c2efd3e95.cpp: At global scope: 71b5b413-2d77-4b30-a2c4-6e2c2efd3e95.cpp:15:5: error: redefinition of ‘int main( )’    15 | int main(){       |     ^~~~ 71b5b413-2d77-4b30-a2c4-6e2c2efd3e95.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,12 +1,8 @@
 #include <string>
 #include <cassert>
-#include <cstring>
 
-int my_string_length(std::string str){
+int my_length(std::string str) {
     return str.length();
 }
 
-int main() {
-    assert(my_string_length("asdasnakj") == 12); 
-    return 0;
-}
+assert(my_length("asdasnakj") == 12);
