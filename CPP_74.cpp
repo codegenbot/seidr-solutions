f0dae4abb9e11b4e3c93686047998c13184b2028
beep boop <githubbot@vadim.me>
2024-07-09 09:40:53 +0200

    Solution 54 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/vgnxrdbb Prompt: b7b6bb82-f541-46f4-a629-6688e48ac02b.cpp:23:37: error: ‘string’ was not declared  in this scope    23 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b7b6bb82-f541-46f4-a629-6688e48ac02b.cpp:23:44: error: ‘b’ was not declared in t his scope    23 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from b7b6bb82-f541-46f4-a629-6688e48ac02b.cpp:22: b7b6bb82-f541-46f4-a629-6688e48ac02b.cpp: In function ‘int main()’: b7b6bb82-f541-46f4-a629-6688e48ac02b.cpp:32:20: error: ‘total_match’ was not dec lared in this scope    32 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ b7b6bb82-f541-46f4-a629-6688e48ac02b.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ b7b6bb82-f541-46f4-a629-6688e48ac02b.cpp: In function ‘int totalChars(int)’: b7b6bb82-f541-46f4-a629-6688e48ac02b.cpp:9:1: warning: control reaches end of no n-void function [-Wreturn-type]     9 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -4,9 +4,17 @@
 
 using namespace std;
 
+int totalChars(const vector<string>& lst) {
+    int total = 0;
+    for (const string& str : lst) {
+        total += str.size();
+    }
+    return total;
+}
+
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    if (lst2.size() == 0) {
-        return {};
+    if (totalChars(lst1) < totalChars(lst2)) {
+        return lst1;
     }
     return lst2;
 }
