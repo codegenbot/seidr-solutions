0d2129175527a717792872bb53000894efa35fd3
beep boop <githubbot@vadim.me>
2024-07-09 13:56:14 +0200

    Solution 7 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7dqgrwjn Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 801de475-b5fb-4f7c-b4c4-c5e7a03d614c.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,28 +1,29 @@
+```
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<float> vec1,std::vector<float>vec2){
-    if(vec1.size() != vec2.size()) return false;
-    for(int i=0; i<vec1.size(); i++){
-        if(abs(vec1[i]) != abs(vec2[i])) return false;
+bool checkIfSame(std::vector<float> a, std::vector<float> b){
+    if(a.size() != b.size()) return false;
+    for(int i=0; i<a.size(); i++){
+        if(abs(a[i]) != abs(b[i])) return false;
     }
     return true;
 }
 
-std::vector<float> sort_even(std::vector<float> input) {
-    std::vector<float> result(input.size());
+std::vector<float> sortEven(std::vector<float> l) {
+    std::vector<float> result(l.size());
     int j = 0;
-    for (int i = 0; i < input.size(); i++) {
+    for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            for(int k=i; k< input.size(); k=k+2){
-                result[j++] = input[k];
+            for(int k=i; k< l.size(); k=k+2){
+                result[j++] = l[k];
             }
             std::sort(result.begin(),result.end());
             for(int i=0; i<4 && j<i+4;i++){
                 result[i] = 0;
             }
         } else {
-            result[i] = input[i];
+            result[i] = l[i];
         }
     }
     return result;
