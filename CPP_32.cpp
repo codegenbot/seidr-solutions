5b110e84b3ecf81700600b82d09bac5ab36fc3a6
beep boop <githubbot@vadim.me>
2024-07-09 03:48:15 +0200

    Solution 81 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nvqm4xhw Prompt: In file included from /usr/include/c++/11/vector:67,                  from 93092c7c-b436-46f9-8158-e829fe9e5983.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 93092c7c-b436-46f9-8158-e829fe9e5983.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 93092c7c-b436-46f9-8158-e829fe9e5983.cpp:26:16: error: expected primary-expressi on before ‘double’    26 |         vector<double> coeffs = {};       |                ^~~~~~ 93092c7c-b436-46f9-8158-e829fe9e5983.cpp:31:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    31 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 93092c7c-b436-46f9-8158-e829fe9e5983.cpp:34:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    34 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -2,16 +2,16 @@
 #include <vector>
 #include <cmath>
 
-double poly(std::vector<double> a_coeffs, double x) {
+double poly(std::vector<double> coefficient, double x) {
     double sum = 0;
-    for (int i = 0; i < a_coeffs.size(); i++) {
-        sum += a_coeffs[i] * pow(x, i);
+    for (int i = 0; i < coefficient.size(); i++) {
+        sum += coefficient[i] * pow(x, i);
     }
     return sum;
 }
 
-double find_zero(std::vector<double> a_xs){
-    if(a_xs.size() % 2 != 0) return -1; 
-    double root = -(a_xs[1])/(2*a_xs[0]); 
+double find_zero(std::vector<double> x_coords){
+    if(x_coords.size() % 2 != 0) return -1; 
+    double root = -x_coords[1]/(2*x_coords[0]); 
     return std::round(root);
 }
