7daaa2279ecb3667fc1057041612c6c2870c9958
beep boop <githubbot@vadim.me>
2024-07-09 12:54:36 +0200

    Solution 77 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qnqbku9p Prompt: <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = std::pair<int, std::__cxx11::basic_string<char> >; _Alloc = std::allocato r<std::pair<int, std::__cxx11::basic_string<char> > >; std::_Vector_base<_Tp, _A lloc>::pointer = std::pair<int, std::__cxx11::basic_string<char> >*; size_t = lo ng unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {int, const char (&)[2]}; _Tp = std::pair<int, std::__cxx11::basic_ string<char> >; _Alloc = std::allocator<std::pair<int, std::__cxx11::basic_strin g<char> > >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::pair<int, std ::__cxx11::basic_string<char> > >::iterator]’ /usr/include/c++/11/bits/vector.tcc:121:21:   required from ‘std::vector<_Tp, _A lloc>::reference std::vector<_Tp, _Alloc>::emplace_back(_Args&& ...) [with _Args  = {int, const char (&)[2]}; _Tp = std::pair<int, std::__cxx11::basic_string<cha r> >; _Alloc = std::allocator<std::pair<int, std::__cxx11::basic_string<char> > >; std::vector<_Tp, _Alloc>::reference = std::pair<int, std::__cxx11::basic_stri ng<char> >&]’ bcc905a4-75b3-4ea2-b311-e9193f840740.cpp:21:26:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::pair<int, std::__cxx11::basic_string<char> > >’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <vector>
 #include <string>
@@ -6,19 +5,19 @@
 std::vector<std::pair<int, std::string>> romanMap;
 
 int main() {
-    romanMap.push_back({1000, "M"});
-    romanMap.push_back({900, "CM"});
-    romanMap.push_back({500, "D"});
-    romanMap.push_back({400, "CD"});
-    romanMap.push_back({100, "C"});
-    romanMap.push_back({90, "XC"});
-    romanMap.push_back({50, "L"});
-    romanMap.push_back({40, "XL"});
-    romanMap.push_back({10, "X"});
-    romanMap.push_back({9, "IX"});
-    romanMap.push_back({5, "V"});
-    romanMap.push_back({4, "IV"});
-    romanMap.push_back({1, "I"});
+    romanMap.push_back({ 1000, "M" });
+    romanMap.push_back({ 900, "CM" });
+    romanMap.push_back({ 500, "D" });
+    romanMap.push_back({ 400, "CD" });
+    romanMap.push_back({ 100, "C" });
+    romanMap.push_back({ 90, "XC" });
+    romanMap.push_back({ 50, "L" });
+    romanMap.push_back({ 40, "XL" });
+    romanMap.push_back({ 10, "X" });
+    romanMap.push_back({ 9, "IX" });
+    romanMap.push_back({ 5, "V" });
+    romanMap.push_back({ 4, "IV" });
+    romanMap.emplace_back(1, "I");
 
     for(int i = 1; i <= 3999; i++) { 
         std::cout << i << ": " << int_to_roman(i) << std::endl;
