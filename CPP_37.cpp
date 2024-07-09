3025797038fed59ae29379ccafa02da9b8f17037
beep boop <githubbot@vadim.me>
2024-07-09 08:08:05 +0200

    Solution 52 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ibgdwwkp Prompt:    31 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 116f7894-db8a-496b-8808-164f42f27d26.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 116f7894-db8a-496b-8808-164f42f27d26.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from 116f7894-db8a-496b-8808-164f42f27d26.cpp:30: 116f7894-db8a-496b-8808-164f42f27d26.cpp: In function ‘int main()’: 116f7894-db8a-496b-8808-164f42f27d26.cpp:40:20: error: ‘sort_even’ was not decla red in this scope    40 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 116f7894-db8a-496b-8808-164f42f27d26.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -3,20 +3,13 @@
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
 vector<float> sort_even(vector<float> l);
-
-bool issame(vector<float> a, vector<float> b){
-    return a == b;
-}
+bool issame(vector<float> a, vector<float> b);
 
 vector<float> sort_even(vector<float> l) {
     vector<float> even_indices;
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) {
-            even_indices.push_back(l[i]);
-        }
+    for (int i = 0; i < l.size(); i += 2) {
+        even_indices.push_back(l[i]);
     }
     sort(even_indices.begin(), even_indices.end());
 
@@ -33,7 +26,11 @@ vector<float> sort_even(vector<float> l) {
     return result;
 }
 
+bool issame(vector<float> a, vector<float> b) {
+    return a == b;
+}
+
 int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
-    return 0;
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}),
+                   {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
 }
