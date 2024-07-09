9782de03142ec63c0c2c55c13a67a2f8779de2f1
beep boop <githubbot@vadim.me>
2024-07-09 05:57:46 +0200

    Solution 54 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/bwaer37w Prompt: 4652261d-3a2f-4f91-9fac-20195d458e90.cpp:1:1: error: stray ‘`’ in program     1 | ```string       | ^ 4652261d-3a2f-4f91-9fac-20195d458e90.cpp:1:2: error: stray ‘`’ in program     1 | ```string       |  ^ 4652261d-3a2f-4f91-9fac-20195d458e90.cpp:1:3: error: stray ‘`’ in program     1 | ```string       |   ^ 4652261d-3a2f-4f91-9fac-20195d458e90.cpp:1:4: error: ‘string’ does not name a ty pe     1 | ```string       |    ^~~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -1,2 +1,10 @@
 ```cpp
-return "Your tweet has " + to_string(min(tweet.length(), 140)) + " characters";
+string validateTweet(string tweet) {
+    if (tweet.empty()) {
+        return "You didn't type anything";
+    }
+    if (tweet.length() > 140) {
+        return "Too many characters";
+    }
+    return "Your tweet has " + to_string(tweet.length()) + " characters";
+}
