af10f964d10526dd75ebd2e6a77392275c659cc9
beep boop <githubbot@vadim.me>
2024-07-09 14:26:29 +0200

    Solution 60 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/9whws9y5 Prompt:       |                    ^~~ 9e4cd738-4fa2-452c-921a-ee1124d9ea8e.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9e4cd738-4fa2-452c-921a-ee1124d9ea8e.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9e4cd738-4fa2-452c-921a-ee1124d9ea8e.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9e4cd738-4fa2-452c-921a-ee1124d9ea8e.cpp:15: 9e4cd738-4fa2-452c-921a-ee1124d9ea8e.cpp: In function ‘int main()’: 9e4cd738-4fa2-452c-921a-ee1124d9ea8e.cpp:25:20: error: ‘factorize’ was not decla red in this scope    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ 9e4cd738-4fa2-452c-921a-ee1124d9ea8e.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -3,32 +3,7 @@
 
 using namespace std;
 
-vector<int> factorize(int n);
 bool issame(vector<int> a, vector<int> b);
-
-bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
-int main(){
-    int n;
-    cin >> n;
-    
-    vector<int> factors = factorize(n);
-    
-    for(int factor : factors){
-        cout << factor << " ";
-    }
-    return 0;
-}
 
 vector<int> factorize(int n){
     vector<int> factors;
@@ -42,4 +17,21 @@ vector<int> factorize(int n){
         factors.push_back(n);
     }
     return factors;
+}
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    // Main function code here
+    return 0;
 }
