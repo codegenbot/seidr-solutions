92ce962502b7ebb11a6016ff4087d0ae1e723c16
beep boop <githubbot@vadim.me>
2024-07-09 06:48:46 +0200

    Solution 15 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/n7rv34qt Prompt: Complete the following code given the task description and function signature.

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,9 +1,13 @@
-long long double_the_difference(vector<float> lst){
+long long double_the_difference(vector<float> lst) {
     long long sum = 0;
-    for(float num : lst){
-        if(int(num) > 0 && (int(num) & 1)){
-            sum += pow((int(num)), 2);
+    for (float num : lst) {
+        if (num > 0 && is_integer(num)) {
+            sum += pow(static_cast<int>(num), 2);
         }
     }
     return sum;
+}
+
+bool is_integer(float f) {
+    return floor(f) == f;
 }
