7eaf1f069d7f6fbb2fdbe30b10442ff9a2a841fb
beep boop <githubbot@vadim.me>
2024-07-09 19:17:41 +0200

    Solution 6 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/62lbc4m4 Prompt: 77fcbc6b-30fb-460a-b8e9-d32293fa9b1b.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string exchange(vector<int> lst1, vector<int> lst2) {       | ^~~~~~ In file included from 77fcbc6b-30fb-460a-b8e9-d32293fa9b1b.cpp:11: 77fcbc6b-30fb-460a-b8e9-d32293fa9b1b.cpp: In function ‘int main()’: 77fcbc6b-30fb-460a-b8e9-d32293fa9b1b.cpp:13:13: error: ‘exchange’ was not declar ed in this scope    13 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,20 +1,9 @@
-string exchange(vector<int> lst1, vector<int> lst2) {
+std::string exchange(vector<int> lst1, vector<int> lst2) {
     int oddCount = 0;
     for (int num : lst1) {
         if (num % 2 != 0) {
             oddCount++;
         }
     }
-    for (int num : lst2) {
-        if (num % 2 == 0 && oddCount > 0) {
-            return "YES";
-        } else if (num % 2 != 0) {
-            oddCount--;
-        }
-    }
-    if (oddCount == 0) {
-        return "YES";
-    } else {
-        return "NO";
-    }
+    return oddCount == 0 ? "YES" : "NO";
 }
