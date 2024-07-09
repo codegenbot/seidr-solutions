17896f59168e6d03dd69ec24b16a3889b0152306
beep boop <githubbot@vadim.me>
2024-07-09 09:41:30 +0200

    Solution 49 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/kzopjomk Prompt: In file included from /usr/include/c++/11/string:39,                  from cf1124fc-1586-4081-817c-64b86387a2c9.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from cf1124fc-1586-4081-817c-64b86387a2c9.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ cf1124fc-1586-4081-817c-64b86387a2c9.cpp:54:44: error: ‘b’ was not declared in t his scope    54 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from cf1124fc-1586-4081-817c-64b86387a2c9.cpp:53: cf1124fc-1586-4081-817c-64b86387a2c9.cpp: In function ‘int main()’: cf1124fc-1586-4081-817c-64b86387a2c9.cpp:63:20: error: ‘numerical_letter_grade’ was not declared in this scope    63 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ cf1124fc-1586-4081-817c-64b86387a2c9.cpp:63:19: error: ‘issame’ cannot be used a s a function    63 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -52,7 +52,4 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
     return letter_grades;
 }
 
-int main() {
-    assert(issame(numerical_letter_grade({0, 0.7}), {"E", "D-"}));
-    return 0;
-}
+assert(issame(numerical_letter_grade({0, 0.7}), {"E", "D-"}));
