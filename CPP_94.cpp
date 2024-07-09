f346f09c76e1c5f93c22fa5d38c7632c35fbc563
beep boop <githubbot@vadim.me>
2024-07-09 18:51:43 +0200

    Solution 11 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/i7z6yt3c Prompt:       | ^~~~~~~~~~~~~ af2de8f6-8e3a-4ad5-b912-eec8eb2bfb2e.cpp:29:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    29 | int main(){       |         ^~ af2de8f6-8e3a-4ad5-b912-eec8eb2bfb2e.cpp:29:9: note: remove parentheses to defau lt-initialize a variable    29 | int main(){       |         ^~       |         -- af2de8f6-8e3a-4ad5-b912-eec8eb2bfb2e.cpp:29:9: note: or replace parentheses with  braces to value-initialize a variable af2de8f6-8e3a-4ad5-b912-eec8eb2bfb2e.cpp:29:11: error: a function-definition is not allowed here before ‘{’ token    29 | int main(){       |           ^ af2de8f6-8e3a-4ad5-b912-eec8eb2bfb2e.cpp:31:2: error: expected ‘}’ at end of inp ut    31 | }       |  ^ af2de8f6-8e3a-4ad5-b912-eec8eb2bfb2e.cpp:16:20: note: to match this ‘{’    16 | bool isPrime(int n){       |                    ^

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,5 +1,16 @@
 int main() {
-    vector<int> lst = {127, 97, 8192};
-    int result = skjkasdkd(lst);
-    cout << "The sum of digits is: " << result << endl;
+    int skjkasdkd(vector<int> lst){
+        int maxPrime = 0;
+        for(int i : lst){
+            if(i > 1 && isPrime(i)){
+                if(maxPrime < i) maxPrime = i;
+            }
+        }
+        int sumOfDigits = 0;
+        while(maxPrime > 0){
+            sumOfDigits += maxPrime % 10;
+            maxPrime /= 10;
+        }
+        return sumOfDigits;
+    }
 }
