5bb283a3865844c6d3a885dd703f5609bb37452b
beep boop <githubbot@vadim.me>
2024-07-09 08:48:30 +0200

    Solution 77 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/duuoprah Prompt: 7c0a6670-307c-4a03-8cc5-c13508dcecf4.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the solution:       | ^~~~ In file included from 7c0a6670-307c-4a03-8cc5-c13508dcecf4.cpp:15: 7c0a6670-307c-4a03-8cc5-c13508dcecf4.cpp: In function ‘int main()’: 7c0a6670-307c-4a03-8cc5-c13508dcecf4.cpp:17:13: error: ‘next_smallest’ was not d eclared in this scope    17 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ^~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,28 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
 int main() {
     vector<int> lst;
     int n;
-    cout<<"Enter number of elements in the list: ";
-    cin>>n;
-    for(int i=0;i<n;i++){
-        int x;
-        cout<<"Enter element "<<i+1<<": ";
-        cin>>x;
-        lst.push_back(x);
+    cout << "Enter number of elements: ";
+    cin >> n;
+    cout << "Enter " << n << " integers: ";
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        lst.push_back(num);
     }
-    cout<<"Next smallest element is :"<<next_smallest(lst)<<endl;
+    cout << "The next smallest integer is: " << next_smallest(lst) << endl;
     return 0;
-}
-
-int next_smallest(vector<int> lst) {
-    if (lst.empty()) return -1; 
-    vector<int> sorted_lst = lst;
-    sort(sorted_lst.begin(), sorted_lst.end());
-    for (int i = 0; i < sorted_lst.size() - 1; i++) {
-        if (sorted_lst[i] != sorted_lst[i + 1]) {
-            return sorted_lst[i + 1];
-        }
-    }
-    return -1; 
 }
