10898ceacd91a5392df73bff87acc52a8e975554
beep boop <githubbot@vadim.me>
2024-07-09 20:57:26 +0200

    Solution 4 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/2wsaf73z Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ac8c726c-ee8c-41db-8a6c-5760d88af b10.py", line 8     return str(float(format(total_price,".1f"))))                                                 ^ SyntaxError: unmatched ')'

--- shopping-list.py
+++ shopping-list.py
@@ -5,4 +5,4 @@ def shopping_list():
     total_price = sum(
         price - (price * discount / 100) for price, discount in zip(prices, discounts)
     )
-    return str(format(total_price, ".1f"))
+    return str(format(total_price,".1f"))
