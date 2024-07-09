0fb475eb4fc42ebc1b52db7050e2d9a9f8bf50bb
beep boop <githubbot@vadim.me>
2024-07-09 17:13:55 +0200

    Solution 89 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/29z5qmt1 Prompt: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from c8fce66d-1fcc-41f6-9f7d-a82de28b767e.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c8fce66d-1fcc-41f6-9f7d-a82de28b767e.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from c8fce66d-1fcc-41f6-9f7d-a82de28b767e.cpp:20: c8fce66d-1fcc-41f6-9f7d-a82de28b767e.cpp: In function ‘int main()’: c8fce66d-1fcc-41f6-9f7d-a82de28b767e.cpp:30:25: error: invalid initialization of  reference of type ‘const std::vector<std::vector<int> >&’ from expression of ty pe ‘<brace-enclosed initializer list>’    30 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ~~~~~^~~~~~~~~~~~~~ c8fce66d-1fcc-41f6-9f7d-a82de28b767e.cpp:16:61: note: in passing argument 1 of ‘ std::vector<int> pluck(const std::vector<std::vector<int> >&, int)’    16 | std::vector<int> pluck(const std::vector<std::vector<int>>& arr, int ind ex) {       |                        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,26 +1,18 @@
+#include <iostream>
 #include <vector>
-#include <algorithm>
 
-int main() {
-    std::vector<int> vec;
-
-    // Define the pluck function
-    std::vector<int> pluck(std::vector<int> nums) {
-        std::vector<int> result;
-        for (int num : nums) {
-            if (num == 7) {
-                result.push_back(num);
-            }
+bool issame(const std::vector<int>& v1, const std::vector<int>& v2) {
+    if (v1.size() != v2.size()) {
+        return false;
+    }
+    for (int i = 0; i < v1.size(); ++i) {
+        if (v1[i] != v2[i]) {
+            return false;
         }
-        return result;
     }
+    return true;
+}
 
-    // Define the issame function
-    bool issame(std::vector<int> a, std::vector<int> b) {
-        return a.size() == b.size();
-    }
-
-    assert(issame(pluck({7, 9, 7, 1}), {}) == true);
-
-    return 0;
+std::vector<int> pluck(const std::vector<std::vector<int>>& arr, int index) {
+    return arr[index];
 }
