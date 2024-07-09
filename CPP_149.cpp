9a56208981b2615a51dbfbb908effe57c8fb2396
beep boop <githubbot@vadim.me>
2024-07-09 04:52:09 +0200

    Solution 43 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/3gtchhci Prompt:    40 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7535cae3-746b-4a32-95f6-5eeeda283365.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ 7535cae3-746b-4a32-95f6-5eeeda283365.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main() {       |     ^~~~ In file included from 7535cae3-746b-4a32-95f6-5eeeda283365.cpp:39: 7535cae3-746b-4a32-95f6-5eeeda283365.cpp: In function ‘int main()’: 7535cae3-746b-4a32-95f6-5eeeda283365.cpp:49:20: error: ‘sorted_list_sum’ was not  declared in this scope    49 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 7535cae3-746b-4a32-95f6-5eeeda283365.cpp:49:19: error: ‘issame’ cannot be used a s a function    49 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -8,13 +8,13 @@ vector<string> sorted_list_sum(vector<string> lst) {
     }
 
     sort(result.begin(), result.end(),
-              [](const string& a, const string& b) {
-                  if (a.length() != b.length()) {
-                      return a.length() < b.length();
-                  } else {
-                      return a < b;
-                  }
-              });
+         [](const string& a, const string& b) {
+             if (a.length() != b.length()) {
+                 return a.length() < b.length();
+             } else {
+                 return a < b;
+             }
+         });
 
     return result;
 }
@@ -31,6 +31,9 @@ bool issame(vector<string> a, vector<string> b) {
     return true;
 }
 
-vector<string> lst={"aaaa", "bbbb", "dd", "cc"};
-sort(lst.begin(),lst.end());
-for(auto& str:lst) cout<<str<<" ";
+vector<string> lst {"aaaa", "bbbb", "dd", "cc"};
+
+int main() {
+    assert(issame(sorted_list_sum(lst), {lst[3], lst[2], lst[0], lst[1]}));
+    return 0;
+}
