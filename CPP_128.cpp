59e47d133ce86e03592623af44ac334a89fc27c3
beep boop <githubbot@vadim.me>
2024-07-09 10:09:51 +0200

    Solution 64 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zje411cm Prompt: 53ec35d9-2170-4a55-a046-ed3f094e4c69.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int prod_signs(vector<int> arr){       |                ^~~~~~ 53ec35d9-2170-4a55-a046-ed3f094e4c69.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int prod_signs(vector<int> arr){       |                       ^~~ In file included from 53ec35d9-2170-4a55-a046-ed3f094e4c69.cpp:15: 53ec35d9-2170-4a55-a046-ed3f094e4c69.cpp: In function ‘int main()’: 53ec35d9-2170-4a55-a046-ed3f094e4c69.cpp:17:23: error: ‘prod_signs’ cannot be us ed as a function    17 |     assert (prod_signs({-1, 1, 1, 0}) == 0);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 int prod_signs(std::vector<int> arr){
     long long res = 1;
@@ -12,4 +13,9 @@ int prod_signs(std::vector<int> arr){
     }
 
     return res * sum == 0 ? -32768 : res;
+}
+
+int main() {
+    assert(prod_signs({-1, 1, 1, 0}) == 0);
+    return 0;
 }
