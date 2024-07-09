bd68d87797f72f9c0380a393508ae2a3a3938e0b
beep boop <githubbot@vadim.me>
2024-07-09 02:33:46 +0200

    Solution 34 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/noyz23n0 Prompt: in this scope    19 |     assert(int_to_mini_roman(1000) == "M");       |     ^~~~~~ bcee4dc7-d87d-4f07-b404-f0a3aa53c2ad.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <string>   +++ |+#include <cassert>     3 | using namespace std; bcee4dc7-d87d-4f07-b404-f0a3aa53c2ad.cpp: At global scope: bcee4dc7-d87d-4f07-b404-f0a3aa53c2ad.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ bcee4dc7-d87d-4f07-b404-f0a3aa53c2ad.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from bcee4dc7-d87d-4f07-b404-f0a3aa53c2ad.cpp:23: bcee4dc7-d87d-4f07-b404-f0a3aa53c2ad.cpp: In function ‘int main()’: bcee4dc7-d87d-4f07-b404-f0a3aa53c2ad.cpp:25:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    25 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,10 +1,13 @@
+#include <vector>
 #include <string>
+#include <cassert>
+
 using namespace std;
 
-string int_to_roman(int number) {
+std::string int_to_mini_roman(int number) {
     vector< pair<int, string> > romans = {{1000,"M"},{900,"CM"},{500,"D"},{400,"CD"},{100,"C"},{90,"XC"},
                                          {50,"L"},{40,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}};
-    string roman = "";
+    std::string roman = "";
     for (auto p : romans) {
         while (number >= p.first) {
             number -= p.first;
@@ -12,4 +15,9 @@ string int_to_roman(int number) {
         }
     }
     return roman;
+}
+
+int main() {
+    assert(int_to_mini_roman(1000) == "M");
+    return 0;
 }
