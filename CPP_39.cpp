5d1e47eba59ac79583520970982836bbb95fafd6
beep boop <githubbot@vadim.me>
2024-07-09 07:48:35 +0200

    Solution 15 of CPP/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/q3zgo5ro Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: 311542e7-44b9-4cb9-a44f-e31178e4c4dd.cpp:30:39:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_39.cpp
+++ CPP_39.cpp
@@ -6,19 +6,19 @@ int prime_fib(int n) {
     int a = 0, b = 1;
     int count = 0;
     while (true) {
-        if (b > n) break;
+        int fib = a + b;
+        if (fib > n) break;
+        a = b;
+        b = fib;
         bool isPrime = true;
-        for (int j = 2; j * j <= b; j++) {
-            if (b % j == 0) {
+        for (int j = 2; j * j <= fib; j++) {
+            if (fib % j == 0) {
                 isPrime = false;
                 break;
             }
         }
         if (isPrime) count++;
-        if (count == n) return b;
-        int temp = a;
-        a = b;
-        b = temp + b;
+        if (count == n) return fib;
     }
     return 0;
 }
@@ -27,5 +27,6 @@ int main() {
     int n;
     cout << "Enter the number: ";
     cin >> n;
-    cout << "The " << n << "th prime Fibonacci number is: " << prime_fib(n) << endl;
+    int num; 
+    cout << "The " << num << "th prime Fibonacci number is: " << prime_fib(num) << endl;
 }
