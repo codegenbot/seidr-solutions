0f47ed2a513ba4f5aff1763aeb22ac1bbaba5472
beep boop <githubbot@vadim.me>
2024-07-09 02:41:56 +0200

    Solution 34 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h3blrigf Prompt: his scope    41 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1f447435-1c42-4b63-a4a9-1231cb1ef4a4.cpp:41:45: error: expression list treated a s compound expression in initializer [-fpermissive]    41 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 1f447435-1c42-4b63-a4a9-1231cb1ef4a4.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 1f447435-1c42-4b63-a4a9-1231cb1ef4a4.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 1f447435-1c42-4b63-a4a9-1231cb1ef4a4.cpp:40: 1f447435-1c42-4b63-a4a9-1231cb1ef4a4.cpp: In function ‘int main()’: 1f447435-1c42-4b63-a4a9-1231cb1ef4a4.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -5,9 +5,9 @@
 std::vector<std::string> sorted_list_sum(std::vector<std::string> lst) {
     auto it = std::remove_if(lst.begin(), lst.end(), [](const std::string& s){ return s.length() % 2; });
     lst.erase(it, lst.end());
-    std::sort(lst.begin(), lst.end(),
-        [](const std::string& a, const std::string& b) {
-            if (a.length() != b.length()) {
+    std::sort(lst.begin(), lst.end(), 
+        [](const std::string& a, const std::string& b){
+            if(a.length() != b.length()) {
                 return a.length() < b.length();
             } else {
                 return a < b;
@@ -28,11 +28,11 @@ int main() {
     
     std::vector<std::string> result = sorted_list_sum(lst);
     
-    std::cout << "Sorted List Sum: ";
+    cout << "Sorted List Sum: ";
     for(auto str : result) {
-        std::cout << str << " ";
+        cout << str << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
     
     return 0;
 }
