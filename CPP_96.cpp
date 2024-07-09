216d5f38622461ce542d4ba7f6fa5c9f08365fa3
beep boop <githubbot@vadim.me>
2024-07-09 10:33:07 +0200

    Solution 51 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/c8pse80p Prompt:    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1344f336-8c5b-462d-951b-a65a2bb2d2bb.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1344f336-8c5b-462d-951b-a65a2bb2d2bb.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1344f336-8c5b-462d-951b-a65a2bb2d2bb.cpp:22: 1344f336-8c5b-462d-951b-a65a2bb2d2bb.cpp: In function ‘int main()’: 1344f336-8c5b-462d-951b-a65a2bb2d2bb.cpp:32:20: error: ‘count_up_to’ was not dec lared in this scope    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 1344f336-8c5b-462d-951b-a65a2bb2d2bb.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,10 +1,24 @@
 #include <vector>
+#include <iostream>
 
 bool issame(int a, int b) {
     return a == b;
 }
 
 std::vector<int> count_up_to(int n);
+
+int main() {
+    int n;
+    std::cin >> n;
+    
+    std::vector<int> result = count_up_to(n);
+    
+    for (int num : result) {
+        std::cout << num << " ";
+    }
+    
+    return 0;
+}
 
 std::vector<int> count_up_to(int n) {
     std::vector<int> result;
