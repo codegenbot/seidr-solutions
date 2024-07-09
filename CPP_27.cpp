c428bcd28411222838b2cf67a533f1edf8bc00ed
beep boop <githubbot@vadim.me>
2024-07-09 03:05:59 +0200

    Solution 37 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/w5jpip5r Prompt: b0be1aba-5c86-424e-8d57-69a083c8cd07.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string flip_case(string str){       | ^~~~~~ b0be1aba-5c86-424e-8d57-69a083c8cd07.cpp:13:8: error: expected constructor, dest ructor, or type conversion before ‘(’ token    13 | assert (flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |        ^ In file included from b0be1aba-5c86-424e-8d57-69a083c8cd07.cpp:15: b0be1aba-5c86-424e-8d57-69a083c8cd07.cpp: In function ‘int main()’: b0be1aba-5c86-424e-8d57-69a083c8cd07.cpp:17:13: error: ‘filp_case’ was not decla red in this scope    17 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -16,5 +16,4 @@ std::string flip_case(std::string str){
 
 int main() {
     assert (flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
-    // ...
 }
