b46560fd6220c74961a6cea98b6e2347762b1678
beep boop <githubbot@vadim.me>
2024-07-09 09:38:31 +0200

    Solution 8 of CPP/92, ratio 0.8 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/kosm8huk Prompt: 874d5f42-9b38-4c5a-bd0c-8daecc9c905f.cpp:1:1: error: expected unqualified-id bef ore ‘if’     1 | if((a == b + c) || (b == a + c) || (c == a + b))       | ^~ 874d5f42-9b38-4c5a-bd0c-8daecc9c905f.cpp:3:5: error: expected unqualified-id bef ore ‘else’     3 |     else       |     ^~~~ 874d5f42-9b38-4c5a-bd0c-8daecc9c905f.cpp:5:1: error: expected declaration before  ‘}’ token     5 | }       | ^ In file included from 874d5f42-9b38-4c5a-bd0c-8daecc9c905f.cpp:7: 874d5f42-9b38-4c5a-bd0c-8daecc9c905f.cpp: In function ‘int main()’: 874d5f42-9b38-4c5a-bd0c-8daecc9c905f.cpp:9:13: error: ‘any_int’ was not declared  in this scope     9 |     assert (any_int(3.01,4,7)==false);       |             ^~~~~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,4 +1,5 @@
-if((a == b + c) || (b == a + c) || (c == a + b))
+bool any_int(int a, int b, int c) {
+    if (a == b + c || b == a + c || c == a + b)
         return true;
     else
         return false;
