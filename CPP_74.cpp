2d6f6d7a9a79a680b0dc08400cae8270b355d117
beep boop <githubbot@vadim.me>
2024-07-09 17:03:52 +0200

    Solution 46 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ok8wkyae Prompt:       |                              ^~~~~~ dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp:17:37: error: ‘string’ was not declared  in this scope    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp:16: dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp: In function ‘int main()’: dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp:26:20: error: ‘total_match’ was not dec lared in this scope    26 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,29 +1,20 @@
 #include <string>
 
-bool issame(string a, string b) {
-        return a == b;
+int sum_chars(vector<string> lst) {
+    int sum = 0;
+    for (string s : lst) {
+        sum += s.size();
     }
+    return sum;
+}
+
+bool issame(vector<string> lst1, vector<string> lst2) {
+    return sum_chars(lst1) < sum_chars(lst2);
+}
 
 vector<string> total_match(vector<string> lst1, vector<string> lst2);
 
 int main() {
-    // Sample usage of the total_match function
-    vector<string> lst1 = {"apple", "banana", "orange"};
-    vector<string> lst2 = {"grape", "melon"};
-    vector<string> result = total_match(lst1, lst2);
-
-    // Display the result
-    for (string s : result) { 
-        cout << s << " ";
-    }
-    cout << endl;
-
+    // Main function can use total_match function here
     return 0;
 }
-
-vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-        if (sum_chars(lst1) < sum_chars(lst2)) {
-            return lst1;
-        }
-        return lst2;
-    }  
