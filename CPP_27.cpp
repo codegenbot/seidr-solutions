53719e98a0b7b62124144951c11b09bcabf87dcc
beep boop <githubbot@vadim.me>
2024-07-09 07:28:43 +0200

    Solution 35 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uw1asumq Prompt:     1 | for (int i = 0; i < str.length(); i++) {       | ^~~ 2f4799ad-ab1f-4930-9493-25225aa55331.cpp:1:17: error: ‘i’ does not name a type     1 | for (int i = 0; i < str.length(); i++) {       |                 ^ 2f4799ad-ab1f-4930-9493-25225aa55331.cpp:1:35: error: ‘i’ does not name a type     1 | for (int i = 0; i < str.length(); i++) {       |                                   ^ 2f4799ad-ab1f-4930-9493-25225aa55331.cpp:8:5: error: expected unqualified-id bef ore ‘return’     8 |     return str;       |     ^~~~~~ 2f4799ad-ab1f-4930-9493-25225aa55331.cpp:9:1: error: expected declaration before  ‘}’ token     9 | }       | ^ In file included from 2f4799ad-ab1f-4930-9493-25225aa55331.cpp:11: 2f4799ad-ab1f-4930-9493-25225aa55331.cpp: In function ‘int main()’: 2f4799ad-ab1f-4930-9493-25225aa55331.cpp:13:13: error: ‘filp_case’ was not decla red in this scope    13 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -14,6 +14,6 @@ std::string filp_case(std::string str) {
 
 int main() {
     assert (filp_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
-
+    
     return 0;
 }
