c0f691544806b5923143af0574ab30538c133fdb
beep boop <githubbot@vadim.me>
2024-07-09 21:38:34 +0200

    Solution 14 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/bw1run1h Prompt: 6cbaa55d-4705-481c-a33d-490ba776861f.cpp:20:29: error: invalid types ‘const int[ int]’ for array subscript    20 |                 result |= (s[i++] == 'T');       |                             ^ 6cbaa55d-4705-481c-a33d-490ba776861f.cpp:21:25: error: invalid types ‘const int[ int]’ for array subscript    21 |             } else if (s[i] == '&') {       |                         ^ 6cbaa55d-4705-481c-a33d-490ba776861f.cpp:24:29: error: invalid types ‘const int[ int]’ for array subscript    24 |                 result |= (s[i] == 'T');       |                             ^ 6cbaa55d-4705-481c-a33d-490ba776861f.cpp:28:24: error: invalid types ‘const int[ int]’ for array subscript    28 |             result = (s[i] == 'T');       |                        ^ 6cbaa55d-4705-481c-a33d-490ba776861f.cpp:33:19: error: expected ‘}’ at end of in put    33 |     return result;       |                   ^ 6cbaa55d-4705-481c-a33d-490ba776861f.cpp:1:41: note: to match this ‘{’     1 | bool solveBoolean(const std::string& s) {       |                                         ^

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -5,27 +5,31 @@ bool solveBoolean(const std::string& s) {
     while (i < s.length()) {
         if (s[i] == '&') {
             i++;
-            while (i < s.length() && s[i] != '|') {
-                if (s[i] != 'T') {
-                    result = false;
-                    break;
-                }
+            if (s.substr(i, 1) == "T") {
+                result &= (s.substr(i, 1) == "T");
+                i++;
+            } else if (s[i] == '|') {
+                result = false;
+                break;
+            } else {
+                result &= (s.substr(i, 1) == "T");
                 i++;
             }
         } else if (s[i] == '|') {
             i++;
-            while (i < s.length() && s[i] != '&') {
-                if (s[i] != 'T') {
-                    result = true;
-                    break;
-                }
+            if (s.substr(i, 1) == "|") {
+                result |= (s.substr(i + 1, 1) == "T");
+                i += 2;
+            } else if (s[i] == '&') {
+                break;
+            } else {
+                result |= (s.substr(i, 1) == "T");
                 i++;
             }
         } else {
-            result = (s[i] == 'T');
+            result = (s.substr(i, 1) == "T");
             i++;
         }
     }
 
     return result;
-}
