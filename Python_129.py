aa12715b6560be5b4cfe642dc9e92d5e8872ca89
beep boop <githubbot@vadim.me>
2024-07-09 08:22:22 +0200

    Solution 19 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/x8fwogdg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/424ef28e-f93b-4d69-8b5d-1162614d5 f59.py", line 29, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/424ef28e-f93b-4d69-8b5d-1162614d5 f59.py", line 26, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -1,22 +1,17 @@
 def minPath(grid, k):
     n = len(grid)
-    res = []
+    res = float("inf")
 
     def dfs(i, j, path, dist):
         nonlocal res
+
         if len(path) == k:
-            res = (
-                min(res, sorted(path))
-                if not res
-                else (res if sum(res) < sum(path) else res)
-            )
-            return False
+            res = min(res, sum(path))
+            return
 
         for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
             if 0 <= x < n and 0 <= y < n and grid[x][y] not in path:
                 dfs(x, y, path + [grid[x][y]], dist + grid[x][y])
-
-        return True
 
     for i in range(n):
         for j in range(n):
