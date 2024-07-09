8a4db4cbc956b56894474cf2a55ce37e4108883f
beep boop <githubbot@vadim.me>
2024-07-09 04:49:21 +0200

    Solution 57 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fv53j2kl Prompt: 580da250-4432-47c8-969a-85d8655933c3.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -6,25 +6,25 @@
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return ((int)boost::any_cast<int>(a)) > boost::any_cast<float>(b) ? a : b;
+        return (int)boost::any_cast<int>(a) > boost::any_cast<float>(b) ? a : b;
     } else if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return ((int)boost::any_cast<int>(a)) > boost::any_cast<double>(b) ? a : b;
-    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        string str = boost::any_cast<string>(a);
+        return (int)boost::any_cast<int>(a) > boost::any_cast<double>(b) ? a : b;
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(int)) {
+        std::string str = boost::any_cast<std::string>(a);
         int num = boost::any_cast<int>(b);
-        return stoi(str) > num ? a : b;
+        return std::stoi(str) > num ? a : b;
     } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
         return boost::any_cast<float>(a) > (int)b ? a : b;
     } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
         return boost::any_cast<double>(a) > (int)b ? a : b;
-    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        string str = boost::any_cast<string>(a);
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(float)) {
+        std::string str = boost::any_cast<std::string>(a);
         float num = boost::any_cast<float>(b);
-        return stof(str) > num ? a : b;
-    } else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        string str = boost::any_cast<string>(a);
+        return std::stof(str) > num ? a : b;
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(double)) {
+        std::string str = boost::any_cast<std::string>(a);
         double num = boost::any_cast<double>(b);
-        return stod(str) > num ? a : b;
+        return std::stod(str) > num ? a : b;
     }
-    throw std::runtime_error("Invalid combination");
+    return boost::any();
 }
