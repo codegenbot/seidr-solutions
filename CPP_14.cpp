043a5a61f13ca8b681f550850a85bc3a96cc5e51
beep boop <githubbot@vadim.me>
2024-07-09 13:12:04 +0200

    Solution 16 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ksdrjzfk Prompt:       |                              ^~~~~~ 75e041b2-ed5a-4a48-bdf0-386cf822132d.cpp:12:37: error: ‘string’ was not declared  in this scope    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 75e041b2-ed5a-4a48-bdf0-386cf822132d.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 75e041b2-ed5a-4a48-bdf0-386cf822132d.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 75e041b2-ed5a-4a48-bdf0-386cf822132d.cpp:11: 75e041b2-ed5a-4a48-bdf0-386cf822132d.cpp: In function ‘int main()’: 75e041b2-ed5a-4a48-bdf0-386cf822132d.cpp:21:20: error: ‘all_prefixes’ was not de clared in this scope    21 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ 75e041b2-ed5a-4a48-bdf0-386cf822132d.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,4 +1,23 @@
-vector<string> all_prefixes(string str) {
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+bool issame(vector<string> a, vector<string> b){
+    if(a.size() != b.size()){
+        return false;
+    }
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<string> all_prefixes(string str){
     vector<string> result;
     string prefix = "";
     for (char c : str) {
@@ -6,4 +25,10 @@ vector<string> all_prefixes(string str) {
         result.push_back(prefix);
     }
     return result;
+}
+
+int main() {
+    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    
+    return 0;
 }
