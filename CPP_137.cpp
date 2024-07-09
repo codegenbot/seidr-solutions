390961989e61abd9a7e087777bc44307dff3943f
beep boop <githubbot@vadim.me>
2024-07-09 02:16:44 +0200

    Solution 15 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/pf8nwdde Prompt: 15470b58-e36c-421b-8ff1-8a8af2feb223.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <boost/any.hpp>
 #include <string>
 
@@ -16,9 +17,11 @@ int main() {
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
         return b;
-    } else if (a.type() == typeid(float) && b.type() == typeid(std::string)) {
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(std::string)) {
         return b;
-    } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+    }
+    else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
         std::string strA = boost::any_cast<std::string>(a);
         std::string strB = boost::any_cast<std::string>(b);
         if (strA > strB)
@@ -27,7 +30,8 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         else
             return boost::any("None");
-    } else if (a.type() == typeid(int) && b.type() == typeid(int)) {
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(int)) {
         int iA = boost::any_cast<int>(a);
         int iB = boost::any_cast<int>(b);
         if (iA > iB)
@@ -36,7 +40,8 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         else
             return boost::any("None");
-    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
         float fA = boost::any_cast<float>(a);
         int iB = boost::any_cast<int>(b);
         if (fA > iB)
@@ -45,7 +50,8 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         else
             return boost::any("None");
-    } else {
+    }
+    else {
         return boost::any("None");
     }
 }
