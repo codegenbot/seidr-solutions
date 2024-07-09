2569bd500521dabffe0ec3c7200b88bb39d35e06
beep boop <githubbot@vadim.me>
2024-07-09 22:01:47 +0200

    Solution 11 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8g4ufl0e Prompt: 04f9ea2c-9a06-4f34-b0bf-1aef72cc7e9a.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string get_closest_vowel(string word) {       | ^~~~~~ In file included from 04f9ea2c-9a06-4f34-b0bf-1aef72cc7e9a.cpp:13: 04f9ea2c-9a06-4f34-b0bf-1aef72cc7e9a.cpp: In function ‘int main()’: 04f9ea2c-9a06-4f34-b0bf-1aef72cc7e9a.cpp:15:13: error: ‘get_closest_vowel’ was n ot declared in this scope    15 |     assert (get_closest_vowel("Above") == "o");       |             ^~~~~~~~~~~~~~~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,12 +1,13 @@
 #include <string>
+#include <cctype>
 
 string get_closest_vowel(string word) {
     for (int i = word.size() - 1; i > 0; --i) {
         if (!isalpha(word[i])) continue;
-        if (isvowel(tolower(word[i]))) return string(1, tolower(word[i]));
+        if (ispunct(tolower(word[i]))) return string(1, tolower(word[i]));
         for (int j = i - 1; j >= 0; --j) {
             if (!isalpha(word[j])) break;
-            if (isvowel(tolower(word[j]))) return string(1, tolower(word[j]));
+            if (ispunct(tolower(word[j]))) return string(1, tolower(word[j]));
         }
     }
     return "";
