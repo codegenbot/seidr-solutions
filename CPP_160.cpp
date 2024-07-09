0c675d7799727829d7832eca901e95b5d1c4a51a
beep boop <githubbot@vadim.me>
2024-07-09 20:24:51 +0200

    Solution 18 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8tmp81k3 Prompt: In file included from /usr/include/c++/11/utility:70,                  from 1dd383e1-4292-4de9-840c-12ccc6c77b26.cpp:1: /usr/include/c++/11/bits/stl_pair.h:211:12: note: ‘std::pair’ declared here   211 |     struct pair       |            ^~~~ 1dd383e1-4292-4de9-840c-12ccc6c77b26.cpp:3:28: error: expected primary-expressio n before ‘char’     3 | int do_algebra(vector<pair<char, int>> operators_and_operands) {       |                            ^~~~ 1dd383e1-4292-4de9-840c-12ccc6c77b26.cpp:3:34: error: expected primary-expressio n before ‘int’     3 | int do_algebra(vector<pair<char, int>> operators_and_operands) {       |                                  ^~~ 1dd383e1-4292-4de9-840c-12ccc6c77b26.cpp:3:62: error: expression list treated as  compound expression in initializer [-fpermissive]     3 | int do_algebra(vector<pair<char, int>> operators_and_operands) {       |                                                              ^ In file included from 1dd383e1-4292-4de9-840c-12ccc6c77b26.cpp:27: 1dd383e1-4292-4de9-840c-12ccc6c77b26.cpp: In function ‘int main()’: 1dd383e1-4292-4de9-840c-12ccc6c77b26.cpp:29:23: error: ‘do_algebra’ cannot be us ed as a function    29 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,16 +1,22 @@
-int do_algebra(std::vector<std::pair<char, int>> operators_and_operands) {
+int do_algebra(vector<pair<char, int>> operators_and_operands) {
+    if (operators_and_operands.size() == 0) {
+        return 0;
+    }
+    
     int result = operators_and_operands[0].second;
-    for (int i = 1; i < operators_and_operands.size(); i++) {
-        if (operators_and_operands[i].first == '+') {
-            result += operators_and_operands[i].second;
-        } else if (operators_and_operands[i].first == '-') {
-            result -= operators_and_operands[i].second;
-        } else if (operators_and_operands[i].first == '*') {
-            result *= operators_and_operands[i].second;
-        } else if (operators_and_operands[i].first == '/') {
-            result /= operators_and_operands[i].second;
-        } else if (operators_and_operands[i].first == '^') {
-            result = pow(result, operators_and_operands[i].second);
+    for (const auto& pair : operators_and_operands) {
+        if (pair.first == '+') {
+            result += pair.second;
+        } else if (pair.first == '-') {
+            result -= pair.second;
+        } else if (pair.first == '*') {
+            result *= pair.second;
+        } else if (pair.first == '/') {
+            if (pair.second != 0) {
+                result /= pair.second;
+            }
+        } else if (pair.first == '**') {
+            result = pow(result, pair.second);
         }
     }
     return result;
