abad536764b276f1c9b9a71e058eb0615b6de292
beep boop <githubbot@vadim.me>
2024-07-09 19:22:11 +0200

    Solution 16 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/dzbvg62p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a402b843-ef04-4633-9b6c-ead09795e e43.py", line 2, in <module>     arr = [int(input()) for _ in range(n)]           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a402b843-ef04-4633-9b6c-ead09795e e43.py", line 2, in <listcomp>     arr = [int(input()) for _ in range(n)]            ^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: '128 1079 7386'

--- cut-vector.py
+++ cut-vector.py
@@ -19,4 +19,6 @@ subvector1 = arr[: cut_index + 1]
 subvector2 = arr[cut_index + 1 :]
 
 print("\n".join(map(str, subvector1)))
-print("\n".join(map(str, subvector2)))-1
+print("\n".join(map(str, subvector2)))
+
+
