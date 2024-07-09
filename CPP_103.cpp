0d5a92575e7b7ab336fc6140eb62420b4a0778ec
beep boop <githubbot@vadim.me>
2024-07-09 10:14:19 +0200

    Solution 12 of CPP/103, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/qrs55vsq Prompt:       |               ^~~~~ 2ee854f1-4b98-43c8-842a-004cf5fd0278.cpp:12:5: error: ‘string’ does not name a t ype    12 |     string binary_avg = "";       |     ^~~~~~ 2ee854f1-4b98-43c8-842a-004cf5fd0278.cpp:13:5: error: expected unqualified-id be fore ‘while’    13 |     while (avg > 0) {       |     ^~~~~ 2ee854f1-4b98-43c8-842a-004cf5fd0278.cpp:18:5: error: expected unqualified-id be fore ‘return’    18 |     return binary_avg;       |     ^~~~~~ 2ee854f1-4b98-43c8-842a-004cf5fd0278.cpp:19:1: error: expected declaration befor e ‘}’ token    19 | }       | ^ In file included from 2ee854f1-4b98-43c8-842a-004cf5fd0278.cpp:21: 2ee854f1-4b98-43c8-842a-004cf5fd0278.cpp: In function ‘int main()’: 2ee854f1-4b98-43c8-842a-004cf5fd0278.cpp:23:13: error: ‘rounded_avg’ was not dec lared in this scope    23 |     assert (rounded_avg(5, 5) == "101");       |             ^~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,19 +1,24 @@
-if (n > m) {
-    return "-1";
-}
+#include <iostream>
+#include <string>
+#include <cmath>
 
-int sum = 0;
-for (int i = n; i <= m; i++) {
-    sum += i;
-}
-
-int avg = round((double)sum / (m - n + 1));
-
-string binary_avg;
-while (avg > 0) {
-    binary_avg = to_string(avg % 2) + binary_avg;
-    avg /= 2;
-}
-
-return binary_avg;
+std::string rounded_avg(int n, int m) {
+    if (n > m) {
+        return "-1";
+    }
+    
+    int sum = 0;
+    for (int i = n; i <= m; i++) {
+        sum += i;
+    }
+    
+    int avg = std::round((double)sum / (m - n + 1));
+    
+    std::string binary_avg = "";
+    while (avg > 0) {
+        binary_avg = std::to_string(avg % 2) + binary_avg;
+        avg /= 2;
+    }
+    
+    return binary_avg;
 }
