857cdfb31e5836a3c59a3ab3aee6d5b4951454ea
beep boop <githubbot@vadim.me>
2024-07-09 22:17:51 +0200

    Solution 66 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/voqe3c6u Prompt:       |                            ^ 0a6c1b9f-35c5-48d8-93fa-c3113aa5f0e1.cpp:31:30: error: ‘vector’ was not declared  in this scope    31 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ 0a6c1b9f-35c5-48d8-93fa-c3113aa5f0e1.cpp:31:37: error: ‘string’ was not declared  in this scope    31 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0a6c1b9f-35c5-48d8-93fa-c3113aa5f0e1.cpp:31:44: error: ‘b’ was not declared in t his scope    31 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 0a6c1b9f-35c5-48d8-93fa-c3113aa5f0e1.cpp:30: 0a6c1b9f-35c5-48d8-93fa-c3113aa5f0e1.cpp: In function ‘int main()’: 0a6c1b9f-35c5-48d8-93fa-c3113aa5f0e1.cpp:40:20: error: ‘by_length’ was not decla red in this scope    40 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 0a6c1b9f-35c5-48d8-93fa-c3113aa5f0e1.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,7 +1,5 @@
 #include <vector>
 #include <string>
-#include <map>
-#include <algorithm>
 
 bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) {
@@ -19,7 +17,7 @@ bool issame(vector<string> a, vector<string> b) {
 
 vector<string> by_length(vector<int> arr) {
     vector<string> result;
-    map<int, string> numMap = {{1,"One"}, {2,"Two"}, {3,"Three"}, {4,"Four"}, {5,"Five"}, {6,"Six"}, {7,"Seven"}, {8,"Eight"}, {9,"Nine"}};
+    map<int, string> numMap = { {1,"One"}, {2,"Two"}, {3,"Three"}, {4,"Four"}, {5,"Five"}, {6,"Six"}, {7,"Seven"}, {8,"Eight"}, {9,"Nine"} };
     
     for (int i : arr) {
         if (i >= 1 && i <= 9)
