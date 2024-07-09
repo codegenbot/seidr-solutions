cfc4ac00543993416e23f86eccde3d6e7d277f62
beep boop <githubbot@vadim.me>
2024-07-09 02:30:49 +0200

    Solution 86 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sfxa53q2 Prompt:       |             ^~~~~~ 71d833e2-b147-4d70-b1b9-bbd2721b048f.cpp:27:20: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 71d833e2-b147-4d70-b1b9-bbd2721b048f.cpp:27:27: error: ‘vector’ was not declared  in this scope    27 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 71d833e2-b147-4d70-b1b9-bbd2721b048f.cpp:27:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 71d833e2-b147-4d70-b1b9-bbd2721b048f.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 71d833e2-b147-4d70-b1b9-bbd2721b048f.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 71d833e2-b147-4d70-b1b9-bbd2721b048f.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -2,10 +2,16 @@
 #include <vector>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
+    if (a.size() != b.size())
+        return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i])
+            return false;
+    }
+    return true;
 }
 
-std::vector<int> sort_array(std::vector<int> arr) {
+std::vector<int> sort_array(std::vector<int>& arr) {
     auto comp = [&] (int a, int b) -> bool {
         if (std::bitset<32>(a).count() != std::bitset<32>(b).count()) {
             return std::bitset<32>(a).count() < std::bitset<32>(b).count();
