dd54a851a2e354ebe0701ca8c2b4f73d61657f5d
beep boop <githubbot@vadim.me>
2024-07-09 04:21:33 +0200

    Solution 32 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ajzrdwh9 Prompt:       |                    ^~~ 612b7107-c2c8-4bcf-96d6-b8f11596a80a.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 612b7107-c2c8-4bcf-96d6-b8f11596a80a.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 612b7107-c2c8-4bcf-96d6-b8f11596a80a.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 612b7107-c2c8-4bcf-96d6-b8f11596a80a.cpp:16: 612b7107-c2c8-4bcf-96d6-b8f11596a80a.cpp: In function ‘int main()’: 612b7107-c2c8-4bcf-96d6-b8f11596a80a.cpp:26:20: error: ‘maximum’ was not declare d in this scope    26 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 612b7107-c2c8-4bcf-96d6-b8f11596a80a.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,21 +1,11 @@
-#include <vector>
-using namespace std;
-
-bool issame(vector<int> a,vector<int> b){
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]) return false;
-    }
-    return true;
-}
-
-vector<int> maximum(vector<int> arr, int k) {
+```
+vector<int> issame(vector<int> arr, int k) {
     priority_queue<int> pq;
     for (int i : arr) {
         pq.push(i);
     }
     vector<int> result;
-    for (int i = 0; i < k; i++) {
+    while (pq.size() > 1 && k--) {
         result.push_back(pq.top());
         pq.pop();
     }
