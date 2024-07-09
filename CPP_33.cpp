b2efccf0451615336959b195c4660faa70b702ce
beep boop <githubbot@vadim.me>
2024-07-09 22:54:12 +0200

    Solution 1 of CPP/33, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/zddxvnac Prompt: Complete the following code given the task description and function signature.

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 
@@ -7,35 +6,20 @@ using namespace std;
 vector<int> sort_third(vector<int> l) {
     vector<int> result;
     for (int i = 0; i < l.size(); i++) {
-        if (i % 3 != 0) {
-            result.push_back(l[i]);
-        } else {
-            auto it = lower_bound(result.begin(), result.end(), l[i]);
-            if (it == result.end()) {
-                result.insert(it, l[i]);
-            } else if (*it > l[i]) {
-                it = result.insert(it, l[i]);
+        if (i % 3 == 0) {
+            vector<int> temp;
+            for (int j = i; j < l.size() && j % 3 == 0; j++) {
+                temp.push_back(l[j]);
             }
+            sort(temp.begin(), temp.end());
+            int k = i;
+            for (int val : temp) {
+                result.push_back(val);
+                k++;
+            }
+        } else {
+            result.push_back(l[i]);
         }
     }
     return result;
-}
-
-int main() {
-    vector<int> test1 = {1, 2, 3};
-    vector<int> test2 = {5, 6, 3, 4, 8, 9, 2};
-
-    cout << "Test 1: ";
-    for (auto x : sort_third(test1)) {
-        cout << x << " ";
-    }
-    cout << endl;
-
-    cout << "Test 2: ";
-    for (auto x : sort_third(test2)) {
-        cout << x << " ";
-    }
-    cout << endl;
-    
-    return 0;
 }
