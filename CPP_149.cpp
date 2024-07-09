a673e8783c640b79df3f1d6f6cfbcafcf994708b
beep boop <githubbot@vadim.me>
2024-07-09 22:31:16 +0200

    Solution 17 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/b0l0pzf4 Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4f8170c6-5efa-40f7-8c9f-52f5e02c5797.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4f8170c6-5efa-40f7-8c9f-52f5e02c5797.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4f8170c6-5efa-40f7-8c9f-52f5e02c5797.cpp:25: 4f8170c6-5efa-40f7-8c9f-52f5e02c5797.cpp: In function ‘int main()’: 4f8170c6-5efa-40f7-8c9f-52f5e02c5797.cpp:35:20: error: ‘sorted_list_sum’ was not  declared in this scope    35 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 4f8170c6-5efa-40f7-8c9f-52f5e02c5797.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,21 +1,25 @@
-vector<string> sorted_list_sum(vector<string> lst) {
-    vector<string> result;
-
-    // Remove strings with odd lengths from the list
-    for (const auto& str : lst) {
-        if (str.length() % 2 == 0) {
-            result.push_back(str);
+if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
+            return false;
         }
     }
 
-    // Sort the resulted vector by length and then alphabetically
-    sort(result.begin(), result.end(), [](const string& a, const string& b) {
-        if (a.length() != b.length()) {
-            return a.length() < b.length();
-        } else {
-            return a < b;
-        }
-    });
+    return true;
+}
 
-    return result;
+int main() {
+    vector<string> lst1 = {"aba", "aa", "ad", "vcd", "baa"};
+    vector<string> lst2 = {"aba", "aa", "ad", "vcd", "baa"};
+
+    if (issame(lst1, lst2)) {
+        cout << "The two lists are the same";
+    } else {
+        cout << "The two lists are different";
+    }
+
+    return 0;
 }
