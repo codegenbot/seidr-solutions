3c772de8a82d7e60e0586bb86f41e6f1a714823e
beep boop <githubbot@vadim.me>
2024-07-09 17:15:36 +0200

    Solution 50 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/2505ch6v Prompt:       |                              ^~~~~~ 2c83d10c-ae0f-43bd-a038-f19d80d3b6c6.cpp:35:37: error: ‘string’ was not declared  in this scope    35 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 2c83d10c-ae0f-43bd-a038-f19d80d3b6c6.cpp:35:44: error: ‘b’ was not declared in t his scope    35 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2c83d10c-ae0f-43bd-a038-f19d80d3b6c6.cpp:35:45: error: expression list treated a s compound expression in initializer [-fpermissive]    35 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2c83d10c-ae0f-43bd-a038-f19d80d3b6c6.cpp:34: 2c83d10c-ae0f-43bd-a038-f19d80d3b6c6.cpp: In function ‘int main()’: 2c83d10c-ae0f-43bd-a038-f19d80d3b6c6.cpp:44:20: error: ‘numerical_letter_grade’ was not declared in this scope    44 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 2c83d10c-ae0f-43bd-a038-f19d80d3b6c6.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,11 +1,7 @@
+#include <iostream>
+#include <vector>
 #include <string>
 using namespace std;
-
-vector<string> numerical_letter_grade(vector<float> grades);
-
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
 
 vector<string> numerical_letter_grade(vector<float> grades) {
     vector<string> letter_grades;
@@ -38,6 +34,10 @@ vector<string> numerical_letter_grade(vector<float> grades) {
             letter_grades.push_back("E");
     }
     return letter_grades;
+}
+
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
 }
 
 int main() {
