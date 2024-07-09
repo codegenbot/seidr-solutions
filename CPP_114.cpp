d857b2a2c68bb8d4e9621c6edb13f8d74412e8cb
beep boop <githubbot@vadim.me>
2024-07-09 02:42:16 +0200

    Solution 85 of CPP/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/f83613l2 Prompt: cb6337dc-8a0e-40c2-93b0-f5343e32d432.cpp:1:1: error: stray ‘`’ in program     1 | ```c++       | ^ cb6337dc-8a0e-40c2-93b0-f5343e32d432.cpp:1:2: error: stray ‘`’ in program     1 | ```c++       |  ^ cb6337dc-8a0e-40c2-93b0-f5343e32d432.cpp:1:3: error: stray ‘`’ in program     1 | ```c++       |   ^ cb6337dc-8a0e-40c2-93b0-f5343e32d432.cpp:4:10: fatal error: assert: No such file  or directory     4 | #include <assert>       |          ^~~~~~~~ compilation terminated.

--- CPP_114.cpp
+++ CPP_114.cpp
@@ -6,18 +6,18 @@
 long long minSubArray(std::vector<long long> nums) {
     long long sum = 0, min_sum = LLONG_MAX;
     for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] > 0)
-            sum += nums[i];
-        else
-            sum = 0;
-
-        if (sum < min_sum)
+        sum += nums[i];
+        if (sum < min_sum) {
             min_sum = sum;
+        }
+        while(sum > 0 && i < nums.size()) {
+            sum -= nums[i++];
+        }
     }
     return min_sum;
 }
 
 int testMain() {
-    assert(minSubArray({1, -1}) == 0);
+    assert(minSubArray({1, -1}) == -1);
     return 0;
 }
