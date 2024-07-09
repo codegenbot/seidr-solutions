9ce0dadce51351bb2ae91e885f2c2f0eb7e38346
beep boop <githubbot@vadim.me>
2024-07-10 01:23:57 +0200

    Solution 90 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/jay8p6s3 Prompt: f4c25685-4be9-42d6-95df-b6310b047699.cpp:3:1: error: ‘string’ does not name a ty pe     3 | string string_to_md5(string text) {       | ^~~~~~ In file included from f4c25685-4be9-42d6-95df-b6310b047699.cpp:17: f4c25685-4be9-42d6-95df-b6310b047699.cpp: In function ‘int main()’: f4c25685-4be9-42d6-95df-b6310b047699.cpp:19:13: error: ‘string_to_md5’ was not d eclared in this scope    19 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <openssl/md5.h>
 #include <sstream>
 #include <iomanip>
@@ -20,6 +21,6 @@ string string_to_md5(string text) {
 }
 
 int main() {
-    assert(string_to_md5("password") == "5d41402abc4e666f1db974fc09c233a3");
+    assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
     return 0;
 }
