ba2c46388a833b1e3f9596b87a81309935f9e8fe
beep boop <githubbot@vadim.me>
2024-07-09 06:05:00 +0200

    Solution 44 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/6n9rycuk Prompt:       |                    ^~~ 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp:19: 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp: In function ‘int main()’: 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp:29:20: error: ‘strange_sort_list’ was n ot declared in this scope    29 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,37 +1,17 @@
-```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
+    if(a.size() != b.size())
         return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
+    for(int i = 0; i<a.size(); i++) {
+        if(a[i] != b[i])
             return false;
-        }
     }
     return true;
-}
-
-int main() {
-    std::vector<int> list1{4, 2, 9, 6, 5, 1, 3, 7, 8};
-    std::vector<int> list2{1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-    if (issame(list1, list2)) {
-        std::cout << "Lists are same.\n";
-    } else {
-        std::cout << "Lists are different.\n";
-    }
-
-    vector<int> result = strange_sort_list(list1);
-    for (int i : result) {
-        std::cout << i << " ";
-    }
-    std::cout << "\n";
-
-    return 0;
 }
 
 vector<int> strange_sort_list(vector<int> lst) {
@@ -47,4 +27,30 @@ vector<int> strange_sort_list(vector<int> lst) {
         }
     }
     return result;
+}
+
+int main() {
+    vector<int> list;
+    int n;
+    cout << "Enter the size of the list: ";
+    cin >> n;
+    for(int i = 0; i<n; i++) {
+        int val;
+        cin >> val;
+        list.push_back(val);
+    }
+    
+    vector<int> result = strange_sort_list(list);
+    
+    if(issame(list, result)) {
+        cout << "The sorted list is: ";
+        for(int i = 0; i<result.size(); i++) {
+            cout << result[i] << " ";
+        }
+        cout << endl;
+    } else {
+        cout << "Failed to sort the list." << endl;
+    }
+    
+    return 0;
 }
