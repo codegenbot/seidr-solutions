d695901da3483a5f8454e4135d84bb7a90b03e0d
beep boop <githubbot@vadim.me>
2024-07-09 02:51:15 +0200

    Solution 44 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zlpto1aq Prompt: L GrQw NqRz ZkDw wR ZuLwH Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f51d9d4-d8db-4d62-a408-f830a3d55 224.py", line 22, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f51d9d4-d8db-4d62-a408-f830a3d55 224.py", line 18, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -3,8 +3,11 @@ def encode(message):
     for char in message:
         if char.isalpha():
             ascii_offset = 65 if char.isupper() else 97
-            result += chr((ord(char) - ascii_offset - 3) % 26 + ascii_offset)
-        elif not char.isalnum():
+            if char.lower().isalpha():
+                result += chr((ord(char) - ascii_offset + 3) % 26 + ascii_offset)
+            else:
+                result += char
+        elif not char.isalnum() and char != ' ':
             result += '*'
         else:
             result += char
