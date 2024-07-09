dca896a43713ddebecae697f73f4f3518b30eacf
beep boop <githubbot@vadim.me>
2024-07-09 06:46:03 +0200

    Solution 60 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/wyqnzult Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6c3e6f0f-81ab-4fb8-91bd-f5f060ff6 b01.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_38.py
+++ Python_38.py
@@ -1,36 +1,20 @@
 ```
+s = input()
+
 def decode_cyclic(s: str):
     result = ""
     temp = ""
     for char in s:
-        if '0' <= char <= '9':
-            temp += char
-        else:
-            while len(temp) > 1 and (temp[0] != '0' or temp[-1] == '3'):
-                if int(temp) % 3 == 0:
-                    result += str(int(temp)//3)
-                else:
-                    result += temp[0]
-                temp = temp[1:]
-            if len(temp) > 0 and (temp[0] != '0' or temp == '3'):
-                if int(temp) % 3 == 0:
-                    result += str(int(temp)//3)
-                else:
-                    result += temp[0]
+        temp += char
+        if len(temp) == 3:
+            if len(result) > 0 and result[-1] == temp[0]:
+                result += temp[1]
             else:
                 result += temp
             temp = ""
-    while len(temp) > 1 and (temp[0] != '0' or temp[-1] == '3'):
-        if int(temp) % 3 == 0:
-            result += str(int(temp)//3)
+    if len(temp) > 0:
+        if len(result) > 0 and result[-1] == temp[0]:
+            result += temp[1]
         else:
-            result += temp[0]
-        temp = temp[1:]
-    if len(temp) > 0 and (temp[0] != '0' or temp == '3'):
-        if int(temp) % 3 == 0:
-            result += str(int(temp)//3)
-        else:
-            result += temp[0]
-    else:
-        result += temp
+            result += temp
     return result
