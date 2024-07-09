291b278d28078278bad3ac41d265d94279bd3783
beep boop <githubbot@vadim.me>
2024-07-09 20:48:48 +0200

    Solution 36 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/emvdltp6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5059d46d-9d26-494f-8a7c-fdb121974 03b.py", line 9, in <module>     check(filter_by_prefix)           ^^^^^^^^^^^^^^^^ NameError: name 'filter_by_prefix' is not defined

--- Python_29.py
+++ Python_29.py
@@ -1,2 +1,2 @@
-def filter_strings(strings, prefix):
-    return [s for s in strings if s.startswith(prefix)]  
+def filter_strings(strings: List[str], prefix: str) -> List[str]:
+    return [s for s in strings if s.startswith(prefix)]
