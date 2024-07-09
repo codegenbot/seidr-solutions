56800650b321c89debb4e2bea517d5ebdd16e636
beep boop <githubbot@vadim.me>
2024-07-09 06:56:15 +0200

    Solution 79 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/73sda81b Prompt:       |           ^~~~~~ In file included from 17fad020-6fff-4359-93e5-8d2488318285.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 17fad020-6fff-4359-93e5-8d2488318285.cpp:37:34: error: expected primary-expressi on before ‘int’    37 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 17fad020-6fff-4359-93e5-8d2488318285.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ 17fad020-6fff-4359-93e5-8d2488318285.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from 17fad020-6fff-4359-93e5-8d2488318285.cpp:36: 17fad020-6fff-4359-93e5-8d2488318285.cpp: In function ‘int main()’: 17fad020-6fff-4359-93e5-8d2488318285.cpp:46:20: error: ‘parse_nested_parens’ was  not declared in this scope    46 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,12 +1,11 @@
+using namespace std;
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
 std::vector<int> parse_nested_parens(string paren_string);
-
 std::vector<int> parse_nested_parens(string paren_string) {
     std::vector<int> depths;
     int max_depth = 0;
@@ -31,7 +30,7 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-int main() {
-    assert(issame(parse_nested_parens("(()(())((())))"), {4}));
+bool main() {
+    assert(issame(parse_nested_parens("(()(())(()))"), {4}));
     return 0;
 }
