a21740ac5ed4e5b1f74feaeba16b7e78a35d4b90
beep boop <githubbot@vadim.me>
2024-07-09 02:05:34 +0200

    Solution 24 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c0h3vdup Prompt:       |           ^~~~~~ In file included from 50563810-7125-41f8-95fb-0dca86f871cc.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 50563810-7125-41f8-95fb-0dca86f871cc.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 50563810-7125-41f8-95fb-0dca86f871cc.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ 50563810-7125-41f8-95fb-0dca86f871cc.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~ In file included from 50563810-7125-41f8-95fb-0dca86f871cc.cpp:11: 50563810-7125-41f8-95fb-0dca86f871cc.cpp: In function ‘int main()’: 50563810-7125-41f8-95fb-0dca86f871cc.cpp:21:20: error: ‘get_odd_collatz’ was not  declared in this scope    21 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -6,17 +6,18 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return (a == b);
 }
 
-std::vector<int> get_odd_collatz(int n) {
-    std::vector<int> result = {n};
-    while (n != 1) {
-        if (n % 2) n = 3 * n + 1;
-        else n /= 2;
-        result.push_back(n);
+std::vector<int> get_odd_collatz(int start) {
+    std::vector<int> result;
+    int num = start;
+    while(num != 1) {
+        if(num % 2 != 0) {
+            result.push_back(num);
+        }
+        num = num / 2;
     }
     return result;
 }
 
 int main() {
     assert(issame(get_odd_collatz(1), {1}));
-    return 0;
 }
