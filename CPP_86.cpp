cc7b373e1b1609d4318e32cad38f4dba0fc14f40
beep boop <githubbot@vadim.me>
2024-07-09 21:51:42 +0200

    Solution 12 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/406cr9yu Prompt: 79f15c22-6342-422b-9318-9bd95af61906.cpp: In function ‘std::string anti_shuffle( std::string)’: 79f15c22-6342-422b-9318-9bd95af61906.cpp:21:68: error: request for member ‘secon d’ in ‘* std::min_element<__gnu_cxx::__normal_iterator<char*, std::__cxx11::basi c_string<char> > >(word.std::__cxx11::basic_string<char>::begin(), word.std::__c xx11::basic_string<char>::end()).__gnu_cxx::__normal_iterator<char*, std::__cxx1 1::basic_string<char> >::operator->()’, which is of non-class type ‘char’    21 |              result += min_element(word.begin(), word.end()) -> second;       |                                                                 ^~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,5 +1,5 @@
-```cpp
 #include <string>
+#include <algorithm>
 using namespace std;
 
 string anti_shuffle(string s) {
@@ -8,33 +8,20 @@ string anti_shuffle(string s) {
         if (s[i] == ' ') {
             result += ' ';
         } else {
-            char temp = s[i];
-            while (temp != ' ') {
-                char c = temp;
-                bool changed = false;
-                for (char j = 'a'; j <= 'z'; j++) {
-                    if (j < c) {
-                        c = j;
-                        changed = true;
-                    }
+            string word = "";
+            bool first = true;
+            for (int j = i; j < s.length() && s[j] != ' '; j++) {
+                char c = s[j];
+                if (!first) {
+                    word += c;
                 }
-                for (char j = 'A'; j <= 'Z'; j++) {
-                    if (j < c) {
-                        c = j;
-                        changed = true;
-                    }
-                }
-                temp = c;
+                first = false;
             }
-            result += temp;
+            for (char c : word) {
+                result += *min_element(word.begin(), word.end());
+            }
+            i += word.length() - 1;
         }
     }
     return result;
-}
-
-int main() {
-    string input;
-    cin >> input;
-    cout << anti_shuffle(input);
-    return 0;
 }
