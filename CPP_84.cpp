0c80a5f0a0c6dbe8bfa7cdab9345fb1a8f78c31f
beep boop <githubbot@vadim.me>
2024-07-09 17:16:27 +0200

    Solution 77 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/155pyajg Prompt: 2e7a20de-757a-4178-8788-9bf18ffe8532.cpp:1:1: error: stray ‘`’ in program     1 | ```c++       | ^ 2e7a20de-757a-4178-8788-9bf18ffe8532.cpp:1:2: error: stray ‘`’ in program     1 | ```c++       |  ^ 2e7a20de-757a-4178-8788-9bf18ffe8532.cpp:1:3: error: stray ‘`’ in program     1 | ```c++       |   ^ 2e7a20de-757a-4178-8788-9bf18ffe8532.cpp:1:4: error: ‘c’ does not name a type     1 | ```c++       |    ^ In file included from 2e7a20de-757a-4178-8788-9bf18ffe8532.cpp:14: 2e7a20de-757a-4178-8788-9bf18ffe8532.cpp: In function ‘int main()’: 2e7a20de-757a-4178-8788-9bf18ffe8532.cpp:16:13: error: ‘solve’ was not declared in this scope    16 |     assert (solve(963) == "10010");       |             ^~~~~

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,8 +1,11 @@
-```cpp
-#include <cassert>
-#include <string>
-#include <stdexcept>
-
-long long solve(std::string binary) {
+long long solve(int n) {
+    std::string binary;
+    while(n > 0) {
+        if(n % 2 == 0)
+            binary.insert(0, "0");
+        else
+            binary.insert(0, "1");
+        n /= 2;
+    }
     return binary.length() == 0 ? -1 : std::stoll(binary, nullptr, 2);
 }
