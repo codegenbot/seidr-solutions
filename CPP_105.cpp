b70fa8b27c3cada69f44fe1e9a0262a525546b9b
beep boop <githubbot@vadim.me>
2024-07-09 19:35:45 +0200

    Solution 34 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/neid1i7l Prompt:       |           ^~~~~~ 915ad20b-3776-47b8-ba9d-c28adea48a0e.cpp:79:44: error: ‘b’ was not declared in t his scope    79 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 915ad20b-3776-47b8-ba9d-c28adea48a0e.cpp:87:5: error: redefinition of ‘int main( )’    87 | int main(){       |     ^~~~ 915ad20b-3776-47b8-ba9d-c28adea48a0e.cpp:65:5: note: ‘int main()’ previously def ined here    65 | int main() {       |     ^~~~ In file included from 915ad20b-3776-47b8-ba9d-c28adea48a0e.cpp:78: 915ad20b-3776-47b8-ba9d-c28adea48a0e.cpp: In function ‘int main()’: 915ad20b-3776-47b8-ba9d-c28adea48a0e.cpp:88:20: error: ‘by_length’ was not decla red in this scope    88 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 915ad20b-3776-47b8-ba9d-c28adea48a0e.cpp:88:19: error: ‘issame’ cannot be used a s a function    88 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,48 +1,76 @@
-```cpp
+#include <iostream>
 #include <vector>
-#include <string>
 #include <algorithm>
+#include <string>
 
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    return a == b;
-}
-
-std::vector<std::string> by_length(const std::vector<int>& input) {
-    vector<string> output;
-    for (int num : input) {
-        switch(num) {
-            case 1:  output.push_back("One"); break;
-            case 2:  output.push_back("Two"); break;
-            case 3:  output.push_back("Three"); break;
-            case 4:  output.push_back("Four"); break;
-            case 5:  output.push_back("Five"); break;
-            case 6:  output.push_back("Six"); break;
-            case 7:  output.push_back("Seven"); break;
-            case 8:  output.push_back("Eight"); break;
-            case 9:  output.push_back("Nine"); break;
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
         }
     }
-    return output;
+    return true;
+}
+
+vector<string> by_length(vector<int> arr) {
+    vector<int> temp;
+    for (int i : arr) {
+        if (i >= 1 && i <= 9) {
+            temp.push_back(i);
+        }
+    }
+
+    sort(temp.begin(), temp.end());
+    reverse(temp.begin(), temp.end());
+
+    vector<string> result;
+    for (int i : temp) {
+        switch (i) {
+            case 1:
+                result.push_back("One");
+                break;
+            case 2:
+                result.push_back("Two");
+                break;
+            case 3:
+                result.push_back("Three");
+                break;
+            case 4:
+                result.push_back("Four");
+                break;
+            case 5:
+                result.push_back("Five");
+                break;
+            case 6:
+                result.push_back("Six");
+                break;
+            case 7:
+                result.push_back("Seven");
+                break;
+            case 8:
+                result.push_back("Eight");
+                break;
+            case 9:
+                result.push_back("Nine");
+                break;
+        }
+    }
+
+    return result;
 }
 
 int main() {
-    vector<int> input;
-    cout << "Enter the numbers: ";
-    int num;
-    while (cin >> num) {
-        if (num >= 1 && num <= 9) {
-            input.push_back(num);
-        }
-        else {
-            break;
-        }
-    }
+    vector<int> input = {1, 4, 8};
     vector<string> output = by_length(input);
     
-    if (issame({"Nine", "Eight", "Four"}, output)) {
+    if (issame({{"One"}, {"Four"}, {"Eight"}}, {{output[0], output[1], output[2]}})) {
         for (string str : output) {
             cout << str << endl;
         }
+    } else {
+        cout << "Output is incorrect." << endl;
     }
-    return 0;
 }
