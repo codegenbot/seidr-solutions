0877c81c4d424cd8e74d5b20efa623859df40385
beep boop <githubbot@vadim.me>
2024-07-09 05:08:11 +0200

    Solution 1 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/d718nrq0 Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,33 +1,34 @@
-#include <algorithm>
-#include<string>
-
-using namespace std;
+map<string, int> numMap = {
+    {"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
+    {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
+    {"eight", 8}, {"nine", 9}
+};
 
 string sort_numbers(string numbers) {
-    map<string,int> numMap = {{"zero",0},{"one",1},{"two",2},{"three",3},{"four",4},
-                               {"five",5},{"six",6},{"seven",7},{"eight",8},{"nine",9}};
+    string result = "";
     vector<string> nums;
-    string temp;
-    for(int i=0; i<numbers.length(); i++){
-        if(isdigit(numbers[i])){
-            temp += numbers[i];
-        }else{
-            nums.push_back(temp);
-            temp = "";
-        }
-    }
-    nums.push_back(temp);
+    int start = 0, end = 0;
 
-    sort(nums.begin(),nums.end());
-    string result;
-    for(auto x: nums){
-        for(auto y:numMap){
-            if(y.first==x){
-                result+=y.first+" ";
-                break;
-            }
+    for (int i = 0; i < numbers.length(); i++) {
+        if (numbers[i] == ' ') {
+            end = i - 1;
+            nums.push_back(numbers.substr(start, end - start));
+            start = i + 1;
         }
     }
 
-    return result.substr(0,result.length()-1);
+    end = numbers.length() - 1;
+    nums.push_back(numbers.substr(start, end - start));
+
+    sort(nums.begin(), nums.end(), [&numMap](string a, string b) {
+        return numMap[a] < numMap[b];
+    });
+
+    for (int i = 0; i < nums.size(); i++) {
+        result += nums[i];
+        if (i != nums.size() - 1)
+            result += " ";
+    }
+
+    return result;
 }
