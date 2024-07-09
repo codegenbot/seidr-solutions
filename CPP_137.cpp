d859fd412989a885d7e687cd88a712d19c3a8960
beep boop <githubbot@vadim.me>
2024-07-09 03:28:05 +0200

    Solution 98 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/pf8nwdde Prompt: 3abe1714-1de4-4651-a62b-4300ca2d2d4d.cpp:3:10: fatal error: boost/variant.hpp: N o such file or directory     3 | #include <boost/variant.hpp>       |          ^~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,11 +1,6 @@
-```cpp
 #include <iostream>
 #include <string>
 #include <boost/variant.hpp>
-
-namespace boost {
-    using namespace variant;
-}
 
 int main() {
     std::string a;
@@ -16,71 +11,89 @@ int main() {
     std::cout << "Enter 1 for integer, 2 for float, 3 for string: ";
     std::cin >> choice;
 
-    variant x = choice;
+    boost::variant<int, float, std::string> x = static_cast<boost::variant<int, float, std::string>>(choice);
 
     if (choice == 1) {
-        std::string b;
+        int b;
         std::cout << "Enter your second value: ";
         std::cin >> b;
-        variant y = b;
+        boost::variant<int, float, std::string> y = static_cast<boost::variant<int, float, std::string>>(b);
         x = compare_one(x, y);
     }
     else if (choice == 2 || choice == 3) {
         float c;
-        if (choice == 2)
+        if (choice == 2) 
             std::cout << "Enter your second value: ";
         else
             std::cout << "Enter your string value: ";
         
         std::cin >> c;
-        variant z = c;
+        boost::variant<int, float, std::string> z = static_cast<boost::variant<int, float, std::string>>(c);
         x = compare_one(x, z);
     }
 
-    std::cout << boost::get<std::string>(x) << std::endl;
+    if (boost::holds_alternative<std::string>(x)) {
+        std::cout << boost::get<std::string>(x) << std::endl;
+    }
+    else if (boost::holds_alternative<int>(x)) {
+        int value = boost::get<int>(x);
+        if(value == 1)
+            std::cout << "Integer 1 is less than Integer 2" << std::endl;
+        else if(value > 1)
+            std::cout << "Integer 1 is greater than or equal to Integer 2" << std::endl;
+        else
+            std::cout << "Integers are equal" << std::endl;
+    }
+    else {
+        float value = boost::get<float>(x);
+        if(value < 0)
+            std::cout << "Float 1 is less than Float 2" << std::endl;
+        else if(value > 0)
+            std::cout << "Float 1 is greater than or equal to Float 2" << std::endl;
+        else
+            std::cout << "Floats are equal" << std::endl;
+    }
 
     return 0;
 }
 
-variant compare_one(variant a, variant b) {
-    if (holds_alternative<int>(a) && holds_alternative<float>(b)) {
-        return "1";
+boost::variant<std::string, int> compare_one(boost::variant<int, float, std::string> a, boost::variant<int, float, std::string> b) {
+    if (std::holds_alternative<int>(a) && std::holds_alternative<float>(b)) {
+        return "Integer 1 is less than Float 2";
     }
-    else if (holds_alternative<int>(a) && holds_alternative<std::string>(b)) {
-        if (get<int>(a) < 0)
+    else if (std::holds_alternative<int>(a) && std::holds_alternative<std::string>(b)) {
+        if (boost::get<int>(a) < 0)
             return "Negative integer is less than string";
         else
             return "Positive integer is greater than or equal to string";
     }
-    else if (holds_alternative<int>(a) && holds_alternative<int>(b)) {
-        int x = get<int>(a);
-        int y = get<int>(b);
-        if(x < y)
+    else if (std::holds_alternative<int>(a) && std::holds_alternative<int>(b)) {
+        int x = boost::get<int>(a);
+        int y = boost::get<int>(b);
+        if (x < y)
             return "Integer 1 is less than Integer 2";
-        else if(x > y)
-            return "Integer 1 is greater than Integer 2";
+        else if (x > y)
+            return "Integer 1 is greater than or equal to Integer 2";
         else
             return "Integers are equal";
     }
-    else if (holds_alternative<float>(a) && holds_alternative<float>(b)) {
-        float x = get<float>(a);
-        float y = get<float>(b);
-        if(x < y)
+    else if (std::holds_alternative<float>(a) && std::holds_alternative<int>(b)) {
+        float x = boost::get<float>(a);
+        int y = boost::get<int>(b);
+        if (x < 0)
+            return "Negative float is less than integer";
+        else
+            return "Positive float is greater than or equal to integer";
+    }
+    else if (std::holds_alternative<float>(a) && std::holds_alternative<float>(b)) {
+        float x = boost::get<float>(a);
+        float y = boost::get<float>(b);
+        if (x < y)
             return "Float 1 is less than Float 2";
-        else if(x > y)
-            return "Float 1 is greater than Float 2";
+        else if (x > y)
+            return "Float 1 is greater than or equal to Float 2";
         else
             return "Floats are equal";
-    }
-    else if (holds_alternative<std::string>(a) && holds_alternative<std::string>(b)) {
-        std::string strA = get<std::string>(a);
-        std::string strB = get<std::string>(b);
-        if (strA > strB)
-            return "String 1 is greater than String 2";
-        else if (strA < strB)
-            return "String 1 is less than String 2";
-        else
-            return "Strings are equal";
     }
     else {
         return "Invalid input";
