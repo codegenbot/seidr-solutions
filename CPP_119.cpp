715a86e08cfffb4eb95f788d193c8851544b1ee0
beep boop <githubbot@vadim.me>
2024-07-09 22:02:54 +0200

    Solution 10 of CPP/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mf7fb8zj Prompt: 7194a1b4-8f7d-4e3d-8668-7b44da58a474.cpp:3:1: error: expected unqualified-id bef ore ‘for’     3 | for(auto str : lst) {       | ^~~ 7194a1b4-8f7d-4e3d-8668-7b44da58a474.cpp:10:1: error: expected unqualified-id be fore ‘if’    10 | if(totalOpen == totalClose)       | ^~ 7194a1b4-8f7d-4e3d-8668-7b44da58a474.cpp:12:1: error: expected unqualified-id be fore ‘else’    12 | else       | ^~~~ In file included from 7194a1b4-8f7d-4e3d-8668-7b44da58a474.cpp:15: 7194a1b4-8f7d-4e3d-8668-7b44da58a474.cpp: In function ‘int main()’: 7194a1b4-8f7d-4e3d-8668-7b44da58a474.cpp:17:13: error: ‘match_parens’ was not de clared in this scope    17 |     assert (match_parens({")", "("}) == "Yes" );       |             ^~~~~~~~~~~~

--- CPP_119.cpp
+++ CPP_119.cpp
@@ -1,16 +1,18 @@
 #include <vector>
 #include <string>
 
-std::vector<std::string> lst = {"(a)", "b(c(d))"};
+std::vector<std::string> lst = {"(abc)", "(def)"};
 
 int match_parens(const std::vector<std::string>& v) {
     int totalOpen = 0, totalClose = 0;
+
     for(auto str : v) {
         for(char c : str) {
             if(c == '(') totalOpen++;
             else if(c == ')') totalClose++;
         }
     }
+
     if(totalOpen == totalClose)
         return "Yes";
     else
@@ -18,7 +20,5 @@ int match_parens(const std::vector<std::string>& v) {
 }
 
 int main() {
-    assert(match_parens({}) == "Yes");
-    std::cout << match_parens(lst) << std::endl;
-    return 0;
+    std::cout << match_parens(lst);
 }
