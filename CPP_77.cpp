f4143d0c8c9d87b39adac75c98ad3959c12a7fef
beep boop <githubbot@vadim.me>
2024-07-09 09:17:19 +0200

    Solution 24 of CPP/77, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/03w5r3bz Prompt: 31d76f4d-371a-45d9-a8a1-acd1a889cbd6.cpp: In function ‘int cpp77Main()’: 31d76f4d-371a-45d9-a8a1-acd1a889cbd6.cpp:2:5: error: ‘originalFunction’ was not declared in this scope     2 |     originalFunction();       |     ^~~~~~~~~~~~~~~~ 31d76f4d-371a-45d9-a8a1-acd1a889cbd6.cpp:3:13: error: ‘isCube’ was not declared in this scope     3 |     assert (isCube(1729) == false);       |             ^~~~~~ 31d76f4d-371a-45d9-a8a1-acd1a889cbd6.cpp:3:5: error: ‘assert’ was not declared i n this scope     3 |     assert (isCube(1729) == false);       |     ^~~~~~ 31d76f4d-371a-45d9-a8a1-acd1a889cbd6.cpp:1:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?   +++ |+#include <cassert>     1 | int cpp77Main(){ In file included from 31d76f4d-371a-45d9-a8a1-acd1a889cbd6.cpp:7: 31d76f4d-371a-45d9-a8a1-acd1a889cbd6.cpp: In function ‘int main()’: 31d76f4d-371a-45d9-a8a1-acd1a889cbd6.cpp:9:13: error: ‘iscuber’ was not declared  in this scope     9 |     assert (iscuber(1729) == false);       |             ^~~~~~~

--- CPP_77.cpp
+++ CPP_77.cpp
@@ -1,10 +1,22 @@
 #include <cassert>
 
-bool isCube(int);
+bool isCube(int n) {
+    int side = round(cbrt(n));
+    if (side * side * side == n)
+        return true;
+    else
+        return false;
+}
 
-void originalFunction() {}
+void originalFunction() {
+    for (int i = 1; i <= 1000; i++) {
+        if (isCube(i)) {
+            cout << "The cube root of " << i << " is " << round(cbrt(i)) << endl;
+        }
+    }
+}
 
-int main(){
+int cpp77Main(){
     originalFunction();
     assert (isCube(1729) == false);
     return 0;
