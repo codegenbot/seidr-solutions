1fc4f750aa3ff48a53fe9e842bc379d7ae6e100b
beep boop <githubbot@vadim.me>
2024-07-09 07:48:40 +0200

    Solution 5 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n5yplexf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ba7cc85f-c446-4f8b-948e-be8e1f87c 233.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,2 +1,6 @@
-def sum_odd(lst):
-    return sum(i for i in lst if isinstance(i, int) and i % 2 != 0)
+def sum_of_odd_numbers(lst):
+    total = 0
+    for i in lst:
+        if i % 2 != 0:
+            total += i
+    return total
