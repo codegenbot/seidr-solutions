add28c1e2bb935778f6e938156f5a9a6dfa760d9
beep boop <githubbot@vadim.me>
2024-07-10 01:09:17 +0200

    Solution 73 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mlild2ig Prompt:    16 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c6738ec1-8cec-41ca-852f-739cbecdfe81.cpp:16:44: error: ‘b’ was not declared in t his scope    16 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c6738ec1-8cec-41ca-852f-739cbecdfe81.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c6738ec1-8cec-41ca-852f-739cbecdfe81.cpp:15: c6738ec1-8cec-41ca-852f-739cbecdfe81.cpp: In function ‘int main()’: c6738ec1-8cec-41ca-852f-739cbecdfe81.cpp:25:20: error: ‘sorted_list_sum’ was not  declared in this scope    25 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ c6738ec1-8cec-41ca-852f-739cbecdfe81.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -3,23 +3,15 @@
 #include <string>
 
 bool isSame(const std::string& a, const std::string& b) {
-    if (a.length() != b.length()) {
-        return false;
-    }
-    for (int i = 0; i < a.length(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
 std::vector<std::string> sorted_list_sum(std::vector<std::string> lst) {
     auto it = unique(lst.begin(), lst.end(),
-                      [](const std::string& a, const std::string& b) { return a.length() % 2 == 1; });
+                      [](const string& a, const string& b) { return a.length() % 2 == 1; });
     lst.erase(it, lst.end());
     sort(lst.begin(), lst.end(),
-         [](const std::string& a, const std::string& b) {
+         [](const string& a, const string& b) {
              if (a.length() != b.length())
                  return a.length() < b.length();
              else
