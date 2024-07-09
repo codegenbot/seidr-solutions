3942226ac15328fe7785a666581c3e8d0a757794
beep boop <githubbot@vadim.me>
2024-07-09 02:47:07 +0200

    Solution 4 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/f2h0xsqp Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 322dd2e6-8c2f-4b96-8e18-cda989f524d5.cpp:15:36: error: expected primary-expressi on before ‘float’    15 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 322dd2e6-8c2f-4b96-8e18-cda989f524d5.cpp:15:43: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 322dd2e6-8c2f-4b96-8e18-cda989f524d5.cpp:14: 322dd2e6-8c2f-4b96-8e18-cda989f524d5.cpp: In function ‘int main()’: 322dd2e6-8c2f-4b96-8e18-cda989f524d5.cpp:24:20: error: ‘rescale_to_unit’ was not  declared in this scope    24 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 322dd2e6-8c2f-4b96-8e18-cda989f524d5.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,15 +1,28 @@
-#include<stdio.h>
-#include<math.h>
-#include<vector>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
-vector<float> rescale_to_unit(vector<float> numbers){
+bool issame(vector<float> a,vector<float>b){
+    for(int i=0;i<a.size();i++){
+        if(a[i] != b[i])
+            return false;
+    }
+    return true;
+}
+
+vector<float> rescale_to_unit(vector<float> numbers) {
     float minVal = *min_element(numbers.begin(), numbers.end());
     float maxVal = *max_element(numbers.begin(), numbers.end());
 
     vector<float> result;
-    for(float num : numbers) {
+    for (float num : numbers) {
         result.push_back((num - minVal) / (maxVal - minVal));
     }
     return result;
+}
+
+int main() {
+    assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.25f, 0.0f, 1.0f, 0.5f, 0.75f}));
+    return 0;
 }
