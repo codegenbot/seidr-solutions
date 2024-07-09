e48df96af87e7f1b931d20c4e6eada8270955ddf
beep boop <githubbot@vadim.me>
2024-07-09 18:18:15 +0200

    Solution 21 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/symxc0fq Prompt:    46 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 30ee6b77-708a-4250-9464-9b6e379c7175.cpp:54:5: error: redefinition of ‘int main( )’    54 | int main(){       |     ^~~~ 30ee6b77-708a-4250-9464-9b6e379c7175.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~ In file included from 30ee6b77-708a-4250-9464-9b6e379c7175.cpp:45: 30ee6b77-708a-4250-9464-9b6e379c7175.cpp: In function ‘int main()’: 30ee6b77-708a-4250-9464-9b6e379c7175.cpp:55:20: error: ‘sort_even’ was not decla red in this scope    55 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 30ee6b77-708a-4250-9464-9b6e379c7175.cpp:55:19: error: ‘issame’ cannot be used a s a function    55 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -31,7 +31,7 @@ vector<float> sort_even(vector<float> l) {
 int main() {
     vector<float> input({5, 8, -12, 4, 23, 2, 3, 11, 12, -10});
     vector<float> expected({-12, 8, 3, 4, 5, 2, 12, 11, 23, -10});
-    
+
     vector<float> result = sort_even(input);
     assert(issame(result, expected));
     
