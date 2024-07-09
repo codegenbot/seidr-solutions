11d5272499709e30726bf7aa520cfd4272a89a40
beep boop <githubbot@vadim.me>
2024-07-09 21:32:37 +0200

    Solution 88 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/y5u3ac0c Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b9db0ce-9fd1-4063-90a7-45908cb9b 590.py", line 1     ```     ^ SyntaxError: invalid syntax

--- paired-digits.py
+++ paired-digits.py
@@ -1,6 +1,5 @@
-```python
 def paired_digits(s):
-    s = str(int(s.lstrip('0'))).replace('00', '')  
+    s = str(int(s.lstrip('0'))).replace('00', '')
     total = 0
     for i in range(len(s)-1):
         if s[i] == s[i+1]:
