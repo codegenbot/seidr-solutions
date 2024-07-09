a30087ff50550bf796660c2bc4893ffc98e77fe7
beep boop <githubbot@vadim.me>
2024-07-09 12:10:39 +0200

    Solution 2 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ccxagfbb Prompt:       |                              ^~~~~~ f0f6fcf5-10ce-4c2a-8897-0dfd35f3690b.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f0f6fcf5-10ce-4c2a-8897-0dfd35f3690b.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f0f6fcf5-10ce-4c2a-8897-0dfd35f3690b.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f0f6fcf5-10ce-4c2a-8897-0dfd35f3690b.cpp:9: f0f6fcf5-10ce-4c2a-8897-0dfd35f3690b.cpp: In function ‘int main()’: f0f6fcf5-10ce-4c2a-8897-0dfd35f3690b.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ f0f6fcf5-10ce-4c2a-8897-0dfd35f3690b.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,14 +1,17 @@
-#include <string>
-#include <vector>
-
-bool areSame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+```cpp
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-std::vector<std::string> all_prefixes(std::string str) {
+vector<string> all_prefixes(string str) {
     vector<string> result;
     for (int i = 1; i <= str.length(); i++) {
         result.push_back(str.substr(0, i));
     }
     return result;
+}
+
+int main() {
+    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    return 0;
 }
