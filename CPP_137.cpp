a3b5ebc4b128e1680addf687bd8b87fa158c092e
beep boop <githubbot@vadim.me>
2024-07-09 16:04:42 +0200

    Solution 59 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/40ydl3g4 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,39 +1,17 @@
-#include <string>
-#include <boost/any.hpp>
-
-using namespace std;
+using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return max(a.convert<int>(), b.convert<int>());
-    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        return max(a.convert<float>(), b.convert<float>());
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = a.convert<string>();
-        string str2 = b.convert<string>();
-
-        size_t pos = 0, start = 0;
-        while ((pos = str1.find('.', start)) != string::npos) {
-            str1.replace(pos, 1, ",");
-            start = pos + 1;
-        }
-
-        pos = 0, start = 0;
-        while ((pos = str2.find('.', start)) != string::npos) {
-            str2.replace(pos, 1, ",");
-            start = pos + 1;
-        }
-
-        if (str1 > str2)
-            return a;
-        else if (str1 < str2)
-            return b;
-        else
-            return boost::any("None");
-    } else if ((a.type() == typeid(int) && b.type() != typeid(int)) ||
-               (a.type() != typeid(int) && b.type() == typeid(int))) {
-        return boost::any("None");
+    if (any_cast<int>(a) > any_cast<int>(b)) {
+        return a;
+    } else if (any_cast<int>(b) > any_cast<int>(a)) {
+        return b;
+    } else if (is_any_of<string>(a) && is_any_of<string>(b)) {
+        return (any_cast<string>(a) > any_cast<string>(b)) ? a : b;
+    } else if (is_any_of<string>(a)) {
+        return "None";
+    } else if (is_any_of<string>(b)) {
+        return "None";
+    } else {
+        return max(a, b);
     }
-
-    return a > b ? a : b;
 }
