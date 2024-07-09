5af696cb66bb1b18bf977b725512a8dee2855b61
beep boop <githubbot@vadim.me>
2024-07-09 10:34:10 +0200

    Solution 15 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/owo9prrk Prompt:       |                              ^~~~~~ 017004f9-7662-4330-953c-1be7b93055bb.cpp:14:37: error: ‘string’ was not declared  in this scope    14 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 017004f9-7662-4330-953c-1be7b93055bb.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 017004f9-7662-4330-953c-1be7b93055bb.cpp:14:45: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 017004f9-7662-4330-953c-1be7b93055bb.cpp:13: 017004f9-7662-4330-953c-1be7b93055bb.cpp: In function ‘int main()’: 017004f9-7662-4330-953c-1be7b93055bb.cpp:23:20: error: ‘reverse_delete’ was not declared in this scope    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 017004f9-7662-4330-953c-1be7b93055bb.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -5,17 +5,16 @@
 #include <cassert>
 
 bool issame(vector<string> a, vector<string> b) {
-}
 
 vector<string> reverse_delete(string s, string c) {
     string result = "";
-    for (char ch : s) {
-        if (c.find(ch) == string::npos) {
+    for(char ch : s) {
+        if(c.find(ch) == string::npos) {
             result += ch;
         }
     }
     string result_reversed = result;
     reverse(result_reversed.begin(), result_reversed.end());
-
+    
     return {result, (result == result_reversed) ? "True" : "False"};
 }
