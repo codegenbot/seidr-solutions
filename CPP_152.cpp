acc3fb61b08a4ee54a5d197c1720f8908c3496a2
beep boop <githubbot@vadim.me>
2024-07-10 01:08:06 +0200

    Solution 64 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/szteghpq Prompt:       |                    ^~~ ffc48acd-4360-460c-933d-5e7ae5ebf7c7.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ffc48acd-4360-460c-933d-5e7ae5ebf7c7.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ffc48acd-4360-460c-933d-5e7ae5ebf7c7.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ffc48acd-4360-460c-933d-5e7ae5ebf7c7.cpp:15: ffc48acd-4360-460c-933d-5e7ae5ebf7c7.cpp: In function ‘int main()’: ffc48acd-4360-460c-933d-5e7ae5ebf7c7.cpp:25:20: error: ‘compare’ was not declare d in this scope    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ ffc48acd-4360-460c-933d-5e7ae5ebf7c7.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,12 +1,53 @@
+#include <iostream>
 #include <vector>
 
-vector<int> compare(vector<int> game, vector<int> guess) {
-    vector<int> result;
+bool issame(int a, int b) {
+    if (a == b)
+        return true;
+    else
+        return false;
+}
+
+std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
+    std::vector<int> result;
     for (int i = 0; i < game.size(); i++) {
-        if (game[i] == guess[i]) {
+        if (issame(game[i], guess[i])) {
             result.push_back(0);
         } else {
-            result.push_back(abs(guess[i] - game[i]));
+            int diff = abs(guess[i] - game[i]);
+            result.push_back(diff);
         }
     }
     return result;
+}
+
+int main() {
+    std::vector<int> game, guess;
+    
+    // Input for game and guess
+    int n;
+    std::cout << "Enter the number of rounds: ";
+    std::cin >> n;
+    
+    cout << "Enter the numbers for game: ";
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        game.push_back(num);
+    }
+    
+    cout << "Enter the numbers for guess: ";
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        guess.push_back(num);
+    }
+    
+    // Compare and print
+    std::vector<int> res = compare(game, guess);
+    cout << "The result is: ";
+    for (int i = 0; i < n; i++) {
+        cout << res[i] << " ";
+    }
+    return 0;
+}
