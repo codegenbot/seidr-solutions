06374c82b0d4b166f038d21e044733c75f9a2acc
beep boop <githubbot@vadim.me>
2024-07-09 07:39:07 +0200

    Solution 1 of Python/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/786zdr3m Prompt: Complete the following code given the task description and function signature.

--- Python_104.py
+++ Python_104.py
@@ -1,3 +1,3 @@
 ```
 def unique_digits(x):
-    return sorted([i for i in x if not any(int(digit) % 2 == 0 for digit in str(i))])
+    return sorted([i for i in x if all(int(d) % 2 != 0 for d in str(i))])
