83426190707b3ebfb699049c763c24594ace32f0
beep boop <githubbot@vadim.me>
2024-07-09 02:07:02 +0200

    Solution 25 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/j2eldqxj Prompt: 64c99f8e-dc6e-4094-ba89-f3636bdc5712.cpp:25:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 64c99f8e-dc6e-4094-ba89-f3636bdc5712.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 64c99f8e-dc6e-4094-ba89-f3636bdc5712.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 64c99f8e-dc6e-4094-ba89-f3636bdc5712.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <iostream>
-#include <cassert>
 using namespace std;
 
 bool issame(vector<int> a,vector<int>b){
@@ -18,12 +17,5 @@ vector<int> maximum(vector<int> arr, int k) {
 
 int main() {
     assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 7) , {0,-23,-400,1,2,3,243}));
-    vector<int> arr = {1, 2, 3, -23, 243, -400, 0};
-    int k = 7;
-    vector<int> result = maximum(arr, k);
-    for(int i = 0; i < result.size(); i++){
-        cout << result[i] << " ";
-    }
-    cout << endl;
     return 0;
 }
