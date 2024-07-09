292ac1854d87e3cb3ab347f7cc52e92614335664
beep boop <githubbot@vadim.me>
2024-07-09 19:17:43 +0200

    Solution 13 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/ri48aft6 Prompt: dc658694-cd23-4c5b-bd3a-e1027b153509.cpp:1:6: error: ‘pair’ in namespace ‘std’ d oes not name a template type     1 | std::pair<int, int> findPair(vector<int>& nums, int target) {       |      ^~~~ dc658694-cd23-4c5b-bd3a-e1027b153509.cpp:1:1: note: ‘std::pair’ is defined in he ader ‘<utility>’; did you forget to ‘#include <utility>’?   +++ |+#include <utility>     1 | std::pair<int, int> findPair(vector<int>& nums, int target) {

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,29 +1,3 @@
-#include <vector>
-#include <algorithm>
+#include <utility>
 
-using namespace std;
-
-pair<int, int> findPair(vector<int>& nums, int target) {
-    sort(nums.begin(), nums.end());
-    for (int i = 0; i < nums.size(); i++) {
-        int complement = target - nums[i];
-        int left = i + 1;
-        int right = nums.size() - 1;
-        
-        while (left <= right) {
-            int sum = nums[left] + nums[right];
-            
-            if (sum == target) {
-                return {nums[i], nums[right]};
-            }
-            else if (sum < target) {
-                left++;
-            }
-            else {
-                right--;
-            }
-        }
-    }
-    
-    return {-1, -1};
-}
+std::pair<int, int> findPair(vector<int>& nums, int target) {
