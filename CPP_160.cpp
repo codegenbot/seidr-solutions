7c393148e3963ed507803cb2fdc152c707e026ba
beep boop <githubbot@vadim.me>
2024-07-09 02:25:32 +0200

    Solution 20 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0xw9ottc Prompt: for array subscript    16 |         } else if (ops[i] == "*") {       |                       ^ 4e4e8024-7cbd-42bd-add3-29a54d0e1167.cpp:18:23: error: invalid types ‘int[int]’ for array subscript    18 |         } else if (ops[i] == "//") {       |                       ^ 4e4e8024-7cbd-42bd-add3-29a54d0e1167.cpp:20:23: error: invalid types ‘int[int]’ for array subscript    20 |         } else if (ops[i] == "**") {       |                       ^ In file included from 4e4e8024-7cbd-42bd-add3-29a54d0e1167.cpp:28: 4e4e8024-7cbd-42bd-add3-29a54d0e1167.cpp: In function ‘int main()’: 4e4e8024-7cbd-42bd-add3-29a54d0e1167.cpp:30:23: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    30 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~       |                       |       |                       <brace-enclosed initializer list> 4e4e8024-7cbd-42bd-add3-29a54d0e1167.cpp:7:31: note:   initializing argument 1 o f ‘int do_algebra(int, std::vector<int>)’     7 | int do_algebra(vector<string> ops, vector<int> nums) {       |                ~~~~~~~~~~~~~~~^~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,24 +1,18 @@
-#include <vector>
-#include <cmath>
-using namespace std;
-
-int do_algebra(vector<string>, vector<int>);
-
 int do_algebra(vector<string> ops, vector<int> nums) {
     int result = 0;
     int temp = 1;
 
     for (int i = 0; i < ops.size(); i++) {
         if (ops[i] == "+") {
-            result += (int)(temp * nums[i]);
+            result += temp * nums[i];
         } else if (ops[i] == "-") {
-            result -= (int)(temp * nums[i]);
+            result -= temp * nums[i];
         } else if (ops[i] == "*") {
             temp *= nums[i];
-        } else if (ops[i] == "//") {
-            temp = temp / nums[i];
+        } else if (ops[i] == "//") { // integer division
+            temp /= nums[i];
         } else if (ops[i] == "**") {
-            temp = pow((double)temp, (double)nums[i]);
+            temp = pow(temp, nums[i]);
         }
     }
 
