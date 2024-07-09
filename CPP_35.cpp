b1c1112c73c6ab0922b61408316130961fd805e0
beep boop <githubbot@vadim.me>
2024-07-09 07:47:36 +0200

    Solution 32 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/wbyr1spe Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 75c67aaf-d9e8-4368-b588-e7250c4c69bb.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -2,7 +2,6 @@
 #include <cassert>
 #include <vector>
 #include <initializer_list>
-#include <iterator>
 using namespace std;
 
 int findMax(vector<int> l) {
@@ -13,5 +12,5 @@ int findMax(vector<int> l) {
 
 int main() {
     vector<int> v = {1, 2, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10};
-    assert(abs(*std::max_element(v.begin(), v.end()))- 12)<1e-4);
+    assert(abs(*std::max_element({1, 2, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}.begin(), {1, 2, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}.end()))- 12)<1e-4);
 }
