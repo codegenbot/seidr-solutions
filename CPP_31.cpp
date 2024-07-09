0c98035d338781f7c70d93a149d16ce3f255cef0
beep boop <githubbot@vadim.me>
2024-07-09 15:18:09 +0200

    Solution 45 of CPP/31, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/1dwzrait Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_31.cpp
+++ CPP_31.cpp
@@ -11,18 +11,15 @@ int cpp_main() {
 
     if(is_prime(num)) {
         std::cout << num << " is a prime number." << std::endl;
-    } else {
-        std::cout << num << " is not a prime number." << std::endl;
-    }
-
-    for(int i = 2; i <= num; i++) { 
-        if(is_prime(i)) {
-            std::cout << i << " is the entered prime number." << std::endl;
-        } else {
-            std::cout << std::to_string(i) << (is_prime(i) ? " is a prime number." : " is not a prime number.") << std::endl;
+        
+        for(int i = 2; i <= num; i++) {
+            if(is_prime(i))
+                std::cout << i << " is a prime number" << std::endl;
+            else
+                std::cout << i << (is_prime(i) ? " is a prime number" : "") << std::endl;
         }
     }
-
+    
     return 0;
 }
 
