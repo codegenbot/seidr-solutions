033540b4637b7d74c44d8fd91e14024d3704e90d
beep boop <githubbot@vadim.me>
2024-07-09 15:28:48 +0200

    Solution 29 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5uknokbc Prompt:       |                                     ^ In file included from /usr/include/c++/11/cassert:44,                  from 0c9be87c-8eec-4252-bb08-f90a34b9db0c.cpp:1: 0c9be87c-8eec-4252-bb08-f90a34b9db0c.cpp: In function ‘int main()’: 0c9be87c-8eec-4252-bb08-f90a34b9db0c.cpp:17:22: error: ‘same_chars’ cannot be us ed as a function    17 |     assert(same_chars("aabb", "aaccc") == false);       |            ~~~~~~~~~~^~~~~~~~~~~~~~~~~ 0c9be87c-8eec-4252-bb08-f90a34b9db0c.cpp: At global scope: 0c9be87c-8eec-4252-bb08-f90a34b9db0c.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 0c9be87c-8eec-4252-bb08-f90a34b9db0c.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 0c9be87c-8eec-4252-bb08-f90a34b9db0c.cpp:21: 0c9be87c-8eec-4252-bb08-f90a34b9db0c.cpp: In function ‘int main()’: 0c9be87c-8eec-4252-bb08-f90a34b9db0c.cpp:23:23: error: ‘same_chars’ cannot be us ed as a function    23 |     assert (same_chars("aabb", "aaccc") == false);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -1,10 +1,19 @@
 #include <cassert>
+#include <string>
 
 bool same_chars(string s0, string s1) {
-    return s0 == s1;
+    if(s0.length() != s1.length()) {
+        return false;
+    }
+    for(int i = 0; i < s0.length(); i++) {
+        if(count(s0.begin(), s0.end(), s1[i]) != 1) {
+            return false;
+        }
+    }
+    assert(same_chars("aabb", "aaccc") == false);
+    return true;
 }
 
 int main() {
-    assert(same_chars("aabb", "aaccc") == false);
     return 0;
 }
