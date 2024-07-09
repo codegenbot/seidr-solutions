8c4bfa840cf9149a19bf6b1d0f2660237187c421
beep boop <githubbot@vadim.me>
2024-07-09 08:02:08 +0200

    Solution 53 of CPP/18, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xxdogq6a Prompt: ka ‘const std::__cxx11::basic_string<char>’} as ‘this’ argument discards qualifi ers [-fpermissive]    13 |         str.erase(0, pos + substring.length());       |         ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from f27c63bf-53b1-4f04-bf5f-cb20d56f12d4.cpp:1: /usr/include/c++/11/bits/basic_string.h:1827:7: note:   in call to ‘std::__cxx11 ::basic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Tra its, _Alloc>::erase(std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_ty pe, std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type) [with _CharT  = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>; std::_ _cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type = long unsigned int]’  1827 |       erase(size_type __pos = 0, size_type __n = npos)       |       ^~~~~ f27c63bf-53b1-4f04-bf5f-cb20d56f12d4.cpp: At global scope: f27c63bf-53b1-4f04-bf5f-cb20d56f12d4.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ f27c63bf-53b1-4f04-bf5f-cb20d56f12d4.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -1,18 +1,16 @@
 #include <string>
 #include <cassert>
 #include <iostream>
-#include <limits>
 
 using namespace std;
 
 int how_many_times(const string& str, const string& substring) {
     int count = 0;
     size_t pos = 0;
-    size_t original_len = str.length();
     
     while ((pos = str.find(substring)) != string::npos) {
         count++;
-        original_len -= (pos + substring.length());
+        str = str.substr(pos + substring.length());
     }
     return count;
 
@@ -23,7 +21,6 @@ int main() {
     cout << "Enter a string: ";
     getline(cin, str);
     cout << "Enter a substring: ";
-    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
     getline(cin, substring);
     
     if (str.find(substring) == string::npos) {
