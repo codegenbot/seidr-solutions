91f97b1bb4eef2b377198da8a359ccf7ca8681b3
beep boop <githubbot@vadim.me>
2024-07-09 23:15:30 +0200

    Solution 95 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zdlkyuzi Prompt: Program did not receive expected input

--- Python_1.py
+++ Python_1.py
@@ -3,7 +3,7 @@ from typing import List
 def separate_paren_groups(paren_string: str) -> List[str]:
     result = []
     stack, temp = [], ""
-    
+
     for char in paren_string:
         if char == "(":
             if temp and stack:
@@ -16,7 +16,7 @@ def separate_paren_groups(paren_string: str) -> List[str]:
             if not stack:
                 result.append(temp + char)
                 temp = ""
-    
+
     if stack:
         return ["Invalid Input"]
     return result
