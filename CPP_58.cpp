ef93e730d441ae7e5b576d757cd24aea44f141ac
beep boop <githubbot@vadim.me>
2024-07-09 21:33:36 +0200

    Solution 46 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g34sr5ik Prompt:       |     ^~~~~~ 46567e9d-34d5-498f-95f1-651298ba9ba9.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | using namespace std; 46567e9d-34d5-498f-95f1-651298ba9ba9.cpp: At global scope: 46567e9d-34d5-498f-95f1-651298ba9ba9.cpp:25:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 46567e9d-34d5-498f-95f1-651298ba9ba9.cpp:4:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     4 | bool issame(vector<int> a,vector<int> b){       |      ^~~~~~ 46567e9d-34d5-498f-95f1-651298ba9ba9.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 46567e9d-34d5-498f-95f1-651298ba9ba9.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+bool issame(vector<int> a,vector<int> b){
+    return a == b;
+}
+
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
