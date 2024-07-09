ab81c0e99eb5bbc689405c2177fad5ec796d5b0e
beep boop <githubbot@vadim.me>
2024-07-09 14:09:14 +0200

    Solution 17 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7dqgrwjn Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from f8167259-6c8d-46b4-96a6-f05fb940bccc.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,7 +1,15 @@
+```cpp
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
 using namespace std;
+
+template<typename T>
+std::vector<T> make_vector(T t) {
+    std::vector<T> result(1, t);
+    return result;
+}
 
 bool isSame(vector<float> a,vector<float>b){
     if(a.size() != b.size()) return false;
@@ -11,7 +19,7 @@ bool isSame(vector<float> a,vector<float>b){
     return true;
 }
 
-vector<float> sortEven(vector<float> l) {
+vector<float> sort_even(vector<float> l) {
     vector<float> result(l.size());
     int j = 0;
     for (int i = 0; i < l.size(); i++) {
@@ -31,8 +39,6 @@ vector<float> sortEven(vector<float> l) {
 }
 
 int main() {
-    vector<float> input = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
-    vector<float> output = sortEven(input);
-    assert(isSame(output , {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    assert(isSame(sort_even(vector<float>(make_vector({5, 8, -12, 4, 23, 2, 3, 11, 12, -10})) ) , {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
     return 0;
 }
