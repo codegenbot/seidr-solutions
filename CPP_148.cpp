ab67b43abc2eec646759aec20662b09cd71ac2f3
beep boop <githubbot@vadim.me>
2024-07-09 02:06:12 +0200

    Solution 17 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/a6f2q0q7 Prompt: ea0ac2b4-4536-4883-91f5-668f950af7fa.cpp:37:37: error: ‘string’ was not declared  in this scope    37 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ea0ac2b4-4536-4883-91f5-668f950af7fa.cpp:37:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    37 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf ea0ac2b4-4536-4883-91f5-668f950af7fa.cpp:37:45: error: expression list treated a s compound expression in initializer [-fpermissive]    37 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ea0ac2b4-4536-4883-91f5-668f950af7fa.cpp:36: ea0ac2b4-4536-4883-91f5-668f950af7fa.cpp: In function ‘int main()’: ea0ac2b4-4536-4883-91f5-668f950af7fa.cpp:46:22: error: ‘bf’ cannot be used as a function    46 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ~~^~~~~~~~~~~~~~~~~~~~~~~ ea0ac2b4-4536-4883-91f5-668f950af7fa.cpp:46:19: error: ‘issame’ cannot be used a s a function    46 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -3,6 +3,7 @@
 
 std::vector<std::string> bf(std::string planet1, std::string planet2) {
     std::vector<std::string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+    
     int index1 = -1, index2 = -1;
     
     for (int i = 0; i < planets.size(); ++i) {
