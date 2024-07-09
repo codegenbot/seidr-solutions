94265b9b1aa2f85ac49779d205abc17a82529f3a
beep boop <githubbot@vadim.me>
2024-07-09 16:05:34 +0200

    Solution 44 of CPP/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sjqp5j6z Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = char]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = char; std::allocator_tra its<std::allocator<_CharT> >::pointer = char*; std::allocator_traits<std::alloca tor<_CharT> >::allocator_type = std::allocator<char>]’ /usr/include/c++/11/bits/stl_deque.h:562:26:   required from ‘std::_Deque_base<_ Tp, _Alloc>::_Ptr std::_Deque_base<_Tp, _Alloc>::_M_allocate_node() [with _Tp = char; _Alloc = std::allocator<char>; std::_Deque_base<_Tp, _Alloc>::_Ptr = char* ]’ /usr/include/c++/11/bits/deque.tcc:497:65:   required from ‘void std::deque<_Tp,  _Alloc>::_M_push_back_aux(_Args&& ...) [with _Args = {const char&}; _Tp = char;  _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/stl_deque.h:1506:20:   required from ‘void std::deque<_ Tp, _Alloc>::push_back(const value_type&) [with _Tp = char; _Alloc = std::alloca tor<char>; std::deque<_Tp, _Alloc>::value_type = char]’ /usr/include/c++/11/bits/stl_stack.h:240:20:   required from ‘void std::stack<_T p, _Sequence>::push(const value_type&) [with _Tp = char; _Sequence = std::deque< char>; std::stack<_Tp, _Sequence>::value_type = char]’ 8f9c4448-d30c-4d45-bfa4-04e63315f323.cpp:10:23:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<char>’ has no member named ‘_M_max_size’

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 bool correct_bracketing(const std::string& s) {
-    std::stack<char> stack; 
+    std::stack<char> stack;
     for (char c : s) {
         if (c == '(') {
             stack.push(c);
