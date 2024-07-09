78edf2a92a43fafd0a94990a530a56993ec07ce4
beep boop <githubbot@vadim.me>
2024-07-09 07:46:29 +0200

    Solution 33 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jicx7wv6 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 6fae166e-1ef7-42dc-9d35-d2cdbf172a74.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,7 +1,40 @@
+```cpp
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double poly(vector<double> coeffs, double solution) {
+    double result = 0;
+    for (int i = 0; i < coeffs.size(); i++) {
+        result += coeffs[i] * pow(solution, i);
+    }
+    return result;
+}
+
+double find_zero(vector<double> xs){
+    double sum = 0;
+    double coeff = xs[0];
+    vector<double> coeffs; 
+    for (int i = 1; i < xs.size(); i++) {
+        if (i % 2 == 0) {
+            sum += xs[i] / coeff;
+            coeffs.push_back(coeff); 
+        }
+        else{
+            coeff = xs[i];
+        }
+    }
+    return -sum / coeff;
+}
+
 int main() {
     vector<double> xs; 
     cout << "Enter coefficients (space separated): ";
-    for(double x; cin >> x; xs.push_back(x)); 
+    for(double x; cin >> x; ) {
+        xs.push_back(x); 
+    }    
     double solution = find_zero(xs);
     vector<double> coeffs;
     for (int i = 1; i < xs.size(); i++) {
@@ -9,6 +42,6 @@ int main() {
             coeffs.push_back(xs[i] / xs[i-1]);
         }
     }
-    cout << "The zero of the polynomial is: " << solution << endl;
+    assert(abs(poly(coeffs, solution))< 1e-3);
     return 0;
 }
