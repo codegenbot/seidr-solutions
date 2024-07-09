087ef46ca439b71ca9c505e7034fca30fc08837d
beep boop <githubbot@vadim.me>
2024-07-09 08:19:25 +0200

    Solution 68 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/eey0tkjs Prompt: 4e3b1437-4c0d-4a52-9e97-e48c5a9cc05e.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 4e3b1437-4c0d-4a52-9e97-e48c5a9cc05e.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 4e3b1437-4c0d-4a52-9e97-e48c5a9cc05e.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 4e3b1437-4c0d-4a52-9e97-e48c5a9cc05e.cpp:2:6: error: ‘string’ in namespace ‘std’  does not name a type     2 | std::string middleCharacter(std::string s) {       |      ^~~~~~ 4e3b1437-4c0d-4a52-9e97-e48c5a9cc05e.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | ```

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,11 +1,10 @@
-```cpp
 #include <string>
 
 std::string middleCharacter(std::string s) {
     int len = s.length();
     if(len % 2 == 1) {
-        return s.substr(len/2, 1);
+        return std::string(1, s.substr(len/2, 1)[0]);
     } else {
-        return s.substr(len/2 - 1, 2);
+        return s.substr((len-1)/2, 2);
     }
 }
