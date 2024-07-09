9e66c5a6772dc0423fd12ac924ad5c35c4ca3399
beep boop <githubbot@vadim.me>
2024-07-09 17:50:19 +0200

    Solution 54 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/xqhc4ygj Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -1,24 +1,14 @@
-#include <cassert>
-#include <iostream>
-#include <cctype>
-
+```cpp
 int prod_signs(std::string str1, std::string str2, std::string str3) {
     for (auto& s : {str1, str2, str3}) {
-        if (!s.empty()) {
-            size_t i = 0;
-            while (i < s.size()) {
-                if (!(std::isalpha(s[i]) || s[i] == ' ')) {
-                    break;
-                }
-                ++i;
-            }
-            if (i > 0) {
-                s.erase(0, i);
-            }
-        }
+        auto upper = [&s](char c){ return std::toupper(c); };
+        s.erase(std::remove_if(s.begin(), s.end(), [](char c){ return !std::isalnum(c); }), s.end());
+        s = std::string(std::transform(s.begin(), s.end(), s.begin(), upper).base(), s.size());
     }
-
-    return (str1.empty() || str2.empty() || str3.empty()) ? 0 : (str1[0] == '-' || str2[0] == '-' || str3[0] == '-') ? -1 : 1;
+    
+    if (str1.empty() || str2.empty() || str3.empty()) return 0;
+    if (str1[0] == '-' || str2[0] == '-' || str3[0] == '-') return -1;
+    return 1;
 }
 
 int main() {
