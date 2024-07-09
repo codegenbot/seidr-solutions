4a147879fbbe87be4cc702773fcaac6f592fb4a8
beep boop <githubbot@vadim.me>
2024-07-09 13:04:31 +0200

    Solution 56 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/bpmy4d9k Prompt:    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 9eab8b4d-58dc-4302-988d-db4fb9fd653c.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 9eab8b4d-58dc-4302-988d-db4fb9fd653c.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 9eab8b4d-58dc-4302-988d-db4fb9fd653c.cpp:9: 9eab8b4d-58dc-4302-988d-db4fb9fd653c.cpp: In function ‘int main()’: 9eab8b4d-58dc-4302-988d-db4fb9fd653c.cpp:19:20: error: ‘filter_by_substring’ was  not declared in this scope    19 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 9eab8b4d-58dc-4302-988d-db4fb9fd653c.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -2,7 +2,7 @@ bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-vector<string> filter_by_substring(vector<string> strings, string substring) {
+vector<string> filter_by_substring(const vector<string> &strings, const string &substring) {
     vector<string> filtered_strings;
     for (const string &str : strings) {
         if (str.find(substring) != string::npos) {
@@ -13,8 +13,8 @@ vector<string> filter_by_substring(vector<string> strings, string substring) {
 }
 
 int main() {
-    vector<string> strings = {"apple", "banana", "grape", "orange"};
-    string substring = "an";
+    vector<string> strings = {"apple", "banana", "grape", "orange", "pear"};
+    string substring = "ra";
     vector<string> result = filter_by_substring(strings, substring);
     for (const string &str : result) {
         cout << str << " ";
