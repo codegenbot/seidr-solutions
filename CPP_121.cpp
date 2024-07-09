bfd1243f1801ba2b3657cd4fca005e927123de39
beep boop <githubbot@vadim.me>
2024-07-09 10:15:20 +0200

    Solution 27 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/awel56y5 Prompt: w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const int*; _Tp = int; _Alloc = std:: allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = i nt; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::allocator_type = std ::allocator<int>]’ 4efe1b9d-740c-42eb-854f-359022b43834.cpp:25:40:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -2,25 +2,29 @@
 #include <iostream>
 #include <vector>
 
-int sumOdd(std::vector<int> lst) {
+int sumOdd(std::vector<std::string> lst) {
     int sum = 0;
-    for (int i = 1; i < lst.size(); i++) {
-        if (lst[i] % 2 != 0) {
-            sum += lst[i];
+    for (std::string i : lst) {
+        int num = std::stoi(i);
+        if (num % 2 != 0) {
+            sum += num;
         }
     }
     return sum;
 }
 
-int solutions(std::vector<int> lst) { 
+int solutions(std::vector<std::string> lst) { 
     int sum = sumOdd(lst);
-    for (int i : lst) {
-        if (i % 2 == 0 && i > 0)
-            sum -= i; 
+    for (std::string i : lst) {
+        int num = std::stoi(i);
+        if (num % 2 == 0 && num > 0)
+            sum -= num; 
     }
     return sum;
 }
 
 int main() {
-    std::cout << solutions({3, 13, 2, 9}) << std::endl;
+    std::vector<std::string> lst = {"3", "13", "2", "9"};
+    assert(solutions(lst) == 25);
+    std::cout << "Test passed.\n";
 }
