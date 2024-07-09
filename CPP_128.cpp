b282e2b34caa03f07a4c6173ab152fb7573d9f22
beep boop <githubbot@vadim.me>
2024-07-09 13:37:06 +0200

    Solution 45 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fmifcpu9 Prompt: 24133d6f-066e-4a42-844a-b3fad8abe3f7.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int prod_signs(vector<int> arr) {       |                ^~~~~~ 24133d6f-066e-4a42-844a-b3fad8abe3f7.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int prod_signs(vector<int> arr) {       |                       ^~~ In file included from 24133d6f-066e-4a42-844a-b3fad8abe3f7.cpp:16: 24133d6f-066e-4a42-844a-b3fad8abe3f7.cpp: In function ‘int main()’: 24133d6f-066e-4a42-844a-b3fad8abe3f7.cpp:18:23: error: ‘prod_signs’ cannot be us ed as a function    18 |     assert (prod_signs({-1, 1, 1, 0}) == 0);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -6,7 +6,7 @@ int prod_signs(vector<int> arr) {
 
     for (int num : arr) {
         if (num == 0) {
-            return -32768; 
+            return -32768; // 0 in the middle of numbers
         }
         sign_product *= ((num > 0) ? 1 : ((num < 0) ? -1 : 0));
         sum += abs(num);
