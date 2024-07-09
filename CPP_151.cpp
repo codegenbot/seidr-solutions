4a09d96ad51866306bf3b111692748f9a3cddc66
beep boop <githubbot@vadim.me>
2024-07-09 17:43:51 +0200

    Solution 8 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nvcrge59 Prompt: r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <vector>   +++ |+#include <cassert>     4 | 11c94cc3-dcf5-4468-b41a-7bb051d415dc.cpp: At global scope: 11c94cc3-dcf5-4468-b41a-7bb051d415dc.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ 11c94cc3-dcf5-4468-b41a-7bb051d415dc.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 11c94cc3-dcf5-4468-b41a-7bb051d415dc.cpp:40: 11c94cc3-dcf5-4468-b41a-7bb051d415dc.cpp: In function ‘int main()’: 11c94cc3-dcf5-4468-b41a-7bb051d415dc.cpp:42:35: error: ‘lst’ was not declared in  this scope    42 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 11c94cc3-dcf5-4468-b41a-7bb051d415dc.cpp:42:43: error: ‘odd_sum’ was not declare d in this scope    42 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <iostream>
 #include <cmath>
 #include <vector>
@@ -15,24 +16,22 @@ long long double_the_difference(std::vector<float> lst) {
     return sum;
 }
 
-int main() {
-    std::vector<float> lst;
-    float num;
+long long odd_sum = 0;
+std::vector<float> lst;
+float num;
 
-    while (std::cin >> num) {
-        lst.push_back(num);
-    }
+while (std::cin >> num) {
+    lst.push_back(num);
+}
 
-    long long odd_sum = 0;
-    for (float num : lst) {
-        if (num > 0 && floor(num) == num) { 
-            if (fmod(num, 2.0) != 0.0) { 
-                odd_sum += pow(num, 2);
-            }
+for (float num : lst) {
+    if (num > 0 && floor(num) == num) { 
+        if (fmod(num, 2.0) != 0.0) { 
+            odd_sum += pow(num, 2);
         }
     }
-
-    assert(double_the_difference(lst) == odd_sum);
-
-    return 0;
 }
+
+assert(double_the_difference(lst) == odd_sum);
+
+return 0;
