c59734780c39735b7959610868f4cfae148c9c56
beep boop <githubbot@vadim.me>
2024-07-09 20:15:00 +0200

    Solution 38 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/iewm74bm Prompt: bf4496a6-b257-4446-bfc8-9a136ba11017.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ bf4496a6-b257-4446-bfc8-9a136ba11017.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ bf4496a6-b257-4446-bfc8-9a136ba11017.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ bf4496a6-b257-4446-bfc8-9a136ba11017.cpp:2:10: fatal error: boost/any.hpp: No su ch file or directory     2 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,35 +1,36 @@
 #include <boost/any.hpp>
 #include <string>
-#include <iostream>
+#include <cmath>
 
+using namespace boost;
 using namespace std;
 
 any compareOne(any a, any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
         int x = boost::any_cast<int>(a);
         int y = boost::any_cast<int>(b);
-        return (x > y) ? a : ((y > x) ? b :typeid(void));
-    }
+        return (x > y ? a : (y > x ? b : typeid(void)));
+    } 
     else if (a.type() == typeid(float) && b.type() == typeid(float)) {
         float x = boost::any_cast<float>(a);
         float y = boost::any_cast<float>(b);
-        return (x > y) ? a : ((y > x) ? b :typeid(void));
-    }
+        return (x > y ? a : (y > x ? b : typeid(void)));
+    } 
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
         string x = boost::any_cast<string>(a);
         string y = boost::any_cast<string>(b);
-        return (x > y) ? a : ((y > x) ? b :typeid(void));
-    }
+        return (x > y ? a : (y > x ? b : typeid(void)));
+    } 
     else if (a.type() == typeid(string) && (b.type() == typeid(int) || b.type() == typeid(float))) {
         string x = boost::any_cast<string>(a);
         double y = boost::any_cast<double>(b);
-        return (stod(x) > y) ? a : ((y > stod(x)) ? b :typeid(void));
-    }
+        return (stod(x) > y ? a : (y > stod(x) ? b : typeid(void)));
+    } 
     else if ((a.type() == typeid(int) || a.type() == typeid(float)) && b.type() == typeid(string)) {
         double x = boost::any_cast<double>(a);
         string y = boost::any_cast<string>(b);
-        return (x > stod(y)) ? a : ((stod(y) > x) ? b :typeid(void));
-    }
+        return (x > stod(y) ? a : (stod(y) > x ? b : typeid(void)));
+    } 
     else {
         return typeid(void);
     }
