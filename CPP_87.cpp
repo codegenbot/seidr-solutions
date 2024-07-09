b93a3be519153f96d53a733ffd54a0541223235a
beep boop <githubbot@vadim.me>
2024-07-09 18:54:23 +0200

    Solution 97 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/rl4jiyfu Prompt:       |                                   ^~~~~~ d800b076-b044-4546-a32b-4e15a65a1b3b.cpp:20:42: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ d800b076-b044-4546-a32b-4e15a65a1b3b.cpp:20:49: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ d800b076-b044-4546-a32b-4e15a65a1b3b.cpp:20:56: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from d800b076-b044-4546-a32b-4e15a65a1b3b.cpp:19: d800b076-b044-4546-a32b-4e15a65a1b3b.cpp: In function ‘int main()’: d800b076-b044-4546-a32b-4e15a65a1b3b.cpp:32:20: error: ‘get_row’ was not declare d in this scope    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ d800b076-b044-4546-a32b-4e15a65a1b3b.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -2,6 +2,10 @@
 
 bool issame(vector<vector<int>> a, vector<vector<int>> b);
 
+bool issame(vector<vector<int>> a, vector<vector<int>> b){
+    return a == b;
+}
+
 vector<vector<int>> get_row(vector<vector<int>> lst, int x){
     vector<vector<int>> result;
     for (int i = 0; i < lst.size(); ++i) {
@@ -18,8 +22,4 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x){
         return a[0] < b[0];
     });
     return result;
-}
-
-bool issame(vector<vector<int>> a, vector<vector<int>> b){
-    return a == b;
 }
