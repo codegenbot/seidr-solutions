e661491e3f74815c2fafc1ee54963dec638f40c7
beep boop <githubbot@vadim.me>
2024-07-10 00:26:35 +0200

    Solution 2 of CPP/138, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/oz4zcux4 Prompt: Complete the following code given the task description and function signature.

--- CPP_138.cpp
+++ CPP_138.cpp
@@ -2,9 +2,9 @@
 
 bool is_equal_to_sum_even(int n) {
     for (int i = 2; i <= n; ++i) {
-        if (n == 0) return true;
+        if (n == i) continue;
         int sum = 0;
-        for (int j = 2; j <= i && sum + 2j <= n; ++j) {
+        for (int j = 2; j <= i && sum + 2*j <= n; ++j) {
             sum += 2 * j;
             if (sum == n) return true;
         }
