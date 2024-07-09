88f88664c45a67bfca9394ee05413886af482c97
beep boop <githubbot@vadim.me>
2024-07-09 09:49:12 +0200

    Solution 62 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/vgnxrdbb Prompt:       |                            ^ 977f176f-4fe1-42cf-92d5-9ea7d666e236.cpp:21:30: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ 977f176f-4fe1-42cf-92d5-9ea7d666e236.cpp:21:37: error: ‘string’ was not declared  in this scope    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 977f176f-4fe1-42cf-92d5-9ea7d666e236.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 977f176f-4fe1-42cf-92d5-9ea7d666e236.cpp:20: 977f176f-4fe1-42cf-92d5-9ea7d666e236.cpp: In function ‘int main()’: 977f176f-4fe1-42cf-92d5-9ea7d666e236.cpp:30:20: error: ‘total_match’ was not dec lared in this scope    30 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 977f176f-4fe1-42cf-92d5-9ea7d666e236.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -2,10 +2,10 @@
 #include <string>
 
 bool issame(const vector<string>& a, const vector<string>& b) {
-    return a == b;
+   return a == b;
 }
 
-int totalChars(const vector<string>& lst) {
+int totalChars(vector<string> lst) {
     int total = 0;
     for (const string& str : lst) {
         total += str.size();
@@ -13,9 +13,11 @@ int totalChars(const vector<string>& lst) {
     return total;
 }
 
-vector<string> total_match(const vector<string>& lst1, const vector<string>& lst2) {
+vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     if (totalChars(lst1) < totalChars(lst2)) {
         return lst1;
     }
     return lst2;
 }
+
+assert(issame(total_match({"this"}, {}), {}));
