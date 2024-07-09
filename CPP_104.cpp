d983f6f55a4ab126fe077aedd79475a464adc058
beep boop <githubbot@vadim.me>
2024-07-09 22:04:37 +0200

    Solution 42 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gc3g1rrt Prompt: In file included from /usr/include/c++/11/vector:67,                  from 1b0b6920-eb21-4ed0-aed1-4a7d4beba717.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1b0b6920-eb21-4ed0-aed1-4a7d4beba717.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1b0b6920-eb21-4ed0-aed1-4a7d4beba717.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1b0b6920-eb21-4ed0-aed1-4a7d4beba717.cpp:30:39: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1b0b6920-eb21-4ed0-aed1-4a7d4beba717.cpp:29: 1b0b6920-eb21-4ed0-aed1-4a7d4beba717.cpp: In function ‘int main()’: 1b0b6920-eb21-4ed0-aed1-4a7d4beba717.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -6,49 +6,13 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 }
 
 int main() {
-    std::vector<int> x;
-    // Read input from user
-    int n;
-    std::cin >> n;
-    for (int i = 0; i < n; i++) {
-        int num;
-        std::cin >> num;
-        x.push_back(num);
+    std::vector<int> x = {1, 2, 3};
+    std::vector<int> y = {1, 2, 4};
+    if (issame(x, y)) {
+        std::cout << "Vectors are same." << std::endl;
+    } else {
+        std::cout << "Vectors are not same." << std::endl;
     }
-
-    std::vector<int> result = unique_digits(x);
-
-    // Print the output
-    bool issameToResult = true;
-    for (int num : x) {
-        if (std::find(result.begin(), result.end(), num) == result.end()) {
-            issameToResult = false;
-            break;
-        }
-    }
-
-    std::cout << issameToResult;
-
+    
     return 0;
-}
-
-std::vector<int> unique_digits(std::vector<int> x) {
-    std::vector<int> result;
-    for (int num : x) {
-        bool hasEvenDigit = false;
-        int temp = num;
-        while (temp > 0) {
-            int digit = temp % 10;
-            if (digit % 2 == 0) {
-                hasEvenDigit = true;
-                break;
-            }
-            temp /= 10;
-        }
-        if (!hasEvenDigit) {
-            result.push_back(num);
-        }
-    }
-    std::sort(result.begin(), result.end());
-    return result;
 }
