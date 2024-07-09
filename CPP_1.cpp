4d00cfc7118aec2ae92782486127764f6dc0264f
beep boop <githubbot@vadim.me>
2024-07-09 12:50:11 +0200

    Solution 72 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/s92p6few Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:26: 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp: In function ‘int main()’: 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,5 +1,7 @@
-bool issame(char a, char b) {
-    return a == b;
+bool issame(string s1, string s2) {
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+    return s1 == s2;
 }
 
 vector<string> separate_paren_groups(string paren_string);
@@ -7,10 +9,13 @@ vector<string> separate_paren_groups(string paren_string);
 int main() {
     string input;
     cin >> input;
+
     vector<string> groups = separate_paren_groups(input);
+
     for (const string& group : groups) {
         cout << group << endl;
     }
+
     return 0;
 }
 
