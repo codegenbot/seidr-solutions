10109f217fa0a626c84cd1f97b19c5292c365819
beep boop <githubbot@vadim.me>
2024-07-09 02:16:51 +0200

    Solution 7 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/8c35lqok Prompt:                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from a73b8930-bf28-44ed-9ba5-1707511a6779.cpp:1: /usr/include/stdio.h:358:12: note: in a call to function ‘int sprintf(char*, con st char*, ...)’ declared ‘nonnull’   358 | extern int sprintf (char *__restrict __s,       |            ^~~~~~~ a73b8930-bf28-44ed-9ba5-1707511a6779.cpp:19:26: warning: null destination pointe r [-Wformat-overflow=]    19 |         result += sprintf(0, "%02x", md5[i]);       |                   ~~~~~~~^~~~~~~~~~~~~~~~~~~ /usr/bin/ld: /tmp/cc3x3Pb5.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> >)': a73b8930-bf28-44ed-9ba5-1707511a6779.cpp:(.text+0x8e): undefined reference to `M D5_Init' /usr/bin/ld: a73b8930-bf28-44ed-9ba5-1707511a6779.cpp:(.text+0xcf): undefined re ference to `MD5_Update' /usr/bin/ld: a73b8930-bf28-44ed-9ba5-1707511a6779.cpp:(.text+0xe5): undefined re ference to `MD5_Final' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <string>
 #include <openssl/md5.h>
 
 using namespace std;
@@ -17,4 +19,13 @@ string string_to_md5(string text) {
         result += sprintf(0, "%02x", md5[i]);
     }
     return result;
+}
+
+int main() {
+    // Link against the OpenSSL library
+    cout << "Enter a string: ";
+    string input;
+    getline(cin, input);
+    cout << "MD5 hash of '" << input << "' is " << string_to_md5(input) << endl;
+    return 0;
 }
