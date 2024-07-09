b5b40f07ef4799926fa448252f9df746bced2e8a
beep boop <githubbot@vadim.me>
2024-07-09 22:54:17 +0200

    Solution 26 of dice-game, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/n8hczhjt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d7d72d01-bf03-4d3a-a710-36b15b8f3 24c.py", line 3     print(round(sum([(n-i)/n for i in range(1, n+1) for j in range(1, m+1) if i > j]) / (n*m), 2)          ^ SyntaxError: '(' was never closed

--- dice-game.py
+++ dice-game.py
@@ -1,3 +1,9 @@
 n = int(input())
 m = int(input())
-print(round(sum([(n-i)/n for i in range(1, n+1) for j in range(1, m+1) if i > j]) / (n*m), 2)
+print(
+    round(
+        sum([(n - i) / n for i in range(1, n + 1) for j in range(1, m + 1) if i > j])
+        / (n * m),
+        2,
+    )
+)
