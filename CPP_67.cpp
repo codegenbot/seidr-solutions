d5a88996232719c97b5b36a88a28b4fcc083834f
beep boop <githubbot@vadim.me>
2024-07-09 21:13:45 +0200

    Solution 1 of CPP/67, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/a11wyyiw Prompt: Complete the following code given the task description and function signature.

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,30 +1,30 @@
-#include<stdio.h>
-#include<string>
+#include <stdio.h>
+#include <string>
 using namespace std;
 
 int fruit_distribution(string s, int n) {
     size_t pos = 0;
-    string str = s + " ";
-    int apples = 0, oranges = 0;
+    string numStr;
+    int totalApples = 0, totalOranges = 0;
     
-    while ((pos = str.find("apples", pos)) != string::npos) {
-        size_t nextPos = str.find(" ", pos);
-        if (nextPos == string::npos) {
-            nextPos = str.length();
+    while ((pos = s.find(" apples")) != string::npos || (pos = s.find(" oranges")) != string::npos) {
+        if (pos > 0) {
+            size_t start = 0;
+            if (s.substr(0, pos).find("apples") == string::npos)
+                start = pos;
+            else
+                start = s.find("apples");
+            numStr = s.substr(start, pos - start);
+            if (!numStr.empty()) {
+                int val = stoi(numStr);
+                if (s.substr(0, pos).find("apples") != string::npos)
+                    totalApples += val;
+                else
+                    totalOranges += val;
+            }
         }
-        string appleStr = str.substr(pos, nextPos - pos);
-        apples = stoi(appleStr.replace("apples and", "").replace("apples", ""));
-        break;
+        s.erase(0, pos + 6);
     }
     
-    while ((pos = str.find("oranges", pos)) != string::npos) {
-        size_t nextPos = str.find(" ", pos);
-        if (nextPos == string::npos) {
-            nextPos = str.length();
-        }
-        string orangeStr = str.substr(pos, nextPos - pos);
-        oranges = stoi(orangeStr.replace("oranges and", "").replace("oranges", ""));
-    }
-    
-    return n - apples - oranges;
+    return n - totalApples - totalOranges;
 }
