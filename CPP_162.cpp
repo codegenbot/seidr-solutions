02da6db0167b6ec3a0007e6c848ab11904202e0f
beep boop <githubbot@vadim.me>
2024-07-09 13:56:20 +0200

    Solution 45 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5tgpuzn0 Prompt: 69595688-f6d2-4069-8429-821cad0508db.cpp:13:14: warning: ‘int MD5_Final(unsigned  char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]    13 |     MD5_Final(md, &ctx);       |     ~~~~~~~~~^~~~~~~~~~ In file included from 69595688-f6d2-4069-8429-821cad0508db.cpp:2: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ 69595688-f6d2-4069-8429-821cad0508db.cpp:17:24: error: cannot convert ‘std::__cx x11::basic_string<char>’ to ‘char*’    17 |         sprintf(result + "%02x", md[i]);       |                 ~~~~~~~^~~~~~~~       |                        |       |                        std::__cxx11::basic_string<char> In file included from /usr/include/c++/11/cstdio:42,                  from /usr/include/c++/11/ext/string_conversions.h:43,                  from /usr/include/c++/11/bits/basic_string.h:6608,                  from /usr/include/c++/11/string:55,                  from 69595688-f6d2-4069-8429-821cad0508db.cpp:1: /usr/include/stdio.h:358:38: note:   initializing argument 1 of ‘int sprintf(cha r*, const char*, ...)’   358 | extern int sprintf (char *__restrict __s,       |                     ~~~~~~~~~~~~~~~~~^~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,5 +1,3 @@
-string result;
-for (int i = 0; i < 16; ++i) {
-    result.push_back(static_cast<char>((md[i] >> 4) & 0x0f));
-    result.push_back(static_cast<char>(md[i] & 0x0f));
-}
+```cpp
+result.push_back(static_cast<char>((md[i] >> 4) & 0x0f));
+result.push_back(static_cast<char>(md[i] & 0x0f));
