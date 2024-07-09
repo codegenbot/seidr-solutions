d3fb6f6a12c2e3d89a6f8db9f85d1f1894913ef1
beep boop <githubbot@vadim.me>
2024-07-09 07:13:45 +0200

    Solution 2 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jicx7wv6 Prompt: 2f600456-3cd1-4a1c-95ab-bad6097e2a34.cpp:24:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    24 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 2f600456-3cd1-4a1c-95ab-bad6097e2a34.cpp:27:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    27 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 2f600456-3cd1-4a1c-95ab-bad6097e2a34.cpp:27:36: error: ‘find_zero’ cannot be use d as a function    27 |         solution = find_zero(coeffs);       |                                    ^ In file included from 2f600456-3cd1-4a1c-95ab-bad6097e2a34.cpp:11: 2f600456-3cd1-4a1c-95ab-bad6097e2a34.cpp:28:21: error: ‘poly’ was not declared i n this scope    28 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 2f600456-3cd1-4a1c-95ab-bad6097e2a34.cpp:28:17: error: ‘abs’ was not declared in  this scope    28 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,10 +1,27 @@
+````
+#include <vector>
+using namespace std;
+
+// Function to calculate polynomial
+double poly(vector<double> coeffs, double solution) {
+    double result = 0;
+    for (int i = 0; i < coeffs.size(); i++) {
+        result += coeffs[i] * pow(solution, i);
+    }
+    return result;
+}
+
+// Function to find zero
 double find_zero(vector<double> xs){
     double sum = 0;
-    double coeff = xs[0];
+    vector<double> coeffs; 
     for (int i = 1; i < xs.size(); i++) {
         if (i % 2 == 0) {
-            sum += xs[i] / coeff;
+            coeffs.push_back(xs[i] / xs[0]);
         }
     }
-    return -sum / coeff;
+    double solution = -sum / xs[0]; 
+    assert(abs(poly(coeffs, solution)) < 1e-3); 
+    return solution;
 }
+```
