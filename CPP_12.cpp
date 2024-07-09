59a7d54c33b9f289d1fa41fd043ce65dad8c97ee
beep boop <githubbot@vadim.me>
2024-07-09 20:32:17 +0200

    Solution 9 of CPP/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/qexuuz7h Prompt: 5262a3d8-710c-40b1-a3ab-676eaa6f50ef.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string longest(vector<string> strings){       | ^~~~~~ In file included from 5262a3d8-710c-40b1-a3ab-676eaa6f50ef.cpp:11: 5262a3d8-710c-40b1-a3ab-676eaa6f50ef.cpp: In function ‘int main()’: 5262a3d8-710c-40b1-a3ab-676eaa6f50ef.cpp:13:13: error: ‘longest’ was not declare d in this scope; did you mean ‘long’?    13 |     assert (longest({"x", "yyy", "zzzz", "www", "kkkk", "abc"}) == "zzzz ");       |             ^~~~~~~

--- CPP_12.cpp
+++ CPP_12.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
 
-string longest(vector<string> strings){
+std::string longest(std::vector<std::string> strings){
     if(strings.empty()) return "";
-    string result = strings[0];
-    for(string s : strings) {
+    std::string result = strings[0];
+    for(std::string s : strings) {
         if(s.length() > result.length())
             result = s;
     }
