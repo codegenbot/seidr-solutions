5e89d96adf3058ff2d6eca18757abca3f8109f63
beep boop <githubbot@vadim.me>
2024-07-09 17:30:18 +0200

    Solution 68 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9d9hp3es Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 74563bbd-0929-4f65-9c85-2ae48b8479c7.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,6 +1,6 @@
+```cpp
 #include <vector>
 #include <initializer_list>
-using namespace std;
 
 bool operator==(const vector<int>& a, initializer_list<int> b) {
     if (a.size() != distance(b.begin(), distance(b.end(), back_inserter(vector<int>(b))))) 
@@ -20,11 +20,4 @@ vector<int> make_a_pile(int n) {
     return pile;
 }
 
-vector<int> make_a_pile(int n) {
-    vector<int> pile = {0, 1};
-    for (int i = 2; i <= n; ++i) {
-        vector<int> temp = {pile.back() + i}; 
-        while (!temp.empty()) pile.insert(pile.begin(), temp.back());
-    }
-    return pile;
-}
+assert(make_a_pile(8) == vector<int>({0, 1, 3, 5, 8, 10, 12, 14, 16, 18, 20, 22}));
