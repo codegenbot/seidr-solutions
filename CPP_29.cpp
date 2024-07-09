f7e3ac8506c60fe05413365de391a669ab3444b0
beep boop <githubbot@vadim.me>
2024-07-09 03:54:50 +0200

    Solution 91 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/o5dcqdjt Prompt:    28 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 52e5503c-5755-4af1-ba87-3534b98ed10a.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 52e5503c-5755-4af1-ba87-3534b98ed10a.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from 52e5503c-5755-4af1-ba87-3534b98ed10a.cpp:27: 52e5503c-5755-4af1-ba87-3534b98ed10a.cpp: In function ‘int main()’: 52e5503c-5755-4af1-ba87-3534b98ed10a.cpp:37:20: error: ‘filter_by_prefix’ was no t declared in this scope    37 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 52e5503c-5755-4af1-ba87-3534b98ed10a.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,9 +1,3 @@
-```cpp
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-
 bool issame(vector<string> a, vector<string> b) {
     if(a.size() != b.size())
         return false;
@@ -22,33 +16,16 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
     }
     return result;
 
-}
-
-vector<string> split(const string& str, char delimiter) {
-    vector<string> tokens;
-    size_t prev = 0, next = 0;
-    do {
-        next = str.find(delimiter, prev);
-        if (next == string::npos) break;
-        tokens.push_back(str.substr(prev, next-prev));
-        prev = next + 1;
-    } while (next != string::npos);
-    tokens.push_back(str.substr(prev));
-    return tokens;
-}
-
 int main() {
     vector<string> strings;
     string input;
 
-    // Get list of strings from user
     cout << "Enter list of strings separated by spaces: ";
     getline(cin, input);
     for (auto& s : split(input, ' ')) {
         strings.push_back(s);
     }
 
-    // Get prefix from user
     string prefix;
     cout << "Enter prefix: ";
     cin >> prefix;
