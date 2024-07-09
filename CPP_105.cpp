754c4cddbce3c173b11c2b251a3089913001c0d1
beep boop <githubbot@vadim.me>
2024-07-09 10:19:30 +0200

    Solution 18 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/upzrl8t8 Prompt:       |                              ^~~~~~ c8c5a1f5-ce15-4a96-b8a9-34c3fefac413.cpp:19:37: error: ‘string’ was not declared  in this scope    19 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c8c5a1f5-ce15-4a96-b8a9-34c3fefac413.cpp:19:44: error: ‘b’ was not declared in t his scope    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c8c5a1f5-ce15-4a96-b8a9-34c3fefac413.cpp:19:45: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c8c5a1f5-ce15-4a96-b8a9-34c3fefac413.cpp:18: c8c5a1f5-ce15-4a96-b8a9-34c3fefac413.cpp: In function ‘int main()’: c8c5a1f5-ce15-4a96-b8a9-34c3fefac413.cpp:28:20: error: ‘by_length’ was not decla red in this scope    28 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ c8c5a1f5-ce15-4a96-b8a9-34c3fefac413.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -4,17 +4,17 @@
 #include <vector>
 #include <string>
 
-std::vector<std::string> by_length(std::vector<int> arr){
-    std::vector<std::string> names = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-    std::vector<int> sorted_filtered;
+vector<string> by_length(vector<int> arr){
+    vector<string> names = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    vector<int> sorted_filtered;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
             sorted_filtered.push_back(num);
         }
     }
-    std::sort(sorted_filtered.begin(), sorted_filtered.end());
-    std::reverse(sorted_filtered.begin(), sorted_filtered.end());
-    std::vector<std::string> result;
+    sort(sorted_filtered.begin(), sorted_filtered.end());
+    reverse(sorted_filtered.begin(), sorted_filtered.end());
+    vector<string> result;
     for (int num : sorted_filtered) {
         result.push_back(names[num - 1]);
     }
