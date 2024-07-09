ac2ccff61922f9fadadc5df11ddf00539bf80996
beep boop <githubbot@vadim.me>
2024-07-09 20:37:37 +0200

    Solution 87 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/cjggo54b Prompt:    58 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2a14012a-3f2b-4eb0-af2b-64e067c93e11.cpp:66:5: error: redefinition of ‘int main( )’    66 | int main(){       |     ^~~~ 2a14012a-3f2b-4eb0-af2b-64e067c93e11.cpp:50:5: note: ‘int main()’ previously def ined here    50 | int main() {       |     ^~~~ In file included from 2a14012a-3f2b-4eb0-af2b-64e067c93e11.cpp:57: 2a14012a-3f2b-4eb0-af2b-64e067c93e11.cpp: In function ‘int main()’: 2a14012a-3f2b-4eb0-af2b-64e067c93e11.cpp:67:20: error: ‘select_words’ was not de clared in this scope    67 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 2a14012a-3f2b-4eb0-af2b-64e067c93e11.cpp:67:19: error: ‘issame’ cannot be used a s a function    67 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -9,7 +9,9 @@ using std::size_t;
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size()) return false;
     for(int i = 0; i<a.size(); i++){
-        if(a[i] != b[i]) return false;
+        auto p1 = a[i];
+        auto p2 = b[i];
+        if(p1 != p2) return false;
     }
     return true;
 }
