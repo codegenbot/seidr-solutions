4b89aacd3e8546328e9a320011874b575ffc8797
beep boop <githubbot@vadim.me>
2024-07-09 03:00:03 +0200

    Solution 17 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zffmqvx4 Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a3eb3e15-00e1-4155-a2a6-513335c6659a.cpp:16:36: error: expected primary-expressi on before ‘float’    16 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ a3eb3e15-00e1-4155-a2a6-513335c6659a.cpp:16:43: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from a3eb3e15-00e1-4155-a2a6-513335c6659a.cpp:15: a3eb3e15-00e1-4155-a2a6-513335c6659a.cpp: In function ‘int main()’: a3eb3e15-00e1-4155-a2a6-513335c6659a.cpp:25:20: error: ‘rescale_to_unit’ was not  declared in this scope    25 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ a3eb3e15-00e1-4155-a2a6-513335c6659a.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,6 +1,3 @@
-#include <algorithm>
-#include <vector>
-
 bool areSame(const vector<float>& a, const vector<float>& b) {
     if (a.size() != b.size()) {
         return false;
@@ -11,16 +8,4 @@ bool areSame(const vector<float>& a, const vector<float>& b) {
         }
     }
     return true;
-}
-
-vector<float> rescale_to_unit(vector<float> numbers) {
-    float min_val = *min_element(numbers.begin(), numbers.end());
-    float max_val = *max_element(numbers.begin(), numbers.end());
-
-    vector<float> result;
-    for (float num : numbers) {
-        result.push_back((num - min_val) / (max_val - min_val));
-    }
-
-    return result;
 }
