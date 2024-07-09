4ac08f036235be9446d77325def7239aede6c133
beep boop <githubbot@vadim.me>
2024-07-09 15:09:17 +0200

    Solution 86 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ey4hkvop Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/85527afe-a586-4dfb-9da2-a22e6a6f6 59b.py", line 7, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/85527afe-a586-4dfb-9da2-a22e6a6f6 59b.py", line 4, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,13 +1,12 @@
-```
 def modp(n: int, p: int):
     def extended_gcd(a, b):
         if a == 0:
             return b, 0, 1
         else:
             gcd, y, x = extended_gcd(b % a, a)
-            return gcd, x - (b // a) * y, y
+            return gcd, y - (b // a) * x, x
 
     gcd, x, _ = extended_gcd(n, p)
     if gcd != 1:
         raise ValueError("n and p are not coprime")
-    return pow(n, p-2, p) * x % p
+    return pow(n, p - 2, p) * x % p
