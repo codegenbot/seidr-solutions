329577db79072819de6d17b1fc89e5cde5509c3f
beep boop <githubbot@vadim.me>
2024-07-09 13:12:38 +0200

    Solution 93 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/p70nshd0 Prompt:    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:16: f8e2b31f-ea39-4798-9646-bf202464bab2.cpp: In function ‘int main()’: f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:26:20: error: ‘sorted_list_sum’ was not  declared in this scope    26 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,14 +1,21 @@
 vector<string> sorted_list_sum(vector<string> lst) {
-    auto is_same = [](const string& a, const string& b) -> bool {
-        return (a.length() % 2 && !b.length() % 2) || (!a.length() % 2 && b.length() % 2);
-    };
+    auto isEven = [](const string& a) {return a.length() % 2 == 0;};
+
     vector<string> result;
-    for (auto i = lst.begin(); i != lst.end(); ++i) {
-        auto it = unique(i, i+1, is_same);
-        vector<string> temp(i, it);
-        sort(temp.begin(), temp.end());
-        if (!result.size() || temp[0].compare(result[0]) < 0)
-            result.insert(result.end(), temp.begin(), temp.end());
+    for (string str : lst) {
+        if (isEven(str)) {
+            if (result.empty()) {
+                result.push_back(str);
+            } else if (!isEven(result.back())) {
+                result.clear();
+                result.push_back(str);
+            }
+        } else {
+            if (!isEven(result.back())) {
+                result.push_back(str);
+            }
+        }
     }
+
     return result;
 }
