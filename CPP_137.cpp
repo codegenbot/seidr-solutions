f6cf04510e754425af270ec799b38d61216ca951
beep boop <githubbot@vadim.me>
2024-07-09 04:29:55 +0200

    Solution 15 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/chasion3 Prompt: 507a5418-9790-477d-a032-bc7abfe2b4a1.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,18 +1,18 @@
 #include <boost/variant.hpp>
 
-boost::variant<int, float, std::string> compare_one(boost::variant<boost::variant<int, float, std::string>, boost::variant<int, float, std::string>> a, boost::variant<int, float, std::string> b) {
-    if (a.which() == 0 && b.which() == 1) { // int and float
-        return boost::get<float>(b) > boost::get<int>(a) ? b : a;
-    } else if (a.which() == 1 && b.which() == 0) { // float and int
-        return boost::get<float>(a) > boost::get<int>(b) ? a : b;
-    } else if (a.which() == 2 && b.which() == 2) { // string and string
-        std::string strA = boost::get<std::string>(a);
-        std::string strB = boost::get<std::string>(b);
-        return stof(strB) > stof(strA) ? b : a;
-    } else if (a.which() == 2 && b.which() == 1) { // string and float
-        return boost::get<float>(b) > stof(boost::get<std::string>(a)) ? b : a;
-    } else if (a.which() == 1 && b.which() == 2) { // float and string
-        return stof(boost::get<std::string>(b)) > boost::get<float>(a) ? b : a;
+boost::variant<int, float> compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return boost::get<float>(b) > boost::get<int>(a) ? boost::move(b) : boost::move(a);
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return boost::get<float>(a) > boost::get<int>(b) ? boost::move(a) : boost::move(b);
+    } else if (a.type() == typeid(boost::variant<std::string, std::string>) && b.type() == typeid(boost::variant<std::string, std::string>)) {
+        boost::variant<std::string, std::string> varA = boost::get<boost::variant<std::string, std::string>>(a);
+        boost::variant<std::string, std::string> varB = boost::get<boost::variant<std::string, std::string>>(b);
+        return boost::get<float>(boost::get<std::string>(varB)) > boost::get<int>(boost::get<std::string>(varA)) ? b : a;
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        return boost::get<float>(b) > stof(boost::any_cast<string>(a)) ? boost::move(b) : boost::move(a);
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        return stof(boost::any_cast<string>(b)) > boost::get<float>(a) ? boost::move(b) : boost::move(a);
     } else {
         return "None";
     }
