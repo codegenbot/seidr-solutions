b589a933aea31dac99146b649aad36388ad430fa
beep boop <githubbot@vadim.me>
2024-07-09 23:31:27 +0200

    Solution 81 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/zt4qvmez Prompt: In file included from 874cac42-c15e-4e4d-8626-75a27d3f8050.cpp:6: 874cac42-c15e-4e4d-8626-75a27d3f8050.cpp:10:11: error: expected unqualified-id b efore numeric constant    10 | const int MD5_DIGEST_LENGTH = 16;       |           ^~~~~~~~~~~~~~~~~ 874cac42-c15e-4e4d-8626-75a27d3f8050.cpp: In function ‘std::string string_to_md5 (std::string)’: 874cac42-c15e-4e4d-8626-75a27d3f8050.cpp:15:16: error: aggregate ‘EVP_MD_CTX md’  has incomplete type and cannot be defined    15 |     EVP_MD_CTX md;       |                ^~ 874cac42-c15e-4e4d-8626-75a27d3f8050.cpp:21:5: error: ‘EVP_DigestFinal_CTX’ was not declared in this scope; did you mean ‘EVP_DigestFinal_ex’?    21 |     EVP_DigestFinal_CTX(&md, digest, NULL);       |     ^~~~~~~~~~~~~~~~~~~       |     EVP_DigestFinal_ex

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -13,12 +13,13 @@ string string_to_md5(string text) {
     if (text.empty()) return "None";
 
     const EVP_MD_CTX *md;
-    EVP_MD_CTX_init(&md);
+    md = EVP_MD_CTX_new();
     unsigned char result[MD5_DIGEST_LENGTH];
 
-    EVP_DigestUpdate(&md, (unsigned char*)text.c_str(), text.size());
+    EVP_DigestInit_ex(md, EVP_md5(), NULL);
+    EVP_DigestUpdate(md, (unsigned char*)text.c_str(), text.size());
     unsigned char *digest = (unsigned char *)OPENSSL_malloc(MD5_DIGEST_LENGTH);
-    EVP_DigestFinal_ex(&md, digest, NULL);
+    EVP_DigestFinal_ex(md, digest, NULL);
 
     stringstream ss;
     for(int i = 0; i < MD5_DIGEST_LENGTH; i++) {
@@ -27,5 +28,6 @@ string string_to_md5(string text) {
 
     OPENSSL_free(digest); 
 
+    EVP_MD_CTX_free(md);
     return ss.str();
 }
