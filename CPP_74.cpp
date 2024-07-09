72fa02cdba998bb198e4047e86d5d9516572bed3
beep boop <githubbot@vadim.me>
2024-07-09 17:35:08 +0200

    Solution 96 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ok8wkyae Prompt:       |                              ^~~~~~ c27547ad-9538-4fb8-b17b-04c1cbf911ab.cpp:16:37: error: ‘string’ was not declared  in this scope    16 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c27547ad-9538-4fb8-b17b-04c1cbf911ab.cpp:16:44: error: ‘b’ was not declared in t his scope    16 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c27547ad-9538-4fb8-b17b-04c1cbf911ab.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c27547ad-9538-4fb8-b17b-04c1cbf911ab.cpp:15: c27547ad-9538-4fb8-b17b-04c1cbf911ab.cpp: In function ‘int main()’: c27547ad-9538-4fb8-b17b-04c1cbf911ab.cpp:25:20: error: ‘total_match’ was not dec lared in this scope    25 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ c27547ad-9538-4fb8-b17b-04c1cbf911ab.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,20 +1,13 @@
 #include <string>
-#include <vector>
 
-using namespace std;
-
-vector<string> issame(vector<string> lst1, vector<string> lst2) {
+bool issame(vector<string> a, vector<string> b) {
     int totalChars1 = 0, totalChars2 = 0;
-    for (const string& str : lst1) {
+    for (const string& str : a) {
         totalChars1 += str.size();
     }
-    for (const string& str : lst2) {
+    for (const string& str : b) {
         totalChars2 += str.size();
     }
     
-    if (totalChars1 < totalChars2) {
-        return lst1;
-    } else {
-        return lst2;
-    }
+    return totalChars1 == totalChars2;
 }
