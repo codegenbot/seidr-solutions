504464a011531e581a5eb4b8f74e837731f42a36
beep boop <githubbot@vadim.me>
2024-07-09 18:16:40 +0200

    Solution 19 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lcw7gzkh Prompt: 9d891490-0e52-46b9-ab4d-2ab3cbc3921f.cpp:28:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    28 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 9d891490-0e52-46b9-ab4d-2ab3cbc3921f.cpp:31:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    31 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 9d891490-0e52-46b9-ab4d-2ab3cbc3921f.cpp:31:36: error: ‘find_zero’ cannot be use d as a function    31 |         solution = find_zero(coeffs);       |                                    ^ In file included from 9d891490-0e52-46b9-ab4d-2ab3cbc3921f.cpp:15: 9d891490-0e52-46b9-ab4d-2ab3cbc3921f.cpp:32:25: error: ‘poly’ cannot be used as a function    32 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ~~~~^~~~~~~~~~~~~~~~~~ 9d891490-0e52-46b9-ab4d-2ab3cbc3921f.cpp:32:17: error: ‘abs’ was not declared in  this scope    32 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,19 +1,20 @@
 using namespace std;
 
-vector<double> coeffs;
-double poly(vector<double> v, double x) {
-    double result = 0;
-    for (int i = 0; i < v.size(); i++) {
-        result += v[i] * pow(x, i);
+std::vector<double> coeffs;
+double poly(std::vector<double> coeff, double x) {
+    double sum = 0;
+    for (int i = 0; i < coeff.size(); i++) {
+        sum += coeff[i] * pow(x, i);
     }
-    return result;
+    return sum;
 }
 
-double find_zero(vector<double> xs) {
+double find_zero(std::vector<double> xs) {
     double a = xs[0];
     double b = 0;
+    coeffs = std::vector<double>();
     for (int i = 1; i < xs.size(); i += 2) {
-        b -= xs[i] / pow(xs[i-1], i/2.0);
+        coeffs.push_back(-xs[i] / pow(xs[i-1], i/2.0));
     }
     return -b / a;
 }
