b10bb5c246af05b735c552f7e2da093afecdc285
beep boop <githubbot@vadim.me>
2024-07-09 02:25:41 +0200

    Solution 41 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ltjr7ccv Prompt:       |             ^~~~~~ 4ce8786c-8f30-4604-9cec-c79264805108.cpp:24:20: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 4ce8786c-8f30-4604-9cec-c79264805108.cpp:24:27: error: ‘vector’ was not declared  in this scope    24 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4ce8786c-8f30-4604-9cec-c79264805108.cpp:24:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 4ce8786c-8f30-4604-9cec-c79264805108.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 4ce8786c-8f30-4604-9cec-c79264805108.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4ce8786c-8f30-4604-9cec-c79264805108.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,8 +2,8 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return std::vector<int>(a) == std::vector<int>(b);
+bool issame(const std::vector<int>& a, const std::vector<int>& b){
+    return a == b;
 }
 
 std::vector<int> strange_sort_list(std::vector<int> lst){
