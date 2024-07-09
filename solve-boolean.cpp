e32f1d8f4626898c8421b7bd2afe1fe9acf40a2c
beep boop <githubbot@vadim.me>
2024-07-09 03:07:38 +0200

    Solution 44 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/gfhqqe4z Prompt: 87f105f8-5c5d-4008-8ac3-9203cbff3956.cpp:1:24: error: ‘string’ is not a member o f ‘std’     1 | bool solveBoolean(std::string s) {       |                        ^~~~~~ 87f105f8-5c5d-4008-8ac3-9203cbff3956.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | bool solveBoolean(std::string s) {

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -2,27 +2,28 @@
 
 bool solveBoolean(std::string s) {
     bool result = true;
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == 'F') {
+    for (int i = 0; i < s.size(); ) {
+        if (s[i] == 'T') {
+            i++;
+        } else if (s[i] == 'F') {
             return false;
         } else if (s[i] == '|') {
-            if (i == s.size() - 1 || (s[i + 1] == '&' && s.find('T', i + 2) != std::string::npos)) {
+            if (i == s.size() - 1) {
                 return true;
-            } else if (i == s.size() - 1 || s[i + 1] == 'F') {
-                return false;
             }
             i++;
         } else if (s[i] == '&') {
             bool foundT = false;
-            for (int j = i; j < s.size(); j++) {
+            for (int j = i; j < s.size(); ) {
                 if (s[j] == 'T') {
                     foundT = true;
                     break;
                 } else if (s[j] == 'F') {
                     return false;
                 }
+                j++;
             }
-            result = foundT;
+            result = foundT && !foundT ? false : result;
         }
     }
     return result;
