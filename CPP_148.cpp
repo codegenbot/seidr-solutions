f578f9d0f58691b3eab5fcc92e6806f03a5c3c54
beep boop <githubbot@vadim.me>
2024-07-09 04:52:22 +0200

    Solution 21 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kttdajki Prompt:       |                              ^~~~~~ 96a1a901-2f4d-4e5c-8b06-b6b0240ce689.cpp:39:37: error: ‘string’ was not declared  in this scope    39 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 96a1a901-2f4d-4e5c-8b06-b6b0240ce689.cpp:39:44: error: ‘b’ was not declared in t his scope    39 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 96a1a901-2f4d-4e5c-8b06-b6b0240ce689.cpp:39:45: error: expression list treated a s compound expression in initializer [-fpermissive]    39 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 96a1a901-2f4d-4e5c-8b06-b6b0240ce689.cpp:38: 96a1a901-2f4d-4e5c-8b06-b6b0240ce689.cpp: In function ‘int main()’: 96a1a901-2f4d-4e5c-8b06-b6b0240ce689.cpp:48:20: error: ‘bf’ was not declared in this scope    48 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 96a1a901-2f4d-4e5c-8b06-b6b0240ce689.cpp:48:19: error: ‘issame’ cannot be used a s a function    48 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -3,13 +3,7 @@
 #include <string>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if (a.size() != b.size()) 
-        return false;
-    for(int i=0; i<a.size(); i++) {
-        if(a[i] != b[i])
-            return false;
-    }
-    return true;
+    return (a == b);
 }
 
 std::vector<string> bf(string planet1, string planet2) {
@@ -50,6 +44,5 @@ std::vector<string> bf(string planet1, string planet2) {
 }
 
 int main() {
-    assert(bf("Jupiter", "Mars") == {});
-    return 0;
+    assert(bf("Jupiter", "Mars") == {"Venus", "Earth"});  
 }
