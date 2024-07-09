f9742d8bf770be902aabac7d6c2b4b1b430ca5cd
beep boop <githubbot@vadim.me>
2024-07-09 21:21:55 +0200

    Solution 25 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g34sr5ik Prompt:       |                    ^~~ fb239132-ecc2-42ae-ad1e-8cbc666116ab.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fb239132-ecc2-42ae-ad1e-8cbc666116ab.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fb239132-ecc2-42ae-ad1e-8cbc666116ab.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fb239132-ecc2-42ae-ad1e-8cbc666116ab.cpp:14: fb239132-ecc2-42ae-ad1e-8cbc666116ab.cpp: In function ‘int main()’: fb239132-ecc2-42ae-ad1e-8cbc666116ab.cpp:24:20: error: ‘common’ was not declared  in this scope    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ fb239132-ecc2-42ae-ad1e-8cbc666116ab.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <set>
-using namespace std;
+#include <algorithm>
 
 bool issame(vector<int> a, vector<int> b){
     return (a == b);
@@ -22,32 +22,13 @@ vector<int> common(vector<int> l1, vector<int> l2) {
 }
 
 int main() {
-    int n1, m1, n2, m2;
-    cout << "Enter the number of elements for the first list: ";
-    cin >> n1;
-    vector<int> l1(n1);
-    cout << "Enter the elements for the first list: ";
-    for (int i = 0; i < n1; i++) {
-        cin >> l1[i];
-    }
-    
-    cout << "Enter the number of elements for the second list: ";
-    cin >> m2;
-    vector<int> l2(m2);
-    cout << "Enter the elements for the second list: ";
-    for (int i = 0; i < m2; i++) {
-        cin >> l2[i];
-    }
-    
+    vector<int> l1 = {1, 2, 3};
+    vector<int> l2 = {2, 4, 5};
+
     vector<int> output = common(l1, l2);
-    if(output.size() == 0) cout << "The lists have no common elements.\n";
-    else {
-        cout << "The lists have the following common elements: ";
-        for (int i : output) {
-            cout << i << " ";
-        }
-        cout << "\n";
+
+    for (int i : output) {
+        std::cout << i << " ";
     }
-    
     return 0;
 }
