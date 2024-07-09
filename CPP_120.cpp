13b9e045ec697768c5c9b9be0826257aa58667b8
beep boop <githubbot@vadim.me>
2024-07-09 04:38:43 +0200

    Solution 52 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ajzrdwh9 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ a2727b45-b72a-40fe-9d48-a147160aa111.cpp:10:25:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -5,29 +5,20 @@
 std::vector<int> maximum(std::vector<int> arr, int k) {
     std::vector<int> result;
     for(int i = 0; i < k; i++) {
-        auto it = *std::max_element(arr.begin(), arr.end());
-        result.push_back(it);
-        arr.erase(std::remove(arr.begin(), arr.end(), it), arr.end());
+        auto it = arr.begin();
+        it = std::max_element(arr.begin(), arr.end());
+        result.push_back(*it);
+        arr.erase(it);
     }
     return result;
 
 }
 
 int main() {
-    std::vector<int> test;
-    int n, k;
-    std::cout << "Enter the size of array: ";
-    std::cin >> n;
-    for(int i = 0; i < n; i++) {
-        std::cout << "Enter element number " << (i+1) << ": ";
-        int x;
-        std::cin >> x;
-        test.push_back(x);
-    }
-    std::cout << "Enter k: ";
-    std::cin >> k;
-    std::vector<int> result = maximum(test, k);
-    for (int i : result) {
+    // Your code here
+    // For example:
+    std::vector<int> test = maximum({1, 2, 3, -23, 243, -400, 0}, 3);
+    for (int i : test) {
         std::cout << i << " ";
     }
     return 0;
