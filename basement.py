5ce1adebcc2a71b91f9e36e3d73491491a7de733
beep boop <githubbot@vadim.me>
2024-07-09 04:52:51 +0200

    Solution 12 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/f13bbgrp Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/969ac8b5-5fe3-4652-b20c-43e15d917 794.py", line 1     Here is the solution:                 ^^^^^^^^ SyntaxError: invalid syntax

--- basement.py
+++ basement.py
@@ -1,10 +1,6 @@
-def basement(nums):
-    for i in range(len(nums)):
-        s = 0
-        j = i
-        while s < 0:
-            s += nums[j]
-            if s < 0:
-                return j
-            j += 1
+```
+def basement(input_list):
+    for i in range(len(input_list)):
+        if sum(input_list[:i+1]) < 0:
+            return i
     return -1
