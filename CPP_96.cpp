a6c3adfb11130631964bedaf99106975a4f672ab
beep boop <githubbot@vadim.me>
2024-07-09 10:37:09 +0200

    Solution 55 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/c8pse80p Prompt:    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6affd264-3441-4f85-893a-a1cafd3f891c.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6affd264-3441-4f85-893a-a1cafd3f891c.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6affd264-3441-4f85-893a-a1cafd3f891c.cpp:22: 6affd264-3441-4f85-893a-a1cafd3f891c.cpp: In function ‘int main()’: 6affd264-3441-4f85-893a-a1cafd3f891c.cpp:32:20: error: ‘count_up_to’ was not dec lared in this scope    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 6affd264-3441-4f85-893a-a1cafd3f891c.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -4,11 +4,6 @@ bool issame(vector<int> a, vector<int> b) {
 
 vector<int> count_up_to(int n);
 
-int main() {
-    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-    return 0;
-}
-
 vector<int> count_up_to(int n) {
     vector<int> result;
     if (n <= 1) {
@@ -29,3 +24,5 @@ vector<int> count_up_to(int n) {
     
     return result;
 }
+
+assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
