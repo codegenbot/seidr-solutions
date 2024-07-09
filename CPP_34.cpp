c008ef40776653210eb7cde9ab3feecb9c197916
beep boop <githubbot@vadim.me>
2024-07-10 01:33:00 +0200

    Solution 10 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uzrnpb22 Prompt: In file included from c2264d0d-d01a-4dd3-87b8-7e2b08a089b8.cpp:18: c2264d0d-d01a-4dd3-87b8-7e2b08a089b8.cpp:28:26: note:   candidate expects 4 argu ments, 1 provided    28 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from c2264d0d-d01a-4dd3-87b8-7e2b08a089b8.cpp:3: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: candidate: ‘template <class _ExecutionPolicy, class _ForwardIterator> __pstl::__internal::__enable_if _execution_policy<_ExecutionPolicy, _ForwardIterator> std::unique(_ExecutionPoli cy&&, _ForwardIterator, _ForwardIterator)’   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note:   template argument deduction/substitution failed: In file included from c2264d0d-d01a-4dd3-87b8-7e2b08a089b8.cpp:18: c2264d0d-d01a-4dd3-87b8-7e2b08a089b8.cpp:28:26: note:   candidate expects 3 argu ments, 1 provided    28 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main() {
     vector<int> input = {5, 3, 5, 2, 3, 3, 9, 0, 123};
     vector<int> output;
-    auto end_unique = unique(input.begin(), input.end());
-    copy(end_unique, input.end(), back_inserter(output));
+    copy(unique(input.begin(), input.end()), input.end(), back_inserter(output));
     sort(output.begin(), output.end());
+    assert(std::equal(std::unique(input.begin(), input.end()), input.end(), output.begin()));
     for (int i : output) {
         cout << i << " ";
     }
