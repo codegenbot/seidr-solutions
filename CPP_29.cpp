d309641dcbe520dd4f34b7726dc249e219122956
beep boop <githubbot@vadim.me>
2024-07-09 03:15:42 +0200

    Solution 34 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lqyq253s Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 25f278a8-2c4e-495a-9082-d45c92f85f54.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 25f278a8-2c4e-495a-9082-d45c92f85f54.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 25f278a8-2c4e-495a-9082-d45c92f85f54.cpp:10: 25f278a8-2c4e-495a-9082-d45c92f85f54.cpp: In function ‘int main()’: 25f278a8-2c4e-495a-9082-d45c92f85f54.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 25f278a8-2c4e-495a-9082-d45c92f85f54.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,15 +1,13 @@
+```cpp
 #include <vector>
 #include <string>
+using namespace std;
 
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size())
-        return false;
-    
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i])
-            return false;
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]) return false;
     }
-    
     return true;
 }
 
