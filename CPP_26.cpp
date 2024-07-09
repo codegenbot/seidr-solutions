116444ef98de1599f60b3a7d470480ffc6a3d014
beep boop <githubbot@vadim.me>
2024-07-09 08:59:53 +0200

    Solution 92 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/0befflxg Prompt: <_Key, _Val, _KeyOfValue, _Compare, _Alloc>::iterator = std::_Rb_tree<int, int, std::_Identity<int>, std::less<int>, std::allocator<int> >::iterator; std::_Rb_t ree<_Key, _Val, _KeyOfValue, _Compare, _Alloc>::const_iterator = std::_Rb_tree<i nt, int, std::_Identity<int>, std::less<int>, std::allocator<int> >::const_itera tor]’ /usr/include/c++/11/bits/stl_tree.h:1102:23:   required from ‘std::__enable_if_t <std::is_same<_Val, typename std::iterator_traits<_InputIterator>::value_type>:: value> std::_Rb_tree<_Key, _Val, _KeyOfValue, _Compare, _Alloc>::_M_insert_range _unique(_InputIterator, _InputIterator) [with _InputIterator = __gnu_cxx::__norm al_iterator<const int*, std::vector<int> >; _Key = int; _Val = int; _KeyOfValue = std::_Identity<int>; _Compare = std::less<int>; _Alloc = std::allocator<int>; std::__enable_if_t<std::is_same<_Val, typename std::iterator_traits<_InputIterat or>::value_type>::value> = void; typename std::iterator_traits<_InputIterator>:: value_type = int]’ /usr/include/c++/11/bits/stl_set.h:193:31:   required from ‘std::set<_Key, _Comp are, _Alloc>::set(_InputIterator, _InputIterator) [with _InputIterator = __gnu_c xx::__normal_iterator<const int*, std::vector<int> >; _Key = int; _Compare = std ::less<int>; _Alloc = std::allocator<int>]’ 0232daf1-208b-485f-9a35-6fa76c1e5c1a.cpp:22:49:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::_Rb_tree_node<int> >’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -19,9 +19,9 @@ bool isEqualVector(const std::vector<int>& a, const std::vector<int>& b) {
 std::vector<int> removeDuplicates(const std::vector<int>& numbers) {
     if (numbers.empty()) return std::vector<int>();
 
-    std::set<int> uniqueNumbersSet(numbers.begin(), numbers.end());
-    std::vector<int> uniqueNumbers(uniqueNumbersSet.begin(), uniqueNumbersSet.end());
-    std::sort(uniqueNumbers.begin(), uniqueNumbers.end());
+    std::sort(numbers.begin(), numbers.end());
+    std::set<int>(numbers.begin(), numbers.end());
+    std::vector<int> uniqueNumbers(std::set<int>(numbers.begin(), numbers.end()).begin(), std::set<int>(numbers.begin(), numbers.end()).end());
 
     return uniqueNumbers;
 }
