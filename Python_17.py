95373366e47ff8d1dc15b5ff0635a17226cdc546
beep boop <githubbot@vadim.me>
2024-07-09 06:30:22 +0200

    Solution 92 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/yf3bs1bd Prompt: Program did not receive expected input

--- Python_17.py
+++ Python_17.py
@@ -1,5 +1,3 @@
 input_list = []
-for _ in range(int(input().strip())):
-    total_test_cases = int(input().strip())
-    for _ in range(total_test_cases):
-        input_list.append(input().strip().split("|"))
+for _ in range(int(input())):
+    input_list.append(input().split("|"))
