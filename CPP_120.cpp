76f8677fd1e7958e88c10b11fb6e7415dce73376
beep boop <githubbot@vadim.me>
2024-07-09 04:21:38 +0200

    Solution 22 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/usez16r3 Prompt:       |                    ^~~ 62ceb78c-04c4-4cef-8677-ef9a5da183aa.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 62ceb78c-04c4-4cef-8677-ef9a5da183aa.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 62ceb78c-04c4-4cef-8677-ef9a5da183aa.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 62ceb78c-04c4-4cef-8677-ef9a5da183aa.cpp:11: 62ceb78c-04c4-4cef-8677-ef9a5da183aa.cpp: In function ‘int main()’: 62ceb78c-04c4-4cef-8677-ef9a5da183aa.cpp:21:20: error: ‘maximum’ was not declare d in this scope    21 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 62ceb78c-04c4-4cef-8677-ef9a5da183aa.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <algorithm>
 
@@ -6,9 +5,10 @@ std::vector<int> maximum(std::vector<int> arr, int k) {
     std::vector<int> result;
     
     for (int i = 0; i < k; i++) {
-        auto it = std::max_element(arr.begin(), arr.end());
-        result.push_back(*it);
-        arr.erase(it);
+        auto it = *std::max_element(arr.begin(), arr.end());
+        result.push_back(it);
+        auto newEnd = remove_if(arr.begin(), arr.end(), [it](int x) { return x == it; });
+        arr.erase(newEnd, arr.end());
     }
     
     return result;
