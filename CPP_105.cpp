1a1685b1c45fec0b831118752c469ab5e23e6421
beep boop <githubbot@vadim.me>
2024-07-09 19:10:54 +0200

    Solution 2 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/neid1i7l Prompt:       |                              ^~~~~~ 870ec033-c8d8-40fb-a263-e9d38588cc17.cpp:49:37: error: ‘string’ was not declared  in this scope    49 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 870ec033-c8d8-40fb-a263-e9d38588cc17.cpp:49:44: error: ‘b’ was not declared in t his scope    49 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 870ec033-c8d8-40fb-a263-e9d38588cc17.cpp:49:45: error: expression list treated a s compound expression in initializer [-fpermissive]    49 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 870ec033-c8d8-40fb-a263-e9d38588cc17.cpp:48: 870ec033-c8d8-40fb-a263-e9d38588cc17.cpp: In function ‘int main()’: 870ec033-c8d8-40fb-a263-e9d38588cc17.cpp:58:20: error: ‘by_length’ was not decla red in this scope    58 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 870ec033-c8d8-40fb-a263-e9d38588cc17.cpp:58:19: error: ‘issame’ cannot be used a s a function    58 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,4 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
 #include <string>
+
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 vector<string> by_length(vector<int> arr) {
     vector<int> temp;
@@ -14,17 +29,47 @@ vector<string> by_length(vector<int> arr) {
     vector<string> result;
     for (int i : temp) {
         switch (i) {
-            case 1: result.push_back("One"); break;
-            case 2: result.push_back("Two"); break;
-            case 3: result.push_back("Three"); break;
-            case 4: result.push_back("Four"); break;
-            case 5: result.push_back("Five"); break;
-            case 6: result.push_back("Six"); break;
-            case 7: result.push_back("Seven"); break;
-            case 8: result.push_back("Eight"); break;
-            case 9: result.push_back("Nine"); break;
+            case 1:
+                result.push_back("One");
+                break;
+            case 2:
+                result.push_back("Two");
+                break;
+            case 3:
+                result.push_back("Three");
+                break;
+            case 4:
+                result.push_back("Four");
+                break;
+            case 5:
+                result.push_back("Five");
+                break;
+            case 6:
+                result.push_back("Six");
+                break;
+            case 7:
+                result.push_back("Seven");
+                break;
+            case 8:
+                result.push_back("Eight");
+                break;
+            case 9:
+                result.push_back("Nine");
+                break;
         }
     }
 
     return result;
+}
+
+int main() {
+    vector<int> input = {9, 4, 8};
+    vector<string> output = by_length(input);
+    
+    if (issame(output, {"Nine", "Eight", "Four"})) {
+        for (string str : output) {
+            cout << str << endl;
+        }
+    }
+    return 0;
 }
