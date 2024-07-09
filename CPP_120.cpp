05a3ca84a2246f1c3b10a7cae9e93b5b4ca2ab16
beep boop <githubbot@vadim.me>
2024-07-09 02:01:19 +0200

    Solution 16 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/j2eldqxj Prompt:       |     ^~~~~~ 221d610c-ff3d-4726-8053-8f9a3ba57ee9.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <iostream>   +++ |+#include <cassert>     3 | using namespace std; 221d610c-ff3d-4726-8053-8f9a3ba57ee9.cpp: At global scope: 221d610c-ff3d-4726-8053-8f9a3ba57ee9.cpp:24:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    24 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 221d610c-ff3d-4726-8053-8f9a3ba57ee9.cpp:5:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     5 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 221d610c-ff3d-4726-8053-8f9a3ba57ee9.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 221d610c-ff3d-4726-8053-8f9a3ba57ee9.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-bool issame(vector<int> a,vector<int>b){
+bool issame(vector<int> a, vector<int> b) {
     if(a.size() == b.size())
         return true;
     else
@@ -12,12 +12,12 @@ bool issame(vector<int> a,vector<int>b){
 }
 
 vector<int> maximum(vector<int> arr, int k) {
-    vector<int> result(arr.begin(), arr.end());
+    vector<int> result(arr.begin(), arr.begin() + k);
     sort(result.begin(), result.end());
     return result;
 }
 
 int main() {
-    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 7) , {0,-23,-400,1,2,3,243}));
+    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 7), {0,-23,-400,1,2,3,243}));
     return 0;
 }
