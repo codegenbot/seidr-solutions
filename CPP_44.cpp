65c89fcfe815d4f1040f820f25115f1ecf06a3ab
beep boop <githubbot@vadim.me>
2024-07-09 18:15:23 +0200

    Solution 6 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/10n6c9qj Prompt: 6b878a88-49f1-42aa-8df8-b65561579f1a.cpp:11:42: note:   mismatched types ‘std::_ _cxx11::basic_string<_CharT, _Traits, _Allocator>’ and ‘int’    11 |             res = 'A' + remainder - 10 + res;       |                                          ^~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 6b878a88-49f1-42aa-8df8-b65561579f1a.cpp:1: /usr/include/c++/11/bits/stl_iterator.h:1276:5: note: candidate: ‘template<class  _Iterator, class _Container> __gnu_cxx::__normal_iterator<_Iterator, _Container > __gnu_cxx::operator+(typename __gnu_cxx::__normal_iterator<_Iterator, _Contain er>::difference_type, const __gnu_cxx::__normal_iterator<_Iterator, _Container>& )’  1276 |     operator+(typename __normal_iterator<_Iterator, _Container>::differe nce_type       |     ^~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h:1276:5: note:   template argument deduct ion/substitution failed: 6b878a88-49f1-42aa-8df8-b65561579f1a.cpp:11:42: note:   ‘std::string’ {aka ‘std: :__cxx11::basic_string<char>’} is not derived from ‘const __gnu_cxx::__normal_it erator<_Iterator, _Container>’    11 |             res = 'A' + remainder - 10 + res;       |                                          ^~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -1,1 +1,12 @@
-res.insert(0, 1, remainder + '0');
+#include<string>
+using namespace std;
+
+string change_base(int x, int base) {
+    string res = "";
+    while (x > 0) {
+        int remainder = x % base;
+        res = (remainder < 10 ? to_string(remainder) : string(1, 'A') + (remainder - 10)) + res;
+        x /= base;
+    }
+    return res;
+}
