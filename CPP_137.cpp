ac2c64827c115a307ea1794217c173a38c5642e9
beep boop <githubbot@vadim.me>
2024-07-09 22:02:08 +0200

    Solution 31 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/xnp67k3w Prompt: pe() == typeid(string) && b.type() == typeid(int))){       | ^~~~ c9e365f3-a2c7-445c-9ffe-d0a998b59b9f.cpp:33:1: error: expected unqualified-id be fore ‘else’    33 | else{       | ^~~~ In file included from c9e365f3-a2c7-445c-9ffe-d0a998b59b9f.cpp:37: c9e365f3-a2c7-445c-9ffe-d0a998b59b9f.cpp: In function ‘int main()’: c9e365f3-a2c7-445c-9ffe-d0a998b59b9f.cpp:39:13: error: ‘boost’ has not been decl ared    39 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ c9e365f3-a2c7-445c-9ffe-d0a998b59b9f.cpp:39:29: error: ‘string’ was not declared  in this scope    39 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ c9e365f3-a2c7-445c-9ffe-d0a998b59b9f.cpp:39:37: error: ‘compare_one’ was not dec lared in this scope    39 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -5,33 +5,25 @@
 
 using namespace std;
 
-template<typename T, typename U>
 boost::any compare(boost::any a, boost::any b) {
-    assert(a.type() == typeid(T) && b.type() == typeid(U));
-    
-    if(boost::any_cast<T>(a) > boost::any_cast<U>(b))
-        return a;
-    else if(boost::any_cast<T>(a) < boost::any_cast<U>(b))
-        return b;
-    else
-        return boost::any("None");
-}
+    assert(a.type() == b.type());
 
-int main() {
-    boost::any a = 5;
-    boost::any b = 10;
-    
-    cout << boost::any_cast<int>(compare<int, int>(a, b)) << endl;
-    
-    a = 3.14f;
-    b = 6.28f;
-    
-    cout << boost::any_cast<float>(compare<float, float>(a, b)) << endl;
-    
-    a = string("hello");
-    b = string("world");
-    
-    cout << boost::any_cast<string>(compare<string, string>(a, b)) << endl;
-    
-    return 0;
+    if (a.type() == typeid(int)) {
+        if (boost::any_cast<int>(a) > boost::any_cast<int>(b))
+            return a;
+        else if (boost::any_cast<int>(a) < boost::any_cast<int>(b))
+            return b;
+    } else if (a.type() == typeid(float)) {
+        if (boost::any_cast<float>(a) > boost::any_cast<float>(b))
+            return a;
+        else if (boost::any_cast<float>(a) < boost::any_cast<float>(b))
+            return b;
+    } else if (a.type() == typeid(string)) {
+        if (stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b)))
+            return a;
+        else if (stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b)))
+            return b;
+    }
+
+    return boost::any();
 }
