920d155f0d16b1a11a12b25864745ab5916f8f5f
beep boop <githubbot@vadim.me>
2024-07-09 11:48:14 +0200

    Solution 57 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8sznj9cc Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ a7846761-a86b-4b78-b163-0840d24fedd3.cpp:23:29:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,17 +1,20 @@
 ```cpp
-#include <ostream>
-
-namespace std { class initializer_list; }
-
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <string>
 #include <initializer_list>
+#include <memory>
 
 using namespace std;
 
 bool issame(const vector<string>& a, const vector<string>& b) {
     return (a == b);
+}
+
+bool issame(initializer_list<string> a, initializer_list<string> b) {
+    vector<string> v1(a), v2(b);
+    return (v1 == v2);
 }
 
 vector<string> sorted_list_sum(vector<string> lst) {
@@ -33,13 +36,14 @@ vector<string> sorted_list_sum(vector<string> lst) {
 }
 
 int main_entry() {
-    assert(issame(vector<string>({"cc","dd","aaaa","bbbb"}), vector<string>({"cc","dd","aaaa","bbbb"})));
+    assert(issame(vector<string>(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"})), vector<string>({"cc","dd","aaaa","bbbb"})));
     
     int n;
     cout << "Enter the number of strings: ";
     cin >> n;
     
     vector<string> inputStrings;
+    inputStrings.reserve(n);  // Add this line
     for(int i = 0; i < n; i++) {
         string str;
         cout << "Enter string " << (i+1) << ": ";
