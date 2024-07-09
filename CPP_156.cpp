d293960a6ae6e4997ad95398eaf46ff8b2318ae3
beep boop <githubbot@vadim.me>
2024-07-09 05:54:42 +0200

    Solution 84 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/urrbvh7e Prompt: d41be63f-55b0-4ec1-a209-07a8cd5f509b.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ d41be63f-55b0-4ec1-a209-07a8cd5f509b.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ d41be63f-55b0-4ec1-a209-07a8cd5f509b.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ d41be63f-55b0-4ec1-a209-07a8cd5f509b.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ In file included from d41be63f-55b0-4ec1-a209-07a8cd5f509b.cpp:83: d41be63f-55b0-4ec1-a209-07a8cd5f509b.cpp: In function ‘int main()’: d41be63f-55b0-4ec1-a209-07a8cd5f509b.cpp:85:13: error: ‘int_to_mini_romank’ was not declared in this scope    85 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,6 +1,6 @@
 ```cpp
-std::string int_to_mini_roman(int num) {
-    std::string roman_str[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",
+string int_to_mini_roman(int num) {
+    string roman[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",
                       "", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
                       "", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
                       "", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
@@ -11,10 +11,11 @@ std::string int_to_mini_roman(int num) {
                       "", "C", "CI", "CII", "CIII", "CIV", "CV", "CVI", "CVII", "CVIII", "CX", "CXI", "CXII", "CXIII", "CXIV", "CXV", "CXVI", "CXVII", "CXVIII", "CXIX", "CXX",
                       "", "CXXI", "CXXII", "CXXIII", "CXXIV", "CXXV", "CXXVI", "CXXVII", "CXXVIII", "CXXIX", "CXXX",
                       "", "CXXXI", "CXXXII", "CXXXIII", "CXXXIV", "CXXXV", "CXXXVI", "CXXXVII", "CXXXVIII", "CXXXIX", "CXL",
-                      "", "CXLI", "CXLII", "CXLIII", "CXLIV", "CXLV", "CXLVI", "CXLVII", "CXLVIII", "CXlix", "CL",
-                      "", "CLI", "CII", "CC", "CCI", "CCC", "CCCI", "CCCII", "CCCIII", "CCCIV", "CCCV", "CCCVI", "CCCvii", "CCcviii", "CCXC"};
+                      "", "CXLI", "CXLII", "CXLIII", "CXLIV", "CXLV", "CXLVI", "CXLVII", "CXLVIII", "Clix", "CL",
+                      "", "CLI", "CII", "CC", "CCI", "CCC", "CCCI", "CCCII", "CCCIII", "CCCIV", "CCCV", "CCCVI", "CCcvii", "CCcviii", "CCXC", 
+                      "", "CM"};
 
-    std::string result = "";
+    string result = "";
 
     while (num > 0) {
         if (num >= 1000) {
@@ -30,30 +31,34 @@ std::string int_to_mini_roman(int num) {
             num -= 400;
             result += "CD";
         } else if (num >= 100) {
-            for(int i = 0; i < (num / 100); ++i) {
-                num -= 100;
+            for(int i = 0; ; ++i) {
+                if((i * 100) > num) break;
+                num -= i * 100;
                 result += "C";
-            }
-        } else if (num >= 90) {
-            num -= 90;
-            result += "XC";
-        } else if (num >= 50) {
-            num -= 50;
-            result += "L";
-        } else if (num >= 40) {
-            num -= 40;
-            result += "XL";
-        } else {
-            for(int i = 0; num > 0; --i) {
-                if(num >= int(roman_str[i].length())) {
-                    num -= int(roman_str[i].length());
-                    result += roman_str[i];
+                for(int j = 0; j < i - 1; ++j) {
+                    result += "X";
                 }
-                else {
-                    for(int j = 0; j < i; ++j) {
-                        result += "I";
-                    }
-                    break;
+            }
+        } else if (num >= 10) {
+            for(int i = 0; ; ++i) {
+                if((i * 10) > num) break;
+                num -= i * 10;
+                result += "X";
+                for(int j = 0; j < i - 1; ++j) {
+                    result += "I";
+                }
+            }
+        } else {
+            while(num > 0) {
+                if(num >= 3) {
+                    num -= 3;
+                    result += "I";
+                } else if (num == 2) {
+                    result += "II";
+                    return result;
+                } else {
+                    num--;
+                    result += "I";
                 }
             }
         }
