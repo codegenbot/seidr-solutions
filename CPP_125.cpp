2e44fa68f1b3a69453b3b2d7fda159870f050c74
beep boop <githubbot@vadim.me>
2024-07-09 06:01:01 +0200

    Solution 98 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/j75cp36i Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 9f9d6989-c271-4f31-9464-ced5be084542.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 9f9d6989-c271-4f31-9464-ced5be084542.cpp:54:44: error: ‘b’ was not declared in t his scope    54 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 9f9d6989-c271-4f31-9464-ced5be084542.cpp:62:5: error: redefinition of ‘int main( )’    62 | int main(){       |     ^~~~ 9f9d6989-c271-4f31-9464-ced5be084542.cpp:48:5: note: ‘int main()’ previously def ined here    48 | int main() {       |     ^~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -33,16 +33,7 @@ std::vector<std::string> split_words(std::string txt) {
 }
 
 bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i].compare(b[i]) != 0) {
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
 int main() {
