026fe03976f34b7da96636c371e0361db6cee2e2
beep boop <githubbot@vadim.me>
2024-07-09 13:24:36 +0200

    Solution 95 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qnqbku9p Prompt:       | ^~~~~~~~ 1cd0aebb-0605-4060-9c5c-c3fa7d6bbb46.cpp:15:33: error: expected unqualified-id b efore ‘)’ token    15 | romanMap.push_back({40, 'X'+'L'});       |                                 ^ 1cd0aebb-0605-4060-9c5c-c3fa7d6bbb46.cpp:16:1: error: ‘romanMap’ does not name a  type    16 | romanMap.emplace_back(1, 'I');       | ^~~~~~~~ 1cd0aebb-0605-4060-9c5c-c3fa7d6bbb46.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 1cd0aebb-0605-4060-9c5c-c3fa7d6bbb46.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main() {       |     ^~~~ In file included from 1cd0aebb-0605-4060-9c5c-c3fa7d6bbb46.cpp:41: 1cd0aebb-0605-4060-9c5c-c3fa7d6bbb46.cpp: In function ‘int main()’: 1cd0aebb-0605-4060-9c5c-c3fa7d6bbb46.cpp:43:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    43 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,45 +1,30 @@
-```cpp
 #include <iostream>
 #include <vector>
-#include <string>
 
-std::vector<std::pair<int, std::string>> romanMap;
+using namespace std;
 
-romanMap.push_back({ 1000, "M" });
-romanMap.push_back({ 900, "CM" });
-romanMap.push_back({ 500, "D" });
-romanMap.push_back({ 400, "CD" });
-romanMap.push_back({ 100, "C" });
-romanMap.push_back({ 90, "XC" });
-romanMap.push_back({ 50, "L" });
-romanMap.push_back({ 40, "XL" });
-romanMap.emplace_back(1, "I");
+vector<pair<int, char>> romanMap;
 
-std::string int_to_string(int number) {
-    if (number == 0)
-        return "0";
-    std::string str = "";
-    char* roman[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-    char* tens[] = {"", "", "X", "XX", "XXX", "XL", "L", "XC", "C"};
-    if (number >= 10) {
-        str += tens[number / 10];
-        number %= 10;
-    }
-    if (number > 0)
-        str += roman[number];
-    return str;
-}
+romanMap.push_back({1000, 'M'});
+romanMap.push_back({900, 'CM'});
+romanMap.push_back({500, 'D'});
+romanMap.push_back({400, 'CD'});
+romanMap.push_back({100, 'C'});
+romanMap.push_back({90, 'XC'});
+romanMap.push_back({50, 'L'});
+romanMap.push_back({40, 'XL'});
+romanMap.emplace_back(1, 'I');
 
-std::string int_to_mini_roman(int number) {
-    std::string roman = "";
+string int_to_roman(int number) {
+    string roman = "";
     for (const auto& pair : romanMap) {
         while (number >= pair.first) {
             number -= pair.first;
-            roman = roman + int_to_string(pair.second);
+            roman += pair.second;
         }
     }
     if (number > 0) {
-        roman += "I";
+        roman += 'I';
     } else if (number < 0) {
         roman = "-" + roman;
     }
@@ -48,7 +33,7 @@ std::string int_to_mini_roman(int number) {
 
 int main() {
     for(int i = 1; i <= 3999; i++) { 
-        std::cout << i << ": " << int_to_mini_roman(i) << std::endl;
+        cout << i << ": " << int_to_roman(i) << endl;
     }
     return 0;
 }
