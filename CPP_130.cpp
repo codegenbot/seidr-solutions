1089cdb8b2f7ce2af886ac6511ef850dcff85311
beep boop <githubbot@vadim.me>
2024-07-09 06:15:23 +0200

    Solution 85 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/o6zsgg2s Prompt:       |                    ^~~ efc09c07-8126-49f9-95c4-8c1f1dcf080b.cpp:4:27: error: ‘vector’ was not declared in this scope     4 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ efc09c07-8126-49f9-95c4-8c1f1dcf080b.cpp:4:34: error: expected primary-expressio n before ‘int’     4 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ efc09c07-8126-49f9-95c4-8c1f1dcf080b.cpp:4:39: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from efc09c07-8126-49f9-95c4-8c1f1dcf080b.cpp:3: efc09c07-8126-49f9-95c4-8c1f1dcf080b.cpp: In function ‘int main()’: efc09c07-8126-49f9-95c4-8c1f1dcf080b.cpp:13:20: error: ‘tri’ was not declared in  this scope    13 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ efc09c07-8126-49f9-95c4-8c1f1dcf080b.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -3,7 +3,7 @@
 
 std::vector<int> completeCode(int n) {
     std::vector<int> result;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; ++i) {
         result.push_back(i);
     }
     return result;
