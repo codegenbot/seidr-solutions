074f523cc2e9ef683b27755d6834ede2e3067e7e
beep boop <githubbot@vadim.me>
2024-07-09 03:26:11 +0200

    Solution 20 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uilznjb0 Prompt:       |             ^~~ 5ee34374-82bb-4f58-b4e7-063961620e87.cpp:29:33: error: expected primary-expressi on before ‘char’    29 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ 5ee34374-82bb-4f58-b4e7-063961620e87.cpp:29:38: error: expected primary-expressi on before ‘int’    29 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ 5ee34374-82bb-4f58-b4e7-063961620e87.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 5ee34374-82bb-4f58-b4e7-063961620e87.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 5ee34374-82bb-4f58-b4e7-063961620e87.cpp:28: 5ee34374-82bb-4f58-b4e7-063961620e87.cpp: In function ‘int main()’: 5ee34374-82bb-4f58-b4e7-063961620e87.cpp:43:29: error: no match for call to ‘(st d::map<char, int>) (const char [2])’    43 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ~~~~~~~~~^~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,6 +1,8 @@
 #include <map>
-#include <cassert>
 #include <string>
+#include <cassert>
+
+using namespace std;
 
 bool issame(std::map<char, int> a, std::map<char, int> b) {
     return a == b;
@@ -15,14 +17,8 @@ std::map<char, int> histogram(std::string str) {
 }
 
 int main() {
-    std::string str1 = "hello";
-    std::string str2 = "world";
-
-    auto hist1 = histogram(str1);
-    auto hist2 = histogram(str2);
-
-    assert(!issame(hist1, hist2));
-    assert(issame(histogram("a"), {{'a', 1}}));
+    std::map<char, int> hist = histogram("a");
+    assert (issame(hist, {{'a', 1}}));
 
     return 0;
 }
