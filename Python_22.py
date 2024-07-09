26524bc5fa9d100df4f31a30b75897d3e6661e75
beep boop <githubbot@vadim.me>
2024-07-10 01:39:47 +0200

    Solution 7 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5oj21un6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b2a2771b-d17e-407f-9bbf-c88d462fc 1f9.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,14 +1,14 @@
 ```
-from typing import Any, list
+from typing import Any, List
 
-def filter_integers(values: list[Any]) -> None:
+def filter_integers(values: List[Any]) -> None:
     print("Please enter a comma-separated list of values (separated by commas):")
     user_input = input()
     
     try:
         user_values = [val.strip() for val in user_input.split(',')]
         
-        filtered_values = [value for value in map(int, user_values) if isinstance(value, int)]
+        filtered_values = [int(value) if value.isdigit() else value for value in user_values]
         
         print("The integers from the input are:", filtered_values)
     except ValueError:
