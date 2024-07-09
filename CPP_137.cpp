6d7230327dc71978ab6a32980404acf7307ecc1e
beep boop <githubbot@vadim.me>
2024-07-09 21:04:32 +0200

    Solution 93 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/iewm74bm Prompt: 4e6ff5b0-5d90-42d7-9f97-747db107ee3e.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,3 @@
-```
 #include <boost/any.hpp>
 #include <string>
 #include <cmath>
@@ -34,12 +33,12 @@ any compareOne(any a, any b) {
     else if (a.type() == typeid(int) && b.type() == typeid(string)) {
         int x = boost::any_cast<int>(a);
         string y = boost::any_cast<string>(b);
-        return (x > stol(y)) ? a : ((stol(y) > x) ? b : any(typeid(void)));
+        return (x > stod(y)) ? a : ((stod(y) > x) ? b : any(typeid(void)));
     }
     else if (a.type() == typeid(float) && b.type() == typeid(string)) {
         float x = boost::any_cast<float>(a);
         string y = boost::any_cast<string>(b);
-        return (x > stof(y)) ? a : ((stof(y) > x) ? b : any(typeid(void)));
+        return (x > stod(y)) ? a : ((stod(y) > x) ? b : any(typeid(void)));
     }
     else {
         return any(typeid(void));
