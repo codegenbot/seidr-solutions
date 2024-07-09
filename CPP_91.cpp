491c151a0cd1280471aab3f56280158fe6e263ca
beep boop <githubbot@vadim.me>
2024-07-09 18:49:19 +0200

    Solution 8 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/pudv0wlu Prompt: 519a0a7f-2f18-4eaa-96b8-abd5d09ce90d.cpp:1:14: error: ‘string’ was not declared in this scope     1 | int is_bored(string S) {       |              ^~~~~~ In file included from 519a0a7f-2f18-4eaa-96b8-abd5d09ce90d.cpp:13: 519a0a7f-2f18-4eaa-96b8-abd5d09ce90d.cpp: In function ‘int main()’: 519a0a7f-2f18-4eaa-96b8-abd5d09ce90d.cpp:15:21: error: ‘is_bored’ cannot be used  as a function    15 |     assert (is_bored("You and I are going for a walk") == 0);       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,12 +1,2 @@
 #include <string>
-
 int is_bored(string S) {
-    int count = 0;
-    size_t pos = 0;
-    while ((pos = S.find("I", pos)) != string::npos) {
-        if (S[pos] == ' ' || pos == 0 || (S[pos - 1] == '.' || S[pos - 1] == '?' || S[pos - 1] == '!')) {
-            count++;
-        }
-        pos += 1;
-    }
-    return count;
