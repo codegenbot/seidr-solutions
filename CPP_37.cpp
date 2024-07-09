d1097677076cdc8d30bc02df9b57753c33d1235a
beep boop <githubbot@vadim.me>
2024-07-09 08:18:04 +0200

    Solution 64 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ibgdwwkp Prompt:    32 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ e06e4caa-357a-48b9-ab73-6c6d8e37c7ab.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ e06e4caa-357a-48b9-ab73-6c6d8e37c7ab.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from e06e4caa-357a-48b9-ab73-6c6d8e37c7ab.cpp:31: e06e4caa-357a-48b9-ab73-6c6d8e37c7ab.cpp: In function ‘int main()’: e06e4caa-357a-48b9-ab73-6c6d8e37c7ab.cpp:41:20: error: ‘sort_even’ was not decla red in this scope    41 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ e06e4caa-357a-48b9-ab73-6c6d8e37c7ab.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,15 +1,39 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cassert>
+using namespace std;
 
-std::vector<float> sort_even(std::vector<float> l) {
-    // Implementation remains the same
+vector<float> sort_even(vector<float> l) {
+    vector<float> even_indices;
+    for (int i = 0; i < l.size(); i++) {
+        if (i % 2 == 0) {
+            even_indices.push_back(l[i]);
+        }
+    }
+    sort(even_indices.begin(), even_indices.end());
+
+    vector<float> result;
+    int even_idx = 0;
+    for (int i = 0; i < l.size(); i++) {
+        if (i % 2 == 0) {
+            result.push_back(even_indices[even_idx++]);
+        } else {
+            result.push_back(l[i]);
+        }
+    }
+
+    return result;
 }
 
-bool issame(std::vector<float> a, std::vector<float> b) {
+bool issame(vector<float> a, vector<float> b) {
     return a == b;
 }
 
 int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), std::vector<float>{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    if (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10})) {
+        cout << "Test Passed" << endl;
+    } else {
+        cout << "Test Failed" << endl;
+    }
+    return 0;
 }
