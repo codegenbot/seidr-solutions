4161f7314540b8f4622b5041100c338452922429
beep boop <githubbot@vadim.me>
2024-07-09 23:20:21 +0200

    Solution 83 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fxnddfx6 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:10: f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp: In function ‘int main()’: f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,8 +1,9 @@
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) return false;
+    if(a.size() != b.size())
+        return false;
     for(int i = 0; i < a.size(); i++) {
-        int pos = a[i].find(prefix);
-        if(pos == string::npos || a[i].substr(0,pos) != prefix) return false;
+        if(find(b.begin(), b.end(), a[i]) == b.end())
+            return false;
     }
     return true;
 }
