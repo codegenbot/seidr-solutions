c4e93fc025bc5490a90bcfc3c3a0e881b48f7798
beep boop <githubbot@vadim.me>
2024-07-09 07:52:37 +0200

    Solution 31 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/404w9ynn Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 6243f296-e931-4d4e-98f6-0ecf82123036.cpp:16:31:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -11,26 +11,19 @@ vector<int> sort_third(vector<int> l) {
     for (i = 0; i < l.size(); ) {
         if (i % 3 == 0 && i < l.size() - 2) {
             vector<int> temp;
+            temp.reserve(min(i + 3, l.size()) - i);
             for (int j = i; j < min(i + 3, l.size()); j++) {
                 temp.push_back(l[j]);
             }
             sort(temp.begin(), temp.end());
             int k = 0;
             for (int t : temp) {
-                if (result.size() < INT_MAX / sizeof(int)) {
-                    result.push_back(t); 
-                    i++;
-                } else {
-                    break;
-                }
+                result.push_back(t); 
+                i++;
             }
         } else {
-            if (result.size() < INT_MAX / sizeof(int)) {
-                result.push_back(l[i]); 
-                i++;
-            } else {
-                break;
-            }
+            result.push_back(l[i]); 
+            i++;
         }
     }
     return result;
