b469018481f8f86d712b363cc0cf28e3e810a63d
beep boop <githubbot@vadim.me>
2024-07-09 07:53:55 +0200

    Solution 70 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/5ahk9ug2 Prompt: ined here    18 | int main() {       |     ^~~~ In file included from 0b1e49e8-3523-4159-9e61-814616cdb6aa.cpp:27: 0b1e49e8-3523-4159-9e61-814616cdb6aa.cpp: In function ‘int main()’: 0b1e49e8-3523-4159-9e61-814616cdb6aa.cpp:37:35: error: cannot bind non-const lva lue reference of type ‘std::vector<float>&’ to an rvalue of type ‘std::vector<fl oat>’    37 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 0b1e49e8-3523-4159-9e61-814616cdb6aa.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<float>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 0b1e49e8-3523-4159-9e61-814616cdb6aa.cpp:9:42: note:   initializing argument 1 o f ‘void rescale_to_unit(std::vector<float>&)’     9 | void rescale_to_unit(std::vector<float>& numbers) {       |                      ~~~~~~~~~~~~~~~~~~~~^~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -7,19 +7,19 @@ bool issame(std::vector<float> v1, std::vector<float> v2) {
 }
 
 void rescale_to_unit(std::vector<float>& numbers) {
-    float min_num = *std::min_element(numbers.begin(), numbers.end());
-    float max_num = *std::max_element(numbers.begin(), numbers.end());
-    
-    for (float &num : numbers) {
-        num = (num - min_num) / (max_num - min_num);
-    }
+	float min_num = *std::min_element(numbers.begin(), numbers.end());
+	float max_num = *std::max_element(numbers.begin(), numbers.end());
+
+	for (float& num : numbers) {
+		num = (num - min_num) / (max_num - min_num);
+	}
 }
 
 int main() {
     std::vector<float> input = {12.0, 11.0, 15.0, 13.0, 14.0};
     rescale_to_unit(input);
-    
+
     assert(issame(input, {0.25, 0.0, 1.0, 0.5, 0.75}));
-    
+
     return 0;
 }
