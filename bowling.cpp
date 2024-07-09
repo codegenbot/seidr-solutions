8de528a0d24da56e5a77f621ae41cba7d9d73eac
beep boop <githubbot@vadim.me>
2024-07-09 17:59:06 +0200

    Solution 61 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/acc9guyz Prompt: b9a08976-f308-459d-bdf7-bec44fd0a03d.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int bowlingScore(std::string input) {       |                       ^~~~~~ b9a08976-f308-459d-bdf7-bec44fd0a03d.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int bowlingScore(std::string input) {

--- bowling.cpp
+++ bowling.cpp
@@ -1,32 +1,30 @@
+#include <string>
+
 int bowlingScore(std::string input) {
     int score = 0;
-    for(int i = 0; i < input.length(); i++) {
-        if(input[i] == 'X') { 
+    int i = 0;
+    while (i < input.length()) {
+        if (input[i] == 'X') { 
             score += 10;
-            if(i < input.length() - 1 && input[i+1] == '/') { 
+            if (i + 1 < input.length() && input[i+1] == 'X') { 
                 score += 10;
                 i++; 
-            } else if(i < input.length() - 1) {
-                roll = input[i+1] - '0';
-                score += 10 + roll;
-                i++; 
+            } else if (i + 2 < input.length()) {
+                score += input[i+1] - '0' + input[i+2] - '0';
+                i+=2; 
             }
         } else if(input[i] == '/') { 
             score += 10 - (input[i+1] - '0' + input[i+2] - '0');
             i+=2; 
         } else {
-            int roll = input[i] - '0';
-            if(i < input.length() - 1) {
-                if(input[i+1] == '/') { 
-                    score += roll;
-                    i++; 
-                } else {
-                    int nextRoll = input[i+1] - '0';
-                    score += roll + nextRoll;
-                    i++; 
-                }
+            int roll1 = input[i] - '0';
+            if(i + 1 < input.length() && input[i+1] == '/') { 
+                score += roll1 + 10 - (input[i+2] - '0' + input[i+3] - '0');
+                i+=3; 
             } else {
-                score += roll;
+                int roll2 = input[i+1] - '0';
+                score += roll1 + roll2;
+                i++; 
             }
         }
     }
