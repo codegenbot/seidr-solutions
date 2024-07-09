9a30f82469a35ebac4f5989d3f681d0a7e21339b
beep boop <githubbot@vadim.me>
2024-07-09 11:00:30 +0200

    Solution 7 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j8w2522w Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,31 +1,23 @@
-#include<string>
+#include <string>
+#include <vector>
+
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = 0, b = 0, c = 0, d = 0;
-    for(int i = 0; i < x.length(); i++){
-        if(x[i] == '/') break;
-        if(i > 0) a = a * 10 + (x[i] - '0');
-    }
-    for(int i = x.length() - 1; i > x.find('/'); i--){
-        a = a * 10 + (x[i] - '0');
-    }
+    int a = stoi(getNumerator(x));
+    int b = stoi(getDenominator(x));
+    int c = stoi(getNumerator(n));
+    int d = stoi(getDenominator(n));
 
-    for(int i = 0; i < n.length(); i++){
-        if(n[i] == '/') break;
-        if(i > 0) b = b * 10 + (n[i] - '0');
-    }
-    for(int i = n.length() - 1; i > n.find('/'); i--){
-        b = b * 10 + (n[i] - '0');
-    }
-
-    c = a / gcd(a, b);
-    d = b / gcd(b, a);
-
-    return c == a && d == b;
+    return (a * d) == (b * c);
 }
 
-int gcd(int a, int b) {
-    if(b == 0) return a;
-    return gcd(b, a % b);
+string getNumerator(string s) {
+    size_t pos = s.find('/');
+    return s.substr(0, pos);
+}
+
+string getDenominator(string s) {
+    size_t pos = s.find('/');
+    return s.substr(pos + 1);
 }
