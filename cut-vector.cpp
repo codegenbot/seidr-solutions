0b5d0bf4f9dbb22efb5821be14eb4c56bb37d5e8
beep boop <githubbot@vadim.me>
2024-07-09 18:49:07 +0200

    Solution 5 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/6rq7tv1u Prompt: 051d2dfa-a2d4-4655-bf2b-c2fb4e360b67.cpp: In function ‘std::vector<std::vector<i nt> > cutVector(std::vector<int>)’: 051d2dfa-a2d4-4655-bf2b-c2fb4e360b67.cpp:8:19: error: ‘INT_MAX’ was not declared  in this scope     8 |     int minDiff = INT_MAX;       |                   ^~~~~~~ 051d2dfa-a2d4-4655-bf2b-c2fb4e360b67.cpp:3:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     2 | #include <vector>   +++ |+#include <climits>     3 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,33 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
+    int minDiff = INT_MAX;
     for (int i = 0; i < n; i++) {
-        if (i == 0 || v[i] != v[0]) {
-            int leftSum = 0, rightSum = 0;
-            int minDiff = INT_MAX, bestCut = -1;
-            for (int j = 0; j <= i; j++) {
-                leftSum += v[j];
-            }
-            for (int j = i + 1; j < n; j++) {
-                rightSum += v[j];
-            }
-            if (leftSum == rightSum) {
-                res[0] = vector<int>(v.begin(), v.begin() + i + 1);
-                res[1] = vector<int>(v.begin() + i, v.end());
-                return res;
-            } else if (abs(leftSum - rightSum) < minDiff) {
-                minDiff = abs(leftSum - rightSum);
-                bestCut = i;
-            }
+        int leftSum = 0, rightSum = 0;
+        for (int j = 0; j <= i; j++) {
+            leftSum += v[j];
+        }
+        for (int j = i + 1; j < n; j++) {
+            rightSum += v[j];
+        }
+        if (abs(leftSum - rightSum) < minDiff) {
+            minDiff = abs(leftSum - rightSum);
+            res[0] = vector<int>(v.begin(), v.begin() + i + 1);
+            res[1] = vector<int>(v.begin() + i, v.end());
         }
     }
-    // If no cut found, the whole array is the result
-    res[0] = vector<int>(v.begin(), v.end());
-    res[1].clear();
     return res;
 }
 
