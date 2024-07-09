0b0f09d5bff25064d7efae447306e64284eab497
beep boop <githubbot@vadim.me>
2024-07-09 08:29:00 +0200

    Solution 49 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/x8c0rtpg Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from e9a51209-22b6-4233-9af1-8685937275ad.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -20,6 +20,7 @@ bool pairs_sum_to_zero(std::vector<std::pair<int, int>> l) {
 }
 
 int main() {
-    std::vector<std::pair<int, int>> input = {{-3, 9}, {-1, 1}, {4, -4}, {2, -2}, {31, -31}};
-    return !pairs_sum_to_zero(input);
+    std::vector<std::pair<int, int>> input = std::vector<std::pair<int, int>>{{-3, 0}, {9, -9}, {-1, 1}, {4, -4}, {2, -2}, {31, -31}};
+    assert(pairs_sum_to_zero(input) == false);
+    return 0;
 }
