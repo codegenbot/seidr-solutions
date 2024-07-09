fa124452ef5c9a52d4ede62189ffdf487f0bde60
beep boop <githubbot@vadim.me>
2024-07-09 21:27:54 +0200

    Solution 70 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/juk3nsjv Prompt: 2eb9babb-b2b2-4d1b-a10c-7201c292fd5c.cpp:3:19: error: ‘vector’ does not name a t ype     3 | bool issame(const vector<float>& a,const vector<float>&b){       |                   ^~~~~~ 2eb9babb-b2b2-4d1b-a10c-7201c292fd5c.cpp:3:25: error: expected ‘,’ or ‘...’ befo re ‘<’ token     3 | bool issame(const vector<float>& a,const vector<float>&b){       |                         ^ 2eb9babb-b2b2-4d1b-a10c-7201c292fd5c.cpp: In function ‘bool issame(int)’: 2eb9babb-b2b2-4d1b-a10c-7201c292fd5c.cpp:4:24: error: ‘a’ was not declared in th is scope     4 |     for(int i = 0; i < a.size(); i++){       |                        ^ 2eb9babb-b2b2-4d1b-a10c-7201c292fd5c.cpp:5:20: error: ‘b’ was not declared in th is scope     5 |         if(a[i] != b[i]){       |                    ^ 2eb9babb-b2b2-4d1b-a10c-7201c292fd5c.cpp: At global scope: 2eb9babb-b2b2-4d1b-a10c-7201c292fd5c.cpp:12:1: error: ‘vector’ does not name a t ype    12 | vector<float> sort_even(vector<float> l){       | ^~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-bool isSame(const vector<float>& a,const vector<float>&b){
+bool issame(const vector<float>& a,const vector<float>&b){
     for(int i = 0; i < a.size(); i++){
         if(a[i] != b[i]){
             return false;
@@ -11,7 +11,7 @@ bool isSame(const vector<float>& a,const vector<float>&b){
     return true;
 }
 
-vector<float> sortEven(vector<float> l){
+vector<float> sort_even(vector<float> l){
     vector<float> l_prime;
     for(int i = 0; i < l.size(); i++){
         if(i % 2 == 0){
