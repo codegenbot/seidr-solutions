c3c2af8f69b46fa7ebb6ab5e791f7fbb332548ff
beep boop <githubbot@vadim.me>
2024-07-09 21:19:20 +0200

    Solution 14 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/xub5v5hi Prompt: 6288d97e-1842-4bd4-9003-b00a6be94cae.cpp:3:18: error: ‘string’ was not declared in this scope     3 | int bowlingScore(string input) {       |                  ^~~~~~ 6288d97e-1842-4bd4-9003-b00a6be94cae.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | using namespace std;

--- bowling.cpp
+++ bowling.cpp
@@ -1,35 +1,30 @@
+```cpp
 #include <string>
-using namespace std;
 
-int bowlingScore(string input) {
-    int score = 0;
-    int i = 0;
-    for (int frame = 1; frame <= 10; frame++) {
-        if (input[i] == 'X') {
-            score += 30;
-            i++;
-            continue;
-        } else if (input.substr(i, 1) == "X") {
-            i++;
-            int nextRoll = stoi(input.substr(i, 1));
-            score += 10 + nextRoll;
-            i++;
-            continue;
-        } else if (input[i] == '/') {
-            i++;
-            int nextTwo = stoi(input.substr(i, 2));
-            score += 10 + nextTwo / 2;
-            i += 2;
-            continue;
+namespace std {
+    using namespace std;
+
+    int bowlingScore(string input) {
+        int score = 0;
+        for (int i = 0; i < 10; i++) {
+            if (input[i] == 'X') {
+                score += 30;
+            } else if (input[i] == '/') {
+                int nextTwo = stoi(input.substr(i + 1, 2));
+                score += 10 + nextTwo;
+                i++;
+            } else {
+                int currentRoll = stoi(input.substr(i, 2));
+                if (currentRoll < 10) {
+                    score += currentRoll;
+                    i++;
+                } else {
+                    int first = stoi(input.substr(i, 1));
+                    int second = stoi(input.substr(i + 1, 1));
+                    score += first + second;
+                }
+            }
         }
-        int currentRoll1 = stoi(input.substr(i, 1));
-        i++;
-        if (input[i] == '0') {
-            int currentRoll2 = stoi(input.substr(i, 1));
-            score += currentRoll1 + currentRoll2;
-            i++;
-        } else {
-            score += currentRoll1;
-        }
+        return score;
     }
-    return score;
+}
