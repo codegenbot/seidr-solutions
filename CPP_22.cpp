c86009215584239ce26c9e3399e017a9f024050d
beep boop <githubbot@vadim.me>
2024-07-09 20:59:10 +0200

    Solution 40 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/v0dpr46q Prompt: 4e1f0983-764d-4c34-aa75-cff565177bb9.cpp: In function ‘std::vector<int> filter_i ntegers(std::__cxx11::list<std::any>)’: 4e1f0983-764d-4c34-aa75-cff565177bb9.cpp:16:34: error: request for member ‘type’  in ‘std::any_cast<int>((* & value))’, which is of non-class type ‘int’    16 |         if (any_cast<int>(value).type() == typeid(int)) {       |                                  ^~~~ 4e1f0983-764d-4c34-aa75-cff565177bb9.cpp: At global scope: 4e1f0983-764d-4c34-aa75-cff565177bb9.cpp:33:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    33 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 4e1f0983-764d-4c34-aa75-cff565177bb9.cpp:9:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     9 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -6,30 +6,24 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return equal(a.begin(), a.end(), b.begin(), b.end());
+bool isSame(vector<int> a, vector<int> b) {
+    return a.size() == b.size() && all_of(a.begin(), a.end(), [&b](int x) {return find(b.begin(), b.end(), x) != b.end();});
 }
 
-vector<int> filter_integers(list<any> values) {
+vector<int> filterIntegers(list<any> values) {
     vector<int> result;
-    unordered_map<int, int> map;
     for (const auto& value : values) {
-        if (any_cast<int>(value).is_of_type<type_index::int_>()) {
+        if (any_cast<int>(value)) {
             int val = any_cast<int>(value);
-            if (map.count(val)) {
-                bool same = false;
-                for (int num : result) {
-                    if (issame({num}, {val})) {
-                        same = true;
-                        break;
-                    }
+            bool same = false;
+            for (int num : result) {
+                if (isSame({num}, {val})) {
+                    same = true;
+                    break;
                 }
-                if (!same)
-                    continue;
-            } else {
-                map[val]++;
-                result.push_back(val);
             }
+            if (!same)
+                result.push_back(val);
         }
     }
     return result;
