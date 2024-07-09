52c1ceecee68f37a125a896ad2fead0742e0e46e
beep boop <githubbot@vadim.me>
2024-07-09 02:44:23 +0200

    Solution 6 of CPP/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ryo1pk4z Prompt:    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ea37f7c3-d046-4d08-9d5e-3706d8bf7dca.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ea37f7c3-d046-4d08-9d5e-3706d8bf7dca.cpp:21:45: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ea37f7c3-d046-4d08-9d5e-3706d8bf7dca.cpp:20: ea37f7c3-d046-4d08-9d5e-3706d8bf7dca.cpp: In function ‘int main()’: ea37f7c3-d046-4d08-9d5e-3706d8bf7dca.cpp:30:20: error: ‘words_string’ was not de clared in this scope    30 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |                    ^~~~~~~~~~~~ ea37f7c3-d046-4d08-9d5e-3706d8bf7dca.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~

--- CPP_101.cpp
+++ CPP_101.cpp
@@ -1,35 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <cassert>
-
-using namespace std;
-
-vector<string> words_string(string s){
-    vector<string> words;
-    stringstream ss(s);
-    string word;
-    while (getline(ss, word, ' ')) {
-        size_t pos = word.find(',');
-        if (pos != string::npos)
-            word.erase(pos, 1);
-        words.push_back(word);
-    }
-    return words;
-}
+vector<string> words_string(string s);
 
 bool issame(vector<string> a, vector<string> b){
-    if(a.size() != b.size())
-        return false;
-    for(size_t i = 0; i < a.size(); ++i){
-        if(a[i] != b[i])
-            return false;
-    }
-    return true;
+    return a == b;
 }
 
 int main(){
     assert(issame(words_string("ahmed     , gamal"), {"ahmed", "gamal"}));
     return 0;
+}
+
+vector<string> words_string(string s){
+    vector<string> words;
+    string word = "";
+    for(char c : s){
+        if(c == ' ' || c == ','){
+            if(word != ""){
+                words.push_back(word);
+                word = "";
+            }
+        } else {
+            word += c;
+        }
+    }
+    if(word != ""){
+        words.push_back(word);
+    }
+    return words;
 }
