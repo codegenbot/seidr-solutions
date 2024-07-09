c4db3f0c44548dfe8a033c9008a2aa47608d0072
beep boop <githubbot@vadim.me>
2024-07-09 11:11:36 +0200

    Solution 27 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qnqbku9p Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: ac173fc3-340a-471e-b6fc-d0d0e9b4f8fc.cpp:22:35:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -19,7 +19,6 @@ std::string int_to_roman(int number) {
 }
 
 int main() {
-    std::string roman = int_to_roman(1000);
-    std::cout << (const std::string&) roman;
+    std::cout << int_to_roman(1000);
     return 0;
 }
