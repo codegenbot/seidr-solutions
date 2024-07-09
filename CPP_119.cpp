29edb698c0791d17d6fc83b12e1f7b80d6f053fa
beep boop <githubbot@vadim.me>
2024-07-09 16:52:14 +0200

    Solution 9 of CPP/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/duee4x3g Prompt: 59056958-6cd1-463f-9850-dd75d1b2883a.cpp: In function ‘int main()’: 59056958-6cd1-463f-9850-dd75d1b2883a.cpp:17:5: error: ‘assert’ was not declared in this scope    17 |     assert(match_parens({")", "("}) == "Yes");       |     ^~~~~~ 59056958-6cd1-463f-9850-dd75d1b2883a.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <string>   +++ |+#include <cassert>     3 | #include <initializer_list> 59056958-6cd1-463f-9850-dd75d1b2883a.cpp: At global scope: 59056958-6cd1-463f-9850-dd75d1b2883a.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 59056958-6cd1-463f-9850-dd75d1b2883a.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_119.cpp
+++ CPP_119.cpp
@@ -1,5 +1,7 @@
+#include <cassert>
 #include <vector>
 #include <string>
+#include <initializer_list>
 
 std::string match_parens(std::vector<std::string> lst) {
     int open = 0, close = 0;
@@ -13,6 +15,6 @@ std::string match_parens(std::vector<std::string> lst) {
 }
 
 int main() {
-    assert(match_parens({{"("}, {")"} }) == "Yes" );
+    assert(match_parens({")", "("}) == "Yes");
     return 0;
 }
