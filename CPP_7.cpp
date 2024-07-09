d2ff55f57d26939630100f79c8558ec14e87c1f8
beep boop <githubbot@vadim.me>
2024-07-09 03:07:06 +0200

    Solution 92 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/863x6wqt Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 0f744d12-43bf-4aa9-a613-891bb3b59373.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,11 +1,12 @@
+```cpp
 #include <vector>
 #include <algorithm>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::vector<std::string>> b) {
-    return a == b[0];
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    return sort(a.begin(), a.end()) == sort(b.begin(), b.end());
 }
 
 int main() {
-    assert(issame({"grunt", "trumpet", "prune", "gruesome"}, {{"grunt", "prune"}}));
+    assert(issame({"grunt", "trumpet", "prune", "gruesome"}, {"run"}, {"grunt", "prune"}));
 }
