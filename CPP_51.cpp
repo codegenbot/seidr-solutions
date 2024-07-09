ebae020cbfeedda83537a85f179274be72e4564c
beep boop <githubbot@vadim.me>
2024-07-09 05:27:28 +0200

    Solution 16 of CPP/51, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xaxcenii Prompt: 300e6028-8313-4d36-b654-7d7d04ec0e91.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string remove_vowels(string text){       | ^~~~~~ In file included from 300e6028-8313-4d36-b654-7d7d04ec0e91.cpp:10: 300e6028-8313-4d36-b654-7d7d04ec0e91.cpp: In function ‘int main()’: 300e6028-8313-4d36-b654-7d7d04ec0e91.cpp:12:13: error: ‘remove_vowels’ was not d eclared in this scope    12 |     assert (remove_vowels("ybcd") == "ybcd");       |             ^~~~~~~~~~~~~

--- CPP_51.cpp
+++ CPP_51.cpp
@@ -1,12 +1,18 @@
-string remove_vowels(string text){
-    string result;
-    for(int i = 0; i < text.length(); i++){
-        if(text[i] != 'a' && text[i] != 'e' && text[i] != 'i' 
-           && text[i] != 'o' && text[i] != 'u' && text[i] != 'A' 
-           && text[i] != 'E' && text[i] != 'I' && text[i] != 'O'
-           && text[i] != 'U'){
-            result += text[i];
-        }
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+std::string remove_vowels(std::string text){
+    std::string result = "";
+    for(char c : text){
+        if(strchr("aeiouAEIOU",c) == NULL)
+            result += c;
     }
     return result;
+}
+
+int main(){
+    assert (remove_vowels("ybcd") == "ybcd");
+    return 0;
 }
