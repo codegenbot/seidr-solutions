afd8531157aa400659db5203cee7000a5e789416
beep boop <githubbot@vadim.me>
2024-07-09 04:13:03 +0200

    Solution 32 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7b288k16 Prompt:       |                              ^~~~~~ 7b63def2-cd35-4f55-9960-8b427b1919e3.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 7b63def2-cd35-4f55-9960-8b427b1919e3.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7b63def2-cd35-4f55-9960-8b427b1919e3.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 7b63def2-cd35-4f55-9960-8b427b1919e3.cpp:29: 7b63def2-cd35-4f55-9960-8b427b1919e3.cpp: In function ‘int main()’: 7b63def2-cd35-4f55-9960-8b427b1919e3.cpp:39:20: error: ‘reverse_delete’ was not declared in this scope    39 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 7b63def2-cd35-4f55-9960-8b427b1919e3.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,8 +1,36 @@
-```cpp
+#include <algorithm>
+#include <vector>
+
 bool issame(vector<string> a, vector<string> b) {
-    return a == b;
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
 }
 
-int main() {
-    assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
+vector<string> reverse_delete(string s, string c) {
+    vector<string> result;
+    string temp = "";
+    for (char ch : s) {
+        bool found = false;
+        for (char cc : c) {
+            if (ch == cc) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            temp += ch;
+        }
+    }
+    result.push_back(temp);
+    string reversed = temp;
+    reverse(reversed.begin(), reversed.end());
+    if (temp == reversed) {
+        result.push_back("True");
+    } else {
+        result.push_back("False");
+    }
+    return result;
 }
