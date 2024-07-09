dbee2c0d0aee2e38953a6513d60128f69b747b82
beep boop <githubbot@vadim.me>
2024-07-09 11:27:31 +0200

    Solution 97 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/w1ib729b Prompt: 2eb3261a-ac41-41f2-b222-bc50c38a7e2b.cpp:1:48: error: expected primary-expressio n before ‘float’     1 | bool has_close_elements(vector<float> numbers, float threshold) {       |                                                ^~~~~ 2eb3261a-ac41-41f2-b222-bc50c38a7e2b.cpp:1:63: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool has_close_elements(vector<float> numbers, float threshold) {       |                                                               ^ 2eb3261a-ac41-41f2-b222-bc50c38a7e2b.cpp: In function ‘int main()’: 2eb3261a-ac41-41f2-b222-bc50c38a7e2b.cpp:14:5: error: ‘vector’ was not declared in this scope    14 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 2eb3261a-ac41-41f2-b222-bc50c38a7e2b.cpp:14:12: error: expected primary-expressi on before ‘float’    14 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~ In file included from 2eb3261a-ac41-41f2-b222-bc50c38a7e2b.cpp:12: 2eb3261a-ac41-41f2-b222-bc50c38a7e2b.cpp:15:31: error: ‘has_close_elements’ cann ot be used as a function    15 |     assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false) ;       |             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,6 +1,4 @@
 #include <vector>
-#include <cmath>
-
 bool has_close_elements(vector<float> numbers, float threshold) {
     for(int i = 0; i < numbers.size(); i++) {
         for(int j = i + 1; j < numbers.size(); j++) {
