1a739ee09d34e214722bfeed7310550c4b2a7124
beep boop <githubbot@vadim.me>
2024-07-09 05:47:46 +0200

    Solution 9 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/lqdbl84j Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6170ea03-1c83-4346-a756-0f3c0c972 e37.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6170ea03-1c83-4346-a756-0f3c0c972 e37.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,12 +1,7 @@
-def sort_even(l: list):
+def sort_even(lst):
     return [
-        (
-            x
-            if i % 2 != 0
-            else sorted(
-                [y for j, y in enumerate(sorted(l)) if j % 2 == 0],
-                key=lambda z: [k for k, m in enumerate(l) if m == z].index,
-            )[i // 2]
-        )
-        for i, x in enumerate(l)
+        x if i % 2 != 0 else y
+        for i, x in enumerate(sorted(lst))
+        for j, y in enumerate(lst)
+        if (j + 1) % 2 == 0
     ]
