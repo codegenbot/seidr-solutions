ee2f608b33feb41e84dff1a4b2a1b97142b03b71
beep boop <githubbot@vadim.me>
2024-07-09 20:58:12 +0200

    Solution 86 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/zf2y1y40 Prompt:       | ^~~~~~~~~~~~~ fc65adde-0b00-4d48-aa67-5c13d126c048.cpp:21:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    21 | int main(){       |         ^~ fc65adde-0b00-4d48-aa67-5c13d126c048.cpp:21:9: note: remove parentheses to defau lt-initialize a variable    21 | int main(){       |         ^~       |         -- fc65adde-0b00-4d48-aa67-5c13d126c048.cpp:21:9: note: or replace parentheses with  braces to value-initialize a variable fc65adde-0b00-4d48-aa67-5c13d126c048.cpp:21:11: error: a function-definition is not allowed here before ‘{’ token    21 | int main(){       |           ^ fc65adde-0b00-4d48-aa67-5c13d126c048.cpp:25:2: error: expected ‘}’ at end of inp ut    25 | }       |  ^ fc65adde-0b00-4d48-aa67-5c13d126c048.cpp:13:12: note: to match this ‘{’    13 | int main() {       |            ^

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -11,8 +11,7 @@ bool has_close_elements(vector<float> numbers, float threshold){
     return false;
 }
 
-int main 
-{
+int main {
     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
     bool result = has_close_elements(a, 0.5);
     cout << "Result: " << (result ? "true" : "false") << endl;
