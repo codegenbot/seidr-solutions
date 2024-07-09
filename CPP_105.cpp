4a9c5a41607848df84d217d1ba492c00d414abb0
beep boop <githubbot@vadim.me>
2024-07-09 08:14:59 +0200

    Solution 47 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/siauvrg0 Prompt:       |                              ^~~~~~ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 964a0b5c-562f-4158-92fc-249bca43033b.cpp:50: 964a0b5c-562f-4158-92fc-249bca43033b.cpp: In function ‘int main()’: 964a0b5c-562f-4158-92fc-249bca43033b.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,23 +1,62 @@
-```cpp
 bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-vector<bool> by_length(vector<int> arr) {
-    vector<bool> result;
+vector<vector<string>> splitByLength(vector<int> arr) {
+    map<int, vector<string>> result;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            bool same = false;
-            for (string name : {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}) {
-                if (stoi(name.substr(0, 3)) == num) {
-                    same = true;
+            string name = "";
+            switch (num) {
+                case 1:
+                    name = "One";
                     break;
-                }
+                case 2:
+                    name = "Two";
+                    break;
+                case 3:
+                    name = "Three";
+                    break;
+                case 4:
+                    name = "Four";
+                    break;
+                case 5:
+                    name = "Five";
+                    break;
+                case 6:
+                    name = "Six";
+                    break;
+                case 7:
+                    name = "Seven";
+                    break;
+                case 8:
+                    name = "Eight";
+                    break;
+                case 9:
+                    name = "Nine";
+                    break;
             }
-            result.push_back(same);
-        } else {
-            result.push_back(false);
+            if (result.count(num)) {
+                result[num].push_back(name);
+            } else {
+                vector<string> temp = {name};
+                result[num] = temp;
+            }
         }
     }
-    return result;
+
+    map<int, vector<string>> sortedResult = result;
+    for (auto& pair : sortedResult) {
+        sort(pair.second.begin(), pair.second.end());
+    }
+
+    vector<vector<string>> finalResult;
+    for (auto& pair : sortedResult) {
+        reverse(pair.second.begin(), pair.second.end());
+        if (!issame(finalResult, {pair.second})) {
+            finalResult.push_back({pair.second});
+        }
+    }
+
+    return finalResult;
 }
