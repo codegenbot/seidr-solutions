da485bb960aa556f6fc2ab90e01b876f33133a42
beep boop <githubbot@vadim.me>
2024-07-09 07:41:33 +0200

    Solution 2 of CPP/50, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/rcsjorwc Prompt: d46a8ac9-73ca-4ba6-afcf-5f9254ff7928.cpp: In function ‘std::string decode_shift( std::string)’: d46a8ac9-73ca-4ba6-afcf-5f9254ff7928.cpp:19:38: error: expected ‘,’ or ‘;’ befor e ‘%’ token    19 |         int w(((int)s[i])-(int)'a'+5)%26+(int)'a';       |                                      ^

--- CPP_50.cpp
+++ CPP_50.cpp
@@ -16,7 +16,7 @@ string decode_shift(string s){
     string out;
     int i;
     for (i=0;i<s.length();i++) {
-        int w(((int)s[i])-(int)'a'+5)%26+(int)'a';
+        int w = (((int)s[i]) - ((int)'a') + 5) % 26 + ((int)'a');
         out=out+(char)w;
     }
     return out;
