5a2d2d182aa64366b3004653b8ad06318b6405d6
beep boop <githubbot@vadim.me>
2024-07-09 18:21:04 +0200

    Solution 97 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/6w4aoj8e Prompt:       |                    ^~~ cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp:18: cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp: In function ‘int main()’: cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp:28:20: error: ‘even_odd_palindrome’ was  not declared in this scope    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ cd3ddaec-f8da-4ad2-967a-54c679f6aa22.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,15 +1,15 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
+bool issame(vector<int> a, vector<int> b) {
+    return a[0] == b[0] && a[1] == b[1];
 }
 
-std::vector<int> solve(int n) {
-    std::vector<int> res(2, 0);
+vector<int> solve(int n) {
+    vector<int> res(2, 0);
     for (int i = 1; i <= n; ++i) {
-        std::string s = std::to_string(i);
-        std::string rev = s;
-        std::reverse(rev.begin(), rev.end());
+        string s = to_string(i);
+        string rev = s;
+        reverse(rev.begin(), rev.end());
         if (s == rev) {
             if (i % 2 == 0) {
                 res[0]++;
