f5e1803403896abd3daa149d8d889d5f1c9cce61
beep boop <githubbot@vadim.me>
2024-07-09 23:52:55 +0200

    Solution 44 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ielra1zr Prompt:       |                              ^~~~~~ 069408d8-a8cb-4551-a6b1-22a7e27fa5fd.cpp:24:37: error: ‘string’ was not declared  in this scope    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 069408d8-a8cb-4551-a6b1-22a7e27fa5fd.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 069408d8-a8cb-4551-a6b1-22a7e27fa5fd.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 069408d8-a8cb-4551-a6b1-22a7e27fa5fd.cpp:23: 069408d8-a8cb-4551-a6b1-22a7e27fa5fd.cpp: In function ‘int main()’: 069408d8-a8cb-4551-a6b1-22a7e27fa5fd.cpp:33:20: error: ‘numerical_letter_grade’ was not declared in this scope    33 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 069408d8-a8cb-4551-a6b1-22a7e27fa5fd.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,6 +1,3 @@
-#include <vector>
-#include <string>
-
 vector<string> numerical_letter_grade(vector<float> grades){
     vector<string> letter_grades;
     for(float g: grades){
@@ -8,7 +5,7 @@ vector<string> numerical_letter_grade(vector<float> grades){
         if(g >= 4.0) letter = "A+";
         else if(g > 3.7) letter = "A";
         else if(g > 3.3) letter = "A-";
-        else if(g > 3.0) letter = "B+";
+        else if(g > 3.0) letter = "B+");
         else if(g > 2.7) letter = "B";
         else if(g > 2.3) letter = "B-";
         else if(g > 2.0) letter = "C+";
