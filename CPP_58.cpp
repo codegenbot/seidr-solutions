9f8ebecb47dff377eafd11b4fe304cd81aadafa9
beep boop <githubbot@vadim.me>
2024-07-09 08:18:12 +0200

    Solution 24 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5av973i7 Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: af1e5da7-aed8-4753-b97f-5868394d2234.cpp:36:22:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -11,25 +11,25 @@ int main() {
 
     for(int i = 0; i < n1; i++) {
         std::string x;
-        std::cin >> x;
+        std::getline(std::cin, x);
         s1.insert(x);
     }
 
     for(int i = 0; i < m1; i++) {
         std::string y;
-        std::cin >> y;
+        std::getline(std::cin, y);
         s2.insert(y);
     }
 
     std::vector<std::string> diff(s1.begin(), s1.end());
     set_difference(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                    std::back_inserter(diff));
+        std::back_inserter(diff));
 
     auto intersection = s1;
     s1.clear();
     std::vector<std::string> intersect(s2.begin(), s2.end());
-    set_intersection(intersection.begin(), intersection.end(), s2.begin(), s2.end(),
-                     std::back_inserter(intersect));
+    set_intersection(s2.begin(), s2.end(), intersection.begin(), intersection.end(),
+        std::back_inserter(intersect));
 
     std::cout << "Difference: ";
     for (const auto& i : diff) {
