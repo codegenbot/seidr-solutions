18e6349b2ca659f2ce2e4e2de8752c271310c47a
beep boop <githubbot@vadim.me>
2024-07-09 17:35:22 +0200

    Solution 47 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/gpojo9to Prompt:       |                    ^~~ db6bcc45-97e4-4abd-a19c-b0aa2329de60.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ db6bcc45-97e4-4abd-a19c-b0aa2329de60.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ db6bcc45-97e4-4abd-a19c-b0aa2329de60.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from db6bcc45-97e4-4abd-a19c-b0aa2329de60.cpp:10: db6bcc45-97e4-4abd-a19c-b0aa2329de60.cpp: In function ‘int main()’: db6bcc45-97e4-4abd-a19c-b0aa2329de60.cpp:20:20: error: ‘sort_array’ was not decl ared in this scope    20 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ db6bcc45-97e4-4abd-a19c-b0aa2329de60.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,17 +1,17 @@
 #include <algorithm>
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
+bool issame(std::vector<int> a, std::vector<int> b){
+    if(a.size() != b.size()) return false;
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]) return false;
     }
     return true;
 }
 
 std::vector<int> sort_array(std::vector<int> array) {
-    if (array.empty()) return array;
-    if ((array.front() + array.back()) % 2 == 0) {
+    if(array.empty()) return array;
+    if((array.front() + array.back()) % 2 == 0) {
         std::sort(array.begin(), array.end(), std::greater<int>());
     } else {
         std::sort(array.begin(), array.end());
