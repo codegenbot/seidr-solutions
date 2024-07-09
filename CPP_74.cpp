a35511b359b7921c155916445962e487c076f6e7
beep boop <githubbot@vadim.me>
2024-07-09 17:13:07 +0200

    Solution 81 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/rhe4sp6u Prompt: ng<char>*; _ForwardIterator = std::__cxx11::basic_string<char>*; _Tp = std::__cx x11::basic_string<char>]’ /usr/include/c++/11/bits/stl_vector.h:1585:33:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const std::__cxx11::basic_string<char >*; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11 ::basic_string<char> >]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = s td::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11::basic_str ing<char> >; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<std::__cx x11::basic_string<char> >]’ dfe876bd-8f95-4c8a-86f5-2e432ecd65a2.cpp:25:26:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -46,17 +46,26 @@ int main() {
     std::cout << "Enter 2 lists of fruits separated by spaces: ";
     std::string user_input1, user_input2;
     std::getline(std::cin, user_input1);
-    std::getline(std::cin, user_input2);
+    std::getline(std::cin, user_input2;
 
-    std::istringstream iss(user_input1);
-    std::string str;
-    while (std::getline(iss >> std::skipws >> str >> std::ws >> std::skipws)) {
-        lst1.push_back(str);
+    size_t i = 0;
+    for (const auto& str : user_input1) {
+        if (str == ' ') {
+            i++;
+        } else {
+            lst1.push_back(user_input1.substr(i, i != 0 ? i - 1 : i));
+            i++;
+        }
     }
 
-    std::istringstream iis(user_input2);
-    while (std::getline(iis >> std::skipws >> str >> std::ws >> std::skipws)) {
-        lst2.push_back(str);
+    i = 0;
+    for (const auto& str : user_input2) {
+        if (str == ' ') {
+            i++;
+        } else {
+            lst2.push_back(user_input2.substr(i, i != 0 ? i - 1 : i));
+            i++;
+        }
     }
 
     if (!lst1.empty() && !lst2.empty()) {
