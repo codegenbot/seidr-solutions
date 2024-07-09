7e806e18bc6d71c0de8c1d3f4206a17a7e5e2e57
beep boop <githubbot@vadim.me>
2024-07-09 04:34:46 +0200

    Solution 88 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8ahd9aop Prompt:  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 731351f2-41a5-46f5-9b26-855355ce1430.cpp:26:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 731351f2-41a5-46f5-9b26-855355ce1430.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 731351f2-41a5-46f5-9b26-855355ce1430.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 731351f2-41a5-46f5-9b26-855355ce1430.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 731351f2-41a5-46f5-9b26-855355ce1430.cpp:25: 731351f2-41a5-46f5-9b26-855355ce1430.cpp: In function ‘int main()’: 731351f2-41a5-46f5-9b26-855355ce1430.cpp:35:20: error: ‘total_match’ was not dec lared in this scope    35 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,10 +1,10 @@
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if (a.size() != b.size()) return false;
+bool issame(std::vector<std::string> a, std::vector<std::string> vec2) {
+    if (a.size() != vec2.size()) return false;
     for (size_t i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
+        if (a[i] != vec2[i]) return false;
     }
     return true;
 }
@@ -23,9 +23,37 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
 }
 
 int main() {
-    vector<string> b;
-    vector<string> a = {"this"};
-    vector<string> c = {};
-    assert(issame(total_match(a, c), c));
-    return 0;
+    int b; // declare variable b
+    vector<string> lst3, lst4;
+    cin >> b;  // read value of 'b'
+    
+    string temp;
+    for(int i = 0; i < b; i++) { 
+        cin >> temp;
+        if(i == 0) {
+            lst1.push_back(temp);
+        } else if(i == 1) {
+            lst2.push_back(temp);
+        } else if(i > 1) {
+            if(issame(lst1, lst2)) {
+                if(sum_match(total_match(lst1, lst2)) >= sum_match(lst3) && sum_match(total_match(lst1, lst2)) <= sum_match(lst4)) {
+                    lst3 = total_match(lst1, lst2);
+                } else {
+                    lst4 = total_match(lst1, lst2);
+                }
+            } 
+        }
+    }
+    
+    if(!issame(lst1, lst2)) { // check if lists are the same
+        cout << "The two lists are not identical." << endl;
+    } else {
+        if(sum_match(total_match(lst1, lst2)) >= sum_match(lst3) && sum_match(total_match(lst1, lst2)) <= sum_match(lst4)) {
+            cout << "The total match list is the same as one of the lists: " << lst3[0] << endl;
+        } else {
+            cout << "The total match list is not the same as either of the lists." << endl;
+        }
+    }
+    
+    return 0; 
 }
