21934938a5bea4f2300acb49055994f693faba54
beep boop <githubbot@vadim.me>
2024-07-09 20:25:58 +0200

    Solution 61 of indices-of-substring, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/k1y7h1sj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/973547a2-db27-4abc-ba59-d9d43e687 c52.py", line 1     ```     ^ SyntaxError: invalid syntax

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -1,8 +1,2 @@
 def indices_of_substring(text, target):
-    result = []
-    while True:
-        pos = text.find(target)
-        if pos == -1:
-            break
-        result.append(pos)
-        text = text[pos+len(target):]
+    return [i for i in range(len(text)) if text[i:i+len(target)] == target]
