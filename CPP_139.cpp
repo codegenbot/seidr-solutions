d84edd8c485f5539759eccfcbe26058a9a2887ca
beep boop <githubbot@vadim.me>
2024-07-09 21:11:31 +0200

    Solution 97 of CPP/139, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gjtnmr3o Prompt:   368 |   operator==(const error_code& __lhs, const error_condition& __rhs) noex cept       |              ~~~~~~~~~~~~~~~~~~^~~~~ /usr/include/c++/11/system_error:376:3: note: candidate: ‘bool std::operator==(c onst std::error_condition&, const std::error_condition&)’   376 |   operator==(const error_condition& __lhs,       |   ^~~~~~~~ /usr/include/c++/11/system_error:376:37: note:   no known conversion for argumen t 1 from ‘std::string’ {aka ‘std::__cxx11::basic_string<char>’} to ‘const std::e rror_condition&’   376 |   operator==(const error_condition& __lhs,       |              ~~~~~~~~~~~~~~~~~~~~~~~^~~~~ /usr/include/c++/11/system_error:408:3: note: candidate: ‘bool std::operator==(c onst std::error_condition&, const std::error_code&)’   408 |   operator==(const error_condition& __lhs, const error_code& __rhs) noex cept       |   ^~~~~~~~ /usr/include/c++/11/system_error:408:37: note:   no known conversion for argumen t 1 from ‘std::string’ {aka ‘std::__cxx11::basic_string<char>’} to ‘const std::e rror_condition&’   408 |   operator==(const error_condition& __lhs, const error_code& __rhs) noex cept       |              ~~~~~~~~~~~~~~~~~~~~~~~^~~~~

--- CPP_139.cpp
+++ CPP_139.cpp
@@ -1,3 +1,4 @@
+```
 #include <iostream>
 #include <cmath>
 
@@ -5,13 +6,21 @@ std::string special_factorial(int n) {
     if (n <= 1)
         return "1";
 
-    std::string result = "1";
+    std::string result;
     for (int i = 2; i <= n; i++) {
         long long mul_result = 1;
         for (int j = 2; j <= i; j++)
             mul_result *= j;
 
-        result = std::to_string(mul_result) + " * " + result;
+        if(i > 1) 
+            result.insert(0, " * ");
+        
+        std::string str = std::to_string(mul_result);
+        result.insert(0, str + " * ");
+
+        while(result.find(" * 1") != std::string::npos)
+            result.erase(0, 4);
+
     }
     return "n! = " + result;
 }
