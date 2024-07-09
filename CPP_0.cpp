eec249547067a3cf6b9d195fa6f18c4466507f1d
beep boop <githubbot@vadim.me>
2024-07-09 07:39:38 +0200

    Solution 40 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/i5yuyk8j Prompt: 9e06d08a-905c-416b-958f-3b77c5138a00.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 9e06d08a-905c-416b-958f-3b77c5138a00.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <cassert>
 using namespace std;
 
 bool has_close_elements(vector<float> a, float tol) {
@@ -16,10 +15,10 @@ bool has_close_elements(vector<float> a, float tol) {
 
 int main() {
     vector<float> numbers = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f, 0};
-    assert(has_close_elements(numbers, 0.5) == false);
+    cout << (has_close_elements(numbers, 0.5) ? "True" : "False") << endl;
     
     vector<float> numbers2 = {1.1f, 2.2f, 3.1f, 4.1f, 5.1f};
-    assert(has_close_elements(numbers2, 0.5) == false);
+    cout << (has_close_elements(numbers2, 0.5) ? "True" : "False") << endl;
     
     return 0;
 }
