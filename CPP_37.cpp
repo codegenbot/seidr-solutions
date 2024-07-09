74250a9d0bdcbb38a3bf26345b8e377b540fec96
beep boop <githubbot@vadim.me>
2024-07-09 14:47:28 +0200

    Solution 46 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7dqgrwjn Prompt: 64c2d49b-f838-4bfd-a5a8-37ab20b4228d.cpp: In function ‘int main()’: 64c2d49b-f838-4bfd-a5a8-37ab20b4228d.cpp:42:5: error: ‘assert’ was not declared in this scope    42 |     assert(isSame(sort_even(input), expected));       |     ^~~~~~ 64c2d49b-f838-4bfd-a5a8-37ab20b4228d.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <algorithm>   +++ |+#include <cassert>     4 | #include <initializer_list> 64c2d49b-f838-4bfd-a5a8-37ab20b4228d.cpp: At global scope: 64c2d49b-f838-4bfd-a5a8-37ab20b4228d.cpp:55:5: error: redefinition of ‘int main( )’    55 | int main(){       |     ^~~~ 64c2d49b-f838-4bfd-a5a8-37ab20b4228d.cpp:39:5: note: ‘int main()’ previously def ined here    39 | int main() {       |     ^~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -37,9 +37,3 @@ vector<float> sort_even(vector<float> l) {
     }
     return result;
 }
-
-int main() { 
-    vector<float> input({5.0f, 8.0f, -12.0f, 4.0f, 23.0f, 2.0f, 3.0f, 11.0f, 12.0f, -10.0f});
-    vector<float> expected = { -12.0f, 8.0f, 3.0f, 4.0f, 5.0f, 2.0f, 12.0f, 11.0f, 23.0f, -10.0f};
-    assert(isSame(sort_even(input), expected));
-}
