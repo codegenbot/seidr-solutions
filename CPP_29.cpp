dcc3476641db016efcbb1cc14fdfe2d771986751
beep boop <githubbot@vadim.me>
2024-07-09 23:20:33 +0200

    Solution 85 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fxnddfx6 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:10: f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp: In function ‘int main()’: f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,8 +1,15 @@
 bool issame(vector<string> a, vector<string> b) {
     if(a.size() != b.size())
         return false;
-    for(int i = 0; i < a.size(); i++){
-        if(a[i].find(b[i]) == string::npos)
+    for(int i = 0; i < a.size(); i++) {
+        bool match = true;
+        size_t j = 0;
+        while(j < a[i].size()) {
+            if(b[i][j] != a[i][j])
+                break;
+            j++;
+        }
+        if(j < a[i].size())
             return false;
     }
     return true;
