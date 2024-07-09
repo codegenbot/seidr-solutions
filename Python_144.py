5210481a42880568c61aff613c23e28c2116e5c3
beep boop <githubbot@vadim.me>
2024-07-09 03:01:30 +0200

    Solution 5 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ot1gte4m Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8015aa0e-e640-49dd-a691-355f18bab 92a.py", line 11, in <module>     check(simplify)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8015aa0e-e640-49dd-a691-355f18bab 92a.py", line 8, in check     assert simplify("1/5", "1/5") == False, "test13"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test13

--- Python_144.py
+++ Python_144.py
@@ -1,9 +1,10 @@
+```
 def simplify(x, n):
     a, b = map(int, x.split("/"))
     c, d = map(int, n.split("/"))
 
     if (a * d) % (b * c) != 0:
-        return f"{x} / {n}"
+        return x + ' / ' + n
     gcd = lambda a, b: a if not b else gcd(b, a%b)
     g = gcd(gcd(a, b), gcd(c, d))
     return f"{a//g}/{b//g} / {c//g}/{d//g}"
