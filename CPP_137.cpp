e47f256960145ab5dfc2ce8ebcb8ad36c1ec082f
beep boop <githubbot@vadim.me>
2024-07-09 17:21:57 +0200

    Solution 15 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/rlozoors Prompt: 9864a451-7f2a-4409-b523-09c7eac37ea1.cpp:3:10: fatal error: boost/algorithm/stri ng.hpp: No such file or directory     3 | #include <boost/algorithm/string.hpp>       |          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,18 +3,6 @@
 
 using namespace std;
 
-bool compare(int a, int b) {
-    if (a > b) return true;
-    else if (b > a) return false;
-    else return false; // or return true, depending on your needs
-}
-
-bool compareString(string a, string b) {
-    if (stoi(a) > stoi(b)) return true;
-    else if (stoi(b) > stoi(a)) return false;
-    else return false; // or return true, depending on your needs
-}
-
 int main() {
     int a, b;
     cout << "Enter the first number: ";
@@ -22,10 +10,10 @@ int main() {
     cout << "Enter the second number: ";
     cin >> b;
 
-    if (compare(a, b)) {
+    if (a > b) {
         cout << "The larger number is: " << a << endl;
     }
-    else if (!compare(b, a)) {
+    else if (b > a) {
         cout << "The larger number is: " << b << endl;
     }
     else {
