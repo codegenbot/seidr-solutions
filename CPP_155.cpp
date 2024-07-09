8261b037ef5d8fc6c3f029262274636b197a5b98
beep boop <githubbot@vadim.me>
2024-07-09 04:49:06 +0200

    Solution 15 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sqlpuqyj Prompt:       |                    ^~~ fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp:14: fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp: In function ‘int main()’: fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp:24:20: error: ‘even_odd_count’ was not declared in this scope    24 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,23 +1,15 @@
 #include <vector>
 
 std::vector<int> even_odd_count(int num) {
-    std::vector<int> result(2);
-    int countEven = 0;
-    int countOdd = 0;
-    
+    int even = 0;
+    int odd = 0;
     while (num != 0) {
         int digit = num % 10;
-        
         if (digit % 2 == 0)
-            countEven++;
+            even++;
         else
-            countOdd++;
-            
+            odd++;
         num /= 10;
     }
-    
-    result[0] = countEven;
-    result[1] = countOdd;
-    
-    return result;
+    return {even, odd};
 }
