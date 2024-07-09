79a16913affc2d0841e1dcb60e385b6a24fbe47b
beep boop <githubbot@vadim.me>
2024-07-09 07:46:05 +0200

    Solution 25 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/qeru7xja Prompt: 1d80e441-b24d-4747-8410-3f1788e76489.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 1d80e441-b24d-4747-8410-3f1788e76489.cpp:29: 1d80e441-b24d-4747-8410-3f1788e76489.cpp: In function ‘int main()’: 1d80e441-b24d-4747-8410-3f1788e76489.cpp:31:13: error: ‘skjkasdkd’ was not decla red in this scope    31 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ^~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,14 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int skjkasdkd(vector<int> lst){
+    int maxPrime = 0;
+    for(int num : lst) {
+        if(isPrime(num)) {
+            if(num > maxPrime)
+                maxPrime = num;
+        }
+    }
+    int sumOfDigits = 0;
+    while(maxPrime > 0) {
+        sumOfDigits += maxPrime % 10;
+        maxPrime /= 10;
+    }
+    return sumOfDigits;
+}
+
+bool isPrime(int n) {
+    if(n <= 1)
+        return false;
+    for(int i = 2; i * i <= n; i++) {
+        if(n % i == 0)
+            return false;
+    }
+    return true;
+
 int main() {
-    int n; 
     vector<int> lst;
-    cout << "Enter the number of elements in the list: ";
-    cin >> n;
-    for(int i = 0; i < n; i++) {
-        int num;
-        cout << "Enter element " << i + 1 << ": ";
+    int num;
+    while(true) {
+        cout << "Enter a number (-1 to stop): ";
         cin >> num;
+        if(num == -1)
+            break;
         lst.push_back(num);
     }
-    cout << "Sum of digits of the maximum prime number in the list is: " << skjkasdkd(lst) << endl;
+    cout << "Sum of digits of max prime in the list: " << skjkasdkd(lst) << endl;
     return 0;
 }
