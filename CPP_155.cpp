f12c9573651c89d1abc3d1c2a10d33bf29552c9a
beep boop <githubbot@vadim.me>
2024-07-09 03:03:54 +0200

    Solution 61 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fkq142tr Prompt:       |             ^~~~~~ c26df72d-79a8-48df-b8a6-acaa04f4e47b.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c26df72d-79a8-48df-b8a6-acaa04f4e47b.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ c26df72d-79a8-48df-b8a6-acaa04f4e47b.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ c26df72d-79a8-48df-b8a6-acaa04f4e47b.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from c26df72d-79a8-48df-b8a6-acaa04f4e47b.cpp:26: c26df72d-79a8-48df-b8a6-acaa04f4e47b.cpp: In function ‘int main()’: c26df72d-79a8-48df-b8a6-acaa04f4e47b.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,31 +1,42 @@
-```
 #include <vector>
 #include <iostream>
 
 bool isSame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) return false;
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
+            return false;
     }
     return true;
 }
 
 std::vector<int> count(int num) {
     std::vector<int> result(2, 0);
-    while (num > 0 || num == 0) {
-        int digit = num % 10;
+    while (num != 0 || num == -1) {
+        int digit = (num % 10);
         if (digit % 2 == 0)
             result[0]++;
         else
             result[1]++;
-        if (num > 0) num /= 10;
-        else break;
+        if (num > 0)
+            num /= 10;
+        else if (num < 0) {
+            num /= 10;
+            ((result[0] += 1), (result[1] += 1));
+        }
     }
     return result;
 }
 
 int main() {
-    std::vector<int> v = count(123456);
-    for (int i : v) std::cout << i << " ";
+    std::cout << "Number of even and odd digits: ";
+    int n = -1; // default input
+    while (n != 0) {
+        std::cin >> n;
+        std::vector<int> temp = count(n);
+        if (!isSame({1, 0}, temp))
+            std::cout << "Number of even and odd digits: " << temp[0] << " " << temp[1] << std::endl;
+    }
     return 0;
 }
