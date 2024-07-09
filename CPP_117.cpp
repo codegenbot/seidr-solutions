91d3371c8ab474e3447cbdf22c21761d0abde8c1
beep boop <githubbot@vadim.me>
2024-07-09 19:32:55 +0200

    Solution 17 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/cjggo54b Prompt: for array subscript    40 |     if (a[i]!=b[i]) return false;       |          ^ da514480-7d7b-4593-b9a3-c3465d1df828.cpp:40:16: error: invalid types ‘int[int]’ for array subscript    40 |     if (a[i]!=b[i]) return false;       |                ^ da514480-7d7b-4593-b9a3-c3465d1df828.cpp: At global scope: da514480-7d7b-4593-b9a3-c3465d1df828.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ da514480-7d7b-4593-b9a3-c3465d1df828.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from da514480-7d7b-4593-b9a3-c3465d1df828.cpp:35: da514480-7d7b-4593-b9a3-c3465d1df828.cpp: In function ‘int main()’: da514480-7d7b-4593-b9a3-c3465d1df828.cpp:45:32: error: ‘select_words’ cannot be used as a function    45 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -28,6 +28,6 @@ vector<string> select_words(string s, int n) {
 }
 
 int main() {
-    assert(issame({} , select_words("a b c d e f", 1)) == vector<string>({"b", "c", "d", "f"}));
+    assert(issame({}, select_words("a b c d e f", 1)) == vector<string>({"b", "c", "d", "f"}));
     return 0;
 }
