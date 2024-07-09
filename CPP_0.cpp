1aa0f39ca72cacbfef61de45270576229317e19b
beep boop <githubbot@vadim.me>
2024-07-09 17:58:42 +0200

    Solution 6 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n0n0xhia Prompt: Complete the following code given the task description and function signature.

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,9 +1,8 @@
-bool has_close_elements(vector<float> numbers, float threshold) {
-    for (int i = 0; i < numbers.size(); ++i) {
-        for (int j = i + 1; j < numbers.size(); ++j) {
-            if (abs(numbers[i] - numbers[j]) <= threshold) {
+bool has_close_elements(vector<float> numbers, float threshold){
+    for(int i = 0; i < numbers.size(); i++){
+        for(int j = i+1; j < numbers.size(); j++){
+            if(abs(numbers[i] - numbers[j]) <= threshold)
                 return true;
-            }
         }
     }
     return false;
