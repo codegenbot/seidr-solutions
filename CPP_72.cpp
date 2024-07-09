567aec6d5b74ac8f1e7004efb8a8e832ff30a9d8
beep boop <githubbot@vadim.me>
2024-07-09 23:38:06 +0200

    Solution 29 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fye5w655 Prompt: 97d6606f-6145-4bb8-93a3-cf26151acbc7.cpp:1:18: error: ‘vector’ was not declared in this scope     1 | bool will_it_fly(vector<int> q, int w) {       |                  ^~~~~~ 97d6606f-6145-4bb8-93a3-cf26151acbc7.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                         ^~~ 97d6606f-6145-4bb8-93a3-cf26151acbc7.cpp:1:33: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                                 ^~~ 97d6606f-6145-4bb8-93a3-cf26151acbc7.cpp:1:38: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool will_it_fly(vector<int> q, int w) {       |                                      ^ In file included from 97d6606f-6145-4bb8-93a3-cf26151acbc7.cpp:13: 97d6606f-6145-4bb8-93a3-cf26151acbc7.cpp: In function ‘int main()’: 97d6606f-6145-4bb8-93a3-cf26151acbc7.cpp:15:24: error: ‘will_it_fly’ cannot be u sed as a function    15 |     assert (will_it_fly({5}, 5) == true);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <algorithm>
+#include <string>
 
 bool will_it_fly(std::vector<int> q, int w) {
     std::string str = "";
@@ -7,7 +8,7 @@ bool will_it_fly(std::vector<int> q, int w) {
         str += to_string(i);
     }
     bool balanced = false;
-    if (str == std::string(str.rbegin(), str.rend()).str()) {
+    if (str == std::string(str.rbegin(), str.rend()).c_str()) {
         balanced = true;
     }
     return balanced && std::accumulate(q.begin(), q.end(), 0) <= w;
