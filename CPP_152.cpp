a84b4c9f6588c152f7fcf94e555b8fa4f31716e0
beep boop <githubbot@vadim.me>
2024-07-09 22:22:56 +0200

    Solution 0 of CPP/152, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/r47ycoc8 Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_152.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+#include<vector>
+using namespace std;
+
+vector<int> compare(vector<int> game, vector<int> guess) {
+    vector<int> result;
+    for (int i = 0; i < game.size(); i++) {
+        int diff = abs(guess[i] - game[i]);
+        result.push_back(diff);
+    }
+    return result;
+}
