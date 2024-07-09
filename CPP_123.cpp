8b5c8f637207391c6906fec7d6d03e8cb98875a8
beep boop <githubbot@vadim.me>
2024-07-09 02:38:28 +0200

    Solution 75 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c0h3vdup Prompt:       |             ^~~~~~ 033549ce-f0ac-4e2e-a8ce-c54eac2ea2d5.cpp:43:34: error: expected primary-expressi on before ‘int’    43 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 033549ce-f0ac-4e2e-a8ce-c54eac2ea2d5.cpp:43:39: error: expression list treated a s compound expression in initializer [-fpermissive]    43 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 033549ce-f0ac-4e2e-a8ce-c54eac2ea2d5.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ 033549ce-f0ac-4e2e-a8ce-c54eac2ea2d5.cpp:36:5: note: ‘int main()’ previously def ined here    36 | int main() {       |     ^~~~ In file included from 033549ce-f0ac-4e2e-a8ce-c54eac2ea2d5.cpp:42: 033549ce-f0ac-4e2e-a8ce-c54eac2ea2d5.cpp: In function ‘int main()’: 033549ce-f0ac-4e2e-a8ce-c54eac2ea2d5.cpp:52:19: error: ‘issame’ cannot be used a s a function    52 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -14,4 +14,5 @@ int main() {
     int num;
     std::cin >> num;
     get_odd_collatz(num);
-    assert(sequenceEqual(get_odd_collatz(1), {1}));
+    assert(sequenceEqual(get_odd_collatz(1), {1}));  
+}
