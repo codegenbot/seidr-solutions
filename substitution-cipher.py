c93666310c8fecec08cdddb96034e20292fefad4
beep boop <githubbot@vadim.me>
2024-07-09 22:56:49 +0200

    Solution 85 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/85wfssrp Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b30fbc58-c1d8-4830-b52e-4a8d23230 221.py", line 1     def decipher_message(key1, key2, message): IndentationError: unexpected indent

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -2,18 +2,14 @@
 def decipher_message(key1, key2, message):
     key1 = key1.lower()
     key2 = key2.lower()
-    
+
     cipher_dict = {k: v for k, v in zip(key1, key2)}
-    
+
     result = ""
 
     for char in message:
         if char.isalpha():
-            result += (
-                cipher_dict.get(char.lower(), char).upper()
-                if char.isupper()
-                else cipher_dict.get(char.lower(), char)
-            )
+            result += (cipher_dict.get(char.lower(), char).upper() if char.isupper() else cipher_dict.get(char.lower(), char))
         else:
             result += char
 
