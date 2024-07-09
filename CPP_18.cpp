920eda7b1b9e7424c4bc623767e860b0ecf57f99
beep boop <githubbot@vadim.me>
2024-07-09 12:50:00 +0200

    Solution 16 of CPP/18, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/stfk6ia7 Prompt: f ‘std’     2 | int how_many_times(std::string s1, std::string s2) {       |                         ^~~~~~ a4d839ac-aab3-41ff-88f7-75f957ad38e8.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | ``` a4d839ac-aab3-41ff-88f7-75f957ad38e8.cpp:2:41: error: ‘string’ is not a member o f ‘std’     2 | int how_many_times(std::string s1, std::string s2) {       |                                         ^~~~~~ a4d839ac-aab3-41ff-88f7-75f957ad38e8.cpp:2:41: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? a4d839ac-aab3-41ff-88f7-75f957ad38e8.cpp:2:50: error: expression list treated as  compound expression in initializer [-fpermissive]     2 | int how_many_times(std::string s1, std::string s2) {       |                                                  ^ In file included from a4d839ac-aab3-41ff-88f7-75f957ad38e8.cpp:12: a4d839ac-aab3-41ff-88f7-75f957ad38e8.cpp: In function ‘int main()’: a4d839ac-aab3-41ff-88f7-75f957ad38e8.cpp:14:27: error: ‘how_many_times’ cannot b e used as a function    14 |     assert (how_many_times("john doe", "john") == 1);       |             ~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -1,9 +1,11 @@
-int how_many_times(string str, string substring) {
+#include <string>
+
+int how_many_times(std::string s1, std::string s2) {
     int count = 0;
     size_t pos = 0;
-    while ((pos = str.find(substring)) != string::npos) {
-        count++;
-        str.erase(pos, substring.length());
+    while ((pos = s1.find(s2, pos)) != std::string::npos) {
+        ++count;
+        pos += s2.size();
     }
     return count;
 }
