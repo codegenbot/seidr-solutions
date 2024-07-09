c5b63ff33b45d38dbd56c994036ae084c58a16e4
beep boop <githubbot@vadim.me>
2024-07-09 12:25:09 +0200

    Solution 44 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/tkok7ny4 Prompt:       |                    ^~~ c9075080-8918-48ec-a4d7-78090532543f.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c9075080-8918-48ec-a4d7-78090532543f.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c9075080-8918-48ec-a4d7-78090532543f.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c9075080-8918-48ec-a4d7-78090532543f.cpp:17: c9075080-8918-48ec-a4d7-78090532543f.cpp: In function ‘int main()’: c9075080-8918-48ec-a4d7-78090532543f.cpp:27:20: error: ‘pluck’ was not declared in this scope    27 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ c9075080-8918-48ec-a4d7-78090532543f.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -8,11 +8,20 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 
 std::vector<int> pluck(std::vector<int> arr) {
     std::vector<int> result;
+    std::vector<std::pair<int, int>> nodes;
+
     for (int i = 0; i < arr.size(); i++) {
         if (arr[i] % 2 == 0) {
-            result.push_back(arr[i]);
+            nodes.push_back({arr[i], i});
         }
     }
-    sort(result.begin(), result.end());
+
+    if (nodes.empty()) {
+        return result;
+    }
+
+    sort(nodes.begin(), nodes.end());
+    result = {nodes[0].first, nodes[0].second};
+
     return result;
 }
