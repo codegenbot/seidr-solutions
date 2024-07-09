d55514c73e46478f487ee9ae160e40f257af7e83
beep boop <githubbot@vadim.me>
2024-07-09 11:14:40 +0200

    Solution 57 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/owo9prrk Prompt: In file included from /usr/include/c++/11/string:39,                  from 6d718a04-4b9e-47de-a73d-681f3ded2053.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 6d718a04-4b9e-47de-a73d-681f3ded2053.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 6d718a04-4b9e-47de-a73d-681f3ded2053.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 6d718a04-4b9e-47de-a73d-681f3ded2053.cpp:19: 6d718a04-4b9e-47de-a73d-681f3ded2053.cpp: In function ‘int main()’: 6d718a04-4b9e-47de-a73d-681f3ded2053.cpp:29:20: error: ‘reverse_delete’ was not declared in this scope; did you mean ‘timer_delete’?    29 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 6d718a04-4b9e-47de-a73d-681f3ded2053.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,3 +1,4 @@
+
 #include <vector>
 #include <string>
 #include <algorithm>
@@ -9,8 +10,8 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
 
 std::vector<std::string> reverse_delete(std::string s, std::string c) {
     std::string result = "";
-    for(char ch : s){
-        if(c.find(ch) == std::string::npos){
+    for(char ch : s) {
+        if (c.find(ch) == std::string::npos) {
             result += ch;
         }
     }
