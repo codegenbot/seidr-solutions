de18f35b244c67f7afd1ad8ad007eb2bfe1946fe
beep boop <githubbot@vadim.me>
2024-07-10 00:42:43 +0200

    Solution 59 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/a5ra8v8x Prompt: f83a440b-fa67-41e4-804b-51d00f9e4187.cpp:1:5: warning: missing terminating ' cha racter     1 | Here's the completed code:       |     ^ f83a440b-fa67-41e4-804b-51d00f9e4187.cpp:1:5: error: missing terminating ' chara cter     1 | Here's the completed code:       |     ^~~~~~~~~~~~~~~~~~~~~~ f83a440b-fa67-41e4-804b-51d00f9e4187.cpp:1:1: error: ‘Here’ does not name a type     1 | Here's the completed code:       | ^~~~ In file included from f83a440b-fa67-41e4-804b-51d00f9e4187.cpp:16: f83a440b-fa67-41e4-804b-51d00f9e4187.cpp: In function ‘int main()’: f83a440b-fa67-41e4-804b-51d00f9e4187.cpp:18:13: error: ‘is_sorted’ was not decla red in this scope    18 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ^~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,12 +1,9 @@
 bool is_sorted(vector<int> lst) {
     for (int i = 1; i < lst.size(); i++) {
-        if (lst[i] >= lst[i - 1]) {
+        if (lst[i] >= lst[i - 1])
             continue;
-        } else {
-            auto it = unique(lst.begin(), lst.end());
-            lst.erase(it, lst.end());
+        else 
             return false;
-        }
     }
     return true;
 }
