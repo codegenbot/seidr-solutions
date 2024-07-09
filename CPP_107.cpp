e723b4517ed3f75596b8477382f8ef62cec56730
beep boop <githubbot@vadim.me>
2024-07-09 03:13:33 +0200

    Solution 19 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/3khcjxc6 Prompt:       |             ^~~~~~ 19ff7b96-72f4-4494-b51a-b6741baa563d.cpp:27:20: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 19ff7b96-72f4-4494-b51a-b6741baa563d.cpp:27:27: error: ‘vector’ was not declared  in this scope    27 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 19ff7b96-72f4-4494-b51a-b6741baa563d.cpp:27:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 19ff7b96-72f4-4494-b51a-b6741baa563d.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 19ff7b96-72f4-4494-b51a-b6741baa563d.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 19ff7b96-72f4-4494-b51a-b6741baa563d.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,24 +1,3 @@
-#include <vector>
-#include <algorithm>
-#include <string>
-
-std::vector<int> even_odd_palindrome(int n) {
-    std::vector<int> res(2, 0);
-    for (int i = 1; i <= n; ++i) {
-        std::string num = std::to_string(i);
-        std::string rev_num = num;
-        std::reverse(rev_num.begin(), rev_num.end());
-        if (num == rev_num) {
-            if (i % 2 == 0) {
-                res[0]++;
-            } else {
-                res[1]++;
-            }
-        }
-    }
-    return res;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool is_same(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
