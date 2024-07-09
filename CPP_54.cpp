10b37325e337318a045b40d969c564e94cd85f21
beep boop <githubbot@vadim.me>
2024-07-09 14:53:53 +0200

    Solution 2 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5uknokbc Prompt: 34e0c394-18bc-4753-86fe-06f85eaac2c4.cpp:1:17: error: ‘string’ was not declared in this scope     1 | bool same_chars(string s0, string s1) {       |                 ^~~~~~ 34e0c394-18bc-4753-86fe-06f85eaac2c4.cpp:1:28: error: ‘string’ was not declared in this scope     1 | bool same_chars(string s0, string s1) {       |                            ^~~~~~ 34e0c394-18bc-4753-86fe-06f85eaac2c4.cpp:1:37: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool same_chars(string s0, string s1) {       |                                     ^ In file included from 34e0c394-18bc-4753-86fe-06f85eaac2c4.cpp:7: 34e0c394-18bc-4753-86fe-06f85eaac2c4.cpp: In function ‘int main()’: 34e0c394-18bc-4753-86fe-06f85eaac2c4.cpp:9:23: error: ‘same_chars’ cannot be use d as a function     9 |     assert (same_chars("aabb", "aaccc") == false);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -1,6 +1,5 @@
-using namespace std;
-
-bool same_chars(string s0, string s1) {
+#include <string>
+bool same_chars(std::string s0, std::string s1) {
     sort(s0.begin(), s0.end());
     sort(s1.begin(), s1.end());
     return s0 == s1;
