7815d7181e0e3aa8b4e3ddaeb0503e6d91d32b7b
beep boop <githubbot@vadim.me>
2024-07-09 09:02:09 +0200

    Solution 40 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qqhcwdm6 Prompt:       |                                       ^ 84c84e71-4581-4388-ab17-ca3e7df561e5.cpp:52:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    52 | int main(){       |         ^~ 84c84e71-4581-4388-ab17-ca3e7df561e5.cpp:52:9: note: remove parentheses to defau lt-initialize a variable    52 | int main(){       |         ^~       |         -- 84c84e71-4581-4388-ab17-ca3e7df561e5.cpp:52:9: note: or replace parentheses with  braces to value-initialize a variable 84c84e71-4581-4388-ab17-ca3e7df561e5.cpp:52:11: error: a function-definition is not allowed here before ‘{’ token    52 | int main(){       |           ^ 84c84e71-4581-4388-ab17-ca3e7df561e5.cpp:54:2: error: expected ‘}’ at end of inp ut    54 | }       |  ^ 84c84e71-4581-4388-ab17-ca3e7df561e5.cpp:38:12: note: to match this ‘{’    38 | int main() {       |            ^

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,4 +1,4 @@
-#include <initializer_list>
+```c++
 #include <vector>
 #include <algorithm>
 
@@ -37,8 +37,8 @@ vector<vector<int>> strange_sort_list(vector<vector<int>>& lst) {
     return result;
 }
 
-int main 
-{
+int main() {
     vector<vector<int>> lst = {{1, 1, 1, 1}};
     assert(issame({1, 2}, {2, 1}));
     vector<vector<int>> result = strange_sort_list(lst);
+}
