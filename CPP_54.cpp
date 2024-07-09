c8b6ac885c60caa486afb69f6d548314ae9d2394
beep boop <githubbot@vadim.me>
2024-07-09 15:31:32 +0200

    Solution 32 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5uknokbc Prompt:       |                                 ^~~~~~ In file included from fe635712-9245-4699-9282-d650151cdb96.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ fe635712-9245-4699-9282-d650151cdb96.cpp:4:37: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | bool same_chars(string s0, string s1) {       |                                     ^ fe635712-9245-4699-9282-d650151cdb96.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ fe635712-9245-4699-9282-d650151cdb96.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from fe635712-9245-4699-9282-d650151cdb96.cpp:21: fe635712-9245-4699-9282-d650151cdb96.cpp: In function ‘int main()’: fe635712-9245-4699-9282-d650151cdb96.cpp:23:23: error: ‘same_chars’ cannot be us ed as a function    23 |     assert (same_chars("aabb", "aaccc") == false);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -1,20 +1,19 @@
 #include <cassert>
 #include <string>
-using namespace std;
 
-bool same_chars(string s0, string s1) {
+bool same_chars(std::string s0, std::string s1) {
     if(s0.length() != s1.length()) {
         return false;
     }
     for(int i = 0; i < s0.length(); i++) {
-        if(count(s0.begin(), s0.end(), s1[i]) != 1) {
+        if(std::count(s0.begin(), s0.end(), s1[i]) != 1) {
             return false;
         }
     }
-    return s0 == s1;
+    return true;
 }
 
 int main() {
-    cout << same_chars("aabb", "aaccc");
+    assert(same_chars("aabb", "aaccc") == false);
     return 0;
 }
