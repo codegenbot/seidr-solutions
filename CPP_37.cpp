d3612846b32aaf8af7c15064d677a9101c7417bb
beep boop <githubbot@vadim.me>
2024-07-09 14:15:41 +0200

    Solution 22 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7dqgrwjn Prompt:    38 |     assert(isSame(sort_even(vector<float>(make_vector({5, 8, -12, 4, 23,  2, 3, 11, 12, -10})) ) , {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |     ^~~~~~ d898e280-bfd0-430b-9a95-05668ec9c668.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | using namespace std; d898e280-bfd0-430b-9a95-05668ec9c668.cpp: In function ‘bool issame(std::vector<f loat>, std::vector<float>)’: d898e280-bfd0-430b-9a95-05668ec9c668.cpp:47:13: error: ‘abs’ was not declared in  this scope    47 |         if (abs(a[i]-b[i])>1e-4) return false;       |             ^~~ d898e280-bfd0-430b-9a95-05668ec9c668.cpp: At global scope: d898e280-bfd0-430b-9a95-05668ec9c668.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ d898e280-bfd0-430b-9a95-05668ec9c668.cpp:37:5: note: ‘int main()’ previously def ined here    37 | int main() {       |     ^~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <cassert>
 #include <cmath>
 using namespace std;
 
@@ -36,7 +35,7 @@ vector<float> sort_even(vector<float> l) {
     return result;
 }
 
-int main() {
-    assert(isSame(sort_even(vector<float>(make_vector({5, 8, -12, 4, 23, 2, 3, 11, 12, -10})) ) , vector<float>({ -12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+int main_test() {
+    assert(isSame(sort_even(vector<float>(make_vector({5, 8, -12, 4, 23, 2, 3, 11, 12, -10})) ) , {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
     return 0;
 }
