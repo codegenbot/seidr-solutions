6217328865f0303879ab9f5a71b1263c859e6f51
beep boop <githubbot@vadim.me>
2024-07-10 00:50:10 +0200

    Solution 26 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mlild2ig Prompt:    20 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ edad6d4f-573b-4b08-9b19-574f1737f078.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ edad6d4f-573b-4b08-9b19-574f1737f078.cpp:20:45: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from edad6d4f-573b-4b08-9b19-574f1737f078.cpp:19: edad6d4f-573b-4b08-9b19-574f1737f078.cpp: In function ‘int main()’: edad6d4f-573b-4b08-9b19-574f1737f078.cpp:29:20: error: ‘sorted_list_sum’ was not  declared in this scope    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ edad6d4f-573b-4b08-9b19-574f1737f078.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,11 +1,26 @@
 bool issame(vector<string> a, vector<string>b) {
-    if(a.size() != b.size()) {
+    if (a.size() != b.size()) {
         return false;
     }
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
+    for (int i = 0; i < a.size(); i++) {
+        if (!(issame_helper(a[i], b[i]))) {
             return false;
         }
+    }
+    return true;
+}
+
+bool issame_helper(string s1, string s2) {
+    vector<int> v1,v2;
+    for(int i=0;i<s1.length();i++){
+        if(s1[i]=='x')v1.push_back(i);
+        if(s2[i]=='x')v2.push_back(i);
+    }
+    if(v1.size()!=v2.size())return false;
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
+    for(int i=0;i<v1.size();i++){
+        if(v1[i]!=v2[i])return false;
     }
     return true;
 }
@@ -19,7 +34,7 @@ vector<string> sorted_list_sum(vector<string> lst) {
     lst.erase(it, lst.end());
     sort(lst.begin(), lst.end(),
         [](const string& a, const string& b) {
-            if(a.length() != b.length()) {
+            if (a.length() != b.length()) {
                 return a.length() < b.length();
             } else {
                 return a < b;
