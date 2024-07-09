b79b13d67c24b76e0752e4148aabb3c5dbd74c80
beep boop <githubbot@vadim.me>
2024-07-09 02:30:49 +0200

    Solution 61 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhnk3fn Prompt:       |     ^~~~ In file included from a145a1e4-c45f-4498-a56e-6460f394999e.cpp:27: a145a1e4-c45f-4498-a56e-6460f394999e.cpp: In function ‘int main()’: a145a1e4-c45f-4498-a56e-6460f394999e.cpp:29:13: error: ‘boost’ has not been decl ared    29 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ a145a1e4-c45f-4498-a56e-6460f394999e.cpp:29:35: error: expected primary-expressi on before ‘>’ token    29 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ a145a1e4-c45f-4498-a56e-6460f394999e.cpp: In function ‘std::variant<std::__cxx11 ::basic_string<char, std::char_traits<char>, std::allocator<char> >, int, double , float> compare_one(std::variant<std::__cxx11::basic_string<char, std::char_tra its<char>, std::allocator<char> >, int, double, float>, std::variant<std::__cxx1 1::basic_string<char, std::char_traits<char>, std::allocator<char> >, int, doubl e, float>)’: a145a1e4-c45f-4498-a56e-6460f394999e.cpp:20:1: warning: control reaches end of n on-void function [-Wreturn-type]    20 | }       | ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -8,47 +8,18 @@ variant<string, int, double, float> compare_one(variant<string, int, double, flo
     if (holds_alternative<string>(a) && holds_alternative<string>(b)) {
         string strA = get<string>(a);
         string strB = get<string>(b);
-        return strA > strB ? a : b;
+        return std::variant<string>(strA > strB ? a : b);
     }
-    else if (holds_alternation<int>(a) && holds_alternation<int>(b)) {
-        int val1 = get<int>(a), val2 = get<int>(b);
+    else if (holds_alternative<int>(a) && holds_alternation<string>(b)) {
+        int val1 = get<int>(a), val2 = 0;
         return val1 > val2 ? a : b;
     }
-    else if ((holds_alternation<string>(a) && holds_alternation<int>(b)) || (holds_alternation<int>(a) && holds_alternation<string>(b))) {
-        variant<string, int> vA = a, vB = b;
-        string strA = get<string>(vA);
-        int val2 = get<int>(vB);
-        return strA > to_string(val2) ? a : b;
+    else if (holds_alternation<string>(a) && holds_alternation<int>(b)) {
+        string strA = get<string>(a), intB = 0;
+        return strA > to_string(intB) ? a : b;
     }
-    else if ((holds_alternation<string>(a) && holds_alternation<double>(b)) || (holds_alternation<double>(a) && holds_alternation<string>(b))) {
-        variant<string, double> vA = a, vB = b;
-        string strA = get<string>(vA);
-        double val2 = get<double>(vB);
-        return strA > to_string(val2) ? a : b;
-    }
-    else if ((holds_alternation<string>(a) && holds_alternation<float>(b)) || (holds_alternation<float>(a) && holds_alternation<string>(b))) {
-        variant<string, float> vA = a, vB = b;
-        string strA = get<string>(vA);
-        float val2 = get<float>(vB);
-        return strA > to_string(val2) ? a : b;
-    }
-    else if ((holds_alternation<int>(a) && holds_alternation<double>(b)) || (holds_alternation<double>(a) && holds_alternation<int>(b))) {
-        variant<int, double> vA = a, vB = b;
-        int val1 = get<int>(vA);
-        double val2 = get<double>(vB);
-        return val1 > val2 ? a : b;
-    }
-    else if ((holds_alternation<int>(a) && holds_alternation<float>(b)) || (holds_alternation<float>(a) && holds_alternation<int>(b))) {
-        variant<int, float> vA = a, vB = b;
-        int val1 = get<int>(vA);
-        float val2 = get<float>(vB);
-        return val1 > val2 ? a : b;
-    }
-    else if ((holds_alternation<double>(a) && holds_alternation<float>(b)) || (holds_alternation<float>(a) && holds_alternation<double>(b))) {
-        variant<double, float> vA = a, vB = b;
-        double val1 = get<double>(vA);
-        float val2 = get<float>(vB);
-        return val1 > val2 ? a : b;
-    }
-    else
-        return 0;
+}
+
+int main() {
+    return 0;
+}
