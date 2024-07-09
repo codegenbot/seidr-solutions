ec927dc02174757554c5fbe58d4b44517f396eac
beep boop <githubbot@vadim.me>
2024-07-09 16:00:08 +0200

    Solution 80 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/fm3jbbbw Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 9d8c340c-93c5-4537-a27c-90fb1f67e6a3.cpp:13:29:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,16 +1,14 @@
-```cpp
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 vector<int> sort_third(vector<int> l) {
     vector<int> result;
     int groupCount = 0;
 
     for (int i : l) {
-        if(groupCount % 3 == 0 || groupCount >= l.size()) { 
+        if(groupCount >= l.size()) { 
             result.push_back(i);
+        } else if(groupCount % 3 == 0) {
+            if(groupCount == 0 || l[groupCount - 1] % 3 != i % 3) {
+                result.push_back(i);
+            }
             groupCount++;
         } else {
             groupCount++;
@@ -18,38 +16,4 @@ vector<int> sort_third(vector<int> l) {
     }
 
     return result;
-}
-
-int main() {
-    vector<int> l1 = vector<int>({1, 2, 3});
-    vector<int> l2 = vector<int>({5, 6, 3, 4, 8, 9, 2});
-
-    cout << "l1: ";
-    for (int i : l1) {
-        cout << i << " ";
-    }
-    cout << endl;
-
-    cout << "l2: ";
-    for (int i : l2) {
-        cout << i << " ";
-    }
-    cout << endl;
-
-    vector<int> result1 = sort_third(l1);
-    vector<int> result2 = sort_third(l2);
-
-    cout << "result1: ";
-    for (int i : result1) {
-        cout << i << " ";
-    }
-    cout << endl;
-
-    cout << "result2: ";
-    for (int i : result2) {
-        cout << i << " ";
-    }
-    cout << endl;
-    
-    return 0;
 }
