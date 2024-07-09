5dc0d054595adea2f1edca73265e0ae29543b3de
beep boop <githubbot@vadim.me>
2024-07-09 02:29:00 +0200

    Solution 58 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhnk3fn Prompt:       |     ^~~~ In file included from a145a1e4-c45f-4498-a56e-6460f394999e.cpp:27: a145a1e4-c45f-4498-a56e-6460f394999e.cpp: In function ‘int main()’: a145a1e4-c45f-4498-a56e-6460f394999e.cpp:29:13: error: ‘boost’ has not been decl ared    29 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ a145a1e4-c45f-4498-a56e-6460f394999e.cpp:29:35: error: expected primary-expressi on before ‘>’ token    29 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ a145a1e4-c45f-4498-a56e-6460f394999e.cpp: In function ‘std::variant<std::__cxx11 ::basic_string<char, std::char_traits<char>, std::allocator<char> >, int, double , float> compare_one(std::variant<std::__cxx11::basic_string<char, std::char_tra its<char>, std::allocator<char> >, int, double, float>, std::variant<std::__cxx1 1::basic_string<char, std::char_traits<char>, std::allocator<char> >, int, doubl e, float>)’: a145a1e4-c45f-4498-a56e-6460f394999e.cpp:20:1: warning: control reaches end of n on-void function [-Wreturn-type]    20 | }       | ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,38 +1,34 @@
-#include <any>
+#include <boost/variant.hpp>
 
-using namespace std;
+using namespace boost;
 
-any compare_one(any a, any b) {
-    if (get<int>(a) > get<int>(b)) {
-        return b;
+variant<string, int, double, float> compare_one(variant<string, int, double, float> a, variant<string, int, double, float> b) {
+    if (holds_alternative<string>(a) && holds_alternative<string>(b)) {
+        string strA = get<string>(a);
+        string strB = get<string>(b);
+        return strA > strB ? a : b;
     }
-    if (get<int>(a) < get<int>(b)) {
-        return a;
+    else if ((holds_alternation<string>(a) && holds_alternation<int>(b)) || (holds_alternation<string>(b) && holds_alternation<int>(a))) {
+        int val1 = get<int>(get_or(a, "None", [](auto& v) { return boost::none; }));
+        string str2 = get<string>(get_or(b, "None", [](auto& v) { return boost::none; }));
+        return val1 > 0 ? a : b;
     }
+    else if ((holds_alternation<int>(a) && holds_alternation<string>(b)) || (holds_alternation<int>(b) && holds_alternation<string>(a))) {
+        int val1 = get<int>(a), val2 = get<int>(get_or(b, "None", [](auto& v) { return boost::none; }));
+        return val1 > val2 ? a : b;
+    }
+    else if ((holds_alternation<double>(a) && holds_alternation<string>(b)) || (holds_alternation<string>(a) && holds_alternation<double>(b))) {
+        double val1 = get<double>(a), val2 = get<double>(get_or(b, "None", [](auto& v) { return boost::none; }));
+        return val1 > val2 ? a : b;
+    }
+    else if ((holds_alternation<float>(a) && holds_alternation<string>(b)) || (holds_alternation<string>(a) && holds_alternation<float>(b))) {
+        float val1 = get<float>(a), val2 = get<float>(get_or(b, "None", [](auto& v) { return boost::none; }));
+        return val1 > val2 ? a : b;
+    }
+    else
+        return "None";
+}
 
-    if (get<float>(a) > get<float>(b)) {
-        return b;
-    }
-    if (get<float>(a) < get<float>(b)) {
-        return a;
-    }
-
-    if (get<double>(a) > get<double>(b)) {
-        return b;
-    }
-    if (get<double>(a) < get<double>(b)) {
-        return a;
-    }
-
-    if (get<string>(a) > get<string>(b)) {
-        return get<string>(a) > get<string>(b) ? a : b;
-    }
-    if (get<string>(a) < get<string>(b)) {
-        return a;
-    }
-    if (get<string>(a) == get<string>(b)) {
-        return a;
-    }
-
-    return a;
+int main() {
+    return 0;
 }
