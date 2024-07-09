120fa7512c357f468f30202f95b3c489270923c3
beep boop <githubbot@vadim.me>
2024-07-09 08:07:52 +0200

    Solution 98 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uw1asumq Prompt: 3d3b3ad6-a0d7-45b3-b81c-ca7ef766c9be.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 3d3b3ad6-a0d7-45b3-b81c-ca7ef766c9be.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main(){       |     ^~~~ In file included from 3d3b3ad6-a0d7-45b3-b81c-ca7ef766c9be.cpp:20: 3d3b3ad6-a0d7-45b3-b81c-ca7ef766c9be.cpp: In function ‘int main()’: 3d3b3ad6-a0d7-45b3-b81c-ca7ef766c9be.cpp:22:13: error: ‘filp_case’ was not decla red in this scope; did you mean ‘flip_case’?    22 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -2,10 +2,10 @@
 #include <cassert>
 
 std::string flip_case(std::string str){
-    for(char &c : str){
-        if(std::islower(c)){
+    for (char &c : str){
+        if (std::islower(c)){
             c = std::toupper(c);
-        } else if(std::isupper(c)){
+        } else if (std::isupper(c)){
             c = std::tolower(c);
         }
     }
@@ -13,6 +13,6 @@ std::string flip_case(std::string str){
 }
 
 int main(){
-    assert(flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
+    assert (flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
     return 0;
 }
