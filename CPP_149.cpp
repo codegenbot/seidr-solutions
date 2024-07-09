28d754d1a8916e907c2e1fd1f993d4e66cb65044
beep boop <githubbot@vadim.me>
2024-07-10 01:16:08 +0200

    Solution 90 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mlild2ig Prompt:    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d322a4fd-1bf9-40aa-bb17-67367a788a9f.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d322a4fd-1bf9-40aa-bb17-67367a788a9f.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d322a4fd-1bf9-40aa-bb17-67367a788a9f.cpp:21: d322a4fd-1bf9-40aa-bb17-67367a788a9f.cpp: In function ‘int main()’: d322a4fd-1bf9-40aa-bb17-67367a788a9f.cpp:31:20: error: ‘sorted_list_sum’ was not  declared in this scope    31 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ d322a4fd-1bf9-40aa-bb17-67367a788a9f.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -4,7 +4,7 @@ vector<string> sorted_list_sum(vector<string> lst){
         if(s.length() % 2 == 0)
             result.push_back(s);
     }
-
+    
     sort(result.begin(), result.end(), 
          [](const string& a, const string& b) {
              if(a.length() != b.length())
@@ -12,6 +12,10 @@ vector<string> sorted_list_sum(vector<string> lst){
              else
                  return a < b;
          });
-
+    
     return result;
+}
+
+int main() {
+    assert(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) == {"cc", "dd", "aaaa", "bbbb"});
 }
