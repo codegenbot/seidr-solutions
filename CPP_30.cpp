d070a757a022b098b94a0b3369c0b5cc3c3156ce
beep boop <githubbot@vadim.me>
2024-07-09 03:28:34 +0200

    Solution 32 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jvy931jd Prompt:       |                    ^~~~~ 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:11: 5d830cd4-1162-4af1-ae79-c42923590cad.cpp: In function ‘int main()’: 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,20 +1,9 @@
 #include <vector>
-using namespace std;
 
-bool issame(vector<float> a, vector<float> b){
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i<a.size(); i++){
-        if(a[i] != b[i]) return false;
-    }
-    return true;
+bool isSame(vector<float> a, vector<float> b) {
+    return (a == b);
 }
 
-std::vector<float> get_positive(std::vector<float> l){
-    std::vector<float> result;
-    for(float i : l){
-        if(i > 0) {
-            result.push_back(i);
-        }
-    }
-    return result;
+int main() {
+    assert(isSame(get_positive({}), {}));
 }
