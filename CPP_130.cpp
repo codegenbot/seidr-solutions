37c05df34d87c5dcc1cf9c83d53e21caaff9eb3a
beep boop <githubbot@vadim.me>
2024-07-09 11:29:32 +0200

    Solution 32 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/7winz8yi Prompt:       |                    ^~~ 09dec808-7add-4fc2-bbec-cd63ad10334d.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 09dec808-7add-4fc2-bbec-cd63ad10334d.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 09dec808-7add-4fc2-bbec-cd63ad10334d.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 09dec808-7add-4fc2-bbec-cd63ad10334d.cpp:18: 09dec808-7add-4fc2-bbec-cd63ad10334d.cpp: In function ‘int main()’: 09dec808-7add-4fc2-bbec-cd63ad10334d.cpp:28:20: error: ‘tri’ was not declared in  this scope    28 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ 09dec808-7add-4fc2-bbec-cd63ad10334d.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
 #include <vector>
 #include <cassert>
 
-using namespace std;
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    return a == b;
+}
 
-vector<int> tri(int n) {
-    vector<int> result;
+std::vector<int> yourFunction(int n) {
+    std::vector<int> result;
     if (n >= 0) {
         result.push_back(3);
         if (n > 0) {
@@ -14,7 +15,7 @@ vector<int> tri(int n) {
                 if (i % 2 == 0) {
                     result.push_back(1 + (i / 2));
                 } else {
-                    result.push_back(result[i - 1] + result[i - 2] + result[i - 3]);
+                    result.push_back(result[i - 1] + result[i - 2] + result[i + 1]);
                 }
             }
         }
@@ -22,12 +23,8 @@ vector<int> tri(int n) {
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 int main() {
-    assert(issame(tri(1), {1, 3}));
-    
+    assert(issame(yourFunction(1), {1, 3}));
+
     return 0;
 }
