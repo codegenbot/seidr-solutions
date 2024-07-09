ae21033eba5b4feb5b68e7a0248860efc64df75e
beep boop <githubbot@vadim.me>
2024-07-09 04:17:02 +0200

    Solution 9 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fv53j2kl Prompt: db44a8da-c59f-4699-b973-12f10e95e7aa.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,17 +1,32 @@
+#include <iostream>
 #include <string>
 #include <boost/any.hpp>
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return (int)a > (float)b ? a : b;
+        return (int)boost::any_cast<int>(a) > (float)boost::any_cast<float>(b)
+               ? a : b;
     } else if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return (int)a > (double)b ? a : b;
+        return (int)boost::any_cast<int>(a) > (double)boost::any_cast<double>(b)
+               ? a : b;
     } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
         string str = boost::any_cast<string>(a);
         int num = boost::any_cast<int>(b);
-        return stoull(str) > num ? a : b;
-    } else if (a.type() == typeid(float) && b.type() == typeid(double)) {
-        return (float)a > (double)b ? a : b;
+        return stod(str) > num ? a : b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return (float)boost::any_cast<float>(a) > (int)boost::any_cast<int>(b)
+               ? a : b;
+    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return (double)boost::any_cast<double>(a) > (int)boost::any_cast<int>(b)
+               ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        string str = boost::any_cast<string>(a);
+        float num = boost::any_cast<float>(b);
+        return stod(str) > num ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        string str = boost::any_cast<string>(a);
+        double num = boost::any_cast<double>(b);
+        return stod(str) > num ? a : b;
     }
     return "None";
 }
