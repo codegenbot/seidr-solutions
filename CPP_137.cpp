f31b0117d262ecc2336ef1b1104cf63b6f03d1f0
beep boop <githubbot@vadim.me>
2024-07-09 13:53:45 +0200

    Solution 70 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/z1i35unk Prompt: c74767a1-b544-4e3d-92d9-8628d5836bf7.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,16 +1,40 @@
+```cpp
+#include <iostream>
 #include <string>
-#include <algorithm>
+#include <boost/any.hpp>
 
 using namespace std;
 
-string compare_one(string a, string b) {
-    float numA = stof(a);
-    float numB = stof(b);
+boost::any compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        int x = boost::any_cast<int>(a);
+        int y = boost::any_cast<int>(b);
+        return (x > y) ? a : ((x < y) ? b : boost::any("None"));
+    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        float x = boost::any_cast<float>(a);
+        float y = boost::any_cast<float>(b);
+        return (x > y) ? a : ((x < y) ? b : boost::any("None"));
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string x = boost::any_cast<string>(a);
+        string y = boost::any_cast<string>(b);
+        return (stof(x) > stof(y)) ? a : ((stof(x) < stof(y)) ? b : boost::any("None"));
+    } else if (a.type() == typeid(string)) {
+        string x = boost::any_cast<string>(a);
+        float y = boost::any_cast<float>(b);
+        return (stof(x) > y) ? a : ((stof(x) < y) ? b : boost::any("None"));
+    } else if (b.type() == typeid(string)) {
+        string y = boost::any_cast<string>(b);
+        float x = boost::any_cast<float>(a);
+        return (stof(y) > x) ? b : ((stof(y) < x) ? a : boost::any("None"));
+    } else {
+        return boost::any("None");
+    }
+}
 
-    if (numA > numB)
-        return a;
-    else if (numA < numB)
-        return b;
-    else
-        return "None";
+int main() {
+    cout << boost::any_cast<string>(compare_one(1, 2.5)) << endl;
+    cout << boost::any_cast<string>(compare_one(1, "2.5")) << endl;
+    cout << boost::any_cast<string>(compare_one("5", "6")) << endl;
+    cout << boost::any_cast<string>(compare_one("1", 1)) << endl;
+    return 0;
 }
