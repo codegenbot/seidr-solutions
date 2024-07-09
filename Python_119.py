2b9c8770fc29eb821c06e7ce60b2e18c212a31f3
beep boop <githubbot@vadim.me>
2024-07-09 03:35:04 +0200

    Solution 1 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/1y3i0irz Prompt: Complete the following code given the task description and function signature.

--- Python_119.py
+++ Python_119.py
@@ -1,13 +1,10 @@
 def match_parens(lst):
     count = 0
-    for char in lst:
+    for char in lst[0] + lst[1]:
         if char == '(':
             count += 1
         else:
             if count == 0:
                 return 'No'
             count -= 1
-    if count == 0:
-        return 'Yes'
-    else:
-        return 'No'
+    return 'Yes'
