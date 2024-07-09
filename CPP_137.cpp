c749eb799c4a529aa84a8ec4c2b6e1f1a61f4a24
beep boop <githubbot@vadim.me>
2024-07-09 19:53:26 +0200

    Solution 15 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/iewm74bm Prompt: d2af02e1-1ecf-419f-b89f-3f5c434bbd06.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,6 +1,5 @@
 #include <boost/any.hpp>
 #include <string>
-#include <stdexcept>
 
 using namespace boost;
 
@@ -8,44 +7,29 @@ bool compare_one(boost::any& a, boost::any& b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
         int x = boost::any_cast<int>(a);
         int y = boost::any_cast<int>(b);
-        if (x > y)
-            return true;
-        else if (y > x)
-            return false;
+        return x > y;
     }
     else if (a.type() == typeid(float) && b.type() == typeid(float)) {
         float x = boost::any_cast<float>(a);
         float y = boost::any_cast<float>(b);
-        if (x > y)
-            return true;
-        else if (y > x)
-            return false;
+        return x > y;
     }
     else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
         std::string x = boost::any_cast<std::string>(a);
         std::string y = boost::any_cast<std::string>(b);
-        if (x > y)
-            return true;
-        else if (y > x)
-            return false;
+        return x > y;
     }
     else if ((a.type() == typeid(int) || a.type() == typeid(float)) && b.type() == typeid(std::string)) {
         double x = boost::any_cast<double>(a);
         std::string y = boost::any_cast<std::string>(b);
-        if (x > stod(y))
-            return true;
-        else if (stod(y) > x)
-            return false;
+        return x > stod(y);
     }
     else if (a.type() == typeid(std::string) && (b.type() == typeid(int) || b.type() == typeid(float))) {
         std::string x = boost::any_cast<std::string>(a);
         double y = boost::any_cast<double>(b);
-        if (stod(x) > y)
-            return true;
-        else if (y > stod(x))
-            return false;
+        return stod(x) > y;
     }
     else {
-        throw std::runtime_error("Invalid types");
+        return false;
     }
 }
