f71807de976a784c2f3d16bdb412f7f011557f43
beep boop <githubbot@vadim.me>
2024-07-09 02:44:48 +0200

    Solution 8 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/633ghytp Prompt:       |                    ^~~ f0fa73fd-b80a-4a0f-b3b1-3b6851ecffed.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f0fa73fd-b80a-4a0f-b3b1-3b6851ecffed.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f0fa73fd-b80a-4a0f-b3b1-3b6851ecffed.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f0fa73fd-b80a-4a0f-b3b1-3b6851ecffed.cpp:14: f0fa73fd-b80a-4a0f-b3b1-3b6851ecffed.cpp: In function ‘int main()’: f0fa73fd-b80a-4a0f-b3b1-3b6851ecffed.cpp:24:20: error: ‘make_a_pile’ was not dec lared in this scope    24 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ f0fa73fd-b80a-4a0f-b3b1-3b6851ecffed.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,10 +1,7 @@
-#include <iostream>
 #include <vector>
 
-using namespace std;
-
-vector<int> make_a_pile(int n) {
-    vector<int> stones;
+std::vector<int> make_a_pile(int n) {
+    std::vector<int> stones;
     stones.push_back(n);
     for (int i = 1; i < n; ++i) {
         if (n % 2 == 0) {
@@ -17,19 +14,12 @@ vector<int> make_a_pile(int n) {
     return stones;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
 }
 
 int main() {
     assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
+    
     return 0;
 }
