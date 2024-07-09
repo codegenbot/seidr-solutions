89cc46f25f45ec082dddd6fb7eff7fc75145c93f
beep boop <githubbot@vadim.me>
2024-07-09 04:37:52 +0200

    Solution 70 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7b288k16 Prompt:       |           ^~~~~~ In file included from 447a72b6-b33a-4762-bbd9-28dcdd378465.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 447a72b6-b33a-4762-bbd9-28dcdd378465.cpp:47:37: error: ‘string’ was not declared  in this scope    47 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 447a72b6-b33a-4762-bbd9-28dcdd378465.cpp:47:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 447a72b6-b33a-4762-bbd9-28dcdd378465.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 447a72b6-b33a-4762-bbd9-28dcdd378465.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 447a72b6-b33a-4762-bbd9-28dcdd378465.cpp:47:44: error: ‘b’ was not declared in t his scope    47 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> (a, b)) {
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if(a.size() != b.size()) {
         return false;
     }
@@ -16,8 +16,8 @@ bool issame(std::vector<std::string> (a, b)) {
 }
 
 std::vector<std::string> reverse_delete(std::string s, std::string c) {
-    std::vector<std::string> result;
-    std::string temp = "";
+    vector<string> result;
+    string temp = "";
     for (int i = 0; i < s.length(); i++) {
         bool found = false;
         for (int j = 0; j < c.length(); j++) {
@@ -31,7 +31,7 @@ std::vector<std::string> reverse_delete(std::string s, std::string c) {
         }
     }
     result.push_back(temp);
-    std::string rev = "";
+    string rev = "";
     for(int i = temp.length()-1; i >= 0; i--) {
         rev += temp[i];
     }
