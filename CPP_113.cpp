016c6ae6cb0de6678e19cf07cc1fc20eb7f3d116
beep boop <githubbot@vadim.me>
2024-07-09 20:06:53 +0200

    Solution 59 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/n0xhbar7 Prompt: 8ba50e1f-b316-495d-8498-e21d07ad4d4a.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 8ba50e1f-b316-495d-8498-e21d07ad4d4a.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 8ba50e1f-b316-495d-8498-e21d07ad4d4a.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 8ba50e1f-b316-495d-8498-e21d07ad4d4a.cpp:57:5: error: redefinition of ‘int main( )’    57 | int main(){       |     ^~~~ 8ba50e1f-b316-495d-8498-e21d07ad4d4a.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -31,15 +31,15 @@ vector<string> odd_count(vector<string> lst) {
 int main() {
     int n;
     cin >> n;
-
+    
     vector<string> lst(n);
-
+    
     for(int i = 0; i < n; i++) {
         cin >> lst[i];
     }
-
+    
     vector<string> output = odd_count(lst);
     displayOutput(output);
-
+    
     return 0;
 }
