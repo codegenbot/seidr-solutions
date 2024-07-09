e3f57123645cd13e04b615d57e8a16204deacb27
beep boop <githubbot@vadim.me>
2024-07-09 05:33:37 +0200

    Solution 64 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/12k2ekn5 Prompt: difference(std::vector<float>)’: c75731a6-2f4e-4104-b89b-fc23eba58f82.cpp:7:33: error: cannot convert ‘float*’ to  ‘double*’     7 |         if(num > 0 && modf(num, &num) == 0.0) {       |                                 ^~~~       |                                 |       |                                 float* In file included from /usr/include/c++/11/cmath:45,                  from c75731a6-2f4e-4104-b89b-fc23eba58f82.cpp:2: /usr/include/x86_64-linux-gnu/bits/mathcalls.h:110:1: note:   initializing argum ent 2 of ‘double modf(double, double*)’   110 | __MATHCALL (modf,, (_Mdouble_ __x, _Mdouble_ *__iptr)) __nonnull ((2));       | ^ In file included from c75731a6-2f4e-4104-b89b-fc23eba58f82.cpp:14: c75731a6-2f4e-4104-b89b-fc23eba58f82.cpp: In function ‘int main()’: c75731a6-2f4e-4104-b89b-fc23eba58f82.cpp:16:35: error: ‘lst’ was not declared in  this scope    16 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ c75731a6-2f4e-4104-b89b-fc23eba58f82.cpp:16:43: error: ‘odd_sum’ was not declare d in this scope    16 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,9 +1,6 @@
-#include <vector>
-#include <cmath>
-
-long long double_the_difference(std::vector<double> &lst){
+long long double_the_difference(std::vector<long double> lst){
     long long sum = 0;
-    for(double num : lst) {
+    for(long double num : lst) {
         if(num > 0 && modf(num, &num) == 0.0) {
             sum += pow(num, 2);
         }
