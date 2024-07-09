472adc998e59f55fe8f9c20df3389c5b07ee55ed
beep boop <githubbot@vadim.me>
2024-07-09 06:55:01 +0200

    Solution 72 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/6axe58t9 Prompt: /usr/include/c++/11/iomanip:210:12: error: ‘class std::basic_ostream<char>’ has no member named ‘precision’   210 |       __os.precision(__f._M_n);       |       ~~~~~^~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from f799ac15-bdf1-4052-bfb6-a7f190a07053.cpp:2: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,5 +1,5 @@
+```cpp
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
 int main() {
@@ -10,7 +10,7 @@ int main() {
 
     double bouncinessIndex = (1 - pow(firstBounceHeight / startHeight, 2)) / (1 + firstBounceHeight / startHeight);
 
-    cout << fixed << setprecision(9) << (2 * startHeight) * (1 - pow(bouncinessIndex, numBounces)) << endl;
+    cout << fixed << setiosflags(ios::fixed) << (2 * startHeight) * (1 - pow(bouncinessIndex, numBounces)) << endl;
 
     return 0;
 }
