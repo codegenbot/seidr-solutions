5e7cbfd16fad6feb7b4b4ae13edc5321055dc10a
beep boop <githubbot@vadim.me>
2024-07-10 00:56:57 +0200

    Solution 91 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/o9mi7ebh Prompt: 4ba1ab74-1b80-4521-b7c6-83c70c196e06.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int prod_signs(vector<int> arr) {       |                ^~~~~~ 4ba1ab74-1b80-4521-b7c6-83c70c196e06.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int prod_signs(vector<int> arr) {       |                       ^~~ In file included from 4ba1ab74-1b80-4521-b7c6-83c70c196e06.cpp:11: 4ba1ab74-1b80-4521-b7c6-83c70c196e06.cpp: In function ‘int main()’: 4ba1ab74-1b80-4521-b7c6-83c70c196e06.cpp:13:23: error: ‘prod_signs’ cannot be us ed as a function    13 |     assert (prod_signs({-1, 1, 1, 0}) == 0);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -4,7 +4,7 @@ int prod_signs(vector<int> arr) {
     int product = 1;
     for (int num : arr) {
         if (num == 0)
-            return 0;
+            return -32768;
         product *= (num > 0 ? 1 : -1);
     }
     return abs(product);
