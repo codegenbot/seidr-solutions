5288e39afbda0fd9337aaabd97384189891d3dfd
beep boop <githubbot@vadim.me>
2024-07-09 18:09:42 +0200

    Solution 6 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/xa85h2bh Prompt:       |                    ^~~~~ a030db0b-0708-46ec-8465-1fbff5504113.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ a030db0b-0708-46ec-8465-1fbff5504113.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ a030db0b-0708-46ec-8465-1fbff5504113.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from a030db0b-0708-46ec-8465-1fbff5504113.cpp:11: a030db0b-0708-46ec-8465-1fbff5504113.cpp: In function ‘int main()’: a030db0b-0708-46ec-8465-1fbff5504113.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ a030db0b-0708-46ec-8465-1fbff5504113.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,23 +1,19 @@
 #include <vector>
+#include <assert.h>
 
-bool issame(std::vector<float> a, std::vector<float> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (!(std::abs(a[i] - b[i]) < 1e-6)) {
-            return false;
-        }
+bool issame(std::vector<float> a, std::vector<float> b){
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i<a.size(); i++){
+        if(a[i] != b[i]) return false;
     }
     return true;
 }
 
 std::vector<float> get_positive(std::vector<float> l){
     std::vector<float> result;
-    for (float x : l) {
-        if(x > 0) {
-            result.push_back(x);
+    for(float i : l) {
+        if(i > 0) {
+            result.push_back(i);
         }
     }
     return result;
-}
