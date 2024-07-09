12b494477cfe397fb9d47abe5cc53ec4c2ba7988
beep boop <githubbot@vadim.me>
2024-07-09 05:31:10 +0200

    Solution 6 of CPP/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/syqctq9k Prompt: Complete the following code given the task description and function signature.

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -1,13 +1,11 @@
-bool correct_bracketing(string brackets){
+int correct_bracketing(string brackets){
     int count = 0;
     for(int i=0; i<brackets.length(); i++){
-        if(brackets[i] == '(')
-            count++;
-        else if(brackets[i] == ')')
-            if(count <= 0)
-                return false;
-            else
-                count--;
+        if(brackets[i] == '(') count++;
+        else if(brackets[i] == ')') {
+            if(count <= 0) return false;
+            else count--;
+        }
     }
     return count == 0;
 }
