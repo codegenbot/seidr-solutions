4acec5607a855fe7f7cad20045a96a7ef064b5de
beep boop <githubbot@vadim.me>
2024-07-09 18:15:01 +0200

    Solution 15 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lcw7gzkh Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = double]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = double; std::allocator_t raits<std::allocator<_CharT> >::pointer = double*; std::allocator_traits<std::al locator<_CharT> >::allocator_type = std::allocator<double>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = double; _Alloc = std::allocator<double>; std::_Vector_base<_Tp, _Alloc>:: pointer = double*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const double&}; _Tp = double; _Alloc = std::allocator<double>; std ::vector<_Tp, _Alloc>::iterator = std::vector<double>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = double; _Alloc = std::al locator<double>; std::vector<_Tp, _Alloc>::value_type = double]’ 20ec4312-c4e1-47bd-8558-d2ebece337b9.cpp:30:29:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<double>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,15 +1,14 @@
+```cpp
 #include <iostream>
 #include <vector>
 #include <cmath>
 
 std::vector<double> coeffs;
-double poly(std::vector<double> polys) {
-    double result = 0.0;
-    for (int i = 0; i < polys.size(); i++) {
-        if (i % 2 == 0)
-            result += polys[i];
-        else
-            result -= pow(1.0, (double)i / 2) * polys[i];
+
+double poly(std::vector<double> v, double x) {
+    double result = 0;
+    for (int i = 0; i < v.size(); i++) {
+        result += v[i] * pow(x, i);
     }
     return result;
 }
