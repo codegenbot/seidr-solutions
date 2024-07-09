aaa74b44f9134ce2747f5f24f9cd9ef7239b1c1b
beep boop <githubbot@vadim.me>
2024-07-09 03:26:01 +0200

    Solution 38 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nvqm4xhw Prompt: a4db2f39-7339-4b5b-b762-5a0061eacb8a.cpp:20:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    20 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff a4db2f39-7339-4b5b-b762-5a0061eacb8a.cpp:23:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    23 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff a4db2f39-7339-4b5b-b762-5a0061eacb8a.cpp:23:36: error: ‘find_zero’ cannot be use d as a function    23 |         solution = find_zero(coeffs);       |                                    ^ In file included from a4db2f39-7339-4b5b-b762-5a0061eacb8a.cpp:7: a4db2f39-7339-4b5b-b762-5a0061eacb8a.cpp:24:21: error: ‘poly’ was not declared i n this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ a4db2f39-7339-4b5b-b762-5a0061eacb8a.cpp:24:17: error: ‘abs’ was not declared in  this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,16 +1,19 @@
+#include <iostream>
 #include <vector>
-#include <cmath>
 
-double poly(std::vector<double> coeffs, double x) {
-    double sum = 0;
-    for (int i = 0; i < coeffs.size(); i++) {
-        sum += coeffs[i] * pow(x, i);
+double poly(std::vector<double> xs, double x) {
+    double res = 0;
+    for (int i = 0; i <= xs.size() - 1; i++) {
+        res += xs[i] * pow(x, xs.size() - 1 - i);
     }
-    return sum;
+    return res;
 }
 
-double find_zero(std::vector<double> xs){
-    if(xs.size() % 2 != 0) return -1; 
-    double root = -xs[1]/(2*xs[0]); 
+double find_zero(std::vector<double> coeffs) {
+    int n = coeffs.size();
+    if (n % 2 != 0)
+        return -1;
+
+    double root = -coeffs[1]/(2*coeffs[0]); 
     return std::round(root);
 }
