ceb35d90bbbbb60f6a552f97cab2fb5ff6ab1f6c
beep boop <githubbot@vadim.me>
2024-07-09 21:38:36 +0200

    Solution 6 of CPP/89, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0514nml7 Prompt: b3c5dd19-d405-4346-96d0-b92f1b856ad9.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string encrypt(string s){       | ^~~~~~ In file included from b3c5dd19-d405-4346-96d0-b92f1b856ad9.cpp:19: b3c5dd19-d405-4346-96d0-b92f1b856ad9.cpp: In function ‘int main()’: b3c5dd19-d405-4346-96d0-b92f1b856ad9.cpp:21:13: error: ‘encrypt’ was not declare d in this scope    21 |     assert (encrypt("a")=="e");       |             ^~~~~~~

--- CPP_89.cpp
+++ CPP_89.cpp
@@ -1,14 +1,16 @@
-string encrypt(string s){
-    string result = "";
+#include <string>
+
+std::string encrypt(std::string s) {
+    std::string result = "";
     for(int i=0; i<s.length(); i++){
         if(s[i] >= 'a' && s[i] <= 'i'){
-            result += (char)((s[i]-'a'+2*2)%26+'a');
+            result += (char)((s[i]-'a'+3)%26+'a');
         } else if(s[i] >= 'j' && s[i] <= 'm'){
-            result += (char)((s[i]-'j'+4*2)%26+'j');
+            result += (char)((s[i]-'j'+6)%26+'j');
         } else if(s[i] >= 'n' && s[i] <= 'v'){
-            result += (char)((s[i]-'n'+6*2)%26+'n');
+            result += (char)((s[i]-'n'+12)%26+'n');
         } else if(s[i] >= 'w' && s[i] <= 'z'){
-            result += (char)((s[i]-'w'+8*2)%26+'w');
+            result += (char)((s[i]-'w'+24)%26+'w');
         } else {
             result += s[i];
         }
