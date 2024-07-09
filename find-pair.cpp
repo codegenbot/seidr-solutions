b36777636670edb083d3f8c23c39c54d04476f1e
beep boop <githubbot@vadim.me>
2024-07-09 19:27:00 +0200

    Solution 3 of find-pair, ratio 0.3 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/bed580vf Prompt: in this scope     8 |         if (numMap.count(complement)) {       |             ^~~~~~ c6e8b033-4c46-41e3-b9b4-45c6c2705b42.cpp:11:9: error: ‘numMap’ was not declared in this scope    11 |         numMap[nums[i]] = i;       |         ^~~~~~ c6e8b033-4c46-41e3-b9b4-45c6c2705b42.cpp: In function ‘int main()’: c6e8b033-4c46-41e3-b9b4-45c6c2705b42.cpp:18:5: error: ‘cin’ was not declared in this scope    18 |     cin >> n;       |     ^~~ c6e8b033-4c46-41e3-b9b4-45c6c2705b42.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; c6e8b033-4c46-41e3-b9b4-45c6c2705b42.cpp:26:5: error: ‘cout’ was not declared in  this scope    26 |     cout << result.first << '\n' << result.second << '\n';       |     ^~~~ c6e8b033-4c46-41e3-b9b4-45c6c2705b42.cpp:26:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’?

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <unordered_map>
+#include <iostream>
 using namespace std;
 
 pair<int, int> findPair(vector<int>& nums, int target) {
@@ -10,7 +12,7 @@ pair<int, int> findPair(vector<int>& nums, int target) {
         }
         numMap[nums[i]] = i;
     }
-    return {-1, -1}; // or throw an exception
+    return {-1, -1}; 
 }
 
 int main() {
