0e8bd9e0c6dba172b1499cfc5a414ec88993bd0d
beep boop <githubbot@vadim.me>
2024-07-09 10:40:09 +0200

    Solution 91 of CPP/77, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/03w5r3bz Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_77.cpp
+++ CPP_77.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <cassert>
 #include <iostream>
 
@@ -13,11 +14,14 @@ bool isCube(int n) {
 }
 
 int main() { 
-    int num;
+    std::string num_str;
     std::cout << "Enter a cube number (like 1729): ";
-    std::cin >> num;
+    std::cin >> num_str;
+
+    if (isCube(std::stoi(num_str)))
+        std::cout << "You entered: " << num_str << std::endl; 
+    else
+        std::cout << "Invalid input." << std::endl;
     
-    assert(isCube(num) == isCube(1729)); 
-    std::cout << "You entered: " << num << std::endl; 
     return 0;
 }
