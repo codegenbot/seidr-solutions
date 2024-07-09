389a21769fbc6a362e84f9449cf97e494531f813
beep boop <githubbot@vadim.me>
2024-07-09 05:15:34 +0200

    Solution 51 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/6bnchz2z Prompt: Program did not receive expected input

--- Python_17.py
+++ Python_17.py
@@ -1,8 +1,9 @@
 import re
+
 def parse_music(music_string: str) -> List[int]:
     beats_per_note = {"o": 4, "o|": 2, ".|": 1}
     try:
         music_notes = re.split('\s+', music_string)
     except Exception as e:
-        return [0]
+        return [0] 
     return [beats_per_note.get(note, 0) for note in music_notes]
