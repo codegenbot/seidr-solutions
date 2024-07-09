599cb62a1add112d65d3c2e3b634d054abd642f6
beep boop <githubbot@vadim.me>
2024-07-10 01:30:49 +0200

    Solution 8 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uzrnpb22 Prompt: In file included from 73e62ec0-3189-47ae-886f-ad3b01f9d6b4.cpp:18: 73e62ec0-3189-47ae-886f-ad3b01f9d6b4.cpp:28:26: note:   candidate expects 4 argu ments, 1 provided    28 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 73e62ec0-3189-47ae-886f-ad3b01f9d6b4.cpp:3: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: candidate: ‘template <class _ExecutionPolicy, class _ForwardIterator> __pstl::__internal::__enable_if _execution_policy<_ExecutionPolicy, _ForwardIterator> std::unique(_ExecutionPoli cy&&, _ForwardIterator, _ForwardIterator)’   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note:   template argument deduction/substitution failed: In file included from 73e62ec0-3189-47ae-886f-ad3b01f9d6b4.cpp:18: 73e62ec0-3189-47ae-886f-ad3b01f9d6b4.cpp:28:26: note:   candidate expects 3 argu ments, 1 provided    28 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 int main() {
     vector<int> input = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    vector<int> output;
-    copy(unique(input.begin(), input.end()), input.end(), back_inserter(output));
-    sort(output.begin(), output.end());
+    vector<int> output(input.begin(), input.end());
+    sort(output.begin(), output.end()); 
+    stable_partition(output.begin(), output.end(), [](int x) { return x > 0; }); 
     for (int i : output) {
         cout << i << " ";
     }
