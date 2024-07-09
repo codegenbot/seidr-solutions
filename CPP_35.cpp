c109259e9179eb2b2a6bf6d8b485ea9248c85c41
beep boop <githubbot@vadim.me>
2024-07-09 11:49:56 +0200

    Solution 53 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/pz5ebhy2 Prompt: f3c657f5-55ad-4062-a780-0dce6710b9d2.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | float max_element(vector<float> l){       |                   ^~~~~~ f3c657f5-55ad-4062-a780-0dce6710b9d2.cpp:1:26: error: expected primary-expressio n before ‘float’     1 | float max_element(vector<float> l){       |                          ^~~~~ In file included from f3c657f5-55ad-4062-a780-0dce6710b9d2.cpp:10: f3c657f5-55ad-4062-a780-0dce6710b9d2.cpp: In function ‘int main()’: f3c657f5-55ad-4062-a780-0dce6710b9d2.cpp:12:28: error: ‘max_element’ cannot be u sed as a function    12 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ f3c657f5-55ad-4062-a780-0dce6710b9d2.cpp:12:13: error: ‘abs’ was not declared in  this scope    12 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
 #include <vector>
+#include <assert.h>
 
 namespace std {
-    float max_element(std::vector<float> l) {
+    float max_element(vector<float> l) {
         float result = l[0];
         for(float x : l) {
             if(x > result)
