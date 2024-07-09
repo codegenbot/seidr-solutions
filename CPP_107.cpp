afb63d8cabcac6208087b641282044256252a82b
beep boop <githubbot@vadim.me>
2024-07-09 19:12:41 +0200

    Solution 2 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/y5snjn2h Prompt:       |                    ^~~ eca2663a-171b-45dd-8e9d-5dfc8e8db8b0.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ eca2663a-171b-45dd-8e9d-5dfc8e8db8b0.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ eca2663a-171b-45dd-8e9d-5dfc8e8db8b0.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from eca2663a-171b-45dd-8e9d-5dfc8e8db8b0.cpp:24: eca2663a-171b-45dd-8e9d-5dfc8e8db8b0.cpp: In function ‘int main()’: eca2663a-171b-45dd-8e9d-5dfc8e8db8b0.cpp:34:20: error: ‘even_odd_palindrome’ was  not declared in this scope    34 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ eca2663a-171b-45dd-8e9d-5dfc8e8db8b0.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,24 +1,15 @@
 #include <vector>
 #include <string>
 
-using namespace std;
-
-vector<int> even_odd_palindrome(int n) {
-    vector<int> result(2);
-    int count_even = 0;
-    int count_odd = 0;
-
-    for (int i = 1; i <= n; i++) {
-        string str = to_string(i);
-        if (str == reverse(str).s)
-            if (stoi(str) % 2 == 0)
-                count_even++;
-            else
-                count_odd++;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) return false;
     }
+    return true;
+}
 
-    result[0] = count_even;
-    result[1] = count_odd;
-
-    return result;
+int main() {
+    assert(issame(even_odd_palindrome(1), {0, 1}));
+    return 0;
 }
