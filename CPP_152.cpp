ff215c6fecc78a97f1caa7f30cb0c10d982e3fe2
beep boop <githubbot@vadim.me>
2024-07-09 04:28:22 +0200

    Solution 9 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/v11g24yz Prompt:       |                    ^~~ bda16655-577c-4c91-8796-517f545c6f78.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bda16655-577c-4c91-8796-517f545c6f78.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bda16655-577c-4c91-8796-517f545c6f78.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bda16655-577c-4c91-8796-517f545c6f78.cpp:15: bda16655-577c-4c91-8796-517f545c6f78.cpp: In function ‘int main()’: bda16655-577c-4c91-8796-517f545c6f78.cpp:25:20: error: ‘compare’ was not declare d in this scope    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ bda16655-577c-4c91-8796-517f545c6f78.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,10 +1,8 @@
 #include <vector>
 #include <iostream>
 
-using namespace std;
-
-vector<int> compare(vector<int> game, vector<int> guess) {
-    vector<int> result;
+std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
+    std::vector<int> result;
     for (int i = 0; i < game.size(); i++) {
         if (game[i] == guess[i]) {
             result.push_back(0);
@@ -13,21 +11,4 @@ vector<int> compare(vector<int> game, vector<int> guess) {
         }
     }
     return result;
-}
-
-int main() {
-    vector<int> game, guess;
-    int n;
-    cin >> n;
-    for(int i = 0; i < n; i++) {
-        int temp;
-        cin >> temp;
-        game.push_back(temp);
-        temp = 0;
-        cin >> temp;
-        guess.push_back(temp);
-    }
-    vector<int> output = compare(game, guess);
-    for(auto x:output) cout << x << " ";
-    return 0;
 }
