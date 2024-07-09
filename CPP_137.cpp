d320604558d3a70ab876b2b2c51e804ef7c39774
beep boop <githubbot@vadim.me>
2024-07-09 07:15:46 +0200

    Solution 89 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/50q1gn5t Prompt: 8ddff5e9-f365-4332-bcfc-1d86639b7bd6.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,12 +1,41 @@
 ```cpp
+#include <iostream>
+#include <string>
+#include <boost/any.hpp>
+
+using namespace std;
+using namespace boost;
+
+int main() {
+    int a = 5;
+    float b = 3.14f;
+    cout << "Enter the first input: ";
+    cin >> a;
+    cout << "Enter the second input: ";
+    if (cin.peek() == '.') {
+        string s; 
+        cin >> s; 
+        b = stof(s);
+    } else {
+        cin >> b;
+    }
+    
+    boost::any result = compare_one(boost::any(a), boost::any(b));
+    if (result.type() == typeid(string)) {
+        cout << "The result is: " << boost::any_cast<string>(result) << endl;
+    } else {
+        cout << "The result is: " << boost::any_cast<float>(result) << endl;
+    }
+    
+    return 0;
+}
+
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
         return b;
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
         return a;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
         string str1 = boost::any_cast<string>(a);
         string str2 = boost::any_cast<string>(b);
 
@@ -16,8 +45,7 @@ boost::any compare_one(boost::any a, boost::any b) {
         for (char c : str1) {
             if (!has_decimal1 && !isdigit(c)) {
                 has_decimal1 = true;
-            }
-            else if (isdigit(c)) {
+            } else if (isdigit(c)) {
                 num1 = num1 * 10 + (c - '0');
             }
         }
@@ -25,8 +53,7 @@ boost::any compare_one(boost::any a, boost::any b) {
         for (char c : str2) {
             if (!has_decimal2 && !isdigit(c)) {
                 has_decimal2 = true;
-            }
-            else if (isdigit(c)) {
+            } else if (isdigit(c)) {
                 num2 = num2 * 10 + (c - '0');
             }
         }
@@ -37,8 +64,7 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         else
             return boost::any("None");
-    }
-    else if (a.type() == typeid(string) && (b.type() == typeid(int) || b.type() == typeid(float))) {
+    } else if (a.type() == typeid(string) && (b.type() == typeid(int) || b.type() == typeid(float))) {
         string str = boost::any_cast<string>(a);
 
         int num1 = 0, has_decimal = false;
@@ -46,8 +72,7 @@ boost::any compare_one(boost::any a, boost::any b) {
         for (char c : str) {
             if (!has_decimal && !isdigit(c)) {
                 has_decimal = true;
-            }
-            else if (isdigit(c)) {
+            } else if (isdigit(c)) {
                 num1 = num1 * 10 + (c - '0');
             }
         }
@@ -60,14 +85,10 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         else
             return boost::any("None");
-    }
-    else {
-        float f1 = boost::any_cast<float>(a);
-        float f2 = boost::any_cast<float>(b);
-
-        if (f1 > f2)
+    } else {
+        if (boost::any_cast<float>(a) > boost::any_cast<float>(b))
             return a;
-        else if (f1 < f2)
+        else if (boost::any_cast<float>(a) < boost::any_cast<float>(b))
             return b;
         else
             return boost::any("None");
