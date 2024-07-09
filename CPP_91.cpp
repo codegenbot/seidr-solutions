5b3ab4ac1022950d004df0036258a03d0ec2cd80
beep boop <githubbot@vadim.me>
2024-07-09 09:25:32 +0200

    Solution 10 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lj9nwutv Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from bbd847f7-5096-46be-af22-66d24e51a08f.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ bbd847f7-5096-46be-af22-66d24e51a08f.cpp:7:27:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,3 +1,7 @@
+```cpp
+#include <string>
+#include <iostream>
+
 int is_bored(std::string S){
     int count = 0;
     std::string boredom = "I am bored";
@@ -5,11 +9,19 @@ int is_bored(std::string S){
     while ((pos = S.find(" ", pos)) != std::string::npos) {
         if (S.substr(0, pos).compare(boredom) == 0) {
             count++;
-            S.erase(0, pos + boredom.size());
-            pos = S.find(" ", pos); // Update this line
+            S = S.substr(pos + boredom.size());
+            pos = 0;
         } else {
             pos++;
         }
     }
     return count;
+}
+
+int main() {
+    std::string S; 
+    cin >> S;
+    int result = is_bored(S);
+    cout << "Count: " << result << endl;
+    return 0;
 }
