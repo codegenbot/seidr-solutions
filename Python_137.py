3c35d800c46043a7f91c02162f5ac3ff36ef9029
beep boop <githubbot@vadim.me>
2024-07-09 08:03:31 +0200

    Solution 9 of Python/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/si1tkotb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d22b2a48-a8e7-4681-a28f-501653a23 13f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_137.py
+++ Python_137.py
@@ -1,16 +1,12 @@
 ```
-def compare_numbers():
-    num1 = input("Enter the first number: ")
-    num2 = input("Enter the second number: ")
+def compare_strings(s1: str, s2: str) -> str:
+    if isinstance(s1, str) and isinstance(s2, str):
+        n1 = s1.replace(',', '.').replace(' ', '').lower()
+        n2 = s2.replace(',', '.').replace(' ', '').lower()
 
-    if isinstance(num1, str) and isinstance(num2, (int, float)):
-        return float(num1.replace(',', '.')) if float(num1.replace(',', '.')) > float(num2) else float(num2)
-    elif isinstance(num2, str) and isinstance(num1, (int, float)):
-        return float(num2.replace(',', '.')) if float(num2.replace(',', '.')) > num1 else num1
-    else:
-        if float(num1) > float(num2):
-            return float(num1)
-        elif float(num2) > float(num1):
-            return float(num2)
+        if n1 > n2:
+            return s1
+        elif n2 > n1:
+            return s2
         else:
             return None
