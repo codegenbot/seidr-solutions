29296531333c84c8c16032589cd9bb492a040b13
beep boop <githubbot@vadim.me>
2024-07-09 15:50:14 +0200

    Solution 35 of CPP/56, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5xo5ju7j Prompt:    27 |     int length = sizeof(char) * 22; // size of the array       |     ^~~ a814a9ae-e3d2-4f97-ac23-efedafd8079c.cpp:27:5: error: expected ‘}’ before ‘int’ a814a9ae-e3d2-4f97-ac23-efedafd8079c.cpp:26:1: note: to match this ‘{’    26 | {       | ^ a814a9ae-e3d2-4f97-ac23-efedafd8079c.cpp:29:10: error: declaration of ‘char brac kets []’ shadows a parameter    29 |     char brackets[] = "<<>";       |          ^~~~~~~~ a814a9ae-e3d2-4f97-ac23-efedafd8079c.cpp:5:37: note: ‘const char* brackets’ prev iously declared here     5 | bool correct_bracketing(const char* brackets, int length){       |                         ~~~~~~~~~~~~^~~~~~~~ In file included from a814a9ae-e3d2-4f97-ac23-efedafd8079c.cpp:35: a814a9ae-e3d2-4f97-ac23-efedafd8079c.cpp: In function ‘int main()’: a814a9ae-e3d2-4f97-ac23-efedafd8079c.cpp:37:36: error: too few arguments to func tion ‘bool correct_bracketing(const char*, int)’    37 |     assert (not (correct_bracketing("<><><<><>><>>><>")));       |                  ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~ a814a9ae-e3d2-4f97-ac23-efedafd8079c.cpp:5:6: note: declared here     5 | bool correct_bracketing(const char* brackets, int length){       |      ^~~~~~~~~~~~~~~~~~

--- CPP_56.cpp
+++ CPP_56.cpp
@@ -1,15 +1,14 @@
 #include <cassert>
 #include <cstring>
 #include <iostream>
-#include <string>
 
-bool correct_bracketing(const char* brackets){
+bool correct_bracketing(const char* bracket_string, int length){
     int count = 0;
-    for(int i=0; i<strlen(brackets); i++){
-        if(brackets[i] == '<'){
+    for(int i=0; i<length; i++){
+        if(bracket_string[i] == '<'){
             count++;
         }
-        else if(brackets[i] == '>'){
+        else if(bracket_string[i] == '>'){
             if(count <= 0) {
                 std::cout << "Incorrect: Unbalanced closing bracket at position " << i+1 << "." << std::endl;
                 return false;
@@ -24,9 +23,10 @@ bool correct_bracketing(const char* brackets){
     return true;
 
 int main() {
-    assert(not correct_bracketing("<><><<><>><>>><>"));
+    char bracket_string[] = "<><><<><>><>>><>";
+    assert(!correct_bracketing(bracket_string, sizeof(bracket_string)/sizeof(char)));
     char brackets[] = "<<>";
-    if(correct_bracketing((char*)brackets)) std::cout << "Correct" << std::endl;
+    if(correct_bracketing((char*)brackets, sizeof(brackets)/sizeof(char))) std::cout << "Correct" << std::endl;
     else std::cout << "Incorrect" << std::endl;
     return 0;
 }
