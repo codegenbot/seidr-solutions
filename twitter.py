b8310ceea03a85d85517957756175e8eca3c43f6
beep boop <githubbot@vadim.me>
2024-07-10 00:52:46 +0200

    Solution 51 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/9kcfsxlz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9bf3ebb6-9a01-4e8b-9ddc-3004cd049 3c2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- twitter.py
+++ twitter.py
@@ -1,13 +1,10 @@
+```
+"""
 def validate_tweet(tweet):
-    if len(tweet.encode('ascii', 'replace').decode()) > 140:
+    if len(tweet) > 140:
         return "Too many characters"
-    elif not tweet.strip().isprintable():
+    elif not tweet.strip():
         return "You didn't type anything"
     else:
         return f"Your tweet has {len(tweet)} characters"
-
-print(validate_tweet("")) 
-print(validate_tweet("1")) 
-print(validate_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it"))
-print(validate_tweet("*(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~```)"))
-print(validate_tweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1"))
+"""
