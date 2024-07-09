d6c296367deb336283f76f066381b491c91694ca
beep boop <githubbot@vadim.me>
2024-07-09 02:35:33 +0200

    Solution 45 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/cf3ofget Prompt:    13 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e08f4dd6-f1e7-4925-ad6d-d2a23f184c1b.cpp:13:44: error: ‘b’ was not declared in t his scope    13 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e08f4dd6-f1e7-4925-ad6d-d2a23f184c1b.cpp:13:45: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e08f4dd6-f1e7-4925-ad6d-d2a23f184c1b.cpp:12: e08f4dd6-f1e7-4925-ad6d-d2a23f184c1b.cpp: In function ‘int main()’: e08f4dd6-f1e7-4925-ad6d-d2a23f184c1b.cpp:22:20: error: ‘sorted_list_sum’ was not  declared in this scope    22 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ e08f4dd6-f1e7-4925-ad6d-d2a23f184c1b.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,41 +1,9 @@
-```cpp
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
+    if(a.size() != b.size())
         return false;
-    }
-    
     for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
+        if(a[i] != b[i])
             return false;
-        }
     }
-    
     return true;
-}
-
-int main() {
-    vector<string> a;
-    vector<string> b;
-
-    cout << "Enter the elements of list A (space separated): ";
-    for(string s; cin >> s; ) {
-        a.push_back(s);
-    }
-
-    cout << "Enter the elements of list B (space separated): ";
-    for(string s; cin >> s; ) {
-        b.push_back(s);
-    }
-    
-    if(issame(a, b)) {
-        cout << "List A and List B are same." << endl;
-    } else {
-        cout << "List A and List B are not same." << endl;
-    }
-    
-    return 0;
 }
