135aeb215016b88159bcd1f5b58de45fea90afae
beep boop <githubbot@vadim.me>
2024-07-10 00:27:12 +0200

    Solution 75 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g8hfxvnh Prompt:       |                              ^~~~~~ 8b6c5d53-9c9e-4008-8d99-dd2a361f8ae8.cpp:48:37: error: ‘string’ was not declared  in this scope    48 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8b6c5d53-9c9e-4008-8d99-dd2a361f8ae8.cpp:48:44: error: ‘b’ was not declared in t his scope    48 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8b6c5d53-9c9e-4008-8d99-dd2a361f8ae8.cpp:48:45: error: expression list treated a s compound expression in initializer [-fpermissive]    48 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8b6c5d53-9c9e-4008-8d99-dd2a361f8ae8.cpp:47: 8b6c5d53-9c9e-4008-8d99-dd2a361f8ae8.cpp: In function ‘int main()’: 8b6c5d53-9c9e-4008-8d99-dd2a361f8ae8.cpp:57:20: error: ‘by_length’ was not decla red in this scope    57 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 8b6c5d53-9c9e-4008-8d99-dd2a361f8ae8.cpp:57:19: error: ‘issame’ cannot be used a s a function    57 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -3,13 +3,43 @@
 using namespace std;
 
 vector<string> by_length(vector<int> arr) {
-    vector<string> numbers;
+    vector<string> result;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            numbers.push_back(to_string(num));
+            string str = "";
+            switch (num) {
+                case 1:
+                    str = "One";
+                    break;
+                case 2:
+                    str = "Two";
+                    break;
+                case 3:
+                    str = "Three";
+                    break;
+                case 4:
+                    str = "Four";
+                    break;
+                case 5:
+                    str = "Five";
+                    break;
+                case 6:
+                    str = "Six";
+                    break;
+                case 7:
+                    str = "Seven";
+                    break;
+                case 8:
+                    str = "Eight";
+                    break;
+                case 9:
+                    str = "Nine";
+                    break;
+            }
+            result.push_back(str);
         }
     }
-    sort(numbers.begin(), numbers.end());
-    reverse(numbers.begin(), numbers.end());
-    return numbers;
+    sort(result.begin(), result.end());
+    reverse(result.begin(), result.end());
+    return result;
 }
