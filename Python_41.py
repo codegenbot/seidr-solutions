fe927af45b544a713a6a56e29b4302398ee6cd97
beep boop <githubbot@vadim.me>
2024-07-09 07:12:21 +0200

    Solution 74 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nnfw2j4t Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6d44d2e2-4511-45a6-885d-f138c0f01 35a.py", line 9, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6d44d2e2-4511-45a6-885d-f138c0f01 35a.py", line 6, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^ TypeError: car_race_collision() takes 0 positional arguments but 1 was given

--- Python_41.py
+++ Python_41.py
@@ -1,4 +1,4 @@
-```
-def car_race_collision(*args):
-    car, speed = args
-    return car * (car - 1) // 2
+def car_race_collision():
+    *speed, = map(int, input("Enter speed of the cars: ").split())
+    *car, = map(int, input("Enter number of cars: ").split())
+    return sum(i*(car-i+1)//2 for i in range(1, car+1)) if len(set(car)) == 1 else -1
