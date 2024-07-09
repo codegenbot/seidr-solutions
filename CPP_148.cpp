a9f2b0e23e12c441bcd6ef9b868cc34895e4818d
beep boop <githubbot@vadim.me>
2024-07-09 04:39:10 +0200

    Solution 8 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kttdajki Prompt:       |                              ^~~~~~ e46cffa7-1235-4745-94bf-bd3c01a7c3a2.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e46cffa7-1235-4745-94bf-bd3c01a7c3a2.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e46cffa7-1235-4745-94bf-bd3c01a7c3a2.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e46cffa7-1235-4745-94bf-bd3c01a7c3a2.cpp:32: e46cffa7-1235-4745-94bf-bd3c01a7c3a2.cpp: In function ‘int main()’: e46cffa7-1235-4745-94bf-bd3c01a7c3a2.cpp:42:20: error: ‘bf’ was not declared in this scope    42 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ e46cffa7-1235-4745-94bf-bd3c01a7c3a2.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,6 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 vector<string> bf(string planet1, string planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
     int index1 = -1;
@@ -22,7 +19,7 @@ vector<string> bf(string planet1, string planet2) {
     vector<string> result;
 
     for (int i = 0; i < planets.size(); i++) {
-        if ((i > index1 && i < index2) || (i < index1 && i > index2)) {
+        if (i > index1 && i < index2) {
             result.push_back(planets[i]);
         }
     }
@@ -30,14 +27,4 @@ vector<string> bf(string planet1, string planet2) {
     sort(result.begin(), result.end());
 
     return result;
-}
-
-int main() {
-    string planet1, planet2;
-    cin >> planet1 >> planet2;
-    vector<string> bf_result = bf(planet1, planet2);
-    for (string planet : bf_result) {
-        cout << planet << endl;
-    }
-    return 0;
 }
