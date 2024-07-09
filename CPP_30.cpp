bf31fa2bde07f7b20733aa0936089977d14bae81
beep boop <githubbot@vadim.me>
2024-07-09 03:30:49 +0200

    Solution 36 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jvy931jd Prompt: In file included from /usr/include/c++/11/vector:67,                  from 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp:13: 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp: In function ‘int main()’: 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,48 +1,20 @@
 #include <vector>
-#include <iostream>
 
-bool isSame(vector<float> a, vector<float> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
-            return false;
-    }
-    return true;
-}
-
-int main() {
-    vector<float> l1 = { -1.0f, 2.0f, -3.0f };
-    vector<float> l2 = { -4.0f, 5.0f, -6.0f };
-    vector<float> result1 = get_positive(l1);
-    vector<float> result2 = get_positive(l2);
-
-    if (isSame(result1, vector<float>())) {
-        std::cout << "result1 is empty" << std::endl;
-    }
-    else
-        for (float i : result1)
-            std::cout << i << " ";
-    
-    std::cout << std::endl;
-
-    if (isSame(result2, vector<float>())) {
-        std::cout << "result2 is empty" << std::endl;
-    }
-    else
-        for (float i : result2)
-            std::cout << i << " ";
-    
-    std::cout << std::endl;
-    return 0;
-}
-
-vector<float> get_positive(vector<float> l) {
-    vector<float> result;
-    for(float i : l){
-        if(i > 0) {
-            result.push_back(i);
+std::vector<float> get_positive(std::vector<float> l){
+    std::vector<float> result;
+    for(float num : l) {
+        if(num > 0) {
+            result.push_back(num);
         }
     }
     return result;
+}
+
+bool are_equal(vector<float> a, vector<float> b){
+    return std::equal(a.begin(),a.end(),b.begin());
+}
+
+int main() {
+    assert (are_equal(get_positive({}), {}) );
+    return 0;
 }
