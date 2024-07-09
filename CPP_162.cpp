a76a1cd0896fafc2d2e7def724438cbcfbe64ec1
beep boop <githubbot@vadim.me>
2024-07-09 12:43:11 +0200

    Solution 80 of CPP/162, ratio 0.5 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7v4cf2nv Prompt:    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ 20a09327-ef29-4b44-a141-6f651c028508.cpp:15:32: error: invalid conversion from ‘ const char*’ to ‘char*’ [-fpermissive]    15 |         sprintf(result.c_str() + i * 2, "%02x", md5[i]);       |                 ~~~~~~~~~~~~~~~^~~~~~~       |                                |       |                                const char* In file included from /usr/include/c++/11/cstdio:42,                  from /usr/include/c++/11/ext/string_conversions.h:43,                  from /usr/include/c++/11/bits/basic_string.h:6608,                  from /usr/include/c++/11/string:55,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 20a09327-ef29-4b44-a141-6f651c028508.cpp:1: /usr/include/stdio.h:358:38: note:   initializing argument 1 of ‘int sprintf(cha r*, const char*, ...)’   358 | extern int sprintf (char *__restrict __s,       |                     ~~~~~~~~~~~~~~~~~^~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,19 +1,17 @@
-#include <openssl/ssl.h>
+#include <iostream>
+#include <string>
+#include <openssl/md5.h>
+
+using namespace std;
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
-    
-    unsigned char md[MD5_DIGEST_LENGTH];
-    MD5_CTX ctx;
-    MD5_Init(&ctx);
-    const char* ptr = text.c_str();
-    while (*ptr) MD5_Update(&ctx, ptr, 1);
-    MD5_Final(md, &ctx);
+
+    unsigned char md5[MD5_DIGEST_LENGTH];
+    MD5((const unsigned char*)text.c_str(), text.size(), md5);
 
     string result;
-    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        sprintf(result + strlen(result), "%02x", md[i]);
-    }
-    
+    for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)
+        sprintf(const_cast<char*>(result.c_str() + i * 2), "%02x", md5[i]);
     return result;
 }
