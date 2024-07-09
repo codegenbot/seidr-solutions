2f7e6690da927eb84b0fcbdb61e260b49ea98d8d
beep boop <githubbot@vadim.me>
2024-07-09 05:52:06 +0200

    Solution 13 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/rjpd2b4p Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0d6c4105-adc9-4ad2-98d9-25f8f2e0dcb7.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0d6c4105-adc9-4ad2-98d9-25f8f2e0dcb7.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0d6c4105-adc9-4ad2-98d9-25f8f2e0dcb7.cpp:26: 0d6c4105-adc9-4ad2-98d9-25f8f2e0dcb7.cpp: In function ‘int main()’: 0d6c4105-adc9-4ad2-98d9-25f8f2e0dcb7.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 0d6c4105-adc9-4ad2-98d9-25f8f2e0dcb7.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,8 +1,31 @@
-bool issame(char a, char b) {
-    return (a == '(' && b == ')');
+bool issame(string s) {
+    int count = 0;
+    for(char c : s) {
+        if (c == '(') {
+            count++;
+        } else if (c == ')') {
+            count--;
+        }
+    }
+    return count == 0;
 }
 
 vector<string> separate_paren_groups(string paren_string);
+
+int main() {
+    string input_string;
+    cout << "Enter a string of parentheses:";
+    cin >> input_string;
+
+    vector<string> groups = separate_paren_groups(input_string);
+
+    cout << "Separated groups:" << endl;
+    for (string group : groups) {
+        cout << group << endl;
+    }
+
+    return 0;
+}
 
 vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
@@ -17,7 +40,7 @@ vector<string> separate_paren_groups(string paren_string) {
             open_count++;
         } else if (c == ')') {
             open_count--;
-            if (open_count == 0 && issame(current_group[0], '(')) {
+            if (open_count == 0 && issame(current_group)) {
                 result.push_back(current_group);
                 current_group.clear();
             } else {
