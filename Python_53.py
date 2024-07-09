060274b2feae44c9bc10badbb00e01ebfd526dbc
beep boop <githubbot@vadim.me>
2024-07-09 04:16:41 +0200

    Solution 66 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kl0f1ktf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d4d59269-39f1-4954-ab3b-db6a2cff1 338.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,7 +1,5 @@
 ```
-def add(a=0, b=0):
-    i = 1
-    x = float(input(f"Enter the {a if i==1 else 'second'} number: ")) if input() else a
-    i += 1
-    y = float(input(f"Enter the {b if i==1 else 'second'} number: ")) if input() else b
+def add(a=0, b=0): 
+    x = int(input(f"Enter the first number: {a} " + (str(a) if not a else '')) or str(a)) 
+    y = int(input(f"Enter the second number: {b} " + (str(b) if not b else '')) or str(b)) 
     print(x + y)
