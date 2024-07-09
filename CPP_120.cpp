e7ad75fb090c5395a9738c1676fb6071252198ca
beep boop <githubbot@vadim.me>
2024-07-09 11:03:41 +0200

    Solution 53 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/yn8qbg5a Prompt: /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {std::pair<int, int>}; _Tp = std::pair<int, int>; _Alloc = std::all ocator<std::pair<int, int> >; std::vector<_Tp, _Alloc>::iterator = std::vector<s td::pair<int, int> >::iterator]’ /usr/include/c++/11/bits/vector.tcc:121:21:   required from ‘std::vector<_Tp, _A lloc>::reference std::vector<_Tp, _Alloc>::emplace_back(_Args&& ...) [with _Args  = {std::pair<int, int>}; _Tp = std::pair<int, int>; _Alloc = std::allocator<std ::pair<int, int> >; std::vector<_Tp, _Alloc>::reference = std::pair<int, int>&]’ /usr/include/c++/11/bits/stl_vector.h:1204:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(std::vector<_Tp, _Alloc>::value_type&&) [with _Tp = std ::pair<int, int>; _Alloc = std::allocator<std::pair<int, int> >; std::vector<_Tp , _Alloc>::value_type = std::pair<int, int>]’ /usr/include/c++/11/bits/stl_queue.h:650:13:   required from ‘void std::priority _queue<_Tp, _Sequence, _Compare>::push(std::priority_queue<_Tp, _Sequence, _Comp are>::value_type&&) [with _Tp = std::pair<int, int>; _Sequence = std::vector<std ::pair<int, int> >; _Compare = std::greater<std::pair<int, int> >; std::priority _queue<_Tp, _Sequence, _Compare>::value_type = std::pair<int, int>]’ 545feba4-5ddf-4f1d-afc9-e68ee46a78fc.cpp:22:16:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::pair<int, int> >’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -19,17 +19,16 @@ std::vector<int> maximum(std::vector<int> arr, int k) {
     std::priority_queue<std::pair<int,int>, std::vector<std::pair<int,int>>, std::greater<std::pair<int,int>>> pq;
 
     for(int i = 0; i < arr.size(); i++) {
-        pq.push(std::make_pair(arr[i],i));
+        pq.push({arr[i],i});
     }
 
-    std::vector<int> result;
-
-    for(int i = 0; i < k && !pq.empty(); i++) {
-        result.push_back(pq.top().first);
+    // Keep popping elements from the priority queue until k elements are popped
+    while (!pq.empty() && k--) {
+        arr.erase(std::remove(arr.begin(), arr.end(), pq.top().first), arr.end());
         pq.pop();
     }
 
-    return result;
+    return arr;
 }
 
 int main() {
@@ -40,14 +39,17 @@ int main() {
     std::cout << "Enter the value of k: ";
     std::cin >> k;
 
-    int val[n];  
+    std::vector<int> arr;
     std::cout << "Enter elements: ";
     for (int i = 0; i < n; i++) {
         std::cout << "Enter element at index " << i << ": ";
-        std::cin >> val[i];
+        int val;
+        std::cin >> val;
+        if (!arr.empty()) { 
+            arr.push_back(val);
+        }
     }
 
-    std::vector<int> arr(val, val + n); 
     std::vector<int> result = maximum(arr, k);
     std::cout << "Maximum elements: ";
     for (int i : result) {
