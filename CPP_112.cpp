36f8ee2f54d9951cb2a2c9720c0f6a8a853660e0
beep boop <githubbot@vadim.me>
2024-07-09 19:42:07 +0200

    Solution 32 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/rzkzf2de Prompt: In file included from /usr/include/c++/11/string:39,                  from adcd7609-1a60-4f43-b075-b1bb7ccd8fba.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from adcd7609-1a60-4f43-b075-b1bb7ccd8fba.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ adcd7609-1a60-4f43-b075-b1bb7ccd8fba.cpp:58:44: error: ‘b’ was not declared in t his scope    58 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from adcd7609-1a60-4f43-b075-b1bb7ccd8fba.cpp:57: adcd7609-1a60-4f43-b075-b1bb7ccd8fba.cpp: In function ‘int main()’: adcd7609-1a60-4f43-b075-b1bb7ccd8fba.cpp:67:20: error: ‘reverse_delete’ was not declared in this scope; did you mean ‘timer_delete’?    67 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ adcd7609-1a60-4f43-b075-b1bb7ccd8fba.cpp:67:19: error: ‘issame’ cannot be used a s a function    67 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,61 +1,52 @@
 #include <vector>
 #include <string>
 
-namespace Solution {
-    bool issame(vector<string> a, string s) {
-        if (a.size() != 1 || s.length() == 0)
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
             return false;
-        for (int i = 0; i < a[0].size(); i++) {
-            if (a[0][i] != s[i])
-                return false;
-        }
-        return true;
     }
+    return true;
+}
 
-    vector<string> reverse_delete(string s, string c) {
-        vector<string> result;
+std::vector<std::string> remove_duplicates(std::string s, std::string c) {
+    std::vector<std::string> result;
 
-        // Delete all characters in s that are equal to any character in c
-        for (char& ch : s) {
-            bool found = false;
-            for (char cc : c) {
-                if (ch == cc) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found)
-                result.push_back(string(1, ch));
-        }
-
-        // Check if the result string is palindrome
-        string res_str = "";
-        for (string str : result)
-            res_str += str;
-        bool is_palindrome = false;
-        int start = 0, end = res_str.length() - 1;
-        while (start < end) {
-            if (res_str[start] != res_str[end])
+    for (char& ch : s) {
+        bool found = false;
+        for (char cc : c) {
+            if (ch == cc) {
+                found = true;
                 break;
-            start++;
-            end--;
+            }
         }
-        if (start >= end)
-            is_palindrome = true;
-
-        // Return the result
-        vector<string> final_result;
-        final_result.push_back(res_str);
-        final_result.push_back(is_palindrome ? "True" : "False");
-        return final_result;
+        if (!found)
+            result.push_back(std::string(1, ch));
     }
 
-    int test() {
-        assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
-        return 0;
+    std::string res_str = "";
+    for (std::string str : result)
+        res_str += str;
+    bool is_palindrome = false;
+    int start = 0, end = res_str.length() - 1;
+    while (start < end) {
+        if (res_str[start] != res_str[end])
+            break;
+        start++;
+        end--;
     }
+    if (start >= end)
+        is_palindrome = true;
+
+    std::vector<std::string> final_result;
+    final_result.push_back(res_str);
+    final_result.push_back(std::to_string(is_palindrome));
+    return final_result;
 }
 
 int main() {
-    return Solution::test();
+    assert(issame(remove_duplicates("mamma", "mia"), {"", "True"}));
+    return 0;
 }
