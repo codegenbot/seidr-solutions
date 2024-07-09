51f934aec15267d56d265fbb0970dde217e11383
beep boop <githubbot@vadim.me>
2024-07-09 22:15:39 +0200

    Solution 9 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g2g75hsi Prompt: 15254d98-242c-431d-8f5c-2607d93c3254.cpp:6:5: error: redefinition of ‘int main() ’     6 | int main(){       |     ^~~~ 15254d98-242c-431d-8f5c-2607d93c3254.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from 15254d98-242c-431d-8f5c-2607d93c3254.cpp:5: 15254d98-242c-431d-8f5c-2607d93c3254.cpp: In function ‘int main()’: 15254d98-242c-431d-8f5c-2607d93c3254.cpp:7:13: error: ‘check_if_last_char_is_a_l etter’ was not declared in this scope     7 |     assert (check_if_last_char_is_a_letter("apple pi e ") == false);       |             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -1,25 +1,14 @@
-```
-int sumEvenFibonacci(int n) {
-    int a = 0;
-    int b = 1;
-    int totalSum = 0;
-
-    for (int i = 0; i <= n; ++i) {
-        if ((i % 2 == 0) && (a <= n)) {
-            totalSum += a;
-        }
-        int temp = a;
-        a = b;
-        b = temp + b;
-    }
-
-    return totalSum;
-}
+```c++
+#include<iostream>
+using namespace std;
 
 int main() {
     int n;
-    cout << "Enter the limit: ";
-    cin >> n;
-    cout << "The sum of even Fibonacci numbers up to " << n << " is " << sumEvenFibonacci(n) << endl;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if(n%2==0)
+        cout<<n<<" is even";
+    else
+        cout<<n<<" is odd";
     return 0;
 }
