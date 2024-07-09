8517b6f7a55fee38edc25e786ed415157b71d48f
beep boop <githubbot@vadim.me>
2024-07-09 03:39:41 +0200

    Solution 74 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h5y6hrvk Prompt: 06507e8c-474a-4a76-9c00-36783a9c1c15.cpp:31:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    31 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 06507e8c-474a-4a76-9c00-36783a9c1c15.cpp:34:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    34 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 06507e8c-474a-4a76-9c00-36783a9c1c15.cpp:34:20: error: ‘find_zero’ was not decla red in this scope    34 |         solution = find_zero(coeffs);       |                    ^~~~~~~~~ In file included from 06507e8c-474a-4a76-9c00-36783a9c1c15.cpp:18: 06507e8c-474a-4a76-9c00-36783a9c1c15.cpp:35:21: error: ‘poly’ was not declared i n this scope    35 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 06507e8c-474a-4a76-9c00-36783a9c1c15.cpp:35:17: error: ‘abs’ was not declared in  this scope    35 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,8 +1,11 @@
 double find_zero(vector<double> xs) {
     int n = xs.size();
-    double coeffs = xs[0];
+    double coeffs;
     vector<double> polyCoeffs;
 
+    // Declare and initialize coeffs here
+    coeffs = xs[0];
+    
     for (int i = 1; i < n; i++) {
         if ((i & 1) == 0) {
             polyCoeffs.insert(polyCoeffs.begin(), -coeffs*xs[i]);
@@ -10,6 +13,9 @@ double find_zero(vector<double> xs) {
             coeffs = xs[i];
         }
     }
+
+    // Define the function poly outside of find_zero
+    double poly(double c, double s) { return c + s; }
 
     return -coeffs / coeffs;
 }
