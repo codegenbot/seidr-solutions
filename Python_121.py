442b5ab0cb6b76f9fc91d5e6ef7944c9e2840ed9
beep boop <githubbot@vadim.me>
2024-07-09 04:44:31 +0200

    Solution 96 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4j02uq4y Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc44ed35-f49d-4075-bab2-32cb8fa1e d54.py", line 12, in <module>     check(solution)           ^^^^^^^^ NameError: name 'solution' is not defined

--- Python_121.py
+++ Python_121.py
@@ -1,3 +1,14 @@
-```
-def solution(lst):
+def sum_of_odd_numbers(lst):
+    "Returns the sum of all odd numbers in a given list."
     return sum([i for i in lst if i % 2 != 0])
+
+
+input_list = [1, 2, 3, 4, 5]
+solution = sum_of_odd_numbers(input_list)
+
+
+def check(solution):
+    print("The sum of odd numbers is:", solution)
+
+
+check(solution)
