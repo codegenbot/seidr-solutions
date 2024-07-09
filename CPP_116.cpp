138a702762fb7481297c8f490981a5e62e4d425d
beep boop <githubbot@vadim.me>
2024-07-09 19:28:51 +0200

    Solution 31 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/8puj9fx4 Prompt:       |                    ^~~ 9da8d231-052c-4104-9fc6-7a449f11804f.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9da8d231-052c-4104-9fc6-7a449f11804f.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9da8d231-052c-4104-9fc6-7a449f11804f.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9da8d231-052c-4104-9fc6-7a449f11804f.cpp:12: 9da8d231-052c-4104-9fc6-7a449f11804f.cpp: In function ‘int main()’: 9da8d231-052c-4104-9fc6-7a449f11804f.cpp:22:20: error: ‘sort_array’ was not decl ared in this scope    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 9da8d231-052c-4104-9fc6-7a449f11804f.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,5 +1,9 @@
 bool issame(vector<int> a, vector<int> b) {
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
+    sort(a.begin(), a.end(), [](int x, int y) {
+        return __builtin_popcount(x) < __builtin_popcount(y);
+    });
+    sort(b.begin(), b.end(), [](int x, int y) {
+        return __builtin_popcount(x) < __builtin_popcount(y);
+    });
     return a == b;
 }
