ca74519fc96a1f6ac36df63f01655a6ab389c75a
beep boop <githubbot@vadim.me>
2024-07-09 13:26:16 +0200

    Solution 8 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ujx0k24n Prompt: af92c65c-5d7f-4ee1-b6e6-312eec8de0aa.cpp:36:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    36 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff af92c65c-5d7f-4ee1-b6e6-312eec8de0aa.cpp:39:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    39 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff af92c65c-5d7f-4ee1-b6e6-312eec8de0aa.cpp:39:36: error: ‘find_zero’ cannot be use d as a function    39 |         solution = find_zero(coeffs);       |                                    ^ In file included from af92c65c-5d7f-4ee1-b6e6-312eec8de0aa.cpp:23: af92c65c-5d7f-4ee1-b6e6-312eec8de0aa.cpp:40:25: error: ‘poly’ cannot be used as a function    40 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ~~~~^~~~~~~~~~~~~~~~~~ af92c65c-5d7f-4ee1-b6e6-312eec8de0aa.cpp:40:17: error: ‘abs’ was not declared in  this scope    40 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,21 +1,24 @@
-double poly(vector<double> orig_coeffs, double x) {
+#include <cmath>
+#include <vector>
+
+double poly(std::vector<double> coeffs, double x) {
     double sum = 0;
-    for (int i = 0; i < orig_coeffs.size(); i++) {
-        sum += orig_coeffs[i] * pow(x, i);
+    for (int i = 0; i < coeffs.size(); i++) {
+        sum += coeffs[i] * pow(x, i);
     }
     return sum;
 }
 
-double find_zero(vector<double> coeffs){
-    vector<double> new_coeffs(coeffs); 
+double find_zero(std::vector<double> coeffs){
+    std::vector<double> solution_coeffs(coeffs); 
     double root = 1;
     double precision = 0.00001;
 
     while (true) {
-        double poly_val = poly(new_coeffs, root);
+        double poly_val = poly(coeffs, root);
         if (abs(poly_val) < precision)
             break;
-        root -= poly_val / poly(new_coeffs, root);
+        root -= poly_val / poly(solution_coeffs, root);
     }
     return root;
 }
