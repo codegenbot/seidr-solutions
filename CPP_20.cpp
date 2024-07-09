2f80471f11e001cceb91e82c3dbf732562e743aa
beep boop <githubbot@vadim.me>
2024-07-09 03:17:37 +0200

    Solution 58 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/iv6qkaf5 Prompt: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from fb374c1d-9b42-4daf-a287-b35d58248c58.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ fb374c1d-9b42-4daf-a287-b35d58248c58.cpp:25:36: error: expected primary-expressi on before ‘float’    25 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ fb374c1d-9b42-4daf-a287-b35d58248c58.cpp:25:43: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from fb374c1d-9b42-4daf-a287-b35d58248c58.cpp:24: fb374c1d-9b42-4daf-a287-b35d58248c58.cpp: In function ‘int main()’: fb374c1d-9b42-4daf-a287-b35d58248c58.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -20,7 +20,3 @@ std::pair<float, float> findClosestElements(const std::vector<float>& numbers) {
 
     return closest_pair;
 }
-
-int main() {
-    assert(std::vector<float>({1.1, 2.2, 3.1, 4.1, 5.1}) == findClosestElements({1.1, 2.2, 3.1, 4.1, 5.1}));
-}
