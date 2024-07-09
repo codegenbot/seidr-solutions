51f81fa5ef8481aad5e8fde8a5a6b529b38f9b91
beep boop <githubbot@vadim.me>
2024-07-09 04:40:00 +0200

    Solution 52 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8q1otf1z Prompt: 7a9dc614-fe73-44fc-9922-1a55f363efaf.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | bool is_sorted(vector<int> lst){       |                ^~~~~~ 7a9dc614-fe73-44fc-9922-1a55f363efaf.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | bool is_sorted(vector<int> lst){       |                       ^~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,18 +1,16 @@
-#include <cassert>
 #include <vector>
+#include <algorithm>
 
-bool is_sorted(std::vector<int> lst) {
-    if (lst.size() <= 1)
+bool is_sorted(vector<int> lst){
+    if(lst.size() <= 1)
         return true;
-    for (int i = 1; i < lst.size(); i++) {
-        if (lst[i - 1] > lst[i]) {
+    for(int i = 1; i < lst.size(); i++){
+        if(lst[i-1] > lst[i]){
             return false;
         }
     }
     return true;
-}
 
 int main() {
-    assert(is_sorted({1, 2, 3, 4}) == true);
-    return 0;
+    assert (is_sorted({1, 2, 3, 4}) == true);
 }
