4a09d0311f2934264894c8e5b4598c50f047f2cc
beep boop <githubbot@vadim.me>
2024-07-09 19:16:34 +0200

    Solution 24 of CPP/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/4szsaqcj Prompt: r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | #include <string> 8fda2ba1-3e9d-4f03-823b-21d50743e477.cpp: At global scope: 8fda2ba1-3e9d-4f03-823b-21d50743e477.cpp:39:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    39 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 8fda2ba1-3e9d-4f03-823b-21d50743e477.cpp:7:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     7 | bool issame(vector<string> a, vector<string> b){       |      ^~~~~~ 8fda2ba1-3e9d-4f03-823b-21d50743e477.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 8fda2ba1-3e9d-4f03-823b-21d50743e477.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main(){       |     ^~~~

--- CPP_101.cpp
+++ CPP_101.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-bool issame(vector<string> a, vector<string> b){
+bool areSame(vector<string> a, vector<string> b){
     if(a.size() != b.size()) return false;
     for(int i=0; i<a.size(); i++){
         if(a[i] != b[i]) return false;
@@ -32,6 +32,6 @@ vector<string> words_string(string s){
 }
 
 int main(){
-    assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal"}));
+    assert (areSame(words_string("ahmed     , gamal") , {"ahmed", "gamal"}));
     return 0;
 }
