fc9bf18325d1b15b58e8b368168b5a59fc76f955
beep boop <githubbot@vadim.me>
2024-07-09 10:50:04 +0200

    Solution 79 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/srcez8ua Prompt:       |                    ^~~ d78dfb81-3aa8-4f39-8feb-2566b5fa5619.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d78dfb81-3aa8-4f39-8feb-2566b5fa5619.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d78dfb81-3aa8-4f39-8feb-2566b5fa5619.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d78dfb81-3aa8-4f39-8feb-2566b5fa5619.cpp:16: d78dfb81-3aa8-4f39-8feb-2566b5fa5619.cpp: In function ‘int main()’: d78dfb81-3aa8-4f39-8feb-2566b5fa5619.cpp:26:20: error: ‘unique_digits’ was not d eclared in this scope    26 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ d78dfb81-3aa8-4f39-8feb-2566b5fa5619.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -5,7 +5,18 @@ bool issame(std::vector<int> a, std::vector<int> b){
 }
 
 std::vector<int> unique_digits(std::vector<int> values) {
-    // original implementation of unique_digits function
+    std::vector<int> result;
+    for (int num : values) {
+        while (num != 0) {
+            int digit = num % 10;
+            if (std::find(result.begin(), result.end(), digit) == result.end()) {
+                result.push_back(digit);
+            }
+            num /= 10;
+        }
+    }
+    std::sort(result.begin(), result.end());
+    return result;
 }
 
 int main() {
