d2fff8b213b48a52579b44b2d689d9169c3e2a12
beep boop <githubbot@vadim.me>
2024-07-09 12:46:12 +0200

    Solution 2 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/14drhf0m Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ec388f89-a58d-48ea-9486-fe2e3997af4b.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ec388f89-a58d-48ea-9486-fe2e3997af4b.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ec388f89-a58d-48ea-9486-fe2e3997af4b.cpp:11: ec388f89-a58d-48ea-9486-fe2e3997af4b.cpp: In function ‘int main()’: ec388f89-a58d-48ea-9486-fe2e3997af4b.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ ec388f89-a58d-48ea-9486-fe2e3997af4b.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,37 +1,22 @@
-#include <iostream>
 #include <vector>
+#include <list>
 #include <boost/any.hpp>
 
-using namespace std;
-namespace boost {
-namespace any {
-class bad_any_cast : public bad_cast { };
-template<class T> struct any_cast {
-    const any& a;
-    explicit any_cast(any& a) : a(a) {}
-    operator T() const { if (!a.good()) throw bad_any_cast(); return any_cast<T>(a).get();
-};
-} // namespace any
-} // namespace boost
+bool issame(vector<int> a, vector<int> b) {
+    // function body here
+}
 
-vector<int> filter_integers(list<boost::any> values) {
+vector<int> filter_integers(list<any> values) {
     vector<int> result;
     for (const auto& value : values) {
-        try {
-            int val = boost::any_cast<int>(value);
-            result.push_back(val);
-        } catch (...) {
-            // ignore non-integer values
+        if (boost::any_cast<int>(value).type().is_class<int>()) {
+            result.push_back(boost::any_cast<int>(value));
         }
     }
     return result;
 }
 
-bool issame(vector<int> a,vector<int>b){
-    return (a.size() == b.size()) && equal(a.begin(), a.end(), b.begin());
-}
-
 int main() {
-    assert(issame(filter_integers({3, boost::any("c"), 3, 3, boost::any("a"), boost::any("b")}) ,{3, 3, 3}));
+    assert(issame(filter_integers({3, any('c'), 3, 3, any('a'), any('b')}), {3, 3, 3}));
     return 0;
 }
