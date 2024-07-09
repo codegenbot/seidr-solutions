4232ba5dec9b4aa61e0e57b00ddb405189489243
beep boop <githubbot@vadim.me>
2024-07-09 19:44:20 +0200

    Solution 23 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ymt5ntk8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7199a138-8414-4c21-996d-743b5fce3 a62.py", line 5     return result     ^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_5.py
+++ Python_5.py
@@ -1,2 +1,2 @@
 def complete_code(numbers, delimiter):
-    return [x for sublist in [[num, delimiter] for num in numbers] for x in sublist[:-1]]
+    return [x for sublist in [[num, delimiter] for num in numbers] for x in sublist[:-1]]  
