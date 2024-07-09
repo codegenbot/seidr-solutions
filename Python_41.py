2ec1f510ba22035f5f1f689605707c7fb5bb5639
beep boop <githubbot@vadim.me>
2024-07-09 06:29:17 +0200

    Solution 26 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nnfw2j4t Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8650054f-915e-4b33-85a6-76c2c63b6 41d.py", line 7, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8650054f-915e-4b33-85a6-76c2c63b6 41d.py", line 4, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_41.py
+++ Python_41.py
@@ -1,6 +1,12 @@
-def car_race_collision(cars, max_speed):
-    n = min(cars, int(max_speed**1.5))
-    if cars > n:
-        return n * (n - 1) // 2 + (cars - n) * (max_speed - 1)
-    else:
-        return n * (n - 1) // 2
+```
+def car_race_collision(n: int, speeds: list):
+    total_time = sum(i for i in speeds)
+    pairs = n * (n - 1) // 2
+    collisions = 0
+    for speed in speeds:
+        if speed <= total_time / n:
+            collisions += n * (n - 1) // 2
+        else:
+            collisions += pairs
+        break
+    return collisions
