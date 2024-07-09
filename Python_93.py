ed69ca52c66bf291f4f63a74fabd09c8d4068426
beep boop <githubbot@vadim.me>
2024-07-09 09:28:04 +0200

    Solution 74 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/j4r73pil Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51fa7e10-84cd-4b7d-8919-2a39b1b5d 799.py", line 21, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51fa7e10-84cd-4b7d-8919-2a39b1b5d 799.py", line 17, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -4,9 +4,11 @@ def encode(message):
     for char in message:
         if char.isalpha():
             if char.upper() in vowels:
-                encoded_message += chr((ord(char) + 3 - ord('A')) % 26 + ord('A')).upper()
+                encoded_message += chr(
+                    (ord(char) + 3 - ord("A")) % 26 + ord("A")
+                ).upper()
             else:
-                encoded_message += chr((ord(char) + 2 - ord('A')) % 26 + ord('A'))
+                encoded_message += chr((ord(char) + 2 - ord("A")) % 26 + ord("A"))
         else:
             encoded_message += char
     return encoded_message
