72eea863fe79d54487ef03633b7c35e4c042dc4f
beep boop <githubbot@vadim.me>
2024-07-09 04:05:42 +0200

    Solution 4 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/79g4422q Prompt:     3 | 70896717-7076-47ed-90e2-4f0a0f721f76.cpp:9:32: error: request for member ‘count’  in ‘a’, which is of non-class type ‘int’     9 |              if (bitset<32>(a).count() != bitset<32>(b).count())       |                                ^~~~~ 70896717-7076-47ed-90e2-4f0a0f721f76.cpp:9:57: error: request for member ‘count’  in ‘b’, which is of non-class type ‘int’     9 |              if (bitset<32>(a).count() != bitset<32>(b).count())       |                                                         ^~~~~ 70896717-7076-47ed-90e2-4f0a0f721f76.cpp:10:39: error: request for member ‘count ’ in ‘a’, which is of non-class type ‘int’    10 |                  return bitset<32>(a).count() < bitset<32>(b).count();       |                                       ^~~~~ 70896717-7076-47ed-90e2-4f0a0f721f76.cpp:10:63: error: request for member ‘count ’ in ‘b’, which is of non-class type ‘int’    10 |                  return bitset<32>(a).count() < bitset<32>(b).count();       |                                                               ^~~~~ In file included from 70896717-7076-47ed-90e2-4f0a0f721f76.cpp:17: 70896717-7076-47ed-90e2-4f0a0f721f76.cpp: In function ‘int main()’: 70896717-7076-47ed-90e2-4f0a0f721f76.cpp:27:20: error: ‘sort_array’ was not decl ared in this scope    27 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,9 +1,15 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 vector<int> sort_array(vector<int> arr) {
-    sort(arr.begin(), arr.end(), [](int a, int b) {
-        if (bitset<32>(a).count() == bitset<32>(b).count()) {
-            return a < b;
-        }
-        return bitset<32>(a).count() < bitset<32>(b).count();
-    });
+    sort(arr.begin(), arr.end(),
+        [](int a, int b) {
+            string str_a = bitset<32>(a).to_string();
+            string str_b = bitset<32>(b).to_string();
+
+            return str_a < str_b;
+        });
     return arr;
 }
