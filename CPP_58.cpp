002f8ab7a35c4096167f20c32fbbf3138e5347bf
beep boop <githubbot@vadim.me>
2024-07-09 09:35:47 +0200

    Solution 83 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5av973i7 Prompt:       |                                  ^~~ e1bebe05-c290-481d-928a-be8a6227fc82.cpp:80:39: error: expression list treated a s compound expression in initializer [-fpermissive]    80 | bool issame(vector<int> a,vector<int>b){       |                                       ^ e1bebe05-c290-481d-928a-be8a6227fc82.cpp:88:5: error: redefinition of ‘int main( )’    88 | int main(){       |     ^~~~ e1bebe05-c290-481d-928a-be8a6227fc82.cpp:74:5: note: ‘int main()’ previously def ined here    74 | int main() {       |     ^~~~ In file included from e1bebe05-c290-481d-928a-be8a6227fc82.cpp:79: e1bebe05-c290-481d-928a-be8a6227fc82.cpp: In function ‘int main()’: e1bebe05-c290-481d-928a-be8a6227fc82.cpp:89:20: error: ‘common’ was not declared  in this scope    89 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ e1bebe05-c290-481d-928a-be8a6227fc82.cpp:89:19: error: ‘issame’ cannot be used a s a function    89 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -7,6 +7,10 @@ bool isSame(const std::set<std::string>& s1, const std::set<std::string>& s2) {
     return s1 == s2;
 }
 
+bool sameSets(const std::set<std::string>& a, const std::set<std::string>& b) {
+    return a.size() == b.size() && isSame(a, b);
+}
+
 void solveProblem() {
     std::set<std::string> s1;
     std::set<std::string> s2;
@@ -58,7 +62,7 @@ void solveProblem() {
     }
     std::cout << "\n";
 
-    if(isSame(s1, s2)) {
+    if(sameSets(s1, s2)) {
         std::cout << "Sets are same.\n";
     } else {
         std::cout << "Sets are not same.\n";
