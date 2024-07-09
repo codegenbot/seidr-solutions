5313bda8fd6173584d20b365862b119f57e9c714
beep boop <githubbot@vadim.me>
2024-07-09 02:31:55 +0200

    Solution 17 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fkq142tr Prompt:       |             ^~~~~~ 4e7a4d03-0954-48aa-8d6b-e4db7ad5ad70.cpp:22:20: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 4e7a4d03-0954-48aa-8d6b-e4db7ad5ad70.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4e7a4d03-0954-48aa-8d6b-e4db7ad5ad70.cpp:22:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 4e7a4d03-0954-48aa-8d6b-e4db7ad5ad70.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 4e7a4d03-0954-48aa-8d6b-e4db7ad5ad70.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4e7a4d03-0954-48aa-8d6b-e4db7ad5ad70.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -2,13 +2,12 @@
 #include <vector>
 #include <cmath>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+bool std::vector<int>::issame(const std::vector<int>& a, const std::vector<int>& b) {
     return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
 }
 
 std::vector<int> even_odd_count(int num) {
-    using namespace std;
-    vector<int> result(2, 0);
+    std::vector<int> result(2, 0);
     while (num != 0) {
         int digit = num % 10;
         if (digit % 2 == 0)
