a4b76ccb63bf8a5b2ee65ab3de074e35fbac8ad1
beep boop <githubbot@vadim.me>
2024-07-09 12:05:16 +0200

    Solution 35 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/7olr1cry Prompt:    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 6b27703e-ba4a-4c7c-8151-a8bb2d4afb22.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6b27703e-ba4a-4c7c-8151-a8bb2d4afb22.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 6b27703e-ba4a-4c7c-8151-a8bb2d4afb22.cpp:9: 6b27703e-ba4a-4c7c-8151-a8bb2d4afb22.cpp: In function ‘int main()’: 6b27703e-ba4a-4c7c-8151-a8bb2d4afb22.cpp:19:20: error: ‘sorted_list_sum’ was not  declared in this scope    19 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 6b27703e-ba4a-4c7c-8151-a8bb2d4afb22.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,8 +1,8 @@
-vector<string> sorted_list_sum(const vector<string>& lst) {
-    auto issame = [](vector<string> a, vector<string> b) {
-        return a == b;
-    };
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
+}
 
+vector<string> sorted_list_sum(const vector<string>& lst) {
     vector<string> sorted_lst = lst;
     sorted_lst.erase(remove_if(sorted_lst.begin(), sorted_lst.end(), [](const string& s) { return s.length() % 2 != 0; }), sorted_lst.end());
     sort(sorted_lst.begin(), sorted_lst.end(), [](const string& a, const string& b) {
