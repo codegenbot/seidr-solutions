356a5a99e56e01a82f511bd48b4b59bc8f9ed1d1
beep boop <githubbot@vadim.me>
2024-07-09 20:24:49 +0200

    Solution 54 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/a5qb31cf Prompt: nt, std::pair<int, int> > >; std::_Vector_base<_Tp, _Alloc>::pointer = std::pair <int, std::pair<int, int> >*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {std::pair<int, std::pair<int, int> >}; _Tp = std::pair<int, std::p air<int, int> >; _Alloc = std::allocator<std::pair<int, std::pair<int, int> > >;  std::vector<_Tp, _Alloc>::iterator = std::vector<std::pair<int, std::pair<int, int> > >::iterator]’ /usr/include/c++/11/bits/vector.tcc:121:21:   required from ‘std::vector<_Tp, _A lloc>::reference std::vector<_Tp, _Alloc>::emplace_back(_Args&& ...) [with _Args  = {std::pair<int, std::pair<int, int> >}; _Tp = std::pair<int, std::pair<int, i nt> >; _Alloc = std::allocator<std::pair<int, std::pair<int, int> > >; std::vect or<_Tp, _Alloc>::reference = std::pair<int, std::pair<int, int> >&]’ /usr/include/c++/11/bits/stl_vector.h:1204:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(std::vector<_Tp, _Alloc>::value_type&&) [with _Tp = std ::pair<int, std::pair<int, int> >; _Alloc = std::allocator<std::pair<int, std::p air<int, int> > >; std::vector<_Tp, _Alloc>::value_type = std::pair<int, std::pa ir<int, int> >]’ 8462cf7f-2d87-42a8-9e03-77f5dd9f8f86.cpp:14:29:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::pair<int, std::pair<int, int> > >’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -4,13 +4,19 @@
 #include <queue>
 using namespace std;
 
+struct Node {
+    int val;
+    pair<int,int> pos;
+
+    bool operator<(const Node& other) const {
+        return val > other.val;
+    }
+};
+
 vector<int> minPath(vector<vector<int>> grid, int k) {
     int n = grid.size();
     vector<vector<bool>>(n, vector<bool>(n, false)) visited;
-    priority_queue<pair<int, pair<int, int>>> pq;
-
-    pq = vector<pair<int, pair<int, int>>>();
-    vector<vector<bool>>(n, vector<bool>(n, false)) visited;
+    priority_queue<Node> pq;
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
@@ -24,21 +30,20 @@ vector<int> minPath(vector<vector<int>> grid, int k) {
     vector<int> res;
 
     while (!pq.empty()) {
-        int val = pq.top().first;
-        int x = pq.top().second.first;
-        int y = pq.top().second.second;
-        res.push_back(val);
+        Node node = pq.top();
         pq.pop();
+
+        res.push_back(node.val);
 
         if (k > 0) {
             --k;
 
             for (int dx : {-1, 0, 1}) {
                 for (int dy : {-1, 0, 1}) {
-                    int nx = x + dx;
-                    int ny = y + dy;
+                    int nx = node.pos.first + dx;
+                    int ny = node.pos.second + dy;
 
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[nx][ny]) {
+                    if(nx>=0&&nx<n&&ny>=0&&ny<n&&!visited[nx][ny]){
                         visited[nx][ny] = true;
                         pq.push({grid[nx][ny], {nx, ny}});
                     }
