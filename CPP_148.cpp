0f7c029bcee9d44b8692f06a65f1df49bc942b9c
beep boop <githubbot@vadim.me>
2024-07-09 02:44:37 +0200

    Solution 60 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/a6f2q0q7 Prompt: 9d55b706-a09c-48a9-b07d-50a9be4a3ee2.cpp:46:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    46 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf 9d55b706-a09c-48a9-b07d-50a9be4a3ee2.cpp:46:45: error: expression list treated a s compound expression in initializer [-fpermissive]    46 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 9d55b706-a09c-48a9-b07d-50a9be4a3ee2.cpp:54:5: error: redefinition of ‘int main( )’    54 | int main(){       |     ^~~~ 9d55b706-a09c-48a9-b07d-50a9be4a3ee2.cpp:40:5: note: ‘int main()’ previously def ined here    40 | int main() {       |     ^~~~ In file included from 9d55b706-a09c-48a9-b07d-50a9be4a3ee2.cpp:45: 9d55b706-a09c-48a9-b07d-50a9be4a3ee2.cpp: In function ‘int main()’: 9d55b706-a09c-48a9-b07d-50a9be4a3ee2.cpp:55:19: error: ‘issame’ cannot be used a s a function    55 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,42 +1,4 @@
-#include <vector>
-#include <algorithm>
-#include <string>
-
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    return a == b;
-}
-
-std::vector<std::string> bf(std::string planet1, std::string planet2) {
-    std::vector<std::string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
-    int index1 = -1, index2 = -1;
-    
-    for (int i = 0; i < planets.size(); ++i) {
-        if (planet1 == planets[i]) {
-            index1 = i;
-        }
-        else if (planet2 == planets[i]) {
-            index2 = i;
-        }
-        
-        if (index1 != -1 && index2 != -1)
-            break;
-    }
-    
-    if (index1 == -1 || index2 == -1) {
-        return {};
-    }
-    
-    std::vector<std::string> result;
-    for (int i = 0; i < planets.size(); ++i) {
-        if (index1 < i && i < index2)
-            result.push_back(planets[i]);
-    }
-    
-    std::sort(result.begin(), result.end());
-    
-    return result;
-}
-
+```cpp
 int main() {
-    assert(issame(bf("Jupiter", "Makemake"), {}));
+    assert(isSame(bf("Jupiter", "Makemake"), {})); 
 }
