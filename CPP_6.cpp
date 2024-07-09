7683c200294191dc79e0df3dbea06ca1c22b047b
beep boop <githubbot@vadim.me>
2024-07-09 06:26:57 +0200

    Solution 38 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/73sda81b Prompt: In file included from /usr/include/c++/11/vector:67,                  from cd69f4fa-a710-4bf6-809c-9558ebe7ea5a.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from cd69f4fa-a710-4bf6-809c-9558ebe7ea5a.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ cd69f4fa-a710-4bf6-809c-9558ebe7ea5a.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from cd69f4fa-a710-4bf6-809c-9558ebe7ea5a.cpp:29: cd69f4fa-a710-4bf6-809c-9558ebe7ea5a.cpp: In function ‘int main()’: cd69f4fa-a710-4bf6-809c-9558ebe7ea5a.cpp:39:20: error: ‘parse_nested_parens’ was  not declared in this scope    39 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ cd69f4fa-a710-4bf6-809c-9558ebe7ea5a.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -11,7 +11,7 @@ vector<int> parse_nested_parens(string paren_string) {
     for (char c : paren_string) {
         if (c == '(') {
             depth++;
-            max_depth = std::max(max_depth, depth);
+            max_depth = max(max_depth, depth);
         } else if (c == ')') {
             depth--;
         } else if (c == ' ') {
