8afd2df2b46f7edffa614d421930e32606126e5f
beep boop <githubbot@vadim.me>
2024-07-09 23:49:52 +0200

    Solution 11 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g8hfxvnh Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,22 +1,49 @@
-vector<string> by_length(vector<int> arr){
-    vector<int> v1;
-    for(int i : arr){
-        if(i >= 1 && i <= 9)
-            v1.push_back(i);
+Here is the completed code:
+
+vector<string> by_length(vector<int> arr) {
+    vector<int> nums;
+    for (int i : arr) {
+        if (i >= 1 && i <= 9) {
+            nums.push_back(i);
+        }
     }
-    sort(v1.begin(), v1.end());
-    reverse(v1.begin(), v1.end());
-    vector<string> v2;
-    for(int i : v1){
-        if(i == 1) v2.push_back("One");
-        else if(i == 2) v2.push_back("Two");
-        else if(i == 3) v2.push_back("Three");
-        else if(i == 4) v2.push_back("Four");
-        else if(i == 5) v2.push_back("Five");
-        else if(i == 6) v2.push_back("Six");
-        else if(i == 7) v2.push_back("Seven");
-        else if(i == 8) v2.push_back("Eight");
-        else if(i == 9) v2.push_back("Nine");
+
+    sort(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.end());
+
+    vector<string> result;
+    for (int num : nums) {
+        string str = "";
+        switch (num) {
+            case 1:
+                str = "One";
+                break;
+            case 2:
+                str = "Two";
+                break;
+            case 3:
+                str = "Three";
+                break;
+            case 4:
+                str = "Four";
+                break;
+            case 5:
+                str = "Five";
+                break;
+            case 6:
+                str = "Six";
+                break;
+            case 7:
+                str = "Seven";
+                break;
+            case 8:
+                str = "Eight";
+                break;
+            case 9:
+                str = "Nine";
+        }
+        result.push_back(str);
     }
-    return v2;
+
+    return result;
 }
