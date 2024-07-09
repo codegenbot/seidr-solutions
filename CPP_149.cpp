e783984938f54dc913a1ccc4f4acedbf192351de
beep boop <githubbot@vadim.me>
2024-07-09 05:18:20 +0200

    Solution 47 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0ywi65gh Prompt:    35 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7a4682ff-1033-433f-8c0e-ef040fa2e77b.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 7a4682ff-1033-433f-8c0e-ef040fa2e77b.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~ In file included from 7a4682ff-1033-433f-8c0e-ef040fa2e77b.cpp:34: 7a4682ff-1033-433f-8c0e-ef040fa2e77b.cpp: In function ‘int main()’: 7a4682ff-1033-433f-8c0e-ef040fa2e77b.cpp:44:20: error: ‘sorted_list_sum’ was not  declared in this scope    44 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 7a4682ff-1033-433f-8c0e-ef040fa2e77b.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -2,30 +2,41 @@
 #include <vector>
 #include <string>
 
-bool isSame(vector<string> a, vector<string> b) {
+bool same(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-int sameCount = 0;
-
-int main() {
-    vector<string> lst1 = {"aa", "bb", "cc", "dd"};
-    vector<string> result1 = sortedListSum(lst1);
-    
-    vector<string> lst2 = {{"a", "b"}, {"c", "d"}};
-    vector<string> result2 = sortedListSum(lst2);
-    bool same = isSame(result1, std::vector<std::string>(result2.rbegin(), result2.rend()));
-    
-    return 0;
-}
-
-vector<string> sortedListSum(vector<string> lst) {
+int sorted_list_sum(vector<string> lst) {
     vector<string> result;
     for (const auto& str : lst) {
         if (str.length() % 2 == 0) {
             result.push_back(str);
         }
     }
+    sort(result.begin(), result.end(),
+         [](const string& a, const string& b) {
+             if (a.length() != b.length()) {
+                 return a.length() < b.length();
+             } else {
+                 return a < b;
+             }
+         });
+    int sum = 0;
+    for(const auto &str : result) {
+        sum += str.length();
+    }
+    return sum;
+}
+
+int main() {
+    vector<string> lst = {"aa", "bb", "cc", "dd"};
+    int sum = sorted_list_sum(lst);
+    bool same = same(sorted({{"a", "b"}, {"c", "d"}}), sorted({{"c", "d"}, {"a", "b"}}));
+    return 0;
+}
+
+vector<string> sorted(vector<string> lst) {
+    vector<string> result = lst;
     sort(result.begin(), result.end(),
          [](const string& a, const string& b) {
              if (a.length() != b.length()) {
