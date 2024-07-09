3446da21c15df21ad6fdea61a44dbb07c958bf75
beep boop <githubbot@vadim.me>
2024-07-09 03:07:54 +0200

    Solution 2 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uilznjb0 Prompt:       |                             ^~~ afc7638f-2d5c-42bc-a416-71423cc3a0c5.cpp:24:33: error: expected primary-expressi on before ‘char’    24 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ afc7638f-2d5c-42bc-a416-71423cc3a0c5.cpp:24:38: error: expected primary-expressi on before ‘int’    24 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ afc7638f-2d5c-42bc-a416-71423cc3a0c5.cpp:24:44: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from afc7638f-2d5c-42bc-a416-71423cc3a0c5.cpp:23: afc7638f-2d5c-42bc-a416-71423cc3a0c5.cpp: In function ‘int main()’: afc7638f-2d5c-42bc-a416-71423cc3a0c5.cpp:38:20: error: ‘histogram’ was not decla red in this scope    38 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ afc7638f-2d5c-42bc-a416-71423cc3a0c5.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,24 +1,3 @@
-map<char, int> histogram(string test) {
-    map<char, int> result;
-    stringstream ss(test);
-    string word;
-    while (ss >> word) {
-        for (char c : word) {
-            result[c]++;
-        }
-    }
-    
-    int maxCount = 0;
-    for (auto it : result) {
-        maxCount = max(maxCount, it.second);
-    }
-    
-    map<char, int> res;
-    for (auto it : result) {
-        if (it.second == maxCount) {
-            res[it.first] = it.second;
-        }
-    }
-    
-    return res;
+bool issame(map<char, int> a, map<char, int> b) {
+    return a == b;
 }
