4335fcfeab7f247bd2e00cfd541c6d3bf6cf8185
beep boop <githubbot@vadim.me>
2024-07-09 17:04:38 +0200

    Solution 47 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ok8wkyae Prompt:       |                              ^~~~~~ dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp:17:37: error: ‘string’ was not declared  in this scope    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp:16: dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp: In function ‘int main()’: dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp:26:20: error: ‘total_match’ was not dec lared in this scope    26 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ dcae53f4-3c65-42f4-929b-f6ea4c62c308.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,5 +1,19 @@
 #include <string>
 
+bool issame(string s1, string s2) {
+    if (s1.size() != s2.size()) {
+        return false;
+    }
+    for (int i = 0; i < s1.size(); i++) {
+        if (s1[i] != s2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<string> total_match(vector<string> lst1, vector<string> lst2);
+
 int sum_chars(vector<string> lst) {
     int sum = 0;
     for (string s : lst) {
@@ -8,13 +22,14 @@ int sum_chars(vector<string> lst) {
     return sum;
 }
 
-bool issame(vector<string> lst1, vector<string> lst2) {
-    return sum_chars(lst1) < sum_chars(lst2);
+vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+    if (sum_chars(lst1) < sum_chars(lst2)) {
+        return lst1;
+    }
+    return lst2;
 }
 
-vector<string> total_match(vector<string> lst1, vector<string> lst2);
-
 int main() {
-    // Main function can use total_match function here
+    // Main function code goes here
     return 0;
 }
