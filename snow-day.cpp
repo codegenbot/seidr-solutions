217ca6fa2e36bfab4d0f46c5fde78a2d48aec04f
beep boop <githubbot@vadim.me>
2024-07-09 21:08:59 +0200

    Solution 9 of snow-day, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/4oyglwyr Prompt: /usr/include/c++/11/iomanip:210:12: error: ‘class std::basic_ostream<char>’ has no member named ‘precision’   210 |       __os.precision(__f._M_n);       |       ~~~~~^~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 4abf04d6-886f-4a58-954c-f21d458247f0.cpp:2: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- snow-day.cpp
+++ snow-day.cpp
@@ -1,20 +1,22 @@
+```cpp
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
-    double hours;
+    int hours;
     cout << "Enter the number of hours: ";
     cin >> hours;
     
-    double initialSnow;
+    float initialSnow;
     cout << "Enter the initial snow on the ground (in feet): ";
     cin >> initialSnow;
     
-    double rateOfSnowFall;
+    float rateOfSnowFall;
     cout << "Enter the rate of snow fall per hour (in feet/hour): ";
     cin >> rateOfSnowFall;
     
-    double proportionOfSnowMeltingPerHour;
+    float proportionOfSnowMeltingPerHour;
     cout << "Enter the proportion of snow melting per hour: ";
     cin >> proportionOfSnowMeltingPerHour;
 
@@ -25,7 +27,8 @@ int main() {
         totalSnow -= initialSnow * proportionOfSnowMeltingPerHour;
     }
     
-    cout << "The amount of snow on the ground after " << hours << " hours is: " << totalSnow << endl;
+    cout << "The amount of snow on the ground after " << hours << " hours is: ";
+    cout << std::fixed << std::setiosflags(std::ios_base::floatfield::fixed) << totalSnow << endl;
 
     return 0;
 }
