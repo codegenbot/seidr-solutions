9dca52fc0a0c4c523ee79a4d4b82e83651e395e2
beep boop <githubbot@vadim.me>
2024-07-09 09:16:50 +0200

    Solution 19 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/kzopjomk Prompt:       |                              ^~~~~~ 71ebc768-e0b0-4b5d-ab35-6799fa866e9d.cpp:35:37: error: ‘string’ was not declared  in this scope    35 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 71ebc768-e0b0-4b5d-ab35-6799fa866e9d.cpp:35:44: error: ‘b’ was not declared in t his scope    35 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 71ebc768-e0b0-4b5d-ab35-6799fa866e9d.cpp:35:45: error: expression list treated a s compound expression in initializer [-fpermissive]    35 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 71ebc768-e0b0-4b5d-ab35-6799fa866e9d.cpp:34: 71ebc768-e0b0-4b5d-ab35-6799fa866e9d.cpp: In function ‘int main()’: 71ebc768-e0b0-4b5d-ab35-6799fa866e9d.cpp:44:20: error: ‘numerical_letter_grade’ was not declared in this scope    44 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 71ebc768-e0b0-4b5d-ab35-6799fa866e9d.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -4,8 +4,8 @@
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b);
 
-vector<string> numerical_letter_grade(vector<float> grades) {
-    vector<string> letter_grades;
+std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
+    std::vector<std::string> letter_grades;
     for (float gpa : grades) {
         if (gpa == 4.0)
             letter_grades.push_back("A+");
@@ -37,10 +37,8 @@ vector<string> numerical_letter_grade(vector<float> grades) {
     return letter_grades;
 }
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b){
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return a == b;
 }
 
-int main() {
-    assert(std::equal(numerical_letter_grade({0, 0.7}).begin(), numerical_letter_grade({0, 0.7}).end(), std::vector<std::string>{"E", "D-"}.begin()));
-}
+assert(std::equal(numerical_letter_grade({0, 0.7}).begin(), numerical_letter_grade({0, 0.7}).end(), std::vector<std::string>{"E", "D-"}.begin()));
