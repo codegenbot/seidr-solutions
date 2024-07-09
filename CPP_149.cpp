b371c3c282ce4a67b742a7b2077217de75d4a046
beep boop <githubbot@vadim.me>
2024-07-09 11:58:53 +0200

    Solution 20 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/7olr1cry Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5bde9453-0321-4905-b1b7-44f0961448d0.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5bde9453-0321-4905-b1b7-44f0961448d0.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 5bde9453-0321-4905-b1b7-44f0961448d0.cpp:11: 5bde9453-0321-4905-b1b7-44f0961448d0.cpp: In function ‘int main()’: 5bde9453-0321-4905-b1b7-44f0961448d0.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 5bde9453-0321-4905-b1b7-44f0961448d0.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,10 +1,9 @@
-
 bool issame(const vector<string>& a, const vector<string>& b) {
     return a == b;
 }
 
 vector<string> sorted_list_sum(const vector<string>& lst) {
-    lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s) { return s.length() % 2 != 0; }), lst.end());
+    lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s){ return s.length() % 2 != 0; }), lst.end());
     sort(lst.begin(), lst.end(), [](const string& a, const string& b) {
         if (a.length() == b.length()) {
             return a < b;
