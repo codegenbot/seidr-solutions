8dcd52dcb91d89d59f5760c54a4c8415e445520b
beep boop <githubbot@vadim.me>
2024-07-09 22:47:49 +0200

    Solution 74 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/oaawrlr4 Prompt: Program did not receive expected input

--- Python_140.py
+++ Python_140.py
@@ -3,6 +3,6 @@ import re
 def fix_spaces(text):
     return re.sub(r"\s{2,}", "-", text.replace(" ", "_"))
 
-text = input("Enter text: ")
+text = input("Enter the text: ")
 result = fix_spaces(text)
 print(result)
