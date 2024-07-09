21bd2b1725daebfc9589a295270864bc46ac57d1
beep boop <githubbot@vadim.me>
2024-07-09 23:26:19 +0200

    Solution 4 of Python/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/8swexx5n Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/73f60c8f-b765-4443-abba-f9715e2ad 790.py", line 21, in <module>     check(tri)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/73f60c8f-b765-4443-abba-f9715e2ad 790.py", line 18, in check     assert tri(1) == [1, 3]            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_130.py
+++ Python_130.py
@@ -1,13 +1,14 @@
 def tri(n):
     if n == 0:
         return [0]
-    elif n == 1:
+    if n == 1:
         return [3]
-    else:
-        tribonacci = [3, 1]
-        for i in range(2, n + 1):
-            if i % 2 == 0:
-                tribonacci.append(1 + i // 2)
-            else:
-                tribonacci.append(tribonacci[i - 1] + tribonacci[i - 2] + tribonacci[i - 3])
-        return tribonacci
+    if n == 2:
+        return [1, 3]
+    sequence = [1, 3]
+    for i in range(3, n + 1):
+        if i % 2 == 0:
+            sequence.append(1 + i // 2)
+        else:
+            sequence.append(sequence[i - 1] + sequence[i - 2] + sequence[i - 3])
+    return sequence
