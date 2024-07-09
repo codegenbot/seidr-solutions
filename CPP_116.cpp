a3f469a778651c1e048bb6f8d3aae1d73e9b8ccc
beep boop <githubbot@vadim.me>
2024-07-09 19:28:57 +0200

    Solution 11 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lvdfcyfj Prompt:       |                           ^~~~~~ f9f1f588-f979-4049-828b-17f3161961ef.cpp:23:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from f9f1f588-f979-4049-828b-17f3161961ef.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from f9f1f588-f979-4049-828b-17f3161961ef.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ f9f1f588-f979-4049-828b-17f3161961ef.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f9f1f588-f979-4049-828b-17f3161961ef.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ f9f1f588-f979-4049-828b-17f3161961ef.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     if(a.size()!=b.size()) return false;
     for(int i=0;i<a.size();i++){
         if(a[i]!=b[i]) return false;
