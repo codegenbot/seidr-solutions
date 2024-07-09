d97fa997ce17bb08c00ff261665260aec15f5d34
beep boop <githubbot@vadim.me>
2024-07-09 09:48:46 +0200

    Solution 23 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/9f7ar2i0 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 2a4b5f76-3eb4-40ae-9d0b-41f5ec94bc2c.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,5 +1,28 @@
-int main
-{
+#include <vector>
+#include <algorithm>
+#include <initializer_list>
+
+using namespace std;
+
+vector<int> count_upto(int n) {
+    vector<int> primes;
+    for (int i = 2; i < n; ++i) {
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; ++j) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            primes.push_back(i);
+        }
+    }
+    return primes;
+
+}
+
+void main() {
     int n;
     cout << "Enter a number: ";
     cin >> n;
@@ -9,5 +32,4 @@ int main
         cout << prime << " ";
     }
     cout << endl;
-    return 0;
 }
