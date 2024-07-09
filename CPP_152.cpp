372ff7087fb04af22504d33f1ff92b9225e44705
beep boop <githubbot@vadim.me>
2024-07-09 17:51:32 +0200

    Solution 14 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5m2wl4s0 Prompt: fcfdaf4e-ebb3-4f58-a0a9-9f46a036baad.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ fcfdaf4e-ebb3-4f58-a0a9-9f46a036baad.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -18,3 +18,8 @@ vector<int> compare(vector<int> game, vector<int> guess) {
     }
     return result;
 }
+
+int main() {
+    assert(compare({1, 2, 3, 5}, {-1, 2, 3, 4}) == vector<int>({2, 0, 0, 1}));
+    return 0;
+}
