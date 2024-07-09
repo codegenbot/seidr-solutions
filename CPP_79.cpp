e6d246478ba3d49fd9d0034d05d10e5d51937bfe
beep boop <githubbot@vadim.me>
2024-07-09 08:59:33 +0200

    Solution 7 of CPP/79, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/i23hsp3e Prompt: _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 955134a1-ceab-4567-b996-28491aac1077.cpp:6:26:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 955134a1-ceab-4567-b996-28491aac1077.cpp:6:26:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -5,9 +5,9 @@ std::string decimal_to_binary(int decimal){
     std::string binary = "";
     while(decimal > 0){
         if(decimal % 2 == 0)
-            binary = "0" + binary;
+            binary += "0";
         else
-            binary = "1" + binary;
+            binary += "1";
         decimal /= 2;
     }
     return "db" + binary + "db";
@@ -15,6 +15,5 @@ std::string decimal_to_binary(int decimal){
 
 int main(){
     assert (decimal_to_binary(15) == "db1111db");
-    std::cout << decimal_to_binary(10) << std::endl;
     return 0;
 }
