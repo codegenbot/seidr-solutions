7bb39cf0af402e17999fe695577e9b9b1290d3c5
beep boop <githubbot@vadim.me>
2024-07-09 16:50:35 +0200

    Solution 64 of CPP/61, ratio 0.8333333333333334 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/l0npsmay Prompt: a58f6e6d-098d-4dbf-9dff-7fa1c527e495.cpp:2:5: error: expected unqualified-id bef ore ‘for’     2 |     for (char c : brackets) {       |     ^~~ a58f6e6d-098d-4dbf-9dff-7fa1c527e495.cpp:12:5: error: expected unqualified-id be fore ‘return’    12 |     return count == 0;       |     ^~~~~~ a58f6e6d-098d-4dbf-9dff-7fa1c527e495.cpp:13:1: error: expected declaration befor e ‘}’ token    13 | }       | ^ In file included from a58f6e6d-098d-4dbf-9dff-7fa1c527e495.cpp:15: a58f6e6d-098d-4dbf-9dff-7fa1c527e495.cpp: In function ‘int main()’: a58f6e6d-098d-4dbf-9dff-7fa1c527e495.cpp:17:18: error: ‘correct_bracketing’ was not declared in this scope    17 |     assert (not (correct_bracketing("()()(()())()))()")));       |                  ^~~~~~~~~~~~~~~~~~

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -1,12 +1,15 @@
-int count = 0;
-    for (char c : brackets) {
-        if (c == '(') {
+#include <string>
+
+bool correct_bracketing(const std::string& brackets) {
+    int count = 0;
+    for (int i = 0; i < brackets.size(); ++i) {
+        if (brackets[i] == '(') {
             count++;
-        } else if (c == ')') {
+        } else if (brackets[i] == ')') {
             count--;
-        }
-        if (count < 0) {
-            return false;
+            if (count < 0) {
+                return false;
+            }
         }
     }
     return count == 0;
