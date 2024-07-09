201443c7ac0af0625f6951e87de285e68d3bd431
beep boop <githubbot@vadim.me>
2024-07-09 12:54:32 +0200

    Solution 77 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jkn2px16 Prompt: 2e84e0f4-123d-4c4d-a610-96fd5e0fccf2.cpp:1:10: fatal error: assert: No such file  or directory     1 | #include <assert>       |          ^~~~~~~~ compilation terminated.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,11 +1,19 @@
 #include <cassert>
 #include <iterator>
+#include <vector>
+#include <algorithm>
 
 class _160 {
 public:
     int do_algebra(const std::vector<std::string>& args, const std::vector<int>& numbers);
 
 };
+
+int main() {
+    _160 obj;
+    assert(obj.do_algebra({"//", "*"}, {7, 3, 4}) == 8);
+    return 0;
+}
 
 int _160::do_algebra(const std::vector<std::string>& args, const std::vector<int>& numbers) {
     if (std::find(std::begin(numbers), std::end(numbers), 0) != std::end(numbers)) {
@@ -20,10 +28,4 @@ int _160::do_algebra(const std::vector<std::string>& args, const std::vector<int
         }
     }
     return result;
-}
-
-int main() {
-    _160 obj;
-    assert(obj.do_algebra({"//", "*"}, {7, 3, 4}) == 8);
-    return 0;
 }
