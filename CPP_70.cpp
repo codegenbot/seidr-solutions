869a623e462773d27b86125972f5211a16bd9d4b
beep boop <githubbot@vadim.me>
2024-07-09 10:01:18 +0200

    Solution 93 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qqhcwdm6 Prompt:  std::unordered_multimap<_Key1, _Tp1, _Hash1, _Pred1, _Alloc1>&)’  2148 |     operator==(const unordered_multimap<_Key, _Tp, _Hash, _Pred, _Alloc> & __x,       |     ^~~~~~~~ /usr/include/c++/11/bits/unordered_map.h:2148:5: note:   template argument deduc tion/substitution failed: In file included from /usr/include/c++/11/bits/stl_algobase.h:71,                  from /usr/include/c++/11/vector:60,                  from db4e29aa-2cd2-490e-b1a9-8b76e17600cd.cpp:3: /usr/include/c++/11/bits/predefined_ops.h:270:24: note:   mismatched types ‘cons t std::unordered_multimap<_Key1, _Tp1, _Hash1, _Pred1, _Alloc1>’ and ‘int’   270 |         { return *__it == _M_value; }       |                  ~~~~~~^~~~~~~~~~~ In file included from /usr/include/c++/11/vector:64,                  from db4e29aa-2cd2-490e-b1a9-8b76e17600cd.cpp:3: /usr/include/c++/11/bits/allocator.h:204:7: note: candidate: ‘bool std::operator ==(const std::allocator<int>&, const std::allocator<int>&)’   204 |       operator==(const allocator&, const allocator&) _GLIBCXX_NOTHROW       |       ^~~~~~~~ /usr/include/c++/11/bits/allocator.h:204:18: note:   no known conversion for arg ument 1 from ‘int’ to ‘const std::allocator<int>&’   204 |       operator==(const allocator&, const allocator&) _GLIBCXX_NOTHROW       |                  ^~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <initializer_list>
 #include <vector>
 #include <algorithm>
@@ -18,8 +17,9 @@ std::vector<std::vector<int>> strange_sort_list(std::vector<int> num) {
     while (!num.empty()) {
         int min_val = *std::min_element(num.begin(), num.end());
         for (int i = 0; i < num.size(); i++) {
-            if (*std::min_element(&num[i], &num[i] + 1) == min_val) {
-                result.push_back({&num[i], &num[i] + 1});
+            if (*std::min_element(&num[i], num.end()) == min_val) {
+                std::vector<int> temp(num.begin(), num.end());
+                result.push_back(temp);
                 num.erase(std::remove(num.begin(), num.end(), *result.rbegin()), num.end());
                 break;
             }
@@ -34,6 +34,6 @@ int main() {
     for (int i = 0; i < n; i++) {
         vec[i] = i + 1; 
     }
-    assert(true);
+    assert(issame(std::vector<std::vector<int>>{{vec.begin(), vec.end()}}, {{vec.begin(), vec.end()}}));
     return 0;
 }
