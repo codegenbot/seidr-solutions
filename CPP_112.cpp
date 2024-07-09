019c0e00c917a84ae903f980839d656154ab6703
beep boop <githubbot@vadim.me>
2024-07-09 13:28:19 +0200

    Solution 85 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/mvcgxdwn Prompt:       |                              ^~~~~~ a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp:25: a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp: In function ‘int main()’: a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp:35:20: error: ‘reverse_delete’ was not declared in this scope    35 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,7 +1,25 @@
-return a == b;
+bool issame(vector<string> a, vector<string>b){
+    return a == b;
 }
 
-int main() {
-    assert(issame({}, {"", "True"}));
-    // Other code
+vector<string> reverse_delete(string s, string c) {
+    string result = "";
+    for (char ch : s) {
+        bool found = false;
+        for (char cc : c) {
+            if (ch == cc) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            result += ch;
+        }
+    }
+    string rev = result;
+    reverse(rev.begin(), rev.end());
+    vector<string> res;
+    res.push_back(result);
+    res.push_back(to_string(result == rev));
+    return res;
 }
