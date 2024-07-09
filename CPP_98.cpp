a99ae7bdba2d61bcb79ae1d2f543c345be5ad7aa
beep boop <githubbot@vadim.me>
2024-07-09 07:14:42 +0200

    Solution 17 of CPP/98, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sv0z9zhj Prompt: a30c7c7b-62af-4328-8331-0b7d5dbb9499.cpp:1:17: error: ‘string’ was not declared in this scope     1 | int count_upper(string s){       |                 ^~~~~~ In file included from a30c7c7b-62af-4328-8331-0b7d5dbb9499.cpp:11: a30c7c7b-62af-4328-8331-0b7d5dbb9499.cpp: In function ‘int main()’: a30c7c7b-62af-4328-8331-0b7d5dbb9499.cpp:13:24: error: ‘count_upper’ cannot be u sed as a function    13 |     assert (count_upper("EEEE") == 2);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_98.cpp
+++ CPP_98.cpp
@@ -4,8 +4,7 @@
 int count_upper(std::string s) {
     int count = 0;
     for(int i = 1; i < s.length(); i += 2){
-        if(toupper(s[i-1]) == 'A' || toupper(s[i-1]) == 'E' || toupper(s[i-1]) == 'I' || toupper(s[i-1]) == 'O' || touper
-                (s[i-1]) == 'U'){
+        if(toupper(s[i-1]) == 'A' || toupper(s[i-1]) == 'E' || toupper(s[i-1]) == 'I' || toupper(s[i-1]) == 'O' || toupper(s[i-1]) == 'U'){
             count++;
         }
     }
