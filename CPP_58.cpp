c97f2b8edad319835f5cb20686e674810d57ba95
beep boop <githubbot@vadim.me>
2024-07-09 21:39:05 +0200

    Solution 56 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g34sr5ik Prompt:       |     ^~~~~~ 85f01211-f020-47ed-818a-ef8f1ea03828.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | using namespace std; 85f01211-f020-47ed-818a-ef8f1ea03828.cpp: At global scope: 85f01211-f020-47ed-818a-ef8f1ea03828.cpp:25:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 85f01211-f020-47ed-818a-ef8f1ea03828.cpp:4:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     4 | bool issame(vector<int> a,vector<int> b){       |      ^~~~~~ 85f01211-f020-47ed-818a-ef8f1ea03828.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 85f01211-f020-47ed-818a-ef8f1ea03828.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -5,10 +5,6 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
@@ -18,9 +14,31 @@ vector<int> common(vector<int> l1, vector<int> l2) {
     vector<int> result(intersection.begin(), intersection.end());
     sort(result.begin(), result.end());
     return result;
-
 }
 
 int main() {
-    assert(issame(common({4, 3, 2, 8}, {}), {}));
+    assert(common({4, 3, 2, 8}, {}) == {});
+    // Ask the user for input
+    cout << "Enter first list of integers: ";
+    vector<int> l1;
+    int x;
+    while (cin >> x) {
+        l1.push_back(x);
+    }
+    cin.clear();
+    cin.ignore(10000, '\n');
+    cout << "Enter second list of integers: ";
+    vector<int> l2;
+    while (cin >> x) {
+        l2.push_back(x);
+    }
+    cin.clear();
+    cin.ignore(10000, '\n');
+    vector<int> output = common(l1, l2);
+    // Print the result
+    cout << "Common elements: ";
+    for (int i : output) {
+        cout << i << " ";
+    }
+    cout << endl;
 }
