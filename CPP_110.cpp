fac87ccb9d85a476dc83a021db3213db9ad2295d
beep boop <githubbot@vadim.me>
2024-07-09 02:02:46 +0200

    Solution 42 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c6pooyd5 Prompt: w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const int*; _Tp = int; _Alloc = std:: allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = i nt; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::allocator_type = std ::allocator<int>]’ 355666f1-9f71-4a5d-8b62-f43196734c88.cpp:23:32:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,8 +1,9 @@
+```cpp
 #include <iostream>
 #include <vector>
 using namespace std;
 
-bool exchange(vector<int> lst1, vector<int> lst2) {
+string exchange(vector<int> lst1, vector<int> lst2) {
     for (int num : lst1) {
         if (num % 2 != 0) {
             bool found = false;
@@ -12,16 +13,16 @@ bool exchange(vector<int> lst1, vector<int> lst2) {
                     found = true;
                 }
             }
-            if (!found) return false;
+            if (!found) return "NO";
         }
     }
-    return true;
+    return "YES";
 }
 
 int main() {
     vector<int> lst1 = {1, 3, 4};
     vector<int> lst2 = {2, 4, 6, 8};
-    bool result = exchange(lst1, lst2);
-    if (result) cout << "YES"; else cout << "NO";
+    string result = exchange(lst1, lst2);
+    if (result == "YES") cout << "YES"; else cout << "NO";
     return 0;
 }
