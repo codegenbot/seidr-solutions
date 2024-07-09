94b1068a334238e11b3d8f703a3ddbc815bc3dd1
beep boop <githubbot@vadim.me>
2024-07-09 03:54:53 +0200

    Solution 32 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/v1n8poum Prompt: In file included from cd586e76-7d25-4e86-b5c4-a8b6c939b2f8.cpp:22: cd586e76-7d25-4e86-b5c4-a8b6c939b2f8.cpp: In function ‘int main()’: cd586e76-7d25-4e86-b5c4-a8b6c939b2f8.cpp:32:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    32 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -4,15 +4,7 @@
 
 using namespace std;
 
-bool issame(vector<int> v1, vector<int> v2) {
-    if(v1.size()!=v2.size()) return false;
-    for(int i=0; i<v1.size(); i++) {
-        if(v1[i]!=v2[i]) return false;
-    }
-    return true;
-}
-
-vector<int> strange_sort_list(vector<int> lst) {
+vector<int> strange_sort_vector(vector<int> lst) {
     vector<int> result;
     if (lst.empty()) return result;
 
@@ -25,9 +17,4 @@ vector<int> strange_sort_list(vector<int> lst) {
     }
 
     return result;
-}
-
-int main() {
-    assert(issame(strange_sort_list({111111}), {111111}));
-    return 0;
 }
