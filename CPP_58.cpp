5225ae9fb55ad09cf54c9f15b4dff729d957e6dc
beep boop <githubbot@vadim.me>
2024-07-09 03:41:27 +0200

    Solution 28 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tzm0q5ti Prompt:       |                    ^~~ d4956851-2a5c-4a4d-b690-517e7a66b648.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d4956851-2a5c-4a4d-b690-517e7a66b648.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d4956851-2a5c-4a4d-b690-517e7a66b648.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d4956851-2a5c-4a4d-b690-517e7a66b648.cpp:14: d4956851-2a5c-4a4d-b690-517e7a66b648.cpp: In function ‘int main()’: d4956851-2a5c-4a4d-b690-517e7a66b648.cpp:24:20: error: ‘common’ was not declared  in this scope    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ d4956851-2a5c-4a4d-b690-517e7a66b648.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,15 +1,27 @@
+```cpp
+#include <iostream>
 #include <vector>
-#include <algorithm>
+#include <set>
 
-using namespace std;
+bool issame(vector<int> a,vector<int>b){
+    if(a.size() != b.size())
+        return false;
+    for(int i=0;i<a.size();i++){
+        if(a[i] != b[i])
+            return false;
+    }
+    return true;
+}
 
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
-    set<int> intersection;
+    set<int> common;
     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-        inserter(result, result.begin()));
+                      inserter(common, common.begin()));
 
+    vector<int> result(common.begin(), common.end());
+    sort(result.begin(), result.end());
     return result;
 }
