c800bc1cec25ae703702008fd22f00763755ea27
beep boop <githubbot@vadim.me>
2024-07-09 09:02:50 +0200

    Solution 44 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/vqtew2dn Prompt: In file included from f6ca918e-9f4c-47b2-a572-27995b759f1a.cpp:55: f6ca918e-9f4c-47b2-a572-27995b759f1a.cpp: In function ‘int main()’: f6ca918e-9f4c-47b2-a572-27995b759f1a.cpp:65:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    65 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~ f6ca918e-9f4c-47b2-a572-27995b759f1a.cpp:9:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     9 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ f6ca918e-9f4c-47b2-a572-27995b759f1a.cpp:56:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    56 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -43,7 +43,7 @@ int mainFunction() {
         input.push_back(i);
     }
     vector<int> output = pluck(input);
-    assert(issame({0}, pluck({7, 9, 7, 1}))); 
+    assert(issame(vector<int>(pluck({7, 9, 7, 1})), vector<int>())); // Modified this line
     cout << "Output: ";
     for (int i : output) {
         cout << i << " ";
