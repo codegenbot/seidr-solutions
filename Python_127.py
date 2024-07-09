adb3ab24003efcdc508c17fb51fbcaa2665a6e17
beep boop <githubbot@vadim.me>
2024-07-09 02:44:46 +0200

    Solution 4 of Python/127, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dfv95y65 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/23a012c5-8aac-47b9-abc8-e9a164983 357.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_127.py
+++ Python_127.py
@@ -1,13 +1,12 @@
-```
 def intersection(interval1, interval2):
     start = max(interval1[0], interval2[0])
     end = min(interval1[1], interval2[1])
-    
+
     if start > end:
         return "NO"
-    
+
     length = end - start + 1
-    
+
     def is_prime(n):
         if n <= 1:
             return False
@@ -20,3 +19,5 @@ def intersection(interval1, interval2):
         return "YES"
     else:
         return "NO"
+
+print(intersection((3,5), (4,7)))
