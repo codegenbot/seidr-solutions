cece9f2c9f18bc11f8f4c21b46d955f89de4998c
beep boop <githubbot@vadim.me>
2024-07-09 05:29:58 +0200

    Solution 92 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/usez16r3 Prompt:       |             ^~~~~~ 878d2e6f-b8a4-4fbb-aad6-5663eccf35bb.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 878d2e6f-b8a4-4fbb-aad6-5663eccf35bb.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 878d2e6f-b8a4-4fbb-aad6-5663eccf35bb.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 878d2e6f-b8a4-4fbb-aad6-5663eccf35bb.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 878d2e6f-b8a4-4fbb-aad6-5663eccf35bb.cpp:20: 878d2e6f-b8a4-4fbb-aad6-5663eccf35bb.cpp: In function ‘int main()’: 878d2e6f-b8a4-4fbb-aad6-5663eccf35bb.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,21 +1,25 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-vector<int> maximum(vector<int> arr, int k) {
+std::vector<int> maximum(std::vector<int> arr, int k) {
     if(k > arr.size()) {
-        cout << "Invalid value of k. It must be less than or equal to the size of array." << endl;
+        std::cout << "Invalid value of k. It must be less than or equal to the size of array." << std::endl;
         return {};
     }
     
-    vector<int> result(arr.begin(), arr.begin() + k);
-    sort(result.begin(), result.end());
-    return result;
+    std::vector<int> result(arr.begin(), arr.begin() + k);
+    std::sort(result.begin(), result.end());
+    bool areUnique = true;
+    for(int i = 1; i < result.size(); ++i) {
+        if(result[i] == result[i - 1]) {
+            areUnique = false;
+            break;
+        }
+    }
+    return areUnique ? result : {};
 }
 
 int main() {
-    assert(maximum({1, 2, 3, -23, 243, -400, 0}, 7) == vector<int>({0, 2, 3}));
+    assert(maximum({1, 2, 3, -23, 243, -400, 0}, 7) == std::vector<int>({0, 2, 3}));
     return 0;
+}
