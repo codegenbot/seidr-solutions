bafcbc70dfd8b4b7b3354b0eb0223975ccdfe4e9
beep boop <githubbot@vadim.me>
2024-07-09 03:19:09 +0200

    Solution 2 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/d66di5pm Prompt:       |                    ^~~ 6adffc51-815b-4cb0-8e20-ee51f51afdcd.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6adffc51-815b-4cb0-8e20-ee51f51afdcd.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6adffc51-815b-4cb0-8e20-ee51f51afdcd.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6adffc51-815b-4cb0-8e20-ee51f51afdcd.cpp:13: 6adffc51-815b-4cb0-8e20-ee51f51afdcd.cpp: In function ‘int main()’: 6adffc51-815b-4cb0-8e20-ee51f51afdcd.cpp:23:20: error: ‘sort_array’ was not decl ared in this scope    23 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 6adffc51-815b-4cb0-8e20-ee51f51afdcd.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,10 +1,19 @@
-sort(arr.begin(), arr.end(), [](int a, int b) {
-    int count_a = __builtin_popcount(a);
-    int count_b = __builtin_popcount(b);
-    if (count_a == count_b) {
-        return a < b;
-    }
-    return count_a < count_b;
-});
-return arr;
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+bool issame(const std::vector<int>& a, const std::vector<int>& b){
+    return a == b;
+}
+
+std::vector<int> sort_array(std::vector<int> arr){
+    sort(arr.begin(), arr.end(), [](int a, int b){
+        int countA = __builtin_popcount(a);
+        int countB = __builtin_popcount(b);
+        if (countA == countB) {
+            return a < b;
+        }
+        return countA < countB;
+    });
+    return arr;
 }
