9affc48460cb174814a37c6f2421626302d24cf0
beep boop <githubbot@vadim.me>
2024-07-09 20:01:03 +0200

    Solution 14 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/95qgo5o5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3f95c445-8e2c-44d3-b64d-c5716e683 d7f.py", line 1     return [max([s.count("(") for s in group.split()]) for group in paren_string .split()]     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_6.py
+++ Python_6.py
@@ -1,5 +1,8 @@
 def count_max_parentheses(paren_string):
-    return [max([s.count("(") for s in group.split()]) for group in paren_string.split()] 
+    return [
+        max([s.count("(") for s in group.split()]) for group in paren_string.split()
+    ]
+
 
 paren_string = input()
-count_max_parentheses(paren_string)
+print(count_max_parentheses(paren_string))
