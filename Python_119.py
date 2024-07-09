7b41c765f57e03e72cd9825e9987c6bf44b7f45a
beep boop <githubbot@vadim.me>
2024-07-09 09:05:09 +0200

    Solution 81 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n98cndzy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c3f7843b-2df7-4e2e-b074-3413809e5 56a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -1,16 +1,32 @@
 ```
 def match_parens(lst):
     """stack = []
+    for s in lst:
+        stack_s = []
+        for char in s:
+            if char == '(':
+                stack_s.append(char)
+            elif char == ')':
+                if len(stack_s) == 0:
+                    return 'No'
+                else:
+                    stack_s.pop()
+        while stack_s and stack:
+            if stack_s.pop() != stack.pop():
+                return 'No'
+    return 'Yes'"""
+stack = []
 for s in lst:
+    stack_s = []
     for char in s:
         if char == '(':
-            stack.append(char)
+            stack_s.append(char)
         elif char == ')':
-            if len(stack) == 0:
+            if len(stack_s) == 0:
                 return 'No'
             else:
-                stack.pop()
-if len(stack) == 0:
-    return 'Yes'
-else:
-    return 'No'""".
+                stack_s.pop()
+    while stack_s and stack:
+        if stack_s.pop() != stack.pop():
+            return 'No'
+return 'Yes''
