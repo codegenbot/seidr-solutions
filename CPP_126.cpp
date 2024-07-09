d13043960c132a3973e48e07f6ac300267b0d975
beep boop <githubbot@vadim.me>
2024-07-09 09:59:46 +0200

    Solution 3 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/pi91z427 Prompt: d38b8867-351f-4f6d-96fe-1efdf3ce40c8.cpp: In function ‘int main()’: d38b8867-351f-4f6d-96fe-1efdf3ce40c8.cpp:14:5: error: ‘assert’ was not declared in this scope    14 |     assert(is_sorted({1, 2, 3, 4}) == true);       |     ^~~~~~ d38b8867-351f-4f6d-96fe-1efdf3ce40c8.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <algorithm>   +++ |+#include <cassert>     3 | d38b8867-351f-4f6d-96fe-1efdf3ce40c8.cpp: At global scope: d38b8867-351f-4f6d-96fe-1efdf3ce40c8.cpp:18:5: error: redefinition of ‘int main( )’    18 | int main(){       |     ^~~~ d38b8867-351f-4f6d-96fe-1efdf3ce40c8.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,11 +1,16 @@
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
-bool is_sorted(vector<int> lst){
+bool is_sorted(std::vector<int> lst){
     for(int i = 1; i < lst.size(); i++){
         if(lst[i] <= lst[i-1]){
             return false;
         }
     }
     return true;
+}
+
+int main() {
+    assert(is_sorted({1, 2, 3, 4}) == true);
 }
