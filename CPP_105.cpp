4c0326453f8cd21536109ad1059bc306b095b497
beep boop <githubbot@vadim.me>
2024-07-09 04:07:51 +0200

    Solution 20 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pe563gn9 Prompt:       |                              ^~~~~~ f3f32415-1abc-4868-9a00-bc1e57ef8251.cpp:50:37: error: ‘string’ was not declared  in this scope    50 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f3f32415-1abc-4868-9a00-bc1e57ef8251.cpp:50:44: error: ‘b’ was not declared in t his scope    50 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f3f32415-1abc-4868-9a00-bc1e57ef8251.cpp:50:45: error: expression list treated a s compound expression in initializer [-fpermissive]    50 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f3f32415-1abc-4868-9a00-bc1e57ef8251.cpp:49: f3f32415-1abc-4868-9a00-bc1e57ef8251.cpp: In function ‘int main()’: f3f32415-1abc-4868-9a00-bc1e57ef8251.cpp:59:20: error: ‘by_length’ was not decla red in this scope    59 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ f3f32415-1abc-4868-9a00-bc1e57ef8251.cpp:59:19: error: ‘issame’ cannot be used a s a function    59 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -2,59 +2,53 @@
 #include <string>
 
 bool issame(vector<string> a, vector<string> b) {
-    return a == b;
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
 }
 
 vector<string> by_length(vector<int> arr) {
-    vector<int> numbers;
+    vector<string> result;
+    vector<string> nums;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            numbers.push_back(num);
+            string str = "";
+            switch (num) {
+                case 1:
+                    str = "One";
+                    break;
+                case 2:
+                    str = "Two";
+                    break;
+                case 3:
+                    str = "Three";
+                    break;
+                case 4:
+                    str = "Four";
+                    break;
+                case 5:
+                    str = "Five";
+                    break;
+                case 6:
+                    str = "Six";
+                    break;
+                case 7:
+                    str = "Seven";
+                    break;
+                case 8:
+                    str = "Eight";
+                    break;
+                case 9:
+                    str = "Nine";
+            }
+            nums.push_back(str);
         }
     }
 
-    sort(numbers.begin(), numbers.end());
-    reverse(numbers.begin(), numbers.end());
+    sort(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.end());
 
-    vector<string> result;
-    for (int num : numbers) {
-        string str = "";
-        switch (num) {
-            case 1:
-                str = "One";
-                break;
-            case 2:
-                str = "Two";
-                break;
-            case 3:
-                str = "Three";
-                break;
-            case 4:
-                str = "Four";
-                break;
-            case 5:
-                str = "Five";
-                break;
-            case 6:
-                str = "Six";
-                break;
-            case 7:
-                str = "Seven";
-                break;
-            case 8:
-                str = "Eight";
-                break;
-            case 9:
-                str = "Nine";
-                break;
-        }
-        result.push_back(str);
-    }
-
-    return result;
-}
-
-int main() {
-    assert(by_length({9, 4, 8}) == vector<string>({"Nine", "Eight", "Four"}));
-    // Rest of your main function...
+    return nums;
 }
