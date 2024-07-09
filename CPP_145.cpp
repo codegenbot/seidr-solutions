0a79425063780482ff4c1157b7aaa9a145f5527b
beep boop <githubbot@vadim.me>
2024-07-09 23:07:02 +0200

    Solution 72 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/aeycljm6 Prompt:                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from cfbb8549-70c0-41e3-97ab-4dceb72e8156.cpp:3: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ cfbb8549-70c0-41e3-97ab-4dceb72e8156.cpp:31:34: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from cfbb8549-70c0-41e3-97ab-4dceb72e8156.cpp:30: cfbb8549-70c0-41e3-97ab-4dceb72e8156.cpp: In function ‘int main()’: cfbb8549-70c0-41e3-97ab-4dceb72e8156.cpp:40:20: error: ‘order_by_points’ was not  declared in this scope    40 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ cfbb8549-70c0-41e3-97ab-4dceb72e8156.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,17 +1,15 @@
 ```cpp
 #include <initializer_list>
 #include <algorithm>
-#include <pmr/vector>
 
-bool issame(std::pmr_vector<int> a, std::pmr_vector<int> b) = delete;
-std::vector<int> order_by_points(std::vector<int> nums);
+using namespace std;
 
-bool issame(std::pmr_vector<int> a, std::pmr_vector<int> b) {
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-std::vector<int> order_by_points(std::vector<int> nums) {
-    std::pmr_vector<pair<int, int>> pairs;
+vector<int> order_by_points(vector<int> nums) {
+    vector<pair<int, int>> pairs;
     for (int i = 0; i < nums.size(); ++i) {
         int sumOfDigits = 0;
         int num = nums[i];
@@ -22,9 +20,9 @@ std::vector<int> order_by_points(std::vector<int> nums) {
         pairs.push_back({sumOfDigits, i});
     }
 
-    std::sort(pairs.begin(), pairs.end());
+    sort(pairs.begin(), pairs.end());
 
-    std::vector<int> result;
+    vector<int> result;
     for (const auto& pair : pairs) {
         result.push_back(nums[pair.second]);
     }
@@ -33,6 +31,6 @@ std::vector<int> order_by_points(std::vector<int> nums) {
 }
 
 int main() {
-    assert(issame(order_by_points({0,6,6,-76,-21,23,4}), {-76, -21, 0, 4, 23, 6, 6}));
+    assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0, 4, 23, 6, 6}));
     return 0;
 }
