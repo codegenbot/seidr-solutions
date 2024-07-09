bacaeb4d1272e1d8b050fbf5398a6712c131d346
beep boop <githubbot@vadim.me>
2024-07-09 03:30:18 +0200

    Solution 54 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/o5dcqdjt Prompt:    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ ec50db17-b022-47ea-b827-40e1d6830a5e.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ ec50db17-b022-47ea-b827-40e1d6830a5e.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from ec50db17-b022-47ea-b827-40e1d6830a5e.cpp:23: ec50db17-b022-47ea-b827-40e1d6830a5e.cpp: In function ‘int main()’: ec50db17-b022-47ea-b827-40e1d6830a5e.cpp:33:20: error: ‘filter_by_prefix’ was no t declared in this scope    33 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ ec50db17-b022-47ea-b827-40e1d6830a5e.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
 bool issame(vector<string> a, vector<string> b) {
     if(a.size() != b.size()) return false;
     for(int i = 0; i < a.size(); i++){
@@ -19,7 +15,5 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
     return result;
 
 int main() {
-    vector<string> strings = {"xxx", "asd", "xxy", "john doe", "xxxAAC", "xxx"};
-    string prefix = "xxx";
-    assert(issame(filter_by_prefix(strings, prefix), {"xxx", "xxxAAA", "xxx"}));
+    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAC", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));
 }
