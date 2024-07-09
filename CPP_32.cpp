a8b4ce8c2fbbcf72d6d6acc36840a406212e8348
beep boop <githubbot@vadim.me>
2024-07-09 03:34:00 +0200

    Solution 55 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h5y6hrvk Prompt: ce48ddd9-ffeb-47c4-9b52-6e5dbb5899b2.cpp:25:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    25 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff ce48ddd9-ffeb-47c4-9b52-6e5dbb5899b2.cpp:28:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    28 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff ce48ddd9-ffeb-47c4-9b52-6e5dbb5899b2.cpp:28:20: error: ‘find_zero’ was not decla red in this scope    28 |         solution = find_zero(coeffs);       |                    ^~~~~~~~~ In file included from ce48ddd9-ffeb-47c4-9b52-6e5dbb5899b2.cpp:12: ce48ddd9-ffeb-47c4-9b52-6e5dbb5899b2.cpp:29:21: error: ‘poly’ was not declared i n this scope    29 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ ce48ddd9-ffeb-47c4-9b52-6e5dbb5899b2.cpp:29:17: error: ‘abs’ was not declared in  this scope    29 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,11 +1,37 @@
-vector<double> coefficients;
+```cpp
+double poly(double x, vector<double> coefficients) {
+    double result = 0;
+    for (int i = 0; i < coefficients.size(); i++) {
+        result += coefficients[i] * pow(x, i);
+    }
+    return result;
+}
 
-double poly(vector<double> coefficients) {
+vector<double> get_coefficients() {
+    int degree;
+    cout << "Enter the degree of the polynomial: ";
+    cin >> degree;
+    vector<double> coeffs(degree + 1);
+    for (int i = 0; i <= degree; i++) {
+        cout << "Enter coefficient for x^" << i << ": ";
+        cin >> coeffs[i];
+    }
+    return coeffs;
+}
+
+double find_zero(vector<double> coefficients) {
     double sum = 0;
-    int coeffs = 1;
+    double coeffs = 1;
     for (int i = 1; i < coefficients.size(); i += 2) {
         sum -= coeffs * coefficients[i] / coefficients[0];
         coeffs *= -1;
     }
     return -sum / coefficients[0];
+}
+
+int main() {
+    vector<double> coefficients = get_coefficients();
+    double x = find_zero(coefficients);
+    cout << "The zero of the polynomial is: " << x << endl;
+    return 0;
 }
