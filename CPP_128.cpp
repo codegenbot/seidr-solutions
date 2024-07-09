0a033beb13ef63a0a1d913a6d0a6c6d46b858751
beep boop <githubbot@vadim.me>
2024-07-09 08:40:32 +0200

    Solution 11 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zje411cm Prompt: Complete the following code given the task description and function signature.

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -1,11 +1,16 @@
-int prod_signs(vector<int> arr) {
-    if (arr.empty()) return -32768;
-    int sign = 1;
-    long sum = 0;
-    for (int num : arr) {
-        if (num == 0) continue;
-        sign *= (num > 0 ? 1 : -1);
-        sum += abs(num);
+int prod_signs(vector<int> arr){
+    int sign_product = 1;
+    long long total_sum = 0;
+
+    for (int i : arr) {
+        if (i == 0)
+            return 0;
+        sign_product *= ((i > 0) ? 1 : -1);
+        total_sum += abs(i);
     }
-    return sign * sum;
+
+    if (arr.empty())
+        return -32768;
+
+    return sign_product * total_sum;
 }
