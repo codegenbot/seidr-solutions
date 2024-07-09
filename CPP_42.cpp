02865bbf8ac3b8de3e23d6c9855647a6bfeb8337
beep boop <githubbot@vadim.me>
2024-07-09 08:02:20 +0200

    Solution 20 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/tmdwwmqk Prompt:     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 011e8d96-0bc8-48d3-adf5-8cc1bffc988a.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 011e8d96-0bc8-48d3-adf5-8cc1bffc988a.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 011e8d96-0bc8-48d3-adf5-8cc1bffc988a.cpp:6: 011e8d96-0bc8-48d3-adf5-8cc1bffc988a.cpp: In function ‘int main()’: 011e8d96-0bc8-48d3-adf5-8cc1bffc988a.cpp:16:20: error: ‘incr_list’ was not decla red in this scope    16 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ 011e8d96-0bc8-48d3-adf5-8cc1bffc988a.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,17 +1,30 @@
+#include <iostream>
 #include <vector>
 #include <cassert>
 
-std::vector<int> incr_list(std::vector<int> l){
-    for(int i = 0; i < l.size(); i++){
-        l[i]++;
+using namespace std;
+
+vector<int> incr_list(const vector<int>& l) {
+    vector<int> result = l;
+    for (int i = 0; i < result.size(); i++) {
+        result[i]++;
     }
-    return l;
+    return result;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
+bool issame(const vector<int>& a, const vector<int>& b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-int main(){
+int main() {
     assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
+    return 0;
 }
