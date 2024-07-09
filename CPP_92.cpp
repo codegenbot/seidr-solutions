cfaf02633c0dc0e28b7f68be5977af35868dc544
beep boop <githubbot@vadim.me>
2024-07-09 16:06:30 +0200

    Solution 5 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/g47u22zu Prompt: 23bc7499-2c47-434d-9345-16624e5f0ff9.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ 23bc7499-2c47-434d-9345-16624e5f0ff9.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,3 +1,4 @@
+```
 #include <cassert>
 
 bool any_int(float a, float b, float c) {
@@ -13,5 +14,4 @@ bool any_int(float a, float b, float c) {
 
 int main() {
     assert(any_int(3.01, 4, 7) == false);
-    return 0;
 }
