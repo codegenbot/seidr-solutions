2d5adbdcd02f573d0bd36fe5d0ee05283caa5a03
beep boop <githubbot@vadim.me>
2024-07-09 21:08:18 +0200

    Solution 50 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/bcr5vftd Prompt: d32bc207-5fe3-4013-8b28-945bcf77d277.cpp:1:6: error: ‘string’ in namespace ‘std’  does not name a type     1 | std::string flip_case(std::string str){       |      ^~~~~~ d32bc207-5fe3-4013-8b28-945bcf77d277.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | std::string flip_case(std::string str){

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,12 +1,13 @@
 #include <string>
 
 std::string flip_case(std::string str) {
-    for (int i = 0; i < str.length(); i++) {
+    std::string result = "";
+    for(int i=0; i<str.length(); i++){
         char c = str[i];
-        if (isalpha(c)) {
-            c = (islower(c)) ? toupper(c) : tolower(c);
-            str[i] = c;
-        }
+        if((c >= 'A' && c <= 'Z'))
+            result += (c - ('A'-'a')) + 'a';
+        else
+            result += c;
     }
-    return str;
+    return result;
 }
