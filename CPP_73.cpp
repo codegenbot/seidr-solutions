706ef993291ae2277ab08edd37a43b6f69913b50
beep boop <githubbot@vadim.me>
2024-07-09 03:36:50 +0200

    Solution 2 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/j5jk9irh Prompt: Complete the following code given the task description and function signature.

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,16 +1,33 @@
-int smallest_change(vector<int> arr) {
-    int left = 0, right = arr.size() - 1;
-    int changes = 0;
-
-    while (left < right) {
-        if (arr[left] != arr[right]) {
-            changes++;
-            right--;
-        } else {
-            left++;
-            right--;
+int smallest_change(vector<int> arr){
+    int n = arr.size();
+    vector<vector<bool>> dp(n, vector<bool>(n));
+    for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+            if(i == j) dp[i][j] = true;
+            else if(arr[i] == arr[j]) dp[i][j] = true;
+            else dp[i][j] = false;
         }
     }
-
-    return changes;
+    int ans = 0;
+    for(int i = 0; i < n-1; i++){
+        if(dp[0][i]){
+            continue;
+        }
+        else{
+            int left = i+1, right = n-i-1;
+            while(left < right){
+                if(arr[left] == arr[right]){
+                    left++;
+                    right--;
+                }
+                else break;
+            }
+            if(left >= right) ans += (n - 2*i - 1);
+            else{
+                int mid = min(left, right);
+                ans += (mid - i - 1)*2 + 1;
+            }
+        }
+    }
+    return ans;
 }
