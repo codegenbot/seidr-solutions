0991fb07f3b2fb838b93a91d8c3234aec6625872
beep boop <githubbot@vadim.me>
2024-07-09 20:28:49 +0200

    Solution 25 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/m3xm8wcy Prompt: 4fc86055-ed2c-4be9-b077-ab025fe8abe7.cpp:2:21: error: ‘string’ is not a member o f ‘std’     2 | int mastermind(std::string code, std::string guess) {       |                     ^~~~~~ 4fc86055-ed2c-4be9-b077-ab025fe8abe7.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | using namespace std; 4fc86055-ed2c-4be9-b077-ab025fe8abe7.cpp:2:39: error: ‘string’ is not a member o f ‘std’     2 | int mastermind(std::string code, std::string guess) {       |                                       ^~~~~~ 4fc86055-ed2c-4be9-b077-ab025fe8abe7.cpp:2:39: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? 4fc86055-ed2c-4be9-b077-ab025fe8abe7.cpp:2:51: error: expression list treated as  compound expression in initializer [-fpermissive]     2 | int mastermind(std::string code, std::string guess) {       |                                                   ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,25 +1,23 @@
 #include <string>
+using namespace std;
 
 int mastermind(string code, string guess) {
-    int black = 0;
-    int white = 0;
-
-    map<char, int> code_count;
-    for (char c : code) {
-        code_count[c]++;
-    }
-
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            black++; // Correct color, correct place - black peg
+    int whitePegs = 0;
+    int blackPegs = 0;
+    
+    for(int i=0; i<4; i++) {
+        if(code[i] == guess[i]) {
+            blackPegs++;
         } else {
-            int count = code_count[guess[i]];
-            if (count > 0) {
-                white++;
-                code_count[guess[i]]--; // Decrement the count
+            bool found = false;
+            for(int j=0; j<4; j++) {
+                if(guess[j] == code[i] && !found) {
+                    whitePegs++;
+                    found = true;
+                }
             }
         }
     }
-
-    return black + white;
+    
+    return blackPegs + whitePegs;
 }
