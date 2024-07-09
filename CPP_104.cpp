6f1908463c4516215da8ed400861cf6811b3a66f
beep boop <githubbot@vadim.me>
2024-07-09 10:21:48 +0200

    Solution 39 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/srcez8ua Prompt:  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d9782df0-c11b-4d36-856a-961cb110a676.cpp:18:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from d9782df0-c11b-4d36-856a-961cb110a676.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from d9782df0-c11b-4d36-856a-961cb110a676.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d9782df0-c11b-4d36-856a-961cb110a676.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from d9782df0-c11b-4d36-856a-961cb110a676.cpp:17: d9782df0-c11b-4d36-856a-961cb110a676.cpp: In function ‘int main()’: d9782df0-c11b-4d36-856a-961cb110a676.cpp:27:20: error: ‘unique_digits’ was not d eclared in this scope    27 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
-#include <unordered_set>
+#include <set>
 
 bool issame(std::vector<int> a, std::vector<int> b){
     if (a.size() != b.size()) {
@@ -16,23 +16,18 @@ bool issame(std::vector<int> a, std::vector<int> b){
 }
 
 std::vector<int> unique_digits(std::vector<int> values) {
-    std::unordered_set<int> uniqueSet;
+    std::set<int> uniqueSet;
     for (int num : values) {
-        while (num > 0) {
+        while (num) {
             uniqueSet.insert(num % 10);
             num /= 10;
         }
     }
-    
-    std::vector<int> uniqueDigits;
-    for (int digit : uniqueSet) {
-        uniqueDigits.push_back(digit);
-    }
-    
+    std::vector<int> uniqueDigits(uniqueSet.begin(), uniqueSet.end());
     return uniqueDigits;
 }
 
 int main() {
-    assert(issame(unique_digits({135, 103, 31}), {1, 3, 5}));
+    assert(issame(unique_digits({135, 103, 31}), {31, 135}));
     return 0;
 }
