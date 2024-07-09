0c40299a75de47df0a96fdea301ba6407097c0fe
beep boop <githubbot@vadim.me>
2024-07-09 07:36:38 +0200

    Solution 4 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/x8c0rtpg Prompt: 777b1812-23ac-484b-baac-0a81b883c527.cpp:5:24: error: ‘vector’ was not declared in this scope     5 | bool pairs_sum_to_zero(vector<int> l) {       |                        ^~~~~~ 777b1812-23ac-484b-baac-0a81b883c527.cpp:2:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?     1 | #include <set>   +++ |+#include <vector>     2 | 777b1812-23ac-484b-baac-0a81b883c527.cpp:5:31: error: expected primary-expressio n before ‘int’     5 | bool pairs_sum_to_zero(vector<int> l) {       |                               ^~~ In file included from 777b1812-23ac-484b-baac-0a81b883c527.cpp:16: 777b1812-23ac-484b-baac-0a81b883c527.cpp: In function ‘int main()’: 777b1812-23ac-484b-baac-0a81b883c527.cpp:18:30: error: ‘pairs_sum_to_zero’ canno t be used as a function    18 |     assert (pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);       |             ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -3,13 +3,8 @@
 
 using namespace std;
 
-bool pairs_sum_to_zero(vector<vector<int>> l) {
-    set<int> s;
-    for (const auto& v : l) {
-        for (int i : v) {
-            s.insert(i);
-        }
-    }
+bool pairs_sum_to_zero(vector<int> l) {
+    set<int> s(l.begin(), l.end());
     for (int i = 0; i != s.end(); i++) { 
         int complement = -s[i];
         if (find(s.begin() + i, s.end(), complement) != s.end()) {
@@ -17,4 +12,10 @@ bool pairs_sum_to_zero(vector<vector<int>> l) {
         }
     }
     return false;
+}
+
+int main() {
+    assert(pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);
+    // test the function
+    return 0;
 }
