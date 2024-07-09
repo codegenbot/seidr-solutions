e97168fa3500ef03dc5efc5ac55489d54dda1a04
beep boop <githubbot@vadim.me>
2024-07-09 09:12:47 +0200

    Solution 29 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zje411cm Prompt: 91ad69da-6918-4956-a785-50d004a0865e.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int prod_signs(vector<int> arr){       |                ^~~~~~ 91ad69da-6918-4956-a785-50d004a0865e.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int prod_signs(vector<int> arr){       |                       ^~~ In file included from 91ad69da-6918-4956-a785-50d004a0865e.cpp:14: 91ad69da-6918-4956-a785-50d004a0865e.cpp: In function ‘int main()’: 91ad69da-6918-4956-a785-50d004a0865e.cpp:16:23: error: ‘prod_signs’ cannot be us ed as a function    16 |     assert (prod_signs({-1, 1, 1, 0}) == 0);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-int prod_signs(std::vector<int> arr) {
+int prod_signs(vector<int> arr) {
     int sign_product = 1;
     long sum = 0;
 
@@ -10,5 +10,5 @@ int prod_signs(std::vector<int> arr) {
         sign_product *= (i > 0 ? 1 : -1);
     }
 
-    return sign_product * sum > 0 ? sum : INT_MIN;
+    return sign_product * sum > 0 ? sum : -32768;
 }
