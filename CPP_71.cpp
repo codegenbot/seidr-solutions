a68d307272b4eda6c3f12be62cadf38aa050e05c
beep boop <githubbot@vadim.me>
2024-07-09 23:26:46 +0200

    Solution 4 of CPP/71, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/2khkmhzp Prompt: Complete the following code given the task description and function signature.

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,7 +1,8 @@
+#include <cmath>
+
 float triangle_area(float a, float b, float c) {
-    if (a + b > c && a + c > b && b + c > a) {
-        float s = (a + b + c) / 2;
-        return round(((s * (s - a)) * (s - b)) * (s - c) / 6.0);
+    if ((a + b > c) && (a + c > b) && (b + c > a)) {
+        return 0.5f * sqrt(pow(c, 2) - pow(a, 2) - pow(b, 2));
     } else {
         return -1;
     }
