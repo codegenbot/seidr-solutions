d99de4ad1beaf4fabc6943c6430fd8a8ac870b53
beep boop <githubbot@vadim.me>
2024-07-09 04:22:34 +0200

    Solution 4 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/kih5li8d Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9a175d83-45a4-43d4-82ef-50e2e1da3 568.py", line 12     print(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214 876*%^#!(@^$_!@^%#$(!#@%$(01234~~``)")          ^ SyntaxError: '(' was never closed

--- twitter.py
+++ twitter.py
@@ -1,5 +1,5 @@
 def validate_tweet(tweet):
-    if len(tweet.replace('\\', '')) > 140:
+    if len(tweet) > 140:
         return "Too many characters"
     elif tweet == "":
         return "You didn't type anything"
@@ -9,5 +9,5 @@ def validate_tweet(tweet):
 print(validate_tweet(" "))
 print(validate_tweet("1"))
 print(validate_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it"))
-print(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``)")
+print(validate_tweet("40172875*&(&%%(^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``)")
 print(validate_tweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1"))
