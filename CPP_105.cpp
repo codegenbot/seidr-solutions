1e85eecee0033cd9d93b9a7b4ef0d8bb15cf71d2
beep boop <githubbot@vadim.me>
2024-07-09 07:21:56 +0200

    Solution 16 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/siauvrg0 Prompt:       |                              ^~~~~~ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 964a0b5c-562f-4158-92fc-249bca43033b.cpp:50: 964a0b5c-562f-4158-92fc-249bca43033b.cpp: In function ‘int main()’: 964a0b5c-562f-4158-92fc-249bca43033b.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,17 +1,7 @@
-#include <string>
+bool issame(vector<string> a, vector<string>b){
+    return a == b;
+}
 
-vector<string> by_length(vector<int> arr) {
-    vector<int> v;
-    for (int i : arr) {
-        if (i >= 1 && i <= 9) {
-            v.push_back(i);
-        }
-    }
-    sort(v.begin(), v.end());
-    reverse(v.begin(), v.end());
-    vector<string> res;
-    for (int i : v) {
-        res.push_back(to_string(i));
-    }
-    return res;
+int main() {
+    assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));
 }
