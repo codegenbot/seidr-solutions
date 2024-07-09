ea75a40475ea49241564bc196fb74c3563982ad1
beep boop <githubbot@vadim.me>
2024-07-09 02:26:42 +0200

    Solution 34 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/cf3ofget Prompt: >*; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11 ::basic_string<char> >]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = s td::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11::basic_str ing<char> >; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<std::__cx x11::basic_string<char> >]’ 624353bf-ba14-4067-bd5a-d62644fd7bd6.cpp:19:61:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’ 624353bf-ba14-4067-bd5a-d62644fd7bd6.cpp: In function ‘bool issame(...)’: 624353bf-ba14-4067-bd5a-d62644fd7bd6.cpp:16:1: warning: control reaches end of n on-void function [-Wreturn-type]    16 | }       | ^

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,10 +1,4 @@
 ```cpp
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 bool issame(int x, int y) {
     if (x > y) {
         return false;
@@ -13,53 +7,4 @@ bool issame(int x, int y) {
     } else {
         return false;
     }
-}
-
-vector<string> sorted_list_sum(vector<string>& lst) {
-    // Remove strings with odd lengths from the vector
-    for (auto it = lst.begin(); it != lst.end(); ) {
-        if (it->length() % 2) {
-            it = lst.erase(it);
-        } else {
-            ++it;
-        }
-    }
-
-    // Sort the remaining vector by length and then alphabetically
-    sort(lst.begin(), lst.end(),
-        [](const string& a, const string& b) {
-            if (a.length() == b.length()) {
-                return a < b;
-            }
-            return a.length() < b.length();
-        });
-
-    return lst;
-}
-
-int main() {
-    vector<string> lst = {"cat", "dog", "elephant", "a", "b"};
-    
-    // Sort the list by length and then alphabetically
-    sort(lst.begin(), lst.end(),
-        [](const string& a, const string& b) {
-            if (a.length() == b.length()) {
-                return a < b;
-            }
-            return a.length() < b.length();
-        });
-
-    for (auto i : lst)
-    {
-        cout << i << " ";
-    }
-
-    vector<string> result = sorted_list_sum(lst);
-
-    for (auto i : result)
-    {
-        cout << i << " ";
-    }
-
-    return 0;
 }
