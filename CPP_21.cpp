317b903889b44945092cf8503c9aaca279ee8898
beep boop <githubbot@vadim.me>
2024-07-09 11:21:57 +0200

    Solution 29 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5dgajo98 Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 5d4ac8be-24bb-4a1c-9db6-6d51aa3f6bd6.cpp:15:36: error: expected primary-expressi on before ‘float’    15 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 5d4ac8be-24bb-4a1c-9db6-6d51aa3f6bd6.cpp:15:43: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 5d4ac8be-24bb-4a1c-9db6-6d51aa3f6bd6.cpp:14: 5d4ac8be-24bb-4a1c-9db6-6d51aa3f6bd6.cpp: In function ‘int main()’: 5d4ac8be-24bb-4a1c-9db6-6d51aa3f6bd6.cpp:24:20: error: ‘rescale_to_unit’ was not  declared in this scope    24 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 5d4ac8be-24bb-4a1c-9db6-6d51aa3f6bd6.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -3,13 +3,11 @@
 #include <algorithm>
 
 bool issame(std::vector<float> a, std::vector<float> b) {
-    if (a.size() != b.size()) {
+    if (a.size() != b.size())
         return false;
-    }
     for (int i = 0; i < a.size(); i++) {
-        if (std::abs(a[i] - b[i]) > 1e-9) { // Compare with some small tolerance
+        if (std::abs(a[i] - b[i]) > 1e-9)
             return false;
-        }
     }
     return true;
 }
