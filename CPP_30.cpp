64f603b7e7db7253b277274512cf3483ba6fe38f
beep boop <githubbot@vadim.me>
2024-07-09 13:52:20 +0200

    Solution 22 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/k551qs2a Prompt:       |                    ^~~~~ d397f115-a414-43c8-802b-24a0dbfdcc8e.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ d397f115-a414-43c8-802b-24a0dbfdcc8e.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ d397f115-a414-43c8-802b-24a0dbfdcc8e.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from d397f115-a414-43c8-802b-24a0dbfdcc8e.cpp:11: d397f115-a414-43c8-802b-24a0dbfdcc8e.cpp: In function ‘int main()’: d397f115-a414-43c8-802b-24a0dbfdcc8e.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ d397f115-a414-43c8-802b-24a0dbfdcc8e.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,6 +1,6 @@
-#include <vector>
+#include<vector>
 
-vector<float> get_positive(vector<float> l){
+vector<float> get_positive(vector<float> l) {
     vector<float> result;
     for (float num : l) {
         if (num > 0) {
@@ -11,11 +11,11 @@ vector<float> get_positive(vector<float> l){
 }
 
 bool issame(float a, float b) {
-    return a == b;
+    return (a == b);
 }
 
 int main() {
-    vector<float> numbers = {1.5, -2.3, 3.7, -4.2, 5.1};
+    vector<float> numbers = {1.5, -2.3, 3.0, 0.0, 4.7};
     vector<float> positive_numbers = get_positive(numbers);
     for (float num : positive_numbers) {
         cout << num << " ";
