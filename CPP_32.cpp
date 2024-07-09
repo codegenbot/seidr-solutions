e106026ace9370e51bfa3c71a53d755c7963acac
beep boop <githubbot@vadim.me>
2024-07-09 07:55:55 +0200

    Solution 80 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/7t5tgpwa Prompt: b2e59594-7e51-43cd-b215-5a9b8764a4eb.cpp: In function ‘int main()’: b2e59594-7e51-43cd-b215-5a9b8764a4eb.cpp:33:25: error: ‘rand’ was not declared i n this scope    33 |         ncoeff = 2 * (1+rand()%4);       |                         ^~~~ b2e59594-7e51-43cd-b215-5a9b8764a4eb.cpp:34:9: error: ‘vector’ was not declared in this scope    34 |         vector<double> coeffs = {};       |         ^~~~~~ b2e59594-7e51-43cd-b215-5a9b8764a4eb.cpp:34:9: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from b2e59594-7e51-43cd-b215-5a9b8764a4eb.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from b2e59594-7e51-43cd-b215-5a9b8764a4eb.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b2e59594-7e51-43cd-b215-5a9b8764a4eb.cpp:34:16: error: expected primary-expressi on before ‘double’    34 |         vector<double> coeffs = {};       |                ^~~~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,10 +1,10 @@
 #include <vector>
-#include <cmath>
+#include <cstdlib>
 
 double poly(std::vector<double> coeffs, double x){
     double result = 0.0;
     for(int i=0; i<coeffs.size(); i++){
-        result += coeffs[i] * std::pow(x, i);
+        result += coeffs[i] * pow(x, i);
     }
     return result;
 }
@@ -13,22 +13,12 @@ double abs(double num){
     return num >= 0 ? num : -num;
 }
 
-double find_zero(std::vector<double> coeffs){
+double find_zero(std::vector<double> coeffs){ // Change vector to std::vector
     double a = coeffs[0];
     double b = coeffs[1];
     return -b/a;
 }
 
-int main() {
-    srand(time(nullptr)); 
-    int ncoeff = 2 * (1 + rand() % 4);
-    std::vector<double> coeffs(ncoeff, 0.0);
-    for(int i = 0; i < ncoeff; i++){
-        coeffs[i] = rand() % 10;
-    }
-
-    double solution = find_zero(coeffs);
-    assert(abs(poly(coeffs, solution)) < 1e-3);
-    
-    return 0;
-}
+std::vector<double> coeffs = {1, -3, 2}; // Example set of coefficients
+double solution = find_zero(coeffs);
+assert(abs(poly(coeffs, solution)) < 1e-3);
