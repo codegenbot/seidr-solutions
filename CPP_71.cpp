a72f48060d9b7b5c65ad4053e5a51fc25b0d6905
beep boop <githubbot@vadim.me>
2024-07-09 15:39:25 +0200

    Solution 19 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vfpw2co7 Prompt: /usr/include/c++/11/bits/stl_algobase.h:741:15: error: ‘_Num’ was not declared i n this scope   741 |           if (_Num)       |               ^~~~ /usr/include/c++/11/bits/stl_algobase.h:743:29: error: ‘_Num’ was not declared i n this scope   743 |           return __result - _Num;       |                             ^~~~ /usr/include/c++/11/bits/stl_algobase.h: At global scope: /usr/include/c++/11/bits/stl_algobase.h:1349:16: error: ‘ptrdiff_t’ does not nam e a type  1349 |         static ptrdiff_t       |                ^~~~~~~~~ /usr/include/c++/11/bits/stl_algobase.h:1349:16: note: ‘ptrdiff_t’ is defined in  header ‘<cstddef>’; did you forget to ‘#include <cstddef>’? b75f300f-f78a-44a2-be6b-2ef31edeac27.cpp:16:5: error: redefinition of ‘int main( )’    16 | int main(){       |     ^~~~ b75f300f-f78a-44a2-be6b-2ef31edeac27.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,13 +1,14 @@
-```cpp
+```c++
 #include <cmath>
 #include <cassert>
 #include <cstddef>
 
 int triangle_area(int a, int b, int c) {
     int s = (a + b + c) / 2;
-    return abs((s*(s-a)*(s-b)*(s-c))/144);
+    return std::abs(std::sqrt(s*(s-a)*(s-b)*(s-c)));
 }
 
 int main() {
+    assert(std::abs(triangle_area(2, 2, 10)) < 0.01);
     return 0;
 }
