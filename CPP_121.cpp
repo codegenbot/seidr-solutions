856796aa9748465a24ec15678e73c9ce75951392
beep boop <githubbot@vadim.me>
2024-07-09 09:58:28 +0200

    Solution 11 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/awel56y5 Prompt:       |                        ^~~ In file included from /usr/include/c++/11/cassert:44,                  from f3442f7f-f693-431b-90a8-21b4089a18f3.cpp:3: f3442f7f-f693-431b-90a8-21b4089a18f3.cpp: In function ‘int main()’: f3442f7f-f693-431b-90a8-21b4089a18f3.cpp:16:18: error: ‘sumOdd’ cannot be used a s a function    16 |     assert(sumOdd({3, 13, 2, 9}) == 16);       |            ~~~~~~^~~~~~~~~~~~~~~ f3442f7f-f693-431b-90a8-21b4089a18f3.cpp: At global scope: f3442f7f-f693-431b-90a8-21b4089a18f3.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ f3442f7f-f693-431b-90a8-21b4089a18f3.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from f3442f7f-f693-431b-90a8-21b4089a18f3.cpp:19: f3442f7f-f693-431b-90a8-21b4089a18f3.cpp: In function ‘int main()’: f3442f7f-f693-431b-90a8-21b4089a18f3.cpp:21:13: error: ‘solutions’ was not decla red in this scope    21 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -2,7 +2,7 @@
 #include <initializer_list>
 #include <cassert>
 
-int solution(std::vector<int> lst) {
+int sumOdd(std::vector<int> lst) {
     int sum = 0;
     for (int i = 1; i < lst.size(); i++) {
         if (lst[i] % 2 != 0) {
@@ -12,11 +12,4 @@ int solution(std::vector<int> lst) {
     return sum;
 }
 
-int main() {
-    try {
-        assert(solution({3, 13, 2, 9}) == 16);
-    } catch (...) {
-        std::cerr << "Error: Test failed.\n";
-        exit(1);
-    }
-}
+assert(sumOdd({3, 13, 2, 9}) == 16);
