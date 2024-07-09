adbdd6570071b7ce1605af8b32679ed938f7ebf9
beep boop <githubbot@vadim.me>
2024-07-09 11:56:10 +0200

    Solution 15 of CPP/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/s26u03fd Prompt: fbdb71b5-2862-4ae6-b7d4-22cca204af07.cpp:5:15: error: ‘string’ was not declared in this scope     5 | bool simplify(string x, string n) {       |               ^~~~~~ fbdb71b5-2862-4ae6-b7d4-22cca204af07.cpp:5:25: error: ‘string’ was not declared in this scope     5 | bool simplify(string x, string n) {       |                         ^~~~~~ fbdb71b5-2862-4ae6-b7d4-22cca204af07.cpp:5:33: error: expression list treated as  compound expression in initializer [-fpermissive]     5 | bool simplify(string x, string n) {       |                                 ^ In file included from fbdb71b5-2862-4ae6-b7d4-22cca204af07.cpp:21: fbdb71b5-2862-4ae6-b7d4-22cca204af07.cpp: In function ‘int main()’: fbdb71b5-2862-4ae6-b7d4-22cca204af07.cpp:23:21: error: ‘simplify’ cannot be used  as a function    23 |     assert (simplify("1/5", "1/5") == false);       |             ~~~~~~~~^~~~~~~~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,7 +1,21 @@
-int num1 = stoi(x.substr(0, x.find('/')));
-    int den1 = stoi(x.substr(x.find('/') + 1));
-    int num2 = stoi(n.substr(0, n.find('/')));
-    int den2 = stoi(n.substr(n.find('/') + 1));
-    
-    return (num1 * num2) % (den1 * den2) == 0;
+#include <string>
+
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+bool simplify(std::string x, std::string n) {
+    int num1 = std::stoi(x.substr(0, x.find('/')));
+    int den1 = std::stoi(x.substr(x.find('/') + 1));
+    int num2 = std::stoi(n.substr(0, n.find('/')));
+    int den2 = std::stoi(n.substr(n.find('/') + 1));
+
+    int num = num1 * num2;
+    int den = den1 * den2;
+
+    int common = gcd(num, den);
+    num /= common;
+    den /= common;
+
+    return den == 1;
 }
