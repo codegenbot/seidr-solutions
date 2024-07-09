e6143308cb19042cb4bfc3fd27019700ad3092a0
beep boop <githubbot@vadim.me>
2024-07-10 01:51:51 +0200

    Solution 18 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/sxf96gd0 Prompt: Program failed

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int fuelCost(vector<int> v) {
     int sum = 0;
     for (int x : v) {
-        int y = (x / 3) > 0 ? ((x / 3) - 2) : 0;
-        sum += y;
+        sum += (x/3) - 2;
     }
     return sum;
 }
