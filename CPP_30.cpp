0d6d215492fc4c22b7deee28dd72084ddcbf4085
beep boop <githubbot@vadim.me>
2024-07-09 03:13:37 +0200

    Solution 24 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/du4l4ndj Prompt:       |                    ^~~~~ 33084806-f9ca-4d31-abb0-251f626bd2d1.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 33084806-f9ca-4d31-abb0-251f626bd2d1.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 33084806-f9ca-4d31-abb0-251f626bd2d1.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 33084806-f9ca-4d31-abb0-251f626bd2d1.cpp:11: 33084806-f9ca-4d31-abb0-251f626bd2d1.cpp: In function ‘int main()’: 33084806-f9ca-4d31-abb0-251f626bd2d1.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 33084806-f9ca-4d31-abb0-251f626bd2d1.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,13 +1,21 @@
 #include <vector>
 
-bool issame(vector<float> a, vector<float> b) {
-    if (a.size() != b.size()) {
+bool issame(std::vector<float> a, std::vector<float> b) {
+    if(a.size() != b.size())
         return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (i >= b.size() || a[i] != b[i]) {
+    for(int i=0; i<a.size(); i++) {
+        if(a[i] != b[i])
             return false;
-        }
     }
     return true;
+}
+
+std::vector<float> get_positive(std::vector<float> l) {
+    std::vector<float> result;
+    for (float i : l) {
+        if (i > 0) {
+            result.push_back(i);
+        }
+    }
+    return result;
 }
