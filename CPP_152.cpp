41800bc228868652f2b40aa41658240c0ea5ea4a
beep boop <githubbot@vadim.me>
2024-07-09 17:40:53 +0200

    Solution 2 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5m2wl4s0 Prompt:       |                    ^~~ dbc3274f-6ff7-4ef0-a313-205dbf1f9268.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ dbc3274f-6ff7-4ef0-a313-205dbf1f9268.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ dbc3274f-6ff7-4ef0-a313-205dbf1f9268.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from dbc3274f-6ff7-4ef0-a313-205dbf1f9268.cpp:13: dbc3274f-6ff7-4ef0-a313-205dbf1f9268.cpp: In function ‘int main()’: dbc3274f-6ff7-4ef0-a313-205dbf1f9268.cpp:23:20: error: ‘compare’ was not declare d in this scope    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ dbc3274f-6ff7-4ef0-a313-205dbf1f9268.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,25 +1,23 @@
 #include <vector>
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+bool isSame(const vector<int>& a, const vector<int>& b) {
+    return (a.size() == b.size()) && all_of(a.begin(), a.end(), [b](int x) { return count(b.begin(), b.end(), x) > 0; });
 }
 
 vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
     for (int i = 0; i < game.size(); i++) {
         if (game[i] == guess[i]) {
-            result.push_back(2);
-        } else if (find(game.begin(), game.end(), guess[i]) != game.end()) {
-            result.push_back(1);
-        } else {
             result.push_back(0);
+        } else {
+            result.push_back(abs(game[i] - guess[i]));
         }
     }
     return result;
 }
 
 int main() {
-    assert(issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));
+    assert(isSame({1,2,3,5},{-1,2,3,4}) == vector<int>({2,0,0,1}));
     return 0;
 }
