0f00ebc79c835523b60d665f5aac67ca70fe1347
beep boop <githubbot@vadim.me>
2024-07-09 20:25:11 +0200

    Solution 18 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/jjh2kxi8 Prompt:       |         ~~~~~~~~~~^~~~~~~~~~~~ In file included from 9c8cd7f5-031a-48ae-939b-b5d2207866cd.cpp:1: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ 9c8cd7f5-031a-48ae-939b-b5d2207866cd.cpp:17:14: warning: ‘int MD5_Final(unsigned  char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]    17 |     MD5_Final(md5, &ctx);       |     ~~~~~~~~~^~~~~~~~~~~ In file included from 9c8cd7f5-031a-48ae-939b-b5d2207866cd.cpp:1: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ /usr/bin/ld: /tmp/cclmgOrC.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> >)': 9c8cd7f5-031a-48ae-939b-b5d2207866cd.cpp:(.text+0x8e): undefined reference to `M D5_Init' /usr/bin/ld: 9c8cd7f5-031a-48ae-939b-b5d2207866cd.cpp:(.text+0xc4): undefined re ference to `MD5_Update' /usr/bin/ld: 9c8cd7f5-031a-48ae-939b-b5d2207866cd.cpp:(.text+0xf0): undefined re ference to `MD5_Final' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,10 +1,9 @@
-using namespace std;
-
+#include <openssl/md5.h>
+#include <iostream>
 #include <string>
 #include <algorithm>
-#include <iostream>
 
-string string_to_md5(string text) {
+std::string string_to_md5(std::string text) {
     if (text.empty()) return "";
     
     unsigned char md5[16];
@@ -17,7 +16,7 @@ string string_to_md5(string text) {
     }
     MD5_Final(md5, &ctx);
 
-    string result(32, '0');
+    std::string result(32, '0');
     for (int i = 0; i < 16; ++i) {
         char buf[3];
         sprintf(buf, "%02x", md5[i]);
