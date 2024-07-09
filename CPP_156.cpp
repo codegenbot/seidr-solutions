8e6ae5e7ce4e56c58b40f8090f1f0a54574a6dfc
beep boop <githubbot@vadim.me>
2024-07-09 05:33:35 +0200

    Solution 61 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/urrbvh7e Prompt: b46f23ff-0e39-4b19-967f-ddfd2b9c9552.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int num) {       | ^~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,56 +1,194 @@
 #include <string>
 std::string int_to_mini_roman(int num) {
-    if (num >= 4000)
-        return "MMMMMMMMM";
-    string roman = "";
-    while (num > 0) {
-        if (num >= 1000) {
-            roman += "M";
-            num -= 1000;
-        } else if (num >= 900) {
-            roman += "CM";
-            num -= 900;
-        } else if (num >= 500) {
-            roman += "D";
-            num -= 500;
-        } else if (num >= 400) {
-            roman += "CD";
-            num -= 400;
-        } else if (num >= 100) {
-            if (num >= 90) {
-                roman += "XC";
-                num -= 90;
-            } else {
+    if (num >= 1000) {
+        string roman = "M";
+        num -= 1000;
+        while (num >= 100) {
+            roman += "C";
+            num -= 100;
+            if (num >= 50) {
                 roman += "L";
-                num -= 100;
-            }
-        } else if (num >= 90) {
-            roman += "XM";
-            num -= 90;
-        } else if (num >= 50) {
-            roman += "L";
-            num -= 50;
-        } else if (num >= 40) {
-            roman += "XL";
-            num -= 40;
-        } else if (num >= 10) {
-            if (num >= 9) {
-                roman += "IX";
-                num -= 9;
-            } else {
+                num -= 50;
+            } else if (num >= 10) {
                 roman += "X";
                 num -= 10;
+                if (num >= 5) {
+                    roman += "V";
+                    num -= 5;
+                } else if (num > 0) {
+                    roman += "I";
+                    num--;
+                }
+            } else {
+                roman += "I";
             }
-        } else if (num >= 5) {
-            roman += "V";
-            num -= 5;
-        } else if (num >= 4) {
-            roman += "IV";
-            num -= 4;
-        } else {
-            roman += "I";
-            num -= 1;
         }
+        while (num >= 1) {
+            if (num == 4) {
+                roman += "IV";
+                num = 0;
+            } else if (num == 5) {
+                roman += "V";
+                num = 0;
+            } else if (num == 9) {
+                roman += "IX";
+                num = 0;
+            } else {
+                roman += "I";
+                num--;
+            }
+        }
+        return roman;
+    } else if (num >= 500) {
+        string roman = "D";
+        num -= 500;
+        while (num >= 100) {
+            roman += "C";
+            num -= 100;
+            if (num >= 50) {
+                roman += "L";
+                num -= 50;
+            } else if (num >= 10) {
+                roman += "X";
+                num -= 10;
+                if (num >= 5) {
+                    roman += "V";
+                    num -= 5;
+                } else if (num > 0) {
+                    roman += "I";
+                    num--;
+                }
+            } else {
+                roman += "I";
+            }
+        }
+        while (num >= 1) {
+            if (num == 4) {
+                roman += "IV";
+                num = 0;
+            } else if (num == 5) {
+                roman += "V";
+                num = 0;
+            } else if (num == 9) {
+                roman += "IX";
+                num = 0;
+            } else {
+                roman += "I";
+                num--;
+            }
+        }
+        return roman;
+    } else if (num >= 100) {
+        string roman = "";
+        while (num >= 100) {
+            roman += "C";
+            num -= 100;
+            if (num >= 50) {
+                roman += "L";
+                num -= 50;
+            } else if (num >= 10) {
+                roman += "X";
+                num -= 10;
+                if (num >= 5) {
+                    roman += "V";
+                    num -= 5;
+                } else if (num > 0) {
+                    roman += "I";
+                    num--;
+                }
+            } else {
+                roman += "I";
+            }
+        }
+        while (num >= 1) {
+            if (num == 4) {
+                roman += "IV";
+                num = 0;
+            } else if (num == 5) {
+                roman += "V";
+                num = 0;
+            } else if (num == 9) {
+                roman += "IX";
+                num = 0;
+            } else {
+                roman += "I";
+                num--;
+            }
+        }
+        return roman;
+    } else if (num >= 50) {
+        string roman = "L";
+        num -= 50;
+        while (num >= 10) {
+            roman += "X";
+            num -= 10;
+            if (num >= 5) {
+                roman += "V";
+                num -= 5;
+            } else if (num > 0) {
+                roman += "I";
+                num--;
+            }
+        }
+        while (num >= 1) {
+            if (num == 4) {
+                roman += "IV";
+                num = 0;
+            } else if (num == 5) {
+                roman += "V";
+                num = 0;
+            } else if (num == 9) {
+                roman += "IX";
+                num = 0;
+            } else {
+                roman += "I";
+                num--;
+            }
+        }
+        return roman;
+    } else if (num >= 10) {
+        string roman = "";
+        while (num >= 10) {
+            roman += "X";
+            num -= 10;
+            if (num >= 5) {
+                roman += "V";
+                num -= 5;
+            } else if (num > 0) {
+                roman += "I";
+                num--;
+            }
+        }
+        while (num >= 1) {
+            if (num == 4) {
+                roman += "IV";
+                num = 0;
+            } else if (num == 5) {
+                roman += "V";
+                num = 0;
+            } else if (num == 9) {
+                roman += "IX";
+                num = 0;
+            } else {
+                roman += "I";
+                num--;
+            }
+        }
+        return roman;
+    } else if (num >= 5) {
+        string roman = "V";
+        num -= 5;
+        while (num > 0) {
+            roman += "I";
+            num--;
+        }
+        return roman;
+    } else {
+        string roman = "";
+        while (num > 0) {
+            roman += "I";
+            num--;
+        }
+        return roman;
     }
-    return roman;
 }
