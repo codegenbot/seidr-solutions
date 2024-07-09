202b6b19b2e16bb3a3c8af2f5b963df72cb3963f
beep boop <githubbot@vadim.me>
2024-07-09 15:50:28 +0200

    Solution 2 of CPP/85, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ej7ra0o3 Prompt: 2d555d91-ecf9-4a1f-b4ee-a4fa1d66ab65.cpp:1:9: error: ‘vector’ was not declared i n this scope     1 | int add(vector<int> lst){       |         ^~~~~~ 2d555d91-ecf9-4a1f-b4ee-a4fa1d66ab65.cpp:1:16: error: expected primary-expressio n before ‘int’     1 | int add(vector<int> lst){       |                ^~~ In file included from 2d555d91-ecf9-4a1f-b4ee-a4fa1d66ab65.cpp:10: 2d555d91-ecf9-4a1f-b4ee-a4fa1d66ab65.cpp: In function ‘int main()’: 2d555d91-ecf9-4a1f-b4ee-a4fa1d66ab65.cpp:12:16: error: ‘add’ cannot be used as a  function    12 |     assert (add({4, 4, 6, 8}) == 12);       |             ~~~^~~~~~~~~~~~~~

--- CPP_85.cpp
+++ CPP_85.cpp
@@ -1,10 +1,18 @@
+#include <iostream>
 #include <vector>
 
-int add(vector<int> lst){
+int add(std::vector<int> lst){
     int sum = 0;
     for(int i = 1; i < lst.size(); i += 2){
         if(lst[i] % 2 == 0)
             sum += lst[i];
     }
     return sum;
+
+}
+
+int main(){
+    std::vector<int> vec = {4, 4, 6, 8};
+    assert (add(vec) == 12);
+    return 0;
 }
