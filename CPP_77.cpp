4067147ee8f425c356fd31f153f10747ca476eb5
beep boop <githubbot@vadim.me>
2024-07-09 09:18:52 +0200

    Solution 25 of CPP/77, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/03w5r3bz Prompt: e78b4044-c700-4376-9792-d7f8027f6980.cpp:14:5: error: redefinition of ‘int main( )’    14 | int main(){       |     ^~~~ e78b4044-c700-4376-9792-d7f8027f6980.cpp:7:5: note: ‘int main()’ previously defi ned here     7 | int main(){       |     ^~~~ In file included from e78b4044-c700-4376-9792-d7f8027f6980.cpp:13: e78b4044-c700-4376-9792-d7f8027f6980.cpp: In function ‘int main()’: e78b4044-c700-4376-9792-d7f8027f6980.cpp:15:13: error: ‘iscuber’ was not declare d in this scope; did you mean ‘isCube’?    15 |     assert (iscuber(1729) == false);       |             ^~~~~~~

--- CPP_77.cpp
+++ CPP_77.cpp
@@ -1,22 +1,10 @@
 #include <cassert>
 
-bool isCube(int n) {
-    int side = round(cbrt(n));
-    if (side * side * side == n)
-        return true;
-    else
-        return false;
-}
+bool isCube(int);
 
-void originalFunction() {
-    for (int i = 1; i <= 1000; i++) {
-        if (isCube(i)) {
-            cout << "The cube root of " << i << " is " << round(cbrt(i)) << endl;
-        }
-    }
-}
+void originalFunction() {}
 
-int cpp77Main(){
+int main(){
     originalFunction();
     assert (isCube(1729) == false);
     return 0;
