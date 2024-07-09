d86ad558c55c048b7fb4508b2b39a9839cff8797
beep boop <githubbot@vadim.me>
2024-07-09 07:43:44 +0200

    Solution 12 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ibgdwwkp Prompt:    20 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ eb2bcfe7-ed47-4c52-8428-4c1b9c780934.cpp:20:36: error: expected primary-expressi on before ‘float’    20 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ eb2bcfe7-ed47-4c52-8428-4c1b9c780934.cpp:20:43: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from eb2bcfe7-ed47-4c52-8428-4c1b9c780934.cpp:19: eb2bcfe7-ed47-4c52-8428-4c1b9c780934.cpp: In function ‘int main()’: eb2bcfe7-ed47-4c52-8428-4c1b9c780934.cpp:29:20: error: ‘sort_even’ was not decla red in this scope    29 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ eb2bcfe7-ed47-4c52-8428-4c1b9c780934.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,35 +1,31 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
-vector<float> sort_even(vector<float> l) {
-    vector<float> even_indices;
+// Function signature
+vector<float> sort_even(const vector<float>& l) {
+    vector<float> l_prime = l;
+    vector<float> even_values;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            even_indices.push_back(l[i]);
+            even_values.push_back(l[i]);
         }
     }
-    sort(even_indices.begin(), even_indices.end());
-
-    vector<float> result;
-    int even_idx = 0;
+    sort(even_values.begin(), even_values.end());
+    int j = 0;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            result.push_back(even_indices[even_idx++]);
-        } else {
-            result.push_back(l[i]);
+            l_prime[i] = even_values[j];
+            j++;
         }
     }
-
-    return result;
-}
-
-bool issame(vector<float> a, vector<float> b) {
-    return a == b;
+    return l_prime;
 }
 
 int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    // Test case
+    assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    
+    return 0;
 }
