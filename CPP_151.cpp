8206d7abf7dd6cab41e111b2bd2772afe940e728
beep boop <githubbot@vadim.me>
2024-07-09 03:01:24 +0200

    Solution 67 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/0re3q8c0 Prompt:                  from 864ef232-39c8-4d5e-9fc9-e6e48579e24a.cpp:2: /usr/include/c++/11/bits/stl_iterator_base_funcs.h:138:5: note: candidate: ‘temp late<class _InputIterator> constexpr typename std::iterator_traits< <template-pa rameter-1-1> >::difference_type std::distance(_InputIterator, _InputIterator)’   138 |     distance(_InputIterator __first, _InputIterator __last)       |     ^~~~~~~~ /usr/include/c++/11/bits/stl_iterator_base_funcs.h:138:5: note:   template argum ent deduction/substitution failed: /usr/include/c++/11/bits/stl_iterator_base_funcs.h: In substitution of ‘template <class _InputIterator> constexpr typename std::iterator_traits< <template-parame ter-1-1> >::difference_type std::distance(_InputIterator, _InputIterator) [with _InputIterator = const float*]’: /usr/include/c++/11/bits/stl_vector.h:1580:39:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const float*; _Tp = float; _Alloc = s td::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = f loat; _Alloc = std::allocator<float>; std::vector<_Tp, _Alloc>::allocator_type =  std::allocator<float>]’ 864ef232-39c8-4d5e-9fc9-e6e48579e24a.cpp:19:42:   required from here /usr/include/c++/11/bits/stl_iterator_base_funcs.h:138:5: error: no type named ‘ difference_type’ in ‘struct std::iterator_traits<const float*>’

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,1 +1,23 @@
-vector<float> lst = {1.5, 2.0, 3.5};
+```cpp
+#include <cmath>
+#include <vector>
+using namespace std;
+
+long long double_the_difference(vector<float> lst) {
+    long long sum = 0;
+    for (float num : lst) {
+        if (num > 0 && floor(num) == num) { 
+            if (fmod(num, 2.0) != 0.0) { 
+                sum += pow(num, 2);
+            }
+        }
+    }
+    return sum;
+}
+
+int main() {
+    vector<float> lst = {1.5, 2.0, 3.5};
+    long long odd_sum = double_the_difference(lst);
+    cout << "The sum of squares is: " << odd_sum << endl;
+    return 0;
+}
