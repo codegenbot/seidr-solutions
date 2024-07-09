d2734cc4d74b123f73bd9e687cd618b16b121dba
beep boop <githubbot@vadim.me>
2024-07-09 08:08:21 +0200

    Solution 5 of Python/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sqfn4dvu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e231c6f0-d400-4cea-844f-390120838 8f8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_141.py
+++ Python_141.py
@@ -1,13 +1,11 @@
 ```
 def file_name_check(file_name):
-    if not all(c.isalnum() or c == '.' for c in file_name):
+    if len([c for c in file_name if c.isdigit()]) > 3:
         return 'No'
     if file_name.count('.') != 1:
         return 'No'
-    if file_name[:file_name.index('.')] and not file_name[:file_name.index()].isalpha():
+    if not file_name[:file_name.index('.').isalpha():
         return 'No'
-    extension = file_name[file_name.index('.') + 1:].lower()
-    valid_extensions = ['txt', 'exe', 'dll']
-    if extension not in valid_extensions:
+    if file_name[file_name.index('.') + 1:].lower() not in ['txt', 'exe', 'dll']:
         return 'No'
     return 'Yes'
