fdee5c7945cfbc372b92f3573296f50a803bca74
beep boop <githubbot@vadim.me>
2024-07-09 05:33:31 +0200

    Solution 4 of CPP/64, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/a339knbk Prompt: Complete the following code given the task description and function signature.

--- CPP_64.cpp
+++ CPP_64.cpp
@@ -1,11 +1,18 @@
 int vowels_count(string s) {
     int count = 0;
     for (char c : s) {
-        if ((c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') &&
-            !isupper(c)) {
-            count++;
-        } else if (c == 'y' && s.back() == 'y') {
-            count++;
+        switch (tolower(c)) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                count++;
+                break;
+            case 'y':
+                if (s.back() == c) // check if the character is at the end of the string
+                    count++;
+                break;
         }
     }
     return count;
