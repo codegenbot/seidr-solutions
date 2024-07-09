c874b308f1271666c997f0038a8d2e5a0fc8024b
beep boop <githubbot@vadim.me>
2024-07-09 02:04:07 +0200

    Solution 5 of Python/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/6xp2gyns Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/40e06cf2-a687-4d05-ae1d-877e8662f a6c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_80.py
+++ Python_80.py
@@ -4,6 +4,6 @@ def is_happy(s):
         return False
     for i in range(0, len(s), 3):
         chunk = s[i:i+3]
-        if set(chunk) != {'a', 'b', 'c'}:
-            return False
-    return True
+        if set(chunk).issubset({'a', 'p', 'p', 'l', 'e'}):
+            return True
+    return False
