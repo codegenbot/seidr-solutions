7c6bc5eb1d65e7c730704ada18f86f7fba28cf2d
beep boop <githubbot@vadim.me>
2024-07-09 03:29:38 +0200

    Solution 15 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/muva7ycx Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4780c444-6f29-4aa1-8ce9-c81f8c417 843.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_4.py
+++ Python_4.py
@@ -1,1 +1,4 @@
-return sum((abs(num - mean) for num in numbers)) / len(numbers)
+```
+def mean_absolute_deviation(numbers: List[float]) -> float:
+    mean = sum(numbers) / len(numbers)
+    return sum((abs(num - mean) for num in numbers)) / len(numbers)
