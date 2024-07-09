ae11060f9aa223e93faf08e0deed224a3dfa2bcc
beep boop <githubbot@vadim.me>
2024-07-09 17:26:00 +0200

    Solution 91 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7gxeyycu Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 8ee6b7b5-e75c-4e9e-b49c-75ab4e1b0714.cpp:14:25:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,9 +1,8 @@
-```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+bool same(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
@@ -11,37 +10,35 @@ std::vector<int> strange_sort_list(std::vector<int> lst) {
     std::vector<int> result;
     while (!lst.empty()) {
         int min_val = *std::min_element(lst.begin(), lst.end());
-        result.push_back(min_val);
+        int max_val = *std::max_element(lst.begin(), lst.end());
 
+        result.push_back(min_val);
         auto it = lst.begin();
-        bool found = false;
+        bool found_min = false;
         for (; it != lst.end(); ++it) {
             if (*it == min_val) {
-                found = true;
+                found_min = true;
                 break;
             }
         }
 
-        if (found) {
+        if (found_min) {
             lst.erase(it);
         }
 
-        if (!lst.empty()) {
-            int max_val = *std::max_element(lst.begin(), lst.end());
-            result.push_back(max_val);
+        result.push_back(max_val);
 
-            it = lst.begin();
-            found = false;
-            for (; it != lst.end(); ++it) {
-                if (*it == max_val) {
-                    found = true;
-                    break;
-                }
+        it = lst.begin();
+        bool found_max = false;
+        for (; it != lst.end(); ++it) {
+            if (*it == max_val) {
+                found_max = true;
+                break;
             }
+        }
 
-            if (found) {
-                lst.erase(it);
-            }
+        if (found_max) {
+            lst.erase(it);
         }
     }
     return result;
@@ -57,7 +54,7 @@ int main() {
 
     std::vector<int> output = strange_sort_list(input);
 
-    if (issame(output, {1,2,3,4,5,6})) {
+    if (same(output, {1,2,3,4,5,6})) {
         std::cout << "True" << std::endl;
     } else {
         std::cout << "False" << std::endl;
