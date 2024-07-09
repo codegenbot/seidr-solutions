707fe31d7a37dccbcab8264e855199a72c62a40a
beep boop <githubbot@vadim.me>
2024-07-09 02:28:26 +0200

    Solution 5 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ca85xkll Prompt:       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 3b8515d2-39af-4a10-94b6-fccb95147ceb.cpp:1: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ 3b8515d2-39af-4a10-94b6-fccb95147ceb.cpp:14:22: error: aggregate ‘std::stringstr eam ss’ has incomplete type and cannot be defined    14 |         stringstream ss;       |                      ^~ 3b8515d2-39af-4a10-94b6-fccb95147ceb.cpp:15:15: error: ‘hex’ was not declared in  this scope    15 |         ss << hex << setfill('0') << setw(2) << (int)result[i];       |               ^~~ 3b8515d2-39af-4a10-94b6-fccb95147ceb.cpp:15:22: error: ‘setfill’ was not declare d in this scope    15 |         ss << hex << setfill('0') << setw(2) << (int)result[i];       |                      ^~~~~~~ 3b8515d2-39af-4a10-94b6-fccb95147ceb.cpp:15:38: error: ‘setw’ was not declared i n this scope; did you mean ‘getw’?    15 |         ss << hex << setfill('0') << setw(2) << (int)result[i];       |                                      ^~~~       |                                      getw

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,7 +1,5 @@
-```cpp
 #include <openssl/md5.h>
 #include <string>
-#include <iostream>
 #include <iomanip>
 #include <sstream>
 
@@ -15,8 +13,8 @@ string string_to_md5(string text) {
     
     string md5_hash;
     for (int i = 0; i < MD5_DIGEST_LENGTH; ++i) {
-        ostringstream ss;
-        ss << hex << setfill('0') << setw(2) << static_cast<int>(result[i]);
+        stringstream ss;
+        ss << hex << setfill('0') << setw(2) << (int)result[i];
         md5_hash += ss.str();
     }
     
