782bbf7c531668f888978c205347cc67d5479ff1
beep boop <githubbot@vadim.me>
2024-07-09 13:29:47 +0200

    Solution 6 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/hkz00flz Prompt: Complete the following code given the task description and function signature.

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -1,10 +1,10 @@
-int sum_squares(vector<int> lst){
-    int result = 0;
-    for(int i = 0; i < lst.size(); i++){
-        if(i%3 == 0 && i%4 != 0) // index is multiple of 3, not a multiple of 4
-            result += lst[i] * lst[i];
-        else if(i%4 == 0 && i%3 != 0) // index is multiple of 4, not a multiple of 3
-            result += pow(lst[i], 3);
+int sum_squares(vector<int> lst) {
+    int total = 0;
+    for (int i = 0; i < lst.size(); i++) {
+        if ((i + 1) % 3 == 0 && (i + 1) % 4 != 0)
+            total += lst[i] * lst[i];
+        else if ((i + 1) % 4 == 0 && (i + 1) % 3 != 0)
+            total += pow(lst[i], 3);
     }
-    return result;
+    return total;
 }
