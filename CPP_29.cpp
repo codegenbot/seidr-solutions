23b7d97017f98cb3d089a252fe97764940c84fa2
beep boop <githubbot@vadim.me>
2024-07-09 23:04:25 +0200

    Solution 45 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fxnddfx6 Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 15bf42cd-3248-49c2-b0b6-7ca2da74f08e.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 15bf42cd-3248-49c2-b0b6-7ca2da74f08e.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 15bf42cd-3248-49c2-b0b6-7ca2da74f08e.cpp:11: 15bf42cd-3248-49c2-b0b6-7ca2da74f08e.cpp: In function ‘int main()’: 15bf42cd-3248-49c2-b0b6-7ca2da74f08e.cpp:21:20: error: ‘filter_by_prefix’ was no t declared in this scope    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 15bf42cd-3248-49c2-b0b6-7ca2da74f08e.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -6,8 +6,8 @@ bool issame(vector<string> a,vector<string> b){
     return a == b;
 }
 
-std::vector<std::string> filter_by_prefix(std::vector<std::string> strings, std::string prefix){
-    std::vector<std::string> result;
+vector<string> filter_by_prefix(vector<string> strings, string prefix){
+    vector<string> result;
     for(string s : strings){
         if(s.find(prefix) == 0){
             result.push_back(s);
