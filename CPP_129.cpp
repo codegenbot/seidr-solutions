3f7defac757ba5c0deafb1ae81d5a279de57af15
beep boop <githubbot@vadim.me>
2024-07-09 19:39:24 +0200

    Solution 9 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/a5qb31cf Prompt:    36 |     reverse(res.begin(), res.end());       |     ^~~~~~~ In file included from 1057b872-c226-4f68-8477-433438ac4cf8.cpp:41: 1057b872-c226-4f68-8477-433438ac4cf8.cpp: In function ‘int main()’: 1057b872-c226-4f68-8477-433438ac4cf8.cpp:51:27: error: cannot bind non-const lva lue reference of type ‘std::vector<std::vector<int> >&’ to an rvalue of type ‘st d::vector<std::vector<int> >’    51 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 1057b872-c226-4f68-8477-433438ac4cf8.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = std::vector<int>; _Alloc = std::allocator<std::vector<int> >; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<std::vector<int> >] ’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 1057b872-c226-4f68-8477-433438ac4cf8.cpp:4:42: note:   initializing argument 1 o f ‘std::vector<int> minPath(std::vector<std::vector<int> >&, int)’     4 | vector<int> minPath(vector<vector<int>>& grid, int k) {       |                     ~~~~~~~~~~~~~~~~~~~~~^~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,36 +1,4 @@
-vector<int> minPath(const vector<vector<int>>& grid, int k) {
-    int n = grid.size();
-    vector<vector<int>> dp(n, vector<int>(n));
-    
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == 0 && j == 0) continue;
-            int val = grid[i][j];
-            if (i > 0) dp[i][j] = min(dp[i-1][j], val);
-            if (j > 0) dp[i][j] = min(dp[i][j-1], val);
-        }
-    }
-    
-    vector<int> res;
-    int i = n - 1, j = n - 1;
-    for (int t = 0; t < k; t++) {
-        if (i > 0 && j > 0) {
-            if (grid[i-1][j] <= grid[i][j-1]) i--;
-            else j--;
-        } else if (i > 0) i--;
-        else j--;
-    }
-    
-    while (k--) {
-        res.push_back(grid[i][j]);
-        if (i > 0 && j > 0) {
-            if (grid[i-1][j] <= grid[i][j-1]) i--;
-            else j--;
-        } else if (i > 0) i--;
-        else j--;
-    }
-    
-    reverse(res.begin(), res.end());
-    
-    return res;
+int main() {
+    const vector<vector<int>> grid = {{1, 3}, {3, 2}};
+    assert(issame(minPath(const_cast<const vector<vector<int>>&>(grid), 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
 }
