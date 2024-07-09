880302d8f7995f40aae9cbd8ecd9298cacfca117
beep boop <githubbot@vadim.me>
2024-07-09 16:26:09 +0200

    Solution 15 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/vtpigf3u Prompt: Complete the following code given the task description and function signature.

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,16 +1,14 @@
-vector<int> strange_sort_vector(vector<int> lst){
-    vector<int> result;
+vector<int> strange_sort_list(vector<int> lst){
     sort(lst.begin(), lst.end());
+    vector<int> result;
     int left = 0, right = lst.size() - 1;
     while(left <= right){
         if(left == right){
             result.push_back(lst[left]);
             break;
         }
-        result.push_back(lst[left]);
-        result.push_back(lst[right]);
-        left++;
-        right--;
+        result.push_back(lst[left++]);
+        result.push_back(lst[right--]);
     }
     return result;
 }
