5fe9651cbf7215b3f6e30ee2af35ee24ac7cbb7b
beep boop <githubbot@vadim.me>
2024-07-09 18:27:42 +0200

    Solution 10 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ux26g4qv Prompt: In file included from 4f82407a-1315-4d41-ae3b-0875bcbcba36.cpp:31: 4f82407a-1315-4d41-ae3b-0875bcbcba36.cpp: In function ‘int main()’: 4f82407a-1315-4d41-ae3b-0875bcbcba36.cpp:41:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    41 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,19 +1,35 @@
-```cpp
 #include <algorithm>
 using namespace std;
 
 vector<int> strange_sort_vector(vector<int> lst) {
+    if (lst.empty()) return lst;
+    
     vector<int> result;
-    while (!lst.empty()) {
-        int min_val = *min_element(lst.begin(), lst.end());
-        result.push_back(min_val);
-        lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
-        if (!lst.empty()) {
-            int max_val = *max_element(lst.begin(), lst.end());
-            result.push_back(max_val);
-            lst.erase(remove(lst.begin(), lst.end(), max_val), lst.end());
+    sort(lst.begin(), lst.end());
+    int min = *min_element(lst.begin(), lst.end());
+    int max = *max_element(lst.begin(), lst.end());
+
+    for(int i : lst) {
+        if(i == min || i == max)
+            continue;
+        if(i < min)
+            min = i;
+        else if(i > max)
+            max = i;
+    }
+
+    while (lst.size() > 0) {
+        if (*min_element(lst.begin(), lst.end()) != min) {
+            result.push_back(*min_element(lst.begin(), lst.end()));
+            lst.erase(std::remove(lst.begin(), lst.end(), *min_element(lst.begin(), lst.end())), lst.end());
+        } else if (*max_element(lst.begin(), lst.end()) != max) {
+            result.push_back(*max_element(lst.begin(), lst.end()));
+            lst.erase(std::remove(lst.begin(), lst.end(), *max_element(lst.begin(), lst.end())), lst.end());
+        } else {
+            result.push_back(lst[0]);
+            lst.erase(lst.begin());
         }
     }
+
     return result;
 }
-assert(issame(strange_sort_vector({111111}), {111111}));
