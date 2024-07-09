7e50185361a3afa537f3726f1c8d5f09e5e6948e
beep boop <githubbot@vadim.me>
2024-07-09 07:52:13 +0200

    Solution 37 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/wbyr1spe Prompt: In file included from 819a1ff5-0bec-476a-ae5c-c7b4c7687091.cpp:19: 819a1ff5-0bec-476a-ae5c-c7b4c7687091.cpp:21:28: note:   candidate expects 4 argu ments, 1 provided    21 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 819a1ff5-0bec-476a-ae5c-c7b4c7687091.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:528:1: note: candidate: ‘template <class _ExecutionPolicy, class _ForwardIterator> __pstl::__internal::__enable_if _execution_policy<_ExecutionPolicy, _ForwardIterator> std::max_element(_Executio nPolicy&&, _ForwardIterator, _ForwardIterator)’   528 | max_element(_ExecutionPolicy&& __exec, _ForwardIterator __first, _Forwar dIterator __last);       | ^~~~~~~~~~~ /usr/include/c++/11/pstl/glue_algorithm_defs.h:528:1: note:   template argument deduction/substitution failed: In file included from 819a1ff5-0bec-476a-ae5c-c7b4c7687091.cpp:19: 819a1ff5-0bec-476a-ae5c-c7b4c7687091.cpp:21:28: note:   candidate expects 3 argu ments, 1 provided    21 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <vector>
 #include <initializer_list>
+#include <iterator>
 using namespace std;
 
 int findMax(vector<int> l) {
@@ -12,5 +13,5 @@ int findMax(vector<int> l) {
 
 int main() {
     vector<int> v = {1, 2, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10};
-    assert(abs(*std::max_element(v.begin(), v.end()))- 12)<1e-4);
+    assert(abs(*max_element(v.begin(), v.end()))- 12)<1e-4);
 }
