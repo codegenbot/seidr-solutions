4af44d4b9caed6a959db586bae49011aaacbd886
beep boop <githubbot@vadim.me>
2024-07-09 02:38:06 +0200

    Solution 20 of Python/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/6qoso50g Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b3634469-bcb3-48b1-a975-328000441 ae2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_69.py
+++ Python_69.py
@@ -13,3 +13,6 @@ def search(lst):
             max_num = key
             break
     return max_num
+
+numbers = input("Enter a list of numbers separated by space: ").split()
+print(search(list(map(int, numbers))) or 0)
