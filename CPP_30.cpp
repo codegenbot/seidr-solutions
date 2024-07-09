7b0e41f93cb776b21163ee996e56395c01af7ba0
beep boop <githubbot@vadim.me>
2024-07-09 13:19:26 +0200

    Solution 5 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/t79zjf21 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 4d370ec4-c8ed-4337-a47e-e2e71e68cf7f.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,9 +1,9 @@
+#include <initializer_list>
+
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
-bool issame(vector<float> a,vector<float>b){
+bool same(vector<float> a,vector<float>b){
     if(a.size() != b.size()) return false;
     for(int i=0; i<a.size();i++){
         if(a[i] != b[i])return false;
@@ -13,10 +13,9 @@ bool issame(vector<float> a,vector<float>b){
 
 vector<float> get_positive(vector<float> l) {
     vector<float> result;
-    for (auto num : l) {
+    for (float num : l) {
         if (num > 0) {
             result.push_back(num);
         }
     }
     return result;
-}
