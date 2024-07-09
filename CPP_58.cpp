6066708b7996a337d43bf12995d4748b4892d685
beep boop <githubbot@vadim.me>
2024-07-09 05:28:37 +0200

    Solution 3 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/t30mjjks Prompt: Complete the following code given the task description and function signature.

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -7,5 +7,6 @@ vector<int> common(vector<int> l1, vector<int> l2) {
         inserter(intersection, intersection.begin()));
 
     vector<int> result(intersection.begin(), intersection.end());
+    sort(result.begin(), result.end());
     return result;
 }
