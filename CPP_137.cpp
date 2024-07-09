8244850d2e1aa1d838d457ba656838796ccff042
beep boop <githubbot@vadim.me>
2024-07-09 20:05:17 +0200

    Solution 29 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/iewm74bm Prompt: c0f2f20e-88ef-4ea3-bd93-8bac0145e194.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ c0f2f20e-88ef-4ea3-bd93-8bac0145e194.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ c0f2f20e-88ef-4ea3-bd93-8bac0145e194.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ c0f2f20e-88ef-4ea3-bd93-8bac0145e194.cpp:2:10: fatal error: boost/any.hpp: No su ch file or directory     2 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,6 +1,8 @@
+```cpp
 #include <boost/any.hpp>
 #include <string>
-#include <cmath>
+#include <algorithm>
+#include <cctype>
 
 using namespace boost;
 
@@ -8,27 +10,72 @@ any compareOne(any a, any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
         int x = boost::any_cast<int>(a);
         int y = boost::any_cast<int>(b);
-        return (x > y) ? a : ((y > x) ? b : typeid(void));
+        if (x > y)
+            return a;
+        else if (y > x)
+            return b;
+        else
+            return typeid(void);
     } 
     else if (a.type() == typeid(float) && b.type() == typeid(float)) {
         float x = boost::any_cast<float>(a);
         float y = boost::any_cast<float>(b);
-        return (x > y) ? a : ((y > x) ? b : typeid(void));
+        if (x > y)
+            return a;
+        else if (y > x)
+            return b;
+        else
+            return typeid(void);
     } 
     else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
         std::string x = boost::any_cast<std::string>(a);
         std::string y = boost::any_cast<std::string>(b);
-        return (x > y) ? a : ((y > x) ? b : typeid(void));
+        if (x > y)
+            return a;
+        else if (y > x)
+            return b;
+        else
+            return typeid(void);
     } 
-    else if (a.type() == typeid(std::string) && (b.type() == typeid(int) || b.type() == typeid(float))) {
+    else if (a.type() == typeid(std::string) && b.type() == typeid(int)) {
         std::string x = boost::any_cast<std::string>(a);
-        double y = boost::any_cast<double>(b);
-        return (std::stod(x) > y) ? a : ((y > std::stod(x)) ? b : typeid(void));
+        int y = boost::any_cast<int>(b);
+        if (std::stoi(x) > y)
+            return a;
+        else if (y > std::stoi(x))
+            return b;
+        else
+            return typeid(void);
     } 
-    else if ((a.type() == typeid(int) || a.type() == typeid(float)) && b.type() == typeid(std::string)) {
-        double x = boost::any_cast<double>(a);
+    else if (a.type() == typeid(int) && b.type() == typeid(std::string)) {
+        int x = boost::any_cast<int>(a);
         std::string y = boost::any_cast<std::string>(b);
-        return (x > std::stod(y)) ? a : ((std::stod(y) > x) ? b : typeid(void));
+        if (x > std::stoi(y))
+            return a;
+        else if (std::stoi(y) > x)
+            return b;
+        else
+            return typeid(void);
+    } 
+    else if (a.type() == typeid(std::string) && b.type() == typeid(float)) {
+        std::string x = boost::any_cast<std::string>(a);
+        float y = boost::any_cast<float>(b);
+        if (std::stod(x) > y)
+            return a;
+        else if (y > std::stod(x))
+            return b;
+        else
+            return typeid(void);
+    } 
+    else if (a.type() == typeid(float) && b.type() == typeid(std::string)) {
+        float x = boost::any_cast<float>(a);
+        std::string y = boost::any_cast<std::string>(b);
+        if (x > std::stod(y))
+            return a;
+        else if (std::stod(y) > x)
+            return b;
+        else
+            return typeid(void);
     } 
     else {
         return typeid(void);
