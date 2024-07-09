223f4fe0a9caf02ef24c571898c5369ed50fbdea
beep boop <githubbot@vadim.me>
2024-07-09 02:46:57 +0200

    Solution 86 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c0h3vdup Prompt:       |                                  ^~~ 143a7dd8-d06d-4a38-b4b0-d6994250e50f.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 143a7dd8-d06d-4a38-b4b0-d6994250e50f.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 143a7dd8-d06d-4a38-b4b0-d6994250e50f.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 143a7dd8-d06d-4a38-b4b0-d6994250e50f.cpp:20: 143a7dd8-d06d-4a38-b4b0-d6994250e50f.cpp: In function ‘int main()’: 143a7dd8-d06d-4a38-b4b0-d6994250e50f.cpp:30:20: error: ‘get_odd_collatz’ was not  declared in this scope    30 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 143a7dd8-d06d-4a38-b4b0-d6994250e50f.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,7 +1,32 @@
-int main 
-{
+#include <iostream>
+#include <vector>
+
+bool sameSequence(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size())
+        return false;
+    
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i])
+            return false;
+    }
+    
+    return true;
+}
+
+std::vector<int> get_odd_collatz(int n) {
+    std::vector<int> sequence;
+    while(n > 1) {
+        if(n % 2 != 0)
+            sequence.push_back(n);
+        if(n == 1)
+            break;
+        n = (n * 3 + 1);
+    }
+    return sequence;
+}
+
+int main() {
     int num;
     std::cin >> num;
-    get_odd_collatz(num);
-    assert(sequenceEqual(get_odd_collatz(1), {1}));
+    assert(sameSequence({get_odd_collatz(num)}, {num}));  
 }
