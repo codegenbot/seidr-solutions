353d12d5a967eec6ea2a28560fe9e1761bc0b8ff
beep boop <githubbot@vadim.me>
2024-07-09 03:51:44 +0200

    Solution 77 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jvy931jd Prompt:       |             ^~~~~~ c9957610-4dc9-4659-bc51-ddf59d38c38f.cpp:25:36: error: expected primary-expressi on before ‘float’    25 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ c9957610-4dc9-4659-bc51-ddf59d38c38f.cpp:25:43: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<float> a,vector<float>b){       |                                           ^ c9957610-4dc9-4659-bc51-ddf59d38c38f.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ c9957610-4dc9-4659-bc51-ddf59d38c38f.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from c9957610-4dc9-4659-bc51-ddf59d38c38f.cpp:24: c9957610-4dc9-4659-bc51-ddf59d38c38f.cpp: In function ‘int main()’: c9957610-4dc9-4659-bc51-ddf59d38c38f.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -17,6 +17,12 @@ bool same(vector<float> a,vector<float>b){
 }
 
 int main() {
-    assert(same(get_positive({}), {}) );
+    vector<float> v1 = get_positive({-3.5, -2, 0, 2, 4.5});
+    vector<float> v2 = get_positive({-3.5, -2, 0, 2, 4.5});
+    if (!same(v1,v2)) {
+        cout << "Vectors are not same." << endl;
+    } else {
+        cout << "Vectors are same." << endl;
+    }
     return 0;
 }
