35a2eef441de0bdb615d67d3badddcf1d5b52401
beep boop <githubbot@vadim.me>
2024-07-09 16:37:13 +0200

    Solution 7 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0bwgh28f Prompt:                  from /usr/include/c++/11/iostream:39,                  from 5dd64309-3452-416c-974c-e695fe5a3508.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 5dd64309-3452-416c-974c-e695fe5a3508.cpp:23:44: error: ‘b’ was not declared in t his scope    23 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5dd64309-3452-416c-974c-e695fe5a3508.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 5dd64309-3452-416c-974c-e695fe5a3508.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 5dd64309-3452-416c-974c-e695fe5a3508.cpp:22: 5dd64309-3452-416c-974c-e695fe5a3508.cpp: In function ‘int main()’: 5dd64309-3452-416c-974c-e695fe5a3508.cpp:32:20: error: ‘by_length’ was not decla red in this scope    32 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -3,15 +3,18 @@
 #include <string>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
 
 int main() {
-    int by_length[3] = {9,4,8};
-    std::vector<std::string> vec = {"Nine", "Eight", "Four"};
-    assert(issame({""},vec));
+    int numArray[] = {9, 4, 8};
+    std::vector<std::string> stringArray = {"Nine", "Four", "Eight"};
+    std::vector<int> by_length(numArray, numArray + sizeof(numArray) / sizeof(numArray[0]));
+
+    assert(issame(std::vector<std::string>(by_length.begin(), by_length.end()), stringArray));
+    return 0;
 }
