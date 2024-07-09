3e9f336031e9a545ba3a13fab75278fc53364a24
beep boop <githubbot@vadim.me>
2024-07-09 04:47:57 +0200

    Solution 12 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sqlpuqyj Prompt:       |                    ^~~ fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp:14: fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp: In function ‘int main()’: fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp:24:20: error: ‘even_odd_count’ was not declared in this scope    24 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ fcbbd4eb-a33a-4e83-a8d6-d32646182b95.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,31 +1,13 @@
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
+#include <vector>
 
-vector<vector<int>> checkNumbers() {
-    int num1, num2;
-    cout << "Enter the first number: ";
-    cin >> num1;
-    cout << "Enter the second number: ";
-    cin >> num2;
-
-    vector<int> even_odd_count1 = even_odd_count(num1);
-    vector<int> even_odd_count2 = even_odd_count(num2);
-
-    if(issame(even_odd_count1, even_odd_count2))
-        return {{even_odd_count1[0], even_odd_count1[1]}, {even_odd_count2[0], even_odd_count2[1]}};
-    else
-        return {{-1,-1}, {-1,-1}};
-}
-
-vector<int> even_odd_count(int num) {
-    int evens = 0, odds = 0;
-    string str = to_string(abs(num));
-    for (char c : str) {
-        if (stoi(c) % 2 == 0)
-            evens++;
+std::vector<int> even_odd_count(int num) {
+    std::vector<int> result(2);
+    while (num != 0) {
+        int digit = num % 10;
+        if (digit % 2 == 0)
+            result[0]++;
         else
-            odds++;
+            result[1]++;
+        num /= 10;
     }
-    return {evens, odds};
-}
+    return result;
