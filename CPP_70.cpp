d27d9385f9e17c92b5110efbb74e77835d7d9bc3
beep boop <githubbot@vadim.me>
2024-07-09 17:11:45 +0200

    Solution 81 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7gxeyycu Prompt: In file included from /usr/include/c++/11/vector:67,                  from 7022dd25-79a9-4b42-913e-1c4e137781cb.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 7022dd25-79a9-4b42-913e-1c4e137781cb.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7022dd25-79a9-4b42-913e-1c4e137781cb.cpp:71:34: error: expected primary-expressi on before ‘int’    71 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7022dd25-79a9-4b42-913e-1c4e137781cb.cpp:71:39: error: expression list treated a s compound expression in initializer [-fpermissive]    71 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7022dd25-79a9-4b42-913e-1c4e137781cb.cpp:70: 7022dd25-79a9-4b42-913e-1c4e137781cb.cpp: In function ‘int main()’: 7022dd25-79a9-4b42-913e-1c4e137781cb.cpp:80:19: error: ‘issame’ cannot be used a s a function    80 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,11 +2,11 @@
 #include <vector>
 #include <algorithm>
 
-bool isEqual(const std::vector<int>& a, const std::vector<int>& b) {
+bool same(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
-std::vector<int> strangeSortList(std::vector<int> lst) {
+std::vector<int> strange_sort_list(std::vector<int> lst) {
     std::vector<int> result;
     while (!lst.empty()) {
         int min_val = *std::min_element(lst.begin(), lst.end());
@@ -54,13 +54,15 @@ int main() {
         input.push_back(num);
     }
 
-    std::vector<int> output = strangeSortList(input);
+    std::vector<int> output = strange_sort_list(input);
 
-    if (isEqual(output, {1,2,3,4,5,6})) {
+    if (same(output, {1,2,3,4,5,6})) {
         std::cout << "True" << std::endl;
     } else {
         std::cout << "False" << std::endl;
     }
 
-    assert(isEqual(strangeSortList({111}), {111}));
+    assert(same(strange_sort_list({111111}), {111111}));
+
     return 0;
+}
