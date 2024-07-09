ad3075f0f3dee683131201b9eb3b0f74be5c250f
beep boop <githubbot@vadim.me>
2024-07-09 09:21:39 +0200

    Solution 47 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/n9li31jt Prompt:       |             ^~~~~~ 1d687a00-000e-4175-b577-d744f69baf5e.cpp:32:34: error: expected primary-expressi on before ‘int’    32 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1d687a00-000e-4175-b577-d744f69baf5e.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 1d687a00-000e-4175-b577-d744f69baf5e.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~ In file included from 1d687a00-000e-4175-b577-d744f69baf5e.cpp:31: 1d687a00-000e-4175-b577-d744f69baf5e.cpp: In function ‘int main()’: 1d687a00-000e-4175-b577-d744f69baf5e.cpp:41:20: error: ‘strange_sort_list’ was n ot declared in this scope    41 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 1d687a00-000e-4175-b577-d744f69baf5e.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -6,20 +6,7 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> strange_sort_list(std::vector<int> lst) {
-    sort(lst.begin(), lst.end());
-    std::vector<int> result;
-    int left = 0, right = lst.size() - 1;
-    while (left <= right) {
-        result.push_back(lst[left]);
-        if (left != right) {
-            result.push_back(lst[right]);
-        }
-        left++;
-        right--;
-    }
-    return result;
-}
+std::vector<int> strange_sort_list(std::vector<int> lst);
 
 int main() {
     assert(issame(strange_sort_list({111111}), {111111}) == true);
