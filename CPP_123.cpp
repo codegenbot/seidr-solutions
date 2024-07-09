5adec5cdbbba45b0eefddf5c685f91d158adbb3c
beep boop <githubbot@vadim.me>
2024-07-09 19:08:49 +0200

    Solution 36 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/4dd6lqce Prompt:       |                    ^~~ 4c497199-b751-4ec4-8c45-e387ccf17ee5.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4c497199-b751-4ec4-8c45-e387ccf17ee5.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4c497199-b751-4ec4-8c45-e387ccf17ee5.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4c497199-b751-4ec4-8c45-e387ccf17ee5.cpp:16: 4c497199-b751-4ec4-8c45-e387ccf17ee5.cpp: In function ‘int main()’: 4c497199-b751-4ec4-8c45-e387ccf17ee5.cpp:26:20: error: ‘get_odd_collatz’ was not  declared in this scope    26 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 4c497199-b751-4ec4-8c45-e387ccf17ee5.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
@@ -10,21 +10,4 @@ bool issame(std::vector<int> a, std::vector<int> b) {
         }
     }
     return true;
-}
-
-std::vector<int> solve(int n) {
-    std::vector<int> result;
-    while (n != 1) {
-        if (n % 2 != 0) {
-            result.push_back(n);
-        }
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else {
-            n = 3 * n + 1;
-        }
-    }
-    result.push_back(1);
-    std::sort(result.begin(), result.end());
-    return result;
 }
