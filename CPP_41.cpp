899fb5b8fe907f6e4f926d948e6f8fe554c19739
beep boop <githubbot@vadim.me>
2024-07-09 19:27:58 +0200

    Solution 98 of CPP/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nfmqjo78 Prompt:       |     ^~~~~~ b28dbf88-bf3f-4682-b1e9-920bc3eed96a.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <iostream>   +++ |+#include <cassert>     2 | #include <string> b28dbf88-bf3f-4682-b1e9-920bc3eed96a.cpp: At global scope: b28dbf88-bf3f-4682-b1e9-920bc3eed96a.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ b28dbf88-bf3f-4682-b1e9-920bc3eed96a.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from b28dbf88-bf3f-4682-b1e9-920bc3eed96a.cpp:25: b28dbf88-bf3f-4682-b1e9-920bc3eed96a.cpp: In function ‘int main()’: b28dbf88-bf3f-4682-b1e9-920bc3eed96a.cpp:27:32: error: could not convert ‘10’ fr om ‘int’ to ‘std::string’ {aka ‘std::__cxx11::basic_string<char>’}    27 |     assert (car_race_collision(10) == 100);       |                                ^~       |                                |       |                                int

--- CPP_41.cpp
+++ CPP_41.cpp
@@ -1,7 +1,25 @@
-int main
-{
-    int n;
-    cin >> n;
-    string s = to_string(n);
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+int car_race_collision(string s) {
+    int n = stoi(s);
+    int collision = 0;
+    for (int i = 1; i <= n; i++) {
+        if ((i % 4 == 0 && i % 8 != 0) || (i % 8 == 0)) {
+            collision++;
+        }
+    }
+    return collision;
+
+}
+
+int main() {
+    string s;
+    cin >> s;  
     cout << "Number of collisions: " << car_race_collision(s) << endl;
+    assert(car_race_collision(to_string(10)) == 2);
     return 0;
+}
