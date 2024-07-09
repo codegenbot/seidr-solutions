4daf151e442f1f055c9a833f2481388ba8d35d4e
beep boop <githubbot@vadim.me>
2024-07-09 03:09:06 +0200

    Solution 80 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0xw9ottc Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ a787f107-7224-4494-ba0e-35cb57b234da.cpp:47:22:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -5,21 +5,23 @@
 
 using namespace std;
 
-int do_algebra(vector<string> ops, vector<string> nums) {
+int do_algebra(vector<string> ops, vector<int> nums) {
     int result = 0;
     long long temp = 1;
 
     for (int i = 0; i < ops.size(); i++) {
         if (ops[i] == "+") {
-            result += temp * stoi(nums[i]);
+            result += temp * nums[i];
         } else if (ops[i] == "-") {
-            result -= temp * stoi(nums[i]);
+            result -= temp * nums[i];
         } else if (ops[i] == "*") {
-            temp *= stoi(nums[i]);
+            temp *= nums[i];
         } else if (ops[i] == "/") {
-            temp /= stoll(nums[i]);
+            if(nums[i] == 0)
+                throw runtime_error("Cannot divide by zero");
+            temp /= nums[i];
         } else if (ops[i] == "^") {
-            temp = pow(temp, stoll(nums[i]));
+            temp = pow(temp, nums[i]);
         }
     }
 
@@ -28,7 +30,7 @@ int do_algebra(vector<string> ops, vector<string> nums) {
 
 int main() {
     vector<string> ops;
-    vector<string> nums;
+    vector<int> nums;
 
     cout << "Enter number of operations: ";
     int n;
@@ -42,12 +44,17 @@ int main() {
         cin >> op;
         cout << "Enter number: ";
         cin >> strNum;
+        int num = std::stoi(strNum);
         
         ops.push_back(op);
-        nums.push_back(strNum);
+        nums.push_back(num);
     }
 
-    cout << "Result: " << do_algebra(ops, nums) << endl;
+    try {
+        cout << "Result: " << do_algebra(ops, nums) << endl;
+    } catch (const runtime_error& e) {
+        cerr << e.what() << endl;
+    }
 
     return 0;
 }
