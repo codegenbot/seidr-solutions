36f3a5d21af517c9681b3e4e425c89d4e585f727
beep boop <githubbot@vadim.me>
2024-07-09 12:11:59 +0200

    Solution 3 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ccxagfbb Prompt: In file included from /usr/include/c++/11/string:39,                  from 5a7241f7-7420-432b-ac4e-0cadb1e28a57.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 5a7241f7-7420-432b-ac4e-0cadb1e28a57.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 5a7241f7-7420-432b-ac4e-0cadb1e28a57.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5a7241f7-7420-432b-ac4e-0cadb1e28a57.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 5a7241f7-7420-432b-ac4e-0cadb1e28a57.cpp:16: 5a7241f7-7420-432b-ac4e-0cadb1e28a57.cpp: In function ‘int main()’: 5a7241f7-7420-432b-ac4e-0cadb1e28a57.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,9 +1,15 @@
-```cpp
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
+```c
+#include <string>
+#include <vector>
+
+std::string isSame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+    if(a == b)
+        return "True";
+    else
+        return "False";
 }
 
-vector<string> all_prefixes(string str) {
+std::vector<std::string> all_prefixes(std::string str) {
     vector<string> result;
     for (int i = 1; i <= str.length(); i++) {
         result.push_back(str.substr(0, i));
@@ -12,6 +18,5 @@ vector<string> all_prefixes(string str) {
 }
 
 int main() {
-    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
-    return 0;
+    assert(isSame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
 }
