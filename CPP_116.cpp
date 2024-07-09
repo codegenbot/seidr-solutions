cff78fff04c8540d5a1d640022585995af00eee3
beep boop <githubbot@vadim.me>
2024-07-09 02:07:38 +0200

    Solution 54 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sfxa53q2 Prompt:       |             ^~~~~~ 1f147d76-22cc-4246-bed5-7181a6535573.cpp:23:20: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 1f147d76-22cc-4246-bed5-7181a6535573.cpp:23:27: error: ‘vector’ was not declared  in this scope    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1f147d76-22cc-4246-bed5-7181a6535573.cpp:23:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 1f147d76-22cc-4246-bed5-7181a6535573.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1f147d76-22cc-4246-bed5-7181a6535573.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1f147d76-22cc-4246-bed5-7181a6535573.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,19 +1,24 @@
-using namespace std;
+#include <vector>
+#include <bitset>
 
-bool issame(const vector<int>& a, const vector<int>& b) {
-    if (a != b)
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    if (a.size() != b.size())
         return false;
+    for(int i = 0; i < a.size(); i++) {
+        if (std::bitset<32>(a[i]).count() != std::bitset<32>(b[i]).count()) 
+            return false;
+    }
     return true;
 }
 
-vector<int> sort_array(vector<int> arr) {
+std::vector<int> sort_array(std::vector<int> arr) {
     auto comp = [&] (int a, int b) {
-        if (bitset<32>(a).count() != bitset<32>(b).count()) {
-            return bitset<32>(a).count() < bitset<32>(b).count();
+        if (std::bitset<32>(a).count() != std::bitset<32>(b).count()) {
+            return std::bitset<32>(a).count() < std::bitset<32>(b).count();
         } else {
             return a < b;
         }
     };
-    sort(arr.begin(), arr.end(), comp);
+    std::sort(arr.begin(), arr.end(), comp);
     return arr;
 }
