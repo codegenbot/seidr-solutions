b2594089d7332be061417d06c665e347de388f28
beep boop <githubbot@vadim.me>
2024-07-09 15:15:45 +0200

    Solution 13 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4eb0byil Prompt:     9 |     set<int> result;  // declare before using       |         ^~~ b492b17b-2c38-4a74-89a7-96b7eb0ab3d6.cpp:10:9: error: expected primary-expressio n before ‘int’    10 |     set<int> common_result;       |         ^~~ b492b17b-2c38-4a74-89a7-96b7eb0ab3d6.cpp:11:5: error: ‘common_result’ was not de clared in this scope    11 |     common_result = set_intersection(s1.begin(), s1.end(), s2.begin(), s 2.end(),       |     ^~~~~~~~~~~~~ b492b17b-2c38-4a74-89a7-96b7eb0ab3d6.cpp:11:38: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    11 |     common_result = set_intersection(s1.begin(), s1.end(), s2.begin(), s 2.end(),       |                                      ^~       |                                      l1 b492b17b-2c38-4a74-89a7-96b7eb0ab3d6.cpp:11:60: error: ‘s2’ was not declared in this scope; did you mean ‘v2’?    11 |     common_result = set_intersection(s1.begin(), s1.end(), s2.begin(), s 2.end(),       |                                                            ^~       |                                                            v2

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,14 +1,3 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-vector<int> common(vector<int> l1, vector<int> v2) {
-    set<int> result(l1.begin(), l1.end());  
-    set<int> s2(v2.begin(), v2.end());
-
-    set<int> result_vector;  
-    result_vector = set_intersection(result.begin(), result.end(), s2.begin(), s2.end(),
-        inserter(result_vector, result_vector.begin()));
-    vector<int> answer(result_vector.begin(), result_vector.end());
-    return answer;
-}
+```cpp
+common_result = set_intersection(l1.begin(), l1.end(), v2.begin(), v2.end(),
+    inserter(common_result, common_result.begin()));
