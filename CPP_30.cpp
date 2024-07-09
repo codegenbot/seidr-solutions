7e7ae072c7b2b6f247d0baf9eb16a08fa3f41079
beep boop <githubbot@vadim.me>
2024-07-09 05:27:22 +0200

    Solution 59 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jk5y03kk Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from a083da37-92e0-4742-8013-70ff432ab24c.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,6 +1,10 @@
 #include <initializer_list>
 
 vector<float> get_positive(vector<float> l) {
-    vector<float> result = {std::begin(l), std::end(std::filter_view<float>(l, [](float x){return x > 0;}))};
+    vector<float> result;
+    for (float num : l) {
+        if (num > 0) {
+            result.push_back(num);
+        }
+    }
     return result;
-}
