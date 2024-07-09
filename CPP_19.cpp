517795b9510f4fcc7b034e1465cfa63bb893d8d9
beep boop <githubbot@vadim.me>
2024-07-09 22:48:16 +0200

    Solution 4 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nxvc2egv Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,31 +1,26 @@
 #include <algorithm>
+#include <string>
+#include <map>
 using namespace std;
 
 string sort_numbers(string numbers) {
-    map<string, int> number_map = {{"zero", 0}, {"one", 1}, {"two", 2},
-                                    {"three", 3}, {"four", 4}, {"five", 5},
-                                    {"six", 6}, {"seven", 7}, {"eight", 8},
-                                    {"nine", 9}};
+    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
+                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
+                                {"eight", 8}, {"nine", 9}};
 
-    vector<string> num_list;
-    string temp = "";
-    for (int i = 0; i < numbers.length(); i++) {
-        if (numbers[i] == ' ') {
-            num_list.push_back(temp);
-            temp = "";
-        } else {
-            temp += numbers[i];
+    string result = "";
+    for (char c : numbers) {
+        if (c == ' ') {
+            continue;
         }
+        string num = "";
+        while (c != ' ' && c != '\0') {
+            num += c;
+            c = getchar();
+        }
+        result += numMap[num] < result.empty() ? "" : " ";
+        result += num;
     }
-    num_list.push_back(temp);
-
-    sort(num_list.begin(), num_list.end(),
-         [&number_map](string a, string b) {
-             return number_map[a] < number_map[b]; });
-
-    string result;
-    for (auto &num : num_list) {
-        result += num + " ";
-    }
-    return result.substr(0, result.length() - 1);
+    sort(result.begin(), result.end());
+    return result;
 }
