1d50c1022aa4711f83ff36dfb1666052b7fc521f
beep boop <githubbot@vadim.me>
2024-07-09 22:35:46 +0200

    Solution 15 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/0rh2bvpd Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,37 +1,8 @@
-int getXNumerator(string x);
-    int getXDenominator(string x);
-    int getNNumerator(string n);
-    int getNDenominator(string n);
-    int gcd(int a, int b);
-    bool simplify(string x, string n) {
-        int numeratorX = getXNumerator(x);
-        int denominatorX = getXDenominator(x);
-        int numeratorN = getNNumerator(n);
-        int denominatorN = getNDenominator(n);
-
-        int numeratorResult = numeratorX * numeratorN;
-        int denominatorResult = denominatorX * denominatorN;
-
-        return (denominatorResult % gcd(numeratorResult, denominatorResult) == 0);
-    }
-
-    int getXNumerator(string x) {
-        return stoi(x.substr(0, x.find('/')));
-    }
-
-    int getXDenominator(string x) {
-        return stoi(x.substr(x.find('/') + 1));
-    }
-
-    int getNNumerator(string n) {
-        return stoi(n.substr(0, n.find('/')));
-    }
-
-    int getNDenominator(string n) {
-        return stoi(n.substr(n.find('/') + 1));
-    }
-
-    int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
-    }
+bool simplify(string x, string n){
+    int num_x = stoi(x.substr(0, x.find('/')));
+    int den_x = stoi(x.substr(x.find('/') + 1));
+    int num_n = stoi(n.substr(0, n.find('/')));
+    int den_n = stoi(n.substr(n.find('/') + 1));
+    
+    return ((num_x * den_n == num_n * den_x) && (num_x * den_n % den_x == 0));
+}
