9a1e5037fa1b1021a17f593de43cd0fc624188da
beep boop <githubbot@vadim.me>
2024-07-09 17:46:30 +0200

    Solution 7 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/hfjhv8f5 Prompt: in this scope    26 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |     ^~~~~~ 49468f1d-0c85-4a15-8f60-7283e0d185a4.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | #include <string> 49468f1d-0c85-4a15-8f60-7283e0d185a4.cpp: At global scope: 49468f1d-0c85-4a15-8f60-7283e0d185a4.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 49468f1d-0c85-4a15-8f60-7283e0d185a4.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from 49468f1d-0c85-4a15-8f60-7283e0d185a4.cpp:30: 49468f1d-0c85-4a15-8f60-7283e0d185a4.cpp: In function ‘int main()’: 49468f1d-0c85-4a15-8f60-7283e0d185a4.cpp:32:57: error: ISO C++ forbids compariso n between pointer and integer [-fpermissive]    32 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
-std::string Strongest_Extension(std::string class_name, std::vector<std::string> extensions);
-
-int main() {
-    assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
-}
+#include <cassert>
 
 int Strongest_Extension(std::string class_name, std::vector<std::string> extensions){
     int max_strength = 0;
     std::string strongest_extension;
 
     for(auto extension : extensions){
-        std::string strong_ext;
+        int strength = 0;
+        bool is_upper = false;
         for(auto c : extension){
-            if(isupper(c)) strong_ext += to_string(c);
-            else if(islower(c)) strong_ext += to_string(c);
+            if(std::isupper(c)) {
+                is_upper = true;
+                strength++;
+            }
         }
-        int strength = strong_ext.length() - (int)strong_ext.convert_to_lower().length();
-        if(strength > max_strength || (strength == max_strength && extension.compare(0, extension.size(), strongest_extension) < 0)){
+        if(strength > max_strength || (strength == max_strength && (!is_upper || extension.compare(0, extension.size(), strongest_extension) < 0))){
             max_strength = strength;
             strongest_extension = extension;
         }
     }
 
     return class_name + "." + strongest_extension;
+}
+
+int main() {
+    assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
+    return 0;
 }
