2583f4c5ac492474a129921395a1c22810290059
beep boop <githubbot@vadim.me>
2024-07-09 08:29:38 +0200

    Solution 65 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n98cndzy Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2d3241d6-b15b-4801-bce1-2a9e428bd 14a.py", line 22, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2d3241d6-b15b-4801-bce1-2a9e428bd 14a.py", line 19, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,13 +1,16 @@
+```
 def match_parens(lst):
     stack = []
     for s in lst:
-        for char in s:
-            if char == "(":
-                stack.append(char)
-            elif char == ")":
-                if len(stack) == 0:
-                    return "No"
-                else:
+        while "(" in s and ")" in s:
+            if s.count("(") == s.count(")"):
+                while "(" in s:
+                    stack.append("(")
+                    s = s.replace("(", "", 1)
+                while ")" in s:
+                    if not stack:
+                        return "No"
+                    s = s.replace(")", "", 1, 1)
                     stack.pop()
     if len(stack) == 0:
         return "Yes"
