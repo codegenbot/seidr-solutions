800ad6687e9a2ee32739a49ca3312629b740774b
beep boop <githubbot@vadim.me>
2024-07-09 18:31:48 +0200

    Solution 6 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d9zek7iz Prompt:       |                              ^~~~~~ c0e28dfa-07c0-4349-90a9-ff8fc020ef2b.cpp:38:37: error: ‘string’ was not declared  in this scope    38 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c0e28dfa-07c0-4349-90a9-ff8fc020ef2b.cpp:38:44: error: ‘b’ was not declared in t his scope    38 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c0e28dfa-07c0-4349-90a9-ff8fc020ef2b.cpp:38:45: error: expression list treated a s compound expression in initializer [-fpermissive]    38 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c0e28dfa-07c0-4349-90a9-ff8fc020ef2b.cpp:37: c0e28dfa-07c0-4349-90a9-ff8fc020ef2b.cpp: In function ‘int main()’: c0e28dfa-07c0-4349-90a9-ff8fc020ef2b.cpp:47:20: error: ‘numerical_letter_grade’ was not declared in this scope    47 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ c0e28dfa-07c0-4349-90a9-ff8fc020ef2b.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,57 +1,38 @@
-#include <iostream>
 #include <vector>
 #include <string>
 
-std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
-    std::vector<std::string> letter_grades;
+vector<string> numerical_letter_grade(vector<float> grades) {
+    vector<string> result;
     for (float grade : grades) {
-        if (grade >= 4.0)
-            letter_grades.push_back("A+");
-        else if (grade > 3.7)
-            letter_grades.push_back("A");
-        else if (grade > 3.3)
-            letter_grades.push_back("A-");
-        else if (grade > 3.0)
-            letter_grades.push_back("B+");
-        else if (grade > 2.7)
-            letter_grades.push_back("B");
-        else if (grade > 2.3)
-            letter_grades.push_back("B-");
-        else if (grade > 2.0)
-            letter_grades.push_back("C+");
-        else if (grade > 1.7)
-            letter_grades.push_back("C");
-        else if (grade > 1.3)
-            letter_grades.push_back("C-");
-        else if (grade > 1.0)
-            letter_grades.push_back("D+");
-        else if (grade > 0.7)
-            letter_grades.push_back("D");
-        else
-            letter_grades.push_back("E");
+        string letterGrade = "";
+        if (grade >= 4.0) {
+            letterGrade = "A+";
+        } else if (grade > 3.7) {
+            letterGrade = "A";
+        } else if (grade > 3.3) {
+            letterGrade = "A-";
+        } else if (grade > 3.0) {
+            letterGrade = "B+";
+        } else if (grade > 2.7) {
+            letterGrade = "B";
+        } else if (grade > 2.3) {
+            letterGrade = "B-";
+        } else if (grade > 2.0) {
+            letterGrade = "C+";
+        } else if (grade > 1.7) {
+            letterGrade = "C";
+        } else if (grade > 1.3) {
+            letterGrade = "C-";
+        } else if (grade > 1.0) {
+            letterGrade = "D+";
+        } else if (grade > 0.7) {
+            letterGrade = "D";
+        } else if (grade > 0.0) {
+            letterGrade = "D-";
+        } else {
+            letterGrade = "E";
+        }
+        result.push_back(letterGrade);
     }
-    return letter_grades;
-}
-
-int main() {
-    int n;
-    std::cout << "Enter the number of grades: ";
-    std::cin >> n;
-    
-    std::vector<float> grades;
-    
-    for(int i = 0; i < n; i++) {
-        float grade;
-        std::cout << "Enter grade " << (i+1) << ": ";
-        std::cin >> grade;
-        grades.push_back(grade);
-    }
-    
-    std::vector<std::string> letter_grades = numerical_letter_grade(grades);
-    
-    for(std::string s : letter_grades) {
-        std::cout << s << "\n";
-    }
-    
-    return 0;
+    return result;
 }
