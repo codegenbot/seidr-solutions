b2aa9c60e9b79b072fd43a1ba7150b073b725bdd
beep boop <githubbot@vadim.me>
2024-07-09 12:16:30 +0200

    Solution 32 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3e7tqgvq Prompt:       |                      ^~~~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘typename __gnu_cxx ::__enable_if<std::__is_char<_Tp>::__value, bool>::__type std::operator==(const std::__cxx11::basic_string<_CharT>&, const std::__cxx11::basic_string<_CharT>&) [with _CharT = char; typename __gnu_cxx::__enable_if<std::__is_char<_Tp>::__valu e, bool>::__type = bool]’: /usr/include/c++/11/bits/basic_string.h:6305:22:   required from ‘bool std::oper ator!=(const std::__cxx11::basic_string<_CharT, _Traits, _Allocator>&, const std ::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _Tra its = std::char_traits<char>; _Alloc = std::allocator<char>]’ 3631a099-fe99-4559-be6e-a1782cbddcf3.cpp:9:24:   required from here /usr/include/c++/11/bits/basic_string.h:6236:21: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6236 |     { return (__lhs.size() == __rhs.size()       |               ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h:6236:37: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6236 |     { return (__lhs.size() == __rhs.size()       |                               ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h:6238:59: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6238 |                                                     __lhs.size())); }       |                                                     ~~~~~~^~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -3,12 +3,8 @@
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string>& b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    return a.size() != b.size();
 }
 
 std::string separate_paren_groups(std::string s) {
