cb5b9d7ad8bdbc055a2c20fc4906d4d61f162896
beep boop <githubbot@vadim.me>
2024-07-09 23:23:22 +0200

    Solution 95 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fxnddfx6 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:10: f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp: In function ‘int main()’: f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ f4793d5e-f9a1-48f6-a926-84e09917c0ee.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,5 +1,23 @@
+vector<string> filter_by_prefix(vector<string> strings, string prefix){
+    vector<string> result;
+    for(string s : strings){
+        if(s.find(prefix) == 0)
+            result.push_back(s);
+    }
+    return result;
+
+}
+
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) return false;
-    for(string s : a) if(find(b.begin(), b.end(), s) == b.end()) return false;
+    if (a.size() != b.size()) 
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (!(find(a[i].begin(), a[i].end(), ' ') == string::npos)) {
+            int pos1 = find(a[i].begin(), a[i].end(), ' ');
+            int pos2 = find(b[i].begin(), b[i].end(), ' ');
+            if (pos1 != pos2) 
+                return false;
+        }
+    }
     return true;
 }
