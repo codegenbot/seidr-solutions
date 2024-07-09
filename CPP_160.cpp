0cfec1a80847aae57265f38d890f3d3a886a13cd
beep boop <githubbot@vadim.me>
2024-07-09 20:22:24 +0200

    Solution 17 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8tmp81k3 Prompt:                  from /usr/include/c++/11/vector:60,                  from d88a1210-9888-4c25-a420-a47908a51ce0.cpp:2: /usr/include/c++/11/bits/stl_pair.h:211:12: note: ‘std::pair’ declared here   211 |     struct pair       |            ^~~~ d88a1210-9888-4c25-a420-a47908a51ce0.cpp:5:28: error: expected primary-expressio n before ‘char’     5 | int do_algebra(vector<pair<char, int>> operators_and_operands) {       |                            ^~~~ d88a1210-9888-4c25-a420-a47908a51ce0.cpp:5:34: error: expected primary-expressio n before ‘int’     5 | int do_algebra(vector<pair<char, int>> operators_and_operands) {       |                                  ^~~ d88a1210-9888-4c25-a420-a47908a51ce0.cpp:5:62: error: expression list treated as  compound expression in initializer [-fpermissive]     5 | int do_algebra(vector<pair<char, int>> operators_and_operands) {       |                                                              ^ In file included from d88a1210-9888-4c25-a420-a47908a51ce0.cpp:23: d88a1210-9888-4c25-a420-a47908a51ce0.cpp: In function ‘int main()’: d88a1210-9888-4c25-a420-a47908a51ce0.cpp:25:23: error: ‘do_algebra’ cannot be us ed as a function    25 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,6 +1,6 @@
 int do_algebra(std::vector<std::pair<char, int>> operators_and_operands) {
     int result = operators_and_operands[0].second;
-    for (int i = 1; i < operators_and_opernds.size(); i++) {
+    for (int i = 1; i < operators_and_operands.size(); i++) {
         if (operators_and_operands[i].first == '+') {
             result += operators_and_operands[i].second;
         } else if (operators_and_operands[i].first == '-') {
@@ -9,6 +9,8 @@ int do_algebra(std::vector<std::pair<char, int>> operators_and_operands) {
             result *= operators_and_operands[i].second;
         } else if (operators_and_operands[i].first == '/') {
             result /= operators_and_operands[i].second;
+        } else if (operators_and_operands[i].first == '^') {
+            result = pow(result, operators_and_operands[i].second);
         }
     }
     return result;
