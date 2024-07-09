0c2e4efa1008a58effdaa790e112ce5edcc40c0e
beep boop <githubbot@vadim.me>
2024-07-09 13:07:07 +0200

    Solution 61 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/n5hu6g1u Prompt:       |                    ^~~ a6e44611-54d7-4a59-92e7-54b237b8fdd6.cpp:21:27: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a6e44611-54d7-4a59-92e7-54b237b8fdd6.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a6e44611-54d7-4a59-92e7-54b237b8fdd6.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a6e44611-54d7-4a59-92e7-54b237b8fdd6.cpp:20: a6e44611-54d7-4a59-92e7-54b237b8fdd6.cpp: In function ‘int main()’: a6e44611-54d7-4a59-92e7-54b237b8fdd6.cpp:30:20: error: ‘make_a_pile’ was not dec lared in this scope    30 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ a6e44611-54d7-4a59-92e7-54b237b8fdd6.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 
-vector<int> make_a_pile(int n) {
-    vector<int> pile;
+std::vector<int> make_a_pile(int n) {
+    std::vector<int> pile;
     int stones = 1;
     for (int i = 0; i < n; i++) {
         if (i % 2 == 0) {
