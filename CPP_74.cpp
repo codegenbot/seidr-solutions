c08e8d10f5a7bf47dd989a5bebc0e24159c3975a
beep boop <githubbot@vadim.me>
2024-07-09 03:58:28 +0200

    Solution 40 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8ahd9aop Prompt:  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 6dfc0806-d042-408f-85c6-b98eb2d173fa.cpp:26:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 6dfc0806-d042-408f-85c6-b98eb2d173fa.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 6dfc0806-d042-408f-85c6-b98eb2d173fa.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 6dfc0806-d042-408f-85c6-b98eb2d173fa.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 6dfc0806-d042-408f-85c6-b98eb2d173fa.cpp:25: 6dfc0806-d042-408f-85c6-b98eb2d173fa.cpp: In function ‘int main()’: 6dfc0806-d042-408f-85c6-b98eb2d173fa.cpp:35:20: error: ‘total_match’ was not dec lared in this scope    35 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,23 +1,23 @@
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> vec2) {
-    if (a.size() != vec2.size()) return false;
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a.size() != b.size()) return false;
     for (size_t i = 0; i < a.size(); i++) {
-        if (a[i] != vec2[i]) return false;
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
 
-std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<std::string> lst2) {
+std::vector<std::string> total_match(std::vector<std::string> a, std::vector<std::string> b) {
     int sum1 = 0, sum2 = 0;
-    for (const auto& s : lst1) {
+    for (const auto& s : a) {
         sum1 += s.length();
     }
-    for (const auto& s : lst2) {
+    for (const auto& s : b) {
         sum2 += s.length();
     }
-    if (sum1 < sum2) return lst1;
-    else if (sum1 > sum2) return lst2;
-    else return lst1;
+    if (sum1 < sum2) return a;
+    else if (sum1 > sum2) return b;
+    else return a;
 }
