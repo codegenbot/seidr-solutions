1d4309f03b3950981b26429125ff15da8df3829b
beep boop <githubbot@vadim.me>
2024-07-09 21:07:38 +0200

    Solution 31 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/juk3nsjv Prompt:    16 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 087197ba-cebc-4d12-bcfc-c177b4fb41c0.cpp:16:36: error: expected primary-expressi on before ‘float’    16 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 087197ba-cebc-4d12-bcfc-c177b4fb41c0.cpp:16:43: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 087197ba-cebc-4d12-bcfc-c177b4fb41c0.cpp:15: 087197ba-cebc-4d12-bcfc-c177b4fb41c0.cpp: In function ‘int main()’: 087197ba-cebc-4d12-bcfc-c177b4fb41c0.cpp:25:20: error: ‘sort_even’ was not decla red in this scope    25 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 087197ba-cebc-4d12-bcfc-c177b4fb41c0.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,22 +1,7 @@
-vector<float> sort_even(vector<float> l) {
-    vector<float> result;
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) { // even index
-            vector<float> evens(l.begin() + i, l.begin() + i + 1);
-            sort(evens.begin(), evens.end());
-            result.push_back(evens[0]);
-        } else { // odd index
-            bool found = false;
-            for (float n : l) {
-                if (!found) {
-                    found = issame({n}, vector<float>(l.begin() + i, l.begin() + i + 1));
-                }
-                if (found) {
-                    result.push_back(n);
-                    break;
-                }
-            }
-        }
+bool issame(vector<float> a,vector<float>b){
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++){
+        if(abs(a[i]-b[i]) > 0.0001) return false;
     }
-    return result;
+    return true;
 }
