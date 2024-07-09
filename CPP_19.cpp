a6dd5f2fe0d9ddb83c486be9dd789e6d5f6a780b
beep boop <githubbot@vadim.me>
2024-07-09 20:35:33 +0200

    Solution 0 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/smr2nss5 Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_19.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+string sort_numbers(string numbers) {
+    vector<string> numVec;
+    string temp;
+
+    for (int i = 0; i < numbers.length(); i++) {
+        if (numbers[i] == ' ') {
+            continue;
+        }
+        temp += numbers[i];
+        if ((i + 1) >= numbers.length() || numbers[i + 1] != ' ') {
+            numVec.push_back(temp);
+            temp = "";
+        }
+    }
+
+    sort(numVec.begin(), numVec.end());
+
+    string result = "";
+    for (auto &num : numVec) {
+        result += num;
+        if (&num != &numVec.back()) {
+            result += " ";
+        }
+    }
+
+    return result;
+}
