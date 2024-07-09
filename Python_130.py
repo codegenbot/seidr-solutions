6238db71884296137c22b50b9641298bcb9ace2f
beep boop <githubbot@vadim.me>
2024-07-09 02:59:40 +0200

    Solution 15 of Python/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/oqjf4p06 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4b3172f3-6ebc-42ef-9b5a-bbb9d4109 a47.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_130.py
+++ Python_130.py
@@ -1,5 +1,15 @@
 def tri(n):
-    sequence = [3, 2]
-    for _ in range(2, n):
-        sequence.append(sum(sequence[-1::-1]))
-    return sequence
+    if n == 0:
+        return [3, 2, 1, 3]
+    elif n == 1:
+        return [3, 2, 1, 3][1:]
+    elif n == 2:
+        return [3, 2]
+    else:
+        sequence = [3, 2]
+        for _ in range(2, n):
+            next_value = sum(sequence[-1::-1])
+            if next_value > 4: 
+                next_value = 9 - next_value
+            sequence.append(next_value)
+        return sequence
