a61551166fa8e081d1e713e4edc314d83e82c704
beep boop <githubbot@vadim.me>
2024-07-09 04:34:21 +0200

    Solution 42 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5ldlq6rg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/166d7adf-1ed2-4643-9ebf-abd2396ff 339.py", line 1     `return " ".join(str(num_dict[i]) for i in sorted(map(int, numbers.split())) )`     ^ SyntaxError: invalid syntax

--- Python_19.py
+++ Python_19.py
@@ -1,1 +1,8 @@
-`def solution(numbers): return " ".join(map(str, sorted(map(int, numbers.split()))))`
+def solution(numbers):
+    num_dict = {}
+    for num in map(int, numbers.split()):
+        if num % 2 == 0:
+            num_dict[num] = "even"
+        else:
+            num_dict[num] = "odd"
+    return " ".join(str(num_dict[i]) for i in sorted(map(int, numbers.split())))
