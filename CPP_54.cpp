695858632214a0fccfcff1e1f74cb70997bf26e6
beep boop <githubbot@vadim.me>
2024-07-09 11:59:27 +0200

    Solution 1 of CPP/54, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/obfs1mt0 Prompt: Complete the following code given the task description and function signature.

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -1,7 +1,11 @@
-Here is the completed code:
+#include<stdio.h>
+#include<string>
+#include<algorithm>
+using namespace std;
 
 bool same_chars(string s0, string s1) {
     sort(s0.begin(), s0.end());
     sort(s1.begin(), s1.end());
+
     return s0 == s1;
 }
