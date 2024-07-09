a2804be0b07da840d5ab617e72530b6152e0ede7
beep boop <githubbot@vadim.me>
2024-07-09 04:57:38 +0200

    Solution 13 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/d9xvhjel Prompt: d9850908-ebe0-4f50-a25e-89eef2902fb5.cpp:3:1: error: ‘string’ does not name a ty pe     3 | string string_to_md5(string text) {       | ^~~~~~ In file included from d9850908-ebe0-4f50-a25e-89eef2902fb5.cpp:19: d9850908-ebe0-4f50-a25e-89eef2902fb5.cpp: In function ‘int main()’: d9850908-ebe0-4f50-a25e-89eef2902fb5.cpp:21:13: error: ‘string_to_md5’ was not d eclared in this scope    21 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -10,9 +10,9 @@ std::string string_to_md5(std::string text) {
     unsigned char result[MD5_DIGEST_LENGTH];
     MD5((const unsigned char*)text.c_str(), text.size(), result);
 
-    string output;
+    std::string output;
     for(int i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        stringstream ss;
+        std::stringstream ss;
         ss << setfill('0') << setw(2) << hex << (int)result[i];
         output += ss.str();
     }
