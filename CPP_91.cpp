8c63193e58577fabdd3dea813f1709dfcc126c95
beep boop <githubbot@vadim.me>
2024-07-09 09:27:08 +0200

    Solution 11 of CPP/91, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lj9nwutv Prompt: 414b02f9-e6e5-4a35-a910-e0380508a3f0.cpp:1:19: error: ‘string’ is not a member o f ‘std’     1 | int is_bored(std::string S){       |                   ^~~~~~ 414b02f9-e6e5-4a35-a910-e0380508a3f0.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int is_bored(std::string S){ In file included from 414b02f9-e6e5-4a35-a910-e0380508a3f0.cpp:17: 414b02f9-e6e5-4a35-a910-e0380508a3f0.cpp: In function ‘int main()’: 414b02f9-e6e5-4a35-a910-e0380508a3f0.cpp:19:21: error: ‘is_bored’ cannot be used  as a function    19 |     assert (is_bored("You and I are going for a walk") == 0);       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,13 +1,17 @@
-int is_bored(string S){
+#include <string>
+
+int is_bored(std::string S){
     int count = 0;
-    string boredom = "I";
+    std::string boredom = "I am bored";
     size_t pos = 0;
-    while ((pos = S.find(" ", pos)) != string::npos) {
+    while ((pos = S.find(" ", pos)) != std::string::npos) {
         if (S.substr(0, pos).compare(boredom) == 0) {
             count++;
-            break;
+            S.erase(0, pos + boredom.size());
+            pos = S.find(" ", pos); // Update this line
+        } else {
+            pos++;
         }
-        pos++;
     }
     return count;
 }
