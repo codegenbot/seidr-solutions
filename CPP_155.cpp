00d65d9879869557a9f30cb0c7bd7a71425763dc
beep boop <githubbot@vadim.me>
2024-07-09 02:27:37 +0200

    Solution 10 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fkq142tr Prompt: In file included from /usr/include/c++/11/vector:67,                  from c3114ee3-b3e8-4149-8ad3-6e88503a95fa.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from c3114ee3-b3e8-4149-8ad3-6e88503a95fa.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c3114ee3-b3e8-4149-8ad3-6e88503a95fa.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c3114ee3-b3e8-4149-8ad3-6e88503a95fa.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c3114ee3-b3e8-4149-8ad3-6e88503a95fa.cpp:16: c3114ee3-b3e8-4149-8ad3-6e88503a95fa.cpp: In function ‘int main()’: c3114ee3-b3e8-4149-8ad3-6e88503a95fa.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,9 +1,15 @@
 #include <vector>
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size())
-        return false;
-    for(int i = 0; i < a.size(); i++)
-        if(a[i] != b[i])
-            return false;
-    return true;
+#include <iostream>
+
+std::vector<int> even_odd_count(int num) {
+    std::vector<int> result(2, 0);
+    while (num != 0) {
+        int digit = num % 10;
+        if (digit % 2 == 0)
+            result[0]++;
+        else
+            result[1]++;
+        num /= 10;
+    }
+    return result;
 }
