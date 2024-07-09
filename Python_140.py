760616d39cc46e5fe88f796ef588a2083b038544
beep boop <githubbot@vadim.me>
2024-07-09 03:15:45 +0200

    Solution 8 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/qhu264n1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/99c874e3-52d6-4435-b74f-90ee05fd7 042.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,10 +1,13 @@
 def fix_spaces(text):
     new_text = ''
     for i in range(len(text)):
-        if i > 0 and text[i-1] == ' ' and text[i] == ' ':
-            new_text += '-'
-        elif text[i] == ' ':
-            new_text += '_'
+        if text[i] == ' ':
+            if i > 0 and text[i-1] not in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+                new_text += '_'
+            else:
+                new_text += ' '
+        elif text[i].lower() in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+            new_text += text[i].upper()
         else:
             new_text += text[i]
     return new_text
