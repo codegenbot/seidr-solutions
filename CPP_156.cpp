e73ccb6301a20c0d4cd749abea9776c81f3bc3dd
beep boop <githubbot@vadim.me>
2024-07-09 05:40:21 +0200

    Solution 69 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/urrbvh7e Prompt: 12c9414f-e8e1-462a-8b4b-6461c417de62.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int num) {       | ^~~~~~ In file included from 12c9414f-e8e1-462a-8b4b-6461c417de62.cpp:87: 12c9414f-e8e1-462a-8b4b-6461c417de62.cpp: In function ‘int main()’: 12c9414f-e8e1-462a-8b4b-6461c417de62.cpp:89:13: error: ‘int_to_mini_romank’ was not declared in this scope    89 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,3 +1,4 @@
+```
 std::string int_to_mini_roman(int num) {
     std::string roman[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",
                           "", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
@@ -10,7 +11,7 @@ std::string int_to_mini_roman(int num) {
                           "", "C", "CI", "CII", "CIII", "CIV", "CV", "CVI", "CVII", "CVIII", "CX", "CXI", "CXII", "CXIII", "CXIV", "CXV", "CXVI", "CXVII", "CXVIII", "CXIX", "CXX",
                           "", "CXXI", "CXXII", "CXXIII", "CXXIV", "CXXV", "CXXVI", "CXXVII", "CXXVIII", "CXXIX", "CXXX",
                           "", "CXXXI", "CXXXII", "CXXXIII", "CXXXIV", "CXXXV", "CXXXVI", "CXXXVII", "CXXXVIII", "CXXXIX", "CXL",
-                          "", "CXLI", "CXLII", "CXLIII", "CXLIV", "CXLV", "CXLVI", "CXLVII", "CXLVIII", "CXlix", "CL",
+                          "", "CXLI", "CXLII", "CXLIII", "CXLIV", "CXLV", "CXLVI", "CXLVII", "CXLVIII", "CXCIX", "CL",
                           "", "CLI", "CII", "CC", "CCI", "CCC", "CCCI", "CCCII", "CCCIII", "CCCIV", "CCCV", "CCCVI", "CCCvii", "CCcviii", "CCXC", 
                           "", "CM"};
 
@@ -29,57 +30,35 @@ std::string int_to_mini_roman(int num) {
         } else if (num >= 400) {
             num -= 400;
             result += "CD";
-        } else {
-            if (num >= 100) {
-                int count = num / 100;
-                for(int i=0; i <count ; i++) {
+        } else if (num >= 100) {
+            for(int i = 0; i < 3; ++i) {
+                if(num >= 100) {
+                    num -= 100;
                     result += "C";
                 }
-                num -= 100 * count;
             }
-
-            if (num >= 90) {
-                num -= 90;
-                result += "XC";
-            } else if (num >= 50) {
-                num -= 50;
-                result += "L";
-            } else if (num >= 40) {
-                num -= 40;
-                result += "XL";
-            } else {
-                while(num > 0) {
-                    if(num >= 10) {
-                        int count = num / 10;
-                        for(int i=0; i <count ; i++) {
-                            result += "X";
-                        }
-                        num -= 10 * count;
-                    }
-
-                    if(num >= 9) {
-                        num -= 9;
-                        result += "IX";
-                    } else if (num >= 5) {
-                        num -= 5;
-                        result += "V";
-                    } else if (num >= 4) {
-                        num -= 4;
-                        result += "IV";
-                    } else {
-                        while(num > 0) {
-                            if(num >= 3) {
-                                num -= 3;
-                                result += "I";
-                            } else if (num == 2) {
-                                result += "II";
-                                return result;
-                            } else {
-                                num--;
-                                result += "I";
-                            }
-                        }
-                    }
+        } else if (num >= 90) {
+            num -= 90;
+            result += "XC";
+        } else if (num >= 50) {
+            for(int i = 0; i < 2; ++i) {
+                if(num >= 50) {
+                    num -= 50;
+                    result += "L";
+                }
+            }
+        } else if (num >= 40) {
+            num -= 40;
+            result += "XL";
+        } else {
+            for(int i = 0; ; ++i) {
+                if(num >= 10) {
+                    num -= 10;
+                    result += "X";
+                } else if (num == 9) {
+                    num -= 9;
+                    result += "IX";
+                    break;
                 }
             }
         }
