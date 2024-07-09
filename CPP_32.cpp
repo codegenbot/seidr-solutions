5069ee8a345a9ed5d3888a5b93ac5f966434fb53
beep boop <githubbot@vadim.me>
2024-07-09 03:41:44 +0200

    Solution 69 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nvqm4xhw Prompt: 1887aa63-7751-4961-8ad0-eccc4a3f707c.cpp:27:16: error: expected primary-expressi on before ‘double’    27 |         vector<double> coeffs = {};       |                ^~~~~~ 1887aa63-7751-4961-8ad0-eccc4a3f707c.cpp:32:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    32 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 1887aa63-7751-4961-8ad0-eccc4a3f707c.cpp:35:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    35 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 1887aa63-7751-4961-8ad0-eccc4a3f707c.cpp:35:36: error: ‘find_zero’ cannot be use d as a function    35 |         solution = find_zero(coeffs);       |                                    ^ In file included from 1887aa63-7751-4961-8ad0-eccc4a3f707c.cpp:19: 1887aa63-7751-4961-8ad0-eccc4a3f707c.cpp:36:25: error: ‘poly’ cannot be used as a function    36 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ~~~~^~~~~~~~~~~~~~~~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -11,23 +11,7 @@ double poly(vector<double> coeffs, double x) {
 
 double find_zero(vector<double> xs){
     if(xs.size() % 2 != 0) return -1; 
-    vector<double> coeffs(xs.size(), 0);
+    vector<double> coeffs = {};
     double root = -xs[1]/(2*xs[0]);
     return std::round(poly(coeffs, root));
-}
-
-int main(){
-    int n;
-    cin >> n;
-    vector<double> xs(n);
-    for (int i=0; i<n; i++){
-        cin >> xs[i];
-    }
-    
-    vector<double> coeffs = {};
-    double solution = find_zero(xs);
-
-    cout << "The zero is: " << solution << endl;
-
-    return 0;
 }
