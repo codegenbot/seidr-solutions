dced9db26c21f8062e9e57a8150dbf2e8d1daa82
beep boop <githubbot@vadim.me>
2024-07-09 20:56:38 +0200

    Solution 23 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/jy7fbynr Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 79eed324-2e78-4b97-a288-c372d5c5e838.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 79eed324-2e78-4b97-a288-c372d5c5e838.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 79eed324-2e78-4b97-a288-c372d5c5e838.cpp:10: 79eed324-2e78-4b97-a288-c372d5c5e838.cpp: In function ‘int main()’: 79eed324-2e78-4b97-a288-c372d5c5e838.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 79eed324-2e78-4b97-a288-c372d5c5e838.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,3 +1,23 @@
+bool issame(string s1, string s2) {
+    for (int i = 0; i < s1.size(); i++) {
+        if (s1[i] != s2[i])
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    vector<string> strings = {"apple", "banana", "apricot", "almond"};
+    string prefix = "a";
+    for(string s : filter_by_prefix(strings, prefix)) {
+        if (issame(s, prefix))
+            cout << s << " is same as the prefix." << endl;
+        else
+            cout << s << " is not same as the prefix." << endl;
+    }
+    return 0;
+}
+
 vector<string> filter_by_prefix(vector<string> strings, string prefix){
     vector<string> result;
     for(string s : strings){
@@ -5,17 +25,4 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
             result.push_back(s);
     }
     return result;
-}
-
-bool issame(const string& a, const string& b) {
-    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
-}
-
-int main() {
-    vector<string> strings = {"abc", "abcd", "xyz"};
-    string prefix = "ab";
-    vector<string> result = filter_by_prefix(strings, prefix);
-    for (string s : result) {
-        cout << s << endl;
-    }
 }
