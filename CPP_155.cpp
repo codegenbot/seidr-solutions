8739dd12401312a7e9bbe254d86f974590412556
beep boop <githubbot@vadim.me>
2024-07-09 02:34:35 +0200

    Solution 21 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fkq142tr Prompt: In file included from /usr/include/c++/11/vector:67,                  from 47d46585-c860-4afa-989a-75a7f5e59aa3.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 47d46585-c860-4afa-989a-75a7f5e59aa3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 47d46585-c860-4afa-989a-75a7f5e59aa3.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 47d46585-c860-4afa-989a-75a7f5e59aa3.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 47d46585-c860-4afa-989a-75a7f5e59aa3.cpp:17: 47d46585-c860-4afa-989a-75a7f5e59aa3.cpp: In function ‘int main()’: 47d46585-c860-4afa-989a-75a7f5e59aa3.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,8 +1,21 @@
-```
 #include <vector>
 #include <iostream>
 
 bool same = (even_odd_count(0) == {1, 0});
+
+int main() {
+    std::vector<int> result;
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+    result = even_odd_count(num);
+    if (same)
+        std::cout << "Number of even and odd digits are same.\n";
+    else
+        std::cout << "Number of even and odd digits are not the same.\n";
+    
+    return 0;
+}
 
 std::vector<int> even_odd_count(int num) {
     std::vector<int> result(2, 0);
