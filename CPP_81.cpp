0fb006f72ef5de727e67cf7ccada7ad49ab4a72f
beep boop <githubbot@vadim.me>
2024-07-09 04:00:40 +0200

    Solution 38 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zxgul3d6 Prompt:  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 9f939135-2799-4fd4-818a-3ecc0cc3c540.cpp:29:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 9f939135-2799-4fd4-818a-3ecc0cc3c540.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 9f939135-2799-4fd4-818a-3ecc0cc3c540.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 9f939135-2799-4fd4-818a-3ecc0cc3c540.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 9f939135-2799-4fd4-818a-3ecc0cc3c540.cpp:28: 9f939135-2799-4fd4-818a-3ecc0cc3c540.cpp: In function ‘int main()’: 9f939135-2799-4fd4-818a-3ecc0cc3c540.cpp:38:42: error: ‘numerical_letter_grade’ cannot be used as a function    38 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -2,12 +2,7 @@
 #include <string>
 
 bool issame(std::vector<std::string> b, std::vector<std::string> a){
-    if(a.size()!=b.size())
-        return false;
-    for(int i=0;i<a.size();i++)
-        if(a[i] != b[i])
-            return false;
-    return true;
+    return (a == b);
 }
 
 std::vector<string> numerical_letter_grade(vector<float> grades){
