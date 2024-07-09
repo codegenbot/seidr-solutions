5f286b375ecfc061a87544c4f0a40bc68e984bba
beep boop <githubbot@vadim.me>
2024-07-09 07:40:45 +0200

    Solution 34 of CPP/18, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xxdogq6a Prompt: ka ‘const std::__cxx11::basic_string<char>’} as ‘this’ argument discards qualifi ers [-fpermissive]    12 |         str.erase(pos, substring.length());       |         ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from 2106d2d1-f835-4d02-b23f-3821e5c03be3.cpp:1: /usr/include/c++/11/bits/basic_string.h:1827:7: note:   in call to ‘std::__cxx11 ::basic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Tra its, _Alloc>::erase(std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_ty pe, std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type) [with _CharT  = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>; std::_ _cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type = long unsigned int]’  1827 |       erase(size_type __pos = 0, size_type __n = npos)       |       ^~~~~ 2106d2d1-f835-4d02-b23f-3821e5c03be3.cpp: At global scope: 2106d2d1-f835-4d02-b23f-3821e5c03be3.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 2106d2d1-f835-4d02-b23f-3821e5c03be3.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -4,15 +4,12 @@
 
 using namespace std;
 
-int how_many_times(const string& str, const string& substring) {
+int how_many_times(string& str, const string& substring) {
     int count = 0;
     size_t pos = 0;
-    size_t original_len = str.length();
-    
     while ((pos = str.find(substring)) != string::npos) {
         count++;
-        str.erase(0, pos + substring.length());
-        original_len -= (pos + substring.length());
+        str.erase(pos, substring.length());
     }
     return count;
 
@@ -26,7 +23,7 @@ int main() {
     cin >> substring;
     
     if (str.find(substring) == string::npos) {
-        cout << "Error: The substring is not found in the string.\n";
+        cout << "The substring is not found in the string.\n";
     } else {
         int result = how_many_times(str, substring);
         cout << "The substring appears " << result << " times.\n";
