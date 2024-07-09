5d74a29df616bb00b8582c10e60bfcbb3ba7b9d8
beep boop <githubbot@vadim.me>
2024-07-09 07:30:06 +0200

    Solution 43 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/5ahk9ug2 Prompt: oat>’    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from d99b9fa9-7da1-4bd5-8847-124c6a0646a5.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<float>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ d99b9fa9-7da1-4bd5-8847-124c6a0646a5.cpp:4:42: note:   initializing argument 1 o f ‘void rescale_to_unit(std::vector<float>&)’     4 | void rescale_to_unit(std::vector<float>& numbers) {       |                      ~~~~~~~~~~~~~~~~~~~~^~~~~~~ In file included from d99b9fa9-7da1-4bd5-8847-124c6a0646a5.cpp:13: d99b9fa9-7da1-4bd5-8847-124c6a0646a5.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -14,5 +14,4 @@ void rescale_to_unit(std::vector<float>& numbers) {
 int main() {
     assert((rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), 
             std::vector<float>({0.25, 0.0, 1.0, 0.5, 0.75})));
-    return 0;
 }
