4f3fdf42c373518501b7ef33f959f4b3d107db97
beep boop <githubbot@vadim.me>
2024-07-09 07:49:37 +0200

    Solution 38 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/hg8vop6t Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,19 +1,16 @@
-#include <cctype>
-#include <string>
-
 std::string filp_case(const std::string& str) {
-    std::string result = str;
+    std::string result = str; 
     for (char &c : result) {
-        if (std::islower(c)) {
-            c = std::toupper(c);
-        } else if (std::isupper(c)) {
-            c = std::tolower(c);
+        if ((c >= 'a' && c <= 'z')) {
+            c = (c - 'a' + 'A'); 
+        } else if ((c >= 'A' && c <= 'Z')) {
+            c = (c - 'A' + 'a');
         }
     }
     return result;
 }
 
 int main() {
-    std::cout << filp_case("These violent delights have violent ends") << std::endl;
+    std::cout << flip_case("These violent delights have violent ends") << std::endl;
     return 0;
 }
