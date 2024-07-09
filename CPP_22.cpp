62dfd26b1bc4288714e94d08dc7317f0d8e5d6b5
beep boop <githubbot@vadim.me>
2024-07-09 05:31:04 +0200

    Solution 87 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qnmgtenv Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ eeadf3d5-4283-4e76-bcf8-d7d072616c35.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ eeadf3d5-4283-4e76-bcf8-d7d072616c35.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from eeadf3d5-4283-4e76-bcf8-d7d072616c35.cpp:11: eeadf3d5-4283-4e76-bcf8-d7d072616c35.cpp: In function ‘int main()’: eeadf3d5-4283-4e76-bcf8-d7d072616c35.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ eeadf3d5-4283-4e76-bcf8-d7d072616c35.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,19 +1,13 @@
-#include <vector>
 #include <boost/any.hpp>
+#include <vector>
+#include <list>
 
-using namespace std;
-using namespace boost;
-
-vector<int> filter_integers(list<any> values) {
-    vector<int> result;
+std::vector<int> filter_integers(std::list<boost::any> values) {
+    std::vector<int> result;
     for (const auto& value : values) {
-        if (any_cast<int>(value).good()) {
-            result.push_back(any_cast<int>(value));
+        if (value.type() == typeid(int) && boost::any_cast<int>(value).good()) {
+            result.push_back(boost::any_cast<int>(value));
         }
     }
     return result;
-}
-
-bool areEqual(vector<int> a, vector<int> b) {
-    return a == b;
 }
