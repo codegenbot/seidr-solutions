1331cab56d4a8808bd102b0ef22242c6234bc1ed
beep boop <githubbot@vadim.me>
2024-07-09 03:03:27 +0200

    Solution 37 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/bvoim4el Prompt: 79b7035b-fe66-4d66-9dd6-130d5adbc8fc.cpp:15:29: error: ‘list_any’ was not declar ed in this scope    15 | vector<int> filter_integers(list_any values) {       |                             ^~~~~~~~ 79b7035b-fe66-4d66-9dd6-130d5adbc8fc.cpp:15:46: error: expected ‘,’ or ‘;’ befor e ‘{’ token    15 | vector<int> filter_integers(list_any values) {       |                                              ^ 79b7035b-fe66-4d66-9dd6-130d5adbc8fc.cpp:26:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    26 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 79b7035b-fe66-4d66-9dd6-130d5adbc8fc.cpp:5:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     5 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ In file included from 79b7035b-fe66-4d66-9dd6-130d5adbc8fc.cpp:25: 79b7035b-fe66-4d66-9dd6-130d5adbc8fc.cpp: In function ‘int main()’: 79b7035b-fe66-4d66-9dd6-130d5adbc8fc.cpp:35:35: error: no match for call to ‘(st d::vector<int>) (<brace-enclosed initializer list>)’    35 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -7,17 +7,15 @@ using namespace std;
 
 typedef boost::any list_any;
 
-bool issame(vector<int> a,vector<int>b){
-    if(a.size() != b.size()) return false;
-    sort(a.begin(),a.end());
-    sort(b.begin(),b.end());
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]) return false;
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) return false;
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
-
-vector<int> filter_integers(list_any values); // Semicolon added
 
 vector<int> filter_integers(list_any values) {
     vector<int> result;
@@ -30,6 +28,6 @@ vector<int> filter_integers(list_any values) {
 }
 
 int main() {
-    assert(issame(filter_integers({3, boost::any(3), 3, boost::any('a'), boost::any('b')}), {3, 3, 3}));
+    assert(issame(filter_integers({3, boost::any(3), 3, boost::any(3), 'a', 'b'}), {3, 3, 3}));
     return 0;
 }
