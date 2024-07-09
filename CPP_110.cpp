feabe596f1f170c9be1db3d033603063c80993e6
beep boop <githubbot@vadim.me>
2024-07-10 00:02:35 +0200

    Solution 26 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/2ghk70sf Prompt: f0b72747-6374-4bfa-b7e6-e6e9a3b86509.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string exchange(vector<int> lst1, vector<int> lst2) {       | ^~~~~~ In file included from f0b72747-6374-4bfa-b7e6-e6e9a3b86509.cpp:18: f0b72747-6374-4bfa-b7e6-e6e9a3b86509.cpp: In function ‘int main()’: f0b72747-6374-4bfa-b7e6-e6e9a3b86509.cpp:20:13: error: ‘exchange’ was not declar ed in this scope    20 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
 #include <string>
+#include <vector>
 
 std::string exchange(std::vector<int> lst1, std::vector<int> lst2) {
     for (int num : lst1) {
@@ -9,7 +7,8 @@ std::string exchange(std::vector<int> lst1, std::vector<int> lst2) {
             int found = 0;
             for (int num2 : lst2) {
                 if (num2 % 2 == 0 && num2 != num) {
-                    std::swap(lst1[std::find(lst1.begin(), lst1.end(), num)], num2);
+                    size_t index = std::find(lst1.begin(), lst1.end(), num) - lst1.begin();
+                    swap(lst1[index], num2);
                     found = 1;
                     break;
                 }
