fb09e47089a926ac21a4e655af356f18efb7e3eb
beep boop <githubbot@vadim.me>
2024-07-09 02:08:01 +0200

    Solution 28 of CPP/31, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/6jkxbb2t Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_31.cpp
+++ CPP_31.cpp
@@ -1,10 +1,11 @@
+```cpp
 #include <iostream>
 using namespace std;
 
 bool is_prime(long long n) {
     if (n <= 1) return false;
-    if (n == 2) return true; 
-    if (n % 2 == 0) return false; 
+    if (n == 2) return true; // Handle 2 as a special case
+    if (n % 2 == 0) return false; // Even numbers are not prime
     for (long long i = 3; i * i <= n; i += 2) {
         if (n % i == 0) return false;
     }
@@ -13,12 +14,12 @@ bool is_prime(long long n) {
 
 int main() {
     int num;
-    cout << "Enter a number: ";
+    cout << "Enter prime or composite (1/0): ";
     cin >> num;
-    if (is_prime(num)) {
-        cout << num << " is a prime number.\n";
-    } else {
+    if (num != 0 && !is_prime(num)) {
         cout << num << " is not a prime number.\n";
+    } else if (num == 1) {
+        cout << "Not a valid input.\n";
     }
     return 0;
 }
