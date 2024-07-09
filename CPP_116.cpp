103c50f4f579524aba8e0b0d9a7bf9621dba5267
beep boop <githubbot@vadim.me>
2024-07-09 02:16:19 +0200

    Solution 66 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sfxa53q2 Prompt:                  from /usr/include/c++/11/bits/stl_algobase.h:64,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from a6828488-84a2-4032-b456-600327066f09.cpp:2: /usr/include/c++/11/type_traits: In instantiation of ‘struct std::is_destructibl e<int>’: /usr/include/c++/11/bits/stl_construct.h:188:51:   required from ‘void std::_Des troy(_ForwardIterator, _ForwardIterator) [with _ForwardIterator = int*]’ /usr/include/c++/11/bits/alloc_traits.h:848:15:   required from ‘void std::_Dest roy(_ForwardIterator, _ForwardIterator, std::allocator<_T2>&) [with _ForwardIter ator = int*; _Tp = int]’ /usr/include/c++/11/bits/stl_vector.h:680:15:   required from ‘std::vector<_Tp, _Alloc>::~vector() [with _Tp = int; _Alloc = std::allocator<int>]’ a6828488-84a2-4032-b456-600327066f09.cpp:20:12:   required from here /usr/include/c++/11/type_traits:885:52: error: static assertion failed: template  argument must be a complete class or an unbounded array   885 |       static_assert(std::__is_complete_or_unbounded(__type_identity<_Tp> {}),       |                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~ ~~~ /usr/include/c++/11/type_traits:885:52: note: ‘std::__is_complete_or_unbounded<s td::__type_identity<int> >((std::__type_identity<int>{}, std::__type_identity<in t>()))’ evaluates to false

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,11 +1,14 @@
-#include <initializer_list>
+#include <string>
+#include <vector>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
+    if (a != b)
+        return false;
+    return true;
 }
 
 std::vector<int> sort_array(std::vector<int> arr) {
-    auto comp = [&] (int a, int b) {
+    auto comp = [&] (int a, int b) -> bool {
         if (std::bitset<32>(a).count() != std::bitset<32>(b).count()) {
             return std::bitset<32>(a).count() < std::bitset<32>(b).count();
         } else {
@@ -13,5 +16,5 @@ std::vector<int> sort_array(std::vector<int> arr) {
         }
     };
     std::sort(arr.begin(), arr.end(), comp);
-    return arr;
+    return sort_array(arr);
 }
