430b0231eb00c6662a2b7c54860932f5dda968fa
beep boop <githubbot@vadim.me>
2024-07-09 04:42:49 +0200

    Solution 67 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pe563gn9 Prompt: In file included from /usr/include/c++/11/string:39,                  from 2e57aaa7-4f90-4fca-8383-86203db10c89.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 2e57aaa7-4f90-4fca-8383-86203db10c89.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 2e57aaa7-4f90-4fca-8383-86203db10c89.cpp:56:44: error: ‘b’ was not declared in t his scope    56 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 2e57aaa7-4f90-4fca-8383-86203db10c89.cpp:55: 2e57aaa7-4f90-4fca-8383-86203db10c89.cpp: In function ‘int main()’: 2e57aaa7-4f90-4fca-8383-86203db10c89.cpp:65:20: error: ‘by_length’ was not decla red in this scope    65 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 2e57aaa7-4f90-4fca-8383-86203db10c89.cpp:65:19: error: ‘issame’ cannot be used a s a function    65 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -6,50 +6,52 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
 }
 
 std::vector<std::vector<std::string>> by_length(std::vector<int> arr) {
-    std::vector<std::vector<std::string>> result;
-    
+    std::vector<int> num;
     for (int i : arr) {
         if (i >= 1 && i <= 9) {
-            std::vector<std::string> temp;
-            
-            switch (i) {
-                case 1:
-                    temp.push_back("One");
-                    break;
-                case 2:
-                    temp.push_back("Two");
-                    break;
-                case 3:
-                    temp.push_back("Three");
-                    break;
-                case 4:
-                    temp.push_back("Four");
-                    break;
-                case 5:
-                    temp.push_back("Five");
-                    break;
-                case 6:
-                    temp.push_back("Six");
-                    break;
-                case 7:
-                    temp.push_back("Seven");
-                    break;
-                case 8:
-                    temp.push_back("Eight");
-                    break;
-                case 9:
-                    temp.push_back("Nine");
-                    break;
-            }
-            
-            result.push_back(temp);
+            num.push_back(i);
         }
     }
-    
-    return result;
+
+    sort(num.begin(), num.end());
+    reverse(num.begin(), num.end());
+
+    std::vector<std::vector<std::string>> result;
+    for (int n : num) {
+        switch (n) {
+            case 1:
+                result.push_back({"One"});
+                break;
+            case 2:
+                result.push_back({"Two"});
+                break;
+            case 3:
+                result.push_back({"Three"});
+                break;
+            case 4:
+                result.push_back({"Four"});
+                break;
+            case 5:
+                result.push_back({"Five"});
+                break;
+            case 6:
+                result.push_back({"Six"});
+                break;
+            case 7:
+                result.push_back({"Seven"});
+                break;
+            case 8:
+                result.push_back({"Eight"});
+                break;
+            case 9:
+                result.push_back({"Nine"});
+                break;
+        }
+    }
+
+    return {result};
 }
 
 int main() {
-    assert(issame(by_length({9, 4, 8}) , {{"Nine"}, {"Four", "Eight"}}));
-    // More assertions...
+    assert(issame(by_length({9, 4, 8}) , {{"Nine"}, {"Eight"}, {"Four"}}));
 }
