a235befd4bc267f7e5a4c05745c4b0a7ff3c64fd
beep boop <githubbot@vadim.me>
2024-07-09 14:20:26 +0200

    Solution 75 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/inwrcvol Prompt: a57c76cf-59d0-4719-aad2-ec5f4e3785dd.cpp:20:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    20 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff a57c76cf-59d0-4719-aad2-ec5f4e3785dd.cpp:23:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    23 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff a57c76cf-59d0-4719-aad2-ec5f4e3785dd.cpp:23:36: error: ‘find_zero’ cannot be use d as a function    23 |         solution = find_zero(coeffs);       |                                    ^ In file included from a57c76cf-59d0-4719-aad2-ec5f4e3785dd.cpp:7: a57c76cf-59d0-4719-aad2-ec5f4e3785dd.cpp:24:21: error: ‘poly’ was not declared i n this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ a57c76cf-59d0-4719-aad2-ec5f4e3785dd.cpp:24:17: error: ‘abs’ was not declared in  this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -2,19 +2,19 @@
 #include <cassert>
 #include <cmath>
 
-double poly(double x, const vector<double>& coeffs){
-    double result = 0.0;
-    for(int i=0; i<coeffs.size(); ++i){
-        result += coeffs[i] * pow(x, i);
-    }
-    return result;
-}
+double poly(double x, const std::vector<double>& coeffs);
 
-double find_zero(vector<double> xs);
-
-double find_zero(vector<double> xs){
-    assert(xs.size() == 2);
+double find_zero(const std::vector<double>& xs){
+    assert(xs.size() >= 2);
     double a = xs[0];
     double b = xs[1];
     return -b/a;
+}
+
+double poly(double x, const std::vector<double>& coeffs){
+    double result = 0.0;
+    for(int i = 0; i < coeffs.size(); ++i){
+        result += coeffs[i] * std::pow(x, i);
+    }
+    return result;
 }
