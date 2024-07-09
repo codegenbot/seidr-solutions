22a0de2c2d91a2496879762fd0d1f27918b58f33
beep boop <githubbot@vadim.me>
2024-07-09 11:16:14 +0200

    Solution 22 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/lwt6ig0o Prompt:       |                    ^~~ 79461336-1f38-4bda-bedb-32a0a84fc484.cpp:7:27: error: ‘vector’ was not declared in this scope     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 79461336-1f38-4bda-bedb-32a0a84fc484.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 79461336-1f38-4bda-bedb-32a0a84fc484.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 79461336-1f38-4bda-bedb-32a0a84fc484.cpp:6: 79461336-1f38-4bda-bedb-32a0a84fc484.cpp: In function ‘int main()’: 79461336-1f38-4bda-bedb-32a0a84fc484.cpp:16:20: error: ‘maximum’ was not declare d in this scope    16 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 79461336-1f38-4bda-bedb-32a0a84fc484.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,14 +1,22 @@
 #include <vector>
 #include <algorithm>
 
-int maximum(vector<int>& arr, int k) {
+int maximum(int a, int b) {
+    return a > b ? a : b;
+}
+
+bool issame(int a, int b) {
+    return a == b;
+}
+
+vector<int> contestProblem(vector<int> arr, int k) {
     sort(arr.begin(), arr.end(), greater<int>());
     arr.resize(k);
     return arr;
 }
 
 int main() {
-    vector<int> arr = {3, 1, 4, 1, 5, 9, 2, 6};
-    assert(maximum(arr, 3) == {9, 6, 5});
+    vector<int> arr = {3, 1, 7, 4, 5};
+    assert(maximum(5, 7) == 7);
     return 0;
 }
