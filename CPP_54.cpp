050fe22efc3ce3be51472d9201ee6a198526137b
beep boop <githubbot@vadim.me>
2024-07-09 16:00:12 +0200

    Solution 54 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5uknokbc Prompt: aeb4b8b0-a3dd-4757-bfba-a90de0672dc1.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ aeb4b8b0-a3dd-4757-bfba-a90de0672dc1.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -1,36 +1,4 @@
-#include <iostream>
-using namespace std;
-
-bool same_chars(string s1, string s2) {
-    if (s1.length() != s2.length()) {
-        return false;
-    }
-    for (int i = 0; i < s1.size(); i++) {
-        int count1 = 0, count2 = 0;
-        while (i + count1 <= s1.size() && s1[i] == s1[i + count1]) {
-            count1++;
-        }
-        while (i + count2 <= s2.size() && s2[i] == s2[i + count2]) {
-            count2++;
-        }
-        if (count1 != count2) {
-            return false;
-        }
-    }
-    return true;
-
-}
-
 int main() {
-    string s1, s2;
-    cout << "Enter the first string: ";
-    cin >> s1;
-    cout << "Enter the second string: ";
-    cin >> s2;
-    if (same_chars(s1, s2)) {
-        cout << "True";
-    } else {
-        cout << "False";
-    }
+    cout << (same_chars("aabb", "aaccc") ? "True" : "False");
     return 0;
 }
