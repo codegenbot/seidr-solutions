6e9fd325beff29b282aebc93b32e417bb0e58420
beep boop <githubbot@vadim.me>
2024-07-09 20:53:37 +0200

    Solution 15 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/1921hktp Prompt: 278da96a-74e5-4ded-9b34-2337c7dd2e15.cpp: In function ‘bool issame(std::vector<f loat>, std::vector<float>)’: 278da96a-74e5-4ded-9b34-2337c7dd2e15.cpp:20:13: error: ‘abs’ was not declared in  this scope    20 |         if (abs(a[i]-b[i])>1e-4) return false;       |             ^~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -6,7 +6,7 @@ using namespace std;
 vector<float> get_positive(vector<float> l) {
     vector<float> result;
     for (float x : l) {
-        if (x > 0 || x == 0) {
+        if (abs(x) > 0) {
             result.push_back(x);
         }
     }
