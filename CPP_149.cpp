71c28a692f0dcff7661e672a7e7cc1a9b89ed1a6
beep boop <githubbot@vadim.me>
2024-07-09 04:47:17 +0200

    Solution 15 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0ywi65gh Prompt:    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bacd5263-ee07-41f7-a6eb-75d6dff93de9.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bacd5263-ee07-41f7-a6eb-75d6dff93de9.cpp:21:45: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from bacd5263-ee07-41f7-a6eb-75d6dff93de9.cpp:20: bacd5263-ee07-41f7-a6eb-75d6dff93de9.cpp: In function ‘int main()’: bacd5263-ee07-41f7-a6eb-75d6dff93de9.cpp:30:20: error: ‘sorted_list_sum’ was not  declared in this scope    30 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ bacd5263-ee07-41f7-a6eb-75d6dff93de9.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -3,18 +3,24 @@
 #include <vector>
 
 bool issame(const std::string& a, const std::string& b) {
-    if (a.length() % 2 && b.length() % 2)
-        return true;
-    else
+    if (a.length() != b.length()) {
         return false;
+    }
+    for (int i = 0; i < a.length(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 std::vector<std::string> sorted_list_sum(std::vector<std::string> lst) {
-    auto it = unique(lst.begin(), lst.end(), issame);
+    auto it = unique(lst.begin(), lst.end(), 
+                      [](const std::string& a, const std::string& b) { return !issame(a, b); });
     lst.erase(it, lst.end());
     
     sort(lst.begin(), lst.end(),
-         [](const std::string& a, const std::string& b) {
+         [](const string& a, const string& b) {
              if (a.length() != b.length()) {
                  return a.length() < b.length();
              } else {
