9e8ebc3014ec4949cc9e6b61a7e736865de5f5c1
beep boop <githubbot@vadim.me>
2024-07-09 18:05:50 +0200

    Solution 6 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/bgddgl49 Prompt:       |                              ^~~~~~ bb8e32d8-2537-4f10-9145-deaa2aa1c36d.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bb8e32d8-2537-4f10-9145-deaa2aa1c36d.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bb8e32d8-2537-4f10-9145-deaa2aa1c36d.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from bb8e32d8-2537-4f10-9145-deaa2aa1c36d.cpp:9: bb8e32d8-2537-4f10-9145-deaa2aa1c36d.cpp: In function ‘int main()’: bb8e32d8-2537-4f10-9145-deaa2aa1c36d.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ bb8e32d8-2537-4f10-9145-deaa2aa1c36d.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,15 +1,39 @@
 ```cpp
-#include <vector>
 #include <string>
+#include <vector>
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
     }
     return true;
 }
 
 int main() {
-    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    vector<string> prefixes = all_prefixes("WWW");
+    vector<vector<string>> inputs = {{"www", "http"}, {"abc", "ab"}};
+    for (auto& input : inputs) {
+        vector<string> a = all_prefixes(input[0]);
+        vector<string> b = all_prefixes(input[1]);
+        if (issame(a, b)) {
+            cout << "True";
+        } else {
+            cout << "False";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+vector<string> all_prefixes(string str) {
+    vector<string> prefixes;
+    for (int i = 1; i <= str.length(); i++) {
+        prefixes.push_back(str.substr(0, i));
+    }
+    return prefixes;
 }
