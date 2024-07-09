1132bd9c54dd7583c630da7b3af2fa3d959a368b
beep boop <githubbot@vadim.me>
2024-07-09 05:16:54 +0200

    Solution 52 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qnmgtenv Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5c8da0d3-3c3b-47eb-9143-b9bdeb38df66.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5c8da0d3-3c3b-47eb-9143-b9bdeb38df66.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5c8da0d3-3c3b-47eb-9143-b9bdeb38df66.cpp:11: 5c8da0d3-3c3b-47eb-9143-b9bdeb38df66.cpp: In function ‘int main()’: 5c8da0d3-3c3b-47eb-9143-b9bdeb38df66.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 5c8da0d3-3c3b-47eb-9143-b9bdeb38df66.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -7,14 +7,24 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 }
 
 bool issame(const boost::any& a, const boost::any& b) {
-    return a.type() == b.type();
+    return false;
 }
 
 std::vector<int> filter_integers(std::list<boost::any> values) {
     std::vector<int> result;
     for (const auto& value : values) {
         if (boost::any_cast<int>(value).good()) {
-            result.push_back(boost::any_cast<int>(value));
+            int i = boost::any_cast<int>(value);
+            bool same = true;
+            for (const auto& other : values) {
+                if (!issame(other, boost::any_cast<boost::any>(i))) {
+                    same = false;
+                    break;
+                }
+            }
+            if (same) {
+                result.push_back(i);
+            }
         }
     }
     return result;
