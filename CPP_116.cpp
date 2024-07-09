ad4d5c705c6695b09b63605414beae2b7c37bf4f
beep boop <githubbot@vadim.me>
2024-07-09 19:42:28 +0200

    Solution 26 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lvdfcyfj Prompt:       |           ^~~~~~ In file included from c19b89d2-b4ab-49d0-b5c4-7f96664a526b.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c19b89d2-b4ab-49d0-b5c4-7f96664a526b.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c19b89d2-b4ab-49d0-b5c4-7f96664a526b.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ c19b89d2-b4ab-49d0-b5c4-7f96664a526b.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from c19b89d2-b4ab-49d0-b5c4-7f96664a526b.cpp:23: c19b89d2-b4ab-49d0-b5c4-7f96664a526b.cpp: In function ‘int main()’: c19b89d2-b4ab-49d0-b5c4-7f96664a526b.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -9,12 +9,12 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return true;
 }
 
-std::vector<int> sort_array(std::vector<int> arr) {
+std::vector<int> sort_array(std::vector<int>& arr) {
     std::sort(arr.begin(), arr.end());
     return arr;
 }
 
 int main() {
-    assert(issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));
+    assert(issame(sort_array({2,4,8,16,32}), {2, 4, 8, 16, 32}));
     return 0;
 }
