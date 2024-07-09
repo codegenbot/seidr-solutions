03aeeffc684171488408922e8f0cc251b975155a
beep boop <githubbot@vadim.me>
2024-07-09 09:52:47 +0200

    Solution 90 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/mir6wa1q Prompt: 6b2ef926-2525-4f1c-9a27-d172f8306375.cpp:1:14: error: ‘vector’ was not declared in this scope     1 | int solution(vector<int> lst) {       |              ^~~~~~ 6b2ef926-2525-4f1c-9a27-d172f8306375.cpp:1:21: error: expected primary-expressio n before ‘int’     1 | int solution(vector<int> lst) {       |                     ^~~ In file included from 6b2ef926-2525-4f1c-9a27-d172f8306375.cpp:11: 6b2ef926-2525-4f1c-9a27-d172f8306375.cpp: In function ‘int main()’: 6b2ef926-2525-4f1c-9a27-d172f8306375.cpp:13:13: error: ‘solutions’ was not decla red in this scope; did you mean ‘solution’?    13 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,29 +1,4 @@
-```cpp
-#include <iostream>
 #include <vector>
-
-int main() {
-    std::vector<int> lst;
-    int n;
-    std::cout << "Enter the number of elements: ";
-    std::cin >> n;
-    for (int i = 0; i < n; i++) {
-        int num;
-        std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> num;
-        lst.push_back(num);
-    }
-    
-    int sum = 0;
-    for (int i = 1; i < lst.size(); i++) {
-        if (i % 2 == 0 && lst[i] % 2 != 0) {
-            sum += lst[i];
-        }
-    }
-    std::cout << "Sum of elements: " << sum << std::endl;
-    
-    return 0;
-}
 
 int solution(vector<int> lst) {
     int sum = 0;
@@ -33,4 +8,9 @@ int solution(vector<int> lst) {
         }
     }
     return sum;
+}
+
+int main() {
+    vector<int> numbers = {4, 6, 10, 20};
+    cout << solution(numbers);
 }
