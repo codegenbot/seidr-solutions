e7d4c0fe890dcd08a7842cabbb91205db8cdddc5
beep boop <githubbot@vadim.me>
2024-07-09 16:56:17 +0200

    Solution 75 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/29z5qmt1 Prompt:       |                                  ^~~ 05de0930-397c-4cb7-bcf9-09ba8f3c327f.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 05de0930-397c-4cb7-bcf9-09ba8f3c327f.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 05de0930-397c-4cb7-bcf9-09ba8f3c327f.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from 05de0930-397c-4cb7-bcf9-09ba8f3c327f.cpp:18: 05de0930-397c-4cb7-bcf9-09ba8f3c327f.cpp: In function ‘int main()’: 05de0930-397c-4cb7-bcf9-09ba8f3c327f.cpp:28:20: error: ‘pluck’ was not declared in this scope    28 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 05de0930-397c-4cb7-bcf9-09ba8f3c327f.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,47 +1,24 @@
-int pluck(int num) {
-    if (num % 2 == 0 && num < INT_MAX) {
-        return num;
-    } else {
-        return -1;
-    }
-}
-
-bool areEqual(const vector<int>& a, const vector<int>& b) {
+bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) return false;
-    
     for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) return false;
     }
-    
-    return true;
-}
-
-bool issame(const vector<int>& a, const vector<vector<int>>& b) {
-    if (a.size() != b.size()) return false;
-    
-    for (int i = 0; i < a.size(); i++) {
-        bool found = false;
-        for (const auto& vec : b) {
-            if (areEqual(a[i], vec)) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) return false;
-    }
-    
     return true;
 }
 
 int main() {
     vector<int> arr = {7, 9, 7, 1};
-    int num = pluck(7);
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
     vector<int> vec;
-    vec.push_back(num);
-    if(num != -1) {
-        assert(issame(vector<int>{num}, arr));
-    } else {
+    if (num % 2 == 0 && num < INT_MAX) {
+        vec.push_back(num);
+    }
+    if (num != -1) {
         assert(issame(vec, arr));
+    } else {
+        assert(issame({}, arr));
     }
     return 0;
 }
