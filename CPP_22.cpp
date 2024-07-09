4618549b13a3ea6fadca05b919b9e420caef2d54
beep boop <githubbot@vadim.me>
2024-07-09 07:12:22 +0200

    Solution 16 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/y7a4nkme Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0a1b0024-4d90-4a76-9178-633088648fb6.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0a1b0024-4d90-4a76-9178-633088648fb6.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0a1b0024-4d90-4a76-9178-633088648fb6.cpp:11: 0a1b0024-4d90-4a76-9178-633088648fb6.cpp: In function ‘int main()’: 0a1b0024-4d90-4a76-9178-633088648fb6.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 0a1b0024-4d90-4a76-9178-633088648fb6.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,15 +1,23 @@
+#include <vector>
+#include <list>
+#include <boost/any.hpp>
+
 bool issame(vector<int> a, vector<int> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-
-    return true;
+    return a == b;
 }
 
-vector<int> filter_integers(list_any values);
+vector<int> filter_integers(list<boost::any> values){
+    vector<int> result;
+    for (auto val : values) {
+        if (val.type() == typeid(int)) {
+            result.push_back(boost::any_cast<int>(val));
+        }
+    }
+    return result;
+}
+
+int main() {
+    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
+    
+    return 0;
+}
