e4729357ed277b80eb952c51ba0ba8a30321eea3
beep boop <githubbot@vadim.me>
2024-07-09 06:44:01 +0200

    Solution 36 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/4gns7r8c Prompt:       |           ^~~~~~ In file included from 90aa8eb6-7c8c-4993-9de1-a2de1851af81.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 90aa8eb6-7c8c-4993-9de1-a2de1851af81.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 90aa8eb6-7c8c-4993-9de1-a2de1851af81.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 90aa8eb6-7c8c-4993-9de1-a2de1851af81.cpp:14: 90aa8eb6-7c8c-4993-9de1-a2de1851af81.cpp: In function ‘int main()’: 90aa8eb6-7c8c-4993-9de1-a2de1851af81.cpp:24:20: error: ‘sum_product’ was not dec lared in this scope    24 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ 90aa8eb6-7c8c-4993-9de1-a2de1851af81.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,22 +1,30 @@
-
+#include <iostream>
 #include <vector>
+#include <utility>
 #include <cassert>
 
-std::pair<int, int> sum_product(const std::vector<int>& numbers) {
-    int sum = 0;
-    int product = 1;
-    for (int num : numbers) {
-        sum += num;
-        product *= num;
-    }
-    return {sum, product};
-}
+using namespace std;
 
-bool issame(const std::pair<int, int>& a, const std::pair<int, int>& b) {
+bool issame(const vector<int>& a, const vector<int>& b) {
     return a == b;
 }
 
+pair<int, int> sum_product(const vector<int>& numbers);
+
 int main() {
     assert(issame(sum_product({10}), {10, 10}));
+    
+    cout << "Test passed!";
+    
     return 0;
+}
+
+pair<int, int> sum_product(const vector<int>& numbers) {
+    int sum = 0;
+    int prod = 1;
+    for (int num : numbers) {
+        sum += num;
+        prod *= num;
+    }
+    return make_pair(sum, prod);
 }
