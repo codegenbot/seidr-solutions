2a18c06919adc4ee7461bd818fa7c58be94b7cac
beep boop <githubbot@vadim.me>
2024-07-09 16:52:42 +0200

    Solution 9 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/82gl89f1 Prompt: _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 580d656b-ab05-41b6-98e0-e768576908ff.cpp:19:20:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 580d656b-ab05-41b6-98e0-e768576908ff.cpp:19:20:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,6 +1,6 @@
-#include <cassert>
 #include <string>
 #include <cctype>
+#include <cassert>
 
 using namespace std;
 
@@ -17,7 +17,7 @@ string get_closest_vowel(string word) {
         if (word[i] == 'Y' && i > 1 && isupper(word[i-1]) && islower(word[i-2]))
             return "Y";
         if (ispunct(word[i]) || isdigit(word[i])) break;
-        if (isvowel(word[i])) return word.substr(i);
+        if (isvowel(word[i])) return string(&word[i]);
     }
     return "";
 }
