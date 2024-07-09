dda0b9834282f7e607761b7e6da1e667ddf10da5
beep boop <githubbot@vadim.me>
2024-07-09 02:26:21 +0200

    Solution 9 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fkq142tr Prompt:       |                    ^~~ 151af435-a744-430b-9a82-f24afbb853a1.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 151af435-a744-430b-9a82-f24afbb853a1.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 151af435-a744-430b-9a82-f24afbb853a1.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 151af435-a744-430b-9a82-f24afbb853a1.cpp:16: 151af435-a744-430b-9a82-f24afbb853a1.cpp: In function ‘int main()’: 151af435-a744-430b-9a82-f24afbb853a1.cpp:26:20: error: ‘even_odd_count’ was not declared in this scope    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 151af435-a744-430b-9a82-f24afbb853a1.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,23 +1,9 @@
 #include <vector>
-
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) 
+    if(a.size() != b.size())
         return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) 
+    for(int i = 0; i < a.size(); i++)
+        if(a[i] != b[i])
             return false;
-    }
-    return true;
-}
-
-bool even_odd_count(int num, vector<int>& result) {
-    while (num != 0) {
-        int digit = num % 10;
-        if (digit % 2 == 0)
-            result[0]++;
-        else
-            result[1]++;
-        num /= 10;
-    }
     return true;
 }
