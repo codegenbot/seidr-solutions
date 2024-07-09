5869e313bdb83184bf308d08afdc7a329dd86381
beep boop <githubbot@vadim.me>
2024-07-09 16:07:15 +0200

    Solution 65 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/40ydl3g4 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,27 +1,74 @@
-#include <string>
-#include <algorithm>
+Here is the completed code:
 
-using namespace boost;
-
+```
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_any_of<a>(int.class)) {
-        int x = any_cast<int>(a);
-        int y = any_cast<int>(b);
-        return (x > y) ? a : ((y > x) ? b : boost::any("None"));
-    }
-    else if (is_any_of<a>(double.class)) {
-        double x = any_cast<double>(a);
-        double y = any_cast<double>(b);
-        return (x > y) ? a : ((y > x) ? b : boost::any("None"));
-    }
-    else if (is_any_of<a>(string.class)) {
-        string x = any_cast<string>(a);
-        string y = any_cast<string>(b);
-        double dx = stod(x), dy = stod(y);
-        return (dx > dy) ? a : ((dy > dx) ? b : boost::any("None"));
-    }
-    else {
-        // This should never happen
-        throw runtime_error("Invalid type");
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        if ((int)a.convert_to<int>() > (float)b.convert_to<float>()) {
+            return b;
+        } else if ((int)a.convert_to<int>() < (float)b.convert_to<float>()) {
+            return b;
+        } else {
+            return "None";
+        }
+    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        if (((string)b).find('.') != string::npos || ((string)b).find(',') != string::npos) {
+            if ((int)a.convert_to<int>() > stod((string)b)) {
+                return b;
+            } else if ((int)a.convert_to<int>() < stod((string)b)) {
+                return b;
+            } else {
+                return "None";
+            }
+        } else {
+            if ((int)a.convert_to<int>() > stoi((string)b)) {
+                return b;
+            } else if ((int)a.convert_to<int>() < stoi((string)b)) {
+                return b;
+            } else {
+                return "None";
+            }
+        }
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        if ((float)a > (int)b) {
+            return a;
+        } else if ((float)a < (int)b) {
+            return a;
+        } else {
+            return "None";
+        }
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        if (((string)a).find('.') != string::npos || ((string)a).find(',') != string::npos) {
+            if (stod((string)a) > (float)b) {
+                return a;
+            } else if (stod((string)a) < (float)b) {
+                return a;
+            } else {
+                return "None";
+            }
+        } else {
+            if (stoi((string)a) > (int)b) {
+                return a;
+            } else if (stoi((string)a) < (int)b) {
+                return a;
+            } else {
+                return "None";
+            }
+        }
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if (((string)a).find('.') != string::npos || ((string)a).find(',') != string::npos &&
+            (((string)b).find('.') != string::npos || ((string)b).find(',') != string::npos)) {
+            return (stod((string)a) > stod((string)b)) ? a : b;
+        } else if (!(((string)a).find('.') == string::npos || ((string)a).find(',') == string::npos)) &&
+                  !((((string)b).find('.') != string::npos || ((string)b).find(',') != string::npos))) {
+            return (stoi((string)a) > (int)b) ? a : b;
+        } else {
+            if (stod((string)a) > (float)b) {
+                return a;
+            } else if (stod((string)a) < (float)b) {
+                return a;
+            } else {
+                return "None";
+            }
+        }
     }
 }
