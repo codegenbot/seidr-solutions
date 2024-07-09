993e11ac024ea4ef3bb1f425bdfce40797bbbc09
beep boop <githubbot@vadim.me>
2024-07-09 02:00:26 +0200

    Solution 95 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/xy0sw73o Prompt: Here is the corrected code: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eebab0c9-ea91-4118-bcb8-b55861c0f 814.py", line 6, in <module>     check(mean_absolute_deviation)           ^^^^^^^^^^^^^^^^^^^^^^^ NameError: name 'mean_absolute_deviation' is not defined

--- Python_4.py
+++ Python_4.py
@@ -1,8 +1,7 @@
-```Python
 from statistics import mean, stdev
 
 def mean_absolute_deviation(data):
     return sum(abs(x - mean(data)) for x in data) / len(data)
 
 data = list(map(float, input().split()))
-print(mean_absolute_deviation(data))
+check(mean_absolute_deviation(data))
