4dfc4992f2d4afc1fa50c83fc592fa8f4333d7bc
beep boop <githubbot@vadim.me>
2024-07-09 16:10:55 +0200

    Solution 11 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/8rtr04vk Prompt: 0497f2ab-5c91-4b8f-ac61-6f8ce74bc9ef.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 0497f2ab-5c91-4b8f-ac61-6f8ce74bc9ef.cpp:6:5: note: ‘int main()’ previously defi ned here     6 | int main() {       |     ^~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,31 +1,16 @@
-#include <string>
+```cpp
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
     int count = 0;
     string boredom = "I";
+    string S;
     cout << "Enter a sentence: ";
-    getline(cin, boredom);
+    cin >> S;
 
-    size_t pos = 0;
-    while ((pos = boredom.find(" ", pos)) != string::npos) {
-        if (boredom.substr(0, pos).compare(boredom) == 0) {
-            count++;
-            break;
-        }
-        pos++;
-    }
-
-    cout << "Count: " << count << std::endl;
-
-    return is_bored(boredom);
-}
-
-int is_bored(string S){
-    int count = 0;
-    string boredom = "I";
     size_t pos = 0;
     while ((pos = S.find(" ", pos)) != string::npos) {
         if (S.substr(0, pos).compare(boredom) == 0) {
@@ -34,5 +19,8 @@ int is_bored(string S){
         }
         pos++;
     }
-    return count;
+
+    cout << "Count: " << count << endl;
+
+    return 0;
 }
