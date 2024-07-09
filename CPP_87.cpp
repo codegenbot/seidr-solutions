0d5ee9741146b3b9e05efe1f33a1024b0e8c6322
beep boop <githubbot@vadim.me>
2024-07-09 17:19:46 +0200

    Solution 16 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/rl4jiyfu Prompt:       |                                   ^~~~~~ 450dd6e1-6d7f-45da-b48d-0fbe5c07344b.cpp:18:42: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 450dd6e1-6d7f-45da-b48d-0fbe5c07344b.cpp:18:49: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 450dd6e1-6d7f-45da-b48d-0fbe5c07344b.cpp:18:56: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 450dd6e1-6d7f-45da-b48d-0fbe5c07344b.cpp:17: 450dd6e1-6d7f-45da-b48d-0fbe5c07344b.cpp: In function ‘int main()’: 450dd6e1-6d7f-45da-b48d-0fbe5c07344b.cpp:30:20: error: ‘get_row’ was not declare d in this scope    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 450dd6e1-6d7f-45da-b48d-0fbe5c07344b.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,18 +1,23 @@
-vector<vector<int>> coordinates;
+#include <vector>
+
+static bool issame(int a, int b) {
+    return a == b;
+}
+
+vector<vector<int>> get_row(vector<vector<int>> lst, int x);
+
+vector<vector<int>> get_row(vector<vector<int>> lst, int x){
+    vector<vector<int>> result;
     for (int i = 0; i < lst.size(); ++i) {
         for (int j = 0; j < lst[i].size(); ++j) {
-            if (lst[i][j] == x) {
-                coordinates.push_back({i, j});
+            if (issame(lst[i][j], x)) {
+                result.push_back({i, j});
             }
         }
     }
-
-    sort(coordinates.begin(), coordinates.end(), [](vector<int>& a, vector<int>& b) {
-        if (a[0] == b[0]) {
-            return a[1] > b[1];
-        }
-        return a[0] < b[0];
-    });
-
-    return coordinates;
+    sort(result.begin(), result.end());
+    for (auto &coord : result) {
+        sort(coord.begin(), coord.end(), greater<int>());
+    }
+    return result;
 }
