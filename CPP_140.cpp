559d423fb9e822fa4a28e0af780f7298f08d495a
beep boop <githubbot@vadim.me>
2024-07-09 13:31:08 +0200

    Solution 10 of CPP/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/a9482bea Prompt: c1b7906a-4fe4-4a77-8955-1989e74a1715.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string fix_spaces(string text){       | ^~~~~~ In file included from c1b7906a-4fe4-4a77-8955-1989e74a1715.cpp:27: c1b7906a-4fe4-4a77-8955-1989e74a1715.cpp: In function ‘int main()’: c1b7906a-4fe4-4a77-8955-1989e74a1715.cpp:29:13: error: ‘fix_spaces’ was not decl ared in this scope    29 |     assert (fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple");       |             ^~~~~~~~~~

--- CPP_140.cpp
+++ CPP_140.cpp
@@ -1,16 +1,26 @@
+using namespace std;
+
 string fix_spaces(string text){
     string result = "";
-    bool was_space = false;
-
-    for(char c : text){
-        if(c == ' ' && !was_space){
+    for(int i = 0; i < text.length(); i++){
+        if(text[i] == ' ' && (i == 0 || text[i-1] != ' ') && (i == text.length() - 1 || text[i+1] != ' ')){
             result += '_';
-            was_space = true;
-        } else if(c != ' ' || was_space){
-            result += c;
-            was_space = (c == ' ');
+        } else if(text[i] == ' '){
+            bool is_consecutive = true;
+            for(int j = i + 1; j < text.length(); j++){
+                if(text[j] != ' ') {
+                    is_consecutive = false;
+                    break;
+                }
+            }
+            if(is_consecutive) {
+                result += '-';
+                i = j - 1;
+            } else {
+                result += '_';
+            }
+        } else {
+            result += text[i];
         }
     }
-
     return result;
-}
