09eaab17ab40dd308271da51d20c7ae46657a71b
beep boop <githubbot@vadim.me>
2024-07-09 05:02:40 +0200

    Solution 77 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ajzrdwh9 Prompt:       |             ^~~~~~ a86aa3c1-e653-46d1-a4f8-5222f96d37a0.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a86aa3c1-e653-46d1-a4f8-5222f96d37a0.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a86aa3c1-e653-46d1-a4f8-5222f96d37a0.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ a86aa3c1-e653-46d1-a4f8-5222f96d37a0.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from a86aa3c1-e653-46d1-a4f8-5222f96d37a0.cpp:23: a86aa3c1-e653-46d1-a4f8-5222f96d37a0.cpp: In function ‘int main()’: a86aa3c1-e653-46d1-a4f8-5222f96d37a0.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,7 +1,16 @@
-```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
+std::vector<int> maximum(std::vector<int> arr, int k) {
+    std::vector<int> result;
+    for(int i = 0; i < k; i++) {
+        auto it = *std::max_element(arr.begin(), arr.end());
+        result.push_back(it);
+        arr.erase(std::remove(arr.begin(), arr.end(), it), arr.end());
+    }
+    return result;
+}
 
 int main() {
     std::vector<int> test = maximum({1, 2, 3, -23, 243, -400, 0}, 3);
@@ -9,15 +18,4 @@ int main() {
         std::cout << i << " ";
     }
     return 0;
-}
-
-std::vector<int> maximum(std::vector<int> arr, int k) {
-    std::vector<int> result;
-    while(arr.size() > 0 && k > 0) {
-        auto it = *std::max_element(arr.begin(), arr.end());
-        result.push_back(it);
-        arr.erase(std::remove(arr.begin(), arr.end(), it), arr.end());
-        k--;
-    }
-    return result;
 }
