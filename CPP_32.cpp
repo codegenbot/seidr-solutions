7d771395709d2ad38b956aa6ba67aa0e98650ebb
beep boop <githubbot@vadim.me>
2024-07-09 13:53:00 +0200

    Solution 14 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/inwrcvol Prompt: Complete the following code given the task description and function signature.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,6 +1,5 @@
 double find_zero(vector<double> xs){
-    double a = xs[0];
-    double b = xs[1];
-
+    double a = xs[0]; // Largest non-zero coefficient
+    double b = xs[1]; // Second coefficient
     return -b/a;
 }
