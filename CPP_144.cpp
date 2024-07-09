6ca8b2d22b90f406916603b815a66ed6de1fdc81
beep boop <githubbot@vadim.me>
2024-07-09 19:54:01 +0200

    Solution 8 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/95dg917u Prompt: 3dd2ab02-5138-4eca-83e2-94ac46a57d4a.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 3dd2ab02-5138-4eca-83e2-94ac46a57d4a.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -7,12 +7,10 @@ using namespace std;
 bool simplify(string x, string n) {
     int a = 0, b = 0, c = 0, d = 0;
     
-    // Split numerator and denominator of x into two integers.
     size_t pos = x.find('/');
     a = stoi(x.substr(0, pos));
     b = stoi(x.substr(pos + 1));
 
-    // Split numerator and denominator of n into two integers.
     pos = n.find('/');
     c = stoi(n.substr(0, pos));
     d = stoi(n.substr(pos + 1));
@@ -21,10 +19,9 @@ bool simplify(string x, string n) {
 }
 
 int main() {
-    cout << simplify("1/5", "5/1") << endl; // true
-    cout << simplify("1/6", "2/1") << endl; // false
-    cout << simplify("7/10", "10/2") << endl; // false
+    cout << simplify("1/5", "5/1") << endl; 
+    cout << simplify("1/6", "2/1") << endl; 
+    cout << simplify("7/10", "10/2") << endl; 
 
     return 0;
 }
-// int main() {
