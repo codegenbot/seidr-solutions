cea7c0b1cec8f575b24ad87249a80213422ac973
beep boop <githubbot@vadim.me>
2024-07-09 22:58:56 +0200

    Solution 41 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/zt4qvmez Prompt: 8030f757-9c14-424f-99e1-7d665b1466d2.cpp: In function ‘std::string string_to_md5 (std::string)’: 8030f757-9c14-424f-99e1-7d665b1466d2.cpp:12:16: error: aggregate ‘EVP_MD_CTX md’  has incomplete type and cannot be defined    12 |     EVP_MD_CTX md;       |                ^~ 8030f757-9c14-424f-99e1-7d665b1466d2.cpp:14:26: error: ‘MD5_DIGEST_LENGTH’ was n ot declared in this scope    14 |     unsigned char result[MD5_DIGEST_LENGTH];       |                          ^~~~~~~~~~~~~~~~~ 8030f757-9c14-424f-99e1-7d665b1466d2.cpp:17:30: error: ‘result’ was not declared  in this scope    17 |     EVP_DigestFinal_CTX(&md, result, NULL);       |                              ^~~~~~ 8030f757-9c14-424f-99e1-7d665b1466d2.cpp:17:5: error: ‘EVP_DigestFinal_CTX’ was not declared in this scope; did you mean ‘EVP_DigestFinal_ex’?    17 |     EVP_DigestFinal_CTX(&md, result, NULL);       |     ^~~~~~~~~~~~~~~~~~~       |     EVP_DigestFinal_ex

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,2 +1,22 @@
 #include <openssl/ssl.h>
 #include <openssl/md5.h>
+
+using namespace std;
+
+string string_to_md5(string text) {
+    if (text.empty()) return "None";
+
+    EVP_MD_CTX md;
+    EVP_MD_CTX_init(&md);
+    unsigned char result[MD5_DIGEST_LENGTH];
+
+    EVP_DigestUpdate(&md, (unsigned char*)text.c_str(), text.size());
+    EVP_DigestFinal_CTX(&md, result, NULL);
+
+    stringstream ss;
+    for(int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        ss << setfill('0') << setw(2) << hex << (int)result[i];
+    }
+
+    return ss.str();
+}
