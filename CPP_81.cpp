caa6f86683543cf8e3a8975a9a1204a620baca8f
beep boop <githubbot@vadim.me>
2024-07-09 18:46:35 +0200

    Solution 28 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d9zek7iz Prompt:       |                                 ^~~~~~ In file included from f85f417a-5407-4b70-94a1-11fee7fd6e53.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ f85f417a-5407-4b70-94a1-11fee7fd6e53.cpp:48:44: error: ‘b’ was not declared in t his scope    48 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f85f417a-5407-4b70-94a1-11fee7fd6e53.cpp:56:10: error: ‘int main()’ redeclared a s different kind of entity    56 | int main(){       |          ^ f85f417a-5407-4b70-94a1-11fee7fd6e53.cpp:45:5: note: previous declaration ‘int m ain’    45 | int main { }       |     ^~~~ In file included from f85f417a-5407-4b70-94a1-11fee7fd6e53.cpp:47: f85f417a-5407-4b70-94a1-11fee7fd6e53.cpp: In function ‘int main()’: f85f417a-5407-4b70-94a1-11fee7fd6e53.cpp:57:5: error: ‘__assert_fail’ was not de clared in this scope    57 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |     ^~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 
-bool same(std::vector<std::string> a, std::vector<std::string> b) {
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i < a.size(); i++)
@@ -43,6 +43,5 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
 }
 
 int main() {
-    assert(same(numerical_letter_grade({0, 0.7}), {"E", "D-"}));
-    return 0;
+    assert(issame(numerical_letter_grade({0, 0.7}), {"E", "D-"}));
 }
