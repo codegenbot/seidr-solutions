23ca75c543380e4df95303b1310fa209facfa1ab
beep boop <githubbot@vadim.me>
2024-07-09 02:35:53 +0200

    Solution 2 of CPP/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/qphitzrg Prompt:       |              std e7b5f1ef-4dfe-48f1-993b-fe5144f3d9d7.cpp:2:23: error: ‘round’ was not declared i n this scope     2 |     int intNum = (int)round(num);       |                       ^~~~~ e7b5f1ef-4dfe-48f1-993b-fe5144f3d9d7.cpp:3:5: error: expected unqualified-id bef ore ‘if’     3 |     if (num - intNum < 0) {       |     ^~ e7b5f1ef-4dfe-48f1-993b-fe5144f3d9d7.cpp:5:7: error: expected unqualified-id bef ore ‘else’     5 |     } else {       |       ^~~~ e7b5f1ef-4dfe-48f1-993b-fe5144f3d9d7.cpp:8:1: error: expected declaration before  ‘}’ token     8 | }       | ^ In file included from e7b5f1ef-4dfe-48f1-993b-fe5144f3d9d7.cpp:10: e7b5f1ef-4dfe-48f1-993b-fe5144f3d9d7.cpp: In function ‘int main()’: e7b5f1ef-4dfe-48f1-993b-fe5144f3d9d7.cpp:12:13: error: ‘closest_integer’ was not  declared in this scope    12 |     assert (closest_integer("0") == 0);       |             ^~~~~~~~~~~~~~~

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,9 +1,11 @@
-double num = stod(value);
-    int closestInt = round(num);
-    if (num - closestInt == 0.5) {
-        closestInt = ceil(num);
-    } else if (num - closestInt == -0.5) {
-        closestInt = floor(num);
+#include <cmath>
+
+int closest_integer(std::string value) {
+    double num = std::stod(value);
+    int intNum = (int)std::round(num);
+    if (num - intNum < 0) {
+        return (int)std::ceil(num);
+    } else {
+        return (int)std::floor(num);
     }
-    return closestInt;
 }
