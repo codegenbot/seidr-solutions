d0be3dd4068f9297836978fc3d663387258b9fd4
beep boop <githubbot@vadim.me>
2024-07-09 05:33:31 +0200

    Solution 90 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kl0f1ktf Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -1,4 +1,4 @@
 def add(a=0, b=0): 
-    x = int(input(f"Enter the first number: {a}" if not a else str(a))) 
-    y = int(input(f"Enter the second number: {b}" if not b else str(b))) 
+    x = int(input(f"Enter the first number: {a} " + str(a) if not a else '')) 
+    y = int(input(f"Enter the second number: {b} " + str(b) if not b else '')) 
     print(x + y)
