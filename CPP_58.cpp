270583d9675b8a4e556afb429664131f9cd9a261
beep boop <githubbot@vadim.me>
2024-07-09 15:54:46 +0200

    Solution 59 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/hn4yt70p Prompt:       |                    ^~~ 8a0e266e-afac-4cd3-ab9c-f45936ffef80.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8a0e266e-afac-4cd3-ab9c-f45936ffef80.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8a0e266e-afac-4cd3-ab9c-f45936ffef80.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8a0e266e-afac-4cd3-ab9c-f45936ffef80.cpp:13: 8a0e266e-afac-4cd3-ab9c-f45936ffef80.cpp: In function ‘int main()’: 8a0e266e-afac-4cd3-ab9c-f45936ffef80.cpp:23:20: error: ‘common’ was not declared  in this scope    23 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 8a0e266e-afac-4cd3-ab9c-f45936ffef80.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,15 +1,18 @@
 #include <vector>
 #include <algorithm>
 
-vector<int> b(vector<int> l1, vector<int> l2) {
-    sort(l1.begin(), l1.end());
-    sort(l2.begin(), l2.end());
+vector<int> b(const vector<int>& l1, const vector<int>& l2) {
+    vector<int> l1_copy = l1;
+    vector<int> l2_copy = l2;
     
+    sort(l1_copy.begin(), l1_copy.end());
+    sort(l2_copy.begin(), l2_copy.end());
+
     vector<int> result;
-    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
-    
+    set_intersection(l1_copy.begin(), l1_copy.end(), l2_copy.begin(), l2_copy.end(), back_inserter(result));
+
     auto last = unique(result.begin(), result.end());
     result.erase(last, result.end());
-    
+
     return result;
 }
