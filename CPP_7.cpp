a0ef93aa83519c50511efb4267335627e4b91a59
beep boop <githubbot@vadim.me>
2024-07-09 11:14:04 +0200

    Solution 57 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/esw41ccx Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 682655b3-4ef6-4341-bdaf-0822af96f8ca.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 682655b3-4ef6-4341-bdaf-0822af96f8ca.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 682655b3-4ef6-4341-bdaf-0822af96f8ca.cpp:11: 682655b3-4ef6-4341-bdaf-0822af96f8ca.cpp: In function ‘int main()’: 682655b3-4ef6-4341-bdaf-0822af96f8ca.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 682655b3-4ef6-4341-bdaf-0822af96f8ca.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,28 +1,16 @@
+```cpp
 #include <vector>
 #include <string>
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
+    if(a.size() != b.size()) {
         return false;
     }
-    for (int i = 0; i < a.size(); i++) {
-        if (issame_helper(a[i], b[i]) == false) {
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
             return false;
         }
     }
-    return true;
-}
-
-bool issame_helper(string s1, string s2) {
-    int len1 = s1.length();
-    int len2 = s2.length();
-
-    for (int i = 0; i < min(len1, len2); i++) {
-        if (s1[i] != s2[i]) {
-            return false;
-        }
-    }
-
     return true;
 }
 
