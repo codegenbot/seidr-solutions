0ca67da30c0d346e7b9f73e8e47be29ede996b80
beep boop <githubbot@vadim.me>
2024-07-09 08:33:15 +0200

    Solution 72 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5k7042fx Prompt:    31 |     return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());       |                                                       ^~~ c5d122f7-3099-465a-9181-bd91b97a40f8.cpp:31:64: error: request for member ‘begin ’ in ‘b’, which is of non-class type ‘int’    31 |    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());       |                                                               ^~~~~ c5d122f7-3099-465a-9181-bd91b97a40f8.cpp: At global scope: c5d122f7-3099-465a-9181-bd91b97a40f8.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ c5d122f7-3099-465a-9181-bd91b97a40f8.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main(){       |     ^~~~ In file included from c5d122f7-3099-465a-9181-bd91b97a40f8.cpp:34: c5d122f7-3099-465a-9181-bd91b97a40f8.cpp: In function ‘int main()’: c5d122f7-3099-465a-9181-bd91b97a40f8.cpp:44:20: error: ‘find_closest_elements’ w as not declared in this scope; did you mean ‘closestElements’?    44 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ^~~~~~~~~~~~~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -5,11 +5,19 @@
 
 using namespace std;
 
-bool sameVectors(vector<float>& a, vector<float>& b) {
-    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
+int main(){
+    int n;
+    cin >> n;
+    vector<float> a(n, 0.0f), b(n, 0.0f);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i] >> b[i];
+    }
+    if(areVectorsEqual(a, b)) cout << "The vectors are the same." << endl;
+    else cout << "The vectors are not the same." << endl;
+    return 0;
 }
 
-float find_closest_elements(vector<float> vec) {
+float closestElements(vector<float> vec) {
     float min_diff = numeric_limits<float>::max();
     for(int i=1; i<vec.size(); i++){
         if(abs(vec[i] - vec[i-1]) < min_diff)
@@ -18,15 +26,6 @@ float find_closest_elements(vector<float> vec) {
     return min_diff;
 }
 
-int main(){
-    int n;
-    cin >> n;
-    vector<float> a(n, 0.0f), b(n, 0.0f);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i] >> b[i];
-    }
-    assert(sameVectors(a, b));
-    if(sameVectors(a, b)) cout << "The vectors are the same." << endl;
-    else cout << "The vectors are not the same." << endl;
-    return 0;
+bool areVectorsEqual(vector<float>& a, vector<float>& b) {
+    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
 }
