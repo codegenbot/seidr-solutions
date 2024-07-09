f0f6346f750bd897fc92329038ee1337b97e25db
beep boop <githubbot@vadim.me>
2024-07-09 13:32:47 +0200

    Solution 95 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/1wotaq8r Prompt:  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3b1e17e7-23ae-4000-a4e0-f7c67a037dee.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3b1e17e7-23ae-4000-a4e0-f7c67a037dee.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3b1e17e7-23ae-4000-a4e0-f7c67a037dee.cpp:13: 3b1e17e7-23ae-4000-a4e0-f7c67a037dee.cpp: In function ‘int main()’: 3b1e17e7-23ae-4000-a4e0-f7c67a037dee.cpp:23:20: error: ‘rolling_max’ was not dec lared in this scope    23 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 3b1e17e7-23ae-4000-a4e0-f7c67a037dee.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
 #include <cassert>
 
 vector<int> rolling_max(const vector<int>& numbers);
@@ -9,7 +9,7 @@ bool issame(const vector<int>& a, const vector<int>& b) {
         return false;
     }
     
-    for (size_t i = 0; i < a.size(); ++i) {
+    for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
@@ -22,7 +22,7 @@ vector<int> rolling_max(const vector<int>& numbers) {
     vector<int> result;
     int max_num = INT_MIN;
     
-    for (size_t i = 0; i < numbers.size(); ++i) {
+    for (int i = 0; i < numbers.size(); ++i) {
         max_num = max(max_num, numbers[i]);
         result.push_back(max_num);
     }
@@ -31,8 +31,14 @@ vector<int> rolling_max(const vector<int>& numbers) {
 }
 
 int main() {
-    assert(issame({1, 2, 3}, {1, 2, 3}));
-    assert(!issame({1, 2, 3}, {1, 2, 4}));
-    
+    vector<int> test1 = {1, 3, 4, 2, 5};
+    vector<int> test2 = {1, 3, 4, 2, 5};
+    assert(issame(test1, test2));
+
+    vector<int> result = rolling_max(test1);
+    for (int num : result) {
+        std::cout << num << " ";
+    }
+
     return 0;
 }
