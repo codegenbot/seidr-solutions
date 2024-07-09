e0aeffd3b038c66d47180d23c161754a88c316c4
beep boop <githubbot@vadim.me>
2024-07-09 04:03:45 +0200

    Solution 58 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tzm0q5ti Prompt:       |                    ^~~ 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp:14: 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp: In function ‘int main()’: 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp:24:20: error: ‘common’ was not declared  in this scope    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 22db3f92-5d1c-4c48-b05a-a4225fd90487.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
 #include <vector>
 #include <set>
 
-bool issame(int a, int b) {
-    return (a == b);
+bool issame(int a) {
+    if(a <= 0)
+        return false;
+    else
+        return true;
 }
 
 int common(vector<int> l1, vector<int> l2) {
@@ -14,26 +16,17 @@ int common(vector<int> l1, vector<int> l2) {
     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
                       inserter(intersection, intersection.begin()));
 
-    vector<int> result(intersection.begin(), intersection.end());
-    return (result.size());
+    return intersection.size();
 }
 
 int main() {
-    int n1, m1, n2, m2;
-    cin >> n1 >> m1;
-    vector<int> l1(n1);
-    for (int i = 0; i < n1; i++) {
-        cin >> l1[i];
-    }
-    cin >> n2 >> m2;
-    vector<int> l2(n2);
-    for (int i = 0; i < n2; i++) {
-        cin >> l2[i];
-    }
-
-    int k = common(l1, l2);
-
-    cout << k << endl;
+    vector<int> v1 = {1, 2, 3, 4, 5};
+    vector<int> v2 = {3, 4, 5, 6, 7};
+    
+    if(issame(common(v1, v2)))
+        cout << "The two lists are the same." << endl;
+    else
+        cout << "The two lists are not the same." << endl;
 
     return 0;
 }
