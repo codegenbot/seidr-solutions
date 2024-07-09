e3d3d1c23fa2f425386c06e9207f0a89e21b5346
beep boop <githubbot@vadim.me>
2024-07-09 16:12:34 +0200

    Solution 75 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/40ydl3g4 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,38 +1,22 @@
-#include<stdio.h>
-#include<string>
-#include<algorithm>
-#include<boost/any.hpp>
-using namespace std;
+```cpp
+#include <boost/lexical_cast.hpp>
+
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return b;
-    }
-    if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        return b;
-    }
-    if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return a;
-    }
-    if (a.type() == typeid(string) && b.type() == typeid(int)) {
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        return a.get<int>() > b.get<int>() ? a : b;
+    } else if ((a.type() == typeid(float) || a.type() == typeid(double)) &&
+               (b.type() == typeid(float) || b.type() == typeid(double))) {
+        return a.get<double>() > b.get<double>() ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = boost::any_cast<string>(a);
+        string str2 = boost::any_cast<string>(b);
+        return str1 > str2 ? a : b;
+    } else if (a.type() != typeid(int) && a.type() != typeid(float) &&
+               a.type() != typeid(double) && a.type() == typeid(string)) {
+        return "None";
+    } else if (b.type() != typeid(int) && b.type() != typeid(float) &&
+               b.type() != typeid(double) && b.type() == typeid(string)) {
         return "None";
     }
-    if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        if (stod(b.cast<string>()) > stod(a.cast<string>())) {
-            return b;
-        } else if (stod(b.cast<string>()) < stod(a.cast<string>())) {
-            return a;
-        } else {
-            return "None";
-        }
-    }
-    if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if (stod(b.cast<string>().substr(1,-2)) > stod(a.cast<string>().substr(1,-2))) {
-            return b;
-        } else if (stod(b.cast<string>().substr(1,-2)) < stod(a.cast<string>().substr(1,-2))) {
-            return a;
-        } else {
-            return "None";
-        }
-    }
-    return "None"; // default case
+    return "None";
 }
