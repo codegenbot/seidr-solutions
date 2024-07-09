a9853d6482f943585bc8291dda572ee8c203706b
beep boop <githubbot@vadim.me>
2024-07-09 08:30:19 +0200

    Solution 12 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uxs77z8s Prompt:       |                    ^~~ 44a3b8d9-cf33-4134-b336-8cdaa65ae3fc.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 44a3b8d9-cf33-4134-b336-8cdaa65ae3fc.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 44a3b8d9-cf33-4134-b336-8cdaa65ae3fc.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 44a3b8d9-cf33-4134-b336-8cdaa65ae3fc.cpp:12: 44a3b8d9-cf33-4134-b336-8cdaa65ae3fc.cpp: In function ‘int main()’: 44a3b8d9-cf33-4134-b336-8cdaa65ae3fc.cpp:22:20: error: ‘common’ was not declared  in this scope    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 44a3b8d9-cf33-4134-b336-8cdaa65ae3fc.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,9 +1,34 @@
 #include <vector>
-bool issame(vector<int> a, vector<int> b) {
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a == b;
+}
+
+vector<int> common(const vector<int>& l1, const vector<int>& l2) {
     vector<int> result;
-    set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(result));
+    
+    sort(l1.begin(), l1.end());
+    sort(l2.begin(), l2.end());
+    
+    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
+    
     result.erase(unique(result.begin(), result.end()), result.end());
+    
     return result;
+}
+
+int main() {
+    // Test cases to verify the common function
+    vector<int> test1 = {1, 2, 3, 4, 5};
+    vector<int> test2 = {3, 4, 5, 6, 7};
+    assert(issame(common(test1, test2), {3, 4, 5}));
+    
+    vector<int> test3 = {1, 2, 3};
+    vector<int> test4 = {4, 5, 6};
+    assert(issame(common(test3, test4), {}));
+    
+    return 0;
 }
