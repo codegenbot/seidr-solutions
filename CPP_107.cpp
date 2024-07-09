28c1494aa02814f1315dc457078b037f1b385720
beep boop <githubbot@vadim.me>
2024-07-09 19:55:38 +0200

    Solution 56 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/y5snjn2h Prompt:       |             ^~~~~~ f8a0041b-0384-43a3-b57a-c038c2d65962.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f8a0041b-0384-43a3-b57a-c038c2d65962.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ f8a0041b-0384-43a3-b57a-c038c2d65962.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ f8a0041b-0384-43a3-b57a-c038c2d65962.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from f8a0041b-0384-43a3-b57a-c038c2d65962.cpp:26: f8a0041b-0384-43a3-b57a-c038c2d65962.cpp: In function ‘int main()’: f8a0041b-0384-43a3-b57a-c038c2d65962.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <cassert>
 
 std::vector<int> even_odd_palindrome(int n) {
     std::vector<int> result;
@@ -12,7 +11,7 @@ std::vector<int> even_odd_palindrome(int n) {
     return result;
 }
 
-bool isSame(std::vector<int> a, std::vector<int> b) {
+bool checkPalindrome(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[std::abs(i - (a.size() - 1))]) return false;
@@ -21,5 +20,6 @@ bool isSame(std::vector<int> a, std::vector<int> b) {
 }
 
 int main() {
-    assert(isSame(even_odd_palindrome(5), std::vector<int>({0, 1, 0, 1, 0})));
+    assert(checkPalindrome(even_odd_palindrome(5), stdvector<int>({0, 1, 0, 1, 0})));
+    return 0;
 }
