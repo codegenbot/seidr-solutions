3162242c4b381d97d4359e8d0d547607e76c1a67
beep boop <githubbot@vadim.me>
2024-07-09 21:25:39 +0200

    Solution 83 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8tmp81k3 Prompt: tor std::uninitialized_copy(_InputIterator, _InputIterator, _ForwardIterator) [w ith _InputIterator = const char*; _ForwardIterator = std::pair<char, int>*]’: /usr/include/c++/11/bits/stl_uninitialized.h:333:37:   required from ‘_ForwardIt erator std::__uninitialized_copy_a(_InputIterator, _InputIterator, _ForwardItera tor, std::allocator<_Tp>&) [with _InputIterator = const char*; _ForwardIterator = std::pair<char, int>*; _Tp = std::pair<char, int>]’ /usr/include/c++/11/bits/stl_vector.h:1585:33:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const char*; _Tp = std::pair<char, in t>; _Alloc = std::allocator<std::pair<char, int> >]’ /usr/include/c++/11/bits/stl_vector.h:657:23:   required from ‘std::vector<_Tp, _Alloc>::vector(_InputIterator, _InputIterator, const allocator_type&) [with _In putIterator = const char*; <template-parameter-2-2> = void; _Tp = std::pair<char , int>; _Alloc = std::allocator<std::pair<char, int> >; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<std::pair<char, int> >]’ 2f549a26-786e-4e4d-ac01-cc24dc0a4eb7.cpp:81:5:   required from here /usr/include/c++/11/bits/stl_uninitialized.h:138:72: error: static assertion fai led: result type must be constructible from value type of input range   138 | static_assert(is_constructible<_ValueType2, decltype(*__first)>::value,       |                                                                  ^~~~~ /usr/include/c++/11/bits/stl_uninitialized.h:138:72: note: ‘std::integral_consta nt<bool, false>::value’ evaluates to false

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,79 +1,30 @@
 ```cpp
-#include <sstream>
-#include <vector>
-#include <utility>
-#include <cmath>
-
-struct pair {
-    char first;
-    int second;
-
-    pair(char x, int y) : first(x), second(y) {}
-};
-
-int do_algebra(std::vector<std::pair<char, int>> operators_and_operands) {
-    if (operators_and_operands.size() == 0) {
-        return 0;
-    }
-    
-    int result = operators_and_operands[0].second;
-    for (const auto& pair : operators_and_operands) {
-        if (pair.first == '+') {
-            result += pair.second;
-        } else if (pair.first == '-') {
-            result -= pair.second;
-        } else if (pair.first == '*') {
-            result *= pair.second;
-        } else if (pair.first == '/') {
-            if (pair.second != 0) {
-                result /= pair.second;
-            }
-        } else if (pair.first == '^') { 
-            result = std::pow(result, pair.second);
+while (iss >> token) {
+    if (!token.empty()) {
+        if (op != '+') {
+            operators_and_operands.push_back({op, operand});
+            op = '+';
+            operand = 0;
+        }
+        if (token[0] == '+') {
+            op = '+';
+            operand = std::stoi(token.substr(1));
+        } else if (token[0] == '-') {
+            op = '-';
+            operand = std::stoll(token.substr(1));
+        } else if (token[0] == '*') {
+            op = '*';
+            operand = std::stoll(token.substr(1));
+        } else if (token[0] == '/') {
+            op = '/';
+            operand = std::stoll(token.substr(1));
+        } else if (token[0] == '^') {
+            op = '^';
+            operand = std::stoi(token.substr(1));
         }
     }
-    return result;
 }
-
-int main() {
-    std::string str = "++7*3+4";
-    std::istringstream iss(str);
-    
-    char op = '+';
-    int operand = 0; // initialize operand variable
-    std::vector<std::pair<char, int>> operators_and_operands;
-    std::string token;
-    while (iss >> token) {
-        if (!token.empty()) {
-            if (op != '+') {
-                operators_and_operands.push_back({op, operand});
-                op = '+';
-                operand = 0;
-            }
-            if (token[0] == '+') {
-                op = '+';
-                operand = std::stoi(token.substr(1));
-            } else if (token[0] == '-') {
-                op = '-';
-                operand = std::stoi(token.substr(1));
-            } else if (token[0] == '*') {
-                op = '*';
-                operand = std::stoi(token.substr(1));
-            } else if (token[0] == '/') {
-                op = '/';
-                operand = std::stoi(token.substr(1));
-            } else if (token[0] == '^') {
-                op = '^';
-                operand = std::stoi(token.substr(1));
-            }
-        }
-    }
-    
-    // add the last operator and operand to vector
-    if (op != '+') {
-        operators_and_operands.push_back({op, operand});
-    }
-    
-    int result = do_algebra(operators_and_operands);
-    return 0;
+// add the last operator and operand to vector
+if (op != '+') {
+    operators_and_operands.push_back({op, operand});
 }
