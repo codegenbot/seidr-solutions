ab581b35f0bc636132d56d0763cd91ea1b8c9fa3
beep boop <githubbot@vadim.me>
2024-07-09 03:13:52 +0200

    Solution 89 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/noyz23n0 Prompt: fe536ab7-59fb-451f-a7ed-8b533f4d1cb0.cpp:1:6: error: ‘string’ in namespace ‘std’  does not name a type     1 | std::string roman = int_to_mini_roman(1000);       |      ^~~~~~ fe536ab7-59fb-451f-a7ed-8b533f4d1cb0.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | std::string roman = int_to_mini_roman(1000); In file included from fe536ab7-59fb-451f-a7ed-8b533f4d1cb0.cpp:3: fe536ab7-59fb-451f-a7ed-8b533f4d1cb0.cpp: In function ‘int main()’: fe536ab7-59fb-451f-a7ed-8b533f4d1cb0.cpp:5:13: error: ‘int_to_mini_romank’ was n ot declared in this scope     5 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,26 +1,25 @@
-```cpp
 #include <string>
+#include <map>
 
-std::string int_to_mini_roman(int n);
+using namespace std;
+
+map<int, string> roman_map = {{1000, "M"}, {900, "CM"}, {500, "D"}, 
+                               {400, "CD"}, {100, "C"}, {90, "XC"},
+                               {50, "L"}, {40, "XL"}, {10, "X"},
+                               {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+
+string int_to_mini_roman(int n) {
+    string roman;
+    for (auto p : roman_map | map_view<less<int>>()) {
+        while (n >= p.first) {
+            roman += p.second;
+            n -= p.first;
+        }
+    }
+    return roman;
+}
 
 int main() {
     std::string roman = int_to_mini_roman(1000);
     // ...
-}
-
-std::string int_to_mini_roman(int n) {
-    if (n <= 0)
-        return "";
-    
-    static const char* Roman[15] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    static int Val[15] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    
-    std::string res;
-    for (int i = 0; i < 13; ++i)
-        while (n >= Val[i]) {
-            n -= Val[i];
-            res += Roman[i];
-        }
-    
-    return res;
 }
