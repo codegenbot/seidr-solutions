dd03927f9e6f3874f6fb1af456b634f7c8f16a05
beep boop <githubbot@vadim.me>
2024-07-09 17:40:17 +0200

    Solution 5 of CPP/97, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/d6dasiv3 Prompt: Complete the following code given the task description and function signature.

--- CPP_97.cpp
+++ CPP_97.cpp
@@ -1,5 +1,5 @@
 int multiply(int a, int b){
-    int unit_a = abs(a) % 10;
-    int unit_b = abs(b) % 10;
-    return unit_a * unit_b;
+    int unit_digit_a = abs(a) % 10;
+    int unit_digit_b = abs(b) % 10;
+    return unit_digit_a * unit_digit_b;
 }
