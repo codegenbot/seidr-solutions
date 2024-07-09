596d61558be9d1a6c6b2e1e253dfa74e49ed48f8
beep boop <githubbot@vadim.me>
2024-07-09 07:48:18 +0200

    Solution 32 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/siauvrg0 Prompt:       |                              ^~~~~~ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 964a0b5c-562f-4158-92fc-249bca43033b.cpp:50: 964a0b5c-562f-4158-92fc-249bca43033b.cpp: In function ‘int main()’: 964a0b5c-562f-4158-92fc-249bca43033b.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,7 +1,48 @@
-```bool issame(vector<string> a, vector<string>b){
+```cpp
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-int main() {
-    assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));
+vector<vector<string>> by_length(vector<int> arr) {
+    vector<vector<string>> result;
+    for (int i = 0; i < 9; i++) {
+        vector<string> temp;
+        for (int num : arr) {
+            if (num >= i + 1 && num <= i + 9) {
+                switch (num) {
+                    case i + 1:
+                        temp.push_back("One");
+                        break;
+                    case i + 2:
+                        temp.push_back("Two");
+                        break;
+                    case i + 3:
+                        temp.push_back("Three");
+                        break;
+                    case i + 4:
+                        temp.push_back("Four");
+                        break;
+                    case i + 5:
+                        temp.push_back("Five");
+                        break;
+                    case i + 6:
+                        temp.push_back("Six");
+                        break;
+                    case i + 7:
+                        temp.push_back("Seven");
+                        break;
+                    case i + 8:
+                        temp.push_back("Eight");
+                        break;
+                    case i + 9:
+                        temp.push_back("Nine");
+                        break;
+                }
+            }
+        }
+        sort(temp.begin(), temp.end());
+        reverse(temp.begin(), temp.end());
+        result.push_back(temp);
+    }
+    return result;
 }
