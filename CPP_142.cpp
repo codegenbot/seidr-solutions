bac7cf65700b901247f147a4f151bb2df4a24d39
beep boop <githubbot@vadim.me>
2024-07-09 16:06:33 +0200

    Solution 42 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8qxhoagc Prompt: Complete the following code given the task description and function signature.

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -1,10 +1,10 @@
 int sum_squares(vector<int> lst) {
-    int sum = 0;
+    int total = 0;
     for (int i = 0; i < lst.size(); i++) {
         if ((i + 1) % 3 == 0 && (i + 1) % 4 != 0)
-            sum += lst[i] * lst[i];
+            total += lst[i] * lst[i];
         else if ((i + 1) % 4 == 0 && (i + 1) % 3 != 0)
-            sum += pow(lst[i], 3);
+            total += pow(lst[i], 3);
     }
-    return sum;
+    return total;
 }
