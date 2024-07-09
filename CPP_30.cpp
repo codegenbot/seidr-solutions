882bc3fc1d08c052a44f3c412680b54e9d7383be
beep boop <githubbot@vadim.me>
2024-07-09 23:27:04 +0200

    Solution 97 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ahcchrnx Prompt:       |                    ^~~~~ 6c16837a-d0ee-4edf-8865-7357dd3e4aa6.cpp:11:29: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 6c16837a-d0ee-4edf-8865-7357dd3e4aa6.cpp:11:36: error: expected primary-expressi on before ‘float’    11 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 6c16837a-d0ee-4edf-8865-7357dd3e4aa6.cpp:11:43: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 6c16837a-d0ee-4edf-8865-7357dd3e4aa6.cpp:10: 6c16837a-d0ee-4edf-8865-7357dd3e4aa6.cpp: In function ‘int main()’: 6c16837a-d0ee-4edf-8865-7357dd3e4aa6.cpp:20:20: error: ‘get_positive’ was not de clared in this scope    20 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 6c16837a-d0ee-4edf-8865-7357dd3e4aa6.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<float> a, std::vector<float>b){
+bool issame(std::vector<float> a, std::vector<float> b){
     if(a.size() != b.size()) return false;
     for(int i=0; i<a.size(); i++){
         if(a[i] != b[i]) return false;
@@ -9,9 +9,9 @@ bool issame(std::vector<float> a, std::vector<float>b){
     return true;
 }
 
-std::vector<float> my_vector(float l[]){ 
+std::vector<float> get_positive(std::vector<float> l){
     std::vector<float> result;
-    for(float i : {l,l+sizeof(l)/sizeof(*l)}) {
+    for(float i : l) {
         if(i > 0)
             result.push_back(i);
     }
@@ -19,6 +19,6 @@ std::vector<float> my_vector(float l[]){
 }
 
 int main() {
-    assert(issame(my_vector({}), {}));
+    assert(issame(get_positive({}), {}));
     return 0;
 }
