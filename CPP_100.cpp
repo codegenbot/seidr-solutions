bfdf13e6599e3054ec1f2275462580b9ebb72aa1
beep boop <githubbot@vadim.me>
2024-07-09 16:53:20 +0200

    Solution 30 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9d9hp3es Prompt:       |                                  ^~~ 035e100b-4531-4872-9ee3-0e36976794c5.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 035e100b-4531-4872-9ee3-0e36976794c5.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 035e100b-4531-4872-9ee3-0e36976794c5.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from 035e100b-4531-4872-9ee3-0e36976794c5.cpp:19: 035e100b-4531-4872-9ee3-0e36976794c5.cpp: In function ‘int main()’: 035e100b-4531-4872-9ee3-0e36976794c5.cpp:29:20: error: ‘make_a_pile’ was not dec lared in this scope; did you mean ‘makeApile’?    29 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 035e100b-4531-4872-9ee3-0e36976794c5.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cassert>
 
 bool vectorSame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
@@ -9,4 +10,9 @@ std::vector<int> makeApile(int n) {
     for (int i = 2 * n; i > 0; i -= 2)
         pile.push_back(i);
     return pile;
+}
+
+int main() {
+    assert(vectorSame(makeApile(4), {2, 4, 6, 8}));
+    return 0;
 }
