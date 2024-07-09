4e5cc57827af27514adc877b52978f39d439d1b0
beep boop <githubbot@vadim.me>
2024-07-09 04:33:44 +0200

    Solution 41 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5ldlq6rg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/166d7adf-1ed2-4643-9ebf-abd2396ff 339.py", line 1     `return " ".join(str(num_dict[i]) for i in sorted(map(int, numbers.split())) )`     ^ SyntaxError: invalid syntax

--- Python_19.py
+++ Python_19.py
@@ -1,16 +1,1 @@
-def sort_numbers(numbers):
-    """
-    Sorts the input string of numbers and returns a string with the numbers separated by spaces.
-
-    Args:
-        numbers (str): A string containing space-separated numbers.
-
-    Returns:
-        str: The input numbers sorted in ascending order, separated by spaces.
-    """
-    num_dict = {}
-    i = 0
-    for num in numbers.split():
-        num_dict[int(num)] = i
-        i += 1
-    return " ".join(str(num) for i in sorted(map(int, numbers.split())))
+`def solution(numbers): return " ".join(map(str, sorted(map(int, numbers.split()))))`
