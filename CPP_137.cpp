7fc49e957f3cc521ea4ee0301b92c059a3a2a467
beep boop <githubbot@vadim.me>
2024-07-09 02:28:10 +0200

    Solution 30 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/pf8nwdde Prompt: 3c028d87-db99-4238-bb78-5404f197f8c4.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 3c028d87-db99-4238-bb78-5404f197f8c4.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 3c028d87-db99-4238-bb78-5404f197f8c4.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 3c028d87-db99-4238-bb78-5404f197f8c4.cpp:2:10: fatal error: boost/any.hpp: No su ch file or directory     2 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,40 +1,6 @@
+```cpp
 #include <boost/any.hpp>
 #include <string>
-#include <iostream>
-
-using namespace std;
-
-boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return a;
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        return a;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string strA = boost::any_cast<string>(a);
-        string strB = boost::any_cast<string>(b);
-        if (strA > strB)
-            return a;
-        else if (strA < strB)
-            return b;
-        else
-            return "None";
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int iA = boost::any_cast<int>(a);
-        int iB = boost::any_cast<int>(b);
-        if (iA > iB)
-            return a;
-        else if (iA < iB)
-            return b;
-        else
-            return "None";
-    }
-    else {
-        return "None";
-    }
-}
 
 int main() {
     boost::any a = "hello";
@@ -42,7 +8,23 @@ int main() {
 
     boost::any result = compare_one(a, b);
 
-    cout << boost::any_cast<string>(result) << endl;
+    std::cout << boost::any_cast<std::string>(result) << std::endl;
 
     return 0;
+}
+
+boost::any compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+        std::string strA = boost::any_cast<std::string>(a);
+        std::string strB = boost::any_cast<std::string>(b);
+        if (strA > strB)
+            return a;
+        else if (strA < strB)
+            return b;
+        else
+            return "None";
+    }
+    else {
+        return "None";
+    }
 }
