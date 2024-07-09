3b28216a4902b3acd46eb9af0b3aeb141926cf44
beep boop <githubbot@vadim.me>
2024-07-09 05:24:41 +0200

    Solution 21 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/mkvi9qpw Prompt:     4 | 7d415d79-9260-4455-82de-e6020c10eb8a.cpp:34:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    34 | int main(){       |         ^~ 7d415d79-9260-4455-82de-e6020c10eb8a.cpp:34:9: note: remove parentheses to defau lt-initialize a variable    34 | int main(){       |         ^~       |         -- 7d415d79-9260-4455-82de-e6020c10eb8a.cpp:34:9: note: or replace parentheses with  braces to value-initialize a variable 7d415d79-9260-4455-82de-e6020c10eb8a.cpp:34:11: error: a function-definition is not allowed here before ‘{’ token    34 | int main(){       |           ^ 7d415d79-9260-4455-82de-e6020c10eb8a.cpp:36:2: error: expected ‘}’ at end of inp ut    36 | }       |  ^ 7d415d79-9260-4455-82de-e6020c10eb8a.cpp:20:12: note: to match this ‘{’    20 | int main() {       |            ^

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -7,7 +7,6 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return true;
 }
 
-
 std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
     std::vector<int> result;
     for (int i = 0; i < game.size(); ++i) {
