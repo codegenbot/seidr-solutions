22c5df11dd1bfb32d3085651eab1564685bdf2e1
beep boop <githubbot@vadim.me>
2024-07-09 12:39:14 +0200

    Solution 36 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/tr3wxu14 Prompt: c45610b2-2b72-4b19-9754-cad1817dffec.cpp: In function ‘std::string string_to_md5 (const string&)’: c45610b2-2b72-4b19-9754-cad1817dffec.cpp:11:8: warning: ‘unsigned char* MD5(cons t unsigned char*, size_t, unsigned char*)’ is deprecated: Since OpenSSL 3.0 [-Wd eprecated-declarations]    11 |     MD5((unsigned char*)text.c_str(), text.length(), digest);       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from c45610b2-2b72-4b19-9754-cad1817dffec.cpp:3: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ Program failed

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <string>
-#include <cassert>
-#include <openssl/md5.h>
+#include <openssl/evp.h>
 
-std::string string_to_md5(std::string text) {
+std::string string_to_md5(const std::string& text) {
     if (text.empty()) {
         return "None";
     }
 
-    unsigned char digest[MD5_DIGEST_LENGTH];
-    MD5((unsigned char*)text.c_str(), text.length(), digest);
+    EVP_MD_CTX *mdctx;
+    mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
+    EVP_DigestUpdate(mdctx, text.c_str(), text.length());
+
+    unsigned char digest[EVP_MAX_MD_SIZE];
+    unsigned int digest_len;
+    EVP_DigestFinal_ex(mdctx, digest, &digest_len);
+    EVP_MD_CTX_free(mdctx);
 
     char mdString[33];
-    int i;
-    for (i = 0; i < 16; i++) {
-        sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
+    for (unsigned int i = 0; i < digest_len; i++) {
+        sprintf(&mdString[i * 2], "%02x", digest[i]);
     }
 
-    return std::string(mdString);
-}
-
-int main() {
-    assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
-    return 0;
+    return mdString;
 }
