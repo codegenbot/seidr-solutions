4eefc59c5e2df09fec4977ffc30dcdf4fda3846d
beep boop <githubbot@vadim.me>
2024-07-09 16:26:33 +0200

    Solution 52 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/29z5qmt1 Prompt:       |             ^~~~~~ 2e564533-d7a7-4c2d-9be1-42f40aa08fe7.cpp:42:34: error: expected primary-expressi on before ‘int’    42 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2e564533-d7a7-4c2d-9be1-42f40aa08fe7.cpp:42:39: error: expression list treated a s compound expression in initializer [-fpermissive]    42 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 2e564533-d7a7-4c2d-9be1-42f40aa08fe7.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ 2e564533-d7a7-4c2d-9be1-42f40aa08fe7.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from 2e564533-d7a7-4c2d-9be1-42f40aa08fe7.cpp:41: 2e564533-d7a7-4c2d-9be1-42f40aa08fe7.cpp: In function ‘int main()’: 2e564533-d7a7-4c2d-9be1-42f40aa08fe7.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -32,7 +32,6 @@ int main() {
     int index = pluck({7, 9, 7, 1});
     std::vector<int> emptyVector;
 
-    if (index % 2 == 0 || index == -1) {
+    if (index % 2 == 0 || index == -1) 
         assert(areEqual({}, emptyVector)); 
-    }
 }
