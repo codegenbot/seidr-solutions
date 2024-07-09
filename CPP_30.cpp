02447a8581da13ea1349c53813d75d3dc6cf03c6
beep boop <githubbot@vadim.me>
2024-07-09 03:26:48 +0200

    Solution 28 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jvy931jd Prompt:       |                    ^~~~~ 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:11: 5d830cd4-1162-4af1-ae79-c42923590cad.cpp: In function ‘int main()’: 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 5d830cd4-1162-4af1-ae79-c42923590cad.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,21 +1,35 @@
 ```cpp
 #include <vector>
-#include <algorithm>
+using namespace std;
 
-bool issame(vector<float> a, vector<float> b) {
-    return a == b;
+bool issame(vector<float> a, vector<float> b){
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i<a.size(); i++){
+        if(a[i] != b[i]) return false;
+    }
+    return true;
 }
 
-int main() {
-    assert(issame(get_positive({}), {}));
+int main(){
+    assert (issame(get_positive({}), {})); 
+    vector<float> l;
+    cout << "Enter elements of the vector (negative to stop): ";
+    while(cin >> l.push_back) {
+        if(l.back() <= 0) break;
+    }
+    vector<float> result = get_positive(l);
+    cout << "Positive numbers are: ";
+    for(float i : result){
+        cout << i << " ";
+    }
     return 0;
 }
 
-vector<float> get_positive(vector<float> l) {
-    vector<float> result;
-    for (float x : l) {
-        if (x > 0) {
-            result.push_back(x);
+vector<std::vector<float>> get_positive(vector<float> l){
+    vector<std::vector<float>> result;
+    for(float i : l){
+        if(i > 0) {
+            result.push_back(std::vector<float>(1, i));
         }
     }
     return result;
