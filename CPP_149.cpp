748396384c8ef84e03f6a11b0d904d250f2a6544
beep boop <githubbot@vadim.me>
2024-07-09 23:05:36 +0200

    Solution 67 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/b0l0pzf4 Prompt:    39 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ 4efd393e-da7f-4e81-91cb-01f41e74e793.cpp:39:37: error: ‘string’ was not declared  in this scope    39 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4efd393e-da7f-4e81-91cb-01f41e74e793.cpp:39:44: error: ‘b’ was not declared in t his scope    39 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 4efd393e-da7f-4e81-91cb-01f41e74e793.cpp:38: 4efd393e-da7f-4e81-91cb-01f41e74e793.cpp: In function ‘int main()’: 4efd393e-da7f-4e81-91cb-01f41e74e793.cpp:48:20: error: ‘sorted_list_sum’ was not  declared in this scope    48 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 4efd393e-da7f-4e81-91cb-01f41e74e793.cpp:48:19: error: ‘issame’ cannot be used a s a function    48 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -8,26 +8,3 @@ bool issame(vector<string> a, vector<string> b) {
     }
     return true;
 }
-
-vector<string> sorted_list_sum(vector<string> lst) {
-    vector<string> result;
-
-    // Remove strings with odd lengths
-    for (const auto& str : lst) {
-        if (str.length() % 2 == 0) {
-            result.push_back(str);
-        }
-    }
-
-    // Sort the resulted vector by length and then alphabetically
-    sort(result.begin(), result.end(),
-              [](const string& a, const string& b) {
-                  if (a.length() != b.length()) {
-                      return a.length() < b.length();
-                  } else {
-                      return a < b;
-                  }
-              });
-
-    return result;
-}
