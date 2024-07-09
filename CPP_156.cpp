33690bfc7e046ba28a94063bb4322f08416988a0
beep boop <githubbot@vadim.me>
2024-07-09 04:43:07 +0200

    Solution 20 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/z1pfhlvw Prompt: f9bda9cc-78eb-4d91-a9aa-2ee288ef9dcc.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int number) {       | ^~~~~~ In file included from f9bda9cc-78eb-4d91-a9aa-2ee288ef9dcc.cpp:16: f9bda9cc-78eb-4d91-a9aa-2ee288ef9dcc.cpp: In function ‘int main()’: f9bda9cc-78eb-4d91-a9aa-2ee288ef9dcc.cpp:18:13: error: ‘int_to_mini_romank’ was not declared in this scope    18 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,15 +1,1 @@
-using namespace std;
-
-vector<string> romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-vector<int> romanValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-
-string int_to_mini_roman(int number) {
-    string result;
-    for (int i = 0; i < romanValues.size(); i++) {
-        while (number >= romanValues[i]) {
-            number -= romanValues[i];
-            result += romanNumerals[i];
-        }
-    }
-    return tolower(result);
-}
+assert(int_to_mini_roman(1000) == "M");
