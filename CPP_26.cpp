867535b085dfc2800eaee2971aeea21a7567bfa0
beep boop <githubbot@vadim.me>
2024-07-09 07:22:36 +0200

    Solution 24 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/06g61nxx Prompt: in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c932aa2c-5fba-4d42-888d-174a772bf5e4.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c932aa2c-5fba-4d42-888d-174a772bf5e4.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c932aa2c-5fba-4d42-888d-174a772bf5e4.cpp:5: c932aa2c-5fba-4d42-888d-174a772bf5e4.cpp: In function ‘int main()’: c932aa2c-5fba-4d42-888d-174a772bf5e4.cpp:15:20: error: ‘remove_duplicates’ was n ot declared in this scope    15 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ c932aa2c-5fba-4d42-888d-174a772bf5e4.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,39 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-vector<int> remove_duplicates(vector<int>& numbers) {
+vector<int> remove_duplicates(vector<int> numbers){
     sort(numbers.begin(), numbers.end());
     numbers.erase(unique(numbers.begin(), numbers.end()), numbers.end());
     return numbers;
 }
 
-bool issame(const vector<int>& a, const vector<int>& b) {
+bool issame(vector<int> a, vector<int> b){
     return a == b;
-}
-
-int main() {
-    // Input numbers from the user
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-    
-    vector<int> numbers(n);
-    cout << "Enter the elements: ";
-    for (int i = 0; i < n; ++i) {
-        cin >> numbers[i];
-    }
-    
-    // Remove duplicates from the vector
-    numbers = remove_duplicates(numbers);
-    
-    // Output the unique elements
-    cout << "Unique elements: ";
-    for (int num : numbers) {
-        cout << num << " ";
-    }
-    
-    return 0;
 }
