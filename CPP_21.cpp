1f053164c3deaa487bf249750dd16802156f1d8a
beep boop <githubbot@vadim.me>
2024-07-09 02:54:34 +0200

    Solution 4 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zffmqvx4 Prompt:    13 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 6f4ee0dd-d0e1-4356-b076-0d2ed371b879.cpp:13:36: error: expected primary-expressi on before ‘float’    13 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 6f4ee0dd-d0e1-4356-b076-0d2ed371b879.cpp:13:43: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 6f4ee0dd-d0e1-4356-b076-0d2ed371b879.cpp:12: 6f4ee0dd-d0e1-4356-b076-0d2ed371b879.cpp: In function ‘int main()’: 6f4ee0dd-d0e1-4356-b076-0d2ed371b879.cpp:22:20: error: ‘rescale_to_unit’ was not  declared in this scope    22 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 6f4ee0dd-d0e1-4356-b076-0d2ed371b879.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,16 +1,8 @@
-#include <vector>
-
-using namespace std;
-
-vector<float> rescale_to_unit(vector<float> numbers) {
-    float minVal = *min_element(numbers.begin(), numbers.end());
-    float maxVal = *max_element(numbers.begin(), numbers.end());
-    
-    vector<float> result;
-    
-    for (float num : numbers) {
-        result.push_back((num - minVal) / (maxVal - minVal));
-    }
-    
-    return result;
+bool issame(vector<float> a, vector<float> b) {
+    if (a.size() != b.size())
+        throw runtime_error("Vector sizes do not match");
+    for (int i = 0; i < a.size(); i++)
+        if (abs(a[i] - b[i]) > 1e-5)
+            return false;
+    return true;
 }
