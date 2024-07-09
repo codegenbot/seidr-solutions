842306f0d7ffee61934af082167f0a1cded46633
beep boop <githubbot@vadim.me>
2024-07-09 11:27:32 +0200

    Solution 3 of CPP/26, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/x5bsqhvf Prompt: Complete the following code given the task description and function signature.

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,11 +1,12 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 vector<int> remove_duplicates(vector<int> numbers){
-    unordered_map<int, bool> mp;
     vector<int> result;
-
-    for(int i=0; i<numbers.size(); i++){
-        if(mp.find(numbers[i]) == mp.end())
-            mp[numbers[i]] = true, result.push_back(numbers[i]);
+    for(int num : numbers) {
+        if(find(result.begin(), result.end(), num) == result.end())
+            result.push_back(num);
     }
-
     return result;
 }
