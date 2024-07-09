e6585fde42d9f8f3dc16d60addcde4be441a3129
beep boop <githubbot@vadim.me>
2024-07-09 03:48:50 +0200

    Solution 83 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nvqm4xhw Prompt: In file included from /usr/include/c++/11/vector:67,                  from 93092c7c-b436-46f9-8158-e829fe9e5983.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 93092c7c-b436-46f9-8158-e829fe9e5983.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 93092c7c-b436-46f9-8158-e829fe9e5983.cpp:26:16: error: expected primary-expressi on before ‘double’    26 |         vector<double> coeffs = {};       |                ^~~~~~ 93092c7c-b436-46f9-8158-e829fe9e5983.cpp:31:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    31 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 93092c7c-b436-46f9-8158-e829fe9e5983.cpp:34:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    34 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -2,16 +2,16 @@
 #include <vector>
 #include <cmath>
 
-double poly(std::vector<double> coeffVector, double x) {
+double poly(std::vector<double> coefficientVector, double x) {
     double sum = 0;
-    for (int i = 0; i < coeffVector.size(); i++) {
-        sum += coeffVector[i] * pow(x, i);
+    for (int i = 0; i < coefficientVector.size(); i++) {
+        sum += coefficientVector[i] * pow(x, i);
     }
     return sum;
 }
 
-double find_zero(std::vector<double> xs){
-    if(xs.size() % 2 != 0) return -1; 
-    double root = -(xs[1]/(2*xs[0])); 
+double find_zero(std::vector<double> xValues){
+    if(xValues.size() % 2 != 0) return -1; 
+    double root = -xValues[1]/(2*xValues[0]); 
     return std::round(root);
 }
