9878495da6dbe753a0b840e6c5ebb700ea8ba790
beep boop <githubbot@vadim.me>
2024-07-09 23:57:15 +0200

    Solution 23 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g8hfxvnh Prompt:       |                              ^~~~~~ ed2acc0a-0b40-4662-8ed0-7f723cd36bf5.cpp:48:37: error: ‘string’ was not declared  in this scope    48 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ed2acc0a-0b40-4662-8ed0-7f723cd36bf5.cpp:48:44: error: ‘b’ was not declared in t his scope    48 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ed2acc0a-0b40-4662-8ed0-7f723cd36bf5.cpp:48:45: error: expression list treated a s compound expression in initializer [-fpermissive]    48 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ed2acc0a-0b40-4662-8ed0-7f723cd36bf5.cpp:47: ed2acc0a-0b40-4662-8ed0-7f723cd36bf5.cpp: In function ‘int main()’: ed2acc0a-0b40-4662-8ed0-7f723cd36bf5.cpp:57:20: error: ‘by_length’ was not decla red in this scope    57 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ ed2acc0a-0b40-4662-8ed0-7f723cd36bf5.cpp:57:19: error: ‘issame’ cannot be used a s a function    57 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -6,52 +6,18 @@ bool issame(vector<string> a, vector<string>b) {
 }
 
 vector<string> by_length(vector<int> arr) {
-    vector<int> temp;
+    vector<string> temp;
     for (int i : arr) {
         if (i >= 1 && i <= 9) {
-            temp.push_back(i);
+            temp.push_back(to_string(i));
         }
     }
     sort(temp.begin(), temp.end());
     reverse(temp.begin(), temp.end());
-    vector<string> result;
-    for (int i : temp) {
-        string s = "";
-        switch (i) {
-            case 1:
-                s = "One";
-                break;
-            case 2:
-                s = "Two";
-                break;
-            case 3:
-                s = "Three";
-                break;
-            case 4:
-                s = "Four";
-                break;
-            case 5:
-                s = "Five";
-                break;
-            case 6:
-                s = "Six";
-                break;
-            case 7:
-                s = "Seven";
-                break;
-            case 8:
-                s = "Eight";
-                break;
-            case 9:
-                s = "Nine";
-                break;
-        }
-        result.push_back(s);
-    }
-    return result;
+    return temp;
 }
 
 int main() {
-    assert (issame(by_length({9, 4, 8}) , {"Nine", "Four", "Eight"}));
+    assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));
     // ...
 }
