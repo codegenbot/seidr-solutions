fe501d631c301b9ca7aff8dd938a9a76730f9abd
beep boop <githubbot@vadim.me>
2024-07-09 22:18:12 +0200

    Solution 20 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fjjhgnby Prompt:    41 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:41:34: error: expected primary-expressi on before ‘int’    41 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:41:39: error: expression list treated a s compound expression in initializer [-fpermissive]    41 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:40: 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp: In function ‘int main()’: 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:50:20: error: ‘minPath’ was not declare d in this scope    50 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,45 +1,38 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-
-using namespace std;
-
-struct MyPair {
-    vector<int> v;
-    pair<int, int> p;
-};
-
-vector<int> minPath(vector<vector<int>> grid, int k) {
+vector<int> minPath(vector<vector<int>>& grid, int k) {
     int n = grid.size();
     vector<vector<bool>> visited(n, vector<bool>(n));
-    priority_queue<MyPair, vector<MyPair>, function<bool(const MyPair &, const MyPair &)>> pq([=](const auto& a, const auto& b) { return a.v[0] + a.p.first > b.v[0] + b.p.first; });
-    pq.push({{grid[0][0], {grid[0][0]}}, {0, 0}});
+    vector<pair<int, int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
     vector<int> res;
-    
-    while (!pq.empty()) {
-        auto [val, path] = pq.top();
-        pq.pop();
-        
-        if (path.second.size() == k) {
-            return path.first;
-        }
-        
-        int r = path.second.first / n, c = path.second.second % n;
-        
-        for (int dr = -1; dr <= 1; dr++) {
-            for (int dc = -1; dc <= 1; dc++) {
-                if (dr == 0 && dc == 0) continue;
-                
-                int nr = r + dr, nc = c + dc;
-                
-                if (nr >= 0 && nr < n && nc >= 0 && nc < n && !visited[nr][nc]) {
-                    visited[nr][nc] = true;
-                    pq.push({{val, path.second + 1}, {path.first, path.second + 1}});
-                }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (grid[i][j] == 1) {
+                pq.push({k, {i, j}});
+                break;
             }
         }
     }
-    
-    return {};
+
+    while (!pq.empty()) {
+        auto [dist, pos] = pq.top();
+        pq.pop();
+
+        if (dist > 0) {
+            res.push_back(grid[pos.first][pos.second]);
+            visited[pos.first][pos.second] = true;
+
+            for (auto& d : dir) {
+                int ni = pos.first + d.first;
+                int nj = pos.second + d.second;
+                if (ni >= 0 && ni < n && nj >= 0 && nj < n && !visited[ni][nj]) {
+                    pq.push({dist - 1, {ni, nj}});
+                }
+            }
+        }
+
+        if (res.size() == k) break;
+    }
+
+    return res;
 }
