f98213dda19e174f49c8c4995f30772f2829e556
beep boop <githubbot@vadim.me>
2024-07-09 19:42:19 +0200

    Solution 46 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/6rq7tv1u Prompt: it must return 9 3274 3794 3551 6271 9783 8532 4034 3533 1372 8 4655 308 3960 8646 8817 5776 8383 3409 for input 17 3274 3794 3551 6271 9783 8532 4034 3533 1372 4655 308 3960 8646 8817 5776 8383 3409, but it returns [3274 3794 3551 6271 9783 8532 4034 3533 1372] [4655 308 3960 8646 8817 5776 838 3 3409].

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -10,8 +10,6 @@ vector<vector<int>> cutVector(vector<int> v) {
     res[0].resize(0);
     res[1].resize(0);
     int minDiff = INT_MAX;
-    int leftIndex = 0;
-    
     for (int i = 0; i < n; i++) {
         int leftSum = 0, rightSum = 0;
         for (int j = 0; j <= i; j++) {
@@ -20,22 +18,18 @@ vector<vector<int>> cutVector(vector<int> v) {
         for (int j = i + 1; j < n; j++) {
             rightSum += v[j];
         }
-        if (leftSum == rightSum || abs(leftSum - rightSum) < minDiff) {
-            if (abs(leftSum - rightSum) < minDiff) {
-                minDiff = abs(leftSum - rightSum);
-                leftIndex = i + 1;
-            }
+        int currDiff = abs(leftSum - rightSum);
+        if (currDiff < minDiff) {
             res[0].clear();
-            for (int j = 0; j <= leftIndex; j++) {
+            for (int j = 0; j <= i; j++) {
                 res[0].push_back(v[j]);
             }
-            if (leftSum != rightSum) {
-                res[1].clear();
-                for (int j = leftIndex; j < n; j++) {
-                    res[1].push_back(v[j]); 
-                }
-            } else {
-                res[1] = res[0];
+            res[1] = res[0];
+            minDiff = currDiff;
+        } else if (currDiff == minDiff) {
+            res[1].clear();
+            for (int j = i + 1; j < n; j++) {
+                res[1].push_back(v[j]);
             }
         }
     }
