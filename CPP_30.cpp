de4b1f8206153baa78f7cca087fb15fbdb0bbd5a
beep boop <githubbot@vadim.me>
2024-07-09 03:39:28 +0200

    Solution 54 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jvy931jd Prompt: 4d3a43f5-932c-4f07-8807-7e3b59a0bf73.cpp:4:1: error: ‘vector’ does not name a ty pe     4 | vector<float> get_positive(vector<float> l){       | ^~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,10 +1,3 @@
 using namespace std;
 
-vector<vector<float>> get_positive(vector<vector<float>> l){
-    vector<vector<float>> result;
-    for(float num : l) {
-        if(num > 0) {
-            result.push_back({num});
-        }
-    }
-    return result;
+vector<float> get_positive(vector<float> l){
