0793e2654a6bc6149a248f70ce47a328107b5cc6
beep boop <githubbot@vadim.me>
2024-07-09 12:48:51 +0200

    Solution 66 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/s92p6few Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:26: 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp: In function ‘int main()’: 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,6 +1,4 @@
-bool issame(string s1, string s2) {
-    sort(s1.begin(), s1.end());
-    sort(s2.begin(), s2.end());
+bool issame(const string& s1, const string& s2) {
     return s1 == s2;
 }
 
@@ -9,12 +7,10 @@ vector<string> separate_paren_groups(string paren_string);
 int main() {
     string input;
     cin >> input;
-
     vector<string> groups = separate_paren_groups(input);
-    for (string group : groups) {
+    for (const string& group : groups) {
         cout << group << endl;
     }
-
     return 0;
 }
 
