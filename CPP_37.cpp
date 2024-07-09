88f7627bc7a88a20ad2716187acca9445f46df0d
beep boop <githubbot@vadim.me>
2024-07-09 08:21:27 +0200

    Solution 50 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/3b0ln6hy Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 9bdba816-2337-4431-9660-d6f2b1d204c0.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,17 +1,17 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 #include <initializer_list>
 
 bool isSameVectors(std::vector<float> a, std::vector<float> b) {
     return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
 }
 
-std::vector<float> sort_even(const std::vector<float>& l) {
-    std::vector<float> evenVals;
+std::vector<std::initializer_list<float>> sort_even(const std::vector<float>& l) {
+    std::vector<std::initializer_list<float>> evenVals;
     for (int j = 0; j < l.size(); j++) {
         if (j % 2 == 0) {
-            evenVals.push_back(l[j]);
+            evenVals.push_back({l[j]});
         }
     }
     std::sort(evenVals.begin(), evenVals.end());
@@ -20,26 +20,26 @@ std::vector<float> sort_even(const std::vector<float>& l) {
 
 int main() {
     std::vector<float> vec = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
-    std::vector<float> result(vec.size());
+    std::vector<std::initializer_list<float>> result(vec.size());
     for (int i = 0; i < vec.size(); i++) {
         if (i % 2 == 0) {
-            result[i] = sort_even(vec)[0];
+            result[i] = {{sort_even(vec)[0]}};
         } else {
-            result[i] = vec[i];
+            result[i] = {{vec[i]}};
         }
     }
     
-    std::vector<float> sortedEven = sort_even(vec);
+    std::vector<std::initializer_list<float>> sortedEven = sort_even(vec);
     for (int i = 0; i < vec.size(); i++) {
         if (i % 2 == 0) {
-            result[i] = sortedEven[0];
+            result[i] = {sortedEven[0]};
             sortedEven.erase(sortedEven.begin());
         } else {
-            result[i] = vec[i];
+            result[i] = {{vec[i]}};
         }
     }
     
     std::vector<float> expected = { -12, 8, 3, 4, 5, 2, 12, 11, 23, -10 };
-    assert(isSameVectors(sort_even(vec), expected));
+    assert(isSameVectors({float(x) for x in *result}, {float(x) for x in *expected}));
     return 0;
 }
