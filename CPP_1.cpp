397e722b63e2e63cc221a7111de2683958a97af8
beep boop <githubbot@vadim.me>
2024-07-09 12:34:35 +0200

    Solution 32 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/s92p6few Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 76ef00a4-98db-499b-a303-d73a6df0dc92.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 76ef00a4-98db-499b-a303-d73a6df0dc92.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 76ef00a4-98db-499b-a303-d73a6df0dc92.cpp:26: 76ef00a4-98db-499b-a303-d73a6df0dc92.cpp: In function ‘int main()’: 76ef00a4-98db-499b-a303-d73a6df0dc92.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 76ef00a4-98db-499b-a303-d73a6df0dc92.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,21 +1,28 @@
-vector<string> separate_paren_groups(string paren_string);
-
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
+bool issame(const vector<string>& a, const vector<string>& b) {
+    return a == b;
 }
 
-int main() {
-    // Include necessary header file where separate_paren_groups function is declared
+vector<string> separate_paren_groups(string paren_string) {
+    vector<string> result;
+    string current_group;
+    int open_count = 0;
 
-    return 0;
+    for (char c : paren_string) {
+        if (c == '(') {
+            if (open_count > 0) {
+                current_group += c;
+            }
+            open_count++;
+        } else if (c == ')') {
+            open_count--;
+            if (open_count >= 0) {
+                current_group += c;
+            } else {
+                result.push_back(current_group);
+                current_group = "";
+            }
+        }
+    }
+
+    return result;
 }
