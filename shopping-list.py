8900fbf08b9a0e3b27882a62e6f587337bc28a81
beep boop <githubbot@vadim.me>
2024-07-09 21:01:13 +0200

    Solution 12 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/2wsaf73z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ecd49398-3cc8-491e-a47e-8b77f8072 4d0.py", line 11, in <module>     print(shopping_list())           ^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ecd49398-3cc8-491e-a47e-8b77f8072 4d0.py", line 6, in shopping_list     discounts.append(int(input()) / 100)                      ^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: '60.79 22.48 98.36 45.36 32. 23 0.93 27.13 69.7 7.5 76.62 95.47 95.35 17.11 27.43 40.39 92.58 41.51'

--- shopping-list.py
+++ shopping-list.py
@@ -1,5 +1,6 @@
-n = int(input())
-prices = list(map(float, input().split()))
-discounts = list(map(int, input().split())) / 100
-total_price = sum(price - (price * discount) for price, discount in zip(prices, discounts))
-return '¤' + str(format(total_price, "#.2f"))
+def shopping_list():
+    n = int(input())
+    prices = list(map(float, input().split()))
+    discounts = list(map(float, input().split()))
+    total_price = sum(price - (price * discount) for price, discount in zip(prices, discounts))
+    return str(format(total_price, "#.2f"))
