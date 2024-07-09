4d7d11b11814db544dd3a814dd773978246452ea
beep boop <githubbot@vadim.me>
2024-07-09 11:12:03 +0200

    Solution 85 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/c8pse80p Prompt:       |                           ^~~~~~ 4bb4c5cd-0200-41b9-b4b0-488d295fed4d.cpp:38:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 4bb4c5cd-0200-41b9-b4b0-488d295fed4d.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 4bb4c5cd-0200-41b9-b4b0-488d295fed4d.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4bb4c5cd-0200-41b9-b4b0-488d295fed4d.cpp:38:34: error: expected primary-expressi on before ‘int’    38 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4bb4c5cd-0200-41b9-b4b0-488d295fed4d.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ 4bb4c5cd-0200-41b9-b4b0-488d295fed4d.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -6,26 +6,7 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> count_up_to(int n) {
-    std::vector<int> result;
-    if (n <= 1) {
-        return result;
-    }
-    
-    std::vector<bool> isPrime(n, true);
-    isPrime[0] = isPrime[1] = false;
-    
-    for (int i = 2; i < n; ++i) {
-        if (isPrime[i]) {
-            result.push_back(i);
-            for (int j = i * 2; j < n; j += i) {
-                isPrime[j] = false;
-            }
-        }
-    }
-    
-    return result;
-}
+std::vector<int> count_up_to(int n);
 
 std::vector<int> count_up_to(int n) {
     std::vector<int> result;
@@ -47,5 +28,3 @@ std::vector<int> count_up_to(int n) {
     
     return result;
 }
-
-assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
