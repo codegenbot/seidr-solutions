fec82910b64a4d65ac0e080c2e432daed725b6e0
beep boop <githubbot@vadim.me>
2024-07-09 13:14:53 +0200

    Solution 95 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/p70nshd0 Prompt:    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:16: f8e2b31f-ea39-4798-9646-bf202464bab2.cpp: In function ‘int main()’: f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:26:20: error: ‘sorted_list_sum’ was not  declared in this scope    26 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,15 +1,26 @@
-bool issame(string a, string b) {
-    if (a.length() % 2 == 0 && b.length() % 2 != 0 || a.length() % 2 != 0 && b.length() % 2 == 0)
-        return false;
-    else
-        return true;
+```
+bool areSortedListsEqual(const string& a, const string& b) {
+    vector<string> list1;
+    vector<string> list2;
+
+    for (int i = 0; i < a.length(); i++) {
+        list1.push_back(a.substr(i, 1));
+    }
+
+    for (int i = 0; i < b.length(); i++) {
+        list2.push_back(b.substr(i, 1));
+    }
+
+    sort(list1.begin(), list1.end());
+    sort(list2.begin(), list2.end());
+
+    return list1 == list2;
 }
 
 vector<string> sorted_list_sum(vector<string> lst) {
     auto it = unique(lst.begin(), lst.end(),
-                      [&lst](const string& a, const string& b) { 
-                          return !issame(a, b); 
-                      });
+                      [this](const string& a, const string& b) 
+                      { return areSortedListsEqual(a, b); });
     lst.erase(it, lst.end());
     sort(lst.begin(), lst.end(),
          [](const string& a, const string& b) {
