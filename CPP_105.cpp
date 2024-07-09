d995747317ced4e24fed0fa3c4f14c1a33c1165f
beep boop <githubbot@vadim.me>
2024-07-09 03:57:31 +0200

    Solution 12 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zvs0j795 Prompt:       |                              ^~~~~~ 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp:24:37: error: ‘string’ was not declared  in this scope    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp:23: 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp: In function ‘int main()’: 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp:33:20: error: ‘by_length’ was not decla red in this scope    33 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,43 +1,45 @@
-#include <vector>
+#include <map>
 #include <string>
-#include <algorithm>
-
-using namespace std;
-
-bool issame(vector<string> a,vector<string>b){
-    if(a.size() != b.size())return false;
-    for(int i=0;i<a.size();i++){
-        if(a[i] != b[i])return false;
-    }
-    return true;
-}
 
 vector<string> by_length(vector<int> arr) {
-    vector<string> temp;
-    for (int i : arr) {
-        if (i >= 1 && i <= 9)
-            temp.push_back(to_string(i));
-    }
-    sort(temp.begin(), temp.end());
-    reverse(temp.begin(), temp.end());
-    vector<string> result;
-    for (string s : temp) {
-        switch (stoi(s)) {
-            case 1: result.push_back("One"); break;
-            case 2: result.push_back("Two"); break;
-            case 3: result.push_back("Three"); break;
-            case 4: result.push_back("Four"); break;
-            case 5: result.push_back("Five"); break;
-            case 6: result.push_back("Six"); break;
-            case 7: result.push_back("Seven"); break;
-            case 8: result.push_back("Eight"); break;
-            case 9: result.push_back("Nine"); break;
+    vector<int> nums;
+    for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            nums.push_back(num);
         }
     }
+    
+    sort(nums.begin(), nums.end());
+    
+    reverse(nums.begin(), nums.end());
+    
+    vector<string> result;
+    map<int, string> digitName = {{1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
+    
+    for (int num : nums) {
+        if (!result.empty() && digitName.find(num) != digitName.end()) {
+            if (!issame(result.back(), digitName[num])) {
+                result.push_back("Error: Same Numbers");
+                break;
+            } else {
+                result.push_back(digitName[num]);
+            }
+        } else {
+            result.push_back(digitName[num]);
+        }
+    }
+    
     return result;
 }
 
-int main() {
-    assert(equal(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
-    return 0;
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
