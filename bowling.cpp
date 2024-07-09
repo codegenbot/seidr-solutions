982cd9304ecd45456af5eac3720f2202ffa91d16
beep boop <githubbot@vadim.me>
2024-07-09 06:36:30 +0200

    Solution 88 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/5yvq0zcc Prompt: f18bb42f-b3c2-4e57-82a9-3fd6abe34e2a.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ f18bb42f-b3c2-4e57-82a9-3fd6abe34e2a.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ f18bb42f-b3c2-4e57-82a9-3fd6abe34e2a.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ f18bb42f-b3c2-4e57-82a9-3fd6abe34e2a.cpp:10:23: error: ‘string’ is not a member of ‘std’    10 | int bowlingScore(std::string s) {       |                       ^~~~~~ f18bb42f-b3c2-4e57-82a9-3fd6abe34e2a.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | ```

--- bowling.cpp
+++ bowling.cpp
@@ -13,19 +13,31 @@ int bowlingScore(std::string s) {
     int score = 0;
     int currentFrame = 1;
     for(int i = 0; i < s.length(); i++) {
-        if(s[i] == '/') {
-            int firstRoll = (s[i-1] - '0');
-            int secondRoll = (s[i+1] - '0');
-            score += getBonus(firstRoll, secondRoll);
-            currentFrame++;
-        } else if(s[i] == 'X') {
+        if(s[i] == 'X') {
             score += 10;
+            if(currentFrame < 9) {
+                char nextChar = (i+2<s.length())?s[i+2]:'/'; // Check the length to avoid out-of-range
+                if(nextChar == '/') {
+                    score += getBonus(10, (s[i+1] - '0'));
+                    currentFrame++;
+                } else {
+                    score += 10 + (s[i+1] - '0');
+                    currentFrame+=2;
+                }
+            } else {
+                currentFrame++;
+            }
+        } else if(s[i] == '/') {
+            int firstRoll = (s[i-1] - '0');
+            score += getBonus(firstRoll, 10);
             currentFrame++;
         } else {
             int roll = s[i] - '0';
             score += roll;
             if(currentFrame < 10) {
-                if(i+1 < s.length() && s[i+1] != '/') {
+                if(i+2<s.length() && s[i+1] == '/') {
+                    currentFrame+=2;
+                } else if(i+1<s.length()) {
                     score += getBonus(roll, (s[i+1] - '0'));
                     currentFrame++;
                 } else {
@@ -35,3 +47,4 @@ int bowlingScore(std::string s) {
         }
     }
     return score;
+}
