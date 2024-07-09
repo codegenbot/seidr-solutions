af751f77327505d354cfb47c9fa5099106f8b698
beep boop <githubbot@vadim.me>
2024-07-09 06:46:34 +0200

    Solution 80 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/gekczo0y Prompt:       |                              ^~~~~~ c81d0175-f4ef-4281-944f-287c54240664.cpp:44:37: error: ‘string’ was not declared  in this scope    44 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c81d0175-f4ef-4281-944f-287c54240664.cpp:44:44: error: ‘b’ was not declared in t his scope    44 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c81d0175-f4ef-4281-944f-287c54240664.cpp:44:45: error: expression list treated a s compound expression in initializer [-fpermissive]    44 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c81d0175-f4ef-4281-944f-287c54240664.cpp:43: c81d0175-f4ef-4281-944f-287c54240664.cpp: In function ‘int main()’: c81d0175-f4ef-4281-944f-287c54240664.cpp:53:20: error: ‘by_length’ was not decla red in this scope    53 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ c81d0175-f4ef-4281-944f-287c54240664.cpp:53:19: error: ‘issame’ cannot be used a s a function    53 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,51 +1,10 @@
 #include <vector>
-#include <string>
+#include <algorithm>
 
-bool isSame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); i++)
-        if (a[i] != b[i])
-            return false;
-    return true;
+bool issame(vector<string> a,vector<string>b){
+    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
 }
 
-vector<string> by_length(vector<int> arr) {
-    vector<string> result;
-    
-    for (int i : arr) {
-        string s;
-        switch (i) {
-            case 1:
-                s = "One";
-                break;
-            case 2:
-                s = "Two";
-                break;
-            case 3:
-                s = "Three";
-                break;
-            case 4:
-                s = "Four";
-                break;
-            case 5:
-                s = "Five";
-                break;
-            case 6:
-                s = "Six";
-                break;
-            case 7:
-                s = "Seven";
-                break;
-            case 8:
-                s = "Eight";
-                break;
-            case 9:
-                s = "Nine";
-                break;
-        }
-        result.push_back(s);
-    }
-    
-    return result;
+int main() {
+    assert(issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));
 }
