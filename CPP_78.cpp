88d67081e44a0a98d40fe7111e139b19f71a92f7
beep boop <githubbot@vadim.me>
2024-07-09 09:14:43 +0200

    Solution 20 of CPP/78, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/6gnv14l3 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_78.cpp
+++ CPP_78.cpp
@@ -1,11 +1,23 @@
-int hex_key(string num) {
+#include <iostream>
+#include <string>
+
+int hex_key(std::string num) {
     int count = 0;
-    for (char c : num) {
-        if (c >= '2' && c <= '7') {
-            count++;
-        } else if (c == 'B' || c == 'D' || c == 'F') {
+    for (auto c : num) { 
+        if ((c >= '2' && c <= '7') || (c == 'B' || c == 'D' || c == 'F')){ 
             count++;
         }
     }
     return count;
+}
+
+int main_func() {
+    std::string num;
+    std::cout << "Enter a hexadecimal number: ";
+    std::cin >> num;
+    int result = hex_key(num);
+    if(result != -1) 
+        std::cout << "The number of characters in the range [2-7] or ['B', 'D', 'F'] is: " << result << std::endl;
+    else
+        std::cout << "Invalid input!" << std::endl; 
 }
