3ee215264c57a9429cd5b0028a9c83d75dd8a149
beep boop <githubbot@vadim.me>
2024-07-09 19:46:52 +0200

    Solution 94 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/vm0h1jt4 Prompt:       |                    ^~~ 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp:9: 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp: In function ‘int main()’: 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp:19:20: error: ‘make_a_pile’ was not dec lared in this scope    19 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,14 +1,10 @@
 #include <vector>
 
-vector<int> make_a_pile(int n); 
+vector<int> make_a_pile(int n);
 bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
+    if(a.size() != b.size()) return false;
+    for(int i=0; i<a.size(); ++i){
+        if(a[i] != b[i]) return false;
     }
     return true;
 }
