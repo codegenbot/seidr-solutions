55c7095faa7f548dba989da7346a5ae2d58d74d7
beep boop <githubbot@vadim.me>
2024-07-09 08:13:45 +0200

    Solution 48 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/1fe633cp Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8b7afa27-7ac4-4a8f-950c-902a49e03 613.py", line 10, in <module>     print(solve_boolean(input()))           ^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8b7afa27-7ac4-4a8f-950c-902a49e03 613.py", line 2, in solve_boolean     return eval(            ^^^^^   File "<string>", line 1, in <module> NameError: name 'tandtortortandforforforfandfandforfandtandf' is not defined

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,10 +1,7 @@
-def solve_boolean(expression):
-    return eval(
-        expression.replace("T", "True")
-        .replace("F", "False")
-        .replace("|", " or ")
-        .replace("&", " and ")
-    )
-
-
+solve_boolean = lambda expression: eval(
+    expression.replace("T", "True")
+    .replace("F", "False")
+    .replace("|", "or")
+    .replace("&", "and")
+)
 print(solve_boolean(input()))
