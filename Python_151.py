9b18bcff20440723c3613e1f90ae9f705114486e
beep boop <githubbot@vadim.me>
2024-07-09 08:26:02 +0200

    Solution 28 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4wqzhzx5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/272a4c78-afa2-4d40-881b-1d22b9f2c f07.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/272a4c78-afa2-4d40-881b-1d22b9f2c f07.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,2 +1,5 @@
-def double_the_difference(numbers):
-    return sum(i ** 2 for i in numbers if isinstance(i, int) and i > 0)
+def double_the_difference(lst):
+    positive_integers = [x**2 for x in lst if isinstance(x, int) and x >= 0]
+    total_sum = sum([x for x in lst if isinstance(x, int)])
+    odd_numbers_sum = sum([x for x in lst if isinstance(x, int) and x % 2 != 0])
+    return 2 * (total_sum - odd_numbers_sum) - sum(positive_integers)
