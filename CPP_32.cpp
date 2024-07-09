c621098e2da6a3c26c69e27caf805376e1c7579a
beep boop <githubbot@vadim.me>
2024-07-09 05:08:46 +0200

    Solution 6 of CPP/32, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/hdimg39k Prompt: Complete the following code given the task description and function signature.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,5 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+#include<vector>
+using namespace std;
+
+double poly(vector<double> xs, double x){
+    double sum=0;
+    int i;
+    for (i=0;i<xs.size();i++)
+    {
+        sum+=xs[i]*pow(x,i);
+    }
+    return sum;
+}
+
 double find_zero(vector<double> xs){
-    int n = xs.size();
-    double x = -xs[1]/(2*xs[0]);
-    return poly(xs,x);
+    double a = xs[0];
+    double b = 0.0;
+    double c = 0.0;
+
+    if(xs.size() > 1) b = xs[1];
+    if(xs.size() > 2) c = xs[2];
+
+    double d = b*b - 4*a*c;
+    
+    if(d < 0)
+        return -1; // No real roots
+    else if(d == 0)
+        return (-b)/(2*a);
+    else {
+        double t = sqrt(d);
+        return ( (-b+t) / (2*a) );
+    }
 }
