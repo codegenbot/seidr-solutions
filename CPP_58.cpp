0eb0b73fd84cf8a60745962a7f9ed5bb6beff9ac
beep boop <githubbot@vadim.me>
2024-07-09 04:06:44 +0200

    Solution 60 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tzm0q5ti Prompt:       |                    ^~~ a3f7549d-a957-4360-9554-2a0398f0c5e7.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a3f7549d-a957-4360-9554-2a0398f0c5e7.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a3f7549d-a957-4360-9554-2a0398f0c5e7.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a3f7549d-a957-4360-9554-2a0398f0c5e7.cpp:13: a3f7549d-a957-4360-9554-2a0398f0c5e7.cpp: In function ‘int main()’: a3f7549d-a957-4360-9554-2a0398f0c5e7.cpp:23:20: error: ‘common’ was not declared  in this scope    23 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ a3f7549d-a957-4360-9554-2a0398f0c5e7.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,13 +1,12 @@
 ```cpp
-#include <iostream>
 #include <vector>
 #include <set>
 
-bool issame(int a, int b) {
-    return (a == b);
+bool issame(vector<int>& a, vector<int>& b) {
+    return (a.size() == b.size()) && (a == b);
 }
 
-int common(vector<int> l1, vector<int> l2) {
+vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
@@ -15,22 +14,11 @@ int common(vector<int> l1, vector<int> l2) {
     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
                       inserter(intersection, intersection.begin()));
 
-    int count = 0;
-    for(int num: intersection) {
-        if(issame(count,0)) return num; // return the first common element
-        count++;
-    }
-    return -1;
+    vector<int> result(intersection.begin(), intersection.end());
+    return result;
 }
 
 int main() {
-    vector<int> l1 = {1,2,3};
-    vector<int> l2 = {2,4,5};
-    int result = common(l1, l2);
-    if(result != -1)
-        std::cout << "The first common element is: " << result << endl;
-    else
-        std::cout << "No common elements are found." << endl;
-
+    assert(issame(common({4, 3, 2, 8}, {}), {}));
     return 0;
 }
