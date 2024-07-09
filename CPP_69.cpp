69bcf5af8d4f6ad66f6aae951e2730d040ea06c0
beep boop <githubbot@vadim.me>
2024-07-09 23:43:17 +0200

    Solution 32 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/yrqa6kat Prompt: ffc8bb58-aca8-4885-8e98-e670f6d7ab25.cpp:1:12: error: ‘vector’ was not declared in this scope     1 | int search(vector<int> lst) {       |            ^~~~~~ ffc8bb58-aca8-4885-8e98-e670f6d7ab25.cpp:1:19: error: expected primary-expressio n before ‘int’     1 | int search(vector<int> lst) {       |                   ^~~ In file included from ffc8bb58-aca8-4885-8e98-e670f6d7ab25.cpp:11: ffc8bb58-aca8-4885-8e98-e670f6d7ab25.cpp: In function ‘int main()’: ffc8bb58-aca8-4885-8e98-e670f6d7ab25.cpp:13:19: error: ‘search’ cannot be used a s a function    13 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,13 +1,9 @@
 #include <vector>
-#include <algorithm>
-
-int search(vector<int> lst) {
+int search(std::vector<int> lst) {
     int max = -1;
     for (auto num : lst) {
         if (num > 0 && num <= count(lst.begin(), lst.end(), num)) {
             max = num;
-        } else if (max != -1) {
-            return max;
         }
     }
     return max;
