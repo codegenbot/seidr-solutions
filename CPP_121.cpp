5ceec2e36240477246ac4c52d6b1b9ea7aa48142
beep boop <githubbot@vadim.me>
2024-07-10 00:38:50 +0200

    Solution 76 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/4rblnted Prompt: 4e11a3c0-d2a5-4c99-8ec9-09ae7369df50.cpp:1:14: error: ‘vector’ was not declared in this scope     1 | int solution(vector<int> lst) {       |              ^~~~~~ 4e11a3c0-d2a5-4c99-8ec9-09ae7369df50.cpp:1:21: error: expected primary-expressio n before ‘int’     1 | int solution(vector<int> lst) {       |                     ^~~ In file included from 4e11a3c0-d2a5-4c99-8ec9-09ae7369df50.cpp:11: 4e11a3c0-d2a5-4c99-8ec9-09ae7369df50.cpp: In function ‘int main()’: 4e11a3c0-d2a5-4c99-8ec9-09ae7369df50.cpp:13:13: error: ‘solutions’ was not decla red in this scope; did you mean ‘solution’?    13 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -13,21 +13,18 @@ int solution(std::vector<int> lst) {
 
 int main() {
     std::vector<int> input;
-    int num;
-    
-    // Read number of elements
+    int n;
     cout << "Enter the number of elements: ";
-    cin >> num;
-
-    // Read the numbers from user
+    cin >> n;
+    
     cout << "Enter the elements: ";
-    for (int i = 0; i < num; i++) {
-        cin >> input.push_back();
+    for(int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        input.push_back(temp);
     }
-
+    
     int result = solution(input);
-    
-    cout << "Sum of odd elements is: " << result;
-    
+    cout << "Sum of odd numbers in the array is: " << result << endl;
     return 0;
 }
