0cca06498d05568341141905925e4fe68ee77b4e
beep boop <githubbot@vadim.me>
2024-07-09 04:06:33 +0200

    Solution 52 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/qhu264n1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/11f45279-164a-46fa-ad39-7dadd8056 e43.py", line 18, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/11f45279-164a-46fa-ad39-7dadd8056 e43.py", line 14, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple"     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/11f45279-164a-46fa-ad39-7dadd8056 e43.py", line 4, in fix_spaces     if char != " " or not previous_char_was_space:                           ^^^^^^^^^^^^^^^^^^^^^^^ UnboundLocalError: cannot access local variable 'previous_char_was_space' where it is not associated with a value

--- Python_140.py
+++ Python_140.py
@@ -1,10 +1,11 @@
 def fix_spaces(text):
-    previous_char_was_space = False
+    previous_char_was_space = False  
     result = ""
     for char in text:
         if char != " " or not previous_char_was_space:
             result += char
         else:
             result += "-"
+        global previous_char_was_space  
         previous_char_was_space = char == " "
     return result
