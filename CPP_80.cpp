8c05d356b588e55dad0b95b353ca8c12d26cb15b
beep boop <githubbot@vadim.me>
2024-07-09 19:11:51 +0200

    Solution 60 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g4d79e6v Prompt: 3d688c43-ad40-4d5c-ad45-614a0558e389.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 3d688c43-ad40-4d5c-ad45-614a0558e389.cpp:6:5: note: ‘int main()’ previously defi ned here     6 | int main() {       |     ^~~~ In file included from 3d688c43-ad40-4d5c-ad45-614a0558e389.cpp:18: 3d688c43-ad40-4d5c-ad45-614a0558e389.cpp: In function ‘int main()’: 3d688c43-ad40-4d5c-ad45-614a0558e389.cpp:20:22: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    20 |     assert (is_happy("iopaxioi") == false );       |                      ^~~~~~~~~~       |                      |       |                      const char* 3d688c43-ad40-4d5c-ad45-614a0558e389.cpp:4:19: note:   initializing argument 1 o f ‘bool is_happy(int)’     4 | bool is_happy(int n);       |               ~~~~^

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -13,4 +13,21 @@ int main() {
     } else {
         std::cout << num << " is not a happy number.\n";
     }
+    return 0;
+}
+
+bool is_happy(int n) {
+    if (n < 1)
+        return false;
+    while (n != 1) {
+        int sum = 0;
+        for (int i = 0; i <= to_string(n).size(); i++) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        if (sum < 1)
+            return false;
+        n = sum;
+    }
+    return true;
 }
