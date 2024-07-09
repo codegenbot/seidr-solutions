e5d20d2a26df6ad5c81cf35af5f43d26e72149bc
beep boop <githubbot@vadim.me>
2024-07-09 06:55:40 +0200

    Solution 92 of CPP/159, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/8g3cj67y Prompt: b437a350-a8d0-4426-8932-9a7ed5341527.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ b437a350-a8d0-4426-8932-9a7ed5341527.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -14,9 +14,3 @@ vector<int> eat(int number, int need, int remaining) {
 bool compare_vectors(vector<int> v1, vector<int> v2) {
     return v1 == v2;
 }
-
-int main() {
-    assert(compare_vectors(eat(4, 5, 1), vector<int>{5, 0}));
-
-    return 0;
-}
