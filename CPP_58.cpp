ac15137bb58c7bc86906f10ad373304b7b4e65fd
beep boop <githubbot@vadim.me>
2024-07-09 03:54:06 +0200

    Solution 48 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tzm0q5ti Prompt:       |                    ^~~ 7b495c80-3b0e-453e-8936-8653f72671c2.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7b495c80-3b0e-453e-8936-8653f72671c2.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7b495c80-3b0e-453e-8936-8653f72671c2.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7b495c80-3b0e-453e-8936-8653f72671c2.cpp:13: 7b495c80-3b0e-453e-8936-8653f72671c2.cpp: In function ‘int main()’: 7b495c80-3b0e-453e-8936-8653f72671c2.cpp:23:20: error: ‘common’ was not declared  in this scope    23 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 7b495c80-3b0e-453e-8936-8653f72671c2.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,31 +1,14 @@
 #include <vector>
-#include <algorithm>
+#include <set>
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
+std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
+    std::set<int> s1(l1.begin(), l1.end());
+    std::set<int> s2(l2.begin(), l2.end());
 
-int main() {
-    vector<int> l1 = {1, 2, 3};
-    vector<int> l2 = {1, 2, 3};
-
-    if(issame(l1,l2)) {
-        vector<int> common;
-        set<int> s1(l1.begin(), l1.end());
-        set<int> s2(l2.begin(), l2.end());
-
-        set<int> intersection;
-        set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+    std::set<int> intersection;
+    std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
                           inserter(intersection, intersection.begin()));
 
-        common.assign(intersection.begin(), intersection.end());
-        cout << "Common elements are: ";
-        for(int i : common) {
-            cout << i << " ";
-        }
-    } else {
-        cout << "The two vectors are not same";
-    }
-
-    return 0;
+    std::vector<int> result(intersection.begin(), intersection.end());
+    return result;
 }
