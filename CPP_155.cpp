07a2889007d3368a8381a2b103c57fd6bff1882d
beep boop <githubbot@vadim.me>
2024-07-09 02:21:55 +0200

    Solution 2 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fkq142tr Prompt:       |                    ^~~ afac728b-a9ad-4a1c-86a3-8face04dbc0b.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ afac728b-a9ad-4a1c-86a3-8face04dbc0b.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ afac728b-a9ad-4a1c-86a3-8face04dbc0b.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from afac728b-a9ad-4a1c-86a3-8face04dbc0b.cpp:16: afac728b-a9ad-4a1c-86a3-8face04dbc0b.cpp: In function ‘int main()’: afac728b-a9ad-4a1c-86a3-8face04dbc0b.cpp:26:20: error: ‘even_odd_count’ was not declared in this scope    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ afac728b-a9ad-4a1c-86a3-8face04dbc0b.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,7 +1,7 @@
-Here is the solution:
+#include <vector>
 
-vector<int> even_odd_count(int num) {
-    vector<int> result(2, 0);
+std::vector<int> even_odd_count(int num) {
+    std::vector<int> result(2, 0);
     while (num != 0) {
         int digit = num % 10;
         if (digit % 2 == 0)
