953db18601f2b44b59a23e26803fe26728bb80f6
beep boop <githubbot@vadim.me>
2024-07-09 09:25:19 +0200

    Solution 35 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/4i26dq2n Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 01a2ac1f-e62f-4c24-aab6-a4bed88987dc.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,7 +1,7 @@
-```cpp
 #include <vector>
 #include <string>
 #include <iostream>
+#include <initializer_list>
 
 using namespace std;
 
@@ -9,59 +9,43 @@ bool same(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-vector<string> totalMatch(vector<vector<string>> lst1, vector<vector<string>> lst2) {
+vector<string> totalMatch(vector<string> lst1, vector<string> lst2) {
     int sum1 = 0;
-    for (const auto& v : lst1) {
-        for (const string& s : v) {
-            sum1 += s.length();
-        }
+    for (const string& s : lst1) {
+        sum1 += s.length();
     }
     int sum2 = 0;
-    for (const auto& v : lst2) {
-        for (const string& s : v) {
-            sum2 += s.length();
-        }
+    for (const string& s : lst2) {
+        sum2 += s.length();
     }
 
-    if (sum1 < sum2) return vector<string>(lst1.begin(), lst1.end());
-    else if (sum1 > sum2) return vector<string>(lst2.begin(), lst2.end());
+    if (sum1 < sum2) return lst1;
+    else if (sum1 > sum2) return lst2;
 
     if (same(lst1, lst2)) {
-        return vector<string>(lst1.begin(), lst1.end());
+        return lst1;
     }
     else if (!lst1.empty()) {
-        for (const auto& v : lst2) {
+        for (const string& s : lst2) {
             bool found = false;
-            for (const string& s : v) {
-                bool foundInList1 = false;
-                for (const auto& t : lst1) {
-                    for (const string& u : t) {
-                        if (s == u) {
-                            foundInList1 = true;
-                            break;
-                        }
-                    }
-                    if (foundInList1) break;
+            for (const string& t : lst1) {
+                if (s == t) {
+                    found = true;
+                    break;
                 }
-                if (!foundInList1) return vector<string>(lst1.begin(), lst1.end());
             }
+            if (!found) return lst1;
         }
     } else {
-        for (const auto& v : lst2) {
+        for (const string& s : lst2) {
             bool found = false;
-            for (const string& s : v) {
-                bool foundInList1 = false;
-                for (const auto& t : lst1) {
-                    for (const string& u : t) {
-                        if (s == u) {
-                            foundInList1 = true;
-                            break;
-                        }
-                    }
-                    if (foundInList1) break;
+            for (const string& t : lst1) {
+                if (s == t) {
+                    found = true;
+                    break;
                 }
-                if (!foundInList1) return vector<string>(lst2.begin(), lst2.end());
             }
+            if (!found) return lst2;
         }
     }
 
@@ -69,12 +53,12 @@ vector<string> totalMatch(vector<vector<string>> lst1, vector<vector<string>> ls
 }
 
 int main() {  
-    vector<vector<string>> result = totalMatch({{{"this"}}}, {{{}}, {{}}});
+    vector<string> lst1 = {"this", "is", "a", "test"};
+    vector<string> lst2;
+    vector<string> result = totalMatch(lst1, lst2);
     cout << "Result: ";
-    for (const auto& v : result) {
-        for (const string& s : v) {
-            cout << s << " ";
-        }
+    for (const string& s : result) {
+        cout << s << " ";
     }
     cout << endl;
     return 0; 
