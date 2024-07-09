3838370276457e10b699f0dbc51d0d290f3ce36e
beep boop <githubbot@vadim.me>
2024-07-09 16:06:36 +0200

    Solution 12 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kr1u3dhc Prompt: this scope     6 |     cout << "Anti-Shuffled String: " << res << endl;       |                                         ^~~ 754c65e6-9e03-4ee2-995b-546debe9d6dd.cpp:6:48: error: ‘endl’ was not declared in  this scope     6 |     cout << "Anti-Shuffled String: " << res << endl;       |                                                ^~~~ 754c65e6-9e03-4ee2-995b-546debe9d6dd.cpp: At global scope: 754c65e6-9e03-4ee2-995b-546debe9d6dd.cpp:11:5: error: redefinition of ‘int main( )’    11 | int main(){       |     ^~~~ 754c65e6-9e03-4ee2-995b-546debe9d6dd.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from 754c65e6-9e03-4ee2-995b-546debe9d6dd.cpp:10: 754c65e6-9e03-4ee2-995b-546debe9d6dd.cpp: In function ‘int main()’: 754c65e6-9e03-4ee2-995b-546debe9d6dd.cpp:12:13: error: ‘anti_shuffle’ was not de clared in this scope    12 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <string>
 
-std::string anti_shuffle(std::string s) {
-    // your implementation here
-}
+std::string anti_shuffle(std::string str);
 
 int main() {
     std::string str;
     std::cout << "Enter a string: ";
     std::cin >> str;
     std::string res = anti_shuffle(str);
-    std::cout << "Anti-Shuffled String: " << res << std::endl;
+    std::cout << "Anti-Shuffled String: " << res << std::endl; 
     return 0;
+}
