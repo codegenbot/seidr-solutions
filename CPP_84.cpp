0304759834e34740d7d339becfa65f2d03c86ed9
beep boop <githubbot@vadim.me>
2024-07-09 17:01:35 +0200

    Solution 64 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/155pyajg Prompt: 82f8110d-c40b-443c-8b2f-761077417e72.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 82f8110d-c40b-443c-8b2f-761077417e72.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from 82f8110d-c40b-443c-8b2f-761077417e72.cpp:27: 82f8110d-c40b-443c-8b2f-761077417e72.cpp: In function ‘int main()’: 82f8110d-c40b-443c-8b2f-761077417e72.cpp:29:24: error: ISO C++ forbids compariso n between pointer and integer [-fpermissive]    29 |     assert (solve(963) == "10010");       |             ~~~~~~~~~~~^~~~~~~~~~

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,19 +1,3 @@
-#include <cassert>
-#include <iostream>
-#include <string>
-
-int solve(int n) {
-    std::string binary = "";
-    while(n > 0) {
-        if(n % 2 == 0)
-            binary = "0" + binary;
-        else
-            binary = "1" + binary;
-        n /= 2;
-    }
-    return binary.length() == 0 ? -1 : std::stoi(binary, 0, 2);
-}
-
 int main() {
     int expected = solve(963); 
     assert (solve(963) == 18); 
