cc85fe95e22e440da0c4bbc9a0ca81b81735ebda
beep boop <githubbot@vadim.me>
2024-07-09 02:26:07 +0200

    Solution 15 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h3blrigf Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 052c8f10-673d-4678-86ab-dc451414cb03.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -3,14 +3,14 @@
 #include <vector>
 #include <string>
 
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b){
     return a == b;
 }
 
-std::vector<std::string> sorted_list_sum(const std::vector<std::string>& lst) {
-    auto it = std::remove_if(lst.begin(), lst.end(), [&](const std::string& s){ return s.length() % 2; });
+std::vector<std::string> sorted_list_sum(std::vector<std::string> lst) {
+    auto it = remove_if(lst.begin(), lst.end(), [](const std::string& s){ return s.length() % 2; });
     lst.erase(it, lst.end());
-    std::sort(lst.begin(), lst.end(), 
+    sort(lst.begin(), lst.end(), 
         [](const std::string& a, const std::string& b){
             if(a.length() != b.length()) {
                 return a.length() < b.length();
