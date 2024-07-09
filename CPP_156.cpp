3d8f2f2e2091e1160f0989836a34347c6608151d
beep boop <githubbot@vadim.me>
2024-07-09 02:19:15 +0200

    Solution 17 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/noyz23n0 Prompt: 70133386-4761-4a8f-9c44-9d5725cd642c.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 70133386-4761-4a8f-9c44-9d5725cd642c.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 70133386-4761-4a8f-9c44-9d5725cd642c.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 70133386-4761-4a8f-9c44-9d5725cd642c.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ In file included from 70133386-4761-4a8f-9c44-9d5725cd642c.cpp:15: 70133386-4761-4a8f-9c44-9d5725cd642c.cpp: In function ‘int main()’: 70133386-4761-4a8f-9c44-9d5725cd642c.cpp:17:13: error: ‘int_to_mini_romank’ was not declared in this scope    17 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,8 +1,10 @@
+```cpp
 #include <vector>
 #include <string>
 
 std::string int_to_mini_romank(int number) {
-    vector< pair<int, char> > romans = {{1000,'M'},{900,'C'},{500,'D'},{400,'C'},{100,'C'},{90,'X'},{50,'L'},{40,'X'},{10,'X'},{9,'I'},{5,'V'},{4,'I'},{1,'I'}};
+    std::vector<std::pair<int, char>> romans = {{1000,'M'},{900,'C'},{500,'D'},{400,'C'},{100,'C'},{90,'X'},
+                                         {50,'L'},{40,'X'},{10,'X'},{9,'I'},{5,'V'},{4,'I'},{1,'I'}};
     std::string roman = "";
     for (auto p : romans) {
         while (number >= p.first) {
