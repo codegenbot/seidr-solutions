ab2cf4a8b9e571004f8c118240cc9c553d53baab
beep boop <githubbot@vadim.me>
2024-07-09 13:25:04 +0200

    Solution 76 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/mvcgxdwn Prompt:       |                              ^~~~~~ a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp:25: a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp: In function ‘int main()’: a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp:35:20: error: ‘reverse_delete’ was not declared in this scope    35 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ a090b341-0a5d-4c81-9a02-d3e94d86c696.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -6,8 +6,8 @@ bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-std::vector<std::string> reverse_delete(std::string s, std::string c) {
-    std::string result = "";
+vector<string> reverse_delete(string s, string c) {
+    string result = "";
     for (char ch : s) {
         bool found = false;
         for (char cc : c) {
@@ -20,11 +20,11 @@ std::vector<std::string> reverse_delete(std::string s, std::string c) {
             result += ch;
         }
     }
-    std::string rev = result;
-    std::reverse(rev.begin(), rev.end());
-    std::vector<std::string> res;
+    string rev = result;
+    reverse(rev.begin(), rev.end());
+    vector<string> res;
     res.push_back(result);
-    res.push_back(std::to_string(result == rev));
+    res.push_back(to_string(result == rev));
     return res;
 }
 
