2f27987e3afa02ca4909196ddbbd1fdb5fa8009b
beep boop <githubbot@vadim.me>
2024-07-09 19:15:24 +0200

    Solution 63 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/n9bn7yqf Prompt: eaf6d088-92db-4363-8a60-92ae7bbf8178.cpp:2:1: error: ‘size_t’ does not name a ty pe     2 | size_t pos = 0;       | ^~~~~~ eaf6d088-92db-4363-8a60-92ae7bbf8178.cpp:1:1: note: ‘size_t’ is defined in heade r ‘<cstddef>’; did you forget to ‘#include <cstddef>’?   +++ |+#include <cstddef>     1 | ``` eaf6d088-92db-4363-8a60-92ae7bbf8178.cpp:3:1: error: expected unqualified-id bef ore ‘while’     3 | while ((pos = str.find(sep)) != string::npos) {       | ^~~~~ eaf6d088-92db-4363-8a60-92ae7bbf8178.cpp:7:1: error: ‘tokens’ does not name a ty pe     7 | tokens.push_back(str);       | ^~~~~~ In file included from eaf6d088-92db-4363-8a60-92ae7bbf8178.cpp:9: eaf6d088-92db-4363-8a60-92ae7bbf8178.cpp: In function ‘int main()’: eaf6d088-92db-4363-8a60-92ae7bbf8178.cpp:11:13: error: ‘anti_shuffle’ was not de clared in this scope    11 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -2,16 +2,11 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> antiShuffle(const std::string& str, char sep) {
+void anti_shuffle(const std::string& str, char sep) {
     size_t pos = 0;
-    std::vector<std::string> tokens;
-    
     while ((pos = str.find(sep)) != std::string::npos) {
         tokens.push_back(str.substr(0, pos));
         str = str.substr(pos + 1);
     }
-    
     tokens.push_back(str);
-    
-    return tokens;
 }
