9b789d2438dc6670f80e76856643107031633555
beep boop <githubbot@vadim.me>
2024-07-10 01:18:03 +0200

    Solution 97 of CPP/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/1fgh7thh Prompt: 2f873da5-4bd3-4586-88fb-d0d832aa1d86.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string fix_spaces(string text){       | ^~~~~~ In file included from 2f873da5-4bd3-4586-88fb-d0d832aa1d86.cpp:24: 2f873da5-4bd3-4586-88fb-d0d832aa1d86.cpp: In function ‘int main()’: 2f873da5-4bd3-4586-88fb-d0d832aa1d86.cpp:26:13: error: ‘fix_spaces’ was not decl ared in this scope    26 |     assert (fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple");       |             ^~~~~~~~~~

--- CPP_140.cpp
+++ CPP_140.cpp
@@ -1,6 +1,6 @@
 #include <string>
 
-string fix_spaces(string text){
+string fix_spaces(string text) {
     string result = "";
     for(int i=0; i < text.length(); i++){
         if(text[i] == ' ' && (i==0 || text[i-1] != ' ') && (i==text.length()-1 || text[i+1] != ' ')){
