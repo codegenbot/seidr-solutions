7ac1ee700e1a8d9f7cbc4786eba6d5b0c14f74f6
beep boop <githubbot@vadim.me>
2024-07-09 02:21:33 +0200

    Solution 4 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g63su6zt Prompt:       |                         ^~~~~~ f0b74e30-0668-468a-b711-0892fb7cf3ad.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ f0b74e30-0668-468a-b711-0892fb7cf3ad.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ f0b74e30-0668-468a-b711-0892fb7cf3ad.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ f0b74e30-0668-468a-b711-0892fb7cf3ad.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from f0b74e30-0668-468a-b711-0892fb7cf3ad.cpp:22: f0b74e30-0668-468a-b711-0892fb7cf3ad.cpp: In function ‘int main()’: f0b74e30-0668-468a-b711-0892fb7cf3ad.cpp:24:32: error: ‘Strongest_Extension’ can not be used as a function    24 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,9 +1,8 @@
-```cpp
+#include <iostream>
 #include <vector>
 #include <string>
-using namespace std;
 
-int Strongest_Extension(string class_name,vector<string> extensions){
+int Strongest_Extension(std::string class_name, std::vector<std::string> extensions){
     int max_strength = -1;
     string strongest_extension;
 
@@ -22,4 +21,9 @@ int Strongest_Extension(string class_name,vector<string> extensions){
     }
 
     return class_name + "." + strongest_extension;
+}
+
+int main() {
+    assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
+    return 0;
 }
