4a485fd1e660e8eb4ed5670577c5b42638010ad8
beep boop <githubbot@vadim.me>
2024-07-09 19:14:56 +0200

    Solution 8 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/neid1i7l Prompt:       |                                 ^~~~~~ In file included from b77f0d0d-517f-4d11-9d1c-fea0cc0d9af0.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ b77f0d0d-517f-4d11-9d1c-fea0cc0d9af0.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ b77f0d0d-517f-4d11-9d1c-fea0cc0d9af0.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from b77f0d0d-517f-4d11-9d1c-fea0cc0d9af0.cpp:32: b77f0d0d-517f-4d11-9d1c-fea0cc0d9af0.cpp: In function ‘int main()’: b77f0d0d-517f-4d11-9d1c-fea0cc0d9af0.cpp:42:20: error: ‘by_length’ was not decla red in this scope    42 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ b77f0d0d-517f-4d11-9d1c-fea0cc0d9af0.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,24 +1,17 @@
 #include <vector>
-#include <string>
+#include <algorithm>
 
-bool issame(vector<string> a, vector<string> b) {
-    // Compare contents of two vectors
-    return (a == b);
+bool issame(vector<string> a, vector<string>b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
 }
 
 vector<string> by_length(vector<int> arr) {
-    vector<int> temp;
-    for (int i : arr) {
-        if (i >= 1 && i <= 9) {
-            temp.push_back(i);
-        }
-    }
-
-    sort(temp.begin(), temp.end());
-    reverse(temp.begin(), temp.end());
-
     vector<string> result;
-    for (int i : temp) {
+    for (int i : arr) {
         switch (i) {
             case 1: result.push_back("One"); break;
             case 2: result.push_back("Two"); break;
