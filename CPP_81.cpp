23e2d960c584aa54c0dff0a82ec9c31fa9c359df
beep boop <githubbot@vadim.me>
2024-07-09 09:44:07 +0200

    Solution 38 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/b00mghv0 Prompt: 9e5188cb-bbad-4278-bab3-b4479937c498.cpp:52:5: error: redefinition of ‘int main( )’    52 | int main(){       |     ^~~~ 9e5188cb-bbad-4278-bab3-b4479937c498.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~ In file included from 9e5188cb-bbad-4278-bab3-b4479937c498.cpp:43: 9e5188cb-bbad-4278-bab3-b4479937c498.cpp: In function ‘int main()’: 9e5188cb-bbad-4278-bab3-b4479937c498.cpp:53:42: error: could not convert ‘{0, 6. 9999999999999996e-1}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector<s td::__cxx11::basic_string<char> >’    53 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~       |                                          |       |                                          <brace-enclosed initializer lis t>

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,6 +1,6 @@
+```
 #include <iostream>
 #include <vector>
-#include <string>
 
 using namespace std;
 
@@ -32,9 +32,9 @@ vector<string> numerical_letter_grade(vector<string> grades) {
 
 int main() {
     vector<string> grades1 = {"0.0", "0.7"};
-    vector<string> result1 = numerical_letter_grade(vector<string>{grades1[0], grades1[1]});
-    
-    if (checkResults(result1, vector<string>{"F", "D+"})) {
+    vector<string> result1 = numerical_letter_grade(grades1);
+
+    if (checkResults(result1, grades1)) {
         cout << "The two vectors are the same." << endl;
     } else {
         cout << "The two vectors are not the same." << endl;
