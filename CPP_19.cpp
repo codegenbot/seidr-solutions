a0e770371ad2c3f93db9bb248739ff48220ce1c7
beep boop <githubbot@vadim.me>
2024-07-09 11:13:23 +0200

    Solution 5 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qjcuo56w Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,30 +1,37 @@
 #include <algorithm>
+#include <string>
 using namespace std;
 
 string sort_numbers(string numbers) {
-    map<string, int> numberMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                                   {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                                   {"eight", 8}, {"nine", 9}};
+    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
+                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
+                                {"eight", 8}, {"nine", 9}};
 
-    vector<string> numVector;
-    string temp = "";
-    for (char c : numbers) {
-        if (c == ' ') {
-            numVector.push_back(temp);
-            temp = "";
-        } else {
-            temp += c;
+    vector<string> numVec;
+    string temp;
+
+    for (int i = 0; i < numbers.length(); i++) {
+        if (numbers[i] == ' ') {
+            continue;
+        }
+        temp += numbers[i];
+        for (auto it = numMap.begin(); it != numMap.end(); it++) {
+            if (temp == it->first) {
+                numVec.push_back(temp);
+                temp = "";
+                break;
+            }
         }
     }
-    numVector.push_back(temp);
 
-    sort(numVector.begin(), numVector.end(), [&numberMap](string a, string b) {
-        return numberMap.at(a) < numberMap.at(b);
-    });
+    sort(numVec.begin(), numVec.end());
 
     string result = "";
-    for (string s : numVector) {
-        result += s + " ";
+    for (int i = 0; i < numVec.size(); i++) {
+        result += numVec[i];
+        if (i != numVec.size() - 1) {
+            result += " ";
+        }
     }
 
     return result;
