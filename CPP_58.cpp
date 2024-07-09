37c6b8d9fe3eac37d7ccdb15748e6215f041c4f8
beep boop <githubbot@vadim.me>
2024-07-09 21:28:52 +0200

    Solution 37 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g34sr5ik Prompt:       |     ^~~~~~ afc3974e-20db-484d-be62-d8c1b0231cad.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | using namespace std; afc3974e-20db-484d-be62-d8c1b0231cad.cpp: At global scope: afc3974e-20db-484d-be62-d8c1b0231cad.cpp:25:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ afc3974e-20db-484d-be62-d8c1b0231cad.cpp:4:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     4 | bool issame(vector<int> a,vector<int> b){       |      ^~~~~~ afc3974e-20db-484d-be62-d8c1b0231cad.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ afc3974e-20db-484d-be62-d8c1b0231cad.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,12 +1,28 @@
-int main() {
-    vector<int> l1 = {4, 3, 2, 8};
-    vector<int> l2 = {4, 5, 6};
+#include <vector>
+#include <cassert>
 
-    vector<int> result = common(l1, l2);
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
+vector<int> common(vector<int> l1, vector<int> l2) {
+    set<int> s1(l1.begin(), l1.end());
+    set<int> s2(l2.begin(), l2.end());
+    set<int> intersection;
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+        inserter(intersection, intersection.begin()));
+    vector<int> result(intersection.begin(), intersection.end());
+    sort(result.begin(), result.end());
+    return result;
+}
+
+int main() {
+    assert(issame(common({4, 3, 2, 8}, {}), {}));
+    vector<int> list1 = {1, 2, 3};
+    vector<int> list2 = {1, 2, 4};
     
-    for (auto i : result) {
-        cout << i << " ";
-    }
-    
+    common(list1, list2);
     return 0;
 }
