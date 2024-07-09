f7d92068d08f1af013a1cc48e8a32f80ecb961e8
beep boop <githubbot@vadim.me>
2024-07-09 11:06:06 +0200

    Solution 48 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/owo9prrk Prompt: In file included from /usr/include/c++/11/string:39,                  from 0b16acfd-dff5-4367-b505-de5fc44a4177.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 0b16acfd-dff5-4367-b505-de5fc44a4177.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 0b16acfd-dff5-4367-b505-de5fc44a4177.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 0b16acfd-dff5-4367-b505-de5fc44a4177.cpp:19: 0b16acfd-dff5-4367-b505-de5fc44a4177.cpp: In function ‘int main()’: 0b16acfd-dff5-4367-b505-de5fc44a4177.cpp:29:20: error: ‘reverse_delete’ was not declared in this scope; did you mean ‘timer_delete’?    29 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 0b16acfd-dff5-4367-b505-de5fc44a4177.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -3,11 +3,11 @@
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b){
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
+
+vector<string> reverse_delete(string s, string c);
 
 vector<string> reverse_delete(string s, string c){
     string result = "";
@@ -21,7 +21,7 @@ vector<string> reverse_delete(string s, string c){
     return {result, issame({result, result_reversed}) ? "True" : "False"};
 }
 
-int main(){
-    assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
+int main() {
+    assert(issame(reverse_delete("mamma", "mia"), { "", "True" }));
     return 0;
 }
