d25af407469aaf4283ee364de4c298b8bbea23e6
beep boop <githubbot@vadim.me>
2024-07-09 14:44:04 +0200

    Solution 29 of CPP/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ynlb1ucq Prompt: 1d505aaf-46aa-4239-8838-a888c30d85ad.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 1d505aaf-46aa-4239-8838-a888c30d85ad.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 1d505aaf-46aa-4239-8838-a888c30d85ad.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 1d505aaf-46aa-4239-8838-a888c30d85ad.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 1d505aaf-46aa-4239-8838-a888c30d85ad.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_36.cpp
+++ CPP_36.cpp
@@ -1,12 +1,6 @@
+```
 #include <iostream>
 using namespace std;
-
-int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    cout << "The result is: " << fizz_buzz(n);
-}
 
 int fizz_buzz(int n) {
     int count = 0;
@@ -15,4 +9,12 @@ int fizz_buzz(int n) {
             count++;
     }
     return count;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "The result is: " << fizz_buzz(n);
+    return 0;
 }
