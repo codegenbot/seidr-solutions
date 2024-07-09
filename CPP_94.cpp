915e257972e12cf12814801aa4fec671a633abed
beep boop <githubbot@vadim.me>
2024-07-09 08:05:37 +0200

    Solution 31 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/qeru7xja Prompt: 1d80e441-b24d-4747-8410-3f1788e76489.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 1d80e441-b24d-4747-8410-3f1788e76489.cpp:29: 1d80e441-b24d-4747-8410-3f1788e76489.cpp: In function ‘int main()’: 1d80e441-b24d-4747-8410-3f1788e76489.cpp:31:13: error: ‘skjkasdkd’ was not decla red in this scope    31 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ^~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,42 +1,12 @@
-```cpp
 #include<bits/stdc++.h>
 using namespace std;
 
-int skjkasdkd(vector<int> lst){
-    int maxPrime = 0;
-    for(int num : lst) {
-        if(isPrime(num)) {
-            if(num > maxPrime)
-                maxPrime = num;
-        }
-    }
-    int sumOfDigits = 0;
-    while(maxPrime > 0) {
-        sumOfDigits += maxPrime % 10;
-        maxPrime /= 10;
-    }
-    return sumOfDigits;
-}
-
-bool isPrime(int n) {
-    if(n <= 1)
-        return false;
-    for(int i = 2; i * i <= n; i++) {
-        if(n % i == 0)
-            return false;
-    }
-    return true;
-
 int main() {
-    vector<int> lst;
+    vector<int> lst; 
     int num;
-    cout<<"Enter number of elements: ";
-    cin>>num;
-    cout<<"Enter "<<num<<" elements:" <<endl;
-    for(int i = 1; i <= num; i++) {
-        cin >> num;
+    while(cin >> num) {
         lst.push_back(num);
     }
-    cout<<"Sum of digits is: "<<skjkasdkd(lst)<<endl;
+    cout << skjkasdkd(lst) << endl;
     return 0;
 }
