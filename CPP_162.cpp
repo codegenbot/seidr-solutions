24f52c4ef41816dd3c92e1bebf3047c704d6043d
beep boop <githubbot@vadim.me>
2024-07-10 01:20:18 +0200

    Solution 78 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/jay8p6s3 Prompt: dd6d0ebc-5bfa-43e4-96e0-59f590cfe137.cpp: In function ‘std::string string_to_md5 (std::string)’: dd6d0ebc-5bfa-43e4-96e0-59f590cfe137.cpp:12:5: error: ‘MD5_CTX’ was not declared  in this scope    12 |     MD5_CTX ctx;       |     ^~~~~~~ dd6d0ebc-5bfa-43e4-96e0-59f590cfe137.cpp:13:15: error: ‘ctx’ was not declared in  this scope    13 |     MD5_Init(&ctx);       |               ^~~ dd6d0ebc-5bfa-43e4-96e0-59f590cfe137.cpp:13:5: error: ‘MD5_Init’ was not declare d in this scope    13 |     MD5_Init(&ctx);       |     ^~~~~~~~ dd6d0ebc-5bfa-43e4-96e0-59f590cfe137.cpp:15:5: error: ‘MD5_Update’ was not decla red in this scope    15 |     MD5_Update(&ctx, cstr, text.size());       |     ^~~~~~~~~~ dd6d0ebc-5bfa-43e4-96e0-59f590cfe137.cpp:16:5: error: ‘MD5_Final’ was not declar ed in this scope    16 |     MD5_Final(md5, &ctx);       |     ^~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,7 +1,8 @@
-#include <openssl/md5.h>
+```
 #include <string>
 #include <iomanip>
 #include <sstream>
+#include <openssl/md5.h>
 
 using namespace std;
 
@@ -14,7 +15,7 @@ string string_to_md5(string text) {
     md5_context ctx;
     MD5_CTX_init(&ctx);
     const char* cstr = text.c_str();
-    MD5_CTX_update(&ctx, (const uint8_t*)cstr, text.size());
+    MD5_CTX_update(&ctx, (unsigned char*)cstr, text.size());
     MD5_CTX_finish(&ctx, md5);
 
     ostringstream oss;
