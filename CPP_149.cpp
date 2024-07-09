a3e1e28d326ef1a4f7226d39ebe59f91f80f0383
beep boop <githubbot@vadim.me>
2024-07-09 03:08:54 +0200

    Solution 66 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h3blrigf Prompt:    23 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 671f158f-c11a-4f61-8a1c-b58c83414db1.cpp:31:10: error: ‘int main()’ redeclared a s different kind of entity    31 | int main(){       |          ^ 671f158f-c11a-4f61-8a1c-b58c83414db1.cpp:1:5: note: previous declaration ‘int ma in’     1 | int main       |     ^~~~ In file included from 671f158f-c11a-4f61-8a1c-b58c83414db1.cpp:22: 671f158f-c11a-4f61-8a1c-b58c83414db1.cpp: In function ‘int main()’: 671f158f-c11a-4f61-8a1c-b58c83414db1.cpp:32:20: error: ‘sorted_list_sum’ was not  declared in this scope    32 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 671f158f-c11a-4f61-8a1c-b58c83414db1.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,36 +1,22 @@
-#include <iostream>
 #include <vector>
-#include <algorithm>
+#include <string>
+using namespace std;
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if(b.size() != a.size()) return false;
-    return std::equal(a.begin(), a.end(), b.begin());
+bool issame(vector<string> a,vector<string>b) {
+    return a == b;
 }
 
-std::vector<std::string> sorted_list_sum(std::vector<std::string> lst) {
-    auto it = std::remove_if(lst.begin(), lst.end(), [](const std::string& s){ return (stoi(s) % 2); });
-    lst.erase(it, lst.end());
-    std::sort(lst.begin(), lst.end());
-    return lst;
-}
-
-int main() {
-    int n; 
-    std::cin >> n;
-    
-    std::vector<std::string> lst;
-    
-    for(int i = 0; i < n; i++) {
-        std::cin >> lst.push_back(std::to_string(i+1));
+vector<string> sorted_list_sum(vector<string> lst) {
+    sort(lst.begin(),lst.end());
+    string str = "";
+    for(auto s : lst) {
+        str += s;
     }
-    
-    std::vector<std::string> result = sorted_list_sum(lst);
-    
-    cout << "Sorted List Sum: ";
-    for(auto str : result) {
-        cout << str << " ";
+    vector<string> result;
+    if(str.length() > 0) {
+        result.push_back(str);
+    } else {
+        result.push_back("0");
     }
-    cout << endl;
-    
-    return 0;
+    return result;
 }
