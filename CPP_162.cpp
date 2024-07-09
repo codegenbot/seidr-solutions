45d6f924995297bc1dda006ece1a0244287fb38f
beep boop <githubbot@vadim.me>
2024-07-09 05:27:32 +0200

    Solution 48 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/d9xvhjel Prompt: d32a3898-aed0-43f0-a816-bac2bd518f52.cpp:3:6: error: ‘string’ in namespace ‘std’  does not name a type     3 | std::string string_to_md5(std::string text) {       |      ^~~~~~ d32a3898-aed0-43f0-a816-bac2bd518f52.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | using namespace std; In file included from d32a3898-aed0-43f0-a816-bac2bd518f52.cpp:19: d32a3898-aed0-43f0-a816-bac2bd518f52.cpp: In function ‘int main()’: d32a3898-aed0-43f0-a816-bac2bd518f52.cpp:21:13: error: ‘string_to_md5’ was not d eclared in this scope    21 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,19 +1,30 @@
 #include <string>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+
 using namespace std;
 
-std::string string_to_md5(std::string text) {
+string string_to_md5(string text) {
     if (text.empty()) return "None";
     
     unsigned char md5[16];
-    #include <md5.h> // Include the MD5 library
-    MD5((const unsigned char*)text.c_str(), text.size(), md5);
+    uint32_t digest[4];
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+    const char *cstr = text.c_str();
+    MD5_Update(&ctx, cstr, text.size());
+    MD5_Final(&ctx, md5);
     
-    std::string result;
-    for(int i = 0; i < 16; ++i){
+    string result;
+    for(int i = 0; i < 4; ++i){
         stringstream ss;
-        ss << hex << (int)md5[i];
+        ss << hex << setfill('0') << setw(8) << ((uint32_t*)&md5)[i];
         result += ss.str();
     }
     
     return result;
 }
+
+#include "digest.h"
+#include "include/md5.h"
