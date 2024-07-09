35f3f13a9334f3dbd56febb6569cb231a010ef1e
beep boop <githubbot@vadim.me>
2024-07-09 07:51:14 +0200

    Solution 20 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/x8c0rtpg Prompt: cfeb67a9-93cf-40d5-a81f-eb7a9abceaa5.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ cfeb67a9-93cf-40d5-a81f-eb7a9abceaa5.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from cfeb67a9-93cf-40d5-a81f-eb7a9abceaa5.cpp:27: cfeb67a9-93cf-40d5-a81f-eb7a9abceaa5.cpp: In function ‘int main()’: cfeb67a9-93cf-40d5-a81f-eb7a9abceaa5.cpp:29:30: error: could not convert ‘{-3, 9 , -1, 4, 2, 31}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector<std::v ector<int> >’    29 |     assert (pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);       |             ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~       |                              |       |                              <brace-enclosed initializer list>

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,28 +1,5 @@
-```cpp
-#include <vector>
-#include <set>
-
-bool pairs_sum_to_zero(std::vector<std::vector<int>> l) {
-    std::set<int> s;
-    for (const auto& v : l) {
-        if(v.size() != 2)
-            return false;
-        int i = v[0];
-        int j = v[1];
-        s.insert(i);
-        s.insert(j);
-    }
-    for (auto it = s.begin(); it != s.end(); ++it) { 
-        int complement = -(*it);
-        if (s.find(complement) != s.end()) {
-            return true;
-        }
-    }
-    return false;
-}
-
 int main() {
-    std::vector<std::vector<int>> input = {{-3, 0}, {9, -9}, {-1, 1}, {4, -4}, {2, -2}, {31, -31}};
-    assert(pairs_sum_to_zero(input) == true);
+    std::vector<std::vector<int>> input = {{-3}, {9}, {-1}, {4}, {2}, {31}};
+    assert(pairs_sum_to_zero({{input[0][0]}, {input[1][0]}, {input[2][0]}, {input[3][0]}, {input[4][0]}, {input[5][0]}}) == false);
     return 0;
 }
