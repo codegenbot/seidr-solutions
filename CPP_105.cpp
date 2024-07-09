232324692c8d16530c92d7dfd6a4e775f040a20d
beep boop <githubbot@vadim.me>
2024-07-09 06:26:46 +0200

    Solution 48 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/gekczo0y Prompt:       |                              ^~~~~~ 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp:52:37: error: ‘string’ was not declared  in this scope    52 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp:52:44: error: ‘b’ was not declared in t his scope    52 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp:52:45: error: expression list treated a s compound expression in initializer [-fpermissive]    52 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp:51: 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp: In function ‘int main()’: 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp:61:20: error: ‘by_length’ was not decla red in this scope    61 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp:61:19: error: ‘issame’ cannot be used a s a function    61 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,25 +1,51 @@
 #include <vector>
-#include <string>
-#include <map>
+#include <algorithm>
 
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-
-vector<string> by_length(vector<int> arr) {
-    vector<string> result;
-    
+std::vector<std::string> by_length(std::vector<int> arr) {
+    vector<int> temp;
     for (int i : arr) {
         if (i >= 1 && i <= 9) {
-            map<int, string> numMap = {{1, "One"}, {2, "Two"}, {3, "Three"}, 
-                                        {4, "Four"}, {5, "Five"}, {6, "Six"}, 
-                                        {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-            result.push_back(numMap[i]);
+            temp.push_back(i);
         }
     }
-
+    
+    sort(temp.begin(), temp.end());
+    
+    vector<string> result;
+    for (int i : temp) {
+        string s = "";
+        switch (i) {
+            case 1:
+                s = "One";
+                break;
+            case 2:
+                s = "Two";
+                break;
+            case 3:
+                s = "Three";
+                break;
+            case 4:
+                s = "Four";
+                break;
+            case 5:
+                s = "Five";
+                break;
+            case 6:
+                s = "Six";
+                break;
+            case 7:
+                s = "Seven";
+                break;
+            case 8:
+                s = "Eight";
+                break;
+            case 9:
+                s = "Nine";
+                break;
+        }
+        result.push_back(s);
+    }
+    
     reverse(result.begin(), result.end());
     
     return result;
