ad00b624681e419425d237bdd2533dbbf513daea
beep boop <githubbot@vadim.me>
2024-07-09 06:17:25 +0200

    Solution 90 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/v8rop5l8 Prompt: 3ea87995-b758-4244-b1ea-e63f6e0d470c.cpp:5:10: fatal error: boost/assert.hpp: No  such file or directory     5 | #include <boost/assert.hpp>       |          ^~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -31,9 +31,9 @@ variant<int, float, string> compare_one(const variant<int, float, string>& a, co
 }
 
 int main() {
-    assert (get<int>(compare_one(10, 5)) == 10);
-    assert (get<string>(compare_one(string("one"), string("two"))) == "two");
-    assert (get<string>(compare_one(string("None"), string("None"))) == "None");
+    assert(get<int>(compare_one(10, 5)) == 10);
+    assert(get<string>(compare_one(string("one"), string("two"))) == "two" && "Failed assertion: Strings comparison");
+    assert(get<string>(compare_one(string("None"), string("None"))) == "None" && "Failed assertion: Equal strings comparison");
 
     return 0;
 }
