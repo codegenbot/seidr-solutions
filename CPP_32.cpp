a44d8219904d62b042af0cd2b763697978cd5efd
beep boop <githubbot@vadim.me>
2024-07-09 07:40:06 +0200

    Solution 41 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/7t5tgpwa Prompt: 4c2b5123-2fd4-4a62-814b-137bed166ca1.cpp:27:16: error: expected primary-expressi on before ‘double’    27 |         vector<double> coeffs = {};       |                ^~~~~~ 4c2b5123-2fd4-4a62-814b-137bed166ca1.cpp:32:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    32 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 4c2b5123-2fd4-4a62-814b-137bed166ca1.cpp:35:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    35 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 4c2b5123-2fd4-4a62-814b-137bed166ca1.cpp:35:36: error: ‘find_zero’ cannot be use d as a function    35 |         solution = find_zero(coeffs);       |                                    ^ In file included from 4c2b5123-2fd4-4a62-814b-137bed166ca1.cpp:19: 4c2b5123-2fd4-4a62-814b-137bed166ca1.cpp:36:25: error: ‘poly’ cannot be used as a function    36 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ~~~~^~~~~~~~~~~~~~~~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -2,12 +2,20 @@
 #include <cmath>
 #include <cassert>
 
-double myAbs(double num){
+double polynomial(std::vector<double> coeffs, double x){
+    double result = 0.0;
+    for(int i=0; i<coeffs.size(); i++){
+        result += coeffs[i] * std::pow(x, i);
+    }
+    return result;
+}
+
+double custom_abs(double num){
     return num >= 0 ? num : -num;
 }
 
 double find_zero(std::vector<double> coeffs){
-    assert(coeffs.size() >= 2);
+    assert(coeffs.size() > 1);
     double a = coeffs[0];
     double b = coeffs[1];
     return -b/a;
