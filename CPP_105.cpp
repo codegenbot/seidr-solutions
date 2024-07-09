471f2f157572cf45abc18f3e5baaa72285ec2847
beep boop <githubbot@vadim.me>
2024-07-09 06:05:18 +0200

    Solution 14 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/gekczo0y Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,26 +1,17 @@
 vector<string> by_length(vector<int> arr) {
-    vector<string> result;
-
-    for (int num : arr) {
-        if (num >= 1 && num <= 9) {
-            sort({num});
-            reverse({num.begin(), num.end()});
-            string str = to_string(num[0]);
-            if (!result.empty()) {
-                while (stoll(str) != num[0]) {
-                    for (char c : str) {
-                        if (c < '9') {
-                            str++;
-                        } else {
-                            str--;
-                            break;
-                        }
-                    }
-                }
-            }
-            result.push_back(("Zero" + str).substr(1));
+    vector<int> result;
+    for (int i : arr) {
+        if (i >= 1 && i <= 9) {
+            result.push_back(i);
         }
     }
-
-    return result;
+    sort(result.begin(), result.end());
+    reverse(result.begin(), result.end());
+    vector<string> output;
+    map<int, string> numberNames = {{1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
+                                     {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
+    for (int i : result) {
+        output.push_back(numberNames[i]);
+    }
+    return output;
 }
