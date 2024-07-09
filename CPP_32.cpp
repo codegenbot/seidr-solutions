3e73a162455bbc9c5cac783a69da4140be7f524d
beep boop <githubbot@vadim.me>
2024-07-09 18:17:24 +0200

    Solution 21 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lcw7gzkh Prompt: 9d891490-0e52-46b9-ab4d-2ab3cbc3921f.cpp:28:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    28 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 9d891490-0e52-46b9-ab4d-2ab3cbc3921f.cpp:31:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    31 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 9d891490-0e52-46b9-ab4d-2ab3cbc3921f.cpp:31:36: error: ‘find_zero’ cannot be use d as a function    31 |         solution = find_zero(coeffs);       |                                    ^ In file included from 9d891490-0e52-46b9-ab4d-2ab3cbc3921f.cpp:15: 9d891490-0e52-46b9-ab4d-2ab3cbc3921f.cpp:32:25: error: ‘poly’ cannot be used as a function    32 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ~~~~^~~~~~~~~~~~~~~~~~ 9d891490-0e52-46b9-ab4d-2ab3cbc3921f.cpp:32:17: error: ‘abs’ was not declared in  this scope    32 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,19 +1,20 @@
-```cpp
 using namespace std;
 
 std::vector<double> coeffs = vector<double>();
 
-std::vector<double> polys;
-double poly(std::vector<double>, double);
+double poly(std::vector<double> c, double x) {
+    double result = 0;
+    for (int i = 0; i < c.size(); i++) {
+        result += c[i] * pow(x, i);
+    }
+    return result;
+}
 
-double find_zero(vector<double> xs) {
-    double a = 1; // assuming coefficients start from 0th power
+double find_zero() {
+    double a = coeffs[0];
     double b = 0;
-    for (int i = 0; i < coeffs.size(); i++) {
-        if (i % 2 == 0)
-            a += coeffs[i];
-        else
-            b -= coeffs[i] / pow((double)xs[0], i/2.0);
+    for (int i = 1; i < coeffs.size(); i += 2) {
+        b -= coeffs[i] / poly(coeffs, (i - 1) / 2.0);
     }
     return -b / a;
 }
