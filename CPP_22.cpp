7f01cda754ff3fddd333b04469abae8f439713d6
beep boop <githubbot@vadim.me>
2024-07-09 05:29:51 +0200

    Solution 83 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qnmgtenv Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ eeadf3d5-4283-4e76-bcf8-d7d072616c35.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ eeadf3d5-4283-4e76-bcf8-d7d072616c35.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from eeadf3d5-4283-4e76-bcf8-d7d072616c35.cpp:11: eeadf3d5-4283-4e76-bcf8-d7d072616c35.cpp: In function ‘int main()’: eeadf3d5-4283-4e76-bcf8-d7d072616c35.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ eeadf3d5-4283-4e76-bcf8-d7d072616c35.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,9 +1,23 @@
-vector<int> filter_integers(list<any> values) {
+vector<int> filter_integers(list_any values) {
     vector<int> result;
     for (const auto& value : values) {
-        if (any_cast<int>(value).good()) {
-            result.push_back(any_cast<int>(value));
+        if (boost::any_cast<int>(value).good()) {
+            bool isSame = true;
+            for (int i = 0; i < result.size(); i++) {
+                if (!areEqual(vector<int>({result[i]}), vector<int>(boost::any_cast<int>(value)))) {
+                    isSame = false;
+                    break;
+                }
+            }
+            if (isSame) {
+                continue;
+            }
+            result.push_back(boost::any_cast<int>(value));
         }
     }
     return result;
+}
+
+bool areEqual(vector<int> a, vector<int> b) {
+    return a == b;
 }
