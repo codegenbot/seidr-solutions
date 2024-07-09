232abb2017eef3fb9f75f3b54cd76f0f940800ce
beep boop <githubbot@vadim.me>
2024-07-09 03:04:36 +0200

    Solution 86 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/863x6wqt Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 59bae4c4-276d-448d-8383-603d6ec9e388.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,15 +1,5 @@
 #include <set>
-#include <vector>
-#include <algorithm>
 
-bool issame(std::set<std::string> a, std::vector<std::string> b) {
-    std::set<std::string> setb;
-    for (const auto& s : b) {
-        setb.insert(s);
-    }
-    return a == setb;
-}
-
-int main() {
-    assert(issame({{"grunt", "trumpet", "prune", "gruesome"}}, {"run"}.second,{{"grunt", "prune"}}));
+bool issame(std::set<std::string> a, std::set<std::string> b) {
+    return a == b;
 }
