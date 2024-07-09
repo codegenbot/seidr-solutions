a43b06648c954d10158eef855ac1a3846bbcad93
beep boop <githubbot@vadim.me>
2024-07-09 20:52:25 +0200

    Solution 58 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/j617bm3d Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const float&}; _Tp = float; _Alloc = std::allocator<float>; std::v ector<_Tp, _Alloc>::iterator = std::vector<float>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = float; _Alloc = std::all ocator<float>; std::vector<_Tp, _Alloc>::value_type = float]’ 99af49b2-fe3d-4ad0-9198-4da302b330ec.cpp:27:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
+#include <variant>
 
-long long double_the_difference(std::vector<double> lst) {
+long long double_the_difference(std::vector<std::variant<float, long long>> lst) {
     long long sum = 0;
-    for (double num : lst) {
-        if (num > 0 && std::floor(num) == num) { 
-            if (static_cast<long long>(num) % 2 != 0) { 
-                sum += static_cast<long long>(std::pow(num, 2)); 
+    for (auto &num : lst) {
+        if (std::holds_alternative<float>(num)) {
+            float f = std::get<float>(num);
+            if (f > 0 && std::floor(f) == f) { 
+                if (static_cast<long long>(f) % 2 != 0) { 
+                    sum += static_cast<long long>(std::pow(f, 2)); 
+                }
             }
         }
     }
@@ -15,16 +18,26 @@ long long double_the_difference(std::vector<double> lst) {
 }
 
 int main() {
-    double input; 
-    int n = 0; 
-    std::vector<double> lst;
-    
+    float input; 
+    int n = 0;
+    std::vector<std::variant<float, long long>> lst;
+
     while (1) {
         std::cout << "Enter a number (negative to stop): ";
         std::cin >> input;
         
         if (input < 0) break;
-        lst.push_back(input);
+        if (input > 0 && std::floor(input) == input) {
+            if (static_cast<long long>(input) % 2 != 0) { 
+                lst.push_back(static_cast<long long>(std::pow(input, 2))); 
+            }
+            else {
+                lst.push_back(input);
+            }
+        }
+        else {
+            lst.push_back(input);
+        }
         n++;
     }
     
