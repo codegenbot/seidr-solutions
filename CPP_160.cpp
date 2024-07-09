78bbe8c94cc1c21ee3e7986f1ff4b92d1fd32f44
beep boop <githubbot@vadim.me>
2024-07-09 21:38:09 +0200

    Solution 95 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8tmp81k3 Prompt: 3ba03624-a64e-4909-9e1e-1d69bec76f79.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 3ba03624-a64e-4909-9e1e-1d69bec76f79.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 3ba03624-a64e-4909-9e1e-1d69bec76f79.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 3ba03624-a64e-4909-9e1e-1d69bec76f79.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ 3ba03624-a64e-4909-9e1e-1d69bec76f79.cpp:28:1: error: expected unqualified-id be fore ‘if’    28 | if (op != '+') {       | ^~ In file included from 3ba03624-a64e-4909-9e1e-1d69bec76f79.cpp:32: 3ba03624-a64e-4909-9e1e-1d69bec76f79.cpp: In function ‘int main()’: 3ba03624-a64e-4909-9e1e-1d69bec76f79.cpp:34:13: error: ‘do_algebra’ was not decl ared in this scope    34 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ^~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,61 +1,20 @@
 ```cpp
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-
-using namespace std;
-
-struct Expression {
-    char op;
-    int operand;
-};
-
-int main() {
-    if (fopen_s(&fin, "input.txt", "r") != 0) {
-        cerr << "Failed to open input file.\n";
-        return 1;
-    }
-
-    vector<Expression> operators_and_operands;
-
-    string token;
-    char op = '+';
-    int operand = 0;
-
-    while (fin >> token) {
-        if (!token.empty()) {
-            if (!op == '+') {
-                operators_and_opernds.push_back({op, operand});
-                op = '+';
+while (iss >> token) {
+    if (!token.empty()) {
+        if (!op.empty()) {
+            operators_and_operands.push_back({op, operand});
+            op = token[0];
+            operand = std::stoi(token.substr(1));
+        } else {
+            op = token[0];
+            if (isdigit(token[1])) {
+                operand = std::stoi(token.substr(1));
+            } else {
                 operand = 0;
-            }
-            if (token[0] == '+') {
-                op = '+';
-                operand = stoi(token.substr(1));
-            } else if (token[0] == '-') {
-                op = '-';
-                operand = stoll(token.substr(1));
-            } else if (token[0] == '*') {
-                op = '*';
-                operand = stoll(token.substr(1));
-            } else if (token[0] == '/') {
-                op = '/';
-                operand = stoll(token.substr(1));
-            } else if (token[0] == '^') {
-                op = '^';
-                operand = stoi(token.substr(1));
             }
         }
     }
-
-    // add the last operator and operand to vector
-    if (!op == '+') {
-        operators_and_operands.push_back({op, operand});
-    }
-
-    for (const auto& expression : operators_and_operands) {
-        cout << "Operator: " << expression.op;
-        cout << ", Operand: " << expression.operand << endl;
-    }
+}
+if (!op.empty()) {
+    operators_and_operands.push_back({op, operand});
 }
