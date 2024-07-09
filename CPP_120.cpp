652febc1e679195ad5357801c093accf69ff6812
beep boop <githubbot@vadim.me>
2024-07-09 02:09:45 +0200

    Solution 28 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/j2eldqxj Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 8c448164-f8df-472e-8c3f-cbc6a6abb19c.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,7 +1,5 @@
-```cpp
 #include <algorithm>
 #include <iostream>
-#include <cassert>
 #include <initializer_list>
 
 using namespace std;
@@ -13,18 +11,6 @@ bool issame(vector<int> a, vector<int> b) {
         return false;
 }
 
-vector<int> maximum(vector<int>, int k) {
-    vector<int> result = {{}}; // Initialize with an empty initializer_list.
-    
-    for (int i : vector<int>{{1, 2, 3, -23, 243, -400, 0}.begin(), {1, 2, 3, -23, 243, -400, 0}.end()} {
-        result.push_back(i);
-    }
-    
-    sort(result.begin(), result.end());
-    return result;
-}
-
-int main() {
-    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 7) , {0,-23,-400,1,2,3,243}));
-    return 0;
+vector<int> maximum(vector<int>, int) {
+    // code for this function should be implemented
 }
