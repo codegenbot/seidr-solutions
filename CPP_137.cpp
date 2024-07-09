babda395a9e7fd787d949c52b50aac60bc1e5e72
beep boop <githubbot@vadim.me>
2024-07-09 04:58:05 +0200

    Solution 68 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fv53j2kl Prompt: 365fcc60-bc5e-4ec8-be49-15146ed2099c.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -5,31 +5,30 @@
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return (int)boost::any_cast<int>(a) > (float)boost::any_cast<float>(b) ? a : b;
+        return ((int)boost::any_cast<int>(a) > boost::any_cast<float>(b)) ? a : b;
     } else if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return (int)boost::any_cast<int>(a) > (double)boost::any_cast<double>(b) ? a : b;
-    } else if (a.type() == typeid(int) && b.type() == typeid(std::string)) {
-        std::string str = boost::any_cast<std::string>(b);
-        int num = boost::any_cast<int>(a);
-        return num > std::stoi(str) ? a : b;
-    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return (float)boost::any_cast<float>(a) > (int)boost::any_cast<int>(b) ? a : b;
-    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return (double)boost::any_cast<double>(a) > (int)boost::any_cast<int>(b) ? a : b;
+        return ((int)boost::any_cast<int>(a) > boost::any_cast<double>(b)) ? a : b;
     } else if (a.type() == typeid(std::string) && b.type() == typeid(int)) {
         std::string str = boost::any_cast<std::string>(a);
         int num = boost::any_cast<int>(b);
-        return std::stoi(str) > num ? a : b;
-    } else if (a.type() == typeid(float) && b.type() == typeid(double)) {
-        return (float)boost::any_cast<float>(a) > (double)boost::any_cast<double>(b) ? a : b;
+        return (std::stoi(str) > num) ? a : b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return (boost::any_cast<float>(a) > static_cast<float>(boost::any_cast<int>(b))) ? a : b;
+    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return (boost::any_cast<double>(a) > static_cast<double>(boost::any_cast<int>(b))) ? a : b;
     } else if (a.type() == typeid(std::string) && b.type() == typeid(float)) {
         std::string str = boost::any_cast<std::string>(a);
-        float num = (float)boost::any_cast<float>(b);
-        return std::stof(str) > num ? a : b;
+        float num = boost::any_cast<float>(b);
+        return (std::stof(str) > num) ? a : b;
     } else if (a.type() == typeid(std::string) && b.type() == typeid(double)) {
         std::string str = boost::any_cast<std::string>(a);
         double num = boost::any_cast<double>(b);
-        return std::stod(str) > num ? a : b;
+        return (std::stod(str) > num) ? a : b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return ((int)boost::any_cast<int>(a) > boost::any_cast<double>(b)) ? a : b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(double)) {
+        return (boost::any_cast<float>(a) > boost::any_cast<double>(b)) ? a : b;
+    } else {
+        return "None";
     }
-    return boost::any("None");
 }
