f8b207bf691485b81d185e019a49a7154e2ad9b5
beep boop <githubbot@vadim.me>
2024-07-09 13:05:16 +0200

    Solution 20 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/mvcgxdwn Prompt:       |                              ^~~~~~ 94fee335-4fd9-4c07-95c4-087178148d0f.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 94fee335-4fd9-4c07-95c4-087178148d0f.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 94fee335-4fd9-4c07-95c4-087178148d0f.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 94fee335-4fd9-4c07-95c4-087178148d0f.cpp:29: 94fee335-4fd9-4c07-95c4-087178148d0f.cpp: In function ‘int main()’: 94fee335-4fd9-4c07-95c4-087178148d0f.cpp:39:20: error: ‘reverse_delete’ was not declared in this scope    39 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 94fee335-4fd9-4c07-95c4-087178148d0f.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,37 +1,3 @@
-#include <algorithm>
-#include <vector>
-#include <string>
-
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
-}
-
-vector<string> reverse_delete(string s, string c) {
-    vector<string> result;
-    string temp = "";
-    for (char ch : s) {
-        bool flag = false;
-        for (char cc : c) {
-            if (ch == cc) {
-                flag = true;
-                break;
-            }
-        }
-        if (!flag) {
-            temp += ch;
-        }
-    }
-    result.push_back(temp);
-    string rev_temp = temp;
-    reverse(rev_temp.begin(), rev_temp.end());
-    if (temp == rev_temp) {
-        result.push_back("True");
-    } else {
-        result.push_back("False");
-    }
-    return result;
+    return a == b;
 }
