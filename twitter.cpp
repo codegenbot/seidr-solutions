3e1b53ffe9af92fe8febf4b7576f9b58a0383bc3
beep boop <githubbot@vadim.me>
2024-07-09 23:51:27 +0200

    Solution 12 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/tiok8txr Prompt:     5 |     cout << "Enter a tweet: ";       |     ^~~~ 8733038b-a647-4a83-963d-c4e60b96814f.cpp:6:18: error: ‘tweet’ was not declared i n this scope     6 |     getline(cin, tweet);       |                  ^~~~~ 8733038b-a647-4a83-963d-c4e60b96814f.cpp:6:5: error: ‘getline’ was not declared in this scope     6 |     getline(cin, tweet);       |     ^~~~~~~ 8733038b-a647-4a83-963d-c4e60b96814f.cpp:7:13: error: ‘validateTweet’ was not de clared in this scope     7 |     cout << validateTweet(tweet) << endl;       |             ^~~~~~~~~~~~~ 8733038b-a647-4a83-963d-c4e60b96814f.cpp:7:37: error: ‘endl’ was not declared in  this scope     7 |     cout << validateTweet(tweet) << endl;       |                                     ^~~~ 8733038b-a647-4a83-963d-c4e60b96814f.cpp: At global scope: 8733038b-a647-4a83-963d-c4e60b96814f.cpp:10:1: error: ‘string’ does not name a t ype    10 | string validateTweet(string tweet) {       | ^~~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -3,15 +3,7 @@
 #include <string>
 #include <limits>
 
-std::string validateTweet(std::string tweet) {
-    if (tweet.empty()) {
-        return "You didn't type anything";
-    }
-    if (tweet.length() > 140) {
-        return "Too many characters";
-    }
-    return "Your tweet has " + std::to_string(tweet.length()) + " characters";
-}
+std::string validateTweet(std::string tweet);
 
 int main() {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -21,4 +13,14 @@ int main() {
     std::getline(std::cin, tweet);
     std::cout << validateTweet(tweet) << std::endl;
     return 0;
+}
+
+std::string validateTweet(std::string tweet) {
+    if (tweet.empty()) {
+        return "You didn't type anything";
+    }
+    if (tweet.length() > 140) {
+        return "Too many characters";
+    }
+    return "Your tweet has " + std::to_string(tweet.length()) + " characters";
 }
