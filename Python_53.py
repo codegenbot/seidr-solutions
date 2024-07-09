e0c94d6b4c725819950ec63f3e068bbf1986d90d
beep boop <githubbot@vadim.me>
2024-07-09 21:51:32 +0200

    Solution 48 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ae1gh594 Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -1,10 +1,8 @@
 import sys
 
-
 def add(numbers):
     return sum(numbers)
 
-
-numbers = [int(num) for num in sys.argv[1:]]
+numbers = [int(num) for num in sys.stdin.readline().split()]
 result = add(numbers)
 print(result)
