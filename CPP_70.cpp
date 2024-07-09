14d56bc8e120c0a49a88d1cb1ca95b14e3939412
beep boop <githubbot@vadim.me>
2024-07-09 05:59:35 +0200

    Solution 39 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/6n9rycuk Prompt:       |                    ^~~ 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp:19: 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp: In function ‘int main()’: 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp:29:20: error: ‘strange_sort_list’ was n ot declared in this scope    29 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 753a5c4c-e8de-4a71-ad87-6fb7262ea4bd.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,50 +1,54 @@
+```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
-bool issame(vector<int> a,vector<int> b){
-    if(a.size() != b.size())
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
         return false;
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i])
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
             return false;
+        }
     }
     return true;
 }
 
-int main(){
-    vector<int> lst;
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-    cout << "Enter the elements: ";
-    for(int i=0; i<n; i++){
-        int x;
-        cin >> x;
-        lst.push_back(x);
-    }
-    vector<int> result = strange_sort_list(lst);
-    cout << "The sorted list is: ";
-    for(int i=0; i<result.size(); i++){
-        cout << result[i] << " ";
-    }
-    cout << endl;
-    return 0;
-}
-
-vector<int> strange_sort_list(vector<int> lst) {
-    vector<int> result;
+std::vector<int> strange_sort_list(std::vector<int> lst) {
+    std::vector<int> result;
     while (!lst.empty()) {
-        int min_val = *min_element(lst.begin(), lst.end());
+        int min_val = *std::min_element(lst.begin(), lst.end());
         result.push_back(min_val);
         lst.erase(std::remove(lst.begin(), lst.end(), min_val), lst.end());
         if (!lst.empty()) {
-            int max_val = *max_element(lst.begin(), lst.end());
+            int max_val = *std::max_element(lst.begin(), lst.end());
             result.push_back(max_val);
             lst.erase(std::remove(lst.begin(), lst.end(), max_val), lst.end());
         }
     }
     return result;
+}
+
+int main() {
+    std::vector<int> input;
+    int n;
+    std::cout << "Enter the number of elements: ";
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        int val;
+        std::cout << "Enter element " << i + 1 << ": ";
+        std::cin >> val;
+        input.push_back(val);
+    }
+    
+    std::vector<int> output = strange_sort_list(input);
+
+    if (issame(output, {1, 2, 3})) {
+        std::cout << "The list is sorted correctly." << std::endl;
+    } else {
+        std::cout << "The list is not sorted correctly." << std::endl;
+    }
+
+    return 0;
 }
