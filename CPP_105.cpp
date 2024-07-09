9169771ea27301c938fe98ec5009cc4f612f3222
beep boop <githubbot@vadim.me>
2024-07-09 03:58:37 +0200

    Solution 15 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zvs0j795 Prompt:       |                              ^~~~~~ 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp:24:37: error: ‘string’ was not declared  in this scope    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp:23: 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp: In function ‘int main()’: 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp:33:20: error: ‘by_length’ was not decla red in this scope    33 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 13d3767a-1d6c-4cfd-883e-9f13331dc5d9.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -3,15 +3,15 @@
 vector<string> by_length(vector<int> arr) {
     vector<string> result;
     map<int, string> digitName = {{1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-    
+
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
             result.push_back(digitName[num]);
         }
     }
-    
+
     sort(result.begin(), result.end());
-    
+
     reverse(result.begin(), result.end());
     
     return result;
