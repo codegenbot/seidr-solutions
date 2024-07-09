9438dbb6dfc34573a5967ab045b3c114da78724b
beep boop <githubbot@vadim.me>
2024-07-09 16:07:53 +0200

    Solution 85 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/s7maa6a5 Prompt: Complete the following code given the task description and function signature.

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,9 +1,8 @@
-int solutions(vector<int> lst) {
+int solution(vector<int> lst){
     int sum = 0;
-    for (int i = 0; i < lst.size(); i++) {
-        if (i % 2 == 1 && lst[i] % 2 != 0) {
+    for(int i = 0; i < lst.size(); i++){
+        if(lst[i] % 2 != 0 && (i+1) % 2 == 0)
             sum += lst[i];
-        }
     }
     return sum;
 }
