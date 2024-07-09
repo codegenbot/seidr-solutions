9a64bf35f29354f7f0d84f9e15dd5bd5caa2b22f
beep boop <githubbot@vadim.me>
2024-07-09 09:01:31 +0200

    Solution 9 of CPP/79, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/i23hsp3e Prompt: 18b7f710-29ac-4f9a-9e5a-08c509cac387.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 18b7f710-29ac-4f9a-9e5a-08c509cac387.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main(){       |     ^~~~

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -1,12 +1,14 @@
-```cpp
+#include <cassert>
+#include <string>
+
 std::string decimal_to_binary(int decimal){
     std::string binary = "";
     while(decimal > 0){
         if(decimal % 2 == 0)
-            binary = "0" + binary;
+            binary += "0";
         else
-            binary = "1" + binary;
+            binary += "1";
         decimal /= 2;
     }
-    return "db" + std::to_string(decimal) + "db";
+    return "db" + binary + "db";
 }
