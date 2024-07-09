43318a33e410a4b73a6e50d6e1b25d6c2ab111b4
beep boop <githubbot@vadim.me>
2024-07-09 19:01:04 +0200

    Solution 6 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/t1wz5kz8 Prompt:       |                    ^~~ f5e50822-ab7f-451b-a675-8db6a6265f95.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f5e50822-ab7f-451b-a675-8db6a6265f95.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f5e50822-ab7f-451b-a675-8db6a6265f95.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f5e50822-ab7f-451b-a675-8db6a6265f95.cpp:18: f5e50822-ab7f-451b-a675-8db6a6265f95.cpp: In function ‘int main()’: f5e50822-ab7f-451b-a675-8db6a6265f95.cpp:28:20: error: ‘make_a_pile’ was not dec lared in this scope    28 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ f5e50822-ab7f-451b-a675-8db6a6265f95.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,23 +1,35 @@
 ```cpp
+#include <iostream>
 #include <vector>
-#include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-vector<int> make_a_pile(int n) {
+std::vector<int> make_a_pile(int n) {
     vector<int> pile;
-    int stones = n;
-    while (stones > 0) {
-        pile.push_back(stones);
-        if (n % 2 == 1) {
-            n++;
-            stones = n;
+    int stones = 1;
+    for (int i = 0; i < n; i++) {
+        if ((i + 1) % 2 == 0) {
+            pile.push_back(stones);
+            stones += 2;
         } else {
-            n++;
-            stones = n * 2;
+            pile.push_back(stones);
+            stones += 1;
         }
     }
     return pile;
+}
+
+int main() {
+    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
+    return 0;
 }
