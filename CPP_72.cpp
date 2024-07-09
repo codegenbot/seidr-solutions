b79494255d90f221c8d7bc89239bf79a1d9a1e9b
beep boop <githubbot@vadim.me>
2024-07-09 18:24:18 +0200

    Solution 3 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mdgxdhrs Prompt:       |                  ^~~~~~ 21fe9a05-61c6-4260-a283-feb3a08d15a6.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                         ^~~ 21fe9a05-61c6-4260-a283-feb3a08d15a6.cpp:1:33: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                                 ^~~ 21fe9a05-61c6-4260-a283-feb3a08d15a6.cpp:1:38: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool will_it_fly(vector<int> q, int w) {       |                                      ^ 21fe9a05-61c6-4260-a283-feb3a08d15a6.cpp:14:20: error: ‘string’ was not declared  in this scope    14 | bool is_palindrome(string s) {       |                    ^~~~~~ In file included from 21fe9a05-61c6-4260-a283-feb3a08d15a6.cpp:20: 21fe9a05-61c6-4260-a283-feb3a08d15a6.cpp: In function ‘int main()’: 21fe9a05-61c6-4260-a283-feb3a08d15a6.cpp:22:24: error: ‘will_it_fly’ cannot be u sed as a function    22 |     assert (will_it_fly({5}, 5) == true);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,31 +1,11 @@
-```cpp
 #include <vector>
 #include <string>
 #include <algorithm>
 
-bool is_palindrome(string s) {
-    string t = s;
-    reverse(t.begin(), t.end());
-    return s == t;
-}
-
-int main() {
-    vector<int> q;
-    int w;
-    cin >> w;
-    for (int i = 0; i < 3; ++i) {
-        int x;
-        cin >> x;
-        q.push_back(x);
-    }
-    cout << ((will_it_fly(q, w)) ? "YES" : "NO") << endl;
-
-}
-
-bool will_it_fly(vector<int> q, int w) {
-    string str = "";
+bool will_it_fly(std::vector<int> q, int w) {
+    std::string str = "";
     for (int i : q) {
-        str += to_string(i);
+        str += std::to_string(i);
     }
     if (!is_palindrome(str)) return false;
     int sum = 0;
@@ -33,4 +13,10 @@ bool will_it_fly(vector<int> q, int w) {
         sum += i;
     }
     return sum <= w;
+}
+
+bool is_palindrome(std::string s) {
+    std::string t = s;
+    std::reverse(t.begin(), t.end());
+    return s == t;
 }
