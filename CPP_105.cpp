1d874d2f4efbce8941570ddccc0368bc78cb73ed
beep boop <githubbot@vadim.me>
2024-07-09 06:42:37 +0200

    Solution 75 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/gekczo0y Prompt:       |                              ^~~~~~ 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp:52:37: error: ‘string’ was not declared  in this scope    52 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp:52:44: error: ‘b’ was not declared in t his scope    52 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp:52:45: error: expression list treated a s compound expression in initializer [-fpermissive]    52 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp:51: 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp: In function ‘int main()’: 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp:61:20: error: ‘by_length’ was not decla red in this scope    61 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 5decb9b7-8e4b-40f7-91aa-831ce4bb24b7.cpp:61:19: error: ‘issame’ cannot be used a s a function    61 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -11,44 +11,40 @@ bool isSame(vector<string> a, vector<string> b) {
 }
 
 vector<string> by_length(vector<int> arr) {
-    vector<string> temp;
+    vector<string> result;
     for (int i : arr) {
-        if (i >= 1 && i <= 9) {
-            switch (i) {
-                case 1:
-                    temp.push_back("One");
-                    break;
-                case 2:
-                    temp.push_back("Two");
-                    break;
-                case 3:
-                    temp.push_back("Three");
-                    break;
-                case 4:
-                    temp.push_back("Four");
-                    break;
-                case 5:
-                    temp.push_back("Five");
-                    break;
-                case 6:
-                    temp.push_back("Six");
-                    break;
-                case 7:
-                    temp.push_back("Seven");
-                    break;
-                case 8:
-                    temp.push_back("Eight");
-                    break;
-                case 9:
-                    temp.push_back("Nine");
-                    break;
-            }
+        string s;
+        switch (i) {
+            case 1:
+                s = "One";
+                break;
+            case 2:
+                s = "Two";
+                break;
+            case 3:
+                s = "Three";
+                break;
+            case 4:
+                s = "Four";
+                break;
+            case 5:
+                s = "Five";
+                break;
+            case 6:
+                s = "Six";
+                break;
+            case 7:
+                s = "Seven";
+                break;
+            case 8:
+                s = "Eight";
+                break;
+            case 9:
+                s = "Nine";
+                break;
         }
+        result.push_back(s);
     }
-    
-    sort(temp.begin(), temp.end());
-    
-    vector<string> result = temp;
     
     return result;
 }
