1b09654b0ee404c89bbe88c1173bcc035c27b4f4
beep boop <githubbot@vadim.me>
2024-07-09 22:48:48 +0200

    Solution 9 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nxvc2egv Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,26 +1,35 @@
-#include <vector>
 #include <algorithm>
+#include <sstream>
+
+using namespace std;
 
 string sort_numbers(string numbers) {
+    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
+                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
+                                {"eight", 8}, {"nine", 9}};
+
     vector<string> numVec;
+    stringstream ss(numbers);
     string num;
 
-    for (int i = 0; i < numbers.length(); i++) {
-        if (numbers[i] == ' ') {
+    while (getline(ss, num, ' ')) {
+        if (numMap.find(num) != numMap.end()) {
             numVec.push_back(num);
-            num = "";
-        } else {
-            num += numbers[i];
         }
     }
 
-    numVec.push_back(num);
-
     sort(numVec.begin(), numVec.end());
 
+    return join(numVec, " ");
+}
+
+string join(vector<string>& vec, string delimiter) {
     string result;
-    for (const auto& str : numVec) {
-        result += str + " ";
+    for (int i = 0; i < vec.size(); ++i) {
+        result += vec[i];
+        if (i < vec.size() - 1) {
+            result += delimiter;
+        }
     }
-    return result.substr(0, result.length() - 1);
+    return result;
 }
