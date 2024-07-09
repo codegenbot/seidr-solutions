3cfff16c4247f846ff56126f7225f3db2e865fa0
beep boop <githubbot@vadim.me>
2024-07-09 05:22:15 +0200

    Solution 50 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/urrbvh7e Prompt: ea1af8b7-1eee-4f24-80a2-d907da8bd100.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int num) {       | ^~~~~~ In file included from ea1af8b7-1eee-4f24-80a2-d907da8bd100.cpp:71: ea1af8b7-1eee-4f24-80a2-d907da8bd100.cpp: In function ‘int main()’: ea1af8b7-1eee-4f24-80a2-d907da8bd100.cpp:73:13: error: ‘int_to_mini_romank’ was not declared in this scope    73 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,4 +1,4 @@
-```cpp
+```
 std::string int_to_mini_roman(int num) {
     std::string roman[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",
                           "", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
@@ -11,8 +11,8 @@ std::string int_to_mini_roman(int num) {
                           "", "C", "CI", "CII", "CIII", "CIV", "CV", "CVI", "CVII", "CVIII", "CX", "CXI", "CXII", "CXIII", "CXIV", "CXV", "CXVI", "CXVII", "CXVIII", "CXIX", "CXX",
                           "", "CXXI", "CXXII", "CXXIII", "CXXIV", "CXXV", "CXXVI", "CXXVII", "CXXVIII", "CXXIX", "CXXX",
                           "", "CXXXI", "CXXXII", "CXXXIII", "CXXXIV", "CXXXV", "CXXXVI", "CXXXVII", "CXXXVIII", "CXXXIX", "CXL",
-                          "", "CXLI", "CXLII", "CXLIII", "CXLIV", "CXLV", "CXLVI", "CXLVII", "CXLVIII", "CXlix", "CL",
-                          "", "CLI", "CII", "CC", "CCI", "CCC", "CCCI", "CCCII", "CCCIII", "CCCIV", "CCCV", "CCCVI", "CCCvii", "CCCIX", "CCCX"};
+                          "", "CXLI", "CXLII", "CXLIII", "CXLIV", "CXLV", "CXLVI", "CXLVII", "CXLVIII", "Clix", "CL",
+                          "", "CLI", "CII", "CC", "CCI", "CCC", "CCCI", "CCCII", "CCCIII", "CCCIV", "CCCV", "CCCvi", "CCCIX", "CCCX"};
 
     std::string result = "";
 
@@ -41,15 +41,14 @@ std::string int_to_mini_roman(int num) {
         } else if (num >= 50) {
             num -= 50;
             result += "L";
-        } else if (num == 4) {
-            result += "IV";
-            return result;
-        } else if (num == 1) {
-            result += "I";
-            return result;
+        } else if (num >= 40) {
+            num -= 40;
+            result += "XL";
         } else {
-            num--;
-            result += "I";
+            while (num > 0) {
+                num--;
+                result += "I";
+            }
         }
     }
 
