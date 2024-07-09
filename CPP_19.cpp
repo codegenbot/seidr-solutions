3eda59e25b6e97a0b857971244833b2ebaca8ca7
beep boop <githubbot@vadim.me>
2024-07-09 02:43:09 +0200

    Solution 1 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7upqa0gi Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,30 +1,37 @@
-map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                            {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                            {"eight", 8}, {"nine", 9}};
+#include <algorithm>
+#include<string>
+
+using namespace std;
 
 string sort_numbers(string numbers) {
+    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
+                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
+                                {"eight", 8}, {"nine", 9}};
     vector<string> nums;
-    string num;
+    string temp;
 
-    for (int i = 0; i < numbers.size(); i++) {
-        if (numbers[i] == ' ') {
-            nums.push_back(num);
-            num = "";
-        } else {
-            num += numbers[i];
+    for (int i = 0; i < numbers.length(); i++) {
+        while (i + 1 <= numbers.length() && !isalpha(numbers[i])) {
+            i++;
+        }
+        temp = "";
+        while (i + 1 <= numbers.length() && isalpha(numbers[i])) {
+            temp += tolower(numbers[i]);
+            i++;
+        }
+
+        if (!temp.empty()) {
+            nums.push_back(temp);
         }
     }
 
-    nums.push_back(num);
-
     sort(nums.begin(), nums.end(), [&numMap](string a, string b) {
-        return to_string(numMap[а]) < to_string(numMap[b]);
+        return numMap[a] < numMap[b];
     });
 
-    string result;
-    for (auto &str : nums) {
+    string result = "";
+    for (auto str : nums) {
         result += str + " ";
     }
-
-    return result.substr(0, result.size() - 1);
+    return result.substr(0, result.length() - 1);
 }
