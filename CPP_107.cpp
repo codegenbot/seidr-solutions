74c81bf7744a64872a37d08c915be68936cf7e32
beep boop <githubbot@vadim.me>
2024-07-09 19:59:02 +0200

    Solution 60 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/y5snjn2h Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 22a7a12a-9e75-467e-b27c-efa6944c5e73.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <vector>
 
 std::vector<int> even_odd_palindrome(int n) {
@@ -20,11 +21,5 @@ bool isSame(std::vector<int> a, std::vector<int> b) {
 }
 
 int main() {
-    int n = 5;
-    std::vector<int> expected({0, 1, 0, 1, 0});
-    for(int i=0; i<n; i++){
-        if(i%2 == 0) expected.push_back(0);
-        else expected.push_back(1);
-    }
-    assert(isSame(even_odd_palindrome(n), expected));
+    assert(isSame(even_odd_palindrome(5), std::vector<int>({0, 1, 0, 1, 0})));
 }
