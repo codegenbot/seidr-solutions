c750a4e81add7ea0b676742e95d1682f99811461
beep boop <githubbot@vadim.me>
2024-07-09 10:32:24 +0200

    Solution 57 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/srcez8ua Prompt:  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1b3b6fba-a90c-41f0-8e90-bbd1c8201657.cpp:13:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 1b3b6fba-a90c-41f0-8e90-bbd1c8201657.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1b3b6fba-a90c-41f0-8e90-bbd1c8201657.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1b3b6fba-a90c-41f0-8e90-bbd1c8201657.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 1b3b6fba-a90c-41f0-8e90-bbd1c8201657.cpp:12: 1b3b6fba-a90c-41f0-8e90-bbd1c8201657.cpp: In function ‘int main()’: 1b3b6fba-a90c-41f0-8e90-bbd1c8201657.cpp:22:20: error: ‘unique_digits’ was not d eclared in this scope    22 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -4,16 +4,21 @@
 #include <cassert>
 #include <initializer_list>
 
-namespace CPPL104 {
-    bool issame(std::vector<int> a, std::vector<int> b) {
-        if (a.size() != b.size()) {
-            return false;
-        }
-        return std::equal(a.begin(), a.end(), b.begin());
+bool issame(std::vector<int> a, std::vector<int> b){
+    if (a.size() != b.size()) {
+        return false;
     }
+    return std::equal(a.begin(), a.end(), b.begin());
+}
 
-    bool unique_digits(std::initializer_list<int> digits) {
-        // Implement the unique_digits function here
-        // Return true if all the digits are unique, false otherwise
+std::vector<int> unique_digits(int n){
+    std::vector<int> result;
+    while (n != 0){
+        int digit = n % 10;
+        if (std::find(result.begin(), result.end(), digit) == result.end()){
+            result.push_back(digit);
+        }
+        n /= 10;
     }
+    return result;
 }
