6ba1b501cfb3a8e105e751287d2a53f5d40dc3ec
beep boop <githubbot@vadim.me>
2024-07-09 07:55:35 +0200

    Solution 20 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n5yplexf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa76607f-b591-4cec-a800-5a9293bf8 b66.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,3 +1,2 @@
 def solution(lst):
-    odd_sum = sum(i for i in lst[1::2] if i % 2 != 0)
-    return "The sum of odd numbers at even indices is: {}".format(odd_sum)
+    return sum(i for i in lst[1::2] if i % 2 != 0)
