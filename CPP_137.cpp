147b56c870f36f65bc11e6eb4ea14bcd767d3c76
beep boop <githubbot@vadim.me>
2024-07-09 17:41:52 +0200

    Solution 33 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/rlozoors Prompt:    27 | int main(){       |     ^~~~ d20a104e-f3de-453a-9a8c-1709a205a457.cpp:6:5: note: ‘int main()’ previously defi ned here     6 | int main() {       |     ^~~~ In file included from d20a104e-f3de-453a-9a8c-1709a205a457.cpp:26: d20a104e-f3de-453a-9a8c-1709a205a457.cpp: In function ‘int main()’: d20a104e-f3de-453a-9a8c-1709a205a457.cpp:28:13: error: ‘boost’ has not been decl ared    28 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ d20a104e-f3de-453a-9a8c-1709a205a457.cpp:28:35: error: expected primary-expressi on before ‘>’ token    28 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ d20a104e-f3de-453a-9a8c-1709a205a457.cpp:28:37: error: ‘compare_one’ was not dec lared in this scope    28 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,36 +1,23 @@
 #include <iostream>
 #include <string>
-#include <boost/any.hpp>
 
 using namespace std;
-
-boost::any compare_one(const string& s, int i) {
-    if (i > stoi(s)) {
-        return boost::any("The larger number is: " + to_string(i));
-    }
-    else if (stoi(s) > i) {
-        return boost::any("The larger number is: " + s);
-    }
-    else {
-        return boost::any("Both numbers are equal.");
-    }
-}
 
 int main() {
     int a, b;
     cout << "Enter the first number: ";
     cin >> a;
     cout << "Enter the second number: ";
-    string b_str;
-    cin >> b_str;
-    int b_int = stoi(b_str);
+    cin >> b;
 
-    boost::any result = compare_one(to_string(a), b);
-    if (result.type() == typeid(string)) {
-        cout << boost::any_cast<string>(result) << endl;
+    if (a > b) {
+        cout << "The larger number is: " << a << endl;
+    }
+    else if (b > a) {
+        cout << "The larger number is: " << b << endl;
     }
     else {
-        cout << boost::any_cast<string>(compare_one(to_string(b_int), to_string(a))) << endl;
+        cout << "Both numbers are equal." << endl;
     }
 
     return 0;
