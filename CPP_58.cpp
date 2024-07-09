83326c4e9fa5158c2fc4eccfb3dc071a7b0713a3
beep boop <githubbot@vadim.me>
2024-07-09 21:17:21 +0200

    Solution 16 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g34sr5ik Prompt:       |                    ^~~ 0b346f57-961e-4bb4-9709-a53a8b77411d.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0b346f57-961e-4bb4-9709-a53a8b77411d.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0b346f57-961e-4bb4-9709-a53a8b77411d.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0b346f57-961e-4bb4-9709-a53a8b77411d.cpp:14: 0b346f57-961e-4bb4-9709-a53a8b77411d.cpp: In function ‘int main()’: 0b346f57-961e-4bb4-9709-a53a8b77411d.cpp:24:20: error: ‘common’ was not declared  in this scope    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 0b346f57-961e-4bb4-9709-a53a8b77411d.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,15 +1,54 @@
+```
+#include <iostream>
 #include <vector>
 #include <set>
 
-vector<int> common(vector<int> l1, vector<int> l2) {
-    set<int> s1(l1.begin(), l1.end());
-    set<int> s2(l2.begin(), l2.end());
+bool issame(int a, int b) {
+    if (a == b)
+        return true;
+    else
+        return false;
+}
+
+std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
+    std::set<int> s1(l1.begin(), l1.end());
+    std::set<int> s2(l2.begin(), l2.end());
     
-    set<int> intersection;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                      inserter(intersection, intersection.begin()));
+    std::set<int> intersection;
+    std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                      std::inserter(intersection, intersection.begin()));
     
-    vector<int> result(intersection.begin(), intersection.end());
-    sort(result.begin(), result.end());
+    std::vector<int> result(intersection.begin(), intersection.end());
+    std::sort(result.begin(), result.end());
     return result;
+}
+
+int main() {
+    int n, m;
+    std::cout << "Enter number of elements for first list: ";
+    std::cin >> n;
+    
+    std::vector<int> l1(n);
+    for (int i = 0; i < n; i++) {
+        std::cout << "Enter element " << i+1 << ": ";
+        std::cin >> l1[i];
+    }
+    
+    std::cout << "Enter number of elements for second list: ";
+    std::cin >> m;
+    
+    std::vector<int> l2(m);
+    for (int i = 0; i < m; i++) {
+        std::cout << "Enter element " << i+1 << ": ";
+        std::cin >> l2[i];
+    }
+    
+    std::vector<int> res = common(l1, l2);
+    
+    std::cout << "Common elements: ";
+    for (int x : res)
+        std::cout << x << " ";
+    std::cout << std::endl;
+    
+    return 0;
 }
