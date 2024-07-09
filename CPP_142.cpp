944ddc2de8c764e26c4c412ebfa39dc1b5dfa518
beep boop <githubbot@vadim.me>
2024-07-09 04:19:17 +0200

    Solution 7 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/a2nhvhv9 Prompt: a4d9cc2f-356f-4ef5-afa7-dad72a7024cf.cpp: In function ‘int sum_squares(std::vect or<int>)’: a4d9cc2f-356f-4ef5-afa7-dad72a7024cf.cpp:10:22: error: ‘pow’ was not declared in  this scope    10 |             total += pow(lst[i], 3);       |                      ^~~

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -1,4 +1,3 @@
-````
 #include <vector>
 #include <cmath>
 using namespace std;
@@ -7,10 +6,8 @@ int sum_squares(vector<int> lst) {
     int total = 0;
     for (int i = 0; i < lst.size(); i++) {
         if ((i + 1) % 3 == 0 && (i + 1) % 4 != 0)
-            total += pow(lst[i], 2);
+            total += lst[i] * lst[i];
         else if ((i + 1) % 4 == 0 && (i + 1) % 3 != 0)
             total += pow(lst[i], 3);
     }
     return total;
-}
-```
