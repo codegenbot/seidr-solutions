3a873ca530d8f08a736677bec1ec2e59666fc771
beep boop <githubbot@vadim.me>
2024-07-09 11:16:33 +0200

    Solution 64 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/esw41ccx Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 682655b3-4ef6-4341-bdaf-0822af96f8ca.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 682655b3-4ef6-4341-bdaf-0822af96f8ca.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 682655b3-4ef6-4341-bdaf-0822af96f8ca.cpp:11: 682655b3-4ef6-4341-bdaf-0822af96f8ca.cpp: In function ‘int main()’: 682655b3-4ef6-4341-bdaf-0822af96f8ca.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 682655b3-4ef6-4341-bdaf-0822af96f8ca.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -2,15 +2,15 @@
 #include <string>
 
 bool issame(vector<string> a, vector<string> b) {
-    for (const auto& str : a) {
-        if (std::find(b.begin(), b.end(), str) != b.end()) {
-            return true;
+    for(const auto& str : a){
+        if(find(b.begin(), b.end(), str) == b.end()){
+            return false;
         }
     }
-    return false;
+    return true;
 }
 
 int main() {
-    assert(issame({{"grunt", "trumpet", "prune", "gruesome"}, {"run"}}, {{"grunt", "prune"}}));
+    assert(issame({{"grunt", "trumpet", "prune", "gruesome"}, "run"} , {{"grunt", "prune"}}));
     return 0;
 }
