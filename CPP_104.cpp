422ff2a2624e14b01ab84496b4e8549ef10ab650
beep boop <githubbot@vadim.me>
2024-07-09 10:12:54 +0200

    Solution 22 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/srcez8ua Prompt:       |                    ^~~ aaf118c3-a2fe-48d4-98b3-0de2d96d8a7c.cpp:21:27: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ aaf118c3-a2fe-48d4-98b3-0de2d96d8a7c.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ aaf118c3-a2fe-48d4-98b3-0de2d96d8a7c.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from aaf118c3-a2fe-48d4-98b3-0de2d96d8a7c.cpp:20: aaf118c3-a2fe-48d4-98b3-0de2d96d8a7c.cpp: In function ‘int main()’: aaf118c3-a2fe-48d4-98b3-0de2d96d8a7c.cpp:30:20: error: ‘unique_digits’ was not d eclared in this scope    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ aaf118c3-a2fe-48d4-98b3-0de2d96d8a7c.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -4,10 +4,32 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    for (int i = 0; i < a.size(); i++) {
+    
+    for (size_t i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
+    
     return true;
+}
+
+std::vector<int> filterNoEvenDigit(std::vector<int> x) {
+    std::vector<int> result;
+    for (int num : x) {
+        int temp = num;
+        bool hasEvenDigit = false;
+        while (temp > 0) {
+            if ((temp % 10) % 2 == 0) {
+                hasEvenDigit = true;
+                break;
+            }
+            temp /= 10;
+        }
+        if (!hasEvenDigit) {
+            result.push_back(num);
+        }
+    }
+    std::sort(result.begin(), result.end());
+    return result;
 }
