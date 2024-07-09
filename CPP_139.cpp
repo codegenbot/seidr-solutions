7299fdc8533742f7ec3bc162022998a9c1deeea9
beep boop <githubbot@vadim.me>
2024-07-09 19:51:56 +0200

    Solution 10 of CPP/139, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gjtnmr3o Prompt: __future_base::_State_baseV2>; _Args = {}; __gnu_cxx::_Lock_policy _Lp = __gnu_c xx::_S_atomic]’ /usr/include/c++/11/bits/shared_ptr_base.h:1342:14:   required from ‘std::__shar ed_ptr<_Tp, _Lp>::__shared_ptr(std::_Sp_alloc_shared_tag<_Tp>, _Args&& ...) [wit h _Alloc = std::allocator<std::__future_base::_State_baseV2>; _Args = {}; _Tp = std::__future_base::_State_baseV2; __gnu_cxx::_Lock_policy _Lp = __gnu_cxx::_S_a tomic]’ /usr/include/c++/11/bits/shared_ptr.h:409:59:   required from ‘std::shared_ptr<_ Tp>::shared_ptr(std::_Sp_alloc_shared_tag<_Tp>, _Args&& ...) [with _Alloc = std: :allocator<std::__future_base::_State_baseV2>; _Args = {}; _Tp = std::__future_b ase::_State_baseV2]’ /usr/include/c++/11/bits/shared_ptr.h:862:14:   required from ‘std::shared_ptr<_ Tp> std::allocate_shared(const _Alloc&, _Args&& ...) [with _Tp = std::__future_b ase::_State_baseV2; _Alloc = std::allocator<std::__future_base::_State_baseV2>; _Args = {}]’ /usr/include/c++/11/bits/shared_ptr.h:878:39:   required from ‘std::shared_ptr<_ Tp> std::make_shared(_Args&& ...) [with _Tp = std::__future_base::_State_baseV2;  _Args = {}]’ /usr/include/c++/11/future:1296:43:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::_Sp_counted_ptr_inplace<std::__future_base::_State_baseV2, std::allo cator<std::__future_base::_State_baseV2>, __gnu_cxx::_S_atomic> >’ has no member  named ‘_M_max_size’

--- CPP_139.cpp
+++ CPP_139.cpp
@@ -1,17 +1,22 @@
-long long factorial(int n) {
-    long long result = 1;
-    for(int i = 2; i <= n; ++i)
-        result *= i;
+#include <iostream>
+#include <cmath>
 
+long long special_factorial(int n) {
+    if (n <= 1)
+        return 1;
+    long long result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+        for (int j = 2; j <= i; j++)
+            result *= j;
+    }
     return result;
 }
 
-long long special_factorial(int n){
-    if(n <= 1)
-        return 1;
-
-    long long result = 1;
-    for(int i = n; i > 0; --i)
-        result *= factorial(i);
-
-    return result;
+int main() {
+    int n;
+    std::cout << "Enter a number: ";
+    std::cin >> n;
+    std::cout << "Special factorial of " << n << " is " << special_factorial(n) << std::endl;
+    return 0;
+}
