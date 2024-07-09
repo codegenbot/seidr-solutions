60e62f7deebee08ee785b11d55b631eab83cadde
beep boop <githubbot@vadim.me>
2024-07-09 22:27:41 +0200

    Solution 95 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/4o6n05as Prompt: ca476b09-087d-4da8-a07a-688821dfa6b3.cpp: In function ‘int next_smallest(std::ve ctor<int>)’: ca476b09-087d-4da8-a07a-688821dfa6b3.cpp:9:5: error: ‘sort’ was not declared in this scope; did you mean ‘short’?     9 |     sort(v.begin(), v.end());       |     ^~~~       |     short ca476b09-087d-4da8-a07a-688821dfa6b3.cpp: At global scope: ca476b09-087d-4da8-a07a-688821dfa6b3.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ ca476b09-087d-4da8-a07a-688821dfa6b3.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -11,7 +11,7 @@ int next_smallest(vector<int> lst) {
     for (int i = 1; i < v.size(); i++) {
         if (v[i] > v[0]) return v[i];
     }
-    assert(false);
+    assert(false); 
     return -1; 
 }
 
