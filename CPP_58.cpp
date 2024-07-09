ed1f04192bb60bb1d260716e0b2b450d7771423a
beep boop <githubbot@vadim.me>
2024-07-09 06:04:03 +0200

    Solution 16 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/isutmiwz Prompt:       |                    ^~~ c3f465f1-0309-466b-8bdb-17a30632093a.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c3f465f1-0309-466b-8bdb-17a30632093a.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c3f465f1-0309-466b-8bdb-17a30632093a.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c3f465f1-0309-466b-8bdb-17a30632093a.cpp:17: c3f465f1-0309-466b-8bdb-17a30632093a.cpp: In function ‘int main()’: c3f465f1-0309-466b-8bdb-17a30632093a.cpp:27:20: error: ‘common’ was not declared  in this scope    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ c3f465f1-0309-466b-8bdb-17a30632093a.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,15 +1,32 @@
+#include <vector>
+#include <set>
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) return false;
+    }
+    return true;
+}
+
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
-
-    set<int> common;
-    for (int x : s1) {
-        if (s2.find(x) != s2.end()) {
-            common.insert(x);
+    set<int> result;
+    
+    for(auto i : s1){
+        if(s2.find(i) != s2.end()){
+            result.insert(i);
         }
     }
+    
+    vector<int> res(result.begin(),result.end());
+    sort(res.begin(),res.end());
+    return res;
+}
 
-    vector<int> result(common.begin(), common.end());
-    sort(result.begin(), result.end());
-    return result;
+int main() {
+    assert(issame(common({4, 3, 2, 8}, {}), {}));
+    // Your implementation for 'main' here
 }
