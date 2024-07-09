0ade69e981ae996ba656f0941ecf10eab74dc8b7
beep boop <githubbot@vadim.me>
2024-07-09 21:42:39 +0200

    Solution 51 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fjs0jsh6 Prompt: 36a7a0d9-ee8b-4707-8c27-15ac5ed1f3a5.cpp:1:18: error: ‘vector’ was not declared in this scope     1 | bool will_it_fly(vector<int> q, int w) {       |                  ^~~~~~ 36a7a0d9-ee8b-4707-8c27-15ac5ed1f3a5.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                         ^~~ 36a7a0d9-ee8b-4707-8c27-15ac5ed1f3a5.cpp:1:33: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                                 ^~~ 36a7a0d9-ee8b-4707-8c27-15ac5ed1f3a5.cpp:1:38: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool will_it_fly(vector<int> q, int w) {       |                                      ^

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -4,9 +4,10 @@ bool will_it_fly(std::vector<int> q, int w) {
     for (int i : q) {
         str += std::to_string(i);
     }
-    if (str != std::string(str).reverse().s) return false;
+    if (str != std::string(str.rbegin(), str.rend()).s) return false;
     int sum = 0;
     for (int i : q) {
         sum += i;
     }
     return sum <= w;
+}
