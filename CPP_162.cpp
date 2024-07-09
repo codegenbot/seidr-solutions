7023bf17c1fea2c9ba54ecd60b8fcd301d7110a0
beep boop <githubbot@vadim.me>
2024-07-09 13:27:49 +0200

    Solution 76 of CPP/162, ratio 0.5 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/tr3wxu14 Prompt: /usr/bin/ld: /tmp/cc6hZMM6.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': aab3f26a-b992-4612-8f61-b95c7bc02cca.cpp:(.text+0x6c): undefined reference to `E VP_MD_CTX_new' /usr/bin/ld: aab3f26a-b992-4612-8f61-b95c7bc02cca.cpp:(.text+0x75): undefined re ference to `EVP_md5' /usr/bin/ld: aab3f26a-b992-4612-8f61-b95c7bc02cca.cpp:(.text+0x87): undefined re ference to `EVP_DigestInit' /usr/bin/ld: aab3f26a-b992-4612-8f61-b95c7bc02cca.cpp:(.text+0xb7): undefined re ference to `EVP_DigestUpdate' /usr/bin/ld: aab3f26a-b992-4612-8f61-b95c7bc02cca.cpp:(.text+0xcf): undefined re ference to `EVP_DigestFinal' /usr/bin/ld: aab3f26a-b992-4612-8f61-b95c7bc02cca.cpp:(.text+0xdb): undefined re ference to `EVP_MD_CTX_free' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <openssl/md5.h>
+#include <openssl/evp.h>
 
 std::string string_to_md5(const std::string& text) {
     if (text.empty()) {
@@ -8,12 +9,16 @@ std::string string_to_md5(const std::string& text) {
     }
 
     unsigned char digest[MD5_DIGEST_LENGTH];
-    MD5((unsigned char*)text.c_str(), text.length(), digest);
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit(mdctx, EVP_md5());
+    EVP_DigestUpdate(mdctx, text.c_str(), text.size());
+    EVP_DigestFinal(mdctx, digest, NULL);
+    EVP_MD_CTX_free(mdctx);
 
     char mdString[33];
-    for (int i = 0; i < 16; i++) {
-        sprintf(&mdString[i * 2], "%02x", (unsigned int)digest[i]);
+    for(int i = 0; i < 16; i++) {
+        sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
     }
 
-    return mdString;
+    return std::string(mdString);
 }
