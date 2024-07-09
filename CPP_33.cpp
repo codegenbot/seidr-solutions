58022bf1871ccf62d6d96f528c8bbd6553ae5a43
beep boop <githubbot@vadim.me>
2024-07-09 18:09:00 +0200

    Solution 0 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/f6c4vub5 Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_33.cpp
@@ -0,0 +1,14 @@
+vector<int> sort_third(vector<int> l) {
+    vector<int> result;
+    for (int i = 0; i < l.size(); i++) {
+        if (i % 3 == 0) {
+            vector<int> temp;
+            temp.insert(temp.begin(), l.begin() + i, l.begin() + i + 1);
+            sort(temp.begin(), temp.end());
+            result.insert(result.end(), temp.begin(), temp.end());
+        } else {
+            result.push_back(l[i]);
+        }
+    }
+    return result;
+}
