19ce5a4207ee77a3bc290c85501dd642f5d1d21c
beep boop <githubbot@vadim.me>
2024-07-09 20:53:32 +0200

    Solution 29 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/v0dpr46q Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 877ed30f-fd77-4c59-b0d0-09a734f0f47b.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 877ed30f-fd77-4c59-b0d0-09a734f0f47b.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 877ed30f-fd77-4c59-b0d0-09a734f0f47b.cpp:11: 877ed30f-fd77-4c59-b0d0-09a734f0f47b.cpp: In function ‘int main()’: 877ed30f-fd77-4c59-b0d0-09a734f0f47b.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 877ed30f-fd77-4c59-b0d0-09a734f0f47b.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,24 +1,19 @@
 #include <boost/any.hpp>
+#include <list>
 
-bool is_same(const boost::any& value1, const boost::any& value2) {
-    return value1.type() == value2.type();
+bool issame(const std::vector<int>& v1, const std::vector<char>& v2) {
+    if (v1.size() != v2.size()) return false;
+    for (int i = 0; i < v1.size(); ++i) {
+        if ((char)v1[i] != v2[i]) return false;
+    }
+    return true;
 }
 
-vector<int> filter_integers(list<any> values) {
-    vector<int> result;
+std::vector<int> filter_integers(std::list<boost::any>& values) {
+    std::vector<int> result;
     for (const auto& value : values) {
-        if (value.type() == typeid(int)) {
-            int v = boost::any_cast<int>(value);
-            bool good = true; // assume int is always good
-            for (const auto& other : values) {
-                if (is_same(value, other)) {
-                    good = false;
-                    break;
-                }
-            }
-            if (good) {
-                result.push_back(v);
-            }
+        if (boost::any_cast<int>(value).good()) {
+            result.push_back(boost::any_cast<int>(value));
         }
     }
     return result;
