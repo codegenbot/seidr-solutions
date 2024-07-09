75cc340cc85d074d33d17ac5c0d7fdbb5b7ecfee
beep boop <githubbot@vadim.me>
2024-07-09 17:26:17 +0200

    Solution 32 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/47ofh1jk Prompt:       |     ^ a26b8a8a-c48c-4ca4-bbce-6f33b9df93e9.cpp:3:16: error: ‘lst’ was not declared in this scope     3 |     int prev = lst[0];       |                ^~~ a26b8a8a-c48c-4ca4-bbce-6f33b9df93e9.cpp:4:5: error: expected unqualified-id bef ore ‘for’     4 |     for (int num : lst) {       |     ^~~ a26b8a8a-c48c-4ca4-bbce-6f33b9df93e9.cpp:13:5: error: expected unqualified-id be fore ‘return’    13 |     return -1;       |     ^~~~~~ a26b8a8a-c48c-4ca4-bbce-6f33b9df93e9.cpp:14:1: error: expected declaration befor e ‘}’ token    14 | }       | ^ In file included from a26b8a8a-c48c-4ca4-bbce-6f33b9df93e9.cpp:16: a26b8a8a-c48c-4ca4-bbce-6f33b9df93e9.cpp: In function ‘int main()’: a26b8a8a-c48c-4ca4-bbce-6f33b9df93e9.cpp:18:13: error: ‘next_smallest’ was not d eclared in this scope    18 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ^~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,10 +1,6 @@
 #include <vector>
 
 int next_smallest(std::vector<int>& lst) {
-    if (lst.size() < 2) {
-        return -1;
-    }
-
     sort(lst.begin(), lst.end());
     int count = 0;
     int prev = lst[0];
@@ -18,10 +14,4 @@ int next_smallest(std::vector<int>& lst) {
         }
     }
     return -1;
-}
-
-int main() {
-    std::vector<int> lst = { -35, 34, 12, -45 };
-    int result = next_smallest(lst);
-    return result;
 }
