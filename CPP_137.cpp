d3ead9d38db46422523937f47672fa1282193fc8
beep boop <githubbot@vadim.me>
2024-07-10 01:02:15 +0200

    Solution 64 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fhsevhcx Prompt: _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 64775137-2923-4c5f-a70b-f6cac4109a0d.cpp:39:5:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 64775137-2923-4c5f-a70b-f6cac4109a0d.cpp:39:5:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,55 +1,33 @@
 #include <string>
 #include <algorithm>
-#include <limits>
 
 using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
         int x = boost::any_cast<int>(a);
-        float y = boost::any_cast<float>(b);
-        return (x > y) ? a : ((y > x) ? b : boost::any("None"));
+        string y = boost::any_cast<string>(boost::any_cast<float>(b));
+        return (x > stof(y)) ? a : ((stof(y) > x) ? b : boost::any("None"));
     } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
         int x = boost::any_cast<int>(a);
         string y = boost::any_cast<string>(b);
-        if (boost::algorithm::istoi(y).good()) {
-            float z = stof(y);
-            return (x > z) ? a : ((z > x) ? b : boost::any("None"));
-        } else {
-            return boost::any("Invalid input");
-        }
+        return (x > stof(y)) ? a : ((stof(y) > x) ? b : boost::any("None"));
     } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
         float x = boost::any_cast<float>(a);
         string y = boost::any_cast<string>(b);
-        if (boost::algorithm::istoi(y).good()) {
-            return (x > stof(y)) ? a : ((stof(y) > x) ? b : boost::any("None"));
-        } else {
-            return boost::any("Invalid input");
-        }
+        return (x > stof(y)) ? a : ((stof(y) > x) ? b : boost::any("None"));
     } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
         string x = boost::any_cast<string>(a);
         int y = boost::any_cast<int>(b);
-        if (boost::algorithm::istoi(x).good()) {
-            return (stof(x) > y) ? a : ((y > stof(x)) ? b : boost::any("None"));
-        } else {
-            return boost::any("Invalid input");
-        }
+        return (stof(x) > y) ? a : ((y > stof(x)) ? b : boost::any("None"));
     } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
         string x = boost::any_cast<string>(a);
         float y = boost::any_cast<float>(b);
-        if (boost::algorithm::istoi(x).good()) {
-            return (stof(x) > y) ? a : ((y > stof(x)) ? b : boost::any("None"));
-        } else {
-            return boost::any("Invalid input");
-        }
+        return (stof(x) > y) ? a : ((y > stof(x)) ? b : boost::any("None"));
     } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
         string x = boost::any_cast<string>(a);
         string y = boost::any_cast<string>(b);
-        if (boost::algorithm::istoi(x).good() && boost::algorithm::istoi(y).good()) {
-            return (stof(x) > stof(y)) ? a : ((stof(y) > stof(x)) ? b : boost::any("None"));
-        } else {
-            return boost::any("Invalid input");
-        }
+        return (stof(x) > stof(y)) ? a : ((stof(y) > stof(x)) ? b : boost::any("None"));
     } else {
         return boost::any("None");
     }
