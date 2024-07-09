8d1fe1dfe7b6d5cf60cfc7732c927c618b15f333
beep boop <githubbot@vadim.me>
2024-07-09 05:30:33 +0200

    Solution 86 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/3gtchhci Prompt:       |                                      ^~~~~~ a9dad765-4e9a-4720-9e48-6555eb6e6ccb.cpp:11:38: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from a9dad765-4e9a-4720-9e48-6555eb6e6ccb.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from a9dad765-4e9a-4720-9e48-6555eb6e6ccb.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ a9dad765-4e9a-4720-9e48-6555eb6e6ccb.cpp:11:46: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a, vector<string> b) {       |                                              ^ a9dad765-4e9a-4720-9e48-6555eb6e6ccb.cpp:11:47: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a, vector<string> b) {       |                                               ^ a9dad765-4e9a-4720-9e48-6555eb6e6ccb.cpp:23:1: error: ‘vector’ does not name a t ype    23 | vector<string> sorted_list_sum(vector<string> lst) {       | ^~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,29 +1,12 @@
-```
 #include <algorithm>
 #include <vector>
 #include <string>
-
-int main() {
-    vector<string> lst = {"aaaa", "bbbb", "dd", "cc"};
-    std::vector<std::string> b = lst;
-    std::sort(b.begin(), b.end());
-    if (!issame(lst, b)) return 1;
-
-    vector<string> result = sorted_list_sum({"aaaa", "bbbb", "dd", "cc"});
-
-    for (const auto& str : result) {
-        std::cout << str << " ";
-    }
-    std::cout << std::endl;
-
-    return 0;
-}
 
 bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) return false;
 
     for (int i = 0; i < a.size(); i++) {
-        if (sorted_list_sum({a[i], b[i]})[0] != sorted_list_sum({b[i], a[i]})[0]) {
+        if (std::abs(std::hash<string>{}(a[i]) - std::hash<string>{}(b[i])) > 0) {
             return false;
         }
     }
@@ -31,8 +14,8 @@ bool issame(vector<string> a, vector<string> b) {
     return true;
 }
 
-std::vector<std::string> sorted_list_sum(std::vector<std::string> lst) {
-    std::vector<std::string> result;
+vector<std::string> sorted_list_sum(vector<string> lst) {
+    vector<std::string> result;
 
     for (const auto& str : lst) {
         if (str.length() % 2 == 0) {
@@ -41,12 +24,25 @@ std::vector<std::string> sorted_list_sum(std::vector<std::string> lst) {
     }
 
     std::sort(result.begin(), result.end(),
-             [](const std::string& a, const std::string& b) {
-                 if (a.length() != b.length()) {
-                     return a.length() < b.length();
-                 } else {
-                     return a < b;
-                 }
-             });
+         [&](const string& a, const string& b) {
+             if (a.length() != b.length()) {
+                 return a.length() < b.length();
+             } else {
+                 return a < b;
+             }
+         });
 
     return result;
+}
+
+int main() {
+    vector<string> lst = {"aaaa", "bbbb", "dd", "cc"};
+    vector<string> sortedListSum = sorted_list_sum(lst);
+    std::vector<std::string> sortedList = {"cc", "dd", "aaaa", "bbbb"};
+    if (!issame(sortedListSum, sortedList)) {
+        // error
+    } else {
+        // no error
+    }
+    return 0;
+}
