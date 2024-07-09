a26e191e72d400f786541fef7b15dd35192f8438
beep boop <githubbot@vadim.me>
2024-07-09 02:09:33 +0200

    Solution 8 of CPP/136, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/wmk4t8hl Prompt: 9d55e879-263d-4747-bb35-112286b0096c.cpp: In function ‘std::vector<int> largest_ smallest_integers(std::vector<int>)’: 9d55e879-263d-4747-bb35-112286b0096c.cpp:6:30: error: ‘INT_MAX’ was not declared  in this scope     6 |     int maxNeg = 0, minPos = INT_MAX;       |                              ^~~~~~~ 9d55e879-263d-4747-bb35-112286b0096c.cpp:3:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     2 | #include <vector>   +++ |+#include <climits>     3 | using namespace std;

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,14 +1,13 @@
+#include <climits> 
+#include <iostream>
+#include <vector>
+using namespace std;
+
 vector<int> largest_smallest_integers(vector<int> lst) {
-    int maxNeg = 0;
-    int minPos = INT_MAX;
-
+    int maxNeg = 0, minPos = INT_MAX;
     for (int num : lst) {
-        if (num < 0 && num > maxNeg) {
-            maxNeg = num;
-        } else if (num > 0 && num < minPos) {
-            minPos = num;
-        }
+        if (num < 0 && num > maxNeg) maxNeg = num;
+        if (num > 0 && num < minPos) minPos = num;
     }
-
-    return {maxNeg, minPos};
+    return {(maxNeg >= 0 ? 0 : maxNeg), (minPos <= 0 ? 0 : minPos)};
 }
