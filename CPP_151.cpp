ddd711ffcb8ddff59e3a34409373e7382d60dfaa
beep boop <githubbot@vadim.me>
2024-07-09 13:15:33 +0200

    Solution 99 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7113sxs1 Prompt: 37a3af99-b8c4-4e9a-901c-5cb85d54c8d5.cpp: In function ‘int double_the_difference (std::initializer_list<int>)’: 37a3af99-b8c4-4e9a-901c-5cb85d54c8d5.cpp:11:32: error: ‘sum_of_odds’ was not dec lared in this scope    11 |     return main_function_sum - sum_of_odds;       |                                ^~~~~~~~~~~ In file included from 37a3af99-b8c4-4e9a-901c-5cb85d54c8d5.cpp:36: 37a3af99-b8c4-4e9a-901c-5cb85d54c8d5.cpp: In function ‘int main()’: 37a3af99-b8c4-4e9a-901c-5cb85d54c8d5.cpp:38:35: error: ‘lst’ was not declared in  this scope    38 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include <initializer_list>
 
-int main_function() {
+int double_the_difference(std::initializer_list<int> lst) {
+    int main_function_sum = 0;
+    int sum_of_odds = odd_sum(lst); // Pass lst to odd_sum function
+    for (int num : lst) {
+        if(num % 2 == 0)
+            main_function_sum += num;
+    }
+    
+    return main_function_sum - sum_of_odds; 
+}
+
+int odd_sum(std::initializer_list<int> lst) {
     int sum_of_odds = 0;
-    std::initializer_list<int> lst = {1, 2, 3, 4, 5};
     for (int num : lst) {
         if(num % 2 != 0)
             sum_of_odds += num;
     }
     return sum_of_odds;
+}
+
+int main_function(std::initializer_list<int> lst) {
+    int actual_odd_sum = odd_sum(lst);
+    
+    int double_the_difference_result = double_the_difference(lst); // Pass lst to double_the_difference function
+    assert (double_the_difference_result == actual_odd_sum ); 
+    
+    return 0; 
 }
