66be1ed77395acac41ee891d7034b7b7dce5bbc0
beep boop <githubbot@vadim.me>
2024-07-09 11:56:48 +0200

    Solution 15 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/bbfhommv Prompt:       |                              ^~~~~~ 89372bf4-ea0c-4cab-8c6d-c885289ce0a3.cpp:29:37: error: ‘string’ was not declared  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 89372bf4-ea0c-4cab-8c6d-c885289ce0a3.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 89372bf4-ea0c-4cab-8c6d-c885289ce0a3.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 89372bf4-ea0c-4cab-8c6d-c885289ce0a3.cpp:28: 89372bf4-ea0c-4cab-8c6d-c885289ce0a3.cpp: In function ‘int main()’: 89372bf4-ea0c-4cab-8c6d-c885289ce0a3.cpp:38:20: error: ‘bf’ was not declared in this scope    38 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 89372bf4-ea0c-4cab-8c6d-c885289ce0a3.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -5,31 +5,8 @@
 
 using namespace std;
 
-vector<string> find_planets_in_between(const vector<string>& planets, const string& planet1, const string& planet2) {
-    vector<string> result;
-    int idx1 = -1, idx2 = -1;
-    
-    for (int i = 0; i < planets.size(); ++i) {
-        if (planets[i] == planet1) {
-            idx1 = i;
-        }
-        if (planets[i] == planet2) {
-            idx2 = i;
-        }
-    }
-    
-    if (idx1 == -1 || idx2 == -1) {
-        return result;
-    }
-    
-    int start = min(idx1, idx2) + 1;
-    int end = max(idx1, idx2);
-    
-    for (int i = start; i < end; ++i) {
-        result.push_back(planets[i]);
-    }
-    
-    return result;
+bool issame(pair<string, string> a, pair<string, string> b) {
+    return (a.first == b.first && a.second == b.second);
 }
 
 int main() {
