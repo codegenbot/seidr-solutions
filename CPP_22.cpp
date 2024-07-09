fa59f98af9cef6ea5497907df4b02af1bcfc9d7e
beep boop <githubbot@vadim.me>
2024-07-09 05:10:54 +0200

    Solution 41 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qnmgtenv Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 43fa0b55-9a0a-4088-977b-f282aca96127.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 43fa0b55-9a0a-4088-977b-f282aca96127.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 43fa0b55-9a0a-4088-977b-f282aca96127.cpp:11: 43fa0b55-9a0a-4088-977b-f282aca96127.cpp: In function ‘int main()’: 43fa0b55-9a0a-4088-977b-f282aca96127.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 43fa0b55-9a0a-4088-977b-f282aca96127.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -6,18 +6,12 @@ bool issame(std::vector<int> a,std::vector<int>b){
     return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
 }
 
-vector<int> filter_integers(list<any> values) {
-    vector<int> result;
+std::vector<int> filter_integers(std::list<boost::any> values) {
+    std::vector<int> result;
     for (const auto& value : values) {
         if (!boost::any_cast<bool>(value)) continue; // skip non-integer values
         int num = boost::any_cast<int>(value);
-        if(result.empty()){
-            result.push_back(num);
-        } else if(!issame(result, vector<int>({num}))){
-            break;
-        } else {
-            result.push_back(num);
-        }
+        result.push_back(num);
     }
     return result;
 }
