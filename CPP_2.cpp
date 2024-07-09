4ced4b94884af82aa67cef937ffc204d947840a4
beep boop <githubbot@vadim.me>
2024-07-10 01:24:19 +0200

    Solution 3 of CPP/2, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ky7b0b7n Prompt: 0f51caca-c8cb-464d-ac55-f2926f625c36.cpp:16:5: error: redefinition of ‘int main( )’    16 | int main(){       |     ^~~~ 0f51caca-c8cb-464d-ac55-f2926f625c36.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~

--- CPP_2.cpp
+++ CPP_2.cpp
@@ -1,13 +1,5 @@
-#include <iostream>
 #include <cmath>
-#include <cassert>
 
 double truncate_number(double number) {
     return number - floor(number);
-}
-
-int main() {
-    assert(fabs(truncate_number(123.456) - 0.456) < 1e-4);
-
-    return 0;
 }
