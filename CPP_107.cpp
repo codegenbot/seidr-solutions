9280636a848481473a565825c2da5fa39a80217b
beep boop <githubbot@vadim.me>
2024-07-09 04:22:37 +0200

    Solution 74 of CPP/107, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/3khcjxc6 Prompt: 84b2d178-90fb-4fda-b3fc-2228421882d2.cpp:25:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 84b2d178-90fb-4fda-b3fc-2228421882d2.cpp:7:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     7 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 84b2d178-90fb-4fda-b3fc-2228421882d2.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 84b2d178-90fb-4fda-b3fc-2228421882d2.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,15 +1,13 @@
-vector<int> res(2, 0);
-    for (int i = 1; i <= n; ++i) {
-        string num = to_string(i);
-        string rev_num = num;
-        reverse(rev_num.begin(), rev_num.end());
-        if (num == rev_num) {
-            if (i % 2 == 0) {
-                res[0]++;
-            } else {
-                res[1]++;
-            }
-        }
+#include <vector>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+vector<int> even_odd_palindrome(int n) {
+    vector<int> result;
+    for(int i = 1; i <= n; ++i) {
+        result.push_back(i % 2 == 0 ? 0 : 1);
     }
-    return res;
+    return result;
 }
