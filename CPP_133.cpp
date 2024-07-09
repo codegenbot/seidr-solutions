e4809f7d8d7a65f36c81059384ad8b969eab96ec
beep boop <githubbot@vadim.me>
2024-07-09 04:30:37 +0200

    Solution 4 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/atx4iofv Prompt: 451991f3-d81e-408a-ab36-04ae4562569b.cpp:2:5: error: expected unqualified-id bef ore ‘for’     2 |     for(float num : lst){       |     ^~~ 451991f3-d81e-408a-ab36-04ae4562569b.cpp:5:5: error: expected unqualified-id bef ore ‘return’     5 |     return sum;       |     ^~~~~~ 451991f3-d81e-408a-ab36-04ae4562569b.cpp:6:1: error: expected declaration before  ‘}’ token     6 | }       | ^ In file included from 451991f3-d81e-408a-ab36-04ae4562569b.cpp:8: 451991f3-d81e-408a-ab36-04ae4562569b.cpp: In function ‘int main()’: 451991f3-d81e-408a-ab36-04ae4562569b.cpp:10:13: error: ‘sum_squares’ was not dec lared in this scope    10 |     assert (sum_squares({-1,1,0})==2);       |             ^~~~~~~~~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,6 +1,6 @@
-int sum_squares(const vector<float>& lst){
+int sum_squares(const vector<float>& lst) {
     int sum = 0;
-    for(float num : lst){
+    for(const float& num : lst){
         sum += ceil(num) * ceil(num);
     }
     return sum;
