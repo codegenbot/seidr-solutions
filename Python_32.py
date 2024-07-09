034255b8de022edb264ec13838d3fe31d120c14f
beep boop <githubbot@vadim.me>
2024-07-09 05:35:02 +0200

    Solution 18 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dfvmwimw Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/85de1163-5e36-456f-a865-932ed41b1 2ad.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,3 +1,6 @@
+import math
+
+
 def find_zero(xs: list):
     if len(xs) % 2 != 0:
         raise ValueError("xs must have even number of coefficients")
@@ -9,8 +12,10 @@ def find_zero(xs: list):
     if c is None and d is None:
         return -b / a
     elif d is None:
-        return -(-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+        return -(-b + math.sqrt(b**2 - 4 * a * c)) / (2 * a)
     else:
-        roots = [(-b + math.sqrt(b**2 - 4*a*d)) / (2*a),
-                 (-b - math.sqrt(b**2 - 4*a*d)) / (2*a)]
+        roots = [
+            (-b + math.sqrt(b**2 - 4 * a * d)) / (2 * a),
+            (-b - math.sqrt(b**2 - 4 * a * d)) / (2 * a),
+        ]
         return min(roots, key=lambda x: abs(x))
