5dff63964c76b5e6bafe2cdbb7164669d77ac461
beep boop <githubbot@vadim.me>
2024-07-09 17:30:13 +0200

    Solution 22 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/03kj9szl Prompt: 128f92f1-fa7d-4d09-a989-c3f981602b14.cpp:32:5: note: ‘assert’ is defined in head er ‘<cassert>’; did you forget to ‘#include <cassert>’? 128f92f1-fa7d-4d09-a989-c3f981602b14.cpp: At global scope: 128f92f1-fa7d-4d09-a989-c3f981602b14.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 128f92f1-fa7d-4d09-a989-c3f981602b14.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main(){       |     ^~~~ In file included from 128f92f1-fa7d-4d09-a989-c3f981602b14.cpp:36: 128f92f1-fa7d-4d09-a989-c3f981602b14.cpp: In function ‘int main()’: 128f92f1-fa7d-4d09-a989-c3f981602b14.cpp:38:24: error: cannot convert ‘<brace-en closed initializer list>’ to ‘double*’    38 |     assert (sum_squares({-1,1,0})==2);       |             ~~~~~~~~~~~^~~~~~~~~~       |                        |       |                        <brace-enclosed initializer list> 128f92f1-fa7d-4d09-a989-c3f981602b14.cpp:5:24: note:   initializing argument 1 o f ‘int sum_squares(double*, int)’     5 | int sum_squares(double arr[], int n){       |                 ~~~~~~~^~~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -5,11 +5,8 @@
 
 int sum_squares(double arr[], int n){
     int total = 0;
-    std::string str = "";
     for(int i=0; i<n; i++){
-        std::string str1 = std::to_string(arr[i]) + " ";
-        str += str1;
-        total += std::pow(static_cast<int>(arr[i]),2);
+        total += std::pow(arr[i],2);
     }
     return total;
 }
@@ -20,7 +17,7 @@ int main(){
     int result = sum_squares(testArray, n);
     std::cout << "Sum of squares: " << result << std::endl;
     
-    assert (sum_squares({1.0, 2.0, 3.0},3) == 14);
+    assert (sum_squares(testArray,n) == 14);
 
     return 0;
 }
