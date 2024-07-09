e476611f7b35683c3126bdeb0279a3e0676091a7
beep boop <githubbot@vadim.me>
2024-07-10 01:53:41 +0200

    Solution 8 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/onqyzxv9 Prompt: 4a7edb50-2a8a-4ac9-aacf-f9eaed5ccedd.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 4a7edb50-2a8a-4ac9-aacf-f9eaed5ccedd.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 4a7edb50-2a8a-4ac9-aacf-f9eaed5ccedd.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 4a7edb50-2a8a-4ac9-aacf-f9eaed5ccedd.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ 4a7edb50-2a8a-4ac9-aacf-f9eaed5ccedd.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,4 +1,4 @@
-```
+```cpp
 #include <cassert>
 #include <vector>
 
@@ -9,8 +9,9 @@ bool below_threshold(std::vector<int> l, int t) {
         }
     }
     return true;
+
 }
 
 int main() {
-    assert(not(below_threshold({1, 8, 4, 10}, 10)));
+    assert(not(below_threshold(std::vector<int>({1, 8, 4, 10}), 10)));
 }
