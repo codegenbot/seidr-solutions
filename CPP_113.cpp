a0e98acf89bf1f3027b3d92cbf4bf965a252d8ce
beep boop <githubbot@vadim.me>
2024-07-09 20:19:07 +0200

    Solution 72 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/n0xhbar7 Prompt: 6898c86a-3657-45f1-b1ab-b8d29fc23302.cpp:56:5: error: redefinition of ‘int main( )’    56 | int main(){       |     ^~~~ 6898c86a-3657-45f1-b1ab-b8d29fc23302.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -1,15 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> odd_count(vector<string> lst);
+
+void displayOutput(vector<string> output) {
+    for (string s : output) {
+        cout << s << endl;
+    }
+}
+
+vector<string> odd_count(vector<string> lst) {
+    vector<string> result;
+    for (int i = 0; i < lst.size(); i++) {
+        int count = 0;
+        for (char c : lst[i]) {
+            if ((int(c) % 2 != 0)) {
+                count++;
+            }
+        }
+        string temp = to_string(i + 1);
+        temp += ": " + to_string(count) + " character(s) are odd.";
+        result.push_back(temp);
+    }
+    return result;
+}
+
 int main() {
     int n;
     cin >> n;
-    
+
     vector<string> lst(n);
-    
+
     for(int i = 0; i < n; i++) {
         cin >> lst[i];
     }
-    
+
     vector<string> output = odd_count(lst);
     displayOutput(output);
-    
+
     return 0;
 }
