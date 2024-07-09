5b45b8b40a9e20de7f22ad789af4699ac66d4c56
beep boop <githubbot@vadim.me>
2024-07-09 04:30:22 +0200

    Solution 13 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/3gtchhci Prompt:    23 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 3049bc68-1e0a-49cf-912c-289dae5fe182.cpp:23:44: error: ‘b’ was not declared in t his scope    23 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3049bc68-1e0a-49cf-912c-289dae5fe182.cpp:23:45: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3049bc68-1e0a-49cf-912c-289dae5fe182.cpp:22: 3049bc68-1e0a-49cf-912c-289dae5fe182.cpp: In function ‘int main()’: 3049bc68-1e0a-49cf-912c-289dae5fe182.cpp:32:20: error: ‘sorted_list_sum’ was not  declared in this scope    32 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 3049bc68-1e0a-49cf-912c-289dae5fe182.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,7 +1,13 @@
+```cpp
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size()!=b.size())return false;
-    for(int i=0;i<a.size();i++)if(a[i].size()!=b[i].size())return false;
-    sort(a.begin(),a.end());
-    sort(b.begin(),b.end());
-    return a==b;
+    for(int i=0; i<a.size(); ++i) {
+        if(a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
 }
