9ff33753f17b0109bba9923eb9418d4a2d6277e9
beep boop <githubbot@vadim.me>
2024-07-09 05:06:23 +0200

    Solution 61 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/za2a3ye3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1a2316ea-d3a2-4c66-8e61-3b352801b bf4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -16,7 +16,7 @@ def minPath(grid, k):
         for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             nx, ny = x + dx, y + dy
             
-            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and ((dx, dy) in [(0, 0), (-1, 0), (1, 0), (0, -1)] or (dx, dy) == (0, 1)):
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and (dx, dy) in [(0, 0), (-1, 0), (1, 0), (0, -1)] or (dx, dy) == (0, 1):
                 visited[nx][ny] = True
                 queue.append((nx, ny, path + [grid[nx][ny]]))
     
