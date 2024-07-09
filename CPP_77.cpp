d029bb795a4f16ef3a635c9746af5e4ef6c9c13d
beep boop <githubbot@vadim.me>
2024-07-09 15:38:04 +0200

    Solution 6 of CPP/77, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/phflndr3 Prompt: /usr/include/c++/11/bits/stl_algobase.h:741:15: error: ‘_Num’ was not declared i n this scope   741 |           if (_Num)       |               ^~~~ /usr/include/c++/11/bits/stl_algobase.h:743:29: error: ‘_Num’ was not declared i n this scope   743 |           return __result - _Num;       |                             ^~~~ /usr/include/c++/11/bits/stl_algobase.h: At global scope: /usr/include/c++/11/bits/stl_algobase.h:1349:16: error: ‘ptrdiff_t’ does not nam e a type  1349 |         static ptrdiff_t       |                ^~~~~~~~~ /usr/include/c++/11/bits/stl_algobase.h:1349:16: note: ‘ptrdiff_t’ is defined in  header ‘<cstddef>’; did you forget to ‘#include <cstddef>’? ac433599-deea-4078-8268-7c37769ae27f.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ ac433599-deea-4078-8268-7c37769ae27f.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main(){       |     ^~~~

--- CPP_77.cpp
+++ CPP_77.cpp
@@ -1,6 +1,6 @@
-```cpp
 #include <cmath>
 #include <cassert>
+#include <cstddef>
 
 bool iscuber(int a){
     int i = 1;
@@ -13,6 +13,6 @@ bool iscuber(int a){
 
 }
 
-int main() {
+int main(){
     assert(iscuber(1729) == false);
 }
