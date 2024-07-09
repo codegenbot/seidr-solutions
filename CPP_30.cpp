72f608d365b5819cc4c1648c704a9b19d69c67d3
beep boop <githubbot@vadim.me>
2024-07-09 14:28:12 +0200

    Solution 59 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/t79zjf21 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -17,33 +17,34 @@ std::vector<std::unique_ptr<float>> get_positive(const std::vector<std::unique_p
 }
 
 int main() {
-    int n;
+    std::string line;
     std::cout << "Enter the number of elements: ";
-    std::cin >> n;
+    std::getline(std::cin, line);
+    float n = stof(line);
 
     if(n > 0) {
         std::vector<std::unique_ptr<float>> numbers;
         for(int i=0; i<n;i++){
             std::cout << "Enter element " << i+1 << ": ";
             float num;
-            cin >> num;
+            std::cin >> num;
             numbers.push_back(std::make_unique<float>(num));
         }
 
         std::vector<std::unique_ptr<float>> positive_numbers = get_positive(numbers);
 
         if(issame(numbers, positive_numbers)) {
-            cout << "The list of positive numbers is: ";
+            std::cout << "The list of positive numbers is: ";
             for (auto& num : positive_numbers) {
-                cout << *num << " ";
+                std::cout << *num << " ";
             }
-            cout << endl;
+            std::cout << std::endl;
         } else {
-            cout << "No positive numbers found." << endl;
+            std::cout << "No positive numbers found." << std::endl;
         }
 
     } else {
-        cout << "Please enter a number greater than zero." << endl;
+        std::cout << "Please enter a number greater than zero." << std::endl;
     }
 
     return 0;
