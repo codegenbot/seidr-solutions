bda3559ef757d58f349cf10a5df407e8e8c41b78
beep boop <githubbot@vadim.me>
2024-07-09 22:26:08 +0200

    Solution 10 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/b0l0pzf4 Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4aa49a65-825a-4911-b6bd-130a887e09e7.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4aa49a65-825a-4911-b6bd-130a887e09e7.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4aa49a65-825a-4911-b6bd-130a887e09e7.cpp:26: 4aa49a65-825a-4911-b6bd-130a887e09e7.cpp: In function ‘int main()’: 4aa49a65-825a-4911-b6bd-130a887e09e7.cpp:36:20: error: ‘sorted_list_sum’ was not  declared in this scope    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 4aa49a65-825a-4911-b6bd-130a887e09e7.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,29 +1,9 @@
-int issame(string a) {
-    int sum = 0;
-    for(int i=0; i<a.length(); i++) {
-        if(a[i] == '1') 
-            sum++;
+bool issame(string s1, string s2) {
+    if (s1.length() != s2.length())
+        return false;
+    for (int i = 0; i < s1.length(); i++) {
+        if (s1[i] != s2[i])
+            return false;
     }
-    return sum;
-}
-
-int main() {
-    vector<string> lst;
-    string temp;
-
-    cout << "Enter words (end with -1):" << endl;
-    while(true) {
-        cin >> temp;
-        if(temp == "-1")
-            break;
-        lst.push_back(temp);
-    }
-
-    vector<string> result = sorted_list_sum(lst);
-
-    cout << "Sorted list: ";
-    for(const auto& str : result)
-        cout << str << " ";
-
-    return 0;
+    return true;
 }
