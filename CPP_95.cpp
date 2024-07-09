c79f6d9b5b6eb204f933ab79692cadd7b2c9512e
beep boop <githubbot@vadim.me>
2024-07-09 21:41:13 +0200

    Solution 6 of CPP/95, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/dus5d50x Prompt:       |                      ^~~ 5373d7bf-2730-445a-ba3a-43ccbbf4106c.cpp:1:26: error: ‘string’ was not declared in this scope     1 | bool check_dict_case(map<string, string> dict) {       |                          ^~~~~~ 5373d7bf-2730-445a-ba3a-43ccbbf4106c.cpp:1:34: error: ‘string’ was not declared in this scope     1 | bool check_dict_case(map<string, string> dict) {       |                                  ^~~~~~ 5373d7bf-2730-445a-ba3a-43ccbbf4106c.cpp:1:42: error: ‘dict’ was not declared in  this scope     1 | bool check_dict_case(map<string, string> dict) {       |                                          ^~~~ 5373d7bf-2730-445a-ba3a-43ccbbf4106c.cpp:1:46: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool check_dict_case(map<string, string> dict) {       |                                              ^ In file included from 5373d7bf-2730-445a-ba3a-43ccbbf4106c.cpp:16: 5373d7bf-2730-445a-ba3a-43ccbbf4106c.cpp: In function ‘int main()’: 5373d7bf-2730-445a-ba3a-43ccbbf4106c.cpp:18:28: error: ‘check_dict_case’ cannot be used as a function    18 |     assert (check_dict_case({}) == false);       |             ~~~~~~~~~~~~~~~^~~~

--- CPP_95.cpp
+++ CPP_95.cpp
@@ -1,6 +1,4 @@
-#include <map>
 #include <string>
-
 bool check_dict_case(map<string, string> dict) {
     if (dict.empty()) return false;
 
@@ -9,8 +7,8 @@ bool check_dict_case(map<string, string> dict) {
 
     for (auto it = dict.begin(); it != dict.end(); ++it) {
         if (!allUpper && !allLower) break;
-        if (allUpper) allUpper = tolower(it->first).find((char)0) == string::npos;
-        if (allLower) allLower = toupper(it->first).find((char)0) == string::npos;
+        if (allUpper) allUpper = tolower(it->first).find((char)0) == std::string::npos;
+        if (allLower) allLower = toupper(it->first).find((char)0) == std::string::npos;
     }
 
     return allUpper && allLower;
