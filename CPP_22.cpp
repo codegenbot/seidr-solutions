a032d140469dc3561f9c841b7f593e6de9593b91
beep boop <githubbot@vadim.me>
2024-07-09 08:11:46 +0200

    Solution 62 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/q0vjqe45 Prompt: <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’ /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:78:33:   required from ‘void std::vector<_Tp , _Alloc>::reserve(std::vector<_Tp, _Alloc>::size_type) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = long unsigned int]’ c0a3a0d0-0507-4578-a0b2-4bee819857bb.cpp:16:14:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -10,24 +10,21 @@ int main() {
     int n;
     std::cin >> n;
 
-    std::vector<int> a;
-    std::vector<int> b;
-
-    a.reserve(static_cast<long unsigned int>(n));
-    b.reserve(static_cast<long unsigned int>(n));
+    std::vector<int> a(n);
+    std::vector<int> b(n);
 
     for(int i = 0; i < n; ++i)
         {
             int x;
             std::cin >> x;
-            a.push_back(x);
+            a[i] = x;
         }
 
     for(int i = 0; i < n; ++i)
         {
             int x;
             std::cin >> x;
-            b.push_back(x);
+            b[i] = x;
         }
 
     if(filter_integers(a, b))
