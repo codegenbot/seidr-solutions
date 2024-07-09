786f48e9919a03172074716a1812719479f0c00a
beep boop <githubbot@vadim.me>
2024-07-09 22:48:21 +0200

    Solution 5 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nxvc2egv Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,26 +1,49 @@
-#include <algorithm>
-#include <string>
-#include <map>
-using namespace std;
-
 string sort_numbers(string numbers) {
-    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                                {"eight", 8}, {"nine", 9}};
-
+    map<string, int> numMap;
     string result = "";
-    for (char c : numbers) {
-        if (c == ' ') {
-            continue;
+    
+    for (int i = 0; i < 10; i++) {
+        switch(i) {
+            case 0:
+                numMap["zero"] = i;
+                break;
+            case 1:
+                numMap["one"] = i;
+                break;
+            case 2:
+                numMap["two"] = i;
+                break;
+            case 3:
+                numMap["three"] = i;
+                break;
+            case 4:
+                numMap["four"] = i;
+                break;
+            case 5:
+                numMap["five"] = i;
+                break;
+            case 6:
+                numMap["six"] = i;
+                break;
+            case 7:
+                numMap["seven"] = i;
+                break;
+            case 8:
+                numMap["eight"] = i;
+                break;
+            case 9:
+                numMap["nine"] = i;
+                break;
         }
-        string num = "";
-        while (c != ' ' && c != '\0') {
-            num += c;
-            c = getchar();
-        }
-        result += numMap[num] < result.empty() ? "" : " ";
-        result += num;
     }
-    sort(result.begin(), result.end());
+    
+    for (int i = 0; i < numbers.size(); i++) {
+        if (i + 1 < numbers.size()) {
+            result += numMap[numbers.substr(i, numbers.find(" ") - i)] + " ";
+        } else {
+            result += numMap[numbers.substr(i)];
+        }
+    }
+    
     return result;
 }
