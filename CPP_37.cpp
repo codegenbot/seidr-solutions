ca1f83dfc280883d952d827082884f2592a0e80e
beep boop <githubbot@vadim.me>
2024-07-10 01:46:35 +0200

    Solution 20 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9939gev7 Prompt: on before ‘float’    41 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 8714bc75-c7c8-4517-9995-6917b82ef5de.cpp:41:43: error: expression list treated a s compound expression in initializer [-fpermissive]    41 | bool issame(vector<float> a,vector<float>b){       |                                           ^ 8714bc75-c7c8-4517-9995-6917b82ef5de.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 8714bc75-c7c8-4517-9995-6917b82ef5de.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main()       |     ^~~~ In file included from 8714bc75-c7c8-4517-9995-6917b82ef5de.cpp:40: 8714bc75-c7c8-4517-9995-6917b82ef5de.cpp: In function ‘int main()’: 8714bc75-c7c8-4517-9995-6917b82ef5de.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <algorithm>
 
-bool isSame(std::vector<float> v1, std::vector<float> v2) {
+bool checkEqual(std::vector<float> v1, std::vector<float> v2) {
     if(v1.size() != v2.size()) return false;
     for(int i = 0; i < v1.size(); i++) {
         if(v1[i] != v2[i]) return false;
@@ -9,7 +9,7 @@ bool isSame(std::vector<float> v1, std::vector<float> v2) {
     return true;
 }
 
-std::vector<float> sortEven(std::vector<float> l) {
+std::vector<float> sort_even(std::vector<float> l) {
     std::vector<float> result(l.size());
     int j = 0;
     for (int i = 0; i < l.size(); i++) {
@@ -30,7 +30,7 @@ int main()
     while(std::cin >> num) {
         input.push_back(num);
     }
-    std::vector<float> output = sortEven(input);
+    std::vector<float> output = sort_even(input);
     for(float f : output) {
         std::cout << f << " ";
     }
