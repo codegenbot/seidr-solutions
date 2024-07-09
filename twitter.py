9c8e88eda2ad6bb703ee57a33c2cc2761326502a
beep boop <githubbot@vadim.me>
2024-07-09 05:04:29 +0200

    Solution 66 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/kih5li8d Prompt: it must return Your tweet has 81 characters for input JW;2dAV1"f>Al#!^A-Ju}0QDUp>]S|UCM3aCo["]t.D2BL5*L7ty"2g\F3.lk(t.2rHe89f|0+J^fQ9kM, but it returns Your tweet has 1 characters Your tweet has 1 characters Your tweet has 139 characters Your tweet has 80 characters Too many characters.

--- twitter.py
+++ twitter.py
@@ -1,14 +1,13 @@
-```
 def validate_tweet(tweet):
-    if len(tweet) > 140:
-        return "Too many characters"
-    elif len(tweet) == 0:
+    if len(tweet) == 0:
         return "You didn't type anything"
+    elif len(tweet) > 140:
+        return "Too many characters"
     else:
         return f"Your tweet has {len(tweet)} characters"
 
-print("Your tweet has {} characters".format(validate_tweet(" ")))
-print("Your tweet has {} characters".format(validate_tweet("1")))
-print("Your tweet has {} characters".format(validate_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeat it")))
-print("Your tweet has {} characters".format(validate_tweet("40172875*\\(&\\,%^^*!@#\\$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~")))
-print("Your tweet has {} characters".format(validate_twitter("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1")))
+print(validate_tweet(" "))
+print(validate_tweet("1"))
+print(validate_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeat it"))
+print(validate_tweet("40172875*\\(&\\,%^^*!@#\\$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~"))
+print(validate_tweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1"))
