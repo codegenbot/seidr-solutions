c81d6f7a6c221dba149c80b67f5819cab265c932
beep boop <githubbot@vadim.me>
2024-07-10 00:32:26 +0200

    Solution 3 of CPP/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/jpbjrpb5 Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,44 +1,17 @@
-#include <string>
-#include <vector>
-
+#include<string>
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = 0, b = 1, c = 0, d = 1;
-    
-    // Convert strings to integers
-    vector<string> partsX = split(x, '/');
-    vector<string> partsN = split(n, '/');
-    
-    a = stoi(partsX[0]);
-    b = stoi(partsX[1]);
-    c = stoi(partsN[0]);
-    d = stoi(partsN[1]);
-    
-    // Calculate the greatest common divisor
-    int gcd = a * d;
-    for (int i = 1; i <= min(a, d); i++) {
-        if (a % i == 0 && d % i == 0) {
-            gcd = i;
-        }
-    }
-    
-    // Simplify fractions
-    int numerator = a / gcd * c / gcd;
-    int denominator = b * d / gcd;
-    
-    // Check if the simplified fraction is an integer
-    return numerator == denominator;
-}
+    int numerator1 = 0, denominator1 = 0;
+    int numerator2 = 0, denominator2 = 0;
 
-// Helper function to split a string by '/'
-vector<string> split(string s, char delimiter) {
-    vector<string> result;
-    size_t pos = 0;
-    while ((pos = s.find(delimiter)) != string::npos) {
-        result.push_back(s.substr(0, pos));
-        s.erase(0, pos + 1);
-    }
-    result.push_back(s);
-    return result;
+    size_t pos = x.find('/');
+    numerator1 = stoi(x.substr(0, pos));
+    denominator1 = stoi(x.substr(pos + 1));
+
+    pos = n.find('/');
+    numerator2 = stoi(n.substr(0, pos));
+    denominator2 = stoi(n.substr(pos + 1));
+
+    return (numerator1 * denominator2) == (numerator2 * denominator1);
 }
