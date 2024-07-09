1c45b2e26e68c6cbc8fb8a847992cfea98b49bc7
beep boop <githubbot@vadim.me>
2024-07-09 04:54:34 +0200

    Solution 68 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ajzrdwh9 Prompt:       |                                  ^~~ 21b07a71-56fe-4757-b77b-363826597277.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 21b07a71-56fe-4757-b77b-363826597277.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 21b07a71-56fe-4757-b77b-363826597277.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 21b07a71-56fe-4757-b77b-363826597277.cpp:23: 21b07a71-56fe-4757-b77b-363826597277.cpp: In function ‘int main()’: 21b07a71-56fe-4757-b77b-363826597277.cpp:33:20: error: ‘maximum’ was not declare d in this scope    33 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 21b07a71-56fe-4757-b77b-363826597277.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -2,21 +2,25 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> maximum(std::vector<int> arr, int k) {
-    std::vector<int> result;
-    for(int i = 0; i < k; i++) {
-        int maxVal = *std::max_element(arr.begin(), arr.end());
-        result.push_back(maxVal);
-        auto it = arr.begin();
-        while(it != arr.end() && *it == maxVal) {
-            it = arr.erase(it);
-        }
+std::vector<int> maxKItems(std::vector<int> arr, int k) {
+    std::priority_queue<int> pq;
+    
+    for(int i = 0; i < arr.size(); i++) {
+        pq.push(arr[i]);
     }
+    
+    std::vector<int> result;
+    
+    for(int i = 0; i < k; i++) {
+        result.push_back(pq.top());
+        pq.pop();
+    }
+    
     return result;
 }
 
 int main() {
-    std::vector<int> test = maximum({1, 2, 3, -23, 243, -400, 0}, 3);
+    std::vector<int> test = maxKItems({1, 2, 3, -23, 243, -400, 0}, 3);
     for (int i : test) {
         std::cout << i << " ";
     }
