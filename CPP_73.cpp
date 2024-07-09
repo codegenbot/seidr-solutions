9dfe9a49b42064f506eca9236fd56cd52c9895c1
beep boop <githubbot@vadim.me>
2024-07-09 21:39:40 +0200

    Solution 40 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3mjvnbwx Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <vector>
-#include <string>
-
-int smallest_change(vector<int> arr) {
-    std::string str = "";
+string smallest_change(vector<int> arr) {
+    int n = arr.size();
+    string str;
     for (int i : arr) {
-        str += std::to_string(i);
+        str += to_string(i);
     }
-    int left = 0, right = str.size() - 1;
+    int left = 0, right = n - 1;
     int changes = 0;
     while (left < right) {
         if (str[left] != str[right]) {
             changes++;
             if (str[left] == str[right-1]) {
-                std::swap(str[left], str[right]);
+                swap(str[left], str[right]);
                 right--;
             } else {
-                std::swap(str[left], str[right]);
+                swap(str[left], str[right]);
                 left++;
             }
         } else {
