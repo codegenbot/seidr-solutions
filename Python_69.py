88ddeabab47be2b154841a7900f3f5a12d89103b
beep boop <githubbot@vadim.me>
2024-07-09 05:37:21 +0200

    Solution 65 of Python/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/6qoso50g Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae172d8f-0490-426b-8a54-6b8441445 223.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_69.py
+++ Python_69.py
@@ -1,12 +1,7 @@
-def most_frequent(lst):
-    freq_dict = {}
+def find_max_positive(lst):
+    max_pos = -1
     for num in lst:
         if num > 0:
-            if num in freq_dict:
-                freq_dict[num] += 1
-            else:
-                freq_dict[num] = 1
-    if not freq_dict:
-        return -1
-    max_num = max(freq_dict, key=freq_dict.get)
-    return max_num
+            if num > max_pos:
+                max_pos = num
+    return max_pos if max_pos != -1 else None
