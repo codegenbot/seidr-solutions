3936ff1b98349154bb26429bd9060feadc29478b
beep boop <githubbot@vadim.me>
2024-07-09 23:18:18 +0200

    Solution 49 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/z9gvrtr6 Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b63854a6-9fba-4930-8b53-f4d0a8a10cc8.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ b63854a6-9fba-4930-8b53-f4d0a8a10cc8.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from b63854a6-9fba-4930-8b53-f4d0a8a10cc8.cpp:11: b63854a6-9fba-4930-8b53-f4d0a8a10cc8.cpp: In function ‘int main()’: b63854a6-9fba-4930-8b53-f4d0a8a10cc8.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ b63854a6-9fba-4930-8b53-f4d0a8a10cc8.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <string>
+#include <algorithm>
 
 bool issame(const std::string& a, const std::string& b) {
     return a.length() == b.length() && a == b;
@@ -9,6 +8,7 @@ bool issame(const std::string& a, const std::string& b) {
 
 std::vector<std::string> sorted_list_sum(const std::vector<std::string>& lst) {
     std::vector<std::string> sorted_lst = lst;
+    sorted_lst.erase(std::remove_if(sorted_lst.begin(), sorted_lst.end(), [](const std::string& s) { return s.length() % 2 != 0; }), sorted_lst.end());
     std::sort(sorted_lst.begin(), sorted_lst.end(), [](const std::string& a, const std::string& b) {
         if (a.length() != b.length()) {
             return a.length() < b.length();
@@ -17,13 +17,4 @@ std::vector<std::string> sorted_list_sum(const std::vector<std::string>& lst) {
         }
     });
     return sorted_lst;
-}
-
-int main() {
-    std::vector<std::string> lst = {"apple", "banana", "orange", "kiwi", "pear", "grape"};
-    
-    lst.erase(std::remove_if(lst.begin(), lst.end(), [](const std::string& s) { return s.length() % 2 != 0; }), lst.end());
-    std::vector<std::string> result = sorted_list_sum(lst);
-    
-    return 0;
 }
