a938d96677554b25f2e875313b2b0b7bd2c0efe4
beep boop <githubbot@vadim.me>
2024-07-09 06:37:48 +0200

    Solution 44 of Python/25, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/i4zfmdg1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6c5fe2b5-fcad-43c5-977e-1d7aad56d 43e.py", line 1, in <module>     def factorize(n: int) -> List[int]:                              ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_25.py
+++ Python_25.py
@@ -1,14 +1,12 @@
-Here is the solution:
-
-def factorize(n: int) -> List[int]:
-    i = 2
-    factors = []
-    while n > 1:
-        count = 0
-        while n % i == 0:
-            n //= i
-            count += 1
-        if count > 0:
-            factors.extend([i] * count)
-        i += 1
+def factorize(n: int) -> list[int]: 
+    i = 2 
+    factors = [] 
+    while n > 1: 
+        count = 0 
+        while n % i == 0: 
+            n //= i 
+            count += 1 
+        if count > 0: 
+            factors.extend([i] * count) 
+        i += 1 
     return factors
