946ccf621ef3750ab0201f27348b4445ee412031
beep boop <githubbot@vadim.me>
2024-07-09 10:00:09 +0200

    Solution 17 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/c8pse80p Prompt:    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8897aeaa-e002-4ce1-be66-a7739c4c791b.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8897aeaa-e002-4ce1-be66-a7739c4c791b.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8897aeaa-e002-4ce1-be66-a7739c4c791b.cpp:24: 8897aeaa-e002-4ce1-be66-a7739c4c791b.cpp: In function ‘int main()’: 8897aeaa-e002-4ce1-be66-a7739c4c791b.cpp:34:20: error: ‘count_up_to’ was not dec lared in this scope    34 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 8897aeaa-e002-4ce1-be66-a7739c4c791b.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,12 +1,12 @@
-bool issame(vector<int> a, vector<int> b){
+bool issame(vector<int> a, vector<int> b) {
     if(a.size() != b.size()) return false;
-    for(int i=0; i < a.size(); ++i){
+    for(int i = 0; i < a.size(); ++i) {
         if(a[i] != b[i]) return false;
     }
     return true;
 }
 
-vector<int> count_up_to(int n){
+vector<int> count_up_to(int n) {
     vector<int> result;
     if(n < 2) return result;
     
