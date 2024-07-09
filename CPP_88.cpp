88130929bfb7fcc49240a44f961545f7e538a9a4
beep boop <githubbot@vadim.me>
2024-07-09 03:50:53 +0200

    Solution 14 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/g6w1o3fa Prompt:       |                    ^~~ 72bb679f-8777-4d3c-930f-da77f8084acf.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 72bb679f-8777-4d3c-930f-da77f8084acf.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 72bb679f-8777-4d3c-930f-da77f8084acf.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 72bb679f-8777-4d3c-930f-da77f8084acf.cpp:13: 72bb679f-8777-4d3c-930f-da77f8084acf.cpp: In function ‘int main()’: 72bb679f-8777-4d3c-930f-da77f8084acf.cpp:23:20: error: ‘sort_array’ was not decl ared in this scope    23 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 72bb679f-8777-4d3c-930f-da77f8084acf.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,33 +1,39 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
-bool issame(vector<int>& v1,vector<int>& v2){
-    return (v1.size() == v2.size()) && equal(v1.begin(),v1.end(),v2.begin());
+bool issame(int a, int b) {
+    return (a == b);
 }
 
-vector<int> sort_array(vector<int>& array) {
-    int sum = 0;
-    for(int i = 0; i < array.size(); i++)
-        sum += array[i];
-    
-    if(sum % 2 == 0){
-        sort(array.begin(), array.end(), greater<int>());
+std::vector<int> sort_array(std::vector<int> array) {
+    if(array.empty()) {
+        return array;
     }
-    else{
-        sort(array.begin(), array.end());
-    }
+
+    std::sort(array.begin(), array.end(), [](int a, int b) {
+        return (!issame(a,b));
+    });
+
     return array;
 }
 
 int main() {
-    vector<int> v1 = {4, 3};
-    vector<int> v2 = {1, 5};
-    
-    if(issame(v1,v2))
-        cout<<"Vectors are same\n";
-    else
-        cout<<"Vectors are not same\n";
-    vector<int> array = {10,7,8,9,1,6,4,3,2,5};
+    int n;
+    std::cout << "Enter the number of elements: ";
+    std::cin >> n;
+
+    std::vector<int> array(n);
+    for (int i = 0; i < n; i++) {
+        std::cout << "Enter element " << i+1 << ": ";
+        std::cin >> array[i];
+    }
+
     sort_array(array);
+
+    for (int x : array) {
+        std::cout << x << " ";
+    }
+    
     return 0;
 }
