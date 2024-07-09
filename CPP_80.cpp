a55ec6dc191425492c55a7166e6854969d43594f
beep boop <githubbot@vadim.me>
2024-07-09 19:19:38 +0200

    Solution 70 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g4d79e6v Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
 #include <string>
 
-bool is_happy(std::string s) {
-    std::string t = s;
-    while (t != "1") {
-        int sum = 0;
-        for (char c : t) {
-            sum += (c - '0') * (c - '0');
-        }
-        t = std::to_string(sum);
-    }
-    return true;
-}
+bool is_happy(int n);
 
 int main() {
-    std::string num;
+    std::string num_str;
     std::cout << "Enter a number: ";
-    std::cin >> num;  
+    std::cin >> num_str;  
+    int num = stoi(num_str);  
     if (is_happy(num)) {
         std::cout << num << " is a happy number.\n";
     } else {
         std::cout << num << " is not a happy number.\n";
     }
-    return 0;
 }
