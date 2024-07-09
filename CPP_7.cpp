f06cc7c322d6ce97eb0af942f42189c8f710e4de
beep boop <githubbot@vadim.me>
2024-07-10 01:09:48 +0200

    Solution 6 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/k88udogy Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 1fc25f37-7f32-434c-9f69-79f8c37ce620.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -17,7 +17,7 @@ bool issame(vector<string> a, vector<string> b) {
 vector<vector<string>> filter_by_substring(vector<vector<string>> strings, string substring) {
     vector<vector<string>> result;
     for (vector<string> s : strings) {
-        vector<string> temp(substring);
+        vector<std::string> temp({substring});
         bool same = issame(s, temp);
         if (!same) {
             result.push_back(s);
