d3829bf4ce58ee70ef84d14ecbb4aecd2873fdc1
beep boop <githubbot@vadim.me>
2024-07-10 01:49:02 +0200

    Solution 2 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/mulxind8 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,43 +1,42 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> nums) {
+vector<vector<int>> cutVector(vector<int> v) {
     int min_diff = INT_MAX;
-    int split_index = 0;
+    int pos = -1;
     
-    for (int i = 1; i < nums.size(); i++) {
-        int diff = abs(nums[i] - nums[0]);
+    for(int i = 0; i < v.size() - 1; i++) {
+        int diff = abs(v[i] - v[i+1]);
         
-        if (diff <= min_diff) {
+        if(diff <= min_diff) {
             min_diff = diff;
-            split_index = i;
+            pos = i;
         }
     }
     
-    return {{nums.begin(), nums.begin() + split_index}, {nums.begin() + split_index, nums.end()}};
+    vector<vector<int>> result(2);
+    result[0].resize(pos + 1);
+    result[1].resize(v.size() - pos - 1);
+    
+    copy(v.begin(), v.begin() + pos + 1, result[0].begin());
+    copy(v.begin() + pos + 1, v.end(), result[1].begin());
+    
+    return result;
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
     }
-    
-    pair<vector<int>, vector<int>> result = cutVector(nums);
-    
-    cout << "[";
-    for (int num : result.first) {
-        cout << num << " ";
+    vector<vector<int>> res = cutVector(v);
+    for(auto &v : res) {
+        for(int x : v) {
+            cout << x << " ";
+        }
+        cout << endl;
     }
-    cout << "]" << endl;
-    
-    cout << "[";
-    for (int num : result.second) {
-        cout << num << " ";
-    }
-    cout << "0]" << endl;
-
     return 0;
 }
