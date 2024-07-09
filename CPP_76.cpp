ce0abf471808aff85b3ec4e93d5b7a76f40b0c47
beep boop <githubbot@vadim.me>
2024-07-09 09:10:41 +0200

    Solution 21 of CPP/76, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/9s50zlnc Prompt: In file included from 7fea433f-1817-4fed-98f0-04487a997908.cpp:31: 7fea433f-1817-4fed-98f0-04487a997908.cpp: In function ‘int main()’: 7fea433f-1817-4fed-98f0-04487a997908.cpp:33:13: error: ‘is_simple_power’ was not  declared in this scope; did you mean ‘isSimplePower’?    33 |     assert (is_simple_power(1, 12)==true);       |             ^~~~~~~~~~~~~~~

--- CPP_76.cpp
+++ CPP_76.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <string>
 
-bool isSimplePower(int num); // Function declaration
+bool isSimplePower(int base, int power); 
+bool isSimplePower(int num); 
 
 int simplePowerCheckerMain() {
     std::string input;
     std::cout << "Enter a number: ";
     std::cin >> input;
     int num = std::stoi(input);
-    if (isSimplePower(num)) {
+    if (isSimplePower(num)) {  
         std::cout << input << " is a simple power.\n";
     } else {
         std::cout << input << " is not a simple power.\n";
@@ -17,13 +18,14 @@ int simplePowerCheckerMain() {
 }
 
 bool isSimplePower(int num) {
-    int root = 1;
-    while (root * root <= num) {
-        if ((root * root) == num) {
-            return true;
+    for(int base = 2; base * base <= num; ++base) {
+        int power = 1;
+        while (power * base <= num) {
+            if ((power * base) == num) {
+                return true;
+            }
+            ++power;
         }
-        root++;
     }
     return false;
 }
-assert(isSimplePower(16) == true);
