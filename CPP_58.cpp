866f3e68908b7f72d23bdcfb0da338d97e8047af
beep boop <githubbot@vadim.me>
2024-07-09 12:11:29 +0200

    Solution 32 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/w26ru0dn Prompt:       |                    ^~~ a7b173a9-2598-4465-a182-7e05ee048596.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a7b173a9-2598-4465-a182-7e05ee048596.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a7b173a9-2598-4465-a182-7e05ee048596.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a7b173a9-2598-4465-a182-7e05ee048596.cpp:17: a7b173a9-2598-4465-a182-7e05ee048596.cpp: In function ‘int main()’: a7b173a9-2598-4465-a182-7e05ee048596.cpp:27:20: error: ‘common’ was not declared  in this scope    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ a7b173a9-2598-4465-a182-7e05ee048596.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -5,23 +5,7 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
-    std::set<int> s1(l1.begin(), l1.end());
-    std::set<int> s2(l2.begin(), l2.end());
-    std::set<int> intersection;
-    
-    for (int x : s1) {
-        if (s2.find(x) != s2.end()) {
-            intersection.insert(x);
-        }
-    }
-    
-    std::vector<int> result(intersection.begin(), intersection.end());
-    sort(result.begin(), result.end());
-    return result;
-}
-
 int main() {
     assert(issame(common({4, 3, 2, 8}, {}), {}));
-    return 0;
+    // Your code here...
 }
