74d54c587b966504ca7c11d930e007738e3b481f
beep boop <githubbot@vadim.me>
2024-07-10 00:58:15 +0200

    Solution 26 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/xub5v5hi Prompt: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::__cxx11::basi c_string<_CharT, _Traits, _Alloc>::basic_string(std::__cxx11::basic_string<_Char T, _Traits, _Alloc>&&) [with _CharT = char; _Traits = std::char_traits<char>; _A lloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:6667:12:   required from here /usr/include/c++/11/bits/basic_string.h:576:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   576 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,26 +1,29 @@
 ```cpp
+#include <string>
+using namespace std;
+
 int bowlingScore(string input) {
     int score = 0;
     int i = 0;
-    while (i < 10) {
-        if (input[i] == 'X') {
+    while(i < input.length()) {
+        if(input[i] == 'X') {
             score += 30;
             i++;
-        } else if (input[i] == '/') {
-            int nextTwo = stoi(input.substr(i + 1, 2));
-            score += 10 + nextTwo;
+        } else if(input[i] == '/') {
+            std::string nextTwo = std::string(input.substr(i+1, 1));
+            score += 10 + (nextTwo == "/" ? 0 : stoi(nextTwo));
             i += 3;
         } else {
-            int currentRoll = stoi(input.substr(i, 2));
-            if (currentRoll < 10) {
+            std::string roll = std::string(input.substr(i,2));
+            int currentRoll = (roll[0] == 'X') ? 10 : ((roll[1] == '/') ? 10 - stoi(roll.substr(0,1)) : stoi(roll));
+            if(currentRoll < 10) {
                 score += currentRoll;
-                i += 2;
             } else {
-                int first = stoi(input.substr(i, 1));
-                int second = stoi(input.substr(i + 1, 1));
+                int first = (input[i] == 'X') ? 10 : (input[i+1] == '/') ? 10 - stoi(std::string(input.substr(i,1))) : stoi(std::string(input.substr(i,1)));
+                int second = (input[i+1] == '/') ? 0 : ((input[i+2] == 'X') ? 10 :stoi(std::string(input.substr(i+1,1))));
                 score += first + second;
-                i += 2;
             }
+            i += 2;
         }
     }
     return score;
