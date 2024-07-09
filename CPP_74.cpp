0e2f620ea7ba68f12c14d166ce90d98f4596d353
beep boop <githubbot@vadim.me>
2024-07-09 04:02:49 +0200

    Solution 46 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8ahd9aop Prompt:  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 339ba246-7437-4d6a-ae25-267fc2734ce4.cpp:26:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 339ba246-7437-4d6a-ae25-267fc2734ce4.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 339ba246-7437-4d6a-ae25-267fc2734ce4.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 339ba246-7437-4d6a-ae25-267fc2734ce4.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 339ba246-7437-4d6a-ae25-267fc2734ce4.cpp:25: 339ba246-7437-4d6a-ae25-267fc2734ce4.cpp: In function ‘int main()’: 339ba246-7437-4d6a-ae25-267fc2734ce4.cpp:35:31: error: ‘total_match’ cannot be u sed as a function    35 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ~~~~~~~~~~~^~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -4,15 +4,7 @@
 bool isSame(std::vector<std::string> a, std::vector<std::string> b) {
     if(a.size() != b.size()) return false;
     for(int i = 0; i < a.size(); i++) {
-        if(!issame(a[i],b[i])) return false;
-    }
-    return true;
-}
-
-bool issame(std::string s1, std::string s2) {
-    if(s1.size() != s2.size()) return false;
-    for(int i = 0; i < s1.size(); i++) {
-        if(s1[i] != s2[i]) return false;
+        if(a[i] != b[i]) return false;
     }
     return true;
 }
@@ -27,5 +19,6 @@ std::vector<string> totalMatch(vector<string> lst1, vector<string> lst2) {
     }
     if (sum1 < sum2) return lst1;
     else if (sum1 > sum2) return lst2;
-    else return {lst1[0], lst2[0]};
+    else if(isSame(lst1, lst2)) return lst1;
+    else return lst2;
 }
