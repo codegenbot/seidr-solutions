032efed6275ae2b59f8c9c93aaed6f6c05f5fb86
beep boop <githubbot@vadim.me>
2024-07-09 02:47:16 +0200

    Solution 4 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/bvoim4el Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b57517bf-2f16-48c2-ab22-c5a2039be55a.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b57517bf-2f16-48c2-ab22-c5a2039be55a.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b57517bf-2f16-48c2-ab22-c5a2039be55a.cpp:11: b57517bf-2f16-48c2-ab22-c5a2039be55a.cpp: In function ‘int main()’: b57517bf-2f16-48c2-ab22-c5a2039be55a.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ b57517bf-2f16-48c2-ab22-c5a2039be55a.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,8 +1,22 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool issame(vector<int> a,vector<int>b){
+    if(a.size() != b.size()) return false;
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]) return false;
+    }
+    return true;
+}
+
 vector<int> filter_integers(list_any values) {
     vector<int> result;
     for (const auto& value : values) {
         if (boost::any_cast<int>(value).good()) {
-            result.push_back(boost::any_cast<int>(value).get());
+            result.push_back(boost::any_cast<int>(value));
         }
     }
     return result;
