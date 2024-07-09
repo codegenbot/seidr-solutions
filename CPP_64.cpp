d88c355256a7d4f66556c36d7573607312301c87
beep boop <githubbot@vadim.me>
2024-07-09 05:36:25 +0200

    Solution 11 of CPP/64, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/a339knbk Prompt: Complete the following code given the task description and function signature.

--- CPP_64.cpp
+++ CPP_64.cpp
@@ -1,11 +1,12 @@
 int vowels_count(string s){
     int count = 0;
-    for(int i=0; i<s.length(); i++){
-        char c = tolower(s[i]);
-        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+    for (char c : s) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
             count++;
-        else if(i==s.length()-1 && c == 'y')
+        }
+        else if (c == 'y' && s.length() - 1 == find(s.begin(), s.end(), c) - s.begin()) {
             count++;
+        }
     }
     return count;
 }
