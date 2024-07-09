179ddaa0e57b897e6fd4933feaaf8acd1977c7a4
beep boop <githubbot@vadim.me>
2024-07-09 18:22:35 +0200

    Solution 38 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0r6o7f3v Prompt: 706408de-1df4-4c07-9a9f-f19afdc7dc71.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~ 706408de-1df4-4c07-9a9f-f19afdc7dc71.cpp: In function ‘int main()’: 706408de-1df4-4c07-9a9f-f19afdc7dc71.cpp:19:5: error: ‘vector’ was not declared in this scope    19 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 706408de-1df4-4c07-9a9f-f19afdc7dc71.cpp:19:5: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 706408de-1df4-4c07-9a9f-f19afdc7dc71.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 706408de-1df4-4c07-9a9f-f19afdc7dc71.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 706408de-1df4-4c07-9a9f-f19afdc7dc71.cpp:19:12: error: expected primary-expressi on before ‘float’    19 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,7 +1,9 @@
 #include <vector>
 #include <assert.h>
 
-int has_close_elements(std::vector<float> v, float epsilon) {
+using namespace std;
+
+int has_close_elements(vector<float> v, float epsilon) {
     for (size_t i = 0; i < v.size() - 1; ++i)
         if ((v[i] >= v[i + 1] - epsilon) && (v[i] <= v[i + 1] + epsilon))
             return true;
@@ -9,7 +11,7 @@ int has_close_elements(std::vector<float> v, float epsilon) {
 }
 
 int main() {
-    std::vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
-    assert(has_close_elements(std::vector<float>{1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f}, 0.5) == false);
+    vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
+    assert(has_close_elements({1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f}, 0.5) == false);
     return 0;
 }
