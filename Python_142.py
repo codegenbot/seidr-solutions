c5fc7bb5763b8a2d6eb07de689dcd997399d2084
beep boop <githubbot@vadim.me>
2024-07-09 05:12:01 +0200

    Solution 39 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/22jclxwk Prompt: Program did not receive expected input

--- Python_142.py
+++ Python_142.py
@@ -9,5 +9,3 @@ def sum_squares():
     return sum(x**2 if i % 3 == 0 and i < len(lst) - 1 and (i + 1) % 4 != 0 \
                else x**3 if i % 4 == 0 and i < len(lst) - 2 and (i + 2) % 3 != 0 \
                else x**2 for i, x in enumerate(lst))
-
-print(sum_squares())
