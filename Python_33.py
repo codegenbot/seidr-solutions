64003ad6f0c0791d0c3db9782b1a5716fb817504
beep boop <githubbot@vadim.me>
2024-07-09 19:39:42 +0200

    Solution 76 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/76nneozn Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9a587de8-c4f6-4c0b-a19f-c87ed28ba ce8.py", line 11, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9a587de8-c4f6-4c0b-a19f-c87ed28ba ce8.py", line 6, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,4 @@
 def sort_third(l: list):
-    sorted_list = sorted(l, key=lambda x: x[2] if len(x) >= 3 else float('inf'))
-    groups = [sorted_list[i:i+3] for i in range(0, len(sorted_list), 3)]
+    sorted_list = sorted(l, key=lambda x: x[2] if len(x) >= 3 else float("inf"))
+    groups = [sorted_list[i : i + 3] for i in range(0, len(sorted_list), 3)]
     return [elem for group in groups for elem in group]
