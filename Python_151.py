a9ce46d6d42459bbf6c8c11e168961eb0db9b5b3
beep boop <githubbot@vadim.me>
2024-07-10 00:50:07 +0200

    Solution 61 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/nl7llnlq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/29356f6c-34cc-4fb5-ab5e-5c6531e80 f66.py", line 12, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/29356f6c-34cc-4fb5-ab5e-5c6531e80 f66.py", line 8, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,8 +1,7 @@
 def double_the_difference(lst):
-    odd_sum = sum(x for x in lst if x > 0 and isinstance(x, int) and x % 2 != 0)
+    odd_sum = sum(x**2 for x in lst if x > 0 and isinstance(x, int) and x % 2 != 0)
     return odd_sum * 2
 
-# Example usage:
-my_list = [1, 2, 3]
-result = double_the_difference(my_list)
+# Calling the function with a list
+result = double_the_difference([1, 2, 3])
 print(result)
