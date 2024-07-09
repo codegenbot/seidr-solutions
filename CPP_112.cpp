393244f8b3e62a4211735011071458ff80d52d6f
beep boop <githubbot@vadim.me>
2024-07-09 11:21:42 +0200

    Solution 65 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/owo9prrk Prompt: In file included from /usr/include/c++/11/string:39,                  from 26878838-e219-49c9-a2af-c391db8c9e5b.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 26878838-e219-49c9-a2af-c391db8c9e5b.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 26878838-e219-49c9-a2af-c391db8c9e5b.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 26878838-e219-49c9-a2af-c391db8c9e5b.cpp:19: 26878838-e219-49c9-a2af-c391db8c9e5b.cpp: In function ‘int main()’: 26878838-e219-49c9-a2af-c391db8c9e5b.cpp:29:20: error: ‘reverse_delete’ was not declared in this scope; did you mean ‘timer_delete’?    29 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 26878838-e219-49c9-a2af-c391db8c9e5b.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -2,22 +2,22 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-std::vector<std::string> reverse_delete(std::string s, std::string c);
+vector<string> reverse_delete(string s, string c);
 
-std::vector<std::string> reverse_delete(std::string s, std::string c){
-    std::string result = "";
+vector<string> reverse_delete(string s, string c){
+    string result = "";
     for(char ch : s){
-        if(c.find(ch) == std::string::npos){
+        if(c.find(ch) == string::npos){
             result += ch;
         }
     }
-    std::string result_reversed = result;
-    std::reverse(result_reversed.begin(), result_reversed.end());
-    return std::vector<std::string>{result, result == result_reversed ? "True" : "False"};
+    string result_reversed = result;
+    reverse(result_reversed.begin(), result_reversed.end());
+    return vector<string>{result, result == result_reversed ? "True" : "False"};
 }
 
 int main() {
