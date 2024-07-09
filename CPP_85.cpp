5c284d41c6b87b1c3d9649a48ab2d6737611fe1e
beep boop <githubbot@vadim.me>
2024-07-09 12:35:46 +0200

    Solution 40 of CPP/85, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/uyc2g709 Prompt: 96f454e5-d574-419a-b055-ab96ae7ef596.cpp:1:9: error: ‘vector’ was not declared i n this scope     1 | int add(vector<int> lst){       |         ^~~~~~ 96f454e5-d574-419a-b055-ab96ae7ef596.cpp:1:16: error: expected primary-expressio n before ‘int’     1 | int add(vector<int> lst){       |                ^~~ In file included from 96f454e5-d574-419a-b055-ab96ae7ef596.cpp:10: 96f454e5-d574-419a-b055-ab96ae7ef596.cpp: In function ‘int main()’: 96f454e5-d574-419a-b055-ab96ae7ef596.cpp:12:16: error: ‘add’ cannot be used as a  function    12 |     assert (add({4, 4, 6, 8}) == 12);       |             ~~~^~~~~~~~~~~~~~

--- CPP_85.cpp
+++ CPP_85.cpp
@@ -1,9 +1,10 @@
-int add(vector<int> lst) {
-    int result = 0;
-    for (int i = 1; i < lst.size(); i += 2) {
-        if (lst[i] % 2 == 0) {
-            result += lst[i];
-        }
+#include <vector>
+
+int add(std::vector<int> lst){
+    int sum = 0;
+    for(int i = 1; i < lst.size(); i += 2) {
+        if(lst[i] % 2 == 0)
+            sum += lst[i];
     }
-    return result;
+    return sum;
 }
