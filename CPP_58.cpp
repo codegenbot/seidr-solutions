c191a400a1480c1909e9c54cf3061aa0cb9f69d9
beep boop <githubbot@vadim.me>
2024-07-10 01:56:48 +0200

    Solution 4 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0hw5ynmd Prompt: loc>::node_type = std::_Rb_tree<int, int, std::_Identity<int>, std::less<int>, s td::allocator<int> >::node_type]’   603 |       insert(const_iterator __hint, node_type&& __nh)       |       ^~~~~~ /usr/include/c++/11/bits/stl_set.h:603:7: note:   candidate expects 2 arguments,  1 provided In file included from /usr/include/c++/11/algorithm:62,                  from 8cd0a19f-e516-441f-82fd-0fd02579fb30.cpp:2: /usr/include/c++/11/bits/stl_algo.h: In instantiation of ‘_OutputIterator std::_ _set_intersection(_InputIterator1, _InputIterator1, _InputIterator2, _InputItera tor2, _OutputIterator, _Compare) [with _InputIterator1 = std::_Rb_tree_const_ite rator<int>; _InputIterator2 = std::_Rb_tree_const_iterator<int>; _OutputIterator  = std::_Rb_tree_const_iterator<int>; _Compare = __gnu_cxx::__ops::_Iter_less_it er]’: /usr/include/c++/11/bits/stl_algo.h:5296:48:   required from ‘_OIter std::set_in tersection(_IIter1, _IIter1, _IIter2, _IIter2, _OIter) [with _IIter1 = std::_Rb_ tree_const_iterator<int>; _IIter2 = std::_Rb_tree_const_iterator<int>; _OIter = std::_Rb_tree_const_iterator<int>]’ 8cd0a19f-e516-441f-82fd-0fd02579fb30.cpp:11:41:   required from here /usr/include/c++/11/bits/stl_algo.h:5246:23: error: assignment of read-only loca tion ‘__result.std::_Rb_tree_const_iterator<int>::operator*()’  5246 |             *__result = *__first1;       |             ~~~~~~~~~~^~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,9 +1,11 @@
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
-    vector<int> result;
-    copy(std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), result.begin()), result.begin() + 0, result.begin() + std::distance(result.begin(), result.end()));
-    
-    return result;
+    return vector<int>(set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end()), set_intersection(s1.end()-1, s1.end(), s2.end()-1, s2.end()));
 }
