017266a10c6e012804f1ac2f1498c759e796f1e7
beep boop <githubbot@vadim.me>
2024-07-09 12:29:10 +0200

    Solution 28 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ejnsoy1t Prompt:       |                              ^~~~~~ 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp:32: 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp: In function ‘int main()’: 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp:42:20: error: ‘numerical_letter_grade’ was not declared in this scope    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,20 +1,22 @@
-bool issame(vector<string> a,vector<string>b){
-    for(int i=0;i<a.size();i++){
-        if(a[i] != b[i]){
-            return false;
-        }
+bool issame(vector<string> a, vector<string>b){
+    if(a.size() != b.size()) return false;
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]) return false;
     }
     return true;
 }
 
 int main() {
-    vector<float> grades = {4.2, 3.8, 3.5, 2.9};
+    vector<float> grades = {3.5, 4.2, 3.8, 2.9};
     vector<string> result = numerical_letter_grade(grades);
     
-    for (string grade : result) {
-        if (!issame(vector<string>(1, "A+"), vector<string>(1, grade))) {
-            cout << "The letter grade is: " << grade << endl;
-        }
+    for(string s : result) cout << s << endl;
+    
+    if(result.size() > 0 && issame({"A+","A+","A","B-" },result)) {
+        cout << "The grades are same as the expected ones." << endl;
+    } else {
+        cout << "The grades are not same as the expected ones." << endl;
     }
+    
     return 0;
 }
