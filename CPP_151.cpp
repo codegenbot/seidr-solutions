b06098cf54bbfa98ab3a2bc58cd8d0af5c215274
beep boop <githubbot@vadim.me>
2024-07-09 03:05:55 +0200

    Solution 75 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/0re3q8c0 Prompt: e85cf3e8-a5c7-4f64-839c-e31c9d8ada9c.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<float> lst = {1.5, 2.0, 3.5};       | ^~~~~~ In file included from e85cf3e8-a5c7-4f64-839c-e31c9d8ada9c.cpp:3: e85cf3e8-a5c7-4f64-839c-e31c9d8ada9c.cpp: In function ‘int main()’: e85cf3e8-a5c7-4f64-839c-e31c9d8ada9c.cpp:5:35: error: ‘lst’ was not declared in this scope     5 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ e85cf3e8-a5c7-4f64-839c-e31c9d8ada9c.cpp:5:13: error: ‘double_the_difference’ wa s not declared in this scope     5 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ e85cf3e8-a5c7-4f64-839c-e31c9d8ada9c.cpp:5:43: error: ‘odd_sum’ was not declared  in this scope     5 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -2,18 +2,17 @@
 #include <vector>
 #include <assert.h>
 
-int double_the_difference(std::vector<float> lst) {
+int double_the_difference(vector<float> lst) {
     int odd_sum = 0;
     for (float num : lst) {
-        if ((int)num % 2 != 0)
-            odd_sum += (int)num * 2;
+        if (num % 1 != 0)
+            odd_sum += (int)(num * 2);
     }
     return odd_sum;
 }
 
 int main() {
-    std::vector<float> lst = {1.5, 2.0, 3.5};
-    int result = double_the_difference(lst);
-    // assert(result == 7); 
+    vector<float> lst = {1.5, 2.0, 3.5};
+    assert(double_the_difference(lst) == 6);
     return 0;
 }
