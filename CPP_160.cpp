c8015b31532ca24de68b872178f50a295f4eeba5
beep boop <githubbot@vadim.me>
2024-07-09 04:34:10 +0200

    Solution 3 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xuigdtqp Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,17 +1,107 @@
 int do_algebra(vector<string> operator_, vector<int> operand) {
-    int result = operand[0];
+    string expression = "";
     for (int i = 0; i < operator_.size(); i++) {
-        if (operator_[i] == "+") {
-            result += operand[i + 1];
-        } else if (operator_[i] == "-") {
-            result -= operand[i + 1];
-        } else if (operator_[i] == "*") {
-            result *= operand[i + 1];
-        } else if (operator_[i] == "//") {
-            result = result / operand[i + 1];
-        } else if (operator_[i] == "**") {
-            result = pow(result, operand[i + 1]);
+        expression += to_string(operand[i]);
+        expression += operator_[i];
+    }
+    expression += to_string(operand[operator_.size()]);
+    
+    int result = eval(expression);
+    
+    return result;
+}
+
+long long eval(const string& s) {
+    int n = s.size();
+    stack<int> st;
+    for (int i = 0; i < n; i++) {
+        if (isdigit(s[i])) {
+            int num = 0;
+            while (i < n && isdigit(s[i])) {
+                num = num * 10 + (s[i] - '0');
+                i++;
+            }
+            st.push(num);
+        } else if (s[i] == '(') {
+            i++;
+            stack<int> temp;
+            while (i < n && s[i] != ')') {
+                if (isdigit(s[i])) {
+                    int num = 0;
+                    while (i < n && isdigit(s[i])) {
+                        num = num * 10 + (s[i] - '0');
+                        i++;
+                    }
+                    temp.push(num);
+                } else if (s[i] == '(') {
+                    temp.push(eval("(" + s.substr(i++) + ")"));
+                } else if (s[i] == ')') {
+                    break;
+                }
+            }
+            while (!temp.empty()) {
+                st.push(temp.top());
+                temp.pop();
+            }
         }
     }
-    return result;
+    
+    int res = 0;
+    char op = '+';
+    while (!st.empty()) {
+        if (op == '+') {
+            res += st.top();
+            st.pop();
+        } else if (op == '-') {
+            res -= st.top();
+            st.pop();
+        } else if (op == '*') {
+            int num = st.top() * 1;
+            st.pop();
+            while (!st.empty() && st.top() == '(') {
+                st.pop();
+            }
+            if (!st.empty()) {
+                num *= st.top();
+                st.pop();
+            }
+            res += num;
+        } else if (op == '/') {
+            int num = st.top() / 1;
+            st.pop();
+            while (!st.empty() && st.top() == '(') {
+                st.pop();
+            }
+            if (!st.empty()) {
+                num /= st.top();
+                st.pop();
+            }
+            res += num;
+        } else if (op == '**') {
+            int num = pow(st.top(), 1);
+            st.pop();
+            while (!st.empty() && st.top() == '(') {
+                st.pop();
+            }
+            if (!st.empty()) {
+                num = pow(num, st.top());
+                st.pop();
+            }
+            res += num;
+        } else if (op == '//') {
+            int num = st.top() / 1;
+            st.pop();
+            while (!st.empty() && st.top() == '(') {
+                st.pop();
+            }
+            if (!st.empty()) {
+                num /= st.top();
+                st.pop();
+            }
+            res += num;
+        }
+        op = s[i++];
+    }
+    
+    return res;
 }
