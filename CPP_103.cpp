657e5a94b5dfe42b21dd4a1ab56b7477ce7cef61
beep boop <githubbot@vadim.me>
2024-07-09 15:23:58 +0200

    Solution 2 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/prxkw4kp Prompt: Complete the following code given the task description and function signature.

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -10,15 +10,12 @@ string rounded_avg(int n, int m) {
         sum += i;
     }
     double avg = round(sum / (m - n + 1));
-    string binary = "";
+    string res = "";
     while (avg > 0) {
-        if (avg >= 2) {
-            avg /= 2;
-            binary = "1" + binary;
-        } else {
-            avg++;
-            binary = "0" + binary;
-        }
+        if (avg & 1) res.push_back('1');
+        else res.push_back('0');
+        avg /= 2;
     }
-    return binary;
+    reverse(res.begin(), res.end());
+    return res;
 }
