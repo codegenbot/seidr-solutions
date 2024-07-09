2065b84d26ef312ffb66cfa4bfe7b3db65f94d35
beep boop <githubbot@vadim.me>
2024-07-09 03:44:20 +0200

    Solution 87 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h5y6hrvk Prompt: e99f59e8-bd40-4bb8-82b8-b7958be4147d.cpp:30:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    30 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff e99f59e8-bd40-4bb8-82b8-b7958be4147d.cpp:33:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    33 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff e99f59e8-bd40-4bb8-82b8-b7958be4147d.cpp:33:36: error: ‘find_zero’ cannot be use d as a function    33 |         solution = find_zero(coeffs);       |                                    ^ In file included from e99f59e8-bd40-4bb8-82b8-b7958be4147d.cpp:17: e99f59e8-bd40-4bb8-82b8-b7958be4147d.cpp:34:21: error: ‘poly’ was not declared i n this scope    34 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ e99f59e8-bd40-4bb8-82b8-b7958be4147d.cpp:34:17: error: ‘abs’ was not declared in  this scope    34 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,18 +1,22 @@
-```cpp
 double find_zero(vector<double> xs) {
-    double c = xs[0];
+    double coeffs = xs[0];
     vector<double> polyCoeffs;
     int n = xs.size();
     for (int i = 1; i < n; i++) {
         if ((i & 1) == 0) {
-            polyCoeffs.insert(polyCoeffs.begin(), -c*xs[i]);
+            coeffs = -coeffs * xs[i];
+            polyCoeffs.push_back(coeffs);
         } else {
-            c = xs[i];
+            coeffs *= xs[i];
         }
     }
-    double sum = 0;
-    for (int i = 0, p = c; i < polyCoeffs.size(); i++, p *= -1) {
-        sum += p * polyCoeffs[i];
+    double res = 0;
+    for (int i = 0; i < polyCoeffs.size(); i++) {
+        if (i % 2 == 1) {
+            res -= polyCoeffs[i];
+        } else {
+            res += polyCoeffs[i];
+        }
     }
-    return -c / sum;
+    return -res / coeffs;
 }
