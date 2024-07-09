63dbf7ef9c52ffbef9c390853f2d6449abf5594b
beep boop <githubbot@vadim.me>
2024-07-09 20:40:39 +0200

    Solution 36 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8tmp81k3 Prompt:                  from /usr/include/c++/11/vector:60,                  from fd3e5ef5-57c1-4c01-9442-a07c2db3992d.cpp:1: /usr/include/c++/11/bits/stl_pair.h:211:12: note: ‘std::pair’ declared here   211 |     struct pair       |            ^~~~ fd3e5ef5-57c1-4c01-9442-a07c2db3992d.cpp:4:28: error: expected primary-expressio n before ‘char’     4 | int do_algebra(vector<pair<char, int>> operators_and_operands) {       |                            ^~~~ fd3e5ef5-57c1-4c01-9442-a07c2db3992d.cpp:4:34: error: expected primary-expressio n before ‘int’     4 | int do_algebra(vector<pair<char, int>> operators_and_operands) {       |                                  ^~~ fd3e5ef5-57c1-4c01-9442-a07c2db3992d.cpp:4:62: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | int do_algebra(vector<pair<char, int>> operators_and_operands) {       |                                                              ^ In file included from fd3e5ef5-57c1-4c01-9442-a07c2db3992d.cpp:28: fd3e5ef5-57c1-4c01-9442-a07c2db3992d.cpp: In function ‘int main()’: fd3e5ef5-57c1-4c01-9442-a07c2db3992d.cpp:30:23: error: ‘do_algebra’ cannot be us ed as a function    30 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,38 +1,39 @@
+#include <iostream>
 #include <vector>
 #include <utility>
 #include <cmath>
 
-struct pair {
-    char first;
-    int second;
-};
+using namespace std;
 
-int do_algebra(std::vector<pair> operators_and_operands) {
+int do_algebra(vector<pair<char, char>> operators_and_operands) {
     if (operators_and_operands.size() == 0) {
         return 0;
     }
     
-    int result = operators_and_operands[0].second;
+    int result = operators_and_operands[0].second - '0'; 
     for (const auto& pair : operators_and_operands) {
-        if (pair.first == '+') {
-            result += pair.second;
-        } else if (pair.first == '-') {
-            result -= pair.second;
-        } else if (pair.first == '*') {
-            result *= pair.second;
-        } else if (pair.first == '/') {
-            if (pair.second != 0) {
-                result /= pair.second;
+        char op = pair.first;
+        int num = pair.second - '0';
+        
+        if (op == '+') {
+            result += num;
+        } else if (op == '-') {
+            result -= num;
+        } else if (op == '*') {
+            result *= num;
+        } else if (op == '/') {
+            if (num != 0) {
+                result /= num;
             }
-        } else if (pair.first == '^') { 
-            result = std::pow(result, pair.second);
+        } else if (op == '^') { 
+            result = pow(result, num);
         }
     }
     return result;
 }
 
 int main() {
-    assert(do_algebra({{'+', 7}, {'*', 3}, {'+', 4}}) == 14);
-    // You can add more test cases here.
+    vector<pair<char, char>> operators_and_operands = { {'+', '7'}, {'*', '3'}, {'/', '4'} };
+    cout << do_algebra(operators_and_operands) << endl;
     return 0;
 }
