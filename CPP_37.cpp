82031f9bb599adc157ed3d4029069a1eeaf8ba25
beep boop <githubbot@vadim.me>
2024-07-09 14:07:33 +0200

    Solution 16 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7dqgrwjn Prompt: db19a58d-2cc1-4910-8a93-2715f6cfc2c2.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ db19a58d-2cc1-4910-8a93-2715f6cfc2c2.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main() {       |     ^~~~ In file included from db19a58d-2cc1-4910-8a93-2715f6cfc2c2.cpp:39: db19a58d-2cc1-4910-8a93-2715f6cfc2c2.cpp: In function ‘int main()’: db19a58d-2cc1-4910-8a93-2715f6cfc2c2.cpp:49:20: error: ‘sort_even’ was not decla red in this scope; did you mean ‘sortEven’?    49 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,7 +1,5 @@
-```cpp
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
 using namespace std;
 
@@ -13,7 +11,7 @@ bool isSame(vector<float> a,vector<float>b){
     return true;
 }
 
-vector<float> sort_even(vector<float> l) {
+vector<float> sortEven(vector<float> l) {
     vector<float> result(l.size());
     int j = 0;
     for (int i = 0; i < l.size(); i++) {
@@ -33,6 +31,8 @@ vector<float> sort_even(vector<float> l) {
 }
 
 int main() {
-    assert(isSame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    vector<float> input = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
+    vector<float> output = sortEven(input);
+    assert(isSame(output , {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
     return 0;
 }
