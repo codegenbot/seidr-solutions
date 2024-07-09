c297e28f26e66a31bf302fa3e80e55e8606f3e53
beep boop <githubbot@vadim.me>
2024-07-09 09:48:40 +0200

    Solution 41 of CPP/83, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jys0vc4c Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -1,23 +1,9 @@
-#include <iostream>
-#include <string>
-#include <sstream>
-using namespace std;
-
-int starts_one_ends(int n);
-
-int main() {
-    int n = 0; 
-    cout << "Enter a positive integer: ";
-    cin >> n;
-    int result = starts_one_ends(n);
-    cout << "Numbers of " << n << "-digit positive integers that start or end with 1: " << result << endl;
-    return 0;
-}
-
+```cpp
 int starts_one_ends(int n) {
     int count = 0;
-    for (int i = 1; i <= pow(10, n-1); i++) {
-        if ((i / pow(10, n-1)) % 10 == 1 || i % 10 == 1) {
+    for (long long i = 1; i <= pow(10, n-1); i++) {
+        std::string str = std::to_string(i);
+        if (str[0] == '1' || str.back() == '1') {
             count++;
         }
     }
