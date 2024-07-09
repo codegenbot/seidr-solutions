f29aea719a66b3306de1e65ec75a8ab63bfd067a
beep boop <githubbot@vadim.me>
2024-07-09 03:04:08 +0200

    Solution 43 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kl0f1ktf Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -1,7 +1,8 @@
 ```
 def add(a=0, b=0):
-    i = 1
-    x = int(input(f"Enter the {a if i==1 else 'second'} number: ") or a)
-    i += 1
-    y = int(input(f"Enter the {b if i==1 else 'second'} number: ") or b)
+    for i in range(1,3):
+        if i == 1:
+            x = int(input(f"Enter the {a if i==1 else 'second'} number: ") or a)
+        else:
+            y = int(input(f"Enter the {b if i==2 else 'second'} number: ') or b)
     print(x + y)
