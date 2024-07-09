9ceff41c8470bbd02e6e22777abe1bac83273595
beep boop <githubbot@vadim.me>
2024-07-09 03:41:27 +0200

    Solution 7 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/wkdjg6vx Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a35daaa1-94e0-441b-bbe1-5ab95fb1f 307.py", line 1, in <module>     def sum_product(numbers: List[int]) -> Tuple[int, int]:                              ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_8.py
+++ Python_8.py
@@ -1,5 +1,7 @@
-from typing import List
-def sum_product(numbers: list[int]) -> tuple[int, int]:
+```
+from typing import List, Tuple
+
+def sum_product(numbers: list[int]) -> Tuple[int, int]:
     total_sum = 0
     product = 1
     for num in numbers:
