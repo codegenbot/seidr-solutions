45a6ee264dcfc703d19bc530e81e822ffd921930
beep boop <githubbot@vadim.me>
2024-07-09 20:35:52 +0200

    Solution 7 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/bed580vf Prompt: this scope    18 |     cin >> n;       |     ^~~ eb68ad0d-96fb-4d6f-9716-25e95beffe02.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; eb68ad0d-96fb-4d6f-9716-25e95beffe02.cpp:26:5: error: ‘cout’ was not declared in  this scope    26 |     cout << result.first << " " << result.second << endl;       |     ^~~~ eb68ad0d-96fb-4d6f-9716-25e95beffe02.cpp:26:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? eb68ad0d-96fb-4d6f-9716-25e95beffe02.cpp:26:53: error: ‘endl’ was not declared i n this scope    26 |     cout << result.first << " " << result.second << endl;       |                                                     ^~~~ eb68ad0d-96fb-4d6f-9716-25e95beffe02.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,7 +1,5 @@
 #include <vector>
 #include <unordered_map>
-#include <iostream>
-
 using namespace std;
 
 pair<int, int> findPair(vector<int>& nums, int target) {
@@ -13,7 +11,7 @@ pair<int, int> findPair(vector<int>& nums, int target) {
         }
         numMap[nums[i]] = i;
     }
-    return {-1, -1}; // or throw an exception if you prefer
+    return {-1, -1}; 
 }
 
 int main() {
