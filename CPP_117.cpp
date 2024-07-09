8be109e3aca64d96717dc1e63e24c4746e16dd98
beep boop <githubbot@vadim.me>
2024-07-09 11:09:38 +0200

    Solution 34 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/k5l0vzzw Prompt:    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 104edf9b-b33b-4a21-bd5d-be86c52993ac.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 104edf9b-b33b-4a21-bd5d-be86c52993ac.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 104edf9b-b33b-4a21-bd5d-be86c52993ac.cpp:29: 104edf9b-b33b-4a21-bd5d-be86c52993ac.cpp: In function ‘int main()’: 104edf9b-b33b-4a21-bd5d-be86c52993ac.cpp:39:20: error: ‘select_words’ was not de clared in this scope    39 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 104edf9b-b33b-4a21-bd5d-be86c52993ac.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,30 +1,4 @@
-vector<string> select_words(string s, int n) {
-    vector<string> result;
-    if (s.empty()) {
-        return result;
-    }
-    
-    string word = "";
-    int count = 0;
-    for (char c : s) {
-        if (c == ' ') {
-            if (count == n) {
-                result.push_back(word);
-            }
-            word = "";
-            count = 0;
-        } else if (isalpha(c) && strchr("aeiouAEIOU", c) == NULL) {
-            count++;
-        }
-        word += c;
-    }
-    
-    if (!word.empty() && count == n) {
-        result.push_back(word);
-    }
-    
-    return result;
-}
+bool issame(vector<string> a, vector<string> b);
 
 bool issame(vector<string> a, vector<string> b) {
     return a == b;
