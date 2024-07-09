f9ed48f7940d38a26d8c16a0c2e1a47a653a016a
beep boop <githubbot@vadim.me>
2024-07-09 21:02:08 +0200

    Solution 14 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/2wsaf73z Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bc7aa0a4-4a06-432d-8173-3b56aa16d 97f.py", line 5     return '¤' + str(format(total_price, "#.2f"))     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- shopping-list.py
+++ shopping-list.py
@@ -3,7 +3,6 @@ def main():
     prices = list(map(float, input().split()))
     discounts = list(map(int, input().split())) / 100
     total_price = sum(price - (price * discount) for price, discount in zip(prices, discounts))
-    print('¤' + str(format(total_price, "#.2f")))
+    return '¤' + str(format(total_price, "#.2f"))
 
-if __name__ == "__main__":
-    main()
+print(main())
