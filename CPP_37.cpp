f4852500556bb11a7c6f69a529f8b7b2f847f190
beep boop <githubbot@vadim.me>
2024-07-09 18:50:19 +0200

    Solution 75 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/symxc0fq Prompt:       |     ^~~~ 21000e98-91f0-41d6-822a-d8c2850b2d13.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main() {       |     ^~~~ In file included from 21000e98-91f0-41d6-822a-d8c2850b2d13.cpp:63: 21000e98-91f0-41d6-822a-d8c2850b2d13.cpp: In function ‘int main()’: 21000e98-91f0-41d6-822a-d8c2850b2d13.cpp:73:20: error: ‘sort_even’ was not decla red in this scope; did you mean ‘myspace::sort_even’?    73 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 21000e98-91f0-41d6-822a-d8c2850b2d13.cpp:13:24: note: ‘myspace::sort_even’ decla red here    13 |     std::vector<float> sort_even(const std::vector<float>& l) {       |                        ^~~~~~~~~ In file included from 21000e98-91f0-41d6-822a-d8c2850b2d13.cpp:63: 21000e98-91f0-41d6-822a-d8c2850b2d13.cpp:73:19: error: ‘issame’ cannot be used a s a function    73 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -30,31 +30,3 @@ namespace myspace {
         return result;
     }
 }
-
-int main() {
-    std::vector<float> input(10);
-    input.push_back(5.0f);
-    input.push_back(8.0f);
-    input.push_back(-12.0f);
-    input.push_back(4.0f);
-    input.push_back(23.0f);
-    input.push_back(2.0f);
-    input.push_back(3.0f);
-    input.push_back(11.0f);
-    input.push_back(12.0f);
-    input.push_back(-10.0f);
-
-    std::cout << "Original array: ";
-    for (float num : input) {
-        std::cout << num << " ";
-    }
-    std::cout << std::endl;
-
-    std::vector<float> sortedEven = myspace::sort_even(input);
-    std::cout << "Sorted even array: ";
-    for (float num : sortedEven) {
-        std::cout << num << " ";
-    }
-    std::cout << std::endl;
-
-    return 0;
