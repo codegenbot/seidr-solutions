0b0cb5b4a8b7f608b55d7dc94dac765cffbaf7b5
beep boop <githubbot@vadim.me>
2024-07-09 12:37:24 +0200

    Solution 80 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7113sxs1 Prompt: 3a066e9c-b2d6-4710-be1e-e720da00c509.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 3a066e9c-b2d6-4710-be1e-e720da00c509.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~ In file included from 3a066e9c-b2d6-4710-be1e-e720da00c509.cpp:18: 3a066e9c-b2d6-4710-be1e-e720da00c509.cpp: In function ‘int main()’: 3a066e9c-b2d6-4710-be1e-e720da00c509.cpp:20:35: error: ‘lst’ was not declared in  this scope    20 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 3a066e9c-b2d6-4710-be1e-e720da00c509.cpp:20:43: error: ‘odd_sum’ was not declare d in this scope    20 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,9 +1,12 @@
+#include <cassert>
+#include <vector>
+
 long double_the_difference(const std::vector<int>& input) {
     long average = 0;
     for (int num : input) {
         average += num;
     }
-    return average * average;
+    return static_cast<long>(average * average);
 }
 
 int main() {
