4e8916af8ec10f79360f909c15b63fd1c28690a7
beep boop <githubbot@vadim.me>
2024-07-09 21:35:32 +0200

    Solution 32 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/oje19k94 Prompt:       |                              ^~~~~~ 90591817-2f58-4a18-82bc-eb560ae165af.cpp:21:37: error: ‘string’ was not declared  in this scope    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 90591817-2f58-4a18-82bc-eb560ae165af.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 90591817-2f58-4a18-82bc-eb560ae165af.cpp:21:45: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 90591817-2f58-4a18-82bc-eb560ae165af.cpp:20: 90591817-2f58-4a18-82bc-eb560ae165af.cpp: In function ‘int main()’: 90591817-2f58-4a18-82bc-eb560ae165af.cpp:30:20: error: ‘total_match’ was not dec lared in this scope    30 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 90591817-2f58-4a18-82bc-eb560ae165af.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,17 +1,4 @@
 ```cpp
-#include <vector>
-#include <string>
-
-bool issame(vector<string> a, vector<string> b) {
-    // your logic here to compare two vectors of strings
-    return true; 
-}
-
-int main() {
-    vector<string> result = total_match({"this"}, {});
-    return 0;
-}
-
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     int sum1 = 0;
     for (const auto& str : lst1) {
@@ -28,5 +15,18 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     else if (sum1 > sum2)
         return lst2;
 
-    return lst1;
+    bool issame(vector<string> a, vector<string> b) {
+        return (a == b);
+    }
+    
+    vector<string> result = total_match(lst1, lst2);
+    if(issame(result, lst1))
+        return lst1;
+    else
+        return lst2;
+}
+
+int main() {
+    assert(issame(total_match({"this"}, {}), {}));
+    return 0;
 }
