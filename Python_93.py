18853dc7b9a2964c7cc9f390469f8c2bde376205
beep boop <githubbot@vadim.me>
2024-07-09 07:33:05 +0200

    Solution 10 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/goeo1gcp Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/54aaddc5-fb7f-4475-86d1-5e47dd853 d0b.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/54aaddc5-fb7f-4475-86d1-5e47dd853 d0b.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,11 +1,8 @@
 def encode(message):
-    result = ""
-    for char in message:
-        if char.isalpha():
-            if char.isupper():
-                result += chr((ord(char) - 65 + 3) % 26 + 65)
-            else:
-                result += chr((ord(char) - 97 + 3) % 26 + 97).lower()
+    if char.isalpha():
+        if char.isupper():
+            result += chr((ord(char) - 65 + 3) % 26 + 65)
         else:
-            result += char
-    return result
+            result += chr((ord(char) - 97 + 3) % 26 + 97).lower()
+    else:
+        result += char
