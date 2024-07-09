938f5ac9a5f46cf35e714417582385994ee4a906
beep boop <githubbot@vadim.me>
2024-07-09 05:21:44 +0200

    Solution 25 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/7cp50heo Prompt: 9ab49f71-81e5-4c37-a5a2-d43740031d9b.cpp:47:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 9ab49f71-81e5-4c37-a5a2-d43740031d9b.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 9ab49f71-81e5-4c37-a5a2-d43740031d9b.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 9ab49f71-81e5-4c37-a5a2-d43740031d9b.cpp:47:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    47 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf 9ab49f71-81e5-4c37-a5a2-d43740031d9b.cpp:55:5: error: redefinition of ‘int main( )’    55 | int main(){       |     ^~~~ 9ab49f71-81e5-4c37-a5a2-d43740031d9b.cpp:40:5: note: ‘int main()’ previously def ined here    40 | int main() {       |     ^~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -39,5 +39,6 @@ std::vector<std::string> bf(std::string planet1, std::string planet2) {
 
 int main() {
     assert(issame(bf("Jupiter", "Makemake"), {}));
+    // Additional test cases can be added here
     return 0;
 }
