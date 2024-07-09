fe492c4c8afc00bb25fc6467121fae1c1494cdba
beep boop <githubbot@vadim.me>
2024-07-09 04:40:30 +0200

    Solution 90 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4j02uq4y Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fbd768da-f262-4c27-bf7a-0dd846fad 121.py", line 11, in <module>     check(solution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fbd768da-f262-4c27-bf7a-0dd846fad 121.py", line 6, in check     assert solution([3, 13, 2, 9]) == 3            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_121.py
+++ Python_121.py
@@ -1,6 +1,3 @@
-```Python
+```
 def solution(lst):
-    return sum([i for i in lst if i % 2 != 0])
-
-lst = [int(x) for x in input("Enter a list of numbers separated by space: ").split()]
-print(solution(lst))
+    return sum(i for i in lst if 1 <= i % 2)
