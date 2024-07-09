12e94aeb96c638002bee5d5214e435ce5b7784be
beep boop <githubbot@vadim.me>
2024-07-09 05:57:24 +0200

    Solution 14 of CPP/95, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/tg25wkpl Prompt: Complete the following code given the task description and function signature.

--- CPP_95.cpp
+++ CPP_95.cpp
@@ -1,29 +1,9 @@
-bool check_dict_case(map<string, string> dict) {
-    if (dict.empty())
-        return false;
-
-    bool allUpper = true;
-    bool allLower = true;
-
-    for (auto it = dict.begin(); it != dict.end(); it++) {
-        if (!islower(it->first[0]) && !isupper(it->first[0]))
-            return false;  // Not a letter, so not in lower or upper case
-
-        if (!allUpper)
-            continue;
-
-        allUpper &= isupper(it->first[0]);
+bool check_dict_case(map<string,string> dict){
+    if(dict.empty()) return false;
+    string firstKey = *(dict.begin())->first;
+    for(auto it=dict.begin();it!=dict.end();++it) {
+        if(tolower(firstKey) != tolower(it->first) && toupper(firstKey) != toupper(it->first))
+            return false;
     }
-
-    for (auto it = dict.begin(); it != dict.end(); it++) {
-        if (!islower(it->first[0]) && !isupper(it->first[0]))
-            return false;  // Not a letter, so not in lower or upper case
-
-        if (!allLower)
-            continue;
-
-        allLower &= islower(it->first[0]);
-    }
-
-    return allUpper || allLower;
+    return true;
 }
