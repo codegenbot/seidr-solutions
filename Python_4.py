176a559723cf12227f05277dac8c280c43c4964e
beep boop <githubbot@vadim.me>
2024-07-09 09:47:09 +0200

    Solution 48 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/qya5cw64 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/edae26f5-a5fd-4fdc-a98c-6792119fe 3d7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_4.py
+++ Python_4.py
@@ -1,9 +1,4 @@
-```
-from typing import List
-
-
-def mean_absolute_deviation(numbers: List[float]) -> float:
-    if len(numbers) == 0:
-        return None
+`def mean_absolute_deviation(numbers: List[float]) -> float:
     mean = sum(numbers) / len(numbers)
-    return sum(abs(num - mean) for num in numbers) / len(numbers)
+    total_diff = sum(abs(num - mean) for num in numbers)
+    return total_diff / len(numbers)`
