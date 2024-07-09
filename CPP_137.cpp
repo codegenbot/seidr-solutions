1c55e341498de2fb15ab46eb5700808e1dcaecf9
beep boop <githubbot@vadim.me>
2024-07-09 04:34:23 +0200

    Solution 23 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/chasion3 Prompt: c4338151-5d85-4b60-9f35-fdcce87e5b1b.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,29 +1,26 @@
+```cpp
 #include <boost/config.hpp>
-#include <boost/any.hpp>
 #include <string>
 #include <algorithm>
+#include <boost/any.hpp>
 
 using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return (int)a.cast_to<int>() > (double)b.cast_to<double>() ? b : a;
+        return (int)a.cast_to<int>() > (double)b.cast_to<double>() ? boost::any(b) : boost::any(a);
     }
     else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        string str = boost::any_cast<string>(b);
-        return (double)a.cast_to<double>() > stod(str) ? b : a;
+        return (double)a.cast_to<double>() > stod(boost::any_cast<string>(b)) ? boost::any(b) : boost::any(a);
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = boost::any_cast<string>(a);
-        string str2 = boost::any_cast<string>(b);
-        return str1 > str2 ? b : a;
+        return boost::any_cast<string>(a) > boost::any_cast<string>(b) ? boost::any(b) : boost::any(a);
     }
     else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        string str = boost::any_cast<string>(b);
-        return (int)a.cast_to<int>() > stod(str) ? b : a;
+        return (int)a.cast_to<int>() > stod(boost::any_cast<string>(b)) ? boost::any(b) : boost::any(a);
     }
     else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return (double)a.cast_to<double>() > (int)b.cast_to<int>() ? b : a;
+        return (double)a.cast_to<double>() > (int)b.cast_to<int>() ? boost::any(b) : boost::any(a);
     }
     else {
         return boost::any("None");
