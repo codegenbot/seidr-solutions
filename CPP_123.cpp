9d675d094644562b2f2a913623241dd9bebbe73a
beep boop <githubbot@vadim.me>
2024-07-09 11:05:42 +0200

    Solution 16 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/dv7sdu05 Prompt:       |                    ^~~ 19def2f8-c2da-432c-b793-19f0b859f2c9.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 19def2f8-c2da-432c-b793-19f0b859f2c9.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 19def2f8-c2da-432c-b793-19f0b859f2c9.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 19def2f8-c2da-432c-b793-19f0b859f2c9.cpp:13: 19def2f8-c2da-432c-b793-19f0b859f2c9.cpp: In function ‘int main()’: 19def2f8-c2da-432c-b793-19f0b859f2c9.cpp:23:20: error: ‘get_odd_collatz’ was not  declared in this scope    23 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 19def2f8-c2da-432c-b793-19f0b859f2c9.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,17 +1,6 @@
 #include <vector>
-#include <algorithm>
-#include <cassert>
-
-using namespace std;
-
-bool issame(vector<int> a, vector<int> b) {
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    return a == b;
-}
-
-vector<int> get_odd_collatz(int n) {
-    vector<int> result;
+std::vector<int> solve(int n) {
+    std::vector<int> result;
     while (n != 1) {
         if (n % 2 != 0) {
             result.push_back(n);
@@ -19,12 +8,6 @@ vector<int> get_odd_collatz(int n) {
         n = (n % 2 == 0) ? n / 2 : 3 * n + 1;
     }
     result.push_back(1);
-    sort(result.begin(), result.end());
+    std::sort(result.begin(), result.end());
     return result;
-}
-
-int main() {
-    assert(issame(get_odd_collatz(12), {1, 3, 5, 9}));
-    assert(issame(get_odd_collatz(19), {1, 3, 5, 9, 17}));
-    return 0;
 }
