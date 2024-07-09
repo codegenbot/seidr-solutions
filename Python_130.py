b5ec520377684758b175dee0d8d3e26b20bb97dd
beep boop <githubbot@vadim.me>
2024-07-09 09:05:26 +0200

    Solution 4 of Python/130, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/fa7ouuiu Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa3d0c3b-b2dc-4f9f-8548-47dc715b7 5bb.py", line 20, in <module>     check(tri)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa3d0c3b-b2dc-4f9f-8548-47dc715b7 5bb.py", line 17, in check     assert tri(1) == [1, 3]            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_130.py
+++ Python_130.py
@@ -2,10 +2,10 @@ def tri(n):
     if n == 0:
         return [0]
     elif n == 1:
-        return [1]
+        return [1, 3]
     else:
         result = [1, 3]
-        for i in range(2, n + 1):
+        for i in range(2, n):
             if i % 2 == 0:
                 result.append(1 + i // 2)
             else:
