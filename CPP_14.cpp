63f4402b6f9ae8ebb124421286c42698bf1daaae
beep boop <githubbot@vadim.me>
2024-07-09 07:16:11 +0200

    Solution 52 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/g3dq5xin Prompt: 3dcf8ceb-c793-416f-adaa-7e12b582a2ea.cpp:26:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    26 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 3dcf8ceb-c793-416f-adaa-7e12b582a2ea.cpp:16:6: note: ‘bool issame(std::vector<st d::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> > )’ previously defined here    16 | bool issame(vector<string> a, vector<string> b){       |      ^~~~~~ 3dcf8ceb-c793-416f-adaa-7e12b582a2ea.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 3dcf8ceb-c793-416f-adaa-7e12b582a2ea.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -7,13 +7,17 @@ using namespace std;
 
 vector<string> all_prefixes(string str){
     vector<string> prefixes;
-    for (int i = 1; i <= str.size(); ++i) {
+    for (unsigned int i = 1; i <= str.size(); ++i) {
         prefixes.push_back(str.substr(0, i));
     }
     return prefixes;
 }
 
+bool areEqual(vector<string> a, vector<string> b){
+    return a == b;
+}
+
 int main() {
-    assert(all_prefixes("WWW") == vector<string>{"W", "WW", "WWW"});
+    assert(areEqual(all_prefixes("WWW"), {"W", "WW", "WWW"}));
     return 0;
 }
