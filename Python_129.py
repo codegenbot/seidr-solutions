5862ad1d72959fe3d459504654666cd46c0efac0
beep boop <githubbot@vadim.me>
2024-07-09 03:03:56 +0200

    Solution 13 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/za2a3ye3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/899b4dc9-79bd-41bc-a06d-ef4d2cc9a 591.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -1,23 +1,22 @@
-```
 def minPath(grid, k):
     N = len(grid)
-    visited = [[False]*N for _ in range(N)]
+    visited = [[False] * N for _ in range(N)]
     queue = [(0, 0, [grid[0][0]])]
     res = []
-    
+
     while queue:
         x, y, path = queue.pop(0)
-        
+
         if len(path) == k + 1:
-            if not res or tuple(sorted(set(path))) < tuple(sorted(set(res))):
-                res = list(set(path[:-1]))
+            if not res or tuple(sorted(path)) < tuple(sorted(res)):
+                res = list(sorted(path[:-1]))
             continue
-        
+
         for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             nx, ny = x + dx, y + dy
-            
+
             if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
                 visited[nx][ny] = True
                 queue.append((nx, ny, path + [grid[nx][ny]]))
-    
+
     return res
