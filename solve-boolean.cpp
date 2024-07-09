a8ad1fcfe2a7b276737a6d659d7ee5c580c4dad7
beep boop <githubbot@vadim.me>
2024-07-09 09:05:34 +0200

    Solution 29 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/y9p8yv78 Prompt: e77eb014-458e-4e70-9eed-aed59480fadb.cpp:1:19: error: ‘string’ was not declared in this scope     1 | bool solveBoolean(string s) {       |                   ^~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -5,30 +5,37 @@ bool solveBoolean(string s) {
     for (char c : s) {
         if (c == 'F') {
             res = false;
-            break;
         }
         else if (c == '&') {
-            while (s.length() > 1 && s[0] == '&') {
-                s = s.substr(1);
-            }
-            if (s.empty()) {
-                return true;
-            } 
-            if (s[0] == 'F') {
-                res = false;
-                break;
+            while(res && s.find('&') != string::npos) {
+                size_t pos = s.find('&');
+                int left = 1, right = 0;
+                for(int i = 0; i < pos; i++) {
+                    if(s[i] == '(')left++;
+                    else if(s[i] == ')')right++;
+                }
+                for(int i = pos + 1; ; i++) {
+                    if(s[i] == '(')left--;
+                    else if(s[i] == ')')right--;
+                    if(left == 0 && right == 0)break;
+                }
+                s.erase(0, i);
             }
         }
         else if (c == '|') {
-            while (s.length() > 1 && s[0] == '|') {
-                s = s.substr(1);
-            }
-            if (s.empty()) {
-                return true;
-            } 
-            if (s[0] == 'F') {
-                res = false;
-                break;
+            while(res || s.find('|') != string::npos) {
+                size_t pos = s.find('|');
+                int left = 1, right = 0;
+                for(int i = 0; i < pos; i++) {
+                    if(s[i] == '(')left++;
+                    else if(s[i] == ')')right++;
+                }
+                for(int i = pos + 1; ; i++) {
+                    if(s[i] == '(')left--;
+                    else if(s[i] == ')')right--;
+                    if(left == 0 && right == 0)break;
+                }
+                s.erase(0, i);
             }
         }
     }
