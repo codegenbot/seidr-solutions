168e4b2f84b15cb7ebb56becc04c668a03a2e7ec
beep boop <githubbot@vadim.me>
2024-07-09 16:15:22 +0200

    Solution 1 of CPP/159, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/y73e28lo Prompt: Complete the following code given the task description and function signature.

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -10,12 +10,3 @@ vector<int> eat(int number, int need, int remaining) {
         return {total, remaining - need};
     }
 }
-
-int main() {
-    cout << "{11, 4}" << endl; // eat(5, 6, 10)
-    cout << "{12, 1}" << endl; // eat(4, 8, 9)
-    cout << "{11, 0}" << endl; // eat(1, 10, 10)
-    cout << "{7, 0}" << endl; // eat(2, 11, 5)
-
-    return 0;
-}
