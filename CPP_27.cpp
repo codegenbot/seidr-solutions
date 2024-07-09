1581e86ddf43bf6291c0540c2b6d5354e137ad42
beep boop <githubbot@vadim.me>
2024-07-09 07:38:01 +0200

    Solution 51 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uw1asumq Prompt: aa97e400-7275-4c08-b475-147ce115e12f.cpp:3:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     3 | string flip_case(string str){       | ^~~~~~       | stdin aa97e400-7275-4c08-b475-147ce115e12f.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ aa97e400-7275-4c08-b475-147ce115e12f.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main(){       |     ^~~~ In file included from aa97e400-7275-4c08-b475-147ce115e12f.cpp:18: aa97e400-7275-4c08-b475-147ce115e12f.cpp: In function ‘int main()’: aa97e400-7275-4c08-b475-147ce115e12f.cpp:20:13: error: ‘filp_case’ was not decla red in this scope    20 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -2,7 +2,7 @@
 #include <cctype>
 #include <cassert>
 
-std::string flip_case(std::string str) {
+std::string flip_case(std::string str){
     for(char &c : str){
         if(islower(c)){
             c = toupper(c);
