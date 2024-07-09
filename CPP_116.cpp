d8e21334e142ae424990d8033c443e8b474e59b2
beep boop <githubbot@vadim.me>
2024-07-09 04:15:45 +0200

    Solution 28 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/hvdy68h7 Prompt:       |                    ^~~ f17fb0a9-9382-4825-b967-f15a0dc446d5.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f17fb0a9-9382-4825-b967-f15a0dc446d5.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f17fb0a9-9382-4825-b967-f15a0dc446d5.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f17fb0a9-9382-4825-b967-f15a0dc446d5.cpp:13: f17fb0a9-9382-4825-b967-f15a0dc446d5.cpp: In function ‘int main()’: f17fb0a9-9382-4825-b967-f15a0dc446d5.cpp:23:20: error: ‘sort_array’ was not decl ared in this scope    23 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ f17fb0a9-9382-4825-b967-f15a0dc446d5.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,27 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-bool issame(vector<int> a,vector<int>b){
-    return a == b;
-}
-
-vector<int> sort_vector(vector<int> arr) {
+vector<int> sort_array(vector<int> arr) {
     vector<int> result = arr;
     sort(result.begin(), result.end(),
-        [](int a, int b) {
-            int ones_a = __builtin_popcount(a);
-            int ones_b = __builtin_popcount(b);
-            if (ones_a == ones_b) return a < b;
-            return ones_a < ones_b;
-        }
-    );
+         [](int a, int b) {
+             return bitset<32>(a).count() < bitset<32>(b).count()
+                 || (bitset<32>(a).count() == bitset<32>(b).count() && a < b);
+         });
     return result;
-}
-
-int main() {
-    assert(issame(vector<int>(sort_vector({2,4,8,16,32})).begin(), vector<int>(sort_vector({2,4,8,16,32}))).end());
-    return 0;
 }
