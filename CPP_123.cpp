608a6850ba70932aa9fa945e2bb3bec2abebed42
beep boop <githubbot@vadim.me>
2024-07-09 04:15:13 +0200

    Solution 28 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jyco84rc Prompt:       |                    ^~~ e0177df9-60e8-44b2-9d09-a3f0623bd6b8.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e0177df9-60e8-44b2-9d09-a3f0623bd6b8.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e0177df9-60e8-44b2-9d09-a3f0623bd6b8.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e0177df9-60e8-44b2-9d09-a3f0623bd6b8.cpp:17: e0177df9-60e8-44b2-9d09-a3f0623bd6b8.cpp: In function ‘int main()’: e0177df9-60e8-44b2-9d09-a3f0623bd6b8.cpp:27:20: error: ‘get_odd_collatz’ was not  declared in this scope    27 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ e0177df9-60e8-44b2-9d09-a3f0623bd6b8.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -4,14 +4,14 @@
 std::vector<int> get_odd_collatz(int n) {
     std::vector<int> result;
     while (n != 1) {
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else {
+        if (n % 2 == 0)
+            n /= 2;
+        else
             n = 3 * n + 1;
-        }
-        if (n % 2 != 0) {
-            result.push_back(n);
-        }
+    }
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 != 0)
+            result.push_back(i);
     }
     sort(result.begin(), result.end());
     return result;
