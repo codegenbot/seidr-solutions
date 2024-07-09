de2480bf3c5c1d8610cd4ad855ae76d545c2d5b4
beep boop <githubbot@vadim.me>
2024-07-09 02:20:16 +0200

    Solution 13 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0xw9ottc Prompt: In file included from /usr/include/c++/11/vector:67,                  from 9575fe88-4d14-4edf-a6ff-2365f94b90f2.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 9575fe88-4d14-4edf-a6ff-2365f94b90f2.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9575fe88-4d14-4edf-a6ff-2365f94b90f2.cpp:4:43: error: expected primary-expressio n before ‘int’     4 | int do_algebra(vector<string> ops, vector<int> nums) {       |                                           ^~~ 9575fe88-4d14-4edf-a6ff-2365f94b90f2.cpp:4:52: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | int do_algebra(vector<string> ops, vector<int> nums) {       |                                                    ^ In file included from 9575fe88-4d14-4edf-a6ff-2365f94b90f2.cpp:25: 9575fe88-4d14-4edf-a6ff-2365f94b90f2.cpp: In function ‘int main()’: 9575fe88-4d14-4edf-a6ff-2365f94b90f2.cpp:27:23: error: ‘do_algebra’ cannot be us ed as a function    27 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,3 +1,22 @@
 using namespace std;
 
 int do_algebra(vector<string> ops, vector<int> nums) {
+    int result = 0;
+    int temp = 1;
+
+    for (int i = 0; i < ops.size(); i++) {
+        if (ops[i] == "+") {
+            result += temp * nums[i];
+        } else if (ops[i] == "-") {
+            result -= temp * nums[i];
+        } else if (ops[i] == "*") {
+            temp *= nums[i];
+        } else if (ops[i] == "//") {
+            temp = temp / nums[i];
+        } else if (ops[i] == "**") {
+            temp = pow(temp, nums[i]);
+        }
+    }
+
+    return result;
+}
