176c072d77a3b897df54373abbf90c2ac88ecac8
beep boop <githubbot@vadim.me>
2024-07-09 04:03:10 +0200

    Solution 24 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zvs0j795 Prompt:       |                              ^~~~~~ db3457e8-3fc8-4343-96b8-f35c469f422b.cpp:45:37: error: ‘string’ was not declared  in this scope    45 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ db3457e8-3fc8-4343-96b8-f35c469f422b.cpp:45:44: error: ‘b’ was not declared in t his scope    45 | bool issame(vector<string> a,vector<string>b){       |                                            ^ db3457e8-3fc8-4343-96b8-f35c469f422b.cpp:45:45: error: expression list treated a s compound expression in initializer [-fpermissive]    45 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from db3457e8-3fc8-4343-96b8-f35c469f422b.cpp:44: db3457e8-3fc8-4343-96b8-f35c469f422b.cpp: In function ‘int main()’: db3457e8-3fc8-4343-96b8-f35c469f422b.cpp:54:20: error: ‘by_length’ was not decla red in this scope    54 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ db3457e8-3fc8-4343-96b8-f35c469f422b.cpp:54:19: error: ‘issame’ cannot be used a s a function    54 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,87 +1,50 @@
 ```cpp
 #include <vector>
-#include <algorithm>
+#include <string>
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) return false;
+    if(a.size() != b.size()) {
+        return false;
+    }
+    for(int i=0; i<a.size(); i++) {
+        if(a[i] != b[i]) {
+            return false;
+        }
     }
     return true;
 }
 
 vector<string> by_length(vector<int> arr) {
-    vector<int> sortedArr;
-    for (int num : arr) {
-        switch (num % 10) {
-            case 1:
-                sortedArr.push_back(1);
-                break;
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-                sortedArr.push_back(2);
-                break;
-            case 6:
-            case 7:
-            case 8:
-                sortedArr.push_back(3);
-                break;
-            case 9:
-                sortedArr.push_back(1);
-                break;
-        }
-    }
-    reverse(sortedArr.begin(), sortedArr.end());
     vector<string> result;
-    for (int num : sortedArr) {
+    for (int num : arr) {
         switch (num) {
             case 1:
                 result.push_back("One");
                 break;
             case 2:
-                switch (sortedArr.back()) % 10 {
-                    case 0: result.push_back("Zero"); 
-                    case 1: result.push_back("Ten"); 
-                    case 2: result.push_back("Eleven"); 
-                    case 3: result.push_back("Twelve");
-                    case 4: result.push_back("Thirteen"); 
-                    case 5: result.push_back("Fourteen");
-                    case 6: result.push_back("Fifteen");
-                    case 7: result.push_back("Sixteen");
-                    case 8: result.push_back("Seventeen");
-                    case 9: result.push_back("Eighteen");
-                }
-                sortedArr.pop_back();
+                result.push_back("Two");
                 break;
             case 3:
-                switch (sortedArr.back()) % 10 {
-                    case 0: result.push_back("Zero"); 
-                    case 1: result.push_back("Ten"); 
-                    case 2: result.push_back("Twelve");
-                    case 3: result.push_back("Thirteen"); 
-                    case 4: result.push_back("Fourteen");
-                    case 5: result.push_back("Fifteen");
-                    case 6: result.push_back("Sixteen");
-                    case 7: result.push_back("Seventeen");
-                    case 8: result.push_back("Eighteen");
-                    case 9: result.push_back("Nineteen");
-                }
-                sortedArr.pop_back();
+                result.push_back("Three");
                 break;
-            default:
-                switch (num) {
-                    case 1:
-                        for (int i = 0; i < 10; ++i) {
-                            result.push_back(to_string(i + 1));
-                        }
-                    case 2: 
-                    case 3:
-                        for (int i = 10; i <= 18; ++i) {
-                            result.push_back(to_string(i));
-                        }
-                }
+            case 4:
+                result.push_back("Four");
+                break;
+            case 5:
+                result.push_back("Five");
+                break;
+            case 6:
+                result.push_back("Six");
+                break;
+            case 7:
+                result.push_back("Seven");
+                break;
+            case 8:
+                result.push_back("Eight");
+                break;
+            case 9:
+                result.push_back("Nine");
+                break;
         }
     }
     return result;
