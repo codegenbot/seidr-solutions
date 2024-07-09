f6ba37b03a3d93b5ed7c614ea26fdf358927cac0
beep boop <githubbot@vadim.me>
2024-07-09 02:51:55 +0200

    Solution 15 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/633ghytp Prompt:       |             ^~~~~~ 1523c173-6931-4bd5-9848-c899d1958e3b.cpp:38:34: error: expected primary-expressi on before ‘int’    38 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1523c173-6931-4bd5-9848-c899d1958e3b.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ 1523c173-6931-4bd5-9848-c899d1958e3b.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from 1523c173-6931-4bd5-9848-c899d1958e3b.cpp:37: 1523c173-6931-4bd5-9848-c899d1958e3b.cpp: In function ‘int main()’: 1523c173-6931-4bd5-9848-c899d1958e3b.cpp:47:20: error: ‘make_a_pile’ was not dec lared in this scope    47 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 1523c173-6931-4bd5-9848-c899d1958e3b.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,24 +1,13 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <cassert>
 
 bool issame = [](vector<int> a, vector<int> b){
     return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
 };
 
-vector<int> make_a_pile(int n) {
-    vector<int> stones;
-    stones.push_back(n);
-    for (int i = 1; i < n; ++i) {
-        if (n % 2 == 0) {
-            n += 2;
-        } else {
-            n += 1;
-        }
-        stones.push_back(n);
-    }
-    return stones;
-}
+vector<int> make_a_pile(int n);
 
 int main() {
     int n;
