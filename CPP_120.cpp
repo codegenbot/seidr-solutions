7ed79d350a739a2fea0a1432e75171078024f317
beep boop <githubbot@vadim.me>
2024-07-09 05:26:43 +0200

    Solution 91 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/usez16r3 Prompt:       |                                  ^~~ ee13d125-e5dd-4cae-9f2a-4c75bc6f2859.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ ee13d125-e5dd-4cae-9f2a-4c75bc6f2859.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ ee13d125-e5dd-4cae-9f2a-4c75bc6f2859.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from ee13d125-e5dd-4cae-9f2a-4c75bc6f2859.cpp:20: ee13d125-e5dd-4cae-9f2a-4c75bc6f2859.cpp: In function ‘int main()’: ee13d125-e5dd-4cae-9f2a-4c75bc6f2859.cpp:30:20: error: ‘maximum’ was not declare d in this scope    30 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ ee13d125-e5dd-4cae-9f2a-4c75bc6f2859.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <algorithm>
 
@@ -17,18 +16,6 @@ vector<int> maximum(vector<int> arr, int k) {
     return result;
 }
 
-vector<int> maximum(vector<int> arr, int k) {
-    if(k > arr.size()) {
-        cout << "Invalid value of k. It must be less than or equal to the size of array." << endl;
-        return {};
-    }
-    
-    vector<int> result(arr.begin(), arr.begin() + k);
-    sort(result.begin(), result.end());
-    return result;
-}
-
 int main() {
     assert(maximum({1, 2, 3, -23, 243, -400, 0}, 7) == vector<int>({0, 2, 3}));
     return 0;
-}
