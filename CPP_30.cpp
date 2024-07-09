6ae9fe8268ed1b91d5254779dfea24435c0b9369
beep boop <githubbot@vadim.me>
2024-07-09 08:11:57 +0200

    Solution 58 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/v1on6s7h Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const float&}; _Tp = float; _Alloc = std::allocator<float>; std::v ector<_Tp, _Alloc>::iterator = std::vector<float>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = float; _Alloc = std::all ocator<float>; std::vector<_Tp, _Alloc>::value_type = float]’ b5bc90d8-8311-43fb-b7aa-2190d2ce693b.cpp:21:38:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -7,7 +7,7 @@ bool same(const std::vector<float>& v1, const std::vector<float>& v2) {
         return false;
     }
     for (int i = 0; i < v1.size(); i++) {
-        if (std::abs(v1[i] - v2[i]) > 0.00001f) {
+        if (std::abs(v1[i] - v2[i]) > 0.000001f) {
             return false;
         }
     }
@@ -26,6 +26,7 @@ std::vector<float> getPositive(const std::vector<float>& numbers) {
 
 int main() {
     int n;
+    float num = 0;
     while (!(std::cin >> n) || n < 1)
 {
     std::cout << "Enter element number: ";
@@ -33,7 +34,6 @@ int main() {
 for(int i=0; i<n; i++)
 {
     std::cout << "Enter element " << i + 1 << ": ";
-    float num;
     std::cin >> num;
     std::vector<float> numbers;
     numbers.push_back(num);
