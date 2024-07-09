33e61857ec32a857a31a9073595b2cad962bc47e
beep boop <githubbot@vadim.me>
2024-07-10 01:34:10 +0200

    Solution 11 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uzrnpb22 Prompt: In file included from 8e3400db-ae0d-446c-b9da-ca52e9879ce7.cpp:19: 8e3400db-ae0d-446c-b9da-ca52e9879ce7.cpp:29:26: note:   candidate expects 4 argu ments, 1 provided    29 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 8e3400db-ae0d-446c-b9da-ca52e9879ce7.cpp:3: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: candidate: ‘template <class _ExecutionPolicy, class _ForwardIterator> __pstl::__internal::__enable_if _execution_policy<_ExecutionPolicy, _ForwardIterator> std::unique(_ExecutionPoli cy&&, _ForwardIterator, _ForwardIterator)’   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note:   template argument deduction/substitution failed: In file included from 8e3400db-ae0d-446c-b9da-ca52e9879ce7.cpp:19: 8e3400db-ae0d-446c-b9da-ca52e9879ce7.cpp:29:26: note:   candidate expects 3 argu ments, 1 provided    29 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,17 +1,6 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-int main() {
-    vector<int> input = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    vector<int> output;
-    copy(unique(input.begin(), input.end()), input.end(), back_inserter(output));
-    sort(output.begin(), output.end());
-    assert(std::equal(std::unique(input.begin(), input.end()), input.end(), output.begin()));
-    for (int i : output) {
-        cout << i << " ";
-    }
-    return 0;
+auto unique_end = unique(input.begin(), input.end());
+vector<int> output(input.begin(), unique_end);
+sort(output.begin(), output.end());
+for (int i : output) {
+    cout << i << " ";
 }
