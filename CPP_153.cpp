dbc792352565c8e07e477d40eb9f8eb8dfd7f88a
beep boop <githubbot@vadim.me>
2024-07-09 12:59:25 +0200

    Solution 91 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/wdo1tu41 Prompt:    14 |  std::cout << Strongest_Extension("Sp", {"671235", "Bb"}) << std::endl;       |                                                                   ^~~~ a3bcc9c9-9a9f-47fa-87d8-8cc8de574e2b.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <initializer_list>   +++ |+#include <ostream>     2 | a3bcc9c9-9a9f-47fa-87d8-8cc8de574e2b.cpp: At global scope: a3bcc9c9-9a9f-47fa-87d8-8cc8de574e2b.cpp:18:5: error: redefinition of ‘int main( )’    18 | int main(){       |     ^~~~ a3bcc9c9-9a9f-47fa-87d8-8cc8de574e2b.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from a3bcc9c9-9a9f-47fa-87d8-8cc8de574e2b.cpp:17: a3bcc9c9-9a9f-47fa-87d8-8cc8de574e2b.cpp: In function ‘int main()’: a3bcc9c9-9a9f-47fa-87d8-8cc8de574e2b.cpp:19:32: error: ‘Strongest_Extension’ can not be used as a function    19 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,5 +1,10 @@
-#include <vector>
-#include <string>
+```cpp
+#include <initializer_list>
+#include <iostream>
+
+int main() {
+    std::cout << Strongest_Extension("Sp", {"671235", "Bb"}) << std::endl;
+}
 
 int Strongest_Extension(std::string className, const std::vector<std::string>& extensions) {
     int strongest = -1;
@@ -9,8 +14,4 @@ int Strongest_Extension(std::string className, const std::vector<std::string>& e
         }
     }
     return strongest == -1 ? -1 : className + "." + extensions[0];
-}
-
-int main() {
-    std::cout << Strongest_Extension("Sp", {"671235.Sp", "Bb.Sp"}) << std::endl;
 }
