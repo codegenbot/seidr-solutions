b5937e1309ffaa80e5c790bad6cb14342f43c0c3
beep boop <githubbot@vadim.me>
2024-07-09 06:39:54 +0200

    Solution 91 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/5yvq0zcc Prompt: f18bb42f-b3c2-4e57-82a9-3fd6abe34e2a.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ f18bb42f-b3c2-4e57-82a9-3fd6abe34e2a.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ f18bb42f-b3c2-4e57-82a9-3fd6abe34e2a.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ f18bb42f-b3c2-4e57-82a9-3fd6abe34e2a.cpp:10:23: error: ‘string’ is not a member of ‘std’    10 | int bowlingScore(std::string s) {       |                       ^~~~~~ f18bb42f-b3c2-4e57-82a9-3fd6abe34e2a.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | ```

--- bowling.cpp
+++ bowling.cpp
@@ -15,25 +15,31 @@ int bowlingScore(std::string s) {
     for(int i = 0; i < s.length(); i++) {
         if(s[i] == 'X') {
             score += 10;
-            if(i+1 < s.length() && (s[i+1] == '/' || s[i+1] == 'X')) {
+            currentFrame++;
+        } else if(s[i] == '/') {
+            if(s[i-1] == 'X') {
+                score += 10;
+                score += 10;
+                currentFrame++;
+            } else {
+                int firstRoll = (s[i-1] - '0');
+                int secondRoll = (s[i+1] - '0');
+                score += getBonus(firstRoll, 10);
                 currentFrame++;
             }
-        } else if(s[i] == '/') {
-            int firstRoll = (s[i-1] - '0');
-            score += getBonus(firstRoll, 10);
-            currentFrame++;
         } else {
             int roll = s[i] - '0';
             score += roll;
             if(i+1 < s.length() && s[i+1] != '/') {
-                if(currentFrame == 9) {
+                if(currentFrame < 9) {
                     score += getBonus(roll, (s[i+1] - '0'));
+                    currentFrame++;
                 } else {
-                    score += getBonus(roll, (s[i+1] - '0'));
+                    score += roll + 10;
                     currentFrame++;
                 }
             } else {
-                break;
+                currentFrame++;
             }
         }
     }
