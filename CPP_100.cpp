f6121bd35a2fd9cc495f82b48115714303870f2f
beep boop <githubbot@vadim.me>
2024-07-09 19:37:06 +0200

    Solution 86 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/vm0h1jt4 Prompt:       |                    ^~~ 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp:9: 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp: In function ‘int main()’: 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp:19:20: error: ‘make_a_pile’ was not dec lared in this scope    19 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 01a2354a-2666-4187-9d1d-e4c1a9f6fa67.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,17 +1,6 @@
 #include <vector>
 
 vector<int> make_a_pile(int n);
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
 
 vector<int> make_a_pile(int n){
     vector<int> result;
@@ -19,4 +8,8 @@ vector<int> make_a_pile(int n){
         result.push_back(n + 2*i);
     }
     return result;
+}
+
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
 }
