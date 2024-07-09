e29fa3e7205172beb288cbf33edca9b1a9a01a69
beep boop <githubbot@vadim.me>
2024-07-09 05:43:59 +0200

    Solution 3 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/lqdbl84j Prompt: Complete the following code given the task description and function signature.

--- Python_37.py
+++ Python_37.py
@@ -1,4 +1,3 @@
 def sort_even(l: list):
-    return [
-        i if i % 2 != 0 else j for j in sorted([x for x in l if x % 2 == 0]) for i in l
-    ]
+    even = sorted([x for x in l[::2]])
+    return [x if i % 2 != 0 else y for i, (x, y) in enumerate(zip(l, even))]
