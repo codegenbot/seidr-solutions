c18e36cb79d9324b57fec77a7b29aee078f112b7
beep boop <githubbot@vadim.me>
2024-07-09 03:58:30 +0200

    Solution 94 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/o5dcqdjt Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a1850264-2240-4e20-9ca0-1dcbfd73ec32.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ a1850264-2240-4e20-9ca0-1dcbfd73ec32.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from a1850264-2240-4e20-9ca0-1dcbfd73ec32.cpp:26: a1850264-2240-4e20-9ca0-1dcbfd73ec32.cpp: In function ‘int main()’: a1850264-2240-4e20-9ca0-1dcbfd73ec32.cpp:36:20: error: ‘filter_by_prefix’ was no t declared in this scope    36 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ a1850264-2240-4e20-9ca0-1dcbfd73ec32.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,3 +1,4 @@
+```cpp
 bool issame(vector<string> a, vector<string> b) {
     if(a.size() != b.size())
         return false;
@@ -16,27 +17,16 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
     }
     return result;
 }
-
 int main() {
-    vector<string> strings;
-    string prefix;
-
-    cin >> prefix;
-    int n;
-    cin >> n;
-    
-    for(int i = 0; i < n; i++) {
-        string str;
-        cin >> str;
-        strings.push_back(str);
-    }
-
+    vector<string> strings = {"xxx", "asd", "xxy", "john doe", "xxxAAC", "xxx"};
+    string prefix = "xxx";
     vector<string> result = filter_by_prefix(strings, prefix);
-
-    if(issame(result , {"xxx", "xxxAAA", "xxx"})) 
-        cout << "The output is correct" << endl;
+    cout << "Filtered strings: ";
+    for(auto s : result)
+        cout << s << " ";
+    cout << endl;
+    if (issame(result, {"xxx", "xxxAAA", "xxx"}))
+        cout << "Strings are the same" << endl;
     else
-        cout << "The output is incorrect" << endl;
-
-    return 0;
+        cout << "Strings are not the same" << endl;
 }
