f4befc77b6ddce99542f9e53fd9c393a5e243e13
beep boop <githubbot@vadim.me>
2024-07-09 03:51:27 +0200

    Solution 46 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/anbc1vtt Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/46c7ec46-37e6-4c1f-ba62-fd6b6ece2 83d.py", line 1, in <module>     def max_fill(grid: List[List[int]], capacity: int) -> int:                        ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_115.py
+++ Python_115.py
@@ -1,8 +1,7 @@
-```
-def max_fill(grid: List[List[int]], capacity: int) -> int:
+def max_fill(grid: list[list[int]], capacity: int) -> int:
     total_water = 0
     for row in grid:
         water_per_row = min(capacity, sum(row))
         total_water += water_per_row
         capacity -= water_per_row
-    return math.ceil(total_water / (capacity + 1))
+    return total_water // (capacity + 1)
