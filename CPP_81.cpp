76ea368be533f6df251e82ad60189c4d0ba93a54
beep boop <githubbot@vadim.me>
2024-07-09 19:27:22 +0200

    Solution 73 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d9zek7iz Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 44d875f8-634e-4efd-a79e-00e28f2ce80c.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <vector>
 #include <string>
 
@@ -12,28 +13,28 @@ bool checkIfSame(std::vector<std::string> a, std::vector<std::string> b) {
 
 std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
     std::vector<std::string> letter_grades;
-    for (int i = 0; i < grades.size(); i++) {
-        if (grades[i] >= 4.0)
+    for (float grade : grades) {
+        if (grade >= 4.0)
             letter_grades.push_back("A+");
-        else if (grades[i] > 3.7)
+        else if (grade > 3.7)
             letter_grades.push_back("A");
-        else if (grades[i] > 3.3)
+        else if (grade > 3.3)
             letter_grades.push_back("A-");
-        else if (grades[i] > 3.0)
+        else if (grade > 3.0)
             letter_grades.push_back("B+");
-        else if (grades[i] > 2.7)
+        else if (grade > 2.7)
             letter_grades.push_back("B");
-        else if (grades[i] > 2.3)
+        else if (grade > 2.3)
             letter_grades.push_back("B-");
-        else if (grades[i] > 2.0)
+        else if (grade > 2.0)
             letter_grades.push_back("C+");
-        else if (grades[i] > 1.7)
+        else if (grade > 1.7)
             letter_grades.push_back("C");
-        else if (grades[i] > 1.3)
+        else if (grade > 1.3)
             letter_grades.push_back("C-");
-        else if (grades[i] > 1.0)
+        else if (grade > 1.0)
             letter_grades.push_back("D+");
-        else if (grades[i] > 0.7)
+        else if (grade > 0.7)
             letter_grades.push_back("D");
         else
             letter_grades.push_back("F");
@@ -42,6 +43,8 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
 }
 
 int main() {
-    assert(checkIfSame(numerical_letter_grade(std::vector<float>({0, 0.7})), {"E", "D-"}));
+    std::vector<float> grades = {0, 0.7};
+    std::vector<std::string> expected_result = {"E", "D-"};
+    assert(checkIfSame(numerical_letter_grade(grades), expected_result));
     return 0;
 }
