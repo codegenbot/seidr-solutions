e9e7c639a55326bed03c5c86b83dabf2b90861b7
beep boop <githubbot@vadim.me>
2024-07-09 18:11:32 +0200

    Solution 10 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/xa85h2bh Prompt:       |     ^~~~~~ 0203195d-4f8f-4c85-bc07-f799761079f6.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | using namespace std; 0203195d-4f8f-4c85-bc07-f799761079f6.cpp: At global scope: 0203195d-4f8f-4c85-bc07-f799761079f6.cpp:26:6: error: redefinition of ‘bool issa me(std::vector<float>, std::vector<float>)’    26 | bool issame(vector<float> a,vector<float>b){       |      ^~~~~~ 0203195d-4f8f-4c85-bc07-f799761079f6.cpp:5:6: note: ‘bool issame(std::vector<flo at>, std::vector<float>)’ previously defined here     5 | bool issame(vector<float> a, vector<float> b) {       |      ^~~~~~ 0203195d-4f8f-4c85-bc07-f799761079f6.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 0203195d-4f8f-4c85-bc07-f799761079f6.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,20 +1,25 @@
+#include <iostream>
 #include <vector>
+#include <cassert>
 
-std::vector<float> get_positive(std::vector<float> l) {
-    std::vector<float> result;
-    for(float i : l) {
-        if(i > 0) {
-            result.push_back(i);
+using namespace std;
+
+bool issame(vector<float> a, vector<float> b) {
+    return a == b;
+}
+
+vector<float> get_positive(vector<float> l){
+    vector<float> result;
+    for (float x : l) {
+        if(x > 0) {
+            result.push_back(x);
         }
     }
     return result;
 }
 
-bool issame(std::vector<float> a, std::vector<float> b) {
-    return a == b;
-}
-
 int main() {
-    assert(get_positive({}) == {});
+    assert(issame(get_positive({}), {}));
+    cout << "Fixed!" << endl;
     return 0;
 }
