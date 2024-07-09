9cdc8ee729aaef5057803e31fd13328aeb6791a9
beep boop <githubbot@vadim.me>
2024-07-09 10:43:55 +0200

    Solution 7 of CPP/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/use91aft Prompt: ore ‘for’     2 |         for (int j = 0; j < grid[0].size(); j++) {       |         ^~~ c65a6752-e582-4cda-bcfe-3b47368680ae.cpp:2:25: error: ‘j’ does not name a type     2 |         for (int j = 0; j < grid[0].size(); j++) {       |                         ^ c65a6752-e582-4cda-bcfe-3b47368680ae.cpp:2:45: error: ‘j’ does not name a type     2 |         for (int j = 0; j < grid[0].size(); j++) {       |                                             ^ c65a6752-e582-4cda-bcfe-3b47368680ae.cpp:9:9: error: expected unqualified-id bef ore ‘return’     9 |         return (total_fill + capacity - 1) / capacity;       |         ^~~~~~ c65a6752-e582-4cda-bcfe-3b47368680ae.cpp:10:5: error: expected declaration befor e ‘}’ token    10 |     }       |     ^ In file included from c65a6752-e582-4cda-bcfe-3b47368680ae.cpp:12: c65a6752-e582-4cda-bcfe-3b47368680ae.cpp: In function ‘int main()’: c65a6752-e582-4cda-bcfe-3b47368680ae.cpp:14:13: error: ‘max_fill’ was not declar ed in this scope; did you mean ‘total_fill’?    14 |     assert (max_fill({{1,1,1,1}, {1,1,1,1}}, 9) == 2);       |             ^~~~~~~~

--- CPP_115.cpp
+++ CPP_115.cpp
@@ -2,8 +2,9 @@
 
 int total_fill(vector<vector<int>>& grid, int capacity) {
     int total_fill = 0;
+    int well_fill = 0;
     for (int j = 0; j < grid[0].size(); j++) {
-        int well_fill = 0;
+        well_fill = 0;
         for (int i = 0; i < grid.size(); i++) {
             well_fill += grid[i][j];
         }
