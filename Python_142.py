a8ef2caffb292793a0884eb143e9374c770fb97e
beep boop <githubbot@vadim.me>
2024-07-09 05:47:45 +0200

    Solution 54 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/22jclxwk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/668dcf9b-6864-4f7c-9ede-877fc8a11 c94.py", line 48, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/668dcf9b-6864-4f7c-9ede-877fc8a11 c94.py", line 18, in check     sum_squares( TypeError: sum_squares() takes 0 positional arguments but 1 was given

--- Python_142.py
+++ Python_142.py
@@ -1,21 +1,13 @@
-```Python
 def sum_squares():
     while True:
         try:
             lst = list(map(int, input("Enter numbers separated by space: ").split()))
-            if len(lst) > 0:
-                break
+            break
         except ValueError:
             print("Invalid input. Please enter numbers separated by spaces.")
 
-    result = sum(
-        (
-            x**2 if i % 3 != 0 else x**3 if i % 4 != 0 else x
-        )
-        for i, x in enumerate(lst)
-    )
+    result = sum(x**2 if i % 3 == 0 and i < len(lst) - 1 and (i + 1) % 4 != 0 \
+                 else x**3 if i % 4 == 0 and i < len(lst) - 2 and (i + 2) % 3 != 0 \
+                 else x for i, x in enumerate(lst))
 
-    return str(result)
-
-
-print("The final result is: ", sum_squares())
+    return result
