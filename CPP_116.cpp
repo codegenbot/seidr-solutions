cab5ee5e13bd9e23628cefaa632b3583344b89db
beep boop <githubbot@vadim.me>
2024-07-09 20:15:58 +0200

    Solution 63 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lvdfcyfj Prompt:       |                    ^~~ 77dd343b-dd8c-49c7-aec8-51b0c151613f.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 77dd343b-dd8c-49c7-aec8-51b0c151613f.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 77dd343b-dd8c-49c7-aec8-51b0c151613f.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 77dd343b-dd8c-49c7-aec8-51b0c151613f.cpp:9: 77dd343b-dd8c-49c7-aec8-51b0c151613f.cpp: In function ‘int main()’: 77dd343b-dd8c-49c7-aec8-51b0c151613f.cpp:19:20: error: ‘sort_array’ was not decl ared in this scope    19 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 77dd343b-dd8c-49c7-aec8-51b0c151613f.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -13,10 +13,3 @@ std::vector<int> sort_array(std::vector<int> v) {
     std::sort(v.begin(), v.end());
     return v;
 }
-
-int main_test() {
-    std::vector<int> a({1, 2, 4, 8, 16, 32});
-    std::vector<int> b = sort_array(a);
-    assert(issame(b, {1, 2, 4, 8, 16, 32}));
-    return 0;
-}
