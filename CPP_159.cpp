ab35b9545d697aab92c0b08d24f3b40e366e14dd
beep boop <githubbot@vadim.me>
2024-07-09 05:16:11 +0200

    Solution 61 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/3dz5v5gs Prompt: 9e1d552b-441d-4e68-9df0-afb689bfec9e.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 9e1d552b-441d-4e68-9df0-afb689bfec9e.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -7,5 +7,6 @@ using namespace std;
 vector<int> eat(int number, int need, int remaining) {
     int total = number + need;
     int left = min(remaining, need);
+    
     return {total, remaining - left};
 }
