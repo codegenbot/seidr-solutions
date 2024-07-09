31f17eb248a251c5ed23ac5a3133421651ccc754
beep boop <githubbot@vadim.me>
2024-07-09 11:57:28 +0200

    Solution 22 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3e7tqgvq Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -7,10 +7,10 @@ bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-std::string separate_paren_groups(std::string s) {
+string separate_paren_groups(string s) {
     int count = 0;
-    std::string current_group = "";
-    std::string result = "";
+    string current_group = "";
+    string result = "";
 
     for (char c : s) {
         if (c == '(') {
@@ -20,9 +20,7 @@ std::string separate_paren_groups(std::string s) {
             count--;
             current_group += c;
             if (count == 0) {
-                for (auto& d : current_group) {
-                    result.push_back(d);
-                }
+                result += current_group;
                 current_group = "";
             }
         } else {
@@ -38,7 +36,7 @@ std::string separate_paren_groups(std::string s) {
 }
 
 int main() {
-    std::string s;
+    string s;
     std::cout << "Enter a string: ";
     std::getline(std::cin, s);
     std::cout << "Result: " << separate_paren_groups(s) << std::endl;
