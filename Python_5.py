27de3bd09072df7ac9a8113c34570a492cf4f5fa
beep boop <githubbot@vadim.me>
2024-07-09 03:35:38 +0200

    Solution 9 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7pnc8lpr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ed2d66b4-dcc4-4e4d-a5d2-49b2bb9a3 271.py", line 1, in <module>     def intersperse(numbers: List[int], delimeter: int) -> List[int]:                              ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_5.py
+++ Python_5.py
@@ -1,7 +1,8 @@
 from typing import list
 
-def intersperse(numbers: list, delimiter: int) -> list:
+
+def intersperse(numbers: list, delimeter: int) -> list:
     result = [numbers[0]]
     for i in range(1, len(numbers)):
-        result.extend([result[-1], delimiter, numbers[i]])
+        result.extend([result[-1], delimeter, numbers[i]])
     return result
