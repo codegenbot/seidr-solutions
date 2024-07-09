6e5c583683df3572d116cc8198f9306b6eca94c2
beep boop <githubbot@vadim.me>
2024-07-09 19:54:40 +0200

    Solution 94 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/d00882cu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb73ef7f-93b4-4f45-a079-1e7812d55 885.py", line 2     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,9 +1,1 @@
-def solve(a, b, c):
-    return -c / b
-
-a = int(input().strip())
-b = int(input().strip())
-c = int(input().strip())
-
-result = solve(a, b, c)
-print(result)
+a, b, c = map(int, input().strip().split())
