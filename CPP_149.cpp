8aeef388120c9c3583aaf0b6a557b7e471e4cc2e
beep boop <githubbot@vadim.me>
2024-07-09 02:19:31 +0200

    Solution 7 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h3blrigf Prompt:    20 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 1829b983-e063-4324-8560-1154c72d3aca.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1829b983-e063-4324-8560-1154c72d3aca.cpp:20:45: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1829b983-e063-4324-8560-1154c72d3aca.cpp:19: 1829b983-e063-4324-8560-1154c72d3aca.cpp: In function ‘int main()’: 1829b983-e063-4324-8560-1154c72d3aca.cpp:29:20: error: ‘sorted_list_sum’ was not  declared in this scope    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 1829b983-e063-4324-8560-1154c72d3aca.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,16 +1,10 @@
-bool issame(string s1, string s2) {
+bool issame(string a, string b) {
     int sum1 = 0, sum2 = 0;
-    for (int i = 0; i < s1.length(); i++) {
-        if (s1[i] % 2 == 0)
-            sum1 += s1[i];
-        else
-            sum1 -= s1[i];
+    for (int i = 0; i < a.length(); i++) {
+        sum1 += (a[i] - '0');
     }
-    for (int i = 0; i < s2.length(); i++) {
-        if (s2[i] % 2 == 0)
-            sum2 += s2[i];
-        else
-            sum2 -= s2[i];
+    for (int i = 0; i < b.length(); i++) {
+        sum2 += (b[i] - '0');
     }
     return sum1 == sum2;
 }
@@ -18,7 +12,7 @@ bool issame(string s1, string s2) {
 vector<string> sorted_list_sum(vector<string> lst) {
     vector<string> result;
     for (const auto& str : lst) {
-        if (issame(str, "a")) {
+        if (str.length() % 2 == 0 || issame(str, "1")) {
             result.push_back(str);
         }
     }
