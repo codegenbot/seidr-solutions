b76b570419474b65aa8b82a783c1531c9c6222c0
beep boop <githubbot@vadim.me>
2024-07-09 09:03:02 +0200

    Solution 84 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/siauvrg0 Prompt:       |                              ^~~~~~ 15924e1d-69a9-4641-9a4f-d3227a554520.cpp:52:37: error: ‘string’ was not declared  in this scope    52 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 15924e1d-69a9-4641-9a4f-d3227a554520.cpp:52:44: error: ‘b’ was not declared in t his scope    52 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 15924e1d-69a9-4641-9a4f-d3227a554520.cpp:52:45: error: expression list treated a s compound expression in initializer [-fpermissive]    52 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 15924e1d-69a9-4641-9a4f-d3227a554520.cpp:51: 15924e1d-69a9-4641-9a4f-d3227a554520.cpp: In function ‘int main()’: 15924e1d-69a9-4641-9a4f-d3227a554520.cpp:61:20: error: ‘by_length’ was not decla red in this scope    61 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 15924e1d-69a9-4641-9a4f-d3227a554520.cpp:61:19: error: ‘issame’ cannot be used a s a function    61 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -45,17 +45,16 @@ vector<string> by_length(vector<int> arr) {
         result.push_back(str);
     }
     
-    bool issame(vector<string> a, vector<string>b){
-        if (a.size() != b.size()) {
-            return false;
-        }
-        for(int i=0; i<a.size();i++){
-            if(a[i] != b[i]){
-                return false;
-            }
-        }
-        return true;
-    }
-    
     return result;
+}
+
+bool issame(vector<string> a, vector<string>b){
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for(int i=0; i<a.size();i++){
+        if(a[i] != b[i]){
+            return false;
+    }
+    return true;
 }
