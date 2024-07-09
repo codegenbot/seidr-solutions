b34e3efc10b6c8532bacefae33f0e2b13c9f3e47
beep boop <githubbot@vadim.me>
2024-07-09 13:22:45 +0200

    Solution 94 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qnqbku9p Prompt: /usr/include/c++/11/bits/basic_string.h:760:30: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   760 |                   _M_destroy(_M_allocated_capacity);       |                              ^~~~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/bits/basic_string.h:765:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   765 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,30 +1,45 @@
+```cpp
 #include <iostream>
 #include <vector>
+#include <string>
 
-using namespace std;
+std::vector<std::pair<int, std::string>> romanMap;
 
-vector<pair<int, char>> romanMap;
+romanMap.push_back({ 1000, "M" });
+romanMap.push_back({ 900, "CM" });
+romanMap.push_back({ 500, "D" });
+romanMap.push_back({ 400, "CD" });
+romanMap.push_back({ 100, "C" });
+romanMap.push_back({ 90, "XC" });
+romanMap.push_back({ 50, "L" });
+romanMap.push_back({ 40, "XL" });
+romanMap.emplace_back(1, "I");
 
-romanMap.push_back({1000, 'M'});
-romanMap.push_back({900, 'C'+'M'});
-romanMap.push_back({500, 'D'});
-romanMap.push_back({400, 'C'+'D'});
-romanMap.push_back({100, 'C'});
-romanMap.push_back({90, 'X'+'C'});
-romanMap.push_back({50, 'L'});
-romanMap.push_back({40, 'X'+'L'});
-romanMap.emplace_back(1, 'I');
+std::string int_to_string(int number) {
+    if (number == 0)
+        return "0";
+    std::string str = "";
+    char* roman[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    char* tens[] = {"", "", "X", "XX", "XXX", "XL", "L", "XC", "C"};
+    if (number >= 10) {
+        str += tens[number / 10];
+        number %= 10;
+    }
+    if (number > 0)
+        str += roman[number];
+    return str;
+}
 
-string int_to_mini_roman(int number) {
-    string roman = "";
+std::string int_to_mini_roman(int number) {
+    std::string roman = "";
     for (const auto& pair : romanMap) {
         while (number >= pair.first) {
             number -= pair.first;
-            roman += pair.second;
+            roman = roman + int_to_string(pair.second);
         }
     }
     if (number > 0) {
-        roman += 'I';
+        roman += "I";
     } else if (number < 0) {
         roman = "-" + roman;
     }
@@ -33,7 +48,7 @@ string int_to_mini_roman(int number) {
 
 int main() {
     for(int i = 1; i <= 3999; i++) { 
-        cout << i << ": " << int_to_mini_roman(i) << endl;
+        std::cout << i << ": " << int_to_mini_roman(i) << std::endl;
     }
     return 0;
 }
