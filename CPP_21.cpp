cde29ba23467f2e2b6d28a14d76f2076922e1bec
beep boop <githubbot@vadim.me>
2024-07-09 13:58:32 +0200

    Solution 64 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/wk2mpzp3 Prompt:    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 1c65f78e-d5df-476c-b7f9-dcc6162c43d4.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 1c65f78e-d5df-476c-b7f9-dcc6162c43d4.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 1c65f78e-d5df-476c-b7f9-dcc6162c43d4.cpp:11: 1c65f78e-d5df-476c-b7f9-dcc6162c43d4.cpp: In function ‘int main()’: 1c65f78e-d5df-476c-b7f9-dcc6162c43d4.cpp:21:20: error: ‘rescale_to_unit’ was not  declared in this scope    21 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 1c65f78e-d5df-476c-b7f9-dcc6162c43d4.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,23 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cassert>
-
-using namespace std;
-
-vector<float> rescale_to_unit(vector<float> numbers) {
-    float min_num = *min_element(numbers.begin(), numbers.end());
-    float max_num = *max_element(numbers.begin(), numbers.end());
-    
-    for (float &num : numbers) {
-        num = (num - min_num) / (max_num - min_num);
-    }
-    
-    return numbers;
-}
-
-int main() {
-    assert(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) == vector<float>({0.25, 0.0, 1.0, 0.5, 0.75}));
-    
-    return 0;
+bool issame(const vector<float>& a, const vector<float>& b) {
+    return a == b;
 }
