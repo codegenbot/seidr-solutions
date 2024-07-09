17604f8b5372ddc9c07062192abac827406bd99e
beep boop <githubbot@vadim.me>
2024-07-10 00:01:22 +0200

    Solution 17 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/1jn0ph2d Prompt:       |                                            ^~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_uninitialized.h:64,                  from /usr/include/c++/11/vector:66,                  from 52ca0293-b0c6-4373-9db8-1d65a2e00dc0.cpp:3: /usr/include/c++/11/ext/alloc_traits.h: In instantiation of ‘struct __gnu_cxx::_ _alloc_traits<std::allocator<double>, double>’: /usr/include/c++/11/bits/stl_vector.h:87:21:   required from ‘struct std::_Vecto r_base<double, std::allocator<double> >’ /usr/include/c++/11/bits/stl_vector.h:389:11:   required from ‘class std::vector <double>’ 52ca0293-b0c6-4373-9db8-1d65a2e00dc0.cpp:7:27:   required from here /usr/include/c++/11/ext/alloc_traits.h:59:53: error: no type named ‘size_type’ i n ‘struct std::allocator_traits<std::allocator<double> >’    59 |     typedef typename _Base_type::size_type          size_type;       |                                                     ^~~~~~~~~ /usr/include/c++/11/ext/alloc_traits.h:60:53: error: no type named ‘difference_t ype’ in ‘struct std::allocator_traits<std::allocator<double> >’    60 |     typedef typename _Base_type::difference_type    difference_type;       |                                                     ^~~~~~~~~~~~~~~ /usr/include/c++/11/ext/alloc_traits.h:68:23: error: ‘max_size’ has not been dec lared in ‘__gnu_cxx::__alloc_traits<std::allocator<double>, double>::_Base_type’    68 |     using _Base_type::max_size;       |                       ^~~~~~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,11 +1,11 @@
 ```cpp
-#include <vector>
 #include <cmath>
+#include <vector>
 
-double vectorDistance(const std::vector<double>& v1, const std::vector<double>& v2) {
+double vectorDistance(const std::vector<float>& v1, const std::vector<float>& v2) {
     double sum = 0;
     for (int i = 0; i < v1.size(); i++) {
-        sum += pow(v1[i] - v2[i], 2);
+        sum += pow((float)v1[i] - (float)v2[i], 2);
     }
     return sqrt(sum);
 }
