85b9f1d196361c03c5c7ada4a2e4101827d28abf
beep boop <githubbot@vadim.me>
2024-07-09 17:17:25 +0200

    Solution 11 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/rlozoors Prompt:    39 | int main(){       |     ^~~~ 164bfd2b-396f-4020-acd2-cff46ac9b53a.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 164bfd2b-396f-4020-acd2-cff46ac9b53a.cpp:38: 164bfd2b-396f-4020-acd2-cff46ac9b53a.cpp: In function ‘int main()’: 164bfd2b-396f-4020-acd2-cff46ac9b53a.cpp:40:13: error: ‘boost’ has not been decl ared    40 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 164bfd2b-396f-4020-acd2-cff46ac9b53a.cpp:40:35: error: expected primary-expressi on before ‘>’ token    40 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 164bfd2b-396f-4020-acd2-cff46ac9b53a.cpp:40:37: error: ‘compare_one’ was not dec lared in this scope; did you mean ‘compare’?    40 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+bool compare(int a, int b) {
+    if (a > b) return true;
+    else if (b > a) return false;
+    else return false; // or return true, depending on your needs
+}
+
+bool compareString(string a, string b) {
+    if (stoi(a) > stoi(b)) return true;
+    else if (stoi(b) > stoi(a)) return false;
+    else return false; // or return true, depending on your needs
+}
+
 int main() {
     int a, b;
     cout << "Enter the first number: ";
@@ -10,10 +22,10 @@ int main() {
     cout << "Enter the second number: ";
     cin >> b;
 
-    if (a > b) {
+    if (compare(a, b)) {
         cout << "The larger number is: " << a << endl;
     }
-    else if (b > a) {
+    else if (!compare(b, a)) {
         cout << "The larger number is: " << b << endl;
     }
     else {
