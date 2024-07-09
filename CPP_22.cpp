71af159dac94c8a2553503ca6703046281b4f5da
beep boop <githubbot@vadim.me>
2024-07-09 02:57:01 +0200

    Solution 24 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/bvoim4el Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ef70fa64-7d45-443b-a248-5c40dbbac68b.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ef70fa64-7d45-443b-a248-5c40dbbac68b.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ef70fa64-7d45-443b-a248-5c40dbbac68b.cpp:11: ef70fa64-7d45-443b-a248-5c40dbbac68b.cpp: In function ‘int main()’: ef70fa64-7d45-443b-a248-5c40dbbac68b.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ ef70fa64-7d45-443b-a248-5c40dbbac68b.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,23 +1,15 @@
-#include <iostream>
-#include <list>
-#include <vector>
-#include <boost/any.hpp>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
+bool issame(vector<int> a, vector<int> b) {
+    return a == b; 
 }
 
-std::vector<int> filter_integers(std::list<boost::any> values) {
-    std::vector<int> result;
-    for (const auto& value : values) {
-        if (boost::any_cast<int>(value, 0) != 0) { 
-            result.push_back(boost::any_cast<int>(value));
+vector<int> filter_integers(vector<vector<int>> values) {
+    vector<int> result;
+    for (auto& value : values) {
+        if (std::find(value.begin(), value.end(), 0) == value.end()) { // check for 0
+            for (int v : value) {
+                result.push_back(v);
+            }
         }
     }
     return result;
-}
-
-int main() {
-    assert(issame(filter_integers({3, boost::any("c"), 3, 3, boost::any("a"), boost::any("b")}), {3, 3, 3}));
-    return 0;
 }
