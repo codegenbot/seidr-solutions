f05ce8ced31bbb6e5d7966d3dfbb4bb0dc3cb59c
beep boop <githubbot@vadim.me>
2024-07-09 15:59:56 +0200

    Solution 30 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7gxeyycu Prompt: tor, _Allocator&) [with _InputIterator = std::move_iterator<int*>; _ForwardItera tor = int*; _Allocator = std::pmr::polymorphic_allocator<int>]’ /usr/include/c++/11/bits/stl_uninitialized.h:355:2:   required from ‘_ForwardIte rator std::__uninitialized_move_if_noexcept_a(_InputIterator, _InputIterator, _F orwardIterator, _Allocator&) [with _InputIterator = int*; _ForwardIterator = int *; _Allocator = std::pmr::polymorphic_allocator<int>]’ /usr/include/c++/11/bits/vector.tcc:474:3:   required from ‘void std::vector<_Tp , _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [w ith _Args = {const int&}; _Tp = int; _Alloc = std::pmr::polymorphic_allocator<in t>; std::vector<_Tp, _Alloc>::iterator = std::vector<int, std::pmr::polymorphic_ allocator<int> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::pmr:: polymorphic_allocator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ fabcf917-6bd8-4328-a8a1-536b04a7c94a.cpp:35:24:   required from here /usr/include/c++/11/memory_resource:251:13: error: no matching function for call  to ‘operator new(sizetype, int*&)’ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘int*’ to ‘std::alig n_val_t’

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <memory_resource>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
-std::vector<int> strange_sort_list(std::vector<int> lst) {
-    std::vector<int> result;
+std::pmr::vector<int> strange_sort_list(std::pmr::pmr_vector<int> lst) {
+    std::pmr::vector<int> result;
     while (!lst.empty()) {
         int min_val = *std::min_element(lst.begin(), lst.end());
         result.push_back(min_val);
@@ -26,14 +27,15 @@ std::vector<int> strange_sort_list(std::vector<int> lst) {
 }
 
 int main() {
-    std::vector<int> input;
+    std::pmr::monotonic_buffer_resource buffer_resource;
+    std::pmr::vector<int> input(buffer_resource);
     for (int i = 0; i < 6; ++i) {
         int num;
         std::cin >> num;
-        input.push_back(num);
+        input.emplace_back(num);
     }
 
-    std::vector<int> output = strange_sort_list(input);
+    std::pmr::vector<int> output = strange_sort_list(input);
 
     if (issame(output, {1,2,3,4,5,6})) {
         std::cout << "True" << std::endl;
