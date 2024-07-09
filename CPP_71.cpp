8e43810e36095d07d2892f84ae2820dbaf1c76fb
beep boop <githubbot@vadim.me>
2024-07-09 21:19:23 +0200

    Solution 5 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/58qxvgdk Prompt: 41ed9a6a-51b0-4281-b93e-ec4af3e764b2.cpp: In function ‘float triangle_area(float , float, float)’: 41ed9a6a-51b0-4281-b93e-ec4af3e764b2.cpp:4:23: error: ‘sqrt’ was not declared in  this scope     4 |         return round((sqrt(s * (s - a) * (s - b) * (s - c))) * 100.0) / 100.0;       |                       ^~~~ 41ed9a6a-51b0-4281-b93e-ec4af3e764b2.cpp:4:16: error: ‘round’ was not declared i n this scope     4 |         return round((sqrt(s * (s - a) * (s - b) * (s - c))) * 100.0) / 100.0;       |                ^~~~~ In file included from 41ed9a6a-51b0-4281-b93e-ec4af3e764b2.cpp:10: 41ed9a6a-51b0-4281-b93e-ec4af3e764b2.cpp: In function ‘int main()’: 41ed9a6a-51b0-4281-b93e-ec4af3e764b2.cpp:12:13: error: ‘abs’ was not declared in  this scope    12 |     assert (abs(triangle_area(2, 2, 10) +1)<0.01);       |             ^~~

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,10 +1,15 @@
 #include <cmath>
 
-float triangle_area(float a, float c) {
+float triangle_area(float a, float float b, float c) {
     if (a + b > c && a + c > b && b + c > a) {
         float s = (a + b + c) / 2;
-        return std::round(std::pow(s, 0.5)*s*(s-a)*(s-b)*(s-c)) * 100.0 / 100.0;
+        return std::round(std::pow(s * (s - a) * (s - b) * (s - c), 0.5)) / 100.0f;
     } else {
         return -1.0f;
     }
+}
+
+int main() {
+    assert (std::abs(triangle_area(2, 2, 10) +1)<0.01);
+    return 0;
 }
