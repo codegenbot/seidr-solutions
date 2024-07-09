af63518d25b0d4e2670da054923c6d92ee56fd65
beep boop <githubbot@vadim.me>
2024-07-09 10:43:01 +0200

    Solution 32 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/gop9ghvx Prompt:       |                             ^~~ c60b51df-29ff-453f-a94a-90e613bb5cab.cpp:26:33: error: expected primary-expressi on before ‘char’    26 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ c60b51df-29ff-453f-a94a-90e613bb5cab.cpp:26:38: error: expected primary-expressi on before ‘int’    26 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ c60b51df-29ff-453f-a94a-90e613bb5cab.cpp:26:44: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from c60b51df-29ff-453f-a94a-90e613bb5cab.cpp:25: c60b51df-29ff-453f-a94a-90e613bb5cab.cpp: In function ‘int main()’: c60b51df-29ff-453f-a94a-90e613bb5cab.cpp:40:20: error: ‘histogram’ was not decla red in this scope    40 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ c60b51df-29ff-453f-a94a-90e613bb5cab.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,17 +1,36 @@
-#include <iostream>
-#include <map>
-#include <sstream>
-#include <cassert>
+map<char, int> histogram(string test);
+map<char, int> issame(const map<char, int>& a, const map<char, int>& b) {
+    map<char, int> result;
 
-using namespace std;
+    for (const auto& elem : a) {
+        if (b.find(elem.first) != b.end() && b.at(elem.first) == elem.second) {
+            result[elem.first] = elem.second;
+        }
+    }
 
-bool issame(map<char, int> a, map<char, int> b) {
-    return a == b;
+    return result;
 }
 
-map<char, int> histogram(string test);
-
-int main() {
-    assert(issame(histogram("a"), {{'a', 1}}));
-    return 0;
+map<char, int> histogram(string test) {
+    map<char, int> result;
+    istringstream iss(test);
+    string word;
+    while (iss >> word) {
+        for (char c : word) {
+            result[c]++;
+        }
+    }
+    int maxCount = 0;
+    for (const auto& entry : result) {
+        if (entry.second > maxCount) {
+            maxCount = entry.second;
+        }
+    }
+    map<char, int> maxCountLetters;
+    for (const auto& entry : result) {
+        if (entry.second == maxCount) {
+            maxCountLetters[entry.first] = entry.second;
+        }
+    }
+    return maxCountLetters;
 }
