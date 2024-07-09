11656ff5dc1fba2226e8d268813b6ce2c5938caa
beep boop <githubbot@vadim.me>
2024-07-09 05:14:45 +0200

    Solution 84 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/kih5li8d Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f8b264f-9300-4768-914a-a292cff80 10a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- twitter.py
+++ twitter.py
@@ -1,5 +1,6 @@
+```
 def validate_tweet(tweet):
-    if len(tweet) == 0:
+    if len(tweet.strip()) == 0:
         return "You didn't type anything"
     elif len(tweet.strip()) > 140:
         return "Too many characters"
@@ -10,3 +11,4 @@ print(validate_tweet(" "))
 print(validate_tweet("1"))
 print(validate_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeat it"))
 print(validate_tweet("40172875*\\(&\\,%^^*!@#\\$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~"))
+print(validate_tweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1"))
