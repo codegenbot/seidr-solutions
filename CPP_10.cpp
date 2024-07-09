8e5afa659016a14d401286a4413e3a71616bc6d5
beep boop <githubbot@vadim.me>
2024-07-09 06:58:44 +0200

    Solution 32 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/m5hu3iak Prompt: 688976f0-c720-4fae-80cd-daa4b8149701.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string make_palindrome(string str){       | ^~~~~~ In file included from 688976f0-c720-4fae-80cd-daa4b8149701.cpp:11: 688976f0-c720-4fae-80cd-daa4b8149701.cpp: In function ‘int main()’: 688976f0-c720-4fae-80cd-daa4b8149701.cpp:13:13: error: ‘make_palindrome’ was not  declared in this scope    13 |     assert (make_palindrome("jerry") == "jerryrrej");       |             ^~~~~~~~~~~~~~~

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,15 +1,14 @@
-
-#include<iostream>
-#include<string>
-#include<cassert>
+#include <iostream>
+#include <string>
+#include <cassert>
 
 using namespace std;
 
-bool is_palindrome(const string &str) {
+bool is_palindrome(const string& str) {
     return str == string(str.rbegin(), str.rend());
 }
 
-string make_palindrome(const string &str){
+string make_palindrome(string str){
     string rev_str(str.rbegin(), str.rend());
     for (int i = str.size(); i >= 0; i--) {
         if (is_palindrome(str.substr(i))) {
