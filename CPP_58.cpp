f28f52f1ef26ee0385d43248e72d826587a4e80e
beep boop <githubbot@vadim.me>
2024-07-09 21:59:06 +0200

    Solution 92 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g34sr5ik Prompt:       |             ^~~~ fb97c354-af62-4033-8679-d0ca17ac075e.cpp:3:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     2 | #include <cassert>   +++ |+#include <ostream>     3 | fb97c354-af62-4033-8679-d0ca17ac075e.cpp: At global scope: fb97c354-af62-4033-8679-d0ca17ac075e.cpp:50:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    50 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ fb97c354-af62-4033-8679-d0ca17ac075e.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a,vector<int> b){       |      ^~~~~~ fb97c354-af62-4033-8679-d0ca17ac075e.cpp:58:5: error: redefinition of ‘int main( )’    58 | int main(){       |     ^~~~ fb97c354-af62-4033-8679-d0ca17ac075e.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,10 +1,10 @@
 #include <vector>
 #include <cassert>
-#include <iostream>
+#include <ostream>
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
+bool same(vector<int> a,vector<int> b){
     return a == b;
 }
 
@@ -20,13 +20,29 @@ vector<int> common(vector<int> l1, vector<int> l2) {
 }
 
 int main() {
-    vector<int> l1 = {4, 3, 2, 8};
-    vector<int> l2 = {2, 4, 6, 8};
+    assert(same(common({4, 3, 2, 8}, {}), {}));
     
-    vector<int> commonElements = common(l1, l2);
-    cout << "Common elements are: ";
-    for(int i : commonElements) {
-        cout << i << " ";
+    int n1, n2; 
+    cout << "Enter the number of elements in list 1: ";
+    cin >> n1;
+    vector<int> l1(n1);
+    for (int i = 0; i < n1; i++) {
+        cin >> l1[i];
     }
+    
+    cout << "Enter the number of elements in list 2: ";
+    cin >> n2;
+    vector<int> l2(n2);
+    for (int i = 0; i < n2; i++) {
+        cin >> l2[i];
+    }
+    
+    vector<int> result = common(l1, l2);
+    cout << "Common elements between the two lists: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+    
     return 0;
 }
