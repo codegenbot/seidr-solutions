24422c04fb86f6fc99e95922d03564c63c3d8a82
beep boop <githubbot@vadim.me>
2024-07-09 02:22:26 +0200

    Solution 30 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/cf3ofget Prompt:  [with _InputIterator = __gnu_cxx::__normal_iterator<const std::__cxx11::basic_s tring<char>*, std::vector<std::__cxx11::basic_string<char> > >; _ForwardIterator  = std::__cxx11::basic_string<char>*]’ /usr/include/c++/11/bits/stl_uninitialized.h:333:37:   required from ‘_ForwardIt erator std::__uninitialized_copy_a(_InputIterator, _InputIterator, _ForwardItera tor, std::allocator<_Tp>&) [with _InputIterator = __gnu_cxx::__normal_iterator<c onst std::__cxx11::basic_string<char>*, std::vector<std::__cxx11::basic_string<c har> > >; _ForwardIterator = std::__cxx11::basic_string<char>*; _Tp = std::__cxx 11::basic_string<char>]’ /usr/include/c++/11/bits/stl_vector.h:558:31:   required from ‘std::vector<_Tp, _Alloc>::vector(const std::vector<_Tp, _Alloc>&) [with _Tp = std::__cxx11::basic _string<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >]’ 7421b3a5-c136-4224-b793-99e2f77ad77d.cpp:57:44:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,29 +1,8 @@
 ```cpp
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-bool issame(int x, int y) {
-    if (x > y) {
-        return false;
-    } else if (y > x) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 vector<string> sorted_list_sum(vector<string> lst) {
     // Remove strings with odd lengths from the vector
-    for (auto it = lst.begin(); it != lst.end(); ) {
-        if (it->length() % 2) {
-            lst.erase(it);
-        } else {
-            ++it;
-        }
-    }
+    lst.erase(std::remove_if(lst.begin(), lst.end(),
+        [](const string& s) { return s.length() % 2; }), lst.end());
 
     // Sort the remaining vector by length and then alphabetically
     std::sort(lst.begin(), lst.end(),
@@ -35,31 +14,4 @@ vector<string> sorted_list_sum(vector<string> lst) {
         });
 
     return lst;
-}
-
-int main() {
-    vector<string> lst = {"cat", "dog", "elephant", "a", "b"};
-    
-    // Sort the list by length and then alphabetically
-    std::sort(lst.begin(), lst.end(),
-        [](const string& a, const string& b) {
-            if (a.length() == b.length()) {
-                return a < b;
-            }
-            return a.length() < b.length();
-        });
-
-    for (auto i : lst)
-    {
-        cout << i << " ";
-    }
-
-    vector<string> result = sorted_list_sum(lst);
-
-    for (auto i : result)
-    {
-        cout << i << " ";
-    }
-
-    return 0;
 }
