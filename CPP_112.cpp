1a8099fbf98ca004141265edf26036d820e40199
beep boop <githubbot@vadim.me>
2024-07-09 21:50:14 +0200

    Solution 4 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/jen3s4nk Prompt:       |                              ^~~~~~ 9e78cdd7-0be1-48db-a01f-68bc829c9628.cpp:28:37: error: ‘string’ was not declared  in this scope    28 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 9e78cdd7-0be1-48db-a01f-68bc829c9628.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 9e78cdd7-0be1-48db-a01f-68bc829c9628.cpp:28:45: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 9e78cdd7-0be1-48db-a01f-68bc829c9628.cpp:27: 9e78cdd7-0be1-48db-a01f-68bc829c9628.cpp: In function ‘int main()’: 9e78cdd7-0be1-48db-a01f-68bc829c9628.cpp:37:20: error: ‘reverse_delete’ was not declared in this scope    37 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 9e78cdd7-0be1-48db-a01f-68bc829c9628.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,11 +1,13 @@
-Here is the completed code:
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-vector<string> reverse_delete(string s, string c) {
-    vector<string> result;
-    string temp = "";
-    for (char ch : s) {
+std::vector<std::string> reverse_delete(std::string s, std::string c) {
+    std::vector<std::string> result;
+    std::string temp = "";
+    for (char& ch : s) {
         bool found = false;
-        for (char cc : c) {
+        for (char& cc : c) {
             if (ch == cc) {
                 found = true;
                 break;
@@ -16,12 +18,17 @@ vector<string> reverse_delete(string s, string c) {
         }
     }
     result.push_back(temp);
-    string revTemp = temp;
-    reverse(revTemp.begin(), revTemp.end());
+    std::string revTemp = temp;
+    std::reverse(revTemp.begin(), revTemp.end());
     if (temp == revTemp) {
         result.push_back("True");
     } else {
         result.push_back("False");
     }
     return result;
+}
+
+int main() {
+    assert(std::vector<std::string>(reverse_delete("mamma", "mia")) == std::vector<std::string>{ "", "True" });
+    return 0;
 }
