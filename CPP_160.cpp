c1db7ffc9484358c76fb281465e30b9927cd6ebb
beep boop <githubbot@vadim.me>
2024-07-09 02:35:04 +0200

    Solution 32 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0xw9ottc Prompt: for array subscript    16 |         } else if (ops[i] == "*") {       |                       ^ 27d11c64-d2b3-4fee-8be9-e29edbffc148.cpp:18:23: error: invalid types ‘int[int]’ for array subscript    18 |         } else if (ops[i] == "//") {       |                       ^ 27d11c64-d2b3-4fee-8be9-e29edbffc148.cpp:20:23: error: invalid types ‘int[int]’ for array subscript    20 |         } else if (ops[i] == "**") {       |                       ^ In file included from 27d11c64-d2b3-4fee-8be9-e29edbffc148.cpp:28: 27d11c64-d2b3-4fee-8be9-e29edbffc148.cpp: In function ‘int main()’: 27d11c64-d2b3-4fee-8be9-e29edbffc148.cpp:30:23: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    30 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~       |                       |       |                       <brace-enclosed initializer list> 27d11c64-d2b3-4fee-8be9-e29edbffc148.cpp:7:31: note:   initializing argument 1 o f ‘int do_algebra(int, std::vector<int>)’     7 | int do_algebra(vector<string> ops, vector<int> nums) {       |                ~~~~~~~~~~~~~~~^~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,18 +1,18 @@
-int do_algebra(vector<string> ops, vector<int> nums) {
+int do_algebra(vector<char> ops, vector<int> nums) {
     int result = 0;
     int temp = 1;
 
     for (int i = 0; i < ops.size(); i++) {
-        if (ops[i] == "+") {
+        if (ops[i] == '+') {
             result += temp * nums[i];
-        } else if (ops[i] == "-") {
+        } else if (ops[i] == '-') {
             result -= temp * nums[i];
-        } else if (ops[i] == "*") {
+        } else if (ops[i] == '*') {
             temp *= nums[i];
-        } else if (ops[i] == "//") {
-            temp = temp / nums[i];
-        } else if (ops[i] == "**") {
-            temp = pow(temp, nums[i]);
+        } else if (ops[i] == '/') {
+            temp /= nums[i];
+        } else if (ops[i] == '^') { // For exponentiation
+            temp = pow((double)temp, (double)nums[i]);
         }
     }
 
