6200f65fb6ccce542f65dbc2aaf7de22d669a7fd
beep boop <githubbot@vadim.me>
2024-07-09 12:07:59 +0200

    Solution 21 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/w26ru0dn Prompt:       |                    ^~~ 98ca7de3-0058-4f97-80c3-0a866336c96d.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 98ca7de3-0058-4f97-80c3-0a866336c96d.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 98ca7de3-0058-4f97-80c3-0a866336c96d.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 98ca7de3-0058-4f97-80c3-0a866336c96d.cpp:13: 98ca7de3-0058-4f97-80c3-0a866336c96d.cpp: In function ‘int main()’: 98ca7de3-0058-4f97-80c3-0a866336c96d.cpp:23:20: error: ‘common’ was not declared  in this scope    23 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 98ca7de3-0058-4f97-80c3-0a866336c96d.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -2,7 +2,15 @@
 #include <vector>
 #include <set>
 
-vector<int> issame(vector<int> a, vector<int> b);
+vector<vector<int>> issame(vector<int> a, vector<int> b) {
+    if(a.size() != b.size()) 
+        return {{a}, {b}};
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) 
+            return {{a}, {b}};
+    }
+    return {{a, b}};
+}
 
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
@@ -14,8 +22,4 @@ vector<int> common(vector<int> l1, vector<int> l2) {
 
     vector<int> result(intersection.begin(), intersection.end());
     return result;
-}
-
-bool issame(vector<int> a, vector<int> b) {
-    // Your logic for checking if the vectors are same
 }
