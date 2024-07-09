6efe64495ee1db5f5e58981093f511d4f19673d7
beep boop <githubbot@vadim.me>
2024-07-09 11:40:16 +0200

    Solution 21 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/pz5ebhy2 Prompt: 9916f1b5-18b8-49a1-87c4-b6767acbaed0.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<float> l;       | ^~~~~~ In file included from 9916f1b5-18b8-49a1-87c4-b6767acbaed0.cpp:3: 9916f1b5-18b8-49a1-87c4-b6767acbaed0.cpp: In function ‘int main()’: 9916f1b5-18b8-49a1-87c4-b6767acbaed0.cpp:5:17: error: ‘max_element’ was not decl ared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ 9916f1b5-18b8-49a1-87c4-b6767acbaed0.cpp:5:13: error: ‘abs’ was not declared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -3,6 +3,6 @@
 
 int main() {
     std::vector<float> l;
-    float max = *std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10});
+    auto max = *std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10});
     return 0;
 }
