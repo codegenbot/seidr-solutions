4e8645407cab8948626c8a63f0df6dac3441fa64
beep boop <githubbot@vadim.me>
2024-07-09 08:58:57 +0200

    Solution 60 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4wqzhzx5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/232b0a17-cfc9-441e-8848-c748ba87f b29.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/232b0a17-cfc9-441e-8848-c748ba87f b29.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,6 +1,9 @@
 ```
 def double_the_difference(lst):
-    odd_sum = sum(i**2 for i in lst if isinstance(i, int) and i % 2 != 0)
-    return (sum(i**2 for i in lst if isinstance(i, int) and i > 0)) * 2 - max(lst)
+    return sum(i**2 for i in lst if isinstance(i, int) and i > 0) * 2 - max(lst)
 
-odd_sum = double_the_difference([1, 2, 3])
+def check(lst1, lst2):
+    if len(set(lst1)) == 1 and len(set(lst2)) == 1:
+        return double_the_difference([*lst1, *lst2])
+    else:
+        return "Invalid input"
