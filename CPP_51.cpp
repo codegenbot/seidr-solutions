3c897d5b557b1a5c6be8a7243689243d714bc06a
beep boop <githubbot@vadim.me>
2024-07-09 15:11:10 +0200

    Solution 11 of CPP/51, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/oemirqy8 Prompt: d1f31eb9-a5f2-405a-a71d-fd65da97bb1a.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string remove_vowels(string text){       | ^~~~~~ In file included from d1f31eb9-a5f2-405a-a71d-fd65da97bb1a.cpp:16: d1f31eb9-a5f2-405a-a71d-fd65da97bb1a.cpp: In function ‘int main()’: d1f31eb9-a5f2-405a-a71d-fd65da97bb1a.cpp:18:13: error: ‘remove_vowels’ was not d eclared in this scope    18 |     assert (remove_vowels("ybcd") == "ybcd");       |             ^~~~~~~~~~~~~

--- CPP_51.cpp
+++ CPP_51.cpp
@@ -1,11 +1,15 @@
+using namespace std;
+
 string remove_vowels(string text){
     string result = "";
     for(char c : text){
         if(toupper(c) != 'A' && toupper(c) != 'E' && toupper(c) != 'I' 
-           && toupper(c) != 'O' && toupper(c) != 'U' && 
-           tolower(c) != 'a' && tolower(c) != 'e' && tolower(c) != 'i' 
-           && tolower(c) != 'o' && tolower(c) != 'u'){
-            result += c; 
+           && toupper(c) != 'O' && toupper(c) != 'U'){
+            if(islower(c)) {
+                result += c;
+            } else {
+                result += tolower(c);
+            }
         }
     }
     return result;
