3940f3a7944456f4f3f12a62d9b1f1ad78b734f0
beep boop <githubbot@vadim.me>
2024-07-09 02:42:31 +0200

    Solution 42 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/yzjvcls6 Prompt:       |                              ^~~~~~ cc2f0347-a681-463b-8b4e-92274998b10e.cpp:17:37: error: ‘string’ was not declared  in this scope    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ cc2f0347-a681-463b-8b4e-92274998b10e.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ cc2f0347-a681-463b-8b4e-92274998b10e.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from cc2f0347-a681-463b-8b4e-92274998b10e.cpp:16: cc2f0347-a681-463b-8b4e-92274998b10e.cpp: In function ‘int main()’: cc2f0347-a681-463b-8b4e-92274998b10e.cpp:26:20: error: ‘bf’ was not declared in this scope    26 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ cc2f0347-a681-463b-8b4e-92274998b10e.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,61 +1,11 @@
-```cpp
 #include <vector>
 #include <string>
-using namespace std;
 
-vector<string> bf(vector<string> planets) {
-    vector<string> result;
-    
-    int mercury = 57.9, venus = 67.2, earth = 0, mars = 225, 
-        jupiter = 778, saturn = 1433, uranus = 2870, neptune = 4495;
+bool issame(vector<string> a,vector<string>b){
+    return a==b;
+}
 
-    for(int i=0; i < planets.size(); i++){
-        if(planets[i] == "Mercury" && (i==planets.size()-1 || find(planets.begin(), planets.end(), "Venus") != planets.end() || 
-            find(planets.begin(), planets.end(), "Earth") != planets.end() || 
-            find(planets.begin(), planets.end(), "Mars") != planets.end() || 
-            find(planets.begin(), planets.end(), "Jupiter") != planets.end() || 
-            find(planets.begin(), planets.end(), "Saturn") != planets.end() || 
-            find(planets.begin(), planets.end(), "Uranus") != planets.end() || 
-            find(planets.begin(), planets.end(), "Neptune") != planets.end())) {
-            result.push_back(planets[i]);
-        }
-        else if(planets[i] == "Venus" && (i==planets.size()-1 || find(planets.begin(), planets.end(), "Earth") != planets.end() || 
-            find(planets.begin(), planets.end(), "Mars") != planets.end() || 
-            find(planets.begin(), planets.end(), "Jupiter") != planets.end() || 
-            find(planets.begin(), planets.end(), "Saturn") != planets.end() || 
-            find(planets.begin(), planets.end(), "Uranus") != planets.end() || 
-            find(planets.begin(), planets.end(), "Neptune") != planets.end())) {
-            result.push_back(planets[i]);
-        }
-        else if(planets[i] == "Earth" && (i==planets.size()-1 || find(planets.begin(), planets.end(), "Mars") != planets.end() || 
-            find(planets.begin(), planets.end(), "Jupiter") != planets.end() || 
-            find(planets.begin(), planets.end(), "Saturn") != planets.end() || 
-            find(planets.begin(), planets.end(), "Uranus") != planets.end() || 
-            find(planets.begin(), planets.end(), "Neptune") != planets.end())) {
-            result.push_back(planets[i]);
-        }
-        else if(planets[i] == "Mars" && (i==planets.size()-1 || find(planets.begin(), planets.end(), "Jupiter") != planets.end() || 
-            find(planets.begin(), planets.end(), "Saturn") != planets.end() || 
-            find(planets.begin(), planets.end(), "Uranus") != planets.end() || 
-            find(planets.begin(), planets.end(), "Neptune") != planets.end())) {
-            result.push_back(planets[i]);
-        }
-        else if(planets[i] == "Jupiter" && (i==planets.size()-1 || find(planets.begin(), planets.end(), "Saturn") != planets.end() || 
-            find(planets.begin(), planets.end(), "Uranus") != planets.end() || 
-            find(planets.begin(), planets.end(), "Neptune") != planets.end())) {
-            result.push_back(planets[i]);
-        }
-        else if(planets[i] == "Saturn" && (i==planets.size()-1 || find(planets.begin(), planets.end(), "Uranus") != planets.end() || 
-            find(planets.begin(), planets.end(), "Neptune") != planets.end())) {
-            result.push_back(planets[i]);
-        }
-        else if(planets[i] == "Uranus" && (i==planets.size()-1 || find(planets.begin(), planets.end(), "Neptune") != planets.end())) {
-            result.push_back(planets[i]);
-        }
-        else if(planets[i] == "Neptune" && (i==planets.size()-1)) {
-            result.push_back(planets[i]);
-        }
-    }
-
-    return result;
+int main() {
+    assert (issame(bf("Jupiter", "Makemake") , {}));
+    return 0;
 }
