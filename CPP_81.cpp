9faec081b5c81bb1baff9927bedda3874d7423c9
beep boop <githubbot@vadim.me>
2024-07-09 10:15:05 +0200

    Solution 68 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/b00mghv0 Prompt:       |           ^~~~~~ In file included from b29de4c6-8665-4479-83fc-4878d2a47f90.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b29de4c6-8665-4479-83fc-4878d2a47f90.cpp:44:37: error: ‘string’ was not declared  in this scope    44 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b29de4c6-8665-4479-83fc-4878d2a47f90.cpp:44:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from b29de4c6-8665-4479-83fc-4878d2a47f90.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from b29de4c6-8665-4479-83fc-4878d2a47f90.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ b29de4c6-8665-4479-83fc-4878d2a47f90.cpp:44:44: error: ‘b’ was not declared in t his scope    44 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -4,14 +4,18 @@
 std::vector<std::string> numerical_letter_grade(const std::vector<double>& grades) {
     std::vector<std::string> result;
     for (double grade : grades) {
-        if (grade >= 0.9)
+        if (grade >= 4.0)
             result.push_back("A");
-        else if (grade >= 0.8)
+        else if (grade >= 3.7)
+            result.push_back("B+");
+        else if (grade >= 3.3)
             result.push_back("B");
-        else if (grade >= 0.7)
+        else if (grade >= 3.0)
+            result.push_back("C+");
+        else if (grade >= 2.7)
             result.push_back("C");
-        else if (grade >= 0.6)
-            result.push_back("D");
+        else if (grade >= 2.3)
+            result.push_back("D+");
         else
             result.push_back("F");
     }
@@ -32,7 +36,7 @@ int main2() {
     std::vector<double> input = {0.0, 0.7};
     std::vector<std::string> result1 = numerical_letter_grade(input);
     
-    std::vector<std::string> result2 = {"F", "C"};
+    std::vector<std::string> result2 = {"F", "D+"};
     if (issame({"F", result1[1]}, result2)) {
         std::cout << "The two vectors are the same." << std::endl;
     } else {
