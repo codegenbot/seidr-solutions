6c18f264c5e68b37b667bfa044edc22eb4d344b3
beep boop <githubbot@vadim.me>
2024-07-09 06:41:47 +0200

    Solution 82 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/rjpd2b4p Prompt:    45 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2e7500ad-c0a0-4a55-8529-bf5d7a5d1e99.cpp:53:5: error: redefinition of ‘int main( )’    53 | int main(){       |     ^~~~ 2e7500ad-c0a0-4a55-8529-bf5d7a5d1e99.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~ In file included from 2e7500ad-c0a0-4a55-8529-bf5d7a5d1e99.cpp:44: 2e7500ad-c0a0-4a55-8529-bf5d7a5d1e99.cpp: In function ‘int main()’: 2e7500ad-c0a0-4a55-8529-bf5d7a5d1e99.cpp:54:20: error: ‘separate_paren_groups’ w as not declared in this scope    54 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 2e7500ad-c0a0-4a55-8529-bf5d7a5d1e99.cpp:54:19: error: ‘issame’ cannot be used a s a function    54 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -3,19 +3,15 @@
 #include <string>
 #include <cassert>
 
-using namespace std;
-
 bool issame(const vector<string>& a, const vector<string>& b) {
     if (a.size() != b.size()) {
         return false;
     }
-  
     for (size_t i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
-  
     return true;
 }
 
@@ -52,8 +48,8 @@ int main() {
     for (string group : groups) {
         cout << group << endl;
     }
-  
-    assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), vector<string>{"()", "(())", "(()())"}));
+
+    assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), {"()", "(())", "(()())"}));
 
     return 0;
 }
