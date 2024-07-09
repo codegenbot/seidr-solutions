b659e21bb246729c36d791b109fb6a07ac1e9b74
beep boop <githubbot@vadim.me>
2024-07-09 03:57:45 +0200

    Solution 32 of CPP/77, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/oq6cd7dv Prompt: bc565365-5ba7-4391-82e7-7cbb95faa621.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from bc565365-5ba7-4391-82e7-7cbb95faa621.cpp:12: bc565365-5ba7-4391-82e7-7cbb95faa621.cpp: In function ‘int main()’: bc565365-5ba7-4391-82e7-7cbb95faa621.cpp:14:13: error: ‘iscuber’ was not declare d in this scope    14 |     assert (iscuber(1729) == false);       |             ^~~~~~~

--- CPP_77.cpp
+++ CPP_77.cpp
@@ -1,10 +1,10 @@
-Here is the completed code:
+#include <cmath>
+#include <cassert>
 
 bool iscuber(int a){
     int i = 1;
     while (pow(i,3) <= a){
-        if (pow(i,3) == a)
-            return true;
+        if (pow(i,3) == a) return true;
         i++;
     }
     return false;
