ef5bf0a551f3cd15fe51a92125f3501dcd09f851
beep boop <githubbot@vadim.me>
2024-07-09 19:24:06 +0200

    Solution 2 of mastermind, ratio 0.4 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/8mudbrv3 Prompt:    13 |             guessFreq[guess[i]]++;       |             ^~~~~~~~~ 33ae914f-3ad0-4f30-ac9a-35768f9ed907.cpp:17:20: error: ‘codeFreq’ was not declar ed in this scope    17 |     for (auto it = codeFreq.begin(); it != codeFreq.end(); ++it) {       |                    ^~~~~~~~ 33ae914f-3ad0-4f30-ac9a-35768f9ed907.cpp:18:38: error: ‘guessFreq’ was not decla red in this scope    18 |         whitePegs += min(it->second, guessFreq[it->first]);       |                                      ^~~~~~~~~ 33ae914f-3ad0-4f30-ac9a-35768f9ed907.cpp:18:22: error: ‘min’ was not declared in  this scope; did you mean ‘main’?    18 |         whitePegs += min(it->second, guessFreq[it->first]);       |                      ^~~       |                      main 33ae914f-3ad0-4f30-ac9a-35768f9ed907.cpp:21:5: error: ‘cout’ was not declared in  this scope    21 |     cout << whitePegs << endl << blackPegs << endl;       |     ^~~~ 33ae914f-3ad0-4f30-ac9a-35768f9ed907.cpp:21:26: error: ‘endl’ was not declared i n this scope    21 |     cout << whitePegs << endl << blackPegs << endl;       |                          ^~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,24 +1,30 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
 int main() {
     string code, guess;
     cin >> code >> guess;
 
-    int white = 0, black = 0;
+    int whitePegs = 0, blackPegs = 0;
     map<char, int> codeFreq, guessFreq;
 
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
-            ++black;
+            blackPegs++;
         } else {
-            ++codeFreq[code[i]];
-            ++guessFreq[guess[i]];
+            codeFreq[code[i]]++;
+            guessFreq[guess[i]]++;
         }
     }
 
     for (auto it = codeFreq.begin(); it != codeFreq.end(); ++it) {
-        white += min(it->second, guessFreq[it->first]);
+        whitePegs += std::min(it->second, guessFreq[it->first]);
     }
 
-    cout << white << endl << black << endl;
+    cout << whitePegs << endl << blackPegs << endl;
 
     return 0;
 }
