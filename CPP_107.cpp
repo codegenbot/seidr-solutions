431e1408920f38692582617e120eb1e17b7be194
beep boop <githubbot@vadim.me>
2024-07-09 10:37:46 +0200

    Solution 29 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ytx8ui5k Prompt:       |                    ^~~ 1544a08d-34df-4889-bf29-8493a8298572.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1544a08d-34df-4889-bf29-8493a8298572.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1544a08d-34df-4889-bf29-8493a8298572.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1544a08d-34df-4889-bf29-8493a8298572.cpp:18: 1544a08d-34df-4889-bf29-8493a8298572.cpp: In function ‘int main()’: 1544a08d-34df-4889-bf29-8493a8298572.cpp:28:20: error: ‘even_odd_palindrome’ was  not declared in this scope    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 1544a08d-34df-4889-bf29-8493a8298572.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
 bool issame(vector<int> a, vector<int> b){
@@ -20,4 +23,10 @@ vector<int> even_odd_palindrome(int n){
         }
     }
     return result;
+}
+
+int main() {
+    assert(issame(even_odd_palindrome(1), {0, 1}));
+    
+    return 0;
 }
