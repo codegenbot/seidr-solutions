08f3f9f5e506a252e770e0cf0cc6941e440ae546
beep boop <githubbot@vadim.me>
2024-07-09 02:33:42 +0200

    Solution 64 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhnk3fn Prompt:       |     ^~~~ In file included from 5fc2fd97-b659-4cbd-bbe6-372e4d4deca9.cpp:27: 5fc2fd97-b659-4cbd-bbe6-372e4d4deca9.cpp: In function ‘int main()’: 5fc2fd97-b659-4cbd-bbe6-372e4d4deca9.cpp:29:13: error: ‘boost’ has not been decl ared    29 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 5fc2fd97-b659-4cbd-bbe6-372e4d4deca9.cpp:29:35: error: expected primary-expressi on before ‘>’ token    29 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 5fc2fd97-b659-4cbd-bbe6-372e4d4deca9.cpp: In function ‘std::variant<std::__cxx11 ::basic_string<char, std::char_traits<char>, std::allocator<char> >, int, double , float> compare_one(std::variant<std::__cxx11::basic_string<char, std::char_tra its<char>, std::allocator<char> >, int, double, float>, std::variant<std::__cxx1 1::basic_string<char, std::char_traits<char>, std::allocator<char> >, int, doubl e, float>)’: 5fc2fd97-b659-4cbd-bbe6-372e4d4deca9.cpp:20:1: warning: control reaches end of n on-void function [-Wreturn-type]    20 | }       | ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,4 @@
-#include <boost/variant.hpp>
+#include <variant>
 #include <string>
 
 using namespace std;
@@ -9,17 +9,47 @@ variant<string, int, double, float> compare_one(variant<string, int, double, flo
         string strB = get<string>(b);
         return strA > strB ? a : b;
     }
-    if ((holds_alternation<int>(a) && holds_alternation<string>(b)) || (holds_alternation<string>(a) && holds_alternation<int>(b))) {
+    else if ((holds_alternation<int>(a) && holds_alternative<string>(b)) || (holds_alternation<string>(a) && holds_alternation<int>(b))) {
         int val1 = get<int>(a), val2 = get<string>(b);
         return val1 > 0 ? a : b;
     }
-    if ((holds_alternation<string>(a) && holds_alternation<int>(b)) || (holds_alternation<string>(a) && holds_alternation<int>(b))) {
-        string strA = get<string>(a), intB = get<int>(b);
-        return strA > to_string(intB) ? a : b;
+    else if ((holds_alternation<string>(a) && holds_alternation<double>(b)) || (holds_alternation<string>(a) && holds_alternation<float>(b))) {
+        string strA = get<string>(a), val2 = get<double>(b);
+        return stod(strA) > val2 ? a : b;
+    }
+    else if ((holds_alternation<int>(a) && holds_alternation<double>(b)) || (holds_alternation<int>(a) && holds_alternation<float>(b))) {
+        int val1 = get<int>(a), val2 = get<double>(b);
+        return val1 > val2 ? a : b;
+    }
+    else if ((holds_alternation<int>(a) && holds_alternation<string>(b)) || (holds_alternation<int>(a) && holds_alternation<string>(b))) {
+        int val1 = get<int>(a), strB = get<string>(b);
+        return stoi(strB) > val1 ? b : a;
+    }
+    else if ((holds_alternation<double>(a) && holds_alternation<int>(b)) || (holds_alternation<double>(a) && holds_alternation<int>(b))) {
+        double val1 = get<double>(a), val2 = get<int>(b);
+        return val1 > val2 ? a : b;
+    }
+    else if ((holds_alternation<double>(a) && holds_alternation<string>(b)) || (holds_alternation<double>(a) && holds_alternation<string>(b))) {
+        double val1 = get<double>(a), strB = get<string>(b);
+        return stod(strB) > val1 ? b : a;
+    }
+    else if ((holds_alternation<float>(a) && holds_alternation<int>(b)) || (holds_alternation<float>(a) && holds_alternation<int>(b))) {
+        float val1 = get<float>(a), val2 = get<int>(b);
+        return val1 > val2 ? a : b;
+    }
+    else if ((holds_alternation<float>(a) && holds_alternation<string>(b)) || (holds_alternation<float>(a) && holds_alternation<string>(b))) {
+        float val1 = get<float>(a), strB = get<string>(b);
+        return stof(strB) > val1 ? b : a;
+    }
+    
+    // handle any other type of variants
+    else {
+        return a;  // or any other logic you need
     }
 }
 
 int main() {
-    assert(compare_one(string("1"), 1) == string("None"));
+    // test the function
+    cout << boost::any_cast<string>(compare_one(string("1"), 1)) << endl;
     return 0;
 }
