040ebf63405b4ee8ec1fcbde9f33b4236ecfc491
beep boop <githubbot@vadim.me>
2024-07-09 18:04:35 +0200

    Solution 37 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/gi93kjk7 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,19 +1,26 @@
-int startHeight;
-int firstBounce;
-int numBounces;
+```cpp
+#include <iostream>
+#include <sstream>
 
-std::cin >> startHeight; 
-std::cin >> firstBounce;
-std::cin >> numBounces;
+int main() {
+    std::string input;
+    std::getline(std::cin, input);
+    int startHeight = std::stoi(input.substr(0, input.find(' ')));
+    double firstBounce = std::stod(input.substr(input.find(' ') + 1, input.find('\n') - input.find(' ') - 1));
+    int numBounces = std::stoi(input.substr(input.find('\n') + 1));
 
-double bouncinessIndex = static_cast<double>(firstBounce) / startHeight;
+    startHeight = static_cast<double>(startHeight);
+    firstBounce = static_cast<double>(firstBounce);
 
-double totalDistance = 2.0 * (static_cast<double>(firstBounce) + startHeight); // initial bounce
-for (int i = 1; i < numBounces; ++i) {
-    totalDistance += 2.0 * startHeight; 
-    startHeight *= bouncinessIndex; 
+    double bouncinessIndex = firstBounce / startHeight;
+
+    double totalDistance = 2.0 * (firstBounce + startHeight); // initial bounce
+    for (int i = 1; i < numBounces; ++i) {
+        totalDistance += 2.0 * startHeight; 
+        startHeight *= bouncinessIndex; 
+    }
+
+    std::cout << "The total distance traveled by the ball is: " << static_cast<int>(totalDistance) << std::endl;
+
+    return 0;
 }
-
-std::cout << "The total distance traveled by the ball is: " << static_cast<int>(totalDistance) << std::endl;
-
-return 0;
