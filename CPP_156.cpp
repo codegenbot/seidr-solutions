fd5a05414624d1bd95f8a78c8e749f44e3ff046b
beep boop <githubbot@vadim.me>
2024-07-09 05:35:08 +0200

    Solution 63 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/urrbvh7e Prompt: b46f23ff-0e39-4b19-967f-ddfd2b9c9552.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int num) {       | ^~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,64 +1,59 @@
 #include <string>
 std::string int_to_mini_roman(int num) {
-    string result = "";
-    while (num > 0) {
-        if (num >= 1000) {
-            result += "M";
-            num -= 1000;
-        } else if (num >= 900) {
-            result += "CM";
-            num -= 900;
-        } else if (num >= 500) {
-            result += "D";
-            num -= 500;
-        } else if (num >= 400) {
-            result += "CD";
-            num -= 400;
-        } else if (num >= 100) {
-            if (num == 100) {
-                result += "C";
-                break;
-            }
-            for (int i = 0; i < num / 100; i++) {
-                result += "C";
-            }
-            num %= 100;
-        } else if (num >= 90) {
-            result += "XC";
-            num -= 90;
-        } else if (num >= 50) {
-            if (num == 50) {
-                result += "L";
-                break;
-            }
-            for (int i = 0; i < num / 50; i++) {
-                result += "L";
-            }
-            num %= 50;
-        } else if (num >= 40) {
-            result += "XL";
-            num -= 40;
-        } else if (num >= 10) {
-            for (int i = 0; i < num / 10; i++) {
-                result += "X";
-            }
-            num %= 10;
-        } else if (num >= 9) {
-            result += "IX";
-            num -= 9;
-        } else if (num >= 5) {
-            for (int i = 0; i < num / 5; i++) {
-                result += "V";
-            }
-            num %= 5;
-        } else if (num >= 4) {
-            result += "IV";
-            num -= 4;
-        } else {
-            for (int i = 0; i < num; i++) {
-                result += "I";
-            }
+    std::string roman = "";
+    if (num >= 1000) {
+        for (int i = 0; i < num / 1000; i++) {
+            roman += "M";
+        }
+        num %= 1000;
+    }
+    if (num >= 900) {
+        roman += "CM";
+        num -= 900;
+    } else if (num >= 500) {
+        for (int i = 0; i < num / 500; i++) {
+            roman += "D";
+        }
+        num %= 500;
+    } else if (num >= 400) {
+        roman += "CD";
+        num -= 400;
+    } else if (num >= 100) {
+        for (int i = 0; i < num / 100; i++) {
+            roman += "C";
+        }
+        num %= 100;
+    } else if (num >= 90) {
+        roman += "XC";
+        num -= 90;
+    } else if (num >= 50) {
+        for (int i = 0; i < num / 50; i++) {
+            roman += "L";
+        }
+        num %= 50;
+    } else if (num >= 40) {
+        roman += "XL";
+        num -= 40;
+    } else if (num >= 10) {
+        for (int i = 0; i < num / 10; i++) {
+            roman += "X";
+        }
+        num %= 10;
+    } else if (num >= 9) {
+        roman += "IX";
+        num -= 9;
+    } else if (num >= 5) {
+        for (int i = 0; i < num / 5; i++) {
+            roman += "V";
+        }
+        num %= 5;
+    } else if (num >= 4) {
+        roman += "IV";
+        num -= 4;
+    } else {
+        for (int i = 0; i < num; i++) {
+            roman += "I";
         }
     }
-    return result;
+    return roman;
 }
