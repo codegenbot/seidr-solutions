2ca670c6495fd0892eec1d2befbe2a0864d37db1
beep boop <githubbot@vadim.me>
2024-07-09 07:03:44 +0200

    Solution 62 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/50q1gn5t Prompt: f9492125-904a-4a1d-9c28-900a65aaa355.cpp:1:1: error: ‘boost’ does not name a typ e     1 | boost::any compare_one(boost::any a, boost::any b) {       | ^~~~~ In file included from f9492125-904a-4a1d-9c28-900a65aaa355.cpp:36: f9492125-904a-4a1d-9c28-900a65aaa355.cpp: In function ‘int main()’: f9492125-904a-4a1d-9c28-900a65aaa355.cpp:38:13: error: ‘boost’ has not been decl ared    38 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ f9492125-904a-4a1d-9c28-900a65aaa355.cpp:38:29: error: ‘string’ was not declared  in this scope    38 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ f9492125-904a-4a1d-9c28-900a65aaa355.cpp:38:37: error: ‘compare_one’ was not dec lared in this scope    38 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <boost/any.hpp>
 
 using namespace boost;
@@ -6,34 +5,34 @@ using namespace boost;
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
         return any_cast<int>(a) > any_cast<int>(b)
-            ? any(a)
-            : any(b);
+            ? a
+            : b;
     } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
         return any_cast<float>(a) > any_cast<float>(b)
-            ? any(a)
-            : any(b);
+            ? a
+            : b;
     } else if (a.type() == typeid(int) && b.type() == typeid(float)) {
         return any_cast<float>(b) > any_cast<int>(a)
-            ? any(b)
-            : any(any_cast<int>(a));
+            ? b
+            : any("None");
     } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
         return any_cast<int>(a) > any_cast<float>(b)
-            ? any(a)
+            ? a
             : any("None");
     } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
         string a_str = any_cast<string>(a);
         string b_str = any_cast<string>(b);
         return stof(b_str) > stof(a_str)
-            ? any(a)
-            : any(b);
+            ? a
+            : b;
     } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
         return stof(any_cast<string>(b)) > any_cast<float>(a)
-            ? any(b)
+            ? b
             : any("None");
     } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
         return any_cast<float>(a) > stof(any_cast<string>(b))
-            ? any(a)
+            ? a
             : any("None");
     }
-    return "None";
+    return any("None");
 }
