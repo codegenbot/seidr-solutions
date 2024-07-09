0a0d2366f2c7b6157e1b43ea77522bfe2385684e
beep boop <githubbot@vadim.me>
2024-07-10 01:24:29 +0200

    Solution 25 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/eycidsiw Prompt: d5d25750-fa92-47fb-af43-6795f2151231.cpp: In function ‘int main()’: d5d25750-fa92-47fb-af43-6795f2151231.cpp:12:22: error: ‘setprecision’ was not de clared in this scope    12 |     cout << fixed << setprecision(4) << bouncinessIndex << endl;       |                      ^~~~~~~~~~~~ d5d25750-fa92-47fb-af43-6795f2151231.cpp:16:26: error: ‘pow’ was not declared in  this scope    16 |         totalDistance += pow(bouncinessIndex, i);       |                          ^~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -9,13 +9,13 @@ int main() {
 
     cin >> startHeight >> firstBounce >> numBounces;
 
-    bouncinessIndex = firstBounce / startHeight;
+    bouncinessIndex = (double)firstBounce / startHeight;
 
     cout << fixed << setprecision(4) << bouncinessIndex << endl;
 
     double totalDistance = 0.0;
     for (int i = 1; i <= numBounces; ++i) {
-        totalDistance += pow(bouncinessIndex, i);
+        totalDistance += pow((double)bouncinessIndex, i);
     }
 
     cout << fixed << setprecision(4) << totalDistance + startHeight << endl;
