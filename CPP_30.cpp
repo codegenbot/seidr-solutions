0d54c466a7cb4a44cec2bc559fa3b0425ad8432d
beep boop <githubbot@vadim.me>
2024-07-09 21:09:40 +0200

    Solution 47 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/1921hktp Prompt: In file included from /usr/include/c++/11/vector:67,                  from ab43515b-11f7-4ea0-b3c2-513a71913fea.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from ab43515b-11f7-4ea0-b3c2-513a71913fea.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ab43515b-11f7-4ea0-b3c2-513a71913fea.cpp:26:36: error: expected primary-expressi on before ‘float’    26 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from ab43515b-11f7-4ea0-b3c2-513a71913fea.cpp:25: ab43515b-11f7-4ea0-b3c2-513a71913fea.cpp: In function ‘int main()’: ab43515b-11f7-4ea0-b3c2-513a71913fea.cpp:35:20: error: ‘get_positive’ was not de clared in this scope    35 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ ab43515b-11f7-4ea0-b3c2-513a71913fea.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cmath>
+#include <iostream>
 
 bool issame(std::vector<float> a, std::vector<float> b) {
     if (a.size() != b.size()) {
@@ -13,33 +14,16 @@ bool issame(std::vector<float> a, std::vector<float> b) {
     return true;
 }
 
-bool get_positive(std::vector<float> l, std::vector<float>& result) {
+std::vector<float> get_positive(std::vector<float> l){
+    std::vector<float> result;
     for (float num : l) {
         if (num > 0) {
             result.push_back(num);
         }
     }
-    return true;
-}
+    return result;
+
 int main() {
-    std::vector<float> vec1, vec2;
-    int n; std::cin >> n;
-    for(int i = 0; i < n; i++) {
-        float num; std::cin >> num;
-        vec1.push_back(num);
-    }
-    int m; std::cin >> m;
-    for(int j = 0; j < m; j++) {
-        float num; std::cin >> num;
-        vec2.push_back(num);
-    }
-    if (issame(vec1, vec2)) {
-        std::vector<float> result;
-        get_positive(vec1, result);
-        for(float f : result) std::cout << f << " ";
-        std::cout << '\n';
-    } else {
-        std::cout << "Vectors are not the same" << '\n';
-    }
+    std::cout << issame(get_positive({}), {}) << std::endl;
     return 0;
 }
