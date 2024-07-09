2fe372568b38d140416eb89fc5147b6db1d32a18
beep boop <githubbot@vadim.me>
2024-07-09 09:28:37 +0200

    Solution 59 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/y9p8yv78 Prompt: 4fd3955b-f180-42e5-9b24-7422b792c2bf.cpp:4:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     4 | string solveBoolean(string s) {       | ^~~~~~       | stdin

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,11 +1,20 @@
 using namespace std;
 
 string solveBoolean(string s) {
-    int res = s[0] == 'T';
-    for(int i=1;i<s.length();i++){
-        if(s[i]=='|') res|=s[i+1]=='T';
-        else if(s[i]&1) res^=(bool)s[i+1]-'F';
-        i+=s[i]=='F'? 0:1;
+    stack<char> st;
+    bool temp = false;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'T' || s[i] == 'F') {
+            while (!st.empty()) st.pop();
+            st.push(s[i]);
+        } else if (s[i] == '|') {
+            temp = (st.top() == 'T');
+            st.pop();
+        } else if (s[i] == '&') {
+            while (!st.empty() && st.top() != '&') st.pop();
+            if (st.empty()) return "False";
+            st.pop();
+        }
     }
-    return res?"True":"False";
+    return temp ? "True" : "False";
 }
