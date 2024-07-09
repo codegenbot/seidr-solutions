a4f2909adc5b5f1d1c1f1805ad542063b48a09c4
beep boop <githubbot@vadim.me>
2024-07-09 18:22:47 +0200

    Solution 30 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/symxc0fq Prompt:    51 | bool issame(vector<float> a,vector<float>b){       |                                           ^ b9d57afb-fc10-4d55-9a43-5183005e35ba.cpp:59:5: error: redefinition of ‘int main( )’    59 | int main(){       |     ^~~~ b9d57afb-fc10-4d55-9a43-5183005e35ba.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from b9d57afb-fc10-4d55-9a43-5183005e35ba.cpp:50: b9d57afb-fc10-4d55-9a43-5183005e35ba.cpp: In function ‘int main()’: b9d57afb-fc10-4d55-9a43-5183005e35ba.cpp:60:20: error: ‘sort_even’ was not decla red in this scope    60 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ b9d57afb-fc10-4d55-9a43-5183005e35ba.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,9 +1,7 @@
-```
 #include <algorithm>
 #include <vector>
-#include <iostream>
 
-bool same(vector<float> a, vector<float> b) {
+bool isSame(vector<float> a, vector<float> b) {
     if(a.size() != b.size()) return false;
     for(int i = 0; i < a.size(); i++) {
         if(a[i] != b[i]) return false;
@@ -11,7 +9,7 @@ bool same(vector<float> a, vector<float> b) {
     return true;
 }
 
-vector<float> sort_even(vector<float> l) {
+vector<float> sortEven(vector<float> l) {
     vector<float> result(l.size());
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
@@ -32,14 +30,19 @@ vector<float> sort_even(vector<float> l) {
 
 int main() {
     vector<float> input({5, 8, -12, 4, 23, 2, 3, 11, 12, -10});
-    vector<float> expected({-12, 8, 3, 4, 5, 2, 12, 11, 23, -10});
-
-    if(!same(sort_even(input), expected)) {
-        return 1;
+    
+    for (float num : input) {
+        cout << num << " ";
     }
+    cout << endl;
 
-    for (float num : sort_even(input)) {
-        std::cout << num << " ";
+    vector<float> result = sortEven(input);
+    if (isSame(result, {5, 8, -12, 4, 23, 2, 3, 11, 12, -10})) {
+        for (float num : result) {
+            cout << num << " ";
+        }
+        cout << endl;
+    } else {
+        cout << "Test failed" << endl;
     }
-    std::cout << endl;
 }
