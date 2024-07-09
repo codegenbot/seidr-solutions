a8caa0f19b4c1f1996760ce3a5ba0f63ec79e916
beep boop <githubbot@vadim.me>
2024-07-09 02:50:09 +0200

    Solution 44 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h3blrigf Prompt: his scope    41 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2a987472-f12a-420f-a198-1d2760cf450a.cpp:41:45: error: expression list treated a s compound expression in initializer [-fpermissive]    41 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 2a987472-f12a-420f-a198-1d2760cf450a.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 2a987472-f12a-420f-a198-1d2760cf450a.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 2a987472-f12a-420f-a198-1d2760cf450a.cpp:40: 2a987472-f12a-420f-a198-1d2760cf450a.cpp: In function ‘int main()’: 2a987472-f12a-420f-a198-1d2760cf450a.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -2,14 +2,17 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    return std::equal(a.begin(), a.end(), b.begin());
-}
-
 std::vector<std::string> sorted_list_sum(std::vector<std::string> lst) {
     auto it = std::remove_if(lst.begin(), lst.end(), [](const std::string& s){ return s.length() % 2; });
     lst.erase(it, lst.end());
-    std::sort(lst.begin(), lst.end());
+    std::sort(lst.begin(), lst.end(), 
+        [](const std::string& a, const std::string& b){
+            if(a.length() != b.length()) {
+                return a.length() < b.length();
+            } else {
+                return a < b;
+            }
+        });
     return lst;
 }
 
@@ -30,8 +33,6 @@ int main() {
         cout << str << " ";
     }
     cout << endl;
-    
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc","dd","aaaa","bbbb"}));
     
     return 0;
 }
