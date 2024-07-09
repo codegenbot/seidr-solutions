32013f12194b8a192aa8e619b6f03bdfc972fb17
beep boop <githubbot@vadim.me>
2024-07-09 12:00:07 +0200

    Solution 24 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/bbfhommv Prompt:       |                              ^~~~~~ 5650f6aa-461a-4be9-a104-0017451c937b.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5650f6aa-461a-4be9-a104-0017451c937b.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5650f6aa-461a-4be9-a104-0017451c937b.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 5650f6aa-461a-4be9-a104-0017451c937b.cpp:25: 5650f6aa-461a-4be9-a104-0017451c937b.cpp: In function ‘int main()’: 5650f6aa-461a-4be9-a104-0017451c937b.cpp:35:20: error: ‘bf’ was not declared in this scope    35 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 5650f6aa-461a-4be9-a104-0017451c937b.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cassert>
 #include <string>
+#include <cassert>
 
 using namespace std;
-
-bool issame(vector<string> a, vector<string> b){
-    return a == b;
-}
 
 vector<string> bf(string planet1, string planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
@@ -34,7 +30,22 @@ vector<string> bf(string planet1, string planet2) {
     return result;
 }
 
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main() {
     assert(issame(bf("Jupiter", "Makemake"), {}));
+    
     return 0;
 }
