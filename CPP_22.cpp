84223feff8ad92974cfa2ef4bcfdd669e927e70f
beep boop <githubbot@vadim.me>
2024-07-09 23:05:32 +0200

    Solution 52 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9b7agl3b Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d1bbd010-ab03-4dae-af05-b6143a4c86e5.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d1bbd010-ab03-4dae-af05-b6143a4c86e5.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d1bbd010-ab03-4dae-af05-b6143a4c86e5.cpp:11: d1bbd010-ab03-4dae-af05-b6143a4c86e5.cpp: In function ‘int main()’: d1bbd010-ab03-4dae-af05-b6143a4c86e5.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ d1bbd010-ab03-4dae-af05-b6143a4c86e5.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,39 +1,8 @@
-#include <boost/any.hpp>
-#include <vector>
-#include <list>
-#include <algorithm>
-
-namespace boost { namespace mpl {
-template <>
-struct is_same<int> : std::true_type {};
-}
-
-using namespace boost;
-
-bool is_same(const any& a, const any& b) {
-    return get<mpl::is_same>(a.type()) && get<mpl::is_same>(b.type());
-}
-
-vector<any> list_any() {
-    vector<any> result;
-    for (int i = 0; i < 6; ++i) {
-        if (i == 1 || i == 2) {
-            result.push_back(any('a'));
-        } else if (i == 3 || i == 4) {
-            result.push_back(any(3));
-        } else {
-            result.push_back(any('b'));
-        }
-    }
-    return result;
-}
-
 vector<int> filter_integers(list<any> values) {
     vector<int> result;
     for (const auto& value : values) {
-        if (is_same(value, any(0))) {
+        if (boost::any_cast<int>(value)) {
             result.push_back(boost::any_cast<int>(value));
         }
     }
     return result;
-}
