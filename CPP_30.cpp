424cdbba40cb1f9d0dd5d1a4ad78e5622ee7f337
beep boop <githubbot@vadim.me>
2024-07-09 03:32:35 +0200

    Solution 40 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jvy931jd Prompt: In file included from /usr/include/c++/11/vector:67,                  from 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp:13: 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp: In function ‘int main()’: 4d6101e9-cea0-4a6d-9a11-af9e322cbcdb.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,6 +1,10 @@
 #include <vector>
 #include <algorithm>
 
+bool issame(std::vector<float> a, std::vector<float> b) {
+    return a == b;
+}
+
 std::vector<float> get_positive(std::vector<float> l){
     std::vector<float> result;
     for(float num : l) {
@@ -11,11 +15,23 @@ std::vector<float> get_positive(std::vector<float> l){
     return result;
 }
 
-bool same(vector<float> a,vector<float>b){
-    return std::equal(a.begin(),a.end(),b.begin());
-}
-
 int main() {
-    assert(same(get_positive({}), {} ));
+    assert(get_positive({}) == {});
+    std::vector<float> a = {1.0, -2.0, 3.0};
+    std::vector<float> b = {-4.0, 5.0, -6.0};
+    if (!issame(get_positive(a), get_positive(b))) {
+        for (float num : a) {
+            if (num > 0 && !std::find(std::begin(b), std::end(b), num) == std::end(b)) {
+                printf("Expected %f in vector b\n", num);
+                return 1;
+            }
+        }
+        for (float num : b) {
+            if (num > 0 && !std::find(std::begin(a), std::end(a), num) == std::end(a)) {
+                printf("Expected %f in vector a\n", num);
+                return 1;
+            }
+        }
+    }
     return 0;
 }
