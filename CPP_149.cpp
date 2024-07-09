1cc63db3092eb89d86fcd671f054781cea1eafa5
beep boop <githubbot@vadim.me>
2024-07-09 13:05:06 +0200

    Solution 88 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/p70nshd0 Prompt:    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:16: f8e2b31f-ea39-4798-9646-bf202464bab2.cpp: In function ‘int main()’: f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:26:20: error: ‘sorted_list_sum’ was not  declared in this scope    26 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,14 +1,15 @@
 vector<string> sorted_list_sum(vector<string> lst) {
     auto issame = [](const string& a, const string& b) {
-        if (a.length() == b.length())
-            return a < b;
-        else
-            return a.length() < b.length();
+        return a.length() % 2 && !b.length() % 2 || (!a.length() % 2 && b.length() % 2);
     };
-    auto it = unique(lst.begin(), lst.end(),
-                      [&issame](const string& a, const string& b)
-                      { return a.length() % 2 && !b.length() % 2 || (!a.length() % 2 && b.length() % 2); });
+    auto it = unique(lst.begin(), lst.end(), issame);
     lst.erase(it, lst.end());
-    sort(lst.begin(), lst.end(), issame);
+    sort(lst.begin(), lst.end(),
+         [](const string& a, const string& b) {
+             if (a.length() == b.length())
+                 return a < b;
+             else
+                 return a.length() < b.length();
+         });
     return lst;
 }
