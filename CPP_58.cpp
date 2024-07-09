11e5d054a86b67a2eb7c2ffd86bf94c480064626
beep boop <githubbot@vadim.me>
2024-07-09 21:24:34 +0200

    Solution 28 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g34sr5ik Prompt:  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 63c680cc-b3ae-46b0-89f8-2878b1694a35.cpp:17:27: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? 63c680cc-b3ae-46b0-89f8-2878b1694a35.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 63c680cc-b3ae-46b0-89f8-2878b1694a35.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 63c680cc-b3ae-46b0-89f8-2878b1694a35.cpp:16: 63c680cc-b3ae-46b0-89f8-2878b1694a35.cpp: In function ‘int main()’: 63c680cc-b3ae-46b0-89f8-2878b1694a35.cpp:26:20: error: ‘common’ was not declared  in this scope    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 63c680cc-b3ae-46b0-89f8-2878b1694a35.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,47 +1,10 @@
-```cpp
-#include <iostream>
 #include <vector>
-#include <set>
+using namespace std;
 
-bool issame(vector<int> a, vector<int> b){
-    return (a == b);
-}
-
-vector<int> common(vector<int> l1, vector<int> l2) {
-    set<int> s1(l1.begin(), l1.end());
-    set<int> s2(l2.begin(), l2.end());
-    set<int> intersection;
-    
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-        inserter(intersection, intersection.begin()));
-    
-    vector<int> result(intersection.begin(), intersection.end());
-    sort(result.begin(), result.end());
-    return result;
+bool issame(vector<int> a,vector<int>b){
+    return a==b;
 }
 
 int main() {
-    int n1, m1, n2, m2;
-    std::cout << "Enter number of elements in first array: ";
-    std::cin >> n1;
-    std::cout << "Enter elements of first array: ";
-    vector<int> l1(n1);
-    for (int i = 0; i < n1; ++i) {
-        cin >> l1[i];
-    }
-    
-    std::cout << "Enter number of elements in second array: ";
-    std::cin >> m2;
-    std::cout << "Enter elements of second array: ";
-    vector<int> l2(m2);
-    for (int i = 0; i < m2; ++i) {
-        cin >> l2[i];
-    }
-    
-    vector<int> result = common(l1, l2);
-    cout << "Common elements are: ";
-    for(int i=0; i<result.size(); i++) {
-        cout << result[i] << " ";
-    }
-    return 0;
+    // Rest of your code...
 }
