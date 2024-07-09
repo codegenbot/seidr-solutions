18cbeec3b3326a4a9942abd5158d9af71080ffc9
beep boop <githubbot@vadim.me>
2024-07-09 15:12:23 +0200

    Solution 7 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4eb0byil Prompt:       |         ^~~ dcaebbde-59cc-47c2-8ff7-0aadc9574752.cpp:7:9: error: expected primary-expression  before ‘int’     7 |     set<int> s2(v2.begin(), v2.end());       |         ^~~ dcaebbde-59cc-47c2-8ff7-0aadc9574752.cpp:9:9: error: expected primary-expression  before ‘int’     9 |     set<int> common_result;       |         ^~~ dcaebbde-59cc-47c2-8ff7-0aadc9574752.cpp:10:22: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    10 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                      ^~       |                      l1 dcaebbde-59cc-47c2-8ff7-0aadc9574752.cpp:10:44: error: ‘s2’ was not declared in this scope; did you mean ‘v2’?    10 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                                            ^~       |                                            v2 dcaebbde-59cc-47c2-8ff7-0aadc9574752.cpp:11:18: error: ‘common_result’ was not d eclared in this scope    11 |         inserter(common_result, common_result.begin()));       |                  ^~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -3,13 +3,17 @@
 using namespace std;
 
 vector<int> common(vector<int> l1, vector<int> v2) {
-    set<int> common_result;
-    set<int> set_s1(l1.begin(), l1.end());
-    set<int> set_s2(v2.begin(), v2.end());
+    set<int> s1(l1.begin(), l1.end());
+    set<int> s2(v2.begin(), v2.end());
 
-    set_intersection(set_s1.begin(), set_s1.end(), set_s2.begin(), set_s2.end(),
+    set<int> common_result;  // declare before using
+    set<int> result;
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
         inserter(common_result, common_result.begin()));
 
-    vector<int> result(common_result.begin(), common_result.end());
-    return result;
+    vector<int> result_vector;
+    for (int x : common_result) {
+        result_vector.push_back(x);
+    }
+    return result_vector;
 }
