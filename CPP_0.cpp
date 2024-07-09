f584828d0cf42a4bb6df209a883c890d4b775c91
beep boop <githubbot@vadim.me>
2024-07-09 18:11:28 +0200

    Solution 17 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0r6o7f3v Prompt: w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const float*; _Tp = float; _Alloc = s td::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = f loat; _Alloc = std::allocator<float>; std::vector<_Tp, _Alloc>::allocator_type =  std::allocator<float>]’ 307aa172-529d-4444-9a45-a5e2bbcb7b76.cpp:17:55:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <cmath>
 
-bool has_close_elements(std::vector<float> numbers, float threshold){
+bool has_close_elements(std::vector<float>& numbers, float threshold){
     for(int i = 0; i < numbers.size() - 1; i++){
         for(int j = i + 1; j < numbers.size(); j++){
             if(std::abs(numbers[i] - numbers[j]) <= threshold)
@@ -14,12 +14,7 @@ bool has_close_elements(std::vector<float> numbers, float threshold){
 
 int main(){
     std::vector<float> a;
-    a.push_back(1.0); 
-    a.push_back(2.0);
-    a.push_back(3.9);
-    a.push_back(4.0);
-    a.push_back(5.0);
-    a.push_back(2.2);
-    assert(has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false);
+    a.push_back(1.0); a.push_back(2.0); a.push_back(3.9); a.push_back(4.0); a.push_back(5.0); a.push_back(2.2);
+    assert(has_close_elements(a, 0.5) == false);
     return 0;
 }
