6adf79e9f2fbafd6a2610fae9a8ec403b71fdda1
beep boop <githubbot@vadim.me>
2024-07-09 11:37:26 +0200

    Solution 64 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/49z9tjo2 Prompt: this scope    24 |     cin >> n;       |     ^~~ cdbccaf2-3660-4739-85d0-9a3437ae0b8b.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; cdbccaf2-3660-4739-85d0-9a3437ae0b8b.cpp:30:5: error: ‘cout’ was not declared in  this scope    30 |     cout << "1 ";       |     ^~~~ cdbccaf2-3660-4739-85d0-9a3437ae0b8b.cpp:30:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? cdbccaf2-3660-4739-85d0-9a3437ae0b8b.cpp:34:13: error: ‘endl’ was not declared i n this scope    34 |     cout << endl;       |             ^~~~ cdbccaf2-3660-4739-85d0-9a3437ae0b8b.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,24 +1,6 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 using namespace std;
-
-pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
-    int minDiff = INT_MAX;
-    int splitIndex = 0;
-    
-    for (int i = 1; i < vec.size(); i++) {
-        int diff = abs(vec[i] - vec[i-1]);
-        if (diff < minDiff) {
-            minDiff = diff;
-            splitIndex = i;
-        }
-    }
-    
-    vector<int> leftVec = vector<int>(vec.begin(), vec.begin() + splitIndex);
-    vector<int> rightVec = vector<int>(vec.begin() + splitIndex, vec.end());
-    
-    return {leftVec, rightVec};
-}
 
 pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
     int minDiff = INT_MAX;
@@ -50,6 +32,6 @@ int main() {
     for (int num : result.second) {
         cout << num << " ";
     }
-    cout << endl;
+    cout << std::endl;
     return 0;
 }
