dc59b36d16632bc9a7cf3a4109d7572d0bdf156a
beep boop <githubbot@vadim.me>
2024-07-09 21:31:04 +0200

    Solution 91 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/owmm65gu Prompt: 1cbf7215-3d26-4dba-bae0-cafd8aeb5f53.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 1cbf7215-3d26-4dba-bae0-cafd8aeb5f53.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 1cbf7215-3d26-4dba-bae0-cafd8aeb5f53.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 1cbf7215-3d26-4dba-bae0-cafd8aeb5f53.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ In file included from 1cbf7215-3d26-4dba-bae0-cafd8aeb5f53.cpp:11: 1cbf7215-3d26-4dba-bae0-cafd8aeb5f53.cpp: In function ‘int main()’: 1cbf7215-3d26-4dba-bae0-cafd8aeb5f53.cpp:13:13: error: ‘int_to_mini_romank’ was not declared in this scope    13 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,9 +1,9 @@
-```
+```cpp
 const std::string roman(int n) {
     if (n >= 1000) return "M" + roman(n - 1000);
     else if (n >= 900) return "CM";
     else if (n >= 500) return "D" + roman(n - 500);
     else if (n >= 400) return "CD";
-    else if (n >= 100) return n >= 90 ? "CM" : n >= 50 ? "L" : n >= 40 ? "XL" : n >= 10 ? (n >= 9 ? "IX" : (n >= 5 ? "V" : (n >= 4 ? "IV" : "I"))) + roman(n - ((n >= 90) || (n >= 50) ? 50 : 10));
+    else if (n >= 100) return n >= 90 ? "XC" : n >= 50 ? "L" : n >= 40 ? "XL" : n >= 10 ? (n >= 9 ? "IX" : (n >= 5 ? "V" : (n >= 4 ? "IV" : "I"))) + roman(n - ((n >= 90) || (n >= 50) ? 50 : 10));
     else return "";
 }
