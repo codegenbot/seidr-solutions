65ee5da10830c2fc46582703cc63e9bead78826e
beep boop <githubbot@vadim.me>
2024-07-10 01:28:46 +0200

    Solution 7 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/6u8y8ika Prompt: In file included from /usr/include/c++/11/vector:67,                  from dde1b056-af28-4ec9-8279-01866d71b4b4.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from dde1b056-af28-4ec9-8279-01866d71b4b4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ dde1b056-af28-4ec9-8279-01866d71b4b4.cpp:34:36: error: expected primary-expressi on before ‘float’    34 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ dde1b056-af28-4ec9-8279-01866d71b4b4.cpp:34:43: error: expression list treated a s compound expression in initializer [-fpermissive]    34 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from dde1b056-af28-4ec9-8279-01866d71b4b4.cpp:33: dde1b056-af28-4ec9-8279-01866d71b4b4.cpp: In function ‘int main()’: dde1b056-af28-4ec9-8279-01866d71b4b4.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,19 +1,19 @@
 #include <vector>
 #include <assert.h>
 
-bool issame(std::initializer_list<float> a, std::initializer_list<float> b) {
+bool issame(std::vector<float> a, std::vector<float> b) {
     if (a.size() != b.size()) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
-        if (a.begin()[i] != b.begin()[i]) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
     return true;
 }
 
-std::vector<float> get_positive(std::initializer_list<float> l) {
+std::vector<float> get_positive(std::vector<float> l) {
     std::vector<float> result;
     for (float num : l) {
         if (num > 0) {
@@ -21,4 +21,10 @@ std::vector<float> get_positive(std::initializer_list<float> l) {
         }
     }
     return result;
+
+}
+
+int mainFunc() {
+    assert(issame(get_positive({}), {}) == true); 
+    return 0;
 }
