b605eb8a432d8dd481db8272b9682389184f0c31
beep boop <githubbot@vadim.me>
2024-07-09 21:23:56 +0200

    Solution 63 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/juk3nsjv Prompt: 9ab3a77a-fe35-4749-b069-c3406a988097.cpp:10:13: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |             ^~~~~~ 9ab3a77a-fe35-4749-b069-c3406a988097.cpp:10:20: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                    ^~~~~ 9ab3a77a-fe35-4749-b069-c3406a988097.cpp:10:29: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 9ab3a77a-fe35-4749-b069-c3406a988097.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from 9ab3a77a-fe35-4749-b069-c3406a988097.cpp:9: 9ab3a77a-fe35-4749-b069-c3406a988097.cpp: In function ‘int main()’: 9ab3a77a-fe35-4749-b069-c3406a988097.cpp:19:20: error: ‘sort_even’ was not decla red in this scope    19 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,8 +1,8 @@
 #include <vector>
 
-bool issame(const vector<float>& a, const vector<float>& b){
+bool issame(const std::vector<float>& a, const std::vector<float>& b){
     if(a.size() != b.size())return false;
     for(int i = 0; i < a.size(); i++){
-        if(abs(a[i] - b[i]) > 1e-9)return false;
+        if(std::abs(a[i] - b[i]) > 1e-9)return false;
     }
     return true;
