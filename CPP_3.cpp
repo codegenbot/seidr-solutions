87a7ff148cd4b7b69bd372d6aa65b240dad82bc6
beep boop <githubbot@vadim.me>
2024-07-09 11:35:01 +0200

    Solution 5 of CPP/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/bsrzxj5v Prompt: dde23a69-bb7c-4772-a72f-7a0b29943dc3.cpp:1:22: error: ‘vector’ is not a member o f ‘std’     1 | bool below_zero(std::vector<int> operations){       |                      ^~~~~~ dde23a69-bb7c-4772-a72f-7a0b29943dc3.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | bool below_zero(std::vector<int> operations){ dde23a69-bb7c-4772-a72f-7a0b29943dc3.cpp:1:29: error: expected primary-expressio n before ‘int’     1 | bool below_zero(std::vector<int> operations){       |                             ^~~

--- CPP_3.cpp
+++ CPP_3.cpp
@@ -1,10 +1,10 @@
-```c++
 #include <vector>
 
-bool below_zero(std::vector<int> operations){
+bool below_zero(std::vector<int> operations) {
     int balance = 0;
-    for(int operation : operations){
+    for(int operation; operation = operations.begin(); operation++){
         balance += operation;
         if(balance < 0) return true;
     }
     return false;
+}
