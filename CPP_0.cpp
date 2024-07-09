5f9e32c3dad31ef7de9d820d8d0d20def7524b05
beep boop <githubbot@vadim.me>
2024-07-09 07:33:23 +0200

    Solution 33 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/i5yuyk8j Prompt: 2f64400f-a750-4950-8ae4-83720d06e737.cpp: In function ‘int main()’: 2f64400f-a750-4950-8ae4-83720d06e737.cpp:18:5: error: ‘assert’ was not declared in this scope    18 |     assert(has_close_elements(numbers, 0.5) == false);       |     ^~~~~~ 2f64400f-a750-4950-8ae4-83720d06e737.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | using namespace std; 2f64400f-a750-4950-8ae4-83720d06e737.cpp: At global scope: 2f64400f-a750-4950-8ae4-83720d06e737.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 2f64400f-a750-4950-8ae4-83720d06e737.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 using namespace std;
 
-bool has_close_elements(float a[], int n, float tol) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (std::abs(a[i] - a[j]) <= tol) {
+bool has_close_elements(vector<float> a, float tol) {
+    for (int i = 0; i < a.size() - 1; i++) {
+        for (int j = i + 1; j < a.size(); j++) {
+            if (abs(a[i] - a[j]) <= tol) {
                 return true;
             }
         }
@@ -15,12 +16,9 @@ bool has_close_elements(float a[], int n, float tol) {
 
 int main() {
     vector<float> numbers = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f, 0};
-    int n = numbers.size();
-    assert(has_close_elements(&numbers[0], n, 0.5) == false);
+    assert(has_close_elements(numbers, 0.5) == false);
     
     vector<float> numbers2 = {1.1f, 2.2f, 3.1f, 4.1f, 5.1f};
-    int m = numbers2.size();
-    assert(has_close_elements(&numbers2[0], m, 0.5) == false);
+    assert(has_close_elements(numbers2, 0.5) == false);
     
     return 0;
-}
