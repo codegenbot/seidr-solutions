7c5aaed2bdf3fc90969b08134011b649384fecb9
beep boop <githubbot@vadim.me>
2024-07-10 01:44:00 +0200

    Solution 12 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ciba3k4d Prompt: ae070e9c-9586-479e-aa2c-3ee37274e95f.cpp:1:24: error: ‘vector’ was not declared in this scope     1 | bool pairs_sum_to_zero(vector<int> l) {       |                        ^~~~~~ ae070e9c-9586-479e-aa2c-3ee37274e95f.cpp:1:31: error: expected primary-expressio n before ‘int’     1 | bool pairs_sum_to_zero(vector<int> l) {       |                               ^~~ In file included from ae070e9c-9586-479e-aa2c-3ee37274e95f.cpp:13: ae070e9c-9586-479e-aa2c-3ee37274e95f.cpp: In function ‘int main()’: ae070e9c-9586-479e-aa2c-3ee37274e95f.cpp:15:30: error: ‘pairs_sum_to_zero’ canno t be used as a function    15 |     assert (pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);       |             ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -2,7 +2,7 @@
 #include <set>
 
 bool pairs_sum_to_zero(vector<int> l) {
-    set<int> s = set<int>(l.begin(), l.end()); 
+    set<int> s(l.begin(), l.end()); 
     for (int i = 0; i < s.size(); i++) { 
         int complement = -s[i];
         auto it = s.find(complement);
