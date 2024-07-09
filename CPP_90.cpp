a6b51a430bf82afab567b700c451a40c16e38346
beep boop <githubbot@vadim.me>
2024-07-09 21:53:04 +0200

    Solution 34 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/4o6n05as Prompt: c955da60-57f1-4c7f-99eb-01ee5ced5601.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int next_smallest(vector<int> lst) {       |                   ^~~~~~ c955da60-57f1-4c7f-99eb-01ee5ced5601.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int next_smallest(vector<int> lst) {       |                          ^~~ In file included from c955da60-57f1-4c7f-99eb-01ee5ced5601.cpp:11: c955da60-57f1-4c7f-99eb-01ee5ced5601.cpp: In function ‘int main()’: c955da60-57f1-4c7f-99eb-01ee5ced5601.cpp:13:26: error: ‘next_smallest’ cannot be  used as a function    13 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,14 +1,15 @@
-```
 using namespace std;
 
 int next_smallest(vector<int> lst) {
-    if (lst.size() < 2) return -1; 
+    if (lst.size() < 2)
+        return -1; 
     vector<int> v(lst);
     sort(v.begin(), v.end());
-    for (int i = 0; i < v.size() - 1; i++) {
-        if (v[i] != v[i + 1]) {
-            for (int j = i + 1; j < v.size(); j++) {
-                if (v[j] > v[i]) return v[i];
+    for (int i = 0; i < v.size()-1; i++) {
+        if (v[i] != v[i+1]) {
+            for (int j = i+1; j<v.size(); j++) {
+                if (v[j] > v[i])
+                    return v[i];
             }
         }
     }
