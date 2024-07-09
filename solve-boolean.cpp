b5786063704971d14690ab8b5b8e6d3b67756c29
beep boop <githubbot@vadim.me>
2024-07-09 21:39:16 +0200

    Solution 16 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/bw1run1h Prompt: nt]’ for array subscript     9 |                 if (s[i] != 'T') {       |                      ^ 8101eed2-c09e-406a-abb1-11e2d62e3092.cpp:15:21: error: invalid types ‘const int[ int]’ for array subscript    15 |         } else if (s[i] == '|') {       |                     ^ 8101eed2-c09e-406a-abb1-11e2d62e3092.cpp:17:26: error: request for member ‘lengt h’ in ‘s’, which is of non-class type ‘const int’    17 |             while (i < s.length() && s[i] != '&') {       |                          ^~~~~~ 8101eed2-c09e-406a-abb1-11e2d62e3092.cpp:17:39: error: invalid types ‘const int[ int]’ for array subscript    17 |             while (i < s.length() && s[i] != '&') {       |                                       ^ 8101eed2-c09e-406a-abb1-11e2d62e3092.cpp:18:22: error: invalid types ‘const int[ int]’ for array subscript    18 |                 if (s[i] != 'T') {       |                      ^ 8101eed2-c09e-406a-abb1-11e2d62e3092.cpp:25:24: error: invalid types ‘const int[ int]’ for array subscript    25 |             result = (s[i] == 'T');       |                        ^

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -2,27 +2,27 @@ bool solveBoolean(const std::string& s) {
     bool result = true;
     int i = 0;
 
-    while (i < s.size()) {
-        if (s[i] == '&') {
+    while (i < s.length()) {
+        if (s.at(i) == '&') {
             i++;
-            while (i < s.size() && s[i] != '|') {
-                if (s[i] != 'T') {
+            while (i < s.length() && s.at(i) != '|') {
+                if (s.at(i) != 'T') {
                     result = false;
                     break;
                 }
                 i++;
             }
-        } else if (s[i] == '|') {
+        } else if (s.at(i) == '|') {
             i++;
-            while (i < s.size() && s[i] != '&') {
-                if (s[i] != 'T') {
+            while (i < s.length() && s.at(i) != '&') {
+                if (s.at(i) != 'T') {
                     result = true;
                     break;
                 }
                 i++;
             }
         } else {
-            result = (s[i] == 'T');
+            result = (s.at(i) == 'T');
             i++;
         }
     }
