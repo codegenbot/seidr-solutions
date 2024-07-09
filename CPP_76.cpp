39407788badf6bb56e3aefefb067b176f9a5b1ef
beep boop <githubbot@vadim.me>
2024-07-09 03:47:52 +0200

    Solution 12 of CPP/76, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/23f0naub Prompt: c040b7aa-46a4-4d12-91ec-49db798fb908.cpp: In function ‘bool is_simple_power(int,  int)’: c040b7aa-46a4-4d12-91ec-49db798fb908.cpp:2:24: error: ‘log’ was not declared in this scope; did you mean ‘long’?     2 |     return pow(n, ceil(log(x)/log(n))) == x;       |                        ^~~       |                        long c040b7aa-46a4-4d12-91ec-49db798fb908.cpp:2:19: error: ‘ceil’ was not declared in  this scope     2 |     return pow(n, ceil(log(x)/log(n))) == x;       |                   ^~~~ c040b7aa-46a4-4d12-91ec-49db798fb908.cpp:2:12: error: ‘pow’ was not declared in this scope     2 |     return pow(n, ceil(log(x)/log(n))) == x;       |            ^~~

--- CPP_76.cpp
+++ CPP_76.cpp
@@ -1,6 +1,4 @@
-Here is the completed code:
-
-bool is_simple_power(int x, int n) {
-    double y = pow(n, log(x)/log(n));
-    return (y == round(y) && y == x);
+#include <cmath>
+bool is_simple_power(int x, int n){
+    return pow(n, ceil(log(x)/log(n))) == x;
 }
