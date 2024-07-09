3e624a384d5a4daa603c239deb0c5140d1682f6f
beep boop <githubbot@vadim.me>
2024-07-09 06:12:22 +0200

    Solution 71 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/5yvq0zcc Prompt: 17c22afe-9024-42ec-a9f7-69380a9cbad4.cpp:1:1: error: stray ‘`’ in program     1 | ```c++       | ^ 17c22afe-9024-42ec-a9f7-69380a9cbad4.cpp:1:2: error: stray ‘`’ in program     1 | ```c++       |  ^ 17c22afe-9024-42ec-a9f7-69380a9cbad4.cpp:1:3: error: stray ‘`’ in program     1 | ```c++       |   ^ 17c22afe-9024-42ec-a9f7-69380a9cbad4.cpp:1:4: error: ‘c’ does not name a type     1 | ```c++       |    ^ 17c22afe-9024-42ec-a9f7-69380a9cbad4.cpp:10:23: error: ‘string’ is not a member of ‘std’    10 | int bowlingScore(std::string s) {       |                       ^~~~~~ 17c22afe-9024-42ec-a9f7-69380a9cbad4.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | ```c++

--- bowling.cpp
+++ bowling.cpp
@@ -19,22 +19,21 @@ int bowlingScore(std::string s) {
             currentFrame++;
         } else if(s[i] == 'X') {
             score += 10;
+            if(currentFrame < 9) {
+                score += 10;
+            }
             currentFrame++;
         } else {
             int roll = s[i] - '0';
             score += roll;
             if(currentFrame < 10) {
-                if(i+1 < s.length() && s[i+1] != '/') {
-                    if(s[i+1] == 'X') {
-                        score += 10;
-                        currentFrame++;
-                    } else {
-                        score += getBonus(roll, (s[i+1] - '0'));
-                        currentFrame++;
-                    }
-                } else {
+                if(i+1 < s.length() && (s[i+1] == '/' || s[i+1] == 'X')) {
                     break;
                 }
+                score += roll;
+                currentFrame++;
+            } else {
+                break;
             }
         }
     }
