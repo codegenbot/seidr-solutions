f248e2e9b2dbb385848b9f5ee992030e8895fd9a
beep boop <githubbot@vadim.me>
2024-07-09 18:20:20 +0200

    Solution 14 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/4evcoe3h Prompt:       |                      ^~~~~~ ca8dcf5c-82e4-4699-b5c4-2163a5d610ce.cpp:3:22: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from ca8dcf5c-82e4-4699-b5c4-2163a5d610ce.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from ca8dcf5c-82e4-4699-b5c4-2163a5d610ce.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ca8dcf5c-82e4-4699-b5c4-2163a5d610ce.cpp:3:29: error: expected primary-expressio n before ‘int’     3 | bool below_threshold(vector<int> l, int t) {       |                             ^~~ ca8dcf5c-82e4-4699-b5c4-2163a5d610ce.cpp:3:37: error: expected primary-expressio n before ‘int’     3 | bool below_threshold(vector<int> l, int t) {       |                                     ^~~ ca8dcf5c-82e4-4699-b5c4-2163a5d610ce.cpp:3:42: error: expression list treated as  compound expression in initializer [-fpermissive]     3 | bool below_threshold(vector<int> l, int t) {       |                                          ^

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,11 +1,10 @@
-```cpp
+```c++
 #include <vector>
 
-bool below_threshold(std::vector<int> l, int t) {
+bool below_threshold(std::vector l, int t) {
     for (int num : l) {
         if (num >= t) {
             return false;
         }
     }
     return true;
-}
