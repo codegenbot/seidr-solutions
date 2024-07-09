ed1eefc9af338ecd832089c4b8ab8d9fb810d9e1
beep boop <githubbot@vadim.me>
2024-07-09 19:34:06 +0200

    Solution 86 of CPP/86, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/n9bn7yqf Prompt:   700 |       operator=(_CharT __c)       |                 ~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h:717:7: note: candidate: ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Traits , _Alloc>::operator=(std::__cxx11::basic_string<_CharT, _Traits, _Alloc>&&) [wit h _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char> ]’ (near match)   717 |       operator=(basic_string&& __str)       |       ^~~~~~~~ /usr/include/c++/11/bits/basic_string.h:717:7: note:   passing ‘const string*’ { aka ‘const std::__cxx11::basic_string<char>*’} as ‘this’ argument discards quali fiers /usr/include/c++/11/bits/basic_string.h:785:7: note: candidate: ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Traits , _Alloc>::operator=(std::initializer_list<_Tp>) [with _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’   785 |       operator=(initializer_list<_CharT> __l)       |       ^~~~~~~~ /usr/include/c++/11/bits/basic_string.h:785:42: note:   no known conversion for argument 1 from ‘std::__cxx11::basic_string<char>’ to ‘std::initializer_list<cha r>’   785 |       operator=(initializer_list<_CharT> __l)       |                 ~~~~~~~~~~~~~~~~~~~~~~~~~^~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,31 +1,53 @@
+#include <iostream>
+#include <vector>
 #include <algorithm>
+#include <string>
+
 using namespace std;
-
-string anti_shuffle(string s) {
-    string result = "";
-    for (const auto& word : split(s, ' ')) {
-        string temp = word;
-        sort(temp.begin(), temp.end());
-        result += temp + " ";
-    }
-    return result.substr(0, result.size() - 1);
-}
-
-string split(const string& str, char sep) {
-    vector<string> tokens;
-    size_t pos = 0;
-    while ((pos = str.find(sep)) != string::npos) {
-        tokens.push_back(str.substr(0, pos));
-        str.erase(0, pos + 1);
-    }
-    tokens.push_back(str);
-    return tokens.size() > 1 ? join(tokens, " ") : tokens[0];
-}
 
 string join(const vector<string>& vec, const string& sep) {
     string result;
     for (const auto& s : vec) {
-        result += s + sep;
+        if (!result.empty()) {
+            result += sep;
+        }
+        result += s;
     }
-    return result.substr(0, result.size() - 1);
+    return result;
+}
+
+void split(string& str, char sep) {  // Changed to 'string&' and removed const
+    vector<string> tokens;
+    size_t pos = 0;
+    while ((pos = str.find(sep)) != string::npos) {
+        tokens.push_back(str.substr(0, pos));
+        str = str.substr(pos + 1);
+    }
+    tokens.push_back(str);
+    return;
+}
+
+string anti_shuffle(string s) {
+    vector<string> words;
+    string word = "";
+    for (const auto& c : s) {
+        if (c == ' ') {
+            words.push_back(word);
+            word = "";
+        } else {
+            word += c;
+        }
+    }
+    words.push_back(word);
+
+    string result = "";
+    for (const auto& word : words) {
+        string w = word;
+        sort(w.begin(), w.end());
+        if (!result.empty()) {
+            result += " ";
+        }
+        result += w;
+    }
+    return result;
 }
