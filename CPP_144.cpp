b15a72ef59534d3b69a921d9afdcf9605e5f867a
beep boop <githubbot@vadim.me>
2024-07-09 10:55:50 +0200

    Solution 31 of CPP/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/289d4dlu Prompt: In file included from 71f6e4e2-2bb5-4861-8991-b0d5575ba2c7.cpp:38: 71f6e4e2-2bb5-4861-8991-b0d5575ba2c7.cpp: In function ‘int main()’: 71f6e4e2-2bb5-4861-8991-b0d5575ba2c7.cpp:40:21: error: too many arguments to fun ction ‘bool simplify(std::string)’    40 |     assert (simplify("1/5", "1/5") == false);       |             ~~~~~~~~^~~~~~~~~~~~~~ 71f6e4e2-2bb5-4861-8991-b0d5575ba2c7.cpp:11:6: note: declared here    11 | bool simplify(std::string x) {       |      ^~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,13 +1,58 @@
 #include <string>
-#include <sstream>
-
 using namespace std;
 
-bool simplify(string x, string n) {
-    int a = 0, b = 0, c = 0, d = 0;
-    stringstream s1(x), s2(n);
-    s1 >> a >> '/' >> b;
-    s2 >> c >> '/' >> d;
-    
-    return (a*d == b*c);
+int gcdFunc(int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcdFunc(b, a % b);
+}
+
+bool simplify(string x1, string x2) {
+    int numerator1 = 0, denominator1 = 1;
+    int i = 0;
+
+    // Calculate the numerator of the first fraction
+    while(i < x1.length()) {
+        if (isdigit(x1[i])) {
+            numerator1 = numerator1 * 10 + (x1[i] - '0');
+        } else if (x1[i] == '/') {
+            break;
+        }
+        i++;
+    }
+
+    // Calculate the denominator of the first fraction
+    i += 1; // Skip '/'
+    while(i < x1.length()) {
+        if (isdigit(x1[i])) {
+            denominator1 = denominator1 * 10 + (x1[i] - '0');
+        } 
+        i++;
+    }
+
+    int numerator2 = 0, denominator2 = 1;
+    i = 0;
+
+    // Calculate the numerator of the second fraction
+    while(i < x2.length()) {
+        if (isdigit(x2[i])) {
+            numerator2 = numerator2 * 10 + (x2[i] - '0');
+        } else if (x2[i] == '/') {
+            break;
+        }
+        i++;
+    }
+
+    // Calculate the denominator of the second fraction
+    i += 1; // Skip '/'
+    while(i < x2.length()) {
+        if (isdigit(x2[i])) {
+            denominator2 = denominator2 * 10 + (x2[i] - '0');
+        } 
+        i++;
+    }
+
+    int gcd = gcdFunc(numerator1, denominator1);
+    return (numerator1 / gcd) == (denominator1 / gcd);
 }
