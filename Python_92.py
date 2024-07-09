6d03fa9b67846d9f5867213f8c13b0c8016f544b
beep boop <githubbot@vadim.me>
2024-07-09 15:28:53 +0200

    Solution 1 of Python/92, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/i2mm8kd2 Prompt: Complete the following code given the task description and function signature.

--- Python_92.py
+++ Python_92.py
@@ -1,2 +1,11 @@
 def any_int(x, y, z):
-    return (x + y == z) or (x + z == y) or (y + z == x)
+    return (
+        x == y + z
+        or x == y - z
+        or x == y + z
+        or y == x + z
+        or y == x - z
+        or y == x + z
+        or z == x + y
+        or z == x - y
+    ) and all(isinstance(i, int) for i in [x, y, z])
