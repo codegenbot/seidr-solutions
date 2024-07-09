56f966f34e9cb5c142d016a32620aa66c1045997
beep boop <githubbot@vadim.me>
2024-07-09 05:09:59 +0200

    Solution 97 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/hvdy68h7 Prompt:       |             ^~~~~~ 425d6857-8839-47d4-98c7-3e5bfcbac0b7.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 425d6857-8839-47d4-98c7-3e5bfcbac0b7.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 425d6857-8839-47d4-98c7-3e5bfcbac0b7.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from 425d6857-8839-47d4-98c7-3e5bfcbac0b7.cpp:26: 425d6857-8839-47d4-98c7-3e5bfcbac0b7.cpp: In function ‘int main()’: 425d6857-8839-47d4-98c7-3e5bfcbac0b7.cpp:36:20: error: ‘sort_array’ was not decl ared in this scope    36 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 425d6857-8839-47d4-98c7-3e5bfcbac0b7.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,25 +1,27 @@
+```cpp
 #include <vector>
 #include <algorithm>
 
-bool issame(int a,int b){
+bool issame(int a, int b){
     return a == b;
 }
 
-void sort_array(std::vector<int>& arr) {
-    std::sort(arr.begin(), arr.end(), [](int a, int b) {
-        int ones_a = __builtin_popcount(a);
-        int ones_b = __builtin_popcount(b);
+bool operator<(int a, int b) {
+    int ones_a = __builtin_popcount(a);
+    int ones_b = __builtin_popcount(b);
 
-        if (ones_a == ones_b)
-            return a < b;
-        else
-            return ones_a < ones_b;
-    });
+    if (ones_a == ones_b)
+        return a < b;
+    else
+        return ones_a < ones_b;
+}
+
+vector<int> sort_array(vector<int> arr) {
+    sort(arr.begin(), arr.end());
+    return arr;
 }
 
 int main() {
-    std::vector<int> arr({2,4,8,16,32});
-    sort_array(arr);
-    for(auto i:arr)std::cout<<i<<" ";
+    assert(issame(sort_array({2,4,8,16,32}), {2, 4, 8, 16, 32}));
     return 0;
 }
