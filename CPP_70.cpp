3ca98221f5cad289ce084888a32d918b64fcde11
beep boop <githubbot@vadim.me>
2024-07-09 16:35:01 +0200

    Solution 23 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/vtpigf3u Prompt:       |                    ^~~ 946a994a-f019-49a3-b230-5d3d7f52fb24.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 946a994a-f019-49a3-b230-5d3d7f52fb24.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 946a994a-f019-49a3-b230-5d3d7f52fb24.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 946a994a-f019-49a3-b230-5d3d7f52fb24.cpp:16: 946a994a-f019-49a3-b230-5d3d7f52fb24.cpp: In function ‘int main()’: 946a994a-f019-49a3-b230-5d3d7f52fb24.cpp:26:20: error: ‘strange_sort_list’ was n ot declared in this scope    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 946a994a-f019-49a3-b230-5d3d7f52fb24.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,6 +1,33 @@
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b);
+bool issame(vector<int> a, vector<int> b){
+    if(a.size() != b.size()){
+        return false;
+    }
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<int> strange_sort_list(vector<int> lst);
+
+int main() {
+    // Test the function strange_sort_list
+    vector<int> input = {3, 1, 4, 1, 5, 9, 2, 6};
+    vector<int> output = strange_sort_list(input);
+    vector<int> expected_output = {1, 6, 1, 5, 2, 4, 3, 9};
+
+    if(issame(output, expected_output)){
+        cout << "Test Passed!" << endl;
+    } else {
+        cout << "Test Failed!" << endl;
+    }
+
+    return 0;
+}
 
 vector<int> strange_sort_list(vector<int> lst){
     sort(lst.begin(), lst.end());
