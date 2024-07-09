b20fca753e4ccede4d03fea45374818e996717b6
beep boop <githubbot@vadim.me>
2024-07-09 19:38:17 +0200

    Solution 26 of CPP/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3a2e7t76 Prompt: 16225471-2623-4735-9ff4-77ccc5c947a9.cpp:1:36: error: ‘std::minSubArraySum’ shou ld have been declared inside ‘std’     1 | long long std::minSubArraySum(std::vector<long long> nums) {       |                                    ^~~~~~ 16225471-2623-4735-9ff4-77ccc5c947a9.cpp:1:36: error: ‘vector’ is not a member o f ‘std’ 16225471-2623-4735-9ff4-77ccc5c947a9.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | long long std::minSubArraySum(std::vector<long long> nums) { 16225471-2623-4735-9ff4-77ccc5c947a9.cpp:1:43: error: expected primary-expressio n before ‘long’     1 | long long std::minSubArraySum(std::vector<long long> nums) {       |                                           ^~~~ In file included from 16225471-2623-4735-9ff4-77ccc5c947a9.cpp:18: 16225471-2623-4735-9ff4-77ccc5c947a9.cpp: In function ‘int main()’: 16225471-2623-4735-9ff4-77ccc5c947a9.cpp:20:27: error: ‘std::minSubArraySum’ can not be used as a function    20 |     assert (minSubArraySum({1, -1}) == -1);       |             ~~~~~~~~~~~~~~^~~~~~~~~

--- CPP_114.cpp
+++ CPP_114.cpp
@@ -1,17 +1,20 @@
-using namespace std;
+#include <vector>
+
+namespace std {
 long long minSubArraySum(vector<long long> nums) {
     long long total = 0;
     long long min_sum = LLONG_MAX;
-    
+
     for (int i = 0; i < nums.size(); i++) {
         total += nums[i];
-        
+
         if (total < min_sum)
             min_sum = total;
-        
+
         if (total > 0) 
             total = 0;
     }
-    
+
     return min_sum;
+}
 }
