0fe8b7c03eb4bdeb1f08a31ea52e7a2b319d31f0
beep boop <githubbot@vadim.me>
2024-07-09 22:16:57 +0200

    Solution 96 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/1olnobwn Prompt:       |                              ^~~~~~ c0e57045-9db5-433d-8d11-83239ed6f231.cpp:41:37: error: ‘string’ was not declared  in this scope    41 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c0e57045-9db5-433d-8d11-83239ed6f231.cpp:41:44: error: ‘b’ was not declared in t his scope    41 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c0e57045-9db5-433d-8d11-83239ed6f231.cpp:41:45: error: expression list treated a s compound expression in initializer [-fpermissive]    41 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c0e57045-9db5-433d-8d11-83239ed6f231.cpp:40: c0e57045-9db5-433d-8d11-83239ed6f231.cpp: In function ‘int main()’: c0e57045-9db5-433d-8d11-83239ed6f231.cpp:50:20: error: ‘numerical_letter_grade’ was not declared in this scope    50 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ c0e57045-9db5-433d-8d11-83239ed6f231.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,14 +1,6 @@
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if(a.size() != b.size()) return false;
-    for(int i=0; i<a.size(); i++) {
-        if(a[i] != b[i]) return false;
-    }
-    return true;
-}
-
 std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
     std::vector<std::string> letter_grades({"", "", "", "", "", "", "", "", "", ""}); 
     for (float grade : grades) {
