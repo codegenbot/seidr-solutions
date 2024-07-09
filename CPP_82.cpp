70a7c3fa4a42536776a384b14a2c83f8c337821c
beep boop <githubbot@vadim.me>
2024-07-09 18:49:02 +0200

    Solution 24 of CPP/82, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/7m4vwdb5 Prompt:       |  ^ cb5f1680-4490-47c4-b2d7-7ca41a6913ed.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ cb5f1680-4490-47c4-b2d7-7ca41a6913ed.cpp: In function ‘int main()’: cb5f1680-4490-47c4-b2d7-7ca41a6913ed.cpp:23:5: error: ‘assert’ was not declared in this scope    23 |     assert (prime_length("0") == false);       |     ^~~~~~ cb5f1680-4490-47c4-b2d7-7ca41a6913ed.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <sstream>   +++ |+#include <cassert>     5 | cb5f1680-4490-47c4-b2d7-7ca41a6913ed.cpp: At global scope: cb5f1680-4490-47c4-b2d7-7ca41a6913ed.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ cb5f1680-4490-47c4-b2d7-7ca41a6913ed.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main(){       |     ^~~~

--- CPP_82.cpp
+++ CPP_82.cpp
@@ -1,3 +1,4 @@
+```
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -7,17 +8,19 @@ using namespace std;
 
 bool prime_length(string str){
     int len = str.length();
+    if(len<1) return false;
     for(int i=2; i*i <= len; i++){
         if(len%i==0)
             return false;
     }
     for(int i=0; i<len; i++){
-        if(str[i] == '2' || str[i] == '3' || str[i] == '4' || str[i] == '5' || str[i] == '6' || str[i] == '7' || str[i] == '8' || str[i] == '9')
+        if(str[i] >= '2' && str[i] <= '9')
             return false;
     }
     return true;
 
 }
+
 int main(){
     assert (prime_length("0") == false);
 }
