06a443058925c2af491f61ca3ad610534c1912a8
beep boop <githubbot@vadim.me>
2024-07-09 22:07:55 +0200

    Solution 42 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/gyjrac6n Prompt: ad89c221-6794-4bc8-893d-cae9dc854015.cpp: In function ‘int main()’: ad89c221-6794-4bc8-893d-cae9dc854015.cpp:16:19: error: ‘accumulate’ was not decl ared in this scope    16 |         int sum = accumulate(nums.begin(), nums.end(), 0);       |                   ^~~~~~~~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <numeric>
 
 using namespace std;
 
@@ -10,39 +12,32 @@ int main() {
     while (cin >> num) {
         nums.push_back(num);
     }
+    
+    if (nums.size() > 1) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int target = sum / 2;
+        int prefixSum = 0;
+        int index = 0;
 
-    int sum = 0;
-    for (int i = 0; i < nums.size(); ++i) {
-        sum += nums[i];
-    }
+        for (int i = 0; i < nums.size(); ++i) {
+            prefixSum += nums[i];
+            if (prefixSum >= target) {
+                int diff1 = prefixSum - target;
+                int diff2 = target - (prefixSum - nums[i]);
+                if (diff1 < diff2) {
+                    index = i;
+                    break;
+                }
+            }
+        }
 
-    int target = sum / 2;
-    int prefixSum = 0;
-    int index = 0;
-
-    while (prefixSum < target) {
-        prefixSum += nums[index];
-        ++index;
-    }
-
-    if (prefixSum == target) {
-        for (int i = 0; i < index; ++i) {
-            cout << nums[i] << " ";
+        for (int i = 0; i <= index; ++i) {
+            cout << nums[i] << endl;
         }
-        cout << endl;
-        for (int i = index; i < nums.size(); ++i) {
-            cout << nums[i] << " ";
+        cout << "---" << endl;
+        for (int i = index + 1; i < nums.size(); ++i) {
+            cout << nums[i] << endl;
         }
-        cout << endl;
-    } else {
-        for (int i = 0; i < index; ++i) {
-            cout << nums[i] << " ";
-        }
-        cout << endl;
-        for (int i = index; i < nums.size(); ++i) {
-            cout << nums[i] << " ";
-        }
-        cout << endl;
     }
 
     return 0;
