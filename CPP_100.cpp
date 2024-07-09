3f7f595d560cbe2fee3c192f8974d8bffc28c261
beep boop <githubbot@vadim.me>
2024-07-09 19:37:45 +0200

    Solution 59 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/t1wz5kz8 Prompt:       |             ^~~~~~ 0f704a6b-7cc6-4cf8-871f-226307d98a38.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0f704a6b-7cc6-4cf8-871f-226307d98a38.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 0f704a6b-7cc6-4cf8-871f-226307d98a38.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 0f704a6b-7cc6-4cf8-871f-226307d98a38.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 0f704a6b-7cc6-4cf8-871f-226307d98a38.cpp:26: 0f704a6b-7cc6-4cf8-871f-226307d98a38.cpp: In function ‘int main()’: 0f704a6b-7cc6-4cf8-871f-226307d98a38.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -21,5 +21,5 @@ std::vector<int> make_a_pile(int n) {
 }
 
 int main() {
-    assert(same(make_a_pile(8), {8,10,12,14,16,18,20,22}));
+    assert(same(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
 }
