cb4e504ca98753e90cbd2a4a6c6444631e71511b
beep boop <githubbot@vadim.me>
2024-07-09 09:07:26 +0200

    Solution 20 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/4i26dq2n Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 3a547193-a365-44b8-ae0d-7cdcf20a7039.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <initializer_list>
 
 using namespace std;
 
@@ -19,8 +20,8 @@ vector<string> totalMatch(vector<string> lst1, vector<string> lst2) {
         sum2 += s.length();
     }
 
-    if (sum1 < sum2) return vector<string>(vector<string>(lst1));
-    else if (sum1 > sum2) return vector<string>(vector<string>(lst2));
+    if (sum1 < sum2) return vector<string>(lst1);
+    else if (sum1 > sum2) return vector<string>(lst2);
     else if (!lst1.size() && !lst2.size()) {
         if (same(vector<string>(lst1), vector<string>(lst2))) return vector<string>(lst1);
         for (const string& s : lst1) {
@@ -43,8 +44,8 @@ vector<string> totalMatch(vector<string> lst1, vector<string> lst2) {
             }
             if (!found) return vector<string>(lst2);
         }
-    } else if (!lst1.size()) return vector<string>(vector<string>(lst1));
-    else return vector<string>(vector<string>(lst2));
+    } else if (!lst1.size()) return vector<string>(lst1);
+    else return vector<string>(lst2);
 
     return {};
 }
