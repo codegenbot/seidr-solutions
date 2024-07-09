6c9fcf2e1c20689c3c4537a895357a91b56a3f44
beep boop <githubbot@vadim.me>
2024-07-09 04:19:16 +0200

    Solution 17 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/usez16r3 Prompt:       |                    ^~~ 38a06611-0ea5-4f30-a1bb-f2c9be652a00.cpp:9:27: error: ‘vector’ was not declared in this scope     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 38a06611-0ea5-4f30-a1bb-f2c9be652a00.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 38a06611-0ea5-4f30-a1bb-f2c9be652a00.cpp:9:39: error: expression list treated as  compound expression in initializer [-fpermissive]     9 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 38a06611-0ea5-4f30-a1bb-f2c9be652a00.cpp:8: 38a06611-0ea5-4f30-a1bb-f2c9be652a00.cpp: In function ‘int main()’: 38a06611-0ea5-4f30-a1bb-f2c9be652a00.cpp:18:20: error: ‘maximum’ was not declare d in this scope    18 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 38a06611-0ea5-4f30-a1bb-f2c9be652a00.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -4,14 +4,13 @@
 
 std::vector<int> maximum(std::vector<int> arr, int k){
     std::vector<int> res(k);
-    auto mid = arr.begin();
-    std::partial_sort(arr.begin(), arr.begin()+k, arr.end(), std::greater<int>());
-    for(int i=0; i<k; i++){
-        res[i] = *(mid+i);
-    }
+    auto end = arr.begin() + min((long long)arr.size(), k);
+    std::partial_sort(arr.begin(), end, arr.end(), std::greater<int>());
+    std::copy(arr.begin(), end, res.begin());
     return res;
 }
 
 int main() {
-    assert(maximum({1, 2, 3, -23, 243, -400, 0}, 3) == std::vector<int>({243, 3, 2})); 
+    assert(maximum({1, 2, 3, -23, 243, -400, 0}, 3) == vector<int>({243, 3, 2})); 
+    return 0;
 }
