8018ca9a9003b804e0f0af9df2a178a55af16792
beep boop <githubbot@vadim.me>
2024-07-09 06:59:59 +0200

    Solution 20 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/g3dq5xin Prompt:       |                              ^~~~~~ 724d7227-5e2a-49f7-bc69-4bdac4215966.cpp:8:37: error: ‘string’ was not declared in this scope     8 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 724d7227-5e2a-49f7-bc69-4bdac4215966.cpp:8:44: error: ‘b’ was not declared in th is scope     8 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 724d7227-5e2a-49f7-bc69-4bdac4215966.cpp:8:45: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 724d7227-5e2a-49f7-bc69-4bdac4215966.cpp:7: 724d7227-5e2a-49f7-bc69-4bdac4215966.cpp: In function ‘int main()’: 724d7227-5e2a-49f7-bc69-4bdac4215966.cpp:17:20: error: ‘all_prefixes’ was not de clared in this scope    17 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ 724d7227-5e2a-49f7-bc69-4bdac4215966.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,21 +1,22 @@
-#include <iostream>
 #include <vector>
 #include <string>
-#include <cassert>
 
-std::vector<std::string> all_prefixes(std::string str) {
-    std::vector<std::string> result;
+bool issame(const vector<string> a, const vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<string> all_prefixes(const string str) {
+    vector<string> result;
     for (int i = 1; i <= str.size(); ++i) {
         result.push_back(str.substr(0, i));
     }
     return result;
-}
-
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
-    return 0;
 }
