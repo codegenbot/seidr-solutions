e8dcf05b97be4142e2bd88b832349e67857c712e
beep boop <githubbot@vadim.me>
2024-07-09 06:24:42 +0200

    Solution 81 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/5yvq0zcc Prompt: c49aaa64-a2a8-4c54-a43c-c1479ae6dedd.cpp:8:23: error: ‘string’ is not a member o f ‘std’     8 | int bowlingScore(std::string s) {       |                       ^~~~~~ c49aaa64-a2a8-4c54-a43c-c1479ae6dedd.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int getBonus(int a, int b) {

--- bowling.cpp
+++ bowling.cpp
@@ -25,17 +25,8 @@ int bowlingScore(std::string s) {
             score += roll;
             if(currentFrame < 10) {
                 if(i+1 < s.length() && s[i+1] != '/') {
-                    if(s[i+1] == 'X') {
-                        score += 10;
-                        currentFrame++;
-                    } else if(s[i+1] == '/') {
-                        int secondRoll = (s[i+2] - '0');
-                        score += getBonus(roll, secondRoll);
-                        currentFrame++;
-                    } else {
-                        score += getBonus(roll, s[i+1] - '0');
-                        currentFrame++;
-                    }
+                    score += getBonus(roll, (s[i+1] - '0'));
+                    currentFrame++;
                 } else {
                     break;
                 }
