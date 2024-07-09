4717f1785d714c494382f5e4b2ae012b84297dbf
beep boop <githubbot@vadim.me>
2024-07-09 03:39:31 +0200

    Solution 6 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8ahd9aop Prompt:       |                              ^~~~~~ 0662524c-c5a4-482c-b17d-670bfaa58d5e.cpp:18:37: error: ‘string’ was not declared  in this scope    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0662524c-c5a4-482c-b17d-670bfaa58d5e.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0662524c-c5a4-482c-b17d-670bfaa58d5e.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0662524c-c5a4-482c-b17d-670bfaa58d5e.cpp:17: 0662524c-c5a4-482c-b17d-670bfaa58d5e.cpp: In function ‘int main()’: 0662524c-c5a4-482c-b17d-670bfaa58d5e.cpp:27:20: error: ‘total_match’ was not dec lared in this scope    27 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 0662524c-c5a4-482c-b17d-670bfaa58d5e.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,23 +1,17 @@
+```cpp
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> s1, std::vector<std::string> s2) {
-    if(s1.size() != s2.size()) return false;
-    for(int i = 0; i < s1.size(); i++) {
-        if(s1[i] != s2[i]) return false;
+bool issame(std::vector<std::string>, std::vector<std::string>) {
+    vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+        int sum1 = 0, sum2 = 0;
+        for (const auto& s : lst1) {
+            sum1 += s.length();
+        }
+        for (const auto& s : lst2) {
+            sum2 += s.length();
+        }
+        if (sum1 < sum2) return lst1;
+        else if (sum1 > sum2) return lst2;
+        else return lst1;
     }
-    return true;
-}
-
-std::vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    int sum1 = 0, sum2 = 0;
-    for (const auto& s : lst1) {
-        sum1 += s.length();
-    }
-    for (const auto& s : lst2) {
-        sum2 += s.length();
-    }
-    if (sum1 < sum2) return lst1;
-    else if (sum1 > sum2) return lst2;
-    else return lst1;
-}
