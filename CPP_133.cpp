ca4a91d48df3c9a4b226efed32fbc668c12e9aed
beep boop <githubbot@vadim.me>
2024-07-10 00:51:09 +0200

    Solution 49 of CPP/133, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/w92tthqe Prompt: aa010c66-892f-402c-a72e-17cb80d4f6ad.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | int sum_squares(vector<float> lst){       |                 ^~~~~~ aa010c66-892f-402c-a72e-17cb80d4f6ad.cpp:1:24: error: expected primary-expressio n before ‘float’     1 | int sum_squares(vector<float> lst){       |                        ^~~~~ In file included from aa010c66-892f-402c-a72e-17cb80d4f6ad.cpp:10: aa010c66-892f-402c-a72e-17cb80d4f6ad.cpp: In function ‘int main()’: aa010c66-892f-402c-a72e-17cb80d4f6ad.cpp:12:24: error: ‘sum_squares’ cannot be u sed as a function    12 |     assert (sum_squares({-1,1,0})==2);       |             ~~~~~~~~~~~^~~~~~~~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,8 +1,13 @@
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
 int sum_squares(vector<float> lst){
-    int result = 0;
+    int sum = 0;
     for(float x : lst){
-        int ceil_x = ceil(x);
-        result += pow(ceil_x, 2);
+        int rounded = ceil(x);
+        sum += pow(rounded,2);
     }
-    return result;
+    return sum;
 }
