ce40bad565f3e4879f5c9a7802551f63aff379c7
beep boop <githubbot@vadim.me>
2024-07-09 03:08:23 +0200

    Solution 10 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/o5dcqdjt Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0b52a8b4-7b15-4d23-bd96-961735564a58.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0b52a8b4-7b15-4d23-bd96-961735564a58.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0b52a8b4-7b15-4d23-bd96-961735564a58.cpp:10: 0b52a8b4-7b15-4d23-bd96-961735564a58.cpp: In function ‘int main()’: 0b52a8b4-7b15-4d23-bd96-961735564a58.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 0b52a8b4-7b15-4d23-bd96-961735564a58.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,21 +1,27 @@
 #include <vector>
 #include <string>
-#include <algorithm>
+using namespace std;
 
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) 
+    if(a.size() != b.size())
         return false;
-    
-    for(int i = 0; i < a.size(); i++) {
-        if(std::find(b.begin(), b.end(), a[i]) == b.end())
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i])
             return false;
     }
-    
     return true;
+}
+
+vector<string> filter_by_prefix(vector<string> strings, string prefix){
+    vector<string> result;
+    for(auto s : strings){
+        if(s.find(prefix) == 0)
+            result.push_back(s);
+    }
+    return result;
 }
 
 int main() {
     vector<string> strings = {"xxx", "asd", "xxy", "john doe", "xxxxAAA", "xxx"};
-    string prefix = "xxx";
-    assert(filter_by_prefix(strings, prefix) == vector<string>({"xxx", "xxxAAA", "xxx"}));
+    assert(filter_by_prefix(strings, "xxx") == vector<string>({"xxx", "xxxAAA", "xxx"}));
 }
