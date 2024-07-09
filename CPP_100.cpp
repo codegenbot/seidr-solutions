dbfded1629d624414ff13881e502a406bcd398cc
beep boop <githubbot@vadim.me>
2024-07-09 04:13:22 +0200

    Solution 38 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/yn69vtgz Prompt:       |             ^~~~~~ 4a2b422f-30b7-4a75-ad29-25986058e96c.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4a2b422f-30b7-4a75-ad29-25986058e96c.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 4a2b422f-30b7-4a75-ad29-25986058e96c.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~ In file included from 4a2b422f-30b7-4a75-ad29-25986058e96c.cpp:17: 4a2b422f-30b7-4a75-ad29-25986058e96c.cpp: In function ‘int main()’: 4a2b422f-30b7-4a75-ad29-25986058e96c.cpp:27:20: error: ‘make_a_pile’ was not dec lared in this scope    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 4a2b422f-30b7-4a75-ad29-25986058e96c.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<int> a,std::vector<int>b){
+bool issame(std::vector<int> a,std::vector<b>b){
     if(a.size() != b.size()) return false;
     for(int i = 0; i < a.size(); ++i) {
         if(a[i] != b[i]) return false;
