18d19018e96ec0e9e82ffa7cb933910ac8dcdc75
beep boop <githubbot@vadim.me>
2024-07-09 23:05:38 +0200

    Solution 43 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ahcchrnx Prompt:       |           ^~~~~~ In file included from 6df82cb8-4f8c-47f2-b24b-9a3a7fa90175.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 6df82cb8-4f8c-47f2-b24b-9a3a7fa90175.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 6df82cb8-4f8c-47f2-b24b-9a3a7fa90175.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 6df82cb8-4f8c-47f2-b24b-9a3a7fa90175.cpp:13: 6df82cb8-4f8c-47f2-b24b-9a3a7fa90175.cpp: In function ‘int main()’: 6df82cb8-4f8c-47f2-b24b-9a3a7fa90175.cpp:23:20: error: ‘get_positive’ was not de clared in this scope    23 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 6df82cb8-4f8c-47f2-b24b-9a3a7fa90175.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -2,14 +2,11 @@
 #include <assert.h>
 
 bool issame(std::vector<float> a, std::vector<float> b) {
-    if (a.size() != b.size()) {
+    if (a.size() != b.size())
         return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
+    for (int i = 0; i < a.size(); ++i)
+        if (std::abs(a[i] - b[i]) > 1e-6f)
             return false;
-        }
-    }
     return true;
 }
 
