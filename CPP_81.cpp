850c80d17db6cb22e6d03908fd82a9c9996cfede
beep boop <githubbot@vadim.me>
2024-07-09 12:28:33 +0200

    Solution 23 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ejnsoy1t Prompt:       |                              ^~~~~~ 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp:32: 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp: In function ‘int main()’: 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp:42:20: error: ‘numerical_letter_grade’ was not declared in this scope    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 792597fe-c7fa-45c5-8af7-700a45eb3361.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,15 +1,23 @@
-bool issame(vector<string> a,vector<string>b){
-    return a == b;
+bool issame(vector<string> a,vector<string> b){
+    for(int i=0;i<a.size()&&!issame(i);i++){
+        if(a[i]!=b[i]){
+            return false;
+        }
+    }
+    return true;
 }
 
-int main() {
-    vector<float> grades = {4.0,3.8,3.5,2.9,1.7};
+int main(){
+    vector<float> grades = {3.5, 4.2, 2.8};
     vector<string> result = numerical_letter_grade(grades);
-    for (string grade : result) {
-        if (issame(result, vector<string>({grade}))){
-            cout << "All students got the same letter grade: " << grade << endl;
-            break;
-        }
+    
+    for(string s : result){
+        cout<<s<<" ";
+    }
+    if(issame(result,{"A+ A+ B-" })){
+        cout<<endl<<"Grades are same";
+    }else{
+        cout<<endl<<"Grades are not same";
     }
     return 0;
 }
