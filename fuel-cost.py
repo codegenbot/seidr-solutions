9eb2b30a006341b09b685bd0aa86f0b1547fe374
beep boop <githubbot@vadim.me>
2024-07-09 20:12:42 +0200

    Solution 89 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/6l4p3yh2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9cabe916-579f-4a90-bab5-7ccc22d86 835.py", line 1     ```     ^ SyntaxError: invalid syntax

--- fuel-cost.py
+++ fuel-cost.py
@@ -1,4 +1,4 @@
 ```
-import math
 def fuel_cost(vector):
-    return sum(math.floor(i/3)-2 for i in vector)
+    """Given a vector of positive integers, divide each by 3, round the result down to the nearest integer, and subtract 2. Return the sum of all of the new integers in the vector."""
+    return sum((i//3)-2 for i in vector)
