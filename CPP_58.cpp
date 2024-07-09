1027099390d3635c6559e0eb99be43c28834fa3f
beep boop <githubbot@vadim.me>
2024-07-09 06:10:20 +0200

    Solution 25 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/isutmiwz Prompt:       |                    ^~~ c3f465f1-0309-466b-8bdb-17a30632093a.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c3f465f1-0309-466b-8bdb-17a30632093a.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c3f465f1-0309-466b-8bdb-17a30632093a.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c3f465f1-0309-466b-8bdb-17a30632093a.cpp:17: c3f465f1-0309-466b-8bdb-17a30632093a.cpp: In function ‘int main()’: c3f465f1-0309-466b-8bdb-17a30632093a.cpp:27:20: error: ‘common’ was not declared  in this scope    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ c3f465f1-0309-466b-8bdb-17a30632093a.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,14 +1,14 @@
-````
 #include <vector>
-#include <set>
 using namespace std;
 
 bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size())
+    if(a.size() != b.size()) {
         return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (find(b.begin(), b.end(), a[i]) == b.end())
+    }
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
             return false;
+        }
     }
     return true;
 }
@@ -31,6 +31,4 @@ vector<int> common(vector<int> l1, vector<int> l2) {
 
 int main() {
     assert(issame(common({4, 3, 2, 8}, {}), {}));
-    // Your implementation for 'main' here
 }
-```
