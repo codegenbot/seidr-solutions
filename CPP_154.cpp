2c20b535371db80c04764a1680a264b2ed6840c0
beep boop <githubbot@vadim.me>
2024-07-09 02:18:39 +0200

    Solution 22 of CPP/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/namiduv7 Prompt: 12a064e4-bfd8-431c-b130-385b6375bc09.cpp:1:23: error: ‘string’ was not declared in this scope     1 | bool cycpattern_check(string a, string b) {       |                       ^~~~~~ 12a064e4-bfd8-431c-b130-385b6375bc09.cpp:1:33: error: ‘string’ was not declared in this scope     1 | bool cycpattern_check(string a, string b) {       |                                 ^~~~~~ 12a064e4-bfd8-431c-b130-385b6375bc09.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool cycpattern_check(string a, string b) {       |                                         ^ In file included from 12a064e4-bfd8-431c-b130-385b6375bc09.cpp:20: 12a064e4-bfd8-431c-b130-385b6375bc09.cpp: In function ‘int main()’: 12a064e4-bfd8-431c-b130-385b6375bc09.cpp:22:30: error: ‘cycpattern_check’ cannot  be used as a function    22 |     assert  (cycpattern_check("winemtt","tinem") == true );       |              ~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_154.cpp
+++ CPP_154.cpp
@@ -1,25 +1,25 @@
 #include <string>
 using namespace std;
 
-int cycpattern_check(string a, string b) {
-    for(int i = 0; i < a.length(); i++) {
-        string temp = a.substr(i);
-        if(temp.length() >= b.length()) {
-            bool flag = true;
-            for(int j = 0; j < b.length(); j++) {
-                if(temp.find(b[j]) == string::npos) { 
-                    flag = false;
-                    break;
-                }
-                temp = temp.substr(1) + temp[0]; 
-            }
-            if(flag) return 1; 
-        }
-    }
-    return 0;
-}
+bool cycpattern_check(string a, string b);
 
 int main() {
-    assert(cycpattern_check("winemtt","tinem") == 1 );
-    return 0;
+    assert(cycpattern_check("winemtt","tinem") == true );
+}
+
+bool cycpattern_check(string a, string b) {
+    for(int i = 0; i < a.length(); i++) {
+        bool flag = true;
+        string temp = a.substr(i);
+        int j = 0;
+        while(j < temp.length()) {
+            if(temp[j] != b[j % b.length()]) {
+                flag = false;
+                break;
+            }
+            j++;
+        }
+        if(flag) return true;
+    }
+    return false;
 }
