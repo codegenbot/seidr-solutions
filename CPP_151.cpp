ab2c95e83ff0d978047d22afcec9e1531baf67c0
beep boop <githubbot@vadim.me>
2024-07-09 17:53:19 +0200

    Solution 17 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nvcrge59 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const float&}; _Tp = float; _Alloc = std::allocator<float>; std::v ector<_Tp, _Alloc>::iterator = std::vector<float>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = float; _Alloc = std::all ocator<float>; std::vector<_Tp, _Alloc>::value_type = float]’ ae7c4fa7-109d-488a-a51b-ce3a96cef981.cpp:23:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,10 +1,5 @@
-```c
-#include <iostream>
-#include <cmath>
-#include <vector>
-
-long double_the_difference(std::vector<float> lst) {
-    long sum = 0;
+double long_the_difference(std::vector<float> lst) {
+    double sum = 0;
     for (float num : lst) {
         if (num > 0 && floor(num) == num) { 
             if (fmod(num, 2.0) != 0.0) { 
@@ -14,16 +9,3 @@ long double_the_difference(std::vector<float> lst) {
     }
     return sum;
 }
-
-std::vector<float> lst;
-float num;
-
-while (std::cin >> num) {
-    lst.push_back(num);
-}
-
-long odd_sum = double_the_difference(lst);
-
-assert(double_the_difference(lst) == odd_sum); 
-
-return 0;
