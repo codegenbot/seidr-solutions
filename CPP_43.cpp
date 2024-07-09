afce47e7acebb4c06935c0f20e16eadbca16bc56
beep boop <githubbot@vadim.me>
2024-07-09 08:53:56 +0200

    Solution 69 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/x8c0rtpg Prompt: in this scope    22 |     assert(pairs_sum_to_zero(input) == true);       |     ^~~~~~ 7347c2dd-c95f-4a36-b083-acc3eea466a6.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <set>   +++ |+#include <cassert>     3 | 7347c2dd-c95f-4a36-b083-acc3eea466a6.cpp: At global scope: 7347c2dd-c95f-4a36-b083-acc3eea466a6.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 7347c2dd-c95f-4a36-b083-acc3eea466a6.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from 7347c2dd-c95f-4a36-b083-acc3eea466a6.cpp:26: 7347c2dd-c95f-4a36-b083-acc3eea466a6.cpp: In function ‘int main()’: 7347c2dd-c95f-4a36-b083-acc3eea466a6.cpp:28:30: error: ‘pairs_sum_to_zero’ canno t be used as a function    28 |     assert (pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);       |             ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,6 +1,9 @@
+```cpp
+#include <initializer_list>
+#include <set>
 #include <cassert>
 
-bool pairs_sum_to_zero(std::initializer_list<std::pair<int, int>> l) {
+bool pairs_sum_to_zero(std::vector<std::pair<int, int>> l) {
     std::set<int> s;
     for (const auto& p : l) {
         if(p.second != -p.first)
@@ -9,14 +12,15 @@ bool pairs_sum_to_zero(std::initializer_list<std::pair<int, int>> l) {
     }
     for (auto it = s.begin(); it != s.end(); ++it) { 
         int complement = -(*it);
-        if (s.find(complement) != s.end()) {
-            return true;
+        if (s.find(complement) == s.end()) {
+            return false;
         }
     }
-    return false; 
+    return true; 
 }
 
 int main() {
-    assert(pairs_sum_to_zero({{-3, 0}, {9, -9}, {-1, 1}, {4, -4}, {2, -2}, {31, -31}}) == false);
+    std::vector<std::pair<int, int>> input({{-3, 0}, {9, -9}, {-1, 1}, {4, -4}, {2, -2}, {31, -31}});
+    assert(pairs_sum_to_zero(input) == false);
     return 0;
 }
