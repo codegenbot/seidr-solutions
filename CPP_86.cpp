3b047fc0504b055b0ed0c50370973598c2ccd068
beep boop <githubbot@vadim.me>
2024-07-09 17:15:47 +0200

    Solution 16 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/z7qvolno Prompt: 6375bf3a-f55c-4648-a968-459644cf8d78.cpp:12:9: error: expected constructor, dest ructor, or type conversion before ‘(’ token    12 |     sort(word.begin(), word.end());       |         ^ 6375bf3a-f55c-4648-a968-459644cf8d78.cpp:13:5: error: ‘result’ does not name a t ype    13 |     result += word;       |     ^~~~~~ 6375bf3a-f55c-4648-a968-459644cf8d78.cpp:14:5: error: expected unqualified-id be fore ‘return’    14 |     return result;       |     ^~~~~~ 6375bf3a-f55c-4648-a968-459644cf8d78.cpp:15:1: error: expected declaration befor e ‘}’ token    15 | }       | ^ In file included from 6375bf3a-f55c-4648-a968-459644cf8d78.cpp:17: 6375bf3a-f55c-4648-a968-459644cf8d78.cpp: In function ‘int main()’: 6375bf3a-f55c-4648-a968-459644cf8d78.cpp:19:13: error: ‘anti_shuffle’ was not de clared in this scope    19 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,10 +1,24 @@
-string ordered_word = s;
-    int start = 0;
-    for (int i = 0; i <= s.length(); ++i) {
-        if (s[i] == ' ' || i == s.length()) {
-            sort(ordered_word.begin() + start, ordered_word.begin() + i);
-            start = i + 1;
+#include <iostream>
+#include <algorithm>
+
+std::string anti_shuffle(std::string s) {
+    std::string result = "";
+    std::string word = "";
+    for (char c : s) {
+        if (c == ' ') {
+            std::sort(word.begin(), word.end());
+            result += word + ' ';
+            word = "";
+        } else {
+            word += c;
         }
     }
-    return ordered_word;
+    std::sort(word.begin(), word.end());
+    result += word;
+    return result;
+}
+
+int main() {
+    assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
+    return 0;
 }
