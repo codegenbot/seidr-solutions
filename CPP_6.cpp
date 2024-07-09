bff7ad58302141bb86932eaee6f605b56355ab25
beep boop <githubbot@vadim.me>
2024-07-09 06:12:52 +0200

    Solution 15 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/73sda81b Prompt:       |                    ^~~ 2ace8f1a-01c4-48f8-9359-b463a41f36d9.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2ace8f1a-01c4-48f8-9359-b463a41f36d9.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2ace8f1a-01c4-48f8-9359-b463a41f36d9.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2ace8f1a-01c4-48f8-9359-b463a41f36d9.cpp:21: 2ace8f1a-01c4-48f8-9359-b463a41f36d9.cpp: In function ‘int main()’: 2ace8f1a-01c4-48f8-9359-b463a41f36d9.cpp:31:20: error: ‘parse_nested_parens’ was  not declared in this scope    31 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ 2ace8f1a-01c4-48f8-9359-b463a41f36d9.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,24 +1,4 @@
 #include <vector>
-
-bool issame(const vector<int>& a, const vector<int>& b);
-
-vector<int> parse_nested_parens(const std::string& paren_string) {
-    vector<int> depths;
-    int max_depth = 0;
-    int current_depth = 0;
-    for (char c : paren_string) {
-        if (c == '(') {
-            current_depth++;
-            max_depth = max(max_depth, current_depth);
-        } else if (c == ')') {
-            current_depth--;
-        }
-        if (c == ' ') {
-            depths.push_back(max_depth);
-            max_depth = 0;
-            current_depth = 0;
-        }
-    }
-    depths.push_back(max_depth);
-    return depths;
+bool parse_nested_parens(const std::vector<int>& a, const std::vector<int>& b) {
+    return a == b;
 }
