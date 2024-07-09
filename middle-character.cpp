8b831a543d3332b1ef36e87d00d09c14e4058deb
beep boop <githubbot@vadim.me>
2024-07-09 03:33:41 +0200

    Solution 26 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/ux0vjpor Prompt: 9d593cbb-6183-4606-8697-eff6b5a777c3.cpp: In function ‘std::string middleCharact er(std::string)’: 9d593cbb-6183-4606-8697-eff6b5a777c3.cpp:5:23: error: could not convert ‘(((n % 2) == 1) ? ((int)s.std::__cxx11::basic_string<char>::operator[](((std::__cxx11:: basic_string<char>::size_type)(n / 2)))) : (((int)s.std::__cxx11::basic_string<c har>::operator[](((std::__cxx11::basic_string<char>::size_type)((n - 1) / 2)))) + ((int)s.std::__cxx11::basic_string<char>::operator[](((std::__cxx11::basic_str ing<char>::size_type)(n / 2))))))’ from ‘int’ to ‘std::string’ {aka ‘std::__cxx1 1::basic_string<char>’}     5 |     return n % 2 == 1 ? s[n / 2] : (s[(n - 1) / 2] + s[n / 2]);       |            ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                       |       |                       int

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,8 +1,11 @@
 std::string middleCharacter(std::string s) {
     int n = s.length();
-    if (n % 2 == 1) {
-        return std::string(1, s[n / 2]);
+    std::string result = "";
+    if(n % 2 == 1) {
+        result += s[n / 2];
     } else {
-        return s.substr(n / 2 - 1, 2);
+        result += s[(n-1)/2];
+        result += s[n/2];
     }
+    return result;
 }
