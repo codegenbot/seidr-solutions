90e53a08beb1081df3b7af81fd86b2968baa7ab1
beep boop <githubbot@vadim.me>
2024-07-09 08:02:31 +0200

    Solution 1 of CPP/124, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7un1y34g Prompt: Complete the following code given the task description and function signature.

--- CPP_124.cpp
+++ CPP_124.cpp
@@ -2,29 +2,36 @@
 using namespace std;
 
 bool valid_date(string date) {
+    int day, month, year;
     size_t pos = 0;
-    string day, month, year;
+    pos = date.find('-');
+    month = stoi(date.substr(0, pos));
+    pos += 1;
+    pos = date.find('-', pos);
+    day = stoi(date.substr(pos-3, 2));
+    year = stoi(date.substr(pos+1));
 
-    if (date.find("-") == string::npos)
+    if (month < 1 || month > 12) {
         return false;
+    }
 
-    pos = date.find("-");
-    day = date.substr(0, pos);
-    month = date.substr(pos + 1, date.find("-") - pos - 1);
-    year = date.substr(date.find("-") + 1);
-
-    if (day.size() > 2 || month.size() > 2 || year.size() > 4)
+    if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            && day > 31) {
         return false;
-
-    int d = stoi(day);
-    int m = stoi(month);
-    int y = stoi(year);
-
-    if (m < 1 || m > 12)
+    }
+    else if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
         return false;
-    
-    if ((m == 2 && d > 29) || (m != 2 && (d < 1 || (m % 2 == 0 && d > 30) || (m % 2 != 0 && d > 31))))
-        return false;
+    }
+    else if (month == 2) {
+        if (day > 29) {
+            return false;
+        }
+        else if (year % 4 != 0) {
+            if (day > 28) {
+                return false;
+            }
+        }
+    }
 
     return true;
 }
