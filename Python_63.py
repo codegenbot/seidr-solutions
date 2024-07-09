e648ff00e9f9e617db85fe981cf4c103ce9d370f
beep boop <githubbot@vadim.me>
2024-07-09 07:02:58 +0200

    Solution 13 of Python/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ai6gi22g Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51efab09-45d7-4d4c-a591-c99b28518 637.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_63.py
+++ Python_63.py
@@ -1,3 +1,4 @@
+```
     def fibfib(n: int):
         if n <= 1:
             return 0
@@ -6,5 +7,6 @@
         else:
             a, b, c = 0, 0, 1
             for _ in range(3, n+1):
-                a, b, c = b, c, a+b+c
+                c = (a + b) % 2
+                a, b = b, c
             return c
