2a47791b2474a74a0aa83d26a2d79378cbcb2fe4
beep boop <githubbot@vadim.me>
2024-07-09 14:17:27 +0200

    Solution 80 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/p6mep8bz Prompt: 0daf32e5-d70c-421f-8817-e6f66dd9a596.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string flip_case(string str){       | ^~~~~~ In file included from 0daf32e5-d70c-421f-8817-e6f66dd9a596.cpp:12: 0daf32e5-d70c-421f-8817-e6f66dd9a596.cpp: In function ‘int main()’: 0daf32e5-d70c-421f-8817-e6f66dd9a596.cpp:14:13: error: ‘filp_case’ was not decla red in this scope    14 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,10 +1,10 @@
 #include <string>
 
-string flip_case(string str){
-    for(char &c : str){
-        if(islower(c)){
+std::string flip_case(std::string str){
+    for (char &c : str) {
+        if (islower(c)) {
             c = toupper(c);
-        } else if(isupper(c)){
+        } else if (isupper(c)) {
             c = tolower(c);
         }
     }
