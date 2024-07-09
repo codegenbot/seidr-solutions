ca8fd425cb4529d658e802e48270811b28b27ad0
beep boop <githubbot@vadim.me>
2024-07-09 03:03:42 +0200

    Solution 64 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/37wdax4t Prompt: declared here  6739 |   to_string(long double __val)       |   ^~~~~~~~~ ee18b46b-9c6a-4a47-9008-c791f0de56a6.cpp:15:18: error: ‘eval’ was not declared i n this scope; did you mean ‘timeval’?    15 |     int result = eval(expression);       |                  ^~~~       |                  timeval ee18b46b-9c6a-4a47-9008-c791f0de56a6.cpp: At global scope: ee18b46b-9c6a-4a47-9008-c791f0de56a6.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ ee18b46b-9c6a-4a47-9008-c791f0de56a6.cpp:43:5: note: ‘int main()’ previously def ined here    43 | int main() {       |     ^~~~ In file included from ee18b46b-9c6a-4a47-9008-c791f0de56a6.cpp:48: ee18b46b-9c6a-4a47-9008-c791f0de56a6.cpp: In function ‘int main()’: ee18b46b-9c6a-4a47-9008-c791f0de56a6.cpp:50:13: error: ‘do_algebra’ was not decl ared in this scope    50 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ^~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -4,19 +4,6 @@
 
 std::vector<std::string> ops = {"//", "*"}; 
 
-int doAlgo(std::vector<std::string> operators, std::vector<int> operands) {
-    std::string expression = "";
-    for (int i = 0; i < operators.size() - 1; i++) {
-        expression += to_string(operands[i]);
-        expression += ops[i];
-    }
-    expression += to_string(operands.back());
-    
-    int result = eval(expression);
-    
-    return result;
-}
-
 int eval(const std::string& s) {
     int result = 0;
     if (s.empty()) {
@@ -33,14 +20,28 @@ int eval(const std::string& s) {
             }
             start = i + 1;
         } else {
-            result += sign * std::stoi(std::string(1, s[i]).c_str());
+            result += sign * std::stoi(s.substr(start, i-start));
+            start = i + 1;
         }
     }
     
     return result;
 }
 
+int do_algorithm(std::vector<std::string> operators, std::vector<int> operands) {
+    std::string expression = "";
+    for (int i = 0; i < operators.size() - 1; i++) {
+        expression += to_string(operands[i]);
+        expression += operators[i];
+    }
+    expression += to_string(operands.back());
+    
+    int result = eval(expression);
+    
+    return result;
+}
+
 int main() {
-    int result = doAlgo(ops, {7, 3, 4});
+    int result = do_algorithm(ops, {7, 3, 4});
     return 0;
 }
