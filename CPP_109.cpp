3e52e563386f733876e79b675b9769c5b8d294ec
beep boop <githubbot@vadim.me>
2024-07-09 04:01:42 +0200

    Solution 19 of CPP/109, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2snjoxbp Prompt: 59aa99a3-4979-476b-ab73-3ebb0120a5c8.cpp:1:20: error: ‘vector’ was not declared in this scope     1 | bool move_one_ball(vector<int> arr) {       |                    ^~~~~~ 59aa99a3-4979-476b-ab73-3ebb0120a5c8.cpp:1:27: error: expected primary-expressio n before ‘int’     1 | bool move_one_ball(vector<int> arr) {       |                           ^~~ In file included from 59aa99a3-4979-476b-ab73-3ebb0120a5c8.cpp:9: 59aa99a3-4979-476b-ab73-3ebb0120a5c8.cpp: In function ‘int main()’: 59aa99a3-4979-476b-ab73-3ebb0120a5c8.cpp:11:26: error: ‘move_one_ball’ cannot be  used as a function    11 |     assert (move_one_ball({})==true);       |             ~~~~~~~~~~~~~^~~~

--- CPP_109.cpp
+++ CPP_109.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <algorithm>
 
 bool move_one_ball(vector<int> arr) {
     int minElement = *min_element(arr.begin(), arr.end());
@@ -7,3 +6,4 @@ bool move_one_ball(vector<int> arr) {
         if (i != minElement) return false;
     }
     return true;
+}
