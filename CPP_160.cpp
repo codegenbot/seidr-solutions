9b5af6a969d85ce5786b3c581d8eb6df8c7a7866
beep boop <githubbot@vadim.me>
2024-07-09 10:46:39 +0200

    Solution 4 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jkn2px16 Prompt:       |                       ^~~~~~ 480d0311-5202-4697-b0b6-f436784451e0.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | using namespace std; 480d0311-5202-4697-b0b6-f436784451e0.cpp:3:31: error: ‘operator_’ was not declar ed in this scope     3 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                               ^~~~~~~~~ 480d0311-5202-4697-b0b6-f436784451e0.cpp:3:42: error: ‘vector’ was not declared in this scope     3 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                          ^~~~~~ 480d0311-5202-4697-b0b6-f436784451e0.cpp:3:42: note: ‘std::vector’ is defined in  header ‘<vector>’; did you forget to ‘#include <vector>’? 480d0311-5202-4697-b0b6-f436784451e0.cpp:3:49: error: expected primary-expressio n before ‘int’     3 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                 ^~~ 480d0311-5202-4697-b0b6-f436784451e0.cpp:3:61: error: expression list treated as  compound expression in initializer [-fpermissive]     3 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                             ^

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,40 +1,41 @@
-#include <string>
+```cpp
 #include <vector>
+#include <string>
 #include <cmath>
 
-int doAlgebra(vector<string> operators, vector<int> operands) {
+int do_algebra(vector<string> operator_, vector<int> operand) {
     int result = 0;
-    for (int i = 0; i < operators.size(); i++) {
-        if (operators[i] == "+") {
-            result += operands[i];
-        } else if (operators[i] == "-") {
-            result -= operands[i];
-        } else if (operators[i] == "*") {
-            int temp = operands[i];
-            for (int j = i + 1; j < operators.size(); j++) {
-                if (operators[j] == "*") {
-                    temp *= operands[j];
-                } else if (operators[j] == "+") {
-                    result += temp;
+    for (int i = 0; i < operator_.size(); i++) {
+        if (operator_[i] == "+") {
+            result += operand[i];
+        } else if (operator_[i] == "-") {
+            result -= operand[i];
+        } else if (operator_[i] == "*") {
+            int temp = 1;
+            for (int j = i; j < operator_.size(); j++) {
+                if (operator_[j] == "*") {
+                    temp *= operand[j + 1];
+                } else if (operator_[j] == "+") {
+                    result += temp * operand[j + 1];
                     break;
                 }
             }
-        } else if (operators[i] == "//") {
-            int temp = operands[i];
-            for (int j = i + 1; j < operators.size(); j++) {
-                if (operators[j] == "//") {
-                    temp /= operands[j];
-                } else if (operators[j] == "+") {
-                    result += temp;
+        } else if (operator_[i] == "//") {
+            int temp = operand[i + 1];
+            for (int j = i + 1; j < operator_.size(); j++) {
+                if (operator_[j] == "//") {
+                    temp /= operand[j + 1];
+                } else if (operator_[j] == "+") {
+                    result += temp / operand[j + 1];
                     break;
                 }
             }
-        } else if (operators[i] == "**") {
-            int temp = pow(operands[i], 1);
-            for (int j = i + 1; j < operators.size(); j++) {
-                if (operators[j] == "**") {
-                    temp = pow(temp, operands[j]);
-                } else if (operators[j] == "+") {
+        } else if (operator_[i] == "**") {
+            int temp = pow(operand[i + 1], 1);
+            for (int j = i + 1; j < operator_.size(); j++) {
+                if (operator_[j] == "**") {
+                    temp = pow(temp, operand[j + 1]);
+                } else if (operator_[j] == "+") {
                     result += temp;
                     break;
                 }
@@ -42,4 +43,3 @@ int doAlgebra(vector<string> operators, vector<int> operands) {
         }
     }
     return result;
-}
