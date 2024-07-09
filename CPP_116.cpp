21ece7abef03f7e5dcb99b696a374e06a2a54ca5
beep boop <githubbot@vadim.me>
2024-07-09 04:00:12 +0200

    Solution 5 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/hvdy68h7 Prompt:       |                    ^~~ d77afb37-ed9f-44d0-89a9-670815d9fbb0.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d77afb37-ed9f-44d0-89a9-670815d9fbb0.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d77afb37-ed9f-44d0-89a9-670815d9fbb0.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d77afb37-ed9f-44d0-89a9-670815d9fbb0.cpp:16: d77afb37-ed9f-44d0-89a9-670815d9fbb0.cpp: In function ‘int main()’: d77afb37-ed9f-44d0-89a9-670815d9fbb0.cpp:26:20: error: ‘sort_array’ was not decl ared in this scope    26 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ d77afb37-ed9f-44d0-89a9-670815d9fbb0.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,8 +1,9 @@
+```cpp
 #include <vector>
 #include <algorithm>
 
-vector<int> sort_array(vector<int> arr) {
-    sort(arr.begin(), arr.end(), [](int a, int b) {
+std::vector<int> sort_array(std::vector<int> arr) {
+    std::sort(arr.begin(), arr.end(), [](int a, int b) {
         int ones_a = __builtin_popcount(a);
         int ones_b = __builtin_popcount(b);
 
