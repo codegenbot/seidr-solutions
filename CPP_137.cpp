381f472bfa8371fef5ce7f591c42a66cba68e481
beep boop <githubbot@vadim.me>
2024-07-09 22:48:18 +0200

    Solution 61 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/j0a1ba12 Prompt: f51c17fe-f7a9-40c7-9f29-32e9ea54a7ff.cpp:1:1: error: ‘boost’ does not name a typ e     1 | boost::any compare_one(boost::any a, boost::any b) {       | ^~~~~ In file included from f51c17fe-f7a9-40c7-9f29-32e9ea54a7ff.cpp:19: f51c17fe-f7a9-40c7-9f29-32e9ea54a7ff.cpp: In function ‘int main()’: f51c17fe-f7a9-40c7-9f29-32e9ea54a7ff.cpp:21:13: error: ‘boost’ has not been decl ared    21 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ f51c17fe-f7a9-40c7-9f29-32e9ea54a7ff.cpp:21:29: error: ‘string’ was not declared  in this scope    21 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ f51c17fe-f7a9-40c7-9f29-32e9ea54a7ff.cpp:21:37: error: ‘compare_one’ was not dec lared in this scope    21 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,10 +3,14 @@
 boost::any compare_one(boost::any a, boost::any b) {
     if (boost::any_cast<std::string>(a) <= boost::any_cast<std::string>(b)) {
         return a;
-    } else if (boost::any_cast<int>(a) <= boost::any_cast<int>(b)) {
-        return a;
-    } else if (boost::any_cast<double>(a) <= boost::any_cast<double>(b)) {
+    }
+    else if (boost::any_cast<int>(a) <= boost::any_cast<int>(b)) {
         return a;
     }
-    return b;
+    else if (boost::any_cast<double>(a) <= boost::any_cast<double>(b)) {
+        return a;
+    }
+    else {
+        return b;
+    }
 }
