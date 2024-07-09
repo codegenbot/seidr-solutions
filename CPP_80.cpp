503707fb681aacebb6548e5ddd0b6eba19da04ec
beep boop <githubbot@vadim.me>
2024-07-09 15:58:14 +0200

    Solution 17 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0u1me4un Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 475fed2a-7b9f-4399-9261-5d5f4981667f.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<const char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 475fed2a-7b9f-4399-9261-5d5f4981667f.cpp:23:20:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,12 +1,20 @@
+```cpp
 #include <string>
-#include <algorithm>
 
 bool is_happy(std::string s) {
     if (s.length() < 3)
         return false;
     for (int i = 0; i <= s.length() - 3; i++) {
-        std::string sub = s.substr(i, 3);
-        if (std::any_of(sub.begin(), sub.end(), [&](char x){ return s.find(x) != std::string::npos; }))
+        std::string sub = (char*)s.c_str()+i;
+        sub.resize(3);
+        bool found = false;
+        for (auto c : sub) {
+            if ((char*)s.c_str() != (char*)sub.c_str()) {
+                found = true;
+                break;
+            }
+        }
+        if (found)
             return false;
     }
     return true;
