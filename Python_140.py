b031cce30564af081358da1f078ee7507ab71911
beep boop <githubbot@vadim.me>
2024-07-09 08:47:22 +0200

    Solution 76 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/2wxb0we4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8d905637-08e9-48a0-94c6-5ba17807d 494.py", line 22, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8d905637-08e9-48a0-94c6-5ba17807d 494.py", line 18, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -3,12 +3,12 @@ def fix_spaces(text):
     new_text = ""
     for i in range(len(text)):
         if text[i] == " ":
-            if i < len(text) - 1 and text[i + 1] == " ":
+            if and (i >= len(text) - 2 or text[i + 1] != " ") :
+                if new_text and not new_text.endswith("_"):
+                    new_text += "_"
+            else:
                 if new_text and not new_text.endswith("-"):
                     new_text += "-"
-            else:
-                if new_text and not new_text.endswith("_") or (new_text and new_text[-1] != " "):
-                    new_text += "_"
         else:
             new_text += text[i]
     return new_text
