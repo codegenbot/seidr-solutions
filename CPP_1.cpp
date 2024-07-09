171388fe24d590721d3d0b526523dcfb407ffee9
beep boop <githubbot@vadim.me>
2024-07-09 12:51:46 +0200

    Solution 76 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/s92p6few Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:26: 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp: In function ‘int main()’: 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 0c5e27f1-56bb-4e33-8bf4-b5757c7a976e.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,51 +1,9 @@
-bool issame(string a, string b) {
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    return a == b;
+bool issame(string s) {
+    return all_of(s.begin(), s.end(), [&](char c) { return c == s[0]; });
 }
 
 vector<string> separate_paren_groups(string paren_string);
 
 int main() {
-    string input;
-    cout << "Enter a string with parentheses: ";
-    cin >> input;
-    vector<string> groups = separate_paren_groups(input);
-
-    for (string group : groups) {
-        string first_part = group.substr(0, group.size() / 2);
-        string second_part = group.substr(group.size() / 2);
-        if (issame(first_part, second_part)) {
-            cout << group << " has the property.\n";
-        } else {
-            cout << group << " does not have the property.\n";
-        }
-    }
-
-    return 0;
-}
-
-vector<string> separate_paren_groups(string paren_string) {
-    vector<string> result;
-    string current_group;
-    int balance = 0;
-
-    for (char c : paren_string) {
-        if (c == '(') {
-            if (balance > 0) {
-                current_group += c;
-            }
-            balance++;
-        } else if (c == ')') {
-            balance--;
-            if (balance > 0) {
-                current_group += c;
-            } else if (balance == 0) {
-                result.push_back(current_group);
-                current_group = "";
-            }
-        }
-    }
-
-    return result;
+    // code here
 }
