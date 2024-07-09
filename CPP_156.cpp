44e5c3efd4ba6575e00b6aa51e679f79e3f89699
beep boop <githubbot@vadim.me>
2024-07-09 23:41:25 +0200

    Solution 98 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/a68ks535 Prompt:       | ^~~~~~~~~~~~~ 7e6b9cb5-0766-4e9f-9722-cb52c878c0d7.cpp:25:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    25 | int main(){       |         ^~ 7e6b9cb5-0766-4e9f-9722-cb52c878c0d7.cpp:25:9: note: remove parentheses to defau lt-initialize a variable    25 | int main(){       |         ^~       |         -- 7e6b9cb5-0766-4e9f-9722-cb52c878c0d7.cpp:25:9: note: or replace parentheses with  braces to value-initialize a variable 7e6b9cb5-0766-4e9f-9722-cb52c878c0d7.cpp:25:11: error: a function-definition is not allowed here before ‘{’ token    25 | int main(){       |           ^ 7e6b9cb5-0766-4e9f-9722-cb52c878c0d7.cpp:27:2: error: expected ‘}’ at end of inp ut    27 | }       |  ^ 7e6b9cb5-0766-4e9f-9722-cb52c878c0d7.cpp:20:12: note: to match this ‘{’    20 | int main() {       |            ^

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -19,11 +19,9 @@ string int_to_mini_roman(int number) {
 }
 
 int main() {
-    if(roman.empty()) {
-        cout << "Error: Empty Roman map!" << endl;
-        return 1; // exit with error code
+    if(1999 == std::stoi(int_to_mini_roman(1999))){
+        return 0;
+    } else {
+        return -1;
     }
-
-    assert(int_to_mini_roman(1000) == "M");
-    return 0;
 }
