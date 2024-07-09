7a2871467aea31b13d2bbfc3a5399607471194b3
beep boop <githubbot@vadim.me>
2024-07-09 11:13:00 +0200

    Solution 7 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/rllrm9mm Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c02dbe69-810a-4d34-960e-1f3a90c78a35.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c02dbe69-810a-4d34-960e-1f3a90c78a35.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c02dbe69-810a-4d34-960e-1f3a90c78a35.cpp:5: c02dbe69-810a-4d34-960e-1f3a90c78a35.cpp: In function ‘int main()’: c02dbe69-810a-4d34-960e-1f3a90c78a35.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ c02dbe69-810a-4d34-960e-1f3a90c78a35.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,47 +1,12 @@
 vector<int> minPath(vector<vector<int>> grid, int k){
     vector<int> path;
-    int n = grid.size();
-    int m = grid[0].size();
-    
-    for (int i = 1; i < n; ++i) {
-        grid[i][0] += grid[i - 1][0];
-    }
-    
-    for (int j = 1; j < m; ++j) {
-        grid[0][j] += grid[0][j - 1];
-    }
-    
-    for (int i = 1; i < n; ++i) {
-        for (int j = 1; j < m; ++j) {
-            grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
-        }
-    }
-    
-    int i = n - 1, j = m - 1;
-    
-    while (i > 0 || j > 0) {
-        path.push_back(grid[i][j]);
-        
-        if (i > 0 && j > 0) {
-            if (grid[i - 1][j] < grid[i][j - 1]) {
-                --i;
-            } else {
-                --j;
+    for (int i = 0; i < grid.size(); ++i) {
+        for (int j = 0; j < grid[0].size(); ++j) {
+            if (grid[i][j] <= k) {
+                path.push_back(grid[i][j]);
             }
-        } else if (i > 0) {
-            --i;
-        } else {
-            --j;
         }
     }
-    
-    path.push_back(grid[0][0]);
-    reverse(path.begin(), path.end());
-    
-    if (k <= path.size()) {
-        path.resize(k);
-    } 
-    
     return path;
 }
 
