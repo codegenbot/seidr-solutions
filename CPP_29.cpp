fdb098f2f62b6a5a3d99c94b45f6d1183005b934
beep boop <githubbot@vadim.me>
2024-07-09 21:02:02 +0200

    Solution 32 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/jy7fbynr Prompt:    67 |     using string    = basic_string<char>;       |           ^~~~~~ 50195f39-acff-4d1b-b990-7bf43da4ca8b.cpp:15:44: error: ‘b’ was not declared in t his scope    15 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 50195f39-acff-4d1b-b990-7bf43da4ca8b.cpp:15:45: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 50195f39-acff-4d1b-b990-7bf43da4ca8b.cpp:14: 50195f39-acff-4d1b-b990-7bf43da4ca8b.cpp: In function ‘int main()’: 50195f39-acff-4d1b-b990-7bf43da4ca8b.cpp:24:20: error: ‘filter_by_prefix’ was no t declared in this scope    24 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 50195f39-acff-4d1b-b990-7bf43da4ca8b.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,8 +1,16 @@
 #include <vector>
 #include <string>
 
-bool issame(vector<string> a, vector<string> b) {
-    return (a == b);
+bool issame(const vector<string>& a, const vector<string>& b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 vector<string> filter_by_prefix(vector<string> strings, string prefix) {
@@ -16,6 +24,6 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix) {
 }
 
 int main() {
-    assert(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxA", "xxx"}, "xxx") == {"xxx", "xxxAAA", "xxx"});
+    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
     return 0;
 }
