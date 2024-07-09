1975fc94272bc8d140f3c0fa0f2f3c35a315e626
beep boop <githubbot@vadim.me>
2024-07-09 03:21:40 +0200

    Solution 22 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h5y6hrvk Prompt: 97fa5af6-d949-45a7-b891-4dc1e7126a9d.cpp:22:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    22 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 97fa5af6-d949-45a7-b891-4dc1e7126a9d.cpp:25:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    25 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 97fa5af6-d949-45a7-b891-4dc1e7126a9d.cpp:25:36: error: ‘find_zero’ cannot be use d as a function    25 |         solution = find_zero(coeffs);       |                                    ^ In file included from 97fa5af6-d949-45a7-b891-4dc1e7126a9d.cpp:9: 97fa5af6-d949-45a7-b891-4dc1e7126a9d.cpp:26:21: error: ‘poly’ was not declared i n this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 97fa5af6-d949-45a7-b891-4dc1e7126a9d.cpp:26:17: error: ‘abs’ was not declared in  this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,14 +1,22 @@
 #include <vector>
 #include <cmath>
 
-double find_zero(std::vector<double> coeffs) {
-    if (coeffs.size() < 2)
-        return -1.0;
-
-    double solution = 1.0;
-    for (int i = coeffs.size() - 1; i >= 0; --i) {
-        solution -= coeffs[i] / std::pow(coeffs[coeffs.size()-1], i);
+double find_zero(std::vector<double> xs) {
+    double sum = 0;
+    int power = 0;
+    
+    for (int i = xs.size() - 1; i >= 0; i--) {
+        sum += xs[i] * pow(-1, xs.size() - 1 - i) * pow(solution(), power);
+        power++;
     }
+    
+    return -sum;
+}
 
-    return solution;
+double solution() {
+    double x = 0.5;
+    for (int i = 2; i < xs.size(); i++)
+        if (xs[i] != 0)
+            return x;
+    return -1.0 * xs[0];
 }
