11bac7fae53351c369a559d732fa48068a3b8504
beep boop <githubbot@vadim.me>
2024-07-09 22:36:44 +0200

    Solution 99 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gc3g1rrt Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 792a6914-4124-4dc3-bbb2-8d58710078d5.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -27,6 +27,6 @@ std::vector<int> unique_digits(std::vector<int> v) {
 }
 
 int main() {
-    assert(same(unique_digits({135, 103, 31}), std::vector<int>({31, 135})));
+    assert(same(unique_digits({135, 103, 31}), std::vector<int>( {31, 135} )));
     return 0;
 }
