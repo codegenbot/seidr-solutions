f8bbdaa6140616c6730872b7ff6c6db10ae676f3
beep boop <githubbot@vadim.me>
2024-07-09 16:18:47 +0200

    Solution 4 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/51ki5569 Prompt: Complete the following code given the task description and function signature.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -7,7 +7,7 @@ string string_to_md5(string text) {
     MD5_CTX ctx;
     MD5_Init(&ctx);
     const char* str = text.c_str();
-    size_t len = text.size();
+    size_t len = text.length();
     MD5_Update(&ctx, str, len);
     MD5_Final(md, &ctx);
 
