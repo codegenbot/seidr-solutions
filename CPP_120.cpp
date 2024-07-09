2d6a0017cb9980bd84397a5e349da07bdab97bde
beep boop <githubbot@vadim.me>
2024-07-09 04:14:12 +0200

    Solution 20 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ajzrdwh9 Prompt:       |                    ^~~ fc02c22d-6994-4ba8-8583-b3a4bed25aee.cpp:8:27: error: ‘vector’ was not declared in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fc02c22d-6994-4ba8-8583-b3a4bed25aee.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fc02c22d-6994-4ba8-8583-b3a4bed25aee.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fc02c22d-6994-4ba8-8583-b3a4bed25aee.cpp:7: fc02c22d-6994-4ba8-8583-b3a4bed25aee.cpp: In function ‘int main()’: fc02c22d-6994-4ba8-8583-b3a4bed25aee.cpp:17:20: error: ‘maximum’ was not declare d in this scope    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ fc02c22d-6994-4ba8-8583-b3a4bed25aee.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -2,19 +2,11 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> v1, vector<int> v2) {
-    if (v1.size() != v2.size()) return false;
-    for(int i = 0; i < v1.size(); i++) {
-        if(v1[i] != v2[i]) return false;
+std::vector<int> maximum(std::vector<int> arr, int k) {
+    if (k > arr.size()) {
+        return {};
     }
-    return true;
-}
-
-vector<int> maximum(vector<int> arr, int k) {
-    vector<int> result(k);
-    partial_sort(arr.begin(), arr.begin() + k, arr.end());
-    for (int i = 0; i < k; i++) {
-        result[i] = arr[i];
-    }
+    std::vector<int> result(arr.begin(), arr.begin() + k);
+    sort(result.begin(), result.end());
     return result;
 }
