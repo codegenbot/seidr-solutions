e04053d851c28de8a8a3c8339fcf01eba9ef559c
beep boop <githubbot@vadim.me>
2024-07-09 18:11:39 +0200

    Solution 2 of CPP/40, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/irqb191x Prompt: 1436814d-45ba-4a5c-9190-3f8f11044156.cpp: In function ‘bool triples_sum_to_zero( std::vector<int>)’: 1436814d-45ba-4a5c-9190-3f8f11044156.cpp:7:5: error: ‘set’ was not declared in t his scope     7 |     set<int> s(l.begin(), l.end());       |     ^~~ 1436814d-45ba-4a5c-9190-3f8f11044156.cpp:3:1: note: ‘std::set’ is defined in hea der ‘<set>’; did you forget to ‘#include <set>’?     2 | #include <algorithm>   +++ |+#include <set>     3 | 1436814d-45ba-4a5c-9190-3f8f11044156.cpp:7:9: error: expected primary-expression  before ‘int’     7 |     set<int> s(l.begin(), l.end());       |         ^~~ 1436814d-45ba-4a5c-9190-3f8f11044156.cpp:8:18: error: ‘s’ was not declared in th is scope     8 |     for (int i : s) {       |                  ^

--- CPP_40.cpp
+++ CPP_40.cpp
@@ -1,10 +1,12 @@
 #include <vector>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
 bool triples_sum_to_zero(vector<int> l) {
-    set<int> s(l.begin(), l.end());
+    set<int> s;
+    s.insert(l.begin(), l.end());
     for (int i : s) {
         int j = -i;
         if (s.find(j) != s.end()) {
