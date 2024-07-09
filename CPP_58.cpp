8b7f96e6b858fed5ad2956af9d549a77f04c3e20
beep boop <githubbot@vadim.me>
2024-07-09 06:16:05 +0200

    Solution 33 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/isutmiwz Prompt:       |                    ^~~ c3f465f1-0309-466b-8bdb-17a30632093a.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c3f465f1-0309-466b-8bdb-17a30632093a.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c3f465f1-0309-466b-8bdb-17a30632093a.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c3f465f1-0309-466b-8bdb-17a30632093a.cpp:17: c3f465f1-0309-466b-8bdb-17a30632093a.cpp: In function ‘int main()’: c3f465f1-0309-466b-8bdb-17a30632093a.cpp:27:20: error: ‘common’ was not declared  in this scope    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ c3f465f1-0309-466b-8bdb-17a30632093a.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,10 +1,11 @@
+```
 #include <vector>
 #include <set>
 
-std::vector<int> common(std::vector<int> l1,std::vector<int> l2){
-    std::set<int> s1(l1.begin(),l1.end());
-    std::set<int> s2(l2.begin(),l2.end());
-    std::set<int> result;
+vector<int> common(vector<int> l1,vector<int> l2){
+    set<int> s1(l1.begin(),l1.end());
+    set<int> s2(l2.begin(),l2.end());
+    set<int> result;
     
     for(auto i : s1){
         if(s2.find(i) != s2.end()){
@@ -12,7 +13,7 @@ std::vector<int> common(std::vector<int> l1,std::vector<int> l2){
         }
     }
     
-    std::vector<int> res(result.begin(),result.end());
+    vector<int> res(result.begin(),result.end());
     sort(res.begin(),res.end());
     return res;
 }
