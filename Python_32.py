2f66d77ea9e673ac0f632c34e7c931f96c1d33cd
beep boop <githubbot@vadim.me>
2024-07-09 05:40:28 +0200

    Solution 23 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dfvmwimw Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f16a87cf-a166-4456-aba3-9169c7a3c 929.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -2,10 +2,5 @@ def find_zero(xs):
     n = len(xs)
     if n % 2 != 0:
         raise ValueError("xs must have even number of coefficients")
-    sum1 = 0
-    sum2 = xs[0]
-    for i in range(n//2):
-        sum1 += xs[2*i] 
-        sum2 += xs[2*i+1]
-    x = sum2 / sum1
+    x = -xs[1] / xs[0]
     return round(x, 2)
