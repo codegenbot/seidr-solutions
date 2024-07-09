4243756d31f63e5facacfa301a80ada97a8e5b1d
beep boop <githubbot@vadim.me>
2024-07-09 14:17:39 +0200

    Solution 23 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7dqgrwjn Prompt:     4 | using namespace std; 293128ce-7dd1-4c9b-aa8b-9862bd3db7cc.cpp:53:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    53 | int main(){       |         ^~ 293128ce-7dd1-4c9b-aa8b-9862bd3db7cc.cpp:53:9: note: remove parentheses to defau lt-initialize a variable    53 | int main(){       |         ^~       |         -- 293128ce-7dd1-4c9b-aa8b-9862bd3db7cc.cpp:53:9: note: or replace parentheses with  braces to value-initialize a variable 293128ce-7dd1-4c9b-aa8b-9862bd3db7cc.cpp:53:11: error: a function-definition is not allowed here before ‘{’ token    53 | int main(){       |           ^ 293128ce-7dd1-4c9b-aa8b-9862bd3db7cc.cpp:55:2: error: expected ‘}’ at end of inp ut    55 | }       |  ^ 293128ce-7dd1-4c9b-aa8b-9862bd3db7cc.cpp:39:12: note: to match this ‘{’    39 | int main() {       |            ^

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,4 +1,6 @@
+```c++
 #include <vector>
+#include <cassert>
 #include <cmath>
 using namespace std;
 
@@ -35,7 +37,9 @@ vector<float> sort_even(vector<float> l) {
     return result;
 }
 
-int main_test() {
-    assert(isSame(sort_even(vector<float>(make_vector({5, 8, -12, 4, 23, 2, 3, 11, 12, -10})) ) , {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+int main() { 
+    vector<float> input = make_vector({5, 8, -12, 4, 23, 2, 3, 11, 12, -10});
+    vector<float> expected = { -12, 8, 3, 4, 5, 2, 12, 11, 23, -10};
+    assert(isSame(sort_even(input), expected));
     return 0;
 }
