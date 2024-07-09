b693147bf7e186f3538d121d39d8118d27becaf0
beep boop <githubbot@vadim.me>
2024-07-09 09:31:02 +0200

    Solution 14 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/hk0wk18u Prompt:       |                                   ^~~~~~ ac0543ae-667d-4420-b628-17a4a5a6f357.cpp:18:42: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ ac0543ae-667d-4420-b628-17a4a5a6f357.cpp:18:49: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ ac0543ae-667d-4420-b628-17a4a5a6f357.cpp:18:56: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from ac0543ae-667d-4420-b628-17a4a5a6f357.cpp:17: ac0543ae-667d-4420-b628-17a4a5a6f357.cpp: In function ‘int main()’: ac0543ae-667d-4420-b628-17a4a5a6f357.cpp:30:20: error: ‘get_row’ was not declare d in this scope    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ ac0543ae-667d-4420-b628-17a4a5a6f357.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,9 +1,5 @@
 #include <vector>
 
-bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b){
-    return a == b;
-}
-
 std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
     std::vector<std::vector<int>> result;
     for (int i = 0; i < lst.size(); ++i) {
@@ -13,11 +9,15 @@ std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
             }
         }
     }
-    sort(result.begin(), result.end());
+    std::sort(result.begin(), result.end());
     for (auto &coord : result) {
-        sort(coord.begin(), coord.end(), std::greater<int>());
+        std::sort(coord.begin(), coord.end(), std::greater<int>());
     }
     return result;
+}
+
+bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b){
+    return a == b;
 }
 
 int main(){
