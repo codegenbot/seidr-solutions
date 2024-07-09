c7556ba8940ad7a7864476dcbbafa34f8c13d0d1
beep boop <githubbot@vadim.me>
2024-07-09 03:17:07 +0200

    Solution 76 of CPP/149, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h3blrigf Prompt: 661b7322-c157-4d42-9c65-6e563641e3f1.cpp: In function ‘std::vector<std::__cxx11: :basic_string<char> > sorted_list_sum(std::vector<std::__cxx11::basic_string<cha r> >)’: 661b7322-c157-4d42-9c65-6e563641e3f1.cpp:10:5: error: ‘sort’ was not declared in  this scope; did you mean ‘short’?    10 |     sort(lst.begin(),lst.end());       |     ^~~~       |     short 661b7322-c157-4d42-9c65-6e563641e3f1.cpp: At global scope: 661b7322-c157-4d42-9c65-6e563641e3f1.cpp:25:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    25 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 661b7322-c157-4d42-9c65-6e563641e3f1.cpp:5:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     5 | bool issame(vector<string> a,vector<string>b) {       |      ^~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,15 +1,23 @@
-Here is the completed code:
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool comp(string a, string b) {
+    return a == b;
+}
 
 vector<string> sorted_list_sum(vector<string> lst) {
-    auto it = remove_if(lst.begin(), lst.end(), [](const string& s){ return s.length() % 2; });
-    lst.erase(it, lst.end());
-    sort(lst.begin(), lst.end(), 
-        [](const string& a, const string& b){
-            if(a.length() != b.length()) {
-                return a.length() < b.length();
-            } else {
-                return a < b;
-            }
-        });
-    return lst;
+    sort(lst.begin(),lst.end());
+    string str = "";
+    for(auto s : lst) {
+        str += s;
+    }
+    vector<string> result;
+    if(str.length() > 0) {
+        result.push_back(str);
+    } else {
+        result.push_back("0");
+    }
+    return result;
 }
