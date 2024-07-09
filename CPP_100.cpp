778aefc40d7e6a9b5e84c768ebf9f89af8120954
beep boop <githubbot@vadim.me>
2024-07-09 17:45:16 +0200

    Solution 82 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9d9hp3es Prompt:       |                                  ^~~ 21f2a4a2-01a5-4859-855c-2ef0748669f8.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 21f2a4a2-01a5-4859-855c-2ef0748669f8.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 21f2a4a2-01a5-4859-855c-2ef0748669f8.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 21f2a4a2-01a5-4859-855c-2ef0748669f8.cpp:20: 21f2a4a2-01a5-4859-855c-2ef0748669f8.cpp: In function ‘int main()’: 21f2a4a2-01a5-4859-855c-2ef0748669f8.cpp:30:20: error: ‘make_a_pile’ was not dec lared in this scope    30 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 21f2a4a2-01a5-4859-855c-2ef0748669f8.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,15 +1,18 @@
 #include <vector>
 #include <algorithm>
 
-bool samePiles(vector<int> a, vector<int> b) {
+bool areEqual(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-int main() {
-    int n = 8;
+vector<int> make_a_pile(int n) {
     vector<int> pile = {0, 1};
     for (int i = 2; i <= n; ++i)
         pile.push_back(pile.back() + i);
-    assert(samePiles(pile, pile));
+    return pile;
+}
+
+int main() {
+    assert(areEqual(make_a_pile(8), make_a_pile(8)));
     return 0;
 }
