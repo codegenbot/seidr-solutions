06f22f95566a2be8ee9dac6b479ede35f7573410
beep boop <githubbot@vadim.me>
2024-07-09 11:21:01 +0200

    Solution 12 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j8w2522w Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,17 +1,18 @@
-#include<string>
-using namespace std;
-
 bool simplify(string x, string n) {
-    int a = 0, b = 0, c = 0, d = 0;
+    int numeratorX = 0, denominatorX = 0, numeratorN = 0, denominatorN = 0;
     
-    // converting fraction strings to integers
-    sscanf(x.c_str(), "%d/%d", &a, &b);
-    sscanf(n.c_str(), "%d/%d", &c, &d);
-
-    // calculating greatest common divisor of both numerators and denominators
-    int numeratorGCD = __gcd(a * d, b * c);
-    int denominatorGCD = __gcd(b, d);
-
-    // check if the fraction is simplified or not
-    return a * d / numeratorGCD != b * c / denominatorGCD;
+    size_t pos = 0;
+    pos = x.find('/');
+    int num1 = stoi(x.substr(0, pos));
+    int deno1 = stoi(x.substr(pos + 1));
+    
+    pos = n.find('/');
+    int num2 = stoi(n.substr(0, pos));
+    int deno2 = stoi(n.substr(pos + 1));
+    
+    if((num1 * deno2) == (deno1 * num2)) {
+        return true;
+    } else {
+        return false;
+    }
 }
