69f8f90851babe276fa73adf948c3d94d802aa17
beep boop <githubbot@vadim.me>
2024-07-09 21:48:45 +0200

    Solution 59 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/oje19k94 Prompt:       |           ^~~~~~ 30dbef45-c515-40c8-8628-dd488083d4ba.cpp:19:44: error: ‘b’ was not declared in t his scope    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 30dbef45-c515-40c8-8628-dd488083d4ba.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 30dbef45-c515-40c8-8628-dd488083d4ba.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 30dbef45-c515-40c8-8628-dd488083d4ba.cpp:18: 30dbef45-c515-40c8-8628-dd488083d4ba.cpp: In function ‘int main()’: 30dbef45-c515-40c8-8628-dd488083d4ba.cpp:28:20: error: ‘total_match’ was not dec lared in this scope    28 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 30dbef45-c515-40c8-8628-dd488083d4ba.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -12,24 +12,10 @@ bool issame(vector<string> a,vector<string>b) {
 
 vector<vector<string>> total_match(vector<string> a, int b) {
     vector<vector<string>> result;
-    
-    // implement your logic here
-    // For example, you can generate all possible permutations of the strings in 'a' and add them to 'result'
-    for(int i = 0; i <= b; i++) {
-        for (int j = 0; j <= a.size() - i - 1; j++) {
-            for (int k = 0; k < a.size(); k++) {
-                if(k >= j && k <= j + i) {
-                    vector<string> perm;
-                    int start = j, end = j+i;
-                    for(int l = start; l <= end; l++) {
-                        perm.push_back(a[l]);
-                    }
-                    result.push_back(perm);
-                }
-            }
-        }
-    }
-    
+    if(b == 0)
+        result = {{}};
+    else
+        result.push_back({a});
     return result;
 }
 
