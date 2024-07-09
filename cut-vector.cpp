467db8e5c49488512eaa3e411032ce60defbed62
beep boop <githubbot@vadim.me>
2024-07-09 05:59:06 +0200

    Solution 19 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/eb053e6f Prompt: >, std::vector<int> > cutVector(std::vector<int>&)’: 2ee60fa9-3c4a-4f31-a9a5-dc67e01ac73b.cpp:5:19: error: ‘INT_MAX’ was not declared  in this scope     5 |     int minDiff = INT_MAX;       |                   ^~~~~~~ 2ee60fa9-3c4a-4f31-a9a5-dc67e01ac73b.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; 2ee60fa9-3c4a-4f31-a9a5-dc67e01ac73b.cpp:8:20: error: ‘abs’ was not declared in this scope     8 |         int diff = abs(nums[i] - nums[i-1]);       |                    ^~~ 2ee60fa9-3c4a-4f31-a9a5-dc67e01ac73b.cpp:14:97: error: expected ‘}’ at end of in put    14 |     return {{nums.begin(), nums.begin() + splitIndex}, {nums.begin() + s plitIndex, nums.end()}};       |                         ^ 2ee60fa9-3c4a-4f31-a9a5-dc67e01ac73b.cpp:4:61: note: to match this ‘{’     4 | pair<vector<int>, vector<int>> cutVector(vector<int>& nums) {       |                                                             ^

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -3,10 +3,6 @@
 using namespace std;
 
 pair<vector<int>, vector<int>> cutVector(vector<int>& nums) {
-    if (nums.size() == 1) {
-        return {{}, {nums[0]}};
-    }
-    
     int minDiff = INT_MAX;
     int splitIndex = 0;
     for (int i = 1; i < nums.size(); i++) {
@@ -16,6 +12,17 @@ pair<vector<int>, vector<int>> cutVector(vector<int>& nums) {
             splitIndex = i;
         }
     }
-    
-    return {{nums.begin(), nums.begin() + splitIndex}, {nums.begin() + splitIndex, nums.end()}};
-}
+
+    vector<int> left, right;
+    if (splitIndex == 0) {
+        left = {nums[0]};
+        right = nums;
+    } else if (splitIndex == nums.size() - 1) {
+        left = nums;
+        right = {nums.back()};
+    } else {
+        left = vector<int>(nums.begin(), nums.begin() + splitIndex);
+        right = vector<int>(nums.begin() + splitIndex, nums.end());
+    }
+
+    return {left, right};
