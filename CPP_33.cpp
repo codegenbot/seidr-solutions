e1a9a5610a9695f64958ea3c035be5ef9b389fe5
beep boop <githubbot@vadim.me>
2024-07-09 07:38:53 +0200

    Solution 20 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/404w9ynn Prompt: /usr/include/c++/11/bits/stl_uninitialized.h:704:44:   required from ‘_ForwardIt erator std::__uninitialized_default_n_a(_ForwardIterator, _Size, std::allocator< _Tp>&) [with _ForwardIterator = int*; _Size = long unsigned int; _Tp = int]’ /usr/include/c++/11/bits/stl_vector.h:1606:36:   required from ‘void std::vector <_Tp, _Alloc>::_M_default_initialize(std::vector<_Tp, _Alloc>::size_type) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = l ong unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:512:9:   required from ‘std::vector<_Tp, _ Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocator_type&) [with  _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = long unsigned int; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<int >]’ ee44d43d-1ed1-4f96-8137-661febef99e0.cpp:10:28:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-int* sort_third(vector<int> l) {
+vector<int> sort_third(vector<int> l) {
     int size = (l.size() + 2)/3 * 3; 
-    int* result = new int[size]; 
+    vector<int> result(size, 0); 
     int i = 0;
     for (i = 0; i < l.size(); ) {
         if (i % 3 == 0 && i < l.size() - 2) {
@@ -16,8 +16,7 @@ int* sort_third(vector<int> l) {
                 temp.push_back(l[j]);
             }
             sort(temp.begin(), temp.end());
-            int k = 0;
-            for (int t : temp) {
+            for (int k : temp) {
                 result[i/3]++; 
                 i++;
             }
@@ -45,23 +44,20 @@ int main() {
     }
     cout << endl;
 
-    int* result1 = sort_third(l1);
-    int* result2 = sort_third(l2);
+    vector<int> result1 = sort_third(l1);
+    vector<int> result2 = sort_third(l2);
 
     cout << "result1: ";
-    for (int i = 0; i < ((l1.size() + 2)/3 * 3); i++) {
-        cout << result1[i] << " ";
+    for (int i : result1) {
+        cout << i << " ";
     }
     cout << endl;
 
     cout << "result2: ";
-    for (int i = 0; i < ((l2.size() + 2)/3 * 3); i++) {
-        cout << result2[i] << " ";
+    for (int i : result2) {
+        cout << i << " ";
     }
     cout << endl;
 
-    delete[] result1;
-    delete[] result2;
-    
     return 0;
 }
