013b42eba6db6f38ea59f4622707f962cbdfc98c
beep boop <githubbot@vadim.me>
2024-07-09 06:57:45 +0200

    Solution 25 of CPP/158, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ocd14082 Prompt: 409c1cb0-354d-48b0-9410-19627e2c0e62.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string find_max(vector<string> words){       | ^~~~~~ In file included from 409c1cb0-354d-48b0-9410-19627e2c0e62.cpp:14: 409c1cb0-354d-48b0-9410-19627e2c0e62.cpp: In function ‘int main()’: 409c1cb0-354d-48b0-9410-19627e2c0e62.cpp:16:14: error: ‘find_max’ was not declar ed in this scope    16 |     assert ((find_max({"play", "play", "play"}) == "play"));       |              ^~~~~~~~

--- CPP_158.cpp
+++ CPP_158.cpp
@@ -1,16 +1,14 @@
-#include <string>
 #include <vector>
 #include <algorithm>
 
-const std::string &find_max(const vector<string> &words) {
-    string res = *max_element(words.begin(), words.end(),
-        [](const string &a, const string &b) {
-            auto a_unique_chars = (int)a.size();
-            auto b_unique_chars = (int)b.size();
+const std::string& find_max(const std::vector<std::string>& words) {
+    return *std::max_element(words.begin(), words.end(),
+        [](const std::string &a, const std::string &b) {
+            auto a_unique_chars = a.size();
+            auto b_unique_chars = b.size();
             if(a_unique_chars == b_unique_chars)
                 return a < b;
             return a_unique_chars > b_unique_chars;
         }
     );
-    return res;
 }
