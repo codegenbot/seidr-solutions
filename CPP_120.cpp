9c0e3e917a189868a8cbe052894f068321ac460b
beep boop <githubbot@vadim.me>
2024-07-09 04:25:45 +0200

    Solution 38 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ajzrdwh9 Prompt:       |             ^~~~~~ a3e18663-cdde-4beb-bb15-4204d79ee562.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a3e18663-cdde-4beb-bb15-4204d79ee562.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a3e18663-cdde-4beb-bb15-4204d79ee562.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ a3e18663-cdde-4beb-bb15-4204d79ee562.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from a3e18663-cdde-4beb-bb15-4204d79ee562.cpp:23: a3e18663-cdde-4beb-bb15-4204d79ee562.cpp: In function ‘int main()’: a3e18663-cdde-4beb-bb15-4204d79ee562.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -5,16 +5,17 @@
 std::vector<int> maximum(std::vector<int> arr, int k) {
     std::vector<int> result;
     for(int i = 0; i < k; i++) {
-        auto it = *std::max_element(arr.begin(), arr.end());
-        result.push_back(it);
-        arr.erase(std::remove(arr.begin(), arr.end(), it), arr.end());
+        auto it = std::max_element(arr.begin(), arr.end());
+        result.push_back(*it);
+        arr.erase(it);
     }
     return result;
 }
 
 int main() {
-    std::vector<int> test = maximum({1, 2, 3, -23, 243, -400, 0}, 3);
-    for (int i : test) {
+    int test[] = {1, 2, 3, -23, 243, -400, 0};
+    std::vector<int> testVector(test, test + sizeof(test) / sizeof(test[0]));
+    for (int i : maximum(testVector, 3)) {
         std::cout << i << " ";
     }
     return 0;
