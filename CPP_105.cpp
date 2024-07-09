69b532360a6bf74697c1260d7034d645748497fe
beep boop <githubbot@vadim.me>
2024-07-09 04:33:42 +0200

    Solution 55 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pe563gn9 Prompt: In file included from /usr/include/c++/11/string:39,                  from edad93f0-07b4-4664-ac89-56c0621d8937.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from edad93f0-07b4-4664-ac89-56c0621d8937.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ edad93f0-07b4-4664-ac89-56c0621d8937.cpp:49:44: error: ‘b’ was not declared in t his scope    49 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from edad93f0-07b4-4664-ac89-56c0621d8937.cpp:48: edad93f0-07b4-4664-ac89-56c0621d8937.cpp: In function ‘int main()’: edad93f0-07b4-4664-ac89-56c0621d8937.cpp:58:20: error: ‘by_length’ was not decla red in this scope    58 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ edad93f0-07b4-4664-ac89-56c0621d8937.cpp:58:19: error: ‘issame’ cannot be used a s a function    58 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -6,45 +6,98 @@ bool issame(vector<string> a, vector<string> b) {
 }
 
 vector<vector<string>> by_length(vector<int> arr) {
-    vector<vector<string>> result;
+    vector<string> num;
     for (int i : arr) {
         if (i >= 1 && i <= 9) {
-            string num;
             switch (i) {
                 case 1:
-                    num = "One";
+                    num.push_back("One");
                     break;
                 case 2:
-                    num = "Two";
+                    num.push_back("Two");
                     break;
                 case 3:
-                    num = "Three";
+                    num.push_back("Three");
                     break;
                 case 4:
-                    num = "Four";
+                    num.push_back("Four");
                     break;
                 case 5:
-                    num = "Five";
+                    num.push_back("Five");
                     break;
                 case 6:
-                    num = "Six";
+                    num.push_back("Six");
                     break;
                 case 7:
-                    num = "Seven";
+                    num.push_back("Seven");
                     break;
                 case 8:
-                    num = "Eight";
+                    num.push_back("Eight");
                     break;
                 case 9:
-                    num = "Nine";
+                    num.push_back("Nine");
                     break;
             }
-            if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8) {
-                result.push_back({num});
-            } else {
-                result.push_back({num, num});
+        } else if (i >= 10) {
+            switch (i % 10) {
+                case 1:
+                    num.push_back(num[0] + "teen");
+                    break;
+                case 2:
+                    num.push_back(num[i / 10 - 1] + "teen");
+                    break;
+                case 3:
+                    num.push_back(num[i / 10 - 1] + "teen");
+                    break;
+                case 4:
+                    if (i % 10 == 9) {
+                        num.push_back("Ninety" + (num[0] == "One" ? "y" : ""));
+                    } else {
+                        num.push_back(num[i / 10 - 1] + "ty");
+                    }
+                    break;
+                case 5:
+                    if (i % 10 != 0) {
+                        num.push_back("Ninety" + (num[0] == "One" ? "y" : ""));
+                        num.push_back("Five");
+                    } else {
+                        num.push_back(num[i / 10 - 1] + "ty");
+                    }
+                    break;
+                case 6:
+                    if (i % 10 != 0) {
+                        num.push_back(num[i / 10 - 1] + "ty and Six");
+                    } else {
+                        num.push_back(num[i / 10 - 1] + "ty");
+                    }
+                    break;
+                case 7:
+                    if (i % 10 != 0) {
+                        num.push_back("Seven" + (num[0] == "One" ? "y" : ""));
+                    } else {
+                        num.push_back(num[i / 10 - 1] + "ty");
+                    }
+                    break;
+                case 8:
+                    if (i % 10 != 0) {
+                        num.push_back("Eight" + (num[0] == "One" ? "y" : ""));
+                    } else {
+                        num.push_back(num[i / 10 - 1] + "ty");
+                    }
+                    break;
+                case 9:
+                    if (i % 10 != 0) {
+                        num.push_back("Nine" + (num[0] == "One" ? "y" : ""));
+                    } else {
+                        num.push_back(num[i / 10 - 1] + "ty");
+                    }
+                    break;
             }
+        } else if (i == 0) {
+            num.push_back("Zero");
         }
     }
+
+    vector<vector<string>> result = {num};
     return result;
 }
