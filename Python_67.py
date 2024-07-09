29ff5aa4e078e9792ebdf45cce408a1e77cbed53
beep boop <githubbot@vadim.me>
2024-07-09 07:51:44 +0200

    Solution 60 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n7aimvu6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/66da4d47-fd38-4ebe-a4bc-dbe56e6d1 566.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,6 +1,4 @@
-s = input("Enter a string: ")
-num_apples = (
-    int("".join(filter(str.isdigit, s)).lstrip("0") or 0)
-    if "".join(filter(str.isdigit, s))
-    else 0
-)
+def fruit_distribution(s, n):
+    s = s.lower()
+    num_apples = int("".join(filter(str.isdigit, s)).lstrip('0') or 0) if ''.join(filter(str.isdigit, s)) else 0
+    return n - num_apples
