bb84f98ebd97266531700d9c118d1bb3b7056ab8
beep boop <githubbot@vadim.me>
2024-07-09 12:47:46 +0200

    Solution 14 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/n5hu6g1u Prompt:       |                    ^~~ 3e89a18b-3313-4a16-9d77-99920d54bc57.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3e89a18b-3313-4a16-9d77-99920d54bc57.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3e89a18b-3313-4a16-9d77-99920d54bc57.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3e89a18b-3313-4a16-9d77-99920d54bc57.cpp:19: 3e89a18b-3313-4a16-9d77-99920d54bc57.cpp: In function ‘int main()’: 3e89a18b-3313-4a16-9d77-99920d54bc57.cpp:29:20: error: ‘make_a_pile’ was not dec lared in this scope    29 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 3e89a18b-3313-4a16-9d77-99920d54bc57.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <vector>
 #include <cassert>
 
@@ -13,7 +14,23 @@ bool issame(vector<int> a, vector<int> b) {
     return true;
 }
 
+vector<int> make_a_pile(int n) {
+    vector<int> pile;
+    int stones = n;
+    while (stones > 0) {
+        if (n % 2 == 1) {
+            pile.push_back(stones);
+            stones = stones + 1;
+        } else {
+            pile.push_back(stones);
+            stones = stones + 2;
+        }
+        n++;
+    }
+    return pile;
+}
+
 int main() {
-    assert(issame(make_a_pile(8), vector<int>({8, 10, 12, 14, 16, 18, 20, 22})));
+    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
     return 0;
 }
