0003bd978aa432c384963b13c75e2de9eeea3176
beep boop <githubbot@vadim.me>
2024-07-09 04:36:51 +0200

    Solution 46 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5ldlq6rg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/407e5e82-0094-4125-ade5-fede1edd1 b35.py", line 24, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/407e5e82-0094-4125-ade5-fede1edd1 b35.py", line 19, in check     sort_numbers("six five four three two one zero")   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/407e5e82-0094-4125-ade5-fede1edd1 b35.py", line 16, in sort_numbers     return " ".join(str(num_dict[i]) for i in sorted_nums)            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/407e5e82-0094-4125-ade5-fede1edd1 b35.py", line 16, in <genexpr>     return " ".join(str(num_dict[i]) for i in sorted_nums)                         ~~~~~~~~^^^ KeyError: 0

--- Python_19.py
+++ Python_19.py
@@ -1,4 +1,4 @@
 def sort_numbers(numbers: str) -> str:
-    nums = [int(num) if num.isdigit() else list(num_dict.keys()).index(num) for num in numbers.split()]
+    nums = [int(num_dict[num]) for num in numbers.split()]
     sorted_nums = sorted(nums)
     return " ".join(str(num) for num in sorted_nums)
