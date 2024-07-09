7509a56557a49a4494539dabd57acc0a917473bf
beep boop <githubbot@vadim.me>
2024-07-09 05:30:35 +0200

    Solution 48 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/mrra3n4c Prompt:       |                           ^~~~~~ 40673815-49da-4f97-bd3b-99e380a37b26.cpp:27:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 40673815-49da-4f97-bd3b-99e380a37b26.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 40673815-49da-4f97-bd3b-99e380a37b26.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 40673815-49da-4f97-bd3b-99e380a37b26.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 40673815-49da-4f97-bd3b-99e380a37b26.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 40673815-49da-4f97-bd3b-99e380a37b26.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -9,7 +9,7 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 std::vector<int> largest_smallest_integers(std::vector<int> lst) {
     int largest_negative = INT_MIN, smallest_positive = INT_MAX;
     for (int num : lst) {
-        if (num < 0 && num > largest_negative) {
+        if (num < 0 && num < largest_negative) {
             largest_negative = num;
         } else if (num > 0 && num < smallest_positive) {
             smallest_positive = num;
