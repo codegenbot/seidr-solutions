adaaea1a498f53191d2572f00e2dc93481954648
beep boop <githubbot@vadim.me>
2024-07-09 05:52:37 +0200

    Solution 86 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/za86kff2 Prompt: Program did not receive expected input

--- Python_1.py
+++ Python_1.py
@@ -1,4 +1,3 @@
-```
 from typing import List
 
 
@@ -9,7 +8,7 @@ def separate_paren_groups(paren_string: str) -> List[str]:
 
     for char in paren_string:
         if char == " ":
-            if temp: 
+            if temp:
                 result.append(temp)
                 temp = ""
         else:
@@ -28,10 +27,7 @@ def separate_paren_groups(paren_string: str) -> List[str]:
             else:
                 temp += char
 
-    if stack:
-        while stack:
-            temp += ")"
-            stack.pop()
-        result.append("(" + temp)
+    if temp:
+        result.append(temp)
 
     return [x for x in result]
