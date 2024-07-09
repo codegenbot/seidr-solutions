ccbad08f37046441d5ed958fb9e855f394ae7953
beep boop <githubbot@vadim.me>
2024-07-09 14:05:14 +0200

    Solution 14 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7dqgrwjn Prompt: , 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |     ^~~~~~ 1a57953c-eb62-40e0-8e7c-f05b1b420e10.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <algorithm>   +++ |+#include <cassert>     3 | 1a57953c-eb62-40e0-8e7c-f05b1b420e10.cpp: At global scope: 1a57953c-eb62-40e0-8e7c-f05b1b420e10.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 1a57953c-eb62-40e0-8e7c-f05b1b420e10.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~ In file included from 1a57953c-eb62-40e0-8e7c-f05b1b420e10.cpp:38: 1a57953c-eb62-40e0-8e7c-f05b1b420e10.cpp: In function ‘int main()’: 1a57953c-eb62-40e0-8e7c-f05b1b420e10.cpp:48:20: error: ‘sort_even’ was not decla red in this scope; did you mean ‘sortEven’?    48 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-bool isSame(vector<float> a,vector<float>b){
-    if(a.size() != b.size()) return false;
-    for(int i=0; i<a.size(); i++){
-        if(abs(a[i]) != abs(b[i])) return false;
+bool isSame(vector<float> a, vector<float> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (abs(a[i]) != abs(b[i])) return false;
     }
     return true;
 }
@@ -20,7 +20,7 @@ vector<float> sortEven(vector<float> l) {
             for(int k=i; k< l.size(); k=k+2){
                 result[j++] = l[k];
             }
-            sort(result.begin(),result.end());
+            sort(result.begin(), result.end());
             for(int i=0; i<4 && j<i+4;i++){
                 result[i] = 0;
             }
@@ -32,6 +32,6 @@ vector<float> sortEven(vector<float> l) {
 }
 
 int main() {
-    assert(isSame(sortEven({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    assert(isSame(sortEven({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
     return 0;
 }
