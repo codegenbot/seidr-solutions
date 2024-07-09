5b8fa11fc1851275181ab665624e3b1cc67be615
beep boop <githubbot@vadim.me>
2024-07-09 05:15:17 +0200

    Solution 92 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fv53j2kl Prompt: 138154b4-da7e-411b-8e81-3fd5e542c858.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,17 +1,28 @@
-#include <iostream>
-using namespace std;
+```cpp
+#include <boost/any.hpp>
+using namespace boost;
 
-int main() {
-    int num1, num2;
-    cout << "Enter first number: ";
-    cin >> num1;
-    cout << "Enter second number: ";
-    cin >> num2;
-    
-    if(num1 > num2)
-        cout << "First number is greater";
-    else
-        cout << "Second number is greater";
-    
-    return 0;
+boost::any compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return ((int)boost::any_cast<int>(a) > boost::any_cast<float>(b)) ? a : b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return ((int)boost::any_cast<int>(a) > boost::any_cast<double>(b)) ? a : b;
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(int)) {
+        std::string str = boost::any_cast<std::string>(a);
+        int num = boost::any_cast<int>(b);
+        return (std::stoi(str) > num) ? a : b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return ((float)boost::any_cast<float>(a) > (int)b) ? a : b;
+    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return ((double)boost::any_cast<double>(a) > (int)b) ? a : b;
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(float)) {
+        std::string str = boost::any_cast<std::string>(a);
+        float num = boost::any_cast<float>(b);
+        return (std::stof(str) > num) ? a : b;
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(double)) {
+        std::string str = boost::any_cast<std::string>(a);
+        double num = boost::any_cast<double>(b);
+        return (std::stod(str) > num) ? a : b;
+    }
+    return a;
 }
