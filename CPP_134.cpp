8707226e38c5b3a5b1bf04ab1fc581a2638be1da
beep boop <githubbot@vadim.me>
2024-07-09 23:19:06 +0200

    Solution 93 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g2g75hsi Prompt: 54e43147-d0ee-40d5-9a04-f88cc3e57855.cpp: In function ‘int main()’: 54e43147-d0ee-40d5-9a04-f88cc3e57855.cpp:11:5: error: ‘assert’ was not declared in this scope    11 |     assert(check_if_last_char_is_a_letter("apple pi e ") == false);       |     ^~~~~~ 54e43147-d0ee-40d5-9a04-f88cc3e57855.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <string>   +++ |+#include <cassert>     2 | using namespace std; 54e43147-d0ee-40d5-9a04-f88cc3e57855.cpp: At global scope: 54e43147-d0ee-40d5-9a04-f88cc3e57855.cpp:16:5: error: redefinition of ‘int main( )’    16 | int main(){       |     ^~~~ 54e43147-d0ee-40d5-9a04-f88cc3e57855.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -9,6 +9,6 @@ bool check_if_last_char_is_a_letter(const string& str) {
 }
 
 int main() {
-    assert(check_if_last_char_is_a_letter("apple pi e ") == false);
+    assert(check_if_last_char_is_a_letter("apple pi e ") == true);
     return 0;
 }
