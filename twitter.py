2aa547e4776a1bb6f1a35c7196ad8a458de9dda0
beep boop <githubbot@vadim.me>
2024-07-09 05:03:44 +0200

    Solution 65 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/kih5li8d Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d1c424b3-b2c6-4eca-a7e2-cc0a1a17a 83c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- twitter.py
+++ twitter.py
@@ -1,16 +1,14 @@
-Here is the modified code:
-
-print( "Your tweet has {len(tweet)} characters" )
+```
 def validate_tweet(tweet):
     if len(tweet) > 140:
         return "Too many characters"
     elif len(tweet) == 0:
         return "You didn't type anything"
     else:
-        return "Your tweet has {} characters".format(len(tweet))
+        return f"Your tweet has {len(tweet)} characters"
 
-print(validate_tweet(" "))
-print(validate_tweet("1"))
-print(validate_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeat it"))
-print(validate_tweet("40172875*\\(&\\,%^^*!@#\\$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~"))
-print(validate_tweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1"))
+print("Your tweet has {} characters".format(validate_tweet(" ")))
+print("Your tweet has {} characters".format(validate_tweet("1")))
+print("Your tweet has {} characters".format(validate_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeat it")))
+print("Your tweet has {} characters".format(validate_tweet("40172875*\\(&\\,%^^*!@#\\$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~")))
+print("Your tweet has {} characters".format(validate_twitter("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1")))
