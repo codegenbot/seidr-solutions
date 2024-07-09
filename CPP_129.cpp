474c4b4f0ef4ee3eba8411eb84cab277013e1ed1
beep boop <githubbot@vadim.me>
2024-07-09 22:18:23 +0200

    Solution 22 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fjjhgnby Prompt:    41 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:41:34: error: expected primary-expressi on before ‘int’    41 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:41:39: error: expression list treated a s compound expression in initializer [-fpermissive]    41 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:40: 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp: In function ‘int main()’: 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:50:20: error: ‘minPath’ was not declare d in this scope    50 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -36,3 +36,13 @@ vector<int> minPath(vector<vector<int>>& grid, int k) {
 
     return res;
 }
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
+            return false;
+    }
+    return true;
+}
