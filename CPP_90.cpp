6a552fb512751bb94d93f7b2ca056dd24b3a44e0
beep boop <githubbot@vadim.me>
2024-07-09 16:09:10 +0200

    Solution 10 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/f9cvv764 Prompt: 3f80b046-340e-47f4-93d5-618159bd417b.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ 3f80b046-340e-47f4-93d5-618159bd417b.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -18,4 +18,5 @@ int main() {
     std::vector<int> lst = { -35, 34, 12, -45 };
     int result = next_smallest(lst);
     std::cout << "The smallest number greater than all elements in the list is: " << result << std::endl;
+    return 0;
 }
