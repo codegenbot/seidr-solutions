eba21891449e8f0f23b71f534cc5a7b3b7b7f97a
beep boop <githubbot@vadim.me>
2024-07-09 23:05:59 +0200

    Solution 48 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/zt4qvmez Prompt: 74790743-fe6e-4ef6-9295-935ee31197d8.cpp: In function ‘std::string string_to_md5 (std::string)’: 74790743-fe6e-4ef6-9295-935ee31197d8.cpp:12:16: error: aggregate ‘EVP_MD_CTX md’  has incomplete type and cannot be defined    12 |     EVP_MD_CTX md;       |                ^~ 74790743-fe6e-4ef6-9295-935ee31197d8.cpp:14:26: error: ‘MD5_DIGEST_LENGTH’ was n ot declared in this scope    14 |     unsigned char result[MD5_DIGEST_LENGTH];       |                          ^~~~~~~~~~~~~~~~~ 74790743-fe6e-4ef6-9295-935ee31197d8.cpp:17:30: error: ‘result’ was not declared  in this scope    17 |     EVP_DigestFinal_CTX(&md, result, NULL);       |                              ^~~~~~ 74790743-fe6e-4ef6-9295-935ee31197d8.cpp:17:5: error: ‘EVP_DigestFinal_CTX’ was not declared in this scope; did you mean ‘EVP_DigestFinal_ex’?    17 |     EVP_DigestFinal_CTX(&md, result, NULL);       |     ^~~~~~~~~~~~~~~~~~~       |     EVP_DigestFinal_ex

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,5 +1,10 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <openssl/ssl.h>
 #include <openssl/evp.h>
-#include <openssl/md5.h>
+#include <openssl/err.h>
 
 using namespace std;
 
@@ -8,13 +13,13 @@ string string_to_md5(string text) {
 
     EVP_MD_CTX md;
     EVP_MD_CTX_init(&md);
-    unsigned char result[MD5_DIGEST_LENGTH];
+    unsigned char result[EVP_MAX_MD_SIZE];
 
-    EVP_DigestUpdate(&md, text.c_str(), text.size());
+    EVP_DigestUpdate(&md, (unsigned char*)text.c_str(), text.size());
     EVP_DigestFinal_CTX(&md, result, NULL);
 
     stringstream ss;
-    for(int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+    for(int i = 0; i < EVP_MD_SIZE(EVP_md5()); i++) {
         ss << setfill('0') << setw(2) << hex << (int)result[i];
     }
 
