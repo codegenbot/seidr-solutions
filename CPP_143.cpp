2100067e53025a57351ae8f6b4c033cfef162ed0
beep boop <githubbot@vadim.me>
2024-07-09 11:07:25 +0200

    Solution 16 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4icgo1mz Prompt: a2281118-0c99-4fa6-983d-8097fbb9934c.cpp:4:1: error: ‘string’ does not name a ty pe     4 | string words_in_sentence(string sentence) {       | ^~~~~~ a2281118-0c99-4fa6-983d-8097fbb9934c.cpp:25:1: error: ‘vector’ does not name a t ype    25 | vector<string> split(const string& str, char c) {       | ^~~~~~ In file included from a2281118-0c99-4fa6-983d-8097fbb9934c.cpp:36: a2281118-0c99-4fa6-983d-8097fbb9934c.cpp: In function ‘int main()’: a2281118-0c99-4fa6-983d-8097fbb9934c.cpp:38:13: error: ‘words_in_sentence’ was n ot declared in this scope    38 |     assert (words_in_sentence("here is") == "is");       |             ^~~~~~~~~~~~~~~~~

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,31 +1,34 @@
-string words_in_sentence(string sentence){
-    string result = "";
-    for(auto word : split(sentence, " ")){
-        if(is_prime(word.length())){
-            result += word + " ";
-        }
+#include <string>
+#include <vector>
+
+std::string words_in_sentence(std::string sentence) {
+    std::vector<int> primeLengths;
+    for (int i = 2; i <= sentence.size(); i++) {
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; j++)
+            if (i % j == 0)
+                isPrime = false;
+        if (isPrime)
+            primeLengths.push_back(i);
     }
+    
+    std::string result = "";
+    std::vector<std::string> words = split(sentence, ' ');
+    for (std::string word : words) {
+        if (find(primeLengths.begin(), primeLengths.end(), word.size()) != primeLengths.end())
+            result += word + " ";
+    }
+    
     return result.substr(0, result.size() - 1);
 }
 
-bool is_prime(int num) {
-    if(num <= 1)
-        return false;
-    for(int i = 2; i*i <= num; i++) {
-        if(num % i == 0)
-            return false;
-    }
-    return true;
-}
-
-vector<string> split(string sentence, string delimiter) {
-    vector<string> result;
+std::vector<std::string> split(const std::string& str, char c) {
+    std::vector<std::string> tokens;
     size_t pos = 0;
-    while((pos = sentence.find(delimiter)) != string::npos) {
-        result.push_back(sentence.substr(0, pos));
-        sentence.erase(0, pos + delimiter.length());
+    while ((pos = str.find(c)) != std::string::npos) {
+        tokens.push_back(str.substr(0, pos));
+        str.erase(0, pos + 1);
     }
-    if (!sentence.empty())
-        result.push_back(sentence);
-    return result;
+    tokens.push_back(str);
+    return tokens;
 }
