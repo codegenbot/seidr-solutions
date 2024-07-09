2c77be80d864a567ae6cb42fdf6a11c60332f347
beep boop <githubbot@vadim.me>
2024-07-10 01:36:35 +0200

    Solution 14 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uzrnpb22 Prompt:     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 44931ac9-2d2f-49a8-b936-e7ebcb74ee89.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 44931ac9-2d2f-49a8-b936-e7ebcb74ee89.cpp:9:39: error: expression list treated as  compound expression in initializer [-fpermissive]     9 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 44931ac9-2d2f-49a8-b936-e7ebcb74ee89.cpp:8: 44931ac9-2d2f-49a8-b936-e7ebcb74ee89.cpp: In function ‘int main()’: 44931ac9-2d2f-49a8-b936-e7ebcb74ee89.cpp:18:20: error: ‘unique’ was not declared  in this scope    18 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 44931ac9-2d2f-49a8-b936-e7ebcb74ee89.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,27 +1,10 @@
-```cpp
 #include <algorithm>
 #include <vector>
 #include <iostream>
 
-bool issame(vector<int> a, vector<int>b){
-    return (a == b);
-}
-
-int main() {
-    vector<int> input;
-    int n;
-    cin >> n;
-    for(int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        input.push_back(x);
-    }
-    
-    auto unique_end = unique(input.begin(), input.end());
-    vector<int> output(input.begin(), unique_end);
-    sort(output.begin(), output.end());
-    
-    for (int i : output) {
-        cout << i << " ";
-    }
+auto unique_end = std::unique(input.begin(), input.end());
+std::vector<int> output(input.begin(), unique_end);
+std::sort(output.begin(), output.end());
+for (int i : output) {
+    std::cout << i << " ";
 }
