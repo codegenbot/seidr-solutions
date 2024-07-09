54b46f37a69e27c0c9b69283b17395292dc6aba8
beep boop <githubbot@vadim.me>
2024-07-09 11:31:15 +0200

    Solution 3 of CPP/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/bsrzxj5v Prompt: a8932e0a-89e3-4446-8fad-0498e862f350.cpp:3:17: error: ‘vector’ was not declared in this scope     3 | bool below_zero(vector<int> operations){       |                 ^~~~~~ a8932e0a-89e3-4446-8fad-0498e862f350.cpp:3:17: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from a8932e0a-89e3-4446-8fad-0498e862f350.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from a8932e0a-89e3-4446-8fad-0498e862f350.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a8932e0a-89e3-4446-8fad-0498e862f350.cpp:3:24: error: expected primary-expressio n before ‘int’     3 | bool below_zero(vector<int> operations){       |                        ^~~

--- CPP_3.cpp
+++ CPP_3.cpp
@@ -1,16 +1,7 @@
-#include <iostream>
-#include <vector>
-
-int below_zero(std::vector<int> operations){
+bool below_zero(std::vector<int> operations){
     int balance = 0;
     for(int operation : operations){
         balance += operation;
-        if(balance < 0) return 1;
+        if(balance < 0) return true;
     }
-    return 0;
-}
-
-int main() {
-    assert(below_zero({1, -2, 2, -2, 5, -5, 4, -4}) == 1);
-    return 0;
-}
+    return false;
