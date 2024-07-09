46f0fd3f615d12c86e3ff1eab6176fdd0ff4792f
beep boop <githubbot@vadim.me>
2024-07-09 02:18:50 +0200

    Solution 72 of CPP/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/vmm4yi2z Prompt:    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bcf577a7-7cd2-49f3-a639-184a69599c8f.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bcf577a7-7cd2-49f3-a639-184a69599c8f.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from bcf577a7-7cd2-49f3-a639-184a69599c8f.cpp:9: bcf577a7-7cd2-49f3-a639-184a69599c8f.cpp: In function ‘int main()’: bcf577a7-7cd2-49f3-a639-184a69599c8f.cpp:19:20: error: ‘words_string’ was not de clared in this scope    19 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |                    ^~~~~~~~~~~~ bcf577a7-7cd2-49f3-a639-184a69599c8f.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~

--- CPP_101.cpp
+++ CPP_101.cpp
@@ -1,15 +1,29 @@
 #include <algorithm>
-bool isSameVectors(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
+#include <vector>
+#include <string>
+
+bool issame(vector<string> a,vector<string>b){
+    return (a==b);
 }
 
-int main() {
+int testMain() {
     vector<string> v1 = splitString("ahmed     , gamal");
     sort(v1.begin(), v1.end()); 
-    assert(isSameVectors(v1, {"ahmed", "gamal"}));
+    assert(issame(v1, {"ahmed", "gamal"}));
     return 0;
+}
+
+vector<string> words_string(string s) {
+    vector<string> tokens;
+    size_t start = 0, end;
+    do {
+        end = s.find(" ", start);
+        if (end == string::npos) {
+            tokens.push_back(s.substr(start));
+            break;
+        }
+        tokens.push_back(s.substr(start, end - start));
+        start = end + 1;
+    } while (1);
+    return tokens;
 }
