7b6915f6abac327de6db8a07368b27dc8261d36e
beep boop <githubbot@vadim.me>
2024-07-09 10:21:15 +0200

    Solution 6 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/289d4dlu Prompt: e3c97642-4f90-4e35-892e-f202c7df0490.cpp: In function ‘bool simplify(std::string , std::string)’: e3c97642-4f90-4e35-892e-f202c7df0490.cpp:13:39: error: request for member ‘c_str ’ in ‘arr2[1]’, which is of non-class type ‘char’    13 |     return (atoi(arr1) * atoi(arr2[1].c_str()) == atoi(arr2[0].c_str()) * atoi(&slash + 1));       |                                       ^~~~~ e3c97642-4f90-4e35-892e-f202c7df0490.cpp:13:64: error: request for member ‘c_str ’ in ‘arr2[0]’, which is of non-class type ‘char’    13 |  return (atoi(arr1) * atoi(arr2[1].c_str()) == atoi(arr2[0].c_str()) * a toi(&slash + 1));       |                                                             ^~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,11 +1,1 @@
-```cpp
-bool simplify(string x, string n) {
-    char arr1[6], arr2[4];
-    stringstream s1(x), s2(n);
-    s1 >> arr1;
-    char slash;
-    s2 >> arr2[0] >> slash >> arr2[1];
-
-    return (atoi(arr1) * atoi(std::string(1, arr2[1]).c_str()) == 
-           atoi(std::string(1, arr2[0]).c_str()) * atoi(&slash + 1));
-}
+return (atoi(arr1) * std::stoi(std::string(1, arr2[1])) == std::stoi(std::string(1, arr2[0])) * (slash - '0'));
