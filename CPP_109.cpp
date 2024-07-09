495cb2506d85622ee34dd22bf8b22585b7880c3f
beep boop <githubbot@vadim.me>
2024-07-09 03:54:42 +0200

    Solution 1 of CPP/109, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2snjoxbp Prompt: Complete the following code given the task description and function signature.

--- CPP_109.cpp
+++ CPP_109.cpp
@@ -1,9 +1,7 @@
 bool move_one_ball(vector<int> arr) {
-    int n = arr.size();
-    for (int i = 0; i < n - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        }
+    int minElement = *min_element(arr.begin(), arr.end());
+    for (int i : arr) {
+        if (i != minElement) return false;
     }
     return true;
 }
