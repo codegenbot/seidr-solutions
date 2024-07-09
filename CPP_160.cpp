1aa7f8f7c3475c946f2377d8ec702c59879f89fa
beep boop <githubbot@vadim.me>
2024-07-09 20:17:07 +0200

    Solution 10 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8tmp81k3 Prompt:                  from /usr/include/c++/11/vector:60,                  from 9bef9d4c-b1f0-4b2c-8873-5e2156a06aa3.cpp:1: /usr/include/c++/11/bits/stl_pair.h:211:12: note: ‘std::pair’ declared here   211 |     struct pair       |            ^~~~ 9bef9d4c-b1f0-4b2c-8873-5e2156a06aa3.cpp:4:28: error: expected primary-expressio n before ‘char’     4 | int do_algebra(vector<pair<char, int>> operators_and_operands) {       |                            ^~~~ 9bef9d4c-b1f0-4b2c-8873-5e2156a06aa3.cpp:4:34: error: expected primary-expressio n before ‘int’     4 | int do_algebra(vector<pair<char, int>> operators_and_operands) {       |                                  ^~~ 9bef9d4c-b1f0-4b2c-8873-5e2156a06aa3.cpp:4:62: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | int do_algebra(vector<pair<char, int>> operators_and_operands) {       |                                                              ^ In file included from 9bef9d4c-b1f0-4b2c-8873-5e2156a06aa3.cpp:22: 9bef9d4c-b1f0-4b2c-8873-5e2156a06aa3.cpp: In function ‘int main()’: 9bef9d4c-b1f0-4b2c-8873-5e2156a06aa3.cpp:24:23: error: ‘do_algebra’ cannot be us ed as a function    24 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,20 +1,24 @@
-#include <vector>
-#include <cmath>
-using namespace std;
+#include <utility>
 
-int do_algebra(vector<string> operator_, vector<int> operand) {
-    int result = operand[0];
-    for (int i = 0; i < operator_.size(); i++) {
-        if (operator_[i] == "+") {
-            result += operand[i + 1];
-        } else if (operator_[i] == "-") {
-            result -= operand[i + 1];
-        } else if (operator_[i] == "*") {
-            result *= operand[i + 1];
-        } else if (operator_[i] == "//" || operator_[i] == "/") { // fix integer division
-            result /= operand[i + 1];
-        } else if (operator_[i] == "**") {
-            result = pow(result, operand[i + 1]);
+int do_algebra(vector<pair<char, int>> operators_and_operands) {
+    if (operators_and_operands.size() == 0) {
+        return 0;
+    }
+    
+    int result = operators_and_operands[0].second;
+    for (const auto& pair : operators_and_operands) {
+        if (pair.first == '+') {
+            result += pair.second;
+        } else if (pair.first == '-') {
+            result -= pair.second;
+        } else if (pair.first == '*') {
+            result *= pair.second;
+        } else if (pair.first == '/') {
+            if (pair.second != 0) {
+                result /= pair.second;
+            }
+        } else if (pair.first == '**') {
+            result = pow(result, pair.second);
         }
     }
     return result;
