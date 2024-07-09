d54c19ba92a47bf31b2db629b3e4f91b326bed9e
beep boop <githubbot@vadim.me>
2024-07-09 11:42:44 +0200

    Solution 12 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/1hgofiwv Prompt:    21 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 4ac6d7c9-0ca5-4193-85d6-8b235ffc2403.cpp:21:36: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 4ac6d7c9-0ca5-4193-85d6-8b235ffc2403.cpp:21:43: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 4ac6d7c9-0ca5-4193-85d6-8b235ffc2403.cpp:20: 4ac6d7c9-0ca5-4193-85d6-8b235ffc2403.cpp: In function ‘int main()’: 4ac6d7c9-0ca5-4193-85d6-8b235ffc2403.cpp:30:20: error: ‘sort_even’ was not decla red in this scope    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 4ac6d7c9-0ca5-4193-85d6-8b235ffc2403.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,23 +1,11 @@
-```cpp
-#include <vector>
-#include <algorithm>
-
-bool issame(std::vector<float> a, std::vector<float> b) {
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(abs(a[i] - b[i]) > 1e-9) return false;
-    }
-    return true;
-}
-
-std::vector<float> sort_even(std::vector<float> l) {
-    std::vector<float> result(l.size());
+vector<float> sort_even(vector<float> l) {
+    vector<float> result(l.size());
     for(int i = 0; i < l.size(); i++) {
         if(i % 2 == 0) {
-            std::vector<float> temp;
-            for(float x : l) {
-                if(x % 2.0 == 0.0) {
-                    temp.push_back(x);
+            vector<float> temp;
+            for(int j = 0; j < l.size(); j++) {
+                if(j % 2 == 0) {
+                    temp.push_back(l[j]);
                 }
             }
             sort(temp.begin(), temp.end());
@@ -27,4 +15,12 @@ std::vector<float> sort_even(std::vector<float> l) {
         }
     }
     return result;
+}
+
+bool issame(vector<float> a, vector<float> b) {
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(abs(a[i] - b[i]) > 1e-9) return false;
+    }
+    return true;
 }
