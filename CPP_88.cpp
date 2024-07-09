c13a9c18e81a8163978349385987434a5d2489bd
beep boop <githubbot@vadim.me>
2024-07-09 03:48:21 +0200

    Solution 10 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/g6w1o3fa Prompt:       |                    ^~~ c0275c8f-68a4-4b79-ae00-5edae4f50d76.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c0275c8f-68a4-4b79-ae00-5edae4f50d76.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c0275c8f-68a4-4b79-ae00-5edae4f50d76.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c0275c8f-68a4-4b79-ae00-5edae4f50d76.cpp:10: c0275c8f-68a4-4b79-ae00-5edae4f50d76.cpp: In function ‘int main()’: c0275c8f-68a4-4b79-ae00-5edae4f50d76.cpp:20:20: error: ‘sort_array’ was not decl ared in this scope    20 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ c0275c8f-68a4-4b79-ae00-5edae4f50d76.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -5,16 +5,16 @@ bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i < a.size(); i++)
-        if (a[i] != b[b.size()-i-1])
+        if (a[i] != b[i])
             return false;
     return true;
 }
 
-vector<vector<int>> sort_array(vector<int> array) {
+vector<int> sort_array(vector<int> array) {
     int first = array.empty() ? 0 : array[0];
     int last = array.empty() ? 0 : array.back();
     if ((first + last) % 2 == 1)
-        return {array};
+        return vector<int>(array);
     else
-        return {{array.rbegin(), array.rend()}};
+        return vector<int>(array.rbegin(), array.rend());
 }
