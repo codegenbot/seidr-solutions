354ebb5c68ebda3b098e8ba32d5e13a971138d8a
beep boop <githubbot@vadim.me>
2024-07-09 10:13:31 +0200

    Solution 7 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/upzrl8t8 Prompt:       |                              ^~~~~~ 987b42ea-f4fb-436c-aa13-6d70d44a0cd8.cpp:22:37: error: ‘string’ was not declared  in this scope    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 987b42ea-f4fb-436c-aa13-6d70d44a0cd8.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 987b42ea-f4fb-436c-aa13-6d70d44a0cd8.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 987b42ea-f4fb-436c-aa13-6d70d44a0cd8.cpp:21: 987b42ea-f4fb-436c-aa13-6d70d44a0cd8.cpp: In function ‘int main()’: 987b42ea-f4fb-436c-aa13-6d70d44a0cd8.cpp:31:20: error: ‘by_length’ was not decla red in this scope    31 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 987b42ea-f4fb-436c-aa13-6d70d44a0cd8.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -3,16 +3,8 @@
 
 using namespace std;
 
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
 }
 
 vector<string> by_length(vector<int> arr){
@@ -37,4 +29,5 @@ vector<string> by_length(vector<int> arr){
 
 int main() {
     assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
+    return 0;
 }
