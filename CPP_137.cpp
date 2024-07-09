63a0600f3fe8cd3804757fa6b78ba6c542a1b0a1
beep boop <githubbot@vadim.me>
2024-07-10 00:44:39 +0200

    Solution 40 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fhsevhcx Prompt: 3d3b714b-9956-4596-9038-b13f5da2dcd2.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,16 +1,16 @@
 #include <boost/any.hpp>
-#include <boost/type_traits.hpp>
+#include <boost/type_index.hpp>
 
 using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_same<any_tag, get_type(a)>() && is_same<any_tag, get_type(b)>()) {
+    if (get_type(a) == typeid(any_tag) && get_type(b) == typeid(any_tag)) {
         return a > b ? a : b;
-    } else if (is_same<string_any_tag, get_type(a)>() && is_same<string_any_tag, get_type(b)>()) {
+    } else if (get_type(a) == typeid(string_any_tag) && get_type(b) == typeid(string_any_tag)) {
         string str1 = any_cast<string>(a);
         string str2 = any_cast<string>(b);
         return str1 > str2 ? a : b;
-    } else if (is_same<string_any_tag, get_type(a)>() || is_same<string_any_tag, get_type(b)>()) {
+    } else if ((get_type(a) == typeid(string_any_tag) || get_type(b) == typeid(string_any_tag))) {
         string str1 = any_cast<string>(a);
         string str2 = any_cast<string>(b);
         if (str1 == str2) {
