c03fac1322f9d8f7a478b36624ce6a23d82a19e7
beep boop <githubbot@vadim.me>
2024-07-09 12:28:20 +0200

    Solution 80 of CPP/58, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/w26ru0dn Prompt: c9aef725-f0d5-4a02-9aaa-8b26194e26fc.cpp:3:1: note: ‘std::set’ is defined in hea der ‘<set>’; did you forget to ‘#include <set>’?     2 | #include<algorithm>   +++ |+#include <set>     3 | using namespace std; c9aef725-f0d5-4a02-9aaa-8b26194e26fc.cpp:7:9: error: expected primary-expression  before ‘int’     7 |     set<int> s1(l1.begin(),l1.end());       |         ^~~ c9aef725-f0d5-4a02-9aaa-8b26194e26fc.cpp:8:9: error: expected primary-expression  before ‘int’     8 |     set<int> s2(l2.begin(),l2.end());       |         ^~~ c9aef725-f0d5-4a02-9aaa-8b26194e26fc.cpp:10:19: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    10 |     for(auto it = s1.begin(); it != s1.end(); ++it) {       |                   ^~       |                   l1 c9aef725-f0d5-4a02-9aaa-8b26194e26fc.cpp:11:13: error: ‘s2’ was not declared in this scope; did you mean ‘l2’?    11 |         if (s2.find(*it) != s2.end()) {       |             ^~       |             l2

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,11 +1,19 @@
+#include<vector>
+#include<set>
+#include<algorithm>
+using namespace std;
+
 vector<int> common(vector<int> l1, vector<int> l2) {
+    vector<int> result;
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
-    set<int> intersection;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-        inserter(intersection, intersection.begin()));
+    for(auto it = l1.begin(); it != l1.end(); ++it) {
+        if (s2.find(*it) != s2.end()) {
+            result.push_back(*it);
+        }
+    }
 
-    vector<int> result(intersection.begin(), intersection.end());
+    sort(result.begin(), result.end());
     return result;
 }
