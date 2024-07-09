ab07192640f4167550dc693694e1ceefea39479d
beep boop <githubbot@vadim.me>
2024-07-09 04:31:58 +0200

    Solution 2 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/mrra3n4c Prompt:    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fc72d16e-cc89-4896-94b7-eaa48a7064f0.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fc72d16e-cc89-4896-94b7-eaa48a7064f0.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fc72d16e-cc89-4896-94b7-eaa48a7064f0.cpp:13: fc72d16e-cc89-4896-94b7-eaa48a7064f0.cpp: In function ‘int main()’: fc72d16e-cc89-4896-94b7-eaa48a7064f0.cpp:23:20: error: ‘largest_smallest_integer s’ was not declared in this scope    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ fc72d16e-cc89-4896-94b7-eaa48a7064f0.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,10 +1,13 @@
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
+
 vector<int> largest_smallest_integers(vector<int> lst){
     int largest_negative = 0, smallest_positive = 0;
-    for(int num : lst){
-        if(num < 0 && num < largest_negative){
+    for (int num : lst) {
+        if (num < 0 && num < largest_negative) {
             largest_negative = num;
-        }
-        if(num > 0 && (num < smallest_positive || smallest_positive == 0)){
+        } else if (num > 0 && (num < smallest_positive || smallest_positive == 0)) {
             smallest_positive = num;
         }
     }
