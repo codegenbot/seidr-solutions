86006702769d1f2c22449f948f7ec86162f24907
beep boop <githubbot@vadim.me>
2024-07-09 04:58:43 +0200

    Solution 82 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kl0f1ktf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f2de1b0-ee44-4443-81d6-e347fce4c 311.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,8 +1,4 @@
 def add(a=0, b=0): 
-    while True:  
-        x = int(input(f"Enter the first number: {a} "))
-        y = int(input(f"Enter the second number: {b} "))
-        print(x + y)
-        response = input("Do you want to continue? (y/n) ")
-        if response.lower() != 'y':
-            break
+    x = int(input(f"Enter the first number: {a} " + str(a) if not a else '')) or str(a) 
+    y = int(input(f"Enter the second number: {b} " + str(b) if not b else '')) or str(b) 
+    print(x + y)
