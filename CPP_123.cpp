a742471fc5d2f2a07ced26a1748bd0c3a18624ad
beep boop <githubbot@vadim.me>
2024-07-09 02:49:53 +0200

    Solution 90 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c0h3vdup Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 0f7b554e-e697-4f3b-9c96-976d1595fd40.cpp:24:27:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,33 +1,15 @@
-#include <vector>
-#include <iostream>
-
 std::vector<int> get_odd_collatz(int num) {
     std::vector<int> sequence;
-    while (num != 1) {
-        if (num % 2 == 0)
+    while(num != 1) {
+        if(num % 2 == 0)
             num = num / 2;
         else
             num = 3 * num + 1;
         sequence.push_back(num);
     }
+    sequence.push_back(1); 
+    for(int i : sequence)
+        std::cout << i << " ";
+    std::cout << std::endl;
     return sequence;
-}
-
-bool sequenceEqual(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size())
-        return false;
-    
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i])
-            return false;
-    }
-    
-    return true;
-}
-
-int main() {
-    int num;
-    std::cin >> num;
-    std::vector<int> result = get_odd_collatz(num);
-    assert(sequenceEqual(result, {1}));  
 }
