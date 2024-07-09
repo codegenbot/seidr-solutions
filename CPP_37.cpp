3e14f0892c7bbb036c767336af61b6cf805d6207
beep boop <githubbot@vadim.me>
2024-07-09 20:52:55 +0200

    Solution 0 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/juk3nsjv Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_37.cpp
@@ -0,0 +1,13 @@
+vector<float> sort_even(vector<float> l){
+    vector<float> l_prime;
+    for(int i = 0; i < l.size(); i++){
+        if(i % 2 == 0){
+            vector<float> even(l.begin() + i, l.begin() + i + 1);
+            sort(even.begin(), even.end());
+            l_prime.push_back(even[0]);
+        }else{
+            l_prime.push_back(l[i]);
+        }
+    }
+    return l_prime;
+}
