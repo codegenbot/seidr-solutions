139277a8f5ebb23d237eb348d35fc62751a43de1
beep boop <githubbot@vadim.me>
2024-07-09 03:16:42 +0200

    Solution 8 of Python/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/fkeqrnh3 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5c09be3d-1171-45f2-9e29-021648122 4b7.py", line 16, in <module>     check(Strongest_Extension)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5c09be3d-1171-45f2-9e29-021648122 4b7.py", line 13, in check     assert Strongest_Extension("Sp", ["671235", "Bb"]) == "Sp.671235"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_153.py
+++ Python_153.py
@@ -1,9 +1,13 @@
 def Strongest_Extension(class_name, extensions):
-    strongest = (None, 0)
+    max_strength = 0
+    strongest_extensions = []
     for extension in extensions:
         cap = sum(1 for c in extension if c.isupper())
         sm = len(extension) - cap
         strength = cap - sm
-        if strength > strongest[1]:
-            strongest = ((extension), strength)
-    return f"{class_name}.{strongest[0][0]}"
+        if strength > max_strength:
+            max_strength = strength
+            strongest_extensions = [(extension),]
+        elif strength == max_strength:
+            strongest_extensions.append((extension,))
+    return f"{class_name}.{','.join(map(lambda x: x[0], strongest_extensions))}"
