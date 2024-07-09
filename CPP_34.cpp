12f17a6b57bfa0779df65c6191fd117bf6234682
beep boop <githubbot@vadim.me>
2024-07-09 07:45:49 +0200

    Solution 40 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uf0b9htf Prompt:                  from /usr/include/c++/11/algorithm:74,                  from c5621b5d-0614-4a18-8ff7-97ed5d3e714d.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c5621b5d-0614-4a18-8ff7-97ed5d3e714d.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from c5621b5d-0614-4a18-8ff7-97ed5d3e714d.cpp:14: c5621b5d-0614-4a18-8ff7-97ed5d3e714d.cpp: In function ‘int main()’: c5621b5d-0614-4a18-8ff7-97ed5d3e714d.cpp:24:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    24 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from c5621b5d-0614-4a18-8ff7-97ed5d3e714d.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,20 +1,12 @@
 #include <algorithm>
 #include <vector>
-#include <cassert>
 
-using namespace std;
-
-bool issame(const vector<int>& a, const vector<int>& b) {
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
-vector<int> removeDuplicates(vector<int>& l) {
-    sort(l.begin(), l.end());
-    l.erase(unique(l.begin(), l.end()), l.end());
+std::vector<int> removeDuplicates(std::vector<int>& l) {
+    std::sort(l.begin(), l.end());
+    l.erase(std::unique(l.begin(), l.end()), l.end());
     return l;
-}
-
-int main() {
-    assert(issame(removeDuplicates({5, 3, 5, 2, 3, 3, 9, 0, 123}), {0, 2, 3, 5, 9, 123}));
-    return 0;
 }
