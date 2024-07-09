693a1ca82c2baeba659e81d95b7e1ee33702fca0
beep boop <githubbot@vadim.me>
2024-07-09 04:03:21 +0200

    Solution 22 of CPP/109, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2snjoxbp Prompt:                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 6d12e1e6-dd70-473a-8449-897db097ce12.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 6d12e1e6-dd70-473a-8449-897db097ce12.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 6d12e1e6-dd70-473a-8449-897db097ce12.cpp:3:27: error: expected primary-expressio n before ‘int’     3 | bool move_one_ball(vector<int> arr) {       |                           ^~~ In file included from 6d12e1e6-dd70-473a-8449-897db097ce12.cpp:18: 6d12e1e6-dd70-473a-8449-897db097ce12.cpp: In function ‘int main()’: 6d12e1e6-dd70-473a-8449-897db097ce12.cpp:20:26: error: ‘move_one_ball’ cannot be  used as a function    20 |     assert (move_one_ball({})==true);       |             ~~~~~~~~~~~~~^~~~

--- CPP_109.cpp
+++ CPP_109.cpp
@@ -1,3 +1,4 @@
+```
 #include <iostream>
 #include <vector>
 
@@ -5,40 +6,36 @@ bool move_one_ball(std::vector<int> arr) {
     int n = arr.size();
     if (n == 0) return true;
     
-    std::vector<int> temp = arr;
-    for (int i = 0; i < n; ++i) {
-        bool changed = false;
+    bool flag = false;
+    while(true){
+        std::vector<int> temp = arr;
         sort(temp.begin(), temp.end());
         if (temp != arr) {
-            int j = 0;
-            while (j < n - 1 && !changed) {
-                if (temp[j] > temp[n - 1]) {
-                    swap(temp[j], temp[n - 1]);
-                    changed = true;
-                }
-                ++j;
+            for(int i=0; i<n-1; ++i){
+                swap(temp[i], temp[n - 1]);
             }
+            flag = true;
+        }else{
+            return flag;
         }
     }
-    return false;
 }
 
 int main() {
-    std::vector<int> arr;
     int n;
     std::cout << "Enter the number of elements: ";
     std::cin >> n;
-    for (int i = 0; i < n; ++i) {
-        int num;
-        std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> num;
-        arr.push_back(num);
+    
+    std::vector<int> arr(n);
+    for(int i=0; i<n; ++i){
+        std::cout << "Enter element " << i+1 << ": ";
+        std::cin >> arr[i];
     }
     
-    if (move_one_ball(arr)) {
-        std::cout << "You can move one ball to make the array sorted.\n";
-    } else {
-        std::cout << "You cannot move one ball to make the array sorted.\n";
+    if(move_one_ball(arr)){
+        std::cout << "The array can be moved to a sorted state by moving one ball.\n";
+    }else{
+        std::cout << "The array cannot be moved to a sorted state by moving one ball.\n";
     }
     
     return 0;
