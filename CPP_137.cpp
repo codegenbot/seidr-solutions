53129dd7fa59e7d58e507bdc6595c537db0b6db5
beep boop <githubbot@vadim.me>
2024-07-09 12:15:11 +0200

    Solution 53 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/g7hbox03 Prompt: 14ad1b82-87c6-4849-a777-35228c227a62.cpp:3:1: error: stray ‘`’ in program     3 | ```cpp       | ^ 14ad1b82-87c6-4849-a777-35228c227a62.cpp:3:2: error: stray ‘`’ in program     3 | ```cpp       |  ^ 14ad1b82-87c6-4849-a777-35228c227a62.cpp:3:3: error: stray ‘`’ in program     3 | ```cpp       |   ^ 14ad1b82-87c6-4849-a777-35228c227a62.cpp:7:9: fatal error: boost/any.hpp: No suc h file or directory     7 | #include<boost/any.hpp>       |         ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,7 +1,6 @@
 ```cpp
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <boost/any.hpp>
 
 using namespace std;
@@ -14,26 +13,23 @@ boost::any compare_one(boost::any a, boost::any b) {
         return b;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = a.convert_to<string>();
-        string str2 = b.convert_to<string>();
-        if (stod(str1) > stod(str2)) {
+        if (stod(a.convert_to<string>()) > stod(b.convert_to<string>())) {
             return a;
         }
-        else if (stod(str1) < stod(str2)) {
+       	else if (stod(a.convert_to<string>()) < stod(b.convert_to<string>())) {
             return b;
         }
-       	else {
+		else {
             return boost::any("None");
         }
     }
     else if (a.type() == typeid(int) && b.type() == typeid(string)) {
         double a_num = stod(a.convert_to<string>());
-        string str2 = b.convert_to<string>();
-        int b_int = stoi(str2);
-        if (a_num > b_int) {
+        double b_num = stod(b.convert_to<string>());
+        if (a_num > b_num) {
             return a;
         }
-       	else if (a_num < b_int) {
+		else if (a_num < b_num) {
             return b;
         }
 		else {
@@ -82,21 +78,4 @@ boost::any compare_one(boost::any a, boost::any b) {
     else {
         return boost::any("None");
     }
-}
-
-int main() {
-    // Test cases
-    boost::any a = 10; // int
-    boost::any b = "20.5"; // string
-    cout << compare_one(a, b) << endl;
-
-    a = 3.14; // float
-    b = "2"; // string
-    cout << compare_one(a, b) << endl;
-
-    a = "10"; // string
-    b = "20"; // string
-    cout << compare_one(a, b) << endl;
-
-    return 0;
 }
