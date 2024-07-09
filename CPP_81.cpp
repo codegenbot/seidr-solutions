0a15dfecc853890937b92fe3d3aebedda8e9ef91
beep boop <githubbot@vadim.me>
2024-07-09 03:53:02 +0200

    Solution 28 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zxgul3d6 Prompt:       |                              ^~~~~~ 8416ac43-38ec-4fde-9044-ca656f868b8f.cpp:38:37: error: ‘string’ was not declared  in this scope    38 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8416ac43-38ec-4fde-9044-ca656f868b8f.cpp:38:44: error: ‘b’ was not declared in t his scope    38 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8416ac43-38ec-4fde-9044-ca656f868b8f.cpp:38:45: error: expression list treated a s compound expression in initializer [-fpermissive]    38 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8416ac43-38ec-4fde-9044-ca656f868b8f.cpp:37: 8416ac43-38ec-4fde-9044-ca656f868b8f.cpp: In function ‘int main()’: 8416ac43-38ec-4fde-9044-ca656f868b8f.cpp:47:20: error: ‘numerical_letter_grade’ was not declared in this scope    47 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 8416ac43-38ec-4fde-9044-ca656f868b8f.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,44 +1,11 @@
 #include <vector>
+#include <string>
 
-bool is_same(string s1, string s2) {
-    if (s1.size() != s2.size())
-        return false;
-    for (int i = 0; i < s1.size(); ++i)
-        if (s1[i] != s2[i])
-            return false;
-    return true;
+bool issame(vector<string> a, vector<string>b) {
+    return a.size() == b.size();
 }
 
-vector<string> numerical_letter_grade(vector<float> grades) {
-    vector<string> result;
-    for (float g : grades) {
-        string letterGrade;
-        if (g >= 4.0)
-            letterGrade = "A+";
-        else if (g > 3.7)
-            letterGrade = "A";
-        else if (g > 3.3)
-            letterGrade = "A-";
-        else if (g > 3.0)
-            letterGrade = "B+";
-        else if (g > 2.7)
-            letterGrade = "B";
-        else if (g > 2.3)
-            letterGrade = "B-";
-        else if (g > 2.0)
-            letterGrade = "C+";
-        else if (g > 1.7)
-            letterGrade = "C";
-        else if (g > 1.3)
-            letterGrade = "C-";
-        else if (g > 1.0)
-            letterGrade = "D+";
-        else if (g > 0.7)
-            letterGrade = "D";
-        else
-            letterGrade = "E";
-
-        result.push_back(letterGrade);
-    }
-    return result;
+int main(){
+    //Your code here
+    return 0;
 }
