605fe43e2e2d4869d44d5b83b4e73c34f1bc2541
beep boop <githubbot@vadim.me>
2024-07-09 19:54:27 +0200

    Solution 60 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/ri48aft6 Prompt:  complement));       |                                   ^~~       |                                   std::min In file included from /usr/include/c++/11/vector:60,                  from f2db569d-9d65-493e-ab38-c18c1dfec092.cpp:1: /usr/include/c++/11/bits/stl_algobase.h:278:5: note: ‘std::min’ declared here   278 |     min(const _Tp& __a, const _Tp& __b, _Compare __comp)       |     ^~~ f2db569d-9d65-493e-ab38-c18c1dfec092.cpp:9:61: error: ‘max’ was not declared in this scope; did you mean ‘std::max’?     9 |             return std::make_pair(min(nums[i], complement), max(nums[i],  complement));       |                                                             ^~~       |                                                             std::max In file included from /usr/include/c++/11/vector:60,                  from f2db569d-9d65-493e-ab38-c18c1dfec092.cpp:1: /usr/include/c++/11/bits/stl_algobase.h:300:5: note: ‘std::max’ declared here   300 |     max(const _Tp& __a, const _Tp& __b, _Compare __comp)       |     ^~~ f2db569d-9d65-493e-ab38-c18c1dfec092.cpp:11:9: error: ‘numMap’ was not declared in this scope    11 |         numMap[nums[i]] = i;       |         ^~~~~~

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,15 +1,17 @@
 #include <vector>
-#include <utility>
 #include <unordered_map>
+#include <utility>
 
-std::pair<int, int> findPair(std::vector<int>& nums, int target) {
-    std::unordered_map<int, int> numMap;
+using namespace std;
+
+pair<int, int> findPair(vector<int>& nums, int target) {
+    unordered_map<int, int> numMap;
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
         if (numMap.find(complement) != numMap.end()) {
-            return std::make_pair(std::min(nums[i], complement), std::max(nums[i], complement));
+            return make_pair(min(nums[i], complement), max(nums[i], complement));
         }
         numMap[nums[i]] = i;
     }
-    return std::pair<int, int>(-1, -1);
+    return pair<int, int>(-1, -1);
 }
