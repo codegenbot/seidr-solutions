c6d75cf29184060f945816d70212eebc7f64a094
beep boop <githubbot@vadim.me>
2024-07-09 08:09:10 +0200

    Solution 38 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n5yplexf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a16bb8f9-c605-4069-9930-4797cac86 815.py", line 11, in <module>     check(solution)           ^^^^^^^^ NameError: name 'solution' is not defined

--- Python_121.py
+++ Python_121.py
@@ -1,6 +1,7 @@
 def sum_odd(lst):
     return sum(i for i in lst if isinstance(i, int) and i % 2 != 0)
 
-if __name__ == '__main__':
-    solution = sum_odd([])  
+
+if __name__ == "__main__":
+    solution = sum_odd([])
     print(solution)
