37422a76b8430c559e2dd4132a54c2caa4616c1e
beep boop <githubbot@vadim.me>
2024-07-09 12:22:16 +0200

    Solution 61 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/behwwxjv Prompt: 7c083544-bec8-459e-aea6-9b9833895f27.cpp: In function ‘long long int double_the_ difference(std::vector<float>)’: 7c083544-bec8-459e-aea6-9b9833895f27.cpp:7:35: error: ‘pow’ was not declared in this scope     7 |             odd_sum += (long long)pow(num, 2);       |                                   ^~~ In file included from 7c083544-bec8-459e-aea6-9b9833895f27.cpp:13: 7c083544-bec8-459e-aea6-9b9833895f27.cpp: In function ‘int main()’: 7c083544-bec8-459e-aea6-9b9833895f27.cpp:15:35: error: ‘lst’ was not declared in  this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 7c083544-bec8-459e-aea6-9b9833895f27.cpp:15:43: error: ‘odd_sum’ was not declare d in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -12,7 +12,6 @@ long long double_the_difference(std::vector<float> lst){
 }
 
 int main() {
-    std::vector<float> lst = {1, 2, 3, 4, 5};
+    std::vector<float> lst = {1.0, 2.0, 3.0, 4.0, 5.0};
     long long odd_sum = double_the_difference(lst);
-    return 0;
 }
