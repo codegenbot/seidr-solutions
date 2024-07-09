c9bd84fe44e1a76c1e4ca88ba933fc4eefc48c89
beep boop <githubbot@vadim.me>
2024-07-09 13:13:31 +0200

    Solution 94 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/p70nshd0 Prompt:    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:16: f8e2b31f-ea39-4798-9646-bf202464bab2.cpp: In function ‘int main()’: f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:26:20: error: ‘sorted_list_sum’ was not  declared in this scope    26 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ f8e2b31f-ea39-4798-9646-bf202464bab2.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,21 +1,22 @@
+bool issame(string a, string b) {
+    if (a.length() % 2 == 0 && b.length() % 2 != 0 || a.length() % 2 != 0 && b.length() % 2 == 0)
+        return false;
+    else
+        return true;
+}
+
 vector<string> sorted_list_sum(vector<string> lst) {
-    auto isEven = [](const string& a) {return a.length() % 2 == 0;};
-
-    vector<string> result;
-    for (string str : lst) {
-        if (isEven(str)) {
-            if (result.empty()) {
-                result.push_back(str);
-            } else if (!isEven(result.back())) {
-                result.clear();
-                result.push_back(str);
-            }
-        } else {
-            if (!isEven(result.back())) {
-                result.push_back(str);
-            }
-        }
-    }
-
-    return result;
+    auto it = unique(lst.begin(), lst.end(),
+                      [&lst](const string& a, const string& b) { 
+                          return !issame(a, b); 
+                      });
+    lst.erase(it, lst.end());
+    sort(lst.begin(), lst.end(),
+         [](const string& a, const string& b) {
+             if (a.length() == b.length())
+                 return a < b;
+             else
+                 return a.length() < b.length();
+         });
+    return lst;
 }
