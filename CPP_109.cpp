21636c50016cee5c4fc20fad39e0ff3482d2c482
beep boop <githubbot@vadim.me>
2024-07-09 03:57:20 +0200

    Solution 8 of CPP/109, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2snjoxbp Prompt: 42957fab-5967-46bf-98d4-1902987148b1.cpp:1:20: error: ‘vector’ was not declared in this scope     1 | bool move_one_ball(vector<int> arr) {       |                    ^~~~~~ 42957fab-5967-46bf-98d4-1902987148b1.cpp:1:27: error: expected primary-expressio n before ‘int’     1 | bool move_one_ball(vector<int> arr) {       |                           ^~~ In file included from 42957fab-5967-46bf-98d4-1902987148b1.cpp:11: 42957fab-5967-46bf-98d4-1902987148b1.cpp: In function ‘int main()’: 42957fab-5967-46bf-98d4-1902987148b1.cpp:13:26: error: ‘move_one_ball’ cannot be  used as a function    13 |     assert (move_one_ball({})==true);       |             ~~~~~~~~~~~~~^~~~

--- CPP_109.cpp
+++ CPP_109.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 
-bool move_one_ball(vector<int> arr) {
+int move_one_ball(vector<int> arr) {
     int n = arr.size();
     for (int i = 0; i < n - 1; i++) {
         if (arr[i] > arr[i + 1]) {
-            return false;
+            return 1;
         }
     }
-    return true;
+    return 0;
 }
