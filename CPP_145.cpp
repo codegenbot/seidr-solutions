234f01ed6d723acbc22d94a85c54864dad2c4d64
beep boop <githubbot@vadim.me>
2024-07-09 23:11:42 +0200

    Solution 76 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/aeycljm6 Prompt:                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from b1e20f27-2b27-482a-be9e-142ab15d03dc.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b1e20f27-2b27-482a-be9e-142ab15d03dc.cpp:31:34: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from b1e20f27-2b27-482a-be9e-142ab15d03dc.cpp:30: b1e20f27-2b27-482a-be9e-142ab15d03dc.cpp: In function ‘int main()’: b1e20f27-2b27-482a-be9e-142ab15d03dc.cpp:40:20: error: ‘order_by_points’ was not  declared in this scope    40 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ b1e20f27-2b27-482a-be9e-142ab15d03dc.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,10 +1,8 @@
-```cpp
+```
 #include <initializer_list>
 #include <algorithm>
 
-using namespace std;
-
-bool issame(vector<int> a, vector<int> b) {
+bool issame(const vector<int>& a, const vector<int>& b) {
     return a == b;
 }
 
@@ -31,6 +29,6 @@ vector<int> order_by_points(vector<int> nums) {
 }
 
 int main() {
-    assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0, 4, 23, 6, 6}));
+    assert(issame(std::vector<int>(order_by_points({0,6,6,-76,-21,23,4}) , std::vector<int>((-76), (-21), 0, 4, 23, 6, 6)));
     return 0;
 }
