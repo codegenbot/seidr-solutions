c318908176d04b943185f2409228bf7fd10e6dab
beep boop <githubbot@vadim.me>
2024-07-10 00:10:23 +0200

    Solution 42 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/2ghk70sf Prompt: f0b72747-6374-4bfa-b7e6-e6e9a3b86509.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string exchange(vector<int> lst1, vector<int> lst2) {       | ^~~~~~ In file included from f0b72747-6374-4bfa-b7e6-e6e9a3b86509.cpp:18: f0b72747-6374-4bfa-b7e6-e6e9a3b86509.cpp: In function ‘int main()’: f0b72747-6374-4bfa-b7e6-e6e9a3b86509.cpp:20:13: error: ‘exchange’ was not declar ed in this scope    20 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,14 +1,12 @@
 #include <string>
-#include <vector>
-#include <algorithm>
 
-std::string exchange(std::vector<int> lst1, std::vector<int> lst2) {
+string exchange(vector<int> lst1, vector<int> lst2) {
     for (int num : lst1) {
         if (num % 2 != 0) {
             int found = 0;
             for (int num2 : lst2) {
                 if (num2 % 2 == 0 && num2 != num) {
-                    std::swap(lst1[std::find(lst1.begin(), lst1.end(), num)], num2);
+                    swap(lst1[find(lst1.begin(), lst1.end(), num)], num2);
                     found = 1;
                     break;
                 }
