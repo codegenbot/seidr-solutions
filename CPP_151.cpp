a71f87833f94d14eb952ba1371b9baa2357a14db
beep boop <githubbot@vadim.me>
2024-07-09 11:05:48 +0200

    Solution 31 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7113sxs1 Prompt: ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = long unsigned int; std:: allocator_traits<std::allocator<_CharT> >::pointer = long unsigned int*; std::al locator_traits<std::allocator<_CharT> >::allocator_type = std::allocator<long un signed int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = long unsigned int; _Alloc = std::allocator<long unsigned int>; std::_Vect or_base<_Tp, _Alloc>::pointer = long unsigned int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const long unsigned int&}; _Tp = long unsigned int; _Alloc = std:: allocator<long unsigned int>; std::vector<_Tp, _Alloc>::iterator = std::vector<l ong unsigned int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = long unsigned int; _Allo c = std::allocator<long unsigned int>; std::vector<_Tp, _Alloc>::value_type = lo ng unsigned int]’ 6956bcbc-ac50-430c-83f4-6f9af41122ec.cpp:27:24:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<long unsigned int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -6,9 +6,9 @@
 size_t double_the_difference(std::vector<size_t> input) {
     size_t sum = 0;
     for (size_t num : input) {
-        if (num > 0 && num == static_cast<size_t>(num)) { 
-            if (num % 2 != 0) { 
-                sum += num * num;
+        if (num > 0 && num == static_cast<size_t>(std::floor((double)num))) { 
+            if ((double)num % 2.0 != 0.0) { 
+                sum += std::pow((double)num, 2);
             }
         }
     }
@@ -24,7 +24,9 @@ int main() {
         size_t num;
         std::cout << "Enter element " << i+1 << ": ";
         std::cin >> num;
-        input.push_back(num);
+        if(num > 0) {
+            input.push_back(num);
+        }
     }
     size_t result = double_the_difference(input);
     std::cout << "The total sum of squares is: " << result << std::endl;
