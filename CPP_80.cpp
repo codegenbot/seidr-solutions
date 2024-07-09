73f7d3be3fb96f8c96e29d96b48a31d3e81d6a90
beep boop <githubbot@vadim.me>
2024-07-09 15:48:18 +0200

    Solution 11 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0u1me4un Prompt: 8a27bc1f-df54-4a35-ac59-58cdf9d81e73.cpp:4:27: note: ‘std::string s’ declared he re     4 | bool is_happy(std::string s) {       |               ~~~~~~~~~~~~^ 8a27bc1f-df54-4a35-ac59-58cdf9d81e73.cpp: In function ‘int main()’: 8a27bc1f-df54-4a35-ac59-58cdf9d81e73.cpp:16:5: error: ‘assert’ was not declared in this scope    16 |     assert(is_happy("iopaxioi") == false );       |     ^~~~~~ 8a27bc1f-df54-4a35-ac59-58cdf9d81e73.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <algorithm>   +++ |+#include <cassert>     3 | 8a27bc1f-df54-4a35-ac59-58cdf9d81e73.cpp: At global scope: 8a27bc1f-df54-4a35-ac59-58cdf9d81e73.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 8a27bc1f-df54-4a35-ac59-58cdf9d81e73.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,14 +1,19 @@
-```cpp
 #include <string>
 #include <algorithm>
+#include <cassert>
 
 bool is_happy(std::string s) {
     if (s.length() < 3)
         return false;
     for (int i = 0; i <= s.length() - 3; i++) {
         std::string sub = s.substr(i, 3);
-        if (!std::all_of(sub.begin(), sub.end(), [s](char x){ return s.find(x) == std::string::npos; }))
+        if (std::any_of(sub.begin(), sub.end(), [x](char x){ return s.find(x) != std::string::npos; }))
             return false;
     }
     return true;
+}
+
+int main() {
+    assert(is_happy("iopaxioi") == false );
+    return 0;
 }
