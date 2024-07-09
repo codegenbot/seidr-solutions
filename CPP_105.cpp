344f3ebe2687a42f7dc2b8300aa12bc417df6c49
beep boop <githubbot@vadim.me>
2024-07-09 07:51:38 +0200

    Solution 34 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/siauvrg0 Prompt:       |                              ^~~~~~ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 964a0b5c-562f-4158-92fc-249bca43033b.cpp:50: 964a0b5c-562f-4158-92fc-249bca43033b.cpp: In function ‘int main()’: 964a0b5c-562f-4158-92fc-249bca43033b.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 964a0b5c-562f-4158-92fc-249bca43033b.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,10 +1,10 @@
-```cpp
 bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-vector<string> by_length(vector<int> arr) {
+vector<vector<string>> by_length(vector<int> arr) {
     vector<vector<string>> result;
+
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
             vector<string> numbers;
@@ -39,51 +39,46 @@ vector<string> by_length(vector<int> arr) {
                     break;
             }
             numbers.push_back(name);
-            for (int i = 0; i < num - 1; i++) {
-                if (i == 0) {
-                    numbers.push_back("Zero");
-                } else {
-                    numbers.push_back(name);
+            for (int i = 1; i <= num; i++) {
+                if (i >= 1 && i <= 9) {
+                    string temp_name = "";
+                    switch (i) {
+                        case 1:
+                            temp_name = "One";
+                            break;
+                        case 2:
+                            temp_name = "Two";
+                            break;
+                        case 3:
+                            temp_name = "Three";
+                            break;
+                        case 4:
+                            temp_name = "Four";
+                            break;
+                        case 5:
+                            temp_name = "Five";
+                            break;
+                        case 6:
+                            temp_name = "Six";
+                            break;
+                        case 7:
+                            temp_name = "Seven";
+                            break;
+                        case 8:
+                            temp_name = "Eight";
+                            break;
+                        case 9:
+                            temp_name = "Nine";
+                            break;
+                    }
+                    numbers.push_back(temp_name);
                 }
             }
+            sort(numbers.begin(), numbers.end());
+            reverse(numbers.begin(), numbers.end());
             result.push_back(numbers);
         }
     }
 
-    vector<string> final_result;
-    for (vector<string> nums : result) {
-        bool is_same = true;
-        string first_name = nums[0];
-        for (string num : nums) {
-            if (!issame(vector<string>{first_name}, vector<string>{num})) {
-                is_same = false;
-                break;
-            }
-        }
-        if (is_same) {
-            final_result.push_back("Same");
-        } else {
-            final_result.push_back(strcat_join(nums, " "));
-        }
-    }
-
-    return final_result;
-}
-
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <cctype>
-
-std::string strcat_join(const std::vector<std::string>& strings, const std::string& separator) {
-    std::string result = "";
-    for (const auto& str : strings) {
-        if (!result.empty()) {
-            result += separator;
-        }
-        for (char c : str) {
-            result += c;
-        }
-    }
     return result;
 }
