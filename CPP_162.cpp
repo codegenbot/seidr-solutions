fb18337c424be5706687b273dad0a53ec311c007
beep boop <githubbot@vadim.me>
2024-07-09 05:11:04 +0200

    Solution 50 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/y3l1gali Prompt: 9");       |     ^~~~~~ 2d8b97f7-9729-4e55-97ca-31755c9939f1.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <string>   +++ |+#include <cassert>     4 | 2d8b97f7-9729-4e55-97ca-31755c9939f1.cpp: At global scope: 2d8b97f7-9729-4e55-97ca-31755c9939f1.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 2d8b97f7-9729-4e55-97ca-31755c9939f1.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from 2d8b97f7-9729-4e55-97ca-31755c9939f1.cpp:33: 2d8b97f7-9729-4e55-97ca-31755c9939f1.cpp: In function ‘int main()’: 2d8b97f7-9729-4e55-97ca-31755c9939f1.cpp:35:13: error: ‘string_to_md5’ was not d eclared in this scope    35 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cassert>
 
-std::string string_to_md5(std::string text) {
+string string_to_md5(string text) {
     if (text.empty()) return "";
     
     unsigned char result[16];
@@ -15,7 +15,7 @@ std::string string_to_md5(std::string text) {
     MD5_Update(&md5Context, buffer, text.length());
     MD5_Final(result, &md5Context);
 
-    std::string md5String;
+    string md5String;
     for (int i = 0; i < 16; i++) {
         char temp[3];
         sprintf(temp, "%02x", result[i]);
