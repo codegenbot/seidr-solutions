fa6cb4fde944defbf56b71c2a30a707648693dcf
beep boop <githubbot@vadim.me>
2024-07-09 21:51:38 +0200

    Solution 49 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/le0u21ok Prompt: it must return G4 for input nY-w!k1r#qDuz3A8USO_&[-f]:ZvFZxZLsGiF&QrYkuqw}LLG4-!hfEX2;"E~Qn){48b2:H*e86[KxZ:bX$Rg$iywlWPChOOX], but it returns Enter a string: G4.

--- middle-character.cpp
+++ middle-character.cpp
@@ -6,25 +6,12 @@ std::string middleCharacter(std::string s) {
     if(len == 0)
         return "";
     
-    if (len == 1) {
-        return "";
-    } else {
+    if (len % 2 == 0) {
         int middleIndex = len / 2;
-        if(len % 2 != 0) {
-            middleIndex++; 
-        }
-        std::string result;
-        if(len % 2 == 0) {
-            int mid1 = len / 2 - 1;
-            if(mid1 >= 0 && mid1 < s.length()) { 
-                result = s.substr(mid1, 2);
-            } else {
-                result = s.substr(middleIndex, 1); 
-            }
-        } else {
-            result = s.substr(middleIndex, 1);
-        }
-        return result;
+        return s.substr(middleIndex-1, 2); 
+    } else {
+        int middleIndex = len / 2; 
+        return s.substr(middleIndex, 1);
     }
 }
 
