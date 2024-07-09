dd287313519246165f1de6267f60343b1faf1d13
beep boop <githubbot@vadim.me>
2024-07-09 19:41:36 +0200

    Solution 16 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ymt5ntk8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/38a559f6-25de-4345-9e74-3d18196f0 18f.py", line 2     for num in numbers[:-1]: IndentationError: unexpected indent

--- Python_5.py
+++ Python_5.py
@@ -1,7 +1,6 @@
-def complete_code(numbers, delimeter):
-    result = []
-    for num in numbers[:-1]:
-        result.extend([num, delimeter])
-    if numbers:
-        result.append(numbers[-1])
-    return result
+result = []
+for num in numbers[:-1]:
+    result.extend([num, delimeter])
+if numbers:
+    result.append(numbers[-1])
+return result
