f8f6b778f02668b4d2ca79246605a4b98b18b61e
beep boop <githubbot@vadim.me>
2024-07-09 20:24:50 +0200

    Solution 30 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/h6h6rl4f Prompt:    28 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 3a529769-297a-474f-9607-1bf3491865f2.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3a529769-297a-474f-9607-1bf3491865f2.cpp:28:45: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3a529769-297a-474f-9607-1bf3491865f2.cpp:27: 3a529769-297a-474f-9607-1bf3491865f2.cpp: In function ‘int main()’: 3a529769-297a-474f-9607-1bf3491865f2.cpp:37:20: error: ‘sorted_list_sum’ was not  declared in this scope    37 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 3a529769-297a-474f-9607-1bf3491865f2.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,36 +1,23 @@
 #include <vector>
-#include <algorithm>
-#include <iostream>
+#include <string>
 
-using namespace std;
-
-vector<string> sorted_list_sum(vector<string> lst) {
-    vector<string> evenLengthStrings;
-    for (const string& str : lst) {
-        if (str.length() % 2 == 0) {
-            evenLengthStrings.push_back(str);
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (sorted_list_sum({a[i], b[i]})[0] != sorted_list_sum({b[i], a[i]})[0]) {
+            return false;
         }
     }
-
-    sort(evenLengthStrings.begin(), evenLengthStrings.end(),
-         [](const string& a, const string& b) {
-             if (a.length() != b.length()) {
-                 return a.length() < b.length();
-             } else {
-                 return a < b;
-             }
-         });
-
-    return evenLengthStrings;
+    return true;
 }
 
 int main() {
-    vector<string> input = {"aaaa", "bbbb", "dd", "cc"};
-    vector<string> expected = {"cc", "dd", "aaaa", "bbbb"};
-    
-    if(sorted_list_sum(input) == expected) {
-        cout << "The lists are the same." << endl;
-    } else {
-        cout << "The lists are not the same." << endl;
-    }
+    std::vector<std::string> input = {"aaaa", "bbbb", "dd", "cc"};
+    std::vector<std::string> expected = {"cc", "dd", "aaaa", "bbbb"};
+
+    assert(issame(input, expected) == true);
+
+    return 0;
 }
