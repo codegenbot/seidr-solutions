48a20e4f1be6fdfeb6687ff5b492df479cfd4760
beep boop <githubbot@vadim.me>
2024-07-09 05:41:31 +0200

    Solution 67 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/d9xvhjel Prompt: b0a7bb5f-f2f6-4225-bc04-7bb07138c054.cpp: In function ‘std::string string_to_md5 (std::string)’: b0a7bb5f-f2f6-4225-bc04-7bb07138c054.cpp:11:5: error: ‘MD5’ was not declared in this scope; did you mean ‘md5’?    11 |     MD5((const unsigned char*)text.c_str(), text.size(), md5);       |     ^~~       |     md5 b0a7bb5f-f2f6-4225-bc04-7bb07138c054.cpp:15:23: error: aggregate ‘std::ostringst ream ss’ has incomplete type and cannot be defined    15 |         ostringstream ss;       |                       ^~ b0a7bb5f-f2f6-4225-bc04-7bb07138c054.cpp:16:15: error: ‘hex’ was not declared in  this scope    16 |         ss << hex << (int)md5[i];       |               ^~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,7 +1,7 @@
-#include<string>
-#include<openssl/md5.h>
-#include<iomanip>
-#include<sstream>
+#include <string>
+#include <openssl/md5.h>
+#include <iomanip>
+#include <sstream>
 
 using namespace std;
 
@@ -9,15 +9,12 @@ string string_to_md5(string text) {
     if (text.empty()) return "None";
     
     unsigned char md5[16];
-    MD5_CTX ctx;
-    MD5_Init(&ctx);
-    MD5_Update(&ctx, (const unsigned char*)text.c_str(), text.size());
-    MD5_Final(md5, &ctx);
-
+    md5((const unsigned char*)text.c_str(), text.size(), md5);
+    
     string result;
     for(int i = 0; i < 16; ++i){
         std::ostringstream ss;
-        ss << hex << setw(2) << setfill('0') << (int)md5[i];
+        ss << hex << (int)md5[i];
         result += ss.str();
     }
     
