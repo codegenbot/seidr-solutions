e0fd0ad132f1ede416901187fab829bc3821e3fd
beep boop <githubbot@vadim.me>
2024-07-09 11:47:45 +0200

    Solution 60 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/yrwz0rv1 Prompt:       |                    ^~~~~ a6d5c5f5-1e7f-4997-859d-d12e29e05304.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ a6d5c5f5-1e7f-4997-859d-d12e29e05304.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ a6d5c5f5-1e7f-4997-859d-d12e29e05304.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from a6d5c5f5-1e7f-4997-859d-d12e29e05304.cpp:11: a6d5c5f5-1e7f-4997-859d-d12e29e05304.cpp: In function ‘int main()’: a6d5c5f5-1e7f-4997-859d-d12e29e05304.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ a6d5c5f5-1e7f-4997-859d-d12e29e05304.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,26 +1,16 @@
-#include <iostream>
 #include <vector>
-#include <assert.h>
+#include <algorithm>
 
-bool issame(std::vector<float> a, std::vector<float> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
+bool issame(std::vector<float> a, std::vector<float>b) {
+    return (a.size() == b.size()) && (std::equal(a.begin(), a.end(), b.begin()));
 }
 
-std::vector<float> get_positive(std::vector<float> l) {
-    std::vector<float> result;
+vector<float> get_positive(vector<float> l) {
+    vector<float> result;
     for (float num : l) {
         if (num > 0) {
             result.push_back(num);
         }
     }
     return result;
-}
-
-int main() {
-    assert(get_positive({}).size() == 0);
-    return 0;
 }
