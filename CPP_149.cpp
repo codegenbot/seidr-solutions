42e907265efe6fc09f1641452543db7e5e9f2277
beep boop <githubbot@vadim.me>
2024-07-09 02:18:08 +0200

    Solution 5 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h3blrigf Prompt:    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 55198734-469d-4e62-9b8c-84fd4a7c9200.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 55198734-469d-4e62-9b8c-84fd4a7c9200.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 55198734-469d-4e62-9b8c-84fd4a7c9200.cpp:17: 55198734-469d-4e62-9b8c-84fd4a7c9200.cpp: In function ‘int main()’: 55198734-469d-4e62-9b8c-84fd4a7c9200.cpp:27:20: error: ‘sorted_list_sum’ was not  declared in this scope    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 55198734-469d-4e62-9b8c-84fd4a7c9200.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,1 +1,13 @@
-assert(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) == {"cc","dd","aaaa","bbbb"});
+vector<string> sorted_list_sum(vector<string> lst) {
+    auto it = remove_if(lst.begin(), lst.end(), [](const string& s){ return s.length() % 2; });
+    lst.erase(it, lst.end());
+    sort(lst.begin(), lst.end(), 
+        [](const string& a, const string& b){
+            if(a.length() != b.length()) {
+                return a.length() < b.length();
+            } else {
+                return a < b;
+            }
+        });
+    return lst;
+}
