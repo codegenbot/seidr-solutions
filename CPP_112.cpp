4d8b6ad91848233cbad377a72f8440f1b80b7c85
beep boop <githubbot@vadim.me>
2024-07-09 04:09:56 +0200

    Solution 24 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7b288k16 Prompt:       |                              ^~~~~~ d76d27fa-63e6-48c1-89fc-378342dd64c6.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d76d27fa-63e6-48c1-89fc-378342dd64c6.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d76d27fa-63e6-48c1-89fc-378342dd64c6.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d76d27fa-63e6-48c1-89fc-378342dd64c6.cpp:25: d76d27fa-63e6-48c1-89fc-378342dd64c6.cpp: In function ‘int main()’: d76d27fa-63e6-48c1-89fc-378342dd64c6.cpp:35:20: error: ‘reverse_delete’ was not declared in this scope    35 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ d76d27fa-63e6-48c1-89fc-378342dd64c6.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,14 +1,11 @@
+#include <iostream>
 #include <vector>
-#include <string>
+#include <algorithm>
+
 using namespace std;
 
-bool issame(vector<string> a,vector<string>b) {
-    if(a.size() != b.size()) 
-        return false;
-    for(int i = 0; i < a.size(); i++) 
-        if(a[i] != b[i])
-            return false;
-    return true;
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
 }
 
 vector<string> reverse_delete(string s, string c) {
@@ -34,6 +31,6 @@ vector<string> reverse_delete(string s, string c) {
 }
 
 int main() {
-    assert(issame(reverse_delete("mamma", "mia") , {"", "True"}));
+    assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
     return 0;
 }
