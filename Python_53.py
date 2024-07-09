ac7dced21994551a16b5cee392187e681b45c796
beep boop <githubbot@vadim.me>
2024-07-09 05:18:41 +0200

    Solution 87 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kl0f1ktf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9668294f-2e32-4860-b3a1-badf1114d c31.py", line 3     x = int(input(f"Enter the first number: {a} " + str(a) if not a else str(a)) ) or str(a))            ^ SyntaxError: unmatched ')'

--- Python_53.py
+++ Python_53.py
@@ -1,7 +1,7 @@
-def add(a=0, b=0):
+def add(a=0, b=0): 
     prompt = f"Enter the first number: {a}" if not a else str(a)
-    x = int(input(prompt))
-    
+    x = int(input(prompt)) 
+
     prompt = f"Enter the second number: {b}" if not b else str(b)
     y = int(input(prompt))
     
