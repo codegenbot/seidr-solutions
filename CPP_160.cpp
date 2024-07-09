ed4aab077526584142f7c0aa7819b5be68677904
beep boop <githubbot@vadim.me>
2024-07-09 04:52:46 +0200

    Solution 29 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xuigdtqp Prompt: this scope; did you mean ‘s’?    43 |                 st.push(temp.top());       |                 ^~       |                 s ee80ae18-8a1b-4062-b225-75870dc0d998.cpp:51:13: error: ‘st’ was not declared in this scope; did you mean ‘s’?    51 |     while (!st.empty()) {       |             ^~       |             s ee80ae18-8a1b-4062-b225-75870dc0d998.cpp:81:23: error: ‘pow’ was not declared in  this scope    81 |             int num = pow(st.top(), 1);       |                       ^~~ ee80ae18-8a1b-4062-b225-75870dc0d998.cpp:103:16: error: ‘i’ was not declared in this scope   103 |         op = s[i++];       |                ^ In file included from ee80ae18-8a1b-4062-b225-75870dc0d998.cpp:109: ee80ae18-8a1b-4062-b225-75870dc0d998.cpp: In function ‘int main()’: ee80ae18-8a1b-4062-b225-75870dc0d998.cpp:111:23: error: ‘do_algebra’ cannot be u sed as a function   111 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,23 +1,115 @@
+long long pow(long long base, int exponent) {
+    long long res = 1;
+    while (exponent--) {
+        res *= base;
+    }
+    return res;
+}
+
 int do_algebra(vector<string> operator_, vector<int> operand) {
     string expression = "";
     for (int i = 0; i < operator_.size(); i++) {
         expression += to_string(operand[i]);
         expression += operator_[i];
-        i++; // Increment i here as well
     }
-    expression += to_string(operand[operator_.size()]);
-
+    expression += to_string(operand[operator_.size() - 1]);
+    
     long long result = eval(expression);
-
-    return result;
+    
+    return static_cast<int>(result);
 }
 
-long long pow(long long base, int exponent) {
-    if(exponent == 0)
-        return 1;
-    else if(exponent > 0)
-        return base * pow(base, exponent - 1);
-    else
-        return 1.0 / pow(base, -exponent);
+long long eval(const string& s) {
+    int i = 0;
+    stack<long long> st;
+    while (i < s.size()) {
+        if (isdigit(s[i])) {
+            long long num = 0;
+            while (i < s.size() && isdigit(s[i])) {
+                num = num * 10 + (s[i] - '0');
+                i++;
+            }
+            st.push(num);
+        } else if (s[i] == '(') {
+            i++;
+            stack<long long> temp;
+            while (i < s.size() && s[i] != ')') {
+                if (isdigit(s[i])) {
+                    long long num = 0;
+                    while (i < s.size() && isdigit(s[i])) {
+                        num = num * 10 + (s[i] - '0');
+                        i++;
+                    }
+                    temp.push(num);
+                } else if (s[i] == '(') {
+                    temp.push(eval("(" + s.substr(i++) + ")"));
+                } else if (s[i] == ')') {
+                    break;
+                }
+            }
+            while (!temp.empty()) {
+                st.push(temp.top());
+                temp.pop();
+            }
+        }
+    }
+    
+    long long res = 0;
+    char op = '+';
+    while (!st.empty()) {
+        if (op == '+') {
+            res += st.top();
+            st.pop();
+        } else if (op == '-') {
+            res -= st.top();
+            st.pop();
+        } else if (op == '*') {
+            long long num = st.top() * 1;
+            st.pop();
+            while (!st.empty() && st.top() == '(') {
+                st.pop();
+            }
+            if (!st.empty()) {
+                num *= st.top();
+                st.pop();
+            }
+            res += num;
+        } else if (op == '/') {
+            long long num = st.top() / 1;
+            st.pop();
+            while (!st.empty() && st.top() == '(') {
+                st.pop();
+            }
+            if (!st.empty()) {
+                num /= st.top();
+                st.pop();
+            }
+            res += num;
+        } else if (op == '**') {
+            long long num = pow(st.top(), 1);
+            st.pop();
+            while (!st.empty() && st.top() == '(') {
+                st.pop();
+            }
+            if (!st.empty()) {
+                num = pow(num, st.top());
+                st.pop();
+            }
+            res += num;
+        } else if (op == '//') {
+            long long num = st.top() / 1;
+            st.pop();
+            while (!st.empty() && st.top() == '(') {
+                st.pop();
+            }
+            if (!st.empty()) {
+                num /= st.top();
+                st.pop();
+            }
+            res += num;
+        }
+        op = s[i++];
+    }
+    
+    return res;
 }
-```
