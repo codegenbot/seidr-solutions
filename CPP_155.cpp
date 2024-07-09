98fc60b3eeabc6ce79775519341c1d5db7935edf
beep boop <githubbot@vadim.me>
2024-07-09 03:05:13 +0200

    Solution 62 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fkq142tr Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 935094e6-0ddc-461d-bf55-3d23b50d931a.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,42 +1,27 @@
+```cpp
 #include <vector>
 #include <iostream>
 
-bool isSame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
-            return false;
-    }
-    return true;
+bool areEqual(std::vector<int> a, std::vector<int> b) {
+    return (a == b);
 }
 
-std::vector<int> count(int num) {
-    std::vector<int> result(2, 0);
-    while (num != 0 || num == -1) {
-        int digit = (num % 10);
+std::vector<int> even_odd_count(int num) {
+    int e = 0, o = 0;
+    std::vector<int> result(2);
+    result[0] = e; result[1] = o;
+    while (num != 0) {
+        int digit = num % 10;
         if (digit % 2 == 0)
             result[0]++;
         else
             result[1]++;
-        if (num > 0)
-            num /= 10;
-        else if (num < 0) {
-            num /= 10;
-            ((result[0] += 1), (result[1] += 1));
-        }
+        num /= 10;
     }
     return result;
 }
 
 int main() {
-    std::cout << "Number of even and odd digits: ";
-    int n = -1; // default input
-    while (n != 0) {
-        std::cin >> n;
-        std::vector<int> temp = count(n);
-        if (!isSame({1, 0}, temp))
-            std::cout << "Number of even and odd digits: " << temp[0] << " " << temp[1] << std::endl;
-    }
+    assert(areEqual(even_odd_count(0), std::vector<int>{1, 0}));
     return 0;
 }
