3316c7a31cf88605765204f589bfaf1acdb98ab9
beep boop <githubbot@vadim.me>
2024-07-09 17:55:17 +0200

    Solution 57 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/acc9guyz Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- bowling.cpp
+++ bowling.cpp
@@ -1,30 +1,69 @@
+#include <iostream>
 #include <string>
 
 int bowlingScore(std::string input) {
     int score = 0;
+    int roll1, roll2, roll3;
+    bool spare = false;
+
     for(int i = 0; i < input.length(); i++) {
         if(input[i] == 'X') { 
             score += 10;
-            i++; 
+            if(i < input.length() - 1) {
+                if(input[i+1] == '/') { 
+                    score += 10;
+                    i++; 
+                } else {
+                    score += input[i+1] - '0';
+                    i++; 
+                }
+            }
         } else if(input[i] == '/') { 
             score += 10 - (input[i+1] - '0' + input[i+2] - '0');
             i+=2; 
         } else {
-            int roll = input[i] - '0';
-            if(input[i+1] == '/') { 
-                score += roll + 10 - (input[i+2] - '0' + input[i+3] - '0');
-                i+=3; 
-            } else {
-                score += roll;
-                if(input[i+1] == 'X') { 
-                    score += 10 + 10 + (input[i+2] - '0' + input[i+3] - '0');
+            roll1 = input[i] - '0';
+            if(i < input.length() - 1) {
+                if(input[i+1] == '/') { 
+                    score += roll1 + 10 - (input[i+2] - '0' + input[i+3] - '0');
                     i+=3; 
+                    spare = true;
                 } else {
-                    score += roll + input[i+1] - '0';
-                    i++; 
+                    score += roll1;
+                    if(i < input.length() - 1) {
+                        if(input[i+1] == 'X') { 
+                            score += 10 + 10 + (input[i+2] - '0' + input[i+3] - '0');
+                            i+=3; 
+                        } else {
+                            roll2 = input[i+1] - '0';
+                            if(i < input.length() - 1) {
+                                if(input[i+2] == '/') { 
+                                    score += roll1 + roll2 + 10 - (input[i+3] - '0' + input[i+4] - '0');
+                                    i+=4; 
+                                } else {
+                                    score += roll1 + roll2;
+                                    i++; 
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
+
+    if(spare) {
+        score += 10;
+    }
+
     return score;
+}
+
+int main() {
+    std::string input;
+    std::cout << "Enter the bowling score string: ";
+    std::cin >> input;
+    int score = bowlingScore(input);
+    std::cout << "The score is: " << score << std::endl;
+    return 0;
 }
