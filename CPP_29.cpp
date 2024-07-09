8e35e446422862141e4ee3edc84cbc602c4709f1
beep boop <githubbot@vadim.me>
2024-07-09 03:51:28 +0200

    Solution 86 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/o5dcqdjt Prompt:    28 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 52e5503c-5755-4af1-ba87-3534b98ed10a.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 52e5503c-5755-4af1-ba87-3534b98ed10a.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from 52e5503c-5755-4af1-ba87-3534b98ed10a.cpp:27: 52e5503c-5755-4af1-ba87-3534b98ed10a.cpp: In function ‘int main()’: 52e5503c-5755-4af1-ba87-3534b98ed10a.cpp:37:20: error: ‘filter_by_prefix’ was no t declared in this scope    37 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 52e5503c-5755-4af1-ba87-3534b98ed10a.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -16,9 +16,10 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
     }
     return result;
 
+}
 int main() {
     vector<string> strings = {"xxx", "asd", "xxy", "john doe", "xxxAAC", "xxx"};
     string prefix = "xxx";
-    vector<string> res = filter_by_prefix(strings, prefix); 
-    assert(issame(res , {"xxx", "xxxAAA", "xxx"}));
+    vector<string> output = filter_by_prefix(strings,prefix); 
+    assert(issame(output, {"xxx", "xxxAAA", "xxx"}));
 }
