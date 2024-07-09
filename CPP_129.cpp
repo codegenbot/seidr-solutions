67f49daec6b9a307b162e9d1b2532eee51403c7e
beep boop <githubbot@vadim.me>
2024-07-09 22:20:08 +0200

    Solution 26 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fjjhgnby Prompt:    41 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:41:34: error: expected primary-expressi on before ‘int’    41 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:41:39: error: expression list treated a s compound expression in initializer [-fpermissive]    41 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:40: 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp: In function ‘int main()’: 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:50:20: error: ‘minPath’ was not declare d in this scope    50 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 45c6d1d7-fb65-4e5a-acee-fd07224195af.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,60 +1,16 @@
-#include <iostream>
 #include <vector>
 #include <queue>
-
 using namespace std;
 
-bool isSame(vector<int> &a, vector<int> &b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); ++i)
-        if (a[i] != b[i])
-            return false;
-    return true;
+int issame(vector<int> v1, vector<int> v2) {
+    if(v1.size() != v2.size()) 
+        return 0;
+    for(int i = 0; i < v1.size(); i++) 
+        if(v1[i] != v2[i]) 
+            return 0;
+    return 1;
 }
 
 vector<int> minPath(vector<vector<int>>& grid, int k) {
-    int n = grid.size();
-    vector<vector<bool>> visited(n, vector<bool>(n));
-    vector<pair<int, int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-    vector<int> res;
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (grid[i][j] == 1) {
-                pq.push({k, {i, j}});
-                break;
-            }
-        }
-    }
-
-    while (!pq.empty()) {
-        auto [dist, pos] = pq.top();
-        pq.pop();
-
-        if (dist > 0) {
-            res.push_back(grid[pos.first][pos.second]);
-            visited[pos.first][pos.second] = true;
-
-            for (auto& d : dir) {
-                int ni = pos.first + d.first;
-                int nj = pos.second + d.second;
-                if (ni >= 0 && ni < n && nj >= 0 && nj < n && !visited[ni][nj]) {
-                    pq.push({dist - 1, {ni, nj}});
-                }
-            }
-        }
-
-        if (res.size() == k) break;
-    }
-
-    return res;
-}
-
-int main() {
-    vector<vector<int>> grid = {{0, 0}, {0, 0}, {1, 1}};
-    int k = 2;
-    vector<int> minPathRes = minPath(grid, k);
-    for (auto &x: minPathRes) cout << x;
+    // your code here...
 }
