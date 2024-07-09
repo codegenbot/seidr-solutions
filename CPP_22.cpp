2f8d809a633d08770b6b6008892e58e4f2f5aaf4
beep boop <githubbot@vadim.me>
2024-07-10 01:23:14 +0200

    Solution 8 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uydoiiwb Prompt: a321a9cd-ff58-4f90-b880-30fdc9d8fec2.cpp:1:10: fatal error: boost/config.hpp: No  such file or directory     1 | #include <boost/config.hpp>       |          ^~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,4 +1,5 @@
-#include <boost/config.hpp>
+#include <boost/any.hpp>
+
 #include <vector>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
@@ -13,4 +14,3 @@ std::vector<int> filter_integers(std::vector<boost::any> values) {
         }
     }
     return result;
-}
