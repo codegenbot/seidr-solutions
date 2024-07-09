3243d69a99eb6b47029c4b68916cd90a24fd545c
beep boop <githubbot@vadim.me>
2024-07-09 10:06:43 +0200

    Solution 4 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/wus1b72s Prompt:       | ^~~~~~~~~~~~~ fd627978-ee32-45fd-b9b5-89bdc999230b.cpp:13:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    13 | int main(){       |         ^~ fd627978-ee32-45fd-b9b5-89bdc999230b.cpp:13:9: note: remove parentheses to defau lt-initialize a variable    13 | int main(){       |         ^~       |         -- fd627978-ee32-45fd-b9b5-89bdc999230b.cpp:13:9: note: or replace parentheses with  braces to value-initialize a variable fd627978-ee32-45fd-b9b5-89bdc999230b.cpp:13:11: error: a function-definition is not allowed here before ‘{’ token    13 | int main(){       |           ^ fd627978-ee32-45fd-b9b5-89bdc999230b.cpp:15:2: error: expected ‘}’ at end of inp ut    15 | }       |  ^ fd627978-ee32-45fd-b9b5-89bdc999230b.cpp:4:40: note: to match this ‘{’     4 | int sum_squares(std::vector<float> lst){       |                                        ^

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 #include <cmath>
 
@@ -9,24 +8,3 @@ int sum_squares(std::vector<float> lst){
         total += std::pow(ceil_x,2);
     }
     return total;
-}
-
-int main() {
-    std::vector<float> inputVector;
-    
-    int size;
-    std::cout << "Enter the number of elements: ";
-    std::cin >> size;
-    for(int i = 0; i < size; ++i) {
-        float val;
-        std::cout << "Enter element " << (i+1) << ": ";
-        std::cin >> val;
-        inputVector.push_back(val);
-    }
-    
-    int result = sum_squares(inputVector);
-
-    std::cout << "Sum of squares: " << result << std::endl;
-
-    return 0;
-}
