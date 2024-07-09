e097a4642ae5277894c7793fab2b039414f88184
beep boop <githubbot@vadim.me>
2024-07-09 21:30:47 +0200

    Solution 41 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g34sr5ik Prompt:       |     ^~~~~~ afc3974e-20db-484d-be62-d8c1b0231cad.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | using namespace std; afc3974e-20db-484d-be62-d8c1b0231cad.cpp: At global scope: afc3974e-20db-484d-be62-d8c1b0231cad.cpp:25:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ afc3974e-20db-484d-be62-d8c1b0231cad.cpp:4:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     4 | bool issame(vector<int> a,vector<int> b){       |      ^~~~~~ afc3974e-20db-484d-be62-d8c1b0231cad.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ afc3974e-20db-484d-be62-d8c1b0231cad.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,24 +1,19 @@
-```
+#include <cassert>
 #include <vector>
-#include <set>
-#include <algorithm>
 using namespace std;
 
-bool issame(vector<int> a,vector<int> b){
-    return a == b;
+set<int> intersection(set<int> s1, set<int> s2) {
+    set<int> result;
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+        inserter(result, result.begin()));
+    return result;
 }
 
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
-    set<int> intersection;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-        inserter(intersection, intersection.begin()));
+    set<int> intersection = intersection(s1, s2);
     vector<int> result(intersection.begin(), intersection.end());
     sort(result.begin(), result.end());
     return result;
-}
-
-int main() {
-    assert(issame(common({4, 3, 2, 8}, {}), {}));
 }
