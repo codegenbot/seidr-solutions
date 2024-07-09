9f341f5419fa224cc0be367263eda31ed8a19600
beep boop <githubbot@vadim.me>
2024-07-09 05:35:59 +0200

    Solution 29 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/htukllsa Prompt:       |                    ^~~~~ af77670f-c55c-4dba-bc56-d478c5ff46b0.cpp:13:29: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ af77670f-c55c-4dba-bc56-d478c5ff46b0.cpp:13:36: error: expected primary-expressi on before ‘float’    13 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ af77670f-c55c-4dba-bc56-d478c5ff46b0.cpp:13:43: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from af77670f-c55c-4dba-bc56-d478c5ff46b0.cpp:12: af77670f-c55c-4dba-bc56-d478c5ff46b0.cpp: In function ‘int main()’: af77670f-c55c-4dba-bc56-d478c5ff46b0.cpp:22:20: error: ‘get_positive’ was not de clared in this scope    22 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ af77670f-c55c-4dba-bc56-d478c5ff46b0.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,4 +1,5 @@
-vector<float> get_positive(vector<float> l){
+```
+vector<float> get_positive(vector<float> l) {
     vector<float> result;
     for(float i : l){
         if(i > 0){
@@ -9,37 +10,29 @@ vector<float> get_positive(vector<float> l){
 }
 
 bool issame(vector<float> a, vector<float> b) {
-    if (a.size() != b.size()) {
+    if(a.size() != b.size())
         return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (abs(a[i] - b[i]) > 1e-9) {
+    for(int i = 0; i < a.size(); i++)
+        if(abs(a[i] - b[i]) > 1e-6)
             return false;
-        }
-    }
     return true;
 }
 
 int main() {
     vector<float> l;
-    float num;
-    cout << "Enter numbers (enter 'stop' when done): ";
-    while (cin >> num && num != 'stop') {
-        l.push_back(num);
+    float x;
+    cout << "Enter the length of array: ";
+    cin >> x;
+    l.resize(x);
+    for(int i = 0; i < x; i++) {
+        cout << "Enter element " << (i+1) << ": ";
+        cin >> l[i];
     }
-    if (l.size() < 2) {
-        cout << "Not enough numbers" << endl;
-    } else {
-        vector<float> pos = get_positive(l);
-        if (!issame(pos, l)) {
-            cout << "The positive numbers are: ";
-            for (float i : pos) {
-                cout << i << " ";
-            }
-            cout << endl;
-        } else {
-            cout << "All numbers were non-positive" << endl;
-        }
-    }
+    vector<float> p = get_positive(l);
+    bool s = issame(p, l);
+    if(s)
+        cout << "The two vectors are same." << endl;
+    else
+        cout << "The two vectors are not same." << endl;
     return 0;
 }
