36ea89b382d92be84a4b04f0da81a863a09464e7
beep boop <githubbot@vadim.me>
2024-07-09 03:34:09 +0200

    Solution 34 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/p33wwvjw Prompt: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::__cxx11::basi c_string<_CharT, _Traits, _Alloc>::basic_string(std::__cxx11::basic_string<_Char T, _Traits, _Alloc>&&) [with _CharT = char; _Traits = std::char_traits<char>; _A lloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:6667:12:   required from here /usr/include/c++/11/bits/basic_string.h:576:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   576 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,17 +1,23 @@
 ```cpp
 #include <string>
 #include <map>
-#include <utility>
 
 std::pair<int, int> mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
 
-    std::map<char, int> codeCount;
-    std::map<char, int> guessCount;
+    int codeArray[4];
+    int guessArray[4];
     for (int i = 0; i < 4; ++i) {
-        codeCount[code[i]]++;
-        guessCount[guess[i]]++;
+        codeArray[i] = code[i];
+        guessArray[i] = guess[i];
+    }
+
+    map<char, int> codeCount;
+    map<char, int> guessCount;
+    for (int i = 0; i < 4; ++i) {
+        codeCount[codeArray[i]]++;
+        guessCount[guessArray[i]]++;
     }
     for (auto& pair : codeCount) {
         if (pair.second > 0 && pair.second != guessCount[pair.first]) {
@@ -21,26 +27,11 @@ std::pair<int, int> mastermind(std::string code, std::string guess) {
 
     int codeIndex = 0;
     for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
+        if (codeArray[i] == guessArray[i]) {
             black++;
             codeIndex = i + 1;
         }
     }
 
-    return std::make_pair(white, black);
-}
-
-int main() {
-    std::string code = "XXXX";
-    std::string guess = "XXXX";
-
-    auto result = mastermind(code, guess);
-
-    int white = result.first;
-    int black = result.second;
-
-    std::cout << "White pegs: " << white << "\n";
-    std::cout << "Black pegs: " << black << "\n";
-
-    return 0;
+    return make_pair(white, black);
 }
