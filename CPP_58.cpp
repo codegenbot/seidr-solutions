843f5f898e1cb8bc95e321f04fd25ef4b505e0ff
beep boop <githubbot@vadim.me>
2024-07-09 08:59:58 +0200

    Solution 56 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uxs77z8s Prompt: In file included from /usr/include/c++/11/vector:67,                  from f39c7d74-0d1e-46e1-bdfc-2f52f56cc668.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from f39c7d74-0d1e-46e1-bdfc-2f52f56cc668.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ f39c7d74-0d1e-46e1-bdfc-2f52f56cc668.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from f39c7d74-0d1e-46e1-bdfc-2f52f56cc668.cpp:15: f39c7d74-0d1e-46e1-bdfc-2f52f56cc668.cpp: In function ‘int main()’: f39c7d74-0d1e-46e1-bdfc-2f52f56cc668.cpp:25:20: error: ‘common’ was not declared  in this scope    25 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ f39c7d74-0d1e-46e1-bdfc-2f52f56cc668.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,15 +1,14 @@
 #include <vector>
 #include <algorithm>
-#include <cassert>
+#include <iterator>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     std::sort(a.begin(), a.end());
     std::sort(b.begin(), b.end());
+
+    std::vector<int> result;
+    std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(result));
+    result.erase(std::unique(result.begin(), result.end()), result.end());
     
     return a == b;
-}
-
-int main() {
-    assert(issame(std::vector<int>{4, 3, 2, 8}, std::vector<int>{}));
-    return 0;
 }
