d04c7b1e5091e2c83bf67126625ad65c715eb989
beep boop <githubbot@vadim.me>
2024-07-09 21:36:03 +0200

    Solution 15 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/xub5v5hi Prompt: 6288d97e-1842-4bd4-9003-b00a6be94cae.cpp:3:18: error: ‘string’ was not declared in this scope     3 | int bowlingScore(string input) {       |                  ^~~~~~ 6288d97e-1842-4bd4-9003-b00a6be94cae.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | using namespace std;

--- bowling.cpp
+++ bowling.cpp
@@ -1,30 +1,27 @@
 ```cpp
 #include <string>
 
-namespace std {
-    using namespace std;
+namespace std { using namespace std; }
 
-    int bowlingScore(string input) {
-        int score = 0;
-        for (int i = 0; i < 10; i++) {
-            if (input[i] == 'X') {
-                score += 30;
-            } else if (input[i] == '/') {
-                int nextTwo = stoi(input.substr(i + 1, 2));
-                score += 10 + nextTwo;
+int bowlingScore(string input) {
+    int score = 0;
+    for (int i = 0; i < 10; i++) {
+        if (input[i] == 'X') {
+            score += 30;
+        } else if (input[i] == '/') {
+            int nextTwo = stoi(input.substr(i + 1, 2));
+            score += 10 + nextTwo;
+            i++;
+        } else {
+            int currentRoll = stoi(input.substr(i, 2));
+            if (currentRoll < 10) {
+                score += currentRoll;
                 i++;
             } else {
-                int currentRoll = stoi(input.substr(i, 2));
-                if (currentRoll < 10) {
-                    score += currentRoll;
-                    i++;
-                } else {
-                    int first = stoi(input.substr(i, 1));
-                    int second = stoi(input.substr(i + 1, 1));
-                    score += first + second;
-                }
+                int first = stoi(input.substr(i, 1));
+                int second = stoi(input.substr(i + 1, 1));
+                score += first + second;
             }
         }
-        return score;
     }
-}
+    return score;
