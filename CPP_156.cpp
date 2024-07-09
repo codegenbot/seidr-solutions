85df8dcc276c5ff768f1c7e6dd9bef0fc1f2c052
beep boop <githubbot@vadim.me>
2024-07-09 04:46:21 +0200

    Solution 11 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/urrbvh7e Prompt: 3e171b2e-fd55-4668-98ba-c1033e50acc8.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int num) {       | ^~~~~~ In file included from 3e171b2e-fd55-4668-98ba-c1033e50acc8.cpp:120: 3e171b2e-fd55-4668-98ba-c1033e50acc8.cpp: In function ‘int main()’: 3e171b2e-fd55-4668-98ba-c1033e50acc8.cpp:122:13: error: ‘int_to_mini_romank’ was  not declared in this scope   122 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,84 +1,73 @@
-std::string intToMiniRoman(int num) {
+```cpp
+#include <string>
+
+std::string int_to_mini_roman(int num) {
     std::string roman[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",
-                          "", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
-                          "", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
-                          "", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
-                          "", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
-                          "", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
-                          "", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
-                          "", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
-                          "", "C", "CI", "CII", "CIII", "CIV", "CV", "CVI", "CVII", "CVIII", "CX", "CXI", "CXII", "CXIII", "CXIV", "CXV", "CXVI", "CXVII", "CXVIII", "CXIX", "CXX",
-                          "", "CXXI", "CXXII", "CXXIII", "CXXIV", "CXXV", "CXXVI", "CXXVII", "CXXVIII", "CXXIX", "CXXX",
-                          "", "CXXXI", "CXXXII", "CXXXIII", "CXXXIV", "CXXXV", "CXXXVI", "CXXXVII", "CXXXVIII", "CXXXIX", "CXL",
-                          "", "CXLI", "CXLII", "CXLIII", "CXLIV", "CXLV", "CXLVI", "CXLVII", "CXLVIII", "CXLIX", "CV",
-                          "", "LIl", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX"};
+                      "", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                      "", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
+                      "", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                      "", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                      "", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                      "", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                      "", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                      "", "C", "CI", "CII", "CIII", "CIV", "CV", "CVI", "CVII", "CVIII", "CX", "CXI", "CXII", "CXIII", "CXIV", "CXV", "CXVI", "CXVII", "CXVIII", "CXIX", "CXX",
+                      "", "CXXI", "CXXII", "CXXIII", "CXXIV", "CXXV", "CXXVI", "CXXVII", "CXXVIII", "CXXIX", "CXXX",
+                      "", "CXXXI", "CXXXII", "CXXXIII", "CXXXIV", "CXXXV", "CXXXVI", "CXXXVII", "CXXXVIII", "CXXXIX", "CXL",
+                      "", "CXLI", "CXLII", "CXLIII", "CXLIV", "CXLV", "CXLVI", "CXLVII", "CXLVIII", "CXLIX", "CXlix",
+                      "", "M", "MI", "MII", "MIII", "MIV", "MV", "MXI", "MXII", "MXIII", "MXIV"};
 
-    if (num < 1 || num > 3999) {
-        return "";
-    }
+    std::string result = "";
 
-    std::string result;
-    do {
-        int remainder = num % 10;
-        switch (remainder) {
-            case 0:
-                break;
-
-            case 3:
-                result = roman[9] + result;
-                num /= 10;
-                break;
-
-            case 4:
-                result = roman[12] + result;
-                num -= 4;
-                break;
-
-            default: {
-                if (remainder > 3) {
-                    int threeTimes = remainder / 3;
-                    for (int i = 0; i < threeTimes; i++) {
-                        result = roman[9] + result;
-                    }
-                    remainder %= 3;
-                }
-
-                if (remainder == 1) {
-                    result = roman[8] + result;
-                } else if (remainder > 3) {
-                    for (int i = 0; i < threeTimes; i++) {
-                        result = roman[5] + result;
-                    }
-                    remainder %= 3;
-
-                    if (remainder == 1) {
-                        result = roman[8] + result;
-                    } else if (remainder > 0) {
-                        for (int i = 0; i < remainder; i++) {
-                            result = roman[1] + result;
-                        }
-                    }
-                } else {
-                    switch (remainder) {
-                        case 1:
-                            result = roman[5] + result;
-                            break;
-
-                        case 2:
-                            result = roman[6] + result;
-                            break;
-
-                        default:
-                            for (int i = 0; i < remainder; i++) {
-                                result = roman[1] + result;
-                            }
-                    }
-                }
-
-                num /= 10;
+    while (num > 0) {
+        if (num >= 1000) {
+            num -= 1000;
+            result += "M";
+        } else if (num >= 900) {
+            num -= 900;
+            result += "CM";
+        } else if (num >= 500) {
+            num -= 500;
+            result += "D";
+        } else if (num >= 400) {
+            num -= 400;
+            result += "CD";
+        } else if (num >= 100) {
+            int hundreds = num / 100;
+            for (int i = 0; i < hundreds; i++) {
+                result += "C";
             }
+            num %= 100;
+        } else if (num >= 90) {
+            num -= 90;
+            result += "XC";
+        } else if (num >= 50) {
+            num -= 50;
+            result += "L";
+        } else if (num >= 40) {
+            num -= 40;
+            result += "XL";
+        } else if (num >= 10) {
+            int ones = num / 10;
+            for (int i = 0; i < ones; i++) {
+                result += "X";
+            }
+            num %= 10;
+        } else if (num >= 9) {
+            num -= 9;
+            result += "IX";
+        } else if (num >= 5) {
+            num -= 5;
+            result += "V";
+        } else if (num >= 4) {
+            num -= 4;
+            result += "IV";
+        } else {
+            for (int i = 0; i < num; i++) {
+                result += "I";
+            }
+            num = 0;
         }
-    } while (num > 3999);
+    }
 
     return result;
 }
