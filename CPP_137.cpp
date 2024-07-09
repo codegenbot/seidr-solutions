67e49fb399bf8757811d3fa0f2ee1292bad7f9d3
beep boop <githubbot@vadim.me>
2024-07-09 05:10:04 +0200

    Solution 85 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fv53j2kl Prompt: 10550c5e-17ee-400b-a7a8-0269d04938a4.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 10550c5e-17ee-400b-a7a8-0269d04938a4.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 10550c5e-17ee-400b-a7a8-0269d04938a4.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 10550c5e-17ee-400b-a7a8-0269d04938a4.cpp:2:10: fatal error: boost/any.hpp: No su ch file or directory     2 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -5,9 +5,9 @@
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return boost::any_cast<int>(a) > boost::any_cast<float>(b) ? a : b;
+        return (int)boost::any_cast<int>(a) > boost::any_cast<float>(b) ? a : b;
     } else if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return boost::any_cast<int>(a) > boost::any_cast<double>(b) ? a : b;
+        return (int)boost::any_cast<int>(a) > boost::any_cast<double>(b) ? a : b;
     } else if (a.type() == typeid(std::string) && b.type() == typeid(int)) {
         std::string str = boost::any_cast<std::string>(a);
         int num = boost::any_cast<int>(b);
@@ -24,8 +24,6 @@ boost::any compare_one(boost::any a, boost::any b) {
         std::string str = boost::any_cast<std::string>(a);
         double num = boost::any_cast<double>(b);
         return std::stod(str) > num ? a : b;
-    } else if (a.type() != b.type()) {
-        throw std::invalid_argument("Different types");
     }
-    return "None";
+    return a;
 }
