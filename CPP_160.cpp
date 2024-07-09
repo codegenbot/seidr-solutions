545466e32b015540ab5d13d9cd444671158e5379
beep boop <githubbot@vadim.me>
2024-07-09 04:58:16 +0200

    Solution 36 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xuigdtqp Prompt: In file included from /usr/include/c++/11/vector:67,                  from 599056ff-9729-4c7e-aef4-76e55d442f14.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 599056ff-9729-4c7e-aef4-76e55d442f14.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 599056ff-9729-4c7e-aef4-76e55d442f14.cpp:4:49: error: expected primary-expressio n before ‘int’     4 | int do_algebra(vector<string> operators, vector<int> operands) {       |                                                 ^~~ 599056ff-9729-4c7e-aef4-76e55d442f14.cpp:4:62: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | int do_algebra(vector<string> operators, vector<int> operands) {       |                                                              ^ In file included from 599056ff-9729-4c7e-aef4-76e55d442f14.cpp:24: 599056ff-9729-4c7e-aef4-76e55d442f14.cpp: In function ‘int main()’: 599056ff-9729-4c7e-aef4-76e55d442f14.cpp:26:23: error: ‘do_algebra’ cannot be us ed as a function    26 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,117 +1,21 @@
-#include <cmath>
-#include <stack>
-#include <string>
 #include <vector>
 
-int do_algebra(vector<string> operator_, vector<int> operand) {
-    string expression = "";
-    for (int i = 0; i < operator_.size(); i++) {
-        expression += to_string(operand[i]);
-        expression += operator_[i];
+int do_algebra(std::vector<std::string> operators, std::vector<int> operands) {
+    int result = operands[0];
+    for (int i = 0; i < operators.size(); i++) {
+        if (operators[i] == "+") {
+            result += operands[i + 1];
+        } else if (operators[i] == "-") {
+            result -= operands[i + 1];
+        } else if (operators[i] == "*") {
+            result *= operands[i + 1];
+        } else if (operators[i] == "/") {
+            if(operands[i+1] != 0) {
+                result /= operands[i + 1];
+            } else {
+                return -1;
+            }
+        } 
     }
-    expression += to_string(operand[operator_.size()]);
-    
-    int result = eval(expression);
-    
     return result;
-}
-
-long long eval(const string& s) {
-    int n = s.size();
-    stack<int> st;
-    for (int i = 0; i < n; i++) {
-        if (isdigit(s[i])) {
-            int num = 0;
-            while (i < n && isdigit(s[i])) {
-                num = num * 10 + (s[i] - '0');
-                i++;
-            }
-            st.push(num);
-        } else if (s[i] == '(') {
-            i++;
-            stack<int> temp;
-            while (i < n && s[i] != ')') {
-                if (isdigit(s[i])) {
-                    int num = 0;
-                    while (i < n && isdigit(s[i])) {
-                        num = num * 10 + (s[i] - '0');
-                        i++;
-                    }
-                    temp.push(num);
-                } else if (s[i] == '(') {
-                    temp.push(eval("(" + s.substr(i++) + ")"));
-                } else if (s[i] == ')') {
-                    break;
-                }
-            }
-            while (!temp.empty()) {
-                st.push(temp.top());
-                temp.pop();
-            }
-        }
-    }
-    
-    int res = 0;
-    char op = '+';
-    for (; !st.empty() || op != '+' ; ) {
-        if (op == '+') {
-            res += st.top();
-            st.pop();
-        } else if (op == '-') {
-            res -= st.top();
-            st.pop();
-        } else if (op == '*') {
-            int num = st.top() * 1;
-            st.pop();
-            while (!st.empty() && st.top() == '(') {
-                st.pop();
-            }
-            if (!st.empty()) {
-                num *= st.top();
-                st.pop();
-            }
-            res += num;
-        } else if (op == '/') {
-            int num = st.top() / 1;
-            st.pop();
-            while (!st.empty() && st.top() == '(') {
-                st.pop();
-            }
-            if (!st.empty()) {
-                num /= st.top();
-                st.pop();
-            }
-            res += num;
-        } else if (op == '**') {
-            int num = pow(st.top(), 1);
-            st.pop();
-            while (!st.empty() && st.top() == '(') {
-                st.pop();
-            }
-            if (!st.empty()) {
-                num = pow(num, st.top());
-                st.pop();
-            }
-            res += num;
-        } else if (op == '//') {
-            int num = st.top() / 1;
-            st.pop();
-            while (!st.empty() && st.top() == '(') {
-                st.pop();
-            }
-            if (!st.empty()) {
-                num /= st.top();
-                st.pop();
-            }
-            res += num;
-        }
-        op = s[i++];
-    }
-    
-    return res;
-}
-
-int main() {
-    assert(do_algebra({"//", "*"}, {7, 3, 4}) == 8);
-    return 0;
 }
