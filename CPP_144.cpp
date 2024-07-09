b780f72459afd4578c96aca4be362239eedf162a
beep boop <githubbot@vadim.me>
2024-07-09 10:18:35 +0200

    Solution 3 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/289d4dlu Prompt: /usr/include/c++/11/sstream:142:39: error: ‘const __string_type’ {aka ‘const cla ss std::__cxx11::basic_string<char>’} has no member named ‘size’   142 |         _M_string(__str.data(), __str.size(), __str.get_allocator())       |                                 ~~~~~~^~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 83ce1fb1-ee6a-45ab-bbd7-918d7d62adca.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 83ce1fb1-ee6a-45ab-bbd7-918d7d62adca.cpp:19:5:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,15 +1,14 @@
-```cpp
 #include <string>
 #include <sstream>
 
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = 0, b = 0, c = 0, d = 0;
+    char arr1[6], arr2[4];
     stringstream s1(x), s2(n);
+    s1 >> arr1;
     char slash;
-    s1 >> a >> slash >> b;
-    s2 >> c >> slash >> d;
+    s2 >> arr2[0] >> slash >> arr2[1];
     
-    return (a*d == b*c);
+    return (atoi(arr1) * atoi(arr2[1].c_str()) == atoi(arr2[0].c_str()) * atoi(&slash + 1));
 }
