9c24b57004da41fd9cb46c75e55d23ef8c18083b
beep boop <githubbot@vadim.me>
2024-07-09 02:41:25 +0200

    Solution 40 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0xw9ottc Prompt: for array subscript    17 |         } else if (ops[i] == "*") {       |                       ^ 968e3145-6ddb-4d3e-8c11-25e64cc879ec.cpp:19:23: error: invalid types ‘int[int]’ for array subscript    19 |         } else if (ops[i] == "//") {       |                       ^ 968e3145-6ddb-4d3e-8c11-25e64cc879ec.cpp:21:23: error: invalid types ‘int[int]’ for array subscript    21 |         } else if (ops[i] == "**") {       |                       ^ In file included from 968e3145-6ddb-4d3e-8c11-25e64cc879ec.cpp:29: 968e3145-6ddb-4d3e-8c11-25e64cc879ec.cpp: In function ‘int main()’: 968e3145-6ddb-4d3e-8c11-25e64cc879ec.cpp:31:23: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    31 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~       |                       |       |                       <brace-enclosed initializer list> 968e3145-6ddb-4d3e-8c11-25e64cc879ec.cpp:8:31: note:   initializing argument 1 o f ‘int do_algebra(int, std::vector<int>)’     8 | int do_algebra(vector<string> ops, vector<int> nums) {       |                ~~~~~~~~~~~~~~~^~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,24 +1,36 @@
-```cpp
 #include <vector>
 #include <cmath>
+using namespace std;
 
 int do_algebra(vector<string> ops, vector<int> nums) {
-    int result = nums[0];
-    int temp = 1;
-
-    for (int i = 1; i < ops.size(); i++) {
+    int result = 0;
+    for (int i = 0; i < ops.size(); i++) {
         if (ops[i] == "+") {
-            result += temp * nums[i];
+            result += nums[i];
         } else if (ops[i] == "-") {
-            result -= temp * nums[i];
+            result -= nums[i];
         } else if (ops[i] == "*") {
-            temp *= nums[i];
-        } else if (ops[i] == "/") {
-            temp /= nums[i];
-        } else if (ops[i] == "+") { 
-            result += temp * nums[i];
+            result *= nums[i];
+        } else if (ops[i] == "//" || ops[i] == "**") {
+            int temp = 1;
+            for(int j = 0; j < i; j++) {
+                if(ops[j] == "*") 
+                    temp *= nums[j]; 
+                else if(ops[j] == "//")
+                    temp /= nums[j];
+                else
+                    temp = pow((double)temp, (double)nums[j]);
+            }
+            result = temp;
         }
     }
 
     return result;
+}
+
+int main() {
+    vector<string> ops = {"+", "+"};
+    vector<int> nums = {2, 3};
+    cout << do_algebra(ops, nums);
+    return 0;
 }
