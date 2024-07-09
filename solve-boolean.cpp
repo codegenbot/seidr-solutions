959573b2d5a926efa208de524eff28ee6b10dcf7
beep boop <githubbot@vadim.me>
2024-07-09 03:01:19 +0200

    Solution 39 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/gfhqqe4z Prompt: bea2c86c-2ef5-49f5-931a-1cdeae7fe207.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ bea2c86c-2ef5-49f5-931a-1cdeae7fe207.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ bea2c86c-2ef5-49f5-931a-1cdeae7fe207.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ bea2c86c-2ef5-49f5-931a-1cdeae7fe207.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -3,16 +3,16 @@ bool solveBoolean(std::string s) {
     int i = 0, n = s.length();
     bool res = true;
     while(i < n){
-        if(s[i] == '|') {
+        if(s[i] == '&') {
+            i++;
             char c1 = (s[i-1] == 'T'? true : false);
-            char c2 = (i+1 < n) ? (s[i+1] == 'T'? true : false) : ((c1)? false: true);
-            res = res || c1 && c2;
-            i += s[i] == 'F' ? 2 : 1;
-        } else if(s[i] == '&') {
+            char c2 = (s[i] == 'T'? true : false);
+            res &= (c1 && c2);
+        } else if(s[i] == '|') {
+            i++;
             char c1 = (s[i-1] == 'T'? true : false);
-            char c2 = (i+1 < n) ? (s[i+1] == 'T'? true : false) : ((c1)? false: true);
-            res &= c1 && c2;
-            i += s[i] == 'F' ? 2 : 1;
+            char c2 = (s[i] == 'T'? true : false);
+            res |= (c1 || c2);
         }
     }
     return res;
