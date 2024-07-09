0924f70b043b9d446c525142bdcfa1c028c70159
beep boop <githubbot@vadim.me>
2024-07-10 01:46:00 +0200

    Solution 23 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uzrnpb22 Prompt:    10 | int main() {       |     ^~~~ In file included from 5c345ca6-034b-4084-9b9f-6b079eed551f.cpp:33: 5c345ca6-034b-4084-9b9f-6b079eed551f.cpp: In function ‘int main()’: 5c345ca6-034b-4084-9b9f-6b079eed551f.cpp:43:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    43 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 5c345ca6-034b-4084-9b9f-6b079eed551f.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ In file included from 5c345ca6-034b-4084-9b9f-6b079eed551f.cpp:33: 5c345ca6-034b-4084-9b9f-6b079eed551f.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,14 +1,13 @@
 #include <algorithm>
 #include <vector>
 #include <iostream>
-#include <initializer_list>
 
-bool issame(vector<int> a, vector<int> b){
+bool std::issame(std::vector<int> a, std::vector<int> b){
     return (a == b);
 }
 
 int main() {
-    vector<int> input;
+    std::vector<int> input;
     int n;
     cin >> n;
     for(int i = 0; i < n; i++) {
@@ -18,14 +17,35 @@ int main() {
     }
     
     auto unique_end = std::unique(input.begin(), input.end());
-    vector<int> output(input.begin(), unique_end);
-    sort(output.begin(), output.end());
+    std::vector<int> output(input.begin(), unique_end);
+    std::sort(output.begin(), output.end());
     
     for (int i : output) {
         cout << i << " ";
     }
     
-    assert(std::issame(std::vector<int>({0, 2, 3, 5, 9, 123}), std::vector<int>(output.begin(), output.end())));
-    
+    assert(std::equal({0, 2, 3, 5, 9, 123}.begin(), {0, 2, 3, 5, 9, 123}.end(), std::vector<int>(std::distinct(input.begin(), input.end()).begin(), std::distinct(input.end())));
+
     return 0;
+}
+
+template<typename It>
+It std::distinct(It first, It last) {
+    if (first == last)
+        return first;
+
+    std::vector<typename std::iterator_traits<It>::value_type> result;
+    for (; first != last; ++first) {
+        bool duplicate = false;
+        for (const auto& value : result) {
+            if (*first == value) {
+                duplicate = true;
+                break;
+            }
+        }
+        if (!duplicate)
+            result.push_back(*first);
+    }
+
+    return result.begin();
 }
