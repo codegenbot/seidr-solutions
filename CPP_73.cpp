38083f396a5ec636d90decb4e62c42f45bd1176f
beep boop <githubbot@vadim.me>
2024-07-09 08:53:59 +0200

    Solution 9 of CPP/73, ratio 0.875 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/iatcrbo9 Prompt: 2843b592-ff91-4770-a6a4-a0df4ca24e90.cpp:1:21: error: ‘vector’ was not declared in this scope     1 | int smallest_change(vector<int> arr){       |                     ^~~~~~ 2843b592-ff91-4770-a6a4-a0df4ca24e90.cpp:1:28: error: expected primary-expressio n before ‘int’     1 | int smallest_change(vector<int> arr){       |                            ^~~ In file included from 2843b592-ff91-4770-a6a4-a0df4ca24e90.cpp:11: 2843b592-ff91-4770-a6a4-a0df4ca24e90.cpp: In function ‘int main()’: 2843b592-ff91-4770-a6a4-a0df4ca24e90.cpp:13:28: error: ‘smallest_change’ cannot be used as a function    13 |     assert (smallest_change({0, 1}) == 1);       |             ~~~~~~~~~~~~~~~^~~~~~~~

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,4 +1,6 @@
-int smallest_change(vector<int> arr){
+#include<vector>
+
+int smallest_change(std::vector<int> arr){
     int changes = 0;
     for (int i = 0; i < arr.size() / 2; ++i) {
         if (arr[i] != arr[arr.size() - 1 - i]) {
