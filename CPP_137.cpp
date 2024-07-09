9a515479f3108485138eb8f8e7250af98c1dc6b9
beep boop <githubbot@vadim.me>
2024-07-09 07:09:44 +0200

    Solution 76 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/50q1gn5t Prompt: f9492125-904a-4a1d-9c28-900a65aaa355.cpp:1:1: error: ‘boost’ does not name a typ e     1 | boost::any compare_one(boost::any a, boost::any b) {       | ^~~~~ In file included from f9492125-904a-4a1d-9c28-900a65aaa355.cpp:36: f9492125-904a-4a1d-9c28-900a65aaa355.cpp: In function ‘int main()’: f9492125-904a-4a1d-9c28-900a65aaa355.cpp:38:13: error: ‘boost’ has not been decl ared    38 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ f9492125-904a-4a1d-9c28-900a65aaa355.cpp:38:29: error: ‘string’ was not declared  in this scope    38 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ f9492125-904a-4a1d-9c28-900a65aaa355.cpp:38:37: error: ‘compare_one’ was not dec lared in this scope    38 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,39 +1,39 @@
-Here's the modified code:
-```cpp
 #include <boost/any.hpp>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return boost::any_cast<int>(a) > boost::any_cast<int>(b)
-            ? boost::any(a)
-            : boost::any(b);
+        return any(a) > any(b) ? a : b;
     } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        return boost::any_cast<float>(a) > boost::any_cast<float>(b)
-            ? boost::any(a)
-            : boost::any(b);
+        return any_cast<float>(a) > any_cast<float>(b)
+            ? any(a)
+            : any(b);
     } else if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return boost::any_cast<float>(b) > boost::any_cast<int>(a)
-            ? boost::any(b)
-            : boost::any(std::stoi(boost::any_cast<std::string>(b)).compare(std::to_string(boost::any_cast<int>(a))) > 0
-                ? b
-                : a;
+        return any_cast<float>(b) > any_cast<int>(a)
+            ? any(b)
+            : any("None");
     } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return boost::any_cast<int>(a) > boost::any_cast<float>(b)
-            ? boost::any(a)
-            : boost::any(b);
-    } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        std::string a_str = boost::any_cast<std::string>(a);
-        std::string b_str = boost::any_cast<std::string>(b);
-        return std::stod(b_str) > std::stod(a_str)
-            ? a
-            : b;
-    } else if (a.type() == typeid(std::string) && b.type() == typeid(float)) {
-        return std::stod(boost::any_cast<std::string>(b)) > boost::any_cast<float>(a)
-            ? b
-            : a;
-    } else if (a.type() == typeid(float) && b.type() == typeid(std::string)) {
-        return boost::any_cast<float>(a) > std::stod(boost::any_cast<std::string>(b))
-            ? a
-            : b;
+        return any_cast<int>(a) > any_cast<float>(b)
+            ? any(a)
+            : any("None");
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string a_str = any_cast<string>(a);
+        string b_str = any_cast<string>(b);
+        return stod(b_str) > stod(a_str)
+            ? any(a)
+            : any(b);
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        return stod(any_cast<string>(b)) > any_cast<float>(a)
+            ? any(b)
+            : any("None");
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        return any_cast<float>(a) > stod(any_cast<string>(b))
+            ? any(a)
+            : any("None");
     }
-    return boost::any();
+    return "None";
+}
