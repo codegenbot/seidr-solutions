03b5d0c7f9fdbbe1db9bba7b35c61d059100589f
beep boop <githubbot@vadim.me>
2024-07-09 15:58:08 +0200

    Solution 40 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/40ydl3g4 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,64 +1,42 @@
-Here is the completed code:
-
-```cpp
+#include <iostream>
 #include <string>
-#include <algorithm>
 #include <boost/any.hpp>
-#include <boost/lexical_cast.hpp>
 
 using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return b;
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        return b;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if (boost::lexical_cast<double>(a.convert_to<string>()) > boost::lexical_cast<double>(b.convert_to<string>())) {
+    if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return max((int)a.convert_to<int>(), (double)b.convert_to<double>());
+    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return max((double)a.convert_to<double>(), (int)b.convert_to<int>());
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if ((string)a.convert_to<string>() > (string)b.convert_to<string>())
             return a;
-        }
-        else if (boost::lexical_cast<double>(a.convert_to<string>()) < boost::lexical_cast<double>(b.convert_to<string>())) {
+        else if ((string)b.convert_to<string>() > (string)a.convert_to<string>())
             return b;
-        }
-        else {
+        else
+            return boost::any("None");
+    } else if (a.type() == typeid(string) && b.type() != typeid(string)) {
+        if (b.type() == typeid(double)) {
+            string s = to_string((double)b.convert_to<double>());
+            if ((string)a.convert_to<string>() > s)
+                return a;
+            else
+                return boost::any("None");
+        } else {
             return boost::any("None");
         }
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        string str = boost::lexical_cast<string>(a);
-        if (boost::lexical_cast<double>(str) > boost::lexical_cast<double>(b.convert_to<string>())) {
-            return a;
-        }
-        else if (boost::lexical_cast<double>(str) < boost::lexical_cast<double>(b.convert_to<string>())) {
-            return b;
-        }
-        else {
+    } else if (b.type() == typeid(string) && a.type() != typeid(string)) {
+        if (a.type() == typeid(double)) {
+            string s = to_string((double)a.convert_to<double>());
+            if ((string)b.convert_to<string>() > s)
+                return b;
+            else
+                return boost::any("None");
+        } else {
             return boost::any("None");
         }
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        string str = boost::lexical_cast<string>(b);
-        if (boost::lexical_cast<double>(a.convert_to<string>()) > boost::lexical_cast<double>(str)) {
-            return a;
-        }
-        else if (boost::lexical_cast<double>(a.convert_to<string>()) < boost::lexical_cast<double>(str)) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
-    }
-    else {
-        if (boost::lexical_cast<double>(a.convert_to<string>()) > boost::lexical_cast<double>(b.convert_to<string>())) {
-            return a;
-        }
-        else if (boost::lexical_cast<double>(a.convert_to<string>()) < boost::lexical_cast<double>(b.convert_to<string>())) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
+    } else {
+        return boost::any("None");
     }
 }
