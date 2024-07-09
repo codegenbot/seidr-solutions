3071e0695f5a7db33895e5f3d1224b7a4cbe41c8
beep boop <githubbot@vadim.me>
2024-07-09 05:24:50 +0200

    Solution 8 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dfvmwimw Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d78b2810-5c3f-40b3-affd-29f6fae41 171.py", line 1     Here is the solution:                 ^^^^^^^^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,8 +1,6 @@
-```"
-def find_zero(xs: list):
+def find_zero(xs):
     n = len(xs)
     if n % 2 != 0:
         raise ValueError("xs must have even number of coefficients")
     max_coeff_idx = xs.index(max(filter(None, xs)))
     return -xs[max_coeff_idx] / xs[max_coeff_idx+1]
-"""
