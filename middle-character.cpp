a8ffb9fcae2cbefc77b14c18f9ade0e093cb6799
beep boop <githubbot@vadim.me>
2024-07-09 21:17:23 +0200

    Solution 6 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/sj2d2vmv Prompt: in this scope    11 |     string s;       |     ^~~~~~ 224cd3ee-f44a-4234-806f-e05b5e670090.cpp:12:5: error: ‘cin’ was not declared in this scope    12 |     cin >> s;       |     ^~~ 224cd3ee-f44a-4234-806f-e05b5e670090.cpp:12:12: error: ‘s’ was not declared in t his scope    12 |     cin >> s;       |            ^ 224cd3ee-f44a-4234-806f-e05b5e670090.cpp:13:5: error: ‘cout’ was not declared in  this scope    13 |     cout << middleCharacter(s) << endl;       |     ^~~~ 224cd3ee-f44a-4234-806f-e05b5e670090.cpp:13:13: error: ‘middleCharacter’ was not  declared in this scope    13 |     cout << middleCharacter(s) << endl;       |             ^~~~~~~~~~~~~~~ 224cd3ee-f44a-4234-806f-e05b5e670090.cpp:13:35: error: ‘endl’ was not declared i n this scope    13 |     cout << middleCharacter(s) << endl;       |                                   ^~~~

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-string middleCharacter(string s);
-
 string middleCharacter(string s) {
-    int n = s.size();
+    int n = s.length();
     if (n % 2 == 0) {
-        return s.substr(n / 2 - 1, 2);
+        return s.substr(n/2 - 1, 2);
     } else {
-        return s.substr(n / 2, 1);
+        return s.substr(n/2, 1);
     }
 }
 
 int main() {
     string s;
-    std::cin >> s;
-    std::cout << middleCharacter(s) << std::endl;
+    cin >> s;
+    cout << middleCharacter(s) << endl;
     return 0;
 }
