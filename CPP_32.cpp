410b75e50f2aa826ed8ac2945d92a8929e9c2b15
beep boop <githubbot@vadim.me>
2024-07-09 03:42:56 +0200

    Solution 82 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h5y6hrvk Prompt: 58bdfbc0-f28f-431c-980a-a608ff9e2a89.cpp:30:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    30 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 58bdfbc0-f28f-431c-980a-a608ff9e2a89.cpp:33:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    33 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 58bdfbc0-f28f-431c-980a-a608ff9e2a89.cpp:33:36: error: ‘find_zero’ cannot be use d as a function    33 |         solution = find_zero(coeffs);       |                                    ^ In file included from 58bdfbc0-f28f-431c-980a-a608ff9e2a89.cpp:17: 58bdfbc0-f28f-431c-980a-a608ff9e2a89.cpp:34:21: error: ‘poly’ was not declared i n this scope    34 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 58bdfbc0-f28f-431c-980a-a608ff9e2a89.cpp:34:17: error: ‘abs’ was not declared in  this scope    34 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,11 +1,12 @@
+#include <iostream>
+using namespace std;
+
 double find_zero(vector<double> xs) {
     double coeffs = xs[0];
     vector<double> polyCoeffs;
     int n = xs.size();
-    double result;
-    vector<double> polyCoeffs;
-    int n = xs.size();
-    coeffs = xs[0];
+    
+    double polys;
     for (int i = 1; i < n; i++) {
         if ((i & 1) == 0) {
             polyCoeffs.insert(polyCoeffs.begin(), -coeffs*xs[i]);
@@ -13,7 +14,21 @@ double find_zero(vector<double> xs) {
             coeffs = xs[i];
         }
     }
-    double poly(double c, double s) { return c + s; }
-    result = -coeffs / coeffs;
-    return result;
+
+    double result = 0.0;
+    for (int i = 0; i < polyCoeffs.size(); i++) {
+        if (i == 0) {
+            result += polyCoeffs[0];
+        } else {
+            int j = 1;
+            while (j <= i) {
+                result += polys(i, j)*polyCoeffs[i-j] * pow(xs[0], j);
+                j++;
+            }
+        }
+    }
+
+    return -coeffs / coeffs;
 }
+
+double poly(double c, double s) { return c + s; }
