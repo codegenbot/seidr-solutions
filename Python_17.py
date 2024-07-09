1698f9321c5387008f8d68679e0d05d9f411188f
beep boop <githubbot@vadim.me>
2024-07-10 01:33:46 +0200

    Solution 16 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dn8nhlqk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/88eb7dda-b199-466d-bb6d-60f0c7359 403.py", line 1, in <module>     def parse_music(music_string: str) -> List[int]:                                           ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_17.py
+++ Python_17.py
@@ -1,5 +1,6 @@
-def parse_music(music_string: str) -> List[int]:
-    durations = {"o": 4, "o|": 2, ".|": 1}
+from typing import List
 
-    musical_notes = music_string.split()
-    return [durations[note] for note in musical_notes]
+
+def parse_music(music_string: str) -> List[int]:
+    notes = {"o": 4, "o|": 2, ".|": 1}
+    return [notes[note] for note in music_string.split()]
