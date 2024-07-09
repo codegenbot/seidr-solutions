db249b4cb212e9f823b0484996fd672f13ce511a
beep boop <githubbot@vadim.me>
2024-07-09 15:28:12 +0200

    Solution 28 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5uknokbc Prompt: 6bcaf68b-bc46-4c14-9f18-90b68bd68003.cpp:6:5: error: ‘assert’ was not declared i n this scope     6 |     assert (same_chars("aabb", "aaccc") == false);       |     ^~~~~~ 6bcaf68b-bc46-4c14-9f18-90b68bd68003.cpp:1:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?   +++ |+#include <cassert>     1 | ```cpp 6bcaf68b-bc46-4c14-9f18-90b68bd68003.cpp: At global scope: 6bcaf68b-bc46-4c14-9f18-90b68bd68003.cpp:11:5: error: redefinition of ‘int main( )’    11 | int main(){       |     ^~~~ 6bcaf68b-bc46-4c14-9f18-90b68bd68003.cpp:5:5: note: ‘int main()’ previously defi ned here     5 | int main() {       |     ^~~~ In file included from 6bcaf68b-bc46-4c14-9f18-90b68bd68003.cpp:10: 6bcaf68b-bc46-4c14-9f18-90b68bd68003.cpp: In function ‘int main()’: 6bcaf68b-bc46-4c14-9f18-90b68bd68003.cpp:12:13: error: ‘same_chars’ was not decl ared in this scope    12 |     assert (same_chars("aabb", "aaccc") == false);       |             ^~~~~~~~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -1,16 +1,7 @@
 #include <cassert>
-#include <string>
 
 bool same_chars(string s0, string s1) {
-    if(s0.length() != s1.length()) {
-        return false;
-    }
-    for(int i = 0; i < s0.length(); i++) {
-        if(count(s0.begin(), s0.end(), s1[i]) != 1) {
-            return false;
-        }
-    }
-    return true;
+    return s0 == s1;
 }
 
 int main() {
