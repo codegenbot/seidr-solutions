b25ce0e95f4218e001b535ec20fc6d4d35d8108a
beep boop <githubbot@vadim.me>
2024-07-09 04:20:50 +0200

    Solution 9 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/epl3akgm Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 50375708-5b83-40ec-879d-bf3c0151c41e.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 50375708-5b83-40ec-879d-bf3c0151c41e.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 50375708-5b83-40ec-879d-bf3c0151c41e.cpp:5: 50375708-5b83-40ec-879d-bf3c0151c41e.cpp: In function ‘int main()’: 50375708-5b83-40ec-879d-bf3c0151c41e.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 50375708-5b83-40ec-879d-bf3c0151c41e.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -2,32 +2,45 @@
 #include <algorithm>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a[0] == b[0] && a[1] == b[1];
+    return (a[0] == b[0] && a[1] == b[1]);
 }
 
-std::vector<int> minPath(std::vector<std::vector<int>> grid, int k){
-    int n = grid.size(), m = grid[0].size();
-    std::vector<std::vector<int>> dp(n, std::vector<int>(m, -1));
-    dp[0][0] = grid[0][0];
+std::vector<int> minPath(std::vector<std::vector<int>> grid, int k) {
+    int n = grid.size();
+    int m = grid[0].size();
+
+    std::vector<std::vector<int>> dp(n, std::vector<int>(m, INT_MAX));
+    dp[0][0] = 0;
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
-            if (i > 0) dp[i][j] = std::max(dp[i][j], dp[i-1][j] + grid[i][j]);
-            if (j > 0) dp[i][j] = std::max(dp[i][j], dp[i][j-1] + grid[i][j]);
+            if (i > 0)
+                dp[i][j] = std::min(dp[i][j], dp[i - 1][j] + issame(grid[i][j], grid[i - 1][j]) ? 0 : grid[i][j]);
+            if (j > 0)
+                dp[i][j] = std::min(dp[i][j], dp[i][j - 1] + issame(grid[i][j], grid[i][j - 1]) ? 0 : grid[i][j]);
         }
     }
+
+    int cost = dp[n - 1][m - 1];
+    if (cost > k)
+        return std::vector<int>();
 
     std::vector<int> path;
-    int curX = n-1, curY = m-1;
-    while (!issame({curX, curY}, {0, 0})) {
-        path.push_back(grid[curX][curY]);
-        if (curX > 0 && dp[curX][curY] - grid[curX][curY] == dp[curX-1][curY]) {
-            curX--;
+    int curCost = cost;
+    int curI = n - 1;
+    int curJ = m - 1;
+
+    while (curI > 0 || curJ > 0) {
+        path.push_back(grid[curI][curJ]);
+        if (curI > 0 && dp[curI - 1][curJ] + issame(grid[curI][curJ], grid[curI - 1][curJ]) < curCost) {
+            curI -= 1;
         } else {
-            curY--;
+            curJ -= 1;
         }
     }
+
     path.push_back(grid[0][0]);
+    std::reverse(path.begin(), path.end());
 
     return path;
-}
+}  
