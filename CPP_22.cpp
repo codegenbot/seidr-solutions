15b17acd33c2022b039fed4bfda42e1429402972
beep boop <githubbot@vadim.me>
2024-07-09 02:51:45 +0200

    Solution 12 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/bvoim4el Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7e38f1c8-0fb9-48d0-9a28-2d5278161538.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7e38f1c8-0fb9-48d0-9a28-2d5278161538.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7e38f1c8-0fb9-48d0-9a28-2d5278161538.cpp:11: 7e38f1c8-0fb9-48d0-9a28-2d5278161538.cpp: In function ‘int main()’: 7e38f1c8-0fb9-48d0-9a28-2d5278161538.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 7e38f1c8-0fb9-48d0-9a28-2d5278161538.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,42 +1,22 @@
+#include <boost/any.hpp>
 #include <vector>
 #include <list>
-#include <any>
-#include <boost/any.hpp>
+#include <typeindex>
 
-using namespace std;
-
-bool areSame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-vector<any> list_any(int n) {
-    vector<any> values;
-    for (int i = 0; i < n; ++i) {
-        if (i % 2 == 0) {
-            values.push_back(i);
-        } else {
-            values.push_back(any('a' + (i / 2)));
-        }
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    if (a.size() != b.size()) return false;
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) return false;
     }
-    return values;
+    return true;
 }
 
-vector<int> filter_integers(vector<any> values) {
-    vector<int> result;
+std::vector<int> filter_integers(std::list<boost::any> values) {
+    std::vector<int> result;
     for (const auto& value : values) {
-        if (any_cast<int>(value).has_value()) {
-            result.push_back(any_cast<int>(value));
-        } else if (any_cast<char>(value).has_value()) {
-            int num = 0;
-            while (any_cast<char>(value)) {
-                num++;
-                any_cast<char>(value) = 'a' + num % 26;
-            }
+        if (boost::any_cast<int>(value).type() == typeid(int)) {
+            result.push_back(boost::any_cast<int>(value));
         }
     }
     return result;
-}
-
-int main() {
-    assert(areSame(filter_integers(list_any(6)), vector<int>{3, 3, 3, 1, 1, 1}) );
 }
