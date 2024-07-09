af222702cc760c4e470e1610c3db63d0968e403f
beep boop <githubbot@vadim.me>
2024-07-09 13:40:55 +0200

    Solution 49 of CPP/135, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/bvib8907 Prompt: 4a3af203-a9ff-461a-9b9c-273c37b26a0b.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | int can_arrange(vector<int> arr){       |                 ^~~~~~ 4a3af203-a9ff-461a-9b9c-273c37b26a0b.cpp:1:24: error: expected primary-expressio n before ‘int’     1 | int can_arrange(vector<int> arr){       |                        ^~~ In file included from 4a3af203-a9ff-461a-9b9c-273c37b26a0b.cpp:9: 4a3af203-a9ff-461a-9b9c-273c37b26a0b.cpp: In function ‘int main()’: 4a3af203-a9ff-461a-9b9c-273c37b26a0b.cpp:11:24: error: ‘can_arrange’ cannot be u sed as a function    11 |     assert (can_arrange({})==-1);       |             ~~~~~~~~~~~^~~~

--- CPP_135.cpp
+++ CPP_135.cpp
@@ -1,9 +1,8 @@
 #include<vector>
-#include<algorithm>
 
 int can_arrange(vector<int> arr){
     for(int i = 1; i < arr.size(); i++){
-        if(std::find(arr.begin(), arr.end(), arr[i]) != arr.end() && std::find(arr.begin(), arr.end(), arr[i-1]) == arr.end())
+        if(arr[i] <= arr[i-1])
             return i - 1;
     }
     return -1;
