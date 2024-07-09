591c747eefa7b4ac0953f7ddcb0c65e23c03edf7
beep boop <githubbot@vadim.me>
2024-07-09 21:03:49 +0200

    Solution 64 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/owmm65gu Prompt: b1853f93-2d41-47b7-a8fc-9a3becc6e53a.cpp:35:5: error: ‘assert’ was not declared in this scope    35 |     assert(int_to_mini_romank(1000) == 1);       |     ^~~~~~ b1853f93-2d41-47b7-a8fc-9a3becc6e53a.cpp:1:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?   +++ |+#include <cassert>     1 | ```cpp b1853f93-2d41-47b7-a8fc-9a3becc6e53a.cpp: At global scope: b1853f93-2d41-47b7-a8fc-9a3becc6e53a.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ b1853f93-2d41-47b7-a8fc-9a3becc6e53a.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main() {       |     ^~~~ In file included from b1853f93-2d41-47b7-a8fc-9a3becc6e53a.cpp:39: b1853f93-2d41-47b7-a8fc-9a3becc6e53a.cpp: In function ‘int main()’: b1853f93-2d41-47b7-a8fc-9a3becc6e53a.cpp:41:38: error: ISO C++ forbids compariso n between pointer and integer [-fpermissive]    41 |     assert (int_to_mini_romank(1000) == "m");       |             ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -2,33 +2,37 @@
 
 const char* roman[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-int int_to_mini_roman(int n) {
+int int_to_mini_romank(int n) {
     if(n >= 1000){
         return strlen(roman[0]);
     } else if (n >= 900) {
-        return strlen(roman[1]);
+        return strlen("CM");
     } else if (n >= 500) {
         return strlen(roman[2]);
     } else if (n >= 400) {
-        return strlen(roman[3]);
+        return strlen("CD");
     } else if (n >= 100) {
         return strlen(roman[4]);
     } else if (n >= 90) {
-        return strlen(roman[5]);
+        return strlen("XC");
     } else if (n >= 50) {
         return strlen(roman[6]);
     } else if (n >= 40) {
-        return strlen(roman[7]);
+        return strlen("XL");
     } else if (n >= 10) {
-        if(n == 9) return strlen(roman[8]);
-        else if(n == 5) return strlen(roman[9]);
-        else if(n > 4 && n < 10) return strlen(roman[10]);
-        else if(n > 4) return strlen(roman[11]);
+        return strlen(roman[8]);
+    } else if (n >= 9) {
+        return strlen("IX");
+    } else if (n >= 5) {
+        return strlen(roman[10]);
+    } else if (n >= 4) {
+        return strlen("IV");
+    } else{
+        return strlen(roman[12]);
     }
-    return strlen("I");
 }
 
 int main() {
-    assert(int_to_mini_roman(1000) == 1);
+    assert(int_to_mini_romank(1000) == 3);
     return 0;
 }
