e1bd5146d4f10854126a773506ad29cc7ea37af6
beep boop <githubbot@vadim.me>
2024-07-09 17:31:30 +0200

    Solution 73 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/kil7uhfg Prompt: 32cf77fe-3d5a-405f-8925-13170197fc45.cpp:2:5: error: expected unqualified-id bef ore ‘while’     2 |     while (N > 0) {       |     ^~~~~ 32cf77fe-3d5a-405f-8925-13170197fc45.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return bitset<32>(sum).to_string().substr(32 - sum);       |     ^~~~~~ In file included from 32cf77fe-3d5a-405f-8925-13170197fc45.cpp:8: 32cf77fe-3d5a-405f-8925-13170197fc45.cpp: In function ‘int main()’: 32cf77fe-3d5a-405f-8925-13170197fc45.cpp:10:13: error: ‘solve’ was not declared in this scope    10 |     assert (solve(963) == "10010");       |             ^~~~~

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -8,7 +8,7 @@ std::string solve(int N) {
         sum += N % 2;
         N /= 2;
     }
-    return bitset<32>(sum).to_string().substr(32 - sum);
+    return std::bitset<32>(sum).to_string().substr(32 - sum);
 }
 
 int main() {
