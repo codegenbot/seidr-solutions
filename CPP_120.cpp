97c505063028a95f5b24313036d463d7f4ab9a11
beep boop <githubbot@vadim.me>
2024-07-09 04:32:39 +0200

    Solution 44 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/5437tmad Prompt:       |             ^~~~~~ 164d7d1e-c55c-4650-8ec9-fd1a548c57d5.cpp:18:20: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 164d7d1e-c55c-4650-8ec9-fd1a548c57d5.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 164d7d1e-c55c-4650-8ec9-fd1a548c57d5.cpp:18:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 164d7d1e-c55c-4650-8ec9-fd1a548c57d5.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 164d7d1e-c55c-4650-8ec9-fd1a548c57d5.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 164d7d1e-c55c-4650-8ec9-fd1a548c57d5.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -12,4 +12,4 @@ std::vector<int> maximum(std::vector<int> arr, int k) {
     return arr;
 }
 
-assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0), std::vector<int>{});
+assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0), std::vector<int>{}));
