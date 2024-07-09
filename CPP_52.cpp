c2a8513d2e40c3628a0e57505340bf3266f6b732
beep boop <githubbot@vadim.me>
2024-07-09 18:18:35 +0200

    Solution 10 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/4evcoe3h Prompt:       |           ^~~~~~ In file included from c41461b7-ffc6-4bed-8e39-8d1636b57683.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c41461b7-ffc6-4bed-8e39-8d1636b57683.cpp:3:29: error: expected primary-expressio n before ‘int’     3 | bool below_threshold(vector<int> l, int t) {       |                             ^~~ c41461b7-ffc6-4bed-8e39-8d1636b57683.cpp:3:37: error: expected primary-expressio n before ‘int’     3 | bool below_threshold(vector<int> l, int t) {       |                                     ^~~ c41461b7-ffc6-4bed-8e39-8d1636b57683.cpp:3:42: error: expression list treated as  compound expression in initializer [-fpermissive]     3 | bool below_threshold(vector<int> l, int t) {       |                                          ^ In file included from c41461b7-ffc6-4bed-8e39-8d1636b57683.cpp:12: c41461b7-ffc6-4bed-8e39-8d1636b57683.cpp: In function ‘int main()’: c41461b7-ffc6-4bed-8e39-8d1636b57683.cpp:14:32: error: ‘below_threshold’ cannot be used as a function    14 |     assert (not(below_threshold({1, 8, 4, 10}, 10)));       |                 ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,8 +1,7 @@
 bool below_threshold(vector<int> l, int t) {
-    for (int x : l) {
-        if (x >= t) {
+    for (int num : l) {
+        if (num >= t) {
             return false;
         }
     }
     return true;
-}
