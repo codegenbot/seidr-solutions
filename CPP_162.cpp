6cbe3e99f1bca28dd273884423a566356119b56b
beep boop <githubbot@vadim.me>
2024-07-09 20:44:16 +0200

    Solution 38 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/jjh2kxi8 Prompt: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ 7237eac2-6936-4101-a1e5-59b5aa999158.cpp:18:14: warning: ‘int MD5_Final(unsigned  char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]    18 |     MD5_Final(md5, &ctx);       |     ~~~~~~~~~^~~~~~~~~~~ In file included from 7237eac2-6936-4101-a1e5-59b5aa999158.cpp:2: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ 7237eac2-6936-4101-a1e5-59b5aa999158.cpp: At global scope: 7237eac2-6936-4101-a1e5-59b5aa999158.cpp:30:1: error: ‘g’ does not name a type    30 | g++ -o a.cpp -lssl       | ^ In file included from 7237eac2-6936-4101-a1e5-59b5aa999158.cpp:32: 7237eac2-6936-4101-a1e5-59b5aa999158.cpp: In function ‘int main()’: 7237eac2-6936-4101-a1e5-59b5aa999158.cpp:34:5: error: ‘__assert_fail’ was not de clared in this scope    34 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |     ^~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,37 +1,28 @@
 ```cpp
-#include <openssl/evp.h>
+#include <openssl/ssl.h>
 #include <iostream>
 #include <string>
 #include <algorithm>
 
 std::string string_to_md5(std::string text) {
     if (text.empty()) return "";
-    
+
     unsigned char md5[16];
-    EVP_MD_CTX ctx;
-    EVP_MD_CTX_init(&ctx);
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
     const char* p = text.c_str();
     while (*p) {
-        EVP_DigestUpdate(&ctx, &(*p), 1);
+        MD5_Update(&ctx, p, 1);
         p++;
     }
-    EVP_DigestFinal(ctx, md5, NULL);
+    MD5_Final(md5, &ctx);
 
     std::string result(32, '0');
     for (int i = 0; i < 16; ++i) {
         char buf[3];
         sprintf(buf, "%02x", md5[i]);
-        result.replace(i*2, 2, buf);
+        result.replace(i * 2, 2, buf);
     }
-    
-    EVP_MD_CTX_cleanup(&ctx);
-    return result;
-}
 
-int main() {
-    std::string text;
-    std::cout << "Enter a string: ";
-    std::getline(std::cin, text);
-    std::cout << "MD5: " << string_to_md5(text) << std::endl;
-    return 0;
+    return result;
 }
