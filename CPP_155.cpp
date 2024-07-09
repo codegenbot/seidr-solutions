ecba1c5c50a49e52c21919097a8bc318790cf412
beep boop <githubbot@vadim.me>
2024-07-09 02:42:03 +0200

    Solution 30 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fkq142tr Prompt: In file included from /usr/include/c++/11/vector:67,                  from 8efad0be-fd37-4c3d-a7b4-9a59f95c4681.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 8efad0be-fd37-4c3d-a7b4-9a59f95c4681.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8efad0be-fd37-4c3d-a7b4-9a59f95c4681.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8efad0be-fd37-4c3d-a7b4-9a59f95c4681.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8efad0be-fd37-4c3d-a7b4-9a59f95c4681.cpp:17: 8efad0be-fd37-4c3d-a7b4-9a59f95c4681.cpp: In function ‘int main()’: 8efad0be-fd37-4c3d-a7b4-9a59f95c4681.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size()!=b.size())
-        return false;
-    for(int i=0;i<a.size();i++)
-        if(a[i]!=b[i])
-            return false;
-    return true;
+    return a == b;
+}
+
+int main() {
+    assert(even_odd_count(0) == std::vector<int>{1, 0});
+    return 0;
 }
 
 std::vector<int> even_odd_count(int num) {
@@ -21,16 +21,4 @@ std::vector<int> even_odd_count(int num) {
         num /= 10;
     }
     return result;
-}
-
-int main() {
-    int num;
-    std::cout << "Enter a number: ";
-    std::cin >> num;
-    std::vector<int> reversed = even_odd_count(num);
-    if(issame(reversed, even_odd_count(std::to_string(num).reverse().erase(0, 1).converts_to_int())) {
-        std::cout << "The input number is the same when its digits are reversed." << std::endl;
-    } else {
-        std::cout << "The input number is not the same when its digits are reversed." << std::endl;
-    }
 }
