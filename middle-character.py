d75694de053ffcf64b859aafac16222a6fb7ff1d
beep boop <githubbot@vadim.me>
2024-07-09 20:36:13 +0200

    Solution 10 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/sookggnt Prompt: it must return _ for input jjj5jA=^7mvR =}Q}J_<$lP;:S7n2*jRLe]ZI, but it returns .

--- middle-character.py
+++ middle-character.py
@@ -1,6 +1,6 @@
 def middle_character(s):
     length = len(s)
     if length % 2 == 0:
-        return "_" + s[length//2-1:length//2+2] + "_"
+        return "_" + s[length//2-1:length//2+1] + "_"
     else:
         return "_" + s[length//2] + "_"
