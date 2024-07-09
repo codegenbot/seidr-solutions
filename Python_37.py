4ee2f23c565ff4579213ae9781f046cf9059c7ba
beep boop <githubbot@vadim.me>
2024-07-09 07:27:40 +0200

    Solution 92 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/lqdbl84j Prompt: Program did not receive expected input

--- Python_37.py
+++ Python_37.py
@@ -1,12 +1,4 @@
 from collections import OrderedDict
 
-
 def sort_even(l: list):
-    if len(l) % 2 != 0:
-        return "Program did not receive expected input"
-    else:
-        return list(
-            OrderedDict(
-                (i // 2, x) if x % 2 == 0 else (i, x) for i, x in enumerate(l)
-            ).values()
-        ) + [x for x in l if x % 2 != 0]
+    return list(OrderedDict.fromkeys((i//2,x) if x % 2 == 0 else (i,x) for i, x in enumerate(l))).values()) + [x for x in l if x % 2 != 0]
