add8809a78220d8c8ef528ca40b321026461d9ec
beep boop <githubbot@vadim.me>
2024-07-09 05:30:31 +0200

    Solution 58 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kttdajki Prompt:                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 644452ea-8a1d-4974-a8ce-c9977e580b1d.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 644452ea-8a1d-4974-a8ce-c9977e580b1d.cpp:37:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    37 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf 644452ea-8a1d-4974-a8ce-c9977e580b1d.cpp:37:45: error: expression list treated a s compound expression in initializer [-fpermissive]    37 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 644452ea-8a1d-4974-a8ce-c9977e580b1d.cpp:36: 644452ea-8a1d-4974-a8ce-c9977e580b1d.cpp: In function ‘int main()’: 644452ea-8a1d-4974-a8ce-c9977e580b1d.cpp:46:19: error: ‘issame’ cannot be used a s a function    46 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -32,8 +32,3 @@ std::vector<std::string> findPlanets(std::string planet1, std::string planet2) {
 
     return result;
 }
-
-int main() {
-    assert(findPlanets("Jupiter", "Makemake") == {});
-    return 0;
-}
