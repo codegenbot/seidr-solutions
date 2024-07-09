96ab21f8958f022106bdc93facf5ca02df9239cb
beep boop <githubbot@vadim.me>
2024-07-09 18:17:24 +0200

    Solution 7 of CPP/51, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/vmdj1pvd Prompt: 7785eae8-7fc0-4cf7-bc6a-66e8de5cfb6f.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string remove_vowels(string text){       | ^~~~~~ In file included from 7785eae8-7fc0-4cf7-bc6a-66e8de5cfb6f.cpp:11: 7785eae8-7fc0-4cf7-bc6a-66e8de5cfb6f.cpp: In function ‘int main()’: 7785eae8-7fc0-4cf7-bc6a-66e8de5cfb6f.cpp:13:13: error: ‘remove_vowels’ was not d eclared in this scope    13 |     assert (remove_vowels("ybcd") == "ybcd");       |             ^~~~~~~~~~~~~

--- CPP_51.cpp
+++ CPP_51.cpp
@@ -1,10 +1,11 @@
 #include <string>
 
-string remove_vowels(string text) {
-    string result = "";
+std::string remove_vowels(std::string text){
+    std::string result = "";
     for(char c : text){
         if(c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' &&
            !isupper(c) && !islower(c))
             result += c;
     }
     return result;
+}
