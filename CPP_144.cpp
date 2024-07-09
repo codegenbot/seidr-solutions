b8854f0f2e2a0687ac70f43f0488966290cbd04d
beep boop <githubbot@vadim.me>
2024-07-09 11:29:08 +0200

    Solution 13 of CPP/144, ratio 0.7692307692307693 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j8w2522w Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -2,10 +2,20 @@
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = stoi(strtok((x.substr(1)).c_str(), "/"));
-    int b = stoi(strtok(nullptr, "/"));
-    int c = stoi(strtok((n.substr(1)).c_str(), "/"));
-    int d = stoi(strtok(nullptr, "/"));
-
-    return (a * c) % (b * d) == 0;
+    int a = 1, b = 1, c = 1, d = 1;
+    int pos = 0;
+    while (pos < x.size()) {
+        if (x[pos] == '/') break;
+        pos++;
+    }
+    for (int i = 0; i < pos; i++) a *= stoi(x.substr(0, i)) - 1;
+    for (int i = pos + 1; i < x.size(); i++) b *= stoi(x.substr(pos + 1, i));
+    pos = 0;
+    while (pos < n.size()) {
+        if (n[pos] == '/') break;
+        pos++;
+    }
+    for (int i = 0; i < pos; i++) c *= stoi(n.substr(0, i)) - 1;
+    for (int i = pos + 1; n.size() > i; i++) d *= stoi(n.substr(pos + 1, i));
+    return a*d == b*c;
 }
