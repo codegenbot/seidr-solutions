787709fc52367dec32ceb37209b77bd6b8e3dade
beep boop <githubbot@vadim.me>
2024-07-10 01:26:31 +0200

    Solution 3 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uzrnpb22 Prompt: In file included from fd1797aa-efed-4fb9-9689-a940559a61f3.cpp:17: fd1797aa-efed-4fb9-9689-a940559a61f3.cpp:27:26: note:   candidate expects 4 argu ments, 1 provided    27 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from fd1797aa-efed-4fb9-9689-a940559a61f3.cpp:3: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: candidate: ‘template <class _ExecutionPolicy, class _ForwardIterator> __pstl::__internal::__enable_if _execution_policy<_ExecutionPolicy, _ForwardIterator> std::unique(_ExecutionPoli cy&&, _ForwardIterator, _ForwardIterator)’   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note:   template argument deduction/substitution failed: In file included from fd1797aa-efed-4fb9-9689-a940559a61f3.cpp:17: fd1797aa-efed-4fb9-9689-a940559a61f3.cpp:27:26: note:   candidate expects 3 argu ments, 1 provided    27 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,16 +1,14 @@
+```
 #include <iostream>
-#include <set>
-using namespace std;
+#include <vector>
+#include <algorithm>
 
-vector<int> unique(vector<int> l) {
-    set<int> result(l.begin(), l.end());
-    vector<int> output(result.begin(), result.end());
-    return output;
-}
+using namespace std;
 
 int main() {
     vector<int> input = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    vector<int> output = unique(input);
+    vector<int> output(unique_copy(input.begin(), input.end(), input.end()));
+    sort(output.begin(), output.end());
     for (int i : output) {
         cout << i << " ";
     }
