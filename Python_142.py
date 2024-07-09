5919774c6913507bf01f0155bf6321cc17799e93
beep boop <githubbot@vadim.me>
2024-07-09 09:09:41 +0200

    Solution 52 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/8ou4ga8l Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7ba16422-22d5-46b7-a569-7665a33be 821.py", line 43, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7ba16422-22d5-46b7-a569-7665a33be 821.py", line 13, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,12 +1,5 @@
 def sum_squares(lst):
     total = 0
     for num in lst:
-        if isinstance(num, int):
-            if (num % 3 == 0 and num % 4 != 0) or (num % 4 == 0 and num % 3 != 0):
-                if num % 3 == 0 and num % 4 == 0:
-                    total += num**2
-                else:
-                    total += num**3
-        else:
-            raise ValueError("Input list must only contain integers")
+        total += num ** 2 + num ** 3
     return total
