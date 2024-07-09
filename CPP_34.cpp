d21b1156c26cfaaa9b1dc0873663ca5f323b9eaf
beep boop <githubbot@vadim.me>
2024-07-09 14:46:08 +0200

    Solution 81 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/jf50la17 Prompt:       |                                  ^~~ 06432d72-397a-4339-84b9-132702b283ea.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 06432d72-397a-4339-84b9-132702b283ea.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~ In file included from 06432d72-397a-4339-84b9-132702b283ea.cpp:38: 06432d72-397a-4339-84b9-132702b283ea.cpp: In function ‘int main()’: 06432d72-397a-4339-84b9-132702b283ea.cpp:48:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    48 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 06432d72-397a-4339-84b9-132702b283ea.cpp:2: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -26,10 +26,11 @@ int findUnique(std::vector<int> vec) {
     return issame({*vec.begin(), *(vec.end()-1)}, vec);
 }
 
-int main2() {
+int main() {
     std::vector<int> uniqueVec = {5, 3, 5, 2, 3, 3, 9, 0, 123};
     if (!findUnique(uniqueVec)) {
         return 1;
     }
+    assert(issame(std::vector<int>(uniqueVec) , {0, 2, 3, 5, 9, 123}));
     return 0;
 }
