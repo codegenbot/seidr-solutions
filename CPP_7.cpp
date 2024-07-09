413b0c180c8d3c9c6a243d9ab95694840ba554b1
beep boop <githubbot@vadim.me>
2024-07-10 01:04:36 +0200

    Solution 3 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/k88udogy Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 3a9aba05-7d40-4c3f-8f89-0802c1edc7d7.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,22 +1,27 @@
 ```cpp
-vector<string> filter_by_substring(vector<string> strings, string substring){
-    vector<string> result;
-    for(string s : strings){
-        if(s.find(substring) != string::npos)
-            result.push_back(s);
-    }
-    return result;
-}
+#include <vector>
+#include <string>
 
-bool issame(vector<string> a, vector<string> b){
-    if(a.size() != b.size())
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
         return false;
-    for(int i = 0; i < a.size(); i++){
-        if(find(b.begin(), b.end(), a[i]) == b.end())
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
             return false;
+        }
     }
     return true;
 }
 
-vector<string> b = {"grunt", "prune"};
-assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), b));
+vector<vector<string>> filter_by_substring(vector<vector<string>> strings, string substring) {
+    vector<vector<string>> result;
+    for (vector<string> s : strings) {
+        vector<string> temp({substring});
+        bool same = issame(s, temp);
+        if (!same) {
+            result.push_back(s);
+        }
+    }
+    return result;
+}
