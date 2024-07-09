693326230fc1b5381ad519898427d7cdc5f9dd55
beep boop <githubbot@vadim.me>
2024-07-09 16:13:36 +0200

    Solution 12 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/g47u22zu Prompt: bd11d575-c17b-4a7e-8a99-ef7a77419375.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ bd11d575-c17b-4a7e-8a99-ef7a77419375.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ bd11d575-c17b-4a7e-8a99-ef7a77419375.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ bd11d575-c17b-4a7e-8a99-ef7a77419375.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ bd11d575-c17b-4a7e-8a99-ef7a77419375.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,9 +1,5 @@
-```cpp
+```
 #include <cassert>
-
-int main() {
-    assert(any_int(3.01f, 4.0f, 7.0f) == false);
-}
 
 bool any_int(float a, float b, float c) {
     int ai = (int)a;
@@ -14,4 +10,9 @@ bool any_int(float a, float b, float c) {
         return true;
     else
         return false;
+}
+
+int main() {
+    assert(any_int(3.01, 4, 7) == false);
+    return 0;
 }
