0f8a95e8ae5405bf989e0cca2060ef1758e228e1
beep boop <githubbot@vadim.me>
2024-07-09 17:48:46 +0200

    Solution 86 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9d9hp3es Prompt:       |                                  ^~~ a32d5f8e-e726-47a5-94f7-01374b40f45a.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a32d5f8e-e726-47a5-94f7-01374b40f45a.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ a32d5f8e-e726-47a5-94f7-01374b40f45a.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~ In file included from a32d5f8e-e726-47a5-94f7-01374b40f45a.cpp:17: a32d5f8e-e726-47a5-94f7-01374b40f45a.cpp: In function ‘int main()’: a32d5f8e-e726-47a5-94f7-01374b40f45a.cpp:27:20: error: ‘make_a_pile’ was not dec lared in this scope    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ a32d5f8e-e726-47a5-94f7-01374b40f45a.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,20 +1,15 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool arePilesSame(std::vector<int> a, std::vector<int> b) {
     return a == b;
-}
-
-std::vector<int> make_a_pile(int n) {
-    std::vector<int> pile = {0, 1};
-    for (int i = 2; i <= n; ++i)
-        pile.push_back(pile.back() + i);
-    return pile;
 }
 
 int main() {
     int n = 8;
-    std::vector<int> pile = make_a_pile(n);
-    assert(issame(pile, pile));
+    std::vector<int> pile = {0, 1};
+    for (int i = 2; i <= n; ++i)
+        pile.push_back(pile.back() + i);
+    assert(arePilesSame(pile, pile));
     return 0;
 }
