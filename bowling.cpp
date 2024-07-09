8db3732ce9820d4d427e0c5d27e3c11606a7ec13
beep boop <githubbot@vadim.me>
2024-07-09 17:53:04 +0200

    Solution 55 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/acc9guyz Prompt: 99c17832-05f1-4543-a0ac-441b0913820f.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int bowlingScore(std::string input) {       |                       ^~~~~~ 99c17832-05f1-4543-a0ac-441b0913820f.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int bowlingScore(std::string input) {

--- bowling.cpp
+++ bowling.cpp
@@ -1,25 +1,30 @@
+#include <string>
+
 int bowlingScore(std::string input) {
     int score = 0;
-    bool lastRoll = false;
-    
-    for (char c : input) {
-        if (c == 'X') {
+    for(int i = 0; i < input.length(); i++) {
+        if(input[i] == 'X') { 
             score += 10;
-            lastRoll = true;
-        } else if (c == '/') {
-            score += 10 - ((input[1] == 'X' || input[2] == 'X') ? 0 : (input[1] - '0' + input[2] - '0'));
-            lastRoll = true;
-        } else if (isdigit(c)) {
-            int roll = c - '0';
-            if (!lastRoll) {
-                score += roll;
+            i++; 
+        } else if(input[i] == '/') { 
+            score += 10 - (input[i+1] - '0' + input[i+2] - '0');
+            i+=2; 
+        } else {
+            int roll = input[i] - '0';
+            if(input[i+1] == '/') { 
+                score += roll + 10 - (input[i+2] - '0' + input[i+3] - '0');
+                i+=3; 
             } else {
-                if (lastRoll) score += 10 - roll; 
-                else score += roll + input[1] - '0'; 
+                score += roll;
+                if(input[i+1] == 'X') { 
+                    score += 10 + 10 + (input[i+2] - '0' + input[i+3] - '0');
+                    i+=3; 
+                } else {
+                    score += roll + input[i+1] - '0';
+                    i++; 
+                }
             }
-            lastRoll = !lastRoll;
         }
     }
-    
     return score;
 }
