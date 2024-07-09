55fe750ed7c2a395db315b7d727b1a006ed953ae
beep boop <githubbot@vadim.me>
2024-07-09 04:19:36 +0200

    Solution 55 of CPP/88, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/g6w1o3fa Prompt:       |     ^~~~~~ 8afb4a86-5c26-4b5d-80d6-fae3324d7bbe.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | using namespace std; 8afb4a86-5c26-4b5d-80d6-fae3324d7bbe.cpp: At global scope: 8afb4a86-5c26-4b5d-80d6-fae3324d7bbe.cpp:24:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    24 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 8afb4a86-5c26-4b5d-80d6-fae3324d7bbe.cpp:4:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     4 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 8afb4a86-5c26-4b5d-80d6-fae3324d7bbe.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 8afb4a86-5c26-4b5d-80d6-fae3324d7bbe.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,8 +1,17 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool same(vector<int> a,vector<int>b){
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i<a.size();i++){
+        if(a[i] != b[b.size()-1-i]) return false;
+    }
+    return true;
+}
+
 vector<int> sort_array(vector<int> array) {
-    int first = array.empty() ? 0 : array[0];
-    int last = array.empty() ? 0 : array.back();
-    if ((first + last) % 2 == 1)
-        return vector<int>(array);
-    else
-        return vector<int>(array.rbegin(), array.rend());
+    vector<int> sortedArray(array);
+    sort(sortedArray.begin(),sortedArray.end());
+    return sortedArray;
 }
