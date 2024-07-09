cc7c09f2786df5b3112c4222979a64a33e4a364c
beep boop <githubbot@vadim.me>
2024-07-09 04:13:22 +0200

    Solution 56 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/auoz3qca Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ea00c7e2-c105-4439-ba13-51fb7cbe2 55e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,8 +1,3 @@
-```
-def double_the_difference(lst):
-    odd_sum = sum(i**2 for i in lst if isinstance(i, int) and i >= 0 and i % 2 != 0)
-    return 2 * odd_sum - sum(lst)
-
 def check(func):
     test_cases = [(4, [1, 3, 5])]
     for i, ((expected_output), (input_list)) in enumerate(test_cases):
@@ -11,5 +6,4 @@ def check(func):
             print(f"Test {i+1} passed")
         else:
             print(f"Test {i+1} failed. Expected: {expected_output}, Got: {result}")
-
-check(double_the_difference)
+    check(double_the_difference)
