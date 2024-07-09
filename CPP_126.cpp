83a88bfd6cc732393400e9ed206d4b3f8723b96c
beep boop <githubbot@vadim.me>
2024-07-09 04:51:36 +0200

    Solution 68 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8q1otf1z Prompt: d9b84fc3-0c5f-4caf-8038-e1f5edef065e.cpp: In function ‘bool is_sorted(std::vecto r<int>)’: d9b84fc3-0c5f-4caf-8038-e1f5edef065e.cpp:9:50: error: ‘unique’ is not a member o f ‘std’     9 |             std::vector<int>::iterator it = std::unique(lst.begin(), lst .end());       |                                                  ^~~~~~ d9b84fc3-0c5f-4caf-8038-e1f5edef065e.cpp: At global scope: d9b84fc3-0c5f-4caf-8038-e1f5edef065e.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ d9b84fc3-0c5f-4caf-8038-e1f5edef065e.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,18 +1,18 @@
+#include <cassert>
 #include <vector>
 #include <algorithm>
 
 bool is_sorted(std::vector<int> lst) {
-    if (lst.size() <= 1)
+    if(lst.size() <= 1)
         return true;
-    for (int i = 1; i < lst.size(); i++) {
-        if (lst[i-1] > lst[i]) {
-            std::vector<int>::iterator it = std::unique(lst.begin(), lst.end());
-            lst.erase(it, lst.end());
+    for(int i = 1; i < lst.size(); i++){
+        if(lst[i-1] > lst[i]){
             return false;
         }
     }
     return true;
+}
 
 int main() {
-    assert (is_sorted({1, 2, 3, 4}) == true);
+    assert(is_sorted({1, 2, 3, 4}) == true);
 }
