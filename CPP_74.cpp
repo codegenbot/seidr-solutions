2a9725a279a225749a8c478421b6ba6ed4fe0b06
beep boop <githubbot@vadim.me>
2024-07-09 22:01:03 +0200

    Solution 79 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/oje19k94 Prompt:       |           ^~~~~~ 5c74fa11-228f-4bab-8133-af4a69f6ce63.cpp:31:44: error: ‘b’ was not declared in t his scope    31 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5c74fa11-228f-4bab-8133-af4a69f6ce63.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 5c74fa11-228f-4bab-8133-af4a69f6ce63.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from 5c74fa11-228f-4bab-8133-af4a69f6ce63.cpp:30: 5c74fa11-228f-4bab-8133-af4a69f6ce63.cpp: In function ‘int main()’: 5c74fa11-228f-4bab-8133-af4a69f6ce63.cpp:40:20: error: ‘total_match’ was not dec lared in this scope    40 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 5c74fa11-228f-4bab-8133-af4a69f6ce63.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -23,6 +23,6 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
 }
 
 int main() {
-    assert(issame({{"this"}, {}} , {}));
+    assert(issame({{"this"}, {}}) , {});
     return 0;
 }
