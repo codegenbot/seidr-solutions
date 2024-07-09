bbf2172e6e9e710ca4d33707d1c8d59594d23bcd
beep boop <githubbot@vadim.me>
2024-07-09 20:05:54 +0200

    Solution 2 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/owmm65gu Prompt: c336ac63-327d-4692-9696-725caa60634b.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int num) {       | ^~~~~~ In file included from c336ac63-327d-4692-9696-725caa60634b.cpp:16: c336ac63-327d-4692-9696-725caa60634b.cpp: In function ‘int main()’: c336ac63-327d-4692-9696-725caa60634b.cpp:18:13: error: ‘int_to_mini_romank’ was not declared in this scope    18 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,15 +1,16 @@
-string int_to_mini_roman(int number) {
-    vector<pair<int, string>> roman_numerals = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-                                                 {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-                                                 {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+using namespace std;
 
-    string roman = "";
-    for (const auto& numeral : roman_numerals) {
-        while (number >= numeral.first) {
-            number -= numeral.first;
-            roman += numeral.second;
+std::string int_to_mini_roman(int num) {
+    std::string roman[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    
+    std::string result = "";
+    for (int i = 0; i < 13; i++) {
+        while (num >= values[i]) {
+            num -= values[i];
+            result += roman[i];
         }
     }
-
-    return roman;
+    
+    return result;
 }
