64abe443f6006ed91e128debd8f8b134c424407e
beep boop <githubbot@vadim.me>
2024-07-09 11:09:36 +0200

    Solution 21 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/e35u2c5u Prompt: 85d5122b-aba9-4828-888e-5b8d3d24d344.cpp:1:15: error: ‘vector’ was not declared in this scope     1 | int solutions(vector<int> lst){       |               ^~~~~~ 85d5122b-aba9-4828-888e-5b8d3d24d344.cpp:1:22: error: expected primary-expressio n before ‘int’     1 | int solutions(vector<int> lst){       |                      ^~~ In file included from 85d5122b-aba9-4828-888e-5b8d3d24d344.cpp:11: 85d5122b-aba9-4828-888e-5b8d3d24d344.cpp: In function ‘int main()’: 85d5122b-aba9-4828-888e-5b8d3d24d344.cpp:13:22: error: ‘solutions’ cannot be use d as a function    13 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-int sum_of_odd_numbers(vector<int> lst){
+int solutions(vector<int> lst){
     int sum = 0;
     for(int i = 0; i < lst.size(); i += 2){
         if(lst[i] % 2 != 0){
@@ -10,8 +10,7 @@ int sum_of_odd_numbers(vector<int> lst){
     return sum;
 }
 
-int main(){
-    int result = sum_of_odd_numbers({3, 13, 2, 9});
+int main() {
+    int result = solutions({3, 13, 2, 9});
     assert(result == 3);
-    return 0;
 }
