319bcf8f0f77c4f1e1f8d3b65fb7fb66c6988c1a
beep boop <githubbot@vadim.me>
2024-07-09 14:23:08 +0200

    Solution 79 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/x43z47vg Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c8d25f2f-9c76-4417-b8c2-e0b391f3c9f3.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c8d25f2f-9c76-4417-b8c2-e0b391f3c9f3.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c8d25f2f-9c76-4417-b8c2-e0b391f3c9f3.cpp:11: c8d25f2f-9c76-4417-b8c2-e0b391f3c9f3.cpp: In function ‘int main()’: c8d25f2f-9c76-4417-b8c2-e0b391f3c9f3.cpp:21:20: error: ‘filter_by_prefix’ was no t declared in this scope    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ c8d25f2f-9c76-4417-b8c2-e0b391f3c9f3.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -4,6 +4,22 @@
 
 using namespace std;
 
+vector<string> filter_by_prefix(vector<string> strings, string prefix){
+    vector<string> result;
+    for (const auto &str : strings) {
+        if (str.substr(0, prefix.size()) == prefix) {
+            result.push_back(str);
+        }
+    }
+    return result;
+}
+
 bool issame(vector<string> a, vector<string> b){
     return a == b;
+}
+
+int main() {
+    assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
+    
+    return 0;
 }
