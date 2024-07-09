ba703edd65ec14e18e4a86fa6c92dfee69425d1a
beep boop <githubbot@vadim.me>
2024-07-10 01:20:03 +0200

    Solution 97 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/szteghpq Prompt:       |                    ^~~ 03d218a0-e357-4dcc-867c-af5228303df4.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 03d218a0-e357-4dcc-867c-af5228303df4.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 03d218a0-e357-4dcc-867c-af5228303df4.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 03d218a0-e357-4dcc-867c-af5228303df4.cpp:15: 03d218a0-e357-4dcc-867c-af5228303df4.cpp: In function ‘int main()’: 03d218a0-e357-4dcc-867c-af5228303df4.cpp:25:20: error: ‘compare’ was not declare d in this scope    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 03d218a0-e357-4dcc-867c-af5228303df4.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -12,14 +12,14 @@ vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
     if(issame(game,guess)){
         for (int i = 0; i < game.size(); i++) {
-            result.push_back(0);
+            result.push_back(2);
         }
-    } else {
+    }else{
         for (int i = 0; i < game.size(); i++) {
             if (game[i] == guess[i]) {
-                result.push_back(0);
+                result.push_back(1);
             } else {
-                result.push_back(abs(game[i] - guess[i]));
+                result.push_back(0);
             }
         }
     }
