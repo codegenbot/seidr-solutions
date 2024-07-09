857a425fcf75657a0f17e8666db2bfeaa9275578
beep boop <githubbot@vadim.me>
2024-07-09 04:48:53 +0200

    Solution 23 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/3dz5v5gs Prompt:       |                    ^~~ a0ae64a8-22af-4dbd-b472-89d434bb8a89.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a0ae64a8-22af-4dbd-b472-89d434bb8a89.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a0ae64a8-22af-4dbd-b472-89d434bb8a89.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a0ae64a8-22af-4dbd-b472-89d434bb8a89.cpp:13: a0ae64a8-22af-4dbd-b472-89d434bb8a89.cpp: In function ‘int main()’: a0ae64a8-22af-4dbd-b472-89d434bb8a89.cpp:23:20: error: ‘eat’ was not declared in  this scope    23 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ^~~ a0ae64a8-22af-4dbd-b472-89d434bb8a89.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,15 +1,17 @@
 #include <vector>
-
 bool issame(vector<int> a,vector<int>b){
     return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
 }
 
-vector<int> eat(int number, int need, int remaining) {
-    int total = number + need;
-    int left = min(total - 1000, remaining);
-    if (left < 0) {
-        return {total, 0};
-    } else {
-        return {total, max(remaining - left, 0)};
+int main(){
+    assert(eat(4, 5, 1) == vector<int>({5,0}));
+    
+    vector<int> eat(int number, int need, int remaining) {
+        int total = number + need;
+        int left = min(total - 1000, remaining);
+        if (left < 0) {
+            return {total, 0};
+        } else {
+            return {total, max(remaining - left, 0)};
+        }
     }
-}
