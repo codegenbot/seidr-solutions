1b9e3c28fe3794e8a92805b624048f4b1fd614c9
beep boop <githubbot@vadim.me>
2024-07-09 06:41:48 +0200

    Solution 2 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/jl3bt86n Prompt:     3 |     cin >> input;       |     ^~~ 477a8e39-ba15-4b74-b0f9-70773f4fd205.cpp:3:12: error: ‘input’ was not declared i n this scope; did you mean ‘int’?     3 |     cin >> input;       |            ^~~~~       |            int 477a8e39-ba15-4b74-b0f9-70773f4fd205.cpp:6:9: error: ‘cout’ was not declared in this scope     6 |         cout << "True" << endl;       |         ^~~~ 477a8e39-ba15-4b74-b0f9-70773f4fd205.cpp:6:27: error: ‘endl’ was not declared in  this scope     6 |         cout << "True" << endl;       |                           ^~~~ 477a8e39-ba15-4b74-b0f9-70773f4fd205.cpp:8:9: error: ‘cout’ was not declared in this scope     8 |         cout << "False" << endl;       |         ^~~~ 477a8e39-ba15-4b74-b0f9-70773f4fd205.cpp:8:28: error: ‘endl’ was not declared in  this scope     8 |         cout << "False" << endl;       |                            ^~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,26 +1,16 @@
-bool evaluateBooleanExpression(const string& expression) {
-    if (expression == "t") {
-        return true;
-    } else if (expression == "f") {
-        return false;
-    } else {
-        bool left = evaluateBooleanExpression(expression.substr(0, 1));
-        bool right = evaluateBooleanExpression(expression.substr(2));
-        if (expression[1] == '&') {
-            return left && right;
-        } else {
-            return left || right;
-        }
-    }
-}
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
-    string expression;
-    cin >> expression;
-    if (evaluateBooleanExpression(expression)) {
+    string input;
+    cin >> input;
+
+    if (input == "T") {
         cout << "True" << endl;
-    } else {
+    } else if (input == "F") {
         cout << "False" << endl;
     }
+
     return 0;
 }
