3066f045dd2299938251d1539e8f7dd9813359b2
beep boop <githubbot@vadim.me>
2024-07-09 03:52:49 +0200

    Solution 47 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tzm0q5ti Prompt:       |                    ^~~ 7b495c80-3b0e-453e-8936-8653f72671c2.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7b495c80-3b0e-453e-8936-8653f72671c2.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7b495c80-3b0e-453e-8936-8653f72671c2.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7b495c80-3b0e-453e-8936-8653f72671c2.cpp:13: 7b495c80-3b0e-453e-8936-8653f72671c2.cpp: In function ‘int main()’: 7b495c80-3b0e-453e-8936-8653f72671c2.cpp:23:20: error: ‘common’ was not declared  in this scope    23 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 7b495c80-3b0e-453e-8936-8653f72671c2.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -7,26 +7,25 @@ bool issame(vector<int> a, vector<int> b) {
 
 int main() {
     vector<int> l1 = {1, 2, 3};
-    vector<int> l2 = {2, 3, 4};
+    vector<int> l2 = {1, 2, 3};
 
-    vector<int> common = common(l1, l2);
+    if(issame(l1,l2)) {
+        vector<int> common;
+        set<int> s1(l1.begin(), l1.end());
+        set<int> s2(l2.begin(), l2.end());
 
-    for (int x : common) {
-        cout << x << " ";
+        set<int> intersection;
+        set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                          inserter(intersection, intersection.begin()));
+
+        common.assign(intersection.begin(), intersection.end());
+        cout << "Common elements are: ";
+        for(int i : common) {
+            cout << i << " ";
+        }
+    } else {
+        cout << "The two vectors are not same";
     }
-    cout << endl;
 
     return 0;
-}
-
-vector<int> common(vector<int> l1, vector<int> l2) {
-    set<int> s1(l1.begin(), l1.end());
-    set<int> s2(l2.begin(), l2.end());
-
-    set<int> intersection;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                      inserter(intersection, intersection.begin()));
-
-    vector<int> result(intersection.begin(), intersection.end());
-    return result;
 }
