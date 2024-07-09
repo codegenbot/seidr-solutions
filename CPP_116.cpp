1768a37f588bd9451b84098326cc9eaab57c4d8f
beep boop <githubbot@vadim.me>
2024-07-09 19:25:37 +0200

    Solution 6 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lvdfcyfj Prompt:                  from /usr/include/c++/11/algorithm:74,                  from 6041864e-a9e3-4ff1-911a-7f571b99f3d7.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 6041864e-a9e3-4ff1-911a-7f571b99f3d7.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6041864e-a9e3-4ff1-911a-7f571b99f3d7.cpp:9:39: error: expression list treated as  compound expression in initializer [-fpermissive]     9 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6041864e-a9e3-4ff1-911a-7f571b99f3d7.cpp:8: 6041864e-a9e3-4ff1-911a-7f571b99f3d7.cpp: In function ‘int main()’: 6041864e-a9e3-4ff1-911a-7f571b99f3d7.cpp:18:20: error: ‘sort_array’ was not decl ared in this scope    18 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 6041864e-a9e3-4ff1-911a-7f571b99f3d7.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,16 +1,12 @@
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
-std::vector<int> sort_vector(std::vector<int> arr) {
-    std::sort(arr.begin(), arr.end());
-    return arr;
-}
-
-int main() {
-    assert(issame(sort_vector({2,4,8,16,32}) , {2, 4, 8, 16, 32}));
-    return 0;
+std::vector<int> sort_array(const std::vector<int>& input) {
+    std::vector<int> output = input;
+    std::sort(output.begin(), output.end());
+    return output;
 }
