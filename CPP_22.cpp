3c4b2823199d245fda052d9a429e37ee99f0b6d3
beep boop <githubbot@vadim.me>
2024-07-09 05:19:54 +0200

    Solution 63 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qnmgtenv Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5c8da0d3-3c3b-47eb-9143-b9bdeb38df66.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5c8da0d3-3c3b-47eb-9143-b9bdeb38df66.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5c8da0d3-3c3b-47eb-9143-b9bdeb38df66.cpp:11: 5c8da0d3-3c3b-47eb-9143-b9bdeb38df66.cpp: In function ‘int main()’: 5c8da0d3-3c3b-47eb-9143-b9bdeb38df66.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 5c8da0d3-3c3b-47eb-9143-b9bdeb38df66.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,23 +1,19 @@
 #include <vector>
 #include <boost/any.hpp>
 
-bool issame(const boost::any& a, const boost::any& b) {
-    if (!boost::any_cast<bool>(a).good() || !boost::any_cast<bool>(b).good())
-        return false;
-    return *boost::any_cast<bool*>(const_cast<boost::any*>(&a)) == *boost::any_cast<bool*>(const_cast<boost::any*>(&b));
+using namespace std;
+using namespace boost;
+
+bool issame(vector<int> a, vector<int> b) {
+    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
 }
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
-}
-
-std::vector<int> filter_integers(std::list<boost::any> values) {
-    std::vector<int> result;
+vector<int> filter_integers(list<any> values) {
+    vector<int> result;
     for (const auto& value : values) {
-        if (!boost::any_cast<bool>(value).good())
-            continue;
-        if (boost::any_cast<bool*>(const_cast<boost::any*>(&value))->bool_ == true)
-            result.push_back(boost::any_cast<int>(value));
+        if (any_cast<int>(value).good()) {
+            result.push_back(any_cast<int>(value));
+        }
     }
     return result;
 }
