24bd2a9742394481a3e1869ae52d24a3076cb3b6
beep boop <githubbot@vadim.me>
2024-07-09 03:15:23 +0200

    Solution 1 of CPP/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/oqbw7g39 Prompt: Complete the following code given the task description and function signature.

--- CPP_39.cpp
+++ CPP_39.cpp
@@ -1,32 +1,27 @@
-#include <iostream>
+#include <stdio.h>
 using namespace std;
-
-bool isPrime(int num) {
-    if (num <= 1)
-        return false;
-    for (int i = 2; i * i <= num; i++)
-        if (num % i == 0)
-            return false;
-    return true;
-}
 
 int prime_fib(int n) {
     int a = 0, b = 1;
-    for (int i = 1; ; i++) {
-        if (isPrime(b)) {
-            if (i == n)
-                return b;
-            int temp = a;
-            a = b;
-            b = temp + b;
-        } else
-            b = a + b;
+    for (int i = 2; ; i++) {
+        int temp = a + b;
+        if (temp > n) break;
+        a = b;
+        b = temp;
+        if (isPrime(temp)) return temp;
     }
 }
 
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; i++)
+        if (num % i == 0) return false;
+    return true;
+}
+
 int main() {
-    for (int i = 1; i <= 5; i++) {
-        cout << prime_fib(i) << endl;
-    }
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", prime_fib(n));
     return 0;
 }
