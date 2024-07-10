a463c428969cb81ab27efbb91ab6d31c974cce69
beep boop <githubbot@vadim.me>
2024-07-10 22:00:39 +0200

    Solution 24 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/icc3h1og Prompt: b3de8fbe-a7c7-40c3-8db3-2a1edaea5f10.cpp: In function ‘std::string rounded_avg(i nt, int)’: b3de8fbe-a7c7-40c3-8db3-2a1edaea5f10.cpp:12:16: error: invalid operands of types  ‘double’ and ‘int’ to binary ‘operator&’    12 |         if(avg & 1) res.push_back('1');       |            ~~~ ^ ~       |            |     |       |            |     int       |            double b3de8fbe-a7c7-40c3-8db3-2a1edaea5f10.cpp: At global scope: b3de8fbe-a7c7-40c3-8db3-2a1edaea5f10.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ b3de8fbe-a7c7-40c3-8db3-2a1edaea5f10.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main(){       |     ^~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include <string>
 using namespace std;
 
 string rounded_avg(int n,int m){
@@ -9,16 +9,9 @@ string rounded_avg(int n,int m){
     avg = floor(avg + 0.5);
     string res = "";
     while(avg > 0){
-        avg = floor(avg + 0.5);
-        if(avg >= 1) res.push_back('1');
+        if(int(avg) % 2) res.push_back('1');
         else res.push_back('0');
         avg /= 2;
     }
     reverse(res.begin(), res.end());
     return res;
-}
-
-int main(){
-    cout << rounded_avg(5, 5) << endl;
-    return 0;
-}
