09c03ff471bc25fadc48bead6729ddb0daab2e88
beep boop <githubbot@vadim.me>
2024-07-10 22:40:34 +0200

    Solution 54 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: b/python3.11/site-packages/programlib/programs/600456ac-f01b-4e30-85ec-0b1d92f29 f91.py", line 10, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/600456ac-f01b-4e30-85ec-0b1d92f29 f91.py", line 5, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/600456ac-f01b-4e30-85ec-0b1d92f29 f91.py", line 2, in sort_third     return sorted([x for sublist in l for x in sublist if not isinstance(x, list )] +                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/600456ac-f01b-4e30-85ec-0b1d92f29 f91.py", line 2, in <listcomp>     return sorted([x for sublist in l for x in sublist if not isinstance(x, list )] +                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ TypeError: 'int' object is not iterable

--- Python_33.py
+++ Python_33.py
@@ -1,3 +1,4 @@
 def sort_third(l):
-    flat_list = [x for sublist in l for x in sublist]
-    return sorted([i for i in flat_list if isinstance(i, int)]) + [sublist for sublist in l if isinstance(sublist, list)]
+    non_lists = [x for sublist in l for x in sublist if not isinstance(x, list)]
+    lists = [sublist for sublist in l if isinstance(sublist, list)]
+    return sorted(non_lists + lists)
