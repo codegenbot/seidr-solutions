71f9f07100052a7877dd2863c9183892887e410c
beep boop <githubbot@vadim.me>
2024-07-10 22:19:59 +0200

    Solution 82 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3mr73mpr Prompt: 36d7e11a-cd92-418f-909a-45fbee909570.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -4,17 +4,17 @@
 using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_any_of<std::string>(a)) {
-        if (is_any_of<std::string>(b)) {
-            return (get<std::string>(a) == get<std::string>(b)) ? any("") : max(a, b);
+    if (is_any_of<string>(a)) {
+        if (is_any_of<string>(b)) {
+            return (get<string>(a) == get<string>(b)) ? any("None") : max(a, b);
         } else {
             return max(a, boost::any(get<double>(b)));
         }
     } else {
-        if (is_any_of<std::string>(b)) {
+        if (is_any_of<string>(b)) {
             return max(boost::any(get<double>(a)), b);
         } else {
-            return (get<double>(a) > get<double>(b)) ? a : ((get<double>(a) == get<double>(b))) ? any("") : b;
+            return (get<double>(a) > get<double>(b)) ? a : ((get<double>(a) == get<double>(b))) ? any("None") : b;
         }
     }
 }
