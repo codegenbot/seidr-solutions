233974478510153bc689aee38f51c2fedf128a0b
beep boop <githubbot@vadim.me>
2024-07-10 03:51:09 +0200

    Solution 57 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/8ib57hw0 Prompt:       |                                     ^~~~~~ 3800e755-5ddb-4e28-bfbc-c72f49592e11.cpp:25:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 3800e755-5ddb-4e28-bfbc-c72f49592e11.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 3800e755-5ddb-4e28-bfbc-c72f49592e11.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 3800e755-5ddb-4e28-bfbc-c72f49592e11.cpp:25:44: error: ‘b’ was not declared in t his scope    25 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3800e755-5ddb-4e28-bfbc-c72f49592e11.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 3800e755-5ddb-4e28-bfbc-c72f49592e11.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cassert>
 
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b){
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
     return a == b;
 }
 
@@ -18,6 +18,5 @@ std::vector<std::string> filter_by_prefix(const std::vector<std::string>& vec, c
 
 int main() {
     assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"), 
-           {"xxx", "xxxAAA", "xxx"}));
-    return 0;
+                   std::vector<std::string>{"xxx", "xxxAAA", "xxx"}));
 }
