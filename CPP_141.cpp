ab7177f1f7b66015f1f1e0beebbd69c5d99bd742
beep boop <githubbot@vadim.me>
2024-07-10 11:01:32 +0200

    Solution 59 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/rtsa7yfm Prompt: Complete the following code given the task description and function signature.

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,22 +1,37 @@
-string file_name_check(string file_name){
-    int digit_count = 0;
-    bool found_dot = false;
-    for(int i=0; i<file_name.length(); i++){
-        if(file_name[i] >= '0' && file_name[i] <= '9'){
-            digit_count++;
-        } else if(file_name[i] == '.'){
-            found_dot = true;
-        } else if(!found_dot && !isalpha((unsigned char)file_name[i])){
+Here is the completed code:
+
+```cpp
+bool has_digit = false;
+int dot_count = 0;
+
+for (char c : file_name) {
+    if (isdigit(c)) {
+        if (!has_digit) {
+            has_digit = true;
+        } else {
+            return "No";
+        }
+    } else if (c == '.') {
+        dot_count++;
+        if (dot_count > 1) {
             return "No";
         }
     }
-    if(digit_count > 3 || !found_dot){
-        return "No";
-    }
-    size_t pos = file_name.find('.');
-    string extension = file_name.substr(pos+1);
-    if(extension != "txt" && extension != "exe" && extension != "dll"){
-        return "No";
-    }
-    return "Yes";
 }
+
+if (file_name.find('.') != string::npos) {
+    string suffix = file_name.substr(file_name.find('.') + 1);
+    if (suffix != "txt" && suffix != "exe" && suffix != "dll") {
+        return "No";
+    }
+} else {
+    if (!isalpha(file_name[0])) {
+        return "No";
+    }
+}
+
+if (has_digit || dot_count > 1) {
+    return "No";
+}
+
+return "Yes";
