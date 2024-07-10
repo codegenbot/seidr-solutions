844324b3a1f072023d4736ad03df540216740d2a
beep boop <githubbot@vadim.me>
2024-07-10 04:54:23 +0200

    Solution 60 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/z0n0e71f Prompt: 18f3b62a-c164-4ebd-ab97-7516af9392b8.cpp:29:26: error: ‘whitePegs’ cannot be use d as a function    29 |     int white = whitePegs(code, guess);       |                 ~~~~~~~~~^~~~~~~~~~~~~ 18f3b62a-c164-4ebd-ab97-7516af9392b8.cpp:30:26: error: ‘blackPegs’ cannot be use d as a function    30 |     int black = blackPegs(code, guess);       |                 ~~~~~~~~~^~~~~~~~~~~~~ 18f3b62a-c164-4ebd-ab97-7516af9392b8.cpp:31:5: error: ‘cout’ was not declared in  this scope    31 |     cout << white << endl;       |     ^~~~ 18f3b62a-c164-4ebd-ab97-7516af9392b8.cpp:31:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 18f3b62a-c164-4ebd-ab97-7516af9392b8.cpp:31:22: error: ‘endl’ was not declared i n this scope    31 |     cout << white << endl;       |                      ^~~~ 18f3b62a-c164-4ebd-ab97-7516af9392b8.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,18 +1,37 @@
-int mastermind(string code, string guess) {
-    int white = 0;
-    int black = 0;
-    vector<char> code_chars(code.begin(), code.end());
-    for (int i = 0; i < 4; ++i) {
-        char c1 = code[i];
-        char c2 = guess[i];
-        if (c1 == c2) {
-            --code_chars.size();
-            black++;
-        } else if (count(code_chars.begin(), code_chars.end(), c2)) {
-            white++;
-            auto it = find(code_chars.begin(), code_chars.end(), c2);
-            code_chars.erase(it);
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int whitePegs(string code, string guess) {
+    int count = 0;
+    for (int i = 0; i < 4; i++) {
+        if (code[i] == guess[i]) {
+            count++;
         }
     }
-    return {white, black};
+    return count;
+}
+
+int blackPegs(string code, string guess) {
+    int count = 0;
+    vector<char> codeVec(code.begin(), code.end());
+    for (int i = 0; i < 4; i++) {
+        if (code[i] == guess[i]) {
+            codeVec.erase(remove(codeVec.begin(), codeVec.end(), code[i]), codeVec.end());
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    string code, guess;
+    cin >> code >> guess;
+    int white = whitePegs(code, guess);
+    int black = blackPegs(code, guess);
+    cout << white << endl;
+    cout << black << endl;
+    return 0;
 }
