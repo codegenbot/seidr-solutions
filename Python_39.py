4ce7d0e0f8946cc23e0f87eeb0b903e86cbf4ddd
beep boop <githubbot@vadim.me>
2024-07-10 17:38:59 +0200

    Solution 39 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/2ur155ri Prompt: Complete the following code given the task description and function signature.

--- Python_39.py
+++ Python_39.py
@@ -11,12 +11,13 @@ def prime_fib(n: int):
         if n <= 1:
             return n
         else:
-            return fibonacci(n-1) + fibonacci(n-2)
+            return fibonacci(n - 1) + fibonacci(n - 2)
 
     count = 0
-    i = 0
+    num = 1
     while count < n:
-        if is_prime(fibonacci(i)):
-            count += 1
-        i += 1
-    return fibonacci(i-1)
+        num += 1
+        if is_prime(num):
+            if num == fibonacci(num):
+                count += 1
+    return num
