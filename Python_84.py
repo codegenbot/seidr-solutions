f7e2706a07ec5e719d54448ffde8cb4bc079170a
beep boop <githubbot@vadim.me>
2024-07-10 16:51:25 +0200

    Solution 50 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/l4jlf75r Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/58632c45-1691-4893-a7ce-1b29d601b f3e.py", line 9, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/58632c45-1691-4893-a7ce-1b29d601b f3e.py", line 6, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -1,4 +1,2 @@
-```python
 def solve(N):
-    result = str(bin(sum(1 if i % 2 else 0 for i in range(1, N+1)))[2:])
-    return result
+    return bin(sum(i for i in range(1, N+1) if i % 4 == 0))[2:]
