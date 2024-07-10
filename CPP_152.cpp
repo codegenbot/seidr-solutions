5547acae72d8290f5f2be4d99a5f62e18d57b00c
beep boop <githubbot@vadim.me>
2024-07-10 08:23:05 +0200

    Solution 28 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/m9ihdebv Prompt:       |                    ^~~ d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp:10: d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp: In function ‘int main()’: d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp:20:20: error: ‘compare’ was not declare d in this scope    20 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -2,26 +2,24 @@
 #include <algorithm>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    if (a.size() != b.size()) {
+    if (a.size() != b.size()) 
         return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
+    for (int i = 0; i < a.size(); i++) 
+        if (a[i] != b[i]) 
             return false;
-        }
-    }
     return true;
 }
 
-std::vector<int> compare(const std::vector<int>& game, const std::vector<int>& guess) {
+std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
     std::vector<int> result;
     for (int i = 0; i < game.size(); i++) {
-        if (issame({game[i], guess[i]}, {guess[i], game[i]}) || issame({game[i], guess[i]}, {guess[i], game[i]})) {
-            result.push_back(1);
-        } else {
-            int diff = abs(guess[i] - game[i]);
+        int diff = abs(guess[i] - game[i]);
+        if (guess[i] == game[i]) 
+            result.push_back(0);
+        else if (std::find(result.begin(), result.end(), diff) != result.end()) 
+            result.push_back(-1);
+        else
             result.push_back(diff);
-        }
     }
     return result;
 }
