0c0a7e142417a633904fe502198f6b4b239ff2c5
beep boop <githubbot@vadim.me>
2024-07-10 18:27:13 +0200

    Solution 3 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hwoml10f Prompt:       |                              ^~~~~~ 68f6ab86-c56f-41dc-a08f-21602cbc6655.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 68f6ab86-c56f-41dc-a08f-21602cbc6655.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 68f6ab86-c56f-41dc-a08f-21602cbc6655.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 68f6ab86-c56f-41dc-a08f-21602cbc6655.cpp:9: 68f6ab86-c56f-41dc-a08f-21602cbc6655.cpp: In function ‘int main()’: 68f6ab86-c56f-41dc-a08f-21602cbc6655.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ 68f6ab86-c56f-41dc-a08f-21602cbc6655.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,23 +1,40 @@
-```cpp
+#include <iostream>
 #include <vector>
 #include <string>
 
-bool issame(string s1, string s2) {
-    if(s1.size() != s2.size()) {
+bool issame(string a, string b) {
+    if(a.length() != b.length()) {
         return false;
     }
-    for(int i = 0; i < s1.size(); i++) {
-        if(s1[i] != s2[i]) {
+    for(int i = 0; i < a.length(); ++i) {
+        if(a[i] != b[i]) {
             return false;
         }
     }
     return true;
 }
 
-vector<string> all_prefixes(string str) {
-    vector<string> result;
-    for (int i = 1; i <= str.length(); ++i) {
-        result.push_back(str.substr(0, i));
+int main() {
+    string str;
+    cin >> str;
+
+    vector<string> all_prefixes(string str) {
+        vector<string> result;
+        for (int i = 1; i <= str.length(); ++i) {
+            result.push_back(str.substr(0, i));
+        }
+        return result;
     }
-    return result;
+
+    vector<string> prefix_list = all_prefixes(str);
+    string target_str;
+    cin >> target_str;
+
+    for(string s : prefix_list) {
+        if(issame(s, target_str)) {
+            cout << "The prefix is: " << s << endl;
+        }
+    }
+
+    return 0;
 }
