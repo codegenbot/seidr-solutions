f3dc0a2cfb0c34ab71c98c41f466def0cde1e6e5
beep boop <githubbot@vadim.me>
2024-07-10 06:01:25 +0200

    Solution 19 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/v619oyq3 Prompt: 338b97bb-42e5-429d-bb34-3a18195ddb96.cpp:1:10: fatal error: boost/algorithm/stri ng.hpp: No such file or directory     1 | #include <boost/algorithm/string.hpp>       |          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,10 +1,10 @@
 #include <boost/config.hpp>
 #include <boost/any.hpp>
 
-vector<int> filter_integers(list-any values) {
+vector<int> filter_integers(list<any> values) {
     vector<int> result;
     for (const auto& value : values) {
-        if (boost::any_cast<int>(value, true)) {
+        if (boost::any_cast<int>(value)) {
             result.push_back(boost::any_cast<int>(value));
         }
     }
