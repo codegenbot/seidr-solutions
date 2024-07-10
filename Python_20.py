c88b8f54327d5e1bb66082903427a937b7dc14cf
beep boop <githubbot@vadim.me>
2024-07-10 19:46:09 +0200

    Solution 33 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/808aqmfb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9af9719f-2737-4793-850a-b141b4df9 5ed.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_20.py
+++ Python_20.py
@@ -1,6 +1,5 @@
 ```
 from typing import List, Tuple
-
 def find_closest_elements() -> Tuple[float, float]:
     while True:
         try:
@@ -19,5 +18,4 @@ def find_closest_elements() -> Tuple[float, float]:
 
         except ValueError:
             print("Invalid input. Please enter valid decimal or integer values.")
-
 print(find_closest_elements())
