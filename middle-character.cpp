f99d1c92b43e9f850a178069a8aae16f33f33a40
beep boop <githubbot@vadim.me>
2024-07-10 19:33:24 +0200

    Solution 8 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/haqt7jhu Prompt: 2ccdcae2-5db4-404d-81c4-c213efba50f9.cpp: In function ‘int main()’: 2ccdcae2-5db4-404d-81c4-c213efba50f9.cpp:2:5: error: ‘string’ was not declared i n this scope     2 |     string s;       |     ^~~~~~ 2ccdcae2-5db4-404d-81c4-c213efba50f9.cpp:3:5: error: ‘cin’ was not declared in t his scope     3 |     cin >> s;       |     ^~~ 2ccdcae2-5db4-404d-81c4-c213efba50f9.cpp:3:12: error: ‘s’ was not declared in th is scope     3 |     cin >> s;       |            ^ 2ccdcae2-5db4-404d-81c4-c213efba50f9.cpp:6:9: error: ‘cout’ was not declared in this scope     6 |         cout << s.substr(len/2 - 1, 2);       |         ^~~~ 2ccdcae2-5db4-404d-81c4-c213efba50f9.cpp:8:9: error: ‘cout’ was not declared in this scope     8 |         cout << s.substr(len/2, 1);       |         ^~~~

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main() {
-    std::string s;
-    std::cin >> s;
+    string s;
+    cin >> s;
     int len = s.length();
     if (len % 2 == 0) {
-        std::cout << s.substr(len/2 - 1, 2);
+        cout << s.substr(len/2 - 1, 2);
     } else {
-        std::cout << s.substr(len/2, 1);
+        cout << s.substr(len/2, 1);
     }
     return 0;
 }
