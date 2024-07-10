7bbb521dd62f10cadc77db060d740a82733c0ee9
beep boop <githubbot@vadim.me>
2024-07-10 12:10:05 +0200

    Solution 67 of Python/141, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/1cmhxa0u Prompt: Complete the following code given the task description and function signature.

--- Python_141.py
+++ Python_141.py
@@ -1,12 +1,12 @@
-```
 def file_name_check(file_name):
-    if len([char for char in file_name if char.isdigit()]) > 3:
+    if len([c for c in file_name if c.isdigit()]) > 3:
         return 'No'
     if file_name.count('.') != 1:
         return 'No'
-    parts = file_name.split('.')
-    if not parts[0][0].isalpha():
+    before_dot = file_name.split('.')[0]
+    after_dot = file_name.split('.')[1].lower()
+    if not before_dot or not set(before_dot).issubset('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'):
         return 'No'
-    if parts[1] not in ['txt', 'exe', 'dll']:
+    if after_dot not in ['txt', 'exe', 'dll']:
         return 'No'
     return 'Yes'
