bfb4870eeb01bd1565face8d40b0cbb8111b4047
beep boop <githubbot@vadim.me>
2024-07-10 10:41:18 +0200

    Solution 94 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/nwvs2wmn Prompt: Complete the following code given the task description and function signature.

--- Python_151.py
+++ Python_151.py
@@ -1,4 +1,2 @@
 def double_the_difference(lst):
-    return sum(
-        num**2 for num in lst if isinstance(num, int) and num > 0 and num % 2 != 0
-    )
+    return sum(x ** 2 for x in lst if x > 0 and x % 2 != 0) if lst else 0
