d603dcbd5ea3760ed55af99b7e6c6626c1c74acb
beep boop <githubbot@vadim.me>
2024-07-10 16:42:55 +0200

    Solution 42 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/v89qwp07 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/04413b1d-007c-4896-9bad-deaaabec5 eaf.py", line 1     Here is the modified code:                 ^^^^^^^^ SyntaxError: invalid syntax

--- Python_50.py
+++ Python_50.py
@@ -1,11 +1,2 @@
-```
 def decode_shift(s: str):
-    if not set(s).issubset('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'):
-        return 'Invalid input. Only alphabets are allowed.'
-    result = ""
-    for ch in s:
-        if 'a' <= ch.lower() <= 'z':
-            result += chr(((ord(ch.lower()) - 5 - ord("a")) % 26) + ord("a"))
-        else:
-            result += ch
-    return result.upper()
+    return "".join([chr(((ord(ch.lower()) - 5 - ord("a")) % 26) + ord("a")) if 'a' <= ch.lower() <= 'z' else ch.lower() if 'a' <= ch.lower() <= 'z' else ch for ch in s]).upper()
