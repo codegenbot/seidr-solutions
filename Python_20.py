8996eb832edeff24cdd053ac82dffb6ac9202dd1
beep boop <githubbot@vadim.me>
2024-07-10 04:18:35 +0200

    Solution 59 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9z8gtv5q Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9d96cf8c-ffbc-4dc3-bd48-6d3eadad7 25e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_20.py
+++ Python_20.py
@@ -1,10 +1,11 @@
+```
 def check(lst):
     lst.sort()
     result = []
     for i in range(len(lst) - 1):
         closest_pair = (abs(lst[i] - lst[i+1]), (lst[i], lst[i+1]))
         result.append(closest_pair)
-    return result[0][0]
+    return min(result)[0]
 
 arr = list(map(int, input().split()))
 print(check(arr))
