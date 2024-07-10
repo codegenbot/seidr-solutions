424f1bd2fa3ac73d82dcc81d806a01b4d3c64f7b
beep boop <githubbot@vadim.me>
2024-07-10 22:44:18 +0200

    Solution 24 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/qew3a3cc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6ee38b1a-a77c-447e-a3f4-8ae9ac6fa f87.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_144.py
+++ Python_144.py
@@ -1,15 +1,4 @@
 def simplify(x, n):
     a, b = map(int, x.split("/"))
     c, d = map(int, n.split("/"))
-
-    if a == 1:
-        a = ""
-    else:
-        a = str(a) + "/"
-
-    if d == 1:
-        d = ""
-    else:
-        d = "/" + str(d)
-
-    return str(a) + str(b) + d
+    return float(a*d) == float(b*c)
