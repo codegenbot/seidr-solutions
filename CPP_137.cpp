45af2dfbe237ae913bf9b36ac1edc78baef267a3
beep boop <githubbot@vadim.me>
2024-07-10 10:59:55 +0200

    Solution 78 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/51eh1c3h Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,41 +1,19 @@
-using namespace boost;
+#include <boost/lexical_cast.hpp>
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_any_of<string>(a)) {
-        string strA = any_cast<string>(a);
-        if (is_any_of<string>(b)) {
-            string strB = any_cast<string>(b);
-            return is_greater(strA, strB) ? a : (is_greater(strB, strA) ? b : boost::any("None"));
-        } else {
-            double numB = any_cast<double>(b);
-            return is_greater(strA, to_string(numB)) ? a : (is_greater(to_string(numB), strA) ? b : boost::any("None"));
-        }
-    } else {
-        double numA = any_cast<double>(a);
-        if (is_any_of<string>(b)) {
-            string strB = any_cast<string>(b);
-            return is_greater(to_string(numA), strB) ? a : (is_greater(strB, to_string(numA)) ? b : boost::any("None"));
-        } else {
-            double numB = any_cast<double>(b);
-            return (numA > numB) ? a : ((numA < numB) ? b : boost::any("None"));
-        }
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        return a.get<int>() > b.get<int>() ? a : b.get<int>() == a.get<int>() ? "None" : b;
+    } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
+        return a.get<double>() > b.get<double>() ? a : b.get<double>() == a.get<double>() ? "None" : b;
+    } else if ((a.type() == typeid(int) || a.type() == typeid(double)) &&
+               (b.type() == typeid(string))) {
+        return boost::any_cast<string>(a) > boost::any_cast<string>(b) ? a : b.get<string>() == boost::any_cast<string>(a) ? "None" : b;
+    } else if ((a.type() == typeid(string)) && (b.type() == typeid(int) || b.type() == typeid(double))) {
+        return boost::any_cast<string>(b) > boost::any_cast<string>(a) ? b : boost::any_cast<string>(a) == boost::any_cast<string>(b) ? "None" : a;
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        return boost::lexical_cast<double>(boost::any_cast<string>(a)) > 
+               boost::lexical_cast<double>(boost::any_cast<string>(b)) ? a : 
+               boost::lexical_cast<double>(boost::any_cast<string>(a)) == 
+               boost::lexical_cast<double>(boost::any_cast<string>(b)) ? "None" : b;
     }
-}
-
-bool is_greater(const string& str1, const string& str2) {
-    vector<string> split1 = split(str1, ",");
-    vector<string> split2 = split(str2, ",");
-    return stod(split1[0]) > stod(split2[0]);
-}
-
-vector<string> split(const string& str, char c) {
-    vector<string> result;
-    string s = str;
-    while (s.find(c) != string::npos) {
-        size_t pos = s.find(c);
-        result.push_back(s.substr(0, pos));
-        s.erase(0, pos + 1);
-    }
-    result.push_back(s);
-    return result;
 }
