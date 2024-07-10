e3503cb5398f659329b56a155b69d3fb3695d574
beep boop <githubbot@vadim.me>
2024-07-10 20:44:00 +0200

    Solution 52 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/808aqmfb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/884aae43-bef2-430a-9854-6a29684cb 86e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_20.py
+++ Python_20.py
@@ -1,12 +1,9 @@
 from typing import Tuple
 
-
 def find_closest_elements() -> Tuple[float, float]:
     while True:
         try:
-            numbers = [
-                float(num) for num in input("Enter space-separated numbers: ").split()
-            ]
+            numbers = [float(num) for num in input("Enter space-separated numbers: ").split()]
             numbers.sort()
             min_diff = float("inf")
             closest_pair = (None, None)
@@ -21,6 +18,5 @@ def find_closest_elements() -> Tuple[float, float]:
 
         except ValueError:
             print("Invalid input. Please enter valid decimal or integer values.")
-
 
 print(find_closest_elements())
