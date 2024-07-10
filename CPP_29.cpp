b8e634834a1efc4598a0d3fa6aded9977d9e320f
beep boop <githubbot@vadim.me>
2024-07-10 02:13:32 +0200

    Solution 49 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/hpb2nzt0 Prompt:    31 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 40d40f77-acf9-447b-a657-1f03b2babcba.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 40d40f77-acf9-447b-a657-1f03b2babcba.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 40d40f77-acf9-447b-a657-1f03b2babcba.cpp:30: 40d40f77-acf9-447b-a657-1f03b2babcba.cpp: In function ‘int main()’: 40d40f77-acf9-447b-a657-1f03b2babcba.cpp:40:19: error: too few arguments to func tion ‘bool issame(const std::vector<std::__cxx11::basic_string<char> >&, const s td::vector<std::__cxx11::basic_string<char> >&, const string&)’    40 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 40d40f77-acf9-447b-a657-1f03b2babcba.cpp:5:6: note: declared here     5 | bool issame(const std::vector<std::string>& v1, const std::vector<std::s tring>& v2, const std::string& b) {       |      ^~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,8 +1,9 @@
+```
 #include <initializer_list>
 #include <vector>
 #include <string>
 
-bool issame(const std::vector<std::string>& v1, const std::vector<std::string>& v2) {
+bool issame(const std::vector<std::string>& v1, const std::vector<std::string>& v2, const std::string& b) {
     return v1.size() == v2.size() && std::equal(v1.begin(), v1.end(), v2.begin());
 }
 
@@ -16,13 +17,4 @@ std::vector<std::string> filter_by_prefix(const std::vector<std::string>& vec, c
     return result;
 }
 
-int main() {
-    std::vector<std::string> a = {"xxx", "asd", "xxy", "john doe", "xxxxAA", "xxx"};
-    std::vector<std::string> b = filter_by_prefix(a, "xxx");
-    
-    if (issame(b, {"xxx", "xxxAAA", "xxx"})) {
-        return 0;
-    } else {
-        return -1; 
-    }
-}
+std::vector<std::string>({"xxx", "xxxAAA", "xxx"})
