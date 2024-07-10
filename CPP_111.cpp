902bdc7efa236ab94dce6bea5d9bbe68e2fa0144
beep boop <githubbot@vadim.me>
2024-07-10 21:04:17 +0200

    Solution 8 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rmdf06ap Prompt:       |                             ^~~ 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp:27:33: error: expected primary-expressi on before ‘char’    27 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp:27:38: error: expected primary-expressi on before ‘int’    27 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp:27:44: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp:26: 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp: In function ‘int main()’: 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp:41:20: error: ‘histogram’ was not decla red in this scope    41 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,6 +1,18 @@
 #include <map>
 #include <string>
 
+bool issame(map<char,int> a,map<char,int> b){
+    if(a.size() != b.size()) return false;
+    for(auto& p : a) {
+        if(b.find(p.first) == b.end() || p.second != b.at(p.first)) return false;
+    }
+    return true;
+}
+
+int main() {
+    assert(issame(histogram("a") , {{'a', 1}}));
+}
+
 map<char, int> histogram(string test) {
     map<char, int> result;
     string str = test;
@@ -18,15 +30,10 @@ map<char, int> histogram(string test) {
     for (auto it = result.begin(); it != result.end(); ++it) {
         if (it->second > maxCount) {
             maxCount = it->second;
-            maxMap.clear();
-            maxMap[it->first] = it->second;
+            maxMap = {{it->first, it->second}};
         } else if (it->second == maxCount) {
             maxMap[make_pair(it->first, it->second)] = it->second;
         }
     }
     return maxMap;
-}
-
-bool issame(map<char,int> a,map<char,int> b) {
-    return a == b;
 }
