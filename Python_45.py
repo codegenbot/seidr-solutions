793dc989c773d961d45404bd0d0830bfd03895f3
beep boop <githubbot@vadim.me>
2024-07-10 20:59:33 +0200

    Solution 2 of Python/45, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/hvvtu8gl Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03236ea1-aed0-4f5b-98e5-3a1fb9a43 0b2.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_45.py
+++ Python_45.py
@@ -1,6 +1,2 @@
-def triangle_area():
-    a = int(input("Enter the base of the triangle: "))
-    h = int(input("Enter the height of the triangle: "))
+def triangle_area(a, h):
     return (a * h) / 2
-
-print(triangle_area())
