d93d710617d6263765bcb0c3f4d34397c4f60862
beep boop <githubbot@vadim.me>
2024-07-11 00:08:00 +0200

    Solution 6 of Python/145, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ue71hic4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dfd01b79-a90b-4880-85a1-10b88ba23 134.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_145.py
+++ Python_145.py
@@ -1,3 +1,2 @@
-def order_by_points():
-    nums = list(map(int, input("Enter the numbers separated by space: ").split()))
-    return sorted([num, sum(map(int, str(abs(num))))) for num in nums])
+def order_by_points(nums):
+    return sorted([num, sum(map(int, str(abs(num))))] for num in nums)
