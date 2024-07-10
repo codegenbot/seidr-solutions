bc17ffa02ff0923901b3a6efdf1144326e6ae51f
beep boop <githubbot@vadim.me>
2024-07-10 20:07:28 +0200

    Solution 11 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ptpr8f4c Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 3f78f420-5881-42de-91de-516960372098.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -2,13 +2,14 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <initializer_list>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> vec) {
-    if (a.size() != vec.size()) {
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a.size() != b.size()) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
-        std::vector<std::string> temp = {a[i], vec[i]};
+        std::vector<std::string> temp = {a[i], b[i]};
         std::sort(temp.begin(), temp.end());
         if (temp[0] != temp[1]) {
             return false;
@@ -22,6 +23,7 @@ std::vector<std::string> sorted_list_sum(std::vector<std::string> v) {
     return v;
 }
 
-std::vector<std::string> vec = {"aaaa", "bbbb", "dd", "cc"};
-assert(issame(sorted_list_sum(vec), {"ccc", "dd", "aaaa", "bbbb"}));
-return 0;
+int main() {
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"ccc", "dd", "aaaa", "bbbb"}) == true);
+    return 0;
+}
