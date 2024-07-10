cb5aaaa5c30fa2c303b7f1f33bf1febc1b36407d
beep boop <githubbot@vadim.me>
2024-07-10 08:23:57 +0200

    Solution 18 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kd2jtp3g Prompt:       |                       ^~~~~~ 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:1:31: error: ‘operator_’ was not declar ed in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                               ^~~~~~~~~ 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:1:42: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                          ^~~~~~ 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:1:49: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                 ^~~ 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:1:61: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                             ^ In file included from 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:50: 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp: In function ‘int main()’: 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:52:23: error: ‘do_algebra’ cannot be us ed as a function    52 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,43 +1,48 @@
+```cpp
 #include <vector>
 #include <string>
 #include <cmath>
 
 int do_algebra(vector<string> operator_, vector<int> operand) {
     int result = 0;
-    int temp = 0;
-    bool inParenthesis = false;
-
-    for (int i = 0; i < operator_.size(); i++) {
-        if (operator_[i] == "(") {
-            inParenthesis = true;
-        } else if (operator_[i] == ")") {
-            inParenthesis = false;
-        } else if (!inParenthesis) {
-            if (operator_[i] == "+") {
-                result += operand[i];
-            } else if (operator_[i] == "-") {
-                result -= operand[i];
-            } else if (operator_[i] == "*") {
-                temp *= operand[i];
-            } else if (operator_[i] == "/") {
-                temp /= operand[i];
-            } else if (operator_[i] == "**") {
-                temp = pow(temp, operand[i]);
+    int i = 0;
+    while (i < operator_.size()) {
+        if (operator_[i] == "+") {
+            result += operand[i];
+        } else if (operator_[i] == "-") {
+            result -= operand[i];
+        } else if (operator_[i] == "*") {
+            int temp = operand[i];
+            for (++i; i < operator_.size(); ++i) {
+                if (operator_[i] == "*") {
+                    temp *= operand[i];
+                } else if (operator_[i] == "/") {
+                    temp /= operand[i];
+                }
             }
-        } else {
-            if (operator_[i] == "+") {
-                temp += operand[i];
-            } else if (operator_[i] == "-") {
-                temp -= operand[i];
-            } else if (operator_[i] == "*") {
-                temp *= operand[i];
-            } else if (operator_[i] == "/") {
-                temp /= operand[i];
-            } else if (operator_[i] == "**") {
-                temp = pow(temp, operand[i]);
+            result += temp;
+        } else if (operator_[i] == "/") {
+            int temp = operand[i];
+            for (++i; i < operator_.size(); ++i) {
+                if (operator_[i] == "*") {
+                    temp *= operand[i];
+                } else if (operator_[i] == "/") {
+                    temp /= operand[i];
+                }
             }
+            result += temp;
+        } else if (operator_[i] == "**") {
+            int temp = operand[i];
+            for (++i; i < operator_.size(); ++i) {
+                if (operator_[i] == "*") {
+                    temp *= operand[i];
+                } else if (operator_[i] == "/") {
+                    temp /= operand[i];
+                }
+            }
+            result += pow(operand[0], temp);
         }
+        ++i;
     }
-
     return result;
 }
