50f9bc721e47d9e1c9ab8e30757d5c5c8c25f428
beep boop <githubbot@vadim.me>
2024-07-11 01:48:10 +0200

    Solution 21 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8kvpmmn6 Prompt:       |                           ^~~~~~ 3c36a9bf-994b-4900-9b20-a4295fbd0907.cpp:27:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 3c36a9bf-994b-4900-9b20-a4295fbd0907.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 3c36a9bf-994b-4900-9b20-a4295fbd0907.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 3c36a9bf-994b-4900-9b20-a4295fbd0907.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3c36a9bf-994b-4900-9b20-a4295fbd0907.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 3c36a9bf-994b-4900-9b20-a4295fbd0907.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -5,7 +5,8 @@
 std::vector<int> compare(std::vector<int> a, std::vector<int> b) {
     std::sort(a.begin(), a.end());
     std::sort(b.begin(), b.end());
-    return {a.size(), b.size()};
+    return {a.size() - (int)std::count(a.begin(), a.end(), 0), 
+            b.size() - (int)std::count(b.begin(), b.end(), 0)};
 }
 
 bool sameSize(std::vector<int> a, std::vector<int> b) {
@@ -20,5 +21,6 @@ bool sameSize(std::vector<int> a, std::vector<int> b) {
 
 int main() {
     assert(sameSize(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));
+    // Your logic
     return 0;
 }
