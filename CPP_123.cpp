5b602f229235598b19acaeaa5fdec926bb457238
beep boop <githubbot@vadim.me>
2024-07-10 19:48:25 +0200

    Solution 70 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/e4xegzqk Prompt:       |                    ^~~ dfea75de-ac70-4701-8221-ca1614282d73.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ dfea75de-ac70-4701-8221-ca1614282d73.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ dfea75de-ac70-4701-8221-ca1614282d73.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from dfea75de-ac70-4701-8221-ca1614282d73.cpp:16: dfea75de-ac70-4701-8221-ca1614282d73.cpp: In function ‘int main()’: dfea75de-ac70-4701-8221-ca1614282d73.cpp:26:20: error: ‘get_odd_collatz’ was not  declared in this scope    26 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ dfea75de-ac70-4701-8221-ca1614282d73.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -5,15 +5,17 @@ bool issame(int a, int b) {
     return a == b;
 }
 
-std::vector<int> get_odd_collatz(int n) {
-    std::vector<int> result;
-    while (n != 1) {
-        if (n % 2 == 1) {
-            result.push_back(n);
-        }
-        n = (n % 2 == 0) ? n / 2 : 3 * n + 1;
+vector<int> result;
+while (n != 1) {
+    if (n % 2 == 1) {
+        result.push_back(n);
     }
-    result.push_back(1);
-    std::sort(result.begin(), result.end());
-    return result;
+    if (n % 2 == 0) {
+        n = n / 2;
+    } else {
+        n = 3 * n + 1;
+    }
 }
+result.push_back(1);
+sort(result.begin(), result.end(), issame);
+return result;
