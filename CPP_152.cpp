41cb2941f1108ff1927cc0d3f12b851188640496
beep boop <githubbot@vadim.me>
2024-07-11 01:08:59 +0200

    Solution 10 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8kvpmmn6 Prompt:       |                    ^~~ bd810298-e895-4050-9029-283cb7d9ee9b.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bd810298-e895-4050-9029-283cb7d9ee9b.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bd810298-e895-4050-9029-283cb7d9ee9b.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bd810298-e895-4050-9029-283cb7d9ee9b.cpp:10: bd810298-e895-4050-9029-283cb7d9ee9b.cpp: In function ‘int main()’: bd810298-e895-4050-9029-283cb7d9ee9b.cpp:20:20: error: ‘compare’ was not declare d in this scope    20 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ bd810298-e895-4050-9029-283cb7d9ee9b.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,13 +1,16 @@
 #include <vector>
+using namespace std;
 
-vector<int> compare(vector<int> game, vector<int> guess) {
-    vector<int> result;
-    for (int i = 0; i < game.size(); i++) {
-        if (game[i] == guess[i]) {
-            result.push_back(0);
-        } else {
-            result.push_back(abs(guess[i] - game[i]));
+bool issame(vector<int> a, vector<int> b) {
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
         }
     }
-    return result;
+    return true;
+}
+
+int main() {
+    assert(issame({1,2,3,5},{-1,2,3,4}) == vector<bool>({{!(a[0]==b[0]),!((a[1]-b[1])!=0),!((a[2]-b[2])!=0),!(a[3]!=b[3])}}));
+    return 0;
 }
