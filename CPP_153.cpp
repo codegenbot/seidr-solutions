a0532e8182ce4b8d8c5bf510afa817d05ecb8cd1
beep boop <githubbot@vadim.me>
2024-07-10 08:34:50 +0200

    Solution 46 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/pv10217g Prompt:       |                         ^~~~~~ c98ab777-7ccc-4c22-9a45-d794e7243460.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ c98ab777-7ccc-4c22-9a45-d794e7243460.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ c98ab777-7ccc-4c22-9a45-d794e7243460.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ c98ab777-7ccc-4c22-9a45-d794e7243460.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from c98ab777-7ccc-4c22-9a45-d794e7243460.cpp:21: c98ab777-7ccc-4c22-9a45-d794e7243460.cpp: In function ‘int main()’: c98ab777-7ccc-4c22-9a45-d794e7243460.cpp:23:32: error: ‘Strongest_Extension’ can not be used as a function    23 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,15 +1,17 @@
+```cpp
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cctype>
 
-int Strongest_Extension(string class_name,vector<string> extensions){
+int Strongest_Extension(std::string class_name, std::vector<std::string> extensions){
     int strongest = -1;
-    string strongest_ext;
+    std::string strongest_ext;
     for(auto ext : extensions){
         int cap = 0, sm = 0;
         for(char c : ext){
-            if(isupper(c)) cap++;
-            else if(islower(c)) sm++;
+            if(std::isupper(c)) cap++;
+            else if(std::islower(c)) sm++;
         }
         int strength = cap - sm;
         if(strength > strongest){
