e08506dbd0d5f42ad1cea811b01a9002f2f80ea6
beep boop <githubbot@vadim.me>
2024-07-10 20:22:52 +0200

    Solution 25 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/mma41iux Prompt:                  from /usr/include/c++/11/bits/stl_algobase.h:64,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from c997a8ad-6f7c-48d9-ab32-63aabd9943ec.cpp:2: /usr/include/c++/11/type_traits: In instantiation of ‘struct std::is_destructibl e<RomanPair>’: /usr/include/c++/11/bits/stl_construct.h:188:51:   required from ‘void std::_Des troy(_ForwardIterator, _ForwardIterator) [with _ForwardIterator = RomanPair*]’ /usr/include/c++/11/bits/alloc_traits.h:848:15:   required from ‘void std::_Dest roy(_ForwardIterator, _ForwardIterator, std::allocator<_T2>&) [with _ForwardIter ator = RomanPair*; _Tp = RomanPair]’ /usr/include/c++/11/bits/stl_vector.h:680:15:   required from ‘std::vector<_Tp, _Alloc>::~vector() [with _Tp = RomanPair; _Alloc = std::allocator<RomanPair>]’ c997a8ad-6f7c-48d9-ab32-63aabd9943ec.cpp:17:71:   required from here /usr/include/c++/11/type_traits:885:52: error: static assertion failed: template  argument must be a complete class or an unbounded array   885 |       static_assert(std::__is_complete_or_unbounded(__type_identity<_Tp> {}),       |                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~ ~~~ /usr/include/c++/11/type_traits:885:52: note: ‘std::__is_complete_or_unbounded<s td::__type_identity<RomanPair> >((std::__type_identity<RomanPair>{}, std::__type _identity<RomanPair>()))’ evaluates to false

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -3,13 +3,19 @@
 #include <vector>
 #include <iostream>
 
-std::vector<std::pair<int, std::string>> romanMap = {{1000, "M"}, {900, "CM"}, {500, "D"},
-                                                  {400, "CD"}, {100, "C"}, {90, "XC"},
-                                                  {50, "L"}, {40, "XL"}, {10, "X"},
-                                                  {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+using namespace std;
 
-std::string int_to_mini_roman(int number) {
-    std::string roman = "";
+struct RomanPair {
+    int first;
+    string second;
+};
+
+string int_to_mini_roman(int number) {
+    vector<pair<int, string>> romanMap = {{1000, "M"}, {900, "CM"}, {500, "D"},
+                              {400, "CD"}, {100, "C"}, {90, "XC"},
+                              {50, "L"}, {40, "XL"}, {10, "X"},
+                              {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+    string roman = "";
     for (const auto& pair : romanMap) {
         while (number >= pair.first) {
             number -= pair.first;
@@ -21,8 +27,8 @@ std::string int_to_mini_roman(int number) {
 
 int main() {
     int num;
-    std::cout << "Enter a number: ";
-    std::cin >> num;
-    std::cout << "The Roman numeral for " << num << " is " << int_to_mini_roman(num) << std::endl;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "The Roman numeral for " << num << " is " << int_to_mini_roman(num) << endl;
     return 0;
 }
