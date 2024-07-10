12cbaad255786abb91912ca531f933d669042b3b
beep boop <githubbot@vadim.me>
2024-07-10 19:41:09 +0200

    Solution 15 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bpxrryax Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/34896319-0965-45f5-9434-6e54c91d1 0cc.py", line 7, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/34896319-0965-45f5-9434-6e54c91d1 0cc.py", line 4, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_41.py
+++ Python_41.py
@@ -1,6 +1,2 @@
-```
 def car_race_collision(n: int):
-    if n < 2:
-        return 0
-    expected_collisions = sum(i * (n - i) for i in range(1, n))
-    return expected_collisions
+    return (n * (n - 1)) // 2
