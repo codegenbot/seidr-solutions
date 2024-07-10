c87d8fdda7952235a77c1e2807414f730f991571
beep boop <githubbot@vadim.me>
2024-07-10 16:33:35 +0200

    Solution 13 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h4xv3ep2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/197e40ed-e850-40f5-b39e-704e4593f c83.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,2 +1,11 @@
 def solve(N):
-    return "".join(str(i) for i in range(1, N+1)) if N > 0 else ""
+    if N == 1:
+        return '0'
+    else:
+        res = ''
+        for i in range(1, N+1):
+            if i % 2 != 0:
+                res += '1' if (N-1) % i == 0 else '0'
+            else:
+                res += '0'
+        return res
