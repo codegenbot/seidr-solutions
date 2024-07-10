062347d8f6594cd8ce07700790f4d7fee826b7de
beep boop <githubbot@vadim.me>
2024-07-10 21:43:14 +0200

    Solution 29 of CPP/16, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/nexaumnt Prompt: d0d6d94f-cee3-456c-b275-0b7ce3c8f808.cpp:3:35: error: ‘str’ was not declared in this scope; did you mean ‘std’?     3 |     auto it = unique(str.begin(), str.end());       |                                   ^~~       |                                   std d0d6d94f-cee3-456c-b275-0b7ce3c8f808.cpp:3:15: error: ‘unique’ was not declared in this scope     3 |     auto it = unique(str.begin(), str.end());       |               ^~~~~~ d0d6d94f-cee3-456c-b275-0b7ce3c8f808.cpp:4:5: error: expected unqualified-id bef ore ‘return’     4 |     return distance(str.begin(), it);       |     ^~~~~~ d0d6d94f-cee3-456c-b275-0b7ce3c8f808.cpp:5:1: error: expected declaration before  ‘}’ token     5 | }       | ^ In file included from d0d6d94f-cee3-456c-b275-0b7ce3c8f808.cpp:7: d0d6d94f-cee3-456c-b275-0b7ce3c8f808.cpp: In function ‘int main()’: d0d6d94f-cee3-456c-b275-0b7ce3c8f808.cpp:9:13: error: ‘count_distinct_characters ’ was not declared in this scope     9 |     assert (count_distinct_characters("Jerry jERRY JeRRRY") == 5);       |             ^~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_16.cpp
+++ CPP_16.cpp
@@ -2,9 +2,9 @@
 #include <cassert>
 
 int count_distinct_characters(const std::string& str) {
-    std::string str_copy = str;
-    std::transform(str_copy.begin(), str_copy.end(), str_copy.begin(), ::tolower);
-    std::sort(str_copy.begin(), str_copy.end());
-    auto it = std::unique(str_copy.begin(), str_copy.end());
-    return std::distance(str_copy.begin(), it);
+    std::string temp = str;
+    std::transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+    std::sort(temp.begin(), temp.end());
+    auto it = std::unique(temp.begin(), temp.end());
+    return std::distance(temp.begin(), it);
 }
