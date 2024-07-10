06025bbf461bea20f33cb079ecafac8456c44a9e
beep boop <githubbot@vadim.me>
2024-07-10 05:00:03 +0200

    Solution 72 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vnld1i80 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7033112f-ebdb-4093-86e9-13aeafae8 934.py", line 22, in <module>     check(digitSum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7033112f-ebdb-4093-86e9-13aeafae8 934.py", line 19, in check     assert digitSum("You arE Very Smart") == 327, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_66.py
+++ Python_66.py
@@ -3,12 +3,8 @@ def digitSum(s):
     total = 0
     is_odd = False
     for c in s:
-        if not c.isdigit() or c.isspace():  
-            continue
-        d = int(c)
-        if is_odd:
-            total += d * 2 - 5 if d > 4 else d * 2
-        else:
-            total += d
-        is_odd = not is_odd
+        if c.isdigit():
+            d = int(c)
+            total += (d * 2) if is_odd else d
+            is_odd = not is_odd
     return total
