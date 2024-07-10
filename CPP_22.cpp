542da48e5372bdf503b14a3493c31cea54b19a1d
beep boop <githubbot@vadim.me>
2024-07-10 03:13:57 +0200

    Solution 99 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uydoiiwb Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 256db051-bd43-4ec8-a412-e122e96fb50c.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,14 +1,21 @@
-std::vector<int> separateZeroes(std::vector<int> values) {
-    std::vector<int> zeroes;
-    std::vector<int> non_zeroes;
+#include <vector>
+#include <cassert>
+#include <memory>
 
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+std::vector<int> filter_integer(std::vector<int> values) {
+    std::vector<int> result; 
     for (int value : values) {
-        if(value != 0) {
-            non_zeroes.push_back(value);
-        } else {
-            zeroes.push_back(value);
-        }
+        if(value != 0)
+            result.push_back(value);
     }
+    return result;
+}
 
-    return {zeroes, non_zeroes};
+int main() {
+    assert(issame(filter_integer({3, 3, 3}), {3, 3, 3}));
+    return 0;
 }
