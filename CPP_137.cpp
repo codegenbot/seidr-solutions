223683ebb8c7a147eeb0077c5b698dbe6fae336a
beep boop <githubbot@vadim.me>
2024-07-10 09:26:24 +0200

    Solution 14 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/wwo0cwud Prompt: 3f81356a-2ea2-41f2-bb04-9ab621771a7e.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 3f81356a-2ea2-41f2-bb04-9ab621771a7e.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 3f81356a-2ea2-41f2-bb04-9ab621771a7e.cpp:20: 3f81356a-2ea2-41f2-bb04-9ab621771a7e.cpp: In function ‘int main()’: 3f81356a-2ea2-41f2-bb04-9ab621771a7e.cpp:22:13: error: ‘boost’ has not been decl ared    22 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 3f81356a-2ea2-41f2-bb04-9ab621771a7e.cpp:22:35: error: expected primary-expressi on before ‘>’ token    22 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
-#include <boost/any.hpp>
+#include <any>
 #include <cassert>
 
 using namespace std;
-using namespace boost;
 
 string compare_one(const string& s, const any& a) {
     if (a.type() == typeid(int)) {
@@ -14,6 +13,6 @@ string compare_one(const string& s, const any& a) {
 }
 
 int main() {
-    assert(boost::any_cast<string>(compare_one("1", boost::any(1))) == "None");
+    assert(std::any_cast<string>(compare_one("1", std::any(1))) == "None");
     return 0;
 }
