54773d6825599c3419766764a8bdfcbc195762bb
beep boop <githubbot@vadim.me>
2024-07-10 20:09:53 +0200

    Solution 33 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/h0my8hka Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/98946bd5-755b-4b17-aac0-20bc78244 7ed.py", line 13, in <module>     check(digits)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/98946bd5-755b-4b17-aac0-20bc78244 7ed.py", line 10, in check     assert digits(2468) == 0            ^^^^^^^^^^^^^^^^^ AssertionError

--- Python_131.py
+++ Python_131.py
@@ -1,6 +1,7 @@
+```
 def digits(n):
     even_product = 1
     for digit in str(n):
-        if int(digit) % 2 == 0:
+        if int(digit) % 2 == 0:  
             even_product *= int(digit)
     return even_product
