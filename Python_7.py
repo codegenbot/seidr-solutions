11f49131b54b24a98cb12bcf954451cc3bc5b244
beep boop <githubbot@vadim.me>
2024-07-10 20:11:29 +0200

    Solution 14 of Python/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3uarqhgx Prompt: Program did not receive expected input

--- Python_7.py
+++ Python_7.py
@@ -5,4 +5,4 @@ def filter_by_substring(strings: List[str], substring: str) -> List[str]:
     if not substring:  
         return []  
     
-    return [s for s in strings if substring.casefold().strip() in s.casefold()]
+    return [s for s in strings if substring.casefold() in s.casefold()]
