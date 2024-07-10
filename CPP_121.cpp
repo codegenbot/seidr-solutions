d94a1ee5bf3b92fb7d15c2a625baa0bd4b03e506
beep boop <githubbot@vadim.me>
2024-07-10 22:00:09 +0200

    Solution 25 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pvc16fn9 Prompt: in this scope     5 | int solution(vector<int> lst) {       |              ^~~~~~ 03d83526-3016-48f0-9230-7c5f1f0bbd7e.cpp:5:14: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 03d83526-3016-48f0-9230-7c5f1f0bbd7e.cpp:3: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 03d83526-3016-48f0-9230-7c5f1f0bbd7e.cpp:3: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 03d83526-3016-48f0-9230-7c5f1f0bbd7e.cpp:5:21: error: expected primary-expressio n before ‘int’     5 | int solution(vector<int> lst) {       |                     ^~~ In file included from 03d83526-3016-48f0-9230-7c5f1f0bbd7e.cpp:15: 03d83526-3016-48f0-9230-7c5f1f0bbd7e.cpp: In function ‘int main()’: 03d83526-3016-48f0-9230-7c5f1f0bbd7e.cpp:17:13: error: ‘solutions’ was not decla red in this scope; did you mean ‘solution’?    17 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,19 +1,29 @@
-```cpp
+```
+#include <initializer_list>
 #include <vector>
 
-int solution(std::vector<int> lst) {
-    int sum = 0;
-    for (int i = 1; i < lst.size(); i++) {
-        if (lst[i] % 2 != 0) {
-            sum += lst[i];
-        }
-    }
-    return sum;
-}
+int solution(vector<int> lst);
 
 int main() {
-    std::vector<int> lst = {5, 4, 3, 6, 7};
-    int result = solution(lst);
-    std::cout << "The sum of the odd numbers is: " << result << std::endl;
+    int sum;
+    vector<int> lst;
+    cout << "Enter numbers separated by spaces: ";
+    for (int i = 0; i < 5; i++) {
+        int num;
+        cin >> num;
+        lst.push_back(num);
+    }
+    sum = solution(lst);
+    cout << "Sum of odd numbers is: " << sum << endl;
     return 0;
+}
+
+int solution(vector<int> lst) {
+    int total_sum = 0;
+    for (int i = 1; i < lst.size(); i++) {
+        if (lst[i] % 2 != 0) {
+            total_sum += lst[i];
+        }
+    }
+    return total_sum;
 }
