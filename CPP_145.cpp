f1012e7a1a0147049e4e070a7b168785db0f222f
beep boop <githubbot@vadim.me>
2024-07-10 21:50:51 +0200

    Solution 58 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/jv7e3pcr Prompt:    54 |  assert (isEqual(vector<int>{-76, -21, 0, 4, 23, 6, 6}, main({0,6,6,-76, -21,23,4})));       |                                                         ~~~~^~~~~~~~~~~~ ~~~~~~~~~~       |                                                             |       |                                                             int 4a348748-9cd6-40ab-bbae-a28358f5936a.cpp: At global scope: 4a348748-9cd6-40ab-bbae-a28358f5936a.cpp:67:5: error: conflicting declaration of  C function ‘int main()’    67 | int main(){       |     ^~~~ 4a348748-9cd6-40ab-bbae-a28358f5936a.cpp:10:13: note: previous declaration ‘int main(std::vector<int>)’    10 | vector<int> main(vector<int> nums) {       |             ^~~~ In file included from 4a348748-9cd6-40ab-bbae-a28358f5936a.cpp:58: 4a348748-9cd6-40ab-bbae-a28358f5936a.cpp: In function ‘int main()’: 4a348748-9cd6-40ab-bbae-a28358f5936a.cpp:68:20: error: ‘order_by_points’ was not  declared in this scope    68 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -7,7 +7,7 @@ bool isEqual(vector<int> a, vector<int> b) {
     return (a.size() == b.size()) && equal(a.begin(), a.end(), b.begin());
 }
 
-vector<int> sortAndReturn(vector<int> nums) {
+vector<int> order_by_points(vector<int> nums) {
     vector<pair<int, int>> pairs;
     for (int i = 0; i < nums.size(); i++) {
         int sumOfDigits = 0;
@@ -41,7 +41,7 @@ int main() {
     }
     cout << "]\n";
     
-    vector<int> result = sortAndReturn(nums);
+    vector<int> result = order_by_points(nums);
     cout << "[";
     for(int i=0; i<result.size();i++) {
         cout << result[i];
@@ -51,6 +51,6 @@ int main() {
     }
     cout << "]\n";
     
-    assert (isEqual(vector<int>{-76, -21, 0, 4, 23, 6, 6}, sortAndReturn({0,6,6,-76,-21,23,4})));
+    assert (isEqual(vector<int>{-76, -21, 0, 4, 23, 6, 6}, order_by_points({0,6,6,-76,-21,23,4})));
     return 0;
 }
