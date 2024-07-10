383cbe925edd5ddc500e7965bbacc80f4f7d580b
beep boop <githubbot@vadim.me>
2024-07-10 18:53:48 +0200

    Solution 6 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uquygx2d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dce7e659-451a-4ecb-b9f1-7aa0c6d6f 30a.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dce7e659-451a-4ecb-b9f1-7aa0c6d6f 30a.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,8 @@
-```
 def sort_even(l: list):
-    evens = sorted([x for x in l if isinstance(x, int) and x % 2 == 0])
-    odds = [x for x in l if isinstance(x, int) and x % 2 != 0]
-    return evens + odds
+    if len([x for x in l if x % 2 == 0]) < 1:
+        return "List should contain at least one even element"
+    else:
+        return [
+            x if i % 2 == 0 else y
+            for i, (x, y) in enumerate(zip(sorted([y for y in l[1::2]]), l[::2]))
+        ]
