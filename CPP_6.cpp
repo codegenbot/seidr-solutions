10671fb3e8bcc0905a116a9f7e6741efcad8aa88
beep boop <githubbot@vadim.me>
2024-07-10 12:37:59 +0200

    Solution 62 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/s88sugx2 Prompt:       |                    ^~~ 56f133b9-20b9-4347-94ed-3c085bc0e934.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 56f133b9-20b9-4347-94ed-3c085bc0e934.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 56f133b9-20b9-4347-94ed-3c085bc0e934.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 56f133b9-20b9-4347-94ed-3c085bc0e934.cpp:21: 56f133b9-20b9-4347-94ed-3c085bc0e934.cpp: In function ‘int main()’: 56f133b9-20b9-4347-94ed-3c085bc0e934.cpp:31:20: error: ‘parse_nested_parens’ was  not declared in this scope    31 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ 56f133b9-20b9-4347-94ed-3c085bc0e934.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,26 +1,14 @@
 #include <vector>
 #include <cassert>
 
-bool issame(vector<int> v1, vector<int> v2){
-    return v1 == v2;
-}
-
-vector<int> parse_nested_parens(string paren_string){
-    vector<int> depths;
-    int max_depth = 0;
-    int current_depth = 0;
-    for(char c : paren_string){
-        if(c == '('){
-            current_depth++;
-            max_depth = max(max_depth, current_depth);
-        } else if(c == ')'){
-            current_depth--;
-        } else if(c == ' '){
-            depths.push_back(max_depth);
-            max_depth = 0;
-            current_depth = 0;
+bool issame(vector<int> vec1, vector<int> vec2){
+    if(vec1.size() != vec2.size()){
+        return false;
+    }
+    for(int i = 0; i < vec1.size(); i++){
+        if(vec1[i] != vec2[i]){
+            return false;
         }
     }
-    depths.push_back(max_depth);
-    return depths;
+    return true;
 }
