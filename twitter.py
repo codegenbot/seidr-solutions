ab8716742d3f584d17c309e50cb58a75920ad4d0
beep boop <githubbot@vadim.me>
2024-07-10 23:39:01 +0200

    Solution 32 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/w4xxcvcb Prompt: it must return Too many characters for input Fz}eR74HX}&LNkBX}Xg4$zC)k3:'L4q29}oOMy:\'QfQ(]Z7??lW''hBXcfKWV[`mc<~jnND!`1[dMDU*50reS6I=;k<"O6guYddAMOOA~NPr!4^{#CAU)[gg!OlueaM)THQU(/B=&tx@pNZ:`Cjb_oNkrhwTUyX2R1}"^j<I8l0, but it returns .

--- twitter.py
+++ twitter.py
@@ -1,7 +1,11 @@
+```
 def validate_tweet(tweet):
-    if len(tweet.encode('utf8').decode('utf8')) > 140:
+    ascii_len = sum(1 for c in tweet if ord(c) < 128)
+    if len(tweet) > 140:
         return "Too many characters"
     elif tweet == "":
         return "You didn't type anything"
+    elif ascii_len + (len(tweet) - ascii_len) > 140:
+        return "Too many characters"
     else:
         return f"Your tweet has {len(tweet)} characters"
