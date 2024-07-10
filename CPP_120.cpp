6853fdfe3baa469d617790d53d67d5a2ae5d09c0
beep boop <githubbot@vadim.me>
2024-07-10 08:08:24 +0200

    Solution 76 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/u6x0d6pd Prompt:       |                    ^~~ 8d04b9f7-68cb-432f-a607-a88a66593d36.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8d04b9f7-68cb-432f-a607-a88a66593d36.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8d04b9f7-68cb-432f-a607-a88a66593d36.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8d04b9f7-68cb-432f-a607-a88a66593d36.cpp:11: 8d04b9f7-68cb-432f-a607-a88a66593d36.cpp: In function ‘int main()’: 8d04b9f7-68cb-432f-a607-a88a66593d36.cpp:21:20: error: ‘maximum’ was not declare d in this scope    21 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 8d04b9f7-68cb-432f-a607-a88a66593d36.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -2,12 +2,16 @@
 #include <vector>
 #include <algorithm>
 
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
 std::vector<int> maximum(std::vector<int> arr,int k){
-    std::vector<int> result;
+    vector<int> result;
     for(int i = 0; i < k; i++){
-        int max_val = *std::max_element(arr.begin(), arr.end());
+        int max_val = *max_element(arr.begin(), arr.end());
         result.push_back(max_val);
-        arr.erase(std::remove(arr.begin(), arr.end(), max_val), arr.end());
+        arr.erase(remove(arr.begin(), arr.end(), max_val), arr.end());
     }
     return result;
 }
