92bc58663190f00d545e53b3790dd72b9aa81ab1
beep boop <githubbot@vadim.me>
2024-07-10 02:43:45 +0200

    Solution 26 of CPP/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/okymjy78 Prompt: e16e7fd5-165a-4aa0-a2c3-240553a35d11.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ e16e7fd5-165a-4aa0-a2c3-240553a35d11.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~

--- CPP_66.cpp
+++ CPP_66.cpp
@@ -30,8 +30,7 @@ int main() {
     std::cout << "Please enter a string: ";
     std::string s;
     std::cin >> s;
-    //assert(digitSum(s) == 327);
     int result = digitSum(s);
-    std::cout << "The sum is: " << result << std::endl;
+    std::cout << "The digit sum is: " << result << std::endl;
     return 0;
 }
