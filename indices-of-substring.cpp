a024fba3d42030b4ac91eacc35dd4a26691b6f9b
beep boop <githubbot@vadim.me>
2024-07-11 01:26:35 +0200

    Solution 83 of indices-of-substring, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/e16hi13c Prompt: Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -3,26 +3,46 @@ using namespace std;
 
 vector<int> indicesOfSubstring(string text, string target) {
     vector<int> result;
-    size_t pos = 0;
-    
-    while ((pos = text.find(target, pos)) != string::npos) {
-        result.push_back(pos);
-        pos += target.size();
+    int n = text.length();
+    int m = target.length();
+
+    for(int i = 0; i <= n - m; i++) {
+        if(text.substr(i, m) == target) {
+            result.push_back(i);
+        }
     }
-    
+
     return result;
 }
 
 int main() {
-    string text;
-    cin >> text;
-    string target;
-    cin >> target;
+    // test cases
+    string text1 = "a";
+    vector<int> output1 = indicesOfSubstring(text1, "");
+    cout << output1[0] << endl;
 
-    vector<int> indices = indicesOfSubstring(text, target);
+    string text2 = "!!";
+    vector<int> output2 = indicesOfSubstring(text2, "!");
+    for(int i : output2) {
+        cout << i << " ";
+    }
+    cout << endl;
 
-    for (int index : indices) {
-        cout << index << " ";
+    string text3 = "rnm,xcnwqnd@#$fwkdjn3";
+    vector<int> output3 = indicesOfSubstring(text3, "r");
+    cout << output3[0] << endl;
+
+    string text4 = "hihihihihihihihihihi";
+    vector<int> output4 = indicesOfSubstring(text4, "hi");
+    for(int i : output4) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    string text5 = "############";
+    vector<int> output5 = indicesOfSubstring(text5, "#");
+    for(int i : output5) {
+        cout << i << " ";
     }
     cout << endl;
 
