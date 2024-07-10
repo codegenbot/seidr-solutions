3899e4df9179d1e5aebe5155a830d56f51b8d4b7
beep boop <githubbot@vadim.me>
2024-07-10 17:37:17 +0200

    Solution 68 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/n9sevabm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5c8b2a0a-e718-485c-bcd9-c7fe19af9 ea5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,5 +1,5 @@
-def solution():
-    lst = []
+```
+def solution(lst=[]):
     while True:
         user_input = input("Enter a number (or 'stop' if finished): ")
         if user_input.lower() == 'stop':
@@ -10,10 +10,7 @@ def solution():
         except ValueError:
             print("Invalid input. Please enter a valid number or type 'stop' to finish.")
     if len(lst) > 0:
-        odd_sum = 0
-        for num in lst:
-            if num % 2 != 0:
-                odd_sum += num
+        odd_sum = sum(i for i in lst if i % 2 != 0)
         return odd_sum
     else:
         return None
