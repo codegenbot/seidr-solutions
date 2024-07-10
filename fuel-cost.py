9e997047702140df7f3febdcbd6217682b0d775a
beep boop <githubbot@vadim.me>
2024-07-10 23:00:53 +0200

    Solution 56 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/fbl3bnmj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/feaa3ba9-a0c8-421b-8b80-1520f0bcd ea3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- fuel-cost.py
+++ fuel-cost.py
@@ -1,3 +1,5 @@
+```
 def fuel_cost():
+    """Solve the fuel-cost problem"""
     arr = list(map(int, input().split()))
-    return sum(((i-2)//3) - 2 for i in arr)
+    return sum((i-2)//3 for i in arr)
