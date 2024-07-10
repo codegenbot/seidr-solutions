e72340b0cad942e1f56d2245cfeb9b37e7d8a38c
beep boop <githubbot@vadim.me>
2024-07-10 06:35:08 +0200

    Solution 56 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lerjqzl3 Prompt:    23 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ a7f4c7b6-e077-405a-aaf7-3b7df12b17ef.cpp:23:36: error: expected primary-expressi on before ‘float’    23 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ a7f4c7b6-e077-405a-aaf7-3b7df12b17ef.cpp:23:43: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from a7f4c7b6-e077-405a-aaf7-3b7df12b17ef.cpp:22: a7f4c7b6-e077-405a-aaf7-3b7df12b17ef.cpp: In function ‘int main()’: a7f4c7b6-e077-405a-aaf7-3b7df12b17ef.cpp:32:20: error: ‘sort_even’ was not decla red in this scope    32 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ a7f4c7b6-e077-405a-aaf7-3b7df12b17ef.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,18 +1,3 @@
-#include <vector>
-#include <algorithm>
-
-bool issame(vector<float> a, vector<float> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 vector<float> sort_even(vector<float> l) {
     vector<float> result(l.size());
     for (int i = 0; i < l.size(); i++) {
@@ -32,11 +17,4 @@ vector<float> sort_even(vector<float> l) {
         }
     }
     return result;
-}
-
-int main() {
-    vector<float> input = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
-    vector<float> expected = {-12, 2, 3, 4, 5, 8, 11, 12, 23, -10};
-    assert(issame(sort_even(input), expected));
-    return 0;
 }
