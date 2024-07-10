729e3ecd5724f9245ef40d7a63e8f363d76691e1
beep boop <githubbot@vadim.me>
2024-07-10 07:42:18 +0200

    Solution 28 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/i4ztvcn8 Prompt:  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d52e3f38-4657-4a3e-b20b-9cec61c2f010.cpp:18:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from d52e3f38-4657-4a3e-b20b-9cec61c2f010.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from d52e3f38-4657-4a3e-b20b-9cec61c2f010.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d52e3f38-4657-4a3e-b20b-9cec61c2f010.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from d52e3f38-4657-4a3e-b20b-9cec61c2f010.cpp:17: d52e3f38-4657-4a3e-b20b-9cec61c2f010.cpp: In function ‘int main()’: d52e3f38-4657-4a3e-b20b-9cec61c2f010.cpp:27:20: error: ‘even_odd_palindrome’ was  not declared in this scope    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,17 +1,31 @@
 #include <vector>
 
-namespace contest {
-    bool issame(std::vector<int> a, std::vector<int> b) {
-        if (a.size() != b.size()) {
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
             return false;
         }
-        
-        for (size_t i = 0; i < a.size(); ++i) {
-            if (a[i] != b[i]) {
-                return false;
-            }
-        }
-        
-        return true;
     }
+    
+    return true;
+}
+
+bool even_odd_palindrome(std::vector<int> arr) {
+    std::vector<int> even, odd;
+    
+    for (int num : arr) {
+        if (num % 2 == 0) {
+            even.push_back(num);
+        } else {
+            odd.push_back(num);
+        }
+    }
+    
+    std::vector<int> odd_reversed(odd.rbegin(), odd.rend());
+    
+    return issame(even, odd_reversed);
 }
