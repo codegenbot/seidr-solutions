48973ffb034ecc333ceddf11f3a3f104a81a740d
beep boop <githubbot@vadim.me>
2024-07-10 23:57:55 +0200

    Solution 97 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/581ffd51-5df0-4cae-9e43-7066304b1 e5e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,4 @@
 def sort_third(lst):
     numbers = sorted([x for x in lst if isinstance(x, (int, float))])
-    others = [item for sublist in [i for i in lst if isinstance(i, list)] for item in sublist]
-    return sorted(others, key=len) + numbers
+    others = sorted([item for sublist in [i for i in lst if isinstance(i, list)] for item in sublist], key=len)
+    return others + numbers
