760cc2326f04ca6703cf0e6d1c9907e8664b514d
beep boop <githubbot@vadim.me>
2024-07-10 22:02:15 +0200

    Solution 45 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tbcv99fp Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5240a279-6b20-4725-9a0a-f79e19cec b4e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,4 +1,4 @@
-```""" 
+```
 def parse_music(music_string: str) -> list:
     music_list = []
     i = 0
@@ -9,21 +9,25 @@ def parse_music(music_string: str) -> list:
         elif i < len(music_string) - 1 and music_string[i:i+2] == 'o|':
             music_list.append(2)
             i += 2
-        elif i < len(music_string) - 2:
-            if music_string[i:i+3] == '.||' or music_string[i:i+2] in ['||', '|.']:
-                if music_string[i:i+3] == '.||':
+        elif i < len(music_string) - 3:
+            if music_string[i:i+3] in ['...||', '.||.', '..|.']:
+                music_list.append(1)
+                i += 3
+            elif (music_string[i:i+2] in ['||', '|.'] and music_string[i+2] == '.') or (music_string[i:i+2] in ['..|', '.|.'] and music_string[i+2] == 'o'):
+                if music_string[i:i+2] in ['||', '|.']:
+                    music_list.append(0.5)
+                    i += 2
+                else:
                     music_list.append(1)
+                    i += 2
+            elif (music_string[i:i+2] in ['..|', '.|.'] and music_string[i+2] == 'o') or (music_string[i:i+3] in ['...||']):
+                if music_string[i:i+3] == '..||':
+                    music_list.append(0.5)
                     i += 3
                 else:
-                    if music_string[i:i+2] in ['||', '|.']:
-                        if music_string[i:i+2] == '||':
-                            music_list.append(0.5)
-                            i += 2
-                        else:
-                            music_list.append(1)
-                            i += 2
+                    music_list.append(1)
+                    i += 3
             else:
                 music_list.append(1)
                 i += 1
     return music_list
-""``
