d6c3f37c359ab08291064a2c53f80f29e4fdf7e8
beep boop <githubbot@vadim.me>
2024-07-10 21:52:50 +0200

    Solution 16 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/hxerdf1c Prompt:    29 |     string input;       |     ^~~~~~ f48c20f0-15d7-41f9-9728-1d358f4fd535.cpp:30:5: error: ‘cin’ was not declared in this scope    30 |     cin >> input;       |     ^~~ f48c20f0-15d7-41f9-9728-1d358f4fd535.cpp:30:12: error: ‘input’ was not declared in this scope; did you mean ‘int’?    30 |     cin >> input;       |            ^~~~~       |            int f48c20f0-15d7-41f9-9728-1d358f4fd535.cpp:31:5: error: ‘cout’ was not declared in  this scope    31 |     cout << calculateBowlingScore(input) << endl;       |     ^~~~ f48c20f0-15d7-41f9-9728-1d358f4fd535.cpp:31:34: error: ‘calculateBowlingScore’ c annot be used as a function    31 |     cout << calculateBowlingScore(input) << endl;       |             ~~~~~~~~~~~~~~~~~~~~~^~~~~~~ f48c20f0-15d7-41f9-9728-1d358f4fd535.cpp:31:45: error: ‘endl’ was not declared i n this scope    31 |     cout << calculateBowlingScore(input) << endl;       |                                             ^~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,52 +1,36 @@
-int get_score(string s) {
+#include <iostream>
+using namespace std;
+
+int calculateBowlingScore(string input) {
     int score = 0;
-    int frame = 1;
-    int ball = 0;
-    vector<int> rolls;
-
-    for (char c : s) {
-        if (c == 'X') {
-            rolls.push_back(10);
+    int frame = 0;
+    for (int i = 0; i < input.size(); i++) {
+        if (input[i] == 'X') {
+            score += 10;
+            if (frame < 9) {
+                score += (input[i + 1] == 'X') ? 10 : (isdigit(input[i + 1]) ? input[i + 1] - '0' : 10);
+                score += (input[i + 2] == 'X') ? 10 : (isdigit(input[i + 2]) ? input[i + 2] - '0' : 10);
+            }
             frame++;
-            if (frame > 10)
-                break;
-        } else if (c == '/') {
-            rolls.push_back(10 - rolls.back());
-        } else if (c == '-') {
-            rolls.push_back(0);
-        } else {
-            rolls.push_back(c - '0');
-        }
-    }
-
-    for (int i = 0; i < rolls.size(); i++) {
-        if (frame > 10)
-            break;
-
-        if (rolls[i] == 10) {
-            score += 10 + rolls[i+1] + rolls[i+2];
-            ball++;
+        } else if (input[i] == '/') {
+            score += 10 - (input[i - 1] - '0');
+            score += (input[i + 1] == 'X') ? 10 : (isdigit(input[i + 1]) ? input[i + 1] - '0' : 10);
             frame++;
-        } else if (rolls[i] + rolls[i+1] == 10) {
-            score += 10 + rolls[i+2];
-            i++;
-            ball += 2;
-            frame++;
-        } else {
-            score += rolls[i] + rolls[i+1];
-            i++;
-            ball += 2;
+        } else if (isdigit(input[i])) {
+            score += input[i] - '0';
+            if (isdigit(input[i + 1])) {
+                score += input[i + 1] - '0';
+                i++;
+            }
             frame++;
         }
     }
-
     return score;
 }
 
 int main() {
-    string s;
-    cin >> s;
-    cout << get_score(s) << endl;
-    
+    string input;
+    std::cin >> input;
+    std::cout << calculateBowlingScore(input) << std::endl;
     return 0;
 }
