92909a48d4f28141dd190acdc888e33be4444fa9
beep boop <githubbot@vadim.me>
2024-07-10 22:49:41 +0200

    Solution 44 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wj4ug1w3 Prompt: Program failed

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -26,7 +26,7 @@ int count_nums(std::vector<int> nums) {
                 int digit = abs(num % 10);
                 if (digit == 5 || digit == 8 || digit == 9) {
                     sum_of_digits += digit - 5;
-                } else if (digit > 4 && !has_negative_digit) {
+                } else if (digit <= 5 && !has_negative_digit) {
                     has_negative_digit = true;
                 } else if (digit > 0) {
                     sum_of_digits += digit;
