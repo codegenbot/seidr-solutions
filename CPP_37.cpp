b753c58b1668e5932071da8cf4c6d883c5ce31fd
beep boop <githubbot@vadim.me>
2024-07-10 05:10:34 +0200

    Solution 82 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/7y3sq4wl Prompt:     6 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 34577bf9-6d5d-4873-a458-41dfdfb755c8.cpp:6:36: error: expected primary-expressio n before ‘float’     6 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 34577bf9-6d5d-4873-a458-41dfdfb755c8.cpp:6:43: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 34577bf9-6d5d-4873-a458-41dfdfb755c8.cpp:5: 34577bf9-6d5d-4873-a458-41dfdfb755c8.cpp: In function ‘int main()’: 34577bf9-6d5d-4873-a458-41dfdfb755c8.cpp:15:20: error: ‘sort_even’ was not decla red in this scope    15 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 34577bf9-6d5d-4873-a458-41dfdfb755c8.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,19 +1,6 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(const std::vector<float>& a, const std::vector<float>& b);
-
-void sort_even(std::vector<float>& vec) {
-    std::sort(vec.begin(), vec.end(), [](float a, float b) {
-        return ((int)a % 2 == 0) && ((int)b % 2 == 0) && a < b;
-    });
-}
-
 bool issame(const std::vector<float>& a, const std::vector<float>& b) {
-    return a == b;
-}
-
-int main() {
-    // Main function implementation
-    return 0;
+    return std::equal(a.begin(), a.end(), b.begin(), b.end());
 }
