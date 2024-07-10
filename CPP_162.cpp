a04c7891382330e422e72c6f24fc91c9cbb7008a
beep boop <githubbot@vadim.me>
2024-07-10 06:15:54 +0200

    Solution 37 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/svifujg2 Prompt: nal(unsigned char*, MD5_CTX*)’    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                                     ~~~~~~~~~~~~~~~^~ 5643940e-367b-4ed3-aaef-87dfee2c5049.cpp:15:56: error: ‘MD5_CTX’ {aka ‘struct MD 5state_st’} has no member named ‘md5_data’    15 |         sprintf(temp, "%02x", ((unsigned char*)(&mdctx.md5_data))[i]);       |                                                        ^~~~~~~~ 5643940e-367b-4ed3-aaef-87dfee2c5049.cpp: At global scope: 5643940e-367b-4ed3-aaef-87dfee2c5049.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 5643940e-367b-4ed3-aaef-87dfee2c5049.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from 5643940e-367b-4ed3-aaef-87dfee2c5049.cpp:31: 5643940e-367b-4ed3-aaef-87dfee2c5049.cpp: In function ‘int main()’: 5643940e-367b-4ed3-aaef-87dfee2c5049.cpp:33:13: error: ‘string_to_md5’ was not d eclared in this scope; did you mean ‘myString_to_md5’?    33 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -2,28 +2,18 @@
 #include <string>
 #include <openssl/md5.h>
 
-std::string myString_to_md5(const std::string& text) {
+std::string myString_to_md5(const char *text) {
     unsigned char result[16];
     MD5_CTX mdctx;
     MD5_Init(&mdctx);
-    MD5_Update(&mdctx, (const unsigned char*)text.c_str(), text.length());
-    unsigned char mdValue[MD5_DIGEST_LENGTH];
-    MD5_Final(&mdctx, mdValue, nullptr);
+    MD5_Update(&mdctx, (const unsigned char*)text, strlen(text));
+    int mdSize = MD5_DIGEST_LENGTH;
+    MD5_Final(NULL, &mdctx, &mdSize);
     std::string output;
-    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+    for (int i = 0; i < mdSize; i++) {
         char temp[3];
-        sprintf(temp, "%02x", mdValue[i]);
+        sprintf(temp, "%02x", ((unsigned char*)c->message_digested_bytes))[i]);
         output += temp;
     }
     return output;
-}
-
-int main() {
-    std::cout << "Enter a string: ";
-    std::string input;
-    std::getline(std::cin, input);
-    const char *input_str = input.c_str();
-    std::string output = myString_to_md5(input);
-    std::cout << "MD5: " << output << std::endl;
-    return 0;
 }
