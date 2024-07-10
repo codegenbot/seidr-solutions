5d8e2d1ecbe1e2c58d14233fa7d7ceed6b1b1f9b
beep boop <githubbot@vadim.me>
2024-07-10 19:24:46 +0200

    Solution 28 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uquygx2d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/22e1315e-be75-4d77-9f88-910fdc369 714.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/22e1315e-be75-4d77-9f88-910fdc369 714.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,9 +1,5 @@
+```
 def sort_even(l: list):
     even = sorted([x for x in l if x % 2 == 0])
     odd = sorted([x for x in l if x % 2 != 0])
-    
-    max_len = max(len(even), len(odd))
-    even += [0] * (max_len - len(even)) 
-    odd += [0] * (max_len - len(odd)) 
-
-    return tuple(i for i in zip(even, odd) or ([], []))
+    return tuple(i for i in zip(even, odd)) + (even[-len(odd):] if len(l) % 2 else [])
