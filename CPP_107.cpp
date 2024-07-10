772b313b8088b23fd41803685a56ea9a5881afdf
beep boop <githubbot@vadim.me>
2024-07-10 07:53:19 +0200

    Solution 39 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/i4ztvcn8 Prompt: 4a4fee87-8946-453a-92b2-03101f9f9f30.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ 4a4fee87-8946-453a-92b2-03101f9f9f30.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main() {       |     ^~~~ In file included from 4a4fee87-8946-453a-92b2-03101f9f9f30.cpp:41: 4a4fee87-8946-453a-92b2-03101f9f9f30.cpp: In function ‘int main()’: 4a4fee87-8946-453a-92b2-03101f9f9f30.cpp:51:20: error: ‘even_odd_palindrome’ was  not declared in this scope; did you mean ‘contest::even_odd_palindrome’?    51 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 4a4fee87-8946-453a-92b2-03101f9f9f30.cpp:19:22: note: ‘contest::even_odd_palindr ome’ declared here    19 |     std::vector<int> even_odd_palindrome(int n) {       |                      ^~~~~~~~~~~~~~~~~~~ In file included from 4a4fee87-8946-453a-92b2-03101f9f9f30.cpp:41: 4a4fee87-8946-453a-92b2-03101f9f9f30.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -31,9 +31,9 @@ namespace contest {
     }
 }
 
-int main() {
-    assert(contest::issame(contest::even_odd_palindrome(1), {0, 1}));
-    assert(contest::issame(contest::even_odd_palindrome(3), {3, 2, 1, 1, 2, 3}));
+int main_contest() {
+    assert(contest::issame(contest::even_odd_palindrome(1), {1, 1, 1}));
+    assert(contest::issame(contest::even_odd_palindrome(3), {3, 2, 1, 1, 2, 3, 3}));
     
     return 0;
 }
