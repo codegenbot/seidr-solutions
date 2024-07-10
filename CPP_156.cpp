d1c5d3adf24a89863e403ee683f559fe4b2b60a1
beep boop <githubbot@vadim.me>
2024-07-10 20:44:08 +0200

    Solution 38 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/mma41iux Prompt: 1cbe7c47-24dc-4231-a72a-06a5f858cded.cpp:27:24: error: ‘mini_roman’ was not decl ared in this scope    27 |         roman += "D" + mini_roman(num - 50);       |                        ^~~~~~~~~~ 1cbe7c47-24dc-4231-a72a-06a5f858cded.cpp:37:25: error: ‘mini_roman’ was not decl ared in this scope    37 |         roman += "IV" + mini_roman(num - 4);       |                         ^~~~~~~~~~ 1cbe7c47-24dc-4231-a72a-06a5f858cded.cpp: At global scope: 1cbe7c47-24dc-4231-a72a-06a5f858cded.cpp:55:5: error: redefinition of ‘int main( )’    55 | int main(){       |     ^~~~ 1cbe7c47-24dc-4231-a72a-06a5f858cded.cpp:46:5: note: ‘int main()’ previously def ined here    46 | int main() {       |     ^~~~ In file included from 1cbe7c47-24dc-4231-a72a-06a5f858cded.cpp:54: 1cbe7c47-24dc-4231-a72a-06a5f858cded.cpp: In function ‘int main()’: 1cbe7c47-24dc-4231-a72a-06a5f858cded.cpp:56:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    56 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,7 +1,8 @@
+```cpp
 #include <string>
 #include <iostream>
 
-std::string int_to_roman(int num) {
+std::string int_to_mini_roman(int num) {
     if (num > 3999) {
         return "Out of range";
     }
@@ -13,7 +14,7 @@ std::string int_to_roman(int num) {
     num %= 1000;
     int hundreds = num / 100;
     if (hundreds > 3) {
-        roman += "CM" + miniRoman(num - 300);
+        roman += "CM" + mini_roman(num - 300);
     } else {
         for (int i = 0; i < hundreds; i++) {
             roman += "C";
@@ -24,7 +25,7 @@ std::string int_to_roman(int num) {
     if (tens == 9) {
         roman += "IX";
     } else if (tens > 5) {
-        roman += "D" + miniRoman(num - 50);
+        roman += "D" + mini_roman(num - 50);
     } else {
         for (int i = 0; i < tens; i++) {
             roman += "X";
@@ -34,7 +35,7 @@ std::string int_to_roman(int num) {
     if (num == 9) {
         roman += "IX";
     } else if (num > 3) {
-        roman += "IV" + miniRoman(num - 4);
+        roman += "IV" + mini_roman(num - 4);
     } else {
         for (int i = 0; i < num; i++) {
             roman += "I";
@@ -43,23 +44,66 @@ std::string int_to_roman(int num) {
     return roman;
 }
 
-std::string miniRoman(int num) {
-    if (num == 9)
-        return "IX";
-    if (num >= 4) {
-        if (num % 4 > 0)
-            return "IV" + std::string(num - 4, 'I');
-        else
-            return "IV";
-    } else {
-        return std::string(num, 'I');
+std::string mini_roman(int num) {
+    if (num < 1) return "";
+    std::string roman;
+    if (num >= 1000) {
+        roman += "M";
+        num -= 1000;
+    } else if (num >= 900) {
+        roman += "CM";
+        num -= 900;
+    } else if (num >= 500) {
+        roman += "D";
+        num -= 500;
+    } else if (num >= 400) {
+        roman += "CD";
+        num -= 400;
     }
+
+    if (num >= 100) {
+        for (int i = 0; i < num / 100; i++) {
+            roman += "C";
+        }
+        num %= 100;
+    } else if (num >= 90) {
+        roman += "XC";
+        num -= 90;
+    } else if (num >= 50) {
+        for (int i = 0; i < num / 50; i++) {
+            roman += "L";
+        }
+        num %= 50;
+    } else if (num >= 40) {
+        roman += "XL";
+        num -= 40;
+    }
+
+    if (num >= 10) {
+        for (int i = 0; i < num / 10; i++) {
+            roman += "X";
+        }
+        num %= 10;
+    } else if (num == 9) {
+        roman += "IX";
+    } else if (num >= 5) {
+        roman += "V";
+        num -= 5;
+    } else if (num >= 4) {
+        roman += "IV";
+        num -= 4;
+    }
+
+    for (int i = 0; i < num; i++) {
+        roman += "I";
+    }
+    return roman;
 }
 
 int main() {
     int num;
     std::cout << "Enter a number: ";
     std::cin >> num;
-    std::cout << "The Roman numeral for " << num << " is " << int_to_roman(num) << std::endl;
+    std::cout << "The Roman numeral for " << num << " is " << int_to_mini_roman(num) << std::endl;
     return 0;
 }
