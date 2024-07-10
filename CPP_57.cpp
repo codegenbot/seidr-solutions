653953c05d9bf7ae39cf64a06eef711157afbcf9
beep boop <githubbot@vadim.me>
2024-07-10 23:44:08 +0200

    Solution 4 of CPP/57, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/rh4r1yvg Prompt:     2 |     for (int i = 1; i < l.size(); ++i) {       |     ^~~ 50ab761f-56a2-421f-a20b-2d3a5d7da3f6.cpp:2:21: error: ‘i’ does not name a type     2 |     for (int i = 1; i < l.size(); ++i) {       |                     ^ 50ab761f-56a2-421f-a20b-2d3a5d7da3f6.cpp:2:35: error: expected unqualified-id be fore ‘++’ token     2 |     for (int i = 1; i < l.size(); ++i) {       |                                   ^~ 50ab761f-56a2-421f-a20b-2d3a5d7da3f6.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return increasing || decreasing;       |     ^~~~~~ 50ab761f-56a2-421f-a20b-2d3a5d7da3f6.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from 50ab761f-56a2-421f-a20b-2d3a5d7da3f6.cpp:9: 50ab761f-56a2-421f-a20b-2d3a5d7da3f6.cpp: In function ‘int main()’: 50ab761f-56a2-421f-a20b-2d3a5d7da3f6.cpp:11:13: error: ‘monotonic’ was not decla red in this scope    11 |     assert (monotonic({9, 9, 9, 9}) == true);       |             ^~~~~~~~~

--- CPP_57.cpp
+++ CPP_57.cpp
@@ -1,14 +1,8 @@
-bool monotonic(vector<float> l) {
-    bool increasing = true;
-    bool decreasing = true;
-
-    for (int i = 1; i < l.size(); i++) {
-        if (l[i] > l[i - 1]) {
-            decreasing = false;
-        } else if (l[i] < l[i - 1]) {
-            increasing = false;
-        }
+bool monotonic(std::vector<int> l) {
+    bool increasing = true, decreasing = true;
+    for (size_t i = 1; i < l.size(); ++i) {
+        if (l[i] < l[i - 1]) increasing = false;
+        if (l[i] > l[i - 1]) decreasing = false;
     }
-
     return increasing || decreasing;
 }
