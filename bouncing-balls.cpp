462a628702698e850020410ea299dbf04ec4ad98
beep boop <githubbot@vadim.me>
2024-07-10 03:58:15 +0200

    Solution 97 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/eycidsiw Prompt: 30fce49e-693a-402f-9aa3-c31bafec562a.cpp: In function ‘int main()’: 30fce49e-693a-402f-9aa3-c31bafec562a.cpp:14:26: error: ‘pow’ was not declared in  this scope    14 |         totalDistance += pow(2, -bounciness) * startHeight;       |                          ^~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
-#include <cmath>
-using namespace std;
+#include <cmath> 
+using namespace std; 
 
-int main() {
-    double startHeight, firstBounce;
-    int numBounces;
+int main() { 
+    double startHeight, firstBounce; 
+    int numBounces; 
 
-    cin >> startHeight >> firstBounce >> numBounces;
+    cin >> startHeight >> firstBounce >> numBounces; 
 
-    double bounciness = firstBounce / startHeight;
+    double bounciness = firstBounce / startHeight; 
 
-    double totalDistance = 0.0;
-    for (int i = 1; i <= numBounces; ++i) {
-        totalDistance += pow(2, -bounciness) * startHeight;
-        startHeight *= 2 * (1 - bounciness);
-    }
+    double totalDistance = 0.0; 
+    for (int i = 1; i <= numBounces; ++i) { 
+        totalDistance += pow(2, -bounciness) * startHeight; 
+        startHeight *= 2 * (1 - bounciness); 
+    } 
 
     cout.precision(5); 
-    cout << fixed << totalDistance << endl;
+    cout << fixed << totalDistance << endl; 
 
     return 0;
-}
