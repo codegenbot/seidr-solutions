de8d4f6644649868d4bb80cd8593e3c3c0efc71a
beep boop <githubbot@vadim.me>
2024-07-10 05:12:50 +0200

    Solution 15 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/c6w360zo Prompt: ’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = long long int; _Alloc = std::allocator<long long int>; std::_Vector_base< _Tp, _Alloc>::pointer = long long int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {long long int}; _Tp = long long int; _Alloc = std::allocator<long long int>; std::vector<_Tp, _Alloc>::iterator = std::vector<long long int>::iter ator]’ /usr/include/c++/11/bits/vector.tcc:121:21:   required from ‘std::vector<_Tp, _A lloc>::reference std::vector<_Tp, _Alloc>::emplace_back(_Args&& ...) [with _Args  = {long long int}; _Tp = long long int; _Alloc = std::allocator<long long int>;  std::vector<_Tp, _Alloc>::reference = long long int&]’ /usr/include/c++/11/bits/stl_vector.h:1204:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(std::vector<_Tp, _Alloc>::value_type&&) [with _Tp = lon g long int; _Alloc = std::allocator<long long int>; std::vector<_Tp, _Alloc>::va lue_type = long long int]’ 332ced07-4e29-45c3-93c8-15b8575d4c80.cpp:24:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<long long int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -2,14 +2,13 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <cassert>
 
-long long double_the_difference(std::vector<double> lst) {
+long long double_the_difference(std::vector<long long> lst) {
     long long sum = 0;
-    for (double num : lst) {
-        if (num > 0 && floor(num) == num) { 
-            if (fmod(num, 2.0) != 0.0) { 
-                sum += pow(num, 2);
+    for (long long num : lst) {
+        if (num > 0 && floor((double)num) == num) { 
+            if (fmod((double)num, 2.0) != 0.0) { 
+                sum += pow((double)num, 2);
             }
         }
     }
@@ -17,15 +16,13 @@ long long double_the_difference(std::vector<double> lst) {
 }
 
 int main() {
-    std::vector<double> lst;
-    double num;
-    long long odd_sum = 0; 
+    std::vector<long long> lst;
+    long long int num;
     while(std::cin >> num) {
         lst.push_back(num);
     }
     
-    int main_result = double_the_difference(lst); 
-    assert(main_result == double_the_difference(lst));
+    long long main_result = double_the_difference(lst); 
     std::cout << "The sum of the squares of the odd numbers is: " << main_result << std::endl;
     return 0;
 }
