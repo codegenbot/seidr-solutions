5397c839ee5975f40141837d511917946a2c4f78
beep boop <githubbot@vadim.me>
2024-07-10 21:41:58 +0200

    Solution 88 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/we682yma Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a8fc9d0e-6730-4a4f-adef-446dcb478 bf0.py", line 17, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a8fc9d0e-6730-4a4f-adef-446dcb478 bf0.py", line 14, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_41.py
+++ Python_41.py
@@ -2,11 +2,6 @@ def car_race_collision(n: int = 0, a1: int = 0, a2: int = 0):
     if n <= 0:
         return 0
     else:
-        total_distance = 0
-        for i in range(1, n+1):
-            distance_to_collide = max(0, (a2 - a1) // 2)
-            if i <= distance_to_collide:
-                total_distance += min(a1, a2) * i
-            else:
-                total_distance += min(a1, a2) * distance_to_collide + min(a1, a2) * (n-i+1)
+        distance_to_collide = (a1 + a2 - abs(a1 - a2)) / 2
+        total_distance = min(a1, a2) * distance_to_collide
         return total_distance
