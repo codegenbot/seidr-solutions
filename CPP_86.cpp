f687f2a60dff7c2f9da851fd495b78158aa9b31f
beep boop <githubbot@vadim.me>
2024-07-10 07:13:40 +0200

    Solution 49 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3q16x72n Prompt: af2ea0f7-0e85-462c-80e8-c3e908bf7727.cpp:4:1: error: ‘string’ does not name a ty pe     4 | string anti_shuffle(string s) {       | ^~~~~~ af2ea0f7-0e85-462c-80e8-c3e908bf7727.cpp:14:1: error: ‘string’ does not name a t ype    14 | string split(const string& str, const string& delimiter) {       | ^~~~~~ In file included from af2ea0f7-0e85-462c-80e8-c3e908bf7727.cpp:25: af2ea0f7-0e85-462c-80e8-c3e908bf7727.cpp: In function ‘int main()’: af2ea0f7-0e85-462c-80e8-c3e908bf7727.cpp:27:13: error: ‘anti_shuffle’ was not de clared in this scope    27 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -14,11 +14,11 @@ std::string anti_shuffle(std::string s) {
 
 std::string split(const std::string& str, const std::string& delimiter) {
     size_t pos = 0;
-    vector<std::string> tokens;
+    vector<string> tokens;
     while ((pos = str.find(delimiter)) != string::npos) {
         tokens.push_back(str.substr(0, pos));
         str.erase(0, pos + delimiter.length());
     }
     tokens.push_back(str);
-    return tokens.size() > 1 ? tokens : vector<std::string>();
+    return tokens.size() > 1 ? tokens : vector<string>();
 }
