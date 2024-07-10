8afd5148c75affea4ddcfbd539d5fc88cd1aa36a
beep boop <githubbot@vadim.me>
2024-07-10 05:04:07 +0200

    Solution 89 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/kidx0dax Prompt: it must return 2 1 for input YOOB BYOY, but it returns .

--- mastermind.py
+++ mastermind.py
@@ -1,15 +1,15 @@
 def mastermind(code, guess):
     black = 0
-    white = [0] * 6
-    code_counts = [0] * 6
+    white = [0]*6 
+    code_counts = [0]*6 
 
-    for i in range(4):
+    for i in range(6):
         if code[i] == guess[i]:
             black += 1
         else:
             color = str(guess[i])
-            if code_counts[ord(color) - 65] > 0:
-                white[ord(color) - 65] += 1
-            code_counts[ord(color) - 65] -= 1
+            if code_counts[ord(color)-65] > 0: 
+                white[ord(color)-65] += 1
+            code_counts[ord(color)-65] -= 1
 
-    return str(black) + "\n" + " ".join(map(str, white))
+    return str(black) + "\n" + ' '.join(map(str,white))
