7c46d3ccc9a420eb136c5397f9d446c03697c5aa
beep boop <githubbot@vadim.me>
2024-07-10 05:28:42 +0200

    Solution 17 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j9zzr72u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5616faf6-2c16-4e5b-8c64-2ccecdc4a acc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,5 +1,8 @@
 ```
 def sorted_list_sum(lst):
     even_words = sorted([word for word in lst if len(word) % 2 == 0])
-
+    
     return sum(len(word) for word in even_words)
+
+lst = [input("Enter words (separated by space): ").split()]
+print(sorted_list_sum(lst[0]))
