83eb601ec1c9dac904a9e6a34d9d4858db172587
beep boop <githubbot@vadim.me>
2024-07-10 03:47:09 +0200

    Solution 94 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ul4hcvmg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8e7a0096-2b45-444a-98ae-1e3ca8727 fdf.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -8,8 +8,8 @@ def minPath(grid, k):
     while queue:
         value, path, step = queue.pop(0)
         if step == k:
-            if not result or sum(int(digit) for digit in str(value)) < sum(int(digit) for digit in str(result)):
-                result = value
+            if not result or len(path) < len(result):
+                result = path
         else:
             for direction in directions:
                 x, y = (value // n) + direction[0], (value % n) + direction[1]
