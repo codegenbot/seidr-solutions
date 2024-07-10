af8219e975c520e32a897c6301c06c30dac608f7
beep boop <githubbot@vadim.me>
2024-07-10 21:41:39 +0200

    Solution 16 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/idv3uybh Prompt:    23 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ af2f47c5-3f23-44e3-a46f-38ecd9f0238b.cpp:23:44: error: ‘b’ was not declared in t his scope    23 | bool issame(vector<string> a,vector<string>b){       |                                            ^ af2f47c5-3f23-44e3-a46f-38ecd9f0238b.cpp:23:45: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from af2f47c5-3f23-44e3-a46f-38ecd9f0238b.cpp:22: af2f47c5-3f23-44e3-a46f-38ecd9f0238b.cpp: In function ‘int main()’: af2f47c5-3f23-44e3-a46f-38ecd9f0238b.cpp:32:20: error: ‘sorted_list_sum’ was not  declared in this scope    32 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ af2f47c5-3f23-44e3-a46f-38ecd9f0238b.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,44 +1,10 @@
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
+    if (a.size() != b.size())
         return false;
-    }
     
     for (int i = 0; i < a.size(); i++) {
-        if (issame_helper(a[i], b[i])) {
-            continue;
-        } else {
+        if (a[i] != b[i])
             return false;
-        }
     }
-    
     return true;
-}
-
-bool issame_helper(string str1, string str2) {
-    vector<char> v1(str1.begin(), str1.end());
-    vector<char> v2(str2.begin(), str2.end());
-    
-    sort(v1.begin(), v1.end());
-    sort(v2.begin(), v2.end());
-    
-    return v1 == v2;
-}
-
-vector<string> sorted_list_sum(vector<string> lst) {
-    vector<string> result;
-
-    for (const string& str : lst) {
-        if (str.length() % 2 == 0) {
-            result.push_back(str);
-        }
-    }
-
-    sort(result.begin(), result.end(),
-         [](const string& a, const string& b) { return a.length() < b.length(); });
-
-    // If two words have the same length, sort alphabetically
-    std::stable_partition(result.begin(), result.end(),
-        [len](const string& str) { return str.length() == len; });
-
-    return result;
 }
