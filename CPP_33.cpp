cd550fd762bd4f2e021e1bf3e41d715edc662d3e
beep boop <githubbot@vadim.me>
2024-07-10 14:00:06 +0200

    Solution 20 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/epocmqxi Prompt:    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 00b14644-c82c-4c5e-8c42-aaf0a5bc973f.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 00b14644-c82c-4c5e-8c42-aaf0a5bc973f.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 00b14644-c82c-4c5e-8c42-aaf0a5bc973f.cpp:9: 00b14644-c82c-4c5e-8c42-aaf0a5bc973f.cpp: In function ‘int main()’: 00b14644-c82c-4c5e-8c42-aaf0a5bc973f.cpp:19:20: error: ‘sort_third’ was not decl ared in this scope    19 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 00b14644-c82c-4c5e-8c42-aaf0a5bc973f.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,24 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cassert>
-
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
-}
-
-std::vector<int> sort_third(const std::vector<int>& l) {
-    std::vector<int> l_copy = l;
-    for (int i = 0; i < l.size(); ++i) {
-        if (i % 3 == 0) {
-            std::sort(l_copy.begin() + i, l_copy.begin() + i + 3);
-        }
+void sort_third(vector<int>& l) {
+    for (int i = 0; i < l.size(); i += 3) {
+        sort(l.begin() + i, l.begin() + i + 3);
     }
-    return l_copy;
 }
 
-int main() {
-    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
-    
-    return 0;
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a == b;
 }
