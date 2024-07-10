630ea385dddf39d0152a1e1fa4c7024b0e1b8485
beep boop <githubbot@vadim.me>
2024-07-10 06:53:18 +0200

    Solution 70 of CPP/51, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0mvu4d4d Prompt: 236ca8e8-7e29-43bd-9dea-20c0bb251b10.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string remove_vowels(string text){       | ^~~~~~ In file included from 236ca8e8-7e29-43bd-9dea-20c0bb251b10.cpp:11: 236ca8e8-7e29-43bd-9dea-20c0bb251b10.cpp: In function ‘int main()’: 236ca8e8-7e29-43bd-9dea-20c0bb251b10.cpp:13:13: error: ‘remove_vowels’ was not d eclared in this scope    13 |     assert (remove_vowels("ybcd") == "ybcd");       |             ^~~~~~~~~~~~~

--- CPP_51.cpp
+++ CPP_51.cpp
@@ -1,9 +1,10 @@
-string remove_vowels(string text){
-    string result = "";
-    for(int i=0; i<text.length();i++){
-        char c = tolower(text[i]);
-        if(c!='a' && c!='e' && c != 'i' && c != 'o' && c != 'u'){
-            result += c;
+#include <string>
+
+std::string remove_vowels(std::string text){
+    std::string result = "";
+    for(int i=0; i<text.length(); i++){
+        if(!((text[i]-'a'>=0 && text[i]-'a'<5) || (text[i]-'A'>=0 && text[i]-'A'<5))){
+            result += text[i];
         }
     }
     return result;
