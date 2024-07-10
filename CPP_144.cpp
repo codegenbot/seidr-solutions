d659c9b77552756d8d7cd996359081862971945a
beep boop <githubbot@vadim.me>
2024-07-10 08:06:28 +0200

    Solution 1 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8t1bc2ba Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -2,10 +2,24 @@
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = stoi(strtok(x.c_str(), "/")[0]);
-    int b = stoi(strtok(NULL, "/")[0]);
-    int c = stoi(strtok(n.c_str(), "/")[0]);
-    int d = stoi(strtok(NULL, "/")[0]);
+    int a = 1, b = 1, c = 1, d = 1;
+    int numeratorX = stoi(strtok(&x[0], "/"));
+    int denominatorX = stoi(strtok(nullptr, "/"));
+    int numeratorN = stoi(strtok(&n[0], "/"));
+    int denominatorN = stoi(strtok(nullptr, "/"));
 
-    return (1.0 * a * d) / (b * c) == (int)((1.0 * a * d) / (b * c));
+    // Find the greatest common divisor of a and b
+    while (d % a != 0)
+        d = d % a;
+
+    // Apply GCD to both fractions
+    a = numeratorX / d;
+    c = numeratorN / d;
+    b = denominatorX / d;
+    d = denominatorN / d;
+
+    if ((a * c) == (b * d))
+        return true;
+    else
+        return false;
 }
