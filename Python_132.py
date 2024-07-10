d62d2d234d62a2f9b82b68ade3b670c872d85a62
beep boop <githubbot@vadim.me>
2024-07-10 11:33:45 +0200

    Solution 11 of Python/132, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/80jllwi0 Prompt: Complete the following code given the task description and function signature.

--- Python_132.py
+++ Python_132.py
@@ -5,7 +5,7 @@ def is_nested(string):
         if char == '[':
             stack.append(char)
         elif char == ']':
-            if len(stack) == 0:
+            if len(stack) < 1:
                 return False
             stack.pop()
     return len(stack) > 0
