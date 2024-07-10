d43506dbeee017fe8bf9bd36f1b36690b56fb790
beep boop <githubbot@vadim.me>
2024-07-10 22:32:38 +0200

    Solution 42 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lx335o7c Prompt: w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const int*; _Tp = int; _Alloc = std:: allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = i nt; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::allocator_type = std ::allocator<int>]’ 6a5494ec-6582-4489-a6d0-1caeeaa24170.cpp:15:42:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,32 +1,36 @@
-#include <vector>
 #include <iostream>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return (a.size() == b.size()) && equal(a.begin(), a.end(), b.begin());
-}
+#include <vector>
 
 std::vector<int> even_odd_palindrome(int n) {
-    int evp = 0;
-    int ovp = 0;
-    for (int i = 1; i <= n; ++i) {
-        if (isEven(i)) {
-            ++evp;
-        } else if (isPalindrome(i)) {
-            ++ovp;
+    int even = 0, odd = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i == reverse(i)) {
+            if (i % 2 == 0)
+                even++;
+            else
+                odd++;
         }
     }
-    return {evp, ovp};
+    return {even, odd};
 }
 
-bool isEven(int num) {
-    return (num & 0x01);
+int main() {
+    int n;
+    std::cout << "Enter a positive integer: ";
+    std::cin >> n;
+
+    std::vector<int> result = even_odd_palindrome(n);
+
+    std::cout << "Number of even palindromes: " << result[0] << std::endl;
+    std::cout << "Number of odd palindromes: " << result[1] << std::endl;
 }
 
-bool isPalindrome(int num) {
-    int rev = 0, original = num;
-    while (original > 0) {
-        int digit = original % 10;
-        rev = rev * 10 + digit;
-        original /= 10;
+int reverse(int n) {
+    int rev = 0;
+    while (n != 0) {
+        int remainder = n % 10;
+        rev = rev * 10 + remainder;
+        n /= 10;
     }
-    return (rev == num);
+    return rev;
+}
