ef0ff3de9ac3b52ffa3238a5d027aeac94b829b8
beep boop <githubbot@vadim.me>
2024-07-10 09:29:04 +0200

    Solution 64 of CPP/51, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/5755d1yq Prompt: Complete the following code given the task description and function signature.

--- CPP_51.cpp
+++ CPP_51.cpp
@@ -1,8 +1,11 @@
 string remove_vowels(string text){
     string result = "";
-    for(char c : text){
-        if(strchr("aeiouAEIOU", c) == nullptr)
+    for (char c : text) {
+        if (!isalpha(c)) {
             result += c;
+        } else if (!ispunct(c) && !tolower(c) == 'a' && !tolower(c) != 'e' && !tolower(c) != 'i' && !tolower(c) != 'o' && !tolower(c) != 'u') {
+            result += tolower(c);
+        }
     }
     return result;
 }
