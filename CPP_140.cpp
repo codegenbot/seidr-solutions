d3dc495dffe21cdc10d6f7219e39d369681a59cf
beep boop <githubbot@vadim.me>
2024-07-10 07:59:45 +0200

    Solution 4 of CPP/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/emysymgl Prompt: Complete the following code given the task description and function signature.

--- CPP_140.cpp
+++ CPP_140.cpp
@@ -1,8 +1,24 @@
 string fix_spaces(string text){
     string result = "";
     for(int i = 0; i < text.length(); i++){
-        if(text[i] == ' ' && (i == 0 || text[i-1] != ' ') && (i == text.length()-1 || text[i+1] != ' ')){
-            result += "_";
+        if(text[i] == ' ' && (i == 0 || text[i-1] != ' ')){
+            result += '_';
+        } else if(text[i] == ' '){
+            bool is_consecutive = true;
+            for(int j = i+1; j < text.length() && j > 0; j++){
+                if(text[j] == ' '){
+                    is_consecutive = true;
+                    break;
+                } else {
+                    is_consecutive = false;
+                }
+            }
+            if(is_consecutive){
+                result += '-';
+                i = text.length();
+            } else {
+                result += '_';
+            }
         } else {
             result += text[i];
         }
