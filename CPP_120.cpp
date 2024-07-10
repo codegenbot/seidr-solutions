963b2dcd634ac233db8a1e9a502b5d4c37c92659
beep boop <githubbot@vadim.me>
2024-07-10 08:18:29 +0200

    Solution 96 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/u6x0d6pd Prompt:       |                    ^~~ fde69491-c94e-4045-b125-e7ae7666b23c.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fde69491-c94e-4045-b125-e7ae7666b23c.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fde69491-c94e-4045-b125-e7ae7666b23c.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fde69491-c94e-4045-b125-e7ae7666b23c.cpp:14: fde69491-c94e-4045-b125-e7ae7666b23c.cpp: In function ‘int main()’: fde69491-c94e-4045-b125-e7ae7666b23c.cpp:24:20: error: ‘maximum’ was not declare d in this scope    24 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ fde69491-c94e-4045-b125-e7ae7666b23c.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -5,13 +5,12 @@ bool issame(int a, int b) {
     return a == b;
 }
 
-std::vector<int> maximum(std::vector<int> arr, int n) {
-    std::vector<int> result;
-    for (int i = 0; i < n; i++) {
-        if(i >= arr.size()) break;
-        auto it = std::max_element(arr.begin(), arr.end());
-        result.push_back(*it);
-        arr.erase(it);
+std::vector<int> maximum(std::vector<int> arr) {
+    std::sort(arr.begin(), arr.end());
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        if (!issame(arr[i], arr[0])) {
+            return {arr[i]};
+        }
     }
-    return result;
+    return {arr[0]};
 }
