36e4fa3bd100a26d63815c2603808f2119c25cba
beep boop <githubbot@vadim.me>
2024-07-10 20:24:55 +0200

    Solution 78 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hwoml10f Prompt: ng<char>; std::allocator_traits<std::allocator<_CharT> >::pointer = std::__cxx11 ::basic_string<char>*; std::allocator_traits<std::allocator<_CharT> >::allocator _type = std::allocator<std::__cxx11::basic_string<char> >]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11::b asic_string<char> >; std::_Vector_base<_Tp, _Alloc>::pointer = std::__cxx11::bas ic_string<char>*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const std::__cxx11::basic_string<char >*; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11 ::basic_string<char> >]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = s td::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11::basic_str ing<char> >; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<std::__cx x11::basic_string<char> >]’ 71a833cc-f0b6-4e47-ad15-94cf61995591.cpp:58:5:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::__cxx11::basic_string<char> >’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, const std::vector<std::string>& b) {
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) return false;
@@ -14,7 +14,8 @@ bool issame(std::vector<std::string> a, const std::vector<std::string>& b) {
 std::vector<std::string> all_prefixes(const std::string& input) {
     std::vector<std::string> result;
     for (int i = 0; i <= input.size(); ++i) {
-        result.push_back(input.substr(0, i+1)); 
+        if(i > 0)
+            result.push_back(input.substr(0, i));
     }
     return result;
 }
