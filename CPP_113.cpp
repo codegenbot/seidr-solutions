18ba1f9fd8fea9e04c703b6f677327e5c7f88083
beep boop <githubbot@vadim.me>
2024-07-10 19:20:00 +0200

    Solution 41 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/xjg4rs13 Prompt: 36f17d77-fb14-4882-b8d7-37c7f17a9c4d.cpp:46:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    46 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 36f17d77-fb14-4882-b8d7-37c7f17a9c4d.cpp:9:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     9 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ 36f17d77-fb14-4882-b8d7-37c7f17a9c4d.cpp:54:5: error: redefinition of ‘int main( )’    54 | int main(){       |     ^~~~ 36f17d77-fb14-4882-b8d7-37c7f17a9c4d.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -4,7 +4,17 @@
 
 using namespace std;
 
-vector<string> odd_count(vector<string> lst);
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 vector<string> odd_count(vector<string> lst) {
     vector<string> result;
@@ -18,18 +28,6 @@ vector<string> odd_count(vector<string> lst) {
         result.push_back("the number of odd elements " + to_string(count) + " in the string " + s + " of the input.");
     }
     return result;
-}
-
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
 }
 
 int main() {
