a463d53e2931ae189ab7d2c3a682abda42d24cbe
beep boop <githubbot@vadim.me>
2024-07-10 20:56:23 +0200

    Solution 48 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/li7ti4tw Prompt: 995c926f-a6a5-4b3c-8f1f-65d7aa631ef9.cpp:6:1: error: ‘string’ does not name a ty pe     6 | string sort_numbers(string numbers) {       | ^~~~~~ In file included from 995c926f-a6a5-4b3c-8f1f-65d7aa631ef9.cpp:30: 995c926f-a6a5-4b3c-8f1f-65d7aa631ef9.cpp: In function ‘int main()’: 995c926f-a6a5-4b3c-8f1f-65d7aa631ef9.cpp:32:13: error: ‘sort_numbers’ was not de clared in this scope    32 |     assert (sort_numbers("six five four three two one zero") == "zero on e two three four five six");       |             ^~~~~~~~~~~~

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,10 +1,5 @@
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 std::string sort_numbers(std::string numbers) {
-    vector<std::string> numVec;
+    std::vector<std::string> numVec;
     std::string temp;
     
     for (char c : numbers) {
@@ -13,11 +8,11 @@ std::string sort_numbers(std::string numbers) {
             numVec.push_back(temp);
             temp = "";
         }
-        temp += to_string(c == ' ' ? 0 : (c - '0' + 1));
+        temp += std::to_string(c == ' ' ? 0 : (c - '0' + 1));
     }
     if (!temp.empty()) numVec.push_back(temp);
 
-    sort(numVec.begin(), numVec.end());
+    std::sort(numVec.begin(), numVec.end());
     
     std::string result;
     for (std::string str : numVec) {
