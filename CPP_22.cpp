946450a29778ca70d0f39b1339aee5aa845bb29a
beep boop <githubbot@vadim.me>
2024-07-10 16:45:02 +0200

    Solution 73 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9x5hjcql Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8a919e1b-3922-4771-a130-613c8dae9e5d.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8a919e1b-3922-4771-a130-613c8dae9e5d.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8a919e1b-3922-4771-a130-613c8dae9e5d.cpp:11: 8a919e1b-3922-4771-a130-613c8dae9e5d.cpp: In function ‘int main()’: 8a919e1b-3922-4771-a130-613c8dae9e5d.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 8a919e1b-3922-4771-a130-613c8dae9e5d.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,20 +1,16 @@
 #include <algorithm>
 #include <vector>
-#include <list>
 #include <boost/any.hpp>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
 }
 
 std::vector<int> filter_integers(std::list<boost::any> values) {
     std::vector<int> result;
     for (const auto& value : values) {
-        if(boost::holds_alternative<int>(value)) {
-            int val = boost::get<int>(value);
-            if(val != 0) {
-                result.push_back(val);
-            }
+        if (boost::any_cast<int>(value) != 0) {
+            result.push_back(boost::any_cast<int>(value));
         }
     }
     return result;
