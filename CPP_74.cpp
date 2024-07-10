81697480507a62791bbbfdd9cc10407ae2a180a7
beep boop <githubbot@vadim.me>
2024-07-10 03:37:04 +0200

    Solution 42 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/j8k6sljz Prompt: dadbb147-0e4e-4db4-86a7-0f3b6a25db2b.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ dadbb147-0e4e-4db4-86a7-0f3b6a25db2b.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ dadbb147-0e4e-4db4-86a7-0f3b6a25db2b.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ dadbb147-0e4e-4db4-86a7-0f3b6a25db2b.cpp:41:1: error: ‘::main’ must return ‘int’    41 | void main() {       | ^~~~ dadbb147-0e4e-4db4-86a7-0f3b6a25db2b.cpp:54:5: error: redefinition of ‘int main( )’    54 | int main(){       |     ^~~~ dadbb147-0e4e-4db4-86a7-0f3b6a25db2b.cpp:41:6: note: ‘int main()’ previously def ined here    41 | void main() {       |      ^~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -28,11 +28,12 @@ int main() {
         string input;
         cin >> input;
         user_input.push_back(input);
-    }
+   
     
     if (check_if_same(total_match(user_input, vector<string>(5)), vector<string>(5))) {
         cout << "The two lists are the same." << endl;
     } else {
         cout << "Error: The two lists are not the same." << endl;
     } 
+    return 0;
 }
