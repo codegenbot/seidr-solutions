9316266306ed23ded1293b5493a73aad47f2511d
beep boop <githubbot@vadim.me>
2024-07-10 11:01:54 +0200

    Solution 71 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/xub5v5hi Prompt: 537f16a3-5d21-484d-bd7d-0b399b71e490.cpp:1:6: error: ‘string’ in namespace ‘std’  does not name a type     1 | std::string roll = input.substr(i, 2);       |      ^~~~~~ 537f16a3-5d21-484d-bd7d-0b399b71e490.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | std::string roll = input.substr(i, 2);

--- bowling.cpp
+++ bowling.cpp
@@ -1,49 +1,32 @@
-#include <string>
+#include <iostream>
+#include <vector>
+#include <sstream>
 
-int bowlingScore(string input) {
-    int total = 0;
-    for (int i = 0; i < 20; i++) {
-        string roll = input.substr(i, 1);
-        if (roll == "X") {
-            total += 10 + getBonus(input, i);
-        } else if (roll[0] == ' ') {
-            int strikeValue = getStrikeValue(input, i);
-            total += strikeValue;
-        } else {
-            int value = roll[0] - '0';
-            total += value;
-            roll = input.substr(i + 1, 2);
-            while (roll != "///") {
-                value += roll[0] - '0';
-                if (roll[1] == '/') {
-                    break;
-                }
-                roll = input.substr(i + 3, 2);
+int bowlingScore(const std::string& rolls) {
+    int score = 0;
+    bool firstRollInFrame = true;
+
+    for (const auto& roll : rolls) {
+        if (roll == '/') {
+            if (firstRollInFrame) {
+                firstRollInFrame = false;
+            } else {
+                score += 10;
             }
-            total += value;
+        } else if (isdigit(roll)) {
+            int currentRoll = roll - '0';
+            if (firstRollInFrame) {
+                firstRollInFrame = false;
+                score += currentRoll;
+            } else {
+                if (currentRoll == 10) {
+                    score += 10;
+                } else {
+                    score += currentRoll;
+                }
+            }
         }
     }
-    return total;
-}
 
-int getStrikeValue(string input, int i) {
-    int strikeValue = 10;
-    for (int j = i; j < i + 2; j++) {
-        string roll = input.substr(j, 1);
-        if (roll == "X") {
-            strikeValue += 10 + getBonus(input, j);
-        } else {
-            strikeValue += roll[0] - '0';
-        }
-    }
-    return strikeValue;
-}
-
-int getBonus(string input, int i) {
-    string roll = input.substr(i + 1, 2);
-    if (roll == "X") {
-        return 10 + getBonus(input, i + 2);
-    } else {
-        return roll[0] - '0';
-    }
+    return score;
 }
