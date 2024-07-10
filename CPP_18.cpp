e3aaa5cebc2f0e67c6c8c671855dabf492a1ab60
beep boop <githubbot@vadim.me>
2024-07-10 02:01:28 +0200

    Solution 43 of CPP/18, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/wsyiheb7 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -1,16 +1,5 @@
-```c++
 #include <iostream>
 #include <string>
-
-int how_many_times(const std::string& str, const std::string& substring) {
-    int count = 0;
-    size_t pos = 0;
-    while ((pos = str.find(substring)) != std::string::npos) {
-        ++count;
-        str.erase(pos, substring.length());
-    }
-    return count;
-}
 
 int main() {
     std::string str, substring;
@@ -21,4 +10,16 @@ int main() {
     int result = how_many_times(str, substring);
     std::cout << "The substring appears " << result << " times." << std::endl;
     return 0;
+}
+
+int how_many_times(std::string str, std::string substring) {
+    int count = 0;
+    size_t pos = 0;
+    while ((pos = str.find(substring)) != std::string::npos) {
+        ++count;
+        if (str.size() == 0)
+            break;
+        str.erase(pos, substring.length());
+    }
+    return count;
 }
