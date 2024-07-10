3b6241f038a3e6d609e9a84b1bb60b5ed48fb01d
beep boop <githubbot@vadim.me>
2024-07-10 03:34:53 +0200

    Solution 1 of CPP/99, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8sbhtw60 Prompt: 71264129-e824-4c1d-bead-6af9a664af80.cpp:3:21: error: ‘string’ was not declared in this scope     3 | int closest_integer(string value) {       |                     ^~~~~~ 71264129-e824-4c1d-bead-6af9a664af80.cpp:3:21: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 71264129-e824-4c1d-bead-6af9a664af80.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 71264129-e824-4c1d-bead-6af9a664af80.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from 71264129-e824-4c1d-bead-6af9a664af80.cpp:8: 71264129-e824-4c1d-bead-6af9a664af80.cpp: In function ‘int main()’: 71264129-e824-4c1d-bead-6af9a664af80.cpp:10:28: error: ‘closest_integer’ cannot be used as a function    10 |     assert (closest_integer("0") == 0);       |             ~~~~~~~~~~~~~~~^~~~~

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,6 +1,7 @@
 #include <string>
+#include <cmath>
 
-int closest_integer(string value) {
-    double num = stod(value);
+int closest_integer(std::string value) {
+    double num = std::stod(value);
     return (num >= 0) ? ceil(num) : floor(num);
 }
