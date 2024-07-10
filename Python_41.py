b720f2c41080cbeee1846c9cc5688a52511682d9
beep boop <githubbot@vadim.me>
2024-07-10 16:34:58 +0200

    Solution 34 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xwmgxcc8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/30c5833c-f6b3-4542-8958-8d52fb932 7b9.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_41.py
+++ Python_41.py
@@ -1,7 +1,7 @@
 def car_race_collision(tracks):
     collisions = 0
-    for i in range(int(input("Enter number of tracks: "))):
-        for j in range(i+1, int(input("Enter number of tracks: ")) + 1):
-            if (i+j) % 3 == 0:
+    for i in range(1, tracks):
+        for j in range(i):
+            if (j + 1) % 3 == 0:
                 collisions += 1
     return collisions
