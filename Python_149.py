76431064dfc4acb0b93fd68c63995e641104816d
beep boop <githubbot@vadim.me>
2024-07-10 06:43:18 +0200

    Solution 35 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j9zzr72u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5ac2eb5b-7ec7-4188-b613-3bb6fcef1 dc0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -2,10 +2,6 @@
 def main():
     lst = input("Enter words (separated by space): ").lower().replace(" ", "").split()
     if len(lst) > 0:
-        print(sum(len(word) for word in lst if len(word) % 2 == 0))
+        print(sum(len(word) for word in lst if int(len(word)) % 2 == 0))
     else:
         print("Please enter at least one word.")
-
-
-if __name__ == "__main__":
-    main()
