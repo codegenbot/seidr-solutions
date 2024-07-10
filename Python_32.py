558b0fd264fa0c1bf0760aeb784ab39ddd3a5ec8
beep boop <githubbot@vadim.me>
2024-07-10 14:39:04 +0200

    Solution 70 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fq8lg3pf Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,3 +1,6 @@
 def find_zero(xs: list):
-    x = -xs[0] / xs[1]
+    n = len(xs) - 1
+    assert n % 2 == 0
+    largest_non_zero_coeff = max([abs(coeff) for coeff in xs[1:]])
+    x = -largest_non_zero_coeff / xs[-1]
     return x
