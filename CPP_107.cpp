cf4e4887f3f387580e03976ded3b1dda620ec54a
beep boop <githubbot@vadim.me>
2024-07-10 07:13:21 +0200

    Solution 2 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/i4ztvcn8 Prompt:       |                    ^~~ ded09e02-8ea0-4dc3-b356-d75fd2382f71.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ded09e02-8ea0-4dc3-b356-d75fd2382f71.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ded09e02-8ea0-4dc3-b356-d75fd2382f71.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ded09e02-8ea0-4dc3-b356-d75fd2382f71.cpp:18: ded09e02-8ea0-4dc3-b356-d75fd2382f71.cpp: In function ‘int main()’: ded09e02-8ea0-4dc3-b356-d75fd2382f71.cpp:28:20: error: ‘even_odd_palindrome’ was  not declared in this scope    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ ded09e02-8ea0-4dc3-b356-d75fd2382f71.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,16 +1,11 @@
 #include <vector>
-#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
-}
-
-std::vector<int> even_odd_palindrome(int n) {
-    std::vector<int> result(2, 0);
+vector<int> even_odd_palindrome(int n){
+    vector<int> result(2, 0);
     for(int i=1; i<=n; ++i){
-        std::string num = std::to_string(i);
-        std::string rev_num = num;
-        std::reverse(rev_num.begin(), rev_num.end());
+        string num = to_string(i);
+        string rev_num = num;
+        reverse(rev_num.begin(), rev_num.end());
         if(num == rev_num){
             if(i % 2 == 0){
                 result[0]++;
@@ -23,7 +18,6 @@ std::vector<int> even_odd_palindrome(int n) {
 }
 
 int main() {
-    assert(issame(even_odd_palindrome(1) , {0, 1}));
-    
+    // Main function code here
     return 0;
 }
