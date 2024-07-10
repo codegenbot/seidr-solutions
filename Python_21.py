44732779e4a74c8fad76534ad7c15eb7c239a675
beep boop <githubbot@vadim.me>
2024-07-10 20:35:17 +0200

    Solution 46 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vmj9u5tm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d6007718-9f34-4a55-834f-01b5e38c1 85f.py", line 2     return ["0.0"] * len(numbers)     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_21.py
+++ Python_21.py
@@ -1,4 +1,5 @@
-```
 def solve_task(numbers):
-    if (max(numbers) - min(numbers)) == 0:
+    min_val = min(numbers)
+    max_val = max(numbers)
+    if (max_val - min_val) == 0:
         return ["0.0"] * len(numbers)
