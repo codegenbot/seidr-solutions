96873a36a0e757abbaa865a878d706e25e99e2b1
beep boop <githubbot@vadim.me>
2024-07-10 03:25:04 +0200

    Solution 16 of CPP/76, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gib6hfmu Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_76.cpp
+++ CPP_76.cpp
@@ -13,20 +13,18 @@ bool is_simple_power(int x, int n) {
 int main() {
     std::string xStr;
     std::cout << "Enter a number: ";
-    int tempX;
-    std::cin >> tempX;
-    xStr = std::to_string(tempX);
+    std::cin >> xStr;
+    int x = std::stoi(xStr);
 
     std::string nStr;
     std::cout << "Enter another number: ";
-    int tempN;
-    std::cin >> tempN;
-    nStr = std::to_string(tempN);
+    std::cin >> nStr;
+    int n = std::stoi(nStr);
     
-    if(is_simple_power(std::stoi(xStr), std::stoi(nStr)))
-        std::cout << (std::stoi(xStr) == pow(std::stod(nStr), log(std::stod(xStr)) / log(std::stod(nStr))) ? xStr + " is a simple power of " + nStr : xStr + " is not a simple power of " + nStr) << std::endl;
+    if(is_simple_power(x, n))
+        std::cout << (x == n ? x << " is a simple power of " << n : x << " is not a simple power of " << n) << std::endl;
     else
-        std::cout << (std::stoi(xStr) == pow(std::stod(nStr), log(std::stod(xStr)) / log(std::stod(nStr))) ? xStr + " is a simple power of " + nStr : xStr + " is not a simple power of " + nStr) << std::endl;
+        std::cout << (x == n ? x << " is a simple power of " << n : x << " is not a simple power of " << n) << std::endl;
 
     return 0;
 }
