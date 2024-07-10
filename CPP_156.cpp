2f2edced0f47cb34566873a8023ab40c23b7d1f4
beep boop <githubbot@vadim.me>
2024-07-10 23:42:54 +0200

    Solution 63 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/mff2pwuv Prompt: 49170b02-9795-480a-8d6a-f8e57b555fe7.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int number) {       | ^~~~~~ In file included from 49170b02-9795-480a-8d6a-f8e57b555fe7.cpp:17: 49170b02-9795-480a-8d6a-f8e57b555fe7.cpp: In function ‘int main()’: 49170b02-9795-480a-8d6a-f8e57b555fe7.cpp:19:13: error: ‘int_to_mini_romank’ was not declared in this scope    19 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,4 +1,5 @@
 #include <string>
+
 string int_to_mini_roman(int number) {
     vector<string> romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     vector<int> decimalValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
