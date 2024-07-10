06735038b33703cb0e7b0e66ecfc8f6222c85ed8
beep boop <githubbot@vadim.me>
2024-07-10 22:55:36 +0200

    Solution 18 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vlwyz493 Prompt: 0.8416666666666667 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5df73815-223b-4930-b762-9c4251e8a b91.py", line 22, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5df73815-223b-4930-b762-9c4251e8a b91.py", line 19, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -1,8 +1,11 @@
 ```
 def fruit_distribution(s, n):
     s = s.lower()
-    fruits = s.replace("and", ",").split(",")
-    total_fruits = sum(1 for fruit in fruits if fruit.split()[0].isdigit())
-    return total_fruits / (n or 1)
+    fruits = s.split("and")
+    for i in range(len(fruits)):
+        if "," in fruits[i]:
+            fruits[i] = fruits[i].replace(",", "")
+    total = sum(int(''.join(fruit.split()[1:]) or '1') for fruit in fruits[:n])
+    return total/n
 
 print(fruit_distribution("1 apples and 100 oranges", 120))
