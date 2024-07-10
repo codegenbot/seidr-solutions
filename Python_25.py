c62b6a78e65f72dbcb85c2643685e0853525be99
beep boop <githubbot@vadim.me>
2024-07-10 09:51:55 +0200

    Solution 94 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/knxcxtbp Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9a491186-448b-493d-83ff-c9c26aa62 c3b.py", line 10, in <module>     check(factorize)           ^^^^^^^^^ NameError: name 'factorize' is not defined

--- Python_25.py
+++ Python_25.py
@@ -1,7 +1,24 @@
-import math
+```
+def factorize(num):
+    i = 2
+    factors = set()
+    while i * i <= num:
+        if num % i:
+            i += 1
+        else:
+            num //= i
+            while num % i == 0:
+                num //= i
+                factors.add(i)
+    if num > 1:
+        factors.add(num)
+    return factors
 
 def check(factorized_num):
     for i in range(2, factorized_num + 1):
-        if set(math.factorize(i)) == set(factorized_num) and i != factorized_num:
+        if set(factorize(i)) == set(factorized_num) and i != factorized_num:
             return str(i)
     return "No such number exists"
+
+num = 32
+print(check(num))
