6c2f78cab3f9eb9047416136fd845ce45c4e4b07
beep boop <githubbot@vadim.me>
2024-07-10 16:25:34 +0200

    Solution 8 of Python/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/i83iao76 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1fb93d1a-0539-485a-bf0d-3f7182e12 753.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_71.py
+++ Python_71.py
@@ -1,17 +1,7 @@
 ```
-import math
-
 def triangle_area(a, b, c):
     if a + b > c and a + c > b and b + c > a:
         s = (a + b + c) / 2
         return round(math.sqrt(s*(s-a)*(s-b)*(s-c)), 2)
     else:
         return -1
-
-side1 = float(input("Enter side 1: "))
-side2 = float(input("Enter side 2: "))
-side3 = float(input("Enter side 3: "))
-
-area = triangle_area(side1, side2, side3)
-
-print(f"The area of the triangle is {area} square units.")
