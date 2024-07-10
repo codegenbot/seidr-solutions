66612b989c3ed651ea8de46d64c70adf8d11c710
beep boop <githubbot@vadim.me>
2024-07-10 20:10:47 +0200

    Solution 50 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/ku2zaqai Prompt: 03f6b8eb-e53c-4e5f-84ed-aa625d33d563.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ 03f6b8eb-e53c-4e5f-84ed-aa625d33d563.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ 03f6b8eb-e53c-4e5f-84ed-aa625d33d563.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,24 +1,27 @@
-int mastermind(std::string code, std::string guess) {
+struct MastermindResult {
+    int blackPegs;
+    int whitePegs;
+};
+
+MastermindResult mastermind(string code, string guess) {
     int blackPegs = 0;
     int whitePegs = 0;
 
-    // Count black pegs
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             blackPegs++;
-            code[i] = ' ';
-            guess[i] = ' ';
         }
     }
 
-    // Count white pegs
-    std::unordered_set<char> codeCopy(code.begin(), code.end());
-    for (int i = 0; i < 4; ++i) {
-        if (codeCopy.count(guess[i]) > 0) {
-            whitePegs++;
-            codeCopy.erase(guess[i]);
+    for (char c : code) {
+        bool found = false;
+        for (int j = 0; j < 4; ++j) {
+            if (guess[j] == c && !found) {
+                found = true;
+            } else if (guess[j] == c) {
+                whitePegs++;
+            }
         }
     }
 
-    return blackPegs << 2 | whitePegs;
-}
+    return {blackPegs, whitePegs};
