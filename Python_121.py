0ec00f474b0b6fa015d77b863009986044955592
beep boop <githubbot@vadim.me>
2024-07-10 17:04:46 +0200

    Solution 22 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/n9sevabm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5cd37e9f-c302-49cc-b9bb-1e8d2dd1e 0a9.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,3 +1,8 @@
-```
 def solution(lst):
-    return sum(i for i in lst if i % 2 != 0)
+    total = 0
+    odd_count = 0
+    for i in lst:
+        if i % 2 != 0:
+            total += i
+            odd_count += 1
+    return f"The average of the odd numbers is {total / odd_count}"
