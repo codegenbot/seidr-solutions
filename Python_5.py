c760bc503cf942feae9ec795e4b896eec50c54d4
beep boop <githubbot@vadim.me>
2024-07-11 00:38:47 +0200

    Solution 70 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/z8pou3iy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d7e3ba7a-abdf-4542-93c0-82acdd573 e50.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_5.py
+++ Python_5.py
@@ -1,5 +1,7 @@
-def intersperse-separated-values(str_values: List[str], separator: str) -> List[str]:
-    result = [str_values[0]]
-    for i in range(1, len(str_values)):
-        result.extend([result[-1], separator, str_values[i]])
+def intersperse_numbers(input_string: str) -> List[int]:
+    numbers = [int(x) for x in input_string.split()]
+    delimeter = input("Enter the delimeter: ")
+    result = [numbers[0]]
+    for i in range(1, len(numbers)):
+        result.extend([result[-1], int(delimeter), numbers[i]])
     return result
