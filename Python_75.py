0899fbb09cab82240ea2e69a279143f99b6a97e3
beep boop <githubbot@vadim.me>
2024-07-10 21:22:07 +0200

    Solution 1 of Python/75, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wwjp9mmb Prompt: Complete the following code given the task description and function signature.

--- Python_75.py
+++ Python_75.py
@@ -8,6 +8,10 @@ def is_multiply_prime(a):
                 return False
         return True
 
-    factors = [i for i in range(2, a+1) if a % i == 0]
-    prime_factors = [f for f in factors if is_prime(f)]
+    factors = []
+    for i in range(2, a+1):
+        while a % i == 0:
+            factors.append(i)
+            a //= i
+    prime_factors = [i for i in factors if is_prime(i)]
     return len(prime_factors) >= 3
