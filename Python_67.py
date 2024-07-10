0db331360bb37df8c6c30d3a3d986ad15f868a39
beep boop <githubbot@vadim.me>
2024-07-10 16:31:28 +0200

    Solution 24 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9wljto1e Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/797ab7d4-335c-4a82-8481-9bf4bda45 455.py", line 12, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/797ab7d4-335c-4a82-8481-9bf4bda45 455.py", line 9, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/797ab7d4-335c-4a82-8481-9bf4bda45 455.py", line 3, in fruit_distribution     apples = int(s[s.index("apples") + 1 :].replace("and", "").replace("apples",  ""))                    ^^^^^^^^^^^^^^^^^ ValueError: 'apples' is not in list

--- Python_67.py
+++ Python_67.py
@@ -1,15 +1,11 @@
-```
 def fruit_distribution(s, n):
     s = " ".join(s).replace("apples and", "apples ").replace("oranges", " oranges").split()
-    
-    if "apples" not in s:
-        apples = 0
-    else:
-        apples = int(' '.join(s[s.index("apples") + 1 :]).replace("and", "").replace("apples", ""))
-        
-    if "oranges" not in s:
-        oranges = 0
-    else:
-        oranges = int(' '.join(s[s.index("oranges") + 7 :]).replace("and", "").replace("oranges", ""))
-    
+    if 'apples' not in s:
+        raise ValueError("Apples not found")
+    if 'oranges' not in s:
+        raise ValueError("Oranges not found")
+
+    apples = int(' '.join(s[s.index('apples') + 1:]).replace("and", "").replace("apples", ""))
+    oranges = int(' '.join(s[s.index('oranges') + 7:]).replace("and", "").replace("oranges", ""))
+
     return n - apples - oranges
