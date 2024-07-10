30a307c6517a18de41bc747fa8394b46cb516478
beep boop <githubbot@vadim.me>
2024-07-10 09:28:54 +0200

    Solution 89 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/fe6sfwwf Prompt: In file included from /usr/include/c++/11/map:61,                  from 1a4190ba-3154-4bb9-9e43-82807acf154a.cpp:1: /usr/include/c++/11/bits/stl_map.h:100:11: note:   ‘std::map’   100 |     class map       |           ^~~ In file included from 1a4190ba-3154-4bb9-9e43-82807acf154a.cpp:1: /usr/include/c++/11/map:78:13: note:   ‘std::pmr::map’    78 |       using map       |             ^~~ 1a4190ba-3154-4bb9-9e43-82807acf154a.cpp:8:33: error: expected primary-expressio n before ‘char’     8 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ 1a4190ba-3154-4bb9-9e43-82807acf154a.cpp:8:38: error: expected primary-expressio n before ‘int’     8 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ In file included from 1a4190ba-3154-4bb9-9e43-82807acf154a.cpp:7: 1a4190ba-3154-4bb9-9e43-82807acf154a.cpp: In function ‘int main()’: 1a4190ba-3154-4bb9-9e43-82807acf154a.cpp:22:20: error: ‘histogram’ was not decla red in this scope    22 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,5 +1,20 @@
 #include <map>
+#include <cassert>
+
+std::map<char, int> histogram(std::string s) {
+    std::map<char, int> hist;
+    for (char c : s) {
+        hist[c]++;
+    }
+    return hist;
+}
 
 bool issame(std::map<char, int> a, std::map<char, int> b) {
     return a == b;
+}
+
+int main() {
+    assert(issame(histogram("a"), {{'a', 1}}));
+    
+    return 0;
 }
