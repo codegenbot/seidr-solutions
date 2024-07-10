428fe50b7938976dacdb4ddb57933ea1c7af0d6c
beep boop <githubbot@vadim.me>
2024-07-10 09:13:30 +0200

    Solution 7 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/wwo0cwud Prompt: 94b4e16f-3495-4bfb-bb23-b0fb2e0e86fa.cpp:44:29: error: ‘string’ was not declared  in this scope    44 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 94b4e16f-3495-4bfb-bb23-b0fb2e0e86fa.cpp:44:29: note: suggested alternatives: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 94b4e16f-3495-4bfb-bb23-b0fb2e0e86fa.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 94b4e16f-3495-4bfb-bb23-b0fb2e0e86fa.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,6 +2,7 @@
 #include <any>
 #include <string>
 #include <algorithm>
+#include <cassert>
 
 std::any compare_one(const std::any& a, const std::any& b){
     if(a.type() == typeid(int) && b.type() == typeid(int)){
