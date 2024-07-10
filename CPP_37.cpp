bbd443ae7828fab64245c2089ec131afed1ea4d8
beep boop <githubbot@vadim.me>
2024-07-10 02:14:38 +0200

    Solution 44 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9939gev7 Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e178e2d7-fd58-433b-884d-c33c9122016a.cpp:42:36: error: expected primary-expressi on before ‘float’    42 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ e178e2d7-fd58-433b-884d-c33c9122016a.cpp:42:43: error: expression list treated a s compound expression in initializer [-fpermissive]    42 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from e178e2d7-fd58-433b-884d-c33c9122016a.cpp:41: e178e2d7-fd58-433b-884d-c33c9122016a.cpp: In function ‘int main()’: e178e2d7-fd58-433b-884d-c33c9122016a.cpp:51:20: error: ‘sort_even’ was not decla red in this scope; did you mean ‘sortEven’?    51 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ e178e2d7-fd58-433b-884d-c33c9122016a.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<float> v1, std::vector<float> v2) {
+bool isSame(std::vector<float> v1, std::vector<float> v2) {
     if(v1.size() != v2.size()) return false;
     for(int i = 0; i < v1.size(); i++) {
         if(v1[i] != v2[i]) return false;
@@ -34,6 +34,6 @@ int main_function()
     for(float f : output) {
         std::cout << f << " ";
     }
-    assert(issame(output, {5, 8, -12, 4, 23, 2, 3, 11, 12, -10}));
+    assert(isSame(output, {5.0f, 8.0f, -12.0f, 4.0f, 23.0f, 2.0f, 3.0f, 11.0f, 12.0f, -10.0f}));
     return 0;
 }
