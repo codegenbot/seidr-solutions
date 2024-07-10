a8fbc92c1f4204587fd01fa01e9b31e5769267b7
beep boop <githubbot@vadim.me>
2024-07-10 23:14:38 +0200

    Solution 64 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k7a6o2at Prompt: a022ce9e-06b4-46d0-938d-e88dbb135f1a.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ a022ce9e-06b4-46d0-938d-e88dbb135f1a.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -8,20 +8,7 @@ int skjkasdkd(const std::vector<int>& vec) {
         sum += num;
     }
     return sum;
+
 }
 
-int main() {
-    std::vector<int> lst; 
-    int n; 
-    std::cout << "Enter the number of elements: ";
-    std::cin >> n; 
-    for(int i=0;i<n;i++) {
-        int x; 
-        std::cout << "Enter element #"<<i+1<<":";
-        std::cin >> x;
-        lst.push_back(x);
-    } 
-    int result = skjkasdkd(lst); 
-    std::cout << "The sum is: " << result << std::endl; 
-    return 0;
-}
+std::cout << "The sum is: " << skjkasdkd({127, 97, 8192}) << std::endl;
