2855b4edd5dac2b42e89da09823f0bb7abd49422
beep boop <githubbot@vadim.me>
2024-07-10 20:58:40 +0200

    Solution 9 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wj4ug1w3 Prompt: cb862277-e4bb-4f4c-b477-c7b090f16058.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int count_nums(vector<int> nums) {       |                ^~~~~~ cb862277-e4bb-4f4c-b477-c7b090f16058.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int count_nums(vector<int> nums) {       |                       ^~~ In file included from cb862277-e4bb-4f4c-b477-c7b090f16058.cpp:21: cb862277-e4bb-4f4c-b477-c7b090f16058.cpp: In function ‘int main()’: cb862277-e4bb-4f4c-b477-c7b090f16058.cpp:23:23: error: ‘count_nums’ cannot be us ed as a function    23 |     assert (count_nums({1}) == 1);       |             ~~~~~~~~~~^~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,10 +1,10 @@
 #include <vector>
 
-int count_nums(std::vector<int> nums) {
+int count_nums(vector<int> nums) {
     int count = 0;
     for (int num : nums) {
         bool has_positive_sum = false;
-        long abs_num = std::abs(num);
+        long abs_num = labs(num);
         while (abs_num > 0) {
             int digit = abs_num % 10;
             if (digit != 0 || num < 0 && digit == -1) {
@@ -18,3 +18,4 @@ int count_nums(std::vector<int> nums) {
         }
     }
     return count;
+}
