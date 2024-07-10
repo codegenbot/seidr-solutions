f580772c14bdd734225587a252092453a010b13b
beep boop <githubbot@vadim.me>
2024-07-10 03:46:48 +0200

    Solution 57 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/785e81cu Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f9b3c6d9-0ce0-4db2-8c51-21d003e98fc4.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f9b3c6d9-0ce0-4db2-8c51-21d003e98fc4.cpp:19: f9b3c6d9-0ce0-4db2-8c51-21d003e98fc4.cpp: In function ‘int main()’: f9b3c6d9-0ce0-4db2-8c51-21d003e98fc4.cpp:29:20: error: ‘rolling_max’ was not dec lared in this scope; did you mean ‘cpp9::rolling_max’?    29 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ f9b3c6d9-0ce0-4db2-8c51-21d003e98fc4.cpp:10:22: note: ‘cpp9::rolling_max’ declar ed here    10 |     std::vector<int> rolling_max(const std::vector<int>& nums) {       |                      ^~~~~~~~~~~ In file included from f9b3c6d9-0ce0-4db2-8c51-21d003e98fc4.cpp:19: f9b3c6d9-0ce0-4db2-8c51-21d003e98fc4.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,3 +1,24 @@
-bool issame(const std::std::vector<int>& a, const std::std::vector<int>& b) {
-    return std::equal(a.begin(), a.end(), b.begin());
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+namespace cpp9 {
+    bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+        return std::equal(a.begin(), a.end(), b.begin());
+    }
+
+    std::vector<int> rolling_max(const std::vector<int>& nums) {
+        std::vector<int> result;
+        for (size_t i = 0; i < nums.size(); ++i) {
+            result.push_back(*std::max_element(nums.begin() + i, nums.begin() + std::min(i + 3, static_cast<int>(nums.size()))));
+        }
+        return result;
+    }
+}
+
+int main() {
+    std::vector<int> nums = {1, 3, 5, 2, 7, 4, 8};
+    std::vector<int> max_values = cpp9::rolling_max(nums);
+
+    return 0;
 }
