cb22c80528bb2414ded869fe58a3175929a28e91
beep boop <githubbot@vadim.me>
2024-07-10 16:18:37 +0200

    Solution 8 of Python/56, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uq8mtcbt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f86423a5-1a6b-430e-8431-6fb7f9820 058.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_56.py
+++ Python_56.py
@@ -1,11 +1,11 @@
-```
 def correct_bracketing(brackets: str):
     stack = []
     for bracket in brackets:
         if bracket == "<":
-            stack.append(bracket)
+            stack.append("<")
         elif bracket == ">":
             if len(stack) == 0:
                 return False
-            stack.pop()
+            if stack.pop() != "<":
+                return False
     return len(stack) == 0
