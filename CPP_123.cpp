9075290e84a211b8176f521a8d4743b3fb3f2531
beep boop <githubbot@vadim.me>
2024-07-10 08:10:30 +0200

    Solution 70 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0cgr7m6b Prompt:       |                    ^~~ f58b787d-f047-4e46-894e-93129d29777a.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f58b787d-f047-4e46-894e-93129d29777a.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f58b787d-f047-4e46-894e-93129d29777a.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f58b787d-f047-4e46-894e-93129d29777a.cpp:19: f58b787d-f047-4e46-894e-93129d29777a.cpp: In function ‘int main()’: f58b787d-f047-4e46-894e-93129d29777a.cpp:29:20: error: ‘get_odd_collatz’ was not  declared in this scope    29 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ f58b787d-f047-4e46-894e-93129d29777a.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,18 +1,21 @@
+```cpp
+#include <iostream>
 #include <vector>
-#include <algorithm>
-
-bool issame(vector<int> a, vector<int> b);
-vector<int> get_odd_collatz(int n);
+using namespace std;
 
 bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+    if (a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) return false;
+    }
+    return true;
 }
 
 vector<int> get_odd_collatz(int n) {
     vector<int> result;
     while (n != 1) {
         if (n % 2 == 0) {
-            n = n / 2;
+            n /= 2;
         } else {
             n = 3 * n + 1;
         }
@@ -22,4 +25,17 @@ vector<int> get_odd_collatz(int n) {
     }
     sort(result.begin(), result.end());
     return result;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    vector<int> res = get_odd_collatz(n);
+    if(issame({1}, res)) {
+        cout << "The sequence is Collatz Conjecture sequence." << endl;
+    } else {
+        cout << "The sequence is not Collatz Conjecture sequence." << endl;
+    }
+    return 0;
 }
