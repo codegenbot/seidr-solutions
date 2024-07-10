405528bd75aef05156f7a716170f53d482665a6a
beep boop <githubbot@vadim.me>
2024-07-10 02:02:55 +0200

    Solution 10 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0hw5ynmd Prompt: td::vector<int>, std::vector<int>)’: 1f51b826-fecc-46c2-8d14-cc701010ecea.cpp:25:15: error: ‘set_intersection’ is not  a member of ‘std’    25 |     copy(std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end( ), back_inserter(result)));       |               ^~~~~~~~~~~~~~~~ 1f51b826-fecc-46c2-8d14-cc701010ecea.cpp: At global scope: 1f51b826-fecc-46c2-8d14-cc701010ecea.cpp:35:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    35 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 1f51b826-fecc-46c2-8d14-cc701010ecea.cpp:8:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     8 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 1f51b826-fecc-46c2-8d14-cc701010ecea.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 1f51b826-fecc-46c2-8d14-cc701010ecea.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,24 +1,33 @@
+```
 #include <iostream>
 #include <vector>
 #include <set>
 
 using namespace std;
 
+bool isSame(vector<int> a, vector<int> b) {
+    if(a.size() != b.size()) {
+        return false;
+    }
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
-    vector<int> result;
-    for(auto it = set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end()), end = set_intersection(s1.end(), s1.end(), s2.end(), s2.end()); it != end; ++it) {
-        result.push_back(*it);
-    }
-    
+    vector<int> result(s1.size() < s2.size() ? s1.size() : s2.size());
+    auto it = std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), result.begin());
+    result.resize(std::distance(result.begin(), it));
+
     return result;
 }
 
 int main() {
-    vector<int> l1 = {4, 3, 2, 8};
-    vector<int> l2 = {};
-    assert(issame(common(l1, l2), l2));
-    return 0;
+    assert(isSame(common({4, 3, 2, 8}, {}), {}));
 }
