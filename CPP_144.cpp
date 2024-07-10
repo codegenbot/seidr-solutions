2f68e6876ebc449a26714d19ecc2bf7580cadaae
beep boop <githubbot@vadim.me>
2024-07-10 11:51:07 +0200

    Solution 27 of CPP/144, ratio 0.9230769230769231 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/o1pkvjf1 Prompt: In file included from bf02863a-0c1c-44f2-b58c-300547e98aff.cpp:3: bf02863a-0c1c-44f2-b58c-300547e98aff.cpp: In function ‘int main()’: bf02863a-0c1c-44f2-b58c-300547e98aff.cpp:5:13: error: ‘simplify’ was not declare d in this scope     5 |     assert (simplify("1/5", "1/5") == false);       |             ^~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,24 +1,5 @@
 #include <string>
-#include <algorithm>
 
-bool checkDivisibility(std::string x, std::string n) {
-    int num1 = stoi(x.substr(0, x.find('/')));
-    int den1 = stoi(x.substr(x.find('/') + 1));
-    int num2 = stoi(n.substr(0, n.find('/')));
-    int den2 = stoi(n.substr(n.find('/') + 1));
-
-    return (num1 * num2) % (den1 * den2) == 0;
-}
-
-bool simplify(std::string x, std::string n) {
-    int num1 = stoi(x.substr(0, x.find('/')));
-    int den1 = stoi(x.substr(x.find('/') + 1));
-    int num2 = stoi(n.substr(0, n.find('/')));
-    int den2 = stoi(n.substr(n.find('/') + 1));
-
-    int gcd = std::__gcd(num1 * den2, num2 * den1);
-    num1 /= gcd;
-    den1 /= gcd;
-
-    return num1 == num2 && den1 == den2;
+bool simplify(std::string str1, std::string str2) {
+    return str1 == str2;
 }
