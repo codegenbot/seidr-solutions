a293bd27ecfaaa13d99c969f19179e552303d8e7
beep boop <githubbot@vadim.me>
2024-07-10 19:38:24 +0200

    Solution 64 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/e4xegzqk Prompt:       |                    ^~~ ea2160ee-f365-4c9a-adc5-5b789bc0b5bd.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ea2160ee-f365-4c9a-adc5-5b789bc0b5bd.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ea2160ee-f365-4c9a-adc5-5b789bc0b5bd.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ea2160ee-f365-4c9a-adc5-5b789bc0b5bd.cpp:13: ea2160ee-f365-4c9a-adc5-5b789bc0b5bd.cpp: In function ‘int main()’: ea2160ee-f365-4c9a-adc5-5b789bc0b5bd.cpp:23:20: error: ‘get_odd_collatz’ was not  declared in this scope    23 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ ea2160ee-f365-4c9a-adc5-5b789bc0b5bd.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,12 +1,20 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(int a, int b) {
-    return a == b;
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-std::vector<int> get_odd_collatz(int n) {
-    std::vector<int> result;
+vector<int> get_odd_collatz(int n) {
+    vector<int> result;
     while (n != 1) {
         if (n % 2 == 1) {
             result.push_back(n);
@@ -14,6 +22,6 @@ std::vector<int> get_odd_collatz(int n) {
         n = (n % 2 == 0) ? n / 2 : 3 * n + 1;
     }
     result.push_back(1);
-    std::sort(result.begin(), result.end());
+    sort(result.begin(), result.end());
     return result;
 }
