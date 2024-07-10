94c5e1dceb4afac3f65dce715e237435083457e5
beep boop <githubbot@vadim.me>
2024-07-10 23:08:20 +0200

    Solution 55 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lgdurmsn Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/be7a3bc0-0990-4246-8c8e-278fa1adf 2c9.py", line 18, in <module>     check(solution)           ^^^^^^^^ NameError: name 'solution' is not defined

--- Python_121.py
+++ Python_121.py
@@ -1,2 +1,5 @@
-```
-solution = sum(int(i) for i in input("Enter the elements of a list separated by space: ").split()[1::2] if int(i) % 2 != 0)
+solution = sum(
+    i
+    for i in input("Enter the elements of a list separated by space: ").split()[1::2]
+    if int(i) % 2 != 0
+)
