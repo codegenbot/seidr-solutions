a2dfbd4a35156e7ca8d9324d688ff12153f50f92
beep boop <githubbot@vadim.me>
2024-07-10 19:36:36 +0200

    Solution 56 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/eixmci7m Prompt: /usr/include/assert.h:92: note: macro "assert" defined here    92 | #  define assert(expr) \       | 24bccd4c-dfbb-46f3-bfce-30caa1b4d795.cpp: In function ‘int main()’: 24bccd4c-dfbb-46f3-bfce-30caa1b4d795.cpp:30:5: error: ‘assert’ was not declared in this scope    30 |     assert(unique_digits({135, 103, 31}) == vector<int>{31, 135});       |     ^~~~~~ 24bccd4c-dfbb-46f3-bfce-30caa1b4d795.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <cassert>   +++ |+#include <cassert>     5 | 24bccd4c-dfbb-46f3-bfce-30caa1b4d795.cpp: At global scope: 24bccd4c-dfbb-46f3-bfce-30caa1b4d795.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ 24bccd4c-dfbb-46f3-bfce-30caa1b4d795.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -27,7 +27,7 @@ vector<int> unique_digits(vector<int> x) {
 }
 
 int main() {
-    std::assert(unique_digits({135, 103, 31}) == vector<int>{31, 135});
+    assert(unique_digits({135, 103, 31}) == vector<int>{31, 135});
     cout << "Test Passed!" << endl;
     return 0;
 }
