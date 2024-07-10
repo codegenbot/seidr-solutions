d5287899f6fb4db110b6830381fa67672f0e3fd4
beep boop <githubbot@vadim.me>
2024-07-10 04:23:28 +0200

    Solution 22 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/nns7oxk8 Prompt:       |                           ^~~~~~ 8e28f5bc-0f2c-4a74-b300-a2f70c4e96c3.cpp:26:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 8e28f5bc-0f2c-4a74-b300-a2f70c4e96c3.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 8e28f5bc-0f2c-4a74-b300-a2f70c4e96c3.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8e28f5bc-0f2c-4a74-b300-a2f70c4e96c3.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8e28f5bc-0f2c-4a74-b300-a2f70c4e96c3.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 8e28f5bc-0f2c-4a74-b300-a2f70c4e96c3.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -5,7 +5,7 @@
 
 std::vector<int> sort_third(std::vector<int> l) {
     std::vector<int> l_copy = l;
-    for (int i = 0; i < l.size(); i++) {
+    for (size_t i = 0; i < l.size(); i++) {
         if ((i + 1) % 3 == 0) {
             std::sort(l_copy.begin() + i - 2, l_copy.begin() + i + 1);
         }
@@ -18,6 +18,6 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 }
 
 int main() {
-    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {3, 6, 2, 4, 8, 9, 5, 1}));
+    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
     return 0;
 }
