a81a3eb6efbe9f912db786547c5beaec9f915c29
beep boop <githubbot@vadim.me>
2024-07-10 23:14:39 +0200

    Solution 81 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tbcv99fp Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/78057110-ddb5-40a8-a57d-5eb99253a 143.py", line 1     Here is the corrected code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,4 +1,4 @@
-```
+````
 def parse_music(music_string: str) -> list:
     music_list = []
     i = 0
@@ -11,23 +11,22 @@ def parse_music(music_string: str) -> list:
             continue
         elif i < len(music_string) - 3:
             if music_string[i:i+3] in ['...||', '.||.', '..|.']:
-                if music_string[i:i+3] == '..||':
-                    music_list.append(0.5)
-                else:
-                    music_list.append(1)
+                music_list.append(1)
                 i += 3
             elif (music_string[i:i+2] in ['||', '|.'] and music_string[i+2] == '.') or (music_string[i:i+2] in ['..|', '.|.'] and music_string[i+2] == 'o'):
                 if music_string[i:i+2] in ['||', '|.']:
                     music_list.append(0.5)
+                    i += 2
                 else:
                     music_list.append(1)
-                i += 2
+                    i += 2
             elif (music_string[i:i+2] in ['..|', '.|.'] and music_string[i+2] == 'o') or (music_string[i:i+3] in ['...||']):
                 if music_string[i:i+3] == '..||':
                     music_list.append(0.5)
+                    i += 3
                 else:
                     music_list.append(1)
-                i += 3
+                    i += 3
             else:
                 music_list.append(1)
                 i += 1
