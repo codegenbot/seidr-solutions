6c04efe647d5f7fd4b8d0a66cdffb54baecf2913
beep boop <githubbot@vadim.me>
2024-07-10 16:25:38 +0200

    Solution 5 of Python/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/s445qvhi Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf62cf8e-ba1b-4e2d-bf22-667623170 46e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_74.py
+++ Python_74.py
@@ -1,11 +1,10 @@
-```
 def total_match(lst1, lst2):
     sum_chars_lst1 = sum(len(x) for x in lst1)
     sum_chars_lst2 = sum(len(x) for x in lst2)
-    
+
     if sum_chars_lst1 < sum_chars_lst2:
-        return 'List 1 has fewer characters.'
+        return (lst1, "list 1 has fewer characters")
     elif sum_chars_lst1 > sum_chars_lst2:
-        return 'List 2 has fewer characters.'
+        return (lst2, "list 2 has more characters")
     else:
-        return 'Both lists have the same number of characters.'
+        return (lst1, "lists are equal")
