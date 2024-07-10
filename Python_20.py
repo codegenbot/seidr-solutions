d91dece05a2b50fa5d167e262d4e40f376325770
beep boop <githubbot@vadim.me>
2024-07-10 05:05:02 +0200

    Solution 73 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9z8gtv5q Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e460d772-e2c5-4e47-804c-258f88b73 150.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_20.py
+++ Python_20.py
@@ -1,11 +1,10 @@
 def check(lst):
     if len(set(lst)) == 1:
         return "All elements are equal"
-    elif len(lst) != len(set(lst)):
-        return "Not all elements are unique"
-    else:
+    elif len(lst) == len(set(lst)):
         return "All elements are unique"
-
+    else:
+        return "Some elements are equal and some are not"
 
 lst = list(map(int, input().split()))
 print(check(lst))
