c4cd1e6ea28cedfbfc7808361b5435684a7f9a33
beep boop <githubbot@vadim.me>
2024-07-10 22:17:17 +0200

    Solution 81 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/mma41iux Prompt: c9eed3ec-a169-498e-98b0-2e427633c1c6.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ c9eed3ec-a169-498e-98b0-2e427633c1c6.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ c9eed3ec-a169-498e-98b0-2e427633c1c6.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ c9eed3ec-a169-498e-98b0-2e427633c1c6.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ c9eed3ec-a169-498e-98b0-2e427633c1c6.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,4 +1,3 @@
-```
 #include <iostream>
 #include <sstream>
 #include <unordered_map>
@@ -36,3 +35,4 @@ int main() {
     std::string result = int_to_mini_romank(num);
     std::cout << "Roman numeral representation: " << result << std::endl;
     return 0;
+}
