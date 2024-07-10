af4a35fb9f89b15877efef4373d182dc5639470d
beep boop <githubbot@vadim.me>
2024-07-10 02:06:01 +0200

    Solution 54 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ul4hcvmg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b72016f8-22a8-4f7c-a503-cd7f5b12d 88f.py", line 27, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b72016f8-22a8-4f7c-a503-cd7f5b12d 88f.py", line 24, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -1,17 +1,16 @@
 def minPath(grid, k):
     n = len(grid)
-    visited = set((0, 0))
+    visited = [[False] * n for _ in range(n)]
     res = []
 
     def dfs(i, j, path):
-        if i == n - 1 and j == n - 1:
+        if (i, j) in [(0, 0), (n - 1, n - 1)]:
             return path
         for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
-            if 0 <= x < n and 0 <= y < n and (x, y) not in visited and (x, y) != (i, j):
-                visited.add((x, y))
+            if 0 <= x < n and 0 <= y < n and not visited[x][y]:
+                visited[x][y] = True
                 res.append(dfs(x, y, path + [grid[x][y]]))
-                visited.remove((x, y))
-
+                visited[x][y] = False
         return min(res) if res else []
 
     return dfs(0, 0, [grid[0][0]])
