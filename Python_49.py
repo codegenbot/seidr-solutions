6b5f11463e996d0c809f5155e65861587210325a
beep boop <githubbot@vadim.me>
2024-07-10 21:48:28 +0200

    Solution 48 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ujpaxwlx Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2c7249dc-1193-453c-a25c-90df0cc09 c4d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,4 +1,7 @@
 def modp(n: int, p: int):
     n = abs(n)
     p = abs(p)
-    return n % p
+    if p == 0:
+        return "Error! Division by zero is not allowed."
+    else:
+        return n % p
