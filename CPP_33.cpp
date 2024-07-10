4ab7db6fe2f3c665c0078813f4c0a9d3eb65054c
beep boop <githubbot@vadim.me>
2024-07-10 03:37:40 +0200

    Solution 2 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/nns7oxk8 Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6e397771-4044-49be-81e7-af41df7575e6.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6e397771-4044-49be-81e7-af41df7575e6.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6e397771-4044-49be-81e7-af41df7575e6.cpp:10: 6e397771-4044-49be-81e7-af41df7575e6.cpp: In function ‘int main()’: 6e397771-4044-49be-81e7-af41df7575e6.cpp:20:20: error: ‘sort_third’ was not decl ared in this scope    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 6e397771-4044-49be-81e7-af41df7575e6.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -3,21 +3,20 @@
 #include <algorithm>
 #include <cassert>
 
-std::vector<int> sort_third(std::vector<int> l) {
-    std::vector<int> l_copy = l;
+using namespace std;
+
+vector<int> sort_third(vector<int> l) {
+    vector<int> l_copy = l;
     for (int i = 0; i < l.size(); i++) {
         if ((i + 1) % 3 == 0) {
-            std::sort(l_copy.begin() + i - 2, l_copy.begin() + i + 1);
+            sort(l_copy.begin() + i - 2, l_copy.begin() + i + 1);
         }
     }
     return l_copy;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
 int main() {
-    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
+    assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
+    
     return 0;
 }
