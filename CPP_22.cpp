160682eb580e42dc6af179ceb12dbad4abbe8363
beep boop <githubbot@vadim.me>
2024-07-10 16:44:26 +0200

    Solution 71 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9x5hjcql Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8a919e1b-3922-4771-a130-613c8dae9e5d.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8a919e1b-3922-4771-a130-613c8dae9e5d.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8a919e1b-3922-4771-a130-613c8dae9e5d.cpp:11: 8a919e1b-3922-4771-a130-613c8dae9e5d.cpp: In function ‘int main()’: 8a919e1b-3922-4771-a130-613c8dae9e5d.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 8a919e1b-3922-4771-a130-613c8dae9e5d.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,34 +1,19 @@
-#include <iostream>
 #include <vector>
 #include <list>
 #include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
+bool issame(vector<int> a, vector<int> b) {
+    return (a.size() == b.size()) && equal(a.begin(), a.end(), b.begin());
 }
 
-std::vector<int> filter_integers(std::list<boost::any>& values) {
-    std::vector<int> result;
+vector<int> filter_integers(list<any> values) {
+    vector<int> result;
     for (const auto& value : values) {
-        if (boost::any_cast<int>(value) != 0 && boost::any_cast<int>(value) > 5) {
-            result.push_back(boost::any_cast<int>(value));
+        if (!value.type().is_stateless_any_castable_to<int>()) continue;
+        int num = boost::any_cast<int>(value);
+        if (num != 0) {
+            result.push_back(num);
         }
     }
     return result;
-}
-
-int main() {
-    std::list<boost::any> values = {{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}};
-    std::vector<int> filtered_values = filter_integers(values);
-    
-    if (!filtered_values.empty()) {
-        for (int value : filtered_values) {
-            std::cout << value << " ";
-        }
-        std::cout << std::endl;
-    } else {
-        std::cout << "No values to display." << std::endl;
-    }
-    
-    return 0;
 }
