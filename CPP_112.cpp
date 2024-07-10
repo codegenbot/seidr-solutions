c0b28e7739dbf6a31a96c236015544f335ea7525
beep boop <githubbot@vadim.me>
2024-07-10 07:54:06 +0200

    Solution 68 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/s6j49414 Prompt:       |                              ^~~~~~ 029b9c7e-38ae-426f-82a2-0c2e17d1fb3c.cpp:41:37: error: ‘string’ was not declared  in this scope    41 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 029b9c7e-38ae-426f-82a2-0c2e17d1fb3c.cpp:41:44: error: ‘b’ was not declared in t his scope    41 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 029b9c7e-38ae-426f-82a2-0c2e17d1fb3c.cpp:41:45: error: expression list treated a s compound expression in initializer [-fpermissive]    41 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 029b9c7e-38ae-426f-82a2-0c2e17d1fb3c.cpp:40: 029b9c7e-38ae-426f-82a2-0c2e17d1fb3c.cpp: In function ‘int main()’: 029b9c7e-38ae-426f-82a2-0c2e17d1fb3c.cpp:50:20: error: ‘reverse_delete’ was not declared in this scope    50 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 029b9c7e-38ae-426f-82a2-0c2e17d1fb3c.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -7,17 +7,15 @@ for (char ch : s) {
             break;
         }
     }
-
+    
     if (!found) {
         result.push_back(string(1, ch));
     }
 }
-
 string resStr = "";
 for (int i = 0; i < result.size(); i++) {
     resStr += result[i];
 }
-
 bool isPalindrome = true;
 int start = 0, end = resStr.length() - 1;
 while (start < end) {
@@ -28,6 +26,5 @@ while (start < end) {
     start++;
     end--;
 }
-
 result.push_back(isPalindrome ? "True" : "False");
 return result;
