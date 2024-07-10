060a28e2788006e091881a668a64686049ab03d0
beep boop <githubbot@vadim.me>
2024-07-10 17:30:26 +0200

    Solution 83 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/f29u7dju Prompt: it must return 3 for input 33656, but it returns .

--- paired-digits.py
+++ paired-digits.py
@@ -1,6 +1,2 @@
 def paired_digits(s):
-    return sum(
-        int(digit)
-        for i, digit in enumerate(s)
-        if int(digit) == int(s[i + 1]) or (digit == s[i + 1])
-    )
+    return sum(int(digit) for i, digit in enumerate(s) if int(digit) == int(s[i+1]) or (digit == s[i+1]))
