25fdc094a29f35c89e39efad8a763b0aa8cd7ff7
beep boop <githubbot@vadim.me>
2024-07-10 04:47:50 +0200

    Solution 49 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9mcoota0 Prompt:       |                      ^~~~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘typename __gnu_cxx ::__enable_if<std::__is_char<_Tp>::__value, bool>::__type std::operator==(const std::__cxx11::basic_string<_CharT>&, const std::__cxx11::basic_string<_CharT>&) [with _CharT = char; typename __gnu_cxx::__enable_if<std::__is_char<_Tp>::__valu e, bool>::__type = bool]’: /usr/include/c++/11/bits/basic_string.h:6305:22:   required from ‘bool std::oper ator!=(const std::__cxx11::basic_string<_CharT, _Traits, _Allocator>&, const std ::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _Tra its = std::char_traits<char>; _Alloc = std::allocator<char>]’ 8caf845f-7de9-4db1-8e4a-ed5deec65f47.cpp:12:23:   required from here /usr/include/c++/11/bits/basic_string.h:6236:21: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6236 |     { return (__lhs.size() == __rhs.size()       |               ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h:6236:37: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6236 |     { return (__lhs.size() == __rhs.size()       |                               ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h:6238:59: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6238 |                                                     __lhs.size())); }       |                                                     ~~~~~~^~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,5 +1,8 @@
 ```cpp
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 bool issame(vector<string> a, vector<string> b) {
@@ -12,14 +15,19 @@ bool issame(vector<string> a, vector<string> b) {
     return true;
 }
 
+string to_string(int i) {
+    ostringstream oss;
+    oss << i;
+    return oss.str();
+}
+
 string reverse_delete(string s1, string s2) {
     int len1 = s1.length();
     int len2 = s2.length();
     for(int i = 0; i < len2; i++) {
-        auto pos = s1.find(string(1, s2[i]));
+        auto pos = s1.find(to_string(s2[i]).c_str());
         if(pos != string::npos) {
-            std::string::iterator pos_iter = s1.begin() + pos;
-            s1.erase(pos_iter);
+            s1 = s1.substr(0, pos) + s1.substr(pos + 1);
         }
     }
     reverse(s1.begin(), s1.end());
@@ -37,5 +45,5 @@ int main() {
     else
         cout << "Second number is greater." << endl;
 
-    assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
+    assert(issame(vector<string>(reverse_delete("mamma", "mia").begin(), reverse_delete("mamma", "mia").end()), {"", "True"}));
 }
