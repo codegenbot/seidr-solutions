df1b93127209d41c9b9b69e7ed78e95003121d4a
beep boop <githubbot@vadim.me>
2024-07-10 10:44:27 +0200

    Solution 41 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/51eh1c3h Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,47 +1,50 @@
 #include <boost/any.hpp>
 #include <string>
+#include <algorithm>
 
-using namespace boost;
+using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
         return b;
-    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
         return b;
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string strA = any_cast<string>(a);
-        string strB = any_cast<string>(b);
-
-        size_t pos = strA.find(",");
-        if (pos != string::npos) {
-            strA = strA.substr(0, pos).erase(pos - 1);
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if (stod(a.convert_to<string>()) > stod(b.convert_to<string>())) {
+            return a;
         }
-
-        pos = strB.find(",");
-        if (pos != string::npos) {
-            strB = strB.substr(0, pos).erase(pos - 1);
+        else if (stod(a.convert_to<string>()) < stod(b.convert_to<string>())) {
+            return b;
         }
-
-        return (stod(strA) > stod(strB)) ? a : b;
-    } else if (a.type() == typeid(string) && (b.type() == typeid(int) || b.type() == typeid(double))) {
-        string str = any_cast<string>(a);
-
-        size_t pos = str.find(",");
-        if (pos != string::npos) {
-            str = str.substr(0, pos).erase(pos - 1);
+        else {
+            return boost::any("None");
         }
-
-        return (stod(str) > any_cast<double>(b)) ? a : b;
-    } else if ((a.type() == typeid(int) || a.type() == typeid(double)) && b.type() == typeid(string)) {
-        string str = any_cast<string>(b);
-
-        size_t pos = str.find(",");
-        if (pos != string::npos) {
-            str = str.substr(0, pos).erase(pos - 1);
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        if (get<int>(a) > get<int>(b)) {
+            return a;
         }
-
-        return (any_cast<double>(a) > stod(str)) ? a : b;
-    } else {
-        return "None";
+        else if (get<int>(a) < get<int>(b)) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        if (get<float>(a) > get<float>(b)) {
+            return a;
+        }
+        else if (get<float>(a) < get<float>(b)) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
+    }
+    else {
+        return boost::any("None");
     }
 }
