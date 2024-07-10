17408f308f10c7b2db17a8e05640a29a2227aa8c
beep boop <githubbot@vadim.me>
2024-07-10 20:35:12 +0200

    Solution 57 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5voqime0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2db00054-e5a3-4f04-a356-58069fc1f 36a.py", line 1     ````     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,10 +1,6 @@
 from typing import List
 
 def filter_integers() -> List[int]:
-    """
-This function filters out integers from user input and returns them as a list.
-It will continue to prompt the user until 'stop' is entered, then it breaks out of the loop.
-"""
     values = []
     while True:
         value = input("Enter an integer (or 'stop' to finish): ")
