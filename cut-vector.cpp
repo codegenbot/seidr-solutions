fcd0ac47dc8abf372410658f33e62e0392c887d9
beep boop <githubbot@vadim.me>
2024-07-10 20:21:34 +0200

    Solution 19 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/kfejmui6 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -2,40 +2,19 @@
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int> v) {
-    int min_diff = INT_MAX;
-    int cut_index = -1;
+    int n = v.size();
+    vector<vector<int>> res;
     
-    for (int i = 0; i < v.size() - 1; i++) {
-        int left_sum = 0, right_sum = 0;
-        
-        for (int j = 0; j <= i; j++) {
-            left_sum += v[j];
-        }
-        
-        for (int j = i + 1; j < v.size(); j++) {
-            right_sum += v[j];
-        }
-        
-        int diff = abs(left_sum - right_sum);
-        
-        if (diff < min_diff) {
-            min_diff = diff;
-            cut_index = i;
+    for (int i = 1; i <= n; i++) {
+        if (i == n || v[i] - v[0] > v[n-1] - v[i-1]) {
+            res.push_back({v[0], v[i-1]});
+            break;
         }
     }
     
-    vector<vector<int>> result(2);
-    result[0].resize(cut_index + 1);
-    for (int i = 0; i <= cut_index; i++) {
-        result[0].push_back(v[i]);
-    }
+    res.push_back({v.back()});
     
-    result[1].resize(v.size() - cut_index - 1);
-    for (int i = cut_index + 1; i < v.size(); i++) {
-        result[1].push_back(v[i]);
-    }
-    
-    return result;
+    return res;
 }
 
 int main() {
@@ -45,20 +24,12 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> v[i];
     }
-    
-    vector<vector<int>> res = cutVector(v);
-    
-    cout << "[";
-    for (int i = 0; i < res[0].size(); i++) {
-        cout << res[0][i] << " ";
+    vector<vector<int>> result = cutVector(v);
+    for (auto &vec : result) {
+        for (int num : vec) {
+            cout << num << " ";
+        }
+        cout << endl;
     }
-    cout << "] [" << endl;
-    
-    cout << "[";
-    for (int i = 0; i < res[1].size(); i++) {
-        cout << res[1][i] << " ";
-    }
-    cout << "]" << endl;
-    
     return 0;
 }
