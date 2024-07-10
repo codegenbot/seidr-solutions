e1d24b19ae5d9070cf5b3c555d19a0a726af30b7
beep boop <githubbot@vadim.me>
2024-07-11 00:06:14 +0200

    Solution 49 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/g0ftvo4y Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/16d618f3-f489-4620-be6d-320fa80ce ea8.py", line 23, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/16d618f3-f489-4620-be6d-320fa80ce ea8.py", line 19, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -6,7 +6,7 @@ def encode(message):
             ascii_offset = 97 if char.islower() else 65
             if char.lower() in 'aeiou':
                 result += chr((ord(char.lower()) - ascii_offset + 3) % 26 + ascii_offset)
-                if char.isupper():
+                if char.isupper() and char.lower() in 'aeiou':
                     result += char.upper()
             else:
                 result += chr((ord(char) - ascii_offset + 3) % 26 + ascii_offset)
