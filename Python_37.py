7d92fe33c2c40184b25e1ccf6af6939afe999c6c
beep boop <githubbot@vadim.me>
2024-07-10 20:22:02 +0200

    Solution 70 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uquygx2d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9f1c98eb-fee8-4ab7-97f1-dab843dd4 b73.py", line 26, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9f1c98eb-fee8-4ab7-97f1-dab843dd4 b73.py", line 21, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,13 +1,12 @@
-```
 def sort_even(l: list):
-    even = sorted([x for x in l if x % 2 == 0])
+    even = sorted([abs(x) for x in l if x % 2 == 0])
     odd = sorted([x for x in l if x % 2 != 0])
 
     result = []
     i, j = 0, 0
     while i < len(even) or j < len(odd):
         if i < len(even) and (j >= len(odd) or even[i] <= odd[j]):
-            result.append(even[i])
+            result.append(-1*even[i] if even[i] < 0 else even[i])
             i += 1
         elif j < len(odd): 
             result.append(odd[j])
