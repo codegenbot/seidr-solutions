46575692ce8c77bc147e07a9acb4673abb210d8a
beep boop <githubbot@vadim.me>
2024-07-10 16:47:40 +0200

    Solution 57 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9wljto1e Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/33f2a5aa-0a35-4c8e-b6b8-3eb5bed35 8cc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,22 +1,18 @@
-def total_or_fruit_left(s, n):
-    s = " ".join(s).replace("apples and", "apples ").replace("oranges", " oranges").split()
-    
+def fruit_distribution(s, n):
+    s = " ".join(s).replace("apples and", "apples ").replace("oranges ", " oranges").split()
     if 'apples' not in s or 'oranges' not in s:
         return None
-    
     apples_count = 0
     oranges_count = 0
-    
-    for i in s:
-        if i == 'apples':
+    for i, word in enumerate(s):
+        if word == 'apples':
             try:
-                apples_count += int(next((j for j in s[s.index(i)+1:] if str.isdigit(j)), ""))
-            except StopIteration:
-                break
-        elif i == 'oranges':
+                apples_count += int(s[i+1])
+            except ValueError:
+                pass
+        elif word == 'oranges':
             try:
-                oranges_count += int(next((j for j in s[s.index(i)+6:] if str.isdigit(j)), ""))
-            except StopIteration:
-                break
-    
+                oranges_count += int(s[i+1])
+            except ValueError:
+                pass
     return n - apples_count - oranges_count
