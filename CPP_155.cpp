751eaf0e8e5cd1d17d0e91f940ca79d7387cd24a
beep boop <githubbot@vadim.me>
2024-07-10 13:20:16 +0200

    Solution 83 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/0r0b8tjy Prompt:       |                                  ^~~ e11edbef-9afe-4d35-9345-28218d7584f4.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ e11edbef-9afe-4d35-9345-28218d7584f4.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ e11edbef-9afe-4d35-9345-28218d7584f4.cpp:5:5: note: ‘int main()’ previously defi ned here     5 | int main() {       |     ^~~~ In file included from e11edbef-9afe-4d35-9345-28218d7584f4.cpp:12: e11edbef-9afe-4d35-9345-28218d7584f4.cpp: In function ‘int main()’: e11edbef-9afe-4d35-9345-28218d7584f4.cpp:22:20: error: ‘even_odd_count’ was not declared in this scope    22 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ e11edbef-9afe-4d35-9345-28218d7584f4.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,24 +1,15 @@
-#include <iostream>
 #include <vector>
-#include <string>
-#include <cstdlib>
-#include <cassert>
-
-bool assert_equal(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
-}
 
 std::vector<int> even_odd_count(int num) {
-    std::vector<int> counts(2, 0);
-    std::string numStr = std::to_string(std::abs(num));
-    for (char c : numStr) {
-        if ((c - '0') % 2 == 0) {
-            counts[0]++;
-        } else {
-            counts[1]++;
-        }
+    int even = 0, odd = 0;
+    while(num != 0) {
+        if(num % 2 == 0)
+            even++;
+        else
+            odd++;
+        num /= 10;
     }
-    return counts;
+    return {even, odd};
 }
 
 int main() {
