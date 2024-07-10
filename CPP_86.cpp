86d84e9a794dd4bdaa74583a3d1d3dcfc28d0c76
beep boop <githubbot@vadim.me>
2024-07-10 19:49:04 +0200

    Solution 6 of CPP/86, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2847a6vn Prompt: cb46f4a5-de73-47cf-b995-7fa5030a531b.cpp: In function ‘std::string anti_shuffle( std::string)’: cb46f4a5-de73-47cf-b995-7fa5030a531b.cpp:18:68: error: request for member ‘first ’ in ‘* std::min_element<__gnu_cxx::__normal_iterator<char*, std::__cxx11::basic _string<char> > >(word.std::__cxx11::basic_string<char>::begin(), word.std::__cx x11::basic_string<char>::end()).__gnu_cxx::__normal_iterator<char*, std::__cxx11 ::basic_string<char> >::operator->()’, which is of non-class type ‘char’    18 |               result += min_element(word.begin(), word.end()) -> first;       |                                                                  ^~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,18 +1,26 @@
-string anti_shuffle(string s){
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+string anti_shuffle(string s) {
     string result = "";
-    for(int i = 0; i < s.length(); i++){
-        if(s[i] == ' '){
-            result += " ";
-            continue;
-        }
-        char temp[s[i] + 1];
-        int j = 0;
-        while(i < s.length() && s[i] != ' '){
-            temp[j++] = s[i++];
-        }
-        temp[j] = '\0';
-        for(int k = 0; k < j; k++){
-            result += (char)temp[k];
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') {
+            result += s[i];
+        } else {
+            string word = "";
+            while (i < s.length() && s[i] != ' ') {
+                word += s[i];
+                i++;
+            }
+            for (char c : word) {
+                result += *min_element(word.begin(), word.end());
+                word.erase(min_element(word.begin(), word.end()));
+            }
+            if (!word.empty()) {
+                result += word;
+            }
         }
     }
     return result;
