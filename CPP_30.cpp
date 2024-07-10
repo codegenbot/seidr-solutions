2c4d58748f211c60c3913f77c5e1209455a1b16b
beep boop <githubbot@vadim.me>
2024-07-10 06:12:53 +0200

    Solution 26 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nedixgdx Prompt:       |           ^~~~~~ In file included from 5f835f85-0a7d-48f4-a179-e7efea21829a.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 5f835f85-0a7d-48f4-a179-e7efea21829a.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 5f835f85-0a7d-48f4-a179-e7efea21829a.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 5f835f85-0a7d-48f4-a179-e7efea21829a.cpp:13: 5f835f85-0a7d-48f4-a179-e7efea21829a.cpp: In function ‘int main()’: 5f835f85-0a7d-48f4-a179-e7efea21829a.cpp:23:20: error: ‘get_positive’ was not de clared in this scope    23 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 5f835f85-0a7d-48f4-a179-e7efea21829a.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,8 +1,9 @@
+```cpp
 #include <vector>
 #include <algorithm>
 
-vector<float> get_positive(vector<float> l) {
-    vector<float> result;
+std::vector<float> get_positive(std::vector<float> l) {
+    std::vector<float> result;
     for (float num : l) {
         if (num > 0) {
             result.push_back(num);
@@ -11,6 +12,6 @@ vector<float> get_positive(vector<float> l) {
     return result;
 }
 
-bool issame(vector<float> a, vector<float> b) {
+bool issame(std::vector<float> a, std::vector<float> b) {
     return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
 }
