978fa0bf5152aecece37cb259b59b4e8ae7ec989
beep boop <githubbot@vadim.me>
2024-07-10 23:12:44 +0200

    Solution 50 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/muttgf56 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/853cb7d2-c95a-40a5-a6dd-b7585129a 1d6.py", line 25, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/853cb7d2-c95a-40a5-a6dd-b7585129a 1d6.py", line 22, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,12 +1,17 @@
-```
 def modp(n: int, p: int):
-    if p == 0:
-        return 1
-    result = pow(n % p, p // 2, p)
-    if p % 2 == 0:
-        return (result * result) % p
+    if n == 0:
+        return 0
+    elif p % 2 == 0:  
+        result = pow(n, (p-1)//2, p)
     else:
-        return (n * result * result) % p
+        if p == 2:
+            return n
+        result = pow(n, p-1, p)
+    if result == 1 and is_prime(p):
+        return pow(n, -1, p)
+    for i in range(2, p):
+        result = (result * n) % p
+    return result
 
 def is_prime(num: int):
     if num < 2:
