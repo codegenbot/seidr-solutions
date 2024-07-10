0f8bb315c9d9a02ad0832937de2d207ec80822ba
beep boop <githubbot@vadim.me>
2024-07-10 22:31:10 +0200

    Solution 32 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lgdurmsn Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0df165ed-2211-4c37-b39c-e23d99f72 50c.py", line 17, in <module>     check(solution)           ^^^^^^^^ NameError: name 'solution' is not defined

--- Python_121.py
+++ Python_121.py
@@ -1,9 +1,10 @@
 ```
 def sum_of_odd_numbers_in_every_other_element():
-    numbers = list(
-        map(int, input("Enter the elements of a list separated by space: ").split())
-    )
+    numbers = list(map(int, input("Enter the elements of a list separated by space: ").split()))
     if not numbers:
         raise ValueError
     solution = sum(i for i in numbers[1::2] if i % 2 != 0)
-    print(solution)
+    return solution
+
+if __name__ == "__main__":
+    print(sum_of_odd_numbers_in_every_other_element())
