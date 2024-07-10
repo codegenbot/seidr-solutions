7bab79bd25d391708e982c0ee9ca15f2dbd8aeac
beep boop <githubbot@vadim.me>
2024-07-10 19:21:30 +0200

    Solution 8 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/h0my8hka Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5631cede-6a85-42b6-8d98-e8bdc24c3 e12.py", line 13, in <module>     check(digits)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5631cede-6a85-42b6-8d98-e8bdc24c3 e12.py", line 10, in check     assert digits(2468) == 0            ^^^^^^^^^^^^^^^^^ AssertionError

--- Python_131.py
+++ Python_131.py
@@ -1,9 +1,5 @@
-```
 def digits(n):
     product = 1
-    has_odd = False
     for digit in str(n):
-        if int(digit) % 2 != 0:
-            has_odd = True
-            product *= int(digit)
-    return product if has_odd else 1
+        product *= int(digit)
+    return product
