73b7f23f4c6f256263102c8bc39a0729634e10df
beep boop <githubbot@vadim.me>
2024-07-10 18:43:13 +0200

    Solution 9 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/varyqd0v Prompt:       |                              ^~~~~~ 03cd9155-12db-4b96-9a93-2f45e6d5e3cb.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 03cd9155-12db-4b96-9a93-2f45e6d5e3cb.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 03cd9155-12db-4b96-9a93-2f45e6d5e3cb.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 03cd9155-12db-4b96-9a93-2f45e6d5e3cb.cpp:29: 03cd9155-12db-4b96-9a93-2f45e6d5e3cb.cpp: In function ‘int main()’: 03cd9155-12db-4b96-9a93-2f45e6d5e3cb.cpp:39:20: error: ‘split_words’ was not dec lared in this scope    39 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 03cd9155-12db-4b96-9a93-2f45e6d5e3cb.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,7 +1,10 @@
-#include <string>
 #include <iostream>
 #include <vector>
+#include <string>
 
+using namespace std;
+
+vector<string> split_words(string txt);
 bool issame(vector<string> a, vector<string> b);
 
 vector<string> split_words(string txt){
@@ -30,4 +33,30 @@ vector<string> split_words(string txt){
         result.push_back(to_string(oddCount));
     }
     return result;
+}
+
+bool issame(vector<string> a, vector<string> b){
+    if(a.size() != b.size()){
+        return false;
+    }
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    string input;
+    cout << "Enter a sentence: ";
+    getline(cin, input);
+    
+    vector<string> words = split_words(input);
+
+    for(int i = 0; i < words.size(); i++){
+        cout << words[i] << " ";
+    }
+    
+    return 0;
 }
