41e59cea2888edf98ee8112315bcea384eebcaa2
beep boop <githubbot@vadim.me>
2024-07-10 21:05:09 +0200

    Solution 8 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/y8702wh3 Prompt: it must return 0 4 for input WGYO WGYO, but it returns .

--- mastermind.py
+++ mastermind.py
@@ -1,13 +1,18 @@
 def mastermind(code, guess):
-    black_pegs = 0
-    white_pegs = 0
-    code_list = list(code)
-    guess_list = list(guess)
+    code_counts = {}
+    for char in code:
+        if char in code_counts:
+            code_counts[char] += 1
+        else:
+            code_counts[char] = 1
 
-    for i in range(4):
-        if code_list[i] == guess_list[i]:
-            black_pegs += 1
-        elif str(guess_list[i]) in ''.join(map(str, code_list)):
-            white_pegs += 1
+    guess_counts = {}
+    for char in guess:
+        if char in guess_counts:
+            guess_counts[char] += 1
+        else:
+            guess_counts[char] = 1
 
+    black_pegs = sum([min(code_counts.get(char, 0), guess_counts.get(char, 0)) for char in code_counts])
+    white_pegs = len([char for char in code if char in guess and code[char] != guess[char]])
     return str(black_pegs) + "\n" + str(white_pegs)
