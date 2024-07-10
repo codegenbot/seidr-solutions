4100e1f0b10dcd33a34bffc6c8668536292fca7c
beep boop <githubbot@vadim.me>
2024-07-10 23:17:41 +0200

    Solution 18 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/g0ftvo4y Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e161524-7fbb-457d-b42b-c73accbd0 8cb.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e161524-7fbb-457d-b42b-c73accbd0 8cb.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,12 +1,12 @@
-```
 def encode(message):
     result = ""
+    message = message.lower()
     for char in message:
         if char.isalpha():
-            if char.lower() in 'aeiou' and char.islower():
+            if char in 'aeiou':
                 result += chr((ord(char) - 97 + 2) % 26 + 97)
             else:
-                result += char.swapcase()
+                result += char.swapcase().lower()
         else:
             result += char
     return result
