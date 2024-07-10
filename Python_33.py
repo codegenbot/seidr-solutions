e535bdea514fc96373e74c7a8801650c89ebea9c
beep boop <githubbot@vadim.me>
2024-07-10 20:40:09 +0200

    Solution 12 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51e2973e-dd25-4206-9f0a-d9cc80282 8bb.py", line 25, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51e2973e-dd25-4206-9f0a-d9cc80282 8bb.py", line 20, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,19 +1,11 @@
-```
 def sort_third(l: list):
     if len(l) % 3 != 0:
         l += [None] * (3 - len(l) % 3)
-    l1 = [x for i, x in enumerate(l) if i % 3 == 0]
+    l1 = sorted([x for i, x in enumerate(l) if i % 3 == 0])
     l2 = [x for i, x in enumerate(l) if i % 3 == 1]
-    l3 = sorted([x for i, x in enumerate(l) if i % 3 == 2])
-
-    sorted_l1 = sorted(l1)
-    sorted_l3 = l3[::-1]
+    l3 = sorted([x for i, x in enumerate(l) if i % 3 == 2])[::-1]
 
     return [
-        (
-            x
-            if i % 3 != 0
-            else (x if i % 3 == 1 else next(y for y in sorted_l3 if y > x) or None)
-        )
-        for i, x in enumerate(sorted_l1 + [l2[0]] + l3)
+        (x if i % 3 != 0 else (x if i % 3 == 1 else next(y for y in l3[::-1] if y > x)))
+        for i, x in enumerate(l1 + l2 + l3)
     ]
