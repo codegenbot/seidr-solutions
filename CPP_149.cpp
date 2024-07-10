fc00ab3778431d8eda95c64602a2ae7310a37602
beep boop <githubbot@vadim.me>
2024-07-10 06:14:40 +0200

    Solution 65 of CPP/149, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/v7cr6z5j Prompt: 7bde6b8c-f239-45a9-8bda-294d493e37a5.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 7bde6b8c-f239-45a9-8bda-294d493e37a5.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 7bde6b8c-f239-45a9-8bda-294d493e37a5.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -4,6 +4,36 @@
 
 using namespace std;
 
+bool aresame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<string> sorted_list_sum(vector<string> input) {
+    sort(input.begin(), input.end());
+    vector<int> sum;
+    for (int i = 0; i < input.size(); i++) {
+        int total = 0;
+        string temp = "";
+        for (char c : input[i]) {
+            if (isalpha(c)) {
+                temp += tolower(c);
+            } else {
+                total += c - '0';
+            }
+        }
+        sum.push_back(total);
+    }
+    return input;
+}
+
 bool aresame(vector<string> a, vector<string> b, vector<string> c) {
     if (a.size() != b.size() || a.size() != c.size()) {
         return false;
@@ -14,51 +44,4 @@ bool aresame(vector<string> a, vector<string> b, vector<string> c) {
         }
     }
     return true;
-}
-
-vector<string> sorted_list_sum(vector<string> input) {
-    sort(input.begin(), input.end());
-    for (int i = 0; i < input.size(); i++) {
-        if (input[i].size() > 1) {
-            char firstChar = input[i][0];
-            for (int j = i + 1; j < input.size(); j++) {
-                if (input[j][0] == firstChar) {
-                    input.erase(input.begin() + j);
-                    j--;
-                }
-            }
-        }
-    }
-    return input;
-}
-
-vector<string> sum(vector<string> a, vector<string> b, vector<string> c) {
-    vector<string> result;
-    for (int i = 0; i < a.size(); i++) {
-        string s1 = a[i], s2 = b[i], s3 = c[i];
-        int sum = stoi(s1) + stoi(s2) + stoi(s3);
-        if (sum > 9) {
-            while (sum > 0) {
-                result.push_back(to_string(sum % 10));
-                sum /= 10;
-            }
-        } else {
-            result.push_back(s1);
-        }
-    }
-    return sorted_list_sum(result);
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<string> a(n), b(n), c(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i] >> b[i] >> c[i];
-    }
-    vector<string> result = sum(a, b, c);
-    for (string s : result) {
-        cout << s << " ";
-    }
-    return 0;
 }
