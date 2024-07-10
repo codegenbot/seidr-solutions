197c2439cd32e271b6a7a1aa394c897db8c82eea
beep boop <githubbot@vadim.me>
2024-07-10 18:22:35 +0200

    Solution 14 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/htnwx5v8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a2ec6eab-3367-4c79-9a94-f91237a8c 873.py", line 15, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a2ec6eab-3367-4c79-9a94-f91237a8c 873.py", line 12, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,6 +1,10 @@
+def is_palindrome(string: str) -> bool:
+    return string == string[::-1]
+
+
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
-        return string + string
-    for i in range(len(string), -1, -1):
+        return string
+    for i in range(len(string), 0, -1):
         if string[:i] == string[:i][::-1]:
-            return string[:i] + string[i:][::-1] + string[:i][::-1]
+            return string[:i] + string[i - 1] + string[i:][::-1]
