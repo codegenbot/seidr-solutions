333f134c0df3a05dd19464b4cb2855c70ead51a0
beep boop <githubbot@vadim.me>
2024-07-10 08:41:07 +0200

    Solution 7 of CPP/129, ratio 0.45454545454545453 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/qp9ccg94 Prompt: 69d42a6c-8ca8-4afa-94be-52d941803e9c.cpp:25:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 69d42a6c-8ca8-4afa-94be-52d941803e9c.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -3,10 +3,6 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 vector<int> minPath(vector<vector<int>> grid, int k) {
     vector<int> result;
     for (int i = 0; i < k; ++i) {
@@ -17,9 +13,4 @@ vector<int> minPath(vector<vector<int>> grid, int k) {
         }
     }
     return result;
-}
-
-int main() {
-    assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
-    return 0;
 }
