989e1c0d895366d5230a4cae1f69f8a4eae4daae
beep boop <githubbot@vadim.me>
2024-07-10 22:09:00 +0200

    Solution 49 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/avcnu0rr Prompt:    49 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6b930875-85eb-4195-a34c-5146276af771.cpp:57:5: error: redefinition of ‘int main( )’    57 | int main(){       |     ^~~~ 6b930875-85eb-4195-a34c-5146276af771.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 6b930875-85eb-4195-a34c-5146276af771.cpp:48: 6b930875-85eb-4195-a34c-5146276af771.cpp: In function ‘int main()’: 6b930875-85eb-4195-a34c-5146276af771.cpp:58:20: error: ‘parse_music’ was not dec lared in this scope    58 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ 6b930875-85eb-4195-a34c-5146276af771.cpp:58:19: error: ‘issame’ cannot be used a s a function    58 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -12,6 +12,18 @@ bool issame(vector<int> a, vector<int> b){
 
 vector<int> parse_music(string music_string);
 
+void main() {
+    string input_music;
+    cin >> input_music;
+    vector<int> parsed_result = parse_music(input_music);
+    
+    vector<int> b = {1, 2, 3, 4};
+    
+    bool result = issame(parsed_result, b);
+    
+    cout << std::boolalpha << result << endl;
+}
+
 vector<int> parse_music(string music_string){
     vector<int> result;
     int beats = 0;
@@ -29,16 +41,4 @@ vector<int> parse_music(string music_string){
         result.push_back(beats);
     }
     return result;
-}
-
-int main() {
-    string input_music;
-    cin >> input_music;
-    vector<int> parsed_result = parse_music(input_music);
-    
-    vector<int> b = {1, 2, 3, 4};
-    
-    bool result = issame(parsed_result, b);
-    
-    cout << std::boolalpha << result << endl;
 }
