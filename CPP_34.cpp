7638798aed90f5daa2b41b2a00024fa6f6c4af1a
beep boop <githubbot@vadim.me>
2024-07-10 02:28:01 +0200

    Solution 58 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uzrnpb22 Prompt:       |                                  ^~~ 5833892d-e9b7-419a-acbd-54c1d99f7e5d.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ 5833892d-e9b7-419a-acbd-54c1d99f7e5d.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from 5833892d-e9b7-419a-acbd-54c1d99f7e5d.cpp:39: 5833892d-e9b7-419a-acbd-54c1d99f7e5d.cpp: In function ‘int main()’: 5833892d-e9b7-419a-acbd-54c1d99f7e5d.cpp:49:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    49 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 5833892d-e9b7-419a-acbd-54c1d99f7e5d.cpp:3: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -28,10 +28,10 @@ It distinct(It first, It last) {
     return result.begin();
 }
 
-int main_test() { 
+int main() {
     std::vector<int> input = {5, 3, 5, 2, 3, 3, 9, 0, 123};
     auto output(distinct(input.begin(), input.end()));
-    auto end = std::namespace::unique(output.begin(), output.end());
+    auto end = std::unique(output.begin(), output.end());
     for (int i : std::vector<int>(output.begin(), end)) {
         std::cout << i << " ";
     }
