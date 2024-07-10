befe1e90222994ff4b2cdff2d972585f1f32613e
beep boop <githubbot@vadim.me>
2024-07-10 22:37:23 +0200

    Solution 10 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/f7tgrwqd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e61c00f2-c476-42bc-9897-2ce5e1bef 93b.py", line 14, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e61c00f2-c476-42bc-9897-2ce5e1bef 93b.py", line 11, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e61c00f2-c476-42bc-9897-2ce5e1bef 93b.py", line 2, in sorted_list_sum     return sum(            ^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e61c00f2-c476-42bc-9897-2ce5e1bef 93b.py", line 3, in <genexpr>     int(x) ValueError: invalid literal for int() with base 10: 'cc'

--- Python_149.py
+++ Python_149.py
@@ -1,3 +1,8 @@
 ```
 def sorted_list_sum(lst):
-    return sum(len(word) for word in lst)
+    return ''.join(
+        str(x)
+        for x in sorted(
+            set([word for word in lst if len(word) % 2 == 0]), key=lambda x: (len(x), x)
+        )
+    )
