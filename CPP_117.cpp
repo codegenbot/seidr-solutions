a02ed3b9da2d25ca0500915b13881f87dde4c143
beep boop <githubbot@vadim.me>
2024-07-10 19:52:44 +0200

    Solution 80 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/5wrs10ic Prompt:    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 57318632-eb1a-4898-9ba7-f41a91343ca6.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 57318632-eb1a-4898-9ba7-f41a91343ca6.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 57318632-eb1a-4898-9ba7-f41a91343ca6.cpp:29: 57318632-eb1a-4898-9ba7-f41a91343ca6.cpp: In function ‘int main()’: 57318632-eb1a-4898-9ba7-f41a91343ca6.cpp:39:20: error: ‘select_words’ was not de clared in this scope    39 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 57318632-eb1a-4898-9ba7-f41a91343ca6.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,19 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cassert>
+bool issame(vector<string> a, vector<string> b);
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    return a == b;
+vector<string> select_words(string s, int n);
+
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    
+    return true;
 }
 
-std::vector<std::string> select_words(std::string s, int n) {
-    std::vector<std::string> result;
+vector<string> select_words(string s, int n) {
+    vector<string> result;
     if (s.empty()) {
         return result;
     }
     
-    std::string word = "";
+    string word = "";
     int consonant_count = 0;
     for (char c : s) {
         if (c == ' ') {
@@ -33,9 +42,4 @@ std::vector<std::string> select_words(std::string s, int n) {
     }
     
     return result;
-}
-
-int main() {
-    assert(issame(select_words("a b c d e f", 1), {"b", "c", "d", "f"}));
-    return 0;
 }
