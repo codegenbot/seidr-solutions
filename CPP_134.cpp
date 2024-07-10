c80a6728daa98d2d90568dec0f42d43e323af60a
beep boop <githubbot@vadim.me>
2024-07-10 20:22:04 +0200

    Solution 40 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/gyiu1elv Prompt:       | ^~ b7c3266f-e3d9-4699-9ccc-ffac07f32d80.cpp:2:21: error: ‘txt’ was not declared in this scope     2 |     char lastChar = txt.back();       |                     ^~~ b7c3266f-e3d9-4699-9ccc-ffac07f32d80.cpp:3:5: error: expected unqualified-id bef ore ‘if’     3 |     if(isalpha(lastChar)){       |     ^~ b7c3266f-e3d9-4699-9ccc-ffac07f32d80.cpp:8:5: error: expected unqualified-id bef ore ‘return’     8 |     return false;       |     ^~~~~~ b7c3266f-e3d9-4699-9ccc-ffac07f32d80.cpp:9:1: error: expected declaration before  ‘}’ token     9 | }       | ^ In file included from b7c3266f-e3d9-4699-9ccc-ffac07f32d80.cpp:11: b7c3266f-e3d9-4699-9ccc-ffac07f32d80.cpp: In function ‘int main()’: b7c3266f-e3d9-4699-9ccc-ffac07f32d80.cpp:13:13: error: ‘check_if_last_char_is_a_ letter’ was not declared in this scope    13 |     assert (check_if_last_char_is_a_letter("apple pi e ") == false);       |             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -1,16 +1,8 @@
-#include <string>
-#include <cassert>
-
-bool check_if_last_char_is_a_letter(const std::string& txt) {
+bool check_if_last_char_is_a_letter(const std::string &txt) {
     if(txt.empty()) return false;
     char lastChar = txt.back();
     if(isalpha(lastChar) && txt.find_last_of(" ") == txt.length()-1) {
         return true;
     }
     return false;
-}
-
-int main() {
-    assert (check_if_last_char_is_a_letter("apple pi e ") == false);
-    return 0;
 }
