155d39a1ddac4a3bd36837079b1d4ffe1cfb7957
beep boop <githubbot@vadim.me>
2024-07-10 18:32:16 +0200

    Solution 11 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8qoyijl2 Prompt: Program did not receive expected input

--- Python_14.py
+++ Python_14.py
@@ -1,5 +1,5 @@
 from typing import list
 
-def all_prefixes(input_string: str) -> list:
-    input_string = input("Enter a string: ")
-    return [input_string[:i] for i in range(1, len(input_string) + 1)]
+def all_prefixes() -> list:
+    string = input("Enter a string: ")
+    return [string[:i] for i in range(1, len(string) + 1)]
