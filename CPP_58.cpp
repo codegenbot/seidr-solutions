74556f275b6239e422b027b953bde05c43c9b5f1
beep boop <githubbot@vadim.me>
2024-07-10 02:34:40 +0200

    Solution 35 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0hw5ynmd Prompt: /usr/include/c++/11/pstl/glue_algorithm_defs.h:452:1: note: candidate: ‘template <class _ExecutionPolicy, class _ForwardIterator1, class _ForwardIterator2, class  _ForwardIterator> __pstl::__internal::__enable_if_execution_policy<_ExecutionPo licy, _ForwardIterator2> std::set_union(_ExecutionPolicy&&, _ForwardIterator1, _ ForwardIterator1, _ForwardIterator2, _ForwardIterator2, _ForwardIterator)’   452 | set_union(_ExecutionPolicy&& __exec, _ForwardIterator1 __first1, _Forwar dIterator1 __last1, _ForwardIterator2 __first2,       | ^~~~~~~~~ /usr/include/c++/11/pstl/glue_algorithm_defs.h:452:1: note:   template argument deduction/substitution failed: bfadcd2b-ef7c-48b3-a9b7-20c3c9ebd230.cpp:14:25: note:   candidate expects 6 argu ments, 4 provided    14 |           std::set_union(s1.begin(), s1.end(), s2.begin(), s2.end()),       |           ~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ bfadcd2b-ef7c-48b3-a9b7-20c3c9ebd230.cpp: At global scope: bfadcd2b-ef7c-48b3-a9b7-20c3c9ebd230.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ bfadcd2b-ef7c-48b3-a9b7-20c3c9ebd230.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,30 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <set>
-#include <algorithm>
-
-using namespace std;
-
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
-    vector<int> result;
-    for (int i : set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                                    std::back_inserter(result))) {
-        if (find(result.begin(), result.end(), i) == result.end())
-            result.push_back(i);
-    }
+    vector<int> result(s1.begin(), s1.end());  // start the output with s1
+    std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                          back_inserter(result));
     
     return result;
-}
-
-int main() {
-    vector<int> l1 = {4, 3, 2, 8};
-    vector<int> l2 = {2, 4, 6, 8};
-    vector<int> commonElements = common(l1, l2);
-    for(int i : commonElements) {
-        cout << i << " ";
-    }
-    return 0;
 }
