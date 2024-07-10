6ca5e5a594c7c836b8c6e08fc96fd9103001e0b7
beep boop <githubbot@vadim.me>
2024-07-10 21:26:12 +0200

    Solution 2 of Python/78, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/qo5b6s5m Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/885d0cb4-8fac-4bc2-a29f-bca58040a 5bb.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_78.py
+++ Python_78.py
@@ -1,8 +1,7 @@
-```
-def hex_key(num):
+def hex_key(num_str):
     primes = set([2, 3, 5, 7, 11, 13, 17])
     count = 0
-    for digit in num:
+    for digit in num_str:
         if digit.upper() in ['B', 'D']:
             count += 1
         elif int(digit, 16) in primes:
