62cb64ed5ad55142351bace06ec4f188228f6b33
beep boop <githubbot@vadim.me>
2024-07-10 23:52:04 +0200

    Solution 35 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/f6bcnret Prompt:       |             ^~~~~~ 2102e382-698d-4a4b-b452-efffa0cb4503.cpp:25:20: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 2102e382-698d-4a4b-b452-efffa0cb4503.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2102e382-698d-4a4b-b452-efffa0cb4503.cpp:25:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 2102e382-698d-4a4b-b452-efffa0cb4503.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 2102e382-698d-4a4b-b452-efffa0cb4503.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 2102e382-698d-4a4b-b452-efffa0cb4503.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -18,5 +18,5 @@ std::vector<int> largest_smallest_integers(std::vector<int> lst) {
         }
     }
 
-    return {std::max({(max_negative > 0 ? INT_MAX : max_negative), (min_positive < 1 ? INT_MIN : min_positive)}, std::nullopt)};
+    return {std::max({(max_negative > 0 ? INT_MAX : max_negative), (min_positive < 1 ? INT_MIN : min_positive)}, 0)};
 }
