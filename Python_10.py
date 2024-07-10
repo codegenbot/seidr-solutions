d6a935b35f67cac00f4a7ca0bbab8acdfb50af98
beep boop <githubbot@vadim.me>
2024-07-10 23:46:59 +0200

    Solution 97 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6ml3f6al Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fbdf5bcf-82f1-4b75-bbb5-5b139e3fa 690.py", line 20, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fbdf5bcf-82f1-4b75-bbb5-5b139e3fa 690.py", line 17, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,7 +1,6 @@
 def is_palindrome(string: str) -> bool:
     return string == string[::-1]
 
-
 def make_palindrome(string: str) -> str:
     if not string:
         return ""
@@ -11,9 +10,7 @@ def make_palindrome(string: str) -> str:
         else:
             for i in range(len(string)):
                 for j in range(len(string)):
-                    new_string = (
-                        string[:i] + chr(ord(string[i]) ^ ord("a")) + string[i:]
-                    )
+                    new_string = string[:i] + chr(ord(string[i]) ^ ord('a')) + string[i:]
                     if is_palindrome(new_string):
                         return "" + new_string + "" + new_string
             return "" + string[::-1] + "" + string[::-1]
