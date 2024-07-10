da3f56c2f87a9ccf9fa330c548520e63e60a1daf
beep boop <githubbot@vadim.me>
2024-07-10 18:40:13 +0200

    Solution 24 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/htnwx5v8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7097b7d4-2a80-4d91-ba26-4e90054c7 2fa.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,8 +1,8 @@
-```python
-    def is_palindrome(string: str) -> bool:
-        return string == string[::-1]
-    
-    def make_palindrome(string: str) -> str:
-        if string == string[::-1]:
-            return string
-        return string + "".join(reversed(string))
+```
+def is_palindrome(string: str) -> bool:
+    return string == string[::-1]
+
+def make_palindrome(string: str) -> str:
+    if string == string[::-1]:
+        return string
+    return string + "".join(reversed(string))
