ba5bd35a7bd018631f4e22a38531a8e79f4b39ef
beep boop <githubbot@vadim.me>
2024-07-10 20:09:00 +0200

    Solution 81 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/o1w7shmx Prompt:       |                                        ^ a890db7e-f88a-47aa-9243-c9ec31e7dd45.cpp:52:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    52 | int main(){       |         ^~ a890db7e-f88a-47aa-9243-c9ec31e7dd45.cpp:52:9: note: remove parentheses to defau lt-initialize a variable    52 | int main(){       |         ^~       |         -- a890db7e-f88a-47aa-9243-c9ec31e7dd45.cpp:52:9: note: or replace parentheses with  braces to value-initialize a variable a890db7e-f88a-47aa-9243-c9ec31e7dd45.cpp:52:11: error: a function-definition is not allowed here before ‘{’ token    52 | int main(){       |           ^ a890db7e-f88a-47aa-9243-c9ec31e7dd45.cpp:54:2: error: expected ‘}’ at end of inp ut    54 | }       |  ^ a890db7e-f88a-47aa-9243-c9ec31e7dd45.cpp:34:21: note: to match this ‘{’    34 | int main_function() {       |                     ^

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -10,6 +10,7 @@ bool issame(const vector<int>& a, const vector<int>& b) {
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
+        }
     }
     return true;
 }
