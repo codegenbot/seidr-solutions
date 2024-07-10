338f3a354304fc427b368556b354029983263f9f
beep boop <githubbot@vadim.me>
2024-07-10 03:06:38 +0200

    Solution 87 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/v2xoqm3t Prompt:                  from ad70aea3-fcac-4fa3-865e-434aeba1494c.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from ad70aea3-fcac-4fa3-865e-434aeba1494c.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ad70aea3-fcac-4fa3-865e-434aeba1494c.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ad70aea3-fcac-4fa3-865e-434aeba1494c.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ ad70aea3-fcac-4fa3-865e-434aeba1494c.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -15,4 +15,7 @@ std::vector<int> remove_duplicates(std::vector<int> v) {
     return unique_v;
 }
 
-assert(issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), std::vector<int>({1, 4, 5})));
+int test_main() {
+    assert(issame(std::vector<int>({1, 2, 3, 2, 4, 3, 5}), std::vector<int>({1, 4, 5})));
+    return 0;
+}
