4caea712a575360a3cd7337c9ad85a86e962c053
beep boop <githubbot@vadim.me>
2024-07-10 19:58:38 +0200

    Solution 3 of CPP/144, ratio 0.7692307692307693 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/l82r7v5r Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,7 +1,10 @@
-int x_num = stoi(x.substr(0, x.find('/')));
-    int x_den = stoi(x.substr(x.find('/') + 1));
-    int n_num = stoi(n.substr(0, n.find('/')));
-    int n_den = stoi(n.substr(n.find('/') + 1));
-
-    return (x_num * n_num) % (x_den * n_den) == 0;
+#include<string>
+using namespace std;
+bool simplify(string x, string n){
+    int num1 = stoi(x.substr(0, x.find('/')));
+    int den1 = stoi(x.substr(x.find('/') + 1));
+    int num2 = stoi(n.substr(0, n.find('/')));
+    int den2 = stoi(n.substr(n.find('/') + 1));
+    
+    return (num1 * den2 == num2 * den1);
 }
