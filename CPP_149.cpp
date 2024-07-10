d6dcc9847c28fbd05e87ba249093b23a1bd9d6c7
beep boop <githubbot@vadim.me>
2024-07-10 11:00:44 +0200

    Solution 30 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/0x1g0oqm Prompt:       |                                     ^~~~~~ 01a9e54a-e674-46a6-a5ad-4ef451870616.cpp:18:37: note: suggested alternatives: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 01a9e54a-e674-46a6-a5ad-4ef451870616.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 01a9e54a-e674-46a6-a5ad-4ef451870616.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 01a9e54a-e674-46a6-a5ad-4ef451870616.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -3,13 +3,13 @@
 #include <iostream>
 #include <cassert>
 
-bool areSame(std::vector<std::string> list1, std::vector<std::string> list2) {
-    std::sort(list1.begin(), list1.end());
-    std::sort(list2.begin(), list2.end());
-    return list1 == list2;
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    std::sort(a.begin(), a.end());
+    std::sort(b.begin(), b.end());
+    return a == b;
 }
 
-std::vector<std::string> sortedListSum(std::vector<std::string> inputList) {
-    std::sort(inputList.begin(), inputList.end());
-    return inputList;
+std::vector<std::string> sorted_list_sum(std::vector<std::string> input) {
+    std::sort(input.begin(), input.end());
+    return input;
 }
