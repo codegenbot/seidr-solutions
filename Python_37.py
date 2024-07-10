afa354ce0d31de22e3362607977f90a732f60b50
beep boop <githubbot@vadim.me>
2024-07-10 18:55:46 +0200

    Solution 8 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uquygx2d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dce7e659-451a-4ecb-b9f1-7aa0c6d6f 30a.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dce7e659-451a-4ecb-b9f1-7aa0c6d6f 30a.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,7 +1,4 @@
 def sort_even(l: list):
-    if len([x for x in l if x % 2 == 0]) < 1:
-        return l
-    return [
-        x if i % 2 == 0 else y
-        for i, (x, y) in enumerate(zip(sorted([y for y in l[1::2]]), l[::2]))
-    ]
+    evens = sorted([x for x in l if x % 2 == 0])
+    odds = [x for x in l if x % 2 != 0]
+    return [x if i < len(evens) else y for i, (x, y) in enumerate(zip(odds, l[1::]))]
