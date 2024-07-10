f87c0b583efdf7e271ae41ee2ac30ed11d087dc3
beep boop <githubbot@vadim.me>
2024-07-10 03:56:39 +0200

    Solution 18 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/yu71ufye Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f33a7192-0133-4bcf-a23f-6abf22612 372.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f33a7192-0133-4bcf-a23f-6abf22612 372.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -5,7 +5,7 @@ def encode(message):
         if char.isupper():
             encoded_message += chr((ord(char) - ord('A') + 4) % 26 + ord('A'))
         elif char.islower():
-            encoded_message += chr((ord(char) - ord('a') + 4) % 26 + ord('a'))
+            encoded_message += chr((ord(char) - ord('a') + 4) % 25 + ord('a'))
         else:
             encoded_message += char
     return encoded_message
