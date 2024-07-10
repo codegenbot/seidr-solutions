bddf7e129b4278ef7b523902013a98c5306f3765
beep boop <githubbot@vadim.me>
2024-07-10 04:39:46 +0200

    Solution 39 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/yu71ufye Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/28a4051d-7a42-4e65-bcad-f52975fcc f68.py", line 26, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/28a4051d-7a42-4e65-bcad-f52975fcc f68.py", line 22, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -6,11 +6,11 @@ def encode(message):
             is_upper = char.isupper()
             char_lower = char.lower()
             if char_lower in vowels:
-                new_char = chr((ord(char_lower) - ord('a') + 5) % 26 + ord('a'))
+                new_char = chr((ord(char_lower) - ord('a') + 3) % 26 + ord('a'))
+                if is_upper:
+                    new_char = new_char.upper()
             else:
                 new_char = char
-            if is_upper:
-                new_char = new_char.upper()
             encoded_message += new_char
         else:
             encoded_message += char
