bc9273f2a1cad4783072e6b5700e89995eb562fe
beep boop <githubbot@vadim.me>
2024-07-10 08:31:34 +0200

    Solution 53 of CPP/111, ratio 0.625 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/fe6sfwwf Prompt: a982f001-a5f9-4eaf-b933-e0097203cad3.cpp:43:6: error: redefinition of ‘bool issa me(std::map<char, int>, std::map<char, int>)’    43 | bool issame(map<char,int> a,map<char,int> b){       |      ^~~~~~ a982f001-a5f9-4eaf-b933-e0097203cad3.cpp:9:6: note: ‘bool issame(std::map<char, int>, std::map<char, int>)’ previously defined here     9 | bool issame(std::map<char, int> a, std::map<char, int> b) {       |      ^~~~~~ a982f001-a5f9-4eaf-b933-e0097203cad3.cpp:56:5: error: redefinition of ‘int main( )’    56 | int main(){       |     ^~~~ a982f001-a5f9-4eaf-b933-e0097203cad3.cpp:37:5: note: ‘int main()’ previously def ined here    37 | int main() {       |     ^~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -4,9 +4,7 @@
 #include <string>
 #include <cassert>
 
-bool issame(std::map<char, int> a, std::map<char, int> b) {
-    return a == b;
-}
+using namespace std;
 
 std::map<char, int> histogram(std::string test) {
     std::map<char, int> result;
@@ -30,9 +28,4 @@ std::map<char, int> histogram(std::string test) {
     }
 
     return result;
-}
-
-int main() {
-    assert(issame(histogram("a"), {{'a', 1}}));
-    return 0;
 }
