eefe6609c94c08d6a0dde728bdf2a2203e600ad6
beep boop <githubbot@vadim.me>
2024-07-10 03:43:25 +0200

    Solution 69 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/agkgesc3 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90bffea6-a6e2-421b-8c58-eb0cb3e3a 107.py", line 38, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90bffea6-a6e2-421b-8c58-eb0cb3e3a 107.py", line 35, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ AssertionError

--- Python_1.py
+++ Python_1.py
@@ -2,32 +2,23 @@ def separate_paren_groups(expression):
     result = []
     temp_group = ""
     in_group = False
-    
+
     for char in expression:
-        if char == '(':
+        if char == "(":
             if not in_group:
                 result.append("(")
                 in_group = True
             else:
                 temp_group += char
-        elif char == ')':
+        elif char == ")":
             if in_group:
                 temp_group += char
-                if temp_group.count('(') == 1 and temp_group.count(')') > 0:
-                    result[-1] = temp_group
+                if temp_group.count("(") == temp_group.count(")"):
+                    result.append(temp_group)
                     in_group = False
-                    temp_group = ""
-                else:
                     temp_group = ""
             else:
                 result.append(")")
                 in_group = False
-        else:
-            if in_group:
-                temp_group += char
-            else:
-                result.append(char)
-    
-    if in_group:
-        result.append(temp_group + ")")
+
     return [x for x in result if x]
