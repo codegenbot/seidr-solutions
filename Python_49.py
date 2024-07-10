d0630a6f339b69efea04037f44b35472d660aff4
beep boop <githubbot@vadim.me>
2024-07-10 19:45:32 +0200

    Solution 20 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/m88lgrnk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f9714160-421b-4067-b8a3-1f04f2d74 33e.py", line 12, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f9714160-421b-4067-b8a3-1f04f2d74 33e.py", line 9, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,7 +1,7 @@
 def modp(n: int, p: int) -> int:
     if not isinstance(n, int) or not isinstance(p, int):
-        return "Invalid input. Both inputs must be integers."
+        return
     elif p <= 0:
-        return "Modulus cannot be less than or equal to zero."
+        return
     else:
-        return n if p == 1 else pow(n, p - 1, p) * n % p
+        return n**p % p
