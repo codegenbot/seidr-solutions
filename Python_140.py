fa8876c827485754b1517da0c7ea680e53ef74cf
beep boop <githubbot@vadim.me>
2024-07-10 05:16:02 +0200

    Solution 18 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/1vtozq5z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/77ebe495-ed44-4b7b-8c85-355229c12 6bd.py", line 12, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/77ebe495-ed44-4b7b-8c85-355229c12 6bd.py", line 8, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple"     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/77ebe495-ed44-4b7b-8c85-355229c12 6bd.py", line 3, in fix_spaces     return re.sub(r' {2,}', '-', text)            ^^ NameError: name 're' is not defined

--- Python_140.py
+++ Python_140.py
@@ -1,5 +1,6 @@
 import re
 
+
 def fix_spaces(text):
-    text = text.replace(' ', '_')
-    return re.sub(r' {2,}', '-', text)
+    text = text.replace(" ", "_")
+    return re.sub(r" {2,}", "-", text)
