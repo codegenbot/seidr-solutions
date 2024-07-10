632900e30b441b6764e82618c26a13d6006b039e
beep boop <githubbot@vadim.me>
2024-07-10 03:00:41 +0200

    Solution 75 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ul4hcvmg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b72016f8-22a8-4f7c-a503-cd7f5b12d 88f.py", line 27, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b72016f8-22a8-4f7c-a503-cd7f5b12d 88f.py", line 24, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -2,22 +2,23 @@ def minPath(grid, k):
     n = len(grid)
     res = []
 
-    def dfs(i, j, path, l):
-        if l == k:
+    def dfs(i, j, path):
+        if len(path) > k:
+            return
+        if len(path) == k:
             return [path]
         for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
             if (
                 0 <= x < n
                 and 0 <= y < n
                 and (x, y) not in set(zip(*[grid[i] for i in range(n)]))
+                and ((x, y) not in [(i, j)] or path[-1] != grid[x][y])
             ):
                 grid[x][y], grid[i][j] = grid[i][j], grid[x][y]
-                temp = dfs(x, y, path + [grid[x][y]], l + 1)
-                if temp:
-                    return temp
+                paths = dfs(x, y, path + [grid[x][y]])
                 grid[x][y], grid[i][j] = grid[i][j], grid[x][y]
-        if l < k:
-            return []
-        return [path]
+                if paths:
+                    res.append(min(paths))
+        return res[0] if res else []
 
-    return min([i for i in set(dfs(0, 0, [grid[0][0]], 1)) if len(i) == k], default=[])
+    return min([path for path in set(dfs(0, 0, [grid[0][0]]))])
