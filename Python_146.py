f4ada744773f4bdb782bd8fd368970c8ae72cf23
beep boop <githubbot@vadim.me>
2024-07-10 17:25:08 +0200

    Solution 12 of Python/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zk7px44c Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/98599685-d4ab-49bc-8f27-9ba938cad fea.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_146.py
+++ Python_146.py
@@ -1,7 +1,10 @@
+```
 def specialFilter(nums):
     count = 0
     for num in nums:
         if abs(num) > 10 and str(abs(num))[-1] % 2 != 0 and int(str(abs(num))[0]) % 2 != 0:
+            count += 1
+        elif (abs(num) <= 10 and (str(abs(num))[-1] % 2 != 0 or int(str(abs(num))[0]) % 2 != 0)):
             count += 1
     return count
 
