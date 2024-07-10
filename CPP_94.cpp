eec4201b21e6888dd6a9abe5be6b8850e90b4cd3
beep boop <githubbot@vadim.me>
2024-07-10 17:26:21 +0200

    Solution 38 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/mmm90hid Prompt: 5e926f8b-3202-4050-b0cf-8edb8b53aedd.cpp:1:15: error: ‘vector’ was not declared in this scope     1 | int skjkasdkd(vector<int> lst){       |               ^~~~~~ 5e926f8b-3202-4050-b0cf-8edb8b53aedd.cpp:1:22: error: expected primary-expressio n before ‘int’     1 | int skjkasdkd(vector<int> lst){       |                      ^~~ In file included from 5e926f8b-3202-4050-b0cf-8edb8b53aedd.cpp:24: 5e926f8b-3202-4050-b0cf-8edb8b53aedd.cpp: In function ‘int main()’: 5e926f8b-3202-4050-b0cf-8edb8b53aedd.cpp:26:22: error: ‘skjkasdkd’ cannot be use d as a function    26 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ~~~~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,7 +1,6 @@
-```cpp
 #include <vector>
 
-int findSumOfDigitsOfMaxPrime(vector<int> lst){
+int maxPrimeSum(vector<int> lst){
     int maxPrime = 0;
     for(int i: lst){
         if(isPrime(i) && i > maxPrime)
