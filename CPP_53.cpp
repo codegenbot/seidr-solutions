92da742703cae654759599a7daa6bbc1a3d66e01
beep boop <githubbot@vadim.me>
2024-07-10 06:39:29 +0200

    Solution 19 of CPP/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/a1j8ixp5 Prompt: 7855847a-b856-4811-8ca1-1fefecf2285e.cpp: In function ‘int main()’: 7855847a-b856-4811-8ca1-1fefecf2285e.cpp:9:15: error: ‘rand’ was not declared in  this scope     9 |         int x=rand()%1000;       |               ^~~~

--- CPP_53.cpp
+++ CPP_53.cpp
@@ -1,13 +1,13 @@
-```cpp
 #include <cstdlib>
+
 int add(int x, int y) {
     return x + y;
 }
 
 int main() {
-    #include <ctime>
     srand(time(0));
-    int num = rand();
-    std::cout << "Random number: " << num << std::endl;
-    return 0;
+    int x = rand() % 10;
+    int y = rand() % 10;
+    cout << "The random numbers are: " << x << ", " << y << endl;
+    cout << "Their sum is: " << add(x, y) << endl;
 }
