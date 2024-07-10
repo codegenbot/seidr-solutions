8544c384eec2b2c2b8860d2e65b03714b6034f59
beep boop <githubbot@vadim.me>
2024-07-10 21:27:13 +0200

    Solution 18 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lx335o7c Prompt: /usr/include/c++/11/bits/stl_uninitialized.h:704:44:   required from ‘_ForwardIt erator std::__uninitialized_default_n_a(_ForwardIterator, _Size, std::allocator< _Tp>&) [with _ForwardIterator = int*; _Size = long unsigned int; _Tp = int]’ /usr/include/c++/11/bits/stl_vector.h:1606:36:   required from ‘void std::vector <_Tp, _Alloc>::_M_default_initialize(std::vector<_Tp, _Alloc>::size_type) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = l ong unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:512:9:   required from ‘std::vector<_Tp, _ Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocator_type&) [with  _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = long unsigned int; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<int >]’ 819e894c-8692-4a34-8fef-398c75e503ea.cpp:7:25:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -37,7 +36,8 @@ int main() {
     cout << "Enter a positive integer: ";
     cin >> n;
 
-    vector<int> result = even_odd_palindrome(n);
+    vector<int> result(2);
+    result = even_odd_palindrome(n);
 
     cout << "Number of even palindromes: " << result[0] << endl;
     cout << "Number of odd palindromes: " << result[1] << endl;
