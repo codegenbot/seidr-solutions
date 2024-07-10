99ec54c86df8883a79a7492798ce78429088f2e6
beep boop <githubbot@vadim.me>
2024-07-10 16:29:22 +0200

    Solution 1 of Python/86, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/h4ka8s36 Prompt: Complete the following code given the task description and function signature.

--- Python_86.py
+++ Python_86.py
@@ -1,9 +1,8 @@
-```
 def anti_shuffle(s):
     words = s.split()
     result = []
     for word in words:
-        letters = sorted(word)
-        new_word = ''.join(letters)
-        result.append(new_word)
-    return ' '.join(result)
+        char_list = sorted(word)
+        new_word = "".join(char_list)
+        result.append(new_word if len(word) == len(new_word) else word)
+    return " ".join(result)
