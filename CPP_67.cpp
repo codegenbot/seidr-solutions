1da9480fb8a224e42d73b1f4940546900bdf36e6
beep boop <githubbot@vadim.me>
2024-07-10 06:03:46 +0200

    Solution 3 of CPP/67, ratio 0.42857142857142855 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/bsg4us0k Prompt: 0958ff72-da32-435e-83a8-26c78a6376ce.cpp:1:29: error: ‘string’ is not a member o f ‘std’     1 | int fruit_distribution(std::string s, int n) {       |                             ^~~~~~ 0958ff72-da32-435e-83a8-26c78a6376ce.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int fruit_distribution(std::string s, int n) { 0958ff72-da32-435e-83a8-26c78a6376ce.cpp:1:39: error: expected primary-expressio n before ‘int’     1 | int fruit_distribution(std::string s, int n) {       |                                       ^~~ 0958ff72-da32-435e-83a8-26c78a6376ce.cpp:1:44: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int fruit_distribution(std::string s, int n) {       |                                            ^ In file included from 0958ff72-da32-435e-83a8-26c78a6376ce.cpp:8: 0958ff72-da32-435e-83a8-26c78a6376ce.cpp: In function ‘int main()’: 0958ff72-da32-435e-83a8-26c78a6376ce.cpp:10:31: error: ‘fruit_distribution’ cann ot be used as a function    10 |     assert (fruit_distribution("1 apples and 100 oranges",120) == 19);       |             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,4 +1,6 @@
-int apples, oranges, mango;
+#include <string>
+int fruit_distribution(std::string s, int n) {
+    int apples, oranges, mango;
     sscanf(s.c_str(), "%d apples and %d oranges", &apples, &oranges);
     mango = n - apples - oranges;
     return mango;
