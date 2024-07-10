489c7151509e4a431d31851df803f158631e0cba
beep boop <githubbot@vadim.me>
2024-07-10 22:43:19 +0200

    Solution 22 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/qew3a3cc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6ee38b1a-a77c-447e-a3f4-8ae9ac6fa f87.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_144.py
+++ Python_144.py
@@ -1,13 +1,8 @@
-def simplify(x, n):
-    a, b = map(int, x.split("/"))
-    c, d = map(int, n.split("/"))
-
-    greatest_common_divisor = lambda a, b: (
-        a if not b else greatest_common_divisor(b, a % b)
-    )
-
-    gcd = greatest_common_divisor(
-        greatest_common_divisor(a, b), greatest_common_divisor(c, d)
-    )
-
-    return f"{a//gcd}/{b//gcd}" / f"{c//gcd}/{d//gcd}"
+def simplified fractions(numerator_list, denominator_list):
+    result = []
+    for x in numerator_list:
+        for y in denominator_list:
+            if float(x)/x == float(y)/y and x!=0 and y!=0:
+                if [int(x), int(y)] not in result and [int(y), int(x)] not in result:
+                    result.append([int(x), int(y)])
+    return result
