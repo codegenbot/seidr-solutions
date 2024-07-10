ec3f316cfa8390ec32b83b73a23faa30c9e23404
beep boop <githubbot@vadim.me>
2024-07-10 10:14:48 +0200

    Solution 99 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/d1mqdnp5 Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,4 +1,5 @@
 Here is the completed code:
 
 def find_zero(xs: list):
-    return -xs[1] / xs[0] if len(xs) == 2 else -sum(xs[2::2]) / sum(xs[1::2])
+    n = len(xs) // 2
+    return -xs[n-1] / xs[-1]
