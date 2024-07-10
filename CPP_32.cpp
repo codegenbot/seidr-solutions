584b3a6fcc7e148cade3a55d0c8120f6f546f8b8
beep boop <githubbot@vadim.me>
2024-07-10 03:33:42 +0200

    Solution 27 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/j9l2m9oz Prompt: In file included from /usr/include/c++/11/vector:67,                  from 29e534b9-c780-43f2-82ca-7142e1f5419a.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 29e534b9-c780-43f2-82ca-7142e1f5419a.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 29e534b9-c780-43f2-82ca-7142e1f5419a.cpp:38:16: error: expected primary-expressi on before ‘double’    38 |         vector<double> coeffs = {};       |                ^~~~~~ 29e534b9-c780-43f2-82ca-7142e1f5419a.cpp:43:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    43 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 29e534b9-c780-43f2-82ca-7142e1f5419a.cpp:46:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    46 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -6,7 +6,7 @@
 double poly(const std::vector<double>& coeffs, double x) {
     double result = 0.0;
     for (int i = 0; i < coeffs.size(); ++i) {
-        result += coeffs[i] * pow(x, i);
+        result += coeffs[i] * std::pow(x, i);
     }
     return result;
 }
@@ -19,7 +19,7 @@ double find_zero(const std::vector<double>& coeffs) {
 
 int main() {
     std::vector<double> coeffs = {1.0, -4.0, 4.0};
-    
+
     double solution;
     solution = find_zero(coeffs);
     assert(std::abs(poly(coeffs, solution)) < 1e-2);
