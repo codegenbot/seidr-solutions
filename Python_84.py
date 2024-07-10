c266a56791a1d4351d92203557d12cada57ed19e
beep boop <githubbot@vadim.me>
2024-07-10 16:36:49 +0200

    Solution 23 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h4xv3ep2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/25c09049-8565-4a2d-91d0-966f8b783 8ea.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,2 +1,13 @@
+```
 def solve(N):
-    return "1" * N if N > 0 else ""
+    result = ""
+    for i in range(1, N+1):
+        if i % 3 == 0 and i % 5 != 0:
+            result += "Fizz"
+        elif i % 3 != 0 and i % 5 == 0:
+            result += "Buzz"
+        elif i % 3 == 0 and i % 5 == 0:
+            result += "FizzBuzz"
+        else:
+            result += str(i)
+    return result
