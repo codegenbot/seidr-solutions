a2c36ea3c95edccf8c87ce41ffbf287fc9439b55
beep boop <githubbot@vadim.me>
2024-07-10 06:17:02 +0200

    Solution 69 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/rhv60fcv Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -11,7 +11,7 @@ int main() {
     while (true) {
         getline(std::cin, num);
         if (num == "stop") break;
-        nums.push_back(num.substr(0, num.size() - 1)); // remove newline character
+        nums.push_back(std::to_string(stoi(num.substr(0, num.size() - 1).c_str()))); 
     }
     std::cout << "Number of special numbers: " << specialFilter(nums) << std::endl;
 }
@@ -19,7 +19,7 @@ int main() {
 int specialFilter(const std::vector<std::string>& nums) {
     int count = 0;
     for (const auto& num : nums) {
-        if (num.length() > 1 && num.length() <= 10 && std::stoi(std::to_string((num[9] - '0')))[0] % 2 != 0 && std::stoi(std::to_string(((num[8] - '0') / 10)))[0] % 2 != 0) {
+        if (num.length() > 1 && num.length() <= 10 && (num.back() - '0') % 2 != 0 && ((num[8] - '0') / 10) % 10 % 2 != 0) {
             count++;
         }
     }
