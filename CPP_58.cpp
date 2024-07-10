22240fdecc792ab53c752fd8fa54b9d32b7de0e8
beep boop <githubbot@vadim.me>
2024-07-10 02:46:56 +0200

    Solution 44 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0hw5ynmd Prompt:       |             ^~~~~~ 0c5e76c8-7d07-4f02-90b0-502d4d08a347.cpp:55:34: error: expected primary-expressi on before ‘int’    55 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0c5e76c8-7d07-4f02-90b0-502d4d08a347.cpp:63:5: error: redefinition of ‘int main( )’    63 | int main(){       |     ^~~~ 0c5e76c8-7d07-4f02-90b0-502d4d08a347.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 0c5e76c8-7d07-4f02-90b0-502d4d08a347.cpp:54: 0c5e76c8-7d07-4f02-90b0-502d4d08a347.cpp: In function ‘int main()’: 0c5e76c8-7d07-4f02-90b0-502d4d08a347.cpp:64:20: error: ‘common’ was not declared  in this scope    64 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 0c5e76c8-7d07-4f02-90b0-502d4d08a347.cpp:64:19: error: ‘issame’ cannot be used a s a function    64 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,30 +1,29 @@
-```cpp
 #include <vector>
 #include <set>
 
-bool issame(vector<vector<int>> a, vector<vector<int>> b) {
+bool same(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b) {
     return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin(),
                                                   [](const auto& v1, const auto& v2) {
-                                                      return issame(v1, v2);
+                                                      return same(v1, v2);
                                                   });
 }
 
-vector<int> common(vector<int> l1, vector<int> l2) {
-    set<int> s1(l1.begin(), l1.end());
-    set<int> s2(l2.begin(), l2.end());
+std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
+    std::set<int> s1(l1.begin(), l1.end());
+    std::set<int> s2(l2.begin(), l2.end());
 
-    vector<int> result(s1.begin(), s1.end());  // start the output with s1
+    std::vector<int> result(s1.begin(), s1.end());  // start the output with s1
     std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
                           back_inserter(result));
     
     return result;
 }
 
-int main() {
+int main_renamed() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<int>> l1(n), l2(m);
+    std::vector<std::vector<int>> l1(n), l2(m);
 
     for (int i = 0; i < n; i++) {
         l1[i].resize(4);
@@ -40,7 +39,7 @@ int main() {
         }
     }
 
-    vector<int> result = common({l1[0][0], l1[0][1], l1[0][2], l1[0][3]}, {l2[0][0], l2[0][1], l2[0][2], l2[0][3]});
+    std::vector<int> result = common({l1[0][0], l1[0][1], l1[0][2], l1[0][3]}, {l2[0][0], l2[0][1], l2[0][2], l2[0][3]});
 
     for (int i = 0; i < result.size(); i++) {
         cout << result[i];
