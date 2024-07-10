88940152e804ee2d9f1645da1c745139eda4baf6
beep boop <githubbot@vadim.me>
2024-07-10 03:46:44 +0200

    Solution 50 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/j8k6sljz Prompt: 621471a7-41e9-4df1-89b6-9f00359df181.cpp:33:22: error: too few arguments to func tion ‘bool check_if_same(std::vector<std::__cxx11::basic_string<char> >, std::ve ctor<std::__cxx11::basic_string<char> >)’    33 |     if (check_if_same(total_match(user_input, vector<string>() {{""}})),  vector<string>(5))) {       |         ~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 621471a7-41e9-4df1-89b6-9f00359df181.cpp:7:6: note: declared here     7 | bool check_if_same(vector<string> a, vector<string> b) {       |      ^~~~~~~~~~~~~ 621471a7-41e9-4df1-89b6-9f00359df181.cpp:33:92: error: expected primary-expressi on before ‘)’ token    33 | otal_match(user_input, vector<string>() {{""}})), vector<string>(5))) {       |                                                                     ^ 621471a7-41e9-4df1-89b6-9f00359df181.cpp: At global scope: 621471a7-41e9-4df1-89b6-9f00359df181.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ 621471a7-41e9-4df1-89b6-9f00359df181.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -30,7 +30,7 @@ int main() {
         user_input.push_back(input);
         
     }
-    if (check_if_same(total_match(user_input, vector<string>(1)), user_input)) {
+    if (check_if_same(total_match(user_input, vector<string>(1, "")), user_input)) {
         cout << "The two lists are the same." << endl;
     } else {
         cout << "Error: The two lists are not the same." << endl;
