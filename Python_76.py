a606fcc5bb8700e71f79481fcda59723f50c9cad
beep boop <githubbot@vadim.me>
2024-07-10 16:30:35 +0200

    Solution 10 of Python/76, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/hzxqx3to Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/655b1909-3af8-4053-a090-92ad05216 445.py", line 11, in <module>     check(is_simple_power)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/655b1909-3af8-4053-a090-92ad05216 445.py", line 7, in check     is_simple_power(1, 12) == True AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_76.py
+++ Python_76.py
@@ -1,7 +1,4 @@
+```
+import math
 def is_simple_power(x, n):
-    i = 2
-    while i <= round(n**0.5) + 1:
-        if x == n**i:
-            return True
-        i += 1
-    return False
+    return math.isclose(x, n ** (math.sqrt(n)), rel_tol=1e-9)
