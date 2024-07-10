be555bdd1f8915d521e093122bd8d3fa3806be13
beep boop <githubbot@vadim.me>
2024-07-10 07:13:56 +0200

    Solution 78 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tn1r4gui Prompt:       |                    ^~~ 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp:16: 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp: In function ‘int main()’: 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp:26:20: error: ‘strange_sort_list’ was n ot declared in this scope    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,10 +1,4 @@
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
-}
+bool issame(vector<int> a, vector<int> b) {}
 
 vector<int> strange_sort_list(vector<int> lst) {
     vector<int> result;
@@ -22,6 +16,6 @@ vector<int> strange_sort_list(vector<int> lst) {
 }
 
 int main() {
-    assert(is_same(strange_sort_list({111111}) , {111111}));
+    assert(strange_sort_list({111111}) == vector<int>({111111}));
     return 0;
 }
