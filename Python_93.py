8eccda471e8355b41f63a9743002eac6be038f3c
beep boop <githubbot@vadim.me>
2024-07-10 16:39:54 +0200

    Solution 14 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/f2ci197u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e9a41359-e0fd-46a0-8d6e-e0efd87fb 1c0.py", line 22, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e9a41359-e0fd-46a0-8d6e-e0efd87fb 1c0.py", line 18, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -4,10 +4,10 @@ def encode(message):
     for char in message:
         if char.isalpha():
             if char.lower() in vowels:
-                index = (vowels.index(char.lower()) + 2) % 6
+                index = (vowels.index(char.lower()) + 1) % 6
                 result += vowels[index].upper() if char.isupper() else vowels[index].lower()
             else:
-                result += char.upper() if char.isupper() else char.lower()
+                result += 'zbcdfghjklmnpqrstvwxyzZBCDFGHJKLMNPQRSTVWXYZ'.replace('z'if char.isupper() else 'Z',char)[0]
         else:
             result += char
     return result
