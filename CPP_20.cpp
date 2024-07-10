a5b81f2fed014571d2798bc4a8d22b3b564e8585
beep boop <githubbot@vadim.me>
2024-07-10 22:44:16 +0200

    Solution 64 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zt6mhwm2 Prompt:       |                             ^~~~~~ b1cf623e-929c-43df-9b14-bef4f581b99b.cpp:28:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from b1cf623e-929c-43df-9b14-bef4f581b99b.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from b1cf623e-929c-43df-9b14-bef4f581b99b.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b1cf623e-929c-43df-9b14-bef4f581b99b.cpp:28:36: error: expected primary-expressi on before ‘float’    28 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ b1cf623e-929c-43df-9b14-bef4f581b99b.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ b1cf623e-929c-43df-9b14-bef4f581b99b.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -19,7 +19,3 @@ std::vector<float> find_closest_elements(std::vector<float> numbers) {
 bool issame(std::vector<float> a, std::vector<float> b) {
     return a == b;
 }
-
-// int main() {
-//     assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}), {2.2, 3.1});
-// }
