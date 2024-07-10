c3bba1478673d8912c05522e4d3dececcb81749e
beep boop <githubbot@vadim.me>
2024-07-10 03:31:59 +0200

    Solution 78 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0hw5ynmd Prompt: a16f6fe4-aa5d-458d-aa26-426e67824556.cpp:69:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    69 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ a16f6fe4-aa5d-458d-aa26-426e67824556.cpp:5:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     5 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ a16f6fe4-aa5d-458d-aa26-426e67824556.cpp:77:5: error: redefinition of ‘int main( )’    77 | int main(){       |     ^~~~ a16f6fe4-aa5d-458d-aa26-426e67824556.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -2,16 +2,7 @@
 #include <iostream>
 using namespace std;
 
-bool isSame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(vector<int> a, vector<int> b) {
 }
 
 vector<int> common(vector<int> a, vector<int> b) {
