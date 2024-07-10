d1f8e594e5234f9d5940eaa417d1028b6e264637
beep boop <githubbot@vadim.me>
2024-07-10 09:28:49 +0200

    Solution 1 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/o1pkvjf1 Prompt:       |                     ^ c65936b7-1f37-416b-9a8b-a63bb58cb351.cpp:4:30: error: ‘n’ was not declared in th is scope     4 |     int den2 = stoi(n.substr(n.find('/') + 1));       |                              ^ c65936b7-1f37-416b-9a8b-a63bb58cb351.cpp:4:16: error: ‘stoi’ was not declared in  this scope     4 |     int den2 = stoi(n.substr(n.find('/') + 1));       |                ^~~~ c65936b7-1f37-416b-9a8b-a63bb58cb351.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return (num1 * num2) % (den1 * den2) == 0;       |     ^~~~~~ c65936b7-1f37-416b-9a8b-a63bb58cb351.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from c65936b7-1f37-416b-9a8b-a63bb58cb351.cpp:9: c65936b7-1f37-416b-9a8b-a63bb58cb351.cpp: In function ‘int main()’: c65936b7-1f37-416b-9a8b-a63bb58cb351.cpp:11:13: error: ‘simplify’ was not declar ed in this scope    11 |     assert (simplify("1/5", "1/5") == false);       |             ^~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,4 +1,7 @@
-int num1 = stoi(x.substr(0, x.find('/')));
+#include <string>
+
+bool checkDivisibility(std::string x, std::string n) {
+    int num1 = stoi(x.substr(0, x.find('/')));
     int den1 = stoi(x.substr(x.find('/') + 1));
     int num2 = stoi(n.substr(0, n.find('/')));
     int den2 = stoi(n.substr(n.find('/') + 1));
