87cfbee914a0425b38e2a9f13e65b37824ab6a6d
beep boop <githubbot@vadim.me>
2024-07-10 09:53:03 +0200

    Solution 27 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/v3alpgmd Prompt: Complete the following code given the task description and function signature.

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -15,9 +15,11 @@ int skjkasdkd(vector<int> lst){
 }
 
 bool isPrime(int n){
-    if(n <= 1) return false;
-    for(int i = 2; i * i <= n; i++)
+    if(n <= 1)
+        return false;
+    for(int i = 2; i * i <= n; i++){
         if(n % i == 0)
             return false;
+    }
     return true;
 }
