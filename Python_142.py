41db70b300f50dba0cba66b3d00a645c7047dee3
beep boop <githubbot@vadim.me>
2024-07-10 17:24:47 +0200

    Solution 16 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/s103d66u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3699d185-9315-4db7-9942-2171e1b99 6bc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,9 +1,9 @@
-def sum_squares():
-    numbers = list(map(int, input("Enter numbers separated by space: ").split()))
+```
+def sum_squares(lst):
     total = 0
-    for i in range(len(numbers)):
+    for i in range(len(lst)):
         if (i + 1) % 3 == 0 and (i + 1) % 4 != 0:
-            total += numbers[i] ** 2
+            total += lst[i] ** 2
         elif (i + 1) % 4 == 0 and (i + 1) % 3 != 0:
-            total += numbers[i] ** 3
-    print("The sum of squares is: ", total)
+            total += lst[i] ** 3
+    return total
