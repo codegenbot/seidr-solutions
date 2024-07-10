6479d5e780eadc720dc6c342f69a02b566f71982
beep boop <githubbot@vadim.me>
2024-07-10 23:39:11 +0200

    Solution 16 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tb7u35g2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1dcca6e-9a73-4992-88e7-d33803798 21b.py", line 15, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1dcca6e-9a73-4992-88e7-d33803798 21b.py", line 12, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_115.py
+++ Python_115.py
@@ -1,5 +1,22 @@
+import math
+
+
 def max_fill(grid, capacity):
     total_water = 0
-    for row in grid:
-        total_water += min(row) * len(row)
+    for row_index in range(len(grid)):
+        for cell_index in range(len(grid[0])):
+            if (
+                row_index == 0
+                or grid[row_index][cell_index] > grid[row_index - 1][cell_index]
+            ):
+                grid[row_index][cell_index] = grid[row_index - 1][cell_index]
+            elif (
+                row_index == len(grid) - 1
+                or grid[row_index][cell_index] > grid[row_index + 1][cell_index]
+            ):
+                grid[row_index][cell_index] = grid[row_index + 1][cell_index]
+            total_water += (
+                min(bucket for bucket in [row[cell_index]] * 2)
+                - grid[row_index][cell_index]
+            )
     return math.ceil(total_water / capacity)
