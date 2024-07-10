96a14fc473873a65f24ad4b42f5ddabf1057cafc
beep boop <githubbot@vadim.me>
2024-07-10 07:50:48 +0200

    Solution 8 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/z32fmi3c Prompt:       |                                     ^~~~~~ 3dfba6e0-51ed-48e3-a7a6-e76e1b3a7044.cpp:40:37: note: suggested alternatives: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 3dfba6e0-51ed-48e3-a7a6-e76e1b3a7044.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 3dfba6e0-51ed-48e3-a7a6-e76e1b3a7044.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 3dfba6e0-51ed-48e3-a7a6-e76e1b3a7044.cpp:40:44: error: ‘b’ was not declared in t his scope    40 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -3,17 +3,15 @@
 #include <string>
 #include <cassert>
 
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b){
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return a == b;
 }
 
-vector<string> select_words(string s, int n) {
-    vector<string> result;
-    string word = "";
+std::vector<std::string> select_words(std::string s, int n) {
+    std::vector<std::string> result;
+    std::string word = "";
     int consonantCount = 0;
-    
+
     for (char c : s) {
         if (c == ' ') {
             if (consonantCount == n) {
@@ -21,19 +19,23 @@ vector<string> select_words(string s, int n) {
             }
             word = "";
             consonantCount = 0;
-        } else if (isalpha(c)) {
-            if (tolower(c) != 'a' && tolower(c) != 'e' && tolower(c) != 'i' && tolower(c) != 'o' && tolower(c) != 'u') {
+        } else if (std::isalpha(c)) {
+            if (std::tolower(c) != 'a' && std::tolower(c) != 'e' && std::tolower(c) != 'i' && std::tolower(c) != 'o' && std::tolower(c) != 'u') {
                 consonantCount++;
             }
             word += c;
         }
     }
-    
+
     if (consonantCount == n) {
         result.push_back(word);
     }
-    
+
     return result;
 }
 
-assert(issame(select_words("a b c d e f", 1), vector<string>{"b", "c", "d", "f"}));
+int main() {
+    assert(issame(select_words("a b c d e f", 1), std::vector<std::string>{"b", "c", "d", "f"}));
+
+    return 0;
+}
