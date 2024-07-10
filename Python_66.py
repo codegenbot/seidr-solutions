9f6a8df740228204141d6afa85ca6f9d61d2b3f6
beep boop <githubbot@vadim.me>
2024-07-10 04:33:11 +0200

    Solution 59 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vnld1i80 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c3f598b9-d4ed-4bb2-afc1-9181b9981 b9b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_66.py
+++ Python_66.py
@@ -2,5 +2,10 @@ def digitSum(s):
     total = 0
     for c in s:
         if c.isdigit():
-            total += int(c) * (2 if total % 2 != 0 else 1)
+            if (total % 2) == 0:
+                total += int(c)
+            else:
+                total += int(c) * 2 - 5 if int(c) > 4 else int(c) * 2
+        else:
+            continue
     return total
