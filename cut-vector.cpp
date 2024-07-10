40b003ef913240999dd89365ed5b86d0880b8bc7
beep boop <githubbot@vadim.me>
2024-07-10 08:41:36 +0200

    Solution 69 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/jtwmj2dd Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,44 +1,33 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+pair<vector<int>, vector<int>> cutVector(vector<int> nums) {
     int min_diff = INT_MAX;
-    int pos = -1;
-    
-    for (int i = 0; i < v.size() - 1; i++) {
-        int diff = abs(v[i] - v[i + 1]);
-        if (diff <= min_diff) {
-            min_diff = diff;
-            pos = i;
+    int cut_index = -1;
+    for (int i = 0; i < nums.size() - 1; i++) {
+        if (abs(nums[i] - nums[i + 1]) <= min_diff) {
+            min_diff = abs(nums[i] - nums[i + 1]);
+            cut_index = i;
         }
     }
-    
-    vector<int> left, right;
-    for (int i = 0; i < pos; i++) {
-        left.push_back(v[i]);
-    }
-    for (int i = pos + 1; i < v.size(); i++) {
-        right.push_back(v[i]);
-    }
-    
-    return {left, right};
+    vector<int> left = vector<int>(nums.begin(), nums.begin() + cut_index);
+    vector<int> right = vector<int>(nums.begin() + cut_index, nums.end());
+    return make_pair(left, right);
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> v(n);
-    for (auto &x : v) cin >> x;
-
-    auto res = cutVector(v);
-
-    cout << "Left: ";
-    for (int x : res.first) cout << x << " ";
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    pair<vector<int>, vector<int>> result = cutVector(nums);
+    cout << "1 ";
+    for (int num : result.first) {
+        cout << num << " ";
+    }
     cout << endl;
-    
-    cout << "Right: ";
-    for (int x : res.second) cout << x << " ";
-    cout << endl;
-    
+    cout << "0" << endl;
     return 0;
 }
