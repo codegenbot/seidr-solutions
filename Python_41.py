68ed955f86206af919a317518f2b100f12ebf2ca
beep boop <githubbot@vadim.me>
2024-07-10 21:34:30 +0200

    Solution 82 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/we682yma Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1fa8eb1d-261d-4f7e-9095-5751d08bc 4b1.py", line 16, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1fa8eb1d-261d-4f7e-9095-5751d08bc 4b1.py", line 13, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_41.py
+++ Python_41.py
@@ -1,9 +1,11 @@
-```
 def car_race_collision(n: int = 0, a1: int = 0, a2: int = 0):
-    if n <= 0:
+    if a1 <= 0 and a2 <= 0:
         return 0
     else:
-        total_distance = 0
+        total_distance = (n * max(a1, a2))
         for i in range(1, n):
-            total_distance += min(a1, a2) * (n - i)
+            if a1 >= a2:
+                total_distance += a2 - (n - i) * a1
+            elif a2 >= a1:
+                total_distance += a1 - (n - i) * a2
         return total_distance
