47f3308aeb4096568504c220fbd479392564e360
beep boop <githubbot@vadim.me>
2024-07-10 17:45:55 +0200

    Solution 28 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zsb71vpb Prompt: 012696b9-22ff-47a7-8be7-28c5a2472709.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string exchange(vector<int> lst1, vector<int> lst2) {       | ^~~~~~ In file included from 012696b9-22ff-47a7-8be7-28c5a2472709.cpp:22: 012696b9-22ff-47a7-8be7-28c5a2472709.cpp: In function ‘int main()’: 012696b9-22ff-47a7-8be7-28c5a2472709.cpp:24:13: error: ‘exchange’ was not declar ed in this scope    24 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -11,6 +11,7 @@ std::string exchange(std::vector<int> lst1, std::vector<int> lst2) {
             break;
         }
     }
+    
     if (!foundOdd) return "YES";
     for (int i = 0; i < lst1.size(); i++) {
         for (int j = 0; j < lst2.size(); j++) {
@@ -21,21 +22,11 @@ std::string exchange(std::vector<int> lst1, std::vector<int> lst2) {
             }
         }
     }
+    
     return foundOdd ? "NO" : "YES";
 }
 
 int main() {
     assert(exchange({100, 200}, {200, 200}) == "YES");
-    std::vector<int> list1 = {2, 4, 6};
-    std::vector<int> list2 = {3, 5, 7};
-    if (exchange(list1, list2) == "NO") {
-        for (int i = 0; i < list1.size(); i++) {
-            for (int j = 0; j < list2.size(); j++) {
-                if (list1[i] % 2 != 0 && list2[j] % 2 == 0) {
-                    std::swap(list1[i], list2[j]);
-                }
-            }
-        }
-    }
     return 0;
 }
