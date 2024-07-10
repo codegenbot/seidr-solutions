2f5cd74098c2dcdbabfe8a3a52eab34737de774b
beep boop <githubbot@vadim.me>
2024-07-10 14:07:32 +0200

    Solution 76 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/pudsv9p5 Prompt:       |                    ^~~~~ 0145e932-f971-4932-afbc-efd15402ff0f.cpp:10:29: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 0145e932-f971-4932-afbc-efd15402ff0f.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 0145e932-f971-4932-afbc-efd15402ff0f.cpp:10:43: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 0145e932-f971-4932-afbc-efd15402ff0f.cpp:9: 0145e932-f971-4932-afbc-efd15402ff0f.cpp: In function ‘int main()’: 0145e932-f971-4932-afbc-efd15402ff0f.cpp:19:20: error: ‘get_positive’ was not de clared in this scope    19 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 0145e932-f971-4932-afbc-efd15402ff0f.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -2,27 +2,27 @@
 #include <cassert>
 
 bool issame(std::vector<float> a, std::vector<float> b){
-    if(a.size() != b.size()) {
+    if (a.size() != b.size()) {
         return false;
     }
-    
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
-    
     return true;
 }
 
 int main() {
-    std::vector<float> l = {1.5, -2.3, 3.0, 4.7, -5.2};
+    std::vector<float> l = {1.5, -2.3, 4.0, 5.6, -7.8, 9.1};
     std::vector<float> positive_numbers;
     for (float num : l) {
         if (num > 0) {
             positive_numbers.push_back(num);
         }
     }
-    
-    assert(issame(positive_numbers, {1.5, 3.0, 4.7}));
+
+    assert(issame(positive_numbers, {1.5, 4.0, 5.6, 9.1}));
+
+    return 0;
 }
