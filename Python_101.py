a62df5768d87f3923e9d54d6819f1964e1e3b424
beep boop <githubbot@vadim.me>
2024-07-10 21:50:51 +0200

    Solution 9 of Python/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zec2zvxr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/429c2a1d-cf11-4f5a-94b9-96d0aba3a ae1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_101.py
+++ Python_101.py
@@ -1,10 +1,7 @@
-def capitalize_and_swap(input_string):
+def sentence_reverser(input_string):
     def words_string(s):
         return s.replace(",", " ").split()
 
     output = words_string(input_string)
-    capitalized_output = [word.capitalize() for word in output]
-    if len(output) > 1:
-        return " ".join(capitalized_output)[::-1]
-    else:
-        return " ".join(capitalized_output)
+    reversed_sentence = " ".join(output[::-1])
+    return reversed_sentence
