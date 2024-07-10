f6cc45b5d13ce38a0354083e4c41240f0324fdac
beep boop <githubbot@vadim.me>
2024-07-10 22:40:57 +0200

    Solution 22 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/p5501436 Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ afd8e7de-ce3a-4113-86cb-99eb15e10dbd.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ afd8e7de-ce3a-4113-86cb-99eb15e10dbd.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from afd8e7de-ce3a-4113-86cb-99eb15e10dbd.cpp:5: afd8e7de-ce3a-4113-86cb-99eb15e10dbd.cpp: In function ‘int main()’: afd8e7de-ce3a-4113-86cb-99eb15e10dbd.cpp:15:20: error: ‘unique’ was not declared  in this scope    15 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ afd8e7de-ce3a-4113-86cb-99eb15e10dbd.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,11 +1,11 @@
 #include <algorithm>
 #include <vector>
 
-bool issame(const std::vector<int>& v1, const std::vector<int>& v2) {
+bool issame(const std::vector<int> &v1, const std::vector<int> &v2) {
     if (v1.size() != v2.size()) {
         return false;
     }
-
+    
     for (size_t i = 0; i < v1.size(); ++i) {
         if (v1[i] != v2[i]) {
             return false;
@@ -15,7 +15,7 @@ bool issame(const std::vector<int>& v1, const std::vector<int>& v2) {
     return true;
 }
 
-std::vector<int> customSort(std::vector<int> l) {
+std::vector<int> solve(std::vector<int> l) {
     sort(l.begin(), l.end());
     l.erase(unique(l.begin(), l.end(), issame), l.end());
     return l;
