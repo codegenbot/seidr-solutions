080e504d34f01ea077a21e22b52b5646f33313d4
beep boop <githubbot@vadim.me>
2024-07-10 21:11:59 +0200

    Solution 22 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05855f50-ac97-4606-9509-303fc9476 697.py", line 17, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05855f50-ac97-4606-9509-303fc9476 697.py", line 12, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05855f50-ac97-4606-9509-303fc9476 697.py", line 7, in sort_third     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05855f50-ac97-4606-9509-303fc9476 697.py", line 8, in <listcomp>     (x if i % 3 != 0 else (x if i % 3 == 1 else l3.pop(0)))                                                 ^^^^^^^^^ IndexError: pop from empty list

--- Python_33.py
+++ Python_33.py
@@ -1,12 +1,5 @@
-```
-def sort_third(l: list):
+```Python
+def sort_third(l):
     l.sort()
-    l1 = [x for i, x in enumerate(l) if i % 3 == 0]
-    l2 = [x for i, x in enumerate(l) if i % 3 == 1]
-    l3 = [x for i, x in enumerate(l) if i % 3 == 2]
-
-    return [
-        (x if i % 3 != 0 else (x if i % 3 == 1 else l3.pop(0))) 
-        if len(l3) > 0 else (x if i % 3 != 0 else x)
-        for i, x in enumerate(l1 + l2 + list(reversed(l3)))
-    ]
+    return [x if i % 3 != 0 else (x if i % 3 == 1 else l.pop(0)) 
+            for i, x in enumerate((i:=iter(l)))][::-1]
