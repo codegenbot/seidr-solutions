e7881372c53ca32257247cf089f95fa448454e71
beep boop <githubbot@vadim.me>
2024-07-10 07:03:05 +0200

    Solution 51 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/i8c2iimk Prompt:       |                  ^~~~~~ 2695b45f-87e9-472f-894e-798c1f6c8442.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                         ^~~ 2695b45f-87e9-472f-894e-798c1f6c8442.cpp:1:33: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                                 ^~~ 2695b45f-87e9-472f-894e-798c1f6c8442.cpp:1:38: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool will_it_fly(vector<int> q, int w) {       |                                      ^ 2695b45f-87e9-472f-894e-798c1f6c8442.cpp:14:19: error: ‘string’ was not declared  in this scope    14 | bool isPalindrome(string s) {       |                   ^~~~~~ In file included from 2695b45f-87e9-472f-894e-798c1f6c8442.cpp:24: 2695b45f-87e9-472f-894e-798c1f6c8442.cpp: In function ‘int main()’: 2695b45f-87e9-472f-894e-798c1f6c8442.cpp:26:24: error: ‘will_it_fly’ cannot be u sed as a function    26 |     assert (will_it_fly({5}, 5) == true);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,10 +1,10 @@
 #include <vector>
 #include <string>
 
-bool will_it_fly(vector<int> q, int w) {
-    string str = "";
+bool will_it_fly(std::vector<int> q, int w) {
+    std::string str = "";
     for (int i : q) {
-        str += to_string(i);
+        str += std::to_string(i);
     }
     if (!isPalindrome(str)) return false;
     int sum = 0;
@@ -14,7 +14,7 @@ bool will_it_fly(vector<int> q, int w) {
     return sum <= w;
 }
 
-bool isPalindrome(string s) {
+bool isPalindrome(std::string s) {
     int left = 0, right = s.length() - 1;
     while (left < right) {
         if (s[left] != s[right]) return false;
