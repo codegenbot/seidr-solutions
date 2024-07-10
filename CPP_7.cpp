1b33ad278bfd2f4da5a3f7a3f72ad6eeabf85058
beep boop <githubbot@vadim.me>
2024-07-10 05:42:26 +0200

    Solution 42 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tuv5tenk Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b0d1aeae-7e4e-4745-94fc-ce9ff0570e00.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ b0d1aeae-7e4e-4745-94fc-ce9ff0570e00.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from b0d1aeae-7e4e-4745-94fc-ce9ff0570e00.cpp:10: b0d1aeae-7e4e-4745-94fc-ce9ff0570e00.cpp: In function ‘int main()’: b0d1aeae-7e4e-4745-94fc-ce9ff0570e00.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ b0d1aeae-7e4e-4745-94fc-ce9ff0570e00.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,15 +1,22 @@
-#include <vector>
-#include <string>
-#include <algorithm>
+vector<string> filter_by_substring(vector<string> strings, string substring){
+    vector<string> result;
+    for(string s : strings) {
+        if(s.find(substring) != string::npos)
+            result.push_back(s);
+    }
+    return result;
 
-bool operator==(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    if (a.size() != b.size())
+}
+
+bool operator==(vector<string> a, vector<string>b){
+    if(a.size() != b.size())
         return false;
-    for (size_t i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
+    for(size_t i = 0; i < a.size(); i++){
+        if(a[i] != b[i])
             return false;
     }
     return true;
+
 }
 
 int main(){
