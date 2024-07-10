d60c6ed0bd2a65503617e3bb8ec6f2e044047ace
beep boop <githubbot@vadim.me>
2024-07-10 05:22:04 +0200

    Solution 94 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/7y3sq4wl Prompt: ined here    18 | int main() {       |     ^~~~ In file included from c8bfc099-c322-4412-b34e-374aff111e87.cpp:29: c8bfc099-c322-4412-b34e-374aff111e87.cpp: In function ‘int main()’: c8bfc099-c322-4412-b34e-374aff111e87.cpp:39:29: error: cannot bind non-const lva lue reference of type ‘std::vector<float>&’ to an rvalue of type ‘std::vector<fl oat>’    39 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from c8bfc099-c322-4412-b34e-374aff111e87.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<float>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ c8bfc099-c322-4412-b34e-374aff111e87.cpp:8:36: note:   initializing argument 1 o f ‘void sort_even(std::vector<float>&)’     8 | void sort_even(std::vector<float>& vec) {       |                ~~~~~~~~~~~~~~~~~~~~^~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -5,15 +5,9 @@
 
 bool issame(const std::vector<float>& a, const std::vector<float>& b);
 
-void sort_even(const std::vector<float>& vec) {
-    std::sort(vec.begin(), vec.end(), [&](float a, float b) {
-        if (static_cast<int>(a) % 2 == 0 && static_cast<int>(b) % 2 == 0) {
-            return a < b;
-        } else if (static_cast<int>(a) % 2 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+void sort_even(std::vector<float> vec) {
+    std::sort(vec.begin(), vec.end(), [](float a, float b) {
+        return (static_cast<int>(a) % 2 == 0) && (static_cast<int>(b) % 2 == 0) && a < b;
     });
 }
 
@@ -23,11 +17,10 @@ bool issame(const std::vector<float>& a, const std::vector<float>& b) {
 
 int main() {
     std::vector<float> testNumbers = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
-    std::vector<float> sortedNumbers = testNumbers;
-    sort_even(sortedNumbers);
-    assert(issame(sortedNumbers, {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    sort_even(std::move(testNumbers));
+    assert(issame(testNumbers, {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
 
-    for (const auto& num : sortedNumbers) {
+    for (const auto& num : testNumbers) {
         std::cout << num << " ";
     }
     return 0;
