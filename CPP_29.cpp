98d2a6e92d2a2d0a36e21177e7b7804f4c034c21
beep boop <githubbot@vadim.me>
2024-07-10 23:45:09 +0200

    Solution 37 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/07y95gq9 Prompt: Complete the following code given the task description and function signature.

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,6 +1,6 @@
 vector<string> filter_by_prefix(vector<string> strings, string prefix){
     vector<string> result;
-    for(auto s : strings){
+    for(string s : strings){
         if(s.find(prefix) == 0)
             result.push_back(s);
     }
