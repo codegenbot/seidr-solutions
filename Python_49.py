766baedb36b0dfea1c23f12ba8391740a76d517e
beep boop <githubbot@vadim.me>
2024-07-10 16:23:54 +0200

    Solution 22 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/izkc6bd7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/12947c96-9698-4e3f-bdbb-f2bdfb31e 368.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,19 +1,6 @@
-def phi(n):
-    res = n
-    p = 2
-    while p * p <= n:
-        if not (n % p):
-            while not (n % p):
-                n //= p
-            res -= res // p
-        else:
-            p += 1
-    if n > 1:
-        res -= res // n
-    return res
-
 def modp(n: int, p: int):
     if n < 1:
         return pow(n, p - 1, p)
     else:
-        return pow(n, phi(n) % (p-1), p)
+        return n ** (p-1) % p
+    return n % p
