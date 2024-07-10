d6281294758103b4009f43928f7eeb5a85be4ce6
beep boop <githubbot@vadim.me>
2024-07-10 20:59:36 +0200

    Solution 17 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/v3l4k5fc Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 22a6ab3a-1f38-42f0-9c3f-504d9773e387.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 22a6ab3a-1f38-42f0-9c3f-504d9773e387.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 22a6ab3a-1f38-42f0-9c3f-504d9773e387.cpp:26: 22a6ab3a-1f38-42f0-9c3f-504d9773e387.cpp: In function ‘int main()’: 22a6ab3a-1f38-42f0-9c3f-504d9773e387.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 22a6ab3a-1f38-42f0-9c3f-504d9773e387.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -6,18 +6,6 @@ vector<string> separate_paren_groups(string paren_string);
 
 bool issame(vector<string> a, vector<string> b);
 
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
     string group;
@@ -44,6 +32,13 @@ vector<string> separate_paren_groups(string paren_string) {
 }
 
 int main() {
-    // Code to test the separate_paren_groups function
+    string input;
+    cout << "Enter a string with parentheses: ";
+    cin >> input;
+    vector<string> groups = separate_paren_groups(input);
+    for (const auto& group : groups) {
+        cout << group << endl;
+    }
+
     return 0;
 }
