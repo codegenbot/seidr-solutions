0c29c7d4e36d49672fc5b30bde082991a271ebf5
beep boop <githubbot@vadim.me>
2024-07-10 04:01:54 +0200

    Solution 16 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/upshh664 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a565d480-18b0-4299-8ade-af2bcd4df b30.py", line 2     return "".join(groups) IndentationError: unexpected indent

--- Python_38.py
+++ Python_38.py
@@ -1,5 +1,2 @@
-def decode_cyclic(s: str):
-    decoded = [s[i] for i in range(1, len(s), 3)]
-    decoded.insert(0, s[-1])
-    decoded += [s[i] for i in range(0, len(s), 3) if i != len(s) - 1]
-    return "".join(decoded)
+groups = [group[-1] + group[:-1] if len(group) == 3 else group for group in groups]
+return "".join(groups)
