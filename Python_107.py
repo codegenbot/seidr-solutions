fb0988f3c5169209fa0826d16b81dfadcd6e1719
beep boop <githubbot@vadim.me>
2024-07-10 16:38:27 +0200

    Solution 0 of Python/107, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8lyd3ea7 Prompt: Complete the following code given the task description and function signature.

---
+++ Python_107.py
@@ -0,0 +1,6 @@
+def even_odd_palindrome(n):
+    count_even = sum(
+        1 for i in range(1, n + 1) if str(i) == str(i)[::-1] and i % 2 == 0
+    )
+    count_odd = sum(1 for i in range(1, n + 1) if str(i) == str(i)[::-1] and i % 2 != 0)
+    return (count_even, count_odd)
