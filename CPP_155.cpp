847a96a0df25db33b046b6c4be43217d92cd59e8
beep boop <githubbot@vadim.me>
2024-07-11 01:36:14 +0200

    Solution 5 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/anmdhyr2 Prompt:       |                    ^~~ 9680f4a9-f7a7-45ae-af35-5b8a0d9a7b5b.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9680f4a9-f7a7-45ae-af35-5b8a0d9a7b5b.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9680f4a9-f7a7-45ae-af35-5b8a0d9a7b5b.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9680f4a9-f7a7-45ae-af35-5b8a0d9a7b5b.cpp:13: 9680f4a9-f7a7-45ae-af35-5b8a0d9a7b5b.cpp: In function ‘int main()’: 9680f4a9-f7a7-45ae-af35-5b8a0d9a7b5b.cpp:23:20: error: ‘even_odd_count’ was not declared in this scope    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 9680f4a9-f7a7-45ae-af35-5b8a0d9a7b5b.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,11 +1,9 @@
 #include <vector>
-#include <bits/stdc++.h>
+#include <string>
 
-using namespace std;
-
-vector<int> even_odd_count(int num) {
+std::vector<int> even_odd_count(int num) {
     int count_even = 0, count_odd = 0;
-    string str_num = to_string(abs(num));
+    std::string str_num = std::to_string(std::abs(num));
     for (char c : str_num) {
         if ((c - '0') % 2 == 0)
             ++count_even;
@@ -13,17 +11,4 @@ vector<int> even_odd_count(int num) {
             ++count_odd;
     }
     return {count_even, count_odd};
-}
-
-int main() {
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    
-    vector<int> result = even_odd_count(num);
-    
-    cout << "Number of even digits: " << result[0] << endl;
-    cout << "Number of odd digits: " << result[1] << endl;
-
-    return 0;
 }
