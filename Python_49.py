dc3dce7a85a75c0eb09a51ae264a767a568345bf
beep boop <githubbot@vadim.me>
2024-07-10 22:24:46 +0200

    Solution 95 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/m88lgrnk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f2c6b881-3d7c-4155-977e-8d5649ef8 2bc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,7 +1,8 @@
+```
 def modp(n: int, p: int) -> int:
     if not isinstance(n, int) or not isinstance(p, int):
         return None
-    elif p <= 0:
+    if p <= 0:
         return None
     else:
         result = 1
