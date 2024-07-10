ea3bc0b8a8eecc6ba3b1afd8abfb0a380b90b5b6
beep boop <githubbot@vadim.me>
2024-07-10 22:36:45 +0200

    Solution 42 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rmdf06ap Prompt:       |                                 ^~~~ de376bd3-3dfa-4aef-9f5d-ebb77742a1cd.cpp:33:38: error: expected primary-expressi on before ‘int’    33 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ de376bd3-3dfa-4aef-9f5d-ebb77742a1cd.cpp:33:44: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ de376bd3-3dfa-4aef-9f5d-ebb77742a1cd.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ de376bd3-3dfa-4aef-9f5d-ebb77742a1cd.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from de376bd3-3dfa-4aef-9f5d-ebb77742a1cd.cpp:32: de376bd3-3dfa-4aef-9f5d-ebb77742a1cd.cpp: In function ‘int main()’: de376bd3-3dfa-4aef-9f5d-ebb77742a1cd.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,14 +1,30 @@
 #include <map>
 #include <string>
 
-// Prototype for histogram function
-std::map<char, int> histogram(std::string);
+std::map<char, int> histogram(std::string s) {
+    std::map<char, int> freqMap;
+    for (char c : s) {
+        if (freqMap.find(c) == freqMap.end()) {
+            freqMap[c] = 1;
+        } else {
+            freqMap[c]++;
+        }
+    }
+    return freqMap;
+}
 
-bool std::issame(std::map<char,int> a,std::map<char,int> b){
-    return a == b;
+bool issameMap(std::map<char, int> a, std::map<char, int> b) {
+    if (a.size() != b.size())
+        return false;
+
+    for (auto it = a.begin(); it != a.end();++it)
+        if (it->second != b.at(it->first))
+            return false;
+
+    return true;
 }
 
 int main() {
-    assert(issame(histogram("a"), { {'a', 1} }));
+    assert(issameMap(histogram("a"), { {'a', 1} }));
     return 0;
 }
