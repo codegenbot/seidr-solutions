5084857240888409269608bd33334a6543be7b16
beep boop <githubbot@vadim.me>
2024-07-10 09:50:20 +0200

    Solution 68 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ubu20nj8 Prompt: In file included from c880ee3a-bdea-4f38-9577-01e83fbc9805.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c880ee3a-bdea-4f38-9577-01e83fbc9805.cpp:6:16: error: wrong number of template a rguments (1, should be 2)     6 | pair<vector<int>, vector<int>> cutVector(vector<int>& v) {       |                ^ In file included from /usr/include/c++/11/bits/stl_algobase.h:64,                  from /usr/include/c++/11/vector:60,                  from c880ee3a-bdea-4f38-9577-01e83fbc9805.cpp:1: /usr/include/c++/11/bits/stl_pair.h:211:12: note: provided for ‘template<class _ T1, class _T2> struct std::pair’   211 |     struct pair       |            ^~~~ c880ee3a-bdea-4f38-9577-01e83fbc9805.cpp:6:17: error: expected unqualified-id be fore ‘,’ token     6 | pair<vector<int>, vector<int>> cutVector(vector<int>& v) {       |                 ^ c880ee3a-bdea-4f38-9577-01e83fbc9805.cpp:6:25: error: expected initializer befor e ‘<’ token     6 | pair<vector<int>, vector<int>> cutVector(vector<int>& v) {       |                         ^

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,13 +1,12 @@
-using namespace std;
-pair<vector<int>, vector<int>> cutVector(vector<int>& v) {
+auto cutVector(vector<int>& v) -> pair<vector<int>, vector<int>> {
     int n = v.size();
     long long total_sum = accumulate(v.begin(), v.end(), 0LL);
-
+    
     pair<long long, long long> prefix_sum({0LL}, accumulate(v.begin(), v.end(), 0LL));
-
+    
     int min_diff = INT_MAX;
     vector<int> left, right;
-
+    
     for (int i = 0; i < n; ++i) {
         if (prefix_sum.second - prefix_sum.first >= total_sum / 2) {
             min_diff = min(min_diff, abs(total_sum - 2 * prefix_sum.second));
@@ -15,6 +14,6 @@ pair<vector<int>, vector<int>> cutVector(vector<int>& v) {
             right = vector<int>(v.end() - (n - i - 1), v.end());
         }
     }
-
+    
     return make_pair(left, right);
 }
