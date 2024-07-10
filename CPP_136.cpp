5c0e8f01fd3c7c397f5dc1d6c53905e8be173e1d
beep boop <githubbot@vadim.me>
2024-07-10 07:57:09 +0200

    Solution 4 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/yor8a7be Prompt: Complete the following code given the task description and function signature.

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -3,17 +3,16 @@
 using namespace std;
 
 vector<int> largest_smallest_integers(vector<int> lst) {
-    int max_negative = 0;
-    int min_positive = INT_MAX;
-
+    int max_neg = 0;
+    int min_pos = INT_MAX;
+    
     for (int num : lst) {
-        if (num < 0 && num > max_negative) {
-            max_negative = num;
-        }
-        else if (num > 0 && num < min_positive) {
-            min_positive = num;
+        if (num < 0 && abs(num) > max_neg) {
+            max_neg = num;
+        } else if (num > 0 && num < min_pos) {
+            min_pos = num;
         }
     }
-
-    return {(max_negative >= 0) ? 0 : max_negative, (min_positive <= 0) ? 0 : min_positive};
+    
+    return {(max_neg > 0) ? 0 : max_neg, (min_pos <= 0) ? 0 : min_pos};
 }
