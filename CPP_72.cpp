a7a2da2b332b4d907bf0f1f2a1a53cfe21f67b00
beep boop <githubbot@vadim.me>
2024-07-11 01:07:36 +0200

    Solution 32 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ivscl8ll Prompt: Complete the following code given the task description and function signature.

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -3,8 +3,10 @@ bool will_it_fly(vector<int> q, int w) {
     for (int i : q) {
         str += to_string(i);
     }
-    if (str != reverse(str).s) return false; // not balanced
+    if (str != reverse(str).s) return false;
     int sum = 0;
-    for (int i : q) sum += i;
-    return sum <= w; // within weight limit
+    for (int i : q) {
+        sum += i;
+    }
+    return sum <= w;
 }
