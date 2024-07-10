302bb465d1a63684f2782cba4e726145cfd39dca
beep boop <githubbot@vadim.me>
2024-07-10 20:17:09 +0200

    Solution 9 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9ut2k3v9 Prompt: Complete the following code given the task description and function signature.

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -5,18 +5,11 @@ bool will_it_fly(vector<int> q, int w) {
     for (int i : q) {
         str += to_string(i);
     }
-    if (!str.equals(reverse(str))) return false;
+    
+    if (str != reverse(str).s) return false;
+    
     int sum = 0;
-    for (int i : q) {
-        sum += i;
-    }
+    for (int i : q) sum += i;
+    
     return sum <= w;
-}
-
-string reverse(string s) {
-    string rev = "";
-    for (int i = s.length() - 1; i >= 0; i--) {
-        rev += s[i];
-    }
-    return rev;
 }
