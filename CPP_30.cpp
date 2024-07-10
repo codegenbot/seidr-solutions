c08cf46976b6904a3c99571fad585869e1dc7deb
beep boop <githubbot@vadim.me>
2024-07-10 16:52:46 +0200

    Solution 80 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/yy6dvdhj Prompt:       |                    ^~~~~ 31ea5f6f-052d-4d69-80a4-ccd77da57cdf.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 31ea5f6f-052d-4d69-80a4-ccd77da57cdf.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 31ea5f6f-052d-4d69-80a4-ccd77da57cdf.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 31ea5f6f-052d-4d69-80a4-ccd77da57cdf.cpp:11: 31ea5f6f-052d-4d69-80a4-ccd77da57cdf.cpp: In function ‘int main()’: 31ea5f6f-052d-4d69-80a4-ccd77da57cdf.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 31ea5f6f-052d-4d69-80a4-ccd77da57cdf.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,8 +1,11 @@
 #include <vector>
-#include <algorithm>
 
-bool issame(vector<float> a,vector<float>b){
-    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
+bool issame(vector<float> a, vector<float> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
 }
 
 vector<float> get_positive(vector<float> l) {
@@ -13,4 +16,9 @@ vector<float> get_positive(vector<float> l) {
         }
     }
     return result;
+}
+
+int main() {
+    assert(issame(get_positive({}), {}));
+    return 0;
 }
