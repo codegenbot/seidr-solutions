d6adf8d0cb6b4c539060b8d32278ab028a116aca
beep boop <githubbot@vadim.me>
2024-07-10 21:43:32 +0200

    Solution 81 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6mp2wjk0 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ efa78f27-f05d-49e3-a74d-b29da9dae38d.cpp:20:21:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -8,11 +8,11 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 
 int main() {
     int n1, n2;
-    std::vector<int> v1, v2;
 
     std::cout << "Enter number of elements in the first vector: ";
     std::cin >> n1;
-    v1.resize(n1);
+    std::vector<int> v1(n1);
+
     for (int i = 0; i < n1; i++) {
         int x;
         std::cout << "Enter element " << i + 1 << ": ";
@@ -22,7 +22,8 @@ int main() {
 
     std::cout << "\nEnter number of elements in the second vector: ";
     std::cin >> n2;
-    v2.resize(n2);
+    std::vector<int> v2(n2);
+
     for (int i = 0; i < n2; i++) {
         int x;
         std::cout << "Enter element " << i + 1 << ": ";
