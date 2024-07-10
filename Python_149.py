2b2a85c18d222a8952514c595f433b8fec00140d
beep boop <githubbot@vadim.me>
2024-07-10 08:30:45 +0200

    Solution 69 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j9zzr72u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f6e3558e-d80c-4bbc-b58d-e693033f2 c42.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -2,16 +2,16 @@
 def main():
     while True:
         try:
-            lst = (input("Enter words (separated by space): ").lower().replace(" ", "").split())
+            lst = input("Enter words (separated by space): ").lower().replace(" ", "").split()
             break
         except Exception as e:
             print(f"Error: {str(e)}")
             continue
 
     if len(lst) > 0:
+        sorted_list_sum = sum(len(word) for word in lst if len(word) % 2 == 0)
         while True:
             try:
-                sorted_list_sum = sum(len(word) for word in lst if len(word) % 2 == 0)
                 input_val = int(input("Press Enter to calculate the sum or type 'q' to quit: "))
                 if input_val == 113:
                     break
