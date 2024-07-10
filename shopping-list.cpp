d90b3bf7b3394057d41e94d69e58db37c815905e
beep boop <githubbot@vadim.me>
2024-07-10 22:00:27 +0200

    Solution 30 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/8cx4yx6f Prompt: n before ‘float’     9 |     vector<float> discounts(n);       |            ^~~~~ 330f6f00-3ee8-4722-b9e3-486cd3083f4f.cpp:11:16: error: ‘discounts’ was not decla red in this scope    11 |         cin >> discounts[i];       |                ^~~~~~~~~ 330f6f00-3ee8-4722-b9e3-486cd3083f4f.cpp:16:18: error: ‘prices’ was not declared  in this scope    16 |         total += prices[i] * (1.0 - discounts[i] / 100.0);       |                  ^~~~~~ 330f6f00-3ee8-4722-b9e3-486cd3083f4f.cpp:16:37: error: ‘discounts’ was not decla red in this scope    16 |         total += prices[i] * (1.0 - discounts[i] / 100.0);       |                                     ^~~~~~~~~ 330f6f00-3ee8-4722-b9e3-486cd3083f4f.cpp:19:5: error: ‘cout’ was not declared in  this scope    19 |     cout << total << endl;       |     ^~~~ 330f6f00-3ee8-4722-b9e3-486cd3083f4f.cpp:19:22: error: ‘endl’ was not declared i n this scope    19 |     cout << total << endl;       |                      ^~~~

--- shopping-list.cpp
+++ shopping-list.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
+    
     vector<float> prices(n);
     vector<float> discounts(n);
     
@@ -17,6 +18,7 @@ int main() {
     }
     
     float total = 0.0;
+    
     for (int i = 0; i < n; ++i) {
         total += prices[i] * (1.0 - discounts[i] / 100.0);
     }
