07912aa382a49a39d916c4317f8cd756e24db041
beep boop <githubbot@vadim.me>
2024-07-10 22:23:05 +0200

    Solution 89 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/kfejmui6 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,30 +1,14 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int>& vec) {
-    int n = vec.size();
-    vector<vector<int>> result;
-    
-    if (n == 0)
-        return {{}};
-    
-    if (n == 1)
-        return {{vec}, {}};
-    
-    for (int i = 1; i < n; i++) {
-        long long sum1 = 0, sum2 = 0;
-        
-        for (int j = 0; j < i; j++)
-            sum1 += vec[j];
-        
-        for (int j = i; j < n; j++)
-            sum2 += vec[j];
-        
-        if (abs(sum1 - sum2) <= abs((sum1 + sum2) / 2 - sum2))
-            result = {{vec.substr(0, i)}};
-        else
-            result = {{vec.substr(0, i)}, {vec.substr(i, n-i)}};
+pair<vector<int>, vector<int>> cutVector(vector<int>& nums) {
+    int minDiff = INT_MAX;
+    int splitIndex = 0;
+    for (int i = 1; i < nums.size(); i++) {
+        int diff = abs(nums[i] - nums[i-1]);
+        if (diff < minDiff) {
+            minDiff = diff;
+            splitIndex = i;
+        }
     }
-    
-    return result;
-}
+    return {vector<int>(nums.begin(), nums.begin() + splitIndex), vector<int>(nums.begin() + splitIndex, nums.end())};
