37da8e74abcade78c6aec10932de5f89d4ad2630
beep boop <githubbot@vadim.me>
2024-07-10 21:48:48 +0200

    Solution 29 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k7a6o2at Prompt:       | ^~~~~~~~~~~~~ bae1d9c9-3223-425c-b58b-c345ad146fe7.cpp:30:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    30 | int main(){       |         ^~ bae1d9c9-3223-425c-b58b-c345ad146fe7.cpp:30:9: note: remove parentheses to defau lt-initialize a variable    30 | int main(){       |         ^~       |         -- bae1d9c9-3223-425c-b58b-c345ad146fe7.cpp:30:9: note: or replace parentheses with  braces to value-initialize a variable bae1d9c9-3223-425c-b58b-c345ad146fe7.cpp:30:11: error: a function-definition is not allowed here before ‘{’ token    30 | int main(){       |           ^ bae1d9c9-3223-425c-b58b-c345ad146fe7.cpp:32:2: error: expected ‘}’ at end of inp ut    32 | }       |  ^ bae1d9c9-3223-425c-b58b-c345ad146fe7.cpp:15:20: note: to match this ‘{’    15 | bool isPrime(int n){       |                    ^

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -20,8 +20,7 @@ bool isPrime(int n){
     }
     return true;
 
-int main 
-{
+int main {
     std::vector<int> lst = {127, 97, 8192}; 
     int result = skjkasdkd(lst); 
     std::cout << "The sum is: " << result << std::endl; 
