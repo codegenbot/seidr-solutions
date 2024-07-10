f0ac00e9c778beadb13670922ea816b0ddb92a6b
beep boop <githubbot@vadim.me>
2024-07-10 06:38:12 +0200

    Solution 10 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3zzocd7v Prompt: 9bbe2f23-2cc3-495b-9f49-43697566962f.cpp:1:22: error: ‘vector’ was not declared in this scope     1 | bool below_threshold(vector<int> l, int t) {       |                      ^~~~~~ 9bbe2f23-2cc3-495b-9f49-43697566962f.cpp:1:29: error: expected primary-expressio n before ‘int’     1 | bool below_threshold(vector<int> l, int t) {       |                             ^~~ 9bbe2f23-2cc3-495b-9f49-43697566962f.cpp:1:37: error: expected primary-expressio n before ‘int’     1 | bool below_threshold(vector<int> l, int t) {       |                                     ^~~ 9bbe2f23-2cc3-495b-9f49-43697566962f.cpp:1:42: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool below_threshold(vector<int> l, int t) {       |                                          ^ In file included from 9bbe2f23-2cc3-495b-9f49-43697566962f.cpp:10: 9bbe2f23-2cc3-495b-9f49-43697566962f.cpp: In function ‘int main()’: 9bbe2f23-2cc3-495b-9f49-43697566962f.cpp:12:32: error: ‘below_threshold’ cannot be used as a function    12 |     assert (not(below_threshold({1, 8, 4, 10}, 10)));       |                 ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,6 +1,10 @@
+#include <vector>
+
 bool below_threshold(vector<int> l, int t) {
-    for (int i : l) {
-        if (i >= t) return false;
+    for (int num : l) {
+        if (num >= t) {
+            return false;
+        }
     }
     return true;
 }
