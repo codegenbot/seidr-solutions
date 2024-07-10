135aca1acd5db70528addc6fdd3cc6eb0be33c2b
beep boop <githubbot@vadim.me>
2024-07-10 17:37:30 +0200

    Solution 27 of CPP/109, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/xnrq9wza Prompt: 768faf1c-873e-41d5-b3e6-3546883101dc.cpp:1:20: error: ‘vector’ was not declared in this scope     1 | bool move_one_ball(vector<int> arr) {       |                    ^~~~~~ 768faf1c-873e-41d5-b3e6-3546883101dc.cpp:1:27: error: expected primary-expressio n before ‘int’     1 | bool move_one_ball(vector<int> arr) {       |                           ^~~ In file included from 768faf1c-873e-41d5-b3e6-3546883101dc.cpp:11: 768faf1c-873e-41d5-b3e6-3546883101dc.cpp: In function ‘int main()’: 768faf1c-873e-41d5-b3e6-3546883101dc.cpp:13:26: error: ‘move_one_ball’ cannot be  used as a function    13 |     assert (move_one_ball({})==true);       |             ~~~~~~~~~~~~~^~~~

--- CPP_109.cpp
+++ CPP_109.cpp
@@ -13,11 +13,37 @@ bool move_one_ball(std::vector<int> arr) {
 
 int main() {
     assert(move_one_ball({}) == true);
-    std::vector<int> test1 = {5,4,3,2,1};
-    assert(move_one_ball(test1));
+    std::vector<int> test1 = {1,2,3,4,5};
+    for(int i=0; i<test1.size();i++){
+        if(test1[i] != 6-i){
+            return 1;
+        }
+    }
+    assert(move_one_ball({1,2,3,4,5}) == true);
     
-    std::vector<int> test2 = {1,2,3,4,5};
-    assert(!move_one_ball(test2));
+    std::vector<int> test2 = {1};
+    for(int i=0; i<test2.size();i++){
+        if(test2[i] != 0){
+            return 1;
+        }
+    }
+    assert(move_one_ball({1}) == true);
+
+    std::vector<int> test3 = {5,4,3,2,1};
+    for(int i=0; i<test3.size();i++){
+        if(test3[i] != 6-i){
+            return 1;
+        }
+    }
+    assert(move_one_ball({5,4,3,2,1}) == true);
+
+    std::vector<int> test4 = {5};
+    for(int i=0; i<test4.size();i++){
+        if(test4[i] != 5){
+            return 1;
+        }
+    }
+    assert(move_one_ball({5}) == true);
     
     return 0;
 }
