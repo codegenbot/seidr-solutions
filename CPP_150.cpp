dcea2b8de319713eba275697e21d21c1a6909106
beep boop <githubbot@vadim.me>
2024-07-11 01:52:38 +0200

    Solution 22 of CPP/150, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zl0ckraw Prompt: f1159242-b446-49d2-8ea5-3449514bf022.cpp:24:9: error: ‘cout’ was not declared in  this scope    24 |         cout << y << endl;       |         ^~~~ f1159242-b446-49d2-8ea5-3449514bf022.cpp:24:22: error: ‘endl’ was not declared i n this scope    24 |         cout << y << endl;       |                      ^~~~ f1159242-b446-49d2-8ea5-3449514bf022.cpp: At global scope: f1159242-b446-49d2-8ea5-3449514bf022.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ f1159242-b446-49d2-8ea5-3449514bf022.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from f1159242-b446-49d2-8ea5-3449514bf022.cpp:30: f1159242-b446-49d2-8ea5-3449514bf022.cpp: In function ‘int main()’: f1159242-b446-49d2-8ea5-3449514bf022.cpp:32:13: error: ‘x_or_y’ was not declared  in this scope    32 |     assert (x_or_y(2, 2, 0) == 2);       |             ^~~~~~

--- CPP_150.cpp
+++ CPP_150.cpp
@@ -1,24 +1,29 @@
-```cpp
 #include <cmath>
-
-bool isPrime(int num);
-
-int x_or_y(int n, int x, int y) {
-    if (isPrime(n)) {
-        return x;
-    } else {
-        return y;
-    }
-}
+#include <iostream>
 
 bool isPrime(int num) {
     if (num <= 1) {
         return false;
     }
-    for (int i = 2; i < std::sqrt((double)num); i++) {
+    for (int i = 2; i <= sqrt(num); i++) {
         if (num % i == 0) {
             return false;
         }
     }
     return true;
+}
+
+int main() {
+    int n, x, y;
+    cin >> n >> x >> y;
+
+    if (n <= 1) {
+        cout << x << endl;
+    } else if (isPrime(n)) {
+        cout << x << endl;
+    } else {
+        cout << y << endl;
+    }
+
+    return 0;
 }
