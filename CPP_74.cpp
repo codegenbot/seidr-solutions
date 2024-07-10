afefa1f4b9b813c35a92c5f1ffa239cc75a676e6
beep boop <githubbot@vadim.me>
2024-07-10 03:25:55 +0200

    Solution 31 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/j8k6sljz Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 9576301f-e7b9-45e8-aad1-ebf4990b3daf.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -8,7 +8,7 @@ bool is_same(vector<string> a, vector<string> b) {
     return std::equal(a.begin(), a.end(), b.begin(), b.end());
 }
 
-std::vector<std::string> user_list_comparer(std::vector<std::string> lst1, std::vector<std::string> lst2) {
+vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     int sum1 = 0;
     for (const string& s : lst1) {
         sum1 += s.length();
@@ -19,7 +19,7 @@ std::vector<std::string> user_list_comparer(std::vector<std::string> lst1, std::
     }
     if (sum1 < sum2) return lst1;
     else if (sum1 > sum2) return lst2;
-    else return {};
+    else return vector<string>();
 }
 
 int main() {
@@ -30,7 +30,7 @@ int main() {
         user_input.push_back(input);
     }
     
-    if (is_same(user_list_comparer(user_input, {}), {})) {
+    if (is_same(total_match(user_input, vector<string>(5)), vector<string>(5))) {
         cout << "The two lists are the same." << endl;
     } else {
         cout << "Error: The two lists are not the same." << endl;
