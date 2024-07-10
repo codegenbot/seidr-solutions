ae1706b6fd35cf9eadb991ce9d37d32629a3a80d
beep boop <githubbot@vadim.me>
2024-07-10 21:54:57 +0200

    Solution 79 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5o0wk2gj Prompt:       |                              ^~~~~~ 684a853f-4d29-417d-bf9a-1f40f76a3311.cpp:25:37: error: ‘string’ was not declared  in this scope    25 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 684a853f-4d29-417d-bf9a-1f40f76a3311.cpp:25:44: error: ‘b’ was not declared in t his scope    25 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 684a853f-4d29-417d-bf9a-1f40f76a3311.cpp:25:45: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 684a853f-4d29-417d-bf9a-1f40f76a3311.cpp:24: 684a853f-4d29-417d-bf9a-1f40f76a3311.cpp: In function ‘int main()’: 684a853f-4d29-417d-bf9a-1f40f76a3311.cpp:34:20: error: ‘reverse_delete’ was not declared in this scope    34 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 684a853f-4d29-417d-bf9a-1f40f76a3311.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -23,7 +23,3 @@ std::vector<std::string> reverse_delete(std::string s, std::string c) {
     result.push_back((is_palindrome ? "True" : "False"));
     return result;
 }
-
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    return a == b;
-}
