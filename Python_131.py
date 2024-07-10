fcb66e8b700e7bc740f770d0a69cae2609e75133
beep boop <githubbot@vadim.me>
2024-07-11 01:56:30 +0200

    Solution 63 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7vpcfgx6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4a50a6b4-148b-467b-801b-1c727a85d 403.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_131.py
+++ Python_131.py
@@ -1,10 +1,11 @@
-try:
-    n = int(input("Enter a number: "))
-except ValueError:
-    print("Invalid input. Please enter an integer.")
-else:
-    product = 1
-    for digit in str(n):
-        if digit != '0':
-            product *= int(digit)
-    print(product)
+while True:
+    try:
+        n = int(input("Enter a number: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+product = 1
+for digit in str(n):
+    if digit != '0':
+        product *= int(digit)
+print(product)
