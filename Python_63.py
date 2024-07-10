e66b04dbe1468c9171b5ec8d6a34e372cab8554d
beep boop <githubbot@vadim.me>
2024-07-11 00:22:42 +0200

    Solution 98 of Python/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/prguudkt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5ac741a9-6cf5-426d-b9f7-48dd6be85 f68.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_63.py
+++ Python_63.py
@@ -1,18 +1,18 @@
 def fibfib(n: int):
     if n <= 1:
-        return str(0)
-    elif n % 2 == 0:
+        return "0"
+    elif n % 2 == 0:  
         a, b = 0, 1
         result = [str(a)]
         while b * b < n:
-            a, b = b, a + b
+            a, b = b, a+b
             result.append(str(a))
-        if int(result[-1]) == 1:
+        if result[-1] == "1":
             return "9"
         else:
             return "8"
     else:
         if n > 1:
-            if n <= 2:
-                return str(0)
-            return str(int(fibfib(n - 1)) + int(fibfib(n - 2)))
+            if n <= 2: 
+                return "0"
+            return str(fibfib(n-1) + fibfib(n-2))
