cf4950fbda366fda70449a61eb56111740717a79
beep boop <githubbot@vadim.me>
2024-07-11 00:19:25 +0200

    Solution 20 of CPP/146, ratio 0.8571428571428571 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/oz21z9z9 Prompt: 1116c87c-2d4d-4c8b-9175-7555a3f4d535.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 1116c87c-2d4d-4c8b-9175-7555a3f4d535.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,4 +1,7 @@
-int specialFilter(vector<int> nums){
+#include <iostream>
+#include <vector>
+
+int specialFilter(std::vector<int> nums){
     int count = 0;
     for(int num : nums){
         if(num > 10 && (num % 10) % 2 != 0 && (num / 10) % 10 % 2 != 0)
