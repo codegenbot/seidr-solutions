24a43fda9b5acf3d7a11127d162431c9ef32faa9
beep boop <githubbot@vadim.me>
2024-07-10 20:59:01 +0200

    Solution 26 of CPP/149, ratio 0.7142857142857143 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/4zoeewsz Prompt: 1af7f616-36b8-47e3-848f-d59bc5e7ff1d.cpp:29:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    29 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 1af7f616-36b8-47e3-848f-d59bc5e7ff1d.cpp:8:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     8 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ 1af7f616-36b8-47e3-848f-d59bc5e7ff1d.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 1af7f616-36b8-47e3-848f-d59bc5e7ff1d.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -5,10 +5,6 @@
 
 using namespace std;
 
-bool issame(const vector<string>& a, const vector<string>& b) {
-    return a == b;
-}
-
 vector<string> sorted_list_sum(vector<string> lst) {
     lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s) { return s.length() % 2 != 0; }), lst.end());
     sort(lst.begin(), lst.end(), [](const string& a, const string& b) {
@@ -18,9 +14,4 @@ vector<string> sorted_list_sum(vector<string> lst) {
         return a.length() < b.length();
     });
     return lst;
-}
-
-int main() {
-    assert(issame(sorted_list_sum({ "aaaa", "bbbb", "dd", "cc" }), { "cc", "dd", "aaaa", "bbbb" }));
-    return 0;
 }
