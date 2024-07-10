83d90a3d6bd6be8fcad247203a8256a8089273bc
beep boop <githubbot@vadim.me>
2024-07-10 23:02:18 +0200

    Solution 38 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zckjcwmr Prompt: 98 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/88652bb0-2c38-483d-9240-b2d6619d1 f0f.py", line 15, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/88652bb0-2c38-483d-9240-b2d6619d1 f0f.py", line 11, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,6 +1,12 @@
-```
 def double_the_difference(lst):
-    odd_sum = sum(i**2 for i in lst if isinstance(i, int) and i >= 0 and i % 2 != 0)
+    odd_sum = 0
+    for i in lst:
+        if isinstance(i, int) and i >= 0 and i % 2 != 0:
+            odd_sum += i**2
+        elif isinstance(i, str):
+            pass
+        else:
+            raise ValueError("Invalid input type")
     return abs(odd_sum - min(lst)) * 2
 
-print(double_the_difference([7, 1]))
+print(double_the_difference(['7', '1']))
