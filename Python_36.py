707f02047f8096731ecae6210b2f0c1f6ae5d297
beep boop <githubbot@vadim.me>
2024-07-10 19:34:43 +0200

    Solution 14 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ie2x67wo Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6391e3c6-f89f-4793-b531-6b1df8be8 2e4.py", line 17, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6391e3c6-f89f-4793-b531-6b1df8be8 2e4.py", line 14, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,11 +1,6 @@
-```
 def fizz_buzz(n: int):
-    result = [str(i) for i in range(1, n+1)]
+    count = 0
     for i in range(1, n+1):
-        if i % 3 == 0:
-            result[i-1] = "Fizz" + result[i-1] if result[i-1].isdigit() else "Fizz"
-        if i % 5 == 0:
-            result[i-1] = "Buzz" + result[i-1] if not result[i-1].startswith("Fizz") and result[i-1].isdigit() else "Buzz"
-        if i % 15 == 0:
-            result[i-1] = "FizzBuzz" 
-    return "\n".join(result)
+        if i % 3 == 0 and i % 5 == 0:
+            count += 1
+    return str(count)
