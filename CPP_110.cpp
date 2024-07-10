4213a682c3156af5988704e0a0136c237835e0e6
beep boop <githubbot@vadim.me>
2024-07-10 20:52:17 +0200

    Solution 26 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9kylqdb8 Prompt: c21de36e-d1ab-43d9-a4af-5382901ca378.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string exchange(vector<int> lst1, vector<int> lst2) {       | ^~~~~~ In file included from c21de36e-d1ab-43d9-a4af-5382901ca378.cpp:18: c21de36e-d1ab-43d9-a4af-5382901ca378.cpp: In function ‘int main()’: c21de36e-d1ab-43d9-a4af-5382901ca378.cpp:20:13: error: ‘exchange’ was not declar ed in this scope    20 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,17 +1,21 @@
+```cpp
+#include <string>
+using namespace std;
+
 int exchange(vector<int> lst1, vector<int> lst2) {
-    int flag = 0;
     for (int num : lst1) {
         if (num % 2 != 0) {
             bool found = false;
             for (int num2 : lst2) {
                 if (num2 % 2 == 0) {
-                    swap(lst1[stoi(to_string(num))], lst2[stoi(to_string(num2))]);
+                    int index1 = stoi(to_string(num));
+                    int index2 = stoi(to_string(num2));
+                    swap(lst1[index1], lst2[index2]);
                     found = true;
-                    flag = 1;
                     break;
                 }
             }
-            if (!found && flag == 0) return 0;
+            if (!found) return 0;
         }
     }
     return 1;
