57f2e7af7fa075fb027f07a5923c6345111e2f25
beep boop <githubbot@vadim.me>
2024-07-10 04:00:53 +0200

    Solution 57 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/5lwwp8ur Prompt: In file included from /usr/include/c++/11/cassert:44,                  from 2cc4c37d-dae3-4e6f-a043-ebe260b10877.cpp:3: /usr/include/assert.h:92: note: macro "assert" defined here    92 | #  define assert(expr) \       | 2cc4c37d-dae3-4e6f-a043-ebe260b10877.cpp:23:1: error: ‘assert’ does not name a t ype    23 | assert(result == vector<int>{10, 30});       | ^~~~~~ 2cc4c37d-dae3-4e6f-a043-ebe260b10877.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <cassert>   +++ |+#include <cassert>     4 | 2cc4c37d-dae3-4e6f-a043-ebe260b10877.cpp:25:1: error: ‘cout’ does not name a typ e    25 | cout << "Sum: " << result[0] << endl;       | ^~~~ 2cc4c37d-dae3-4e6f-a043-ebe260b10877.cpp:26:1: error: ‘cout’ does not name a typ e    26 | cout << "Product: " << result[1] << endl;       | ^~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -7,29 +7,20 @@ using namespace std;
 vector<int> sum_product(vector<int> nums){
     int sum = 0;
     int product = 1;
-    
+
     for(int num : nums){
         sum += num;
         product *= num;
     }
-    
+
     return {sum, product};
 }
 
 int main(){
-    vector<int> nums;
-    int n;
-    cin >> n;
-    
-    for(int i=0; i<n; i++){
-        int num;
-        cin >> num;
-        nums.push_back(num);
-    }
-
+    vector<int> nums = {2, 5, 3};
     vector<int> result = sum_product(nums);
 
-    assert(result == vector<int>{10, 10});
+    assert(result == vector<int>{10, 30});
 
     cout << "Sum: " << result[0] << endl;
     cout << "Product: " << result[1] << endl;
