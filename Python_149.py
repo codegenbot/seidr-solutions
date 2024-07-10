1dabb1110a8a5876e5fa412a9770305bbb151ece
beep boop <githubbot@vadim.me>
2024-07-10 05:21:06 +0200

    Solution 15 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j9zzr72u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1100f845-aba4-4d6e-8ea2-a0b55da3e c05.py", line 11, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1100f845-aba4-4d6e-8ea2-a0b55da3e c05.py", line 8, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1100f845-aba4-4d6e-8ea2-a0b55da3e c05.py", line 4, in sorted_list_sum     return sum(even_words)            ^^^^^^^^^^^^^^^ TypeError: unsupported operand type(s) for +: 'int' and 'str'

--- Python_149.py
+++ Python_149.py
@@ -1,6 +1,5 @@
+```
 def sorted_list_sum(lst):
-    even_words = sorted(
-        [(word, len(word)) for word in lst if len(word) % 2 == 0], key=lambda x: x[1]
-    )
-
-    return [word for word, _ in even_words]
+    even_words = sorted([word for word in lst if len(word) % 2 == 0])
+    
+    return even_words
