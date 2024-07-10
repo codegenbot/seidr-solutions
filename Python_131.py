4b361ac6463f3a3ae052a82316d4412cd33e34a7
beep boop <githubbot@vadim.me>
2024-07-11 00:35:14 +0200

    Solution 41 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7vpcfgx6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d6f2b31-1ab4-4c14-a277-e83a79640 747.py", line 15, in <module>     check(digits)           ^^^^^^ NameError: name 'digits' is not defined

--- Python_131.py
+++ Python_131.py
@@ -1,8 +1,8 @@
-```
 def max_product_of_digits(n):
     n = str(n)
-    max_product = 1
-    for digit in sorted(n, reverse=True):
+    digits = list(n) 
+    product = 1
+    for digit in sorted(digits, reverse=True):
         if digit != '0':
-            max_product *= int(digit)
-    return str(max_product) or '0'
+            product *= int(digit)
+    return str(product) or '0'
