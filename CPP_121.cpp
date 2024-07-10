0020898fe41b81d98d631a9bc26e162a1fb87908
beep boop <githubbot@vadim.me>
2024-07-10 08:18:29 +0200

    Solution 23 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/d0rgnz5y Prompt: a0b3f6d3-25b1-43c8-80e0-24d48d7ed770.cpp:7:28: error: ‘i’ was not declared in th is scope     7 |         for (size_t i = 0; i < lst.size(); i += 1) {       |                            ^ a0b3f6d3-25b1-43c8-80e0-24d48d7ed770.cpp: At global scope: a0b3f6d3-25b1-43c8-80e0-24d48d7ed770.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ a0b3f6d3-25b1-43c8-80e0-24d48d7ed770.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from a0b3f6d3-25b1-43c8-80e0-24d48d7ed770.cpp:21: a0b3f6d3-25b1-43c8-80e0-24d48d7ed770.cpp: In function ‘int main()’: a0b3f6d3-25b1-43c8-80e0-24d48d7ed770.cpp:23:13: error: ‘solutions’ was not decla red in this scope; did you mean ‘Contest::solutions’?    23 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~ a0b3f6d3-25b1-43c8-80e0-24d48d7ed770.cpp:5:9: note: ‘Contest::solutions’ declare d here     5 |     int solutions(const std::vector<int>& lst) {       |         ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,11 +1,12 @@
 #include <vector>
 #include <cassert>
+using namespace std;
 
 namespace Contest {
-    int solutions(const std::vector<int>& lst){
+    int solutions(const vector<int>& lst) {
         int sum = 0;
-        for(size_t i = 0; i < lst.size(); i += 1){
-            if(i % 2 == 0 && lst[i] % 2 != 0){
+        for (size_t i = 0; i < lst.size(); i += 1) {
+            if (i % 2 == 0 && lst[i] % 2 != 0) {
                 sum += lst[i];
             }
         }
@@ -13,7 +14,7 @@ namespace Contest {
     }
 }
 
-int main(){
+int main() {
     assert(Contest::solutions({3, 13, 2, 9}) == 13);
     return 0;
 }
