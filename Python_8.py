f082bc84c2eac6a3692afa748bdf68907ad5260f
beep boop <githubbot@vadim.me>
2024-07-10 10:07:19 +0200

    Solution 37 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0ug47qqm Prompt: Complete the following code given the task description and function signature.

--- Python_8.py
+++ Python_8.py
@@ -2,11 +2,7 @@ from typing import List, Tuple
 
 
 def sum_product(numbers: List[int]) -> Tuple[int, int]:
-    total_sum = 0
-    product = 1
-
-    for num in numbers:
-        total_sum += num
-        product *= num
-
-    return total_sum, product if len(numbers) > 0 else (0, 1)
+    return (
+        sum(numbers),
+        1 if len(numbers) == 0 else functools.reduce(lambda x, y: x * y, numbers),
+    )
