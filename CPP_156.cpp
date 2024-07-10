8b38bc333faceb0b61cb65dd63bc71ade155a404
beep boop <githubbot@vadim.me>
2024-07-10 08:22:38 +0200

    Solution 13 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gvtcxb9h Prompt:       |             ~~~~~~^~~~~ /usr/include/c++/11/bits/basic_string.h:6730:3: note: candidate: ‘std::string st d::__cxx11::to_string(double)’  6730 |   to_string(double __val)       |   ^~~~~~~~~ /usr/include/c++/11/bits/basic_string.h:6730:20: note:   no known conversion for  argument 1 from ‘const std::__cxx11::basic_string<char>’ to ‘double’  6730 |   to_string(double __val)       |             ~~~~~~~^~~~~ /usr/include/c++/11/bits/basic_string.h:6739:3: note: candidate: ‘std::string st d::__cxx11::to_string(long double)’  6739 |   to_string(long double __val)       |   ^~~~~~~~~ /usr/include/c++/11/bits/basic_string.h:6739:25: note:   no known conversion for  argument 1 from ‘const std::__cxx11::basic_string<char>’ to ‘long double’  6739 |   to_string(long double __val)       |             ~~~~~~~~~~~~^~~~~ In file included from b6d3d826-c17b-4609-bb0d-ce08b0ef4af6.cpp:22: b6d3d826-c17b-4609-bb0d-ce08b0ef4af6.cpp: In function ‘int main()’: b6d3d826-c17b-4609-bb0d-ce08b0ef4af6.cpp:24:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    24 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,7 +1,4 @@
-#include <vector>
-#include <string>
-using namespace std;
-
+```cpp
 string int_to_mini_roman(int number) {
     vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {500, "D"},
                                          {400, "CD"}, {100, "C"}, {90, "XC"},
