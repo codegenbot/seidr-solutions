25fed06369f7fb8fd7f31195fe0d8407c42776dd
beep boop <githubbot@vadim.me>
2024-07-10 22:20:56 +0200

    Solution 46 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d0761e40-a2b7-4ed6-846d-2b9c58691 21e.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d0761e40-a2b7-4ed6-846d-2b9c58691 21e.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,3 +1,3 @@
 def sort_third(l: list):
-    return sorted([i for sub in l for i in [sub] if isinstance(sub, list)] +
-                  sorted([x for x in l if not isinstance(x, list)]))
+    return sorted([i for subl in l for i in [subl] if isinstance(subl, list)] + 
+                  [x for x in l if not isinstance(x, list)], key=lambda x: (0 if isinstance(x, list) else 1, len(x) if isinstance(x, list) else x))
