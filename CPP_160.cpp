df44566a874847b28a68a98985a9d25e0ce4f874
beep boop <githubbot@vadim.me>
2024-07-10 08:50:43 +0200

    Solution 71 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kd2jtp3g Prompt: 9beb1aed-fc7c-44e3-a639-ce139026b063.cpp:1:61: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                             ^ 9beb1aed-fc7c-44e3-a639-ce139026b063.cpp: In function ‘int eval(char*)’: 9beb1aed-fc7c-44e3-a639-ce139026b063.cpp:20:25: error: ‘atoi’ was not declared i n this scope    20 |     sprintf(temp, "%d", atoi(s));       |                         ^~~~ 9beb1aed-fc7c-44e3-a639-ce139026b063.cpp:20:5: error: ‘sprintf’ was not declared  in this scope    20 |     sprintf(temp, "%d", atoi(s));       |     ^~~~~~~ 9beb1aed-fc7c-44e3-a639-ce139026b063.cpp:1:1: note: ‘sprintf’ is defined in head er ‘<cstdio>’; did you forget to ‘#include <cstdio>’?   +++ |+#include <cstdio>     1 | int do_algebra(vector<string> operator_, vector<int> operand) { In file included from 9beb1aed-fc7c-44e3-a639-ce139026b063.cpp:24: 9beb1aed-fc7c-44e3-a639-ce139026b063.cpp: In function ‘int main()’: 9beb1aed-fc7c-44e3-a639-ce139026b063.cpp:26:23: error: ‘do_algebra’ cannot be us ed as a function    26 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -16,10 +16,10 @@ int do_algebra(vector<string> operator_, vector<int> operand) {
     }
 
     int result = 0;
-    for(int i=1; i<operator_.size(); i++) {
-        if(operator_[i-1] == "+") {
+    for(int i=0; i<operator_.size(); i++) {
+        if(operator_[i] == "+") {
             result += operand[i];
-        } else if(operator_[i-1] == "*") {
+        } else if(operator_[i] == "*") {
             result *= operand[i];
         }
     }
