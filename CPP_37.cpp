e4f21f25bac8cf4e5904e8092ef28d28239e3df9
beep boop <githubbot@vadim.me>
2024-07-10 22:59:29 +0200

    Solution 13 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dp8f9aqb Prompt:    17 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ d615c298-04f3-4e64-9c22-59ebd91b2f66.cpp:17:36: error: expected primary-expressi on before ‘float’    17 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ d615c298-04f3-4e64-9c22-59ebd91b2f66.cpp:17:43: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from d615c298-04f3-4e64-9c22-59ebd91b2f66.cpp:16: d615c298-04f3-4e64-9c22-59ebd91b2f66.cpp: In function ‘int main()’: d615c298-04f3-4e64-9c22-59ebd91b2f66.cpp:26:20: error: ‘sort_even’ was not decla red in this scope    26 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ d615c298-04f3-4e64-9c22-59ebd91b2f66.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,4 +1,5 @@
-vector<float> issame(vector<float> l, vector<float> &sorted_even) {
+vector<float> issame(vector<float> l, int k) {
+    vector<float> sorted_even = l;
     vector<float> even_values;
     for (int i = 0; i < l.size(); i += 2) {
         even_values.push_back(l[i]);
@@ -11,10 +12,4 @@ vector<float> issame(vector<float> l, vector<float> &sorted_even) {
         }
     }
     return sorted_even;
-}
-int main() {
-    vector<float> l = {3.14, 2.71, 1.41, 1.61, 1.73};
-    vector<float> sort_even(l.size());
-    assert(issame(l, sort_even) == vector<float>{1.41, 2.71, 1.61, 3.14, 1.73});
-    return 0;
 }
