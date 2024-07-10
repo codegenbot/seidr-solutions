cdb7579063ef4514f7938e59f4a8647d1fa965ac
beep boop <githubbot@vadim.me>
2024-07-10 14:38:04 +0200

    Solution 86 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/xub5v5hi Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -4,33 +4,26 @@
 int bowlingScore(const std::string& input) {
     int score = 0;
     int rollCount = 0;
-    for (char c : input) {
+    for (auto& c : input) {
         if (c == '/') {
-            if (rollCount >= 2) {
-                score += rollCount * 10 + 10;
-            } else {
+            if (rollCount > 1)
+                score += 10 + rollCount - 2;
+            else
                 score += rollCount * 10;
-            }
             rollCount = 0;
         } else if ('1' <= c && c <= '9') {
             rollCount *= 10 + (c - '0');
-        } else if (c == 'X') {
-            score += 10 + (rollCount > 1 ? rollCount * 10 : 20);
-        } else if (c == '-') {
-            score += rollCount;
-            rollCount = 0;
         }
     }
-    if (rollCount >= 2) {
-        score += rollCount * 10 + 10;
-    } else if (rollCount > 0) {
-        score += rollCount * 10;
-    }
+    score += rollCount;
     return score;
 }
 
 int main() {
-    std::string input; 
+    std::string input;
+    std::cout << "Enter the string representing the individual bowls in a 10-frame round of 10 pin bowling: ";
+    std::cin >> input;
     int result = bowlingScore(input);
-    return result;
+    std::cout << "The total score is: " << result << std::endl;
+    return 0;
 }
