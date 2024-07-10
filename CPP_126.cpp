710cdf4e7c208ae14aa2f7c69c6246c59cee9eaa
beep boop <githubbot@vadim.me>
2024-07-10 21:53:00 +0200

    Solution 2 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vlwtmqlv Prompt: ab7a27df-3bee-4096-b8bd-00858f0e2a6a.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from ab7a27df-3bee-4096-b8bd-00858f0e2a6a.cpp:15: ab7a27df-3bee-4096-b8bd-00858f0e2a6a.cpp: In function ‘int main()’: ab7a27df-3bee-4096-b8bd-00858f0e2a6a.cpp:17:13: error: ‘is_sorted’ was not decla red in this scope    17 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ^~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,13 +1,17 @@
-Here's the completed code:
+#include <vector>
+using namespace std;
 
 bool is_sorted(vector<int> lst){
     for(int i = 1; i < lst.size(); i++){
-        if(lst[i] <= lst[i-1]){
-            vector<int>::iterator it;
-            it = unique(lst.begin(), lst.end());
-            lst.erase(it, lst.end());
+        if(lst[i-1] > lst[i]){
+            return false;
+        }
+        else if(count(lst.begin(), lst.end(), lst[i]) > 1){
             return false;
         }
     }
     return true;
+
+int main() {
+    assert(is_sorted({1, 2, 3, 4}) == true);
 }
