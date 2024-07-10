664ddc372b3dcb4d57a93ad6357a0a3e44a68316
beep boop <githubbot@vadim.me>
2024-07-10 05:14:50 +0200

    Solution 12 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j9zzr72u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d78d5c3f-2a8b-4403-b48f-988af4184 2e5.py", line 11, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d78d5c3f-2a8b-4403-b48f-988af4184 2e5.py", line 8, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,5 +1,3 @@
-```
 def sorted_list_sum(lst):
-    even_words = sorted([(word, len(word)) for word in lst if len(word) % 2 == 0], key=lambda x: x[1])
-    
-    return [word for word, _ in even_words]
+    even_words = [word for word in lst if len(word) % 2 == 0]
+    return ' '.join(sorted(even_words)) if even_words else ''
