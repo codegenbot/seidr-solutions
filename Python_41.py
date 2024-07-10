ba063f5368f1206955c5fd42149f1b218ce9b618
beep boop <githubbot@vadim.me>
2024-07-10 22:25:11 +0200

    Solution 45 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/1li5iway Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d11a1a50-4007-4ec4-ac9c-83a6e8b24 004.py", line 12, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d11a1a50-4007-4ec4-ac9c-83a6e8b24 004.py", line 9, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_41.py
+++ Python_41.py
@@ -1,13 +1,8 @@
-```Python
+```
 def car_race_collision(n=None):
     if n is None:
-        while True:
-            try:
-                n = int(input("Enter the number of cars: "))
-                if n < 2:
-                    print("Number of cars should be at least 2. Please enter again.")
-                else:
-                    break
-            except ValueError:
-                print("Invalid input. Please enter a whole number.")
-    return str((n*(n-1))//2)
+        try:
+            n = int(input("Enter the number of cars: "))
+        except ValueError:
+            n = 1
+    return str(n*(n-1)//2)
