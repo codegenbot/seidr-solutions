1744fefc27376c0fb7d86af9e7d425a34dbcde9b
beep boop <githubbot@vadim.me>
2024-07-10 22:14:21 +0200

    Solution 71 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/mpytqz2q Prompt:  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 613cf7bf-4b04-49e3-9e90-b509cee938b4.cpp:20:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 613cf7bf-4b04-49e3-9e90-b509cee938b4.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 613cf7bf-4b04-49e3-9e90-b509cee938b4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 613cf7bf-4b04-49e3-9e90-b509cee938b4.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 613cf7bf-4b04-49e3-9e90-b509cee938b4.cpp:19: 613cf7bf-4b04-49e3-9e90-b509cee938b4.cpp: In function ‘int main()’: 613cf7bf-4b04-49e3-9e90-b509cee938b4.cpp:29:20: error: ‘maximum’ was not declare d in this scope    29 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -3,7 +3,7 @@
 
 bool issame(std::vector<int> arr) {
     if(arr.empty()) return true;
-    return std::all_of(arr.begin(), arr.end(), [&](int num){return num == arr[0];});
+    return std::all_of(arr.begin(),arr.end(),[&](int num){return num == arr[0];});
 }
 
 std::vector<int> max(std::vector<int> arr, int k){
