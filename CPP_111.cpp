710aa23c96a660af5908bb9e68fb912e054e7418
beep boop <githubbot@vadim.me>
2024-07-10 22:05:38 +0200

    Solution 29 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rmdf06ap Prompt:       |                                 ^~~~ c41990aa-4cbe-46d3-9025-36af0719d09e.cpp:14:38: error: expected primary-expressi on before ‘int’    14 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ c41990aa-4cbe-46d3-9025-36af0719d09e.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ c41990aa-4cbe-46d3-9025-36af0719d09e.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~ In file included from c41990aa-4cbe-46d3-9025-36af0719d09e.cpp:13: c41990aa-4cbe-46d3-9025-36af0719d09e.cpp: In function ‘int main()’: c41990aa-4cbe-46d3-9025-36af0719d09e.cpp:28:20: error: ‘histogram’ was not decla red in this scope    28 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ c41990aa-4cbe-46d3-9025-36af0719d09e.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -3,15 +3,15 @@
 #include <string>
 
 std::map<char,int> histogram(const std::string& str) {
-    std::map<char,int> result;
+    std::map<char,int> res;
     for (char c : str) {
-        if (result.find(c) == result.end()) {
-            result[c] = 1;
+        if(res.find(c) != res.end()) {
+            res[c]++;
         } else {
-            result[c]++;
+            res[c] = 1;
         }
     }
-    return result;
+    return res;
 }
 
 bool issame(std::map<char,int> a, std::map<char,int> b) {
