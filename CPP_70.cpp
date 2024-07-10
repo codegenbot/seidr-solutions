488082eb7bca6c484305c065bd6ff5e692a4d4f6
beep boop <githubbot@vadim.me>
2024-07-11 01:48:10 +0200

    Solution 11 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/i0lazltj Prompt:       |                    ^~~ 49cd2eea-1dbd-4e6c-8f19-779d8698054a.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 49cd2eea-1dbd-4e6c-8f19-779d8698054a.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 49cd2eea-1dbd-4e6c-8f19-779d8698054a.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 49cd2eea-1dbd-4e6c-8f19-779d8698054a.cpp:16: 49cd2eea-1dbd-4e6c-8f19-779d8698054a.cpp: In function ‘int main()’: 49cd2eea-1dbd-4e6c-8f19-779d8698054a.cpp:26:20: error: ‘strange_sort_list’ was n ot declared in this scope    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 49cd2eea-1dbd-4e6c-8f19-779d8698054a.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,13 +2,9 @@
 #include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
+    if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
@@ -28,14 +24,15 @@ std::vector<int> strange_sort_list(std::vector<int> lst){
     return result;
 }
 
-int main() {
-    std::vector<int> test1 = {9, 2, 7, 5, 6, 3};
-    std::vector<int> result1 = strange_sort_list(test1);
-    assert(issame(result1, {2, 9, 3, 7, 5, 6}));
+int main(){
+    // Test case for strange_sort_list function
+    std::vector<int> input = {3, 1, 4, 1, 5, 9};
+    std::vector<int> expected_output = {1, 9, 1, 5, 3, 4};
 
-    std::vector<int> test2 = {3, 1, 4, 1, 5};
-    std::vector<int> result2 = strange_sort_list(test2);
-    assert(issame(result2, {1, 5, 1, 4, 3}));
+    std::vector<int> output = strange_sort_list(input);
+
+    // Validate the test case
+    assert(issame(output, expected_output));
 
     return 0;
 }
