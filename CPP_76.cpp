6dace8e525a52109726394c6963fd036818d714e
beep boop <githubbot@vadim.me>
2024-07-10 03:36:43 +0200

    Solution 24 of CPP/76, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gib6hfmu Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_76.cpp
+++ CPP_76.cpp
@@ -6,8 +6,11 @@
 bool is_simple_power(int x, int n);
 
 bool is_simple_power(int x, int n) {
-    double y = pow(n, log(x) / log(n));
-    return (y == x);
+    std::string str = std::to_string(pow(n, log10(x)/log10(n)));
+    if(std::stoi(str) == x)
+        return true;
+    else 
+        return false;
 }
 
 int main() {
@@ -15,19 +18,14 @@ int main() {
     int tempX;
     std::cin >> tempX;
 
+    int n; 
     std::cout << "Enter another number: ";
-    int nTemp;
-    std::cin >> nTemp;
+    std::cin >> n;
 
-    // Convert integer to string
-    std::string strN = std::to_string(nTemp);
-
-    std::cout << strN << std::endl;
-
-    if(is_simple_power(tempX, nTemp))  
-        std::cout << tempX << " is a simple power of " << nTemp << std::endl;
+    if(is_simple_power(tempX, n))  
+        std::cout << tempX << " is a simple power of " << n << std::endl;
     else
-        std::cout << tempX << " is not a simple power of " << nTemp << std::endl;
+        std::cout << tempX << " is not a simple power of " << n << std::endl;
 
     return 0;
 }
