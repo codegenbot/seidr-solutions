fe43c7e897be7af292d406a36d193c5b0622ba56
beep boop <githubbot@vadim.me>
2024-07-10 08:30:33 +0200

    Solution 5 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d7c6jo29 Prompt: Complete the following code given the task description and function signature.

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,8 +1,7 @@
 vector<string> all_prefixes(string str) {
     vector<string> prefixes;
     for (int i = 1; i <= str.length(); i++) {
-        string prefix = str.substr(0, i);
-        prefixes.push_back(prefix);
+        prefixes.push_back(str.substr(0, i));
     }
     return prefixes;
 }
