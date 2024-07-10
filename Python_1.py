6137b42662788e16c6ef3a00aa1839cc69f91a94
beep boop <githubbot@vadim.me>
2024-07-10 10:45:20 +0200

    Solution 70 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dfoyf7t9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/06112812-54bb-48b1-a410-2d379d735 647.py", line 1, in <module>     def separate_paren_groups(paren_string: str) -> List[str]:                                                     ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_1.py
+++ Python_1.py
@@ -2,12 +2,15 @@ from typing import List
 
 def separate_paren_groups(paren_string: str) -> List[str]:
     result = []
+    count = 0
     temp = ""
     for char in paren_string:
         if char == "(":
-            temp += char
+            count += 1
         elif char == ")":
-            temp += char
-            result.append(temp)
+            count -= 1
+        temp += char
+        if count == 0:
+            result.append(temp.replace(" ", ""))
             temp = ""
     return result
