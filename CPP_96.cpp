8deecba28833022b791034ecc5eeef932f8f1b62
beep boop <githubbot@vadim.me>
2024-07-10 07:19:32 +0200

    Solution 13 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uy7puokw Prompt: In file included from 21455bf9-7d96-4978-8f04-93bebbb0f9e4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 21455bf9-7d96-4978-8f04-93bebbb0f9e4.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 21455bf9-7d96-4978-8f04-93bebbb0f9e4.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 21455bf9-7d96-4978-8f04-93bebbb0f9e4.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~ In file included from 21455bf9-7d96-4978-8f04-93bebbb0f9e4.cpp:14: 21455bf9-7d96-4978-8f04-93bebbb0f9e4.cpp: In function ‘int main()’: 21455bf9-7d96-4978-8f04-93bebbb0f9e4.cpp:24:20: error: ‘count_up_to’ was not dec lared in this scope    24 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -2,38 +2,19 @@
 #include <cassert>
 #include <cmath>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i=0; i<a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 std::vector<int> count_up_to(int n) {
-    std::vector<int> result;
-    for (int i = 2; i <= n; ++i) {
+    std::vector<int> primes;
+    for (int num = 2; num <= n; ++num) {
         bool is_prime = true;
-        for (int j = 2; j <= sqrt(i); ++j) {
-            if (i % j == 0) {
+        for (int i = 2; i <= sqrt(num); ++i) {
+            if (num % i == 0) {
                 is_prime = false;
                 break;
             }
         }
         if (is_prime) {
-            result.push_back(i);
+            primes.push_back(num);
         }
     }
-    return result;
-}
-
-int main() {
-    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 
-                                     37, 41, 43, 47, 53, 59, 61, 67, 71, 
-                                     73, 79, 83, 89, 97}));
-    return 0;
+    return primes;
 }
