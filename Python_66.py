2fb7c2ed318f85b1a799d3443379754c18947d9e
beep boop <githubbot@vadim.me>
2024-07-10 06:53:06 +0200

    Solution 99 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vnld1i80 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a7f0b8f4-42ff-4b40-95c8-90de5eb51 657.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_66.py
+++ Python_66.py
@@ -1,6 +1,8 @@
+```
 def digitSum():
     s = input("Enter a string: ")
     total = sum(int(c) for c in s if c.isdigit())
     print(total)
 
-digitSum()
+if __name__ == "__main__":
+    digitSum()
