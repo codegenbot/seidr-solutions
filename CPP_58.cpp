6ebefc5e49b84ae8fe178426712f6cac471f21d8
beep boop <githubbot@vadim.me>
2024-07-10 19:43:31 +0200

    Solution 28 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6mp2wjk0 Prompt:       |             ^~~~~~ 5bc7ba86-7d11-4cd7-a73c-f31f1b4fc3c4.cpp:8:20: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 5bc7ba86-7d11-4cd7-a73c-f31f1b4fc3c4.cpp:8:27: error: ‘vector’ was not declared in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5bc7ba86-7d11-4cd7-a73c-f31f1b4fc3c4.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 5bc7ba86-7d11-4cd7-a73c-f31f1b4fc3c4.cpp:7: 5bc7ba86-7d11-4cd7-a73c-f31f1b4fc3c4.cpp: In function ‘int main()’: 5bc7ba86-7d11-4cd7-a73c-f31f1b4fc3c4.cpp:17:20: error: ‘common’ was not declared  in this scope    17 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 5bc7ba86-7d11-4cd7-a73c-f31f1b4fc3c4.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,12 +1,7 @@
-```cpp
 #include <vector>
-#include <algorithm>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+#include <initializer_list>
+
+bool issame(std::vector<int> a, std::vector<int> b) {
     return std::equal(a.begin(), a.end(), b.begin());
-}
-
-int main() {
-    assert(issame({4, 3, 2, 8}, {}));
-    return 0;
 }
