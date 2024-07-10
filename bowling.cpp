de399a0b65d4a9f43a8f0ff7d3ba033c6cfe3c75
beep boop <githubbot@vadim.me>
2024-07-11 01:12:54 +0200

    Solution 81 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/5eehrv7c Prompt: 6e2346d7-7f4a-426d-9829-58978facf590.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int bowlingScore(std::string input) {       |                       ^~~~~~ 6e2346d7-7f4a-426d-9829-58978facf590.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int bowlingScore(std::string input) {

--- bowling.cpp
+++ bowling.cpp
@@ -5,18 +5,23 @@ int bowlingScore(std::string input) {
     for (int i = 0; i < input.size(); ++i) {
         if (input[i] == 'X') {
             score += 10;
-            if (i < input.size() - 1 && input[i + 1] != '/') {
-                score += input[i + 2] - '0' + input[i + 3] - '0';
+            if (i + 1 < input.size() && input[i+1] != ' ') {
+                if (input[i+1] == 'X') {
+                    score += 10;
+                    i++;
+                } else {
+                    score += input[i+2] - '0' + input[i+1] - '0';
+                    i++;
+                }
             }
         } else if (input[i] == '/') {
-            score += 10 - score;
-        } else if (input[i] >= '1' && input[i] <= '9') {
-            int pins = input[i] - '0';
-            if (i < input.size() - 1 && input[i + 1] == '+') {
-                score += pins + input[i + 2] - '0';
-            } else {
-                score += pins;
-            }
+            score += 10 - (3 - (input[i-1] - '0') + (input[i+1] - '0'));
+        } else if (input[i] == '-') {
+            int j = i;
+            while (++j < input.size() && input[j] != '/' && input[j] != ' ') {}
+            score += 10 - (3 - (input[i] - '0') + (j-i-1));
+        } else {
+            score += input[i] - '0';
         }
     }
     return score;
