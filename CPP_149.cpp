918f34b048978b680a46fd76b8e136ee8fdb30ac
beep boop <githubbot@vadim.me>
2024-07-10 21:23:15 +0200

    Solution 51 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ptpr8f4c Prompt:       |                                                         ^~~~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:255:23:   required from ‘void std::__cxx 11::basic_string<_CharT, _Traits, _Alloc>::_M_construct_aux(_InIterator, _InIter ator, std::__false_type) [with _InIterator = const char*; _CharT = char; _Traits  = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:274:20:   required from ‘void std::__cxx 11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterator ) [with _InIterator = const char*; _CharT = char; _Traits = std::char_traits<cha r>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:641:16:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(_InputIterator, _InputIterato r, const _Alloc&) [with _InputIterator = const char*; <template-parameter-2-2> =  void; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator< char>]’ 34fa3b79-787e-427e-bf73-ddc1f7a29cf3.cpp:25:18:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,10 +1,11 @@
+```cpp
 #include <string>
 #include <vector>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); i++) {
-        if (a[i] + b[i] != sortListSum({{a[i], b[i]}})[0]) return false;
+        if (stoi(a[i].c_str() + b[i].c_str()) != stoi(sortListSum({{a[i], b[i]}})[0])) return false;
     }
     return true;
 }
@@ -12,12 +13,8 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
 std::vector<int> sortListSum(std::vector<std::pair<std::string, std::string>> v) {
     std::vector<int> result;
     for (const auto& pair : v) {
-        int num = 0;
-        for (char c : pair.first + pair.second) {
-            if (c >= '0' && c <= '9') {
-                num = num * 10 + (c - '0');
-            }
-        }
+        int num = stoi(pair.first + pair.second);
+        if (!num) return {};
         result.push_back(num);
     }
     std::sort(result.begin(), result.end());
