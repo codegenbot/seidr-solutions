a60ddb1f3ee1e135e2aa89f166abb6eb15aadd6b
beep boop <githubbot@vadim.me>
2024-07-10 21:03:10 +0200

    Solution 78 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/rrmjrqz4 Prompt: Given a vector of integers, return the ﬁrst index such that the sum of all integers from the start of the vector to that index (inclusive) is negative.

--- basement.cpp
+++ basement.cpp
@@ -1,32 +1,10 @@
 #include <vector>
-using namespace std;
 
-int basementIndex(vector<int>& nums) {
+int basementIndex(const std::vector<int>& numbers) {
     int sum = 0;
-    for (int i = 0; i < nums.size(); i++) {
-        sum += nums[i];
-        if (sum < 0) return i;
+    for (int i = 0; ; i++) {
+        sum += numbers[i];
+        if (sum < 0)
+            return i;
     }
-    return -1;
-}
-
-int main() {
-    vector<int> inputs;
-    int n;
-    cin >> n;
-    while(n--) {
-        cin.ignore();
-        string s;
-        getline(cin, s);
-        inputs.clear();
-        size_t pos = 0;
-        while((pos = s.find(' ')) != string::npos) {
-            inputs.push_back(stoi(s.substr(0, pos)));
-            s.erase(0, pos + 1);
-        }
-        inputs.push_back(stoi(s));
-        int result = basementIndex(inputs);
-        cout << result << endl;
-    }
-    return 0;
 }
