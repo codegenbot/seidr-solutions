45bafc0b8f2a732ec59fd172664738ad376d54ec
beep boop <githubbot@vadim.me>
2024-07-10 05:46:44 +0200

    Solution 88 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9mcoota0 Prompt:       |                      ^~~~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘typename __gnu_cxx ::__enable_if<std::__is_char<_Tp>::__value, bool>::__type std::operator==(const std::__cxx11::basic_string<_CharT>&, const std::__cxx11::basic_string<_CharT>&) [with _CharT = char; typename __gnu_cxx::__enable_if<std::__is_char<_Tp>::__valu e, bool>::__type = bool]’: /usr/include/c++/11/bits/basic_string.h:6305:22:   required from ‘bool std::oper ator!=(const std::__cxx11::basic_string<_CharT, _Traits, _Allocator>&, const std ::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _Tra its = std::char_traits<char>; _Alloc = std::allocator<char>]’ aec10bc5-6be3-4298-b0b3-a074ea98ab00.cpp:65:18:   required from here /usr/include/c++/11/bits/basic_string.h:6236:21: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6236 |     { return (__lhs.size() == __rhs.size()       |               ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h:6236:37: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6236 |     { return (__lhs.size() == __rhs.size()       |                               ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h:6238:59: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6238 |                                                     __lhs.size())); }       |                                                     ~~~~~~^~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -7,9 +7,9 @@
 using namespace std;
 
 bool issame(string a, string b) {
-    if(a.size() != b.size())
+    if(a.length() != b.length())
         return false;
-    for(int i = 0; i < a.size(); i++) {
+    for(int i = 0; i < a.length(); i++) {
         if(a[i] != b[i])
             return false;
     }
@@ -28,16 +28,16 @@ string to_string(int i) {
 }
 
 string reverse_delete(string s1, string s2) {
-    int len1 = s1.size();
-    int len2 = s2.size();
+    int len1 = s1.length();
+    int len2 = s2.length();
     for(int i = 0; i < len2; i++) {
         auto pos = s1.find(s2[i]);
-        if(pos != std::string::npos) {
+        if(pos != string::npos) {
             s1 = s1.substr(0, pos) + s1.substr(pos + 1);
         }
     }
-    string reversed = s1; 
-    reverse(reversed.begin(),reversed.end());
+    std::string reversed = s1; 
+    std::reverse(reversed.begin(),reversed.end());
     return reversed;
 }
 
