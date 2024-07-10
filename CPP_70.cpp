8b2539684a0ba29c7b8d13f9e2205b8c17bfd9b9
beep boop <githubbot@vadim.me>
2024-07-10 04:13:19 +0200

    Solution 81 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/c6jnr1hd Prompt: rator = int*; _Size = long unsigned int]’ /usr/include/c++/11/bits/stl_uninitialized.h:704:44:   required from ‘_ForwardIt erator std::__uninitialized_default_n_a(_ForwardIterator, _Size, std::allocator< _Tp>&) [with _ForwardIterator = int*; _Size = long unsigned int; _Tp = int]’ /usr/include/c++/11/bits/vector.tcc:627:35:   required from ‘void std::vector<_T p, _Alloc>::_M_default_append(std::vector<_Tp, _Alloc>::size_type) [with _Tp = i nt; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = long uns igned int]’ /usr/include/c++/11/bits/stl_vector.h:940:4:   required from ‘void std::vector<_ Tp, _Alloc>::resize(std::vector<_Tp, _Alloc>::size_type) [with _Tp = int; _Alloc  = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = long unsigned int] ’ 15dc021b-3de5-4159-aa11-e1148cffc3fa.cpp:26:13:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -22,13 +22,14 @@ int main() {
     
     std::cout << "Enter element 1 to " << n1 << ": " << std::endl; // let user know how many elements are expected
 
-    std::vector<int> a(n1, 0); 
+    std::vector<int> a;
+    a.resize(n1, 0); 
 
     for(int i = 0; i < n1; i++) {
         std::cout << "Enter element " << i+1 << ": ";
         int temp; 
         std::cin >> temp;
-        a[i] = temp;
+        a.push_back(temp);
     }
     
     std::cout << "Enter the size of second vector: ";
@@ -36,13 +37,14 @@ int main() {
 
     std::cout << "Enter element 1 to " << n2 << ": " << std::endl; // let user know how many elements are expected
 
-    std::vector<int> b(n2, 0);
+    std::vector<int> b;
+    b.resize(n2, 0);
     
     for(int i = 0; i < n2; i++) {
         std::cout << "Enter element " << i+1 << ": ";
         int temp; 
         std::cin >> temp;
-        b[i] = temp;
+        b.push_back(temp);
     }
     if(issame(a, b)) {
         std::cout << "The two vectors are the same." << std::endl;
