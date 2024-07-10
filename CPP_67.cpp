132a8c7629a74c823ad54cc5fd853f5ebcd57436
beep boop <githubbot@vadim.me>
2024-07-10 19:03:38 +0200

    Solution 1 of CPP/67, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dlq5boh8 Prompt: Complete the following code given the task description and function signature.

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,35 +1,28 @@
-#include <iostream>
+#include <stdio.h>
 #include <string>
 using namespace std;
 
 int fruit_distribution(string s, int n) {
-    size_t pos = 0;
-    int apples = 0, oranges = 0;
+    int total_apples = 0;
+    int total_oranges = 0;
+    string temp;
 
-    while ((pos = s.find("apples", pos)) != string::npos) {
-        if (s.substr(pos + 7, 2).find("and") == string::npos) {
-            apples = stoi(s.substr(pos + 8));
-            break;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') {
+            continue;
+        } else if (isdigit(s[i])) {
+            temp += s[i];
+        } else {
+            if (temp != "") {
+                if (temp.find("apples") != string::npos) {
+                    total_apples = stoi(temp);
+                } else if (temp.find("oranges") != string::npos) {
+                    total_oranges = stoi(temp);
+                }
+                temp = "";
+            }
         }
-        pos += 10;
     }
 
-    while ((pos = s.find("oranges", pos)) != string::npos) {
-        if (s.substr(pos + 7, 2).find("and") == string::npos) {
-            oranges = stoi(s.substr(pos + 8));
-            break;
-        }
-        pos += 10;
-    }
-
-    return n - apples - oranges;
-}
-
-int main() {
-    cout << fruit_distribution("5 apples and 6 oranges", 19) << endl;
-    cout << fruit_distribution("0 apples and 1 oranges", 3) << endl;
-    cout << fruit_distribution("2 apples and 3 oranges", 100) << endl;
-    cout << fruit_distribution("100 apples and 1 oranges", 120) << endl;
-
-    return 0;
+    return n - total_apples - total_oranges;
 }
