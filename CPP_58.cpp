53c2819b68b0e13249728f9b1ae3f2e914ffc9a2
beep boop <githubbot@vadim.me>
2024-07-10 02:18:01 +0200

    Solution 22 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0hw5ynmd Prompt: t>; _Compare = std::less<int>; _Alloc = std::allocator<int>; std::_Rb_tree<_Key,  _Val, _KeyOfValue, _Compare, _Alloc>::iterator = std::_Rb_tree<int, int, std::_ Identity<int>, std::less<int>, std::allocator<int> >::iterator; std::_Rb_tree<_K ey, _Val, _KeyOfValue, _Compare, _Alloc>::const_iterator = std::_Rb_tree<int, in t, std::_Identity<int>, std::less<int>, std::allocator<int> >::const_iterator]’ /usr/include/c++/11/bits/stl_tree.h:1102:23:   required from ‘std::__enable_if_t <std::is_same<_Val, typename std::iterator_traits<_InputIterator>::value_type>:: value> std::_Rb_tree<_Key, _Val, _KeyOfValue, _Compare, _Alloc>::_M_insert_range _unique(_InputIterator, _InputIterator) [with _InputIterator = __gnu_cxx::__norm al_iterator<int*, std::vector<int> >; _Key = int; _Val = int; _KeyOfValue = std: :_Identity<int>; _Compare = std::less<int>; _Alloc = std::allocator<int>; std::_ _enable_if_t<std::is_same<_Val, typename std::iterator_traits<_InputIterator>::v alue_type>::value> = void; typename std::iterator_traits<_InputIterator>::value_ type = int]’ /usr/include/c++/11/bits/stl_set.h:193:31:   required from ‘std::set<_Key, _Comp are, _Alloc>::set(_InputIterator, _InputIterator) [with _InputIterator = __gnu_c xx::__normal_iterator<int*, std::vector<int> >; _Key = int; _Compare = std::less <int>; _Alloc = std::allocator<int>]’ 0b8131f0-cfd8-4472-b30e-a1d97f2b87a6.cpp:10:37:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::_Rb_tree_node<int> >’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -6,15 +6,9 @@
 
 using namespace std;
 
-vector<int> common(vector<int> l1, vector<int> l2) {
-    sort(l1.begin(), l1.end());
-    sort(l2.begin(), l2.end());
-
-    set<int> s1(l1.begin(), l1.end());
-    set<int> s2(l2.begin(), l2.end());
-
+vector<int> common(set<int> l1, set<int> l2) {
     vector<int> result;
-    auto range = set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+    auto range = set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(),
                                     std::back_inserter(result));
     
     return result;
@@ -23,7 +17,10 @@ vector<int> common(vector<int> l1, vector<int> l2) {
 int main() {
     vector<int> l1 = {4, 3, 2, 8};
     vector<int> l2 = {2, 4, 6, 8};
-    vector<int> commonElements = common(l1, l2);
+    set<int> s1(l1.begin(), l1.end());
+    set<int> s2(l2.begin(), l2.end());
+
+    vector<int> commonElements = common(s1, s2);
     for(int i : commonElements) {
         cout << i << " ";
     }
