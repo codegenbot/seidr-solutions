e52d85763487486c1d13633bcbf42cc676e6a7b7
beep boop <githubbot@vadim.me>
2024-07-10 05:07:51 +0200

    Solution 7 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/xk01ab12 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 7bb766af-a4a4-4f93-8f17-b5f2dd2f9a76.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,6 +1,4 @@
 #include <vector>
-#include <initializer_list>
-#include <bits/stl_pair.h>
 
 bool issame(vector<int> game, vector<int> guess) {
     for (int i = 0; i < game.size(); i++) {
@@ -21,20 +19,3 @@ vector<int> compare(vector<int> game, vector<int> guess) {
         }
     }
     return result;
-}
-
-int main() {
-    vector<int> game = {1,2,3};
-    vector<int> guess = {1,4,5};
-    cout << issame(game,guess) << endl; 
-    cout << "Guess: ";
-    for (int i : guess) {
-        cout << i << " ";
-    }
-    cout << endl;
-    cout << "Result: ";
-    for (int i : compare(game,guess)) {
-        cout << i << " ";
-    }
-    cout << endl;
-}
