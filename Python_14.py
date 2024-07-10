b4a13715d67fb101324450c22a93868cca7f8a44
beep boop <githubbot@vadim.me>
2024-07-10 20:33:35 +0200

    Solution 55 of Python/14, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8qoyijl2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bbc691db-88f1-4c64-832b-89361dedf 717.py", line 7, in <module>     check(all_prefixes)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bbc691db-88f1-4c64-832b-89361dedf 717.py", line 4, in check     assert all_prefixes("WWW") == ["W", "WW", "WWW"]            ^^^^^^^^^^^^^^^^^^^ TypeError: all_prefixes() takes 0 positional arguments but 1 was given

--- Python_14.py
+++ Python_14.py
@@ -1,2 +1,2 @@
-def all_prefixes(string: str) -> List[str]:
-    return [string[:i] for i in range(1, len(string) + 1)]
+def all_prefixes(s: str) -> list:
+    return [s[:i] for i in range(1, len(s) + 1)]
