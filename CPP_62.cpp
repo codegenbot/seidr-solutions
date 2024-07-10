360e245c61d554fc9c26a3db23592baf007b30ee
beep boop <githubbot@vadim.me>
2024-07-10 15:32:06 +0200

    Solution 73 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/e3rnk79e Prompt:       |                    ^~~~~ c42bc0a2-89ea-4e1d-bba7-be35cc62f71c.cpp:10:29: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ c42bc0a2-89ea-4e1d-bba7-be35cc62f71c.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ c42bc0a2-89ea-4e1d-bba7-be35cc62f71c.cpp:10:43: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from c42bc0a2-89ea-4e1d-bba7-be35cc62f71c.cpp:9: c42bc0a2-89ea-4e1d-bba7-be35cc62f71c.cpp: In function ‘int main()’: c42bc0a2-89ea-4e1d-bba7-be35cc62f71c.cpp:19:20: error: ‘derivative’ was not decl ared in this scope    19 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~ c42bc0a2-89ea-4e1d-bba7-be35cc62f71c.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -1,18 +1,20 @@
 #include <vector>
 
-bool issame(vector<float> a, vector<float> b) {
+bool issame(vector<float> a, vector<float> b){
     if (a.size() != b.size()) {
         return false;
     }
+
     for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) {
             return false;
         }
     }
+
     return true;
 }
 
-vector<float> derivative(vector<float> xs) {
+vector<float> derivative(vector<float> xs){
     vector<float> result;
     for (int i = 1; i < xs.size(); i++) {
         result.push_back(xs[i] * i);
@@ -22,5 +24,4 @@ vector<float> derivative(vector<float> xs) {
 
 int main() {
     assert(issame(derivative({1}), {}));
-    return 0;
 }
