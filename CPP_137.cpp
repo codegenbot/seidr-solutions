fec4024127b6e7592ef45348febe70c958e8bcc7
beep boop <githubbot@vadim.me>
2024-07-10 10:53:51 +0200

    Solution 60 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/51eh1c3h Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,33 +1,8 @@
-```
-boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return boost::any_cast<float>(b);
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string str = boost::any_cast<string>(b);
-        return (float)a > atof(str.c_str()) ? a : b;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = boost::any_cast<string>(a);
-        string str2 = boost::any_cast<string>(b);
-        return str1 > str2 ? a : (str1 < str2 ? b : boost::any("None")));
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int x = boost::any_cast<int>(a);
-        string str = boost::any_cast<string>(b);
-        return atof(str.c_str()) > x ? a : b;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        string str = boost::any_cast<string>(a);
-        int y = boost::any_cast<int>(b);
-        return atof(str.c_str()) > y ? a : b;
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        float x = boost::any_cast<float>(a);
-        int y = boost::any_cast<int>(b);
-        return x > y ? a : b;
-    }
-    else {
-        return boost::any("None");
-    }
-}
+if(a.type() == typeid(int) && b.type() == typeid(int))
+    return (int)a > (int)b ? boost::any(a) : boost::any(b);
+else if(a.type() == typeid(float) && b.type() == typeid(float))
+    return (float)a > (float)b ? boost::any(a) : boost::any(b);
+else if(a.type() == typeid(string) && b.type() == typeid(string))
+    return (string)a > (string)b ? boost::any(a) : boost::any(b);
+else
+    return "None";
