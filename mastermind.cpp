84a76ad4ce7439022ad70ca1d66dd839a19f162a
beep boop <githubbot@vadim.me>
2024-07-10 12:37:36 +0200

    Solution 34 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/tjtxcwav Prompt: 7cde0093-1ad3-45dc-9a83-0e1bfa5245f8.cpp:3:22: error: ‘string’ was not declared in this scope     3 | PegCounts mastermind(string code, string guess) {       |                      ^~~~~~ 7cde0093-1ad3-45dc-9a83-0e1bfa5245f8.cpp:3:35: error: ‘string’ was not declared in this scope     3 | PegCounts mastermind(string code, string guess) {       |                                   ^~~~~~ 7cde0093-1ad3-45dc-9a83-0e1bfa5245f8.cpp:3:49: error: expected ‘,’ or ‘;’ before  ‘{’ token     3 | PegCounts mastermind(string code, string guess) {       |                                                 ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 struct PegCounts { int white; int black; };
 
 PegCounts mastermind(string code, string guess) {
@@ -13,19 +15,28 @@ PegCounts mastermind(string code, string guess) {
     }
 
     // Count white pegs
-    int whiteCount = 0;
+    int codeCount[256] = {0};
     for (int i = 0; i < 4; ++i) {
-        int count = 0;
-        for (int j = 0; j < 4; ++j) {
-            if (code[j] == remainingGuess[i]) {
-                count++;
+        codeCount[code[i]]++;
+    }
+    for (int i = 0; i < 4; ++i) {
+        if (code[i] != guess[i]) {
+            codeCount[guess[i]]--;
+            if (codeCount[guess[i]] == 0) {
+                codeCount[guess[i]] = -1;
             }
         }
-        if (count > 0) {
-            whiteCount += count - 1; // subtract one, as the correct position is already counted
+    }
+    for (int i = 0; i < 4; ++i) {
+        if (remainingGuess[i] != ' ') {
+            if (codeCount[code[i]] > 0) {
+                result.white++;
+                codeCount[code[i]]--;
+            } else if (codeCount[guess[i]] == -1) {
+                result.black++;
+            }
         }
     }
 
-    result.white = whiteCount;
     return result;
 }
