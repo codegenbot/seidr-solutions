c688370b880811317083df5554de091601d24e36
beep boop <githubbot@vadim.me>
2024-07-11 01:58:37 +0200

    Solution 61 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/tmnr21o2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5641a9ff-87b9-446a-b7ab-aceed92b5 5df.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5641a9ff-87b9-446a-b7ab-aceed92b5 5df.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5641a9ff-87b9-446a-b7ab-aceed92b5 5df.py", line 2, in sort_even     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5641a9ff-87b9-446a-b7ab-aceed92b5 5df.py", line 3, in <listcomp>     x if i % 2 != 0 else sorted([y for y in l if y <= x])[j]          ^ NameError: name 'i' is not defined. Did you mean: 'id'?

--- Python_37.py
+++ Python_37.py
@@ -1,3 +1,3 @@
 def sort_even(l: list):
     return [x if i % 2 != 0 else sorted([y for j, y in enumerate(l) if y <= x])[-1] 
-            for i, x in enumerate(sorted(l))]"
+            for i, x in enumerate(sorted(l))]
