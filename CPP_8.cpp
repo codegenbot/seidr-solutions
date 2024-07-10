b5b0b20ca29022b923b64fcffd6fbf143ef571fa
beep boop <githubbot@vadim.me>
2024-07-10 04:50:42 +0200

    Solution 91 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/5lwwp8ur Prompt: eaae5354-54b5-4c33-8667-2fc0a8636a2a.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ eaae5354-54b5-4c33-8667-2fc0a8636a2a.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from eaae5354-54b5-4c33-8667-2fc0a8636a2a.cpp:23: eaae5354-54b5-4c33-8667-2fc0a8636a2a.cpp: In function ‘int main()’: eaae5354-54b5-4c33-8667-2fc0a8636a2a.cpp:33:20: error: ‘sum_product’ was not dec lared in this scope; did you mean ‘my_namespace::sum_product’?    33 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ eaae5354-54b5-4c33-8667-2fc0a8636a2a.cpp:10:22: note: ‘my_namespace::sum_product ’ declared here    10 |     std::vector<int> sum_product(std::vector<int> nums) {       |                      ^~~~~~~~~~~ In file included from eaae5354-54b5-4c33-8667-2fc0a8636a2a.cpp:23: eaae5354-54b5-4c33-8667-2fc0a8636a2a.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -2,18 +2,25 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-std::vector<int> sum_product(std::vector<int> nums) {
-    int sum = 0;
-    int product = 1;
-
-    for(int num : nums) {
-        sum += num;
-        product *= num;
+namespace my_namespace {
+    bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+        return a == b;
     }
 
-    return std::vector<int>{sum, product};
+    std::vector<int> sum_product(std::vector<int> nums) {
+        int sum = 0;
+        int product = 1;
+
+        for(int num : nums) {
+            sum += num;
+            product *= num;
+        }
+
+        return std::vector<int>{sum, product};
+    }
+}
+
+int main() {
+    assert(my_namespace::issame(my_namespace::sum_product({10}), {10, 10}));
+    return 0;
 }
