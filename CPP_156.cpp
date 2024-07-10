ae261e31671083c3db0a7817162bca7fb028f9d0
beep boop <githubbot@vadim.me>
2024-07-10 20:17:36 +0200

    Solution 22 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/mma41iux Prompt: 30c15970-ca11-4e9a-b4a9-69c9c09a60bb.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 30c15970-ca11-4e9a-b4a9-69c9c09a60bb.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from 30c15970-ca11-4e9a-b4a9-69c9c09a60bb.cpp:35: 30c15970-ca11-4e9a-b4a9-69c9c09a60bb.cpp: In function ‘int main()’: 30c15970-ca11-4e9a-b4a9-69c9c09a60bb.cpp:37:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    37 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <string>
 #include <vector>
 #include <iostream>
@@ -10,7 +9,7 @@ struct RomanPair {
     string second;
 };
 
-string int_to_mini_roman(int number) {
+string int_to_roman(int number) {
     vector<RomanPair> romanMap = {{1000, "M"}, {900, "CM"}, {500, "D"},
                               {400, "CD"}, {100, "C"}, {90, "XC"},
                               {50, "L"}, {40, "XL"}, {10, "X"},
@@ -29,6 +28,6 @@ int main() {
     int num;
     cout << "Enter a number: ";
     cin >> num;
-    cout << "The Roman numeral for " << num << " is " << int_to_mini_roman(num) << endl;
+    cout << "The Roman numeral for " << num << " is " << int_to_roman(num) << endl;
     return 0;
 }
