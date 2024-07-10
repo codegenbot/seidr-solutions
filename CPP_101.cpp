399c28547f948ef995437376e891fab6cefc6e24
beep boop <githubbot@vadim.me>
2024-07-10 22:56:18 +0200

    Solution 73 of CPP/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/i21pdm16 Prompt: In file included from e7ac634a-d2ad-430c-ba7e-598e5839b412.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e7ac634a-d2ad-430c-ba7e-598e5839b412.cpp:35:44: error: ‘b’ was not declared in t his scope    35 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e7ac634a-d2ad-430c-ba7e-598e5839b412.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ e7ac634a-d2ad-430c-ba7e-598e5839b412.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from e7ac634a-d2ad-430c-ba7e-598e5839b412.cpp:34: e7ac634a-d2ad-430c-ba7e-598e5839b412.cpp: In function ‘int main()’: e7ac634a-d2ad-430c-ba7e-598e5839b412.cpp:44:20: error: ‘words_string’ was not de clared in this scope    44 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |                    ^~~~~~~~~~~~

--- CPP_101.cpp
+++ CPP_101.cpp
@@ -2,10 +2,23 @@
 #include <vector>
 #include <string>
 
-bool issame(const std::vector<std::string>& vec) {
-    for (int i = 0; i < vec.size(); i++) {
-        if (std::string(vec[i]) == "")
-            return false;
+bool issame(const std::vector<std::string>& vec1, const std::vector<std::string>& vec2) {
+    if (vec1.size() != vec2.size())
+        return false;
+    
+    for (int i = 0; i < vec1.size(); i++) {
+        if (std::string(vec1[i]) == "" || std::string(vec2[i]) == "") {
+            if ((vec1[i] != vec2[i]))
+                return false;
+        } else {
+            if (std::transform(vec1.begin(), vec1.end(), vec2.begin(),
+                               [](const auto& a, const auto& b) {
+                                   return a == b;
+                               }).all())
+                return true;
+            else
+                return false;
+        }
     }
     
     return true;
@@ -21,5 +34,5 @@ int main() {
         words_string.push_back(s);
     }
     
-    assert(issame(words_string));
+    assert(issame(words_string, words_string));
 }
