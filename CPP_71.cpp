e4a6483667f7b5fec9a12387ade1b0538303e272
beep boop <githubbot@vadim.me>
2024-07-10 03:24:40 +0200

    Solution 40 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ctqurfce Prompt: 9ac5c862-ff09-4738-a1df-175675aeeb13.cpp: In function ‘int main()’: 9ac5c862-ff09-4738-a1df-175675aeeb13.cpp:23:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    23 | int main(){       |         ^~ 9ac5c862-ff09-4738-a1df-175675aeeb13.cpp:23:9: note: remove parentheses to defau lt-initialize a variable    23 | int main(){       |         ^~       |         -- 9ac5c862-ff09-4738-a1df-175675aeeb13.cpp:23:9: note: or replace parentheses with  braces to value-initialize a variable 9ac5c862-ff09-4738-a1df-175675aeeb13.cpp:23:11: error: a function-definition is not allowed here before ‘{’ token    23 | int main(){       |           ^ 9ac5c862-ff09-4738-a1df-175675aeeb13.cpp:25:2: error: expected ‘}’ at end of inp ut    25 | }       |  ^ 9ac5c862-ff09-4738-a1df-175675aeeb13.cpp:14:12: note: to match this ‘{’    14 | int main() {       |            ^

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -12,10 +12,10 @@ float triangle_area(int a, int b, int c) {
     }
 }
 
-int main{
+int main() { 
     int a, b, c; 
     std::cin >> a >> b >> c; 
     assert(std::abs(triangle_area(a, b, c) + 1)<0.01);
     float result = triangle_area(a, b, c);
     std::cout << "The area of the triangle is: " << static_cast<int>(result) << std::endl;
-    return 0;
+}
