c5cbc2b1fae0e230a2544226a2d0f98e24d258c5
beep boop <githubbot@vadim.me>
2024-07-10 21:15:29 +0200

    Solution 12 of CPP/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9iudnkpt Prompt: 4cd6c2af-8770-45bf-b2be-87014e270c78.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string fix_spaces(string text){       | ^~~~~~ In file included from 4cd6c2af-8770-45bf-b2be-87014e270c78.cpp:19: 4cd6c2af-8770-45bf-b2be-87014e270c78.cpp: In function ‘int main()’: 4cd6c2af-8770-45bf-b2be-87014e270c78.cpp:21:13: error: ‘fix_spaces’ was not decl ared in this scope    21 |     assert (fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple");       |             ^~~~~~~~~~

--- CPP_140.cpp
+++ CPP_140.cpp
@@ -1,7 +1,7 @@
 using namespace std;
 
-std::string fix_spaces(std::string text){
-    std::string result = "";
+string fix_spaces(string text){
+    string result = "";
     for(int i=0; i < text.length(); i++){
         if(text[i] == ' '){
             if(i+1 < text.length() && text[i+1] == ' '){
