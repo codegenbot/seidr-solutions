9a361f5973b9cb78ca6e67965272dee847e0da94
beep boop <githubbot@vadim.me>
2024-07-10 21:03:01 +0200

    Solution 6 of CPP/15, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/gfp60v66 Prompt: c08ef014-4d82-4645-a8ef-897cfe77af26.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string string_sequence(int n){       | ^~~~~~ In file included from c08ef014-4d82-4645-a8ef-897cfe77af26.cpp:10: c08ef014-4d82-4645-a8ef-897cfe77af26.cpp: In function ‘int main()’: c08ef014-4d82-4645-a8ef-897cfe77af26.cpp:12:13: error: ‘string_sequence’ was not  declared in this scope    12 |     assert (string_sequence(10) == "0 1 2 3 4 5 6 7 8 9 10");       |             ^~~~~~~~~~~~~~~

--- CPP_15.cpp
+++ CPP_15.cpp
@@ -1,7 +1,7 @@
-#include <string> 
-using namespace std; 
+#include <string>
+using namespace std;
 
-string string_sequence(int n); 
+string string_sequence(int n);
 
 string string_sequence(int n){ 
     string result;
@@ -12,6 +12,6 @@ string string_sequence(int n){
     return result;
 }
 
-int main() { 
+int main() {
     assert (string_sequence(10) == "0 1 2 3 4 5 6 7 8 9 10");
 }
