40a64da37f51828748faad5a956549967a3e06df
beep boop <githubbot@vadim.me>
2024-07-10 19:49:49 +0200

    Solution 64 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/xjg4rs13 Prompt:       | ebf3b2ae-474c-423e-ac86-6e1d9d6e50ae.cpp: In function ‘int main()’: ebf3b2ae-474c-423e-ac86-6e1d9d6e50ae.cpp:22:5: error: ‘assert’ was not declared in this scope    22 |     assert(odd_count({"271", "137", "314"}) == vector<string>{"the numbe r of odd elements 2 in the string 271 of the input.", "the number of odd element s 2 in the string 137 of the input.", "the number of odd elements 2 in the strin g 314 of the input."});       |     ^~~~~~ ebf3b2ae-474c-423e-ac86-6e1d9d6e50ae.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <cassert>   +++ |+#include <cassert>     4 | ebf3b2ae-474c-423e-ac86-6e1d9d6e50ae.cpp: At global scope: ebf3b2ae-474c-423e-ac86-6e1d9d6e50ae.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ ebf3b2ae-474c-423e-ac86-6e1d9d6e50ae.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <cassert> 
+#include <cassert>
 
 using namespace std;
 
@@ -19,7 +19,9 @@ vector<string> odd_count(vector<string> lst) {
 }
 
 int main() {
-    assert(odd_count({"271", "137", "314"}) == vector<string>{"the number of odd elements 2 in the string 271 of the input.", "the number of odd elements 2 in the string 137 of the input.", "the number of odd elements 2 in the string 314 of the input."});
+    assert(odd_count({"271", "137", "314"}) == vector<string>{"the number of odd elements 2 in the string 271 of the input.", 
+                                                             "the number of odd elements 2 in the string 137 of the input.", 
+                                                             "the number of odd elements 2 in the string 314 of the input."});
     
     return 0;
 }
