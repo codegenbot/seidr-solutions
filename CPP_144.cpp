241f7067fc6b3375f1ecfed9fa03c3412ea9323d
beep boop <githubbot@vadim.me>
2024-07-10 11:03:46 +0200

    Solution 16 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/o1pkvjf1 Prompt: Program failed

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -4,39 +4,18 @@
 
 bool checkDivisibility(std::string x, std::string n) {
     if (x.find('/') == std::string::npos || n.find('/') == std::string::npos) {
-        return false; // Return false if '/' is not found in input strings
+        return false;
     }
 
     int den1 = stoi(x.substr(x.find('/') + 1));
     int den2 = stoi(n.substr(n.find('/') + 1));
 
     if (den1 == 0 || den2 == 0) {
-        return false; // Return false if any denominator is 0
+        return false;
     }
 
     int num1 = stoi(x.substr(0, x.find('/')));
     int num2 = stoi(n.substr(0, n.find('/')));
 
-    if (den1 == 0 || den2 == 0) {
-        return false; // Return false if any denominator is 0
-    }
-
-    return (num1 * num2) % (den1 * den2) == 0;
-}
-
-bool simplify(std::string x, std::string n) {
-    int num1 = stoi(x.substr(0, x.find('/')));
-    int den1 = stoi(x.substr(x.find('/') + 1));
-    int num2 = stoi(n.substr(0, n.find('/')));
-    int den2 = stoi(n.substr(n.find('/') + 1));
-
-    if (den1 == 0 || den2 == 0) {
-        return false; // Return false if any denominator is 0
-    }
-
-    int gcd = std::gcd(num1 * den2, num2 * den1);
-    num1 /= gcd;
-    den1 /= gcd;
-
-    return num1 == num2 && den1 == den2;
+    return (num1 * den2) % (den1 * num2) == 0;
 }
