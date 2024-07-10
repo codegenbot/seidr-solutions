a3d1603b69a849b4f3050f27f54e0fc7d3aa78d2
beep boop <githubbot@vadim.me>
2024-07-10 19:56:51 +0200

    Solution 9 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/e7rinnjl Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f571bd5e-af28-4017-8b3e-6d3bcd39b 910.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_14.py
+++ Python_14.py
@@ -1,5 +1,10 @@
+```python
 from typing import List
 
 
 def all_prefixes(string: str) -> List[str]:
-    return [""] + [string[:i] for i in range(1, len(string) + 1)] + [string]
+    return [string[:i] for i in range(1, len(string) + 1)] + [string]
+
+
+string = input("Please enter a string: ")
+print(all_prefixes(string))
