c3c4de9efccea6b1187e889b39dd3482a295d9f3
beep boop <githubbot@vadim.me>
2024-07-11 01:06:21 +0200

    Solution 41 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5mt0m7n3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fceffd98-b16d-4b1f-b37f-fb801e8d6 1b8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,6 +1,6 @@
 from typing import List, Any
 
+
 def filter_integers(values: List[Any]) -> list:
     filtered_values = [value for value in values if isinstance(value, int)]
-    sorted_values = sorted([int(i) for i in filtered_values])
-    return sorted_values
+    return [x for x in filtered_values if x % 2 == 0]
