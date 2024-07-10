2be065c14c961e85d55e57a6de88018a42c74007
beep boop <githubbot@vadim.me>
2024-07-11 01:44:13 +0200

    Solution 46 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/tmnr21o2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/83a959ec-9f82-4344-b2de-bdbbbde3c ec9.py", line 10, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/83a959ec-9f82-4344-b2de-bdbbbde3c ec9.py", line 5, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/83a959ec-9f82-4344-b2de-bdbbbde3c ec9.py", line 2, in sort_even     return [x if i % 2 != 0 else sorted([y for y in l if y <= x])[j]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/83a959ec-9f82-4344-b2de-bdbbbde3c ec9.py", line 2, in <listcomp>     return [x if i % 2 != 0 else sorted([y for y in l if y <= x])[j]                  ^ NameError: name 'i' is not defined. Did you mean: 'id'?

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,3 @@
 def sort_even(l: list):
-    return [
-        x if i % 2 != 0 else sorted([y for y in l if y <= x])[-1]
-        for _, x in enumerate(sorted(l))
-    ]
+    return [x if i % 2 != 0 else sorted([y for y in l if y <= x])[-1] 
+        for _, x in enumerate(sorted(l))].
