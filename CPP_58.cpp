0e41f595b2e2605fbb0d9c49a4607f032150b046
beep boop <githubbot@vadim.me>
2024-07-10 02:43:19 +0200

    Solution 42 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0hw5ynmd Prompt: In file included from /usr/include/c++/11/vector:67,                  from ed5ce55c-2311-4e76-8963-0503eb6c2355.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from ed5ce55c-2311-4e76-8963-0503eb6c2355.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ed5ce55c-2311-4e76-8963-0503eb6c2355.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from ed5ce55c-2311-4e76-8963-0503eb6c2355.cpp:19: ed5ce55c-2311-4e76-8963-0503eb6c2355.cpp: In function ‘int main()’: ed5ce55c-2311-4e76-8963-0503eb6c2355.cpp:29:20: error: ‘common’ was not declared  in this scope    29 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ ed5ce55c-2311-4e76-8963-0503eb6c2355.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,18 +1,16 @@
 #include <vector>
+#include <algorithm>
 #include <set>
 
-bool issame(vector<vector<int>> a, vector<vector<int>> b) {
-    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin(),
-                                                  [](const auto& v1, const auto& v2) {
-                                                      return issame(v1, v2);
-                                                  });
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
 }
 
-vector<int> common(vector<int> l1, vector<int> l2) {
-    set<int> s1(l1.begin(), l1.end());
-    set<int> s2(l2.begin(), l2.end());
+std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
+    std::set<int> s1(l1.begin(), l1.end());
+    std::set<int> s2(l2.begin(), l2.end());
 
-    vector<int> result(s1.begin(), s1.end());  // start the output with s1
+    std::vector<int> result;
     std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
                           back_inserter(result));
     
@@ -20,33 +18,6 @@ vector<int> common(vector<int> l1, vector<int> l2) {
 }
 
 int main() {
-    int n, m;
-    cin >> n >> m;
-
-    vector<vector<int>> l1(n), l2(m);
-
-    for (int i = 0; i < n; i++) {
-        l1[i].resize(4);
-        for (int j = 0; j < 4; j++) {
-            cin >> l1[i][j];
-        }
-    }
-
-    for (int i = 0; i < m; i++) {
-        l2[i].resize(4);
-        for (int j = 0; j < 4; j++) {
-            cin >> l2[i][j];
-        }
-    }
-
-    vector<int> result = common({l1[0][0], l1[0][1], l1[0][2], l1[0][3]}, {l2[0][0], l2[0][1], l2[0][2], l2[0][3]});
-
-    for (int i = 0; i < result.size(); i++) {
-        cout << result[i];
-        if (i < result.size() - 1) {
-            cout << " ";
-        }
-    }
-
+    assert(std::issame(common({4, 3, 2, 8}, {}), {}));
     return 0;
 }
