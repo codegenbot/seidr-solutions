705cd250a8cc2b03c818bcea9217c5d392b34dcf
beep boop <githubbot@vadim.me>
2024-07-10 06:09:58 +0200

    Solution 76 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/n2wga4wg Prompt: /usr/include/c++/11/bits/basic_string.h:760:30: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   760 |                   _M_destroy(_M_allocated_capacity);       |                              ^~~~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/bits/basic_string.h:765:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   765 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,8 +1,35 @@
+```cpp
 #include <iostream>
-#include <string>
 #include <sstream>
 
-using namespace std;
+int main_func() {
+    int a;
+    std::cout << "Enter an integer: ";
+    std::cin >> a;
+
+    double b;
+    std::string str;
+    std::cout << "Enter a floating point number or a string: ";
+    std::cin >> b; // This will be read as a double if possible
+
+    if (std::cin.fail()) { 
+        std::cin.clear();
+        std::cin >> std::ws;
+        str = "";
+        try {
+            b = std::stod(str);
+            std::cout << "The result is: " << compare_one(a, b) << std::endl;
+        } catch (std::invalid_argument& e) {
+            int temp = std::stoi(str); 
+            std::cout << "The result is: " << compare_one(a, temp) << std::endl;
+        }
+    } else {
+        str = std::to_string(b);
+        std::cout << "The result is: " << compare_one(a, std::stod(str)) << std::endl;
+    }
+
+    return 0;
+}
 
 double compare_one(int a, double b) {
     if (b > a) {
@@ -10,25 +37,4 @@ double compare_one(int a, double b) {
     } else {
         return static_cast<double>(a);
     }
-}
-
-int main_func() {
-    int a;
-    cout << "Enter an integer: ";
-    cin >> a;
-
-    double b;
-    string str;
-    cout << "Enter a floating point number or a string: ";
-    cin >> b; 
-
-    if (cin.fail()) { 
-        cin.clear();
-        cin >> ws;
-        cout << "The result is: " << compare_one(a, 0) << endl;
-    } else {
-        cout << "The result is: " << compare_one(a, b) << endl;
-    }
-
-    return 0;
 }
