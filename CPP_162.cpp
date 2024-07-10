a49a671fc2473911df7e64bffa3ca9c9416df4b4
beep boop <githubbot@vadim.me>
2024-07-10 21:19:05 +0200

    Solution 56 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ubokzjtg Prompt: /usr/bin/ld: /tmp/ccP895tE.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> >)': ae91368b-ef3b-4650-b3b5-0f239628f5b5.cpp:(.text+0x84): undefined reference to `E VP_MD_CTX_new' /usr/bin/ld: ae91368b-ef3b-4650-b3b5-0f239628f5b5.cpp:(.text+0x90): undefined re ference to `EVP_md5' /usr/bin/ld: ae91368b-ef3b-4650-b3b5-0f239628f5b5.cpp:(.text+0xaa): undefined re ference to `EVP_DigestInit_ex' /usr/bin/ld: ae91368b-ef3b-4650-b3b5-0f239628f5b5.cpp:(.text+0xf6): undefined re ference to `EVP_DigestUpdate' /usr/bin/ld: ae91368b-ef3b-4650-b3b5-0f239628f5b5.cpp:(.text+0x113): undefined r eference to `EVP_DigestFinal_ex' /usr/bin/ld: ae91368b-ef3b-4650-b3b5-0f239628f5b5.cpp:(.text+0x1d3): undefined r eference to `EVP_MD_CTX_free' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,3 +1,14 @@
+#include <openssl/opensslconf.h>
+#include <string>
+#include <openssl/evp.h>
+
+using namespace std;
+
+extern "C" {
+    #include <openssl/ssl.h>
+    #include <openssl/err.h>
+}
+
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
