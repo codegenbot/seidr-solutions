370684366d2ab86d3a500ffcd073f928b3d3da25
beep boop <githubbot@vadim.me>
2024-07-10 22:51:23 +0200

    Solution 59 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b11534f0-b709-49b9-8cb3-911689308 c6f.py", line 11, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b11534f0-b709-49b9-8cb3-911689308 c6f.py", line 6, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,5 @@
+```
 def sort_third(l):
     int_list = sorted([i for i in l if isinstance(i, int)])
-    lst_list = [sublist for sublist in l if isinstance(sublist, list)]
-    return int_list + lst_list
+    lst_list = [[sublist.sort(), len(sublist)] for sublist in [sublist for sublist in l if isinstance(sublist, list)]]
+    return int_list + [sublist[1] for sublist in lst_list]
