bbfaf94d35f4b37e523daeee98cef0f33280dee2
beep boop <githubbot@vadim.me>
2024-07-11 00:23:07 +0200

    Solution 88 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/99vwaciq Prompt: In file included from /usr/include/c++/11/string:39,                  from 6a59ff76-8eb8-4b6f-ad64-a38c87fd0de8.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 6a59ff76-8eb8-4b6f-ad64-a38c87fd0de8.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 6a59ff76-8eb8-4b6f-ad64-a38c87fd0de8.cpp:43:44: error: ‘b’ was not declared in t his scope    43 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6a59ff76-8eb8-4b6f-ad64-a38c87fd0de8.cpp:43:45: error: expression list treated a s compound expression in initializer [-fpermissive]    43 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 6a59ff76-8eb8-4b6f-ad64-a38c87fd0de8.cpp:42: 6a59ff76-8eb8-4b6f-ad64-a38c87fd0de8.cpp: In function ‘int main()’: 6a59ff76-8eb8-4b6f-ad64-a38c87fd0de8.cpp:52:19: error: ‘issame’ cannot be used a s a function    52 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,31 +1,21 @@
-```cpp
+```c++
 #include <vector>
 #include <set>
 #include <string>
 
-bool operator==(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); ++i)
-        if (a[i] != b[i]) return false;
-    return true;
-}
-
 std::vector<std::string> reverse_delete(std::string s, std::string c) {
     std::vector<std::string> result;
-
-    // Remove characters from string 's' that are in the set 'c'
-    if(s.empty() || c.empty())
-        return {{"", (s.empty()) ? "True" : "False"}};
     
     std::set<char> c_set(c.begin(), c.end());
+    
     std::string temp = "";
+    
     for (char ch : s) {
         if (c_set.find(ch) == c_set.end()) {
             temp += ch;
         }
     }
-
-    // Check if the resulting string is a palindrome
+    
     bool is_palindrome = true;
     int i = 0, length = temp.length();
     while (i < length / 2) {
@@ -35,9 +25,16 @@ std::vector<std::string> reverse_delete(std::string s, std::string c) {
         }
         i++;
     }
-
+    
     result.push_back(temp);
     result.push_back((is_palindrome) ? "True" : "False");
-
+    
     return result;
+}
+
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i)
+        if (a[i] != b[i]) return false;
+    return true;
 }
