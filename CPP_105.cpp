4ce308dfe6509bc44de813f144205c3362e86516
beep boop <githubbot@vadim.me>
2024-07-10 18:39:58 +0200

    Solution 16 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dctrev1c Prompt:       |                                 ^~~~~~ In file included from ff2b86dd-048e-4d69-b3b3-95e77a1104f1.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ ff2b86dd-048e-4d69-b3b3-95e77a1104f1.cpp:40:44: error: ‘b’ was not declared in t his scope    40 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ff2b86dd-048e-4d69-b3b3-95e77a1104f1.cpp:40:45: error: expression list treated a s compound expression in initializer [-fpermissive]    40 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ff2b86dd-048e-4d69-b3b3-95e77a1104f1.cpp:39: ff2b86dd-048e-4d69-b3b3-95e77a1104f1.cpp: In function ‘int main()’: ff2b86dd-048e-4d69-b3b3-95e77a1104f1.cpp:49:20: error: ‘by_length’ was not decla red in this scope    49 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ ff2b86dd-048e-4d69-b3b3-95e77a1104f1.cpp:49:19: error: ‘issame’ cannot be used a s a function    49 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -4,22 +4,20 @@
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
-vector<string> by_length(vector<int> arr);
-
-bool issame(vector<string> a, vector<string> b){
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
     return a == b;
 }
 
-int main(){
+std::vector<std::string> by_length(const std::vector<int>& arr);
+
+int main() {
     assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
     return 0;
 }
 
-vector<string> by_length(vector<int> arr){
-    vector<string> result;
-    vector<int> sorted_arr;
+std::vector<std::string> by_length(const std::vector<int>& arr){
+    std::vector<std::string> result;
+    std::vector<int> sorted_arr;
 
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
@@ -27,20 +25,13 @@ vector<string> by_length(vector<int> arr){
         }
     }
 
-    sort(sorted_arr.begin(), sorted_arr.end());
+    std::sort(sorted_arr.begin(), sorted_arr.end());
+    std::reverse(sorted_arr.begin(), sorted_arr.end());
 
-    reverse(sorted_arr.begin(), sorted_arr.end());
-
-    map<int, string> num_to_name = {
-        {1, "One"},
-        {2, "Two"},
-        {3, "Three"},
-        {4, "Four"},
-        {5, "Five"},
-        {6, "Six"},
-        {7, "Seven"},
-        {8, "Eight"},
-        {9, "Nine"}
+    std::map<int, std::string> num_to_name = {
+        {1, "One"}, {2, "Two"}, {3, "Three"},
+        {4, "Four"}, {5, "Five"}, {6, "Six"},
+        {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
     };
 
     for (int num : sorted_arr) {
