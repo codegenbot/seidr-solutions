145d9138d5472ce7348433e6c4fff74df648cd06
beep boop <githubbot@vadim.me>
2024-07-10 16:41:29 +0200

    Solution 6 of Python/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/4eawga2p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b448b104-a34e-4109-af70-25d9e16bd a33.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_99.py
+++ Python_99.py
@@ -1,9 +1,11 @@
-def closest_integer():
-    value = float(input("Enter a number: "))
-    num = value
-    if num % 1 < 0.5:
-        return int(num)
-    else:
-        return int(round(num))
-
-print(closest_integer())
+def round_half_up(value):
+    if value < 0:
+        if int(value) == value:
+            return int(value)
+        else:
+            return round(value)
+    elif value > 0:
+        if int(value) == value:
+            return int(value)
+        else:
+            return math.ceil(value)
