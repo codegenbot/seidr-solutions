cb01663d4d1da05c034b9db1fd0a951ddb5c9673
beep boop <githubbot@vadim.me>
2024-07-10 16:42:13 +0200

    Solution 48 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9wljto1e Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c7f48a37-0d8f-494a-beec-b12b8c897 6f5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,13 +1,7 @@
 def fruit_distribution(s, n):
     s = " ".join(s).replace("apples and", "apples ").replace("oranges", " oranges").split()
-    
-    if not any(word in s for word in ['apples', 'oranges']):
+    if 'apples' not in s or 'oranges' not in s:
         return None
-    
-    apples = int(' '.join(s[s.index("apples") + 6:]).replace("and", "").replace("apples ", ""))
-    oranges = int(' '.join(s[s.index(" oranges") + 8:]).replace("and", "").replace("oranges ", ""))
-    
-    if not (isinstance(apples, int) and isinstance(oranges, int)):
-        return None
-    
+    apples = int(s[s.index("apples") + 6:].replace("and", "").replace("apples ", ""))
+    oranges = int(s[s.index(" oranges") + 8:].replace("and", "").replace("oranges ", ""))
     return n - apples - oranges
