8ac69280575620a716672827c60019f7e28698b9
beep boop <githubbot@vadim.me>
2024-07-10 20:35:04 +0200

    Solution 10 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f77eacc6-013a-4a77-854b-f806b383b d98.py", line 21, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f77eacc6-013a-4a77-854b-f806b383b d98.py", line 16, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f77eacc6-013a-4a77-854b-f806b383b d98.py", line 9, in sort_third     sorted_l3 = sorted(l3)[::-1]                 ^^^^^^^^^^ TypeError: '<' not supported between instances of 'NoneType' and 'int'

--- Python_33.py
+++ Python_33.py
@@ -1,18 +1,15 @@
+```
 def sort_third(l: list):
-    if len(l) % 3 != 0:
-        l += [None] * (3 - len(l) % 3)
+    while None in l:
+        l.remove(None)
     l1 = [x for i, x in enumerate(l) if i % 3 == 0]
     l2 = [x for i, x in enumerate(l) if i % 3 == 1]
     l3 = [x for i, x in enumerate(l) if i % 3 == 2]
 
     sorted_l1 = sorted(l1)
-    sorted_l3 = sorted([x for x in l3 if x is not None])[::-1]
+    sorted_l3 = sorted([y for y in l3 if y is not None])[::-1]
 
     return [
-        (
-            x
-            if i % 3 != 0
-            else (x if i % 3 == 1 else next(y for y in sorted_l3[::-1] if y > x))
-        )
+        x if i % 3 != 0 else (x if i % 3 == 1 else next(y for y in l3[::-1] if y > x))
         for i, x in enumerate(sorted_l1 + [l2[0]] + sorted_l3)
     ]
