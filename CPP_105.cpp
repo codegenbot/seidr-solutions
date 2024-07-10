b580d58302e7a248ddabe4f28ae78d4193b92897
beep boop <githubbot@vadim.me>
2024-07-10 18:29:24 +0200

    Solution 9 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dctrev1c Prompt:       |                              ^~~~~~ 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp:28:37: error: ‘string’ was not declared  in this scope    28 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp:28:45: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp:27: 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp: In function ‘int main()’: 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp:37:20: error: ‘by_length’ was not decla red in this scope    37 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -4,35 +4,21 @@
 #include <algorithm>
 
 bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
+    if (a.size() != b.size()) {
+        return false;
+    }
 
-vector<string> by_length(vector<int> arr) {
-    vector<string> result;
-    vector<int> filtered_arr;
-
-    for (int num : arr) {
-        if (num >= 1 && num <= 9) {
-            filtered_arr.push_back(num);
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
         }
     }
 
-    sort(filtered_arr.begin(), filtered_arr.end());
-
-    reverse(filtered_arr.begin(), filtered_arr.end());
-
-    map<int, string> num_to_name = {
-        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
-        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
-    };
-
-    for (int num : filtered_arr) {
-        result.push_back(num_to_name[num]);
-    }
-
-    return result;
+    return true;
 }
 
 int main() {
-    assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"});
+    assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
+
+    return 0;
 }
