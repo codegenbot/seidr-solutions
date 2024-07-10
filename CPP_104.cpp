81473dccf9f350898b366c7866d6176893b38972
beep boop <githubbot@vadim.me>
2024-07-10 17:39:16 +0200

    Solution 11 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/zpn5dbli Prompt:       |                    ^~~ f7344644-53f4-46ab-aff3-6dde583399b6.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f7344644-53f4-46ab-aff3-6dde583399b6.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f7344644-53f4-46ab-aff3-6dde583399b6.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f7344644-53f4-46ab-aff3-6dde583399b6.cpp:21: f7344644-53f4-46ab-aff3-6dde583399b6.cpp: In function ‘int main()’: f7344644-53f4-46ab-aff3-6dde583399b6.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ f7344644-53f4-46ab-aff3-6dde583399b6.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -6,7 +6,22 @@ bool are_same(vector<int> a, vector<int> b){
 
 vector<int> unique_digits(vector<int> x){
     vector<int> result;
-    // Implementation of unique_digits function
+    for (int num : x) {
+        bool hasEvenDigit = false;
+        int temp = num;
+        while (temp > 0) {
+            if (temp % 2 == 0) {
+                hasEvenDigit = true;
+                break;
+            }
+            temp /= 10;
+        }
+        if (!hasEvenDigit) {
+            result.push_back(num);
+        }
+    }
+    sort(result.begin(), result.end());
+    return result;
 }
 
 int main(){
