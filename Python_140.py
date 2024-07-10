0cb66efa1ba61dc8b47a2af8888e3827f1352ab3
beep boop <githubbot@vadim.me>
2024-07-10 22:49:46 +0200

    Solution 30 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ygu5uohz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2ab52bee-af50-4d38-a01f-ea9387b34 480.py", line 29, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2ab52bee-af50-4d38-a01f-ea9387b34 480.py", line 25, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -4,7 +4,7 @@ def fix_spaces(text):
     in_word = False
     for char in text:
         if char == ' ':
-            if not in_word:
+            if char == ' ' and not in_word:
                 new_text += ' '
             else:
                 if count <= 2:
@@ -13,14 +13,7 @@ def fix_spaces(text):
                     new_text += '-'
                 count = 0
             in_word = False
-        elif char == '\n':
-            if count <= 2:
-                new_text += '\n_'
-            else:
-                new_text += '\n-'
-            in_word = False
-            count = 0
-        elif char != ' ' and char != '\n':
+        elif char != '\n':
             new_text += char
             in_word = True
             count += 1
