2c8839da91a0a026912b9c2fb8856541f129e397
beep boop <githubbot@vadim.me>
2024-07-10 22:11:24 +0200

    Solution 42 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/5eehrv7c Prompt: 9cc75997-c440-411b-a471-0427d9d1ea64.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int bowlingScore(std::string input) {       |                       ^~~~~~ 9cc75997-c440-411b-a471-0427d9d1ea64.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int bowlingScore(std::string input) {

--- bowling.cpp
+++ bowling.cpp
@@ -1,56 +1,41 @@
-#include <iostream>
 #include <string>
 
-int bowlingScore(string input) {
+int bowlingScore(std::string input) {
     int score = 0;
-    int currentRoll = 0;
-    int frameNumber = 1;
-
-    for (int i = 0; i < input.size(); i++) {
-        if (input.substr(i, 2) == "X/") { // Strike
-            score += 10 + bowlingScoreForExtraRolls(input.substr(i+2), 0);
-            i += 3;
-            frameNumber++;
-        } else if (input[i] == '/') {
-            currentRoll = 0;
-            frameNumber++;
-        } else if (isdigit(input[i])) { // Number of pins knocked down
-            currentRoll = (currentRoll * 10) + (input[i] - '0');
-            if (i < input.size() - 1 && input[i+1] == '/') {
-                score += currentRoll;
-                i++; // Skip the '/'
-                currentRoll = 0;
-                frameNumber++;
+    bool firstInFrame = true;
+    bool strikeOrSpare = false;
+    for (char c : input) {
+        if (c == '|') {
+            if (!firstInFrame && !strikeOrSpare) {
+                if (score < 10) {
+                    score += 10 - score;
+                } else {
+                    score += 10;
+                }
             }
-        } else {
-            continue; // ignore non-numerical characters
-        }
-    }
-
-    return score;
-}
-
-int bowlingScoreForExtraRolls(string input, int i) {
-    int score = 0;
-
-    for (int j = i + 1; j < input.size(); j++) {
-        if (input.substr(j, 2) == "X/") { // Strike
+            firstInFrame = true;
+            strikeOrSpare = false;
+        } else if (c == 'X') {
             score += 10;
-            continue;
-        } else if (isdigit(input[j])) { // Number of pins knocked down
-            int currentRoll = (input[j] - '0');
-            if (j < input.size() - 1 && input[j+1] == '/') {
-                score += currentRoll;
-                j++; // Skip the '/'
-                continue;
+            if (!firstInFrame && !strikeOrSpare) {
+                score += input[1] - '0' + input[2] - '0';
             }
-            return score + currentRoll;
+            firstInFrame = true;
+            strikeOrSpare = false;
+        } else if (c == '-') {
+            firstInFrame = true;
+        } else {
+            int pins = c - '0';
+            score += pins;
+            if (!firstInFrame) {
+                if (score + pins >= 10) {
+                    score += 10 - score;
+                } else {
+                    score += pins;
+                }
+                strikeOrSpare = true;
+            }
+            firstInFrame = false;
         }
     }
-
-    return score; // Return score without considering the last frame
-}
-
-int main() {
-    std::cout << bowlingScore("X|---5-8-|X||25--|-1-3-4-6-8-9-7");
-}
+    return score;
