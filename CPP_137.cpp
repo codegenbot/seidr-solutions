f584b322d1b3cf4445062191b1d94b4ce08e9c82
beep boop <githubbot@vadim.me>
2024-07-11 01:21:08 +0200

    Solution 69 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9snlnzo8 Prompt: 4066f59e-cca0-4a16-9682-7321fd060e60.cpp:3:10: fatal error: boost/variant.hpp: N o such file or directory     3 | #include <boost/variant.hpp>       |          ^~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -8,4 +8,4 @@ boost::any compare_one(boost::any a, boost::any b) {
     if (holds_alternative<int>(a) && holds_alternative<double>(b)) {
         return get<int>(a) > get<double>(b) ? a : b;
     }
-    else if (holds_alternation
+    else if (holds_alternative<double>(a) && holds_alternation
