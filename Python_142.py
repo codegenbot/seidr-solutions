8f6d65848b272f7f7105baa7665ebd28fea5793e
beep boop <githubbot@vadim.me>
2024-07-11 00:35:52 +0200

    Solution 13 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/be60kwzt Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b7239f93-4832-45e0-92b6-2db35ee05 2cb.py", line 42, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b7239f93-4832-45e0-92b6-2db35ee05 2cb.py", line 12, in check     sum_squares(   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b7239f93-4832-45e0-92b6-2db35ee05 2cb.py", line 5, in sum_squares     raise ValueError("Invalid input. Only numbers are allowed.") ValueError: Invalid input. Only numbers are allowed.

--- Python_142.py
+++ Python_142.py
@@ -1,8 +1,5 @@
 def sum_squares(*args):
-    total_sum = 0
-    for i in range(len(args)):
-        if i % 3 == 0 and i % 4 != 0:
-            total_sum += args[i] ** 2
-        elif i % 4 == 0 and i % 3 != 0:
-            total_sum += args[i] ** 3
-    return total_sum
+    for arg in args:
+        if not isinstance(arg, (int, float)):
+            raise ValueError("Invalid input. Only numbers are allowed.")
+    return sum([i ** 2 for i in args])
