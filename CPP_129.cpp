f1bf32ab53a342d271336bd25a520463a6099926
beep boop <githubbot@vadim.me>
2024-07-10 07:50:58 +0200

    Solution 1 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/jyhopen8 Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,52 +1,42 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
 
 vector<int> minPath(vector<vector<int>>& grid, int k) {
     int n = grid.size();
-    vector<vector<bool>> visited(n, vector<bool>(n));
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>> > pq;
-    vector<int> res;
-    
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (!visited[i][j]) {
-                visited[i][j] = true;
-                pq.push({grid[i][j], {i, j}});
+    vector<vector<int>> dp(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 && j == 0) {
+                dp[i][j] = grid[i][j];
+            } else if (i > 0) {
+                dp[i][j] = min({dp[i-1][j], dp[i][j-1]});
+            } else {
+                dp[i][j] = min({dp[i][j-1]});
             }
         }
     }
 
-    while (!pq.empty()) {
-        int val = pq.top().first;
-        int i = pq.top().second.first;
-        int j = pq.top().second.second;
-        pq.pop();
-        
-        res.push_back(val);
-        
-        if (k > 0) {
-            for (int x : {-1, 0, 1}) {
-                for (int y : {-1, 0, 1}) {
-                    if (i + x >= 0 && i + x < n && j + y >= 0 && j + y < n && !visited[i + x][j + y]) {
-                        visited[i + x][j + y] = true;
-                        pq.push({val, {i + x, j + y}});
-                    }
-                }
-            }
-        } else break;
-
-        --k;
+    vector<int> res;
+    int i = n - 1, j = n - 1;
+    while (k--) {
+        res.push_back(grid[i][j]);
+        if (i > 0 && dp[i-1][j] < dp[i][j-1]) {
+            i--;
+        } else {
+            j--;
+        }
     }
 
     return res;
 }
 
 int main() {
-    vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector<vector<int>> grid = {{1,2,3}, {4,5,6}, {7,8,9}};
     int k = 3;
-    vector<int> res = minPath(grid, k);
-    for (int i : res) cout << i << " ";
+    vector<int> result = minPath(grid, k);
+    for (int i : result) {
+        cout << i << " ";
+    }
     return 0;
 }
