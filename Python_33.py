d93bd5e4077286df94f8affd626d5cf6bcda9a3b
beep boop <githubbot@vadim.me>
2024-07-10 19:53:39 +0200

    Solution 54 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4wfttiik Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ffdf6115-d67d-4128-9b93-3f36f0cb3 3dc.py", line 15, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ffdf6115-d67d-4128-9b93-3f36f0cb3 3dc.py", line 10, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,8 +1,8 @@
+```
 def sort_third(l: list):
-    result = sorted([i for i in l if i % 3 != 0])
-    if len(l) % 3 == 1:
-        return [l[-1]]
-    elif len(l) % 3 == 2:
-        return [l[-1], l[-2]]
-    else:
-        return result
+    thirds = [i for i in l if i % 3 == 0]
+    others = sorted([i for i in l if i % 3 != 0])
+    result = others
+    if len(thirds) > 0:
+        result += thirds
+    return result
