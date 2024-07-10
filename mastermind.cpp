e533d710d5f723e0eb6a82e65a90a39fbb862391
beep boop <githubbot@vadim.me>
2024-07-10 09:24:08 +0200

    Solution 45 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/l24z641c Prompt: Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.

--- mastermind.cpp
+++ mastermind.cpp
@@ -2,42 +2,35 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 int mastermind(string code, string guess) {
     int white = 0;
     int black = 0;
-    
-    // Count the number of correct colors in wrong places
-    for (int i = 0; i < 4; i++) {
-        if (code[i] == guess[i]) {
-            black++;
-            code[i] = '\0';
-            guess[i] = '\0';
-        }
-    }
-    
-    // Count the number of correct colors in right places
-    for (int i = 0; i < 4; i++) {
-        int j = 0;
-        while (j < 4) {
-            if (code[j] == guess[i]) {
-                white++;
-                code[j] = '\0';
-                break;
-            }
-            j++;
-        }
-    }
-    
-    return make_pair(white, black).second;
-}
 
-int main() {
-    string code, guess;
-    cin >> code >> guess;
-    int result = mastermind(code, guess);
-    cout << result.first << endl;
-    cout << result.second << endl;
-    return 0;
+    // Count the number of correct colors in wrong positions
+    vector<char> code_chars(code.begin(), code.end());
+    for (char c : guess) {
+        if (code_chars.count(c)) {
+            code_chars.erase(remove(code_chars.begin(), code_chars.end(), c), code_chars.end());
+            white++;
+        }
+    }
+
+    // Count the number of correct colors in correct positions
+    int i = 0;
+    while (i < 4) {
+        if (guess[i] == code[i]) {
+            black++;
+            for (char& c : code_chars) {
+                if (c == guess[i]) {
+                    code_chars.erase(remove(code_chars.begin(), code_chars.end(), c), code_chars.end());
+                    break;
+                }
+            }
+            i++;
+        } else {
+            i++;
+        }
+    }
+
+    return black + white;
 }
