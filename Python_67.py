a5ff9ef2f12fe8ab85d1c401dfeeb7cd53e9518c
beep boop <githubbot@vadim.me>
2024-07-10 16:52:31 +0200

    Solution 65 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9wljto1e Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dbc4228a-c58a-4981-bf31-a6d59ee98 420.py", line 16, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dbc4228a-c58a-4981-bf31-a6d59ee98 420.py", line 13, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: fruit_distribution() takes 1 positional argument but 2 were given

--- Python_67.py
+++ Python_67.py
@@ -1,9 +1,2 @@
-def fruit_distribution(s):
-    s = " ".join(s).replace("apples and", "apples ").replace("oranges", " oranges").split()
-    if 'apples' not in s or 'oranges' not in s:
-        return None
-    apples_and_part = [part for part in s if "apples" in part][0]
-    apples = int(apples_and_part.replace("apples ", "").replace("and", ""))
-    oranges_and_part = [part for part in s if "oranges" in part][0]
-    oranges = int(oranges_and_part.replace("oranges ", "").replace("and", ""))
-    return len(s) - sum([1 if x == 'apples' else 1 if x == 'oranges' else 0 for x in s])
+def check(s):
+    return fruit_distribution("1 apples and 100 oranges")
