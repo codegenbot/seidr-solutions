ff3b695566fcf1e1f33792512c7d92921b8fe34a
beep boop <githubbot@vadim.me>
2024-07-10 19:49:10 +0200

    Solution 38 of Python/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uth21hc5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c924c5e8-1e88-4fe6-8188-c6fa11973 069.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_7.py
+++ Python_7.py
@@ -5,6 +5,6 @@ def filter_by_substring(strings: List[str], substring: str) -> List[str]:
     return [s for s in strings if substring in s]
 
 
-strings = input("Enter the list of strings (comma separated): ").strip().split(",")
-strings = [x.strip() for x in strings]
-print(filter_by_substring(strings, input("Enter the substring to filter by: ").strip()))
+strings = input("Enter the list of strings (comma separated): ").strip().split(',')
+substring = input("Enter the substring to filter by: ").strip()
+print(filter_by_substring([x.strip() for x in strings], substring))
