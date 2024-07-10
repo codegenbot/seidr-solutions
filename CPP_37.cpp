f39ab3612e9a36ac3a241be51291623c9cc79530
beep boop <githubbot@vadim.me>
2024-07-10 06:42:30 +0200

    Solution 87 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lerjqzl3 Prompt:    21 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 604a1eed-c580-4342-bfdb-ad8152512ee5.cpp:21:36: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 604a1eed-c580-4342-bfdb-ad8152512ee5.cpp:21:43: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 604a1eed-c580-4342-bfdb-ad8152512ee5.cpp:20: 604a1eed-c580-4342-bfdb-ad8152512ee5.cpp: In function ‘int main()’: 604a1eed-c580-4342-bfdb-ad8152512ee5.cpp:30:20: error: ‘sort_even’ was not decla red in this scope    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 604a1eed-c580-4342-bfdb-ad8152512ee5.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -7,41 +7,19 @@ int main() {
     vector<float> v(n);
     for (float &x : v) cin >> x;
 
-    vector<float> result = sort_even(v);
-
-    cout << "Sorted even numbers: ";
-    for (float x : result) {
-        if (!issame(result, v)) break;
-        cout << x << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
-
-bool issame(vector<float> a, vector<float> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (abs(a[i] - b[i]) > 1e-6) return false;
-    }
-    return true;
-}
-
-vector<float> sort_even(vector<float> l) {
-    vector<float> result(l.size());
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) {
-            vector<float> temp;
-            for (int j = 0; j < l.size(); j++) {
-                if (j % 2 == 0) {
-                    temp.push_back(l[j]);
-                }
-            }
-            sort(temp.begin(), temp.end());
-            result[i] = temp[0];
-        } else {
-            result[i] = l[i];
+    bool check = true;
+    for (int i = 0; i < n - 1; i++) {
+        if (v[i] > v[i + 1]) {
+            check = false;
+            break;
         }
     }
-    return result;
+
+    if (check) {
+        cout << "The array is sorted in ascending order." << endl;
+    } else {
+        cout << "The array is not sorted in ascending order." << endl;
+    }
+
+    return 0;
 }
