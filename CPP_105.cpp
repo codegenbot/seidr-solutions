2af7e42e4e1c5e2357ed4cf5bd98bf1f51389418
beep boop <githubbot@vadim.me>
2024-07-10 07:17:29 +0200

    Solution 8 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/173h9p48 Prompt:       |                                 ^~~~~~ In file included from 12bff23c-0956-4cba-9c85-f0c5cb735b2a.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 12bff23c-0956-4cba-9c85-f0c5cb735b2a.cpp:15:44: error: ‘b’ was not declared in t his scope    15 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 12bff23c-0956-4cba-9c85-f0c5cb735b2a.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 12bff23c-0956-4cba-9c85-f0c5cb735b2a.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~ In file included from 12bff23c-0956-4cba-9c85-f0c5cb735b2a.cpp:14: 12bff23c-0956-4cba-9c85-f0c5cb735b2a.cpp: In function ‘int main()’: 12bff23c-0956-4cba-9c85-f0c5cb735b2a.cpp:24:20: error: ‘by_length’ was not decla red in this scope    24 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -7,15 +7,16 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return a == b;
 }
 
-bool by_length(std::vector<int> nums) {
-    std::map<int, std::string> num_to_word = {{9, "Nine"}, {4, "Four"}, {8, "Eight"}};
-    std::vector<std::string> words;
-    for (int num : nums) {
-        words.push_back(num_to_word[num]);
+std::vector<std::string> by_length(std::vector<int> indices) {
+    std::vector<std::string> words = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    std::vector<std::string> result;
+    for (int i : indices) {
+        result.push_back(words[i - 1]);
     }
-    return issame(words, {"Nine", "Eight", "Four"});
+    return result;
 }
 
 int main() {
-    assert(by_length({9, 4, 8}));
+    assert(issame({"Nine", "Eight", "Four"}, {"Nine", "Eight", "Four"}));
+    assert(issame(by_length({9, 4, 8}), {"Nine", "Four", "Eight"});
 }
