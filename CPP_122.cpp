ba1b264034f9546e0ecd2f40968e67d3a4267d4f
beep boop <githubbot@vadim.me>
2024-07-10 20:05:08 +0200

    Solution 80 of CPP/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/5k4yo8s0 Prompt:     2 |     for (int i = 0; i < k; ++i) {       |     ^~~ d90ac925-81cd-40c5-8e4a-38be8d4cbfc2.cpp:2:21: error: ‘i’ does not name a type     2 |     for (int i = 0; i < k; ++i) {       |                     ^ d90ac925-81cd-40c5-8e4a-38be8d4cbfc2.cpp:2:28: error: expected unqualified-id be fore ‘++’ token     2 |     for (int i = 0; i < k; ++i) {       |                            ^~ d90ac925-81cd-40c5-8e4a-38be8d4cbfc2.cpp:7:5: error: expected unqualified-id bef ore ‘return’     7 |     return sum;       |     ^~~~~~ d90ac925-81cd-40c5-8e4a-38be8d4cbfc2.cpp:8:1: error: expected declaration before  ‘}’ token     8 | }       | ^ In file included from d90ac925-81cd-40c5-8e4a-38be8d4cbfc2.cpp:10: d90ac925-81cd-40c5-8e4a-38be8d4cbfc2.cpp: In function ‘int main()’: d90ac925-81cd-40c5-8e4a-38be8d4cbfc2.cpp:12:13: error: ‘add_elements’ was not de clared in this scope    12 |     assert (add_elements({1}, 1) == 1);       |             ^~~~~~~~~~~~

--- CPP_122.cpp
+++ CPP_122.cpp
@@ -1,17 +1,9 @@
-#include <iostream>
-#include <cassert>
-
-int add_elements(int* arr, int k) {
+int add_elements(int arr[], int k) {
     int sum = 0;
     for (int i = 0; i < k; ++i) {
-        if (arr[i]>=10 && arr[i]<=99) {
+        if (arr[i] >= 10 && arr[i] <= 99) {
             sum += arr[i];
         }
     }
     return sum;
-}
-
-int main() {
-    assert (add_elements({1}, 1) == 1);
-    return 0;
 }
