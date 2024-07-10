5b860da679d8b2a71b8a92e41372ec91497549c0
beep boop <githubbot@vadim.me>
2024-07-10 02:18:22 +0200

    Solution 5 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0oye3yx5 Prompt: 7d3d7635-24f0-4846-8020-b1fc27110d01.cpp:3:12: error: ‘vector’ was not declared in this scope     3 | int search(vector<int> lst) {       |            ^~~~~~ 7d3d7635-24f0-4846-8020-b1fc27110d01.cpp:3:19: error: expected primary-expressio n before ‘int’     3 | int search(vector<int> lst) {       |                   ^~~ In file included from 7d3d7635-24f0-4846-8020-b1fc27110d01.cpp:20: 7d3d7635-24f0-4846-8020-b1fc27110d01.cpp: In function ‘int main()’: 7d3d7635-24f0-4846-8020-b1fc27110d01.cpp:22:19: error: ‘search’ cannot be used a s a function    22 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,8 +1,8 @@
-#include <vector>
 #include <map>
+#include <vector>
 
 int search(vector<int> lst) {
-    std::map<int, int> freq;
+    map<int, int> freq;
     for (int num : lst) {
         if (freq.find(num) == freq.end()) {
             freq[num] = 1;
