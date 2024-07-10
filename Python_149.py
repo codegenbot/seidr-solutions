b79b258764b36c0d949d2f0d2154239d3f7ed55e
beep boop <githubbot@vadim.me>
2024-07-10 08:01:44 +0200

    Solution 56 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j9zzr72u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc48c781-9e44-486c-935e-e4305ee13 31d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,25 +1,19 @@
 def main():
     while True:
         try:
-            lst = (input("Enter words (separated by space): ") 
-                   .lower() 
-                   .replace(" ", "") 
-                   .split())
+            lst = (
+                input("Enter words (separated by space): ")
+                .lower()
+                .replace(" ", "")
+                .split()
+            )
             break
         except Exception as e:
             print(f"Error: {str(e)}")
             continue
 
     if len(lst) > 0:
-        while True:
-            try:
-                even_word_sum = sum(len(word) for word in lst if len(word) % 2 == 0)
-                input_val = int(input("Press Enter to calculate the sum or type 'q' to quit: "))
-                if input_val == 113: 
-                    break
-                print(even_word_sum)
-            except Exception as e:
-                print(f"Error: {str(e)}")
-                continue
+        even_word_sum = sum(len(word) for word in lst if len(word) % 2 == 0)
+        print(even_word_sum)
     else:
         print("Please enter at least one word.")
