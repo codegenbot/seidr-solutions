590b964984d8d6eea72354ca553b5cf948e82a39
beep boop <githubbot@vadim.me>
2024-07-10 16:25:27 +0200

    Solution 3 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qdrmvsfx Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -1,11 +1,14 @@
-```
 def add(x: int, y: int):
     return x + y
 
 x = input("Enter first number: ")
 y = input("Enter second number: ")
 
-if not (x.isnumeric() and y.isnumeric()):
+try:
+    x = int(x)
+    y = int(y)
+
+    print("The sum of the given numbers is", add(x, y))
+
+except ValueError:
     print("Invalid inputs. Please enter integers.")
-else:
-    print("The sum of the given numbers is", add(int(x), int(y)))
