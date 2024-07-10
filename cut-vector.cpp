85ae2425ead1625e0919433ea36938f4e3cb0746
beep boop <githubbot@vadim.me>
2024-07-10 09:11:13 +0200

    Solution 92 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/jtwmj2dd Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,49 +1,26 @@
 #include <vector>
-#include <iostream>
+using namespace std;
 
-std::pair<std::vector<int>, std::vector<int>> cutVector(std::vector<int> vec) {
+vector<int> cutVector(vector<int>& nums) {
     int minDiff = INT_MAX;
     int index = -1;
-
-    for (int i = 0; i < vec.size(); ++i) {
-        int sumLeft = 0, sumRight = 0;
-        for (int j = 0; j < i; ++j)
-            sumLeft += vec[j];
-        for (int j = i + 1; j < vec.size(); ++j)
-            sumRight += vec[j];
-
-        if (sumLeft == sumRight || abs(sumLeft - sumRight) < minDiff) {
-            minDiff = abs(sumLeft - sumRight);
+    
+    for (int i = 0; i < nums.size() - 1; ++i) {
+        if (abs(nums[i] - nums[i + 1]) <= minDiff) {
+            minDiff = abs(nums[i] - nums[i + 1]);
             index = i;
         }
     }
-
-    std::vector<int> left, right;
-    for (int i = 0; i < index; ++i)
-        left.push_back(vec[i]);
-    for (int i = index; i < vec.size(); ++i)
-        right.push_back(vec[i]);
-
+    
+    vector<int> left = {nums[0]};
+    for (int i = 0; i < index; ++i) {
+        left.push_back(nums[i + 1]);
+    }
+    
+    vector<int> right = {nums[index + 1]};
+    for (int i = index + 1; i < nums.size(); ++i) {
+        right.push_back(nums[i]);
+    }
+    
     return {left, right};
-}
-
-int main() {
-    int n;
-    std::cin >> n;
-
-    std::vector<int> vec(n);
-    for (int &x : vec) 
-        cin >> x;
-
-    pair<vector<int>, vector<int>> res = cutVector(vec);
-
-    cout << "[";
-    for (int x : res.first)
-        cout << x << " ";
-    cout << "] ["; 
-    for (int x : res.second)
-        cout << x << " ";
-    cout << "]" << endl;
-
-    return 0;
 }
