ed23d49c1d77d4ff340b16eb940be696ea1db53d
beep boop <githubbot@vadim.me>
2024-07-10 04:05:25 +0200

    Solution 4 of Python/125, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/b1h3gbde Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b9502b6a-ff18-4956-bad7-1b77689ad 5a0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_125.py
+++ Python_125.py
@@ -1,7 +1,12 @@
-```
 def split_words(txt):
     words = txt.split()
     if len(words) > 0 and any(char.isalnum() for char in words[0]):
         return words
     else:
-        return sum(1 for i in range(ord('a'), ord('z')+1) if txt.count(chr(i)) % 2 != 0)
+        count_dict = {}
+        for char in txt:
+            if char in count_dict:
+                count_dict[char] += 1
+            else:
+                count_dict[char] = 1
+        return sum(1 for count in count_dict.values() if count % 2 != 0)
