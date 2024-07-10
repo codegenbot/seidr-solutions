15b118791a02b02b609b211bf07834e279e66029
beep boop <githubbot@vadim.me>
2024-07-10 16:27:12 +0200

    Solution 13 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g2bsquhq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3e8b7627-88b1-4be5-93d3-41529c2bf 6b7.py", line 12, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3e8b7627-88b1-4be5-93d3-41529c2bf 6b7.py", line 9, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3e8b7627-88b1-4be5-93d3-41529c2bf 6b7.py", line 4, in fruit_distribution     oranges = int(quantities[-1].replace("oranges ",""))               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: 'oranges'

--- Python_67.py
+++ Python_67.py
@@ -1,4 +1,4 @@
-```
+```python
 def fruit_distribution(s, n):
     quantities = [i for i in s.split() if "apples" in i or "oranges" in i]
     apples = int(''.join(filter(str.isdigit, ' '.join(quantities)[0:-9])))
