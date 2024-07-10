594cda54a501f8b786738ed9eea0302b3fcd170a
beep boop <githubbot@vadim.me>
2024-07-10 08:23:47 +0200

    Solution 48 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/jtwmj2dd Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,42 +1,37 @@
 #include <vector>
-#include <iostream>
+using namespace std;
 
-std::pair<std::vector<int>, std::vector<int>> cutVector(const std::vector<int>& v) {
+vector<vector<int>> cutVector(vector<int> vec) {
     int minDiff = INT_MAX;
-    int cutIndex = 0;
-
-    for (int i = 1; i < v.size(); ++i) {
-        int diff = abs(v[i] - v[0]);
-        if (diff <= minDiff) {
+    int splitIndex = 0;
+    
+    for (int i = 1; i < vec.size(); i++) {
+        int diff = abs(vec[i] - vec[i-1]);
+        if (diff < minDiff) {
             minDiff = diff;
-            cutIndex = i;
+            splitIndex = i;
         }
     }
-
-    return {{v[0]}, {v.begin() + cutIndex, v.end()}};
+    
+    vector<vector<int>> result(2);
+    result[0].assign(vec.begin(), vec.begin() + splitIndex);
+    result[1].assign(vec.begin() + splitIndex, vec.end());
+    return result;
 }
 
 int main() {
-    std::vector<int> input;
     int n;
-    std::cin >> n;
-    for (int i = 0; i < n; ++i) {
-        int num;
-        std::cin >> num;
-        input.push_back(num);
-    }
+    cin >> n;
+    vector<int> vec(n);
+    for (auto &x : vec) cin >> x;
 
-    std::pair<std::vector<int>, std::vector<int>> result = cutVector(input);
+    vector<vector<int>> res = cutVector(vec);
 
-    for (const auto& num : result.first) {
-        std::cout << num << " ";
-    }
-    std::cout << "\n";
-
-    for (const auto& num : result.second) {
-        std::cout << num << " ";
-    }
-    std::cout << "\n";
+    cout << "[";
+    for (const auto &x : res[0]) cout << x << " ";
+    cout << "]\n[";
+    for (const auto &x : res[1]) cout << x << " ";
+    cout << "]";
 
     return 0;
 }
