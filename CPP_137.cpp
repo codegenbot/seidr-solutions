3293c7d436f2430428e9aadb7365d27fe2dd7578
beep boop <githubbot@vadim.me>
2024-07-10 10:59:20 +0200

    Solution 72 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/51eh1c3h Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,40 +1,79 @@
-#include <boost/any.hpp>
-#include <string>
-
-using namespace boost;
-
+```
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return (get<int>(a) > get<int>(b)) ? a : ((get<int>(a) < get<int>(b)) ? b : boost::any("None")));
-    }
-    else if (a.type() == typeid(double) && b.type() == typeid(double)) {
-        return (get<double>(a) > get<double>(b)) ? a : ((get<double>(a) < get<double>(b)) ? b : boost::any("None")));
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = any_cast<string>(a);
-        string str2 = any_cast<string>(b);
-        double num1 = stod(str1.substr(0, str1.find(',')));
-        double num2 = stod(str2.substr(0, str2.find(',')));
-        return (num1 > num2) ? a : ((num1 < num2) ? b : boost::any("None")));
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        int i = get<int>(a);
-        double d = get<double>(b);
-        return (i > d) ? a : ((i < d) ? b : boost::any("None")));
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return (int)b > (int)a ? b : "None";
     }
     else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int i = get<int>(a);
-        string s = any_cast<string>(b);
-        double d = stod(s.substr(0, s.find(',')));
-        return ((i > d) ? a : ((i < d) ? b : boost::any("None"))));
+        string str = (string)b;
+        int val = 0;
+        bool isDecimal = false;
+        for (char c : str) {
+            if (!isDecimal && (c == '.' || c == ',')) {
+                isDecimal = true;
+                continue;
+            }
+            if (isdigit(c)) {
+                val = val * 10 + (c - '0');
+            }
+        }
+        return val > a.convert_to<int>() ? b : "None";
     }
-    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        double d = get<double>(a);
-        string s = any_cast<string>(b);
-        int i = stod(s.substr(0, s.find(',')));
-        return ((d > i) ? a : ((d < i) ? b : boost::any("None"))));
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = (string)a;
+        string str2 = (string)b;
+        int val1 = 0, val2 = 0;
+        bool isDecimal1 = false, isDecimal2 = false;
+        for (char c : str1) {
+            if (!isDecimal1 && (c == '.' || c == ',')) {
+                isDecimal1 = true;
+                continue;
+            }
+            if (isdigit(c)) {
+                val1 = val1 * 10 + (c - '0');
+            }
+        }
+        for (char c : str2) {
+            if (!isDecimal2 && (c == '.' || c == ',')) {
+                isDecimal2 = true;
+                continue;
+            }
+            if (isdigit(c)) {
+                val2 = val2 * 10 + (c - '0');
+            }
+        }
+        return val1 > val2 ? a : b == a ? "None" : b;
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        string str = (string)b;
+        float val = 0.0f;
+        bool isDecimal = false;
+        for (char c : str) {
+            if (!isDecimal && (c == '.' || c == ',')) {
+                isDecimal = true;
+                continue;
+            }
+            if (isdigit(c)) {
+                val = val * 10 + (c - '0');
+            }
+        }
+        return a.convert_to<float>() > val ? a : b;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        string str = (string)a;
+        float val = a.convert_to<float>();
+        bool isDecimal = false;
+        for (char c : str) {
+            if (!isDecimal && (c == '.' || c == ',')) {
+                isDecimal = true;
+                continue;
+            }
+            if (isdigit(c)) {
+                val = val * 10 + (c - '0');
+            }
+        }
+        return val > a.convert_to<float>() ? a : "None";
     }
     else {
-        return boost::any("None");
+        return a > b ? a : b == a ? "None" : b;
     }
 }
