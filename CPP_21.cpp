bfc6b6466a28abe7fcb982fa5e3bdc72295c64c0
beep boop <githubbot@vadim.me>
2024-07-10 22:33:40 +0200

    Solution 46 of CPP/21, ratio 0.6 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/gajkdsd6 Prompt: f36bc1f4-6a4e-4a41-88f1-18aae352d975.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ f36bc1f4-6a4e-4a41-88f1-18aae352d975.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,13 +1,9 @@
-
 #include <iostream>
 #include <vector>
-#include <cassert>
 
-bool issame(vector<float> a, vector<float> b) {
-    return a == b;
-}
+using namespace std;
 
-vector<float> rescale_to_unit(const vector<float>& numbers) {
+vector<float> rescale_to_unit(vector<float> numbers) {
     float min_num = numbers[0];
     float max_num = numbers[0];
     for (float num : numbers) {
@@ -26,10 +22,4 @@ vector<float> rescale_to_unit(const vector<float>& numbers) {
     }
 
     return rescaled_numbers;
-}
-
-int main() {
-    assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), {0.25, 0.0, 1.0, 0.5, 0.75}));
-
-    return 0;
 }
