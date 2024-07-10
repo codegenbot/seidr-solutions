f673e6dce2229f14c2becfeec354fec038582c5e
beep boop <githubbot@vadim.me>
2024-07-10 05:15:55 +0200

    Solution 24 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/v7cr6z5j Prompt:     4 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a138f660-8715-4449-982d-ccf4568fdc47.cpp:4:44: error: ‘b’ was not declared in th is scope     4 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a138f660-8715-4449-982d-ccf4568fdc47.cpp:4:45: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a138f660-8715-4449-982d-ccf4568fdc47.cpp:3: a138f660-8715-4449-982d-ccf4568fdc47.cpp: In function ‘int main()’: a138f660-8715-4449-982d-ccf4568fdc47.cpp:13:20: error: ‘sorted_list_sum’ was not  declared in this scope    13 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ a138f660-8715-4449-982d-ccf4568fdc47.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,19 +1,40 @@
 #include <vector>
 #include <string>
 
-bool same(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
+bool issame(vector<string> a, vector<string>b) {
+    if(a.size() != b.size()) {
         return false;
     }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[b.size()-1-i]) {
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
             return false;
         }
     }
     return true;
 }
 
-int main() {
-    assert(same({{"aaaa", "bbbb"}, {"cc", "dd"}} , {{"cc", "dd"}, {"aaaa", "bbbb"}}));
-    return 0;
+struct Result {
+    std::string str;
+    int sum;
+};
+
+Result process(vector<string> input) {
+    vector<string> a(input.begin(), input.end());
+    sort(a.begin(), a.end());
+    
+    if(issame(a, {"apple", "banana"})) {
+        return { "same", 0 };
+    } else if(issame(a, {"banana", "apple"})) {
+        return { "same", 1 };
+    } else {
+        Result result = {"different", 2};
+        for(int i = 0; i < a.size(); i++) {
+            if(a[i] == "apple") {
+                result.sum += 0;
+            } else if(a[i] == "banana") {
+                result.sum += 1;
+            }
+        }
+        return result;
+    }
 }
