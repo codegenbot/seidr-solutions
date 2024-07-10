d9ca2f5ccdb4accd06ed5a4673d1b3c2e328f517
beep boop <githubbot@vadim.me>
2024-07-10 19:38:19 +0200

    Solution 26 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/76f84mdo Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/22c0b5ff-c34d-40ac-a8c7-0f589c7e6 c34.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bouncing-balls.py
+++ bouncing-balls.py
@@ -1,17 +1,14 @@
+```
 def bouncing_balls():
     start_height = float(input())
     first_bounce_height = float(input())
-    bounciness_index = (first_bounce_height / start_height)
-    
+    bounciness_index = first_bounce_height / start_height
     num_bounces = int(input())
     
     total_distance = 0
-    prev_start_height = start_height
-    
     for _ in range(num_bounces):
-        distance = prev_start_height - start_height
+        start_height, distance = start_height * 2, (start_height + first_bounce_height) * 2
         total_distance += distance
-        prev_start_height = start_height
-        start_height *= 2
+        first_bounce_height = start_height
     
     return str(int(total_distance))
