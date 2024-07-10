052bcfdbf5d5430ddf8f37ca594448a237f7f72a
beep boop <githubbot@vadim.me>
2024-07-10 06:54:27 +0200

    Solution 4 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/xe1y8h7l Prompt:       |                                          ^~~~~~ b37d4018-27fa-442e-82c8-fe247c16df31.cpp:33:42: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from b37d4018-27fa-442e-82c8-fe247c16df31.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from b37d4018-27fa-442e-82c8-fe247c16df31.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b37d4018-27fa-442e-82c8-fe247c16df31.cpp:33:49: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ b37d4018-27fa-442e-82c8-fe247c16df31.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ b37d4018-27fa-442e-82c8-fe247c16df31.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main(){       |     ^~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -2,9 +2,7 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b) {
-    return a == b;
-}
+using namespace std;
 
 std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
     std::vector<std::vector<int>> result;
@@ -24,7 +22,11 @@ std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
     return result;
 }
 
+bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b) {
+    return a == b;
+}
+
 int main(){
-    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
+    assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));
     return 0;
 }
