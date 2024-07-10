d7e10ddc153ec297eedab48755fb2a2226908715
beep boop <githubbot@vadim.me>
2024-07-10 07:17:14 +0200

    Solution 83 of CPP/70, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tn1r4gui Prompt:       |                    ^~~ 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp:16: 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp: In function ‘int main()’: 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp:26:20: error: ‘strange_sort_list’ was n ot declared in this scope    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 8b6977a2-cd73-4135-9a72-f2fc3fe78fbf.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,22 +1,32 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 
-vector<int> strange_sort_vector(vector<int> lst) {
+bool issame(int a, int b) {
+    if (a % 2 == 0 && b % 2 == 0)
+        return true;
+    else if (a % 2 != 0 && b % 2 != 0)
+        return true;
+    else
+        return false;
+}
+
+vector<int> strange_sort_list(vector<int> lst) {
     vector<int> result;
     while (!lst.empty()) {
         int min_val = *min_element(lst.begin(), lst.end());
         result.push_back(min_val);
-        lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
-        
+        lst.erase(std::remove(lst.begin(), lst.end(), min_val), lst.end());
         if (!lst.empty()) {
             int max_val = *max_element(lst.begin(), lst.end());
-            result.push_back(max_val);
-            lst.erase(remove(lst.begin(), lst.end(), max_val), lst.end());
+            vector<int> temp;
+            for (int i : lst) {
+                if (issame(i, max_val))
+                    temp.push_back(i);
+            }
+            result.insert(result.end(), temp.rbegin(), temp.rend());
+            lst.erase(std::remove(lst.begin(), lst.end(), max_val), lst.end());
         }
     }
-    
     return result;
 }
