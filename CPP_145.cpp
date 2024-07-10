1bc9b449219c67e76ea7ca8dd207e44ce7cc71a4
beep boop <githubbot@vadim.me>
2024-07-10 20:10:52 +0200

    Solution 10 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/jv7e3pcr Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7800feb5-7316-4fa5-9227-47edb8e02c70.cpp:32:34: error: expected primary-expressi on before ‘int’    32 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7800feb5-7316-4fa5-9227-47edb8e02c70.cpp:32:39: error: expression list treated a s compound expression in initializer [-fpermissive]    32 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7800feb5-7316-4fa5-9227-47edb8e02c70.cpp:31: 7800feb5-7316-4fa5-9227-47edb8e02c70.cpp: In function ‘int main()’: 7800feb5-7316-4fa5-9227-47edb8e02c70.cpp:41:20: error: ‘order_by_points’ was not  declared in this scope; did you mean ‘orderPoints’?    41 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 7800feb5-7316-4fa5-9227-47edb8e02c70.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -29,4 +29,6 @@ std::vector<int> orderPoints(std::vector<int> nums) {
     return result;
 }
 
-assert(sameVectors(orderPoints({0,6,6,-76,-21,23,4}), {-76, -21, 0, 4, 23, 6, 6}));
+int main() {
+    assert(sameVectors(orderPoints({0,6,6,-76,-21,23,4}), {-76, -21, 0, 4, 23, 6, 6}));
+}
