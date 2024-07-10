412c8653120302105af441cec00d970945501782
beep boop <githubbot@vadim.me>
2024-07-10 03:45:04 +0200

    Solution 12 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/09anxcah Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,8 +1,3 @@
 def find_zero(xs: list):
-    if len(xs) % 2 != 0:
-        raise ValueError("List xs must have an even number of coefficients")
-
-    max_non_zero_coeff = max([abs(coeff) for coeff in xs if coeff != 0])
-    sign = -1 if xs[0] < 0 else 1
-
-    return max_non_zero_coeff / xs[0] * sign
+    x = -xs[0] / xs[1]
+    return x
