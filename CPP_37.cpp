5d6f467936344b788be4064e4b4ca99849b8f8fe
beep boop <githubbot@vadim.me>
2024-07-10 06:40:00 +0200

    Solution 75 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lerjqzl3 Prompt:    21 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 604a1eed-c580-4342-bfdb-ad8152512ee5.cpp:21:36: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 604a1eed-c580-4342-bfdb-ad8152512ee5.cpp:21:43: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 604a1eed-c580-4342-bfdb-ad8152512ee5.cpp:20: 604a1eed-c580-4342-bfdb-ad8152512ee5.cpp: In function ‘int main()’: 604a1eed-c580-4342-bfdb-ad8152512ee5.cpp:30:20: error: ‘sort_even’ was not decla red in this scope    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 604a1eed-c580-4342-bfdb-ad8152512ee5.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,13 +1,3 @@
-bool issame(vector<float> a,vector<float>b){
-    if(a.size() != b.size())
-        return false;
-    for(int i = 0; i < a.size(); i++){
-        if(abs(a[i] - b[i]) > 1e-6) 
-            return false;
-    }
-    return true;
-}
-
 vector<float> sort_even(vector<float> l) {
     vector<float> result(l.size());
     for (int i = 0; i < l.size(); i++) {
@@ -25,4 +15,13 @@ vector<float> sort_even(vector<float> l) {
         }
     }
     return result;
+}
+bool issame(vector<float> a,vector<float>b){
+    if(a.size() != b.size())
+        return false;
+    for(int i = 0; i < a.size(); i++){
+        if(abs(a[i] - b[i]) > 1e-6) 
+            return false;
+    }
+    return true;
 }
