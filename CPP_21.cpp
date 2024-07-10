01edf13d2f5d1f1aea4adbdb8734bbbabfbdae39
beep boop <githubbot@vadim.me>
2024-07-10 21:23:19 +0200

    Solution 4 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/gajkdsd6 Prompt:    22 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 32065bf7-388c-4bee-9659-e44a30395547.cpp:22:36: error: expected primary-expressi on before ‘float’    22 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 32065bf7-388c-4bee-9659-e44a30395547.cpp:22:43: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 32065bf7-388c-4bee-9659-e44a30395547.cpp:21: 32065bf7-388c-4bee-9659-e44a30395547.cpp: In function ‘int main()’: 32065bf7-388c-4bee-9659-e44a30395547.cpp:31:20: error: ‘rescale_to_unit’ was not  declared in this scope    31 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 32065bf7-388c-4bee-9659-e44a30395547.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,11 +1,36 @@
-float min_num = *min_element(numbers.begin(), numbers.end());
-    float max_num = *max_element(numbers.begin(), numbers.end());
-    
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<float> rescale_to_unit(vector<float> numbers) {
+    float min_num = numbers[0];
+    float max_num = numbers[0];
+    for (float num : numbers) {
+        if (num < min_num) {
+            min_num = num;
+        }
+        if (num > max_num) {
+            max_num = num;
+        }
+    }
+
     vector<float> rescaled_numbers;
     for (float num : numbers) {
         float rescaled_num = (num - min_num) / (max_num - min_num);
         rescaled_numbers.push_back(rescaled_num);
     }
-    
+
     return rescaled_numbers;
+}
+
+int main() {
+    vector<float> input_numbers = {12.5, 21.3, 15.7, 8.2, 10.9};
+    vector<float> rescaled_output = rescale_to_unit(input_numbers);
+    
+    for (float num : rescaled_output) {
+        cout << num << " ";
+    }
+    
+    return 0;
 }
