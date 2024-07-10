3ada5d1e3bc5324e55e02eb0af6e487c95e8cd17
beep boop <githubbot@vadim.me>
2024-07-10 09:00:46 +0200

    Solution 70 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d9fwc7cu Prompt: Complete the following code given the task description and function signature.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,6 +1,9 @@
 vector<int> filter_integers(list_any values) {
     vector<int> result;
     for (const auto& value : values) {
-        boost::any_cast<int>(value), 0;
+        if (any_cast<int>(value)) {
+            result.push_back(any_cast<int>(value));
+        }
     }
+    return result;
 }
