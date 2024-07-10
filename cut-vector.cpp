a0d2d1cc458fb1bbeeb98a8b41add0deacaf0a34
beep boop <githubbot@vadim.me>
2024-07-11 00:52:45 +0200

    Solution 45 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/om8f24n2 Prompt:  this scope    20 |         cout << nums[i] << endl;       |         ^~~~ 7c2e7dcb-1c4c-46cc-b844-723aea4240fb.cpp:20:17: error: ‘nums’ was not declared i n this scope    20 |         cout << nums[i] << endl;       |                 ^~~~ 7c2e7dcb-1c4c-46cc-b844-723aea4240fb.cpp:20:28: error: ‘endl’ was not declared i n this scope    20 |         cout << nums[i] << endl;       |                            ^~~~ 7c2e7dcb-1c4c-46cc-b844-723aea4240fb.cpp:22:5: error: ‘cout’ was not declared in  this scope    22 |     cout << endl;       |     ^~~~ 7c2e7dcb-1c4c-46cc-b844-723aea4240fb.cpp:22:13: error: ‘endl’ was not declared i n this scope    22 |     cout << endl;       |             ^~~~ 7c2e7dcb-1c4c-46cc-b844-723aea4240fb.cpp:24:17: error: ‘nums’ was not declared i n this scope    24 |         cout << nums[i] << endl;       |                 ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,35 +1,34 @@
 #include <iostream>
 #include <vector>
-#include <numeric>
-#include <climits>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> nums[i];
-    }
-    
-    int min_diff = INT_MAX;
-    int cut_idx = -1;
-    for (int i = 1; i < n; ++i) {
-        int diff = abs(accumulate(nums.begin(), nums.begin() + i, 0) - accumulate(nums.begin() + i, nums.end(), 0));
-        if (diff < min_diff) {
-            min_diff = diff;
-            cut_idx = i;
-        }
-    }
-    
-    for (int i = 0; i < cut_idx; ++i) {
-        cout << nums[i] << endl;
-    }
-    cout << endl;
-    for (int i = cut_idx; i < n; ++i) {
-        cout << nums[i] << endl;
-    }
-    
-    return 0;
+int n;
+cin >> n;
+vector<int> nums(n);
+for (int i = 0; i < n; ++i) {
+cin >> nums[i];
+}
+
+int min_diff = INT_MAX;
+int cut_idx = -1;
+for (int i = 1; i < n; ++i) {
+int diff = abs(accumulate(nums.begin(), nums.begin() + i, 0) - accumulate(nums.begin() + i, nums.end(), 0));
+if (diff < min_diff) {
+min_diff = diff;
+cut_idx = i;
+}
+}
+
+for (int i = 0; i < cut_idx; ++i) {
+cout << nums[i] << endl;
+}
+cout << endl;
+for (int i = cut_idx; i < n; ++i) {
+cout << nums[i] << endl;
+}
+
+return 0;
 }
