923e58e3a499cbb6486bb4311350787b25461b0e
beep boop <githubbot@vadim.me>
2024-07-10 12:26:21 +0200

    Solution 30 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/s88sugx2 Prompt:       |                    ^~~ 6e4ddc59-bab3-4441-814d-4a464aa43833.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6e4ddc59-bab3-4441-814d-4a464aa43833.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6e4ddc59-bab3-4441-814d-4a464aa43833.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6e4ddc59-bab3-4441-814d-4a464aa43833.cpp:21: 6e4ddc59-bab3-4441-814d-4a464aa43833.cpp: In function ‘int main()’: 6e4ddc59-bab3-4441-814d-4a464aa43833.cpp:31:20: error: ‘parse_nested_parens’ was  not declared in this scope    31 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ 6e4ddc59-bab3-4441-814d-4a464aa43833.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,31 +1,18 @@
 #include <vector>
-#include <cassert>
+#include <string>
+#include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
-std::vector<int> parse_nested_parens(std::string paren_string){
-    std::vector<int> result;
+vector<int> parse_nested_parens(string paren_string) {
+    vector<int> result;
     int max_depth = 0;
     int current_depth = 0;
-    for(char c : paren_string){
-        if(c == '('){
+    for (char c : paren_string) {
+        if (c == '(') {
             current_depth++;
-            max_depth = std::max(max_depth, current_depth);
-        }
-        else if(c == ')'){
+            max_depth = max(max_depth, current_depth);
+        } else if (c == ')') {
             current_depth--;
-        }
-        else if(c == ' '){
+        } else if (c == ' ') {
             result.push_back(max_depth);
             max_depth = 0;
             current_depth = 0;
@@ -33,9 +20,4 @@ std::vector<int> parse_nested_parens(std::string paren_string){
     }
     result.push_back(max_depth);
     return result;
-}
-
-int main(){
-    assert(issame(parse_nested_parens("(()(())((())))"), std::vector<int>{4}));
-    return 0;
 }
