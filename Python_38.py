a11be23d646fea4693664e42f496a5e86b9d2389
beep boop <githubbot@vadim.me>
2024-07-10 20:07:35 +0200

    Solution 40 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/disnge4r Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c12665af-e0e6-40a9-9c42-8309e5b81 ca8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_38.py
+++ Python_38.py
@@ -1,13 +1,8 @@
 def encode_cyclic(s: str):
-    if len(s) < 3:
-        raise ValueError("Input string should be at least 3 characters")
     groups = []
     for i in range(0, len(s), 3):
-        group = s[i : (i + 3)] if i + 3 <= len(s) else s[i:]
-        if len(group) == 3:
-            groups.append(group[1] + group[0])
-        else:
-            groups.append(group)
+        group = s[i : i + 3] if i + 3 <= len(s) else s[i:]
+        groups.append(group[1:] + group[0] if len(group) == 3 else group)
     return "".join(groups).lstrip("x")
 
 
