da9f088cdb4ebc5fc4d34db6d944599da3f17502
beep boop <githubbot@vadim.me>
2024-07-10 19:44:31 +0200

    Solution 42 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uquygx2d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e47a046b-0514-4a6f-ba04-664a73fdd feb.py", line 18, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e47a046b-0514-4a6f-ba04-664a73fdd feb.py", line 13, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,10 +1,12 @@
-```
+from itertools import zip_longest
+
+
 def sort_even(l: list):
     even = sorted([x for x in l if x % 2 == 0])
     odd = sorted([x for x in l if x % 2 != 0])
-    
-    max_len = max(len(even), len(odd))
-    even += [0] * (max_len - len(even)) 
-    odd += [0] * (max_len - len(odd)) 
 
-    return tuple(i for i in zip(even, odd))
+    max_len = max(len(even), len(odd))
+    even += [0] * (max_len - len(even))
+    odd += [0] * (max_len - len(odd))
+
+    return tuple(i[0] if i[0] is not None else i[1] for i in zip_longest(even, odd))
