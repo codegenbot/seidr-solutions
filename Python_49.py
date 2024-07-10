e2a84423bf1494c2409a6ccf89a240de4c585818
beep boop <githubbot@vadim.me>
2024-07-10 21:28:18 +0200

    Solution 31 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ujpaxwlx Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d1dc809-7300-431b-8830-2ac3d5c7c 4a4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,11 +1,2 @@
-```
 def modp(n: int, p: int):
-    if n < 0:
-        n += p
-    elif p == 1:
-        return 0
-    elif p % 2 == 0:
-        half = modp(n, p // 2)
-        return (n * half) % p
-    else:
-        return (n % p)
+    return pow(n, p, p)
