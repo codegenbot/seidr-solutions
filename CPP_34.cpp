a1da40e6cef524e4bd02808dd7db0e3ebbcaea1f
beep boop <githubbot@vadim.me>
2024-07-10 04:31:52 +0200

    Solution 97 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/l1klcm5p Prompt:     3 | int main() {       |     ^~~~ In file included from eb29b8b1-88be-4646-9f6d-c528cdbe8e2c.cpp:7: eb29b8b1-88be-4646-9f6d-c528cdbe8e2c.cpp: In function ‘int main()’: eb29b8b1-88be-4646-9f6d-c528cdbe8e2c.cpp:17:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    17 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from eb29b8b1-88be-4646-9f6d-c528cdbe8e2c.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ In file included from eb29b8b1-88be-4646-9f6d-c528cdbe8e2c.cpp:7: eb29b8b1-88be-4646-9f6d-c528cdbe8e2c.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,1 +1,17 @@
 #include <algorithm>
+#include <cassert>
+#include <vector>
+
+bool removeDuplicates(std::vector<int>& nums) {
+    nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
+    return nums;
+}
+
+int main() {
+    std::vector<int> nums = {5, 3, 5, 2, 3, 3, 9, 0, 123};
+    std::vector<int> expected_result = {0, 2, 3, 5, 9, 123};
+    
+    removeDuplicates(nums);
+    
+    assert(std::equal(nums.begin(), nums.end(), expected_result.begin(), expected_result.end()));
+}
