145b63263219a35aa2bb3b1ba83b3a6c65054078
beep boop <githubbot@vadim.me>
2024-07-10 22:43:31 +0200

    Solution 14 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vlwyz493 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f386a5f6-f82d-49ee-bf66-f2183284b aec.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,15 +1,7 @@
-def fruit_distribution():
-    s = input("Enter the string: ")
-    n = int(input("Enter the number of fruits to consider: "))
+def fruit_distribution(s, n):
     s = s.lower()
     fruits = s.split("and")
     for i in range(len(fruits)):
         if "," in fruits[i]:
             fruits[i] = fruits[i].replace(",", "")
-    return (
-        sum(
-            int(fruit.split()[0]) if fruit.split()[0].isdigit() else 1
-            for fruit in fruits[:n]
-        )
-        / n
-    )
+    return sum(int(fruit.split()[0]) if fruit.split()[0].isdigit() else 1 for fruit in fruits[:n]) / n
