f3e86a083a8c97b2b60c976a08b790ad0488ddf7
beep boop <githubbot@vadim.me>
2024-07-10 20:57:03 +0200

    Solution 2 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3mr73mpr Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,21 +1,41 @@
-#include <boost/any_cast.hpp>
+#include <boost/any.hpp>
+#include <string>
+
+using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == b.type()) {
-        if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
-            return a;
-        } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
-            return b;
-        } else {
-            return "None";
-        }
-    }
-
-    if (boost::any_cast<double>(a) > boost::any_cast<double>(b)) {
-        return a;
-    } else if (boost::any_cast<double>(a) < boost::any_cast<double>(b)) {
+    if (a.type() == typeid(int) && b.type() == typeid(double)) {
         return b;
-    } else {
-        return "None";
+    }
+    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return b;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = boost::any_cast<string>(a);
+        string str2 = boost::any_cast<string>(b);
+
+        if (str1 == "None" || str2 == "None")
+            return a;
+
+        double num1 = stod(str1);
+        double num2 = stod(str2);
+
+        if (num1 > num2)
+            return a;
+        else if (num1 < num2)
+            return b;
+        else
+            return boost::any("None");
+    }
+    else {
+        double num1 = boost::any_cast<double>(a);
+        double num2 = boost::any_cast<double>(b);
+
+        if (num1 > num2)
+            return a;
+        else if (num1 < num2)
+            return b;
+        else
+            return boost::any("None");
     }
 }
