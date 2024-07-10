eeacaae79e6db96f00264a43faf8532860d16add
beep boop <githubbot@vadim.me>
2024-07-10 08:07:32 +0200

    Solution 4 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8t1bc2ba Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,21 +1,21 @@
-#include<string>
+#include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = stoi(getNumerator(x));
-    int b = stoi(getDenominator(x));
-    int c = stoi(getNumerator(n));
-    int d = stoi(getDenominator(n));
+    int a = stoi(strtok((x.substr(1)).c_str(), "/"));
+    int b = stoi(x.substr(0, 1));
+    int c = stoi(strtok((n.substr(1)).c_str(), "/"));
+    int d = stoi(n.substr(0, 1));
 
-    return (a*d == b*c);
-}
+    long long numerator = (long long)a * d;
+    long long denominator = (long long)b * c;
 
-string getNumerator(string s) {
-    size_t pos = s.find('/');
-    return s.substr(0, pos);
-}
+    if (numerator % denominator != 0) {
+        return false;
+    }
 
-string getDenominator(string s) {
-    size_t pos = s.find('/');
-    return s.substr(pos+1);
+    return true;
 }
