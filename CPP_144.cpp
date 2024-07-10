b560d4801215b1f6b24bf0e746a572c937c118d8
beep boop <githubbot@vadim.me>
2024-07-10 08:07:41 +0200

    Solution 5 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8t1bc2ba Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,21 +1,35 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-
+#include<string>
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = stoi(strtok((x.substr(1)).c_str(), "/"));
-    int b = stoi(x.substr(0, 1));
-    int c = stoi(strtok((n.substr(1)).c_str(), "/"));
-    int d = stoi(n.substr(0, 1));
+    int a = 0, b = 0, c = 0, d = 0;
+    string s1, s2;
 
-    long long numerator = (long long)a * d;
-    long long denominator = (long long)b * c;
-
-    if (numerator % denominator != 0) {
-        return false;
+    for (int i = 0; i < x.length(); i++) {
+        if (x[i] == '/') break;
+        if (isdigit(x[i])) a = a * 10 + (x[i] - '0');
     }
+    for (int i = x.length() - 1; i >= 0; i--) {
+        if (x[i] == '/') break;
+        s1 += x[i];
+    }
+    reverse(s1.begin(), s1.end());
 
-    return true;
+    for (int i = 0; i < n.length(); i++) {
+        if (n[i] == '/') break;
+        if (isdigit(n[i])) c = c * 10 + (n[i] - '0');
+    }
+    for (int i = n.length() - 1; i >= 0; i--) {
+        if (n[i] == '/') break;
+        s2 += n[i];
+    }
+    reverse(s2.begin(), s2.end());
+
+    int g = __gcd(a, c);
+    return a / g == b && c / g == d;
+}
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
 }
