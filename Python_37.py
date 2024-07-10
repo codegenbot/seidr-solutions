53787167ee96bcdd3a9d92e5aee4039d28881c12
beep boop <githubbot@vadim.me>
2024-07-10 21:14:10 +0200

    Solution 80 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n709d7eu Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2aa30cd5-b7ce-418f-80b6-d9c126105 37d.py", line 25, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2aa30cd5-b7ce-418f-80b6-d9c126105 37d.py", line 20, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -6,6 +6,7 @@ def sort_even(l: list):
     result = []
     even_index = 0
     odd_index = 0
+    
     while even_index < len(evens) and odd_index < len(odds):
         if even_index < len(evens): 
             result.append(evens[even_index])
@@ -15,8 +16,5 @@ def sort_even(l: list):
             odd_index += 1
 
     result.extend(evens[even_index:])
-    while odd_index < len(odds):
-        result.append(odds[odd_index])
-        odd_index += 1
     
     return tuple(result)
