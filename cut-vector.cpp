a9c9697d8b4e25203e0bf50f3e17dadea2430df1
beep boop <githubbot@vadim.me>
2024-07-10 08:26:46 +0200

    Solution 55 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/jtwmj2dd Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -3,122 +3,22 @@ using namespace std;
 
 pair<vector<int>, vector<int>> cutVector(vector<int> v) {
     int min_diff = INT_MAX;
-    pair<int, int> split_index;
+    int cut_index = 0;
+    
     for (int i = 1; i < v.size(); i++) {
-        int left_sum = 0, right_sum = 0;
-        for (int j = 0; j < i; j++) {
-            left_sum += v[j];
+        int sum_left = accumulate(v.begin(), v.begin() + i, 0);
+        int sum_right = accumulate(v.begin() + i, v.end(), 0);
+        
+        if (sum_left == sum_right) {
+            return {vector<int>(v.begin(), v.begin() + i), vector<int>(v.begin() + i, v.end())};
+        } else {
+            int diff = abs(sum_left - sum_right);
+            if (diff < min_diff) {
+                min_diff = diff;
+                cut_index = i;
+            }
         }
-        for (int j = i; j < v.size(); j++) {
-            right_sum += v[j];
-        }
-        if (left_sum == right_sum) {
-            return {{v.begin(), v.begin() + i}, {v.begin() + i, v.end()}};
-        } else if (abs(left_sum - right_sum) < min_diff) {
-            min_diff = abs(left_sum - right_sum);
-            split_index = {i, i};
-        }
     }
-    int index = split_index.first;
-    return {{v.begin(), v.begin() + index}, {v.begin() + index, v.end()}};
-}
-
-int main() {
-    // Test cases
-    vector<int> input1 = {1};
-    pair<vector<int>, vector<int>> output1 = cutVector(input1);
-    cout << "Input: ";
-    for (int num : input1) {
-        cout << num << " ";
-    }
-    cout << endl;
-    cout << "Output:" << endl;
-    cout << "Left subvector: ";
-    for (int num : output1.first) {
-        cout << num << " ";
-    }
-    cout << endl;
-    cout << "Right subvector: ";
-    for (int num : output1.second) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    vector<int> input2 = {1, 10};
-    pair<vector<int>, vector<int>> output2 = cutVector(input2);
-    cout << "Input: ";
-    for (int num : input2) {
-        cout << num << " ";
-    }
-    cout << endl;
-    cout << "Output:" << endl;
-    cout << "Left subvector: ";
-    for (int num : output2.first) {
-        cout << num << " ";
-    }
-    cout << endl;
-    cout << "Right subvector: ";
-    for (int num : output2.second) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    vector<int> input3 = {1, 100};
-    pair<vector<int>, vector<int>> output3 = cutVector(input3);
-    cout << "Input: ";
-    for (int num : input3) {
-        cout << num << " ";
-    }
-    cout << endl;
-    cout << "Output:" << endl;
-    cout << "Left subvector: ";
-    for (int num : output3.first) {
-        cout << num << " ";
-    }
-    cout << endl;
-    cout << "Right subvector: ";
-    for (int num : output3.second) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    vector<int> input4 = {1, 1000};
-    pair<vector<int>, vector<int>> output4 = cutVector(input4);
-    cout << "Input: ";
-    for (int num : input4) {
-        cout << num << " ";
-    }
-    cout << endl;
-    cout << "Output:" << endl;
-    cout << "Left subvector: ";
-    for (int num : output4.first) {
-        cout << num << " ";
-    }
-    cout << endl;
-    cout << "Right subvector: ";
-    for (int num : output4.second) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    vector<int> input5 = {1, 10000};
-    pair<vector<int>, vector<int>> output5 = cutVector(input5);
-    cout << "Input: ";
-    for (int num : input5) {
-        cout << num << " ";
-    }
-    cout << endl;
-    cout << "Output:" << endl;
-    cout << "Left subvector: ";
-    for (int num : output5.first) {
-        cout << num << " ";
-    }
-    cout << endl;
-    cout << "Right subvector: ";
-    for (int num : output5.second) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    return 0;
+    
+    return {vector<int>(v.begin(), v.begin() + cut_index), vector<int>(v.begin() + cut_index, v.end())};
 }
