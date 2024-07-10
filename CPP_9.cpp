41abb01d142a1329469fab14a1c87c2b566d6f72
beep boop <githubbot@vadim.me>
2024-07-10 04:09:34 +0200

    Solution 73 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/785e81cu Prompt:    34 | int main(){       |     ^~~~ c38c2e77-5fea-40f8-b7b0-f6d3d6a2c4f7.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from c38c2e77-5fea-40f8-b7b0-f6d3d6a2c4f7.cpp:25: c38c2e77-5fea-40f8-b7b0-f6d3d6a2c4f7.cpp: In function ‘int main()’: c38c2e77-5fea-40f8-b7b0-f6d3d6a2c4f7.cpp:35:20: error: ‘rolling_max’ was not dec lared in this scope; did you mean ‘cpp9::rolling_max’?    35 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ c38c2e77-5fea-40f8-b7b0-f6d3d6a2c4f7.cpp:11:22: note: ‘cpp9::rolling_max’ declar ed here    11 |     std::vector<int> rolling_max(std::vector<int> input) {       |                      ^~~~~~~~~~~ In file included from c38c2e77-5fea-40f8-b7b0-f6d3d6a2c4f7.cpp:25: c38c2e77-5fea-40f8-b7b0-f6d3d6a2c4f7.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -18,7 +18,6 @@ namespace cpp9 {
 }
 
 int main() {
-    using namespace cpp9;
-    assert(issame(rolling_max({3, 2, 3, 100, 3}), {3, 3, 3, 100, 100}));
+    assert(cpp9::issame(cpp9::rolling_max({3, 2, 3, 100, 3}), {3, 3, 3, 100, 100}));
     return 0;
 }
