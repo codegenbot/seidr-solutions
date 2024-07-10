c711d8f76014d7981f58476d8c83302cda295d98
beep boop <githubbot@vadim.me>
2024-07-10 13:34:24 +0200

    Solution 77 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/vb5h98q2 Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8c663de2-e028-497d-9b37-353d309a10bc.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8c663de2-e028-497d-9b37-353d309a10bc.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8c663de2-e028-497d-9b37-353d309a10bc.cpp:11: 8c663de2-e028-497d-9b37-353d309a10bc.cpp: In function ‘int main()’: 8c663de2-e028-497d-9b37-353d309a10bc.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 8c663de2-e028-497d-9b37-353d309a10bc.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,30 +1,13 @@
-#include <iostream>
-#include <list>
-#include <vector>
-#include <boost/any.hpp>
-
-bool issame(const boost::any& value, const std::type_info& type) {
+bool issame(const boost::any& value, const std::type_info& type){
     return value.type() == type;
 }
 
-std::vector<int> filter_integers(std::list<boost::any> values);
+vector<int> filter_integers(const std::list<boost::any>& values);
 
-int main() {
-    std::list<boost::any> values = {10, 20, 3.14, "hello", 30};
-
-    std::vector<int> result = filter_integers(values);
-
-    for (const auto& num : result) {
-        std::cout << num << " ";
-    }
-
-    return 0;
-}
-
-std::vector<int> filter_integers(std::list<boost::any> values) {
-    std::vector<int> result;
-    for (const auto& val : values) {
-        if (issame(val, typeid(int))) {
+vector<int> filter_integers(const std::list<boost::any>& values){
+    vector<int> result;
+    for(const auto& val : values){
+        if(issame(val, typeid(int))){
             result.push_back(boost::any_cast<int>(val));
         }
     }
