1166b223e9ea03519f8eb9d8614907fe2659db44
beep boop <githubbot@vadim.me>
2024-07-10 23:00:33 +0200

    Solution 64 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7ee48065-382b-434a-9eda-182ce633f 601.py", line 11, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7ee48065-382b-434a-9eda-182ce633f 601.py", line 6, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,3 +1,4 @@
 def sort_third(l):
-    int_list = sorted([i for sublist in l for i in sublist if isinstance(i, int)])
-    return [sublist[1] for sublist in [[sublist, len(sorted(sublist))] for sublist in l if isinstance(sublist, list)]]
+    int_list = sorted([i for i in l if isinstance(i, int)])
+    lst_list = sorted([[sublist, len(sublist)] for sublist in [sublist for sublist in l if isinstance(sublist, list)]], key=lambda x: x[1])
+    return int_list + [sublist[1] for sublist in lst_list]
