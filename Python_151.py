98da671721684c5900a8bb521d23fa32ce5730ae
beep boop <githubbot@vadim.me>
2024-07-11 00:16:33 +0200

    Solution 12 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/49frezuz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f2586f7a-9aa8-4706-84f4-51370992d f86.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -2,8 +2,8 @@
 def double_the_difference(lst):
     return sum(i ** 2 for i in lst) - 2 * sum(lst)
 
-def check(lst1, func, lst2=None):
-    result = func(lst1)
+def check(lst, func):
+    result = func(lst)
     print(f"The difference between the sum of squares and twice the sum is {result}.")
 
 check([1, 2, 3], double_the_difference)
