88a1747ef0a1fb613114a728dc14c6b44e944470
beep boop <githubbot@vadim.me>
2024-07-10 19:00:16 +0200

    Solution 24 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hwoml10f Prompt:       |                              ^~~~~~ 1b70be05-1960-4f59-8469-8e89420eb270.cpp:24:30: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? 1b70be05-1960-4f59-8469-8e89420eb270.cpp:24:37: error: ‘string’ was not declared  in this scope    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 1b70be05-1960-4f59-8469-8e89420eb270.cpp:24:37: note: ‘std::string’ is defined i n header ‘<string>’; did you forget to ‘#include <string>’? 1b70be05-1960-4f59-8469-8e89420eb270.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 1b70be05-1960-4f59-8469-8e89420eb270.cpp:23: 1b70be05-1960-4f59-8469-8e89420eb270.cpp: In function ‘int main()’: 1b70be05-1960-4f59-8469-8e89420eb270.cpp:33:20: error: ‘all_prefixes’ was not de clared in this scope    33 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ 1b70be05-1960-4f59-8469-8e89420eb270.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,14 +1,22 @@
-#include <string>
+#include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
+    if(a.size() != b.size()) {
         return false;
     }
     for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
+        bool same = true;
+        for(int j = 0; j < a[i].size(); j++) {
+            if(a[i][j] != b[i][j]) {
+                same = false;
+                break;
+            }
+        }
+        if(!same) {
             return false;
         }
     }
@@ -21,4 +29,28 @@ vector<string> all_prefixes(string str) {
         result.push_back(str.substr(0, i));
     }
     return result;
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    
+    vector<string> prefixes = all_prefixes(str);
+    bool isSame = true;
+    
+    for(int i = 1; i <= str.length(); ++i) {
+        if(!issame(prefixes.begin(), prefixes.end(), prefixes.begin() + i - 1, prefixes.begin())) {
+            isSame = false;
+            break;
+        }
+    }
+    
+    if(isSame) {
+        cout << "The string and its prefixes are the same." << endl;
+    } else {
+        cout << "The string and its prefixes are not the same." << endl;
+    }
+    
+    return 0;
 }
