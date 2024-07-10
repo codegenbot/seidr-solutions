72700e71b215cdae2c235f1fa0f55a88bb688bc3
beep boop <githubbot@vadim.me>
2024-07-11 00:55:47 +0200

    Solution 95 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/99vwaciq Prompt:       |                              ^~~~~~ 900f67fe-897d-4fc9-a11b-55414884716e.cpp:4:37: error: ‘string’ was not declared in this scope     4 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 900f67fe-897d-4fc9-a11b-55414884716e.cpp:4:44: error: ‘b’ was not declared in th is scope     4 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 900f67fe-897d-4fc9-a11b-55414884716e.cpp:4:45: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 900f67fe-897d-4fc9-a11b-55414884716e.cpp:3: 900f67fe-897d-4fc9-a11b-55414884716e.cpp: In function ‘int main()’: 900f67fe-897d-4fc9-a11b-55414884716e.cpp:13:20: error: ‘reverse_delete’ was not declared in this scope    13 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 900f67fe-897d-4fc9-a11b-55414884716e.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,24 +1,21 @@
+```cpp
 #include <vector>
 #include <string>
 
 std::vector<std::pair<std::string,bool>> reverse_delete(std::string s1, std::string s2) {
     std::vector<std::pair<std::string,bool>> result;
-    
-    for(int i = s1.length() - 1; i >= 0; i--) {
-        if(s2.find(s1.substr(i, 1)) != std::string::npos) {
-            result.push_back({s1.substr(i, 1), true});
+    for(int i = s1.length() - 1; i >= 0 ; i--) {
+        if(s2.find(s1.substr(i,1)) != std::string::npos) {
+            result.push_back(std::make_pair(s1.substr(i,1), true));
             s1.erase(i, 1);
+        } else {
+            result.push_back(std::make_pair(s1.substr(i,1), false));
         }
     }
-
-    for(std::string str : s1) {
-        result.push_back({str, false});
-    }
-
     return result;
 }
 
 int main() {
-    assert((reverse_delete("mamma", "mia")) == (std::vector<std::pair<std::string,bool>>{{"ma",true}, {"a",false}}));
+    assert((reverse_delete("mamma", "mia")) == (std::vector<std::pair<std::string,bool>>{{"ma",true}}));
     return 0;
 }
