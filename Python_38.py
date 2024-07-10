f9b2f5dc3dcbf5e7e7e1621e0fba245c91cc0e81
beep boop <githubbot@vadim.me>
2024-07-10 10:02:07 +0200

    Solution 12 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/iiz9z3iq Prompt: Complete the following code given the task description and function signature.

--- Python_38.py
+++ Python_38.py
@@ -3,9 +3,9 @@ def decode_cyclic(s: str):
     temp = ""
     for char in s:
         temp += char
-        if len(temp) % 3 == 0:
-            result.append(temp[1:] + temp[0])
+        if len(temp) == 3:
+            result.append(temp[1] + temp[0] + temp[2])
             temp = ""
     if temp:
-        result.append(temp[1:] + temp[0])
+        result.append(temp)
     return "".join(result)
