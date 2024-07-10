48e8d7112f275546867d302c059c013c7b55eb08
beep boop <githubbot@vadim.me>
2024-07-10 23:36:48 +0200

    Solution 72 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rmdf06ap Prompt:       |                                 ^~~~ a946d119-4205-49f9-a7bd-6a1af322a3cf.cpp:34:38: error: expected primary-expressi on before ‘int’    34 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ a946d119-4205-49f9-a7bd-6a1af322a3cf.cpp:34:44: error: expression list treated a s compound expression in initializer [-fpermissive]    34 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ a946d119-4205-49f9-a7bd-6a1af322a3cf.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ a946d119-4205-49f9-a7bd-6a1af322a3cf.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from a946d119-4205-49f9-a7bd-6a1af322a3cf.cpp:33: a946d119-4205-49f9-a7bd-6a1af322a3cf.cpp: In function ‘int main()’: a946d119-4205-49f9-a7bd-6a1af322a3cf.cpp:48:19: error: ‘issame’ cannot be used a s a function    48 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,7 +1,6 @@
-```c++
+```cpp
 #include <map>
 #include <string>
-#include <assert.h>
 
 std::map<char, int> histogram(std::string s) {
     std::map<char, int> freqMap;
@@ -15,18 +14,15 @@ std::map<char, int> histogram(std::string s) {
     return freqMap;
 }
 
-bool issameMap(std::map<char, int> a, std::map<char, int> b) {
+bool isSameMap(std::map<char, int> a, std::map<char, int> b) {
     if (a.size() != b.size())
         return false;
 
     for (auto it = a.begin(); it != a.end();++it)
-        if (it->second != b.at(it->first))
+        if (it->second != b.find(it->first)->second)
             return false;
 
     return true;
 }
 
-int main() {
-    assert(issameMap(histogram("aa"), histogram("a")));
-    return 0;
-}
+assert(isSameMap(histogram("aa"), histogram("a")));
