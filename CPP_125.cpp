546bb676124262f2f83acdba67f8cb0cb47e030d
beep boop <githubbot@vadim.me>
2024-07-10 19:36:38 +0200

    Solution 49 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/bvk9zac5 Prompt:       |                              ^~~~~~ 0771cf2a-9c37-404c-8055-9ba6c94f96d3.cpp:29:37: error: ‘string’ was not declared  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0771cf2a-9c37-404c-8055-9ba6c94f96d3.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0771cf2a-9c37-404c-8055-9ba6c94f96d3.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0771cf2a-9c37-404c-8055-9ba6c94f96d3.cpp:28: 0771cf2a-9c37-404c-8055-9ba6c94f96d3.cpp: In function ‘int main()’: 0771cf2a-9c37-404c-8055-9ba6c94f96d3.cpp:38:20: error: ‘split_words’ was not dec lared in this scope    38 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 0771cf2a-9c37-404c-8055-9ba6c94f96d3.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -4,32 +4,6 @@
 
 bool issame(vector<string> a, vector<string> b);
 
-vector<string> words;
-string word = "";
-for (char c : txt) {
-    if (c == ' ' || c == ',') {
-        if (!word.empty()) {
-            words.push_back(word);
-            word = "";
-        }
-    } else {
-        word += c;
-    }
-}
-if (!word.empty()) {
-    words.push_back(word);
-}
-if (words.empty()) {
-    int count = 0;
-    for (char c : txt) {
-        if (islower(c) && (c - 'a') % 2 == 1) {
-            count++;
-        }
-    }
-    words.push_back(to_string(count));
-}
-return words;
-
 bool issame(vector<string> a, vector<string> b) {
     // Add your implementation here
 }
