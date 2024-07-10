23df6c2bbeb8a265e7733503f5ede9c1c2f005fa
beep boop <githubbot@vadim.me>
2024-07-10 05:47:23 +0200

    Solution 50 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/rhv60fcv Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ 638af075-97bc-435f-897b-a24baaf68fad.cpp:14:23:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <iostream>
 #include <vector>
 
@@ -8,8 +9,7 @@ int main() {
     std::cin >> n;
     for (int i = 0; i < n; i++) {
         std::string num;
-        std::cout << "Enter element: ";
-        getline(std::cin, num);
+        std::getline(std::cin, num);
         nums.push_back(num);
     }
     std::cout << "Number of special numbers: " << specialFilter(nums) << std::endl;
@@ -18,7 +18,7 @@ int main() {
 int specialFilter(const std::vector<std::string>& nums) {
     int count = 0;
     for (const auto& num : nums) {
-        if (num.length() > 1 && num.length() <= 10 && (num[9] - '0') % 2 != 0 && ((num[8] - '0') / 10) % 10 % 2 != 0) {
+        if (num.length() > 10 && (num[9] - '0') % 2 != 0 && ((num[8] - '0') / 10) % 10 % 2 != 0) {
             count++;
         }
     }
