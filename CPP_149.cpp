f56c3fb32c96b0abee4bb83d3f7ef302eab1f989
beep boop <githubbot@vadim.me>
2024-07-10 20:48:54 +0200

    Solution 36 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ptpr8f4c Prompt:    20 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 49fc1504-2be1-4f61-9b36-32f037046339.cpp:20:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 49fc1504-2be1-4f61-9b36-32f037046339.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 49fc1504-2be1-4f61-9b36-32f037046339.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 49fc1504-2be1-4f61-9b36-32f037046339.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 49fc1504-2be1-4f61-9b36-32f037046339.cpp:19: 49fc1504-2be1-4f61-9b36-32f037046339.cpp: In function ‘int main()’: 49fc1504-2be1-4f61-9b36-32f037046339.cpp:29:20: error: ‘sorted_list_sum’ was not  declared in this scope; did you mean ‘sortListSum’?    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,12 +1,8 @@
 ```cpp
-#include <algorithm>
-#include <string>
-#include <vector>
-
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] + b[i] != sortListSum({{a[i], b[i]}})[0]) return false;
+bool issame(vector<string> v1, vector<string> v2) {
+    if (v1.size() != v2.size()) return false;
+    for (int i = 0; i < v1.size(); i++) {
+        if (v1[i] + v2[i] != sortListSum({{v1[i], v2[i]}})[0]) return false;
     }
     return true;
 }
