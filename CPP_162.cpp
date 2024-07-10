671e18376ab24a87178dbe690be97f99ecac6a02
beep boop <githubbot@vadim.me>
2024-07-10 05:35:03 +0200

    Solution 10 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/svifujg2 Prompt:       |     ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from f8590a60-8e0c-4541-8eca-1c5d387fa89a.cpp:3: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ f8590a60-8e0c-4541-8eca-1c5d387fa89a.cpp:19:14: warning: ‘int MD5_Final(unsigned  char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]    19 |     MD5_Final(md5, &ctx);       |     ~~~~~~~~~^~~~~~~~~~~ In file included from f8590a60-8e0c-4541-8eca-1c5d387fa89a.cpp:3: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ f8590a60-8e0c-4541-8eca-1c5d387fa89a.cpp: At global scope: f8590a60-8e0c-4541-8eca-1c5d387fa89a.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ f8590a60-8e0c-4541-8eca-1c5d387fa89a.cpp:7:5: note: ‘int main()’ previously defi ned here     7 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,20 +1,22 @@
 #include <string>
 #include <openssl/ssl.h>
-#include <openssl/evp.h>
+#include <openssl/md5.h>
 
 using namespace std;
 
 string string_to_md5(string text) {
-    unsigned char md[16];
-    EVP_MD_CTX ctx;
-    EVP_DigestInit(&ctx, EVP_md5());
-    EVP_DigestUpdate(&ctx, (const unsigned char*)text.c_str(), text.size());
-    EVP_DigestFinal(&ctx, md, nullptr);
+    if (text.empty()) return "";
+
+    unsigned char md5[16];
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+    MD5_Update(ctx, text.c_str(), text.size());
+    MD5_Final(md5, &ctx);
 
     string result;
     for (int i = 0; i < 16; ++i) {
         char buffer[3];
-        sprintf(buffer, "%02x", md[i]);
+        sprintf(buffer, "%02x", md5[i]);
         result += string(buffer);
     }
 
