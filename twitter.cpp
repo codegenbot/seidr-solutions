bb3be8c69c4d809d84cbb947dc22369c24dec34d
beep boop <githubbot@vadim.me>
2024-07-10 07:03:22 +0200

    Solution 91 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/tiok8txr Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: 5cbc1ba7-a752-478d-9e99-d448e2c5dbaf.cpp:18:26:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -4,22 +4,21 @@
 
 std::string validateTweet(const std::string& tweet) {
     if (tweet.empty()) return "You didn't type anything";
-    if (tweet.size() > 140) return "Too many characters";
-    return "Your tweet has " + std::to_string(tweet.size()) + " characters";
+    if (tweet.length() > 140) return "Too many characters";
+    return std::to_string(tweet.length()) + " characters";
 }
 
 int main() {
-    using namespace std;
     while (true) { 
-        cout << "Enter a tweet: ";
-        string line;
-        getline(cin, line);
-        string output = validateTweet(line); 
+        std::cout << "Enter a tweet: ";
+        std::string line;
+        getline(std::cin, line);
+        std::string output = validateTweet(line); 
         if(output == "You didn't type anything") {
-            cout << output << endl;
+            std::cout << output << std::endl;
             return 0; 
         }
-        cout << output << endl;
+        std::cout << output << std::endl;
     }   
     return 0;
 }
