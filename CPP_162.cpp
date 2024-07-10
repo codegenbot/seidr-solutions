ca0f9619444e36777d4f43dea0c01a7965b5d094
beep boop <githubbot@vadim.me>
2024-07-10 06:01:07 +0200

    Solution 27 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/svifujg2 Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: ad939158-2ba0-4e84-a086-8250b1b23c1a.cpp:28:29:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,20 +1,30 @@
+```cpp
 #include <iostream>
 #include <string>
-#include <openssl/md5.h>
 
-std::string myString_to_md5(std::string text) {
+std::string myString_to_md5(const char *text) {
     unsigned char result[16];
-    MD5((const unsigned char*)text.c_str(), text.size(), result);
+    EVP_MD5_CTX mdctx;
+    EVP_MD5_Init(&mdctx);
+    EVP_MD5_Update(&mdctx, (const unsigned char*)text, strlen(text));
+    unsigned char mdValue[EVP_MAX_MD_SIZE];
+    int mdSize = sizeof(mdValue);
+    EVP_MD5_Final(&mdctx, mdValue, &mdSize);
     std::string output;
     for (int i = 0; i < 16; i++) {
         char temp[3];
-        sprintf(temp, "%02x", result[i]);
+        sprintf(temp, "%02x", mdValue[i]);
         output += temp;
     }
     return output;
 }
 
 int main() {
-    std::cout << myString_to_md5("Your_input_string"); 
+    std::cout << "Enter a string: ";
+    std::string input;
+    std::cin >> input;
+    const char *input_str = input.c_str();
+    std::string output = myString_to_md5(input_str);
+    std::cout << "MD5: " << output << std::endl;
     return 0;
 }
