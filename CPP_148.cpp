ecb3764fd19de759fb35c947decbe0aafa740483
beep boop <githubbot@vadim.me>
2024-07-10 11:32:35 +0200

    Solution 51 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/twp8lzfu Prompt:   731 |         __copy_move_b(const _Tp* __first, const _Tp* __last, _Tp* __resu lt)       |         ^~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_algobase.h:731:9: note:   template argument deducti on/substitution failed: /usr/include/c++/11/bits/stl_algobase.h:760:65: note:   deduced conflicting type s for parameter ‘_Tp’ (‘char’ and ‘const char’)   758 |     return std::__copy_move_backward<_IsMove,       |            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   759 |                                      __memcpyable<_BI2, _BI1>::__value,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   760 |                                      _Category>::__copy_move_b(__first,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~   761 |                                                                __last,       |                                                                ~~~~~~~   762 |                                                                __result) ;       |                                                                ~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,19 +1,1 @@
-#include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
-#include <iterator>
-
-using namespace std;
-
-bool areSame(const vector<string>& a, const vector<string>& b){
-    return a == b;
-}
-
-vector<string> bf(const string& s1, const string& s2) {
-    vector<string> common_chars;
-    sort(s1.begin(), s1.end());
-    sort(s2.begin(), s2.end());
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), back_inserter(common_chars));
-    return common_chars;
-}
