ee198813c3acab2990e20c3586b3960e8328fa26
beep boop <githubbot@vadim.me>
2024-07-10 08:06:31 +0200

    Solution 59 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j9zzr72u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3f55e9cb-9156-4e81-84b3-215848792 973.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,25 +1,31 @@
-```Python
 def main():
     while True:
         try:
-            lst = (input("Enter words (separated by space): ") 
-                   .lower() 
-                   .replace(" ", "") 
-                   .split())
+            lst = (
+                input("Enter words (separated by space): ")
+                .lower()
+                .replace(" ", "")
+                .split()
+            )
             break
         except Exception as e:
             print(f"Error: {str(e)}")
             continue
 
     if len(lst) > 0:
-        sum_all_words = sum(len(word) for word in lst)
         while True:
             try:
-                input_val = int(input("Press Enter to calculate the sum or type 'q' to quit: "))
-                if input_val == 113: 
+                even_word_sum = sum(len(word) for word in lst if len(word) % 2 == 0)
+                input_val = int(
+                    input("Press Enter to calculate the sum or type 'q' to quit: ")
+                )
+                if input_val == 113:
                     break
-                print(sum_all_words)
+                print(even_word_sum)  # directly use even_word_sum here
             except ValueError:
-                print("Invalid Input. Please enter a number or 'q'.")
+                print("Invalid input. Please enter a number or 'q'.")
+            except Exception as e:
+                print(f"Error: {str(e)}")
+                continue
     else:
         print("Please enter at least one word.")
