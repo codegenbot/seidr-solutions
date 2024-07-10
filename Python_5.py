644d3fe3b6c1f2e225b608468cad4af163883744
beep boop <githubbot@vadim.me>
2024-07-10 22:56:02 +0200

    Solution 92 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pc4qhgax Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/24d50d25-9188-4d6f-8988-b2555f1e1 d34.py", line 12, in <module>     check(intersperse)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/24d50d25-9188-4d6f-8988-b2555f1e1 d34.py", line 9, in check     assert intersperse([2, 2, 2], 2) == [2, 2, 2, 2, 2]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_5.py
+++ Python_5.py
@@ -1,7 +1,8 @@
 def intersperse(a, b):
     if not a:
         return [b]
-    result = [b] + a
-    for i in range(len(result) - 1):
-        result.insert(i+1, b)
-    return [b] + [result[-1]]
+    result = [a[0]]
+    for item in a[1:]:
+        result.extend([item, b])
+    result.append(b)
+    return result
