ea774cb7778b06084ead02a20c01bb2012582d05
beep boop <githubbot@vadim.me>
2024-07-10 19:38:50 +0200

    Solution 17 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/h0my8hka Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f147e51c-f25f-41a3-8c7a-ee468799c bc7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_131.py
+++ Python_131.py
@@ -3,7 +3,7 @@ def digits(n):
     product = 1
     while n:
         digit = n % 10
-        if digit % 2 == 1:
+        if digit % 2 == 0:
             product *= digit
         n //= 10
     return product
