e16289dfd27d6e68ab8b2f0af626e1fd27a4a67e
beep boop <githubbot@vadim.me>
2024-07-10 17:47:56 +0200

    Solution 52 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/dodal3gc Prompt:    27 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4fd7784e-4ee1-4765-99e1-b5175f130561.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4fd7784e-4ee1-4765-99e1-b5175f130561.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4fd7784e-4ee1-4765-99e1-b5175f130561.cpp:26: 4fd7784e-4ee1-4765-99e1-b5175f130561.cpp: In function ‘int main()’: 4fd7784e-4ee1-4765-99e1-b5175f130561.cpp:36:20: error: ‘count_up_to’ was not dec lared in this scope    36 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 4fd7784e-4ee1-4765-99e1-b5175f130561.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,17 +1,14 @@
-#include <iostream>
-#include <vector>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-std::vector<int> count_up_to(int n) {
-    std::vector<int> result;
+vector<int> count_up_to(int n) {
+    vector<int> result;
     if (n < 2) {
         return result;
     }
     
-    std::vector<bool> isPrime(n, true);
+    vector<bool> isPrime(n, true);
     isPrime[0] = isPrime[1] = false;
     
     for (int i = 2; i * i < n; ++i) {
@@ -29,17 +26,4 @@ std::vector<int> count_up_to(int n) {
     }
     
     return result;
-}
-
-int main() {
-    int n;
-    std::cin >> n;
-    
-    std::vector<int> result = count_up_to(n);
-    
-    for (int prime : result) {
-        std::cout << prime << " ";
-    }
-    
-    return 0;
 }
