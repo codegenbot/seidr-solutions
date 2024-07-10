bc9d2fd09ff6e61fe5a8cba3b25c0747f8818ef0
beep boop <githubbot@vadim.me>
2024-07-11 01:30:14 +0200

    Solution 45 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/49frezuz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0a3040e3-d895-4b46-9f54-688aa6455 99d.py", line 5, in <module>     print(check([1, 2, 3], lambda x: double_the_difference(x)))           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0a3040e3-d895-4b46-9f54-688aa6455 99d.py", line 2, in check     return func(lst)            ^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0a3040e3-d895-4b46-9f54-688aa6455 99d.py", line 5, in <lambda>     print(check([1, 2, 3], lambda x: double_the_difference(x)))                                      ^^^^^^^^^^^^^^^^^^^^^ NameError: name 'double_the_difference' is not defined

--- Python_151.py
+++ Python_151.py
@@ -1,9 +1,11 @@
 ```
-def double_the_difference(nums):
-    avg = sum(nums) / len(nums)
-    return abs(sum(num - avg for num in nums))
+def double_the_difference(lst):
+    lst.sort()
+    return (lst[-1] - lst[0]) * 2
+
 
 def check(lst, func):
     return func(lst)
+
 
 print(check([1, 2, 3], lambda x: double_the_difference(x)))
