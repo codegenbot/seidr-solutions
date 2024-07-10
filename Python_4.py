d4e0fad94f3b9b37ff51909f48c9fa2d5ecfbf05
beep boop <githubbot@vadim.me>
2024-07-10 18:43:35 +0200

    Solution 16 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eqvqvyvk Prompt: Program did not receive expected input

--- Python_4.py
+++ Python_4.py
@@ -1,12 +1,11 @@
 ```
 from typing import List
 
-
 def mean_absolute_deviation(numbers: List[float]) -> float:
     from statistics import mean as stat_mean
     from statistics import stdev as stat_stdev
 
     m = stat_mean(numbers)
-    mad = sum(abs(num - m) for num in numbers) / len(numbers)
-
-    return mad
+    deviations = [abs(num - m) for num in numbers]
+    
+    return sum(deviations) / len(deviations)
