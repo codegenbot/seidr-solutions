ab66b749584275431092b5df4cc5a1c4d9686a00
beep boop <githubbot@vadim.me>
2024-07-10 08:16:38 +0200

    Solution 12 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/r0rakuau Prompt:    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5de1abe2-f04f-46ad-ba6e-57d17536a71c.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5de1abe2-f04f-46ad-ba6e-57d17536a71c.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 5de1abe2-f04f-46ad-ba6e-57d17536a71c.cpp:23: 5de1abe2-f04f-46ad-ba6e-57d17536a71c.cpp: In function ‘int main()’: 5de1abe2-f04f-46ad-ba6e-57d17536a71c.cpp:33:20: error: ‘sorted_list_sum’ was not  declared in this scope    33 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 5de1abe2-f04f-46ad-ba6e-57d17536a71c.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,3 +1,14 @@
+#include <vector>
+#include <algorithm>
+
+bool issame(const std::vector<std::string>& v1, const std::vector<std::string>& v2) {
+    if (v1.size() != v2.size()) return false;
+    for (int i = 0; i < v1.size(); ++i) {
+        if (v1[i] != v2[i]) return false;
+    }
+    return true;
+}
+
 vector<string> sorted_list_sum(vector<string> lst) {
     auto it = lst.begin();
     erase_remove:
@@ -19,8 +30,8 @@ vector<string> sorted_list_sum(vector<string> lst) {
 }
 
 int main() {
-    if (!issame(vector<string>(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"})), vector<string>({"cc","dd","aaaa","bbbb"}))) {
-        return 1;
-    }
-    return 0;
+    if (!issame(vector<string>(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"})), vector<string>({"cc","dd","aaaa","bbbb"}))) { 
+        return 1; 
+    } 
+    return 0; 
 }
