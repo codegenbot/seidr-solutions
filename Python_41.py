9d6782c576b74a4b6abb13bca02677a81eb3c818
beep boop <githubbot@vadim.me>
2024-07-10 19:34:35 +0200

    Solution 9 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bpxrryax Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/91b94087-af4c-4047-b881-6e1cf6ea1 12a.py", line 7, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/91b94087-af4c-4047-b881-6e1cf6ea1 12a.py", line 4, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_41.py
+++ Python_41.py
@@ -1,8 +1,7 @@
-```
 def car_race_collision():
     n = int(input("Enter a positive integer: "))
     total = 0
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         if i % 2 != 0:
-            total += i ** 2
+            total += i**2
     return total
