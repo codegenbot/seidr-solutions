b0503cbd1dabbaa502d6024f1f3ec03d04b8f991
beep boop <githubbot@vadim.me>
2024-07-10 08:57:08 +0200

    Solution 90 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/i4ztvcn8 Prompt: bc102b3c-1545-4e7a-bf0d-ec81027b12d8.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ bc102b3c-1545-4e7a-bf0d-ec81027b12d8.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from bc102b3c-1545-4e7a-bf0d-ec81027b12d8.cpp:31: bc102b3c-1545-4e7a-bf0d-ec81027b12d8.cpp: In function ‘int main()’: bc102b3c-1545-4e7a-bf0d-ec81027b12d8.cpp:41:20: error: ‘even_odd_palindrome’ was  not declared in this scope; did you mean ‘contest::even_odd_palindrome’?    41 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ bc102b3c-1545-4e7a-bf0d-ec81027b12d8.cpp:9:22: note: ‘contest::even_odd_palindro me’ declared here     9 |     std::vector<int> even_odd_palindrome(int n) {       |                      ^~~~~~~~~~~~~~~~~~~ In file included from bc102b3c-1545-4e7a-bf0d-ec81027b12d8.cpp:31: bc102b3c-1545-4e7a-bf0d-ec81027b12d8.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -9,12 +9,12 @@ namespace contest {
     std::vector<int> even_odd_palindrome(int n) {
         std::vector<int> result;
         
-        for (int i = 1; i <= n; ++i) {
-            result.push_back(i);
+        for (int i = 0; i < n; ++i) {
+            result.push_back(i + 1);
         }
         
-        for (int i = n - 1; i >= 1; --i) {
-            result.push_back(i);
+        for (int i = n - 2; i >= 0; --i) {
+            result.push_back(i + 1);
         }
         
         return result;
@@ -22,8 +22,8 @@ namespace contest {
 }
 
 int main() {
-    assert(contest::issame(contest::even_odd_palindrome(1), {1, 1}));
-    assert(contest::issame(contest::even_odd_palindrome(3), {1, 2, 3, 2, 1}));
+    assert(contest::issame(contest::even_odd_palindrome(1), { 1, 1 }));
+    assert(contest::issame(contest::even_odd_palindrome(3), { 1, 2, 3, 2, 1 }));
     
     return 0;
 }
