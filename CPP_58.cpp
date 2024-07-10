16948e298c837aaec9f57c59543c77aed0e38ad9
beep boop <githubbot@vadim.me>
2024-07-10 19:00:50 +0200

    Solution 10 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6mp2wjk0 Prompt: In file included from /usr/include/c++/11/vector:67,                  from 4d7405fe-2278-4dc2-8866-72525694b17c.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 4d7405fe-2278-4dc2-8866-72525694b17c.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4d7405fe-2278-4dc2-8866-72525694b17c.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 4d7405fe-2278-4dc2-8866-72525694b17c.cpp:8: 4d7405fe-2278-4dc2-8866-72525694b17c.cpp: In function ‘int main()’: 4d7405fe-2278-4dc2-8866-72525694b17c.cpp:18:20: error: ‘common’ was not declared  in this scope    18 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 4d7405fe-2278-4dc2-8866-72525694b17c.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,24 +1,6 @@
 #include <vector>
-#include <set>
 #include <algorithm>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) 
-        return false;
-    for(int i=0; i<a.size(); i++) {
-        if(a[i] != b[i])
-            return false;
-    }
-    return true;
-}
-
-std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
-    std::set<int> s1(l1.begin(), l1.end());
-    std::set<int> s2(l2.begin(), l2.end());
-
-    std::set<int> intersection;
-    std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-        inserter(intersection, intersection.begin()));
-
-    return std::vector<int>(intersection.begin(), intersection.end());
+    return std::equal(a.begin(), a.end(), b.begin());
 }
