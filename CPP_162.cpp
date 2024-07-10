afe99013642a1bc45fe48a1c61c6f35231c1a008
beep boop <githubbot@vadim.me>
2024-07-10 14:15:09 +0200

    Solution 83 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/n3y415cj Prompt:       |                          ^~~~       |                          std::hash In file included from /usr/include/c++/11/string_view:42,                  from /usr/include/c++/11/bits/basic_string.h:48,                  from /usr/include/c++/11/string:55,                  from 8a573a94-4ff7-48ef-a8c7-28a7c9d9e79a.cpp:3: /usr/include/c++/11/bits/functional_hash.h:59:12: note: ‘std::hash’ declared her e    59 |     struct hash;       |            ^~~~ 8a573a94-4ff7-48ef-a8c7-28a7c9d9e79a.cpp:34:48: error: ‘hash’ was not declared i n this scope; did you mean ‘std::hash’?    34 |         ss << std::setw(2) << static_cast<int>(hash[i]);       |                                                ^~~~       |                                                std::hash In file included from /usr/include/c++/11/string_view:42,                  from /usr/include/c++/11/bits/basic_string.h:48,                  from /usr/include/c++/11/string:55,                  from 8a573a94-4ff7-48ef-a8c7-28a7c9d9e79a.cpp:3: /usr/include/c++/11/bits/functional_hash.h:59:12: note: ‘std::hash’ declared her e    59 |     struct hash;       |            ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,1 +1,38 @@
-OPENSSL_init_crypto(OPENSSL_INIT_ADD_ALL_CIPHERS, nullptr);
+#include <openssl/evp.h>
+#include <openssl/crypto.h>
+#include <string>
+#include <iostream>
+#include <cassert>
+#include <iomanip>
+#include <sstream>
+
+std::string string_to_md5(const std::string& input) {
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_create();
+    const EVP_MD *md = EVP_md5();
+    unsigned char md5_hash[MD5_DIGEST_LENGTH];
+    unsigned int hash_len;
+
+    OpenSSL_add_all_algorithms();
+
+    if (!EVP_DigestInit_ex(mdctx, md, NULL) ||
+        !EVP_DigestUpdate(mdctx, input.c_str(), input.size()) ||
+        !EVP_DigestFinal_ex(mdctx, md5_hash, &hash_len)) {
+        EVP_MD_CTX_free(mdctx);
+        return ""; // Handle error
+    }
+
+    EVP_MD_CTX_free(mdctx);
+
+    std::string md5_hash_str;
+    for (unsigned int i = 0; i < hash_len; i++) {
+        md5_hash_str += char(md5_hash[i]);
+    }
+
+    std::stringstream ss;
+    ss << std::hex << std::setfill('0');
+    for (unsigned int i = 0; i < hash_len; i++) {
+        ss << std::setw(2) << static_cast<int>(md5_hash[i]);
+    }
+
+    return ss.str();
+}
