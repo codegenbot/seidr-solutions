2e34de2ec4a70b6509da506489725e4914b80a8a
beep boop <githubbot@vadim.me>
2024-07-10 21:52:24 +0200

    Solution 60 of CPP/161, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/edbifdt7 Prompt: In file included from 46585d60-5bb1-43a2-a12a-68d67d6bc72c.cpp:13: 46585d60-5bb1-43a2-a12a-68d67d6bc72c.cpp: In function ‘int main()’: 46585d60-5bb1-43a2-a12a-68d67d6bc72c.cpp:15:27: error: ISO C++ forbids compariso n between pointer and integer [-fpermissive]    15 |     assert (solve("#ccc") == "#CCC");       |             ~~~~~~~~~~~~~~^~~~~~~~~

--- CPP_161.cpp
+++ CPP_161.cpp
@@ -1,8 +1,6 @@
 int solve(std::string s) {
-    for (int i = 0; i < s.length(); i++) {
-        if (std::isupper(s[i])) { 
-            s[i] = static_cast<char>(std::tolower(std::uint8_t(s[i])));
-        }
+    for (char &c : s) {
+        c = std::tolower(c);
     }
     return s.length();
 }
