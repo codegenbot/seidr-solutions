c29cd5b2b468524742e36759b77376436d043d20
beep boop <githubbot@vadim.me>
2024-07-10 21:05:07 +0200

    Solution 22 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/v3l4k5fc Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 44569544-2430-4887-a426-a8882b598b12.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 44569544-2430-4887-a426-a8882b598b12.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 44569544-2430-4887-a426-a8882b598b12.cpp:26: 44569544-2430-4887-a426-a8882b598b12.cpp: In function ‘int main()’: 44569544-2430-4887-a426-a8882b598b12.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 44569544-2430-4887-a426-a8882b598b12.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,16 +1,28 @@
 bool issame(vector<string> a, vector<string> b) {
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 vector<string> separate_paren_groups(string paren_string);
 
 int main() {
     string input;
+    cout << "Enter a string with parentheses: ";
     cin >> input;
+
     vector<string> result = separate_paren_groups(input);
-    for (const string& group : result) {
+
+    for (const auto &group : result) {
         cout << group << endl;
     }
+
     return 0;
 }
 
