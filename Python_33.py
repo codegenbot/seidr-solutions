fa4cf14c45e4a04305a8001172bdb99a657d6a87
beep boop <githubbot@vadim.me>
2024-07-10 23:31:16 +0200

    Solution 81 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a0d9e244-ce4f-4fe9-bb6f-d997b6c3b f20.py", line 15, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a0d9e244-ce4f-4fe9-bb6f-d997b6c3b f20.py", line 10, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,9 +1,8 @@
-```
 def sort_third(lst):
     numbers = [x for x in lst if not isinstance(x, list)]
-    long_lists = [x for x in lst if isinstance(x, list) and len(x) > 3]
+    lists = [sorted(x) for x in lst if isinstance(x, list) and len(x) >= 3]
     
     numbers.sort()
-    long_lists.sort(key=lambda x: x[2] if len(x) > 2 else float('inf'))
+    lists.sort(key=lambda x: x[2] if len(x) > 2 else float('inf'))
     
-    return numbers + long_lists
+    return numbers + lists
