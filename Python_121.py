0742c76964622e417740c5ddc2a9de5b3727749a
beep boop <githubbot@vadim.me>
2024-07-10 16:54:49 +0200

    Solution 22 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/7atr5wdy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/64f29117-d39b-4db8-afca-3177d0858 40e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,2 +1,7 @@
-def solution(lst):
-    return sum(i for i, _ in enumerate(lst) if i % 2 != 0)
+```
+def highest_rank(lst):
+    from collections import Counter
+    counter = Counter(lst)
+    max_value = max(counter.values())
+    max_keys = [key for key, value in counter.items() if value == max_value]
+    return -1 if len(max_keys) > 1 else max_keys[0]
