2167f364a7a0c0eacb4d504a8cf7a66bc7432f53
beep boop <githubbot@vadim.me>
2024-07-10 10:38:56 +0200

    Solution 16 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/0x1g0oqm Prompt:    20 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4d931069-ddb0-408f-aa6a-182448ab4601.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4d931069-ddb0-408f-aa6a-182448ab4601.cpp:20:45: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4d931069-ddb0-408f-aa6a-182448ab4601.cpp:19: 4d931069-ddb0-408f-aa6a-182448ab4601.cpp: In function ‘int main()’: 4d931069-ddb0-408f-aa6a-182448ab4601.cpp:29:20: error: ‘sorted_list_sum’ was not  declared in this scope    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 4d931069-ddb0-408f-aa6a-182448ab4601.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,10 +1,9 @@
 bool issame(vector<string> a, vector<string> b) {
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    sort(a.begin(), a.end(), [](const string& x, const string& y) {
+        return make_pair(x.length() % 2, x) < make_pair(y.length() % 2, y);
+    });
+    sort(b.begin(), b.end(), [](const string& x, const string& y) {
+        return make_pair(x.length() % 2, x) < make_pair(y.length() % 2, y);
+    });
+    return a == b;
 }
