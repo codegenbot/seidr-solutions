a1f89c01021e4793d6ff9b993acd1b64eadfd50d
beep boop <githubbot@vadim.me>
2024-07-10 04:07:28 +0200

    Solution 19 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/xre89lws Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 30e110ed-cec2-4d4b-9165-f6f5dae7ef4b.cpp:32:23:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -21,4 +21,21 @@ int count_nums(std::vector<int> nums) {
 
 }
 
-std::cout << "Count of positive sums is: " << count_nums(nums) << std::endl;
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> nums;
+    while (n--) {
+        int num; 
+        if (!(std::cin >> num)) {  // Check for input failure
+            break; 
+        }
+        nums.push_back(num);
+    }
+    if (nums.empty()) {  // If no numbers were read, there's nothing to count
+        std::cout << "No numbers were read." << std::endl;
+        return 0;
+    }
+    std::cout << "Count of positive sums is: " << count_nums(nums) << std::endl;
+    return 0;
+}
