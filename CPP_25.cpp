f41d9130dad88b71eb8532c9b789e4c01656ac63
beep boop <githubbot@vadim.me>
2024-07-10 13:50:58 +0200

    Solution 42 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/4bxiw8kp Prompt:       |                    ^~~ 30531d3d-2339-4251-8794-0560ccd562dc.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 30531d3d-2339-4251-8794-0560ccd562dc.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 30531d3d-2339-4251-8794-0560ccd562dc.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 30531d3d-2339-4251-8794-0560ccd562dc.cpp:15: 30531d3d-2339-4251-8794-0560ccd562dc.cpp: In function ‘int main()’: 30531d3d-2339-4251-8794-0560ccd562dc.cpp:25:20: error: ‘factorize’ was not decla red in this scope    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ 30531d3d-2339-4251-8794-0560ccd562dc.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,18 +1,6 @@
 #include <vector>
 #include <cassert>
 
-bool issame(vector<int> a, vector<int> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 vector<int> factorize(int n){
     vector<int> factors;
     for (int i = 2; i * i <= n; i++) {
@@ -25,6 +13,18 @@ vector<int> factorize(int n){
         factors.push_back(n);
     }
     return factors;
+}
+
+bool issame(vector<int> a, vector<int> b){
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main() {
