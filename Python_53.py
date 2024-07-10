5845efc1ffc69fc0ad9a1336ac7b559abd52223f
beep boop <githubbot@vadim.me>
2024-07-10 16:56:24 +0200

    Solution 15 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/y3v4v2tq Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -1,4 +1,4 @@
-def add(x=None, y=None): 
-    x = int(input("Enter first number: ") if x is None else x) 
+def add(x=None, y=None):
+    x = int(input("Enter first number: ") if x is None else x)
     y = int(input("Enter second number: ") if y is None else y)
     return x + y
