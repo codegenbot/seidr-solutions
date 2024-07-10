456c126e934dfa778244812b6282f3fdb0358d58
beep boop <githubbot@vadim.me>
2024-07-10 22:36:45 +0200

    Solution 2 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/f6bcnret Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9d19809d-4f97-434c-8d72-e79232d02bd5.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9d19809d-4f97-434c-8d72-e79232d02bd5.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9d19809d-4f97-434c-8d72-e79232d02bd5.cpp:16: 9d19809d-4f97-434c-8d72-e79232d02bd5.cpp: In function ‘int main()’: 9d19809d-4f97-434c-8d72-e79232d02bd5.cpp:26:20: error: ‘largest_smallest_integer s’ was not declared in this scope    26 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 9d19809d-4f97-434c-8d72-e79232d02bd5.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,12 +1,20 @@
+```cpp
+bool issame(vector<int> a,vector<int>b){
+    return a==b;
+}
+
 vector<int> largest_smallest_integers(vector<int> lst){
-    int max_negative = 0, min_positive = INT_MAX;
-    for(int num : lst){
-        if(num < 0 && num > max_negative){
-            max_negative = num;
-        }
-        else if(num > 0 && num < min_positive){
-            min_positive = num;
+    int max_negative = 0;
+    int min_positive = INT_MAX;
+
+    for(int i : lst) {
+        if(i < 0 && i > max_negative) {
+            max_negative = i;
+        } else if(i > 0 && i < min_positive) {
+            min_positive = i;
         }
     }
-    return {(max_negative != 0)?max_negative:0, (min_positive != INT_MAX)?min_positive:0};
+
+    return {std::max({(max_negative > 0 ? INT_MAX : max_negative), 
+                      (min_positive < 1 ? INT_MIN : min_positive)},0);
 }
