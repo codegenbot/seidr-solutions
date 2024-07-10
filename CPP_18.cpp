51616500dec5bf25057769d3e16d4f2a7f18d8cb
beep boop <githubbot@vadim.me>
2024-07-10 20:41:52 +0200

    Solution 30 of CPP/18, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/71jt3mt4 Prompt: e046d5f3-12f7-40ac-8316-ea63ff08bccf.cpp:1:20: error: ‘string’ was not declared in this scope     1 | int how_many_times(string str, string substring) {       |                    ^~~~~~ e046d5f3-12f7-40ac-8316-ea63ff08bccf.cpp:1:32: error: ‘string’ was not declared in this scope     1 | int how_many_times(string str, string substring) {       |                                ^~~~~~ e046d5f3-12f7-40ac-8316-ea63ff08bccf.cpp:1:48: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int how_many_times(string str, string substring) {       |                                                ^ In file included from e046d5f3-12f7-40ac-8316-ea63ff08bccf.cpp:11: e046d5f3-12f7-40ac-8316-ea63ff08bccf.cpp: In function ‘int main()’: e046d5f3-12f7-40ac-8316-ea63ff08bccf.cpp:13:27: error: ‘how_many_times’ cannot b e used as a function    13 |     assert (how_many_times("john doe", "john") == 1);       |             ~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -1,11 +1,10 @@
-```cpp
+#include <string>
+
 int how_many_times(string str, string substring) {
     int count = 0;
     size_t pos = 0;
-    string temp = str;
-    while ((pos = temp.find(substring)) != string::npos) {
-        ++count;
-        temp.erase(pos, substring.length());
+    while ((pos = str.find(substring)) != string::npos) {
+        count++;
+        str.erase(0, pos + substring.size());
     }
     return count;
-}
