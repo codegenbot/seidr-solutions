57f07ff2ad8d86140b01b22af817c5570d588808
beep boop <githubbot@vadim.me>
2024-07-10 14:54:45 +0200

    Solution 97 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fq8lg3pf Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,8 +1,12 @@
+import math
+
+
+def poly(xs: list, x: float):
+    return sum([coeff * math.pow(x, i) for i, coeff in enumerate(xs)])
+
+
 def find_zero(xs: list):
-    n = len(xs) - 1
-    a = xs[0]
-    b = xs[-1]
-    if n == 2:
-        return -a / b
-    else:
-        return b / a
+    assert len(xs) % 2 == 0
+    max_coeff = max(xs, key=abs)
+    x = -max_coeff
+    return x
