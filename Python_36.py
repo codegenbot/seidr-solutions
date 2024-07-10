2f7a73a1e9ab29d22cfadbb22e644759bdd529b9
beep boop <githubbot@vadim.me>
2024-07-10 19:52:15 +0200

    Solution 20 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ie2x67wo Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/214a8a9a-b031-4d6c-8582-9734b8f20 885.py", line 17, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/214a8a9a-b031-4d6c-8582-9734b8f20 885.py", line 14, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,7 +1,12 @@
-```
 def fizz_buzz(n: int):
-    total = 0
+    result = []
     for i in range(1, n+1):
         if i % 3 == 0 and i % 5 == 0:
-            total += 1
-    return str(total)
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz" if i % 5 != 0 else "FizzBuzz")
+        elif i % 5 == 0:
+            result.append("Buzz" if i % 3 != 0 else "FizzBuzz")
+        else:
+            result.append(str(i))
+    return "\n".join(map(str, result))
