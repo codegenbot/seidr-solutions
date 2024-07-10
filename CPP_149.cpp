0585a531b5f9b075f360e2262df7f6ec96a540c9
beep boop <githubbot@vadim.me>
2024-07-10 10:55:45 +0200

    Solution 27 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/0x1g0oqm Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from eb4ce203-79d1-48f8-8409-60b3eda91967.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ eb4ce203-79d1-48f8-8409-60b3eda91967.cpp:23:44: error: ‘b’ was not declared in t his scope    23 | bool issame(vector<string> a,vector<string>b){       |                                            ^ eb4ce203-79d1-48f8-8409-60b3eda91967.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ eb4ce203-79d1-48f8-8409-60b3eda91967.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -13,8 +13,3 @@ std::vector<std::string> sorted_list_sum(std::vector<std::string> input) {
     std::sort(input.begin(), input.end());
     return input;
 }
-
-int main() {
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
-    return 0;
-}
