154ea0dc6ad27955ecf4fffa6c6046c51a84e884
beep boop <githubbot@vadim.me>
2024-07-10 21:03:12 +0200

    Solution 19 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f3f2fb7d-f847-4ac8-84bb-2e9fbe229 240.py", line 22, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f3f2fb7d-f847-4ac8-84bb-2e9fbe229 240.py", line 17, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,23 +1,10 @@
 def sort_third(l: list):
-    if len(l) % 3 != 0:
-        l += [None] * (3 - len(l) % 3)
-    first, middle, last = [], [], []
-    for i, x in enumerate(l):
-        if i % 3 == 0:
-            first.append(x)
-        elif i % 3 == 1:
-            middle.append(x)
-        else:
-            last.append(x)
+    l.sort()
+    l1 = [x for i, x in enumerate(l) if i % 3 == 0]
+    l2 = [x for i, x in enumerate(l) if i % 3 == 1]
+    l3 = [x for i, x in enumerate(l) if i % 3 == 2]
 
-    first, last = sorted(first), sorted(last, reverse=True)
-    result = []
-    for i in range(len(l)):
-        if i % 3 == 0:
-            result.append(first.pop(0))
-        elif i % 3 == 1:
-            result.append(middle.pop(0))
-        else:
-            result.append(last.pop(0) or None)
-
-    return tuple(result)
+    return [
+        (x if i % 3 != 0 else (x if i % 3 == 1 else l3.pop(0)))
+        for i, x in enumerate(l1 + l2 + list(reversed(l3)))
+    ]
