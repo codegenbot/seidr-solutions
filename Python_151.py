25851f22a98419190a16c17e1dbe908e5a80fb6b
beep boop <githubbot@vadim.me>
2024-07-10 22:31:30 +0200

    Solution 4 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zckjcwmr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/14bbfafb-848d-47bc-bcaa-27e3b12de 394.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/14bbfafb-848d-47bc-bcaa-27e3b12de 394.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,9 +1,5 @@
-```
 def double_the_difference(lst):
-    odd_sum = sum(i**2 for i in lst if isinstance(i, int) and i >= 0 and i % 2 != 0)
-    return double_the_difference_value(odd_sum)
+    return sum(i**2 for i in lst if isinstance(i, int) and i >= 0 and i % 2 != 0)
 
-double_the_difference_value = lambda x: (x[1] - x[0]) * 2
-
-check_result = double_the_difference([7, 1])
-print(check_result)
+expected_output = double_the_difference([1, 2, 3, 4])
+check(expected_output)
