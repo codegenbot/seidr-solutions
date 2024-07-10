c8d741f2adb3bf29203ed18bd8704995dd3e7bd6
beep boop <githubbot@vadim.me>
2024-07-11 00:06:53 +0200

    Solution 66 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vlwyz493 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9ce2d1cf-69ba-450c-b7e2-92e0c7df1 78d.py", line 1     Here's the corrected code:         ^ SyntaxError: unterminated string literal (detected at line 1)

--- Python_67.py
+++ Python_67.py
@@ -21,4 +21,8 @@ def fruit_distribution(s, n):
                 if fruit:
                     fruits[fruit] = remaining_count
         i += 1
-    return len(fruits) + (1 if any(count > n for count in fruits.values()) or sum(fruits.values(), 0) > n else 0)
+    return len(fruits) + (
+        1
+        if any(count > n for count in fruits.values()) or sum(fruits.values(), 0) > n
+        else 0
+    )
