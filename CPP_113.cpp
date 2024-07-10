1c84b4399602d796c33996ecdde183359f48b956
beep boop <githubbot@vadim.me>
2024-07-10 18:20:49 +0200

    Solution 2 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/xjg4rs13 Prompt:       |                                            ^ e06e96ac-3ab6-4538-aee9-6ce5a7296390.cpp:15:45: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e06e96ac-3ab6-4538-aee9-6ce5a7296390.cpp:14: e06e96ac-3ab6-4538-aee9-6ce5a7296390.cpp: In function ‘int main()’: e06e96ac-3ab6-4538-aee9-6ce5a7296390.cpp:24:20: error: ‘odd_count’ was not decla red in this scope    24 |     assert (issame(odd_count({"271", "137", "314"}) , {       |                    ^~~~~~~~~ e06e96ac-3ab6-4538-aee9-6ce5a7296390.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(odd_count({"271", "137", "314"}) , {       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    25 |         "the number of odd elements 2n the str2ng 2 of the 2nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    26 |         "the number of odd elements 3n the str3ng 3 of the 3nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    27 |         "the number of odd elements 2n the str2ng 2 of the 2nput."       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    28 |     }));       |     ~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -1,14 +1,13 @@
 vector<string> odd_count(vector<string> lst) {
     vector<string> result;
     for (string s : lst) {
-        int oddCount = 0;
+        int count = 0;
         for (char c : s) {
             if ((c - '0') % 2 != 0) {
-                oddCount++;
+                count++;
             }
         }
-        string res = "the number of odd elements " + to_string(oddCount) + "n the str" + to_string(oddCount) + "ng " + s + " of the " + to_string(oddCount) + "nput.";
-        result.push_back(res);
+        result.push_back("the number of odd elements " + to_string(count) + " in the string " + s + " of the input.");
     }
     return result;
 }
