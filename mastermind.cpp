ce8eceb6415f541c8fb310d9cdaf47f214934661
beep boop <githubbot@vadim.me>
2024-07-10 13:10:48 +0200

    Solution 38 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/tjtxcwav Prompt: 1dce7007-b4f2-4bbf-aae5-5923b2a1785b.cpp:3:22: error: ‘string’ was not declared in this scope     3 | PegCounts mastermind(string code, string guess) {       |                      ^~~~~~ 1dce7007-b4f2-4bbf-aae5-5923b2a1785b.cpp:3:35: error: ‘string’ was not declared in this scope     3 | PegCounts mastermind(string code, string guess) {       |                                   ^~~~~~ 1dce7007-b4f2-4bbf-aae5-5923b2a1785b.cpp:3:49: error: expected ‘,’ or ‘;’ before  ‘{’ token     3 | PegCounts mastermind(string code, string guess) {       |                                                 ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -2,9 +2,9 @@
 
 struct PegCounts { int white; int black; };
 
-PegCounts mastermind(std::string code, std::string guess) {
+PegCounts mastermind(string code, string guess) {
     PegCounts result = {0, 0};
-    std::string remainingGuess = guess;
+    string remainingGuess = guess;
 
     // Count black pegs
     for (int i = 0; i < 4; ++i) {
@@ -15,26 +15,15 @@ PegCounts mastermind(std::string code, std::string guess) {
     }
 
     // Count white pegs
-    int codeCount[256] = {0};
     for (int i = 0; i < 4; ++i) {
-        codeCount[code[i]]++;
-    }
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] != guess[i]) {
-            codeCount[guess[i]]--;
-            if (codeCount[guess[i]] == 0) {
-                codeCount[guess[i]] = -1;
+        int count = 0;
+        for (int j = 0; j < 4; ++j) {
+            if (code[j] == remainingGuess[i]) {
+                count++;
             }
         }
-    }
-    for (int i = 0; i < 4; ++i) {
-        if (remainingGuess[i] != ' ') {
-            if (codeCount[code[i]] > 0) {
-                result.white++;
-                codeCount[code[i]]--;
-            } else if (codeCount[guess[i]] == -1) {
-                result.black++;
-            }
+        if (count > 0) {
+            result.white += count - 1; 
         }
     }
 
