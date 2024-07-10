aea5a35fcceb2c6e8a6f235c8a6ab91483e04d0f
beep boop <githubbot@vadim.me>
2024-07-10 18:27:05 +0200

    Solution 8 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/eixmci7m Prompt:       |                    ^~~ c2760279-9a1d-4248-8165-8479c954f595.cpp:21:27: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c2760279-9a1d-4248-8165-8479c954f595.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c2760279-9a1d-4248-8165-8479c954f595.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c2760279-9a1d-4248-8165-8479c954f595.cpp:20: c2760279-9a1d-4248-8165-8479c954f595.cpp: In function ‘int main()’: c2760279-9a1d-4248-8165-8479c954f595.cpp:30:20: error: ‘unique_digits’ was not d eclared in this scope    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ c2760279-9a1d-4248-8165-8479c954f595.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,11 +1,14 @@
-#include <vector>
-
-bool issame(vector<int> a, vector<int> b) {
-    // code for issame function
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    std::sort(a.begin(), a.end());
+    std::sort(b.begin(), b.end());
+    return a == b;
 }
 
-vector<int> unique_digits(vector<int> x) {
-    vector<int> result;
+std::vector<int> unique_digits(std::vector<int> x) {
+    std::vector<int> result;
     for (int num : x) {
         bool hasEvenDigit = false;
         int temp = num;
@@ -20,6 +23,11 @@ vector<int> unique_digits(vector<int> x) {
             result.push_back(num);
         }
     }
-    sort(result.begin(), result.end());
+    std::sort(result.begin(), result.end());
     return result;
+}
+
+int main() {
+    // Add test cases and assertions here
+    return 0;
 }
