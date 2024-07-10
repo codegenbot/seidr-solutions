6249fe2d1fe462c5bd9cbc7dccbcdac93b4d8fba
beep boop <githubbot@vadim.me>
2024-07-11 00:27:39 +0200

    Solution 93 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tb2d1yc4 Prompt: Complete the following code given the task description and function signature.

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -1,19 +1,16 @@
 #include <algorithm>
 
 vector<float> find_closest_elements(vector<float> numbers) {
+    vector<float> closest;
     float min_diff = numeric_limits<float>::max();
-    pair<float, float> closest_pair;
-
     for (int i = 0; i < numbers.size() - 1; ++i) {
         for (int j = i + 1; j < numbers.size(); ++j) {
-            float diff = abs(numbers[i] - numbers[j]);
-            if (diff < min_diff) {
+            float diff = numbers[j] - numbers[i];
+            if (abs(diff) < abs(min_diff)) {
                 min_diff = diff;
-                closest_pair.first = numbers[i];
-                closest_pair.second = numbers[j];
+                closest = {numbers[i], numbers[j]};
             }
         }
     }
-
-    return vector<float> {closest_pair.first, closest_pair.second};
+    return closest;
 }
