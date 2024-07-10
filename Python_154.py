7ab0624898413656bb608a8115dd6fbc8027bc96
beep boop <githubbot@vadim.me>
2024-07-10 08:45:10 +0200

    Solution 85 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/opkwh9ja Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4ade9869-278e-4487-8043-aacca2c77 9a9.py", line 2     return len(a) == len(b) and any(a == (b * 2)[i:i+len(a)] for i in range(len( a))                                    ^ SyntaxError: '(' was never closed

--- Python_154.py
+++ Python_154.py
@@ -1,2 +1,2 @@
 def cycpattern_check(a, b):
-    return len(a) == len(b) and any(a == (b * 2)[i:i+len(a)] for i in range(len(b) - len(a) + 1))
+    return len(a) == len(b) and any(a == (b * 2)[i:i+len(a)] for i in range(len(a)))
