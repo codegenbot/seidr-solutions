65e1d508a5d783ebf3cf7983cf16cb8ee25ee605
beep boop <githubbot@vadim.me>
2024-07-10 18:30:28 +0200

    Solution 10 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/eixmci7m Prompt:       |             ^~~~~~ 0c953c3f-881e-4b15-979e-935bc3385570.cpp:35:34: error: expected primary-expressi on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0c953c3f-881e-4b15-979e-935bc3385570.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 0c953c3f-881e-4b15-979e-935bc3385570.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~ In file included from 0c953c3f-881e-4b15-979e-935bc3385570.cpp:34: 0c953c3f-881e-4b15-979e-935bc3385570.cpp: In function ‘int main()’: 0c953c3f-881e-4b15-979e-935bc3385570.cpp:44:20: error: ‘unique_digits’ was not d eclared in this scope    44 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ 0c953c3f-881e-4b15-979e-935bc3385570.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,13 +1,39 @@
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    std::sort(a.begin(), a.end());
-    std::sort(b.begin(), b.end());
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b);
+vector<int> unique_digits(vector<int> x);
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
+vector<int> unique_digits(vector<int> x){
+    vector<int> result;
+    for (int num : x) {
+        bool hasEvenDigit = false;
+        int temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+            if (digit % 2 == 0) {
+                hasEvenDigit = true;
+                break;
+            }
+            temp /= 10;
+        }
+        if (!hasEvenDigit) {
+            result.push_back(num);
         }
     }
-    return true;
+    sort(result.begin(), result.end());
+    return result;
+}
+
+int main() {
+    assert(issame(unique_digits({135, 103, 31}), {31, 135}));
+    return 0;
 }
