a1a7d70b2b61d459cd2835f3d3e3afe7834f712e
beep boop <githubbot@vadim.me>
2024-07-10 03:59:39 +0200

    Solution 7 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/hbdwuvhz Prompt: 91160939-3bd6-4375-ad45-ba8fc709c8a6.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ 91160939-3bd6-4375-ad45-ba8fc709c8a6.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 bool isvowel(char c) {
-    c = tolower(c);
-    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') ||
+           (c >= 'A' && c <= 'U');
 }
 
 string get_closest_vowel(string word) {
