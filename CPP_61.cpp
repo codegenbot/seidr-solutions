a5e316b9d487d8d49a4144e7c064525cc1218bcf
beep boop <githubbot@vadim.me>
2024-07-10 17:17:30 +0200

    Solution 63 of CPP/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/bzsz8u64 Prompt: 20c1453a-7272-45f9-92af-a0916e7ac892.cpp:2:1: error: expected unqualified-id bef ore ‘for’     2 | for(int i=0; i<brackets.length(); i++){       | ^~~ 20c1453a-7272-45f9-92af-a0916e7ac892.cpp:2:14: error: ‘i’ does not name a type     2 | for(int i=0; i<brackets.length(); i++){       |              ^ 20c1453a-7272-45f9-92af-a0916e7ac892.cpp:2:35: error: ‘i’ does not name a type     2 | for(int i=0; i<brackets.length(); i++){       |                                   ^ 20c1453a-7272-45f9-92af-a0916e7ac892.cpp:10:1: error: expected unqualified-id be fore ‘return’    10 | return count == 0;       | ^~~~~~ In file included from 20c1453a-7272-45f9-92af-a0916e7ac892.cpp:12: 20c1453a-7272-45f9-92af-a0916e7ac892.cpp: In function ‘int main()’: 20c1453a-7272-45f9-92af-a0916e7ac892.cpp:14:18: error: ‘correct_bracketing’ was not declared in this scope    14 |     assert (not (correct_bracketing("()()(()())()))()")));       |                  ^~~~~~~~~~~~~~~~~~

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -3,7 +3,7 @@
 
 bool correct_bracketing(std::string brackets) {
     int count = 0;
-    for(int i=0; i< brackets.length(); i++){
+    for(int i=0; i<std::strlen(brackets.c_str())); i++){
         if(brackets[i] == '(') {
             count++;
         } else if (brackets[i] == ')') {
