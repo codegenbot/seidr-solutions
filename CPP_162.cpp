deb72547bbc1340d4f46882101396ccc4ea3a342
beep boop <githubbot@vadim.me>
2024-07-10 14:18:42 +0200

    Solution 85 of CPP/162, ratio 0.5 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/n3y415cj Prompt: 920254fd-2959-4d7d-b6ea-28b0957d6bb4.cpp: In function ‘std::string string_to_md5 (const string&)’: 920254fd-2959-4d7d-b6ea-28b0957d6bb4.cpp:12:28: error: ‘MD5_DIGEST_LENGTH’ was n ot declared in this scope    12 |     unsigned char md5_hash[MD5_DIGEST_LENGTH];       |                            ^~~~~~~~~~~~~~~~~ 920254fd-2959-4d7d-b6ea-28b0957d6bb4.cpp:19:36: error: ‘md5_hash’ was not declar ed in this scope    19 |         !EVP_DigestFinal_ex(mdctx, md5_hash, &hash_len)) {       |                                    ^~~~~~~~ 920254fd-2959-4d7d-b6ea-28b0957d6bb4.cpp:28:30: error: ‘md5_hash’ was not declar ed in this scope; did you mean ‘md5_hash_str’?    28 |         md5_hash_str += char(md5_hash[i]);       |                              ^~~~~~~~       |                              md5_hash_str 920254fd-2959-4d7d-b6ea-28b0957d6bb4.cpp:34:48: error: ‘md5_hash’ was not declar ed in this scope; did you mean ‘md5_hash_str’?    34 |         ss << std::setw(2) << static_cast<int>(md5_hash[i]);       |                                                ^~~~~~~~       |                                                md5_hash_str

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -9,29 +9,24 @@
 std::string string_to_md5(const std::string& input) {
     EVP_MD_CTX *mdctx = EVP_MD_CTX_create();
     const EVP_MD *md = EVP_md5();
-    unsigned char hash[MD5_DIGEST_LENGTH];
+    unsigned char md5_hash[EVP_MD_size(md)];
     unsigned int hash_len;
 
     OpenSSL_add_all_algorithms();
 
     if (!EVP_DigestInit_ex(mdctx, md, NULL) ||
         !EVP_DigestUpdate(mdctx, input.c_str(), input.size()) ||
-        !EVP_DigestFinal_ex(mdctx, hash, &hash_len)) {
+        !EVP_DigestFinal_ex(mdctx, md5_hash, &hash_len)) {
         EVP_MD_CTX_free(mdctx);
         return ""; // Handle error
     }
 
     EVP_MD_CTX_free(mdctx);
 
-    std::string md5_hash;
-    for (unsigned int i = 0; i < hash_len; i++) {
-        md5_hash += char(hash[i]);
-    }
-
     std::stringstream ss;
     ss << std::hex << std::setfill('0');
     for (unsigned int i = 0; i < hash_len; i++) {
-        ss << std::setw(2) << static_cast<int>(hash[i]); // Change 'hash' to a different name, e.g., 'hash_val'
+        ss << std::setw(2) << static_cast<int>(md5_hash[i]);
     }
 
     return ss.str();
