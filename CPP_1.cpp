314b9fe545a0363bfabb777c22be7ba895468e84
beep boop <githubbot@vadim.me>
2024-07-10 20:49:17 +0200

    Solution 11 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/v3l4k5fc Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bf710701-515b-4185-8824-72e07b24e5e2.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bf710701-515b-4185-8824-72e07b24e5e2.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from bf710701-515b-4185-8824-72e07b24e5e2.cpp:25: bf710701-515b-4185-8824-72e07b24e5e2.cpp: In function ‘int main()’: bf710701-515b-4185-8824-72e07b24e5e2.cpp:35:20: error: ‘separate_paren_groups’ w as not declared in this scope    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ bf710701-515b-4185-8824-72e07b24e5e2.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,3 +1,8 @@
+
+bool issame(const string& a, const string& b) {
+    return a == b;
+}
+
 vector<string> separate_paren_groups(const string& paren_string) {
     vector<string> groups;
     string group;
@@ -22,15 +27,4 @@ vector<string> separate_paren_groups(const string& paren_string) {
 
     return groups;
 }
-
-bool issame(const vector<string>& a, const vector<string>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
+```
