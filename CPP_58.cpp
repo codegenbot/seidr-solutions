14e6cfc212995df7e2459d2f15f87e5a872bf90f
beep boop <githubbot@vadim.me>
2024-07-10 06:13:41 +0200

    Solution 44 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/x3oovagu Prompt:       |             ^~~~~~ 31c9956c-a9fa-4353-93fb-155d19b2641c.cpp:33:20: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 31c9956c-a9fa-4353-93fb-155d19b2641c.cpp:33:27: error: ‘vector’ was not declared  in this scope    33 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 31c9956c-a9fa-4353-93fb-155d19b2641c.cpp:33:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 31c9956c-a9fa-4353-93fb-155d19b2641c.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 31c9956c-a9fa-4353-93fb-155d19b2641c.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 31c9956c-a9fa-4353-93fb-155d19b2641c.cpp:33:34: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -4,7 +4,7 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    for (size_t i = 0; i < a.size(); ++i) {
+    for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
@@ -25,9 +25,6 @@ std::vector<int> common(std::vector<int> v1, std::vector<int> v2) {
     return result;
 }
 
-int main() {
-    assert(issame(common({4, 3, 2, 8}, {2, 3, 5}), {2, 3}));
-    assert(issame(common({1, 2, 3}, {4, 5, 6}), {}));
-    assert(issame(common({1, 2, 3}, {1, 2, 3}), {1, 2, 3}));
-    return 0;
-}
+static_assert(issame(common({4, 3, 2, 8}, {2, 3, 5}), {2, 3}));
+static_assert(issame(common({1, 2, 3}, {4, 5, 6}), {}));
+static_assert(issame(common({1, 2, 3}, {1, 2, 3}), {1, 2, 3}));
