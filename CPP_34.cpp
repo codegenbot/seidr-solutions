ad1377d854a9b5578adf2ce14166661d2864f9f4
beep boop <githubbot@vadim.me>
2024-07-10 03:03:19 +0200

    Solution 82 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uzrnpb22 Prompt:    39 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4243994a-20f9-4fdd-9151-6949bb092860.cpp:38: 4243994a-20f9-4fdd-9151-6949bb092860.cpp: In function ‘int main()’: 4243994a-20f9-4fdd-9151-6949bb092860.cpp:48:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    48 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 4243994a-20f9-4fdd-9151-6949bb092860.cpp:3: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ In file included from 4243994a-20f9-4fdd-9151-6949bb092860.cpp:38: 4243994a-20f9-4fdd-9151-6949bb092860.cpp:48:19: error: ‘issame’ cannot be used a s a function    48 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -25,18 +25,12 @@ namespace std {
     }
 }
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
-}
-
 int main_test() { 
     std::vector<int> input = {5, 3, 5, 2, 3, 3, 9, 0, 123};
     auto output = input; // Create a copy of the input vector
-    auto uniqueOutput = std::unique(output.begin(), output.end()); // Use std::unique to get the unique elements
-    for (auto i = output.begin(); i != uniqueOutput; ++i) {
+    output.erase(std::unique(output.begin(), output.end()), output.end());
+    for (auto i = output.begin(); i != output.end(); ++i) {
         std::cout << *i << " ";
     }
     std::cout << std::endl;
-
-    assert(issame({0, 2, 3, 5, 9, 123}, std::vector<int>(unique(input).first, unique(input).second)));
 }
