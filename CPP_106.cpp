745a1996ec39ea73df57d231bc409c479078719f
beep boop <githubbot@vadim.me>
2024-07-10 19:44:17 +0200

    Solution 74 of CPP/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/kuwfv8yt Prompt:       |                           ^~~~~~ 7710ab9a-be7b-49e7-bc84-c56877464e73.cpp:31:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 7710ab9a-be7b-49e7-bc84-c56877464e73.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 7710ab9a-be7b-49e7-bc84-c56877464e73.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7710ab9a-be7b-49e7-bc84-c56877464e73.cpp:31:34: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7710ab9a-be7b-49e7-bc84-c56877464e73.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 7710ab9a-be7b-49e7-bc84-c56877464e73.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main(){       |     ^~~~

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -1,15 +1,28 @@
 #include <vector>
+#include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b){
-    if (a.size() != b.size()) {
+    if(a.size() != b.size())
         return false;
-    }
     
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
+    for(size_t i = 0; i < a.size(); ++i){
+        if(a[i] != b[i])
             return false;
-        }
     }
     
     return true;
+}
+
+std::vector<int> f(int n){
+    std::vector<int> res(n);
+    for(int i = 0; i < n; ++i){
+        res[i] = i + 1;
+    }
+    return res;
+}
+
+int main(){
+    assert (issame(f(3), {1, 2, 3}));
+    
+    return 0;
 }
