dea11b350f456550dd90f9cae7ce122043a9239a
beep boop <githubbot@vadim.me>
2024-07-10 22:56:28 +0200

    Solution 45 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/f1jr1l8u Prompt: 53979785-1f37-4fc5-b038-3bff1728e40a.cpp:15:14: warning: ‘int MD5_Final(unsigned  char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]    15 |     MD5_Final(md5, &ctx);       |     ~~~~~~~~~^~~~~~~~~~~ In file included from 53979785-1f37-4fc5-b038-3bff1728e40a.cpp:2: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ 53979785-1f37-4fc5-b038-3bff1728e40a.cpp:19:32: error: invalid conversion from ‘ const char*’ to ‘char*’ [-fpermissive]    19 |         sprintf(output.c_str() + output.size(), "%02x", md5[i]);       |                 ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~       |                                |       |                                const char* In file included from /usr/include/c++/11/cstdio:42,                  from /usr/include/c++/11/ext/string_conversions.h:43,                  from /usr/include/c++/11/bits/basic_string.h:6608,                  from /usr/include/c++/11/string:55,                  from 53979785-1f37-4fc5-b038-3bff1728e40a.cpp:1: /usr/include/stdio.h:358:38: note:   initializing argument 1 of ‘int sprintf(cha r*, const char*, ...)’   358 | extern int sprintf (char *__restrict __s,       |                     ~~~~~~~~~~~~~~~~~^~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <string>
 #include <openssl/md5.h>
 
@@ -13,6 +14,7 @@ string string_to_md5(string text) {
     size_t len = text.size();
     MD5_Update(&ctx, str, len);
     MD5_Final((unsigned char*)md5, &ctx); 
+
     string output;
     for (int i = 0; i < MD5_DIGEST_LENGTH; ++i) {
         sprintf((char*)(output.c_str() + output.size()), "%02x", md5[i]);
