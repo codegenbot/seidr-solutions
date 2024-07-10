37cb3a1cf0673dc3e2b8df38de5754d68f56f3df
beep boop <githubbot@vadim.me>
2024-07-10 20:52:40 +0200

    Solution 39 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pc4qhgax Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4505e4f3-5d41-41f8-a67c-a2bcbec8b 9ff.py", line 13, in <module>     result = intersperse(list1 + list2[:], list2)              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4505e4f3-5d41-41f8-a67c-a2bcbec8b 9ff.py", line 3, in intersperse     min_len = min(len(iter(a)), len(b))                   ^^^^^^^^^^^^ TypeError: object of type 'list_iterator' has no len()

--- Python_5.py
+++ Python_5.py
@@ -1,13 +1,11 @@
 def intersperse(a, b):
-    if isinstance(b, int):  
-        return [i for pair in zip(a, [b]*len(a)) for i in pair] + [b]
     result = []
     min_len = min(len(a), len(b))
     for i in range(min_len):
-        result.append(a.pop(0))
-        result.append(b.pop(0))
-    result.extend(a)
-    result.extend(b)
+        result.append(next(iter(a)))
+        result.append(next(iter(b)))
+    result.extend(list(a)[min_len:])
+    result.extend(list(b)[min_len:])
     return result
 
 list1 = [1, 3, 5]
