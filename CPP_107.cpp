235b465fe29fdddcf088fad190ca69e0024f59c1
beep boop <githubbot@vadim.me>
2024-07-10 08:07:26 +0200

    Solution 53 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/i4ztvcn8 Prompt: 74735e04-2df2-42fa-80b9-720907901023.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 74735e04-2df2-42fa-80b9-720907901023.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from 74735e04-2df2-42fa-80b9-720907901023.cpp:28: 74735e04-2df2-42fa-80b9-720907901023.cpp: In function ‘int main()’: 74735e04-2df2-42fa-80b9-720907901023.cpp:38:20: error: ‘even_odd_palindrome’ was  not declared in this scope; did you mean ‘contest::even_odd_palindrome’?    38 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 74735e04-2df2-42fa-80b9-720907901023.cpp:9:22: note: ‘contest::even_odd_palindro me’ declared here     9 |     std::vector<int> even_odd_palindrome(int n) {       |                      ^~~~~~~~~~~~~~~~~~~ In file included from 74735e04-2df2-42fa-80b9-720907901023.cpp:28: 74735e04-2df2-42fa-80b9-720907901023.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -2,8 +2,6 @@
 #include <cassert>
 
 namespace contest {
-    #include <vector>
-    
     bool issame(const std::vector<int>& a, const std::vector<int>& b) {
         return a == b;
     }
@@ -20,7 +18,7 @@ namespace contest {
     }
 }
 
-int main_task() {
+int main() {
     assert(contest::issame(contest::even_odd_palindrome(1), {1, 1}));
     assert(contest::issame(contest::even_odd_palindrome(3), {3, 2, 1, 1, 2, 3}));
     
