cf51b360d535e81c3db7be112fca12336c6d936c
beep boop <githubbot@vadim.me>
2024-07-10 22:26:56 +0200

    Solution 62 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/avcnu0rr Prompt:    53 | bool issame(vector<int> a,vector<int>b){       |                                       ^ f17d058a-dc3f-4f93-bc54-f28d2be739f4.cpp:61:5: error: redefinition of ‘int main( )’    61 | int main(){       |     ^~~~ f17d058a-dc3f-4f93-bc54-f28d2be739f4.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from f17d058a-dc3f-4f93-bc54-f28d2be739f4.cpp:52: f17d058a-dc3f-4f93-bc54-f28d2be739f4.cpp: In function ‘int main()’: f17d058a-dc3f-4f93-bc54-f28d2be739f4.cpp:62:20: error: ‘parse_music’ was not dec lared in this scope    62 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ f17d058a-dc3f-4f93-bc54-f28d2be739f4.cpp:62:19: error: ‘issame’ cannot be used a s a function    62 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,24 +1,16 @@
-vector<int> issame(vector<int> a, vector<int> b);
+vector<int> issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return {-1};
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return {-1};
+        }
+    }
+    return {1};
+}
 
 vector<int> parse_music(string music_string);
-
-int main() {
-    string music_string;
-    cin >> music_string;
-    
-    vector<int> music_beats = parse_music(music_string);
-
-    vector<int> example = {1, 2, 3};
-
-    vector<int> result = issame(music_beats, example);
-
-    for (int i : result) {
-        cout << i << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
 
 vector<int> parse_music(string music_string){
     vector<int> result;
@@ -39,14 +31,20 @@ vector<int> parse_music(string music_string){
     return result;
 }
 
-vector<int> issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return {-1};
+int main() {
+    string music_string;
+    cin >> music_string;
+    
+    vector<int> music_beats = parse_music(music_string);
+
+    vector<int> example = {1, 2, 3};
+
+    vector<int> result = issame(music_beats, example);
+
+    for (int i : result) {
+        cout << i << " ";
     }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return {-1};
-        }
-    }
-    return {1};
+    cout << endl;
+
+    return 0;
 }
