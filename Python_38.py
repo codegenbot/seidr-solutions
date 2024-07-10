7d9882a12610959819225fb005b197337aeba910
beep boop <githubbot@vadim.me>
2024-07-10 20:49:07 +0200

    Solution 64 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/disnge4r Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0639aa85-6144-4192-836f-7cce8fe0b ee2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_38.py
+++ Python_38.py
@@ -1,14 +1,16 @@
 ```
 def decode_cyclic(s: str):
-    temp = ''
-    result = ''
+    result = ""
+    temp = ""
+    is_next_x = False
     for char in s:
-        if char == 'x':
-            if temp:
-                result += temp[::-1] + 'x'
-                temp = ''
+        if char == "x":
+            if not is_next_x and temp:
+                result += temp[::-1] + ("x" if not is_next_x else "")
+                temp = ""
+            is_next_x = not is_next_x
         else:
             temp += char
     if temp:
-        result += temp[::-1]
-    return result.replace('xx', 'x').replace('x', '')
+        result += temp[::-1] + ("x" if not is_next_x else "")
+    return result.replace("xx", "x").replace("x", "")```
