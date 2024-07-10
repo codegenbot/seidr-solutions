77828421175e462a151316558a5158693491a6d2
beep boop <githubbot@vadim.me>
2024-07-10 23:31:47 +0200

    Solution 41 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vlwyz493 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f0390666-658c-4e0f-bfef-ee03b489e 2b3.py", line 30, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f0390666-658c-4e0f-bfef-ee03b489e 2b3.py", line 27, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -10,15 +10,15 @@ def fruit_distribution(s, n):
         if words[i].isdigit():
             count = int(words[i])
             i += 1
-            fruit = ''
+            fruit = ""
             for j in range(i, len(words)):
                 if not words[j].isalnum():
                     break
-                fruit += words[j] + ' '
+                fruit += words[j] + " "
             fruit = fruit.strip()
-            total_count = 0
+            total_count = sum(fruits.get(k, 0) for k in fruits)
             if fruit:
-                total_count = min(count, n - total_count)
+                total_count += min(count, n - total_count)
                 fruits[fruit] = total_count
         i += 1
     return len(fruits)
