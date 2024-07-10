98cb24ec5807bd84e2e6e84b6c96ed1f84f8d98d
beep boop <githubbot@vadim.me>
2024-07-10 21:38:38 +0200

    Solution 64 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/kfejmui6 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,48 +1,20 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
-    int min_diff = INT_MAX;
-    pair<vector<int>, vector<int>> result;
+vector<int> cutVector(vector<int> nums) {
+    int minDiff = INT_MAX;
+    int cutIndex = 0;
     
-    for (int i = 1; i <= vec.size(); i++) {
-        int left_sum = 0, right_sum = 0;
-        
-        for (int j = 0; j < i; j++) {
-            left_sum += vec[j];
-        }
-        
-        for (int j = i; j < vec.size(); j++) {
-            right_sum += vec[j];
-        }
-        
-        int diff = abs(left_sum - right_sum);
-        
-        if (diff < min_diff) {
-            min_diff = diff;
-            result = {vector<int>(vec.begin(), vec.begin() + i), vector<int>(vec.begin() + i, vec.end())};
+    for (int i = 1; i < nums.size(); i++) {
+        int diff = abs(nums[i] - nums[0]);
+        if (diff <= minDiff) {
+            minDiff = diff;
+            cutIndex = i;
         }
     }
     
-    return result;
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) {
-        cin >> vec[i];
-    }
-    pair<vector<int>, vector<int>> res = cutVector(vec);
-    cout << "[";
-    for (int x : res.first) {
-        cout << x << " ";
-    }
-    cout << "]\n[";
-    for (int x : res.second) {
-        cout << x << " ";
-    }
-    cout << "]";
-    return 0;
+    vector<int> left(nums.begin(), nums.begin() + cutIndex);
+    vector<int> right(nums.begin() + cutIndex, nums.end());
+    
+    return {left, right};
 }
