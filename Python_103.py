bbd8fb9d35c1b8ff43aea50c3b05597583cb7a7e
beep boop <githubbot@vadim.me>
2024-07-11 01:26:49 +0200

    Solution 55 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/th7rjdae Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8b0eed12-2b46-4195-bfd9-550815870 d6f.py", line 10, in <module>     check(rounded_avg)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8b0eed12-2b46-4195-bfd9-550815870 d6f.py", line 7, in check     assert rounded_avg(5, 5) == "0b101"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_103.py
+++ Python_103.py
@@ -1,17 +1,3 @@
-```
-import math
-
 def rounded_avg(n, m):
-    avg = (n + m) / 2
-    if int(avg) == round(avg):
-        return f"0b{bin(int(round(avg)))[2:].ljust(8, '0')}"
-    else:
-        bin_str = ""
-        while True:
-            if int(avg) == round(avg):
-                break
-            avg *= 2
-            bin_str += '01'[int(math.modf(avg)[0])]
-        return bin_str.ljust(8, '0')
-
-print(rounded_avg(5, 3))
+    avg = (n + m) / 2.0
+    return f"0b{bin(int(round(avg)))[2:].ljust(8, '0')}"
