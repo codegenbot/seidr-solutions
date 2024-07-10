34c3059778c47d7b767002925e4cb559745cb613
beep boop <githubbot@vadim.me>
2024-07-10 20:19:58 +0200

    Solution 49 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bx3axgl1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/369663fd-9f9d-4d22-aa1d-4845968b7 fb5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_26.py
+++ Python_26.py
@@ -1,12 +1,8 @@
 ````
-""" 
-Problem: 
-Remove duplicates from a list of numbers
-Input: A list of integers
-Output: The same list with duplicate numbers removed
 """
 from typing import List
 
 
 def remove_duplicates(numbers: List[int]) -> List[int]:
-    return [x for n, x in enumerate(sorted(set(numbers))) if x not in numbers[:n]]
+    return list(set(sorted(numbers)))
+"""
