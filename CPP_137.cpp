35b0931b383c73760e3126c825326d88b60ec9a8
beep boop <githubbot@vadim.me>
2024-07-11 01:28:57 +0200

    Solution 71 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9snlnzo8 Prompt: 5e146c5e-375c-49f0-a112-9cab458034d6.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 5e146c5e-375c-49f0-a112-9cab458034d6.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 5e146c5e-375c-49f0-a112-9cab458034d6.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 5e146c5e-375c-49f0-a112-9cab458034d6.cpp:4:10: fatal error: boost/variant.hpp: N o such file or directory     4 | #include <boost/variant.hpp>       |          ^~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -24,7 +24,7 @@ any compare_one(any a, any b) {
         double num = get<double>(a);
         return to_string(num) > str ? a : b;
     }
-    else if (holds_alternative<string>(a) && holds_alternation<std::string>(b)) {
+    else if (holds_alternative<string>(a) && holds_alternative<string>(b)) {
         string str1 = get<string>(a);
         string str2 = get<string>(b);
         return str1 > str2 ? a : b;
@@ -34,12 +34,12 @@ any compare_one(any a, any b) {
         string str = get<string>(b);
         return to_string(num) > str ? a : b;
     }
-    else if (holds_alternative<string>(a) && holds_alternation<int>(b)) {
+    else if (holds_alternative<string>(a) && holds_alternative<int>(b)) {
         int num = get<int>(b);
         string str = get<string>(a);
         return str > to_string(num) ? a : b;
     }
-    else if (holds_alternative<double>(a) && holds_alternation<double>(b)) {
+    else if (holds_alternative<double>(a) && holds_alternative<double>(b)) {
         double num1 = get<double>(a);
         double num2 = get<double>(b);
         return num1 > num2 ? a : b;
