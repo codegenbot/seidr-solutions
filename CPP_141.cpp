c8579d3063adc0562fe7a17537bdfb88dc837f2b
beep boop <githubbot@vadim.me>
2024-07-10 08:08:56 +0200

    Solution 10 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/bchzkib9 Prompt: _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 5b53a7bd-4d68-42d9-855e-e4b390b76987.cpp:32:24:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 5b53a7bd-4d68-42d9-855e-e4b390b76987.cpp:32:24:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,33 +1,9 @@
-string file_name_check(string file_name){
-    int dot_count = 0, digit_count = 0;
-    bool valid = true;
-
-    for(int i=0; i<file_name.size(); i++){
-        char c = file_name[i];
-        if(c == '.'){
-            dot_count++;
-            if(dot_count > 1) {
-                valid = false;
-                break;
-            }
-        } else if(isdigit(c)){
-            digit_count++;
-            if(digit_count > 3) {
-                valid = false;
-                break;
-            }
-        } else if(!isalpha(c) && c != '.'){
-            valid = false;
-            break;
-        }
+char* c_str = file_name.c_str();
+string extension = "";
+for (int i = strlen(c_str) - 1; i > 0; i--) {
+    if (c_str[i] == '.') {
+        break;
+    } else {
+        extension += c_str[i];
     }
-
-    if(valid){
-        string ext = file_name.substr(file_name.find('.')+1);
-        if(ext == "txt" || ext == "exe" || ext == "dll")
-            return "Yes";
-        else
-            return "No";
-    } else
-        return "No";
 }
