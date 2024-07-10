70e7b8b110a43cc190c6cf900626f8367bac86d9
beep boop <githubbot@vadim.me>
2024-07-10 21:42:57 +0200

    Solution 58 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pc4qhgax Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb1de572-919e-412f-85b4-c74a71500 850.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_5.py
+++ Python_5.py
@@ -1,14 +1,14 @@
 def intersperse(a, b):
     result = []
-    for item in a:
+    for i, item in enumerate(a):
         result.append(item)
-        if isinstance(b, int):
-            result.extend([b]*len(a))
+        if isinstance(b, (list, tuple)):
+            result.extend([b[i % len(b)]])
         else:
             result.append(b)
     return result
 
 list1 = [2, 2, 2]
-list2 = 2
+list2 = [1, 2, 3]
 result = intersperse(list1, list2)
 print(result)
