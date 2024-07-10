5a6e23f444db19a7dac9df17d542071ea944f352
beep boop <githubbot@vadim.me>
2024-07-10 17:16:24 +0200

    Solution 11 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/s103d66u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5fe70e3b-9318-4273-a2ee-7de10498a 782.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -2,7 +2,9 @@
 def sum_squares(lst):
     total = 0
     for num in lst:
-        if num % 3 == 0 and num % 4 != 0:
+        if type(num) != int:
+            return "Invalid input: list must contain integers."
+        elif num % 3 == 0 and num % 4 != 0:
             total += num ** 2
         elif num % 4 == 0 and num % 3 != 0:
             total += num ** 3
