0e6af8ab2f4f5871eed7cadb0b94458b056708e2
beep boop <githubbot@vadim.me>
2024-07-10 23:58:52 +0200

    Solution 61 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/e9ane987 Prompt: Program did not receive expected input

--- camel-case.py
+++ camel-case.py
@@ -1,4 +1,4 @@
 import re
 
 def camel_case(s):
-    return ''.join(word.capitalize() if i > 0 else word for i, word in enumerate(re.sub('-| ', '-', s.split()).split('-'))
+    return ''.join(word.capitalize() if i > 0 else word for i, word in enumerate(re.sub('-| ', '-', s.replace(' -', ' ').replace('- ', ' ').split()).split('-'))
