ee1985e2430b91054c3abe2f50570f2154da2d8e
beep boop <githubbot@vadim.me>
2024-07-10 18:40:11 +0200

    Solution 10 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hwoml10f Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 822a5b07-9685-4d62-b5a3-9096c96ccacd.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,23 +1,21 @@
-#include <vector>
-#include <string>
+using namespace std;
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
+bool issame(string s1, string s2) {
+    if(s1.size() != s2.size()) {
+        return false;
+    }
+    for(int i = 0; i < s1.size(); i++) {
+        if(s1[i] != s2[i]) {
+            return false;
+        }
     }
     return true;
 }
 
-std::vector<std::string> all_prefixes(std::string str) {
-    std::vector<std::string> result;
-    for(int i = 1; i <= str.length(); i++) {
+vector<string> all_prefixes(string str) {
+    vector<string> result;
+    for (int i = 1; i <= str.length(); ++i) {
         result.push_back(str.substr(0, i));
     }
     return result;
-}
-
-int main() {
-    assert (issame(all_prefixes("WWW") , {"W", "WW", "WWW"}));
-    return 0;
 }
