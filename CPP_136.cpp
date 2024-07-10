9f959be4f33de494cf04a09208808046bc54c962
beep boop <githubbot@vadim.me>
2024-07-11 01:36:18 +0200

    Solution 58 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/f6bcnret Prompt: 1d37362e-9aac-4340-a577-9a4deb291b9b.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1d37362e-9aac-4340-a577-9a4deb291b9b.cpp:25: 1d37362e-9aac-4340-a577-9a4deb291b9b.cpp: In function ‘int main()’: 1d37362e-9aac-4340-a577-9a4deb291b9b.cpp:35:20: error: ‘largest_smallest_integer s’ was not declared in this scope; did you mean ‘MyNamespace::largest_smallest_i ntegers’?    35 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 1d37362e-9aac-4340-a577-9a4deb291b9b.cpp:9:22: note: ‘MyNamespace::largest_small est_integers’ declared here     9 |     std::vector<int> largest_smallest_integers(std::vector<int> lst) {       |                      ^~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 1d37362e-9aac-4340-a577-9a4deb291b9b.cpp:25: 1d37362e-9aac-4340-a577-9a4deb291b9b.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,22 +1,28 @@
 #include <vector>
 #include <algorithm>
 
-namespace {
-bool operator==(const std::vector<int>& a, const std::vector<int>& b) {
-    return std::equal(a.begin(), a.end(), b.begin());
-}
-
-std::vector<int> largest_smallest_integers(std::vector<int> lst) {
-    int max_negative = INT_MAX;
-    int min_positive = 0;
-
-    for(int i : lst) {
-        if(i < 0 && i < max_negative) {
-            max_negative = i;
-        } else if(i > 0 && i < min_positive) {
-            min_positive = i;
-        }
+namespace MyNamespace {
+    bool isSame(const std::vector<int>& a, const std::vector<int>& b) {
+        return std::equal(a.begin(), a.end(), b.begin());
     }
 
-    return {max_negative, min_positive};
+    std::vector<std::vector<int>> largest_smallest_integers(std::vector<int> lst) {
+        int max_negative = 0;
+        int min_positive = INT_MAX;
+
+        for(int i : lst) {
+            if(i < 0 && i > max_negative) {
+                max_negative = i;
+            } else if(i > 0 && i < min_positive) {
+                min_positive = i;
+            }
+        }
+
+        return {{std::max(max_negative, (int)0), std::min(min_positive, (int)1)}};
+    }
+}
+
+int main() {
+    assert(MyNamespace::isSame({std::max(-6, 0), std::min(1, -100)}, {-3, 1}));
+    return 0;
 }
