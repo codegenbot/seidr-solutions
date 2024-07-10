1b4be182e2fcc045e8d9385fe4b3fcc5e6a92750
beep boop <githubbot@vadim.me>
2024-07-10 23:07:40 +0200

    Solution 36 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/kgm636ao Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b2c6b5ea-4504-4f97-aff8-d4396552d 6f8.py", line 1     """     ^ SyntaxError: unterminated triple-quoted string literal (detected at line 20)

--- Python_103.py
+++ Python_103.py
@@ -1,4 +1,4 @@
-```
+```python
 n = int(input("Enter first number: "))
 m = int(input("Enter second number: "))
 def rounded_avg(n, m):
@@ -10,4 +10,4 @@ def rounded_avg(n, m):
         avg += 1
         bin_str = format(avg, "b")
     return bin_str.zfill(32)
-print(rounded_avg(n, m)) """
+print(rounded_avg(n, m))
