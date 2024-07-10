ecb29dcb0a24dec2d62ae15c5b2951ac7dd574d1
beep boop <githubbot@vadim.me>
2024-07-10 06:33:24 +0200

    Solution 99 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/nns7oxk8 Prompt:     5 | 8590654b-bcf9-4870-bed6-1689c8ec4cd5.cpp:34:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    34 | int main(){       |         ^~ 8590654b-bcf9-4870-bed6-1689c8ec4cd5.cpp:34:9: note: remove parentheses to defau lt-initialize a variable    34 | int main(){       |         ^~       |         -- 8590654b-bcf9-4870-bed6-1689c8ec4cd5.cpp:34:9: note: or replace parentheses with  braces to value-initialize a variable 8590654b-bcf9-4870-bed6-1689c8ec4cd5.cpp:34:11: error: a function-definition is not allowed here before ‘{’ token    34 | int main(){       |           ^ 8590654b-bcf9-4870-bed6-1689c8ec4cd5.cpp:36:2: error: expected ‘}’ at end of inp ut    36 | }       |  ^ 8590654b-bcf9-4870-bed6-1689c8ec4cd5.cpp:17:12: note: to match this ‘{’    17 | int main() {       |            ^

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -18,6 +18,6 @@ int main() {
     assert(issame(std::vector<int>({5, 6, 3, 4, 8, 9, 2, 1}), std::vector<int>({1, 2, 3, 4, 5, 6, 8, 9}));
     assert(!issame(std::vector<int>({5, 6, 3, 4, 8, 9, 2, 1}), std::vector<int>({2, 6, 3, 4, 8, 9, 5, 1}));
     assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), std::vector<int>({1, 2, 3, 4, 5, 6, 8, 9}));
-    
+
     return 0;
 }
