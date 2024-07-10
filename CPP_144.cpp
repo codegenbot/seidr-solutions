ba66a0547a2cae26069d7475f2a5196f187a5205
beep boop <githubbot@vadim.me>
2024-07-10 11:39:22 +0200

    Solution 24 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/o1pkvjf1 Prompt: Program failed

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,28 +1,6 @@
-bool checkDivisibility(std::string x, std::string n) {
-    if (x.find('/') == std::string::npos || n.find('/') == std::string::npos) {
-        return true;
-    }
-
-    try {
-        int den1 = std::stoi(x.substr(x.find('/') + 1));
-        int den2 = std::stoi(n.substr(n.find('/') + 1));
-
-        if (den1 == 0 || den2 == 0) {
-            return false;
-        }
-
-        int num1 = std::stoi(x.substr(0, x.find('/')));
-        int num2 = std::stoi(n.substr(0, n.find('/')));
-
-        return (num1 * num2) % (den1 * den2) == 0;
-    } catch (std::invalid_argument& e) {
-        return false;
-    }
-}
-
 bool simplify(std::string x, std::string n) {
     if (x.find('/') == std::string::npos || n.find('/') == std::string::npos) {
-        return true;
+        return false;
     }
 
     try {
@@ -35,10 +13,7 @@ bool simplify(std::string x, std::string n) {
             return false;
         }
 
-        int gcd = std::__gcd(num1 * den2, num2 * den1);
-        if (gcd == 1) {
-            gcd = customGcd(num1 * den2, num2 * den1);
-        }
+        int gcd = customGcd(num1 * den2, num2 * den1);
 
         num1 /= gcd;
         den1 /= gcd;
