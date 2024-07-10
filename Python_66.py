2fcd5b56182250fe6491210af015b02acecc643f
beep boop <githubbot@vadim.me>
2024-07-10 03:48:28 +0200

    Solution 40 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vnld1i80 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f6c75d6a-b2e1-4bcf-9392-9c5d5f62c c0f.py", line 13, in <module>     check(digitSum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f6c75d6a-b2e1-4bcf-9392-9c5d5f62c c0f.py", line 10, in check     assert digitSum("You arE Very Smart") == 327, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_66.py
+++ Python_66.py
@@ -1,10 +1,6 @@
-```
 def digitSum(s):
     total = 0
-    for i, c in enumerate(s):
+    for c in s:
         if c.isdigit():
-            if (i % 2) == 0:
-                total += int(c)
-            else:
-                total += int(c) * 2 - 5 if int(c) > 4 else int(c) * 2
+            total += int(c)
     return total
