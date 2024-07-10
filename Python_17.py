fff0ed28b3be7dfd44f765ade2dbb9e23f113c62
beep boop <githubbot@vadim.me>
2024-07-10 21:19:09 +0200

    Solution 30 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tbcv99fp Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d15b015b-a8b4-46b5-9ef0-48fd7fcad d47.py", line 26, in <module>     check(parse_music)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d15b015b-a8b4-46b5-9ef0-48fd7fcad d47.py", line 23, in check     assert parse_music("o| .| o| .| o o| o o|") == [2, 1, 2, 1, 4, 2, 4, 2]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_17.py
+++ Python_17.py
@@ -1,3 +1,4 @@
+```
 def parse_music(music_string: str) -> list:
     music_list = []
     i = 0
@@ -7,7 +8,7 @@ def parse_music(music_string: str) -> list:
             i += 1
         elif i < len(music_string) - 2 and music_string[i:i+2] == 'o|':
             music_list.append(2)
-            i += 2
+            i += 1
         elif i < len(music_string) - 3 and music_string[i:i+3] == '.||':
             music_list.append(1)
             i += 3
