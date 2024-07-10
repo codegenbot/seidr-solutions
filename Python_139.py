91b24408add55624a3ae52d90530203a700b6604
beep boop <githubbot@vadim.me>
2024-07-10 17:10:20 +0200

    Solution 0 of Python/139, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ytzhbb9l Prompt: Complete the following code given the task description and function signature.

---
+++ Python_139.py
@@ -0,0 +1,10 @@
+import math
+
+
+def special_factorial(n):
+    if n <= 0:
+        return "Input must be greater than zero"
+    result = 1
+    for i in range(1, n + 1):
+        result *= math.factorial(i)
+    return result
