d1f9ae8c32566bdeb6327a78104d02167b4c21a8
beep boop <githubbot@vadim.me>
2024-07-10 04:13:27 +0200

    Solution 55 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/6yzx5jug Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03d5dd3f-2b6c-4056-a62c-33ec2d2d4 709.py", line 1, in <module>     user_input = raw_input("Enter an alphanumeric string: ")                  ^^^^^^^^^ NameError: name 'raw_input' is not defined

--- Python_50.py
+++ Python_50.py
@@ -1,12 +1,9 @@
-user_input = input("Enter an alphanumeric string: ").rstrip()
-
+user_input = input("Enter an alphanumeric string: ")
 if not user_input.isalnum():
     print("Invalid input. Please enter an alphanumeric string.")
 else:
-    result = "".join(
-        [
-           chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) if ch.isalpha() else ch
-           for ch in user_input
-        ]
-    )
+    result = "".join([
+        chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) if ch.isalpha() else ch
+        for ch in user_input
+    ])
     print(result)
