5b3022a70e805e29fb63af258d67de64aa66a90b
beep boop <githubbot@vadim.me>
2024-07-10 17:53:27 +0200

    Solution 74 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/t5k8vwk5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f841cbf3-a466-41eb-b8b5-f91ab3319 301.py", line 13, in <module>     check(digitSum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f841cbf3-a466-41eb-b8b5-f91ab3319 301.py", line 10, in check     assert digitSum("You arE Very Smart") == 327, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f841cbf3-a466-41eb-b8b5-f91ab3319 301.py", line 3, in digitSum     n = int("".join(filter(str.isdigit, n.replace(" ", "").lower())))         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: ''

--- Python_66.py
+++ Python_66.py
@@ -1,6 +1,8 @@
+```Python
 def digitSum(n=""):
     if isinstance(n, str):
-        n = sum(int(digit) for digit in n.replace(" ", "").lower())
+        n = "".join(filter(str.isdigit, n.replace(" ", "").lower()))
+        return sum(int(c) for c in n)
     elif not n:  
         n = int(input("Enter a number: "))
-    return sum(int(i) for i in str(abs(n)))
+        return sum(int(c) for c in str(abs(n)) if c.isdigit())
