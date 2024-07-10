cb052087e63666737209d8d214b9b81e64b2b948
beep boop <githubbot@vadim.me>
2024-07-10 07:56:23 +0200

    Solution 89 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/svifujg2 Prompt: /usr/bin/ld: /tmp/ccqW9XAC.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': 12c9d60a-5df1-4b04-be38-dae21ee4d582.cpp:(.text+0x25): undefined reference to `E VP_MD_CTX_new' /usr/bin/ld: 12c9d60a-5df1-4b04-be38-dae21ee4d582.cpp:(.text+0x2e): undefined re ference to `EVP_md5' /usr/bin/ld: 12c9d60a-5df1-4b04-be38-dae21ee4d582.cpp:(.text+0x45): undefined re ference to `EVP_DigestInit_ex' /usr/bin/ld: 12c9d60a-5df1-4b04-be38-dae21ee4d582.cpp:(.text+0x75): undefined re ference to `EVP_DigestUpdate' /usr/bin/ld: 12c9d60a-5df1-4b04-be38-dae21ee4d582.cpp:(.text+0x8d): undefined re ference to `EVP_DigestFinal_ex' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <openssl/ssl.h>
 
 std::ostringstream ss;
 
@@ -10,6 +11,7 @@ std::string string_to_md5(const std::string& str) {
     EVP_MD_CTX* md5;
     unsigned char hash[16];
     md5 = EVP_MD_CTX_new();
+    OpenSSL_add_all_algorithms();
     EVP_DigestInit_ex(md5, EVP_md5(), 0);
     EVP_DigestUpdate(md5, str.c_str(), str.size());
     EVP_DigestFinal_ex(md5, hash, nullptr);
@@ -24,5 +26,6 @@ std::string string_to_md5(const std::string& str) {
 
 int main_test() {
     assert(string_to_md5("password") == "5d41402abc4de22fbc1fc7ce2a607762");
+    OpenSSL_add_all_algorithms();
     return 0;
 }
