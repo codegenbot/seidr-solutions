73561c5bc31d9404f3596c55c5f45faf450bd38c
beep boop <githubbot@vadim.me>
2024-07-10 06:29:12 +0200

    Solution 31 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lerjqzl3 Prompt:    16 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 8e0c7f66-de5b-4cb4-aac9-e82422c9b0d3.cpp:16:36: error: expected primary-expressi on before ‘float’    16 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 8e0c7f66-de5b-4cb4-aac9-e82422c9b0d3.cpp:16:43: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 8e0c7f66-de5b-4cb4-aac9-e82422c9b0d3.cpp:15: 8e0c7f66-de5b-4cb4-aac9-e82422c9b0d3.cpp: In function ‘int main()’: 8e0c7f66-de5b-4cb4-aac9-e82422c9b0d3.cpp:25:20: error: ‘sort_even’ was not decla red in this scope    25 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 8e0c7f66-de5b-4cb4-aac9-e82422c9b0d3.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <algorithm>
 #include <vector>
 
@@ -9,11 +8,11 @@ bool issame(vector<float> a, vector<float> b) {
 vector<float> sort_even(vector<float> l) {
     vector<float> result;
     for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) { 
+        if (i % 2 == 0) { // even index
             vector<float> temp(l.begin() + i, l.begin() + i + 1);
             sort(temp.begin(), temp.end());
             result.insert(result.end(), temp.begin(), temp.end());
-        } else { 
+        } else { // odd index
             result.push_back(l[i]);
         }
     }
