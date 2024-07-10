5c2eb6a7188dc2f5197b77e88919f6700652dab3
beep boop <githubbot@vadim.me>
2024-07-10 22:36:36 +0200

    Solution 10 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zckjcwmr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf30ce99-af28-4a86-9d30-7c8d4ebf8 61d.py", line 7, in <module>     check_result = double_the_difference([7, 1])                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf30ce99-af28-4a86-9d30-7c8d4ebf8 61d.py", line 3, in double_the_difference     return double_the_difference_value(odd_sum)            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf30ce99-af28-4a86-9d30-7c8d4ebf8 61d.py", line 5, in <lambda>     double_the_difference_value = lambda x: (x[1] - x[0]) * 2                                              ~^^^ TypeError: 'int' object is not subscriptable

--- Python_151.py
+++ Python_151.py
@@ -1,2 +1,7 @@
+```
 def double_the_difference(lst):
-    return 2 * (sum(i**2 for i in lst if isinstance(i, int) and i > 0))
+    odd_sum = sum(i**2 for i in lst if isinstance(i, int) and i >= 0 and i % 2 != 0)
+    return abs(odd_sum - min(lst)) * 2
+
+check_result = double_the_difference([7, 1])
+print(check_result)
