6ef5eedb0e81d2d7048f68a72a745c2c0a75f058
beep boop <githubbot@vadim.me>
2024-07-10 14:39:37 +0200

    Solution 64 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/epocmqxi Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 68134cc8-1cd2-411e-9376-48c18423e499.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 68134cc8-1cd2-411e-9376-48c18423e499.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 68134cc8-1cd2-411e-9376-48c18423e499.cpp:10: 68134cc8-1cd2-411e-9376-48c18423e499.cpp: In function ‘int main()’: 68134cc8-1cd2-411e-9376-48c18423e499.cpp:20:20: error: ‘sort_third’ was not decl ared in this scope    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 68134cc8-1cd2-411e-9376-48c18423e499.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,13 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
 bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    return equal(a.begin(), a.end(), b.begin());
 }
 
 vector<int> sort_third(vector<int> l) {
@@ -22,5 +21,6 @@ vector<int> sort_third(vector<int> l) {
 
 int main() {
     assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
+    
     return 0;
 }
