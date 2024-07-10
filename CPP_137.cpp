0e9c3b0644bd429612dcdaa0e8985d40ce213713
beep boop <githubbot@vadim.me>
2024-07-10 22:42:37 +0200

    Solution 5 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9snlnzo8 Prompt: 7b9cd2e4-a0e5-41c5-a06d-2facdb79f217.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,38 +3,62 @@
 
 using namespace std;
 
-template<typename... Ts>
-variant<Ts> compare_one(const variant<Ts>& a, const variant<Ts>& b) {
-    if (holds_alternative<int, double>(a) && holds_alternative<double, int>(b)) {
-        if (get_if<int>(&get<int>(a))) {
-            return (int)get<int>(a) > get<double>(b) ? a : b;
-        } else {
-            return (double)get<double>(a) > get<int>(b) ? a : b;
-        }
-    } else if (holds_alternative<string>(a) && holds_alternative<double, int>(b)) {
+variant<int, double, string> compare_one(variant<int, double, string> a, variant<int, double, string> b) {
+    if (holds_alternative<int, double>(a) && holds_alternalternative<double, int>(b)) {
+        if (get<int>(a) > get<double>(b))
+            return a;
+        else
+            return b;
+    }
+    else if (holds_alternative<string>(a) && holds_alternative<double>(b)) {
         string str = get<string>(a);
         double num = get<double>(b);
-        return str > to_string(num) ? a : b;
-    } else if (holds_alternative<double, int>(a) && holds_alternative<string>(b)) {
+        if (str > to_string(num))
+            return a;
+        else
+            return b;
+    }
+    else if (holds_alternative<double>(a) && holds_alternative<string>(b)) {
         double num = get<double>(a);
         string str = get<string>(b);
-        return to_string(num) > str ? a : b;
-    } else if (holds_alternative<string>(a) && holds_alternative<string>(b)) {
+        if (to_string(num) > str)
+            return a;
+        else
+            return b;
+    }
+    else if (holds_alternation<string>(a) && holds_alternation<string>(b)) {
         string str1 = get<string>(a);
         string str2 = get<string>(b);
-        return str1 > str2 ? a : b;
-    } else if (holds_alternative<int, double>(a) && holds_alternative<string>(b)) {
+        if (str1 > str2)
+            return a;
+        else
+            return b;
+    }
+    else if (holds_alternative<int>(a) && holds_alternative<string>(b)) {
         int num = get<int>(a);
         string str = get<string>(b);
-        return to_string(num) > str ? a : b;
-    } else if (holds_alternative<string>(a) && holds_alternative<int, double>(b)) {
+        if (to_string(num) > str)
+            return a;
+        else
+            return b;
+    }
+    else if (holds_alternation<string>(a) && holds_alternation<int>(b)) {
         string str = get<string>(a);
         int num = get<int>(b);
-        return str > to_string(num) ? a : b;
-    } else if (holds_alternative<double, int>(a) && holds_alternative<double, int>(b)) {
+        if (str > to_string(num))
+            return a;
+        else
+            return b;
+    }
+    else if (holds_alternation<double>(a) && holds_alternation<double>(b)) {
         double num1 = get<double>(a);
         double num2 = get<double>(b);
-        return num1 > num2 ? a : b;
+        if (num1 > num2)
+            return a;
+        else
+            return b;
     }
-    return "None";
+    else {
+        return "None";
+    }
 }
