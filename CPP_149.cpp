1ffe0858c9665004c0363c6d959dd6f94dab99b4
beep boop <githubbot@vadim.me>
2024-07-10 10:25:44 +0200

    Solution 8 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/0x1g0oqm Prompt: d6e1a03e-50e4-4930-ab21-5e47e06d7fd2.cpp:37:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    37 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ d6e1a03e-50e4-4930-ab21-5e47e06d7fd2.cpp:8:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     8 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ d6e1a03e-50e4-4930-ab21-5e47e06d7fd2.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ d6e1a03e-50e4-4930-ab21-5e47e06d7fd2.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -5,30 +5,12 @@
 
 using namespace std;
 
-bool is_same(vector<string> a, vector<string> b) {
-    a.erase(remove_if(a.begin(), a.end(), [](const string& s) { return s.length() % 2 != 0; }), a.end());
-    sort(a.begin(), a.end(), [](const string& x, const string& y) {
-        if (x.length() == y.length()) {
-            return x < y;
-        }
-        return x.length() < y.length();
-    });
-    b.erase(remove_if(b.begin(), b.end(), [](const string& s) { return s.length() % 2 != 0; }), b.end());
-    sort(b.begin(), b.end(), [](const string& x, const string& y) {
-        if (x.length() == y.length()) {
-            return x < y;
-        }
-        return x.length() < y.length();
-    });
-    return a == b;
-}
-
 vector<string> sorted_list_sum(vector<string> a) {
     sort(a.begin(), a.end());
     return a;
 }
 
 int main() {
-    assert(is_same(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
     return 0;
 }
