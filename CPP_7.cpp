0358cc0562b3449fd2e2f073ff5fe0c1ad23160f
beep boop <githubbot@vadim.me>
2024-07-10 05:44:47 +0200

    Solution 50 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tuv5tenk Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8bf1ba9e-78a6-4066-92a0-81746e70fdd3.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8bf1ba9e-78a6-4066-92a0-81746e70fdd3.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8bf1ba9e-78a6-4066-92a0-81746e70fdd3.cpp:10: 8bf1ba9e-78a6-4066-92a0-81746e70fdd3.cpp: In function ‘int main()’: 8bf1ba9e-78a6-4066-92a0-81746e70fdd3.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 8bf1ba9e-78a6-4066-92a0-81746e70fdd3.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,23 +1,16 @@
 ```cpp
+bool issame(vector<string> a,vector<string>b){
+    return a == b;
+}
 vector<string> filter_by_substring(vector<string> strings, string substring){
     vector<string> result;
-    for(string s : strings) {
-        if(s.find(substring) != string::npos)
-            result.push_back(s);
+    for (const auto& str : strings) {
+        if (str.find(substring) != string::npos)
+            result.push_back(str);
     }
     return result;
-
 }
-bool operator==(vector<string> a, vector<string>b){
-    if(a.size() != b.size())
-        return false;
-    for(size_t i = 0; i < a.size(); i++){
-        if(a[i] != b[i])
-            return false;
-    }
-    return true;
-}
-
-int main(){
-    assert(std::equal(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run") ,{"grunt", "prune"}));
+int main() {
+    assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run") ,{"grunt", "prune"}));
+    return 0;
 }
