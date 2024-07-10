bf8c4155b148536c4128455fac411672aa73c04d
beep boop <githubbot@vadim.me>
2024-07-10 06:44:21 +0200

    Solution 75 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/c6w360zo Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 41107314-3f3f-4fde-8409-ad6f1aca3522.cpp:21:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,20 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <cassert>
+```cpp
+void calculate_odd_sum() {
+    int num;
+    std::vector<int> lst; 
+    int odd_sum = 0; 
 
-int double_the_difference(const std::vector<int>& nums) {
-    int oddSum = 0;
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] % 2 != 0) {
-            oddSum += nums[i];
-        }
-    }
-    return oddSum * oddSum;
-}
-
-int main() {
-    std::vector<int> lst;
-    int num, odd_sum = 0;
     while(std::cin >> num) {
         lst.push_back(num); 
         if (num % 2 != 0) {
@@ -24,5 +13,4 @@ int main() {
     int result = double_the_difference(lst); 
     std::cout << "The sum of squares of odd numbers is: " << result << std::endl;
     assert(double_the_difference(lst) == odd_sum);
-    return 0;
 }
