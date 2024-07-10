07d97bc38ff73de038d2f5a8e3f3a0597dd7586f
beep boop <githubbot@vadim.me>
2024-07-10 08:03:15 +0200

    Solution 10 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/2yxr1kn3 Prompt: 67069482-f289-42fc-8381-e8ed79b107c0.cpp:4:10: fatal error: boost/any.hpp: No su ch file or directory     4 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,32 +1,35 @@
+#include <iostream>
 #include <string>
 #include <algorithm>
 #include <boost/any.hpp>
-#include <boost/multi_array.hpp>
-
 using namespace std;
-using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_numeric(a) && is_numeric(b)) {
-        double da = get<double>(a);
-        double db = get<double>(b);
-
-        return make_any((da > db) ? a : b);
-    } else if (!is_numeric(a) && !is_numeric(b)) {
-        string sa = get<string>(a);
-        string sb = get<string>(b);
-
-        return make_any((sa > sb) ? a : b);
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        int x = boost::any_cast<int>(a);
+        float y = boost::any_cast<float>(b);
+        return (x > y) ? a : ((y > x) ? b : boost::any((boost::any_cast<int>(a) > boost::any_cast<float>(b)) ? a : b));
+    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        int x = boost::any_cast<int>(a);
+        string y = boost::any_cast<string>(b);
+        return (stoi(y) > x) ? b : ((x > stoi(y)) ? a : boost::any((boost::any_cast<int>(a) > stoi(string(to_wstring(boost::any_cast<int>(a))))) ? a : b));
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        float x = boost::any_cast<float>(a);
+        int y = boost::any_cast<int>(b);
+        return (x > y) ? a : ((y > x) ? b : boost::any((boost::any_cast<float>(a) > boost::any_cast<int>(b)) ? a : b));
+    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        string x = boost::any_cast<string>(a);
+        int y = boost::any_cast<int>(b);
+        return (stod(x) > y) ? a : ((y > stod(x)) ? b : boost::any((stod(string(to_wstring(boost::any_cast<float>(a)))) > boost::any_cast<int>(b)) ? a : b));
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        string x = boost::any_cast<string>(a);
+        float y = boost::any_cast<float>(b);
+        return (stod(x) > y) ? a : ((y > stod(x)) ? b : boost::any((stod(string(to_wstring(boost::any_cast<int>(a)))) > boost::any_cast<float>(b)) ? a : b));
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string x = boost::any_cast<string>(a);
+        string y = boost::any_cast<string>(b);
+        return (stod(x) > stod(y)) ? a : ((stod(y) > stod(x)) ? b : boost::any((stod(string(to_wstring(boost::any_cast<int>(a)))) > stod(string(to_wstring(boost::any_cast<int>(b))))) ? a : b));
     } else {
-        return make_any("None");
-    }
-}
-
-bool is_numeric(boost::any a) {
-    try {
-        get<double>(a);
-        return true;
-    } catch (...) {
-        return false;
+        return boost::any("Invalid input");
     }
 }
