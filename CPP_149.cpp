e87555f2d98ced239d572ec7ac3bacac6a1d2d5f
beep boop <githubbot@vadim.me>
2024-07-10 21:40:10 +0200

    Solution 59 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ptpr8f4c Prompt: _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:255:23:   required from ‘void std::__cxx 11::basic_string<_CharT, _Traits, _Alloc>::_M_construct_aux(_InIterator, _InIter ator, std::__false_type) [with _InIterator = const char*; _CharT = char; _Traits  = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:274:20:   required from ‘void std::__cxx 11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterator ) [with _InIterator = const char*; _CharT = char; _Traits = std::char_traits<cha r>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:641:16:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(_InputIterator, _InputIterato r, const _Alloc&) [with _InputIterator = const char*; <template-parameter-2-2> =  void; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator< char>]’ 7864896f-3dbd-42d9-ae0f-4b18e0380115.cpp:26:18:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -5,7 +5,7 @@
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); i++) {
-        int num = stoi(a[i] + " " + b[i]) ? stoi(a[i] + b[i]) : 0;
+        int num = stoi(a[i] + " " + b[i]);
         if (num != stoi(sortListSum({{a[i], b[i]}})[0])) return false;
     }
     return true;
