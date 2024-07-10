91e180739a909d3e83740efeafa07e32c79b1a99
beep boop <githubbot@vadim.me>
2024-07-10 17:33:58 +0200

    Solution 35 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/dodal3gc Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ be00e43f-3d8d-4e0e-a546-d861dfd4bb2b.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ be00e43f-3d8d-4e0e-a546-d861dfd4bb2b.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from be00e43f-3d8d-4e0e-a546-d861dfd4bb2b.cpp:16: be00e43f-3d8d-4e0e-a546-d861dfd4bb2b.cpp: In function ‘int main()’: be00e43f-3d8d-4e0e-a546-d861dfd4bb2b.cpp:26:20: error: ‘count_up_to’ was not dec lared in this scope    26 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ be00e43f-3d8d-4e0e-a546-d861dfd4bb2b.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,17 +1,3 @@
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
-vector<int> primes;
-for (int i = 2; i < n; ++i) {
-    bool is_prime = true;
-    for (int j = 2; j * j <= i; ++j) {
-        if (i % j == 0) {
-            is_prime = false;
-            break;
-        }
-    }
-    if (is_prime) {
-        primes.push_back(i);
-    }
-}
-return primes;
