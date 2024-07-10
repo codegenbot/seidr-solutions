5be5cd5d731d4f63aa890fbe691acf8f4692f4af
beep boop <githubbot@vadim.me>
2024-07-10 13:26:33 +0200

    Solution 88 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/0r0b8tjy Prompt:       |                           ^~~~~~ 88042b91-d990-4c66-803d-fdfb527a2af6.cpp:28:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 88042b91-d990-4c66-803d-fdfb527a2af6.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 88042b91-d990-4c66-803d-fdfb527a2af6.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 88042b91-d990-4c66-803d-fdfb527a2af6.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 88042b91-d990-4c66-803d-fdfb527a2af6.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 88042b91-d990-4c66-803d-fdfb527a2af6.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -17,6 +17,8 @@ std::vector<int> even_odd_count(int num) {
     return {even, odd};
 }
 
-assert(issame(even_odd_count(0) , {1, 0}));
-assert(issame(even_odd_count(123456), {3, 3}));
-assert(issame(even_odd_count(-87654), {2, 3}));
+void test_even_odd_count() {
+    assert(issame(even_odd_count(0) , {1, 0}));
+    assert(issame(even_odd_count(123456), {3, 3}));
+    assert(issame(even_odd_count(-87654), {2, 3}));
+}
