dbaccb17640bfa2ff4738c5a1e9e6a63b7fbad03
beep boop <githubbot@vadim.me>
2024-07-10 04:46:27 +0200

    Solution 21 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8pf1cn7u Prompt:     5 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c2a2dc7d-1fe6-408d-95c0-2193319479f4.cpp:5:34: error: expected primary-expressio n before ‘int’     5 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c2a2dc7d-1fe6-408d-95c0-2193319479f4.cpp:5:39: error: expression list treated as  compound expression in initializer [-fpermissive]     5 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c2a2dc7d-1fe6-408d-95c0-2193319479f4.cpp:4: c2a2dc7d-1fe6-408d-95c0-2193319479f4.cpp: In function ‘int main()’: c2a2dc7d-1fe6-408d-95c0-2193319479f4.cpp:14:20: error: ‘minPath’ was not declare d in this scope    14 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ c2a2dc7d-1fe6-408d-95c0-2193319479f4.cpp:14:19: error: ‘issame’ cannot be used a s a function    14 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,49 +1,7 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
-using namespace std;
-
 bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-
-int minPath(vector<vector<int>>& edges, int n) {
-    vector<int> dist(n+1, 1000000);
-    dist[1] = 0;
-    
-    for (int i = 1; i <= n; i++) {
-        for (auto edge : edges) {
-            if (dist[edge[0]] + edge[2] < dist[edge[1]]) {
-                dist[edge[1]] = dist[edge[0]] + edge[2];
-            }
-        }
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
-    
-    vector<int> path;
-    int curr = n;
-    while (curr != 1) {
-        for (auto edge : edges) {
-            if (dist[edge[0]] == dist[curr] - edge[2]) {
-                path.push_back(edge[1]);
-                curr = edge[0];
-                break;
-            }
-        }
-    }
-    
-    path.push_back(1);
-    reverse(path.begin(), path.end());
-    
-    return path.size();
-}
-
-int main() {
-    vector<vector<int>> edges = {{1, 3, 10}, {3, 2, 3}};
-    int n = 4;
-    cout << minPath({{1, 3, 10}, {3, 2, 3}}, 4) << endl;
-    
-    return 0;
+    return true;
 }
