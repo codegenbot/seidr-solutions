680de77b1255380a3d67c8340d52b6bfbc1ed9bd
beep boop <githubbot@vadim.me>
2024-07-10 19:41:13 +0200

    Solution 60 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/eixmci7m Prompt:       |                                  ^~~ b39f01fc-d9a7-4c95-9a3b-ca1cd2a86e79.cpp:34:39: error: expression list treated a s compound expression in initializer [-fpermissive]    34 | bool issame(vector<int> a,vector<int>b){       |                                       ^ b39f01fc-d9a7-4c95-9a3b-ca1cd2a86e79.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ b39f01fc-d9a7-4c95-9a3b-ca1cd2a86e79.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from b39f01fc-d9a7-4c95-9a3b-ca1cd2a86e79.cpp:33: b39f01fc-d9a7-4c95-9a3b-ca1cd2a86e79.cpp: In function ‘int main()’: b39f01fc-d9a7-4c95-9a3b-ca1cd2a86e79.cpp:43:20: error: ‘unique_digits’ was not d eclared in this scope; did you mean ‘uniqueDigits’?    43 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ b39f01fc-d9a7-4c95-9a3b-ca1cd2a86e79.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -3,18 +3,6 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 std::vector<int> uniqueDigits(std::vector<int> x) {
     std::vector<int> result;
     for (int num : x) {
@@ -37,7 +25,7 @@ std::vector<int> uniqueDigits(std::vector<int> x) {
 }
 
 int main() {
-    assert(issame(uniqueDigits({135, 103, 31}), std::vector<int>{31, 135}));
+    assert(std::issame(uniqueDigits({135, 103, 31}), std::vector<int>{31, 135}));
     std::cout << "Test Passed!" << std::endl;
     return 0;
 }
