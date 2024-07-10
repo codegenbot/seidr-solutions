4414a69e7875984a525b7f1d9e781ea9ed9ee9dc
beep boop <githubbot@vadim.me>
2024-07-10 22:42:37 +0200

    Solution 21 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/qew3a3cc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fbef7189-b1a6-44fd-8264-9841c5da4 e79.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_144.py
+++ Python_144.py
@@ -1,5 +1,13 @@
 def simplify(x, n):
     a, b = map(int, x.split("/"))
     c, d = map(int, n.split("/"))
-    g = abs((a*d) // (b*c))
-    return f"{a//g}/{b//g}" if g != 1 else f"{a}/{b}"
+
+    greatest_common_divisor = lambda a, b: (
+        a if not b else greatest_common_divisor(b, a % b)
+    )
+
+    gcd = greatest_common_divisor(
+        greatest_common_divisor(a, b), greatest_common_divisor(c, d)
+    )
+
+    return f"{a//gcd}/{b//gcd}" / f"{c//gcd}/{d//gcd}"
