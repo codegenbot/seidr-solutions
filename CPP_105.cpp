cb6c429d1cd3d6cb95a43f757eb44348a31a2657
beep boop <githubbot@vadim.me>
2024-07-10 17:25:42 +0200

    Solution 10 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tu21n5fh Prompt:       |                              ^~~~~~ 77ef7113-6886-45f5-b689-f0c665fd9c83.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 77ef7113-6886-45f5-b689-f0c665fd9c83.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 77ef7113-6886-45f5-b689-f0c665fd9c83.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 77ef7113-6886-45f5-b689-f0c665fd9c83.cpp:50: 77ef7113-6886-45f5-b689-f0c665fd9c83.cpp: In function ‘int main()’: 77ef7113-6886-45f5-b689-f0c665fd9c83.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 77ef7113-6886-45f5-b689-f0c665fd9c83.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,47 +1,14 @@
-vector<string> by_length(vector<int> arr) {
-    vector<int> numArr;
-    for (int i : arr) {
-        if (i >= 1 && i <= 9) {
-            numArr.push_back(i);
-        }
-    }
-    
-    sort(numArr.begin(), numArr.end());
-    
-    reverse(numArr.begin(), numArr.end());
+bool issame(vector<string> a,vector<string>b){
+    return (a == b);
+}
 
-    vector<string> result;
-    for (int i : numArr) {
-        switch (i) {
-            case 1:
-                result.push_back("One");
-                break;
-            case 2:
-                result.push_back("Two");
-                break;
-            case 3:
-                result.push_back("Three");
-                break;
-            case 4:
-                result.push_back("Four");
-                break;
-            case 5:
-                result.push_back("Five");
-                break;
-            case 6:
-                result.push_back("Six");
-                break;
-            case 7:
-                result.push_back("Seven");
-                break;
-            case 8:
-                result.push_back("Eight");
-                break;
-            case 9:
-                result.push_back("Nine");
-                break;
-        }
-    }
+int main() {
+    vector<int> arr = {1,2,3,4,5};
+    vector<string> result = by_length(arr);
+    if(issame(result, {"One","Two","Three","Four","Five"}))
+        cout<<"The output is correct.\n";
+    else
+        cout<<"The output is incorrect.\n";
 
-    return result;
+    return 0;
 }
