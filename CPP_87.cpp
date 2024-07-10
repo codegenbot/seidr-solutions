58f1209631cee55caaa69a455262e93c37b107e7
beep boop <githubbot@vadim.me>
2024-07-10 07:19:25 +0200

    Solution 31 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/xe1y8h7l Prompt: In file included from /usr/include/c++/11/vector:67,                  from e595f0d3-80fd-4ac8-84ae-d36f9eac81f7.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from e595f0d3-80fd-4ac8-84ae-d36f9eac81f7.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e595f0d3-80fd-4ac8-84ae-d36f9eac81f7.cpp:30:49: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ e595f0d3-80fd-4ac8-84ae-d36f9eac81f7.cpp:30:56: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from e595f0d3-80fd-4ac8-84ae-d36f9eac81f7.cpp:29: e595f0d3-80fd-4ac8-84ae-d36f9eac81f7.cpp: In function ‘int main()’: e595f0d3-80fd-4ac8-84ae-d36f9eac81f7.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,7 +1,3 @@
-#include <vector>
-#include <algorithm>
-#include <cassert>
-
 bool is_same(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b) {
     return a == b;
 }
@@ -24,7 +20,4 @@ std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
     return result;
 }
 
-int main() {
-    assert(is_same(get_row({{}, {1}, {1, 2, 3}}, 3), std::vector<std::vector<int>>{{2, 2}}));
-    return 0;
-}
+assert(is_same(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
