870d865138e8238d2a4eab6ae62baf1007add6df
beep boop <githubbot@vadim.me>
2024-07-11 00:31:09 +0200

    Solution 46 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/7lbr298e Prompt:       |           ^~~~~~ In file included from 60477039-9965-43e4-90fe-5546eeee4344.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 60477039-9965-43e4-90fe-5546eeee4344.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 60477039-9965-43e4-90fe-5546eeee4344.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 60477039-9965-43e4-90fe-5546eeee4344.cpp:14: 60477039-9965-43e4-90fe-5546eeee4344.cpp: In function ‘int main()’: 60477039-9965-43e4-90fe-5546eeee4344.cpp:24:20: error: ‘common’ was not declared  in this scope    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 60477039-9965-43e4-90fe-5546eeee4344.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -6,10 +6,15 @@
 bool issame(std::vector<int> a, std::vector<int> b);
 
 std::vector<int> solve(std::vector<int>& l1, std::vector<int>& l2) {
-    sort(l1.begin(), l1.end());
-    sort(l2.begin(), l2.end());
+    std::sort(l1.begin(), l1.end());
+    std::sort(l2.begin(), l2.end());
     std::vector<int> result;
-    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
+    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), std::back_inserter(result));
     result.erase(std::unique(result.begin(), result.end()), result.end());
     return result;
+}
+
+int main() {
+    // Test cases and assertions can be added here
+    return 0;
 }
