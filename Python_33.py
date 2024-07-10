eb4090b0f2dd7af6a61d9d6b71330b25758184bd
beep boop <githubbot@vadim.me>
2024-07-10 23:48:19 +0200

    Solution 92 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a2f08329-bef7-47df-9cc3-a687f8b39 66c.py", line 11, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a2f08329-bef7-47df-9cc3-a687f8b39 66c.py", line 6, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,6 +1,8 @@
 def sort_third(lst):
-    numbers = sorted({x for x in lst if isinstance(x, (int, float))})
+    numbers = sorted([x for x in lst if isinstance(x, (int, float))])
     others = [
-        item for sublist in [i for i in lst if isinstance(i, list)] for item in sublist
+        item
+        for sublist in [i for i in lst if isinstance(i, list)]
+        for item in sorted(sublist)
     ]
-    return [num for _ in range(numbers.count(num)) for num in numbers] + sorted(others)
+    return numbers + others
