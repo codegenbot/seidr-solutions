709d6cd44bb70fcdeeb2ebde786c2550d1719962
beep boop <githubbot@vadim.me>
2024-07-10 22:31:11 +0200

    Solution 12 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ygu5uohz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc0881e0-dfaa-4b36-b8d4-2e1c1da29 442.py", line 24, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc0881e0-dfaa-4b36-b8d4-2e1c1da29 442.py", line 20, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,18 +1,13 @@
+```
 def fix_spaces(text):
+    text += ' '
     new_text = ''
-    count = 0
     for char in text:
-        if char == ' ':
-            if count < 2:
+        if char == ' ' and new_text != '':
+            if len(new_text) < 2:
                 new_text += '_'
             else:
                 new_text += '-'
-            count = 0
-        elif count >= 3:
-            new_text += ' '
-            new_text += '_'
-            count = 1
-        else:
+        elif char != ' ':
             new_text += char
-            count += 1
     return new_text.strip()
