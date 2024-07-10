bbfb160896220f1a37af2aacb7625914b1668279
beep boop <githubbot@vadim.me>
2024-07-10 10:40:11 +0200

    Solution 36 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/51eh1c3h Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,43 +1,37 @@
 #include <boost/any.hpp>
-#include <string>
-#include <iostream>
-
-using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
+    if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return b;
     }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string str = (string)b.convert_to<string>();
-        size_t pos = str.find(',');
-        if (pos != string::npos)
-            str.erase(pos, 1);
-        return max((float)a.convert_to<float>(), stof(str));
+    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return b;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        if ((dynamic_pointer_cast<string>(a))->compare((boost::lexical_cast<string>(b))) > 0)
+            return a;
+        else if ((boost::lexical_cast<string>(b)).compare((dynamic_pointer_cast<string>(a))) > 0)
+            return b;
+        else
+            return boost::any("None");
+    }
+    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
+        if ((boost::lexical_cast<string>(a)).compare((dynamic_pointer_cast<string>(b))) > 0)
+            return a;
+        else if ((dynamic_pointer_cast<string>(b))->compare((boost::lexical_cast<string>(a))) > 0)
+            return b;
+        else
+            return boost::any("None");
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        size_t pos_a = (string)a.convert_to<string>().find(',');
-        size_t pos_b = (string)b.convert_to<string>().find(',');
-
-        if (pos_a != string::npos)
-            (string)a.convert_to<string>().erase(pos_a, 1);
-        if (pos_b != string::npos)
-            (string)b.convert_to<string>().erase(pos_b, 1);
-
-        return max(stof((string)a.convert_to<string>()), stof((string)b.convert_to<string>()));
+        if ((dynamic_pointer_cast<string>(a))->compare((dynamic_pointer_cast<string>(b))) > 0)
+            return a;
+        else if ((dynamic_pointer_cast<string>(b))->compare((dynamic_pointer_cast<string>(a))) > 0)
+            return b;
+        else
+            return boost::any("None");
     }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        size_t pos = (string)b.convert_to<string>().find(',');
-        if (pos != string::npos)
-            (string)b.convert_to<string>().erase(pos, 1);
-        return max((int)a.convert_to<int>(), stof((string)b.convert_to<string>()));
+    else {
+        return boost::any("None");
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        size_t pos = (string)a.convert_to<string>().find(',');
-        if (pos != string::npos)
-            (string)a.convert_to<string>().erase(pos, 1);
-        return max(stof((string)a.convert_to<string>()), (int)b.convert_to<int>());
-    }
-    else
-        return "None";
 }
