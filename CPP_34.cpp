10e11960039add8e91e9e6cbefb91c3074861ff6
beep boop <githubbot@vadim.me>
2024-07-10 02:22:48 +0200

    Solution 54 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uzrnpb22 Prompt:       |           ^~~~~~ In file included from 4625a93d-2ee7-4cbd-924c-ee1ce64db14e.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4625a93d-2ee7-4cbd-924c-ee1ce64db14e.cpp:37:34: error: expected primary-expressi on before ‘int’    37 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 4625a93d-2ee7-4cbd-924c-ee1ce64db14e.cpp:36: 4625a93d-2ee7-4cbd-924c-ee1ce64db14e.cpp: In function ‘int main()’: 4625a93d-2ee7-4cbd-924c-ee1ce64db14e.cpp:46:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    46 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 4625a93d-2ee7-4cbd-924c-ee1ce64db14e.cpp:3: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b){
+bool std::issame(const std::vector<int>& a, const std::vector<int>& b){
     return (a == b);
 }
 
@@ -11,7 +11,7 @@ It distinct(It first, It last) {
     if (first == last)
         return first;
 
-    vector<typename std::iterator_traits<It>::value_type> result;
+    std::vector<typename std::iterator_traits<It>::value_type> result;
     for (; first != last; ++first) {
         bool duplicate = false;
         for (const auto& value : result) {
@@ -28,10 +28,11 @@ It distinct(It first, It last) {
 }
 
 int main() {
-    vector<int> input = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    auto output(distinct(input.begin(), input.end()));
-    auto end = std::unique(output.begin(), output.end());
-    for (int i : vector<int>(output.begin(), end)) {
+    std::vector<int> input({5, 3, 5, 2, 3, 3, 9, 0, 123});
+    std::vector<int> output(distinct(input.begin(), input.end()), std::unique(input.begin(), input.end()));
+
+    for (int i : output) {
         std::cout << i << " ";
     }
+    return 0;
 }
