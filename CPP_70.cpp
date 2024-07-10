76c024675962d6c53a83e96a56e3fc1a60c08581
beep boop <githubbot@vadim.me>
2024-07-10 07:07:03 +0200

    Solution 56 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tn1r4gui Prompt:       |                    ^~~ 5fb3acc7-489a-4e86-9ed8-346e9bc1e30c.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5fb3acc7-489a-4e86-9ed8-346e9bc1e30c.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5fb3acc7-489a-4e86-9ed8-346e9bc1e30c.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5fb3acc7-489a-4e86-9ed8-346e9bc1e30c.cpp:17: 5fb3acc7-489a-4e86-9ed8-346e9bc1e30c.cpp: In function ‘int main()’: 5fb3acc7-489a-4e86-9ed8-346e9bc1e30c.cpp:27:20: error: ‘strange_sort_list’ was n ot declared in this scope    27 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 5fb3acc7-489a-4e86-9ed8-346e9bc1e30c.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,29 +1,43 @@
-```cpp
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
 std::vector<int> strange_sort_list(std::vector<int> lst) {
     std::vector<int> result;
     while (!lst.empty()) {
-        int min_val = *min_element(lst.begin(), lst.end());
+        int min_val = *std::min_element(lst.begin(), lst.end());
         result.push_back(min_val);
-        lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
+        lst.erase(std::remove(lst.begin(), lst.end(), min_val), lst.end());
 
         if (!lst.empty()) {
-            int max_val = *max_element(lst.begin(), lst.end());
+            int max_val = *std::max_element(lst.begin(), lst.end());
             result.push_back(max_val);
-            lst.erase(remove(lst.begin(), lst.end(), max_val), lst.end());
+            lst.erase(std::remove(lst.begin(), lst.end(), max_val), lst.end());
         }
     }
     return result;
 }
 
 int main() {
-    std::vector<int> input = {5, 2, 8, 3, 1};
-    std::vector<int> output = strange_sort_list(input);
-    
-    for (int i : output) {
-        std::cout << i << " ";
+    std::vector<int> input;
+    int n;
+    std::cout << "Enter the number of elements: ";
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        std::cout << "Enter element " << i + 1 << ": ";
+        int num;
+        std::cin >> num;
+        input.push_back(num);
     }
+
+    std::vector<int> output = strange_sort_list(input);
+
+    std::cout << "Sorted list: ";
+    for (int val : output) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
