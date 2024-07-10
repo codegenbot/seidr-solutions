14fc1f1b5c9a425874c29a123ced383bba0dba99
beep boop <githubbot@vadim.me>
2024-07-10 06:09:01 +0200

    Solution 75 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/n2wga4wg Prompt: /usr/include/c++/11/bits/basic_string.h:760:30: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   760 |                   _M_destroy(_M_allocated_capacity);       |                              ^~~~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/bits/basic_string.h:765:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   765 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -21,21 +20,12 @@ int main_func() {
     double b;
     string str;
     cout << "Enter a floating point number or a string: ";
-    cin >> b; // This will be read as a double if possible
+    cin >> b; 
 
     if (cin.fail()) { 
         cin.clear();
         cin >> ws;
-        try {
-            std::stringstream ss; // use stringstream for this
-            ss << b;
-            str = ss.str();
-            b = stod(str);  // now str is a valid double string that can be converted back to a double
-            cout << "The result is: " << compare_one(a, b) << endl;
-        } catch (invalid_argument& e) {
-            int temp = stoi(str); 
-            cout << "The result is: " << compare_one(a, temp) << endl;
-        }
+        cout << "The result is: " << compare_one(a, 0) << endl;
     } else {
         cout << "The result is: " << compare_one(a, b) << endl;
     }
