d434d90865cb19dcc7144058bec532413c9375fa
beep boop <githubbot@vadim.me>
2024-07-10 23:57:07 +0200

    Solution 22 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xx7jwekz Prompt: dec75dc9-473c-4a86-b055-ce7f7066f989.cpp:3:6: error: ‘string’ in namespace ‘std’  does not name a type     3 | std::string words_in_sentence(std::string sentence) {       |      ^~~~~~ dec75dc9-473c-4a86-b055-ce7f7066f989.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | using namespace std; dec75dc9-473c-4a86-b055-ce7f7066f989.cpp:28:6: error: ‘vector’ in namespace ‘std ’ does not name a template type    28 | std::vector<std::string> split(std::string sentence, char delimiter) {       |      ^~~~~~ dec75dc9-473c-4a86-b055-ce7f7066f989.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | using namespace std; In file included from dec75dc9-473c-4a86-b055-ce7f7066f989.cpp:46: dec75dc9-473c-4a86-b055-ce7f7066f989.cpp: In function ‘int main()’: dec75dc9-473c-4a86-b055-ce7f7066f989.cpp:48:13: error: ‘words_in_sentence’ was n ot declared in this scope    48 |     assert (words_in_sentence("here is") == "is");       |             ^~~~~~~~~~~~~~~~~

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,13 +1,47 @@
-string words_in_sentence(string sentence) {
-    vector<string> words = split(sentence, ' ');
-    string result;
+#include <string>
+#include <vector>
+
+using namespace std;
+
+std::string words_in_sentence(std::string sentence) {
+    vector<std::string> words = split(sentence, ' ');
+    std::string result;
     
     for (int i = 0; i < words.size(); i++) {
-        words[i].erase(remove(words[i].begin(), words[i].end(), ' '), words[i].end());
-        if (!words[i].empty() && is_prime(words[i].length())) {
+        if (is_prime(words[i].length())) {
             result += words[i] + " ";
         }
     }
     
     return result.substr(0, result.length() - 1);
+}
+
+bool is_prime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::vector<std::string> split(std::string sentence, char delimiter) {
+    std::vector<std::string> result;
+    std::string temp;
+
+    for (int i = 0; i < sentence.length(); i++) {
+        if (sentence[i] == delimiter) {
+            result.push_back(temp);
+            temp = "";
+        } else {
+            temp += sentence[i];
+        }
+    }
+
+    result.push_back(temp);
+
+    return result;
 }
