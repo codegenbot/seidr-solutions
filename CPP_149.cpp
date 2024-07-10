7fed49123c4bd60cabf8c3bd5b271dc94ecbc913
beep boop <githubbot@vadim.me>
2024-07-10 19:32:00 +0200

    Solution 7 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/4zoeewsz Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 61671fcf-9a8d-4a99-9136-bc679cc72890.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 61671fcf-9a8d-4a99-9136-bc679cc72890.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 61671fcf-9a8d-4a99-9136-bc679cc72890.cpp:11: 61671fcf-9a8d-4a99-9136-bc679cc72890.cpp: In function ‘int main()’: 61671fcf-9a8d-4a99-9136-bc679cc72890.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 61671fcf-9a8d-4a99-9136-bc679cc72890.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -2,9 +2,12 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> solve(std::vector<std::string> lst) {
-    lst.erase(std::remove_if(lst.begin(), lst.end(), [](const std::string& s) { return s.length() % 2 != 0; }), lst.end());
-    std::sort(lst.begin(), lst.end(), [](const std::string& a, const std::string& b) {
+std::vector<string> sorted_list_sum(std::vector<string> a, std::vector<string> b) {
+    a.insert(a.end(), b.begin(), b.end());
+    std::vector<string> lst = a;
+
+    lst.erase(std::remove_if(lst.begin(), lst.end(), [](const string& s) { return s.length() % 2 != 0; }), lst.end());
+    std::sort(lst.begin(), lst.end(), [](const string& a, const string& b) {
         if (a.length() == b.length()) {
             return a < b;
         }
