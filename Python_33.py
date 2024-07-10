6301794a8bcc98b417ffab69337861a9ac46c4f5
beep boop <githubbot@vadim.me>
2024-07-10 19:22:05 +0200

    Solution 28 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4wfttiik Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b21c7ca2-9b34-4441-9084-604f2ca10 7a0.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b21c7ca2-9b34-4441-9084-604f2ca10 7a0.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b21c7ca2-9b34-4441-9084-604f2ca10 7a0.py", line 2, in sort_third     return sorted([i for i in l if i % 3 != 0]) + [x[-1] for x in [[i for i in l [:j+1] if i % 3 == 0]] if j % 3 == 0]   ^ NameError: name 'j' is not defined

--- Python_33.py
+++ Python_33.py
@@ -1,2 +1,4 @@
 def sort_third(l: list):
-    return sorted([i for i in l if i % 3 != 0]) + [[x for x in [y[-1] for y in [l[i:i+int(len(l)/3)+1] if i%3==0]]][0]]
+    return sorted([i for i in l if i % 3 != 0]) + [
+        l[i - 1] for i in range(1, len(l), 3) if (i - 1) % 3 == 0
+    ]
