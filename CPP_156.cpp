97463aa385cc8963f716a93adf50640f1b3f24e4
beep boop <githubbot@vadim.me>
2024-07-10 11:09:44 +0200

    Solution 59 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mudgt7ti Prompt: Complete the following code given the task description and function signature.

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,11 +1,13 @@
 string int_to_mini_roman(int number) {
-    vector<pair<int, string>> romanValues = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+    vector< pair<int,string> > romanValues = {{1000,"M"},{900,"CM"},{500,"D"},
+                                            {400,"CD"},{100,"C"},{90,"XC"},
+                                            {50,"L"},{40,"XL"},{10,"X"},
+                                            {9,"IX"},{5,"V"},{4,"IV"},{1,"I"}};
     string roman = "";
-    for (const auto& pair : romanValues) {
-        while (number >= pair.first) {
-            number -= pair.first;
-            roman += pair.second;
+    for (const auto &value : romanValues) {
+        while (number >= value.first) {
+            roman += value.second;
+            number -= value.first;
         }
     }
     return roman;
