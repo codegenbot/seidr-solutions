818944efa0d8e672584eb425a23932f2a6379eb9
beep boop <githubbot@vadim.me>
2024-07-10 20:32:47 +0200

    Solution 44 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vmj9u5tm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/82c55857-17f0-4b3e-beaf-b4bb54e2c 431.py", line 2     return ["0.0"] * len(numbers)     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_21.py
+++ Python_21.py
@@ -1,6 +1,4 @@
-def find_average(numbers):
-    min_val = min(numbers)
-    max_val = max(numbers)
-    if (max_val - min_val) == 0:
+```
+def solve_task(numbers):
+    if (max(numbers) - min(numbers)) == 0:
         return ["0.0"] * len(numbers)
-    return [(str(num) + ".0") for num in numbers]
