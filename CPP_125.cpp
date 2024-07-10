53f685ef349946a9bc59b8e654e4970b5f14dad5
beep boop <githubbot@vadim.me>
2024-07-11 00:03:53 +0200

    Solution 70 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rho8j4le Prompt: f32c6a88-ed34-487f-8e99-3704914db18b.cpp: In function ‘int main()’: f32c6a88-ed34-487f-8e99-3704914db18b.cpp:44:5: error: ‘assert’ was not declared in this scope    44 |     assert(isSame(split_words(""), vector<string>({"0"})));       |     ^~~~~~ f32c6a88-ed34-487f-8e99-3704914db18b.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <string>   +++ |+#include <cassert>     3 | using namespace std; f32c6a88-ed34-487f-8e99-3704914db18b.cpp: At global scope: f32c6a88-ed34-487f-8e99-3704914db18b.cpp:57:5: error: redefinition of ‘int main( )’    57 | int main(){       |     ^~~~ f32c6a88-ed34-487f-8e99-3704914db18b.cpp:43:5: note: ‘int main()’ previously def ined here    43 | int main() {       |     ^~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,12 +1,19 @@
-#include <cassert>
 #include <vector>
 #include <string>
-#include <cctype>
+#include <cassert>
 
 using namespace std;
 
-bool issame(const vector<string>& a) {
-    return (a.size() == 1 && a[0] == "0");
+bool isSame(const vector<string>& a, const vector<string>& b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 vector<string> split_words(string txt) {
@@ -36,6 +43,6 @@ vector<string> split_words(string txt) {
 }
 
 int main() {
-    assert(issame(split_words("")) == true);
+    assert(isSame(split_words(""), vector<string>({"0"})));
     return 0;
 }
