e307691075a48ebd3051e6ae7791fd821e50cc3e
beep boop <githubbot@vadim.me>
2024-07-11 01:25:16 +0200

    Solution 16 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/tmnr21o2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8c124aec-93fa-498d-8b0d-b05226eca 666.py", line 19, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8c124aec-93fa-498d-8b0d-b05226eca 666.py", line 14, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8c124aec-93fa-498d-8b0d-b05226eca 666.py", line 2, in sort_even     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8c124aec-93fa-498d-8b0d-b05226eca 666.py", line 4, in <listcomp>     for i, (x, y) in enumerate(            ^^^^^^ ValueError: too many values to unpack (expected 2)

--- Python_37.py
+++ Python_37.py
@@ -1,8 +1,4 @@
-def sort_even(lst):
-    even_numbers = sorted([x for x in lst if x % 2 == 0])
-    odd_numbers = sorted([x for x in lst if x % 2 != 0])
-
-    return [
-        x if i < len(even_numbers) else y
-        for i, (x, y) in enumerate(zip(even_numbers, odd_numbers))
-    ]
+def sort_even(l: list):
+    evens = sorted([x for x in l if l.index(x) % 2 == 0])
+    odds = [x for x in l if l.index(x) % 2 != 0]
+    return [evens.pop(0) if i % 2 == 0 else odds.pop(0) for _, i in enumerate(l)]
