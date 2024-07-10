86ef82344ed2ec3c6170d9d12eb1ae908562475b
beep boop <githubbot@vadim.me>
2024-07-10 22:26:55 +0200

    Solution 43 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/gajkdsd6 Prompt:    38 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 9924eb01-7ac1-48c5-96b0-4cfa60c884e7.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ 9924eb01-7ac1-48c5-96b0-4cfa60c884e7.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from 9924eb01-7ac1-48c5-96b0-4cfa60c884e7.cpp:37: 9924eb01-7ac1-48c5-96b0-4cfa60c884e7.cpp: In function ‘int main()’: 9924eb01-7ac1-48c5-96b0-4cfa60c884e7.cpp:47:20: error: ‘rescale_to_unit’ was not  declared in this scope    47 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 9924eb01-7ac1-48c5-96b0-4cfa60c884e7.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 bool issame(vector<float> a, vector<float> b);
-
 vector<float> rescale_to_unit(const vector<float>& numbers);
 
 bool issame(vector<float> a, vector<float> b) {
@@ -35,7 +34,7 @@ vector<float> rescale_to_unit(const vector<float>& numbers) {
 }
 
 int main() {
-    assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), {0.25, 0.0, 1.0, 0.5, 0.75}));
+    assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), {0.25, 0.0, 1.0, 0.5, 0.75});
     
     return 0;
 }
