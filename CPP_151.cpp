989f751bbbfb00b21bd7f55034a4ed9a73ec2549
beep boop <githubbot@vadim.me>
2024-07-11 00:44:22 +0200

    Solution 14 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jafm6m1z Prompt: 6a01dc4b-e386-4ce2-b580-9772ff514b0c.cpp: At global scope: 6a01dc4b-e386-4ce2-b580-9772ff514b0c.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 6a01dc4b-e386-4ce2-b580-9772ff514b0c.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~ In file included from 6a01dc4b-e386-4ce2-b580-9772ff514b0c.cpp:28: 6a01dc4b-e386-4ce2-b580-9772ff514b0c.cpp: In function ‘int main()’: 6a01dc4b-e386-4ce2-b580-9772ff514b0c.cpp:30:35: error: ‘lst’ was not declared in  this scope    30 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 6a01dc4b-e386-4ce2-b580-9772ff514b0c.cpp:30:38: error: ‘double_the_difference’ c annot be used as a function    30 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ 6a01dc4b-e386-4ce2-b580-9772ff514b0c.cpp:30:43: error: ‘odd_sum’ was not declare d in this scope    30 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,31 +1,29 @@
+```cpp
+int odd_sum = 0; 
+
 long long double_the_difference(vector<float> lst){
     long long sum = 0;
     for (float num : lst) {
         if (num > 0 && modf(num, &num) == 0.0) {
             sum += pow(num, 2);
+        } else {
+            odd_sum += num;
         }
     }
     return sum;
-
 }
 
 int main() {
     vector<float> lst; 
-    long long odd_sum = 0;
-    
-    cout << "Enter numbers (enter 'stop' to finish):" << endl;
-    while(1) {
-        float num;
-        
-        cin >> num;
-        
-        if(std::to_string(num) == "stop") break;
-
-        if(num > 0 && modf(fmod(num, 1), &num) == 0.0) {
-            odd_sum = double_the_difference(lst);
-        }
-        lst.push_back(num);    
+    cin >> lst.size();
+    for (int i = 0; i < lst.size(); i++) {
+        float x;
+        cin >> x;
+        lst.push_back(x);
     }
+    
+    long long result = double_the_difference(lst);
+    cout << result;
 
-    cout << "Sum of squared values: " << odd_sum << endl;
+    return 0;
 }
