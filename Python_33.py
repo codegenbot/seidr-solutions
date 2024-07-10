2c0f077b38c018e6bcc6d98b791b7c1aca64c0dd
beep boop <githubbot@vadim.me>
2024-07-10 19:38:14 +0200

    Solution 40 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4wfttiik Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/24993783-a50f-44f1-bc43-7959163dc 439.py", line 2     return sorted([i for i in l if i % 3 != 0]) + [[x for x in [y[-1] for y in [ l[i:i+int(len(l)/3)+1] if i%3==0]]][0]] ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: expected 'else' after 'if' expression

--- Python_33.py
+++ Python_33.py
@@ -1,5 +1,4 @@
-```Python
 def sort_third(l: list):
-    first = [i for i in l if i % 3 != 0]
-    second = [[y[-1] for y in [l[i:i+int(len(l)/3)+1] if i%3==0]][0] if any(i%3==0 for i in range(0, len(l), 3)) else []
-    return sorted(first) + [second]
+    return sorted([i for i in l if i % 3 != 0]) + (
+        l[::3][-1] if len(l) % 3 == 0 else []
+    )
