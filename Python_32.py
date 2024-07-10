d64bfea8697dd627debc66f37d8db582293f025a
beep boop <githubbot@vadim.me>
2024-07-10 19:00:49 +0200

    Solution 25 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/o04iuf34 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/53a64d1b-e5db-4f51-a781-025887c29 83b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,13 +1,11 @@
 def find_roots(a: float, b: float, c: float):
-    disc = b**2 - 4 * a * c
-    if disc < 0:
+    d = b**2 - 4 * a * c
+    if d < 0:
         return "No real roots"
-    elif disc == 0:
-        root = -b / (2 * a)
-        return (
-            f"The quadratic equation {a}x^2 + {b}x + {c} has one repeated root: {root}"
-        )
+    elif d == 0:
+        root = (-b + math.sqrt(d)) / (2 * a)
+        return [root]
     else:
-        root1 = (-b + math.sqrt(disc)) / (2 * a)
-        root2 = (-b - math.sqrt(disc)) / (2 * a)
-        return f"The quadratic equation {a}x^2 + {b}x + {c} has two distinct roots: {root1} and {root2}"
+        r1 = (-b + math.sqrt(d)) / (2 * a)
+        r2 = (-b - math.sqrt(d)) / (2 * a)
+        return sorted([r1, r2])
