b40fba95ea16cdbf4ceb2ddcf46862b7a5df8378
beep boop <githubbot@vadim.me>
2024-07-10 08:18:33 +0200

    Solution 10 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/m9ihdebv Prompt:       |                    ^~~ d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp:10: d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp: In function ‘int main()’: d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp:20:20: error: ‘compare’ was not declare d in this scope    20 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ d7006e3f-dff5-4e9c-96ef-b36a061f68ca.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,13 +1,63 @@
-Here is the completed code:
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+bool issame(vector<int> game, vector<int>) {
+    for (int i = 0; i < game.size(); i++) {
+        if (game[i] != i + 1) return false;
+    }
+    return true;
+}
 
 vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
     for (int i = 0; i < game.size(); i++) {
-        if (game[i] == guess[i]) {
-            result.push_back(0);
+        int diff = abs(guess[i] - game[i]);
+        if (diff == 0) {
+            result.push_back(1);
+        } else if (guess[i] > game[i]) {
+            result.push_back(2);
         } else {
-            result.push_back(abs(game[i] - guess[i]));
+            result.push_back(3);
         }
     }
     return result;
+}
+
+int main() {
+    vector<int> game, guess;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x != i + 1) {
+            cout << "Invalid input. The number should be from 1 to " << n << endl;
+            return -1;
+        }
+        game.push_back(x);
+    }
+    
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x < 1 || x > n) {
+            cout << "Invalid input. The number should be from 1 to " << n << endl;
+            return -1;
+        }
+        guess.push_back(x);
+    }
+    
+    vector<int> res = compare(game, guess);
+    if (!issame(game, guess)) {
+        for (int i = 0; i < n; i++) {
+            cout << res[i] << " ";
+        }
+        cout << endl;
+    } else {
+        cout << "Yes" << endl;
+    }
+    
+    return 0;
 }
