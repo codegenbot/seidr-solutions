6472015831d5eeaaa4c40aae6aa179d55a06f724
beep boop <githubbot@vadim.me>
2024-07-10 04:53:32 +0200

    Solution 94 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/5lwwp8ur Prompt: a824cc3f-ad7a-4aa5-9ab7-a389811ac29b.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ a824cc3f-ad7a-4aa5-9ab7-a389811ac29b.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from a824cc3f-ad7a-4aa5-9ab7-a389811ac29b.cpp:28: a824cc3f-ad7a-4aa5-9ab7-a389811ac29b.cpp: In function ‘int main()’: a824cc3f-ad7a-4aa5-9ab7-a389811ac29b.cpp:38:20: error: ‘sum_product’ was not dec lared in this scope; did you mean ‘my_namespace::sum_product’?    38 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ a824cc3f-ad7a-4aa5-9ab7-a389811ac29b.cpp:10:22: note: ‘my_namespace::sum_product ’ declared here    10 |     std::vector<int> sum_product(std::vector<int> nums) {       |                      ^~~~~~~~~~~ In file included from a824cc3f-ad7a-4aa5-9ab7-a389811ac29b.cpp:28: a824cc3f-ad7a-4aa5-9ab7-a389811ac29b.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -3,24 +3,21 @@
 #include <cassert>
 
 namespace my_namespace {
-    bool issame(const std::vector<int>& a, const std::vector<int>& b);
-    std::vector<int> sum_product(std::vector<int> nums);
-}
-
-bool my_namespace::issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
-}
-
-std::vector<int> my_namespace::sum_product(std::vector<int> nums) {
-    int sum = 0;
-    int product = 1;
-
-    for(int num : nums) {
-        sum += num;
-        product *= num;
+    bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+        return a == b;
     }
 
-    return std::vector<int>{sum, product};
+    std::vector<int> sum_product(std::vector<int> nums) {
+        int sum = 0;
+        int product = 1;
+
+        for(int num : nums) {
+            sum += num;
+            product *= num;
+        }
+
+        return std::vector<int>{sum, product};
+    }
 }
 
 int main() {
