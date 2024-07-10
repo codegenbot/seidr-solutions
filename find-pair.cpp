79cc612dc6273c1e687927618d61a7c68dbf51ef
beep boop <githubbot@vadim.me>
2024-07-10 06:56:18 +0200

    Solution 20 of find-pair, ratio 0.96 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/rnzo0sbm Prompt: n before ‘int’    11 |     map<int, int> mp;       |         ^~~ 577b340c-1c4f-4070-9641-5f53fca579ef.cpp:13:35: error: ‘nums’ was not declared i n this scope    13 |         int complement = target - nums[i];       |                                   ^~~~ 577b340c-1c4f-4070-9641-5f53fca579ef.cpp:14:13: error: ‘mp’ was not declared in this scope    14 |         if (mp.find(complement) != mp.end()) {       |             ^~ 577b340c-1c4f-4070-9641-5f53fca579ef.cpp:15:13: error: ‘cout’ was not declared i n this scope    15 |             cout << complement << endl;       |             ^~~~ 577b340c-1c4f-4070-9641-5f53fca579ef.cpp:15:35: error: ‘endl’ was not declared i n this scope    15 |             cout << complement << endl;       |                                   ^~~~ 577b340c-1c4f-4070-9641-5f53fca579ef.cpp:19:9: error: ‘mp’ was not declared in t his scope    19 |         mp[nums[i]] = i;       |         ^~

--- find-pair.cpp
+++ find-pair.cpp
@@ -2,23 +2,27 @@
 #include <vector>
 #include <map>
 
+using namespace std;
+
 int main() {
-    int n, target;
-    std::cin >> n;
-    std::vector<int> nums(n);
+    int n;
+    cin >> n;
+    vector<int> nums(n);
     for (int i = 0; i < n; ++i) {
-        std::cin >> nums[i];
+        cin >> nums[i];
     }
-    std::cin >> target;
+    int target;
+    cin >> target;
     
-    std::map<int, int> hash;
+    map<int, int> mp;
     for (int i = 0; i < n; ++i) {
         int complement = target - nums[i];
-        if (hash.find(complement) != hash.end()) {
-            std::cout << complement << std::endl << nums[i];
+        if (mp.find(complement) != mp.end()) {
+            cout << complement << endl;
+            cout << nums[i] << endl;
             break;
         }
-        hash[nums[i]] = i;
+        mp[nums[i]] = i;
     }
     
     return 0;
