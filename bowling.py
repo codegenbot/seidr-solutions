0d6e80f0a2f1f8ef069bdee0f2778ad5c4cf787d
beep boop <githubbot@vadim.me>
2024-07-10 19:00:18 +0200

    Solution 16 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/7n8lk0nu Prompt: it must return 111 for input 129/16X44XX8-44-2, but it returns .

--- bowling.py
+++ bowling.py
@@ -1,31 +1,26 @@
 def bowling_score(game):
     score = 0
     roll = 0
-    game_frames = [int(x) for x in game.split('/') if x.isdigit()]
-    game_spare_strike = [x.replace('!', '').replace('X', '') for x in game.split('/')]
-    
-    for i, frame in enumerate(game_frames):
-        if len(str(frame)) == 1:
-            score += frame
+    for frame in game.split('/'):
+        if len(frame) == 1:
+            score += int(frame)
             roll += 1
-        elif len(str(frame)) == 2:
-            first_roll, second_roll = map(int, str(frame))
+        elif len(frame) == 2:
+            first_roll, second_roll = map(int, frame)
             score += first_roll + second_roll
             roll += 1
         else:
-            first_roll = frame
-            if first_roll == 10: 
-                score += first_roll
+            first_roll = int(frame[0])
+            score += first_roll
+            if first_roll == 10:
                 roll += 1
-                if i < len(game_frames) - 1 and game_spare_strike[i] != 'X':
-                    score += game_frames[i + 1]
-                    roll += 1
-                elif i < len(game_frames) - 2:
-                    score += sum(game_frames[i + 1:i + 3])
-                    roll += 2
-            else: 
-                if i < len(game_frames) - 1:
-                    score += first_roll + game_frames[i + 1]
-                    roll += 2
-    
+                next_frame = game.split('/')[game.split('/').index(frame)+1].split()
+                score += sum(map(int, [next_frame[0]] + next_frame[1:]))
+            else:
+                if len(game.split('/')[game.split('/').index(frame)+1]) > 1 and int(game.split('/')[game.split('/').index(frame)+1][0]) + int(game.split(['/frame]+1][1]) < 10:
+                    score += first_roll + int(game.split('/')[game.split('/').index(frame)+1][1])
+                else:
+                    next_rolls = map(int, game.split('/')[game.split('/').index(frame)+1].split()[:2])
+                    score += sum(next_rolls)
+                roll += 1
     return score if roll == 10 else 100
