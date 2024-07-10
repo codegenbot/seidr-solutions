5f5a48f2e01478386a87775f26cba743a79ac766
beep boop <githubbot@vadim.me>
2024-07-10 11:15:39 +0200

    Solution 32 of Python/95, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/d3fb0q4o Prompt: Complete the following code given the task description and function signature.

--- Python_95.py
+++ Python_95.py
@@ -2,5 +2,6 @@
 def check_dict_case(dict):
     if not dict:
         return False
-    case = all(isinstance(key, str) and key.islower() for key in dict)
-    return case or all(isinstance(key, str) and key.isupper() for key in dict)
+    all_upper = all(isinstance(key, str) and key.isupper() for key in dict)
+    all_lower = all(isinstance(key, str) and key.islower() for key in dict)
+    return all_upper or all_lower
