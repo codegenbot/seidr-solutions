185cc423d0ab793617df5fdab66dce05232dce27
beep boop <githubbot@vadim.me>
2024-07-10 04:54:12 +0200

    Solution 78 of CPP/83, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ziclxyyi Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -1,28 +1,16 @@
-```cpp
 #include <iostream>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
 int starts_one_ends(int n) {
     int count = 0;
     for (long long i = 1; i <= pow(10, n-1); i++) {
-        string str;
+        string str = to_string(i);
         bool start_with_one = false, end_with_one = false;
-        for (auto &c : str) {
-            c = (i % 2 == 0 ? '0' : '1');
-            if (c == '1') {
-                start_with_one = true;
-                break;
-            }
-        }
-        for (int j = n - 1; j >= 0; j--) {
-            str += (i % 2 == 0 ? '0' : '1');
-            if (str[j] == '1') {
-                end_with_one = true;
-                break;
-            }
-        }
+        if (str[0] == '1') start_with_one = true;
+        if (str.back() == '1') end_with_one = true;
         if ((start_with_one || i == 1) && (end_with_one || i == pow(10, n-1))) {
             count++;
         }
