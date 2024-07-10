d857010426b645b56dec804ad316d16a94aba0ca
beep boop <githubbot@vadim.me>
2024-07-10 21:11:14 +0200

    Solution 46 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pc4qhgax Prompt: [1, 'a', 1, 'a', 1, 'a', 'a', 'b', 'c'] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/40f2b1bb-1833-4ab0-9481-989a332fd 155.py", line 22, in <module>     check(intersperse)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/40f2b1bb-1833-4ab0-9481-989a332fd 155.py", line 19, in check     assert intersperse([2, 2, 2], 2) == [2, 2, 2, 2, 2]            ^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/40f2b1bb-1833-4ab0-9481-989a332fd 155.py", line 4, in intersperse     min_len = min(len(list(a)), len(list(b)))                                     ^^^^^^^ TypeError: 'int' object is not iterable

--- Python_5.py
+++ Python_5.py
@@ -3,16 +3,17 @@ def intersperse(a, b):
     result = []
     if hasattr(a, '__iter__'):
         min_len = min(len(list(a)), len(list(b)))
-        for i in range(min_len):
-            result.append(next(iter(a)))
-            result.append(b)
     else:
-        for _ in range(len(list(b))):
-            result.append(a)
-            result.append(b)
+        min_len = 0
+    for i in range(min_len):
+        result.append(next(iter(a)))
+        result.append(next(iter(b)))
+    result.extend([a] * (min_len - len(list(a))))
+    if a != b:
+        result.extend([b] * ((len(result) // 2) + len(list(a)) % 2))
     return result
 
-list1 = 2
-list2 = 2
-result = intersperse(list1, list2)
+list1 = [1, 3, 5]
+list2 = ['a', 'b', 'c']
+result = intersperse(list1 + list2[:], list2)
 print(result)
