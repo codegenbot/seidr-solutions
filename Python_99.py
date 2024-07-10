0e5991c01ab89e4588972ae5bf4d4dcc226faa35
beep boop <githubbot@vadim.me>
2024-07-10 16:47:15 +0200

    Solution 12 of Python/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/4eawga2p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fdc9406d-d562-4b3b-bb84-2956c3da0 1a2.py", line 14, in <module>     check(closest_integer)           ^^^^^^^^^^^^^^^ NameError: name 'closest_integer' is not defined

--- Python_99.py
+++ Python_99.py
@@ -1,7 +1,9 @@
-def closest_integer_to_month(value): 
+def closest_integer_to_month(value):
     months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     month_number = int(input("Enter the number of the month (1-12): "))
     if month_number < 1 or month_number > 12:
         return "Invalid input. Please enter a number between 1 and 12."
     else:
         return months[month_number - 1]
+
+closest_integer_to_month(5)
