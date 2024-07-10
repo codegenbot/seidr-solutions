e00cc15feb60b74059bbba576337dde6a3099efa
beep boop <githubbot@vadim.me>
2024-07-10 18:52:29 +0200

    Solution 31 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/bdlkg0p4 Prompt:       |                           ^~~~~~ c2d43832-e392-4787-a520-ebde2fe474ec.cpp:29:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from c2d43832-e392-4787-a520-ebde2fe474ec.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from c2d43832-e392-4787-a520-ebde2fe474ec.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c2d43832-e392-4787-a520-ebde2fe474ec.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c2d43832-e392-4787-a520-ebde2fe474ec.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ c2d43832-e392-4787-a520-ebde2fe474ec.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main(){       |     ^~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -16,19 +16,5 @@ std::vector<int> make_a_pile(int n){
 }
 
 bool issame(std::vector<int> a, std::vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(size_t i = 0; i < a.size(); ++i){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
-int main(){
-    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
-    
-    return 0;
+    return a == b;
 }
