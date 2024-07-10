4e2d0c63bd458a5f90ea86319c528886124b7192
beep boop <githubbot@vadim.me>
2024-07-11 00:32:38 +0200

    Solution 10 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jafm6m1z Prompt: d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp: At global scope: d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~ In file included from d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp:32: d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp: In function ‘int main()’: d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp:34:35: error: ‘lst’ was not declared in  this scope    34 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp:34:38: error: ‘double_the_difference’ c annot be used as a function    34 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp:34:43: error: ‘odd_sum’ was not declare d in this scope    34 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,8 +1,35 @@
+```cpp
 long long double_the_difference(vector<float> lst){
     long long sum = 0;
-    for(auto x : lst){
-        if(int(x) > 0 && int(x) == (int)x){
-            sum += pow(int(x), 2);
+    for (float num : lst) {
+        if (num > 0 && modf(num, &num) == 0.0) {
+            sum += pow(num, 2);
         }
     }
     return sum;
+
+}
+
+int main() {
+    int odd_sum = 0;
+    float num;
+    
+    cout << "Enter numbers (enter 'stop' to finish):" << endl;
+    while(1) {
+        cin >> num;
+        
+        if(std::to_string(num) == "stop") break;
+
+        if(num > 0 && modf(num, &num) == 0.0) {
+            vector<float> lst;
+            for (float n : lst) {
+                if (n > 0 && modf(n, &n) == 0.0) {
+                    odd_sum = double_the_difference(lst);
+                }
+            }
+        }
+        lst.push_back(num);    
+    }
+
+    cout << "Sum of squared values: " << odd_sum << endl;
+}
