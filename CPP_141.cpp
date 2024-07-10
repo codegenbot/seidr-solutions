da6781281d238348d711519d60f60d60ee21c664
beep boop <githubbot@vadim.me>
2024-07-10 11:09:26 +0200

    Solution 76 of CPP/141, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/rtsa7yfm Prompt: Complete the following code given the task description and function signature.

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,20 +1,34 @@
-string file_name_check(string file_name){
+#include <string>
+
+using namespace std;
+
+string file_name_check(string file_name) {
     int digit_count = 0;
     bool found_dot = false;
-    for(int i=0; i<file_name.length(); i++){
-        if(file_name[i] >= '0' && file_name[i] <= '9'){
+    string first_part = "";
+    
+    for (char c : file_name) {
+        if (isdigit(c)) {
             digit_count++;
-            if(digit_count > 3) return "No";
-        }
-        else if(file_name[i] == '.'){
+            if (digit_count > 3)
+                return "No";
+        } else if (c == '.') {
             found_dot = true;
-        }
-        else if(!found_dot && !isalpha((unsigned char)file_name[i])){
-            return "No";
+        } else if (!found_dot) {
+            first_part += c;
         }
     }
-    if(!found_dot) return "No";
-    string extension = file_name.substr(file_name.find('.')+1);
-    if(extension != "txt" && extension != "exe" && extension != "dll") return "No";
-    return "Yes";
+    
+    if (!first_part.size() || !isalpha(first_part[0]))
+        return "No";
+    
+    string second_part = file_name.substr(file_name.find('.') + 1);
+    
+    if (second_part != "txt" && second_part != "exe" && second_part != "dll")
+        return "No";
+    
+    if (found_dot)
+        return "Yes";
+    else
+        return "No";
 }
