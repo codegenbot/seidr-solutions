d89380b866892489b01177ff33526aad43da5980
beep boop <githubbot@vadim.me>
2024-07-10 23:10:55 +0200

    Solution 94 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/d5osglcb Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,42 +1,10 @@
-map<string, int> num_to_val = {
-    {"zero", 0},
-    {"one", 1},
-    {"two", 2},
-    {"three", 3},
-    {"four", 4},
-    {"five", 5},
-    {"six", 6},
-    {"seven", 7},
-    {"eight", 8},
-    {"nine", 9}
-};
-
-map<int, string> val_to_num;
-for (auto it = num_to_val.begin(); it != num_to_val.end(); it++) {
-    val_to_num[it->second] = it->first;
+map<string, int> number_map = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
+map<int, string> reverse_map;
+for (auto& pair : number_map) {
+    reverse_map[pair.second] = pair.first;
 }
-
-vector<int> nums;
-string curr_num;
-for (int i = 0; i < numbers.length(); i++) {
-    if (numbers[i] != ' ') {
-        curr_num += numbers[i];
-    } else {
-        nums.push_back(num_to_val[curr_num]);
-        curr_num = "";
-    }
+string result = "";
+for (int i = 0; i < numbers.length(); i += 6) {
+    result += reverse_map[number_map[numbers.substr(i, 5)]] + " ";
 }
-nums.push_back(num_to_val[curr_num]);
-
-sort(nums.begin(), nums.end());
-
-string sorted_numbers;
-for (int i = 0; i < nums.size(); i++) {
-    sorted_numbers += val_to_num[nums[i]];
-    if (i != nums.size() - 1) {
-        sorted_numbers += " ";
-    }
-}
-
-return sorted_numbers;
-}
+return result;
