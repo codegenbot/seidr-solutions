4b4e24c0a9d1df50e07957394d9c7474a15a4f3b
beep boop <githubbot@vadim.me>
2024-07-10 21:10:52 +0200

    Solution 17 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/1li5iway Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/46c5bb7d-94c2-45a3-9806-2bb6ea343 95a.py", line 11, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/46c5bb7d-94c2-45a3-9806-2bb6ea343 95a.py", line 8, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_41.py
+++ Python_41.py
@@ -1,6 +1,6 @@
 def car_race_collision(n: int):
     if not isinstance(n, int) or n < 1:
         return "Invalid input. Please enter a positive integer."
-    for i in range(1, n+1):
-        if (i * (3*i - 1)) // 2 == n:
-            return [i]
+    for i in range(1, n + 1):
+        if (i * (3 * i - 1)) // 2 == n:
+            return i
