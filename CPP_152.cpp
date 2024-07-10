3c386d5f18ad11e5ee90a4d817e6afc6d8bc328b
beep boop <githubbot@vadim.me>
2024-07-10 08:30:32 +0200

    Solution 42 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/m9ihdebv Prompt:       |                    ^~~ e48eab85-3f1a-49c3-9650-e32c19a06d2d.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e48eab85-3f1a-49c3-9650-e32c19a06d2d.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e48eab85-3f1a-49c3-9650-e32c19a06d2d.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e48eab85-3f1a-49c3-9650-e32c19a06d2d.cpp:13: e48eab85-3f1a-49c3-9650-e32c19a06d2d.cpp: In function ‘int main()’: e48eab85-3f1a-49c3-9650-e32c19a06d2d.cpp:23:20: error: ‘compare’ was not declare d in this scope    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ e48eab85-3f1a-49c3-9650-e32c19a06d2d.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,16 +1,17 @@
-```cpp
-#include <vector>
 #include <vector>
 bool issame(vector<int> a,vector<int>b){
     return (a==b);
 }
+
 vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
     for (int i = 0; i < game.size(); i++) {
-        if (issame({game[i], guess[i]}, {guess[i], game[i]})) {
+        if (issame({game[i],guess[i]}, {game[i],game[i]})) {
             result.push_back(0);
-        } else {
+        } else if (issame({game[i],guess[i]}, {guess[i],game[i]})) {
             result.push_back(abs(guess[i] - game[i]));
+        } else {
+            result.push_back(-1);
         }
     }
     return result;
