6aa9560a63e0dc89239a5b4998f887267c031823
beep boop <githubbot@vadim.me>
2024-07-10 07:11:24 +0200

    Solution 98 of Python/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/m7fdemu2 Prompt: Function call is correct Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e5a3f530-7f3b-4825-a825-cfcf27c72 b7c.py", line 15, in <module>     check(incr_list)           ^^^^^^^^^ NameError: name 'incr_list' is not defined

--- Python_42.py
+++ Python_42.py
@@ -1,9 +1,9 @@
 def increment_list_elements(sample_list):
     return [num + 1 for num in sample_list]
 
-def check(increment_list_elements):
+def check(func):
     sample_list = [1, 2, 3]
-    result = increment_list_elements(sample_list)
+    result = func(sample_list)
     assert result == [2, 3, 4]
     print("Function call is correct")
 
