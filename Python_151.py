2823af09b9673479eae90ca72aaf00f579aa8615
beep boop <githubbot@vadim.me>
2024-07-11 01:27:23 +0200

    Solution 44 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/49frezuz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/41941909-ce09-4a18-92f6-7d4f85761 eb0.py", line 1, in <module>     print(check([1, 2, 3], lambda x: double_the_difference(x)))           ^^^^^ NameError: name 'check' is not defined

--- Python_151.py
+++ Python_151.py
@@ -1,5 +1,9 @@
+```
+def double_the_difference(nums):
+    avg = sum(nums) / len(nums)
+    return abs(sum(num - avg for num in nums))
+
 def check(lst, func):
     return func(lst)
-
 
 print(check([1, 2, 3], lambda x: double_the_difference(x)))
