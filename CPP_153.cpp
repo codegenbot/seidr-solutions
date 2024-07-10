c422ee9e04f4963994869c81bf02eac1ef52f42b
beep boop <githubbot@vadim.me>
2024-07-10 05:05:37 +0200

    Solution 5 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8yenuxfu Prompt: 95580cc3-7479-4058-bb3a-0e063f29182b.cpp:1:6: error: ‘string’ in namespace ‘std’  does not name a type     1 | std::string Strongest_Extension(std::string class_name, std::vector<std: :string> extensions){       |      ^~~~~~ 95580cc3-7479-4058-bb3a-0e063f29182b.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | std::string Strongest_Extension(std::string class_name, std::vector<std: :string> extensions){

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,2 +1,4 @@
-```cpp
-return strongest_extension;
+#include <string>
+#include <vector>
+
+std::basic_string<char> Strongest_Extension(std::basic_string<char> class_name, std::vector<std::basic_string<char>> extensions){
