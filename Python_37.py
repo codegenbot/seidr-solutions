8d41fa62e0b9be8b26043fce3848f6d50f992ecb
beep boop <githubbot@vadim.me>
2024-07-10 19:50:33 +0200

    Solution 31 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n709d7eu Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9df36abf-a78f-4e5e-a920-9cb04289e 75d.py", line 20, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9df36abf-a78f-4e5e-a920-9cb04289e 75d.py", line 15, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,7 +1,7 @@
 def sort_even(l: list):
     evens = [x for i, x in enumerate(l) if i % 2 == 0]
     odds = sorted([x for i, x in enumerate(l) if i % 2 != 0])
-
+    
     result = []
     even_index = 0
     for i, x in enumerate(l):
@@ -9,5 +9,5 @@ def sort_even(l: list):
             result.append(evens[even_index])
             even_index += 1
         else:
-            result.append(odds[(i - 1) // 2])
+            result.append(odds[(i-1)//2])
     return tuple(result)
