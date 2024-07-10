0700dc68b5d43dca02dc580e7af9b2b4a41ae320
beep boop <githubbot@vadim.me>
2024-07-10 03:31:34 +0200

    Solution 46 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ctqurfce Prompt: 57c1928b-ddff-4728-9a6e-c46ddcbf5a91.cpp: In function ‘int main()’: 57c1928b-ddff-4728-9a6e-c46ddcbf5a91.cpp:13:5: error: ‘assert’ was not declared in this scope    13 |     assert(std::abs(triangle_area(a, b, c) + 1)<0.01);       |     ^~~~~~ 57c1928b-ddff-4728-9a6e-c46ddcbf5a91.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <cmath>   +++ |+#include <cassert>     3 | 57c1928b-ddff-4728-9a6e-c46ddcbf5a91.cpp: At global scope: 57c1928b-ddff-4728-9a6e-c46ddcbf5a91.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ 57c1928b-ddff-4728-9a6e-c46ddcbf5a91.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -11,7 +11,7 @@ int main() {
     int a, b, c; 
     std::cout << "Enter sides of triangle: ";
     std::cin >> a >> b >> c; 
-    assert(std::abs(triangle_area(a, b, c)) < 0.01);
+    assert(std::abs(triangle_area(a, b, c) + 1)<0.01);
     float result = triangle_area(a, b, c);
     std::cout << "The area of the triangle is: " << static_cast<int>(result) << std::endl;
     return 0;
