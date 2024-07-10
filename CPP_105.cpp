88bf9df6826bd2a842455dea2fa84a9bf761f3cb
beep boop <githubbot@vadim.me>
2024-07-10 18:27:24 +0200

    Solution 8 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dctrev1c Prompt:       |                              ^~~~~~ 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp:28:37: error: ‘string’ was not declared  in this scope    28 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp:28:45: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp:27: 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp: In function ‘int main()’: 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp:37:20: error: ‘by_length’ was not decla red in this scope    37 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 4d7de361-75f9-41c4-a5d0-c2a6e94d80b6.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -2,34 +2,37 @@
 #include <string>
 #include <map>
 #include <algorithm>
-#include <cassert>
 
-std::vector<std::string> by_length(std::vector<int> arr){
-    std::vector<std::string> result;
-    std::vector<int> filtered_arr;
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
+}
 
-    for(int num : arr){
-        if(num >= 1 && num <= 9){
+vector<string> by_length(vector<int> arr) {
+    vector<string> result;
+    vector<int> filtered_arr;
+
+    for (int num : arr) {
+        if (num >= 1 && num <= 9) {
             filtered_arr.push_back(num);
         }
     }
 
-    std::sort(filtered_arr.begin(), filtered_arr.end());
+    sort(filtered_arr.begin(), filtered_arr.end());
 
-    std::reverse(filtered_arr.begin(), filtered_arr.end());
+    reverse(filtered_arr.begin(), filtered_arr.end());
 
-    std::map<int, std::string> num_to_name = {
+    map<int, string> num_to_name = {
         {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
         {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
     };
 
-    for(int num : filtered_arr){
+    for (int num : filtered_arr) {
         result.push_back(num_to_name[num]);
     }
 
     return result;
 }
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    // Your implementation of issame function here
+int main() {
+    assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"});
 }
