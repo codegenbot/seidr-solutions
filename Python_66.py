ea1b946332005484d65238f5d4a362192caa7c93
beep boop <githubbot@vadim.me>
2024-07-10 21:27:58 +0200

    Solution 16 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lck10yx3 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d63fb3da-78e0-4420-9ac5-290093234 cb6.py", line 9, in <module>     check(digitSum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d63fb3da-78e0-4420-9ac5-290093234 cb6.py", line 6, in check     assert digitSum("You arE Very Smart") == 327, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d63fb3da-78e0-4420-9ac5-290093234 cb6.py", line 2, in digitSum     return sum(int(digit) for digit in str(n))            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d63fb3da-78e0-4420-9ac5-290093234 cb6.py", line 2, in <genexpr>     return sum(int(digit) for digit in str(n))                ^^^^^^^^^^ ValueError: invalid literal for int() with base 10: 'Y'

--- Python_66.py
+++ Python_66.py
@@ -1,3 +1,2 @@
-```
 def digitSum(n):
-    return sum(int(digit) for digit in str(n).replace(" ", "").lower() if digit.isdigit())
+    return sum(int(digit) for digit in filter(str.isdigit, str(n)))
