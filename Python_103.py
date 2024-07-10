bdbbe4170b6157220da377253ac9c0af81c0d42b
beep boop <githubbot@vadim.me>
2024-07-11 00:04:57 +0200

    Solution 27 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/th7rjdae Prompt: -1 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e45fcbe3-6e75-4835-a9ed-b3be554c4 cb8.py", line 22, in <module>     check(rounded_avg)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e45fcbe3-6e75-4835-a9ed-b3be554c4 cb8.py", line 19, in check     assert rounded_avg(5, 5) == "0b101"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_103.py
+++ Python_103.py
@@ -1,11 +1,18 @@
+```Python
+import math
+
+
 def rounded_avg(n, m):
     if n > m:
         return -1
+    elif n < m:
+        n, m = m, n
     avg = round((n + m) / 2.0)
-    
-    bin_str = bin(avg)[2:].zfill(8)
-    
-    return "0b" + bin_str
+    bin_str = ""
+    while avg > 1:
+        avg //= 2
+    bin_str = "0b" + ("10" * math.ceil(math.log2(avg)))[::-1].ljust(8, "0")
+    return bin_str
 
 
 print(rounded_avg(5, 3))
