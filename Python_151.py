1a3ba95e3fe243de3142b244973df1ff53337599
beep boop <githubbot@vadim.me>
2024-07-11 01:52:04 +0200

    Solution 55 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/49frezuz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/234bb1db-ba07-43ef-98be-db939ed3c d89.py", line 1, in <module>     print(check([1, 2, 3], lambda x: double_the_difference(x)))           ^^^^^ NameError: name 'check' is not defined

--- Python_151.py
+++ Python_151.py
@@ -1,10 +1,9 @@
-def double_the_difference(lst):
-    lst.sort()
-    return (lst[-1] - lst[0]) * 2
-
-
+```
 def check(lst, func):
     return func(lst)
 
+def double_the_difference(lst):
+    lst.sort()
+    return (lst[-1] - lst[0]) * 2
 
 print(check([1, 2, 3], lambda x: double_the_difference(x)))
