0135e136a755aa0c962296162b419428c0635cf8
beep boop <githubbot@vadim.me>
2024-07-10 18:48:50 +0200

    Solution 10 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/6yg1z8j0 Prompt: f57daade-a338-4fbd-861f-b522c417fcb1.cpp: In function ‘std::pair<std::vector<int >, std::vector<int> > cutVector(std::vector<int>)’: f57daade-a338-4fbd-861f-b522c417fcb1.cpp:7:20: error: ‘INT_MAX’ was not declared  in this scope     7 |     int min_diff = INT_MAX;       |                    ^~~~~~~ f57daade-a338-4fbd-861f-b522c417fcb1.cpp:3:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     2 | #include <vector>   +++ |+#include <climits>     3 |

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,27 +1,33 @@
+````
 #include <iostream>
 #include <vector>
+#include <limits>
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
-    int min_diff = INT_MAX;
+using namespace std;
+
+pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
+    int min_diff = numeric_limits<int>::max();
     pair<vector<int>, vector<int>> result;
     
-    for(int i=0; i<v.size(); i++) {
+    for (int i = 1; i < vec.size(); ++i) {
         int left_sum = 0, right_sum = 0;
         
-        for(int j=0; j<i; j++) {
-            left_sum += v[j];
-        }
+        for (int j = 0; j < i; ++j)
+            left_sum += vec[j];
         
-        for(int j=i; j<v.size(); j++) {
-            right_sum += v[j];
+        for (int j = i; j < vec.size(); ++j)
+            right_sum += vec[j];
+        
+        if (left_sum == right_sum) {
+            return {{vec[0]}, vector<int>(vec.begin() + 1, vec.end())};
         }
         
         int diff = abs(left_sum - right_sum);
         
-        if(diff < min_diff) {
+        if (diff < min_diff) {
             min_diff = diff;
-            result.first = vector<int>(v.begin(), v.begin() + i);
-            result.second = vector<int>(v.begin() + i, v.end());
+            result.first = vector<int>(vec.begin(), vec.begin() + i);
+            result.second = vector<int>(vec.begin() + i, vec.end());
         }
     }
     
@@ -31,22 +37,28 @@ pair<vector<int>, vector<int>> cutVector(vector<int> v) {
 int main() {
     int n;
     cin >> n;
-    vector<int> v(n);
-    for(int i=0; i<n; i++) {
-        cin >> v[i];
+    vector<int> vec(n+1);
+    for (int i = 0; i <= n; ++i)
+        cin >> vec[i];
+    
+    pair<vector<int>, vector<int>> res = cutVector(vec);
+    
+    cout << "[";
+    for (int num : res.first) {
+        cout << num;
+        if (&num != &res.first.back())
+            cout << " ";
+    }
+    cout << "] [";
+
+    for (int num : res.second) {
+        cout << num;
+        if (&num != &res.second.back())
+            cout << " ";
     }
     
-    pair<vector<int>, vector<int>> res = cutVector(v);
-    cout << "First half: ";
-    for(auto x : res.first) {
-        cout << x << " ";
-    }
-    cout << endl;
-    cout << "Second half: ";
-    for(auto x : res.second) {
-        cout << x << " ";
-    }
-    cout << endl;
+    cout << "]" << endl;
     
     return 0;
 }
+```
