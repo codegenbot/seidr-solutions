c72f44d3adf8b49af3cd97e7f48f1aa5ce9f9bba
beep boop <githubbot@vadim.me>
2024-07-10 20:05:24 +0200

    Solution 47 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/ku2zaqai Prompt: 2c668d4f-9f3b-4cec-83e5-ea13869a469e.cpp: In function ‘int mastermind(std::strin g, std::string)’: 2c668d4f-9f3b-4cec-83e5-ea13869a469e.cpp:23:18: error: ‘count’ is not a member o f ‘std’; did you mean ‘cout’?    23 |         if (std::count(codeCopy.begin(), codeCopy.end(), guess[i]) > 0) {       |                  ^~~~~       |                  cout 2c668d4f-9f3b-4cec-83e5-ea13869a469e.cpp:25:18: error: ‘replace’ is not a member  of ‘std’    25 |             std::replace(codeCopy.begin(), codeCopy.end(), guess[i], ' ' );       |                  ^~~~~~~ 2c668d4f-9f3b-4cec-83e5-ea13869a469e.cpp:26:18: error: ‘replace’ is not a member  of ‘std’    26 |             std::replace(guessCopy.begin(), guessCopy.end(), guess[i], '  ');       |                  ^~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -12,13 +12,12 @@ int mastermind(std::string code, std::string guess) {
     }
 
     // Count white pegs
-    std::vector<char> codeCopy(code.begin(), code.end());
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             blackPegs++;
         } else {
             bool existsInCode = false;
-            for (char c : codeCopy) {
+            for (char c : code) {
                 if (c == guess[i]) {
                     existsInCode = true;
                     break;
