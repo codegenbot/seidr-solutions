cfaa2655c8d53cfb7fb643333a2bd3bce55bb97c
beep boop <githubbot@vadim.me>
2024-07-10 06:37:55 +0200

    Solution 52 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/i5osy56j Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f977a8e8-b281-4e53-b361-2c114074f e85.py", line 2     return sorted(input_list, key=lambda x: int(str(x)[2]) if isinstance(x, int)  and len(str(x)) >= 3 else int(str(x)[0])                  ^ SyntaxError: '(' was never closed

--- Python_33.py
+++ Python_33.py
@@ -1,2 +1,9 @@
 def sort_third(input_list):
-    return sorted(input_list, key=lambda x: int(str(x)[2]) if isinstance(x, int) and len(str(x)) >= 3 else int(str(x)[0]))
+    return sorted(
+        input_list,
+        key=lambda x: (
+            int(str(x)[2])
+            if isinstance(x, int) and len(str(x)) >= 3
+            else int(str(x)[0])
+        ),
+    )
