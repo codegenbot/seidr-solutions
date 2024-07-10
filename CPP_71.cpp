2f9fb7bb6d6f70f0fdbdc8232e70fb52dba4d160
beep boop <githubbot@vadim.me>
2024-07-10 03:20:52 +0200

    Solution 37 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ctqurfce Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cmath>
 #include <cassert>
-#include <limits>
 
 float triangle_area(int a, int b, int c) {
     if ((a + b > c) && (a + c > b) && (b + c > a)) {
@@ -14,21 +13,8 @@ float triangle_area(int a, int b, int c) {
 
 int main() {
     int a, b, c; 
-    std::cin >> a;
-    while (!(std::cin >> b)) { 
-        std::cout << "Invalid input. Please enter an integer: ";
-        std::cin.clear(); 
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
-        std::cin >> b;
-    }
-    while (!(std::cin >> c)) {
-        std::cout << "Invalid input. Please enter an integer: ";
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cin >> c;
-    } 
+    std::cin >> a >> b >> c; 
     assert(std::abs(triangle_area(a, b, c) + 1)<0.01);
     float result = triangle_area(a, b, c);
     std::cout << "The area of the triangle is: " << static_cast<int>(result) << std::endl;
     return 0;
-}
