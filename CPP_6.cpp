26e32f0b0226409ff541c034adf4b540690e0867
beep boop <githubbot@vadim.me>
2024-07-10 18:35:25 +0200

    Solution 9 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/o1w7shmx Prompt:  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3b1c6ac2-1d7d-4ed6-86e4-8de9b1367132.cpp:19:27: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? 3b1c6ac2-1d7d-4ed6-86e4-8de9b1367132.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3b1c6ac2-1d7d-4ed6-86e4-8de9b1367132.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3b1c6ac2-1d7d-4ed6-86e4-8de9b1367132.cpp:18: 3b1c6ac2-1d7d-4ed6-86e4-8de9b1367132.cpp: In function ‘int main()’: 3b1c6ac2-1d7d-4ed6-86e4-8de9b1367132.cpp:28:20: error: ‘parse_nested_parens’ was  not declared in this scope    28 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ 3b1c6ac2-1d7d-4ed6-86e4-8de9b1367132.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -4,17 +4,14 @@
 using namespace std;
 
 bool issame(vector<vector<int>>& a, vector<vector<int>>& b) {
-    if(a.size() != b.size()) {
+    if (a.size() != b.size())
         return false;
-    }
-    for(int i = 0; i<a.size(); i++) {
-        if(a[i].size() != b[i].size()) {
+    for(int i = 0; i<a.size(); i++){
+        if(a[i].size()!=b[i].size())
             return false;
-        }
-        for(int j = 0; j<a[i].size(); j++) {
-            if(a[i][j] != b[i][j]) {
+        for(int j=0;j<a[i].size();j++){
+            if(a[i][j] != b[i][j])
                 return false;
-            }
         }
     }
     return true;
