2a9484dec8bcd0385554fdae161cbef2024e77ae
beep boop <githubbot@vadim.me>
2024-07-10 17:27:44 +0200

    Solution 2 of Python/156, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/mgtumz9n Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3aa55173-7424-48cd-8e4a-31804631d 1c5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_156.py
+++ Python_156.py
@@ -1,5 +1,4 @@
-```
-def int_to_mini_roman(number):
+def int_to_mini_roman(number): 
     roman_dict = {1: 'i', 4: 'iv', 5: 'v', 9: 'ix', 10: 'x', 
                   40: 'xl', 50: 'l', 90: 'xc', 100: 'c', 
                   400: 'cd', 500: 'd', 900: 'cm', 1000: 'm'}
