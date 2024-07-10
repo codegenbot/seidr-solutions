3380666e2f0522d0af2f21e178cbb665540e8253
beep boop <githubbot@vadim.me>
2024-07-10 10:35:32 +0200

    Solution 64 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gty3h18y Prompt: Complete the following code given the task description and function signature.

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -8,8 +8,9 @@ string intersection(vector<int> interval1, vector<int> interval2) {
     int length = end - start + 1;
 
     for (int i = 2; i * i <= length; i++) {
-        if (length % i == 0)
+        if (length % i == 0) {
             return "NO";
+        }
     }
 
     return "YES";
