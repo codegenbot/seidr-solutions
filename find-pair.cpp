7843cc3d42edda96fba0f3e57d11f1abdd77dae3
beep boop <githubbot@vadim.me>
2024-07-10 10:42:02 +0200

    Solution 71 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/bed580vf Prompt: 21260bb4-aa11-4553-9199-224c9dd558ea.cpp:1:6: error: ‘pair’ in namespace ‘std’ d oes not name a template type     1 | std::pair<int, int> findPair(vector<int>& nums, int target) {       |      ^~~~ 21260bb4-aa11-4553-9199-224c9dd558ea.cpp:1:1: note: ‘std::pair’ is defined in he ader ‘<utility>’; did you forget to ‘#include <utility>’?   +++ |+#include <utility>     1 | std::pair<int, int> findPair(vector<int>& nums, int target) {

--- find-pair.cpp
+++ find-pair.cpp
@@ -5,7 +5,7 @@ std::pair<std::int32_t, std::int32_t> findPair(std::vector<int>& nums, int targe
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
         if (numMap.count(complement)) {
-            return std::make_pair(nums[i], complement);
+            return std::make_pair(complement, nums[i]);
         }
         numMap[nums[i]] = i;
     }
