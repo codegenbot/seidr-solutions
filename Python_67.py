6047b5ae87c2c28e834c148d066c50e018a7098e
beep boop <githubbot@vadim.me>
2024-07-10 16:26:43 +0200

    Solution 14 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9wljto1e Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/990b6da7-a806-49f3-a9e2-6991da2a1 f09.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,4 +1,9 @@
 def fruit_distribution(s, n):
+    """
+    Given a string of words where the first word is 'apples', the second word represents the number of apples,
+    and the third word is either 'oranges' or 'and'. If the third word is 'oranges', then it's followed by a number.
+    Return the number of people that can be formed with the given fruits, where each person needs one apple and one orange.
+    """
     s = " ".join(s).replace("apples and", "apples ").replace("oranges", " oranges").split()
     apples = int(s[s.index("apples") + 1 :].replace("and", "").replace("apples", ""))
     oranges = int(s[s.index("oranges") + 7 :].replace("and", "").replace("oranges", ""))
