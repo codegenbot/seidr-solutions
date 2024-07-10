1aa1468809869575bf287819fe6c4bdfdd85a5f5
beep boop <githubbot@vadim.me>
2024-07-11 00:14:06 +0200

    Solution 28 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/chwkt3kc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/393ee6d4-bc41-4c22-9629-7aad48fe5 c84.py", line 33, in <module>     check(minPath)           ^^^^^^^ NameError: name 'minPath' is not defined. Did you mean: 'minPathSum'?

--- Python_129.py
+++ Python_129.py
@@ -1,10 +1,11 @@
+```
 def minPathSum(grid):
     n = len(grid)
     m = [[0] * n for _ in range(n)]
 
     def dfs(i, j, path, visited):
         if (i, j) == (n - 1, n - 1):
-            return sum(path) + grid[i][j]
+            return sum(path)
         visited.add((i, j))
         min_path = None
         for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
@@ -20,6 +21,6 @@ def minPathSum(grid):
     for i in range(n):
         for j in range(n):
             path = dfs(i, j, [], set())
-            if min_path is None or sum(path) + grid[i][j] < sum(min_path):
+            if min_path is None or sum(path) < sum(min_path):
                 min_path = path
     return sum(min_path)
