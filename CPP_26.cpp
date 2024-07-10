e41418be2df0d9867d1d3f8f36d9e5d66aef2193
beep boop <githubbot@vadim.me>
2024-07-10 13:42:10 +0200

    Solution 66 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/y3ltsp11 Prompt:  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 065e8bc9-d9c8-4616-ade1-b58fcf616224.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 065e8bc9-d9c8-4616-ade1-b58fcf616224.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 065e8bc9-d9c8-4616-ade1-b58fcf616224.cpp:10: 065e8bc9-d9c8-4616-ade1-b58fcf616224.cpp: In function ‘int main()’: 065e8bc9-d9c8-4616-ade1-b58fcf616224.cpp:20:20: error: ‘remove_duplicates’ was n ot declared in this scope    20 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 065e8bc9-d9c8-4616-ade1-b58fcf616224.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,8 +1,12 @@
 vector<int> unique_numbers;
-for (int num : numbers) {
-    if (count(numbers.begin(), numbers.end(), num) == 1) {
-        unique_numbers.push_back(num);
+sort(numbers.begin(), numbers.end());
+for (int i = 0; i < numbers.size(); ++i) {
+    if (i == 0 && numbers[i] != numbers[i + 1]) {
+        unique_numbers.push_back(numbers[i]);
+    } else if (i == numbers.size() - 1 && numbers[i] != numbers[i - 1]) {
+        unique_numbers.push_back(numbers[i]);
+    } else if (numbers[i] != numbers[i - 1] && numbers[i] != numbers[i + 1]) {
+        unique_numbers.push_back(numbers[i]);
     }
 }
-sort(unique_numbers.begin(), unique_numbers.end());
 return unique_numbers;
