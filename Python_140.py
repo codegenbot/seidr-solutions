21faed66b9f8919b76e64eeae911ce13e046b511
beep boop <githubbot@vadim.me>
2024-07-10 17:39:46 +0200

    Solution 27 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/1l81b5hx Prompt: Error: name 're' is not defined Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e3fc5a71-63f9-431b-9f9b-04d764b5e be6.py", line 14, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e3fc5a71-63f9-431b-9f9b-04d764b5e be6.py", line 10, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,10 +1,7 @@
+```
 import re
-
-
 def fix_spaces(text):
     try:
-        return re.sub(
-            r"\s+", lambda x: "_" if len(x.group().strip()) == 0 else " - ", text
-        )
+        return re.sub(r"\s+", lambda x: "_" if len(x.group().strip()) == 0 else " - ", text)
     except Exception as e:
         print(f"Error: {e}")
