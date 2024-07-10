e231f0d4676cf2cde07590223d0a94d1b528e708
beep boop <githubbot@vadim.me>
2024-07-10 19:04:48 +0200

    Solution 1 of Python/44, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/s5tsm620 Prompt: Complete the following code given the task description and function signature.

--- Python_44.py
+++ Python_44.py
@@ -1,7 +1,6 @@
-```
 def change_base(x: int, base: int):
-    convert_string = "0123456789ABC"
+    convertString = "0123456789ABC"
     if x < base:
-        return convert_string[x]
+        return convertString[x]
     else:
-        return change_base(x // base, base) + convert_string[x % base]
+        return change_base(x // base, base) + convertString[x % base]
