3ff1cb933d3e93aff2fbecd68eb2a2a582527281
beep boop <githubbot@vadim.me>
2024-07-10 16:29:09 +0200

    Solution 25 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lu60jru2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/83336bc8-86fb-4019-a9b4-df2e60adb 727.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_50.py
+++ Python_50.py
@@ -1,3 +1,4 @@
+```python
 def decode_shift(s: str):
-    """Return the decoded string by shifting each lowercase letter in the input string by 5 positions backwards."""
+    """Decodes a Caesar cipher with a shift of 5."""
     return "".join([chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) if ch.isalpha() and ch.islower() else ch for ch in s])
