aa6721a0be429f329af51050de158df29c276c2c
beep boop <githubbot@vadim.me>
2024-07-10 08:07:51 +0200

    Solution 6 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8t1bc2ba Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,35 +1,30 @@
-#include<string>
+#include <string>
+
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = 0, b = 0, c = 0, d = 0;
-    string s1, s2;
+    int a = 1, b = 1, c = 1, d = 1;
+    
+    // Convert string fractions to integers.
+    size_t pos = x.find('/');
+    a = stoi(x.substr(0, pos));
+    b = stoi(x.substr(pos + 1));
 
-    for (int i = 0; i < x.length(); i++) {
-        if (x[i] == '/') break;
-        if (isdigit(x[i])) a = a * 10 + (x[i] - '0');
-    }
-    for (int i = x.length() - 1; i >= 0; i--) {
-        if (x[i] == '/') break;
-        s1 += x[i];
-    }
-    reverse(s1.begin(), s1.end());
+    pos = n.find('/');
+    c = stoi(n.substr(0, pos));
+    d = stoi(n.substr(pos + 1));
 
-    for (int i = 0; i < n.length(); i++) {
-        if (n[i] == '/') break;
-        if (isdigit(n[i])) c = c * 10 + (n[i] - '0');
-    }
-    for (int i = n.length() - 1; i >= 0; i--) {
-        if (n[i] == '/') break;
-        s2 += n[i];
-    }
-    reverse(s2.begin(), s2.end());
-
-    int g = __gcd(a, c);
-    return a / g == b && c / g == d;
+    // Check if the product is an integer.
+    long long gcd = __gcd(a * c, b * d);
+    
+    return (a * c) / gcd == (b * d) / gcd;
 }
 
-int gcd(int a, int b) {
-    if (b == 0) return a;
-    return gcd(b, a % b);
+// Function to calculate GCD of two numbers
+long long __gcd(long long a,long long b)
+{
+    if(b==0)
+        return a;
+    else
+        return __gcd(b,a%b);
 }
