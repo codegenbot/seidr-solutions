06192093c806431a78055bfd1eb1c5f99af7700b
beep boop <githubbot@vadim.me>
2024-07-10 19:18:41 +0200

    Solution 2 of CPP/77, ratio 0.875 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1af2o2rt Prompt: 2b1bd41f-7139-45b7-a70b-37ef99e35f46.cpp: In function ‘bool iscuber(int)’: 2b1bd41f-7139-45b7-a70b-37ef99e35f46.cpp:4:21: error: ‘pow’ was not declared in this scope     4 |         int cubed = pow(i, 3);       |                     ^~~

--- CPP_77.cpp
+++ CPP_77.cpp
@@ -1,6 +1,13 @@
-Here is the completed code:
+#include <cmath>
 
 bool iscuber(int a){
-    int c = round(cbrt(a));
-    return (c*c*c) == a;
+    int i = 1;
+    while (true) {
+        int cubed = pow(i, 3);
+        if (cubed > a)
+            return false;
+        else if (cubed == a)
+            return true;
+        i++;
+    }
 }
