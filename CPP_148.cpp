4556e1a6805640ecbefed7302c451f4d803e6273
beep boop <githubbot@vadim.me>
2024-07-10 20:56:06 +0200

    Solution 25 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/6kgk2p4r Prompt:       |                              ^~~~~~ bdae4cda-ae2d-47b2-b9ff-a2cdd6fc7401.cpp:31:37: error: ‘string’ was not declared  in this scope    31 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bdae4cda-ae2d-47b2-b9ff-a2cdd6fc7401.cpp:31:44: error: ‘b’ was not declared in t his scope    31 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bdae4cda-ae2d-47b2-b9ff-a2cdd6fc7401.cpp:31:45: error: expression list treated a s compound expression in initializer [-fpermissive]    31 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from bdae4cda-ae2d-47b2-b9ff-a2cdd6fc7401.cpp:30: bdae4cda-ae2d-47b2-b9ff-a2cdd6fc7401.cpp: In function ‘int main()’: bdae4cda-ae2d-47b2-b9ff-a2cdd6fc7401.cpp:40:20: error: ‘bf’ was not declared in this scope    40 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ bdae4cda-ae2d-47b2-b9ff-a2cdd6fc7401.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -3,11 +3,13 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b);
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    return a == b;
+}
 
-std::vector<std::string> bf(std::string planet1, std::string planet2) {
-    std::vector<std::string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
-    std::vector<std::string> result;
+vector<string> bf(string planet1, string planet2) {
+    vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+    vector<string> result;
 
     int start = -1, end = -1;
     for (int i = 0; i < planets.size(); ++i) {
@@ -32,10 +34,6 @@ std::vector<std::string> bf(std::string planet1, std::string planet2) {
     }
 
     return result;
-}
-
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    return a == b;
 }
 
 int main() {
