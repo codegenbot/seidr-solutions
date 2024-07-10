2953060ded784c4b540cd1c44eb60ddfe6511c55
beep boop <githubbot@vadim.me>
2024-07-10 20:49:28 +0200

    Solution 38 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pc4qhgax Prompt: [1, 'a', 3, 'b', 5, 'c', 'a', 'b', 'c'] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/283e203f-1187-4c55-8a95-e98dd33ce 303.py", line 19, in <module>     check(intersperse)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/283e203f-1187-4c55-8a95-e98dd33ce 303.py", line 16, in check     assert intersperse([2, 2, 2], 2) == [2, 2, 2, 2, 2]            ^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/283e203f-1187-4c55-8a95-e98dd33ce 303.py", line 3, in intersperse     min_len = min(len(a), len(b))                           ^^^^^^ TypeError: object of type 'int' has no len()

--- Python_5.py
+++ Python_5.py
@@ -1,11 +1,13 @@
 def intersperse(a, b):
+    if isinstance(b, int):  
+        return [i for pair in zip(a, [b]*len(a)) for i in pair] + [b]
     result = []
-    min_len = min(len(iter(a)), len(b))
+    min_len = min(len(a), len(b))
     for i in range(min_len):
-        result.append(next(iter(a)))
-        result.append(next(iter(b)))
-    result.extend(list(iter(a))[min_len:])
-    result.extend(list(iter(b))[min_len:])
+        result.append(a.pop(0))
+        result.append(b.pop(0))
+    result.extend(a)
+    result.extend(b)
     return result
 
 list1 = [1, 3, 5]
