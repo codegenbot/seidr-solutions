32a03176340cd8e22c7e9c6510446595cb4ccd1b
beep boop <githubbot@vadim.me>
2024-07-10 10:01:17 +0200

    Solution 98 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/knxcxtbp Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9c082990-f552-4509-bb24-f05b54496 59a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,12 +1,8 @@
+```
 import math
-
 
 def check(num):
     for i in range(2, num + 1):
         if set(math.factorize(i)) == set(math.factorize(num)) and i != num:
             return str(i)
     return str(num)
-
-
-num = int(input("Enter a number: "))
-print(check(num))
