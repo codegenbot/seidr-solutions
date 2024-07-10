a2608b89ff6764b139cae85761eb52bfc78f8817
beep boop <githubbot@vadim.me>
2024-07-10 13:20:55 +0200

    Solution 98 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/xcyg7swg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc9af0cd-19b9-482c-be4e-68ca7178b 6b4.py", line 26, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc9af0cd-19b9-482c-be4e-68ca7178b 6b4.py", line 23, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_39.py
+++ Python_39.py
@@ -1,21 +1,13 @@
 from math import isqrt
 
-def fibonacci(n):
-    if n <= 1:
-        return n
-    a, b = 0, 1
-    for _ in range(2, n+1):
-        a, b = b, a+b
-    return a
-
 def is_prime(num):
     if num < 2:
         return False
-    for i in range(2, isqrt(num) + 1):
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    for i in range(3, isqrt(num)+1, 2):
         if num % i == 0:
             return False
     return True
-
-def prime_fib(n):
-    nth_fib = fibonacci(n)
-    return is_prime(nth_fib)
