936bf8978e49abe862c446e6de975f6efcfa0b53
beep boop <githubbot@vadim.me>
2024-07-11 00:42:15 +0200

    Solution 72 of CPP/139, ratio 0.25 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/o8bsrkr9 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_139.cpp
+++ CPP_139.cpp
@@ -1,24 +1,24 @@
 ```cpp
 #include <iostream>
 #include <limits>
-#include <string>
 
 using namespace std;
 
-long long special_factorial(int n) {
+long long special_factorial(long long n) {
     long long result = 1;
-    for (int i = 1; i <= n; ++i) {
-        result *= 1LL * i;
+    for (long long i = 1; i <= n; ++i) {
+        result *= i;
     }
     return result;
 }
 
 int main() {
-    int n;
-    cout << "Enter a positive integer: ";
-    cin >> n;
-    if(n < 0)
-        cout << "Error! Please enter a non-negative number." << endl;
-    else
-        cout << "The special factorial of " << to_string(n) << " is: " << special_factorial(n) << endl;
+    long long result;
+    cout << "Enter a number: ";
+    cin >> result;
+    if(result < 0) {
+        cout << "Error! Please enter a positive integer." << endl;
+    } else {
+        cout << "The special factorial of " << result << " is: " << special_factorial(result) << endl;
+    }
 }
