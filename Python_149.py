4eeebf926bf8cfb6afd8a478df8c7e9b21edd5e9
beep boop <githubbot@vadim.me>
2024-07-10 08:15:38 +0200

    Solution 62 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j9zzr72u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b29d0727-35ba-4971-b6a2-0c19e3a18 f88.py", line 38, in <module>     check(sorted_list_sum)           ^^^^^^^^^^^^^^^ NameError: name 'sorted_list_sum' is not defined

--- Python_149.py
+++ Python_149.py
@@ -14,15 +14,16 @@ def main():
             continue
 
     if len(lst) > 0:
+        even_word_sum = sum(len(word) for word in lst if len(word) % 2 == 0)
+        
         while True:
             try:
-                sorted_list_sum = sum(len(word) for word in lst if len(word) % 2 == 0)
                 input_val = int(
                     input("Press Enter to calculate the sum or type 'q' to quit: ")
                 )
                 if input_val == 113:
                     break
-                print(sorted_list_sum)  
+                print(even_word_sum)  
             except ValueError:
                 print("Invalid input. Please enter a number or 'q'.")
             except Exception as e:
