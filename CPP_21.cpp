95459cc24d1b8699910080f977dfbeabb86513cf
beep boop <githubbot@vadim.me>
2024-07-10 13:33:44 +0200

    Solution 50 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/i7l2mkmf Prompt:    14 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 5b1fd266-73de-492b-9907-2873dc568142.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 5b1fd266-73de-492b-9907-2873dc568142.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 5b1fd266-73de-492b-9907-2873dc568142.cpp:13: 5b1fd266-73de-492b-9907-2873dc568142.cpp: In function ‘int main()’: 5b1fd266-73de-492b-9907-2873dc568142.cpp:23:20: error: ‘rescale_to_unit’ was not  declared in this scope    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 5b1fd266-73de-492b-9907-2873dc568142.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,12 +1,13 @@
-vector<float> rescale_to_unit(const vector<float>& numbers) {
-    float min_num = *min_element(numbers.begin(), numbers.end());
-    float max_num = *max_element(numbers.begin(), numbers.end());
-    
-    vector<float> scaled_numbers;
-    for (float num : numbers) {
-        float scaled_num = (num - min_num) / (max_num - min_num);
-        scaled_numbers.push_back(scaled_num);
+bool issame(vector<float> a, vector<float> b) {
+    if (a.size() != b.size()) {
+        return false;
     }
     
-    return scaled_numbers;
+    for (int i = 0; i < a.size(); ++i) {
+        if (abs(a[i] - b[i]) > 0.0001) {
+            return false;
+        }
+    }
+    
+    return true;
 }
