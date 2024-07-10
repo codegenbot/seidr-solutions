6b827ad416bca688ffa8809e8384da8891afa8b0
beep boop <githubbot@vadim.me>
2024-07-10 08:40:25 +0200

    Solution 52 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ehlsqt8n Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/409e4679-f316-4824-9a49-602064688 d94.py", line 1     ```     ^ SyntaxError: invalid syntax

--- leaders.py
+++ leaders.py
@@ -1,34 +1,9 @@
+```
 def leaders(arr):
-    leaders = [arr[len(arr) - 1]]
+    leaders = [arr[len(arr)-1]]
     for i in range(len(arr) - 2, -1, -1):
         if arr[i] >= leaders[-1]:
             leaders.append(arr[i])
     return leaders[::-1]
 
-
-print(
-    leaders(
-        [
-            19,
-            748,
-            560,
-            43,
-            279,
-            439,
-            440,
-            462,
-            50,
-            715,
-            551,
-            459,
-            413,
-            60,
-            509,
-            829,
-            451,
-            362,
-            316,
-            680,
-        ]
-    )
-)
+print(leaders([19, 748, 560, 43, 279, 439, 440, 462, 50, 715, 551, 459, 413, 60, 509, 829, 451, 362, 316, 680]))
