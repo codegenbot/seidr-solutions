d1c3339c19e7d9ddf0212750fa3f92e3505d4f54
beep boop <githubbot@vadim.me>
2024-07-10 20:21:16 +0200

    Solution 20 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/bq0fcx76 Prompt:    34 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5a8c09de-ffb3-45ad-82bc-2ede00bf718c.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 5a8c09de-ffb3-45ad-82bc-2ede00bf718c.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~ In file included from 5a8c09de-ffb3-45ad-82bc-2ede00bf718c.cpp:33: 5a8c09de-ffb3-45ad-82bc-2ede00bf718c.cpp: In function ‘int main()’: 5a8c09de-ffb3-45ad-82bc-2ede00bf718c.cpp:43:20: error: ‘order_by_points’ was not  declared in this scope    43 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 5a8c09de-ffb3-45ad-82bc-2ede00bf718c.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,13 +1,19 @@
 #include <vector>
 #include <algorithm>
-#include <cassert>
+#include <assert.h>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b);
+
+vector<int> order_by_points(vector<int> nums);
+
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-std::vector<int> order_by_points(std::vector<int> nums) {
-    std::sort(nums.begin(), nums.end(), [](int a, int b) {
+vector<int> order_by_points(vector<int> nums) {
+    sort(nums.begin(), nums.end(), [](int a, int b) {
         int sum_a = 0, sum_b = 0;
         int temp_a = abs(a), temp_b = abs(b);
         while (temp_a > 0) {
@@ -19,7 +25,7 @@ std::vector<int> order_by_points(std::vector<int> nums) {
             temp_b /= 10;
         }
         if (sum_a == sum_b) {
-            return std::find(nums.begin(), nums.end(), a) < std::find(nums.begin(), nums.end(), b);
+            return find(nums.begin(), nums.end(), a) < find(nums.begin(), nums.end(), b);
         }
         return sum_a < sum_b;
     });
