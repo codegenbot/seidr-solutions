f12167ef93e4143eef6be6a825cbc2790d8bba3e
beep boop <githubbot@vadim.me>
2024-07-10 07:46:45 +0200

    Solution 36 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/q7w4gwkk Prompt: In file included from 086842d6-f1a9-46ad-99c3-55351420a7fc.cpp:20: 086842d6-f1a9-46ad-99c3-55351420a7fc.cpp: In function ‘int main()’: 086842d6-f1a9-46ad-99c3-55351420a7fc.cpp:30:20: error: ‘sort_array’ was not decl ared in this scope    30 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -4,14 +4,6 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
-}
-
 vector<int> sort_vector(vector<int> arr) {
     vector<int> result = arr;
     sort(result.begin(), result.end(),
@@ -26,7 +18,4 @@ vector<int> sort_vector(vector<int> arr) {
     return result;
 }
 
-int main() {
-    assert(issame(sort_vector({2,4,8,16,32}) , {2, 4, 8, 16, 32}));
-    return 0;
-}
+assert(issame(sort_vector({2,4,8,16,32}) , {2, 4, 8, 16, 32}));
