0dd024a0668b6f0984bd8bf064d762d5d2f6030a
beep boop <githubbot@vadim.me>
2024-07-10 21:46:14 +0200

    Solution 39 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tbcv99fp Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fe14e6e9-4520-4f62-84fd-2f69763df bff.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,3 +1,4 @@
+```
 def parse_music(music_string: str) -> list:
     music_list = []
     i = 0
@@ -8,18 +9,19 @@ def parse_music(music_string: str) -> list:
         elif i < len(music_string) - 1 and music_string[i:i+2] == 'o|':
             music_list.append(2)
             i += 2
-        elif i < len(music_string) - 2 and (music_string[i:i+3] == '.||' or music_string[i:i+2] == '||'):
-            if music_string[i:i+3] == '.||':
-                music_list.append(1)
-                i += 3
-            else:
-                if music_string[i:i+2] == '||':
-                    music_list.append(0.5)
-                    i += 2
-                else:
+        elif i < len(music_string) - 2:
+            if music_string[i:i+3] == '.||' or music_string[i:i+2] == '||':
+                if music_string[i:i+3] == '.||':
                     music_list.append(1)
-                    i += 1
-        else:
-            music_list.append(1)
-            i += 1
+                    i += 3
+                else:
+                    if music_string[i:i+2] == '||':
+                        music_list.append(0.5)
+                        i += 2
+                    else:
+                        music_list.append(1)
+                        i += 1
+            else:
+                music_list.append(1)
+                i += 1
     return music_list
