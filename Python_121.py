343a66d71883e33445432da875baf0808efc9f33
beep boop <githubbot@vadim.me>
2024-07-10 23:36:49 +0200

    Solution 63 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lgdurmsn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d5693c56-ee67-47d1-915f-920340ba7 178.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,17 +1,11 @@
 def sum_of_odd_numbers_in_every_other_element():
     while True:
         try:
-            numbers = list(
-                map(
-                    int,
-                    input("Enter the elements of a list separated by space: ").split(),
-                )
-            )
+            numbers = list(map(int, input("Enter the elements of a list separated by space: ").split()))
             if not numbers:
                 raise ValueError
             return sum(i for i in numbers[1::2] if i % 2 != 0)
         except ValueError:
             print("Invalid input. Please try again.")
-
 
 print(sum_of_odd_numbers_in_every_other_element())
