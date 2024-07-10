eca3afc676b439b531a4d1be7bd4296f70a20b2f
beep boop <githubbot@vadim.me>
2024-07-10 20:27:12 +0200

    Solution 54 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/n709d7eu Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9f3c947e-c600-4454-aecf-9c2b7c173 33a.py", line 26, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9f3c947e-c600-4454-aecf-9c2b7c173 33a.py", line 21, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,11 +1,17 @@
-```
-def sort_even(l):
+def sort_even(l: list):
     evens = [x for i, x in enumerate(l) if i % 2 == 0]
     odds = sorted([x for i, x in enumerate(l) if i % 2 != 0])
-    
-    combined_list = list(zip(*sorted(list(zip(*[(i, x) for i, x in enumerate(l)])))[1:]))
-    
-    result = [x[1] for i, x in enumerate(combined_list) if i % 2 == 0]
-    result.extend([x[1] for i, x in enumerate(combined_list) if i % 2 != 0])
-    
+
+    result = []
+    even_index = odd_index = 0
+    while even_index < len(evens) and odd_index < len(odds):
+        is_even = evens[even_index] % 2 == 0
+        if is_even:
+            result.append(evens[even_index])
+            even_index += 1
+        else:
+            result.append(odds[odd_index])
+            odd_index += 1
+
+    result.extend(evens[even_index:])
     return tuple(result)
