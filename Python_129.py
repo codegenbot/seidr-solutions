5136fd4ec9ede22effe36f1811fedaabaae3d261
beep boop <githubbot@vadim.me>
2024-07-10 11:47:11 +0200

    Solution 62 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/gs0n39vc Prompt: Complete the following code given the task description and function signature.

--- Python_129.py
+++ Python_129.py
@@ -1,10 +1,30 @@
 def minPath(grid, k):
     n = len(grid)
-    res = []
+    memo = {}
+
+    def dfs(i, j, path, cnt):
+        if (i, j, tuple(path)) in memo:
+            return memo[(i, j, tuple(path))]
+
+        if cnt == k:
+            return path
+
+        min_path = None
+        for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            ni, nj = i + x, j + y
+            if 0 <= ni < n and 0 <= nj < n:
+                if grid[ni][nj] not in path:
+                    new_path = dfs(ni, nj, path + [grid[ni][nj]], cnt + 1)
+                    if min_path is None or min_path > new_path:
+                        min_path = new_path
+
+        memo[(i, j, tuple(path))] = min_path
+        return min_path
+
     for i in range(n):
         for j in range(n):
-            if len(res) < k:
-                res.append(grid[i][j])
-            else:
-                break
-    return res
+            path = dfs(i, j, [grid[i][j]], 0)
+            if path:
+                return list(path)
+
+    return []
