fe0c0976fd3d9f6738c194f6906a38b6e2b9c1a3
beep boop <githubbot@vadim.me>
2024-07-10 09:11:08 +0200

    Solution 82 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/knxcxtbp Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/91c0443c-05f2-49d3-b976-54e6be46c 9f6.py", line 6     def check(factorize): IndentationError: expected an indented block after function definition on line 5

--- Python_25.py
+++ Python_25.py
@@ -2,5 +2,10 @@ from typing import List
 
 
 def main():
-    def check(factorize):
-        pass
+    def check(factorize: List[int]) -> bool:
+        if len(factorize) == 0:
+            return True
+        for i in range(2, int(factorize[0] ** 0.5) + 1):
+            while factorize[0] % i == 0:
+                factorize[0] //= i
+        return factorize[0] == 1
