966a318b5f8ee446c952a0af883b4a2c88c195f6
beep boop <githubbot@vadim.me>
2024-07-10 07:41:12 +0200

    Solution 2 of CPP/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gsomjlod Prompt: Complete the following code given the task description and function signature.

--- CPP_122.cpp
+++ CPP_122.cpp
@@ -1,4 +1,6 @@
-int add_elements(vector<int> arr, int k) {
+#include <vector>
+
+int add_elements(std::vector<int> arr, int k) {
     int sum = 0;
     for (int i = 0; i < k; i++) {
         if (to_string(arr[i]).size() <= 2) {
