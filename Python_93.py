cc8e5388871f6924b5702da5d3a953775f18da31
beep boop <githubbot@vadim.me>
2024-07-10 16:45:50 +0200

    Solution 36 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/5lfyz3vl Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4b0322e9-d747-4691-8203-10f936c89 ae5.py", line 22, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4b0322e9-d747-4691-8203-10f936c89 ae5.py", line 18, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,13 +1,15 @@
+```
 def encode(message):
     result = ""
     for char in message:
         if char.isalpha():
-            if char.lower() in 'aeiou':
-                result += chr((ord(char) - 97 + 3) % 26 + 97)
-            elif not char.islower():  
-                result += chr((ord(char) - 65 + 3) % 26 + 65)
+            if set('aeiou').intersection({char.lower()}) != {''}:
+                if char.lower() in 'aeiou':
+                    result += chr((ord(char) - 97 + 3) % 26 + 97)
+                else:  
+                    result += chr((ord(char) - 65 + 3) % 26 + 65)
             else:
-                result += chr((ord(char) - 97 + 3) % 26 + 97)  
+                result += char
         else:  
             result += char
     return result
