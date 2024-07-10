4f6ccd87f8da29c202ae73e61caef92abec1fe2b
beep boop <githubbot@vadim.me>
2024-07-10 22:45:33 +0200

    Solution 6 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/f6bcnret Prompt:    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7637589d-80f7-4b6b-9b20-64277d1bb6dc.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7637589d-80f7-4b6b-9b20-64277d1bb6dc.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7637589d-80f7-4b6b-9b20-64277d1bb6dc.cpp:14: 7637589d-80f7-4b6b-9b20-64277d1bb6dc.cpp: In function ‘int main()’: 7637589d-80f7-4b6b-9b20-64277d1bb6dc.cpp:24:20: error: ‘largest_smallest_integer s’ was not declared in this scope    24 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 7637589d-80f7-4b6b-9b20-64277d1bb6dc.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,23 +1,14 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
-bool issame(vector<int> v1, vector<int> v2) {
-    return v1 == v2;
-}
-
-vector<int> largest_smallest_integers(vector<int> lst){
-    int max_negative = 0;
-    int min_positive = INT_MAX;
-
-    for(int i : lst) {
-        if(i < 0 && i > max_negative) {
-            max_negative = i;
-        } else if(i > 0 && i < min_positive) {
-            min_positive = i;
+std::pair<int, int> largest_smallest_integers(const std::vector<int>& lst) {
+    int max_negative = 0, min_positive = INT_MAX;
+    for (int num : lst) {
+        if (num < 0 && num > max_negative) {
+            max_negative = num;
+        } else if (num > 0 && num < min_positive) {
+            min_positive = num;
         }
     }
-
-    return {(max_negative > 0 ? 0 : max_negative), (min_positive < 1 ? 0 : min_positive)};
+    return {max_negative, min_positive};
 }
