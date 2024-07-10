fc87a4c202592cb2f3b540ffa65df36d3f605e94
beep boop <githubbot@vadim.me>
2024-07-10 21:04:58 +0200

    Solution 9 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rmdf06ap Prompt:       |                             ^~~ 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp:27:33: error: expected primary-expressi on before ‘char’    27 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp:27:38: error: expected primary-expressi on before ‘int’    27 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp:27:44: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp:26: 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp: In function ‘int main()’: 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp:41:20: error: ‘histogram’ was not decla red in this scope    41 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ 5d170173-067d-428b-8a3d-a6e16585a1a8.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -11,29 +11,27 @@ bool issame(map<char,int> a,map<char,int> b){
 
 int main() {
     assert(issame(histogram("a") , {{'a', 1}}));
-}
-
-map<char, int> histogram(string test) {
-    map<char, int> result;
-    string str = test;
-    for (char c : str) {
-        if (c != ' ') {
-            if (result.find(c) == result.end()) {
-                result[c] = 1;
-            } else {
-                result[c]++;
+    map<char, int> histogram(string test) {
+        map<char, int> result;
+        string str = test;
+        for (char c : str) {
+            if (c != ' ') {
+                if (result.find(c) == result.end()) {
+                    result[c] = 1;
+                } else {
+                    result[c]++;
+                }
             }
         }
-    }
-    map<char, int> maxMap;
-    int maxCount = 0;
-    for (auto it = result.begin(); it != result.end(); ++it) {
-        if (it->second > maxCount) {
-            maxCount = it->second;
-            maxMap = {{it->first, it->second}};
-        } else if (it->second == maxCount) {
-            maxMap[make_pair(it->first, it->second)] = it->second;
+        map<char, int> maxMap;
+        int maxCount = 0;
+        for (auto it = result.begin(); it != result.end(); ++it) {
+            if (it->second > maxCount) {
+                maxCount = it->second;
+                maxMap = {{it->first, it->second}};
+            } else if (it->second == maxCount) {
+                maxMap[make_pair(it->first, it->second)] = it->second;
+            }
         }
+        return maxMap;
     }
-    return maxMap;
-}
