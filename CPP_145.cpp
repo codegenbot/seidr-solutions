4d70db8ce69c5257ee92e1245f4a77e93bfe5f05
beep boop <githubbot@vadim.me>
2024-07-10 11:02:56 +0200

    Solution 58 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/gaisl9om Prompt:       |                                          ^~~~~~~~~ c1ce3ac7-e7cf-4457-b0dc-3aeac9e941a5.cpp:16:74: error: ‘to_string’ is not a memb er of ‘std’    16 | um_b = std::accumulate(std::to_string(abs(b)).begin(), std::to_string(ab s(b)).end(), 0, [](int acc, char c) { return acc + c - '0'; });       |                                                             ^~~~~~~~~ In file included from c1ce3ac7-e7cf-4457-b0dc-3aeac9e941a5.cpp:25: c1ce3ac7-e7cf-4457-b0dc-3aeac9e941a5.cpp: In function ‘int main()’: c1ce3ac7-e7cf-4457-b0dc-3aeac9e941a5.cpp:35:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    35 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~ c1ce3ac7-e7cf-4457-b0dc-3aeac9e941a5.cpp:8:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     8 | bool issame(const std::vector<int>& a, const std::vector<int>& b){       |      ^~~~~~ c1ce3ac7-e7cf-4457-b0dc-3aeac9e941a5.cpp:26:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    26 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -5,33 +5,19 @@
 
 using namespace std;
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b){
-    return a == b;
-}
-
-int sum_of_digits(int num) {
-    int sum = 0;
-    while (num != 0) {
-        sum += abs(num) % 10;
-        num /= 10;
-    }
-    return sum;
-}
-
 std::vector<int> order_by_points(const std::vector<int>& nums) {
+    auto int_to_str = [](int num) {
+        return to_string(abs(num));
+    };
+
     std::vector<int> sorted_nums = nums; 
     std::sort(sorted_nums.begin(), sorted_nums.end(), [&](int a, int b) {
-        int sum_a = sum_of_digits(a);
-        int sum_b = sum_of_digits(b);
+        int sum_a = std::accumulate(int_to_str(a).begin(), int_to_str(a).end(), 0, [](int acc, char c) { return acc + c - '0'; });
+        int sum_b = std::accumulate(int_to_str(b).begin(), int_to_str(b).end(), 0, [](int acc, char c) { return acc + c - '0'; });
         if (sum_a == sum_b) {
             return a < b;
         }
         return sum_a < sum_b;
     });
     return sorted_nums;
-}
-
-int main() {
-    assert(issame(order_by_points({0,6,6,-76,-21,23,4}), {-76, -21, 0, 4, 23, 6, 6}));
-    return 0;
 }
