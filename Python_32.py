20bfa5affdae17bcde6affbbc8834e0c6130958d
beep boop <githubbot@vadim.me>
2024-07-10 03:47:04 +0200

    Solution 15 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/09anxcah Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,21 +1,6 @@
 def find_zero(xs: list):
-    """xs are coefficients of a polynomial.
-    find_zero find x such that poly(x) = 0.
-    find_zero returns only only zero point, even if there are many.
-    Moreover, find_zero only takes list xs having even number of coefficients
-    and largest non zero coefficient as it guarantees
-    a solution.
-    >>> round(find_zero([1, 2]), 2) # f(x) = 1 + 2x
-    -0.5
-    >>> round(find_zero([-6, 11, -6, 1]), 2) # (x - 1) * (x - 2) * (x - 3) = -6 + 11x - 6x^2 + x^3
-    1.0
-    """
-    if xs == [0]:
-        return 0
-
-    non_zero_coeff = next((c for c in reversed(xs) if c != 0))
     n = len(xs) - 1
-    if n % 2 == 0:
-        a = non_zero_coeff
-        b = -sum([c * (i + 1) for i, c in enumerate(xs[:-1])]) / non_zero_coeff
-        return -b / a
+    assert n % 2 == 0
+    assert xs[n] != 0
+    p = [-x for x in xs[:-1]]
+    return round(np.roots(p)[0].real, 2)
