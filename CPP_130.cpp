8d90b417dc0afcd4a4979710efbfb90f9c7b60b3
beep boop <githubbot@vadim.me>
2024-07-10 10:16:29 +0200

    Solution 48 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ll7detmu Prompt:       |           ^~~~~~ In file included from 9ef56449-d371-4287-8fe5-afc909a1be53.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9ef56449-d371-4287-8fe5-afc909a1be53.cpp:4:34: error: expected primary-expressio n before ‘int’     4 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9ef56449-d371-4287-8fe5-afc909a1be53.cpp:4:39: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9ef56449-d371-4287-8fe5-afc909a1be53.cpp:3: 9ef56449-d371-4287-8fe5-afc909a1be53.cpp: In function ‘int main()’: 9ef56449-d371-4287-8fe5-afc909a1be53.cpp:13:20: error: ‘tri’ was not declared in  this scope    13 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ 9ef56449-d371-4287-8fe5-afc909a1be53.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,12 +1,11 @@
 #include <vector>
-#include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
 int main() {
-    std::vector<int> a = {1, 3};
-    assert(issame(std::vector<int>{1}, a));
+    assert(issame({1}, {1, 3}));
+    
     return 0;
 }
