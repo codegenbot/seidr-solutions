806cbe9a30b384a51c973dc0d4115c315a33c133
beep boop <githubbot@vadim.me>
2024-07-10 11:13:45 +0200

    Solution 64 of CPP/145, ratio 0.5 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/gaisl9om Prompt: 18136501-90a7-4bfe-9871-53a3e5d9124c.cpp:40:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    40 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 18136501-90a7-4bfe-9871-53a3e5d9124c.cpp:8:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     8 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ 18136501-90a7-4bfe-9871-53a3e5d9124c.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ 18136501-90a7-4bfe-9871-53a3e5d9124c.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main() {       |     ^~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,18 +1,28 @@
-sort(nums.begin(), nums.end(), [](int a, int b) {
-    int sum_a = 0, sum_b = 0;
-    if (a < 0) a *= -1;
-    if (b < 0) b *= -1;
-    while (a > 0) {
-        sum_a += a % 10;
-        a /= 10;
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cassert>
+
+using namespace std;
+
+int sum_of_digits(int num) {
+    int sum = 0;
+    while (num != 0) {
+        sum += abs(num) % 10;
+        num /= 10;
     }
-    while (b > 0) {
-        sum_b += b % 10;
-        b /= 10;
-    }
-    if (sum_a == sum_b) {
-        return a < b;
-    }
-    return sum_a < sum_b;
-});
-return nums;
+    return sum;
+}
+
+vector<int> order_by_points(const vector<int>& nums) {
+    vector<int> sorted_nums = nums; 
+    sort(sorted_nums.begin(), sorted_nums.end(), [&](int a, int b) {
+        int sum_a = sum_of_digits(a);
+        int sum_b = sum_of_digits(b);
+        if (sum_a == sum_b) {
+            return a < b;
+        }
+        return sum_a < sum_b;
+    });
+    return sorted_nums;
+}
