2588480cf5e5865da27d581e2bfd94237ec3f8fe
beep boop <githubbot@vadim.me>
2024-07-11 00:42:11 +0200

    Solution 28 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sr9olerd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/27307fe7-218e-4eb6-9405-ea4370df1 a5b.py", line 21, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/27307fe7-218e-4eb6-9405-ea4370df1 a5b.py", line 18, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,16 +1,9 @@
-```
 def cycpattern_check(a, b):
     if len(b) > len(a):
         return False
+
     for i in range(len(a)):
-        k = i
-        j = 0
-        while j < len(b):
-            if a[k] != b[j]:
-                break
-            k = (k + 1) % len(a)
-            j += 1
-        if j == len(b):
-            if all(a[k:k+len(b)]==b for k in range(i+len(b),len(a))):
-                return True
+        if a[i:] + a[:i] == b:
+            return True
+
     return False
