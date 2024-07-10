bf2d914926e1f676fe3de8946c161e38aecb3b59
beep boop <githubbot@vadim.me>
2024-07-10 02:51:00 +0200

    Solution 30 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5oj21un6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2169c905-16f6-4b10-92bd-070504c58 60b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,13 +1,13 @@
-```
-import re
-
 def filter_integers() -> None:
     while True:
-        print("Please enter a comma-separated list of values (separated by commas):")
-        user_input = input()
+        user_input = input(
+            "Please enter a comma-separated list of values (separated by commas): "
+        )
 
         if not re.match(r"^[0-9]+(,[0-9]+)*$", user_input):
-            print("Invalid input. Please enter a valid list of comma-separated integers.")
+            print(
+                "Invalid input. Please enter a valid list of comma-separated integers."
+            )
         else:
             break
 
@@ -18,9 +18,7 @@ def filter_integers() -> None:
         min_value = min(user_values)
 
         print(f"The integers from the input are: {user_values}")
-        print(f"Average Value: {avg_value}")
+        print(f"Average Value: {avg_value:.2f}")
         print(f"Minimum Value: {min_value}")
     except ValueError:
         print("Invalid input. Please enter a valid list of comma-separated integers.")
-
-filter_integers()
