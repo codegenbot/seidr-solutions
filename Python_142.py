46276f92b41d110d4cb246dbf1b8533d41bda939
beep boop <githubbot@vadim.me>
2024-07-11 01:37:47 +0200

    Solution 30 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/be60kwzt Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a0b015a0-76ab-496d-bc5f-35039a6f5 980.py", line 45, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a0b015a0-76ab-496d-bc5f-35039a6f5 980.py", line 15, in check     sum_squares( TypeError: sum_squares() takes 0 positional arguments but 1 was given

--- Python_142.py
+++ Python_142.py
@@ -1,3 +1,10 @@
-def check(func):
-    result = func()
-    return result
+def sum_squares(user_input=""):
+    numbers = user_input.split()
+    for i in range(len(numbers)):
+        while True:
+            try:
+                num = float(numbers[i])
+                break
+            except ValueError as e:
+                print(e)
+    return sum([float(i) ** 2 for i in numbers])
