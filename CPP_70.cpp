05ce4532b181dd9ad491f8e8ad24b8933b470713
beep boop <githubbot@vadim.me>
2024-07-10 04:30:04 +0200

    Solution 94 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/c6jnr1hd Prompt: /usr/include/c++/11/bits/stl_uninitialized.h:704:44:   required from ‘_ForwardIt erator std::__uninitialized_default_n_a(_ForwardIterator, _Size, std::allocator< _Tp>&) [with _ForwardIterator = int*; _Size = long unsigned int; _Tp = int]’ /usr/include/c++/11/bits/stl_vector.h:1606:36:   required from ‘void std::vector <_Tp, _Alloc>::_M_default_initialize(std::vector<_Tp, _Alloc>::size_type) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = l ong unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:512:9:   required from ‘std::vector<_Tp, _ Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocator_type&) [with  _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = long unsigned int; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<int >]’ 5b55773d-835d-4ad6-85dd-d2f6b91f226b.cpp:24:26:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -21,25 +21,29 @@ int main() {
     std::cout << "Enter the size of first vector: ";
     std::cin >> n1;
     
-    std::vector<int> a;
+    std::vector<int> a(n1, 0);
     
-    std::cout << "Enter elements for vector 1: " << std::endl; 
+    std::cout << "Enter element 1 to " << n1 << ": " << std::endl; 
+
     for(int i = 0; i < n1; i++) {
+        std::cout << "Enter element " << i+1 << ": ";
         int temp; 
         std::cin >> temp;
-        a.push_back(temp);
+        a[i] = temp;
     }
     
     std::cout << "Enter the size of second vector: ";
     std::cin >> n2;
 
-    std::vector<int> b;
+    std::vector<int> b(n2, 0);
     
-    std::cout << "Enter elements for vector 2: " << std::endl; 
+    std::cout << "Enter element 1 to " << n2 << ": " << std::endl; 
+
     for(int i = 0; i < n2; i++) {
+        std::cout << "Enter element " << i+1 << ": ";
         int temp; 
         std::cin >> temp;
-        b.push_back(temp);
+        b[i] = temp;
     }
     
     if(issame(a, b)) {
