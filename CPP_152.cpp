8881a5c166413ecbaf05ab2b564a4e4d9c455763
beep boop <githubbot@vadim.me>
2024-07-10 22:13:12 +0200

    Solution 30 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dd4t2i9v Prompt:       |                    ^~~ ad174ca2-cef7-4990-b2dd-f3b09cc24a05.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ad174ca2-cef7-4990-b2dd-f3b09cc24a05.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ad174ca2-cef7-4990-b2dd-f3b09cc24a05.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ad174ca2-cef7-4990-b2dd-f3b09cc24a05.cpp:13: ad174ca2-cef7-4990-b2dd-f3b09cc24a05.cpp: In function ‘int main()’: ad174ca2-cef7-4990-b2dd-f3b09cc24a05.cpp:23:20: error: ‘compare’ was not declare d in this scope    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ ad174ca2-cef7-4990-b2dd-f3b09cc24a05.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,37 +1,20 @@
 #include <vector>
-#include <iostream>
-
-bool issame(int a, int b) {
-    return (a == b);
-}
 
 std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
     std::vector<int> result;
-    for(int i = 0; i < game.size(); i++) {
-        if(issame(game[i], guess[i])) {
-            result.push_back(0);
+    for (int i = 0; i < game.size(); i++) {
+        if (game[i] == guess[i]) {
+            result.push_back(2);
         } else {
-            result.push_back(abs(guess[i] - game[i]));
+            bool isSameNumberInGameAndGuess = false;
+            for(int j = 0; j< game.size(); j++){
+                if(game[j] == guess[i]){
+                    isSameNumberInGameAndGuess = true;
+                    break;
+                }
+            }
+            result.push_back(isSameNumberInGameAndGuess ? 1 : 0);
         }
     }
     return result;
-}
-
-int main() {
-    std::vector<int> game, guess;
-    int n;
-    std::cout << "Enter number of rounds: ";
-    std::cin >> n;
-    
-    for(int i = 0; i < n; i++) {
-        std::cout << "Enter score for round " << (i+1) << ": ";
-        std::cin >> game[i] >> guess[i];
-    }
-    
-    std::vector<int> output = compare(game, guess);
-    
-    for(int i : output) {
-        std::cout << i << " ";
-    }
-    return 0;
 }
