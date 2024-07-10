c33b35e8d7a62ace0a77335c7db230ac58d57912
beep boop <githubbot@vadim.me>
2024-07-11 00:58:28 +0200

    Solution 97 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/muttgf56 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d3b540f-31bb-4fe9-82b1-5e69687b9 db2.py", line 6     continue     ^^^^^^^^ SyntaxError: 'continue' not properly in loop

--- Python_49.py
+++ Python_49.py
@@ -1,46 +1,4 @@
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
 def modp(num, p):
     if num < p:
-        return num
-    res = 1
-    while num > 1:
-        if num & 1:
-            res = (res * num) % p
-        num //= 2
-    return res
-
-
-def main():
-    while True:
-        num = input("Enter a number: ")
-        p = input("Enter the prime number: ")
-
-        while True:
-            try:
-                num = int(num)
-                p = int(p)
-                break
-            except ValueError:
-                print("Invalid input. Please enter numbers.")
-                num = input("Enter a number: ")
-                p = input("Enter the prime number: ")
-
-        if is_prime(p):
-            n = num**p
-            result = modp(n, p)
-            print(f"{num} to the power of {p} mod {p} is {result}")
-        else:
-            print("The given prime number is not valid. Please enter a prime number.")
-    pass
-
-
-if __name__ == "__main__":
-    main()
+        print(f"Number {num} is less than the prime number {p}. Please enter a valid input.")
+        return
