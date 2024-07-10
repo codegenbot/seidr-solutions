34d1d54a092ac5016ee2843b9c8b1362f7c3c55c
beep boop <githubbot@vadim.me>
2024-07-10 06:26:00 +0200

    Solution 20 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lerjqzl3 Prompt:    24 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ bfce9a40-a15a-417b-b6e5-308c515a43af.cpp:24:36: error: expected primary-expressi on before ‘float’    24 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ bfce9a40-a15a-417b-b6e5-308c515a43af.cpp:24:43: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from bfce9a40-a15a-417b-b6e5-308c515a43af.cpp:23: bfce9a40-a15a-417b-b6e5-308c515a43af.cpp: In function ‘int main()’: bfce9a40-a15a-417b-b6e5-308c515a43af.cpp:33:20: error: ‘sort_even’ was not decla red in this scope    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ bfce9a40-a15a-417b-b6e5-308c515a43af.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,12 +1,35 @@
+```cpp
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<float> a, vector<float> b) {
+bool issame(std::vector<float> a, std::vector<float> b) {
     return a == b;
 }
 
-vector<float> sort_even(vector<float> l){
-    vector<float> result(l.size());
+std::vector<float> sort_even(std::vector<float> l){
+    if(l.size() <= 0) {
+        // Handle the error case where l is empty
+        std::vector<float> result = l; 
+        for(int i = 0; i < l.size(); i++){
+            if(i % 2 == 0){
+                vector<float> temp;
+                for(auto j: l | std::views::slices(0, l.size(), 2)){
+                    temp.push_back(j);
+                }
+                sort(temp.begin(), temp.end());
+                result[i] = temp[0];
+                for(int k = 1; k < temp.size(); k++){
+                    if(i + k * 2 < l.size()){
+                        result[i+k*2] = temp[k];
+                    }
+                }
+            }else{
+                result[i] = l[i];
+            }
+        }
+        return result;
+    }
+    std::vector<float> result(l.size());
     for(int i = 0; i < l.size(); i++){
         if(i % 2 == 0){
             vector<float> temp;
@@ -28,6 +51,6 @@ vector<float> sort_even(vector<float> l){
 }
 
 int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), { -12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
     return 0;
 }
