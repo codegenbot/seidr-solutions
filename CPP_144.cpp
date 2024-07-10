bb771e5d87bc529331ecf264a7af837cb96539cd
beep boop <githubbot@vadim.me>
2024-07-10 04:43:49 +0200

    Solution 3 of CPP/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/f1i6mjz9 Prompt: /usr/include/c++/11/sstream:142:39: error: ‘const __string_type’ {aka ‘const cla ss std::__cxx11::basic_string<char>’} has no member named ‘size’   142 |         _M_string(__str.data(), __str.size(), __str.get_allocator())       |                                 ~~~~~~^~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from a15c624f-e4e0-4ee4-9502-cc3dc871955f.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ a15c624f-e4e0-4ee4-9502-cc3dc871955f.cpp:19:5:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -6,8 +6,12 @@ using namespace std;
 bool simplify(string x, string n) {
     int a = 0, b = 0, c = 0, d = 0;
     stringstream s1(x), s2(n);
-    s1 >> a >> '/' >> b;
-    s2 >> c >> '/' >> d;
-    
+    string s1str, s2str;
+    char ch;
+    getline(s1, s1str, ' '); 
+    s1 >> a >> ch >> b; 
+    getline(s2, s2str, ' '); 
+    s2 >> c >> ch >> d; 
+
     return (a*d == b*c);
 }
