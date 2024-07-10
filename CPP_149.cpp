aace71324a02dead1eceb8b46844cd0d46aa933e
beep boop <githubbot@vadim.me>
2024-07-10 20:25:57 +0200

    Solution 23 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ptpr8f4c Prompt:       |           ^~~~~~ In file included from ab8d8231-6bfc-4cd4-a629-dc2b0eb7c253.cpp:4: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ab8d8231-6bfc-4cd4-a629-dc2b0eb7c253.cpp:28:37: error: ‘string’ was not declared  in this scope    28 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ab8d8231-6bfc-4cd4-a629-dc2b0eb7c253.cpp:28:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from ab8d8231-6bfc-4cd4-a629-dc2b0eb7c253.cpp:6: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from ab8d8231-6bfc-4cd4-a629-dc2b0eb7c253.cpp:6: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ ab8d8231-6bfc-4cd4-a629-dc2b0eb7c253.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,30 +1,27 @@
-```c++
-#include <initializer_list>
-
-#include <vector>
 #include <algorithm>
 #include <string>
+#include <vector>
 
-bool issame(std::vector<std::string> a, std::vector b) {
-    if (a.size() != size()) {
-        return false;
+#include <initializer_list>
+
+namespace std {
+
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        throw runtime_error("Vectors are not the same size");
     }
     for (int i = 0; i < a.size(); i++) {
-        std::vector<std::string> temp = {a[i], b[i]};
-        std::sort(temp.begin(), temp.end());
+        vector<string> temp = {a[i], b[i]};
+        sort(temp.begin(), temp.end());
         if (temp[0] != temp[1]) {
-            return false;
+            throw runtime_error("Vectors are not the same");
         }
     }
     return true;
 }
 
-std::vector<std::string> sorted_list_sum(std::vector<std::string> v) {
-    std::sort(v.begin(), v.end());
+vector<string> sorted_list_sum(vector<string> v) {
+    sort(v.begin(), v.end());
     return v;
 }
-
-int main() {
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"ccc", "dd", "aaaa", "bbbb"}) == true);
-    return 0;
 }
