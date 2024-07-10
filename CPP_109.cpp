bb7726c21843dc632998de092a3cdf0822e8212c
beep boop <githubbot@vadim.me>
2024-07-10 17:43:16 +0200

    Solution 30 of CPP/109, ratio 0.8 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/xnrq9wza Prompt:                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from ccc66f0b-9847-40ea-893c-b2965c995d88.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from ccc66f0b-9847-40ea-893c-b2965c995d88.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ccc66f0b-9847-40ea-893c-b2965c995d88.cpp:3:27: error: expected primary-expressio n before ‘int’     3 | bool move_one_ball(vector<int> arr) {       |                           ^~~ In file included from ccc66f0b-9847-40ea-893c-b2965c995d88.cpp:18: ccc66f0b-9847-40ea-893c-b2965c995d88.cpp: In function ‘int main()’: ccc66f0b-9847-40ea-893c-b2965c995d88.cpp:20:26: error: ‘move_one_ball’ cannot be  used as a function    20 |     assert (move_one_ball({})==true);       |             ~~~~~~~~~~~~~^~~~

--- CPP_109.cpp
+++ CPP_109.cpp
@@ -1,9 +1,16 @@
-bool move_one_ball(vector<int> arr) {
-    int max = *max_element(arr.begin(), arr.end());
+#include <algorithm>
+#include <vector>
+
+bool move_one_ball(std::vector<int> arr) {
+    if (arr.empty()) return true;
+
+    std::sort(arr.begin(), arr.end());
+    
+    int minPos = std::distance(arr.begin(), std::min_element(arr.begin(), arr.end()));
+    
     for (int i = 0; i < arr.size(); i++) {
-        if (arr[i] != max - i) {
-            return false;
-        }
+        if ((i != minPos) && (arr[i] == arr[minPos])) return false;
     }
+    
     return true;
 }
