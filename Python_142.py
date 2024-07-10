e8475de1837ba0b3be7bbf0d872f0ae487aacee9
beep boop <githubbot@vadim.me>
2024-07-10 17:40:35 +0200

    Solution 24 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/s103d66u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0f033e3b-f95c-4e7c-bbdb-75023b90d 566.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,14 +1,9 @@
+```
 def sum_squares(lst):
     total = 0
     for num in lst:
         if type(num) != int:
             return "Invalid input: list must contain integers."
-        elif num % 3 == 0 and num % 4 != 0:
-            total += num ** 2
-        elif num % 4 == 0 and num % 3 != 0:
-            total += num ** 3
+        elif (num % 3 == 0 and num % 4 != 0) or (num % 4 == 0 and num % 3 != 0):
+            total += num ** ((num % 4 == 0) * 2 + 1)
     return total
-
-lst = input("Enter a list of numbers separated by space: ")
-lst = [int(i) for i in lst.split()]
-print(sum_squares(lst))
