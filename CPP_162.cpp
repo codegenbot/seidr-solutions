23a2eedeaa6c2bd90d4824ae41daec84350d97c5
beep boop <githubbot@vadim.me>
2024-07-10 08:47:58 +0200

    Solution 65 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/pgnf728q Prompt:  in this scope    10 |     MD5_CTX ctx;       |     ^~~~~~~ 4e59ec77-434b-4a85-9a09-c0b33cd73c1d.cpp:11:15: error: ‘ctx’ was not declared in  this scope    11 |     MD5_Init(&ctx);       |               ^~~ 4e59ec77-434b-4a85-9a09-c0b33cd73c1d.cpp:11:5: error: ‘MD5_Init’ was not declare d in this scope    11 |     MD5_Init(&ctx);       |     ^~~~~~~~ 4e59ec77-434b-4a85-9a09-c0b33cd73c1d.cpp:14:9: error: ‘MD5_Update’ was not decla red in this scope    14 |         MD5_Update(&ctx, ptr, 1);       |         ^~~~~~~~~~ 4e59ec77-434b-4a85-9a09-c0b33cd73c1d.cpp:17:15: error: ‘md’ was not declared in this scope    17 |     MD5_Final(md, &ctx);       |               ^~ 4e59ec77-434b-4a85-9a09-c0b33cd73c1d.cpp:17:5: error: ‘MD5_Final’ was not declar ed in this scope    17 |     MD5_Final(md, &ctx);       |     ^~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -5,18 +5,17 @@ using namespace std;
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
-    unsigned char md[16];
+    unsigned char md[MD5_DIGEST_LENGTH];
     MD5_CTX ctx;
     MD5_Init(&ctx);
     const char* ptr = text.c_str();
     while (*ptr) {
-        MD5_Update(&ctx, ptr, 1);
-        ptr++;
+        MD5_Update(&ctx, ptr++, 1);
     }
     MD5_Final(md, &ctx);
 
     string result;
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
         char buffer[3];
         sprintf(buffer, "%02x", md[i]);
         result += buffer;
