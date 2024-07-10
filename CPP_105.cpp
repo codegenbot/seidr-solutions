bfed2184d28fe8cfef89a828bb87c17c6059ee55
beep boop <githubbot@vadim.me>
2024-07-10 17:47:15 +0200

    Solution 38 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tu21n5fh Prompt:       |                                 ^~~~~~ In file included from bcaee2c1-606a-4ff7-b7a9-1d58b7025491.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ bcaee2c1-606a-4ff7-b7a9-1d58b7025491.cpp:50:44: error: ‘b’ was not declared in t his scope    50 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bcaee2c1-606a-4ff7-b7a9-1d58b7025491.cpp:50:45: error: expression list treated a s compound expression in initializer [-fpermissive]    50 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from bcaee2c1-606a-4ff7-b7a9-1d58b7025491.cpp:49: bcaee2c1-606a-4ff7-b7a9-1d58b7025491.cpp: In function ‘int main()’: bcaee2c1-606a-4ff7-b7a9-1d58b7025491.cpp:59:20: error: ‘by_length’ was not decla red in this scope    59 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ bcaee2c1-606a-4ff7-b7a9-1d58b7025491.cpp:59:19: error: ‘issame’ cannot be used a s a function    59 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -3,60 +3,47 @@
 #include <algorithm>
 
 std::vector<std::string> by_length(std::vector<int> arr) {
-    std::vector<std::string> result;
-    
+    std::vector<std::int32_t> temp;
     for (int i : arr) {
         if (i >= 1 && i <= 9)
-            switch (i) {
-                case 1:
-                    result.push_back("One");
-                    break;
-                case 2:
-                    result.push_back("Two");
-                    break;
-                case 3:
-                    result.push_back("Three");
-                    break;
-                case 4:
-                    result.push_back("Four");
-                    break;
-                case 5:
-                    result.push_back("Five");
-                    break;
-                case 6:
-                    result.push_back("Six");
-                    break;
-                case 7:
-                    result.push_back("Seven");
-                    break;
-                case 8:
-                    result.push_back("Eight");
-                    break;
-                case 9:
-                    result.push_back("Nine");
-            }
+            temp.push_back(i);
+    }
+    
+    std::sort(temp.begin(), temp.end());
+    
+    std::reverse(temp.begin(), temp.end());
+    
+    std::vector<std::string> result;
+    for (int i : temp) {
+        switch (i) {
+            case 1:
+                result.push_back("One");
+                break;
+            case 2:
+                result.push_back("Two");
+                break;
+            case 3:
+                result.push_back("Three");
+                break;
+            case 4:
+                result.push_back("Four");
+                break;
+            case 5:
+                result.push_back("Five");
+                break;
+            case 6:
+                result.push_back("Six");
+                break;
+            case 7:
+                result.push_back("Seven");
+                break;
+            case 8:
+                result.push_back("Eight");
+                break;
+            case 9:
+                result.push_back("Nine");
+        }
     }
     
     return result;
-}
-
-int main() {
-    std::vector<int> arr;
-    int n;
-    std::cout << "Enter the number of integers: ";
-    std::cin >> n;
-    for (int i = 0; i < n; i++) {
-        int num;
-        std::cout << "Enter integer: ";
-        std::cin >> num;
-        arr.push_back(num);
-    }
-    
-    std::vector<std::string> result = by_length(arr);
-    
-    for (const auto& s : result) {
-        std::cout << s << " ";
-    }
-    
-    return 0;
 }
