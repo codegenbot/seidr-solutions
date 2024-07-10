2d6ab0a43c6ecee7b84882f9d20006ab5dd59bd4
beep boop <githubbot@vadim.me>
2024-07-10 07:48:43 +0200

    Solution 42 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/q7w4gwkk Prompt: In file included from 086842d6-f1a9-46ad-99c3-55351420a7fc.cpp:20: 086842d6-f1a9-46ad-99c3-55351420a7fc.cpp: In function ‘int main()’: 086842d6-f1a9-46ad-99c3-55351420a7fc.cpp:30:20: error: ‘sort_array’ was not decl ared in this scope    30 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,22 +1,3 @@
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 int main() {
     assert(is_same(sort_vector({2,4,8,16,32}) , {2, 4, 8, 16, 32}));
-}
-
-vector<int> sort_vector(vector<int> arr) {
-    vector<int> result = arr;
-    sort(result.begin(), result.end(),
-         [](int a, int b) {
-             int ones_a = __builtin_popcount(a);
-             int ones_b = __builtin_popcount(b);
-             if (ones_a == ones_b)
-                 return a < b;
-             else
-                 return ones_a < ones_b;
-         });
-    return result;
 }
