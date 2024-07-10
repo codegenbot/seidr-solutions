5bd393f52056eee8f05cc161b7b25ea96a2d9d13
beep boop <githubbot@vadim.me>
2024-07-10 13:23:07 +0200

    Solution 85 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/0r0b8tjy Prompt:       |             ^~~~~~ 56e1cafc-6753-4c28-8d2e-18dd8c3f3c6e.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 56e1cafc-6753-4c28-8d2e-18dd8c3f3c6e.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 56e1cafc-6753-4c28-8d2e-18dd8c3f3c6e.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 56e1cafc-6753-4c28-8d2e-18dd8c3f3c6e.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 56e1cafc-6753-4c28-8d2e-18dd8c3f3c6e.cpp:22: 56e1cafc-6753-4c28-8d2e-18dd8c3f3c6e.cpp: In function ‘int main()’: 56e1cafc-6753-4c28-8d2e-18dd8c3f3c6e.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,30 +1,25 @@
-#include <iostream>
 #include <vector>
 #include <cassert>
 
-std::vector<int> even_odd_count(int n) {
-    int even_count = 0, odd_count = 0;
-    
-    while (n != 0) {
-        if (n % 2 == 0) {
-            even_count++;
-        } else {
-            odd_count++;
-        }
-        n /= 10;
-    }
-    
-    return {even_count, odd_count};
-}
-
-bool assert_equal(const std::vector<int>& a, const std::vector<int>& b) {
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
+std::vector<int> even_odd_count(int num) {
+    int even = 0, odd = 0;
+    while(num != 0) {
+        if(num % 2 == 0)
+            even++;
+        else
+            odd++;
+        num /= 10;
+    }
+    return {even, odd};
+}
+
 int main() {
-    assert_equal(even_odd_count(0), std::vector<int>{1, 0});
-    assert_equal(even_odd_count(123456), std::vector<int>{3, 3});
-    assert_equal(even_odd_count(-87654), std::vector<int>{2, 3});
-    
+    assert(issame(even_odd_count(0) , {1, 0}));
+    assert(issame(even_odd_count(123456), {3, 3}));
+    assert(issame(even_odd_count(-87654), {2, 3}));
     return 0;
 }
