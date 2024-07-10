c9b789cb7bf128a6da9b81af0092b48be41de153
beep boop <githubbot@vadim.me>
2024-07-10 04:02:16 +0200

    Solution 27 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/6oj4lbge Prompt: /usr/include/c++/11/bits/stl_iterator.h:1693:5: note:   template argument deduct ion/substitution failed: d02ae6e3-58f4-4658-a4e6-57aad76d8c08.cpp:10:89: note:   ‘std::string’ {aka ‘std: :__cxx11::basic_string<char>’} is not derived from ‘const std::move_iterator<_It eratorL>’    10 | sci) || (sai == sbi - sci) || (sai == sci + sbi) || (sai == sci - sbi))       |                                                                   ^~~ d02ae6e3-58f4-4658-a4e6-57aad76d8c08.cpp: At global scope: d02ae6e3-58f4-4658-a4e6-57aad76d8c08.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ d02ae6e3-58f4-4658-a4e6-57aad76d8c08.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ d02ae6e3-58f4-4658-a4e6-57aad76d8c08.cpp: In function ‘bool any_int(float, float , float)’: d02ae6e3-58f4-4658-a4e6-57aad76d8c08.cpp:14:1: warning: control reaches end of n on-void function [-Wreturn-type]    14 | }       | ^

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,10 +1,21 @@
+#include <iostream>
+#include <assert.h>
+#include <string>
+#include <cmath>
+
 bool any_int(float a, float b, float c) {
-    std::string sai = std::to_string((int)a);
-    std::string sbi = std::to_string((int)b);
-    std::string sci = std::to_string((int)c);
+    std::string sai = std::to_string(round(a));
+    std::string sbi = std::to_string(round(b));
+    std::string sci = std::to_string(round(c));
 
     if ((sai == sbi + sci) || (sai == sbi - sci) || (sai == sci + sbi) || (sai == sci - sbi))
         return true;
     else
         return false;
+}
+
+int main() {
+    assert(any_int(3.01f, 4.0f, 7.0f) == false);
+    std::cout << "Code runs correctly." << std::endl;
+    return 0;
 }
