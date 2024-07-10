61a2b97d22449b5810937d376314a003bbf7b3d8
beep boop <githubbot@vadim.me>
2024-07-10 12:09:37 +0200

    Solution 85 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/vsggr6pn Prompt:     5 | 0f2d0177-0d7f-4306-b700-fbba39f7eff8.cpp:30:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    30 | int main(){       |         ^~ 0f2d0177-0d7f-4306-b700-fbba39f7eff8.cpp:30:9: note: remove parentheses to defau lt-initialize a variable    30 | int main(){       |         ^~       |         -- 0f2d0177-0d7f-4306-b700-fbba39f7eff8.cpp:30:9: note: or replace parentheses with  braces to value-initialize a variable 0f2d0177-0d7f-4306-b700-fbba39f7eff8.cpp:30:11: error: a function-definition is not allowed here before ‘{’ token    30 | int main(){       |           ^ 0f2d0177-0d7f-4306-b700-fbba39f7eff8.cpp:33:2: error: expected ‘}’ at end of inp ut    33 | }       |  ^ 0f2d0177-0d7f-4306-b700-fbba39f7eff8.cpp:16:12: note: to match this ‘{’    16 | int main() {       |            ^

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,4 +1,4 @@
-int main {
-    assert(is_same(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-100, 1});
+int main() {
+    assert(is_same(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-100, 1}));
     return 0;
 }
