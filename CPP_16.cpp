d7565235db4ec41f59edbdd40de01a391fd8ee5d
beep boop <githubbot@vadim.me>
2024-07-10 19:56:25 +0200

    Solution 6 of CPP/16, ratio 0.6 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ma2hacqa Prompt: Complete the following code given the task description and function signature.

--- CPP_16.cpp
+++ CPP_16.cpp
@@ -1,15 +1,21 @@
-vector<char> characters;
+#include<stdio.h>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
 
-for (char c : str) {
-    int index = 0;
-    for (; index < characters.size(); ++index) {
-        if (tolower(c) == tolower(characters[index])) {
-            break;
-        }
+int count_distinct_characters(string str) {
+    string temp = "";
+    for (char c : str) {
+        if (tolower(c) > 96 && tolower(c) < 123 || tolower(c) > 64 && tolower(c) < 91)
+            temp += tolower(c);
     }
-    if (index == characters.size()) {
-        characters.push_back(c);
+    vector<char> v(temp.begin(), temp.end());
+    sort(v.begin(), v.end());
+    int count = 0;
+    for (int i = 0; i < v.size(); i++) {
+        if (i == 0 || v[i] != v[i - 1])
+            count++;
     }
+    return count;
 }
-
-return characters.size();
