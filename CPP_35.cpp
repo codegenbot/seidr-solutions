61ca6984ed7e817f30c55d6a955180caa1abac29
beep boop <githubbot@vadim.me>
2024-07-10 16:41:52 +0200

    Solution 72 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/oo2tiyws Prompt: 32f8450d-e80f-44da-9ad9-642bca119f9c.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return *max_element(l.begin(), l.end());       | ^~~~~~ In file included from 32f8450d-e80f-44da-9ad9-642bca119f9c.cpp:3: 32f8450d-e80f-44da-9ad9-642bca119f9c.cpp: In function ‘int main()’: 32f8450d-e80f-44da-9ad9-642bca119f9c.cpp:5:17: error: ‘max_element’ was not decl ared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ 32f8450d-e80f-44da-9ad9-642bca119f9c.cpp:5:13: error: ‘abs’ was not declared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,8 +1,1 @@
-```cpp
-#include <algorithm>
-using namespace std;
-
-int findMax(int l[]) {
-    vector<int> v(l, l + sizeof(l)/sizeof(l[0]));
-    return *max_element(v.begin(), v.end());
-}
+return *max_element(l.begin(), l.end());
