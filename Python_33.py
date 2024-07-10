25ba9dae2a10d32d7231f7452105a53ac15d14cb
beep boop <githubbot@vadim.me>
2024-07-10 19:50:42 +0200

    Solution 51 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4wfttiik Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b89821a3-dee3-41f2-a186-eaa719eb8 d4a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,8 +1,9 @@
+```
 def sort_third(l: list):
     result = sorted([i for i in l if i % 3 != 0])
     if len(l) % 3 == 1:
         return [l[-1]]
     elif len(l) % 3 == 2:
-        return [l[-1], l[-2]]
+        return [l[-2], l[-1]]
     else:
         return result
