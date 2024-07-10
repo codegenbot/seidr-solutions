840afda45a2a9e6930cf9f44cc36e9a907b3f8f8
beep boop <githubbot@vadim.me>
2024-07-10 21:06:47 +0200

    Solution 8 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/99vwaciq Prompt:       |                              ^~~~~~ ac3f6db3-a8be-473a-aa0d-b96b1e495a22.cpp:38:37: error: ‘string’ was not declared  in this scope    38 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ac3f6db3-a8be-473a-aa0d-b96b1e495a22.cpp:38:44: error: ‘b’ was not declared in t his scope    38 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ac3f6db3-a8be-473a-aa0d-b96b1e495a22.cpp:38:45: error: expression list treated a s compound expression in initializer [-fpermissive]    38 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ac3f6db3-a8be-473a-aa0d-b96b1e495a22.cpp:37: ac3f6db3-a8be-473a-aa0d-b96b1e495a22.cpp: In function ‘int main()’: ac3f6db3-a8be-473a-aa0d-b96b1e495a22.cpp:47:20: error: ‘reverse_delete’ was not declared in this scope    47 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ ac3f6db3-a8be-473a-aa0d-b96b1e495a22.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,6 +1,19 @@
-```
+#include <vector>
 #include <string>
-using namespace std;
+
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
 
 vector<string> reverse_delete(string s, string c) {
     vector<string> result;
@@ -34,8 +47,4 @@ vector<string> reverse_delete(string s, string c) {
     }
     
     return result;
-}
-
-bool issame(vector<string> a, vector<string>b){
-    return a == b;
 }
