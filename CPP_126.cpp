24d2beda0e70419e3432437fc3d9b7689de5a095
beep boop <githubbot@vadim.me>
2024-07-11 00:32:31 +0200

    Solution 56 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vlwtmqlv Prompt:       | ^~~~~~~~~~~~~ e9bc8eeb-f2ed-406c-baef-700f42e31230.cpp:24:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    24 | int main(){       |         ^~ e9bc8eeb-f2ed-406c-baef-700f42e31230.cpp:24:9: note: remove parentheses to defau lt-initialize a variable    24 | int main(){       |         ^~       |         -- e9bc8eeb-f2ed-406c-baef-700f42e31230.cpp:24:9: note: or replace parentheses with  braces to value-initialize a variable e9bc8eeb-f2ed-406c-baef-700f42e31230.cpp:24:11: error: a function-definition is not allowed here before ‘{’ token    24 | int main(){       |           ^ e9bc8eeb-f2ed-406c-baef-700f42e31230.cpp:26:2: error: expected ‘}’ at end of inp ut    26 | }       |  ^ e9bc8eeb-f2ed-406c-baef-700f42e31230.cpp:7:32: note: to match this ‘{’     7 | bool is_sorted(vector<int> lst){       |                                ^

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,5 +1,5 @@
-int main() {
+int main
+{
     vector<int> lst = {1,2,3,4};
-    cout << (is_sorted(lst) ? "True" : "False") << endl;
-    return 0;
+    cout << (is_sorted(lst) ? "True" : "False") << endl; 
 }
