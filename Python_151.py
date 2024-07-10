5f54e617dca13eb5eb9add13319f7755b277b590
beep boop <githubbot@vadim.me>
2024-07-10 20:00:31 +0200

    Solution 29 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pt9a9h2k Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1d8d0885-72b1-4120-ba45-cc381292d 6a7.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1d8d0885-72b1-4120-ba45-cc381292d 6a7.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,2 +1,7 @@
-def double_the_difference(lst): 
-    return (max(lst) - min(lst)) * 2
+def total_double_difference(lst1, lst2):
+    first_half = [i**2 for i in lst1[: len(lst1) // 2] if isinstance(i, int) and i >= 0]
+    second_half = [
+        j**2 for j in lst2[: len(lst2) // 2] if isinstance(j, int) and j >= 0
+    ]
+
+    return abs(sum(first_half) - sum(second_half))
