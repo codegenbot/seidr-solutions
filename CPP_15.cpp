6040b37a3ae2edda8aca727c53f863636ba31923
beep boop <githubbot@vadim.me>
2024-07-10 19:47:16 +0200

    Solution 0 of CPP/15, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/90nemcl7 Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_15.cpp
@@ -0,0 +1,10 @@
+#include<string>
+using namespace std;
+
+string string_sequence(int n){
+    string result = "";
+    for(int i=0; i<=n; i++){
+        result += to_string(i) + " ";
+    }
+    return result;
+}
