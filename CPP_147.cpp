62487d872bd4081773a6cad11990686a81be3881
beep boop <githubbot@vadim.me>
2024-07-10 21:11:16 +0200

    Solution 50 of CPP/147, ratio 0.25 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fs9tnvpd Prompt: 006079d8-91c6-4116-920d-8ea46f3b3868.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 006079d8-91c6-4116-920d-8ea46f3b3868.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_147.cpp
+++ CPP_147.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 int get_matrix_triples(int n) {
     std::vector<int> a(n);
@@ -23,5 +24,6 @@ int main() {
     int n;
     std::cin >> n;
     std::cout << get_matrix_triples(n) << std::endl;
+    // assert(get_matrix_triples(100) == 53361);
     return 0;
 }
