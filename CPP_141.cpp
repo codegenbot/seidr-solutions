3f385ab9280b437b1280fa5dc1c5c1afbc089d97
beep boop <githubbot@vadim.me>
2024-07-10 23:25:16 +0200

    Solution 6 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/p2ju98kx Prompt: 9d1a733b-725c-456c-ab0c-3af573ff6151.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string file_name_check(string file_name){       | ^~~~~~ In file included from 9d1a733b-725c-456c-ab0c-3af573ff6151.cpp:24: 9d1a733b-725c-456c-ab0c-3af573ff6151.cpp: In function ‘int main()’: 9d1a733b-725c-456c-ab0c-3af573ff6151.cpp:26:13: error: ‘file_name_check’ was not  declared in this scope    26 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,18 +1,25 @@
-string file_name_check(string file_name){
-    int count = 0;
-    bool foundDot = false;
-    for(int i=0; i<file_name.size(); i++){
+```
+#include <string>
+
+std::string file_name_check(std::string file_name){
+    int digit_count = 0;
+    bool found_dot = false;
+    bool valid_prefix = true;
+
+    for(int i = 0; i < file_name.size(); i++){
         if(isdigit(file_name[i])){
-            count++;
-            if(count > 3) return "No";
+            digit_count++;
         }
         else if(file_name[i] == '.'){
-            foundDot = true;
+            found_dot = true;
         }
-        else if(foundDot && (file_name.substr(i).find("txt") != string::npos || file_name.substr(i).find("exe") != string::npos || file_name.substr(i).find("dll") != string::npos)){
-            return "Yes";
+        else{
+            if(!found_dot)
+                valid_prefix = isalpha(file_name[i]);
+            else
+                break;
         }
     }
-    if(count > 3 || !foundDot) return "No";
-    if((file_name[0] < 'a' && file_name[0] != '0') || (file_name[0] > 'z' && file_name[0] < 'A') || (file_name[0] > 'Z')) return "No";
-    return "Yes";
+
+    return (digit_count <= 3 && found_dot && valid_prefix) ? "Yes" : "No";
+}
