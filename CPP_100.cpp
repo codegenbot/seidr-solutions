b207d097942470109cc9fab264e30a69dd4b7c3a
beep boop <githubbot@vadim.me>
2024-07-10 03:45:43 +0200

    Solution 9 of CPP/100, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/w8wumm4k Prompt: In file included from 606290d7-66bd-458c-97cd-f36fde135916.cpp:15: 606290d7-66bd-458c-97cd-f36fde135916.cpp: In function ‘int main()’: 606290d7-66bd-458c-97cd-f36fde135916.cpp:25:31: error: could not convert ‘make_a _pile(8)’ from ‘int’ to ‘std::vector<int>’    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ~~~~~~~~~~~^~~       |                               |       |                               int

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,19 +1,13 @@
 #include <vector>
 using namespace std;
 
-vector<int> make_a_pile(int n) {
-    vector<int> pile;
-    for (int i = 0; i < n; i++) {
-        pile.push_back(i + 1 + 2*(i));
-    }
-    return pile;
-}
-
 bool sameVectors(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-int main() {
-    assert(sameVectors(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
-    // Rest of your code
+vector<int> make_a_pile(int n) {
+    vector<int> pile;
+    for (int i = 2 * n; i >= n; --i)
+        pile.push_back(i);
+    return pile;
 }
