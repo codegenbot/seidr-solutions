a2d9bb6b3e91b43f9c4b7d39e8d1fbb3210aa472
beep boop <githubbot@vadim.me>
2024-07-10 20:46:26 +0200

    Solution 14 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/312dfbf5-0046-49af-9ed3-b95d8eaab d2e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,15 +1,18 @@
-def sort_third(l: list):
+def sort_third(l):
     if len(l) % 3 != 0:
         l += [None] * (3 - len(l) % 3)
-    l1 = sorted([x for i, x in enumerate(l) if i % 3 == 0])
+    l1 = [x for i, x in enumerate(l) if i % 3 == 0]
     l2 = [x for i, x in enumerate(l) if i % 3 == 1]
     l3 = sorted([x for i, x in enumerate(l) if i % 3 == 2])
+
+    sorted_l1 = sorted(l1)
+    sorted_l3 = l3[::-1]
 
     return [
         (
             x
             if i % 3 != 0
-            else (x if i % 3 == 1 else next((y for y in reversed(l3) if y > x), None))
+            else (x if i % 3 == 1 else next(y for y in sorted_l3 if y > x) or None)
         )
-        for i, x in enumerate(l1 + l2 + list(reversed(l3)))
+        for i, x in enumerate(sorted_l1 + [l2[0]] + l3)
     ]
