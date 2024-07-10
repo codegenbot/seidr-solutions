47f802b9a8549b576bb61c5d56be4be89341f3ec
beep boop <githubbot@vadim.me>
2024-07-10 20:02:49 +0200

    Solution 7 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ptpr8f4c Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from fab6a727-81db-4a85-8e68-420de8fb79a9.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ fab6a727-81db-4a85-8e68-420de8fb79a9.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from fab6a727-81db-4a85-8e68-420de8fb79a9.cpp:19: fab6a727-81db-4a85-8e68-420de8fb79a9.cpp: In function ‘int main()’: fab6a727-81db-4a85-8e68-420de8fb79a9.cpp:29:20: error: ‘sorted_list_sum’ was not  declared in this scope    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ fab6a727-81db-4a85-8e68-420de8fb79a9.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,34 +1,27 @@
-```cpp
 #include <vector>
 #include <algorithm>
+#include <string>
 
-bool issame(vector<string> a, vector<string> b) {
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size()) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
-        if (sorted_list_sum({a[i], b[i]}).size() != 1) {
+        std::vector<std::string> temp = {a[i], b[i]};
+        std::sort(temp.begin(), temp.end());
+        if (temp[0] != temp[1]) {
             return false;
         }
     }
     return true;
 }
 
-int sorted_list_sum(vector<string> list) {
-    vector<int> sums;
-    for (const auto& str : list) {
-        int sum = 0;
-        for (char c : str) {
-            sum += c - 'a' + 1;
-        }
-        sums.push_back(sum);
-    }
-    return *std::max_element(sums.begin(), sums.end());
+std::vector<std::string> sorted_list_sum(std::vector<std::string> v) {
+    std::sort(v.begin(), v.end());
+    return v;
 }
 
 int main() {
-    std::vector<std::string> v1 = {"aaaa", "bbbb", "dd", "cc"};
-    std::vector<std::string> v2 = {"cc", "dd", "aaaa", "bbbb"};
-
-    assert(issame(sorted_list_sum({v1, v2}), v2)); 
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"ccc", "dd", "aaaa", "bbbb"}));
+    return 0;
 }
