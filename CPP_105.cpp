922942c875c2924833bb16ea94283ed333601d30
beep boop <githubbot@vadim.me>
2024-07-10 18:09:28 +0200

    Solution 64 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/5cdfm2hw Prompt:       |                              ^~~~~~ fd0a6233-310e-469a-b3fa-a11a741ec194.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ fd0a6233-310e-469a-b3fa-a11a741ec194.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ fd0a6233-310e-469a-b3fa-a11a741ec194.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from fd0a6233-310e-469a-b3fa-a11a741ec194.cpp:25: fd0a6233-310e-469a-b3fa-a11a741ec194.cpp: In function ‘int main()’: fd0a6233-310e-469a-b3fa-a11a741ec194.cpp:35:20: error: ‘by_length’ was not decla red in this scope    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ fd0a6233-310e-469a-b3fa-a11a741ec194.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -21,13 +21,26 @@ bool issame(vector<string> a, vector<string> b) {
     return true;
 }
 
-int main() {
-    vector<int> input_nums = {5, 2, 9, 4, 1, 11, 7};
-    vector<string> result = by_length(input_nums);
+vector<string> by_length(vector<int> arr) {
+    vector<string> result;
+    map<int, string> num_to_name = {
+        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
+        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
+    };
 
-    for (const auto& str : result) {
-        std::cout << str << " ";
+    vector<int> valid_nums;
+    for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            valid_nums.push_back(num);
+        }
     }
 
-    return 0;
+    sort(valid_nums.begin(), valid_nums.end());
+    reverse(valid_nums.begin(), valid_nums.end());
+
+    for (int num : valid_nums) {
+        result.push_back(num_to_name[num]);
+    }
+
+    return result;
 }
