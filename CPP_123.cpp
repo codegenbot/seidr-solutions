76b4a3e70ab4bace3280409195b521f2bd3c7394
beep boop <githubbot@vadim.me>
2024-07-10 10:15:09 +0200

    Solution 20 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ifhduk5e Prompt: Complete the following code given the task description and function signature.

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -5,28 +5,31 @@
 using namespace std;
 
 vector<int> get_odd_collatz(int n) {
-    vector<int> result;
+    vector<int> collatz;
     while (n != 1) {
         if (n % 2 == 0) {
             n /= 2;
         } else {
             n = 3 * n + 1;
         }
-        if (n % 2 != 0) {
-            result.push_back(n);
+        collatz.push_back(n);
+    }
+    sort(collatz.begin(), collatz.end());
+    vector<int> odd_collatz;
+    for (int num : collatz) {
+        if (num % 2 != 0) {
+            odd_collatz.push_back(num);
         }
     }
-    return result;
+    return odd_collatz;
 }
 
 int main() {
-    int n;
-    cout << "Enter a positive integer: ";
-    cin >> n;
-    vector<int> res = get_odd_collatz(n);
-    sort(res.begin(), res.end());
-    for (int i : res) {
-        cout << i << " ";
+    int n = 5;
+    vector<int> result = get_odd_collatz(n);
+    for (int num : result) {
+        cout << num << " ";
     }
+    cout << endl;
     return 0;
 }
