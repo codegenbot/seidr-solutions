ff470ae0f9ae0f5e04866c2e33629a34a604e75a
beep boop <githubbot@vadim.me>
2024-07-11 00:41:35 +0200

    Solution 27 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sr9olerd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/27307fe7-218e-4eb6-9405-ea4370df1 a5b.py", line 21, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/27307fe7-218e-4eb6-9405-ea4370df1 a5b.py", line 18, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,7 +1,16 @@
+```
 def cycpattern_check(a, b):
-    a_len = len(a)
-    for i in range(a_len):
-        for j in range(len(b), 0, -1):
-            if a[i:i+j] == b:
+    if len(b) > len(a):
+        return False
+    for i in range(len(a)):
+        k = i
+        j = 0
+        while j < len(b):
+            if a[k] != b[j]:
+                break
+            k = (k + 1) % len(a)
+            j += 1
+        if j == len(b):
+            if all(a[k:k+len(b)]==b for k in range(i+len(b),len(a))):
                 return True
     return False
