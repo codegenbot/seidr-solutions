61b0c17148305f1cb5605d597a53a1f91e6a5d82
beep boop <githubbot@vadim.me>
2024-07-10 03:19:23 +0200

    Solution 15 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/8ib57hw0 Prompt: b5ad42b3-497c-4f03-bfad-1050c7642b65.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from b5ad42b3-497c-4f03-bfad-1050c7642b65.cpp:27: b5ad42b3-497c-4f03-bfad-1050c7642b65.cpp: In function ‘int main()’: b5ad42b3-497c-4f03-bfad-1050c7642b65.cpp:37:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    37 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ b5ad42b3-497c-4f03-bfad-1050c7642b65.cpp:7:6: note: candidate: ‘bool issame(cons t std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx1 1::basic_string<char> >&)’     7 | bool issame(const vector<string>& v1, const vector<string>& v2) {       |      ^~~~~~ b5ad42b3-497c-4f03-bfad-1050c7642b65.cpp:28:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    28 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -11,7 +11,7 @@ bool issame(const vector<string>& v1, const vector<string>& v2) {
 vector<string> filter_by_prefix(const vector<string>& vec, const string& prefix) {
     vector<string> result;
     for (const auto& str : vec) {
-        if (str.find(prefix) == 0u) {
+        if (str.find(prefix) == 0) {
             result.push_back(str);
         }
     }
@@ -20,6 +20,6 @@ vector<string> filter_by_prefix(const vector<string>& vec, const string& prefix)
 
 int main() {
     assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
-  
+
     return 0;
 }
