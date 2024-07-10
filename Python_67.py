be1846fee6f53de9c5b3e23dea24960d95cc8d74
beep boop <githubbot@vadim.me>
2024-07-10 16:39:50 +0200

    Solution 43 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9wljto1e Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/82ecd86f-95ff-4bb2-b3a5-ab2ff65ab a30.py", line 14, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/82ecd86f-95ff-4bb2-b3a5-ab2ff65ab a30.py", line 11, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -3,8 +3,14 @@ def fruit_distribution(s, n):
     s = " ".join(s).replace("apples and", "apples ").replace("oranges", " oranges").split()
     if 'apples' not in s or 'oranges' not in s:
         return None
-    apples_and_part = [part for part in s if "apples" in part][0]
-    apples = int(apples_and_part.replace("apples ", "").replace("and", ""))
-    oranges_and_part = [part for part in s if "oranges" in part][0]
-    oranges = int(oranges_and_part.replace("oranges ", "").replace("and", ""))
-    return n - apples - oranges
+    apples_part = [i for i in s[:s.index('and')] if i in ['apples', 'and']][1:]
+    oranges_part = [i for i in s[s.index('and')+4:] if i in ['oranges', 'and']]
+    if 'and' not in apples_part:
+        apples = int(apples_part[0].replace("apples", ""))
+    else:
+        apples = int(''.join(filter(str.isdigit, apples_part)))
+    if 'and' not in oranges_part:
+        oranges = int(oranges_part[0].replace("oranges", ""))
+    else:
+        oranges = int(''.join(filter(str.isdigit, oranges_part)))
+    return n - int(apples) - int(oranges)
