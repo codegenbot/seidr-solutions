aa71cdefa00d31d218a39b45c6e1a3d6e63794a9
beep boop <githubbot@vadim.me>
2024-07-10 07:58:09 +0200

    Solution 90 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/svifujg2 Prompt: /usr/bin/ld: /tmp/ccqW9XAC.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': 12c9d60a-5df1-4b04-be38-dae21ee4d582.cpp:(.text+0x25): undefined reference to `E VP_MD_CTX_new' /usr/bin/ld: 12c9d60a-5df1-4b04-be38-dae21ee4d582.cpp:(.text+0x2e): undefined re ference to `EVP_md5' /usr/bin/ld: 12c9d60a-5df1-4b04-be38-dae21ee4d582.cpp:(.text+0x45): undefined re ference to `EVP_DigestInit_ex' /usr/bin/ld: 12c9d60a-5df1-4b04-be38-dae21ee4d582.cpp:(.text+0x75): undefined re ference to `EVP_DigestUpdate' /usr/bin/ld: 12c9d60a-5df1-4b04-be38-dae21ee4d582.cpp:(.text+0x8d): undefined re ference to `EVP_DigestFinal_ex' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,31 +1,34 @@
-#include <openssl/evp.h>
+#include <openssl/ssl.h>
 #include <cassert>
 #include <string>
 #include <sstream>
 #include <iomanip>
-#include <openssl/ssl.h>
 
 std::ostringstream ss;
 
 std::string string_to_md5(const std::string& str) {
     EVP_MD_CTX* md5;
     unsigned char hash[16];
-    md5 = EVP_MD_CTX_new();
     OpenSSL_add_all_algorithms();
-    EVP_DigestInit_ex(md5, EVP_md5(), 0);
-    EVP_DigestUpdate(md5, str.c_str(), str.size());
-    EVP_DigestFinal_ex(md5, hash, nullptr);
+    if (!(md5 = EVP_MD_CTX_new())) return ""; // error case
+
+    if (1 != EVP_DigestInit_ex(md5, EVP_md5(), 0)) return ""; // error case
+    if (1 != EVP_DigestUpdate(md5, str.c_str(), str.size())) return ""; // error case
+    unsigned char* md5out = NULL;
+    size_t outlen;
+    if (1 != EVP_DigestFinal_ex(md5, hash, &outlen)) return ""; // error case
 
     ss << std::hex;
     for (int i = 0; i < 16; ++i) {
         ss << std::setfill('0') << std::setw(2) << std::hex << static_cast<int>(hash[i]);
     }
 
+    EVP_MD_CTX_free(md5);
+
     return ss.str();
 }
 
 int main_test() {
     assert(string_to_md5("password") == "5d41402abc4de22fbc1fc7ce2a607762");
-    OpenSSL_add_all_algorithms();
     return 0;
 }
