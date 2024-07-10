8dbfd81a39699be16f60aabe872f7b6a6ee97b69
beep boop <githubbot@vadim.me>
2024-07-10 12:26:50 +0200

    Solution 34 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/62610hl2 Prompt:       |                    ^~~ 5bb3c09f-50c4-4f91-8096-4371bc0a2a12.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5bb3c09f-50c4-4f91-8096-4371bc0a2a12.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5bb3c09f-50c4-4f91-8096-4371bc0a2a12.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5bb3c09f-50c4-4f91-8096-4371bc0a2a12.cpp:12: 5bb3c09f-50c4-4f91-8096-4371bc0a2a12.cpp: In function ‘int main()’: 5bb3c09f-50c4-4f91-8096-4371bc0a2a12.cpp:22:20: error: ‘intersperse’ was not dec lared in this scope    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ 5bb3c09f-50c4-4f91-8096-4371bc0a2a12.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 vector<int> intersperse(vector<int> numbers, int delimeter);
 
+bool issame(vector<int> a, vector<int> b);
+
 vector<int> intersperse(vector<int> numbers, int delimeter){
     vector<int> result;
     for(int i=0; i<numbers.size(); i++){
@@ -16,13 +18,11 @@ vector<int> intersperse(vector<int> numbers, int delimeter){
     return result;
 }
 
-bool issame(vector<int> a,vector<int>b);
-
-bool issame(vector<int> a,vector<int>b){
+bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
 int main() {
-    // Main function implementation here
+    // Add your main function code here
     return 0;
 }
