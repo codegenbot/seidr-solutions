df31e42ebaff8f312f7b6be3b8d67fc510d5c033
beep boop <githubbot@vadim.me>
2024-07-10 23:22:08 +0200

    Solution 39 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/byq3z2zd Prompt: 2ec57595-b5a7-46f0-8d09-9a9d130cfb37.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2ec57595-b5a7-46f0-8d09-9a9d130cfb37.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2ec57595-b5a7-46f0-8d09-9a9d130cfb37.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 2ec57595-b5a7-46f0-8d09-9a9d130cfb37.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 2ec57595-b5a7-46f0-8d09-9a9d130cfb37.cpp:19: 2ec57595-b5a7-46f0-8d09-9a9d130cfb37.cpp: In function ‘int main()’: 2ec57595-b5a7-46f0-8d09-9a9d130cfb37.cpp:29:20: error: ‘sort_third’ was not decl ared in this scope    29 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(const vector<int>& a,const vector<int>& b){
+bool issame(const vector<int>& a,const vector<int>& b) {
     return a == b;
 }
 
