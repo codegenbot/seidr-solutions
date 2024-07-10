13c18c500d0cd7dd135b60d2a8dff2a2994cc59a
beep boop <githubbot@vadim.me>
2024-07-11 01:08:01 +0200

    Solution 35 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/49frezuz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d49e42cb-996f-40aa-8ab3-072b561f4 859.py", line 9, in <module>     print(check([1, 2, 3], lambda x: double_the_difference(x)))           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d49e42cb-996f-40aa-8ab3-072b561f4 859.py", line 7, in check     return func(sum(lst))            ^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d49e42cb-996f-40aa-8ab3-072b561f4 859.py", line 9, in <lambda>     print(check([1, 2, 3], lambda x: double_the_difference(x)))                                      ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d49e42cb-996f-40aa-8ab3-072b561f4 859.py", line 2, in double_the_difference     if len(x) < 2:        ^^^^^^ TypeError: object of type 'int' has no len()

--- Python_151.py
+++ Python_151.py
@@ -1,10 +1,10 @@
-```
+```Python
 def double_the_difference(x):
     if len(x) < 2:
         return 0
     return (max(x) - min(x)) * 2
 
 def check(lst, func):
-    return func(sum(lst))
+    return func(lst)
 
 print(check([1, 2, 3], lambda x: double_the_difference(x)))
