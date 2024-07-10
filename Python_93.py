bb1d822eae58b7b69b420e7c535bcf015b94b770
beep boop <githubbot@vadim.me>
2024-07-10 03:47:59 +0200

    Solution 11 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/yu71ufye Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0531c307-1084-47c0-b99a-a9d576edd 687.py", line 21, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0531c307-1084-47c0-b99a-a9d576edd 687.py", line 17, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,11 +2,5 @@ def encode(message):
     encoded_message = ""
     vowels = "aeiouAEIOU"
     for char in message:
-        if char.isalpha():
-            if char.islower():
-                encoded_message += chr((ord(char) - ord('a') + 2) % 26 + ord('a'))
-            else:
-                encoded_message += chr((ord(char) - ord('A') + 2) % 26 + ord('A'))
-        else:
-            encoded_message += char
+        encoded_message += chr((ord(char) - ord('A') + 2) % 26 + ord('A')) if char.isupper() else char
     return encoded_message
