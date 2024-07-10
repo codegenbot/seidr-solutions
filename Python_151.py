f65ef880a87d2ca4d4ef05da99bd17529581dd0a
beep boop <githubbot@vadim.me>
2024-07-10 09:58:18 +0200

    Solution 91 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/e7h6db9i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9a03abbc-f6f4-440e-ae59-e319b1168 47c.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9a03abbc-f6f4-440e-ae59-e319b1168 47c.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,4 +1,5 @@
+```
 def double_the_difference(lst):
-    positive_even_squares = sum(i**2 for i in lst if isinstance(i, int) and i >= 0 and i % 2 == 0)
-    all_squares = sum(i**2 for i in lst if isinstance(i, int))
-    return (all_squares - positive_even_squares) * 2
+    even_squares = sum(i**2 for i in lst if isinstance(i, int) and i >= 0 and i % 2 == 0)
+    odd_numbers = sum(1 for i in lst if isinstance(i, int) and i >= 0 and i % 2 != 0)
+    return (even_squares - odd_numbers) * 2
