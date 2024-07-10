7cc0c9b7677d8d051de978dd0555e4ce5776e4dd
beep boop <githubbot@vadim.me>
2024-07-11 00:20:15 +0200

    Solution 92 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dp8f9aqb Prompt: ined here    22 | int main() {       |     ^~~~ In file included from 3f941063-31d8-4969-b1e7-9df66d8e6745.cpp:47: 3f941063-31d8-4969-b1e7-9df66d8e6745.cpp: In function ‘int main()’: 3f941063-31d8-4969-b1e7-9df66d8e6745.cpp:57:29: error: cannot bind non-const lva lue reference of type ‘std::vector<float>&’ to an rvalue of type ‘std::vector<fl oat>’    57 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 3f941063-31d8-4969-b1e7-9df66d8e6745.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<float>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 3f941063-31d8-4969-b1e7-9df66d8e6745.cpp:31:31: note:   initializing argument 1 of ‘void sort_even(std::vector<float>&)’    31 | void sort_even(vector<float>& l) {       |                ~~~~~~~~~~~~~~~^

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -20,10 +20,14 @@ bool issame(const vector<float>& a, const vector<float>& b) {
 void sort_even(vector<float>& l);
 
 int main() {
-    vector<float> input = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
-    sort_even(input);
-    assert(issame(input, {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    vector<float> l = {3, 5, 1, 4, 2, 6};
+    vector<float> sorted_l = {1, 4, 2, 5, 3, 6};
+    sort_even(l);
+    assert(issame(l, sorted_l));
     cout << "Sorted even indices match expected result." << endl;
+
+    assert(issame(sort_even(vector<float>{5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+
     return 0;
 }
 
