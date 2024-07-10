35dc1ff1bc6c24db4dab7eb19fed3b6346c5557c
beep boop <githubbot@vadim.me>
2024-07-10 20:31:56 +0200

    Solution 58 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bpxrryax Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0aa0c645-aa56-47eb-8ba9-6cea64e0a ea4.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_41.py
+++ Python_41.py
@@ -1,6 +1,2 @@
-def car_race_collision(x1, y1, x2, y2):
-    if x1 == x2:
-        return abs(y1 - y2) <= 0
-    m = (y2 - y1) / (x2 - x1)
-    b = y1 - m * x1
-    return x1 <= x2 and min(x1, x2) >= max((b + m), (-b + m))
+def car_race_collision(n: int):
+    return n * (n - 1) // 2
