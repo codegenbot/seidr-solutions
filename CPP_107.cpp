6f8d44f4860b3c4122538f83ed911ed643bef42a
beep boop <githubbot@vadim.me>
2024-07-10 23:36:08 +0200

    Solution 73 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lx335o7c Prompt:       |             ^~~~~~ 8e3f33f5-37c2-4cc1-a186-1441b16f180b.cpp:43:20: error: expected primary-expressi on before ‘int’    43 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 8e3f33f5-37c2-4cc1-a186-1441b16f180b.cpp:43:27: error: ‘vector’ was not declared  in this scope    43 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8e3f33f5-37c2-4cc1-a186-1441b16f180b.cpp:43:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 8e3f33f5-37c2-4cc1-a186-1441b16f180b.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 8e3f33f5-37c2-4cc1-a186-1441b16f180b.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8e3f33f5-37c2-4cc1-a186-1441b16f180b.cpp:43:34: error: expected primary-expressi on before ‘int’    43 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <vector>
 
-std::vector<bool> issame(std::vector<int> a, std::vector<int> b) {
-    return {(a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin())};
-}
-
 std::vector<int> even_odd_palindrome(int n) {
     int even = 0, odd = 0;
     for (int i = 1; i <= n; i++) {
