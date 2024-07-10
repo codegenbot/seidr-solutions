4326cae329c8ef684e89b076ff885f3cdadb0a36
beep boop <githubbot@vadim.me>
2024-07-10 21:17:03 +0200

    Solution 53 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/mma41iux Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 71ec969b-418d-4b5e-bc2e-ed96eb3999ad.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 71ec969b-418d-4b5e-bc2e-ed96eb3999ad.cpp:20:16:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,6 +1,6 @@
-```cpp
 #include <string>
 #include <iostream>
+#include <sstream>
 
 const char* miniRoman(int num) {
     if (num == 9)
@@ -19,45 +19,42 @@ std::string int_to_roman(int num) {
     if (num > 3999) {
         return "Out of range";
     }
-    std::string roman;
+    std::ostringstream oss;
     int thousands = num / 1000;
     for (int i = 0; i < thousands; i++) {
-        roman += "M";
+        oss << "M";
     }
     num %= 1000;
     int hundreds = num / 100;
     if (hundreds > 3) {
-        std::string temp = miniRoman(num - 300);
-        roman += "CM" + temp;
+        oss << "CM" << miniRoman(num - 300);
     } else {
         for (int i = 0; i < hundreds; i++) {
-            roman += "C";
+            oss << "C";
         }
     }
     num %= 100;
     int tens = num / 10;
     if (tens == 9) {
-        roman += "IX";
+        oss << "IX";
     } else if (tens > 5) {
-        std::string temp = miniRoman(num - 50);
-        roman += "D" + temp;
+        oss << "D" << miniRoman(num - 50);
     } else {
         for (int i = 0; i < tens; i++) {
-            roman += "X";
+            oss << "X";
         }
     }
     num %= 10;
     if (num == 9) {
-        roman += "IX";
+        oss << "IX";
     } else if (num > 3) {
-        std::string temp = miniRoman(num - 4);
-        roman += "IV" + temp;
+        oss << "IV" << miniRoman(num - 4);
     } else {
         for (int i = 0; i < num; i++) {
-            roman += "I";
+            oss << "I";
         }
     }
-    return roman;
+    return oss.str();
 }
 
 int main() {
