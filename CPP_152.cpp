32a847a2e673aca83a98851cc359b7ac594f99ef
beep boop <githubbot@vadim.me>
2024-07-10 08:49:06 +0200

    Solution 84 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/m9ihdebv Prompt:       |                    ^~~ eca20f6b-c4f2-4b76-bddf-5a8e815c02ca.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ eca20f6b-c4f2-4b76-bddf-5a8e815c02ca.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ eca20f6b-c4f2-4b76-bddf-5a8e815c02ca.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from eca20f6b-c4f2-4b76-bddf-5a8e815c02ca.cpp:13: eca20f6b-c4f2-4b76-bddf-5a8e815c02ca.cpp: In function ‘int main()’: eca20f6b-c4f2-4b76-bddf-5a8e815c02ca.cpp:23:20: error: ‘compare’ was not declare d in this scope    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ eca20f6b-c4f2-4b76-bddf-5a8e815c02ca.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,18 +1,12 @@
-#include <algorithm>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
 std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
     vector<int> result;
     for (int i = 0; i < game.size(); i++) {
         if (game[i] == guess[i]) {
             result.push_back(0);
-        } else if (issame({game[i], guess[i]}, {guess[i], game[i]})) {
+        } else if (abs(game[i] - guess[i]) == game.size() - i - 1) {
+            result.push_back(2);
+        } else if (abs(game[i] - guess[i]) == 1) {
             result.push_back(1);
-        } else {
-            result.push_back(abs(game[i] - guess[i]));
         }
     }
     return result;
