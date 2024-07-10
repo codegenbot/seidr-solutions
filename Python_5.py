3455afef610acea93908de623e3bc621552447db
beep boop <githubbot@vadim.me>
2024-07-10 19:59:44 +0200

    Solution 15 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pc4qhgax Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2269629a-0d8f-4f44-be81-187a21384 b21.py", line 9     return result     ^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_5.py
+++ Python_5.py
@@ -1,11 +1,11 @@
-def intersperse(list1: List[int], list2: List[int]) -> List[int]:
+def solve(list1, list2):
     result = []
-    
-    while any(i for i in [list1, list2]):
+    while any(x for x in (list1, list2)):
         if list1 and (not list2 or len(result) % 2 == 0):
             result.append(list1.pop(0))
         elif list2:
             result.append(list2.pop(0))            
         else:
-            result.append(list1.pop(0))            
+            result.extend(list1)
+            break
     return result
