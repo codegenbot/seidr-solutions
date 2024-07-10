0dcc82f3242bf6c6eedb4160531d41b4e2ec6b9d
beep boop <githubbot@vadim.me>
2024-07-10 08:41:58 +0200

    Solution 69 of CPP/96, ratio 0.6 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uy7puokw Prompt: 9156e7a7-fc98-4a62-a7d2-691fd39db953.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ 9156e7a7-fc98-4a62-a7d2-691fd39db953.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,24 +1,28 @@
-vector<int> result;
-    if (n < 2) {
-        return result;
-    }
-    
-    vector<bool> isPrime(n, true);
-    isPrime[0] = isPrime[1] = false;
-    
-    for (int i = 2; i * i < n; ++i) {
-        if (isPrime[i]) {
-            for (int j = i * i; j < n; j += i) {
-                isPrime[j] = false;
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+vector<int> count_up_to(int n) {
+    vector<int> primes;
+    for (int num = 2; num <= n; ++num) {
+        bool is_prime = true;
+        for (int i = 2; i * i <= num; ++i) {
+            if (num % i == 0) {
+                is_prime = false;
+                break;
             }
         }
-    }
-    
-    for (int i = 2; i < n; ++i) {
-        if (isPrime[i]) {
-            result.push_back(i);
+        if (is_prime) {
+            primes.push_back(num);
         }
     }
+    return primes;
+}
+
+void test_primes() {
+    vector<int> expected_primes = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
+    vector<int> computed_primes = count_up_to(101);
     
-    return result;
+    assert(computed_primes == expected_primes);
 }
