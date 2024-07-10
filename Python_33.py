de7ae2938e059e5e1c4453a90890a6beeaa7396d
beep boop <githubbot@vadim.me>
2024-07-10 21:48:03 +0200

    Solution 34 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0ac15d5e-680a-45b3-864c-26689018d ff3.py", line 10, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0ac15d5e-680a-45b3-864c-26689018d ff3.py", line 5, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,3 +1,3 @@
+```
 def sort_third(l: list):
-    sorted_l = sorted((x, i) for i, x in enumerate(l))
-    return [x for _, x in sorted(sorted_l)]
+    return [x for _, (_, x) in sorted(enumerate(l))]
