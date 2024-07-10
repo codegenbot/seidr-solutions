6374b382f30c3e11dde0742af14d9a31e1696019
beep boop <githubbot@vadim.me>
2024-07-10 03:45:44 +0200

    Solution 13 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/09anxcah Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,3 +1,11 @@
 def find_zero(xs: list):
-    x = -xs[0] / xs[1]
-    return x
+    n = len(xs)
+    assert n % 2 == 0
+    assert xs[0] != 0
+    highest_power = n // 2
+    new_xs = [xs[i] / xs[0] for i in range(1, n)]
+    return (
+        -new_xs[0]
+        if highest_power == 1
+        else round(1 + 1 / highest_power * (new_xs[1] - new_xs[highest_power - 1]), 2)
+    )
