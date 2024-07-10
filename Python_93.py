65b973831a6aedeeb58aff65a73cae720474904f
beep boop <githubbot@vadim.me>
2024-07-10 22:15:48 +0200

    Solution 45 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ysx0gw7p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b22021bf-0c2c-4021-8d97-506df92fb 37d.py", line 37, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b22021bf-0c2c-4021-8d97-506df92fb 37d.py", line 33, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,28 +1,28 @@
 def encode(message):
     result = ""
     for char in message:
-        if not char.isalpha():
+        if not char.isalpha():  
             result += char
         else:
             if char.isupper():
-                if char == "X" or char == "x":
-                    result += "A"
-                elif char == "Y" or char == "y":
-                    result += "B"
+                if char == 'X' or char == 'x':
+                    result += 'A'
+                elif char == 'Y' or char == 'y':
+                    result += 'B'
                 else:
                     result += chr(ord(char.lower()) + 1).upper()
             else:
-                if char in ["a", "e", "i", "o", "u"]:
-                    if char == "a":
-                        result += "c"
-                    elif char == "e":
-                        result += "g"
-                    elif char == "i":
-                        result += "k"
-                    elif char == "o":
-                        result += "q"
+                if char in ['a', 'e', 'i', 'o', 'u']:
+                    if char == 'a':
+                        result += 'c'
+                    elif char == 'e':
+                        result += 'g'
+                    elif char == 'i':
+                        result += 'k'
+                    elif char == 'o':
+                        result += 'q'
                     else:
-                        result += "s"
+                        result += 's'
                 else:
                     result += chr(ord(char) + 1)
     return result
