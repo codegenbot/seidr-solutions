8e251ef8337a23c537bdab112b8eaa0d05df1602
beep boop <githubbot@vadim.me>
2024-07-10 17:11:21 +0200

    Solution 94 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/dpgwlfse Prompt:       |                                   ^~~~~~ 551047aa-df07-46b7-b109-759363422a40.cpp:20:42: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 551047aa-df07-46b7-b109-759363422a40.cpp:20:49: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 551047aa-df07-46b7-b109-759363422a40.cpp:20:56: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 551047aa-df07-46b7-b109-759363422a40.cpp:19: 551047aa-df07-46b7-b109-759363422a40.cpp: In function ‘int main()’: 551047aa-df07-46b7-b109-759363422a40.cpp:32:20: error: ‘get_row’ was not declare d in this scope    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 551047aa-df07-46b7-b109-759363422a40.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -15,24 +15,6 @@ bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b){
     return true;
 }
 
-vector<vector<int>> get_row(vector<vector<int>> lst, int x){
-    vector<vector<int>> result;
-    for (int i = 0; i < lst.size(); ++i) {
-        for (int j = 0; j < lst[i].size(); ++j) {
-            if (lst[i][j] == x) {
-                result.push_back({i, j});
-            }
-        }
-    }
-    
-    sort(result.begin(), result.end());
-    for (auto &coord : result) {
-        sort(coord.begin(), coord.end(), greater<int>());
-    }
-    
-    return result;
-}
-
 int main() {
     assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
     return 0;
