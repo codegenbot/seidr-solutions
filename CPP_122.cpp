01dd39c6d9214e6d2ef6418e109cc96a13291257
beep boop <githubbot@vadim.me>
2024-07-10 21:23:09 +0200

    Solution 9 of CPP/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eqjhhqq6 Prompt:       |           ^~~~~~ In file included from 66feef60-c92b-4c52-a603-e7cc18102fe1.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 66feef60-c92b-4c52-a603-e7cc18102fe1.cpp:4:25: error: expected primary-expressio n before ‘int’     4 | int add_elements(vector<int> arr, int k) {       |                         ^~~ 66feef60-c92b-4c52-a603-e7cc18102fe1.cpp:4:35: error: expected primary-expressio n before ‘int’     4 | int add_elements(vector<int> arr, int k) {       |                                   ^~~ 66feef60-c92b-4c52-a603-e7cc18102fe1.cpp:4:40: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | int add_elements(vector<int> arr, int k) {       |                                        ^ In file included from 66feef60-c92b-4c52-a603-e7cc18102fe1.cpp:14: 66feef60-c92b-4c52-a603-e7cc18102fe1.cpp: In function ‘int main()’: 66feef60-c92b-4c52-a603-e7cc18102fe1.cpp:16:25: error: ‘add_elements’ cannot be used as a function    16 |     assert (add_elements({1}, 1) == 1);       |             ~~~~~~~~~~~~^~~~~~~~

--- CPP_122.cpp
+++ CPP_122.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 namespace std;
-int add_elements(std::vector<int> arr, int k) {
-    int sum = 0;
+int add_elements(std::vector<int> arr, int k) { 
+    int sum = 0; 
     for (int i = 0; i < k; i++) {
         if (to_string(arr[i]).size() <= 2) {
             sum += arr[i];
         }
-    }
+    } 
     return sum;
 }
