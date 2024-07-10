69ef344607a658dd7aa441349d975e4f143c6ca4
beep boop <githubbot@vadim.me>
2024-07-10 05:21:06 +0200

    Solution 99 of CPP/83, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ziclxyyi Prompt: /usr/include/c++/11/bits/basic_string.h:760:30: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   760 |                   _M_destroy(_M_allocated_capacity);       |                              ^~~~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/bits/basic_string.h:765:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   765 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -1,28 +1,23 @@
-```cpp
 #include <iostream>
 #include <cmath>
-#include <string>
 
 using namespace std;
 
 int starts_one_ends(int n) {
     int count = 0;
     for (long long i = 1; i <= pow(10, n-1); i++) {
-        string str;
         bool start_with_one = false, end_with_one = false;
 
         for (int j = 0; j < n; j++) {
             if ((i % 2 == 0) && j == 0) {
                 start_with_one = true;
             }
-            str.push_back((i % 2 == 0) ? '1' : '0');
         }
 
         for (int j = n - 1; j >= 0; j--) {
             if ((i % 2 == 0) && j == n - 1) {
                 end_with_one = true;
             }
-            str.push_back((i % 2 == 0) ? '1' : '0');
         }
 
         if ((start_with_one || i == 1) && (end_with_one || i == pow(10, n-1))) {
