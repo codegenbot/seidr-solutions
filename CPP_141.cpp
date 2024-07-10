9220e3c2cad7a8ad36aab832c5b38829f036eb72
beep boop <githubbot@vadim.me>
2024-07-10 21:11:06 +0200

    Solution 44 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/40k0frq6 Prompt: 7fec9bba-f694-4eef-a526-856547fcd221.cpp:21:5: error: expected unqualified-id be fore ‘if’    21 |     if (!isalpha(before_dot[0])) {       |     ^~ 7fec9bba-f694-4eef-a526-856547fcd221.cpp:24:5: error: expected unqualified-id be fore ‘if’    24 |     if (after_dot != "txt" && after_dot != "exe" && after_dot != "dll") {       |     ^~ 7fec9bba-f694-4eef-a526-856547fcd221.cpp:27:5: error: expected unqualified-id be fore ‘return’    27 |     return "Yes";       |     ^~~~~~ 7fec9bba-f694-4eef-a526-856547fcd221.cpp:28:1: error: expected declaration befor e ‘}’ token    28 | }       | ^ In file included from 7fec9bba-f694-4eef-a526-856547fcd221.cpp:30: 7fec9bba-f694-4eef-a526-856547fcd221.cpp: In function ‘int main()’: 7fec9bba-f694-4eef-a526-856547fcd221.cpp:32:13: error: ‘file_name_check’ was not  declared in this scope    32 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,4 +1,4 @@
-string validate_file_name(string file_name) {
+string check_file_name(string file_name) {
     int count_digits = 0;
     int dot_position = -1;
     for (int i = 0; i < file_name.length(); ++i) {
