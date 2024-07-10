124c66bf8c7071e184d6f72426aadb46172756f9
beep boop <githubbot@vadim.me>
2024-07-10 10:16:39 +0200

    Solution 61 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mmzl1wqj Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,48 +1,18 @@
 vector<string> by_length(vector<int> arr) {
     vector<int> nums;
-    for (int num : arr) {
-        if (num >= 1 && num <= 9) {
-            nums.push_back(num);
+    for (int i : arr) {
+        if (i >= 1 && i <= 9) {
+            nums.push_back(i);
         }
     }
-    
     sort(nums.begin(), nums.end());
     reverse(nums.begin(), nums.end());
-
     vector<string> result;
-    for (int num : nums) {
-        string str = "";
-        switch (num) {
-            case 1:
-                str = "One";
-                break;
-            case 2:
-                str = "Two";
-                break;
-            case 3:
-                str = "Three";
-                break;
-            case 4:
-                str = "Four";
-                break;
-            case 5:
-                str = "Five";
-                break;
-            case 6:
-                str = "Six";
-                break;
-            case 7:
-                str = "Seven";
-                break;
-            case 8:
-                str = "Eight";
-                break;
-            case 9:
-                str = "Nine";
-                break;
-        }
-        result.push_back(str);
+    map<int, string> numMap = {{1, "One"}, {2, "Two"}, {3, "Three"}, 
+                              {4, "Four"}, {5, "Five"}, {6, "Six"}, 
+                              {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
+    for (int i : nums) {
+        result.push_back(numMap[i]);
     }
-
     return result;
 }
