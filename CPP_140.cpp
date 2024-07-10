1752fd43f111288003b95acd498e0efb762710d4
beep boop <githubbot@vadim.me>
2024-07-10 21:06:03 +0200

    Solution 4 of CPP/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9iudnkpt Prompt: Complete the following code given the task description and function signature.

--- CPP_140.cpp
+++ CPP_140.cpp
@@ -1,13 +1,25 @@
 string fix_spaces(string text){
     string result = "";
-    for(int i=0; i<text.length(); i++){
-        if(text[i] == ' '){
-            if(i < text.length() - 1 && text[i+1] == ' '){
-                if(result.length() > 0 || i > 0) result += '-';
-                else continue;
+    for(int i=0; i < text.length(); i++){
+        if(text[i] == ' ' && (i==0 || text[i-1] != ' ') && (i == text.length()-1 || text[i+1] != ' ')){
+            result += "_";
+        }
+        else if(text[i] == ' '){
+            bool isConsecutive = true;
+            for(int j=i+1; j < text.length() && j < i+3; j++){
+                if(text[j] != ' '){
+                    isConsecutive = false;
+                    break;
+                }
             }
-            result += '_';
-        }else{
+            if(isConsecutive){
+                result += "-";
+                i+=2;
+            } else {
+                result += "_";
+            }
+        }
+        else{
             result += text[i];
         }
     }
