08c1c76f28001ce72b8c99a0823b8036c2da108f
beep boop <githubbot@vadim.me>
2024-07-10 13:00:09 +0200

    Solution 20 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/6q78mgcy Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2f724d5f-3af7-45e6-bdc5-916ecfcefe12.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2f724d5f-3af7-45e6-bdc5-916ecfcefe12.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2f724d5f-3af7-45e6-bdc5-916ecfcefe12.cpp:19: 2f724d5f-3af7-45e6-bdc5-916ecfcefe12.cpp: In function ‘int main()’: 2f724d5f-3af7-45e6-bdc5-916ecfcefe12.cpp:29:20: error: ‘parse_music’ was not dec lared in this scope    29 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ 2f724d5f-3af7-45e6-bdc5-916ecfcefe12.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,13 +1,11 @@
-#include <iostream>
 #include <vector>
+#include <string>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
-}
+using namespace std;
 
-std::vector<int> parse_music(std::string music_string){
-    std::vector<int> beats;
+vector<int> parse_music(string music_string){
+    vector<int> beats;
     int i = 0;
     while (i < music_string.size()) {
         if (music_string[i] == 'o') {
@@ -24,7 +22,11 @@ std::vector<int> parse_music(std::string music_string){
     return beats;
 }
 
+bool issame(const vector<int>& a, const vector<int>& b){
+    return a == b;
+}
+
 int main(){
-    assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 4, 2, 4, 2}));
+    assert (issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 4, 2, 4, 2}));
     return 0;
 }
