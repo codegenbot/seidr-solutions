2a07765bb35098267108e1d7775bc89f0d23645f
beep boop <githubbot@vadim.me>
2024-07-10 19:02:35 +0200

    Solution 14 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uquygx2d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eacd472c-f7ca-4965-b550-21ba8b7f7 f92.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eacd472c-f7ca-4965-b550-21ba8b7f7 f92.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,13 @@
 ```
 def sort_even(l: list):
-    evens = sorted([x for i, x in enumerate(l) if i % 2 == 0])
-    odds = sorted([x for i, x in enumerate(l) if i % 2 != 0])
-    return [x for i, x in zip(range(len(l)), l) if i % 2 == 0] + odds
+    even = sorted([x for i, x in enumerate(l) if i % 2 == 0])
+    odd = [x for i, x in enumerate(l) if i % 2 != 0]
+    result = []
+    evencount = 0
+    for x in l:
+        if evencount < len(even):
+            result.append(even[evencount])
+            evencount += 1
+        else:
+            result.append(odd.pop(0))
+    return result
