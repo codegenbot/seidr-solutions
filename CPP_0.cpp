ae51a43778b52173e6902592420fd6cf3033237f
beep boop <githubbot@vadim.me>
2024-07-10 19:16:51 +0200

    Solution 34 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xpot87ex Prompt: 44c2bbf1-7d13-4712-b542-b7df64eca8bf.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ 44c2bbf1-7d13-4712-b542-b7df64eca8bf.cpp: In function ‘int main()’: 44c2bbf1-7d13-4712-b542-b7df64eca8bf.cpp:22:5: error: ‘vector’ was not declared in this scope    22 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 44c2bbf1-7d13-4712-b542-b7df64eca8bf.cpp:22:5: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 44c2bbf1-7d13-4712-b542-b7df64eca8bf.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 44c2bbf1-7d13-4712-b542-b7df64eca8bf.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 44c2bbf1-7d13-4712-b542-b7df64eca8bf.cpp:22:12: error: expected primary-expressi on before ‘float’    22 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,7 +1,10 @@
 #include <vector>
 #include <cmath>
+#include <assert.h>
 
-bool has_close_elements(std::vector<float> numbers, float threshold){
+using namespace std;
+
+bool has_close_elements(vector<float> numbers, float threshold){
     for(int i = 0; i < numbers.size(); i++){
         for(int j = i + 1; j < numbers.size(); j++){
             if(abs(numbers[i] - numbers[j]) <= threshold)
@@ -12,6 +15,6 @@ bool has_close_elements(std::vector<float> numbers, float threshold){
 }
 
 int main() {
-    std::vector<float> a={1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
-    assert (has_close_elements({1.1f, 2.2f, 3.1f, 4.1f, 5.1f}, 0.5) == false);
+    vector<float> a={1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
+    assert (has_close_elements({1.1f, 2.2f, 3.1f, 4.1f, 5.1f}, 0.5f) == false);
     return 0;
