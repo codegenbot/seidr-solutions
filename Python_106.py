7bf7bbc51ff66b914df045bed154460e2246a9c5
beep boop <githubbot@vadim.me>
2024-07-10 16:44:01 +0200

    Solution 5 of Python/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lm3x82y0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/46996453-8467-457b-b274-f7bb41a7d 69e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_106.py
+++ Python_106.py
@@ -1,19 +1,10 @@
-```
+````
 def f(n):
-    """return a list of integers where each integer is either 
-    the factorial of the number if it's even or the sum of all 
-    integers from 1 to that number if it's odd"""
-    result = []
+    """result = []
     for i in range(1, n+1):
         if i % 2 == 0:
             result.append(math.factorial(i))
         else:
             result.append(sum(range(1, i+1)))
-    return result
-
-import math
-def factorial(x):
-    if x == 0 or x == 1:
-        return 1
-    else:
-        return x * factorial(x-1)
+    return result"""
+```
