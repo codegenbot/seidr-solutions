39f99a2f27c5095e23dfd0fa35ffbb401b544b8d
beep boop <githubbot@vadim.me>
2024-07-10 19:21:36 +0200

    Solution 30 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/g9vkr95p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1ea6abf0-b62e-45cb-b8d7-8fa5a6d95 f63.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_36.py
+++ Python_36.py
@@ -1,13 +1,8 @@
-```python
-    def fizz_buzz(n: int):
-        output = []
-        for i in range(1, n+1):
-            if i % 3 == 0 and i % 5 == 0:
-                output.append("FizzBuzz")
-            elif i % 3 == 0:
-                output.append("Fizz")
-            elif i % 5 == 0:
-                output.append("Buzz")
-            else:
-                output.append(str(i))
-        return "\n".join(map(str, output))
+def fizz_buzz(n: int):
+    output = []
+    for i in range(1, n+1):
+        if i % 11 == 0 and i % 13 == 0:
+            output.append("F1894704")
+        else:
+            output.append(str(i))
+    return "\n".join(map(str, output))
