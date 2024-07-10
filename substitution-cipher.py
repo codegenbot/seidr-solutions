ae3042db759dd4363e04333f0473e6d5f9b572c2
beep boop <githubbot@vadim.me>
2024-07-11 01:03:21 +0200

    Solution 56 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/junfawc7 Prompt: it must return aluotjsbrojxzzoekzuonv for input nqrodebmuphigfxvzats iqohrkazbuxtsnjfeplv btprixgudrxhmmrzemprfs, but it returns .

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -1,28 +1,22 @@
+```
 def substitution_cipher(cipher1, cipher2, message):
     if len(cipher1) != len(cipher2):
-        if len(cipher1) > len(cipher2):
-            cipher = {}
-            for i in range(len(cipher2)):
-                if cipher2[i] not in cipher:
-                    cipher[cipher2[i]] = cipher1[i]
-            for i in range(len(cipher1)-len(cipher2), len(cipher1)):
-                if cipher1[i] not in cipher:
-                    cipher[cipher1[i]] = ''
-        else:
-            cipher = {}
-            for i in range(min(len(cipher1), len(cipher2))):
-                if cipher1[i] not in cipher:
-                    cipher[cipher1[i]] = cipher2[i]
-                if cipher2[i] not in cipher:
-                    cipher[cipher2[i]] = cipher1[i]
+        return "Error: Cipher strings are not of equal length."
 
-        result = ''
-        for char in message:
-            if char.isalpha():  
-                if char.isupper():
-                    result += cipher.get(char.upper(), char).upper()  
-                else:
-                    result += cipher.get(char.lower(), char).lower()
-            else:  
-                result += char
+    cipher = {}
+    for i in range(min(len(cipher1), len(cipher2))):
+        if cipher1[i] not in cipher:
+            cipher[cipher1[i]] = cipher2[i]
+        if cipher2[i] not in cipher:
+            cipher[cipher2[i]] = cipher1[i]
+
+    result = ''
+    for char in message:
+        if char.isalpha():  
+            if char.isupper():
+                result += chr((ord(char) - ord('A') + list(cipher.values())[0].index(list(cipher.keys())[0])) % 26 + ord('A'))
+            else:
+                result += chr((ord(char) - ord('a') + list(cipher.values())[0].index(list(cipher.keys())[0])) % 26 + ord('a'))
+        else:  
+            result += char
     return result
