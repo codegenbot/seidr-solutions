33c299a866967fd843093b67a315db18c5ea7bdb
beep boop <githubbot@vadim.me>
2024-07-11 01:51:55 +0200

    Solution 84 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/viu8tbbl Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/221f8148-d478-4f00-808a-7ff87aef4 c0d.py", line 1     `['FizzBuzz' if i % 15 == 0 else 'Buzz' if i % 5 == 0 else 'Fizz' if i % 3 = = 0 else str(i) for i in range(1, n+1)]`     ^ SyntaxError: invalid syntax

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,2 +1,1 @@
-[i % 15 == 0 and 'FizzBuzz' or
-(i % 5 == 0 and 'Buzz' or (i % 3 == 0 and 'Fizz' or str(i))) for i in range(1, n+1)]
+[str('FizzBuzz' if i % 15 == 0 else 'Buzz' if i % 5 == 0 else 'Fizz' if i % 3 == 0 else str(i)) for i in range(1, n+1)]
