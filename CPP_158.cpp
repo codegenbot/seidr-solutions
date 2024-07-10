d3999d90f97a0e0180802fe98c1b1b1d91f72104
beep boop <githubbot@vadim.me>
2024-07-10 08:41:51 +0200

    Solution 96 of CPP/158, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/2ojr046b Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ 49e3f4fd-c57e-4e7d-8112-943e5a673c36.cpp:28:28:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_158.cpp
+++ CPP_158.cpp
@@ -19,15 +19,14 @@ int main()
     int n;
     std::cout << "Enter the number of words: ";
     std::cin >> n;
+    std::cin.ignore();  // Add this line
     std::vector<std::string> words;
     for (int i = 0; i < n; ++i) {
         std::string str;
         std::getline(std::cin, str); 
-        size_t pos = str.find_first_not_of(' ');
-        if(pos != std::string::npos && pos < str.size()) { 
-            str.erase(0, pos); 
+        if (!str.empty()) { 
+            words.push_back(str);
         }
-        words.push_back(str);
     }
     std::cout << "The maximum length word is: " << find_max(words) << std::endl;
 }
