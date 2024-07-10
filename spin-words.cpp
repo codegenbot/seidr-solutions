dc03c237055152aa731053745a285a0624d43f02
beep boop <githubbot@vadim.me>
2024-07-10 12:04:45 +0200

    Solution 70 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/pyoqf5v1 Prompt:   230 |     min(const _Tp& __a, const _Tp& __b)       |     ^~~ /usr/include/c++/11/bits/stl_algobase.h:230:5: note:   template argument deducti on/substitution failed: fa458a67-f44b-4b44-baf2-23e112c56ed0.cpp:17:36: note:   deduced conflicting type s for parameter ‘const _Tp’ (‘int’ and ‘std::__cxx11::basic_string<char>::size_t ype’ {aka ‘long unsigned int’})    17 |             for (int j = i; j < min(i+6, str.length()); j++) {       |                                 ~~~^~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:60,                  from fa458a67-f44b-4b44-baf2-23e112c56ed0.cpp:1: /usr/include/c++/11/bits/stl_algobase.h:278:5: note: candidate: ‘template<class _Tp, class _Compare> constexpr const _Tp& std::min(const _Tp&, const _Tp&, _Comp are)’   278 |     min(const _Tp& __a, const _Tp& __b, _Compare __comp)       |     ^~~ /usr/include/c++/11/bits/stl_algobase.h:278:5: note:   template argument deducti on/substitution failed: fa458a67-f44b-4b44-baf2-23e112c56ed0.cpp:17:36: note:   deduced conflicting type s for parameter ‘const _Tp’ (‘int’ and ‘std::__cxx11::basic_string<char>::size_t ype’ {aka ‘long unsigned int’})    17 |             for (int j = i; j < min(i+6, str.length()); j++) {       |                                 ~~~^~~~~~~~~~~~~~~~~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,17 +1,33 @@
-```cpp
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 string spinWords(string str) {
     string result = "";
-    int start = 0;
-    for (int i = 0; i <= str.length(); i++) {
-        if (i == str.length() || str[i] == ' ') {
-            string word = str.substr(start, i - start);
-            if (word.length() >= 5) {
-                result += word.substr(word.length() - 1) + word.substr(0, word.length() - 1) + " ";
-            } else {
-                result += word + " ";
+    int i = 0;
+    while (i < str.length()) {
+        if (str[i] == ' ') {
+            result += str[i];
+        } else if (i + str.length() - i - 1 >= 5) {
+            for (int j = i + str.length() - i - 1; j >= max(0, i-5); j--) {
+                result += str[j];
             }
-            start = i + 1;
+        } else {
+            for (int j = i; j < min(i+6, str.length()); j++) {
+                result += str[j];
+            }
         }
+        i++;
     }
     return result;
+}
+
+int main() {
+    string str;
+    cout << "Enter a sentence: ";
+    getline(cin, str);
+    cout << spinWords(str) << endl;
+    return 0;
 }
