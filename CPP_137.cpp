6cb7e57860e0eaa38abb7bdd8a0cc829f4115b54
beep boop <githubbot@vadim.me>
2024-07-10 04:56:48 +0200

    Solution 24 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/n2wga4wg Prompt:    42 | int main(){       |     ^~~~ bb5fe5ff-78b3-48cc-bf04-1b74e5f45bea.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from bb5fe5ff-78b3-48cc-bf04-1b74e5f45bea.cpp:41: bb5fe5ff-78b3-48cc-bf04-1b74e5f45bea.cpp: In function ‘int main()’: bb5fe5ff-78b3-48cc-bf04-1b74e5f45bea.cpp:43:13: error: ‘boost’ has not been decl ared    43 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ bb5fe5ff-78b3-48cc-bf04-1b74e5f45bea.cpp:43:35: error: expected primary-expressi on before ‘>’ token    43 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ bb5fe5ff-78b3-48cc-bf04-1b74e5f45bea.cpp:43:37: error: ‘compare_one’ was not dec lared in this scope; did you mean ‘CompareOne’?    43 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
+#include <any>
+#include <boost/any.hpp>
 
 using namespace std;
 
-int CompareOne(int a, double b) {
-    if (b > a) {
-        return b;
-    } else if (a == b) {
+int CompareOne(int a, boost::any b) {
+    if (boost::any_cast<double>(b) > a) {
+        return boost::any_cast<double>(b);
+    } else if (a == boost::any_cast<double>(b)) {
         return 0;
     } else {
         return a;
@@ -18,16 +20,17 @@ int main() {
     cout << "Enter an integer: ";
     cin >> a;
 
-    double b;
+    boost::any b;
     cout << "Enter a floating point number or a string: ";
     cin >> b;
 
     if (cin.fail()) {
         // If the input is not a valid float, assume it's a string
-        string str;
-        cin.clear();
-        cin >> str;
-        cout << "The result is: " << CompareOne(a, stod(str)) << endl;
+        if (boost::any_cast<string>(b).find('.') != string::npos || boost::any_cast<string>(b).find('e') != string::npos) {
+            cout << "The result is: " << CompareOne(a, stod(boost::any_cast<string>(b))) << endl;
+        } else {
+            cout << "The result is: " << CompareOne(a, b) << endl;
+        }
     } else {
         // If the input is a valid float
         cout << "The result is: " << CompareOne(a, b) << endl;
