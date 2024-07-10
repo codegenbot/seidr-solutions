c5b7ef79f3a18225e83a0047780e51389f2ee21a
beep boop <githubbot@vadim.me>
2024-07-10 22:51:50 +0200

    Solution 50 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lx335o7c Prompt:       |             ^~~~~~ 7dbb8d92-ed4c-430f-b4e5-8337cbb5fe8c.cpp:39:34: error: expected primary-expressi on before ‘int’    39 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7dbb8d92-ed4c-430f-b4e5-8337cbb5fe8c.cpp:39:39: error: expression list treated a s compound expression in initializer [-fpermissive]    39 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 7dbb8d92-ed4c-430f-b4e5-8337cbb5fe8c.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 7dbb8d92-ed4c-430f-b4e5-8337cbb5fe8c.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 7dbb8d92-ed4c-430f-b4e5-8337cbb5fe8c.cpp:38: 7dbb8d92-ed4c-430f-b4e5-8337cbb5fe8c.cpp: In function ‘int main()’: 7dbb8d92-ed4c-430f-b4e5-8337cbb5fe8c.cpp:48:19: error: ‘issame’ cannot be used a s a function    48 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
 #include <vector>
 
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
+}
+
 std::vector<int> even_odd_palindrome(int n) {
     int even = 0, odd = 0;
-    for (int i = 1; i <= n; ++i) {
-        if (isPalindrome(i)) {
+    for (int i = 1; i <= n; i++) {
+        if (i == reverse(i)) {
             if (i % 2 == 0)
                 even++;
             else
                 odd++;
         }
     }
-    return std::vector<int>({even, odd});
-}
-
-bool isPalindrome(int num) {
-    int reversed = 0;
-    int original = num;
-
-    while (num != 0) {
-        int remainder = num % 10;
-        reversed = reversed * 10 + remainder;
-        num /= 10;
-    }
-
-    return original == reversed;
+    return {even, odd};
 }
 
 int main() {
@@ -36,4 +27,14 @@ int main() {
 
     std::cout << "Number of even palindromes: " << result[0] << std::endl;
     std::cout << "Number of odd palindromes: " << result[1] << std::endl;
+}
+
+int reverse(int n) {
+    int rev = 0;
+    while (n != 0) {
+        int remainder = n % 10;
+        rev = rev * 10 + remainder;
+        n /= 10;
+    }
+    return rev;
 }
