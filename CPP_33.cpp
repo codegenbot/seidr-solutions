703d9e28b74e4808b7a6f8db09c9d152bf8b0302
beep boop <githubbot@vadim.me>
2024-07-10 22:33:13 +0200

    Solution 4 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/byq3z2zd Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b42b9cf2-e7f5-4023-9630-83b8a0a246a2.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b42b9cf2-e7f5-4023-9630-83b8a0a246a2.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b42b9cf2-e7f5-4023-9630-83b8a0a246a2.cpp:10: b42b9cf2-e7f5-4023-9630-83b8a0a246a2.cpp: In function ‘int main()’: b42b9cf2-e7f5-4023-9630-83b8a0a246a2.cpp:20:20: error: ‘sort_third’ was not decl ared in this scope    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ b42b9cf2-e7f5-4023-9630-83b8a0a246a2.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,8 +1,17 @@
-vector<int> l_copy = l;
-    for (int i = 0; i < l.size(); ++i) {
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a == b;
+}
+
+vector<int> sort_third(const vector<int>& l) {
+    vector<int> l_prime = l;
+    for (int i = 0; i < l.size(); i++) {
         if (i % 3 == 0) {
-            sort(l_copy.begin() + i, l_copy.begin() + i + 3);
+            sort(l_prime.begin() + i, l_prime.begin() + i + 3);
         }
     }
-    return l_copy;
+    return l_prime;
+}
+
+int main() {
+    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
 }
