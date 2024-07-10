f4295951e367efec073b97d7f1fbd8c6a1b5be09
beep boop <githubbot@vadim.me>
2024-07-10 16:43:32 +0200

    Solution 38 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/l4jlf75r Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7903e030-32be-441e-9552-20ebda7f8 354.py", line 10, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7903e030-32be-441e-9552-20ebda7f8 354.py", line 7, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7903e030-32be-441e-9552-20ebda7f8 354.py", line 2, in solve     result = str(bin(sum(int(i) for i in str((i%2)for i in range(N+1))))[2:])                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7903e030-32be-441e-9552-20ebda7f8 354.py", line 2, in <genexpr>     result = str(bin(sum(int(i) for i in str((i%2)for i in range(N+1))))[2:])                          ^^^^^^ ValueError: invalid literal for int() with base 10: '<'

--- Python_84.py
+++ Python_84.py
@@ -1,4 +1,3 @@
 def solve(N):
-    total_sum = sum(int(i) % 2 for i in range(1, N+1))
-    result = str(bin(total_sum)[2:])
+    result = str(bin(sum(int(i) for i in str.join('', (str((i%2))for i in range(N+1))))))[2:]
     return result
