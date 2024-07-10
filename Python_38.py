49492d53427584c73347f1cd3a2ae2e491186d1c
beep boop <githubbot@vadim.me>
2024-07-10 06:32:11 +0200

    Solution 26 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/kwbe238u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f6c9efec-e59b-4799-bf0a-c49b3dbff 07e.py", line 2     rotated_groups = [s[i+1] for i in range(len(s)-1)] + [s[0]                                                          ^ SyntaxError: '[' was never closed

--- Python_38.py
+++ Python_38.py
@@ -1,3 +1,3 @@
 def decode_cyclic(s: str):
-    rotated_groups = [s[i+1] for i in range(len(s)-1)] + [s[0]]
+    rotated_groups = [s[i + 1] for i in range(len(s) - 1)] + [s[0]]
     return "".join(rotated_groups)
