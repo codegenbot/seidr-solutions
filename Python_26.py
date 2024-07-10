21a6b2ba556570f194dd2847d4abdf54626dfc23
beep boop <githubbot@vadim.me>
2024-07-10 20:57:59 +0200

    Solution 64 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bx3axgl1 Prompt: Program did not receive expected input

--- Python_26.py
+++ Python_26.py
@@ -1,3 +1,5 @@
+```Python
+from typing import List
+
 def remove_duplicates(numbers: List[int]) -> List[int]:
-    seen = set()
-    return [num for num in numbers if not (num in seen or seen.add(num))]
+    return list(set(sorted(numbers)))
