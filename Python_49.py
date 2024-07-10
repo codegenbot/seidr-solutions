d9336ea3be2e1466e02b68f45a8844fd7cc59fb8
beep boop <githubbot@vadim.me>
2024-07-10 16:36:51 +0200

    Solution 36 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/izkc6bd7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/888a1e41-2a81-437c-a774-386a1745c 559.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,6 +1,7 @@
-```
 def modp(n: int, p: int):
     return pow(n, p, p)
 
 if __name__ == "__main__":
-    print(modp(int(input("Enter n: ")), int(input("Enter p: "))))
+    x = int(input("Enter n/p (separated by space): "))
+    n, p = map(int, x.split())
+    print(modp(n, p))
