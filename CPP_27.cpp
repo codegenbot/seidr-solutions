c152902434c98ff33fee48547d3a6961744c6ab2
beep boop <githubbot@vadim.me>
2024-07-11 00:04:23 +0200

    Solution 64 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zzdbyj98 Prompt: Complete the following code given the task description and function signature.

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,9 +1,9 @@
-string filp_case(string str){
+string flip_case(string str){
     string result = "";
-    for(int i=0; i<str.length();i++){
+    for(int i=0; i<str.length(); i++){
         if(str[i] >= 'a' && str[i] <= 'z')
             result += toupper(str[i]);
-        else 
+        else
             result += tolower(str[i]);
     }
     return result;
