7da7b294eea4f8d109ce33699713720d31b93f8a
beep boop <githubbot@vadim.me>
2024-07-10 17:37:26 +0200

    Solution 8 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/dvlg5zm1 Prompt: ared in this scope    28 |     for (int num : subvector1) {       |                    ^~~~~~~~~~ 8a2f9e59-93fa-436b-8b58-29d32175ead3.cpp:29:9: error: ‘cout’ was not declared in  this scope    29 |         cout << num << endl;       |         ^~~~ 8a2f9e59-93fa-436b-8b58-29d32175ead3.cpp:29:24: error: ‘endl’ was not declared i n this scope    29 |         cout << num << endl;       |                        ^~~~ 8a2f9e59-93fa-436b-8b58-29d32175ead3.cpp:31:20: error: ‘subvector2’ was not decl ared in this scope    31 |     for (int num : subvector2) {       |                    ^~~~~~~~~~ 8a2f9e59-93fa-436b-8b58-29d32175ead3.cpp:32:9: error: ‘cout’ was not declared in  this scope    32 |         cout << num << endl;       |         ^~~~ 8a2f9e59-93fa-436b-8b58-29d32175ead3.cpp:32:24: error: ‘endl’ was not declared i n this scope    32 |         cout << num << endl;       |                        ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-using namespace std;
+#include <vector>
 
 int main() {
     int n;
-    cin >> n;
-    vector<int> nums(n);
+    std::cin >> n;
+    std::vector<int> nums(n);
     for (int i = 0; i < n; ++i) {
-        cin >> nums[i];
+        std::cin >> nums[i];
     }
     
     int sum = 0;
@@ -25,14 +25,14 @@ int main() {
         }
     }
     
-    vector<int> subvector2(nums.begin() + idx + 1, nums.end());
-    vector<int> subvector1(nums.begin(), nums.begin() + idx + 1);
+    std::vector<int> subvector1(nums.begin(), nums.begin() + idx + 1);
+    std::vector<int> subvector2(nums.begin() + idx + 1, nums.end());
     
     for (int num : subvector1) {
-        std::cout << num << "\n";
+        std::cout << num << std::endl;
     }
     for (int num : subvector2) {
-        std::cout << num << "\n";
+        std::cout << num << std::endl;
     }
     
     return 0;
