4e385083dcd3fac5e407af847dcb8668dc9a789b
beep boop <githubbot@vadim.me>
2024-07-10 16:28:55 +0200

    Solution 24 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lu60jru2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/83336bc8-86fb-4019-a9b4-df2e60adb 727.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_50.py
+++ Python_50.py
@@ -1,11 +1,3 @@
 def decode_shift(s: str):
-    return "".join(
-        [
-            (
-                chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a"))
-                if ch.isalpha() and ch.islower()
-                else ch
-            )
-            for ch in s
-        ]
-    )
+    """Return the decoded string by shifting each lowercase letter in the input string by 5 positions backwards."""
+    return "".join([chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) if ch.isalpha() and ch.islower() else ch for ch in s])
