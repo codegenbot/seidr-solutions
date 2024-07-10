c07a0d0cec09eda47aa5d8cfc1f5f290c47be571
beep boop <githubbot@vadim.me>
2024-07-10 21:53:01 +0200

    Solution 10 of Python/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zec2zvxr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dfaf6ac9-cb86-416a-8536-ea6907c20 f97.py", line 9, in <module>     check(words_string)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dfaf6ac9-cb86-416a-8536-ea6907c20 f97.py", line 6, in check     assert words_string("ahmed     , gamal") == ["ahmed", "gamal"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_101.py
+++ Python_101.py
@@ -1,7 +1,4 @@
-def sentence_reverser(input_string):
-    def words_string(s):
-        return s.replace(",", " ").split()
-
-    output = words_string(input_string)
-    reversed_sentence = " ".join(output[::-1])
-    return reversed_sentence
+```
+def words_string(s):
+    s = " ".join(filter(None, s.replace(",", " ").split())).lower()
+    return s.split()
