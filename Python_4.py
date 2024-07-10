5e16a5c5d610a865617f01b305f85e776b6611fe
beep boop <githubbot@vadim.me>
2024-07-10 18:51:18 +0200

    Solution 22 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eqvqvyvk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1219cd27-7a08-47af-bddd-78cab611f ce2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_4.py
+++ Python_4.py
@@ -1,16 +1,8 @@
-```
-```python
-from typing import List
-
-
 def mean_absolute_deviation(numbers: List[float]) -> float:
     if len(numbers) == 0:
         return 0
 
     mean = sum(numbers) / len(numbers)
     
-    if len(numbers) > 1:
-        return round(sum(abs(n - mean) for n in numbers) / len(numbers),2)
-    else:
-        return 0
-```
+    median_deviation = sum(abs(n - mean) for n in numbers) / len(numbers)
+    return median_deviation
