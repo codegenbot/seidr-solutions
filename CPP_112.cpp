24398745f6bfc3530a8835998669a7d536662b24
beep boop <githubbot@vadim.me>
2024-07-10 23:21:28 +0200

    Solution 60 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/99vwaciq Prompt:       |           ^~~~~~ In file included from 48df7052-92ce-4bbb-a101-e8579f958928.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 48df7052-92ce-4bbb-a101-e8579f958928.cpp:39:37: error: ‘string’ was not declared  in this scope    39 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 48df7052-92ce-4bbb-a101-e8579f958928.cpp:39:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 48df7052-92ce-4bbb-a101-e8579f958928.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 48df7052-92ce-4bbb-a101-e8579f958928.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 48df7052-92ce-4bbb-a101-e8579f958928.cpp:39:44: error: ‘b’ was not declared in t his scope    39 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,44 +1,4 @@
 ```cpp
-#include <vector>
-#include <set>
-#include <string>
-
-std::vector<std::string> reverse_delete(std::string s, std::string c) {
-    std::vector<std::string> result;
-    
-    std::set<char> c_set(c.begin(), c.end());
-    
-    std::string temp = "";
-    
-    for (char ch : s) {
-        if (c_set.find(ch) == c_set.end()) {
-            temp += ch;
-        }
-    }
-    
-    bool is_palindrome = true;
-    int i = 0, length = temp.length();
-    while (i < length / 2) {
-        if (temp[i] != temp[length - i - 1]) {
-            is_palindrome = false;
-            break;
-        }
-        i++;
-    }
-    
-    result.push_back(temp);
-    result.push_back((is_palindrome) ? "True" : "False");
-    
-    return result;
-}
-
-bool std::vector<std::string>::operator== (const std::vector<std::string>& b) const {
-    if (size() != b.size()) 
-        return false;
-
-    for (int i = 0; i < size(); i++)
-        if (at(i) != b.at(i))
-            return false;
-
-    return true;
+bool issame(std::vector<std::string> a) {
+    return (a == std::vector<std::string>({"True", "False"}));
 }
