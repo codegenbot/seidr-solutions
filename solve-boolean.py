915c76342050bf441148444b233c3b7dc2518559
beep boop <githubbot@vadim.me>
2024-07-10 20:14:36 +0200

    Solution 65 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/toqfs3kb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8d4bc4b5-f5ed-47cf-acd3-3f0562436 90f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,14 +1,11 @@
-```Python
 def solve_boolean(expression):
     result = True
     for c in expression:
         if c == 'F':
             result = False
             break
-        elif c == '|':
-            result = not result
-        elif c == '&':
-            if not result:
-                result = False
-                break
+        elif c == '|' and result:
+            result = False
+        elif c == '&' and not result:
+            result = False
     return result
