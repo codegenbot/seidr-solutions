4fcb505705f5807d7acaee97d5413f4012ffe9ba
beep boop <githubbot@vadim.me>
2024-07-11 00:35:41 +0200

    Solution 88 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wj4ug1w3 Prompt: a78787c8-33a8-4e94-a26f-7341e13e8ebb.cpp:1:21: error: ‘vector’ is not a member o f ‘std’     1 | int count_nums(std::vector<int> nums) {       |                     ^~~~~~ a78787c8-33a8-4e94-a26f-7341e13e8ebb.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | int count_nums(std::vector<int> nums) { a78787c8-33a8-4e94-a26f-7341e13e8ebb.cpp:1:28: error: expected primary-expressio n before ‘int’     1 | int count_nums(std::vector<int> nums) {       |                            ^~~ In file included from a78787c8-33a8-4e94-a26f-7341e13e8ebb.cpp:31: a78787c8-33a8-4e94-a26f-7341e13e8ebb.cpp: In function ‘int main()’: a78787c8-33a8-4e94-a26f-7341e13e8ebb.cpp:33:23: error: ‘count_nums’ cannot be us ed as a function    33 |     assert (count_nums({1}) == 1);       |             ~~~~~~~~~~^~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,19 +1,37 @@
 #include <vector>
+using namespace std;
 
-int count_nums(std::vector<int> nums) {
+int count_nums(vector<int> nums) {
     int count = 0;
     for (int num : nums) {
-        bool has_negative_digit = false;
-        while (num != 0) {
-            int digit = abs(num % 10);
-            if ((num > 0 && digit == 5 || digit == 8 || digit == 9)
-                || (num < 0 && digit > 4)) {
-                count++;
-            } else if (digit > 0) {
-                count += digit - 1;
+        if (num > 0) {
+            bool has_negative_digit = false;
+            while (num != 0) {
+                int digit = abs(num % 10);
+                if (digit == 5 || digit == 8 || digit == 9) {
+                    count++;
+                } else if (digit > 0) {
+                    count += digit - 1;
+                }
+                num /= 10;
             }
-            num /= 10;
+        } else if (num < 0) {
+            bool has_negative_digit = false;
+            while (abs(num) != 0) {
+                int digit = abs(num % 10);
+                if (digit == 5 || digit == 8 || digit == 9) {
+                    count++;
+                } else if (digit > 4) {
+                    count += digit - 5;
+                }
+                num /= 10;
+            }
         }
     }
     return count;
+}
+
+int main() {
+    assert(count_nums({1}) == 1);
+    return 0;
 }
