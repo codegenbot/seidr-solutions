816a56d087496e2a6bb2c1f6d374ba2173bd6cec
beep boop <githubbot@vadim.me>
2024-07-10 06:13:10 +0200

    Solution 35 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/cgdvx5jj Prompt: [1, 3] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d288ef1f-7317-4d08-b02a-d48cd63fd e40.py", line 14, in <module>     check(remove_duplicates)           ^^^^^^^^^^^^^^^^^ NameError: name 'remove_duplicates' is not defined

--- Python_26.py
+++ Python_26.py
@@ -1,9 +1,9 @@
 def find_unique_numbers(numbers):
     return [num for num in numbers if numbers.count(num) == 1]
 
-def check(find_unique_numbers):
+def find_unique_numbers(find_unique_numbers):
     numbers = [1, 2, 2, 3, 4, 4]
     result = find_unique_numbers(numbers)
     print(result)
 
-check(find_unique_numbers)
+find_unique_numbers(find_unique_numbers)
