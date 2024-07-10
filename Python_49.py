e81a407d05afd9631dff9a3a6dc4a63aee6705d6
beep boop <githubbot@vadim.me>
2024-07-10 08:04:04 +0200

    Solution 54 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7rb6mmg8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/18fada42-b0c5-4e0c-a040-aa1b21e1c 1a1.py", line 7, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/18fada42-b0c5-4e0c-a040-aa1b21e1c 1a1.py", line 4, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,12 +1,5 @@
-```
 def modp(n: int, p: int):
-    if n < 0:
-        n += p
-    if gcd(n, p) != 1:
-        raise ValueError("No multiplicative inverse exists")
-    return pow(n, p - 2, p)
-
-def gcd(a: int, b: int):
-    while b:
-        a, b = b, a % b
-    return a
+    if pow(n, p - 1, p) == 1:
+        return pow(n, -1, p)
+    else:
+        return None
