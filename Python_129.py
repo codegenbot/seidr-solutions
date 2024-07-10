fc477c4d4d1d919dc9caa3ba35ec53075e7377cf
beep boop <githubbot@vadim.me>
2024-07-10 05:00:08 +0200

    Solution 3 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/v3hxxebd Prompt: Complete the following code given the task description and function signature.

--- Python_129.py
+++ Python_129.py
@@ -1,26 +1,29 @@
 def minPath(grid, k):
     n = len(grid)
-    visited = set()
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    visited = [[False] * n for _ in range(n)]
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
     def dfs(i, j, path):
         if len(path) == k:
             return path
 
-        visited.add((i, j))
-        next_cells = [(i + di, j + dj) for di, dj in directions]
-        next_cells.sort(key=lambda x: (grid[x[0]][x[1]], x))
+        visited[i][j] = True
+        next_cells = []
+        for di, dj in directions:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < n and 0 <= nj < n and not visited[ni][nj]:
+                next_cells.append((grid[ni][nj], ni, nj))
+        next_cells.sort()
 
-        for ni, nj in next_cells:
-            if 0 <= ni < n and 0 <= nj < n and (ni, nj) not in visited:
-                new_path = dfs(ni, nj, path + [grid[ni][nj]])
-                if new_path:
-                    return new_path
+        for val, ni, nj in next_cells:
+            res = dfs(ni, nj, path + [val])
+            if res:
+                return res
 
-        visited.remove((i, j))
+        visited[i][j] = False
 
     for i in range(n):
         for j in range(n):
-            result = dfs(i, j, [grid[i][j]])
-            if result:
-                return result
+            res = dfs(i, j, [grid[i][j]])
+            if res:
+                return res
