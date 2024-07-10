15a4ee95ef7ce153826f55552dd351a2a8c5924d
beep boop <githubbot@vadim.me>
2024-07-10 13:43:15 +0200

    Solution 7 of CPP/31, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/u4czcmyk Prompt: Complete the following code given the task description and function signature.

--- CPP_31.cpp
+++ CPP_31.cpp
@@ -1,8 +1,8 @@
-if (n <= 1) {
+if(n <= 1){
         return false;
     }
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
+    for(long long i=2; i*i <= n; i++){
+        if(n % i == 0){
             return false;
         }
     }
