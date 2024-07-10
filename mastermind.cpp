a1b2e4b98366c78612eb2940c4fda7a2cc9f4470
beep boop <githubbot@vadim.me>
2024-07-10 18:47:41 +0200

    Solution 10 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/0p7z3hmy Prompt:       |                                 ^~~~~~ In file included from a5669f4a-7d32-4fff-9dd1-cd2184b6c58c.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ a5669f4a-7d32-4fff-9dd1-cd2184b6c58c.cpp:3:29: error: ‘string’ was not declared in this scope     3 | int mastermind(string code, string guess) {       |                             ^~~~~~ a5669f4a-7d32-4fff-9dd1-cd2184b6c58c.cpp:3:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from a5669f4a-7d32-4fff-9dd1-cd2184b6c58c.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from a5669f4a-7d32-4fff-9dd1-cd2184b6c58c.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ a5669f4a-7d32-4fff-9dd1-cd2184b6c58c.cpp:3:41: error: expression list treated as  compound expression in initializer [-fpermissive]     3 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,33 +1,24 @@
-```cpp
-int numBlackPegs(string code, string guess) {
-    int blackPegs = 0;
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            blackPegs++;
+#include <string>
+
+int mastermind(std::string code, std::string guess) {
+    int white = 0;
+    int black = 0;
+
+    for(int i=0; i<4; i++) {
+        if(code[i] == guess[i]) {
+            black++;
         }
     }
-    return blackPegs;
-}
 
-int numWhitePegs(string code, string guess) {
-    int whitePegs = 0;
-    map<char, int> codeCount, guessCount;
-    for (int i = 0; i < 4; ++i) {
-        codeCount[code[i]]++;
-        guessCount[guess[i]]++;
-    }
-    for (int i = 0; i < 6; ++i) {
-        if (codeCount[char('A' + i)] > 0 && guessCount[char('A' + i)] > 0) {
-            whitePegs += min(codeCount[char('A' + i)], guessCount[char('A' + i)]);
+    for(int i=0; i<6; i++) {
+        int count = 0;
+        for(int j=0; j<4; j++) {
+            if(guess[j] == (char)(i+65) && code.find((char)(i+65)) != std::string::npos) {
+                count++;
+            }
         }
+        white += std::min(count, black);
     }
-    return whitePegs;
-}
 
-int main() {
-    string code, guess;
-    cin >> code >> guess;
-    cout << numWhitePegs(code, guess) << endl;
-    cout << numBlackPegs(code, guess) << endl;
-    return 0;
+    return black;
 }
