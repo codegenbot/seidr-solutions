7fa1aa7c59c40950dd3a52bf8b9268983a1d0646
beep boop <githubbot@vadim.me>
2024-07-10 22:52:00 +0200

    Solution 60 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/gajkdsd6 Prompt:    23 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ dd867737-99a7-4edc-8dbf-85d840a253f5.cpp:23:36: error: expected primary-expressi on before ‘float’    23 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ dd867737-99a7-4edc-8dbf-85d840a253f5.cpp:23:43: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from dd867737-99a7-4edc-8dbf-85d840a253f5.cpp:22: dd867737-99a7-4edc-8dbf-85d840a253f5.cpp: In function ‘int main()’: dd867737-99a7-4edc-8dbf-85d840a253f5.cpp:32:20: error: ‘rescale_to_unit’ was not  declared in this scope    32 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ dd867737-99a7-4edc-8dbf-85d840a253f5.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -2,15 +2,8 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<float> a, std::vector<float> b){
+bool issame(std::vector<float> a, std::vector<float> b) {
     return a == b;
-}
-
-std::vector<float> rescale_to_unit(std::vector<float> numbers);
-
-int main() {
-    assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), {0.25, 0.0, 1.0, 0.5, 0.75}));
-    return 0;
 }
 
 std::vector<float> rescale_to_unit(std::vector<float> numbers) {
@@ -32,4 +25,9 @@ std::vector<float> rescale_to_unit(std::vector<float> numbers) {
     }
 
     return rescaled_numbers;
+}
+
+int main() {
+    assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), {0.25, 0.0, 1.0, 0.5, 0.75}));
+    return 0;
 }
