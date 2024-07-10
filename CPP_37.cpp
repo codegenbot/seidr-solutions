48eb4d64850102292d873afcd94c1d121b6aa4b4
beep boop <githubbot@vadim.me>
2024-07-10 06:29:21 +0200

    Solution 33 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lerjqzl3 Prompt:    16 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 8e0c7f66-de5b-4cb4-aac9-e82422c9b0d3.cpp:16:36: error: expected primary-expressi on before ‘float’    16 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 8e0c7f66-de5b-4cb4-aac9-e82422c9b0d3.cpp:16:43: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 8e0c7f66-de5b-4cb4-aac9-e82422c9b0d3.cpp:15: 8e0c7f66-de5b-4cb4-aac9-e82422c9b0d3.cpp: In function ‘int main()’: 8e0c7f66-de5b-4cb4-aac9-e82422c9b0d3.cpp:25:20: error: ‘sort_even’ was not decla red in this scope    25 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 8e0c7f66-de5b-4cb4-aac9-e82422c9b0d3.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <algorithm>
 #include <vector>
 
@@ -17,10 +18,4 @@ vector<float> sort_even(vector<float> l) {
         }
     }
     return result;
-}
-
-int main() {
-    vector<float> test = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
-    assert(issame(sort_even(test), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
-    return 0;
 }
