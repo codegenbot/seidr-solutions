997831cf7ca36ce1db7c80075e1e59079ee46edc
beep boop <githubbot@vadim.me>
2024-07-10 22:55:37 +0200

    Solution 9 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dp8f9aqb Prompt:    24 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ be38ada4-b1c7-414c-bef6-cc79799b5351.cpp:24:36: error: expected primary-expressi on before ‘float’    24 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ be38ada4-b1c7-414c-bef6-cc79799b5351.cpp:24:43: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from be38ada4-b1c7-414c-bef6-cc79799b5351.cpp:23: be38ada4-b1c7-414c-bef6-cc79799b5351.cpp: In function ‘int main()’: be38ada4-b1c7-414c-bef6-cc79799b5351.cpp:33:20: error: ‘sort_even’ was not decla red in this scope    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ be38ada4-b1c7-414c-bef6-cc79799b5351.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,13 +1,25 @@
 bool issame(vector<float> a, vector<float> b) {
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-void sort_even(vector<float>& l) {
+void sort_even(vector<float> &l) {
     vector<float> l_copy = l;
+    vector<float> l_sorted;
     sort(l_copy.begin(), l_copy.end());
     for (int i = 0; i < l.size(); ++i) {
         if (i % 2 == 0) {
-            l[i] = l_copy[i/2 * 2];
+            l_sorted.push_back(l_copy[i]);
+        } else {
+            l_sorted.push_back(l[i]);
         }
     }
+    l = l_sorted;
 }
