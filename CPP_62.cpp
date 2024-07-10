c0de669fa97f36fcddc287be5027b33993d4178a
beep boop <githubbot@vadim.me>
2024-07-10 16:42:17 +0200

    Solution 10 of CPP/62, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0i0cgwrt Prompt: fb65833e-e282-41ce-8349-ee15fa6792a2.cpp: In function ‘bool issame(std::vector<f loat>, std::vector<float>)’: fb65833e-e282-41ce-8349-ee15fa6792a2.cpp:18:13: error: ‘abs’ was not declared in  this scope    18 |         if (abs(a[i]-b[i])>1e-4) return false;       |             ^~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <vector>
 
 using namespace std;
@@ -5,7 +6,7 @@ using namespace std;
 vector<float> derivative(vector<float> xs) {
     vector<float> res(xs.size() - 1);
     for (int i = 1; i < xs.size(); i++) {
-        res[i-1] = i * xs[i];
+        res[i-1] = (xs[i] - xs[i-1]) / ((i+1)*1.0);
     }
     return res;
 }
