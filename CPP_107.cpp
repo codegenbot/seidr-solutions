a8b2aebe38fb753708e5d922d5d3419cf9d9d261
beep boop <githubbot@vadim.me>
2024-07-10 08:34:46 +0200

    Solution 73 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/i4ztvcn8 Prompt: 08c086f6-231b-49e2-870b-f32d4108e658.cpp:32:34: error: expected primary-expressi on before ‘int’    32 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 08c086f6-231b-49e2-870b-f32d4108e658.cpp:32:39: error: expression list treated a s compound expression in initializer [-fpermissive]    32 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 08c086f6-231b-49e2-870b-f32d4108e658.cpp:31: 08c086f6-231b-49e2-870b-f32d4108e658.cpp: In function ‘int main()’: 08c086f6-231b-49e2-870b-f32d4108e658.cpp:41:20: error: ‘even_odd_palindrome’ was  not declared in this scope; did you mean ‘contest::even_odd_palindrome’?    41 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 08c086f6-231b-49e2-870b-f32d4108e658.cpp:9:22: note: ‘contest::even_odd_palindro me’ declared here     9 |     std::vector<int> even_odd_palindrome(int n) {       |                      ^~~~~~~~~~~~~~~~~~~ In file included from 08c086f6-231b-49e2-870b-f32d4108e658.cpp:31: 08c086f6-231b-49e2-870b-f32d4108e658.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -22,8 +22,8 @@ namespace contest {
 }
 
 int main_contest() {
-    assert(std::issame(contest::even_odd_palindrome(1), {1, 1}));
-    assert(std::issame(contest::even_odd_palindrome(3), {3, 2, 1, 1, 2, 3}));
+    assert(contest::issame(contest::even_odd_palindrome(1), {1, 1}));
+    assert(contest::issame(contest::even_odd_palindrome(3), {3, 2, 1, 1, 2, 3}));
     
     return 0;
 }
