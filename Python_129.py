798992dcd807728cf0653aa9a210cca007433689
beep boop <githubbot@vadim.me>
2024-07-10 12:58:32 +0200

    Solution 5 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/b03fo3jb Prompt: Complete the following code given the task description and function signature.

--- Python_129.py
+++ Python_129.py
@@ -1,22 +1,19 @@
 def minPath(grid, k):
-    N = len(grid)
-    visited = [[False] * N for _ in range(N)]
-
     def dfs(i, j, path):
         if len(path) == k:
             return path
-
-        visited[i][j] = True
         for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-            if 0 <= x < N and 0 <= y < N and not visited[x][y]:
-                new_path = dfs(x, y, path + [grid[x][y]])
-                if new_path:
-                    return new_path
+            if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and (x, y) not in visited:
+                visited.add((x, y))
+                res = dfs(x, y, path + [grid[x][y]])
+                if res:
+                    return res
+                visited.remove((x, y))
 
-        visited[i][j] = False
-
-    for i in range(N):
-        for j in range(N):
-            path = dfs(i, j, [grid[i][j]])
-            if path:
-                return path
+    visited = set()
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            visited.add((i, j))
+            res = dfs(i, j, [grid[i][j]])
+            if res:
+                return res
