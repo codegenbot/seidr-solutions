81a1c4fa96ea7da09c1ed36657a9174efc8cb9a9
beep boop <githubbot@vadim.me>
2024-07-10 08:10:14 +0200

    Solution 52 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uy7puokw Prompt:    92 | #  define assert(expr) \       | 0463c18a-ebc0-47d8-8227-a80c5f7d9d9e.cpp: In function ‘int main()’: 0463c18a-ebc0-47d8-8227-a80c5f7d9d9e.cpp:9:5: error: ‘assert’ was not declared i n this scope     9 |     assert(count_up_to(101) == vector<int>{2,3,5,7,11,13,17,19,23,29,31, 37,41,43,47,53,59,61,67,71,73,79,83,89,97});       |     ^~~~~~ 0463c18a-ebc0-47d8-8227-a80c5f7d9d9e.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <cassert>   +++ |+#include <cassert>     3 | 0463c18a-ebc0-47d8-8227-a80c5f7d9d9e.cpp: At global scope: 0463c18a-ebc0-47d8-8227-a80c5f7d9d9e.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 0463c18a-ebc0-47d8-8227-a80c5f7d9d9e.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+vector<int> count_up_to(int n);
+
+int main() {
+    assert(count_up_to(101) == vector<int>{2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97});
+    return 0;
+}
+
 vector<int> count_up_to(int n) {
     vector<int> primes;
     for (int num = 2; num <= n; ++num) {
@@ -18,9 +25,4 @@ vector<int> count_up_to(int n) {
         }
     }
     return primes;
-}
-
-int main() {
-    assert(count_up_to(101) == vector<int>{2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97});
-    return 0;
 }
