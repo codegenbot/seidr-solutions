6887d488458cf2700dfc2df7b4f9af0d367ab418
beep boop <githubbot@vadim.me>
2024-07-10 02:48:37 +0200

    Solution 62 of CPP/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uyb76z4y Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_39.cpp
+++ CPP_39.cpp
@@ -1,44 +1,14 @@
 ```cpp
-#include <iostream>
-using namespace std;
-
-bool isPrime(int num); // Function prototype for isPrime()
-
-int prime_fib(int n);
-
-int main() {
-    int n;
-    cout << "Enter the number: ";
-    cin >> n;
-    if (n <= 0) {
-        cout << "Invalid input. Please enter a positive integer." << endl;
-        return 1; // Exit the program with an error code
-    } else {
-        int result = prime_fib(n);
-        if (result == -1) {
-            cout << "There is no " << n << "th prime Fibonacci number." << endl;
-        } else {
-            cout << "The " << n << "th prime Fibonacci number is: " << std::to_string(result) << endl;
-        }
-    }
-}
-
-bool isPrime(int num) {
-    if (num <= 1)
-        return false;
-    for (int i = 2; i * i <= num; i++)
-        if (num % i == 0)
-            return false;
-    return true;
-}
-
 int prime_fib(int n) {
     int a = 0, b = 1, count = 0;
+    string fibResult = "";
     while (true) {
         if (isPrime(b))
             count++;
-        if (count == n)
-            return b;
+        if (count == n) {
+            fibResult += to_string(b);
+            return stoi(fibResult); 
+        }
         a += b;
         b = a - b;
     }
