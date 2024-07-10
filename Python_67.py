3eff97b3d11e9f30981759d5426ec25f83691bcc
beep boop <githubbot@vadim.me>
2024-07-10 23:08:51 +0200

    Solution 25 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vlwyz493 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0de2b190-5de9-4eb0-8721-d21bc3ee6 730.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,7 +1,7 @@
-```Python
+```
 def fruit_distribution(s, n):
     s = s.lower()
-    fruits = [word for word in s.split() if 'fruit' in word or not 'and' in word]
+    fruits = [word for word in s.split() if word not in ['and', 'of'] and not word[0].isdigit()]
     return len(fruits) / (n or 1)
 
 print(fruit_distribution("1 apples and 100 oranges", 120))
