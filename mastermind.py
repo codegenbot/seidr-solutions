2385e667f5b58d3a5e61a67593aad2b99161aaee
beep boop <githubbot@vadim.me>
2024-07-10 22:10:41 +0200

    Solution 42 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/rce4vfqu Prompt: it must return 0 2 for input YYWO YRBO, but it returns .

--- mastermind.py
+++ mastermind.py
@@ -1,10 +1,15 @@
 def mastermind(code, guess):
     black = 0
-    white = [min(code.count(char), guess.count(char)) for char in set(guess)]
+    white = 0
+    code_count = {char: code.count(char) for char in set(code)}
+    guess_count = {char: guess.count(char) for char in set(guess)}
+
     for i in range(4):
         if code[i] == guess[i]:
             black += 1
-        else:
-            white[code.find(guess[i])] -= 1
+            code_count[code[i]] -= 1
+            guess_count[guess[i]] -= 1
 
-    return str(black) + "\n" + ' '.join(map(str, white))
+    white = sum(count for count in (code_count.get(char, 0) - (char_count > 0) for char, char_count in guess_count.items() if char_count > 0))
+
+    return str(black) + "\n" + str(white)
