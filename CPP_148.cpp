e7be3c4425811f175ff3260b9fbacdb363490096
beep boop <githubbot@vadim.me>
2024-07-10 19:59:55 +0200

    Solution 14 of CPP/148, ratio 0.14285714285714285 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/81b8y4jk Prompt: In file included from /usr/include/c++/11/string:39,                  from e9d1e1bd-cf15-4a33-8041-89c558aa7bad.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from e9d1e1bd-cf15-4a33-8041-89c558aa7bad.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e9d1e1bd-cf15-4a33-8041-89c558aa7bad.cpp:9:44: error: ‘b’ was not declared in th is scope     9 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from e9d1e1bd-cf15-4a33-8041-89c558aa7bad.cpp:8: e9d1e1bd-cf15-4a33-8041-89c558aa7bad.cpp: In function ‘int main()’: e9d1e1bd-cf15-4a33-8041-89c558aa7bad.cpp:18:20: error: ‘bf’ was not declared in this scope    18 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ e9d1e1bd-cf15-4a33-8041-89c558aa7bad.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -2,34 +2,11 @@
 #include <string>
 #include <cassert>
 
-using namespace std;
-
-vector<string> bf(string planet1, string planet2) {
-    vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
-    vector<string> result;
-    
-    int start = -1, end = -1;
-    for (int i = 0; i < planets.size(); ++i) {
-        if (planets[i] == planet1) {
-            start = i;
-        } else if (planets[i] == planet2) {
-            end = i;
-        }
-    }
-    
-    if (start == -1 || end == -1 || start >= end) {
-        return result;
-    }
-    
-    for (int i = start + 1; i < end; ++i) {
-        result.push_back(planets[i]);
-    }
-    
-    return result;
+bool issame(const vector<string>& a, const vector<string>& b) {
+    return a == b;
 }
 
 int main() {
-    vector<string> expected_solution = {};
-    assert(bf("Jupiter", "Makemake") == expected_solution);
+    assert(issame({"Jupiter", "Makemake"}, {}));
     return 0;
 }
