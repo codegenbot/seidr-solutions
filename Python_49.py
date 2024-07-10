6766f8e58089081f97df006f07706adb06faf406
beep boop <githubbot@vadim.me>
2024-07-10 16:23:04 +0200

    Solution 20 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/izkc6bd7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90ba552e-dbd4-40b2-9dc8-7d83cf518 a71.py", line 10, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90ba552e-dbd4-40b2-9dc8-7d83cf518 a71.py", line 7, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,2 +1,19 @@
+def phi(n: int):
+    result = n
+    p = 2
+    while p * p <= n:
+        if n % p == 0:
+            while n % p == 0:
+                n //= p
+            result -= result // p
+        else:
+            p += 1
+    if n > 1:
+        result -= 1
+    return result
+
 def modp(n: int, p: int):
-    return pow(n, p, p)
+    if n < 1:
+        return pow(n, p - 1, p)
+    else:
+        return pow(n, phi(n) % (p-1), p)
