2bb1bc33cdf08fc5de21395038c63e56a4b609b0
beep boop <githubbot@vadim.me>
2024-07-10 19:21:41 +0200

    Solution 22 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6mp2wjk0 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from d28ae618-7c77-45a5-b1b4-c17b01081e2d.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,35 +1,4 @@
-#include <vector>
-#include <algorithm>
-
+```cpp
 bool issame(std::vector<int> a, std::vector<int> b) {
-    return std::equal(a.begin(), a.end(), b.begin());
-}
-
-std::vector<int> common() {
-    int n;
-    std::vector<int> v;
-    std::cout << "Enter number of elements: ";
-    std::cin >> n;
-    for (int i = 0; i < n; i++) {
-        std::cout << "Enter element: ";
-        int x;
-        std::cin >> x;
-        v.push_back(x);
-    }
-    return v;
-}
-
-int main() {
-    auto v1 = common();
-    int m;
-    std::vector<int> v2;
-    std::cout << "Enter number of elements (second vector): ";
-    std::cin >> m;
-    for (int i = 0; i < m; i++) {
-        std::cout << "Enter element: ";
-        int x;
-        std::cin >> x;
-        v2.push_back(x);
-    }
-    assert(issame(v1, v2));  
+    return a == b;
 }
