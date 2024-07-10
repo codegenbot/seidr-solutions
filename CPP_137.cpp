a6f1f9233010d55d4f41a7cfdcf76d09dab954b2
beep boop <githubbot@vadim.me>
2024-07-10 21:34:12 +0200

    Solution 38 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3mr73mpr Prompt: 8c7f96fe-44f9-4a9b-97f6-b3fb7e2bdd81.cpp:1:10: fatal error: boost/any_cast.hpp: No such file or directory     1 | #include <boost/any_cast.hpp>       |          ^~~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,18 +2,25 @@
 #include <boost/convert.hpp>
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == b.type()) {
+    if (a.type() == typeid(int)) {
         if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
             return a;
         } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
             return b;
+        } else {
+            return boost::any(typeid(int));
         }
+    } 
+    else if (a.type() == typeid(double)) {
+        if (boost::any_cast<double>(a) > boost::any_cast<double>(b)) {
+            return a;
+        } else if (boost::any_cast<double>(a) < boost::any_cast<double>(b)) {
+            return b;
+        } else {
+            return boost::any(typeid(double));
+        }
+    } 
+    else {
+        return boost::any(typeid(int));
     }
-
-    if (boost::any_cast<double>(a) > boost::any_cast<double>(b)) {
-        return a;
-    } else if (boost::any_cast<double>(a) < boost::any_cast<double>(b)) {
-        return b;
-    }
-
-    return b;
+}
