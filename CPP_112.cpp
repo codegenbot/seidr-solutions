b82ee6331f2442ce1f574f572823c770fc198731
beep boop <githubbot@vadim.me>
2024-07-10 07:42:04 +0200

    Solution 31 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/s6j49414 Prompt:       |                              ^~~~~~ 0936961c-6740-45de-a52f-02dc4f49f2fc.cpp:39:37: error: ‘string’ was not declared  in this scope    39 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0936961c-6740-45de-a52f-02dc4f49f2fc.cpp:39:44: error: ‘b’ was not declared in t his scope    39 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0936961c-6740-45de-a52f-02dc4f49f2fc.cpp:39:45: error: expression list treated a s compound expression in initializer [-fpermissive]    39 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0936961c-6740-45de-a52f-02dc4f49f2fc.cpp:38: 0936961c-6740-45de-a52f-02dc4f49f2fc.cpp: In function ‘int main()’: 0936961c-6740-45de-a52f-02dc4f49f2fc.cpp:48:20: error: ‘reverse_delete’ was not declared in this scope    48 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 0936961c-6740-45de-a52f-02dc4f49f2fc.cpp:48:19: error: ‘issame’ cannot be used a s a function    48 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,8 +1,5 @@
-#include <vector>
-#include <algorithm>
-
-std::vector<std::string> reverse_delete(std::string s, std::string c) {
-    std::vector<std::string> result;
+vector<string> reverse_delete(string s, string c) {
+    vector<string> result;
     
     for (char ch : s) {
         bool found = false;
@@ -14,11 +11,11 @@ std::vector<std::string> reverse_delete(std::string s, std::string c) {
         }
         
         if (!found) {
-            result.push_back(std::to_string(ch));
+            result.push_back(string(1, ch));
         }
     }
     
-    std::string temp = "";
+    string temp = "";
     for (int i = 0; i < result.size(); i++) {
         temp += result[i];
     }
@@ -34,6 +31,6 @@ std::vector<std::string> reverse_delete(std::string s, std::string c) {
         right--;
     }
     
-    result.push_back(std::to_string(is_palindrome).erase(0,1));
+    result.push_back(to_string(is_palindrome));
     return result;
 }
