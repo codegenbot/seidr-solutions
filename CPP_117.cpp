673c0f798eed3a8f83fd2ac7d248d1e2d2636fc8
beep boop <githubbot@vadim.me>
2024-07-10 20:07:02 +0200

    Solution 86 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/5wrs10ic Prompt:    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 57318632-eb1a-4898-9ba7-f41a91343ca6.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 57318632-eb1a-4898-9ba7-f41a91343ca6.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 57318632-eb1a-4898-9ba7-f41a91343ca6.cpp:29: 57318632-eb1a-4898-9ba7-f41a91343ca6.cpp: In function ‘int main()’: 57318632-eb1a-4898-9ba7-f41a91343ca6.cpp:39:20: error: ‘select_words’ was not de clared in this scope    39 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 57318632-eb1a-4898-9ba7-f41a91343ca6.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,17 +1,5 @@
 bool issame(vector<string> a, vector<string> b);
 
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 vector<string> select_words(string s, int n) {
     vector<string> result;
     if (s.empty()) {
@@ -38,4 +26,16 @@ vector<string> select_words(string s, int n) {
     }
     
     return result;
+}
+
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
