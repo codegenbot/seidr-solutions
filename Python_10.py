7af047901b47bec9bfd68f80d56bb339504cffaa
beep boop <githubbot@vadim.me>
2024-07-10 23:43:32 +0200

    Solution 88 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6ml3f6al Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d6499f4f-ff46-44cc-9813-44e9b68db fb0.py", line 18, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d6499f4f-ff46-44cc-9813-44e9b68db fb0.py", line 15, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,13 +1,12 @@
-def is_palindrome(string: str) -> bool:
-    return string == string[::-1]
-
-
+```
 def make_palindrome(string: str) -> str:
     if not string:
         return ""
     elif is_palindrome(string):
-        return string + string
+        return string
     else:
-        for i in range(len(string)):
-            if is_palindrome(string[:i] + string[i:][::-1]):
-                return string[:i] + (string[i:][::-1]) + string[i]
+        for i in range(len(string), 0, -1):
+            for j in range(len(string)):
+                new_string = string[:j] + string[-i:]
+                if is_palindrome(new_string):
+                    return new_string
