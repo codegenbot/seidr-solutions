f12ad622f8464c6b1d1876a4d9fa3f18aa8e8dea
beep boop <githubbot@vadim.me>
2024-07-10 06:44:53 +0200

    Solution 90 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lerjqzl3 Prompt:    19 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 382470cc-aa40-4459-9f1d-0d3ef4af0be6.cpp:19:36: error: expected primary-expressi on before ‘float’    19 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 382470cc-aa40-4459-9f1d-0d3ef4af0be6.cpp:19:43: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 382470cc-aa40-4459-9f1d-0d3ef4af0be6.cpp:18: 382470cc-aa40-4459-9f1d-0d3ef4af0be6.cpp: In function ‘int main()’: 382470cc-aa40-4459-9f1d-0d3ef4af0be6.cpp:28:20: error: ‘sort_even’ was not decla red in this scope    28 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 382470cc-aa40-4459-9f1d-0d3ef4af0be6.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,31 +1,20 @@
-bool issame(vector<float> a, vector<float> b);
-vector<float> sort_even(vector<float> l);
+#include <vector>
+#include <algorithm>
 
-int main() {
-    int n;
-    cin >> n;
-    vector<float> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-    
-    vector<float> result = sort_even(v);
-    for (float f : result) {
-        cout << f << " ";
-    }
-    return 0;
+bool issame(vector<float> a, vector<float> b) {
+    return a == b;
 }
 
 vector<float> sort_even(vector<float> l) {
     vector<float> result(l.size());
-    int j = 0;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            while(j < l.size() && i % 2 != j % 2) {
-                j++;
-            }
-            result[i] = l[j];
-            j++;
+            vector<float> temp;
+            for (int j = 0; j < l.size(); j++)
+                if (j % 2 == 0)
+                    temp.push_back(l[j]);
+            sort(temp.begin(), temp.end());
+            result[i] = temp[0];
         } else {
             result[i] = l[i];
         }
