278b4b638aef41a54fe5371dd821a6e73cf8d535
beep boop <githubbot@vadim.me>
2024-07-10 04:30:53 +0200

    Solution 18 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/z0n0e71f Prompt: 827a6812-b78e-4cef-b51b-25b7eebb4326.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ 827a6812-b78e-4cef-b51b-25b7eebb4326.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ 827a6812-b78e-4cef-b51b-25b7eebb4326.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,33 +1,21 @@
 #include <vector>
-#include <algorithm>
-#include <string>
+using namespace std;
 
-std::pair<int, int> mastermind(const std::string& code, const std::string& guess) {
+int mastermind(string code, string guess) {
     int white = 0;
     int black = 0;
-    std::vector<char> code_chars(code.begin(), code.end());
+    vector<char> code_chars(code.begin(), code.end());
     for (int i = 0; i < 4; ++i) {
         char c1 = code[i];
         char c2 = guess[i];
         if (c1 == c2) {
             --code_chars.size();
             black++;
-        } else if (std::count(code_chars.begin(), code_chars.end(), c2)) {
+        } else if (count(code_chars.begin(), code_chars.end(), c2)) {
             white++;
-            auto it = std::find(code_chars.begin(), code_chars.end(), c2);
+            auto it = find(code_chars.begin(), code_chars.end(), c2);
             code_chars.erase(it);
         }
     }
     return {white, black};
-}
-
-int main() {
-    std::string code, guess;
-    std::cout << "Enter the Mastermind code: ";
-    std::cin >> code;
-    std::cout << "Enter your guess: ";
-    std::cin >> guess;
-    auto result = mastermind(code, guess);
-    std::cout << "White pegs: " << result.first << ", Black pegs: " << result.second << std::endl;
-    return 0;
 }
