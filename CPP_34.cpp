615f0600aadda0d3cd1ba60a88f6e0bbe02f63c3
beep boop <githubbot@vadim.me>
2024-07-10 02:54:15 +0200

    Solution 76 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/uzrnpb22 Prompt:    39 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0c2a863c-8eb5-40f8-860b-a64e583a7ef1.cpp:38: 0c2a863c-8eb5-40f8-860b-a64e583a7ef1.cpp: In function ‘int main()’: 0c2a863c-8eb5-40f8-860b-a64e583a7ef1.cpp:48:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    48 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 0c2a863c-8eb5-40f8-860b-a64e583a7ef1.cpp:3: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ In file included from 0c2a863c-8eb5-40f8-860b-a64e583a7ef1.cpp:38: 0c2a863c-8eb5-40f8-860b-a64e583a7ef1.cpp:48:19: error: ‘issame’ cannot be used a s a function    48 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -27,10 +27,10 @@ namespace std {
 
 int main_test() { 
     std::vector<int> input = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    auto output = input; // Create a copy of the input vector
-    auto uniqueOutput = std::unique(output.begin(), output.end()); // Use std::unique to get the unique elements
-    for (auto i = output.begin(); i != uniqueOutput; ++i) {
-        std::cout << *i << " ";
+    std::vector<int> uniqueInput(input.begin(), input.end());
+    auto output = std::distinct(uniqueInput.begin(), uniqueInput.end());
+    for (auto i : output) {
+        std::cout << i << " ";
     }
-    std::cout << std::endl;
+    assert(std::vector<int>({0, 2, 3, 5, 9, 123}) == std::vector<int>(std::distinct(uniqueInput.begin(), uniqueInput.end())));
 }
