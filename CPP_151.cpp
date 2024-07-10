739df9dc4f880967cb78f559c9dbce1bf7931c63
beep boop <githubbot@vadim.me>
2024-07-11 00:38:08 +0200

    Solution 12 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jafm6m1z Prompt: d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp: At global scope: d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~ In file included from d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp:32: d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp: In function ‘int main()’: d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp:34:35: error: ‘lst’ was not declared in  this scope    34 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp:34:38: error: ‘double_the_difference’ c annot be used as a function    34 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ d051a2ea-1daf-4cd7-808a-de9ec15c3b85.cpp:34:43: error: ‘odd_sum’ was not declare d in this scope    34 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,4 +1,3 @@
-```cpp
 long long double_the_difference(vector<float> lst){
     long long sum = 0;
     for (float num : lst) {
@@ -11,23 +10,19 @@ long long double_the_difference(vector<float> lst){
 }
 
 int main() {
-    float num;
+    vector<float> lst; 
+    long long odd_sum = 0;
     
     cout << "Enter numbers (enter 'stop' to finish):" << endl;
-    vector<float> lst; 
-    int odd_sum = 0;
-    
     while(1) {
+        float num;
+        
         cin >> num;
         
         if(std::to_string(num) == "stop") break;
 
-        if(num > 0 && modf(num, &num) == 0.0) {
-            for (float n : lst) {
-                if (n > 0 && modf(n, &n) == 0.0) {
-                    odd_sum = double_the_difference(lst);
-                }
-            }
+        if(num > 0 && modf((num + 1.0) / 2, &num) == 0.5) {
+            odd_sum = double_the_difference(lst);
         }
         lst.push_back(num);    
     }
