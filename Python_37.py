138780f6fafdf8695d70fd9f2c53b686fa438034
beep boop <githubbot@vadim.me>
2024-07-10 18:58:58 +0200

    Solution 11 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uquygx2d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/25b3afec-50c3-456e-82a1-e16a681ea b32.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/25b3afec-50c3-456e-82a1-e16a681ea b32.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,6 +1,5 @@
+```Python
 def sort_even(l: list):
     even = sorted([x for x in l if x % 2 == 0])
     odd = sorted([x for x in l if x % 2 != 0])
-    return [i for i in zip(even, odd) or (even, [])][0] + [
-        i for i in (l if len(l) % 2 else l[:-1]) if i % 2 != 0
-    ]
+    return [i for i in zip(even, odd) or (even, [])][0] + [i for i in (l if len(l) % 2 else l[:-1]) if i % 2 != 0]
