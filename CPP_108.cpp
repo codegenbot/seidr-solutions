0a8f1f0aeb59190d068da0cb2e1eb8f5003e25b6
beep boop <githubbot@vadim.me>
2024-07-10 20:31:56 +0200

    Solution 10 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/hrbw7j9p Prompt: 8a1f3eed-c59a-4e96-bc9c-39e87ca7d205.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int count_nums(vector<int> nums) {       |                ^~~~~~ 8a1f3eed-c59a-4e96-bc9c-39e87ca7d205.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int count_nums(vector<int> nums) {       |                       ^~~ In file included from 8a1f3eed-c59a-4e96-bc9c-39e87ca7d205.cpp:37: 8a1f3eed-c59a-4e96-bc9c-39e87ca7d205.cpp: In function ‘int main()’: 8a1f3eed-c59a-4e96-bc9c-39e87ca7d205.cpp:39:23: error: ‘count_nums’ cannot be us ed as a function    39 |     assert (count_nums({1}) == 1);       |             ~~~~~~~~~~^~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,17 +1,36 @@
-int count_nums(vector<int> n) {
+#include <vector>
+
+int count_nums(vector<int> nums) {
     int count = 0;
-    for (int num : n) {
-        if (num < 0) {
-            num = -num;
-        }
-        int sum_digits = 0;
-        while (num > 0) {
-            sum_digits += num % 10;
-            num /= 10;
-        }
-        if (sum_digits > 0) {
-            count++;
+    for (int num : nums) {
+        if (num >= 0) {
+            int sum = 0;
+            int temp = abs(num);
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            if (sum > 0) {
+                count++;
+            }
+        } else {
+            int sum = 0;
+            int temp = -num;
+            bool isNegative = true;
+            while (temp > 0) {
+                if (isNegative) {
+                    if (temp % 10 < 0) {
+                        break;
+                    }
+                    sum -= temp % 10;
+                } else {
+                    sum += temp % 10;
+                }
+                temp /= 10;
+            }
+            if (sum > 0) {
+                count++;
+            }
         }
     }
     return count;
-}
