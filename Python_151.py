825cdaf861b9b9fe411881c863ca6d96e6e39679
beep boop <githubbot@vadim.me>
2024-07-10 09:04:21 +0200

    Solution 14 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/e7h6db9i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5a8d358e-8ebf-44a6-b151-a95eb3361 3b2.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5a8d358e-8ebf-44a6-b151-a95eb3361 3b2.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,13 +1,9 @@
 ```
 def double_the_difference(lst):
-    result = (max(lst) - min(lst)) * 2
-    for i in lst:
-        if isinstance(i, int) and i > 0:
-            result += i**2
-    return result
+    return sum(i**2 for i in lst if isinstance(i, int) and i > 0)
 
 list1 = [1, 2, 3]
 list2 = [-4, 5, -6]
 
-check(double_the_difference(list1))
-check(double_the_difference(list2))
+print(double_the_difference(list1))
+print(double_the_difference(list2))
