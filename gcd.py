10ae53e5dd084fdacc6e17dbd1e64ee9ebada27d
beep boop <githubbot@vadim.me>
2024-07-10 19:06:14 +0200

    Solution 23 of gcd, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/j6gjiw27 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/21570168-9855-4b41-a198-4fbe1f211 750.py", line 1     ```     ^ SyntaxError: invalid syntax

--- gcd.py
+++ gcd.py
@@ -12,7 +12,7 @@ def find_indices(text, target):
     return indices
 
 print(gcd(1, 1))  
-print(gcd(4, 400000))  
-print(gcd(54, 24))  
-print(gcd(4200, 3528))  
+print(gcd(4, 400000)) 
+print(gcd(54, 24)) 
+print(gcd(4200, 3528)) 
 print(gcd(820000, 63550))
