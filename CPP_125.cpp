1599134b93ca2cc1569819f0c6acaf1c77f575ed
beep boop <githubbot@vadim.me>
2024-07-10 22:05:37 +0200

    Solution 17 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rho8j4le Prompt:       |                              ^~~~~~ 36199b2c-8c94-4ce3-8825-abca71bae51b.cpp:29:37: error: ‘string’ was not declared  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 36199b2c-8c94-4ce3-8825-abca71bae51b.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 36199b2c-8c94-4ce3-8825-abca71bae51b.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 36199b2c-8c94-4ce3-8825-abca71bae51b.cpp:28: 36199b2c-8c94-4ce3-8825-abca71bae51b.cpp: In function ‘int main()’: 36199b2c-8c94-4ce3-8825-abca71bae51b.cpp:38:20: error: ‘split_words’ was not dec lared in this scope    38 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 36199b2c-8c94-4ce3-8825-abca71bae51b.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -3,47 +3,15 @@
 #include <string>
 
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) {
+    if(a.size() != b.size())
         return false;
-    }
-    
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
+    for(int i = 0; i<a.size(); i++)
+        if(a[i] != b[i])
             return false;
-        }
-    }
-    
     return true;
 }
 
 int main() {
     assert(issame(split_words(""), {"0"}));
     return 0;
-}
-
-vector<string> split_words(string txt) {
-    vector<string> result;
-    bool inWord = false;
-    string word = "";
-
-    for (char c : txt) {
-        if (!inWord && !isalnum(c)) {
-            // Check for commas
-            if (c == ',') {
-                continue;
-            }
-            inWord = true;
-        } else if (inWord && !isalnum(c)) {
-            result.push_back(word);
-            word = "";
-            inWord = false;
-        } else {
-            word += c;
-        }
-    }
-
-    if (inWord) {
-        result.push_back(word);
-    }
-    return result;
 }
