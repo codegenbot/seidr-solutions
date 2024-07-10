574824be01085d96fba47db4e46548d230cddd68
beep boop <githubbot@vadim.me>
2024-07-10 23:41:41 +0200

    Solution 8 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/axs5dzrq Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 720647f5-dc89-4f20-a162-9d2965e17273.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 720647f5-dc89-4f20-a162-9d2965e17273.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 720647f5-dc89-4f20-a162-9d2965e17273.cpp:26: 720647f5-dc89-4f20-a162-9d2965e17273.cpp: In function ‘int main()’: 720647f5-dc89-4f20-a162-9d2965e17273.cpp:36:20: error: ‘order_by_points’ was not  declared in this scope    36 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 720647f5-dc89-4f20-a162-9d2965e17273.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,16 +1,31 @@
 #include <vector>
 #include <algorithm>
 
+bool issame(int a, int b) {
+    int sumA = 0, sumB = 0;
+    while(a > 0) {
+        sumA += a % 10;
+        a /= 10;
+    }
+    while(b > 0) {
+        sumB += b % 10;
+        b /= 10;
+    }
+    return sumA == sumB;
+}
+
 vector<int> order_by_points(vector<int> nums) {
     vector<pair<int, int>> pairs;
     for (int i = 0; i < nums.size(); i++) {
-        int sumOfDigits = 0;
         int num = nums[i];
         while (num > 0) {
-            sumOfDigits += num % 10;
-            num /= 10;
+            int sumOfDigits = 0;
+            while (num > 0) {
+                sumOfDigits += num % 10;
+                num /= 10;
+            }
+            pairs.emplace_back(sumOfDigits, i);
         }
-        pairs.emplace_back(sumOfDigits, i);
     }
 
     sort(pairs.begin(), pairs.end());
