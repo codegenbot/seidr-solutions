ec0e8c5731b5b059426cc33c8ccfaedfaea8ed29
beep boop <githubbot@vadim.me>
2024-07-10 03:41:36 +0200

    Solution 67 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/06fq9cb0 Prompt: 53b82489-ba89-436b-ac89-bc79fd42571c.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ 53b82489-ba89-436b-ac89-bc79fd42571c.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from 53b82489-ba89-436b-ac89-bc79fd42571c.cpp:37: 53b82489-ba89-436b-ac89-bc79fd42571c.cpp: In function ‘int main()’: 53b82489-ba89-436b-ac89-bc79fd42571c.cpp:47:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    47 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~ 53b82489-ba89-436b-ac89-bc79fd42571c.cpp:6:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     6 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ 53b82489-ba89-436b-ac89-bc79fd42571c.cpp:38:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    38 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-bool issame(vector<int>& a, vector<int> b) {
+bool issame(const vector<int>& a, const vector<int>& b) {
     return a == b;
 }
 
@@ -22,14 +22,28 @@ vector<int> pluck(const vector<int>& n) {
 
 }
 
-int main() {
+void testMain() {
     vector<int> v1 = {7, 9, 7, 1};
     vector<int> v2;
 
-    if (issame(pluck(v1), pluck({})) )  
+    for (int i = 0; i < 4; i++) {
+        int x;
+        cin >> x;
+        if(x == 7 || x == 9)
+            v2.push_back(1); 
+        else if(x == 1)
+            v2.push_back(0); 
+
+    }
+
+    if (issame(pluck(v1), pluck(v2)))  
         cout << "Arrays are same" << endl;
     else   
         cout << "Arrays are not same" << endl;
 
+}
+
+int main() {
+    testMain();
     return 0;
 }
