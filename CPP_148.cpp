92b436d6038275d7617b3970d2210d3dee2fa948
beep boop <githubbot@vadim.me>
2024-07-10 11:34:48 +0200

    Solution 53 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/twp8lzfu Prompt:       |             ^~~~~~ 34f0167d-3b31-466c-bb04-c3312c081a30.cpp:4:37: error: ‘string’ was not declared in this scope     4 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 34f0167d-3b31-466c-bb04-c3312c081a30.cpp:4:44: error: ‘b’ was not declared in th is scope     4 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 34f0167d-3b31-466c-bb04-c3312c081a30.cpp:4:45: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 34f0167d-3b31-466c-bb04-c3312c081a30.cpp:3: 34f0167d-3b31-466c-bb04-c3312c081a30.cpp: In function ‘int main()’: 34f0167d-3b31-466c-bb04-c3312c081a30.cpp:13:20: error: ‘bf’ was not declared in this scope    13 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 34f0167d-3b31-466c-bb04-c3312c081a30.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,23 +1,11 @@
-#include <iostream>
-#include <string>
 #include <vector>
-#include <algorithm>
+#include <string>
 #include <cassert>
 
-using namespace std;
-
-bool areSame(vector<string> a, vector<string> b){
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b){
     return a == b;
 }
 
-vector<string> bf(const string& s1, const string& s2) {
-    vector<string> common_chars;
-    string sorted_s1 = s1;
-    string sorted_s2 = s2;
-    sort(sorted_s1.begin(), sorted_s1.end());
-    sort(sorted_s2.begin(), sorted_s2.end());
-    
-    std::set_intersection(sorted_s1.begin(), sorted_s1.end(), sorted_s2.begin(), sorted_s2.end(), back_inserter(common_chars));
-    
-    return common_chars;
+int main(){
+    assert (issame({"Jupiter", "Makemake"} , {}));
 }
