ff0c0ec9332032f4f277d059ff5fac4bc567bbae
beep boop <githubbot@vadim.me>
2024-07-10 22:24:42 +0200

    Solution 24 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xrg5ajon Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6afa0414-ac17-455a-b47e-26cbe05d4 3ec.py", line 21, in <module>     check(match_parens)           ^^^^^^^^^^^^ NameError: name 'match_parens' is not defined

--- Python_119.py
+++ Python_119.py
@@ -1,17 +1,13 @@
-```
 def find_max_score(lst):
     max_score = 0
     current_score = 0
     for s in lst:
-        match_count = 0
         for c in s:
             if c == 'O':
                 current_score += 1
-                match_count += 1
             elif c == 'X':
-                if current_score > 0 and match_count > 0:
+                if current_score > 0:
                     current_score -= 1
-                    match_count -= 1
                 else:
                     return -1
         max_score = max(max_score, current_score)
