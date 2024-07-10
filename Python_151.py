d919eea43f1e0f4a11d51225fb7300ca423d08fb
beep boop <githubbot@vadim.me>
2024-07-10 22:42:07 +0200

    Solution 16 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zckjcwmr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ee0bbc24-97d6-4820-8295-13f8eb3bc bb9.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,5 +1,8 @@
+```
+double_the_difference_value = lambda x: (x[1] - x[0]) * 2
 def double_the_difference(lst):
     odd_sum = sum(i**2 for i in lst if isinstance(i, int) and i >= 0 and i % 2 != 0)
-    return (sum(lst) - odd_sum) * 2
+    return double_the_difference_value(odd_sum)
 
-print(double_the_difference([7, 1]))
+check_result = double_the_difference([7, 1])
+print(check_result)
