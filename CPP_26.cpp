9cbff9478e61d8a2a5e6b0c840b5fb353d915c37
beep boop <githubbot@vadim.me>
2024-07-10 03:20:26 +0200

    Solution 98 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/v2xoqm3t Prompt: 32a377e9-e7b3-46b9-8992-37268d2442fc.cpp:35:34: error: expected primary-expressi on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 32a377e9-e7b3-46b9-8992-37268d2442fc.cpp:35:39: error: expression list treated a s compound expression in initializer [-fpermissive]    35 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 32a377e9-e7b3-46b9-8992-37268d2442fc.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 32a377e9-e7b3-46b9-8992-37268d2442fc.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~ In file included from 32a377e9-e7b3-46b9-8992-37268d2442fc.cpp:34: 32a377e9-e7b3-46b9-8992-37268d2442fc.cpp: In function ‘int main()’: 32a377e9-e7b3-46b9-8992-37268d2442fc.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,8 +1,15 @@
 #include <algorithm>
 #include <vector>
 
-bool areEqual(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
+bool areEqualArrays(const std::vector<int>& a, const std::vector<int>& b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (std::find(b.begin(), b.end(), a[i]) == b.end())
+            return false;
+    }
+    return true;
 }
 
 std::vector<int> remove_duplicates(std::vector<int> v) {
@@ -15,18 +22,7 @@ std::vector<int> remove_duplicates(std::vector<int> v) {
     return unique_v;
 }
 
-bool isEqual(std::vector<int> a, std::vector<int> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (std::find(b.begin(), b.end(), a[i]) == b.end())
-            return false;
-    }
-    return true;
-}
-
 int main() {
-    assert(isEqual(std::vector<int>({1, 2, 3, 2, 4, 3, 5}), std::vector<int>({1, 4, 5})));
+    assert(areEqualArrays(std::vector<int>({1, 2, 3, 2, 4, 3, 5}), std::vector<int>({1, 4, 5})));
     return 0;
 }
