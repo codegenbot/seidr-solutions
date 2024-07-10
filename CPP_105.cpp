ede9559b1657f437ad203b4f9d87b9a040f80007
beep boop <githubbot@vadim.me>
2024-07-10 10:21:06 +0200

    Solution 78 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mmzl1wqj Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,25 +1,17 @@
 vector<string> by_length(vector<int> arr) {
-    vector<int> numVec;
+    vector<int> temp;
     for (int i : arr) {
-        if (i >= 1 && i <= 9) {
-            numVec.push_back(i);
-        }
+        if (i >= 1 && i <= 9)
+            temp.push_back(i);
     }
-
-    sort(numVec.begin(), numVec.end());
-    
-    reverse(numVec.begin(), numVec.end());
-
+    sort(temp.begin(), temp.end());
+    reverse(temp.begin(), temp.end());
     vector<string> result;
-    map<int, string> numberMap = {{1, "One"}, {2, "Two"}, {3, "Three"}, 
-                                   {4, "Four"}, {5, "Five"}, {6, "Six"},
-                                   {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-
-    for (int i : numVec) {
-        if (numberMap.find(i) != numberMap.end()) {
-            result.push_back(numberMap[i]);
-        }
+    map<int, string> digitNames = {{1, "One"}, {2, "Two"}, {3, "Three"}, 
+                                     {4, "Four"}, {5, "Five"}, {6, "Six"}, 
+                                     {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
+    for (int i : temp) {
+        result.push_back(digitNames[i]);
     }
-
     return result;
 }
