59aa83be9d0b2995720f48704d7206002f8824b1
beep boop <githubbot@vadim.me>
2024-07-10 03:42:23 +0200

    Solution 87 of CPP/58, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0hw5ynmd Prompt: 36579f10-8e7b-40c2-9683-79196332fd3d.cpp:77:5: error: redefinition of ‘int main( )’    77 | int main(){       |     ^~~~ 36579f10-8e7b-40c2-9683-79196332fd3d.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,15 +1,65 @@
 #include <vector>
-#include <algorithm>
+#include <iostream>
 using namespace std;
 
-vector<int> common(vector<int> l1, vector<int> l2) {
-    set<int> s1(l1.begin(), l1.end());
-    set<int> s2(l2.begin(), l2.end());
+bool isSame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+      }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+          }
+    }
+    return true;
+}
 
-    set<int> intersection;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-        inserter(intersection, intersection.begin()));
-
-    vector<int> result(intersection.begin(), intersection.end());
+vector<int> common(vector<int> a, vector<int> b) {
+    vector<int> result;
+    for (int i = 0; i < a.size(); i++) {
+      int val1 = a[(i+3)%4];
+      int val2 = a[(i+1)%4];
+      for(int j=0; j<4; j++){
+        if(val1 == b[j] && val2==b[(j+1)%4]){
+          result.push_back(a[i]);
+          break;
+        }
+      }
+    }
     return result;
+
+}
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+
+  vector<vector<int>> l1(n), l2(m);
+
+  for (int i = 0; i < n; i++) {
+    l1[i].resize(4);
+    for (int j = 0; j < 4; j++) {
+      cin >> l1[i][j];
+    }
+  }
+
+  for (int i = 0; i < m; i++) {
+    l2[i].resize(4);
+    for (int j = 0; j < 4; j++) {
+      cin >> l2[i][j];
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    vector<int> temp(l1[i]);
+    vector<int> result = common(temp, {l2[0][0], l2[0][1], l2[0][2], l2[0][3]});
+    for (int j : result) {
+      cout << j;
+      if (j < result.size() - 1) {
+        cout << " ";
+      }
+    }
+    cout << endl;
+  }
+
 }
