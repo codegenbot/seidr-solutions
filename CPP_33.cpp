404503ff233e51d142c61b760fc5da49d3d07b56
beep boop <githubbot@vadim.me>
2024-07-10 22:37:38 +0200

    Solution 2 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/v1colh7b Prompt: Complete the following code given the task description and function signature.

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,5 +1,5 @@
 vector<int> l_copy = l;
-    for (int i = 0; i < l.size(); i++) {
+    for (int i = 0; i < l.size(); ++i) {
         if ((i + 1) % 3 == 0) {
             sort(l_copy.begin() + i - 2, l_copy.begin() + i + 1);
         }
