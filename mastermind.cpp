6ead54ac1ea68a3048fa4b6fc0cbd05338c3bd31
beep boop <githubbot@vadim.me>
2024-07-10 07:05:47 +0200

    Solution 2 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/tjtxcwav Prompt: 849e49f1-390a-4181-a3c5-461695ff1e46.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ 849e49f1-390a-4181-a3c5-461695ff1e46.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ 849e49f1-390a-4181-a3c5-461695ff1e46.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,20 +1,37 @@
-int mastermind(string code, string guess) {
-    int white = 0;
-    int black = 0;
+#include <string>
+using namespace std;
 
-    for (int i = 0; i < 4; i++) {
+struct PegCounts {
+    int black;
+    int white;
+};
+
+PegCounts mastermind(string code, string guess) {
+    int black = 0;
+    int white = 0;
+
+    // Count the number of correct colors in wrong positions
+    for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             black++;
-        } else {
-            bool found = false;
-            for (int j = 0; j < 4; j++) {
-                if (guess[j] == code[i] && !found) {
-                    white++;
-                    found = true;
-                }
-            }
+            code[i] = ' '; // Mark as used to avoid counting again
+            guess[i] = ' ';
         }
     }
 
-    return black + white - 2;
+    // Count the number of correct colors in correct positions
+    for (int i = 0; i < 4; ++i) {
+        bool correctColor = false;
+        for (int j = 0; j < 4; ++j) {
+            if (code[j] == guess[i]) {
+                correctColor = true;
+                break;
+            }
+        }
+        if (correctColor) {
+            white++;
+        }
+    }
+
+    return {black, white};
 }
