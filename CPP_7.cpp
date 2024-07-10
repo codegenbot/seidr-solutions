104f3cafefbcf9b8e52ead9fc7c247a2f05b2ea6
beep boop <githubbot@vadim.me>
2024-07-10 12:48:12 +0200

    Solution 71 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/vsjr6hya Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e787904a-dab7-4243-bc36-365e9e64a56e.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e787904a-dab7-4243-bc36-365e9e64a56e.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e787904a-dab7-4243-bc36-365e9e64a56e.cpp:11: e787904a-dab7-4243-bc36-365e9e64a56e.cpp: In function ‘int main()’: e787904a-dab7-4243-bc36-365e9e64a56e.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ e787904a-dab7-4243-bc36-365e9e64a56e.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,20 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cassert>
-
-using namespace std;
-
-vector<string> filter_by_substring(vector<string> strings, string substring) {
-    vector<string> filtered_strings;
-    for (const auto& str : strings) {
-        if (str.find(substring) != string::npos) {
-            filtered_strings.push_back(str);
-        }
-    }
-    return filtered_strings;
-}
-
 bool issame(vector<string> a, vector<string> b){
     return a == b;
 }
