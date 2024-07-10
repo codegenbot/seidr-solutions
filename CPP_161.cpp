798d85141d8403dcd514eeaf9e307e961fd7579b
beep boop <githubbot@vadim.me>
2024-07-10 02:37:08 +0200

    Solution 40 of CPP/161, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/9b04gq71 Prompt:       | ^~~ 42f4db13-3691-48fd-9b04-4279dfd1cd65.cpp:6:5: error: expected unqualified-id bef ore ‘if’     6 |     if(all_of(s.begin(), s.end(), [](char c){ return !isalpha(c); })){       |     ^~ 42f4db13-3691-48fd-9b04-4279dfd1cd65.cpp:6:68: error: expected unqualified-id be fore ‘)’ token     6 |     if(all_of(s.begin(), s.end(), [](char c){ return !isalpha(c); })){       |                                                                    ^ 42f4db13-3691-48fd-9b04-4279dfd1cd65.cpp:9:5: error: expected unqualified-id bef ore ‘return’     9 |     return s;       |     ^~~~~~ 42f4db13-3691-48fd-9b04-4279dfd1cd65.cpp:10:1: error: expected declaration befor e ‘}’ token    10 | }       | ^ In file included from 42f4db13-3691-48fd-9b04-4279dfd1cd65.cpp:12: 42f4db13-3691-48fd-9b04-4279dfd1cd65.cpp: In function ‘int main()’: 42f4db13-3691-48fd-9b04-4279dfd1cd65.cpp:14:13: error: ‘solve’ was not declared in this scope    14 |     assert (solve("#ccc") == "#CCC");       |             ^~~~~

--- CPP_161.cpp
+++ CPP_161.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <algorithm>
 
+std::string solve(std::string s);
+
+int main() {
+    std::string s;
+    std::getline(std::cin, s);
+    std::cout << solve(s) << std::endl;
+}
+
 std::string solve(std::string s) {
     for(char &c : s){
         if(isalpha(c)){
@@ -11,11 +19,4 @@ std::string solve(std::string s) {
         reverse(s.begin(), s.end());
     }
     return s;
-}
-
-int main() {
-    std::string s;
-    std::getline(std::cin, s);
-    std::cout << solve(s) << std::endl;
-    return 0;
 }
