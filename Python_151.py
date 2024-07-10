a4ce73990662b1771630c0409f850eb1cb928b2b
beep boop <githubbot@vadim.me>
2024-07-10 05:27:37 +0200

    Solution 45 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/520ebw7r Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/12adc53b-ce95-467e-b1f9-4fccf13ad 09b.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/12adc53b-ce95-467e-b1f9-4fccf13ad 09b.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,12 +1,6 @@
-# Input handling
-lst = [1, 3, 5, 7, 9]  # Insert the desired list of odd positive integers here
-
-
-# Function definition
+lst = [1, 3, 5, 7, 9]
 def double_the_difference(lst):
     return sum(x**2 for x in lst if x > 0 and isinstance(x, int) and x % 2 != 0) * 2
 
-
-# Call the function with the defined input
 result = double_the_difference(lst)
 print(result)
