2c79c5c23a528ca57ad50fb8d69a44efdbfa3c7b
beep boop <githubbot@vadim.me>
2024-07-10 10:15:55 +0200

    Solution 1 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/i6gqigap Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -4,39 +4,43 @@ using namespace std;
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
     int n = grid.size();
-    vector<vector<int>> dp(n, vector<int>(n, 0));
-    
+    vector<vector<int>> dp(n, vector<int>(n, 1e9));
+    vector<vector<int>> prev(n, vector<int>(n, -1));
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (i == 0 && j == 0) {
                 dp[i][j] = grid[i][j];
             } else if (i > 0) {
-                dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + grid[i][j];
-            } else {
-                dp[i][j] = min({dp[i][j-1], dp[i-1][j]}) + grid[i][j];
+                dp[i][j] = min(dp[i][j], dp[i - 1][j] + grid[i][j]);
+                prev[i][j] = i > 1 ? prev[i - 1][j] : j;
+            } else if (j > 0) {
+                dp[i][j] = min(dp[i][j], dp[i][j - 1] + grid[i][j]);
+                prev[i][j] = j > 1 ? prev[i][j - 1] : i;
             }
         }
     }
-    
-    int res = INT_MAX;
-    vector<int> ans;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (dp[i][j] == dp[n-1][n-1]) {
-                int path_len = 0;
-                int curr_val = grid[i][j];
-                while (path_len < k) {
-                    ans.push_back(curr_val);
-                    if (i > 0 && dp[i-1][j] == dp[i][j]) i--;
-                    else if (j > 0 && dp[i][j-1] == dp[i][j]) j--;
-                    else i--, j--;
-                    curr_val = grid[i][j];
-                    path_len++;
-                }
-                return ans;
-            }
+
+    vector<int> path;
+    int x = n - 1, y = n - 1;
+    for (int i = 0; i < k; i++) {
+        path.push_back(grid[x][y]);
+        if (prev[x][y] == x - 1) {
+            x--;
+        } else if (prev[x][y] == y - 1) {
+            y--;
         }
     }
-    
-    return {};
+
+    return path;
+}
+
+int main() {
+    vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int k = 3;
+    vector<int> result = minPath(grid, k);
+    for (int i : result) {
+        cout << i << " ";
+    }
+    return 0;
 }
