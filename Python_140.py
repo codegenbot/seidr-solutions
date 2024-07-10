46d7d889850836f226256c6fa5e1c8fd37aa0533
beep boop <githubbot@vadim.me>
2024-07-10 22:40:55 +0200

    Solution 20 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ygu5uohz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b1b507b4-9f88-40ea-bdab-3f1192c2c 7b4.py", line 27, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b1b507b4-9f88-40ea-bdab-3f1192c2c 7b4.py", line 23, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,20 +1,23 @@
 def fix_spaces(text):
     new_text = ''
     count = 0
-    in_word = False
     for char in text:
         if char == ' ':
-            if not in_word:
-                new_text += ' '
+            if count < 2:
+                new_text += '_'
             else:
-                if count <= 2:
-                    new_text += '_'
-                else:
-                    new_text += '-'
-                count = 0
-            in_word = False
-        elif char != '\n':
+                new_text += '-'
+            count = 0
+        elif count >= 3:
+            new_text += ' '
+            new_text += '_'
+            count = 1
+        else:
             new_text += char
-            in_word = True
             count += 1
+    if count > 0:
+        if count < 2:
+            new_text += '_' * (count + 1)
+        else:
+            new_text += '-' * (count + 1)
     return new_text.strip()
