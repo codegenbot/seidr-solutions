4c8ec71ea9818ed766600d820d827431e70e7ff8
beep boop <githubbot@vadim.me>
2024-07-10 19:33:56 +0200

    Solution 37 of indices-of-substring, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/xet6th78 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1d7d74e-8b13-43d3-8635-6b7d0954f 4b1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -1,6 +1,4 @@
+```
 def indices_of_substring(text, target):
-    result = [i for i in range(len(text)) if text.startswith(target) and i <= len(text) - len(target)]
-    for i in range(len(text)):
-        if text[i:i+len(target)] == target:
-            result.append(i)
+    result = [i for i in range(len(text)) if text[i:i+len(target)] == target]
     return result
