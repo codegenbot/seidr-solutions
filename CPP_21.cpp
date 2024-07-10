872c0909bbca2cf1ad8948bd536d8a0eb047108e
beep boop <githubbot@vadim.me>
2024-07-10 03:01:10 +0200

    Solution 6 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/p4f8v4no Prompt:    38 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 432b806c-3749-489a-b0cd-199affe01898.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ 432b806c-3749-489a-b0cd-199affe01898.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~ In file included from 432b806c-3749-489a-b0cd-199affe01898.cpp:37: 432b806c-3749-489a-b0cd-199affe01898.cpp: In function ‘int main()’: 432b806c-3749-489a-b0cd-199affe01898.cpp:47:20: error: ‘rescale_to_unit’ was not  declared in this scope    47 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 432b806c-3749-489a-b0cd-199affe01898.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -9,13 +9,13 @@ bool issame(vector<float> a, vector<float> b);
 vector<float> rescale_to_unit(vector<float> numbers) {
     float min_num = *min_element(numbers.begin(), numbers.end());
     float max_num = *max_element(numbers.begin(), numbers.end());
-
+    
     vector<float> rescaled_numbers;
     for (float num : numbers) {
         float rescaled_num = (num - min_num) / (max_num - min_num);
         rescaled_numbers.push_back(rescaled_num);
     }
-
+    
     return rescaled_numbers;
 }
 
@@ -23,13 +23,13 @@ bool issame(vector<float> a, vector<float> b) {
     if (a.size() != b.size()) {
         return false;
     }
-
+    
     for (int i = 0; i < a.size(); ++i) {
         if (abs(a[i] - b[i]) > 1e-6) {
             return false;
         }
     }
-
+    
     return true;
 }
 
