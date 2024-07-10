84c489d8e22aecbc96f27b184af3d3476a9fa29f
beep boop <githubbot@vadim.me>
2024-07-10 21:08:47 +0200

    Solution 62 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6mp2wjk0 Prompt:       |                           ^~~~~~ ec39d7f0-2a82-4557-ab6e-b8dd185d8236.cpp:4:34: error: expected primary-expressio n before ‘int’     4 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ec39d7f0-2a82-4557-ab6e-b8dd185d8236.cpp:4:39: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ec39d7f0-2a82-4557-ab6e-b8dd185d8236.cpp:3: ec39d7f0-2a82-4557-ab6e-b8dd185d8236.cpp: In function ‘int main()’: ec39d7f0-2a82-4557-ab6e-b8dd185d8236.cpp:13:20: error: ‘common’ was not declared  in this scope    13 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ ec39d7f0-2a82-4557-ab6e-b8dd185d8236.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ec39d7f0-2a82-4557-ab6e-b8dd185d8236.cpp:13:5: error: ‘__assert_fail’ was not de clared in this scope    13 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |     ^~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,31 +1,26 @@
-int main 
-{   
-    int n1, n2;
-    std::vector<int> v1, v2;
+#include <iostream>
+#include <vector>
 
-    std::cout << "Enter number of elements in the first vector: ";
-    std::cin >> n1;
-    for (int i = 0; i < n1; i++) {
-        int x;
-        std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> x;
-        v1.push_back(x);
+std::vector<int> common(std::vector<int> a, std::vector<int> b) {
+    std::vector<int> result;
+    for(int x : a){
+        if(std::find(b.begin(),b.end(),x)!=b.end())
+            result.push_back(x);
     }
+    return result;
+}
 
-    std::cout << "\nEnter number of elements in the second vector: ";
-    std::cin >> n2;
-    for (int i = 0; i < n2; i++) {
-        int x;
-        std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> x;
-        v2.push_back(x);
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if(a.size()!=b.size())
+        return false;
+    for(int x : a){
+        if(std::find(b.begin(),b.end(),x)==b.end())
+            return false;
     }
+    return true;
+}
 
-    if(issam(v1, v2)) {
-        std::cout << "The vectors are the same.";
-    } else {
-        std::cout << "The vectors are not the same.";
-    }
-    
-    return 0;   
+int main() {
+    assert(issame(common({4, 3, 2, 8}, {4, 3, 2}, {4, 3}), {4, 3}));
+    return 0;
 }
