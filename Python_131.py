8a4a5d75eec7c85e99299f756843e3bf9a8c4ba5
beep boop <githubbot@vadim.me>
2024-07-10 23:59:45 +0200

    Solution 20 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7vpcfgx6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/76c94ca6-9066-4518-a29f-1a8518524 337.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_131.py
+++ Python_131.py
@@ -1,9 +1,8 @@
-```
-def digits(n):
+'''def digits(n):
     max_product = 1
     while n:
         digit = n % 10
-        if digit != 0 and digit % 2 == 0: 
+        if digit != 0 and digit % 2 != 0: 
             max_product *= digit
         n //= 10
-    return max_product if max_product else 0
+    return max_product if max_product else 0'''
