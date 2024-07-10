719e10d134ec167637c121faa4077c0f51957655
beep boop <githubbot@vadim.me>
2024-07-10 04:45:27 +0200

    Solution 68 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9z8gtv5q Prompt: Program did not receive expected input

--- Python_20.py
+++ Python_20.py
@@ -2,12 +2,12 @@
 def check(lst):
     lst.sort()
     result = float('inf')
-    if len(lst) >= 2:
-        for i in range(len(lst) - 1):
-            if lst[i] != lst[i+1]:
-                diff = abs(lst[i] - lst[i+1])
-                result = min(result, diff)
+    for i in range(len(lst) - 1):
+        if lst[i] != lst[i+1]:
+            diff = abs(lst[i] - lst[i+1])
+            result = min(result, diff)
     return result
 
+n = int(input())
 arr = list(map(int, input().split()))
 print(check(arr))
