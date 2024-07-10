1675b8254f1126fbde460971fd05c88b722e41f6
beep boop <githubbot@vadim.me>
2024-07-10 04:51:19 +0200

    Solution 19 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/n2wga4wg Prompt:    40 | int main(){       |     ^~~~ 8b861213-0743-45b2-a9da-3a462d8086a1.cpp:6:5: note: ‘int main()’ previously defi ned here     6 | int main() {       |     ^~~~ In file included from 8b861213-0743-45b2-a9da-3a462d8086a1.cpp:39: 8b861213-0743-45b2-a9da-3a462d8086a1.cpp: In function ‘int main()’: 8b861213-0743-45b2-a9da-3a462d8086a1.cpp:41:13: error: ‘boost’ has not been decl ared    41 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 8b861213-0743-45b2-a9da-3a462d8086a1.cpp:41:35: error: expected primary-expressi on before ‘>’ token    41 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 8b861213-0743-45b2-a9da-3a462d8086a1.cpp:41:37: error: ‘compare_one’ was not dec lared in this scope; did you mean ‘compareOne’?    41 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,45 +1,40 @@
-#include <boost/config.hpp>
-#include <boost/any.hpp>
+```cpp
+#include <iostream>
 #include <string>
-#include <algorithm>
+#include <any>
 
 using namespace std;
+using namespace boost;
 
-boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return max((int)a.convertible_to<int>(), (float)b.convertable_to<float>());
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string str = (string)a.convertible_to<string>();
-        float f = (float)b.convertable_to<float>();
-        return (f > stof(str)) ? boost::any(f) : boost::any(str);
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = (string)a.convertible_to<string>();
-        string str2 = (string)b.convertible_to<string>();
-        return (stof(str2) > stof(str1)) ? boost::any(str2) : (stof(str1) == stof(str2)) ? boost::any("None") : boost::any(str1);
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int i = (int)a.convertible_to<int>();
-        string str = (string)b.convertible_to<string>();
-        return (stof(str) > i) ? boost::any(str) : (i == stof(str)) ? boost::any("None") : boost::any(i);
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        string str = (string)a.convertible_to<string>();
-        int i = (int)b.convertible_to<int>();
-        return (stof(str) > i) ? boost::any(str) : (i == stof(str)) ? boost::any("None") : boost::any(i);
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        float f = (float)a.convertible_to<float>();
-        int i = (int)b.convertible_to<int>();
-        return (f > i) ? boost::any(f) : (i == f) ? boost::any("None") : boost::any(i);
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int i1 = (int)a.convertible_to<int>();
-        int i2 = (int)b.convertible_to<int>();
-        return (i2 > i1) ? boost::any(i2) : (i1 == i2) ? boost::any("None") : boost::any(i1);
-    }
-    else {
+int CompareOne(int a, any b) {
+    if (any_cast<int>(b) > a) {
+        return any_cast<int>(b);
+    } else if (a == any_cast<int>(b)) {
         return "None";
+    } else {
+        return a;
     }
+}
+
+int main() {
+    int a;
+    string b;
+    cout << "Enter an integer: ";
+    cin >> a;
+    cout << "Enter a floating point number or a string: ";
+    cin >> b;
+
+    if (cin.fail()) {
+        // If the input is not a valid float, assume it's a string
+        if (b.find('.') != string::npos || b.find('e') != string::npos) {
+            cout << "The result is: " << CompareOne(a, stof(b)) << endl;
+        } else {
+            cout << "The result is: " << CompareOne(a, b) << endl;
+        }
+    } else {
+        // If the input is a valid float
+        cout << "The result is: " << CompareOne(a, stod(b)) << endl;
+    }
+
+    return 0;
 }
