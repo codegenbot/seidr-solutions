9df618506039a5b0170aa6d2c4bb12c2a3581d77
beep boop <githubbot@vadim.me>
2024-07-10 20:58:13 +0200

    Solution 46 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nib5ef5b Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f59985bf-45e7-43e4-af8a-aa6542a42 97c.py", line 31, in <module>     check(parse_nested_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f59985bf-45e7-43e4-af8a-aa6542a42 97c.py", line 28, in check     assert parse_nested_parens("(()(())((())))") == [4]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f59985bf-45e7-43e4-af8a-aa6542a42 97c.py", line 26, in parse_nested_parens     return [int(x) for x in current_group.replace('(','-').replace(')', '').spli t(',')] + result            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f59985bf-45e7-43e4-af8a-aa6542a42 97c.py", line 26, in <listcomp>     return [int(x) for x in current_group.replace('(','-').replace(')', '').spli t(',')] + result             ^^^^^^ ValueError: invalid literal for int() with base 10: '-------'

--- Python_6.py
+++ Python_6.py
@@ -14,11 +14,13 @@ def parse_nested_parens(paren_string: str) -> list:
             if max_level < 0:
                 result.append(-max_level)
                 max_level = 0
+                if not current_group:
+                    continue
                 while current_group and current_group[0] == '(':
-                    group_value = int(current_group[1:-1].replace('-', '')) 
-                    result.append(group_value) 
+                    group_value = int(current_group[1:-1])
+                    result.append(group_value)
                     current_group = current_group[1:-1]
         elif char.isdigit():
             current_group += char
 
-    return [int(x) for x in current_group.replace('(','-').replace(')', '').split(',')] + result
+    return [int(x) for x in current_group.replace('(','-').replace(')', '').split(',')] if current_group else []
