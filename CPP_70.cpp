f65d6fb7108a40dad1765ff03d28361030d10d8e
beep boop <githubbot@vadim.me>
2024-07-10 06:54:10 +0200

    Solution 20 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tn1r4gui Prompt: In file included from 1a49d028-fdb5-4c13-9dbe-b26677e5189a.cpp:24: 1a49d028-fdb5-4c13-9dbe-b26677e5189a.cpp: In function ‘int main()’: 1a49d028-fdb5-4c13-9dbe-b26677e5189a.cpp:34:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    34 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -4,22 +4,19 @@
 
 using namespace std;
 
-int main() {
-    vector<int> input;
-    int n;
-    cin >> n;
-    
-    for(int i=0; i<n; i++) {
-        int val;
-        cin >> val;
-        input.push_back(val);
+vector<int> strange_sort_vector(vector<int> lst) {
+    vector<int> result;
+    while (!lst.empty()) {
+        int min_val = *min_element(lst.begin(), lst.end());
+        result.push_back(min_val);
+        lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
+        
+        if (!lst.empty()) {
+            int max_val = *max_element(lst.begin(), lst.end());
+            result.push_back(max_val);
+            lst.erase(remove(lst.begin(), lst.end(), max_val), lst.end());
+        }
     }
     
-    vector<int> output = strange_sort_vector(input);
-    
-    for (int num : output) {
-        cout << num << " ";
-    }
-    
-    return 0;
+    return result;
 }
