cf54ced73a433f1efc58465f786d81adb3660ae7
beep boop <githubbot@vadim.me>
2024-07-10 08:12:07 +0200

    Solution 2 of Python/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/t1ps5exv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d8ad2fc1-6681-4769-9065-5b4077f13 6ce.py", line 1     return a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_157.py
+++ Python_157.py
@@ -1,8 +1,2 @@
-def is_pythagorean_triplet(a, b, c):
+def is_pythagorean_triple(a, b, c):
     return a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2
-
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-c = int(input("Enter third number: "))
-
-print(is_pythagorean_triplet(a, b, c))
