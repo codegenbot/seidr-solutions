2f5b69cd59876fb718a13f0769e79680a67fee1c
beep boop <githubbot@vadim.me>
2024-07-10 16:38:25 +0200

    Solution 4 of Python/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/4eawga2p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/36dd200c-73f1-4531-8bbb-eb21713de 89e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_99.py
+++ Python_99.py
@@ -1,3 +1,9 @@
-def find_average(lst):
-    average = sum(lst) / len(lst)
-    return average
+def closest_integer():
+    value = float(input("Enter a number: "))
+    num = value
+    if num % 1 < 0.5:
+        return int(num)
+    else:
+        return int(round(num))
+
+print(closest_integer())
