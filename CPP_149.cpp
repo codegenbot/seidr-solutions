ab62222e00c482a6557825a869aaed2ee064eb2d
beep boop <githubbot@vadim.me>
2024-07-10 22:08:50 +0200

    Solution 72 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ptpr8f4c Prompt: In file included from aa2c21c7-0642-4bcb-8837-6ee87bf4733c.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ aa2c21c7-0642-4bcb-8837-6ee87bf4733c.cpp:41:44: error: ‘b’ was not declared in t his scope    41 | bool issame(vector<string> a,vector<string>b){       |                                            ^ aa2c21c7-0642-4bcb-8837-6ee87bf4733c.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ aa2c21c7-0642-4bcb-8837-6ee87bf4733c.cpp:36:5: note: ‘int main()’ previously def ined here    36 | int main() {       |     ^~~~ In file included from aa2c21c7-0642-4bcb-8837-6ee87bf4733c.cpp:40: aa2c21c7-0642-4bcb-8837-6ee87bf4733c.cpp: In function ‘int main()’: aa2c21c7-0642-4bcb-8837-6ee87bf4733c.cpp:50:20: error: ‘sorted_list_sum’ was not  declared in this scope; did you mean ‘sortListSum’?    50 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,3 +1,4 @@
+```
 #include <string>
 #include <vector>
 
@@ -34,6 +35,7 @@ std::vector<int> sortListSum(std::vector<std::pair<std::string, std::string>> v)
 }
 
 int main() {
-    assert(issame({{"aaaa", "bbbb"}, {"cccc", "ddd"}}, {{"cccc", "ddd"}, {"aaaa", "bbbb"}}));
-    assert(issame(std::vector<std::string>{{"aaaa", "bbbb"}, {"dd", "cc"}},
-                  std::vector<std::string>{{"cc", "dd"}, {"aaaa", "bbbb"}}));
+    std::vector<std::string> a = {"aaaa", "bbbb"};
+    std::vector<std::string> b = {"cccc", "ddd"};
+    assert(issame(a, b));
+}
