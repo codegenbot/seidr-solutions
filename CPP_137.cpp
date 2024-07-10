22acbef74760bd10bd728b82fca3b0a2d304c522
beep boop <githubbot@vadim.me>
2024-07-10 11:07:01 +0200

    Solution 94 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/51eh1c3h Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,21 +1,16 @@
-#include<stdio.h>
-#include<string>
-#include<algorithm>
-#include<boost/any.hpp>
-using namespace std;
+```cpp
+#include <boost/lexical_cast.hpp>
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return (int)a > (float)b ? a : boost::any(b);
-    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        return ((float)a > stof(any_cast<string>(b))) ? a : boost::any(b);
-    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        return stod(any_cast<string>(b)) > (int)a ? boost::any(b) : boost::any(a);
-    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return (float)a > (int)b ? a : boost::any(b);
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return any_cast<string>(a) > any_cast<string>(b) ? a : boost::any(b);
-    }
-    
-    return boost::any("None");
+    if (a.type() == typeid(int) && b.type() == typeid(int))
+        return (boost::any_cast<int>(a) > boost::any_cast<int>(b)) ? a : (boost::any_cast<int>(a) < boost::any_cast<int>(b)) ? b : boost::any("None");
+    if ((a.type() == typeid(float) || a.type() == typeid(double)) && (b.type() == typeid(float) || b.type() == typeid(double)))
+        return (boost::any_cast<float>(a) > boost::any_cast<float>(b)) ? a : (boost::any_cast<float>(a) < boost::any_cast<float>(b)) ? b : boost::any("None");
+    if ((a.type() == typeid(string) || a.type() == typeid(wstring)) && (b.type() == typeid(string) || b.type() == typeid(wstring)))
+        return (boost::lexical_cast<string>(boost::any_cast<any>(a)).compare(boost::lexical_cast<string>(boost::any_cast<any>(b))) > 0) ? a : ((boost::lexical_cast<string>(boost::any_cast<any>(a))).compare(boost::lexical_cast<string>(boost::any_cast<any>(b))) < 0) ? b : boost::any("None");
+    if (a.type() == typeid(string) && b.type() != typeid(string))
+        return (boost::lexical_cast<string>(boost::any_cast<any>(a)).compare(boost::lexical_cast<string>(boost::any_cast<int>(b))) > 0) ? a : ((boost::lexical_cast<string>(boost::any_cast<any>(a))).compare(boost::lexical_cast<string>(boost::any_cast<int>(b))) < 0) ? b : boost::any("None");
+    if (a.type() != typeid(string) && b.type() == typeid(string))
+        return (boost::lexical_cast<string>(boost::any_cast<any>(b)).compare(boost::lexical_cast<string>(boost::any_cast<int>(a))) > 0) ? b : ((boost::lexical_cast<string>(boost::any_cast<any>(b))).compare(boost::lexical_cast<string>(boost::any_cast<int>(a))) < 0) ? a : boost::any("None");
+    return "None";
 }
