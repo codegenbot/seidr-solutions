64bbccd251f63453347d73789d9fc6eca3863d20
beep boop <githubbot@vadim.me>
2024-07-10 23:51:26 +0200

    Solution 93 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80c5413a-a3f3-4c88-81b9-0ff8276d8 37b.py", line 13, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80c5413a-a3f3-4c88-81b9-0ff8276d8 37b.py", line 8, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80c5413a-a3f3-4c88-81b9-0ff8276d8 37b.py", line 6, in sort_third     return [num for _ in range(numbers.count(num)) for num in numbers] + sorted( others)                                              ^^^ NameError: name 'num' is not defined. Did you mean: 'sum'?

--- Python_33.py
+++ Python_33.py
@@ -1,8 +1,5 @@
+```
 def sort_third(lst):
-    numbers = sorted([x for x in lst if isinstance(x, (int, float))])
-    others = [
-        item
-        for sublist in [i for i in lst if isinstance(i, list)]
-        for item in sorted(sublist)
-    ]
-    return numbers + others
+    numbers = sorted({x for x in lst if isinstance(x, (int, float))})
+    others = [item for sublist in [i for i in lst if isinstance(i, list)] for item in sublist]
+    return sorted(others, key=len) + numbers
