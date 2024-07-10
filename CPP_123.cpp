d228708c221eedfd38902a575f0ed7f16ee876f7
beep boop <githubbot@vadim.me>
2024-07-10 18:35:47 +0200

    Solution 30 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/e4xegzqk Prompt:       |                    ^~~ 14bb666b-739a-4c06-92cf-be71a2539263.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 14bb666b-739a-4c06-92cf-be71a2539263.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 14bb666b-739a-4c06-92cf-be71a2539263.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 14bb666b-739a-4c06-92cf-be71a2539263.cpp:12: 14bb666b-739a-4c06-92cf-be71a2539263.cpp: In function ‘int main()’: 14bb666b-739a-4c06-92cf-be71a2539263.cpp:22:20: error: ‘get_odd_collatz’ was not  declared in this scope    22 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 14bb666b-739a-4c06-92cf-be71a2539263.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,16 +1,9 @@
 #include <vector>
-#include <algorithm>
-
-using namespace std;
-
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
 
 vector<int> get_odd_collatz(int n) {
     vector<int> result;
     while (n != 1) {
-        if (n % 2 == 1) {
+        if (n % 2 != 0) {
             result.push_back(n);
         }
         n = (n % 2 == 0) ? n / 2 : 3 * n + 1;
@@ -20,8 +13,10 @@ vector<int> get_odd_collatz(int n) {
     return result;
 }
 
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
 int main() {
-    assert (issame(get_odd_collatz(1) , {1}));
-    
-    return 0;
+    assert(issame(get_odd_collatz(1), {1}));
 }
