dbf4cd185e5f5165a44261252e62ff99729ef088
beep boop <githubbot@vadim.me>
2024-07-10 23:45:25 +0200

    Solution 92 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/frboqn66 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b35503bb-961f-456a-a30e-39ef68110 d78.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_26.py
+++ Python_26.py
@@ -1,7 +1,10 @@
-```from typing import List
+```
+from typing import List
+
 
 def remove_duplicates(numbers: List[int]) -> List[int]:
     return list(dict.fromkeys(numbers))
+
 
 if __name__ == "__main__":
     while True:
@@ -10,10 +13,6 @@ if __name__ == "__main__":
         if not numbers or numbers == ",":
             print("No input entered.")
         else:
-            if not numbers:  
-                print("No input entered.")
-                continue
-
             try:
                 numbers = [int(num) for num in numbers.split(",") if num]
                 print(remove_duplicates(numbers))
