c95927129f99192eb16e217b8456294ba6d8064f
beep boop <githubbot@vadim.me>
2024-07-10 02:06:57 +0200

    Solution 38 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9939gev7 Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8fbdaa97-2144-48eb-b82b-b91d98d4e5b3.cpp:44:36: error: expected primary-expressi on before ‘float’    44 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 8fbdaa97-2144-48eb-b82b-b91d98d4e5b3.cpp:44:43: error: expression list treated a s compound expression in initializer [-fpermissive]    44 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 8fbdaa97-2144-48eb-b82b-b91d98d4e5b3.cpp:43: 8fbdaa97-2144-48eb-b82b-b91d98d4e5b3.cpp: In function ‘int main()’: 8fbdaa97-2144-48eb-b82b-b91d98d4e5b3.cpp:53:20: error: ‘sort_even’ was not decla red in this scope; did you mean ‘sortEven’?    53 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 8fbdaa97-2144-48eb-b82b-b91d98d4e5b3.cpp:53:19: error: ‘issame’ cannot be used a s a function    53 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -34,8 +34,6 @@ int main_function()
     for(float f : output) {
         std::cout << f << " ";
     }
-    if (!isSame({5, 8, -12, 4, 23, 2, 3, 11, 12, -10},{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10})) {
-        std::cout << "Test failed";
-    }
+    assert(isSame(output, {1,-12,8,3,4,5,2,12,11,23,-10}));
     return 0;
 }
