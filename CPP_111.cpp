6749cdf41e598db40bfcb5e76d9d8bc43e07269c
beep boop <githubbot@vadim.me>
2024-07-10 23:27:55 +0200

    Solution 66 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rmdf06ap Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_map.h:62,                  from /usr/include/c++/11/map:61,                  from bb7a039c-65eb-4e4d-9b81-867c99b9e390.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,5 +1,23 @@
+```
 #include <map>
 #include <string>
+
+int main() {
+    std::string s1 = "aa";
+    std::string s2 = "a";
+    auto hist1 = histogram(s1);
+    auto hist2 = histogram(s2);
+
+    bool sameMap = issameMap(hist1, hist2);
+
+    if (sameMap) {
+        std::cout << "Histograms are the same.\n";
+    } else {
+        std::cout << "Histograms are different.\n";
+    }
+    
+    return 0;
+}
 
 std::map<char, int> histogram(std::string s) {
     std::map<char, int> freqMap;
@@ -13,7 +31,7 @@ std::map<char, int> histogram(std::string s) {
     return freqMap;
 }
 
-bool std::issameMap(std::map<char, int> a, std::map<char, int> b) {
+bool issameMap(std::map<char, int> a, std::map<char, int> b) {
     if (a.size() != b.size())
         return false;
 
@@ -22,9 +40,4 @@ bool std::issameMap(std::map<char, int> a, std::map<char, int> b) {
             return false;
 
     return true;
-}
-
-int main() {
-    assert(issameMap(histogram("aa"), std::map<char, int>{{'a', 1}}));
-    return 0;
 }
