4dd312a66dc443c92240e5fdbd9f15ad2847c296
beep boop <githubbot@vadim.me>
2024-07-10 22:56:20 +0200

    Solution 51 of CPP/121, ratio 0.8571428571428571 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pvc16fn9 Prompt: 370c689c-a3cd-416a-9ff9-6ba430b043af.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 370c689c-a3cd-416a-9ff9-6ba430b043af.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 370c689c-a3cd-416a-9ff9-6ba430b043af.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ 370c689c-a3cd-416a-9ff9-6ba430b043af.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 370c689c-a3cd-416a-9ff9-6ba430b043af.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 370c689c-a3cd-416a-9ff9-6ba430b043af.cpp:21: 370c689c-a3cd-416a-9ff9-6ba430b043af.cpp: In function ‘int main()’: 370c689c-a3cd-416a-9ff9-6ba430b043af.cpp:23:13: error: ‘solutions’ was not decla red in this scope; did you mean ‘solution’?    23 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,7 +1,7 @@
 #include <initializer_list>
 #include <cassert>
 
-int solution(std::initializer_list<int> lst) {
+int solutions(std::initializer_list<int> lst) {
     int sum = 0;
     for (auto i : lst) {
         if (i % 2 != 0) {
