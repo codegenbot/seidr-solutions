2093fc2a53f447f75d861d97720771e9627519a3
beep boop <githubbot@vadim.me>
2024-07-10 21:17:53 +0200

    Solution 25 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3mr73mpr Prompt: b5812e5f-fb63-467d-b71c-98a18a9c1532.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,30 +1,23 @@
+#include <iostream>
 #include <string>
-#include <typeindex>
-#include <any>
 
-std::any compare_one(std::any a, std::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return (int) a.convert_to<int>() < (float) b.convert_to<float>() ? b : a;
+using namespace std;
+
+any compare_one(any a, any b) {
+    if (any_cast<int>(a) < any_cast<float>(b)) {
+        return b;
     }
-    else if (a.type() == typeid(float) && b.type() == typeid(std::string)) {
-        std::string str = b.convert_to<std::string>();
-        float f = (float) a.convert_to<float>();
-        return f > atof(str.c_str()) ? a : b;
+    else if (any_cast<float>(a) > atof(any_cast<string>(b).c_str())) {
+        return a;
     }
-    else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        std::string str1 = a.convert_to<std::string>();
-        std::string str2 = b.convert_to<std::string>();
-        return str1 > str2 ? a : b;
+    else if (any_cast<string>(a) > any_cast<string>(b)) {
+        return a;
     }
-    else if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int i1 = (int) a.convert_to<int>();
-        int i2 = (int) b.convert_to<int>();
-        return i1 < i2 ? b : a;
+    else if (any_cast<int>(a) < any_cast<int>(b)) {
+        return b;
     }
-    else if (a.type() == typeid(std::string) && b.type() == typeid(float)) {
-        std::string str = a.convert_to<std::string>();
-        float f = (float) b.convert_to<float>();
-        return atof(str.c_str()) > f ? a : b;
+    else if (atof(any_cast<string>(a).c_str()) > any_cast<float>(b)) {
+        return a;
     }
     else {
         return "None";
