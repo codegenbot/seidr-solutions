9783f95c0049a30c6a024808e52da87022412c71
beep boop <githubbot@vadim.me>
2024-07-10 19:30:40 +0200

    Solution 12 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ie2x67wo Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc8a24b5-cc45-4f6e-ba42-d3cefd269 c1f.py", line 17, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc8a24b5-cc45-4f6e-ba42-d3cefd269 c1f.py", line 14, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,13 +1,11 @@
 ```
 def fizz_buzz(n: int):
-    result = []
+    result = ["" for _ in range(n)]
     for i in range(1, n+1):
         if i % 3 == 0 and i % 5 == 0:
-            result.append("FizzBuzz")
+            result[i-1] = "FizzBuzz"
         elif i % 3 == 0:
-            result.append("Fizz")
+            result[i-1] = "Fizz"
         elif i % 5 == 0:
-            result.append("Buzz")
-        else:
-            result.append(str(i))
-    return "\n".join(map(str, result))
+            result[i-1] = "Buzz"
+    return str(result.count("FizzBuzz") + result.count("Fizz")*3 + result.count("Buzz")*5 - 15)
