204202d8ef89a9a7f01e06a080f260c22f4ab5d6
beep boop <githubbot@vadim.me>
2024-07-10 22:31:08 +0200

    Solution 69 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/idv3uybh Prompt:    25 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 65b892b6-c098-4727-a582-880f0b5d8031.cpp:25:44: error: ‘b’ was not declared in t his scope    25 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 65b892b6-c098-4727-a582-880f0b5d8031.cpp:25:45: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 65b892b6-c098-4727-a582-880f0b5d8031.cpp:24: 65b892b6-c098-4727-a582-880f0b5d8031.cpp: In function ‘int main()’: 65b892b6-c098-4727-a582-880f0b5d8031.cpp:34:20: error: ‘sorted_list_sum’ was not  declared in this scope    34 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 65b892b6-c098-4727-a582-880f0b5d8031.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -8,5 +8,27 @@ bool issame(vector<string> a, vector<string> b) {
 
 int main() {
     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cccc", "dddd", "aaaaa", "bbbbb"}));
-    // Rest of your code
+    
+    return 0;
+}
+
+vector<string> sorted_list_sum(vector<string> lst) {
+    vector<string> result;
+
+    for (const auto& str : lst) {
+        if (str.length() % 2 == 0) {
+            result.push_back(str);
+        }
+    }
+
+    sort(result.begin(), result.end(),
+         [](const string& a, const string& b) {
+             if (a.length() == b.length()) {
+                 return a < b;
+             } else {
+                 return a.length() < b.length();
+             }
+         });
+
+    return result;
 }
