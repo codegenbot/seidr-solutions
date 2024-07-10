a0c53fb271fb57c791d8898485240c7f6e2c0479
beep boop <githubbot@vadim.me>
2024-07-10 23:20:23 +0200

    Solution 62 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rmdf06ap Prompt:       |                                 ^~~~ f4499f80-06e9-4f1b-8ef3-4e7710098a6a.cpp:33:38: error: expected primary-expressi on before ‘int’    33 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ f4499f80-06e9-4f1b-8ef3-4e7710098a6a.cpp:33:44: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ f4499f80-06e9-4f1b-8ef3-4e7710098a6a.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ f4499f80-06e9-4f1b-8ef3-4e7710098a6a.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from f4499f80-06e9-4f1b-8ef3-4e7710098a6a.cpp:32: f4499f80-06e9-4f1b-8ef3-4e7710098a6a.cpp: In function ‘int main()’: f4499f80-06e9-4f1b-8ef3-4e7710098a6a.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -14,18 +14,18 @@ std::map<char, int> histogram(std::string s) {
     return freqMap;
 }
 
-bool issameMap(std::map<char, int> a, std::map<char, int> b) {
+bool issame(std::map<char, int> a, std::map<char, int> b) {
     if (a.size() != b.size())
         return false;
 
     for (auto it = a.begin(); it != a.end();++it)
-        if (it->second != b.find(it->first)->second)
+        if (it->second != b.at(it->first))
             return false;
 
     return true;
 }
 
 int main() {
-    assert(issameMap(histogram("aa"), histogram("a")));
+    assert(issame(histogram("aa"), std::map<char, int>{{'a', 1}}));
     return 0;
 }
