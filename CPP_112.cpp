37d797eee03e09334a93dbeffc648b770dd665d2
beep boop <githubbot@vadim.me>
2024-07-10 04:25:53 +0200

    Solution 33 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9mcoota0 Prompt:       |                                            ^ 87531fea-cec3-4e17-91f6-a955f015c695.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 87531fea-cec3-4e17-91f6-a955f015c695.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ 87531fea-cec3-4e17-91f6-a955f015c695.cpp:4:5: note: ‘int main()’ previously defi ned here     4 | int main() {       |     ^~~~ In file included from 87531fea-cec3-4e17-91f6-a955f015c695.cpp:16: 87531fea-cec3-4e17-91f6-a955f015c695.cpp: In function ‘int main()’: 87531fea-cec3-4e17-91f6-a955f015c695.cpp:26:20: error: ‘reverse_delete’ was not declared in this scope; did you mean ‘timer_delete’?    26 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 87531fea-cec3-4e17-91f6-a955f015c695.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,13 +1,25 @@
+```cpp
 #include <iostream>
 using namespace std;
 
-bool issame(vector<string> a,vector<string>b){
+bool issame(vector<string> a, vector<string> b) {
     if(a.size() != b.size())
         return false;
-    for(int i = 0; i < a.size(); i++)
+    for(int i = 0; i < a.size(); i++) {
         if(a[i] != b[i])
             return false;
+    }
     return true;
+}
+
+string reverse_delete(string s1, string s2) {
+    int len1 = s1.length();
+    int len2 = s2.length();
+    for(int i = 0; i < len2; i++) {
+        if(s1.find(s2.substr(i, 1)) != string::npos)
+            s1.erase(s1.find(s2.substr(i, 1)), 1);
+    }
+    return s1;
 }
 
 int main() {
@@ -20,4 +32,6 @@ int main() {
         cout << "First number is greater." << endl;
     else
         cout << "Second number is greater." << endl;
+
+    assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
 }
