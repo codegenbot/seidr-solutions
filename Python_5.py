c2c4679528105a3e21181d377d5baf5584b9990b
beep boop <githubbot@vadim.me>
2024-07-10 22:01:20 +0200

    Solution 66 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pc4qhgax Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ca4500d4-8fd3-48db-9f0f-9626f0105 dec.py", line 12, in <module>     check(intersperse)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ca4500d4-8fd3-48db-9f0f-9626f0105 dec.py", line 9, in check     assert intersperse([2, 2, 2], 2) == [2, 2, 2, 2, 2]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_5.py
+++ Python_5.py
@@ -1,15 +1,13 @@
 ```
 def intersperse(a, b):
     result = []
+    a_len = len(a)
+    
     for i in range(len(a) + (1 if isinstance(b, int) else 0)):
-        if i < len(a): 
+        if i < a_len: 
             result.append(a[i])
-        if isinstance(b, int) and i < len(a): 
-            result.append(a[i])
-        elif isinstance(b, str):
+        elif isinstance(b, int) and i % (a_len + 1) != 0:
             result.append(b)
-        else:
-            result.append(b)
-    while b is not None:  
-        result.append(b)
+        elif isinstance(b, iter):
+            if i > 0: result.append(b.__next__())
     return result
