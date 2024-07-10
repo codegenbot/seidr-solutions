d73da596db7c2b2a0e95bb0bedbbe0852e59e6b9
beep boop <githubbot@vadim.me>
2024-07-10 03:01:26 +0200

    Solution 53 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0hw5ynmd Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 71c48967-ae1f-46a6-b7a7-6b7f64f60853.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,29 +1,3 @@
-```cpp
-#include <vector>
-#include <set>
-#include <algorithm>
-
-bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b) {
-    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin(),
-                                                  [](const auto& v1, const auto& v2) {
-                                                      return v1 == v2;
-                                                  });
-}
-
-std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
-    std::set<int> s1(l1.begin(), l1.end());
-    std::set<int> s2(l2.begin(), l2.end());
-
-    std::vector<int> result;
-    for (int i : s1) {
-        if (s2.find(i) != s2.end()) {
-            result.push_back(i);
-        }
-    }
-
-    return result;
-}
-
 int main() {
     int n, m;
     cin >> n >> m;
@@ -45,7 +19,8 @@ int main() {
     }
 
     for (int i = 0; i < n; i++) {
-        std::vector<int> result = common({l1[i][0], l1[i][1], l1[i][2], l1[i][3]}, {l2[0][0], l2[0][1], l2[0][2], l2[0][3]});
+        std::vector<int> result = common({l1[i][0], l1[i][1], l1[i][2], l1[i][3]},
+                                          {l2[0][0], l2[0][1], l2[0][2], l2[0][3]});
         for (int j : result) {
             cout << j;
             if (j < result.size() - 1) {
