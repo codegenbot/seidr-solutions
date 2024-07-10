87843a7b80e1501c2533900cfa2ec934fe9612fe
beep boop <githubbot@vadim.me>
2024-07-11 01:01:18 +0200

    Solution 8 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8kvpmmn6 Prompt:       |                    ^~~ c3a19786-8c75-42ab-91e7-6a6adbff6bed.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c3a19786-8c75-42ab-91e7-6a6adbff6bed.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c3a19786-8c75-42ab-91e7-6a6adbff6bed.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c3a19786-8c75-42ab-91e7-6a6adbff6bed.cpp:15: c3a19786-8c75-42ab-91e7-6a6adbff6bed.cpp: In function ‘int main()’: c3a19786-8c75-42ab-91e7-6a6adbff6bed.cpp:25:20: error: ‘compare’ was not declare d in this scope    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ c3a19786-8c75-42ab-91e7-6a6adbff6bed.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,26 +1,4 @@
-bool issame(vector<int> a,vector<int>b){
-    return (a==b);
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> game(n), guess(n);
-    for(int i=0; i<n; i++){
-        cin >> game[i] >> guess[i];
-    }
-    bool same = issame(game,guess);
-    if(same){
-        cout << "Game and Guess are the same.\n";
-    }else{
-        vector<int> compare_game_guess = compare(game,guess);
-        for(int num : compare_game_guess) {
-            cout << num << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
-}
+#include <vector>
 
 vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
