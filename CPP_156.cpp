b6c0bbe4372708b6725f17ecda1884d6f90c6abd
beep boop <githubbot@vadim.me>
2024-07-10 09:02:41 +0200

    Solution 90 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gvtcxb9h Prompt: 0600c2c9-7950-4cf2-9e59-1935bfd2a4a4.cpp:4:1: error: ‘string’ does not name a ty pe     4 | string int_to_mini_roman(int number) {       | ^~~~~~ In file included from 0600c2c9-7950-4cf2-9e59-1935bfd2a4a4.cpp:23: 0600c2c9-7950-4cf2-9e59-1935bfd2a4a4.cpp: In function ‘int main()’: 0600c2c9-7950-4cf2-9e59-1935bfd2a4a4.cpp:25:13: error: ‘int_to_mini_romank’ was not declared in this scope    25 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,27 +1,21 @@
-```cpp
-#include <iostream>
+#include <map>
+#include <string>
 using namespace std;
 
 string int_to_mini_roman(int number) {
-    vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-                                         {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-                                         {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
-
+    map<int, string> roman = {{1, "i"}, {4, "iv"}, {5, "v"}, {9, "ix"},
+                                {10, "x"}, {40, "xl"}, {50, "l"},
+                                {90, "xc"}, {100, "c"}, {400, "cd"},
+                                {500, "d"}, {900, "cm"}, {1000, "m"}};
     string result = "";
-    for (const auto& rom : roman) {
-        while (number >= rom.first) {
-            number -= rom.first;
-            result += rom.second;
+    for (int i = 1000; i >= 1; i /= 10) {
+        while (number >= i) {
+            number -= i;
+            result += roman[i];
+        }
+        if (number < i && i != 1) {
+            result += roman[i / 10] + roman[i % 10];
+            break;
         }
     }
-
     return result;
-}
-
-int main() {
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    cout << "Roman numeral representation: " << int_to_mini_roman(num) << endl;
-    return 0;
-}
