5b048afcf9a50c65f61fc0ef848ace18b0d52a7c
beep boop <githubbot@vadim.me>
2024-07-10 08:41:21 +0200

    Solution 68 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/jtwmj2dd Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,37 +1,44 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> nums) {
+pair<vector<int>, vector<int>> cutVector(vector<int> v) {
     int min_diff = INT_MAX;
-    int index = 0;
-    for (int i = 1; i < nums.size(); i++) {
-        int diff = abs(nums[i] - nums[i-1]);
+    int pos = -1;
+    
+    for (int i = 0; i < v.size() - 1; i++) {
+        int diff = abs(v[i] - v[i + 1]);
         if (diff <= min_diff) {
             min_diff = diff;
-            index = i;
+            pos = i;
         }
     }
-    vector<int> left(nums.begin(), nums.begin() + index);
-    vector<int> right(nums.begin() + index, nums.end());
+    
+    vector<int> left, right;
+    for (int i = 0; i < pos; i++) {
+        left.push_back(v[i]);
+    }
+    for (int i = pos + 1; i < v.size(); i++) {
+        right.push_back(v[i]);
+    }
+    
     return {left, right};
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
-    pair<vector<int>, vector<int>> result = cutVector(nums);
+    vector<int> v(n);
+    for (auto &x : v) cin >> x;
+
+    auto res = cutVector(v);
+
     cout << "Left: ";
-    for (int num : result.first) {
-        cout << num << " ";
-    }
+    for (int x : res.first) cout << x << " ";
     cout << endl;
+    
     cout << "Right: ";
-    for (int num : result.second) {
-        cout << num << " ";
-    }
+    for (int x : res.second) cout << x << " ";
+    cout << endl;
+    
     return 0;
 }
