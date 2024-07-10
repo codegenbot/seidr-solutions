e8f5decdb0bc5064b037f9bb1308aa32762ffcc8
beep boop <githubbot@vadim.me>
2024-07-10 05:03:09 +0200

    Solution 60 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9mcoota0 Prompt: ng<char>*; _ForwardIterator = std::__cxx11::basic_string<char>*; _Tp = std::__cx x11::basic_string<char>]’ /usr/include/c++/11/bits/stl_vector.h:1585:33:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const std::__cxx11::basic_string<char >*; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11 ::basic_string<char> >]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = s td::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11::basic_str ing<char> >; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<std::__cx x11::basic_string<char> >]’ e15ab4c4-6c3a-4f7f-b9ff-efedc173a96d.cpp:53:18:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <iostream>
 #include <vector>
 #include <string>
@@ -49,5 +50,5 @@ int main() {
     else
         cout << "Second number is greater." << endl;
 
-    assert(issame(vector<char>(reverse_delete("mamma", "mia").begin(), reverse_delete("mamma", "mia").end()), vector<char>()));
+    assert(issame(vector<string>(reverse_delete("mamma", "mia").c_str(), reverse_delete("mamma", "mia").c_str()), {"", "True"}));
 }
