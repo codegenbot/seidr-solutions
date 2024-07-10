8c900f605a2708aeef42374cd6071dee5a346b59
beep boop <githubbot@vadim.me>
2024-07-10 10:55:33 +0200

    Solution 65 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/rvrw4kay Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,37 +1,18 @@
-#include <vector>
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-bool solveBoolean(string s) {
-    if(s.size() == 0)
-        return false;
-    
-    string temp = "";
-    for(int i = 0; i < s.size(); i++) {
-        if(s[i] == '|') {
-            bool left = (temp == "t") ? true : false;
-            bool right = (s.substr(i+1, s.size()-i-1) == "t") ? true : false;
-            return left || right;
-        } else if(s[i] == '&') {
-            bool left = (temp == "t") ? true : false;
-            bool right = (s.substr(i+1, s.size()-i-1) == "t") ? true : false;
-            return left && right;
+bool solveBoolean(string expression) {
+    stack<char> s;
+    for (int i = 0; i < expression.length(); i++) {
+        if (expression[i] == '&') {
+            while (!s.empty() && s.top() == '&') {
+                s.pop();
+            }
+        } else if (expression[i] == '|') {
+            while (!s.empty()) {
+                s.pop();
+            }
+            s.push(expression[i]);
+        } else {
+            s.push(expression[i]);
         }
-        temp += s[i];
     }
-    return temp == "t";
-}
-
-int main() {
-    string s;
-    cout << "Enter a Boolean expression: ";
-    cin >> s;
-    bool result = solveBoolean(s);
-    if(result)
-        cout << "True\n";
-    else
-        cout << "False\n";
-    return 0;
+    return s.top() == 'T';
 }
