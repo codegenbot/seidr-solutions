2904ccf4696b913f75bcc05d0b3d4347c4defcc7
beep boop <githubbot@vadim.me>
2024-07-10 10:10:38 +0200

    Solution 96 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9n2fueyj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a0425624-c5b5-4e5b-bd1e-9bfbe4d01 656.py", line 1, in <module>     music_data = music_input.split() if music_input and all(':' in note for note  in music_input.split()) else print("Invalid input format.")                                         ^^^^^^^^^^^ NameError: name 'music_input' is not defined

--- Python_17.py
+++ Python_17.py
@@ -1,2 +1,6 @@
 music_input = input("Enter music data: ")
-music_data = music_input.split() if music_input and all(':' in note for note in music_input.split()) else print("Invalid input format.")
+music_data = (
+    music_input.split()
+    if music_input and all(":" in note for note in music_input.split())
+    else print("Invalid input format.")
+)
