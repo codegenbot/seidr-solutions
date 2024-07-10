0bd12579b22c7ebb6c61885eec103374ca2e6a56
beep boop <githubbot@vadim.me>
2024-07-10 06:12:27 +0200

    Solution 42 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/z5aml1ah Prompt:  ‘std’    21 | < "The mini Roman numeral is: " << int_to_mini_roman(num) << std::endl;       |                                                                   ^~~~ 0493cdf7-6637-4c2a-9d5d-475b883f98f2.cpp:1:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?   +++ |+#include <ostream>     1 | ```cpp 0493cdf7-6637-4c2a-9d5d-475b883f98f2.cpp: At global scope: 0493cdf7-6637-4c2a-9d5d-475b883f98f2.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ 0493cdf7-6637-4c2a-9d5d-475b883f98f2.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 0493cdf7-6637-4c2a-9d5d-475b883f98f2.cpp:24: 0493cdf7-6637-4c2a-9d5d-475b883f98f2.cpp: In function ‘int main()’: 0493cdf7-6637-4c2a-9d5d-475b883f98f2.cpp:26:13: error: ‘int_to_mini_romank’ was not declared in this scope    26 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <string>
 
@@ -6,7 +5,7 @@ std::pair<int, std::string> romanMap[] = {{1000, "M"}, {900, "CM"}, {500, "D"},
     {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"},
     {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
 
-std::string int_to_mini_roman(int number) const {
+std::string int_to_roman(int number) {
     std::string roman = "";
     for (const auto& pair : romanMap) {
         while (number >= pair.first) {
@@ -21,5 +20,5 @@ int main() {
     int num;
     std::cout << "Enter a number: ";
     std::cin >> num;
-    std::cout << "The mini Roman numeral is: " << int_to_mini_roman(num) << std::endl;
+    std::cout << "The mini Roman numeral is: " << int_to_roman(num) << std::endl;
 }
