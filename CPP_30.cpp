0eb8e0ba1418acbb04d937bcf3b9d128cace6caa
beep boop <githubbot@vadim.me>
2024-07-10 22:33:12 +0200

    Solution 16 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/lbbb1zby Prompt:       |                    ^~~~~ d803ac8f-ea91-4e75-8aa8-d7222c6fe341.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ d803ac8f-ea91-4e75-8aa8-d7222c6fe341.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ d803ac8f-ea91-4e75-8aa8-d7222c6fe341.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from d803ac8f-ea91-4e75-8aa8-d7222c6fe341.cpp:11: d803ac8f-ea91-4e75-8aa8-d7222c6fe341.cpp: In function ‘int main()’: d803ac8f-ea91-4e75-8aa8-d7222c6fe341.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ d803ac8f-ea91-4e75-8aa8-d7222c6fe341.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,11 +1,23 @@
 #include <vector>
-#include <cassert>
 
 bool issame(const vector<float>& a, const vector<float>& b){
-    return a == b;
+    if(a.size() != b.size()){
+        return false;
+    }
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
 }
 
-int main(){
-    assert (issame(get_positive({}) , {}));
-    return 0;
+vector<float> get_positive(vector<float> l){
+    vector<float> result;
+    for(float num : l){
+        if(num > 0){
+            result.push_back(num);
+        }
+    }
+    return result;
 }
