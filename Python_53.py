8698652422d427b4c0f965dd3eebe89520c4b237
beep boop <githubbot@vadim.me>
2024-07-10 23:06:37 +0200

    Solution 64 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k05otzbt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d7b760a9-1317-4ecd-95f0-bb4687ec2 6eb.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -6,7 +6,7 @@ while True:
     try:
         x = int(input("Enter first number: "))
         y = int(input("Enter second number: "))
-        print(add(x, y))
+        print(f"The sum of {x} and {y} is {add(x,y)}")
         break
     except ValueError:
         print("Invalid input. Please enter integers.")
