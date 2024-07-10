d5585d1e2704e287ee614a71149988d06b09c88d
beep boop <githubbot@vadim.me>
2024-07-10 19:09:22 +0200

    Solution 20 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uquygx2d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/40b23e7b-8221-42e3-8d0e-c2af2a645 c53.py", line 13, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/40b23e7b-8221-42e3-8d0e-c2af2a645 c53.py", line 8, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/40b23e7b-8221-42e3-8d0e-c2af2a645 c53.py", line 4, in sort_even     return [i for i in zip(even, odd) or (even, [])][0] + [            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: can only concatenate tuple (not "list") to tuple

--- Python_37.py
+++ Python_37.py
@@ -1,4 +1,4 @@
 def sort_even(l: list):
     even = sorted([x for x in l if x % 2 == 0])
-    odd = sorted([x for x in l if x % 2 != 0])
-    return tuple(i for i in zip(even, odd) or (even, [])) + tuple(i for i in (l if len(l) % 2 else l[:-1]) if i % 2 != 0)
+    odd = [i for i in (l if len(l) % 2 else l[:-1]) if i % 2 != 0]
+    return [(i, j) for i, j in zip(even, odd)]
