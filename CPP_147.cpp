2e6cf7a7b02eba2e24190678369c62d670d98260
beep boop <githubbot@vadim.me>
2024-07-10 20:47:31 +0200

    Solution 20 of CPP/147, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/osgzm1tv Prompt:     2 |     for (int i = 1; i <= n; ++i) {       |     ^~~ 0c7823a9-f1ba-4cf8-803c-3ca68f61b70e.cpp:2:21: error: ‘i’ does not name a type     2 |     for (int i = 1; i <= n; ++i) {       |                     ^ 0c7823a9-f1ba-4cf8-803c-3ca68f61b70e.cpp:2:29: error: expected unqualified-id be fore ‘++’ token     2 |     for (int i = 1; i <= n; ++i) {       |                             ^~ 0c7823a9-f1ba-4cf8-803c-3ca68f61b70e.cpp:11:5: error: expected unqualified-id be fore ‘return’    11 |     return count;       |     ^~~~~~ 0c7823a9-f1ba-4cf8-803c-3ca68f61b70e.cpp:12:1: error: expected declaration befor e ‘}’ token    12 | }       | ^ In file included from 0c7823a9-f1ba-4cf8-803c-3ca68f61b70e.cpp:14: 0c7823a9-f1ba-4cf8-803c-3ca68f61b70e.cpp: In function ‘int main()’: 0c7823a9-f1ba-4cf8-803c-3ca68f61b70e.cpp:16:13: error: ‘get_matrix_triples’ was not declared in this scope    16 |     assert (get_matrix_triples(100) == 53361);       |             ^~~~~~~~~~~~~~~~~~

--- CPP_147.cpp
+++ CPP_147.cpp
@@ -1,4 +1,5 @@
-int count = 0;
+int get_matrix_triples(int n) {
+    int count = 0;
     for (int i = 1; i <= n; ++i) {
         for (int j = i + 1; j <= n; ++j) {
             for (int k = j + 1; k <= n; ++k) {
