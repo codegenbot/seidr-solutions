2cdb848e7a35fae328068489a820916a0f217974
beep boop <githubbot@vadim.me>
2024-07-10 04:03:52 +0200

    Solution 36 of CPP/82, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/co2evohj Prompt: 7fa375e9-9d76-42a7-9ea6-c222027e6928.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 7fa375e9-9d76-42a7-9ea6-c222027e6928.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main()       |     ^~~~

--- CPP_82.cpp
+++ CPP_82.cpp
@@ -5,7 +5,7 @@ bool prime_length(const std::string& str) {
     int len = static_cast<int>(str.length());
     if(len <= 1) return false;
     for(int i=2; i*i<=len; i++){
-        //if(len%i==0) return false;
+        if(len%i==0) return false;
     }
     return true;
 
