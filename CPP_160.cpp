a9b10ab120f358fc060bff23c3b92aa1ee85a709
beep boop <githubbot@vadim.me>
2024-07-10 08:27:43 +0200

    Solution 27 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kd2jtp3g Prompt:       |                       ^~~~~~ 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:1:31: error: ‘operator_’ was not declar ed in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                               ^~~~~~~~~ 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:1:42: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                          ^~~~~~ 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:1:49: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                 ^~~ 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:1:61: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                             ^ In file included from 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:50: 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp: In function ‘int main()’: 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:52:23: error: ‘do_algebra’ cannot be us ed as a function    52 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -9,18 +9,17 @@ int do_algebra(vector<string> operator_, vector<int> operand) {
         } else if (operator_[i] == "-") {
             result -= operand[i];
         } else if (operator_[i] == "*") {
-            int temp = 0;
+            int temp = 1;
             for (int j = i; j < operator_.size(); j++) {
                 if (operator_[j] == "*") {
                     temp *= operand[j + 1];
                 } else if (operator_[j] == "/") {
                     temp /= operand[j + 1];
-                } else if (operand_[j] == "**") {
+                } else if (operator_[j] == "**") {
                     temp = pow(temp, operand[j + 1]);
                 }
             }
             result += temp;
-            i = j; 
         } else if (operator_[i] == "/") {
             int temp = operand[i];
             for (int j = i + 1; j < operator_.size(); j++) {
