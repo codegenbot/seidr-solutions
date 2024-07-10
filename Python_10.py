a2c3ac30b057da7439b721b2ab1786e676ec67d7
beep boop <githubbot@vadim.me>
2024-07-10 05:36:56 +0200

    Solution 11 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/kqeo9b5n Prompt: Program did not receive expected input

--- Python_10.py
+++ Python_10.py
@@ -1,12 +1,13 @@
 def reverse_string(string, i):
     return string + string[:i][::-1] if i > 0 else string
 
+
 def make_palindrome(s):
-    s = s.lower().strip()
     for i in range(len(s)):
         if s == reverse_string(s, i):
             return s[:i][::-1] + s
 
-input_string = str(raw_input("Enter a string: "))
+
+input_string = input("Enter a string: ").strip()
 result = make_palindrome(input_string)
 print(result)
