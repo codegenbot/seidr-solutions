bcf7ad205b71f3d8b68f1aad6f54a36ae4564ab4
beep boop <githubbot@vadim.me>
2024-07-10 02:35:06 +0200

    Solution 2 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/s1xncepo Prompt:       |                              ^~~~~~ 5c570946-386c-41c8-a238-ab9d93dc219a.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5c570946-386c-41c8-a238-ab9d93dc219a.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5c570946-386c-41c8-a238-ab9d93dc219a.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 5c570946-386c-41c8-a238-ab9d93dc219a.cpp:9: 5c570946-386c-41c8-a238-ab9d93dc219a.cpp: In function ‘int main()’: 5c570946-386c-41c8-a238-ab9d93dc219a.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ 5c570946-386c-41c8-a238-ab9d93dc219a.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,20 +1,19 @@
 #include <vector>
 #include <string>
-#include <iostream>
 #include <cassert>
 
 using namespace std;
 
-bool issame(vector<string> a, vector<string> b){
-    return a == b;
-}
-
-vector<string> all_prefixes(string str){
+vector<string> all_prefixes(string str) {
     vector<string> result;
     for (int i = 1; i <= str.size(); ++i) {
         result.push_back(str.substr(0, i));
     }
     return result;
+}
+
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
 }
 
 int main() {
