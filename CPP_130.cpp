09dc8c3b0385ca25ff5493e6668e49151adf4950
beep boop <githubbot@vadim.me>
2024-07-10 20:51:59 +0200

    Solution 81 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/1td62fj3 Prompt:       |                    ^~~ 21451b5c-ab50-4a39-9a87-c899d5033d71.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 21451b5c-ab50-4a39-9a87-c899d5033d71.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 21451b5c-ab50-4a39-9a87-c899d5033d71.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 21451b5c-ab50-4a39-9a87-c899d5033d71.cpp:17: 21451b5c-ab50-4a39-9a87-c899d5033d71.cpp: In function ‘int main()’: 21451b5c-ab50-4a39-9a87-c899d5033d71.cpp:27:20: error: ‘tri’ was not declared in  this scope    27 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ 21451b5c-ab50-4a39-9a87-c899d5033d71.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,18 +1,6 @@
 #include <vector>
 #include <cassert>
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 vector<int> tri(int n) {
     vector<int> res;
     res.push_back(3);
@@ -29,6 +17,14 @@ vector<int> tri(int n) {
         }
     }
     return res;
+}
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
 }
 
 assert(issame(tri(1), {1, 3}));
