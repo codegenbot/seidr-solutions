f2b8d03184230c29ff24dbf474552095fbdfc0f8
beep boop <githubbot@vadim.me>
2024-07-10 18:22:39 +0200

    Solution 4 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dctrev1c Prompt:       |                              ^~~~~~ 409dd457-b9eb-4f16-a09f-993a54f11b59.cpp:35:37: error: ‘string’ was not declared  in this scope    35 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 409dd457-b9eb-4f16-a09f-993a54f11b59.cpp:35:44: error: ‘b’ was not declared in t his scope    35 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 409dd457-b9eb-4f16-a09f-993a54f11b59.cpp:35:45: error: expression list treated a s compound expression in initializer [-fpermissive]    35 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 409dd457-b9eb-4f16-a09f-993a54f11b59.cpp:34: 409dd457-b9eb-4f16-a09f-993a54f11b59.cpp: In function ‘int main()’: 409dd457-b9eb-4f16-a09f-993a54f11b59.cpp:44:20: error: ‘by_length’ was not decla red in this scope    44 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 409dd457-b9eb-4f16-a09f-993a54f11b59.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,37 +1,21 @@
 #include <vector>
 #include <string>
-#include <map>
 #include <algorithm>
+#include <map>
+#include <cassert>
 
-vector<string> by_length(vector<int> arr){
-    vector<string> result;
-    vector<int> sorted_arr;
-
-    for (int num : arr) {
-        if (num >= 1 && num <= 9) {
-            sorted_arr.push_back(num);
+bool issame(std::vector<std::string> vec1, std::vector<std::string> vec2) {
+    if (vec1.size() != vec2.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < vec1.size(); ++i) {
+        if (vec1[i] != vec2[i]) {
+            return false;
         }
     }
+    return true;
+}
 
-    sort(sorted_arr.begin(), sorted_arr.end());
-
-    reverse(sorted_arr.begin(), sorted_arr.end());
-
-    map<int, string> num_to_name = {
-        {1, "One"},
-        {2, "Two"},
-        {3, "Three"},
-        {4, "Four"},
-        {5, "Five"},
-        {6, "Six"},
-        {7, "Seven"},
-        {8, "Eight"},
-        {9, "Nine"}
-    };
-
-    for (int num : sorted_arr) {
-        result.push_back(num_to_name[num]);
-    }
-
-    return result;
+int main() {
+    assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
 }
