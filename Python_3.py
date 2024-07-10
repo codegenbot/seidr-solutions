dda24a404df414bcf567bf5f8638fe3fafbd2b0e
beep boop <githubbot@vadim.me>
2024-07-10 19:51:20 +0200

    Solution 7 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kzrjxt39 Prompt: Program did not receive expected input

--- Python_3.py
+++ Python_3.py
@@ -1,8 +1,6 @@
-from typing import List
-
 def below_zero(operations: List[int]) -> bool:
-    max_temp = 0
+    min_temp = float("inf")
     for operation in operations:
-        if operation > max_temp:
-            max_temp = operation
-    return max_temp < 0
+        if operation < min_temp:
+            min_temp = operation
+    return min_temp < 0
