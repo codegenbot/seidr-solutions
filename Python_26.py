93352d3fbea5e568a302a77e03bfe99819832b9b
beep boop <githubbot@vadim.me>
2024-07-10 21:55:02 +0200

    Solution 43 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/frboqn66 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/591685f8-fb79-4637-95c5-f9f06c2c2 931.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_26.py
+++ Python_26.py
@@ -1,2 +1,21 @@
+```
+from typing import List
+
+
 def remove_duplicates(numbers: List[int]) -> List[int]:
     return list(dict.fromkeys(numbers))
+
+
+if __name__ == "__main__":
+    print("Enter comma-separated integers: ")
+    numbers = input()
+    while True:
+        if numbers:
+            try:
+                numbers = [int(num) for num in numbers.split(",")]
+                print(remove_duplicates(numbers))
+                break
+            except ValueError:
+                print("Invalid input. Please enter comma-separated integers.")
+        else:
+            print("Invalid input. Please enter some values.")
