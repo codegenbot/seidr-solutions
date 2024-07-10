1128a5f8602c58963e390be31d1da3bae01e0dfc
beep boop <githubbot@vadim.me>
2024-07-10 14:08:47 +0200

    Solution 30 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/epocmqxi Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b1fdf64f-df51-46b6-9ae0-45db94364d74.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b1fdf64f-df51-46b6-9ae0-45db94364d74.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b1fdf64f-df51-46b6-9ae0-45db94364d74.cpp:10: b1fdf64f-df51-46b6-9ae0-45db94364d74.cpp: In function ‘int main()’: b1fdf64f-df51-46b6-9ae0-45db94364d74.cpp:20:20: error: ‘sort_third’ was not decl ared in this scope    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ b1fdf64f-df51-46b6-9ae0-45db94364d74.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,13 +1,20 @@
-void sort_third(vector<int>& l) {
-    for (int i = 0; i < l.size(); i += 3) {
-        sort(l.begin() + i, l.begin() + i + 3);
-    }
-}
+#include <vector>
+#include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b);
+
+std::vector<int> sort_third(std::vector<int> l);
+
+bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-int main() {
-    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
+std::vector<int> sort_third(std::vector<int> l) {
+    std::vector<int> l_copy = l;
+    for (int i = 0; i < l.size(); ++i) {
+        if (i % 3 == 0) {
+            std::sort(l_copy.begin() + i, l_copy.begin() + i + 3);
+        }
+    }
+    return l_copy;
 }
