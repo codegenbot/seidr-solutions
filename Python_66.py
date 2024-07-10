465295ade97be515459fb55d2ed6d3e24ab12955
beep boop <githubbot@vadim.me>
2024-07-10 17:24:47 +0200

    Solution 61 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/t5k8vwk5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/001ff3bd-b258-4bfa-ba76-5366648ab d44.py", line 11, in <module>     check(digitSum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/001ff3bd-b258-4bfa-ba76-5366648ab d44.py", line 8, in check     assert digitSum("You arE Very Smart") == 327, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/001ff3bd-b258-4bfa-ba76-5366648ab d44.py", line 4, in digitSum     return sum(int(digit) for digit in str(abs(n)))                                            ^^^^^^ TypeError: bad operand type for abs(): 'str'

--- Python_66.py
+++ Python_66.py
@@ -1,4 +1,4 @@
 def digitSum(n=""):
-    if not n:
-        n = int(input("Enter a number: "))
+    if not n:  
+        n = input("Enter a number: ")
     return sum(int(digit) for digit in str(abs(int(n))))
