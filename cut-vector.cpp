11cbc573b9271cb85454ae5a63da1fb96334e620
beep boop <githubbot@vadim.me>
2024-07-10 02:28:04 +0200

    Solution 61 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/mulxind8 Prompt:   +++ |+#include <iostream>     2 | using namespace std; 3f3995fd-ce3a-4ba1-8b08-b84215fdfad2.cpp:37:13: error: ‘cout’ was not declared i n this scope    37 |             cout << x << " ";       |             ^~~~ 3f3995fd-ce3a-4ba1-8b08-b84215fdfad2.cpp:37:13: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’? 3f3995fd-ce3a-4ba1-8b08-b84215fdfad2.cpp:39:9: error: ‘cout’ was not declared in  this scope    39 |         cout << endl;       |         ^~~~ 3f3995fd-ce3a-4ba1-8b08-b84215fdfad2.cpp:39:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 3f3995fd-ce3a-4ba1-8b08-b84215fdfad2.cpp:39:17: error: ‘endl’ was not declared i n this scope    39 |         cout << endl;       |                 ^~~~ 3f3995fd-ce3a-4ba1-8b08-b84215fdfad2.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,29 +1,7 @@
 #include <vector>
 #include <iostream>
-using namespace std;
 
-vector<vector<int>> cutVector(vector<int> v) {
-    int min_diff = INT_MAX;
-    int pos = -1;
-    
-    for(int i = 0; i < v.size() - 1; i++) {
-        int diff = abs(v[i] - v[i+1]);
-        
-        if(diff <= min_diff) {
-            min_diff = diff;
-            pos = i;
-        }
-    }
-    
-    vector<vector<int>> result(2);
-    result[0].resize(pos + 1);
-    result[1].resize(v.size() - pos - 1);
-    
-    copy(v.begin(), v.begin() + pos + 1, result[0].begin());
-    copy(v.begin() + pos + 1, v.end(), result[1].begin());
-    
-    return result;
-}
+using namespace std;
 
 vector<vector<int>> cutVector(vector<int> v) {
     int min_diff = INT_MAX;
@@ -58,9 +36,9 @@ int main() {
     vector<vector<int>> res = cutVector(v);
     for(auto &v : res) {
         for(int x : v) {
-            std::cout << x << " ";
+            cout << x << " ";
         }
-        std::cout << std::endl;
+        cout << endl;
     }
     return 0;
 }
