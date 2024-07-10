9e0c464d0af0128198e091a8593a10924b451a49
beep boop <githubbot@vadim.me>
2024-07-10 20:20:32 +0200

    Solution 18 of CPP/18, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/71jt3mt4 Prompt: Program failed

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -1,9 +1,9 @@
+```c++
 int how_many_times(string str, string substring) {
     int count = 0;
-    size_t pos = 0;
-    for (size_t i = 0; (pos = str.find(substring, i)) != string::npos;) {
+    for (size_t pos = 0; (pos = str.find(substring)) != string::npos;) {
         ++count;
-        i = pos + substring.length();
+        str.erase(pos, substring.length());
     }
     return count;
 }
