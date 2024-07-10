e393548270e0974b037bfacbb5d136d4918c1c4b
beep boop <githubbot@vadim.me>
2024-07-10 23:01:09 +0200

    Solution 93 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/qsa27kiy Prompt: 4fd1c526-1102-4679-a757-352442923bfe.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string words_in_sentence(string sentence){       | ^~~~~~ In file included from 4fd1c526-1102-4679-a757-352442923bfe.cpp:27: 4fd1c526-1102-4679-a757-352442923bfe.cpp: In function ‘int main()’: 4fd1c526-1102-4679-a757-352442923bfe.cpp:29:13: error: ‘words_in_sentence’ was n ot declared in this scope    29 |     assert (words_in_sentence("here is") == "is");       |             ^~~~~~~~~~~~~~~~~

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,19 +1,13 @@
 #include <string>
-
 using namespace std;
-
-int main() {
-    assert (words_in_sentence("here is") == "is");
-    return 0;
-}
 
 string words_in_sentence(string sentence){
     string result = "";
     int count = 0;
     for(int i=2; i<=100; i++){
         if(isPrime(i)){
-            int pos = 0;
-            while((pos = sentence.find(" ")) != -1 && isPrime(atoi(sentence.substr(0, pos).c_str()))){
+            while(sentence.find(" ") != -1 && isPrime(sentence.size())){
+                int pos = sentence.find(" ");
                 result += sentence.substr(0, pos) + " ";
                 sentence.erase(0, pos+1);
             }
