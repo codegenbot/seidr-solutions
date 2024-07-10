5457da5c301f362a07f06107e689fd1aa3fca2e5
beep boop <githubbot@vadim.me>
2024-07-10 23:11:16 +0200

    Solution 70 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6e5a28de-8b57-43d2-a3f6-15123c6da 146.py", line 11, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6e5a28de-8b57-43d2-a3f6-15123c6da 146.py", line 6, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,3 +1,9 @@
-```
 def sort_third(l):
-    return sorted([sublist[1] for sublist in l if isinstance(sublist, list) and len(sublist)>1])
+    int_list = []
+    for item in l:
+        if isinstance(item, list):
+            int_list.append(item[1])
+        else:
+            int_list.append(item)
+    int_list.sort()
+    return [sublist[1] for sublist in [[int_list.count(i), i] for i in set(int_list)]]
