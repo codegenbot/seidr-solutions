b2f09dd9af6b512238a396772b91df1e63edaebd
beep boop <githubbot@vadim.me>
2024-07-10 08:23:51 +0200

    Solution 17 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kd2jtp3g Prompt:       |                       ^~~~~~ 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:1:31: error: ‘operator_’ was not declar ed in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                               ^~~~~~~~~ 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:1:42: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                          ^~~~~~ 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:1:49: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                 ^~~ 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:1:61: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                             ^ In file included from 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:50: 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp: In function ‘int main()’: 4d387f1a-159e-49ba-8cb1-0c25e489bde0.cpp:52:23: error: ‘do_algebra’ cannot be us ed as a function    52 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,38 +1,43 @@
-```cpp
 #include <vector>
 #include <string>
 #include <cmath>
 
-int do_algebra(vector<string> operators, vector<int> operands) {
+int do_algebra(vector<string> operator_, vector<int> operand) {
     int result = 0;
-    int sign = 1;
-    for (int i = 0; i < operators.size(); i++) {
-        if (operators[i] == "+") {
-            result += sign * operands[i];
-        } else if (operators[i] == "-") {
-            result -= sign * operands[i];
-            sign = -1;
-        } else if (operators[i] == "*") {
-            int temp = 0;
-            for (int j = i; j < operators.size(); j++) {
-                if (j < operators.size() - 1 && operators[j + 1] == "*") {
-                    temp *= operands[j + 1];
-                } else if (j < operators.size() - 1 && operators[j + 1] == "/") {
-                    temp /= operands[j + 1];
-                }
+    int temp = 0;
+    bool inParenthesis = false;
+
+    for (int i = 0; i < operator_.size(); i++) {
+        if (operator_[i] == "(") {
+            inParenthesis = true;
+        } else if (operator_[i] == ")") {
+            inParenthesis = false;
+        } else if (!inParenthesis) {
+            if (operator_[i] == "+") {
+                result += operand[i];
+            } else if (operator_[i] == "-") {
+                result -= operand[i];
+            } else if (operator_[i] == "*") {
+                temp *= operand[i];
+            } else if (operator_[i] == "/") {
+                temp /= operand[i];
+            } else if (operator_[i] == "**") {
+                temp = pow(temp, operand[i]);
             }
-            result += sign * temp;
-        } else if (operators[i] == "/") {
-            int temp = operands[i];
-            for (int j = i + 1; j < operators.size(); j++) {
-                if (j < operators.size() - 1 && operators[j + 1] == "*") {
-                    temp *= operands[j + 1];
-                } else if (j < operators.size() - 1 && operators[j + 1] == "/") {
-                    temp /= operands[j + 1];
-                }
+        } else {
+            if (operator_[i] == "+") {
+                temp += operand[i];
+            } else if (operator_[i] == "-") {
+                temp -= operand[i];
+            } else if (operator_[i] == "*") {
+                temp *= operand[i];
+            } else if (operator_[i] == "/") {
+                temp /= operand[i];
+            } else if (operator_[i] == "**") {
+                temp = pow(temp, operand[i]);
             }
-            result += sign / temp;
         }
     }
+
     return result;
 }
