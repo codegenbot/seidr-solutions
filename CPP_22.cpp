2b0f4ec315ba96615d0af525a81b251848129068
beep boop <githubbot@vadim.me>
2024-07-10 23:36:16 +0200

    Solution 36 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5lnke8m2 Prompt: Complete the following code given the task description and function signature.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -5,11 +5,12 @@
 typedef std::list<boost::any> list_any;
 using namespace std;
 
-vector<int> filter_integers(list_any values) {
+vector<int> filter_integers(list_any values){
     vector<int> result;
     for (const auto& value : values) {
-        if (boost::any_cast<int>(value, 0)) {
-            result.push_back(boost::any_cast<int>(value));
+        if (boost::any_cast<bool>(value)) {
+            int val = boost::any_cast<int>(value);
+            result.push_back(val);
         }
     }
     return result;
