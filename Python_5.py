a935bbbd9aa10451b16f635b04e23d94aee292a1
beep boop <githubbot@vadim.me>
2024-07-10 13:30:06 +0200

    Solution 86 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/y81l3d0f Prompt: Complete the following code given the task description and function signature.

--- Python_5.py
+++ Python_5.py
@@ -1,6 +1,5 @@
-result = []
-for i in range(len(numbers)):
-    result.append(numbers[i])
-    if i != len(numbers) - 1:
-        result.append(delimeter)
-return result
+return [
+    item
+    for sublist in [[num, delimeter] for num in numbers[:-1]] + [numbers[-1:]]
+    for item in sublist
+]
