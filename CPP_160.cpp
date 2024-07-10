04b4e8224680112d0012b3e6ae56429087e6710e
beep boop <githubbot@vadim.me>
2024-07-10 08:32:49 +0200

    Solution 30 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kd2jtp3g Prompt: d in this scope    86 |             while (isdigit(*p)) {       |                    ^~~~~~~ 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp:89:21: error: ‘atoi’ was not declared i n this scope    89 |             int x = atoi(p - 1);       |                     ^~~~ 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp:90:17: error: ‘pow’ was not declared in  this scope    90 |             y = pow(y, x);       |                 ^~~ 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp:91:26: error: ‘strlen’ was not declared  in this scope    91 |             p = p + (int)strlen(p);       |                          ^~~~~~ 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp:91:26: note: ‘strlen’ is defined in hea der ‘<cstring>’; did you forget to ‘#include <cstring>’? In file included from 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp:98: 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp: In function ‘int main()’: 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp:100:23: error: ‘do_algebra’ cannot be u sed as a function   100 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,51 +1,119 @@
 #include <vector>
+#include <string>
+#include <limits>
+#include <cstring>
+#include <cstdlib>
 #include <cmath>
+
+using namespace std;
+
+long long eval(const char *p) {
+    return eval(p, 0);
+}
+
+long long eval(const char *p, long long y) {
+    while (*p) {
+        if (isdigit(*p)) {
+            int x = 0;
+            while (isdigit(*p)) {
+                x = x * 10 + (*p - '0');
+                p++;
+            }
+            if (*p == 'y') {
+                y += x;
+                p++;
+                continue;
+            }
+            y = x;
+            p++;
+            continue;
+        }
+        if (*p == ' ') {
+            p++;
+            continue;
+        }
+        if (*p == '+') {
+            p++;
+            if (*p == 'y') {
+                y++;
+                p += 2;
+                continue;
+            }
+            p++;
+            continue;
+        }
+        if (*p == '-') {
+            p++;
+            if (*p == 'y') {
+                y--;
+                p += 2;
+                continue;
+            }
+            p++;
+            continue;
+        }
+        if (*p == '*') {
+            p++;
+            int x = 0;
+            while (isdigit(*p)) {
+                x = x * 10 + (*p - '0');
+                p++;
+            }
+            y *= x;
+            p++;
+            continue;
+        }
+        if (*p == '/') {
+            p++;
+            int x = 0;
+            while (isdigit(*p)) {
+                x = x * 10 + (*p - '0');
+                p++;
+            }
+            if (x != 0)
+                y /= x;
+            else
+                return numeric_limits<long long>::max();
+            p++;
+            continue;
+        }
+        if (*p == '^') {
+            p++;
+            int x = 0;
+            while (isdigit(*p)) {
+                x = x * 10 + (*p - '0');
+                p++;
+            }
+            y = pow(y, x);
+            p++;
+            continue;
+        }
+    }
+    return y;
+}
 
 int do_algebra(vector<string> operator_, vector<int> operand) {
     int result = 0;
+    string expression = "";
+    
     for (int i = 0; i < operator_.size(); i++) {
-        if (operator_[i] == "+") {
-            result += operand[i];
-        } else if (operator_[i] == "-") {
-            result -= operand[i];
-        } else if (operator_[i] == "*") {
-            int temp = 0;
-            for (int j = i; j < operator_.size(); j++) {
-                if (operator_[j] == "*") {
-                    temp *= operand[j + 1];
-                } else if (operator_[j] == "/") {
-                    temp /= operand[j + 1];
-                } else if (operator_[j] == "**") {
-                    temp = pow(temp, operand[j + 1]);
-                }
-            }
-            result += temp;
-            i = j; // skip the rest of the loop
-        } else if (operator_[i] == "/") {
-            int temp = operand[i];
-            for (int j = i + 1; j < operator_.size(); j++) {
-                if (operator_[j] == "*") {
-                    temp *= operand[j + 1];
-                } else if (operator_[j] == "/") {
-                    temp /= operand[j + 1];
-                } else if (operator_[j] == "**") {
-                    temp = pow(temp, operand[j + 1]);
-                }
-            }
-            result += temp;
-        } else if (operator_[i] == "**") {
-            int temp = 1;
-            for (int j = i; j < operator_.size(); j++) {
-                if (operator_[j] == "*") {
-                    temp *= operand[j + 1];
-                } else if (operator_[j] == "/") {
-                    temp /= operand[j + 1];
-                } else if (operator_[j] == "**") {
-                    temp = pow(temp, operand[j + 1]);
-                }
-            }
-            result += pow(operand[0], temp);
+        if (i == 0)
+            expression += to_string(operand[i]);
+        else if (i == operator_.size() - 1)
+            expression += operator_[i] + to_string(operand[i]);
+        else
+            expression += " " + operator_[i] + " " + to_string(operand[i]);
+    }
+    
+    int temp = 0;
+    for (int i = 0; i < operand.size(); i++) {
+        if (expression.find(to_string(operand[i])) != string::npos) {
+            temp = operand[i];
+            break;
         }
     }
+    
+    result = eval(expression.c_str());
+    
     return result;
 }
