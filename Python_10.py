5fc586200e75ba3fd4f06894127a69857d293246
beep boop <githubbot@vadim.me>
2024-07-10 23:46:43 +0200

    Solution 96 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6ml3f6al Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fbdf5bcf-82f1-4b75-bbb5-5b139e3fa 690.py", line 20, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fbdf5bcf-82f1-4b75-bbb5-5b139e3fa 690.py", line 17, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,7 +1,19 @@
+def is_palindrome(string: str) -> bool:
+    return string == string[::-1]
+
+
 def make_palindrome(string: str) -> str:
     if not string:
         return ""
     else:
-        half = string[: len(string) // 2]
-        rest = string[len(string) // 2 :]
-        return "".join([half[::-1], string, half[::-1]])
+        if is_palindrome(string):
+            return string + string
+        else:
+            for i in range(len(string)):
+                for j in range(len(string)):
+                    new_string = (
+                        string[:i] + chr(ord(string[i]) ^ ord("a")) + string[i:]
+                    )
+                    if is_palindrome(new_string):
+                        return "" + new_string + "" + new_string
+            return "" + string[::-1] + "" + string[::-1]
