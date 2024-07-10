304b450c3d81829d3a9ae6a9028d6318c90bdb7b
beep boop <githubbot@vadim.me>
2024-07-10 23:42:44 +0200

    Solution 84 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6ml3f6al Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d6499f4f-ff46-44cc-9813-44e9b68db fb0.py", line 18, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d6499f4f-ff46-44cc-9813-44e9b68db fb0.py", line 15, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,12 +1,13 @@
+def is_palindrome(string: str) -> bool:
+    return string == string[::-1]
+
+
 def make_palindrome(string: str) -> str:
     if not string:
         return ""
     elif is_palindrome(string):
-        return string
+        return string + string
     else:
         for i in range(len(string)):
-            reversed_half = string[i:][::-1]
-            new_string = string[:i] + reversed_half + reversed_half
-            if is_palindrome(new_string):
-                return new_string
-        return string + string[::-1]
+            if is_palindrome(string[:i] + string[i:][::-1]):
+                return string[:i] + (string[i:][::-1]) + string[i]
