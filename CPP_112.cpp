fa7de13ce6d170f9621e59f28fde2dbf06f518c1
beep boop <githubbot@vadim.me>
2024-07-10 05:27:10 +0200

    Solution 76 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9mcoota0 Prompt: In file included from /usr/include/c++/11/bits/stl_algobase.h:64,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/ios:40,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from ab082e09-c188-42cd-81b8-ba273999d549.cpp:2: /usr/include/c++/11/bits/stl_pair.h:541:5: note: candidate: ‘template<class _T1,  class _T2> typename std::enable_if<(! std::__and_<std::__is_swappable<_T1>, std ::__is_swappable<_T2> >::value)>::type std::swap(std::pair<_T1, _T2>&, std::pair <_T1, _T2>&)’ (deleted)   541 |     swap(pair<_T1, _T2>&, pair<_T1, _T2>&) = delete;       |     ^~~~ /usr/include/c++/11/bits/stl_pair.h:541:5: note:   template argument deduction/s ubstitution failed: In file included from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/ios:40,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from ab082e09-c188-42cd-81b8-ba273999d549.cpp:2: /usr/include/c++/11/bits/stl_algobase.h:182:11: note:   mismatched types ‘std::p air<_T1, _T2>’ and ‘const char’   182 |       swap(*__a, *__b);       |       ~~~~^~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -35,7 +35,9 @@ string reverse_delete(const string s1, const string s2) {
             s1 = s1.substr(0, pos) + s1.substr(pos + 1);
         }
     }
-    std::reverse(s1.begin(), s1.end());
+    string temp = s1;
+    reverse(temp.begin(), temp.end());
+    s1 = temp;
     return s1;
 }
 
