acd4477b40d06e2363784039ec8237adb3e3161e
beep boop <githubbot@vadim.me>
2024-07-10 16:41:39 +0200

    Solution 9 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/y3v4v2tq Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -2,4 +2,4 @@ def add(x: int, y: int):
     if isinstance(x, int) and isinstance(y, int):
         return x + y
     else:
-        return "Invalid inputs. Please enter integers."
+        return "Invalid input"
