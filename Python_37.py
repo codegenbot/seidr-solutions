139b357d7e7e453292efcee92359d4328a37dd0d
beep boop <githubbot@vadim.me>
2024-07-10 19:41:11 +0200

    Solution 40 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uquygx2d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6db25fa5-7070-45b5-818e-7fe9d12e1 eb8.py", line 16, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6db25fa5-7070-45b5-818e-7fe9d12e1 eb8.py", line 11, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,3 +1,4 @@
+```
 def sort_even(l: list):
     even = sorted([x for x in l if x % 2 == 0])
     odd = sorted([x for x in l if x % 2 != 0])
@@ -6,4 +7,4 @@ def sort_even(l: list):
     even += [0] * (max_len - len(even)) 
     odd += [0] * (max_len - len(odd)) 
 
-    return tuple(zip(even, odd)[:min(max_len, len(even), len(odd))])
+    return tuple(i for i in zip(even, odd))
