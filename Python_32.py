32d525e4d3d3fc3f208dad928fa5c15d2c537323
beep boop <githubbot@vadim.me>
2024-07-11 01:45:19 +0200

    Solution 56 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/774qpb9z Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6fce4e19-4213-4e7d-8e5b-c44ead89b 3c9.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,8 +1,5 @@
-def find_zero():
-    n = int(input("Enter the number of coefficients: "))
-    xs = list(map(float, input("Enter coefficients (space-separated): ").split()))
-
+def find_zero(xs: list):
+    n = len(xs)
     if n % 2 != 0:
         raise ValueError("xs must have even number of coefficients")
-
-    return -xs[1] / xs[-1]
+    return -xs[1] / xs[n - 1]
