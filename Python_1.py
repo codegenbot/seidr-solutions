b1e24314650866b8b46be3aa502aea7ce16449f2
beep boop <githubbot@vadim.me>
2024-07-11 00:28:32 +0200

    Solution 76 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/gpbpylxc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f897ad5-db5b-46a3-914e-341093af9 ada.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_1.py
+++ Python_1.py
@@ -1,25 +1,22 @@
-```
-```python
 from typing import List
+
 
 def separate_paren_groups(paren_string: str) -> List[str]:
     result = []
     stack = []
-    temp_str = ''
+    temp_str = ""
 
     for char in paren_string:
-        if char == ' ':
+        if char == " ":
             continue
-        if char == '(':
+        if char == "(":
             stack.append(char)
             temp_str += char
-        elif char == ')':
-            if stack and stack[-1] == '(':
+        elif char == ")":
+            if stack and stack[-1] == "(":
                 stack.pop()
                 temp_str += char
                 result.append(temp_str)
-                temp_str = ''
-            else:
-                temp_str += char
-    return result
-```
+                temp_str = ""
+
+    return [temp_str]
