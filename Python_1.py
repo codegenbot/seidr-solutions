a0b08b3ac2d34318320c5c5779a11abead8eaabe
beep boop <githubbot@vadim.me>
2024-07-10 03:15:13 +0200

    Solution 57 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/agkgesc3 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/552c2f86-9e9f-4b21-9d79-a926b21bd 963.py", line 28, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/552c2f86-9e9f-4b21-9d79-a926b21bd 963.py", line 25, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ AssertionError

--- Python_1.py
+++ Python_1.py
@@ -1,22 +1,27 @@
 def separate_paren_groups(expression):
     result = []
     temp_group = ""
-    
     in_group = False
     
     for char in expression:
         if char == '(':
-            in_group = True
-            temp_group += char
-        elif char == ')':
-            in_group = False
-            temp_group += char
-            result.append(temp_group)
-            temp_group = ""
-        else:
             if not in_group:
-                result.append(char)
+                result.append("(")
+                in_group = True
             else:
                 temp_group += char
+        elif char == ')':
+            if in_group:
+                temp_group += char
+            else:
+                result.append(")")
+                in_group = False
+        else:
+            if in_group:
+                temp_group += char
+            else:
+                result.append(char)
     
-    return result
+    if in_group:
+        result.append(temp_group + ")")
+    return [x for x in result if x]
