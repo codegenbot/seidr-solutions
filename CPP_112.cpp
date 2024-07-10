606e7c869a22f250f668aa0fe120ca4e998b67c1
beep boop <githubbot@vadim.me>
2024-07-10 17:50:11 +0200

    Solution 13 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/xn80h5dq Prompt:       |                              ^~~~~~ 38f70554-f97b-4445-a021-c6d9f338b074.cpp:13:37: error: ‘string’ was not declared  in this scope    13 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 38f70554-f97b-4445-a021-c6d9f338b074.cpp:13:44: error: ‘b’ was not declared in t his scope    13 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 38f70554-f97b-4445-a021-c6d9f338b074.cpp:13:45: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 38f70554-f97b-4445-a021-c6d9f338b074.cpp:12: 38f70554-f97b-4445-a021-c6d9f338b074.cpp: In function ‘int main()’: 38f70554-f97b-4445-a021-c6d9f338b074.cpp:22:20: error: ‘reverse_delete’ was not declared in this scope    22 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 38f70554-f97b-4445-a021-c6d9f338b074.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 bool issame(vector<string> a, vector<string> b) {
 vector<string> reverse_delete(string s, string c) {
-    vector<string> result = {""};
+    vector<string> result;
     for (char ch : s) {
         if (c.find(ch) == string::npos) {
-            result[0] += ch;
+            result.push_back(string(1, ch));
         }
     }
-    vector<string> reversed = result;
-    reverse(reversed[0].begin(), reversed[0].end());
-    return {result[0], result[0] == reversed[0] ? "True" : "False"};
+    string reversed = accumulate(result.rbegin(), result.rend(), string(""));
+    return {result, result == reversed ? "True" : "False"};
 }
