c182491335cb8c2a43925e10467127adee5042aa
beep boop <githubbot@vadim.me>
2024-07-10 20:33:16 +0200

    Solution 5 of CPP/157, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/erqy5zon Prompt: dcb29470-22e0-43ae-bb77-1d7f0ff639d8.cpp:1:1: error: expected unqualified-id bef ore ‘if’     1 | if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c ==  a * a) {       | ^~ dcb29470-22e0-43ae-bb77-1d7f0ff639d8.cpp:3:7: error: expected unqualified-id bef ore ‘else’     3 |     } else {       |       ^~~~ dcb29470-22e0-43ae-bb77-1d7f0ff639d8.cpp:6:1: error: expected declaration before  ‘}’ token     6 | }       | ^ In file included from dcb29470-22e0-43ae-bb77-1d7f0ff639d8.cpp:8: dcb29470-22e0-43ae-bb77-1d7f0ff639d8.cpp: In function ‘int main()’: dcb29470-22e0-43ae-bb77-1d7f0ff639d8.cpp:10:13: error: ‘right_angle_triangle’ wa s not declared in this scope    10 |     assert (right_angle_triangle(2, 2, 10) == false);       |             ^~~~~~~~~~~~~~~~~~~~

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,3 +1,3 @@
 bool right_angle_triangle(int a, int b, int c) {
-    return (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a);
+    return a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a;
 }
