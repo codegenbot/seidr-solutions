93cc97cd4bec7bcf0b69195f13817224f56757fa
beep boop <githubbot@vadim.me>
2024-07-10 18:26:32 +0200

    Solution 8 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/frjrui8h Prompt:       |                    ^~~ c19fdc06-996c-4f63-b5ea-23a972f1322d.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c19fdc06-996c-4f63-b5ea-23a972f1322d.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c19fdc06-996c-4f63-b5ea-23a972f1322d.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c19fdc06-996c-4f63-b5ea-23a972f1322d.cpp:18: c19fdc06-996c-4f63-b5ea-23a972f1322d.cpp: In function ‘int main()’: c19fdc06-996c-4f63-b5ea-23a972f1322d.cpp:28:20: error: ‘even_odd_palindrome’ was  not declared in this scope    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ c19fdc06-996c-4f63-b5ea-23a972f1322d.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,12 +1,9 @@
 #include <vector>
-
+#include <string>
+#include <algorithm>
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-vector<int> even_odd_palindrome(int n) {
+vector<int> even_odd_palindrome(int n){
     vector<int> result(2, 0);
     for (int i = 1; i <= n; ++i) {
         string s = to_string(i);
@@ -23,7 +20,11 @@ vector<int> even_odd_palindrome(int n) {
     return result;
 }
 
-int main() {
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
+int main(){
     assert(issame(even_odd_palindrome(1), {0, 1}));
     return 0;
 }
