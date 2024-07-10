3e365daec0deaf172b3e5eddc34482a78b59928a
beep boop <githubbot@vadim.me>
2024-07-10 23:55:10 +0200

    Solution 10 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/oz21z9z9 Prompt: c19dad83-0779-428c-a8f5-e12f90b7c1de.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ c19dad83-0779-428c-a8f5-e12f90b7c1de.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,10 +1,16 @@
-```cpp
+#include <iostream>
 #include <vector>
 
-int specialFilter(vector<int> nums){
+int specialFilter(std::vector<int> nums){
     int count = 0;
     for(int num : nums){
-        if(abs(num) > 10 && (abs(num)%10)%2 != 0 && (abs(num)/10)%10%2 != 0)
+        if(num > 10 && (num % 10) % 2 != 0 && (num / 10) % 10 % 2 != 0)
             count++;
     }
     return count;
+}
+
+int main() {
+    std::cout << specialFilter({1,11,12,13}) << std::endl; 
+    return 0;
+}
