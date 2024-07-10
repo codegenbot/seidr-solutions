2d2700cfb2e9fb40959dd67777e2dbb61a1d4bd3
beep boop <githubbot@vadim.me>
2024-07-10 21:09:21 +0200

    Solution 94 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lkc4njlx Prompt: Program did not receive expected input

--- Python_6.py
+++ Python_6.py
@@ -1,20 +1,19 @@
 from typing import List
 
+
 def parse_nested_parens(paren_string: str) -> List[int]:
     result = []
-    level = 1 if paren_string[0] == '(' else 0
-    deepest = level
-    group_depths = [deepest]
-    for char in paren_string[1:]:
-        if char == '(' and level > 0:
+    level = 0
+    deepest = 0
+    for char in paren_string:
+        if char == "(" and level > 0:
             continue
-        elif char == '(':
+        elif char == "(":
             level += 1
             deepest = level
-            group_depths.append(deepest)
-        elif char == ')':
+        elif char == ")":
             level -= 1
-        if level == 0:
-            result.append(deepest)
-            deepest = 0
+            if level == 0:
+                result.append(deepest)
+                deepest = 0
     return result
