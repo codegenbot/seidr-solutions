a9109e9651d9965951414acc6926afba5da288f4
beep boop <githubbot@vadim.me>
2024-07-10 21:46:18 +0200

    Solution 33 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0ac15d5e-680a-45b3-864c-26689018d ff3.py", line 10, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0ac15d5e-680a-45b3-864c-26689018d ff3.py", line 5, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,7 +1,3 @@
 def sort_third(l: list):
     sorted_l = sorted((x, i) for i, x in enumerate(l))
-    l1 = [x for x, _ in sorted_l if _ % 3 == 0]
-    l2 = [x for x, _ in sorted_l if _ % 3 == 1]
-    l3 = [x for x, _ in sorted_l if _ % 3 == 2]
-
-    return [x for _, i in sorted((i, x) for x, i in enumerate(l))][::3] + [x for _, i in sorted((i, x) for x, i in enumerate(l))[1::3]] + [x for _, i in sorted((i, x) for x, i in enumerate(l))[2::3]]
+    return [x for _, x in sorted(sorted_l)]
