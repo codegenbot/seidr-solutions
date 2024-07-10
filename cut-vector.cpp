e93d2adb621bbb65582884b7a2effd3122e95ebd
beep boop <githubbot@vadim.me>
2024-07-10 08:31:19 +0200

    Solution 58 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/jtwmj2dd Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,36 +1,30 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
-    int min_diff = INT_MAX;
-    int split_idx = 0;
-    for (int i = 1; i < v.size(); ++i) {
-        int diff = v[i] - v[0];
-        if (diff <= min_diff) {
-            min_diff = diff;
-            split_idx = i;
+pair<vector<int>, vector<int>> cutVector(vector<int> nums) {
+    int minDiff = INT_MAX;
+    int splitIndex = -1;
+    
+    for (int i = 0; i < nums.size(); ++i) {
+        int leftSum = 0, rightSum = 0;
+        
+        for (int j = 0; j < i; ++j)
+            leftSum += nums[j];
+        for (int j = i + 1; j < nums.size(); ++j)
+            rightSum += nums[j];
+        
+        int diff = abs(leftSum - rightSum);
+        if (diff <= minDiff) {
+            minDiff = diff;
+            splitIndex = i;
         }
     }
-    return {vector<int>(v.begin(), v.begin() + split_idx), vector<int>(v.begin() + split_idx, v.end())};
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> v[i];
-    }
-    pair<vector<int>, vector<int>> res = cutVector(v);
-    cout << "{";
-    for (int x : res.first) {
-        cout << x << " ";
-    }
-    cout << "}" << endl;
-    cout << "{";
-    for (int x : res.second) {
-        cout << x << " ";
-    }
-    cout << "0" << endl;
-    return 0;
+    
+    vector<int> leftVec, rightVec;
+    for (int i = 0; i < splitIndex; ++i)
+        leftVec.push_back(nums[i]);
+    for (int i = splitIndex; i < nums.size(); ++i)
+        rightVec.push_back(nums[i]);
+    
+    return {leftVec, rightVec};
 }
