420795b2e02c63cec07597568e48407bedb1bb18
beep boop <githubbot@vadim.me>
2024-07-10 08:33:41 +0200

    Solution 39 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kd2jtp3g Prompt: d in this scope    86 |             while (isdigit(*p)) {       |                    ^~~~~~~ 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp:89:21: error: ‘atoi’ was not declared i n this scope    89 |             int x = atoi(p - 1);       |                     ^~~~ 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp:90:17: error: ‘pow’ was not declared in  this scope    90 |             y = pow(y, x);       |                 ^~~ 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp:91:26: error: ‘strlen’ was not declared  in this scope    91 |             p = p + (int)strlen(p);       |                          ^~~~~~ 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp:91:26: note: ‘strlen’ is defined in hea der ‘<cstring>’; did you forget to ‘#include <cstring>’? In file included from 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp:98: 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp: In function ‘int main()’: 46a7017c-67f5-47bf-87c7-76f6fb22d39f.cpp:100:23: error: ‘do_algebra’ cannot be u sed as a function   100 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,9 +1,82 @@
+```cpp
 #include <vector>
 #include <string>
 #include <limits>
-#include <cstring>
-#include <cstdlib>
 #include <cmath>
+#include <cctype>
+
+using namespace std;
+
+long long eval(const char *p) {
+    return eval(p, 0);
+}
+
+long long eval(const char *p, long long y) {
+    while (*p) {
+        if (isdigit(*p)) {
+            p++;
+            continue;
+        }
+        if (*p == ' ') {
+            p++;
+            continue;
+        }
+        if (*p == '+') {
+            p++;
+            if (*p == 'y') {
+                y++;
+                p += 2;
+                continue;
+            }
+            p++;
+            continue;
+        }
+        if (*p == '-') {
+            p++;
+            if (*p == 'y') {
+                y--;
+                p += 2;
+                continue;
+            }
+            p++;
+            continue;
+        }
+        if (*p == '*') {
+            int x = 1;
+            while (isdigit(*p)) {
+                x = x * (int)(*p - '0');
+                p++;
+            }
+            y *= x;
+            p += (int)strlen(p);
+            continue;
+        }
+        if (*p == '/') {
+            int x = 1;
+            while (isdigit(*p)) {
+                x = x * (int)(*p - '0');
+                p++;
+            }
+            if (x != 0)
+                y /= x;
+            else
+                return numeric_limits<long long>::max();
+            p += (int)strlen(p);
+            continue;
+        }
+        if (*p == '^') {
+            int x = 1;
+            while (isdigit(*p)) {
+                x = x * (int)(*p - '0');
+                p++;
+            }
+            y = pow(y, x);
+            p += (int)strlen(p);
+            continue;
+        }
+    }
+    return y;
+}
 
 int do_algebra(vector<string> operator_, vector<int> operand) {
     int result = 0;
@@ -29,84 +102,4 @@ int do_algebra(vector<string> operator_, vector<int> operand) {
     result = eval(expression.c_str());
     
     return result;
-}
-
-long long eval(const char *p) {
-    return eval(p, 0);
-}
-
-long long eval(const char *p, long long y) {
-    while (*p) {
-        if (isdigit(*p)) {
-            int x = 0;
-            while (isdigit(*p)) {
-                x = x * 10 + (*p - '0');
-                p++;
-            }
-            if (*p == 'y') {
-                y = x;
-                p++;
-                continue;
-            }
-            y = x;
-        }
-        else if (*p == ' ') {
-            p++;
-            continue;
-        }
-        else if (*p == '+') {
-            p++;
-            if (*p == 'y') {
-                y++;
-                p += 2;
-                continue;
-            }
-            p++;
-            continue;
-        }
-        else if (*p == '-') {
-            p++;
-            if (*p == 'y') {
-                y--;
-                p += 2;
-                continue;
-            }
-            p++;
-            continue;
-        }
-        else if (*p == '*') {
-            p++;
-            int x = 0;
-            while (isdigit(*p)) {
-                x = x * 10 + (*p - '0');
-                p++;
-            }
-            y *= x;
-            continue;
-        }
-        else if (*p == '/') {
-            p++;
-            int x = 0;
-            while (isdigit(*p)) {
-                x = x * 10 + (*p - '0');
-                p++;
-            }
-            if (x != 0)
-                y /= x;
-            else
-                return std::numeric_limits<long long>::max();
-            continue;
-        }
-        else if (*p == '^') {
-            p++;
-            int x = 0;
-            while (isdigit(*p)) {
-                x = x * 10 + (*p - '0');
-                p++;
-            }
-            y = pow(y, x);
-            continue;
-        }
-    }
-    return y;
 }
