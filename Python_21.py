2b91c5aaecf388c6b3d498d931f1fe9f1bad9106
beep boop <githubbot@vadim.me>
2024-07-10 20:49:00 +0200

    Solution 53 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vmj9u5tm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b399245-ec20-4284-9e8d-5fb6756f1 bcf.py", line 17, in <module>     check(rescale_to_unit)           ^^^^^^^^^^^^^^^ NameError: name 'rescale_to_unit' is not defined

--- Python_21.py
+++ Python_21.py
@@ -1,3 +1,11 @@
+```
+def find_average(numbers):
+    min_val = min(numbers)
+    max_val = max(numbers)
+    if (max_val - min_val) == 0:
+        return ["0.0"] * len(numbers)
+    return [str((i - min_val) / (max_val - min_val)) for i in numbers]
+
 def check(func, *args):
     return func(*args)
 
