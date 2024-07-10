6d344794b338e4655e07c9857b1631c9de4d75e4
beep boop <githubbot@vadim.me>
2024-07-10 04:49:18 +0200

    Solution 69 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9z8gtv5q Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/986e1db9-1da6-49b1-8b19-b4c5792e8 4fd.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_20.py
+++ Python_20.py
@@ -1,13 +1,2 @@
-```
-def check(lst):
-    lst.sort()
-    result = float('inf')
-    for i in range(len(lst) - 1):
-        if lst[i] != lst[i+1]:
-            diff = abs(lst[i] - lst[i+1])
-            result = min(result, diff)
-    return result
-
-n = int(input())
-arr = list(map(int, input().split()))
-print(check(arr))
+lst = list(map(int, input().split()))
+print(check(lst))
