f12ed3fb9bd503e50ee93ccb6453f2b32801887e
beep boop <githubbot@vadim.me>
2024-07-10 09:45:07 +0200

    Solution 46 of Python/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/z9sk4atx Prompt: Complete the following code given the task description and function signature.

--- Python_106.py
+++ Python_106.py
@@ -1,11 +1,5 @@
 def f(n):
-    return [
-        1 if i == 0 else factorial(i) if i % 2 == 0 else sum(range(1, i + 1))
-        for i in range(n)
+    return [1] + [
+        math.factorial(i) if i % 2 == 0 else sum(range(1, i + 1))
+        for i in range(2, n + 1)
     ]
-
-
-def factorial(num):
-    if num == 0:
-        return 1
-    return num * factorial(num - 1)
