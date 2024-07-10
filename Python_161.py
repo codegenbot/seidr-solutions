a15281b09212ff37b83989f2cb20338dd7bc5cdf
beep boop <githubbot@vadim.me>
2024-07-10 22:44:48 +0200

    Solution 1 of Python/161, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1cqd04lc Prompt: Complete the following code given the task description and function signature.

--- Python_161.py
+++ Python_161.py
@@ -1,9 +1,14 @@
 ```
 def solve(s):
     result = ""
-    for char in s:
-        if char.isalpha():
-            result += char.swapcase()
+    is_prev_letter = False
+    for c in s:
+        if c.isalpha():
+            result += c.swapcase()
+            is_prev_letter = True
+        elif is_prev_letter:
+            result += c
+            is_prev_letter = False
         else:
-            result += char
-    return result[::-1] if not any(char.isalpha() for char in s) else result
+            result += c
+    return result[::-1] if not any(c.isalpha() for c in s) else result
