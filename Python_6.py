f07780b4cc5143f0fc17e545a65f7c7934ac6b4b
beep boop <githubbot@vadim.me>
2024-07-10 17:39:21 +0200

    Solution 1 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/4bm0qi0v Prompt: Complete the following code given the task description and function signature.

--- Python_6.py
+++ Python_6.py
@@ -9,9 +9,8 @@ def parse_nested_parens(paren_string: str) -> List[int]:
         for char in group:
             if char == '(':
                 level += 1
-                if level > max_level:
-                    max_level = level
             elif char == ')':
                 level -= 1
+            max_level = max(max_level, level)
         result.append(max_level)
     return result
