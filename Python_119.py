71cb08c5023ec5e3a6384beb9f65c4d11c49ebe3
beep boop <githubbot@vadim.me>
2024-07-11 01:12:34 +0200

    Solution 73 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xrg5ajon Prompt: Program did not receive expected input

--- Python_119.py
+++ Python_119.py
@@ -13,5 +13,4 @@ def match_parens(lst):
 
 user_input = input("Enter a list of strings: ")
 lst = [s.strip() for s in user_input.split()]
-parsed_lst = [[c for c in l] for l in lst]
-print(match_parens(parsed_lst))
+print(match_parens(lst))
