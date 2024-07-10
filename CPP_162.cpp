3b800a7a0596432aea4c9473ce6aa79589b0dabf
beep boop <githubbot@vadim.me>
2024-07-10 23:34:54 +0200

    Solution 80 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/f1jr1l8u Prompt:    13 |     MD5_Init(&md5ctx);       |               ^~~~~~ 9050beca-e7e6-48a9-8700-2eb03bdd9564.cpp:13:5: error: ‘MD5_Init’ was not declare d in this scope    13 |     MD5_Init(&md5ctx);       |     ^~~~~~~~ 9050beca-e7e6-48a9-8700-2eb03bdd9564.cpp:14:5: error: ‘MD5_Update’ was not decla red in this scope    14 |     MD5_Update(&md5ctx, text.c_str(), text.size());       |     ^~~~~~~~~~ 9050beca-e7e6-48a9-8700-2eb03bdd9564.cpp:15:5: error: ‘MD5_Final’ was not declar ed in this scope    15 |     MD5_Final(md5, &md5ctx);       |     ^~~~~~~~~ 9050beca-e7e6-48a9-8700-2eb03bdd9564.cpp:18:15: error: ‘setfill’ was not declare d in this scope    18 |         ss << setfill('0') << setw(2) << hex << (int)md5[i];       |               ^~~~~~~ 9050beca-e7e6-48a9-8700-2eb03bdd9564.cpp:18:31: error: ‘setw’ was not declared i n this scope; did you mean ‘getw’?    18 |         ss << setfill('0') << setw(2) << hex << (int)md5[i];       |                               ^~~~       |                               getw

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,22 +1,24 @@
+#include <openssl/md5.h>
+#include <iomanip>
 #include <string>
 #include <sstream>
-#include <openssl/md5.h>
 
 using namespace std;
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
-
+    
+    stringstream ss;
     unsigned char md5[16];
+    
     MD5_CTX md5ctx;
     MD5_Init(&md5ctx);
     MD5_Update(&md5ctx, text.c_str(), text.size());
     MD5_Final(md5, &md5ctx);
-
-    stringstream ss;
+    
     for (int i = 0; i < 16; ++i) {
         ss << setfill('0') << setw(2) << hex << (int)(md5[i] & 0xFF);
     }
-
+    
     return ss.str();
 }
