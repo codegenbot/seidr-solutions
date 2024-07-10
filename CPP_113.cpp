25bc9019313e21125da75029384d992b27bb8819
beep boop <githubbot@vadim.me>
2024-07-10 19:21:35 +0200

    Solution 42 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/xjg4rs13 Prompt: In file included from /usr/include/c++/11/cassert:44,                  from 78771a10-86a3-44ed-813a-1df417ea1494.cpp:3: 78771a10-86a3-44ed-813a-1df417ea1494.cpp: In function ‘int main()’: 78771a10-86a3-44ed-813a-1df417ea1494.cpp:24:12: error: ‘issame’ was not declared  in this scope; did you mean ‘isspace’?    24 |     assert(issame(odd_count({"271", "137", "314"}), {       |            ^~~~~~ 78771a10-86a3-44ed-813a-1df417ea1494.cpp: At global scope: 78771a10-86a3-44ed-813a-1df417ea1494.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 78771a10-86a3-44ed-813a-1df417ea1494.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -4,18 +4,6 @@
 
 using namespace std;
 
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 vector<string> odd_count(vector<string> lst) {
     vector<string> result;
     for (string s : lst) {
@@ -31,11 +19,7 @@ vector<string> odd_count(vector<string> lst) {
 }
 
 int main() {
-    assert(issame(odd_count({"271", "137", "314"}), {
-        "the number of odd elements 2 in the string 271 of the input.",
-        "the number of odd elements 2 in the string 137 of the input.",
-        "the number of odd elements 2 in the string 314 of the input."
-    }));
+    assert(odd_count({"271", "137", "314"}) == vector<string>{"the number of odd elements 2 in the string 271 of the input.", "the number of odd elements 2 in the string 137 of the input.", "the number of odd elements 2 in the string 314 of the input."});
     
     return 0;
 }
