94278cd2b26e1892c2fc245e24ec1cb198f0ebaf
beep boop <githubbot@vadim.me>
2024-07-10 18:47:42 +0200

    Solution 9 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/5mdo215h Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ eaad0561-b8df-4fe4-b164-a8c4229e095c.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ eaad0561-b8df-4fe4-b164-a8c4229e095c.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from eaad0561-b8df-4fe4-b164-a8c4229e095c.cpp:5: eaad0561-b8df-4fe4-b164-a8c4229e095c.cpp: In function ‘int main()’: eaad0561-b8df-4fe4-b164-a8c4229e095c.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ eaad0561-b8df-4fe4-b164-a8c4229e095c.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -8,10 +8,10 @@ bool issame(std::vector<int> a, std::vector<int> b){
 
 std::vector<int> minPath(std::vector<std::vector<int>> grid, int k){
     std::vector<int> result;
-    for (int i = 0; i < k / 2; i++) {
+    for (int i = 0; i < k; ++i) {
         for (const auto& row : grid) {
-            for (const int& value : row) {
-                result.push_back(value);
+            for (int num : row) {
+                result.push_back(num);
             }
         }
     }
@@ -20,6 +20,6 @@ std::vector<int> minPath(std::vector<std::vector<int>> grid, int k){
 
 int main(){
     assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
-
+    
     return 0;
 }
