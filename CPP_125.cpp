ae3cd337845c853dd87756eb4f9f6962c64b9401
beep boop <githubbot@vadim.me>
2024-07-10 23:30:39 +0200

    Solution 53 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rho8j4le Prompt:       |                              ^~~~~~ bf67279d-9383-41b5-a8fe-432d035237ae.cpp:38:37: error: ‘string’ was not declared  in this scope    38 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bf67279d-9383-41b5-a8fe-432d035237ae.cpp:38:44: error: ‘b’ was not declared in t his scope    38 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bf67279d-9383-41b5-a8fe-432d035237ae.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ bf67279d-9383-41b5-a8fe-432d035237ae.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from bf67279d-9383-41b5-a8fe-432d035237ae.cpp:37: bf67279d-9383-41b5-a8fe-432d035237ae.cpp: In function ‘int main()’: bf67279d-9383-41b5-a8fe-432d035237ae.cpp:47:20: error: ‘split_words’ was not dec lared in this scope    47 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,28 +1,4 @@
+```cpp
 bool issame(vector<vector<string>> v1, vector<vector<string>> v2) {
     return v1==v2;
-}
-vector<string> split_words(string txt) {
-    vector<string> result;
-    bool inWord = false;
-    string word = "";
-
-    for (char c : txt) {
-        if (!inWord && !isalpha(c)) { 
-            if (c == ',') {
-                continue;
-            }
-            inWord = true;
-        } else if (inWord && !isalpha(c)) { 
-            result.push_back(word);
-            word = "";
-            inWord = false;
-        } else {
-            word += c;
-        }
-    }
-
-    if (inWord) {
-        result.push_back(word);
-    }
-    return result;
 }
