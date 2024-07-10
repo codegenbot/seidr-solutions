998355bf117267a7265f731c2ed26184e5c4e4d2
beep boop <githubbot@vadim.me>
2024-07-10 20:04:07 +0200

    Solution 8 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ptpr8f4c Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from fab6a727-81db-4a85-8e68-420de8fb79a9.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ fab6a727-81db-4a85-8e68-420de8fb79a9.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from fab6a727-81db-4a85-8e68-420de8fb79a9.cpp:19: fab6a727-81db-4a85-8e68-420de8fb79a9.cpp: In function ‘int main()’: fab6a727-81db-4a85-8e68-420de8fb79a9.cpp:29:20: error: ‘sorted_list_sum’ was not  declared in this scope    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ fab6a727-81db-4a85-8e68-420de8fb79a9.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,14 +1,17 @@
+```cpp
 #include <vector>
 #include <algorithm>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+typedef std::basic_string<char> string;
+
+bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
-        std::vector<std::string> temp = {a[i], b[i]};
-        std::sort(temp.begin(), temp.end());
+        vector<string> temp = {a[i], b[i]};
+        sort(temp.begin(), temp.end());
         if (temp[0] != temp[1]) {
             return false;
         }
@@ -16,9 +19,13 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return true;
 }
 
-std::vector<std::string> sorted_list_sum(std::vector<std::string> v) {
-    std::sort(v.begin(), v.end());
-    return v;
+string sorted_list_sum(vector<string> input) {
+    string result = "";
+    for (const auto& str : input) {
+        std::sort(str.begin(), str.end());
+        result += str;
+    }
+    return result;
 }
 
 int main() {
