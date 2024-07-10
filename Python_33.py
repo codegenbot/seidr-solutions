9147ee65764303f4461c5bdcbfabb645392fc0a8
beep boop <githubbot@vadim.me>
2024-07-10 23:27:34 +0200

    Solution 79 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zbhn643u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/454a5c4d-6cff-4035-8fe3-17cec3593 11d.py", line 15, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/454a5c4d-6cff-4035-8fe3-17cec3593 11d.py", line 10, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,8 +1,8 @@
 def sort_third(lst):
     numbers = [x for x in lst if not isinstance(x, list)]
-    lists = [x for x in lst if isinstance(x, list) and len(x) > 2]
+    lists = [x for x in lst if isinstance(x, list) and len(x) >= 3]
     
     numbers.sort()
-    lists.sort(key=lambda x: x[2])
+    lists.sort(key=lambda x: x[2] if len(x) > 2 else float('inf'))
     
     return numbers + lists
