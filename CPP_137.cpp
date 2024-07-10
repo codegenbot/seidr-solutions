92cc5840a051fcb8880fceeb8e30c3ccf73cb927
beep boop <githubbot@vadim.me>
2024-07-10 22:40:08 +0200

    Solution 4 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9snlnzo8 Prompt: 7b9cd2e4-a0e5-41c5-a06d-2facdb79f217.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,43 +1,40 @@
-#include <boost/variant.hpp>
+#include <string>
+#include <variant>
 
-boost::variant<int, double, std::string> compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return (int)a > (double)b ? boost::get<int>(a) : boost::get<boost::variant<int, double, std::string>>(b));
+using namespace std;
+
+template<typename... Ts>
+variant<Ts> compare_one(const variant<Ts>& a, const variant<Ts>& b) {
+    if (holds_alternative<int, double>(a) && holds_alternative<double, int>(b)) {
+        if (get_if<int>(&get<int>(a))) {
+            return (int)get<int>(a) > get<double>(b) ? a : b;
+        } else {
+            return (double)get<double>(a) > get<int>(b) ? a : b;
+        }
+    } else if (holds_alternative<string>(a) && holds_alternative<double, int>(b)) {
+        string str = get<string>(a);
+        double num = get<double>(b);
+        return str > to_string(num) ? a : b;
+    } else if (holds_alternative<double, int>(a) && holds_alternative<string>(b)) {
+        double num = get<double>(a);
+        string str = get<string>(b);
+        return to_string(num) > str ? a : b;
+    } else if (holds_alternative<string>(a) && holds_alternative<string>(b)) {
+        string str1 = get<string>(a);
+        string str2 = get<string>(b);
+        return str1 > str2 ? a : b;
+    } else if (holds_alternative<int, double>(a) && holds_alternative<string>(b)) {
+        int num = get<int>(a);
+        string str = get<string>(b);
+        return to_string(num) > str ? a : b;
+    } else if (holds_alternative<string>(a) && holds_alternative<int, double>(b)) {
+        string str = get<string>(a);
+        int num = get<int>(b);
+        return str > to_string(num) ? a : b;
+    } else if (holds_alternative<double, int>(a) && holds_alternative<double, int>(b)) {
+        double num1 = get<double>(a);
+        double num2 = get<double>(b);
+        return num1 > num2 ? a : b;
     }
-    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return (double)a > (int)b ? boost::get<boost::variant<int, double, std::string>>(a) : boost::get<int>(b));
-    }
-    else if (a.type() == typeid(std::string) && b.type() == typeid(double)) {
-        std::string str = boost::any_cast<std::string>(a);
-        double num = boost::any_cast<double>(b);
-        return str > std::to_string(num) ? boost::get<boost::variant<int, double, std::string>>(a) : boost::get<boost::variant<int, double, std::string>>(b));
-    }
-    else if (a.type() == typeid(double) && b.type() == typeid(std::string)) {
-        std::string str = boost::any_cast<std::string>(b);
-        double num = boost::any_cast<double>(a);
-        return std::to_string(num) > str ? boost::get<boost::variant<int, double, std::string>>(a) : boost::get<boost::variant<int, double, std::string>>(b));
-    }
-    else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        std::string str1 = boost::any_cast<std::string>(a);
-        std::string str2 = boost::any_cast<std::string>(b);
-        return str1 > str2 ? boost::get<boost::variant<int, double, std::string>>(a) : boost::get<boost::variant<int, double, std::string>>(b));
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(std::string)) {
-        int num = boost::any_cast<int>(a);
-        std::string str = boost::any_cast<std::string>(b);
-        return std::to_string(num) > str ? boost::get<boost::variant<int, double, std::string>>(a) : boost::get<boost::variant<int, double, std::string>>(b));
-    }
-    else if (a.type() == typeid(std::string) && b.type() == typeid(int)) {
-        int num = boost::any_cast<int>(b);
-        std::string str = boost::any_cast<std::string>(a);
-        return str > std::to_string(num) ? boost::get<boost::variant<int, double, std::string>>(a) : boost::get<boost::variant<int, double, std::string>>(b));
-    }
-    else if (a.type() == typeid(double) && b.type() == typeid(double)) {
-        double num1 = boost::any_cast<double>(a);
-        double num2 = boost::any_cast<double>(b);
-        return num1 > num2 ? boost::get<boost::variant<int, double, std::string>>(a) : boost::get<boost::variant<int, double, std::string>>(b));
-    }
-    else {
-        return boost::variant<int, double, std::string>();
-    }
+    return "None";
 }
