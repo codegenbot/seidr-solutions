9bead9897cf3f6897d27148b7ef85ca4f17bea5f
beep boop <githubbot@vadim.me>
2024-07-11 00:10:05 +0200

    Solution 28 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xx7jwekz Prompt: 1b5c6865-7a5d-425d-86f7-54c31996c124.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string words_in_sentence(string sentence) {       | ^~~~~~

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,9 +1,6 @@
-#include <string>
-using namespace std;
-
-string words_in_sentence(string sentence) {
-    vector<string> words = split(sentence, ' ');
-    string result;
+std::string words_in_sentence(std::string sentence) {
+    vector<std::string> words = split(sentence, ' ');
+    std::string result;
     
     for (int i = 0; i < words.size(); i++) {
         if (is_prime(words[i].length())) {
