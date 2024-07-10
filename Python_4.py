809c988e4d35824097289b714f3012059d4345d3
beep boop <githubbot@vadim.me>
2024-07-10 19:48:32 +0200

    Solution 47 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eqvqvyvk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/034fcea3-f212-4d14-9334-0e938ce67 b54.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_4.py
+++ Python_4.py
@@ -1,10 +1,7 @@
+```
 numbers = list(map(float, input("Enter numbers (space separated): ").split()))
-
-
-def mean_absolute_deviation(data):
-    mean = sum(data) / len(data)
-    deviation = [(x - mean) for x in data]
-    return sum([abs(x) for x in deviation]) / len(data)
-
-
+def mean_absolute_deviation(numbers):
+    average = sum(numbers) / len(numbers)
+    deviations = [(num - average) for num in numbers]
+    return sum(abs(x) for x in deviations) / len(deviations)
 print(mean_absolute_deviation(numbers))
