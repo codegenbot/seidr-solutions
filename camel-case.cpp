e787768adf12c9395e8c77f090a9c71ae74b58f7
beep boop <githubbot@vadim.me>
2024-07-10 06:07:15 +0200

    Solution 32 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ywihod1a Prompt:   152 | extern int remove (const char *__filename) __THROW;       |                    ~~~~~~~~~~~~^~~~~~~~~~ b40c6f62-1d38-4ff7-b94b-93c1eaac43c5.cpp:26:36: error: cannot convert ‘std::__cx x11::basic_string<char>::iterator’ to ‘const char*’    26 |         s.erase(std::remove(s.begin(), s.end(), ' '), s.end());       |                             ~~~~~~~^~       |                                    |       |                                    std::__cxx11::basic_string<char>::ite rator In file included from /usr/include/c++/11/cstdio:42,                  from /usr/include/c++/11/ext/string_conversions.h:43,                  from /usr/include/c++/11/bits/basic_string.h:6608,                  from /usr/include/c++/11/string:55,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from b40c6f62-1d38-4ff7-b94b-93c1eaac43c5.cpp:1: /usr/include/stdio.h:152:32: note:   initializing argument 1 of ‘int remove(cons t char*)’   152 | extern int remove (const char *__filename) __THROW;       |                    ~~~~~~~~~~~~^~~~~~~~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,4 +1,3 @@
-```
 #include <iostream>
 #include <string>
 
@@ -23,8 +22,8 @@ std::string camelCase(const std::string& s) {
 int main() {
     std::string s;
     while (std::cin >> s) {
-        s.erase(std::remove(s.begin(), s.end(), ' '), s.end()) - s.begin();
-        s.erase(std::remove(s.begin(), s.end(), '-'), s.end()) - s.begin();
+        s.erase(std::remove(s.begin(), s.end(), '-'), s.end());
+        s.erase(std::remove(s.begin(), s.end(), ' ') - 1);
         std::cout << camelCase(s) << '\n';
     }
     return 0;
