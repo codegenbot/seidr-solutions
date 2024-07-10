b8bdabacbe6f6fcb1fbc0a644a3dbf366c5559ab
beep boop <githubbot@vadim.me>
2024-07-10 08:11:40 +0200

    Solution 39 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/jtwmj2dd Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,28 +1,45 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+vector<vector<int>> cutVector(vector<int>& vec) {
     int min_diff = INT_MAX;
-    int split_index = -1;
+    vector<vector<int>> result;
     
-    for (int i = 0; i < v.size(); i++) {
+    for (int i = 1; i < vec.size(); ++i) {
         int left_sum = 0, right_sum = 0;
         
-        for (int j = 0; j < i; j++)
-            left_sum += v[j];
-        for (int j = i + 1; j < v.size(); j++)
-            right_sum += v[j];
+        for (int j = 0; j < i; ++j) {
+            left_sum += vec[j];
+        }
         
-        if (left_sum == right_sum) {
-            return {{v[0], v.begin() + 1, v.end()}}, {v.begin(), v.end() - 1};
+        for (int j = i; j < vec.size(); ++j) {
+            right_sum += vec[j];
         }
         
         int diff = abs(left_sum - right_sum);
-        if (diff < min_diff) {
+        
+        if (diff <= min_diff) {
             min_diff = diff;
-            split_index = i;
+            result = {{vec.begin(), vec.begin() + i}, {vec.begin() + i, vec.end()}};
         }
     }
     
-    return {{v[0], v.begin() + 1, v.end()}}, {v.begin(), v.end() - 1};
+    return result;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (auto &x : vec) {
+        cin >> x;
+    }
+    vector<vector<int>> res = cutVector(vec);
+    for (const auto &v : res) {
+        for (int x : v) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
