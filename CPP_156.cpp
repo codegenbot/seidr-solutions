08aa43c35ec4237d19782805ff13a606b3a77110
beep boop <githubbot@vadim.me>
2024-07-10 20:33:27 +0200

    Solution 33 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/mma41iux Prompt:       |     ^~~~ 49adb4dd-738e-4859-a258-2060f0565a7d.cpp:7:5: error: expected unqualified-id bef ore ‘return’     7 |     return 0;       |     ^~~~~~ 49adb4dd-738e-4859-a258-2060f0565a7d.cpp:8:1: error: expected declaration before  ‘}’ token     8 | }       | ^ 49adb4dd-738e-4859-a258-2060f0565a7d.cpp:11:10: error: ‘int main()’ redeclared a s different kind of entity    11 | int main(){       |          ^ 49adb4dd-738e-4859-a258-2060f0565a7d.cpp:1:5: note: previous declaration ‘int ma in’     1 | int main       |     ^~~~ In file included from 49adb4dd-738e-4859-a258-2060f0565a7d.cpp:10: 49adb4dd-738e-4859-a258-2060f0565a7d.cpp: In function ‘int main()’: 49adb4dd-738e-4859-a258-2060f0565a7d.cpp:12:13: error: ‘int_to_mini_romank’ was not declared in this scope    12 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,8 +1,10 @@
-int main 
-{
+```cpp
+int main() {
     int num;
     cout << "Enter a number: ";
     cin >> num;
     cout << "The Roman numeral for " << num << " is " << int_to_mini_roman(num) << endl;
     return 0;
 }
+
+string int_to_mini_roman(int num);
