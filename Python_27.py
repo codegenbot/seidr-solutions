21a9e594805b862cf21a8de2b6cf7996a4c2e928
beep boop <githubbot@vadim.me>
2024-07-10 11:37:40 +0200

    Solution 65 of Python/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/af8adk34 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ddc30ce3-41eb-4cfb-915e-dbb35cfd1 8fe.py", line 1     return string.swapcase()     ^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_27.py
+++ Python_27.py
@@ -1,9 +1,6 @@
 def swapcase_string(string):
     return string.swapcase()
 
-# Input
 input_string = input("Enter a string: ")
-
-# Output
 output_string = swapcase_string(input_string)
 print(output_string)
