0b7be5eea9ceadad898151c20d13ec9e8b18100f
beep boop <githubbot@vadim.me>
2024-07-10 19:51:54 +0200

    Solution 24 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bpxrryax Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3f3506aa-574d-49b4-b184-6e9e3fec0 180.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_41.py
+++ Python_41.py
@@ -1,3 +1,3 @@
-```
-def car_race_collision(n: int):
+```python
+def """car_race_collision: The number of collisions between cars at an intersection is the sum of the number of ways each car can collide with every other car. Given n as the number of cars, this function calculates the total number of collisions."""(n: int):
     return sum(i * (n - i) for i in range(1, n))
