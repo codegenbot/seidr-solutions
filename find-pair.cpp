014ba8b6443b3eaffb3252913475d973dd05bd2b
beep boop <githubbot@vadim.me>
2024-07-10 18:47:30 +0200

    Solution 7 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/zfkjj0jk Prompt:     1 | #include <vector>   +++ |+#include <unordered_map>     2 | using namespace std; 093d87a6-400e-47cb-ab29-74f6153691c3.cpp:5:19: error: expected primary-expressio n before ‘int’     5 |     unordered_map<int, int> numMap;       |                   ^~~ 093d87a6-400e-47cb-ab29-74f6153691c3.cpp:8:13: error: ‘numMap’ was not declared in this scope     8 |         if (numMap.find(complement) != numMap.end()) {       |             ^~~~~~ 093d87a6-400e-47cb-ab29-74f6153691c3.cpp:11:9: error: ‘numMap’ was not declared in this scope    11 |         numMap[nums[i]] = i;       |         ^~~~~~ 093d87a6-400e-47cb-ab29-74f6153691c3.cpp:13:11: error: ‘invalid_argument’ was no t declared in this scope    13 |     throw invalid_argument("No pair found");       |           ^~~~~~~~~~~~~~~~ 093d87a6-400e-47cb-ab29-74f6153691c3.cpp:14:1: warning: control reaches end of n on-void function [-Wreturn-type]    14 | }       | ^

--- find-pair.cpp
+++ find-pair.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 pair<int, int> findPair(const vector<int>& nums, int target) {
-    std::unordered_map<int, int> numMap;
+    unordered_map<int, int> numMap;
+    numMap = unordered_map<int, int>();
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
         if (numMap.find(complement) != numMap.end()) {
