7d43303591a2b2001e18fd25e99086fbca2bb24c
beep boop <githubbot@vadim.me>
2024-07-10 06:37:09 +0200

    Solution 61 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lerjqzl3 Prompt:    23 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ a7f4c7b6-e077-405a-aaf7-3b7df12b17ef.cpp:23:36: error: expected primary-expressi on before ‘float’    23 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ a7f4c7b6-e077-405a-aaf7-3b7df12b17ef.cpp:23:43: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from a7f4c7b6-e077-405a-aaf7-3b7df12b17ef.cpp:22: a7f4c7b6-e077-405a-aaf7-3b7df12b17ef.cpp: In function ‘int main()’: a7f4c7b6-e077-405a-aaf7-3b7df12b17ef.cpp:32:20: error: ‘sort_even’ was not decla red in this scope    32 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ a7f4c7b6-e077-405a-aaf7-3b7df12b17ef.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,22 +1,8 @@
 ```cpp
 bool issame(vector<float> a, vector<float> b) {
-    bool result = true;
-    if(a.size() != b.size()) {
-        result = false;
-    } else {
-        for(int i = 0; i < a.size(); i++) {
-            if(std::abs(a[i] - b[i]) > 1e-9) {
-                result = false;
-                break;
-            }
-        }
-    }
-    return result;
-}
-
-int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
-    return 0;
+    if(a.size()!=b.size())return false;
+    for(int i=0;i<a.size();i++)if(abs(a[i]-b[i])>0.00001)return false;
+    return true;
 }
 
 vector<float> sort_even(vector<float> l) {
@@ -38,4 +24,9 @@ vector<float> sort_even(vector<float> l) {
         }
     }
     return result;
+}
+
+int main() {
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    return 0;
 }
