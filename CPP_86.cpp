d1cae23f72d94875f49811cc726825b126e33ae4
beep boop <githubbot@vadim.me>
2024-07-10 07:07:21 +0200

    Solution 27 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3q16x72n Prompt: 7756b400-9b85-479a-8c2d-b20e3deb0f2a.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string anti_shuffle(string s){       | ^~~~~~ In file included from 7756b400-9b85-479a-8c2d-b20e3deb0f2a.cpp:19: 7756b400-9b85-479a-8c2d-b20e3deb0f2a.cpp: In function ‘int main()’: 7756b400-9b85-479a-8c2d-b20e3deb0f2a.cpp:21:13: error: ‘anti_shuffle’ was not de clared in this scope    21 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,19 +1,21 @@
+#include <iostream>
 #include <string>
 #include <algorithm>
 
-string anti_shuffle(string s){
-    string result = "";
+std::string anti_shuffle(std::string s){
+    std::string result = "";
     for(int i=0; i<s.length(); i++){
         if(s[i] == ' '){
             result += ' ';
             continue;
         }
-        string word = "";
+        std::string word = "";
         for(int j=i; j<=s.length()-1 && s[j] != ' '; j++){
             word += s[j];
         }
         i = j - 1;
-        sort(word.begin(), word.end());
+        std::sort(word.begin(), word.end());
         result += word;
     }
     return result;
+}
