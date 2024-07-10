ade1e9fdc76188a9d8b3a153677c7be13697ae36
beep boop <githubbot@vadim.me>
2024-07-10 07:36:55 +0200

    Solution 24 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/s6j49414 Prompt:       |                              ^~~~~~ d1e5bbd0-9285-4831-9ff7-4becb66015d2.cpp:31:37: error: ‘string’ was not declared  in this scope    31 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d1e5bbd0-9285-4831-9ff7-4becb66015d2.cpp:31:44: error: ‘b’ was not declared in t his scope    31 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d1e5bbd0-9285-4831-9ff7-4becb66015d2.cpp:31:45: error: expression list treated a s compound expression in initializer [-fpermissive]    31 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d1e5bbd0-9285-4831-9ff7-4becb66015d2.cpp:30: d1e5bbd0-9285-4831-9ff7-4becb66015d2.cpp: In function ‘int main()’: d1e5bbd0-9285-4831-9ff7-4becb66015d2.cpp:40:20: error: ‘reverse_delete’ was not declared in this scope    40 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ d1e5bbd0-9285-4831-9ff7-4becb66015d2.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,26 +1,9 @@
-vector<string> reverse_delete(string s, string c) {
-    vector<string> result;
-    string temp = "";
-    
-    for(int i=0; i<s.length(); i++) {
-        bool found = false;
-        for(int j=0; j<c.length(); j++) {
-            if(s[i] == c[j]) {
-                found = true;
-                break;
-            }
-        }
-        
-        if(!found) {
-            temp += s[i];
-        }
+bool issame(vector<string> a, vector<string> b) {
+    if(a.size() != b.size())
+        return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i])
+            return false;
     }
-    
-    string temp2 = temp;
-    reverse(temp.begin(), temp.end());
-    
-    result.push_back(temp);
-    result.push_back((temp==temp2)?"True":"False");
-    
-    return result;
+    return true;
 }
