f18814022b53c9ba18d2eeed41f0ca2f3c1fe200
beep boop <githubbot@vadim.me>
2024-07-11 00:24:03 +0200

    Solution 20 of CPP/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/8trf56ge Prompt: Complete the following code given the task description and function signature.

--- CPP_39.cpp
+++ CPP_39.cpp
@@ -1,39 +1,36 @@
-#include <iostream>
+#include <stdio.h>
+#include <vector>
 
 using namespace std;
 
-bool isPrime(int num) {
-    if (num <= 1)
-        return false;
-    for (int i = 2; i * i <= num; i++)
-        if (num % i == 0)
-            return false;
-    return true;
-}
-
 int prime_fib(int n) {
-    int a = 0, b = 1, fib = 0;
-    int count = 0;
-
-    while (true) {
-        fib = a + b;
+    vector<int> fib(1);
+    int a = 0, b = 1;
+    for (int i = 2; i <= n; ++i) {
+        int temp = a + b;
         a = b;
-        b = fib;
-
-        if (isPrime(fib)) {
-            count++;
-            if (count == n)
-                return fib;
-        }
+        b = temp;
+        fib.push_back(b);
     }
+    for (int i = 0; i < fib.size(); ++i) {
+        bool isPrime = true;
+        if (fib[i] == 1)
+            continue;
+        for (int j = 2; j * j <= fib[i]; ++j) {
+            if (fib[i] % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime)
+            return fib[i];
+    }
+    return -1;
 }
 
 int main() {
-    cout << prime_fib(1) << endl;
-    cout << prime_fib(2) << endl;
-    cout << prime_fib(3) << endl;
-    cout << prime_fib(4) << endl;
-    cout << prime_fib(5) << endl;
-
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", prime_fib(n));
     return 0;
 }
