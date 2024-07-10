568c300f87d4e1bd7c173e04297456e85375134a
beep boop <githubbot@vadim.me>
2024-07-10 23:21:44 +0200

    Solution 5 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/p2ju98kx Prompt: 551be15e-bac0-4327-9be7-b8818606689c.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 551be15e-bac0-4327-9be7-b8818606689c.cpp:23: 551be15e-bac0-4327-9be7-b8818606689c.cpp: In function ‘int main()’: 551be15e-bac0-4327-9be7-b8818606689c.cpp:25:13: error: ‘file_name_check’ was not  declared in this scope    25 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -14,5 +14,5 @@ string file_name_check(string file_name){
         }
     }
     if(count > 3 || !foundDot) return "No";
-    if(file_name[0] < 'a' || (file_name[0] > 'z' && file_name[0] < 'A') || (file_name[0] > 'Z')) return "No";
+    if((file_name[0] < 'a' && file_name[0] != '0') || (file_name[0] > 'z' && file_name[0] < 'A') || (file_name[0] > 'Z')) return "No";
     return "Yes";
