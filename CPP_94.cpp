7ef9932fa0063e3a09051830e361c1a2e7a80e72
beep boop <githubbot@vadim.me>
2024-07-11 00:01:39 +0200

    Solution 92 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k7a6o2at Prompt: b1381feb-fb33-4ae3-a3df-8cc3a35eef1e.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ b1381feb-fb33-4ae3-a3df-8cc3a35eef1e.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from b1381feb-fb33-4ae3-a3df-8cc3a35eef1e.cpp:18: b1381feb-fb33-4ae3-a3df-8cc3a35eef1e.cpp: In function ‘int main()’: b1381feb-fb33-4ae3-a3df-8cc3a35eef1e.cpp:20:13: error: ‘skjkasdkd’ was not decla red in this scope    20 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ^~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <assert.h>
+
 int sumVector(std::vector<int> lst) {
     int total = 0;
     for (int i : lst) {
@@ -7,15 +10,7 @@ int sumVector(std::vector<int> lst) {
 
 }
 
-int calculateSum(std::vector<int> numbers) {
-    int total = 0;
-    for (int number : numbers) {
-        total += number;
-    }
-    return total;
-}
-
 int main() {
-    assert(calculateSum({127, 97, 8192}) == 1216);
+    assert(sumVector({127, 97, 8192}) == 1216);
     return 0;
 }
