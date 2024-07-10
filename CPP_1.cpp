26c72ef87b6cb25e27f08e78de77ed19dab36209
beep boop <githubbot@vadim.me>
2024-07-10 12:23:29 +0200

    Solution 24 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ppgg1t3g Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 42ce730d-7f5a-4813-923b-305eec79adc0.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 42ce730d-7f5a-4813-923b-305eec79adc0.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 42ce730d-7f5a-4813-923b-305eec79adc0.cpp:25: 42ce730d-7f5a-4813-923b-305eec79adc0.cpp: In function ‘int main()’: 42ce730d-7f5a-4813-923b-305eec79adc0.cpp:35:20: error: ‘separate_paren_groups’ w as not declared in this scope    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 42ce730d-7f5a-4813-923b-305eec79adc0.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,25 +1,3 @@
 bool issame(vector<string> a, vector<string> b){
     return a == b;
 }
-vector<string> result;
-string current_group;
-int open_braces = 0;
-
-for (char c : paren_string) {
-    if (c == '(') {
-        if (open_braces > 0) {
-            current_group += c;
-        }
-        open_braces++;
-    } else if (c == ')') {
-        open_braces--;
-        if (open_braces > 0) {
-            current_group += c;
-        } else if (open_braces == 0) {
-            result.push_back(current_group);
-            current_group = "";
-        }
-    }
-}
-
-return result;
