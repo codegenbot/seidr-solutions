c2ecc017b095356a9af2788bd7c0eafcafad9073
beep boop <githubbot@vadim.me>
2024-07-10 17:22:08 +0200

    Solution 96 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/e8sl3wil Prompt: Program did not receive expected input

--- Python_49.py
+++ Python_49.py
@@ -1,13 +1,20 @@
-n = 0
-p = 0
+import random
 
-while True:
-    try:
-        n = int(input("Enter the value of n: "))
-        p = int(input("Enter the value of p: "))
-        break
-    except ValueError:
-        print("Invalid input. Please enter an integer.")
+
+def modp(n: int, p: int):
+    if pow(2, n - 1, p) == 1:
+        return False
+    for _ in range(p - 2):
+        a = random.randint(2, p - 2)
+        if pow(a, n, p) == 1:
+            continue
+        if (a ** ((n - 1) // 2)) % p != p - 1:
+            return False
+    return True
+
+
+n = int(input("Enter the value of n: "))
+p = int(input("Enter the value of p: "))
 
 result = modp(n, p)
 
