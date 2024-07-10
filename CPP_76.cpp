258d69004c0007cc4f5097b1fc6ce8bb6096a513
beep boop <githubbot@vadim.me>
2024-07-10 03:49:59 +0200

    Solution 33 of CPP/76, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gib6hfmu Prompt: a9de30ee-967b-49d0-9fb9-75436bb5e0eb.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ a9de30ee-967b-49d0-9fb9-75436bb5e0eb.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~

--- CPP_76.cpp
+++ CPP_76.cpp
@@ -1,11 +1,10 @@
 ```
 #include <cmath>
 #include <iostream>
-#include <string>
 
 bool is_simple_power(int x, int n);
 
-bool is_simple_power(int x, int n) {
+bool is_simple_power(int x, int int) {
     double y = pow(n, log(x) / log(n));
     return (y == x);
 }
