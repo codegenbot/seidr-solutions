bb80ee83bae74a0e51d716926ebc70105b6ce588
beep boop <githubbot@vadim.me>
2024-07-10 10:01:38 +0200

    Solution 97 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/s3wxl4yb Prompt:       |                           ^~~~~~ c15f66d0-24e3-4039-8998-bf118d13e88f.cpp:19:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from c15f66d0-24e3-4039-8998-bf118d13e88f.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from c15f66d0-24e3-4039-8998-bf118d13e88f.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c15f66d0-24e3-4039-8998-bf118d13e88f.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c15f66d0-24e3-4039-8998-bf118d13e88f.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ c15f66d0-24e3-4039-8998-bf118d13e88f.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -10,7 +10,3 @@ std::vector<int> sort_array(std::vector<int> arr) {
     std::sort(arr.begin(), arr.end());
     return arr;
 }
-
-int main() {
-    assert(issame(sort_array({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
-}
