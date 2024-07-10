92e6c80553c553628cc59d6721f31ff1e62b512e
beep boop <githubbot@vadim.me>
2024-07-10 06:02:38 +0200

    Solution 29 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/0pt0o0pg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/df4b5231-bc3a-4040-9b57-10e90afdb eb7.py", line 4     return [string[: i + 1] for i in range(len(string)]                                                       ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '('

--- Python_14.py
+++ Python_14.py
@@ -1,12 +1,7 @@
-import sys
 from typing import List
 
 def all_prefixes(string: str) -> List[str]:
-    return [string[: i + 1] for i in range(len(string)]
+    return [string[:i + 1] for i in range(len(string))]
 
-try:
-    while True:
-        input_string = sys.stdin.readline().strip()
-        print(all_prefixes(input_string))
-except EOFError:
-    pass
+input_string = input()
+print(all_prefixes(input_string))
