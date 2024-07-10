2b5235eea6a9302e63c767249cbbc71d44e74020
beep boop <githubbot@vadim.me>
2024-07-10 20:45:54 +0200

    Solution 34 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ptpr8f4c Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 03117d67-5e30-46fb-aa55-addd167c2996.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 03117d67-5e30-46fb-aa55-addd167c2996.cpp:19:44: error: ‘b’ was not declared in t his scope    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 03117d67-5e30-46fb-aa55-addd167c2996.cpp:18: 03117d67-5e30-46fb-aa55-addd167c2996.cpp: In function ‘int main()’: 03117d67-5e30-46fb-aa55-addd167c2996.cpp:28:20: error: ‘sorted_list_sum’ was not  declared in this scope    28 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 03117d67-5e30-46fb-aa55-addd167c2996.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,17 +1,28 @@
-```
 #include <algorithm>
 #include <string>
 #include <vector>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> other) {
-    if (a.size() != other.size()) return false;
+using namespace std;
+
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); i++) {
-        if (a[i] + other[i] != sortListSum({{a[i], other[i]}})[0]) return false;
+        if (a[i] + b[i] != sortListSum({{a[i], b[i]}})[0]) return false;
     }
     return true;
 }
 
-std::vector<std::string> sortListSum(std::vector<std::string> v) {
-    std::sort(v.begin(), v.end());
-    return v;
+vector<string> sortListSum(vector<pair<string, string>> v) {
+    sort(v.begin(), v.end());
+    vector<string> result;
+    for (auto p : v) {
+        result.push_back(p.first + p.second);
+    }
+    return result;
+}
+
+int main() {
+    assert(issame({{"aaaa", "bbbb"}, {"dd", "cc"}}, 
+                  {{"cc", "dd"}, {"aaaa", "bbbb"}}));
+    return 0;
 }
