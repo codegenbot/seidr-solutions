33ba680ebccf6527101cdfe6b4a204aa88045288
beep boop <githubbot@vadim.me>
2024-07-10 21:56:51 +0200

    Solution 12 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rho8j4le Prompt:       |                              ^~~~~~ 6e074841-f1e8-48c2-8c67-2aa73a78e000.cpp:29:37: error: ‘string’ was not declared  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 6e074841-f1e8-48c2-8c67-2aa73a78e000.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6e074841-f1e8-48c2-8c67-2aa73a78e000.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 6e074841-f1e8-48c2-8c67-2aa73a78e000.cpp:28: 6e074841-f1e8-48c2-8c67-2aa73a78e000.cpp: In function ‘int main()’: 6e074841-f1e8-48c2-8c67-2aa73a78e000.cpp:38:20: error: ‘split_words’ was not dec lared in this scope    38 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 6e074841-f1e8-48c2-8c67-2aa73a78e000.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,39 +1,7 @@
-```cpp
-#include <vector>
-#include <string>
-#include <cctype>
-
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    return a == b;
-}
-
-std::vector<std::string> split_words(string txt) {
-    vector<string> result;
-    bool inWord = false;
-    string word = "";
-
-    for (char c : txt) {
-        if (!inWord && !isalnum(c)) {
-            // Check for commas
-            if (c == ',') {
-                continue;
-            }
-            inWord = true;
-        } else if (inWord && !std::isalnum(c)) {
-            result.push_back(word);
-            word = "";
-            inWord = false;
-        } else {
-            word += c;
-        }
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) return false;
     }
-
-    if (inWord) {
-        result.push_back(word);
-    }
-    return result;
-}
-
-int main() {
-    assert(issame(split_words(""), {"0"}));
+    return true;
 }
