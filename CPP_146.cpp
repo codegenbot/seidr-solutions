058146a6102b7c536dd63900bf3614616083e325
beep boop <githubbot@vadim.me>
2024-07-10 07:06:16 +0200

    Solution 95 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/rhv60fcv Prompt: ic_string<char>*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {std::__cxx11::basic_string<char, std::char_traits<char>, std::allo cator<char> >}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator< std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::ve ctor<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/vector.tcc:121:21:   required from ‘std::vector<_Tp, _A lloc>::reference std::vector<_Tp, _Alloc>::emplace_back(_Args&& ...) [with _Args  = {std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char > >}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx 11::basic_string<char> >; std::vector<_Tp, _Alloc>::reference = std::__cxx11::ba sic_string<char>&]’ /usr/include/c++/11/bits/stl_vector.h:1204:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(std::vector<_Tp, _Alloc>::value_type&&) [with _Tp = std ::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11::basic_strin g<char> >; std::vector<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<cha r>]’ c4591329-faac-4717-995f-bc1094f070a8.cpp:33:27:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::__cxx11::basic_string<char> >’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <vector>
 
-int specialFilter(const std::vector<std::string>& nums) {
+int specialFilter(const std::vector<std::string>& v) {
     int count = 0;
-    for (const auto& num : nums) {
-        if (std::stoi(num).length() % 2 != 0) {
-            ++count;
+    for (const auto& str : v) {
+        if (std::stoi(str) % 2 == 1) {
+            count++;
         }
     }
     return count;
@@ -19,19 +19,12 @@ void userInput() {
     while (true) {
         getline(std::cin, num);
         if (num == "stop") break;
-        int n;
-        std::string temp = num;
-        if(temp.length() > 1000000){ 
+        if(num.length() > 1000000){ 
             std::cout << "String too long. Please enter a shorter one." << std::endl;
             continue; 
         }
-        n = std::stoi(temp);
-        if (temp.length() > 1000000) { 
-            std::cout << "String too long. Please enter a shorter one." << std::endl;
-            continue; 
-        } else {
-            nums.push_back(std::to_string(n)); 
-        }
+        int n = std::stoi(num);
+        nums.push_back(std::to_string(n)); 
     }
     std::cout << "Number of special numbers: " << specialFilter(nums) << std::endl;
 }
