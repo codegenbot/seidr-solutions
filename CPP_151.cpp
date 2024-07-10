968f6d11cd60b5bd15130067bb9ebf857aa87bdd
beep boop <githubbot@vadim.me>
2024-07-10 12:01:49 +0200

    Solution 56 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/53d0gwcb Prompt: 96899699-ca25-4fe6-8fb7-4e71159deb6c.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 96899699-ca25-4fe6-8fb7-4e71159deb6c.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 96899699-ca25-4fe6-8fb7-4e71159deb6c.cpp:25: 96899699-ca25-4fe6-8fb7-4e71159deb6c.cpp: In function ‘int main()’: 96899699-ca25-4fe6-8fb7-4e71159deb6c.cpp:27:35: error: ‘lst’ was not declared in  this scope    27 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 96899699-ca25-4fe6-8fb7-4e71159deb6c.cpp:27:43: error: ‘odd_sum’ was not declare d in this scope    27 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -4,7 +4,7 @@
 
 float double_the_difference(std::vector<float>& lst) {
     float odd_sum = 0.0;
-
+    
     for (float num : lst) {
         if (num > 0 && num == (int)num && (int)num % 2 != 0) {
             odd_sum += num;
