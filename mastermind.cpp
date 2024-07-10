068d1cbb6198f0a2e0a5bed12cf9774f0ffb6269
beep boop <githubbot@vadim.me>
2024-07-10 18:41:58 +0200

    Solution 6 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/0p7z3hmy Prompt: n this scope    28 |     cin >> code >> guess;       |            ^~~~ 5cbd396a-e770-436f-b0af-52a11dd3effa.cpp:28:20: error: ‘guess’ was not declared in this scope    28 |     cin >> code >> guess;       |                    ^~~~~ 5cbd396a-e770-436f-b0af-52a11dd3effa.cpp:29:5: error: ‘cout’ was not declared in  this scope    29 |     cout << numWhitePegs(code, guess) << endl;       |     ^~~~ 5cbd396a-e770-436f-b0af-52a11dd3effa.cpp:29:25: error: ‘numWhitePegs’ cannot be used as a function    29 |     cout << numWhitePegs(code, guess) << endl;       |             ~~~~~~~~~~~~^~~~~~~~~~~~~ 5cbd396a-e770-436f-b0af-52a11dd3effa.cpp:29:42: error: ‘endl’ was not declared i n this scope    29 |     cout << numWhitePegs(code, guess) << endl;       |                                          ^~~~ 5cbd396a-e770-436f-b0af-52a11dd3effa.cpp:30:25: error: ‘numBlackPegs’ cannot be used as a function    30 |     cout << numBlackPegs(code, guess) << endl;       |             ~~~~~~~~~~~~^~~~~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,25 +1,9 @@
-#include <string>
-using namespace std;
-
-int mastermind(string code, string guess) {
-    int white = 0;
-    int black = 0;
-
-    for(int i=0; i<4; i++) {
-        if(code[i] == guess[i]) {
-            black++;
-        }
-    }
-
-    for(int i=0; i<6; i++) {
-        int count = 0;
-        for(int j=0; j<4; j++) {
-            if(guess[j] == (char)(i+65) && code.find((char)(i+65)) != string::npos) {
-                count++;
-            }
-        }
-        white += min(count, black);
-    }
-
-    return black + (4 - black);
+int main() {
+    string code, guess;
+    int numBlackPegsResult, numWhitePegsResult;
+    cin >> code >> guess;
+    numBlackPegsResult = numBlackPegs(code, guess);
+    cout << numWhitePegs(code, guess) << endl;
+    cout << numBlackPegsResult << endl;
+    return 0;
 }
