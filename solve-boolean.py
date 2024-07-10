71f2a1d31a5ecfaa0d75401c4a61ac3c30e89636
beep boop <githubbot@vadim.me>
2024-07-10 19:43:12 +0200

    Solution 47 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/toqfs3kb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/93759efb-b096-4242-a683-089578ad3 2af.py", line 1     return eval(''.join(['T' if c == 'T' else 'False' for c in expression]).repl ace('&', lambda s: 'and' + (' True' if s == 'True' else ' False') ))     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,2 +1,2 @@
 def solve_boolean(expression):
-    return eval(''.join(['T' if c == 'T' else 'False' for c in expression]).replace('&', lambda s: 'and' + (' True' if s == 'True' else ' False') ))
+    return eval(''.join(['T' if c == 'T' else 'False' for c in expression]).replace('&', lambda s: ' and' if s == 'True' else ' and False'))
