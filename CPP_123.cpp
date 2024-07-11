6efc86477f4d7918d23c0e9a1b6fba130b9999f9
beep boop <githubbot@vadim.me>
2024-07-11 05:51:04 +0200

    Solution 58 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/xv6zce7z Prompt: Complete the following code given the task description and function signature.

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,8 +1,7 @@
-vector<int> get_odd_collatz(int n){
-    vector<int> result;
-    while(n != 1){
+vector<int> result;
+    while (n != 1) {
         result.push_back(n);
-        if(n % 2 == 0){
+        if (n % 2 == 0) {
             n = n / 2;
         } else {
             n = 3 * n + 1;
@@ -10,14 +9,12 @@ vector<int> get_odd_collatz(int n){
     }
     result.push_back(1);
     
-    vector<int> odd_result;
-    for(int num : result){
-        if(num % 2 != 0){
-            odd_result.push_back(num);
+    vector<int> odd_numbers;
+    for (int num : result) {
+        if (num % 2 != 0) {
+            odd_numbers.push_back(num);
         }
     }
     
-    sort(odd_result.begin(), odd_result.end());
-    
-    return odd_result;
-}
+    sort(odd_numbers.begin(), odd_numbers.end());
+    return odd_numbers;
