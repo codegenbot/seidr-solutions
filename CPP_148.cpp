bddd594ab4a920d8dd85a2c4877aa361fd4d6517
beep boop <githubbot@vadim.me>
2024-07-11 10:50:54 +0200

    Solution 57 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9t4atg8f Prompt:       |                                           ^ 0d31ddac-df51-4857-b442-b5dabf96cce7.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0d31ddac-df51-4857-b442-b5dabf96cce7.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 0d31ddac-df51-4857-b442-b5dabf96cce7.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~ In file included from 0d31ddac-df51-4857-b442-b5dabf96cce7.cpp:13: 0d31ddac-df51-4857-b442-b5dabf96cce7.cpp: In function ‘int main()’: 0d31ddac-df51-4857-b442-b5dabf96cce7.cpp:23:20: error: ‘bf’ was not declared in this scope    23 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 0d31ddac-df51-4857-b442-b5dabf96cce7.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -5,16 +5,14 @@
 using namespace std;
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
+    if (a.size() != b.size())
         return false;
-    }
     
     for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
+        if (a[i] != b[i])
             return false;
-        }
     }
-    
+
     return true;
 }
 
