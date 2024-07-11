00461f463169eb953b29286caea2fe273fc53bb8
beep boop <githubbot@vadim.me>
2024-07-11 13:51:19 +0200

    Solution 75 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mq03fasb Prompt:       | ^~~~~~~~~~~~~ ce9a9d5f-eb4a-46e7-b944-5e940c22b19b.cpp:21:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    21 | int main(){       |         ^~ ce9a9d5f-eb4a-46e7-b944-5e940c22b19b.cpp:21:9: note: remove parentheses to defau lt-initialize a variable    21 | int main(){       |         ^~       |         -- ce9a9d5f-eb4a-46e7-b944-5e940c22b19b.cpp:21:9: note: or replace parentheses with  braces to value-initialize a variable ce9a9d5f-eb4a-46e7-b944-5e940c22b19b.cpp:21:11: error: a function-definition is not allowed here before ‘{’ token    21 | int main(){       |           ^ ce9a9d5f-eb4a-46e7-b944-5e940c22b19b.cpp:23:2: error: expected ‘}’ at end of inp ut    23 | }       |  ^ ce9a9d5f-eb4a-46e7-b944-5e940c22b19b.cpp:6:33: note: to match this ‘{’     6 | string int_to_roman(int number) {       |                                 ^

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,4 +1,4 @@
-```c
+```c++
 #include <vector>
 #include <string>
 
@@ -17,12 +17,11 @@ string int_to_roman(int number) {
         }
     }
     return roman;
-
 }
 
 int main() {
     int number;
-    cin >> number;
-    cout << int_to_roman(number) << endl;
+    cin >> number; 
+    cout << int_to_roman(number) << endl; 
     return 0;
 }
