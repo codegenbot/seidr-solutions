d35f356e36643f5baec297fa721d158990c8e96b
beep boop <githubbot@vadim.me>
2024-07-11 17:18:39 +0200

    Solution 62 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/juryj07u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2f5f8f74-b1b1-4077-981a-5436a0182 bfc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,16 +1,11 @@
-```
-def double_the_difference(lst):
-    if len(lst) < 2:
-        return "Invalid input. Please provide at least two integers."
-    
+def double_the_difference():
+    lst = list(map(int, input().split()))
     difference = max(lst) - min(lst)
     double_diff = difference * 2
-    return str(double_diff)
+    return double_diff
 
-
-def check(func, args=""):
-    print(func(args))
-
+def check(func):
+    print(func())
 
 if __name__ == "__main__":
-    check(double_the_difference, input().split())
+    check(double_the_difference)
