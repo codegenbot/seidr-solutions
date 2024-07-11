576364ecff15143d853536e0acbbbfb314c46bec
beep boop <githubbot@vadim.me>
2024-07-11 14:52:41 +0200

    Solution 65 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/01a7l32z Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -1,12 +1,11 @@
-```
 def find_zero(xs):
     if type(xs) != list or len(xs) < 2:
-        return 
+        return
     if xs[0] != 0:
-        return 
+        return
     for x in xs[1:]:
         if x != 0:
-            return 
+            return
     if len(set(xs[1:])) > 1:
-        return 
+        return
     return -xs[-1] / (2 * sum(xs[1:]))
