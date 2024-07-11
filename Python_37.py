571e645fe0f65506b42125fd7836335952c12284
beep boop <githubbot@vadim.me>
2024-07-12 00:15:41 +0200

    Solution 48 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/nmo4hme9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/18fdf41d-ef68-484a-bd91-8e0d0c7dd 959.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/18fdf41d-ef68-484a-bd91-8e0d0c7dd 959.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,4 +1,9 @@
-def sort_even(l: list):
-    evens = sorted([x for x in l if x % 2 == 0])
-    odds = sorted([x for x in l if x % 2 != 0])
-    return evens + odds
+def sort_even(lst):
+    return [
+        (
+            x
+            if i % 2 != 0
+            else sorted([y for j, y in enumerate(lst) if (j := i) // 2 * 2 == j])[0]
+        )
+        for i, x in enumerate(lst)
+    ]
