1deaf4f586480f7b5ce9a4d7202609e175d96b8b
beep boop <githubbot@vadim.me>
2024-07-11 18:05:40 +0200

    Solution 4 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/v9cxlkxs Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/615310e9-553b-4944-9297-64e46c04d c0a.py", line 17, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/615310e9-553b-4944-9297-64e46c04d c0a.py", line 13, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,7 +2,7 @@ def encode(message):
     encoded_message = ''
     for char in message:
         if char.lower() in 'aeiou':
-            encoded_message += chr(ord(char) + 2).upper()
+            encoded_message += chr((ord(char) + 2 - 97) % 26 + 97).upper()
         else:
             encoded_message += char.swapcase()
-    return encoded_message.lower()
+    return encoded_message
