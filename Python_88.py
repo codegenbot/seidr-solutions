6de1a63054657a4b92c28698d8a0fd81f9bd41d0
beep boop <githubbot@vadim.me>
2024-07-11 11:12:31 +0200

    Solution 40 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/er6nmf1f Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/63f68fc6-b75d-45b0-9f37-63d8e6739 10c.py", line 17, in <module>     check(sort_array)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/63f68fc6-b75d-45b0-9f37-63d8e6739 10c.py", line 14, in check     assert sort_array([21, 14, 23, 11]) == [23, 21, 14, 11], "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_88.py
+++ Python_88.py
@@ -3,6 +3,8 @@ def sort_array(array):
     n = len(sorted_array)
 
     if n % 2 != 0:
-        return [sorted_array[n//2-1], sorted_array[n//2]]
+        return [sorted_array[n//2]]
     else:
-        return []
+        mid1 = (n // 2) - 1
+        mid2 = n // 2
+        return [sorted_array[mid1], sorted_array[mid2]]
