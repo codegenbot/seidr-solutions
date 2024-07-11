28e78a934f4176407f154f66cb0c32a10edb79db
beep boop <githubbot@vadim.me>
2024-07-11 02:40:54 +0200

    Solution 59 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ctv3qlto Prompt: Complete the following code given the task description and function signature.

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -8,7 +8,7 @@ int sum = 0;
     }
     
     for (int i = 0; i < q.size() / 2; i++) {
-        if (q[i] != q[q.size() - i - 1]) {
+        if (q[i] != q[q.size() - 1 - i]) {
             return false;
         }
     }
