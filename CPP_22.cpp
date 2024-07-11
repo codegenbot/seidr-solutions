db31cb79a251bf8064045a82d1ba45b9d2e39e44
beep boop <githubbot@vadim.me>
2024-07-11 06:45:31 +0200

    Solution 98 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2dqf0nts Prompt:       |         ^~ e494bd1f-8d74-433b-98f2-50051e4a4199.cpp:39:9: note: remove parentheses to defau lt-initialize a variable    39 | int main(){       |         ^~       |         -- e494bd1f-8d74-433b-98f2-50051e4a4199.cpp:39:9: note: or replace parentheses with  braces to value-initialize a variable e494bd1f-8d74-433b-98f2-50051e4a4199.cpp:39:11: error: a function-definition is not allowed here before ‘{’ token    39 | int main(){       |           ^ e494bd1f-8d74-433b-98f2-50051e4a4199.cpp:41:2: error: expected ‘}’ at end of inp ut    41 | }       |  ^ e494bd1f-8d74-433b-98f2-50051e4a4199.cpp:14:53: note: to match this ‘{’    14 | bool issame(std::vector<int> a, std::vector<int> b) {       |                                                     ^ e494bd1f-8d74-433b-98f2-50051e4a4199.cpp:41:2: warning: control reaches end of n on-void function [-Wreturn-type]    41 | }       |  ^

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,7 +1,6 @@
-```cpp
+```c++
 #include <vector>
 #include <list>
-#include <boost/any.hpp>
 
 std::vector<int> filter_integers(std::list<boost::any> values) {
     std::vector<int> result;
@@ -28,3 +27,4 @@ int main
 {
     assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}));
     return 0;
+}
