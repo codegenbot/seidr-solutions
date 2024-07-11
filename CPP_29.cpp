fae5f5f70e54bfd4e2e6d33df6855a05ac70640e
beep boop <githubbot@vadim.me>
2024-07-11 12:33:49 +0200

    Solution 50 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xbr14c3t Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:10: f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp: In function ‘int main()’: f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,6 +1,3 @@
-#include <vector>
-#include <string>
-
 bool issame(vector<string> a, vector<string>b) {
     if(a.size() != b.size()) return false;
     for(int i=0; i<a.size(); i++) {
@@ -13,6 +10,5 @@ int main() {
     vector<string> strings = {"xxx", "asd", "xxy", "john doe", "xxxAAC", "xxx"};
     string prefix = "xxx";
     vector<string> result = filter_by_prefix(strings, prefix);
-    bool same = issame(result, vector<string>{prefix, prefix + "AAA"});
-    return 0;
+    assert(issame(result, {"xxx", "xxxAAA", "xxx"}));
 }
