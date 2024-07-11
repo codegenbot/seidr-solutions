c53700b72527aac00c13a5f85cea35c2ff5bc71a
beep boop <githubbot@vadim.me>
2024-07-11 04:35:15 +0200

    Solution 72 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wg5nozkz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bc1f78c5-1364-4cfd-bd1b-8a4931f58 836.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_124.py
+++ Python_124.py
@@ -1,27 +1,22 @@
-```Python
+```
 def valid_date(date):
     day, month, year = date.split('-')
     if len(day) == 1:
         day = '0' + day
     if len(month) == 1:
         month = '0' + month
-    
     try:
         int(year)
-        if not (int(month) >= 1 and int(month) <= 12):
+        if not (1 <= int(month) <= 12):
             return False
         if int(month) in [4,6,9,11]:
-            if int(day) > 30 or int(day) < 1:
+            if 1 > int(day) or int(day) > 30:
                 return False
         elif int(month) == 2:
-            if int(year) % 4 != 0:
-                if int(day) > 28 or int(day) < 1:
-                    return False
-            else:
-                if int(day) > 29 or int(day) < 1:
-                    return False
+            if not ((int(year) % 4 == 0 and 1 < int(day) < 29) or (int(year) % 4 != 0 and 1 < int(day) < 28)):
+                return False
         else:
-            if int(day) > 31 or int(day) < 1:
+            if 1 > int(day) or int(day) > 31:
                 return False
     except ValueError:
         return False
