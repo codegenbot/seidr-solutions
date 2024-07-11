269a10b30cad46ba626ad8d3827576822605537e
beep boop <githubbot@vadim.me>
2024-07-11 12:20:55 +0200

    Solution 61 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/l6tw0a3u Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 925e9370-9184-4cbf-8637-c79e0890885e.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -21,7 +21,7 @@ vector<string> sorted_list_sum(vector<string> lst) {
 
     // Sort the resulted list by length and then alphabetically
     std::sort(result.begin(), result.end(),
-              [](const string& a, const string& b) {
+              [&](const string& a, const string& b) {
                   if (a.length() != b.length())
                       return a.length() - b.length();
                   else
@@ -32,7 +32,5 @@ vector<string> sorted_list_sum(vector<string> lst) {
 }
 
 int main() {
-    vector<string> test = {"aaaa", "bbbb", "dd", "cc"};
-    assert(issame(sorted_list_sum(test), {"ccc", "dd", "aaaa", "bbbb"}));
-    return 0;
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"ccc", "dd", "aaaa", "bbbb"}));
 }
