abe7371966607133fd330479bd5585629ed542dc
beep boop <githubbot@vadim.me>
2024-07-11 14:56:10 +0200

    Solution 68 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/q4phxnzk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bb26bdc2-01d1-4ae6-b159-e079bdeb1 e3f.py", line 19, in <module>     check(filter_by_prefix)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bb26bdc2-01d1-4ae6-b159-e079bdeb1 e3f.py", line 14, in check     assert filter_by_prefix(            ^^^^^^^^^^^^^^^^^ TypeError: filter_by_prefix() takes from 0 to 1 positional arguments but 2 were given

--- Python_29.py
+++ Python_29.py
@@ -1,8 +1,10 @@
 def filter_by_prefix(prefix: str = '') -> None:
     while True:
         try:
-            strings = input("Enter strings (comma-separated): ").split(",")
-            filtered_strings = [s.strip() for s in strings if len(strings) > 1 and s.strip().startswith(prefix)]
+            prefix_input = input("Enter prefix: ")
+            strings = input(f"Enter strings (comma-separated): ").split(",")
+
+            filtered_strings = [s.strip() for s in strings if len(strings) > 1 and s.strip().startswith(prefix_input)]
 
             print(filtered_strings)
             break
