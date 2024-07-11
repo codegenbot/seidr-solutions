9b905a7c6ac8e538dc2e25921f1cf2f7e3d6d14e
beep boop <githubbot@vadim.me>
2024-07-11 03:18:26 +0200

    Solution 11 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wg5nozkz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f64c0315-8891-4ee4-903a-e41aaf8f5 e6a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_124.py
+++ Python_124.py
@@ -1,21 +1,23 @@
 def valid_date(date):
     day, month, year = map(int, date.split('-'))
-    if (month < 1 or month > 12) or (day < 1):
+    if not (1 <= year <= 9999):
         return False
-    if month in [4,6,9,11]:
-        if day > 30:
-            return False
-    elif month == 2:
-        if year % 4 != 0 and day > 29:
-            return False
-        elif year % 100 != 0 and day > 28:
-            return False
-        elif year % 400 != 0 and day > 29:
-            return False
+    if not (1 <= month <= 12):
+        return False
+    if not (1 <= day <= 31):
+        return False
+    if month in [4,6,9,11] and day > 30:
+        return False
+    if month == 2:
+        if year % 4 != 0:
+            return day <= 28
         else:
-            if day > 29:
-                return False
-    else:
-        if day > 31:
-            return False
-    return True
+            if year % 100 != 0:
+                return day <= 29
+            elif year % 400 != 0:
+                return day <= 28
+            else:
+                return day <= 29
+    elif month in [1,3,5,7,8,10,12]:
+        return day <= 31
+    return False
