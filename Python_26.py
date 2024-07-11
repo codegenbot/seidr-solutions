e7691f3ccfc01e000d7e8270a81c26457c8130ab
beep boop <githubbot@vadim.me>
2024-07-11 10:09:39 +0200

    Solution 87 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7s9yk3dz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d5253e3b-ea2a-4711-89ef-58fa0decb 23f.py", line 11, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d5253e3b-ea2a-4711-89ef-58fa0decb 23f.py", line 8, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_26.py
+++ Python_26.py
@@ -1,6 +1,7 @@
+```
 def remove_duplicates(input_string):
     if isinstance(input_string, list):
         input_string = ','.join(map(str, input_string))
     
     input_list = [int(x) for x in input_string.split(",")]
-    return list(set(input_list))
+    return sorted(set(input_list))
