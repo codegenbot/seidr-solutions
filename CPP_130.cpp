0e62a6c2ff19360fd7d6a2c00eab1c12017cd5bd
beep boop <githubbot@vadim.me>
2024-07-11 09:08:53 +0200

    Solution 21 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/j9jhcy8s Prompt:       |                    ^~~ 592c6d34-d498-4140-a348-d5e9a8285cc7.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 592c6d34-d498-4140-a348-d5e9a8285cc7.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 592c6d34-d498-4140-a348-d5e9a8285cc7.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 592c6d34-d498-4140-a348-d5e9a8285cc7.cpp:18: 592c6d34-d498-4140-a348-d5e9a8285cc7.cpp: In function ‘int main()’: 592c6d34-d498-4140-a348-d5e9a8285cc7.cpp:28:20: error: ‘tri’ was not declared in  this scope    28 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ 592c6d34-d498-4140-a348-d5e9a8285cc7.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,5 +1,19 @@
 #include <vector>
 
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
 vector<int> tri(int n) {
     vector<int> result;
     if (n >= 0) {
@@ -16,8 +30,4 @@ vector<int> tri(int n) {
         }
     }
     return result;
-}
-
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
 }
