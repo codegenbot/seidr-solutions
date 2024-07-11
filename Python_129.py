ba33741045b82f32cf253275dac0214dba215580
beep boop <githubbot@vadim.me>
2024-07-11 14:52:23 +0200

    Solution 14 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/82odhmj0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f2beb4ba-a10d-448f-9b42-6e9001bce 44e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -2,27 +2,24 @@
 def minPath(grid, k):
     m = len(grid)
     visited = set()
-    visited_cells = set()
 
-    def dfs(i, j, path, remaining, visited_cells):
+    def dfs(i, j, path, remaining):
         if remaining == 0:
             return path
         if (i, j) in visited:
             return None
         visited.add((i, j))
-        if (i, j) not in visited_cells:
-            for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                new_i, new_j = i + x, j + y
-                if 0 <= new_i < m and 0 <= new_j < m:
-                    new_path = path + [grid[new_i][new_j]]
-                    res = dfs(new_i, new_j, new_path, remaining - 1, visited_cells | {(i, j)})
-                    if res:
-                        return res
+        res = None
+        for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            new_i, new_j = i + x, j + y
+            if 0 <= new_i < m and 0 <= new_j < m:
+                new_path = path + [grid[new_i][new_j]]
+                res = min(res, dfs(new_i, new_j, new_path, remaining-1)) if res else dfs(new_i, new_j, new_path, remaining-1)
         visited.remove((i, j))
-        return None
+        return res
 
     for i in range(m):
         for j in range(m):
-            path = dfs(i, j, [], m * m - k, set())
+            path = dfs(i, j, [], m*m - k)
             if path:
                 return path
