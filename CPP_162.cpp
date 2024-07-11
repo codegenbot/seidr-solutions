de0d7acd8a86d494adde4f2725bf4d5c421be502
beep boop <githubbot@vadim.me>
2024-07-11 03:26:37 +0200

    Solution 39 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fcqijk62 Prompt: 332672ec-731e-4e64-9e18-20624a5b6451.cpp:4:10: fatal error: sha1.h: No such file  or directory     4 | #include "sha1.h"       |          ^~~~~~~~ compilation terminated.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -6,14 +6,13 @@ int main() {
     std::getline(std::cin, s);
     
     CryptoPP::SHA1 sha;
-    unsigned char md5[20];
-    unsigned char* ptr = sha.Calculate((const unsigned char*)s.c_str(), (int)s.size(), md5);
+    unsigned char md5[16];
+    sha.Calculate((const unsigned char*)s.c_str(), (int)s.size(), md5);
 
     for(int i = 0; i < 16; ++i) {
-        std::cout << ((ptr[i] * 4 > 9 ? std::hex : std::nospace)
-            << std::setfill('0') << std::setw(2) << (int)(ptr[i]));
+        std::cout << std::hex << std::setfill('0') << std::setw(2) << (int)(md5[i]);
     }
-    
+
     std::cout << "The SHA1 is: " << std::endl;
     
     return 0;
