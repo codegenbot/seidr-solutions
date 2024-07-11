89ea3bb047a5522cc0d553fb6b09daf10978b51e
beep boop <githubbot@vadim.me>
2024-07-11 12:04:00 +0200

    Solution 11 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hv4j11wl Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,46 +1,30 @@
-map<string,int> num_map = {
-    {"zero", 0},
-    {"one", 1},
-    {"two", 2},
-    {"three", 3},
-    {"four", 4},
-    {"five", 5},
-    {"six", 6},
-    {"seven", 7},
-    {"eight", 8},
-    {"nine", 9}
-};
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 string sort_numbers(string numbers) {
-    vector<string> num_list;
-    string temp;
+    vector<string> numVec;
+    map<string, int> numberMap;
 
-    for(int i = 0; i < numbers.length(); i++) {
-        if(i == numbers.length() - 1 || (numbers[i] != ' ' && numbers[i + 1] == ' ')) {
-            temp = "";
-            int j = i;
-            while(j > 0 && numbers[j-1] != ' ') {
-                temp = numbers[j-1] + temp;
-                j--;
+    for (int i = 0; i <= 9; i++) {
+        if (i == 0)
+            numberMap["zero"] = i;
+        else
+            numberMap[to_string(i)] = i;
+        if (i < 7) {
+            string str = to_string(i);
+            while (str.length() < 3) {
+                str = "0" + str;
             }
-            num_list.push_back(temp);
-            i += temp.length();
+            numberMap[str] = i;
         }
     }
 
-    sort(num_list.begin(), num_list.end());
-    string result = "";
-    for(int i = 0; i < num_list.size(); i++) {
-        if(result != "") {
-            result += " ";
-        }
-        for(auto &num : num_map) {
-            if(num.first == num_list[i]) {
-                result += num.first;
-                break;
-            }
-        }
+    string[] nums = numbers.split(' ');
+    for (int i = 0; i < nums.length; i++) {
+        numVec.add(nums[i]);
     }
-
-    return result;
+    sort(numVec);
+    return join(numVec, " ");
 }
