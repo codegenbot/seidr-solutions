0576b855154d7c9a10da2b6d85e306ad216bc7da
beep boop <githubbot@vadim.me>
2024-07-11 10:36:25 +0200

    Solution 2 of Python/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5zdmh3ux Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/07e7564b-5885-466a-83fb-73ac3fdf0 219.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_86.py
+++ Python_86.py
@@ -3,6 +3,6 @@ def anti_shuffle(s):
     words = s.split()
     result = []
     for word in words:
-        new_word = ''.join(sorted(word, reverse=True))
+        new_word = ''.join(sorted(word))
         result.append(new_word)
     return ' '.join(result)
