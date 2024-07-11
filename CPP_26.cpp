1cc7c8cdd911ca82cad37b286d3f7c0e220e9227
beep boop <githubbot@vadim.me>
2024-07-11 04:49:22 +0200

    Solution 10 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7auz6hs3 Prompt:       |                           ^~~~~~ b3f903cb-8d37-4f2c-b0c2-1539d536c83f.cpp:19:27: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? b3f903cb-8d37-4f2c-b0c2-1539d536c83f.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b3f903cb-8d37-4f2c-b0c2-1539d536c83f.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b3f903cb-8d37-4f2c-b0c2-1539d536c83f.cpp:18: b3f903cb-8d37-4f2c-b0c2-1539d536c83f.cpp: In function ‘int main()’: b3f903cb-8d37-4f2c-b0c2-1539d536c83f.cpp:28:20: error: ‘remove_duplicates’ was n ot declared in this scope    28 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ b3f903cb-8d37-4f2c-b0c2-1539d536c83f.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,14 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
+#include<bits/stdc++.h>
 using namespace std;
 
-bool isSame(vector<int> v1, vector<int> v2) {
-    if (v1.size() != v2.size())
-        return false;
-    for (int i = 0; i < v1.size(); i++) {
-        if (v1[i] != v2[i])
-            return false;
+bool isSame(vector<int> &v1,vector<int>& v2){
+    if(v1.size()!=v2.size())return false;
+    for(int i=0;i<v1.size();i++){
+        if(find(v2.begin(),v2.end(),v1[i]) == v2.end()) return false;
     }
     return true;
 }
@@ -28,27 +24,8 @@ vector<int> remove_duplicates(vector<int> numbers) {
 }
 
 int main() {
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-
-    vector<int> numbers(n);
-
-    for (int i = 0; i < n; i++) {
-        cout << "Enter element " << i+1 << ": ";
-        cin >> numbers[i];
-    }
-
-    vector<int> res = remove_duplicates(numbers);
-    
-    if (!res.empty()) {
-        cout << "Duplicates removed: ";
-        for (int num : res) {
-            cout << num << " ";
-        }
-        cout << endl;
-    } else
-        cout << "No duplicates found" << endl;
-
+    vector<int> nums1={1,2,3};
+    vector<int> nums2={2,2,3};
+    cout << (isSame(remove_duplicates(nums1),remove_duplicates(nums2)) ? "Yes" : "No")<<endl;
     return 0;
 }
