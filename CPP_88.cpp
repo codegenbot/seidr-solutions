a5e7095dbf44f9a4cfddc5c01a37d130b1f167cc
beep boop <githubbot@vadim.me>
2024-07-11 05:06:37 +0200

    Solution 39 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fpqidjs7 Prompt:       |                           ^~~~~~ 1fdf4531-3a86-4f08-b114-2a761241e88e.cpp:24:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 1fdf4531-3a86-4f08-b114-2a761241e88e.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1fdf4531-3a86-4f08-b114-2a761241e88e.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1fdf4531-3a86-4f08-b114-2a761241e88e.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1fdf4531-3a86-4f08-b114-2a761241e88e.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 1fdf4531-3a86-4f08-b114-2a761241e88e.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,7 +1,6 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
-#include <iostream>
 
 std::vector<int> sort_array(std::vector<int> array) {
     int sum = array.front() + array.back();
@@ -15,4 +14,9 @@ std::vector<int> sort_array(std::vector<int> array) {
 
 bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
+}
+
+int main() {
+    assert(issame(sort_array({21, 14, 23, 11}), {23, 21, 14, 11}));
+    return 0;
 }
