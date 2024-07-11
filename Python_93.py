8b476778cb7d13bcbc69e96518a0cc3b95eef842
beep boop <githubbot@vadim.me>
2024-07-11 19:44:18 +0200

    Solution 52 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/v9cxlkxs Prompt: i dqnT kNoW wHaT To wRiTe Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae4e7ed5-c68d-4d15-8fd8-fe435a4a3 fa1.py", line 19, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae4e7ed5-c68d-4d15-8fd8-fe435a4a3 fa1.py", line 15, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,7 +1,7 @@
 def encode(message):
     encoded_message = ""
     for char in message:
-        if char.islower() and char.lower() in "aeiou":
+        if char.lower() in "aeiou":
             encoded_message += chr((ord(char) - ord('a') + 2) % 26 + ord('a'))
         else:
             encoded_message += char.swapcase()
