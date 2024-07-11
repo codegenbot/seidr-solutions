c39801f7c049c116e717beab7bf3bdc586c7f049
beep boop <githubbot@vadim.me>
2024-07-11 18:13:45 +0200

    Solution 13 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/5mxvjosi Prompt: In file included from /usr/include/c++/11/vector:67,                  from fc101110-1f5f-4045-b69d-c0e893a8c0ac.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from fc101110-1f5f-4045-b69d-c0e893a8c0ac.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ fc101110-1f5f-4045-b69d-c0e893a8c0ac.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fc101110-1f5f-4045-b69d-c0e893a8c0ac.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fc101110-1f5f-4045-b69d-c0e893a8c0ac.cpp:13: fc101110-1f5f-4045-b69d-c0e893a8c0ac.cpp: In function ‘int main()’: fc101110-1f5f-4045-b69d-c0e893a8c0ac.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -6,16 +6,6 @@ bool issame(const std::pair<T, T>& a, const std::pair<T, T>& b) {
     return a == b;
 }
 
-std::pair<int, int> sum_product(const std::vector<int>& numbers) {
-    int sum = 0;
-    int product = 1;
-    for (int num : numbers) {
-        sum += num;
-        product *= num;
-    }
-    return {sum, product};
-}
-
 int main() {
     assert(issame(sum_product({10}), {10, 10}));
     return 0;
