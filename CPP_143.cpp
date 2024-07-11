530eda2a30ab5f16337dca1bcca51d09a5c72712
beep boop <githubbot@vadim.me>
2024-07-11 04:57:57 +0200

    Solution 38 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/i0trhc1x Prompt: Complete the following code given the task description and function signature.

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,26 +1,39 @@
 string words_in_sentence(string sentence){
     string result = "";
-    int primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-    for (int i = 0; i < sentence.size(); i++) {
-        if (i > 0) {
-            result += " ";
+    int n = 0;
+    for(int i=2; ;i++){
+        if(isPrime(i)){
+            n++;
+            if(n > sentence.size()) break;
         }
-        string word = "";
-        while (i < sentence.size() && sentence[i] != ' ') {
-            word += sentence[i];
-            i++;
-        }
-        int len = word.length();
-        bool isPrime = false;
-        for (int j = 0; j < sizeof(primes) / sizeof(primes[0]); j++) {
-            if (primes[j] == len) {
-                isPrime = true;
-                break;
-            }
-        }
-        if (isPrime) {
+    }
+    for(auto word : split(sentence, " ")) {
+        if(word.size() == n) {
             result += word + " ";
         }
     }
-    return result;
+    return result.substr(0, result.size()-1);
+}
+
+vector<string> split(const string& str, const string& delimiter) {
+    vector<string> tokens;
+    size_t pos = 0;
+    while ((pos = str.find(delimiter)) != string::npos) {
+        tokens.push_back(str.substr(0, pos));
+        str.erase(0, pos + delimiter.size());
+    }
+    tokens.push_back(str);
+    return tokens;
+}
+
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
