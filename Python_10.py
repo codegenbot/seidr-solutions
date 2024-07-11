53bdd15b08dbc426d400f4538d3aa9f57703a705
beep boop <githubbot@vadim.me>
2024-07-11 08:42:23 +0200

    Solution 58 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/31odiee1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae2ed049-b907-47c0-93de-0c6d4c509 7d9.py", line 26, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae2ed049-b907-47c0-93de-0c6d4c509 7d9.py", line 23, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae2ed049-b907-47c0-93de-0c6d4c509 7d9.py", line 5, in make_palindrome     if s[i] != s[j]:                ~^^^ IndexError: list index out of range

--- Python_10.py
+++ Python_10.py
@@ -1,6 +1,19 @@
 def make_palindrome(s: str) -> str:
     s = list(s)
     i, j = 0, len(s) - 1
+    if i < j and s[i] != s[j]:
+        for c in set(s):
+            left, right = 0, len(s) - 1
+            while left < right and s[left] != c:
+                left += 1
+            while left < right and s[right] != c:
+                right -= 1
+            if left < right:
+                s.pop(left)
+                j -= 1
+                break
+        else:
+            s.pop(j)
     while i < j:
         if s[i] != s[j]:
             for c in set(s):
@@ -10,14 +23,11 @@ def make_palindrome(s: str) -> str:
                 while left < right and s[right] != c:
                     right -= 1
                 if left < right:
-                    for k in range(i + 1):
-                        if s[k] == c:
-                            i, j = k, len(s) - 1
-                            break
-                    for k in range(j, -1, -1):
-                        if s[k] == c:
-                            i, j = 0, k
-                            break
+                    s.pop(left)
+                    j -= 1
+                    break
+            else:
+                s.pop(j)
         else:
             i += 1
             j -= 1
