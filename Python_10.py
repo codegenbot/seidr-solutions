6720a6d468a157046ded150dc7a38bc68667986d
beep boop <githubbot@vadim.me>
2024-07-11 14:03:52 +0200

    Solution 48 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c4exytmv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/889bcb21-1ea8-49e3-8250-82bf42610 239.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,27 +1,28 @@
-def make_palindrome(s: str) -> str:
-    if s == s[::-1]:
-        return s
-    left = list(s)
-    right = list(s)[::-1]
+```
+def make_palindrome(string: str) -> str:
+    if string == string[::-1]:
+        return string
+
+    left = list(string)
+    right = list(string)[::-1]
 
     i, j = 0, len(right) - 1
-
     while i < len(left):
         if left[i] != right[j]:
             if j > 0:
-                if left[0] == right[j - 1]:
+                if left[0] == right[j-1]:
                     left.insert(0, right[j])
                     break
                 else:
-                    left.insert(0, right[j - 1])
+                    left.insert(0, right[j-1])
                     j -= 1
             else:
                 if len(left) >= 2 and left[0] != right[0]:
                     left.insert(0, right[0])
                 else:
-                    left.insert(0, "a")
+                    left.insert(0, 'a')
         else:
             i += 1
             j -= 1
 
-    return "".join(left + s)
+    return "".join(left + list(string))
