6b442749f1b9bbd46420129e962f9890782b86df
beep boop <githubbot@vadim.me>
2024-07-11 04:19:10 +0200

    Solution 38 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/497575qq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,55 +1,39 @@
 #include <boost/any.hpp>
 #include <string>
-#include <vector>
+#include <algorithm>
 
-using namespace boost;
+using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_any_of<a>(std::string("int")) && is_any_of<b>(std::string("int"))) {
-        int x = boost::any_cast<int>(a);
-        int y = boost::any_cast<int>(b);
-        return (x > y) ? a : ((x < y) ? b : boost::any(std::string("None")));
-    } else if (is_any_of<a>(std::string("float")) && is_any_of<b>(std::string("float"))) {
-        float x = boost::any_cast<float>(a);
-        float y = boost::any_cast<float>(b);
-        return (x > y) ? a : ((x < y) ? b : boost::any(std::string("None")));
-    } else if (is_any_of<a>(std::string("std::string")) && is_any_of<b>(std::string("std::string"))) {
-        std::string x = boost::any_cast<std::string>(a);
-        std::string y = boost::any_cast<std::string>(b);
-        return (x > y) ? a : ((x < y) ? b : boost::any(std::string("None")));
-    } else if (is_any_of<a>(std::string("int")) && is_any_of<b>(std::string("float"))) {
-        int x = boost::any_cast<int>(a);
-        float y = boost::any_cast<float>(b);
-        return (x > y) ? a : ((x < y) ? b : boost::any(std::string("None")));
-    } else if (is_any_of<a>(std::string("float")) && is_any_of<b>(std::string("int"))) {
-        float x = boost::any_cast<float>(a);
-        int y = boost::any_cast<int>(b);
-        return (x > y) ? a : ((x < y) ? b : boost::any(std::string("None")));
-    } else if (is_any_of<a>(std::string("int")) && is_any_of<b>(std::string("std::string"))) {
-        int x = boost::any_cast<int>(a);
-        std::string y = boost::any_cast<std::string>(b);
-        return (x > convert_to_int(y)) ? a : ((x < convert_to_int(y)) ? b : boost::any(std::string("None")));
-    } else if (is_any_of<a>(std::string("float")) && is_any_of<b>(std::string("std::string"))) {
-        float x = boost::any_cast<float>(a);
-        std::string y = boost::any_cast<std::string>(b);
-        return (x > convert_to_float(y)) ? a : ((x < convert_to_float(y)) ? b : boost::any(std::string("None")));
-    } else {
-        return boost::any(std::string("None"));
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return max(a.convert_to<int>(), b.convert_to<float>());
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        string str = to_string(b.convert_to<string>());
+        if (str.find('.') != string::npos || str.find(',') != string::npos)
+            return a;
+        else
+            return b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if (a.convert_to<string>() > b.convert_to<string>())
+            return a;
+        else if (a.convert_to<string>() < b.convert_to<string>())
+            return b;
+        else
+            return boost::any("None");
+    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        string str = to_string(a.convert_to<int>());
+        if (str.find('.') != string::npos || str.find(',') != string::npos)
+            return b;
+        else
+            return boost::any("None");
+    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        string str = to_string(b.convert_to<int>());
+        if (str.find('.') != string::npos || str.find(',') != string::npos)
+            return a;
+        else
+            return boost::any("None");
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return max(a, boost::any(b.convert_to<int>()));
     }
-}
-
-int convert_to_int(const std::string& s) {
-    int x;
-    if (std::istringstream(s) >> x)
-        return x;
-    else
-        throw std::runtime_error("Invalid integer string");
-}
-
-float convert_to_float(const std::string& s) {
-    float x;
-    if (std::istringstream(s) >> x)
-        return x;
-    else
-        throw std::runtime_error("Invalid float string");
+    return boost::any();
 }
