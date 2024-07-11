1033bef5bb2901f740b3818518c5341a2d993460
beep boop <githubbot@vadim.me>
2024-07-11 13:45:11 +0200

    Solution 30 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c4exytmv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/43492c7f-bf73-4d59-9b38-0e3df086f f3b.py", line 25, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/43492c7f-bf73-4d59-9b38-0e3df086f f3b.py", line 22, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,21 +1,18 @@
 ```
-def make_palindrome(string: str) -> str:
-    if string == string[::-1]:
-        return string
+def make_palindrome(s: str) -> str:
+    if s == s[::-1]:
+        return s
 
-    left = list(string)
-    right = "".join(list(reversed(string)))
+    left = list(s)
+    right = s[::-1]
 
     i, j = 0, len(right) - 1
     while i < len(left):
         if left[i] != right[j]:
-            if j > 0:
-                left.insert(0, right[j-1])
-                j -= 1
-            else:
-                left.insert(0, right[0])
+            left.insert(0, right[j])
+            j -= 1
         else:
             i += 1
             j -= 1
 
-    return "".join(left) + right
+    return "".join(left)
