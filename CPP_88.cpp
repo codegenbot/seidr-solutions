61ad26f4c463a7c678368b3b629a566f4b385eff
beep boop <githubbot@vadim.me>
2024-07-11 05:36:54 +0200

    Solution 53 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fpqidjs7 Prompt:       |                           ^~~~~~ 38ff643c-f712-4acb-904f-9a776c817cf7.cpp:24:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 38ff643c-f712-4acb-904f-9a776c817cf7.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 38ff643c-f712-4acb-904f-9a776c817cf7.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 38ff643c-f712-4acb-904f-9a776c817cf7.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 38ff643c-f712-4acb-904f-9a776c817cf7.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 38ff643c-f712-4acb-904f-9a776c817cf7.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -4,13 +4,11 @@
 
 std::vector<int> sort_array(std::vector<int> array) {
     int sum = array.front() + array.back();
-    
     if (sum % 2 == 0) {
         std::sort(array.rbegin(), array.rend());
     } else {
         std::sort(array.begin(), array.end());
     }
-    
     return array;
 }
 
