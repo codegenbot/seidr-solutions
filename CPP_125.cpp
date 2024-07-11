d512b7c91b25122c497b1ae0857d925a27ea79df
beep boop <githubbot@vadim.me>
2024-07-11 17:34:37 +0200

    Solution 90 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/oip7hvah Prompt: aa796a74-99b8-4394-85bc-a3c188c4a3f4.cpp: In function ‘std::vector<std::__cxx11: :basic_string<char> > split_words(std::string)’: aa796a74-99b8-4394-85bc-a3c188c4a3f4.cpp:16:65: error: ‘c’ was not declared in t his scope    16 |                 return {to_string(count(txt.begin(), txt.end(), c))};       |                                                                 ^ aa796a74-99b8-4394-85bc-a3c188c4a3f4.cpp:16:68: error: could not convert ‘{<expr ession error>}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector<std::__ cxx11::basic_string<char> >’    16 |                 return {to_string(count(txt.begin(), txt.end(), c))};       |                                                                    ^       |                                                                    |       |                                                                    <brac e-enclosed initializer list> aa796a74-99b8-4394-85bc-a3c188c4a3f4.cpp: At global scope: aa796a74-99b8-4394-85bc-a3c188c4a3f4.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ aa796a74-99b8-4394-85bc-a3c188c4a3f4.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,56 +1,33 @@
+```
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
 
 using namespace std;
 
 vector<string> split_words(string txt) {
     vector<string> result;
-    size_t pos = 0;
-    while (pos < txt.size()) {
-        if (isspace(txt[pos])) {
-            // Split on whitespace
-            string word = txt.substr(0, pos);
-            result.push_back(word);
-            pos++;
-            while (pos < txt.size() && isspace(txt[pos])) {
-                pos++;
-            }
-        } else if (txt.find(',') != string::npos) {
-            // Split on comma
-            size_t commaPos = txt.find(',');
-            if (commaPos == string::npos) {
-                result.push_back(txt.substr(pos));
-                break;
-            }
-            string word = txt.substr(pos, commaPos - pos);
-            result.push_back(word);
-            pos = commaPos + 1;
-        } else {
-            // No whitespace or commas, count odd lower-case letters
-            int count = 0;
-            for (char c : txt) {
-                if (islower(c) && (c - 'a') % 2 != 0) {
-                    count++;
-                }
-            }
-            result.push_back(to_string(count));
-            break;
+    size_t pos = 0, prev_pos = 0;
+
+    while (pos != string::npos) {
+        pos = txt.find(' ', prev_pos);
+        if (pos == string::npos) {
+            pos = txt.find(',');
         }
+        result.push_back(txt.substr(prev_pos, pos - prev_pos));
+        prev_pos = pos + 1;
     }
+
     return result;
 }
 
 int main() {
-    string test1 = "Hello world!";
-    vector<string> result1 = split_words(test1);
-    for (string s : result1) {
+    cout << "Enter a string: ";
+    string txt;
+    getline(cin, txt);
+    vector<string> res = split_words(txt);
+    for (auto s : res) {
         cout << s << endl;
     }
-
-    string test2 = "Hello,world!";
-    vector<string> result2 = split_words(test2);
-    for (string s : result2) {
-        cout << s << endl;
-    }
+    return 0;
+}
