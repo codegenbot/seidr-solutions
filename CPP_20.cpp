4e93efb8a4b100a052babe6197321a01ff4f91ae
beep boop <githubbot@vadim.me>
2024-07-11 05:51:49 +0200

    Solution 46 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ufbj3mil Prompt:       |     ^~~~~~ 10b12e57-3d16-4f62-8022-38cc0667baae.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <cmath>   +++ |+#include <cassert>     4 | 10b12e57-3d16-4f62-8022-38cc0667baae.cpp: At global scope: 10b12e57-3d16-4f62-8022-38cc0667baae.cpp:44:6: error: redefinition of ‘bool issa me(std::vector<float>, std::vector<float>)’    44 | bool issame(vector<float> a,vector<float>b){       |      ^~~~~~ 10b12e57-3d16-4f62-8022-38cc0667baae.cpp:7:6: note: ‘bool issame(std::vector<flo at>, std::vector<float>)’ previously defined here     7 | bool issame(vector<float> a, vector<float> b) {       |      ^~~~~~ 10b12e57-3d16-4f62-8022-38cc0667baae.cpp:52:5: error: redefinition of ‘int main( )’    52 | int main(){       |     ^~~~ 10b12e57-3d16-4f62-8022-38cc0667baae.cpp:38:5: note: ‘int main()’ previously def ined here    38 | int main() {       |     ^~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -13,7 +13,6 @@ bool issame(vector<float> a, vector<float> b) {
             return false;
     }
     return true;
-}
 
 vector<float> find_closest_elements(vector<float> numbers) {
     if (numbers.size() <= 1) {
@@ -33,10 +32,10 @@ vector<float> find_closest_elements(vector<float> numbers) {
         }
     }
     
-    return vector<float>{closest_pair.first, closest_pair.second};
+    return {closest_pair.first, closest_pair.second};
 }
 
 int main() {
     vector<float> result = find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1});
-    assert(issame(result, vector<float>{2.2, 3.1}));
+    assert(issame(result, {2.2, 3.1}));
 }
