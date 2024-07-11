771115e498627c514074b1e6640a4eb0702a0a30
beep boop <githubbot@vadim.me>
2024-07-11 06:53:34 +0200

    Solution 4 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/g9nn0tvk Prompt: Complete the following code given the task description and function signature.

--- Python_88.py
+++ Python_88.py
@@ -2,8 +2,8 @@
 def sort_array(array):
     if len(array) == 0:
         return array
-    sum_first_last = array[0] + array[-1]
-    if sum_first_last % 2 != 0:
+    first_last_sum = sum([array[0], array[-1]])
+    if first_last_sum % 2 != 0:
         return sorted(array)
     else:
         return sorted(array, reverse=True)
