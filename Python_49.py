53f218d1bc5ab74828fa65590180963be6cbf676
beep boop <githubbot@vadim.me>
2024-07-11 19:26:07 +0200

    Solution 45 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/sdfqpced Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b0ef67b-72f1-453c-bdd7-42e4a3cce c36.py", line 7, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b0ef67b-72f1-453c-bdd7-42e4a3cce c36.py", line 4, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b0ef67b-72f1-453c-bdd7-42e4a3cce c36.py", line 2, in modp     return pow(n, p - 2, p) if gcd(n, p) == 1 else None                                ^^^ NameError: name 'gcd' is not defined

--- Python_49.py
+++ Python_49.py
@@ -1,3 +1,8 @@
 from mathlib import gcd
+
+
 def modp(n: int, p: int):
-    return pow(n, p - 2, p) if gcd(n, p) == 1 else None
+    if gcd(n, p) == 1:
+        return pow(n, p - 2, p)
+    else:
+        return None
