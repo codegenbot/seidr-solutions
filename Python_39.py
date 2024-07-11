8f33d9ccc2823884abddcdf0da19285e676e1cec
beep boop <githubbot@vadim.me>
2024-07-12 00:20:55 +0200

    Solution 46 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zyretos8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6ca7719d-bb64-4094-af23-ca6f1fdfc e23.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_39.py
+++ Python_39.py
@@ -1,4 +1,4 @@
-def find_nth_prime_fibonacci(n: int):
+def nth_fibonacci_prime(n: int) -> int:
     a, b = 0, 1
     count = 0
     while True:
@@ -9,7 +9,7 @@ def find_nth_prime_fibonacci(n: int):
         a, b = b, a + b
 
 
-def is_prime(num: int):
+def is_prime(num: int) -> bool:
     if num < 2:
         return False
     for i in range(2, int(num**0.5) + 1):
