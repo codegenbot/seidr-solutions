49a1a9d9a7f3e27c2b2ba5db29d6064bf361127a
beep boop <githubbot@vadim.me>
2024-07-11 04:37:57 +0200

    Solution 26 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/io82wp3u Prompt: er ‘<climits>’; did you forget to ‘#include <climits>’?   +++ |+#include <climits>     1 | int main() { 21ebb51f-8ae2-45f8-a04e-7e1d65e3adee.cpp:16:20: error: ‘abs’ was not declared in  this scope    16 |         int diff = abs(leftSum - rightSum);       |                    ^~~ 21ebb51f-8ae2-45f8-a04e-7e1d65e3adee.cpp:25:9: error: ‘cout’ was not declared in  this scope    25 |         cout << nums[i] << endl;       |         ^~~~ 21ebb51f-8ae2-45f8-a04e-7e1d65e3adee.cpp:25:28: error: ‘endl’ was not declared i n this scope    25 |         cout << nums[i] << endl;       |                            ^~~~ 21ebb51f-8ae2-45f8-a04e-7e1d65e3adee.cpp:28:5: error: ‘cout’ was not declared in  this scope    28 |     cout << 0 << endl;       |     ^~~~ 21ebb51f-8ae2-45f8-a04e-7e1d65e3adee.cpp:28:18: error: ‘endl’ was not declared i n this scope    28 |     cout << 0 << endl;       |                  ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -3,21 +3,21 @@
 #include <cstdlib>
 
 int main() {
-    std::vector<int> nums;
+    vector<int> nums;
     int num;
     
-    while (std::cin >> num) {
+    while (cin >> num) {
         nums.push_back(num);
     }
     
-    int leftSum = 0, rightSum = std::accumulate(nums.begin(), nums.end(), 0);
+    int leftSum = 0, rightSum = accumulate(nums.begin(), nums.end(), 0);
     int minDiff = INT_MAX;
     int cutIndex = 0;
     
     for (int i = 0; i < nums.size(); ++i) {
         leftSum += nums[i];
         rightSum -= nums[i];
-        int diff = std::abs(leftSum - rightSum);
+        int diff = abs(leftSum - rightSum);
         
         if (diff < minDiff) {
             minDiff = diff;
@@ -26,16 +26,16 @@ int main() {
     }
     
     for (int i = 0; i <= cutIndex; ++i) {
-        std::cout << nums[i] << std::endl;
+        cout << nums[i] << endl;
     }
     
-    std::cout << 0 << std::endl;
+    cout << 0 << endl;
     
     for (int i = cutIndex + 1; i < nums.size(); ++i) {
-        std::cout << nums[i] << std::endl;
+        cout << nums[i] << endl;
     }
     
-    std::cout << 0 << std::endl;
+    cout << 0 << endl;
     
     return 0;
 }
