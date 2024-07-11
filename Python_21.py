0ea7c86453729a8d9805fadbc27d3f013ff6c494
beep boop <githubbot@vadim.me>
2024-07-11 13:27:57 +0200

    Solution 2 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/bhjebtvq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3666ea01-3f6f-4dd7-afd6-d3e2e73e4 d51.py", line 1, in <module>     def rescale_to_unit(numbers: List[float]) -> List[float]:                                  ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_21.py
+++ Python_21.py
@@ -1,4 +1,7 @@
-def rescale_to_unit(numbers: List[float]) -> List[float]:
+```
+from typing import List
+
+def rescale_to-unit(numbers: List[float]) -> List[float]:
     min_val = min(numbers)
     max_val = max(numbers)
     return [(x - min_val) / (max_val - min_val) for x in numbers]
