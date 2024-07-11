0f346bd4d001c481027f33559cc32be98e5b1c4e
beep boop <githubbot@vadim.me>
2024-07-11 16:19:10 +0200

    Solution 72 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/94rb1n5e Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8664cb6a-9751-4092-8214-fe01fe18a d6a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,16 +1,14 @@
 from typing import List
+note_lengths = {'o': 4, 'o|': 2, '.': 1}
 
 def parse_music(music_string: str) -> List[int]:
-    note_lengths = {'o': 4, 'o|': 2, '.': 1}
     result = []
     i = 0
     while i < len(music_string):
         if music_string[i:i+3] == '.|.__':
-            i += 6
+            i += 6;
         elif i + 1 < len(music_string) and music_string[i:i+2] in note_lengths:
-            if music_string[i+1] == '|':
-                result.append(2 * note_lengths[music_string[i:i+2]])
-            else:
-                result.append(note_lengths[music_string[i:i+1]])
-        i += 1
-    return result
+            if music_string[i+1] == '|': result.append(2 * note_lengths[music_string[i:i+2]]);
+            else: result.append(note_lengths[music_string[i:i+1]]);
+        i += 1;
+    return result;
