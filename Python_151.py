7eb96571f16735fa16fbeed73d00f82414b59c8e
beep boop <githubbot@vadim.me>
2024-07-11 18:59:39 +0200

    Solution 41 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nxewkfgj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1c1892e7-b112-4f8e-8fb6-61850a40a 261.py", line 1, in <module>     print(double_the_difference([1, 3, 5], [2, 4, 6]) == (max(2,4,6) - min(1,3,5 )) * 2)           ^^^^^^^^^^^^^^^^^^^^^ NameError: name 'double_the_difference' is not defined

--- Python_151.py
+++ Python_151.py
@@ -2,4 +2,4 @@ def double_the_difference(list1, list2):
     max_diff = (max(list2) - min(list1)) * 2
     return max_diff
 
-print(double_the_difference([1, 3, 5], [2, 4, 6]) == (max(2,4,6) - min(1,3,5)) * 2)
+print(double_the_difference([1, 3, 5], [2, 4, 6]) == (max(6,4,2) - min(1,3,5)) * 2)
