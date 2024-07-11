2e40d8509c2a1dbda196cc7706281823f7ab9a10
beep boop <githubbot@vadim.me>
2024-07-11 12:35:22 +0200

    Solution 96 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/er6nmf1f Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8d79e935-59ff-4888-ad4d-ab438d104 c1a.py", line 19, in <module>     check(sort_array)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8d79e935-59ff-4888-ad4d-ab438d104 c1a.py", line 16, in check     assert sort_array([21, 14, 23, 11]) == [23, 21, 14, 11], "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_88.py
+++ Python_88.py
@@ -1,4 +1,3 @@
-```
 def sort_array(user_input=None):
     if user_input is None:
         array = list(map(int, input("Enter space-separated numbers: ").split()))
@@ -7,7 +6,7 @@ def sort_array(user_input=None):
     n = len(array)
 
     if n % 2 != 0:
-        return [array[:n//2], array[n//2+1:]]
+        return array[:n//2+1]+array[n//2+1:]
     else:
         mid = n // 2
-        return [array[:mid], array[mid:]]
+        return array[:mid+1]
