fb32efd2d502d4648392ce195492405991353a2d
beep boop <githubbot@vadim.me>
2024-07-11 08:10:19 +0200

    Solution 47 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d6xmmywr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aa8fc1e1-6522-4403-8675-a5c41cc53 311.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aa8fc1e1-6522-4403-8675-a5c41cc53 311.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,5 +1,7 @@
 def double_the_difference(lst, target_diff):
-    even_numbers = [i for i in lst if isinstance(i, int) and i % 2 == 0]
-    pairs = [(even_numbers[i], even_numbers[j]) for i in range(len(even_numbers)) 
-             for j in range(i+1, len(even_numbers)) if abs(even_numbers[i] - even_numbers[j]) == target_diff]
-    return sum(pair[0]**2 for pair in pairs)
+    even_nums = [i for i in lst if isinstance(i, int) and i % 2 == 0]
+    return sum(
+        i**2
+        for pair in zip(even_nums, even_nums[1:])
+        if abs(pair[1] - pair[0]) == target_diff
+    )
