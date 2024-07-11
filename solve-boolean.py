68e66f6cc890a450db03a1ae3d4828046369a12f
beep boop <githubbot@vadim.me>
2024-07-11 07:00:31 +0200

    Solution 96 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/7l0q1tv9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/46f555e9-5ea0-41f8-9899-425188092 8f0.py", line 13, in <module>     result = solve_boolean(expression)              ^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/46f555e9-5ea0-41f8-9899-425188092 8f0.py", line 8, in solve_boolean     return eval(replaced_expression)            ^^^^^^^^^^^^^^^^^^^^^^^^^   File "<string>", line 1, in <module> NameError: name 'fandforfandtandforfandtorfandforforf' is not defined

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,14 +1,13 @@
 def solve_boolean(expression):
     replaced_expression = (
-        expression.replace("T", "True ")
-        .replace("F", "False ")
-        .replace("|", " or ")
-        .replace("&", " and ")
+        expression.replace("|", "or")
+        .replace("&", "and")
+        .replace("T", "True")
+        .replace("F", "False")
     )
     return eval(replaced_expression)
 
 
-# Read input from user
 expression = input()
 result = solve_boolean(expression)
 print(result)
