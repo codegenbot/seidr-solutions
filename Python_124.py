60499c315eeb5c1dfa6d423c8c31a813a58af332
beep boop <githubbot@vadim.me>
2024-07-11 15:31:59 +0200

    Solution 57 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hxdmbpad Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e23a669e-6181-44d6-b1dc-5cebc6021 cfc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_124.py
+++ Python_124.py
@@ -1,4 +1,3 @@
-```
 def valid_date(date):
     year, month, day = map(int, date.split("-"))
     if month < 1 or month > 12:
@@ -8,27 +7,10 @@ def valid_date(date):
     if month in [4, 6, 9, 11] and day > 30:
         return False
     if month == 2 and (day < 1 or day > 29):
-        return False
+        if year % 4 != 0:
+            return False
+        elif year % 100 == 0 and year % 400 != 0:
+            return False
+        else:
+            return True
     return True
-
-if __name__ == "__main__":
-    while True: 
-        print("Enter date (yyyy-mm-dd):")
-        date = input()
-        while not date.strip():
-            print("Program did not receive expected input. Please try again.")
-            date = input()
-
-        if len(date) != 10 or "-" not in date:
-            print("Invalid date format! Please use 'yyyy-mm-dd'.")
-        else: 
-            try:
-                year, month, day = map(int, date.split("-"))
-            except ValueError:
-                print("Invalid date input! Please enter a valid date (yyyy-mm-dd).")
-            else:
-                if not valid_date(date):
-                    print(f"The date {date} is not valid.")
-                else:
-                    print(f"The date {date} is valid.")
-                    break
