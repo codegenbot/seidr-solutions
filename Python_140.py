cadfc591bb46711d150446404148a5587778dbd2
beep boop <githubbot@vadim.me>
2024-07-11 16:50:21 +0200

    Solution 96 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g87kg5gr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/81191d5a-23b2-4a3a-8c9e-46b584f01 c55.py", line 23, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/81191d5a-23b2-4a3a-8c9e-46b584f01 c55.py", line 19, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,14 +1,14 @@
 def fix_spaces(text):
     new_text = ""
-    prev_was_space = text and text[0] == " "
+    prev_was_space = False
     for char in text:
-        if not prev_was_space and char == " ":
+        if not prev_was_space and (char.isspace() or char == '\n' or char == '\t'):
             new_text += "_"
             prev_was_space = True
-        elif char == " " and prev_was_space:
+        elif (char.isspace() or char == '\n' or char == '\t') and prev_was_space:
             new_text += "-"
             prev_was_space = True
         else:
             new_text += char
-            prev_was_space = False if not char.isspace() else True
+            prev_was_space = False if not (char.isspace() or char == '\n' or char == '\t') else True
     return new_text
