2b2a9b2104d10d467259dccf655e4dc58cd0b5d3
beep boop <githubbot@vadim.me>
2024-07-11 11:56:39 +0200

    Solution 26 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bl8evmth Prompt: 2108e486-7b87-43da-9e22-9576deb9780f.cpp:3:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     3 | string string_to_md5(string text) {       | ^~~~~~       | stdin In file included from 2108e486-7b87-43da-9e22-9576deb9780f.cpp:24: 2108e486-7b87-43da-9e22-9576deb9780f.cpp: In function ‘int main()’: 2108e486-7b87-43da-9e22-9576deb9780f.cpp:26:13: error: ‘string_to_md5’ was not d eclared in this scope    26 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -23,7 +23,6 @@ string string_to_md5(string text) {
 }
 
 int main() {
-    // Use the string_to_md5 function
     assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
     return 0;
 }
