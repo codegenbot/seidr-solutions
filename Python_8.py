25c5e60dfddecbee87c0126bd49b8610d946c61c
beep boop <githubbot@vadim.me>
2024-07-11 15:12:37 +0200

    Solution 50 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/n2r84zmd Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0e1bc45a-77af-4f90-aa2b-f35aa832c 5b5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -1,16 +1,12 @@
-```
 from typing import List, Tuple
 
 
 def sum_product(numbers: List[int]) -> Tuple[int, int]:
     total_sum = 0
     product = 1
+    
     for num in numbers:
         total_sum += num
         product *= num
-    return total_sum, product
-
-
-numbers = list(map(int, input("Enter space-separated numbers: ").split()))
-result = sum_product(numbers)
-print(result)
+        
+    return (total_sum, product)
