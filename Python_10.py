ec64b284ab28328e263905af10c700e4da88179f
beep boop <githubbot@vadim.me>
2024-07-11 07:45:42 +0200

    Solution 4 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/31odiee1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6c049396-f7aa-45c5-8929-a94d5ea9c dec.py", line 16, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6c049396-f7aa-45c5-8929-a94d5ea9c dec.py", line 13, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -2,7 +2,19 @@
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
         return string + string
-    for i in range(len(string)):
-        if string[i:] == string[i:][::-1]:
-            return string[:i] + string[i:][::-1]
-    return string + string[::-1]
+
+    result = ''
+    i, j = 0, len(string) - 1
+    while i <= j:
+        if string[i] == string[j]:
+            result += string[i]
+            i += 1
+            j -= 1
+        else:
+            result = string[j] + result
+            j -= 1
+            if string[i] != string[j]:
+                result = string[i] + result
+                i += 1
+
+    return result
