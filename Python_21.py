122d3633309b943b1ae6f5e40f6aaf46fccf95f7
beep boop <githubbot@vadim.me>
2024-07-11 06:38:26 +0200

    Solution 45 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/nn78v90w Prompt: Program did not receive expected input

--- Python_21.py
+++ Python_21.py
@@ -1,5 +1,3 @@
 from statistics import fmin, fmax
-
-
 def rescale_to_unit(numbers: List[float]) -> List[float]:
     return [(num - fmin(numbers)) / (fmax(numbers) - fmin(numbers)) for num in numbers]
