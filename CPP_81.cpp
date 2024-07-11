49d99f214cae73951001b0c618d5060f628b1fc3
beep boop <githubbot@vadim.me>
2024-07-12 01:32:20 +0200

    Solution 18 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/nxmnsqpn Prompt:       |                                     ^~~~~~ 3fea7af6-030c-46be-a605-9354aa5efaea.cpp:15:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 3fea7af6-030c-46be-a605-9354aa5efaea.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 3fea7af6-030c-46be-a605-9354aa5efaea.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 3fea7af6-030c-46be-a605-9354aa5efaea.cpp:15:44: error: ‘b’ was not declared in t his scope    15 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3fea7af6-030c-46be-a605-9354aa5efaea.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 3fea7af6-030c-46be-a605-9354aa5efaea.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,33 +1,16 @@
 #include <vector>
 #include <string>
-using namespace std;
+#include <cassert>
 
-bool issame(vector<string> a, vector<string> b);
-
-vector<string> numerical_letter_grade(vector<float> grades) {
-    vector<string> letter_grades;
-    for (float gpa : grades) {
-        if (gpa == 4.0) letter_grades.push_back("A+");
-        else if (gpa > 3.7) letter_grades.push_back("A");
-        else if (gpa > 3.3) letter_grades.push_back("A-");
-        else if (gpa > 3.0) letter_grades.push_back("B+");
-        else if (gpa > 2.7) letter_grades.push_back("B");
-        else if (gpa > 2.3) letter_grades.push_back("B-");
-        else if (gpa > 2.0) letter_grades.push_back("C+");
-        else if (gpa > 1.7) letter_grades.push_back("C");
-        else if (gpa > 1.3) letter_grades.push_back("C-");
-        else if (gpa > 1.0) letter_grades.push_back("D+");
-        else if (gpa > 0.7) letter_grades.push_back("D");
-        else if (gpa > 0.0) letter_grades.push_back("D-");
-        else letter_grades.push_back("E");
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a.size() != b.size()) return false;
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) return false;
     }
-    return letter_grades;
+    return true;
 }
 
-bool issame(vector<string> a, vector<string> b) {
-    // implement the issame function here
-}
-
-int main() {
-    // existing main function content
+std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
+    // Implement this function to convert numerical grades to letter grades
+    // Return a vector of letter grades corresponding to the input numerical grades
 }
