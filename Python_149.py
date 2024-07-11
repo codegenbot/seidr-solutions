81c2769ff36727228f9ba8d5755922acab425dac
beep boop <githubbot@vadim.me>
2024-07-11 11:56:06 +0200

    Solution 4 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8z1koym7 Prompt: b/python3.11/site-packages/programlib/programs/4f0d82ce-f83d-4108-90c1-0ff736afb e7e.py", line 9, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f0d82ce-f83d-4108-90c1-0ff736afb e7e.py", line 6, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f0d82ce-f83d-4108-90c1-0ff736afb e7e.py", line 2, in sorted_list_sum     return sum(int(i) for i in ''.join(sorted([word for word in lst if len(word)  % 2 == 0], key=lambda x: (len(x), x))))            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f0d82ce-f83d-4108-90c1-0ff736afb e7e.py", line 2, in <genexpr>     return sum(int(i) for i in ''.join(sorted([word for word in lst if len(word)  % 2 == 0], key=lambda x: (len(x), x))))                ^^^^^^ ValueError: invalid literal for int() with base 10: 'c'

--- Python_149.py
+++ Python_149.py
@@ -1,3 +1,5 @@
-```
+```python
 def sorted_list_sum(lst):
-    return ''.join(sorted([word for word in lst if len(word) % 2 == 0], key=lambda x: (len(x), x)))
+    even_words = [word for word in lst if len(word) % 2 == 0]
+    even_words.sort(key=lambda x: (len(x), x))
+    return even_words
