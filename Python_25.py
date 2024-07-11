40658a54ecb82300d3dc1772496536084ee9d832
beep boop <githubbot@vadim.me>
2024-07-11 08:47:10 +0200

    Solution 18 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/a5bd2efq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/78c2ef14-f60a-4a2c-8b37-16e97e800 26e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,3 +1,18 @@
+from typing import List
+import math
+
+def find_prime_numbers(n: int) -> List[int]:
+    prime_numbers = []
+    for possiblePrime in range(2, n + 1):
+        isPrime = True
+        for num in range(2, int(math.sqrt(possiblePrime)) + 1):
+            if possiblePrime % num == 0:
+                isPrime = False
+                break
+        if isPrime:
+            prime_numbers.append(possiblePrime)
+    return prime_numbers
+
 while True:
     try:
         n = int(input("Enter an integer: "))
