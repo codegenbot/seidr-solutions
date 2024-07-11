c24d30db535fdbb9393f0c10e0e237f1a8b6d8f0
beep boop <githubbot@vadim.me>
2024-07-11 16:24:23 +0200

    Solution 70 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/qompfmfp Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/74a0855e-9fa3-4400-8d4c-7a383cf56 a4c.py", line 18, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/74a0855e-9fa3-4400-8d4c-7a383cf56 a4c.py", line 13, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,12 +1,12 @@
-```
 def sort_third(l: list):
     result = []
     for i in range(len(l)):
         if i % 3 == 0:
-            if i // 3 * 3 + 3 <= len(l):
-                result.append(sorted(l[i//3*3:i//3*3+3])[0])
+            temp = l[i:i+3]
+            if len(temp) == 1:
+                result.append(temp[0])
             else:
-                result.append(sorted(l[i:])[0] if l else None)
+                result.append(sorted(temp)[0])
         else:
             result.append(l[i])
     return tuple(result)
