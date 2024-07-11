867c014728d9195a83f6800831e19ab79fbbcf56
beep boop <githubbot@vadim.me>
2024-07-11 04:06:19 +0200

    Solution 5 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vu3dgd1z Prompt: cc515dc0-2132-4cfd-b947-12f7426b7f58.cpp:5:1: error: ‘string’ does not name a ty pe     5 | string sort_numbers(string numbers) {       | ^~~~~~ In file included from cc515dc0-2132-4cfd-b947-12f7426b7f58.cpp:37: cc515dc0-2132-4cfd-b947-12f7426b7f58.cpp: In function ‘int main()’: cc515dc0-2132-4cfd-b947-12f7426b7f58.cpp:39:13: error: ‘sort_numbers’ was not de clared in this scope    39 |     assert (sort_numbers("six five four three two one zero") == "zero on e two three four five six");       |             ^~~~~~~~~~~~

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,3 +1,33 @@
-using std::string;
+using namespace std;
 
-std::string sort_numbers(std::string numbers) {
+string sort_numbers(string numbers) {
+    vector<int> numValues;
+    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
+                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
+                                {"eight", 8}, {"nine", 9}};
+
+    string numStr;
+    for (char c : numbers) {
+        if (c == ' ') {
+            numValues.push_back(numMap[numStr]);
+            numStr = "";
+        } else {
+            numStr += c;
+        }
+    }
+    numValues.push_back(numMap[numStr]);
+
+    sort(numValues.begin(), numValues.end());
+
+    string result = "";
+    for (int n : numValues) {
+        for (auto it = numMap.begin(); it != numMap.end(); ++it) {
+            if (n == it->second) {
+                result += it->first + " ";
+                break;
+            }
+        }
+    }
+
+    return result.substr(0, result.length() - 1);
+}
