8389ba6cc4e9574e59e3af368c9170b639342de3
beep boop <githubbot@vadim.me>
2024-07-11 13:06:48 +0200

    Solution 94 of CPP/162, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bl8evmth Prompt: c82eaebd-1605-461b-8ceb-3492b1782867.cpp:3:1: error: expected unqualified-id bef ore ‘...’ token     3 | ...       | ^~~ In file included from c82eaebd-1605-461b-8ceb-3492b1782867.cpp:5: c82eaebd-1605-461b-8ceb-3492b1782867.cpp: In function ‘int main()’: c82eaebd-1605-461b-8ceb-3492b1782867.cpp:7:13: error: ‘string_to_md5’ was not de clared in this scope     7 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~ c82eaebd-1605-461b-8ceb-3492b1782867.cpp:7:5: error: ‘__assert_fail’ was not dec lared in this scope     7 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |     ^~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,20 +1,8 @@
-#include <string>
-#include <openssl/md5.h>
+#include <sstream>
+#include <algorithm>
 
-using namespace std;
-
-string string_to_md5(string text) {
-    if (text.empty()) return "";
-    
-    unsigned char md5[MD5_DIGEST_LENGTH];
-    MD5((const unsigned char*)text.c_str(), text.size(), md5);
-    
-    string result;
-    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        stringstream ss;
-        ss << hex << setfill('0') << setw(2) << (int)md5[i];
-        result += ss.str();
-    }
-    
-    return result;
+std::string string_to_md5(const std::string& str) {
+    std::stringstream ss;
+    ss << str;
+    return ss.str();
 }
