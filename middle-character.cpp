40ebd45520132aa8e2f80c6791c4f125751e8c90
beep boop <githubbot@vadim.me>
2024-07-11 08:36:14 +0200

    Solution 16 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/4rir67c6 Prompt: his scope     3 |     cin >> s;       |     ^~~ 2e50742d-e961-4153-a5c3-02928ba89f1d.cpp:3:12: error: ‘s’ was not declared in th is scope     3 |     cin >> s;       |            ^ 2e50742d-e961-4153-a5c3-02928ba89f1d.cpp:6:9: error: ‘cout’ was not declared in this scope     6 |         cout << s.substr(len/2 - 1, 2) << endl;       |         ^~~~ 2e50742d-e961-4153-a5c3-02928ba89f1d.cpp:6:43: error: ‘endl’ was not declared in  this scope     6 |         cout << s.substr(len/2 - 1, 2) << endl;       |                                           ^~~~ 2e50742d-e961-4153-a5c3-02928ba89f1d.cpp:8:9: error: ‘cout’ was not declared in this scope     8 |         cout << s.substr(len/2, 1) << endl;       |         ^~~~ 2e50742d-e961-4153-a5c3-02928ba89f1d.cpp:8:39: error: ‘endl’ was not declared in  this scope     8 |         cout << s.substr(len/2, 1) << endl;       |                                       ^~~~

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,12 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 int main() {
     string s;
     cin >> s;
-    
-    if (s.size() % 2 == 0) {
-        cout << s.substr(s.size() / 2 - 1, 2);
+    int len = s.length();
+    if (len % 2 == 0) {
+        cout << s.substr(len/2 - 1, 2) << endl;
     } else {
-        cout << s.substr(s.size() / 2, 1);
+        cout << s.substr(len/2, 1) << endl;
     }
-    
     return 0;
 }
