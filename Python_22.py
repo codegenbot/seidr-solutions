679eac705daff94ed4054fbe04752eb72deff944
beep boop <githubbot@vadim.me>
2024-07-11 08:39:18 +0200

    Solution 25 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5v09uzk5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a8d3fbbe-0a39-46b4-b4a9-c6f341122 41c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,19 +1,50 @@
-```
-from typing import List
+from typing import List, Any
 
-def filter_integers():
+
+def get_values() -> List[Any]:
     while True:
         try:
-            values = input("Enter a list of integers and/or strings separated by commas: ")
+            values = input(
+                "Enter a list of integers and/or strings separated by commas: "
+            )
             values = [value.strip() for value in values.split(",")]
             print("Input received:", values)
-            if all((value.replace('.', '',).replace('-', '',).isdigit()) or value.isalpha() for value in values):
-                int_values = [int(value) for value in values]
-                if any(isinstance(i, int) for i in int_values):
-                    return int_values
-                else:
-                    print("Invalid input. Please enter integers and/or strings separated by commas.")
+            if all(
+                value.isdigit()
+                or (
+                    value.replace(
+                        ".",
+                        "",
+                    )
+                    .replace(
+                        "-",
+                        "",
+                    )
+                    .isdigit()
+                )
+                for value in values
+            ):
+                return [
+                    (
+                        int(value)
+                        if value.isdigit()
+                        else (
+                            float(value) if "." in value and "-" not in value else value
+                        )
+                    )
+                    for value in values
+                ]
             else:
-                print("Invalid input. Please enter integers and/or strings separated by commas.")
+                print(
+                    "Invalid input. Please enter integers and/or strings separated by commas."
+                )
         except ValueError as e:
-            print(f"Invalid input. Please enter integers and/or strings separated by commas: {e}")
+            print(
+                f"Invalid input. Please enter integers and/or strings separated by commas: {e}"
+            )
+
+
+def filter_integers():
+    result = get_values()
+    if result is not None:
+        return result
