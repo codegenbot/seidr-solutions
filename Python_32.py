61179fb3169260723f026f46f5b009a971326862
beep boop <githubbot@vadim.me>
2024-07-11 14:42:32 +0200

    Solution 58 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/01a7l32z Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/89945092-88b4-4c68-9317-10f3654c1 68e.py", line 1     ````     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,12 +1,18 @@
 def find_zero(xs):
     if len(xs) % 2 != 1:
         return "Error: xs must start with 0"
-    for i in range(len(xs)):
-        if xs[i] != 0 and i == 0:
-            return "Error: xs must start with 0"
-        if xs[i] != 0:
-            break
-    for i in range(i, len(xs)):
-        if xs[i] == 0:
-            return i
-    return None
+    if xs[0] != 0:
+        return "Error: xs must be [0, *coefficients*]"
+    x = xs[1]
+    y = xs[3:]
+    for i in range(len(y)):
+        if y[i] == 0 and (i % 2 == 0 or len(y) % 2 != i):
+            return "Error: xs must be [0, *coefficients*]"
+    if sum(y) == 0:
+        return None
+    if x[1:] != y:
+        return None
+    if len(x) % 2 != 0:
+        return None
+    n = int(sum(x) / (sum(y) or 1))
+    return n
