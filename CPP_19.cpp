19951143586de14c6f0b7a5f31c7eade9aed2192
beep boop <githubbot@vadim.me>
2024-07-11 04:03:56 +0200

    Solution 4 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vu3dgd1z Prompt: cc515dc0-2132-4cfd-b947-12f7426b7f58.cpp:5:1: error: ‘string’ does not name a ty pe     5 | string sort_numbers(string numbers) {       | ^~~~~~ In file included from cc515dc0-2132-4cfd-b947-12f7426b7f58.cpp:37: cc515dc0-2132-4cfd-b947-12f7426b7f58.cpp: In function ‘int main()’: cc515dc0-2132-4cfd-b947-12f7426b7f58.cpp:39:13: error: ‘sort_numbers’ was not de clared in this scope    39 |     assert (sort_numbers("six five four three two one zero") == "zero on e two three four five six");       |             ^~~~~~~~~~~~

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,27 +1,3 @@
-#include <algorithm>
-#include <vector>
+using std::string;
 
-using namespace std;
-
-string sort_numbers(string numbers) {
-    vector<string> numVec;
-    string temp = "";
-    for (char c : numbers) {
-        if (c != ' ') {
-            temp += c;
-        } else {
-            numVec.push_back(temp);
-            temp = "";
-        }
-    }
-    numVec.push_back(temp);
-
-    sort(numVec.begin(), numVec.end());
-
-    string result = "";
-    for (string s : numVec) {
-        result += s + " ";
-    }
-
-    return result.substr(0, result.length() - 1);
-}
+std::string sort_numbers(std::string numbers) {
