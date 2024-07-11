69a61f42532b1a2dc6ba329cc540d13c22a36a53
beep boop <githubbot@vadim.me>
2024-07-11 02:49:32 +0200

    Solution 16 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/l6shnh36 Prompt: 207af7c6-04cf-4aa6-953d-281557eb5b30.cpp:3:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     3 | string int_to_mini_roman(int number) {       | ^~~~~~       | stdin In file included from 207af7c6-04cf-4aa6-953d-281557eb5b30.cpp:17: 207af7c6-04cf-4aa6-953d-281557eb5b30.cpp: In function ‘int main()’: 207af7c6-04cf-4aa6-953d-281557eb5b30.cpp:19:13: error: ‘int_to_mini_romank’ was not declared in this scope    19 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,7 +1,9 @@
+#include <string>
+
 std::string int_to_mini_roman(int number) {
     std::vector<std::pair<int, std::string>> romans = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-                                         {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-                                         {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+                                                       {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+                                                       {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
     std::string roman = "";
     for (const auto& pair : romans) {
         while (number >= pair.first) {
