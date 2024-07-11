4d5ef1bc021d134b2900fe5de4c7995b0989ac49
beep boop <githubbot@vadim.me>
2024-07-11 02:50:10 +0200

    Solution 3 of Python/83, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5aw5dkgo Prompt: Complete the following code given the task description and function signature.

--- Python_83.py
+++ Python_83.py
@@ -1,7 +1,7 @@
 ```
 def starts_one_ends(n):
-    result = 0
+    count = 0
     for i in range(10**(n-1), 10**n):
-        if str(i)[0] == '1' or str(i)[-1] == '1':
-            result += 1
-    return result
+        if str(i)[0] == '1' or str(i)[len(str(i)) - 1] == '1':
+            count += 1
+    return count
