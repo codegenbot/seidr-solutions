c501e22b8039d64f608a30a239c8d9634bcd4dc6
beep boop <githubbot@vadim.me>
2024-07-11 14:23:27 +0200

    Solution 10 of Python/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/2dftcqwt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dccc82b2-ab0d-4649-805d-66725caf3 e16.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_69.py
+++ Python_69.py
@@ -1,9 +1,8 @@
-from collections import Counter
-
 def search(lst):
+    from collections import Counter
     counter = Counter(lst)
     most_common = counter.most_common(1)[0]
     if most_common[1] > 1:
-        return max(set(lst), key=lst.count)
+        return max(set(lst), key=lambda x: lst.count(x))
     else:
-        return most_common[0]
+        return -1
