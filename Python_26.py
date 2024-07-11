ddd2506f06f94b72d30eb5214ca4119fb2e647b1
beep boop <githubbot@vadim.me>
2024-07-11 09:56:53 +0200

    Solution 70 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7s9yk3dz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aab0aeec-0053-4c82-a5d1-af36c6d6d 327.py", line 8, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aab0aeec-0053-4c82-a5d1-af36c6d6d 327.py", line 5, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aab0aeec-0053-4c82-a5d1-af36c6d6d 327.py", line 2, in remove_duplicates     input_list = [int(x) for x in input_string.split(",")]                                   ^^^^^^^^^^^^^^^^^^ AttributeError: 'list' object has no attribute 'split'

--- Python_26.py
+++ Python_26.py
@@ -1,4 +1,3 @@
 ```
-def remove_duplicates(input_string):
-    input_list = [int(x) for x in input_string.split(",")]
-    return ",".join(map(str, set(map(int, input_list))))
+def remove_duplicates(input_list):
+    return list(set(input_list))
