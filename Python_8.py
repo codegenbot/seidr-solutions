6a559c59d30fc71f0adc3cde653b40b19d585fb7
beep boop <githubbot@vadim.me>
2024-07-11 04:38:59 +0200

    Solution 30 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/p2jf7nxt Prompt: Program did not receive expected input

--- Python_8.py
+++ Python_8.py
@@ -1,11 +1,12 @@
-```
 from typing import List, Tuple
 
 
 def sum_product(numbers: List[int]) -> Tuple[int, int]:
-    total_sum = sum(numbers)
+    total_sum = 0
     product = 1
+
     for num in numbers:
+        total_sum += num
         product *= num
 
     return (total_sum, product)
