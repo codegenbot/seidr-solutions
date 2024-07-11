709c0c013251a66a818efb639d0eb98e6b4a589c
beep boop <githubbot@vadim.me>
2024-07-11 15:03:05 +0200

    Solution 50 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ixjphj4n Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/efab9694-0b19-4467-b42a-5e3723069 035.py", line 15, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/efab9694-0b19-4467-b42a-5e3723069 035.py", line 12, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/efab9694-0b19-4467-b42a-5e3723069 035.py", line 2, in make_palindrome     if is_palindrome(string):        ^^^^^^^^^^^^^ NameError: name 'is_palindrome' is not defined. Did you mean: 'make_palindrome'?

--- Python_10.py
+++ Python_10.py
@@ -1,12 +1,13 @@
 def make_palindrome(string: str) -> str:
-    if string == string[::-1]:
+    def is_palindrome(s: str) -> bool:
+        return s == s[::-1]
+
+    if is_palindrome(string):
         return string + string[::-1]
     else:
-        while True:
+        while not is_palindrome(string):
             for char in 'abcdefghijklmnopqrstuvwxyz':
                 if string.find(char) == -1:
                     string = string + char
                     break
-            if string == string[::-1]:
-                break
         return string + string[::-1]
