61f42085a4553bf8faddcfff6971000c913444d5
beep boop <githubbot@vadim.me>
2024-07-11 04:57:14 +0200

    Solution 23 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/wyd6ko9w Prompt: Complete the following code given the task description and function signature.

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -3,9 +3,9 @@
 vector<int> order_by_points(vector<int> nums) {
     vector<pair<int, int>> pairs;
     for (int i = 0; i < nums.size(); i++) {
-        int sumOfDigits = 0;
         int num = nums[i];
-        while (num != 0) {
+        int sumOfDigits = 0;
+        while (num > 0) {
             sumOfDigits += num % 10;
             num /= 10;
         }
