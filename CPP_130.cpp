b049976e577a26c8efc6abba4123017494797b08
beep boop <githubbot@vadim.me>
2024-07-11 07:12:45 +0200

    Solution 60 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/57lypmg0 Prompt: Complete the following code given the task description and function signature.

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,26 +1,17 @@
 vector<int> result;
-    if (n == 0) {
-        return result;
-    }
-    result.push_back(3);
-    if (n == 1) {
-        return result;
-    }
-    result.push_back(1);
-    if (n == 2) {
-        return result;
-    }
-    result.push_back(2);
-    int tri_n_minus_2 = 1, tri_n_minus_1 = 3, tri_n;
-    for (int i = 3; i <= n; ++i) {
-        if (i % 2 == 0) {
-            tri_n = 1 + i / 2;
-        } else {
-            tri_n = tri_n_minus_1 + tri_n_minus_2 + tri_n_minus_2 + 1;
+    if (n >= 0) {
+        result.push_back(3);
+        if (n > 0) {
+            result.push_back(n % 2 == 0 ? 1 + n / 2 : 3);
+            if (n > 1) {
+                int t0 = 3, t1 = n % 2 == 0 ? 1 + n / 2 : 3, t2 = 0;
+                for (int i = 2; i <= n; ++i) {
+                    t2 = t0 + t1 + t2;
+                    result.push_back(t2);
+                    t0 = t1;
+                    t1 = t2;
+                }
+            }
         }
-        result.push_back(tri_n);
-        tri_n_minus_2 = tri_n_minus_1;
-        tri_n_minus_1 = tri_n;
     }
     return result;
-}
