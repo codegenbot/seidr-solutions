edc316159fe184a9550aaf6ae2c202094031b00c
beep boop <githubbot@vadim.me>
2024-07-11 07:25:02 +0200

    Solution 76 of CPP/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xsowexf3 Prompt: In file included from /usr/include/c++/11/iostream:40,                  from 4419d130-0769-4756-8d8a-cdc7b74c0eb3.cpp:2: /usr/include/c++/11/istream:1006:5: note: candidate: ‘template<class _Istream, c lass _Tp> _Istream&& std::operator>>(_Istream&&, _Tp&&)’  1006 |     operator>>(_Istream&& __is, _Tp&& __x)       |     ^~~~~~~~ /usr/include/c++/11/istream:1006:5: note:   template argument deduction/substitu tion failed: /usr/include/c++/11/istream: In substitution of ‘template<class _Istream, class _Tp> _Istream&& std::operator>>(_Istream&&, _Tp&&) [with _Istream = std::basic_i stream<char>&; _Tp = float]’: 4419d130-0769-4756-8d8a-cdc7b74c0eb3.cpp:18:32:   required from here /usr/include/c++/11/istream:1006:5: error: no type named ‘type’ in ‘struct std:: enable_if<false, void>’ 4419d130-0769-4756-8d8a-cdc7b74c0eb3.cpp: At global scope: 4419d130-0769-4756-8d8a-cdc7b74c0eb3.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 4419d130-0769-4756-8d8a-cdc7b74c0eb3.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -12,10 +12,12 @@ bool right_angle_triangle(float a, float b, float c) {
 
 int main() {
     assert(right_angle_triangle(2, 2, 10) == false);
+    
+    int a, b, c;
     cout << "Input the lengths of three sides (a, b, c): ";
-    float a, b, c;
-    cin >> a >> b >> c;
+    cin >> a >> b >> c; 
     if (right_angle_triangle(a, b, c))
         cout << "The triangle is right-angled." << endl;
     else
         cout << "The triangle is not right-angled." << endl;
+}
