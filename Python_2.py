6edea4c092d91c9d54ad5b66ef669a61ee7571a1
beep boop <githubbot@vadim.me>
2024-07-11 08:25:21 +0200

    Solution 36 of Python/2, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mtyau5r5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/509c19f3-db8f-418a-a8bc-725750c9d e2b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_2.py
+++ Python_2.py
@@ -3,10 +3,10 @@ def truncate_number(number, n):
     return format(round(float(f"{number:.{n}f}"), 2), ".2f")
 
 def truncate_number_to_two_decimal_places():
-    number = float(input("Enter a number (default is 0): ") or "0")
+    number = float(input("   Enter a number (default is 0): ") or "0")
     while True:
         try:
-            n = int(input("Enter the number of decimal places (default is 2): ") or "2")
+            n = int(input("   Enter the number of decimal places (default is 2): ") or "2")
             if n < 0:
                 print("Number of decimal places should be non-negative.")
             else:
