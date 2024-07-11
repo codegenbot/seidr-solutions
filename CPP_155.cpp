4a391971c511e2b837876466a67903b29397cba4
beep boop <githubbot@vadim.me>
2024-07-11 13:27:24 +0200

    Solution 90 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/df71cwu3 Prompt:       |                                                                ^~~~ 831b4c7d-a2a1-40cc-9be0-72f26ad0bec0.cpp:3:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     2 | #include <cassert>   +++ |+#include <ostream>     3 | 831b4c7d-a2a1-40cc-9be0-72f26ad0bec0.cpp: At global scope: 831b4c7d-a2a1-40cc-9be0-72f26ad0bec0.cpp:37:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    37 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 831b4c7d-a2a1-40cc-9be0-72f26ad0bec0.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a,vector<int> b){       |      ^~~~~~ 831b4c7d-a2a1-40cc-9be0-72f26ad0bec0.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ 831b4c7d-a2a1-40cc-9be0-72f26ad0bec0.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,14 +1,13 @@
 #include <vector>
 #include <cassert>
 #include <iostream>
-#include <ostream>
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
+bool issame(vector<int> a,vector<int> b){
+    if(a.size() != b.size()) return false;
+    for(int i=0; i<a.size();i++){
+        if(a[i] != b[i]) return false;
     }
     return true;
 }
