4ba0b76350b2bf3b56f610f57e2ff89b56f603ec
beep boop <githubbot@vadim.me>
2024-07-11 10:33:42 +0200

    Solution 43 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xzmxc8nx Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from e66e33d8-b452-4020-8966-950ab9fc1b41.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,59 +1,57 @@
-```
+```cpp
 #include <vector>
 #include <algorithm>
 #include <string>
 
 bool issame(int a, int b) {
-}
-
-vector<string> by_length(vector<int> numbers) {
-    vector<int> vec;
-    for (int num : numbers) {
-        if (num >= 1 && num <= 9) {
-            vec.push_back(num);
+    vector<string> by_length(vector<int> numbers) {
+        vector<int> resultNumbers;
+        for (int num : numbers) {
+            if (num >= 1 && num <= 9) {
+                resultNumbers.push_back(num);
+            }
         }
-    }
-    
-    sort(vec.begin(), vec.end());
-    
-    reverse(vec.begin(), vec.end());
-    
-    vector<string> result;
-    for (int num : vec) {
-        switch (num) {
-            case 1:
-                result.push_back("One");
-                break;
-            case 2:
-                result.push_back("Two");
-                break;
-            case 3:
-                result.push_back("Three");
-                break;
-            case 4:
-                result.push_back("Four");
-                break;
-            case 5:
-                result.push_back("Five");
-                break;
-            case 6:
-                result.push_back("Six");
-                break;
-            case 7:
-                result.push_back("Seven");
-                break;
-            case 8:
-                result.push_back("Eight");
-                break;
-            case 9:
-                result.push_back("Nine");
-                break;
+        
+        sort(resultNumbers.begin(), resultNumbers.end());
+        
+        reverse(resultNumbers.begin(), resultNumbers.end());
+        
+        vector<string> result;
+        for (int num : resultNumbers) {
+            switch (num) {
+                case 1:
+                    result.push_back("One");
+                    break;
+                case 2:
+                    result.push_back("Two");
+                    break;
+                case 3:
+                    result.push_back("Three");
+                    break;
+                case 4:
+                    result.push_back("Four");
+                    break;
+                case 5:
+                    result.push_back("Five");
+                    break;
+                case 6:
+                    result.push_back("Six");
+                    break;
+                case 7:
+                    result.push_back("Seven");
+                    break;
+                case 8:
+                    result.push_back("Eight");
+                    break;
+                case 9:
+                    result.push_back("Nine");
+                    break;
+            }
         }
+        
+        return result;
     }
-    
-    return result;
-}
 
 int main() {
-    // Your code goes here
+    
 }
