cc1f8f36427858c9d4a253516899c27dbbb094a4
beep boop <githubbot@vadim.me>
2024-07-11 10:33:47 +0200

    Solution 83 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/b67vlen3 Prompt:       |                                             ^ 88532b71-28ba-40d5-90ce-65953815ed7e.cpp:45:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    45 | int main(){       |         ^~ 88532b71-28ba-40d5-90ce-65953815ed7e.cpp:45:9: note: remove parentheses to defau lt-initialize a variable    45 | int main(){       |         ^~       |         -- 88532b71-28ba-40d5-90ce-65953815ed7e.cpp:45:9: note: or replace parentheses with  braces to value-initialize a variable 88532b71-28ba-40d5-90ce-65953815ed7e.cpp:45:11: error: a function-definition is not allowed here before ‘{’ token    45 | int main(){       |           ^ 88532b71-28ba-40d5-90ce-65953815ed7e.cpp:48:2: error: expected ‘}’ at end of inp ut    48 | }       |  ^ 88532b71-28ba-40d5-90ce-65953815ed7e.cpp:31:12: note: to match this ‘{’    31 | int main() {       |            ^

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -24,16 +24,14 @@ vector<string> numerical_letter_grade(vector<float> grades){
         else if(g>1.3)letter_grades.push_back("C-");
         else if(g>1.0)letter_grades.push_back("D+");
         else if(g>0.7)letter_grades.push_back("D");
-        else letter_grades.push_back("F");
+        else letter_grades.push_back("E");
     }
     return letter_grades;
 }
 
 int main() {
-    vector<float> grades = {4.0, 3.2};
-    cout << "Letter Grades: ";
-    for(string g: numerical_letter_grade(grades)){
-        cout << g << " ";
-    }
+    vector<float> grades = {0, 0.7};
+    vector<string> result = numerical_letter_grade(grades);
+    assert(issame(result, {"E", "D-"}));
     return 0;
 }
