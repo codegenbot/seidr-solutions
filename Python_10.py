2a7ed2d48958b369ba0f36ba5ef2a6a716057832
beep boop <githubbot@vadim.me>
2024-07-11 14:07:47 +0200

    Solution 37 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tdfielxa Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/52e76548-f80b-482a-a901-36079db36 ca3.py", line 18, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/52e76548-f80b-482a-a901-36079db36 ca3.py", line 15, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -2,15 +2,8 @@ def make_palindrome(s: str) -> str:
     if s == s[::-1]:
         return s + s
     
-    result = []
-    for char in s:
-        result.append(char)
+    result = list(s)
+    while result[0] != result[-1]:
+        result.insert(0, result.pop())
     
-    middle_index = len(result) // 2
-    reversed_half = list(reversed(result[middle_index:]))
-    half_length = len(reversed_half)
-    
-    for i in range(half_length):
-        result.insert(0, reversed_half[i])
-    
-    return "".join(result)
+    return ''.join(result)
