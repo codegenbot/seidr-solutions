73fa4b2e21a18174af0552aee495e1bfbb322373
beep boop <githubbot@vadim.me>
2024-07-11 22:29:47 +0200

    Solution 90 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/bhx6s3yc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa1d0c6a-5a71-4708-be41-7bd13f86c 900.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -2,4 +2,4 @@ def find_max_min(lst):
     if len(lst) == 0:
         return "The list is empty"
     else:
-        return str(min(lst)) + ' and ' + str(max(lst))
+        return str(min(lst)) + " and " + str(max(lst))
