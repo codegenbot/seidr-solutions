0823f16a2f4eea18ea801f958543ac13bb595eaa
beep boop <githubbot@vadim.me>
2024-07-11 06:50:41 +0200

    Solution 96 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j8jl9my3 Prompt: Complete the following code given the task description and function signature.

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,7 +1,8 @@
+```
 long long double_the_difference(vector<float> lst) {
     long long sum = 0;
     for (float num : lst) {
-        if (num >= 0 && num == floor(num)) {
+        if (num > 0 && modf(num, &num) == 0.0) {
             sum += pow(num, 2);
         }
     }
