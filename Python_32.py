7a52022860973906db4cd0fdf17815369b894dc9
beep boop <githubbot@vadim.me>
2024-07-11 14:01:24 +0200

    Solution 54 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/k91vort1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ce831340-eddb-4801-8305-c4c6a9147 75a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,15 +1,16 @@
-def quadratic_equation():
-    while True:
-        print("Enter coefficients (a b c [d]): ")
-        try:
-            xs = input().split()
-            if len(xs) not in [3, 4]:
-                raise ValueError
-            a, b, c = map(int, xs[0:3])
-            d = int(xs[3]) if len(xs) == 4 else None
-            break
-        except ValueError as e:
-            print(e)
+def quadratic_equation(a=None, b=None, c=None, d=None):
+    if a is None:
+        while True:
+            print("Enter coefficients (a b c [d]): ")
+            try:
+                xs = [int(i) for i in input().split()]
+                if len(xs) not in [3, 4]:
+                    raise ValueError
+                d = xs[3] if len(xs) == 4 else None
+                a, b, c = xs[0], xs[1], xs[2]
+                break
+            except ValueError as e:
+                print(e)
     p = (b**2) - (4 * a * c)
     if d is not None and d != 0:
         if p >= 0:
@@ -37,3 +38,6 @@ def quadratic_equation():
             print(f"There is one solution: {result}.")
         else:
             print("There are no real solutions.")
+
+if __name__ == "__main__":
+    quadratic_equation(1, -3, 2)
