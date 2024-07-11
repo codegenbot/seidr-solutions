6db9b06e26357950973b437c4a752abf4b921eed
beep boop <githubbot@vadim.me>
2024-07-11 11:39:12 +0200

    Solution 18 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6e857rqz Prompt:       |                    ^~~ bb189f6b-ae2e-4d65-bad7-b635338d0b32.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bb189f6b-ae2e-4d65-bad7-b635338d0b32.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bb189f6b-ae2e-4d65-bad7-b635338d0b32.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bb189f6b-ae2e-4d65-bad7-b635338d0b32.cpp:15: bb189f6b-ae2e-4d65-bad7-b635338d0b32.cpp: In function ‘int main()’: bb189f6b-ae2e-4d65-bad7-b635338d0b32.cpp:25:20: error: ‘compare’ was not declare d in this scope    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ bb189f6b-ae2e-4d65-bad7-b635338d0b32.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 
-bool issame(int x, int y) {
-    if(x==y)
+bool issame(int a, int b) {
+    if (a == b)
         return true;
     else
         return false;
@@ -10,12 +10,14 @@ bool issame(int x, int y) {
 vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
     for (int i = 0; i < game.size(); i++) {
-        if(game[i] == guess[i]) 
+        if (game[i] == guess[i]) {
             result.push_back(0);
-        else if(abs(guess[i]-game[i]) == 1)
-            result.push_back(2);
-        else
-            result.push_back(1);
+        } else {
+            if(abs(guess[i] - game[i]) == 1)
+                result.push_back(1);
+            else
+                result.push_back(2);
+        }
     }
     return result;
 }
