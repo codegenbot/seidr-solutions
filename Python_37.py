7eff3bf8441a4676cebbcc2e13a16e0f6679466d
beep boop <githubbot@vadim.me>
2024-07-11 09:13:09 +0200

    Solution 18 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v70dnp3u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5697a15c-3466-43e7-bfde-f9d89e352 2c7.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5697a15c-3466-43e7-bfde-f9d89e352 2c7.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,5 @@
 def sort_even(l: list):
     evens = sorted([x for x in l if x % 2 == 0])
-    odds_pos = [x for x in l if x > 0 and x % 2 != 0]
-    neg_odds = sorted([-x for x in l if x < 0 and x % 2 != 0]) 
-    return evens + sorted(odds_pos) + neg_odds
+    neg_odds = sorted([-abs(x) for x in l if x < 0 and x % 2 != 0]) + [0]
+    odds_pos = sorted([x for x in l if x > 0 and x % 2 != 0])
+    return neg_odds + [0] + odds_pos + evens
