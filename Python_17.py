2beb956717ca7c3bbdafed8e75e24afbb3374a7d
beep boop <githubbot@vadim.me>
2024-07-11 19:03:41 +0200

    Solution 84 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/rbhiu3bl Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cdf9c269-56fd-444f-8dca-4cefa3994 014.py", line 1, in <module>     def parse_music(music_string: str) -> List[int]:                                           ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_17.py
+++ Python_17.py
@@ -1,17 +1,17 @@
-from typing import List
+from typing import list
 
-def parse_music(music_string: str) -> List[int]:
+def parse_music(music_string: str) -> list[int]:
     beats = [4, 2, 1]
     result = []
     i = 0
     while i < len(music_string):
-        if music_string[i:i+3] == 'o..':
-            result.extend(beats[:1])
+        if music_string[i] == 'o':
+            result.append(4)
             i += 3
-        elif music_string[i:i+4] == 'o||':
-            result.extend(beats[1:2])
+        elif music_string[i:i+2] == 'o|':
+            result.append(2)
+            i += 2
+        elif music_string[i:i+3] == '.| ':
+            result.append(1)
             i += 3
-        elif music_string[i:i+5] == 'o|||':
-            result.extend(beats)
-            i += 4
     return result
