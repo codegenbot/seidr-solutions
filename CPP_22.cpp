60240a368b051a1ad8e6bc20c259083b0c320549
beep boop <githubbot@vadim.me>
2024-07-11 04:44:54 +0200

    Solution 18 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2dqf0nts Prompt: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a331a921-96e5-43be-8255-9f15ca232918.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a331a921-96e5-43be-8255-9f15ca232918.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a331a921-96e5-43be-8255-9f15ca232918.cpp:13: a331a921-96e5-43be-8255-9f15ca232918.cpp: In function ‘int main()’: a331a921-96e5-43be-8255-9f15ca232918.cpp:23:35: error: no match for call to ‘(st d::vector<int>) (<brace-enclosed initializer list>)’    23 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~ a331a921-96e5-43be-8255-9f15ca232918.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,17 +1,26 @@
 #include <boost/container.hpp>
+#include <list>
+#include <any>
 
-bool same(boost::container::vector<int> a, boost::container::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
 
+std::boost::container::vector<int> filter_integers(std::list<std::any> values) {
+    std::boost::container::vector<int> result;
+    for (const auto& value : values) {
+        if (value.type() == typeid(int)) {
+            int x = boost::any_cast<int>(value);
+            result.push_back(x);
+        }
+    }
+    return result;
+}
+
 int main() {
-    assert(same(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3})); 
+    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3})); 
 }
