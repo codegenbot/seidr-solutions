a259ca4abc069e86358a426d02a2189f76ec9dca
beep boop <githubbot@vadim.me>
2024-07-11 06:21:02 +0200

    Solution 18 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/s9w9fwch Prompt:       |                              ^~~~~~ 7de37caa-cf91-44fc-8951-2e38a58b496d.cpp:14:37: error: ‘string’ was not declared  in this scope    14 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 7de37caa-cf91-44fc-8951-2e38a58b496d.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7de37caa-cf91-44fc-8951-2e38a58b496d.cpp:14:45: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 7de37caa-cf91-44fc-8951-2e38a58b496d.cpp:13: 7de37caa-cf91-44fc-8951-2e38a58b496d.cpp: In function ‘int main()’: 7de37caa-cf91-44fc-8951-2e38a58b496d.cpp:23:20: error: ‘reverse_delete’ was not declared in this scope    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 7de37caa-cf91-44fc-8951-2e38a58b496d.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -3,12 +3,6 @@
 #include <algorithm>
 #include <cassert>
 
-vector<string> reverse_delete(string s, string c);
-
-bool issame(vector<string> a, const vector<string>& b){
-    return a == b;
-}
-
 vector<string> reverse_delete(string s, string c){
     string result = "";
     for(char ch : s){
@@ -19,4 +13,8 @@ vector<string> reverse_delete(string s, string c){
     string rev_result = result;
     reverse(rev_result.begin(), rev_result.end());
     return {result, (result == rev_result) ? "True" : "False"};
+}
+
+bool issame(vector<string> a, const vector<string>& b){  
+    return a == b;  
 }
