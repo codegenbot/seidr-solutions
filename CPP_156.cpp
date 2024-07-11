d0fec7f77eb4284cf50d1fcb2cb59bbe83bb5e88
beep boop <githubbot@vadim.me>
2024-07-11 13:08:20 +0200

    Solution 63 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mq03fasb Prompt: 78048558-bf3c-4cd9-be2d-ff850ef29a57.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int number) {       | ^~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <utility>
 
 string int_to_mini_roman(int number) {
     vector<pair<int, string>> roman_numerals = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
