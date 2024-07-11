ff2ab79f94798c03194f8c91c37c976878b52aac
beep boop <githubbot@vadim.me>
2024-07-11 16:04:16 +0200

    Solution 68 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hxdmbpad Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f0a17580-0fcb-42a2-a952-6eb25f6aa 8ed.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_124.py
+++ Python_124.py
@@ -1,5 +1,20 @@
-pass
-
-
+```
 def valid_date(date):
-    pass
+    parts = date.split("-")
+    if len(parts) != 3:
+        return False
+    year, month, day = map(int, parts)
+    if month < 1 or month > 12:
+        return False
+    if day < 1 or day > 31:
+        return False
+    if month in [4,6,9,11] and day > 30:
+        return False
+    if month == 2 and (day < 1 or day > 28):
+        if year % 4 != 0:
+            return False
+        elif year % 100 == 0 and year % 400 != 0:
+            return False
+        else:
+            return True
+    return True
