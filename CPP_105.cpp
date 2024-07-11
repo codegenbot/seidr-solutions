d878ba6afdce113f088f9c778b1abca5c66ed103
beep boop <githubbot@vadim.me>
2024-07-11 10:06:50 +0200

    Solution 16 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xzmxc8nx Prompt:       |                              ^~~~~~ 6668236a-4556-4a60-a16f-f0ac23e20909.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 6668236a-4556-4a60-a16f-f0ac23e20909.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6668236a-4556-4a60-a16f-f0ac23e20909.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 6668236a-4556-4a60-a16f-f0ac23e20909.cpp:50: 6668236a-4556-4a60-a16f-f0ac23e20909.cpp: In function ‘int main()’: 6668236a-4556-4a60-a16f-f0ac23e20909.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 6668236a-4556-4a60-a16f-f0ac23e20909.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,72 +1,57 @@
-bool issame(int num1, int num2) {
-    if (num1 == num2) {
-        return true;
-    }
-    return false;
+#include <vector>
+#include <string>
+
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
 }
 
 vector<string> by_length(vector<int> arr) {
-    vector<int> nums;
+    vector<vector<string>> result(10);
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            nums.push_back(num);
-        }
-    }
-
-    sort(nums.begin(), nums.end());
-    reverse(nums.begin(), nums.end());
-
-    vector<string> result;
-    int same_count = 0;
-    string prev_str = "";
-    for (int num : nums) {
-        string str = "";
-        switch (num) {
-            case 1:
-                str = "One";
-                break;
-            case 2:
-                str = "Two";
-                break;
-            case 3:
-                str = "Three";
-                break;
-            case 4:
-                str = "Four";
-                break;
-            case 5:
-                str = "Five";
-                break;
-            case 6:
-                str = "Six";
-                break;
-            case 7:
-                str = "Seven";
-                break;
-            case 8:
-                str = "Eight";
-                break;
-            case 9:
-                str = "Nine";
-                break;
-        }
-        if (!issame(num, 0)) {
-            same_count++;
-            result.push_back(str);
-            prev_str = str;
-        } else {
-            for (int i = 0; i < same_count; i++) {
-                result.push_back(prev_str);
+            string str = "";
+            switch (num) {
+                case 1:
+                    str = "One";
+                    break;
+                case 2:
+                    str = "Two";
+                    break;
+                case 3:
+                    str = "Three";
+                    break;
+                case 4:
+                    str = "Four";
+                    break;
+                case 5:
+                    str = "Five";
+                    break;
+                case 6:
+                    str = "Six";
+                    break;
+                case 7:
+                    str = "Seven";
+                    break;
+                case 8:
+                    str = "Eight";
+                    break;
+                case 9:
+                    str = "Nine";
+                    break;
             }
-            same_count = 1;
-            prev_str = str;
+            if (!result[num-1].empty()) {
+                if (issame(result[num-1], vector<string>(str))) {
+                    continue;
+                }
+            }
+            result[num-1].push_back(str);
         }
     }
 
-    while (same_count > 0) {
-        result.push_back(prev_str);
-        same_count--;
+    vector<string> res;
+    for (int i = 0; i < 10; ++i) {
+        res.insert(res.end(), result[i].begin(), result[i].end());
     }
 
-    return result;
+    return res;
 }
