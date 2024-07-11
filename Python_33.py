ef2cb317fb47ec97280f6b0fbef5ca19ea64bd18
beep boop <githubbot@vadim.me>
2024-07-11 09:19:11 +0200

    Solution 24 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b73c5094-908a-4ce1-936e-734e3daf3 f04.py", line 17, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b73c5094-908a-4ce1-936e-734e3daf3 f04.py", line 12, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b73c5094-908a-4ce1-936e-734e3daf3 f04.py", line 9, in sort_third     result.append(next(cycle(sorted([x for j, x in enumerate(l) if (j+1) % 3 != 0] + [x for j, x in enumerate(l) if (j+1) % 3 == 0]), reverse=True)))                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: cycle() takes no keyword arguments

--- Python_33.py
+++ Python_33.py
@@ -1,14 +1,10 @@
-```Python
+from itertools import cycle, accumulate
+
 def sort_third(l: list):
     odd = [x for j, x in enumerate(l) if (j+1) % 3 != 0]
     even = [x for j, x in enumerate(l) if (j+1) % 3 == 0]
     
     result = []
-    while len(odd) > 0 and len(even) > 0:
-        odd.sort(reverse=True)
-        even.sort(reverse=True)
-        result.append(odd.pop(0))
-        result.append(even.pop(0))
-    result += odd
-    result += even
+    for _ in range(len(l)):
+        result.append(next(accumulate(reversed(sorted([x for j, x in enumerate(l) if (j+1) % 3 != 0] + [x for j, x in enumerate(l) if (j+1) % 3 == 0])), initial=None)).next)
     return result
