f10f1fdb1a7171db24d4de86a6ed8f6850e35cb4
beep boop <githubbot@vadim.me>
2024-07-11 06:04:53 +0200

    Solution 8 of CPP/42, ratio 0.6666666666666666 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5s45z6n9 Prompt: Program failed

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -5,7 +5,7 @@ using namespace std;
 vector<int> incr_list(vector<int> l){
     vector<int> result;
     for(int i : l) {
-        result.push_back(i+1);
+        result.push_back(i++);
     }
     return result;
 }
