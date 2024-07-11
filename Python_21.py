86f66632ff87d6eb5e825753b1de8ea31b807edd
beep boop <githubbot@vadim.me>
2024-07-11 17:48:47 +0200

    Solution 11 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8wwmydft Prompt: Program did not receive expected input

--- Python_21.py
+++ Python_21.py
@@ -4,5 +4,5 @@ from typing import List
 def rescale_to_unit(numbers: List[float]) -> List[float]:
     if len(numbers) < 2:
         raise ValueError("List should have at least two elements")
-
+    
     return [(x - min(numbers)) / (max(numbers) - min(numbers)) for x in numbers]
