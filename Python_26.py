fd0ecae17dcb9f9974d5a4adda5d9ebab917a589
beep boop <githubbot@vadim.me>
2024-07-11 10:08:15 +0200

    Solution 85 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7s9yk3dz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8f49e974-3a67-4185-ba6f-875a19be6 5f0.py", line 11, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8f49e974-3a67-4185-ba6f-875a19be6 5f0.py", line 8, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_26.py
+++ Python_26.py
@@ -1,15 +1,6 @@
-```
 def remove_duplicates(input_string):
     if isinstance(input_string, list):
         input_string = ','.join(map(str, input_string))
     
     input_list = [int(x) for x in input_string.split(",")]
-    
-    seen = set()
-    result = []
-    for value in input_list:
-        if value not in seen:
-            seen.add(value)
-            result.append(value)
-            
-    return result
+    return [x for i, x in enumerate(sorted(input_list)) if i == 0 or x != input_list[i-1]]
