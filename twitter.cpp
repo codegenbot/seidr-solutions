abc6b88830f4326be17aa4cc12a7c94a725cfb88
beep boop <githubbot@vadim.me>
2024-07-11 08:06:51 +0200

    Solution 25 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/p41z3lg8 Prompt: 65c09ea6-7e36-478a-841b-438952504cbc.cpp: In function ‘int main()’: 65c09ea6-7e36-478a-841b-438952504cbc.cpp:15:13: error: ‘count’ was not declared in this scope    15 |     else if(count(tweet.begin(), tweet.end(), ' ') + count(tweet.begin() , tweet.end(), '!') + count(tweet.begin(), tweet.end(), '#') > 140) {       |             ^~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -8,15 +8,15 @@ int main() {
     string tweet;
     cout << "Enter a tweet: ";
     getline(cin, tweet);
-    
+
     if(tweet.empty()) {
         cout << "You didn't type anything" << endl;
     }
-    
+
     else if(tweet.length() > 140) {
         cout << "Too many characters" << endl;
     }
-    
+
     else {
         cout << "Your tweet has " << tweet.length() << " characters" << endl;
     }
