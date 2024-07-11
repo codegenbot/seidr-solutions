a4d1e889d3118529ea49639070c224a1049dd777
beep boop <githubbot@vadim.me>
2024-07-11 02:44:33 +0200

    Solution 36 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8kvpmmn6 Prompt: bfcf022e-b1f1-4994-a14d-344c03e0b9d9.cpp:10:29: warning: narrowing conversion of  ‘b.std::vector<int>::size()’ from ‘std::vector<int>::size_type’ {aka ‘long unsi gned int’} to ‘int’ [-Wnarrowing] bfcf022e-b1f1-4994-a14d-344c03e0b9d9.cpp: In function ‘int main()’: bfcf022e-b1f1-4994-a14d-344c03e0b9d9.cpp:14:5: error: ‘assert’ was not declared in this scope    14 |     assert(issame({1,2,3,5},{-1,2,3,4}) == compare({1,2,3,5},{-1,2,3,4})  == vector<int>{2,0,0,1});       |     ^~~~~~ bfcf022e-b1f1-4994-a14d-344c03e0b9d9.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <cassert>   +++ |+#include <cassert>     4 | bfcf022e-b1f1-4994-a14d-344c03e0b9d9.cpp: At global scope: bfcf022e-b1f1-4994-a14d-344c03e0b9d9.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ bfcf022e-b1f1-4994-a14d-344c03e0b9d9.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -4,8 +4,13 @@
 
 using namespace std;
 
-std::vector<int> compare(std::vector<int> a, std::vector<int> b) {
+tuple<size_t, size_t, size_t, size_t> compare(vector<int> a, vector<int> b) {
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
-    return {static_cast<int>(a.size()), static_cast<int>(long(b.size()))};
+    return make_tuple(count(a.begin(), a.end(), 1), count(a.begin(), a.end(), -1), count(b.begin(), b.end(), 1), count(b.begin(), b.end(), -1));
+}
+
+int main() {
+    assert(get<0)(compare({1,2,3,5},{-1,2,3,4})) == get<0>(compare({1,2,3,5},{-1,2,3,4}));
+    return 0;
 }
