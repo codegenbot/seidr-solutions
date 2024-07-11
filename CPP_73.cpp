9e622660448455bf402d52bbe4171c8137b5fb44
beep boop <githubbot@vadim.me>
2024-07-11 17:57:00 +0200

    Solution 17 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/l2zbrz22 Prompt:       |   ^ 96697ef8-92d3-48e0-8cd9-8af1e8220b91.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ 96697ef8-92d3-48e0-8cd9-8af1e8220b91.cpp: In function ‘int main()’: 96697ef8-92d3-48e0-8cd9-8af1e8220b91.cpp:19:5: error: ‘assert’ was not declared in this scope    19 |     assert(smallest_change({0, 1}) == 1);       |     ^~~~~~ 96697ef8-92d3-48e0-8cd9-8af1e8220b91.cpp:6:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     5 | #include <climits>   +++ |+#include <cassert>     6 | 96697ef8-92d3-48e0-8cd9-8af1e8220b91.cpp: At global scope: 96697ef8-92d3-48e0-8cd9-8af1e8220b91.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 96697ef8-92d3-48e0-8cd9-8af1e8220b91.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,12 +1,11 @@
 ```cpp
+#include <initializer_list>
 #include <vector>
 #include <iostream>
 #include <climits>
-#include <initializer_list>
+#include <cassert>
 
-using namespace std;
-
-int smallest_change(vector<int> arr) {
+int smallest_change(std::vector<int> arr) {
     int min = INT_MAX;
     for (int i : arr) {
         if (i < min)
@@ -27,5 +26,4 @@ int main() {
         arr.push_back(val);
     }
     cout << "The smallest change is: " << smallest_change(arr) << endl;
-    return 0;
 }
