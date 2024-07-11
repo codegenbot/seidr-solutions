3e195e19436ad128c711e1adeca2a880708fce03
beep boop <githubbot@vadim.me>
2024-07-11 20:03:49 +0200

    Solution 34 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ocjywund Prompt:  this scope    35 |     cout << "Enter a string of words: ";       |     ^~~~ 0e376319-3a5f-4af3-befd-b260ff08ee81.cpp:3:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     2 | #include <string>   +++ |+#include <iostream>     3 | 0e376319-3a5f-4af3-befd-b260ff08ee81.cpp:36:13: error: ‘cin’ was not declared in  this scope    36 |     getline(cin, str);       |             ^~~ 0e376319-3a5f-4af3-befd-b260ff08ee81.cpp:36:13: note: ‘std::cin’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 0e376319-3a5f-4af3-befd-b260ff08ee81.cpp:37:50: error: ‘endl’ was not declared i n this scope    37 |     cout << "Spun result: " << spinWords(str) << endl;       |                                                  ^~~~ 0e376319-3a5f-4af3-befd-b260ff08ee81.cpp:3:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     2 | #include <string>   +++ |+#include <ostream>     3 |

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
+
 using namespace std;
 
 string spinWords(string str) {
-    using namespace std;
-    string result = "";
-    int start = 0;
-    for (int i = 0; i <= str.length(); i++) {
-        if (i == str.length() || str[i] == ' ') {
-            string word = str.substr(start, i - start);
-            if (word.length() >= 5)
-                result.append(word.rbegin(), word.rend());
-            else
-                result.append(word);
-            if (i < str.length())
-                result.append(" ");
-            start = i + 1;
+    string result = " ";
+    int i = 0;
+    while (i <= str.length()) {
+        if (str[i] == ' ') {
+            result += " ";
+            i++;
+            continue;
         }
+        int j = i;
+        while (j < str.length() && str[j] != ' ') {
+            j++;
+        }
+        string word = str.substr(i, j - i);
+        if (word.length() >= 5) {
+            for (int k = word.length() - 1; k >= 0; k--) {
+                result += word[k];
+            }
+            result += " ";
+        } else {
+            result += word + " ";
+        }
+        i = j;
     }
     return result;
 }
@@ -27,4 +37,3 @@ int main() {
     getline(cin, str);
     cout << "Spun result: " << spinWords(str) << endl;
     return 0;
-}
