341f34c0ade9cec463a8beca2cba3e5fbc6c6d54
beep boop <githubbot@vadim.me>
2024-07-11 17:57:46 +0200

    Solution 16 of Python/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/zv6ipe57 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6cd65062-7542-4315-9c20-f0b5aa1bc 80d.py", line 7, in <module>     check(incr_list)           ^^^^^^^^^ NameError: name 'incr_list' is not defined

--- Python_42.py
+++ Python_42.py
@@ -1,8 +1,2 @@
-def increment_list(l):
-    return [num + 1 for num in l]
-
-input_list = input("Enter a list of numbers separated by space: ").split()
-input_list = [int(num) for num in input_list]
-
-result = increment_list(input_list)
-print(result)
+def check(increment_list):
+    check(increment_list)
