31454e174b70c24bbcac0a9e90a1d9cb230dd84a
beep boop <githubbot@vadim.me>
2024-07-11 11:19:13 +0200

    Solution 46 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/er6nmf1f Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/610ae97f-38fc-4084-941b-2787ffb34 a8d.py", line 16, in <module>     check(sort_array)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/610ae97f-38fc-4084-941b-2787ffb34 a8d.py", line 13, in check     assert sort_array([21, 14, 23, 11]) == [23, 21, 14, 11], "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_88.py
+++ Python_88.py
@@ -1,9 +1,9 @@
-def sort_array(array):
-    sorted_array = sorted(array)
-    n = len(sorted_array)
+def find_median(array):
+    array.sort()
+    n = len(array)
 
     if n % 2 != 0:
-        return [sorted_array[n//2-1], sorted_array[n//2]]
+        return [array[n//2]]
     else:
-        median = (sorted_array[n//2 - 1] + sorted_array[n//2]) / 2
-        return [[median]]
+        median = (array[n//2 - 1] + array[n//2]) / 2
+        return [median]
