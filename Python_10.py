91f6743f13c90e9960392adb090b7ca5c707135c
beep boop <githubbot@vadim.me>
2024-07-11 08:31:55 +0200

    Solution 50 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/31odiee1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4209a4d0-0907-40df-a6b3-be2188784 9d2.py", line 15, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4209a4d0-0907-40df-a6b3-be2188784 9d2.py", line 12, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,17 +1,13 @@
-```
+```Python
 def make_palindrome(s: str) -> str:
     s = list(s)
     i, j = 0, len(s) - 1
     while i < j:
         if s[i] != s[j]:
-            if s[0] == s[j]:
-                s.pop()
-            elif s[-1] == s[i]:
-                s.pop(j)
-            else:
-                s.insert(0, s[j])
-                s.pop(j)
-            break
+            for _ in range(len(s)):
+                s.insert(0, s.pop())
+                if s[i] == s[j]:
+                    break
         i += 1
         j -= 1
     return ''.join(s)
