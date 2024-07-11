d10be66c8b706c04e930281142e3aad324c1ff88
beep boop <githubbot@vadim.me>
2024-07-11 16:31:07 +0200

    Solution 50 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c12oc588 Prompt: 85 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e196ce9c-a239-45a9-b643-565ca2801 464.py", line 45, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e196ce9c-a239-45a9-b643-565ca2801 464.py", line 15, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,9 +1,15 @@
+```
 def sum_squares(lst):
     total_sum = 0
+    has_negative = False
     for num in lst:
-        if isinstance(num, int) and num >= 0: 
-            if (num ** 0.5 == int(num ** 0.5)):
-                total_sum += num ** 2
-            else:
-                total_sum += num
-    return total_sum
+        if num <= 0:
+            has_negative = True
+            break
+        if num > 0 and (num ** 0.5 == int(num ** 0.5)):
+            total_sum += num ** 2
+        else:
+            total_sum += num
+    return 0 if has_negative else total_sum
+
+print(sum_squares([1, 4, -3, 9, -10]))
