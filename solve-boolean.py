54e56745380cb74973695ef91f751ed22720a77c
beep boop <githubbot@vadim.me>
2024-07-11 05:44:23 +0200

    Solution 85 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/7l0q1tv9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b611bcec-3c7c-4d6a-9d50-641e4da80 d28.py", line 7, in <module>     result = solve_boolean(expression)              ^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b611bcec-3c7c-4d6a-9d50-641e4da80 d28.py", line 3, in solve_boolean     return eval(replaced_expression)            ^^^^^^^^^^^^^^^^^^^^^^^^^   File "<string>", line 1, in <module> NameError: name 'fandforfandtandforfandtorfandforforf' is not defined

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,14 +1,8 @@
 import re
 
-
 def solve_boolean(expression):
-    replaced_expression = re.sub(
-        r"T",
-        "True",
-        re.sub(r"F", "False", re.sub(r"\|", "or", re.sub(r"&", "and", expression))),
-    )
+    replaced_expression = re.sub(r'T', 'True', re.sub(r'F', 'False', re.sub(r'\|', 'or', re.sub(r'&', 'and', expression))))
     return eval(replaced_expression)
-
 
 # Read input from user
 expression = input()
