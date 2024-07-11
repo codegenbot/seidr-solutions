cdd971778c40e717822c9e8c99736596b65ab500
beep boop <githubbot@vadim.me>
2024-07-11 19:12:14 +0200

    Solution 30 of CPP/70, ratio 0.2222222222222222 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7l4rvpyq Prompt:       |             ^~~~~~ a2e8b950-5779-4840-b8f2-cf82e9be117e.cpp:33:34: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a2e8b950-5779-4840-b8f2-cf82e9be117e.cpp:33:39: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a2e8b950-5779-4840-b8f2-cf82e9be117e.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ a2e8b950-5779-4840-b8f2-cf82e9be117e.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from a2e8b950-5779-4840-b8f2-cf82e9be117e.cpp:32: a2e8b950-5779-4840-b8f2-cf82e9be117e.cpp: In function ‘int main()’: a2e8b950-5779-4840-b8f2-cf82e9be117e.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,23 +1,21 @@
-#include <memory_resource>
-#include <algorithm>
+#include <vector>
+using namespace std;
 
-bool same_vectors(const std::pmr::vector<int>& a, const std::pmr::vector<int>& b) {
-    return std::equal(a.begin(), a.end(), b.begin());
+bool same_vectors(const vector<int>& a, const vector<int>& b) {
+    return equal(a.begin(), a.end(), b.begin());
 }
 
-std::pmr::polymorphic_allocator<int> alloc;
-
-std::pmr::vector<int> strange_sort_list(std::pmr::vector<int> lst) {
-    std::pmr::vector<int> result(alloc);
+vector<int> strange_sort_list(vector<int> lst) {
+    vector<int> result;
     while (!lst.empty()) {
-        int min_val = *std::min_element(lst.begin(), lst.end());
+        int min_val = *min_element(lst.begin(), lst.end());
         result.push_back(min_val);
-        auto new_end = std::remove(lst.begin(), lst.end(), min_val);
+        auto new_end = remove(lst.begin(), lst.end(), min_val);
         lst.erase(new_end, lst.end());
         if (!lst.empty()) {
-            int max_val = *std::max_element(lst.begin(), lst.end());
+            int max_val = *max_element(lst.begin(), lst.end());
             result.push_back(max_val);
-            auto new_end2 = std::remove(lst.rbegin(), lst.rend(), --max_val);
+            auto new_end2 = remove(lst.rbegin(), lst.rend(), --max_val);
             lst.erase(new_end2.base(), lst.end());
         }
     }
@@ -25,6 +23,5 @@ std::pmr::vector<int> strange_sort_list(std::pmr::vector<int> lst) {
 }
 
 int main() {
-    alloc = std::pmr::polymorphic_allocator<int>();
     assert(same_vectors({1, 1, 1, 1, 1}, strange_sort_list({1, 1, 1, 1, 1})));
 }
