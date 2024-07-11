d078533c6e1e7228117660a4cd36cb623e2597f1
beep boop <githubbot@vadim.me>
2024-07-11 03:50:27 +0200

    Solution 56 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/h7augh0t Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,41 +1,89 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
 using namespace std;
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
-    vector<int> result;
-    for (int i = 0; i < k; i++) {
-        int minVal = INT_MAX;
-        int row, col;
-        for (int j = 0; j < grid.size(); j++) {
-            for (int x = 0; x < grid[0].size(); x++) {
-                if (grid[j][x] <= minVal) {
-                    minVal = grid[j][x];
-                    row = j;
-                    col = x;
-                }
-            }
-        }
-        result.push_back(minVal);
-        for (int i = 0; i < grid.size(); i++) {
-            for (int x = 0; x < grid[0].size(); x++) {
-                if (i == row && x == col) {
-                    grid[i][x] = INT_MAX;
-                }
+    int n = grid.size();
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (k == 1) {
+                dp[i][j] = grid[i][j];
+            } else {
+                int minVal = INT_MAX;
+                if (i > 0) minVal = min(minVal, dp[i-1][j]);
+                if (j > 0) minVal = min(minVal, dp[i][j-1]);
+                if (i < n - 1) minVal = min(minVal, dp[i+1][j]);
+                if (j < n - 1) minVal = min(minVal, dp[i][j+1]);
+                dp[i][j] = grid[i][j] + minVal;
             }
         }
     }
-    return result;
+    
+    int i = 0, j = 0;
+    while (k > 0) {
+        if (i > 0 && j < n - 1 && (dp[i-1][j+1] < dp[i][j+1])) {
+            i--;
+            k--;
+        } else if (i < n - 1 && j > 0 && (dp[i+1][j-1] < dp[i][j-1])) {
+            j--;
+            k--;
+        } else if (i > 0 && j > 0 && (dp[i-1][j-1] < dp[i][j-1])) {
+            i--;
+            j--;
+            k -= 2;
+        } else if (i < n - 1 && j < n - 1 && (dp[i+1][j+1] < dp[i][j+1])) {
+            i++;
+            j++;
+            k -= 2;
+        } else if (i > 0) {
+            i--;
+            k--;
+        } else {
+            j--;
+            k--;
+        }
+    }
+    
+    vector<int> res;
+    while (k > 0) {
+        res.push_back(grid[i][j]);
+        if (k == 1) break;
+        if (i < n - 1 && j < n - 1 && grid[i+1][j+1] <= grid[i][j]) {
+            i++;
+            j++;
+            k -= 2;
+        } else if (i > 0 && j < n - 1 && grid[i-1][j+1] <= grid[i][j]) {
+            i--;
+            j++;
+            k -= 2;
+        } else if (i < n - 1 && j > 0 && grid[i+1][j-1] <= grid[i][j]) {
+            i++;
+            j--;
+            k -= 2;
+        } else if (i > 0 && j > 0 && grid[i-1][j-1] <= grid[i][j]) {
+            i--;
+            j--;
+            k -= 2;
+        } else if (i < n - 1) {
+            i++;
+            k--;
+        } else {
+            j--;
+            k--;
+        }
+    }
+    
+    return res;
 }
 
 int main() {
-    vector<vector<int>> grid = {{1,2,3}, {4,5,6}, {7,8,9}};
+    vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     int k = 3;
     vector<int> result = minPath(grid, k);
-    for (auto x : result) {
-        cout << x << " ";
+    for (int i : result) {
+        cout << i << " ";
     }
     return 0;
 }
