56e5e3538620a131ef29222ef728dbc01fbb7c81
beep boop <githubbot@vadim.me>
2024-07-11 18:23:25 +0200

    Solution 21 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8wwmydft Prompt: Program did not receive expected input

--- Python_21.py
+++ Python_21.py
@@ -1,7 +1,7 @@
-from typing import List
-
-
-def rescale_to_unit(numbers: List[float]) -> List[float]:
-    min_value = min(numbers)
-    max_value = max(numbers)
-    return [(x - min_value) / (max_value - min_value) for x in numbers]
+def rescale_to_unit(numbers: List[List[float]]) -> List[List[float]]:
+    result = []
+    for lst in numbers:
+        min_value = min(lst)
+        max_value = max(lst)
+        result.append([(x - min_value) / (max_value - min_value) for x in lst])
+    return result
