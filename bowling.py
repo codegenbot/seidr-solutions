f28115f6d0081679a84abd41a69054942b9e082c
beep boop <githubbot@vadim.me>
2024-07-11 19:12:28 +0200

    Solution 5 of bowling, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/fy1zocty Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,29 +1,49 @@
-def bowling_score(frames):
+def bowling_score(bowls):
     score = 0
     frame = 1
-    i = 0
+    bowl_index = 0
     while frame <= 10:
-        if frames[i] == "X":
-            score += 10 + (
-                10
-                if frames[i + 2] == "X"
-                else (
-                    10
-                    if frames[i + 3] == "/"
-                    else int(frames[i + 2]) + int(frames[i + 3])
-                )
-            )
-            i += 1
-        elif frames[i + 1] == "/":
+        if bowls[bowl_index] == "X":
             score += 10
-            score += 10 if frames[i + 2] == "X" else int(frames[i + 2])
-            i += 2
+            score += get_strike_bonus(bowls, bowl_index)
+            bowl_index += 1
+        elif bowls[bowl_index + 1] == "/":
+            score += 10
+            score += get_spare_bonus(bowls, bowl_index)
+            bowl_index += 2
         else:
-            score += int(frames[i]) + int(frames[i + 1])
-            i += 2
+            score += get_frame_score(bowls, bowl_index)
+            bowl_index += 2
         frame += 1
     return score
 
 
-frames = input()
-print(bowling_score(frames))
+def get_strike_bonus(bowls, index):
+    if bowls[index + 2] == "X":
+        return 10
+    elif bowls[index + 2] == "-":
+        return 0
+    else:
+        return int(bowls[index + 2])
+
+
+def get_spare_bonus(bowls, index):
+    if bowls[index + 2] == "X":
+        return 10
+    elif bowls[index + 2] == "-":
+        return 0
+    else:
+        return int(bowls[index + 2])
+
+
+def get_frame_score(bowls, index):
+    score = 0
+    if bowls[index] != "-":
+        score += int(bowls[index])
+    if bowls[index + 1] != "-":
+        score += int(bowls[index + 1])
+    return score
+
+
+bowls = input()
+print(bowling_score(bowls))
