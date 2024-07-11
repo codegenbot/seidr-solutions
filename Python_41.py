7010d141ecfb49edf3321690e49b5bf0e5a72877
beep boop <githubbot@vadim.me>
2024-07-11 10:57:35 +0200

    Solution 48 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/md3ef0th Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b2d90565-12f1-4e88-943d-81693f046 bad.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_41.py
+++ Python_41.py
@@ -32,11 +32,9 @@ def car_race_collision():
         except ValueError:
             print("Invalid input. Please enter a valid integer.")
 
-    if abs(speed1 - speed2) == 0:
+    if speed1 == speed2:
         return f"Both cars are traveling at the same speed."
     elif speed1 > speed2:
         return f"Car 1 is moving with a speed of {speed1} km/h and car 2 is moving with a speed of {speed2} km/h."
     else:
         return f"Car 1 is moving with a speed of {speed2} km/h and car 2 is moving with a speed of {speed1} km/h."
-
-print(car_race_collision())
