1f2e1c805946dd404e0d57ea4feaf416a842a866
beep boop <githubbot@vadim.me>
2024-07-11 03:31:45 +0200

    Solution 34 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wg5nozkz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/17193ec9-e123-4cbd-af4c-4cbc02b43 c0b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_124.py
+++ Python_124.py
@@ -1,15 +1,12 @@
-```
 def valid_date(date):
-    day, month, year = date.split('-')
-    if not all(part.isdigit() and 0 < int(part) <= 12 for part in [month, day]):
+    day, month, year = date.split("-")
+    if not all(part.isdigit() and 1 <= int(part) <= 31 for part in [day, month]):
         return False
-    if int(month) > 2 and int(day) > 29:
+    if (int(month) in [4, 6, 9, 11] and int(day) > 30) or (
+        (int(month) == 2 and 0 < int(day) > 29)
+        or (int(month) % 4 == 0 and 0 < int(day) > 29)
+    ):
         return False
-    if int(month) % 4 == 0 and (int(month) % 100 != 0 or int(year) % 400 == 0):
-        if int(day) > 29:
-            return False
-    if int(month) in [1,3,5,7,8,10,12] and int(day) > 31:
-        return False
-    if int(month) in [4,6,9,11] and int(day) > 30:
+    if not all(part.isdigit() and 1900 <= int(part) <= 9999 for part in [year]):
         return False
     return True
