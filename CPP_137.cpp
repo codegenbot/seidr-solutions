b1fced6ec2d32e9b910b6eebcb723e006232b102
beep boop <githubbot@vadim.me>
2024-07-11 03:42:02 +0200

    Solution 10 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/497575qq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,45 +1,57 @@
-#include <boost/any.hpp>
 #include <string>
 #include <algorithm>
+#include <boost/any.hpp>
+#include <boost/lexical_cast.hpp>
 
 using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
+        return boost::any(b);
     }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string str = (string)b.convert_to<string>();
-        size_t pos = str.find(',');
-        if (pos != string::npos) {
-            str = str.substr(0, pos);
-        }
-        return max((float)a.convert_to<float>(), stof(str));
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return boost::any(a);
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        size_t pos1 = (string)a.convert_to<string>().find(',');
-        size_t pos2 = (string)b.convert_to<string>().find(',');
-        string str1 = (pos1 != string::npos) ? (string)a.convert_to<string>().substr(0, pos1) : (string)a.convert_to<string>();
-        string str2 = (pos2 != string::npos) ? (string)b.convert_to<string>().substr(0, pos2) : (string)b.convert_to<string>();
-        return max(stof(str1), stof(str2));
+        string strA = boost::any_cast<string>(a);
+        string strB = boost::any_cast<string>(b);
+        if (strA > strB) {
+            return a;
+        }
+        else if (strA < strB) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
     }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        size_t pos = (string)b.convert_to<string>().find(',');
-        string str = (pos != string::npos) ? (string)b.convert_to<string>().substr(0, pos) : (string)b.convert_to<string>();
-        return max((int)a.convert_to<int>(), stof(str));
+    else if (a.type() == typeid(string) && (b.type() == typeid(int) || b.type() == typeid(float))) {
+        string strA = boost::any_cast<string>(a);
+        float numB = boost::any_cast<float>(b);
+        if (strA > to_string(numB)) {
+            return a;
+        }
+        else if (strA < to_string(numB)) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        size_t pos = (string)a.convert_to<string>().find(',');
-        string str = (pos != string::npos) ? (string)a.convert_to<string>().substr(0, pos) : (string)a.convert_to<string>();
-        return max(stof(str), (int)b.convert_to<int>());
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return boost::any("None");
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        return max((float)a.convert_to<float>(), (float)b.convert_to<float>());
+    else if ((a.type() == typeid(int) || a.type() == typeid(float)) && b.type() == typeid(string)) {
+        float numA = boost::any_cast<float>(a);
+        string strB = boost::any_cast<string>(b);
+        if (to_string(numA) > strB) {
+            return a;
+        }
+        else if (to_string(numA) < strB) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
     }
     else {
-        return boost::any("None");
+        throw invalid_argument("Invalid input type");
     }
 }
