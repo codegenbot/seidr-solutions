66ad265e706fb2f70aea212d0d6e1e16a66564b6
beep boop <githubbot@vadim.me>
2024-07-11 03:05:44 +0200

    Solution 80 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/axs5dzrq Prompt: >; _Alloc = std::allocator<std::pair<int, int> >; std::vector<_Tp, _Alloc>::poin ter = std::pair<int, int>*; std::vector<_Tp, _Alloc>::_Tp_alloc_type = std::vect or<std::pair<int, int> >::_Tp_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {int&, int&}; _Tp = std::pair<int, int>; _Alloc = std::allocator<st d::pair<int, int> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::pair< int, int> >::iterator]’ /usr/include/c++/11/bits/vector.tcc:121:21:   required from ‘std::vector<_Tp, _A lloc>::reference std::vector<_Tp, _Alloc>::emplace_back(_Args&& ...) [with _Args  = {int&, int&}; _Tp = std::pair<int, int>; _Alloc = std::allocator<std::pair<in t, int> >; std::vector<_Tp, _Alloc>::reference = std::pair<int, int>&]’ 39539157-7efb-48ba-b7c5-eefa3489ee0b.cpp:43:27:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,5 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class stdVectorInt {
+public:
+    stdVectorInt() {}
+};
+
+int main() {
+    int n; 
+    std::cin >> n;
+    std::vector<int> nums(n);
+    for (auto& num : nums) 
+        std::cin >> num;
+    
+    std::sort(nums.begin(), nums.end());
+    
+    if (nums == nums)
+        std::cout << "The sorted array is the same as the original one.\n";
+    else
+        std::cout << "The sorted array is different from the original one.\n";
+
+    std::vector<int> result = order_by_points(nums);
+
+    for (const auto& num : result) 
+        std::cout << num << " ";
+    std::cout << "\n";
+    
+    return 0;
+}
+
 std::vector<int> order_by_points(std::vector<int>& nums) {
-    std::vector<std::pair<int, int>> pairs;
+    std::vector<int> pairs;
     for (int i = 0; i < nums.size(); ++i) {
         int sum = 0;
         int num = abs(nums[i]);
@@ -7,12 +39,12 @@ std::vector<int> order_by_points(std::vector<int>& nums) {
             sum += num % 10;
             num /= 10;
         }
-        pairs.emplace_back(i, sum);
+        pairs.push_back(std::make_pair(i, sum));
     }
     std::sort(pairs.begin(), pairs.end());
     std::vector<int> result;
     for (const auto& pair : pairs) {
-        result.push_back(nums[pair.first]);
+        result.push_back(nums[pair.second]);
     }
     return result;
 }
