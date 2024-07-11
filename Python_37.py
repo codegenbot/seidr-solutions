f6594bd7f2d32881cf8ca2c0e8d54b239cab9d33
beep boop <githubbot@vadim.me>
2024-07-11 14:19:01 +0200

    Solution 20 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/sa1121ta Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ba41d264-e5c0-4181-846c-f1372f182 865.py", line 18, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ba41d264-e5c0-4181-846c-f1372f182 865.py", line 13, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,11 +1,11 @@
 ```
 def sort_even(l: list):
-    even = sorted([x for x in l if abs(x) % 2 == 0])
+    even = sorted([abs(x) for x in l if abs(x) % 2 == 0])
     result = []
     idx = 0
     for x in l:
         if abs(x) % 2 == 0:
-            result.append(even[idx])
+            result.append(even[idx] * (1 if x >= 0 else -1))
             idx += 1
         else:
             result.append(x)
