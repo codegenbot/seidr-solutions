bc820ee9a57b73a18d835bd582320c20c69fab20
beep boop <githubbot@vadim.me>
2024-07-11 13:32:10 +0200

    Solution 1 of CPP/42, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/47756ntt Prompt: Complete the following code given the task description and function signature.

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,11 +1,10 @@
 #include <vector>
-
 using namespace std;
 
 vector<int> incr_list(vector<int> l) {
     vector<int> result;
-    for (int x : l) {
-        result.push_back(x + 1);
+    for (int i : l) {
+        result.push_back(i + 1);
     }
     return result;
 }
