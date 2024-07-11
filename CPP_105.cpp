c07444ae9bc643d4955f63adbf1b21f1ca81c141
beep boop <githubbot@vadim.me>
2024-07-11 11:19:59 +0200

    Solution 85 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xzmxc8nx Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 8127f913-5ab7-4946-a687-79603f0b3afc.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -3,61 +3,61 @@
 #include <string>
 #include <initializer_list>
 
-bool issame(int a, int b) {
-    vector<string> by_length(std::vector<int> arr) {
-        std::vector<int> numbers;
-        for (int num : arr) {
-            if (num >= 1 && num <= 9) {
-                numbers.push_back(num);
-            }
+vector<string> by_length(vector<int> arr) {
+    vector<int> numbers;
+    for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            numbers.push_back(num);
         }
-        
-        std::sort(numbers.begin(), numbers.end());
-        
-        std::reverse(numbers.begin(), numbers.end());
-        
-        std::vector<string> result;
-        for (int num : numbers) {
-            switch (num) {
-                case 1:
-                    result.push_back("One");
-                    break;
-                case 2:
-                    result.push_back("Two");
-                    break;
-                case 3:
-                    result.push_back("Three");
-                    break;
-                case 4:
-                    result.push_back("Four");
-                    break;
-                case 5:
-                    result.push_back("Five");
-                    break;
-                case 6:
-                    result.push_back("Six");
-                    break;
-                case 7:
-                    result.push_back("Seven");
-                    break;
-                case 8:
-                    result.push_back("Eight");
-                    break;
-                case 9:
-                    result.push_back("Nine");
-                    break;
-            }
-        }
-        
-        return result;
     }
+    
+    sort(numbers.begin(), numbers.end());
+    
+    reverse(numbers.begin(), numbers.end());
+    
+    vector<string> result;
+    for (int num : numbers) {
+        switch (num) {
+            case 1:
+                result.push_back("One");
+                break;
+            case 2:
+                result.push_back("Two");
+                break;
+            case 3:
+                result.push_back("Three");
+                break;
+            case 4:
+                result.push_back("Four");
+                break;
+            case 5:
+                result.push_back("Five");
+                break;
+            case 6:
+                result.push_back("Six");
+                break;
+            case 7:
+                result.push_back("Seven");
+                break;
+            case 8:
+                result.push_back("Eight");
+                break;
+            case 9:
+                result.push_back("Nine");
+                break;
+        }
+    }
+    
+    return result;
+}
 
 int main() {
-    std::vector<int> numbers = {1, 2, 3};
-    std::vector<string> output = by_length(numbers);
+    vector<int> numbers = {1, 2, 3};
+    vector<string> output = by_length(numbers);
     
     for (const auto &word : output) {
-        std::cout << word << " ";
+        cout << word << " ";
     }
     
     return 0;
+}
