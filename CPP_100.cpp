a3cf02a916eda24d9b03e8cdbef415670887cad2
beep boop <githubbot@vadim.me>
2024-07-11 16:21:45 +0200

    Solution 30 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/d31cl2gl Prompt:       |             ^~~~~~ bc243849-7a22-4c37-b374-4ccc596e446c.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bc243849-7a22-4c37-b374-4ccc596e446c.cpp:29:39: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<int> a,vector<int>b){       |                                       ^ bc243849-7a22-4c37-b374-4ccc596e446c.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ bc243849-7a22-4c37-b374-4ccc596e446c.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from bc243849-7a22-4c37-b374-4ccc596e446c.cpp:28: bc243849-7a22-4c37-b374-4ccc596e446c.cpp: In function ‘int main()’: bc243849-7a22-4c37-b374-4ccc596e446c.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,10 +1,5 @@
 #include <vector>
 #include <cassert>
-#include <algorithm>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return std::equal(a.begin(), a.end(), b.begin());
-}
 
 std::vector<int> make_a_pile(int n) {
     std::vector<int> stones;
@@ -20,4 +15,11 @@ std::vector<int> make_a_pile(int n) {
     return stones;
 }
 
-assert(std::issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
+bool is_same(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+int main() {
+    assert(is_same(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
+    return 0;
+}
