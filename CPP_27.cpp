f7da0116877d4503a436c56e6697f60f5d4678a3
beep boop <githubbot@vadim.me>
2024-07-11 12:16:30 +0200

    Solution 19 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4eqf459r Prompt: a521485a-9417-4ab9-a6e8-dda55cfa91ef.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string flip_case(string str){       | ^~~~~~ In file included from a521485a-9417-4ab9-a6e8-dda55cfa91ef.cpp:14: a521485a-9417-4ab9-a6e8-dda55cfa91ef.cpp: In function ‘int main()’: a521485a-9417-4ab9-a6e8-dda55cfa91ef.cpp:16:13: error: ‘filp_case’ was not decla red in this scope    16 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -12,5 +12,5 @@ string flipCase(string str){
 }
 
 int main() {
-    assert(flipCase("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
+    assert (flipCase("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
 }
