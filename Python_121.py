a3596aa61fa53fa8930e9db822692bacf1567eef
beep boop <githubbot@vadim.me>
2024-07-11 17:25:00 +0200

    Solution 86 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5n5qhplk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/55927201-f6bb-47f8-a39b-30fef5de6 516.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,16 +1,19 @@
-```Python
+````
 def solution(lst):
     return sum(i for i in lst[1::2] if i % 2 != 0)
 
 if __name__ == "__main__":
-    while True:
-        user_input = input("Enter space-separated integers: ")
-        numbers = user_input.split()
-        
-        try:
-            lst = [int(i) for i in numbers]
-            break
-        except ValueError:
-            print("     Invalid input. Please enter only integers.")
+    user_input = input("Enter space-separated integers: ")
+    numbers = user_input.split()
     
-    print(solution(lst))
+    for num in numbers:
+        int_num = int(num)
+        if not isinstance(int_num, int):
+            raise ValueError
+        
+    lst = [int(i) for i in numbers]
+    
+    try:
+        print(solution(lst))
+    except ValueError:
+        print("     Invalid input. Please enter only integers.")
