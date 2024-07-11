e50e8115d6015747ed1e5bdd99e3cd7b12cbcf37
beep boop <githubbot@vadim.me>
2024-07-11 10:24:27 +0200

    Solution 20 of CPP/153, ratio 0.7777777777777778 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fyb0o416 Prompt: 4aaecf32-2f1c-434c-9f74-870a292d416a.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string Strongest_Extension(string class_name, vector<string> extensions)  {       | ^~~~~~ In file included from 4aaecf32-2f1c-434c-9f74-870a292d416a.cpp:25: 4aaecf32-2f1c-434c-9f74-870a292d416a.cpp: In function ‘int main()’: 4aaecf32-2f1c-434c-9f74-870a292d416a.cpp:27:13: error: ‘Strongest_Extension’ was  not declared in this scope    27 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,10 +1,16 @@
-string Strongest_Extension(string class_name, vector<string> extensions) {
-    string result;
+#include <string>
+#include <vector>
+#include <cctype>
+#include <climits>
+#include <cassert>
+
+std::string Strongest_Extension(std::string class_name, std::vector<std::string> extensions) {
+    std::string strongest_extension = "";
     int max_strength = INT_MIN;
     
-    for (const string& ext : extensions) {
+    for (const auto& extension : extensions) {
         int cap_count = 0, sm_count = 0;
-        for (char c : ext) {
+        for (char c : extension) {
             if (isupper(c)) {
                 cap_count++;
             } else if (islower(c)) {
@@ -13,11 +19,11 @@ string Strongest_Extension(string class_name, vector<string> extensions) {
         }
         
         int strength = cap_count - sm_count;
-        if (strength > max_strength || (strength == max_strength && ext < result.substr(class_name.size() + 1))) {
+        if (strength > max_strength || (strength == max_strength && strongest_extension.empty())) {
             max_strength = strength;
-            result = class_name + "." + ext;
+            strongest_extension = extension;
         }
     }
     
-    return result;
+    return class_name + "." + strongest_extension;
 }
