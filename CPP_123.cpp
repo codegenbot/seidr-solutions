58d89878a4d12bdc0bec2838884ce10d01af9cee
beep boop <githubbot@vadim.me>
2024-07-11 11:07:36 +0200

    Solution 17 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dx3gnirw Prompt:       |                    ^~~ 99b14443-0051-4b97-ba28-b6ec92f15b20.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 99b14443-0051-4b97-ba28-b6ec92f15b20.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 99b14443-0051-4b97-ba28-b6ec92f15b20.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 99b14443-0051-4b97-ba28-b6ec92f15b20.cpp:21: 99b14443-0051-4b97-ba28-b6ec92f15b20.cpp: In function ‘int main()’: 99b14443-0051-4b97-ba28-b6ec92f15b20.cpp:31:20: error: ‘get_odd_collatz’ was not  declared in this scope    31 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 99b14443-0051-4b97-ba28-b6ec92f15b20.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,28 +1,27 @@
 bool issame(vector<int> a, vector<int>b) {
-    if(a.size() != b.size()) return false;
+    if(a.size() != b.size())
+        return false;
     for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
+        if(a[i] != b[i])
+            return false;
     }
     return true;
 }
 
 vector<vector<int>> get_odd_collatz(int n) {
-    vector<vector<int>> collatz_sequence;
+    vector<int> collatz_sequence;
     while (n != 1) {
         if (n % 2 == 0)
             n = n / 2;
         else
             n = 3 * n + 1;
-        collatz_sequence.push_back({n});
+        collatz_sequence.push_back(n);
     }
-    sort(collatz_sequence[0].begin(), collatz_sequence[0].end());
-    vector<vector<int>> odd_collatz;
-    for (int i = 0; i < collatz_sequence.size(); i++) {
-        if(issame(collatz_sequence[i], {1})) continue;
-        for(int num : collatz_sequence[i]) {
-            if(num % 2 != 0)
-                odd_collatz.push_back({num});
-        }
+    sort(collatz_sequence.begin(), collatz_sequence.end());
+    vector<int> odd_collatz;
+    for (int num : collatz_sequence) {
+        if (num % 2 != 0)
+            odd_collatz.push_back(num);
     }
-    return odd_collatz;
+    return {collatz_sequence, odd_collatz};
 }
