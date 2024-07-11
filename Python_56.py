a3dbf4c712e188d3c7b74f16a1b85f6d1ebb14b2
beep boop <githubbot@vadim.me>
2024-07-11 13:22:44 +0200

    Solution 81 of Python/56, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hhf9fjri Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b5294446-0496-45f5-ab92-88d9ee1ad 4c5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_56.py
+++ Python_56.py
@@ -7,13 +7,7 @@ while True:
         elif not isinstance(expression, str):
             print("Expected a string as the input. Please enter again.")
         else:
-            if is_valid_expression(expression):
-                print(
-                    "Correct bracketing!"
-                    if correct_bracketing(expression)
-                    else "Incorrect bracketing. Try again."
-                )
-            else:
-                print("Invalid mathematical expression. Please try again.")
+            print("Correct bracketing!" if correct_bracketing(expression) else "Incorrect bracketing. Try again.")
     except Exception as e:
         print(f"Error occurred: {e}")
+        raise
