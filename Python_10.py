20ea4b7e0b27e1388cc4f02f4ab0058bfb021875
beep boop <githubbot@vadim.me>
2024-07-11 08:08:40 +0200

    Solution 27 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/31odiee1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5dc8c571-3ae5-4ec5-b3b1-455467c13 dde.py", line 20, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5dc8c571-3ae5-4ec5-b3b1-455467c13 dde.py", line 17, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,7 +1,18 @@
-```python
+```
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
         return string
-    for i in range(len(string), 0, -1):
-        if string + string[:i][::-1] == string[:i][::-1] + string[i:]:
-            return string[:i][::-1] + string[i:]
+    left = right = 0
+    while right < len(string):
+        if string[left : right + 1] == string[left : right + 1][::-1]:
+            if left == 0 and right == len(string) - 1:
+                return string
+            return string[:left][::-1] + string[left : right + 1][::-1]
+        if right == len(string) - 1:
+            right = 0
+            left += 1
+        else:
+            right += 1
+    if not string or len(string) % 2 == 0:
+        return (string[0] + "a") * (len(string) // 2)
+    return (string[0] + "a" + string[0]) * (len(string) // 2 + 1)
