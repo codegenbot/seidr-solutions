bdacd79e16234eb132e98413ff4ae0fa9a646a23
beep boop <githubbot@vadim.me>
2024-07-11 06:41:11 +0200

    Solution 77 of CPP/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/4o4m1fnd Prompt: Complete the following code given the task description and function signature.

--- CPP_114.cpp
+++ CPP_114.cpp
@@ -1,11 +1,11 @@
 long long minSubArraySum(vector<long long> nums){
     long long min_sum = nums[0];
     long long current_sum = 0;
-
-    for (long long num : nums) {
+    
+    for(long long num : nums){
         current_sum = min(num, current_sum + num);
         min_sum = min(min_sum, current_sum);
     }
-
+    
     return min_sum;
 }
