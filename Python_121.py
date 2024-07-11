41e8f79bdc8602b5503bd4451d014e4b0d8cc2a4
beep boop <githubbot@vadim.me>
2024-07-11 15:25:23 +0200

    Solution 24 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5n5qhplk Prompt: Program did not receive expected input

--- Python_121.py
+++ Python_121.py
@@ -1,8 +1,12 @@
-```
+```Python
 def solution(lst):
     return sum(i for i in lst[1::2] if i % 2 != 0)
 
 if __name__ == "__main__":
-    str_lst = input("Enter space-separated integers: ")
-    lst = list(map(int, str_lst.split()))
-    print(solution(lst))
+    while True:
+        try:
+            lst = list(map(int, input("Enter space-separated integers: ").split()))
+            print(solution(lst))
+            break
+        except ValueError:
+            print("Invalid input. Please enter valid integer values separated by spaces.")
