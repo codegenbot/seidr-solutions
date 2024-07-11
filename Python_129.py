e1b0bf745f2d9f6fe45451f732321036191139a4
beep boop <githubbot@vadim.me>
2024-07-11 18:08:21 +0200

    Solution 41 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/t93lgfgj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5689abc3-cc80-4b4c-8600-8c8f0ba4e 630.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -1,10 +1,11 @@
+```
 from collections import deque
 
 def minPath(grid, k):
     n = len(grid)
     res = []
     for _ in range(k):
-        queue = [(i, j, i * n + j) for i in range(n) for j in range(n)]
+        queue = deque([(i, j, i*n+j) for i in range(n) for j in range(n)])
         visited = set()
         cur_min = float("inf")
         while queue:
@@ -14,4 +15,4 @@ def minPath(grid, k):
                 if cell < cur_min:
                     cur_min = cell
         res.append(cur_min)
-    return res"
+    return res
