e89a65d5ba2756a8b4fe8d79e06d4effad9f147e
beep boop <githubbot@vadim.me>
2024-07-11 04:59:52 +0200

    Solution 68 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/497575qq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,37 +1,68 @@
+#include <string>
+#include <algorithm>
+#include <boost/any.hpp>
+
+using namespace std;
+
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return boost::any_cast<float>(b) > boost::any_cast<int>(a)
-            ? b
-            : a;
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        return (get<int>(a) > get<int>(b)) ? a : ((get<int>(a) < get<int>(b)) ? b : boost::any("None"));
+    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        return (get<float>(a) > get<float>(b)) ? a : ((get<float>(a) < get<float>(b)) ? b : boost::any("None"));
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = any_cast<string>(a);
+        string str2 = any_cast<string>(b);
+
+        int n1 = 0, n2 = 0;
+        for (char &c : str1) {
+            if (!isdigit(c)) break;
+            n1 = n1 * 10 + c - '0';
+        }
+        for (char &c : str2) {
+            if (!isdigit(c)) break;
+            n2 = n2 * 10 + c - '0';
+        }
+
+        return ((n1 > n2) ? a : ((n1 < n2) ? b : boost::any("None")));
+    } else if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return (get<int>(a) > get<float>(b)) ? a : ((get<int>(a) < get<float>(b)) ? b : boost::any("None"));
+    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        int n = 0;
+        for (char &c : any_cast<string>(b)) {
+            if (!isdigit(c)) break;
+            n = n * 10 + c - '0';
+        }
+        return ((get<int>(a) > n) ? a : ((get<int>(a) < n) ? b : boost::any("None")));
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return (get<float>(a) > get<int>(b)) ? a : ((get<float>(a) < get<int>(b)) ? b : boost::any("None"));
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        float n = 0;
+        for (char &c : any_cast<string>(b)) {
+            if (!isdigit(c)) break;
+            n = n * 10 + c - '0';
+        }
+        return ((get<float>(a) > n) ? a : ((get<float>(a) < n) ? b : boost::any("None")));
+    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        int n = get<int>(b);
+        string str1 = any_cast<string>(a);
+
+        int n1 = 0;
+        for (char &c : str1) {
+            if (!isdigit(c)) break;
+            n1 = n1 * 10 + c - '0';
+        }
+        return ((n1 > n) ? a : ((n1 < n) ? b : boost::any("None")));
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        float n = get<float>(b);
+        string str1 = any_cast<string>(a);
+
+        int n1 = 0;
+        for (char &c : str1) {
+            if (!isdigit(c)) break;
+            n1 = n1 * 10 + c - '0';
+        }
+        return ((n1 > n) ? a : ((n1 < n) ? b : boost::any("None")));
     }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string str = boost::any_cast<string>(b);
-        return stof(str) > boost::any_cast<float>(a)
-            ? b
-            : a;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = boost::any_cast<string>(a);
-        string str2 = boost::any_cast<string>(b);
-        return stof(str2) > stof(str1)
-            ? b
-            : a;
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int num = boost::any_cast<int>(a);
-        string str = boost::any_cast<string>(b);
-        return stof(str) > num
-            ? b
-            : a;
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        float flt = boost::any_cast<float>(a);
-        int num = boost::any_cast<int>(b);
-        return flt > num
-            ? a
-            : b;
-    }
-    else {
-        return boost::any("None");
-    }
+
+    return "Error"; // This line should never be reached
 }
