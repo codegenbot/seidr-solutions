6521146d8262c228b7d09e6388a33b4f468041ee
beep boop <githubbot@vadim.me>
2024-07-11 04:43:02 +0200

    Solution 92 of CPP/126, ratio 0.07692307692307693 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vlwtmqlv Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 489d1650-6a77-4548-8c9c-596a92c8d21f.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,13 +1,33 @@
-Here is the completed code:
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
 
 bool is_sorted(vector<int> lst){
     for(int i = 1; i < lst.size(); i++){
         if(lst[i-1] > lst[i]){
             return false;
         }
-        else if(count(lst.begin(), lst.end(), lst[i]) > 1){
+        else if(std::count(lst.begin(), lst.end(), lst[i]) > 1){
             return false;
         }
     }
     return true;
+
+int main() {
+    vector<int> lst;
+    int num;
+    
+    cout << "Enter numbers, enter -1 to stop: ";
+    
+    while((cin >> num) && (num != -1)){
+        lst.push_back(num);
+    }
+    
+    if(lst.empty())
+        cout << "List is empty." << endl;
+    else
+        cout << (is_sorted(lst) ? "True" : "False") << endl; 
+    return 0;
 }
