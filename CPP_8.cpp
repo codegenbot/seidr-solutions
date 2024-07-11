5982fae6e53587cb604aa68df8792ae2e97f7249
beep boop <githubbot@vadim.me>
2024-07-11 17:51:48 +0200

    Solution 6 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/5mxvjosi Prompt:       |                    ^~~ 98d0de15-3a06-4b19-bdee-4be944b0c698.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 98d0de15-3a06-4b19-bdee-4be944b0c698.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 98d0de15-3a06-4b19-bdee-4be944b0c698.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 98d0de15-3a06-4b19-bdee-4be944b0c698.cpp:12: 98d0de15-3a06-4b19-bdee-4be944b0c698.cpp: In function ‘int main()’: 98d0de15-3a06-4b19-bdee-4be944b0c698.cpp:22:20: error: ‘sum_product’ was not dec lared in this scope; did you mean ‘product’?    22 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ 98d0de15-3a06-4b19-bdee-4be944b0c698.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,21 +1,18 @@
 #include <vector>
-#include <cassert>
 
-std::pair<int, int> sum_product(const std::vector<int>& numbers) {
+std::pair<int, int> calculateSumAndProduct(const std::vector<int>& numbers1, const std::vector<int>& numbers2) {
     int sum = 0;
     int product = 1;
-    for (int num : numbers) {
+
+    for (int num : numbers1) {
         sum += num;
         product *= num;
     }
+
+    for (int num : numbers2) {
+        sum += num;
+        product *= num;
+    }
+
     return {sum, product};
-}
-
-bool issame(const std::pair<int, int>& a, const std::pair<int, int>& b){
-    return a == b;
-}
-
-int main() {
-    assert(issame(sum_product({10}), {10, 10}));
-    return 0;
 }
