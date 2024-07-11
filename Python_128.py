0d9ae398c945bb45cc23108831da78cb0fe1c8ab
beep boop <githubbot@vadim.me>
2024-07-11 11:37:18 +0200

    Solution 20 of Python/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rfsrpo3u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d12b7158-4bf6-49da-bbb6-bfffce95b cf9.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_128.py
+++ Python_128.py
@@ -3,7 +3,7 @@ def product_of_signs(arr):
     return 1 if all(x > 0 for x in arr) else -1
 
 def check():
-    arr = list(map(int, input("Enter numbers separated by space: ").split()))
+    arr = list(map(int, input().split()))
     print(product_of_signs(arr))
 
 check()
