4488aca4290acb34f9c2385bcd5cc01110b60ce4
beep boop <githubbot@vadim.me>
2024-07-11 09:10:47 +0200

    Solution 14 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f9f84f91-d35f-47e2-a09d-748522499 4c6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,16 +1,19 @@
 from itertools import cycle
 
+
 def sort_third(l: list):
     first_half = sorted([x for i, x in enumerate(l) if i % 3 != 0])
-    second_half = sorted([x for i, x in enumerate(l) if i % 3 == 0 and x not in first_half], reverse=True)
-    
+    second_half = sorted([x for i, x in enumerate(l) if i % 3 == 0], reverse=True)
+
     result = []
-    for i, x in enumerate(l):
+    for i, x in cycle(enumerate(l)):
         if i % 3 != 0:
             result.append(x)
         else:
-            if i // 3 < len(first_half):
-                result.append(first_half[i // 3])
-            else:
-                result.append(second_half[i // 3 - len(first_half)])
+            if len(first_half):
+                result.append(next(item for item in first_half))
+                del first_half[0]
+            elif len(second_half):
+                result.append(next(item for item in second_half))
+                del second_half[0]
     return result
