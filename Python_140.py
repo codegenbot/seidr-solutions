5da59bd4553388d9f8cbda4aa5ed5bad49407a93
beep boop <githubbot@vadim.me>
2024-07-11 14:59:37 +0200

    Solution 14 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g87kg5gr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7525b409-9aa9-487d-9317-28e88acfe 165.py", line 23, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7525b409-9aa9-487d-9317-28e88acfe 165.py", line 19, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,16 +1,14 @@
 def fix_spaces(text):
     new_text = ""
-    prev_was_space = False
+    prev_was_space = True
     for char in text:
         if not prev_was_space and char == " ":
             new_text += "_"
             prev_was_space = True
-        elif char == " ":
+        elif char != " ":
+            new_text += char
+            prev_was_space = False
+        else:
             new_text += "-"
             prev_was_space = True
-        else:
-            if prev_was_space:
-                new_text += " "
-                prev_was_space = False
-            new_text += char
     return new_text
