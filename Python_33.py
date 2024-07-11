61ff7b436329d522602ba055893a785bff16d8e4
beep boop <githubbot@vadim.me>
2024-07-11 04:59:47 +0200

    Solution 89 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/z1kvmlai Prompt: 9ed.py", line 18, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/68135ef3-e047-45de-8b65-1a5fde314 9ed.py", line 13, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/68135ef3-e047-45de-8b65-1a5fde314 9ed.py", line 2, in sort_third     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/68135ef3-e047-45de-8b65-1a5fde314 9ed.py", line 7, in <listcomp>     [y for y in l if (i - 1) // 3 * 3 + j == i - 1 and j % 3 != 0][::-1]     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/68135ef3-e047-45de-8b65-1a5fde314 9ed.py", line 7, in <listcomp>     [y for y in l if (i - 1) // 3 * 3 + j == i - 1 and j % 3 != 0][::-1]                                         ^ NameError: name 'j' is not defined

--- Python_33.py
+++ Python_33.py
@@ -3,9 +3,7 @@ def sort_third(l: list):
         (
             x
             if i % 3 != 0
-            else sorted(
-                [y for y in l if (i_val := i) - 1 // 3 * 3 + j == i_val - 1 and j % 3 != 0][::-1]
-            )[0]
+            else sorted([y for y in l if (i_val - 1) // 3 * 3 + j == i_val - 1 and j % 3 != 0][::-1])[0]
         )
         for i, x in enumerate(l)
     ]
