ede61ff76f0b0afe8c547e5514289eb02dc32e3e
beep boop <githubbot@vadim.me>
2024-07-11 22:09:33 +0200

    Solution 67 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/joxoawd6 Prompt: it must return 105 for input 9/35619/5/X6124219-, but it returns 83.

--- bowling.cpp
+++ bowling.cpp
@@ -4,37 +4,43 @@ using namespace std;
 int calculateScore(string s) {
     int total = 0;
     int frame = 0;
-    for (int i = 0; i < s.size(); i++) {
-        switch (s[i]) {
+    int i = 0;
+    
+    while (frame < 10) {
+        switch(s[i]) {
             case 'X':
                 total += 10;
-                if (s[i+2] == 'X') {
+                if (s[i+2] == '/')
                     total += 10;
-                    if (s[i+4] == 'X') {
-                        total += 10;
-                    } else if (s[i+4] == '/') {
-                        total += 10 - (s[i+3] - '0');
-                    } else {
-                        total += s[i+3] - '0' + s[i+4] - '0';
-                    }
-                } else if (s[i+3] == '/') {
-                    total += 10;
-                } else {
-                    total += s[i+2] - '0' + s[i+3] - '0';
-                }
-                frame++;
+                else
+                    total += s[i+1] == 'X' ? 10 : s[i+1] - '0';
+                i++;
                 break;
-            
+                
             case '/':
                 total += 10 - (s[i-1] - '0');
                 break;
                 
             default:
                 total += s[i] - '0';
+                if (s[i+1] == '/')
+                    total += 10 - (s[i] - '0');
+                else
+                    total += s[i+1] - '0';
+                i++;
+                break;
         }
-        if (frame == 10) {
-            break;
-        }
+        
+        i++;
+        frame++;
     }
+    
     return total;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << calculateScore(s) << endl;
+    return 0;
 }
