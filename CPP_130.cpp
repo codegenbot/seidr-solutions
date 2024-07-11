1120af3143136edefda8668afe265b8b1e995585
beep boop <githubbot@vadim.me>
2024-07-11 15:11:35 +0200

    Solution 9 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/d5txd85m Prompt:       |                    ^~~ f51a6224-83a1-41c9-a2d2-8ed776571225.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f51a6224-83a1-41c9-a2d2-8ed776571225.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f51a6224-83a1-41c9-a2d2-8ed776571225.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f51a6224-83a1-41c9-a2d2-8ed776571225.cpp:14: f51a6224-83a1-41c9-a2d2-8ed776571225.cpp: In function ‘int main()’: f51a6224-83a1-41c9-a2d2-8ed776571225.cpp:24:20: error: ‘tri’ was not declared in  this scope    24 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ f51a6224-83a1-41c9-a2d2-8ed776571225.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -2,13 +2,9 @@
 #include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
+    if (a.size() != b.size()) return false;
     for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
@@ -21,7 +17,7 @@ std::vector<int> tri(int n) {
         if (i % 2 == 0) {
             result.push_back(1 + i / 2);
         } else {
-            result.push_back(result[i - 1] + result[i - 2] + result[i - 3]);
+            result.push_back(result[i - 1] + result[i - 2] + result[i - 1]);
         }
     }
     return result;
