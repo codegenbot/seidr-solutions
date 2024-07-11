67e3c05af38ca012c64547b8ddbc00efe6eb9690
beep boop <githubbot@vadim.me>
2024-07-11 16:50:23 +0200

    Solution 96 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/lwjwgefn Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d0081a78-a0f3-45f3-a05a-2dabbc3a2 6f6.py", line 1, in <module>     f"Your tweet has {len(re.sub(r'w+', '', tweet))} characters"                           ^^ NameError: name 're' is not defined

--- twitter.py
+++ twitter.py
@@ -1,10 +1,2 @@
 import re
-
-input_str = input("Enter a tweet: ")
-
-if len(input_str) > 140:
-    print("Too many characters")
-elif input_str == "":
-    print("You didn't type anything")
-else:
-    print(f"Your tweet has {len(input_str)} characters")
+f"Your tweet has {len(re.sub(r'\s+', '', tweet))} characters"
