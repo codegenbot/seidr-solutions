31ba7852e52a1daf2789b0adefe493280f5a08bd
beep boop <githubbot@vadim.me>
2024-07-11 14:08:32 +0200

    Solution 67 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/dlv2ukls Prompt: 087121b9-50f8-4962-b77e-93e4fc41fd48.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int bowlingScore(std::string s) {       |                       ^~~~~~ 087121b9-50f8-4962-b77e-93e4fc41fd48.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int bowlingScore(std::string s) {

--- bowling.cpp
+++ bowling.cpp
@@ -2,46 +2,45 @@
 
 int bowlingScore(std::string s) {
     int score = 0;
+    int roll1 = 0;
+    int roll2 = 0;
+    bool firstFrame = true;
+
     for(int i = 0; i < s.size(); i++) {
         if(s[i] == '/') {
-            score += (roll1 + roll2) * 10;
+            if(firstFrame) {
+                score += 10;
+            } else {
+                score += (roll1 + roll2);
+            }
+            firstFrame = false;
             roll1 = 0;
             roll2 = 0;
         } else if('0' <= s[i] && s[i] <= '9') {
-            int roll = (s[i] - '0');
-            if(roll1 == 0) {
-                roll1 = roll;
+            int value = s[i] - '0';
+            if(firstFrame) {
+                roll1 = value;
             } else {
-                roll2 = roll;
-                score += roll1 * 10 + (10 > roll2 ? roll2 : 10);
-                roll1 = 0;
-                roll2 = 0;
+                roll2 = value;
             }
         } else if(s[i] == 'X') {
             score += 10;
-            if(roll1 != 0) {
-                score += roll1 * 10 + (10 > roll2 ? roll2 : 10);
+            if(firstFrame) {
+                roll1 = 10;
+            } else {
+                roll1 + roll2 > 10 ? score += 10 : score += roll1 + roll2;
+                firstFrame = false;
                 roll1 = 0;
                 roll2 = 0;
             }
-        } else if(s[i] == 'S') {
-            int roll = 5;
-            if(roll1 != 0) {
-                score += roll1 * 10 + (10 > roll2 ? roll2 : 10);
-                roll1 = 0;
-                roll2 = 0;
-            }
-            score += roll * 10;
         }
     }
-    if(roll1 != 0 || roll2 != 0) {
-        if(roll1 == 0) {
-            score += roll2 * 10;
-        } else if(roll2 == 0) {
-            score += roll1 * 10 + (10 > roll2 ? roll2 : 10);
-        } else {
-            score += roll1 * 10 + roll2;
-        }
+
+    if(firstFrame) {
+        score += roll1 + roll2;
+    } else {
+        roll1 + roll2 > 10 ? score += 10 : score += roll1 + roll2;
     }
+
     return score;
 }
