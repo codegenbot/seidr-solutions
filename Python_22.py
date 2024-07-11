56deb70731121bdf1bb1f5e90620157891d98200
beep boop <githubbot@vadim.me>
2024-07-11 09:32:36 +0200

    Solution 55 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5v09uzk5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e0e12160-66f1-45a6-84f8-1ec43e4e0 705.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,17 +1,16 @@
-```
 from typing import List
 
 def get_values() -> List[int]:
+    user_values = input("Enter some values (separated by space), 'stop' to finish: ")
     result = []
     while True:
-        values = input("Enter some values (separated by space), 'stop' to finish: ")
-        if values.lower() == 'stop':
+        if user_values.lower() == 'stop':
             break
-        for value in values.split():
+        for value in user_values.split():
             if value.isdigit():
                 result.append(int(value))
             else:
                 print(f"Invalid input: {value}. Please enter integers only.")
-        if all(val.replace(".", "", 1).replace("-", "", 1).isdigit() for val in values.split()):
+        if all(val.replace(".", "", 1).replace("-", "", 1).isdigit() for val in user_values.split()):
             break
     return result
