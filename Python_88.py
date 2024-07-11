16c0d5b248990f808f652c430a4276a29d1f8824
beep boop <githubbot@vadim.me>
2024-07-11 11:26:06 +0200

    Solution 50 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/er6nmf1f Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/133ecd7d-fd24-4100-8310-7a7ddec0a 466.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_88.py
+++ Python_88.py
@@ -1,5 +1,4 @@
-def sort_array():
-    array = list(map(int, input("Enter space-separated numbers: ").split()))
+def sort_array(array):
     sorted_array = sorted(array)
     n = len(sorted_array)
 
@@ -7,4 +6,4 @@ def sort_array():
         return [sorted_array[n//2-1], sorted_array[n//2]]
     else:
         median = (sorted_array[n//2 - 1] + sorted_array[n//2]) / 2.0
-        return [[median]]
+        return [median]
