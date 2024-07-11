8dc7a3121cf641128fe468bca6b119b1f2ebbf21
beep boop <githubbot@vadim.me>
2024-07-11 06:14:11 +0200

    Solution 4 of CPP/43, ratio 0.7777777777777778 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ha8to3ix Prompt: b7179e3c-7fe5-4e83-a74f-c9d20060ab09.cpp: In function ‘bool pairs_sum_to_zero(st d::vector<int>)’: b7179e3c-7fe5-4e83-a74f-c9d20060ab09.cpp:5:5: error: ‘set’ was not declared in t his scope     5 |     set<int> s(l.begin(), l.end());       |     ^~~ b7179e3c-7fe5-4e83-a74f-c9d20060ab09.cpp:2:1: note: ‘std::set’ is defined in hea der ‘<set>’; did you forget to ‘#include <set>’?     1 | #include <vector>   +++ |+#include <set>     2 | using namespace std; b7179e3c-7fe5-4e83-a74f-c9d20060ab09.cpp:5:9: error: expected primary-expression  before ‘int’     5 |     set<int> s(l.begin(), l.end());       |         ^~~ b7179e3c-7fe5-4e83-a74f-c9d20060ab09.cpp:6:18: error: ‘s’ was not declared in th is scope     6 |     for (int i : s) {       |                  ^

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,8 +1,10 @@
 #include <vector>
+#include <set>
+
 using namespace std;
 
 bool pairs_sum_to_zero(vector<int> l) {
-    set<int> s(l.begin(), l.end());
+    set<int> s(set<int>(l.begin(), l.end()));
     for (int i : s) {
         if (s.count(-i)) return true;
     }
