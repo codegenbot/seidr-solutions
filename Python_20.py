96d014ce55af45c1a279058e1a106ca0e829e64d
beep boop <githubbot@vadim.me>
2024-07-11 15:46:15 +0200

    Solution 61 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/7mtn3xy5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e32642f7-29c2-4f08-922a-1f6033786 ef4.py", line 20, in <module>     check(find_closest_elements)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e32642f7-29c2-4f08-922a-1f6033786 ef4.py", line 17, in check     assert find_closest_elements([1.1, 2.2, 3.1, 4.1, 5.1]) == (2.2, 3.1)            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: find_closest_elements() missing 1 required positional argument: 'targ et'

--- Python_20.py
+++ Python_20.py
@@ -1,15 +1,16 @@
-def find_closest_elements(arr1, arr2, target):
-    merged_array = sorted([num for num in arr1 + arr2])
+def closest_elements_to_target(arr1, arr2, target):
+    combined_array = sorted(arr1 + arr2)
+    
     left = 0
-    right = len(merged_array) - 1
-
+    right = len(combined_array) - 1
+    
     while True:
         if left == right:
-            return [merged_array[left], merged_array[left]]
-
-        if target < (right - left) // 2 * (merged_array[right] - merged_array[left]) + merged_array[left]:
+            return [combined_array[left], combined_array[left]]
+        
+        if target < (right - left) // 2 * (combined_array[right] - combined_array[left]):
             right = left + (right - left) // 2
         else:
             left = right - (right - left) // 2 + 1
-
-    return [merged_array[left], merged_array[right]]
+    
+    return [combined_array[left], combined_array[right]]
