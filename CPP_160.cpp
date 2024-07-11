0a8e7e07de4b20988151835fd3460126243ff01f
beep boop <githubbot@vadim.me>
2024-07-11 11:41:52 +0200

    Solution 12 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mjzf4cav Prompt:       |                       ^~~~~~ 4d107c60-bc4a-44ce-8ba1-1830921cd61a.cpp:1:31: error: ‘operato’ was not declared  in this scope     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                               ^~~~~~~ 4d107c60-bc4a-44ce-8ba1-1830921cd61a.cpp:1:40: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                                        ^~~~~~ 4d107c60-bc4a-44ce-8ba1-1830921cd61a.cpp:1:47: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                                               ^~~ 4d107c60-bc4a-44ce-8ba1-1830921cd61a.cpp:1:59: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                                                           ^ In file included from 4d107c60-bc4a-44ce-8ba1-1830921cd61a.cpp:21: 4d107c60-bc4a-44ce-8ba1-1830921cd61a.cpp: In function ‘int main()’: 4d107c60-bc4a-44ce-8ba1-1830921cd61a.cpp:23:23: error: ‘do_algebra’ cannot be us ed as a function    23 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,32 +1,19 @@
-```cpp
-#include <vector>
-#include <cmath>
-
-int do_algebra(vector<string> operators, vector<int> operands) {
+int do_algebra(vector<string> operators, vector<int> operand) {
     int result = 0;
-    for (int i = 1; i < operands.size(); i++) {
-        if (*operators.begin() == "+") {
-            result += operands[i];
-        } else if (*operators.begin() == "-") {
-            result -= operands[i];
-        } else if (*operators.begin() == "*") {
-            result *= operands[i];
-        } else if (*operators.begin() == "//") {
-            if (operands[i] != 0) {
-                result = result / operands[i];
-            } else {
-                return 0;
-            }
-        } else if (*operators.begin() == "**") {
-            result = pow(result, operands[i]);
+    int temp = operand[0];
+    for (int i = 1; i < operand.size(); i++) {
+        if (*min_element(operators.begin(), operators.end()) == "+") {
+            result += temp + operand[i];
+        } else if (*min_element(operators.begin(), operators.end()) == "-") {
+            result -= temp - operand[i];
+        } else if (*min_element(operators.begin(), operators.end()) == "*") {
+            result *= temp * operand[i];
+        } else if (*min_element(operators.begin(), operators.end()) == "//") {
+            result = temp / (int)round((double)operand[i]);
+        } else if (*min_element(operators.begin(), operators.end()) == "**") {
+            result = pow(temp, operand[i]);
         }
+        operators.erase(min_element(operators.begin(), operators.end()));
     }
     return result;
-}
-
-int main() {
-    vector<string> operators = {"*", "+"};
-    vector<int> operands = {2, 3, 4};
-    cout << do_algebra(operators, operands) << endl;
-    return 0;
 }
