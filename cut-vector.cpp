6e8797bc58c26f1e7d4bc83a29f6f7a10ddb5825
beep boop <githubbot@vadim.me>
2024-07-11 10:41:44 +0200

    Solution 60 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/66vz1sa5 Prompt: /usr/include/c++/11/bits/stl_vector.h:510:51: note:   no known conversion for ar gument 2 from ‘std::vector<int>’ to ‘const allocator_type&’ {aka ‘const std::all ocator<int>&’}   510 |       vector(size_type __n, const allocator_type& __a = allocator_type() )       |                             ~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_vector.h:497:7: note: candidate: ‘std::vector<_Tp, _Alloc>::vector(const allocator_type&) [with _Tp = int; _Alloc = std::allocator< int>; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<int>]’   497 |       vector(const allocator_type& __a) _GLIBCXX_NOEXCEPT       |       ^~~~~~ /usr/include/c++/11/bits/stl_vector.h:497:7: note:   candidate expects 1 argumen t, 2 provided /usr/include/c++/11/bits/stl_vector.h:487:7: note: candidate: ‘std::vector<_Tp, _Alloc>::vector() [with _Tp = int; _Alloc = std::allocator<int>]’   487 |       vector() = default;       |       ^~~~~~ /usr/include/c++/11/bits/stl_vector.h:487:7: note:   candidate expects 0 argumen ts, 2 provided 7362c942-ae14-49bd-9a7a-2dfa29581aa4.cpp:20:1: warning: control reaches end of n on-void function [-Wreturn-type]    20 | }       | ^

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -10,8 +10,10 @@ vector<vector<int>> cutVector(vector<int>& nums) {
         else if(diff > 0 && (min_idx == -1 || i < min_idx)) min_idx = i;
     }
     if(min_idx == -1) {
-        return {{}, {}};
-    } else {
+        vector<int> res;
+        return {{res}, {res}};
+    } 
+    else {
         vector<int> left(min_idx+1), right(n-min_idx-1);
         for(int i=0; i<=min_idx; i++) left.push_back(nums[i]);
         for(int i=min_idx+1; i<n; i++) right.push_back(nums[i]);
