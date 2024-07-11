f5e672847c2d202e86df66dc4be9bcc7628a8894
beep boop <githubbot@vadim.me>
2024-07-11 22:56:52 +0200

    Solution 76 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/u5pl03lv Prompt: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 49552d67-2d83-48f9-ab8a-a00a2c203f0b.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 49552d67-2d83-48f9-ab8a-a00a2c203f0b.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 49552d67-2d83-48f9-ab8a-a00a2c203f0b.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 49552d67-2d83-48f9-ab8a-a00a2c203f0b.cpp:21: 49552d67-2d83-48f9-ab8a-a00a2c203f0b.cpp: In function ‘int main()’: 49552d67-2d83-48f9-ab8a-a00a2c203f0b.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
 #include <vector>
 #include <string>
 #include <cassert>
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
 
 std::vector<int> parse_music(std::string music) {
     std::vector<int> result;
@@ -18,6 +23,8 @@ std::vector<int> parse_music(std::string music) {
     return result;
 }
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
+int main() {
+    assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 4, 2, 4, 2}));
+    
+    return 0;
 }
