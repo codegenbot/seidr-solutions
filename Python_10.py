babadd5fb5da7f74dda308b68e61daa95df2d954
beep boop <githubbot@vadim.me>
2024-07-11 16:14:47 +0200

    Solution 82 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tdfielxa Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/864d6b74-275c-4469-beca-7beb082eb 3b0.py", line 20, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/864d6b74-275c-4469-beca-7beb082eb 3b0.py", line 17, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -3,16 +3,10 @@ def make_palindrome(s: str) -> str:
     i, j = 0, len(s) - 1
     while i < j:
         if s[i] != s[j]:
-            t = s[j]
-            for k in range(len(s) - 1, -1, -1):
-                if s[k] == t:
-                    if i <= k:  
-                        s[:k - i + 1] = list(t * (k - i + 1))
-                    else:  
-                        s[k:] = list(t * (j - k))
+            for k in range(len(s) - 1, j, -1):
+                if s[k] == s[j]:
+                    s[k + 1:j+1] = list(s[j]*abs(j-i))
                     break
-            else:
-                s[: j + 1] = list(s[j] * (j + 1))
         i += 1
         j -= 1
     return "".join(s)
