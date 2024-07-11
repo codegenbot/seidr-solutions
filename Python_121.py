3292c272e9379045e9bdd4aaca5d1fc1ab3763b9
beep boop <githubbot@vadim.me>
2024-07-11 11:24:30 +0200

    Solution 13 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rzs1si5s Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/659dc5a2-1625-43b5-8aa2-6ec10f6d0 7ba.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,2 +1,5 @@
-def solution(lst):
+def 'This function takes a list as input and returns the sum of all odd numbers in that list.'':
+    """
+    This function takes a list as input and returns the sum of all odd numbers in that list.
+    """
     return sum(i for i in lst if i % 2 != 0)
