fd3ca35e1503fbfa20e4d8f444a7ef2fd798e03d
beep boop <githubbot@vadim.me>
2024-07-11 19:09:49 +0200

    Solution 2 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/fy1zocty Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,28 +1,28 @@
-def bowling(input_str):
+def bowling(score_str):
     score = 0
-    frames = input_str.split("/")
-    for i in range(len(frames)):
-        if frames[i] == "X":
-            score += 10
-            if i < 9:
-                if input_str[i + 1] == "X":
-                    score += 10
-                else:
-                    score += int(input_str[i + 1])
-                    if input_str[i + 2] == "/":
-                        score += 10 - int(input_str[i + 1])
-                    elif input_str[i + 2] != "-":
-                        score += int(input_str[i + 2])
+    frame = 1
+    rolls = []
+    for char in score_str:
+        if char == "X":
+            rolls.append(10)
+            if frame < 10:
+                rolls.append(0)
+        elif char == "/":
+            rolls.append(10 - rolls[-1])
+        elif char == "-":
+            rolls.append(0)
         else:
-            for bowl in frames[i]:
-                if bowl == "X":
-                    score += 10
-                elif bowl == "-":
-                    score += 0
-                else:
-                    score += int(bowl)
+            rolls.append(int(char))
+
+        while len(rolls) >= 2 and (
+            rolls[-2] == 10 or rolls[-1] == 10 or rolls[-2] + rolls[-1] == 10
+        ):
+            score += rolls.pop(0)
+
+        frame += 1
+
     return score
 
 
-input_str = input()
-print(bowling(input_str))
+score_str = input()
+print(bowling(score_str))
