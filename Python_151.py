4989bfa248efda3c851cd85e3de5b25c8444b617
beep boop <githubbot@vadim.me>
2024-07-11 15:36:11 +0200

    Solution 21 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/juryj07u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e46124d-e8fc-42cc-8df1-6368299b4 568.py", line 6, in <module>     check(double_the_difference())     ^^^^^ NameError: name 'check' is not defined

--- Python_151.py
+++ Python_151.py
@@ -1,11 +1,10 @@
+```
 def check(func):
     return func([1, 3, 5]) == 6 or func([4, 2, 10]) == 8
-
 
 def double_the_difference(lst):
     difference = max(lst) - min(lst)
     double_diff = difference * 2
     return double_diff
-
 
 check(double_the_difference())
