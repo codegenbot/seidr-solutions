24a9f43dc6ae8049e49caec33967cfccac895a58
beep boop <githubbot@vadim.me>
2024-07-11 09:07:37 +0200

    Solution 8 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v70dnp3u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e49e9413-daf6-4a31-9260-1f467b8b1 d76.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e49e9413-daf6-4a31-9260-1f467b8b1 d76.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,5 @@
 def sort_even(l: list):
-    evens = [x for x in l if x % 2 == 0]
-    odds = [x for x in l if x % 2 != 0]
-    sorted_evens = sorted(evens)
-    return [x for i, x in enumerate(sorted_evens) if i % 2 != 0] + odds
+    l.sort(key=lambda x: (x % 2, x))
+    return [x for i, x in enumerate(l) if i % 2 != 0] + l[:][
+        l.index(min(x for x in l if x % 2 == 0)) : :
+    ]
