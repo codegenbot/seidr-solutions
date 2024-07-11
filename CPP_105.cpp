766f535a28eb09eff337dc4f2e996557b36d2c8c
beep boop <githubbot@vadim.me>
2024-07-11 09:49:44 +0200

    Solution 1 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xzmxc8nx Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,47 +1,48 @@
 vector<string> by_length(vector<int> arr) {
-    vector<int> numbers;
+    vector<int> nums;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            numbers.push_back(num);
+            nums.push_back(num);
         }
     }
-    
-    sort(numbers.begin(), numbers.end());
-    
-    reverse(numbers.begin(), numbers.end());
-    
+
+    sort(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.end());
+
     vector<string> result;
-    for (int num : numbers) {
+    for (int num : nums) {
+        string str = "";
         switch (num) {
             case 1:
-                result.push_back("One");
+                str = "One";
                 break;
             case 2:
-                result.push_back("Two");
+                str = "Two";
                 break;
             case 3:
-                result.push_back("Three");
+                str = "Three";
                 break;
             case 4:
-                result.push_back("Four");
+                str = "Four";
                 break;
             case 5:
-                result.push_back("Five");
+                str = "Five";
                 break;
             case 6:
-                result.push_back("Six");
+                str = "Six";
                 break;
             case 7:
-                result.push_back("Seven");
+                str = "Seven";
                 break;
             case 8:
-                result.push_back("Eight");
+                str = "Eight";
                 break;
             case 9:
-                result.push_back("Nine");
+                str = "Nine";
                 break;
         }
+        result.push_back(str);
     }
-    
+
     return result;
 }
