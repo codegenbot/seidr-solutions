d1cd330b10cea062203787e1f91d89d58a6aec2d
beep boop <githubbot@vadim.me>
2024-07-11 10:44:48 +0200

    Solution 1 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/k63p2myk Prompt: Given a string representing a tweet, validate whether the tweet meets Twitter’s original character requirements. If the tweet has more than 140 characters, return the string "Too many characters". If the tweet is empty, return the string "You didn't type anything". Otherwise, return "Your tweet has X characters", where the X is the number of characters in the tweet.

--- twitter.cpp
+++ twitter.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
+string validateTweet(string tweet) {
+    if(tweet.empty()) return "You didn't type anything";
+    if(tweet.length() > 140) return "Too many characters";
+    return "Your tweet has " + to_string(tweet.length()) + " characters";
+}
+
 int main() {
-    std::string tweet;
-    std::cout << "Enter your tweet: ";
-    std::getline(std::cin, tweet);
-    
-    if(tweet.empty()) {
-        std::cout << "You didn't type anything" << std::endl;
-    } else if(tweet.length() > 140) {
-        std::cout << "Too many characters" << std::endl;
-    } else {
-        std::cout << "Your tweet has " << tweet.length() << " characters" << std::endl;
-    }
-    
+    string tweet;
+    cout << "Enter a tweet: ";
+    getline(cin, tweet);
+    cout << validateTweet(tweet) << endl;
     return 0;
 }
