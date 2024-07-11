f3e8c2bc612fef3749298dba57507fd13c996397
beep boop <githubbot@vadim.me>
2024-07-11 07:54:37 +0200

    Solution 33 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d6xmmywr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aa8fc1e1-6522-4403-8675-a5c41cc53 311.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aa8fc1e1-6522-4403-8675-a5c41cc53 311.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,14 +1,6 @@
 def double_the_difference(lst, target_diff):
-    even_numbers = [i for i in lst if isinstance(i, int) and i >= 0 and i % 2 == 0]
-    min_even = min(even_numbers)
-    max_even = max(even_numbers)
-
-    if max_even - min_even < target_diff:
-        return 0
-    elif max_even - min_even > target_diff:
-        return sum(i**2 for i in even_numbers if isinstance(i, int) and i % 2 == 0)
-    else:
-        first_half = [i for i in even_numbers if i <= min_even + (target_diff / 2)]
-        second_half = [i for i in even_numbers if i >= min_even + target_diff / 2]
-
-        return sum(i**2 for i in first_half) - sum(i**2 for i in second_half)
+    even_numbers = [i for i in lst if isinstance(i, int) and i % 2 == 0]
+    pairs = [
+        (a, b) for a in even_numbers for b in even_numbers if abs(a - b) == target_diff
+    ]
+    return sum([pair[0] ** 2 + pair[1] ** 2 for pair in pairs])
