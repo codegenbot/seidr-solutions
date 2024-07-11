1c36f95ed6d82d9825fc2fdae95cab8c56ae3da3
beep boop <githubbot@vadim.me>
2024-07-11 12:29:24 +0200

    Solution 45 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mq03fasb Prompt: 55462bed-27b2-4783-88c3-8bce1a7121cc.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int number) {       | ^~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -11,9 +11,6 @@ string int_to_mini_roman(int number) {
             roman += numeral.second;
             number -= numeral.first;
         }
-        if (number > 0) {
-            roman += "I";
-            number--;
-        }
     }
     return roman;
+}
