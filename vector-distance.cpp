7a877e7fab211d996dd150eea9c2d2ef5d57b016
beep boop <githubbot@vadim.me>
2024-07-11 22:30:37 +0200

    Solution 79 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/9zdgihs1 Prompt: n this scope    17 |         sum += pow(vec1[i] - vec2[i], 2);       |                    ^~~~ 9b2827af-6863-4586-854b-84fb83daa76e.cpp:17:30: error: ‘vec2’ was not declared i n this scope    17 |         sum += pow(vec1[i] - vec2[i], 2);       |                              ^~~~ 9b2827af-6863-4586-854b-84fb83daa76e.cpp:17:16: error: ‘pow’ was not declared in  this scope    17 |         sum += pow(vec1[i] - vec2[i], 2);       |                ^~~ 9b2827af-6863-4586-854b-84fb83daa76e.cpp:20:5: error: ‘cout’ was not declared in  this scope    20 |     cout << sqrt(sum) << endl;       |     ^~~~ 9b2827af-6863-4586-854b-84fb83daa76e.cpp:20:13: error: ‘sqrt’ was not declared i n this scope    20 |     cout << sqrt(sum) << endl;       |             ^~~~ 9b2827af-6863-4586-854b-84fb83daa76e.cpp:20:26: error: ‘endl’ was not declared i n this scope    20 |     cout << sqrt(sum) << endl;       |                          ^~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -18,10 +18,10 @@ int main() {
 
     double sum = 0.0;
     for (int i = 0; i < n; ++i) {
-        sum += pow(vec1[i] - vec2[i], 2);
+        sum += std::pow(vec1[i] - vec2[i], 2);
     }
 
-    std::cout << sqrt(sum) << std::endl;
+    std::cout << std::sqrt(sum) << std::endl;
 
     return 0;
 }
