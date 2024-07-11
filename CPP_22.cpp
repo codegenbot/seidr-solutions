4999885b297e3ad91620f39d35fba0dbf8f97d66
beep boop <githubbot@vadim.me>
2024-07-11 12:06:46 +0200

    Solution 44 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/bp0n055l Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9e6b6b30-aca2-4910-ae3b-e0042f5c94e5.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9e6b6b30-aca2-4910-ae3b-e0042f5c94e5.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9e6b6b30-aca2-4910-ae3b-e0042f5c94e5.cpp:11: 9e6b6b30-aca2-4910-ae3b-e0042f5c94e5.cpp: In function ‘int main()’: 9e6b6b30-aca2-4910-ae3b-e0042f5c94e5.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 9e6b6b30-aca2-4910-ae3b-e0042f5c94e5.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,26 +1,18 @@
-#include <iostream>
-#include <vector>
-#include <boost/any.hpp>
-
-using namespace std;
-
 vector<int> filter_integers(list_any values);
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-vector<int> filter_integers(list_any values){
-    vector<int> result;
-    for(auto& val : values){
-        if(typeid(int) == val.type()){
-            result.push_back(boost::any_cast<int>(val));
+bool issame(vector<int> a, vector<int> b){
+    if(a.size() != b.size()){
+        return false;
+    }
+    for(size_t i = 0; i < a.size(); ++i){
+        if(a[i] != b[i]){
+            return false;
         }
     }
-    return result;
+    return true;
 }
 
-int main() {
-    // Main function code here
+int main(){
+    // Your main function code here
     return 0;
 }
