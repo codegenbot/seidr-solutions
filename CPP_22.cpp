93113cf25121243d43de3918f0225147acf8571e
beep boop <githubbot@vadim.me>
2024-07-11 06:05:41 +0200

    Solution 68 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2dqf0nts Prompt: In file included from 9bf7f02d-f7b6-4cc4-89f8-673c548f24ac.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9bf7f02d-f7b6-4cc4-89f8-673c548f24ac.cpp:33:34: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9bf7f02d-f7b6-4cc4-89f8-673c548f24ac.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ 9bf7f02d-f7b6-4cc4-89f8-673c548f24ac.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from 9bf7f02d-f7b6-4cc4-89f8-673c548f24ac.cpp:32: 9bf7f02d-f7b6-4cc4-89f8-673c548f24ac.cpp: In function ‘int main()’: 9bf7f02d-f7b6-4cc4-89f8-673c548f24ac.cpp:42:35: error: no match for call to ‘(st d::vector<int>) (<brace-enclosed initializer list>)’    42 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,27 +1,37 @@
-#include <iostream>
-#include <list>
 #include <vector>
 #include <any>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
 
 std::vector<int> filter_integers(std::list<std::any> values) {
     std::vector<int> result;
     for (const auto& value : values) {
         if (value.type() == typeid(int)) {
-            int num = std::any_cast<int>(value);
-            bool flag = false;
-            for(const auto& i: result){
-                if(i==num)flag=true;break;
-            }
-            if(!flag)result.push_back(num);
+            int integer = std::any_cast<int>(value);
+            result.push_back(integer);
         }
     }
     return result;
 }
 
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
-    assert (issame(filter_integers({3, 3, 'c', 'a', 'b'}), {1, 2, 3}) == false);
+    std::any a = 3;
+    std::any b = 'c';
+    std::any c = 3;
+    std::any d = 3;
+    std::any e = 'a';
+    std::any f = 'b';
+    std::list<std::any> values = {a, b, c, d, e, f};
+    assert(issame(filter_integers(values) ,{3, 3, 3}));
+    return 0;
 }
