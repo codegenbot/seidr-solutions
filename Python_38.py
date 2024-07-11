a82404a1eda036efa2822e1b1dc62756fff40f54
beep boop <githubbot@vadim.me>
2024-07-11 18:33:15 +0200

    Solution 32 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/9ng5poo5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3ef838c-384d-4013-8cc8-33ef70bea df0.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_38.py
+++ Python_38.py
@@ -3,16 +3,15 @@ def decode_cyclic(s: str):
     result = ""
     temp_group = ""
     for char in s:
-        temp_group += char
-        if len(temp_group) == 3:
-            if len(result) > 0 and len(result[-1]) == 3:
-                result[-1] = temp_group[2] + temp_group[1] + temp_group[0]
-            else:
-                result += temp_group
-            temp_group = ""
-    if len(temp_group) > 0:
-        if len(result) > 0 and len(result[-1]) == 3:
-            result[-1] = temp_group[2] + temp_group[1] + temp_group[0]
+        if len(temp_group) >= 3 and temp_group[0] == temp_group[-1]:
+            while len(temp_group) > 0 and (temp_group[0] == temp_group[-1]):
+                result += temp_group[0]
+                temp_group = temp_group[1:-1]
         else:
-            result += temp_group
+            temp_group += char
+    if len(temp_group) > 0:
+        while len(temp_group) > 0 and (temp_group[0] == temp_group[-1]):
+            result += temp_group[0]
+            temp_group = temp_group[1:-1]
+        result += temp_group
     return result
