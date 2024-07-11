d12b163bab63a01445200d4a324a33ec4922515c
beep boop <githubbot@vadim.me>
2024-07-11 19:04:59 +0200

    Solution 60 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h0l13eex Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b095d755-1244-4a4f-80c3-ec4c9ba4d 476.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,5 +1,2 @@
-def third_order_string(s: str):
-    words = s.split()
-    sorted_words = sorted(words)
-    result = " ".join(sorted_words)
-    return result
+def sort_third(l: list):
+    return sorted([x[2] for x in l])
