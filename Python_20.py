74a3bccc2c24367773af549232c3e1cdc5cc6d28
beep boop <githubbot@vadim.me>
2024-07-11 09:15:09 +0200

    Solution 40 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4lreuc04 Prompt: (1.1, 5.1) Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9aa005a8-39f0-4271-830a-a9d6b4034 08f.py", line 16, in <module>     check(find_closest_elements)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9aa005a8-39f0-4271-830a-a9d6b4034 08f.py", line 13, in check     assert find_closest_elements([1.1, 2.2, 3.1, 4.1, 5.1]) == (2.2, 3.1)            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_20.py
+++ Python_20.py
@@ -1,15 +1,8 @@
 ```
 def find_closest_elements(nums: list)-> tuple:
-    min_diff = float('inf')
-    closest_pair = ()
-    
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            diff = abs(nums[i] - nums[j])
-            if diff < min_diff:
-                min_diff = diff
-                closest_pair = (nums[i], nums[j])
-    
-    return closest_pair
+    nums = sorted(set(nums))
+    if len(nums) == 1:
+        return (nums[0], nums[0])
+    return (nums[0], nums[-1])
 
 print(find_closest_elements([1.1, 2.2, 3.1, 4.1, 5.1]))
