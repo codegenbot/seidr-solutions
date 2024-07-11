8fcc95fe229eed97203fccf40c5f7c3716d0444c
beep boop <githubbot@vadim.me>
2024-07-11 18:05:38 +0200

    Solution 63 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/d31cl2gl Prompt: in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f191d960-0853-4b80-b5b6-819df3179909.cpp:8:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from f191d960-0853-4b80-b5b6-819df3179909.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from f191d960-0853-4b80-b5b6-819df3179909.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ f191d960-0853-4b80-b5b6-819df3179909.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from f191d960-0853-4b80-b5b6-819df3179909.cpp:7: f191d960-0853-4b80-b5b6-819df3179909.cpp: In function ‘int main()’: f191d960-0853-4b80-b5b6-819df3179909.cpp:17:20: error: ‘make_a_pile’ was not dec lared in this scope    17 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -6,3 +6,24 @@ vector<int> make_a_pile(int n);
 bool issame(const vector<int>& a, const vector<int>& b) {
     return a == b;
 }
+
+int main() {
+    vector<int> a = make_a_pile(5);
+    vector<int> b = make_a_pile(5);
+
+    if (issame(a, b)) {
+        cout << "The piles are the same." << endl;
+    } else {
+        cout << "The piles are different." << endl;
+    }
+
+    return 0;
+}
+
+vector<int> make_a_pile(int n) {
+    vector<int> pile;
+    for (int i = 0; i < n; ++i) {
+        pile.push_back(i);
+    }
+    return pile;
+}
