c3a9526b19f7e1c0a0c6470a9cde6133c53a40ed
beep boop <githubbot@vadim.me>
2024-07-11 18:04:37 +0200

    Solution 19 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bh3tq9if Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,13 +1,24 @@
-```cpp
+#include <iostream>
+#include <cassert>
+#include <cmath>
+
+float triangle_area(float a, float b, float c) {
+    if ((a + b > c) && (a + c > b) && (b + c > a)) {
+        float s = (a + b + c) / 2.0;
+        return round((sqrt(s * (s - a) * (s - b) * (s - c))) * 100.0) / 100.0;
+    } else {
+        return -1.0f;
+    }
+}
+
 int main() {
-    int a, b, c;
+    float a, b, c;
     std::cout << "Enter the lengths of three sides of a triangle: ";
-    std::cin >> a >> b >> c;
+    std::cin >> std::fixed >> std::showpoint >> a >> b >> c; 
     float area = triangle_area(a, b, c);
     if (area == -1.0f)
         std::cout << "Not a valid triangle.\n";
     else
-        std::cout << "The area of the triangle is: " << static_cast<float>(a) << ", "
-                   << static_cast<float>(b) << ", " << static_cast<float>(c) << "\n";
+        std::cout << "The area of the triangle is: " << area << "\n";
     return 0;
 }
