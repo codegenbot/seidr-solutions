f06c03ab1b3f8580c7427cab34607f88d0d846bc
beep boop <githubbot@vadim.me>
2024-07-11 16:15:56 +0200

    Solution 52 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5n5qhplk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f53f3161-0577-4276-91e3-d70830416 f0d.py", line 16     except ValueError:     ^^^^^^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
     user_input = input("Enter space-separated integers: ")
     numbers = user_input.split()
     
-    for num in numbers:
-        int_num = int(num)
-        if not isinstance(int_num, int):
-            raise ValueError
-        
-    lst = [int(i) for i in numbers]
-    
     try:
+        for num in numbers:
+            int_num = int(num)
+            if not isinstance(int_num, int):
+                raise ValueError
+        
+        lst = [int(i) for i in numbers]
+        
         print(solution(lst))
     except ValueError:
-        print("     Invalid input. Please enter only integers.")
+        print("Invalid input. Please enter only integers.")
