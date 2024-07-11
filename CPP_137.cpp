a97905a67ebae4a92cc3371800e221a1d1f5f521
beep boop <githubbot@vadim.me>
2024-07-11 05:33:10 +0200

    Solution 90 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/497575qq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,22 +1,77 @@
 using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(double) && b.type() == typeid(double)) {
-        return (double)typeid(double)>((double)a,(double)b)?b:a;
+    if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return b;
     }
-    else if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return (int)typeid(int)>=(int)a,(int)b)?b:a;
+    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return b;
     }
-    else if ((a.type() == typeid(string) || a.type() == typeid(double)) &&
-             (b.type() == typeid(string) || b.type() == typeid(double))) {
-        if (boost::any_cast<double>(a) > boost::any_cast<double>(b))
-            return a;
-        else if (boost::any_cast<double>(a) < boost::any_cast<double>(b))
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string strA = any_cast<string>(a);
+        string strB = any_cast<string>(b);
+
+        size_t commaPos1 = strA.find(',');
+        size_t commaPos2 = strB.find(',');
+
+        double numA, numB;
+        if (commaPos1 != string::npos) {
+            std::istringstream is(strA.substr(0, commaPos1));
+            is >> numA;
+        }
+        else {
+            istringstream(is) >> numA;
+        }
+
+        if (commaPos2 != string::npos) {
+            std::istringstream is(strB.substr(0, commaPos2));
+            is >> numB;
+        }
+        else {
+            istringstream(is) >> numB;
+        }
+
+        return (numA > numB) ? a : b;
+    }
+    else if (a.type() == typeid(string)) {
+        string str = any_cast<string>(a);
+        size_t commaPos = str.find(',');
+        double num;
+
+        if (commaPos != string::npos) {
+            std::istringstream is(str.substr(0, commaPos));
+            is >> num;
+        }
+        else {
+            istringstream(is) >> num;
+        }
+
+        return (b.type() == typeid(double) && b.convert_to<double>() > num) ? b : a;
+    }
+    else if (b.type() == typeid(string)) {
+        string str = any_cast<string>(b);
+        size_t commaPos = str.find(',');
+        double num;
+
+        if (commaPos != string::npos) {
+            std::istringstream is(str.substr(0, commaPos));
+            is >> num;
+        }
+        else {
+            istringstream(is) >> num;
+        }
+
+        return (a.type() == typeid(double) && a.convert_to<double>() > num) ? a : b;
+    }
+
+    if (a.convert_to<int>() != a.convert_to<double>()) {
+        if (b.convert_to<int>() == b.convert_to<double>()) {
             return b;
-        else
-            return "None";
+        }
     }
-    else {
-        throw invalid_argument("Invalid input types");
+    else if (a.convert_to<int>() < a.convert_to<double>()) {
+        return b;
     }
+
+    return "None";
 }
