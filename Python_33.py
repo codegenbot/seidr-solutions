2ce93de663d5ba88fc69c0584020e34741aa8336
beep boop <githubbot@vadim.me>
2024-07-11 15:47:02 +0200

    Solution 56 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/qompfmfp Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f7af460e-4ee6-41f8-a71e-f3857a574 4f0.py", line 20, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f7af460e-4ee6-41f8-a71e-f3857a574 4f0.py", line 15, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -3,10 +3,10 @@ def sort_third(l: list):
     result = []
     for i, x in enumerate(l):
         if i % 3 == 0:
-            start = max(0, i // 3 * 3)
-            sorted_segment = sorted([y for y in l[start:i+1]])
-            last_element = sorted_segment[-1]
+            segment_start = i
+            sorted_segment = sorted([y for y in l[segment_start:i+1]])
+            last_element = sorted_segment[-1] if len(sorted_segment) > 0 else None
         else:
-            last_element = x
+            last_element = x if i != 0 and (i-1)%3 == 2 else None
         result.append(last_element)
     return result
