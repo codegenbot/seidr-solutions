e23fc228334c6bca1c418390586f67701bf008ca
beep boop <githubbot@vadim.me>
2024-07-11 08:05:12 +0200

    Solution 43 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d6xmmywr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aa8fc1e1-6522-4403-8675-a5c41cc53 311.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aa8fc1e1-6522-4403-8675-a5c41cc53 311.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,5 +1,10 @@
-```
-def double_the_difference(lst, target_diff):
+def double_the_difference(lst, target):
     even_numbers = [i for i in lst if isinstance(i, int) and i >= 0 and i % 2 == 0]
-    closest_even_number = min(even_numbers, key=lambda x:abs(x-target_diff))
-    return sum((i**2 for i in even_numbers if abs(i-closest_even_number) <= target_diff))
+    try:
+        diff = min(even_numbers)
+        while True:
+            sum_of_squares = sum(i**2 for i in even_numbers if abs(i - diff) == target)
+            return sum_of_squares
+            diff += target
+    except ZeroDivisionError:
+        pass
