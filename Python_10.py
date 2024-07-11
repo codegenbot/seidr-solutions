ebda2a64f6d0d1b64c506730b3656893288e60f1
beep boop <githubbot@vadim.me>
2024-07-11 16:53:25 +0200

    Solution 95 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tdfielxa Prompt: Program did not receive expected input

--- Python_10.py
+++ Python_10.py
@@ -1,11 +1,17 @@
-```
 def make_palindrome(s: str) -> str:
     s = list(s)
     left, right = 0, len(s) - 1
     
     while right > left:
         if s[left] != s[right]:
-            s.insert(0, s.pop(right))
+            if s[0] == s[right]:  
+                s.pop()               
+                right -= 1            
+            elif s[-1] == s[left]:  
+                s.pop()               
+                left += 1            
+            else:
+                s.insert(0, s.pop(right))  
         else:
             left += 1
             right -= 1
