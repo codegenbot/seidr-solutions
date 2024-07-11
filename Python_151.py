db90ec5238dc1dbf93734b8046da25b449b4ea84
beep boop <githubbot@vadim.me>
2024-07-11 08:22:57 +0200

    Solution 62 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d6xmmywr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/71d6d340-90f1-43a6-ba9c-3772e25aa e6a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,11 +1,16 @@
 """
-Write a function that calculates the difference between the sum of squares of all even numbers in a given list of integers and the product of their cubes.
-"""
+Given a list of integers, find the difference between the product of all negative numbers and the product of all non-negative numbers.
+For example, if input is [-1, -2, 3, 0], output will be (-1 * -2) - (3 * 0) = 2.
 
+"""
 def double_the_difference(lst):
-    even_sum_of_squares = sum(i**2 for i in lst if isinstance(i, int) and i % 2 == 0)
-    cubes_product = 1
-    for i in lst:
-        if isinstance(i, int):
-            cubes_product *= i ** 3
-    return (even_sum_of_squares - cubes_product) * 2
+    neg_product = 1
+    pos_or_zero_product = 1
+    
+    for num in lst:
+        if num < 0:
+            neg_product *= num
+        elif num >= 0:
+            pos_or_zero_product *= num
+            
+    return neg_product - pos_or_zero_product
