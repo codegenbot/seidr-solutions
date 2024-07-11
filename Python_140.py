939bdba2bb93c96e63442a1321201f16f2f76132
beep boop <githubbot@vadim.me>
2024-07-11 15:51:09 +0200

    Solution 50 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g87kg5gr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e59124be-a035-4b5c-b295-833549f08 97f.py", line 23, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e59124be-a035-4b5c-b295-833549f08 97f.py", line 19, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -5,13 +5,10 @@ def fix_spaces(text):
         if not prev_was_space and char == " ":
             new_text += "_"
             prev_was_space = True
-        elif char != " " and prev_was_space:
-            if char == " ":
-                new_text += "-"
-            else:
-                new_text += char
-            prev_was_space = False
+        elif char == " " and prev_was_space:
+            new_text += "-"
+            prev_was_space = True
         else:
             new_text += char
-            prev_was_space = False
+            prev_was_space = False  # <--- Reset here!
     return new_text
