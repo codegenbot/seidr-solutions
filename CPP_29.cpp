d10fdbc855edd288fbb1fe57fb3da72a71e6aca3
beep boop <githubbot@vadim.me>
2024-07-11 20:30:30 +0200

    Solution 55 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/joxftqtx Prompt: c5f8a8a5-c823-4053-a7d3-79d3548675ad.cpp:19:45: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c5f8a8a5-c823-4053-a7d3-79d3548675ad.cpp:18: c5f8a8a5-c823-4053-a7d3-79d3548675ad.cpp: In function ‘int main()’: c5f8a8a5-c823-4053-a7d3-79d3548675ad.cpp:28:20: error: ‘filter_by_prefix’ was no t declared in this scope; did you mean ‘Solution::filter_by_prefix’?    28 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ c5f8a8a5-c823-4053-a7d3-79d3548675ad.cpp:7:30: note: ‘Solution::filter_by_prefix ’ declared here     7 |     std::vector<std::string> filter_by_prefix(std::vector<std::string> s trings, std::string prefix) {       |                              ^~~~~~~~~~~~~~~~ In file included from c5f8a8a5-c823-4053-a7d3-79d3548675ad.cpp:18: c5f8a8a5-c823-4053-a7d3-79d3548675ad.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,5 +1,13 @@
 bool Solution::issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-assert(Solution::issame(Solution::filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAA", "xxx"}, "xxx"), {"xxx", "xxxAA", "xxx"}));
+assert (Solution::issame(Solution::filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAA", "xxx"}, "xxx"), {"xxx", "xxxAA", "xxx"}));
