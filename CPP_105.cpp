0d97342f4b4c7c4024c6a9bafd48df6d557f6c8f
beep boop <githubbot@vadim.me>
2024-07-11 10:50:27 +0200

    Solution 60 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xzmxc8nx Prompt:                  from a5201e39-3341-4229-bf94-35ea6fc56ae2.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from a5201e39-3341-4229-bf94-35ea6fc56ae2.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ a5201e39-3341-4229-bf94-35ea6fc56ae2.cpp:67:44: error: ‘b’ was not declared in t his scope    67 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from a5201e39-3341-4229-bf94-35ea6fc56ae2.cpp:66: a5201e39-3341-4229-bf94-35ea6fc56ae2.cpp: In function ‘int main()’: a5201e39-3341-4229-bf94-35ea6fc56ae2.cpp:76:20: error: ‘by_length’ was not decla red in this scope    76 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,51 +1,25 @@
-```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <string>
 
 bool issame(int a, int b) {
-    vector<string> result;
-    if (a == b) {
-        return true;
-    } else {
+    if (a > 9 || b > 9) {
         return false;
     }
+    vector<string> result = by_length({a, b});
+    for (int i = 0; i < min(result.size(), 2); i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+    return true;
 }
 
 vector<string> by_length(vector<int> arr) {
-    vector<string> numbers;
+    vector<int> numbers;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            switch (num) {
-                case 1:
-                    numbers.push_back("One");
-                    break;
-                case 2:
-                    numbers.push_back("Two");
-                    break;
-                case 3:
-                    numbers.push_back("Three");
-                    break;
-                case 4:
-                    numbers.push_back("Four");
-                    break;
-                case 5:
-                    numbers.push_back("Five");
-                    break;
-                case 6:
-                    numbers.push_back("Six");
-                    break;
-                case 7:
-                    numbers.push_back("Seven");
-                    break;
-                case 8:
-                    numbers.push_back("Eight");
-                    break;
-                case 9:
-                    numbers.push_back("Nine");
-                    break;
-            }
+            numbers.push_back(num);
         }
     }
     
@@ -53,29 +27,45 @@ vector<string> by_length(vector<int> arr) {
     
     reverse(numbers.begin(), numbers.end());
     
-    return numbers;
+    vector<string> result;
+    for (int num : numbers) {
+        switch (num) {
+            case 1:
+                result.push_back("One");
+                break;
+            case 2:
+                result.push_back("Two");
+                break;
+            case 3:
+                result.push_back("Three");
+                break;
+            case 4:
+                result.push_back("Four");
+                break;
+            case 5:
+                result.push_back("Five");
+                break;
+            case 6:
+                result.push_back("Six");
+                break;
+            case 7:
+                result.push_back("Seven");
+                break;
+            case 8:
+                result.push_back("Eight");
+                break;
+            case 9:
+                result.push_back("Nine");
+                break;
+        }
+    }
+    
+    return result;
 }
 
 int main() {
-    int n; 
-    std::cout << "Enter the number of elements: ";
-    std::cin >> n;
-
-    vector<int> arr(n);
-
-    for(int i =0 ;i<n;i++){
-        cin>>arr[i];
-    }
-
-    cout<<"The numbers are : "<<endl;
-    for(int i = 0; i <n; i++) {
-        cout<<arr[i]<<" ";
-    }cout<<endl;
-
-    vector<string> res = by_length(arr);
-    cout << "Output: ";
-    for (string s : res) {
-        cout << s << " ";
-    }
-    return 0;
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    issame(a, b);
 }
