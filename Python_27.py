87f64db1189897ed13334740f85f21227fb388d2
beep boop <githubbot@vadim.me>
2024-07-11 17:42:01 +0200

    Solution 60 of Python/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/h3nmtrpq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f6fd2a95-aff6-4751-a1ad-70a4c8db3 da2.py", line 1     return "".join([char.lower() if char.isupper() else char.upper() for char in  string])     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_27.py
+++ Python_27.py
@@ -1,6 +1,2 @@
-def convert_string_case(string):
+def swap_case(string):
     return "".join([char.lower() if char.isupper() else char.upper() for char in string])
-
-input_string = input("Enter a string: ")
-output = convert_string_case(input_string)
-print(output)
