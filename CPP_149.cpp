1bf203c7e19131115d5842d058d9db20b235afac
beep boop <githubbot@vadim.me>
2024-07-11 17:47:50 +0200

    Solution 68 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/8qwjmztj Prompt: Complete the following code given the task description and function signature.

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -7,11 +7,12 @@ vector<string> sorted_list_sum(vector<string> lst) {
             result.push_back(str);
         }
     }
-    sort(result.begin(), result.end(), [](const string& a, const string& b) {
-        if (a.length() != b.length()) {
-            return a.length() < b.length();
-        } else {
-            return a < b;
-        }
-    });
+    sort(result.begin(), result.end(),
+         [](const string& a, const string& b) {
+             if (a.length() != b.length()) {
+                 return a.length() < b.length();
+             } else {
+                 return a < b;
+             }
+         });
     return result;
