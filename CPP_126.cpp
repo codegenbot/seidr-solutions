48858fdf5d59030dd1c59e294ae75905786f6f51
beep boop <githubbot@vadim.me>
2024-07-11 11:19:11 +0200

    Solution 20 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/popkjeby Prompt: 33a3a373-ce61-4e4d-8e2c-6ce9288e831a.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 33a3a373-ce61-4e4d-8e2c-6ce9288e831a.cpp:14: 33a3a373-ce61-4e4d-8e2c-6ce9288e831a.cpp: In function ‘int main()’: 33a3a373-ce61-4e4d-8e2c-6ce9288e831a.cpp:16:13: error: ‘is_sorted’ was not decla red in this scope    16 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ^~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,40 +1,10 @@
-int main(){
-    vector<int> lst;
-    cout<<"Enter elements for list (space separated): ";
-    string input;
-    getline(cin,input);
-    size_t pos = 0;
-    while((pos=input.find(" "))!=-1){
-        int val = stoi(input.substr(0,pos));
-        lst.push_back(val);
-        input.erase(0,pos+1);
-    }
-    int val =stoi(input);
-    lst.push_back(val);
-    
-    bool result = is_sorted(lst);
-    if(result)
-        cout<<"The list is sorted.\n";
-    else
-        cout<<"The list is not sorted.\n";
-    
-    return 0;
-}
-
-bool is_sorted(vector<int> lst){
-    for(int i = 1; i < lst.size(); i++){
-        if(lst[i] <= lst[i-1]){
-            return false;
-        }
-        int count = 0;
-        for(int j = 0; j < lst.size(); j++){
-            if(lst[j] == lst[i]){
-                count++;
-            }
-        }
-        if(count > 1){
+bool is_sorted(vector<int> lst) {
+    for (int i = 1; i < lst.size(); ++i) {
+        if (lst[i] <= lst[i - 1]) {
             return false;
         }
     }
-    return true;
+    vector<int> temp(lst.begin(), lst.end());
+    sort(temp.begin(), temp.end());
+    return temp == lst;
 }
