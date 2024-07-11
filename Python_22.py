0a691515cc0620a407fe30338f981c34a9334cf1
beep boop <githubbot@vadim.me>
2024-07-11 09:23:12 +0200

    Solution 50 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5v09uzk5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1b17d805-4073-4866-9f29-2ffb0db87 989.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -12,6 +12,9 @@ def get_values() -> List[int]:
                 result.append(int(value))
             else:
                 print(f"Invalid input: {value}. Please enter integers only.")
-        if all(val.replace(".", "", 1).replace("-", "", 1).isdigit() for val in values.split()):
+        if all(
+            val.replace(".", "", 1).replace("-", "", 1).isdigit()
+            for val in values.split()
+        ):
             break
     return result
