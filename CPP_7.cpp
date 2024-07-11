f89ca86d802070faabb1de96e2a5f3983d0d32fa
beep boop <githubbot@vadim.me>
2024-07-11 12:28:06 +0200

    Solution 67 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zkvvbdxl Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 84b1e249-0c70-4fd0-8dd5-e0fface8727c.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 84b1e249-0c70-4fd0-8dd5-e0fface8727c.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 84b1e249-0c70-4fd0-8dd5-e0fface8727c.cpp:10: 84b1e249-0c70-4fd0-8dd5-e0fface8727c.cpp: In function ‘int main()’: 84b1e249-0c70-4fd0-8dd5-e0fface8727c.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 84b1e249-0c70-4fd0-8dd5-e0fface8727c.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 
-std::vector<std::string> filter_by_substring(std::vector<std::string> strings, std::string substring){
+std::vector<std::string> filter_by_substring(std::vector<std::string> strings, std::string substring) {
     std::vector<std::string> result;
     for(auto str : strings) {
         if(str.find(substring) != std::string::npos)
@@ -17,5 +17,4 @@ bool isEqualTo(const std::vector<std::string>& a, const std::vector<std::string>
 
 int main() {
     assert(isEqualTo(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
-    return 0;
 }
