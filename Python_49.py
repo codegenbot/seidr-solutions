2367c4606d8774f6e51d4a65bab68d87874382c5
beep boop <githubbot@vadim.me>
2024-07-11 21:56:01 +0200

    Solution 64 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/62ryo8im Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/170e7841-dc1e-4115-b855-56ca2fe11 14d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,2 +1,3 @@
-def modp(n: int, p: int):
-    return pow(n, p - 2, p) * n % p if math.gcd(n, p) == 1 else None
+def power_of_modular_inverse():
+    def modp(n: int, p: int):
+        return pow(n, p - 2, p) * n % p if math.gcd(n, p) == 1 else None
