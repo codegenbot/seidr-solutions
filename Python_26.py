12366212b1a33b21f218e75ff9ff144db93ab49b
beep boop <githubbot@vadim.me>
2024-07-11 09:58:07 +0200

    Solution 72 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7s9yk3dz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d5ecfc71-658b-4cd9-a683-4ea6edb51 79f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_26.py
+++ Python_26.py
@@ -1,3 +1,4 @@
+```
 def remove_duplicates(input_string):
     input_list = [int(x) for x in input_string.split(",")]
-    return ",".join(map(str, set(input_list)))
+    return ",".join(map(str, set(map(int, input_list)))))
