a9ea7e911dcbdde3e7c98d8f61518c982f166abf
beep boop <githubbot@vadim.me>
2024-07-11 08:00:31 +0200

    Solution 8 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hn1msxib Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b58ac9aa-3bf1-4917-b84a-08900fec2 241.py", line 25, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b58ac9aa-3bf1-4917-b84a-08900fec2 241.py", line 20, in check     sort_numbers("six five four three two one zero") TypeError: sort_numbers() takes 0 positional arguments but 1 was given

--- Python_19.py
+++ Python_19.py
@@ -2,17 +2,15 @@
 def num_dict():
     return {"1": 1, "2": 2}
 
-
-def sort_numbers():
-    numbers = input("Enter space-separated numbers: ")
-    return [int(x) for x in numbers.split()]
-
+def sort_numbers(user_input):
+    return [int(x) for x in user_input.split()]
 
 def solve():
-    sorted_nums = sorted([num_dict()[i] for i in sort_numbers()])
+    sorted_nums = sorted([num_dict()[i] for i in sort_numbers(input("Enter space-separated numbers: "))])
     result = " ".join(str(num) for num in sorted_nums)
     return result
 
-
 def check(f):
     print(f())
+
+check(solve)
