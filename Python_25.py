58fd84aaaf83c7370ec36c0ca3f955f1c94ef127
beep boop <githubbot@vadim.me>
2024-07-11 14:42:52 +0200

    Solution 25 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/4cwfx790 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/acf39d6b-42b8-46e4-b591-84591f477 b76.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -4,4 +4,4 @@ def calculate_factors():
     for i in range(1, num + 1):
         if num % i == 0:
             factors.append(i)
-    print("Factors of {} are {}".format(num, ', '.join(map(str, set(factors)))))
+    print("Factors of {} are {}".format(num, ", ".join(map(str, factors))))
