17a3c5b71034bed9c973739f58fae818a2ff985c
beep boop <githubbot@vadim.me>
2024-07-11 13:55:40 +0200

    Solution 94 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ors1r84u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8d1e2921-81e7-4f26-b0a2-32f7dbcb4 969.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_66.py
+++ Python_66.py
@@ -1,24 +1,6 @@
-```
 def digitSum(n):
     sum = 0
     while n > 0:
         sum += n % 10
         n //= 10
     return sum
-
-while True:
-    user_input = input("Enter a number (or type 'exit' to quit): ")
-    if user_input.lower() == 'exit':
-        break
-    for char in user_input:
-        if not char.isdigit():
-            print("Invalid input. Please enter an integer.")
-            continue
-    try:
-        n = int(user_input) 
-        if n > 2**31 - 1 or n < -2**31 + 1:
-            print("Invalid input. The number should be within the range of the int type.")
-        else:
-            print(digitSum(n))
-    except ValueError:
-        print("Invalid input. Please enter a valid number.")
