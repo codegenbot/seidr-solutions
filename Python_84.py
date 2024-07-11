ceb1289c1d210d6d4fd1a6a242ee4428e37d120d
beep boop <githubbot@vadim.me>
2024-07-11 11:24:50 +0200

    Solution 28 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ipbpzph6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ec925f8f-043d-4dd7-a8e4-555fa3798 f3c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,7 +1,8 @@
+def convert_to_binary(n):
+    return bin(int(n))[2:]
+
 def solve():
     N = input("Enter a number: ")
-    def convert_to_binary(n):
-        return bin(int(n))[2:]
     return convert_to_binary(N)
 
 print(solve())
