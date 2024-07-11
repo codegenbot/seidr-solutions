3022159fdd1050e314973b4ba02ae4cddbca6484
beep boop <githubbot@vadim.me>
2024-07-11 07:12:45 +0200

    Solution 75 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/5w75f9ts Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/251366d1-7e56-405a-8fe1-e1a5e6f27 457.py", line 35, in <module>     print(bowling_score(bowls))           ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/251366d1-7e56-405a-8fe1-e1a5e6f27 457.py", line 6, in bowling_score     if bowls[bowl_index] == "X":        ~~~~~^^^^^^^^^^^^ IndexError: string index out of range

--- bowling.py
+++ bowling.py
@@ -14,9 +14,11 @@ def bowling_score(bowls):
             score += get_value(bowls[bowl_index + 1])
         else:
             score += get_value(bowls[bowl_index])
+        
         bowl_index += 1
         if bowls[bowl_index - 1] in ["X", "/"]:
             frame += 1
+    
     return score
 
 
