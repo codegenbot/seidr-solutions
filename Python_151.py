1bcf297abf8c9ffbafe7398a7b5af151283552d0
beep boop <githubbot@vadim.me>
2024-07-11 04:11:05 +0200

    Solution 66 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bha7y640 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d553e39a-3ecf-4641-9835-7a7d45b35 5e2.py", line 12, in <module>     check(double_the_difference)           ^^^^^^^^^^^^^^^^^^^^^ NameError: name 'double_the_difference' is not defined

--- Python_151.py
+++ Python_151.py
@@ -1,5 +1,8 @@
 def check(input_list):
-    if not all(isinstance(i, int) for i in input_list):
-        raise ValueError("All elements of the list must be integers.")
-    difference = abs(max(input_list) - min(input_list))
-    return (difference * 2) + sum(i**2 for i in input_list if i > 0)
+    if not isinstance(input_list, list) or not all(
+        isinstance(i, int) for i in input_list
+    ):
+        raise TypeError("Input should be a list of integers")
+    lst = [i for i in input_list if i > 0]
+    difference = abs(max(lst) - min(input_list))
+    return (difference * 2) + sum(i**2 for i in lst)
