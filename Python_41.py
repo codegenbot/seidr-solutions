fc9a1297be38aaf58f54a8c21818b87f01dfcc47
beep boop <githubbot@vadim.me>
2024-07-11 10:31:12 +0200

    Solution 39 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/md3ef0th Prompt: Program did not receive expected input

--- Python_41.py
+++ Python_41.py
@@ -1,14 +1,26 @@
 def car_race_collision():
     time_to_meet = int(input("Enter time at which both cars meet: "))
-    
-    speed1 = float(input("Enter distance traveled by car 1: ")) / time_to_meet
-    speed2 = (float(input("Enter total race distance: ")) - speed1 * time_to_meet) / time_to_meet
-    
+
+    while True:
+        speed1 = float(input("Enter distance traveled by car 1: ")) / time_to_meet
+        if speed1 >= 0:
+            break
+        print("Invalid input. Car's speed cannot be negative.")
+
+    while True:
+        speed2 = (
+            float(input("Enter total race distance: ")) - speed1 * time_to_meet
+        ) / time_to_meet
+        if speed2 >= 0:
+            break
+        print("Invalid input. Car's speed cannot be negative.")
+
     if abs(speed1 - speed2) == 0:
         return f"Both cars are traveling at the same speed."
     elif speed1 > speed2:
         return f"Car 1 is moving with a speed of {speed1} km/h and car 2 is moving with a speed of {speed2} km/h."
     else:
         return f"Car 1 is moving with a speed of {speed2} km/h and car 2 is moving with a speed of {speed1} km/h."
+
 
 print(car_race_collision())
