76f71ccba567578d983f6c202252bd57577f309b
beep boop <githubbot@vadim.me>
2024-07-11 22:09:22 +0200

    Solution 67 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ug2t8crd Prompt: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::__cxx11::basi c_string<_CharT, _Traits, _Alloc>::basic_string(std::__cxx11::basic_string<_Char T, _Traits, _Alloc>&&) [with _CharT = char; _Traits = std::char_traits<char>; _A lloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:6667:12:   required from here /usr/include/c++/11/bits/basic_string.h:576:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   576 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,19 +1,29 @@
+#include <iostream>
+#include <string>
+
 int bowlingScore(const char* s) {
     int score = 0;
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 20; ++i) {
         if (s[i] == 'X') {
             score += 30;
         } else if (isdigit(s[i])) {
             int roll1 = s[i] - '0';
-            if (i < 8 && s[i + 1] == '/') {
+            if (i < 18 && s[i + 1] == '/') {
                 int roll2 = s[i + 2] - '0';
-                score += roll1 + roll2;
+                if (i < 19) {
+                    score += roll1 + roll2;
+                } else {
+                    score += roll1 + roll2;
+                }
             } else {
                 score += roll1 * 2;
             }
         } else {
+            if (s[i] == '-') {
+                continue;
+            }
             int roll1 = s[i] - '0';
-            if (i < 9) {
+            if (i < 19) {
                 int roll2 = s[i + 1] - '0';
                 score += roll1 + roll2;
             } else {
@@ -22,4 +32,11 @@ int bowlingScore(const char* s) {
         }
     }
     return score;
+}
+
+int main() {
+    std::string input = "X/22/X--;32/-5/X-8/42/X2/13/-9/X10";
+    int score = bowlingScore(input.c_str());
+    std::cout << "The bowling score is: " << score << std::endl;
+    return 0;
 }
