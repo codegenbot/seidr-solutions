7733733b39bbe2cf68d0e2aa967fc37391b7aa01
beep boop <githubbot@vadim.me>
2024-07-11 02:34:34 +0200

    Solution 73 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/chwkt3kc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f0c7c97e-1e48-4b42-bd1f-1f38cd4a5 aac.py", line 1, in <module>     minPath = minPathSum(grid)               ^^^^^^^^^^ NameError: name 'minPathSum' is not defined

--- Python_129.py
+++ Python_129.py
@@ -1,3 +1,19 @@
 ```
-grid = [[1,3,1],[1,5,1],[4,2,1]]
-print(minPathSum(grid))
+def minPathSum(grid):
+    m = len(grid)
+    n = len(grid[0])
+    
+    for i in range(1, m):
+        grid[i][0] += grid[i-1][0]
+        
+    for j in range(1, n):
+        grid[0][j] += grid[0][j-1]
+        
+    for i in range(1, m):
+        for j in range(1, n):
+            grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+            
+    return grid[m-1][n-1]
+
+minPath = minPathSum([[1,3,1],[1,5,1],[4,2,1]])
+check(minPath)
