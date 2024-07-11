431924f598987e134f9086cff506d32570fb29fd
beep boop <githubbot@vadim.me>
2024-07-11 16:24:26 +0200

    Solution 70 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/etx4my5z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d4a85477-d352-44f2-8f68-3cb7c67c3 c15.py", line 31, in <module>     check(rolling_max)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d4a85477-d352-44f2-8f68-3cb7c67c3 c15.py", line 28, in check     assert rolling_max([3, 2, 3, 100, 3]) == [3, 3, 3, 100, 100]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_9.py
+++ Python_9.py
@@ -1,4 +1,4 @@
-```Python
+```
 def rolling_max(input_str: str) -> list[int]:
     input_str = "".join(filter(str.isdigit, str(input_str)))
 
@@ -18,10 +18,10 @@ def rolling_max(input_str: str) -> list[int]:
     max_so_far = current_max = numbers[0]
     for num in numbers[1:]:
         if num > current_max:
-            result.append(current_max)
+            max_so_far = current_max = num
+        elif num >= current_max:
             current_max = num
-        else:
-            result.append(current_max)
+        result.append(current_max)
         max_so_far = max(max_so_far, current_max)
 
     while len(result) < len(numbers):
