f4c13eeac5547dc598df1bc15de5868b4e8ba797
beep boop <githubbot@vadim.me>
2024-07-11 19:57:50 +0200

    Solution 31 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ocjywund Prompt: eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 05fd7758-d0d6-493d-8b8d-f177e88cebb2.cpp:39:18: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    39 |     getline(cin, str);       |                  ^~~       |                  std 05fd7758-d0d6-493d-8b8d-f177e88cebb2.cpp:39:5: error: ‘getline’ was not declared  in this scope    39 |     getline(cin, str);       |     ^~~~~~~ 05fd7758-d0d6-493d-8b8d-f177e88cebb2.cpp:40:32: error: ‘spinWords’ was not decla red in this scope    40 |     cout << "Spun result: " << spinWords(str) << endl;       |                                ^~~~~~~~~ 05fd7758-d0d6-493d-8b8d-f177e88cebb2.cpp:40:50: error: ‘endl’ was not declared i n this scope    40 |     cout << "Spun result: " << spinWords(str) << endl;       |                                                  ^~~~ 05fd7758-d0d6-493d-8b8d-f177e88cebb2.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <algorithm>   +++ |+#include <ostream>     2 | using namespace std;

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,70 +1,19 @@
+```c++
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
 string spinWords(string str) {
     string result = "";
-    bool inWord = false;
-    for (int i = 0; i <= str.length(); i++) {
-        if (i < str.length()) {
-            char c = str[i];
-            if (!inWord && !isSpace(c)) {
-                inWord = true;
-            }
-            if (inWord) {
-                result += c;
-                if (c == ' ') {
-                    inWord = false;
-                }
-            } else if (isSpace(c)) {
-                result += " ";
-            }
-        } else {
-            if (inWord) {
-                string word = result;
-                reverse(word.begin(), word.end());
-                if (word.length() >= 5) {
-                    result = word + " ";
-                } else {
-                    result = word + " ";
-                }
-                inWord = false;
-            }
-            result += c;
-        }
-    }
-    return result;
-}
-
-string spinWords(string str) {
-    string result = "";
-    bool inWord = false;
-    for (int i = 0; i <= str.length(); i++) {
-        if (i < str.length()) {
-            char c = str[i];
-            if (!inWord && !isSpace(c)) {
-                inWord = true;
-            }
-            if (inWord) {
-                result += c;
-                if (c == ' ') {
-                    inWord = false;
-                }
-            } else if (isSpace(c)) {
-                result += " ";
-            }
-        } else {
-            if (inWord) {
-                string word = result;
-                reverse(word.begin(), word.end());
-                if (word.length() >= 5) {
-                    result = word + " ";
-                } else {
-                    result = word + " ";
-                }
-                inWord = false;
-            }
-            result += c;
+    int start = 0;
+    for(int end = 0; end <= str.length(); end++){
+        if(end == str.length() || str[end] == ' '){
+            string word = str.substr(start, end - start);
+            if(word.length() >= 5)
+                result.append(string(word.rbegin(), word.rend()) + " ");
+            else
+                result.append(word + " ");
+            start = end + 1;
         }
     }
     return result;
@@ -76,3 +25,4 @@ int main() {
     getline(cin, str);
     cout << "Spun result: " << spinWords(str) << endl;
     return 0;
+}
