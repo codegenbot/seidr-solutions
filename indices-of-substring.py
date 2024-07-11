1e31517d16f57545f110a501c7b63525d345f495
beep boop <githubbot@vadim.me>
2024-07-11 13:51:50 +0200

    Solution 20 of indices-of-substring, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/6cl91of5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d3835f8b-1773-4aca-ba54-da09907d0 646.py", line 1     ```     ^ SyntaxError: invalid syntax

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -1,4 +1,11 @@
-def indices_of_substring(text):
-    target = input()
-    result = [i for i in range(len(text)) if text.startswith(target, i)]
+def indices_of_substring(text, target):
+    result = []
+    for i in range(len(text)):
+        if text[i:].startswith(target):
+            pos = text[i:].find(target)
+            while True:
+                if pos == -1:
+                    break
+                result.append(i + pos)
+                i += pos + 1
     return result
