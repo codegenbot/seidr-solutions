a1743ff8cb4cbaa3a1b6cd51ceca587a801ec6e6
beep boop <githubbot@vadim.me>
2024-07-11 10:46:34 +0200

    Solution 1 of Python/98, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/yehivqhb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9c65dc59-3b7a-4d50-987a-edc9b1142 94a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_98.py
+++ Python_98.py
@@ -1,3 +1,4 @@
-```
 def count_upper(s):
+    if not isinstance(s, str): 
+        return "Error: Input must be a string"
     return sum(1 for i, c in enumerate(s) if 'A' <= c <= 'E' and i % 2 == 0)
