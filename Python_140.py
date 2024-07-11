35404502d5f8f2efc70358bc7df946fcefb58c70
beep boop <githubbot@vadim.me>
2024-07-11 15:31:36 +0200

    Solution 22 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/di6yrh0q Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/803fcc09-22ee-467c-bf9f-df61b0f69 6dd.py", line 11, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/803fcc09-22ee-467c-bf9f-df61b0f69 6dd.py", line 7, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,12 +1,2 @@
 def fix_spaces(text):
-    return "".join(
-        [
-            (
-                "_"
-                if c == " "
-                and (i == len(text) - 1 or (i + 1 < len(text) and text[i + 1] != " "))
-                else c
-            )
-            for i, c in enumerate(text)
-        ]
-    )
+    return "".join(["_" if c == " " and (i == len(text) - 1 or text[i + 1] != " ") else c for i, c in enumerate(text) if c != " " or (i == 0 or text[i - 1] != " ")]).
