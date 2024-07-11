da7bc562fde9a53d962ba97510b8291a4c563402
beep boop <githubbot@vadim.me>
2024-07-11 08:42:56 +0200

    Solution 50 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6qgp4zk2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e8e59867-47a7-4ca2-a583-85df47769 b56.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_12.py
+++ Python_12.py
@@ -1,6 +1,5 @@
-...'Find the longest string in a list of strings.
-
-def longest(strings: List[str]) -> Optional[str]:
+```
+def longest(strings: list[str]) -> str | None:
     if not strings:
         return None
     
@@ -12,18 +11,4 @@ def longest(strings: List[str]) -> Optional[str]:
             max_len = len(s)
             result = s
 
-    return result if max_len != 0 else None'...
-
-def longest(strings: List[str]) -> Optional[str]:
-    if not strings:
-        return None
-    
-    max_len = 0
-    result = None
-
-    for s in strings:
-        if len(s) > max_len:
-            max_len = len(s)
-            result = s
-
-    return result if max_len != 0 else None
+    return result
