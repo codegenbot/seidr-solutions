52974d5e8e3d8fe132fca58061d40fb2c0d25931
beep boop <githubbot@vadim.me>
2024-07-11 14:46:33 +0200

    Solution 42 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lwme29gf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/650fa033-cda5-4426-85ce-73d753191 b08.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/650fa033-cda5-4426-85ce-73d753191 b08.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: 'int' object is not iterable

--- Python_33.py
+++ Python_33.py
@@ -1,2 +1,3 @@
+```
 def sort_third(l: list):
-    return sorted([x for i, x in enumerate(l) if (i % 3 == 0 or l.index(x) % 3 == 0)][0]
+    return sorted([i for i in range(len(l)) if (i) % 3 == 0])[:1]
