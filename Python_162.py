7720215d582899ead7e0b245ae747ececff4e212
beep boop <githubbot@vadim.me>
2024-07-11 02:19:16 +0200

    Solution 43 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/yw92pi5n Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5c78c680-38b1-4052-99cc-d3385fd6b fea.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_162.py
+++ Python_162.py
@@ -1,13 +1,15 @@
-```
 import hashlib
 
 def string_to_md5():
     while True:
-        text = input("Enter some text: ")
-        if text.isprintable():  
-            return hashlib.md5(text.encode('utf-8')).hexdigest()
-        else:
-            print("Invalid input. Please enter printable characters.")
+        try:
+            text = input("Enter some text: ")
+            if text.isprintable():  
+                return hashlib.md5(text.encode('utf-8')).hexdigest()
+            else:
+                print("Invalid input. Please enter printable characters.")
+        except AttributeError:
+            print("Error: Please enter a string.")
 
 result = string_to_md5()
 print(result)
