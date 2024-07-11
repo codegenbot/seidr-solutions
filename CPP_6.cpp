3fb90e887142f521d86b5dc8d88ce24a734f78bb
beep boop <githubbot@vadim.me>
2024-07-11 11:49:21 +0200

    Solution 57 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fw4dzic9 Prompt:       |                    ^~~ 82f031ad-5218-4215-add2-9344dfc295fa.cpp:27:27: error: ‘vector’ was not declared  in this scope    27 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 82f031ad-5218-4215-add2-9344dfc295fa.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 82f031ad-5218-4215-add2-9344dfc295fa.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 82f031ad-5218-4215-add2-9344dfc295fa.cpp:26: 82f031ad-5218-4215-add2-9344dfc295fa.cpp: In function ‘int main()’: 82f031ad-5218-4215-add2-9344dfc295fa.cpp:36:20: error: ‘parse_nested_parens’ was  not declared in this scope    36 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ 82f031ad-5218-4215-add2-9344dfc295fa.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,13 +1,24 @@
 #include <vector>
 
-bool issame(vector<int> v1, vector<int> v2) {
-    return v1 == v2;
+bool issame(vector<int> v1, vector<int> v2){
+    if (v1.size() != v2.size()) {
+        return false;
+    }
+
+    for (int i = 0; i < v1.size(); i++) {
+        if (v1[i] != v2[i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 vector<int> parse_nested_parens(string paren_string);
 
 int main() {
-    // Implementation of parse_nested_parens function
+    // Call parse_nested_parens and issame functions here
+    return 0;
 }
 
 vector<int> parse_nested_parens(string paren_string){
