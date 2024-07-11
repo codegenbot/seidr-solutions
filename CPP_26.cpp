095300a95a2a9a4b4841430e281a7b3082770bfc
beep boop <githubbot@vadim.me>
2024-07-11 22:40:04 +0200

    Solution 94 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/27myevuf Prompt:    35 | int main(){       |     ^~~~ dbd102a9-1de4-408d-8551-e672f527956e.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from dbd102a9-1de4-408d-8551-e672f527956e.cpp:26: dbd102a9-1de4-408d-8551-e672f527956e.cpp: In function ‘int main()’: dbd102a9-1de4-408d-8551-e672f527956e.cpp:36:20: error: ‘remove_duplicates’ was n ot declared in this scope; did you mean ‘std::remove_duplicates’?    36 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ dbd102a9-1de4-408d-8551-e672f527956e.cpp:8:22: note: ‘std::remove_duplicates’ de clared here     8 |     std::vector<int> remove_duplicates(const std::vector<int>& vec) {       |                      ^~~~~~~~~~~~~~~~~ In file included from dbd102a9-1de4-408d-8551-e672f527956e.cpp:26: dbd102a9-1de4-408d-8551-e672f527956e.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -18,4 +18,7 @@ namespace std {
     }
 }
 
-assert(std::issame(std::remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5}));
+int main() {
+    assert(std::issame(std::remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5}));
+    return 0;
+}
