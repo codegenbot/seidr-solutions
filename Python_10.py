43ea92e00a03605c86f7b318cfb8e9c7c8f28b48
beep boop <githubbot@vadim.me>
2024-07-11 15:26:23 +0200

    Solution 63 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tdfielxa Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e35910e6-39f3-4fba-a883-a39bf0b34 b14.py", line 18, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e35910e6-39f3-4fba-a883-a39bf0b34 b14.py", line 15, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e35910e6-39f3-4fba-a883-a39bf0b34 b14.py", line 7, in make_palindrome     if t[0] != t[r - l + 1]:                ~^^^^^^^^^^^ IndexError: list index out of range

--- Python_10.py
+++ Python_10.py
@@ -1,17 +1,13 @@
-```
 def make_palindrome(s: str) -> str:
     if s == s[::-1]:
         return s
     t = list(s)
-    l, r = 0, len(t)-1
-    while l < r:
-        if t[l] != t[r]:
+    l, r = 0, len(t) - 1
+    while l < r and l + 1 <= r - l + 1:
+        if t[0] != t[r - l + 1]:
             mid = (l + r) // 2
-            for i in range(len(s)-1, -1, -1):
-                if s[i] == t[r]: 
-                    t.insert(mid, s[i])
-                    break
-            l, r = l+1, len(t)-1
+            t.pop(r), t.insert(mid, t[0])
+            l, r = l + 1, len(t) - 1
         else:
-            l, r = l+1, r-1
+            l, r = l + 1, r - 1
     return "".join(t)
