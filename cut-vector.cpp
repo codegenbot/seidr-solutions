1ef771959bc22ce6c92a5619d80cb3c93a8fdbe3
beep boop <githubbot@vadim.me>
2024-07-11 11:10:35 +0200

    Solution 67 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/66vz1sa5 Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -3,23 +3,21 @@ using namespace std;
 
 vector<vector<int>> cutVector(vector<int>& nums) {
     int n = nums.size();
-    vector<int> left;
-    vector<int> right;
+    int min_diff = INT_MAX;
+    int min_idx = -1;
     for(int i=0; i<n-1; i++){
-        if(nums[i+1] - nums[i] <= 0){
-            left.push_back(nums[i]);
-            for(int j=i; j<n-1; j++){
-                left.push_back(nums[j+1]);
-                break;
-            }
-            break;
-        } else {
-            left.push_back(nums[i]);
-        }
+        int diff = abs(nums[i+1] - nums[i]);
+        if(diff == 0 && (min_diff > 0 || min_idx == -1)) min_diff = 0, min_idx = i;
+        else if(diff < min_diff) min_diff = diff, min_idx = i;
     }
-    for(int i=left.size(); i<n; i++) right.push_back(nums[i]);
-    
-    return {{left}, {right}};
+    if(min_idx == -1) {
+        return {{}, {}};
+    } else {
+        vector<int> left(min_idx+1), right(n-min_idx-1);
+        for(int i=0; i<=min_idx; i++) left.push_back(nums[i]);
+        for(int i=min_idx+1; i<n; i++) right.push_back(nums[i]);
+        return {{left}, {right}};
+    }
 }
 
 int main() {
