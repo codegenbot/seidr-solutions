4416ac08f1361453131b46e79055d3bf6040d4ef
beep boop <githubbot@vadim.me>
2024-07-11 14:24:41 +0200

    Solution 30 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lwme29gf Prompt: 1ef.py", line 14, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b4278e8-c53c-4d15-aeed-6c053fbf6 1ef.py", line 9, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b4278e8-c53c-4d15-aeed-6c053fbf6 1ef.py", line 2, in sort_third     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b4278e8-c53c-4d15-aeed-6c053fbf6 1ef.py", line 3, in <listcomp>     min([x for x, i in enumerate(l) if (i - l.index(x)) % 3 == 0])         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b4278e8-c53c-4d15-aeed-6c053fbf6 1ef.py", line 3, in <listcomp>     min([x for x, i in enumerate(l) if (i - l.index(x)) % 3 == 0])                                             ^^^^^^^^^^ ValueError: 0 is not in list

--- Python_33.py
+++ Python_33.py
@@ -1,3 +1,4 @@
-def sort_third(l):
-    sorted_list = sorted((x, i) for i, x in enumerate(l))
-    return [x for x, _ in sorted_list if (_ - list(sorted(x for _, x in sorted_list)).index(x)) % 3 == 0][0]
+def sort_third(l: list):
+    return [next(x for x, i in enumerate(l) if (i - l.index(x)) % 3 != 0) if i == 0 
+            else sorted([x for x, _ in enumerate(l) if (index := i - l.index(x)) % 3 != 0])[0] 
+            for index, _ in enumerate(l)]
