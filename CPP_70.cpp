8ffcc889d2036bb3c8427b444992b7590dd60aef
beep boop <githubbot@vadim.me>
2024-07-11 08:31:33 +0200

    Solution 22 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/b3c1v916 Prompt:       |                    ^~~ 92f7661e-a88d-4291-b616-e056ee27444c.cpp:23:27: error: ‘vector’ was not declared  in this scope    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 92f7661e-a88d-4291-b616-e056ee27444c.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 92f7661e-a88d-4291-b616-e056ee27444c.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 92f7661e-a88d-4291-b616-e056ee27444c.cpp:22: 92f7661e-a88d-4291-b616-e056ee27444c.cpp: In function ‘int main()’: 92f7661e-a88d-4291-b616-e056ee27444c.cpp:32:20: error: ‘strange_sort_list’ was n ot declared in this scope    32 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 92f7661e-a88d-4291-b616-e056ee27444c.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,34 +1,47 @@
-```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
-bool issame(int i1,int i2){
-    if(i1==i2) 
-        return true;    
-    else return false;
+using namespace std;
+
+bool issame(vector<int> v1, vector<int> v2) {
+    if(v1.size() != v2.size()) 
+        return false;
+        
+    for(int i = 0; i < v1.size(); i++) {
+        if(v1[i] != v2[i]) 
+            return false;
+    }
+    
+    return true;
 }
 
-std::vector<int> strange_sort_list(std::vector<int> lst) {
-    std::vector<int> result;
+vector<int> strange_sort_list(vector<int> lst) {
+    vector<int> result;
     if (lst.empty()) return result;
 
-    sort(lst.begin(), lst.end());
-    bool minThenMax = true;
+    vector<vector<int>> output;
+    vector<bool> minThenMax(lst.size(), false);
 
-    for (int i : lst) {
-        if (minThenMax) {
-            int min_element = *min_element(lst.begin(), lst.end());
-            result.push_back(min_element);
-            auto it = remove(lst.begin(), lst.end(), min_element);
-            lst.erase(it, lst.end());
+    for (int i = 0; i < lst.size(); i++) {
+        if (i == 0) {
+            sort(lst.begin(), lst.end());
+            result.push_back(*min_element(lst.begin(), lst.end()));
+            lst.erase(remove(lst.begin(), lst.end(), *min_element(lst.begin(), lst.end())), lst.end());
+            minThenMax[i] = true;
         } else {
-            int max_element = *max_element(lst.begin(), lst.end());
-            result.push_back(max_element);
-            auto it = remove(lst.begin(), lst.end(), max_element);
-            lst.erase(it, lst.end());
+            if (minThenMax[i-1]) {
+                sort(lst.begin(), lst.end());
+                result.push_back(*max_element(lst.begin(), lst.end()));
+                lst.erase(remove(lst.begin(), lst.end(), *max_element(lst.begin(), lst.end())), lst.end());
+                minThenMax[i] = false;
+            } else {
+                sort(lst.begin(), lst.end());
+                result.push_back(*min_element(lst.begin(), lst.end()));
+                lst.erase(remove(lst.begin(), lst.end(), *min_element(lst.begin(), lst.end())), lst.end());
+                minThenMax[i] = true;
+            }
         }
-        minThenMax = !minThenMax;
     }
 
     return result;
