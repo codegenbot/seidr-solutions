c1208d719e7c3a0d091e9841951320c1d8bcb139
beep boop <githubbot@vadim.me>
2024-07-11 04:01:42 +0200

    Solution 25 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/497575qq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,21 +1,62 @@
-#include <boost/any_cast.hpp>
+#include <boost/any.hpp>
+#include <string>
+
+using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (boost::any_cast<int>(a) > boost::any_cast<int>(b))
-        return a;
-    else if (boost::any_cast<int>(b) > boost::any_cast<int>(a))
+    if (a.type() == typeid(int) && b.type() == typeid(double)) {
         return b;
-
-    double aDouble = boost::any_cast<double>(a);
-    double bDouble = boost::any_cast<double>(b);
-
-    if (aDouble > bDouble)
-        return a;
-    else if (bDouble > aDouble)
+    }
+    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
         return b;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = any_cast<string>(a);
+        string str2 = any_cast<string>(b);
 
-    string aStr = boost::any_cast<string>(a);
-    string bStr = boost::any_cast<string>(b);
+        int num1 = std::stoi(str1.substr(0, str1.find(',')));
+        int num2 = std::stoi(str2.substr(0, str2.find(',')));
 
-    return (stod(aStr) > stod(bStr)) ? a : ((stod(bStr) > stod(aStr)) ? b : "None");
+        if (num1 > num2) {
+            return a;
+        }
+        else if (num1 < num2) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        string str = any_cast<string>(a);
+        double num = any_cast<double>(b);
+
+        int num1 = std::stoi(str.substr(0, str.find(',')));
+        if (num1 > num) {
+            return a;
+        }
+        else if (num1 < num) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
+    }
+    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
+        string str = any_cast<string>(b);
+        double num = any_cast<double>(a);
+
+        int num1 = std::stoi(str.substr(0, str.find(',')));
+        if (num > num1) {
+            return b;
+        }
+        else if (num < num1) {
+            return a;
+        }
+        else {
+            return boost::any("None");
+        }
+    }
+
+    return boost::any("None");
 }
