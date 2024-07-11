2f8f25db2a0ae00edf2decc7ae09ef4b71307b68
beep boop <githubbot@vadim.me>
2024-07-11 05:17:30 +0200

    Solution 45 of CPP/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xsowexf3 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,6 +1,5 @@
+```cpp
 #include <iostream>
-#include <string>
-
 using namespace std;
 
 bool right_angle_triangle(float a, float b, float c) {
@@ -14,14 +13,9 @@ int main() {
     assert(right_angle_triangle(2, 2, 10) == false);
     cout << "Input the lengths of three sides (a, b, c): ";
     float a, b, c;
-    string inputA = to_string(a);
-    string inputB = to_string(b);
-    string inputC = to_string(c);
-
-    cin >> inputA >> inputB >> inputC;
-
-    if (right_angle_triangle(stof(inputA), stof(inputB), stof(inputC)))
+    cin >> a >> b >> c;
+    if (right_angle_triangle(a, b, c))
         cout << "The triangle is right-angled." << endl;
     else
-        cout << "The triangle is not right-angled." << endl;
+        cout << "Invalid input! Please enter three distinct positive values for the sides of the triangle." << endl;
 }
