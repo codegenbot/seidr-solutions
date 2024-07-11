1b7a34ec8d1451d3442354c6f7dcfd6da6cdf5ef
beep boop <githubbot@vadim.me>
2024-07-11 16:35:26 +0200

    Solution 84 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g87kg5gr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e9d8a007-f739-411c-abca-23a372f6e 5ff.py", line 23, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e9d8a007-f739-411c-abca-23a372f6e 5ff.py", line 19, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -3,12 +3,12 @@ def fix_spaces(text):
     prev_was_space = False
     for char in text:
         if not prev_was_space and char == " ":
-            new_text += "_"
-            prev_was_space = True
-        elif char == " " and prev_was_space:
             new_text += "-"
             prev_was_space = True
+        elif char == " " and prev_was_space:
+            if new_text[-1] != "_":
+                new_text += "-"
         else:
             new_text += char
-            prev_was_space = False if not char.isspace() else True
+            prev_was_space = False
     return new_text
