0428bfd56fb26ded7809ecc64de9384cb004b27d
beep boop <githubbot@vadim.me>
2024-07-11 14:32:22 +0200

    Solution 96 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/1di1qs3f Prompt:     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0c6191c3-4b05-41d7-869d-e8e077ae8e80.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0c6191c3-4b05-41d7-869d-e8e077ae8e80.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0c6191c3-4b05-41d7-869d-e8e077ae8e80.cpp:7: 0c6191c3-4b05-41d7-869d-e8e077ae8e80.cpp: In function ‘int main()’: 0c6191c3-4b05-41d7-869d-e8e077ae8e80.cpp:17:20: error: ‘incr_list’ was not decla red in this scope    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ 0c6191c3-4b05-41d7-869d-e8e077ae8e80.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,23 +1,24 @@
-
 #include <iostream>
 #include <vector>
 #include <cassert>
 
 using namespace std;
 
-vector<int> incr_list(const vector<int>& l) {
-    vector<int> res = l;
-    for (int i = 0; i < res.size(); i++) {
-        res[i]++;
+vector<int> incr_list(vector<int> l)
+{
+    for (int i = 0; i < l.size(); i++) {
+        l[i]++;
     }
-    return res;
+    return l;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b; 
+bool issame(vector<int> a, vector<int> b)
+{
+    return a == b;
 }
 
-int main() {
+int main()
+{
     assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
     
     return 0;
