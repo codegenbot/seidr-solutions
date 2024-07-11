a378b3c6555326bf738cd38cf83b55fe0efafe7c
beep boop <githubbot@vadim.me>
2024-07-11 22:29:59 +0200

    Solution 72 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/62ryo8im Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/991bd291-85dc-42c3-b7e6-05be8f655 115.py", line 10, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/991bd291-85dc-42c3-b7e6-05be8f655 115.py", line 7, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,5 +1,30 @@
+```
 import math
 
 
+def modinv(a: int, m: int):
+    if math.gcd(a, m) != 1:
+        return None
+    a = a % m
+    t = 0
+    newt = 1
+    r = m
+    newr = a
+
+    while newr != 0:
+        quotient = r // newr
+        t, newt = newt, t - quotient * newt
+        r, newr = newr, r - quotient * newr
+
+    if r > 1:
+        return None
+    if t < 0:
+        t = t + m
+
+    return t
+
+
 def modp(n: int, p: int):
-    return pow(n, p - 1, p) * n % p if math.gcd(n, p) == 1 else None
+    if math.gcd(n, p) != 1:
+        return None
+    return pow(n, modinv(p-2, p), p) * n % p
