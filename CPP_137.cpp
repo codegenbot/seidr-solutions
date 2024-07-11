95bfe7d04fa657f4946a7eac862a0db84d04ac38
beep boop <githubbot@vadim.me>
2024-07-11 07:58:05 +0200

    Solution 66 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/tdobskm4 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,28 +1,29 @@
 if(a.type() == typeid(int) && b.type() == typeid(int)){
-    if(boost::any_cast<int>(a) > boost::any_cast<int>(b))
-        return a;
-    else if(boost::any_cast<int>(a) < boost::any_cast<int>(b))
-        return b;
-    else
-        return "None";
+    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)) return a;
+    else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)) return b;
+    else return "None";
 }
-if(a.type() == typeid(float) && b.type() == typeid(float)){
-    if(boost::any_cast<float>(a) > boost::any_cast<float>(b))
-        return a;
-    else if(boost::any_cast<float>(a) < boost::any_cast<float>(b))
-        return b;
-    else
-        return "None";
+else if(a.type() == typeid(float) && b.type() == typeid(float)){
+    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)) return a;
+    else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)) return b;
+    else return "None";
 }
-if(a.type() == typeid(string) && b.type() == typeid(string)){
-    string strA = boost::any_cast<string>(a);
-    string strB = boost::any_cast<string>(b);
-    if(stof(strA) > stof(strB))
-        return a;
-    else if(stof(strA) < stof(strB))
-        return b;
-    else
-        return "None";
+else if(a.type() == typeid(string) && b.type() == typeid(string)){
+    float fa, fb;
+    if(((string)boost::any_cast<string>(a)).find(",") != string::npos){
+        ((string)boost::any_cast<string>(a)).replace(((string)boost::any_cast<string>(a)).find(","), 1, ".");
+        fa = stof(boost::any_cast<string>(a));
+    }
+    else fa = stof(boost::any_cast<string>(a));
+    
+    if(((string)boost::any_cast<string>(b)).find(",") != string::npos){
+        ((string)boost::any_cast<string>(b)).replace(((string)boost::any_cast<string>(b)).find(","), 1, ".");
+        fb = stof(boost::any_cast<string>(b));
+    }
+    else fb = stof(boost::any_cast<string>(b));
+    
+    if(fa > fb) return a;
+    else if(fa < fb) return b;
+    else return "None";
 }
-return "None";
-}
+else return "None";
