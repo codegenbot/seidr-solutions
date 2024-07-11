882c57de9944743b7483996860bad00f2229013b
beep boop <githubbot@vadim.me>
2024-07-11 08:55:36 +0200

    Solution 6 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5a3e6ab2-a86a-4d19-bb9f-f960f1710 3a5.py", line 14, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5a3e6ab2-a86a-4d19-bb9f-f960f1710 3a5.py", line 9, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5a3e6ab2-a86a-4d19-bb9f-f960f1710 3a5.py", line 2, in sort_third     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5a3e6ab2-a86a-4d19-bb9f-f960f1710 3a5.py", line 5, in <listcomp>     else next((y for y in l if (i - l.index(y)) % 3 != 0), None).sort()[-1])          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AttributeError: 'int' object has no attribute 'sort'

--- Python_33.py
+++ Python_33.py
@@ -1,7 +1,3 @@
 def sort_third(l: list):
-    return [
-        (x
-         if i % 3 != 0
-         else next((y for y in l if (i - l.index(y)) % 3 != 0), None) or next((x for x in sorted((x for i, x in enumerate(l) if i % 3 != 0))), None))
-        for i, x in enumerate(l)
-    ]
+    sorted_list = sorted([x for i, x in enumerate(l) if i % 3 != 0])
+    return [x if i % 3 != 0 else sorted_list[-1] for i, x in enumerate(l)]
