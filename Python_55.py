cd51eaea57d175184f6d3478bb7d36ade6465b58
beep boop <githubbot@vadim.me>
2024-07-11 09:57:51 +0200

    Solution 3 of Python/55, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/g0anmoyf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0aa517e1-bbb6-4f11-99f5-2134214bf 22b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_55.py
+++ Python_55.py
@@ -1,8 +1,10 @@
-```
 def fib(n: int):
     if n <= 1:
         return n
     a, b = 1, 1
-    for _ in range(2, n):
-        a, b = b, a + b
+    for _ in range(2, n + 1):
+        if n > 2:
+            a, b = b, a + b
+        else:
+            a, b = 1, 1
     return b
