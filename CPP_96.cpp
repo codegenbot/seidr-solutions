b22d567adccb90bc1a4597e2847863ea67c9c0ad
beep boop <githubbot@vadim.me>
2024-07-11 06:30:59 +0200

    Solution 40 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/8zvpzqkx Prompt:    43 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8e3d7396-0a69-4f74-bb73-15112bae1f8e.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ 8e3d7396-0a69-4f74-bb73-15112bae1f8e.cpp:36:5: note: ‘int main()’ previously def ined here    36 | int main(){       |     ^~~~ In file included from 8e3d7396-0a69-4f74-bb73-15112bae1f8e.cpp:42: 8e3d7396-0a69-4f74-bb73-15112bae1f8e.cpp: In function ‘int main()’: 8e3d7396-0a69-4f74-bb73-15112bae1f8e.cpp:52:20: error: ‘count_up_to’ was not dec lared in this scope    52 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 8e3d7396-0a69-4f74-bb73-15112bae1f8e.cpp:52:19: error: ‘issame’ cannot be used a s a function    52 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-bool issame(std::vector<int> a, std::vector<int> b){
+bool issame(vector<int> a, vector<int> b){
     if(a.size() != b.size()){
         return false;
     }
@@ -18,8 +18,8 @@ bool issame(std::vector<int> a, std::vector<int> b){
     return true;
 }
 
-std::vector<int> count_up_to(int n){
-    std::vector<int> primes;
+vector<int> count_up_to(int n){
+    vector<int> primes;
     for(int i=2; i<n; ++i){
         bool is_prime = true;
         for(int j=2; j*j<=i; ++j){
@@ -36,7 +36,7 @@ std::vector<int> count_up_to(int n){
 }
 
 int main(){
-    assert(issame(count_up_to(101), std::vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
+    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
     
     return 0;
 }
