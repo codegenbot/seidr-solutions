c383af3ae5b2b9eaae8a1abc9779dee585bd32f8
beep boop <githubbot@vadim.me>
2024-07-11 10:25:10 +0200

    Solution 89 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/20a7a430-41c0-43fc-aa46-a058eb7e2 c71.py", line 10, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/20a7a430-41c0-43fc-aa46-a058eb7e2 c71.py", line 5, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,4 @@
 ```
 def sort_third(l: list):
-    l.sort(key=lambda x: ('BOOL' if isinstance(x, bool) and x else 'INT' if isinstance(x, int) else 'STR') + str(x))
+    l.sort(key=lambda x: str(x))
     return l
