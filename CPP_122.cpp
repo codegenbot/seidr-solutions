54fd76a4e2f48b621d6dd837b235783d02186ee8
beep boop <githubbot@vadim.me>
2024-07-11 13:19:57 +0200

    Solution 30 of CPP/122, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/hi48euf8 Prompt: 6f1a9478-a7cb-4c5e-a0c0-49b8af84957b.cpp:1:18: error: ‘vector’ was not declared in this scope     1 | int add_elements(vector<int> arr, int k) {       |                  ^~~~~~ 6f1a9478-a7cb-4c5e-a0c0-49b8af84957b.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | int add_elements(vector<int> arr, int k) {       |                         ^~~ 6f1a9478-a7cb-4c5e-a0c0-49b8af84957b.cpp:1:35: error: expected primary-expressio n before ‘int’     1 | int add_elements(vector<int> arr, int k) {       |                                   ^~~ 6f1a9478-a7cb-4c5e-a0c0-49b8af84957b.cpp:1:40: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int add_elements(vector<int> arr, int k) {       |                                        ^ In file included from 6f1a9478-a7cb-4c5e-a0c0-49b8af84957b.cpp:11: 6f1a9478-a7cb-4c5e-a0c0-49b8af84957b.cpp: In function ‘int main()’: 6f1a9478-a7cb-4c5e-a0c0-49b8af84957b.cpp:13:25: error: ‘add_elements’ cannot be used as a function    13 |     assert (add_elements({1}, 1) == 1);       |             ~~~~~~~~~~~~^~~~~~~~

--- CPP_122.cpp
+++ CPP_122.cpp
@@ -1,7 +1,9 @@
-int add_elements(vector<int> arr, int k) {
+#include <vector>
+
+int add_elements(std::vector<int> arr, int k) {
     int sum = 0;
-    for (int i = 0; i < min(k, arr.size()); i++) {
-        if (to_string(arr[i]).size() <= 2) {
+    for (int i = 0; i < k; i++) {
+        if ((arr[i] >= 10 && arr[i] <= 99)) {
             sum += arr[i];
         }
     }
