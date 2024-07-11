ca6c15e83c21b726b7abdf975e6f7d879e4e2e98
beep boop <githubbot@vadim.me>
2024-07-11 02:34:14 +0200

    Solution 69 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/49frezuz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/532d2362-126a-4bf9-91ab-e9a99fe4e c97.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,5 +1,7 @@
-```
-def check(lst, func):
-    return func(lst)
+def double_the_difference():
+    def inner(lst):
+        lst.sort()
+        return (lst[-1] - lst[0]) * 2
+    return inner
 
-print(check([1, 2, 3], double_the_difference()))
+check([1, 2, 3], double_the_difference())
