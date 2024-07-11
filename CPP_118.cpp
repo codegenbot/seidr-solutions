4706cdc6f84cb603c241cb510ed6419ffaca0de4
beep boop <githubbot@vadim.me>
2024-07-11 17:06:00 +0200

    Solution 47 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mtng0nmn Prompt: 2e187efc-6d52-45a3-9312-972e9ba162f5.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string get_closest_vowel(string word) {       | ^~~~~~ 2e187efc-6d52-45a3-9312-972e9ba162f5.cpp: In function ‘bool isvowel(char)’: 2e187efc-6d52-45a3-9312-972e9ba162f5.cpp:12:9: error: ‘tolower’ was not declared  in this scope    12 |     c = tolower(c);       |         ^~~~~~~ In file included from 2e187efc-6d52-45a3-9312-972e9ba162f5.cpp:16: 2e187efc-6d52-45a3-9312-972e9ba162f5.cpp: In function ‘int main()’: 2e187efc-6d52-45a3-9312-972e9ba162f5.cpp:18:13: error: ‘get_closest_vowel’ was n ot declared in this scope    18 |     assert (get_closest_vowel("Above") == "o");       |             ^~~~~~~~~~~~~~~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -3,7 +3,7 @@
 
 std::string get_closest_vowel(std::string word) {
     int left = 0, right = word.size() - 1;
-    while (left <= right) {
+    while (left < right) {
         if (!isvowel(word[left])) ++left;
         else if (!isvowel(word[right])) --right;
         else return word.substr(left + 1, right - left);
@@ -14,4 +14,9 @@ std::string get_closest_vowel(std::string word) {
 bool isvowel(char c) {
     c = tolower(c);
     return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
+
+int main() {
+    assert(get_closest_vowel("Above") == "o");
+    return 0;
 }
