5c7f95a1a09033c4ff93d04805632d6ba857dd27
beep boop <githubbot@vadim.me>
2024-07-11 08:15:33 +0200

    Solution 53 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d6xmmywr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/71d6d340-90f1-43a6-ba9c-3772e25aa e6a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,7 +1,7 @@
 """
-Given a list of integers, find the difference between the sum of squares of all positive integers in the list and twice the sum of all positive integers in the list.
+Given a list of integers, find the difference between the sum of squares of all positive numbers and twice the product of all positive numbers.
 """
 
 def double_the_difference(lst):
-    pos_ints = [i for i in lst if isinstance(i, int) and i > 0]
-    return (sum(i**2 for i in pos_ints) - 2 * sum(pos_ints))
+    pos_nums = [i for i in lst if isinstance(i, int) and i > 0]
+    return sum(i**2 for i in pos_nums) - 2 * math.prod(pos_nums)
