9dbd7f76277e40b7d2ea9df7c3e88dbc084152e3
beep boop <githubbot@vadim.me>
2024-07-11 16:25:17 +0200

    Solution 12 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/t93lgfgj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1b7652a-c2c3-43c5-afef-80f08d7ea 467.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -1,3 +1,4 @@
+```
 def minPath(grid, k):
     n = len(grid)
     m = [[i * n + j for j in range(n)] for i in range(n)]
@@ -7,10 +8,11 @@ def minPath(grid, k):
         cur_cell = None
         for i in range(n):
             for j in range(n):
-                if m[i][j] < cur_min:
-                    cur_min = m[i][j]
+                if grid[i][j] < cur_min:
+                    cur_min = grid[i][j]
                     cur_cell = (i, j)
         res.append(cur_min)
         if cur_cell is not None:
-            m[cur_cell[0]][cur_cell[1]] = float("inf")
+            for i in range(n):
+                grid[i][cur_cell[1]] = float("inf")
     return res
