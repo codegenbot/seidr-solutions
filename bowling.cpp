3d0ffae244eceeaaae20d59c1e57d76c473c2d43
beep boop <githubbot@vadim.me>
2024-07-11 21:14:51 +0200

    Solution 40 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/joxoawd6 Prompt: it must return 105 for input 9/35619/5/X6124219-, but it returns -15024.

--- bowling.cpp
+++ bowling.cpp
@@ -4,23 +4,27 @@ using namespace std;
 int calculateScore(string s) {
     int score = 0;
     int frame = 1;
-    int i = 0;
-    
-    while (frame <= 10) {
+    for (int i = 0; i < s.size(); i++) {
         if (s[i] == 'X') {
-            score += 10; 
-            score += (s[i+2] == '/') ? 10 : (s[i+1] - '0') + (s[i+2] - '0');
-            i++; 
-        } else if (s[i+1] == '/') {
-            score += 10 + (s[i+2] == 'X' ? 10 : s[i+2] - '0');
-            i += 2; 
-        } else {
-            score += (s[i] - '0') + (s[i+1] - '0');
-            i += 2; 
+            score += 10;
+            if (frame < 10) {
+                score += (s[i + 1] == 'X') ? 10 : (isdigit(s[i + 1]) ? s[i + 1] - '0' : 10);
+                score += (s[i + 2] == 'X') ? 10 : (isdigit(s[i + 2]) ? s[i + 2] - '0' : 10);
+            }
+            frame++;
+        } else if (s[i] == '/') {
+            score += 10 - (s[i - 1] - '0');
+            score += (isdigit(s[i + 1]) ? s[i + 1] - '0' : 10);
+            if (frame < 10) {
+                score += (s[i + 2] == 'X') ? 10 : (isdigit(s[i + 2]) ? s[i + 2] - '0' : 10);
+            }
+            frame++;
+            i++;
+        } else if (isdigit(s[i])) {
+            score += s[i] - '0';
+            frame++;
         }
-        frame++; 
     }
-    
     return score;
 }
 
