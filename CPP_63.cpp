9c40239146a3bef7cd763f9871741a85c3ef78d4
beep boop <githubbot@vadim.me>
2024-07-11 07:13:46 +0200

    Solution 1 of CPP/63, ratio 0.8571428571428571 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/3fbvknnm Prompt: Complete the following code given the task description and function signature.

--- CPP_63.cpp
+++ CPP_63.cpp
@@ -1,27 +1,17 @@
-#include <stdio.h>
-using namespace std;
-
 int fibfib(int n) {
     if (n == 0 || n == 1)
         return 0;
     else if (n == 2)
         return 1;
     else {
-        int a = 0, b = 0, c = 1, d = 1;
+        int a = 0, b = 0, c = 1, d = 1, e = 1;
         for (int i = 3; i <= n; i++) {
-            int temp = a + b + c;
             a = b;
             b = c;
             c = d;
-            d = temp;
+            d = e;
+            e = a + b + c;
         }
-        return d;
+        return e;
     }
-}
-
-int main() {
-    int n;
-    scanf("%d", &n);
-    printf("%d\n", fibfib(n));
-    return 0;
 }
