38dc4a831d2c0b7dcbe5af3995806e8ec0fe09a5
beep boop <githubbot@vadim.me>
2024-07-11 17:00:00 +0200

    Solution 24 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/t93lgfgj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a0d21f4c-8347-40bc-81eb-2e3918bac e8b.py", line 24, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a0d21f4c-8347-40bc-81eb-2e3918bac e8b.py", line 21, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -1,3 +1,11 @@
-```
 def minPath(grid, k):
-    return sorted([val for row in grid for val in row])[:k]
+    n = len(grid)
+    res = []
+    for _ in range(k):
+        cur_min = float("inf")
+        for i in range(n):
+            for j in range(n):
+                if abs(sum(grid[i]) - grid[i][j]) + abs(sum([grid[m][j] for m in range(n)])) < cur_min:
+                    cur_min = abs(sum(grid[i]) - grid[i][j]) + abs(sum([grid[m][j] for m in range(n)]))
+        res.append(cur_min)
+    return res
