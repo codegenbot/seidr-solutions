9bc6c08f3301f406fdfddc7fd0311964ba8db4ac
beep boop <githubbot@vadim.me>
2024-07-11 16:22:56 +0200

    Solution 70 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/oip7hvah Prompt: eeaef6b6-c8e9-43a7-868d-5f4d2065b3ea.cpp:76:5: error: redefinition of ‘int main( )’    76 | int main(){       |     ^~~~ eeaef6b6-c8e9-43a7-868d-5f4d2065b3ea.cpp:45:5: note: ‘int main()’ previously def ined here    45 | int main() {       |     ^~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,29 +1,62 @@
+#include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
-std::vector<std::string> split_words(std::string txt) {
-    std::vector<std::string> result;
-    size_t pos = 0, prev_pos = 0;
+using namespace std;
 
-    while (pos != std::string::npos) {
-        pos = txt.find_first_of(" ,", prev_pos);
-        if (pos == std::string::npos) {
-            // No whitespaces or commas found
+vector<string> split_words(string txt) {
+    vector<string> result;
+    size_t pos = 0;
+    while (pos < txt.size()) {
+        if (isspace(txt[pos])) {
+            // Split on whitespace
+            string word = txt.substr(0, pos);
+            result.push_back(word);
+            pos++;
+            while (pos < txt.size() && isspace(txt[pos])) {
+                pos++;
+            }
+        } else if (txt.find(',') != string::npos) {
+            // Split on comma
+            size_t commaPos = txt.find(',');
+            if (commaPos == string::npos) {
+                result.push_back(txt.substr(pos));
+                break;
+            }
+            string word = txt.substr(pos, commaPos - pos);
+            result.push_back(word);
+            pos = commaPos + 1;
+        } else {
+            // No whitespace or commas, count odd lower-case letters
             int count = 0;
             for (char c : txt) {
-                if (islower(c)) {
+                if (islower(c) && (c - 'a') % 2 != 0) {
                     count++;
                 }
             }
-            result.push_back(std::to_string(count));
+            result.push_back(to_string(count));
             break;
         }
-
-        std::string word = txt.substr(prev_pos, pos - prev_pos);
-        result.push_back(word);
-
-        prev_pos = pos + 1;
     }
-
     return result;
 }
+
+int main() {
+    string test1 = "Hello world!";
+    vector<string> result1 = split_words(test1);
+    for (string s : result1) {
+        cout << s << endl;
+    }
+
+    string test2 = "Hello,world!";
+    vector<string> result2 = split_words(test2);
+    for (string s : result2) {
+        cout << s << endl;
+    }
+
+    string test3 = "abcdef";
+    vector<string> result3 = split_words(test3);
+    for (string s : result3) {
+        cout << s << endl;
+    }
