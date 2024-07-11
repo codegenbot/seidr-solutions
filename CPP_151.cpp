15a37f9cea813a6d2cf05d3c526bfd459d86334e
beep boop <githubbot@vadim.me>
2024-07-11 12:31:34 +0200

    Solution 64 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/64h8y2gp Prompt:       |                                        ^~~~~ 869b8799-4f95-4567-b697-8988cb338d85.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 869b8799-4f95-4567-b697-8988cb338d85.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from 869b8799-4f95-4567-b697-8988cb338d85.cpp:20: 869b8799-4f95-4567-b697-8988cb338d85.cpp: In function ‘int main()’: 869b8799-4f95-4567-b697-8988cb338d85.cpp:22:35: error: ‘lst’ was not declared in  this scope    22 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 869b8799-4f95-4567-b697-8988cb338d85.cpp:22:38: error: ‘double_the_difference’ c annot be used as a function    22 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ 869b8799-4f95-4567-b697-8988cb338d85.cpp:22:43: error: ‘odd_sum’ was not declare d in this scope    22 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,17 +1,17 @@
-```c++
 int main() {
     vector<float> lst;
-    long long odd_sum = 0;
-    int result = double_the_difference(lst);
-    return 0;
+    long long odd_sum = 0; 
+    int result = double_the_difference(lst, &odd_sum);
 }
 
-long long double_the_difference(vector<float> lst) {
+long long double_the_difference(vector<float> lst, long long* odd_sum) {
     long long sum = 0;
     for (float num : lst) {
         if (num > 0 && floor(num) == num) { 
             if (num % 2 != 0) { 
                 sum += pow(num, 2);
+            } else {
+                *odd_sum += num;
             }
         }
     }
