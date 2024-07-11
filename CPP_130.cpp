cee6166f495ef953f90144eb0ff25ac048735bc5
beep boop <githubbot@vadim.me>
2024-07-11 19:08:25 +0200

    Solution 46 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/sd8nlopu Prompt:       |                    ^~~ 64bf10a6-4c6f-4bd9-ae1f-07f57192d6f0.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 64bf10a6-4c6f-4bd9-ae1f-07f57192d6f0.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 64bf10a6-4c6f-4bd9-ae1f-07f57192d6f0.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 64bf10a6-4c6f-4bd9-ae1f-07f57192d6f0.cpp:18: 64bf10a6-4c6f-4bd9-ae1f-07f57192d6f0.cpp: In function ‘int main()’: 64bf10a6-4c6f-4bd9-ae1f-07f57192d6f0.cpp:28:20: error: ‘tri’ was not declared in  this scope    28 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ 64bf10a6-4c6f-4bd9-ae1f-07f57192d6f0.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,24 +1,10 @@
 #include <vector>
 #include <cassert>
 
-std::vector<int> tri(int n) {
-    std::vector<int> res;
-    if (n >= 0) {
-        res.push_back(3);
-        if (n > 0) {
-            res.push_back(1);
-        }
-        for (int i = 2; i <= n; ++i) {
-            if (i % 2 == 0) {
-                res.push_back(1 + i / 2);
-            } else {
-                res.push_back(res[i - 1] + res[i - 2] + res[i - 3]);
-            }
-        }
-    }
-    return res;
-}
-
 bool issame(std::vector<int> a, std::vector<int> b) {
     // Implementation of the function
+}
+
+std::vector<int> tri(int n) {
+    // Implement the logic for the tri function
 }
