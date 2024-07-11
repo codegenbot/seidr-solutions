3b16676fb5bba0f360283838d694ac024033db1a
beep boop <githubbot@vadim.me>
2024-07-11 02:57:00 +0200

    Solution 82 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/49frezuz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c86d67a7-d60b-4021-9883-df10394ab 5d2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,8 +1,10 @@
-def check(f):
-    print(f([1, 2, 3]))
+```
+def check(f, lst):
+    print(f(lst))
 
 def double_the_difference(lst):
     lst.sort()
     return (lst[-1] - lst[0]) * 2
 
-check(double_the_difference)
+num_list = input("Enter numbers separated by space: ").split()
+check(double_the_difference, num_list)
