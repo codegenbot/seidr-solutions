141fc3600d5aac23369e28e4a397afda71f7393a
beep boop <githubbot@vadim.me>
2024-07-11 19:18:35 +0200

    Solution 16 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qjc1pntq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05b11172-5f29-4109-9bc6-7c4a6ae0e d26.py", line 12, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05b11172-5f29-4109-9bc6-7c4a6ae0e d26.py", line 9, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05b11172-5f29-4109-9bc6-7c4a6ae0e d26.py", line 3, in fruit_distribution     apples = int(''.join(filter(str.isdigit, [x for x in s if 'apples' in x]))[: -7])              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^ ValueError: invalid literal for int() with base 10: ''

--- Python_67.py
+++ Python_67.py
@@ -1,15 +1,5 @@
-```
-import re
 def fruit_distribution(s,n):
     s = s.split()
-    apple_count_str = next((s for s in s if 'apples' in s), None)
-    if apple_count_str is not None:
-        apples = int(re.search(r'\d+', apple_count_str).group())
-    else:
-        apples = 0
-    orange_count_str = next((s for s in s if 'oranges' in s), None)
-    if orange_count_str is not None:
-        oranges = int(re.search(r'\d+', orange_count_str).group())
-    else:
-        oranges = 0
+    apples = int(''.join(filter(str.isdigit, [x for x in s if 'apples' in x])) or 0)
+    oranges = int(''.join(filter(str.isdigit, [x for x in s if 'oranges' in x])) or 0)
     return n - apples - oranges
