87675d2faddef87c528853eb13018e66fc559960
beep boop <githubbot@vadim.me>
2024-07-11 06:00:07 +0200

    Solution 61 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/j8jl9my3 Prompt: Complete the following code given the task description and function signature.

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,10 +1,8 @@
-long long double_the_difference(vector<float> lst) {
+long long double_the_difference(vector<float> lst){
     long long sum = 0;
-    for (float x : lst) {
-        if (x > 0 && floor(x) == x) { // check if positive and integer
-            if (modf(sqrt(x), &x) == 0.0) { // check if square root is an integer
-                sum += pow(x, 2);
-            }
+    for (float num : lst) {
+        if (num > 0 && floor(num) == num) {
+            sum += pow(num, 2);
         }
     }
     return sum;
