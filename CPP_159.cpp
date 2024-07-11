165955fc2a24ab48ae4a821698e38e127578c912
beep boop <githubbot@vadim.me>
2024-07-11 08:35:54 +0200

    Solution 51 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/vzkiiaic Prompt: Complete the following code given the task description and function signature.

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,5 +1,5 @@
-int totalCarrots = number + need;
-    int eatenCarrots = totalCarrots > remaining ? totalCarrots - remaining : totalCarrots;
-    int leftCarrots = remaining > totalCarrots ? 0 : remaining - totalCarrots;
-    return {eatenCarrots, leftCarrots};
+int total = number + need;
+    int eaten = total <= remaining ? total : remaining;
+    int left = remaining - eaten;
+    return {eaten, left};
 }
