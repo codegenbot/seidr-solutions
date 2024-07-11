e4408e636188d1b90b7055f054e04259cb3ef257
beep boop <githubbot@vadim.me>
2024-07-11 12:53:23 +0200

    Solution 54 of Python/14, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gwf131aq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b4059ae3-8b18-4f01-8ee9-2983571ed 382.py", line 13, in <module>     check(all_prefixes)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b4059ae3-8b18-4f01-8ee9-2983571ed 382.py", line 10, in check     assert all_prefixes("WWW") == ["W", "WW", "WWW"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_14.py
+++ Python_14.py
@@ -1,5 +1,8 @@
 def all_prefixes(string: str) -> list[str]:
     if not isinstance(string, str) or len(string) == 0:
         return ['']
-    result = [prefix for i in range(len(string) + 1) for prefix in (string[:i],)]
+    result = []
+    for i in range(len(string)):
+        prefix = string[:i+1]
+        result.append(prefix)
     return result
