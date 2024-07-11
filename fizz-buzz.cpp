6f4e1b04dbf17176a515a3dd7c2bcd442a332cb3
beep boop <githubbot@vadim.me>
2024-07-11 11:57:06 +0200

    Solution 30 of fizz-buzz, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/jhgjhmda Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: aab54b33-181a-48aa-85a5-36e2734fd89d.cpp:20:23:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,16 +1,24 @@
-#include <string>
+#include <iostream>
+#include <sstream>
 using namespace std;
 
-class Solution {
-public:
-    string fizzBuzz(int x) {
-        if(x % 3 == 0 && x % 5 == 0)
-            return "FizzBuzz";
-        else if(x % 3 == 0)
-            return "Fizz";
-        else if(x % 5 == 0)
-            return "Buzz";
-        else
-            return to_string(x);
+string fizzBuzz(int x) {
+    if (x % 3 == 0 && x % 5 == 0)
+        return "FizzBuzz";
+    else if (x % 3 == 0)
+        return "Fizz";
+    else if (x % 5 == 0)
+        return "Buzz";
+    else {
+        stringstream ss;
+        ss << x;
+        return ss.str();
     }
-};
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fizzBuzz(n) << endl;
+    return 0;
+}
