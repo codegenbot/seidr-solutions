5e7ac5d66c69d9b63cacfa563698e714ae01d184
beep boop <githubbot@vadim.me>
2024-07-11 08:18:43 +0200

    Solution 57 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d6xmmywr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/71d6d340-90f1-43a6-ba9c-3772e25aa e6a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,6 +1,10 @@
 """
-Write a function that takes a list of integers as an input and returns the sum of all squares of numbers that are both odd and positive.
+Given a list of integers and strings, find the difference between the sum of squares of positive integers and the product of all digits in the given strings.
 """
-
 def double_the_difference(lst):
-    return sum(i**2 for i in lst if isinstance(i, int) and i % 2 != 0 and i > 0)
+    squares_sum = sum(i**2 for i in lst if isinstance(i, int) and i > 0)
+    digits_product = 1
+    for s in [str(i) for i in lst if isinstance(i, str)]:
+        for digit in s:
+            digits_product *= int(digit)
+    return squares_sum - digits_product
