5571684be4fa85720d748e86498c75926d50440b
beep boop <githubbot@vadim.me>
2024-07-11 11:25:35 +0200

    Solution 6 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hn4pgr1r Prompt:       |                              ^~~~~~ 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:32: 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp: In function ‘int main()’: 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:42:20: error: ‘bf’ was not declared in this scope    42 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,4 +1,37 @@
 ```
+#include <vector>
+#include <algorithm>
+
+bool issame(string planet1, string planet2) {
+    vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+    int index1 = -1;
+    int index2 = -1;
+
+    for (int i = 0; i < planets.size(); i++) {
+        if (planet1 == planets[i]) {
+            index1 = i;
+        }
+        else if (planet2 == planets[i]) {
+            index2 = i;
+        }
+    }
+
+    if (index1 == -1 || index2 == -1) {
+        return false;
+    }
+
+    int start = min(index1, index2);
+    int end = max(index1, index2);
+
+    for (int i = 0; i < planets.size(); i++) {
+        if ((i >= start && i <= end) != issame(planets[i], planet1)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 vector<string> bf(string planet1, string planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
     int index1 = -1;
