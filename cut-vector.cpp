7136ddff38b9483c2df48bd9fa9604b786407bf5
beep boop <githubbot@vadim.me>
2024-07-12 00:18:08 +0200

    Solution 45 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/pai4d1z1 Prompt:       |         ^~~~ 0e4d7d6a-1fe7-4601-bcd8-fdd8bfe3b701.cpp:28:17: error: ‘nums’ was not declared i n this scope; did you mean ‘num’?    28 |         cout << nums[i] << endl;       |                 ^~~~       |                 num 0e4d7d6a-1fe7-4601-bcd8-fdd8bfe3b701.cpp:28:28: error: ‘endl’ was not declared i n this scope    28 |         cout << nums[i] << endl;       |                            ^~~~ 0e4d7d6a-1fe7-4601-bcd8-fdd8bfe3b701.cpp:30:5: error: ‘cout’ was not declared in  this scope    30 |     cout << endl;       |     ^~~~ 0e4d7d6a-1fe7-4601-bcd8-fdd8bfe3b701.cpp:30:13: error: ‘endl’ was not declared i n this scope    30 |     cout << endl;       |             ^~~~ 0e4d7d6a-1fe7-4601-bcd8-fdd8bfe3b701.cpp:31:31: error: ‘nums’ was not declared i n this scope; did you mean ‘num’?    31 |     for (int i = cut_idx; i < nums.size(); ++i) {       |                               ^~~~       |                               num

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -5,14 +5,14 @@
 int main() {
     std::vector<int> nums;
     int num;
-    
+
     while (std::cin >> num) {
         nums.push_back(num);
     }
-    
+
     int min_diff = INT_MAX;
     int cut_idx = -1;
-    
+
     for (int i = 1; i < nums.size(); ++i) {
         int left_sum = 0, right_sum = 0;
         for (int j = 0; j < i; ++j) {
@@ -27,7 +27,7 @@ int main() {
             cut_idx = i;
         }
     }
-    
+
     for (int i = 0; i < cut_idx; ++i) {
         std::cout << nums[i] << std::endl;
     }
@@ -35,6 +35,6 @@ int main() {
     for (int i = cut_idx; i < nums.size(); ++i) {
         std::cout << nums[i] << std::endl;
     }
-    
+
     return 0;
 }
