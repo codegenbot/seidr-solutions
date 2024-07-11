c65d1e030d7a857bd3a3306d623f8b3be9258072
beep boop <githubbot@vadim.me>
2024-07-11 05:52:29 +0200

    Solution 62 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7auz6hs3 Prompt:       |     ^~~~~~ c601ac8f-ff39-401d-b3e6-db72c1ea00a1.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | using namespace std; c601ac8f-ff39-401d-b3e6-db72c1ea00a1.cpp: At global scope: c601ac8f-ff39-401d-b3e6-db72c1ea00a1.cpp:28:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    28 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ c601ac8f-ff39-401d-b3e6-db72c1ea00a1.cpp:4:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     4 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ c601ac8f-ff39-401d-b3e6-db72c1ea00a1.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ c601ac8f-ff39-401d-b3e6-db72c1ea00a1.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <unordered_map>
 #include <cassert>
 using namespace std;
 
@@ -24,4 +23,3 @@ vector<int> remove_duplicates(vector<int> numbers) {
 int main() {
     assert(isame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 2, 3, 4, 5}));
     return 0;
-}
