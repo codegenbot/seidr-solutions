3690030c34f5390eff116a4b6aa4932f89f8da40
beep boop <githubbot@vadim.me>
2024-07-11 16:19:11 +0200

    Solution 57 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/bjc22r2q Prompt:    35 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d4ee3ec7-443e-4434-bf02-e4e27831e1e8.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ d4ee3ec7-443e-4434-bf02-e4e27831e1e8.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~ In file included from d4ee3ec7-443e-4434-bf02-e4e27831e1e8.cpp:34: d4ee3ec7-443e-4434-bf02-e4e27831e1e8.cpp: In function ‘int main()’: d4ee3ec7-443e-4434-bf02-e4e27831e1e8.cpp:44:39: error: invalid initialization of  reference of type ‘const string&’ {aka ‘const std::__cxx11::basic_string<char>& ’} from expression of type ‘std::vector<std::__cxx11::basic_string<char> >’    44 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~ d4ee3ec7-443e-4434-bf02-e4e27831e1e8.cpp:5:32: note: in passing argument 1 of ‘b ool issame(const string&, const string&)’     5 | bool issame(const std::string& str1, const std::string& str2) {       |             ~~~~~~~~~~~~~~~~~~~^~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 
-bool issame(const std::string& str1, const std::string& str2) {
+bool issame(const std::string& str1, const std::string& str2){
     return str1 == str2;
 }
 
