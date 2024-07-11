0cd393819529157305b0168f998a9bd99dc813a2
beep boop <githubbot@vadim.me>
2024-07-11 16:23:15 +0200

    Solution 86 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tdfielxa Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6e616f31-0269-41d6-8dce-b2343f9f2 eda.py", line 16, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6e616f31-0269-41d6-8dce-b2343f9f2 eda.py", line 13, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,12 +1,14 @@
-```python
 def make_palindrome(s: str) -> str:
     s = list(s)
     i, j = 0, len(s) - 1
     while i < j:
         if s[i] != s[j]:
-            t = s.pop(j)
-            s.insert(0, t)
-        else:
-            i += 1
-            j -= 1
+            for k in range(len(s), i, -1):
+                if s[k-1] == s[j]:
+                    s.insert(i, s.pop(j))
+                    break
+            else:
+                s.insert(0, s.pop())
+        i += 1
+        j -= 1
     return "".join(s)
