b47ec48528c903dee35b885306342a48d57dadad
beep boop <githubbot@vadim.me>
2024-07-11 14:39:58 +0200

    Solution 4 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/gufw9jxa Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,26 +1,17 @@
-#include <string>
-#include <map>
-
-using namespace std;
-
 vector<string> by_length(vector<int> arr) {
-    vector<int> numVec;
-    for (int i : arr) {
-        if (i >= 1 && i <= 9) {
-            numVec.push_back(i);
-        }
+    vector<int> temp;
+    for (int num : arr) {
+        if (num >= 1 && num <= 9)
+            temp.push_back(num);
     }
-
-    sort(numVec.begin(), numVec.end());
-    reverse(numVec.begin(), numVec.end());
-
+    sort(temp.begin(), temp.end());
+    reverse(temp.begin(), temp.end());
     vector<string> result;
-    map<int, string> numMap = {{1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
-                                 {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-
-    for (int i : numVec) {
-        result.push_back(numMap[i]);
+    map<int, string> numToName = {{1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, 
+                                  {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
+    for (int num : temp) {
+        if (numToName.find(num) != numToName.end())
+            result.push_back(numToName[num]);
     }
-
     return result;
 }
