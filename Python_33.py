66fb5ffedad068de96d92a9c0d4cabe47afbf280
beep boop <githubbot@vadim.me>
2024-07-11 09:14:27 +0200

    Solution 18 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9ceef58e-c11e-463a-9725-f2ed1c986 efb.py", line 14, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9ceef58e-c11e-463a-9725-f2ed1c986 efb.py", line 9, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9ceef58e-c11e-463a-9725-f2ed1c986 efb.py", line 6, in sort_third     result.append(next(cycle(sorted([x for j, x in enumerate(l) if (j+1) % 3 != 0]), sorted([x for j, x in enumerate(l) if (j+1) % 3 == 0], reverse=True))))                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: cycle expected 1 argument, got 2

--- Python_33.py
+++ Python_33.py
@@ -1,11 +1,14 @@
-```Python
 from itertools import cycle
 
 def sort_third(l: list):
-    odd = [x for j, x in enumerate(l) if (j+1) % 3 != 0]
-    even = [x for j, x in enumerate(l) if (j+1) % 3 == 0]
-    
+    evens = [x for j, x in enumerate(l) if (j+1) % 3 == 0]
+    odds = sorted([x for j, x in enumerate(l) if (j+1) % 3 != 0])
     result = []
     for _ in range(len(l)):
-        result.append(next(cycle(sorted(odd), sorted(even, reverse=True))))
+        result.append(next(cycle(evens)))
+        if evens:
+            evens.pop(0)
+        result.append(next(cycle(odds)))
+        if odds:
+            odds.pop(0)
     return result
