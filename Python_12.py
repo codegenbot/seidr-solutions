4f21c4a12b50d42d32c76734f2cbdd4bf8855eba
beep boop <githubbot@vadim.me>
2024-07-11 15:05:43 +0200

    Solution 45 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/22acifad Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5558f329-57c8-454b-8cc7-2f8034ace 0f2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_12.py
+++ Python_12.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
+
 def longest(strings: list) -> Optional[str]:
     if not strings:
         return None
     return max(strings, key=len)
+
 
 strings = [input("Enter a string (or 'stop' to finish): ") for _ in range(int(input("How many strings do you want to enter? "))) if input() != 'stop']
 longest_string = longest(strings)
