01644a1431d4a94e589a38d5113224eb15593fa4
beep boop <githubbot@vadim.me>
2024-07-11 07:31:38 +0200

    Solution 16 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ucgrnrw1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bbf19e4b-b9d3-484a-9182-7ea64bc33 3fb.py", line 14, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bbf19e4b-b9d3-484a-9182-7ea64bc33 3fb.py", line 10, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,11 +1,7 @@
+import re
+
 def fix_spaces(text):
-    new_text = ""
-    for char in text:
-        if char != " " or (new_text.endswith(" ") and len(new_text) > 1):
-            new_text += char.replace(" ", "_")
-        else:
-            if len(new_text) > 2:
-                new_text += "-"
-            else:
-                new_text += "_"
-    return new_text
+    text = text.replace(" ", "_")
+    while re.search(r"__", text):
+        text = re.sub(r"__", "_-", text)
+    return text
