b731348cdb95a21175057f38fb31cfb04bd500ee
beep boop <githubbot@vadim.me>
2024-07-11 14:39:01 +0200

    Solution 36 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/sa1121ta Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aa6cb819-4a9a-4906-9a64-02352d051 482.py", line 17, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aa6cb819-4a9a-4906-9a64-02352d051 482.py", line 12, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aa6cb819-4a9a-4906-9a64-02352d051 482.py", line 2, in sort_even     sorted_l = sorted((x if abs(x) % 2 != 0 else None for x in l))                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: '<' not supported between instances of 'NoneType' and 'int'

--- Python_37.py
+++ Python_37.py
@@ -1,4 +1,8 @@
 def sort_even(l: list):
-    odd_numbers = [x for x in l if abs(x) % 2 != 0]
-    even_numbers = sorted([x for x in l if abs(x) % 2 == 0])
-    return tuple(even_numbers + odd_numbers)
+    odd = sorted([x for x in l if abs(x) % 2 != 0])
+    even = [
+        x
+        for pair in zip(sorted([x for x in l if abs(x) % 2 == 0]), repeat(len(odd)))
+        for x in pair
+    ]
+    return tuple(odd + even)
