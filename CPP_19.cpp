1f5a8959dfa5ba57c0b39fa87020aad38639f5b0
beep boop <githubbot@vadim.me>
2024-07-11 12:37:06 +0200

    Solution 52 of CPP/19, ratio 0.8 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hv4j11wl Prompt: std::string)’: 4a814887-49d9-4035-8d2e-fcdfdc6b2401.cpp:9:5: error: ‘vector’ was not declared i n this scope     9 |     vector<string> nums;       |     ^~~~~~ 4a814887-49d9-4035-8d2e-fcdfdc6b2401.cpp:3:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?     2 | #include<map>   +++ |+#include <vector>     3 | using namespace std; 4a814887-49d9-4035-8d2e-fcdfdc6b2401.cpp:9:18: error: expected primary-expressio n before ‘>’ token     9 |     vector<string> nums;       |                  ^ 4a814887-49d9-4035-8d2e-fcdfdc6b2401.cpp:9:20: error: ‘nums’ was not declared in  this scope     9 |     vector<string> nums;       |                    ^~~~ 4a814887-49d9-4035-8d2e-fcdfdc6b2401.cpp:26:5: error: ‘sort’ was not declared in  this scope; did you mean ‘short’?    26 |     sort(nums.begin(), nums.end(), [&numMap](string a, string b) {       |     ^~~~       |     short

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,3 +1,6 @@
+#include <string>
+#include <map>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
@@ -5,25 +8,27 @@ string sort_numbers(string numbers) {
     map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
                                 {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
                                 {"eight", 8}, {"nine", 9}};
-    vector<string> numVec;
+    vector<string> nums;
     string temp;
 
-    for (int i = 0; i < numbers.length(); i++) {
-        while (i + 1 <= numbers.length() && !ispunct(numbers[i])) {
+    for (int i = 0; i < numbers.size(); i++) {
+        if (numbers[i] == ' ') {
+            continue;
+        }
+        temp = "";
+        while (i < numbers.size() && numbers[i] != ' ') {
             temp += numbers[i];
             i++;
         }
-        if (!numMap.count(temp)) {
-            return "Invalid input";
-        }
-        numVec.push_back(temp);
-        temp = "";
+        nums.push_back(temp);
     }
 
-    sort(numVec.begin(), numVec.end());
-    string result;
-    for (const auto& num : numVec) {
+    sort(nums.begin(), nums.end());
+
+    string result = "";
+    for (const auto& num : nums) {
         result += num + " ";
     }
-    return result.substr(0, result.length() - 1);
+
+    return result;
 }
