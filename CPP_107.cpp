9ab0cadbbdd214ae7d8c0ddb9ec3f2370ec07d93
beep boop <githubbot@vadim.me>
2024-07-11 15:06:45 +0200

    Solution 1 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/bvg9l28l Prompt: Complete the following code given the task description and function signature.

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,16 +1,16 @@
 vector<int> even_odd_palindrome(int n){
-    vector<int> result(2, 0);
-    for(int i=1; i<=n; ++i){
-        string num = to_string(i);
-        string rev_num = num;
-        reverse(rev_num.begin(), rev_num.end());
-        if(num == rev_num){
-            if(i%2 == 0){
-                result[0]++;
+    vector<int> res(2, 0);
+    for(int i = 1; i <= n; ++i){
+        string str = to_string(i);
+        string rev_str = str;
+        reverse(rev_str.begin(), rev_str.end());
+        if(str == rev_str){
+            if(i % 2 == 0){
+                res[0]++;
             } else {
-                result[1]++;
+                res[1]++;
             }
         }
     }
-    return result;
+    return res;
 }
