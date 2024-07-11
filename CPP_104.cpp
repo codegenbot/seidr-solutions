1b3122e6f2a53fce30acf1517aadd19aa01c6fd1
beep boop <githubbot@vadim.me>
2024-07-11 05:40:24 +0200

    Solution 20 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/m351ng1l Prompt:       |                    ^~~ 6e1444cb-e899-4120-afbe-8464e60e5109.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6e1444cb-e899-4120-afbe-8464e60e5109.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6e1444cb-e899-4120-afbe-8464e60e5109.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6e1444cb-e899-4120-afbe-8464e60e5109.cpp:21: 6e1444cb-e899-4120-afbe-8464e60e5109.cpp: In function ‘int main()’: 6e1444cb-e899-4120-afbe-8464e60e5109.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ 6e1444cb-e899-4120-afbe-8464e60e5109.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,33 +1,33 @@
 #include <vector>
-#include <algorithm>
 
-vector<int> unique_digits(vector<int> x);
-
-bool issame(const vector<int>& a, const vector<int>& b) {
-    return a == b;
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 vector<int> unique_digits(vector<int> x){
     vector<int> result;
-    
     for (int num : x) {
         bool hasEvenDigit = false;
         int temp = num;
         while (temp > 0) {
-            int digit = temp % 10;
-            if (digit % 2 == 0) {
+            if (temp % 2 == 0) {
                 hasEvenDigit = true;
                 break;
             }
             temp /= 10;
         }
-        
         if (!hasEvenDigit) {
             result.push_back(num);
         }
     }
-    
     sort(result.begin(), result.end());
-    
     return result;
 }
