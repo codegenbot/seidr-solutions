cb80dcaaf534c5ca5cb6b6debf3c5733f81ab58b
beep boop <githubbot@vadim.me>
2024-07-11 12:10:21 +0200

    Solution 44 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/64h8y2gp Prompt: __future_base::_State_baseV2>; _Args = {}; __gnu_cxx::_Lock_policy _Lp = __gnu_c xx::_S_atomic]’ /usr/include/c++/11/bits/shared_ptr_base.h:1342:14:   required from ‘std::__shar ed_ptr<_Tp, _Lp>::__shared_ptr(std::_Sp_alloc_shared_tag<_Tp>, _Args&& ...) [wit h _Alloc = std::allocator<std::__future_base::_State_baseV2>; _Args = {}; _Tp = std::__future_base::_State_baseV2; __gnu_cxx::_Lock_policy _Lp = __gnu_cxx::_S_a tomic]’ /usr/include/c++/11/bits/shared_ptr.h:409:59:   required from ‘std::shared_ptr<_ Tp>::shared_ptr(std::_Sp_alloc_shared_tag<_Tp>, _Args&& ...) [with _Alloc = std: :allocator<std::__future_base::_State_baseV2>; _Args = {}; _Tp = std::__future_b ase::_State_baseV2]’ /usr/include/c++/11/bits/shared_ptr.h:862:14:   required from ‘std::shared_ptr<_ Tp> std::allocate_shared(const _Alloc&, _Args&& ...) [with _Tp = std::__future_b ase::_State_baseV2; _Alloc = std::allocator<std::__future_base::_State_baseV2>; _Args = {}]’ /usr/include/c++/11/bits/shared_ptr.h:878:39:   required from ‘std::shared_ptr<_ Tp> std::make_shared(_Args&& ...) [with _Tp = std::__future_base::_State_baseV2;  _Args = {}]’ /usr/include/c++/11/future:1296:43:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::_Sp_counted_ptr_inplace<std::__future_base::_State_baseV2, std::allo cator<std::__future_base::_State_baseV2>, __gnu_cxx::_S_atomic> >’ has no member  named ‘_M_max_size’

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,17 +1,20 @@
-int main() {
-    vector<float> lst;
-    long long odd_sum = 0;
-    int result = double_the_difference(lst);
-}
+#include <iostream>
+#include <vector>
 
-long long double_the_difference(vector<float> lst) {
-    long long sum = 0;
+long long double_the_difference(std::vector<float> lst) {
+    long long odd_sum = 0;
     for (float num : lst) {
-        if (num > 0 && floor(num) == num) { 
+        if (num > 0 && std::floor(num) == num) { 
             if (num % 2 != 0) { 
-                sum += pow(num, 2);
+                odd_sum += std::pow(num, 2);
             }
         }
     }
-    return sum;
+    return odd_sum;
+}
+
+int main() {
+    std::vector<float> lst;  
+    long long result = double_the_difference(lst);
+    std::cout << result << std::endl;
 }
