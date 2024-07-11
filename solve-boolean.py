14efc2728fcd1a2a59c4572a4f25db6a1bf82293
beep boop <githubbot@vadim.me>
2024-07-11 21:13:45 +0200

    Solution 69 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/7uz7cvp2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cad91093-7dbc-47d6-9451-781a53a68 5a6.py", line 10, in <module>     print(solve_boolean(expression))           ^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cad91093-7dbc-47d6-9451-781a53a68 5a6.py", line 2, in solve_boolean     return eval(            ^^^^^   File "<string>", line 1, in <module> NameError: name 'tortandfandtorforforfandforforfort' is not defined

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,9 +1,8 @@
 def solve_boolean(expression):
-    mapping = {"T": True, "F": False, "|": "or", "&": "and"}
+    mapping = {'T': True, 'F': False, '|': 'or', '&': 'and'}
     for key, value in mapping.items():
         expression = expression.replace(key, str(value))
     return eval(expression)
-
 
 expression = input()
 print(solve_boolean(expression))
