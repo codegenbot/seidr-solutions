ea68016c55f153003f8afe4dcd8079f0132c0aa2
beep boop <githubbot@vadim.me>
2024-07-11 15:46:19 +0200

    Solution 56 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/d4o756z2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/95ce5d66-8797-47b8-b99d-a1f72aa90 4bb.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -1,1 +1,22 @@
-input('"Please enter numbers: "')
+from typing import List, Tuple
+
+
+def sum_product(numbers: List[int]) -> Tuple[int, int]:
+    total_sum = 0
+    product = 1
+
+    for num in numbers:
+        total_sum += num
+        product *= num
+
+    return total_sum, product if product != 0 else 1
+
+
+numbers = str(input('Please enter numbers: '))
+try:
+    result = sum_product(list(map(int, numbers.split(','))))
+except ValueError:
+    print("Invalid input! Please enter comma-separated numbers.")
+else:
+    total_sum, product = result
+    print(f"Total sum is {total_sum} and the product is {product}.")
