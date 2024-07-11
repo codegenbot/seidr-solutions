80323b9746d70da0114ba3df81777bcf9f66f2d3
beep boop <githubbot@vadim.me>
2024-07-11 10:07:03 +0200

    Solution 24 of Python/49, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ufwrkg9v Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/398b85df-dccc-4c81-a143-cf91da5bb c3b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,15 +1,8 @@
-def find_max_number():
-    n = int(input("Enter a number: "))
-    p = int(input("Enter another number: "))
-    def modp(n: int, p: int):
-        if p == 1:
-            return 0
-        elif p % 2 == 0:
-            half_pow = modp(n, p // 2) % p
-            return (n * half_pow * half_pow) % p
-        else:
-            if p % 2 == 1: 
-                return n 
-            else: 
-                return (n * modp(n, p // 2) ** 2) % p
-    print(modp(n, p))
+def modp(n: int, p: int):
+    if p == 1:
+        return n
+    elif p % 2 == 0:
+        half_pow = modp(n, p // 2) % p
+        return (n * half_pow * half_pow) % p
+    else:
+        return (n * modp(n, p - 1) % p) % p
