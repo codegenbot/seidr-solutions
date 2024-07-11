1808619f1c988375b52fd3cfe1d395c5771842c2
beep boop <githubbot@vadim.me>
2024-07-11 22:58:05 +0200

    Solution 19 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/6lhchzih Prompt: 1f5274aa-f88f-40cf-b181-e7b0068b3f8e.cpp:1:1: error: expected unqualified-id bef ore ‘if’     1 | if (numMap.find(complement) != numMap.end()) {       | ^~

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,12 +1,12 @@
 int findPair(vector<int>& nums, int target) {
-    unordered_map<int,int> numMap;
-    for(int i = 0; i < nums.size(); i++) {
+    unordered_map<int, bool> numMap;
+    for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
         if (numMap.find(complement) != numMap.end()) {
             int min = (nums[i] < complement ? nums[i] : complement);
             int max = (nums[i] > complement ? nums[i] : complement);
             return {min, max};
         }
-        numMap[nums[i]] = i;
+        numMap[nums[i]] = true;
     }
 }
