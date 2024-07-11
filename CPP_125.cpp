e0480e63fe6f360bbb6e02b3b19d3c93b2ce6c07
beep boop <githubbot@vadim.me>
2024-07-11 02:25:01 +0200

    Solution 4 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/cyujl28f Prompt: Complete the following code given the task description and function signature.

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,21 +1,17 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
 vector<string> split_words(string txt) {
     vector<string> result;
-    size_t pos = 0;
-    while ((pos = txt.find(' ')) != string::npos) {
-        result.push_back(txt.substr(0, pos));
-        txt.erase(0, pos + 1);
+    size_t pos = 0, prev_pos = 0;
+
+    while ((pos = txt.find_first_of(" ,")) != string::npos) {
+        if (pos == 0 || txt[pos - 1] != ' ') {
+            result.push_back(txt.substr(prev_pos, pos - prev_pos));
+            prev_pos = pos + 1;
+        }
     }
-    if (txt.empty()) {
-        return result;
-    }
-    if (txt.find(',') == string::npos) {
+
+    if (prev_pos < txt.size()) {
+        result.push_back(txt.substr(prev_pos));
+    } else if (txt.empty()) {
         int count = 0;
         for (char c : txt) {
             if (c >= 'a' && c <= 'z' && (count & 1)) {
@@ -23,19 +19,7 @@ vector<string> split_words(string txt) {
             }
         }
         result.push_back(to_string(count));
-    } else {
-        result.push_back(txt);
     }
-    return result;
-}
 
-int main() {
-    string txt;
-    cout << "Enter a string: ";
-    getline(cin, txt);
-    vector<string> result = split_words(txt);
-    for (string s : result) {
-        cout << s << endl;
-    }
-    return 0;
+    return result;
 }
