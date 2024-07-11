8217789fc3f779b91aef6155e57542da4b893818
beep boop <githubbot@vadim.me>
2024-07-11 22:37:57 +0200

    Solution 26 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/g6325oyo Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/10f52d97-7d32-4d00-b94c-92c0829b1 3aa.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -3,17 +3,16 @@ def encode(message):
     result = ''
     for char in message:
         if char.isalpha():
-            if char.lower() in 'aeiou':
-                if char.lower() == 'a':
-                    result += 'c'
-                elif char.lower() == 'e':
-                    result += 'g'
-                elif char.lower() == 'i':
-                    result += 'k'
-                elif char.lower() == 'o':
-                    result += 'q'
-                elif char.lower() == 'u':
-                    result += 'y'
+            if char.lower() == 'a':
+                result += 'c'
+            elif char.lower() == 'e':
+                result += 'g'
+            elif char.lower() == 'i':
+                result += 'k'
+            elif char.lower() == 'o':
+                result += 'q'
+            elif char.lower() == 'u':
+                result += 'y'
             else:
                 if char.isupper():
                     result += chr((ord(char) - ord('A') + 3) % 26 + ord('A'))
