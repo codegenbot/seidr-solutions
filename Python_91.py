fe5a0975475dbd244e91de6e08375eca1f7082fd
beep boop <githubbot@vadim.me>
2024-07-11 02:58:30 +0200

    Solution 4 of Python/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1fbfgm4d Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d7767609-3cad-4791-8570-36803d248 cf1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_91.py
+++ Python_91.py
@@ -1,3 +1,13 @@
-```
 def is_bored(S):
-    return sum(1 for sentence in S.split('. ') if sentence.strip().startswith('I'))
+    return sum(
+        1
+        for s in S.split(". ") + S.split("? ") + S.split("! ")
+        if s.strip().lower().startswith("i")
+    )
+
+
+if __name__ == "__main__":
+    S = input("Enter text (type 'q' to quit): ")
+    while S != "q":
+        print(is_bored(S))
+        S = input("Enter text (type 'q' to quit): ")
