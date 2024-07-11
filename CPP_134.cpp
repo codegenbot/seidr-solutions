61bbaae4b31a429ece2468e7bee41f464ccba14f
beep boop <githubbot@vadim.me>
2024-07-11 09:36:34 +0200

    Solution 29 of CPP/134, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/7zeka4l0 Prompt:       | ^~ 20b06521-e913-4d99-ae93-5175196e2028.cpp:4:21: error: ‘txt’ was not declared in this scope     4 |     char lastChar = txt.back();       |                     ^~~ 20b06521-e913-4d99-ae93-5175196e2028.cpp:5:5: error: expected unqualified-id bef ore ‘if’     5 |     if(isalpha(lastChar)){       |     ^~ 20b06521-e913-4d99-ae93-5175196e2028.cpp:10:5: error: expected unqualified-id be fore ‘return’    10 |     return false;       |     ^~~~~~ 20b06521-e913-4d99-ae93-5175196e2028.cpp:11:1: error: expected declaration befor e ‘}’ token    11 | }       | ^ In file included from 20b06521-e913-4d99-ae93-5175196e2028.cpp:13: 20b06521-e913-4d99-ae93-5175196e2028.cpp: In function ‘int main()’: 20b06521-e913-4d99-ae93-5175196e2028.cpp:15:13: error: ‘check_if_last_char_is_a_ letter’ was not declared in this scope    15 |     assert (check_if_last_char_is_a_letter("apple pi e ") == false);       |             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -2,8 +2,7 @@
 #include <cassert>
 using namespace std;
 
-bool check_if_last_char_is_a_letter(const std::string& txt)
-{
+bool check_if_last_char_is_a_letter(const std::string& txt) {
     if(txt.empty())
         return false;
     
