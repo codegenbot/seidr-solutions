d1f9454dc285e989f795e6579e975c3b2ece49bd
beep boop <githubbot@vadim.me>
2024-07-11 16:49:49 +0200

    Solution 57 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/lrpvh4qq Prompt:       |                                 ^~~~~~ In file included from 50baa3bd-dd66-4881-a590-27cddccba071.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 50baa3bd-dd66-4881-a590-27cddccba071.cpp:30:28: error: ‘string’ was not declared  in this scope    30 | int blackPegs(string code, string guess) {       |                            ^~~~~~ 50baa3bd-dd66-4881-a590-27cddccba071.cpp:30:28: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 50baa3bd-dd66-4881-a590-27cddccba071.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 50baa3bd-dd66-4881-a590-27cddccba071.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 50baa3bd-dd66-4881-a590-27cddccba071.cpp:30:40: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | int blackPegs(string code, string guess) {       |                                        ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -6,8 +6,8 @@ int whitePegs(std::string code, std::string guess) {
     std::vector<int> codeMap(6), guessMap(6);
     for (int i = 0; i < 4; i++) {
         if (guess[i] == code[i]) {
-            codeMap[code[i] - 'A']++;
-            guessMap[guess[i] - 'A']++;
+            codeMap[code[i]-'A']++;
+            guessMap[guess[i]-'A']++;
             count++;
         } else {
             for (int j = 0; j < 6; j++) {
