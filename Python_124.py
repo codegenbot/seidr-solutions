e22b435126db02a98ef4d9dce13f3a6163fd31dd
beep boop <githubbot@vadim.me>
2024-07-11 03:16:44 +0200

    Solution 7 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wg5nozkz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f64c0315-8891-4ee4-903a-e41aaf8f5 e6a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_124.py
+++ Python_124.py
@@ -13,17 +13,12 @@ def valid_date(date):
             if int(day) > 30 or int(day) < 1:
                 return False
         elif int(month) == 2:
-            if not (is_leap(year) and int(day) > 29 or int(day) < 1):
-                return False
+            if not ((int(year) % 4 == 0 and int(year) % 100 != 0) or (int(year) % 400 == 0)):
+                if int(day) > 29 or int(day) < 1:
+                    return False
         else:
             if int(day) > 31 or int(day) < 1:
                 return False
     except ValueError:
         return False
     return True
-
-def is_leap(year):
-    if int(year) % 4 == 0 and (int(year) % 100 != 0 or int(year) % 400 == 0):
-        return True
-    else:
-        return False
