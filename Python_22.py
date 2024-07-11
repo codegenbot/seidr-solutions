0f75f812f52f8aec6cb8712010841e435f7b8a1b
beep boop <githubbot@vadim.me>
2024-07-11 15:55:45 +0200

    Solution 58 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/i20fm1ub Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3da7e098-8eae-4d72-954e-32dbdebb6 dca.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -6,10 +6,11 @@ def filter_integers(values: list[Any]) -> list[int]:
     return [value for value in values if isinstance(value, int)]
 
 
-user_input = input("Please enter some integers (separated by spaces): ")
+user_input = input("Please enter some values (separated by spaces): ")
 try:
     values = [int(x) for x in user_input.split()]
 except ValueError as e:
     print(f"Invalid input: {e}")
     values = []
-print(filter_integers(values))
+if values:
+    print(filter_integers(values))
