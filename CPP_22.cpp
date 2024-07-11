14e46fcc353fee328d6385cfaed9775a4719effc
beep boop <githubbot@vadim.me>
2024-07-11 04:20:52 +0200

    Solution 7 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2dqf0nts Prompt:  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f11d616a-b5fa-4a20-8e04-72b15206e165.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f11d616a-b5fa-4a20-8e04-72b15206e165.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f11d616a-b5fa-4a20-8e04-72b15206e165.cpp:12: f11d616a-b5fa-4a20-8e04-72b15206e165.cpp: In function ‘int main()’: f11d616a-b5fa-4a20-8e04-72b15206e165.cpp:22:20: error: ‘filter_integers’ was not  declared in this scope    22 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ f11d616a-b5fa-4a20-8e04-72b15206e165.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,27 +1,24 @@
+```cpp
+#include <boost/any.hpp>
 #include <iostream>
 #include <list>
 #include <vector>
-#include <boost/any.hpp>
 
-bool issame(vector<int> a,vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
 std::vector<int> filter_integers(std::list<boost::any> values) {
     std::vector<int> result;
     for (const auto& value : values) {
-        if (boost::any_cast<bool>(value)) {
-            int num = boost::any_cast<int>(value);
-            bool flag = false;
-            for(const auto& i: result){
-                if(i==num)flag=true;break;
-            }
-            if(!flag)result.push_back(num);
+        int casted_value = boost::any_cast<int>(value);
+        if (!boost::any_cast<bool>(value)) {
+            result.push_back(casted_value);
         }
     }
     return result;
 }
 
 int main() {
-    assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {1, 2, 3}) == false);
+    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {1, 2, 3}) == false);
 }
