b2bf7fc7dae28a37c6665f55bab28db595ba181d
beep boop <githubbot@vadim.me>
2024-07-11 17:38:13 +0200

    Solution 5 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/65kjjion Prompt:       |                    ^~~ d90c0297-6709-48d6-83d8-3593e1d518eb.cpp:24:27: error: ‘vector’ was not declared  in this scope    24 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d90c0297-6709-48d6-83d8-3593e1d518eb.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d90c0297-6709-48d6-83d8-3593e1d518eb.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d90c0297-6709-48d6-83d8-3593e1d518eb.cpp:23: d90c0297-6709-48d6-83d8-3593e1d518eb.cpp: In function ‘int main()’: d90c0297-6709-48d6-83d8-3593e1d518eb.cpp:33:20: error: ‘parse_nested_parens’ was  not declared in this scope    33 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ d90c0297-6709-48d6-83d8-3593e1d518eb.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b);
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
 
 vector<int> parse_nested_parens(string paren_string){
     vector<int> result;
