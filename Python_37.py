6583c8b41309fc63decb460ad03b7ebfcf3f6c22
beep boop <githubbot@vadim.me>
2024-07-11 14:25:36 +0200

    Solution 26 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/sa1121ta Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1e9420ed-4e9b-4340-a797-22f3cb990 14f.py", line 18, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1e9420ed-4e9b-4340-a797-22f3cb990 14f.py", line 13, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,15 +1,10 @@
-```
 def sort_even(l: list):
-    even = sorted([x for x in l if x % 2 == 0])
-    result = []
-    idx = 0
-    for x in l:
-        if x % 2 == 0:
-            if idx < len(even):
-                result.append(even[idx])
-                idx += 1
-            else:
-                result.append(x)
-        else:
-            result.append(x)
+    sorted_l = sorted((x if abs(x) % 2 != 0 else None for x in l))
+    result = [x for x in sorted_l if x is not None] + [
+        x
+        for pair in zip(
+            sorted([x for x in l if abs(x) % 2 == 0]), repeat(len(sorted_l) // 2)
+        )
+        for x in pair
+    ]
     return tuple(result)
