0a9ab80c17424a8cfbe63c3bcaa23e0315d2f8b7
beep boop <githubbot@vadim.me>
2024-07-11 08:59:20 +0200

    Solution 32 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/b3c1v916 Prompt: In file included from 77769450-d14c-413a-b5c4-072656d0d8d5.cpp:20: 77769450-d14c-413a-b5c4-072656d0d8d5.cpp: In function ‘int main()’: 77769450-d14c-413a-b5c4-072656d0d8d5.cpp:30:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    30 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+bool issame(vector<int> v1, vector<int> v2) {
+    if(v1.size()!=v2.size()) return false;
+    for(int i=0; i<v1.size(); i++) {
+        if(v1[i]!=v2[i]) return false;
+    }
+    return true;
+}
+
 vector<int> strange_sort_vector(vector<int> lst) {
     vector<int> result;
     if (lst.empty()) return result;
@@ -15,13 +23,4 @@ vector<int> strange_sort_vector(vector<int> lst) {
             sort(lst.begin(), lst.end());
     }
     return result;
-}
-
-int main() {
-    vector<int> numbers = {1, 3, 2, 4};
-    vector<int> sortedNumbers = strange_sort_vector(numbers);
-    for(int i : sortedNumbers) {
-        cout << i << endl;
-    }
-    return 0;
 }
