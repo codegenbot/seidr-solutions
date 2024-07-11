9f012329e315ea417ddec46311fea45d72bd55ec
beep boop <githubbot@vadim.me>
2024-07-11 10:22:39 +0200

    Solution 81 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v70dnp3u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/53f6206b-c7f8-44e9-9c11-66fd6ead0 f49.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_37.py
+++ Python_37.py
@@ -1,7 +1,10 @@
-```
-def sort_even(lst):
-    return sorted([x for x in lst if x % 2 == 0])
+def sort_even(l):
+    return sorted([x for x in l if x % 2 == 0])
 
 def task_description():
-    l = list(map(int, input().split()))
-    print(sort_even(l))
+    num_list = input().split()
+    num_list = [int(i) for i in num_list]
+    print(sort_even(num_list))
+
+if __name__ == "__main__":
+    task_description()
