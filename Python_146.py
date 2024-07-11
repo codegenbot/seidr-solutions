ebd8b8bfa97bb9af352b2e19b27a66e5c60248f4
beep boop <githubbot@vadim.me>
2024-07-11 03:23:29 +0200

    Solution 0 of Python/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0er37o17 Prompt: Complete the following code given the task description and function signature.

---
+++ Python_146.py
@@ -0,0 +1,7 @@
+```
+def specialFilter(nums):
+    count = 0
+    for num in nums:
+        if abs(num) > 10 and (num % 10) % 2 != 0 and ((abs(num)) // 10) % 2 != 0:
+            count += 1
+    return count
