dbc508bdc759c66b24c9372d1322495c730c6ac5
beep boop <githubbot@vadim.me>
2024-07-11 11:36:46 +0200

    Solution 4 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bl8evmth Prompt:       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from ae749954-af53-4c19-8c40-3a1889b869d0.cpp:2: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ ae749954-af53-4c19-8c40-3a1889b869d0.cpp:14:22: error: aggregate ‘std::stringstr eam ss’ has incomplete type and cannot be defined    14 |         stringstream ss;       |                      ^~ ae749954-af53-4c19-8c40-3a1889b869d0.cpp:15:15: error: ‘hex’ was not declared in  this scope    15 |         ss << hex << setfill('0') << setw(2) << (int)md5[i];       |               ^~~ ae749954-af53-4c19-8c40-3a1889b869d0.cpp:15:22: error: ‘setfill’ was not declare d in this scope    15 |         ss << hex << setfill('0') << setw(2) << (int)md5[i];       |                      ^~~~~~~ ae749954-af53-4c19-8c40-3a1889b869d0.cpp:15:38: error: ‘setw’ was not declared i n this scope; did you mean ‘getw’?    15 |         ss << hex << setfill('0') << setw(2) << (int)md5[i];       |                                      ^~~~       |                                      getw

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,21 +1,5 @@
-#include <string>
-#include <algorithm>
+#include <stringstream>
+#include <iomanip>
 
-std::string string_to_md5(std::string text) {
-    if (text.empty()) return "";
-    
-    unsigned char result[16];
-    MD5_CTX ctx;
-    MD5_Init(&ctx);
-    MD5_Update(&ctx, text.c_str(), text.size());
-    MD5_Final(result, &ctx);
-
-    std::string hashValue = "";
-    for (int i = 0; i < 16; i++) {
-        char temp[3];
-        sprintf(temp, "%02x", result[i]);
-        hashValue += temp;
-    }
-    
-    return hashValue;
-}
+// existing code...
+ss << std::hex << std::setfill('0') << std::setw(2) << (int)md5[i];
