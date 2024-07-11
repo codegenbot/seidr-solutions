9e6bf0700f67530f4064887faf66b290ec877c26
beep boop <githubbot@vadim.me>
2024-07-11 15:42:04 +0200

    Solution 37 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d03dbk1h Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ffe4a8fe-cf42-44e1-a749-ba92957fa bbd.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -8,7 +8,7 @@ def encode(message):
                 index = ord(char.lower()) - 97
                 new_char = chr((index + 2) % 26 + 97)
             else:
-                new_char = chr((ord(char.lower()) - 96 + 1) % 26 + 96).upper() if char.isupper() else chr((ord(char.lower()) - 96 + 1) % 26 + 96)
+                new_char = chr((ord(char.lower()) - 65 + 1) % 26 + 65) if char.isupper() else chr((ord(char.lower()) - 97 + 1) % 26 + 97)
             result += new_char
         else:
             result += char
