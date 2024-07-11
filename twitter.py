f21afbdd339c1ac9d41a5c2d708e5971d7ad1ce3
beep boop <githubbot@vadim.me>
2024-07-11 10:34:43 +0200

    Solution 5 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/lwjwgefn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2288f070-dba0-42d0-9c61-3d75bea50 b54.py", line 1     Here is the Python solution for the problem:                 ^^^^^^ SyntaxError: invalid syntax

--- twitter.py
+++ twitter.py
@@ -7,8 +7,9 @@ def validate_tweet(tweet):
     else:
         return f"Your tweet has {len(tweet)} characters"
 
-print(validate_tweet(""))  
-print(validate_tweet("1"))  
+# test cases
+print(validate_tweet("")) 
+print(validate_tweet("1")) 
 print(validate_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it")) 
-print(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``))"))  
+print(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``))")) 
 print(validate_tweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1"))
