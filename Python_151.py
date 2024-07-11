1850065bc7833332baa1d49f6de74581d39773a1
beep boop <githubbot@vadim.me>
2024-07-11 21:11:02 +0200

    Solution 88 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nxewkfgj Prompt: 2 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bfa1123a-6194-4189-a41e-bb6c40bba f8b.py", line 15, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bfa1123a-6194-4189-a41e-bb6c40bba f8b.py", line 11, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -3,13 +3,4 @@ def double_the_difference(list1, list2):
     return difference * 2
 
 
-def main():
-    list1 = input("Enter the first list of numbers (separated by commas): ").split(",")
-    list1 = [int(x) for x in list1]
-    list2 = input("Enter the second list of numbers (separated by commas): ").split(",")
-    list2 = [int(x) for x in list2]
-    print(double_the_difference(list1, list2))
-
-
-if __name__ == "__main__":
-    main()
+print(double_the_difference([1, 3, 5], [8, 4, 7]))
