20dd48e27a1cf3bde7e8fb05ddfa87ae77b9efdd
beep boop <githubbot@vadim.me>
2024-07-11 10:27:13 +0200

    Solution 6 of Python/73, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xqimsiz5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f18bdbf8-5ba8-437d-a0d4-ce35b1673 9e3.py", line 25, in <module>     check(smallest_change)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f18bdbf8-5ba8-437d-a0d4-ce35b1673 9e3.py", line 22, in check     assert smallest_change([0, 1]) == 1            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_73.py
+++ Python_73.py
@@ -1,19 +1,23 @@
-```
 def smallest_change(arr):
     n = len(arr)
     left, right = 0, n - 1
     changes = 0
-    
+
     while left < right:
         if arr[left] != arr[right]:
-            for i in range(left + 1, right):
+            for i in range(left + 1, right + 1):
                 if arr[i] == arr[right]:
                     arr[i], arr[left] = arr[left], arr[i]
                     changes += 1
                     break
             else:
-                changes += 2
+                if arr[left] < arr[right]:  
+                    arr.insert(left, arr[right])
+                    changes += 1
+                elif arr[left] > arr[right]:  
+                    del arr[left]
+                    changes += 1
         left += 1
         right -= 1
-    
+
     return changes
