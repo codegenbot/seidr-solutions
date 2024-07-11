93ce299919e51acb000ff0f6dab49abaa8a4baa4
beep boop <githubbot@vadim.me>
2024-07-11 03:07:06 +0200

    Solution 6 of Python/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1fbfgm4d Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d7767609-3cad-4791-8570-36803d248 cf1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_91.py
+++ Python_91.py
@@ -1,11 +1,10 @@
 def is_bored(S):
-    if S.strip().lower().startswith('i am bored'):
-        return True
-    else:
-        return False
+    # Your solution here
+    pass
+
 
 if __name__ == "__main__":
     S = input("Enter text (type 'q' to quit): ")
-    while S != 'q':
+    while S != "q":
         print(is_bored(S))
         S = input("Enter text (type 'q' to quit): ")
