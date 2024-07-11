a6bc9200460b6fa3a446d4bd45e14c2170f1025f
beep boop <githubbot@vadim.me>
2024-07-11 21:18:35 +0200

    Solution 52 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/bhx6s3yc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/afafea25-76cf-4aa9-969c-ef366ac4a 224.py", line 22, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/afafea25-76cf-4aa9-969c-ef366ac4a 224.py", line 19, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,16 +1,15 @@
-def match_parens():
-    lst = input("Enter a list of strings: ")
+def match_parens(lst):
     stack = []
-    for s in lst.split(','):
+    for s in lst:
         for c in s:
-            if c == '(':
+            if c == "(":
                 stack.append(c)
-            elif c == ')':
-                if len(stack) == 0:
-                    return 'No'
-                else:
+            elif c == ")":
+                if len(stack) > 0:
                     stack.pop()
+                else:
+                    return "No"
     if len(stack) == 0:
-        return 'Yes'
+        return "Yes"
     else:
-        return 'No'
+        return "No"
