70fe1ecc13e23e32b8b1ae7ac1197bbd312bd749
beep boop <githubbot@vadim.me>
2024-07-11 15:33:27 +0200

    Solution 23 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/bso5h56w Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/72940d94-a172-4576-b16d-368cfc14c 07f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,11 +1,8 @@
-def canvas_info():
-    width = int(input("Enter the width of the canvas: "))
-    height = int(input("Enter the height of the canvas: "))
-    color = input("Enter the color of the canvas (red, blue, green): ")
-    return width, height, color
-
-
-width, height, color = canvas_info()
-print(
-    f"The dimensions of your blank canvas are {width} inches wide and {height} inches tall. The background is a lovely {color}."
-)
+def paint Canvas():
+    for _ in range(10):
+        for __ in range(20):
+            if _ == 5:
+                print('#', end=' ')
+            else:
+                print(' ', end=' ')
+        print()
