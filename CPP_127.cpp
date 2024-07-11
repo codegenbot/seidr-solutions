1688b39436e3cd7d81fb9e0e21680e9437b8d057
beep boop <githubbot@vadim.me>
2024-07-11 11:06:00 +0200

    Solution 8 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ds9e243y Prompt: e9a7493c-c317-4d22-9ef5-19edbd4d0573.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string intersection(vector<int> interval1, vector<int> interval2) {       | ^~~~~~ In file included from e9a7493c-c317-4d22-9ef5-19edbd4d0573.cpp:21: e9a7493c-c317-4d22-9ef5-19edbd4d0573.cpp: In function ‘int main()’: e9a7493c-c317-4d22-9ef5-19edbd4d0573.cpp:23:13: error: ‘intersection’ was not de clared in this scope    23 |     assert (intersection({-2, -2}, {-3, -2}) == "NO");       |             ^~~~~~~~~~~~

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -22,6 +22,6 @@ std::string intersection(std::vector<int> interval1, std::vector<int> interval2)
 }
 
 int main() {
-    assert(intersection({-2, -2}, {-3, -2}) == "NO");
+    assert (intersection({-2, -2}, {-3, -2}) == "NO");
     return 0;
 }
