a0278b6d39a6c8a2be2d029c901ea50fe131cee4
beep boop <githubbot@vadim.me>
2024-07-11 12:44:16 +0200

    Solution 71 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ifoxj73p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7283f5a1-d35f-42c3-96de-1c683cd4f 4eb.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,8 +1,11 @@
-```Python
 def add(x: int, y: int):
-    if not isinstance(x, int) or not isinstance(y, int):
+    try:
+        x = int(x)
+        y = int(y)
+    except ValueError:
         return "Invalid input. Please enter positive integers."
-    if x < 0 and y < 0:
-        return x + y
+    if x < 0 or y < 0:
+        return -1 * (abs(x) + abs(y))
     else:
+        print("The sum is: ", x+y)
         return x + y
