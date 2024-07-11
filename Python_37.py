08e74a2c57d3fd5bdec58111312a21a018ce4b4f
beep boop <githubbot@vadim.me>
2024-07-11 13:57:24 +0200

    Solution 13 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9hixz5xx Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/38c29d6c-c104-48ea-af40-63930e272 446.py", line 10, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/38c29d6c-c104-48ea-af40-63930e272 446.py", line 5, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,4 +1,6 @@
 def sort_even(l: list):
     evens = sorted([x for x in l if x % 2 == 0])
     odds = sorted([x for x in l if x % 2 != 0])
-    return [i if i % 2 != 0 else y for i, y in zip(sorted(l), itertools.cycle(evens + odds))]
+    return [
+        i if i % 2 != 0 else y for i, y in zip(sorted(l), itertools.cycle(evens + odds))
+    ]
