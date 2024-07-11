9880399b1c4f2fa88b1b37c59c83c70ff8ad548b
beep boop <githubbot@vadim.me>
2024-07-11 07:43:45 +0200

    Solution 8 of Python/2, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mtyau5r5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/39ec1eaa-225b-4a6b-aade-31061e942 4ad.py", line 9, in <module>     check(truncate_number)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/39ec1eaa-225b-4a6b-aade-31061e942 4ad.py", line 6, in check     assert abs(truncate_number(123.456) - 0.456) < 1e-6            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_2.py
+++ Python_2.py
@@ -1,4 +1,4 @@
-```
-def truncate_number(number: float, n: int) -> float:
-    multiplier = 10 ** -n
-    return round(number * multiplier) / multiplier
+def truncate_number(number: float) -> float:
+    integer_part = int(abs(number))
+    fractional_part = abs(number) - integer_part
+    return integer_part + (number >= 0) * fractional_part
