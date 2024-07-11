86a63f8b12f2a9c318707d413459260a2ddd3de1
beep boop <githubbot@vadim.me>
2024-07-11 15:07:22 +0200

    Solution 44 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/i3umme6d Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a26b8330-d338-4fb6-a662-7aa723e62 b97.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_26.py
+++ Python_26.py
@@ -1,7 +1,7 @@
+````
 from typing import List
 from collections import OrderedDict
 
-def remove_duplicates(numbers: List[int]) -> List[int]:
-    return list(dict.fromkeys(map(int, input("Enter numbers separated by space:\n").split())))
 
-```
+def remove_duplicates(numbers: List[int]) -> List[int]:
+    return list(OrderedDict.fromkeys(numbers))
