ff3ffbacfcbc76e674df53a18c199ec785c1d7d2
beep boop <githubbot@vadim.me>
2024-07-11 05:34:59 +0200

    Solution 91 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/497575qq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,77 +1,44 @@
-using namespace boost;
+#include <string>
+#include <algorithm>
+
+using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return b;
-    }
-    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return b;
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return greater<int>(get<int>(a), get<float>(b));
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string strA = any_cast<string>(a);
-        string strB = any_cast<string>(b);
-
-        size_t commaPos1 = strA.find(',');
-        size_t commaPos2 = strB.find(',');
-
-        double numA, numB;
-        if (commaPos1 != string::npos) {
-            std::istringstream is(strA.substr(0, commaPos1));
-            is >> numA;
-        }
-        else {
-            istringstream(is) >> numA;
-        }
-
-        if (commaPos2 != string::npos) {
-            std::istringstream is(strB.substr(0, commaPos2));
-            is >> numB;
-        }
-        else {
-            istringstream(is) >> numB;
-        }
-
-        return (numA > numB) ? a : b;
-    }
-    else if (a.type() == typeid(string)) {
-        string str = any_cast<string>(a);
-        size_t commaPos = str.find(',');
-        double num;
-
-        if (commaPos != string::npos) {
-            std::istringstream is(str.substr(0, commaPos));
-            is >> num;
-        }
-        else {
-            istringstream(is) >> num;
-        }
-
-        return (b.type() == typeid(double) && b.convert_to<double>() > num) ? b : a;
-    }
-    else if (b.type() == typeid(string)) {
-        string str = any_cast<string>(b);
-        size_t commaPos = str.find(',');
-        double num;
-
-        if (commaPos != string::npos) {
-            std::istringstream is(str.substr(0, commaPos));
-            is >> num;
-        }
-        else {
-            istringstream(is) >> num;
-        }
-
-        return (a.type() == typeid(double) && a.convert_to<double>() > num) ? a : b;
-    }
-
-    if (a.convert_to<int>() != a.convert_to<double>()) {
-        if (b.convert_to<int>() == b.convert_to<double>()) {
+        string str1 = boost::any_cast<string>(a);
+        string str2 = boost::any_cast<string>(b);
+        if(str1 > str2){
+            return a;
+        }else if(str1 < str2){
             return b;
+        }else{
+            return boost::any("None");
         }
     }
-    else if (a.convert_to<int>() < a.convert_to<double>()) {
-        return b;
+    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        string str = boost::any_cast<string>(a);
+        int num = boost::any_cast<int>(b);
+        return (stoi(str) > num ? a : (num > stoi(str) ? b : boost::any("None")));
     }
-
-    return "None";
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        float f = boost::any_cast<float>(a);
+        int num = boost::any_cast<int>(b);
+        return (f > num ? a : (num > f ? b : boost::any("None")));
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        float f1 = boost::any_cast<float>(a);
+        float f2 = boost::any_cast<float>(b);
+        return (f1 > f2 ? a : (f2 > f1 ? b : boost::any("None")));
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        int i1 = boost::any_cast<int>(a);
+        int i2 = boost::any_cast<int>(b);
+        return (i1 > i2 ? a : (i2 > i1 ? b : boost::any("None")));
+    }
+    else{
+        return boost::any("None");
+    }
 }
