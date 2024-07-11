d20984e806e800e722aefe12f101f34e32f7a1ce
beep boop <githubbot@vadim.me>
2024-07-11 07:31:36 +0200

    Solution 84 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/bxm4xln1 Prompt: Complete the following code given the task description and function signature.

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -1,10 +1,7 @@
 if(arr.empty()) return -32768;
-    int product = 1;
-    int sum = 0;
-    for(int num : arr){
-        if(num > 0) product *= 1;
-        else if(num < 0) product *= -1;
-        sum += abs(num);
-    }
-    return sum * product;
+int product = 1, sum_magnitudes = 0;
+for(int num : arr){
+    product *= num == 0 ? 1 : num / abs(num);
+    sum_magnitudes += abs(num);
 }
+return product * sum_magnitudes;
