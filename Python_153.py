0bf118597a8b8310e346f35e2af7182bdda7a524
beep boop <githubbot@vadim.me>
2024-07-11 13:06:37 +0200

    Solution 1 of Python/153, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nbmv30ym Prompt: Complete the following code given the task description and function signature.

--- Python_153.py
+++ Python_153.py
@@ -1,6 +1,5 @@
 def Strongest_Extension(class_name, extensions):
     strongest_extension = max(
-        extensions,
-        key=lambda x: (sum(c.isupper() for c in x), -sum(c.islower() for c in x)),
+        extensions, key=lambda x: (x.upper().count(x), -x.lower().count(x))
     )
     return f"{class_name}.{strongest_extension}"
