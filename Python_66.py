574f638506e5748db26048a616336df68f7eaffd
beep boop <githubbot@vadim.me>
2024-07-11 10:23:06 +0200

    Solution 16 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ors1r84u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f4c19dca-1bbe-4a97-81a1-101a8e4a6 e20.py", line 15, in <module>     check(digitSum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f4c19dca-1bbe-4a97-81a1-101a8e4a6 e20.py", line 12, in check     assert digitSum("You arE Very Smart") == 327, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f4c19dca-1bbe-4a97-81a1-101a8e4a6 e20.py", line 7, in digitSum     total += ord(char.isupper() and char or char.lower())[0] - 96              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^^^ TypeError: 'int' object is not subscriptable

--- Python_66.py
+++ Python_66.py
@@ -1,9 +1,8 @@
-```
 def digitSum(s):
     total = 0
     for char in s:
         if char.isdigit():
             total += int(char)
         elif char.isalpha():
-            total += ord('a' if char.islower() else 'A') - 96
+            total += ord(char.isupper() and char or char.lower()) - 96
     return total
