f891f1816a5f3bf5df58010f6429663fd8440d1f
beep boop <githubbot@vadim.me>
2024-07-11 04:56:16 +0200

    Solution 23 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zk9dbxmh Prompt: Program did not receive expected input

--- Python_14.py
+++ Python_14.py
@@ -1,9 +1,8 @@
-```
 from typing import List
 
 
 def all_prefixes(string: str) -> List[str]:
-    result = ['']
+    result = [""]
     for char in string:
         temp = []
         for prefix in result:
