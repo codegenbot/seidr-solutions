a519335f2e251a48402aa84f44ec51a211ef2722
beep boop <githubbot@vadim.me>
2024-07-11 12:52:22 +0200

    Solution 78 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xbr14c3t Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:10: f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp: In function ‘int main()’: f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,4 +1,3 @@
-```cpp
 bool issame(vector<string> a, vector<string> b){
     if(a == b)
         return true;
@@ -6,11 +5,28 @@ bool issame(vector<string> a, vector<string> b){
         return false;
 }
 
-vector<vector<string>> split(vector<string> strings, string prefix){
-    vector<vector<string>> result;
+vector<string> filter_by_prefix(vector<string> strings, string prefix){
+    vector<string> result;
     for(string s : strings){
-        if(s.find(prefix) == 0)
-            result.push_back({s});
+        if(s.find(prefix) == 0 || issame(split(s), split(prefix))){
+            result.push_back(s);
+        }
     }
     return result;
+}
+
+vector<string> split(string str, char ch){
+    vector<string> tokens;
+    string token;
+    for(int i = 0; i <= str.length(); i++){
+        if(str[i] == ch || i == str.length()){
+            token += ch;
+            tokens.push_back(token);
+            token = "";
+        }
+        else{
+            token += str[i];
+        }
+    }
+    return tokens;
 }
