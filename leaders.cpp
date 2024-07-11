6ae02370340f1aea3cb75453cb994b25632bae2f
beep boop <githubbot@vadim.me>
2024-07-11 10:52:48 +0200

    Solution 5 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/wcn8tlg7 Prompt: 0a3e15af-ce79-4831-be4d-313dac7be441.cpp: In function ‘std::vector<int> findLead ers(std::vector<int>)’: 0a3e15af-ce79-4831-be4d-313dac7be441.cpp:7:20: error: ‘INT_MIN’ was not declared  in this scope     7 |     int maxRight = INT_MIN;       |                    ^~~~~~~ 0a3e15af-ce79-4831-be4d-313dac7be441.cpp:3:1: note: ‘INT_MIN’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     2 | #include <algorithm>   +++ |+#include <climits>     3 |

--- leaders.cpp
+++ leaders.cpp
@@ -1,8 +1,9 @@
+#include <climits>
 #include <vector>
-using namespace std;
+#include <algorithm>
 
-vector<int> findLeaders(vector<int> nums) {
-    vector<int> leaders;
+std::vector<int> findLeaders(std::vector<int> nums) {
+    std::vector<int> leaders;
     int n = nums.size();
     int maxRight = INT_MIN;
     
@@ -13,7 +14,7 @@ vector<int> findLeaders(vector<int> nums) {
         }
     }
     
-    reverse(leaders.begin(), leaders.end());
+    std::reverse(leaders.begin(), leaders.end());
     
     return leaders;
 }
