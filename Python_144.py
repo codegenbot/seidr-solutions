6a4a827b4f7c8a088b0714c237871748949a856f
beep boop <githubbot@vadim.me>
2024-07-11 04:30:50 +0200

    Solution 62 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/unjkhk0w Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/231d83f9-5ec6-4f23-b9e2-a31574f09 69e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_144.py
+++ Python_144.py
@@ -1,4 +1,3 @@
-```
 import math
 
 
@@ -47,7 +46,9 @@ while True:
         simplified_y_num = c // common_divisor
         simplified_y_denom = d // common_divisor
 
-        print(f"{simplified_x_num}/{simplified_x_denom} and {simplified_y_num}/{simplified_y_denom}")
+        print(
+            f"{simplified_x_num}/{simplified_x_denom} and {simplified_y_num}/{simplified_y_denom}"
+        )
 
         sum_num = a * simplified_y_denom + c * simplified_x_denom
         sum_denom = b * simplified_y_denom
