da4dd8a09480cbeb32f4e68200dfb1fa24e10873
beep boop <githubbot@vadim.me>
2024-07-11 11:04:34 +0200

    Solution 33 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/er6nmf1f Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a78404d2-ade4-431c-b514-89b93719e 155.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_88.py
+++ Python_88.py
@@ -1,9 +1,7 @@
 def sort_array(array):
     array.sort()
-    n = len(array)
-    if n % 2 == 0:
-        median1 = array[n // 2 - 1]
-        median2 = array[n // 2]
-        return [median1, median2]
+    median = len(array) // 2
+    if len(array) % 2 != 0:
+        return [array[median]]
     else:
-        return [array[n // 2]]
+        return [array[:median], array[median:]]
