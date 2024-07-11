b620a78e987f21898100fee0f05d8f60f1c86bd7
beep boop <githubbot@vadim.me>
2024-07-11 03:03:09 +0200

    Solution 29 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/h7augh0t Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 vector<int> minPath(vector<vector<int>> grid, int k) {
     int n = grid.size();
     vector<vector<int>> dp(n, vector<int>(n));
@@ -11,24 +7,33 @@ vector<int> minPath(vector<vector<int>> grid, int k) {
             if (i == 0 && j == 0) {
                 dp[i][j] = grid[i][j];
             } else if (i > 0 && j > 0) {
-                dp[i][j] = min({grid[i][j], dp[i-1][j], dp[i][j-1]});
+                dp[i][j] = min({grid[i-1][j], grid[i][j-1], grid[i-1][j-1]});
             } else if (i > 0) {
-                dp[i][j] = dp[i-1][j];
+                dp[i][j] = grid[i-1][j];
             } else {
-                dp[i][j] = dp[i][j-1];
+                dp[i][j] = grid[i][j-1];
             }
         }
     }
     
     vector<int> path;
     int i = n - 1, j = n - 1;
-    for (int l = 0; l < k; l++) {
+    while (k > 0) {
         path.push_back(grid[i][j]);
         if (i > 0 && j > 0) {
-            if (grid[i-1][j] < grid[i][j-1]) i--;
-            else j--;
-        } else if (i > 0) i--;
-        else j--;
+            if (grid[i-1][j] < grid[i][j-1] && grid[i-1][j] < grid[i-1][j-1]) {
+                i--;
+            } else if (grid[i][j-1] < grid[i-1][j] && grid[i][j-1] < grid[i-1][j-1]) {
+                j--;
+            } else {
+                i--; j--;
+            }
+        } else if (i > 0) {
+            i--;
+        } else {
+            j--;
+        }
+        k--;
     }
     
     reverse(path.begin(), path.end());
