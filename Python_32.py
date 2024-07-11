7c020e5a86d95ce70c99d2110d28c32be0011fd9
beep boop <githubbot@vadim.me>
2024-07-11 14:50:15 +0200

    Solution 63 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/01a7l32z Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -1,12 +1,1 @@
 xs = [int(x) for x in input().split()] + [0]
-
-
-def find_zero(xs):
-    if len(xs) % 2 != 0 or xs[0] != 0:
-        return "Error: xs must start with 0"
-    for i in range(len(xs) - 1):
-        if xs[i] != 0:
-            return "Error: xs must be [0, *coefficients*]"
-    if len(set(xs[1:])) == 1:
-        return None
-    return -xs[-1] / (2 * sum(xs[1:]))
