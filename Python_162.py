9e668dfe0ba90b8945ee18518ab0cddb6a0de32c
beep boop <githubbot@vadim.me>
2024-07-11 10:00:06 +0200

    Solution 52 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/nlobiejv Prompt: Program did not receive expected input

--- Python_162.py
+++ Python_162.py
@@ -2,9 +2,7 @@
 import hashlib
 
 def string_to_md5():
+    text = input("Enter some text: ")
     if not text:
         return None
     return hashlib.md5(text.encode()).hexdigest()
-
-text = input("Enter some text: ")
-print(string_to_md5())
