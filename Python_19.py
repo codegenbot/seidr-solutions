03e295e68b81d217ba488778fb1eeaf6e65acf50
beep boop <githubbot@vadim.me>
2024-07-11 13:18:24 +0200

    Solution 3 of Python/19, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/gfcalwkk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/de22b344-a342-4be5-83a6-a3e7199a9 621.py", line 25, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/de22b344-a342-4be5-83a6-a3e7199a9 621.py", line 20, in check     sort_numbers("six five four three two one zero")   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/de22b344-a342-4be5-83a6-a3e7199a9 621.py", line 16, in sort_numbers     [i for i in [k for k, v in num_dict.items() if v == j] for j in sorted_list]                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/de22b344-a342-4be5-83a6-a3e7199a9 621.py", line 16, in <listcomp>     [i for i in [k for k, v in num_dict.items() if v == j] for j in sorted_list]                                                         ^ NameError: name 'j' is not defined

--- Python_19.py
+++ Python_19.py
@@ -12,6 +12,4 @@ def sort_numbers(numbers: str) -> str:
         "nine": 9,
     }
     sorted_list = sorted([num_dict[i] for i in numbers.split()])
-    return " ".join(
-        [i for i in [k for k, v in num_dict.items() if v == j] for j in sorted_list]
-    )
+    return " ".join([k for k, v in num_dict.items() if v in sorted_list])
