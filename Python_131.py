ae902bd1a05eaebcd880ffcebbdbebfcf8142fb0
beep boop <githubbot@vadim.me>
2024-07-11 11:41:31 +0200

    Solution 19 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mr5ea0uo Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/479b3272-4d9f-4144-b344-925fa247c 2f7.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_131.py
+++ Python_131.py
@@ -1,3 +1,7 @@
 ```
 def digits(n):
-    return len(str(abs(n)))
+    n = str(abs(n))
+    if n[0] == '-':
+        return max(int(digit) for digit in n[1:])
+    else:
+        return max(int(digit) for digit in n)
