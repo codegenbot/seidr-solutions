3cbf5f639584b319a07b8515a506246f57e5ed67
beep boop <githubbot@vadim.me>
2024-07-11 16:36:48 +0200

    Solution 50 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/lrpvh4qq Prompt: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::__cxx11::basi c_string<_CharT, _Traits, _Alloc>::basic_string(std::__cxx11::basic_string<_Char T, _Traits, _Alloc>&&) [with _CharT = char; _Traits = std::char_traits<char>; _A lloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:6667:12:   required from here /usr/include/c++/11/bits/basic_string.h:576:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   576 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,21 +1,20 @@
-#include <string>
-
+```cpp
 int whitePegs(string code, string guess) {
     int count = 0;
-    vector<char> codeMap(6), guessMap(6);
+    vector<int> codeMap(6), guessMap(6);
     for (int i = 0; i < 4; i++) {
         if (guess[i] == code[i]) {
-            codeMap[code[i]]++;
-            guessMap[guess[i]]++;
             count++;
         }
     }
-    for (int i = 0; i < 6; i++) {
-        if (codeMap[i] > 1 || guessMap[i] > 1) {
-            return -1;
-        }
+    for (char c : code) {
+        codeMap[c]++;
     }
-    return count - blackPegs(code, guess);
+    for (char c : guess) {
+        guessMap[c]++;
+    }
+    int blackPegsCount = blackPegs(code, guess);
+    return count - blackPegsCount;
 }
 
 int blackPegs(string code, string guess) {
