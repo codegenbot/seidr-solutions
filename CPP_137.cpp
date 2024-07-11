986dbb5a2df72128bd457a81c24931710f85625d
beep boop <githubbot@vadim.me>
2024-07-11 07:01:51 +0200

    Solution 11 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/tdobskm4 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,21 +1,35 @@
-if (a.type() == typeid(int) && b.type() == typeid(int)) {
-    if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
+if(a.type() == typeid(int) && b.type() == typeid(int)){
+    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
         return a;
-    } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
+    } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
         return b;
+    } else {
+        return "None";
     }
-} else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-    if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
+} else if(a.type() == typeid(float) && b.type() == typeid(float)){
+    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
         return a;
-    } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
+    } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
         return b;
+    } else {
+        return "None";
     }
-} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-    if (stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))) {
+} else if(a.type() == typeid(string) && b.type() == typeid(string)){
+    string strA = boost::any_cast<string>(a);
+    string strB = boost::any_cast<string>(b);
+    if(strA.find_first_of(".,") != string::npos){
+        replace(strA.begin(), strA.end(), ',', '.');
+    }
+    if(strB.find_first_of(".,") != string::npos){
+        replace(strB.begin(), strB.end(), ',', '.');
+    }
+    if(stof(strA) > stof(strB)){
         return a;
-    } else if (stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b))) {
+    } else if(stof(strA) < stof(strB)){
         return b;
+    } else {
+        return "None";
     }
-}
-return "None";
+} else {
+    return "None";
 }
