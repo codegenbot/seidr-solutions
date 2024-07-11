a19fde2c53fbf186c01df9a518ad821079170e7d
beep boop <githubbot@vadim.me>
2024-07-11 14:30:49 +0200

    Solution 16 of CPP/110, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mybof5jy Prompt: ad9a3d00-306e-4d92-88a6-133ad9c5bd87.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string exchange(vector<int> lst1, vector<int> lst2) {       | ^~~~~~ In file included from ad9a3d00-306e-4d92-88a6-133ad9c5bd87.cpp:24: ad9a3d00-306e-4d92-88a6-133ad9c5bd87.cpp: In function ‘int main()’: ad9a3d00-306e-4d92-88a6-133ad9c5bd87.cpp:26:13: error: ‘exchange’ was not declar ed in this scope    26 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,4 +1,8 @@
-string exchange(vector<int> lst1, vector<int> lst2) {
+#include <vector>
+#include <algorithm>
+#include <string>
+
+std::string exchange(std::vector<int> lst1, std::vector<int> lst2) {
     int evenCount = 0;
     for (int num : lst1) {
         if (num % 2 == 0)
@@ -9,7 +13,7 @@ string exchange(vector<int> lst1, vector<int> lst2) {
             bool found = false;
             for (int otherNum : lst2) {
                 if (otherNum % 2 == 0) {
-                    swap(lst1[find(lst1.begin(), lst1.end(), num) - lst1.begin()], lst2[find(lst2.begin(), lst2.end(), otherNum) - lst2.begin()]);
+                    std::swap(lst1[std::find(lst1.begin(), lst1.end(), num) - lst1.begin()], lst2[std::find(lst2.begin(), lst2.end(), otherNum) - lst2.begin()]);
                     found = true;
                     break;
                 }
