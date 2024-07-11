ea0138e92df42f1a06d1a1510ed100a3fa5e638f
beep boop <githubbot@vadim.me>
2024-07-11 06:40:30 +0200

    Solution 49 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/66vz1sa5 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from feb82592-1fb5-49d1-9c14-1412c53d1a56.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,4 +1,4 @@
-```
+```cpp
 #include <vector>
 using namespace std;
 
@@ -16,6 +16,6 @@ vector<vector<int>> cutVector(vector<int>& nums) {
         vector<int> left(min_idx+1), right(n-min_idx-1);
         for(int i=0; i<=min_idx; i++) left.push_back(nums[i]);
         for(int i=min_idx+1; i<n; i++) right.push_back(nums[i]);
-        return {{vector<int>(left.begin(), left.end())}, {vector<int>(right.begin(), right.end())}};
+        return {vector<int>(left.begin(), left.end()), vector<int>(right.begin(), right.end())};
     }
 }
