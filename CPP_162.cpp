45a4301980855073940131ec273d5ea20181b85e
beep boop <githubbot@vadim.me>
2024-07-11 02:39:57 +0200

    Solution 17 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fcqijk62 Prompt:     1 | ```       |  ^ 7cf566c1-258c-4e8c-984e-54a461383231.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 7cf566c1-258c-4e8c-984e-54a461383231.cpp:2:1: error: expected unqualified-id bef ore ‘for’     2 | for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)       | ^~~ 7cf566c1-258c-4e8c-984e-54a461383231.cpp:2:17: error: ‘i’ does not name a type     2 | for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)       |                 ^ 7cf566c1-258c-4e8c-984e-54a461383231.cpp:2:40: error: expected unqualified-id be fore ‘++’ token     2 | for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)       |                                        ^~ In file included from 7cf566c1-258c-4e8c-984e-54a461383231.cpp:5: 7cf566c1-258c-4e8c-984e-54a461383231.cpp: In function ‘int main()’: 7cf566c1-258c-4e8c-984e-54a461383231.cpp:7:13: error: ‘string_to_md5’ was not de clared in this scope     7 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -6,21 +7,27 @@
 
 using namespace std;
 
+void string_to_md5(string input, unsigned char* md5) {
+    MD5(input.c_str(), strlen(input.c_str()), md5);
+}
+
 int main() {
     string input;
-    cout << "Enter a password: ";
-    cin >> input;
+    cout << "Enter your password: ";
+    getline(cin, input);
     
     unsigned char md5[16];
-    MD5(input, md5);
+    string_to_md5(input, md5);
 
     vector<string> result;
     for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)
-        result.push_back(setw(2) << hex << setfill('0') << ((int)(md5[i])));
-
-    cout << "MD5 Hash: ";
-    for(string s : result){
+        result.push_back(setw(2) << hex << setfill('0') << (int)(md5[i]));
+    
+    cout << "MD5: ";
+    for (string s : result) {
         cout << s;
     }
+    cout << endl;
+
     return 0;
 }
