5fb2bd03788489465b95f33a85e333261de6ae84
beep boop <githubbot@vadim.me>
2024-07-11 19:48:26 +0200

    Solution 38 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/xmg4twii Prompt: Program did not receive expected input

--- Python_28.py
+++ Python_28.py
@@ -1,10 +1,9 @@
 from typing import List
 
-
 def concatenate() -> str:
     user_input = input("Enter your text: ")
-    result = ""
-    for string in user_input.split(","):
+    result = ''
+    for string in user_input.split(','):
         if len(string.strip()) > 0:
             result += string.strip()
     return result
