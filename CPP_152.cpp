1437c8b9e22c9551e1a9f39137071f7962081175
beep boop <githubbot@vadim.me>
2024-07-11 02:54:30 +0200

    Solution 41 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8kvpmmn6 Prompt:       |                       ~~~~~~^~ fbf3daac-706d-4017-9fba-a41f4ade2fa6.cpp:10:29: warning: narrowing conversion of  ‘b.std::vector<int>::size()’ from ‘std::vector<int>::size_type’ {aka ‘long unsi gned int’} to ‘int’ [-Wnarrowing] fbf3daac-706d-4017-9fba-a41f4ade2fa6.cpp: In function ‘int main()’: fbf3daac-706d-4017-9fba-a41f4ade2fa6.cpp:14:5: error: ‘assert’ was not declared in this scope    14 |     assert(issame({1,2,3,5},{-1,2,3,4})=={2,0,0,1});       |     ^~~~~~ fbf3daac-706d-4017-9fba-a41f4ade2fa6.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <cassert>   +++ |+#include <cassert>     4 | fbf3daac-706d-4017-9fba-a41f4ade2fa6.cpp: At global scope: fbf3daac-706d-4017-9fba-a41f4ade2fa6.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ fbf3daac-706d-4017-9fba-a41f4ade2fa6.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -7,7 +7,7 @@ using namespace std;
 pair<int, pair<int, int>> compare(vector<int> a, vector<int> b) {
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
-    return {{a.size(), {b.size(), make_pair(0, 0)}};
+    return {a.size(), {b.size(), make_pair(0, 0)}};
 }
 
 int main() {
