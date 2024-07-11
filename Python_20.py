5c0a1181388a5389eeaddc1991950d89b31d6003
beep boop <githubbot@vadim.me>
2024-07-11 15:06:32 +0200

    Solution 50 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g0yxi6dq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b519b56-d167-4b0f-ac55-ad7fc16d1 98f.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_20.py
+++ Python_20.py
@@ -1,13 +1,3 @@
-```Python
-from typing import List, Tuple
-
 def find_closed_elements(numbers: List[float]) -> Tuple[float, float]:
     if len(numbers) <= 1:
         return ()
-    
-    numbers.sort()
-    for i in range(len(numbers) - 1):
-        diff = abs((numbers[i] + numbers[i+1])/2 - numbers[i])
-        if diff <= 0.5:
-            return (numbers[i], (numbers[i] + numbers[i+1])/2)
-    return ()
