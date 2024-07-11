d5669fb16b703b13ca4e87401df304809fb4a3c4
beep boop <githubbot@vadim.me>
2024-07-11 20:02:18 +0200

    Solution 37 of Python/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/2dftcqwt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0f480898-098b-4932-9cfa-15fb2c3f5 26e.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_69.py
+++ Python_69.py
@@ -2,7 +2,6 @@ from collections import Counter
 
 
 def search():
-    max_num = None
     while True:
         user_input = input("Enter a list of numbers separated by space: ")
         if user_input == "":
@@ -11,8 +10,8 @@ def search():
         try:
             lst = [int(x) for x in user_input.split()]
             counter = Counter(lst)
-            num, _ = counter.most_common(1)[0]
-            max_num = num
+            max_num, _ = counter.most_common(1)[0]
+            return max_num
         except ValueError:
             print("Invalid input. Please enter a list of numbers separated by space.")
-    return max_num
+    print("Program ended.")
