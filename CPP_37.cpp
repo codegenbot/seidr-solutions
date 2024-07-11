0952026303c5618178f8d363db764e91e70d82cc
beep boop <githubbot@vadim.me>
2024-07-11 20:31:51 +0200

    Solution 11 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/1ppleavh Prompt:    25 | bool issame(vector<float> a,vector<float>b){       |                    ^~~~~ 9d01535c-0a2a-4cb4-ae83-bc0ea3984e68.cpp:25:29: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 9d01535c-0a2a-4cb4-ae83-bc0ea3984e68.cpp:25:36: error: expected primary-expressi on before ‘float’    25 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from 9d01535c-0a2a-4cb4-ae83-bc0ea3984e68.cpp:24: 9d01535c-0a2a-4cb4-ae83-bc0ea3984e68.cpp: In function ‘int main()’: 9d01535c-0a2a-4cb4-ae83-bc0ea3984e68.cpp:34:20: error: ‘sort_even’ was not decla red in this scope    34 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 9d01535c-0a2a-4cb4-ae83-bc0ea3984e68.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -5,11 +5,11 @@
 
 using namespace std;
 
-bool issame(vector<float> a, vector<float> b) {
+bool issame(const vector<float>& a, const vector<float>& b) {
     return a == b;
 }
 
-void sort_even(vector<float> &l) {
+void sort_even(vector<float>& l) {
     vector<float> even_values;
     vector<float> sorted_even_values;
     for (int i = 0; i < l.size(); i++) {
@@ -30,7 +30,7 @@ void sort_even(vector<float> &l) {
 
 int main() {
     assert(issame({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}, {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
-    vector<float> values = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
-    sort_even(values);
+    vector<float> data = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
+    sort_even(data);
     return 0;
 }
