7ef85ce4563676d5a3204021d31aab25b488e8fd
beep boop <githubbot@vadim.me>
2024-07-11 12:24:46 +0200

    Solution 30 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xbr14c3t Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 2006c02d-b903-43d1-a75a-949169edd48b.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2006c02d-b903-43d1-a75a-949169edd48b.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2006c02d-b903-43d1-a75a-949169edd48b.cpp:11: 2006c02d-b903-43d1-a75a-949169edd48b.cpp: In function ‘int main()’: 2006c02d-b903-43d1-a75a-949169edd48b.cpp:21:20: error: ‘filter_by_prefix’ was no t declared in this scope    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 2006c02d-b903-43d1-a75a-949169edd48b.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,23 +1,11 @@
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size())
+    if(a.size() != b.size()) {
         return false;
+    }
     for(int i = 0; i < a.size(); i++) {
-        bool ismatch = false;
-        for(string s : a[i].split(' ')) {
-            if(find(b[i].begin(), b[i].end(), s) == b[i].end())
-                break;
-            else
-                ismatch = true;
-        }
-        if(!ismatch)
+        if(a[i] != b[i]) {
             return false;
+        }
     }
     return true;
-}
-
-int main() {
-    assert(issame(filter_by_prefix({"apple apple juice", "banana milk", "cherry milk"}, " "), 
-                   filter_by_prefix({"banana juice", "mango smoothie", "grapes fruit salad"}, " ")) == true);
-    
-    return 0;
 }
