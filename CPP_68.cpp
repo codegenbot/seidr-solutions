d50bfef546969e9ac03cbbb22aa1ef8a037ccad0
beep boop <githubbot@vadim.me>
2024-07-12 00:55:37 +0200

    Solution 8 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/s2w6ri6b Prompt:       |                    ^~~ 869ca191-77f6-4f6c-8bed-f32ccd7f5703.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 869ca191-77f6-4f6c-8bed-f32ccd7f5703.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 869ca191-77f6-4f6c-8bed-f32ccd7f5703.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 869ca191-77f6-4f6c-8bed-f32ccd7f5703.cpp:21: 869ca191-77f6-4f6c-8bed-f32ccd7f5703.cpp: In function ‘int main()’: 869ca191-77f6-4f6c-8bed-f32ccd7f5703.cpp:31:20: error: ‘pluck’ was not declared in this scope    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 869ca191-77f6-4f6c-8bed-f32ccd7f5703.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,41 +1,35 @@
 #include <vector>
-#include <cassert>
 
-bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
+bool issame(vector<int> a, vector<int> b) {
+    vector<int> pluck(vector<int> arr){
+        vector<int> result;
+        int smallestEven = INT_MAX;
+        int smallestIndex = INT_MAX;
+        
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] % 2 == 0 && arr[i] < smallestEven){
+                smallestEven = arr[i];
+                smallestIndex = i;
+            }
         }
-    }
-    
-    return true;
-}
-
-vector<int> pluck(vector<int> arr){
-    vector<int> result;
-    int smallestEven = INT_MAX;
-    int smallestIndex = INT_MAX;
-    
-    for(int i = 0; i < arr.size(); i++){
-        if(arr[i] % 2 == 0 && arr[i] < smallestEven){
-            smallestEven = arr[i];
-            smallestIndex = i;
+        
+        if(smallestEven != INT_MAX){
+            result.push_back(smallestEven);
+            result.push_back(smallestIndex);
         }
+        
+        return result;
     }
-    
-    if(smallestEven != INT_MAX){
-        result.push_back(smallestEven);
-        result.push_back(smallestIndex);
-    }
-    
-    return result;
-}
 
-int main() {
-    // Main function can go here for testing
-    return 0;
+    int main() {
+        vector<int> input = {3, 6, 8, 2, 5};
+        
+        vector<int> output = pluck(input);
+        
+        for(int elem : output){
+            cout << elem << " ";
+        }
+        
+        return 0;
+    }
 }
