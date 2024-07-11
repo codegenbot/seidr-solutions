f289ab4d0849c1798747c1ef9dd3ab5f7a6beab0
beep boop <githubbot@vadim.me>
2024-07-11 13:39:57 +0200

    Solution 7 of CPP/47, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/11lwlti4 Prompt: Complete the following code given the task description and function signature.

--- CPP_47.cpp
+++ CPP_47.cpp
@@ -2,11 +2,11 @@
 #include <algorithm>
 
 float median(std::vector<float> l) {
-    std::sort(l.begin(), l.end());
-    int size = l.size();
-    if (size % 2 == 0) {
-        return ((l[size / 2 - 1] + l[size / 2]) / 2.0);
+    sort(l.begin(), l.end());
+    int n = l.size();
+    if (n % 2 == 0) {
+        return (l[n/2 - 1] + l[n/2]) / 2.0;
     } else {
-        return l[size / 2];
+        return l[n/2];
     }
 }
