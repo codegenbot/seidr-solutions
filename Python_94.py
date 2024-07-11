3dc076951c74cdd902e63a2232232ffc03adb878
beep boop <githubbot@vadim.me>
2024-07-11 05:41:58 +0200

    Solution 8 of Python/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/q3nnfb6a Prompt: Complete the following code given the task description and function signature.

--- Python_94.py
+++ Python_94.py
@@ -8,5 +8,5 @@ def skjkasdkd(lst):
                 return False
         return True
 
-    max_prime = max(filter(is_prime, lst))
+    max_prime = max((x for x in lst if is_prime(x)))
     return sum(int(digit) for digit in str(max_prime))
