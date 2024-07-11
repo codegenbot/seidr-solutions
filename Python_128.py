f6f5fdeff77fe48464d4c82c72237df4ef517339
beep boop <githubbot@vadim.me>
2024-07-11 11:58:59 +0200

    Solution 24 of Python/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rfsrpo3u Prompt: Program did not receive expected input

--- Python_128.py
+++ Python_128.py
@@ -1,10 +1,13 @@
+```
 def product_of_signs(arr):
     result = 1
     for x in arr:
-        if x != 0:
-            if x < 0:
-                result *= -1
-    return result
+        if x < 0:
+            result *= -1
+        elif x > 0:
+            continue
+        elif x == 0:
+            return 1
 
 
 def check():
