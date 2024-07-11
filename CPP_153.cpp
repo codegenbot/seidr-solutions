45528c82fe99a4eb984d0520505d6ff190f1469c
beep boop <githubbot@vadim.me>
2024-07-11 12:28:06 +0200

    Solution 58 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/qk069l9j Prompt: In file included from /usr/include/c++/11/string:39,                  from 6c4021bd-61f0-4511-80d8-8ff66aa76e25.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 6c4021bd-61f0-4511-80d8-8ff66aa76e25.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 6c4021bd-61f0-4511-80d8-8ff66aa76e25.cpp:4:59: error: ‘extensions’ was not decla red in this scope     4 | int Strongest_Extension(string class_name, vector<string> extensions) {       |                                                           ^~~~~~~~~~ 6c4021bd-61f0-4511-80d8-8ff66aa76e25.cpp:4:69: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | int Strongest_Extension(string class_name, vector<string> extensions) {       |                                                                     ^ In file included from 6c4021bd-61f0-4511-80d8-8ff66aa76e25.cpp:24: 6c4021bd-61f0-4511-80d8-8ff66aa76e25.cpp: In function ‘int main()’: 6c4021bd-61f0-4511-80d8-8ff66aa76e25.cpp:26:32: error: ‘Strongest_Extension’ can not be used as a function    26 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <vector>
 #include <string>
@@ -10,19 +9,19 @@ int main() {
 
 int Strongest_Extension(std::string class_name, std::vector<std::string> extensions) {
     int strongest = -1;
-    std::string result = "";
-    for (std::string extension : extensions) {
+    string result = "";
+    for (string extension : extensions) {
         int cap = 0, sm = 0;
         for (char c : extension) {
-            if (std::isupper(c)) cap++;
-            else if (std::islower(c)) sm++;
+            if (isupper(c)) cap++;
+            else if (islower(c)) sm++;
         }
         int strength = cap - sm;
         if (strength > strongest) {
             strongest = strength;
             result = extension;
         } else if (strength == strongest) {
-            result = (result < extension ? result : extension);
+            result = min(result, extension);
         }
     }
     return class_name + "." + result;
