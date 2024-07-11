e93bb98dd8380d20e8306b953dd1d3c0cbb12570
beep boop <githubbot@vadim.me>
2024-07-11 02:06:53 +0200

    Solution 93 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/yrw0nlqo Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae816f4d-5f38-4c00-8023-96561c630 3b7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -1,9 +1,9 @@
+```
 from typing import List, Tuple
 
-
 def sum_product(numbers: List[int]) -> Tuple[int, int]:
-    if not all(isinstance(x, int) for x in numbers):
-        return "Error: Input must be a list of integers"
+    if not numbers:
+        return ("Input is empty. Please provide a non-empty list of integers")
     total_sum = sum(numbers)
     product = 1
     for num in numbers:
