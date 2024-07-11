c91c85288b7a216f604f8fb2f24628a2d5c13d75
beep boop <githubbot@vadim.me>
2024-07-11 05:52:37 +0200

    Solution 2 of Python/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/9q647kg1 Prompt: Complete the following code given the task description and function signature.

--- Python_111.py
+++ Python_111.py
@@ -2,20 +2,14 @@
 def histogram(test):
     if not test:
         return {}
-    
-    word_list = test.split()
+    test = test.split()
     max_count = 0
     result = {}
-    
-    for word in word_list:
-        if word not in result:
-            result[word] = 1
-        else:
-            result[word] += 1
-        
-        if result[word] > max_count:
-            max_count = result[word]
-    
-    final_result = {k: v for k, v in result.items() if v == max_count}
-    
-    return final_result
+    for letter in set(test):
+        count = test.count(letter)
+        if count > max_count:
+            max_count = count
+            result = {letter: count}
+        elif count == max_count:
+            result[letter] = count
+    return result
