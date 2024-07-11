81b4453118ca63dd79f4de9b183522ddd636b404
beep boop <githubbot@vadim.me>
2024-07-11 13:36:48 +0200

    Solution 32 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/k91vort1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/31922f61-8a46-484a-bbd1-d4aa48370 884.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,3 +1,4 @@
+```
 while True:
     try:
         xs = [int(i) for i in input("Enter coefficients (a b c [d]): ").split()]
@@ -9,14 +10,7 @@ while True:
             print(f"The solution is {result}.")
         elif len(xs) == 4:
             a, b, c, d = xs
-            p = (b**2) - (4*a*c)
-            if p >= 0:
-                r1 = (-b + p**(0.5)) / (2 * a)
-                r2 = (-b - p**(0.5)) / (2 * a)
-                print(f"The solutions are {r1} and {r2}.")
-            else:
-                result = -b / a
-                print(f"There is one solution: {result}.")
+            result = (c - b) / a
         break
     except ValueError:
         print("Invalid input. Please enter exactly 3 or 4 coefficients: ")
