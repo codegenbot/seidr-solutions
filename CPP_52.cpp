2e7d8efb225e25245d2874aa994c17f72d9064ed
beep boop <githubbot@vadim.me>
2024-07-11 07:05:03 +0200

    Solution 24 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/gkcf4knn Prompt: 89532967-6dcf-425f-a14a-0f0aeb9abb05.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 89532967-6dcf-425f-a14a-0f0aeb9abb05.cpp:12: 89532967-6dcf-425f-a14a-0f0aeb9abb05.cpp: In function ‘int main()’: 89532967-6dcf-425f-a14a-0f0aeb9abb05.cpp:14:17: error: ‘below_threshold’ was not  declared in this scope    14 |     assert (not(below_threshold({1, 8, 4, 10}, 10)));       |                 ^~~~~~~~~~~~~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,6 +1,3 @@
-#include <vector>
-using namespace std;
-
 bool below_threshold(vector<int> l, int t) {
     for (int num : l) {
         if (num >= t) {
@@ -13,4 +10,5 @@ bool below_threshold(vector<int> l, int t) {
 
 int main() {
     assert(not(below_threshold({1, 8, 4, 10}, 10)));
+    return 0;
 }
