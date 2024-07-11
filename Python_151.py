8554c95bc7129ca12ec05a2a0d6dd0de9e0e4ff1
beep boop <githubbot@vadim.me>
2024-07-11 19:47:40 +0200

    Solution 24 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/wu8wktzq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1e240cd3-aed3-483c-91c7-888a901d1 772.py", line 17, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1e240cd3-aed3-483c-91c7-888a901d1 772.py", line 13, in check     double_the_difference(lst) == odd_sum                                   ^^^^^^^ NameError: name 'odd_sum' is not defined

--- Python_151.py
+++ Python_151.py
@@ -1,9 +1,6 @@
-def double_the_difference(lst):
-    return sum(x**2 for x in lst if x > 0 and x % 2 != 0)
-
 def check(odd_sum, lst):
     return double_the_difference(lst) == odd_sum
 
 lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-check_value = check(double_the_difference(lst), lst)
-print(check_value)
+check_result = check(double_the_difference(lst), lst)
+print(check_result)
