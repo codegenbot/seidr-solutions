1ae21669ddcf3921024911096be1139ca2eec8ee
beep boop <githubbot@vadim.me>
2024-07-11 16:46:54 +0200

    Solution 6 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7l4rvpyq Prompt:       |             ^~~~~~ 6a59f5be-2d2e-4554-9714-f257b433b013.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6a59f5be-2d2e-4554-9714-f257b433b013.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 6a59f5be-2d2e-4554-9714-f257b433b013.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from 6a59f5be-2d2e-4554-9714-f257b433b013.cpp:28: 6a59f5be-2d2e-4554-9714-f257b433b013.cpp: In function ‘int main()’: 6a59f5be-2d2e-4554-9714-f257b433b013.cpp:38:20: error: ‘strange_sort_list’ was n ot declared in this scope    38 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 6a59f5be-2d2e-4554-9714-f257b433b013.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
+bool same(vector<int> a, vector<int> b) {
     return a == b;
 }
 
@@ -21,5 +21,6 @@ vector<int> strange_sort_vector(vector<int> lst) {
 }
 
 int main() {
-    assert(issame(vector<int>(strange_sort_vector({1, 1, 1, 1, 1})), vector<int>({1, 1, 1, 1, 1})));
+    assert(same(strange_sort_vector({1, 1, 1, 1, 1}), vector<int>({1, 1, 1, 1, 1})));
+    return 0;
 }
