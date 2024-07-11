064532114b15a7b649ad760b1365a59ef9899a42
beep boop <githubbot@vadim.me>
2024-07-11 11:30:01 +0200

    Solution 62 of indices-of-substring, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/wdnofitq Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 7878a573-7bf7-45ed-937c-caa68b8b513e.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -7,24 +7,16 @@ using namespace std;
 vector<int> indicesOfSubstring(string text, string target) {
     vector<int> result;
     int pos = 0; // position in text
-
-    while (true) { 
-        pos = text.find(target, pos);
-        
-        if (pos == string::npos) {
-            break;
-        }
-
+    while ((pos = text.find(target)) != string::npos) {
         result.push_back(pos);
         pos += target.size(); // move to the next occurrence
     }
-    
     return result;
 }
 
 int main() {
-    string text = "3/G(N7";
-    string target = "3/G";
+    string text = "ABCDEF";
+    string target = "CDE";
     vector<int> result = indicesOfSubstring(text, target);
     for (int i : result) {
         cout << i << endl;
