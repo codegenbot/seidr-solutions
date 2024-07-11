ab4eee7f36ae1f3fa577c8aa967dca752399657d
beep boop <githubbot@vadim.me>
2024-07-11 02:14:40 +0200

    Solution 75 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5mt0m7n3 Prompt: Program did not receive expected input

--- Python_22.py
+++ Python_22.py
@@ -2,5 +2,7 @@ from typing import List, Any
 
 
 def filter_integers(values: List[Any]) -> list:
-    values = input("Enter comma-separated integers (or any other data type): ").split(",")
-    return [value for value in map(lambda x: int(x) if x else None, values) if isinstance(int(value), int)]
+    values = input("Enter comma-separated integers (or any other data type): ").split(
+        ","
+    )
+    return [int(value) for value in values if isinstance(int(value), int)]
