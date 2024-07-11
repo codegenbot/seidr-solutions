6e856fbcdbcdf0fc1210ce2e120e4c84b0375b67
beep boop <githubbot@vadim.me>
2024-07-11 19:57:46 +0200

    Solution 48 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qjc1pntq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0f7591aa-c452-48ec-9330-50c4b575f ca5.py", line 13, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0f7591aa-c452-48ec-9330-50c4b575f ca5.py", line 10, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -1,7 +1,7 @@
-```Python
+```
 def fruit_distribution(s, n):
     s = s.lower()
     n = int(n)
-    apples = int("".join(filter(str.isdigit, s.split()[:-2])).split()[0])
-    oranges = int("".join(filter(str.isdigit, s.split()[:-2])).split()[1])
-    return n - (apples + oranges)
+    apples = sum(int(x) for x in "".join(filter(str.isdigit, s.split()[-2])).split())
+    oranges = n - apples
+    return oranges
