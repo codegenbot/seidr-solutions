67796318bba12a2cbd191ee7985a48270daa4975
beep boop <githubbot@vadim.me>
2024-07-11 10:43:38 +0200

    Solution 43 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/z33m1s2p Prompt: In file included from 62023361-2285-4b97-aece-9d0c4891e9ae.cpp:13: 62023361-2285-4b97-aece-9d0c4891e9ae.cpp: In function ‘int main()’: 62023361-2285-4b97-aece-9d0c4891e9ae.cpp:15:35: error: ‘lst’ was not declared in  this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 62023361-2285-4b97-aece-9d0c4891e9ae.cpp:15:43: error: ‘odd_sum’ was not declare d in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 
 long long double_the_difference(std::vector<float> lst){
@@ -13,6 +14,8 @@ long long double_the_difference(std::vector<float> lst){
 int main() {
     std::vector<float> lst = {1.1, 2.2, 3.3, 4.4, 5.0};
     long long odd_sum = double_the_difference(lst);
+
+    std::cout << odd_sum;
 
     return 0;
 }
