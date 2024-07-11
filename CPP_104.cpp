fc9be3a8b73a97737ce2925cd5d5be4e81a2a540
beep boop <githubbot@vadim.me>
2024-07-11 05:39:07 +0200

    Solution 19 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/m351ng1l Prompt:       |                    ^~~ 98ac55f7-0757-4866-8552-d939f16f66ac.cpp:27:27: error: ‘vector’ was not declared  in this scope    27 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 98ac55f7-0757-4866-8552-d939f16f66ac.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 98ac55f7-0757-4866-8552-d939f16f66ac.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 98ac55f7-0757-4866-8552-d939f16f66ac.cpp:26: 98ac55f7-0757-4866-8552-d939f16f66ac.cpp: In function ‘int main()’: 98ac55f7-0757-4866-8552-d939f16f66ac.cpp:36:20: error: ‘unique_digits’ was not d eclared in this scope    36 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ 98ac55f7-0757-4866-8552-d939f16f66ac.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -3,4 +3,31 @@
 
 vector<int> unique_digits(vector<int> x);
 
-bool issame(const vector<int>& a, const vector<int>& b);
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a == b;
+}
+
+vector<int> unique_digits(vector<int> x){
+    vector<int> result;
+    
+    for (int num : x) {
+        bool hasEvenDigit = false;
+        int temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+            if (digit % 2 == 0) {
+                hasEvenDigit = true;
+                break;
+            }
+            temp /= 10;
+        }
+        
+        if (!hasEvenDigit) {
+            result.push_back(num);
+        }
+    }
+    
+    sort(result.begin(), result.end());
+    
+    return result;
+}
