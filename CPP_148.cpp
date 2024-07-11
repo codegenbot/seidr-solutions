e5c8a3e13dbeed22f3c9313e23abe56212e19f6f
beep boop <githubbot@vadim.me>
2024-07-11 11:31:12 +0200

    Solution 13 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hn4pgr1r Prompt:       |                              ^~~~~~ 27e5f8f7-be19-4d90-8896-a6b2b166d2bb.cpp:40:37: error: ‘string’ was not declared  in this scope    40 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 27e5f8f7-be19-4d90-8896-a6b2b166d2bb.cpp:40:44: error: ‘b’ was not declared in t his scope    40 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 27e5f8f7-be19-4d90-8896-a6b2b166d2bb.cpp:40:45: error: expression list treated a s compound expression in initializer [-fpermissive]    40 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 27e5f8f7-be19-4d90-8896-a6b2b166d2bb.cpp:39: 27e5f8f7-be19-4d90-8896-a6b2b166d2bb.cpp: In function ‘int main()’: 27e5f8f7-be19-4d90-8896-a6b2b166d2bb.cpp:49:20: error: ‘bf’ was not declared in this scope    49 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 27e5f8f7-be19-4d90-8896-a6b2b166d2bb.cpp:49:19: error: ‘issame’ cannot be used a s a function    49 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,10 +1,18 @@
-```cpp
 #include <vector>
 #include <string>
 
+bool issame(vector<string> a, vector<string>) {
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != planets[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 std::vector<std::string> bf(std::string planet1, std::string planet2) {
     std::vector<std::string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Makemake"};
-
+    
     int index1 = -1;
     for (int i = 0; i < planets.size(); i++) {
         if (planet1 == planets[i]) {
@@ -12,7 +20,7 @@ std::vector<std::string> bf(std::string planet1, std::string planet2) {
             break;
         }
     }
-
+    
     int index2 = -1;
     for (int i = 0; i < planets.size(); i++) {
         if (planet2 == planets[i]) {
@@ -20,9 +28,9 @@ std::vector<std::string> bf(std::string planet1, std::string planet2) {
             break;
         }
     }
-
+    
     std::vector<std::string> result;
-
+    
     if (index1 >= 0 && index2 >= 0) {
         if (index1 > index2) {
             for (int i = index1; i >= index2 + 1; i--) {
@@ -36,6 +44,6 @@ std::vector<std::string> bf(std::string planet1, std::string planet2) {
             return result;
         }
     }
-
+    
     return result;
 }
