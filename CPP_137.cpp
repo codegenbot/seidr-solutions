82e1d1d1eb0016de123fe16f772152f5f8175d86
beep boop <githubbot@vadim.me>
2024-07-11 03:53:27 +0200

    Solution 19 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/497575qq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,37 +1,22 @@
 #include <boost/any.hpp>
-#include <string>
-#include <algorithm>
-
-using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
+    if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return (int)a > (double)b ? a : b;
+    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return (double)a > (int)b ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = boost::any_cast<string>(a);
+        string str2 = boost::any_cast<string>(b);
+        return str1 > str2 ? a : b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        int num1 = boost::any_cast<int>(a);
+        string str2 = boost::any_cast<string>(b);
+        return num1 > stoi(str2) ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        string str1 = boost::any_cast<string>(a);
+        int num2 = boost::any_cast<int>(b);
+        return stoi(str1) > num2 ? a : b;
     }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string str = (string)b.convert_to<string>();
-        size_t pos = str.find(',');
-        if (pos != string::npos) {
-            float f1 = stof(str.substr(0, pos));
-            float f2 = stof(str.substr(pos + 1));
-            return max(f1, f2);
-        }
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = (string)a.convert_to<string>();
-        size_t pos1 = str1.find(',');
-        string str2 = (string)b.convert_to<string>();
-        size_t pos2 = str2.find(',');
-
-        if (pos1 != string::npos && pos2 != string::npos) {
-            float f1 = stof(str1.substr(0, pos1));
-            float f2 = stof(str2.substr(0, pos2));
-            return max(f1, f2);
-        }
-        else if (pos1 == string::npos && pos2 == string::npos) {
-            return "None";
-        }
-    }
-
-    return b;
+    return boost::any("None");
 }
