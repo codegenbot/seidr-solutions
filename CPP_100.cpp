774c59c268a932a7d01d933e0489623ba254fc1c
beep boop <githubbot@vadim.me>
2024-07-11 14:47:15 +0200

    Solution 2 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/d31cl2gl Prompt:       |                    ^~~ 6d4af33c-d263-4991-a987-bddc0b9c45c2.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6d4af33c-d263-4991-a987-bddc0b9c45c2.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6d4af33c-d263-4991-a987-bddc0b9c45c2.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6d4af33c-d263-4991-a987-bddc0b9c45c2.cpp:13: 6d4af33c-d263-4991-a987-bddc0b9c45c2.cpp: In function ‘int main()’: 6d4af33c-d263-4991-a987-bddc0b9c45c2.cpp:23:20: error: ‘make_a_pile’ was not dec lared in this scope    23 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 6d4af33c-d263-4991-a987-bddc0b9c45c2.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,13 +1,27 @@
-vector<int> make_a_pile(int n){
-    vector<int> result;
-    result.push_back(n);
+#include <vector>
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<int> make_a_pile(int n) {
+    vector<int> stones;
+    stones.push_back(n);
     for (int i = 1; i < n; ++i) {
         if (n % 2 == 0) {
             n += 2;
         } else {
             n += 1;
         }
-        result.push_back(n);
+        stones.push_back(n);
     }
-    return result;
+    return stones;
 }
