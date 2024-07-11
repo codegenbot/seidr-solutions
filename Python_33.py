a79182b033c76289978d3de17fea82d81ff6b9ba
beep boop <githubbot@vadim.me>
2024-07-11 16:53:39 +0200

    Solution 86 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/qompfmfp Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c5339f02-837b-4392-8561-ee99958b5 b4a.py", line 16, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c5339f02-837b-4392-8561-ee99958b5 b4a.py", line 11, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,10 +1,12 @@
 ```
 def sort_third(l: list):
     result = []
-    for i in range(0, len(l), 3):
-        temp = l[i:i+3]
-        if temp:
-            result.append(sorted(temp)[0])
-        else:
-            result.append(None)
+    i = 0
+    while i < len(l):
+        if i % 3 == 0:
+            temp = l[i:i+3]
+            result.append(sorted(temp)[0] if temp else None)
+        elif i < len(l) - 1:
+            result.append(min(l[i:]))
+        i += 1
     return tuple(result)
