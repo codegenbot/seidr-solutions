113c81f5c12aed64b1200a03896d68b23543949b
beep boop <githubbot@vadim.me>
2024-07-12 01:24:03 +0200

    Solution 33 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/dckwey15 Prompt: ec7df4c9-0505-4924-ac57-dc91cb1c8b8b.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -5,24 +5,33 @@ int bowlingScore(string s) {
     int i = 0;
     while (i < s.length()) {
         if (s[i] == '/') {
-            int firstRoll = stoi(s.substr(0, i));
-            int secondRoll = stoi(s.substr(i + 1, s.find('/', i + 1) - i - 1));
-            if (firstRoll + secondRoll >= 10) {
+            string firstRoll = s.substr(0, i);
+            s.erase(0, i + 1);
+            i = -1;
+        }
+        else if (i >= s.length() - 1 || s[i+1] != '/') {
+            int roll = stoi(s.substr(i));
+            if (roll > 10) {
                 score += 10;
-                i = s.find('/', i + 1);
-            } else {
-                score += firstRoll + secondRoll;
-                i = s.find('/', i + 3);
-            }
-        } else {
-            int roll = stoi(s.substr(i, 1));
-            if (roll == 10) {
-                score += 10;
-                i++;
-            } else {
+                s.erase(0, i+2);
+                i = -1;
+            } 
+            else {
                 score += roll;
                 i++;
             }
+        } 
+        else {
+            int firstRoll = stoi(s.substr(0, i+1));
+            string secondRoll = s.substr(i+1);
+            if (firstRoll +stoi(secondRoll) >= 10) {
+                score += 10;
+            } 
+            else {
+                score += firstRoll + stoi(secondRoll);
+            }
+            s.erase(0, i+2);
+            i = -1;
         }
     }
     return score;
