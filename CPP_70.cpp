557946809c8137cc4acb59ce5b2968d6c30d932f
beep boop <githubbot@vadim.me>
2024-07-11 09:52:57 +0200

    Solution 61 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/b3c1v916 Prompt:       |           ^~~~~~ In file included from 7a808e7d-c2f6-4782-9e8b-ebc8a316c9cc.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7a808e7d-c2f6-4782-9e8b-ebc8a316c9cc.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7a808e7d-c2f6-4782-9e8b-ebc8a316c9cc.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7a808e7d-c2f6-4782-9e8b-ebc8a316c9cc.cpp:22: 7a808e7d-c2f6-4782-9e8b-ebc8a316c9cc.cpp: In function ‘int main()’: 7a808e7d-c2f6-4782-9e8b-ebc8a316c9cc.cpp:32:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    32 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 7a808e7d-c2f6-4782-9e8b-ebc8a316c9cc.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -9,18 +9,15 @@ std::vector<int> strange_sort_vector(std::vector<int> lst) {
     std::vector<int> result;
     while (!lst.empty()) {
         int min_val = *min_element(lst.begin(), lst.end());
-        int max_val = *max_element(lst.begin(), lst.end());
+        auto it_min = min_element(lst.begin(), lst.end());
+        int max_val = *max_element(it_min, lst.end());
         if (min_val == max_val) {
-            for(int x : lst) {
-                if(x==min_val)
-                    result.push_back(x);
-            }
+            result.push_back(min_val);
             lst.clear();
         } else {
             result.push_back(min_val);
-            auto it = find_if(lst.begin(), lst.end(), [max_val](int x) { return x == max_val; });
-            result.push_back(*it);
-            lst.erase(it);
+            it_min = lst.erase(it_min);
+            result.push_back(*it_min);
         }
     }
     return result;
