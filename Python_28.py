cc1d238358011f59f1015b8d3c982ea692e16b5b
beep boop <githubbot@vadim.me>
2024-07-11 17:26:14 +0200

    Solution 97 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/dszeq8nu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f49e0f41-2d42-450a-ae3e-1534ccfc0 306.py", line 4     return ", ".join(sorted(map(str, set(result)))))                                                    ^ SyntaxError: unmatched ')'

--- Python_28.py
+++ Python_28.py
@@ -2,5 +2,3 @@ def concatenate(strings: str) -> str:
     try:
         result = [s.strip() for s in strings.split(",")]
         return ", ".join(sorted(map(str, set(result))))))
-    except ValueError:
-        return "Error: Invalid input"
