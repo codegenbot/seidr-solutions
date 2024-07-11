9d2dbdc089638e98d8233eded93d9b83104d7c2e
beep boop <githubbot@vadim.me>
2024-07-11 15:34:59 +0200

    Solution 34 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/q9iin0k2 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 34d29481-912f-4a77-8dd6-aad4297d4ea9.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- leaders.cpp
+++ leaders.cpp
@@ -5,7 +5,7 @@ vector<int> leaders(vector<int>& arr) {
     int n = arr.size();
     vector<int> leaders;
     
-    for (int i = n - 1; i >= 0; --i) {
+    for (int i = 0; i < n; ++i) {
         bool leader = true;
         for (int j = i + 1; j < n; ++j) {
             if (arr[j] >= arr[i]) {
@@ -17,4 +17,3 @@ vector<int> leaders(vector<int>& arr) {
     }
     
     return leaders;
-}
