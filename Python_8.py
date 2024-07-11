823ae3bbeee6bf9f34447527c640036efe73fdf0
beep boop <githubbot@vadim.me>
2024-07-11 15:19:49 +0200

    Solution 48 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/d4o756z2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b7758b28-8fde-4371-961c-44716e849 6f5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -14,7 +14,7 @@ def sum_product(numbers: List[int]) -> Tuple[int, int]:
 
 numbers = input("Please enter numbers: ")
 try:
-    result = sum_product(list(map(int, numbers.split(","))))
+    result = sum_product(list(map(int, numbers.split(','))))
 except ValueError:
     print("Invalid input! Please enter comma-separated numbers.")
 else:
