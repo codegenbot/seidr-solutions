734ef0bacdd0f7097bae243c940c85b1e27d5e4e
beep boop <githubbot@vadim.me>
2024-07-11 21:31:10 +0200

    Solution 92 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/nhmldqrz Prompt:    20 |     return "None";       |     ^~~~~~ bc1653d3-538d-40cf-b1b4-c8fab862d9ae.cpp:21:1: error: expected declaration befor e ‘}’ token    21 | }       | ^ In file included from bc1653d3-538d-40cf-b1b4-c8fab862d9ae.cpp:23: bc1653d3-538d-40cf-b1b4-c8fab862d9ae.cpp: In function ‘int main()’: bc1653d3-538d-40cf-b1b4-c8fab862d9ae.cpp:25:13: error: ‘boost’ has not been decl ared    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ bc1653d3-538d-40cf-b1b4-c8fab862d9ae.cpp:25:29: error: ‘string’ was not declared  in this scope    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ bc1653d3-538d-40cf-b1b4-c8fab862d9ae.cpp:25:37: error: ‘compare_one’ was not dec lared in this scope    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -16,10 +16,10 @@ boost::any compare_one(const boost::any &a, const boost::any &b) {
         } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
             return b;
         }
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if (stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))) {
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+        if (stof(boost::any_cast<std::string>(a)) > stof(boost::any_cast<std::string>(b))) {
             return a;
-        } else if (stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b))) {
+        } else if (stof(boost::any_cast<std::string>(a)) < stof(boost::any_cast<std::string>(b))) {
             return b;
         }
     }
