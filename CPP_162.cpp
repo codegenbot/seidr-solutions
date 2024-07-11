b0503d782ab2919449864d5a329382f0482d7146
beep boop <githubbot@vadim.me>
2024-07-11 02:24:42 +0200

    Solution 11 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fcqijk62 Prompt: 0a14fc0e-2fe1-49a8-ba29-77724c32f3ef.cpp:1:2: error: stray ‘`’ in program     1 | ```c++       |  ^ 0a14fc0e-2fe1-49a8-ba29-77724c32f3ef.cpp:1:3: error: stray ‘`’ in program     1 | ```c++       |   ^ 0a14fc0e-2fe1-49a8-ba29-77724c32f3ef.cpp:1:4: error: ‘c’ does not name a type     1 | ```c++       |    ^ 0a14fc0e-2fe1-49a8-ba29-77724c32f3ef.cpp:2:17: error: ‘i’ does not name a type     2 | for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)       |                 ^ 0a14fc0e-2fe1-49a8-ba29-77724c32f3ef.cpp:2:40: error: expected unqualified-id be fore ‘++’ token     2 | for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)       |                                        ^~ In file included from 0a14fc0e-2fe1-49a8-ba29-77724c32f3ef.cpp:5: 0a14fc0e-2fe1-49a8-ba29-77724c32f3ef.cpp: In function ‘int main()’: 0a14fc0e-2fe1-49a8-ba29-77724c32f3ef.cpp:7:13: error: ‘string_to_md5’ was not de clared in this scope     7 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -2,6 +2,5 @@
 int main() {
     for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)
         result.push_back(((unsigned char)md5[i]) * 2 + ((md5[i] * 4) > 9) ? ((unsigned char)md5[i]) * 2 + ((md5[i] * 4) > 9) : "0");
-    std::cerr << "Error: Invalid input. Please try again." << std::endl;
     return -1;
 }
