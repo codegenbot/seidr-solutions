31d44e6db6fa749ad9c1186e569700cfb026f866
beep boop <githubbot@vadim.me>
2024-07-11 19:15:25 +0200

    Solution 9 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ocjywund Prompt: 0a1685d5-3cbc-44df-93c0-cc920867ca4e.cpp: In function ‘std::string spinWords(std ::string)’: 0a1685d5-3cbc-44df-93c0-cc920867ca4e.cpp:13:13: error: ‘reverse’ was not declare d in this scope    13 |             reverse(word.begin(), word.end());       |             ^~~~~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,4 +1,7 @@
 #include <algorithm>
+#include <vector>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -8,12 +11,19 @@ string spinWords(string str) {
     while ((pos = str.find(" ")) != string::npos) {
         string word = str.substr(0, pos);
         if (word.length() >= 5) {
-            vector<char> vec(word.begin(), word.end());
-            reverse(vec.begin(), vec.end());
-            word = string(vec.begin(), vec.end());
+            reverse(word.begin(), word.end());
         }
         result += word + " ";
         str.erase(0, pos + 1);
     }
     result += str;
     return result;
+}
+
+int main() {
+    string str;
+    while (cin >> str) {
+        cout << spinWords(str) << endl;
+    }
+    return 0;
+}
