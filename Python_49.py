282561ed24e163f0f09ebafed776a9e80611504d
beep boop <githubbot@vadim.me>
2024-07-11 14:18:20 +0200

    Solution 14 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3783cyso Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9bfc240f-31f1-4ef7-a2f4-8ec930457 2a7.py", line 9, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9bfc240f-31f1-4ef7-a2f4-8ec930457 2a7.py", line 6, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,6 +1,5 @@
+```
 import math
+
 def modp(n: int, p: int):
-    if p == 0:
-        return "Denominator cannot be zero"
-    else:
-        return int(math.pow(n, 1/p) % p)
+    return pow(n, p-2, p)
