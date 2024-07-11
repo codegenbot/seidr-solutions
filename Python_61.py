dbed82ea4be83291d1fd03417a4cec7ce7917b8f
beep boop <githubbot@vadim.me>
2024-07-11 20:48:43 +0200

    Solution 4 of Python/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8fe3f7wt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c3e41928-2606-4159-9d1e-9b9ae1f53 bf4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_61.py
+++ Python_61.py
@@ -1,11 +1,19 @@
-```
-def correct_bracketing(brackets: str):
+def calculate_expression_value(expression: str):
     stack = []
-    for bracket in brackets:
-        if bracket == "(":
-            stack.append(bracket)
-        elif bracket == ")":
-            if len(stack) == 0:
-                return False
-            stack.pop()
-    return len(stack) == 0
+    total = 0
+    last_operator = "+"
+
+    for char in expression:
+        if char.isdigit():
+            if last_operator == "+":
+                stack.append(int(char))
+            elif last_operator == "-":
+                stack.append(-int(char))
+            elif last_operator == "*":
+                stack.append(stack.pop() * int(char))
+            elif last_operator == "/":
+                stack.append(int(stack.pop()) // int(char))
+        else:
+            last_operator = char
+
+    return sum(stack)
