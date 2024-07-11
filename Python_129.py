9af9445be609f3acd176819128acd4dc2f5d21a1
beep boop <githubbot@vadim.me>
2024-07-11 16:05:06 +0200

    Solution 4 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/t93lgfgj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f2d88ab4-163f-4184-8cc6-011f97766 031.py", line 23, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f2d88ab4-163f-4184-8cc6-011f97766 031.py", line 20, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f2d88ab4-163f-4184-8cc6-011f97766 031.py", line 14, in minPath     m[cur_cell[0]][cur_cell[1]] = float("inf")       ~~~~~~~~^^^ TypeError: 'NoneType' object is not subscriptable

--- Python_129.py
+++ Python_129.py
@@ -1,4 +1,3 @@
-```
 def minPath(grid, k):
     n = len(grid)
     m = [[i * n + j for j in range(n)] for i in range(n)]
@@ -11,7 +10,7 @@ def minPath(grid, k):
                 if m[i][j] < cur_min:
                     cur_min = m[i][j]
                     cur_cell = (i, j)
-        res.append(cur_min)
         if cur_cell is not None:
+            res.append(cur_min)
             m[cur_cell[0]][cur_cell[1]] = float("inf")
     return res
