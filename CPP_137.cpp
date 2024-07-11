6af01127635f8f2e0ffd5d376829eb2cb7308736
beep boop <githubbot@vadim.me>
2024-07-11 11:55:24 +0200

    Solution 45 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/3bk0svvl Prompt: d0ec96ae-ac97-4ac1-8785-b006b5ec659c.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -21,7 +21,7 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         }
         else {
-            return boost::any("None");
+            return boost::any(string("None"));
         }
     }
     else if (a.type() == typeid(int) && b.type() == typeid(string)) {
@@ -34,7 +34,7 @@ boost::any compare_one(boost::any a, boost::any b) {
             return a;
         }
         else {
-            return boost::any("None");
+            return boost::any(string("None"));
         }
     }
     else if (a.type() == typeid(string) && b.type() == typeid(int)) {
@@ -47,7 +47,7 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         }
         else {
-            return boost::any("None");
+            return boost::any(string("None"));
         }
     }
     else if (a.type() == typeid(float) && b.type() == typeid(int)) {
@@ -60,7 +60,7 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         }
         else {
-            return boost::any("None");
+            return boost::any(string("None"));
         }
     }
     else if (a.type() == typeid(int) && b.type() == typeid(float)) {
@@ -73,8 +73,7 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         }
         else {
-            return boost::any("None");
+            return boost::any(string("None"));
         }
     }
     return boost::any();
-}
