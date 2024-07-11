ddac2c43d4020bc10c3aa396688fe75f95f29d31
beep boop <githubbot@vadim.me>
2024-07-11 10:44:44 +0200

    Solution 54 of CPP/98, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2z46e9n8 Prompt: a0753ac3-f1a3-460c-a52a-816ece797a3f.cpp:4:17: error: ‘string’ was not declared in this scope     4 | int count_upper(string s) {       |                 ^~~~~~ a0753ac3-f1a3-460c-a52a-816ece797a3f.cpp:4:17: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from a0753ac3-f1a3-460c-a52a-816ece797a3f.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from a0753ac3-f1a3-460c-a52a-816ece797a3f.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from a0753ac3-f1a3-460c-a52a-816ece797a3f.cpp:15: a0753ac3-f1a3-460c-a52a-816ece797a3f.cpp: In function ‘int main()’: a0753ac3-f1a3-460c-a52a-816ece797a3f.cpp:17:24: error: ‘count_upper’ cannot be u sed as a function    17 |     assert (count_upper("EEEE") == 2);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_98.cpp
+++ CPP_98.cpp
@@ -1,9 +1,10 @@
 #include <string>
+#include <cctype>
 
 int count_upper(std::string s) {
     int count = 0;
     for (int i = 0; i < s.length(); i++) {
-        if ((i % 2 == 0 && isupper(s[i])) && 
+        if ((i % 2 == 0 && isupper(s[i])) &&
             (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')) {
             count++;
         }
