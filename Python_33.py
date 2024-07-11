d9e6bac521d0857c367101d1f1cba3c9dee17eab
beep boop <githubbot@vadim.me>
2024-07-11 19:02:23 +0200

    Solution 58 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h0l13eex Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bdbbe5e3-5c87-4b37-9715-6e82fff47 bb1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,2 +1,5 @@
 def sort_third(lst):
-    return [x[2] for x in sorted(lst)]
+    if len(lst) % 3 == 0:
+        return sorted([i for r in [list(lst[i:i+3]) for i in range(0, len(lst), 3)] for i in r])
+    else:
+        return "Input list should be a multiple of three"
