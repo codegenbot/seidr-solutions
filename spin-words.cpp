85646ed0b6ef78be56a8fcde67a7e8569d4a796c
beep boop <githubbot@vadim.me>
2024-07-11 19:20:02 +0200

    Solution 12 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ocjywund Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from eb6e551b-7670-42bd-a78a-8a8e11e9ab21.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<const char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ eb6e551b-7670-42bd-a78a-8a8e11e9ab21.cpp:6:21:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,4 +1,7 @@
 ```cpp
+#include <algorithm>
+using namespace std;
+
 string spinWords(string str) {
     string result = "";
     int i = 0;
@@ -14,10 +17,18 @@ string spinWords(string str) {
         int pos = str.find(" ", i);
         string word = str.substr(i, pos - i);
         if (word.length() >= 5)
-            result += string(word.rbegin(), word.rend()) + " ";
+            result += std::string(word).reverse() + " ";
         else
             result += word + " ";
         i = pos + 1;
     }
     return result.substr(0, result.length() - 1);
+}
+
+int main() {
+    string str;
+    cout << "Enter a string of words: ";
+    getline(cin, str);
+    cout << "Spun result: " << spinWords(str) << endl;
+    return 0;
 }
