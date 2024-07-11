542c57f6fc8d81da881d42a7611252224712dd18
beep boop <githubbot@vadim.me>
2024-07-11 12:03:45 +0200

    Solution 56 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/3bk0svvl Prompt:       | ^~~~ In file included from 1d2e9af6-384b-47dc-8259-321ac91553d7.cpp:9: 1d2e9af6-384b-47dc-8259-321ac91553d7.cpp: In function ‘int main()’: 1d2e9af6-384b-47dc-8259-321ac91553d7.cpp:11:13: error: ‘boost’ has not been decl ared    11 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 1d2e9af6-384b-47dc-8259-321ac91553d7.cpp:11:29: error: ‘string’ was not declared  in this scope    11 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 1d2e9af6-384b-47dc-8259-321ac91553d7.cpp:11:37: error: ‘compare_one’ was not dec lared in this scope    11 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~ 1d2e9af6-384b-47dc-8259-321ac91553d7.cpp:11:5: error: ‘__assert_fail’ was not de clared in this scope    11 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |     ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,24 +1,10 @@
 #include <boost/any.hpp>
 #include <string>
 
-int main() {
-    std::string user_input;
-    boost::any input;
-
-    // Ask the user for some data
-    std::cout << "Please enter your favorite color: ";
-    std::cin >> user_input;
-
-    // Check if the input is either 'red' or 'green'
-    if (user_input == "red" || user_input == "green") {
-        // Convert the string to a boost::any object
-        input = boost::any(user_input);
-
-        // Print out the input, whether it's 'red', 'green', or something else!
-        std::cout << "You entered: " << boost::any_cast<std::string>(input) << std::endl;
+std::any compare_one(std::string str1, int num1) {
+    if (num1 == 1) {
+        return std::any(str1);
     } else {
-        std::cout << "Sorry, that's not a valid color." << std::endl;
+        return std::any("None");
     }
-
-    return 0;
 }
