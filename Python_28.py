9c8f2e430c0fd6bb0c03dd95b9c8cef9346ecb47
beep boop <githubbot@vadim.me>
2024-07-11 17:06:36 +0200

    Solution 86 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/dszeq8nu Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/91b194b5-4489-4679-862a-643bccbb9 391.py", line 10, in <module>     check(concatenate)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/91b194b5-4489-4679-862a-643bccbb9 391.py", line 7, in check     assert concatenate(["x", "y", "z", "w", "k"]) == "xyzwk"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/91b194b5-4489-4679-862a-643bccbb9 391.py", line 3, in concatenate     return "".join(sorted([s.strip() for s in strings.split(",")]))                                               ^^^^^^^^^^^^^ AttributeError: 'list' object has no attribute 'split'

--- Python_28.py
+++ Python_28.py
@@ -1,5 +1,6 @@
+```
 def concatenate(strings: str) -> str:
     try:
-        return "".join(sorted([s.strip() for s in strings.split(",")]))
+        return "".join(sorted([s.strip() for s in [s.split(",") for s in strings.split(",")]]))
     except ValueError:
         return "Error: Invalid input"
