43b00bc772157e90991d116ddae4611824cddb0d
beep boop <githubbot@vadim.me>
2024-07-11 20:54:37 +0200

    Solution 71 of CPP/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/si4cbgr8 Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: e2effd76-2a49-4038-bafc-1e641c141841.cpp:13:53:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_53.cpp
+++ CPP_53.cpp
@@ -1,23 +1,27 @@
+```cpp
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
-
-int sum(int a, int int b) {
-    return a + b;
-}
 
 int main() {
     srand(time(nullptr)); 
     int randomNum = rand() % 1000; 
 
-    cout << "Random Number: " << to_string(randomNum) << endl; 
+    std::stringstream ss;
+    ss << "Random Number: " << randomNum << endl;
+    cout << ss.str() << endl; 
 
     int num1, num2;
     cout << "Enter two numbers: ";
     cin >> num1 >> num2;
 
     cout << "Sum of the numbers: " << sum(num1, num2) << endl;
+}
+
+int sum(int a, int b) {
+    return a + b;
 }
