82499bc0b2060b2c35adcc835238633556a32b6c
beep boop <githubbot@vadim.me>
2024-07-11 11:45:43 +0200

    Solution 25 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/l6tw0a3u Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 484cd035-026c-4430-9483-a8e309fed316.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 484cd035-026c-4430-9483-a8e309fed316.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 484cd035-026c-4430-9483-a8e309fed316.cpp:26: 484cd035-026c-4430-9483-a8e309fed316.cpp: In function ‘int main()’: 484cd035-026c-4430-9483-a8e309fed316.cpp:36:20: error: ‘sorted_list_sum’ was not  declared in this scope    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 484cd035-026c-4430-9483-a8e309fed316.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,3 +1,34 @@
-bool issame(string str1, string str2) {
-    return str1 == str2;
+bool issame(string s1, string s2) {
+    int count = 0;
+    for (int i = 0; i < min(s1.length(), s2.length()); i++) {
+        if (s1[i] == s2[i]) {
+            count++;
+        } else {
+            break;
+        }
+    }
+    return count > 0;
+}
+
+vector<string> sorted_list_sum(vector<string> lst) {
+    vector<string> result;
+
+    // Remove strings with odd lengths from the list
+    for (const auto& str : lst) {
+        if (str.length() % 2 == 0) {
+            result.push_back(str);
+        }
+    }
+
+    // Sort the resulting list by length and then alphabetically
+    sort(result.begin(), result.end(),
+         [](const string& a, const string& b) {
+             if (a.length() != b.length()) {
+                 return a.length() < b.length();
+             } else {
+                 return issame(a, b);
+             }
+         });
+
+    return result;
 }
