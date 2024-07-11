381ea618cd8785ff338eefaa49b68b330100ff1f
beep boop <githubbot@vadim.me>
2024-07-11 20:55:44 +0200

    Solution 8 of Python/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8fe3f7wt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c3e41928-2606-4159-9d1e-9b9ae1f53 bf4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_61.py
+++ Python_61.py
@@ -1,14 +1,11 @@
-def find_max_sum(lst):
-    max_sum = float("-inf")
-    current_sum = 0
-
-    for num in lst:
-        current_sum += num
-
-        if current_sum > max_sum:
-            max_sum = current_sum
-
-        if current_sum < 0:
-            current_sum = 0
-
-    return max_sum
+```
+def correct_bracketing(brackets: str):
+    stack = []
+    for bracket in brackets:
+        if bracket == "(":
+            stack.append(bracket)
+        elif bracket == ")":
+            if len(stack) == 0:
+                return False
+            stack.pop()
+    return len(stack) == 0
