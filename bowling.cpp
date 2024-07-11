4f06bf92b6a6d9c407644fc003dee9ee0a00c780
beep boop <githubbot@vadim.me>
2024-07-11 18:12:19 +0200

    Solution 88 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/dlv2ukls Prompt: 3f104130-b079-426d-ae6b-edcdfc78527c.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int bowlingScore(std::string s) {       |                       ^~~~~~ 3f104130-b079-426d-ae6b-edcdfc78527c.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int bowlingScore(std::string s) {

--- bowling.cpp
+++ bowling.cpp
@@ -2,38 +2,20 @@
 
 int bowlingScore(std::string s) {
     int score = 0;
+    int roll = 0;
     for(int i = 0; i < s.size(); i++) {
         if(s[i] == '/') {
-            if(i > 1 && s[i-2] != 'X') {
-                score += (10 - (s[i-1] - '0'));
+            if(roll < 2) {
+                score += 10 + (10 - '0');
+            } else {
+                score += 10 * 10;
             }
-            score += 10;
+            roll = 0;
         } else if('0' <= s[i] && s[i] <= '9') {
-            int val = s[i] - '0';
-            if(i < s.size() - 1) {
-                if(s[i+1] == '/') {
-                    if(i > 1 && s[i-2] != 'X') {
-                        score += (10 - (s[i-1] - '0'));
-                    }
-                    score += 10;
-                    i++;
-                    continue;
-                } else if(s[i+1] == 'X') {
-                    score += val + 10;
-                    i++;
-                    continue;
-                }
-            }
-            score += val;
+            roll = roll * 10 + (s[i] - '0');
         } else if(s[i] == 'X') {
             score += 10;
-            for(int j = i+1; j < s.size(); j++) {
-                if(j < s.size() - 1 && s[j+1] == '/') {
-                    continue;
-                }
-                score += (s[j] == 'X') ? 10 : s[j] - '0';
-            }
+            roll = 0;
         }
     }
     return score;
-}
