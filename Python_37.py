6b79198cd9243ebc878a955cf0565ea5bf5bba1a
beep boop <githubbot@vadim.me>
2024-07-11 09:23:32 +0200

    Solution 32 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v70dnp3u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a62ca1f9-fd8c-453e-a7b9-e373e7ace 8c3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_37.py
+++ Python_37.py
@@ -1,7 +1,6 @@
-numbers = [int(i) for i in input("Enter numbers separated by space: ").split()]
-print(sort_even(numbers))
+```
 def sort_even(l):
     evens = sorted([x for x in l if x % 2 == 0])
-    neg_odds = sorted([-x for x in l if x < 0 and x % 2 != 0])
-    odds_pos = sorted([x for x in l if x > 0 and x % 2 != 0])
+    neg_odds = sorted([-x for x in l if x < 0 and x % 2 != 0], key=lambda x: -abs(x))
+    odds_pos = sorted([x for x in l if x > 0 and x % 2 != 0], key=lambda x: abs(x))
     return evens + neg_odds + odds_pos
