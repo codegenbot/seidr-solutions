fe7502028c25fc6b322c20190f092c09156f88f1
beep boop <githubbot@vadim.me>
2024-07-11 14:25:52 +0200

    Solution 48 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/4k12p6tg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dfeac008-0dc7-475c-889a-20c005685 ab9.py", line 6, in <module>     check(separate_paren_groups)           ^^^^^^^^^^^^^^^^^^^^^ NameError: name 'separate_paren_groups' is not defined

--- Python_1.py
+++ Python_1.py
@@ -1,4 +1,6 @@
-def get_separated_parentheses(text):
-    return [text[i:i+2] for i in range(0, len(text), 2)]
+```
+def separate_paren_groups(input_str):
+    return input_str.split(")(")
 
-result = get_separated_parentheses(input())
+input_str = input("Enter a string: ")
+print(separate_paren_groups(input_str))
