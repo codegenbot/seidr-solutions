9ac1bb9a97ba42f6da28f77f8271d8d582d45dca
beep boop <githubbot@vadim.me>
2024-07-11 18:51:15 +0200

    Solution 99 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/juryj07u Prompt: Program did not receive expected input

--- Python_151.py
+++ Python_151.py
@@ -1,14 +1,12 @@
 def check(func):
     print(func())
 
-
 def double_the_difference():
     sep = input("Enter the separator: ")
     lst = list(map(int, input().split(sep)))
     difference = max(lst) - min(lst)
-    double_diff = str(difference * 2)
+    double_diff = str(difference * 2) 
     return double_diff
-
 
 if __name__ == "__main__":
     check(double_the_difference())
