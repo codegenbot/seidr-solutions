0802e8e6dcab75dd6fe333f50fd5c25eab1ec0d8
beep boop <githubbot@vadim.me>
2024-07-11 11:26:04 +0200

    Solution 6 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/l6tw0a3u Prompt:    25 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 6221db5c-a860-445b-8644-7a9cb532b350.cpp:25:44: error: ‘b’ was not declared in t his scope    25 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6221db5c-a860-445b-8644-7a9cb532b350.cpp:25:45: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 6221db5c-a860-445b-8644-7a9cb532b350.cpp:24: 6221db5c-a860-445b-8644-7a9cb532b350.cpp: In function ‘int main()’: 6221db5c-a860-445b-8644-7a9cb532b350.cpp:34:20: error: ‘sorted_list_sum’ was not  declared in this scope    34 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 6221db5c-a860-445b-8644-7a9cb532b350.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,29 +1,30 @@
+#include <vector>
+#include <algorithm>
+#include <string>
+
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); ++i)
-        if (a[i] != b[i])
+    for (int i = 0; i < a.size() && i < b.size(); ++i) {
+        if (!a[i].compare(b[i]))
             return false;
-    return true;
+    }
+    return a.size() == b.size();
 }
 
 vector<string> sorted_list_sum(vector<string> lst) {
     vector<string> result;
 
-    // Remove strings with odd lengths from the list
     for (const auto& str : lst) {
         if (str.length() % 2 == 0)
             result.push_back(str);
     }
 
-    // Sort the resulted list by length and then alphabetically
-    std::sort(result.begin(), result.end(),
-              [](const string& a, const string& b) {
-                  if (a.length() != b.length())
-                      return a.length() - b.length();
-                  else
-                      return a < b;
-              });
+    sort(result.begin(), result.end(),
+         [](const string& a, const string& b) {
+             if (a.length() != b.length())
+                 return a.length() - b.length();
+             else
+                 return a < b;
+         });
 
     return result;
 }
