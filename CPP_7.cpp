32ba70c315c28f156a39fb49c833f096a2b9516e
beep boop <githubbot@vadim.me>
2024-07-11 03:42:09 +0200

    Solution 43 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8mh3uiun Prompt:    14 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 728910ab-5a62-4c8a-a167-afddd79a1536.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 728910ab-5a62-4c8a-a167-afddd79a1536.cpp:14:45: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 728910ab-5a62-4c8a-a167-afddd79a1536.cpp:13: 728910ab-5a62-4c8a-a167-afddd79a1536.cpp: In function ‘int main()’: 728910ab-5a62-4c8a-a167-afddd79a1536.cpp:23:20: error: ‘filter_by_substring’ was  not declared in this scope    23 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 728910ab-5a62-4c8a-a167-afddd79a1536.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,40 +1,42 @@
 ```cpp
-#include <iostream>
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<string> a,vector<string> b){
-    if(a.size() != b.size())
+bool issame(vector<string> a,vector<string>b){
+    if (a.size() != b.size())
         return false;
-
+    
     for (int i = 0; i < a.size(); i++) {
-        if (find(b.begin(), b.end(), a[i]) == b.end())
+        vector<string>::iterator it = find(b.begin(), b.end(), a[i]);
+        if (it == b.end())
             return false;
     }
-
+    
     return true;
 }
 
-int main(){
+int main() {
     int n;
-    std::cin >> n;
-    vector<string> a,b;
+    cin >> n;
 
-    for(int i=0;i<n;i++){
-        string temp;
-        cin>>temp;
-        if(i<n/2) a.push_back(temp);
-        else b.push_back(temp);
+    vector<vector<string>> words(2);
+    for(int i = 0; i < 2; ++i){
+        int m;
+        cin >> m;
+        words[i].resize(m);
+        for (int j = 0; j < m; ++j) {
+            string word;
+            cin >> word;
+            words[i].push_back(word);
+        }
     }
 
-    // Check if lists are same
-    bool flag = issame(a, b);
-
-    // Print the result
-    if(flag) 
-        std::cout << "Lists are same.\n";
-    else    
-        std::cout << "Lists are not same.\n";
+    bool same = issame(words[0],words[1]);
+    
+    if(same)
+        cout << "Same\n";
+    else
+        cout << "Not Same\n";
 
     return 0;
 }
