5841d7e584e0f82177c8426ff81bb838d945164d
beep boop <githubbot@vadim.me>
2024-07-11 15:38:17 +0200

    Solution 35 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/12a7amke Prompt:       |                    ^~~ d0978065-8300-4f76-99ca-02635efda505.cpp:23:27: error: ‘vector’ was not declared  in this scope    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d0978065-8300-4f76-99ca-02635efda505.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d0978065-8300-4f76-99ca-02635efda505.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d0978065-8300-4f76-99ca-02635efda505.cpp:22: d0978065-8300-4f76-99ca-02635efda505.cpp: In function ‘int main()’: d0978065-8300-4f76-99ca-02635efda505.cpp:32:20: error: ‘pluck’ was not declared in this scope    32 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ d0978065-8300-4f76-99ca-02635efda505.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,16 +1,11 @@
 #include <vector>
 
 bool issame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
-}
-
-int main() {
-    assert(issame(pluck({7, 9, 7, 1}) , {}));
-    return 0;
 }
 
 vector<int> pluck(vector<int> arr) {
@@ -30,4 +25,9 @@ vector<int> pluck(vector<int> arr) {
     result.push_back(minIndex);
 
     return result;
+}
+
+int main() {
+    assert(issame(pluck({7, 9, 7, 1}) , {}));
+    return 0;
 }
