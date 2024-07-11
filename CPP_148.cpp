a69f275aa5de9cb37f46da2f901a8d308193ad24
beep boop <githubbot@vadim.me>
2024-07-11 11:23:55 +0200

    Solution 4 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hn4pgr1r Prompt:       |                              ^~~~~~ 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:32: 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp: In function ‘int main()’: 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:42:20: error: ‘bf’ was not declared in this scope    42 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,41 +1,35 @@
-Here is the completed code:
-
+```
 vector<string> bf(string planet1, string planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
-    vector<string> result;
-
     int index1 = -1;
     int index2 = -1;
 
     for (int i = 0; i < planets.size(); i++) {
-        if (planets[i] == planet1) {
+        if (planet1 == planets[i]) {
             index1 = i;
-        } else if (planets[i] == planet2) {
+        }
+        else if (planet2 == planets[i]) {
             index2 = i;
         }
     }
 
-    if (index1 == -1 || index2 == -1)
-        return vector<string>();
-
-    for (int i = 0; i < planets.size(); i++) {
-        if (i >= index1 && i <= index2)
-            result.push_back(planets[i]);
+    if (index1 == -1 || index2 == -1) {
+        return {};
     }
 
-    sort(result.begin(), result.end(), [](const string& a, const string& b) {
-        int posA = -1;
-        int posB = -1;
+    int start, end;
 
-        for (int i = 0; i < planets.size(); i++) {
-            if (planets[i] == a)
-                posA = i;
-            else if (planets[i] == b)
-                posB = i;
+    if(index1 < index2)
+        swap(start, end);
+    else
+        std::tie(start, end) = {index1, index2};
+
+    vector<string> result;
+    for (int i = 0; i < planets.size(); i++) {
+        if(i >= start && i <= end) {
+            result.push_back(planets[i]);
         }
-
-        return posA < posB;
-    });
+    }
 
     return result;
 }
