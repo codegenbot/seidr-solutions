2bb1dce691ddfa460723673c174759707728090f
beep boop <githubbot@vadim.me>
2024-07-11 02:54:36 +0200

    Solution 56 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/00b9pw3v Prompt: Complete the following code given the task description and function signature.

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,16 +1,27 @@
-int count_nums(vector<int> v) {
+int count_nums(vector<int> nums) {
     int count = 0;
-    for (int num : v) {
-        if (num < 0) {
-            num = -num; // make it positive to calculate sum of digits
+    for (int num : nums) {
+        if (num >= 0) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            if (sum > 0)
+                count++;
+        } else {
+            int sum = 0, sign = 1;
+            do {
+                if (num < 0) {
+                    sign = -1;
+                    num = -num;
+                }
+                sum += num % 10;
+                num /= 10;
+            } while (num > 0);
+            if (sign * sum > 0)
+                count++;
         }
-        int sum = 0;
-        while (num > 0) {
-            sum += num % 10;
-            num /= 10;
-        }
-        if (sum > 0)
-            count++;
     }
     return count;
 }
