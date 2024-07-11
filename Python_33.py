864c10cadd32f57cfd367a9efc10e5e43e4f5829
beep boop <githubbot@vadim.me>
2024-07-11 10:18:38 +0200

    Solution 81 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt: [2, 4, 7, 10, False, True] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc46fcdf-c2d7-4619-b640-48f96df16 4d1.py", line 13, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc46fcdf-c2d7-4619-b640-48f96df16 4d1.py", line 8, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,6 +1,3 @@
-```
 def sort_third(l: list):
-    l.sort(key=lambda x: '000' if not isinstance(x, int) else str(x).zfill(3))
+    l.sort(key=lambda x: '000' if isinstance(x, bool) else ('0000' if not isinstance(x, int) else str(x).zfill(3)))
     return l
-
-print(sort_third([123, "abc", 45, None, "def"]))
