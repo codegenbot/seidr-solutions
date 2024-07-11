7ab068f573b68717e086db56c49210b8ace285d7
beep boop <githubbot@vadim.me>
2024-07-11 06:20:20 +0200

    Solution 26 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/gbmvfmha Prompt:       |               ^~~~~ 077b16f0-9666-42e3-9423-b3145ab9411a.cpp:12:5: error: ‘string’ does not name a t ype    12 |     string binary_avg = "";       |     ^~~~~~ 077b16f0-9666-42e3-9423-b3145ab9411a.cpp:13:5: error: expected unqualified-id be fore ‘while’    13 |     while (avg > 0) {       |     ^~~~~ 077b16f0-9666-42e3-9423-b3145ab9411a.cpp:18:5: error: expected unqualified-id be fore ‘return’    18 |     return binary_avg;       |     ^~~~~~ 077b16f0-9666-42e3-9423-b3145ab9411a.cpp:19:1: error: expected declaration befor e ‘}’ token    19 | }       | ^ In file included from 077b16f0-9666-42e3-9423-b3145ab9411a.cpp:21: 077b16f0-9666-42e3-9423-b3145ab9411a.cpp: In function ‘int main()’: 077b16f0-9666-42e3-9423-b3145ab9411a.cpp:23:13: error: ‘rounded_avg’ was not dec lared in this scope    23 |     assert (rounded_avg(5, 5) == "101");       |             ^~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,10 +1,5 @@
 #include <string>
 
-int main() {
-    assert(rounded_avg(5, 5) == "101");
-    return 0;
-}
-
 std::string rounded_avg(int n, int m) {
     if (n > m) {
         return "-1";
@@ -25,3 +20,8 @@ std::string rounded_avg(int n, int m) {
     
     return binary_avg;
 }
+
+int main() {
+    assert(rounded_avg(5, 5) == "101");
+    return 0;
+}  
