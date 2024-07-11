51c68614f46f3f7e1bbdc63e52848111783b9949
beep boop <githubbot@vadim.me>
2024-07-11 12:05:46 +0200

    Solution 14 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hv4j11wl Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,34 +1,44 @@
-map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                             {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                             {"eight", 8}, {"nine", 9}};
+#include<stdio.h>
+#include<string>
+#include<map>
+using namespace std;
 
-string sort_numbers(string numbers) {
-    vector<string> numVec;
-    string temp;
+string sort_numbers(string numbers){
+    map<string,int> numMap = {
+        {"zero", 0},
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9}
+    };
 
-    for (int i = 0; i < numbers.length(); i++) {
-        if (numbers[i] == ' ') {
-            numVec.push_back(temp);
-            temp = "";
-        } else {
-            temp += numbers[i];
+    string sortedNumbers = "";
+    vector<string> nums;
+    for(int i = 0; i < numbers.length(); i++){
+        if(numbers[i] == ' ')
+            continue;
+        for(int j = i + 1; j <= numbers.length(); j++){
+            if(numbers[j] != ' ' && numbers[j] != '\0')
+                break;
+            nums.push_back(numbers.substr(i, j - i));
+            i = j - 1;
+            break;
         }
     }
-    numVec.push_back(temp);
 
-    sort(numVec.begin(), numVec.end());
+    sort(nums.begin(), nums.end(), 
+         [&numMap](string a, string b) { return numMap[a] < numMap[b];});
 
-    string result;
-    for (int i = 0; i < numVec.size(); i++) {
-        for (auto it = numMap.begin(); it != numMap.end(); ++it) {
-            if (numVec[i] == it->first) {
-                if (i > 0) {
-                    result += " ";
-                }
-                result += it->first;
-            }
-        }
+    for(int i = 0; i < nums.size(); i++){
+        sortedNumbers += nums[i];
+        if(i != nums.size() - 1)
+            sortedNumbers += ' ';
     }
 
-    return result;
+    return sortedNumbers;
 }
