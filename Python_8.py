6748421e4db6eb982e978b3326ce7e23f4665a0d
beep boop <githubbot@vadim.me>
2024-07-11 20:54:42 +0200

    Solution 29 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/5ug6le41 Prompt: Program did not receive expected input

--- Python_8.py
+++ Python_8.py
@@ -1,12 +1,14 @@
 from typing import List, Tuple
 
+
 def sum_product(numbers: List[int]) -> Tuple[int, int]:
     return (
         sum(numbers),
         1 if not numbers else 1 if 0 in numbers else eval("*".join(map(str, numbers))),
     )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     numbers = list(map(int, input().split()))
     result = sum_product(numbers)
     print(result)
