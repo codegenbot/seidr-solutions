a6ab42d1bb9e13b36bcb88578b75a9d9805edb32
beep boop <githubbot@vadim.me>
2024-07-11 23:19:08 +0200

    Solution 24 of Python/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/gyunx5r6 Prompt: Complete the following code given the task description and function signature.

--- Python_74.py
+++ Python_74.py
@@ -1,7 +1,7 @@
 ```
 def total_match(lst1, lst2):
-    total_chars_lst1 = sum(len(i) for i in lst1)
-    total_chars_lst2 = sum(len(i) for i in lst2)
+    total_chars_lst1 = sum(len(s) for s in lst1)
+    total_chars_lst2 = sum(len(s) for s in lst2)
     
     if total_chars_lst1 < total_chars_lst2:
         return lst1
