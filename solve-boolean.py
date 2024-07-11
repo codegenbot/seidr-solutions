9dc948aeed4464d825b2ea50e9256e1329337519
beep boop <githubbot@vadim.me>
2024-07-11 20:32:16 +0200

    Solution 95 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/v4ghdt3d Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4a2e35a0-3470-4d87-9a57-8f426e476 f11.py", line 1     ```     ^ SyntaxError: invalid syntax

--- solve-boolean.py
+++ solve-boolean.py
@@ -14,12 +14,3 @@ def solve(expression):
         result1 = solve(left)
         result2 = solve(right)
         return result1 or result2
-
-expression = input()
-while 'T' in expression or 'F' in expression:
-    if 'T' in expression:
-        expression = expression.replace('T', '')
-    else:
-        expression = expression.replace('F', '')
-
-print("Result:", "True" if solve(expression) else "False")
