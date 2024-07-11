3c4987f2cf424b558c7c005cdf2b0e28cc1660cf
beep boop <githubbot@vadim.me>
2024-07-11 13:36:52 +0200

    Solution 20 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c4exytmv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/93d6cd37-de8e-41c5-9024-aa8e0b58a 941.py", line 18, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/93d6cd37-de8e-41c5-9024-aa8e0b58a 941.py", line 15, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,20 +1,6 @@
+```
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
         return string
-
-    left = list(string)
-    right = ""
-
-    for char in reversed(string):
-        right += char
-
-    i, j = 0, len(right) - 1
-    while i < len(left):
-        if left[i] != right[j]:
-            left.insert(0, right[j])
-            j -= 1
-        else:
-            i += 1
-            j -= 1
-
-    return "".join(left)
+    new_string = string + string[::-1]
+    return new_string
