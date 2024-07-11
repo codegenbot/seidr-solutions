833e19f17c928b62a64b5be889b793461a9dee50
beep boop <githubbot@vadim.me>
2024-07-11 22:18:46 +0200

    Solution 66 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/u5pl03lv Prompt:    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3d770bde-7034-4168-b6b9-85c2a0a04571.cpp:16:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 3d770bde-7034-4168-b6b9-85c2a0a04571.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 3d770bde-7034-4168-b6b9-85c2a0a04571.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 3d770bde-7034-4168-b6b9-85c2a0a04571.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 3d770bde-7034-4168-b6b9-85c2a0a04571.cpp:15: 3d770bde-7034-4168-b6b9-85c2a0a04571.cpp: In function ‘int main()’: 3d770bde-7034-4168-b6b9-85c2a0a04571.cpp:25:20: error: ‘parse_music’ was not dec lared in this scope    25 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -2,26 +2,18 @@
 #include <string>
 #include <cassert>
 
-std::vector<int> parse_music(std::string music_string) {
+std::vector<int> parse_music(std::string music) {
     std::vector<int> result;
-    std::string delimiter = "| ";
-    size_t pos = 0;
-    while ((pos = music_string.find(delimiter)) != std::string::npos) {
-        std::string token = music_string.substr(0, pos);
-        if (token == "o")
-            result.push_back(2);
-        else if (token == ".")
-            result.push_back(1);
-        music_string.erase(0, pos + delimiter.length());
+    int count = 0;
+    for (char c : music) {
+        if (c == ' ')
+            continue;
+        else if (c == 'o')
+            count++;
+        else if (c == '|') {
+            result.push_back(count);
+            count = 0;
+        }
     }
     return result;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-void main() {
-    assert(issame(parse_music("o| .| o| .| o o| o o|"),
-                   {2, 1, 2, 1, 2, 2, 2, 2}));
 }
