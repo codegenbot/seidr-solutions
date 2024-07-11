2353de582b2ec1157bc5543d93d0859412e97240
beep boop <githubbot@vadim.me>
2024-07-11 14:04:46 +0200

    Solution 66 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/dlv2ukls Prompt: 087121b9-50f8-4962-b77e-93e4fc41fd48.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int bowlingScore(std::string s) {       |                       ^~~~~~ 087121b9-50f8-4962-b77e-93e4fc41fd48.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int bowlingScore(std::string s) {

--- bowling.cpp
+++ bowling.cpp
@@ -2,20 +2,46 @@
 
 int bowlingScore(std::string s) {
     int score = 0;
-    int roll = 0;
     for(int i = 0; i < s.size(); i++) {
         if(s[i] == '/') {
-            if(roll < 2) {
-                score += 10 + (roll > 0 ? roll * 10 : 10);
-            } else {
-                score += 10 * 10;
-            }
-            roll = 0;
+            score += (roll1 + roll2) * 10;
+            roll1 = 0;
+            roll2 = 0;
         } else if('0' <= s[i] && s[i] <= '9') {
-            roll = roll * 10 + (s[i] - '0');
+            int roll = (s[i] - '0');
+            if(roll1 == 0) {
+                roll1 = roll;
+            } else {
+                roll2 = roll;
+                score += roll1 * 10 + (10 > roll2 ? roll2 : 10);
+                roll1 = 0;
+                roll2 = 0;
+            }
         } else if(s[i] == 'X') {
             score += 10;
-            roll = 0;
+            if(roll1 != 0) {
+                score += roll1 * 10 + (10 > roll2 ? roll2 : 10);
+                roll1 = 0;
+                roll2 = 0;
+            }
+        } else if(s[i] == 'S') {
+            int roll = 5;
+            if(roll1 != 0) {
+                score += roll1 * 10 + (10 > roll2 ? roll2 : 10);
+                roll1 = 0;
+                roll2 = 0;
+            }
+            score += roll * 10;
+        }
+    }
+    if(roll1 != 0 || roll2 != 0) {
+        if(roll1 == 0) {
+            score += roll2 * 10;
+        } else if(roll2 == 0) {
+            score += roll1 * 10 + (10 > roll2 ? roll2 : 10);
+        } else {
+            score += roll1 * 10 + roll2;
         }
     }
     return score;
+}
