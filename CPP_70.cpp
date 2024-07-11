79902751b5e997fbc0eb530867a6f089fcbc217c
beep boop <githubbot@vadim.me>
2024-07-11 14:26:16 +0200

    Solution 6 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/3tk43at3 Prompt: Complete the following code given the task description and function signature.

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,16 +1,16 @@
-#include <algorithm>
-using namespace std;
-
 vector<int> strange_sort_vector(vector<int> lst) {
     vector<int> result;
     if (lst.empty()) return result;
 
-    sort(lst.begin(), lst.end());
+    std::sort(lst.begin(), lst.end());
+
     while (!lst.empty()) {
-        result.push_back(*lst.begin());
-        lst.erase(lst.begin());
+        result.push_back(lst.back());
+        lst.pop_back();
+
         if (!lst.empty())
-            sort(lst.begin(), lst.end());
+            std::sort(lst.begin(), lst.end());
     }
+
     return result;
 }
