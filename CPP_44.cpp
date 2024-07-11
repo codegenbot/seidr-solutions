15a63d981a9ef7faebf35ed8ad54e8c017f4a8bf
beep boop <githubbot@vadim.me>
2024-07-11 06:42:16 +0200

    Solution 12 of CPP/44, ratio 0.8571428571428571 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8p92kmfd Prompt: Program failed

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -1,15 +1,19 @@
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
 string change_base(int x, int base) {
+    if(base > 10) {
+        return "Base must be less than or equal to 10";
+    }
     string res = "";
     while (x > 0) {
-        int temp = x % base;
-        if (temp > 9)
-            res.insert(0, 1, (char)(temp + 55));
+        int rem = x % base;
+        if(rem >= 10)
+            res.insert(0, 1, 'A' + rem - 10);
         else
-            res.insert(0, to_string(temp));
+            res.insert(0, to_string(rem));
         x /= base;
     }
     return res;
