ebcc647692a3d808a2e2ff7ff0dec91591eba616
beep boop <githubbot@vadim.me>
2024-07-11 22:06:31 +0200

    Solution 28 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/y7d6gb8x Prompt:    14 |             ++guess_count[guess[i] - 'A'];       |               ^~~~~~~~~~~ 43a4d6a6-9a23-48b9-9f9c-a3470940ef42.cpp:19:22: error: ‘code_count’ was not decl ared in this scope    19 |         white += min(code_count[i], guess_count[i]);       |                      ^~~~~~~~~~ 43a4d6a6-9a23-48b9-9f9c-a3470940ef42.cpp:19:37: error: ‘guess_count’ was not dec lared in this scope    19 |         white += min(code_count[i], guess_count[i]);       |                                     ^~~~~~~~~~~ 43a4d6a6-9a23-48b9-9f9c-a3470940ef42.cpp:19:18: error: ‘min’ was not declared in  this scope; did you mean ‘main’?    19 |         white += min(code_count[i], guess_count[i]);       |                  ^~~       |                  main 43a4d6a6-9a23-48b9-9f9c-a3470940ef42.cpp:22:5: error: ‘cout’ was not declared in  this scope    22 |     cout << white << endl << black << endl;       |     ^~~~ 43a4d6a6-9a23-48b9-9f9c-a3470940ef42.cpp:22:22: error: ‘endl’ was not declared i n this scope    22 |     cout << white << endl << black << endl;       |                      ^~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -2,31 +2,31 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 int main() {
     string code, guess;
     cin >> code >> guess;
-    
+
     int black = 0, white = 0;
-    vector<int> codeCount(6, 0);
-    vector<int> guessCount(6, 0);
-    
+    vector<int> code_count(6, 0);
+    vector<int> guess_count(6, 0);
+
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
-            black++;
+            ++black;
         } else {
-            codeCount[code[i] - 'A']++;
-            guessCount[guess[i] - 'A']++;
+            ++code_count[code[i] - 'A'];
+            ++guess_count[guess[i] - 'A'];
         }
     }
-    
+
     for (int i = 0; i < 6; ++i) {
-        white += std::min(codeCount[i], guessCount[i]);
+        white += min(code_count[i], guess_count[i]);
     }
-    
-    std::cout << white << std::endl;
-    std::cout << black << std::endl;
-    
+
+    cout << white << endl << black << endl;
+
     return 0;
 }
