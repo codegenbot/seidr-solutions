77bf2feb37e27d000b5f512d6d47ebef89a92a1b
beep boop <githubbot@vadim.me>
2024-07-11 15:41:34 +0200

    Solution 57 of Python/14, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/bzmqs3ef Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e5199329-c5ee-4d5b-bd83-ce109de5b e44.py", line 1, in <module>     print(all_prefixes(input("Please enter a string: ")))           ^^^^^^^^^^^^ NameError: name 'all_prefixes' is not defined

--- Python_14.py
+++ Python_14.py
@@ -1,8 +1,3 @@
-from typing import List
-
-
-def all_prefixes(string: str) -> List[str]:
-    prefixes = []
-    for i in range(1, len(string) + 1):
-        prefixes.append(string[:i])
+def all_prefixes(s):
+    prefixes = [s[:i+1] for i in range(len(s))]
     return prefixes
