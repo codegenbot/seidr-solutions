4460c43b05707b50f12dd3e178cde79165220aa9
beep boop <githubbot@vadim.me>
2024-07-11 10:24:51 +0200

    Solution 84 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v70dnp3u Prompt: Program did not receive expected input

--- Python_37.py
+++ Python_37.py
@@ -3,9 +3,14 @@ def sort_even(l):
     return sorted([x for x in l if x % 2 == 0])
 
 def task_description():
-    num_list = input("Enter numbers separated by comma: ").split(",")
-    num_list = [int(i) for i in num_list]
-    print(sort_even(num_list))
+    while True:
+        num_list = input().split()
+        try:
+            num_list = [int(i) for i in num_list]
+            print(sort_even(num_list))
+            break
+        except ValueError:
+            print("Please enter integers. Try again!")
 
 if __name__ == "__main__":
     task_description()
