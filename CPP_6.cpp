5d5315648c3c22ab09bd17af46bec7a8f80a24a7
beep boop <githubbot@vadim.me>
2024-07-11 12:05:05 +0200

    Solution 91 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fw4dzic9 Prompt:       |                    ^~~ dc250d63-8960-4f91-9594-fa4a2c236553.cpp:24:27: error: ‘vector’ was not declared  in this scope    24 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ dc250d63-8960-4f91-9594-fa4a2c236553.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ dc250d63-8960-4f91-9594-fa4a2c236553.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from dc250d63-8960-4f91-9594-fa4a2c236553.cpp:23: dc250d63-8960-4f91-9594-fa4a2c236553.cpp: In function ‘int main()’: dc250d63-8960-4f91-9594-fa4a2c236553.cpp:33:20: error: ‘parse_nested_parens’ was  not declared in this scope    33 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ dc250d63-8960-4f91-9594-fa4a2c236553.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,25 +1,6 @@
 #include <vector>
 
-vector<int> parse_nested_parens(string paren_string);
-
-vector<int> parse_nested_parens(string paren_string){
-    vector<int> result;
-    int max_depth = 0;
-    int current_depth = 0;
-    for (char c : paren_string) {
-        if (c == '(') {
-            current_depth++;
-            if (current_depth > max_depth) {
-                max_depth = current_depth;
-            }
-        } else if (c == ')') {
-            current_depth--;
-        } else if (c == ' ') {
-            result.push_back(max_depth);
-            max_depth = 0;
-            current_depth = 0;
-        }
-    }
-    result.push_back(max_depth);
-    return result;
+bool areSame(int x, int y){
+    vector<int> parse_nested_parens(string paren_string);
+    return (x == y);
 }
