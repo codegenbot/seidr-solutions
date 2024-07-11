2d23229af20a7a9535d89bb7f17964e7f9b3efe0
beep boop <githubbot@vadim.me>
2024-07-11 11:18:51 +0200

    Solution 62 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/r8hjdq3f Prompt:       |                                  ^~~ 1e470787-9127-4e86-b26a-d42a375c2878.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 1e470787-9127-4e86-b26a-d42a375c2878.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 1e470787-9127-4e86-b26a-d42a375c2878.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 1e470787-9127-4e86-b26a-d42a375c2878.cpp:25: 1e470787-9127-4e86-b26a-d42a375c2878.cpp: In function ‘int main()’: 1e470787-9127-4e86-b26a-d42a375c2878.cpp:35:20: error: ‘even_odd_count’ was not declared in this scope    35 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 1e470787-9127-4e86-b26a-d42a375c2878.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -23,7 +23,7 @@ int main() {
     cin >> n;
     vector<int> result = even_odd_count(n);
     cout << result[0] << " " << result[1] << endl;
-
+    
     assert(even_odd_count(0) == vector<int>({1, 0}));
 
     return 0;
