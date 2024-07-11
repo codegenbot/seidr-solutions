3e00358b56cdc767f681933dad3201c96f0d41d7
beep boop <githubbot@vadim.me>
2024-07-11 11:37:11 +0200

    Solution 5 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bl8evmth Prompt:       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from ae749954-af53-4c19-8c40-3a1889b869d0.cpp:2: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ ae749954-af53-4c19-8c40-3a1889b869d0.cpp:14:22: error: aggregate ‘std::stringstr eam ss’ has incomplete type and cannot be defined    14 |         stringstream ss;       |                      ^~ ae749954-af53-4c19-8c40-3a1889b869d0.cpp:15:15: error: ‘hex’ was not declared in  this scope    15 |         ss << hex << setfill('0') << setw(2) << (int)md5[i];       |               ^~~ ae749954-af53-4c19-8c40-3a1889b869d0.cpp:15:22: error: ‘setfill’ was not declare d in this scope    15 |         ss << hex << setfill('0') << setw(2) << (int)md5[i];       |                      ^~~~~~~ ae749954-af53-4c19-8c40-3a1889b869d0.cpp:15:38: error: ‘setw’ was not declared i n this scope; did you mean ‘getw’?    15 |         ss << hex << setfill('0') << setw(2) << (int)md5[i];       |                                      ^~~~       |                                      getw

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,5 +1,20 @@
 #include <stringstream>
 #include <iomanip>
 
-// existing code...
-ss << std::hex << std::setfill('0') << std::setw(2) << (int)md5[i];
+using namespace std;
+
+string string_to_md5(string text) {
+    if (text.empty()) return "";
+    
+    unsigned char md5[MD5_DIGEST_LENGTH];
+    MD5((const unsigned char*)text.c_str(), text.size(), md5);
+    
+    string result;
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        stringstream ss;
+        ss << std::hex << std::setfill('0') << std::setw(2) << (int)md5[i];
+        result += ss.str();
+    }
+    
+    return result;
+}
