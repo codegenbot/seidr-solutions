5da08c14b91d65fdfbc1bbd7dfeb5c58b054c270
beep boop <githubbot@vadim.me>
2024-07-11 14:15:51 +0200

    Solution 63 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/xc3n7t3j Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2756eca3-ad69-4a49-a705-8f13c22d6 736.py", line 25, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2756eca3-ad69-4a49-a705-8f13c22d6 736.py", line 20, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,19 +1,7 @@
-def sort_even(l: list):
-    evens = sorted([x for x in l if x % 2 == 0])
-    odds = sorted([abs(x) for x in l if x % 2 != 0])  
-
-    result = []
-    i, j = 0, 0
-
-    while i < len(evens) and j < len(odds):
-        if evens[i] <= abs(odds[j]):
-            result.append(evens[i])
-            i += 1
-        else:
-            result.append(odds[j])
-            j += 1
-
-    result.extend(evens[i:])
-    result.extend([-x for x in odds[j:]])
-    
-    return result
+while i < len(evens) and j < len(odds):
+    if -abs(odds[j]) >= evens[i]:
+        result.append(evens[i])
+        i += 1
+    else:
+        result.append(odds[j])
+        j += 1
