c8d83963e9b8229c9e61191a383bac4e906c30cb
beep boop <githubbot@vadim.me>
2024-07-11 02:01:55 +0200

    Solution 69 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5mt0m7n3 Prompt: Program did not receive expected input

--- Python_22.py
+++ Python_22.py
@@ -1,9 +1,4 @@
 from typing import List, Any
 
-
 def filter_integers(values: List[Any]) -> list:
     return [value for value in values if isinstance(value, int)]
-
-
-integers = [1, 2, 3]
-print(filter_integers(integers))
