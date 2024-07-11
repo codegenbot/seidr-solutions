182fed09e4b7a3de312a766f61497ac9648d80c0
beep boop <githubbot@vadim.me>
2024-07-11 13:49:32 +0200

    Solution 68 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ipbpzph6 Prompt: Program did not receive expected input

--- Python_84.py
+++ Python_84.py
@@ -1,15 +1,13 @@
-```
 def solve(binary):
     while True:
-        try:
+        if binary:
             N = str(input(f"Enter a {'binary' if binary else 'decimal'} number: ")).strip()
-            if set(N).difference({'0', '1'}) != set() and not binary:
+            if set(N).difference({'0', '1'}) != set():
                 raise ValueError
             N = int(N, 2) if N.isdigit() and len(N) > 0 and N[0] != '0' or (len(N) > 1 and N[0] == '0' and N[1] != '0') else float(N)
-            break
-        except ValueError:
-            print(f"Invalid input! Please enter a valid {'binary' if binary else 'decimal'}")
-
+        else:
+            N = float(input(f"Enter a {'decimal' if not binary else 'binary'} number: ").strip())
+        break
     print(str(N))
 
 solve(True)
