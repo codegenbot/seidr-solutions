ae7742bcd2bd91d2b27d6b80c272ba44a1092621
beep boop <githubbot@vadim.me>
2024-07-11 13:51:38 +0200

    Solution 16 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/8j04hxex Prompt:       |                    ^~~ 78cf2ec1-e251-453a-9604-cd8836ef3a93.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 78cf2ec1-e251-453a-9604-cd8836ef3a93.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 78cf2ec1-e251-453a-9604-cd8836ef3a93.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 78cf2ec1-e251-453a-9604-cd8836ef3a93.cpp:9: 78cf2ec1-e251-453a-9604-cd8836ef3a93.cpp: In function ‘int main()’: 78cf2ec1-e251-453a-9604-cd8836ef3a93.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 78cf2ec1-e251-453a-9604-cd8836ef3a93.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,10 +1,15 @@
-sort(l1.begin(), l1.end());
-sort(l2.begin(), l2.end());
+#include <vector>
+#include <algorithm>
 
-vector<int> result;
-set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
+bool issame(int a, int b) {
+    return a == b;
+}
 
-result.erase(unique(result.begin(), result.end()), result.end());
-
-return result;
+vector<int> solve(vector<int>& l1, vector<int>& l2) {
+    sort(l1.begin(), l1.end());
+    sort(l2.begin(), l2.end());
+    vector<int> result;
+    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
+    result.erase(unique(result.begin(), result.end(), issame), result.end());
+    return result;
 }
