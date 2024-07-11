32e3a2518795576400a4f35013f4a99b5c601661
beep boop <githubbot@vadim.me>
2024-07-11 10:15:36 +0200

    Solution 25 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xzmxc8nx Prompt:       |                                 ^~~~~~ In file included from d4102b30-322c-4025-85cf-99718a88eca4.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ d4102b30-322c-4025-85cf-99718a88eca4.cpp:53:44: error: ‘b’ was not declared in t his scope    53 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d4102b30-322c-4025-85cf-99718a88eca4.cpp:53:45: error: expression list treated a s compound expression in initializer [-fpermissive]    53 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d4102b30-322c-4025-85cf-99718a88eca4.cpp:52: d4102b30-322c-4025-85cf-99718a88eca4.cpp: In function ‘int main()’: d4102b30-322c-4025-85cf-99718a88eca4.cpp:62:20: error: ‘by_length’ was not decla red in this scope    62 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ d4102b30-322c-4025-85cf-99718a88eca4.cpp:62:19: error: ‘issame’ cannot be used a s a function    62 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,78 +1,71 @@
-```cpp
+```
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
-bool issame(const std::vector<std::string>& v1, const std::vector<std::string>& v2) {
-    if(v1.size() != v2.size()) return false;
-    for(int i=0; i<v1.size(); ++i) {
-        if(v1[i] != v2[i]) return false;
+using namespace std;
+
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
 
-std::vector<std::string> by_length(std::vector<int> arr) {
-    std::vector<int> numbers;
+vector<string> by_length(vector<int> arr) {
+    vector<string> result;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            numbers.push_back(num);
+            string str;
+            switch (num) {
+                case 1:
+                    str = "One";
+                    break;
+                case 2:
+                    str = "Two";
+                    break;
+                case 3:
+                    str = "Three";
+                    break;
+                case 4:
+                    str = "Four";
+                    break;
+                case 5:
+                    str = "Five";
+                    break;
+                case 6:
+                    str = "Six";
+                    break;
+                case 7:
+                    str = "Seven";
+                    break;
+                case 8:
+                    str = "Eight";
+                    break;
+                case 9:
+                    str = "Nine";
+                    break;
+            }
+            result.push_back(str);
         }
     }
-
-    sort(numbers.begin(), numbers.end());
-    reverse(numbers.begin(), numbers.end());
-
-    std::vector<std::string> result;
-    for (int num : numbers) {
-        std::string str = "";
-        switch (num) {
-            case 1:
-                str = "One";
-                break;
-            case 2:
-                str = "Two";
-                break;
-            case 3:
-                str = "Three";
-                break;
-            case 4:
-                str = "Four";
-                break;
-            case 5:
-                str = "Five";
-                break;
-            case 6:
-                str = "Six";
-                break;
-            case 7:
-                str = "Seven";
-                break;
-            case 8:
-                str = "Eight";
-                break;
-            case 9:
-                str = "Nine";
-                break;
-        }
-        result.push_back(str);
-    }
-
+    sort(result.begin(), result.end());
+    reverse(result.begin(), result.end());
     return result;
 }
 
 int main() {
-    std::vector<int> test = {1, 2, 3};
-    std::vector<std::string> res = by_length(test);
-    for(auto x:res) cout<<x<<" ";
-    cout<<endl;
+    vector<int> arr = {1, 3, 5, 2, 4, 9};
+    vector<string> res = by_length(arr);
+    for (string str : res) {
+        cout << str << endl;
+    }
+    if (issame({res}, {"Nine", "Five", "Four", "Three", "Two", "One"})) {
+        cout << "Same" << endl;
+    } else {
+        cout << "Not Same" << endl;
+    }
     
-    // check if the function returns same strings 
-    vector<string> v1 = by_length({9,8,7});
-    vector<string> v2 = by_length({1,1,1});
-    if(issame(v1,v2)) {
-        cout<<"vectors are equal";
-    }
-    else {
-        cout<<"vectors are not equal";
-    }
-
+    return 0;
 }
