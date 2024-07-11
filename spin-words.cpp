bd6b0d330b4170a46b202f147da2557750ecca91
beep boop <githubbot@vadim.me>
2024-07-11 20:02:07 +0200

    Solution 33 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ocjywund Prompt: eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 05fd7758-d0d6-493d-8b8d-f177e88cebb2.cpp:39:18: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    39 |     getline(cin, str);       |                  ^~~       |                  std 05fd7758-d0d6-493d-8b8d-f177e88cebb2.cpp:39:5: error: ‘getline’ was not declared  in this scope    39 |     getline(cin, str);       |     ^~~~~~~ 05fd7758-d0d6-493d-8b8d-f177e88cebb2.cpp:40:32: error: ‘spinWords’ was not decla red in this scope    40 |     cout << "Spun result: " << spinWords(str) << endl;       |                                ^~~~~~~~~ 05fd7758-d0d6-493d-8b8d-f177e88cebb2.cpp:40:50: error: ‘endl’ was not declared i n this scope    40 |     cout << "Spun result: " << spinWords(str) << endl;       |                                                  ^~~~ 05fd7758-d0d6-493d-8b8d-f177e88cebb2.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <algorithm>   +++ |+#include <ostream>     2 | using namespace std;

--- spin-words.cpp
+++ spin-words.cpp
@@ -5,31 +5,17 @@ using namespace std;
 string spinWords(string str) {
     using namespace std;
     string result = "";
-    for (int i = 0; i < str.length(); i++) {
-        if (i < str.length() - 1 && str[i + 1] == ' ') {
-            int pos = i;
-            string word = str.substr(pos);
+    int start = 0;
+    for (int i = 0; i <= str.length(); i++) {
+        if (i == str.length() || str[i] == ' ') {
+            string word = str.substr(start, i - start);
             if (word.length() >= 5)
-                result.append(string(word.rbegin(), word.rend()) + " ");
+                result.append(word.rbegin(), word.rend());
             else
-                result.append(word + " ");
-            i += word.length();
-        } else {
-            if (str.find(" ", i) == -1 || i == str.length() - 1) {
-                string word = str.substr(i);
-                if (word.length() >= 5)
-                    result.append(string(word.rbegin(), word.rend()) + " ");
-                else
-                    result.append(word + " ");
-                break;
-            }
-            int pos = str.find(" ", i);
-            string word = str.substr(i, pos - i);
-            if (word.length() >= 5)
-                result.append(string(word.rbegin(), word.rend()) + " ");
-            else
-                result.append(word + " ");
-            i = pos;
+                result.append(word);
+            if (i < str.length())
+                result.append(" ");
+            start = i + 1;
         }
     }
     return result;
@@ -39,5 +25,6 @@ int main() {
     string str;
     cout << "Enter a string of words: ";
     getline(cin, str);
-    cout << "Spun result: " << spinWords(str) << std::endl;
+    cout << "Spun result: " << spinWords(str) << endl;
     return 0;
+}
