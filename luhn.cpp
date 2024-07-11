5f4b0610f61b7c99b7a27bc0af0ba2c1ff1c8f12
beep boop <githubbot@vadim.me>
2024-07-11 17:36:24 +0200

    Solution 96 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/3fyzd4t3 Prompt: f949b078-b75a-4fe8-acf6-2191adf50b48.cpp:27:22: warning: overflow in conversion from ‘long int’ to ‘int’ changes value from ‘14946721334973’ to ‘235144893’ [-Wo verflow]    27 |     int cardNumber = 14946721334973;       |                      ^~~~~~~~~~~~~~ f949b078-b75a-4fe8-acf6-2191adf50b48.cpp:28:10: error: ‘cout’ is not a member of  ‘std’    28 |     std::cout << "The Luhn checksum for this card is: " << luhn(cardNumb er) << std::endl;       |          ^~~~ f949b078-b75a-4fe8-acf6-2191adf50b48.cpp:1:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?   +++ |+#include <iostream>     1 | int luhn(int cardNumber) { f949b078-b75a-4fe8-acf6-2191adf50b48.cpp:28:85: error: ‘endl’ is not a member of  ‘std’    28 | The Luhn checksum for this card is: " << luhn(cardNumber) << std::endl;       |                                                                   ^~~~ f949b078-b75a-4fe8-acf6-2191adf50b48.cpp:1:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?   +++ |+#include <ostream>     1 | int luhn(int cardNumber) {

--- luhn.cpp
+++ luhn.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-using namespace std;
 
 int luhn(long long cardNumber) {
     int sum = 0;
 
-    vector<int> numbers;
+    std::vector<int> numbers;
     while (cardNumber > 0) {
         numbers.push_back(cardNumber % 10);
         cardNumber /= 10;
@@ -28,6 +27,6 @@ int luhn(long long cardNumber) {
 
 int main() {
     long long cardNumber = 14946721334973;  
-    cout << "The Luhn checksum for this card is: " << luhn(cardNumber) << endl;
+    std::cout << "The Luhn checksum for this card is: " << luhn(cardNumber) << std::endl;
     return 0;
 }
