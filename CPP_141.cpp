861168f8190002480c96678bb93b6f919d87a3ee
beep boop <githubbot@vadim.me>
2024-07-11 02:51:22 +0200

    Solution 30 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/p2ju98kx Prompt: 1b887692-f85a-4e45-953a-73fb41de63a5.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return (digit_count <= 3 && found_dot && valid_prefix) ? "Yes" :       | ^~~~~~ In file included from 1b887692-f85a-4e45-953a-73fb41de63a5.cpp:4: 1b887692-f85a-4e45-953a-73fb41de63a5.cpp: In function ‘int main()’: 1b887692-f85a-4e45-953a-73fb41de63a5.cpp:6:13: error: ‘file_name_check’ was not declared in this scope     6 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,15 +1,23 @@
-string file_name_check(string file_name) {
+int file_name_check(string file_name) {
     int digit_count = 0;
     bool found_dot = false;
-    string prefix = "";
+    bool valid_prefix = true;
+
     for (char c : file_name) {
         if (isdigit(c)) {
             digit_count++;
+            if (digit_count > 3) {
+                valid_prefix = false;
+                break;
+            }
         } else if (c == '.') {
             found_dot = true;
+        } else if (!isalpha(c)) {
+            valid_prefix = false;
+            break;
         }
-        prefix += c;
     }
-    return (digit_count <= 3 && found_dot && prefix.find('.') != string::npos) ? "Yes" : 
-           (!found_dot && !std::all_of(file_name.begin() + digit_count, file_name.end(), ::isalpha())) ? "No" : "Yes";
+
+    return (digit_count <= 3 && found_dot && valid_prefix) ? 1 : 
+           (!found_dot && !std::all_of(file_name.begin() + digit_count, file_name.end(), ::isalpha())) ? 0 : 1;
 }
