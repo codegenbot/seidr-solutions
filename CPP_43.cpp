6413dc1a457a719b13ce9abb5bff7a8b15d0b86d
beep boop <githubbot@vadim.me>
2024-07-11 14:12:15 +0200

    Solution 32 of CPP/43, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/r5shtlkb Prompt: Program failed

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,7 +1,13 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 bool pairs_sum_to_zero(vector<int> l) {
-    sort(l.begin(), l.end());
-    for (int i = 0; i < l.size() / 2; i++) { 
-        if (l[i] + l[l.size() - 1 - i] == 0)
+    vector<int> vec(l);
+    sort(vec.begin(), vec.end());
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i] * -1 == vec[vec.size() - i - 1])
             return true;
     }
     return false;
