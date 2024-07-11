16f45c1dc6da8700dbf147751c91c3c3065ee4de
beep boop <githubbot@vadim.me>
2024-07-11 05:20:28 +0200

    Solution 29 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/m9pke3ue Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/270d812c-b27e-4672-af0f-3510980d0 1b1.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,11 +1,1 @@
-```"
-def find_zero(xs: list):
-    "if the length of the input list xs is odd, raise a ValueError with an appropriate error message. 
-    else, calculate and return the zero point for the given quadratic equation (ax^2 + bx + c = 0)"
-    if len(xs) % 2 != 0:
-        raise ValueError("List xs must have even number of coefficients")
-    c = xs[1]
-    a = xs[-1]
-    b = -xs[0] / a
-    return round(-b / (2*a), 2)
-"```
+```def python_32():"""def find_zero(xs: list):    if len(xs) % 2 != 0:        raise ValueError("List xs must have even number of coefficients")    a = xs[-1]    b = -xs[1] / a    return round(-b / 2, 2)"```
