125738b214f6c2615bf8ac6038e910c2d5abc54b
beep boop <githubbot@vadim.me>
2024-07-11 11:18:57 +0200

    Solution 4 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/62wfe1t1 Prompt: b9ce5ab7-612e-4db8-ae27-ef3a151e6fa0.cpp: In function ‘bool simplify(std::string , std::string)’: b9ce5ab7-612e-4db8-ae27-ef3a151e6fa0.cpp:7:18: error: ‘getNumerator’ was not dec lared in this scope     7 |     int a = stoi(getNumerator(x));       |                  ^~~~~~~~~~~~ b9ce5ab7-612e-4db8-ae27-ef3a151e6fa0.cpp:8:18: error: ‘getDenominator’ was not d eclared in this scope     8 |     int b = stoi(getDenominator(x));       |                  ^~~~~~~~~~~~~~ b9ce5ab7-612e-4db8-ae27-ef3a151e6fa0.cpp: In function ‘std::string getDenominato r(std::string)’: b9ce5ab7-612e-4db8-ae27-ef3a151e6fa0.cpp:21:20: error: cannot resolve overloaded  function ‘find’ based on conversion to type ‘size_t’ {aka ‘long unsigned int’}    21 |     size_t pos = s.find '/';       |                    ^~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,16 +1,25 @@
+```c++
 #include <string>
+#include <iostream>
 
 using namespace std;
 
-bool simplify(string x, string n) {
-    int a = 1, b = 1, c = 1, d = 1;
-    istringstream s(x);
-    s >> a >> char('/'); 
-    s >> c;
-    
-    istringstream t(n);
-    t >> b >> char('/'); 
-    t >> d;
+string getNumerator(string s) {
+    size_t pos = s.find('/');
+    return s.substr(0, pos);
+}
 
-    return (a*d) == (b*c);
+string getDenominator(string s) {
+    size_t pos = s.find '/';
+    string temp = s.substr(pos + 1);
+    return temp;
+}
+
+bool simplify(string x, string n) {
+    int a = stoi(getNumerator(x));
+    int b = stoi(getDenominator(x));
+    int c = stoi(getNumerator(n));
+    int d = stoi(getDenominator(n));
+
+    return (a * d) == (b * c);
 }
