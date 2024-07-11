925a12a58c5b327393997f6741736cdade1e64e6
beep boop <githubbot@vadim.me>
2024-07-11 15:56:41 +0200

    Solution 48 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d03dbk1h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/82f3ea6d-552f-422f-993c-afeebbbb2 c63.py", line 23, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/82f3ea6d-552f-422f-993c-afeebbbb2 c63.py", line 19, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -4,16 +4,11 @@ def encode(message):
     result = ''
     for char in message:
         if char.isalpha():
-            if char.lower() in vowels:
+            if ord(char.lower()) - 97 < 5 or ord(char.lower()) - 97 >= 21:
+                new_char = char.swapcase()
+            else:
                 index = ord(char.lower()) - 97
                 new_char = chr((index + 2) % 26 + 97)
-            else:
-                new_index = (ord(char.lower()) - 97) % 26
-                if new_index < 3 or new_index > 18:
-                    new_char = char.swapcase()
-                else:
-                    index = ord(char.lower()) - 3
-                    new_char = chr((index + 2) % 26 + 97)
             result += new_char
         else:
             result += char
