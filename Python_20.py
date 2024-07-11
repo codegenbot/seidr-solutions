d725688e50a30a67c8d68dfb19752766d3f806f8
beep boop <githubbot@vadim.me>
2024-07-11 16:13:15 +0200

    Solution 74 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/7mtn3xy5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/86b0b815-72af-48b6-961e-6d7f959ab 66b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_20.py
+++ Python_20.py
@@ -1,1 +1,15 @@
-def find_closest_elements(arr, target=0):
+def find_closest_elements(arr, target):
+    arr.sort()
+    left = 0
+    right = len(arr) - 1
+
+    while True:
+        if left == right:
+            return [arr[left], arr[left]]
+
+        if target < (right - left) * arr[left] + arr[right]:
+            right = left + (right - left) // 2
+        else:
+            left = right - (right - left) // 2 + 1
+
+    return [arr[left], arr[right]]
