45c3f750d945ac607a88f503c29d754a5fb3cd78
beep boop <githubbot@vadim.me>
2024-07-11 12:31:47 +0200

    Solution 98 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/bp0n055l Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d7141c6a-b21a-4471-9311-4819db8f7d51.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d7141c6a-b21a-4471-9311-4819db8f7d51.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d7141c6a-b21a-4471-9311-4819db8f7d51.cpp:11: d7141c6a-b21a-4471-9311-4819db8f7d51.cpp: In function ‘int main()’: d7141c6a-b21a-4471-9311-4819db8f7d51.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ d7141c6a-b21a-4471-9311-4819db8f7d51.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,13 +1,16 @@
+#include <iostream>
 #include <vector>
 #include <list>
 #include <boost/any.hpp>
 #include <typeinfo>
 #include <cassert>
 
-bool issame(vector<int> a, vector<int> b);
+bool issame(std::vector<int> a, std::vector<int> b){
+    return a == b;
+}
 
-vector<int> filter_integers(std::list<boost::any> values){
-    vector<int> result;
+std::vector<int> filter_integers(std::list<boost::any> values){
+    std::vector<int> result;
     for(auto val : values){
         if (val.type() == typeid(int)){
             result.push_back(boost::any_cast<int>(val));
@@ -17,9 +20,7 @@ vector<int> filter_integers(std::list<boost::any> values){
 }
 
 int main() {
-    std::list<boost::any> input = {'c', 5, 'c', 10, 'c', 15};
-    vector<int> output = filter_integers(input);
-    vector<int> expected = {5, 10, 15};
-    assert(output == expected);
-    return 0;
+    std::list<boost::any> input = {'c'};
+    std::vector<int> output = filter_integers(input);
+    assert(output == std::vector<int>{});
 }
