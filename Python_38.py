bea281ed4072c05d39b2acaef57acfc6ac350016
beep boop <githubbot@vadim.me>
2024-07-11 14:06:36 +0200

    Solution 72 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/6zqnr7oa Prompt: Program did not receive expected input

--- Python_38.py
+++ Python_38.py
@@ -1,15 +1,16 @@
-```
 def decode_cyclic():
-    s = input("Enter the cyclic string: ")
+    while True:
+        s = input("Enter the cyclic string: ")
+        if len(set(s)) > 1: 
+            break
+        else:
+            print("Invalid input. Please enter a string with more than one unique character.")
     result = ""
     i = 0
     if len(s) <= 1:
         return s
     while True:
-        if i == 0:
-            group = s[i] + ''.join([s[(i+1)%len(s)], s[i][(i+1)%len(s)].__reversed__()[0]]) + s[i]
-        else:
-            group = s[i%len(s)] + ''.join([s[(i-1)%len(s)], s[i][(i-1)%len(s)].__reversed__()[0]]) + s[i%len(s)]
+        group = s[i] + ''.join(s[i+1:i+2][::-1]) + s[i]
         if i > 0:
             result += group[-1] + group[1:-1][::-1] + group[0]
         else:
