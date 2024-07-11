bbcdda6a81cc9a78bc212bc3f28c9ca0c3080cc2
beep boop <githubbot@vadim.me>
2024-07-11 15:51:07 +0200

    Solution 66 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g0yxi6dq Prompt: Program did not receive expected input

--- Python_20.py
+++ Python_20.py
@@ -1,11 +1,5 @@
 from typing import List, Tuple
 
-
-def find_closed_elements(numbers: List[float]) -> Tuple[float, float]:
+def find_closed_elements(numbers: list[float]) -> Tuple[float, float]:
     if len(numbers) <= 1:
-        return ()
-    numbers.sort()
-    for i in range(len(numbers) - 1):
-        if abs((numbers[i + 1] - numbers[i])) < 0.000001:
-            return (numbers[i], numbers[i + 1])
-    return ()
+        return (0.0, 0.0)
