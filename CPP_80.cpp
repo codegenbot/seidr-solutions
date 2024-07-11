2ef89b6c596ad06bd5e74d619d5fb567521c11fe
beep boop <githubbot@vadim.me>
2024-07-11 06:48:39 +0200

    Solution 85 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zzo3yblj Prompt: 76bae4f6-1624-4b0e-af35-a2f509a20817.cpp:6:73: error: invalid types ‘const int[i nt]’ for array subscript     6 | nt(s[i]) == int(s[i+1]) || int(s[i]) == int(s[i+2]) || int(s[i+1]) == in t(s[i+2]))       |                                                             ^ 76bae4f6-1624-4b0e-af35-a2f509a20817.cpp:6:88: error: invalid types ‘const int[i nt]’ for array subscript     6 |  int(s[i+1]) || int(s[i]) == int(s[i+2]) || int(s[i+1]) == int(s[i+2]))       |                                                                 ^ In file included from 76bae4f6-1624-4b0e-af35-a2f509a20817.cpp:13: 76bae4f6-1624-4b0e-af35-a2f509a20817.cpp: In function ‘int main()’: 76bae4f6-1624-4b0e-af35-a2f509a20817.cpp:15:22: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    15 |     assert (is_happy("iopaxioi") == false );       |                      ^~~~~~~~~~       |                      |       |                      const char* 76bae4f6-1624-4b0e-af35-a2f509a20817.cpp:1:29: note:   initializing argument 1 o f ‘bool is_happy(const int&)’     1 | bool is_happy(const string& s) {       |               ~~~~~~~~~~~~~~^

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,11 +1,11 @@
 bool is_happy(const string& s) {
     if(s.length() < 3)
         return false;
-
+    
     for(int i = 0; i < s.length() - 2; ++i) {
         if(s[i] - 'a' == s[i + 1] - 'a' || s[i] - 'a' == s[i + 2] - 'a' || s[i + 1] - 'a' == s[i + 2] - 'a')
             return false;
     }
-
+    
     return true;
 }
