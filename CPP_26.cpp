a17731ac8b351f62a89379c0a5050ccfdfb417ff
beep boop <githubbot@vadim.me>
2024-07-11 18:44:07 +0200

    Solution 13 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/27myevuf Prompt:                  from 02015c5e-e98c-4be4-a3fd-86f0b98fa112.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 02015c5e-e98c-4be4-a3fd-86f0b98fa112.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 02015c5e-e98c-4be4-a3fd-86f0b98fa112.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 02015c5e-e98c-4be4-a3fd-86f0b98fa112.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 02015c5e-e98c-4be4-a3fd-86f0b98fa112.cpp:9: 02015c5e-e98c-4be4-a3fd-86f0b98fa112.cpp: In function ‘int main()’: 02015c5e-e98c-4be4-a3fd-86f0b98fa112.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -4,6 +4,7 @@
 #include <cassert>
 
 std::vector<int> remove_duplicates(std::vector<int> numbers) {
+    std::sort(numbers.begin(), numbers.end());
     numbers.erase(std::unique(numbers.begin(), numbers.end()), numbers.end());
     return numbers;
 }
