ca8bd7febae9a766c393f9d33bb5b8bb439e306b
beep boop <githubbot@vadim.me>
2024-07-11 11:21:01 +0200

    Solution 2 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hn4pgr1r Prompt: Complete the following code given the task description and function signature.

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,35 +1,29 @@
 vector<string> bf(string planet1, string planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+    int index1 = -1, index2 = -1;
     
-    int index1 = -1;
     for (int i = 0; i < planets.size(); i++) {
         if (planet1 == planets[i]) {
             index1 = i;
+        } else if (planet2 == planets[i]) {
+            index2 = i;
+        }
+        
+        if (index1 != -1 && index2 != -1) {
             break;
         }
     }
     
-    int index2 = -1;
-    for (int i = 0; i < planets.size(); i++) {
-        if (planet2 == planets[i]) {
-            index2 = i;
-            break;
-        }
+    if (index1 == -1 || index2 == -1) {
+        return {};
     }
     
     vector<string> result;
-    
-    if (index1 >= 0 && index2 >= 0) {
-        if (index1 > index2) {
-            for (int i = index1; i >= index2 + 1; i--) {
-                result.push_back(planets[i]);
-            }
-        } else if (index1 < index2) {
-            for (int i = index1; i <= index2 - 1; i++) {
-                result.push_back(planets[i]);
-            }
-        } else {
-            return result;
+    for (int i = 0; i < planets.size(); i++) {
+        if (i > index1 && i < index2) {
+            result.push_back(planets[i]);
+        } else if (index1 > i && i > index2) {
+            result.push_back(planets[i]);
         }
     }
     
