f65807999e52de1af3d97a12bf94cb21aba2e74c
beep boop <githubbot@vadim.me>
2024-07-11 16:04:49 +0200

    Solution 77 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tdfielxa Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/23f1649e-c532-4936-804b-f0e630c36 51b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,22 +1,15 @@
-```
 def make_palindrome(s: str) -> str:
-    if s == s[::-1]:
-        return s
-    t = list(s)
-    l, r = 0, len(t) - 1
-    while l < r and l + 1 <= r - l:
-        if t[0] != t[r - 1]:
-            mid = (l + r) // 2
-            t.pop(r), t.insert(mid, t[0])
-            l += 1
-            r -= 1
-        else:
-            l, r = l + 1, r - 1
-    while s_list = list(s):
-        if s_list[0] == t[-1]:
-            t.append(s_list.pop(0))
-        elif len(s_list) > len(t):
-            t.insert(0, s_list.pop(0))
-        else:
-            t.append(s_list.pop())
-    return "".join(t)
+    s = list(s)
+    i, j = 0, len(s) - 1
+    while i < j:
+        if s[i] != s[j]:
+            t = s[j]
+            for k in range(j, i, -1):
+                if s[k] == t:
+                    s[i : k + 1] = list(t * (k - i + 1))
+                    break
+            else:
+                s[i : j + 1] = list(t * (j - i + 1))
+        i += 1
+        j -= 1
+    return "".join(s)
