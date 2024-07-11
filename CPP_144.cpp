16fd1a8ee204dc667e3fa421ee6eb54ed4ebee65
beep boop <githubbot@vadim.me>
2024-07-11 09:30:45 +0200

    Solution 12 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/o8kpcxb8 Prompt: f67a75fa-1475-4a35-a78f-c4290da9f08c.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ f67a75fa-1475-4a35-a78f-c4290da9f08c.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,8 +1,20 @@
-bool simplify(string x, string n) {
-    int num1 = stoi(x.substr(0, x.find("/")));
-    int den1 = stoi(x.substr(x.find("/") + 1));
-    int num2 = stoi(n.substr(0, n.find("/")));
-    int den2 = stoi(n.substr(n.find("/") + 1);
+#include <iostream>
+#include <string>
 
-    return (num1 * num2) % (den1 * den2) == 0;
+using namespace std;
+
+bool simplify(string x, string n){
+    int num1 = stoi(x.substr(0, x.find('/')));
+    int den1 = stoi(x.substr(x.find('/') + 1));
+    int num2 = stoi(n.substr(0, n.find('/')));
+    int den2 = stoi(n.substr(n.find('/') + 1));
+
+    return (num1 * den2 == num2 * den1);
+}
+
+int main() {
+    string x, n;
+    cin >> x >> n;
+    cout << (simplify(x, n) ? "true" : "false") << endl;
+    return 0;
 }
