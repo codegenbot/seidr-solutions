194084573636e26e8344181a13da42c3cf860183
beep boop <githubbot@vadim.me>
2024-07-11 11:49:21 +0200

    Solution 34 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/j28ml60h Prompt:    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ea7a9043-dba3-4832-8cc0-ef86a39815ef.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ea7a9043-dba3-4832-8cc0-ef86a39815ef.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ea7a9043-dba3-4832-8cc0-ef86a39815ef.cpp:21: ea7a9043-dba3-4832-8cc0-ef86a39815ef.cpp: In function ‘int main()’: ea7a9043-dba3-4832-8cc0-ef86a39815ef.cpp:31:20: error: ‘parse_music’ was not dec lared in this scope    31 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ ea7a9043-dba3-4832-8cc0-ef86a39815ef.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -5,7 +5,27 @@
 vector<int> parse_music(string music_string);
 
 bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+    return a == b; // or provide your own comparison logic
+}
+
+vector<int> parse_music(string music_string){ 
+    vector<int> beats;
+    string note = "";
+    for (char c : music_string) {
+        if (c == 'o') {
+            if (note == "o|") {
+                beats.push_back(2);
+            } else if (note == ".|") {
+                beats.push_back(1);
+            } else {
+                beats.push_back(4);
+            }
+            note = "";
+        } else {
+            note += c;
+        }
+    }
+    return beats;
 }
 
 int main() {
