9af4276adaa5f567708e99a83de88e4f9a349b87
beep boop <githubbot@vadim.me>
2024-07-11 17:04:34 +0200

    Solution 92 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g0yxi6dq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8d733edb-0c96-4882-823c-5c4f13a32 6ff.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_20.py
+++ Python_20.py
@@ -1,21 +1,19 @@
-```
 from typing import List, Tuple
 
 
 def find_closed_elements(numbers: list[float]) -> Tuple[float, float]:
-    
     if len(numbers) <= 1:
         return (0.0, 0.0)
-    
     min_diff = float("inf")
     min_range = (float("inf"), -float("inf"))
-    
     for i in range(len(numbers)):
         for j in range(i + 1, len(numbers)):
             diff = numbers[j] - numbers[i]
             if -1 <= diff <= 1:
                 if abs(diff) < min_diff:
                     min_diff = abs(diff)
-                    min_range = (min(numbers[i], numbers[j]), max(numbers[i], numbers[j]))
-    
+                    min_range = (
+                        min(numbers[i], numbers[j]),
+                        max(numbers[i], numbers[j]),
+                    )
     return min_range
