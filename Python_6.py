909bb51d9d5df222dfa99e027d934d0457fb4cfe
beep boop <githubbot@vadim.me>
2024-07-11 20:32:59 +0200

    Solution 81 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4elyvad2 Prompt: Complete the following code given the task description and function signature.

--- Python_6.py
+++ Python_6.py
@@ -2,13 +2,13 @@
 def parse_nested_parens(paren_string: str) -> List[int]:
     result = []
     for s in paren_string.split():
-        stack = []
-        max_depth = 0
+        level = 0
+        max_level = 0
         for char in s:
             if char == '(':
-                stack.append(1)
-                max_depth = max(max_depth, len(stack))
+                level += 1
+                max_level = max(max_level, level)
             elif char == ')':
-                stack.pop()
-        result.append(max_depth)
+                level -= 1
+        result.append(max_level)
     return result
