fd0a269b8dac784db1962ca5ea26bb45a78ce740
beep boop <githubbot@vadim.me>
2024-07-11 10:47:05 +0200

    Solution 41 of CPP/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/o8kpcxb8 Prompt: Program failed

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -2,13 +2,10 @@
 #include <cassert>
 
 bool simplify(std::string x, std::string n) {
-    std::string s;
-    bool simplify;
+    int num1 = std::stoi(x.substr(0, x.find('/')));
+    int den1 = std::stoi(x.substr(x.find('/') + 1));
+    int num2 = std::stoi(n.substr(0, n.find('/')));
+    int den2 = std::stoi(n.substr(n.find('/') + 1));
 
-    int num1 = stoi(x.substr(0, x.find('/')));
-    int den1 = stoi(x.substr(x.find('/') + 1));
-    int num2 = stoi(n.substr(0, n.find('/')));
-    int den2 = stoi(n.substr(n.find('/') + 1));
-
-    return (num1 * num2 * den1) % (den1 * den2) == 0;
+    return (num1 * num2) % (den1 * den2) == 0;
 }
