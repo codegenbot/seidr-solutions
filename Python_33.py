294fe378982c713cc7dc01d6033b764c01b1a6e8
beep boop <githubbot@vadim.me>
2024-07-11 22:13:07 +0200

    Solution 23 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/pmqokffl Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a4c8f9d9-b946-4d59-bb69-8381cbeb1 0df.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a4c8f9d9-b946-4d59-bb69-8381cbeb1 0df.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a4c8f9d9-b946-4d59-bb69-8381cbeb1 0df.py", line 2, in sort_third     return [sorted(l[i]) if i % 3 == 0 else l[i] for i in range(len(l))]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a4c8f9d9-b946-4d59-bb69-8381cbeb1 0df.py", line 2, in <listcomp>     return [sorted(l[i]) if i % 3 == 0 else l[i] for i in range(len(l))]             ^^^^^^^^^^^^ TypeError: 'int' object is not iterable

--- Python_33.py
+++ Python_33.py
@@ -1,2 +1,3 @@
+
 def sort_third(l: list):
-    return [sorted(l[i : i + 3]) if i % 3 == 0 else l[i] for i in range(len(l))]
+    return [sorted(l[i:i+3]) if i % 3 == 0 else l[i] for i in range(len(l))]
