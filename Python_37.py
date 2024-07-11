0ef56cc9997415680fa82e7da7b12b569c0c5c1e
beep boop <githubbot@vadim.me>
2024-07-11 17:28:32 +0200

    Solution 80 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/aexjympc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2c7b4684-b1ce-4687-8625-fc5c857c6 f58.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2c7b4684-b1ce-4687-8625-fc5c857c6 f58.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,14 +1,6 @@
-```
 def sort_even(l: list):
-    evens = sorted([x for x in l if x % 2 == 0])
-    result = []
-    i = j = 0
-    while j < len(l):
-        if i < len(evens):
-            result.append(evens[i])
-        else:
-            result.append(j)
-        j += 1
-        if i < len(evens) and j < len(l) and l[j] % 2 == 0:
-            i += 1
-    return result
+    evens = [
+        (j, j) if j % 2 == 0 else (j, i)
+        for i, j in zip(sorted([j for j in l if j % 2 == 0]), l)
+    ]
+    return [i if pair[1] % 2 != 0 else pair[0] for pair in evens]
