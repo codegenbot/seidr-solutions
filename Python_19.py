6a53ea939dea64923dc66dd8aafb99e9f648a093
beep boop <githubbot@vadim.me>
2024-07-11 08:13:02 +0200

    Solution 16 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hn1msxib Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f2058f5f-3d83-4487-9141-be863156f 909.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_19.py
+++ Python_19.py
@@ -1,6 +1,16 @@
 ```
-def main():
-    check(solve)
+def num_dict():
+    return {"1": 1, "2": 2}
 
-if __name__ == "__main__":
-    main()
+def sort_numbers(user_input=""):
+    numbers = user_input.split() if user_input else input("Enter space-separated numbers: ")
+    return [int(x) for x in numbers]
+
+def solve():
+    sorted_nums = sorted([num_dict()[i] for i in sort_numbers()])
+    return " ".join(str(num) for num in sorted_nums)
+
+def check(f):
+    print(f())
+
+check(solve)
