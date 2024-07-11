12419ea2a43c5492065f86b4fa4f819e6909d061
beep boop <githubbot@vadim.me>
2024-07-11 14:12:07 +0200

    Solution 56 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c4exytmv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ccc3c874-bf76-463a-9bb1-ad5020e53 84e.py", line 32, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ccc3c874-bf76-463a-9bb1-ad5020e53 84e.py", line 29, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,11 +1,10 @@
-```
-def make_palindrome(s: str) -> str:
-    if s == s[::-1]:
-        return s
-    
-    left = list(s)
-    right = list(s)[::-1]
-    
+def make_palindrome(string: str) -> str:
+    if string == string[::-1]:
+        return string
+
+    left = list(string)
+    right = list(string)[::-1]
+
     i, j = 0, len(right) - 1
     while i < len(left):
         if left[i] != right[j]:
@@ -24,5 +23,5 @@ def make_palindrome(s: str) -> str:
         else:
             i += 1
             j -= 1
-    
-    return "".join(left + list(s))
+
+    return "".join(right + left[::-1])
