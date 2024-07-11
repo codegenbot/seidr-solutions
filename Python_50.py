f0db92a08dd1a3f21fe7761c0223aa7751c10e14
beep boop <githubbot@vadim.me>
2024-07-11 02:36:55 +0200

    Solution 36 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7wf1ua6m Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/db48725a-04ca-430d-a290-191abcfd3 498.py", line 3     return "".join([chr(((ord(ch) - 97 - (0 if ch.isalpha() else 1)) % 26) + 97) ) if ch.isalpha() else ch for ch in s]) ^ SyntaxError: closing parenthesis ')' does not match opening parenthesis '['

--- Python_50.py
+++ Python_50.py
@@ -1,2 +1,1 @@
-def decode_shift(s: str):
-    return "".join([chr(((ord(ch) - 5 - 97 if ch.isalpha() and ch.lower() == ch else ord(ch) - 65)) % 26 + 97 if ch.isalpha() and ch.lower() == ch else ord(ch)) for ch in s])
+return "".join([chr(((ord(ch) - 97 - (0 if ch.isalpha() else 1)) % 26) + 97) for ch in s])
