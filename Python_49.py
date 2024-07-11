7a408f4ceffc664af1d4c5d6a54a07414958637a
beep boop <githubbot@vadim.me>
2024-07-12 00:11:10 +0200

    Solution 14 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/61v9nxqf Prompt: Complete the following code given the task description and function signature.

--- Python_49.py
+++ Python_49.py
@@ -1,6 +1,2 @@
 def modp(n: int, p: int):
-    return (
-        pow(n, p - 2, p) * n % p
-        if (n := n.bit_length(), p := pow(10, p.bit_length()))[1] % 2 == 0
-        else pow(n, p, p)
-    )
+    return pow(n, p - 2, p) * n % p
