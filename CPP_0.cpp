804874f020ab35c6bb6f24c95a1d1b80a6df0fd3
beep boop <githubbot@vadim.me>
2024-07-11 04:39:22 +0200

    Solution 60 of CPP/0, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pp6rfqku Prompt:   +++ |+#include <ostream>     2 | using namespace std; 5711d710-1d28-48fb-9a4a-bd2cf41a07a3.cpp:20:9: error: ‘cout’ was not declared in  this scope    20 |         cout << "The array does not contain close elements." << endl;       |         ^~~~ 5711d710-1d28-48fb-9a4a-bd2cf41a07a3.cpp:20:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 5711d710-1d28-48fb-9a4a-bd2cf41a07a3.cpp:20:65: error: ‘endl’ was not declared i n this scope    20 |         cout << "The array does not contain close elements." << endl;       |                                                                 ^~~~ 5711d710-1d28-48fb-9a4a-bd2cf41a07a3.cpp:20:65: note: ‘std::endl’ is defined in header ‘<ostream>’; did you forget to ‘#include <ostream>’? 5711d710-1d28-48fb-9a4a-bd2cf41a07a3.cpp: At global scope: 5711d710-1d28-48fb-9a4a-bd2cf41a07a3.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 5711d710-1d28-48fb-9a4a-bd2cf41a07a3.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 bool has_close_elements(vector<float> numbers, float threshold){
     for(int i = 0; i < numbers.size(); i++){
         for(int j = i+1; j < numbers.size(); j++){
@@ -6,4 +10,5 @@ bool has_close_elements(vector<float> numbers, float threshold){
         }
     }
     return false;
+
 }
