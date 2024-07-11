8f08d584f3aba76905121d1942c2453d30a0f39a
beep boop <githubbot@vadim.me>
2024-07-11 23:59:53 +0200

    Solution 63 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/abgk8arz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/db12795d-39ea-462a-a2d8-252fa197b 26a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,3 +1,4 @@
+```
 def bowling_score(frames):
     score = 0
     frames = frames.replace("-", "/").split("/")
@@ -5,11 +6,11 @@ def bowling_score(frames):
         if f == "X":
             score += 10
             if i < len(frames) - 1:
-                score += int(flatten(f).lstrip("0")) if flatten(f).else 0
+                score += int(f"{f[1:]}" or 0)
         elif f.strip() == "":
             score += 10
             if i < len(frames) - 1:
-                score += int(frames[i + 1])
+                score += int(f"{frames[i + 1]}" or 0)
         else:
             rolls = [int(roll) for roll in f.split(" ")]
             if sum(rolls) == 10:
@@ -17,8 +18,3 @@ def bowling_score(frames):
             else:
                 score += sum(rolls)
     return score
-
-def flatten(f):
-    while "" in f and len(f) > 0:
-        f = f.replace("", "0")
-    return f
