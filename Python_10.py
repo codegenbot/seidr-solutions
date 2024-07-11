f81628006a7bf96d24177b467c784ad672b0b83a
beep boop <githubbot@vadim.me>
2024-07-11 14:24:22 +0200

    Solution 70 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c4exytmv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/588a83b2-3d0f-4c68-88a2-cde3ef42e 6b4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,4 +1,3 @@
-```
 def make_palindrome(s: str) -> str:
     if s == s[::-1]:
         return s
@@ -10,17 +9,17 @@ def make_palindrome(s: str) -> str:
     while i < len(left):
         if left[i] != right[j]:
             if j > 0:
-                if left[0] == right[j-1]:
+                if left[0] == right[j - 1]:
                     left.insert(0, right[j])
                     break
                 else:
-                    left.insert(0, right[j-1])
+                    left.insert(0, right[j - 1])
                     j -= 1
             else:
                 if len(left) >= 2 and left[0] != right[0]:
                     left.insert(0, right[0])
                 else:
-                    left.insert(0, 'a')
+                    left.insert(0, "a")
         else:
             i += 1
             j -= 1
@@ -29,4 +28,4 @@ def make_palindrome(s: str) -> str:
         left.insert(0, s[-1])
         s = s[:-1]
 
-    return "".join(left + list(s))
+    return "".join(left + [s[i] for i in range(len(s) - 1, -1, -1)])
