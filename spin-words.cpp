73088f880b39132d050653de8fab71a25d84fe3a
beep boop <githubbot@vadim.me>
2024-07-11 16:05:11 +0200

    Solution 56 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/3re30ali Prompt: it must return r bewjpsiv mflg iab for input r vispjweb mflg iab, but it returns Enter a sentence: r bewjpsiv mflg iab.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,14 +1,8 @@
+```c++
 #include <string>
 #include <iostream>
 
 using namespace std;
-
-string reverseWord(string str) {
-    string revWord;
-    for (int j = str.length() - 1; j >= 0; j--)
-        revWord += str[j];
-    return revWord;
-}
 
 string spinWords(string str) {
     string result = "";
@@ -17,7 +11,10 @@ string spinWords(string str) {
     for (int i = 0; i < str.length(); i++) {
         if (str[i] == ' ') {
             if (word.length() >= 5) {
-                result += reverseWord(word) + " ";
+                string revWord;
+                for (int j = word.length() - 1; j >= 0; j--)
+                    revWord += word[j];
+                result += revWord + " ";
                 word = "";
             } else
                 result += word + " ";
@@ -27,11 +24,18 @@ string spinWords(string str) {
         }
     }
 
-    if (word.length() >= 5)
-        result += reverseWord(word) + " ";
-    else
+    if (word.length() >= 5) {
+        string revWord;
+        for (int j = word.length() - 1; j >= 0; j--)
+            revWord += word[j];
+        result += revWord + " ";
+    } else
         result += word + " ";
 
+    size_t pos = 0;
+    while ((pos = result.find(" ")) != string::npos) {
+        result.erase(pos, 1);
+    }
     return result;
 }
 
