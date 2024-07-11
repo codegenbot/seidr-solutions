a4487c48aaa9be106868a998a203793d5c8511ce
beep boop <githubbot@vadim.me>
2024-07-11 05:28:20 +0200

    Solution 53 of CPP/158, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/3wv3jf67 Prompt: 6f660d61-7fab-4a20-8e60-3de0662ea2ee.cpp:3:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     2 | #include <vector>   +++ |+#include <iostream>     3 | 6f660d61-7fab-4a20-8e60-3de0662ea2ee.cpp:26:42: error: ‘endl’ is not a member of  ‘std’    26 |     std::cout << find_max(words) << std::endl;       |                                          ^~~~ 6f660d61-7fab-4a20-8e60-3de0662ea2ee.cpp:3:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     2 | #include <vector>   +++ |+#include <ostream>     3 | 6f660d61-7fab-4a20-8e60-3de0662ea2ee.cpp: At global scope: 6f660d61-7fab-4a20-8e60-3de0662ea2ee.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 6f660d61-7fab-4a20-8e60-3de0662ea2ee.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_158.cpp
+++ CPP_158.cpp
@@ -15,7 +15,7 @@ std::string find_max(std::vector<std::string> words) {
         if (unique_chars.size() > max_unique_chars) {
             max_unique_chars = unique_chars.size();
             result = word;
-        } else if (unique_chars.size() == max_unique_chars && word < result) {
+        } else if (unique_chars.size() == max_unique_chars && word.compare(result) > 0) {
             result = word;
         }
     }
