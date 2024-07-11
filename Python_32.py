42aa91b7b3b90efe7479e43e7e1e37c3691db0a8
beep boop <githubbot@vadim.me>
2024-07-11 14:21:53 +0200

    Solution 40 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/01a7l32z Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d408cf66-a985-41cf-890f-f805b120f d3a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,12 +1,7 @@
-```
 def find_zero(xs):
     if len(xs) != 2:
         return "Error: xs must have exactly 2 coefficients"
-    elif len(xs) % 2 != 0:
+    if len(xs[0]) % 2 != 0:
         return "Error: xs must have an even number of coefficients"
-    else:
-        max_coeff = max(abs(x) for x in xs)
-        if abs(xs[0]) == max_coeff:
-            return -xs[1] / (2 * xs[0])
-        else:
-            return None
+    max_coeff = max(abs(x) for x in xs)
+    return -xs[1] / (2 * xs[0]) if abs(xs[0]) == max_coeff else None
