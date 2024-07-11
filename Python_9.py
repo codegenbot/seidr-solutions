bb1dafa4442d9f581432aba09d2f9541bc5335f8
beep boop <githubbot@vadim.me>
2024-07-11 13:23:29 +0200

    Solution 6 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/etx4my5z Prompt: Program did not receive expected input

--- Python_9.py
+++ Python_9.py
@@ -1,10 +1,13 @@
 from typing import List
 
 def rolling_max(numbers: List[int]) -> List[int]:
-    result = [numbers[0]]
-    max_so_far = numbers[0]
-    for num in numbers[1:]:
-        if num > max_so_far:
-            max_so_far = num
-        result.append(max(num, max_so_far))
+    if len(numbers) > 1:
+        result = [numbers[0]]
+        max_so_far = numbers[0]
+        for num in numbers[1:]:
+            if num > max_so_far:
+                max_so_far = num
+            result.append(max_so_far)
+    else:
+        result = numbers.copy() 
     return result
