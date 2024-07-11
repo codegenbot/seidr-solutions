2c764e2bb5bcd647061ea2cf0d7245016f17ec3e
beep boop <githubbot@vadim.me>
2024-07-11 22:25:04 +0200

    Solution 86 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lwas6rhh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ec59ff6e-a95c-4388-82be-c37407d76 b31.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -1,17 +1,18 @@
 ```
 def minPath(grid, k):
     n = len(grid)
-    
+    res = []
+
     def dfs(i, j, path):
         if len(path) == k:
             return path
         for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
             if 0 <= x < n and 0 <= y < n and grid[x][y] not in path:
-                return dfs(x, y, path + [grid[x][y]])
-        return []
+                res.append(dfs(x, y, path + [grid[x][y]]))
+        return min([p for p in res if len(p) == k])
 
     for i in range(n):
         for j in range(n):
-            res = dfs(i, j, [grid[i][j]])
-            if res:
-                return res[1:]
+            res.append(dfs(i, j, [grid[i][j]]))
+
+    return min([p for p in res if len(p) == k])[1:]
