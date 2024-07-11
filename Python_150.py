75dbafb4b0ad37316254e81e7ea1656dac3ea782
beep boop <githubbot@vadim.me>
2024-07-11 23:28:00 +0200

    Solution 5 of Python/150, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/dx8vttbb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/be4e0d8c-d242-4743-b922-94bda240d 557.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_150.py
+++ Python_150.py
@@ -1,19 +1,6 @@
-def is_prime_number(x):
-    if x < 2:
-        return False
-    for i in range(2, int(x**0.5) + 1):
-        if x % i == 0:
-            return False
-    return True
-
-
-def prime_factors(n):
-    factors = []
-    for i in range(2, n + 1):
-        if is_prime_number(i) and (n % i == 0):
-            while n % i == 0:
-                factors.append(i)
-                n //= i
+def x_or_y(n, x, y):
     if n > 1:
-        factors.append(n)
-    return x_or_y(len(factors), "y", "x")
+        for i in range(2, n):
+            if (n % i) == 0:
+                return y
+    return x
