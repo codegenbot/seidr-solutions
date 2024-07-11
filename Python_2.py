c9a4d8846be6c20c295c76ed5742bd86cef5d65c
beep boop <githubbot@vadim.me>
2024-07-11 09:02:32 +0200

    Solution 70 of Python/2, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mtyau5r5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03e104aa-d245-47ec-b873-de14db002 0c3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_2.py
+++ Python_2.py
@@ -1,9 +1,4 @@
 ```
-def check(number):
-    result = format(round(number, 2), '.2f')
-    if float(result) >= 10:
-        print("The number is greater than or equal to 10.")
-    elif float(result) < 5:
-        print("The number is less than 5.")
-    else:
-        print("The number is between 5 and 10.")
+def check():
+    number = float(input("Enter a number: "))
+    return format(round(number, 2), '.2f')
