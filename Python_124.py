d96914da79adf5437a32a29188bcbe2d89105e09
beep boop <githubbot@vadim.me>
2024-07-11 04:29:06 +0200

    Solution 68 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wg5nozkz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bc1f78c5-1364-4cfd-bd1b-8a4931f58 836.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_124.py
+++ Python_124.py
@@ -1,9 +1,11 @@
+```
 def valid_date(date):
     day, month, year = date.split('-')
     if len(day) == 1:
         day = '0' + day
     if len(month) == 1:
         month = '0' + month
+
     try:
         int(year)
         if not (int(month) >= 1 and int(month) <= 12):
@@ -12,7 +14,10 @@ def valid_date(date):
             if int(day) > 30 or int(day) < 1:
                 return False
         elif int(month) == 2:
-            if not ((int(year) % 4 == 0 and int(year) % 100 != 0) or (int(year) % 400 == 0)):
+            if int(year) % 4 != 0:
+                if int(day) > 28 or int(day) < 1:
+                    return False
+            else:
                 if int(day) > 29 or int(day) < 1:
                     return False
         else:
