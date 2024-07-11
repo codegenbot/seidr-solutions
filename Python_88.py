b8f310e27647d6ffd2144134bdbbcd220432de0e
beep boop <githubbot@vadim.me>
2024-07-11 11:09:34 +0200

    Solution 37 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/er6nmf1f Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c28f5519-b4bf-476a-b14f-e8b527598 940.py", line 14, in <module>     check(sort_array)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c28f5519-b4bf-476a-b14f-e8b527598 940.py", line 11, in check     assert sort_array([21, 14, 23, 11]) == [23, 21, 14, 11], "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_88.py
+++ Python_88.py
@@ -1,7 +1,10 @@
-```
 def sort_array(array):
-    array.sort()
-    if len(array) % 2 == 0:
-        return [array[len(array)//2-1: len(array)//2+1]]
+    sorted_array = sorted(array)
+    n = len(sorted_array)
+
+    if n % 2 != 0:
+        return [sorted_array[n//2]]
     else:
-        return [array[(len(array)+1)//2-1: (len(array)+1)//2]]
+        mid1 = n // 2 - 1
+        mid2 = n // 2
+        return [[sorted_array[mid1], sorted_array[mid2]]]
