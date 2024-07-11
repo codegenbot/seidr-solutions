474ae1cb788cd7a91db0154ad27d3792c67cd91f
beep boop <githubbot@vadim.me>
2024-07-11 20:28:23 +0200

    Solution 3 of CPP/42, ratio 0.3333333333333333 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/cy2zr74a Prompt:     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9598ca47-dafc-400d-b715-b9934e8a3ed4.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9598ca47-dafc-400d-b715-b9934e8a3ed4.cpp:9:39: error: expression list treated as  compound expression in initializer [-fpermissive]     9 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9598ca47-dafc-400d-b715-b9934e8a3ed4.cpp:8: 9598ca47-dafc-400d-b715-b9934e8a3ed4.cpp: In function ‘int main()’: 9598ca47-dafc-400d-b715-b9934e8a3ed4.cpp:18:20: error: ‘incr_list’ was not decla red in this scope    18 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ 9598ca47-dafc-400d-b715-b9934e8a3ed4.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,4 +1,9 @@
-vector<int> result;
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
+
+vector<int> incr_list(vector<int> l){
+    vector<int> result;
     for (int num : l) {
         result.push_back(num + 1);
     }
