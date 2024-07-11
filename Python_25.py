ee5ef221939e0c218c2b105985e0060fd766234a
beep boop <githubbot@vadim.me>
2024-07-11 03:36:48 +0200

    Solution 72 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/32t7vnt2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dc4cdf36-493a-494d-a508-53a7f93c3 9d5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,9 +1,4 @@
-```
-from typing import List
-import math
-
-
-def factorize(n: int) -> List[int]:
+def findPrime(n: int) -> List[int]:
     factors = []
     i = 2
     while i * i <= n:
@@ -14,8 +9,8 @@ def factorize(n: int) -> List[int]:
             while n % i == 0:
                 count += 1
                 n //= i
-            factors.append(i ** count)
+            factors.append(i**count)
             i = 2
     if n > 1:
         factors.append(n)
-    return factors
+    return [f for f in factors if math.isqrt(f) ** 2 == f]
