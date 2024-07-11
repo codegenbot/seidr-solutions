b968d4e0d0c84f514d0e914610247dcd8b16653d
beep boop <githubbot@vadim.me>
2024-07-11 22:05:58 +0200

    Solution 65 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/joxoawd6 Prompt: it must return 105 for input 9/35619/5/X6124219-, but it returns 83.

--- bowling.cpp
+++ bowling.cpp
@@ -3,40 +3,32 @@ using namespace std;
 
 int calculateScore(string s) {
     int total = 0;
-    int frame = 0;
+    int frame = 1;
     int i = 0;
-    while (frame < 10) {
+
+    while (frame <= 10) {
         switch (s[i]) {
             case 'X':
                 total += 10;
-                if (s[i+2] == 'X') {
-                    total += 10;
-                    if (s[i+4] == 'X') {
-                        total += 10;
-                    } else if (s[i+4] == '/') {
-                        total += 10 - (s[i+3] - '0');
-                    } else {
-                        total += s[i+3] - '0' + s[i+4] - '0';
-                    }
-                    i += 2;
-                } else if (s[i+3] == '/') {
-                    total += 10;
-                    i += 3;
+                if (s[i + 2] == 'X') {
+                    total += 10 + (s[i + 4] == 'X' ? 10 : (s[i + 4] - '0'));
                 } else {
-                    total += s[i+2] - '0' + s[i+3] - '0';
-                    i += 2;
+                    total += (s[i + 2] == '/' ? 10 : (s[i + 2] - '0' + s[i + 3] - '0'));
                 }
+                i++;
                 break;
             case '/':
-                total += 10 - (s[i-1] - '0');
+                total += 10 - (s[i - 1] - '0');
+                total += (s[i + 1] == 'X' ? 10 : (s[i + 1] - '0'));
+                i += 2;
                 break;
             default:
-                total += s[i] - '0';
-                break;
+                total += s[i] - '0' + s[i + 1] - '0';
+                i += 2;
         }
-        i++;
         frame++;
     }
+
     return total;
 }
 
