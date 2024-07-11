76f24fc6ce26a763220939a396bdcf0399e7237d
beep boop <githubbot@vadim.me>
2024-07-11 15:30:23 +0200

    Solution 94 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/01a7l32z Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f2976243-ba5e-43c6-a4e5-c97a18be0 6e8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,10 +1,10 @@
-```Python
+```
 def find_zero():
     coeffs = list(map(int, input("Enter the coefficients: ").split()))
     if len(coeffs) % 2 != 1:
         return "Error: xs must start with 0"
 
     x = [0]
-    y = [sum([coeff * (i+1)**(len(coeffs)-1-j) for j, coeff in enumerate(coeffs)]) for i in range(len(coeffs))]
+    y = coeffs[1:]
 
-    return list(zip(x, y))
+    return y
