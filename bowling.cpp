d46da316c4c94b13217de3730be9d5126a5c2194
beep boop <githubbot@vadim.me>
2024-07-11 13:58:47 +0200

    Solution 64 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/dlv2ukls Prompt: 087121b9-50f8-4962-b77e-93e4fc41fd48.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int bowlingScore(std::string s) {       |                       ^~~~~~ 087121b9-50f8-4962-b77e-93e4fc41fd48.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int bowlingScore(std::string s) {

--- bowling.cpp
+++ bowling.cpp
@@ -2,21 +2,35 @@
 
 int bowlingScore(std::string s) {
     int score = 0;
-    int roll = 0;
     for(int i = 0; i < s.size(); i++) {
         if(s[i] == '/') {
-            if(roll < 2) {
-                score += (10 - '0') * 10 + (10 - 'X');
+            if(i + 1 >= s.size() || s[i+1] != '/') {
+                score += (s[i-1] - '0') * 10;
             } else {
-                score += (10 - 'X') * 10;
+                int rolls = 2;
+                while(rolls--) {
+                    score += (s[i-1] - '0') * 10 + (s[i] - '0');
+                    i++;
+                }
             }
-            roll = 0;
         } else if('0' <= s[i] && s[i] <= '9') {
-            roll = roll * 10 + (s[i] - '0');
+            int rolls = 1;
+            while(rolls--) {
+                score += (s[i] - '0');
+                i++;
+                if(i < s.size() && s[i] == '/') break;
+            }
         } else if(s[i] == 'X') {
             score += 10;
-            roll = 0;
+            if(i + 1 < s.size() && s[i+1] != '/') {
+                int rolls = 2;
+                while(rolls--) {
+                    score += (s[i+1] - '0');
+                    i++;
+                }
+            } else {
+                i++;
+            }
         }
     }
     return score;
-}
