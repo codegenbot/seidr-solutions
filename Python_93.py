1765c18ac2467ba00778abb06087103ae2e7d870
beep boop <githubbot@vadim.me>
2024-07-11 20:55:04 +0200

    Solution 80 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ctj0vn6n Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6dcf4540-3cea-4380-976b-9047dfe6c 78e.py", line 18, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6dcf4540-3cea-4380-976b-9047dfe6c 78e.py", line 14, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -3,10 +3,10 @@ def encode(message):
     for char in message:
         if char.isalpha():
             ascii_offset = 65 if char.isupper() else 97
-            if char.isupper():
-                result += chr((ord(char) - ascii_offset + 2) % 26 + ascii_offset)
-            else:
+            if char.islower():
                 result += chr((ord(char) - ascii_offset + 2) % 26 + ascii_offset).lower()
+            else:
+                result += chr((ord(char) - ascii_offset + 2) % 26 + ascii_offset)
         else:
             result += char
     return result
