52c5e8f7f2028e4a6e9cc0c7ebf5d9f095bd0203
beep boop <githubbot@vadim.me>
2024-07-11 09:10:57 +0200

    Solution 33 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4lreuc04 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e1ff1642-c836-4f18-8095-e7c82b93e 614.py", line 18, in <module>     check(find_closest_elements)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e1ff1642-c836-4f18-8095-e7c82b93e 614.py", line 15, in check     assert find_closest_elements([1.1, 2.2, 3.1, 4.1, 5.1]) == (2.2, 3.1)            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: find_closest_elements() takes 0 positional arguments but 1 was given

--- Python_20.py
+++ Python_20.py
@@ -1,8 +1,13 @@
-def find_closest_elements():
+def find_closest_elements(ignored_input:tuple)-> tuple:
     while True:
         try:
             num1 = float(input("Enter the first number: "))
             num2 = float(input("Enter the second number: "))
-            return (num1, num2)
+            if abs(num1 - num2) < 0.00001:
+                return (num1, num2)
+            elif num1 < num2:
+                return (num1, num2)
+            else:
+                return (num2, num1)
         except ValueError:
             print("Invalid input. Please enter valid numbers.")
