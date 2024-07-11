f4d825c19b79301954662c7c66d9bf833155ef6b
beep boop <githubbot@vadim.me>
2024-07-11 03:17:38 +0200

    Solution 96 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/49frezuz Prompt: 4 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9fd955da-f9f8-4344-87fc-e4deee8e8 908.py", line 16, in <module>     check(double_the_difference)           ^^^^^^^^^^^^^^^^^^^^^ NameError: name 'double_the_difference' is not defined

--- Python_151.py
+++ Python_151.py
@@ -1,7 +1,5 @@
 def calculate_difference(lst):
-    def double_the_difference():
-        lst.sort()
-        return (lst[-1] - lst[0]) * 2
-    return double_the_difference() 
+    lst.sort()
+    return (lst[-1] - lst[0]) * 2
 
 print(calculate_difference([1, 2, 3]))
