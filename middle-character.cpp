9de1ba45bf264e6fb14635165b7b7db5e39575a8
beep boop <githubbot@vadim.me>
2024-07-11 09:40:54 +0200

    Solution 48 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/g0xumz8p Prompt: 40708f4c-bee3-4028-87a2-3db2ef5f4c93.cpp: In function ‘int main()’: 40708f4c-bee3-4028-87a2-3db2ef5f4c93.cpp:2:5: error: ‘string’ was not declared i n this scope     2 |     string s;       |     ^~~~~~ 40708f4c-bee3-4028-87a2-3db2ef5f4c93.cpp:3:5: error: ‘cin’ was not declared in t his scope     3 |     cin >> s;       |     ^~~ 40708f4c-bee3-4028-87a2-3db2ef5f4c93.cpp:3:12: error: ‘s’ was not declared in th is scope     3 |     cin >> s;       |            ^ 40708f4c-bee3-4028-87a2-3db2ef5f4c93.cpp:8:9: error: ‘cout’ was not declared in this scope     8 |         cout << s.substr(n/2 - 1, 2);       |         ^~~~ 40708f4c-bee3-4028-87a2-3db2ef5f4c93.cpp:10:9: error: ‘cout’ was not declared in  this scope    10 |         cout << s.substr(n/2, 1);       |         ^~~~

--- middle-character.cpp
+++ middle-character.cpp
@@ -4,11 +4,14 @@
 int main() {
     std::string s;
     std::cin >> s;
+    
     int n = s.size();
-    if (n % 2 == 1) {
-        std::cout << s[n / 2];
+    
+    if (n % 2 == 0) {
+        std::cout << s.substr(n/2 - 1, 2);
     } else {
-        std::cout << s.substr(n / 2 - 1, 2);
+        std::cout << s.substr(n/2, 1);
     }
+    
     return 0;
 }
