23666df9ed0135309f487538f72aa3b212d6673e
beep boop <githubbot@vadim.me>
2024-07-11 07:11:45 +0200

    Solution 22 of CPP/135, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/wyzw57ki Prompt: Complete the following code given the task description and function signature.

--- CPP_135.cpp
+++ CPP_135.cpp
@@ -1,9 +1,9 @@
 int can_arrange(vector<int> arr){
-    int largestIndex = -1;
+    int idx = -1;
     for (int i = 1; i < arr.size(); ++i) {
-        if (arr[i] < arr[i - 1]) {
-            largestIndex = i - 1;
+        if (arr[i] <= arr[i-1]) {
+            idx = i - 1;
         }
     }
-    return largestIndex;
+    return idx;
 }
