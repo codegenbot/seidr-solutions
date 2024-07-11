523220b1192e8b16dc92c143aae1ee7772932119
beep boop <githubbot@vadim.me>
2024-07-11 18:17:26 +0200

    Solution 14 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/5mxvjosi Prompt:                  from e3e9b1b1-eb1f-4b45-9a9d-30cbf4518c2c.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from e3e9b1b1-eb1f-4b45-9a9d-30cbf4518c2c.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e3e9b1b1-eb1f-4b45-9a9d-30cbf4518c2c.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from e3e9b1b1-eb1f-4b45-9a9d-30cbf4518c2c.cpp:17: e3e9b1b1-eb1f-4b45-9a9d-30cbf4518c2c.cpp: In function ‘int main()’: e3e9b1b1-eb1f-4b45-9a9d-30cbf4518c2c.cpp:27:31: error: invalid initialization of  reference of type ‘const std::vector<int>&’ from expression of type ‘std::pair< int, int>’    27 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ~~~~~~~~~~~^~~~~~ e3e9b1b1-eb1f-4b45-9a9d-30cbf4518c2c.cpp:13:37: note: in passing argument 1 of ‘ bool issame(const std::vector<int>&, const std::vector<int>&)’    13 | bool issame(const std::vector<int>& a, const std::vector<int>& b) {       |             ~~~~~~~~~~~~~~~~~~~~~~~~^

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,12 +1,22 @@
 #include <vector>
 #include <cassert>
 
-template<typename T>
-bool issame(const std::pair<T, T>& a, const std::pair<T, T>& b) {
+std::pair<int, int> sum_product(const std::vector<int>& numbers) {
+    int sum = 0;
+    int product = 1;
+    for (int num : numbers) {
+        sum += num;
+        product *= num;
+    }
+    return {sum, product};
+}
+
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
 int main() {
-    assert(issame(sum_product({10}), {10, 10}));
+    assert(issame(std::vector<int>{sum_product({10}).first, sum_product({10}).second}, {10, 10}));
+  
     return 0;
 }
