b5c9d510c0f8a32b06e4d56eae19fb69d0b433dd
beep boop <githubbot@vadim.me>
2024-07-11 10:57:33 +0200

    Solution 55 of Python/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mqn4uw0k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/37a2e6a1-2edd-43dc-b909-7cdf9e58b 83d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_63.py
+++ Python_63.py
@@ -1,9 +1,9 @@
+````
 phi = (1 + 5**0.5) / 2
 
 def fibfib(n: int):
     a, b = 0, 1
-    result = []
-    for _ in range(n):
-        result.append(b)
+    for _ in range(n-1):
         a, b = b, a + b
-    return result
+    return int(a * phi ** (n - 1)) % 10**9 + 1
+"""
