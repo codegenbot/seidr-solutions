ad529e1493627005b244aa57ec333b9887728495
beep boop <githubbot@vadim.me>
2024-07-11 17:06:05 +0200

    Solution 7 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1ntz4uj4 Prompt: r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <string>   +++ |+#include <cassert>     3 | 6de50058-0b4a-444e-8e86-04a8d4ddecb3.cpp: At global scope: 6de50058-0b4a-444e-8e86-04a8d4ddecb3.cpp:41:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    41 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 6de50058-0b4a-444e-8e86-04a8d4ddecb3.cpp:6:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     6 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ 6de50058-0b4a-444e-8e86-04a8d4ddecb3.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 6de50058-0b4a-444e-8e86-04a8d4ddecb3.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,13 +1,16 @@
 #include <vector>
 #include <string>
+#include <cassert>
+
+using namespace std;
 
 bool issame(vector<string> a, vector<string> b) {
     int sum1 = 0;
-    for (const std::string& s : a) {
+    for (const string& s : a) {
         sum1 += s.length();
     }
     int sum2 = 0;
-    for (const std::string& s : b) {
+    for (const string& s : b) {
         sum2 += s.length();
     }
     return sum1 == sum2;
@@ -16,11 +19,11 @@ bool issame(vector<string> a, vector<string> b) {
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     if (issame(lst1, lst2)) return lst1;
     int sum1 = 0;
-    for (const std::string& s : lst1) {
+    for (const string& s : lst1) {
         sum1 += s.length();
     }
     int sum2 = 0;
-    for (const std::string& s : lst2) {
+    for (const string& s : lst2) {
         sum2 += s.length();
     }
     if (sum1 < sum2) return lst1;
@@ -28,7 +31,9 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     else return lst1;
 }
 
-int main() {
-    assert(issame(total_match({"this"}, {}), {}));
+int main_func() {
+    vector<string> a = {"this"};
+    vector<string> b = {};
+    assert(issame(total_match(a, b), {}));
     return 0;
 }
