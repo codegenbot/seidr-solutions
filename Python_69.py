615d1069d93a3fa4502bf453a28a2de83ba109e9
beep boop <githubbot@vadim.me>
2024-07-11 22:51:19 +0200

    Solution 9 of Python/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pujdmso6 Prompt: Complete the following code given the task description and function signature.

--- Python_69.py
+++ Python_69.py
@@ -1,13 +1,13 @@
 ```
 def search(lst):
-    frequency_dict = {}
+    freq_dict = {}
     for num in lst:
-        if num in frequency_dict:
-            frequency_dict[num] += 1
+        if num in freq_dict:
+            freq_dict[num] += 1
         else:
-            frequency_dict[num] = 1
+            freq_dict[num] = 1
     max_num = -1
-    for key, value in frequency_dict.items():
-        if key > 0 and value >= key:
-            max_num = key
+    for num, freq in freq_dict.items():
+        if num > 0 and freq >= num:
+            max_num = num
     return max_num
