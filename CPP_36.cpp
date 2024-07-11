291271f2b901bc00082a9e4e6b0980481fecd87b
beep boop <githubbot@vadim.me>
2024-07-11 12:53:48 +0200

    Solution 11 of CPP/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/jtb1pit3 Prompt: Complete the following code given the task description and function signature.

--- CPP_36.cpp
+++ CPP_36.cpp
@@ -1,6 +1,6 @@
 int count = 0;
-    for (int i = 0; i < n; i++) {
-        if ((i % 11 == 0 || i % 13 == 0) && i % 10 == 7) {
+    for(int i = 1; i < n; i++){
+        if((i % 11 == 0 || i % 13 == 0) && i % 10 == 7){
             count++;
         }
     }
