964958231b5d1be35dc3420c36dd988d019e7583
beep boop <githubbot@vadim.me>
2024-07-11 07:16:37 +0200

    Solution 2 of Python/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/sfm4qxzd Prompt: Complete the following code given the task description and function signature.

--- Python_128.py
+++ Python_128.py
@@ -3,9 +3,11 @@ def prod_signs(arr):
     if not arr:
         return None
     sign_product = 1
+    magnitude_sum = 0
     for num in arr:
         if num < 0:
             sign_product *= -1
         elif num > 0:
             pass
-    return sum(abs(num) for num in arr) * sign_product
+        magnitude_sum += abs(num)
+    return sign_product * magnitude_sum
