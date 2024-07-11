ac71d9bad645c27824f0565c7a11eca87744a56c
beep boop <githubbot@vadim.me>
2024-07-12 01:57:55 +0200

    Solution 50 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/s2w6ri6b Prompt: in this scope     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ aa7d5f8c-54b0-43ec-8b75-bef6ed3d0c5b.cpp:9:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from aa7d5f8c-54b0-43ec-8b75-bef6ed3d0c5b.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from aa7d5f8c-54b0-43ec-8b75-bef6ed3d0c5b.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ aa7d5f8c-54b0-43ec-8b75-bef6ed3d0c5b.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from aa7d5f8c-54b0-43ec-8b75-bef6ed3d0c5b.cpp:8: aa7d5f8c-54b0-43ec-8b75-bef6ed3d0c5b.cpp: In function ‘int main()’: aa7d5f8c-54b0-43ec-8b75-bef6ed3d0c5b.cpp:18:20: error: ‘pluck’ was not declared in this scope    18 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -2,22 +2,14 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return std::equal(a.begin(), a.end(), b.begin(), b.end());
-}
-
-std::vector<int> pluck(std::vector<int> nums) {
+std::vector<int> pluck(std::vector<int> v) {
     std::vector<int> result;
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] % 2 != 0) {
-            result.push_back(nums[i]);
-        }
+    for (int i = 0; i < v.size(); i+=2) {
+        result.push_back(v[i]);
     }
     return result;
 }
 
-int main() {
-    assert(issame(pluck({7, 9, 7, 1}), {7, 9, 7, 1}));
-    
-    return 0;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
 }
