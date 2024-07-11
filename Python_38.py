92cb1ff9ba9199a333e2a17576a4d97c162c57eb
beep boop <githubbot@vadim.me>
2024-07-11 20:05:18 +0200

    Solution 37 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/oxh92hfa Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6b515791-9591-451f-a8d8-715d680e5 63e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_38.py
+++ Python_38.py
@@ -1,4 +1,17 @@
 def encode_cyclic(s: str):
+    result = ""
+    i = 0
+    while i < len(s):
+        if i + 2 < len(s) and s[i] == s[i + 1]:
+            result += s[i] * 3
+            i += 3
+        else:
+            result += s[i]
+            i += 1
+    return result
+
+
+def decode_cyclic(s: str):
     groups = []
     for i in range(0, (len(s) + 2) // 3):
         if i * 3 + 3 > len(s):
@@ -7,23 +20,3 @@ def encode_cyclic(s: str):
             groups.append(s[(i * 3) : min((i * 3) + 3, len(s))])
     groups = [(group[1:] + group[0]) if len(group) == 3 else group for group in groups]
     return "".join(groups)
-
-
-def decode_cyclic(s: str):
-    result = ""
-    i = 0
-    while i < len(s):
-        if len(s) - i >= 2:
-            if s[i + 1] == s[i]:
-                result += s[i] * 2 + s[i + 2]
-                i += 3
-            else:
-                result += s[i]
-                i += 1
-        elif len(s) - i == 1:
-            result += s[i]
-            i += 1
-        else:
-            result += s[i]
-            i += 1
-    return result
