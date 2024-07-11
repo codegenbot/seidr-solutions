987b4c69b3b3a19515422c0d0e1549548af367c0
beep boop <githubbot@vadim.me>
2024-07-11 11:58:54 +0200

    Solution 36 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6e857rqz Prompt:       |     ^~~~~~ 21d133b3-411d-417a-a726-dfb30f755e3a.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | using namespace std; 21d133b3-411d-417a-a726-dfb30f755e3a.cpp: At global scope: 21d133b3-411d-417a-a726-dfb30f755e3a.cpp:26:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    26 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 21d133b3-411d-417a-a726-dfb30f755e3a.cpp:4:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     4 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 21d133b3-411d-417a-a726-dfb30f755e3a.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 21d133b3-411d-417a-a726-dfb30f755e3a.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,50 +1,24 @@
 #include <vector>
-#include <iostream>
+#include <cassert>
 
-std::vector<int> compare(std::vector<int> game, std::vector<int> guess);
+using namespace std;
 
-int main() {
-    int n;
-    std::cin >> n;
-    std::vector<int> g, g1;
-    
-    for (int i = 0; i < n; ++i) {
-        int x;
-        std::cin >> x;
-        g.push_back(x);
-    }
-    
-    for (int i = 0; i < n; ++i) {
-        int x;
-        std::cin >> x;
-        g1.push_back(x);
-    }
-
-    std::vector<int> result = compare(g, g1);
-
-    for (int i = 0; i < result.size(); ++i) {
-        if (result[i] == 0)
-            std::cout << "M" << '\n';
-        else if (result[i] == 1)
-            std::cout << "A" << '\n';
-        else
-            std::cout << "B" << '\n';
-    }
-
-    return 0;
+bool issame(vector<int> a, vector<int> b) {
+    return (a.size() == b.size()) && equal(a.begin(), a.end(), b.begin());
 }
 
-std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
-    std::vector<int> result;
+vector<int> compare(vector<int> game, vector<int> guess) {
+    vector<int> result;
     for (int i = 0; i < game.size(); i++) {
         if (game[i] == guess[i]) {
             result.push_back(0);
         } else {
-            if (guess[i] == game[i] - 1 || guess[i] == game[i] + 1)
-                result.push_back(1);
-            else
-                result.push_back(abs(game[i] - guess[i]));
+            result.push_back(abs(game[i] - guess[i]));
         }
     }
     return result;
 }
+
+int main() {
+    assert(issame(compare({1, 2, 3, 5}, {-1, 2, 3, 4}), {2, 0, 0, 1}));
+    return 0;
