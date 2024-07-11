1bb57ef70ad74b06f34d8e87284d6548b06145c1
beep boop <githubbot@vadim.me>
2024-07-11 14:48:36 +0200

    Solution 71 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ofa8tsxx Prompt: Complete the following code given the task description and function signature.

--- Python_17.py
+++ Python_17.py
@@ -1,18 +1,18 @@
 ```
+from typing import List
+
+
 def parse_music(music_string: str) -> List[int]:
     music_list = []
     i = 0
     while i < len(music_string):
         if music_string[i] == 'o':
             music_list.append(4)
-            i += 1
-            continue
+            i += 5
         elif music_string[i:i+2] == 'o|':
             music_list.append(2)
             i += 2
-            continue
-        elif music_string[i:i+3] == '.|':
+        elif music_string[i:i+3] == '.| ':
             music_list.append(1)
             i += 3
-            continue
     return music_list
