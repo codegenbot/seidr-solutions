bedbfd859899b3c4aef7a48e66c767df5a970caf
beep boop <githubbot@vadim.me>
2024-07-11 12:52:13 +0200

    Solution 52 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gwf131aq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3df22836-419e-47c2-9450-01c610784 8a6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_14.py
+++ Python_14.py
@@ -1,8 +1,5 @@
 def all_prefixes(string: str) -> list[str]:
     if not isinstance(string, str) or len(string) == 0:
         return ['']
-    result = []
-    for i in range(len(string) + 1):
-        prefix = string[:i]
-        result.append(prefix)
+    result = [prefix for i in range(len(string) + 1) for prefix in (string[:i],)]
     return result
