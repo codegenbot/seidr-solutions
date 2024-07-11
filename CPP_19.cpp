a0b2603a0f4daa5c38ef3aabb79ba890440793bc
beep boop <githubbot@vadim.me>
2024-07-11 17:57:42 +0200

    Solution 8 of CPP/19, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/7xrljbza Prompt:       | ^~~ 405e0928-6409-4141-9c41-413bfafe22cf.cpp:11:17: error: ‘i’ does not name a type    11 | for (int i = 0; i <= 9; ++i) {       |                 ^ 405e0928-6409-4141-9c41-413bfafe22cf.cpp:11:25: error: expected unqualified-id b efore ‘++’ token    11 | for (int i = 0; i <= 9; ++i) {       |                         ^~ 405e0928-6409-4141-9c41-413bfafe22cf.cpp:21:1: error: expected unqualified-id be fore ‘return’    21 | return result;       | ^~~~~~ 405e0928-6409-4141-9c41-413bfafe22cf.cpp:22:1: error: expected declaration befor e ‘}’ token    22 | }       | ^ In file included from 405e0928-6409-4141-9c41-413bfafe22cf.cpp:24: 405e0928-6409-4141-9c41-413bfafe22cf.cpp: In function ‘int main()’: 405e0928-6409-4141-9c41-413bfafe22cf.cpp:26:13: error: ‘sort_numbers’ was not de clared in this scope    26 |     assert (sort_numbers("six five four three two one zero") == "zero on e two three four five six");       |             ^~~~~~~~~~~~

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,31 +1,28 @@
-map<string, int> number_map = {
-    {"zero", 0},
-    {"one", 1},
-    {"two", 2},
-    {"three", 3},
-    {"four", 4},
-    {"five", 5},
-    {"six", 6},
-    {"seven", 7},
-    {"eight", 8},
-    {"nine", 9}
-};
+#include <iostream>
+#include <map>
+
+using namespace std;
 
 string sort_numbers(string numbers) {
+    map<string, int> number_map = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
+    map<int, string> reverse_map;
+
+    for (auto const& pair : number_map) {
+        reverse_map[pair.second] = pair.first;
+    }
+
     string result;
-    map<int, string> sorted_numbers;
+    int count = 0;
 
-    size_t pos = 0;
-    while ((pos = numbers.find(" ")) != string::npos) {
-        string token = numbers.substr(0, pos);
-        sorted_numbers[number_map[token]] = token;
-        numbers.erase(0, pos + 1);
-    }
-    sorted_numbers[number_map[numbers]] = numbers;
-
-    for (const auto& pair : sorted_numbers) {
-        result += pair.second + " ";
+    for (int i = 0; i <= 9; i++) {
+        if (numbers.find(reverse_map[i]) != string::npos) {
+            if (count > 0) {
+                result += " ";
+            }
+            result += reverse_map[i];
+            ++count;
+        }
     }
 
-    return result.substr(0, result.size() - 1);
+    return result;
 }
