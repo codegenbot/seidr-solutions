6d3175efbdbff679b84c7bd7519c24d6ad2ad3dc
beep boop <githubbot@vadim.me>
2024-07-11 12:42:55 +0200

    Solution 15 of CPP/32, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5qrkowr6 Prompt: Complete the following code given the task description and function signature.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,8 +1,31 @@
-double find_zero(vector<double> xs){
-    int n = xs.size();
-    double sum = xs[n/2];
-    for(int i=1; i<n/2; i++){
-        sum -= xs[i]/(i*1.0);
+#include<stdio.h>
+#include<math.h>
+#include<vector>
+using namespace std;
+
+double poly(vector<double> xs, double x){
+    double sum=0;
+    int i;
+    for (i=0;i<xs.size();i++)
+    {
+        sum+=xs[i]*pow(x,i);
     }
-    return -sum;
+    return sum;
+}
+
+double find_zero(vector<double> xs){
+    if(xs.size() % 2 != 0) return -1.0; // Only works with even number of coefficients
+    double maxCoeff = 0.0;
+    int maxIndex = 0;
+    for(int i = 1; i < xs.size(); i+=2)
+    {
+        if(abs(xs[i]) > abs(maxCoeff))
+        {
+            maxCoeff = xs[i];
+            maxIndex = i;
+        }
+    }
+
+    double x = -maxCoeff / (xs[maxIndex]);
+    return x;
 }
