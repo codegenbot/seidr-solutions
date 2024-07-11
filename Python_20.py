b8c25610c4e5537244f40875320a1a384a6efa0c
beep boop <githubbot@vadim.me>
2024-07-11 08:29:33 +0200

    Solution 17 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4lreuc04 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e8f84949-91dd-448d-83cf-a895d2d4c f15.py", line 1, in <module>     def find_closest_elements() -> Tuple[float, float]:                                    ^^^^^ NameError: name 'Tuple' is not defined. Did you mean: 'tuple'?

--- Python_20.py
+++ Python_20.py
@@ -1,7 +1,7 @@
-from typing import Tuple
+```
+from typing import tuple
 
-
-def find_closest_elements() -> Tuple[float, float]:
+def find_closest_elements() -> tuple:
     numbers = [float(input("Enter a number: ")) for _ in range(2)]
     min_diff = float("inf")
     closest_pair = ()
