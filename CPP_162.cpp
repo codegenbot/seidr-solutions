24465b7f7b0b94edd376604ab4222975133cc856
beep boop <githubbot@vadim.me>
2024-07-11 04:50:57 +0200

    Solution 67 of CPP/162, ratio 0.25 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fcqijk62 Prompt: e3094b09-b8a2-4d4d-83e8-65de1ac82f30.cpp:1:10: fatal error: CryptoPP/sha1.h: No such file or directory     1 | #include "CryptoPP/sha1.h"       |          ^~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,29 +1,23 @@
+#include "cryptopp/sha1.h"
+
 #include <iostream>
 #include <sstream>
-#include <string>
-#include "cryptopp/sha1.h"
 
 int main() {
     std::cout << "Enter a string: ";
     std::string s;
     std::getline(std::cin, s);
 
+    std::stringstream ss;
     CryptoPP::SHA1 sha;
     unsigned char md[20];
     sha.Calculate((const unsigned char*)s.c_str(), (int)s.size(), md);
 
     for(int i = 0; i < 16; ++i) {
-        std::stringstream ss;
-        if(md[i] * 4 > 9)
-            ss << std::hex << std::showbase;
-        else
-            ss << std::hex << std::noshowbase;
-        ss << std::setfill('0') << std::setw(2) << (int)(md[i]);
+        ss << ((md[i] * 4 > 9 ? std::hex << std::showbase : std::hex << std::noshowbase)
+            << std::setfill('0') << std::setw(2) << (int)(md[i])) << " ";
     }
-    std::cout << "The SHA1 is: ";
-    for(int i = 0; i < 16; ++i)
-        std::cout << std::hex << (int)(md[i]) << " ";
-    std::cout << std::endl;
+    std::cout << "The SHA1 is: " << ss.str() << std::endl;
 
     return 0;
 }
