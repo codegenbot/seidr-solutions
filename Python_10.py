83e14ec897e394e76fe0e44d5f0ca4387ac3a201
beep boop <githubbot@vadim.me>
2024-07-11 15:22:16 +0200

    Solution 61 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tdfielxa Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f0396867-d12d-4ab6-b32b-e1a48b698 5f0.py", line 21, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f0396867-d12d-4ab6-b32b-e1a48b698 5f0.py", line 18, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,17 +1,13 @@
-```Python
 def make_palindrome(s: str) -> str:
-    s = s.lower()
     if s == s[::-1]:
         return s
-
-    result = list(s)
-    mid = len(result) // 2
-
-    for i in range(len(result) - 1, mid, -1):
-        while result[0] != result[i]:
-            if result[0] < result[i]:
-                result.append(result.pop(0))
-            else:
-                result.insert(0, result.pop())
-
-    return "".join(result)
+    t = list(s)
+    l, r = 0, len(t) - 1
+    while l < r:
+        if t[0] != t[r - l + 1]:
+            mid = (l + r) // 2
+            t.pop(r), t.insert(mid, t[0])
+            l, r = l + 1, len(t) - 1
+        else:
+            l, r = l + 1, r - 1
+    return "".join(t)
