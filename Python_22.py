ab49ed4dcc39c1d67561c287d6d49111eb3d5462
beep boop <githubbot@vadim.me>
2024-07-11 19:22:56 +0200

    Solution 76 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/933nqf72 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9712700c-8f74-42c0-af2b-fc26940b2 e87.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,8 +1,4 @@
-```
 from typing import List, Any
 
 def filter_integers(values: List[Any]) -> List[int]:
-    user_input = input("Enter a list of values (separated by spaces): ")
-    user_values = [value.strip() for value in user_input.split()]
-    
-    return [int(value) for value in user_values if isinstance(int(value), int) and 1 <= int(value) <= 100]
+    return [value for value in values if isinstance(value, int) and 1 <= value <= 100]
