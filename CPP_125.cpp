7db4a5ed7a958683feac7c3ce3ee7e4b7123085d
beep boop <githubbot@vadim.me>
2024-07-11 02:24:37 +0200

    Solution 3 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/cyujl28f Prompt: Complete the following code given the task description and function signature.

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -7,38 +7,25 @@ using namespace std;
 
 vector<string> split_words(string txt) {
     vector<string> result;
-    size_t pos = 0, prevPos = 0;
-
-    while ((pos = txt.find_first_of(" ,")) != string::npos) {
-        if (pos == 0) {
-            // Split on commas
-            if (txt[0] == ',') {
-                result.push_back(txt.substr(1));
-                break;
-            }
-        }
-
-        string word = txt.substr(prevPos, pos - prevPos);
-        if (!word.empty()) {
-            result.push_back(word);
-        }
-
-        prevPos = pos + 1;
-
-        if (pos == string::npos) {
-            // No more whitespaces or commas
-            size_t count = 0;
-            for (char c : txt) {
-                if (c >= 'a' && c <= 'z') {
-                    count++;
-                }
-            }
-
-            result.push_back(to_string(count));
-            break;
-        }
+    size_t pos = 0;
+    while ((pos = txt.find(' ')) != string::npos) {
+        result.push_back(txt.substr(0, pos));
+        txt.erase(0, pos + 1);
     }
-
+    if (txt.empty()) {
+        return result;
+    }
+    if (txt.find(',') == string::npos) {
+        int count = 0;
+        for (char c : txt) {
+            if (c >= 'a' && c <= 'z' && (count & 1)) {
+                count++;
+            }
+        }
+        result.push_back(to_string(count));
+    } else {
+        result.push_back(txt);
+    }
     return result;
 }
 
@@ -47,10 +34,8 @@ int main() {
     cout << "Enter a string: ";
     getline(cin, txt);
     vector<string> result = split_words(txt);
-
-    for (const auto& word : result) {
-        cout << word << endl;
+    for (string s : result) {
+        cout << s << endl;
     }
-
     return 0;
 }
