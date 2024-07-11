d63a6603989e67554dcb3b6d2bddf0a83d7788d3
beep boop <githubbot@vadim.me>
2024-07-11 08:45:57 +0200

    Solution 34 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9fdnc5i0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6e219c77-cb1d-4170-873c-36e3d76b0 612.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -12,8 +12,10 @@ def calculate_average_and_median() -> Tuple[float, float]:
         return 0.0, None
     average = sum(numbers) / len(numbers)
     sorted_numbers = sorted(numbers)
-    median = (sorted_numbers[len(sorted_numbers)//2] 
-             if len(sorted_numbers)%2 != 0 
-             else (sorted_numbers[len(sorted_numbers)//2 - 1] + sorted_numbers[len(sorted_numbers)//2]) / 2)
-    
+    median = (
+        sorted_numbers[len(sorted_numbers) // 2]
+        if len(sorted_numbers) % 2 != 0
+        else (sorted_numbers[len(sorted_numbers) // 2 - 1] + sorted_numbers[len(sorted_numbers) // 2]) / 2
+    )
+
     return average, median
