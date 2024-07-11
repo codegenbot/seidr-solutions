7a8866a03253d0f71c91bbf29b31bd7b8eb93e88
beep boop <githubbot@vadim.me>
2024-07-11 03:20:45 +0200

    Solution 53 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8kvpmmn6 Prompt: In file included from c39878fb-b4c3-497b-b2ea-e9ff9edb10c9.cpp:12: c39878fb-b4c3-497b-b2ea-e9ff9edb10c9.cpp: In function ‘int main()’: c39878fb-b4c3-497b-b2ea-e9ff9edb10c9.cpp:22:27: error: could not convert ‘compar e(std::vector<int>(std::initializer_list<int>{((const int*)(& const int [4]{1, 2 , 3, 5})), 4}, std::allocator<int>()), std::vector<int>(std::initializer_list<in t>{((const int*)(& const int [4]{-1, 2, 3, 4})), 4}, std::allocator<int>()))’ fr om ‘std::pair<int, int>’ to ‘std::vector<int>’    22 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~       |                           |       |                           std::pair<int, int>

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -6,12 +6,5 @@ using namespace std;
 pair<int, int> compare(vector<int> a, vector<int> b) {
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
-    
-    if(count(a.begin(), a.end(), 1) > count(b.begin(), b.end(), -1)){
-        return make_pair(1,0);
-    } else if(count(b.begin(), b.end(), -1) > count(a.begin(), a.end(), 1)) {
-        return make_pair(0,1);
-    } else {
-        return make_pair(0,0);
-    }
+    return {count(a.begin(), a.end(), 0), count(b.begin(), b.end(), 0)};
 }
