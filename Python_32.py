e4f0b76b50b617afc9577b9cca618e7b3767a065
beep boop <githubbot@vadim.me>
2024-07-11 21:19:58 +0200

    Solution 21 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zuf53w1d Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -6,7 +6,7 @@ def find_zero(xs: list):
         raise ValueError("xs must have even number of coefficients")
     a = xs[-1]
     b = -sum([coeff for i, coeff in enumerate(xs) if i % 2 == 1])
-    c = sum([coeff for i, coeff in enumerate(xs) if i % 4 == 3 and coeff != 0])
+    c = sum([coeff for i, coeff in enumerate(xs) if i % 4 == 3])
     d = -xs[0]
-    x = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+    x = (-b + math.sqrt(b**2 - 4*a*c)) / (2 * a)
     return round(x, 2)
