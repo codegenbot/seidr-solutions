ddf51e556b31fa20ead4c040a0707dba70a1bd3f
beep boop <githubbot@vadim.me>
2024-07-11 13:44:08 +0200

    Solution 28 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c4exytmv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3590ddc1-c2e3-4882-a36f-81ba2cd0a 925.py", line 29, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3590ddc1-c2e3-4882-a36f-81ba2cd0a 925.py", line 26, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,9 +1,10 @@
+```
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
         return string
 
     left = list(string)
-    right = list(string)[::-1]
+    right = "".join(list(reversed(string)))
 
     i, j = 0, len(right) - 1
     while i < len(left):
@@ -13,9 +14,8 @@ def make_palindrome(string: str) -> str:
                 j -= 1
             else:
                 left.insert(0, right[0])
-                j = 0
         else:
             i += 1
             j -= 1
 
-    return "".join(left + list(string))
+    return "".join(left) + right
