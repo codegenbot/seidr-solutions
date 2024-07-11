65c5cf3afa75d9693fd54746c7646a533ec066cb
beep boop <githubbot@vadim.me>
2024-07-11 17:46:46 +0200

    Solution 34 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d8lg90sn Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aab63d8e-98dd-41d9-8868-10ef3cdbb 465.py", line 13, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aab63d8e-98dd-41d9-8868-10ef3cdbb 465.py", line 10, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -5,7 +5,7 @@ def fruit_distribution(s, n):
     oranges = 0
     for i in range(len(words)):
         if words[i].lower() == "apples":
-            apples += int("".join(filter(str.isdigit, " ".join(words[:i]))))
+            apples += int("".join(filter(str.isdigit, " ".join(words[:i+1]))))
         elif words[i].lower() == "oranges":
             oranges += int("".join(filter(str.isdigit, " ".join(words[:i+1]))))
     return max(0, n - min(apples, oranges))
