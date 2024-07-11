fbeaf87b6896bcad37ab76c9dd8eb53015e7ee67
beep boop <githubbot@vadim.me>
2024-07-11 15:08:41 +0200

    Solution 8 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/d31cl2gl Prompt:       |                    ^~~ e7113f16-285f-4a45-83a9-7f4c488cca7b.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e7113f16-285f-4a45-83a9-7f4c488cca7b.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e7113f16-285f-4a45-83a9-7f4c488cca7b.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e7113f16-285f-4a45-83a9-7f4c488cca7b.cpp:15: e7113f16-285f-4a45-83a9-7f4c488cca7b.cpp: In function ‘int main()’: e7113f16-285f-4a45-83a9-7f4c488cca7b.cpp:25:20: error: ‘make_a_pile’ was not dec lared in this scope    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ e7113f16-285f-4a45-83a9-7f4c488cca7b.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,17 +1,17 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b);
-
-vector<int> make_a_pile(int n){
-    std::vector<int> result;
-    result.push_back(n);
-    for (int i = 1; i < n; ++i) {
-        if (n % 2 == 0) {
-            n += 2;
-        } else {
-            n += 1;
-        }
-        result.push_back(n);
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size())
+        return false;
+    
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i])
+            return false;
     }
-    return result;
+    
+    return true;
+}
+
+int main() {
+    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
 }
