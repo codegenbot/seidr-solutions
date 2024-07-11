9fc9c7278ed59630d9c2efeb2918d5c7ee9a4138
beep boop <githubbot@vadim.me>
2024-07-11 16:10:43 +0200

    Solution 41 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lgvazghp Prompt: Program did not receive expected input

--- Python_25.py
+++ Python_25.py
@@ -2,21 +2,21 @@ from typing import List
 import math
 
 
-def factorize(num: int) -> List[int]:
+def factorize(number: int) -> List[int]:
     factors = []
     i = 2
-    while i * i <= num:
-        if num % i:
+    while i * i <= number:
+        if number % i:
             i += 1
         else:
             count = 0
-            while num % i == 0:
-                num //= i
+            while number % i == 0:
+                number //= i
                 count += 1
             factors.append(i ** count)
             i = 2  
-    if num > 1:
-        factors.append(num)
+    if number > 1:
+        factors.append(number)
     return factors
 
 print(factorize(int(input("Enter a number: "))))
