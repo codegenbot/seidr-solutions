971f29fa6a357c44a610fbdc2ecaec9fa0a09b1b
beep boop <githubbot@vadim.me>
2024-07-11 23:26:36 +0200

    Solution 3 of Python/149, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ng7hri2x Prompt: Complete the following code given the task description and function signature.

--- Python_149.py
+++ Python_149.py
@@ -1,4 +1,3 @@
-```
 def sorted_list_sum(lst):
-    even_length_words = [word for word in lst if len(word) % 2 == 0]
-    return sorted(even_length_words)
+    lst = [word for word in lst if len(word) % 2 == 0]
+    return sorted(set(lst), key=lambda x: (len(x), x))
