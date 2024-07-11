a80557134358e51d8357e7abe4ce8d9fff9e0679
beep boop <githubbot@vadim.me>
2024-07-11 11:26:28 +0200

    Solution 76 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/66vz1sa5 Prompt: 6e1b9d81-00cf-477b-a0ba-83721a21008e.cpp: In function ‘int main()’: 6e1b9d81-00cf-477b-a0ba-83721a21008e.cpp:29:9: error: ‘cout’ was not declared in  this scope    29 |         cout << "Subvector " << (i+1) << ": ";       |         ^~~~ 6e1b9d81-00cf-477b-a0ba-83721a21008e.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; 6e1b9d81-00cf-477b-a0ba-83721a21008e.cpp:33:17: error: ‘endl’ was not declared i n this scope    33 |         cout << endl;       |                 ^~~~ 6e1b9d81-00cf-477b-a0ba-83721a21008e.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,18 +1,14 @@
 #include <vector>
-#include <iostream> 
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int>& nums) {
     int n = nums.size();
     vector<int> left;
     vector<int> right;
+    
     for(int i=0; i<n-1; i++){
         if(abs(nums[i+1] - nums[i]) <= 0){
             left.push_back(nums[i]);
-            for(int j=i; j<n-1; j++){
-                left.push_back(nums[j+1]);
-                break;
-            }
             break;
         } else {
             left.push_back(nums[i]);
@@ -23,6 +19,9 @@ vector<vector<int>> cutVector(vector<int>& nums) {
     return {{left}, {right}};
 }
 
+#include <iostream>
+using namespace std;
+
 int main() {
     vector<int> nums = {1, 2, 3, 4, 5};
     vector<vector<int>> result = cutVector(nums);
@@ -31,7 +30,7 @@ int main() {
         for(int j=0; j<result[i].size(); j++) {
             cout << result[i][j] << " ";
         }
-        cout << endl;
+        cout << "\n";
     }
     return 0;
 }
