bad39456b0a5adfbf75d2de1520c3490a91838d3
beep boop <githubbot@vadim.me>
2024-07-11 16:25:33 +0200

    Solution 46 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c12oc588 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6ea6d266-8ca2-4be2-adac-c27b50800 d9d.py", line 43, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6ea6d266-8ca2-4be2-adac-c27b50800 d9d.py", line 13, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,10 +1,13 @@
+```
 def sum_squares(lst):
+    if not isinstance(lst, list):  
+        return "Invalid input. Please provide a list."
+    
     total_sum = 0
     for num in lst:
         if num > 0 and (num ** 0.5 == int(num ** 0.5)):
             total_sum += num ** 2
         else:
             total_sum += num
+    
     return total_sum
-
-print(sum_squares([1, 4, -3, 9, -10]))
