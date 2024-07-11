69a339b6216d68a0a30a66fffa8d8719d278420b
beep boop <githubbot@vadim.me>
2024-07-12 01:23:40 +0200

    Solution 22 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/s2w6ri6b Prompt:       |                    ^~~ 7e988ebe-fe53-4513-a2d5-c3788caf4b82.cpp:4:27: error: ‘vector’ was not declared in this scope     4 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7e988ebe-fe53-4513-a2d5-c3788caf4b82.cpp:4:34: error: expected primary-expressio n before ‘int’     4 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 7e988ebe-fe53-4513-a2d5-c3788caf4b82.cpp:3: 7e988ebe-fe53-4513-a2d5-c3788caf4b82.cpp: In function ‘int main()’: 7e988ebe-fe53-4513-a2d5-c3788caf4b82.cpp:13:20: error: ‘pluck’ was not declared in this scope    13 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 7e988ebe-fe53-4513-a2d5-c3788caf4b82.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~ 7e988ebe-fe53-4513-a2d5-c3788caf4b82.cpp:13:5: error: ‘__assert_fail’ was not de clared in this scope    13 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |     ^~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,13 +1,6 @@
 #include <vector>
-#include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    std::sort(a.begin(), a.end());
-    std::sort(b.begin(), b.end());
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
-}
-
-int main() {
-    assert(issame(std::vector<int>({7, 9, 7, 1}), std::vector<int>{}));
 }
