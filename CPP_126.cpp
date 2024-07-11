d05a342ffe32bc6f5acb097e18228d333f267727
beep boop <githubbot@vadim.me>
2024-07-11 07:31:40 +0200

    Solution 7 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/1apywzug Prompt: ore ‘for’     1 | for (int i = 1; i < lst.size(); i++) {       | ^~~ 6da0acaf-4178-4c0b-acc0-d9eed66ac42b.cpp:1:17: error: ‘i’ does not name a type     1 | for (int i = 1; i < lst.size(); i++) {       |                 ^ 6da0acaf-4178-4c0b-acc0-d9eed66ac42b.cpp:1:33: error: ‘i’ does not name a type     1 | for (int i = 1; i < lst.size(); i++) {       |                                 ^ 6da0acaf-4178-4c0b-acc0-d9eed66ac42b.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return true;       |     ^~~~~~ 6da0acaf-4178-4c0b-acc0-d9eed66ac42b.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from 6da0acaf-4178-4c0b-acc0-d9eed66ac42b.cpp:9: 6da0acaf-4178-4c0b-acc0-d9eed66ac42b.cpp: In function ‘int main()’: 6da0acaf-4178-4c0b-acc0-d9eed66ac42b.cpp:11:13: error: ‘is_sorted’ was not decla red in this scope    11 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ^~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -6,10 +6,10 @@ using namespace std;
 bool is_sorted(vector<int>& lst) {
     for (int i = 1; i < lst.size(); i++) {
         if (lst[i] <= lst[i - 1] || count(lst.begin(), lst.end(), lst[i]) > 1) {
-            return false;
+            return true;
         }
     }
-    return true;
+    return false;
 }
 
 int main() {
