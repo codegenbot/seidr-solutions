fba75e9c76481328e32eee1ee7c6d16bae1fab3f
beep boop <githubbot@vadim.me>
2024-07-11 23:57:07 +0200

    Solution 90 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/1ppleavh Prompt: ined here    16 | int main() {       |     ^~~~ In file included from a263f205-a61c-406d-82e6-39512efb658f.cpp:23: a263f205-a61c-406d-82e6-39512efb658f.cpp: In function ‘int main()’: a263f205-a61c-406d-82e6-39512efb658f.cpp:33:29: error: cannot bind non-const lva lue reference of type ‘std::vector<float>&’ to an rvalue of type ‘std::vector<fl oat>’    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from a263f205-a61c-406d-82e6-39512efb658f.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<float>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ a263f205-a61c-406d-82e6-39512efb658f.cpp:6:36: note:   initializing argument 1 o f ‘void sort_even(std::vector<float>&)’     6 | void sort_even(std::vector<float>& values) {       |                ~~~~~~~~~~~~~~~~~~~~^~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -3,8 +3,9 @@
 #include <algorithm>
 #include <cassert>
 
-void sort_even(std::vector<float>& values) {
-    std::sort(values.begin(), values.end(), [](float a, float b) {
+void sort_even(const std::vector<float>& values) {
+    std::vector<float> temp = values;
+    std::sort(temp.begin(), temp.end(), [](float a, float b) {
         return ((int)a % 2 == 0) && ((int)b % 2 == 0) && (a < b);
     });
 }
@@ -14,8 +15,8 @@ bool issame(std::vector<float> a, std::vector<float> b) {
 }
 
 int main() {
-    std::vector<float> input_values = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
-    sort_even(input_values);
-    assert(issame(input_values, {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    std::vector<float> test_values = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
+    sort_even(test_values);
+    assert(issame(test_values, {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
     return 0;
 }
