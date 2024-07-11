3dd75e59dd31dc3396b9df33bcad49dc685764d1
beep boop <githubbot@vadim.me>
2024-07-11 23:49:21 +0200

    Solution 26 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/nmo4hme9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb4dbeae-d30e-44b2-ab02-0026bbcd8 f62.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb4dbeae-d30e-44b2-ab02-0026bbcd8 f62.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,6 +1,4 @@
 def sort_even(l: list):
-    evens = sorted(
-        (x for i, x in enumerate(l) if i % 2 == 0), key=lambda x: (i // 2, x)
-    )
+    evens = sorted((x for i, x in enumerate(l) if i % 2 == 0), key=lambda x: (i // 2, x))
     odds = [x for i, x in enumerate(l) if i % 2 != 0]
     return evens + sorted(odds)
