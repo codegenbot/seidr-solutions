b837dbb896a19a9e0d3a05a5ff07815d28b8a311
beep boop <githubbot@vadim.me>
2024-07-11 18:33:28 +0200

    Solution 20 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/v9cxlkxs Prompt: k dqnt knqw whct tq wrktg Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7101a4e5-e42c-48fe-b1b4-ffdf9b7cc 7a5.py", line 19, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7101a4e5-e42c-48fe-b1b4-ffdf9b7cc 7a5.py", line 15, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,9 +2,9 @@ def encode(message):
     encoded_message = ""
     for char in message:
         if char.lower() in "aeiouAEIOU":
-            encoded_message += chr(ord(char) + 2).lower()
+            encoded_message += chr(ord(char) + 2).upper()
         else:
-            encoded_message += char.swapcase()
+            encoded_message += char.upper()
     return encoded_message.lower()
 
 print(encode("I DoNt KnOw WhAt tO WrItE")) # Output: "k dQnT kNqW wHcT Tq wRkTg"
