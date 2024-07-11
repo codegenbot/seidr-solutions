232e06a07fc05e33ed1cc2ce5e39faf071170e66
beep boop <githubbot@vadim.me>
2024-07-11 11:41:16 +0200

    Solution 1 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/54byhyxx Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4a8cf0ef-2fed-48c5-97f7-d6664422e 578.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_144.py
+++ Python_144.py
@@ -1,6 +1,11 @@
-```
 def simplify(x, n):
-    x_num, x_denom = map(int, x.split('/'))
-    n_num, n_denom = map(int, n.split('/'))
+    x_num, x_denom = map(int, [i for i in x.split('/')])
+    n_num, n_denom = map(int, [i for i in n.split('/')])
     
-    return (x_num * n_denom) % (x_denom * n_num) == 0
+    if (x_num / x_denom) * (n_num / n_denom) == 0:
+        return False
+    else:
+        gcd = lambda a, b: a if not b else gcd(b, a % b)
+        common_divisor = gcd(x_denom, n_denom)
+        
+        return str(x_num // common_divisor) + '/' + str(x_denom // common_divisor), str(n_num // common_divisor) + '/' + str(n_denom // common_divisor)
