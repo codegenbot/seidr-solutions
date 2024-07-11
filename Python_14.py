bb7302db423f6b0b43a501970d89ad23e180c36f
beep boop <githubbot@vadim.me>
2024-07-11 12:20:32 +0200

    Solution 29 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gwf131aq Prompt: Program did not receive expected input

--- Python_14.py
+++ Python_14.py
@@ -1,6 +1,8 @@
+```
 from typing import List
 
 def all_prefixes(string: str) -> List[str]:
+    result = []
     for i in range(len(string) + 1):
         prefix = string[:i]
         if len(prefix) == 0 or not (prefix in string):
