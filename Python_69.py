9fe82d1c2a007b4632189dc8a9d2fe45c7ab3924
beep boop <githubbot@vadim.me>
2024-07-11 02:39:05 +0200

    Solution 6 of Python/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bwvzrivq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/407ce038-13b7-4521-a82b-92901229a 25d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_69.py
+++ Python_69.py
@@ -1,7 +1,5 @@
-```
-def search(lst):
-    max_num = -1
-    for num in lst:
-        if num > max_num:
-            max_num = num
-    return max_num
+def find_first_non_repeating_character(s):
+    for char in s:
+        if s.count(char) == 1:
+            return char
+    return None
