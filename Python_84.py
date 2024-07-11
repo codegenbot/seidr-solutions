3fbeca183b6e9a89114f4002b7f39b289ce4ab5d
beep boop <githubbot@vadim.me>
2024-07-11 13:10:17 +0200

    Solution 55 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ipbpzph6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/592a34c6-2cf2-460e-bf72-d3ae0b59c d61.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,24 +1,14 @@
-def solve():
+def solve(binary):
     while True:
-        binary = (
-            input("Do you want to convert from binary (yes) or decimal (no)? ").lower()
-            == "yes"
-        )
         try:
             if binary:
-                N = int(input("Enter a {} number: ".format("binary")), 2)
+                N = int(input(f"Enter a {'binary' if binary else 'decimal'} number: "), 2 if binary else 10)
             else:
-                N = float(input("Enter a {} number: ".format("decimal")))
-            print(str(N))
-            cont = input("Do you want to continue? (yes/no) ")
-            if cont.lower() != "yes":
-                break
+                N = float(input(f"Enter a {'decimal' if not binary else 'binary'} number: "))
+            break
         except ValueError:
-            print(
-                "Invalid input! Please enter a valid {}".format(
-                    "binary" if binary else "decimal"
-                )
-            )
+            print(f"Invalid input! Please enter a valid {'binary' if binary else 'decimal'}")
 
+    print(str(N))
 
-solve()
+solve(True)
