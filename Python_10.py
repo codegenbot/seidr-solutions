e789d86cfb2146a99153305ade01a43a23a000c4
beep boop <githubbot@vadim.me>
2024-07-11 14:57:37 +0200

    Solution 98 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c4exytmv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c8cf5708-9b5f-4279-95c2-043ac7e1b 106.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,7 +1,27 @@
 def make_palindrome(s: str) -> str:
-    s = s.lower()
-    for i in range(len(s)):
-        for j in range(i + 1, len(s) + 1):
-            substring = s[i:j]
-            if substring == substring[::-1]:
-                return substring
+    if s == s[::-1]:
+        return s
+    left = list(s)
+    right = list(s)[::-1]
+    i, j = 0, len(right) - 1
+    while i < len(left):
+        if left[i] != right[j]:
+            if j > 0:
+                if left[0] == right[j-1]:
+                    left.insert(0, right[j])
+                    break
+                else:
+                    left.insert(0, right[j-1])
+                    j -= 1
+            else:
+                if len(left) >= 2 and left[0] != right[0]:
+                    left.insert(0, right[0])
+                else:
+                    left.insert(0, 'a')
+        else:
+            i += 1
+            j -= 1
+    while len(left) < len(s):
+        left.insert(0, s[-1])
+        s = s[:-1]
+    return "".join(left + list(s))
