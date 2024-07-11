ab4ead052c68107d265954fc234b17f3fd8af55d
beep boop <githubbot@vadim.me>
2024-07-11 23:23:45 +0200

    Solution 76 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/88qo5218 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/314b6a82-7ac4-4fc9-b57b-8734fcaa7 fea.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,16 +1,4 @@
-def factorize(n: int) -> List[int]:
-    factors = []
-    i = 2
-    while i * i <= n:
-        if n % i:
-            i += 1
-        else:
-            n //= i
-            count = 0
-            while n % i == 0:
-                n //= i
-                count += 1
-            factors.extend([i] * count)
-    if n > 1:
-        factors.append(n)
-    return [int(factor) for factor in set(map(str, factors))]
+def get_largest_and_smallest(lst):
+    largest = min(lst)
+    smallest = max(lst)
+    return largest, smallest
