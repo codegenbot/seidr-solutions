e775ffa819240030089faabec7e1e8de8cfc0c7b
beep boop <githubbot@vadim.me>
2024-07-11 17:08:29 +0200

    Solution 59 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/61yhdfz8 Prompt:       |                             ^~~ 87d50e0a-dad2-41c8-9493-5d856f79ba00.cpp:23:33: error: expected primary-expressi on before ‘char’    23 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ 87d50e0a-dad2-41c8-9493-5d856f79ba00.cpp:23:38: error: expected primary-expressi on before ‘int’    23 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ 87d50e0a-dad2-41c8-9493-5d856f79ba00.cpp:23:44: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from 87d50e0a-dad2-41c8-9493-5d856f79ba00.cpp:22: 87d50e0a-dad2-41c8-9493-5d856f79ba00.cpp: In function ‘int main()’: 87d50e0a-dad2-41c8-9493-5d856f79ba00.cpp:37:20: error: ‘histogram’ was not decla red in this scope    37 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ 87d50e0a-dad2-41c8-9493-5d856f79ba00.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,31 +1,4 @@
-#include <iostream>
-#include <map>
-
-using namespace std;
-
 map<char, int> histogram(string test);
-
 bool issame(const map<char, int>& a, const map<char, int>& b) {
     return a == b;
-}
-
-map<char, int> histogram(string test) {
-    map<char, int> result;
-    map<char, int> freq;
-    int maxCount = 0;
-
-    for (char c : test) {
-        if (c != ' ') {
-            freq[c]++;
-            maxCount = max(maxCount, freq[c]);
-        }
-    }
-
-    for (const auto& pair : freq) {
-        if (pair.second == maxCount) {
-            result[pair.first] = pair.second;
-        }
-    }
-
-    return result;
 }
