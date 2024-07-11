3518057f10c5dcabd601243161ed8efd10589178
beep boop <githubbot@vadim.me>
2024-07-12 01:33:11 +0200

    Solution 19 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/nxmnsqpn Prompt:       |                                     ^~~~~~ 3fea7af6-030c-46be-a605-9354aa5efaea.cpp:15:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 3fea7af6-030c-46be-a605-9354aa5efaea.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 3fea7af6-030c-46be-a605-9354aa5efaea.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 3fea7af6-030c-46be-a605-9354aa5efaea.cpp:15:44: error: ‘b’ was not declared in t his scope    15 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3fea7af6-030c-46be-a605-9354aa5efaea.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 3fea7af6-030c-46be-a605-9354aa5efaea.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -3,14 +3,14 @@
 #include <cassert>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if (a.size() != b.size()) return false;
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
+    return std::equal(a.begin(), a.end(), b.begin());
 }
 
 std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
     // Implement this function to convert numerical grades to letter grades
     // Return a vector of letter grades corresponding to the input numerical grades
+}
+
+int main() {
+    assert(issame(numerical_letter_grade({0, 0.7}), {"E", "D-"}));
 }
