a1ffe68d3b22c84257b466ae59b92ae107eada54
beep boop <githubbot@vadim.me>
2024-07-11 15:28:47 +0200

    Solution 40 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3783cyso Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/718a34a4-9436-4e26-81c4-234b0cc6b 950.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,2 +1,4 @@
-def modp(n: int, p: int):
-    return (n ** (1 / p)) % p
+def modp():
+    n = int(input("Enter number n: "))
+    p = int(input("Enter prime number p: "))
+    return n ** (1 / p) % p
