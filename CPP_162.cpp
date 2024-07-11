abc71eb14545d23436696e88521f0f9566e1d9cf
beep boop <githubbot@vadim.me>
2024-07-11 03:28:24 +0200

    Solution 40 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fcqijk62 Prompt: 332672ec-731e-4e64-9e18-20624a5b6451.cpp:4:10: fatal error: sha1.h: No such file  or directory     4 | #include "sha1.h"       |          ^~~~~~~~ compilation terminated.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,19 +1,24 @@
-#include "cryptopp/sha.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "cryptopp/sha1.h"
 
 int main() {
     std::cout << "Enter a string: ";
     std::string s;
     std::getline(std::cin, s);
     
+    std::stringstream ss;
     CryptoPP::SHA1 sha;
     unsigned char md5[16];
     sha.Calculate((const unsigned char*)s.c_str(), (int)s.size(), md5);
 
     for(int i = 0; i < 16; ++i) {
-        std::cout << std::hex << std::setfill('0') << std::setw(2) << (int)(md5[i]);
+        ss << ((md5[i] * 4 > 9 ? std::hex << std::showbase : std::hex << std::noshowbase)
+            << std::setfill('0') << std::setw(2) << (int)(md5[i]));
     }
 
-    std::cout << "The SHA1 is: " << std::endl;
+    std::cout << "The SHA1 is: " << ss.str() << std::endl;
     
     return 0;
 }
