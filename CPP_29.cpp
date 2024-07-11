b3fca888d999cc299b2a2db09b46a8ae811b5d29
beep boop <githubbot@vadim.me>
2024-07-11 12:29:18 +0200

    Solution 41 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xbr14c3t Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 2006c02d-b903-43d1-a75a-949169edd48b.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2006c02d-b903-43d1-a75a-949169edd48b.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2006c02d-b903-43d1-a75a-949169edd48b.cpp:11: 2006c02d-b903-43d1-a75a-949169edd48b.cpp: In function ‘int main()’: 2006c02d-b903-43d1-a75a-949169edd48b.cpp:21:20: error: ‘filter_by_prefix’ was no t declared in this scope    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 2006c02d-b903-43d1-a75a-949169edd48b.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,7 +1,4 @@
 ```cpp
-#include <vector>
-#include <string>
-
 bool issame(vector<string> a,vector<string>b){
     return a == b;
 }
@@ -9,9 +6,26 @@ bool issame(vector<string> a,vector<string>b){
 vector<string> filter_by_prefix(vector<string> strings, string prefix){
     vector<string> result;
     for(string s : strings) {
-        if(s.find(prefix) == 0) {
+        if(s.find(prefix) == 0 || issame(split(s,prefix), split(strings[0],prefix))) {
             result.push_back(s);
         }
     }
     return result;
+}
+
+vector<string> split(string str,string prefix){
+    vector<string> words;
+    string word = "";
+    for(char c : str) {
+        if(c == ' ') {
+            words.push_back(word);
+            word = "";
+        } else {
+            word += c;
+        }
+    }
+    if(!word.empty()) {
+        words.push_back(word);
+    }
+    return words;
 }
