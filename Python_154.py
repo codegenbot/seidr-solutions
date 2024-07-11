c8b4458df0d98964fbc6f7a42d516a4d64bbfcd3
beep boop <githubbot@vadim.me>
2024-07-11 03:02:07 +0200

    Solution 97 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sr9olerd Prompt: Program did not receive expected input

--- Python_154.py
+++ Python_154.py
@@ -2,9 +2,11 @@
 def cycpattern_check(a, b):
     if len(b) > len(a):
         return False
-    for i in range(len(a)):
-        if a[i:i+len(b)].casefold() == b.casefold():
-            return True
+    
+    for i in range(len(b), len(a)+1):
+        for j in range(i-len(b)+1):
+            if a[j:j+len(b)].casefold() == b.casefold():
+                return True
     return False
 
 def check():
@@ -12,5 +14,6 @@ def check():
     b = input("Enter the pattern to be checked: ")
     result = cycpattern_check(a, b)
     print("Is cyclic pattern present?" if result else "Cyclic pattern is not present")
+
 
 check()
