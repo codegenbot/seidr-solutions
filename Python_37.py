71bedb6d6b0485500f5e4cf94c3afd9b6ba82efc
beep boop <githubbot@vadim.me>
2024-07-11 02:19:14 +0200

    Solution 88 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/tmnr21o2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bfc3acb4-20bf-4b16-913d-f065b6c3a c99.py", line 10, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bfc3acb4-20bf-4b16-913d-f065b6c3a c99.py", line 5, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,12 +1,4 @@
 def sort_even(l: list):
-    evens = [x for x in l if x % 2 == 0]
-    sorted_evens = sorted(evens)
-    result = []
-    j = 0
-    for x in l:
-        if x % 2 == 0:
-            result.append(sorted_evens[j])
-            j += 1
-        else:
-            result.append(x)
-    return tuple(result)
+    even = [x for x in l if x % 2 == 0]
+    odd = [x for x in l if x % 2 != 0]
+    return sorted(odd) + sorted(even)
