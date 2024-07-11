99ef9e0dca3326b8757466d4ae75d17f968a30be
beep boop <githubbot@vadim.me>
2024-07-11 14:09:48 +0200

    Solution 75 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/6zqnr7oa Prompt: Program did not receive expected input

--- Python_38.py
+++ Python_38.py
@@ -1,19 +1,21 @@
+```
 def decode_cyclic():
-    s = input("Enter the cyclic string: ")
-    result = ""
-    i = 0
-    if len(s) <= 1:
-        return s
-    while True:
-        if i == 0:
-            group = s[i] + ''.join([s[(i+1)%len(s)], s[:i+1][::-1][0]]) + s[i]
-        else:
-            group = s[i%len(s)] + ''.join([s[(i-1)%len(s)], s[:i][::-1][0]]) + s[i%len(s)]
-        if i > 0:
-            result += group[-1] + group[1:-1][::-1] + group[0]
-        else:
-            result = group
-        i = (i+1)%len(s)
-        if i == 0:
-            break
-    return result
+    try:
+        i = int(input("Enter the number of iterations: "))
+        s = input("Enter the cyclic string: ")
+        result = ""
+        if len(s) <= 1:
+            return s
+        while i > 0:
+            if i == 1:
+                group = s[i-1] + ''.join([s[0], next(reversed(s))[0]]) + s[i-1]
+            else:
+                group = s[(i-1)%len(s)] + ''.join([s[(i-2)%len(s)], next(reversed(s[(i-1)%len(s)]))]) + s[(i-1)%len(s)]
+            if i > 1:
+                result += group[-1] + group[1:-1][::-1] + group[0]
+            else:
+                result = group
+            i = (i-1)
+        return result
+    except Exception as e:
+        print(f"An error occurred: {e}")
