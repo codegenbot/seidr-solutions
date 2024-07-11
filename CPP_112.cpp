f154ce24cf71de70134bb24d7275f47a3996e28d
beep boop <githubbot@vadim.me>
2024-07-11 16:48:43 +0200

    Solution 51 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/z8h4hhts Prompt:       |                              ^~~~~~ e6d58350-d4d3-49b1-b92b-85c49385e1a9.cpp:14:37: error: ‘string’ was not declared  in this scope    14 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e6d58350-d4d3-49b1-b92b-85c49385e1a9.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e6d58350-d4d3-49b1-b92b-85c49385e1a9.cpp:14:45: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e6d58350-d4d3-49b1-b92b-85c49385e1a9.cpp:13: e6d58350-d4d3-49b1-b92b-85c49385e1a9.cpp: In function ‘int main()’: e6d58350-d4d3-49b1-b92b-85c49385e1a9.cpp:23:20: error: ‘reverse_delete’ was not declared in this scope    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ e6d58350-d4d3-49b1-b92b-85c49385e1a9.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,5 +1,24 @@
 #include <string>
+#include <vector>
+#include <algorithm>
+
+vector<string> reverse_delete(string s, string c) {
+    string result = "";
+    for (char ch : s) {
+        if (c.find(ch) == string::npos) {
+            result += ch;
+        }
+    }
+    string reverse_result = result;
+    reverse(reverse_result.begin(), reverse_result.end());
+    return {result, result == reverse_result ? "True" : "False"};
+}
 
 bool issame(vector<string> a, vector<string> b) {
-    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
+    return a == b;
+}
+
+int main() {
+    assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
+    return 0;
 }
