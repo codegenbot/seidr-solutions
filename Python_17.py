21d5bf2d49affc843bf1f5d10f0eeaf32f581b76
beep boop <githubbot@vadim.me>
2024-07-11 18:27:23 +0200

    Solution 13 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/77gn1yay Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/64182c18-557f-4496-ae61-49d9bbe1a 880.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,25 +1,15 @@
-```
 from typing import List
 
 
 def parse_music(music_string: str) -> List[int]:
-    notes = {'o': 4, 'o|': 2, '.|': 1}
+    notes = {"o": 4, "o|": 2, ".|": 1}
     beats = []
     i = 0
     while i < len(music_string):
-        if music_string[i] == '.':
-            if i+3 < len(music_string) and music_string[i+1] in notes:
-                beats.append(notes[music_string[i+1:i+3]])
-                i += 2
-            else:
-                beats.append(0)
-                i += 4
+        if music_string[i : i + 2] in ["..|", "ooo"]:
+            i += 3
         else:
-            note_type = music_string[i:i+2]
-            if note_type in notes:
-                beats.append(notes[note_type])
-                i += 2
-            else:
-                beats.append(0)
-                i += 2
+            note_type = music_string[i : i + 2]
+            i += 2
+            beats.append(notes[note_type])
     return beats
