59df2f078aa68fe0a0a4a9daba501fdbd8a000b4
beep boop <githubbot@vadim.me>
2024-07-11 23:38:18 +0200

    Solution 60 of square-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/ycexitug Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/683e663f-9510-4cd5-b319-71929d6de e88.py", line 1     ```     ^ SyntaxError: invalid syntax

--- square-digits.py
+++ square-digits.py
@@ -1,4 +1,3 @@
 ```
-if __name__ == '__main__':
-    def square_digits(n):
-        return ''.join(str(int(i) ** 2) for i in str(n))
+def square_digits(n):
+    return ''.join(str(int(i) ** 2) for i in str(n))
