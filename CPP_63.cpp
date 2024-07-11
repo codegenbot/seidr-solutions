7541971e1f71c3756dcfc14545786d98cf9e326c
beep boop <githubbot@vadim.me>
2024-07-12 00:10:35 +0200

    Solution 4 of CPP/63, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/dhdsmgyx Prompt:     9 |     for (int i = 3; i <= n; ++i) {       |     ^~~ 317016aa-4ccc-40db-976e-81cf25c50f8e.cpp:9:21: error: ‘i’ does not name a type     9 |     for (int i = 3; i <= n; ++i) {       |                     ^ 317016aa-4ccc-40db-976e-81cf25c50f8e.cpp:9:29: error: expected unqualified-id be fore ‘++’ token     9 |     for (int i = 3; i <= n; ++i) {       |                             ^~ 317016aa-4ccc-40db-976e-81cf25c50f8e.cpp:16:5: error: expected unqualified-id be fore ‘return’    16 |     return res;       |     ^~~~~~ 317016aa-4ccc-40db-976e-81cf25c50f8e.cpp:17:1: error: expected declaration befor e ‘}’ token    17 | }       | ^ In file included from 317016aa-4ccc-40db-976e-81cf25c50f8e.cpp:19: 317016aa-4ccc-40db-976e-81cf25c50f8e.cpp: In function ‘int main()’: 317016aa-4ccc-40db-976e-81cf25c50f8e.cpp:21:13: error: ‘fibfib’ was not declared  in this scope    21 |     assert (fibfib(14) == 927);       |             ^~~~~~

--- CPP_63.cpp
+++ CPP_63.cpp
@@ -1,10 +1,12 @@
-if (n == 0 || n == 1) {
-        return 0;
+
+int fibfib(int n) {
+    if (n == 0 || n == 1) {
+        return n;
     } else if (n == 2) {
         return 1;
     }
     
-    int a = 0, b = 0, c = 1, res = 0;
+    int a = 0, b = 1, c = 1, res = 0;
     
     for (int i = 3; i <= n; ++i) {
         res = a + b + c;
