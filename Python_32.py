f8ac03d77941a7177da39c84c482fbf3ed65b648
beep boop <githubbot@vadim.me>
2024-07-11 23:26:46 +0200

    Solution 16 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fvnu47iy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8d34136f-9116-4555-8e20-b771643f6 eb9.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,4 +1,4 @@
-Here is the completed code:
-
 def find_zero(xs: list):
-    return -xs[0] / xs[-1]
+    if len(xs) % 2 != 0:
+        raise ValueError("xs must have even number of coefficients")
+    return -xs[1] / xs[0]
