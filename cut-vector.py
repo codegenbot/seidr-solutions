36a8442a7133c0551496c0eda68cd5279324b221
beep boop <githubbot@vadim.me>
2024-07-11 05:13:47 +0200

    Solution 53 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/xtgfgfqy Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/09e8c018-4381-4512-9046-0320629ef 658.py", line 2, in <module>     arr = [int(input()) for _ in range(n)]           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/09e8c018-4381-4512-9046-0320629ef 658.py", line 2, in <listcomp>     arr = [int(input()) for _ in range(n)]            ^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: '1573 481 9099 8885 429 2609  886 7048 8140 7054 8503 572 6355 3728 638 997 1703 3933 8399'

--- cut-vector.py
+++ cut-vector.py
@@ -15,8 +15,8 @@ for i in range(len(arr)):
         min_diff = diff
         cut_index = i
 
-subvector1 = arr[: cut_index + 1]
-subvector2 = arr[cut_index + 1 :]
+subvector1 = arr[:cut_index + 1]
+subvector2 = arr[cut_index + 1:]
 
 print(*subvector1)
 print(*subvector2)
