783535ab3685a91026e4482ff7054ce16ec7a981
beep boop <githubbot@vadim.me>
2024-07-11 15:55:19 +0200

    Solution 30 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c12oc588 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/41692c20-b576-4733-9c0e-50b46c355 a3e.py", line 43, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/41692c20-b576-4733-9c0e-50b46c355 a3e.py", line 13, in check     sum_squares(   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/41692c20-b576-4733-9c0e-50b46c355 a3e.py", line 4, in sum_squares     if num ** 0.5 == int(num ** 0.5):                      ^^^^^^^^^^^^^^^ TypeError: int() argument must be a string, a bytes-like object or a real number , not 'complex'

--- Python_142.py
+++ Python_142.py
@@ -1,9 +1,11 @@
+```
+import math
+
 def sum_squares(lst):
     total_sum = 0
     for num in lst:
-        if isinstance(num, (int, float)):
-            if num ** 0.5 == int(num ** 0.5):
-                total_sum += num ** 2
-            else:
-                total_sum += num
+        if math.isqrt(num) ** 2 == num:
+            total_sum += num ** 2
+        else:
+            total_sum += num
     return total_sum
