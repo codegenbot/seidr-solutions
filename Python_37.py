c03578dc4bdd20976bfc2c45d1de8361171f18ad
beep boop <githubbot@vadim.me>
2024-07-11 13:17:54 +0200

    Solution 16 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/xc3n7t3j Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6f6bd73b-7c31-414d-a344-ac3b743ef e45.py", line 19, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6f6bd73b-7c31-414d-a344-ac3b743ef e45.py", line 14, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,13 +1,12 @@
-```
 def sort_even(l: list):
     evens = [x for x in l if x % 2 == 0]
-    sorted_evens = sorted([abs(x) for x in evens])
+    sorted_evens = sorted(evens)
     result = []
-    even_index = 0
+    evens_index = 0
     for num in l:
         if num % 2 == 0:
-            result.append(sorted_evens[even_index] * (1 if num >= 0 else -1))
-            even_index += 1
+            result.append(sorted_evens[evens_index])
+            evens_index += 1
         else:
             result.append(num)
     return result
