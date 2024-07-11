c954ec226bdba84fdee887da41530566681b2402
beep boop <githubbot@vadim.me>
2024-07-11 16:36:48 +0200

    Solution 35 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/aexjympc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7746cda2-c21b-4249-92cb-e4bccce00 2c5.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7746cda2-c21b-4249-92cb-e4bccce00 2c5.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,4 +1,8 @@
 def sort_even(l: list):
     evens = [x for x in l if x % 2 == 0]
     sorted_evens = sorted(evens)
-    return [x if i % 2 != 0 else y for i, x in enumerate(l) for y in (sorted_evens if i < len(evens) else [x])]
+    return [
+        x if i % 2 != 0 else y
+        for i, x in enumerate(l)
+        for y in (sorted_evens if i < len(evens) else [x])
+    ]
