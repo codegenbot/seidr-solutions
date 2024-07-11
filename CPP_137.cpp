c2c06d9c289174e0da28d7b8c7f523214d213198
beep boop <githubbot@vadim.me>
2024-07-11 03:43:53 +0200

    Solution 11 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/497575qq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,57 +1,54 @@
-#include <string>
-#include <algorithm>
-#include <boost/any.hpp>
-#include <boost/lexical_cast.hpp>
-
-using namespace std;
-
+```
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return boost::any(b);
+        return boost::any_cast<float>(b) > boost::any_cast<int>(a) ? b : a;
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return boost::any_cast<double>(b) > boost::any_cast<int>(a) ? b : a;
     }
     else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return boost::any(a);
+        return boost::any_cast<float>(a) > boost::any_cast<int>(b) ? a : b;
+    }
+    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return boost::any_cast<double>(a) > boost::any_cast<int>(b) ? a : b;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        string str = boost::any_cast<string>(a);
+        float f = boost::any_cast<float>(b);
+        return stof(str) > f ? a : b;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        string str = boost::any_cast<string>(a);
+        double d = boost::any_cast<double>(b);
+        return stod(str) > d ? a : b;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        string str = boost::any_cast<string>(a);
+        int i = boost::any_cast<int>(b);
+        return stof(str) > i ? a : b;
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        float f = boost::any_cast<float>(a);
+        string str = boost::any_cast<string>(b);
+        return f > stof(str) ? a : b;
+    }
+    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
+        double d = boost::any_cast<double>(a);
+        string str = boost::any_cast<string>(b);
+        return d > stod(str) ? a : b;
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        int i = boost::any_cast<int>(a);
+        string str = boost::any_cast<string>(b);
+        return i > stoi(str) ? a : b;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string strA = boost::any_cast<string>(a);
-        string strB = boost::any_cast<string>(b);
-        if (strA > strB) {
-            return a;
-        }
-        else if (strA < strB) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
+        string str1 = boost::any_cast<string>(a);
+        string str2 = boost::any_cast<string>(b);
+        return stod(str1) > stod(str2) ? a : b;
     }
-    else if (a.type() == typeid(string) && (b.type() == typeid(int) || b.type() == typeid(float))) {
-        string strA = boost::any_cast<string>(a);
-        float numB = boost::any_cast<float>(b);
-        if (strA > to_string(numB)) {
-            return a;
-        }
-        else if (strA < to_string(numB)) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
+    else if (boost::any_cast<float>(a) == boost::any_cast<float>(b)) {
+        return "None";
     }
-    else if ((a.type() == typeid(int) || a.type() == typeid(float)) && b.type() == typeid(string)) {
-        float numA = boost::any_cast<float>(a);
-        string strB = boost::any_cast<string>(b);
-        if (to_string(numA) > strB) {
-            return a;
-        }
-        else if (to_string(numA) < strB) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
-    }
-    else {
-        throw invalid_argument("Invalid input type");
-    }
+    return a;
 }
