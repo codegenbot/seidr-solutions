dc1dd7fda78afceccd93182960a8dde08400c843
beep boop <githubbot@vadim.me>
2024-07-11 10:03:28 +0200

    Solution 78 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7s9yk3dz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51569ce7-5ffc-46bb-b106-7fd4a91fb 449.py", line 11, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51569ce7-5ffc-46bb-b106-7fd4a91fb 449.py", line 8, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_26.py
+++ Python_26.py
@@ -1,6 +1,4 @@
 def remove_duplicates(input_string):
     if isinstance(input_string, list):
-        input_string = ','.join(map(str, input_string))
-    
-    input_list = [int(x) for x in input_string.split(",")]
-    return [x for x in set(input_list)]
+        return ','.join(map(str, set(input_string)))
+    return ','.join(map(str, set(int(x) for x in input_string.split(","))))
