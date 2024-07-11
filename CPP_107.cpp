d484f6fecabd9488e821b8bd25a4ba99a46e7128
beep boop <githubbot@vadim.me>
2024-07-11 06:56:56 +0200

    Solution 38 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/no1lvps7 Prompt:       |             ^~~~~~ ede522e3-7c27-4bab-be58-0e9fa028f0df.cpp:28:20: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ ede522e3-7c27-4bab-be58-0e9fa028f0df.cpp:28:27: error: ‘vector’ was not declared  in this scope    28 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ede522e3-7c27-4bab-be58-0e9fa028f0df.cpp:28:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from ede522e3-7c27-4bab-be58-0e9fa028f0df.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from ede522e3-7c27-4bab-be58-0e9fa028f0df.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ede522e3-7c27-4bab-be58-0e9fa028f0df.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -2,24 +2,24 @@
 #include <vector>
 
 std::vector<int> even_odd_palindrome(int n) {
-    std::vector<int> result = {0, 0};
-    for (int i = 1; i <= n; ++i) {
-        std::string num = std::to_string(i);
-        std::string rev_num = num;
-        std::reverse(rev_num.begin(), rev_num.end());
-        if (num == rev_num) {
-            if (i % 2 == 0) {
-                result[0]++;
-            } else {
-                result[1]++;
-            }
-        }
-    }
-    return result;
+std::vector<int> result = {0, 0};
+for (int i = 1; i <= n; ++i) {
+std::string num = std::to_string(i);
+std::string rev_num = num;
+std::reverse(rev_num.begin(), rev_num.end());
+if (num == rev_num) {
+if (i % 2 == 0) {
+result[0]++;
+} else {
+result[1]++;
+}
+}
+}
+return result;
 }
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
+return a == b;
 }
 
 assert(issame(even_odd_palindrome(1), {0, 1}));
