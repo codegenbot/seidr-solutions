693172f2120778f6560a06da5ce6312512cd2f73
beep boop <githubbot@vadim.me>
2024-07-11 02:07:02 +0200

    Solution 90 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xrg5ajon Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/02ff0c73-2088-4934-bd88-83f4553ce 027.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -1,3 +1,4 @@
+```
 def match_parens(lst):
     stack = []
     for s in lst:
@@ -12,4 +13,5 @@ def match_parens(lst):
 
 user_input = input("Enter a list of strings: ")
 lst = [s.strip() for s in user_input.split(',')]
-print(match_parens(lst))
+parsed_lst = [[c for c in l.split()] for l in lst]
+print(match_parens(parsed_lst))
