8dcbfab68e3b64fb16f14a8d654b9f533f183f80
beep boop <githubbot@vadim.me>
2024-07-11 02:55:14 +0200

    Solution 47 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/2ykzqvlr Prompt: Complete the following code given the task description and function signature.

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,8 +1,9 @@
 if(array.empty()) return array;
-    vector<int> sorted_array = array;
-    if((sorted_array.front() + sorted_array.back()) % 2 == 0)
-        sort(sorted_array.rbegin(), sorted_array.rend());
+    
+    if((array.front() + array.back()) % 2 == 0)
+        sort(array.rbegin(), array.rend());
     else
-        sort(sorted_array.begin(), sorted_array.end());
-    return sorted_array;
+        sort(array.begin(), array.end());
+    
+    return array;
 }
