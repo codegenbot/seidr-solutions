2a9d74cf13d34968cfe6b185dc584a441ffc2094
beep boop <githubbot@vadim.me>
2024-07-11 15:28:09 +0200

    Solution 88 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ovcg7qmp Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f3b4610-8dde-4f44-b549-5d0c01882 57d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_38.py
+++ Python_38.py
@@ -1,42 +1,7 @@
 def decode_cyclic(s: str):
     s = s * (3 - len(s) % 3)
     result = ""
-    temp = ""
-    for char in s + s:
-        temp += char
-        if len(temp) == 3:
-            if len(result) > 0 and result[-1] == temp[0]:
-                result += temp[1:]
-            elif temp[0] == temp[2]:
-                if temp[2] == "(":
-                    result += "(" + temp[1:-1]
-                else:
-                    result += temp[1:-1] + temp[0]
-            else:
-                if len(temp) % 3 == 1 and temp[0] == result[-1]:
-                    result += temp[1:-1] + temp[0]
-                else:
-                    if len(result) > 0 and result[-1] == temp[0]:
-                        result += temp
-                    elif temp[0] == temp[2]:
-                        if temp[2] == "(":
-                            result += "(" + temp[1:]
-                        else:
-                            result += temp[1:] + temp[0]
-                    else:
-                        result += temp
-            temp = ""
-    if len(temp) % 3 == 1 and temp[0] == result[-1]:
-        result += temp[1:-1] + temp[0]
-    elif len(temp) > 0:
-        if len(result) > 0 and temp[0] == result[-1]:
-            result += temp
-        else:
-            if len(temp) % 3 == 1 and temp[0] == temp[2]:
-                if temp[2] == "(":
-                    result += "(" + temp[1:]
-                else:
-                    result += temp[1:] + temp[0]
-            else:
-                result += temp
+    for char in s:
+        if char not in result:
+            result += char
     return result
