6449627ed1edf55782258e356d9b98f4ad964fac
beep boop <githubbot@vadim.me>
2024-07-11 02:39:29 +0200

    Solution 60 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/wgqv5w84 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48eb55d7-732d-44ae-a40f-59426745c 494.py", line 18, in <module>     check(solution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48eb55d7-732d-44ae-a40f-59426745c 494.py", line 13, in check     assert solution([3, 13, 2, 9]) == 3            ^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48eb55d7-732d-44ae-a40f-59426745c 494.py", line 3, in solution     lst = list(map(int, x.split()))                         ^^^^^^^ AttributeError: 'list' object has no attribute 'split'

--- Python_121.py
+++ Python_121.py
@@ -1,9 +1,13 @@
-def solution():
-    try:
-        lst = list(map(int, input().split()))
-    except ValueError:
-        return "Invalid input. Please enter integers only."
-    
-    if not lst:
-        return 0
-    return sum(1 for _ in lst[::2]) / len(lst)
+```
+def solution(x):
+    if isinstance(x, str):
+        try:
+            lst = list(map(int, x.split()))
+        except ValueError:
+            return "Invalid input. Please enter integers only."
+        
+        if not lst:
+            return 0
+        return sum(1 for _ in lst[::2]) / len(lst)
+    else:
+        return "Invalid input. Please enter integers only as a space-separated string."
