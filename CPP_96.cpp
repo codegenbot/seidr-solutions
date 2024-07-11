0b3603a34acd63a498416cfb8b6f43e9aef814ae
beep boop <githubbot@vadim.me>
2024-07-11 05:12:53 +0200

    Solution 10 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/8zvpzqkx Prompt:    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5fb6e05f-2d86-43c4-a71a-12f6e8425375.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5fb6e05f-2d86-43c4-a71a-12f6e8425375.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5fb6e05f-2d86-43c4-a71a-12f6e8425375.cpp:18: 5fb6e05f-2d86-43c4-a71a-12f6e8425375.cpp: In function ‘int main()’: 5fb6e05f-2d86-43c4-a71a-12f6e8425375.cpp:28:20: error: ‘count_up_to’ was not dec lared in this scope    28 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 5fb6e05f-2d86-43c4-a71a-12f6e8425375.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,26 +1,26 @@
-bool issame(vector<int> a, vector<int> b) {
+bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
-vector<int> count_up_to(int n) {
+vector<int> count_up_to(int n){
     vector<int> primes;
-    for (int i = 2; i < n; ++i) {
+    for(int i=2; i<n; ++i){
         bool is_prime = true;
-        for (int j = 2; j * j <= i; ++j) {
-            if (i % j == 0) {
+        for(int j=2; j*j<=i; ++j){
+            if(i % j == 0){
                 is_prime = false;
                 break;
             }
         }
-        if (is_prime) {
+        if(is_prime){
             primes.push_back(i);
         }
     }
     return primes;
 }
 
-int main() {
+int main(){
     assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-
+    
     return 0;
 }
