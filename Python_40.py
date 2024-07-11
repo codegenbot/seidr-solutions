e34d6cab04d9a4dad26c0ae42ee7f597efd94c10
beep boop <githubbot@vadim.me>
2024-07-11 13:55:04 +0200

    Solution 4 of Python/40, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/iwvltvsr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/43f0b72a-c117-4c57-b697-e94f17cb2 782.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_40.py
+++ Python_40.py
@@ -1,9 +1,7 @@
-```
 def triples_sum_to_zero(l: list):
-    n = len(l)
-    for i in range(n):
-        for j in range(i + 1, n):
-            for k in range(j + 1, n):
+    for i in range(len(l)):
+        for j in range(i + 1, len(l)):
+            for k in range(j + 1, len(l)):
                 if l[i] + l[j] + l[k] == 0:
                     return True
     return False
