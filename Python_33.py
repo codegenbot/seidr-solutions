fd533edbc64684d64a283433056910803d6ab2f7
beep boop <githubbot@vadim.me>
2024-07-11 09:54:33 +0200

    Solution 54 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/548a2c26-f5e7-4f04-936c-ae95bc098 2bf.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/548a2c26-f5e7-4f04-936c-ae95bc098 2bf.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,2 +1,5 @@
+```Python
 def sort_third(l: list):
-    return sorted([x for i, x in enumerate(l) if (i+1) % 3 != 0] + [y for i, y in enumerate(l) if (i+1) % 3 == 0])
+    third = [x for i, x in enumerate(l) if (i+1) % 3 == 0]
+    rest = sorted([x for i, x in enumerate(l) if (i+1) % 3 != 0])
+    return [x for x in sorted(third + rest)]
