7c5e60a0e8b809bf83bc65f8f7ab78c338c9a1ab
beep boop <githubbot@vadim.me>
2024-07-11 05:53:04 +0200

    Solution 44 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/l6shnh36 Prompt: 4a84b318-2a7e-4ca0-90dd-c22c27d255cd.cpp:1:6: error: ‘string’ in namespace ‘std’  does not name a type     1 | std::string result = "";       |      ^~~~~~ 4a84b318-2a7e-4ca0-90dd-c22c27d255cd.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | std::string result = ""; In file included from 4a84b318-2a7e-4ca0-90dd-c22c27d255cd.cpp:3: 4a84b318-2a7e-4ca0-90dd-c22c27d255cd.cpp: In function ‘int main()’: 4a84b318-2a7e-4ca0-90dd-c22c27d255cd.cpp:5:13: error: ‘int_to_mini_romank’ was n ot declared in this scope     5 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,15 +1,2 @@
 #include <string>
-using namespace std;
-
-string int_to_mini_romank(int n) {
-    if (n >= 1000)
-        return "m";
-    else
-        return "";
-}
-
-int main() {
-    string result = "";
-    assert (int_to_mini_romank(1000) == "m");
-    return 0;
-}
+std::string result = "";
