dc312e4a6c778a96a5e70174c83d9758f8e76c91
beep boop <githubbot@vadim.me>
2024-07-11 07:07:23 +0200

    Solution 40 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ex70v8cc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c6e02fed-d640-47e1-b9b7-78cb50a75 522.py", line 25, in <module>     check(valid_date)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c6e02fed-d640-47e1-b9b7-78cb50a75 522.py", line 22, in check     assert valid_date("04-2003") == False            ^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c6e02fed-d640-47e1-b9b7-78cb50a75 522.py", line 5, in valid_date     year = int(date_parts[2])                ~~~~~~~~~~^^^ IndexError: list index out of range

--- Python_124.py
+++ Python_124.py
@@ -1,22 +1,16 @@
 ```
 def valid_date(date):
     date_parts = date.split("-")
-    
+    if len(date_parts) == 2:
+        return False
     if len(date) != 9:
         return False
-    if len(date_parts) != 3:
-        return False
-    
-    month = int(date_parts[0])
-    day = int(date_parts[1])
-    year = int(date_parts[2])
-
-    if month < 1 or month > 12:
+    if int(date_parts[0]) < 1 or int(date_parts[0]) > 12:
         return False
     if (
-        (month in [1, 3, 5, 7, 8, 10, 12] and day > 31)
-        or (month in [4, 6, 9, 11] and day > 30)
-        or (month == 2 and day > 29)
+        (int(date_parts[0]) in [1, 3, 5, 7, 8, 10, 12] and int(date_parts[1]) > 31)
+        or (int(date_parts[0]) in [4, 6, 9, 11] and int(date_parts[1]) > 30)
+        or (int(date_parts[0]) == 2 and int(date_parts[1]) > 29)
     ):
         return False
 
