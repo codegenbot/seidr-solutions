cbb3d3eb2d4c6daa7ff99b616641a257b4267f73
beep boop <githubbot@vadim.me>
2024-07-11 17:08:42 +0200

    Solution 31 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/f755cb2g Prompt: 3df4c624-ab19-46fe-afe1-a91b9a837bf8.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int prod_signs(vector<int> arr){       |                ^~~~~~ 3df4c624-ab19-46fe-afe1-a91b9a837bf8.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int prod_signs(vector<int> arr){       |                       ^~~ In file included from 3df4c624-ab19-46fe-afe1-a91b9a837bf8.cpp:17: 3df4c624-ab19-46fe-afe1-a91b9a837bf8.cpp: In function ‘int main()’: 3df4c624-ab19-46fe-afe1-a91b9a837bf8.cpp:19:23: error: ‘prod_signs’ cannot be us ed as a function    19 |     assert (prod_signs({-1, 1, 1, 0}) == 0);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -2,16 +2,16 @@
 
 int prod_signs(std::vector<int> arr){
     if(arr.empty()) return -32768;
-
+    
     int signProduct = 1;
     int sumMagnitudes = 0;
-
+    
     for(int num : arr){
         if(num > 0) signProduct *= 1;
         else if(num < 0) signProduct *= -1;
-
+        
         sumMagnitudes += abs(num);
     }
-
+    
     return signProduct * sumMagnitudes;
 }
