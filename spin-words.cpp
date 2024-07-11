e7e208490229a261f3a2070a90230ff0a7d46f8a
beep boop <githubbot@vadim.me>
2024-07-11 21:11:18 +0200

    Solution 42 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ocjywund Prompt: 3addddf2-59c5-40d5-a226-dfe18228c8d7.cpp: In function ‘int main()’: 3addddf2-59c5-40d5-a226-dfe18228c8d7.cpp:39:14: error: expected ‘}’ at end of in put    39 |     return 0;       |              ^ 3addddf2-59c5-40d5-a226-dfe18228c8d7.cpp:34:12: note: to match this ‘{’    34 | int main() {       |            ^

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,29 +1,35 @@
-```cpp
+```
 #include <iostream>
+#include <algorithm>
 #include <string>
 
 using namespace std;
 
 string spinWords(string str) {
     string result = "";
-    string word;
-    int start = 0;
-    
-    for (int i = 0; i <= str.length(); i++) {
-        if (i == str.length() || str[i] == ' ') {
-            word = str.substr(start, i - start);
-            if (word.length() >= 5) {
-                string reversedWord = "";
-                for (int k = word.length() - 1; k >= 0; k--)
-                    reversedWord += word[k];
-                result += reversedWord + " ";
-            } else 
-                result += word + " ";
-            start = i + 1;
+    int i = 0;
+    while (i <= str.length()) {
+        if (str[i] == ' ') {
+            result += " ";
+            i++;
+            continue;
         }
+        int j = i;
+        while (j < str.length() && str[j] != ' ') {
+            j++;
+        }
+        string word = str.substr(i, j - i);
+        if (word.length() >= 5) {
+            for (int k = word.length() - 1; k >= 0; k--) {
+                result += word[k];
+            }
+            result += " ";
+        } else {
+            result += word + " ";
+        }
+        i = j;
     }
-    
-    return result.substr(0, result.length() - 1);
+    return result;
 }
 
 int main() {
@@ -31,5 +37,4 @@ int main() {
     cout << "Enter a string of words: ";
     getline(cin, str);
     cout << "Spun result: " << spinWords(str) << endl;
-    return 0;
-}
+    }
