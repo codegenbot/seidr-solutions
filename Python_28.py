49f22841f50736e6b2cd317907c54cf4b491e647
beep boop <githubbot@vadim.me>
2024-07-11 17:41:09 +0200

    Solution 11 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/mi6k06nw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4d3480b7-2b75-431e-89d8-438dba941 64c.py", line 7, in <module>     check(concatenate)           ^^^^^^^^^^^ NameError: name 'concatenate' is not defined

--- Python_28.py
+++ Python_28.py
@@ -1,9 +1,2 @@
 def concatenate_strings(strings):
     return "".join(strings)
-
-
-def check(concatenate_strings):
-    print(concatenate_strings(["Hello", "world"]))
-
-
-check(concatenate_strings)
