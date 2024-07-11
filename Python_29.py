d03aacbd22a4faad950fa883e24ab1f6b1d3c976
beep boop <githubbot@vadim.me>
2024-07-11 15:04:11 +0200

    Solution 76 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/q4phxnzk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d83a9671-b689-4573-8d8b-95dd8c16e 141.py", line 17, in <module>     check(filter_by_prefix)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d83a9671-b689-4573-8d8b-95dd8c16e 141.py", line 12, in check     assert filter_by_prefix(            ^^^^^^^^^^^^^^^^^ TypeError: filter_by_prefix() takes from 0 to 1 positional arguments but 2 were given

--- Python_29.py
+++ Python_29.py
@@ -1,8 +1,8 @@
-def filter_by_prefix(prefix='') -> None: 
-    while True: 
-        try: 
+def filter_by_prefix() -> None:
+    while True:
+        try:
             strings = input("Enter strings (comma-separated): ").split(",")
-            filtered_strings = [s.strip() for s in strings if len(strings) > 1 and s.strip().startswith(prefix)]
+            filtered_strings = [s.strip() for s in strings if len(strings) > 1 and s.strip()]
 
             print(filtered_strings)
             break
