76cc2f80f8b111e5d8f59a577202440bd6f75d7f
beep boop <githubbot@vadim.me>
2024-07-11 17:02:37 +0200

    Solution 70 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c12oc588 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c6b05978-6219-49b8-b607-37ee0bbe9 6d0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,15 +1,13 @@
-```
 def sum_squares(lst):
     total_sum = 0
     has_negative = False
     for num in lst:
-        if num <= 0:
+        if num < 0:
             has_negative = True
-            break
-        if num > 0 and (num ** 0.5 == int(num ** 0.5)):
+        elif (num > 0 and (num ** 0.5 == int(num ** 0.5))):
             total_sum += num ** 2
         else:
             total_sum += num
-    return 0 if has_negative else total_sum
+    return total_sum if not has_negative else 0
 
 print(sum_squares([1, 4, -3, 9, -10]))
