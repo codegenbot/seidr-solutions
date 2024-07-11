ab6b574db26a636327e00e44cc776974b5016abc
beep boop <githubbot@vadim.me>
2024-07-11 21:45:42 +0200

    Solution 34 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/3khmd3h9 Prompt: Complete the following code given the task description and function signature.

--- Python_25.py
+++ Python_25.py
@@ -10,11 +10,10 @@ def factorize(n: int) -> List[int]:
         if n % i:
             i += 1
         else:
-            n //= i
             count = 0
             while n % i == 0:
-                n //= i
                 count += 1
+                n //= i
             factors.extend([i] * count)
     if n > 1:
         factors.append(n)
