c8786a9f2b82e1536d9579efc5c96e5019f79ffa
beep boop <githubbot@vadim.me>
2024-07-11 18:13:24 +0200

    Solution 99 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/oip7hvah Prompt: aa796a74-99b8-4394-85bc-a3c188c4a3f4.cpp: In function ‘std::vector<std::__cxx11: :basic_string<char> > split_words(std::string)’: aa796a74-99b8-4394-85bc-a3c188c4a3f4.cpp:16:65: error: ‘c’ was not declared in t his scope    16 |                 return {to_string(count(txt.begin(), txt.end(), c))};       |                                                                 ^ aa796a74-99b8-4394-85bc-a3c188c4a3f4.cpp:16:68: error: could not convert ‘{<expr ession error>}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector<std::__ cxx11::basic_string<char> >’    16 |                 return {to_string(count(txt.begin(), txt.end(), c))};       |                                                                    ^       |                                                                    |       |                                                                    <brac e-enclosed initializer list> aa796a74-99b8-4394-85bc-a3c188c4a3f4.cpp: At global scope: aa796a74-99b8-4394-85bc-a3c188c4a3f4.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ aa796a74-99b8-4394-85bc-a3c188c4a3f4.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -7,16 +7,19 @@ using namespace std;
 vector<string> split_words(string txt) {
     vector<string> result;
     size_t pos = 0, prev_pos = 0;
-    
+
     while (pos != string::npos) {
         pos = txt.find(' ', prev_pos);
-        if (pos == string::npos) {
-            pos = txt.find(',');
-        }
+        if (pos == string::npos || pos < txt.find(','))
+            break;
         result.push_back(txt.substr(prev_pos, pos - prev_pos));
         prev_pos = pos + 1;
     }
-    
+
+    if (prev_pos < txt.length()) {
+        result.push_back(txt.substr(prev_pos));
+    }
+
     return result;
 }
 
