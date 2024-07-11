a9e7349902b9fc7a3ae07218dc7d025334e61cdd
beep boop <githubbot@vadim.me>
2024-07-11 21:09:25 +0200

    Solution 32 of Python/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/175ypbth Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4885411d-b156-46a3-9ffb-84f3a58fa fd3.py", line 1     return "".join([char.lower() if char.isupper() else char.upper() for char in  string])     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_27.py
+++ Python_27.py
@@ -1,5 +1,2 @@
-def toggle_case(string):
+def modify_string(string):
     return "".join([char.lower() if char.isupper() else char.upper() for char in string])
-
-# Call the function with the desired string as an argument
-toggle_case("Hello World")
