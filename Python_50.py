40a9fb50bde174ecf637126b0c207b36487e0688
beep boop <githubbot@vadim.me>
2024-07-11 14:00:09 +0200

    Solution 0 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/l0dejohc Prompt: Complete the following code given the task description and function signature.

--- Python_50.py
+++ Python_50.py
@@ -1,11 +1,2 @@
 def decode_shift(s: str):
-    return "".join(
-        [
-            (
-                ch.upper()
-                if "A" <= ch <= "Z"
-                else chr(((ord(ch.lower()) - 5 - ord("a")) % 26) + ord("a")).upper()
-            )
-            for ch in s
-        ]
-    ).upper()
+    return "".join([chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) for ch in s])
