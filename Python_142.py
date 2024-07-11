31c2f3ecf92dc30bcd1570400912596951481eaf
beep boop <githubbot@vadim.me>
2024-07-11 05:20:43 +0200

    Solution 84 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/be60kwzt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bee5c050-f4cc-47e2-bf30-7041c2d64 26e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -10,9 +10,9 @@ def check():
     n = int(input("Enter a number: "))
     num = calculate_sum_of_cubes(n)
     if num < 1000000:
-        return "The sum of cubes is less than 1 million."
+        print("The sum of cubes is less than 1 million.")
     else:
-        return "The sum of cubes is greater than or equal to 1 million."
+        print("The sum of cubes is greater than or equal to 1 million.")
 
 
-print(check())
+check()
