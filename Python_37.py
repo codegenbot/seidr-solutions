eca6887f515a6e3e6e9c9ba45611bc32b6002c6f
beep boop <githubbot@vadim.me>
2024-07-11 13:06:02 +0200

    Solution 2 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/xc3n7t3j Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f1baa36b-bda0-45ed-81c0-b61515bb9 f3c.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f1baa36b-bda0-45ed-81c0-b61515bb9 f3c.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,3 +1,5 @@
 def sort_even(l: list):
-    return [x if i % 2 != 0 else sorted([y for y in l if y % 2 == 0])[0] 
-            for i, x in enumerate(l)]
+    return [
+        x if i % 2 != 0 else min([y for y in l if (i // 2) * 2 == j] or [None])[1]
+        for i, (x, j) in enumerate(zip(l, range(len(l))))
+    ]
