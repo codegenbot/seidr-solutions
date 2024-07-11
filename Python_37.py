9f4276079e557fb9502c978f5f45b5b3100d95d7
beep boop <githubbot@vadim.me>
2024-07-11 14:27:50 +0200

    Solution 32 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9hixz5xx Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5fa7bd4f-0087-4dce-91f5-2b83bbf22 7d8.py", line 10, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5fa7bd4f-0087-4dce-91f5-2b83bbf22 7d8.py", line 5, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,6 +1,6 @@
 ```
 def sort_even(l: list):
     evens = sorted([x for x in l if x % 2 == 0])
-    odds = sorted([x for x in l if x % 2 != 0], reverse=True)
+    count_even = len(evens)
     
-    return [i if i % 2 != 0 else j for i, j in zip(odds, evens)]
+    return [i if i % 2 != 0 else evens.pop(0) if count_even > 0 else None for i in sorted([x for x in l if x % 2 != 0], reverse=True)]
