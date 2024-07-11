d5413c48efd23975aacd9bc2854bd0c714ff168a
beep boop <githubbot@vadim.me>
2024-07-11 15:07:20 +0200

    Solution 9 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/hrb5ev4r Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb53072d-9c9b-4410-8f16-c29c550f5 95f.py", line 4     return [string[: i + 1] for i in range(len(string)]                                                       ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '('

--- Python_14.py
+++ Python_14.py
@@ -1,8 +1,6 @@
 from typing import List
 
 def all_prefixes(string: str) -> List[str]:
-    if not string.isalpha():
-        return "Input should contain only characters."
     return [string[: i + 1] for i in range(len(string))]
 
 input_string = input("Enter a string: ")
