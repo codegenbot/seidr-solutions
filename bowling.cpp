25bdda91fe9847d91db6c7effcf4a672753daf83
beep boop <githubbot@vadim.me>
2024-07-11 15:18:49 +0200

    Solution 79 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/dlv2ukls Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- bowling.cpp
+++ bowling.cpp
@@ -5,32 +5,41 @@ int bowlingScore(std::string s) {
     int score = 0;
     int roll1 = 0;
     int roll2 = 0;
-    int roll3 = 0;
-    for(int i = 0; i < s.size(); i++) {
-        if(s[i] == '/') {
-            if(roll1 + roll2 < 10) {
-                score += roll1 + roll2 + (10 - '0') - '0';
-            } else {
-                score += roll1 + roll2 + 10;
-            }
-            roll1 = roll2 = 0;
-        } else if('0' <= s[i] && s[i] <= '9') {
-            roll3 = roll3 * 10 + (s[i] - '0');
-            if(roll1 == 0) {
-                roll1 = roll2;
-                roll2 = roll3;
-                roll3 = 0;
-            }
-        } else if(s[i] == 'X') {
+    int frame = 1;
+
+    for(int i = 0; i <= s.size() - 1; i++) {
+        if(s[i] == 'X') {
             score += 10;
-            roll1 = roll2 = roll3 = 0;
-        }
-    }
-    if(roll1 + roll2 > 0) {
-        if(roll1 + roll2 < 10) {
-            score += roll1 + roll2 + (10 - '0') - '0';
+            if(i < s.size() - 1) {
+                if(s[i + 1] == '/') {
+                    score += 10 - (frame % 2);
+                } else {
+                    roll1 = 10;
+                }
+            }
+            frame++;
+        } else if(s[i] == '/') {
+            score += 10 - (roll1 + roll2);
+            frame++;
+            roll1 = 0;
+            roll2 = 0;
         } else {
-            score += roll1 + roll2 + 10;
+            if(i < s.size() - 2 && s[i + 1] == 'X' && s[i + 2] == 'X') {
+                score += 10 * 3;
+                i++;
+                i++;
+                frame += 2;
+            } else {
+                roll1 = roll1 * 10 + (s[i] - '0');
+                if(i < s.size() - 1) {
+                    if(s[i + 1] == '/') {
+                        score += 10 - (frame % 2);
+                        i++;
+                    } else {
+                        roll2 = (s[i + 1] - '0');
+                    }
+                }
+            }
         }
     }
     return score;
