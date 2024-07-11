2ee519a18b9c0b2beb9b507135287105b5a44519
beep boop <githubbot@vadim.me>
2024-07-11 16:45:48 +0200

    Solution 95 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bkan34hb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b426f892-61a1-4b2a-81f0-a5ca364df 5f8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,4 +1,4 @@
-'''
+```
 def main():
     while True:
         try:
@@ -17,4 +17,3 @@ def main():
         except (EOFError, KeyboardInterrupt):
             print("Program terminated/interrupted by user.")
             break
-'''
