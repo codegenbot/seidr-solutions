cb576b7df62f90dc512f0d45eac0b7d7756e1b9a
beep boop <githubbot@vadim.me>
2024-07-11 20:24:35 +0200

    Solution 25 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/7uz7cvp2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bd729039-0cda-43f3-a1f9-20f1c5495 916.py", line 5, in <module>     print(solve_boolean(expression))           ^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bd729039-0cda-43f3-a1f9-20f1c5495 916.py", line 2, in solve_boolean     return eval(expression.replace("T", "True").replace("F", "False"))            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "<string>", line 1, in <module> NameError: name 't' is not defined

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,4 +1,5 @@
 def solve_boolean(expression):
     return eval(expression.replace("T", "True").replace("F", "False").replace("|", "or").replace("&", "and"))
+
 expression = input()
 print(solve_boolean(expression))
