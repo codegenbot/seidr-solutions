3cca2e1253fee94a67dad12ae8488104762b4008
beep boop <githubbot@vadim.me>
2024-07-11 02:06:55 +0200

    Solution 39 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/be60kwzt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a072da78-05d7-4211-bfff-e6503b967 ef5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -7,4 +7,6 @@ def sum_squares():
     num_list = list(map(int, input("Enter space-separated integers: ").split()))
     return sum([i**2 for i in num_list])
 
-sum_squares()
+print('''
+{}: {}
+'''.format(check(sum_squares), sum_squares))
