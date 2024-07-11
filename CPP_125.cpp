1b6e28c9b84f0d84ec06ea88862aa88a63d37060
beep boop <githubbot@vadim.me>
2024-07-11 15:35:30 +0200

    Solution 58 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/oip7hvah Prompt:       |                              ^~~~~~ 363e2c73-a3de-4fed-87d1-acf9744a9c26.cpp:29:37: error: ‘string’ was not declared  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 363e2c73-a3de-4fed-87d1-acf9744a9c26.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 363e2c73-a3de-4fed-87d1-acf9744a9c26.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 363e2c73-a3de-4fed-87d1-acf9744a9c26.cpp:28: 363e2c73-a3de-4fed-87d1-acf9744a9c26.cpp: In function ‘int main()’: 363e2c73-a3de-4fed-87d1-acf9744a9c26.cpp:38:20: error: ‘split_words’ was not dec lared in this scope    38 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 363e2c73-a3de-4fed-87d1-acf9744a9c26.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <algorithm>
-#include <cctype>
 
 using namespace std;
 
@@ -8,29 +7,7 @@ bool issame(vector<string> a, vector<string> b) {
     return (a.size() == b.size()) && equal(a.begin(), a.end(), b.begin());
 }
 
-vector<string> split_words(string txt) {
-    vector<string> result;
-    size_t pos = 0, prev_pos = 0;
-
-    while (pos != string::npos) {
-        pos = txt.find_first_of(" ,", prev_pos);
-        if (pos == string::npos) {
-            // No whitespaces or commas found
-            int count = 0;
-            for (char c : txt) {
-                if (islower(c)) {
-                    count++;
-                }
-            }
-            result.push_back(to_string(count));
-            break;
-        }
-
-        string word = txt.substr(prev_pos, pos - prev_pos);
-        result.push_back(word);
-
-        prev_pos = pos + 1;
-    }
-
-    return result;
+int main() {
+    assert(issame(split_words(""), {"0"}));
+    return 0;
 }
