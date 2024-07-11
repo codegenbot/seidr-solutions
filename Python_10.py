6d3927ec8376b944f44298e8a7dda0130f8da5d9
beep boop <githubbot@vadim.me>
2024-07-11 08:22:13 +0200

    Solution 39 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/31odiee1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/58885090-df3c-4a9a-8dbb-a1224a726 813.py", line 17, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/58885090-df3c-4a9a-8dbb-a1224a726 813.py", line 14, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -2,10 +2,11 @@ def make_palindrome(string: str) -> str:
     if string == string[::-1]:
         return string
     for i in range(len(string)):
-        for j in range(i+1, len(string)+1):
+        for j in range(i + 1, len(string)):
             substring = string[i:j]
             if substring == substring[::-1]:
                 return substring + string[:i][::-1] + string[j:][::-1]
     if not string or len(string) % 2 == 0:
-        return (string[0] + "a" * ((len(string) // 2) + (len(string) % 2)))
-    return (string[0] + "a" + string[0]) * ((len(string) // 2) + (len(string) % 2))
+        middle = (len(string) // 2) * "a"
+        return string[0] + middle + string[0]
+    return string[0] + "a" + string[0] + (len(string) - 1) * "a" + string[0]
