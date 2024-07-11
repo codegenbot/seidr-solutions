cb99b197ae0eccd68227b3ed32080335fd29d1a9
beep boop <githubbot@vadim.me>
2024-07-11 12:21:03 +0200

    Solution 54 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/64h8y2gp Prompt:       |                                        ^~~~~ 477ce257-68bd-455f-88ca-c78ac02c8dc7.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 477ce257-68bd-455f-88ca-c78ac02c8dc7.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from 477ce257-68bd-455f-88ca-c78ac02c8dc7.cpp:21: 477ce257-68bd-455f-88ca-c78ac02c8dc7.cpp: In function ‘int main()’: 477ce257-68bd-455f-88ca-c78ac02c8dc7.cpp:23:35: error: ‘lst’ was not declared in  this scope    23 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 477ce257-68bd-455f-88ca-c78ac02c8dc7.cpp:23:38: error: ‘double_the_difference’ c annot be used as a function    23 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ 477ce257-68bd-455f-88ca-c78ac02c8dc7.cpp:23:43: error: ‘odd_sum’ was not declare d in this scope    23 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,4 +1,3 @@
-```cpp
 int main() {
     vector<float> lst;
     long long odd_sum = 0;
@@ -8,8 +7,8 @@ int main() {
         cin >> num;
         lst.push_back(num);
     }
-    odd_sum = double_the_difference(lst);
-    cout << "The sum of the squares of all positive integers in the list is: " << odd_sum << endl;
+    long long result = double_the_difference(lst);
+    cout << "The sum of the squares of all positive integers in the list is: " << result << endl;
     return 0;
 }
 
