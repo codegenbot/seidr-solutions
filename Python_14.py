5501a3fef31bffcbe2f24aba6112f3a9379a0a35
beep boop <githubbot@vadim.me>
2024-07-11 13:43:09 +0200

    Solution 17 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xuylmkxp Prompt: Program did not receive expected input

--- Python_14.py
+++ Python_14.py
@@ -5,6 +5,6 @@ def all_prefixes(string: str) -> List[str]:
     prefixes = [""]
     if string and len(string) > 0:
         for i in range(len(string)):
-            prefix = string[:i + 1]
+            prefix = string[: i + 1]
             prefixes.append(prefix)
     return prefixes
