e62035fb306ec0dd559c92857991ad50defaad0d
beep boop <githubbot@vadim.me>
2024-07-11 13:09:16 +0200

    Solution 92 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/3bk0svvl Prompt: 0c2b2a59-d4f4-479f-89e2-a4019e4206f3.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <iostream>
 #include <string>
 #include <boost/any.hpp>
@@ -6,10 +7,10 @@ using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return a;
+        return b;
     }
     else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return a;
+        return b;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
         string str1 = any_cast<string>(a);
@@ -21,64 +22,53 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         }
         else {
-            return boost::any("None");
+            return boost::any("Equal");
         }
     }
-    else if ((a.type() == typeid(int) && b.type() == typeid(string)) || 
-             (b.type() == typeid(int) && a.type() == typeid(string))) {
-        int num = any_cast<int>(any(a));
+    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        int num = any_cast<int>(a);
         string str = any_cast<string>(b);
         if (stoi(str) > num) {
             return b;
         }
-        else if (stoi(str) < num) {
-            return a;
-        }
-        else {
-            return boost::any("None");
+       	else if (stoi(str) < num) {
+           	return a;
+       } else {
+        	return boost::any("Equal");
         }
     }
-    else if ((a.type() == typeid(string) && b.type() == typeid(int)) || 
-             (b.type() == typeid(string) && a.type() == typeid(int))) {
+    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
         string str = any_cast<string>(a);
         int num = any_cast<int>(b);
         if (stoi(str) > num) {
             return a;
-        }
-        else if (stoi(str) < num) {
-            return b;
-        }
-        else {
-            return boost::any("None");
+        } else if (stoi(str) < num) {
+           	return b;
+       } else {
+        	return boost::any("Equal");
         }
     }
-    else if ((a.type() == typeid(float) && b.type() == typeid(int)) || 
-             (b.type() == typeid(float) && a.type() == typeid(int))) {
-        float fl = any_cast<float>(a);
-        int num = any_cast<int>(b);
-        if (fl > num) {
-            return a;
-        }
-        else if (fl < num) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
+   	else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+		float fl = any_cast<float>(a);
+		int num = any_cast<int>(b);
+		if (fl > num) {
+			return a;
+		} else if (fl < num) {
+			return b;
+		} else {
+			return boost::any("Equal");
+		}
     }
-    else if ((a.type() == typeid(int) && b.type() == typeid(float)) || 
-             (b.type() == typeid(int) && a.type() == typeid(float))) {
-        int num = any_cast<int>(a);
-        float fl = any_cast<float>(b);
-        if (num > fl) {
-            return a;
-        }
-        else if (num < fl) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
+	else if (a.type() == typeid(int) && b.type() == typeid(float)) {
+		int num = any_cast<int>(a);
+		float fl = any_cast<float>(b);
+		if (num > fl) {
+			return a;
+		} else if (num < fl) {
+			return b;
+		} else {
+			return boost::any("Equal");
+		}
     }
-    return boost::any();
+    return boost::any("Equal");
 }
