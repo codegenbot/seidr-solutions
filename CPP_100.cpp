ff3e78ac6b13db3913d92a460ed20e6bf72eed52
beep boop <githubbot@vadim.me>
2024-07-11 15:29:25 +0200

    Solution 14 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/d31cl2gl Prompt: In file included from /usr/include/c++/11/vector:67,                  from 79c6e8f2-2927-4ecd-9d06-ed4e5bb25a7e.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 79c6e8f2-2927-4ecd-9d06-ed4e5bb25a7e.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 79c6e8f2-2927-4ecd-9d06-ed4e5bb25a7e.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 79c6e8f2-2927-4ecd-9d06-ed4e5bb25a7e.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 79c6e8f2-2927-4ecd-9d06-ed4e5bb25a7e.cpp:19: 79c6e8f2-2927-4ecd-9d06-ed4e5bb25a7e.cpp: In function ‘int main()’: 79c6e8f2-2927-4ecd-9d06-ed4e5bb25a7e.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,30 +1,6 @@
 #include <vector>
-#include <iostream>
 #include <cassert>
 
-using namespace std;
-
-vector<int> make_a_pile(int n) {
-    vector<int> stones;
-    stones.push_back(n);
-    for (int i = 1; i < n; ++i) {
-        if (n % 2 == 0) {
-            n += 2;
-        } else {
-            n += 1;
-        }
-        stones.push_back(n);
-    }
-    return stones;
-}
-
-bool issame(vector<int> a, vector<int> b) {
+bool issame(const std::vector<int>& a, const std::vector<int>& b){
     return a == b;
-}
-
-int main() {
-    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
-    cout << "Test passed successfully!" << endl;
-
-    return 0;
 }
