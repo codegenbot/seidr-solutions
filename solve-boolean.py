318149643b43070fdc7707d9991c43709ae30610
beep boop <githubbot@vadim.me>
2024-07-11 17:52:44 +0200

    Solution 3 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/v4ghdt3d Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7560c474-6d62-4335-85ba-44f5a4b9e 398.py", line 1     Here is the Python solution for the problem:                 ^^^^^^ SyntaxError: invalid syntax

--- solve-boolean.py
+++ solve-boolean.py
@@ -13,6 +13,6 @@ def solve_boolean(expression):
 
 print("Result:", "True" if solve_boolean('t') else "False")
 print("Result:", "True" if solve_boolean('f') else "False")
-print("Result:", "False" if solve_boolean('f&f') else "True")
-print("Result:", "False" if solve_boolean('f&t') else "True")
-print("Result:", "False" if solve_boolean('t&f') else "True")
+print("Result:", "True" if solve_boolean('f&f') else "False")
+print("Result:", "True" if solve_boolean('f&t') else "False")
+print("Result:", "True" if solve_boolean('t&f') else "False")
