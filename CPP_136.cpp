58e85f59c1bf557fc9c6645938b8e8e1b2154df3
beep boop <githubbot@vadim.me>
2024-07-11 05:03:09 +0200

    Solution 96 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/f6bcnret Prompt: s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ee7c2bb9-dac9-4fe9-a804-0236003b0204.cpp:26: ee7c2bb9-dac9-4fe9-a804-0236003b0204.cpp: In function ‘int main()’: ee7c2bb9-dac9-4fe9-a804-0236003b0204.cpp:36:20: error: ‘largest_smallest_integer s’ was not declared in this scope; did you mean ‘MyNamespace::largest_smallest_i ntegers’?    36 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ ee7c2bb9-dac9-4fe9-a804-0236003b0204.cpp:10:35: note: ‘MyNamespace::largest_smal lest_integers’ declared here    10 |     std::vector<std::vector<int>> largest_smallest_integers(std::vector< int> lst) {       |                                   ^~~~~~~~~~~~~~~~~~~~~~~~~ In file included from ee7c2bb9-dac9-4fe9-a804-0236003b0204.cpp:26: ee7c2bb9-dac9-4fe9-a804-0236003b0204.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,9 +1,10 @@
+```cpp
 #include <vector>
 #include <algorithm>
 #include <initializer_list>
 
 namespace MyNamespace {
-    bool compareVectors(const std::vector<int>& a, const std::vector<int>& b) {
+    bool issame(const std::vector<int>& a, const std::vector<int>& b) {
         return std::equal(a.begin(), a.end(), b.begin());
     }
 
@@ -23,9 +24,4 @@ namespace MyNamespace {
     }
 }
 
-int main() {
-    assert(MyNamespace::compareVectors(
-        MyNamespace::largest_smallest_integers({-6, -4, -4, -3, -100, 1}),
-        {{-3, 1}}));
-    return 0;
-}
+assert(MyNamespace::largest_smallest_integers({-6, -4, -4, -3, -100, 1}) == {{-3, 1}});
