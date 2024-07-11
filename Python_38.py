f66eae6061af0f83310e06bd69b062829c5ee050
beep boop <githubbot@vadim.me>
2024-07-11 19:19:22 +0200

    Solution 4 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/oxh92hfa Prompt: Program did not receive expected input

--- Python_38.py
+++ Python_38.py
@@ -1,14 +1,29 @@
+```
+def encode_cyclic(s: str):
+    groups = []
+    for i in range(0, (len(s) + 2) // 3):
+        if i * 3 + 3 > len(s):
+            groups.append(s[i * 3:])
+        else:
+            groups.append(s[(i*3):min((i*3)+3, len(s))])
+    groups = [(group[1:] + group[0]) if len(group) == 3 else group for group in groups]
+    return "".join(groups)
+
+
 def decode_cyclic(s: str):
     result = ""
     i = 0
     while i < len(s):
-        if (i + 3) <= len(s):
-            group = s[i : i + 3]
+        if len(s) - i >= 2:
+            if s[i+1] == s[i]:
+                result += s[i]*2 + s[i+2]
+                i += 3
+            else:
+                result += s[i]
+                i += 1
+        elif len(s) - i == 1:
+            result += s[i]
+            i += 1
         else:
-            group = s[i:]
-        if len(group) == 3:
-            result += group[1] + group[0] + group[2]
-        else:
-            result += group
-        i += 3
-    return result
+            result += s[i]
+            i += 1
