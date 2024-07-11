d0422d7671e2d77ce04726535a3589ff872dd91d
beep boop <githubbot@vadim.me>
2024-07-11 07:22:24 +0200

    Solution 56 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/8zvpzqkx Prompt:    41 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d01f5e9c-0ac9-467b-9722-d969e7415829.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ d01f5e9c-0ac9-467b-9722-d969e7415829.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main(){       |     ^~~~ In file included from d01f5e9c-0ac9-467b-9722-d969e7415829.cpp:40: d01f5e9c-0ac9-467b-9722-d969e7415829.cpp: In function ‘int main()’: d01f5e9c-0ac9-467b-9722-d969e7415829.cpp:50:20: error: ‘count_up_to’ was not dec lared in this scope    50 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ d01f5e9c-0ac9-467b-9722-d969e7415829.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b){
+bool issame(const vector<int>& a, const vector<int>& b){
     if(a.size() != b.size()) {
         return false;
     }
@@ -14,6 +14,14 @@ bool issame(vector<int> a, vector<int> b){
         }
     }
     return true;
+}
+
+vector<int> count_up_to(int n);
+
+int main(){
+    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
+    
+    return 0;
 }
 
 vector<int> count_up_to(int n){
@@ -31,10 +39,4 @@ vector<int> count_up_to(int n){
         }
     }
     return primes;
-}
-
-int main(){
-    assert (issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-    
-    return 0;
 }
