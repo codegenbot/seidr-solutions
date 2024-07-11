48923bebb93ceb7ed16140516dfebbaa87cfefee
beep boop <githubbot@vadim.me>
2024-07-11 13:19:56 +0200

    Solution 52 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rzs1si5s Prompt: Program did not receive expected input

--- Python_121.py
+++ Python_121.py
@@ -1,11 +1,9 @@
 while True:
-    try:
-        lst = input("Enter a list of numbers separated by space: ")
-        if len(lst.split()) > 0 and all(num.isdigit() for num in lst.split()):
-            nums = [int(i) for i in lst.split()]
-            break
-        elif lst.lower() in ["q", "quit", "exit"]:
-            print("Goodbye!")
-            exit()
-        else:
-            print("Invalid input. Please enter a correct number or 'q' to quit.")
+    lst = input("Enter a list of numbers separated by space: ")
+    if 'q' in [i.lower() for i in lst.split()] or 'quit' in lst.lower():
+        print("Goodbye!")
+        exit()
+    nums = [int(i) for i in lst.split()]
+    break
+else:
+    print("Invalid input. Please enter a correct number.")
