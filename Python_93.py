d8fcb429fdbbec21144b8c4970fcba706df2385b
beep boop <githubbot@vadim.me>
2024-07-11 16:46:51 +0200

    Solution 76 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d03dbk1h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c2f9ad4b-d24c-4174-8bfc-023634f63 9b5.py", line 24, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c2f9ad4b-d24c-4174-8bfc-023634f63 9b5.py", line 20, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,4 +1,3 @@
-```
 def encode(message):
     vowels = "aeiouAEIOU"
     result = ""
@@ -7,9 +6,15 @@ def encode(message):
             if char.lower() in vowels:
                 new_char = chr((ord(char.lower()) - 96 + 2) % 26 + 96)
             elif char.isupper():
-                new_char = chr((ord(char) - 65 + 2) % 26 + 65)
+                if char.lower() not in vowels:
+                    new_char = chr(((ord(char.lower()) - 64) % 26 + 1) % 26 + 64).swapcase()
+                else:
+                    new_char = chr((ord(char.upper()) - 64 + 2) % 26 + 64)
             else:
-                new_char = chr((ord(char) - 97 + 2) % 26 + 97)
+                if char.lower() not in vowels:
+                    new_char = chr(((ord(char) - 97) % 26 + 1) % 26 + 97).swapcase()
+                else:
+                    new_char = char.swapcase()
             result += new_char
         else:
             result += char
