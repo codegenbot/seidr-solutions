6a33ef23d73fafdd57532a68aa149d966e847eba
beep boop <githubbot@vadim.me>
2024-07-11 14:32:27 +0200

    Solution 34 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/22acifad Prompt: Program did not receive expected input

--- Python_12.py
+++ Python_12.py
@@ -7,7 +7,7 @@ def longest(strings: list) -> Optional[str]:
         return None
     return max(strings, key=len)
 
-n = int(input("How many strings do you want to enter? "))
-strings = [input("Enter a string (or 'stop' to finish): ") for _ in range(n) if input != 'stop']
+
+strings = [input("Enter a string (or 'stop' to finish): ") for _ in range(int(input("How many strings do you want to enter? "))) if input() != 'stop']
 longest_string = longest(strings)
 print(longest_string)
