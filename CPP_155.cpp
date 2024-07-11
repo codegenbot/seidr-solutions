7c668264c4ddcfb02ef4267ba21e424e95b3b2c3
beep boop <githubbot@vadim.me>
2024-07-11 02:18:46 +0200

    Solution 17 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/anmdhyr2 Prompt: In file included from /usr/include/c++/11/vector:67,                  from 51c326a8-2845-4155-b4b8-26b4d4302886.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 51c326a8-2845-4155-b4b8-26b4d4302886.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 51c326a8-2845-4155-b4b8-26b4d4302886.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 51c326a8-2845-4155-b4b8-26b4d4302886.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 51c326a8-2845-4155-b4b8-26b4d4302886.cpp:16: 51c326a8-2845-4155-b4b8-26b4d4302886.cpp: In function ‘int main()’: 51c326a8-2845-4155-b4b8-26b4d4302886.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,24 +1,13 @@
-```cpp
-#include <vector>
-#include <string>
-
-std::vector<int> even_odd_count(int num) {
-    int count_even = 0, count_odd = 0;
-    std::string str_num = std::to_string(std::abs(num));
-    for (char c : str_num) {
-        if ((c - '0') % 2 == 0)
-            ++count_even;
-        else
-            ++count_odd;
-    }
-    return {count_even, count_odd};
-}
-
 bool isSame(std::vector<int> a, std::vector<int> b) {
-    return (a[0] == b[0]) && (a[1] == b[1]);
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-int main() {
-    assert(isSame(even_odd_count(0), {1, 0}));
-    return 0;
-}
+assert(isSame(even_odd_count(0), {1, 0}));
