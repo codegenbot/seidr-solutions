7e55b3fd2bef98c05e725be831ace3c46a540a29
beep boop <githubbot@vadim.me>
2024-07-11 11:22:14 +0200

    Solution 44 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/emu3cny7 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 885f0e5d-e132-4c0b-89d7-a25c77949a14.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,17 +1,12 @@
-```cpp
 #include <vector>
 
-int solution(vector<int> numbers) {
-    int oddSum = 0;
-    for (int i = 0; i < numbers.size(); i++) {
-        if (numbers[i] % 2 != 0) {
-            oddSum += numbers[i];
+int solution(std::vector<int> a) {
+    std::vector<int> b(a);  
+    int res = 0;
+    for (int i = 1; i < a.size(); i++) {
+        if ((a[i] % 4 != 0) && (a[i] % 2 != 0)) {
+            res += a[i];
         }
     }
-    return oddSum;
-}
-
-int main() {
-    assert(solution({3, 13, 2, 9}) == 3);
-    return 0;
+    return res;
 }
