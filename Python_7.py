71b21af9cb0afe34e9b131ee4e5f08af234f8a41
beep boop <githubbot@vadim.me>
2024-07-11 16:22:32 +0200

    Solution 0 of Python/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/d6j1bdbf Prompt: Complete the following code given the task description and function signature.

--- Python_7.py
+++ Python_7.py
@@ -1,3 +1,2 @@
-```
-def filter_by_substring(strings: list, substring: str) -> list:
+def filter_by_substring(strings: List[str], substring: str) -> List[str]:
     return [s for s in strings if substring in s]
