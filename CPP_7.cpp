972869c8a5d45d786446dd52dcf6f6f45260d110
beep boop <githubbot@vadim.me>
2024-07-11 12:22:58 +0200

    Solution 59 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zkvvbdxl Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 84b1e249-0c70-4fd0-8dd5-e0fface8727c.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 84b1e249-0c70-4fd0-8dd5-e0fface8727c.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 84b1e249-0c70-4fd0-8dd5-e0fface8727c.cpp:10: 84b1e249-0c70-4fd0-8dd5-e0fface8727c.cpp: In function ‘int main()’: 84b1e249-0c70-4fd0-8dd5-e0fface8727c.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 84b1e249-0c70-4fd0-8dd5-e0fface8727c.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,17 +1,25 @@
-#include <iostream>
+```cpp
 #include <vector>
-using namespace std;
+#include <string>
 
-bool same(vector<string> a, vector<string> b){
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i = 0; i<a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
+bool same(vector<string> a, vector<string> b) {
+    if(a.size() != b.size()) return false;
+    for(int i=0; i<a.size(); ++i) {
+        if(!a[i].compare(0, a[i].size(), b[i])) return false;
     }
     return true;
+}
+
+int main() {
+    vector<string> strings = {"apple", "banana", "cat", "dog", "elephant"};
+    string substring = "an";
+    vector<string> result = filter_by_substring(strings, substring);
+    if(same({substring}, {s.substr(1) for s in result})) {
+        cout << "Substrings of the input strings are the same as the given substring" << endl;
+    } else {
+        cout << "Substrings of the input strings are not the same as the given substring" << endl;
+    }
+    return 0;
 }
 
 vector<string> filter_by_substring(vector<string> strings, string substring){
@@ -21,20 +29,4 @@ vector<string> filter_by_substring(vector<string> strings, string substring){
             result.push_back(str);
     }
     return result;
-}
-
-int main() {
-    vector<string> a = {"apple", "banana", "cherry"};
-    vector<string> b = {"apple", "banana", "cherry"};
-    
-    cout << boolalpha << same(a, b) << endl;
-
-    vector<string> strings = {"hello world", "world hello", "cpp is awesome", "cpp is amazing", "hello again"}; 
-    string substring = "world";
-    vector<string> filtered_strings = filter_by_substring(strings, substring);
-    
-    for(auto str : filtered_strings)
-        cout << str << endl;
-    
-    return 0;
 }
