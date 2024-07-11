88903f4f22f8bfbe48700022619890adc25327ce
beep boop <githubbot@vadim.me>
2024-07-11 03:55:13 +0200

    Solution 50 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fcqijk62 Prompt: 6bbca2c4-4806-4f23-be74-e89334d3e559.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 6bbca2c4-4806-4f23-be74-e89334d3e559.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 6bbca2c4-4806-4f23-be74-e89334d3e559.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 6bbca2c4-4806-4f23-be74-e89334d3e559.cpp:2:10: fatal error: CryptoPP/SHA1.h: No such file or directory     2 | #include <CryptoPP/SHA1.h>       |          ^~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include <string>
 #include <sstream>
-#include <CryptoPP/SHA1.h>
+#include <iomanip>
+#include <string>
+#include <crypto++/sha.h>
 
 int main() {
     std::cout << "Enter a string: ";
@@ -9,22 +10,15 @@ int main() {
     std::getline(std::cin, s);
     
     CryptoPP::SHA1 sha;
-    unsigned char md5[16];
+    unsigned char md5[20];
     sha.Calculate((const unsigned char*)s.c_str(), (int)s.size(), md5);
 
-    for(int i = 0; i < 16; ++i) {
-        std::stringstream ss;
-        ss << ((md5[i] * 4 > 9 ? std::hex << std::showbase : std::hex << std::noshowbase)
-            << std::setfill('0') << std::setw(2) << (int)(md5[i]));
+    std::stringstream ss;
+    for(int i = 0; i < 20; ++i) {
+        ss << std::hex << std::setfill('0') << std::setw(2) << ((int)md5[i]);
     }
 
-    std::cout << "The SHA1 is: ";
-    for(int i = 0; i < 16; ++i) {
-        std::cout << ((md5[i] * 4 > 9 ? std::hex << std::showbase : std::hex << std::noshowbase)
-            << std::setfill('0') << std::setw(2) << (int)(md5[i]));
-    }
-    
-    std::cout << std::endl;
+    std::cout << "The SHA1 is: " << ss.str() << std::endl;
     
     return 0;
 }
