51a21f8d43cbdeb8782600dd3c4dcdfa3d080d62
beep boop <githubbot@vadim.me>
2024-07-11 09:07:41 +0200

    Solution 28 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/b67vlen3 Prompt:       |                              ^~~~~~ 3c77c6b4-b02a-42ad-9371-14e8175739df.cpp:35:37: error: ‘string’ was not declared  in this scope    35 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 3c77c6b4-b02a-42ad-9371-14e8175739df.cpp:35:44: error: ‘b’ was not declared in t his scope    35 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3c77c6b4-b02a-42ad-9371-14e8175739df.cpp:35:45: error: expression list treated a s compound expression in initializer [-fpermissive]    35 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3c77c6b4-b02a-42ad-9371-14e8175739df.cpp:34: 3c77c6b4-b02a-42ad-9371-14e8175739df.cpp: In function ‘int main()’: 3c77c6b4-b02a-42ad-9371-14e8175739df.cpp:44:20: error: ‘numerical_letter_grade’ was not declared in this scope    44 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 3c77c6b4-b02a-42ad-9371-14e8175739df.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,48 +1,41 @@
-Here is the modified code:
-
 ```cpp
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size())
-        return false;
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+    if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
-            return false;
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
 
-vector<string> numerical_letter_grade(vector<float> grades) {
+std::vector<string> numerical_letter_grade(vector<float> grades) {
     vector<string> result;
-    for (float g : grades) {
-        string letter;
-        if (g >= 4.0) {
-            letter = "A+";
-        } else if (g > 3.7) {
-            letter = "A";
-        } else if (g > 3.3) {
-            letter = "A-";
-        } else if (g > 3.0) {
-            letter = "B+";
-        } else if (g > 2.7) {
-            letter = "B";
-        } else if (g > 2.3) {
-            letter = "B-";
-        } else if (g > 2.0) {
-            letter = "C+";
-        } else if (g > 1.7) {
-            letter = "C";
-        } else if (g > 1.3) {
-            letter = "C-";
-        } else if (g > 1.0) {
-            letter = "D+";
-        } else if (g > 0.7) {
-            letter = "D";
-        } else if (g > 0.0) {
-            letter = "D-";
-        } else {
-            letter = "F";
-        }
-        result.push_back(letter);
+    for (float grade : grades) {
+        string letterGrade;
+        if (grade >= 4.0)
+            letterGrade = "A+";
+        else if (grade > 3.7)
+            letterGrade = "A";
+        else if (grade > 3.3)
+            letterGrade = "A-";
+        else if (grade > 3.0)
+            letterGrade = "B+";
+        else if (grade > 2.7)
+            letterGrade = "B";
+        else if (grade > 2.3)
+            letterGrade = "B-";
+        else if (grade > 2.0)
+            letterGrade = "C+";
+        else if (grade > 1.7)
+            letterGrade = "C";
+        else if (grade > 1.3)
+            letterGrade = "C-";
+        else if (grade > 1.0)
+            letterGrade = "D+";
+        else if (grade > 0.7)
+            letterGrade = "D";
+        else
+            letterGrade = "F";
+        result.push_back(letterGrade);
     }
     return result;
 }
