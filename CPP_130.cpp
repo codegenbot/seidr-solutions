ce73d0a07b81a3b972c70b6500214d912f5a7afd
beep boop <githubbot@vadim.me>
2024-07-11 09:03:05 +0200

    Solution 18 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/j9jhcy8s Prompt:       |                    ^~~ 6988ef55-032f-469b-bcef-5fcfedffd990.cpp:23:27: error: ‘vector’ was not declared  in this scope    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6988ef55-032f-469b-bcef-5fcfedffd990.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6988ef55-032f-469b-bcef-5fcfedffd990.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6988ef55-032f-469b-bcef-5fcfedffd990.cpp:22: 6988ef55-032f-469b-bcef-5fcfedffd990.cpp: In function ‘int main()’: 6988ef55-032f-469b-bcef-5fcfedffd990.cpp:32:20: error: ‘tri’ was not declared in  this scope    32 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ 6988ef55-032f-469b-bcef-5fcfedffd990.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -11,24 +11,3 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     }
     return true;
 }
-
-vector<int> result;
-if (n == 0) {
-    result.push_back(0);
-    return result;
-}
-if (n == 1) {
-    result.push_back(3);
-    return result;
-}
-result.push_back(3);
-result.push_back(1);
-result.push_back(2);
-for (int i = 3; i <= n; ++i) {
-    if (i % 2 == 0) {
-        result.push_back(1 + i / 2);
-    } else {
-        result.push_back(result[i - 1] + result[i - 2] + result[i - 3]);
-    }
-}
-return result;
