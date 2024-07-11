6a4f765b4fbc5718eb633fd10f17a1f7b2b811d6
beep boop <githubbot@vadim.me>
2024-07-11 10:51:22 +0200

    Solution 64 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uufudmg3 Prompt: w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const int*; _Tp = int; _Alloc = std:: allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = i nt; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::allocator_type = std ::allocator<int>]’ 84eb466b-3fa6-467b-9bdb-4e64a2b67f1d.cpp:36:5:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -24,10 +24,9 @@ bool isPrime(int n) {
     return true;
 }
 
-vector<int> lst;
-std::vector<int>::allocator_type a = std::allocator<int>();
 int main() {
-    assert(skkkasdkd({127, 97, 8192}) == 10);
+    vector<int> lst = {127, 97, 8192}; // Correct way
+    assert(skkkasdkd(lst) == 10);
     cout << "Hello World!" << endl;
     return 0;
 }
