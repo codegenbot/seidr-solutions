f3d854ec42c71f8daa37a0f7b2233eac62252cc2
beep boop <githubbot@vadim.me>
2024-07-11 10:04:51 +0200

    Solution 63 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/abae4f03-eddd-4f70-afa3-7acfc3eb6 c6f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,8 +1,3 @@
-```
-def sort_third():
-    l = input("Enter list of integers separated by space: ").split()
-    l = list(map(int, l))
+def sort_third(l: list):
     l.sort(key=lambda x: int(str(x)[2]))
     return l
-
-print(sort_third())
