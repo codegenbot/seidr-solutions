5a9e5720340202ba04bca2c0c45fe9bf162379ea
beep boop <githubbot@vadim.me>
2024-07-11 09:12:01 +0200

    Solution 15 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/02005635-0c4e-4639-9db4-c671ce492 0c1.py", line 23, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/02005635-0c4e-4639-9db4-c671ce492 0c1.py", line 18, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,19 +1,7 @@
 from itertools import cycle
 
-
 def sort_third(l: list):
-    first_half = sorted([x for i, x in enumerate(l) if i % 3 != 0])
-    second_half = sorted([x for i, x in enumerate(l) if i % 3 == 0], reverse=True)
-
     result = []
-    for i, x in cycle(enumerate(l)):
-        if i % 3 != 0:
-            result.append(x)
-        else:
-            if len(first_half):
-                result.append(next(item for item in first_half))
-                del first_half[0]
-            elif len(second_half):
-                result.append(next(item for item in second_half))
-                del second_half[0]
+    for i in range(len(l)):
+        result.append(next(cycle(sorted([x for j, x in enumerate(l) if (j+1) % 3 != 0]), sorted([x for j, x in enumerate(l) if (j+1) % 3 == 0], reverse=True))))
     return result
