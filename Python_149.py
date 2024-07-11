8a11e4839e41f0580917e2a80e423404d8c0bda3
beep boop <githubbot@vadim.me>
2024-07-11 07:28:47 +0200

    Solution 2 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9rhtkacy Prompt: Complete the following code given the task description and function signature.

--- Python_149.py
+++ Python_149.py
@@ -1,4 +1,2 @@
-```
 def sorted_list_sum(lst):
-    odd_length_words = [word for word in lst if len(word) % 2 != 0]
-    return sorted([word for word in lst if len(word) % 2 == 0], key=lambda x: (len(x), x))
+    return sorted([i for i in lst if len(i) % 2 == 0])
