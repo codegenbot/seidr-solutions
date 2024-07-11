cd7e303877f22dbb4628f0c90688fa80aef75c4d
beep boop <githubbot@vadim.me>
2024-07-11 04:50:22 +0200

    Solution 20 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2dqf0nts Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:11: 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp: In function ‘int main()’: 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,5 +1,20 @@
-#include <boost/container.hpp>
+#include <boost/any.hpp>
+#include <vector>
+#include <list>
 
-bool same(boost::container::vector<int> a, boost::container::vector<int> b) {
-    return (a.size() == b.size()) && std::all_of(a.begin(), a.end(), [&](int x) { return std::find(b.begin(), b.end(), x) != b.end(); });
+bool issame(const boost::any& a, const boost::any& b) {
+    if (!a.type() == b.type()) return false;
+    return boost::any_cast<const vector<int>&>(a) == boost::any_cast<const vector<int>&>(b);
+}
+
+vector<int> filter_integers(list<any> values) {
+    vector<int> result;
+    for (const auto& value : values) {
+        if (is_same<list<any>, list<int>>::value || issame(value, make_any<int>(0))) {
+            int val = boost::any_cast<int>(value);
+            if (!val) continue;
+            result.push_back(val);
+        }
+    }
+    return result;
 }
