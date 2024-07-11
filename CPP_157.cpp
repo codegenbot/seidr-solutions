475336998761dabae21b2e9517d98d83d1ba5983
beep boop <githubbot@vadim.me>
2024-07-11 03:30:28 +0200

    Solution 28 of CPP/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xsowexf3 Prompt:       | ^~~~~~~~~~~~~ 2cca9fe3-7464-423a-8fd1-172ce1555e38.cpp:23:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    23 | int main(){       |         ^~ 2cca9fe3-7464-423a-8fd1-172ce1555e38.cpp:23:9: note: remove parentheses to defau lt-initialize a variable    23 | int main(){       |         ^~       |         -- 2cca9fe3-7464-423a-8fd1-172ce1555e38.cpp:23:9: note: or replace parentheses with  braces to value-initialize a variable 2cca9fe3-7464-423a-8fd1-172ce1555e38.cpp:23:11: error: a function-definition is not allowed here before ‘{’ token    23 | int main(){       |           ^ 2cca9fe3-7464-423a-8fd1-172ce1555e38.cpp:25:2: error: expected ‘}’ at end of inp ut    25 | }       |  ^ 2cca9fe3-7464-423a-8fd1-172ce1555e38.cpp:12:12: note: to match this ‘{’    12 | int main() {       |            ^

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 
 using namespace std;
@@ -10,8 +9,7 @@ bool right_angle_triangle(float a, float b, float c) {
         return false;
 }
 
-int main 
-{
+int main() {
     assert(right_angle_triangle(2, 2, 10) == false);
     cout << "Input the lengths of three sides (a, b, c): ";
     float a, b, c;
