ba264db9b7ddff53e36933e78cd1dfcada6c9967
beep boop <githubbot@vadim.me>
2024-07-11 05:58:50 +0200

    Solution 0 of Python/46, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2tkzzbpt Prompt: Complete the following code given the task description and function signature.

---
+++ Python_46.py
@@ -0,0 +1,10 @@
+def fib4(n: int):
+    if n < 0:
+        raise ValueError("n must be a non-negative integer")
+    elif n <= 3:
+        return [0, 0, 2, 0][n]
+    else:
+        f = [0, 0, 2, 0]
+        for i in range(4, n + 1):
+            f.append(f[i - 1] + f[i - 2] + f[i - 3] + f[i - 4])
+        return f[n]
