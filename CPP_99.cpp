4dfe61b7b817080aeaf6ba40199bc2170d458e9a
beep boop <githubbot@vadim.me>
2024-07-11 04:57:46 +0200

    Solution 4 of CPP/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9mf886lq Prompt:       |                 ^~~~~ 254820b7-ebc5-405f-a1f4-9f9e9938d8e7.cpp:3:18: error: ‘ceil’ was not declared in  this scope     3 |     int higher = ceil(num);       |                  ^~~~ 254820b7-ebc5-405f-a1f4-9f9e9938d8e7.cpp:5:5: error: expected unqualified-id bef ore ‘if’     5 |     if (num - lower < higher - num) {       |     ^~ 254820b7-ebc5-405f-a1f4-9f9e9938d8e7.cpp:7:7: error: expected unqualified-id bef ore ‘else’     7 |     } else {       |       ^~~~ 254820b7-ebc5-405f-a1f4-9f9e9938d8e7.cpp:10:1: error: expected declaration befor e ‘}’ token    10 | }       | ^ In file included from 254820b7-ebc5-405f-a1f4-9f9e9938d8e7.cpp:12: 254820b7-ebc5-405f-a1f4-9f9e9938d8e7.cpp: In function ‘int main()’: 254820b7-ebc5-405f-a1f4-9f9e9938d8e7.cpp:14:13: error: ‘closest_integer’ was not  declared in this scope    14 |     assert (closest_integer("0") == 0);       |             ^~~~~~~~~~~~~~~

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,7 +1,13 @@
-double num = stod(value);
-int closestInt = static_cast<int>(round(num));
-if (num - closestInt > 0) {
-    return ceil(num);
-} else {
-    return floor(num);
+#include <cmath>
+
+int closest_integer(std::string value) {
+    float num = stof(value);
+    int lower = floor(num);
+    int higher = ceil(num);
+
+    if (num - lower < higher - num) {
+        return lower;
+    } else {
+        return higher;
+    }
 }
