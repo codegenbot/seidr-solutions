627d4727d2d609092a1678fa20e844ba96ade0e7
beep boop <githubbot@vadim.me>
2024-07-11 07:06:21 +0200

    Solution 38 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/c5cl2o2t Prompt:       |             ^~~ 7f561816-759f-47eb-94e5-57114d5db202.cpp:41:33: error: expected primary-expressi on before ‘char’    41 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ 7f561816-759f-47eb-94e5-57114d5db202.cpp:41:38: error: expected primary-expressi on before ‘int’    41 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ 7f561816-759f-47eb-94e5-57114d5db202.cpp:54:5: error: redefinition of ‘int main( )’    54 | int main(){       |     ^~~~ 7f561816-759f-47eb-94e5-57114d5db202.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~ In file included from 7f561816-759f-47eb-94e5-57114d5db202.cpp:40: 7f561816-759f-47eb-94e5-57114d5db202.cpp: In function ‘int main()’: 7f561816-759f-47eb-94e5-57114d5db202.cpp:55:20: error: ‘histogram’ was not decla red in this scope    55 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -34,6 +34,6 @@ std::map<char, int> histogram(std::string test) {
 }
 
 int main() {
-    assert(issame(histogram("a"), {{'a', 1}}));
+    assert (issame(histogram("a"), {{'a', 1}}));
     return 0;
 }
