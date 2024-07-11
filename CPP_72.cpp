682ea8a2eeae4aa016f4a47157b612e18843d44b
beep boop <githubbot@vadim.me>
2024-07-11 07:49:56 +0200

    Solution 4 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7nruqk3x Prompt: ddc84a7b-d2e7-4733-80ec-cd65a51dab6e.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from ddc84a7b-d2e7-4733-80ec-cd65a51dab6e.cpp:16: ddc84a7b-d2e7-4733-80ec-cd65a51dab6e.cpp: In function ‘int main()’: ddc84a7b-d2e7-4733-80ec-cd65a51dab6e.cpp:18:13: error: ‘will_it_fly’ was not dec lared in this scope    18 |     assert (will_it_fly({5}, 5) == true);       |             ^~~~~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,18 +1,12 @@
 bool will_it_fly(vector<int> q, int w) {
-    string s = "";
+    string str = "";
     for (int i : q) {
-        s += to_string(i);
+        str += to_string(i);
     }
-    if (!s.compare(toString(s))) return false;
+    if (!str.compare(to_string(reverse(str)))) return false;
     int sum = 0;
     for (int i : q) {
         sum += i;
     }
     return sum <= w;
-}
-
-string toString(string s) {
-    string t = s;
-    reverse(t.begin(), t.end());
-    return t;
 }
