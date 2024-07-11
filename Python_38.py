ff95560959aa34ef14392e799db9e111be46a05b
beep boop <githubbot@vadim.me>
2024-07-11 15:12:56 +0200

    Solution 76 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ovcg7qmp Prompt: Program did not receive expected input

--- Python_38.py
+++ Python_38.py
@@ -4,40 +4,24 @@ def decode_cyclic(s: str):
     result = ""
     temp = ""
     for char in s + s:
-        temp += char
-        if len(temp) == 3:
-            if len(result) > 0 and result[-1] == temp[0]:
-                result += temp[1:]
-            elif temp[0] == temp[2]:
-                if temp[2] == "(":
-                    result += "(" + temp[1:-1]
+        if char in "(a-zA-Z)":
+            temp += char
+        elif temp and len(temp) == 1:
+            if temp[0] in "({})" and result:
+                if result[-1] == '(' and temp[0] == ')':
+                    return ""
+                elif temp[0] == ')' and result[-1] == '(':
+                    temp += ")"
                 else:
-                    result += temp[1:-1] + temp[0]
+                    temp = "(" + temp
             else:
-                if len(temp) % 3 == 1 and temp[0] == result[-1]:
-                    result += temp[1:-1] + temp[0]
-                else:
-                    if len(result) > 0 and result[-1] == temp[0]:
-                        result += temp
-                    elif temp[0] == temp[2]:
-                        if temp[2] == "(":
-                            result += "(" + temp[1:]
-                        else:
-                            result += temp[1:] + temp[0]
-                    else:
-                        result += temp
-            temp = ""
-    if len(temp) % 3 == 1 and temp[0] == result[-1]:
-        result += temp[1:-1] + temp[0]
-    elif len(temp) > 0:
-        if len(result) > 0 and temp[0] == result[-1]:
-            result += temp
-        else:
-            if len(temp) % 3 == 1 and temp[0] == temp[2]:
-                if temp[2] == "(":
-                    result += "(" + temp[1:]
-                else:
-                    result += temp[1:] + temp[0]
-            else:
+                if result and len(result) > 2 and temp[0] == result[-1]:
+                    if temp[0].lower() in "abcdefghijklmnopqrstuvwxyz":
+                        result = result[:-1]
+                    elif temp[0] == ')':
+                        temp += "("
                 result += temp
+            temp = ""
+        elif not temp:
+            continue
     return result
