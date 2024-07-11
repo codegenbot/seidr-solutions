e9bb143e37355ec7fea7c4f1a2254ff5b21f4afd
beep boop <githubbot@vadim.me>
2024-07-11 02:07:58 +0200

    Solution 72 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/tmnr21o2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a12b1cb2-6b09-4015-b72c-b3e5460fb 736.py", line 10, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a12b1cb2-6b09-4015-b72c-b3e5460fb 736.py", line 5, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,4 +1,4 @@
 def sort_even(l: list):
-    evens = [x for x in l if x % 2 == 0]
-    sorted_evens = sorted(evens)
-    return [x if j % 2 != 0 else y for j, (x, y) in enumerate(zip(l, sorted_evens))]
+    even = [x for x in l if x % 2 == 0]
+    odd = [x for x in l if x % 2 != 0]
+    return sorted(even) + sorted(odd)
