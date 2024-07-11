44663f55fc7e870a1534767d4d1e9d776666b74b
beep boop <githubbot@vadim.me>
2024-07-11 17:04:09 +0200

    Solution 77 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3783cyso Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4400d04f-15b2-450e-a74e-cbc0feb4b bb3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,10 +1,11 @@
+```
 def modp(n: int, p: int):
     def extended_euclidean(a: int, b: int) -> (int, int):
         if a == 0:
             return b, 0
         else:
-            gcd, y, x = extended_euclidean(b % a, a)
-            return gcd, x - (b // a) * y, y
+            gcd, x, y = extended_euclidean(b % a, a)
+            return gcd, y, x - (b // a) * y
 
     gcd, inv_n, _ = extended_euclidean(n, p)
     if gcd != 1:
