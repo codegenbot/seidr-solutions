384ee5e062a641b0a362e916452205b7c2ad3dca
beep boop <githubbot@vadim.me>
2024-07-11 12:27:34 +0200

    Solution 63 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bl8evmth Prompt: b3f9214f-5c7f-4490-8831-fa5e18484d55.cpp: In function ‘std::string string_to_md5 (std::string)’: b3f9214f-5c7f-4490-8831-fa5e18484d55.cpp:9:23: error: ‘MD5_DIGEST_LENGTH’ was no t declared in this scope     9 |     unsigned char md5[MD5_DIGEST_LENGTH];       |                       ^~~~~~~~~~~~~~~~~ b3f9214f-5c7f-4490-8831-fa5e18484d55.cpp:10:58: error: ‘md5’ was not declared in  this scope    10 |     MD5((const unsigned char*)text.c_str(), text.size(), md5);       |                                                          ^~~ b3f9214f-5c7f-4490-8831-fa5e18484d55.cpp:10:5: error: ‘MD5’ was not declared in this scope    10 |     MD5((const unsigned char*)text.c_str(), text.size(), md5);       |     ^~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -6,12 +6,10 @@ string string_to_md5(string text) {
     unsigned char md5[MD5_DIGEST_LENGTH];
     MD5((const unsigned char*)text.c_str(), text.size(), md5);
     
-    string result;
-    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        stringstream ss;
-        ss << hex << setfill('0') << setw(2) << (int)md5[i];
-        result += ss.str();
+    ostringstream oss;
+    for(int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        oss << setfill('0') << setw(2) << hex << uppercase << (int)md5[i];
     }
     
-    return result;
+    return oss.str();
 }
