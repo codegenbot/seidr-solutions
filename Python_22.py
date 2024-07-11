8d06a3283b0d07b6e16267751b93b338b2d11bbd
beep boop <githubbot@vadim.me>
2024-07-11 08:57:43 +0200

    Solution 34 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5v09uzk5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/015ae61e-1440-4ea6-b952-ff940038d 134.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,4 +1,34 @@
-```
-def filter_integers() -> List[int]:
+from typing import List, Any
+
+
+def get_values() -> List[Any]:
+    while True:
+        try:
+            values = input(
+                "Enter a list of integers and/or strings separated by commas: "
+            )
+            values = [value.strip() for value in values.split(",")]
+            print("Input received:", values)
+            if values:
+                return [
+                    (
+                        int(value)
+                        if value.isdigit()
+                        else (
+                            float(value) if "." in value and "-" not in value else value
+                        )
+                    )
+                    for value in values
+                ]
+        except ValueError as e:
+            print(
+                f"Invalid input. Please enter integers and/or strings separated by commas: {e}"
+            )
+
+
+def filter_integers():
     result = get_values()
     return [value for value in result if isinstance(value, int)]
+
+
+print(filter_integers())
