2adb0150a1861dc03378d37051a0fc1cb42d6635
beep boop <githubbot@vadim.me>
2024-07-11 17:49:32 +0200

    Solution 72 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/juryj07u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7db0bad2-0612-4429-9ec8-792e1c5bc e93.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -4,12 +4,10 @@ def double_the_difference(separator):
     double_diff = difference * 2
     return double_diff
 
+
 def check(func):
     print(func())
 
+
 if __name__ == "__main__":
-    def the_function():
-        separator = input("Enter a separator: ")
-        result = double_the_difference(separator)
-        return result
-    check(the_function)
+    check(double_the_difference)
