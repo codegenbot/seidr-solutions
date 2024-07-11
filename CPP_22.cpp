c0717d4924d8b889df3e4e5add2540ab264a4e7c
beep boop <githubbot@vadim.me>
2024-07-11 04:43:16 +0200

    Solution 17 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2dqf0nts Prompt: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a331a921-96e5-43be-8255-9f15ca232918.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a331a921-96e5-43be-8255-9f15ca232918.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a331a921-96e5-43be-8255-9f15ca232918.cpp:13: a331a921-96e5-43be-8255-9f15ca232918.cpp: In function ‘int main()’: a331a921-96e5-43be-8255-9f15ca232918.cpp:23:35: error: no match for call to ‘(st d::vector<int>) (<brace-enclosed initializer list>)’    23 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~ a331a921-96e5-43be-8255-9f15ca232918.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,10 +1,17 @@
 #include <boost/container.hpp>
-#include <list>
 
 bool same(boost::container::vector<int> a, boost::container::vector<int> b) {
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main() {
-    assert(same(filter_integers({3, 'c', 3, 3, 'a', 'b'}), boost::container::vector<int>{3, 3, 3})); 
+    assert(same(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3})); 
 }
