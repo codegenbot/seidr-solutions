a77b2e07120dc56af3898a2ec3abf8a9028fa86d
beep boop <githubbot@vadim.me>
2024-07-11 12:21:54 +0200

    Solution 20 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8z1koym7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cb4458b2-93ad-4383-bf58-a521e8ea4 17a.py", line 12, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cb4458b2-93ad-4383-bf58-a521e8ea4 17a.py", line 9, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cb4458b2-93ad-4383-bf58-a521e8ea4 17a.py", line 3, in sorted_list_sum     odd_words = [_[1] for _, parity in words if not parity]                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cb4458b2-93ad-4383-bf58-a521e8ea4 17a.py", line 3, in <listcomp>     odd_words = [_[1] for _, parity in words if not parity]                  ~^^^ TypeError: 'int' object is not subscriptable

--- Python_149.py
+++ Python_149.py
@@ -1,8 +1,5 @@
-```
 def sorted_list_sum(lst):
-    words = [(len(word), len(word) % 2 != 0) for word in lst]
-    words.sort()
-    even_words = [word for word, parity in words if not parity]
-    odd_words = [word for word, parity in words if parity]
-    return "".join([even_word[i] for even_word, _ in zip(even_words, range(len(even_words)))] + 
-                   ["", "".join([odd_word[i] for odd_word, _ in zip(odd_words, range(len(odd_words)))]), ""])
+    words = sorted([(len(word), len(word) % 2 != 0) for word in lst])
+    odd_words = [parity[1] for _, parity in words if not parity]
+    even_words = [parity[1] for _, parity in words if parity]
+    return "".join([word[i] for word, _ in zip(even_words, range(len(even_words)))]) + "".join([word[i] for word, _ in zip(odd_words, range(len(odd_words)))])
