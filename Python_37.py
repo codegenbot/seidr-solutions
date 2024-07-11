9f87cbe0a40ae8587698aed6484964106f8dd1c1
beep boop <githubbot@vadim.me>
2024-07-11 09:07:06 +0200

    Solution 7 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v70dnp3u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e49e9413-daf6-4a31-9260-1f467b8b1 d76.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e49e9413-daf6-4a31-9260-1f467b8b1 d76.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,10 +1,5 @@
-```
-def sort_even():
-    l = input("Enter a list of numbers: ").split()
-    l = [int(x) for x in l]
-    sorted_l = sorted(l)
-    odd = [x for x in sorted_l if x % 2 != 0]
-    even = [x for x in sorted_l if x % 2 == 0]
-    return [l[i] for i in range(len(l)) if i % 2 != 0] + even
-
-print(sort_even())
+def sort_even(l: list):
+    evens = [x for x in l if x % 2 == 0]
+    odds = [x for x in l if x % 2 != 0]
+    sorted_evens = sorted(evens)
+    return [x for i, x in enumerate(sorted_evens) if i % 2 != 0] + odds
