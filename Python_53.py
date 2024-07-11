043e7b7c368008a233fe3b9f6b6fdbd07917f331
beep boop <githubbot@vadim.me>
2024-07-11 23:16:39 +0200

    Solution 61 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kfq890s1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6fc026d6-6148-4ed0-b30a-69b4a7c14 9f1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,13 +1,21 @@
-name = input("Enter your name: ")
-age = int(input("Enter age: "))
-
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
+def main():
+    pass_value = "pass"
 
 
-def add(x: int, y: int):
-    return x + y
+while True:
+    x = int(
+        input("Enter first number: ")
+        if input("Is this run for a pass (yes/no)? ").lower() == "yes"
+        else None
+    )
 
+    if x is None:
+        break
 
-result = add(x, y)
-print(f"The sum of {x} and {y} is {result}")
+    y = int(input("Enter second number: "))
+
+    def add(x: int, y: int):
+        return x + y
+
+    result = add(x, y)
+    print(f"The sum of {x} and {y} is {result}")
