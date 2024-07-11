58c6055029796cf273b70dd18074259e09acaaa2
beep boop <githubbot@vadim.me>
2024-07-11 17:15:05 +0200

    Solution 17 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/volfybov Prompt:       |                    ^~~ 12f11c39-9969-4d35-8222-db622ce02867.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 12f11c39-9969-4d35-8222-db622ce02867.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 12f11c39-9969-4d35-8222-db622ce02867.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 12f11c39-9969-4d35-8222-db622ce02867.cpp:14: 12f11c39-9969-4d35-8222-db622ce02867.cpp: In function ‘int main()’: 12f11c39-9969-4d35-8222-db622ce02867.cpp:24:20: error: ‘even_odd_count’ was not declared in this scope    24 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 12f11c39-9969-4d35-8222-db622ce02867.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -2,6 +2,16 @@
 #include <string>
 #include <cassert>
 
+std::vector<int> even_odd_count(int num);
+bool issame(std::vector<int> a, std::vector<int> b);
+
+int main() {
+    std::vector<int> even_odd_count(int num);
+    std::vector<int> result = even_odd_count(123456789);
+    assert(issame(result, {4, 5}));
+    return 0;
+}
+
 std::vector<int> even_odd_count(int num){
     std::vector<int> result(2, 0);
     std::string numStr = std::to_string(std::abs(num));
@@ -17,10 +27,4 @@ std::vector<int> even_odd_count(int num){
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
-}
-
-int main() {
-    std::vector<int> count = even_odd_count(123456);
-    assert(issame(count, {3, 3}));
-    return 0;
 }
