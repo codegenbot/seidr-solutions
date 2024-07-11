35cf2ff69633888dcc6b69e16604572503c08036
beep boop <githubbot@vadim.me>
2024-07-11 06:34:13 +0200

    Solution 80 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zzo3yblj Prompt: 9fc0b7c1-c280-4651-8d9f-c32fbb7fc981.cpp:6:127: error: invalid types ‘const int[ int]’ for array subscript     6 |  abs(s[i] - s[i+1]) == 1 || abs(s[i] - s[i+2]) == 1 || abs(s[i+1] - s[i+ 2]) == 1)       |                                                             ^ 9fc0b7c1-c280-4651-8d9f-c32fbb7fc981.cpp:6:136: error: invalid types ‘const int[ int]’ for array subscript     6 |  - s[i+1]) == 1 || abs(s[i] - s[i+2]) == 1 || abs(s[i+1] - s[i+2]) == 1)       |                                                             ^ In file included from 9fc0b7c1-c280-4651-8d9f-c32fbb7fc981.cpp:13: 9fc0b7c1-c280-4651-8d9f-c32fbb7fc981.cpp: In function ‘int main()’: 9fc0b7c1-c280-4651-8d9f-c32fbb7fc981.cpp:15:22: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    15 |     assert (is_happy("iopaxioi") == false );       |                      ^~~~~~~~~~       |                      |       |                      const char* 9fc0b7c1-c280-4651-8d9f-c32fbb7fc981.cpp:1:29: note:   initializing argument 1 o f ‘bool is_happy(const int&)’     1 | bool is_happy(const string& s) {       |               ~~~~~~~~~~~~~~^

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,14 +1,11 @@
 #include <string>
-#include <iostream>
 #include <cassert>
 
-using namespace std;
-
-bool is_happy(const string& s) {
+bool is_happy(const std::string& s) {
     if(s.length() < 3)
         return false;
 
-    for(int i = 0; i < s.length() - 2; ++i){
+    for(size_t i = 0; i < s.length() - 2; ++i){
         if(s[i] == s[i+1] || s[i] == s[i+2] || s[i+1] == s[i+2] || abs(static_cast<int>(s[i]) - static_cast<int>(s[i+1])) == 1 || abs(static_cast<int>(s[i]) - static_cast<int>(s[i+2])) == 1 || abs(static_cast<int>(s[i+1]) - static_cast<int>(s[i+2])) == 1)
             return false;
     }
@@ -17,7 +14,7 @@ bool is_happy(const string& s) {
 }
 
 int main() {
-    assert(is_happy("iopaxioi") == false );
+    assert(is_happy("iopaxioi") == false);
     
     return 0;
 }
