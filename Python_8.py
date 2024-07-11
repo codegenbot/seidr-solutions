d3e70a0344bcc45a25987db0fabd598a03b48444
beep boop <githubbot@vadim.me>
2024-07-11 11:53:16 +0200

    Solution 10 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/wlbnr5ac Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e9abd746-cce2-446f-a0a1-59f143959 e2e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -1,14 +1,10 @@
-from typing import List, Tuple
-
-
-def sum_product(numbers: List[int]) -> Tuple[int, int]:
-    if len(numbers) == 0:
-        return "Error: Input list is empty"
+```
+def sum_product(numbers):
+    if not numbers:
+        return 0, 1
     total_sum = 0
     product = 1
-
     for num in numbers:
         total_sum += num
         product *= num
-
-    return (total_sum, product)
+    return total_sum, product
