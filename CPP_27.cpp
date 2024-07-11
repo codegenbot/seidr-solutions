d8aaee60e05d727c24d1ab7d54dbf3a1792efe78
beep boop <githubbot@vadim.me>
2024-07-11 22:41:19 +0200

    Solution 91 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/urg735i0 Prompt: face165d-834d-4146-9db4-1f212534ce96.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ face165d-834d-4146-9db4-1f212534ce96.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from face165d-834d-4146-9db4-1f212534ce96.cpp:23: face165d-834d-4146-9db4-1f212534ce96.cpp: In function ‘int main()’: face165d-834d-4146-9db4-1f212534ce96.cpp:25:13: error: ‘filp_case’ was not decla red in this scope; did you mean ‘flip_case’?    25 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -16,6 +16,6 @@ std::string flip_case(std::string str) {
 
 int main {
     assert(flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
-
+    
     return 0;
 }
