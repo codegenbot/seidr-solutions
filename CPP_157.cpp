82ac927bd23c42d3a6cb63a2f6f2acf26b951e8c
beep boop <githubbot@vadim.me>
2024-07-11 02:27:14 +0200

    Solution 14 of CPP/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xsowexf3 Prompt: 297f7ad2-593a-411b-919e-b9ce10f923b3.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 297f7ad2-593a-411b-919e-b9ce10f923b3.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,4 +1,6 @@
-#include <cassert>
+```c++
+#include <bits/stdc++.h>
+using namespace std;
 
 bool right_angle_triangle(float a, float b, float c) {
     if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
@@ -8,6 +10,11 @@ bool right_angle_triangle(float a, float b, float c) {
 }
 
 int main() {
-    assert(right_angle_triangle(2, 2, 10) == false);
-    return 0;
+    float side1,side2,side3;
+    cout<<"Enter sides of triangle (in cm):"<<endl;
+    cin>>side1>>side2>>side3;
+    if(side1+side2>side3 && right_angle_triangle(side1,side2,side3)) 
+        cout<< "The given sides form a right-angled triangle." << endl;  
+    else
+        cout << "The given sides do not form a right-angled triangle." << endl;
 }
