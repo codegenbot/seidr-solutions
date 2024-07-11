3b567a6e9b4806a695f1893f0ceb6c6f27331e41
beep boop <githubbot@vadim.me>
2024-07-11 13:40:59 +0200

    Solution 24 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tdfielxa Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51724c1e-3af2-4d4e-8728-0c386694a fca.py", line 17, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51724c1e-3af2-4d4e-8728-0c386694a fca.py", line 14, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,12 +1,22 @@
+```
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
         return string + string
 
     new_str = ""
-    for i in range(len(string)):
-        if i > (len(string) - i) // 2 and string[i:] == string[i:][::-1]:
-            new_str += string[i]
+    left, right = 0, len(string) - 1
+    while left <= right:
+        if string[left:] == string[left:][::-1] and string[right:] == string[right:][::-1]:
+            new_str += string[left]
+            left += 1
+        elif string[right:] == string[right:][::-1]:
+            new_str += string[right]
+            right -= 1
         else:
-            new_str += string[-i - 1]
+            if string[left] != string[right]:
+                return ""
+            new_str += string[left]
+            left += 1
+            right -= 1
 
     return new_str + new_str[::-1]
