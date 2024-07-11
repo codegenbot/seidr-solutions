1a50231c9b7395a596a1613160a26a86056f0711
beep boop <githubbot@vadim.me>
2024-07-11 17:02:30 +0200

    Solution 5 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/dckwey15 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,87 +1,27 @@
 int bowlingScore(string s) {
     int score = 0;
-    int currentFrame = 1;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'X') {
-            score += 10 + (currentFrame < 10 ? getBonus(s, i+1) : 0);
-            currentFrame++;
-        } else if (s[i] == '/') {
-            int ballsLeft = 2;
-            while (ballsLeft--) {
-                if (s[i+1] == 'X') {
-                    score += 10 + (currentFrame < 10 ? getBonus(s, i+2) : 0);
-                    currentFrame++;
-                    break;
-                } else {
-                    int pins = s[i+1] - '0';
-                    if (pins == 10) {
-                        score += 10;
-                    } else {
-                        score += pins;
-                    }
-                    i++; ballsLeft--;
-                }
+    int rolls = 0;
+    int currentRoll = 0;
+    
+    for (char c : s) {
+        if (c != '/') {
+            currentRoll++;
+            if (isdigit(c)) {
+                score += c - '0';
+            } else if (c == 'X') {
+                score += 10;
+                currentRoll = 0;
             }
-            currentFrame++;
         } else {
-            int ballsLeft = 2;
-            while (ballsLeft--) {
-                if (s[i+1] == 'X') {
-                    score += 10 + getBonus(s, i+2);
-                    break;
-                } else {
-                    int pins = s[i] - '0' + s[i+1] - '0';
-                    if (pins > 10) {
-                        pins = 10;
-                    }
-                    score += pins;
-                    i++; ballsLeft--;
-                }
+            if (currentRoll > 1) {
+                score += currentRoll;
             }
+            currentRoll = 0;
         }
+        
+        rolls++;
+        if (rolls >= 20) break;
     }
+    
     return score;
-}
-
-int getBonus(string s, int start) {
-    int bonus = 0;
-    for (int i = start; i < s.length(); i++) {
-        if (s[i] == 'X') {
-            bonus += 10 + (i+1 < s.length() ? getBonus(s, i+1) : 0);
-            break;
-        } else if (s[i] == '/') {
-            int ballsLeft = 2;
-            while (ballsLeft--) {
-                if (s[i+1] == 'X') {
-                    bonus += 10 + getBonus(s, i+2);
-                    break;
-                } else {
-                    int pins = s[i+1] - '0';
-                    if (pins == 10) {
-                        bonus += 10;
-                    } else {
-                        bonus += pins;
-                    }
-                    i++; ballsLeft--;
-                }
-            }
-            break;
-        } else {
-            int ballsLeft = 2;
-            while (ballsLeft--) {
-                if (s[i+1] == 'X') {
-                    bonus += 10 + getBonus(s, i+2);
-                    break;
-                } else {
-                    int pins = s[i] - '0' + s[i+1] - '0';
-                    if (pins > 10) {
-                        pins = 10;
-                    }
-                    bonus += pins;
-                    i++; ballsLeft--;
-                }
-            }
-        }
-    }
-    return bonus;
 }
