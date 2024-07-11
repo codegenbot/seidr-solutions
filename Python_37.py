3a92ec3b354c75c515fc7f6bbc121eeb8fc6c78b
beep boop <githubbot@vadim.me>
2024-07-11 10:27:25 +0200

    Solution 86 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v70dnp3u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a90d2af7-4631-49ee-b49b-36d4f8c47 bc3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_37.py
+++ Python_37.py
@@ -1,12 +1,11 @@
+```
 def sort_even(l):
     return sorted([x for x in l if x % 2 == 0])
-
 
 def task_description():
     num_list = input("Enter numbers separated by comma: ").split(",")
     num_list = [int(i) for i in num_list]
     print(sort_even(num_list))
-
 
 if __name__ == "__main__":
     task_description()
