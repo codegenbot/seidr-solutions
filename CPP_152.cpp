c7be533907fdc8e2e1bdd03a7041aeca8a0c5558
beep boop <githubbot@vadim.me>
2024-07-11 13:17:00 +0200

    Solution 96 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6e857rqz Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 3a91ec4a-c54b-464a-aeb5-9ca545acd189.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,6 +1,5 @@
-```cpp
 #include <vector>
-#include <algorithm>
+#include <cassert>
 
 using namespace std;
 
@@ -8,41 +7,21 @@ bool issame(vector<int> a, vector<int> b) {
     return (a.size() == b.size()) && equal(a.begin(), a.end(), b.begin());
 }
 
-vector<int> compare(vector<vector<int>> gameGuessList) {
-    vector<vector<int>> result;
-    
-    for(int i = 0; i < gameGuessList.size(); i++) {
-        vector<int> game, guess;
-        
-        // Separate game and guess from the pair
-        for(int j = 0; j < 2; j++) {
-            if(j == 0) {
-                game = gameGuessList[i];
-            }
-            else {
-                guess = gameGuessList[i];
-            }
+vector<int> compare(vector<int> game, vector<int> guess) {
+    vector<int> result;
+    for (int i = 0; i < game.size(); i++) {
+        if (game[i] == guess[i]) {
+            result.push_back(0);
+        } else {
+            result.push_back(abs(game[i] - guess[i]));
         }
-
-        vector<int> temp;
-        
-        // Compare game and guess and store the result
-        for(int k = 0; k < game.size(); k++) {
-            if(game[k] == guess[k]) {
-                temp.push_back(0);
-            } else {
-                temp.push_back(abs(game[k] - guess[k]));
-            }
-        }
-        
-        // Add the temporary result to final result
-        result.push_back(temp);
     }
-    
     return result;
 }
 
 int main() {
-    assert(issame(compare({{1, 2, 3, 5}, {-1, 2, 3, 4}}), {{2, 0, 0, 1}}));
+    vector<int> game = vector<int>(4); 
+    game[0] = 1; game[1] = 2; game[2] = 3; game[3] = 5;
+    assert(issame(compare(game, vector<int>({-1, 2, 3, 4})), vector<int>(2, 0, 0, 1)));
     return 0;
 }
