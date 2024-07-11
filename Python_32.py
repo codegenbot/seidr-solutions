9207efd718ce4aa753f6de3da525119b814c941a
beep boop <githubbot@vadim.me>
2024-07-11 15:07:16 +0200

    Solution 74 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/01a7l32z Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7657847b-7bc4-4138-8f55-d0d168fcb bc0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,22 +1,19 @@
-def find_zero(xs):
+def find_zero():
+    xs = list(map(int, input("Enter the coefficients: ").split()))
     if len(xs) % 2 != 1:
         return "Error: xs must start with 0"
-    elif xs[0] != 0:
+    if xs[0] != 0:
         return "Error: xs must be [0, *coefficients*]"
-    else:
-        x = xs[1]
-        y = xs[3:]
-
-        for i in range(len(y)):
-            if y[i] == 0 and (i % 2 == 0 or len(y) % 2 != i):
-                return "Error: xs must be [0, *coefficients*]"
-
-        if sum(y) == 0:
-            return None
-        elif x[1:] != y:
-            return None
-        else:
-            if len(x) % 2 != 0:
-                return None
-            n = int(sum(x) / (sum(y) or 1))
-            return n
+    x = xs[1]
+    y = xs[3:]
+    for i in range(len(y)):
+        if y[i] == 0 and (i % 2 == 0 or len(y) % 2 != i):
+            return "Error: xs must be [0, *coefficients*]"
+    if sum(y) == 0:
+        return None
+    if x[1:] != y:
+        return None
+    if len(x) % 2 != 0:
+        return None
+    n = int(sum(x) / (sum(y) or 1))
+    return n
