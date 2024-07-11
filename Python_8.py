2e3e7cb30cac5e7dfdfb70e3e43c27d9441e2523
beep boop <githubbot@vadim.me>
2024-07-11 10:30:04 +0200

    Solution 86 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9fdnc5i0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c8671b31-b271-4d06-824b-9498052a5 2b4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -1,20 +1,37 @@
 from typing import Tuple
 import statistics
 
+
 def calculate_average_and_median() -> Tuple[float, float]:
     numbers = []
     while True:
         try:
-            user_input = input("Enter numbers separated by space (or type 'q' to quit): ")
-            if not user_input or user_input.lower() == 'q':
+            user_input = input(
+                "Enter numbers separated by space (or type 'q' to quit): "
+            )
+            if not user_input or user_input.lower() == "q":
                 break
-            numbers.extend([int(num) for num in user_input.split() if num.replace('-', '',).replace('.', '').isnumeric()])
+            numbers.extend(
+                [
+                    int(num)
+                    for num in user_input.split()
+                    if num.replace(
+                        "-",
+                        "",
+                    )
+                    .replace(".", "")
+                    .isnumeric()
+                ]
+            )
         except ValueError:
-            print("Invalid input. Please enter numbers separated by space or type 'q' to quit.")
+            print(
+                "Invalid input. Please enter numbers separated by space or type 'q' to quit."
+            )
     if len(set(numbers)) < 2:
-        return sum(numbers) / len(numbers), float('nan')
+        return sum(numbers) / len(numbers), float("nan")
     else:
         return sum(numbers) / len(numbers), statistics.median(sorted(set(numbers)))
+
 
 average, median = calculate_average_and_median()
 print(f"The average is {average} and the median is {median}.")
