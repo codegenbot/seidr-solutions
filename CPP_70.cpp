08c4807d5941e61ceb529704f3e4a2d11acfaac1
beep boop <githubbot@vadim.me>
2024-07-12 01:49:59 +0200

    Solution 36 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/nfplvphz Prompt:       |                           ^~~~~~ db9c64d0-0fe2-47d0-8b14-adbd0477f620.cpp:30:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from db9c64d0-0fe2-47d0-8b14-adbd0477f620.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from db9c64d0-0fe2-47d0-8b14-adbd0477f620.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ db9c64d0-0fe2-47d0-8b14-adbd0477f620.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ db9c64d0-0fe2-47d0-8b14-adbd0477f620.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ db9c64d0-0fe2-47d0-8b14-adbd0477f620.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -6,14 +6,14 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> strange_sort_list(std::vector<int> lst){
+std::vector<int> strangeSortList(std::vector<int> lst){
     std::sort(lst.begin(), lst.end());
     std::vector<int> result;
     int left = 0, right = lst.size() - 1;
     while (left <= right) {
-        result.push_back(lst[right]);
+        result.push_back(lst[left]);
         if (left != right) {
-            result.push_back(lst[left]);
+            result.push_back(lst[right]);
         }
         left++;
         right--;
@@ -22,6 +22,6 @@ std::vector<int> strange_sort_list(std::vector<int> lst){
 }
 
 int main() {
-    assert(issame(strange_sort_list({111111}), {111111}));
+    assert(issame(strangeSortList({111111}), {111111}));
     return 0;
 }
