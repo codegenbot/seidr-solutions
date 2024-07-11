a920b986c5243de0c3ceb19e55aa26de8516174f
beep boop <githubbot@vadim.me>
2024-07-11 22:06:36 +0200

    Solution 64 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/wpktily2 Prompt:       |                             ^~~~~~ bb0c7312-3fc1-4128-b7d3-19c5e79c4b59.cpp:24:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from bb0c7312-3fc1-4128-b7d3-19c5e79c4b59.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from bb0c7312-3fc1-4128-b7d3-19c5e79c4b59.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ bb0c7312-3fc1-4128-b7d3-19c5e79c4b59.cpp:24:36: error: expected primary-expressi on before ‘float’    24 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ bb0c7312-3fc1-4128-b7d3-19c5e79c4b59.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ bb0c7312-3fc1-4128-b7d3-19c5e79c4b59.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -16,6 +16,6 @@ std::vector<float> get_positive(std::vector<float> l) {
 }
 
 int main() {
-    assert(issame(get_positive({}), std::vector<float>{}));
+    assert(issame(get_positive({}), {}));
     return 0;
 }
