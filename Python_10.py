86dbfdc4f58a4746a7cb39b810c3a0b6751ada7f
beep boop <githubbot@vadim.me>
2024-07-11 14:28:46 +0200

    Solution 28 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xye2igmr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/348fa3b8-1679-46fa-a706-efce94ab8 5ff.py", line 16, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/348fa3b8-1679-46fa-a706-efce94ab8 5ff.py", line 13, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,9 +1,8 @@
-```
 def is_palindrome(string: str) -> bool:
     """Test if given string is a palindrome"""
     return string == string[::-1]
 
 
 def make_palindrome(string: str) -> str:
-    s = ''.join(e for e in string if e.isalnum()).lower()
+    s = "".join(e for e in string if e.isalnum()).lower()
     return s + s[::-1]
