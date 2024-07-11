9e8f5f4962008b02781b74487c50108726087cc2
beep boop <githubbot@vadim.me>
2024-07-11 11:42:32 +0200

    Solution 3 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/54byhyxx Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/191e0bfa-c177-4788-a17d-b075f1036 328.py", line 18, in <module>     check(simplify)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/191e0bfa-c177-4788-a17d-b075f1036 328.py", line 15, in check     assert simplify("1/5", "1/5") == False, "test13"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test13

--- Python_144.py
+++ Python_144.py
@@ -1,6 +1,12 @@
-```python
+```
 def simplify(x, n):
-    x_num, x_denom = map(int, x.split('/'))
-    n_num, n_denom = map(int, n.split('/'))
-
-    return (x_num * n_denom) % (x_denom * n_num) == 0
+    x_num, x_denom = map(int, [i for i in x.split('/')])
+    n_num, n_denom = map(int, [i for i in n.split('/')])
+    
+    if str(x_num/x_denom) == str(n_num/n_denom):
+        return False
+    else:
+        gcd = lambda a, b: a if not b else gcd(b, a % b)
+        common_divisor = gcd(x_denom, n_denom)
+        
+        return str(x_num // common_divisor) + '/' + str(x_denom // common_divisor), str(n_num // common_divisor) + '/' + str(n_denom // common_divisor)
