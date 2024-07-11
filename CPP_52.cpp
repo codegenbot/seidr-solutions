447517f6327d1baba62d3515c4df370750e38070
beep boop <githubbot@vadim.me>
2024-07-11 16:40:08 +0200

    Solution 18 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/u00nofpt Prompt: 67d6b9c6-bed6-4c67-bc5d-024779bff165.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ 67d6b9c6-bed6-4c67-bc5d-024779bff165.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -12,12 +12,4 @@ bool below_threshold(std::vector<int> l, int t) {
 
 }
 
-int main() {
-    std::vector<int> numbers;
-    numbers.push_back(1);
-    numbers.push_back(8);
-    numbers.push_back(4);
-    numbers.push_back(10);
-
-    std::cout << (below_threshold(numbers, 10) ? "True\n" : "False\n");
-}
+std::cout << (below_threshold(numbers, 10) ? "True\n" : "False\n");
