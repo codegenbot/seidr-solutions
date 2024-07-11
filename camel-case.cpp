2186fbfeac15cbf57d3c83a58e41b6febe0ca318
beep boop <githubbot@vadim.me>
2024-07-11 12:47:20 +0200

    Solution 2 of camel-case, ratio 0.82 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/1gw4933o Prompt:     3 |     getline(cin, input);       |             ^~~ 7fa7f1b5-dd5d-4e50-9574-a96630b6a3be.cpp:3:18: error: ‘input’ was not declared i n this scope; did you mean ‘int’?     3 |     getline(cin, input);       |                  ^~~~~       |                  int 7fa7f1b5-dd5d-4e50-9574-a96630b6a3be.cpp:3:5: error: ‘getline’ was not declared in this scope     3 |     getline(cin, input);       |     ^~~~~~~ 7fa7f1b5-dd5d-4e50-9574-a96630b6a3be.cpp:10:13: error: ‘cout’ was not declared i n this scope    10 |             cout << (char)toupper(c);       |             ^~~~ 7fa7f1b5-dd5d-4e50-9574-a96630b6a3be.cpp:10:27: error: ‘toupper’ was not declare d in this scope    10 |             cout << (char)toupper(c);       |                           ^~~~~~~ 7fa7f1b5-dd5d-4e50-9574-a96630b6a3be.cpp:13:13: error: ‘cout’ was not declared i n this scope    13 |             cout << c;       |             ^~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -11,7 +11,7 @@ int main() {
         if (c == '-') {
             capitalize = true;
         } else if (capitalize) {
-            std::cout << (char)std::toupper(c);
+            std::cout << (char)toupper(c);
             capitalize = false;
         } else {
             std::cout << c;
