f6f3598a9e5f9586ca7649a1fc98bbeb71421830
beep boop <githubbot@vadim.me>
2024-07-11 15:35:17 +0200

    Solution 90 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/4k12p6tg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1b0e413f-ae6e-4c77-bc1e-6432abd3a 6c6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_1.py
+++ Python_1.py
@@ -1,26 +1,24 @@
-``` 
-from typing import List
-
-def separate_paren_groups(paren_string: str) -> List[str]:
-    result = []
-    stack = []
-    groups = []
-
-    for char in paren_string:
-        if char == ' ': 
-            continue
-        if char in ['(', '[', '{']:
-            stack.append(char)
-            groups.append('')
-        elif char in {')': ')', ']': '[', '}': '{'}[char]:
-            if stack: 
-                top = stack.pop()
-                groups[-1] += char
-                if not stack and {')': ')', ']': '[', '}': '{'}[char] == {')': ')', ']': '[', '}': '{'}[top]:
-                    result.append(groups.pop().lstrip().rstrip())
-            else:
-                while stack and (stack[-1] in ['(', '[']) and {')': ')', ']': '[', '}': '{'}[char] != {'(': ')', '[': ']', '{': '}'}[stack[-1]]:
-                    stack.pop()
-                if not stack and {')': ')', ']': '[', '}': '{'}[char] == {')': ')', ']': '[', '}': '{'}['({'.index(top)]:
-                    result.append(groups.pop().lstrip().rstrip())
-    return result
+```
+'def separate_paren_groups(paren_string: str) -> List[str]:\n'\
+'    result = []\n'\
+'    stack = []\n'\
+'    groups = []\n'\
+'\n'\
+'    for char in paren_string:\n'\
+'        if char == " ": \n'\
+'            continue\n'\
+'        if char in ["(", "[", "{"]:\n'\
+'            stack.append(char)\n'\
+'            groups.append("")\n'\
+'        elif char in {")": ")", "]": "[", "}": "{"}[char]:\n'\
+'            if stack: \n'\
+'                top = stack.pop()\n'\
+'                groups[-1] += char\n'\
+'                if not stack and {")": ")", "]": "[", "}": "{"}[char] == {")": ")", "]": "[", "}": "{"}[top]:\n'\
+'                    result.append(groups.pop().lstrip().rstrip())\n'\
+'            else:\n'\
+'                while stack and (stack[-1] in ["(", "["] and {")": ")", "]": "[", "}": "{"}[char] != {"(": ")","[": ']', "{': '}'}[stack[-1]]:\n'\
+'                    stack.pop()\n'\
+'                if not stack and {")": ")", "]": "[", "}": "{"}[char] == {")": ")", "]": "[", "}": "{"}["({".index(top)]:\n'\
+'                    result.append(groups.pop().lstrip().rstrip())\n'\
+'    return result\n'
