15ab6b505f7ba41f7a13d353a36f7fea0fcf2a7a
beep boop <githubbot@vadim.me>
2024-07-11 05:05:25 +0200

    Solution 3 of Python/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ds1u6n24 Prompt: Complete the following code given the task description and function signature.

--- Python_71.py
+++ Python_71.py
@@ -1,8 +1,9 @@
 ```
+import math
+
 def triangle_area(a, b, c):
     if a + b > c and a + c > b and b + c > a:
         s = (a + b + c) / 2
-        area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-        return round(area, 2)
+        return round(math.sqrt(s * (s - a) * (s - b) * (s - c)), 2)
     else:
         return -1
