65ff520b912f53123ff594a1557506f6be6417dc
beep boop <githubbot@vadim.me>
2024-07-11 02:55:11 +0200

    Solution 93 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/sr9olerd Prompt: Program did not receive expected input

--- Python_154.py
+++ Python_154.py
@@ -1,6 +1,6 @@
 ```
 def cycpattern_check(a, b):
-    if len(b) > len(a) // 2:
+    if len(b) > len(a):
         return False
     for i in range(len(a)):
         if a[i:i+len(b)].casefold() == b.casefold():
