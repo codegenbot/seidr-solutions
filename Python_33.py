f27a57a8e689fbc31f09e23bc0679f26e4edfe89
beep boop <githubbot@vadim.me>
2024-07-11 08:54:25 +0200

    Solution 4 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b61573ad-9d39-4595-ab2e-b8ee6b0ad a4b.py", line 9     ]     ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '(' on l ine 3

--- Python_33.py
+++ Python_33.py
@@ -1,7 +1,9 @@
 def sort_third(l: list):
     return [
-        (x
-         if i % 3 != 0
-         else next((y for y in l if (i - l.index(y)) % 3 != 0), None).sort()[-1])
+        (
+            x
+            if i % 3 != 0
+            else sorted([y for y in l if (i - l.index(y)) % 3 != 0])[-1]
+        )
         for i, x in enumerate(l)
     ]
