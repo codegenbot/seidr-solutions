efc4b7557cdac52205c3689ee91a8440c3b772ea
beep boop <githubbot@vadim.me>
2024-07-11 10:37:27 +0200

    Solution 3 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/b0fi23t3 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5ccdd7a2-979f-43e1-95ef-8f7becf20 336.py", line 5, in <module>     print(solve_boolean(input_str))           ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5ccdd7a2-979f-43e1-95ef-8f7becf20 336.py", line 2, in solve_boolean     return eval(expression.replace("T", "True").replace("F ", "False"))            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "<string>", line 1, in <module> NameError: name 'f' is not defined

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,5 +1,2 @@
 def solve_boolean(expression):
-    return eval(f'({expression.replace("T", "True").replace("F", "False")})')
-
-input_str = input()
-print(solve_boolean(input_str))
+    return eval(expression.replace("T", "True").replace("F", "False"))
