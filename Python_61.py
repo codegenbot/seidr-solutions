d50ef98b320180976acd4de796dee144346d4ab1
beep boop <githubbot@vadim.me>
2024-07-11 20:53:54 +0200

    Solution 7 of Python/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8fe3f7wt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c3e41928-2606-4159-9d1e-9b9ae1f53 bf4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_61.py
+++ Python_61.py
@@ -1,10 +1,14 @@
-def most_common_letter(word: str):
-    letter_count = {}
-    for letter in word:
-        if letter in letter_count:
-            letter_count[letter] += 1
-        else:
-            letter_count[letter] = 1
-    max_count = max(letter_count.values())
-    common_letters = [k for k, v in letter_count.items() if v == max_count]
-    return sorted(common_letters)[0]
+def find_max_sum(lst):
+    max_sum = float("-inf")
+    current_sum = 0
+
+    for num in lst:
+        current_sum += num
+
+        if current_sum > max_sum:
+            max_sum = current_sum
+
+        if current_sum < 0:
+            current_sum = 0
+
+    return max_sum
