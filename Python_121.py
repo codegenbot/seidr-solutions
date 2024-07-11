e45a6b8edbf729f540b246ed10383b976a5439f9
beep boop <githubbot@vadim.me>
2024-07-11 18:16:14 +0200

    Solution 64 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ebtie55w Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb0df8f6-b4bd-41cf-af98-a05992378 271.py", line 17, in <module>     check(solution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb0df8f6-b4bd-41cf-af98-a05992378 271.py", line 12, in check     assert solution([3, 13, 2, 9]) == 3            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_121.py
+++ Python_121.py
@@ -2,7 +2,7 @@ def solution(lst):
     if len(lst) == 0:
         pass
     elif len(lst) % 2 != 0:
-        odd_sum = sum(i for i in lst[:1] + lst[1::2] if i % 2 != 0)
+        odd_sum = sum(i for i in lst[:1] + lst[1:] if i % 2 != 0)
     else:
-        odd_sum = sum(i for i in lst[1::2] if i % 2 != 0)
+        odd_sum = sum(i for i in lst[1:] if i % 2 != 0)
     return odd_sum
