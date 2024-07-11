3568c62d8e21235c3c46dbbf5917b5d22a7310b7
beep boop <githubbot@vadim.me>
2024-07-11 23:06:29 +0200

    Solution 60 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/pmqokffl Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f7418fd5-c0da-4d14-ad1d-a5343a212 c2c.py", line 13, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f7418fd5-c0da-4d14-ad1d-a5343a212 c2c.py", line 8, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,7 +1,10 @@
 def sort_third(l: list):
-    l += [0] * (3 - len(l) % 3)
+    padded_list = l + [float("inf")] * (3 - len(l) % 3)
     return [
         value
-        for sublist in [sorted(l[i : i + 3]) for i in range(0, len(l), 3)]
+        for sublist in [
+            sorted(padded_list[i : i + 3]) for i in range(0, len(padded_list), 3)
+        ]
         for value in sublist
+        if value != float("inf")
     ]
