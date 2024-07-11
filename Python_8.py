2df4cae580ae1ebc839bd2061f3f563829650de2
beep boop <githubbot@vadim.me>
2024-07-11 04:41:09 +0200

    Solution 32 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/p2jf7nxt Prompt: Program did not receive expected input

--- Python_8.py
+++ Python_8.py
@@ -3,12 +3,15 @@ from typing import List, Tuple
 
 
 def sum_product(numbers: List[int]) -> Tuple[int, int]:
+    if isinstance(numbers, tuple) and len(numbers) == 0:
+        return (0, 1)
+    elif not isinstance(numbers, list):
+        return "Invalid Input"
+
     total_sum = 0
     product = 1
 
     for num in numbers:
-        if not isinstance(num, int):
-            raise TypeError("The list must only contain integers.")
         total_sum += num
         product *= num
 
