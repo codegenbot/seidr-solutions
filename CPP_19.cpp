443995700f87e0c705de0fdcb96d6dd6098a9411
beep boop <githubbot@vadim.me>
2024-07-11 04:38:01 +0200

    Solution 16 of CPP/19, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vu3dgd1z Prompt: 525ad967-0854-4f1e-b9eb-78647189d9f9.cpp:6:1: error: ‘string’ does not name a ty pe     6 | string sort_numbers(string numbers) {       | ^~~~~~ In file included from 525ad967-0854-4f1e-b9eb-78647189d9f9.cpp:30: 525ad967-0854-4f1e-b9eb-78647189d9f9.cpp: In function ‘int main()’: 525ad967-0854-4f1e-b9eb-78647189d9f9.cpp:32:13: error: ‘sort_numbers’ was not de clared in this scope    32 |     assert (sort_numbers("six five four three two one zero") == "zero on e two three four five six");       |             ^~~~~~~~~~~~

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,35 +1,29 @@
-#include <algorithm>
 #include <vector>
+#include <algorithm>
+#include <string>
+
 using namespace std;
 
-string sort_numbers(string numbers) {
-    vector<int> numValues;
-    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                                {"eight", 8}, {"nine", 9}};
+std::string sort_numbers(std::string numbers) {
+    vector<string> numVec;
+    string temp;
 
-    string numStr;
-    for (char c : numbers) {
-        if (c == ' ') {
-            numValues.push_back(numMap[numStr]);
-            numStr = "";
+    for (int i = 0; i < numbers.length(); i++) {
+        if (numbers[i] == ' ') {
+            numVec.push_back(temp);
+            temp = "";
         } else {
-            numStr += c;
+            temp += numbers[i];
         }
     }
-    numValues.push_back(numMap[numStr]);
+    numVec.push_back(temp);
 
-    sort(numValues.begin(), numValues.end());
-
+    sort(numVec.begin(), numVec.end());
+    
     string result = "";
-    for (int n : numValues) {
-        for (auto it = numMap.begin(); it != numMap.end(); ++it) {
-            if (n == it->second) {
-                result += it->first + " ";
-                break;
-            }
-        }
+    for (int i = 0; i < numVec.size(); i++) {
+        result += numVec[i] + " ";
     }
-
-    return result.substr(0, result.length() - 1);
+    
+    return result;
 }
