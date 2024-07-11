5be7095a4089adf9a0c6893997a3c48526de65e2
beep boop <githubbot@vadim.me>
2024-07-11 09:30:33 +0200

    Solution 4 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/dlv2ukls Prompt: 3e8cca21-434f-46a5-8f13-88a1bed818e5.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,27 +1,20 @@
-int bowlingScore(string s) {
+int bowlingScore(std::string s) {
     int score = 0;
-    bool lastRoll = false;
-    
-    for (int i = 0; i < 10; i++) {
-        if (s[i] == 'X') { // Strike
-            score += 10 + (i < 8 ? 10 : 0);
-            lastRoll = true;
-        } else if (s[i] == '/') { // Spare
+    int roll = 0;
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == '/') {
+            if(roll < 2) {
+                score += (10 - '0') * 10 + (10 - 'X');
+            } else {
+                score += (10 - 'X') * 10;
+            }
+            roll = 0;
+        } else if('0' <= s[i] && s[i] <= '9') {
+            roll = roll * 10 + (s[i] - '0');
+        } else if(s[i] == 'X') {
             score += 10;
-            lastRoll = true;
-        } else {
-            int roll1 = s[i] - '0';
-            int roll2 = s[i + 1] - '0';
-            if (lastRoll) {
-                return -1; // Invalid input
-            }
-            score += roll1 + roll2;
-            lastRoll = false;
-            if (roll1 == 10) { // Next two rolls are not part of this frame
-                i++;
-            }
+            roll = 0;
         }
     }
-    
     return score;
 }
