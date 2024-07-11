8cf3479903b72bcb5f1d390008c51e1708a06d0f
beep boop <githubbot@vadim.me>
2024-07-11 05:01:35 +0200

    Solution 31 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/gd44lxb8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9a6af0e8-2590-4648-b13a-03500a411 33f.py", line 1, in <module>     def find_closest_elements(numbers: List[float]) -> Tuple[float, float]:                                        ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_20.py
+++ Python_20.py
@@ -1,5 +1,6 @@
 ```
 from typing import List, Tuple
+
 def find_closest_elements(numbers: List[float]) -> Tuple[float, float]:
     min_diff = float("inf")
     closest_pair = None
@@ -8,8 +9,5 @@ def find_closest_elements(numbers: List[float]) -> Tuple[float, float]:
             diff = abs(numbers[i] - numbers[j])
             if diff < min_diff:
                 min_diff = diff
-                closest_pair = (
-                    min(numbers[i], numbers[j]),
-                    max(numbers[i], numbers[j]),
-                )
+                closest_pair = (min(numbers[i], numbers[j]), max(numbers[i], numbers[j]))
     return closest_pair
