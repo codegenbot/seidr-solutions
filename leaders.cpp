e7fae567a3fde118b4febb2a2a845947486cc5b8
beep boop <githubbot@vadim.me>
2024-07-11 02:42:49 +0200

    Solution 71 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/nedoexr3 Prompt: Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.

--- leaders.cpp
+++ leaders.cpp
@@ -1,51 +1,18 @@
 #include <vector>
 using namespace std;
 
-vector<int> leaders(vector<int>& arr) {
-    int n = arr.size();
-    vector<int> leaders;
-    
-    for (int i = n - 1; i >= 0; --i) {
-        bool isLeader = true;
-        for (int j = i + 1; j < n; ++j) {
-            if (arr[j] >= arr[i]) {
-                isLeader = false;
+vector<int> leaders(vector<int>& v) {
+    vector<int> result;
+    int n = v.size();
+    for(int i=n-1; i>=0; i--) {
+        bool leader = true;
+        for(int j=i+1; j<n; j++) {
+            if(v[j] >= v[i]) {
+                leader = false;
                 break;
             }
         }
-        
-        if (isLeader) leaders.push_back(arr[i]);
+        if(leader) result.push_back(v[i]);
     }
-    
-    return leaders;
-}
-
-int main() {
-    // test the code
-    vector<int> arr1 = {0};
-    vector<int> result1 = leaders(arr1);
-    for (auto &i : result1) cout << i << " ";
-    cout << endl;
-
-    vector<int> arr2 = {1, 0};
-    vector<int> result2 = leaders(arr2);
-    for (auto &i : result2) cout << i << " ";
-    cout << endl;
-
-    vector<int> arr3 = {1, 451};
-    vector<int> result3 = leaders(arr3);
-    for (auto &i : result3) cout << i << " ";
-    cout << endl;
-
-    vector<int> arr4 = {2, 1000, 0};
-    vector<int> result4 = leaders(arr4);
-    for (auto &i : result4) cout << i << " ";
-    cout << endl;
-
-    vector<int> arr5 = {2, 0, 1000};
-    vector<int> result5 = leaders(arr5);
-    for (auto &i : result5) cout << i << " ";
-    cout << endl;
-    
-    return 0;
+    return result;
 }
