b62dcf62d65874e19a0aa87c3dde12a1e8526393
beep boop <githubbot@vadim.me>
2024-07-11 12:03:50 +0200

    Solution 33 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zkvvbdxl Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ cee88c03-6679-4951-98a2-727cc6197c84.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ cee88c03-6679-4951-98a2-727cc6197c84.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from cee88c03-6679-4951-98a2-727cc6197c84.cpp:11: cee88c03-6679-4951-98a2-727cc6197c84.cpp: In function ‘int main()’: cee88c03-6679-4951-98a2-727cc6197c84.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ cee88c03-6679-4951-98a2-727cc6197c84.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <string>
 #include <algorithm>
@@ -13,52 +12,32 @@ int main() {
     vector<string> strings;
     string substring;
 
-    // Read input from user
-    cout << "Enter the list of strings (separated by space): ";
-    getline(cin, substring);
-    strings = split(substring);
+    // Input
+    cout << "Enter the number of strings: ";
+    int n; cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "Enter string " << i + 1 << ": ";
+        getline(cin, strings[i]);
+    }
 
     cout << "Enter the substring: ";
     getline(cin, substring);
 
+    // Processing
     vector<string> result = filter_by_substring(strings, substring);
-
-    // Print output
-    if(result.empty()) {
+    
+    // Output
+    if (result.empty()) {
         cout << "No strings contain the given substring." << endl;
+    } else if (issame({substring}, result)) {
+        cout << "All strings contain the given substring." << endl;
     } else {
-        cout << "Strings that contain the given substring:" << endl;
-        for(const auto& s : result) {
-            cout << s << endl;
+        cout << "Strings that contain the given substring: ";
+        for (const auto& s : result) {
+            cout << s << " ";
         }
+        cout << endl;
     }
 
     return 0;
-}
-
-vector<string> split(const string& str) {
-    vector<string> tokens;
-    size_t start = 0;
-    size_t end = str.find(' ');
-
-    while (end != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-        start = end + 1;
-        end = str.find(' ', start);
-    }
-
-    // Don't forget the last token
-    tokens.push_back(str.substr(start));
-
-    return tokens;
-}
-
-vector<string> filter_by_substring(vector<string> strings, string substring){
-    vector<string> result;
-    for (const auto& s : strings) {
-        if (s.find(substring) != string::npos) {
-            result.push_back(s);
-        }
-    }
-    return result;
 }
