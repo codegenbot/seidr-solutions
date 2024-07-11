75a7f7f24662a7748a43f239334811abd190698c
beep boop <githubbot@vadim.me>
2024-07-11 16:24:00 +0200

    Solution 41 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/juryj07u Prompt: Program did not receive expected input

--- Python_151.py
+++ Python_151.py
@@ -1,12 +1,11 @@
+```
 def double_the_difference():
     lst = list(map(int, input().split()))
     difference = max(lst) - min(lst)
     double_diff = difference * 2
     return double_diff
 
-
 def check(func):
     print(func())
-
 
 check(double_the_difference)
