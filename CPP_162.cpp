ac779985f4cb3c909df5a9aff3812b2014bb260d
beep boop <githubbot@vadim.me>
2024-07-11 03:44:50 +0200

    Solution 47 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fcqijk62 Prompt: 43ca3228-9bac-4cba-a159-dfae3431b86a.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 43ca3228-9bac-4cba-a159-dfae3431b86a.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 43ca3228-9bac-4cba-a159-dfae3431b86a.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 43ca3228-9bac-4cba-a159-dfae3431b86a.cpp:5:10: fatal error: CryptoPP/SHA1.h: No such file or directory     5 | #include <CryptoPP/SHA1.h>       |          ^~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -4,16 +4,17 @@ int main() {
     std::cout << "Enter a string: ";
     std::string s;
     std::getline(std::cin, s);
-    
-    CryptoPP::SHA1 sha;
+
+    CryptoPP::SHA1 sha1;
     unsigned char md5[16];
-    sha.Calculate((const unsigned char*)s.c_str(), (int)s.size(), md5);
+    sha1.Calculate((const unsigned char*)s.c_str(), (int)s.size(), md5);
 
     for(int i = 0; i < 16; ++i) {
-        std::cout << ((int)(md5[i])) << "";
+        std::cout << ((md5[i] * 4 > 9 ? std::hex << std::showbase : std::hex << std::noshowbase)
+            << std::setfill('0') << std::setw(2) << (int)(md5[i]));
     }
-    
+
     std::cout << std::endl;
-    
+
     return 0;
 }
