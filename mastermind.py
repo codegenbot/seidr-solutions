4f475bc8db4ca99f156ec10a57fb72535fdfdbfb
beep boop <githubbot@vadim.me>
2024-07-11 03:09:24 +0200

    Solution 90 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/rce4vfqu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/04c5235b-0772-4769-a9fe-a9e06c459 2f9.py", line 10     return str(black) + "\n" + str(white)     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- mastermind.py
+++ mastermind.py
@@ -1,12 +1,11 @@
 def mastermind(code, guess):
     black = 0
     white = 0
-    code_count = {char: code.count(char) for char in set(code)}
-    for i in range(4):
-        if code[i] == guess[i]:
+
+    for char in code:
+        if char == guess[code.index(char)]:
             black += 1
-        elif code_count.get(guess[i]) > 0:
+        elif guess.count(char) > 0:
             white += 1
-            code_count[guess[i]] -= 1
 
     return str(black) + "\n" + str(white)
