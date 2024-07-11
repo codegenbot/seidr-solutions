2b97b4220ff2dff2ec08dc9114104bec2ff8c983
beep boop <githubbot@vadim.me>
2024-07-11 12:40:46 +0200

    Solution 60 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xbr14c3t Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:10: f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp: In function ‘int main()’: f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ f0976f51-fa0a-46f8-952d-1b59b6c4176f.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,8 +1,5 @@
-Here is the solution:
-
-```cpp
 #include <vector>
-using namespace std;
+#include <string>
 
 bool issame(vector<string> a, vector<string>b) {
     if(a.size() != b.size()) return false;
@@ -19,8 +16,4 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
             result.push_back(s);
     }
     return result;
-}
-
-int main() {
-    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAC", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
 }
