7874b92224e871c71819623bd686569199ed6011
beep boop <githubbot@vadim.me>
2024-07-12 00:54:47 +0200

    Solution 2 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ykczpfik Prompt: 3b686bd3-5d8b-4e8a-aa85-aac93b961bda.cpp:2:5: error: expected unqualified-id bef ore ‘for’     2 |     for (int num : lst) {       |     ^~~ 3b686bd3-5d8b-4e8a-aa85-aac93b961bda.cpp:13:5: error: expected unqualified-id be fore ‘return’    13 |     return res;       |     ^~~~~~ 3b686bd3-5d8b-4e8a-aa85-aac93b961bda.cpp:14:1: error: expected declaration befor e ‘}’ token    14 | }       | ^ In file included from 3b686bd3-5d8b-4e8a-aa85-aac93b961bda.cpp:16: 3b686bd3-5d8b-4e8a-aa85-aac93b961bda.cpp: In function ‘int main()’: 3b686bd3-5d8b-4e8a-aa85-aac93b961bda.cpp:18:13: error: ‘search’ was not declared  in this scope    18 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ^~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,14 +1,15 @@
-int maxFreq = -1;
+int maximumSpecialNumber(std::vector<int> lst) {
+    int res = -1;
     for (int num : lst) {
         int freq = 0;
-        for (int i : lst) {
-            if (i == num) {
+        for (int n : lst) {
+            if (n == num) {
                 freq++;
             }
         }
-        if (num > freq && freq > maxFreq) {
-            maxFreq = freq;
+        if (num > 0 && freq >= num && num > res) {
+            res = num;
         }
     }
-    return maxFreq > 0 ? maxFreq : -1;
+    return res;
 }
