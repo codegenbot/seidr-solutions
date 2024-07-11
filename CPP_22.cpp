f05fe98826543e92ea78aa61e5220ac53569404b
beep boop <githubbot@vadim.me>
2024-07-11 12:29:14 +0200

    Solution 92 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/bp0n055l Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d7141c6a-b21a-4471-9311-4819db8f7d51.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d7141c6a-b21a-4471-9311-4819db8f7d51.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d7141c6a-b21a-4471-9311-4819db8f7d51.cpp:11: d7141c6a-b21a-4471-9311-4819db8f7d51.cpp: In function ‘int main()’: d7141c6a-b21a-4471-9311-4819db8f7d51.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ d7141c6a-b21a-4471-9311-4819db8f7d51.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -5,13 +5,7 @@
 
 bool issame(vector<int> a, vector<int> b);
 
-vector<int> filter_integers(list_any values);
-
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
-
-vector<int> filter_integers(list_any values){
+vector<int> filter_integers(std::list<boost::any> values){
     vector<int> result;
     for(auto val : values){
         if (val.type() == typeid(int)){
@@ -21,11 +15,15 @@ vector<int> filter_integers(list_any values){
     return result;
 }
 
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
+
 int main(){
     vector<int> a = {1, 2, 3};
     vector<int> b = {1, 2, 3};
-
+    
     assert(issame(a, b));
-  
+    
     return 0;
 }
