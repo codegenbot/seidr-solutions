d2ae101ef1c30273ca0fcf527d91405f08a7926d
beep boop <githubbot@vadim.me>
2024-07-11 17:31:06 +0200

    Solution 10 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/6op2pbfk Prompt: n this scope    17 |         sum += pow(vec1[i] - vec2[i], 2);       |                    ^~~~ 4423e67a-11cd-47fa-bea7-682d1ed6b2bd.cpp:17:30: error: ‘vec2’ was not declared i n this scope    17 |         sum += pow(vec1[i] - vec2[i], 2);       |                              ^~~~ 4423e67a-11cd-47fa-bea7-682d1ed6b2bd.cpp:17:16: error: ‘pow’ was not declared in  this scope    17 |         sum += pow(vec1[i] - vec2[i], 2);       |                ^~~ 4423e67a-11cd-47fa-bea7-682d1ed6b2bd.cpp:20:5: error: ‘cout’ was not declared in  this scope    20 |     cout << sqrt(sum) << endl;       |     ^~~~ 4423e67a-11cd-47fa-bea7-682d1ed6b2bd.cpp:20:13: error: ‘sqrt’ was not declared i n this scope    20 |     cout << sqrt(sum) << endl;       |             ^~~~ 4423e67a-11cd-47fa-bea7-682d1ed6b2bd.cpp:20:26: error: ‘endl’ was not declared i n this scope    20 |     cout << sqrt(sum) << endl;       |                          ^~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,23 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
 int main() {
     int n;
-    cin >> n;
-    vector<double> vec1(n);
+    std::cin >> n;
+    std::vector<float> vec1(n);
     for (int i = 0; i < n; ++i) {
-        cin >> vec1[i];
+        std::cin >> vec1[i];
     }
 
-    cin >> n;
-    vector<double> vec2(n);
+    std::cin >> n;
+    std::vector<float> vec2(n);
     for (int i = 0; i < n; ++i) {
-        cin >> vec2[i];
+        std::cin >> vec2[i];
     }
 
-    double sum = 0.0;
+    float sum = 0.0;
     for (int i = 0; i < n; ++i) {
-        sum += pow(vec1[i] - vec2[i], 2);
+        sum += std::pow(vec1[i] - vec2[i], 2);
     }
 
-    cout << sqrt(sum) << endl;
+    std::cout << std::sqrt(sum) << std::endl;
 
     return 0;
 }
