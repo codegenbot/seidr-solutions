7021a6853b04783ee9c6810db38dcbdfc6d50f51
beep boop <githubbot@vadim.me>
2024-07-11 20:35:24 +0200

    Solution 60 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ctj0vn6n Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d7ffb85f-8f8c-4216-b0bb-06ee70261 3e9.py", line 21, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d7ffb85f-8f8c-4216-b0bb-06ee70261 3e9.py", line 17, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,20 +1,11 @@
 def encode(message):
     result = ""
-    last_alphanumeric = True
     for char in message:
         if char.isalpha():
             if char.isupper():
-                if last_alphanumeric:
-                    result += chr((ord(char) - 65 + 2) % 26 + 65)
-                else:
-                    result += chr((ord(char) - 65 + 2) % 26 + 65).lower()
+                result += chr((ord(char) - 65 + 2) % 26 + 65)
             else:
-                if last_alphanumeric:
-                    result += chr((ord(char) - 97 + 2) % 26 + 97)
-                else:
-                    result += chr((ord(char) - 97 + 2) % 26 + 97).upper()
-            last_alphanumeric = True
+                result += chr((ord(char) - 97 + 2) % 26 + 97)
         else:
-            result += char
-            last_alphanumeric = False
+            result += char  
     return result
