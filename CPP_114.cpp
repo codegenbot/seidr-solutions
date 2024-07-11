f3ed0246318eacbe217a5412abac66e93d205573
beep boop <githubbot@vadim.me>
2024-07-11 03:34:14 +0200

    Solution 73 of CPP/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/czkuqvx2 Prompt: Complete the following code given the task description and function signature.

--- CPP_114.cpp
+++ CPP_114.cpp
@@ -1,13 +1,12 @@
-long long minSubArraySum(vector<long long> nums) {
+long long minSubArraySum(vector<long long> nums){
     long long res = LLONG_MAX;
     long long curr_sum = 0;
 
-    for (long long num : nums) {
+    for(long long num : nums) {
         curr_sum += num;
         res = min(res, curr_sum);
-        if (curr_sum < 0) {
+        if(curr_sum < 0)
             curr_sum = 0;
-        }
     }
 
     return res;
