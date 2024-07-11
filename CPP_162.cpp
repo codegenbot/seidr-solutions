16d8173546fccf82daa706fc007a721f7b07a4fd
beep boop <githubbot@vadim.me>
2024-07-11 05:22:23 +0200

    Solution 80 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fcqijk62 Prompt: 60587093-b68b-420d-98ae-b392234a09f2.cpp:4:10: fatal error: cryptopp/sha1.h: No such file or directory     4 | #include "cryptopp/sha1.h"       |          ^~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -12,17 +12,9 @@ int main() {
     unsigned char md[20];
     sha.Calculate((const unsigned char*)s.c_str(), (int)s.size(), md);
 
-    for(int i = 0; i < 16; ++i) {
-        std::stringstream ss;
-        if(md[i] * 4 > 9)
-            ss << std::hex << std::showbase;
-        else
-            ss << std::hex << std::noshowbase;
-        ss << std::setfill('0') << std::setw(2) << (int)(md[i]);
-    }
-    std::cout << "The SHA1 is: ";
-    for(int i = 0; i < 16; ++i)
+    for(int i = 0; i < 20; ++i) {
         std::cout << std::hex << (int)(md[i]) << " ";
+    }
     std::cout << std::endl;
 
     return 0;
