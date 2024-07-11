bede0212b267699c9ce529aa6218cff2eed7a16d
beep boop <githubbot@vadim.me>
2024-07-11 19:19:13 +0200

    Solution 88 of gcd, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/c6v9l7j7 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- gcd.cpp
+++ gcd.cpp
@@ -4,16 +4,15 @@ using namespace std;
 
 int gcd(int a, int b) {
     while (b != 0)
-        b = a % b;
+        a %= b;
     return a;
 }
 
 int main() {
-    string str1, str2;
-    cout << "Enter the first string: ";
-    getline(cin, str1);
-    cout << "Enter the second string: ";
-    getline(cin, str2);
-    cout << "GCD is: " << gcd(str1.length(), str2.length());
+    int a, b;
+    string text, target;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    cout << "GCD is: " << gcd(a,b);
     return 0;
 }
