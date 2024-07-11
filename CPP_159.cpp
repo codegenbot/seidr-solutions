5005c37f4967f0a65fc77a5b70dab8aab06392cc
beep boop <githubbot@vadim.me>
2024-07-11 11:45:44 +0200

    Solution 76 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/e2fphx8t Prompt:       |                           ^~~~~~ a217a3e5-f3e0-495c-9e43-0548c9624d20.cpp:22:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from a217a3e5-f3e0-495c-9e43-0548c9624d20.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from a217a3e5-f3e0-495c-9e43-0548c9624d20.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a217a3e5-f3e0-495c-9e43-0548c9624d20.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a217a3e5-f3e0-495c-9e43-0548c9624d20.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ a217a3e5-f3e0-495c-9e43-0548c9624d20.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,4 +1,3 @@
-#include <vector>
 #include <cassert>
 
 std::pair<int, int> eat(int number, int need, int remaining) {
@@ -14,6 +13,6 @@ bool issame(std::pair<int, int> a, std::pair<int, int> b) {
 
 int main() {
     assert(issame(eat(4, 5, 1), std::make_pair(5, 0)));
-
+  
     return 0;
 }
