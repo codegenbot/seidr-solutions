2d67aea8fa11c5ffa528f175a50184060a10d034
beep boop <githubbot@vadim.me>
2024-07-11 16:02:34 +0200

    Solution 32 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/juryj07u Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/45192b75-9979-4eae-b028-c84c21cce c15.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,13 +1,16 @@
-def double_the_difference(lst):
+def double_the_difference():
+    lst = [int(i) for i in input().split()]
     difference = max(lst) - min(lst)
     double_diff = difference * 2
     return double_diff
 
+
+import builtins
+
+
 def check(func):
-    user_input = input()  
-    lst = [int(i) for i in user_input.split()]
-    if len(lst) != 1:
-        print("Error: Please enter single integer.")
+    if len(builtins.input()) > 1:
+        print("Error: Please enter single input.")
         return None
     else:
-        return func(lst)
+        return func()
