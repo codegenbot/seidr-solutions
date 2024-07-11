4fb163040c58717d50fb3226d7896d698857c7c9
beep boop <githubbot@vadim.me>
2024-07-11 08:57:43 +0200

    Solution 8 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/013bc642-5b1a-4fa7-9534-98617eb6e fa0.py", line 14, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/013bc642-5b1a-4fa7-9534-98617eb6e fa0.py", line 9, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,7 +1,3 @@
 ```
-from itertools import cycle
-
 def sort_third(l: list):
-    first_half = sorted((x for i, x in enumerate(l) if i % 3 != 0))
-    second_half = sorted((x for i, x in enumerate(l) if i % 3 == 0 and x not in first_half), reverse=True)
-    return [x if i % 3 != 0 else y for i, (x, y) in zip(enumerate(l), cycle([first_half, second_half]))]
+    return sorted((x for i, x in enumerate(l) if i % 3 != 0), key=lambda y: l[i//3])
