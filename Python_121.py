298b5c108e07b65c52935ab2e2a2a8765c256b2b
beep boop <githubbot@vadim.me>
2024-07-11 19:24:13 +0200

    Solution 88 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ebtie55w Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fd874aa7-e5ab-42e6-9e52-77f7c38e7 f0f.py", line 17, in <module>     check(solution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fd874aa7-e5ab-42e6-9e52-77f7c38e7 f0f.py", line 12, in check     assert solution([3, 13, 2, 9]) == 3            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_121.py
+++ Python_121.py
@@ -1,13 +1,8 @@
-```Python
+```
 def solution(lst):
     if len(lst) == 0:
         return 0
     elif len(lst) % 2 != 0:
         return sum(i for i in lst if i % 2 != 0)
     else:
-        half = len(lst) // 2
-        result1 = sum(i for i in lst[:half+1] if i % 2 != 0)
-        result2 = sum(i for i in lst[half:] if i % 2 != 0)
-        return max(result1, result2)
-
-assert solution([3, 13, 2, 9]) == 15
+        return sum(i for i in lst if i % 2 != 0)
