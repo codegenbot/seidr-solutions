a9a90519b9ec88c9f7609a8d5233ba58e2d6cac7
beep boop <githubbot@vadim.me>
2024-07-11 18:07:37 +0200

    Solution 22 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h0l13eex Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a98f8136-3d4a-4cbf-8f17-8bc1337b0 16f.py", line 16, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a98f8136-3d4a-4cbf-8f17-8bc1337b0 16f.py", line 11, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,15 +1,8 @@
+```Python
 def sort_third(l: list):
-    sorted_elements = [y for i, y in enumerate(l) if (i + 1) % 3 == 0]
+    sorted_elements = [y for i, y in enumerate(l) if (i+1) % 3 == 0]
     sorted_elements = sorted(sorted_elements)
-    return [
-        (
-            x
-            if i % 3 != 0
-            else (
-                next((y for y in sorted_elements if y not in l), None)
-                if any(y in sorted_elements for y in l) and (i + 1) % 3 == 0
-                else x
-            )
-        )
-        for i, x in enumerate(l)
-    ]
+    return [x if i % 3 != 0 else next((y for y in sorted_elements if y not in l), None) 
+            if any(y in sorted_elements for y in l) and (i+1) % 3 == 0 
+            else x 
+            for i, x in enumerate(l)]
