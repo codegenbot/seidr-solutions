453141409b5e7b007c0460bf62151d0f04eb9b28
beep boop <githubbot@vadim.me>
2024-07-11 08:34:27 +0200

    Solution 64 of CPP/141, ratio 0.9615384615384616 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/p2ju98kx Prompt: 005ba71c-130f-4b27-9e3e-504f7bb45e19.cpp: In function ‘int main()’: 005ba71c-130f-4b27-9e3e-504f7bb45e19.cpp:28:5: error: ‘assert’ was not declared in this scope    28 |     assert(file_name_check("s.") == "No");       |     ^~~~~~ 005ba71c-130f-4b27-9e3e-504f7bb45e19.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <string>   +++ |+#include <cassert>     2 | 005ba71c-130f-4b27-9e3e-504f7bb45e19.cpp: At global scope: 005ba71c-130f-4b27-9e3e-504f7bb45e19.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 005ba71c-130f-4b27-9e3e-504f7bb45e19.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,24 +1,26 @@
 #include <string>
+#include <cassert>
 
-std::string file_name_check(std::string file_name){
+std::string file_name_check(std::string filename) {
     int digit_count = 0;
     bool found_dot = false;
     bool valid_prefix = true;
 
-    for(int i = 0; i < file_name.size(); i++){
-        if(isdigit(file_name[i])){
+    for (char c : filename) {
+        if (isdigit(c)) {
             digit_count++;
-        }
-        else if(file_name[i] == '.'){
+            if (digit_count > 3)
+                return "No";
+        } else if (c == '.') {
             found_dot = true;
-        }
-        else{
-            if(!found_dot)
-                valid_prefix = isalpha(file_name[i]);
-            else
-                break;
+        } else {
+            if (digit_count > 0)
+                valid_prefix = false;
         }
     }
 
-    return (digit_count <= 3 && found_dot && valid_prefix) ? "Yes" : "No";
+    if (digit_count <= 3 && found_dot && valid_prefix)
+        return "Yes";
+    else
+        return "No";
 }
