50417beb095bae352351603458d8b8e8c1af8e65
beep boop <githubbot@vadim.me>
2024-07-11 03:37:55 +0200

    Solution 42 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/iacogw5j Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e6d25c07-04b3-4aa6-b753-29d7f47ac 9f8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_20.py
+++ Python_20.py
@@ -2,13 +2,9 @@ from typing import List, Tuple
 
 
 def find_closest_elements(nums: List[float], target: float) -> Tuple[float, float]:
-    closest_pair = (float("inf"), float("-inf"))
+    left = min((i for i in nums if i < target), default=None)
+    right = max((i for i in nums if i > target), default=None)
 
-    for num in nums:
-        if abs(num - target) < abs(closest_pair[0] - target):
-            if abs(num - target) == abs(closest_pair[0] - target):
-                closest_pair = min((closest_pair, (num,)), key=lambda x: x[1])
-            else:
-                closest_pair = ((num),)
+    closest_pair = (left, right) if left and right else ()
 
     return closest_pair
