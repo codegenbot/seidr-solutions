e912c755c9123c96c5732e3c1881df9c8bfb54a2
beep boop <githubbot@vadim.me>
2024-07-11 15:15:58 +0200

    Solution 47 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/60fn05nm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d2f000e-0e8e-47db-b0d7-b9a40f749 5f5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_9.py
+++ Python_9.py
@@ -1,10 +1,9 @@
-```
 from typing import List
 
 
 def rolling_max():
     T = int(input("Enter number of test cases: "))
-    
+
     for _ in range(T):
         numbers = list(map(int, input("Enter numbers separated by space: ").split()))
         max_seen_so_far = []
@@ -12,7 +11,8 @@ def rolling_max():
             if not max_seen_so_far:
                 max_seen_so_far.append(max(0))
             else:
-                max_seen_so_far.append(max(max_seen_so_far[i-1], numbers[i]))
+                max_seen_so_far.append(max(max_seen_so_far[i - 1], numbers[i]))
         print(max_seen_so_far)
+
 
 rolling_max()
