f60bd84552c7098ab896f7bb378d0278bfe3f12c
beep boop <githubbot@vadim.me>
2024-07-11 10:16:08 +0200

    Solution 26 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xzmxc8nx Prompt:       |                                 ^~~~~~ In file included from d4102b30-322c-4025-85cf-99718a88eca4.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ d4102b30-322c-4025-85cf-99718a88eca4.cpp:53:44: error: ‘b’ was not declared in t his scope    53 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d4102b30-322c-4025-85cf-99718a88eca4.cpp:53:45: error: expression list treated a s compound expression in initializer [-fpermissive]    53 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d4102b30-322c-4025-85cf-99718a88eca4.cpp:52: d4102b30-322c-4025-85cf-99718a88eca4.cpp: In function ‘int main()’: d4102b30-322c-4025-85cf-99718a88eca4.cpp:62:20: error: ‘by_length’ was not decla red in this scope    62 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ d4102b30-322c-4025-85cf-99718a88eca4.cpp:62:19: error: ‘issame’ cannot be used a s a function    62 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,71 +1,60 @@
-```
-#include <iostream>
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
+bool issame(const std::vector<std::string>& vec1, const std::vector<std::string>& vec2) {
+    if (vec1.size() != vec2.size())
+        return false;
+    for (int i = 0; i < vec1.size(); ++i)
+        if (vec1[i] != vec2[i])
+            return false;
     return true;
 }
 
-vector<string> by_length(vector<int> arr) {
-    vector<string> result;
+std::vector<std::string> by_length(std::vector<int> arr) {
+    std::vector<int> numbers;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            string str;
-            switch (num) {
-                case 1:
-                    str = "One";
-                    break;
-                case 2:
-                    str = "Two";
-                    break;
-                case 3:
-                    str = "Three";
-                    break;
-                case 4:
-                    str = "Four";
-                    break;
-                case 5:
-                    str = "Five";
-                    break;
-                case 6:
-                    str = "Six";
-                    break;
-                case 7:
-                    str = "Seven";
-                    break;
-                case 8:
-                    str = "Eight";
-                    break;
-                case 9:
-                    str = "Nine";
-                    break;
-            }
-            result.push_back(str);
+            numbers.push_back(num);
         }
     }
-    sort(result.begin(), result.end());
-    reverse(result.begin(), result.end());
-    return result;
-}
 
-int main() {
-    vector<int> arr = {1, 3, 5, 2, 4, 9};
-    vector<string> res = by_length(arr);
-    for (string str : res) {
-        cout << str << endl;
+    std::sort(numbers.begin(), numbers.end());
+    std::reverse(numbers.begin(), numbers.end());
+
+    std::vector<std::string> result;
+    for (int num : numbers) {
+        std::string str;
+        switch (num) {
+            case 1:
+                str = "One";
+                break;
+            case 2:
+                str = "Two";
+                break;
+            case 3:
+                str = "Three";
+                break;
+            case 4:
+                str = "Four";
+                break;
+            case 5:
+                str = "Five";
+                break;
+            case 6:
+                str = "Six";
+                break;
+            case 7:
+                str = "Seven";
+                break;
+            case 8:
+                str = "Eight";
+                break;
+            case 9:
+                str = "Nine";
+                break;
+        }
+        result.push_back(str);
     }
-    if (issame({res}, {"Nine", "Five", "Four", "Three", "Two", "One"})) {
-        cout << "Same" << endl;
-    } else {
-        cout << "Not Same" << endl;
-    }
-    
-    return 0;
+
+    return result;
 }
