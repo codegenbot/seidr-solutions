e9358180766ac781eb297ef3e0dde2934f13e49e
beep boop <githubbot@vadim.me>
2024-07-11 11:37:12 +0200

    Solution 2 of Python/141, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/n0d6ocm6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/887a2779-553f-46b6-a537-8c5cb6a4f 986.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_141.py
+++ Python_141.py
@@ -1,4 +1,4 @@
-'''def file_name_check(file_name):
+def file_name_check(file_name):
     if len([c for c in file_name if c.isdigit()]) > 3:
         return 'No'
     if file_name.count('.') != 1:
@@ -9,4 +9,4 @@
         return 'No'
     if after_dot not in ['txt', 'exe', 'dll']:
         return 'No'
-    return 'Yes''''
+    return 'Yes'
