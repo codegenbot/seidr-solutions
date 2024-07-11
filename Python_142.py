278b64600aca982ac8c79f4c960c7fc8985d480e
beep boop <githubbot@vadim.me>
2024-07-11 10:19:16 +0200

    Solution 80 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/16anmmfz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3cfbbc56-1b81-4da8-8e9c-6573fc410 30e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,2 +1,9 @@
+```Python
 def sum_squares(lst):
-    return sum(i**2 for i in lst)
+    total = 0
+    for i in range(len(lst)):
+        if i % 3 == 0 and i % 4 != 0:
+            total += lst[i] ** 2
+        elif i % 4 == 0 and i % 3 != 0:
+            total += lst[i] ** 3
+    return total
