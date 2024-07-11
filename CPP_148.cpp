6ff97a3f3fd528f849f2040710b81e9262f7d113
beep boop <githubbot@vadim.me>
2024-07-11 12:01:39 +0200

    Solution 41 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hn4pgr1r Prompt:       |                              ^~~~~~ db795cea-f0f3-4a4d-8dc3-83eebbd53f64.cpp:38:37: error: ‘string’ was not declared  in this scope    38 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ db795cea-f0f3-4a4d-8dc3-83eebbd53f64.cpp:38:44: error: ‘b’ was not declared in t his scope    38 | bool issame(vector<string> a,vector<string>b){       |                                            ^ db795cea-f0f3-4a4d-8dc3-83eebbd53f64.cpp:38:45: error: expression list treated a s compound expression in initializer [-fpermissive]    38 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from db795cea-f0f3-4a4d-8dc3-83eebbd53f64.cpp:37: db795cea-f0f3-4a4d-8dc3-83eebbd53f64.cpp: In function ‘int main()’: db795cea-f0f3-4a4d-8dc3-83eebbd53f64.cpp:47:20: error: ‘bf’ was not declared in this scope    47 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ db795cea-f0f3-4a4d-8dc3-83eebbd53f64.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -2,7 +2,7 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return a == b;
 }
 
-std::vector<string> bf(string planet1, string planet2) {
+std::vector<std::string> bf(std::string planet1, std::string planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
     int index1 = -1;
     int index2 = -1;
@@ -23,9 +23,9 @@ std::vector<string> bf(string planet1, string planet2) {
     int start, end;
 
     if(index1 < index2)
-        std::swap(start, end);
+        swap(start, end);
     else
-        std::tie(std::move(start), std::move(end)) = {index1, index2};
+        std::tie(std::swap(start, end));
 
     vector<string> result;
     for (int i = 0; i < planets.size(); i++) {
