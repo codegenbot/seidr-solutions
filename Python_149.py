28c7bbc9b11e6cb1e8a666aab29d20226d7da696
beep boop <githubbot@vadim.me>
2024-07-11 12:08:59 +0200

    Solution 10 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8z1koym7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aab7985c-b382-458a-ad2f-e43b5405e 5ed.py", line 9, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aab7985c-b382-458a-ad2f-e43b5405e 5ed.py", line 6, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,4 +1,3 @@
+```
 def sorted_list_sum(lst):
-    odd_words = sorted([word for word in lst if len(word) % 2 != 0])
-    even_words = sorted([word for word in lst if len(word) % 2 == 0], key=len)
-    return odd_words + even_words
+    return sum(len(word) for word in sorted(set(lst)) if len(str(word)) % 2 == 0)
