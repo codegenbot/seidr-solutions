3aaffd7a9f8f9a1c525e5bc3dd6072f2b549f4d3
beep boop <githubbot@vadim.me>
2024-07-11 10:08:37 +0200

    Solution 6 of CPP/109, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jihndk6x Prompt: e3c47543-f6a2-4135-a720-60cdb68fe40a.cpp:1:20: error: ‘vector’ was not declared in this scope     1 | bool move_one_ball(vector<int> arr) {       |                    ^~~~~~ e3c47543-f6a2-4135-a720-60cdb68fe40a.cpp:1:27: error: expected primary-expressio n before ‘int’     1 | bool move_one_ball(vector<int> arr) {       |                           ^~~ In file included from e3c47543-f6a2-4135-a720-60cdb68fe40a.cpp:12: e3c47543-f6a2-4135-a720-60cdb68fe40a.cpp: In function ‘int main()’: e3c47543-f6a2-4135-a720-60cdb68fe40a.cpp:14:26: error: ‘move_one_ball’ cannot be  used as a function    14 |     assert (move_one_ball({})==true);       |             ~~~~~~~~~~~~~^~~~

--- CPP_109.cpp
+++ CPP_109.cpp
@@ -1,13 +1,14 @@
-```
+```cpp
 #include <vector>
 #include <algorithm>
 
 bool move_one_ball(vector<int> arr) {
     int max = *max_element(arr.begin(), arr.end());
-    for (int i = 0; i <= arr.size(); i++) {
-        if (i > 0 && arr[i] != max - i + 1) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] != max) {
             return false;
         }
+        max--;
     }
     return true;
 }
