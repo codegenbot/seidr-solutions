a7e076cb4239a7ec89250a84e20c3e83eb84ed0f
beep boop <githubbot@vadim.me>
2024-07-11 18:04:12 +0200

    Solution 68 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jrkitbdv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/01de0e0f-319f-4f86-a3b8-5878fe96a c58.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -1,7 +1,7 @@
-def calculate_median(numbers: List[int]) -> float:
-    numbers.sort()
-    if len(numbers) % 2 == 0:
-        median = (numbers[len(numbers)//2 - 1] + numbers[len(numbers)//2]) / 2
-    else:
-        median = numbers[len(numbers)//2]
-    return median
+def concatenate_strings(input_list: List[str]) -> str:
+    result = ""
+    for string in input_list:
+        if len(result) > 0:
+            result += " "
+        result += string
+    return result
