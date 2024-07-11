9fce3f85a2a1117d8eeed1b64f4c8c802b4d69f0
beep boop <githubbot@vadim.me>
2024-07-11 06:39:07 +0200

    Solution 30 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ex70v8cc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c6e02fed-d640-47e1-b9b7-78cb50a75 522.py", line 25, in <module>     check(valid_date)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c6e02fed-d640-47e1-b9b7-78cb50a75 522.py", line 22, in check     assert valid_date("04-2003") == False            ^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c6e02fed-d640-47e1-b9b7-78cb50a75 522.py", line 5, in valid_date     year = int(date_parts[2])                ~~~~~~~~~~^^^ IndexError: list index out of range

--- Python_124.py
+++ Python_124.py
@@ -1,20 +1,21 @@
 ```
 def valid_date(date):
-    day, month, year = date.split('-')
-    if len(date) != 10 or not all(part.isdigit() for part in date.split('-')):
+    date_parts = date.split("-")
+    if len(date_parts) != 3:
         return False
-    try:
-        int(month), int(day)
-    except ValueError:
+    month = int(date_parts[0])
+    day = int(date_parts[1])
+    year = int(date_parts[2])
+
+    if len(date) != 9:
         return False
-    if int(month) < 1 or int(month) > 12:
+    if month < 1 or month > 12:
         return False
-    days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
-    if int(month) == 2 and (int(day) < 1 or int(day) > 29):
+    if (
+        (month in [1, 3, 5, 7, 8, 10, 12] and day > 31)
+        or (month in [4, 6, 9, 11] and day > 30)
+        or (month == 2 and day > 29)
+    ):
         return False
-    elif int(month) in [4,6,9,11] and (int(day) < 1 or int(day) > 30):
-        return False
-    elif int(month) in [1,3,5,7,8,10,12] and (int(day) > 31):
-        return False
-    else:
-        return True
+
+    return True
