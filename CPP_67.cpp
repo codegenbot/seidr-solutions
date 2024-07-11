0ea7203cc6ca52018ce00895378c03295b31bee9
beep boop <githubbot@vadim.me>
2024-07-11 07:30:27 +0200

    Solution 4 of CPP/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8hk7urfp Prompt: Program failed

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,32 +1,29 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
 int fruit_distribution(string s, int n) {
-    string temp;
-    int total_apples = 0, total_oranges = 0;
-
-    for (int i = 0; i < s.length(); i++) {
-        if (isdigit(s[i])) {
-            temp += s[i];
-        } else if (!temp.empty()) {
-            if (s.substr(i - 2, 8) == "apples and") {
-                total_apples = stoi(temp);
-            } else if (s.substr(i - 7, 9) == "oranges and") {
-                total_oranges = stoi(temp);
+    int total_apples = 0;
+    int total_oranges = 0;
+    
+    string temp = "";
+    for (char c : s) {
+        if (c == ' ') {
+            if (!temp.empty()) {
+                if (temp.find("apples") != string::npos)
+                    total_apples = stoi(temp.substr(0, temp.find(" ")).erase(0, 1));
+                else if (temp.find("oranges") != string::npos)
+                    total_oranges = stoi(temp.substr(0, temp.find(" ")).erase(0, 1));
+                temp.clear();
             }
-            temp = "";
+        } else {
+            temp += c;
         }
     }
-
+    
+    // Process the last string
+    if (!temp.empty()) {
+        if (temp.find("apples") != string::npos)
+            total_apples = stoi(temp.substr(0, temp.find(" ")).erase(0, 1));
+        else if (temp.find("oranges") != string::npos)
+            total_oranges = stoi(temp.substr(0, temp.find(" ")).erase(0, 1));
+    }
+    
     return n - total_apples - total_oranges;
-}
-
-int main() {
-    cout << fruit_distribution("5 apples and 6 oranges", 19) << endl; 
-    cout << fruit_distribution("0 apples and 1 oranges",3) << endl; 
-    cout << fruit_distribution("2 apples and 3 oranges", 100) << endl; 
-    cout << fruit_distribution("100 apples and 1 oranges",120) << endl; 
-
-    return 0;
 }
