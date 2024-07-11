c1e9af1ee37e3934c47c7b90e728b9a99a9628f5
beep boop <githubbot@vadim.me>
2024-07-11 11:42:54 +0200

    Solution 18 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/df71cwu3 Prompt:       |                    ^~~ 8588a4c6-2015-4add-b3a4-b4439eacbd9e.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8588a4c6-2015-4add-b3a4-b4439eacbd9e.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8588a4c6-2015-4add-b3a4-b4439eacbd9e.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8588a4c6-2015-4add-b3a4-b4439eacbd9e.cpp:13: 8588a4c6-2015-4add-b3a4-b4439eacbd9e.cpp: In function ‘int main()’: 8588a4c6-2015-4add-b3a4-b4439eacbd9e.cpp:23:20: error: ‘even_odd_count’ was not declared in this scope    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 8588a4c6-2015-4add-b3a4-b4439eacbd9e.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,14 +1,7 @@
+```cpp
 #include <vector>
 #include <string>
 #include <cctype>
-
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
-}
 
 std::vector<int> even_odd_count(int num) {
     std::vector<int> result(2, 0);
@@ -22,7 +15,11 @@ std::vector<int> even_odd_count(int num) {
     return result;
 }
 
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    return (a.size() == b.size()) && (std::equal(a.begin(), a.end(), b.begin()));
+}
+
 int main() {
-    assert(std::issame(even_odd_count(0), {1, 0})); 
+    assert(std::issame(even_odd_count(0), {1, 0}));
     return 0;
 }
