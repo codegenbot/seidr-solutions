4bfac0e8041d1401fa34582e53d63f1cbb9300ae
beep boop <githubbot@vadim.me>
2024-07-11 02:39:43 +0200

    Solution 18 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/cyujl28f Prompt: Complete the following code given the task description and function signature.

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,41 +1,74 @@
-#include<stdio.h>
-#include<vector>
-#include<string>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
 vector<string> split_words(string txt) {
     vector<string> result;
-    size_t pos = 0, prev_pos = 0;
-    
-    while ((pos = txt.find_first_of(" ,")) != string::npos) {
-        if (pos == 0) {
-            if (txt.find(',') == string::npos) {
-                return {to_string(count_lowercase(txt))};
+    size_t pos = 0;
+    while ((pos = txt.find(' ')) != string::npos) {
+        result.push_back(txt.substr(0, pos));
+        txt.erase(0, pos + 1);
+    }
+    if (txt.empty()) {
+        return result;
+    }
+    if (txt.find(',') == string::npos) {
+        int count = 0;
+        for (char c : txt) {
+            if (c >= 'a' && c <= 'z') {
+                if ((count++) % 2 != 0) {
+                    break;
+                }
+            } else {
+                break;
             }
-            result.push_back(txt.substr(0, pos));
-            txt.erase(0, pos + 1);
-            prev_pos = pos;
-        } else {
-            result.push_back(txt.substr(prev_pos, pos - prev_pos));
-            txt.erase(0, pos - prev_pos);
-            prev_pos = pos;
         }
+        result.push_back(to_string(count));
+    } else {
+        string word = "";
+        for (char c : txt) {
+            if (c == ',') {
+                result.push_back(word);
+                word = "";
+            } else {
+                word += c;
+            }
+        }
+        result.push_back(word);
     }
-    
-    if (txt.length() > 0) {
-        result.push_back(txt);
-    }
-    
     return result;
 }
 
-int count_lowercase(string s) {
-    int count = 0;
-    for (char c : s) {
-        if (islower(c)) {
-            count++;
-        }
+int main() {
+    // Test cases
+    string test1 = "Hello world!";
+    string test2 = "Hello,world!";
+    string test3 = "abcdef";
+    
+    vector<string> result1 = split_words(test1);
+    vector<string> result2 = split_words(test2);
+    vector<string> result3 = split_words(test3);
+
+    cout << "Test 1: ";
+    for (string s : result1) {
+        cout << s << " ";
     }
-    return count;
+    cout << endl;
+    
+    cout << "Test 2: ";
+    for (string s : result2) {
+        cout << s << " ";
+    }
+    cout << endl;
+    
+    cout << "Test 3: ";
+    for (string s : result3) {
+        cout << s << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
