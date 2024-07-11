434051c4057b52bf89e0ce0f7803e1fe1960d58b
beep boop <githubbot@vadim.me>
2024-07-11 13:59:02 +0200

    Solution 42 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/c4exytmv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80f1f1cc-c503-44c3-9738-7a9c5cc59 0c4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,21 +1,27 @@
-def make_palindrome(s: str) -> str:
-    if s == s[::-1]:
-        return s
+def make_palindrome(string: str) -> str:
+    if string == string[::-1]:
+        return string
 
-    left = list(s)
-    right = list(s)[::-1]
+    left = list(string)
+    right = list(string)[::-1]
 
     i, j = 0, len(right) - 1
     while i < len(left):
         if left[i] != right[j]:
             if j > 0:
-                left.insert(0, right[j - 1])
-                j -= 1
+                if left[0] == right[j-1]:
+                    left.insert(0, right[j])
+                    break
+                else:
+                    left.insert(0, right[j-1])
+                    j -= 1
             else:
-                left.insert(0, right[0])
-                j = 0
+                if len(left) >= 2 and left[0] != right[0]:
+                    left.insert(0, right[0])
+                else:
+                    left.insert(0, 'a')
         else:
             i += 1
             j -= 1
 
-    return "".join(left + list(s))
+    return "".join(left + list(string))
