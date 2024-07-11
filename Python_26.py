f6982340c8fbed7af1e2a44afe3ab813c1efc409
beep boop <githubbot@vadim.me>
2024-07-11 15:21:04 +0200

    Solution 50 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/i3umme6d Prompt: Program did not receive expected input

--- Python_26.py
+++ Python_26.py
@@ -1,7 +1,5 @@
-````
+```
 from typing import List
-from collections import OrderedDict
-
 
 def remove_duplicates(numbers: List[int]) -> List[int]:
-    return list(OrderedDict.fromkeys(numbers))
+    return list(set(numbers))
