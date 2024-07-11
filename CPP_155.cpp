e5b5c134d201d872fdb950d7744a1d40d856601a
beep boop <githubbot@vadim.me>
2024-07-11 17:27:55 +0200

    Solution 20 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/volfybov Prompt:       |                    ^~~ 12f11c39-9969-4d35-8222-db622ce02867.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 12f11c39-9969-4d35-8222-db622ce02867.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 12f11c39-9969-4d35-8222-db622ce02867.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 12f11c39-9969-4d35-8222-db622ce02867.cpp:14: 12f11c39-9969-4d35-8222-db622ce02867.cpp: In function ‘int main()’: 12f11c39-9969-4d35-8222-db622ce02867.cpp:24:20: error: ‘even_odd_count’ was not declared in this scope    24 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 12f11c39-9969-4d35-8222-db622ce02867.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,19 +1,14 @@
 #include <vector>
-#include <string>
-#include <cassert>
 
-std::vector<int> even_odd_count(int num);
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-int main() {
-    std::vector<int> evenOdd1 = even_odd_count(123456);
-    std::vector<int> evenOdd2 = even_odd_count(-987654);
-
-    assert(issame(evenOdd1, {3, 3}));
-    assert(issame(evenOdd2, {3, 3}));
-
-    return 0;
+vector<int> even_odd_count(int num){
+    vector<int> result(2, 0);
+    string numStr = to_string(abs(num));
+    for (char digit : numStr) {
+        if ((digit - '0') % 2 == 0) {
+            result[0]++;
+        } else {
+            result[1]++;
+        }
+    }
+    return result;
 }
