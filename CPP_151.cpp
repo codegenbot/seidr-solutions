4f821f33de0bf7b43500cb1c75f81995929e7ab6
beep boop <githubbot@vadim.me>
2024-07-11 05:18:36 +0200

    Solution 82 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jafm6m1z Prompt: 09c8f0ce-d5ac-44da-9b24-213ec10f98fa.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ 09c8f0ce-d5ac-44da-9b24-213ec10f98fa.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 09c8f0ce-d5ac-44da-9b24-213ec10f98fa.cpp:40: 09c8f0ce-d5ac-44da-9b24-213ec10f98fa.cpp: In function ‘int main()’: 09c8f0ce-d5ac-44da-9b24-213ec10f98fa.cpp:42:35: error: ‘lst’ was not declared in  this scope    42 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 09c8f0ce-d5ac-44da-9b24-213ec10f98fa.cpp:42:43: error: ‘odd_sum’ was not declare d in this scope    42 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-long long double_the_difference(vector<long double> lst) {
-    long long sum = 0;
-    for (long double num : lst) {
+long double double_the_difference(vector<float> lst) {
+    long double sum = 0;
+    for (float num : lst) {
         if (num > 0 && abs(num - lround(num)) < 1e-9) {
             sum += pow(num, 2);
         }
@@ -16,23 +16,23 @@ long long double_the_difference(vector<long double> lst) {
 }
 
 int main() {
-    vector<long double> lst; 
-    long long odd_sum = 0;
+    vector<float> lst;
+    long double odd_sum = 0;
     
     cout << "Enter numbers (enter 'stop' to finish):" << endl;
     while(1) {
-        long double num;
+        float num;
         
         cin >> num;
         
         if(to_string(num) == "stop") break;
 
         if(abs(num - lround(num)) < 1e-9) {
-            odd_sum = double_the_difference(lst);
+            odd_sum = 0; 
         }
         lst.push_back(num);    
     }
 
-    cout << "Sum of squared values: " << odd_sum << endl;
+    cout << "Sum of squared values: " << double_the_difference(lst) << endl;
     return 0; 
 }
