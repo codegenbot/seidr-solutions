03a89923d316c2aa3237f9c5441ec499b2a512e9
beep boop <githubbot@vadim.me>
2024-07-11 08:31:50 +0200

    Solution 34 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/qer0vab3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d4914bc2-585e-4f53-be1b-e57571e62 44f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_6.py
+++ Python_6.py
@@ -1,4 +1,7 @@
-```Python
+```
+import re
+from typing import List
+
 def parse_nested_parens(paren_string: str) -> List[int]:
     result = []
     for group in re.findall(r'\([^()]+?\)', paren_string):
