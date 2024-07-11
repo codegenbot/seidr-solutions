09427181297b0fa63f79bb911ab386c404273978
beep boop <githubbot@vadim.me>
2024-07-11 19:30:09 +0200

    Solution 52 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nxewkfgj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e9d02fdb-ee45-4db4-9207-e6a54a80a 214.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,6 +1,5 @@
-```python
 def double_the_difference(list1, list2):
-    difference = abs(max(list2) - min(list1))
+    difference = abs((max(list2) - min(list1)))
     return difference * 2
 
 print(double_the_difference([1, 3, 5], [8, 4, 7]))
