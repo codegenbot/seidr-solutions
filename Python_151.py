ffb399ca9b01e4b0a45e97fd234a2c3b919a55f9
beep boop <githubbot@vadim.me>
2024-07-11 15:38:47 +0200

    Solution 75 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dmxi7cce Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/72dc1423-ccb9-4f95-8b15-d3fdf6ea2 3a4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,10 +1,9 @@
-```
 def double_the_difference(lst):
     if len(lst) == 2:
         a, b = map(int, lst)
         return abs(a - b) * 2
     else:
-        print("Please enter exactly two numbers!")
+        return "Please enter exactly two numbers!"
 
 print("Enter elements separated by comma: ")
 user_input = input()
