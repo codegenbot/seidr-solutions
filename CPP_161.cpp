3a419011fc9f7c4b3039da45963c2917fa28ea2f
beep boop <githubbot@vadim.me>
2024-07-11 06:34:06 +0200

    Solution 79 of CPP/161, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/rl258xcu Prompt: Complete the following code given the task description and function signature.

--- CPP_161.cpp
+++ CPP_161.cpp
@@ -1,18 +1,13 @@
-string solve(string s) {
+string solve(string s){
     string result = "";
-    bool foundLetter = false;
-
-    for (int i = 0; i < s.length(); i++) {
-        if (!foundLetter && !isalpha(s[i])) {
-            foundLetter = true;
-        } else if (foundLetter && !isalpha(s[i])) {
-            foundLetter = false;
-        } else if (isalpha(s[i])) {
-            result += (foundLetter ? tolower(s[i]) : toupper(s[i]));
-        } else {
+    for(int i=0; i<s.length(); i++){
+        if(isalpha(s[i])){
+            if(islower(s[i])) result += toupper(s[i]);
+            else result += tolower(s[i]);
+        }else{
             result += s[i];
         }
     }
-
-    return (result.length() > 0) ? result : string(reverse(s));
+    if(result.length() == 0) return string(s).reverse();
+    return result;
 }
