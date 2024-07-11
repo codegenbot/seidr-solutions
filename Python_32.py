1123056bb9eee31dc7abe709d3cb61989b299061
beep boop <githubbot@vadim.me>
2024-07-11 23:20:56 +0200

    Solution 10 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fvnu47iy Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,4 +1,17 @@
-Here is the completed code:
+import math
+
+
+def poly(xs: list, x: float):
+    return sum([coeff * math.pow(x, i) for i, coeff in enumerate(xs)])
+
 
 def find_zero(xs: list):
-    return -xs[0] / xs[-1]
+    n = len(xs)
+    if n % 2 != 0:
+        raise ValueError("xs must have even number of coefficients")
+    a0 = xs[0]
+    an = xs[-1]
+    if math.fabs(an) < math.fabs(a0):
+        raise ValueError("largest non zero coefficient should be the leading one")
+    x = -a0 / an
+    return round(x, 2)
