6a0ddc9dc9d51a1bd8213a27799967178120fd3f
beep boop <githubbot@vadim.me>
2024-07-11 13:30:45 +0200

    Solution 9 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/etx4my5z Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e1e0c42d-fe41-47ca-8e85-c24bf23aa fa3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_9.py
+++ Python_9.py
@@ -1,8 +1,9 @@
+```
 from typing import List
 
 def rolling_max(numbers: List[int]) -> List[int]:
-    if len(numbers) == 1:
-        return numbers
+    if len(numbers) < 2:
+        return []
     result = [numbers[0]]
     max_so_far = numbers[0]
     for num in numbers[1:]:
