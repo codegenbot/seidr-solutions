2bd870fa90b25f56413508e6edc87cdab8066d4a
beep boop <githubbot@vadim.me>
2024-07-11 09:53:46 +0200

    Solution 22 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/md3ef0th Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e93bdbc7-246d-426d-bd23-71f9112b7 7e2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_41.py
+++ Python_41.py
@@ -1,10 +1,11 @@
 def car_race_collision(distance):
     time_to_meet = int(input("Enter time at which both cars meet: "))
-    
+
     speed1 = distance / time_to_meet
     speed2 = (distance - speed1 * time_to_meet) / time_to_meet
-    
+
     return speed1, speed2
 
-distance = int(input("Enter the total race distance: "))
-car_race_collision(distance)
+
+speed1, speed2 = car_race_collision(int(input("Enter the total distance: ")))
+print(f"Car 1's speed is {speed1} and Car 2's speed is {speed2}")
