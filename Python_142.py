5c3b9723fc462961f707fadecfc1889c0a783b88
beep boop <githubbot@vadim.me>
2024-07-11 20:14:04 +0200

    Solution 54 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/z6dgsuc9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/177fdd33-b86c-4cb8-8665-0860e0853 c46.py", line 46, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/177fdd33-b86c-4cb8-8665-0860e0853 c46.py", line 16, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,15 +1,8 @@
-```
 def sum_squares(lst):
     total = 0
     for num in lst:
-        if isinstance(num, int) and isinstance(lst, list):
-            if num % 2 == 0 and num % 3 != 0:
-                total += num**2
-            elif (num % 4 == 0 and num % 6 != 0) or (num % 4 == 0 and num % 6 == 0):
-                if num % 6 == 0:
-                    total += num**2
-                else:
-                    total += num**3
-        else:
-            return "Invalid input. Please provide a list of integers."
+        if num % 2 == 0 and num % 3 != 0:
+            total += num**2
+        elif (num % 4 == 0 and num % 6 == 0) or (num % 4 == 0 and num % 3 != 0):
+            total += num**2 if num % 6 == 0 else num**3
     return total
