6a3e5c8eba9594f00051221af796aa90846a3fbb
beep boop <githubbot@vadim.me>
2024-07-11 16:53:27 +0200

    Solution 47 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/bvg9l28l Prompt:       |                    ^~~ d66369d0-0b1f-4897-9bc5-8201487cbcff.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d66369d0-0b1f-4897-9bc5-8201487cbcff.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d66369d0-0b1f-4897-9bc5-8201487cbcff.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d66369d0-0b1f-4897-9bc5-8201487cbcff.cpp:18: d66369d0-0b1f-4897-9bc5-8201487cbcff.cpp: In function ‘int main()’: d66369d0-0b1f-4897-9bc5-8201487cbcff.cpp:28:20: error: ‘even_odd_palindrome’ was  not declared in this scope    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ d66369d0-0b1f-4897-9bc5-8201487cbcff.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -6,3 +6,20 @@ bool issame(vector<int> a, vector<int> b){
 }
 
 vector<int> even_odd_palindrome(int n);
+
+vector<int> even_odd_palindrome(int n){
+    vector<int> result(2, 0);
+    for(int i=1; i<=n; ++i){
+        string num = to_string(i);
+        string rev_num = num;
+        reverse(rev_num.begin(), rev_num.end());
+        if(num == rev_num){
+            if(i%2 == 0){
+                result[0]++;
+            } else {
+                result[1]++;
+            }
+        }
+    }
+    return result;
+}
