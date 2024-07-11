dc471d693b7d1d7ec68d54604752224e4c1638ea
beep boop <githubbot@vadim.me>
2024-07-11 03:06:52 +0200

    Solution 29 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fcqijk62 Prompt: c8f6977f-cf2a-4c55-a486-f6e502cde1bb.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ c8f6977f-cf2a-4c55-a486-f6e502cde1bb.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ c8f6977f-cf2a-4c55-a486-f6e502cde1bb.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ c8f6977f-cf2a-4c55-a486-f6e502cde1bb.cpp:2:10: fatal error: cryptopp/sha.h: No s uch file or directory     2 | #include "cryptopp/sha.h"       |          ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -14,8 +14,7 @@ int main() {
     sha.Calculate((const unsigned char*)s.c_str(), (int)s.size(), md5);
 
     for(int i = 0; i < 16; ++i) {
-        ss << ((md5[i] * 4 > 9 ? std::hex << std::showbase : std::hex << std::noshowbase)
-            << std::setfill('0') << std::setw(2) << (int)(md5[i]));
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)(md5[i]);
     }
 
     std::cout << "The SHA1 is: " << ss.str() << std::endl;
