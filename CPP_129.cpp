a440ab4bd96035e4843505a54997beab7b3397e5
beep boop <githubbot@vadim.me>
2024-07-11 03:51:45 +0200

    Solution 57 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/h7augh0t Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,89 +1,58 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-vector<int> minPath(vector<vector<int>> grid, int k) {
+vector<int> minPath(vector<vector<int>>& grid, int k) {
     int n = grid.size();
-    vector<vector<int>> dp(n, vector<int>(n, 0));
-    
+    vector<vector<int>> dp(n, vector<int>(n));
+    vector<int> res(k);
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (k == 1) {
+            if (i == 0) {
                 dp[i][j] = grid[i][j];
             } else {
                 int minVal = INT_MAX;
-                if (i > 0) minVal = min(minVal, dp[i-1][j]);
-                if (j > 0) minVal = min(minVal, dp[i][j-1]);
-                if (i < n - 1) minVal = min(minVal, dp[i+1][j]);
-                if (j < n - 1) minVal = min(minVal, dp[i][j+1]);
+                for (int x = -1; x <= 1; x++) {
+                    for (int y = -1; y <= 1; y++) {
+                        if ((x == 0 && y == 0) || i + x < 0 || j + y < 0 || i + x >= n || j + y >= n)
+                            continue;
+                        minVal = min(minVal, dp[i + x][j + y]);
+                    }
+                }
                 dp[i][j] = grid[i][j] + minVal;
             }
         }
     }
-    
-    int i = 0, j = 0;
-    while (k > 0) {
-        if (i > 0 && j < n - 1 && (dp[i-1][j+1] < dp[i][j+1])) {
-            i--;
-            k--;
-        } else if (i < n - 1 && j > 0 && (dp[i+1][j-1] < dp[i][j-1])) {
-            j--;
-            k--;
-        } else if (i > 0 && j > 0 && (dp[i-1][j-1] < dp[i][j-1])) {
-            i--;
-            j--;
-            k -= 2;
-        } else if (i < n - 1 && j < n - 1 && (dp[i+1][j+1] < dp[i][j+1])) {
-            i++;
-            j++;
-            k -= 2;
-        } else if (i > 0) {
-            i--;
-            k--;
-        } else {
-            j--;
-            k--;
-        }
-    }
-    
-    vector<int> res;
-    while (k > 0) {
-        res.push_back(grid[i][j]);
-        if (k == 1) break;
-        if (i < n - 1 && j < n - 1 && grid[i+1][j+1] <= grid[i][j]) {
-            i++;
-            j++;
-            k -= 2;
-        } else if (i > 0 && j < n - 1 && grid[i-1][j+1] <= grid[i][j]) {
-            i--;
-            j++;
-            k -= 2;
-        } else if (i < n - 1 && j > 0 && grid[i+1][j-1] <= grid[i][j]) {
-            i++;
-            j--;
-            k -= 2;
-        } else if (i > 0 && j > 0 && grid[i-1][j-1] <= grid[i][j]) {
-            i--;
-            j--;
-            k -= 2;
-        } else if (i < n - 1) {
-            i++;
-            k--;
-        } else {
-            j--;
-            k--;
-        }
-    }
-    
-    return res;
-}
 
-int main() {
-    vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int k = 3;
-    vector<int> result = minPath(grid, k);
-    for (int i : result) {
-        cout << i << " ";
+    int val = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dp[i][j] <= val) {
+                val = dp[i][j];
+                res[0] = grid[i][j];
+            }
+        }
     }
-    return 0;
+
+    vector<int> path(k);
+    int pos = 1;
+    int x = 0, y = 0;
+    for (int i = k - 1; i >= 0; i--) {
+        path[i] = res[0];
+        res.erase(res.begin());
+        if (x == 0 && y == 0) {
+            break;
+        }
+        int minVal = INT_MAX;
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                if ((dx == 0 && dy == 0) || x + dx < 0 || y + dy < 0 || x + dx >= n || y + dy >= n)
+                    continue;
+                int val = dp[x + dx][y + dy];
+                if (val <= minVal) {
+                    minVal = val;
+                    x = x + dx, y = y + dy;
+                }
+            }
+        }
+    }
+
+    return path;
 }
