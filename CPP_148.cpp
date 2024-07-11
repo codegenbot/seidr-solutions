b5bfb6b27452b60fd4e14179bcfa738a477dec21
beep boop <githubbot@vadim.me>
2024-07-11 09:49:28 +0200

    Solution 7 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9t4atg8f Prompt:       |                              ^~~~~~ 79e13e52-f6e2-48a7-b607-5837616c34c1.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 79e13e52-f6e2-48a7-b607-5837616c34c1.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 79e13e52-f6e2-48a7-b607-5837616c34c1.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 79e13e52-f6e2-48a7-b607-5837616c34c1.cpp:29: 79e13e52-f6e2-48a7-b607-5837616c34c1.cpp: In function ‘int main()’: 79e13e52-f6e2-48a7-b607-5837616c34c1.cpp:39:20: error: ‘bf’ was not declared in this scope    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 79e13e52-f6e2-48a7-b607-5837616c34c1.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -3,9 +3,30 @@
 using namespace std;
 
 bool issame(vector<string> a, vector<string> b) {
-    // Function body
-}
+    vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+    vector<string> result;
+    int start = -1, end = -1;
 
-int main() {
-    // Test cases
+    for (int i = 0; i < 8; i++) {
+        if (a == planets[i]) {
+            start = i;
+        }
+        if (b == planets[i]) {
+            end = i;
+        }
+    }
+
+    if (start == -1 || end == -1) {
+        return false;
+    }
+
+    if (start > end) {
+        swap(start, end);
+    }
+
+    for (int i = start + 1; i < end; i++) {
+        result.push_back(planets[i]);
+    }
+
+    return true;
 }
