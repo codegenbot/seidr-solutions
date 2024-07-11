f0e5009699ca631d434c5737761eb25f82db0b19
beep boop <githubbot@vadim.me>
2024-07-11 11:22:25 +0200

    Solution 74 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/66vz1sa5 Prompt: 6e1b9d81-00cf-477b-a0ba-83721a21008e.cpp: In function ‘int main()’: 6e1b9d81-00cf-477b-a0ba-83721a21008e.cpp:29:9: error: ‘cout’ was not declared in  this scope    29 |         cout << "Subvector " << (i+1) << ": ";       |         ^~~~ 6e1b9d81-00cf-477b-a0ba-83721a21008e.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; 6e1b9d81-00cf-477b-a0ba-83721a21008e.cpp:33:17: error: ‘endl’ was not declared i n this scope    33 |         cout << endl;       |                 ^~~~ 6e1b9d81-00cf-477b-a0ba-83721a21008e.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,20 +1,32 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int>& nums) {
     int n = nums.size();
-    int min_diff = INT_MAX;
-    int min_idx = -1;
+    vector<int> left;
+    vector<int> right;
     for(int i=0; i<n-1; i++){
-        int diff = abs(nums[i+1] - nums[i]);
-        if(diff <= min_diff) min_diff = diff, min_idx = i;
+        if(nums[i+1] - nums[i] <= 0){
+            left.push_back(nums[i]);
+            right = {nums.begin()+i, nums.end()};
+            break;
+        } else {
+            left.push_back(nums[i]);
+        }
     }
-    if(min_idx == -1) {
-        return {{}, {}};
-    } else {
-        vector<int> left(min_idx+1), right(n-min_idx-1);
-        for(int i=0; i<=min_idx; i++) left.push_back(nums[i]);
-        for(int i=min_idx+1; i<n; i++) right.push_back(nums[i]);
-        return {{left}, {right}};
+    return {{left}, {vector<int>(right.begin(), right.end())}};
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 5};
+    vector<vector<int>> result = cutVector(nums);
+    for(int i=0; i<result.size(); i++) {
+        cout << "Subvector " << (i+1) << ": ";
+        for(int j=0; j<result[i].size(); j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
     }
+    return 0;
 }
