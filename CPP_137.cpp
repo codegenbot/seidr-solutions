473301969d074efba0e14248f3996150db5cc99a
beep boop <githubbot@vadim.me>
2024-07-11 03:12:27 +0200

    Solution 96 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9snlnzo8 Prompt: 6ef3ab5b-3a53-4eea-980e-4627ab119201.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,48 +1,50 @@
+```cpp
 #include <boost/any.hpp>
 #include <string>
-#include <boost/variant.hpp>
 
-using boost::any;
-using boost::variant;
-
-any compare_one(any a, any b) {
-    if (holds_alternative<int>(a) && holds_alternative<double>(b)) {
-        return get<int>(a) > get<double>(b) ? a : b;
+boost::any compare_one(boost::any a, boost::any b) {
+    if (boost::holds_alternative<int>(a) && boost::holds_alternative<double>(b)) {
+        return boost::get<int>(a) > boost::get<double>(b) ? a : b;
     }
-    else if (holds_alternative<double>(a) && holds_alternative<int>(b)) {
-        return get<double>(a) > get<int>(b) ? a : b;
+    else if (boost::holds_alternative<double>(a) && boost::holds_alternative<int>(b)) {
+        return boost::get<double>(a) > boost::get<int>(b) ? a : b;
     }
-    else if (holds_alternative<std::string>(a) && holds_alternative<double>(b)) {
+    else if (boost::holds_alternative<std::string>(a) && boost::holds_alternative<double>(b)) {
         std::string str = boost::get<std::string>(a);
-        double num = get<double>(b);
+        double num = boost::get<double>(b);
         return str > std::to_string(num) ? a : b;
     }
-    else if (holds_alternative<double>(a) && holds_alternative<std::string>(b)) {
+    else if (boost::holds_alternative<double>(a) && boost::holds_alternative<std::string>(b)) {
         std::string str = boost::get<std::string>(b);
-        double num = get<double>(a);
+        double num = boost::get<double>(a);
         return std::to_string(num) > str ? a : b;
     }
-    else if (holds_alternative<std::string>(a) && holds_alternative<std::string>(b)) {
+    else if (boost::holds_alternative<std::string>(a) && boost::holds_alternative<std::string>(b)) {
         std::string str1 = boost::get<std::string>(a);
         std::string str2 = boost::get<std::string>(b);
         return str1 > str2 ? a : b;
     }
-    else if (holds_alternative<int>(a) && holds_alternative<std::string>(b)) {
-        int num = get<int>(a);
+    else if (boost::holds_alternative<int>(a) && boost::holds_alternative<std::string>(b)) {
+        int num = boost::get<int>(a);
         std::string str = boost::get<std::string>(b);
         return std::to_string(num) > str ? a : b;
     }
-    else if (holds_alternative<std::string>(a) && holds_alternative<int>(b)) {
-        int num = get<int>(b);
+    else if (boost::holds_alternative<std::string>(a) && boost::holds_alternative<int>(b)) {
+        int num = boost::get<int>(b);
         std::string str = boost::get<std::string>(a);
         return str > std::to_string(num) ? a : b;
     }
-    else if (holds_alternative<double>(a) && holds_alternative<double>(b)) {
-        double num1 = get<double>(a);
-        double num2 = get<double>(b);
+    else if (boost::holds_alternative<double>(a) && boost::holds_alternative<double>(b)) {
+        double num1 = boost::get<double>(a);
+        double num2 = boost::get<double>(b);
+        return num1 > num2 ? a : b;
+    }
+    else if (boost::holds_alternative<int>(a) && boost::holds_alternative<int>(b)) {
+        int num1 = boost::get<int>(a);
+        int num2 = boost::get<int>(b);
         return num1 > num2 ? a : b;
     }
     else {
-        return any();
+        return boost::any();
     }
 }
