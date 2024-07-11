62e989aa78eb81d131881b6407fb1ff1b2c10ac6
beep boop <githubbot@vadim.me>
2024-07-11 18:09:24 +0200

    Solution 80 of CPP/64, ratio 0.8571428571428571 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zx5gdxbr Prompt: 4e5b0f93-c15e-4c6a-af8b-7ca477ce8fd4.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int vowels_count(string s){       |                  ^~~~~~ In file included from 4e5b0f93-c15e-4c6a-af8b-7ca477ce8fd4.cpp:11: 4e5b0f93-c15e-4c6a-af8b-7ca477ce8fd4.cpp: In function ‘int main()’: 4e5b0f93-c15e-4c6a-af8b-7ca477ce8fd4.cpp:13:25: error: ‘vowels_count’ cannot be used as a function    13 |     assert (vowels_count("ACEDY") == 3);       |             ~~~~~~~~~~~~^~~~~~~~~

--- CPP_64.cpp
+++ CPP_64.cpp
@@ -1,17 +1,12 @@
 #include <string>
-#include <cassert>
+#include <cctype>
 
 int vowels_count(std::string s){
     int count = 0;
     for(char c : s){
-        if(std::tolower(c) == 'a' || std::tolower(c) == 'e' || std::tolower(c) == 'i' || std::tolower(c) == 'o' || (std::tolower(c) == 'u' && c == s.back())){
+        if(tolower(c) == 'a' || tolower(c) == 'e' || tolower(c) == 'i' || tolower(c) == 'o' || (tolower(c) == 'u' && (c == s.back()))){
             count++;
         }
     }
     return count;
-}
-
-int main(){
-    assert(vowels_count("ACEDY") == 3);
-    return 0;
 }
