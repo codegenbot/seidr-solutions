57b11ce83f2487a679c0a1da790a60e6cb216e54
beep boop <githubbot@vadim.me>
2024-07-11 15:18:49 +0200

    Solution 62 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lwme29gf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4ba1aedd-74ff-4c9a-b6a5-7a8f9d0bc e97.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4ba1aedd-74ff-4c9a-b6a5-7a8f9d0bc e97.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4ba1aedd-74ff-4c9a-b6a5-7a8f9d0bc e97.py", line 4, in sort_third     l.sort(key=lambda x: x[2])   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4ba1aedd-74ff-4c9a-b6a5-7a8f9d0bc e97.py", line 4, in <lambda>     l.sort(key=lambda x: x[2])                          ~^^^ TypeError: 'int' object is not subscriptable

--- Python_33.py
+++ Python_33.py
@@ -1,5 +1,5 @@
 def sort_third(l):
     if len(l) < 3:
         return []
-    l.sort(key=lambda x: x[2])
-    return [l[i][2] for i in range(len(l)) if (i+1)%3 == 0][:1]
+    l.sort(key=lambda x: int(x))
+    return [x for i, x in enumerate(l) if (i + 1) % 3 == 0][:1]
