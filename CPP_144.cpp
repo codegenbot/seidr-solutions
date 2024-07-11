3517478fe1dd9e3bdf1864c830f9572b6f9f28e6
beep boop <githubbot@vadim.me>
2024-07-11 11:16:41 +0200

    Solution 2 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/62wfe1t1 Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,11 +1,38 @@
 #include <string>
+#include <vector>
+
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = stoi(substring(x, 1, x.find('/')));
-    int b = stoi(substring(x, x.find('/') + 1));
-    int c = stoi(substring(n, 1, n.find('/')));
-    int d = stoi(substring(n, n.find('/') + 1));
+    int numeratorX = 0, denominatorX = 1;
+    int numeratorN = 0, denominatorN = 1;
 
-    return (a * d) == (b * c);
+    // parse fraction x
+    size_t pos = x.find('/');
+    numeratorX = stoi(x.substr(0, pos));
+    denominatorX = stoi(x.substr(pos + 1));
+
+    // parse fraction n
+    pos = n.find('/');
+    numeratorN = stoi(n.substr(0, pos));
+    denominatorN = stoi(n.substr(pos + 1));
+
+    int commonDivisor = gcd(denominatorX, denominatorN);
+    int numeratorXNew = numeratorX / commonDivisor;
+    int denominatorXNew = denominatorX / commonDivisor;
+
+    if (numeratorN % denominatorXNew != 0) {
+        return false;
+    }
+
+    return true;
+}
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
