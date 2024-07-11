5343e5313b542d872f8d92fa95746f2c390454da
beep boop <githubbot@vadim.me>
2024-07-11 22:22:21 +0200

    Solution 63 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/1lthbr49 Prompt:                  from 8f63b3ec-1b28-42ab-b3fa-d76b6c9ae8e3.cpp:1: /usr/include/c++/11/bits/ios_base.h:1071:3: note: ‘std::fixed’ declared here  1071 |   fixed(ios_base& __base)       |   ^~~~~ 8f63b3ec-1b28-42ab-b3fa-d76b6c9ae8e3.cpp:22:22: error: ‘setprecision’ was not de clared in this scope; did you mean ‘std::setprecision’?    22 |     cout << fixed << setprecision(5);       |                      ^~~~~~~~~~~~       |                      std::setprecision In file included from 8f63b3ec-1b28-42ab-b3fa-d76b6c9ae8e3.cpp:3: /usr/include/c++/11/iomanip:195:3: note: ‘std::setprecision’ declared here   195 |   setprecision(int __n)       |   ^~~~~~~~~~~~ 8f63b3ec-1b28-42ab-b3fa-d76b6c9ae8e3.cpp:23:30: error: ‘endl’ was not declared i n this scope; did you mean ‘std::endl’?    23 |     cout << totalDistance << endl;       |                              ^~~~       |                              std::endl In file included from /usr/include/c++/11/iostream:39,                  from 8f63b3ec-1b28-42ab-b3fa-d76b6c9ae8e3.cpp:1: /usr/include/c++/11/ostream:684:5: note: ‘std::endl’ declared here   684 |     endl(basic_ostream<_CharT, _Traits>& __os)       |     ^~~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,27 +1,3 @@
-```
-#include <iostream>
-#include <cmath> 
-#include <iomanip>
-
-int main() {
-    double startHeight;
-    cin >> startHeight;
-
-    double firstBounceHeight;
-    cin >> firstBounceHeight;
-
-    double bouncinessIndex = firstBounceHeight / startHeight;
-
-    int numBounces;
-    cin >> numBounces;
-
-    double totalDistance = 0.0;
-    for(int i = 1; i <= numBounces; i++) {
-        totalDistance += (2.0 * startHeight) * pow(bouncinessIndex, i);
-    }
-
-    cout << std::fixed << std::setprecision(5);
-    cout << totalDistance << std::endl;
-
-    return 0;
-}
+```c++
+cout << std::fixed << std::setprecision(5);
+cout << totalDistance << std::endl;
