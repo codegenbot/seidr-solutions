8730b3fbab77f38e62b03c93c6b2e4c3d7f5d79e
beep boop <githubbot@vadim.me>
2024-07-11 12:39:19 +0200

    Solution 71 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/l6tw0a3u Prompt:    42 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7fd4b21c-4095-4142-b9a5-db6ea9d034e7.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ 7fd4b21c-4095-4142-b9a5-db6ea9d034e7.cpp:37:5: note: ‘int main()’ previously def ined here    37 | int main() {       |     ^~~~ In file included from 7fd4b21c-4095-4142-b9a5-db6ea9d034e7.cpp:41: 7fd4b21c-4095-4142-b9a5-db6ea9d034e7.cpp: In function ‘int main()’: 7fd4b21c-4095-4142-b9a5-db6ea9d034e7.cpp:51:20: error: ‘sorted_list_sum’ was not  declared in this scope    51 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 7fd4b21c-4095-4142-b9a5-db6ea9d034e7.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,4 +1,4 @@
-```cpp
+```
 vector<vector<string>> sorted_list_sum(vector<string> lst) {
     vector<vector<string>> result;
 
@@ -7,13 +7,13 @@ vector<vector<string>> sorted_list_sum(vector<string> lst) {
             result.push_back({str});
     }
 
-    std::sort(result.begin(), result.end(),
-              [](const vector<string>& a, const vector<string>& b) {
-                  if (a.size() != b.size())
-                      return a.size() - b.size();
-                  else
-                      return a[0] < b[0];
-              });
+    sort(result.begin(), result.end(),
+         [](const vector<string>& a, const vector<string>& b) {
+             if (a.size() != b.size())
+                 return a.size() - b.size();
+             else
+                 return a[0] < b[0];
+         });
 
     return result;
 }
@@ -32,25 +32,6 @@ bool issame(vector<string> a, vector<string> b) {
             return false;
     }
     return a.size() == b.size();
-}
-
-vector<vector<string>> sorted_list_sum(vector<string> lst) {
-    vector<vector<string>> result;
-
-    for (const auto& str : lst) {
-        if (str.length() % 2 == 0)
-            result.push_back({str});
-    }
-
-    std::sort(result.begin(), result.end(),
-              [](const vector<string>& a, const vector<string>& b) {
-                  if (a.size() != b.size())
-                      return a.size() - b.size();
-                  else
-                      return a[0] < b[0];
-              });
-
-    return result;
 }
 
 int main() {
