d8dd83e2b8d27fc979de00ae99937dc179ad4de8
beep boop <githubbot@vadim.me>
2024-07-11 10:58:20 +0200

    Solution 43 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/r8hjdq3f Prompt:       |           ^~~~~~ In file included from 79d4302a-38d6-4744-80a8-d149263ef975.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 79d4302a-38d6-4744-80a8-d149263ef975.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 79d4302a-38d6-4744-80a8-d149263ef975.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 79d4302a-38d6-4744-80a8-d149263ef975.cpp:16: 79d4302a-38d6-4744-80a8-d149263ef975.cpp: In function ‘int main()’: 79d4302a-38d6-4744-80a8-d149263ef975.cpp:26:20: error: ‘even_odd_count’ was not declared in this scope    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 79d4302a-38d6-4744-80a8-d149263ef975.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -4,24 +4,13 @@
 
 bool issame(std::vector<int> a, std::vector<int> b);
 
-std::vector<int> even_odd_count(int num) {
-    std::vector<int> counts = {0, 0};
-    std::string numStr = std::to_string(std::abs(num));
-    for (char c : numStr) {
-        if (c % 2 == 0) {
-            counts[0]++;
-        } else {
-            counts[1]++;
-        }
-    }
-    return counts;
-}
+std::vector<int> even_odd_count(int num);
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    for (size_t i = 0; i < a.size(); i++) {
+    for (size_t i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
