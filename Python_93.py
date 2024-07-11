80927b64b3b0141705e6eb93f394e91fcd9beeb1
beep boop <githubbot@vadim.me>
2024-07-11 15:53:09 +0200

    Solution 44 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d03dbk1h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f6877e30-6328-4111-987e-73d5c2194 7f7.py", line 23, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f6877e30-6328-4111-987e-73d5c2194 7f7.py", line 19, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -5,12 +5,10 @@ def encode(message):
     for char in message:
         if char.isalpha():
             if char.lower() in vowels:
-                new_char = chr((ord(char.lower()) - 97 + 2) % 26 + 97)
-            elif char.isupper():
-                new_char = chr((ord(char.lower()) - 65 + 2) % 26 + 65)
+                index = ord(char.lower()) - 97
+                new_char = chr((index + 2) % 26 + 97)
             else:
                 new_char = char.swapcase()
             result += new_char
         else:
             result += char
-    return result
