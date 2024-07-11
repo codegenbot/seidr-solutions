26346715220d7a8d29222a2d07307693eb332f6b
beep boop <githubbot@vadim.me>
2024-07-11 05:11:55 +0200

    Solution 33 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2dqf0nts Prompt:  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ef9202e2-ee23-4231-9239-309e18cfb047.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ef9202e2-ee23-4231-9239-309e18cfb047.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ef9202e2-ee23-4231-9239-309e18cfb047.cpp:12: ef9202e2-ee23-4231-9239-309e18cfb047.cpp: In function ‘int main()’: ef9202e2-ee23-4231-9239-309e18cfb047.cpp:22:20: error: ‘filter_integers’ was not  declared in this scope    22 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ ef9202e2-ee23-4231-9239-309e18cfb047.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -10,7 +10,7 @@ vector<any> filter_integers(list<any> values) {
     for (const auto& value : values) {
         any_cast<int>(value);
         if (any_cast<int>(value)) {
-            result.push_back(any_cast<any>(static_cast<int>(value)));
+            result.push_back(any_cast<int>(value));
         }
     }
     return result;
