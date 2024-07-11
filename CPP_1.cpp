9af5ee1bc0db6a3198252cbed3e758e5d9a68af0
beep boop <githubbot@vadim.me>
2024-07-11 11:22:21 +0200

    Solution 35 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/3hcg7pjp Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 2017aed0-c0e2-47fe-8849-5769071c82c3.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2017aed0-c0e2-47fe-8849-5769071c82c3.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2017aed0-c0e2-47fe-8849-5769071c82c3.cpp:26: 2017aed0-c0e2-47fe-8849-5769071c82c3.cpp: In function ‘int main()’: 2017aed0-c0e2-47fe-8849-5769071c82c3.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 2017aed0-c0e2-47fe-8849-5769071c82c3.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,9 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
-
 vector<string> separate_paren_groups(string paren_string);
 
 bool issame(vector<string> a, vector<string> b);
@@ -33,36 +27,16 @@ vector<string> separate_paren_groups(string paren_string) {
     return result;
 }
 
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int main() {
     string input;
-    cout << "Enter a string with parentheses: ";
     cin >> input;
 
     vector<string> groups = separate_paren_groups(input);
 
-    cout << "Separated groups:" << endl;
-    for (string group : groups) {
-        cout << group << endl;
-    }
-
-    vector<string> test_groups = separate_paren_groups(input);
-
-    if (issame(groups, test_groups)) {
-        cout << "The separated groups are the same." << endl;
+    if (issame(groups, groups)) {
+        cout << "Groups are the same" << endl;
     } else {
-        cout << "The separated groups are different." << endl;
+        cout << "Groups are different" << endl;
     }
 
     return 0;
