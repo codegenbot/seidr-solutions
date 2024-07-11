f19af64355e938c0f6fdcb5b2c68a0036a607cff
beep boop <githubbot@vadim.me>
2024-07-11 03:14:57 +0200

    Solution 12 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/tbtrd5cm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26a1e8f9-5288-4951-8d92-d795d4848 d3c.py", line 11, in <module>     check(solution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26a1e8f9-5288-4951-8d92-d795d4848 d3c.py", line 6, in check     assert solution([3, 13, 2, 9]) == 3            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_121.py
+++ Python_121.py
@@ -1,7 +1,2 @@
-Here is the solution:
-
-```python
 def solution(lst):
-    odd_sum = sum(i for i in lst[1::2] if i % 2 != 0)
-    even_count = len([i for i in lst[::2] if i % 2 == 0])
-    return f"The sum of all odd numbers is {odd_sum}, and the count of all even numbers is {even_count}."
+    return sum(i for i in lst[1:] if i % 2 != 0)
