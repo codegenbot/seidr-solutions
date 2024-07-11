a6f36ec69566bcc2fc2a1223ff297cc54a436392
beep boop <githubbot@vadim.me>
2024-07-11 02:48:47 +0200

    Solution 68 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/kqas3fcz Prompt: 20 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dc93e219-0d1e-45c6-8096-e2498f716 5d7.py", line 12, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dc93e219-0d1e-45c6-8096-e2498f716 5d7.py", line 9, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^ TypeError: car_race_collision() missing 1 required positional argument: 'm'

--- Python_41.py
+++ Python_41.py
@@ -1,5 +1,8 @@
+```
 def car_race_collision(n: int, m: int):
     if n < m:
-        return (n - 1) * m
+        return n * m
     else:
-        return (m - 1) * (2 * m - 1) // 2
+        return m * (m - 1)
+
+print(car_race_collision(10, 10))
