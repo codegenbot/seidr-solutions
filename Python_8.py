11ef0234d6960a2cbcf268cb190e8d874f249797
beep boop <githubbot@vadim.me>
2024-07-11 05:02:16 +0200

    Solution 46 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/p2jf7nxt Prompt: Program did not receive expected input

--- Python_8.py
+++ Python_8.py
@@ -6,8 +6,7 @@ def sum_product(numbers: List[int]) -> Tuple[int, int]:
     if isinstance(numbers, tuple) and len(numbers) == 0:
         return (0, 1)
     elif not isinstance(numbers, list):
-        return "Invalid Input"
-
+        return 
     total_sum = 0
     product = 1
 
