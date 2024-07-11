e76ff15d04d7d2b032ff269bde59271479883ae9
beep boop <githubbot@vadim.me>
2024-07-11 12:23:08 +0200

    Solution 61 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hn4pgr1r Prompt:       |                              ^~~~~~ 8d50f7d7-6ffa-455f-ae5c-ac5e7a0a3fdd.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8d50f7d7-6ffa-455f-ae5c-ac5e7a0a3fdd.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8d50f7d7-6ffa-455f-ae5c-ac5e7a0a3fdd.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8d50f7d7-6ffa-455f-ae5c-ac5e7a0a3fdd.cpp:32: 8d50f7d7-6ffa-455f-ae5c-ac5e7a0a3fdd.cpp: In function ‘int main()’: 8d50f7d7-6ffa-455f-ae5c-ac5e7a0a3fdd.cpp:42:20: error: ‘bf’ was not declared in this scope    42 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 8d50f7d7-6ffa-455f-ae5c-ac5e7a0a3fdd.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -2,7 +2,33 @@ bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-int main() {
-    assert(bf("Jupiter", "Neptune") == vector<string>({"Earth"})); 
-    return 0;
+vector<string> bf(string planet1, string planet2) {
+    vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+    int index1 = -1;
+    int index2 = -1;
+
+    for (int i = 0; i < planets.size(); i++) {
+        if (planet1 == planets[i]) {
+            index1 = i;
+        }
+        else if (planet2 == planets[i]) {
+            index2 = i;
+        }
+    }
+
+    if (index1 == -1 || index2 == -1) {
+        return {};
+    }
+
+    int start = min(index1, index2);
+    int end = max(index1, index2);
+
+    vector<string> result;
+    for (int i = 0; i < planets.size(); i++) {
+        if ((index1 <= i && i <= index2) || (i >= index1 && i <= index2)) {
+            result.push_back(planets[i]);
+        }
+    }
+
+    return result;
 }
