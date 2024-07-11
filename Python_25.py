a8a10058dd38aefaca0e4394318dc603d671f007
beep boop <githubbot@vadim.me>
2024-07-11 14:53:35 +0200

    Solution 26 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/4cwfx790 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/11c149da-13cb-446a-95d9-c5e43f560 03f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,7 +1,20 @@
-def calculate_factors():
-    num = int(input("Enter a number: "))
+from typing import List
+import math
+
+
+def factorize(n: int) -> List[int]:
     factors = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            factors.append(i)
-    print("Factors of {} are {}".format(num, ", ".join(map(str, factors))))
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            count = 0
+            while n % i == 0:
+                n //= i
+                count += 1
+            factors.append(i ** count)
+            i += 1
+    if n > 1:
+        factors.append(n)
+    return factors
