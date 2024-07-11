61e645358cab181c15b9700e39cab8436a12334f
beep boop <githubbot@vadim.me>
2024-07-11 13:40:06 +0200

    Solution 16 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ixjphj4n Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/08b0e5af-0d93-4923-ada0-edf85eb81 7df.py", line 18, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/08b0e5af-0d93-4923-ada0-edf85eb81 7df.py", line 15, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,4 +1,3 @@
-```Python
 def is_palindrome(string: str) -> bool:
     return string == string[::-1]
 
@@ -7,8 +6,8 @@ def make_palindrome(string: str) -> str:
     if is_palindrome(string):
         return string + string[::-1]
     else:
-        for i in range(len(string)):
+        for i in range(len(string), 0, -1):
             postfix = string[i:]
-            if is_palindrome(postfix): 
-                prefix = string[:i][::-1] 
-                return prefix + postfix + postfix[::-1]
+            if is_palindrome(postfix):
+                prefix = string[:i][::-1]
+                return prefix + string + postfix
