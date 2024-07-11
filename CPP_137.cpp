0e5c189c0b345e832d6456f0b6b825f956df6423
beep boop <githubbot@vadim.me>
2024-07-11 05:12:00 +0200

    Solution 76 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/497575qq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,13 +1,18 @@
+#include <boost/any.hpp>
+
 using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_any_of<int>(a) && is_any_of<int>(b)) {
-        return any_cast<int>(a) > any_cast<int>(b) ? a : (any_cast<int>(a) < any_cast<int>(b) ? b : boost::any("None"));
-    } else if (is_any_of<float>(a) && is_any_of<float>(b)) {
-        return any_cast<float>(a) > any_cast<float>(b) ? a : (any_cast<float>(a) < any_cast<float>(b) ? b : boost::any("None"));
-    } else if (is_any_of<string>(a) && is_any_of<string>(b)) {
-        return !any_cast<string>(a).compare(0, string::npos, any_cast<string>(b), 0, string::npos) ? a : b;
-    } else {
-        throw invalid_argument("Invalid types");
+    if (is_same<any_type, int>(a.type()) && is_same<any_type, float>(b.type())) {
+        return b;
+    } else if (is_same<any_type, float>(a.type()) && is_same<any_type, string>(b.type())) {
+        return b;
+    } else if (is_same<any_type, string>(a.type()) && is_same<any_type, int>(b.type())) {
+        return a;
+    } else if (is_same<any_type, string>(a.type()) && is_same<any_type, float>(b.type())) {
+        return (boost::any_cast<string>(a) > boost::any_cast<string>(b)) ? a : b;
+    } else if (is_same<any_type, int>(a.type()) && is_same<any_type, int>(b.type())) {
+        return (boost::any_cast<int>(a) > boost::any_cast<int>(b)) ? a : b;
     }
+    return "None";
 }
