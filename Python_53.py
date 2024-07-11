39e09461828aa3c26a36ac408411ec246e62d781
beep boop <githubbot@vadim.me>
2024-07-11 11:00:52 +0200

    Solution 27 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ifoxj73p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9dadea11-8aea-4bed-ab04-88ef07553 baf.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,18 +1,14 @@
-```
-def add(x: int, y: int):
-    if x < 0 and y < 0:
-        return -1 * (abs(x) + abs(y))
-    else:
-        return x + y
+    def add(x: int, y: int):
+        if x < 0 and y < 0:
+            return -1 * (abs(x) + abs(y))
+        else:
+            return x + y
 
-
-while True:
-    try:
-        while True:
+    while True:
+        try:
             x = int(input("Enter first number: "))
             y = int(input("Enter second number: "))
             print(add(x, y))  
             break
-        break
-    except ValueError:
-        print("Invalid input. Please enter whole numbers.")
+        except ValueError:
+            print("Invalid input. Please enter whole numbers.")
