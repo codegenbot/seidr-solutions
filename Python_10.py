fc8846263b5612890250051130310539cee00ddb
beep boop <githubbot@vadim.me>
2024-07-11 16:09:45 +0200

    Solution 80 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tdfielxa Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/70e1e8c8-dcc2-4bc0-b4c7-0ffbab16b 9e4.py", line 20, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/70e1e8c8-dcc2-4bc0-b4c7-0ffbab16b 9e4.py", line 17, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,15 +1,6 @@
-def make_palindrome(s: str) -> str:
-    s = list(s)
-    i, j = 0, len(s) - 1
-    while i < j:
-        if s[i] != s[j]:
-            t = s[j]
-            for k in range(len(s) - 1, -1, -1):
-                if s[k] == t:
-                    s[k + 1 :] = list(t * (len(s) - k - 1))
-                    break
-            else:
-                s[: j + 1] = list(t * (j + 1))
-        i += 1
-        j -= 1
-    return "".join(s)
+for k in range(j, i-1, -1):
+    if s[k] == t:
+        s[i : k + 1] = list(t * (k - i))
+        break
+else:
+    s[i : j+1] = list(t * (j - i+1))
