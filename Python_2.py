9d896104676903a07020b23e80d0a4ac62ec3c24
beep boop <githubbot@vadim.me>
2024-07-11 07:47:30 +0200

    Solution 12 of Python/2, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mtyau5r5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/017897a7-6bbc-43e0-b454-bd770ad45 4d0.py", line 8, in <module>     check(truncate_number)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/017897a7-6bbc-43e0-b454-bd770ad45 4d0.py", line 5, in check     assert abs(truncate_number(123.456) - 0.456) < 1e-6                ^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: truncate_number() missing 1 required positional argument: 'n'

--- Python_2.py
+++ Python_2.py
@@ -1,3 +1,5 @@
-def truncate_number(number: float, n: int) -> float:
+def truncate_number() -> str:
+    number = float(input("Enter the number to be truncated: "))
+    n = int(input("Enter the number of decimal places to truncate: "))
     multiplier = 10**-n
-    return round(number * multiplier) / multiplier
+    return str(round(number * multiplier) / multiplier)
