5b864c0cb6016002968086aea295e7aefc7e2660
beep boop <githubbot@vadim.me>
2024-07-11 16:09:46 +0200

    Solution 18 of CPP/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/60cvfrg5 Prompt:       |                    ^~~ 9c01f865-b38d-48ab-b3e1-2823fafb5ae2.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9c01f865-b38d-48ab-b3e1-2823fafb5ae2.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9c01f865-b38d-48ab-b3e1-2823fafb5ae2.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9c01f865-b38d-48ab-b3e1-2823fafb5ae2.cpp:21: 9c01f865-b38d-48ab-b3e1-2823fafb5ae2.cpp: In function ‘int main()’: 9c01f865-b38d-48ab-b3e1-2823fafb5ae2.cpp:31:20: error: ‘f’ was not declared in t his scope    31 |     assert (issame(f(3) , {1, 2, 6}));       |                    ^ 9c01f865-b38d-48ab-b3e1-2823fafb5ae2.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(f(3) , {1, 2, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -5,30 +5,10 @@ bool issame(std::vector<int> a, std::vector<int> b){
     if(a.size() != b.size()){
         return false;
     }
-    for(int i=0; i<a.size(); i++){
+    for(size_t i=0; i<a.size(); i++){
         if(a[i] != b[i]){
             return false;
         }
     }
     return true;
-}
-
-std::vector<int> f(int n){
-    std::vector<int> result(n);
-    for(int i=0; i<n; i++){
-        if(i % 2 == 0){
-            int fact = 1;
-            for(int j=1; j<=i; j++){
-                fact *= j;
-            }
-            result[i] = fact;
-        } else {
-            int sum = 0;
-            for(int j=1; j<=i; j++){
-                sum += j;
-            }
-            result[i] = sum;
-        }
-    }
-    return result;
 }
