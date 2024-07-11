41040f82646d044a861df7ce18c9a28466b40356
beep boop <githubbot@vadim.me>
2024-07-11 02:50:02 +0200

    Solution 64 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/wgqv5w84 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6fa3cc8d-1fd3-479c-b085-5b4fab702 7e6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,12 +1,11 @@
-```
-def solution(lst):
-    try:
-        lst = list(map(int, lst.split()))
-    except ValueError:
-        return "Invalid input. Please enter integers only."
-    
-    if not lst:
-        return 0
-    return sum(1 for _ in lst[::2]) / len(lst)
+    def solution(lst):
+        try:
+            lst = list(map(int, lst.split()))
+        except ValueError:
+            return "Invalid input. Please enter integers only."
+        
+        if not lst:
+            return 0
+        return sum(1 for _ in lst[::2]) / len(lst)
 
-print(solution(input()))
+    print(solution(input()))
