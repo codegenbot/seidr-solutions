ff8279d287d4c67a2cec3d0c3a075c4684b65533
beep boop <githubbot@vadim.me>
2024-07-11 11:29:39 +0200

    Solution 11 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hn4pgr1r Prompt:       |                              ^~~~~~ 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:32: 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp: In function ‘int main()’: 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:42:20: error: ‘bf’ was not declared in this scope    42 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 15744654-fa6d-491a-b8e8-e2c9a9069673.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,20 +1,8 @@
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-bool issame(vector<string> v1, vector<string> v2) {
-    if (v1.size() != v2.size()) {
-        return false;
-    }
-    sort(v1.begin(), v1.end());
-    sort(v2.begin(), v2.end());
-    for (int i = 0; i < v1.size(); i++) {
-        if (v1[i] != v2[i]) {
-            return false;
-        }
-    }
-    return true;
-}
+bool issame(vector<string>, vector<string>);
+vector<string> bf(string, string);
 
 vector<string> bf(string planet1, string planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
