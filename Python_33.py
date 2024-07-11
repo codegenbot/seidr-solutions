3889f87b7478fe8edd2e2ba9b7619019689c785b
beep boop <githubbot@vadim.me>
2024-07-11 10:19:11 +0200

    Solution 82 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt: [2, 4, 7, 10, False, True] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc46fcdf-c2d7-4619-b640-48f96df16 4d1.py", line 13, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc46fcdf-c2d7-4619-b640-48f96df16 4d1.py", line 8, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,3 +1,7 @@
+```
 def sort_third(l: list):
-    l.sort(key=lambda x: '000' if isinstance(x, bool) else ('0000' if not isinstance(x, int) else str(x).zfill(3)))
+    l.sort(key=lambda x: '000' if isinstance(x, bool) else str(x).zfill(3) if isinstance(x, int) else x)
     return l
+
+my_list = [4, 7, True, 2, False, 10]
+print(sort_third(my_list))
