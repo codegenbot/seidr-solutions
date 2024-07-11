c42041f713c5155e1d3860123e772e249ecfc0b9
beep boop <githubbot@vadim.me>
2024-07-11 03:52:25 +0200

    Solution 18 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/497575qq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,28 +1,37 @@
 #include <boost/any.hpp>
+#include <string>
+#include <algorithm>
+
+using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return boost::any_cast<double>(b);
-    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return boost::any_cast<double>(a);
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = boost::any_cast<string>(a);
-        string str2 = boost::any_cast<string>(b);
-        if (str1 > str2) return a;
-        else if (str1 < str2) return b;
-        else return boost::any("None");
-    } else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        string str = boost::any_cast<string>(a);
-        double num = boost::any_cast<double>(b);
-        if (stod(str) > num) return a;
-        else if (stod(str) < num) return b;
-        else return boost::any("None");
-    } else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        double num = boost::any_cast<double>(a);
-        string str = boost::any_cast<string>(b);
-        if (num > stod(str)) return a;
-        else if (num < stod(str)) return b;
-        else return boost::any("None");
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
     }
-    return boost::any();
+    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        string str = (string)b.convert_to<string>();
+        size_t pos = str.find(',');
+        if (pos != string::npos) {
+            float f1 = stof(str.substr(0, pos));
+            float f2 = stof(str.substr(pos + 1));
+            return max(f1, f2);
+        }
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = (string)a.convert_to<string>();
+        size_t pos1 = str1.find(',');
+        string str2 = (string)b.convert_to<string>();
+        size_t pos2 = str2.find(',');
+
+        if (pos1 != string::npos && pos2 != string::npos) {
+            float f1 = stof(str1.substr(0, pos1));
+            float f2 = stof(str2.substr(0, pos2));
+            return max(f1, f2);
+        }
+        else if (pos1 == string::npos && pos2 == string::npos) {
+            return "None";
+        }
+    }
+
+    return b;
 }
