e35cb9337f553ffc00e315ded9ddbc30570417ef
beep boop <githubbot@vadim.me>
2024-07-11 08:38:17 +0200

    Solution 2 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/3g7mbajf Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,29 +1,23 @@
 if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
-            return a;
-        } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
-            return b;
-        }
-    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
-            return a;
-        } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
-            return b;
-        }
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string strA = boost::any_cast<string>(a);
-        string strB = boost::any_cast<string>(b);
-        if (strA.find_first_of(".,") != string::npos) {
-            replace(strA.begin(), strA.end(), ',', '.');
-        }
-        if (strB.find_first_of(".,") != string::npos) {
-            replace(strB.begin(), strB.end(), ',', '.');
-        }
-        if (stof(strA) > stof(strB)) {
-            return a;
-        } else if (stof(strA) < stof(strB)) {
-            return b;
-        }
+    if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
+        return a;
+    } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
+        return b;
     }
-    return "None";
+} else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+    if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
+        return a;
+    } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
+        return b;
+    }
+} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+    float val1 = stof(boost::any_cast<string>(a));
+    float val2 = stof(boost::any_cast<string>(b));
+    if (val1 > val2) {
+        return a;
+    } else if (val1 < val2) {
+        return b;
+    }
+}
+return "None";
 }
