c2a5cfb2dbb7609e1016cb8e22f1d41d6026772c
beep boop <githubbot@vadim.me>
2024-07-11 22:14:16 +0200

    Solution 83 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/27myevuf Prompt:    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ dd250512-f472-45c3-8651-fe5e3bffb10f.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from dd250512-f472-45c3-8651-fe5e3bffb10f.cpp:20: dd250512-f472-45c3-8651-fe5e3bffb10f.cpp: In function ‘int main()’: dd250512-f472-45c3-8651-fe5e3bffb10f.cpp:30:20: error: ‘remove_duplicates’ was n ot declared in this scope; did you mean ‘std::remove_duplicates’?    30 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ dd250512-f472-45c3-8651-fe5e3bffb10f.cpp:7:22: note: ‘std::remove_duplicates’ de clared here     7 |     std::vector<int> remove_duplicates(const std::vector<int>& vec) {       |                      ^~~~~~~~~~~~~~~~~ In file included from dd250512-f472-45c3-8651-fe5e3bffb10f.cpp:20: dd250512-f472-45c3-8651-fe5e3bffb10f.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -19,6 +19,6 @@ namespace std {
 }
 
 int main() {
-    assert(std::issame(std::remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5}));
+    assert(std::issame(std::remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5})); // Add more test cases if needed
     return 0;
 }
