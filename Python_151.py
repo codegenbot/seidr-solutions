6d203324e74fe2ed6661a1bfd036e8115576e569
beep boop <githubbot@vadim.me>
2024-07-11 16:14:35 +0200

    Solution 87 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dmxi7cce Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dfe14e71-6c19-4d4a-848d-3bf45feda 6cb.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,13 +1,17 @@
 ```
-print(
-    "Enter elements separated by comma: "
-)
+def double_the_difference(lst):
+    if len(lst) != 2:
+        return "Please enter exactly two numbers!"
+    try:
+        num1, num2 = map(int, lst)
+        return abs(num1 - num2) * 2
+    except ValueError:
+        return "Invalid input. Please enter exact two numeric values!"
+
+print("Enter elements separated by space: ")
 user_input = input()
 try:
-    lst = [int(x.strip()) for x in user_input.split(',')]
-    if len(lst) != 2:
-        print("Please enter exactly two numbers!")
-    else:
-        print(double_the_difference(lst))
+    lst = [x for x in user_input.split() if x.strip()]
+    print(double_the_difference(lst))
 except ValueError:
-    print("Invalid input. Please enter exact two numeric values!")
+    print("Invalid input. Please enter exactly two numbers!")
