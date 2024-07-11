7bc60fabda74b5ea3ea1efffb91180c4cd8d37fb
beep boop <githubbot@vadim.me>
2024-07-11 02:30:13 +0200

    Solution 38 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jafm6m1z Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const float&}; _Tp = float; _Alloc = std::allocator<float>; std::v ector<_Tp, _Alloc>::iterator = std::vector<float>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = float; _Alloc = std::all ocator<float>; std::vector<_Tp, _Alloc>::value_type = float]’ 10960587-79ee-49c5-aaab-5620fe6de731.cpp:37:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <string>
 
 long long double_the_difference(std::vector<float> lst) {
     long long sum = 0;
     for (float num : lst) {
-        if (num > 0 && std::frexp(num, &num) == 0.0) {
+        if (num > 0 && modf(num, &num) == 0.0) {
             sum += pow(num, 2);
         }
     }
@@ -19,20 +20,23 @@ int main() {
     
     cout << "Enter numbers (enter 'stop' to finish):" << endl;
     while(1) {
-        float num;
-        
-        cin >> num;
-        
-        if(std::to_string(num) == "stop") break;
+        string str;
 
-        if(num > 0 && std::frexp(num, &num) == 0.0) {
+        cin >> str; 
+
+        if(str == "stop") break;
+
+        float num = stof(str); 
+
+        if(num > 0 && modf(num, &num) == 0.0) {
             odd_sum = 0; 
             for (float n : lst) { 
-                if (n > 0 && std::frexp(n, &n) == 0.0) {
-                    odd_sum = double_the_difference(lst);
+                if (n > 0 && modf(n, &n) == 0.0) {
+                    odd_sum += double_the_difference(lst);
                 }
             }
         }
+
         lst.push_back(num);    
     }
 
