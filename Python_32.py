a6a66af592f6067f4414d4fdbd0d143dc4d5af54
beep boop <githubbot@vadim.me>
2024-07-11 05:14:54 +0200

    Solution 15 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/m9pke3ue Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,5 +1,5 @@
 def find_zero(xs: list):
-    if len(xs) % 2 != 0:
-        raise ValueError("xs must have even number of coefficients")
-    largest_nonzero_coeff = max(coeff for coeff in xs if abs(coeff) > 1e-9)
-    return -largest_nonzero_coeff / xs[-1]
+    n = len(xs)
+    assert n % 2 == 0, "find_zero only takes list xs having even number of coefficients"
+    m = math.sqrt(4 * xs[1] ** 2 - 4 * xs[0] * xs[-1])
+    return (-xs[1] + m) / (2 * xs[0])
