5c7ad2aedbcad92743be67d7179c074c9bb32143
beep boop <githubbot@vadim.me>
2024-07-11 15:51:31 +0200

    Solution 21 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jpsqttvi Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from a097acfb-260b-411e-aec6-09efeda8545e.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,9 +1,8 @@
 #include <algorithm>
 #include <vector>
 
-int findMaxPair(const std::vector<std::pair<int, int>>& p) {
-    auto max_pair = *std::max_element(p.begin(), p.end(), [](const auto& a, const auto& b) {
-        return a.second < b.second;
-    });
-    return max_pair.first;
-}
+std::vector<std::pair<int, int>> p = {{5, 3}, {-5, 2}, {-3, 3}, {9, 0}, {124, 1}, {-10, 12}};
+
+auto max_pair = *std::max_element(p.begin(), p.end(), [](const auto& a, const auto& b) {
+    return a.second < b.second;
+});
