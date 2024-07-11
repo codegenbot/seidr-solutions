6ea7cd5e39dc22390cd6fb471fb834e67de69a99
beep boop <githubbot@vadim.me>
2024-07-11 14:12:16 +0200

    Solution 39 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/tdfielxa Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c7125991-d991-4f2f-9143-0c8b70f0a f65.py", line 14, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c7125991-d991-4f2f-9143-0c8b70f0a f65.py", line 11, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,21 +1,13 @@
-```Python
 def make_palindrome(s: str) -> str:
     if s == s[::-1]:
         return s + s
     
     result = list(s)
     
-    left, right = 0, len(result) - 1
-    while left < right:
-        result.insert(right, result.pop(left))
-        left += 1
-        right -= 1
+    mid = len(result) // 2
     
-    if not is_palindrome(''.join(result)):
-        to_add = ''.join(result)[::-1]
-        return ''.join(result) + to_add
-
-def is_palindrome(s: str) -> bool:
-    return s == s[::-1]
-
-print(make_palindrome("jerry"))
+    for i in range(len(result) - 1, mid - 1, -1):
+        if result[0] != result[i]:
+            result.insert(0, result.pop(i))
+    
+    return ''.join(result)
