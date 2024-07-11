56de839ca2ff726eee563d23d441cf60c6e3c00d
beep boop <githubbot@vadim.me>
2024-07-11 19:07:36 +0200

    Solution 12 of Python/69, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/t3ngfygd Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c80fece3-020c-4c26-bbcf-3c4399aea 4ac.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_69.py
+++ Python_69.py
@@ -1,14 +1,13 @@
-```
 def search(lst):
-    frequency_dict = {}
+    freq_dict = {}
     for num in lst:
-        if num > 0:
-            if num in frequency_dict:
-                frequency_dict[num] += 1
-            else:
-                frequency_dict[num] = 1
-    max_num = -1
-    for key, value in frequency_dict.items():
-        if key >= value and key > 0 and key > max_num:
-            max_num = key
-    return max_num
+        if num in freq_dict:
+            freq_dict[num] += 1
+        else:
+            freq_dict[num] = 1
+    max_freq = -1
+    for key, value in freq_dict.items():
+        if key > 0 and value >= key:
+            max_freq = key
+            break
+    return max_freq
