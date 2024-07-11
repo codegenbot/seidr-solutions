a0ec74e838476d7b2a78c2902e1b1810a57dc8e3
beep boop <githubbot@vadim.me>
2024-07-11 09:39:47 +0200

    Solution 43 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/27b19334-e398-4f26-b34f-5c84704fd b27.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/27b19334-e398-4f26-b34f-5c84704fd b27.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,6 +1,5 @@
-```Python
 def sort_third(l: list):
-    even = sorted([x for i, x in enumerate(l) if (i+1) % 3 == 0])
-    mid = sorted([x for i, x in enumerate(l) if (i+1) % 3 != 0])
+    even = [(x, i+1) for i, x in enumerate(l) if (i+1) % 3 == 0]
+    mid = [(x, i+1) for i, x in enumerate(l) if (i+1) % 3 != 0]
     
-    return even + mid
+    return sorted([x for _, x in even + mid])
