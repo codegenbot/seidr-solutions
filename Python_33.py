561737b87f826752678867c014909c3fb768eacd
beep boop <githubbot@vadim.me>
2024-07-11 09:28:02 +0200

    Solution 33 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/566e3913-f3bf-4633-a7ba-83c9a9509 02d.py", line 20, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/566e3913-f3bf-4633-a7ba-83c9a9509 02d.py", line 15, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,14 +1,11 @@
-```
 def sort_third(l: list):
     odd = [x for j, x in enumerate(reversed(l)) if (j+1) % 3 != 0]
     even = [x for j, x in enumerate(reversed(l)) if (j+1) % 3 == 0]
     
     result = []
     while len(odd) > 0 and len(even) > 0:
-        odd.sort(reverse=True)
-        even.sort(reverse=True)
-        result.append(odd.pop(0))
-        result.append(even.pop(0))
+        result.append(even.pop())
+        result.append(odd.pop())
     result += odd
     result += list(reversed(even))
     return result
