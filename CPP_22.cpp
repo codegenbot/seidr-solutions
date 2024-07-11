055be3e6dbccd6f457d5e3c9dcb95215850f1595
beep boop <githubbot@vadim.me>
2024-07-11 04:59:44 +0200

    Solution 25 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2dqf0nts Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:11: 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp: In function ‘int main()’: 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -2,19 +2,17 @@
 #include <list>
 #include <boost/any.hpp>
 
-bool issame(std::vector<int> a,std::vector<int>b){
+bool issame(vector<int> a,vector<int>b){
     return a==b;
 }
 
-std::vector<boost::any> inputs = {3, 'c', 3, 3, 'a', 'b' };
-std::vector<boost::any> result = filter_integers(inputs);
+vector< boost::any > inputs = {3, 'c', 3, 3, 'a', 'b' };
+vector<int> result = filter_integers(inputs);
 
-std::vector<boost::any> filter_integers(std::list<boost::any> values) {
-    std::vector<boost::any> result;
-    for (const auto& value : values) {
-        if (boost::any_cast<int>(value).type() == typeid(int)) {
-            result.push_back(value);
-        }
+vector<boost::any> filter_integers(list<any> values) {
+    vector<int> result;
+    for (const auto& value : values | views::filter([](auto&& val){ return boost::holds_alternative<int>(val); })) {
+        result.push_back(boost::get<int>(value));
     }
     return result;
 }
