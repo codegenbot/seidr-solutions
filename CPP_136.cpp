0cd1181861f3dced336a63b6600ee57a473718a1
beep boop <githubbot@vadim.me>
2024-07-11 11:10:30 +0200

    Solution 86 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/4tzr7z0i Prompt: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from b659acfc-a733-4553-b18e-c0e415304ba4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b659acfc-a733-4553-b18e-c0e415304ba4.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b659acfc-a733-4553-b18e-c0e415304ba4.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b659acfc-a733-4553-b18e-c0e415304ba4.cpp:24: b659acfc-a733-4553-b18e-c0e415304ba4.cpp: In function ‘int main()’: b659acfc-a733-4553-b18e-c0e415304ba4.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -2,29 +2,36 @@
 #include <climits>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) {
-        return false;
+bool issame(std::vector<int> a, std::vector<int> b){
+    if(a.size() != b.size()) return false;
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]) return false;
     }
-    
-    for(int i = 0; i < a.size(); ++i) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    
     return true;
 }
 
+std::vector<int> largest_smallest_integers(std::vector<int> lst){
+    std::vector<int> result = {INT_MIN, INT_MAX};
+    int largest_negative = INT_MIN;
+    int smallest_positive = INT_MAX;
+    
+    for (int num : lst) {
+        if (num < 0 && num > largest_negative) {
+            largest_negative = num;
+        }
+        if (num > 0 && (num < smallest_positive)) {
+            smallest_positive = num;
+        }
+    }
+    
+    result[0] = largest_negative;
+    result[1] = smallest_positive;
+    
+    return result;
+}
+
 int main() {
-    // Test issame function
-    std::vector<int> test1 = {1, 2, 3};
-    std::vector<int> test2 = {1, 2, 3};
-    assert(issame(test1, test2) == true);
-    
-    std::vector<int> test3 = {4, 5, 6};
-    std::vector<int> test4 = {4, 5};
-    assert(issame(test3, test4) == false);
-    
-    return 0;
+    // Add your test cases here
+    assert(issame(largest_smallest_integers({-10, 5, 8, -3, 0}), {-3, 5}));
+    assert(issame(largest_smallest_integers({-1, -3, -5, -8}), {-1, 0}));
 }
