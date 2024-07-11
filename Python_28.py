eedf21f0c980f5e951701087ede340a3c9ad4006
beep boop <githubbot@vadim.me>
2024-07-11 14:16:16 +0200

    Solution 26 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/p8cwgqr5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/57b5c703-16b9-43b0-8e6f-be5d2f564 b47.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -1,6 +1,7 @@
 from typing import List
 
-def concatenate() -> str:
-    strings = input("Enter a series of strings (separated by space): ")
-    strings_list: List[str] = [s.strip() for s in strings.split()]
-    return ''.join(s for s in strings_list if s)
+
+def concatenate(strings: List[str]) -> str:
+    if not strings:
+        return ""
+    return "".join(s for s in strings if s)
