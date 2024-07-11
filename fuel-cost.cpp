39ef8117feb8e9dac004758d20af2cc708f9d5ee
beep boop <githubbot@vadim.me>
2024-07-11 09:17:50 +0200

    Solution 1 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/ib6oidjq Prompt:     4 |     while (cin >> n) {       |            ^~~ 5128340b-996f-4ac7-b53d-c1e0b93726e0.cpp:5:9: error: ‘nums’ was not declared in this scope     5 |         nums.push_back(n);       |         ^~~~ 5128340b-996f-4ac7-b53d-c1e0b93726e0.cpp:9:20: error: ‘nums’ was not declared in  this scope; did you mean ‘num’?     9 |     for (int num : nums) {       |                    ^~~~       |                    num 5128340b-996f-4ac7-b53d-c1e0b93726e0.cpp:10:16: error: ‘max’ was not declared in  this scope    10 |         sum += max(num / 3 - 2, 0);       |                ^~~ 5128340b-996f-4ac7-b53d-c1e0b93726e0.cpp:13:5: error: ‘cout’ was not declared in  this scope    13 |     cout << sum << endl;       |     ^~~~ 5128340b-996f-4ac7-b53d-c1e0b93726e0.cpp:13:20: error: ‘endl’ was not declared i n this scope    13 |     cout << sum << endl;       |                    ^~~~

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -1,16 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 int main() {
     vector<int> nums;
     int n;
     while (cin >> n) {
         nums.push_back(n);
     }
-    
+
     int sum = 0;
     for (int num : nums) {
-        sum += max(num / 3 - 2, 0);
+        sum += std::max(num / 3 - 2, 0);
     }
-    
-    cout << sum << endl;
-    
+
+    cout << sum << "\n";
+
     return 0;
 }
