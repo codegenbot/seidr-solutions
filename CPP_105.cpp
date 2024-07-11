a806f0f16bb8592cb18e0eb2f51022b2582c9ed7
beep boop <githubbot@vadim.me>
2024-07-11 10:58:19 +0200

    Solution 66 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xzmxc8nx Prompt:                  from a5201e39-3341-4229-bf94-35ea6fc56ae2.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from a5201e39-3341-4229-bf94-35ea6fc56ae2.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ a5201e39-3341-4229-bf94-35ea6fc56ae2.cpp:67:44: error: ‘b’ was not declared in t his scope    67 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from a5201e39-3341-4229-bf94-35ea6fc56ae2.cpp:66: a5201e39-3341-4229-bf94-35ea6fc56ae2.cpp: In function ‘int main()’: a5201e39-3341-4229-bf94-35ea6fc56ae2.cpp:76:20: error: ‘by_length’ was not decla red in this scope    76 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -7,7 +7,6 @@ bool issame(int a, int b) {
     if (a > 9 || b > 9) {
         return false;
     }
-    
     vector<string> result = by_length({a, b});
     for (int i = 0; i < min(result.size(), 2); i++) {
         cout << result[i] << " ";
@@ -17,48 +16,36 @@ bool issame(int a, int b) {
 }
 
 vector<string> by_length(vector<int> arr) {
-    vector<int> numbers;
+    vector<vector<string>> result;
     
     for (int num : arr) {
-        if (num >= 1 && num <= 9) {
-            numbers.push_back(num);
-        }
-    }
-    
-    sort(numbers.begin(), numbers.end());
-    
-    reverse(numbers.begin(), numbers.end());
-    
-    vector<string> result;
-    
-    for (int num : numbers) {
         switch (num) {
             case 1:
-                result.push_back("One");
+                result.push_back({"One"});
                 break;
             case 2:
-                result.push_back("Two");
+                result.push_back({"Two"});
                 break;
             case 3:
-                result.push_back("Three");
+                result.push_back({"Three"});
                 break;
             case 4:
-                result.push_back("Four");
+                result.push_back({"Four"});
                 break;
             case 5:
-                result.push_back("Five");
+                result.push_back({"Five"});
                 break;
             case 6:
-                result.push_back("Six");
+                result.push_back({"Six"});
                 break;
             case 7:
-                result.push_back("Seven");
+                result.push_back({"Seven"});
                 break;
             case 8:
-                result.push_back("Eight");
+                result.push_back({"Eight"});
                 break;
             case 9:
-                result.push_back("Nine");
+                result.push_back({"Nine"});
                 break;
         }
     }
