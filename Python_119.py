e80fdab3c671488ca83c3fd583e0bbe795de96a3
beep boop <githubbot@vadim.me>
2024-07-11 21:59:26 +0200

    Solution 68 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/bhx6s3yc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/56e2a231-c167-4e6e-bf1e-1257198e6 e29.py", line 1     def find_max_min(lst): IndentationError: unexpected indent

--- Python_119.py
+++ Python_119.py
@@ -1,16 +1,2 @@
-```
-def match_parens(lst):
-    stack = []
-    for s in lst:
-        for c in s:
-            if c == '(':
-                stack.append(c)
-            elif c == ')':
-                if len(stack) == 0:
-                    return 'No'
-                else:
-                    stack.pop()
-    if len(stack) == 0:
-        return 'Yes'
-    else:
-        return 'No'
+def find_max_min(lst):
+    return str(min(lst)) + ' and ' + str(max(lst))
