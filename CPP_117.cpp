e2199cc2cb84431b29623eebcf2d54a7a4b99a21
beep boop <githubbot@vadim.me>
2024-07-11 06:35:21 +0200

    Solution 24 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/nwllhm7a Prompt:    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8d3db480-1d75-475b-ab9b-f3149f6d65de.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8d3db480-1d75-475b-ab9b-f3149f6d65de.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8d3db480-1d75-475b-ab9b-f3149f6d65de.cpp:28: 8d3db480-1d75-475b-ab9b-f3149f6d65de.cpp: In function ‘int main()’: 8d3db480-1d75-475b-ab9b-f3149f6d65de.cpp:38:20: error: ‘select_words’ was not de clared in this scope    38 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 8d3db480-1d75-475b-ab9b-f3149f6d65de.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,18 +1,22 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
 vector<string> select_words(string s, int n);
 
 bool issame(vector<string> a, vector<string> b) {
-    return a == b;
+    if (a.size() != b.size())
+        return false;
+    
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
+            return false;
+    }
+    
+    return true;
 }
 
 vector<string> select_words(string s, int n) {
     vector<string> result;
     string word = "";
     int consonantCount = 0;
-
+    
     for (char c : s) {
         if (c == ' ') {
             if (consonantCount == n) {
@@ -27,12 +31,10 @@ vector<string> select_words(string s, int n) {
             word += c;
         }
     }
-
+    
     if (consonantCount == n) {
         result.push_back(word);
     }
-
+    
     return result;
 }
-
-assert(issame(select_words("a b c d e f", 1), vector<string>{"b", "c", "d", "f"}));
