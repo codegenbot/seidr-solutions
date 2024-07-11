3af00f604c8b10101f2f27efbfe901902c343a3a
beep boop <githubbot@vadim.me>
2024-07-11 09:03:16 +0200

    Solution 17 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/jwff9tsk Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 50221de9-9ccc-4c4f-ac38-5a9a52d1f472.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 50221de9-9ccc-4c4f-ac38-5a9a52d1f472.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 50221de9-9ccc-4c4f-ac38-5a9a52d1f472.cpp:5: 50221de9-9ccc-4c4f-ac38-5a9a52d1f472.cpp: In function ‘int main()’: 50221de9-9ccc-4c4f-ac38-5a9a52d1f472.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 50221de9-9ccc-4c4f-ac38-5a9a52d1f472.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,20 +1,13 @@
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 vector<int> minPath(vector<vector<int>> grid, int k) {
     int m = grid.size();
     int n = grid[0].size();
-    
-    vector<int> path;
-    
-    for (int i = 0; i < m * n - 1; ++i) {
-        path.push_back(grid[i / n][i % n]);
-        if (i > 0 && path[i] < path[i - 1])
-            grid[i / n][i % n] += path[i - 1] - path[i] + k;
+    vector<int> result;
+
+    for (int i = 0; i < k / m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            result.push_back(grid[i % m][j]);
+        }
     }
-    
-    path.push_back(grid[(m * n - 1) / n][(m * n - 1) % n]);
-    
-    return path;
+
+    return result;
 }
