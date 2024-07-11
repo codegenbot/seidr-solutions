c35e57fc1f012bfde5f327cab4e7ba192e181f77
beep boop <githubbot@vadim.me>
2024-07-11 04:56:56 +0200

    Solution 65 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/497575qq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -5,16 +5,43 @@
 using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_same_type<int>(a) && is_same_type<int>(b)) {
-        return max(a.get<int>(), b.get<int>());
-    } else if (is_same_type<float>(a) && is_same_type<float>(b)) {
-        return max(a.get<float>(), b.get<float>());
-    } else if (is_same_type<string>(a) && is_same_type<string>(b)) {
-        return a.get<string>() > b.get<string>() ? a : b;
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        int x = boost::any_cast<int>(a);
+        int y = boost::any_cast<int>(b);
+        if (x > y) return a;
+        else if (x < y) return b;
+        else return boost::any("None");
+    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        float x = boost::any_cast<float>(a);
+        float y = boost::any_cast<float>(b);
+        if (x > y) return a;
+        else if (x < y) return b;
+        else return boost::any("None");
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string x = boost::any_cast<string>(a);
+        string y = boost::any_cast<string>(b);
+        if (x > y) return a;
+        else if (x < y) return b;
+        else return boost::any("None");
+    } else if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        int x = boost::any_cast<int>(a);
+        float y = boost::any_cast<float>(b);
+        if (x > y) return a;
+        else if (x < y) return b;
+        else return boost::any("None");
+    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        int x = boost::any_cast<int>(a);
+        string y = boost::any_cast<string>(b);
+        if (x > lexical_cast<float>(y)) return a;
+        else if (x < lexical_cast<float>(y)) return b;
+        else return boost::any("None");
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        float x = boost::any_cast<float>(a);
+        string y = boost::any_cast<string>(b);
+        if (x > lexical_cast<float>(y)) return a;
+        else if (x < lexical_cast<float>(y)) return b;
+        else return boost::any("None");
+    } else {
+        return "Invalid input";
     }
-    return boost::any("None");
-}
-
-bool is_same_type(const boost::any& x, const boost::any& y) {
-    return typeid(x.type()) == typeid(y.type());
 }
