9ec77807450a63fd5ea7efd580deb19482e79c5b
beep boop <githubbot@vadim.me>
2024-07-11 02:14:25 +0200

    Solution 28 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8kvpmmn6 Prompt:       |     ^~~~~~ 3c1f7b51-aa76-4d70-84c9-48b9c518b61b.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <algorithm>   +++ |+#include <cassert>     3 | 3c1f7b51-aa76-4d70-84c9-48b9c518b61b.cpp: At global scope: 3c1f7b51-aa76-4d70-84c9-48b9c518b61b.cpp:28:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    28 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 3c1f7b51-aa76-4d70-84c9-48b9c518b61b.cpp:12:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    12 | bool issame(std::vector<int> a, std::vector<int> b) {       |      ^~~~~~ 3c1f7b51-aa76-4d70-84c9-48b9c518b61b.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 3c1f7b51-aa76-4d70-84c9-48b9c518b61b.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -4,13 +4,30 @@
 
 using namespace std;
 
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
+            return false;
+    }
+    return true;
+}
+
 std::vector<int> compare(std::vector<int> a, std::vector<int> b) {
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
-    return {a.size(), b.size()};
+    vector<int> res(3);
+    res[0] = a.size();
+    res[1] = b.size();
+    if (!issame(a, b))
+        res[2] = 0;
+    else
+        res[2] = 1;
+    return res;
 }
 
 int main() {
-    assert(issame({1,2,3,5},{-1,2,3,4})=={2,0,0,1});
+    assert(issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));
     return 0;
 }
