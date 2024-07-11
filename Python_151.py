d33b374158a81fde6e9343d2cd9d361c3a61c0de
beep boop <githubbot@vadim.me>
2024-07-11 07:47:49 +0200

    Solution 26 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d6xmmywr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aa8fc1e1-6522-4403-8675-a5c41cc53 311.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aa8fc1e1-6522-4403-8675-a5c41cc53 311.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,8 +1,4 @@
-```
 def double_the_difference(lst):
-    if not isinstance(lst, list):
-        raise TypeError("Input should be a list")
-    for i in lst:
-        if not isinstance(i, int) or i < 0 or i % 2 == 0:
-            raise ValueError("List should only contain positive odd integers")
-    return sum(i**2 for i in lst)
+    return (
+        sum(i**2 for i in lst if isinstance(i, int) and i >= 0 and i % 2 != 0)
+    ) / ((sum(1 for i in lst if isinstance(i, int) and i >= 0 and i % 2 == 0)) or 1)
