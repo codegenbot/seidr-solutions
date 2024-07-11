da445168358121f0f61fa1700257567295cc7dec
beep boop <githubbot@vadim.me>
2024-07-11 17:59:24 +0200

    Solution 4 of Python/96, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7hdxr0i9 Prompt: Complete the following code given the task description and function signature.

--- Python_96.py
+++ Python_96.py
@@ -1,4 +1,3 @@
-```
 def count_up_to(n):
     def is_prime(num):
         if num < 2:
@@ -8,8 +7,8 @@ def count_up_to(n):
                 return False
         return True
 
-    primes = []
+    result = []
     for i in range(2, n):
         if is_prime(i):
-            primes.append(i)
-    return primes
+            result.append(i)
+    return result
