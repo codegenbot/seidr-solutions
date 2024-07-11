d3e405230de0167d18a80a12aed77cb40fe27349
beep boop <githubbot@vadim.me>
2024-07-11 22:21:46 +0200

    Solution 89 of CPP/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/si4cbgr8 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_53.cpp
+++ CPP_53.cpp
@@ -1,9 +1,7 @@
-```
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <string>
-#include <sstream>
 
 using namespace std;
 
@@ -16,8 +14,18 @@ int main() {
     cout << "Random Number: " << randomNum << endl; 
 
     int num1, num2;
+    string strInput;
     cout << "Enter two numbers: ";
-    cin >> num1 >> num2;
+    cin >> strInput;
+
+    int spaceIndex = strInput.find(' ');
+    if(spaceIndex != -1) {
+        num1 = stoi(strInput.substr(0, spaceIndex));
+        num2 = stoi(strInput.substr(spaceIndex + 1));
+    } else {
+        cout << "Please enter two numbers separated by a space." << endl;
+        return 1; // exit the program
+    }
 
     cout << "Sum of the numbers: " << add(num1, num2) << endl;
 }
