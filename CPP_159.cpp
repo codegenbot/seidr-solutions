8efba7c33d47100bb795c213a280e2b85b3ebf73
beep boop <githubbot@vadim.me>
2024-07-11 02:09:33 +0200

    Solution 12 of CPP/159, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9h6llu00 Prompt: Program failed

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 vector<int> eat(int number, int need, int remaining) {
+    if(need > remaining)
+        return vector<int>{}; 
     int total = number + need;
-    int left = min(remaining, need);
-    return {total, max(0, remaining - left)};
+    int left = remaining - min(remaining, need);
+    vector<int> result = {total, left};
+    return result;
 }
