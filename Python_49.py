75cf7415311d7bf45ef5fc41a7f79cd79a1aaf2e
beep boop <githubbot@vadim.me>
2024-07-11 21:35:21 +0200

    Solution 50 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/62ryo8im Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3c7f130-a966-4e2f-b6a4-4ee285547 8b8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,6 +1,10 @@
 ```
-def modp(n: int, p: int):
-    if math.gcd(n, p) != 1:
-        return None
-    else:
+import math
+
+def modp():
+    n = int(input("Enter a number (n): "))
+    p = int(input("Enter a prime number (p): "))
+    if math.gcd(n, p) == 1:
         return pow(n, p - 2, p) * n % p
+    else:
+        return None
