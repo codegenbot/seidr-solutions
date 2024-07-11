809e7ca1ff2479aae1e961387af00d0e04b87487
beep boop <githubbot@vadim.me>
2024-07-11 02:06:24 +0200

    Solution 6 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/19v1k3du Prompt: Complete the following code given the task description and function signature.

--- Python_12.py
+++ Python_12.py
@@ -3,9 +3,9 @@ def longest(strings: List[str]) -> Optional[str]:
     if not strings:
         return None
     max_length = 0
-    result = None
-    for s in strings:
-        if len(s) > max_length:
-            max_length = len(s)
-            result = s
+    result = strings[0]
+    for string in strings:
+        if len(string) > max_length:
+            max_length = len(string)
+            result = string
     return result
