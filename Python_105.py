5ad9aa9a929bbf465a055134a2cd3ee4dbae80f6
beep boop <githubbot@vadim.me>
2024-07-11 07:00:52 +0200

    Solution 3 of Python/105, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dmcspkb9 Prompt: Complete the following code given the task description and function signature.

--- Python_105.py
+++ Python_105.py
@@ -1,16 +1,14 @@
 def by_length(arr):
-    new_arr = sorted([str(i) for i in arr if 1 <= i <= 9])
-    return [
-        {
-            "One": 1,
-            "Two": 2,
-            "Three": 3,
-            "Four": 4,
-            "Five": 5,
-            "Six": 6,
-            "Seven": 7,
-            "Eight": 8,
-            "Nine": 9,
-        }[str(i)]
-        for i in reversed(new_arr)
-    ]
+    names = {
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine",
+    }
+    sorted_arr = sorted([x for x in arr if 1 <= x <= 9])
+    return [names[i] for i in reversed(sorted_arr)]
