ade15216353cd77da95f5e69a7084dcb40c8abd8
beep boop <githubbot@vadim.me>
2024-07-11 12:14:11 +0200

    Solution 50 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/df71cwu3 Prompt:       |     ^~~~~~ a0eda05e-9274-4904-8b70-17cf97241b6d.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | using namespace std; a0eda05e-9274-4904-8b70-17cf97241b6d.cpp: At global scope: a0eda05e-9274-4904-8b70-17cf97241b6d.cpp:32:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    32 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ a0eda05e-9274-4904-8b70-17cf97241b6d.cpp:4:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     4 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ a0eda05e-9274-4904-8b70-17cf97241b6d.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ a0eda05e-9274-4904-8b70-17cf97241b6d.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,12 +1,13 @@
+```cpp
 #include <vector>
 #include <cassert>
 
 using namespace std;
 
-bool issame(vector<int> a,vector<int>b){
-    if(a.size() != b.size()) return false;
-    for(int i=0; i<a.size();i++){
-        if(a[i] != b[i]) return false;
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
@@ -14,28 +15,29 @@ bool issame(vector<int> a,vector<int>b){
 vector<int> even_odd_count(int num) {
     vector<int> result;
     int count = 0;
-    while(num != 0) {
+    while (num != 0) {
         int digit = num % 10;
-        if(digit % 2 == 0)
+        if (digit % 2 == 0)
             count++;
         num /= 10;
     }
     result.push_back(count);
-    if(count > (abs(num)/10)) result.push_back(1);
-    else result.push_back(0);
+    result.push_back(abs(num));
     return result;
 }
 
 int main() {
-    assert(issame(even_odd_count(0), vector<int>{1,0}));
+    assert(issame(even_odd_count(0), vector<int>{1, 0}));
+    
     int num;
     cout << "Enter a number: ";
     cin >> num;
-    vector<int> res = even_odd_count(num);
-    cout << "Number of even digits: " << res[0] << endl;
-    if(res[1])
-        cout << "The number contains odd digits." << endl;
-    else
-        cout << "The number does not contain odd digits." << endl;
-    return 0;
+    
+    vector<int> result = even_odd_count(num);
+    if (issame(result, vector<int>{0, 0})) {
+        cout << "The given number is even.\n";
+    }
+    else {
+        cout << "The given number is odd.\n";
+    }
 }
