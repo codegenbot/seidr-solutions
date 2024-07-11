549408505111844bc0c1c055756b272de35e1ba4
beep boop <githubbot@vadim.me>
2024-07-11 10:53:23 +0200

    Solution 19 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ipbpzph6 Prompt: Program did not receive expected input

--- Python_84.py
+++ Python_84.py
@@ -1,7 +1,2 @@
 def convert_to_binary(N):
-    return "".join(str((N // (10**i)) % 2) for i in reversed(range(len(str(N)))))
-
-
-if __name__ == "__main__":
-    N = int(input("Enter a number: "))
-    print(convert_to_binary(N))
+    return bin(N)[2:]
