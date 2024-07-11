91d027ffacf3271a24600c6b9a05d2000dbbe2ee
beep boop <githubbot@vadim.me>
2024-07-11 15:10:48 +0200

    Solution 63 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dmxi7cce Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c812d5e2-0782-49c3-a1b2-35af3b538 251.py", line 17, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c812d5e2-0782-49c3-a1b2-35af3b538 251.py", line 13, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,11 +1,8 @@
 def double_the_difference():
-    while True:
-        print("Enter elements separated by comma: ")
-        user_input = input()
-        if len(user_input.split(',')) == 2:
-            a, b = map(int, user_input.split(','))
-            return abs(a - b) * 2
-        else:
-            print("Please enter exactly two numbers!")
-
-print(double_the_difference())
+    print("Enter elements separated by comma: ")
+    user_input = input()
+    if len(user_input.split(',')) == 2:
+        a, b = map(int, user_input.split(','))
+        return abs(a - b) * 2
+    else:
+        print("Please enter exactly two numbers!")
