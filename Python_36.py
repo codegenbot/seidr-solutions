57094127812ad85b0c1295bd0526b74666cc5d86
beep boop <githubbot@vadim.me>
2024-07-11 10:48:45 +0200

    Solution 38 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/oyiygbr0 Prompt: Program did not receive expected input

--- Python_36.py
+++ Python_36.py
@@ -1,13 +1,11 @@
-```
 def fizz_buzz(n: int):
     for i in range(1, n+1):
+        output = ""
         if i % 3 == 0:
-            output = "Fizz"
-        else: output = str(i)
-
+            output += "Fizz"
         if i % 5 == 0:
             output += "Buzz"
-
+        output = output or str(i)
         print(output)
 
 fizz_buzz(int(input("Enter a number: ")))
