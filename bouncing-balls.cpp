7d86ff5d8d7d1dbf38bd0825e9098b6e50ea9fec
beep boop <githubbot@vadim.me>
2024-07-11 14:18:29 +0200

    Solution 88 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/vu2op4l7 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,16 +1,30 @@
-```cpp
 #include <iostream>
 #include <cmath>
 
 int main() {
-    double startHeight, heightAfterFirstBounce;
-    std::cin >> startHeight >> ' ' >> heightAfterFirstBounce;
+    double startHeight = 0;
+    double heightAfterFirstBounce = 0;
+
+    std::cin >> startHeight >> heightAfterFirstBounce;
+
+    if(std::cin.fail()) {
+        std::cout << "Invalid input. Please enter numbers for the starting height and first bounce height." << std::endl;
+        return 1; // Exit with error code
+    }
+
     double bouncinessIndex = pow(heightAfterFirstBounce / startHeight, 0.5);
+
     int numBounces;
     std::cin >> numBounces;
-    double totalDistance = 0;
-    for(int i=1; i<=numBounces; i++) {
-        totalDistance += heightAfterFirstBounce * (2**i) * bouncinessIndex;
+
+    if(std::cin.fail()) {
+        std::cout << "Invalid input. Please enter a number for the number of bounces." << std::endl;
+        return 1; // Exit with error code
     }
-    std::cout << totalDistance;
+
+    double totalDistance = (startHeight + heightAfterFirstBounce) * pow(2, numBounces - 1);
+
+    std::cout << "The total distance is: " << totalDistance << std::endl;
+
+    return 0;
 }
