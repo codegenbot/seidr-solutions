8f2da5a061e9b90fd0018f9405ed5622a4d319de
beep boop <githubbot@vadim.me>
2024-07-11 10:20:55 +0200

    Solution 19 of Python/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mqn4uw0k Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1ce14fad-98b7-49b5-97af-87af9c7a8 31b.py", line 12, in <module>     check(fibfib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1ce14fad-98b7-49b5-97af-87af9c7a8 31b.py", line 9, in check     assert fibfib(14) == 927            ^^^^^^^^^^^^^^^^^ AssertionError

--- Python_63.py
+++ Python_63.py
@@ -1,10 +1,7 @@
 def fibfib(n: int):
     if n <= 1:
-        return 0
-    elif n == 2:
-        return 1
-    else:
-        a, b = 0, 1
-        for _ in range(3, n+1):
-            a, b = b, a+b
-        return b
+        return n
+    sqrt_5 = (1 + 5 ** 0.5) / 2
+    phi = (1 + sqrt_5) / 2
+    psi = (1 - sqrt_5) / 2
+    return int((phi ** (n + 1) - psi ** (n + 1)) / sqrt_5)
