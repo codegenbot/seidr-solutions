c0b85e103c0b69775d2ca506c9ce0d29aca2743a
beep boop <githubbot@vadim.me>
2024-07-11 02:59:54 +0200

    Solution 20 of CPP/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xsowexf3 Prompt: 5bfd2fc8-daba-44a5-ae04-56facb19da0a.cpp:17:14: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’? 5bfd2fc8-daba-44a5-ae04-56facb19da0a.cpp:17:66: error: ‘endl’ is not a member of  ‘std’    17 |         std::cout << "The triangle is not right-angled." << std::endl;       |                                                                  ^~~~ 5bfd2fc8-daba-44a5-ae04-56facb19da0a.cpp:17:66: note: ‘std::endl’ is defined in header ‘<ostream>’; did you forget to ‘#include <ostream>’? 5bfd2fc8-daba-44a5-ae04-56facb19da0a.cpp: At global scope: 5bfd2fc8-daba-44a5-ae04-56facb19da0a.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 5bfd2fc8-daba-44a5-ae04-56facb19da0a.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~ In file included from 5bfd2fc8-daba-44a5-ae04-56facb19da0a.cpp:20: 5bfd2fc8-daba-44a5-ae04-56facb19da0a.cpp: In function ‘int main()’: 5bfd2fc8-daba-44a5-ae04-56facb19da0a.cpp:22:13: error: ‘right_angle_triangle’ wa s not declared in this scope    22 |     assert (right_angle_triangle(2, 2, 10) == false);       |             ^~~~~~~~~~~~~~~~~~~~

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std;
+#include <cassert>
 
 bool right_angle_triangle(float a, float b, float c) {
     if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
@@ -10,11 +10,13 @@ bool right_angle_triangle(float a, float b, float c) {
 
 int main() {
     assert(right_angle_triangle(2, 2, 10) == false);
-    cout << "Input the lengths of three sides (a, b, c): ";
+
+    std::cout << "Input the lengths of three sides (a, b, c): ";
     float a, b, c;
-    cin >> a >> b >> c;
+    std::cin >> a >> b >> c;
+
     if (right_angle_triangle(a, b, c))
-        cout << "The triangle is right-angled." << endl;
+        std::cout << "The triangle is right-angled." << std::endl;
     else
-        cout << "The triangle is not right-angled." << endl;
+        std::cout << "The triangle is not right-angled." << std::endl;
 }
