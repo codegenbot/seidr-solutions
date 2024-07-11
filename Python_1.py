dcad2439412d2d8685b0af6b29b46e1cb0e523cc
beep boop <githubbot@vadim.me>
2024-07-11 16:29:04 +0200

    Solution 84 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/837wq9ey Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c4d43637-c0d0-47a5-a917-7aef39c3b bc0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_1.py
+++ Python_1.py
@@ -1,20 +1,20 @@
 ```
-def separate_paren_groups(paren_string: str) -> List[str]:
-    def check(paren_string: str):
-        output = []
-        current_group = ""
-        for char in paren_string:
-            if char == "(":
-                if current_group:
-                    output.append(current_group)
-                    current_group = ""
-                current_group += "("
-            elif char == ")":
-                current_group += ")"
-            else:
-                current_group += char
-        if current_group:
-            output.append(current_group)
-        return output
+from typing import List
 
-    return check(paren_string)
+
+def separate_paren_groups(paren_string: str) -> List[str]:
+    output = []
+    current_group = ""
+    for char in paren_string:
+        if char == "(":
+            if current_group:
+                output.append(current_group)
+                current_group = ""
+            current_group += "("
+        elif char == ")":
+            current_group += ")"
+        else:
+            current_group += char
+    if current_group:
+        output.append(current_group)
+    return output
