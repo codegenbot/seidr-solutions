bb6da9d6596a6e5ea6cdc529b142729dc69d31c9
beep boop <githubbot@vadim.me>
2024-07-11 02:07:31 +0200

    Solution 4 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fcqijk62 Prompt: In file included from 75d6414a-b740-4602-b9cb-b0d162a6249e.cpp:3: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ 75d6414a-b740-4602-b9cb-b0d162a6249e.cpp:22:54: error: invalid conversion from ‘ const char*’ to ‘char’ [-fpermissive]    22 |         result.push_back(to_string((int)md5[i]).c_str());       |                          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~       |                                                      |       |                                                      const char* In file included from /usr/include/c++/11/string:55,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 75d6414a-b740-4602-b9cb-b0d162a6249e.cpp:1: /usr/include/c++/11/bits/basic_string.h:1341:24: note:   initializing argument 1  of ‘void std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::push_back(_CharT)  [with _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator< char>]’  1341 |       push_back(_CharT __c)       |                 ~~~~~~~^~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,3 +1,4 @@
-```
+string result;
 for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)
-    result.push_back(((unsigned char)md5[i]) * 2 + ((md5[i] * 4) > 9) ? ((unsigned char)md5[i]) * 2 + ((md5[i] * 4) > 9) : "0");
+    result.push_back((char)(md5[i]));
+return result;
