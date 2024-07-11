b74ad5730ff0c637c1fc3e2b9bb4487a64849427
beep boop <githubbot@vadim.me>
2024-07-11 16:31:13 +0200

    Solution 17 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tk67a2mb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/64a4e69f-c390-4d71-acfb-c04c9bf50 53f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_131.py
+++ Python_131.py
@@ -1,2 +1,8 @@
-def sum_of_digits(n):
-    return sum(int(digit) for digit in str(n))
+def product_of_digits(n):
+    product = 1
+    while n > 0:
+        digit = n % 10
+        if digit != 0:
+            product *= digit
+        n //= 10
+    return product
