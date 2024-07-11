945da851c928f3d0c9b2f6e83c121ce29cac9feb
beep boop <githubbot@vadim.me>
2024-07-12 01:21:57 +0200

    Solution 10 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ykczpfik Prompt: e06ae96f-be6d-43ac-8b23-d4029aae62f3.cpp:1:31: error: ‘vector’ is not a member o f ‘std’     1 | int maximumSpecialNumber(std::vector<int> lst) {       |                               ^~~~~~ e06ae96f-be6d-43ac-8b23-d4029aae62f3.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | int maximumSpecialNumber(std::vector<int> lst) { e06ae96f-be6d-43ac-8b23-d4029aae62f3.cpp:1:38: error: expected primary-expressio n before ‘int’     1 | int maximumSpecialNumber(std::vector<int> lst) {       |                                      ^~~ In file included from e06ae96f-be6d-43ac-8b23-d4029aae62f3.cpp:17: e06ae96f-be6d-43ac-8b23-d4029aae62f3.cpp: In function ‘int main()’: e06ae96f-be6d-43ac-8b23-d4029aae62f3.cpp:19:13: error: ‘search’ was not declared  in this scope    19 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ^~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,24 +1,17 @@
-#include <iostream>
 #include <vector>
-#include <cassert>
 
-int search(const std::vector<int>& nums) {
-    int maxFreq = -1;
-    for (int num : nums) {
+int maximumSpecialNumber(std::vector<int> lst) {
+    int res = -1;
+    for (int num : lst) {
         int freq = 0;
-        for (int i : nums) {
-            if (i == num) {
+        for (int n : lst) {
+            if (n == num) {
                 freq++;
             }
         }
-        if (num > freq && freq > maxFreq) {
-            maxFreq = freq;
+        if (num > 0 && freq >= num && num > res) {
+            res = num;
         }
     }
-    return maxFreq > 0 ? maxFreq : -1;
-}
-
-int main() {
-    assert(search({3, 10, 10, 9, 2}) == -1);
-    return 0;
+    return res;
 }
