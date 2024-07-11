ef1fa03e38f7e2ecddedd6875b3daab98febb391
beep boop <githubbot@vadim.me>
2024-07-11 16:58:47 +0200

    Solution 93 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/c24elj42 Prompt: 793ae5aa-a84f-46fd-a896-9fdce0e7aa5e.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 793ae5aa-a84f-46fd-a896-9fdce0e7aa5e.cpp:13: 793ae5aa-a84f-46fd-a896-9fdce0e7aa5e.cpp: In function ‘int main()’: 793ae5aa-a84f-46fd-a896-9fdce0e7aa5e.cpp:15:13: error: ‘solutions’ was not decla red in this scope    15 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int solutions(vector<int> lst) {
     int sum = 0;
-    for (int i = 0; i < lst.size(); i++) {
+    for (int i = 1; i < lst.size(); i++) {
         if (lst[i] % 2 != 0) {
             sum += lst[i];
         }
