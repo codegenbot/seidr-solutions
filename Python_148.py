1bbaac4f38d3f65fe1bf556ffd4ba7b1cf649883
beep boop <githubbot@vadim.me>
2024-07-11 03:28:48 +0200

    Solution 7 of Python/148, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/md0gi63k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f7b7d8cf-9345-4b78-a4a9-20c5f0aad 05d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_148.py
+++ Python_148.py
@@ -1,9 +1,17 @@
-```
 def bf(planet1, planet2):
-    planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+    planets = [
+        "Mercury",
+        "Venus",
+        "Earth",
+        "Mars",
+        "Jupiter",
+        "Saturn",
+        "Uranus",
+        "Neptune",
+    ]
     index1 = None
     index2 = None
-    
+
     for i in range(len(planets)):
         if planet1 == planets[i]:
             index1 = i
@@ -14,7 +22,7 @@ def bf(planet1, planet2):
         return ()
 
     result = []
-    for i in range(index1 + 1, index2):
+    for i in range(min(index1, index2), max(index1, index2)):
         result.append(planets[i])
 
     return tuple(result)
