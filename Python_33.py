88ff5d8cab2106925692b54a2497ed625b2a259b
beep boop <githubbot@vadim.me>
2024-07-11 09:21:36 +0200

    Solution 26 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c0abbb85-da03-467c-a544-4c9f348cd 411.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,14 +1,11 @@
 Here is the modified code:
 
-```python
 def sort_third(l: list):
-    odd = [x for j, x in enumerate(l) if (j+1) % 3 != 0]
-    even = [x for j, x in enumerate(l) if (j+1) % 3 == 0]
+    odd = sorted([x for j, x in enumerate(l) if (j+1) % 3 != 0], reverse=True)
+    even = sorted([x for j, x in enumerate(l) if (j+1) % 3 == 0], reverse=True)
     
     result = []
     while len(odd) > 0 and len(even) > 0:
-        odd.sort(reverse=True)
-        even.sort(reverse=True)
         result.append(odd.pop(0))
         result.append(even.pop(0))
     result += odd
