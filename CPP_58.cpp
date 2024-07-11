853bc598b14ebec8291e57655539081bc3f97a63
beep boop <githubbot@vadim.me>
2024-07-11 15:04:09 +0200

    Solution 61 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/8j04hxex Prompt:       |                    ^~~ 9e9afbc9-2b13-4988-88e8-346d469c2a5d.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9e9afbc9-2b13-4988-88e8-346d469c2a5d.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9e9afbc9-2b13-4988-88e8-346d469c2a5d.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9e9afbc9-2b13-4988-88e8-346d469c2a5d.cpp:9: 9e9afbc9-2b13-4988-88e8-346d469c2a5d.cpp: In function ‘int main()’: 9e9afbc9-2b13-4988-88e8-346d469c2a5d.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 9e9afbc9-2b13-4988-88e8-346d469c2a5d.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,12 +1,17 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
+vector<int> common(vector<int> l1, vector<int> l2) {
     sort(l1.begin(), l1.end());
     sort(l2.begin(), l2.end());
-    std::vector<int> result;
-    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
-    result.erase(std::unique(result.begin(), result.end()), result.end());
+    vector<int> result;
+    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
+    result.erase(unique(result.begin(), result.end()), result.end());
     return result;
 }
-common({4, 3, 2, 8}, {});
+
+bool issame(vector<int> a, vector<int> b) {
+    return (a == b);
+}
+
+vector<int> common({4, 3, 2, 8}, {});
