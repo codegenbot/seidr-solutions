8c52a0743e0765e53cf37c7255c34cdbe4f7a7fa
beep boop <githubbot@vadim.me>
2024-07-11 13:51:07 +0200

    Solution 19 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g0yxi6dq Prompt: Program did not receive expected input

--- Python_20.py
+++ Python_20.py
@@ -1,6 +1,7 @@
+```
 from typing import List, Tuple
 
-def find_closed_elements(numbers: List[float]) -> Tuple[float, float]:
+def find_closest_elements(numbers: List[float]) -> Tuple[float, float]:
     if len(numbers) <= 1:
         return ()
     
