5c21bdd5f050ca12980ea459a08d88c9cb7c6155
beep boop <githubbot@vadim.me>
2024-07-11 11:28:57 +0200

    Solution 16 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/630ydf47 Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 955531ef-7744-4eba-943f-715cc21fda7f.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 955531ef-7744-4eba-943f-715cc21fda7f.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 955531ef-7744-4eba-943f-715cc21fda7f.cpp:11: 955531ef-7744-4eba-943f-715cc21fda7f.cpp: In function ‘int main()’: 955531ef-7744-4eba-943f-715cc21fda7f.cpp:21:20: error: ‘rolling_max’ was not dec lared in this scope    21 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 955531ef-7744-4eba-943f-715cc21fda7f.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,12 +1,20 @@
-vector<int> rolling_max(vector<int> numbers) {
+vector<int> rolling_max(vector<int> numbers);
+
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
+
+vector<int> rolling_max(vector<int> numbers){
     vector<int> result;
-    int max_so_far = numbers[0];
-    result.push_back(max_so_far);
-
-    for (int i = 1; i < numbers.size(); i++) {
-        max_so_far = max(max_so_far, numbers[i]);
-        result.push_back(max_so_far);
+    int max_num = numbers[0];
+    for(int i=0; i<numbers.size(); i++){
+        max_num = max(max_num, numbers[i]);
+        result.push_back(max_num);
     }
-
     return result;
+}
+
+int main(){
+    assert (issame(rolling_max({3, 2, 3, 100, 3}), {3, 3, 3,  100, 100}));
+    return 0;
 }
