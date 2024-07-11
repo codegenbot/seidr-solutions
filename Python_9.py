a520f4631fb467cc6b569480826e12218898d9c8
beep boop <githubbot@vadim.me>
2024-07-12 00:06:03 +0200

    Solution 11 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/sq5l3lrl Prompt: Complete the following code given the task description and function signature.

--- Python_9.py
+++ Python_9.py
@@ -1,6 +1,6 @@
 def rolling_max(numbers: List[int]) -> List[int]:
     result = []
-    max_seen = numbers[0]
+    max_seen = float("-inf")
     for num in numbers:
         result.append(max(num, max_seen))
         max_seen = num
