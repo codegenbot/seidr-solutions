01b55c9804e4524637e58da70a25d0ce600c160e
beep boop <githubbot@vadim.me>
2024-07-11 20:35:11 +0200

    Solution 22 of Python/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/dd2ay5xt Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3d51378-1c35-409c-b313-821d8cbe1 5d0.py", line 5, in <module>     result = is_pythagorean_triplet(a, b, c)                                     ^ NameError: name 'a' is not defined

--- Python_157.py
+++ Python_157.py
@@ -1,9 +1,9 @@
+a = int(input("Enter the value for a: "))
+b = int(input("Enter the value for b: "))
+c = int(input("Enter the value for c: "))
+
 def is_pythagorean_triplet(a, b, c):
     return a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2
 
-
-a = int(input("Enter value for a: "))
-b = int(input("Enter value for b: "))
-c = int(input("Enter value for c: "))
-
 result = is_pythagorean_triplet(a, b, c)
+print(result)
