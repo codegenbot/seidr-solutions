1a34613b2e9ee656ee2205dcb5d89400e9b5a627
beep boop <githubbot@vadim.me>
2024-07-11 12:43:24 +0200

    Solution 25 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dmxi7cce Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6eb987ff-d518-4b41-9752-aac99a364 62a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -2,8 +2,8 @@
 def double_the_difference(lst):
     if len(lst) < 2:
         return None
+    lst = [int(i) for i in lst]
     lst.sort()
     return (lst[-1] - lst[0]) * 2
 
-input_list = list(map(int, input("Enter elements separated by space: ").split()))
-print(double_the_difference(input_list))
+print(double_the_difference(input("Enter elements separated by space: ").split()))
