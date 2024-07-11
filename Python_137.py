1b2e44bbb0bc9df267aa95520b407d2982c4e125
beep boop <githubbot@vadim.me>
2024-07-11 23:03:58 +0200

    Solution 1 of Python/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0mvq9flb Prompt: Complete the following code given the task description and function signature.

--- Python_137.py
+++ Python_137.py
@@ -1,14 +1,13 @@
 ```
 def compare_one(a, b):
     if isinstance(a, str) and isinstance(b, (int, float)):
-        a = str(a).replace('.', '').replace(',', '.').format('{:.0f}')
-        return max(a, str(b).replace('.', '').replace(',', '.')) if (a != '0' or b != '0') else None
+        a = str(a).replace('.', '').replace(',', '.').format('{0:.1f}')
     elif isinstance(b, str) and isinstance(a, (int, float)):
-        b = str(b).replace('.', '').replace(',', '.').format('{:.0f}')
-        return max(str(a).replace('.', '').replace(',', '.'), b) if (str(a) != '0' or b != '0') else None
-    elif isinstance(a, str) and isinstance(b, str):
-        a = float(str(a).replace('.', '').replace(',', '.'))
-        b = float(str(b).replace('.', '').replace(',','.'))
-        return max(a, b) if a != b else None
+        b = str(b).replace('.', '').replace(',', '.').format('{0:.1f}')
+
+    if a > b:
+        return a
+    elif a < b:
+        return b
     else:
-        return max(a, b) if a > b else None
+        return None
