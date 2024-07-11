00e501c8210220fda81254054e9d6bc3926f25ce
beep boop <githubbot@vadim.me>
2024-07-11 23:17:02 +0200

    Solution 9 of Python/137, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0mvq9flb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/174cdd6e-6376-4067-8a81-e42d7f63b 3d1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_137.py
+++ Python_137.py
@@ -1,14 +1,17 @@
-```
 def compare_one(a, b):
-    if isinstance(a, str) and isinstance(b, (int, float)):
-        a = str(a).replace('.', '').replace(',', '.').format('{:.0f}')
-        return max(a, str(b).replace('.', '').replace(',', '.')) if (a != '0' or b != '0') else None
-    elif isinstance(b, str) and isinstance(a, (int, float)):
-        b = str(b).replace('.', '').replace(',', '.').format('{:.0f}')
-        return max(str(a).replace('.', '').replace(',', '.'), b) if (str(a) != '0' or b != '0') else None
+    def to_num(s):
+        s = str(s).replace(".", "").replace(",", ".")
+        if s == "0":
+            return 0
+        return float(s)
+
+    a, b = map(to_num, (a, b))
+
+    if isinstance(a, int) and isinstance(b, str):
+        return max(a, to_num(b)) if a > 0 else None
+    elif isinstance(b, int) and isinstance(a, str):
+        return max(to_num(a), b) if b > 0 else None
     elif isinstance(a, str) and isinstance(b, str):
-        a = float(str(a).replace('.', '').replace(',', '.'))
-        b = float(str(b).replace('.', '').replace(',','.'))
-        return max(a, b) if a != b else None
+        return max(a, b) if to_num(a) != to_num(b) else None
     else:
-        return max(a, b) if a > b else None
+        return max(a, b)
