33c0a755cd3e97bb26e3db670fcfa17e3fcb96aa
beep boop <githubbot@vadim.me>
2024-07-11 03:06:13 +0200

    Solution 88 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/49frezuz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/abeb0304-04d4-494c-83f2-4b25a0486 42b.py", line 1     def """Complete the following code given the task description and function s ignature...         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,7 +1,6 @@
-def calculate_difference():
-    def double_the_difference(lst):
-        lst.sort()
-        return (lst[-1] - lst[0]) * 2
-
+```
+def double_the_difference(lst):
+    lst.sort()
+    return (lst[-1] - lst[0]) * 2
 
 print(double_the_difference([1, 2, 3]))
