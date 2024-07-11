d3fceac30077b1f499437a9afebc9c63e08022e0
beep boop <githubbot@vadim.me>
2024-07-11 06:17:07 +0200

    Solution 12 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/nwllhm7a Prompt:    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bc1ea8cd-af84-4889-973e-a32c9e06fcfa.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bc1ea8cd-af84-4889-973e-a32c9e06fcfa.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from bc1ea8cd-af84-4889-973e-a32c9e06fcfa.cpp:32: bc1ea8cd-af84-4889-973e-a32c9e06fcfa.cpp: In function ‘int main()’: bc1ea8cd-af84-4889-973e-a32c9e06fcfa.cpp:42:20: error: ‘select_words’ was not de clared in this scope    42 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ bc1ea8cd-af84-4889-973e-a32c9e06fcfa.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,31 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cassert>
-
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b){
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
 vector<string> select_words(string s, int n) {
     vector<string> result;
+    if (s.empty()) {
+        return result;
+    }
+
     string word = "";
     int consonantCount = 0;
 
     for (char c : s) {
-        if (c == ' ') {
+        if (c != ' ') {
+            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+                consonantCount++;
+            }
+            word += c;
+        } else {
             if (consonantCount == n) {
                 result.push_back(word);
             }
             word = "";
             consonantCount = 0;
-        } else if (isalpha(c)) {
-            if (tolower(c) != 'a' && tolower(c) != 'e' && tolower(c) != 'i' && tolower(c) != 'o' && tolower(c) != 'u') {
-                consonantCount++;
-            }
-            word += c;
         }
     }
 
