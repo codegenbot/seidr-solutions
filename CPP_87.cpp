633ed549d74415b5b7429ab5e60fc1635ef1b1f1
beep boop <githubbot@vadim.me>
2024-07-12 01:52:24 +0200

    Solution 4 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ozl7l1g0 Prompt:       |                                   ^~~~~~ 6490cabe-f8af-4ce5-b41d-b0c442497b1b.cpp:20:42: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 6490cabe-f8af-4ce5-b41d-b0c442497b1b.cpp:20:49: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 6490cabe-f8af-4ce5-b41d-b0c442497b1b.cpp:20:56: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 6490cabe-f8af-4ce5-b41d-b0c442497b1b.cpp:19: 6490cabe-f8af-4ce5-b41d-b0c442497b1b.cpp: In function ‘int main()’: 6490cabe-f8af-4ce5-b41d-b0c442497b1b.cpp:32:20: error: ‘get_row’ was not declare d in this scope    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 6490cabe-f8af-4ce5-b41d-b0c442497b1b.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
+bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b) {
     return a == b;
 }
 
@@ -22,9 +22,4 @@ std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
         return a[0] < b[0];
     });
     return result;
-}
-
-int main() {
-    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
-    return 0;
 }
