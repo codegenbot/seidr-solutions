fe87ca6bc257ed7fcd5c349c02ba319e8f5b3854
beep boop <githubbot@vadim.me>
2024-07-11 17:20:19 +0200

    Solution 2 of CPP/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/aaxxv26o Prompt: Complete the following code given the task description and function signature.

--- CPP_154.cpp
+++ CPP_154.cpp
@@ -1,4 +1,7 @@
-if(a.length() != b.length()) return false;
+int n = a.length();
+    int m = b.length();
+    if (n != m) return false;
+    
     string combined = a + a;
     return combined.find(b) != string::npos;
 }
