f72a1ae67b147618e361d6e7bb53af538bf20b32
beep boop <githubbot@vadim.me>
2024-07-11 20:45:00 +0200

    Solution 69 of CPP/53, ratio 0.16666666666666666 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/si4cbgr8 Prompt:    12 | srand(time(nullptr));       |      ^ 61101fa3-dd71-49da-a5f3-07180b3091ce.cpp:15:1: error: ‘cout’ does not name a typ e    15 | cout << "Random Number: " << to_string(randomNum) << endl;       | ^~~~ 61101fa3-dd71-49da-a5f3-07180b3091ce.cpp:18:1: error: ‘cout’ does not name a typ e    18 | cout << "Enter two numbers: ";       | ^~~~ 61101fa3-dd71-49da-a5f3-07180b3091ce.cpp:19:1: error: ‘cin’ does not name a type    19 | cin >> num1 >> num2;       | ^~~ 61101fa3-dd71-49da-a5f3-07180b3091ce.cpp:21:1: error: ‘cout’ does not name a typ e    21 | cout << "Sum of the numbers: " << sum(num1, num2) << endl;       | ^~~~ In file included from 61101fa3-dd71-49da-a5f3-07180b3091ce.cpp:23: 61101fa3-dd71-49da-a5f3-07180b3091ce.cpp: In function ‘int main()’: 61101fa3-dd71-49da-a5f3-07180b3091ce.cpp:29:17: error: ‘add’ was not declared in  this scope    29 |         assert (add(x, y) == x + y);       |                 ^~~

--- CPP_53.cpp
+++ CPP_53.cpp
@@ -6,11 +6,7 @@
 
 using namespace std;
 
-int sum(int a, int b) {
-    return a + b;
-}
-
-int mainInit() { 
+int main() {
     srand(time(nullptr)); 
     int randomNum = rand() % 1000; 
 
@@ -20,9 +16,9 @@ int mainInit() {
     cout << "Enter two numbers: ";
     cin >> num1 >> num2;
 
-    cout << "Sum of the numbers: " << sum(num1, num2) << endl; 
+    cout << "Sum of the numbers: " << sum(num1, num2) << endl;
 }
 
-int main() {
-    mainInit();
+int sum(int a, int b) {
+    return a + b;
 }
