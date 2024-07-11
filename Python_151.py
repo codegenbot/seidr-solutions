c15b6d4865f2c953798d146758c9c0a2e4b90871
beep boop <githubbot@vadim.me>
2024-07-11 02:25:16 +0200

    Solution 66 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/49frezuz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/67d64946-7fe2-40cc-a55b-11854adcf 2af.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,9 +1,7 @@
+def check(lst, func):
+    return func(lst)
+
 def double_the_difference():
-    """
-    This function calculates the double of the difference between the maximum and minimum numbers in a list.
-    It takes a list and returns its doubled difference.
-    :return: 
-    """
     def inner(lst):
         lst.sort()
         return (lst[-1] - lst[0]) * 2
