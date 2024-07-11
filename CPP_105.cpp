3949120b91133d514e9cd4a44bd0fc77f0d78981
beep boop <githubbot@vadim.me>
2024-07-11 10:18:10 +0200

    Solution 28 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xzmxc8nx Prompt:       |                              ^~~~~~ 061cb1f6-fa0f-4b24-b93b-85f03ca556e1.cpp:18:37: error: ‘string’ was not declared  in this scope    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 061cb1f6-fa0f-4b24-b93b-85f03ca556e1.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 061cb1f6-fa0f-4b24-b93b-85f03ca556e1.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 061cb1f6-fa0f-4b24-b93b-85f03ca556e1.cpp:17: 061cb1f6-fa0f-4b24-b93b-85f03ca556e1.cpp: In function ‘int main()’: 061cb1f6-fa0f-4b24-b93b-85f03ca556e1.cpp:27:20: error: ‘by_length’ was not decla red in this scope    27 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 061cb1f6-fa0f-4b24-b93b-85f03ca556e1.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,73 +1,29 @@
 #include <vector>
 #include <algorithm>
+using namespace std;
 
-bool issame(vector<string> vec1, vector<string> vec2) {
-    if (vec1.size() != vec2.size()) return false;
-    for (int i = 0; i < vec1.size(); ++i) {
-        if (vec1[i] != vec2[i]) return false;
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
+            return false;
     }
     return true;
 }
 
 vector<string> by_length(vector<int> arr) {
-    vector<int> numbers;
-    for (int num : arr) {
-        if (num >= 1 && num <= 9) {
-            numbers.push_back(num);
-        }
-    }
-
-    sort(numbers.begin(), numbers.end());
-    reverse(numbers.begin(), numbers.end());
-
     vector<string> result;
-    for (int num : numbers) {
-        string str = "";
-        switch (num) {
-            case 1:
-                str = "One";
-                break;
-            case 2:
-                str = "Two";
-                break;
-            case 3:
-                str = "Three";
-                break;
-            case 4:
-                str = "Four";
-                break;
-            case 5:
-                str = "Five";
-                break;
-            case 6:
-                str = "Six";
-                break;
-            case 7:
-                str = "Seven";
-                break;
-            case 8:
-                str = "Eight";
-                break;
-            case 9:
-                str = "Nine";
-                break;
-        }
-        result.push_back(str);
+    for (int i : arr) {
+        if (i >= 1 && i <= 9)
+            result.push_back(to_string(i));
     }
-
-    return result;
-}
-
-int main() {
-    vector<int> arr = {1, 2, 3, 4};
-    vector<string> output = by_length(arr);
-
-    bool same = issame(output, {"One", "Four", "Nine", "Three"});
-    if (same) {
-        cout << "The vectors are the same." << endl;
-    } else {
-        cout << "The vectors are different." << endl;
+    sort(result.begin(), result.end());
+    reverse(result.begin(), result.end());
+    vector<string> names = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    vector<string> output;
+    for (int i : result) {
+        output.push_back(names[stoi(i) - 1]);
     }
-
-    return 0;
+    return output;
 }
