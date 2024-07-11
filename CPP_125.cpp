0785b911791f55a6a4cb346fa645528d01ac2c26
beep boop <githubbot@vadim.me>
2024-07-11 02:27:46 +0200

    Solution 7 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/cyujl28f Prompt: Complete the following code given the task description and function signature.

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-
+#include<stdio.h>
+#include<vector>
+#include<string>
+#include<algorithm>
 using namespace std;
 
 vector<string> split_words(string txt) {
     vector<string> result;
-    size_t pos = 0, prevPos = 0;
-
+    size_t pos = 0, prev_pos = 0;
+    
     while ((pos = txt.find_first_of(" ,")) != string::npos) {
         if (pos == 0) {
             if (txt.find(',') == string::npos)
@@ -16,21 +15,22 @@ vector<string> split_words(string txt) {
             else
                 pos = txt.find(',');
         }
-        result.push_back(txt.substr(prevPos, pos - prevPos));
-        prevPos = pos + 1;
+        result.push_back(txt.substr(prev_pos, pos - prev_pos));
+        prev_pos = pos + 1;
     }
-
-    result.push_back(txt.substr(prevPos));
+    
+    if (prev_pos < txt.size())
+        result.push_back(txt.substr(prev_pos));
 
     return result;
 }
 
-int count_lowercase(string str) {
+int count_lowercase(string s) {
     int count = 0;
-    for (char c : str) {
+    for (char c : s) {
         if (islower(c)) {
             count++;
-            if (count % 2 != 0)
+            if (count % 2 == 1)
                 break;
         }
     }
