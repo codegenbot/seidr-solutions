8570636cdfc77091f8db09c86d6772e4c3392c49
beep boop <githubbot@vadim.me>
2024-07-11 06:19:17 +0200

    Solution 52 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/l6shnh36 Prompt: ef951122-32d9-4682-84a4-1de0011ad9dc.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ ef951122-32d9-4682-84a4-1de0011ad9dc.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -6,17 +6,20 @@ string int_to_mini_romank(int n) {
     if (n == 1) {
         return "i";
     } else if (n >= 1000) {
-        return "m";
+        return "M";
     } else if (n >= 200) {
-        return to_string(n / 200) + "c" + string(1, 'k' - (n % 200 > 0));
+        string roman = to_string(n / 200) + "C";
+        if ((n % 200) > 0) {
+            roman += "d";
+        }
+        return roman;
     } else if (n >= 20) {
-        return to_string(n / 10) + "x";
+        string roman = to_string(n / 10) + "X";
+        if ((n % 10) > 0) {
+            roman += "i";
+        }
+        return roman;
     } else {
         return "i";
     }
-}
-
-int main() {
-    cout << int_to_mini_romank(1000) << endl;
-    return 0;
 }
