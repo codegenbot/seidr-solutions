48acaa14a7533bf278b53d03394a92028dc333fe
beep boop <githubbot@vadim.me>
2024-07-11 20:09:00 +0200

    Solution 38 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/pai4d1z1 Prompt:       |         ^~~~ 0e4d7d6a-1fe7-4601-bcd8-fdd8bfe3b701.cpp:28:17: error: ‘nums’ was not declared i n this scope; did you mean ‘num’?    28 |         cout << nums[i] << endl;       |                 ^~~~       |                 num 0e4d7d6a-1fe7-4601-bcd8-fdd8bfe3b701.cpp:28:28: error: ‘endl’ was not declared i n this scope    28 |         cout << nums[i] << endl;       |                            ^~~~ 0e4d7d6a-1fe7-4601-bcd8-fdd8bfe3b701.cpp:30:5: error: ‘cout’ was not declared in  this scope    30 |     cout << endl;       |     ^~~~ 0e4d7d6a-1fe7-4601-bcd8-fdd8bfe3b701.cpp:30:13: error: ‘endl’ was not declared i n this scope    30 |     cout << endl;       |             ^~~~ 0e4d7d6a-1fe7-4601-bcd8-fdd8bfe3b701.cpp:31:31: error: ‘nums’ was not declared i n this scope; did you mean ‘num’?    31 |     for (int i = cut_idx; i < nums.size(); ++i) {       |                               ^~~~       |                               num

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -21,7 +21,7 @@ int main() {
         for (int j = i; j < nums.size(); ++j) {
             right_sum += nums[j];
         }
-        int diff = abs(left_sum - right_sum);
+        int diff = std::abs(left_sum - right_sum);
         if (diff < min_diff) {
             min_diff = diff;
             cut_idx = i;
