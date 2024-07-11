d0d00f9ba8221ad168484a68eb2a507dab801cd1
beep boop <githubbot@vadim.me>
2024-07-11 19:46:06 +0200

    Solution 66 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/vrris38p Prompt: llocator<float>; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:305:9:   required from ‘std::_Vector_base< _Tp, _Alloc>::_Vector_base(size_t, const allocator_type&) [with _Tp = float; _Al loc = std::allocator<float>; size_t = long unsigned int; std::_Vector_base<_Tp, _Alloc>::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:524:47:   required from ‘std::vector<_Tp, _Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const value_type&, const al locator_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_T p, _Alloc>::size_type = long unsigned int; std::vector<_Tp, _Alloc>::value_type = float; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/vector.tcc:262:11:   required from ‘void std::vector<_T p, _Alloc>::_M_fill_assign(size_t, const value_type&) [with _Tp = float; _Alloc = std::allocator<float>; size_t = long unsigned int; std::vector<_Tp, _Alloc>::v alue_type = float]’ /usr/include/c++/11/bits/stl_vector.h:750:9:   required from ‘void std::vector<_ Tp, _Alloc>::assign(std::vector<_Tp, _Alloc>::size_type, const value_type&) [wit h _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Alloc>::size_ty pe = long unsigned int; std::vector<_Tp, _Alloc>::value_type = float]’ 286daa49-e2e0-4a51-91b0-824b9cbb013f.cpp:19:14:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -9,18 +9,16 @@ double vectorDistance(int n, const std::vector<float>& v1, const std::vector<flo
         double diff = v1[i] - v2[i];
         sum += std::pow(diff, 2);
     }
-    if (sum > 0.0) {
-        return std::sqrt(sum);
-    } else {
-        return 0.0;
-    }
+    return std::sqrt(sum);
 }
 
 int main() {
     int n;
     std::cin >> n; 
     std::vector<float> v1, v2;
-    v1.resize(n); v2.resize(n);
+    for (float& val : v1) { std::cin >> val; } v1.resize(n); v2.resize(n);
+    for (float& val : v1) { val = 0.0f; } 
+    for (float& val : v2) { val = 0.0f; }
     for (float& val : v1) { std::cin >> val; }
     for (float& val : v2) { std::cin >> val; }
     double dist = vectorDistance(n, v1, v2);
