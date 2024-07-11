1aad9d3882872616452c63f44a8532a89812eb94
beep boop <githubbot@vadim.me>
2024-07-11 15:37:07 +0200

    Solution 1 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/pwk450v7 Prompt: Program did not receive expected input

--- Python_162.py
+++ Python_162.py
@@ -2,7 +2,4 @@ import hashlib
 
 
 def string_to_md5(text):
-    if text:
-        return hashlib.md5(text.encode()).hexdigest()
-    else:
-        return None
+    return hashlib.md5(text.encode()).hexdigest() if text else None
