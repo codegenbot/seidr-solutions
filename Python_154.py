ec0f2f64e9910e7e31df0922737242731ea2bd83
beep boop <githubbot@vadim.me>
2024-07-11 12:18:26 +0200

    Solution 11 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fkmlxvoz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d853d1bb-dec0-493c-8983-8a8a3bba6 328.py", line 12, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d853d1bb-dec0-493c-8983-8a8a3bba6 328.py", line 9, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d853d1bb-dec0-493c-8983-8a8a3bba6 328.py", line 3, in cycpattern_check     if b == a[0] + a[-1*a.index(b):]:                         ^^^^^^^^^^ ValueError: substring not found

--- Python_154.py
+++ Python_154.py
@@ -1,6 +1,6 @@
 def cycpattern_check(a, b):
     for _ in range(len(a)):
-        a = a[1:] + a[0]
-        if b.lower() in a.lower():
-            return True
+        if a[0] == b[0]:
+            if b == a[:len(b)]:
+                return True
     return False
