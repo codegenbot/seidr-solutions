27dd614c27fd46648e55180c67923f12b2956119
beep boop <githubbot@vadim.me>
2024-07-11 15:14:21 +0200

    Solution 60 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/sa1121ta Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e50c2b3c-32c5-4915-a4aa-87c0078bd f93.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e50c2b3c-32c5-4915-a4aa-87c0078bd f93.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,4 @@
-```
 def sort_even(l: list):
     even_numbers = sorted([x for x in l if x % 2 == 0])
-    odd_numbers = sorted([x for x in l if x % 2 != 0])
-    return tuple(even_numbers + sorted(odd_numbers))
+    odd_numbers = sorted([abs(x) for x in l if x % 2 != 0], key=lambda x: (x%2, x))
+    return tuple(even_numbers + odd_numbers)
