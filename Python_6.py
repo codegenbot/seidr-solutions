355a2b22ded5f3d09069780b3785e4119371f448
beep boop <githubbot@vadim.me>
2024-07-11 21:21:37 +0200

    Solution 28 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/gl758tng Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fe82e60d-8b85-4d37-b850-322a75348 bab.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_6.py
+++ Python_6.py
@@ -6,11 +6,14 @@ def parse_nested_parens(paren_string: str) -> List[int]:
     for group in paren_string.split():
         level = 0
         max_level = 0
+        temp_result = []
         for char in group:
             if char == '(':
                 level += 1
+                temp_result.append(level)
             elif char == ')':
                 level -= 1
+                temp_result.append(level)
             max_level = max(max_level, level)
-        result.append(max_level)
+        result.append(max(temp_result))
     return result
