e5e6b74f4b88c1385428a337c214094115704303
beep boop <githubbot@vadim.me>
2024-07-11 04:54:28 +0200

    Solution 22 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2dqf0nts Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:11: 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp: In function ‘int main()’: 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 6b7cb184-e6f8-4615-99c1-35ac16f5cd9a.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,21 +1,17 @@
 #include <boost/any.hpp>
 #include <vector>
-#include <list>
 
-bool issame(const vector<int>& a, const vector<int>& b) {
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-vector<int> filter_integers(list< boost::any > values) {
-    vector<int> result;
+std::vector<int> filter_integers(std::list<boost::any> values) {
+    std::vector<int> result;
     for (const auto& value : values) {
-        if(boost::any_cast<int>(value).good() && isnumeric(boost::any_cast<int>(value))) {
-            result.push_back(boost::any_cast<int>(value));
+        if (boost::any_cast<int>(value).type() != typeid(int)) {
+            continue;
         }
+        result.push_back(boost::any_cast<int>(value));
     }
     return result;
-}
-
-bool isnumeric(int num) {
-    return true;
 }
