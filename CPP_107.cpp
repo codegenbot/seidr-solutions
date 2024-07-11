5e2c1dc71cd78abec4bda72f529ead8a2b374279
beep boop <githubbot@vadim.me>
2024-07-11 17:43:28 +0200

    Solution 60 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/bvg9l28l Prompt:       |                    ^~~ e0bd0716-9fe3-47eb-935c-81a04d5b75c7.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e0bd0716-9fe3-47eb-935c-81a04d5b75c7.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e0bd0716-9fe3-47eb-935c-81a04d5b75c7.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e0bd0716-9fe3-47eb-935c-81a04d5b75c7.cpp:18: e0bd0716-9fe3-47eb-935c-81a04d5b75c7.cpp: In function ‘int main()’: e0bd0716-9fe3-47eb-935c-81a04d5b75c7.cpp:28:20: error: ‘even_odd_palindrome’ was  not declared in this scope    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ e0bd0716-9fe3-47eb-935c-81a04d5b75c7.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -3,18 +3,18 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(const std::string &str1, const std::string &str2) {
+bool issame(const std::string& str1, const std::string& str2) {
     return str1 == str2;
 }
 
 std::vector<int> even_odd_palindrome(int n) {
     std::vector<int> res(2, 0);
-    for (int i = 1; i <= n; ++i) {
+    for(int i = 1; i <= n; ++i) {
         std::string str = std::to_string(i);
         std::string rev_str = str;
         std::reverse(rev_str.begin(), rev_str.end());
-        if (issame(str, rev_str)) {
-            if (i % 2 == 0) {
+        if(issame(str, rev_str)) {
+            if(i % 2 == 0) {
                 res[0]++;
             } else {
                 res[1]++;
