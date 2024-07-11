aa6b5467ce4039d245fa592a62763b8e10b9b204
beep boop <githubbot@vadim.me>
2024-07-11 13:48:50 +0200

    Solution 41 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dmxi7cce Prompt: Program did not receive expected input

--- Python_151.py
+++ Python_151.py
@@ -1,3 +1,13 @@
-elements = input("Enter elements separated by comma: ")
-result = double_the_difference([int(i) for i in elements.split(",")])
+def double_the_difference(lst):
+    if len(lst) < 2:
+        return None
+    try:
+        lst = [int(i.strip()) for i in (lst[0].strip().split(","))]
+        lst.sort()
+        return (lst[-1] - lst[0]) * 2
+    except ValueError:
+        return "Invalid input"
+
+
+result = double_the_difference(input("Enter elements separated by comma: "))
 print(result)
