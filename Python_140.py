97a2f8a29d59ef839e38a6c865c3cb240a3d7321
beep boop <githubbot@vadim.me>
2024-07-11 17:33:51 +0200

    Solution 29 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/xs7jwkcr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f3858917-29b6-40ae-b5c7-430e1846b cbf.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,2 +1,8 @@
-def fix_spaces(text):
-    return text.replace("   ", "_").replace("  ", "_").replace(" ", "_")
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
+    prefix = strs[0]
+    for s in strs:
+        while not s.startswith(prefix):
+            prefix = prefix[:len(common_prefix) - 1]
+    return prefix
