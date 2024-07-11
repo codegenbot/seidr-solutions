49d891d92ea840272fd4d40af1a93fa578a80c9b
beep boop <githubbot@vadim.me>
2024-07-11 06:33:51 +0200

    Solution 41 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/8zvpzqkx Prompt:    43 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8e3d7396-0a69-4f74-bb73-15112bae1f8e.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ 8e3d7396-0a69-4f74-bb73-15112bae1f8e.cpp:36:5: note: ‘int main()’ previously def ined here    36 | int main(){       |     ^~~~ In file included from 8e3d7396-0a69-4f74-bb73-15112bae1f8e.cpp:42: 8e3d7396-0a69-4f74-bb73-15112bae1f8e.cpp: In function ‘int main()’: 8e3d7396-0a69-4f74-bb73-15112bae1f8e.cpp:52:20: error: ‘count_up_to’ was not dec lared in this scope    52 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 8e3d7396-0a69-4f74-bb73-15112bae1f8e.cpp:52:19: error: ‘issame’ cannot be used a s a function    52 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -8,13 +8,13 @@ bool issame(vector<int> a, vector<int> b){
     if(a.size() != b.size()){
         return false;
     }
-    
+
     for(int i=0; i<a.size(); ++i){
         if(a[i] != b[i]){
             return false;
         }
     }
-    
+
     return true;
 }
 
@@ -37,6 +37,6 @@ vector<int> count_up_to(int n){
 
 int main(){
     assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-    
+
     return 0;
 }
