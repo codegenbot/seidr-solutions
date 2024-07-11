0a60d723c4ded26ad65cb26862c96372dad8e848
beep boop <githubbot@vadim.me>
2024-07-11 15:38:23 +0200

    Solution 63 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g0yxi6dq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/94a3258c-fd21-41fa-a216-a23c84076 fb9.py", line 1, in <module>     def find_closed_elements(numbers: List[float]) -> Tuple[float, float]:                                       ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_20.py
+++ Python_20.py
@@ -1,4 +1,6 @@
 from typing import List, Tuple
-def find_closed_elements(numbers: list) -> Tuple[float, float]:
+
+
+def find_closed_elements(numbers: list[float]) -> Tuple[float, float]:
     if len(numbers) <= 1:
         return ()
