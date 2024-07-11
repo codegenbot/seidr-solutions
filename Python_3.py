cce95038914dec680bc44a3c5223726da9f58763
beep boop <githubbot@vadim.me>
2024-07-11 14:05:31 +0200

    Solution 26 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/19ayzbs5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/37b1a3a5-412c-4767-a394-91f0cdd93 b70.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_3.py
+++ Python_3.py
@@ -1,9 +1,4 @@
-def below_zero(operations: List[int]) -> bool:
-    if not isinstance(operations, list):
-        return None
-
-    for operation in operations:
-        if not isinstance(operation, int):
-            return None
-
-    return any(op < 0 for op in operations)
+```
+def below_zero(operations):
+    total = sum([num for num in operations if type(num) == int])
+    return total < 0
