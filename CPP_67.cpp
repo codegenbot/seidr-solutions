5addd15069551fdc54d07877bac3f07b36f92c2c
beep boop <githubbot@vadim.me>
2024-07-11 07:55:49 +0200

    Solution 12 of CPP/67, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8hk7urfp Prompt: 297299e7-ac67-48cb-b86a-3a9df4dc636f.cpp: In function ‘int fruit_distribution(st d::string, int)’: 297299e7-ac67-48cb-b86a-3a9df4dc636f.cpp:10:36: error: request for member ‘lengt h’ in ‘"apples"’, which is of non-class type ‘const char [7]’    10 |         if (s.substr(pos, "apples".length()).compare("apples") == 0)       |                                    ^~~~~~ 297299e7-ac67-48cb-b86a-3a9df4dc636f.cpp:17:39: error: request for member ‘lengt h’ in ‘"apples"’, which is of non-class type ‘const char [7]’    17 |         size_t start = pos + "apples".length();       |                                       ^~~~~~ 297299e7-ac67-48cb-b86a-3a9df4dc636f.cpp:26:37: error: request for member ‘lengt h’ in ‘"oranges"’, which is of non-class type ‘const char [8]’    26 |         if (s.substr(pos, "oranges".length()).compare("oranges") == 0)       |                                     ^~~~~~ 297299e7-ac67-48cb-b86a-3a9df4dc636f.cpp:33:40: error: request for member ‘lengt h’ in ‘"oranges"’, which is of non-class type ‘const char [8]’    33 |         size_t start = pos + "oranges".length();       |                                        ^~~~~~

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,25 +1,40 @@
-#include <stdio.h>
 #include <string>
 using namespace std;
 
 int fruit_distribution(string s, int n) {
-    int total_apples = 0;
-    int total_oranges = 0;
+    size_t pos = 0;
+    int apples = 0, oranges = 0;
     
-    string temp = "";
-    for (char c : s) {
-        if (c == ' ') {
-            if (!temp.empty()) {
-                if (temp.find("apples") != string::npos)
-                    total_apples = stoi(temp.substr(0, temp.find(" ")).erase(0, 1));
-                else if (temp.find("oranges") != string::npos)
-                    total_oranges = stoi(temp.substr(0, temp.find(" ")).erase(0, 1));
-                temp.clear();
-            }
-        } else {
-            temp += c;
+    while ((pos = s.find("apples", pos)) != string::npos) {
+        if (s.substr(pos, 7).compare("apples") == 0)
+            break;
+        else
+            pos = s.find("apples", pos + 1);
+    }
+    
+    if (pos != string::npos) {
+        size_t start = pos + 7;
+        while (s[start] >= '0' && s[start] <= '9') {
+            apples = apples * 10 + (s[start] - '0');
+            start++;
         }
     }
     
-    return n - total_apples - total_oranges;
+    pos = 0;
+    while ((pos = s.find("oranges", pos)) != string::npos) {
+        if (s.substr(pos, 8).compare("oranges") == 0)
+            break;
+        else
+            pos = s.find("oranges", pos + 1);
+    }
+    
+    if (pos != string::npos) {
+        size_t start = pos + 8;
+        while (s[start] >= '0' && s[start] <= '9') {
+            oranges = oranges * 10 + (s[start] - '0');
+            start++;
+        }
+    }
+    
+    return n - apples - oranges;
 }
