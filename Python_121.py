35d8f0c223541852bbde93beaca84f39516e2403
beep boop <githubbot@vadim.me>
2024-07-11 11:29:53 +0200

    Solution 17 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rzs1si5s Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d76856a6-99a5-4540-b8ca-3268f5129 db0.py", line 14, in <module>     check(solution)           ^^^^^^^^ NameError: name 'solution' is not defined

--- Python_121.py
+++ Python_121.py
@@ -1,2 +1,1 @@
-def get_odd_sum(lst):
-    return sum(i for i in lst if i % 2 != 0)
+this_function_takes_a_list_as_input_and_returns_the_sum_of_all_odd_numbers_in_that_list = lambda lst: sum(i for i in lst if i % 2 != 0)
