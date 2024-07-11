46fd542e5840036da4078e458f9d059156aae95b
beep boop <githubbot@vadim.me>
2024-07-11 10:08:22 +0200

    Solution 46 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/4tzr7z0i Prompt: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from ef71cca8-5bb5-418d-aec6-06f82c3a1df2.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ef71cca8-5bb5-418d-aec6-06f82c3a1df2.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ef71cca8-5bb5-418d-aec6-06f82c3a1df2.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ef71cca8-5bb5-418d-aec6-06f82c3a1df2.cpp:23: ef71cca8-5bb5-418d-aec6-06f82c3a1df2.cpp: In function ‘int main()’: ef71cca8-5bb5-418d-aec6-06f82c3a1df2.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,3 +1,11 @@
-bool issame(std::vector<int> v1, std::vector<int> v2) {
-    return v1 == v2;
+bool issame(std::vector<int> vec1, std::vector<int> vec2) {
+    if (vec1.size() != vec2.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < vec1.size(); ++i) {
+        if (vec1[i] != vec2[i]) {
+            return false;
+        }
+    }
+    return true;
 }
