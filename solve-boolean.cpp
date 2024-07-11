89f1662709b586408d612ae00164c945b693a290
beep boop <githubbot@vadim.me>
2024-07-11 02:33:07 +0200

    Solution 16 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/w7xo0y1g Prompt:     3 |     cin >> input;       |     ^~~ 9e9b114d-34c3-4fb8-b84b-91f8f1b96d0b.cpp:3:12: error: ‘input’ was not declared i n this scope; did you mean ‘int’?     3 |     cin >> input;       |            ^~~~~       |            int 9e9b114d-34c3-4fb8-b84b-91f8f1b96d0b.cpp:6:9: error: ‘cout’ was not declared in this scope     6 |         cout << "True" << endl;       |         ^~~~ 9e9b114d-34c3-4fb8-b84b-91f8f1b96d0b.cpp:6:27: error: ‘endl’ was not declared in  this scope     6 |         cout << "True" << endl;       |                           ^~~~ 9e9b114d-34c3-4fb8-b84b-91f8f1b96d0b.cpp:8:9: error: ‘cout’ was not declared in this scope     8 |         cout << "False" << endl;       |         ^~~~ 9e9b114d-34c3-4fb8-b84b-91f8f1b96d0b.cpp:8:28: error: ‘endl’ was not declared in  this scope     8 |         cout << "False" << endl;       |                            ^~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,15 +1,15 @@
-int solveBoolean(string s) {
-    if (s == "t") {
-        return true;
-    } else if (s == "f") {
-        return false;
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string input;
+    std::cin >> input;
+    
+    if (input == "t") {
+        std::cout << "True" << std::endl;
     } else {
-        bool op1 = solveBoolean(s.substr(0, s.size() - 2));
-        bool op2 = solveBoolean(s.substr(s.size() - 1, 1));
-        if (s[s.size() - 2] == '|') {
-            return op1 || op2;
-        } else {
-            return op1 && op2;
-        }
+        std::cout << "False" << std::endl;
     }
+    
+    return 0;
 }
