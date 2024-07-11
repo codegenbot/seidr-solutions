48a39512da39be6dda0786fb0fbbdbca6bd7269b
beep boop <githubbot@vadim.me>
2024-07-11 14:14:03 +0200

    Solution 24 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9hixz5xx Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/89946a86-b8e0-4ec6-88dd-2f2b3a80b 9c7.py", line 10, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/89946a86-b8e0-4ec6-88dd-2f2b3a80b 9c7.py", line 5, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,13 +1,5 @@
-```
+```Python
 def sort_even(l: list):
     evens = sorted([x for x in l if x % 2 == 0])
-    result = []
-    i = j = 0
-    while i < len(l):
-        if j < len(evens):
-            result.append(evens[j])
-            j += 1
-        else:
-            result.append(l[i])
-            i += 1
-    return result
+    odds = [i for i in l if i % 2 != 0]
+    return [x for pair in zip(evens, odds) for x in pair] + evens[len(odds):]
