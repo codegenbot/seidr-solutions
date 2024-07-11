6dcfc250368b931b00503bae0528412fc8bde89a
beep boop <githubbot@vadim.me>
2024-07-11 15:34:50 +0200

    Solution 24 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/bso5h56w Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/72940d94-a172-4576-b16d-368cfc14c 07f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,8 +1,7 @@
-def paint Canvas():
-    for _ in range(10):
-        for __ in range(20):
-            if _ == 5:
-                print('#', end=' ')
-            else:
-                print(' ', end=' ')
-        print()
+```
+def add(x, y):
+    return x + y
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print(add(x, y))
