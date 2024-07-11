4933311202d92bdeb2c5a5a7450e20ab6ffcbd2f
beep boop <githubbot@vadim.me>
2024-07-11 14:24:41 +0200

    Solution 43 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/01a7l32z Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/43ba56d6-d328-49a6-9953-68408c9d8 d51.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -2,8 +2,10 @@
 def find_zero(xs):
     if len(xs) % 2 != 1:
         return "Error: xs must start with 0"
-    for i in range(len(xs)-1):
-        if xs[i] != 0:
+    leading_zero = 0
+    for x in xs[1:]:
+        if x != 0:
             return "Error: xs must be [0, *coefficients*]"
-    max_coeff = max(abs(x) for x in xs[1:])
-    return -xs[-1] / (2 * sum(xs[1:])) if max_coeff == abs(sum(xs[1:])) else None
+        leading_zero += x
+    max_coeff = max(abs(x) for x in xs)
+    return -xs[-1] / (2 * leading_zero) if abs(leading_zero) == max_coeff else None
