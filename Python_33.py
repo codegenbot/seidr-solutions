a6a17866043d8bcdc090a784f6350d7906fd23eb
beep boop <githubbot@vadim.me>
2024-07-11 08:59:37 +0200

    Solution 9 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6vagafko Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0eb3f1af-833f-456b-954d-63f7123ff 85b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,3 +1,6 @@
-```
+from itertools import cycle
+
 def sort_third(l: list):
-    return sorted((x for i, x in enumerate(l) if i % 3 != 0), key=lambda y: l[i//3])
+    first_half = sorted((x for i, x in enumerate(l) if i % 3 != 0))
+    second_half = sorted((x for i, x in enumerate(l) if i % 3 == 0 and x not in first_half), reverse=True)
+    return [x if i % 3 != 0 else y for i, (x, y) in zip(enumerate(l), cycle([first_half, second_half]))]
