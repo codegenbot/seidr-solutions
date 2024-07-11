00066fbb30db3f2361ef13d8dcd23cb9b1ba7a5f
beep boop <githubbot@vadim.me>
2024-07-11 17:50:30 +0200

    Solution 28 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/4talfebb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cd34c2d6-896c-4aa2-a771-970ae3392 f74.py", line 15     print(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214 876*%^#!(@^$_!@^%#$(!#@%$(01234~~``)) # Your tweet has 90 characters                          ^ SyntaxError: unterminated string literal (detected at line 15)

--- twitter.py
+++ twitter.py
@@ -1,8 +1,7 @@
-print("Here is the solution:")
 def validate_tweet(tweet):
-    if len(tweet) > 140:
-        return "Too many characters"
-    elif tweet == "":
+    if len(tweet) == 0:
         return "You didn't type anything"
+    elif len(tweet) > 140:
+        return "Too many characters"
     else:
         return f"Your tweet has {len(tweet)} characters"
