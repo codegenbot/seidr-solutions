e8f9414fe0e35574a6221b39ac4f12574aa3b42b
beep boop <githubbot@vadim.me>
2024-07-11 21:53:23 +0200

    Solution 22 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/jq1yikmk Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -2,6 +2,6 @@ int num1 = stoi(x.substr(0, x.find('/')));
     int den1 = stoi(x.substr(x.find('/') + 1));
     int num2 = stoi(n.substr(0, n.find('/')));
     int den2 = stoi(n.substr(n.find('/') + 1));
-
+    
     return (num1 * num2) % (den1 * den2) == 0;
 }
