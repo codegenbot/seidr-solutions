87064f674249d87b9a556aa4fea937cf0b58a948
beep boop <githubbot@vadim.me>
2024-07-11 11:55:56 +0200

    Solution 31 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/qk069l9j Prompt:       |                         ^~~~~~ 3a52bd20-4a96-4dd8-aef8-c69dfc303a41.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ 3a52bd20-4a96-4dd8-aef8-c69dfc303a41.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ 3a52bd20-4a96-4dd8-aef8-c69dfc303a41.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ 3a52bd20-4a96-4dd8-aef8-c69dfc303a41.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from 3a52bd20-4a96-4dd8-aef8-c69dfc303a41.cpp:22: 3a52bd20-4a96-4dd8-aef8-c69dfc303a41.cpp: In function ‘int main()’: 3a52bd20-4a96-4dd8-aef8-c69dfc303a41.cpp:24:32: error: ‘Strongest_Extension’ can not be used as a function    24 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,8 +1,11 @@
+Here is the modified code:
+
+```cpp
 #include <string>
 #include <vector>
 #include <cctype>
 
-int Strongest_Extension(string class_name, vector<string> extensions){
+int Strongest_Extension(string class_name,vector<string> extensions){
     int strongest_strength = 0;
     string strongest_extension;
 
