ae4bb50639a6a0fb00706f945d3e1be3996071e7
beep boop <githubbot@vadim.me>
2024-07-11 19:22:59 +0200

    Solution 87 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ebtie55w Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fd874aa7-e5ab-42e6-9e52-77f7c38e7 f0f.py", line 17, in <module>     check(solution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fd874aa7-e5ab-42e6-9e52-77f7c38e7 f0f.py", line 12, in check     assert solution([3, 13, 2, 9]) == 3            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_121.py
+++ Python_121.py
@@ -1,3 +1,4 @@
+```Python
 def solution(lst):
     if len(lst) == 0:
         return 0
@@ -5,9 +6,8 @@ def solution(lst):
         return sum(i for i in lst if i % 2 != 0)
     else:
         half = len(lst) // 2
-        result1 = sum(i for i in lst[: half + 1] if i % 2 != 0)
+        result1 = sum(i for i in lst[:half+1] if i % 2 != 0)
         result2 = sum(i for i in lst[half:] if i % 2 != 0)
         return max(result1, result2)
-
 
 assert solution([3, 13, 2, 9]) == 15
