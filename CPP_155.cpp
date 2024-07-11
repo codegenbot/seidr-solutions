137b64f31a0806866b9a1ed1add1ad7ba3c1800c
beep boop <githubbot@vadim.me>
2024-07-11 03:59:53 +0200

    Solution 44 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/anmdhyr2 Prompt:    37 | int main(){       |     ^~~~ 9161b170-eb67-40ed-a561-6f4ab7b35d25.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from 9161b170-eb67-40ed-a561-6f4ab7b35d25.cpp:28: 9161b170-eb67-40ed-a561-6f4ab7b35d25.cpp: In function ‘int main()’: 9161b170-eb67-40ed-a561-6f4ab7b35d25.cpp:38:34: error: cannot convert ‘std::vect or<int>’ to ‘int’    38 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ~~~~~~~~~~~~~~^~~       |                                  |       |                                  std::vector<int> 9161b170-eb67-40ed-a561-6f4ab7b35d25.cpp:4:12: note:   initializing argument 1 o f ‘int issame(int)’     4 | int issame(const vector<int>& a, const vector<int>& b) {       |            ^~~~~~~~~~~~ 9161b170-eb67-40ed-a561-6f4ab7b35d25.cpp: In function ‘int issame(int)’: 9161b170-eb67-40ed-a561-6f4ab7b35d25.cpp:9:1: warning: control reaches end of no n-void function [-Wreturn-type]     9 | }       | ^

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,18 +1,11 @@
-```c++
 #include <vector>
 #include <string>
 
-int issame(const vector<int>& a, const vector<int>& b) {
-    if(a.size() == b.size()) {
-        for (int i = 0; i < a.size(); ++i) {
-            if (a[i] != b[i]) {
-                return -1;
-            }
-        }
-        return 0;
-    } else {
-        return -1;
-    }
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    if(a == b)
+        return true;
+    else
+        return false;
 }
 
 std::vector<int> even_odd_count(int num) {
@@ -27,7 +20,7 @@ std::vector<int> even_odd_count(int num) {
     return {count_even, count_odd};
 }
 
-int main(int argc, char* argv[]) {
+int main() {
     assert(even_odd_count(0) == std::vector<int>{1, 0});
     return 0;
 }
