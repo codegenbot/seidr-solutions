fbfd03c2b71920221c7b71e91bfdfa6d9c8a82c6
beep boop <githubbot@vadim.me>
2024-07-11 02:36:01 +0200

    Solution 14 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/cyujl28f Prompt: Complete the following code given the task description and function signature.

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,38 +1,28 @@
-#include<stdio.h>
-#include<vector>
-#include<string>
-#include<algorithm>
-using namespace std;
+Here is the completed code:
 
+```cpp
 vector<string> split_words(string txt) {
     vector<string> result;
-    size_t pos = 0, prev_pos = 0;
-    
-    while ((pos = txt.find_first_of(" ,")) != string::npos) {
-        if (pos == 0) {
-            if (txt.find(',') == string::npos)
-                return {to_string(count_lowercase(txt))};
-            else
-                break;
-        }
-        result.push_back(txt.substr(prev_pos, pos - prev_pos));
-        prev_pos = pos + 1;
+    size_t pos = 0;
+    while ((pos = txt.find(' ')) != string::npos) {
+        result.push_back(txt.substr(0, pos));
+        txt.erase(0, pos + 1);
     }
-    
-    if (prev_pos < txt.size())
-        result.push_back(txt.substr(prev_pos));
-    
+    if (txt.empty()) {
+        return result;
+    }
+    if (txt.find(',') == string::npos) {
+        int count = 0;
+        for (char c : txt) {
+            if (c >= 'a' && c <= 'z') {
+                if ((int)c % 2 != 0) {
+                    count++;
+                }
+            }
+        }
+        result.push_back(to_string(count));
+    } else {
+        result.push_back(txt);
+    }
     return result;
-}
-
-int count_lowercase(string s) {
-    int count = 0;
-    for (char c : s) {
-        if (islower(c)) {
-            count++;
-            if (count % 2 == 1)
-                break;
-        }
-    }
-    return count;
 }
