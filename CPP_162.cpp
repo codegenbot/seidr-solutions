828e823926d8bab211a45ac8631b8a9c7c1fd1ba
beep boop <githubbot@vadim.me>
2024-07-11 02:37:28 +0200

    Solution 16 of CPP/162, ratio 0.25 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fcqijk62 Prompt:     1 | ```       |  ^ 7cf566c1-258c-4e8c-984e-54a461383231.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 7cf566c1-258c-4e8c-984e-54a461383231.cpp:2:1: error: expected unqualified-id bef ore ‘for’     2 | for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)       | ^~~ 7cf566c1-258c-4e8c-984e-54a461383231.cpp:2:17: error: ‘i’ does not name a type     2 | for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)       |                 ^ 7cf566c1-258c-4e8c-984e-54a461383231.cpp:2:40: error: expected unqualified-id be fore ‘++’ token     2 | for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)       |                                        ^~ In file included from 7cf566c1-258c-4e8c-984e-54a461383231.cpp:5: 7cf566c1-258c-4e8c-984e-54a461383231.cpp: In function ‘int main()’: 7cf566c1-258c-4e8c-984e-54a461383231.cpp:7:13: error: ‘string_to_md5’ was not de clared in this scope     7 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,25 +1,26 @@
-```cpp
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
 #include "md5.h"
 
-std::string string_to_md5(const std::string& input) {
-    unsigned char md5[MD5_DIGEST_LENGTH];
-    MD5(input.c_str(), input.length(), md5);
-    std::string result;
-    for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)
-        result.push_back(tohex(md5[i]));
-    return result;
-}
-
-std::string tohex(unsigned int num) {
-    char buff[3];
-    sprintf(buff, "%02x", num);
-    return std::string(buff);
-}
+using namespace std;
 
 int main() {
-    std::string md5_hash = string_to_md5("password");
-    assert(md5_hash == "5f4dcc3b5aa765d61d8327deb882cf99");
+    string input;
+    cout << "Enter a password: ";
+    cin >> input;
+    
+    unsigned char md5[16];
+    MD5(input, md5);
+
+    vector<string> result;
+    for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)
+        result.push_back(setw(2) << hex << setfill('0') << ((int)(md5[i])));
+
+    cout << "MD5 Hash: ";
+    for(string s : result){
+        cout << s;
+    }
     return 0;
 }
