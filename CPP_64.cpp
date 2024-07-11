22fbc8513c8cee903563ee5445c6486458512315
beep boop <githubbot@vadim.me>
2024-07-11 17:47:42 +0200

    Solution 74 of CPP/64, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zx5gdxbr Prompt: 7b72abe0-6cf4-4823-9cb5-cd861394c719.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int vowels_count(string s){       |                  ^~~~~~ In file included from 7b72abe0-6cf4-4823-9cb5-cd861394c719.cpp:11: 7b72abe0-6cf4-4823-9cb5-cd861394c719.cpp: In function ‘int main()’: 7b72abe0-6cf4-4823-9cb5-cd861394c719.cpp:13:25: error: ‘vowels_count’ cannot be used as a function    13 |     assert (vowels_count("ACEDY") == 3);       |             ~~~~~~~~~~~~^~~~~~~~~

--- CPP_64.cpp
+++ CPP_64.cpp
@@ -4,7 +4,7 @@
 int vowels_count(std::string s){
     int count = 0;
     for(char c : s){
-        if(tolower(c) == 'a' || tolower(c) == 'e' || tolower(c) == 'i' || tolower(c) == 'o' || (tolower(c) == 'u' && c == s.back())){
+        if(tolower(c) == 'a' || tolower(c) == 'e' || tolower(c) == 'i' || tolower(c) == 'o' || tolower(c) == 'u'){
             count++;
         }
     }
