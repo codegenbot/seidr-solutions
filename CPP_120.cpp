ec49d2aa3f50e1d7d7720ef6719cdd156ddb0dee
beep boop <githubbot@vadim.me>
2024-07-11 10:46:58 +0200

    Solution 12 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zox4aix9 Prompt:       |                    ^~~ a8f5a519-1db1-4de1-8c6e-c91a2399a82e.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a8f5a519-1db1-4de1-8c6e-c91a2399a82e.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a8f5a519-1db1-4de1-8c6e-c91a2399a82e.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a8f5a519-1db1-4de1-8c6e-c91a2399a82e.cpp:11: a8f5a519-1db1-4de1-8c6e-c91a2399a82e.cpp: In function ‘int main()’: a8f5a519-1db1-4de1-8c6e-c91a2399a82e.cpp:21:20: error: ‘maximum’ was not declare d in this scope    21 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ a8f5a519-1db1-4de1-8c6e-c91a2399a82e.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,21 +1,11 @@
-#include <vector>
-#include <algorithm>
+using namespace std;
 
-bool issame(std::vector<int> a,std::vector<int>b){
-    if(a.size()!=b.size())return false;
-    for(int i=0; i<a.size();i++){
-        if(a[i]!=b[i]) return false;
+vector<int> maximum(vector<int> arr, int k) {
+    vector<int> result;
+    for (int i = 0; i < k; i++) {
+        auto it = max_element(arr.begin(), arr.end());
+        result.push_back(*it);
+        arr.erase(it);
     }
-    return true;
-}
-
-std::vector<int> maximum(std::vector<int> arr, int k) {
-    std::vector<int> result(arr.begin(), arr.begin() + k);
-    sort(result.begin(), result.end());
     return result;
-}
-
-int main() {
-    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 7) , {0, -23, 1, 2, 3, -400, 243}));
-    return 0;
 }
