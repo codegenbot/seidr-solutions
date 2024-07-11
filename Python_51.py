dbcd33fa219df780b38bc425aa672f4cd5771328
beep boop <githubbot@vadim.me>
2024-07-11 13:17:40 +0200

    Solution 7 of Python/51, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zp0p3oi7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/24917be4-05dc-4a32-b7a2-1b909d734 ffa.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_51.py
+++ Python_51.py
@@ -1,5 +1,3 @@
-```
-def remove_vowels():
-    text = input("Enter a sentence: ")
-    result = ''.join([char for char in text if char.lower() not in 'aeiou'])
-    print(result)
+```Python
+def remove_vowels(text):
+    return ''.join([char for char in text if char.lower() not in 'aeiou'])
