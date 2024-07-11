f50b585a08c0ed215530e40a6a084584acb5a1b5
beep boop <githubbot@vadim.me>
2024-07-11 21:36:18 +0200

    Solution 91 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/65kjjion Prompt:       |                                  ^~~ 80efb364-77fd-49a4-a511-b3c6236a99d8.cpp:37:39: error: expression list treated a s compound expression in initializer [-fpermissive]    37 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 80efb364-77fd-49a4-a511-b3c6236a99d8.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ 80efb364-77fd-49a4-a511-b3c6236a99d8.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from 80efb364-77fd-49a4-a511-b3c6236a99d8.cpp:36: 80efb364-77fd-49a4-a511-b3c6236a99d8.cpp: In function ‘int main()’: 80efb364-77fd-49a4-a511-b3c6236a99d8.cpp:46:39: error: no match for call to ‘(st d::vector<int>) (const char [15])’    46 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~ 80efb364-77fd-49a4-a511-b3c6236a99d8.cpp:46:19: error: ‘issame’ cannot be used a s a function    46 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -30,6 +30,6 @@ std::vector<int> parse_nested_parens(std::string paren_string) {
 }
 
 int main() {
-    assert(custom::issame(parse_nested_parens("(()(())((())))"), std::vector<int>{4, 3, 2, 1}));
+    assert(custom::issame(parse_nested_parens("(()(())(( )))"), std::vector<int>{4, 3, 2, 1}));
     return 0;
 }
