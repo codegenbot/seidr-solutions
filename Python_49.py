e515aa434323cf027ea36f7e03bdf55a778175bc
beep boop <githubbot@vadim.me>
2024-07-11 16:23:30 +0200

    Solution 60 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3783cyso Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2aa07b4b-a3c0-4d42-93f1-26373d025 06f.py", line 19, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2aa07b4b-a3c0-4d42-93f1-26373d025 06f.py", line 16, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,12 +1,15 @@
+```
 def modp(n: int, p: int):
-    def extended_euclidean(a: int, b: int) -> (int, int):
+    def extended_euclidean(a: int, b: int) -> (int, int, int):
         if a == 0:
             return b, 0, 1
         else:
             gcd, y, x = extended_euclidean(b % a, a)
             return gcd, x - (b // a) * y, y
 
-    gcd, inv_n, _ = extended_euclidean(n, p)
-    if gcd != 1:
-        raise ValueError(f"{n} is not invertible modulo {p}")
-    return pow(n, inv_n, p)
+    _, x, _ = extended_euclidean(p, p)
+    if (p - 1) % x == 0:
+        inv_p = x
+    else:
+        raise ValueError(f"{p} is not invertible modulo {p}")
+    return pow(n, inv_p, p)
