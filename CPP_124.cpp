7d914ad2020b16ab625a0e2245692beb8783ed8a
beep boop <githubbot@vadim.me>
2024-07-11 16:19:42 +0200

    Solution 11 of CPP/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ho3davf8 Prompt: Complete the following code given the task description and function signature.

--- CPP_124.cpp
+++ CPP_124.cpp
@@ -1,38 +1,26 @@
-int daysInMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    size_t pos1 = date.find('-');
-    size_t pos2 = date.rfind('-');
-
-    if (pos1 == string::npos || pos2 == string::npos || pos1 == pos2) {
-        return false;
-    }
-
-    string sDay = date.substr(pos1 + 1, pos2 - pos1 - 1);
-    string sMonth = date.substr(0, pos1);
-    string sYear = date.substr(pos2 + 1);
-
-    int day = atoi(sDay.c_str());
-    int month = atoi(sMonth.c_str());
-    int year = atoi(sYear.c_str());
-
-    if (day < 1 || month < 1 || month > 12) {
-        return false;
-    }
-
-    if (month == 2) {
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            if (day > 29) {
-                return false;
-            }
-        } else {
-            if (day > 28) {
-                return false;
-            }
-        }
-    } else {
-        if (day > daysInMonth[month]) {
-            return false;
-        }
-    }
-
-    return true;
+if (date.empty()) {
+    return false;
 }
+
+int month, day, year;
+if (sscanf(date.c_str(), "%d-%d-%d", &month, &day, &year) != 3) {
+    return false;
+}
+
+if (month < 1 || month > 12) {
+    return false;
+}
+
+if ((day < 1 || day > 31) && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)) {
+    return false;
+}
+
+if ((day < 1 || day > 30) && (month == 4 || month == 6 || month == 9 || month == 11)) {
+    return false;
+}
+
+if ((day < 1 || day > 29) && (month == 2)) {
+    return false;
+}
+
+return true;
