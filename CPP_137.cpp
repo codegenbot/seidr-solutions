35c4c6492b99e26e4fda8fdd77f1013b7a008517
beep boop <githubbot@vadim.me>
2024-07-12 14:54:34 +0200

    Solution 7 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pal7yz6s Prompt: ared    36 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 9040f141-1002-46d2-9822-0d156028387f.cpp:36:35: error: expected primary-expressi on before ‘>’ token    36 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 9040f141-1002-46d2-9822-0d156028387f.cpp:36:62: error: could not convert ‘1’ fro m ‘int’ to ‘std::string’ {aka ‘std::__cxx11::basic_string<char>’}    36 |    assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "None ");       |                                                             ^       |                                                             |       |                                                             int 9040f141-1002-46d2-9822-0d156028387f.cpp: In function ‘std::string compare_one(s td::string, std::string)’: 9040f141-1002-46d2-9822-0d156028387f.cpp:20:1: warning: control reaches end of n on-void function [-Wreturn-type]    20 | }       | ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -4,25 +4,29 @@
 
 using namespace std;
 
-string compare_one(string a, string b) {
-    if (stof(a) > stof(b)) 
+string compare_one(string a) {
+    stof s1 = stof(a);
+    if (a.find('x') != string::npos)
         return a;
-    else if (stof(a) == stof(b))
-        return "None";
-    else if (a > b) 
-        return a;
-    else
-        return b;
+    else {
+        a = to_string(stoi(a)); 
+        stof s2 = stof(a);
+        if (s1 > s2)
+            return a;
+        else if (s1 == s2)
+            return "None";
+        else
+            return a;
+    }
 }
 
 int main() {
     string a, b;
-    cout << "Enter the first number or word: ";
+    cout << "Enter the first number: ";
     cin >> a;
-    cout << "Enter the second number or word: ";
+    cout << "Enter the second number: ";
     cin >> b;
-
-    cout << compare_one(a, b) << endl;
+    cout << compare_one(a) << endl;
 
     return 0;
 }
