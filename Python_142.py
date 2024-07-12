927ad79775af9c4eb0099ca5245a5b1e7cd69029
beep boop <githubbot@vadim.me>
2024-07-12 12:15:59 +0200

    Solution 91 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/y0gq3031 Prompt: Program did not receive expected input

--- Python_142.py
+++ Python_142.py
@@ -1,26 +1,22 @@
 ```
-def sum_squares(lst):
-    return sum(i**2 for i in lst)
-
 while True:
-    while input("Do you want to start? (yes/no): ") != "yes":
-        pass
-    lst = input("Enter a list of numbers separated by space: ").split()
-    
-    int_lst = []
-    while len(int_lst) < len(lst):
+    response = input("Do you want to start? (yes/no): ")
+    if response.lower() != "yes":
+        continue
+    while True:
+        user_input = input("Enter a list of numbers separated by space: ").split()
+        
         try:
-            temp = [int(i) for i in lst if i.lstrip("-").isdigit()]
+            int_lst = [int(i) for i in user_input]
         except ValueError:
             print("Some numbers are out of range. Please re-enter.")
         else:
-            if all(-100 <= n <= 100 for n in temp):
-                int_lst = temp
+            if all(-100 <= n <= 100 for n in int_lst):
+                print(sum_squares(int_lst))
                 break
             else:
                 print("Some numbers are out of range. Please re-enter.")
-    if int_lst:
-        print(sum_squares(int_lst))
-        break
-    else:
-        print("No valid integers found.")
+
+
+def sum_squares(lst):
+    return sum(i**2 for i in lst)
