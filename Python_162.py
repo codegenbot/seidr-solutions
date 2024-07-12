8da89c896e5ea83abd16db856891a81215d4123a
beep boop <githubbot@vadim.me>
2024-07-12 09:27:03 +0200

    Solution 30 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ua814v8v Prompt: Please enter some text: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/edf5c364-d0ec-4690-9df5-45e76e102 4a1.py", line 2, in <module>     print(f"Output: {string_to_md5(input().encode())}")                      ^^^^^^^^^^^^^ NameError: name 'string_to_md5' is not defined

--- Python_162.py
+++ Python_162.py
@@ -1,8 +1,7 @@
-```Python
 import hashlib
 
 def string_to_md5(s):
     return hashlib.md5(s).hexdigest()
 
-print("Please enter some text: ", end="")
+print("Please enter some text:", end="")
 print(f"Output: {string_to_md5(input().encode())}")
