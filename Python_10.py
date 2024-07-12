7ac527c51522e291864a2e6add0678d92e1f576a
beep boop <githubbot@vadim.me>
2024-07-12 14:30:37 +0200

    Solution 62 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3pjnfwz2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/29d45aeb-fef2-44de-958a-1ee34bdad 7b4.py", line 19, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/29d45aeb-fef2-44de-958a-1ee34bdad 7b4.py", line 16, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/29d45aeb-fef2-44de-958a-1ee34bdad 7b4.py", line 2, in make_palindrome     if is_palindrome(string):        ^^^^^^^^^^^^^ NameError: name 'is_palindrome' is not defined. Did you mean: 'palindrome'?

--- Python_10.py
+++ Python_10.py
@@ -1,12 +1,17 @@
+````
+def is_palindrome(s: str) -> bool:
+    return s == s[::-1]
+
 def make_palindrome(string: str) -> str:
     if is_palindrome(string):
         return string
     n = len(string)
     new_string = "#" + "#".join(list(string)) + "#"
-    while not is_palindrome(new_string):
-        new_string = new_string[:n] + "#"
-        n -= 1
     palindrome = ""
+    i_max = n - 1
+    while not is_palindrome(new_string):
+        new_string = new_string[:i_max+1] + "#"
+        i_max -= 1
     for i in range(n*2-1):
         if (i < n) or ((n * 2 - 1) > i >= n - 1 and i % 2 != 0):
             palindrome += new_string[i]
