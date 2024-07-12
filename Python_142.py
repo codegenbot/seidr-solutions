421feb5fc5afd921df7479bd49273aad0758f803
beep boop <githubbot@vadim.me>
2024-07-12 11:19:35 +0200

    Solution 59 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/y0gq3031 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/df524e6e-3df3-4f59-ae18-a826deaf8 e63.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,4 +1,7 @@
 ```
+def sum_squares(lst):
+    return sum(i**2 for i in lst)
+
 while True:
     try:
         while input("Do you want to start? (yes/no): ") != "yes":
@@ -6,7 +9,7 @@ while True:
         lst = input("Enter a list of numbers separated by space: ").split()
         int_lst = [int(i) for i in lst if i.lstrip("-").isdigit()]
         if all(-100 <= n <= 100 for n in int_lst):
-            print(sum([n**2 for n in int_lst]))
+            print(sum_squares(int_lst))
             break
         else:
             print("Some numbers are out of range.")
