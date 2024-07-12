5ede5f8bd73d47d6ad3c517eb74371ba9f78187d
beep boop <githubbot@vadim.me>
2024-07-12 23:17:26 +0200

    Solution 4 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/h4l69bke Prompt:       |                                      ^~~~~~ 51477984-84f5-4c9a-b333-cd20718f67e6.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | long long double_the_difference(std::vector<float> lst) { 51477984-84f5-4c9a-b333-cd20718f67e6.cpp:1:45: error: expected primary-expressio n before ‘float’     1 | long long double_the_difference(std::vector<float> lst) {       |                                             ^~~~~ In file included from 51477984-84f5-4c9a-b333-cd20718f67e6.cpp:11: 51477984-84f5-4c9a-b333-cd20718f67e6.cpp: In function ‘int main()’: 51477984-84f5-4c9a-b333-cd20718f67e6.cpp:13:35: error: ‘lst’ was not declared in  this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 51477984-84f5-4c9a-b333-cd20718f67e6.cpp:13:38: error: ‘double_the_difference’ c annot be used as a function    13 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ 51477984-84f5-4c9a-b333-cd20718f67e6.cpp:13:43: error: ‘odd_sum’ was not declare d in this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,9 +1,9 @@
 #include <vector>
 #include <cmath>
 
-long long double_the_difference(std::vector<float> lst) {
+long long double_the_difference(std::vector<double> lst) {
     long long sum = 0;
-    for (float num : lst) {
+    for (double num : lst) {
         if (num > 0 && modf(num, &num) == 0) {
             sum += pow(num, 2);
         }
@@ -12,7 +12,13 @@ long long double_the_difference(std::vector<float> lst) {
 }
 
 int main() {
-    std::vector<float> lst = {1.0, 2.0, 3.0};
-    int odd_sum = double_the_difference(lst); 
+    std::vector<double> lst = {1.5, 3.0, 4.25, 6.75};
+    double odd_sum = 0;
+    for (double num : lst) {
+        if (num > 0 && modf(num, &num) == 0) {
+            odd_sum += pow(num, 2);
+        }
+    }
+    assert(double_the_difference(lst) == odd_sum );
     return 0;
 }
