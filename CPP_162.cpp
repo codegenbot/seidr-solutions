bed7f6629bb2544b3b997ac700f13dce812f3e39
beep boop <githubbot@vadim.me>
2024-07-13 00:35:42 +0200

    Solution 2 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/qeqjd3zn Prompt: d2e99e97-8ea4-4587-ae4a-df0281fc8ce2.cpp:13:5: error: ‘MD5_Update’ was not decla red in this scope    13 |     MD5_Update(&ctx, text.c_str(), text.size());       |     ^~~~~~~~~~ d2e99e97-8ea4-4587-ae4a-df0281fc8ce2.cpp:14:5: error: ‘MD5_Final’ was not declar ed in this scope    14 |     MD5_Final(mdBuffer, &ctx);       |     ^~~~~~~~~ d2e99e97-8ea4-4587-ae4a-df0281fc8ce2.cpp:17:31: error: invalid conversion from ‘ const char*’ to ‘char*’ [-fpermissive]    17 |         sprintf(ss.str().c_str(), "%02x", mdBuffer[i]);       |                 ~~~~~~~~~~~~~~^~       |                               |       |                               const char* In file included from /usr/include/c++/11/cstdio:42,                  from /usr/include/c++/11/ext/string_conversions.h:43,                  from /usr/include/c++/11/bits/basic_string.h:6608,                  from /usr/include/c++/11/string:55,                  from d2e99e97-8ea4-4587-ae4a-df0281fc8ce2.cpp:1: /usr/include/stdio.h:358:38: note:   initializing argument 1 of ‘int sprintf(cha r*, const char*, ...)’   358 | extern int sprintf (char *__restrict __s,       |                     ~~~~~~~~~~~~~~~~~^~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <sstream>
-#include <md5.h>
+#include <iomanip> 
+#include <openssl/md5.h>
 
 using namespace std;
 
@@ -9,14 +10,14 @@ string string_to_md5(string text) {
 
     MD5_CTX ctx;
     unsigned char mdBuffer[16];
-    stringstream ss;
+    ostringstream ss; 
+
     MD5_Init(&ctx);
     MD5_Update(&ctx, text.c_str(), text.size());
     MD5_Final(mdBuffer, &ctx);
 
     for (int i = 0; i < 16; i++) {
-        sprintf(&ss.str()[0], "%02x", (char)mdBuffer[i]);
-        ss.seekp(0);
+        ss << setfill('0') << setw(2) << hex << static_cast<int>(mdBuffer[i]);
     }
 
     return ss.str();
