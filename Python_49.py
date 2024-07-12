e8a341b0ac9eb4fe40f45a96d90325cad35c4bfb
beep boop <githubbot@vadim.me>
2024-07-12 07:28:25 +0200

    Solution 68 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/d8swkc5z Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/003a86bf-5afa-4396-9385-c87597754 978.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,13 +1,13 @@
 ```
-def power_mod(a: int, n: int, p: int):
+def power_mod(a: int, n: int):
     if n < 0:
         a = pow(a, -1, p)
     elif n > 1:
-        a = (a * power_mod(a, n-1, p)) % p
+        a = (a * power_mod(a, n-1)) % p
     return a
 
 def modp(n: int, p: int):
     if p == 0:
         raise ValueError("Modulus cannot be zero")
-    result = power_mod(n, p, p)
+    result = power_mod(n, p)
     return result
