b4c2ee3bd1030150f130074fd56c76d45ea85bfa
beep boop <githubbot@vadim.me>
2024-07-12 10:13:04 +0200

    Solution 16 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/zoj3wa6d Prompt:       |                    ^~~~~ e8e2fac4-fcdf-4760-8514-e774a58eca9f.cpp:10:29: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ e8e2fac4-fcdf-4760-8514-e774a58eca9f.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ e8e2fac4-fcdf-4760-8514-e774a58eca9f.cpp:10:43: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from e8e2fac4-fcdf-4760-8514-e774a58eca9f.cpp:9: e8e2fac4-fcdf-4760-8514-e774a58eca9f.cpp: In function ‘int main()’: e8e2fac4-fcdf-4760-8514-e774a58eca9f.cpp:19:20: error: ‘get_positive’ was not de clared in this scope    19 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ e8e2fac4-fcdf-4760-8514-e774a58eca9f.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,7 +1,9 @@
 #include <vector>
-#include <cassert>
-std::vector<float> get_positive(std::vector<float> l) {
-    std::vector<float> result;
+
+using namespace std;
+
+vector<float> get_positive(vector<float> l) {
+    vector<float> result;
     for (float num : l) {
         if (num > 0) {
             result.push_back(num);
@@ -10,11 +12,11 @@ std::vector<float> get_positive(std::vector<float> l) {
     return result;
 }
 
-bool issame(std::vector<float> a, std::vector<float> b){
+bool issame(vector<float> a, vector<float> b) {
     return a == b;
 }
 
 int main() {
-    assert (issame(get_positive({}), {}));
+    assert(issame(get_positive(vector<float>{}), {})); 
     return 0;
 }
