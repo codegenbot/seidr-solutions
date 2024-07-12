8e3997f91ec664ff92bb5773ae58e8a295dd1b81
beep boop <githubbot@vadim.me>
2024-07-12 23:25:59 +0200

    Solution 71 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ndqvsqhd Prompt:       |                    ^~~ 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp:9:27: error: ‘vector’ was not declared in this scope     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp:9:39: error: expression list treated as  compound expression in initializer [-fpermissive]     9 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp:8: 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp: In function ‘int main()’: 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp:18:20: error: ‘make_a_pile’ was not dec lared in this scope    18 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,34 +1,16 @@
-```cpp
 #include <vector>
-#include <cassert>
-#include <iostream>
-#include <initializer_list>
 
-using namespace std;
-
-bool same(const vector<int>& a, const vector<int>& b) {
+bool same(const std::vector<int>& a, const std::vector<int>& b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
     return a == b;
 }
 
-vector<int> make_a_pile(int n) {
-    int stones = 1;
-    vector<int> pile;
-    for (int i = 0; i < n; i++) {
-        pile.push_back(stones);
-        stones++;
-    }
-    return pile;
-}
-
-vector<int> make_b_pile(int n) {
-    int stones = 2;
-    vector<int> pile;
-    for (int i = 0; i < n; i++) {
-        if ((i & 1) == 0)
-            pile.push_back(stones);
-        else
-            pile.push_back(1);
-        stones += 3;
+std::vector<int> make_a_pile(int n) {
+    std::vector<int> pile;
+    for (int i = 1; i <= n; ++i) {
+        pile.push_back(i);
     }
     return pile;
 }
