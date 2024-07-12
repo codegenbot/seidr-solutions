99dc548f2166be0eaaaad006519ace7ad76c962a
beep boop <githubbot@vadim.me>
2024-07-12 06:10:06 +0200

    Solution 33 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/iezgr6j6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e2b27250-d030-4c6f-b934-455b220fd 010.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_12.py
+++ Python_12.py
@@ -11,7 +11,7 @@ def longest_common_substring(str1: str, str2: str) -> Optional[str]:
                 if m[x][y] > longest:
                     longest = m[x][y]
                     x_longest = x
-    return str1[x_longest - longest: x_longest] if longest else ""
+    return str1[x_longest - longest - 1: x_longest]
 
 str1 = input("Enter the first string: ")
 str2 = input("Enter the second string: ")
