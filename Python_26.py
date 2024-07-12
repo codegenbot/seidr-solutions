80d517a82910fcb601b8c27a1584906866d94be7
beep boop <githubbot@vadim.me>
2024-07-12 06:17:05 +0200

    Solution 21 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/t61x2loc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3f979ec5-4ac5-421b-b0d6-f32b6d2bd 1b9.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_26.py
+++ Python_26.py
@@ -1,11 +1,16 @@
+```
 def remove_duplicates(input_numbers=None):
     if input_numbers is not None:
-        return list(set(input_numbers))
-    numbers = []
+        return list(dict.fromkeys(input_numbers))
+
+    numbers = set()
+    
     while True:
         user_num = int(input("Enter an integer (or 'q' to quit): "))
+        
         if str(user_num).lower() == 'q':
             break
-        if user_num not in numbers:
-            numbers.append(user_num)
-    return numbers
+        
+        numbers.add(user_num)
+    
+    return list(numbers)
