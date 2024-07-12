503950e6bafbdfaa7749e160199dbd953aa076c9
beep boop <githubbot@vadim.me>
2024-07-12 21:26:27 +0200

    Solution 8 of Python/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lzp5ko8j Prompt: Complete the following code given the task description and function signature.

--- Python_73.py
+++ Python_73.py
@@ -1,8 +1,7 @@
 def smallest_change(arr):
     n = len(arr)
     rev_arr = arr[::-1]
-    count = 0
-    for i in range(n):
-        if arr[i] != rev_arr[i]:
-            count += 1
-    return count
+
+    changes = sum(x != y for x, y in zip(arr, rev_arr))
+
+    return changes
