02e76a37f327618de7422cb14fe4139007015bbb
beep boop <githubbot@vadim.me>
2024-07-12 15:06:58 +0200

    Solution 45 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ag0c73oo Prompt: 28256d65-0bda-4602-b486-9ba99e28d9fc.cpp:12:15: error: expected ‘;’ before ‘mid2 ’    12 |         string mid2 = s.substr(len / 2, 1);       |               ^~~~~       |               ; 28256d65-0bda-4602-b486-9ba99e28d9fc.cpp:13:51: error: ‘mid1’ was not declared i n this scope    13 |         std::cout << "Middle characters are: " << mid1 + mid2 << std::en dl;       |                                                   ^~~~ 28256d65-0bda-4602-b486-9ba99e28d9fc.cpp:13:58: error: ‘mid2’ was not declared i n this scope    13 |         std::cout << "Middle characters are: " << mid1 + mid2 << std::en dl;       |                                                          ^~~~ 28256d65-0bda-4602-b486-9ba99e28d9fc.cpp:15:15: error: expected ‘;’ before ‘mid’    15 |         string mid = s.substr(len / 2, 1);       |               ^~~~       |               ; 28256d65-0bda-4602-b486-9ba99e28d9fc.cpp:16:49: error: ‘mid’ was not declared in  this scope    16 |         std::cout << "Middle character is: " << mid << std::endl;       |                                                 ^~~

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,18 +1,19 @@
+```cpp
 #include <string>
 #include <iostream>
 
 int main() {
-    string s;
+    std::string s;
     std::cout << "Enter a string: ";
     std::cin >> s;
 
     int len = s.length();
     if (len % 2 == 0) {
-        string mid1 = s.substr(len / 2 - 1, 1);
-        string mid2 = s.substr(len / 2, 1);
+        std::string mid1 = s.substr(len / 2 - 1, 1);
+        std::string mid2 = s.substr(len / 2, 1);
         std::cout << "Middle characters are: " << mid1 + mid2 << std::endl;
     } else {
-        string mid = s.substr(len / 2, 1);
+        std::string mid = s.substr(len / 2, 1);
         std::cout << "Middle character is: " << mid << std::endl;
     }
 
