b83e657853c7c4f8bc1191b96963006f8d0b1195
beep boop <githubbot@vadim.me>
2024-07-12 08:38:57 +0200

    Solution 93 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/3aegallq Prompt:    10 |             return accumulate(to_string(abs(num)).begin(), to_string(abs (num)).end(), 0,       |                               ^~~~~~~~~ In file included from 6c1faf9a-1727-4012-a8c6-fcbc506cdcb2.cpp:26: 6c1faf9a-1727-4012-a8c6-fcbc506cdcb2.cpp: In function ‘int main()’: 6c1faf9a-1727-4012-a8c6-fcbc506cdcb2.cpp:36:35: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    36 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 6c1faf9a-1727-4012-a8c6-fcbc506cdcb2.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 6c1faf9a-1727-4012-a8c6-fcbc506cdcb2.cpp:7:42: note:   initializing argument 1 o f ‘std::vector<int> order_by_points(std::vector<int>&)’     7 | vector<int> order_by_points(vector<int>& nums) {       |                             ~~~~~~~~~~~~~^~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -5,5 +5,18 @@
 using namespace std;
 
 vector<int> order_by_points(const vector<int>& nums) {
-    return {nums.begin(), nums.end()};
+    return accumulate(to_string(abs(num)).begin(), to_string(abs(num)).end(), 0,
+          [](int sum, char c) {
+              return sum + (c - '0');
+          });
+    };
+    int sumA = sumDigits(a);
+    int sumB = sumDigits(b);
+    if (sumA == sumB) {
+        return a < b;
+    } else {
+        return sumA < sumB;
+    }
+});
+    return nums;
 }
