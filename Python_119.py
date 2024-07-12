6f0ea42037f6443f86264751c7622b1e9e26f4d2
beep boop <githubbot@vadim.me>
2024-07-12 13:33:32 +0200

    Solution 3 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/aybhzbcy Prompt: Complete the following code given the task description and function signature.

--- Python_119.py
+++ Python_119.py
@@ -1,13 +1,2 @@
-```
 def match_parens(lst):
-    s1, s2 = lst
-    stack = []
-    for c in s1 + s2:
-        if c == '(':
-            stack.append(c)
-        elif c == ')':
-            if len(stack) > 0 and stack.pop() == '(':
-                continue
-            else:
-                return 'No'
-    return 'Yes' if not stack else 'No'
+    return 'Yes' if sum(1 for s in lst if s.count('(') != s.count(')')) == 0 else 'No'
