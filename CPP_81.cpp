afcdcca89baca98ca51108acc2387d122136b295
beep boop <githubbot@vadim.me>
2024-07-12 21:40:49 +0200

    Solution 74 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/b72cckkd Prompt:       |                                             ^ e181a33d-9b4a-412e-b0bc-f2f6a97abd1d.cpp:52:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    52 | int main(){       |         ^~ e181a33d-9b4a-412e-b0bc-f2f6a97abd1d.cpp:52:9: note: remove parentheses to defau lt-initialize a variable    52 | int main(){       |         ^~       |         -- e181a33d-9b4a-412e-b0bc-f2f6a97abd1d.cpp:52:9: note: or replace parentheses with  braces to value-initialize a variable e181a33d-9b4a-412e-b0bc-f2f6a97abd1d.cpp:52:11: error: a function-definition is not allowed here before ‘{’ token    52 | int main(){       |           ^ e181a33d-9b4a-412e-b0bc-f2f6a97abd1d.cpp:55:2: error: expected ‘}’ at end of inp ut    55 | }       |  ^ e181a33d-9b4a-412e-b0bc-f2f6a97abd1d.cpp:31:12: note: to match this ‘{’    31 | int main() {       |            ^

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,1 +1,14 @@
-int main {
+int main 
+{
+    std::cout << "Enter the first grade: ";
+    float grade1; std::cin >> grade1;
+    std::cout << "Enter the second grade: ";
+    float grade2; std::cin >> grade2;
+    std::vector<float> grades = {grade1, grade2};
+    std::vector<std::string> result = numerical_letter_grade({grade1, grade2});
+    if (!assertSame(result, {"E", "C+"})) 
+    {
+        assert(false);
+    }
+    return 0;
+}
