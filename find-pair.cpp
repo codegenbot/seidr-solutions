2de3b60fbdef151cb67da542e5c173bd82e73f41
beep boop <githubbot@vadim.me>
2024-07-12 14:48:35 +0200

    Solution 9 of find-pair, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/wq344pje Prompt:   +++ |+#include <iostream>     2 | using namespace std; c899bdca-26ff-42a4-ac5e-378c4365f081.cpp:28:5: error: ‘cout’ was not declared in  this scope    28 |     cout << "Enter the target sum: ";       |     ^~~~ c899bdca-26ff-42a4-ac5e-378c4365f081.cpp:28:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? c899bdca-26ff-42a4-ac5e-378c4365f081.cpp:32:30: error: ‘endl’ was not declared i n this scope    32 |         cout << result[0] << endl << result[1] << endl;       |                              ^~~~ c899bdca-26ff-42a4-ac5e-378c4365f081.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std; c899bdca-26ff-42a4-ac5e-378c4365f081.cpp:34:25: error: ‘endl’ was not declared i n this scope    34 |         cout << "-1" << endl << "-1" << endl;       |                         ^~~~ c899bdca-26ff-42a4-ac5e-378c4365f081.cpp:34:25: note: ‘std::endl’ is defined in header ‘<ostream>’; did you forget to ‘#include <ostream>’?

--- find-pair.cpp
+++ find-pair.cpp
@@ -5,30 +5,25 @@
 using namespace std;
 
 vector<int> findPair(vector<int>& nums, int target) {
-    vector<int> result;
     unordered_map<int, int> numMap;
-
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
         if (numMap.count(complement)) {
-            result.push_back(complement);
-            result.push_back(nums[i]);
-            break;
+            return {complement, nums[i]};
         }
         numMap[nums[i]] = i;
     }
-
-    return result;
+    return {};
 }
 
 int main() {
-    int n, x, y;
+    int n;
     cin >> n;
     vector<int> nums(n);
     for (int i = 0; i < n; i++) {
         cin >> nums[i];
     }
-    cout << "Enter the target sum: ";
+    int x;
     cin >> x;
     vector<int> result = findPair(nums, x);
     if (!result.empty()) {
