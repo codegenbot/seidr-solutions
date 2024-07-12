1cea9b82b4b491c0e3835b1641c1a9ce28f41496
beep boop <githubbot@vadim.me>
2024-07-12 22:25:09 +0200

    Solution 3 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/y3xpzjxs Prompt: bf64dd13-8a6c-4eda-b056-8ccf375a5911.cpp:3:17: error: ‘vector’ was not declared in this scope     3 | int sum_squares(vector<float> lst){       |                 ^~~~~~ bf64dd13-8a6c-4eda-b056-8ccf375a5911.cpp:3:17: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from bf64dd13-8a6c-4eda-b056-8ccf375a5911.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from bf64dd13-8a6c-4eda-b056-8ccf375a5911.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ bf64dd13-8a6c-4eda-b056-8ccf375a5911.cpp:3:24: error: expected primary-expressio n before ‘float’     3 | int sum_squares(vector<float> lst){       |                        ^~~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,15 +1,8 @@
 #include <vector>
 
-using namespace std;
-
-int sum_squares(vector<float> lst){
+int sum_squares(std::vector<float> lst){
     int total = 0;
     for(float x : lst){
         total += ceil(x)*ceil(x);
     }
     return total;
-}
-
-int main(){
-    assert(sum_squares({-1,1,0})==2);
-}
