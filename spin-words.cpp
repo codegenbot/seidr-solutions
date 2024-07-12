966689d87b66aa54ab5f63049835bab9f016489b
beep boop <githubbot@vadim.me>
2024-07-12 23:23:04 +0200

    Solution 19 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/xbibyc5v Prompt: it must return yeawtwlw g xe n for input wlwtwaey g xe n, but it returns .

--- spin-words.cpp
+++ spin-words.cpp
@@ -8,11 +8,11 @@ using namespace std;
 string spinWords(string str) {
     string result = "";
     
-    int wordStart = 0;
-    
     for(int i=0; i<str.length(); i++) {
+        string word;
+        
         if(i == str.length() - 1) {
-            string word = str.substr(wordStart, i-wordStart);
+            word = str.substr(i);
             
             if(word.length() >= 5) {
                 reverse(word.begin(), word.end());
@@ -21,7 +21,7 @@ string spinWords(string str) {
             result += word + " ";
             break;
         } else if (str[i] == ' ') {
-            string word = str.substr(wordStart, i-wordStart);
+            word = str.substr(wordStart, i-wordStart);
 
             if(word.length() >= 5) {
                 reverse(word.begin(), word.end());
@@ -30,7 +30,8 @@ string spinWords(string str) {
             result += word + " ";
             wordStart = i+1;
         } else {
-            wordStart++;
+            word += str[i];
+            wordStart = i;
         }
     }
     
@@ -47,4 +48,3 @@ int main() {
     cout << "Output: " << output << endl;
 
     return 0;
-}
