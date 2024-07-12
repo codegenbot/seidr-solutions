b70aa0217d98ba3d36f84ebd179d136d65f5be7b
beep boop <githubbot@vadim.me>
2024-07-12 15:22:38 +0200

    Solution 26 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/o34xz3qi Prompt: 3a00768b-6524-415b-a322-9b2151b34a6e.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3a00768b-6524-415b-a322-9b2151b34a6e.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 3a00768b-6524-415b-a322-9b2151b34a6e.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from 3a00768b-6524-415b-a322-9b2151b34a6e.cpp:28: 3a00768b-6524-415b-a322-9b2151b34a6e.cpp: In function ‘int main()’: 3a00768b-6524-415b-a322-9b2151b34a6e.cpp:38:31: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    38 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ~~~~~~~~~~~^~~ 3a00768b-6524-415b-a322-9b2151b34a6e.cpp:4:31: note:   initializing argument 1 o f ‘bool issame(std::vector<int>&, std::vector<int>&)’     4 | bool issame(std::vector<int>& a, std::vector<int>& b) {       |             ~~~~~~~~~~~~~~~~~~^

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 
-bool isSame(std::vector<int>& a, std::vector<int>& b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i < a.size(); i++)
@@ -22,6 +22,6 @@ std::vector<int> make_a_pile(int n) {
 }
 
 int main() {
-    assert(isSame(make_a_pile(8), std::vector<int>{1,3,5,7,9,11,13,15}) );
+    assert(issame(make_a_pile(8), std::vector<int>{1,3,5,7,9,11,13,15}) );
     return 0;
 }
