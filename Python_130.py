71b4f8f8e84cf70a37c421620dcd71fc155319c2
beep boop <githubbot@vadim.me>
2024-07-12 04:04:13 +0200

    Solution 6 of Python/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ludmgl91 Prompt: Complete the following code given the task description and function signature.

--- Python_130.py
+++ Python_130.py
@@ -6,11 +6,7 @@ def tri(n):
     elif n == 2:
         return [3, 2]
     else:
-        tribonacci = [3, 2]
-        for i in range(2, n):
-            if i % 2 == 0:
-                tribonacci.append(int((i + 1) / 2))
-            else:
-                a, b = tribonacci[-2], tribonacci[-1]
-                tribonacci.append(a + b + int((i + 3) / 2))
-        return tribonacci
+        sequence = [3, 2]
+        for _ in range(2, n):
+            sequence.append(sum(sequence[-2:]))
+        return sequence
