b21c2d7e312101e723fb77c0462d1a38e4e51060
beep boop <githubbot@vadim.me>
2024-07-12 07:18:08 +0200

    Solution 74 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zzjubszw Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/34c0d7cd-8801-49f3-9a04-68c18218c 1f6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_0.py
+++ Python_0.py
@@ -38,10 +38,6 @@ else:
             print("Invalid input. Please enter a valid number.")
 
     if not has_close_elements(numbers, threshold):
-        print(
-            f"The numbers {', '.join(map(str, numbers))} have close elements for the given threshold {threshold}."
-        )
+        print(f"The numbers {', '.join(map(str, numbers))} have close elements for the given threshold {threshold}.")
     else:
-        print(
-            f"The numbers {', '.join(map(str, numbers))} do not have close elements for the given threshold {threshold}."
-        )
+        print(f"The numbers {', '.join(map(str, numbers))} do not have close elements for the given threshold {threshold}."
