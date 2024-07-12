003298e8c213f2380f61329a67fdc140e272f4eb
beep boop <githubbot@vadim.me>
2024-07-13 01:09:58 +0200

    Solution 95 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/086csx52 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ d94fe65b-f66b-45e3-9ae1-d7fb324fb677.cpp:72:27:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,90 +1,73 @@
+```cpp
 #include <iostream>
 #include <vector>
 
 std::string exchange(const std::vector<int>& lst1, const std::vector<int>& lst2);
 
 int main() {
-    int n1 = 0;
-    bool flag = false;
-    while (flag == false) {
-        try {
-            std::cout << "Enter the number of elements in lst1: ";
-            std::cin >> n1;
-            flag = true;
-        } catch (...) {
-            std::cout << "Invalid input. Please enter a positive integer: ";
-            std::cin.clear();
-            std::cin.ignore();
-        }
+    long n1;
+    while (!(std::cin >> n1)) {
+        std::cout << "Invalid input. Please enter a positive integer: ";
+        std::cin.clear();
+        std::cin.ignore();
+        std::cin >> n1;
     }
 
-    int m1 = 0;
-    flag = false;
-    while (flag == false) {
-        try {
-            std::cout << "Enter the number of elements in lst2: ";
-            std::cin >> m1;
-            flag = true;
-        } catch (...) {
-            std::cout << "Invalid input. Please enter a positive integer: ";
-            std::cin.clear();
-            std::cin.ignore();
-        }
+    long m1 = 0;
+    while (!(std::cin >> m1)) {
+        std::cout << "Invalid input. Please enter a positive integer: ";
+        std::cin.clear();
+        std::cin.ignore();
+        std::cin >> m1;
     }
 
-    int n2 = 0;
-    flag = false;
-    while (flag == false) {
-        try {
-            std::cout << "Enter the number of elements in lst1: ";
-            std::cin >> n2;
-            flag = true;
-        } catch (...) {
-            std::cout << "Invalid input. Please enter a positive integer: ";
-            std::cin.clear();
-            std::cin.ignore();
-        }
+    int n2, m2;
+    n2 = m1; 
+    while (!(std::cin >> n2)) {
+        std::cout << "Invalid input. Please enter a positive integer: ";
+        std::cin.clear();
+        std::cin.ignore();
+        std::cin >> n2;
     }
 
-    if (m1 < n1) {
-        std::cout << "You need to enter " << n1 - m1 << " more elements.\n";
+    if (m2 < m1) {
+        std::cout << "You need to enter " << m2 - m1 << " more elements.\n";
     } else {
-        return 0;
-    }
-
-    int m2 = 0;
-    flag = false;
-    while (flag == false) {
-        try {
+        int temp = -1;
+        while (temp == -1) {
             std::cout << "Enter the number of elements in lst2: ";
-            std::cin >> m2;
-            flag = true;
-        } catch (...) {
-            std::cout << "Invalid input. Please enter a positive integer: ";
-            std::cin.clear();
-            std::cin.ignore();
+            try {
+                std::cin >> temp;
+            } catch (...) {
+                std::cout << "Invalid input. Please enter a positive integer: ";
+                std::cin.clear();
+                std::cin.ignore();
+                std::cin >> temp;
+            }
+        }
+
+        if (temp < m1) {
+            std::cout << "You need to enter " << m1 - temp << " more elements.\n";
+        } else {
+            return 0;
         }
     }
 
-    if (m2 < n1) {
-        std::cout << "You need to enter " << n1 - m2 << " more elements.\n";
-    } else {
-        return 0;
-    }
-
+    int m = m2; 
     std::vector<int> lst1;
     for (int i = 0; i < n1; i++) {
-        int temp = 0;
-        do {
+        int temp = -1;
+        while (temp == -1) {
+            std::cout << "Enter element of lst1: ";
             try {
-                std::cout << "Enter element of lst1: ";
                 std::cin >> temp;
             } catch (...) {
                 std::cout << "Invalid input. Please enter an integer: ";
                 std::cin.clear();
                 std::cin.ignore();
+                std::cin >> temp;
             }
-        } while (temp == 0);
+        }
         if (temp != 0) {
             lst1.push_back(temp);
         }
@@ -93,17 +76,18 @@ int main() {
     int m = m2; 
     std::vector<int> lst2;
     for (int i = 0; i < m; i++) {
-        int temp = 0;
-        do {
+        int temp = -1;
+        while (temp == -1) {
+            std::cout << "Enter element of lst2: ";
             try {
-                std::cout << "Enter element of lst2: ";
                 std::cin >> temp;
             } catch (...) {
                 std::cout << "Invalid input. Please enter an integer: ";
                 std::cin.clear();
                 std::cin.ignore();
+                std::cin >> temp;
             }
-        } while (temp == 0);
+        }
         if (temp != 0) {
             lst2.push_back(temp);
         }
@@ -118,15 +102,15 @@ int main() {
 }
 
 std::string exchange(const std::vector<int>& lst1, const std::vector<int>& lst2) {
-    int oddCount = 0;
-    for (int i : lst1) {
-        if (i % 2 != 0) {
-            oddCount++;
+    if (lst1.size() != lst2.size()) {
+        return "Lists are not of the same size";
+    }
+
+    for (int i = 0; i < lst1.size(); i++) {
+        if (lst1[i] != lst2[i]) {
+            return "Lists are not equal";
         }
     }
-    if (oddCount > 0) {
-        return "Odd";
-    } else {
-        return "Even";
-    }
+
+    return "Lists are equal";
 }
