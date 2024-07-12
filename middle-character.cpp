5adef42c692060f72bf1d4ddcfd863dee93da882
beep boop <githubbot@vadim.me>
2024-07-12 14:04:16 +0200

    Solution 18 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ag0c73oo Prompt: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::__cxx11::basi c_string<_CharT, _Traits, _Alloc>::basic_string(std::__cxx11::basic_string<_Char T, _Traits, _Alloc>&&) [with _CharT = char; _Traits = std::char_traits<char>; _A lloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:6667:12:   required from here /usr/include/c++/11/bits/basic_string.h:576:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   576 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,17 +1,21 @@
+```cpp
 #include <string>
 using namespace std;
 
 int main() {
-    string s = "your test string here";
-    cout << middleCharacter(s) << endl;
-    return 0;
-}
-
-string middleCharacter(string s) {
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
+    
     int len = s.length();
     if (len % 2 == 0) {
-        return s.substr(len / 2 - 1, 2);
+        string res = s.substr(len / 2 - 1, 1);
+        res += s.substr(len / 2, 1);
+        cout << "Middle characters are: " << res << endl;
     } else {
-        return s.substr(len / 2, 1);
+        string res = s.substr(len / 2, 1);
+        cout << "Middle character is: " << res << endl;
     }
+    
+    return 0;
 }
