c36421f23f72326bf9ce0ddccc74fa073aae067a
beep boop <githubbot@vadim.me>
2024-07-12 08:44:17 +0200

    Solution 26 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2l83sobi Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/174aeeff-6f04-4bf4-9d18-2efd9bf69 ddc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,7 +1,7 @@
 ```
 def double_the_difference(lst):
-    even_sum = sum(i for i in lst if i % 2 == 0)
-    odd_sum = sum(i for i in lst if i % 2 != 0)
+    even_sum = sum([i for i in lst if i % 2 == 0])
+    odd_sum = sum([i for i in lst if i % 2 != 0])
     return (even_sum - odd_sum) * 2
 
 def check(lst):
