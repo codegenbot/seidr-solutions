5169e339585b4a39c2571d8499fc85402afae8e7
beep boop <githubbot@vadim.me>
2024-07-12 19:28:51 +0200

    Solution 29 of CPP/31, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/cfqxaqy5 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_31.cpp
+++ CPP_31.cpp
@@ -1,7 +1,9 @@
+```cpp
 #include <iostream>
 using namespace std;
 
-bool is_prime(int num) {
+bool is_prime(string num_str) {
+    int num = stoi(num_str);
     if (num <= 1)
         return false;
     for (int i = 2; i * i <= num; i++) {
@@ -12,15 +14,7 @@ bool is_prime(int num) {
 }
 
 int main() {
-    while(true) {
-        int num;
-        cout << "Enter a number: ";
-        cin >> num;
-        if (cin.fail()) {
-            cout << "Invalid input. Try again." << endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        } else break;
-    }
-    cout << (is_prime(num) ? "True" : "False") << endl;
+    string num_str;
+    cin >> num_str;
+    cout << (is_prime(num_str) ? "True" : "False") << endl;
 }
