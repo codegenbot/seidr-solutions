7f86ace0e1c9187d5d7516b889f9d95fd51e0e07
beep boop <githubbot@vadim.me>
2024-07-12 05:40:12 +0200

    Solution 4 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g167l76a Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/00a9a170-e52b-4afb-9553-3598e535c 5e0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_19.py
+++ Python_19.py
@@ -9,7 +9,11 @@ def sort_numbers(numbers: str) -> str:
         "six": 6,
         "seven": 7,
         "eight": 8,
-        "nine": 9,
+        "nine": 9
     }
-    sorted_nums = sorted([num_dict[i] for i in numbers.split()])
+    nums_to_sort = []
+    for num in numbers.split():
+        if num in num_dict:
+            nums_to_sort.append(num_dict[num])
+    sorted_nums = sorted(nums_to_sort)
     return " ".join([str(k) for _, k in [(k, v) for k, v in num_dict.items() if v == i] for i in sorted_nums])
