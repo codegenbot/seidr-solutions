8645ea3580aa42e478195f9387d386083893cb3a
beep boop <githubbot@vadim.me>
2024-07-12 18:13:19 +0200

    Solution 94 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qerfd1ru Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/62304b8b-a3c7-4f77-9d17-c3c22457c bac.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/62304b8b-a3c7-4f77-9d17-c3c22457c bac.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,3 +1,3 @@
 def sort_even(l: list):
-    even = sorted([i for i in map(int, l) if int(i) % 2 == 0])
-    return [i if str(int(i)) % 2 != 0 else j for i, j in zip(even, map(int, l))]
+    even = sorted([i for i in l if isinstance(i, int) and i % 2 == 0])
+    return [i if not isinstance(i, int) or i % 2 != 0 else j for i, j in zip(even, l)]
