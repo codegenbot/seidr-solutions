b82c4153a88e11bad60381a2457872b347b1391c
beep boop <githubbot@vadim.me>
2024-07-12 14:38:51 +0200

    Solution 32 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d8rx6wrb Prompt:  in this scope    33 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9ef91a99-d2cd-4019-a009-635e5997fdcd.cpp:33:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 9ef91a99-d2cd-4019-a009-635e5997fdcd.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 9ef91a99-d2cd-4019-a009-635e5997fdcd.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9ef91a99-d2cd-4019-a009-635e5997fdcd.cpp:33:34: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,31 +1,3 @@
-```cpp
-#include <algorithm>
-#include <vector>
-#include <assert.h>
-
-bool same(std::vector<int> a, std::vector<int> b) {
-    return (a == b);
-}
-
-std::vector<int> sort_third(std::vector<int> l) {
-    std::vector<int> result;
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 3 == 0) {
-            // values at the indices that are divisible by three are equal to the values of the corresponding indicies of l, but sorted
-            std::vector<int> temp;
-            for (int j = i; j < l.size() && j % 3 == 0; j++) {
-                temp.push_back(l[j]);
-            }
-            if (!same(temp, result)) {
-                assert(false);
-            }
-            std::sort(temp.begin(), temp.end());
-            for (int k = 0; k < temp.size(); k++) {
-                result.push_back(temp[k]);
-            }
-        } else {
-            result.push_back(l[i]);
-        }
-    }
-    return result;
+bool isequal(std::vector<int> a) {
+    return (a == result);
 }
