e4c89e2e6abae769484d7466ed77bf6c0e64e0f5
beep boop <githubbot@vadim.me>
2024-07-12 07:34:20 +0200

    Solution 21 of Python/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zx5e9imf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a6dee283-75af-489d-8713-96476f597 87d.py", line 22, in <module>     check(fibfib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a6dee283-75af-489d-8713-96476f597 87d.py", line 19, in check     assert fibfib(14) == 927            ^^^^^^^^^^^^^^^^^ AssertionError

--- Python_63.py
+++ Python_63.py
@@ -1,12 +1,17 @@
 def fibfib(n: int):
     if n <= 1:
         return 0
-    a, b = 0, 1
-    total_sum = 0
-    while True:
-        if b > n:
-            break
-        if b % 2 == 0:
-            total_sum += b
-        a, b = b, a+b
-    return total_sum
+    elif n == 2:
+        return 1
+    else:
+        a, b = 0, 1
+        total_sum = 0
+        seen = {0, 1}  
+        while True:
+            if b % 2 == 0 and b not in seen:  # check if the number is even and hasn't been added before
+                total_sum += b
+                seen.add(b)  
+            a, b = b, a+b
+            if b > n:
+                break
+        return total_sum
