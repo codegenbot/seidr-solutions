a01831e572484ce3242a98c37902d71fc5eb4708
beep boop <githubbot@vadim.me>
2024-07-12 07:09:25 +0200

    Solution 68 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h3qtxl8o Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/abe2ac04-eede-4a86-8b23-0eebf0b59 d55.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,12 +1,9 @@
-```
+```Python
 import random
 
 def make_palindrome():
-    word = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(5))
-    if random.randint(0,1):
-        word += word[::-1]
-    else:
-        word = word + ''.join(reversed(word))
+    words = ["madam", "mom", "dad", "noon"]
+    word = random.choice(words)
     return word.capitalize()
 
 while True:
