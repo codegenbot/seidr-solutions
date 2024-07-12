3b26e5216900832023b7f569a68737c15a3d6c71
beep boop <githubbot@vadim.me>
2024-07-12 18:57:11 +0200

    Solution 29 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0hrfoi8h Prompt: f0a0cec6-b5c2-410b-80b5-a844a8934961.cpp:43:34: error: expected primary-expressi on before ‘int’    43 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f0a0cec6-b5c2-410b-80b5-a844a8934961.cpp:43:39: error: expression list treated a s compound expression in initializer [-fpermissive]    43 | bool issame(vector<int> a,vector<int>b){       |                                       ^ f0a0cec6-b5c2-410b-80b5-a844a8934961.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ f0a0cec6-b5c2-410b-80b5-a844a8934961.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from f0a0cec6-b5c2-410b-80b5-a844a8934961.cpp:42: f0a0cec6-b5c2-410b-80b5-a844a8934961.cpp: In function ‘int main()’: f0a0cec6-b5c2-410b-80b5-a844a8934961.cpp:52:19: error: ‘issame’ cannot be used a s a function    52 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -34,6 +34,7 @@ int main() {
         std::cout << "Maximum up to " << i + 1 << " is " << maxNumbers[i] << std::endl;
     }
 
-    assert(issame(rolling_max({3, 2, 3, 100, 3}), {3, 3, 3, 100, 100}));
+    assert(issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100}));
+    
     return 0;
 }
