01269731fa2f64665ffecdedefde18a49803696f
beep boop <githubbot@vadim.me>
2024-07-12 12:42:47 +0200

    Solution 78 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/duil6uim Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fbd4818a-af4e-4b1c-8696-371e5edc8 1b5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -6,7 +6,10 @@ def encode(message):
             if char.lower() == 'a' or char.lower() == 'e' or char.lower() == 'i' or char.lower() == 'o' or char.lower() == 'u':
                 result += chr((ord(char) - 97 + 3) % 26 + 97)
             else:
-                result += char.upper() if char.islower() else char.lower()
+                if char.islower():
+                    result += 'z-'[char<'m'] + chr((ord(char) - 97 + 1) % 26 + 97)
+                else:
+                    result += 'Z-'[char<'M'] + chr((ord(char) - 65 + 1) % 26 + 65)
         else:
             result += char
     return result
