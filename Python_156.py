58b16295267a1270f24a58efab434a2e0c9b0e8a
beep boop <githubbot@vadim.me>
2024-07-12 04:46:18 +0200

    Solution 3 of Python/156, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/agy5yyds Prompt: Complete the following code given the task description and function signature.

--- Python_156.py
+++ Python_156.py
@@ -1,23 +1,22 @@
-```
 def int_to_mini_roman(number):
-    val = [
-        1000, 900, 500, 400,
-        100, 90, 50, 40,
-        10, 9, 5, 4,
-        1
-    ]
-    syb = [
-        "M", "CM", "D", "CD",
-        "C", "XC", "L", "XL",
-        "X", "IX", "V", "IV",
-        "I"
-    ]
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    mini_roman = ""
 
-    roman_num = ''
-    i = 0
-    while number > 0:
-        for _ in range(number // val[i]):
-            roman_num += syb[i]
-            number -= val[i]
-        i += 1
-    return roman_num.lower()
+    for i in range(0, number):
+        if i < 10:
+            mini_roman += "i" * (i + 1)
+        elif i < 50:
+            mini_roman += "l" if i == 40 or i == 50 else "iv" * (i // 4 + 1)
+        elif i < 1000:
+            mini_roman += (
+                "m"
+                if i == 900 or i == 500 or i == 400 or i == 1000
+                else (
+                    "c"
+                    if i >= 90 and i <= 99 or i == 50
+                    else "d" if i == 500 else "vii" * (i // 7 + 1)
+                )
+            )
+        else:
+            return "number out of range"
+    return mini_roman.lower()
