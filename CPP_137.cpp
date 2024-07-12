915857d359534e9bd95a5d5a0bfe59bbbfbefc00
beep boop <githubbot@vadim.me>
2024-07-12 18:11:22 +0200

    Solution 15 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dhj7i5v3 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,50 +1,13 @@
-#include<stdio.h>
-#include<string>
-#include<algorithm>
-#include<boost/any.hpp>
-using namespace std;
-boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        if (get<int>(a) > get<float>(b))
-            return a;
-        else if (get<float>(b) > get<int>(a))
-            return b;
-        else
-            return boost::any("None");
-    } 
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        if (get<int>(a) > stod(get<string>(b)))
-            return a;
-        else if (stod(get<string>(b)) > get<int>(a))
-            return b;
-        else
-            return boost::any("None");
-    } 
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        if (get<float>(a) > stod(get<string>(b)))
-            return a;
-        else if (stod(get<string>(b)) > get<float>(a))
-            return b;
-        else
-            return boost::any("None");
-    } 
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        if (stod(get<string>(a)) > get<int>(b))
-            return a;
-        else if (get<int>(b) > stod(get<string>(a)))
-            return b;
-        else
-            return boost::any("None");
-    } 
-    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        if (stod(get<string>(a)) > get<float>(b))
-            return a;
-        else if (get<float>(b) > stod(get<string>(a)))
-            return b;
-        else
-            return boost::any("None");
-    } 
-    else {
-        return boost::any("None");
-    }
+if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
+    return a;
+} else if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
+    return a;
+} else if (boost::any_cast<std::string>(a) > boost::any_cast<std::string>(b)) {
+    return a;
+} else if (boost::any_cast<int>(a) == boost::any_cast<int>(b)) {
+    return "None";
+} else if (boost::any_cast<float>(a) == boost::any_cast<float>(b)) {
+    return "None";
+} else {
+    return b;
 }
