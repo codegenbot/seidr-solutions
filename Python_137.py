4f2d9d90f9739c1c43269ef3442b9a60308a8cea
beep boop <githubbot@vadim.me>
2024-07-12 18:25:32 +0200

    Solution 29 of Python/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/a6y0mb3q Prompt: Complete the following code given the task description and function signature.

--- Python_137.py
+++ Python_137.py
@@ -1,9 +1,10 @@
 ```
 def compare_one(a, b):
     if isinstance(a, str) and isinstance(b, (int, float)):
-        a = str(a).replace('.', '').replace(',', '.').format('{:.0f}')
+        a = str(a).replace('.', '').replace(',', '.').format(float)
     elif isinstance(b, str) and isinstance(a, (int, float)):
-        b = str(b).replace('.', '').replace(',', '.').format('{:.0f}')
+        b = str(b).replace('.', '').replace(',', '.').format(float)
+
     if a > b:
         return a
     elif a < b:
