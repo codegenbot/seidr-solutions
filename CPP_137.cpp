919feae325514f1b234cd9afeb49b5e9f7d2d540
beep boop <githubbot@vadim.me>
2024-07-12 18:00:06 +0200

    Solution 7 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dhj7i5v3 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,21 +1,35 @@
 if (a.type() == typeid(int) && b.type() == typeid(int)) {
-    int x = boost::any_cast<int>(a);
-    int y = boost::any_cast<int>(b);
-    if (x > y) return a;
-    else if (x < y) return b;
-    else return boost::any("None");
+    if (get<int>(a) > get<int>(b))
+        return a;
+    else if (get<int>(a) < get<int>(b))
+        return b;
+    else
+        return boost::any("None");
 } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-    float x = boost::any_cast<float>(a);
-    float y = boost::any_cast<float>(b);
-    if (x > y) return a;
-    else if (x < y) return b;
-    else return boost::any("None");
-} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-    string x = boost::any_cast<string>(a);
-    string y = boost::any_cast<string>(b);
-    if (stof(x) > stof(y)) return a;
-    else if (stof(x) < stof(y)) return b;
-    else return boost::any("None");
+    if (get<float>(a) > get<float>(b))
+        return a;
+    else if (get<float>(a) < get<float>(b))
+        return b;
+    else
+        return boost::any("None");
+} else if ((a.type() == typeid(string) && b.type() == typeid(int)) || 
+           (a.type() == typeid(int) && b.type() == typeid(string))) {
+    if (get<string>(a).compare(get<string>(b) + "") > 0)
+        return a;
+    else if (get<string>(a).compare(get<string>(b) + "") < 0)
+        return b;
+    else
+        return boost::any("None");
+} else if ((a.type() == typeid(float) && b.type() == typeid(string)) || 
+           (a.type() == typeid(string) && b.type() == typeid(float))) {
+    string str = get<string>(b);
+    float f = stof(str);
+    if (get<float>(a) > f)
+        return a;
+    else if (get<float>(a) < f)
+        return b;
+    else
+        return boost::any("None");
 } else {
     return boost::any("None");
 }
