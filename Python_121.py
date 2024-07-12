fc228d51cd12edbff3157f4ea188cedb196e906d
beep boop <githubbot@vadim.me>
2024-07-12 08:28:53 +0200

    Solution 27 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ccbjq8ug Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1fb7b16e-0c1e-42af-a807-904936995 c36.py", line 17, in <module>     check(solution)           ^^^^^^^^ NameError: name 'solution' is not defined. Did you mean: 'Solution'?

--- Python_121.py
+++ Python_121.py
@@ -1,9 +1,9 @@
+```
 class Solution:
     def count_odd_numbers(self):
         while True:
             try:
                 lst = [int(x) for x in input("Enter numbers separated by space: ").split()]
-                odd_count = len([i for i in lst if i % 2 != 0])
-                return odd_count
+                return len([i for i in lst if i % 2 != 0])
             except ValueError:
                 print("Invalid input. Please enter numbers separated by spaces.")
