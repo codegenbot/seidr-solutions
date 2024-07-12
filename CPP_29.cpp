d26fd4679791c909768718c1f19a80fbfb9daebf
beep boop <githubbot@vadim.me>
2024-07-12 10:27:19 +0200

    Solution 42 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/21owtffi Prompt: 4fdd4f2e-1e22-4354-96c1-11e98a3749ee.cpp:30:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    30 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 4fdd4f2e-1e22-4354-96c1-11e98a3749ee.cpp:7:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     7 | bool issame(vector<string> a, vector<string> b){       |      ^~~~~~ 4fdd4f2e-1e22-4354-96c1-11e98a3749ee.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 4fdd4f2e-1e22-4354-96c1-11e98a3749ee.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main(){       |     ^~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -6,16 +6,13 @@ using namespace std;
 
 vector<string> filter_by_prefix(vector<string> strings, string prefix);
 
-int main()
-{
-    vector<string> result = filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx");
-    assert(result == vector<string>{"xxx", "xxxAAA", "xxx"});
+void main(){
+    assert(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx") == vector<string>{"xxx", "xxxAAA", "xxx"});
     
     return 0;
 }
 
-vector<string> filter_by_prefix(vector<string> strings, string prefix)
-{
+vector<string> filter_by_prefix(vector<string> strings, string prefix){
     vector<string> result;
     for(const string &str : strings) {
         if(str.find(prefix) == 0) {
