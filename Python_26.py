bc7da66be6beb6a5588ad3ffd8761068dd23838e
beep boop <githubbot@vadim.me>
2024-07-12 07:21:03 +0200

    Solution 79 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/t61x2loc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c5e863d1-aa71-4df0-92e3-0875f67ed 4d9.py", line 11, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c5e863d1-aa71-4df0-92e3-0875f67ed 4d9.py", line 8, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_26.py
+++ Python_26.py
@@ -3,5 +3,5 @@ def remove_duplicates(input_numbers=None):
     if input_numbers is None:
         input_str = input("Enter numbers separated by space: ")
         input_numbers = list(map(int, input_str.split()))
-    unique_numbers = [x for i, x in enumerate(input_numbers) if x not in input_numbers[:i]]
-    return unique_numbers
+    unique_numbers = list(set(input_numbers))
+    return sorted(unique_numbers)
