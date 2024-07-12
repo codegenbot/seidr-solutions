79dbe36cf52db630eccd04bdaf7acab767775016
beep boop <githubbot@vadim.me>
2024-07-12 23:51:20 +0200

    Solution 15 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uswfokmt Prompt:       |             ^~~~~~ e564d0c6-628c-4fac-acc4-55788f5f5020.cpp:42:34: error: expected primary-expressi on before ‘int’    42 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e564d0c6-628c-4fac-acc4-55788f5f5020.cpp:42:39: error: expression list treated a s compound expression in initializer [-fpermissive]    42 | bool issame(vector<int> a,vector<int>b){       |                                       ^ e564d0c6-628c-4fac-acc4-55788f5f5020.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ e564d0c6-628c-4fac-acc4-55788f5f5020.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~ In file included from e564d0c6-628c-4fac-acc4-55788f5f5020.cpp:41: e564d0c6-628c-4fac-acc4-55788f5f5020.cpp: In function ‘int main()’: e564d0c6-628c-4fac-acc4-55788f5f5020.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,16 +1,16 @@
 #include <vector>
 #include <iostream>
 
-bool issame(vector<int> a, vector<int> b) {
+bool isSame(std::vector<int> a, std::vector<int> b) {
     for(int i = 0; i < a.size();i++){
         if(a[i] != b[i]) return false;
     }
     return true;
 }
 
-vector<int> compare(vector<int> game, vector<int> guess) {
-    vector<int> result;
-    if(issame(game,guess)){
+std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
+    std::vector<int> result;
+    if(isSame(game,guess)){
         for (int i = 0; i < game.size(); i++) {
             result.push_back(2);
         }
@@ -28,12 +28,12 @@ vector<int> compare(vector<int> game, vector<int> guess) {
 
 int main() {
     int n;
-    cin >> n;
-    vector<int> game(n),guess(n);
+    std::cin >> n;
+    std::vector<int> game(n),guess(n);
     for(int i = 0; i < n;i++){
-        cin >> game[i] >> guess[i];
+        std::cin >> game[i] >> guess[i];
     }
-    vector<int> res = compare(game,guess);
-    for(auto x:res)cout << x << " ";
+    std::vector<int> res = compare(game,guess);
+    for(auto x:res)std::cout << x << " ";
     return 0;
 }
