b9cb857654a0f35250327eacf186a13e02108bb6
beep boop <githubbot@vadim.me>
2024-07-12 06:59:33 +0200

    Solution 9 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/d8swkc5z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/62c4af07-88d3-4bf7-bdc5-c0120d6cd 6d7.py", line 10, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/62c4af07-88d3-4bf7-bdc5-c0120d6cd 6d7.py", line 7, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/62c4af07-88d3-4bf7-bdc5-c0120d6cd 6d7.py", line 2, in modp     if gcd(p, n) == 1:        ^^^ NameError: name 'gcd' is not defined

--- Python_49.py
+++ Python_49.py
@@ -1,10 +1,11 @@
-def modp(n: int, p: int):
-    if gcd(p, n) == 1:
-        return pow(n, (p-1)//gcd(p, n), p) * n % p
-    else:
-        return None
-
-def gcd(a: int, b: int):
+```
+def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
+
+def modp(n: int, p: int):
+    if gcd(p, n) == 1:
+        return pow(n, p-1, p) * n % p
+    else:
+        return n
