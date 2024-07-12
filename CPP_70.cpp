2d8bd5ce4a6d7291ad693be1fd7fe2fe65150f23
beep boop <githubbot@vadim.me>
2024-07-12 15:32:58 +0200

    Solution 34 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1o525827 Prompt: In file included from /usr/include/c++/11/vector:67,                  from c4c1abe1-fdbf-40f4-99f8-7f7a137f6f15.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from c4c1abe1-fdbf-40f4-99f8-7f7a137f6f15.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c4c1abe1-fdbf-40f4-99f8-7f7a137f6f15.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c4c1abe1-fdbf-40f4-99f8-7f7a137f6f15.cpp:28:39: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c4c1abe1-fdbf-40f4-99f8-7f7a137f6f15.cpp:27: c4c1abe1-fdbf-40f4-99f8-7f7a137f6f15.cpp: In function ‘int main()’: c4c1abe1-fdbf-40f4-99f8-7f7a137f6f15.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,9 +1,8 @@
-```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
@@ -14,16 +13,17 @@ std::vector<int> strange_sort_list(std::vector<int> lst) {
         int max_val = *std::max_element(lst.begin(), lst.end());
         if (min_val < max_val) {
             result.push_back(min_val);
-            lst.erase(std::remove(lst.begin(), lst.end(), min_val), lst.end());
+            lst.erase(std::remove(lst.begin(), lst.end(), min_val), lst.end() - 1);
         } else {
             result.push_back(max_val);
-            lst.erase(std::remove(lst.begin(), lst.end(), max_val), lst.end());
+            lst.erase(std::remove(lst.begin(), lst.end(), max_val), lst.end() - 1);
         }
     }
     return result;
 }
 
 int main() {
-    assert(issame(strange_sort_list({1, 2, 3}), {1, 2, 3}));
+    std::vector<int> input = {111111};
+    assert(std::equal(strange_sort_list(input).begin(), strange_sort_list(input).end(), input.begin()));
     return 0;
 }
