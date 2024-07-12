4925e7c556970449d1338e38d9106ce1d0fc196c
beep boop <githubbot@vadim.me>
2024-07-12 07:26:17 +0200

    Solution 3 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/o3nrap9p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d934ed16-58d0-47af-a4be-c0a68f837 983.py", line 9, in <module>     check(digitSum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d934ed16-58d0-47af-a4be-c0a68f837 983.py", line 6, in check     assert digitSum("You arE Very Smart") == 327, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d934ed16-58d0-47af-a4be-c0a68f837 983.py", line 2, in digitSum     return sum(int(c.upper()) - 55 if c.isupper() else int(c) for c in s)            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d934ed16-58d0-47af-a4be-c0a68f837 983.py", line 2, in <genexpr>     return sum(int(c.upper()) - 55 if c.isupper() else int(c) for c in s)                ^^^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: 'Y'

--- Python_66.py
+++ Python_66.py
@@ -1,3 +1,4 @@
-```
 def digitSum(s):
-    return sum(int(c) if c.isdigit() else 0 for c in s)
+    return sum(
+        ord(c) - 48 if "0" <= c <= "9" else ord(c) - 55 if c.isalpha() else 0 for c in s
+    )
