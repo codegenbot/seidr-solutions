5292c8d402d2b96f45a6660d9b28131cb251f93b
beep boop <githubbot@vadim.me>
2024-07-12 04:50:30 +0200

    Solution 95 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/6i6te4am Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a57482f7-5254-41cd-b652-b82b43892 34a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -2,9 +2,10 @@
 def fix_spaces(text):
     new_text = ''
     for char in text:
-        if char == ' ' and len(new_text) > 0 and not new_text[-1].isalnum():
-            new_text += '-'
-        elif char == ' ':
+        if char == ' ':
+            if new_text and new_text[-1] != '-':
+                new_text += '-'
+        elif new_text and new_text[-1] == '-':
             new_text += '_'
         else:
             new_text += char
