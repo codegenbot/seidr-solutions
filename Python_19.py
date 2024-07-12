bbf66c6e2a8a7444c3075198df2ff0b5185deab3
beep boop <githubbot@vadim.me>
2024-07-12 06:26:41 +0200

    Solution 47 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g167l76a Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e5694b99-335d-44a5-b653-2575828eb cf5.py", line 13, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e5694b99-335d-44a5-b653-2575828eb cf5.py", line 8, in check     sort_numbers("six five four three two one zero") AssertionError

--- Python_19.py
+++ Python_19.py
@@ -1,5 +1,6 @@
+import re
 def sort_numbers(input_str: str) -> str:
-    numbers = [int(num) for num in input_str.split() if num.isdigit()]
+    numbers = [int(num) for num in re.split(r'\s+', input_str) if num.isdigit()]
     if not numbers:
         return "No numbers found"
     return ' '.join(map(str, sorted(numbers)))
