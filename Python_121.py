f3f71ad71e3e5e192d906c2f1b7024b32a30adc4
beep boop <githubbot@vadim.me>
2024-07-12 08:39:53 +0200

    Solution 39 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ccbjq8ug Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e844bc61-954a-438a-b8ac-b6c23a405 319.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,9 +1,9 @@
-```
-class Solution:
-    def count_odd_numbers(self):
-        while True:
-            try:
-                lst = [int(x) for x in input("Enter numbers separated by space: ").split()]
-                print(len([i for i in lst if i % 2 != 0]))
-            except ValueError:
-                print("Invalid input. Please enter numbers separated by spaces.")
+def count_odd_numbers():
+    while True:
+        try:
+            lst = [int(x) for x in input("Enter numbers separated by space: ").split()]
+            print(len([i for i in lst if i % 2 != 0]))
+        except ValueError:
+            print("Invalid input. Please enter numbers separated by spaces.")
+
+count_odd_numbers()
