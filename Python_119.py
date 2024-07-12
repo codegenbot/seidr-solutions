90c61ac632582df59f06bd3b57177134ff5dd964
beep boop <githubbot@vadim.me>
2024-07-12 12:45:12 +0200

    Solution 37 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/rfytxfeq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8cbd27fc-f6b1-4a07-be16-9524823a9 31d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -1,19 +1,18 @@
-````
-def match_parens(lst):
-"""Docstring here"""
-s1 = lst[0]
-s2 = lst[1]
-stack = []
+```
+def """match_parens(lst):
+    s1 = lst[0]
+    s2 = lst[1]
+    stack = []
     
-for char in s1 + s2:
-    if char == '(':
-        stack.append(char)
-    elif char == ')':
-        if not stack:
-            return 'No'
-        stack.pop()
+    for char in s1 + s2:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return 'No'
+            stack.pop()
     
-if stack:
-    return 'No'
-else:
-    return 'Yes'
+    if stack:
+        return 'No'
+    else:
+        return 'Yes'""```
