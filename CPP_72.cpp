5be95b90b498327cc0da0c5cdc42273afda277b6
beep boop <githubbot@vadim.me>
2024-07-12 16:14:18 +0200

    Solution 52 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eui281em Prompt:   +++ |+#include <iostream>     1 | int main() { 36db1ca0-0b11-41ee-8a14-6f15d26a34dd.cpp:7:14: error: ‘cout’ is not a member of ‘std’     7 |         std::cout << "Will fly.\n";       |              ^~~~ 36db1ca0-0b11-41ee-8a14-6f15d26a34dd.cpp:7:14: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 36db1ca0-0b11-41ee-8a14-6f15d26a34dd.cpp: At global scope: 36db1ca0-0b11-41ee-8a14-6f15d26a34dd.cpp:13:5: error: redefinition of ‘int main( )’    13 | int main(){       |     ^~~~ 36db1ca0-0b11-41ee-8a14-6f15d26a34dd.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from 36db1ca0-0b11-41ee-8a14-6f15d26a34dd.cpp:12: 36db1ca0-0b11-41ee-8a14-6f15d26a34dd.cpp: In function ‘int main()’: 36db1ca0-0b11-41ee-8a14-6f15d26a34dd.cpp:14:13: error: ‘will_it_fly’ was not dec lared in this scope    14 |     assert (will_it_fly({5}, 5) == true);       |             ^~~~~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
-
-bool will_it_fly(std::vector<std::vector<int>> q, int w) {
-    for (auto v : q) {
-        if ((v[0] * v[1]) > w)
-            return true;
-    }
-    return false;
-}
+bool will_it_fly(std::vector<std::vector<int>>, int);
 
 int main() {
     std::vector<std::vector<int>> q = {{1, 2}, {3, 4}};
@@ -17,3 +10,4 @@ int main() {
         std::cout << "Will fly.\n";
     }
     return 0;
+}
