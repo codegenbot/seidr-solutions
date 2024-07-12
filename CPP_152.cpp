21017dff67281e7e5e4f03944f9b3b0b6592d271
beep boop <githubbot@vadim.me>
2024-07-12 23:55:35 +0200

    Solution 17 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uswfokmt Prompt:       |             ^~~~~~ 9ef077bb-d44c-454c-8006-4d99eb722b81.cpp:42:34: error: expected primary-expressi on before ‘int’    42 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9ef077bb-d44c-454c-8006-4d99eb722b81.cpp:42:39: error: expression list treated a s compound expression in initializer [-fpermissive]    42 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 9ef077bb-d44c-454c-8006-4d99eb722b81.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ 9ef077bb-d44c-454c-8006-4d99eb722b81.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~ In file included from 9ef077bb-d44c-454c-8006-4d99eb722b81.cpp:41: 9ef077bb-d44c-454c-8006-4d99eb722b81.cpp: In function ‘int main()’: 9ef077bb-d44c-454c-8006-4d99eb722b81.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <iostream>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     for(int i = 0; i < a.size();i++){
         if(a[i] != b[i]) return false;
     }
