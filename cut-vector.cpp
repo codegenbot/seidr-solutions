78c872d65653aea198fbf63b440c3b2edf951e6a
beep boop <githubbot@vadim.me>
2024-07-12 22:44:33 +0200

    Solution 15 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/k7mxwgg8 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,40 +1,40 @@
 int main() {
     vector<int> nums;
     int num;
+    
     while (cin >> num) {
         nums.push_back(num);
     }
     
-    int n = nums.size();
-    int totalSum = 0;
-    for (int i = 0; i < n; ++i) {
-        totalSum += nums[i];
-    }
+    int min_diff = INT_MAX;
+    int cut_index = 0;
     
-    vector<int> firstHalf;
-    int sum = 0;
-    int closestDiff = INT_MAX;
-    int cutIndex = 0;
-    
-    for (int i = 0; i < n; ++i) {
-        sum += nums[i];
-        int diff = abs(sum - (totalSum - sum));
-        if (diff < closestDiff) {
-            closestDiff = diff;
-            cutIndex = i;
+    for (int i = 1; i < nums.size(); i++) {
+        int sum_left = 0;
+        int sum_right = 0;
+        
+        for (int j = 0; j < i; j++) {
+            sum_left += nums[j];
+        }
+        
+        for (int j = i; j < nums.size(); j++) {
+            sum_right += nums[j];
+        }
+        
+        int diff = abs(sum_left - sum_right);
+        if (diff < min_diff) {
+            min_diff = diff;
+            cut_index = i;
         }
     }
     
-    for (int i = 0; i <= cutIndex; ++i) {
-        firstHalf.push_back(nums[i]);
+    for (int i = 0; i < cut_index; i++) {
+        cout << nums[i] << endl;
     }
-    
-    cout << "1" << endl;
-    for (int i = 0; i < firstHalf.size(); ++i) {
-        cout << firstHalf[i] << endl;
+    cout << endl;
+    for (int i = cut_index; i < nums.size(); i++) {
+        cout << nums[i] << endl;
     }
-    
-    cout << "0" << endl;
     
     return 0;
 }
