f822a47c5d8aab980150bbff11a5b41ddf19a243
beep boop <githubbot@vadim.me>
2024-07-12 04:40:26 +0200

    Solution 4 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/ty89jro2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a4edd52b-1085-4628-be80-fb34e1710 41e.py", line 5, in <module>     print(solve_boolean(input_str))           ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a4edd52b-1085-4628-be80-fb34e1710 41e.py", line 2, in solve_boolean     return eval(expression.replace("T", "True").replace("F", "False").replace("| ", "or").replace("&", "and"))            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "<string>", line 1, in <module> NameError: name 'fandtorfortortortortorfandfandfandfandtortandf' is not defined

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,6 +1,5 @@
 def solve_boolean(expression):
-    return eval(expression, {"T": True, "F": False, "|": "or", "&": "and"})
-
+    return eval(expression.replace("T", "True").replace("F", "False").replace("|", "or").replace("&", "and"), {'True': True, 'False': False})
 
 input_str = input()
 print(solve_boolean(input_str))
