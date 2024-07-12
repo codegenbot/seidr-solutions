210481f9811542acaf960a38cf2c735702c81ee7
beep boop <githubbot@vadim.me>
2024-07-12 11:55:46 +0200

    Solution 45 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/q9bacny6 Prompt:       |                           ^~~~~~ cad291d1-4b7e-44c7-9d76-e771551d1565.cpp:24:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from cad291d1-4b7e-44c7-9d76-e771551d1565.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from cad291d1-4b7e-44c7-9d76-e771551d1565.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ cad291d1-4b7e-44c7-9d76-e771551d1565.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cad291d1-4b7e-44c7-9d76-e771551d1565.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ cad291d1-4b7e-44c7-9d76-e771551d1565.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -4,8 +4,8 @@
 
 std::vector<int> incr_list(const std::vector<int>& l) {
     std::vector<int> result = l;
-    for (int& num : result) {
-        num++;
+    for (int &n : result) {
+        n++;
     }
     return result;
 }
@@ -16,6 +16,6 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 
 int main() {
     assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
-
+    
     return 0;
 }
