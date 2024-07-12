ce9569107f01f311650c2e2f0aa311a766e80648
beep boop <githubbot@vadim.me>
2024-07-12 13:53:14 +0200

    Solution 79 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/545auy28 Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a5e900ec-7a44-44c4-b882-a43c59373424.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a5e900ec-7a44-44c4-b882-a43c59373424.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a5e900ec-7a44-44c4-b882-a43c59373424.cpp:11: a5e900ec-7a44-44c4-b882-a43c59373424.cpp: In function ‘int main()’: a5e900ec-7a44-44c4-b882-a43c59373424.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ a5e900ec-7a44-44c4-b882-a43c59373424.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,28 +1,34 @@
-```cpp
 #include <boost/any.hpp>
 #include <vector>
 #include <list>
 #include <typeinfo>
 
-bool issame(const boost::any& a, const boost::any& b) {
-    if (!a.type() == typeid(int) || !b.type() == typeid(int))
+using namespace std;
+using namespace boost;
+
+bool issame(list_any a, list_any b) {
+    if(a.type() != b.type())
         return false;
-    int x = boost::any_cast<int>(a);
-    int y = boost::any_cast<int>(b);
-    return x == y;
+
+    vector-any v1 = a.to_vector();
+    vector-any v2 = b.to_vector();
+
+    if(v1.size() != v2.size())
+        return false;
+
+    for(int i = 0; i < v1.size(); i++) {
+        if(v1[i].type() != v2[i].type())
+            return false;
+    }
+
+    return true;
 }
 
-vector<int> filter_integers(list<boost::any> values) {
+vector<int> filter_integers(list_any values) {
     vector<int> result;
     for (const auto& value : values) {
-        if(boost::any_cast<int>(value).type() == typeid(int)) {
-            bool found = false;
-            for (int i = 0; i < result.size(); ++i) {
-                if(issame(value, boost::any(result[i])))
-                    {found = true;break;}
-            }
-            if(!found)
-                result.push_back(boost::any_cast<int>(value));
+        if (boost::any_cast<int>(value).type() == typeid(int)) {
+            result.push_back(boost::any_cast<int>(value));
         }
     }
     return result;
