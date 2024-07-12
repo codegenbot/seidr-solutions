2d8e377948dc3616b68a80bb9bfa3897574135c1
beep boop <githubbot@vadim.me>
2024-07-12 17:02:41 +0200

    Solution 77 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eui281em Prompt: erator std::__uninitialized_default_n_a(_ForwardIterator, _Size, std::allocator< _Tp>&) [with _ForwardIterator = std::vector<int>*; _Size = long unsigned int; _T p = std::vector<int>]’ /usr/include/c++/11/bits/stl_vector.h:1606:36:   required from ‘void std::vector <_Tp, _Alloc>::_M_default_initialize(std::vector<_Tp, _Alloc>::size_type) [with _Tp = std::vector<int>; _Alloc = std::allocator<std::vector<int> >; std::vector< _Tp, _Alloc>::size_type = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:512:9:   required from ‘std::vector<_Tp, _ Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocator_type&) [with  _Tp = std::vector<int>; _Alloc = std::allocator<std::vector<int> >; std::vector <_Tp, _Alloc>::size_type = long unsigned int; std::vector<_Tp, _Alloc>::allocato r_type = std::allocator<std::vector<int> >]’ 4616e18f-2184-423b-bb56-6f5c47ca8c75.cpp:35:5:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,1 +1,31 @@
-vector<vector<int>> q = {{}, {}};
+```
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool will_it_fly(vector<vector<int>> q, int w) {
+    if(w >= (q[0][1] - q[0][0]) && w >= (q[1][1] - q[1][0])) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    vector<vector<int>> q = {{}, {}};
+    int n;  // number of pairs
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        int x, y; // two numbers in a pair
+        cin >> x >> y;
+        q.push_back({x,y});
+    }
+    int w;
+    cin >> w;
+    if (!will_it_fly(q, w)) {
+        cout << "Will not fly.\n";
+    } else {
+        cout << "Will fly.\n";
+    }
+    return 0;
+}
