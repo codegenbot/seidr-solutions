76a811ce3b1926cda4e813266aa68b32206fefac
beep boop <githubbot@vadim.me>
2024-07-12 23:10:06 +0200

    Solution 63 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lqy3vi1c Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -5,18 +5,22 @@ int count_nums(char** nums, int size) {
     int count = 0;
     for (int i = 0; i < size; i++) {
         if (atoi(nums[i]) >= 0) {
-            std::string str = std::to_string(atoi(nums[i]));
             int sum = 0;
-            for (char c : str) {
+            for (char c : std::string(nums[i])) {
+                if (c == '-') {
+                    continue;
+                }
                 sum += c - '0';
             }
             if (sum % 2 != 0)
                 count++;
         } else {
-            std::string str = "-" + std::to_string(std::abs(atoi(nums[i])));
-            int sum = 0, sign = 1;
-            for(int j=str.length()-1; j>=0; j--) {
-                sum += std::abs(str[j] - '0') * sign;
+            int sign = -1, sum = 0;
+            for(int j=std::string(nums[i]).length()-1; j>=0; j--) {
+                if (std::string(nums[i])[j] == '-') {
+                    continue;
+                }
+                sum += std::abs(std::string(nums[i])[j] - '0') * sign;
                 sign *= -1;
             }
             if (sign * sum % 2 != 0)
@@ -27,6 +31,7 @@ int count_nums(char** nums, int size) {
 
 }
 
+char* numbers[] = {"1"};
 int main() {
-    assert(count_nums((char**){{"1"}}, 1) == 1);
+    assert(count_nums(numbers, 1) == 1);
 }
