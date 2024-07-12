ca047809693ff5bd6fdc379b234243b4703b2ba3
beep boop <githubbot@vadim.me>
2024-07-12 07:54:39 +0200

    Solution 16 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/wp716kai Prompt: 46c90d4e-e338-4d13-945e-f21412ea6343.cpp: In function ‘std::string string_to_md5 (const string&)’: 46c90d4e-e338-4d13-945e-f21412ea6343.cpp:10:8: warning: ‘unsigned char* MD5(cons t unsigned char*, size_t, unsigned char*)’ is deprecated: Since OpenSSL 3.0 [-Wd eprecated-declarations]    10 |     MD5((unsigned char*)text.c_str(), text.length(), digest);       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 46c90d4e-e338-4d13-945e-f21412ea6343.cpp:2: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ Program failed

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -6,21 +6,20 @@ std::string string_to_md5(const std::string &text) {
         return "None";
     }
 
-    EVP_MD_CTX *mdctx;
     unsigned char digest[EVP_MAX_MD_SIZE];
-    unsigned int digest_len;
-    
+    EVP_MD_CTX *mdctx;
+    size_t md_len;
     mdctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
+    EVP_DigestInit_ex(mdctx, EVP_md5(), nullptr);
     EVP_DigestUpdate(mdctx, text.c_str(), text.length());
-    EVP_DigestFinal_ex(mdctx, digest, &digest_len);
+    EVP_DigestFinal_ex(mdctx, digest, &md_len);
     EVP_MD_CTX_free(mdctx);
 
     char mdString[33];
-    for (unsigned int i = 0; i < digest_len; i++) {
-        sprintf(&mdString[i * 2], "%02x", digest[i]);
+    for (size_t i = 0; i < md_len; i++) {
+        sprintf(&mdString[i * 2], "%02x", (unsigned int)digest[i]);
     }
+
     mdString[32] = '\0'; // Add null terminator
-    
     return mdString;
 }
