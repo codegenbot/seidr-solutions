c3ec5aa884844be3cb217cb7041d0a1112643641
beep boop <githubbot@vadim.me>
2024-07-12 18:34:43 +0200

    Solution 52 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/20nxyk55 Prompt: b/python3.11/site-packages/programlib/programs/e71fe680-e293-4776-bed8-1d3b33557 3ff.py", line 5, in sort_third     return [flatten_list(sorted(l[i:i+3], reverse=True)) for i in range(0, len(l ), 3)]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e71fe680-e293-4776-bed8-1d3b33557 3ff.py", line 5, in <listcomp>     return [flatten_list(sorted(l[i:i+3], reverse=True)) for i in range(0, len(l ), 3)]             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e71fe680-e293-4776-bed8-1d3b33557 3ff.py", line 2, in flatten_list     return [item for sublist in list_of_lists for item in sublist]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e71fe680-e293-4776-bed8-1d3b33557 3ff.py", line 2, in <listcomp>     return [item for sublist in list_of_lists for item in sublist]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: 'int' object is not iterable

--- Python_33.py
+++ Python_33.py
@@ -2,7 +2,4 @@ def flatten_list(list_of_lists):
     return [item for sublist in list_of_lists for item in sublist]
 
 def sort_third(l: list):
-    if len(l) % 3 != 0:
-        l += [0] * (3 - len(l) % 3)
-    
-    return [flatten_list(sorted([l[i], l[i+1], l[i+2]], reverse=True)) for i in range(0, len(l), 3)]
+    return [flatten_list(sorted([l[i:i+3]], reverse=True)) for i in range(0, len(l), 3)]
