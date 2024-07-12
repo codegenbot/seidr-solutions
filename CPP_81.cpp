95aaa72910ce7cd66997ea6cc071f0f9cbc60e45
beep boop <githubbot@vadim.me>
2024-07-12 15:09:04 +0200

    Solution 24 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/59i5y8lf Prompt:       |           ^~~~~~ 673b92d7-976a-408b-b334-3dd5d1bbaa39.cpp:64:44: error: ‘b’ was not declared in t his scope    64 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 673b92d7-976a-408b-b334-3dd5d1bbaa39.cpp:72:5: error: redefinition of ‘int main( )’    72 | int main(){       |     ^~~~ 673b92d7-976a-408b-b334-3dd5d1bbaa39.cpp:51:5: note: ‘int main()’ previously def ined here    51 | int main() {       |     ^~~~ In file included from 673b92d7-976a-408b-b334-3dd5d1bbaa39.cpp:63: 673b92d7-976a-408b-b334-3dd5d1bbaa39.cpp: In function ‘int main()’: 673b92d7-976a-408b-b334-3dd5d1bbaa39.cpp:73:20: error: ‘numerical_letter_grade’ was not declared in this scope    73 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 673b92d7-976a-408b-b334-3dd5d1bbaa39.cpp:73:19: error: ‘issame’ cannot be used a s a function    73 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,44 +1,43 @@
-```cpp
 #include <vector>
 #include <string>
 
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size())
+bool issame(vector<string> a, vector<string>b){
+    if(a.size() != b.size())
         return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i])
             return false;
     }
     return true;
 }
 
-vector<string> numerical_letter_grade(vector<float> grades) {
+vector<string> numerical_letter_grade(vector<float> grades){
     vector<string> result;
-    for (float g : grades) {
+    for(float g : grades) {
         string letterGrade;
-        if (g >= 4.0) {
+        if(g >= 4.0) {
             letterGrade = "A+";
-        } else if (g > 3.7) {
+        } else if(g > 3.7) {
             letterGrade = "A";
-        } else if (g > 3.3) {
+        } else if(g > 3.3) {
             letterGrade = "A-";
-        } else if (g > 3.0) {
+        } else if(g > 3.0) {
             letterGrade = "B+";
-        } else if (g > 2.7) {
+        } else if(g > 2.7) {
             letterGrade = "B";
-        } else if (g > 2.3) {
+        } else if(g > 2.3) {
             letterGrade = "B-";
-        } else if (g > 2.0) {
+        } else if(g > 2.0) {
             letterGrade = "C+";
-        } else if (g > 1.7) {
+        } else if(g > 1.7) {
             letterGrade = "C";
-        } else if (g > 1.3) {
+        } else if(g > 1.3) {
             letterGrade = "C-";
-        } else if (g > 1.0) {
+        } else if(g > 1.0) {
             letterGrade = "D+";
-        } else if (g > 0.7) {
+        } else if(g > 0.7) {
             letterGrade = "D";
-        } else if (g > 0.0) {
+        } else if(g > 0.0) {
             letterGrade = "D-";
         } else {
             letterGrade = "E";
@@ -50,12 +49,14 @@ vector<string> numerical_letter_grade(vector<float> grades) {
 
 int main() {
     vector<float> grades = {3.4, 4.1, 2.9};
-    vector<string> grades_letter = numerical_letter_grade(grades);
+    vector<string> grade_letters = numerical_letter_grade(grades);
 
-    if (issame(grades_letter, numerical_letter_grade(grades)))
+    if(issame(numerical_letter_grade(grades), grade_letters))
         cout << "Grades are same." << endl;
     else
         cout << "Grades are not the same." << endl;
+
+    assert (issame(numerical_letter_grade({0.0, 0.7}), {"E", "D-" }));
 
     return 0;
 }
