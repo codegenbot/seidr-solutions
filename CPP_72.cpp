1fdc8ce437e4d2b22f3e048c0a1866d9a77fdeb6
beep boop <githubbot@vadim.me>
2024-07-12 17:09:10 +0200

    Solution 79 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eui281em Prompt:  1203 |       push_back(value_type&& __x)       |       ^~~~~~~~~ /usr/include/c++/11/bits/stl_vector.h:1203:7: note:   candidate expects 1 argume nt, 2 provided c46a2951-b186-4860-a58f-63401dcbe87b.cpp: At global scope: c46a2951-b186-4860-a58f-63401dcbe87b.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ c46a2951-b186-4860-a58f-63401dcbe87b.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from c46a2951-b186-4860-a58f-63401dcbe87b.cpp:32: c46a2951-b186-4860-a58f-63401dcbe87b.cpp: In function ‘int main()’: c46a2951-b186-4860-a58f-63401dcbe87b.cpp:34:24: error: converting to ‘std::vecto r<std::vector<int> >’ from initializer list would use explicit constructor ‘std: :vector<_Tp, _Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocato r_type&) [with _Tp = std::vector<int>; _Alloc = std::allocator<std::vector<int> >; std::vector<_Tp, _Alloc>::size_type = long unsigned int; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<std::vector<int> >]’    34 |     assert (will_it_fly({5}, 5) == true);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -11,17 +12,17 @@ bool will_it_fly(vector<vector<int>> q, int w) {
 }
 
 int main() {
-    vector<vector<int>> q;
     int n;  // number of pairs
     cin >> n;
+    vector<vector<int>> q(n);
     for(int i=0; i<n; i++) {
         int x, y; // two numbers in a pair
         cin >> x >> y;
-        q.push_back({x,y});
+        q[i].push_back({x,y});
     }
     int w;
     cin >> w;
-    if (!will_it_fly({{q[0][0], q[0][1]}, {q[1][0], q[1][1]}}, w)) {
+    if (!will_it_fly(q, w)) {
         cout << "Will not fly.\n";
     } else {
         cout << "Will fly.\n";
