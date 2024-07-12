28b162f03e121252f3b2d507b3f317ecd181b293
beep boop <githubbot@vadim.me>
2024-07-12 06:06:46 +0200

    Solution 20 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7bd6h5bq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c58ce28f-3ed4-4d51-8646-9aa00d1dc d5d.py", line 10, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c58ce28f-3ed4-4d51-8646-9aa00d1dc d5d.py", line 5, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c58ce28f-3ed4-4d51-8646-9aa00d1dc d5d.py", line 2, in sort_third     grouped = [list(g) for k, g in groupby(enumerate(lst), key=lambda x: x[0]//3 )]                                    ^^^^^^^ NameError: name 'groupby' is not defined. Did you mean: 'grouped'?

--- Python_33.py
+++ Python_33.py
@@ -1,6 +1,6 @@
-import itertools as it
+from itertools import groupby
 
 
 def sort_third(lst):
-    grouped = [list(g) for k, g in it.groupby(enumerate(lst), key=lambda x: x[0] // 3)]
+    grouped = [list(g) for k, g in groupby(enumerate(lst), key=lambda x: x[0] // 3)]
     return [min(i) if i[0] % 3 != 0 else max(sorted(i)[1:]) for i in grouped]
