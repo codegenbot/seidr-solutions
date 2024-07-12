ad29cb97f0f31ce15a09752b67ffe7a62a165317
beep boop <githubbot@vadim.me>
2024-07-12 15:25:10 +0200

    Solution 66 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zj95medv Prompt: 4f13a1d6-1a0f-40aa-a1d0-e322d55a1023.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 4f13a1d6-1a0f-40aa-a1d0-e322d55a1023.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,11 +1,10 @@
-```cpp
 #include <vector>
 #include <algorithm>
 #include <cassert>
 
 using namespace std;
 
-bool issame(const vector<int>& v1, const vector<int>& v2) {
+bool sameVectors(const vector<int>& v1, const vector<int>& v2) {
     return v1 == v2;
 }
 
@@ -18,6 +17,6 @@ vector<int> incr_list(const vector<int>& l){
 }
 
 int main() {
-    assert(issame({6, 3, 6,3, 4, 4, 10, 1, 124}, {5, 2, 5, 2, 3, 3, 9, 0, 123}));
+    assert(sameVectors({6, 3, 6,3, 4, 4, 10, 1, 125}, incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123})));
     return 0;
 }
