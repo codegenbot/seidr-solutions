861bd51746db43ff9c0d2c9f5ba8922c9b4d44c3
beep boop <githubbot@vadim.me>
2024-07-12 21:51:45 +0200

    Solution 67 of Python/153, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/jxft3xng Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e85a3c58-a3f3-43cb-9957-8595f460e c82.py", line 26, in <module>     check(Strongest_Extension)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e85a3c58-a3f3-43cb-9957-8595f460e c82.py", line 23, in check     assert Strongest_Extension("Sp", ["671235", "Bb"]) == "Sp.671235"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e85a3c58-a3f3-43cb-9957-8595f460e c82.py", line 3, in Strongest_Extension     return f"{extension}.{max(int(ext) for ext in extensions_list)}"                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e85a3c58-a3f3-43cb-9957-8595f460e c82.py", line 3, in <genexpr>     return f"{extension}.{max(int(ext) for ext in extensions_list)}"                               ^^^^^^^^ ValueError: invalid literal for int() with base 10: 'Bb'

--- Python_153.py
+++ Python_153.py
@@ -1,19 +1,14 @@
 def Strongest_Extension(extension, extensions_list):
-    if not extension.isdigit():
-        return f"{extension}.{max(int(ext) for ext in extensions_list)}"
+    extensions_list = [int(ext) for ext in extensions_list if ext.isdigit()]
 
-    extensions_list = [int(ext) for ext in extensions_list]
-
-    if not isinstance(extension, int) or extension not in extensions_list:
+    if not extension.isdigit() or int(extension) not in extensions_list:
         return f"{extension}.{max(extensions_list)}"
 
+    extension = int(extension)
     extension_index = extensions_list.index(extension)
-
-    if extension_index == 0:
-        return f"{extension}.{max(extensions_list)}"
 
     return (
         f"{extension}.{extensions_list[extension_index - 1]}"
-        if extensions_list[extension_index - 1] > extension
-        else f"{extension}.{extension}"
+        if extension_index > 0
+        else f"{extension}.{max(extensions_list)}"
     )
