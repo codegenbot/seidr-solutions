6655ee3b7be66f6d7c67faeca171e9de7e215931
beep boop <githubbot@vadim.me>
2024-07-13 00:26:54 +0200

    Solution 27 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hpkkxb14 Prompt: lloc>::iterator = std::vector<std::pair<std::__cxx11::basic_string<char>, std::_ _cxx11::basic_string<char> > >::iterator]’ /usr/include/c++/11/bits/vector.tcc:121:21:   required from ‘std::vector<_Tp, _A lloc>::reference std::vector<_Tp, _Alloc>::emplace_back(_Args&& ...) [with _Args  = {std::pair<std::__cxx11::basic_string<char, std::char_traits<char>, std::allo cator<char> >, std::__cxx11::basic_string<char, std::char_traits<char>, std::all ocator<char> > >}; _Tp = std::pair<std::__cxx11::basic_string<char>, std::__cxx1 1::basic_string<char> >; _Alloc = std::allocator<std::pair<std::__cxx11::basic_s tring<char>, std::__cxx11::basic_string<char> > >; std::vector<_Tp, _Alloc>::ref erence = std::pair<std::__cxx11::basic_string<char>, std::__cxx11::basic_string< char> >&]’ /usr/include/c++/11/bits/stl_vector.h:1204:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(std::vector<_Tp, _Alloc>::value_type&&) [with _Tp = std ::pair<std::__cxx11::basic_string<char>, std::__cxx11::basic_string<char> >; _Al loc = std::allocator<std::pair<std::__cxx11::basic_string<char>, std::__cxx11::b asic_string<char> > >; std::vector<_Tp, _Alloc>::value_type = std::pair<std::__c xx11::basic_string<char>, std::__cxx11::basic_string<char> >]’ cea6b9dc-6d43-42c5-8a3e-d251e066fad9.cpp:17:25:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::pair<std::__cxx11::basic_string<char>, std::__cxx11::basic_string<ch ar> > >’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cassert>
 
 std::string Strongest_Extension(const std::pair<std::string, std::string>& extension) {
     if (extension.second.empty()) {
@@ -12,8 +13,9 @@ std::string Strongest_Extension(const std::pair<std::string, std::string>& exten
 
 int main() {
     std::vector<std::pair<std::string, std::string>> extensions;
-    extensions.push_back({"Sp", "671235"}); 
-    extensions.push_back({"Bb", ""});
+    extensions.push_back(std::make_pair("Sp", "671235")); 
+    extensions.push_back(std::make_pair("Bb", ""));
+    
     assert(Strongest_Extension({{"Sp", "671235"}}) == "Sp.671235");
+    std::cout << Strongest_Extension({{"Sp", "671235"}}); 
     return 0;
-}
