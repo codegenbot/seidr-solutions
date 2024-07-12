f30da99e3bee7bfccda70d26ef000696088e49c7
beep boop <githubbot@vadim.me>
2024-07-13 01:44:57 +0200

    Solution 31 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d45c05lr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/72724171-8e77-4c5b-bcc7-afdc9e9cb a87.py", line 13, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/72724171-8e77-4c5b-bcc7-afdc9e9cb a87.py", line 10, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,8 +1,9 @@
+```
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
-        return string
-    for i in range(len(string), 0, -1):
-        char = "abcdefghijklmnopqrstuvwxyz"[i-1]
-        new_string = string + char + string[::-1]
-        if new_string == new_string[::-1]:
-            return new_string
+        return string + string
+    new_string = ""
+    for i in range(len(string)):
+        new_string += string[i]
+        new_string += string[len(string)-i-1]
+    return new_string
