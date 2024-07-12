8a1a26cf609c7f0d6b52ec0070e4aac7f1889c7a
beep boop <githubbot@vadim.me>
2024-07-12 18:23:23 +0200

    Solution 17 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mm8jcipt Prompt:    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a738313a-d19b-4b3a-b203-c46683885f14.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a738313a-d19b-4b3a-b203-c46683885f14.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a738313a-d19b-4b3a-b203-c46683885f14.cpp:21: a738313a-d19b-4b3a-b203-c46683885f14.cpp: In function ‘int main()’: a738313a-d19b-4b3a-b203-c46683885f14.cpp:31:20: error: ‘sorted_list_sum’ was not  declared in this scope    31 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ a738313a-d19b-4b3a-b203-c46683885f14.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,3 +1,10 @@
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) return false;
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    return a == b;
+}
+
 vector<string> sorted_list_sum(vector<string> lst) {
     vector<string> result = lst;
     for (auto it = result.begin(); it != result.end();) {
@@ -18,9 +25,7 @@ vector<string> sorted_list_sum(vector<string> lst) {
     return result;
 }
 
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) return false;
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    return a == b;
+int main() {
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cccc", "dddd", "aaaaa", "bbbbb"}));
+    return 0;
 }
