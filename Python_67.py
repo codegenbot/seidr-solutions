abeaa5555fb40618067273e1ac4146107447a6ec
beep boop <githubbot@vadim.me>
2024-07-12 08:01:54 +0200

    Solution 89 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/rq5tl5sf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa8cf1b5-25e0-4a44-bcdd-b07639d8c 385.py", line 18, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa8cf1b5-25e0-4a44-bcdd-b07639d8c 385.py", line 15, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: fruit_distribution() takes 1 positional argument but 2 were given

--- Python_67.py
+++ Python_67.py
@@ -1,11 +1,12 @@
-def fruit_distribution(s=None):
-    s = input().lower()
+```Python
+def fruit_distribution(*s):
     total = 0
-    parts = s.split()
-    for part in parts:
-        if part.isdigit():
-            total += int(part)
-        elif part.startswith("and"):
-            last_number = int(parts[parts.index(part) - 1])
-            total -= (last_number // 2)
+    for s_part in s:
+        s_part = s_part.lower()
+        for part in s_part.split():
+            if part.isdigit():
+                total += int(part)
+            elif part.startswith("and"):
+                last_number = int(s_part.split()[s_part.split().index(part) - 1])
+                total -= (last_number // 2)
     return -(total // 2)
