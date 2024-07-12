c3b4bd37ab4920940963fdee5a08d25fd3e779df
beep boop <githubbot@vadim.me>
2024-07-12 02:56:55 +0200

    Solution 24 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/52q4ikre Prompt: in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5b5f4bc1-f708-4589-8af1-2a7c32dd11e9.cpp:8:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 5b5f4bc1-f708-4589-8af1-2a7c32dd11e9.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 5b5f4bc1-f708-4589-8af1-2a7c32dd11e9.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 5b5f4bc1-f708-4589-8af1-2a7c32dd11e9.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 5b5f4bc1-f708-4589-8af1-2a7c32dd11e9.cpp:7: 5b5f4bc1-f708-4589-8af1-2a7c32dd11e9.cpp: In function ‘int main()’: 5b5f4bc1-f708-4589-8af1-2a7c32dd11e9.cpp:17:20: error: ‘even_odd_palindrome’ was  not declared in this scope    17 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -3,13 +3,3 @@
 bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
-
-std::vector<int> even_odd_palindrome(int n){
-    // Function definition goes here
-}
-
-int main(){
-    assert(issame(even_odd_palindrome(1), {0, 1}));
-    // Other code here
-    return 0;
-}
