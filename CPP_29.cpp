d5fdfa2da08dc47f7bcfce7c2d37bad10daf1380
beep boop <githubbot@vadim.me>
2024-07-12 18:47:43 +0200

    Solution 2 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pyvjbhec Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e93f14cb-a2bd-4ab9-a462-d6d2376bbb0a.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e93f14cb-a2bd-4ab9-a462-d6d2376bbb0a.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e93f14cb-a2bd-4ab9-a462-d6d2376bbb0a.cpp:10: e93f14cb-a2bd-4ab9-a462-d6d2376bbb0a.cpp: In function ‘int main()’: e93f14cb-a2bd-4ab9-a462-d6d2376bbb0a.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ e93f14cb-a2bd-4ab9-a462-d6d2376bbb0a.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,6 +1,16 @@
-```c++
-bool issame(vector<string> a, vector<string>b){
-    return a == b;
+#include <vector>
+#include <string>
+
+bool issame(vector<string> a, vector<string> b){
+    if(a.size() != b.size())
+        return false;
+    
+    for(int i = 0; i<a.size(); i++){
+        if(a[i] != b[i])
+            return false;
+    }
+    
+    return true;
 }
 
 vector<string> filter_by_prefix(vector<string> strings, string prefix){
