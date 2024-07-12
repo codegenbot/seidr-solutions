4a1e3d05c98252936ab1581acce4b7548eec247a
beep boop <githubbot@vadim.me>
2024-07-12 21:37:37 +0200

    Solution 4 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/i3ayp14j Prompt: 81cd744f-bde3-42f9-96df-c612865ab9e8.cpp:4:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     4 | string get_closest_vowel(string word) {       | ^~~~~~       | stdin In file included from 81cd744f-bde3-42f9-96df-c612865ab9e8.cpp:15: 81cd744f-bde3-42f9-96df-c612865ab9e8.cpp: In function ‘int main()’: 81cd744f-bde3-42f9-96df-c612865ab9e8.cpp:17:13: error: ‘get_closest_vowel’ was n ot declared in this scope    17 |     assert (get_closest_vowel("Above") == "o");       |             ^~~~~~~~~~~~~~~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,16 +1,10 @@
-#include <iostream>
-#include <string>
-#include <cctype>
-
-using namespace std;
-
 std::string get_closest_vowel(std::string word) {
     int n = word.size();
     for (int i = n - 1; i > 0; --i) {
         if (!isalpha(word[i])) continue;
-        if (isvowel(word[i])) return string(1, tolower(word[i]));
+        if (isvowel(word[i])) return std::string(1, tolower(word[i]));
         while (!isalpha(word[i-1]) && i > 0) --i;
-        if (isvowel(word[i-1])) return string(1, tolower(word[i-1]));
+        if (isvowel(word[i-1])) return std::string(1, tolower(word[i-1]));
     }
     return "";
 }
