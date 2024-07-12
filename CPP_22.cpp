50b9704e1e8e18e0b6d7babc8530ef63da837639
beep boop <githubbot@vadim.me>
2024-07-12 13:17:05 +0200

    Solution 44 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/545auy28 Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 341501ef-6bc4-43e0-8586-94816f1d34e7.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 341501ef-6bc4-43e0-8586-94816f1d34e7.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 341501ef-6bc4-43e0-8586-94816f1d34e7.cpp:11: 341501ef-6bc4-43e0-8586-94816f1d34e7.cpp: In function ‘int main()’: 341501ef-6bc4-43e0-8586-94816f1d34e7.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 341501ef-6bc4-43e0-8586-94816f1d34e7.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -2,16 +2,17 @@
 #include <vector>
 #include <list>
 #include <boost/any.hpp>
+#include <boost/container/list.hpp>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-std::vector<int> filter_integers(std::list<boost::any> values) {
-    std::vector<int> result;
+vector<int> filter_integers(list<boost::any> values) {
+    vector<int> result;
     for (const auto& value : values) {
-        if (boost::any_cast<int>(value) != 0) { 
-            result.push_back(boost::any_cast<int>(value));
+        if (boost::get<int>(value) != 0) { 
+            result.push_back(boost::get<int>(value));
         }
     }
     return result;
