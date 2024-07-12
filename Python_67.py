4aeb2e0de5d0c34a82a523e08e73e62c54a1795f
beep boop <githubbot@vadim.me>
2024-07-12 07:27:29 +0200

    Solution 5 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/rq5tl5sf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/85478092-ede8-4cdf-af0d-1ffa2b8e6 b29.py", line 13, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/85478092-ede8-4cdf-af0d-1ffa2b8e6 b29.py", line 10, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -2,9 +2,6 @@
 def fruit_distribution(s, n):
     s = s.replace("apples", "0").replace("oranges", "0")
     parts = s.split()
-    count = 0
-    for i in range(len(parts)):
-        if parts[i] == 'and':
-            break
-        count += 1
-    return n - int(count)
+    for i in range(len(parts)-1, 2, -1): 
+        if not parts[i].isdigit():
+            return int(i)
