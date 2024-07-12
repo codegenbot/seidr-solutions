c27ef3b019730bfd04adf24b5b70eec50c71bc64
beep boop <githubbot@vadim.me>
2024-07-13 01:23:47 +0200

    Solution 50 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hpkkxb14 Prompt: cxx11::basic_string<char> > >; _Compare = int (*)(const std::pair<std::__cxx11:: basic_string<char>, std::__cxx11::basic_string<char> >&, const std::pair<std::__ cxx11::basic_string<char>, std::__cxx11::basic_string<char> >&)]’: /usr/include/c++/11/bits/stl_algo.h:5688:12:   required from ‘constexpr _Forward Iterator std::__max_element(_ForwardIterator, _ForwardIterator, _Compare) [with _ForwardIterator = __gnu_cxx::__normal_iterator<const std::__cxx11::basic_string <char>*, std::vector<std::__cxx11::basic_string<char> > >; _Compare = __gnu_cxx: :__ops::_Iter_comp_iter<int (*)(const std::pair<std::__cxx11::basic_string<char> , std::__cxx11::basic_string<char> >&, const std::pair<std::__cxx11::basic_strin g<char>, std::__cxx11::basic_string<char> >&)>]’ /usr/include/c++/11/bits/stl_algo.h:5739:43:   required from ‘constexpr _FIter s td::max_element(_FIter, _FIter, _Compare) [with _FIter = __gnu_cxx::__normal_ite rator<const std::__cxx11::basic_string<char>*, std::vector<std::__cxx11::basic_s tring<char> > >; _Compare = int (*)(const std::pair<std::__cxx11::basic_string<c har>, std::__cxx11::basic_string<char> >&, const std::pair<std::__cxx11::basic_s tring<char>, std::__cxx11::basic_string<char> >&)]’ 46c6d2d4-7f45-4de9-8c1d-1a1e7ce72249.cpp:12:45:   required from here /usr/include/c++/11/bits/predefined_ops.h:158:30: error: invalid initialization of reference of type ‘const std::pair<std::__cxx11::basic_string<char>, std::__c xx11::basic_string<char> >&’ from expression of type ‘const std::__cxx11::basic_ string<char>’   158 |         { return bool(_M_comp(*__it1, *__it2)); }       |                       ~~~~~~~^~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -4,12 +4,18 @@
 
 std::vector<std::pair<std::string, std::string>> extensions;
 
+int len(const std::string& s) {
+    return s.length();
+}
+
 int compare(const std::pair<std::string, std::string>& a, const std::pair<std::string, std::string>& b) {
-    return a.second.size() - b.second.size();
+    int lenA = len(a.second);
+    int lenB = len(b.second);
+    return (lenA > lenB) ? 1 : ((lenA < lenB) ? -1 : 0);
 }
 
 std::string Strongest_Extension(std::string type, const std::vector<std::string>& strong) {
-    extensions.push_back({type, *std::max_element(strong.begin(), strong.end(), compare)});
+    extensions.push_back({type, *max_element(strong.begin(), strong.end(), compare)});
     return extensions.back().first + "." + extensions.back().second;
 }
 
