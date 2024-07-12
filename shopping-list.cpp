42c96f769b7554696ccad80b770847e312fa007c
beep boop <githubbot@vadim.me>
2024-07-12 08:20:07 +0200

    Solution 50 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/tubyrcq1 Prompt: red in this scope    19 |         cin >> discounts[i];       |                ^~~~~~~~~ eb5dc98f-a648-4a10-a588-d5d4ce1e0692.cpp:22:39: error: ‘prices’ was not declared  in this scope    22 |     float total = calculateTotalPrice(prices, discounts);       |                                       ^~~~~~ eb5dc98f-a648-4a10-a588-d5d4ce1e0692.cpp:22:47: error: ‘discounts’ was not decla red in this scope    22 |     float total = calculateTotalPrice(prices, discounts);       |                                               ^~~~~~~~~ eb5dc98f-a648-4a10-a588-d5d4ce1e0692.cpp:22:56: error: ‘calculateTotalPrice’ can not be used as a function    22 |     float total = calculateTotalPrice(prices, discounts);       |                                                        ^ eb5dc98f-a648-4a10-a588-d5d4ce1e0692.cpp:23:5: error: ‘cout’ was not declared in  this scope    23 |     cout << total << endl;       |     ^~~~ eb5dc98f-a648-4a10-a588-d5d4ce1e0692.cpp:23:22: error: ‘endl’ was not declared i n this scope    23 |     cout << total << endl;       |                      ^~~~

--- shopping-list.cpp
+++ shopping-list.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-float calculateTotalPrice(std::vector<float> prices, std::vector<float> discounts) {
+float calculateTotalPrice(vector<float> prices, vector<float> discounts) {
     float total = 0.0;
     for (int i = 0; i < prices.size(); ++i) {
         total += prices[i] * (1.0 - discounts[i] / 100.0);
@@ -11,21 +12,20 @@ float calculateTotalPrice(std::vector<float> prices, std::vector<float> discount
 }
 
 int main() {
-    int numItems;
-    std::cin >> numItems;
-
-    std::vector<float> prices(numItems);
-    for (int i = 0; i < numItems; ++i) {
-        std::cin >> prices[i];
+    int n;
+    cin >> n;
+    vector<float> prices(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> prices[i];
     }
 
-    std::vector<float> discounts(numItems);
-    for (int i = 0; i < numItems; ++i) {
-        std::cin >> discounts[i];
+    vector<float> discounts(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> discounts[i];
     }
 
     float total = calculateTotalPrice(prices, discounts);
-    std::cout << total << std::endl;
+    cout << total << endl;
 
     return 0;
 }
