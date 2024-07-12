728af72383783159db8418f1fe8935967561f3e3
beep boop <githubbot@vadim.me>
2024-07-13 00:25:34 +0200

    Solution 12 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lpo8cfzh Prompt: 3be662f6-5565-49d5-b0d8-28fd273a7251.cpp:8:88: error: narrowing conversion of ‘2 2595’ from ‘int’ to ‘char’ [-Wnarrowing] 3be662f6-5565-49d5-b0d8-28fd273a7251.cpp:8:88: error: narrowing conversion of ‘2 2604’ from ‘int’ to ‘char’ [-Wnarrowing] 3be662f6-5565-49d5-b0d8-28fd273a7251.cpp:8:88: error: narrowing conversion of ‘1 8776’ from ‘int’ to ‘char’ [-Wnarrowing] 3be662f6-5565-49d5-b0d8-28fd273a7251.cpp:8:88: error: narrowing conversion of ‘1 8774’ from ‘int’ to ‘char’ [-Wnarrowing] 3be662f6-5565-49d5-b0d8-28fd273a7251.cpp: At global scope: 3be662f6-5565-49d5-b0d8-28fd273a7251.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 3be662f6-5565-49d5-b0d8-28fd273a7251.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from 3be662f6-5565-49d5-b0d8-28fd273a7251.cpp:27: 3be662f6-5565-49d5-b0d8-28fd273a7251.cpp: In function ‘int main()’: 3be662f6-5565-49d5-b0d8-28fd273a7251.cpp:29:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘intToMiniRoman’?    29 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -5,7 +5,7 @@
 std::string intToMiniRoman(int n) {
     std::string roman;
     int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    char syb[] = {'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'};
+    char* syb[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     int i = 0;
     while (n > 0) {
         if (n >= val[i]) {
