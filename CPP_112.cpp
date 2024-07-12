c627bb882c9b058ff6375d7327b448e62abac183
beep boop <githubbot@vadim.me>
2024-07-12 07:34:22 +0200

    Solution 94 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i4swyuyp Prompt:       |                                     ^~~~~~ d6043672-aef9-4c6a-9860-cd462ff2c39d.cpp:28:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from d6043672-aef9-4c6a-9860-cd462ff2c39d.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from d6043672-aef9-4c6a-9860-cd462ff2c39d.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ d6043672-aef9-4c6a-9860-cd462ff2c39d.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d6043672-aef9-4c6a-9860-cd462ff2c39d.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ d6043672-aef9-4c6a-9860-cd462ff2c39d.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -19,7 +19,7 @@ bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b
     return a == b;
 }
 
-/*int main() {
-    assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
+int main() {
+    assert(issame(reverse_delete("mamma", "mia"), std::vector<std::string>{"", "True"}));
     return 0;
-}*/
+}
