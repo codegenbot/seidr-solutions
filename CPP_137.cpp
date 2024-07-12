ab8c7d263680c32a977d5f1ea87f4f8430f47883
beep boop <githubbot@vadim.me>
2024-07-12 15:58:40 +0200

    Solution 22 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pal7yz6s Prompt: efdd9c4a-e64c-42bf-a193-103e2f01e40b.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ efdd9c4a-e64c-42bf-a193-103e2f01e40b.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from efdd9c4a-e64c-42bf-a193-103e2f01e40b.cpp:30: efdd9c4a-e64c-42bf-a193-103e2f01e40b.cpp: In function ‘int main()’: efdd9c4a-e64c-42bf-a193-103e2f01e40b.cpp:32:13: error: ‘boost’ has not been decl ared    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ efdd9c4a-e64c-42bf-a193-103e2f01e40b.cpp:32:35: error: expected primary-expressi on before ‘>’ token    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-string compare_one(string a) {
+string compare_one(string a, int b) {
     double s1 = stod(a);
     if (a.find('x') != string::npos)
         return "None";
@@ -22,7 +22,7 @@ int main() {
     string a;
     cout << "Enter the first number: ";
     cin >> a;
-    cout << compare_one(a) << endl;
+    cout << compare_one(a, 0) << endl;
 
     return 0;
 }
