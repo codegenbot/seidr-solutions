f25b90b2895e0a29d0b04968b1eb098c6ce7b1f2
beep boop <githubbot@vadim.me>
2024-07-12 09:38:39 +0200

    Solution 67 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jkuz8vxn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1ef33bee-1d15-4628-b99d-3ca0a90c2 4a0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,14 +1,8 @@
-```
 import re
 
 def solve(text):
     return re.sub(r"(\s+)([a-zA-Z])", lambda m: "_" + m.group(2).upper(), text)
 
-text = "Enter the text: "
-while True:
-    try:
-        user_input = input(text)
-        print(solve(user_input))
-        break
-    except ValueError:
-        print("Invalid input. Please enter a valid sentence.")
+text = "Enter your name or title: "
+user_input = input(text)
+print(solve(user_input))
