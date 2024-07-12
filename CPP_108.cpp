8cdb07bfb0da61b1ee0a5458ea2a4dc7e8710286
beep boop <githubbot@vadim.me>
2024-07-12 22:36:43 +0200

    Solution 48 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lqy3vi1c Prompt: /usr/include/c++/11/bits/basic_string.h:760:30: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   760 |                   _M_destroy(_M_allocated_capacity);       |                              ^~~~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/bits/basic_string.h:765:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   765 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -6,20 +6,21 @@ int count_nums(char** nums, int size) {
     int count = 0;
     for (int i = 0; i < size; i++) {
         if (atoi(nums[i]) >= 0) {
-            std::string str = std::to_string(atoi(nums[i]));
+            std::string str = nums[i];
             int sum = 0;
-            for (char c : str) {
-                sum += c - '0';
+            while (str[0] != '\0') {
+                sum += abs(str[0] - '0');
             }
+            for(int j=str.length()-1; j>=0; j--) str[j]=str[j+1];
             if (sum % 2 != 0)
                 count++;
         } else {
-            std::string str = std::to_string(atoi(nums[i]));
+            std::string str = nums[i];
             int sum = 0, sign = 1;
-            for(int j=str.length()-1; j>=0; j--) {
-                sum += abs(str[j] - '0') * sign;
-                sign *= -1;
+            while (str[0] != '\0') {
+                sum += abs(str[0] - '0');
             }
+            for(int j=str.length()-1; j>=0; j--) str[j]=str[j+1];
             if (sign * sum % 2 != 0)
                 count++;
         }
@@ -37,7 +38,8 @@ int main() {
         std::cout << "Enter integer # " << i + 1 << ": ";
         int num;
         std::cin >> num;
-        std::string str = std::to_string(num);
+        std::string str;
+        str = std::to_string(num);
         nums[i] = new char[str.length() + 1];
         strcpy(nums[i], str.c_str());
     }
