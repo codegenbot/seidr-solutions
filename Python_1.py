c82dc653363002a07d26128159350398d03c779b
beep boop <githubbot@vadim.me>
2024-07-12 17:35:56 +0200

    Solution 79 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/iw090qzp Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3ba84f6a-b899-4e62-a2a3-a345f1901 f30.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_1.py
+++ Python_1.py
@@ -1,8 +1,9 @@
+```
 def separate_paren_groups(paren_string: str) -> List[str]:
     stack = []
     result = []
     temp = ''
-    
+
     for char in paren_string:
         if char == ' ':
             continue
