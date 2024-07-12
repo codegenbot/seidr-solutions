20fb687876ff7e238d48db2aea3aca270e1c7c31
beep boop <githubbot@vadim.me>
2024-07-12 18:34:50 +0200

    Solution 98 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/aybhzbcy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80436dec-0637-4bed-988f-02a31dd93 ba6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -1,18 +1,17 @@
-````
-def match_parens(lst):
+`def match_parens(lst):`
+```
     stack = []
     for s in lst:
-        for c in s:
-            if c == '(':
-                stack.append(c)
-            elif c == ')':
-                if len(stack) == 0:
-                    return 'No'
-                else:
-                    stack.pop()
-        if len(stack) != 0:
-            return 'No'
+        while '(' in s:
+            s = s.replace('(', '', 1)
+            stack.append('(')
+        while ')' in s:
+            s = s.replace(')', '', 1)
+            if len(stack) == 0:
+                return 'No'
+            else:
+                stack.pop()
     if len(stack) == 0:
         return 'Yes'
     else:
-        return 'No'
+        return 'No'```
