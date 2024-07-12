13513036e9f65fc75a889f24c347eab7d4426e04
beep boop <githubbot@vadim.me>
2024-07-12 16:44:21 +0200

    Solution 80 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/58tgfwqh Prompt: 15a0714c-5501-42b0-8d1f-bcac079c9d30.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 15a0714c-5501-42b0-8d1f-bcac079c9d30.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from 15a0714c-5501-42b0-8d1f-bcac079c9d30.cpp:35: 15a0714c-5501-42b0-8d1f-bcac079c9d30.cpp: In function ‘int main()’: 15a0714c-5501-42b0-8d1f-bcac079c9d30.cpp:37:13: error: ‘search’ was not declared  in this scope    37 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ^~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,10 +1,6 @@
-int testOriginalMain() {
+int main() {
     int numbers[] = {3, 10, 10, 9, 2};
     int size = sizeof(numbers)/sizeof(numbers[0]);
     assert(findMaxFreq(numbers, size) == -1);
     return 0;
-}
-
-int main() {
-    return testOriginalMain();
 }
