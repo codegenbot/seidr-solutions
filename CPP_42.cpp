ba8640a2e783ba4ca4dd8737097f2dbc1ed8a555
beep boop <githubbot@vadim.me>
2024-07-12 14:30:26 +0200

    Solution 20 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zj95medv Prompt:   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from e6958199-daac-488c-81c7-d6f65238cd97.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e6958199-daac-488c-81c7-d6f65238cd97.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from e6958199-daac-488c-81c7-d6f65238cd97.cpp:18: e6958199-daac-488c-81c7-d6f65238cd97.cpp: In function ‘int main()’: e6958199-daac-488c-81c7-d6f65238cd97.cpp:28:20: error: ‘incr_list’ was not decla red in this scope    28 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ e6958199-daac-488c-81c7-d6f65238cd97.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,9 +1,6 @@
-```cpp
+```c++
 #include <vector>
 #include <algorithm>
-#include <cassert>
-
-using namespace std;
 
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
@@ -17,7 +14,6 @@ vector<int> incr_list(vector<int> l){
     return result;
 }
 
-int main() {
+int main() { 
     assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6,3, 4, 4, 10, 1, 124}));
-    return 0;
 }
