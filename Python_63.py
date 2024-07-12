888d1b3a60a65bc1b3808d608937505ec391f7ee
beep boop <githubbot@vadim.me>
2024-07-12 08:05:44 +0200

    Solution 81 of Python/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zx5e9imf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/acf79f5e-b4cb-42ec-8067-28c4b2701 46e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_63.py
+++ Python_63.py
@@ -1,11 +1,10 @@
+```
 def fibfib(n: int):
     if n <= 1:
         return 0
     a, b = 0, 1
     total = 0
-    while True:
+    while b <= n:
         total += a
-        if a > n:
-            break
         a, b = b, a + b
     return total
