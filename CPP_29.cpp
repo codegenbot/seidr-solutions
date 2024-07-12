206af2125ed8b8b9a5b2709822f99777ea903295
beep boop <githubbot@vadim.me>
2024-07-12 15:13:25 +0200

    Solution 58 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fayzjz97 Prompt: his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 01b7baa7-2bf1-46a4-bed3-c9fe2934e258.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 01b7baa7-2bf1-46a4-bed3-c9fe2934e258.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 01b7baa7-2bf1-46a4-bed3-c9fe2934e258.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main(){       |     ^~~~ In file included from 01b7baa7-2bf1-46a4-bed3-c9fe2934e258.cpp:26: 01b7baa7-2bf1-46a4-bed3-c9fe2934e258.cpp: In function ‘int main()’: 01b7baa7-2bf1-46a4-bed3-c9fe2934e258.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -4,14 +4,14 @@
 std::vector<std::string> filter_by_prefix(std::vector<std::string> strings, std::string prefix) { 
     std::vector<std::string> result; 
     for(auto s : strings){ 
-        if (!s.empty() && s.find(prefix) == 0)
+        if (!s.empty() && s.find(prefix) == 0) 
             result.push_back(s); 
     } 
     return result;
 }
 
 int main(){
-    std::vector<std::string> strings = {"hello", "hi", "", "hey"};
+    std::vector<std::string> strings = {"hello", "hi", "hey"};
     std::string prefix = "h";
     
     std::vector<std::string> filteredStrings = filter_by_prefix(strings, prefix);
@@ -19,6 +19,8 @@ int main(){
     for(auto s : filteredStrings){
         std::cout << s << "\n";
     }
+
+    assert(filteredStrings == std::vector<std::string>({"hi", "hey"}));
 
     return 0;
 }
