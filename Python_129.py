5fe579ee935142c6f8085aa9f0c208696e2f74f8
beep boop <githubbot@vadim.me>
2024-07-12 08:25:07 +0200

    Solution 22 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/p0oo0rpk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/886fbafe-4906-4f6b-9fc4-33fe5481f 95d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -1,2 +1,5 @@
+```
 def min_length(all_paths):
-    return min(sum(int(path) for path in all_paths))
+    return min(sum(int(digit) for digit in path) for path in all_paths)
+
+print(min_length(['123', '456', '789']))
