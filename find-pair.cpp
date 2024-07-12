4d9e92bb2fd04c5d0e734b4d256f5a699d4cfbbe
beep boop <githubbot@vadim.me>
2024-07-12 11:05:01 +0200

    Solution 14 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/4074xw9y Prompt: n before ‘int’    11 |     map<int, int> numIndex;       |         ^~~ 0b055ac2-a103-4e0e-96e9-2a6720ff83eb.cpp:13:35: error: ‘nums’ was not declared i n this scope    13 |         int complement = target - nums[i];       |                                   ^~~~ 0b055ac2-a103-4e0e-96e9-2a6720ff83eb.cpp:14:13: error: ‘numIndex’ was not declar ed in this scope    14 |         if (numIndex.find(complement) != numIndex.end()) {       |             ^~~~~~~~ 0b055ac2-a103-4e0e-96e9-2a6720ff83eb.cpp:15:13: error: ‘cout’ was not declared i n this scope    15 |             cout << complement << endl << nums[i];       |             ^~~~ 0b055ac2-a103-4e0e-96e9-2a6720ff83eb.cpp:15:35: error: ‘endl’ was not declared i n this scope    15 |             cout << complement << endl << nums[i];       |                                   ^~~~ 0b055ac2-a103-4e0e-96e9-2a6720ff83eb.cpp:18:9: error: ‘numIndex’ was not declare d in this scope    18 |         numIndex[nums[i]] = i;       |         ^~~~~~~~

--- find-pair.cpp
+++ find-pair.cpp
@@ -18,7 +18,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         int complement = target - nums[i];
         if (numIndex.find(complement) != numIndex.end()) {
-            cout << complement << endl << nums[i];
+            cout << complement << " " << nums[i] << endl;
             break;
         }
         numIndex[nums[i]] = i;
