d0de210d0933d512cee41887aabf9be00874a83f
beep boop <githubbot@vadim.me>
2024-07-12 03:42:17 +0200

    Solution 43 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/xtk8wid6 Prompt:       |         ^~~~ 980d2555-13f6-4fa7-9331-57059479cefa.cpp:21:17: error: ‘input’ was not declared in this scope; did you mean ‘int’?    21 |         cout << input[i] << endl;       |                 ^~~~~       |                 int 980d2555-13f6-4fa7-9331-57059479cefa.cpp:21:29: error: ‘endl’ was not declared i n this scope    21 |         cout << input[i] << endl;       |                             ^~~~ 980d2555-13f6-4fa7-9331-57059479cefa.cpp:24:5: error: ‘cout’ was not declared in  this scope    24 |     cout << 0 << endl;       |     ^~~~ 980d2555-13f6-4fa7-9331-57059479cefa.cpp:24:18: error: ‘endl’ was not declared i n this scope    24 |     cout << 0 << endl;       |                  ^~~~ 980d2555-13f6-4fa7-9331-57059479cefa.cpp:26:33: error: ‘input’ was not declared in this scope; did you mean ‘int’?    26 |     for (int i = cut_index; i < input.size(); ++i) {       |                                 ^~~~~       |                                 int

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -9,14 +9,14 @@ using namespace std;
 int main() {
     vector<int> input;
     int num;
-
+    
     while (cin >> num) {
         input.push_back(num);
     }
-
+    
     int min_diff = INT_MAX;
     int cut_index = 0;
-
+    
     for (int i = 1; i < input.size(); ++i) {
         int diff = abs(accumulate(input.begin(), input.begin() + i, 0) - accumulate(input.begin() + i, input.end(), 0));
         if (diff < min_diff) {
@@ -24,18 +24,18 @@ int main() {
             cut_index = i;
         }
     }
-
+    
     for (int i = 0; i < cut_index; ++i) {
         cout << input[i] << endl;
     }
-
+    
     cout << 0 << endl;
-
+    
     for (int i = cut_index; i < input.size(); ++i) {
         cout << input[i] << endl;
     }
-
+    
     cout << 0 << endl;
-
+    
     return 0;
 }
