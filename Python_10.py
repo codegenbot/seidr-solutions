438e7e07e1a7158814a37bbcf7618bbac495582a
beep boop <githubbot@vadim.me>
2024-07-12 16:43:53 +0200

    Solution 76 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/y9kfabj7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/78dc83eb-5311-4c0c-a234-e35976e16 d2c.py", line 17, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/78dc83eb-5311-4c0c-a234-e35976e16 d2c.py", line 14, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,8 +1,10 @@
+```
 def make_palindrome(string: str) -> str:
     if is_palindrome(string):
         return string
+
     for i in range(len(string)):
-        if is_palindrome(string[i:]):
+        if len(string[i:]) < 2 or is_palindrome(string[i:] + string[:i][::-1]):
             half = string[:i+1]
-            second_half = string[i:]
-            return half + second_half[::-1] + half[0]
+            second_half = string[i:][::-1] + half
+            return second_half
