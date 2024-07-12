3ef943494dd18167742d36701b33858da9ae41a2
beep boop <githubbot@vadim.me>
2024-07-12 07:04:04 +0200

    Solution 18 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/d8swkc5z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f7b49c6a-262c-49db-8247-a20045416 f13.py", line 12, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f7b49c6a-262c-49db-8247-a20045416 f13.py", line 9, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,9 +1,5 @@
-from math import gcd
-
 def modp(n: int, p: int):
-    if n % p == 0:
-        return 0
-    elif gcd(p, n) == 1:
-        return pow(n, (p-1)//gcd(p,n), p) * n % p
+    if gcd(p, n) == 1:
+        return pow(n, (p-1)//p, p) * n % p
     else:
         return n
