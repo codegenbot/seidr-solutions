71bd91ffe1e8ee72990ab7f4ece22e71a49be6d8
beep boop <githubbot@vadim.me>
2024-07-12 15:27:58 +0200

    Solution 81 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2vwtr5ad Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 06ea98de-322c-4343-9251-5b7b12389a15.cpp:21:29:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -16,7 +16,7 @@ bool issame(vector<int> a, vector<int> b) {
 vector<int> remove_duplicates(vector<int> numbers) {
     vector<int> result;
     for (int num : numbers) {
-        if (find(result.begin(), result.end(), num) == result.end() || result.empty()) {
+        if (std::find(result.begin(), result.end(), num) == result.end() || result.empty()) {
             result.push_back(num);
         }
     }
