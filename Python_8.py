bc6eb3d92c0fb49b421df6fa0942a541c51d5bfc
beep boop <githubbot@vadim.me>
2024-07-12 15:50:20 +0200

    Solution 6 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/5jigyd4w Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/73351af6-579b-4922-b627-b58afd051 246.py", line 4     return (sum(numbers), 1 if not numbers else 1 if 0 in numbers else eval('*'. join(map(str, numbers)))            ^ SyntaxError: '(' was never closed

--- Python_8.py
+++ Python_8.py
@@ -2,18 +2,7 @@ from typing import List, Tuple
 
 
 def sum_product(numbers: List[int]) -> Tuple[int, int]:
-    sum_result = sum(numbers)
-    product_result = 1
-    for num in numbers:
-        product_result *= num
-    return sum_result, product_result
-
-
-# Read input from the user
-numbers = list(map(int, input().split()))
-
-# Call the function using user input
-result = sum_product(numbers)
-
-# Output the result
-print(*result)
+    return (
+        sum(numbers),
+        1 if not numbers else 1 if 0 in numbers else eval("*".join(map(str, numbers))),
+    )
