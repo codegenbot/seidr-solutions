2305645048473ea95bab160c985e6829d4c1f443
beep boop <githubbot@vadim.me>
2024-07-12 06:59:24 +0200

    Solution 60 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h3qtxl8o Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/af2a8c13-83cc-436e-bcbe-67a94edf8 f31.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,12 +1,17 @@
+```
 import random
 
 
 def make_palindrome() -> str:
     while True:
         s = "".join(random.choice("abcdefghijklmnopqrstuvwxyz") for _ in range(5))
-        if s == s[::-1]:
+        if is_palindrome(s):
             break
     return s
+
+
+def is_palindrome(s: str) -> bool:
+    return s == s[::-1]
 
 
 print("Enter your desired output (or press enter to get a random palindrome): ")
