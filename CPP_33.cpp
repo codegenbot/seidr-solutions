a433161084fa4bb7f1cf0086ee9b903ea1702c0e
beep boop <githubbot@vadim.me>
2024-07-12 15:09:03 +0200

    Solution 56 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d8rx6wrb Prompt:  in this scope    39 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a83930e3-2336-48b7-a574-a9e1aab6b2ba.cpp:39:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from a83930e3-2336-48b7-a574-a9e1aab6b2ba.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from a83930e3-2336-48b7-a574-a9e1aab6b2ba.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a83930e3-2336-48b7-a574-a9e1aab6b2ba.cpp:39:34: error: expected primary-expressi on before ‘int’    39 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -23,7 +23,7 @@ std::vector<int> sort_third(std::vector<int> l) {
                 temp.push_back(l[j]);
                 j++;
             }
-            sort(temp.begin(), temp.end());
+            std::sort(temp.begin(), temp.end());
             for (int k = 0; k < temp.size(); k++) {
                 result.push_back(temp[k]);
             }
