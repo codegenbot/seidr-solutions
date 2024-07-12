6d36fc0068e955fcd2c1a86ad2717e755a4baa8b
beep boop <githubbot@vadim.me>
2024-07-12 14:25:17 +0200

    Solution 16 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d8rx6wrb Prompt:    26 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ c6f223d3-d5f8-4695-adfc-f1c2bf61298b.cpp:26:27: error: ‘vector’ was not declared  in this scope    26 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c6f223d3-d5f8-4695-adfc-f1c2bf61298b.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from c6f223d3-d5f8-4695-adfc-f1c2bf61298b.cpp:25: c6f223d3-d5f8-4695-adfc-f1c2bf61298b.cpp: In function ‘int main()’: c6f223d3-d5f8-4695-adfc-f1c2bf61298b.cpp:35:20: error: ‘sort_third’ was not decl ared in this scope    35 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ c6f223d3-d5f8-4695-adfc-f1c2bf61298b.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,13 +1,8 @@
-```
 #include <vector>
-using namespace std;
+#include <algorithm>
 
-bool issame(vector<int> a,vector<int>b){
-    if(a.size()!=b.size())return false;
-    for(int i=0; i<a.size(); i++){
-        if(a[i]!=b[i])return false;
-    }
-    return true;
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
 vector<int> sort_third(vector<int> l) {
