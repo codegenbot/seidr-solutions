7ef67169cf6cf8f0f9c53da632a23518dd65a8f4
beep boop <githubbot@vadim.me>
2024-07-12 06:21:46 +0200

    Solution 14 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/n5bixqk7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/af275fc7-b189-42de-979e-992daf62c c38.py", line 1     """     ^ SyntaxError: unterminated triple-quoted string literal (detected at line 32)

--- Python_38.py
+++ Python_38.py
@@ -1,17 +1,17 @@
-```
 def decode_cyclic():
+    """ """
     s = input("Enter the string: ")
     result = ""
     i = 0
-    group = ""  
+    group = ""
     while i < len(s):
-        if i + 2 <= len(s):  
+        if i + 2 <= len(s):
             group = s[i : i + 2] + s[i]
         else:
             group = s[i:]
         if len(group) == 3:
             result += group[1] + group[0] + group[2]
-        elif len(group) == 2:  
+        elif len(group) == 2:
             result += group[1] + group[0]
         else:
             result += group
