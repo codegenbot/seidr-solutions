66c941239e6ac2717bdffb4e5c09df12d2935cc2
beep boop <githubbot@vadim.me>
2024-07-12 10:25:14 +0200

    Solution 48 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/w366zt3j Prompt: SE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |     ^~~~~~ edfc62c9-27bb-49c6-ac00-2276991d3b74.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <string>   +++ |+#include <cassert>     2 | edfc62c9-27bb-49c6-ac00-2276991d3b74.cpp: At global scope: edfc62c9-27bb-49c6-ac00-2276991d3b74.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ edfc62c9-27bb-49c6-ac00-2276991d3b74.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main(){       |     ^~~~ In file included from edfc62c9-27bb-49c6-ac00-2276991d3b74.cpp:19: edfc62c9-27bb-49c6-ac00-2276991d3b74.cpp: In function ‘int main()’: edfc62c9-27bb-49c6-ac00-2276991d3b74.cpp:21:13: error: ‘filp_case’ was not decla red in this scope    21 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,13 +1,13 @@
-#include <cassert>
+#include <iostream>
 #include <string>
-#include <cctype>
+#include <cassert>
 
 std::string flip_case(std::string str) {
-    for (int i = 0; i < str.length(); i++) {
-        if (std::islower(str[i])) {
-            str[i] = std::toupper(str[i]);
-        } else if (std::isupper(str[i])) {
-            str[i] = std::tolower(str[i]);
+    for (char& c : str) {
+        if (std::islower(c)) {
+            c = std::toupper(c);
+        } else if (std::isupper(c)) {
+            c = std::tolower(c);
         }
     }
     return str;
