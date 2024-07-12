44c3d2f820077a891a4d65fe69f65fc9e4fb7961
beep boop <githubbot@vadim.me>
2024-07-12 08:48:19 +0200

    Solution 24 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/oaf2ryzo Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bc262040-a0a2-43cc-ad83-40fa711fa 915.py", line 9, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bc262040-a0a2-43cc-ad83-40fa711fa 915.py", line 6, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bc262040-a0a2-43cc-ad83-40fa711fa 915.py", line 2, in sorted_list_sum     return sum(sorted([word for word in lst if len(word) % 2 == 0]))            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: unsupported operand type(s) for +: 'int' and 'str'

--- Python_149.py
+++ Python_149.py
@@ -1,2 +1,9 @@
 def sorted_list_sum(lst):
-    return "".join(sorted([word for word in lst if len(word) % 2 == 0], key=len))
+    return sum(
+        [
+            word
+            for word in sorted(
+                [(len(word), word) for word in lst if len(word) % 2 == 0], reverse=True
+            )
+        ]
+    )
