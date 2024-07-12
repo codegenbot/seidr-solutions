da5dbc6753dc9cfdedff366df5a10a4ac10cd996
beep boop <githubbot@vadim.me>
2024-07-12 09:28:13 +0200

    Solution 31 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4o64e9le Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ad98a49c-486a-4594-a639-96148da94 b3c.py", line 19, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ad98a49c-486a-4594-a639-96148da94 b3c.py", line 16, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,3 +1,21 @@
 def fizz_buzz(n: int):
-    count = sum(1 for i in range(1, n + 1) if (i % 11 == 0 or '7' in str(i)) and i % 13 != 0 and '7' not in str(i) and '3' not in str(i) and i % 21 != 0)
+    count = sum(
+        1
+        for i in range(1, n + 1)
+        if (i % 3 == 0 and i % 5 == 0)
+        or (
+            (i % 11 == 0 or "7" in str(i))
+            and i % 13 != 0
+            and "7" not in str(i)
+            and "3" not in str(i)
+            and i % 21 != 0
+        )
+        or (
+            i % 3 == 0
+            and i % 5 != 0
+            and i % 11 != 0
+            and "7" not in str(i)
+            and "3" not in str(i)
+        )
+    )
     return count
