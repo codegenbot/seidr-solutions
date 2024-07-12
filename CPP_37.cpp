b349dea0b6291a33edd419e02e0bbaba4797d315
beep boop <githubbot@vadim.me>
2024-07-12 15:53:12 +0200

    Solution 71 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/owlfkvjg Prompt: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = __gnu_cxx::__normal_iterator<float*, std::vector<float> >; _Tp = float; _Alloc = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:657:23:   required from ‘std::vector<_Tp, _Alloc>::vector(_InputIterator, _InputIterator, const allocator_type&) [with _In putIterator = __gnu_cxx::__normal_iterator<float*, std::vector<float> >; <templa te-parameter-2-2> = void; _Tp = float; _Alloc = std::allocator<float>; std::vect or<_Tp, _Alloc>::allocator_type = std::allocator<float>]’ fc008fa2-88f2-4eb2-85c0-3aa1df7f72e4.cpp:19:73:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,17 +1,4 @@
 ```cpp
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-bool same(std::vector<float> a, std::vector<float> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); i++)
-        if (std::abs(a[i] - b[i]) > 1e-6)
-            return false;
-    return true;
-}
-
 std::vector<float> sort_even(std::vector<float> l) {
     std::vector<float> result;
     for (float i : l) {
@@ -24,9 +11,4 @@ std::vector<float> sort_even(std::vector<float> l) {
         }
     }
     return result;
-}
-
-int main() {
-    assert(same(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
-    return 0;
 }
