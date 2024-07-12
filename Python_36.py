67c6df866cbe0752b34a64610ef45b2ae4e59cc9
beep boop <githubbot@vadim.me>
2024-07-12 11:02:18 +0200

    Solution 1 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/97a0pe46 Prompt: Complete the following code given the task description and function signature.

--- Python_36.py
+++ Python_36.py
@@ -1,8 +1,9 @@
 ```
 def fizz_buzz(n: int):
     count = 0
-    for i in range(n-1, -1, -1):
-        if i % 11 == 0 or i % 13 == 0:
-            str_i = str(i)
-            count += str_i.count('7')
+    for i in range(11, n, 11):
+        while i < n and i % 13 == 0:
+            if str(i).count('7') > 0:
+                count += 1
+            i += 1
     return count
