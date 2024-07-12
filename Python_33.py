bdc0aabe265b54da73a305786d196747914722d6
beep boop <githubbot@vadim.me>
2024-07-12 09:21:01 +0200

    Solution 26 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/3xd4xzwr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2b8baeb1-a964-4772-9a39-b542a2f62 36e.py", line 11, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2b8baeb1-a964-4772-9a39-b542a2f62 36e.py", line 6, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2b8baeb1-a964-4772-9a39-b542a2f62 36e.py", line 2, in sort_third     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2b8baeb1-a964-4772-9a39-b542a2f62 36e.py", line 2, in <listcomp>     return [            ^ TypeError: 'int' object is not iterable

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,2 @@
 def sort_third(l: list):
-    return [
-        elem for sublist in sorted(zip(*[iter(l)] * 3)) for i in sublist for elem in [i]
-    ]
+    return [elem for sublist in sorted(zip(*[iter(l)] * 3)) for elem in sublist]
