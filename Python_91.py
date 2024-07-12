7b70ba180f13a4967c04715bd3464f68b2800a73
beep boop <githubbot@vadim.me>
2024-07-12 03:04:57 +0200

    Solution 1 of Python/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/4wzgnw0p Prompt: Complete the following code given the task description and function signature.

--- Python_91.py
+++ Python_91.py
@@ -1,3 +1,2 @@
-```
 def is_bored(S):
-    return sum(1 for sentence in S.split('. ') if sentence.strip().startswith('I'))
+    return sum(1 for sentence in S.split('.?!") if sentence.strip().startswith('I'))
