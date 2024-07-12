7acdf461d4f064ea401035902ab7872f13b69348
beep boop <githubbot@vadim.me>
2024-07-12 11:30:54 +0200

    Solution 48 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/37by9irg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eaaaaaa7-07bf-46fd-ae5d-db010864b c24.py", line 31, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eaaaaaa7-07bf-46fd-ae5d-db010864b c24.py", line 27, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -5,15 +5,13 @@ def encode(message):
     for char in message:
         if char.isalpha():
             char_code = ord(char)
-            if char.lower() in vowels:
-                new_code = char_code + 1
-            else:
-                new_code = char_code + 2
 
-            if char.islower() and new_code > 122:
-                new_code -= 26
-            elif char.isupper() and new_code > 90:
-                new_code -= 26
+            step = 1 if char.lower() in vowels else 2
+
+            if char.islower():
+                new_code = (char_code - ord("a") + step) % 26 + ord("a")
+            else:
+                new_code = (char_code - ord("A") + step) % 26 + ord("A")
 
             encoded_message += chr(new_code)
         else:
