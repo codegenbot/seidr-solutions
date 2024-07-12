b544e7762cc0b8ceb4618627fff2b897d2a9ac06
beep boop <githubbot@vadim.me>
2024-07-12 02:45:21 +0200

    Solution 4 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/3vck5e53 Prompt:       |                             ^~~ cb143497-c194-4d42-9251-3d1e08098e70.cpp:26:33: error: expected primary-expressi on before ‘char’    26 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ cb143497-c194-4d42-9251-3d1e08098e70.cpp:26:38: error: expected primary-expressi on before ‘int’    26 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ cb143497-c194-4d42-9251-3d1e08098e70.cpp:26:44: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from cb143497-c194-4d42-9251-3d1e08098e70.cpp:25: cb143497-c194-4d42-9251-3d1e08098e70.cpp: In function ‘int main()’: cb143497-c194-4d42-9251-3d1e08098e70.cpp:40:20: error: ‘histogram’ was not decla red in this scope    40 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ cb143497-c194-4d42-9251-3d1e08098e70.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,12 +1,6 @@
-#include <iostream>
 #include <map>
-#include <cassert>
 
-using namespace std;
-
-bool issame(map<char, int> a, map<char, int> b) {
-    return a == b;
-}
+bool issame(map<char, int> a, map<char, int> b);
 
 map<char, int> histogram(string test) {
     map<char, int> result;
@@ -32,7 +26,11 @@ map<char, int> histogram(string test) {
     return result;
 }
 
+bool issame(map<char, int> a, map<char, int> b) {
+    return a == b;
+}
+
 int main() {
-    assert(issame(histogram("a"), {{'a', 1}}));
+    issame(histogram("a"), {{'a', 1}});
     return 0;
 }
