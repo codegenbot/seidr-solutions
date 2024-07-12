e7c8eab57963b6e19a740f385dc80a0e99fc84e5
beep boop <githubbot@vadim.me>
2024-07-12 17:21:46 +0200

    Solution 16 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vs7otlte Prompt: 66c6b1cc-204c-42cd-81d2-7d615c670000.cpp:4:1: error: ‘string’ does not name a ty pe     4 | string anti_shuffle(string s) {       | ^~~~~~ In file included from 66c6b1cc-204c-42cd-81d2-7d615c670000.cpp:26: 66c6b1cc-204c-42cd-81d2-7d615c670000.cpp: In function ‘int main()’: 66c6b1cc-204c-42cd-81d2-7d615c670000.cpp:28:13: error: ‘anti_shuffle’ was not de clared in this scope    28 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,21 +1,29 @@
-Here is the completed code:
-
-```cpp
 #include <algorithm>
-#include <string>
-
 using namespace std;
 
-string anti_shuffle(string s) {
+string antiShuffle(string s) {
     string result = "";
-    size_t prev = 0;
-    for (size_t i = 0; i <= s.size(); ++i) {
-        if (i == s.size() || isspace(s[i])) {
-            string word = s.substr(prev, i - prev);
-            sort(word.begin(), word.end());
-            result += word + (i < s.size() ? " " : "");
-            prev = i;
+    string word;
+    for (char c : s) {
+        if (c == ' ') {
+            result += word + " ";
+            word = "";
+        } else {
+            int nextCharIndex = 0;
+            while (nextCharIndex < word.length()) {
+                if (word[nextCharIndex] <= c) {
+                    break;
+                }
+                nextCharIndex++;
+            }
+            word.insert(nextCharIndex, 1, c);
         }
     }
+    result += word;
     return result;
+}
+
+int main() {
+    assert(antiShuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
+    return 0;
 }
