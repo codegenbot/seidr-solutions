ca5ea3d0bfeaf80262eb8f3b21cae8092f396ff2
beep boop <githubbot@vadim.me>
2024-07-12 17:11:09 +0200

    Solution 29 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gwb2dchn Prompt: Program did not receive expected input

--- Python_144.py
+++ Python_144.py
@@ -3,4 +3,4 @@ from fractions import Fraction
 def simplify():
     x = float(input("Enter numerator: "))
     n = float(input("Enter denominator: "))
-    return f"The simplified fraction is {Fraction(x)/Fraction(n)}"
+    return f"The simplified fraction is {Fraction(x)/n}"
