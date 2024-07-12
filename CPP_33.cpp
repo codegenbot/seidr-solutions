21908c22d2bf68a2351c2745aa71c6b4b09140d4
beep boop <githubbot@vadim.me>
2024-07-12 15:33:52 +0200

    Solution 78 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d8rx6wrb Prompt:       |                                  ^~~ 6424bc48-d933-402a-b42b-04712836ed78.cpp:4:39: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6424bc48-d933-402a-b42b-04712836ed78.cpp:3: 6424bc48-d933-402a-b42b-04712836ed78.cpp: In function ‘int main()’: 6424bc48-d933-402a-b42b-04712836ed78.cpp:13:20: error: ‘sort_third’ was not decl ared in this scope    13 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 6424bc48-d933-402a-b42b-04712836ed78.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~ 6424bc48-d933-402a-b42b-04712836ed78.cpp:13:5: error: ‘__assert_fail’ was not de clared in this scope    13 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |     ^~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,36 +1,15 @@
-#include <algorithm>
-#include <assert.h>
+```cpp
 #include <vector>
+#include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
-            return false;
-    }
-    return true;
+std::size_t issame(std::vector<int> a, std::vector<int> b) {
+    return (a.size() == b.size()) && equal(a.begin(), a.end(), b.begin());
 }
 
-std::vector<int> sort_third(std::vector<int> l) {
-    std::vector<int> result;
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 3 == 0) {
-            auto temp = l.begin() + i;
-            while(temp != l.end() && *temp % 3 == 0) {
-                temp++;
-            }
-            std::sort(std::make_reverse_iterator(temp), l.end());
-            for (; temp != l.end(); temp++) {
-                if (*temp % 3 == 0) {
-                    result.push_back(*temp);
-                } else {
-                    break;
-                }
-            }
-        } else {
-            result.push_back(l[i]);
-        }
+std::vector<int> sort_third(std::vector<int> v) {
+    sort(v.begin(), v.end());
+    for (int i = 1; i < v.size(); i += 2) {
+        swap(v[i - 1], v[i]);
     }
-    return result;
+    return v;
 }
