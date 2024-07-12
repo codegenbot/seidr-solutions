0de17d17534bbc64ef3a0cda840f4b81fd14e06c
beep boop <githubbot@vadim.me>
2024-07-12 18:57:00 +0200

    Solution 7 of CPP/31, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/cfqxaqy5 Prompt: 12f9cb39-7352-4b18-b4a3-2de32e9bdcbd.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 12f9cb39-7352-4b18-b4a3-2de32e9bdcbd.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 12f9cb39-7352-4b18-b4a3-2de32e9bdcbd.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 12f9cb39-7352-4b18-b4a3-2de32e9bdcbd.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ In file included from 12f9cb39-7352-4b18-b4a3-2de32e9bdcbd.cpp:11: 12f9cb39-7352-4b18-b4a3-2de32e9bdcbd.cpp: In function ‘int main()’: 12f9cb39-7352-4b18-b4a3-2de32e9bdcbd.cpp:13:13: error: ‘is_prime’ was not declar ed in this scope    13 |     assert (is_prime(13441 * 19) == false);       |             ^~~~~~~~

--- CPP_31.cpp
+++ CPP_31.cpp
@@ -1,19 +1,22 @@
 ```cpp
 #include <iostream>
-#include <string>
+using namespace std;
 
-bool is_prime(int n) {
-    bool is_divisible = false;
-    for (int i = 2; i * i <= n && !is_divisible; ++i) {
-        if (n % i == 0) {
-            is_divisible = true;
-        }
+bool is_prime(int num) {
+    if (num <= 1)
+        return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return false;
     }
-    return !is_divisible;
+    return true;
 }
 
 int main() {
     int num;
-    std::cin >> num;
-    std::cout << std::to_string(is_prime(num)); 
+    cin >> num;
+    if (is_prime(num))
+        cout << "true";
+    else
+        cout << "false";
 }
