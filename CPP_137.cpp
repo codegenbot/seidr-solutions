f72b1639ead9dc9cbca7a705dc7433d7a3562e54
beep boop <githubbot@vadim.me>
2024-07-12 23:24:42 +0200

    Solution 23 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jkmyomz8 Prompt: In file included from 72142a79-f199-4def-ba75-7287b7d71712.cpp:1: /usr/include/c++/11/any: In instantiation of ‘_ValueType std::any_cast(std::any& ) [with _ValueType = std::__cxx11::basic_string<char>]’: 72142a79-f199-4def-ba75-7287b7d71712.cpp:15:51:   required from here /usr/include/c++/11/any:491:21: error: static assertion failed: Template argumen t must be constructible from an lvalue.   491 |       static_assert(is_constructible_v<_ValueType, _Up&>,       |                     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/any:491:21: note: ‘std::is_constructible_v<std::__cxx11::bas ic_string<char>, std::__cxx11::basic_string<char, std::char_traits<char>, std::a llocator<char> >&>’ evaluates to false /usr/include/c++/11/any:495:16: error: invalid use of incomplete type ‘class std ::__cxx11::basic_string<char>’   495 |         return static_cast<_ValueType>(*__p);       |                ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/bits/shared_ptr.h:52,                  from /usr/include/c++/11/memory:77,                  from 72142a79-f199-4def-ba75-7287b7d71712.cpp:3: /usr/include/c++/11/bits/stringfwd.h:74:11: note: declaration of ‘class std::__c xx11::basic_string<char>’    74 |     class basic_string;       |           ^~~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,26 +1,8 @@
-#include <any>
-#include <typeindex>
-#include <memory>
-#include <string>
+#include <algorithm>
+// ...
 
-bool operator>(const std::string& x, const std::string& y) {
-    return x.compare(y) > 0;
-}
-
-std::any compare_one(std::any a, std::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int x = std::any_cast<int>(a);
-        int y = std::any_cast<int>(b);
-        return (x > y) ? a : ((y > x) ? b : a);
-    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        float x = std::any_cast<float>(a);
-        float y = std::any_cast<float>(b);
-        return (x > y) ? a : ((y > x) ? b : a);
-    } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        std::string x = std::any_cast<std::string>(a);
-        std::string y = std::any_cast<std::string>(b);
-        return (x > y) ? a : ((y > x) ? b : a); 
-    } else {
-        return a;
-    }
+else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+    std::string x = std::any_cast<std::string>(a);
+    std::string y = std::any_cast<std::string>(b);
+    return (x < y) ? a : ((y < x) ? b : a); 
 }
