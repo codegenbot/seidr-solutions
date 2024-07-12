775f42f9316aca0bc231f83eac6f95fd619b5694
beep boop <githubbot@vadim.me>
2024-07-12 19:36:08 +0200

    Solution 51 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0hrfoi8h Prompt: s compound expression in initializer [-fpermissive]    47 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 399bd99e-c450-47f1-baf8-d1f187584d83.cpp:55:5: error: redefinition of ‘int main( )’    55 | int main(){       |     ^~~~ 399bd99e-c450-47f1-baf8-d1f187584d83.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from 399bd99e-c450-47f1-baf8-d1f187584d83.cpp:46: 399bd99e-c450-47f1-baf8-d1f187584d83.cpp: In function ‘int main()’: 399bd99e-c450-47f1-baf8-d1f187584d83.cpp:56:20: error: ‘rolling_max’ was not dec lared in this scope    56 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 399bd99e-c450-47f1-baf8-d1f187584d83.cpp:56:19: error: ‘issame’ cannot be used a s a function    56 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -3,13 +3,14 @@
 #include <vector>
 #include <algorithm>
 
-bool operator==(const std::vector<int>& v1, const std::vector<int>& v2) {
-    return (v1.size() == v2.size()) && std::equal(v1.begin(), v1.end(), v2.begin(),
-                                                  [](int i1, int i2){return i1 == i2;});
-}; 
+using namespace std;
 
-std::vector<int> rolling_max(std::vector<int> numbers) {
-    std::vector<int> maxNumbers;
+bool operator==(const vector<int>& v1, const vector<int>& v2) {
+    return (v1.size() == v2.size()) && equal(v1.begin(), v1.end(), v2.begin());
+}
+
+vector<int> rolling_max(vector<int> numbers) {
+    vector<int> maxNumbers;
     int currentMax = INT_MIN;
 
     for (int num : numbers) {
@@ -21,27 +22,33 @@ std::vector<int> rolling_max(std::vector<int> numbers) {
     return maxNumbers;
 }
 
+bool issame(vector<int> a, vector<int> b) {
+    return a == b; // implement your comparison logic here
+}
+
 int main() {
-    std::vector<int> numbers;
+    vector<int> numbers;
     int num;
 
-    std::cout << "Enter numbers (enter -1 to stop): ";
-    while ((std::cin >> num) && (num != -1)) {
+    cout << "Enter numbers (enter -1 to stop): ";
+    while ((cin >> num) && (num != -1)) {
         numbers.push_back(num);
     }
 
-    std::vector<int> maxNumbers = rolling_max(numbers);
+    vector<int> maxNumbers = rolling_max(numbers);
 
     for (int i = 0; i < maxNumbers.size(); i++) {
-        std::cout << "Maximum up to " << i + 1 << " is " << maxNumbers[i] << std::endl;
+        cout << "Maximum up to " << i + 1 << " is " << maxNumbers[i] << endl;
     }
     
-    std::vector<int> v1({3, 2, 3, 100, 3});
-    std::vector<int> v2({3, 3, 3, 100, 100});
+    vector<int> v1({3, 2, 3, 100, 3});
+    vector<int> v2({3, 3, 3, 100, 100});
     if (v1 == v2)
-        std::cout << "Vectors are equal" << std::endl;
+        cout << "Vectors are equal" << endl;
     else
-        std::cout << "Vectors are not equal" << std::endl;
+        cout << "Vectors are not equal" << endl;
+    
+    assert(issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100}));
     
     return 0;
 }
