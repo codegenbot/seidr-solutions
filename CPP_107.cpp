89f7dfc45e9b2fb542562e79e7896bbba8a23341
beep boop <githubbot@vadim.me>
2024-07-12 03:42:01 +0200

    Solution 39 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/52q4ikre Prompt:       |           ^~~~~~ In file included from 7178f5b0-247e-4f94-83bb-029372370152.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7178f5b0-247e-4f94-83bb-029372370152.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7178f5b0-247e-4f94-83bb-029372370152.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7178f5b0-247e-4f94-83bb-029372370152.cpp:13: 7178f5b0-247e-4f94-83bb-029372370152.cpp: In function ‘int main()’: 7178f5b0-247e-4f94-83bb-029372370152.cpp:23:20: error: ‘even_odd_palindrome’ was  not declared in this scope    23 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 7178f5b0-247e-4f94-83bb-029372370152.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,12 +1,6 @@
 #include <vector>
 #include <cassert>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b){
-    assert(a.size() == b.size());
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    return a == b;
 }
