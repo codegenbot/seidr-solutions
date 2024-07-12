55e95c5b8efc8487b0e3686ce30af2a2ba683c66
beep boop <githubbot@vadim.me>
2024-07-12 23:14:01 +0200

    Solution 67 of CPP/100, ratio 0.6 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ndqvsqhd Prompt: 42378d1c-d462-48ac-a679-5d39cefc4d78.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 42378d1c-d462-48ac-a679-5d39cefc4d78.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,14 +1,20 @@
+#include <vector>
+#include <cassert>
+#include <iostream>
+#include <initializer_list>
+
+using namespace std;
+
+bool same(const vector<int>& a, const vector<int>& b) {
+    return a == b;
+}
+
 vector<int> make_a_pile(int n) {
+    int stones = 1;
     vector<int> pile;
-    int stones = n;
     for (int i = 0; i < n; i++) {
-        if (stones % 2 == 1) {
-            pile.push_back(stones);
-            stones += 2;
-        } else {
-            pile.push_back(stones);
-            stones += 1;
-        }
+        pile.push_back(stones);
+        stones++;
     }
     return pile;
 }
