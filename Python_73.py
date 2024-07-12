4cf1f9ed80fb0c62c684b642a0ac190cee90dba4
beep boop <githubbot@vadim.me>
2024-07-12 16:26:14 +0200

    Solution 16 of Python/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/3njjqjah Prompt: Complete the following code given the task description and function signature.

--- Python_73.py
+++ Python_73.py
@@ -12,7 +12,7 @@ def smallest_change(arr):
                     changes += 1
                     break
             else:
-                changes += right - left
+                changes += (right - left)
             break
         left += 1
         right -= 1
