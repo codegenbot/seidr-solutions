965ac1a1669e1b3f05321d2c12e87b6808b7523f
beep boop <githubbot@vadim.me>
2024-07-12 14:08:05 +0200

    Solution 48 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3pjnfwz2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f57b1a2-dcab-4f9a-9bda-628e5dbef 530.py", line 26, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f57b1a2-dcab-4f9a-9bda-628e5dbef 530.py", line 23, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,19 +1,10 @@
-def is_palindrome(s: str) -> bool:
-    return s == s[::-1]
-
-def make_palindrome(string: str) -> str:
-    if is_palindrome(string):
-        return string
-    n = len(string)
-    new_string = "#" + "#".join(list(string)) + "#"
+def make_palindrome(s: str) -> str:
+    if is_palindrome(s):
+        return s * 2
+    n = len(s)
+    new_string = "#" + "#".join(list(s)) + "#"
     i_max = n - 1
     while not is_palindrome(new_string):
         i_max -= 1
-        new_string = new_string[: i_max + 1] + "#"
-    palindrome = ""
-    for i in range(2 * n - 1):
-        if i < n:
-            palindrome += string[i // 2]
-        elif i >= n and i < 2 * n - 1:
-            palindrome += string[(n - 1) - ((i - n) // 2)]
-    return palindrome
+        new_string = new_string[:i_max + 1] + "#"
+    return "".join(c for c in new_string if c != "#") * (n // 2)
