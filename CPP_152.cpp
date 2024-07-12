6b5372e3db432392023be96dc67419a2eba0cb35
beep boop <githubbot@vadim.me>
2024-07-13 01:53:34 +0200

    Solution 64 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uswfokmt Prompt: 111a0aab-696b-4b2a-b828-9627c421a6cf.cpp:58:13: note: ‘assert’ is defined in hea der ‘<cassert>’; did you forget to ‘#include <cassert>’? 111a0aab-696b-4b2a-b828-9627c421a6cf.cpp: At global scope: 111a0aab-696b-4b2a-b828-9627c421a6cf.cpp:73:5: error: redefinition of ‘int main( )’    73 | int main(){       |     ^~~~ 111a0aab-696b-4b2a-b828-9627c421a6cf.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main() {       |     ^~~~ In file included from 111a0aab-696b-4b2a-b828-9627c421a6cf.cpp:64: 111a0aab-696b-4b2a-b828-9627c421a6cf.cpp: In function ‘int main()’: 111a0aab-696b-4b2a-b828-9627c421a6cf.cpp:74:27: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    74 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~       |                           |       |                           <brace-enclosed initializer list> 111a0aab-696b-4b2a-b828-9627c421a6cf.cpp:12:17: note:   initializing argument 1 of ‘int compare(int, int)’    12 | int compare(int a, int b) {       |             ~~~~^

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -32,7 +32,7 @@ vector<int> getResult(vector<int> game, vector<int> guess) {
     return res;
 }
 
-void testGame() {
+int main() {
     int n;
     cin >> n;
     vector<int> game(n), guess(n);
@@ -49,20 +49,16 @@ void testGame() {
     
     vector<int> testGame = {1,2,3,5};
     vector<int> testGuess = {-1,2,3,4};
-    int result = compare(testGame[0],testGuess[0]);
-    for(int i = 1; i < testGame.size();i++){
-        if(issame(testGame[i],testGuess[i]))
-            continue;
-        else if(result == 1)
-            assert(compare(testGame[i],testGuess[i]) == -1);
-        else
-            assert(result == compare(testGame[i],testGuess[i]));
+    if(issame(compare(testGame[0],testGuess[0]), 2)) {
+        for(int i = 1; i < testGame.size();i++){
+            if(issame(testGame[i],testGuess[i]))
+                continue;
+            else if(compare(testGame[i],testGuess[i]) == -1)
+                assert(compare(testGame[i],testGuess[i]) == -1);
+            else
+                assert(compare(testGame[i],testGuess[i]) == 0);
+        }
     }
     
-    return;
-}
-
-int main() {
-    testGame();
     return 0;
 }
