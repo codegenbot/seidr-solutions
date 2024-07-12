adfbb480ce31c18409768bb462612bc8b4bde2e2
beep boop <githubbot@vadim.me>
2024-07-12 16:58:48 +0200

    Solution 3 of Python/132, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/a034t9jx Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/904dfcd2-ddd4-497a-9fc2-bee15bff6 2f6.py", line 15, in <module>     check(is_nested)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/904dfcd2-ddd4-497a-9fc2-bee15bff6 2f6.py", line 12, in check     assert is_nested("]]]]]]]]") == False            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_132.py
+++ Python_132.py
@@ -3,6 +3,8 @@ def is_nested(string):
     for char in string:
         if char == '[':
             stack.append(char)
-        elif char == ']' and stack:
+        elif char == ']' and stack and stack[-1] == '[':
             stack.pop()
-    return len(stack) < 2
+        else:
+            return False
+    return not stack
