120575f0d97a1c8f891b1003867a4136025dd8d3
beep boop <githubbot@vadim.me>
2024-07-12 18:00:59 +0200

    Solution 86 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qerfd1ru Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1bb1e258-b85b-46a9-9b67-91dd13747 c78.py", line 14, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1bb1e258-b85b-46a9-9b67-91dd13747 c78.py", line 9, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1bb1e258-b85b-46a9-9b67-91dd13747 c78.py", line 2, in sort_even     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1bb1e258-b85b-46a9-9b67-91dd13747 c78.py", line 4, in <listcomp>     for (i, x), (_, y) in zip(         ^^^^^^ TypeError: cannot unpack non-iterable int object

--- Python_37.py
+++ Python_37.py
@@ -1,3 +1,5 @@
 def sort_even(l: list):
-    even = sorted([x for i, x in enumerate(l) if i % 2 == 0])
-    return [x if i % 2 != 0 else y for i, x in enumerate(l)]
+    return [
+        y if i % 2 != 0 else x
+        for i, x in enumerate(sorted([x for i, x in enumerate(l) if i % 2 == 0]))
+    ]
