4bffc143bf666c1061a749cefba387e5492031ad
beep boop <githubbot@vadim.me>
2024-07-13 00:13:14 +0200

    Solution 44 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jkmyomz8 Prompt: e");       |             ^~~~~ 1b21d6ef-ce05-46d8-b96b-1c97c64de28e.cpp:32:29: error: ‘string’ was not declared  in this scope    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 1b21d6ef-ce05-46d8-b96b-1c97c64de28e.cpp:32:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 1b21d6ef-ce05-46d8-b96b-1c97c64de28e.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 1b21d6ef-ce05-46d8-b96b-1c97c64de28e.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 1b21d6ef-ce05-46d8-b96b-1c97c64de28e.cpp: In function ‘int compare_one(const str ing&, int)’: 1b21d6ef-ce05-46d8-b96b-1c97c64de28e.cpp:11:1: warning: control reaches end of n on-void function [-Wreturn-type]    11 | }       | ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,13 +3,13 @@
 
 int compare_one(const std::string& s, int i) {
     if (i == 1)
-        return std::stoi(s) - 10;
+        return std::stod(s) - 10;
     else if (i == 2)
-        return std::stoi(s);
+        return std::stod(s);
     else if (i == 3)
-        return std::stoi(s) + 10;
-    else 
-        return 0;
+        return std::stod(s) + 10;
+    else
+        return 0; // default value for i not equal to 1 or 2
 }
 
 int main() {
@@ -20,12 +20,10 @@ int main() {
     std::cin >> b;
 
     if (a.size() > 0 && b.size() > 0) {
-        std::string a_num = a; 
-        std::string b_num = b;
-        int x = compare_one(a_num, 1);
-        int y = compare_one(b_num, 2);
+        int x = compare_one(a, 1);
+        int y = compare_one(b, 2);
 
-        return (x < y) ? -1 : ((y < x) ? 1 : 0); return 0;
+        return (x < y) ? -1 : ((y < x) ? 1 : 0);
     }
     
     return 0;
