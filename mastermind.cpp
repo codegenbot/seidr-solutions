fb3587e2f008ffbb72071599f9d969ea8cf506c7
beep boop <githubbot@vadim.me>
2024-07-12 10:50:28 +0200

    Solution 90 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/j8u5m9on Prompt: Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,28 +1,41 @@
+int whitePegs(string code, string guess) {
+    int count = 0;
+    for (int i = 0; i < 4; ++i) {
+        if (code[i] != guess[i]) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int blackPegs(string code, string guess) {
+    int count = 0;
+    vector<int> freq(6, 0);
+    for (int i = 0; i < 4; ++i) {
+        if (code[i] == guess[i]) {
+            count++;
+        } else {
+            freq[code[i] - 'A']++;
+            freq[guess[i] - 'A']--;
+        }
+    }
+    for (int i = 0; i < 6; ++i) {
+        if (freq[i] > 0) {
+            count += freq[i];
+        }
+    }
+    return count;
+}
+
 int main() {
     string code, guess;
     cin >> code >> guess;
     
-    int blackPegs = 0, whitePegs = 0;
-    int codeCount[6] = {0};
-    int guessCount[6] = {0};
+    int white = whitePegs(code, guess);
+    int black = blackPegs(code, guess);
     
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            ++blackPegs;
-        } else {
-            ++codeCount[code[i] - 'A'];
-            ++guessCount[guess[i] - 'A'];
-        }
-    }
-    
-    for (int i = 0; i < 6; ++i) {
-        whitePegs += min(codeCount[i], guessCount[i]);
-    }
-    
-    whitePegs -= blackPegs;
-    
-    cout << whitePegs << endl;
-    cout << blackPegs << endl;
+    cout << white << endl;
+    cout << black << endl;
     
     return 0;
 }
