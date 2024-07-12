5b04995dd26dcd5723b0df8c580402057028a750
beep boop <githubbot@vadim.me>
2024-07-12 17:25:17 +0200

    Solution 74 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q8x7adb5 Prompt:    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 76db85df-3aee-4065-9a02-ffd3350bf8ed.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 76db85df-3aee-4065-9a02-ffd3350bf8ed.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 76db85df-3aee-4065-9a02-ffd3350bf8ed.cpp:23: 76db85df-3aee-4065-9a02-ffd3350bf8ed.cpp: In function ‘int main()’: 76db85df-3aee-4065-9a02-ffd3350bf8ed.cpp:33:29: error: invalid initialization of  reference of type ‘const std::vector<std::__cxx11::basic_string<char> >&’ from expression of type ‘<brace-enclosed initializer list>’    33 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ~~~~~~~~~^~~~~~~~~~~ 76db85df-3aee-4065-9a02-ffd3350bf8ed.cpp:8:68: note: in passing argument 1 of ‘s td::vector<std::__cxx11::basic_string<char> > by_length(const std::vector<std::_ _cxx11::basic_string<char> >&)’     8 | d::vector<std::string> by_length(const std::vector<std::string>& vec) {       |                                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,17 +1,4 @@
-#include <vector>
-#include <string>
-using namespace std;
-
-bool isSame(vector<string> v1, vector<string> v2) {
-    if (v1.size() != v2.size()) 
-        return false;
-    for (int i = 0; i < v1.size(); i++) 
-        if (v1[i] != v2[i]) 
-            return false;
-    return true;
+```cpp
+int main() {
+    return by_length({std::make_pair("Nine"), std::make_pair("Eight"), std::make_pair("Four")});
 }
-
-bool by_length(const string& s1, const string& s2) {
-    return s1.length() > s2.length();
-}
-assert(isSame(vector<string>{{"Nine", "Eight", "Four"}}, vector<string>{{"Nine"}, {"Eight"}, {"Four"}}));
