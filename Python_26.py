29806524ae379e9a9a4686ce574bb7c3f167d89f
beep boop <githubbot@vadim.me>
2024-07-12 07:17:55 +0200

    Solution 74 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/t61x2loc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fdc81b3c-6b14-436c-9c95-fcdbfc3a9 f05.py", line 10, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fdc81b3c-6b14-436c-9c95-fcdbfc3a9 f05.py", line 7, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_26.py
+++ Python_26.py
@@ -1,8 +1,11 @@
-```
+```Python
 def remove_duplicates(input_numbers=None):
     if input_numbers is None:
         input_numbers = list(map(int, input("Enter numbers separated by space: ").split()))
-    unique_numbers = []
-    for num in set(input_numbers):
-        unique_numbers.append(num)
+    if len(input_numbers) == 0:
+        return input_numbers
+    unique_numbers = [input_numbers[0]]
+    for number in input_numbers[1:]:
+        if number != unique_numbers[-1]:
+            unique_numbers.append(number)
     return unique_numbers
