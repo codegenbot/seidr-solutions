b9af3e96ffd514d65172009d99d9d08a2af002bd
beep boop <githubbot@vadim.me>
2024-07-12 14:40:19 +0200

    Solution 51 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/0w9p2qvs Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -4,5 +4,5 @@ def find_zero(xs: list):
     n = len(xs)
     if n % 2 == 1:
         raise ValueError("xs must have even number of coefficients")
-    a = xs[n//2]
-    return -a / xs[0]
+    largest_coeff = max(i for i in xs if i != 0)
+    return -largest_coeff / xs[-1]
