726ee6dd0e759b7ee2879bcf6cf09b1b8b080789
beep boop <githubbot@vadim.me>
2024-07-12 07:02:23 +0200

    Solution 54 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/t61x2loc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2ba83849-cdf5-4755-8291-5407e1ea4 292.py", line 19, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2ba83849-cdf5-4755-8291-5407e1ea4 292.py", line 16, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: remove_duplicates() takes 0 positional arguments but 1 was given

--- Python_26.py
+++ Python_26.py
@@ -1,9 +1,16 @@
-```
 def remove_duplicates(input_numbers):
     unique_numbers = []
 
-    for num in input_numbers:
-        if num not in unique_numbers:
-            unique_numbers.append(num)
+    while True:
+        if not input_numbers: 
+            break
+
+        user_num = input_numbers.pop(0)
+
+        if str(user_num).lower() != "q":
+            if user_num not in unique_numbers:
+                unique_numbers.append(user_num)
+        else:
+            break
 
     return list(set(unique_numbers))
