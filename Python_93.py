f9280fe11961d16fafc3501284d032ac7d3a1209
beep boop <githubbot@vadim.me>
2024-07-12 08:29:03 +0200

    Solution 91 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ppkt8fr3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/141ed203-5134-4840-bc49-3b32e48bd 1f4.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -1,14 +1,19 @@
 ```
-import re
 def encode(message):
     vowels = 'aeiouAEIOU'
     result = ""
     for char in message:
-        if not re.match(r'\w', char):
+        if not char.isalpha():
             result += char
         elif char.lower() not in vowels:  
             index = (ord(char.lower()) - ord('a')) % 26
-            result += chr((index + 1) % 26 + ord('a'))
+            if index == 0:
+                index = 26
+            if char.lower() in 'ouieOUIE':
+                index = ((index - 1) + 3) % 4
+            else:
+                index = ((index - 1) + 5) % 6
+            result += chr((index) + ord('a'))
         else:  
             if char.lower() in 'ouieOUIE':
                 index = (ord(char.lower()) - ord('o')) % 4
@@ -18,4 +23,4 @@ def encode(message):
                 result += chr((index + 1) % 6 + ord('a'))
     return result
 
-print(encode("your_message_here"))
+print(encode("I DoNt KnOw WhAt tO WrItE"))
