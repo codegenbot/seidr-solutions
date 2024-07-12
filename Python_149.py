30435b543ba2fb579d13ed4245743f2279c0b354
beep boop <githubbot@vadim.me>
2024-07-12 08:36:35 +0200

    Solution 6 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/oaf2ryzo Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6c5fa067-00c7-4ee2-84a9-48d7734dc 720.py", line 14, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6c5fa067-00c7-4ee2-84a9-48d7734dc 720.py", line 11, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,5 +1,2 @@
-```
 def sorted_list_sum(lst):
-    even_words = [word for word in lst if len(word) % 2 == 0]
-    even_words.sort(key=lambda x: (len(x), x))
-    return " -> ".join(even_words) + " Sum of lengths: " + str(sum(len(word) for word in even_words))
+    return sum(len(word) for word in sorted([word for word in lst if len(word) % 2 == 0], key=lambda x: (len(x), x)))
