c45662a1435eccad0605385e59fd62902b20a52d
beep boop <githubbot@vadim.me>
2024-07-12 15:42:36 +0200

    Solution 7 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ikbtb8rw Prompt:    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b0307494-8eaa-4996-8500-8397b3c6dd6e.cpp:21: b0307494-8eaa-4996-8500-8397b3c6dd6e.cpp: In function ‘int main()’: b0307494-8eaa-4996-8500-8397b3c6dd6e.cpp:31:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    31 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from b0307494-8eaa-4996-8500-8397b3c6dd6e.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ In file included from b0307494-8eaa-4996-8500-8397b3c6dd6e.cpp:21: b0307494-8eaa-4996-8500-8397b3c6dd6e.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,11 +1,11 @@
 #include <algorithm>
 
-vector<int> new_unique(vector<int> l) {
+vector<int> removeDuplicates(vector<int> l) {
     vector<int> result(l.begin(), unique(l.begin(), l.end()).end());
     return result;
 }
 
-vector<int>::iterator my_unique(vector<int>::iterator start, vector<int>::iterator end) {
+vector<int>::iterator myUnique(vector<int>::iterator start, vector<int>::iterator end) {
     sort(start, end);
     auto it = start;
     while (it != end) {
