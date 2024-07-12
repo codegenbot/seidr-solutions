ad92275eee60b34f26972da9d924e3c437771324
beep boop <githubbot@vadim.me>
2024-07-12 12:34:22 +0200

    Solution 46 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/nz5vz3jp Prompt: Enter a number: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9f1c6ba1-c377-4f4b-b251-e5f217bdd 256.py", line 25, in <module>     check(factorize)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9f1c6ba1-c377-4f4b-b251-e5f217bdd 256.py", line 22, in check     assert factorize(3 * 2 * 3) == [2, 3, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_25.py
+++ Python_25.py
@@ -1,13 +1,22 @@
-def factorize(n):
-    return [i for i in range(1, n + 1) if n % i == 0]
+import sys
+import select
 
+def factorize(n):
+    factors = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            factors.append(i)
+    return factors
 
 try:
     print("Enter a number: ")
-    n = int(input())
+    rlist, _, _ = select.select([sys.stdin], [], [], 10)
 
-    result = factorize(n)
-    print(result)
+    if rlist:
+        n = int(input())
+
+        result = factorize(n)
+        print(result)
 
 except ValueError:
     print("Invalid input. Please enter a valid number.")
