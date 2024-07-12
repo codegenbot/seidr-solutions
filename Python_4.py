6997780413ac8d533542270376c49dddbc94668c
beep boop <githubbot@vadim.me>
2024-07-13 01:45:22 +0200

    Solution 39 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/rbtvzix5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f36a39c5-4ac1-4dfe-9285-996dd6231 22d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_4.py
+++ Python_4.py
@@ -1,15 +1,11 @@
-```
-def mean_absolute_deviation() -> float:
-    while True:
-        print("Enter some numbers (space-separated): ")
-        try:
-            numbers = [float(num) for num in input().split()]
-            break
-        except ValueError:
-            print("Invalid input. Please enter numbers only.")
+def mean_absolute_deviation(numbers) -> float:
+    try:
+        numbers = [float(num) for num in input("Enter some numbers (space-separated): ").split()]
+    except ValueError:
+        print("Invalid input. Please enter numbers only.")
     
     if len(numbers) < 2:
-        print("Please enter at least two numbers.")
+        return "Please enter at least two numbers."
     else:
         mean = sum(numbers) / len(numbers)
         return round(sum(abs(num - mean) for num in numbers) / len(numbers), 2)
