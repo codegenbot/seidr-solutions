2cb238f10f12bd74c8acf01ec34ac5ad2bb86b80
beep boop <githubbot@vadim.me>
2024-07-12 02:23:21 +0200

    Solution 88 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/ez2wjiwn Prompt: it must return 1 759 for input 7 691 311 145 302 11 471 759, but it returns .

--- leaders.cpp
+++ leaders.cpp
@@ -10,7 +10,7 @@ vector<int> findLeaders(const vector<int>& nums) {
     int maxRight = INT_MIN;
 
     for (int i = n - 1; i >= 0; --i) {
-        if (nums[i] > maxRight) {
+        if (nums[i] >= maxRight) {
             maxRight = nums[i];
             leaders.push_back(nums[i]);
         }
@@ -18,4 +18,32 @@ vector<int> findLeaders(const vector<int>& nums) {
 
     reverse(leaders.begin(), leaders.end());
     return leaders;
+}
+
+int main() {
+    vector<int> nums;
+    int num;
+    
+    cout << "Enter positive integers (0 to end input, press enter after each number, then Ctrl+D to finish):" << endl;
+    cout.flush();
+
+    do {
+        if (!(cin >> num)) {
+            break;
+        }
+        if (num == 0) {
+            break;
+        }
+        nums.push_back(num);
+    } while (true);
+    
+    vector<int> result = findLeaders(nums);
+    
+    cout << "Leaders in the vector are:";
+    for (int leader : result) {
+        cout << " " << leader;
+    }
+    cout << endl;
+    
+    return 0;
 }
