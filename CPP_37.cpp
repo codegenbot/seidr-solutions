c725d15d1226bf21ada27be86257911292571910
beep boop <githubbot@vadim.me>
2024-07-12 15:10:12 +0200

    Solution 44 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/owlfkvjg Prompt:       |                             ^~~~~~ 3fb4c242-9ccd-4a7f-b6a6-60cf780c85ba.cpp:39:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 3fb4c242-9ccd-4a7f-b6a6-60cf780c85ba.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 3fb4c242-9ccd-4a7f-b6a6-60cf780c85ba.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 3fb4c242-9ccd-4a7f-b6a6-60cf780c85ba.cpp:39:36: error: expected primary-expressi on before ‘float’    39 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 3fb4c242-9ccd-4a7f-b6a6-60cf780c85ba.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 3fb4c242-9ccd-4a7f-b6a6-60cf780c85ba.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-bool same(std::vector<float> a, std::vector<float> b) {
+bool isSame(std::vector<float> a, std::vector<float> b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i < a.size(); i++)
@@ -11,18 +11,18 @@ bool same(std::vector<float> a, std::vector<float> b) {
     return true;
 }
 
-std::vector<float> sort_even(std::vector<float> l) {
+std::vector<float> sortEven(std::vector<float> l) {
     std::vector<float> result(l.size(), 0.0);
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            std::vector<float> even_indices;
+            std::vector<float> evenIndices(l.size(), 0.0f); 
             for (int j = 0; j < l.size(); j++) {
                 if (j % 2 == 0) {
-                    even_indices.push_back(l[j]);
+                    evenIndices.push_back(l[j]);
                 }
             }
-            std::sort(even_indices.begin(), even_indices.end());
-            result[i] = even_indices[0];
+            std::sort(evenIndices.begin(), evenIndices.end());
+            result[i] = evenIndices[0];
         } else {
             result[i] = l[i];
         }
@@ -31,6 +31,6 @@ std::vector<float> sort_even(std::vector<float> l) {
 }
 
 int main() {
-    assert(same(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    assert(isSame(sortEven({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
     return 0;
 }
