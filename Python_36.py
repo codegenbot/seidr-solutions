d5199ef1143fa948e38e656dca425078b16b5005
beep boop <githubbot@vadim.me>
2024-07-12 10:38:55 +0200

    Solution 98 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4o64e9le Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5bac2cdc-04bd-4938-860a-d57cc3560 eaf.py", line 20, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5bac2cdc-04bd-4938-860a-d57cc3560 eaf.py", line 17, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -3,12 +3,12 @@ def fizz_buzz(n: int):
         1
         for i in range(1, n + 1)
         if (
-            (i % 11 == 0 or i % 7 == 0)
-            or ("7" in str(i))
-            or any(char == '7' for char in str(i))
+            ((i % 3 == 0) or (i % 5 == 0) or (i % 7 == 0) or (i % 11 == 0))
+            and (i % 13 != 0) and (i % 15 != 0) and (i % 21 != 0)
+            and (i % 33 != 0) and (i % 35 != 0) and (i % 77 != 0)
+            and ("7" in str(i) or any(char == '7' for char in str(i)))
+            and all(char.isdigit() and int(char) > 7 for char in str(i))
+            and "5" not in str(i)
         )
-        and all(i % num != 0 for num in [13, 21, 15, 35, 33, 77])
-        and "5" not in str(i)
-        and any(char.isdigit() and int(char) > 7 for char in str(i))
     )
     return count
