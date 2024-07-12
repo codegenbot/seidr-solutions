4aee0cbaf2b3e2845d36035164ae976d90bf7998
beep boop <githubbot@vadim.me>
2024-07-12 22:04:46 +0200

    Solution 86 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/cqdaf7is Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/08ed61d2-9b31-4c01-a3de-769b5050c 6b3.py", line 2, in <module>     result = eval(expression.replace("T", "True").replace("F", "False").replace( "|", "or").replace("&", "and"))              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "<string>", line 1, in <module> NameError: name 'tandtandtandtortandfandtortorfortandtandtandfandtorfandfortorta ndfandf' is not defined

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,8 +1,3 @@
 expression = input()
-result = eval(
-    expression.replace("&", "and")
-    .replace("|", "or")
-    .replace("T", "True")
-    .replace("F", "False")
-)
+result = eval(expression.replace("&", "and").replace("|", "or").replace("T", "True").replace("F", "False"))
 print(result)
