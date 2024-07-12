1b9257202ec43e39236640059b6e6d806a6fcb60
beep boop <githubbot@vadim.me>
2024-07-12 19:49:39 +0200

    Solution 89 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/avzqcthh Prompt: 1f1cb7b4-1875-49c6-8a71-31514bd720b8.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~ 1f1cb7b4-1875-49c6-8a71-31514bd720b8.cpp: In function ‘int main()’: 1f1cb7b4-1875-49c6-8a71-31514bd720b8.cpp:22:5: error: ‘vector’ was not declared in this scope    22 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 1f1cb7b4-1875-49c6-8a71-31514bd720b8.cpp:22:5: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 1f1cb7b4-1875-49c6-8a71-31514bd720b8.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1f1cb7b4-1875-49c6-8a71-31514bd720b8.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1f1cb7b4-1875-49c6-8a71-31514bd720b8.cpp:22:12: error: expected primary-expressi on before ‘float’    22 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -13,7 +13,7 @@ bool has_close_elements(const std::vector<float>& v, float epsilon) {
 int main() {
     std::vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
     
-    assert(has_close_elements({1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f}, 0.1) == true);
+    assert(has_close_elements(a, 0.1) == true);
     
     return 0;
 }
