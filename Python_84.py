d18b00a19c998eb8b32b64794c028653d9761b46
beep boop <githubbot@vadim.me>
2024-07-12 07:52:12 +0200

    Solution 17 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ts2ifgq4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2afa0fd9-e60f-4c16-8dde-631eb8ea9 10d.py", line 16, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2afa0fd9-e60f-4c16-8dde-631eb8ea9 10d.py", line 13, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -1,3 +1,10 @@
-```Python
+```
 def solve(N):
-    return str(bin(N)[2:]).replace("1", "10")
+    try:
+        N = int(bin(N)[2:])
+    except ValueError:
+        return "Invalid Input. Please provide a binary number."
+    if N > 1:
+        return bin(N | (N - 1))[2:].replace("1", "10")
+    else:
+        return str(bin(N)[2:])
