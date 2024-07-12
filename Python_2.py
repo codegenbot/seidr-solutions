3e60676ac5cd125950fcd04704b24e0c8b0a9f72
beep boop <githubbot@vadim.me>
2024-07-12 08:57:20 +0200

    Solution 15 of Python/2, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/go6zbb2i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7aaf0d05-5873-4645-bcc1-fddbcaf17 810.py", line 5, in <module>     input_number = raw_input()                    ^^^^^^^^^ NameError: name 'raw_input' is not defined

--- Python_2.py
+++ Python_2.py
@@ -2,5 +2,5 @@ def calculate_decimal_part(number):
     number = float(number)
     return number % 1
 
-input_number = float(input())
+input_number = input()
 print(calculate_decimal_part(input_number))
