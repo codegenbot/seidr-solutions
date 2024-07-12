04b96245065bbf39ef714c8497d8182344cb4b6f
beep boop <githubbot@vadim.me>
2024-07-12 11:07:46 +0200

    Solution 57 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/xihp4u2w Prompt:       |                                  ^~~ bfc7b075-dbfa-475a-8aea-9475e89b4954.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ bfc7b075-dbfa-475a-8aea-9475e89b4954.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ bfc7b075-dbfa-475a-8aea-9475e89b4954.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from bfc7b075-dbfa-475a-8aea-9475e89b4954.cpp:24: bfc7b075-dbfa-475a-8aea-9475e89b4954.cpp: In function ‘int main()’: bfc7b075-dbfa-475a-8aea-9475e89b4954.cpp:34:20: error: ‘factorize’ was not decla red in this scope    34 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ bfc7b075-dbfa-475a-8aea-9475e89b4954.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -5,7 +5,7 @@ bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
-int main(){
+int solve_cpp_25(){
     std::vector<int> expected_factors = {2, 2, 3, 5};
     assert(factorize(60) == expected_factors);
     assert(issame(factorize(3 * 2 * 3), {2, 3, 3}));
