784839667fb0f1124983d760d70b7b078c28f477
beep boop <githubbot@vadim.me>
2024-07-12 17:01:17 +0200

    Solution 64 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/y4i48eyu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8aa3d94e-80f1-4abd-8ba4-98153f457 0f5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,8 +1,3 @@
-def check_numbers():
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
-    
-    if (num1 >=0 and isinstance(num1,int)) and (num2>=0 and isinstance(num2,int)):
-        return num1 + num2
-    else:
-        return "Error: Please enter non-negative integers"
+def add(x: int, y: int):
+    result = x + y
+    return result
