d870c5c8b8fd7f47ce8f47950af21bee79597449
beep boop <githubbot@vadim.me>
2024-07-12 23:05:14 +0200

    Solution 47 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/086csx52 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ dde7253a-191e-4e95-8e23-c30714608916.cpp:21:23:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -12,7 +12,7 @@ int main() {
     std::cout << "Enter the number of elements in lst1: ";
     std::cin >> n1;
     std::cout << "Enter the number of elements in lst2: ";
-    std::cin >> n2;
+    std::cin >> m1;
     std::vector<int> lst1;
     for (int i = 0; i < n1; i++) {
         int temp;
@@ -20,10 +20,8 @@ int main() {
         std::cin >> temp;
         lst1.push_back(temp);
     }
-    std::cout << "Enter the number of elements in lst2: ";
-    std::cin >> m2;
-    std::vector<int> lst2;
-    for (int i = 0; i < m2; i++) {
+    std::cout << "Enter the elements in lst2: ";
+    for (int i = 0; i < m1; i++) {
         int temp;
         std::cout << "Enter element of lst2: ";
         std::cin >> temp;
