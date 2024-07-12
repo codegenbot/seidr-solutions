0187e13a1f06b231ed1ca5af80a368707a8c8cb2
beep boop <githubbot@vadim.me>
2024-07-12 08:27:32 +0200

    Solution 67 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/0t6m7jpw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05659ea6-decd-4a52-b3ec-1fadd97d2 240.py", line 11, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05659ea6-decd-4a52-b3ec-1fadd97d2 240.py", line 8, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05659ea6-decd-4a52-b3ec-1fadd97d2 240.py", line 2, in max_fill     total_water = sum(len(row) for row in capacity_grid)                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: 'int' object is not iterable

--- Python_115.py
+++ Python_115.py
@@ -1,11 +1,4 @@
-import math
-
-
 def max_fill(n, capacity_grid):
-    total_water = 0
-    max_capacity = float("inf")
-    for row in capacity_grid:
-        total_water += len(row)
-        if min(row) < max_capacity:
-            max_capacity = min(row)
+    total_water = sum(sum(row) for row in capacity_grid)
+    max_capacity = min(min(sublist) for sublist in capacity_grid)
     return math.ceil(total_water / (n * max_capacity))
