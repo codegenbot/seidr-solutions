f9747fdcebaa62ee1608c10bede27fc6f014b1fb
beep boop <githubbot@vadim.me>
2024-07-12 10:04:24 +0200

    Solution 87 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jkuz8vxn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f2e1de20-5a10-4d60-a8b8-702194959 c44.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,4 +1,4 @@
-```python
+```
 import re
 
 
@@ -6,5 +6,5 @@ def solve(text):
     return re.sub(r"(\s+)([a-zA-Z])", lambda m: "_" + m.group(2).upper(), text)
 
 
-text = input("Enter your name or title: ").encode('utf-8').decode('utf-8')  
+text = input("Enter your name or title: ")
 print(solve(text))
