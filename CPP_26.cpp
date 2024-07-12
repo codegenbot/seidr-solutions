512bf89b44ffe7c151a132ccf9340059b86ef1fa
beep boop <githubbot@vadim.me>
2024-07-12 09:51:24 +0200

    Solution 20 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/xw89prdq Prompt:  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2850e572-2220-4f69-ae5e-5e08110bccd9.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2850e572-2220-4f69-ae5e-5e08110bccd9.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2850e572-2220-4f69-ae5e-5e08110bccd9.cpp:10: 2850e572-2220-4f69-ae5e-5e08110bccd9.cpp: In function ‘int main()’: 2850e572-2220-4f69-ae5e-5e08110bccd9.cpp:20:20: error: ‘remove_duplicates’ was n ot declared in this scope    20 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 2850e572-2220-4f69-ae5e-5e08110bccd9.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,20 +1,26 @@
-vector<int> remove_duplicates(vector<int> numbers) {
-    numbers.erase(unique(numbers.begin(), numbers.end()), numbers.end());
-    return numbers;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+vector<int> remove_duplicates(const vector<int>& numbers) {
+    vector<int> unique_numbers;
+    for (int num : numbers) {
+        if (count(numbers.begin(), numbers.end(), num) == 1) {
+            unique_numbers.push_back(num);
+        }
+    }
+    return unique_numbers;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return (a == b);
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a == b;
 }
 
 int main() {
-    vector<int> numbers = {1, 2, 2, 3, 4, 4, 5};
-    
-    vector<int> unique_numbers = remove_duplicates(numbers);
-    
-    for (auto num : unique_numbers) {
-        cout << num << " ";
-    }
-    
+    assert(issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5}));
+  
     return 0;
 }
