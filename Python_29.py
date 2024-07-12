10365caea8147100506d8323db2b913ebc4a05fd
beep boop <githubbot@vadim.me>
2024-07-12 06:23:21 +0200

    Solution 86 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7ealauoz Prompt: b/python3.11/site-packages/programlib/programs/ae1f14f8-296d-41b9-bc23-780db5e0b cdb.py", line 15, in <module>     check(filter_by_prefix)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae1f14f8-296d-41b9-bc23-780db5e0b cdb.py", line 10, in check     assert filter_by_prefix(            ^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae1f14f8-296d-41b9-bc23-780db5e0b cdb.py", line 7, in filter_by_prefix     result = [s.strip() for s in strings_list if s and s.lower().startswith(pref ix.lower())]              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae1f14f8-296d-41b9-bc23-780db5e0b cdb.py", line 7, in <listcomp>     result = [s.strip() for s in strings_list if s and s.lower().startswith(pref ix.lower())]                                                                             ^^^^ ^^^^^^^^ AttributeError: 'list' object has no attribute 'lower'

--- Python_29.py
+++ Python_29.py
@@ -5,5 +5,5 @@ def filter_by_prefix(prefix="", user_input=None):
     else:
         strings = user_input
     strings_list = [s.strip().lower() for s in strings.split(',')]
-    result = [s.strip() for s in strings_list if s and s.startswith(prefix.lower())]
+    result = [s.strip() for s in strings_list if s and s.lower().startswith(prefix.lower())]
     print(result)
