6539c5cdc587280987bd61f33ba707a9fa71ff19
beep boop <githubbot@vadim.me>
2024-07-12 08:35:49 +0200

    Solution 4 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/oaf2ryzo Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6d1afe6d-bf0c-48d5-bb59-d61e15f41 eaa.py", line 11, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6d1afe6d-bf0c-48d5-bb59-d61e15f41 eaa.py", line 8, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6d1afe6d-bf0c-48d5-bb59-d61e15f41 eaa.py", line 2, in sorted_list_sum     return sum(            ^^^^ TypeError: unsupported operand type(s) for +: 'int' and 'str'

--- Python_149.py
+++ Python_149.py
@@ -1,7 +1,3 @@
+```
 def sorted_list_sum(lst):
-    return sum(
-        len(word)
-        for word in sorted(
-            [word for word in lst if len(word) % 2 == 0], key=lambda x: (len(x), x)
-        )
-    )
+    return sum(len(word) for word in [word for word in lst if len(word) % 2 == 0])
