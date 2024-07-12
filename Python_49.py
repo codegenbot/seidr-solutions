e9a98ddcc59f5b205c7c448f185e27995a8305c7
beep boop <githubbot@vadim.me>
2024-07-12 08:11:09 +0200

    Solution 88 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/d8swkc5z Prompt: Program did not receive expected input

--- Python_49.py
+++ Python_49.py
@@ -1,14 +1,15 @@
 def power_mod(a, n, p):
-    a = int(input(f"Enter value of a: "))
-    n = int(input(f"Enter value of n: "))
-    p = int(input(f"Enter value of p: "))
-
     if n < 0:
-        return pow(power_mod(a, -n, p), -1, p)
+        return pow(pow_mod(a, -n, p), -1, p)
     elif n > 0:
         return (a * power_mod(a, n - 1, p)) % p
     else:
         return a % p
 
 
-print(power_mod(0, 0, 0))
+a = int(input("Enter value of a: "))
+n = int(input("Enter value of n: "))
+p = int(input("Enter value of p: "))
+
+
+print(power_mod(a, n, p))
