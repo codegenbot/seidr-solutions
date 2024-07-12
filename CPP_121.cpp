41d0bd714ada7e994feee5bfbeadf7d11e3ec7ac
beep boop <githubbot@vadim.me>
2024-07-12 03:12:42 +0200

    Solution 6 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/g0d9ukrg Prompt: bed9d7f0-9647-4cc3-b810-fff0173c2c9a.cpp:1:14: error: ‘vector’ was not declared in this scope     1 | int solution(vector<int> lst) {       |              ^~~~~~ bed9d7f0-9647-4cc3-b810-fff0173c2c9a.cpp:1:21: error: expected primary-expressio n before ‘int’     1 | int solution(vector<int> lst) {       |                     ^~~ In file included from bed9d7f0-9647-4cc3-b810-fff0173c2c9a.cpp:11: bed9d7f0-9647-4cc3-b810-fff0173c2c9a.cpp: In function ‘int main()’: bed9d7f0-9647-4cc3-b810-fff0173c2c9a.cpp:13:13: error: ‘solutions’ was not decla red in this scope; did you mean ‘solution’?    13 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
 #include <vector>
 
-int solutions(vector<int> lst){
+int solution(std::vector<int> lst) {
     int sum = 0;
     for (int i = 0; i < lst.size(); i += 2) {
         if (lst[i] % 2 != 0) {
@@ -9,11 +8,4 @@ int solutions(vector<int> lst){
         }
     }
     return sum;
-}
-
-using namespace std;
-
-int main() {
-    assert(solutions({3, 13, 2, 9}) == 3);
-    return 0;
 }
