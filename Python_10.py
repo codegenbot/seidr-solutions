f53069d53e5bfa83bfaf804977eeaa9465ce229b
beep boop <githubbot@vadim.me>
2024-07-12 13:29:57 +0200

    Solution 29 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tboxn4kg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4413f35d-3231-4bcc-af1a-0c76c1158 efc.py", line 18, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4413f35d-3231-4bcc-af1a-0c76c1158 efc.py", line 15, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,9 +1,13 @@
+```
+def is_palindrome(string: str) -> bool:
+    return string == string[::-1]
+
 def make_palindrome(string: str) -> str:
-    if string == string[::-1]:
+    if is_palindrome(string):
         return string + string
     min_len = len(string)
     for i in range(len(string)):
-        for j in range(i + 1, len(string)):
+        for j in range(i+1, len(string)):
             prefix = string[:i]
             suffix = string[j:]
             palindrome = prefix + string[i:j][::-1] + suffix[::-1]
