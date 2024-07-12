6320b998dc4a7f1d98184296020d03bfefc937ff
beep boop <githubbot@vadim.me>
2024-07-12 17:37:53 +0200

    Solution 72 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/x1ivwqte Prompt: In file included from b5fd0fc9-9c53-4645-afee-fe3fcfc0fa5a.cpp:23: b5fd0fc9-9c53-4645-afee-fe3fcfc0fa5a.cpp: In function ‘int main()’: b5fd0fc9-9c53-4645-afee-fe3fcfc0fa5a.cpp:25:13: error: ‘filp_case’ was not decla red in this scope    25 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,1 +1,1 @@
-assert(changeCase("These violent delights have violent ends") == "tHESE VIOLENT DELIGETS HAVE VIOLENT EDNS");
+```assert(changeCase("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
