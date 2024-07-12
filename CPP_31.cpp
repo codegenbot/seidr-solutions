4f393578d7c570ffdb175a897c19caaa8183c72f
beep boop <githubbot@vadim.me>
2024-07-12 10:59:21 +0200

    Solution 20 of CPP/31, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/9bulaysg Prompt: ore ‘for’     4 |     for (long long i = 2; i * i <= n; i++) {       |     ^~~ 4610c10d-e1ca-4ff5-8f1b-d0807ec324cc.cpp:4:27: error: ‘i’ does not name a type     4 |     for (long long i = 2; i * i <= n; i++) {       |                           ^ 4610c10d-e1ca-4ff5-8f1b-d0807ec324cc.cpp:4:39: error: ‘i’ does not name a type     4 |     for (long long i = 2; i * i <= n; i++) {       |                                       ^ 4610c10d-e1ca-4ff5-8f1b-d0807ec324cc.cpp:9:5: error: expected unqualified-id bef ore ‘return’     9 |     return true;       |     ^~~~~~ 4610c10d-e1ca-4ff5-8f1b-d0807ec324cc.cpp:10:1: error: expected declaration befor e ‘}’ token    10 | }       | ^ In file included from 4610c10d-e1ca-4ff5-8f1b-d0807ec324cc.cpp:12: 4610c10d-e1ca-4ff5-8f1b-d0807ec324cc.cpp: In function ‘int main()’: 4610c10d-e1ca-4ff5-8f1b-d0807ec324cc.cpp:14:13: error: ‘is_prime’ was not declar ed in this scope    14 |     assert (is_prime(13441 * 19) == false);       |             ^~~~~~~~

--- CPP_31.cpp
+++ CPP_31.cpp
@@ -1,1 +1,22 @@
-bool is_prime(long long n){
+bool is_prime(long long n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (long long i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    long long num;
+    cin >> num;
+    if (is_prime(num)) {
+        cout << "Prime";
+    } else {
+        cout << "Not Prime";
+    }
+    return 0;
+}
