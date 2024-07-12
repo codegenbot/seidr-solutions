f7c6c8f2b0f9d7d13b3d9ab53ab1d8017fd85cd4
beep boop <githubbot@vadim.me>
2024-07-12 14:56:59 +0200

    Solution 17 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/bdn495i9 Prompt:       |                              ^~~~~~ 23187f19-a051-4b1f-a20a-5a79ed989ea5.cpp:35:37: error: ‘string’ was not declared  in this scope    35 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 23187f19-a051-4b1f-a20a-5a79ed989ea5.cpp:35:44: error: ‘b’ was not declared in t his scope    35 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 23187f19-a051-4b1f-a20a-5a79ed989ea5.cpp:35:45: error: expression list treated a s compound expression in initializer [-fpermissive]    35 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 23187f19-a051-4b1f-a20a-5a79ed989ea5.cpp:34: 23187f19-a051-4b1f-a20a-5a79ed989ea5.cpp: In function ‘int main()’: 23187f19-a051-4b1f-a20a-5a79ed989ea5.cpp:44:20: error: ‘reverse_delete’ was not declared in this scope    44 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 23187f19-a051-4b1f-a20a-5a79ed989ea5.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,11 +1,9 @@
-#include <iostream>
+```cpp
 #include <vector>
 #include <algorithm>
 #include <string>
 
-using namespace std;
-
-bool is_palindrome(string s) {
+bool is_palindrome(std::string s) {
     int start = 0, end = s.length() - 1;
     while (start < end) {
         if (s[start] != s[end]) {
@@ -17,16 +15,16 @@ bool is_palindrome(string s) {
     return true;
 }
 
-vector<string> reverse_delete(string s, string c) {
-    vector<char> char_s(s.begin(), s.end());
+std::vector<std::string> reverse_delete(std::string s, std::string c) {
+    std::vector<char> char_s(s.begin(), s.end());
     for (char ch : c) {
         auto it = find(char_s.begin(), char_s.end(), ch);
         if (it != char_s.end()) {
             char_s.erase(it);
         }
     }
-    string result_str(char_s.begin(), char_s.end());
-    vector<string> result;
+    std::string result_str(char_s.begin(), char_s.end());
+    std::vector<std::string> result;
     result.push_back(result_str);
     if (is_palindrome(result_str)) {
         result.push_back("True");
@@ -34,7 +32,6 @@ vector<string> reverse_delete(string s, string c) {
         result.push_back("False");
     }
     return result;
-
 }
 
 int main() {
