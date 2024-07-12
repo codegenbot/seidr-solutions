38d3fc5c9c7e83983d0e164d71e1a3169c4a79fe
beep boop <githubbot@vadim.me>
2024-07-12 12:01:38 +0200

    Solution 75 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/37by9irg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/76ec57af-eca5-40f4-8961-e3e0a4a44 76e.py", line 25, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/76ec57af-eca5-40f4-8961-e3e0a4a44 76e.py", line 21, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -4,11 +4,11 @@ def encode(message):
     for char in message:
         if char.isalpha():
             char_code = ord(char)
-            increment = 3 if char.lower() not in "aeiou" else 2
+            increment = 2 if char.lower() not in "aeiou" else 3
             new_code = (char_code - ord('a' if char.islower() else 'A') + increment) % 26 + ord('a' if char.islower() else 'A')
                 
             encoded_message += chr(new_code)
-        elif char == " ":
+        if char == " ":
             encoded_message += " "
         else:
             encoded_message += char
