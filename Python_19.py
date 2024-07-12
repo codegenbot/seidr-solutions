823f4b891f7787fe2b375f1dc5879d82a5e281d5
beep boop <githubbot@vadim.me>
2024-07-12 06:55:49 +0200

    Solution 65 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g167l76a Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/36ba9259-bd5d-4d37-8014-c1bf9e56f 554.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_19.py
+++ Python_19.py
@@ -2,17 +2,17 @@
 import re
 
 
-def sort_numbers() -> str:
+def sort_numbers(input_str: str) -> str:
+   	indentation
     while True:
-        input_str = input("Enter a string of space-separated integers (or 'q' to quit): ")
-        if input_str.lower() == 'q':
-            return None
         try:
             if not re.match(r"^\s*\d+(?:\s+\d+)*)$", input_str):
                 raise ValueError
             numbers = [int(num) for num in input_str.split() if num.isdigit()]
-            if not numbers:
-                return str(numbers)
-            return " ".join(map(str, sorted(numbers)))
+            break
         except ValueError:
             print("Invalid input. Please enter a string of space-separated integers.")
+            input_str = input("Enter your input: ")
+    if not numbers:
+        return str(numbers)
+    return " ".join(map(str, sorted(numbers)))
