2b58a717a8fd97825030c701df442619161f7be7
beep boop <githubbot@vadim.me>
2024-07-12 15:07:03 +0200

    Solution 22 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/bdn495i9 Prompt:       |                              ^~~~~~ a0efb027-98c5-429b-85ee-4ed87c1e582a.cpp:42:37: error: ‘string’ was not declared  in this scope    42 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a0efb027-98c5-429b-85ee-4ed87c1e582a.cpp:42:44: error: ‘b’ was not declared in t his scope    42 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a0efb027-98c5-429b-85ee-4ed87c1e582a.cpp:42:45: error: expression list treated a s compound expression in initializer [-fpermissive]    42 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a0efb027-98c5-429b-85ee-4ed87c1e582a.cpp:41: a0efb027-98c5-429b-85ee-4ed87c1e582a.cpp: In function ‘int main()’: a0efb027-98c5-429b-85ee-4ed87c1e582a.cpp:51:20: error: ‘reverse_delete’ was not declared in this scope    51 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ a0efb027-98c5-429b-85ee-4ed87c1e582a.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,51 +1,51 @@
 #include <vector>
 #include <string>
 
-std::vector<std::string> reverse_delete(std::string s, std::string c) {
-    std::vector<std::string> result;
-
-    for (char& ch : s) {
-        bool found = false;
-        for (char& cc : c) {
-            if (ch == cc) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            result.push_back(std::string(1, ch));
-        }
-    }
-
-    std::string resStr = "";
-    for (std::string str : result) {
-        resStr += str;
-    }
-
-    bool isPalindrome = false;
-    int left = 0, right = resStr.length() - 1;
-    while (left < right) {
-        if (resStr[left] != resStr[right]) {
-            isPalindrome = false;
-            break;
-        }
-        left++;
-        right--;
-    }
-    if (isPalindrome) {
-        result.push_back("True");
-    } else {
-        result.push_back("False");
-    }
-
-    return result;
-}
-
 bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
     return a == b;
 }
 
 int main() {
-    assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
-    // ... rest of your code
+    assert(issame(reverse_delete("mamma", "a"), {"", "True"}));
+    
+    vector<string> reverse_delete(string s, string c) {
+        vector<string> result;
+
+        for (char& ch : s) {
+            bool found = false;
+            for (char& cc : c) {
+                if (ch == cc) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                result.push_back(string(1, ch));
+            }
+        }
+
+        string resStr = "";
+        for (string str : result) {
+            resStr += str;
+        }
+
+        bool isPalindrome = false;
+        int left = 0, right = 0;
+        while (right < resStr.length()) {
+            if (resStr[left] != resStr[right]) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right++;
+        }
+        if (isPalindrome) {
+            result.push_back("True");
+        } else {
+            result.push_back("False");
+        }
+
+        return result;
+    }
+    
 }
