e2b15bfb3a81319d34847395a11485cff715a423
beep boop <githubbot@vadim.me>
2024-07-13 00:45:56 +0200

    Solution 6 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/qeqjd3zn Prompt:       |     ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 3d592190-dcf3-46f6-ad31-6565ef982591.cpp:1: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ 3d592190-dcf3-46f6-ad31-6565ef982591.cpp:15:14: warning: ‘int MD5_Final(unsigned  char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]    15 |     MD5_Final(mdBuffer, &ctx);       |     ~~~~~~~~~^~~~~~~~~~~~~~~~ In file included from 3d592190-dcf3-46f6-ad31-6565ef982591.cpp:1: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ /usr/bin/ld: /tmp/ccEEEJL9.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> >)': 3d592190-dcf3-46f6-ad31-6565ef982591.cpp:(.text+0x9d): undefined reference to `M D5_Init' /usr/bin/ld: 3d592190-dcf3-46f6-ad31-6565ef982591.cpp:(.text+0xd6): undefined re ference to `MD5_Update' /usr/bin/ld: 3d592190-dcf3-46f6-ad31-6565ef982591.cpp:(.text+0xec): undefined re ference to `MD5_Final' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,4 +1,4 @@
-#include <openssl/md5.h>
+#include <openssl/evp.h>
 #include <string>
 #include <sstream>
 
@@ -10,14 +10,20 @@ string string_to_md5(string text) {
     MD5_CTX ctx;
     unsigned char mdBuffer[16];
     stringstream ss;
-    MD5_Init(&ctx);
-    MD5_Update(&ctx, text.c_str(), text.size());
-    MD5_Final(mdBuffer, &ctx);
+    EVP_MD_CTX* ctx2 = EVP_MD_CTX_new();
+    EVP_MD_CTX_init(ctx2);
+    EVP_DigestInit_ex(ctx2, EVP_md5(), NULL);
+    EVP_DigestUpdate(ctx2, text.c_str(), text.size());
+    unsigned char mdValue[EVP_MAX_BLOCK_LENGTH];
+    int outLen;
+    EVP_DigestFinal_ex(ctx2, mdValue, &outLen);
 
-    for (int i = 0; i < 16; i++) {
-        sprintf(&ss.str()[0], "%02x", (char)mdBuffer[i]);
+    for (int i = 0; i < outLen; i++) {
+        sprintf(&ss.str()[0], "%02x", (char)mdValue[i]);
         ss.seekp(0);
     }
+
+    EVP_MD_CTX_free(ctx2);
 
     return ss.str();
 }
