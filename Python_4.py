902e4d2bcdfa34d90f0bd2d343a7f62cd4ae07c1
beep boop <githubbot@vadim.me>
2024-07-12 16:07:39 +0200

    Solution 48 of Python/4, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/63yredxm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b3131893-d999-449f-bcbb-a168b932f 1bd.py", line 1, in <module>     def mean_absolute_deviation(numbers: List[float]) -> float:                                          ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_4.py
+++ Python_4.py
@@ -1,11 +1,8 @@
-from typing import List
-def mean_absolute_deviation(numbers: List[float]) -> float:
+def mean_absolute_deviation(numbers: list[float]) -> float:
     if not numbers:  
         return 0 
     try:
         mean = sum(numbers) / len(numbers)
         return sum(abs(num - mean) for num in numbers) / len(numbers)
-    except ZeroDivisionError:
-        return "Cannot calculate mean with empty list."
     except TypeError:
         return "Invalid input. Please enter only numbers."
