f5ba841b2b95e03a6c1567003b5ba37293dde209
beep boop <githubbot@vadim.me>
2024-07-12 14:16:25 +0200

    Solution 53 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3pjnfwz2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/451e263e-c908-4ec2-87ed-86885f0f9 fc9.py", line 24, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/451e263e-c908-4ec2-87ed-86885f0f9 fc9.py", line 21, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/451e263e-c908-4ec2-87ed-86885f0f9 fc9.py", line 18, in make_palindrome     palindrome += new_string[i]                   ~~~~~~~~~~^^^ IndexError: string index out of range

--- Python_10.py
+++ Python_10.py
@@ -1,20 +1,20 @@
-```
 def is_palindrome(s: str) -> bool:
     return s == s[::-1]
+
 
 def make_palindrome(string: str) -> str:
     if is_palindrome(string):
         return string
     n = len(string)
     new_string = "#" + "#".join(list(string)) + "#"
-    palindrome = ""
     i_max = n - 1
     while True:
         if i_max < 0:
             break
-        new_string = new_string[:i_max + 1] + "#"
+        new_string = new_string[: i_max + 1] + "#"
         i_max -= 1
-    for i in range(n * 2 - 1):
-        if i < n or i >= n * 2 - n - 1:
+    palindrome = ""
+    for i in range(len(new_string) - 1):
+        if i < len(string) or i >= len(string) * 2 - n - 1:
             palindrome += new_string[i]
     return palindrome
