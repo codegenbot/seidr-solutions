03986d014975c8de9f1ac77c171bd5d689c34eba
beep boop <githubbot@vadim.me>
2024-07-12 03:01:54 +0200

    Solution 12 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/uhj0t12v Prompt: Complete the following code given the task description and function signature.

--- Python_14.py
+++ Python_14.py
@@ -1,3 +1,2 @@
 def all_prefixes(string: str) -> List[str]:
-    prefixes = [string[: i + 1] for i in range(len(string))]
-    return prefixes
+    return [string[: i + 1] for i in range(len(string))]
