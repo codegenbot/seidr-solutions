9b927cc5603c9ffa2017b89ebb06981111c353a8
beep boop <githubbot@vadim.me>
2024-07-13 01:08:57 +0200

    Solution 10 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tzw1bm7i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d959dee6-5613-452c-9718-900c9e416 08c.py", line 12, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d959dee6-5613-452c-9718-900c9e416 08c.py", line 7, in check     sort_numbers("six five four three two one zero") TypeError: sort_numbers() takes 0 positional arguments but 1 was given

--- Python_19.py
+++ Python_19.py
@@ -1,6 +1,4 @@
-def sort_numbers():
-    numbers = input("Enter space-separated numbers: ")
-    sorted_numbers = " ".join(map(str, sorted([int(num) for num in numbers.split()])))
+def sort_numbers(user_input=""):
+    numbers = user_input.split(" ")
+    sorted_numbers = " ".join(map(str, sorted([int(num) for num in numbers])))
     print(sorted_numbers)
-
-sort_numbers()
