600dc1525342380992570d715191395eca8bb814
beep boop <githubbot@vadim.me>
2024-07-12 07:12:28 +0200

    Solution 68 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/t61x2loc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/88deae6c-eaf0-4f66-aef4-2b99284d2 ecc.py", line 9, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/88deae6c-eaf0-4f66-aef4-2b99284d2 ecc.py", line 6, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: remove_duplicates() takes 0 positional arguments but 1 was given

--- Python_26.py
+++ Python_26.py
@@ -1,5 +1,8 @@
+```
 def remove_duplicates(input_numbers=None):
     if input_numbers is None:
         input_numbers = list(map(int, input("Enter numbers separated by space: ").split()))
     unique_numbers = set(map(str, input_numbers))
     return [int(x) for x in unique_numbers]
+
+print(remove_duplicates([1, 2, 3, 2, 4, 3, 5]))
