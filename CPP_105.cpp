ad4c18ded05f67cc494df36a14e828dca24e5a10
beep boop <githubbot@vadim.me>
2024-07-12 17:00:39 +0200

    Solution 63 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q8x7adb5 Prompt:       |                              ^~~~~~ deb892a1-436b-45b0-98b9-a3741502402d.cpp:227:37: error: ‘string’ was not declare d in this scope   227 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ deb892a1-436b-45b0-98b9-a3741502402d.cpp:227:44: error: ‘b’ was not declared in this scope   227 | bool issame(vector<string> a,vector<string>b){       |                                            ^ deb892a1-436b-45b0-98b9-a3741502402d.cpp:227:45: error: expression list treated as compound expression in initializer [-fpermissive]   227 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from deb892a1-436b-45b0-98b9-a3741502402d.cpp:226: deb892a1-436b-45b0-98b9-a3741502402d.cpp: In function ‘int main()’: deb892a1-436b-45b0-98b9-a3741502402d.cpp:236:20: error: ‘by_length’ was not decl ared in this scope   236 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ deb892a1-436b-45b0-98b9-a3741502402d.cpp:236:19: error: ‘issame’ cannot be used as a function   236 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,44 +1,102 @@
-vector<string> numberToWords(int num) {
-    vector<string> words;
+vector<string> by_length(int n) {
+    vector<string> result;
     
-    if (num < 0 || num > 99999999)
-        return {"Out of range"};
-
-    int numCopies = 3;
-
-    string[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    string[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    string[] teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-
-    if (num < 20) {
-        words.push_back(ones[num]);
-    } else if (num < 100) {
-        int tensPlace = num / 10;
-        int onesPlace = num % 10;
-        
-        if (onesPlace > 0)
-            words.push_back(tens[tensPlace] + " " + ones[onesPlace]);
-
-        if (onesPlace == 0)
-            words.push_back(tens[tensPlace]);
-    } else {
-        int hundredsPlace = num / 100;
-        int tensPlace = (num % 100) / 10;
-        int onesPlace = num % 10;
-
-        words.push_back(ones[hundredsPlace] + " Hundred");
-
-        if (tensPlace > 0 || onesPlace > 0)
-            words.push_back("and ");
-
-        if (tensPlace > 1)
-            words.push_back(tens[tensPlace]);
-        else if (tensPlace == 1)
-            words.push_back(teens[onesPlace]);
-
-        if (onesPlace > 0 && !words.get(words.size() - 1).equals("Ten"))
-            words.push_back(ones[onesPlace]);
+    if (n >= 1000) {
+        int q = n / 1000;
+        result.push_back(to_string(q));
+        result.push_back(" Thousand");
+        n %= 1000;
     }
+    
+    if (n >= 90) {
+        if (n % 100 >= 10)
+            result.push_back(issame(n % 100, 10) ? " Ninety" : to_string(n % 100) + " Ten");
+        else
+            result.push_back(" Ninety");
+        n %= 90;
+    } else if (n >= 80) {
+        if (n % 100 >= 20)
+            result.push_back(issame(n % 100, 10) ? " Eighty" : to_string(n % 100) + " Ten");
+        else
+            result.push_back(" Eighty");
+        n %= 80;
+    } else if (n >= 70) {
+        if (n % 100 >= 30)
+            result.push_back(issame(n % 100, 20) ? " Seventy" : to_string(n % 100) + " Ten");
+        else
+            result.push_back(" Seventy");
+        n %= 70;
+    } else if (n >= 60) {
+        if (n % 100 >= 40)
+            result.push_back(issame(n % 100, 20) ? " Sixty" : to_string(n % 100) + " Ten");
+        else
+            result.push_back(" Sixty");
+        n %= 60;
+    } else if (n >= 50) {
+        if (n % 100 == 50)
+            result.push_back(" Fifty");
+        else
+            result.push_back(to_string(n % 50));
+        n %= 50;
+    } else if (n >= 40) {
+        if (n % 100 == 40)
+            result.push_back(" Forty");
+        else
+            result.push_back(to_string(n % 40) + " Ten");
+        n %= 40;
+    } else if (n >= 30) {
+        if (n % 100 == 30)
+            result.push_back(" Thirty");
+        else
+            result.push_back(to_string(n % 30));
+        n %= 30;
+    } else if (n >= 20) {
+        if (n % 100 >= 10)
+            result.push_back(issame(n % 100, 10) ? " Twenty" : to_string(n % 100) + " Ten");
+        else
+            result.push_back(" Twenty");
+        n %= 20;
+    } else {
+        if (n == 19)
+            result.push_back(" Nineteen");
+        else if (n >= 10) {
+            result.push_back(to_string(n));
+        } else {
+            switch (n) {
+                case 1:
+                    result.push_back(" One");
+                    break;
+                case 2:
+                    result.push_back(" Two");
+                    break;
+                case 3:
+                    result.push_back(" Three");
+                    break;
+                case 4:
+                    result.push_back(" Four");
+                    break;
+                case 5:
+                    result.push_back(" Five");
+                    break;
+                case 6:
+                    result.push_back(" Six");
+                    break;
+                case 7:
+                    result.push_back(" Seven");
+                    break;
+                case 8:
+                    result.push_back(" Eight");
+                    break;
+                case 9:
+                    result.push_back(" Nine");
+                    break;
+            }
+        }
+    }
+    
+    return result;
+}
 
-    return words;
+bool issame(int n, int base) {
+    return n % base == 0;
 }
