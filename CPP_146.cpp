d51b6cfc42a9b62213cb897b049a4d1cdb520378
beep boop <githubbot@vadim.me>
2024-07-12 23:40:48 +0200

    Solution 22 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/3xmj1ii0 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,9 +1,8 @@
+```cpp
 #include <iostream>
-using namespace std;
 
 int specialFilter(int num) {
-    string num_str = to_string(num);
-    if (abs(stoi(num_str)) > 10 && (stoi(num_str) % 10) % 2 != 0 && (stoi(num_str) / 10) % 10 % 2 != 0) {
+    if (abs(num) > 10 && (num % 10) % 2 != 0 && (num / 10) % 10 % 2 != 0) {
         return 1;
     }
     return 0;
@@ -11,14 +10,14 @@ int specialFilter(int num) {
 
 int main() {
     int n, count = 0;
-    cout << "Enter the number of elements: ";
-    cin >> n;
+    std::cout << "Enter the number of elements: ";
+    std::cin >> n;
     for (int i = 0; i < n; i++) {
         int num;
-        cout << "Enter element " << i + 1 << ": ";
-        cin >> num;
-        count += specialFilter(num);
+        std::cout << "Enter element " << i + 1 << ": ";
+        std::cin >> num;
+        count += specialFilter(std::stoi(std::to_string(num)));
     }
-    cout << "Number of elements that meet the condition: " << count;
+    std::cout << "Number of elements that meet the condition: " << count;
     return 0;
 }
