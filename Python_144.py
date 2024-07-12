30ebd2eec87242da7e22d449221345ca7a7e0982
beep boop <githubbot@vadim.me>
2024-07-13 00:20:43 +0200

    Solution 88 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gwb2dchn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/68a100c4-6759-423a-9065-992ef40ce 235.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_144.py
+++ Python_144.py
@@ -1,4 +1,3 @@
-```
 from fractions import Fraction
 
 def simplify():
@@ -6,12 +5,9 @@ def simplify():
         x = float(input("Enter numerator: "))
         y = float(input("Enter denominator: "))
     except ValueError:
-        return """Invalid input! Please enter a number.
-Make sure to enter numbers, not words or special characters. Try again!"""
+        return "Invalid input! Please enter a number."
     
     if y == 0:
-        return """Denominator cannot be zero!
-In a fraction, the denominator is what we divide by to find our result. So, it's impossible for something to have a zero in this position.
-Try entering different values and see how your answer changes."""
+        return "Denominator cannot be zero!"
     else:
         return str(Fraction(x,y))
