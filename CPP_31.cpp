cc70dab06bfb618933f7d3bf77e60afb722ba8ea
beep boop <githubbot@vadim.me>
2024-07-12 19:26:30 +0200

    Solution 28 of CPP/31, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/cfqxaqy5 Prompt: e2bdff02-d329-44d0-870c-a3e8ba3b0725.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ e2bdff02-d329-44d0-870c-a3e8ba3b0725.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ e2bdff02-d329-44d0-870c-a3e8ba3b0725.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ e2bdff02-d329-44d0-870c-a3e8ba3b0725.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ e2bdff02-d329-44d0-870c-a3e8ba3b0725.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_31.cpp
+++ CPP_31.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 using namespace std;
 
@@ -13,7 +12,15 @@ bool is_prime(int num) {
 }
 
 int main() {
-    int num;
-    cin >> num;
+    while(true) {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        if (cin.fail()) {
+            cout << "Invalid input. Try again." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        } else break;
+    }
     cout << (is_prime(num) ? "True" : "False") << endl;
 }
