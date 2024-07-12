d8cf9a83bff7a37d12da237cf38abf83600a3fde
beep boop <githubbot@vadim.me>
2024-07-12 17:13:40 +0200

    Solution 29 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/sp39c892 Prompt: Program did not receive expected input

--- Python_1.py
+++ Python_1.py
@@ -1,14 +1,14 @@
 def separate_paren_groups(paren_string: str) -> List[str]:
     stack, result, start = [], [], -1
-    
+
     for i, char in enumerate(paren_string):
-        if char in '({':
+        if char in "({":
             if not stack:
                 start = i
             stack.append(char)
-        elif char in ')}':
+        elif char in ")}":
             stack.pop()
             if not stack:
-                result.append(paren_string[start:i+1])
-    
+                result.append(paren_string[start : i + 1])
+
     return result
