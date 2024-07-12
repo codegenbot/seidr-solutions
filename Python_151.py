cf0b660b26f11d2c3c0dede7476dea683376e2e1
beep boop <githubbot@vadim.me>
2024-07-13 00:48:37 +0200

    Solution 75 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/decphxfi Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5a5cfe81-8b98-4299-9d8f-5b6037d23 348.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5a5cfe81-8b98-4299-9d8f-5b6037d23 348.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,6 +1,5 @@
-def double_the_difference(lst):
-    if isinstance(lst, list):
-        odd_sum = sum(i**2 for i in lst if isinstance(i, int) and i >= 0 and i % 2 != 0)
-        return (sum(lst) - odd_sum) * 2
-    else:
-        raise TypeError("Invalid input")
+def check(num1, num2):
+    if double_the_difference([num1, num2]) == double_the_difference(
+        [abs(x) for x in [num1, num2]]
+    ):
+        return "It's a Pitfall!"
