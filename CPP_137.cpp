fc0e7be20214414b8ab78c4d707bf9ad97223cd0
beep boop <githubbot@vadim.me>
2024-07-12 15:13:07 +0200

    Solution 16 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pal7yz6s Prompt: ared    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 1a9a0a4d-17fd-40f6-9c82-029cac9b5d52.cpp:33:35: error: expected primary-expressi on before ‘>’ token    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 1a9a0a4d-17fd-40f6-9c82-029cac9b5d52.cpp:33:48: error: too many arguments to fun ction ‘std::string compare_one(std::string)’    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ~~~~~~~~~~~^~~~~~~~~~~~~~~~ 1a9a0a4d-17fd-40f6-9c82-029cac9b5d52.cpp:6:8: note: declared here     6 | string compare_one(string a) {       |        ^~~~~~~~~~~ 1a9a0a4d-17fd-40f6-9c82-029cac9b5d52.cpp: In function ‘std::string compare_one(s td::string)’: 1a9a0a4d-17fd-40f6-9c82-029cac9b5d52.cpp:20:1: warning: control reaches end of n on-void function [-Wreturn-type]    20 | }       | ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,26 +3,30 @@
 
 using namespace std;
 
-string compare_one(string a) {
-    double s1 = stod(a);
+string compare_one(string a, int b) {
+    stof s1 = stof(a);
     if (a.find('x') != string::npos)
-        return "None";
+        return a;
     else {
-        int s2 = stoi(a); 
+        a = to_string(b); 
+        stof s2 = stof(a);
         if (s1 > s2)
-            return to_string(s1);
+            return to_string(s2);
         else if (s1 == s2)
             return "None";
         else
-            return to_string(s1);
+            return to_string(s2);
     }
 }
 
 int main() {
     string a;
+    int b; 
     cout << "Enter the first number: ";
     cin >> a;
-    cout << compare_one(a) << endl;
+    cout << "Enter the second number: ";
+    cin >> b;
+    cout << compare_one(a, b) << endl;
 
     return 0;
 }
