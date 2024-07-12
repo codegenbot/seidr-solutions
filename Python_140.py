fd41f29a4563d3ee235881fe0bc1064b65e851c3
beep boop <githubbot@vadim.me>
2024-07-12 08:56:51 +0200

    Solution 29 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jkuz8vxn Prompt: Program did not receive expected input

--- Python_140.py
+++ Python_140.py
@@ -1,17 +1,2 @@
 ```
-import re
-
-
-def fix_spaces(text):
-    if not isinstance(text, str):
-        return "Invalid input. Please enter a string."
-
-    try:
-        new_text = re.sub(" +", "_", text)
-        if ' ' in new_text:
-            return new_text
-        else:
-            return "No space found"
-    except Exception as e:
-        print(f"Error: {e}")
-        return "Invalid input"
+return re.sub(r"\s+", "_", text)
