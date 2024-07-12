960c4f3b6a1322aafcc521021d1ddd602a9cc25a
beep boop <githubbot@vadim.me>
2024-07-12 17:38:57 +0200

    Solution 34 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pal7yz6s Prompt: ined here    21 | int main() {       |     ^~~~ In file included from 87efd82e-0cd6-47dc-8b23-81a326d1ed80.cpp:30: 87efd82e-0cd6-47dc-8b23-81a326d1ed80.cpp: In function ‘int main()’: 87efd82e-0cd6-47dc-8b23-81a326d1ed80.cpp:32:13: error: ‘boost’ has not been decl ared    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 87efd82e-0cd6-47dc-8b23-81a326d1ed80.cpp:32:35: error: expected primary-expressi on before ‘>’ token    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 87efd82e-0cd6-47dc-8b23-81a326d1ed80.cpp:32:48: error: too many arguments to fun ction ‘std::string compare_one(std::string)’    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ~~~~~~~~~~~^~~~~~~~~~~~~~~~ 87efd82e-0cd6-47dc-8b23-81a326d1ed80.cpp:6:8: note: declared here     6 | string compare_one(string a) {       |        ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,24 +3,26 @@
 
 using namespace std;
 
-string compare_one(string a, string b) {
+string compare_one(string a) {
     double s1 = stod(a);
-    int s2 = stoi(b); 
-    if (s1 > s2)
-        return "First number is greater";
-    else if (s1 == s2)
-        return "Numbers are equal";
-    else
-        return "Second number is greater";
+    if (a.find('x') != string::npos)
+        return "None";
+    else {
+        int s2 = stoi(a); 
+        if (s1 > s2)
+            return to_string(s1);
+        else if (s1 == s2)
+            return "None";
+        else
+            return to_string(s2);
+    }
 }
 
 int main() {
-    string a, b;
+    string a;
     cout << "Enter the first number: ";
     cin >> a;
-    cout << "Enter the second number: ";
-    cin >> b;
-    cout << compare_one(a, b) << endl;
+    cout << compare_one(a) << endl;
 
     return 0;
 }
