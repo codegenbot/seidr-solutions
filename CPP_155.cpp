bb2a089e5b7feb938216dca613f4b9d26b5c015e
beep boop <githubbot@vadim.me>
2024-07-12 06:55:17 +0200

    Solution 14 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ufq7am75 Prompt: 5cc49bcb-45b2-4a48-9225-6e0df844cd09.cpp:30:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    30 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 5cc49bcb-45b2-4a48-9225-6e0df844cd09.cpp:20:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    20 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ 5cc49bcb-45b2-4a48-9225-6e0df844cd09.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 5cc49bcb-45b2-4a48-9225-6e0df844cd09.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -17,11 +17,9 @@ vector<int> even_odd_count(int num) {
     return counts;
 }
 
-bool is_same(vector<int> a, vector<int> b){
-    return a == b;
-}
-
 int main() {
-    assert(is_same(even_odd_count(0), {1, 0}));
+    vector<int> expected = {1, 0};
+    vector<int> result = even_odd_count(0);
+    assert(expected == result);
     return 0;
 }
