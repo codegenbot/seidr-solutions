23ba14b29857de101dc0ea38cb4cf8fb970c3ba2
beep boop <githubbot@vadim.me>
2024-07-12 15:11:53 +0200

    Solution 54 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zj95medv Prompt: )’    33 | int main(){       |     ^~~~ a545ad34-f97e-4038-a24f-675552b25109.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from a545ad34-f97e-4038-a24f-675552b25109.cpp:24: a545ad34-f97e-4038-a24f-675552b25109.cpp: In function ‘int main()’: a545ad34-f97e-4038-a24f-675552b25109.cpp:34:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    34 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~ a545ad34-f97e-4038-a24f-675552b25109.cpp:7:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     7 | bool issame(const vector<int>& v1, const vector<int>& v2) {       |      ^~~~~~ a545ad34-f97e-4038-a24f-675552b25109.cpp:25:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,20 +1,13 @@
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
 using namespace std;
 
-bool same(const vector<int>& v1, const vector<int>& v2) {
-    if(v1.size() != v2.size())
-        return false;
-    for(int i = 0; i < v1.size(); i++) {
-        if(v1[i] != v2[i])
-            return false;
-    }
-    return true;
+bool isSame(const vector<int>& v1, const vector<int>& v2) {
+    return v1 == v2;
 }
 
-vector<int> incr_list(const vector<int>& l){
+vector<int> incrList(const vector<int>& l){
     vector<int> result(l.size());
     for(int i = 0; i < l.size(); i++) {
         result[i] = l[i]+1;
@@ -23,6 +16,6 @@ vector<int> incr_list(const vector<int>& l){
 }
 
 int main() {
-    assert(same({6, 3, 6,3, 4, 4, 10, 1, 124}, incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123})));
+    assert(isSame({6, 3, 6,3, 4, 4, 10, 1, 124}, incrList({5, 2, 5, 2, 3, 3, 9, 0, 123})));
     return 0;
 }
