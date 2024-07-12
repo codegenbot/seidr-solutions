a8127ca2dfaa471674280e0447dc9685e72ff32a
beep boop <githubbot@vadim.me>
2024-07-13 00:51:56 +0200

    Solution 77 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/6zi3sqj2 Prompt: Program did not receive expected input

--- leaders.cpp
+++ leaders.cpp
@@ -4,31 +4,32 @@ using namespace std;
 
 vector<int> findLeaders(const vector<int>& nums) {
     vector<int> leaders;
-    int maxRight = INT_MIN;
-    
+    int max_right = 0;
+
     for (int i = nums.size() - 1; i >= 0; i--) {
-        if (nums[i] >= maxRight) {
+        if (nums[i] >= max_right) {
             leaders.push_back(nums[i]);
-            maxRight = nums[i];
+            max_right = nums[i];
         }
     }
-    
+
     return leaders;
 }
 
 int main() {
     vector<int> nums;
     int num;
-    
     while (cin >> num) {
         nums.push_back(num);
+        if (cin.peek() == '\n') {
+            break;
+        }
     }
-    
+
     vector<int> result = findLeaders(nums);
-    
     for (int i = result.size() - 1; i >= 0; i--) {
         cout << result[i] << " ";
     }
-    
+
     return 0;
 }
