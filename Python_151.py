9badb4ba7708991109899d1dc97f63df27da42e5
beep boop <githubbot@vadim.me>
2024-07-12 03:07:49 +0200

    Solution 44 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ob2502x2 Prompt: 168 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/94d98b35-191b-49bd-81a7-08b1636f4 0b8.py", line 14, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/94d98b35-191b-49bd-81a7-08b1636f4 0b8.py", line 10, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,9 +1,5 @@
-def double_the_difference(lst):
-    return sum(x**2 for x in lst if x > 0 and type(x) == int and x % 2 != 0) * 2
-
-def check(lst):  # Define the variable lst
-    odd_sum = double_the_difference(lst)
-    return double_the_difference(lst) == odd_sum
+def double_the_difference(list):
+    return sum(x**2 for x in list if x > 0 and type(x) == int and x % 2 != 0) * 2
 
 input_list = [1, 3, 5, 2, 7]
-print(check(input_list))
+print(double_the_difference(input_list))
