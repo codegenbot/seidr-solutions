1a11eea19d180de513bb369a09477721dba2fd6f
beep boop <githubbot@vadim.me>
2024-07-12 11:06:36 +0200

    Solution 21 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/37by9irg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d739d3c9-17e6-4275-b340-a2f265dc1 a2f.py", line 31, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d739d3c9-17e6-4275-b340-a2f265dc1 a2f.py", line 27, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -6,7 +6,7 @@ def encode(message):
         if char.isalpha():
             char_code = ord(char)
             if char.lower() in vowels:
-                new_code = (char_code - ord('A') + vowels.index(char.lower()) - 2) % 26 + ord('A')
+                new_code = (char_code - ord('A') + vowels.index(char.lower()) + 1) % 26 + ord('A')
                 if char.isupper():
                     encoded_message += chr(
                         new_code if new_code <= 90 else new_code - 26
@@ -18,5 +18,5 @@ def encode(message):
             else:
                 encoded_message += char.swapcase()
         else:
-            encoded_message += " "
+            encoded_message += char
     return encoded_message
