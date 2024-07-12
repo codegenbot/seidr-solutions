c0e47dd1b90d970a132fc9463511096f36451260
beep boop <githubbot@vadim.me>
2024-07-12 16:52:58 +0200

    Solution 60 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q8x7adb5 Prompt:       |                              ^~~~~~ 095764fc-cccf-4c54-998b-8130b13caa3d.cpp:260:37: error: ‘string’ was not declare d in this scope   260 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 095764fc-cccf-4c54-998b-8130b13caa3d.cpp:260:44: error: ‘b’ was not declared in this scope   260 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 095764fc-cccf-4c54-998b-8130b13caa3d.cpp:260:45: error: expression list treated as compound expression in initializer [-fpermissive]   260 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 095764fc-cccf-4c54-998b-8130b13caa3d.cpp:259: 095764fc-cccf-4c54-998b-8130b13caa3d.cpp: In function ‘int main()’: 095764fc-cccf-4c54-998b-8130b13caa3d.cpp:269:20: error: ‘by_length’ was not decl ared in this scope   269 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 095764fc-cccf-4c54-998b-8130b13caa3d.cpp:269:19: error: ‘issame’ cannot be used as a function   269 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,16 +1,92 @@
-#include <vector>
-#include <string>
-using namespace std;
-
-bool isSame(vector<string> v1, vector<string> v2) {
-    if (v1.size() != v2.size()) 
-        return false;
-    for (int i = 0; i < v1.size(); i++) 
-        if (v1[i] != v2[i]) 
-            return false;
-    return true;
+bool issame(vector<string> a, vector<string> b) {
+    return a.size() == b.size();
 }
 
-bool by_length(const string& s1, const string& s2) {
-    return s1.length() > s2.length();
+vector<vector<string>> by_length(const vector<int>& numbers) {
+    map<int, vector<string>> result;
+    
+    for (int num : numbers) {
+        if (num < 10) {
+            string str = to_string(num);
+            if (!result.count(str.length())) {
+                result[str.length()] = {};
+            }
+            result[str.length()].push_back(str);
+        } else if (num >= 100) {
+            string str = "One Hundred";
+            if (num % 100 == 11) str += "th";
+            else if (num % 100 >= 12 && num % 100 <= 13) str += "ths";
+            else if (num % 100 >= 14 && num % 100 <= 19) str += "teenth";
+            result[6].push_back(str);
+        } else {
+            string str = "";
+            int temp = num;
+            if (temp / 20 > 0) {
+                str += get_tens(temp / 20) + " ";
+                temp %= 20;
+            }
+            if (temp >= 10) {
+                str += get_teens(temp);
+                break;
+            } else {
+                for (int i = 1; i <= 9; i++) {
+                    if ((num % 10) == i) {
+                        str += get_single(i);
+                        break;
+                    }
+                }
+            }
+            result[str.size()].push_back(str);
+        }
+    }
+
+    vector<vector<string>> final_result(result.begin(), result.end());
+    return final_result;
+}
+
+string get_tens(int tens) {
+    switch (tens) {
+        case 2:
+            return "Twenty";
+        case 3:
+            return "Thirty";
+        case 4:
+            return "Forty";
+        case 5:
+            return "Fifty";
+        default:
+            break;
+    }
+}
+
+string get_teens(int teens) {
+    switch (teens) {
+        case 11: return "Eleven";
+        case 12: return "Twelve";
+        case 13: return "Thirteen";
+        case 14: return "Fourteen";
+        case 15: return "Fifteen";
+        case 16: return "Sixteen";
+        case 17: return "Seventeen";
+        case 18: return "Eighteen";
+        case 19: return "Nineteen";
+        default:
+            break;
+    }
+}
+
+string get_single(int single) {
+    switch (single) {
+        case 1: return "One";
+        case 2: return "Two";
+        case 3: return "Three";
+        case 4: return "Four";
+        case 5: return "Five";
+        case 6: return "Six";
+        case 7: return "Seven";
+        case 8: return "Eight";
+        case 9: return "Nine";
+        default:
+            break;
+    }
 }
