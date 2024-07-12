676bd6fefd27b4a742e039ebd2b1591f5cd6602b
beep boop <githubbot@vadim.me>
2024-07-12 14:11:49 +0200

    Solution 1 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/5y8dhe10 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,49 +1,25 @@
 #include <vector>
 using namespace std;
 
-vector<int> cutVector(vector<int>& vec) {
+pair<vector<int>, vector<int>> cutVector(vector<int> v) {
     int minDiff = INT_MAX;
-    int splitIndex = -1;
+    int cutIndex = 0;
     
-    for (int i = 0; i < vec.size() - 1; i++) {
-        int leftSum = 0, rightSum = 0;
-        
-        for (int j = 0; j <= i; j++)
-            leftSum += vec[j];
-        for (int j = i + 1; j < vec.size(); j++)
-            rightSum += vec[j];
-        
-        int diff = abs(leftSum - rightSum);
+    for (int i = 1; i < v.size(); i++) {
+        int diff = abs(v[i] - v[0]);
         if (diff < minDiff) {
             minDiff = diff;
-            splitIndex = i;
+            cutIndex = i;
         }
     }
     
-    vector<int> leftVec(vec.begin(), vec.begin() + splitIndex + 1);
-    vector<int> rightVec(vec.begin() + splitIndex + 1, vec.end());
+    vector<int> left = {v[0]};
+    for (int i = 1; i <= cutIndex; i++) {
+        left.push_back(v[i]);
+    }
     
-    return {leftVec, rightVec};
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++)
-        cin >> vec[i];
+    vector<int> right = v;
+    right.erase(right.begin(), right.begin() + cutIndex);
     
-    pair<vector<int>, vector<int>> result = cutVector(vec);
-    
-    cout << "Left: ";
-    for (int num : result.first)
-        cout << num << " ";
-    cout << endl;
-    
-    cout << "Right: ";
-    for (int num : result.second)
-        cout << num << " ";
-    cout << endl;
-    
-    return 0;
+    return make_pair(left, right);
 }
