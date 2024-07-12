004d668bc90e595dd62b2817e1f2a60841b44ef1
beep boop <githubbot@vadim.me>
2024-07-12 09:49:21 +0200

    Solution 64 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ccbjq8ug Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f039964a-4454-4e7a-9532-5a742866c 737.py", line 25, in <module>     check(solution)           ^^^^^^^^ NameError: name 'solution' is not defined

--- Python_121.py
+++ Python_121.py
@@ -1,12 +1,11 @@
+```
 def count_odd_numbers():
     while True:
         cont = input("Do you want to continue? (y/n): ")
         should_continue = cont.strip().lower() == "y"
         while not should_continue:
             try:
-                lst = [
-                    int(x) for x in input("Enter numbers separated by space: ").split()
-                ]
+                lst = [int(x) for x in input("Enter numbers separated by space: ").split()]
                 if len(lst) == 0:
                     break
                 print(len([i for i in lst if i % 2 != 0]))
@@ -16,10 +15,3 @@ def count_odd_numbers():
                 print("Invalid input. Please enter numbers separated by space.")
         else:
             print("Program ended.")
-
-
-def solution():
-    count_odd_numbers()
-
-
-solution()
