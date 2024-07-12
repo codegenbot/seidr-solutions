1a32ca4d07f81957fde98353d1a4469569ef34a9
beep boop <githubbot@vadim.me>
2024-07-12 16:00:43 +0200

    Solution 44 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eui281em Prompt:       | ^~~~~~~~~~~~~ 0a601551-46e5-4268-a80d-ce3f728d3f93.cpp:23:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    23 | int main(){       |         ^~ 0a601551-46e5-4268-a80d-ce3f728d3f93.cpp:23:9: note: remove parentheses to defau lt-initialize a variable    23 | int main(){       |         ^~       |         -- 0a601551-46e5-4268-a80d-ce3f728d3f93.cpp:23:9: note: or replace parentheses with  braces to value-initialize a variable 0a601551-46e5-4268-a80d-ce3f728d3f93.cpp:23:11: error: a function-definition is not allowed here before ‘{’ token    23 | int main(){       |           ^ 0a601551-46e5-4268-a80d-ce3f728d3f93.cpp:25:2: error: expected ‘}’ at end of inp ut    25 | }       |  ^ 0a601551-46e5-4268-a80d-ce3f728d3f93.cpp:17:12: note: to match this ‘{’    17 | int main() {       |            ^

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,5 +1,6 @@
-int main() {
-    std::vector<std::vector<int>> q = {{1}};
-    int w = 2;
-    std::cout << (will_it_fly(q, w) ? "The plane will fly." : "The plane won't fly.") << std::endl;
+int main {
+    std::vector<std::vector<int>> q = {{1,2,3}};
+    assert(will_it_fly(q, 6) == true);
+    std::cout << will_it_fly(q, 5) << std::endl;
     return 0;
+}
