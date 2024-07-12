14a400ff4dce2dc701b08b954f8aeeb86bd69ae4
beep boop <githubbot@vadim.me>
2024-07-12 09:34:36 +0200

    Solution 80 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ts2ifgq4 Prompt: Program did not receive expected input

--- Python_84.py
+++ Python_84.py
@@ -3,8 +3,9 @@ def solve():
         N = input("Please provide a binary number: ")
         if all(c in "01" for c in N):
             print(bin(int(N, 2))[2:])
-            break
         else:
             print("Invalid Input. Please provide a binary number.")
+            continue
+        break
 
 solve()
