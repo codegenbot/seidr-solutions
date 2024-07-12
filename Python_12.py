ad2b6d8bc028826d99067d29d61c7c20a59eb335
beep boop <githubbot@vadim.me>
2024-07-13 01:17:29 +0200

    Solution 16 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/vkgoy280 Prompt: Program did not receive expected input

--- Python_12.py
+++ Python_12.py
@@ -4,11 +4,4 @@ from typing import List, Optional
 def longest(strings: List[Optional[str]]) -> Optional[str]:
     if not all(isinstance(s, str) or s is None for s in strings):
         return None
-    return next(
-        (
-            s
-            for s in strings
-            if s is not None and len(s) == len(max(filter(None, strings)))
-        ),
-        None,
-    )
+    return next((s for s in strings if len(s) == max(len(x) for x in strings)), None)
