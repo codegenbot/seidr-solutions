61594d2ad6d5cc3a9f0d814e7d1df86c23b863cf
beep boop <githubbot@vadim.me>
2024-07-13 00:30:39 +0200

    Solution 14 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lpo8cfzh Prompt: 4534928f-b102-4c62-aec4-eba55538790f.cpp:8:88: error: narrowing conversion of ‘2 2595’ from ‘int’ to ‘char’ [-Wnarrowing] 4534928f-b102-4c62-aec4-eba55538790f.cpp:8:88: error: narrowing conversion of ‘2 2604’ from ‘int’ to ‘char’ [-Wnarrowing] 4534928f-b102-4c62-aec4-eba55538790f.cpp:8:88: error: narrowing conversion of ‘1 8776’ from ‘int’ to ‘char’ [-Wnarrowing] 4534928f-b102-4c62-aec4-eba55538790f.cpp:8:88: error: narrowing conversion of ‘1 8774’ from ‘int’ to ‘char’ [-Wnarrowing] 4534928f-b102-4c62-aec4-eba55538790f.cpp: At global scope: 4534928f-b102-4c62-aec4-eba55538790f.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 4534928f-b102-4c62-aec4-eba55538790f.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from 4534928f-b102-4c62-aec4-eba55538790f.cpp:27: 4534928f-b102-4c62-aec4-eba55538790f.cpp: In function ‘int main()’: 4534928f-b102-4c62-aec4-eba55538790f.cpp:29:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘intToMiniRoman’?    29 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -4,13 +4,13 @@
 
 std::string intToMiniRoman(int n) {
     std::string roman;
-    int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    char *syb[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    const int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    const char* syb[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     int i = 0;
     while (n > 0) {
         if (n >= val[i]) {
             n -= val[i];
-            roman += syb[i][0]; 
+            roman += syb[i];
         } else {
             i++;
         }
@@ -18,8 +18,5 @@ std::string intToMiniRoman(int n) {
     return roman;
 }
 
-int main() {
-    assert(intToMiniRoman(1000) == "M");
-    std::cout << intToMiniRoman(1999) << std::endl;
-    return 0;
-}
+assert(intToMiniRoman(1000) == "M");
+std::cout << intToMiniRoman(1999) << std::endl;
