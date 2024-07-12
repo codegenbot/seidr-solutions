28a48816b639e48cbe684d0d124c20ac498da63b
beep boop <githubbot@vadim.me>
2024-07-12 14:26:54 +0200

    Solution 2 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/8xd1obee Prompt: 157c241d-a1c9-49cc-9934-4b3fe30aaffe.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ 157c241d-a1c9-49cc-9934-4b3fe30aaffe.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ 157c241d-a1c9-49cc-9934-4b3fe30aaffe.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,22 +1,25 @@
+#include <string>
+
 int mastermind(string code, string guess) {
     int white = 0;
     int black = 0;
 
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            ++black;
+    // Count the number of correct colors in wrong places (white pegs)
+    for(int i=0; i<4; i++) {
+        if(code[i] == guess[i]) {
+            black++;
+        }
+        else if(count(guess.begin(), guess.end(), code[i]) > 0) {
+            white++;
         }
     }
 
-    for (char c : code) {
-        int count = 0;
-        for (char d : guess) {
-            if (c == d && code.find(d) != code.find(c)) {
-                ++count;
-            }
+    // Count the number of correct colors in correct places (black pegs)
+    for(int i=0; i<4; i++) {
+        if(code[i] == guess[i]) {
+            black++;
         }
-        white += count;
     }
 
-    return black + white;
+    return black;
 }
