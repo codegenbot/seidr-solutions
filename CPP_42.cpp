e07306b7f2b7286e747feec934bf990c9ae423e4
beep boop <githubbot@vadim.me>
2024-07-12 11:28:01 +0200

    Solution 17 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/q9bacny6 Prompt:     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bf49d87a-82e0-4bb2-8c9b-c2b2885128e2.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bf49d87a-82e0-4bb2-8c9b-c2b2885128e2.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bf49d87a-82e0-4bb2-8c9b-c2b2885128e2.cpp:7: bf49d87a-82e0-4bb2-8c9b-c2b2885128e2.cpp: In function ‘int main()’: bf49d87a-82e0-4bb2-8c9b-c2b2885128e2.cpp:17:20: error: ‘incr_list’ was not decla red in this scope    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ bf49d87a-82e0-4bb2-8c9b-c2b2885128e2.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
-vector<int> incr_list(vector<int> l) {
+std::vector<int> incr_list(std::vector<int> l) {
     for (int i = 0; i < l.size(); i++) {
         l[i]++;
     }
     return l;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a == b) {
-        return true;
-    } else {
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
         return false;
     }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main() {
-    vector<int> vec = {1, 2, 3, 4, 5};
-    vector<int> modified_vec = incr_list(vec);
+    std::vector<int> v = {1, 2, 3};
+    std::vector<int> v_incr = incr_list(v);
+    std::vector<int> v_copy = {2, 3, 4};
 
-    for (int num : modified_vec) {
-        cout << num << " ";
+    if (issame(v_incr, v_copy)) {
+        std::cout << "v_incr is the same as v_copy." << std::endl;
+    } else {
+        std::cout << "v_incr is different from v_copy." << std::endl;
     }
 
     return 0;
