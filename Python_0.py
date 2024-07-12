29619d09ccfca81ede011681b4f5f388175b53ea
beep boop <githubbot@vadim.me>
2024-07-12 23:49:19 +0200

    Solution 6 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tgwb3xo1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/74daac23-8f10-4b1c-a500-c4b8a6e7b e9f.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_0.py
+++ Python_0.py
@@ -1,4 +1,5 @@
-def has_close_elements(numbers: List[float], threshold: float) -> bool:
+```python
+def has_close_elements(numbers: list[float], threshold: float) -> bool:
     numbers = sorted(set(numbers))
     for i in range(len(numbers) - 1):
         if abs(numbers[i] - numbers[i+1]) <= threshold:
