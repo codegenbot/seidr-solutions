fe3a47a8fd41c36e045084c04b94b2d262ae6200
beep boop <githubbot@vadim.me>
2024-07-12 14:15:45 +0200

    Solution 2 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d8rx6wrb Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 014e94b6-6e12-4a61-bd95-dff22dda3bb9.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 014e94b6-6e12-4a61-bd95-dff22dda3bb9.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 014e94b6-6e12-4a61-bd95-dff22dda3bb9.cpp:23: 014e94b6-6e12-4a61-bd95-dff22dda3bb9.cpp: In function ‘int main()’: 014e94b6-6e12-4a61-bd95-dff22dda3bb9.cpp:33:20: error: ‘sort_third’ was not decl ared in this scope    33 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 014e94b6-6e12-4a61-bd95-dff22dda3bb9.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,7 +1,16 @@
+```cpp
+#include <algorithm>
+#include <vector>
+
+bool issame(vector<int> a,vector<int>b){
+    return a == b;
+}
+
 vector<int> sort_third(vector<int> l) {
     vector<int> result;
     for (int i = 0; i < l.size(); i++) {
         if (i % 3 == 0) {
+            // values at the indices that are divisible by three are equal to the values of the corresponding indicies of l, but sorted
             vector<int> temp;
             for (int j = i; j < l.size() && j % 3 == 0; j++) {
                 temp.push_back(l[j]);
