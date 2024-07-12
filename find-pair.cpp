ee7f6a4b56a969eb8dd9d14fb2e1b75dd9ffc445
beep boop <githubbot@vadim.me>
2024-07-12 17:32:31 +0200

    Solution 8 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/wrjgqrw5 Prompt: n before ‘int’    10 |     map<int, int> numIndex;       |         ^~~ 16f2bac5-f7f6-48a7-88bd-18137534e7a8.cpp:12:35: error: ‘nums’ was not declared i n this scope    12 |         int complement = target - nums[i];       |                                   ^~~~ 16f2bac5-f7f6-48a7-88bd-18137534e7a8.cpp:13:13: error: ‘numIndex’ was not declar ed in this scope    13 |         if (numIndex.find(complement) != numIndex.end()) {       |             ^~~~~~~~ 16f2bac5-f7f6-48a7-88bd-18137534e7a8.cpp:14:13: error: ‘cout’ was not declared i n this scope    14 |             cout << complement << endl;       |             ^~~~ 16f2bac5-f7f6-48a7-88bd-18137534e7a8.cpp:14:35: error: ‘endl’ was not declared i n this scope    14 |             cout << complement << endl;       |                                   ^~~~ 16f2bac5-f7f6-48a7-88bd-18137534e7a8.cpp:18:9: error: ‘numIndex’ was not declare d in this scope    18 |         numIndex[nums[i]] = i;       |         ^~~~~~~~

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 #include <vector>
 #include <map>
+using namespace std;
 
 int main() {
     int n, target;
-    std::cin >> n;
-    std::vector<int> nums(n);
+    cin >> n;
+    vector<int> nums(n);
     for (int i = 0; i < n; ++i) {
-        std::cin >> nums[i];
+        cin >> nums[i];
     }
-    std::cin >> target;
+    cin >> target;
 
-    std::map<int, int> numIndex;
+    map<int, int> numIndex;
     for (int i = 0; i < n; ++i) {
         int complement = target - nums[i];
         if (numIndex.find(complement) != numIndex.end()) {
-            std::cout << complement << std::endl;
-            std::cout << nums[i] << std::endl;
+            cout << complement << endl;
+            cout << nums[i] << endl;
             break;
         }
         numIndex[nums[i]] = i;
