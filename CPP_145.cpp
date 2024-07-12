497383a4463afdc14883786ad5ea15a483e5684b
beep boop <githubbot@vadim.me>
2024-07-13 00:50:15 +0200

    Solution 58 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d8ku8tb5 Prompt: In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 77cd4896-b8b1-4727-ac69-0e242ab1af23.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 77cd4896-b8b1-4727-ac69-0e242ab1af23.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 77cd4896-b8b1-4727-ac69-0e242ab1af23.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 77cd4896-b8b1-4727-ac69-0e242ab1af23.cpp:23: 77cd4896-b8b1-4727-ac69-0e242ab1af23.cpp: In function ‘int main()’: 77cd4896-b8b1-4727-ac69-0e242ab1af23.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -11,16 +11,6 @@ int order_by_points(const std::vector<int>& nums) {
     return points;
 }
 
-bool compare(const std::vector<int>& nums) {
-    for (int i = 1; i < nums.size(); i++) {
-        if (nums[i] <= nums[i - 1]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int main() {
-    assert(compare(order_by_points({0,6,6,-76,-21,23,4}) ) == {-76, -21, 0,4,23,6,6});
-    return 0;
+bool same(const std::vector<int>& a,const std::vector<int>& b) {
+    return (a == b);
 }
