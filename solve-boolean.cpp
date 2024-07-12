3a4499b5d134e1cd7bfd5f91b63381b5064c20ec
beep boop <githubbot@vadim.me>
2024-07-13 01:30:06 +0200

    Solution 24 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/om6uiz7z Prompt: cc842598-3f87-406c-8db1-d87050015069.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string solveBoolean(string s) {       | ^~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,31 +1,27 @@
-```cpp
-#include <iostream>
 #include <string>
-
-using namespace std;
 
 string solveBoolean(string s) {
     bool result = false;
     for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'T') {
+        if (s[i] == 't') {
             result = true;
-        } else if (s[i] == 'F') {
+        } else if (s[i] == 'f') {
             return "False";
         } else if (s[i] == '|') {
             if (result) {
                 for (int j = i + 1; j < s.length(); j++) {
-                    if (s[j] == 'T') {
+                    if (s[j] == 't') {
                         result = true;
                         break;
-                    } else if (s[j] == 'F') {
+                    } else if (s[j] == 'f') {
                         return "False";
                     }
                 }
             } else {
                 for (int j = i + 1; j < s.length(); j++) {
-                    if (s[j] == 'T') {
+                    if (s[j] == 't') {
                         return "True";
-                    } else if (s[j] == 'F') {
+                    } else if (s[j] == 'f') {
                         result = true;
                         break;
                     }
@@ -34,18 +30,18 @@ string solveBoolean(string s) {
         } else if (s[i] == '&') {
             if (!result) {
                 for (int j = i + 1; j < s.length(); j++) {
-                    if (s[j] == 'T') {
+                    if (s[j] == 't') {
                         result = true;
                         break;
-                    } else if (s[j] == 'F') {
+                    } else if (s[j] == 'f') {
                         return "False";
                     }
                 }
             } else {
                 for (int j = i + 1; j < s.length(); j++) {
-                    if (s[j] == 'T') {
+                    if (s[j] == 't') {
                         continue;
-                    } else if (s[j] == 'F') {
+                    } else if (s[j] == 'f') {
                         return "False";
                     }
                 }
