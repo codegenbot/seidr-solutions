2e3bea51e676ea2bbd50ac560db7a8f28c20d88d
beep boop <githubbot@vadim.me>
2024-07-13 00:09:11 +0200

    Solution 9 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/agmlm4ji Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4e6c868f-8a3f-4151-ae73-02df00007 c11.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_5.py
+++ Python_5.py
@@ -1,16 +1,17 @@
-```
 from typing import List
 
 
 def intersperse(numbers: list, delimiter: int) -> List[int]:
+    if not isinstance(numbers, list):
+        return [0]
+    if not all(isinstance(i, int) for i in numbers):
+        return [1]
+    if not isinstance(delimiter, int):
+        return [2]
+
+    if not numbers:
+        return []
     result = [numbers[0]]
     for i in range(1, len(numbers)):
         result.extend([result[-1], delimiter, numbers[i]])
     return result
-
-numbers = input("Enter a list of numbers separated by comma (no space): ")
-delimiter = int(input("Enter an integer as a delimiter: "))
-
-input_numbers = [int(x) for x in numbers.split(",")]
-
-print(intersperse(input_numbers, delimiter))
