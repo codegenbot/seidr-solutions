7940679cf4411f58c11b8d2245e14dac867c3dd9
beep boop <githubbot@vadim.me>
2024-07-12 15:25:14 +0200

    Solution 15 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q8x7adb5 Prompt: In file included from /usr/include/c++/11/string:39,                  from e3918c0e-5797-48ad-9188-5cab00c89836.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from e3918c0e-5797-48ad-9188-5cab00c89836.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e3918c0e-5797-48ad-9188-5cab00c89836.cpp:77:44: error: ‘b’ was not declared in t his scope    77 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e3918c0e-5797-48ad-9188-5cab00c89836.cpp:77:45: error: expression list treated a s compound expression in initializer [-fpermissive]    77 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e3918c0e-5797-48ad-9188-5cab00c89836.cpp:76: e3918c0e-5797-48ad-9188-5cab00c89836.cpp: In function ‘int main()’: e3918c0e-5797-48ad-9188-5cab00c89836.cpp:86:19: error: ‘issame’ cannot be used a s a function    86 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,18 +1,7 @@
-```cpp
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
+bool issame(std::vector<std::string>, std::vector<std::string>);
 
 std::vector<std::string> by_length(std::vector<int> arr) {
     std::vector<int> sortedArr;
