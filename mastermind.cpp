f9a3c8149e204ba21c69d15268dc032dca6bb922
beep boop <githubbot@vadim.me>
2024-07-12 21:48:41 +0200

    Solution 16 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/8xd1obee Prompt:       |                                 ^~~~~~ In file included from af03a88f-04e1-4da7-99f6-948c43f70a4a.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ af03a88f-04e1-4da7-99f6-948c43f70a4a.cpp:4:29: error: ‘string’ was not declared in this scope     4 | int mastermind(string code, string guess) {       |                             ^~~~~~ af03a88f-04e1-4da7-99f6-948c43f70a4a.cpp:4:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from af03a88f-04e1-4da7-99f6-948c43f70a4a.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from af03a88f-04e1-4da7-99f6-948c43f70a4a.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ af03a88f-04e1-4da7-99f6-948c43f70a4a.cpp:4:41: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,27 +1,26 @@
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
 
 int mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
 
+    // Count the number of correct colors in wrong places (white pegs)
+    for(int i=0; i<4; i++) {
+        if(code[i] == guess[i]) {
+            black++;
+        }
+        else if(std::count(guess.begin(), guess.end(), code[i]) > 0) {
+            white++;
+        }
+    }
+
+    // Count the number of correct colors in correct places (black pegs)
     for(int i=0; i<4; i++) {
         if(code[i] == guess[i]) {
             black++;
         }
     }
 
-    for(int i=0; i<4; i++) {
-        bool inCode = false;
-        for(int j=0; j<4; j++) {
-            if(guess[i] == code[j]) {
-                inCode = true;
-                break;
-            }
-        }
-        if(!inCode) continue;
-        white++;
-    }
-
-    return black;
+    return white;
 }
