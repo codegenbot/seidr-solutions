e26b2c03bc352b5cf9fba92925b33ac36854f54c
beep boop <githubbot@vadim.me>
2024-07-12 08:24:41 +0200

    Solution 60 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/0t6m7jpw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d96b20a6-bd1b-4b97-ab96-e0f351b85 2cd.py", line 16, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d96b20a6-bd1b-4b97-ab96-e0f351b85 2cd.py", line 13, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d96b20a6-bd1b-4b97-ab96-e0f351b85 2cd.py", line 5, in max_fill     rows = len(capacity_grid)            ^^^^^^^^^^^^^^^^^^ TypeError: object of type 'int' has no len()

--- Python_115.py
+++ Python_115.py
@@ -1,6 +1,8 @@
+import math
+
 def max_fill(n, capacity_grid):
-    rows = len(capacity_grid)
-    cols = len(capacity_grid[0])
-    total_water = rows * cols
-    max_capacity = min(row for row in capacity_grid[0])
+    total_water = 0
+    for row in capacity_grid:
+        total_water += len(row)
+    max_capacity = min(min(row) for row in capacity_grid)
     return math.ceil(total_water / (n * max_capacity))
