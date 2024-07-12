87c87e44b8ad27d0da396f60d02dfc770279839c
beep boop <githubbot@vadim.me>
2024-07-12 16:10:52 +0200

    Solution 40 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lbeiz7pq Prompt: r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <algorithm>   +++ |+#include <cassert>     3 | 5faad482-b1df-4cea-92a8-2c9f4f1f4163.cpp:22:43: error: expected ‘}’ before ‘;’ t oken    22 |     assert(is_happy("iopaxioi") == false );       |                                           ^ 5faad482-b1df-4cea-92a8-2c9f4f1f4163.cpp:21:10: note: to match this ‘{’    21 | int main {       |          ^ 5faad482-b1df-4cea-92a8-2c9f4f1f4163.cpp:23:5: error: expected unqualified-id be fore ‘return’    23 |     return 0;       |     ^~~~~~ 5faad482-b1df-4cea-92a8-2c9f4f1f4163.cpp:26:10: error: ‘int main()’ redeclared a s different kind of entity    26 | int main(){       |          ^ 5faad482-b1df-4cea-92a8-2c9f4f1f4163.cpp:21:5: note: previous declaration ‘int m ain’    21 | int main {       |     ^~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,6 +1,6 @@
-#include <cassert>
 #include <string>
 #include <algorithm>
+#include <cassert>
 
 bool is_happy(std::string s){
     if(s.length() < 3) return false;
@@ -19,7 +19,7 @@ bool is_happy(std::string s){
 
 }
 
-int main(){
+int main() {
     assert(is_happy("iopaxioi") == false );
     return 0;
 }
