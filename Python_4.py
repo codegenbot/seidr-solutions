9cf52d947b4f962c162c5c5eb77e8bdfd0c1276b
beep boop <githubbot@vadim.me>
2024-07-12 14:32:07 +0200

    Solution 0 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/63yredxm Prompt: Complete the following code given the task description and function signature.

--- Python_4.py
+++ Python_4.py
@@ -2,7 +2,5 @@ from typing import List
 
 
 def mean_absolute_deviation(numbers: List[float]) -> float:
-    if len(numbers) == 0:
-        return None
     mean = sum(numbers) / len(numbers)
     return sum(abs(num - mean) for num in numbers) / len(numbers)
