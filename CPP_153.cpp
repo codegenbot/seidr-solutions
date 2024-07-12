cda4b0c7f75631d85a8e7a40856a3e1b36f7d6cf
beep boop <githubbot@vadim.me>
2024-07-13 00:01:46 +0200

    Solution 17 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hpkkxb14 Prompt: g Strongest_Extension(const std::vector<std::__cxx11::basic_string<char> >&)’     4 | std::string Strongest_Extension(const std::vector<std::string>& extensio ns);       |             ^~~~~~~~~~~~~~~~~~~ dcf7b44d-2d66-4e72-b65c-331a20c674c9.cpp: In function ‘int Strongest_Extension(c onst std::vector<std::__cxx11::basic_string<char> >&)’: dcf7b44d-2d66-4e72-b65c-331a20c674c9.cpp:29:12: error: cannot convert ‘std::stri ng’ {aka ‘std::__cxx11::basic_string<char>’} to ‘int’ in return    29 |     return strongest_extension;       |            ^~~~~~~~~~~~~~~~~~~ In file included from dcf7b44d-2d66-4e72-b65c-331a20c674c9.cpp:32: dcf7b44d-2d66-4e72-b65c-331a20c674c9.cpp: In function ‘int main()’: dcf7b44d-2d66-4e72-b65c-331a20c674c9.cpp:34:33: error: invalid initialization of  reference of type ‘const std::vector<std::__cxx11::basic_string<char> >&’ from expression of type ‘const char [3]’    34 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |                                 ^~~~ dcf7b44d-2d66-4e72-b65c-331a20c674c9.cpp:4:65: note: in passing argument 1 of ‘s td::string Strongest_Extension(const std::vector<std::__cxx11::basic_string<char > >&)’     4 | :string Strongest_Extension(const std::vector<std::string>& extensions);       |                             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,28 +1,28 @@
 #include <vector>
 #include <string>
 
-std::string Strongest_Extension(const std::vector<std::string>& extensions);
+std::string Strongest_Extension(const std::vector<std::pair<std::string, std::string>>& extensions);
 
-int main() {
-    std::vector<std::string> extensions = {"Sp", "671235", "Bb"};
+int run() {
+    std::vector<std::pair<std::string, std::string>> extensions = {{"Sp", "671235"}, {"Bb"}};
     assert(Strongest_Extension(extensions) == "Sp.671235");
     return 0;
 }
 
-int Strongest_Extension(const std::vector<std::string>& extensions){
+std::string Strongest_Extension(const std::vector<std::pair<std::string, std::string>>& extensions){
     int max_strength = 0;
     std::string strongest_extension;
 
     for(auto extension : extensions){
         int cap = 0, sm = 0;
-        for(char c : extension){
+        for(char c : extension.first){
             if(std::isupper(c)) cap++;
             else if(std::islower(c)) sm++;
         }
         int strength = cap - sm;
-        if(strength > max_strength || (strength == max_strength && extension < strongest_extension)){
+        if(strength > max_strength || (strength == max_strength && extension.first < strongest_extension)){
             max_strength = strength;
-            strongest_extension = extension;
+            strongest_extension = extension.first + "." + extension.second;
         }
     }
 
