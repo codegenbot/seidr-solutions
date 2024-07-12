f632ca476b8ea07a9eedc56b525c985457a8a3b0
beep boop <githubbot@vadim.me>
2024-07-12 14:27:02 +0200

    Solution 10 of mastermind, ratio 0.1 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/tok1wzrn Prompt:    13 |             ++guessFreq[guess[i]];       |               ^~~~~~~~~ 5bc3fbbb-ff6d-41ef-b3db-1de9aabce094.cpp:17:23: error: ‘codeFreq’ was not declar ed in this scope    17 |     for (auto& pair : codeFreq) {       |                       ^~~~~~~~ 5bc3fbbb-ff6d-41ef-b3db-1de9aabce094.cpp:18:35: error: ‘guessFreq’ was not decla red in this scope    18 |         white += min(pair.second, guessFreq[pair.first]);       |                                   ^~~~~~~~~ 5bc3fbbb-ff6d-41ef-b3db-1de9aabce094.cpp:18:18: error: ‘min’ was not declared in  this scope; did you mean ‘main’?    18 |         white += min(pair.second, guessFreq[pair.first]);       |                  ^~~       |                  main 5bc3fbbb-ff6d-41ef-b3db-1de9aabce094.cpp:21:5: error: ‘cout’ was not declared in  this scope    21 |     cout << white << endl << black << endl;       |     ^~~~ 5bc3fbbb-ff6d-41ef-b3db-1de9aabce094.cpp:21:22: error: ‘endl’ was not declared i n this scope    21 |     cout << white << endl << black << endl;       |                      ^~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,24 +1,29 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
 int main() {
     string code, guess;
     cin >> code >> guess;
     
-    int white_pegs = 0, black_pegs = 0;
-    map<char, int> code_freq, guess_freq;
+    int black = 0, white = 0;
+    map<char, int> codeFreq, guessFreq;
     
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
-            black_pegs++;
+            ++black;
         } else {
-            code_freq[code[i]]++;
-            guess_freq[guess[i]]++;
+            ++codeFreq[code[i]];
+            ++guessFreq[guess[i]];
         }
     }
     
-    for (auto& it : code_freq) {
-        white_pegs += min(it.second, guess_freq[it.first]);
+    for (auto& pair : codeFreq) {
+        white += min(pair.second, guessFreq[pair.first]);
     }
     
-    cout << white_pegs << "\n" << black_pegs << endl;
+    cout << white << endl << black << endl;
     
     return 0;
 }
