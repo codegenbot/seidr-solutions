f813b092e4ec3a36d58af70e0552e27a53ca9e0c
beep boop <githubbot@vadim.me>
2024-07-12 10:25:41 +0200

    Solution 78 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/petz6wwb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/54535bfa-bdcb-4fea-ae4f-76af24144 7e9.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,8 +1,18 @@
-Problem description: Write a Python function that takes a string as input and returns whether the string is a palindrome or not.
+Problem description: Write a Python function that checks if a given string is a palindrome and another function to make the input string into a palindrome.
 
 function signature:
 def is_palindrome(string: str) -> bool:
+    def make_palindrome(string: str) -> str:
 
 Solution:
 def is_palindrome(string: str) -> bool:
     return string == string[::-1]
+
+def make_palindrome(string: str) -> str:
+    if string.isalnum():
+        for i in range(len(string), -1, -1):
+            if string[:i].lower() == string[:i][::-1].lower():
+                return string + string[:i][::-1]
+        return string + string[::-1]
+    else:
+        return 'Error: Input contains non-alphanumeric characters.'
