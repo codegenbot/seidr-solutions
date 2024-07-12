3187ab011b963c41204dd22a1eec48e09a8a7cc5
beep boop <githubbot@vadim.me>
2024-07-12 14:28:25 +0200

    Solution 16 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/sk6gcwl2 Prompt: in this scope     4 | bool pairs_sum_to_zero(vector<int> l) {       |                        ^~~~~~ 64e6a017-9ec7-4a8a-9e0d-905b20563ba3.cpp:4:24: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 64e6a017-9ec7-4a8a-9e0d-905b20563ba3.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 64e6a017-9ec7-4a8a-9e0d-905b20563ba3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 64e6a017-9ec7-4a8a-9e0d-905b20563ba3.cpp:4:31: error: expected primary-expressio n before ‘int’     4 | bool pairs_sum_to_zero(vector<int> l) {       |                               ^~~ In file included from 64e6a017-9ec7-4a8a-9e0d-905b20563ba3.cpp:15: 64e6a017-9ec7-4a8a-9e0d-905b20563ba3.cpp: In function ‘int main()’: 64e6a017-9ec7-4a8a-9e0d-905b20563ba3.cpp:17:30: error: ‘pairs_sum_to_zero’ canno t be used as a function    17 |     assert (pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);       |             ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,11 +1,11 @@
-#include <vector>
+#include<vector>
 using namespace std;
 
 bool pairs_sum_to_zero(vector<int> l) {
     set<int> s(l.begin(), l.end());
-    for (int i = 0; i < s.size(); i++) {
-        int target = -s[i];
-        if (target != s[i] && find(target, s) != s.end()) {
+    for (auto i = s.begin(); i != s.end(); ++i) {
+        int target = -*i;
+        if (*i != target && count(s.begin(), s.end(), target)) {
             return true;
         }
     }
