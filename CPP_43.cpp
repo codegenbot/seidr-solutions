0418a09dec0b0fff2baf8d9f47b21da42efb140b
beep boop <githubbot@vadim.me>
2024-07-12 14:21:33 +0200

    Solution 9 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/sk6gcwl2 Prompt: 146908a9-5944-425e-be05-89483a2ce8ce.cpp:1:24: error: ‘vector’ was not declared in this scope     1 | bool pairs_sum_to_zero(vector<int> l) {       |                        ^~~~~~ 146908a9-5944-425e-be05-89483a2ce8ce.cpp:1:31: error: expected primary-expressio n before ‘int’     1 | bool pairs_sum_to_zero(vector<int> l) {       |                               ^~~ In file included from 146908a9-5944-425e-be05-89483a2ce8ce.cpp:12: 146908a9-5944-425e-be05-89483a2ce8ce.cpp: In function ‘int main()’: 146908a9-5944-425e-be05-89483a2ce8ce.cpp:14:30: error: ‘pairs_sum_to_zero’ canno t be used as a function    14 |     assert (pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);       |             ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -4,7 +4,7 @@ bool pairs_sum_to_zero(vector<int> l) {
     set<int> s(l.begin(), l.end());
     for (int i = 0; i < s.size(); i++) {
         int complement = -s[i];
-        if (find(s.begin(), s.end(), complement) != s.end() && s[i] != complement) {
+        if (s.find(complement) != s.end() && s[i] != complement) {
             return true;
         }
     }
