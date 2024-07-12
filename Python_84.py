5e8159b638937842d9b7766a4bfaeed5e6da25ff
beep boop <githubbot@vadim.me>
2024-07-12 09:46:25 +0200

    Solution 86 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ts2ifgq4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/686054d7-4f93-46fb-be61-60bbd908d 080.py", line 16, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/686054d7-4f93-46fb-be61-60bbd908d 080.py", line 13, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^ TypeError: solve() takes 0 positional arguments but 1 was given

--- Python_84.py
+++ Python_84.py
@@ -1,13 +1,10 @@
 ```
-def solve(num):
+def solve():
     while True:
-        if num != 963:
-            N = input("Please provide a binary number: ")
-            if all(c in "01" for c in N):
-                print(bin(int(N, 2))[2:])
-                break
-            else:
-                print("Invalid Input. Please provide a binary number.")
-                continue
+        N = input("Please provide a binary number: ")
+        if all(c in "01" for c in N):
+            print(bin(int(N, 2))[2:])
+            break
         else:
-            return bin(18)[2:]
+            print("Invalid Input. Please provide a binary number.")
+            continue
