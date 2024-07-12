bfc34bccaa8f8ac28732d94140f1c775f8ab0f80
beep boop <githubbot@vadim.me>
2024-07-12 13:39:12 +0200

    Solution 42 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tboxn4kg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e0b8ec7b-68aa-4bf1-a7f6-ce1377415 33f.py", line 12, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e0b8ec7b-68aa-4bf1-a7f6-ce1377415 33f.py", line 9, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -2,8 +2,8 @@ def make_palindrome(string: str) -> str:
     if string == string[::-1]:
         return string
     for char in set(string):
-        mid = string.replace(char, '')
-        new_string = char + string[:len(mid)//2 + len(mid)//2] + mid[::-1] + char
+        mid = string.replace(char, "")
+        new_string = char + string[: len(mid) // 2 + len(mid) // 2] + mid[::-1] + char
         if new_string == new_string[::-1]:
             return new_string
     return string + string[::-1]
