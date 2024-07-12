b39a0a41826a2d0197f3cfeb7cf1804d3837a879
beep boop <githubbot@vadim.me>
2024-07-12 22:09:47 +0200

    Solution 69 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/f2q410zk Prompt: __future_base::_State_baseV2>; _Args = {}; __gnu_cxx::_Lock_policy _Lp = __gnu_c xx::_S_atomic]’ /usr/include/c++/11/bits/shared_ptr_base.h:1342:14:   required from ‘std::__shar ed_ptr<_Tp, _Lp>::__shared_ptr(std::_Sp_alloc_shared_tag<_Tp>, _Args&& ...) [wit h _Alloc = std::allocator<std::__future_base::_State_baseV2>; _Args = {}; _Tp = std::__future_base::_State_baseV2; __gnu_cxx::_Lock_policy _Lp = __gnu_cxx::_S_a tomic]’ /usr/include/c++/11/bits/shared_ptr.h:409:59:   required from ‘std::shared_ptr<_ Tp>::shared_ptr(std::_Sp_alloc_shared_tag<_Tp>, _Args&& ...) [with _Alloc = std: :allocator<std::__future_base::_State_baseV2>; _Args = {}; _Tp = std::__future_b ase::_State_baseV2]’ /usr/include/c++/11/bits/shared_ptr.h:862:14:   required from ‘std::shared_ptr<_ Tp> std::allocate_shared(const _Alloc&, _Args&& ...) [with _Tp = std::__future_b ase::_State_baseV2; _Alloc = std::allocator<std::__future_base::_State_baseV2>; _Args = {}]’ /usr/include/c++/11/bits/shared_ptr.h:878:39:   required from ‘std::shared_ptr<_ Tp> std::make_shared(_Args&& ...) [with _Tp = std::__future_base::_State_baseV2;  _Args = {}]’ /usr/include/c++/11/future:1296:43:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::_Sp_counted_ptr_inplace<std::__future_base::_State_baseV2, std::allo cator<std::__future_base::_State_baseV2>, __gnu_cxx::_S_atomic> >’ has no member  named ‘_M_max_size’

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,7 +1,6 @@
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <iostream>
-#include <initializer_list>
 
 int findNextSmallest(std::vector<int> lst) {
     if (lst.empty()) return -1; 
