fadd54fba2ca30fe499415ad3b353eb5f2947a52
beep boop <githubbot@vadim.me>
2024-07-12 15:39:06 +0200

    Solution 86 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3pjnfwz2 Prompt: #j#Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3062b0a2-3c47-43cd-97cf-4cfa9c8c3 c44.py", line 21, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3062b0a2-3c47-43cd-97cf-4cfa9c8c3 c44.py", line 18, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3062b0a2-3c47-43cd-97cf-4cfa9c8c3 c44.py", line 15, in make_palindrome     print(new_string[i], end="")           ~~~~~~~~~~^^^ IndexError: string index out of range

--- Python_10.py
+++ Python_10.py
@@ -1,6 +1,20 @@
-print("Anagrams:")
-if result:
-    for i, word_list in enumerate(result):
-        print(f"Group {i+1}: {' -> '.join(word_list)}")
-else:
-    print("No anagrams found.")
+def is_palindrome(s: str) -> bool:
+    return s == s[::-1]
+
+def make_palindrome(string: str) -> str:
+    if is_palindrome(string):
+        return string
+    n = len(string)
+    new_string = "#" + "#".join(list(string)) + "#"
+    i_max = n - 1
+    while not is_palindrome(new_string):
+        new_string = new_string[:i_max+1] + "#"
+        i_max -= 1
+    result = ""
+    for i in range(len(new_string)):
+        if (i < len(string)) or ((len(string) * 2 - 1) > i >= len(string) - 1 and i % 2 != 0):
+            if new_string[i] == "#":
+                result += "r"
+            else:
+                result += new_string[i]
+    return result
