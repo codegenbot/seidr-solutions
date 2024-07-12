50e644f9054325775d991d1cf2175a6bf1947be7
beep boop <githubbot@vadim.me>
2024-07-12 04:33:24 +0200

    Solution 46 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i4swyuyp Prompt:       |                                     ^~~~~~ 5a6c5d3b-74bb-4d85-b5c9-bf679399ebdf.cpp:28:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 5a6c5d3b-74bb-4d85-b5c9-bf679399ebdf.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 5a6c5d3b-74bb-4d85-b5c9-bf679399ebdf.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 5a6c5d3b-74bb-4d85-b5c9-bf679399ebdf.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5a6c5d3b-74bb-4d85-b5c9-bf679399ebdf.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 5a6c5d3b-74bb-4d85-b5c9-bf679399ebdf.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -3,8 +3,6 @@
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
 std::vector<std::string> reverse_delete(const std::string& s, const std::string& c) {
     std::string result = "";
     for (char ch : s) {
