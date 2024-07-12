7806297ee1b5081eaf7505162270b12c9d4736e4
beep boop <githubbot@vadim.me>
2024-07-12 12:02:02 +0200

    Solution 78 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/8x79oae2 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 4dd3313d-3aea-47aa-9749-c0611648d1a7.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,16 +1,17 @@
+```
 #include <vector>
 #include <iostream>
 #include <string>
 
-# define c++11
-
 using namespace std;
 
-string kebabToCamel(string str) {
-    string result = "";
+# define c++11
+
+size_t kebabToCamel(string str) {
+    size_t result = "";
     for (char c : str) {
         if (c == '-') {
-            result += toupper(str.substr(str.find(c)+1, 1));
+            result += toupper(str[str.find(c) + 1]);
             str.erase(str.find(c), 1);
         } else if (c == ' ') {
             result += " ";
