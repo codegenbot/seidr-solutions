34e0ab376291f3b88e59fbc804694b66ac7dce9e
beep boop <githubbot@vadim.me>
2024-07-12 14:26:31 +0200

    Solution 38 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/us1y8bfy Prompt: f ‘std’     2 | int pairedDigits(std::string s) {       |                       ^~~~~~ 5a4b4b88-5bfa-4eea-a7f3-c266672702b5.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | char input[] = "1233345556"; 5a4b4b88-5bfa-4eea-a7f3-c266672702b5.cpp: In function ‘int main()’: 5a4b4b88-5bfa-4eea-a7f3-c266672702b5.cpp:13:10: error: ‘string’ is not a member of ‘std’    13 |     std::string str(input);       |          ^~~~~~ 5a4b4b88-5bfa-4eea-a7f3-c266672702b5.cpp:13:10: note: ‘std::string’ is defined i n header ‘<string>’; did you forget to ‘#include <string>’? 5a4b4b88-5bfa-4eea-a7f3-c266672702b5.cpp:14:28: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    14 |     int sum = pairedDigits(str);       |                            ^~~       |                            std 5a4b4b88-5bfa-4eea-a7f3-c266672702b5.cpp:14:31: error: ‘pairedDigits’ cannot be used as a function    14 |     int sum = pairedDigits(str);       |                               ^

--- paired-digits.cpp
+++ paired-digits.cpp
@@ -1,20 +1,17 @@
-```cpp
 #include <string>
 
 int pairedDigits(std::string s) {
     int sum = 0;
-    for (int i = 0; i < s.length(); i++) {
-        for (int j = i + 1; j <= s.length(); j++) {
-            if (s[i] == s[j % s.length()]) {
-                sum += s[i] - '0';
-            }
+    for (int i = 0; i < s.length() - 1; i++) {
+        if (s[i] == s[i + 1]) {
+            sum += s[i] - '0';
         }
     }
     return sum;
 }
 
 int main() {
-    char input[] = "1233345556";
+    const char* input = "1233345556";
     std::string str(input);
     int sum = pairedDigits(str);
     return 0;
