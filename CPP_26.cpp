7d6c4d5375f043b5b9814285c1f92801e7e1ae57
beep boop <githubbot@vadim.me>
2024-07-12 18:54:47 +0200

    Solution 16 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/glxnmwbk Prompt:  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 030afeab-eb38-405d-80e1-1adef05a1fe3.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 030afeab-eb38-405d-80e1-1adef05a1fe3.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 030afeab-eb38-405d-80e1-1adef05a1fe3.cpp:9: 030afeab-eb38-405d-80e1-1adef05a1fe3.cpp: In function ‘int main()’: 030afeab-eb38-405d-80e1-1adef05a1fe3.cpp:19:20: error: ‘remove_duplicates’ was n ot declared in this scope    19 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 030afeab-eb38-405d-80e1-1adef05a1fe3.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,13 +1,9 @@
-bool areSameVectors(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) return false;
+if(a.size() != b.size()) return false;
     for(int i = 0; i < a.size(); i++) {
         if(a[i] != b[i]) return false;
     }
     return true;
-}
 
-int main() {
-    std::vector<int> test1 = remove_duplicates({1, 2, 3, 2, 4, 3, 5});
-    assert (areSameVectors(test1, {1, 4, 5}));
-    return 0;
+std::vector<int> remove_duplicates(const std::vector<int>& vec) {
+    return std::vector<int>(vec.begin(), unique(vec.begin(), vec.end()));
 }
