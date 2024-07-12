20af792ee492a7aeeaf89c9f6420861bea09cc1f
beep boop <githubbot@vadim.me>
2024-07-12 19:43:44 +0200

    Solution 46 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gwb2dchn Prompt: Program did not receive expected input

--- Python_144.py
+++ Python_144.py
@@ -4,10 +4,6 @@ from fractions import Fraction
 def simplify():
     x = float(input("Enter numerator: "))
     y = float(input("Enter denominator: "))
-
-    if y == 0:
-        print("Invalid input! Denominator cannot be zero.")
-    else:
-        return Fraction(x, y) == 1
+    return y != 0 and Fraction(x,y) == 1
 
 print(simplify())
