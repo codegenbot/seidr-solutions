86852195b56fa4fb3d628eac1faee102ad37e2d1
beep boop <githubbot@vadim.me>
2024-07-12 07:03:01 +0200

    Solution 16 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/d8swkc5z Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/547465ac-3c72-472c-94f2-ea421452f 1b4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -2,6 +2,6 @@ from math import gcd
 
 def modp(n: int, p: int):
     if gcd(p, n) == 1:
-        return pow(n, (p-1)//gcd(p,n), p) * n % p
+        return pow(n, p - 1, p) * n % p
     else:
         return n
