20f2be7087ac43b00482564c99b8cb813e91ef91
beep boop <githubbot@vadim.me>
2024-07-12 15:37:03 +0200

    Solution 12 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uakx7mir Prompt:       |                    ^~~ 16da8044-9026-403f-8cd5-9de1bb6b3f41.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 16da8044-9026-403f-8cd5-9de1bb6b3f41.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 16da8044-9026-403f-8cd5-9de1bb6b3f41.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 16da8044-9026-403f-8cd5-9de1bb6b3f41.cpp:9: 16da8044-9026-403f-8cd5-9de1bb6b3f41.cpp: In function ‘int main()’: 16da8044-9026-403f-8cd5-9de1bb6b3f41.cpp:19:20: error: ‘generate_integers’ was n ot declared in this scope    19 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ 16da8044-9026-403f-8cd5-9de1bb6b3f41.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <cassert>
 
-std::vector<int> generate_integers(int a, int b){
+std::vector<int> generate_integers(int a, int b) {
     std::vector<int> result;
     for(int i = a; i <= b; i++){
         if(i % 2 == 0){
@@ -11,6 +11,14 @@ std::vector<int> generate_integers(int a, int b){
     return result;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if(a.size() != b.size()) {
+        return false;
+    }
+    for(size_t i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
