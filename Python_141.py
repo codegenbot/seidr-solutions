3240ee65bdf83cc070bf248b9e0b94099d5605ac
beep boop <githubbot@vadim.me>
2024-07-12 18:45:22 +0200

    Solution 30 of Python/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xl7a449t Prompt: Complete the following code given the task description and function signature.

--- Python_141.py
+++ Python_141.py
@@ -5,9 +5,9 @@ def file_name_check(file_name):
     if file_name.count('.') != 1:
         return 'No'
     before_dot = file_name.split('.')[0]
-    after_dot = file_name.split('.')[1].lower()
+    after_dot = file_name.split('.')[1]
     if not before_dot or not before_dot[0].isalpha():
         return 'No'
-    if after_dot not in ['txt', 'exe', 'dll']:
+    if not set(['txt', 'exe', 'dll']).issuperset({after_dot.lower()}):
         return 'No'
     return 'Yes'
