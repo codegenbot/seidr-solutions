d47d4de58f233b6ac1bea5b04ff28e0b840f5090
beep boop <githubbot@vadim.me>
2024-07-12 19:24:00 +0200

    Solution 55 of Python/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xl7a449t Prompt: Complete the following code given the task description and function signature.

--- Python_141.py
+++ Python_141.py
@@ -7,7 +7,7 @@ def file_name_check(file_name):
     parts = file_name.split('.')
     if not parts[0][0].isalpha():
         return 'No'
-    allowed_extensions = ['txt', 'exe', 'dll']
-    if parts[-1] not in allowed_extensions:
+    extensions = ['txt', 'exe', 'dll']
+    if parts[-1] not in extensions:
         return 'No'
     return 'Yes'
