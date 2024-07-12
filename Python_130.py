9ecbe7c609b8a14cdf8db7c6aff67f965391222b
beep boop <githubbot@vadim.me>
2024-07-12 12:54:28 +0200

    Solution 60 of Python/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/uv3d2j2s Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/462f6ebc-8052-457e-ab0f-834b8ed58 998.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_130.py
+++ Python_130.py
@@ -1,4 +1,3 @@
-```
 def tri(n):
     if n == 0:
         return [3]
@@ -9,7 +8,7 @@ def tri(n):
         if len(tribonacci_sequence) % 2 == 1:
             next_number = tribonacci_sequence[-1] + tribonacci_sequence[-2] + (tribonacci_sequence[-3] if len(tribonacci_sequence) >= 3 else 0)
         else:
-            next_number = 1 + len(tribonacci_sequence) // 2
+            next_number = 1 + float(len(tribonacci_sequence)) / 2
         
         tribonacci_sequence.append(next_number)
     
