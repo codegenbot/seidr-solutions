0ab75d6936161b432206e3b97e129cf72db9896b
beep boop <githubbot@vadim.me>
2024-07-12 19:36:47 +0200

    Solution 52 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/6r1nddgt Prompt: Program did not receive expected input

--- Python_8.py
+++ Python_8.py
@@ -1,15 +1,12 @@
 import sys
 from typing import List, Tuple
 
-def get_user_input() -> List[int]:
-    return list(map(int, sys.stdin.readline().rstrip().split()))
-
 def sum_product(numbers: List[int]) -> Tuple[int, int]:
     return (
         sum(numbers),
         0 if 0 in numbers else eval("*".join(map(str, numbers))),
     )
 
-numbers = get_user_input()
+numbers = list(map(int, sys.argv[1:]))
 result = sum_product(numbers)
 print(result)
