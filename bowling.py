46bd118ced80c8183609d410b6586b8e39b71c80
beep boop <githubbot@vadim.me>
2024-07-12 02:55:29 +0200

    Solution 1 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/jbgummqd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9dff5d9e-533d-4f44-9541-4e9a7a78f 36d.py", line 59, in <module>     print(bowling_score(bowls))           ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9dff5d9e-533d-4f44-9541-4e9a7a78f 36d.py", line 8, in bowling_score     score += get_strike_bonus(bowls, bowl_index)              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9dff5d9e-533d-4f44-9541-4e9a7a78f 36d.py", line 30, in get_strike_bonus     bonus += get_frame_score(bowls, index + 2)              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9dff5d9e-533d-4f44-9541-4e9a7a78f 36d.py", line 49, in get_frame_score     if bowls[index + 1] == "-":        ~~~~~^^^^^^^^^^^ IndexError: string index out of range

--- bowling.py
+++ bowling.py
@@ -2,7 +2,7 @@ def bowling_score(bowls):
     score = 0
     frame = 1
     bowl_index = 0
-    while frame <= 10:
+    while frame <= 10 and bowl_index < len(bowls):
         if bowls[bowl_index] == "X":
             score += 10
             score += get_strike_bonus(bowls, bowl_index)
@@ -16,44 +16,3 @@ def bowling_score(bowls):
             bowl_index += 2
         frame += 1
     return score
-
-
-def get_strike_bonus(bowls, index):
-    bonus = 0
-    if bowls[index + 2] == "X":
-        bonus += 10
-        if bowls[index + 4] == "X":
-            bonus += 10
-        else:
-            bonus += int(bowls[index + 4])
-    else:
-        bonus += get_frame_score(bowls, index + 2)
-    return bonus
-
-
-def get_spare_bonus(bowls, index):
-    bonus = 0
-    if bowls[index + 2] == "X":
-        bonus += 10
-    else:
-        bonus += int(bowls[index + 2])
-    return bonus
-
-
-def get_frame_score(bowls, index):
-    score = 0
-    if bowls[index] == "-":
-        score += 0
-    else:
-        score += int(bowls[index])
-    if bowls[index + 1] == "-":
-        score += 0
-    elif bowls[index + 1] == "/":
-        score += 10 - int(bowls[index])
-    else:
-        score += int(bowls[index + 1])
-    return score
-
-
-bowls = input()
-print(bowling_score(bowls))
