70dc9596c95cd93458043c07078cbf9b290ff069
beep boop <githubbot@vadim.me>
2024-07-12 15:56:51 +0200

    Solution 10 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qerfd1ru Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/861edb31-ebe9-4418-9b61-cf10cd82e 0b5.py", line 16, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/861edb31-ebe9-4418-9b61-cf10cd82e 0b5.py", line 11, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/861edb31-ebe9-4418-9b61-cf10cd82e 0b5.py", line 2, in sort_even     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/861edb31-ebe9-4418-9b61-cf10cd82e 0b5.py", line 6, in <listcomp>     else sorted([y for y in l if (i - 1) // 2 == (l.index(y)) // 2])[0]          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^^^ IndexError: list index out of range

--- Python_37.py
+++ Python_37.py
@@ -1,2 +1,6 @@
 def sort_even(l: list):
-    return [i if i % 2 != 0 else j for (i, j) in zip(l, sorted([x for x in l[1::2]]))]
+    return [
+        x if i % 2 != 0
+        else sorted([y for j, y in enumerate(l) if (i - 1) // 2 == j])[0]
+        for i, x in enumerate(l)
+    ]
