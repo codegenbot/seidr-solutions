7d2052dfe703a322cbe6eb6dec8b7b08703ecaef
beep boop <githubbot@vadim.me>
2024-07-12 12:06:38 +0200

    Solution 68 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/xj0imabs Prompt: 87dfee7a-8833-4e51-9893-355db0b7af39.cpp: In function ‘double vectorDistance(int )’: 87dfee7a-8833-4e51-9893-355db0b7af39.cpp:5:9: error: ‘cin’ was not declared in t his scope     5 |         cin >> val1 >> val2;       |         ^~~ 87dfee7a-8833-4e51-9893-355db0b7af39.cpp:6:16: error: ‘abs’ was not declared in this scope     6 |         sum += abs(val2 - val1);       |                ^~~ 87dfee7a-8833-4e51-9893-355db0b7af39.cpp:8:12: error: ‘sqrt’ was not declared in  this scope     8 |     return sqrt(sum);       |            ^~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,24 +1,12 @@
-#include <vector>
 #include <iostream>
-#include <cmath> 
-#include <iomanip>
+#include <cmath>
 
-using namespace std;
-
-double vectorDistance() {
-    int n;
-    cin >> n;
-
+double vectorDistance(int n) {
     double sum = 0;
     for (int i = 0; i < n; i++) {
         double val1, val2;
-        cin >> val1 >> val2;
-        sum += pow(val2 - val1, 2);
+        std::cin >> val1 >> val2;
+        sum += std::abs(val2 - val1);
     }
-    return sqrt(sum);
-}
-
-int main() {
-    cout << fixed << setprecision(10) << vectorDistance() << endl;
-    return 0;
+    return std::sqrt(sum);
 }
