364e0079ae300d951501b36f94cdeb447261c5d5
beep boop <githubbot@vadim.me>
2024-07-12 08:02:33 +0200

    Solution 7 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2chutce6 Prompt: Program did not receive expected input

--- Python_88.py
+++ Python_88.py
@@ -1,3 +1,4 @@
+array = [int(i) for i in input().split()]
 def sort_array(array):
     if len(array) == 0:
         return array
@@ -5,4 +6,6 @@ def sort_array(array):
     if total % 2 != 0:
         return sorted(array)
     else:
-        return sorted(array)[::-1]
+        return sorted(array, reverse=True)
+
+print(sort_array(array))
