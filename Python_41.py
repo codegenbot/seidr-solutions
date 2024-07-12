228d61e2ba52bc74cafabf791d8a6aeb109ca977
beep boop <githubbot@vadim.me>
2024-07-12 07:15:38 +0200

    Solution 61 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zb2z36u0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5321f66f-02f8-4b97-91f0-93ce9bd58 e93.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_41.py
+++ Python_41.py
@@ -1,10 +1,10 @@
 ```
 car_race_collision = int(input("Enter car race collision: "))
-def check(car_race_collision):
+def check():
     if car_race_collision > 5:
         print("Collision is severe")
     elif car_race_collision == 5:
         print("Collision is moderate")
     else:
         print("No collision")
-check(car_race_collision)
+check()
