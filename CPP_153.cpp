987d745983a9abe0a1891069a8ac5363eb9a9d7d
beep boop <githubbot@vadim.me>
2024-07-12 23:45:46 +0200

    Solution 10 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hpkkxb14 Prompt: In file included from /usr/include/c++/11/string:39,                  from d79a9abd-ac60-4c43-928a-3c75e8e6076f.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from d79a9abd-ac60-4c43-928a-3c75e8e6076f.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ d79a9abd-ac60-4c43-928a-3c75e8e6076f.cpp:4:59: error: ‘extensions’ was not decla red in this scope     4 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                                           ^~~~~~~~~~ d79a9abd-ac60-4c43-928a-3c75e8e6076f.cpp:4:69: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                                                     ^ In file included from d79a9abd-ac60-4c43-928a-3c75e8e6076f.cpp:24: d79a9abd-ac60-4c43-928a-3c75e8e6076f.cpp: In function ‘int main()’: d79a9abd-ac60-4c43-928a-3c75e8e6076f.cpp:26:32: error: ‘Strongest_Extension’ can not be used as a function    26 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 #include <string>
 
-int Strongest_Extension(vector<string> extensions){
+int Strongest_Extension(string class_name, vector<string> extensions){
     int max_strength = 0;
     string strongest_extension;
 
-    for(auto extension : extensions){
+    for(auto extension : extensions){ 
         int cap = 0, sm = 0;
         for(char c : extension){
             if(isupper(c)) cap++;
@@ -18,5 +18,5 @@ int Strongest_Extension(vector<string> extensions){
         }
     }
 
-    return strongest_extension;
+    return strongest_extension.size();
 }
