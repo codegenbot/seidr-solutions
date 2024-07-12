c2ebb0ff18c92320b7843085e65d4a564212031f
beep boop <githubbot@vadim.me>
2024-07-12 05:59:10 +0200

    Solution 22 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7ealauoz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/85eed9e8-1cc7-4224-9908-7d5ac296e 54c.py", line 1     ````     ^ SyntaxError: invalid syntax

--- Python_29.py
+++ Python_29.py
@@ -1,4 +1,5 @@
 def filter_by_prefix(user_input=None):
+    """
     strings = input('Enter a list of strings (separated by commas): ') if user_input is None else user_input
     prefix = input('Enter a prefix to filter: ')
     strings_list = [s.strip() for s in strings.split(',')]
