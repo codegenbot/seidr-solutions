19b87984b2a873fdbe3bb25ace51576f38a4dcb7
beep boop <githubbot@vadim.me>
2024-07-12 14:54:39 +0200

    Solution 14 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/59i5y8lf Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ 28a26037-28c3-4c39-9fc8-2735ea5fa3d7.cpp:47:25:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,11 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool issame(vector<string> a, vector<string>b){
+    if(a.size() != b.size())
+        return false;
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i])
+            return false;
+    }
+    return true;
+}
+
+vector<string> numerical_letter_grade(vector<float> grades){
+    vector<string> result;
+    for(float g : grades) {
+        string letterGrade;
+        if(g >= 4.0) {
+            letterGrade = "A+";
+        } else if(g > 3.7) {
+            letterGrade = "A";
+        } else if(g > 3.3) {
+            letterGrade = "A-";
+        } else if(g > 3.0) {
+            letterGrade = "B+";
+        } else if(g > 2.7) {
+            letterGrade = "B";
+        } else if(g > 2.3) {
+            letterGrade = "B-";
+        } else if(g > 2.0) {
+            letterGrade = "C+";
+        } else if(g > 1.7) {
+            letterGrade = "C";
+        } else if(g > 1.3) {
+            letterGrade = "C-";
+        } else if(g > 1.0) {
+            letterGrade = "D+";
+        } else if(g > 0.7) {
+            letterGrade = "D";
+        } else if(g > 0.0) {
+            letterGrade = "D-";
+        } else {
+            letterGrade = "E";
+        }
+        result.push_back(letterGrade);
+    }
+    return result;
+}
+
 int main() {
-    vector<float> grades = {3.4, 4.1, 2.9};
-    vector<string> grades_letter = numerical_letter_grade(grades);
-
-    if (issame(grades_letter, numerical_letter_grade(grades)))
+    vector<float> grades = {3.8, 4.2, 1.9};
+    vector<string> gradeLetters = numerical_letter_grade(grades);
+    
+    if (issame({"A+", "A+", "D-"}, gradeLetters)) {
         cout << "Grades are same." << endl;
-    else
-        cout << "Grades are not the same." << endl;
-
+    } else {
+        cout << "Grades are different." << endl;
+    }
+    
     return 0;
 }
