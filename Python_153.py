ad178acdc574d6ffd54ec0bce1b57d3c12302680
beep boop <githubbot@vadim.me>
2024-07-12 11:36:05 +0200

    Solution 2 of Python/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/o0yfnjyr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/58555c2a-fee2-4871-8427-7665e3971 7e2.py", line 13, in <module>     check(Strongest_Extension)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/58555c2a-fee2-4871-8427-7665e3971 7e2.py", line 10, in check     assert Strongest_Extension("Sp", ["671235", "Bb"]) == "Sp.671235"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_153.py
+++ Python_153.py
@@ -2,6 +2,5 @@ def Strongest_Extension(class_name, extensions):
     def strength(extension):
         return sum(1 for char in extension if char.isupper()) - sum(1 for char in extension if char.islower())
 
-    filtered_extensions = [ext for ext in extensions if any(char.isalpha() for char in ext)]
-    strongest_extension = max(filtered_extensions, key=lambda x: (strength(x), extensions.index(x)))
+    strongest_extension = max(extensions, key=lambda x: (strength(x), extensions.index(x)))
     return f"{class_name}.{strongest_extension}"
