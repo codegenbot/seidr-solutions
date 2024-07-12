6d9485ee88ee596c83f89851e8c2987b11e3ff21
beep boop <githubbot@vadim.me>
2024-07-12 18:21:25 +0200

    Solution 84 of CPP/92, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zbwm2dvn Prompt: In file included from 2d14f549-ec90-41b8-8c02-e761c5945791.cpp:26: 2d14f549-ec90-41b8-8c02-e761c5945791.cpp: In function ‘int main()’: 2d14f549-ec90-41b8-8c02-e761c5945791.cpp:28:13: error: ‘any_int’ was not declare d in this scope; did you mean ‘u_int’?    28 |     assert (any_int(3.01,4,7)==false);       |             ^~~~~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-bool is_int(float a, float b, float c) {
+bool any_int(float a, float b, float c) {
     int ai = (int)a;
     int bi = (int)b;
     int ci = (int)c;
@@ -15,7 +15,7 @@ int main_function() {
     float a, b, c;
     cout << "Enter three numbers: ";
     cin >> a >> b >> c;
-    if (is_int(a, b, c)) 
+    if (any_int(a, b, c)) 
         cout << "At least one of the input numbers is an integer.\n";
     else
         cout << "None of the input numbers are integers.\n";
