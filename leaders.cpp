fcc6d90d5837d6227bea54cf562dbb09111edccc
beep boop <githubbot@vadim.me>
2024-07-13 00:50:17 +0200

    Solution 76 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/6zi3sqj2 Prompt:     4 |     while (cin >> num) {       |            ^~~ 7c03297c-b30d-4557-866d-a32f7aa1cc3a.cpp:5:9: error: ‘nums’ was not declared in this scope; did you mean ‘num’?     5 |         nums.push_back(num);       |         ^~~~       |         num 7c03297c-b30d-4557-866d-a32f7aa1cc3a.cpp:7:5: error: ‘cin’ was not declared in t his scope     7 |     cin.clear();       |     ^~~ 7c03297c-b30d-4557-866d-a32f7aa1cc3a.cpp:10:12: error: expected primary-expressi on before ‘int’    10 |     vector<int> result = findLeaders(nums);       |            ^~~ 7c03297c-b30d-4557-866d-a32f7aa1cc3a.cpp:11:18: error: ‘result’ was not declared  in this scope    11 |     for (int i = result.size() - 1; i >= 0; i--) {       |                  ^~~~~~ 7c03297c-b30d-4557-866d-a32f7aa1cc3a.cpp:12:9: error: ‘cout’ was not declared in  this scope    12 |         cout << result[i] << " ";       |         ^~~~

--- leaders.cpp
+++ leaders.cpp
@@ -4,29 +4,31 @@ using namespace std;
 
 vector<int> findLeaders(const vector<int>& nums) {
     vector<int> leaders;
-    int max_right = 0;
-
+    int maxRight = INT_MIN;
+    
     for (int i = nums.size() - 1; i >= 0; i--) {
-        if (nums[i] >= max_right) {
+        if (nums[i] >= maxRight) {
             leaders.push_back(nums[i]);
-            max_right = nums[i];
+            maxRight = nums[i];
         }
     }
-
+    
     return leaders;
 }
 
 int main() {
     vector<int> nums;
     int num;
+    
     while (cin >> num) {
         nums.push_back(num);
     }
-
+    
     vector<int> result = findLeaders(nums);
+    
     for (int i = result.size() - 1; i >= 0; i--) {
         cout << result[i] << " ";
     }
-
+    
     return 0;
 }
