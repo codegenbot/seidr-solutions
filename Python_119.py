ed9c3362f5fd935322494a29bad7f93e6c2a63f0
beep boop <githubbot@vadim.me>
2024-07-12 03:44:16 +0200

    Solution 13 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/e30yzf7t Prompt: Complete the following code given the task description and function signature.

--- Python_119.py
+++ Python_119.py
@@ -1,3 +1,13 @@
 ```
 def match_parens(lst):
-    return 'Yes' if sum(1 for s in lst if s.count('(') != s.count(')')) else 'No'
+    open_parentheses = 0
+    close_parentheses = 0
+    for s in lst:
+        if s[0] == '(':
+            open_parentheses += 1
+        elif s[0] == ')':
+            close_parentheses += 1
+    if open_parentheses == close_parentheses:
+        return 'Yes'
+    else:
+        return 'No'
