479d541bd0310d3dbc885dff16524a7d7e5055b0
beep boop <githubbot@vadim.me>
2024-07-12 15:08:19 +0200

    Solution 52 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zj95medv Prompt: )’    33 | int main(){       |     ^~~~ 34a21db9-3102-46b2-b7ea-0a968fb59004.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 34a21db9-3102-46b2-b7ea-0a968fb59004.cpp:24: 34a21db9-3102-46b2-b7ea-0a968fb59004.cpp: In function ‘int main()’: 34a21db9-3102-46b2-b7ea-0a968fb59004.cpp:34:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    34 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~ 34a21db9-3102-46b2-b7ea-0a968fb59004.cpp:7:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     7 | bool issame(const vector<int>& v1, const vector<int>& v2) {       |      ^~~~~~ 34a21db9-3102-46b2-b7ea-0a968fb59004.cpp:25:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -17,6 +17,6 @@ vector<int> incr_list(const vector<int>& l){
 }
 
 int main() {
-    assert(issame({6, 3, 6,3, 4, 4, 10, 1, 124}, {5, 2, 5, 2, 3, 3, 9, 0, 123}));
+    assert(issame({7, 3, 7,3, 5, 5, 11, 1, 125}, incr_list({6, 2, 6, 2, 4, 4, 10, 0, 124})));
     return 0;
 }
