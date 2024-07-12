94262602456a23713b2e2d03f430c5592db54d35
beep boop <githubbot@vadim.me>
2024-07-12 23:48:55 +0200

    Solution 33 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jkmyomz8 Prompt: 284e3ae5-7a29-404d-a6c6-cfd35c61d16f.cpp:6:1: error: expected unqualified-id bef ore ‘if’     6 | if (a.type() == typeid(string) && b.type() == typeid(string)) {       | ^~ In file included from 284e3ae5-7a29-404d-a6c6-cfd35c61d16f.cpp:12: 284e3ae5-7a29-404d-a6c6-cfd35c61d16f.cpp: In function ‘int main()’: 284e3ae5-7a29-404d-a6c6-cfd35c61d16f.cpp:14:13: error: ‘boost’ has not been decl ared    14 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 284e3ae5-7a29-404d-a6c6-cfd35c61d16f.cpp:14:35: error: expected primary-expressi on before ‘>’ token    14 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 284e3ae5-7a29-404d-a6c6-cfd35c61d16f.cpp:14:37: error: ‘compare_one’ was not dec lared in this scope    14 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,12 +1,25 @@
-```cpp
-#include <boost/any.hpp>
+#include <any>
 #include <string>
+#include <boost/any.hpp>
 
-std::any a;
-std::any b;
+using namespace boost;
+using namespace std;
 
-if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-    std::string x = std::any_cast<std::string>(a);
-    std::string y = std::any_cast<std::string>(b);
-    return (x < y) ? a : ((y < x) ? b : a);
+int compare_one(const string& s, int i) {
+    if (i == 1)
+        return stof(s) - 10;
+    else if (i == 2)
+        return stof(s);
+    else if (i == 3)
+        return stof(s) + 10;
+}
+
+int main() {
+    if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string x = any_cast<string>(a);
+        string y = any_cast<string>(b);
+        return (x < y) ? 1 : ((y < x) ? -1 : 0);
+    }
+    
+    assert (compare_one("1", 1) == 0);
 }
