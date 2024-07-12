bd9e548e8f8ec2635c1d753f43220b1e0f15d817
beep boop <githubbot@vadim.me>
2024-07-12 18:46:16 +0200

    Solution 22 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0hrfoi8h Prompt: s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 641701a1-d599-4163-a7e2-fd4b21645232.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 641701a1-d599-4163-a7e2-fd4b21645232.cpp:1:8: note: ‘int main()’ previously defi ned here     1 | ```int main() {       |        ^~~~ In file included from 641701a1-d599-4163-a7e2-fd4b21645232.cpp:19: 641701a1-d599-4163-a7e2-fd4b21645232.cpp: In function ‘int main()’: 641701a1-d599-4163-a7e2-fd4b21645232.cpp:29:20: error: ‘rolling_max’ was not dec lared in this scope    29 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 641701a1-d599-4163-a7e2-fd4b21645232.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> rolling_max(std::vector<int> numbers) {
+std::vector<int> rolling_max(const std::vector<int>& numbers) {
     std::vector<int> maxNumbers;
     int currentMax = INT_MIN;
 
@@ -15,22 +15,16 @@ std::vector<int> rolling_max(std::vector<int> numbers) {
     return maxNumbers;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    // your logic here to check if two vectors are same
-    // for now, it's assumed that they are the same
-    return true; 
-}
-
 int main() {
-    std::vector<int> numbers;
+    vector<int> numbers;
     int num;
-
+    
     cout << "Enter numbers (enter -1 to stop): ";
     while ((cin >> num) && (num != -1)) {
         numbers.push_back(num);
     }
-
-    std::vector<int> maxNumbers = rolling_max(numbers);
+    
+    vector<int> maxNumbers = rolling_max(numbers);
 
     for (int i = 0; i < maxNumbers.size(); i++) {
         cout << "Maximum up to " << i + 1 << " is " << maxNumbers[i] << endl;
