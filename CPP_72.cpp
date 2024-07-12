6a1cd8002bb47395a2fe54b5c59e6cb07071ee5e
beep boop <githubbot@vadim.me>
2024-07-12 16:50:01 +0200

    Solution 69 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eui281em Prompt: d0300f59-d7c0-4123-a165-88e6f3dc24b3.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ d0300f59-d7c0-4123-a165-88e6f3dc24b3.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from d0300f59-d7c0-4123-a165-88e6f3dc24b3.cpp:32: d0300f59-d7c0-4123-a165-88e6f3dc24b3.cpp: In function ‘int main()’: d0300f59-d7c0-4123-a165-88e6f3dc24b3.cpp:34:24: error: converting to ‘std::vecto r<std::vector<int> >’ from initializer list would use explicit constructor ‘std: :vector<_Tp, _Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocato r_type&) [with _Tp = std::vector<int>; _Alloc = std::allocator<std::vector<int> >; std::vector<_Tp, _Alloc>::size_type = long unsigned int; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<std::vector<int> >]’    34 |     assert (will_it_fly({5}, 5) == true);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -10,4 +10,10 @@ bool will_it_fly(vector<vector<int>> q, int w) {
     }
 }
 
-bool test = will_it_fly({{0,5},{7,10}}, 9);
+bool will_it_fly(vector<vector<int>> q, int w) {
+    if(w >= (q[0][1] - q[0][0]) && w >= (q[1][1] - q[1][0])) {
+        return true;
+    } else {
+        return false;
+    }
+}
