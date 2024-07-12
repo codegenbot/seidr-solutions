539c32b560b40c2edb1c288cc10d6b19e0340905
beep boop <githubbot@vadim.me>
2024-07-12 11:08:51 +0200

    Solution 69 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/1ektllgn Prompt: ined here    20 | int main() {       |     ^~~~ In file included from eb0f6934-740b-4f6b-b931-18f285827755.cpp:28: eb0f6934-740b-4f6b-b931-18f285827755.cpp: In function ‘int main()’: eb0f6934-740b-4f6b-b931-18f285827755.cpp:38:41: error: cannot bind non-const lva lue reference of type ‘std::vector<float>&’ to an rvalue of type ‘std::vector<fl oat>’    38 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from eb0f6934-740b-4f6b-b931-18f285827755.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<float>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ eb0f6934-740b-4f6b-b931-18f285827755.cpp:7:57: note:   initializing argument 1 o f ‘std::pair<float, float> find_closest_elements(std::vector<float>&)’     7 | pair<float, float> find_closest_elements(vector<float>& numbers) {       |                                          ~~~~~~~~~~~~~~~^~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -5,13 +5,14 @@
 using namespace std;
 
 pair<float, float> find_closest_elements(const vector<float>& numbers) {
-    sort(numbers.begin(), numbers.end());
-    float min_diff = numbers[1] - numbers[0];
-    pair<float, float> result = {numbers[0], numbers[1]};
-    for (int i = 1; i < numbers.size() - 1; ++i) {
-        if (numbers[i + 1] - numbers[i] < min_diff) {
-            min_diff = numbers[i + 1] - numbers[i];
-            result = {numbers[i], numbers[i + 1]};
+    vector<float> sortedNumbers = numbers;
+    sort(sortedNumbers.begin(), sortedNumbers.end());
+    float min_diff = sortedNumbers[1] - sortedNumbers[0];
+    pair<float, float> result = {sortedNumbers[0], sortedNumbers[1]};
+    for (int i = 1; i < sortedNumbers.size() - 1; ++i) {
+        if (sortedNumbers[i + 1] - sortedNumbers[i] < min_diff) {
+            min_diff = sortedNumbers[i + 1] - sortedNumbers[i];
+            result = {sortedNumbers[i], sortedNumbers[i + 1]};
         }
     }
     return {result.first, result.second};
