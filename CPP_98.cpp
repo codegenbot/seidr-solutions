41fb33e05a4838a7bb71f79f28eaeb802054abd9
beep boop <githubbot@vadim.me>
2024-07-12 17:23:30 +0200

    Solution 11 of CPP/98, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/73elaj2a Prompt: Complete the following code given the task description and function signature.

--- CPP_98.cpp
+++ CPP_98.cpp
@@ -1,8 +1,9 @@
-int count_upper(string s){
+int count_upper(string s) {
     int count = 0;
-    for(int i = 1; i < s.length(); i=i+2) {
-        if(isupper(s[i]))
+    for (int i = 1; i < s.length(); i++) {
+        if ((s[i] >= 'A' && s[i] <= 'E') || (s[i] >= 'I' && s[i] <= 'O') || (s[i] >= 'U')) {
             count++;
+        }
     }
     return count;
 }
