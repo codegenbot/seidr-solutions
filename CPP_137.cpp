7cc505ba1ce3d589fcae6e62953e409474bd623c
beep boop <githubbot@vadim.me>
2024-07-12 17:03:35 +0200

    Solution 30 of CPP/137, ratio 0.125 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pal7yz6s Prompt: ined here    21 | int main() {       |     ^~~~ In file included from 20681cb7-9328-499e-947f-5d73d72795c5.cpp:30: 20681cb7-9328-499e-947f-5d73d72795c5.cpp: In function ‘int main()’: 20681cb7-9328-499e-947f-5d73d72795c5.cpp:32:13: error: ‘boost’ has not been decl ared    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 20681cb7-9328-499e-947f-5d73d72795c5.cpp:32:35: error: expected primary-expressi on before ‘>’ token    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 20681cb7-9328-499e-947f-5d73d72795c5.cpp:32:48: error: too many arguments to fun ction ‘std::string compare_one(std::string)’    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ~~~~~~~~~~~^~~~~~~~~~~~~~~~ 20681cb7-9328-499e-947f-5d73d72795c5.cpp:6:8: note: declared here     6 | string compare_one(string a) {       |        ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,23 +1,3 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-string compare_one(string a) {
-    double s1 = stod(a);
-    if (a.find('x') != string::npos)
-        return "None";
-    else {
-        int s2 = stoi(a); 
-        if (s1 > s2)
-            return to_string(s1);
-        else if (s1 == s2)
-            return "None";
-        else
-            return to_string(s2);
-    }
-}
-
 int main() {
     string a;
     cout << "Enter the first number: ";
