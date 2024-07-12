04c98c444b2b86669f71dadfcd39502f87a23aaa
beep boop <githubbot@vadim.me>
2024-07-12 19:40:23 +0200

    Solution 72 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/894y32ic Prompt:       |                         ^~~~~~ e017db18-c46e-41e5-b7c8-dbaf7abd524d.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ e017db18-c46e-41e5-b7c8-dbaf7abd524d.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ e017db18-c46e-41e5-b7c8-dbaf7abd524d.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ e017db18-c46e-41e5-b7c8-dbaf7abd524d.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from e017db18-c46e-41e5-b7c8-dbaf7abd524d.cpp:26: e017db18-c46e-41e5-b7c8-dbaf7abd524d.cpp: In function ‘int main()’: e017db18-c46e-41e5-b7c8-dbaf7abd524d.cpp:28:32: error: ‘Strongest_Extension’ can not be used as a function    28 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -2,18 +2,18 @@
 #include <vector>
 #include <string>
 
-int Strongest_Extension(string class_name, vector<string> extensions) {
+std::string Strongest_Extension(std::string class_name, std::vector<std::string> extensions) {
     int max_strength = 0;
-    string strongest_extension;
+    std::string strongest_extension;
 
-    for (string extension : extensions) {
+    for (const auto& extension : extensions) {
         int cap = 0;
         int sm = 0;
 
         for (char c : extension) {
-            if (isupper(c))
+            if (std::isupper(c))
                 cap++;
-            else if (islower(c))
+            else if (std::islower(c))
                 sm++;
         }
 
