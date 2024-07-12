55c59d8d0c7018b0d1f5a42219ca58aca53f6350
beep boop <githubbot@vadim.me>
2024-07-12 14:48:24 +0200

    Solution 10 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/58tgfwqh Prompt: in this scope     4 | int search(vector<int> lst) {       |            ^~~~~~ 53f04587-cc06-4817-81a2-2624f7c191b3.cpp:4:12: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 53f04587-cc06-4817-81a2-2624f7c191b3.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 53f04587-cc06-4817-81a2-2624f7c191b3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 53f04587-cc06-4817-81a2-2624f7c191b3.cpp:4:19: error: expected primary-expressio n before ‘int’     4 | int search(vector<int> lst) {       |                   ^~~ In file included from 53f04587-cc06-4817-81a2-2624f7c191b3.cpp:25: 53f04587-cc06-4817-81a2-2624f7c191b3.cpp: In function ‘int main()’: 53f04587-cc06-4817-81a2-2624f7c191b3.cpp:27:19: error: ‘search’ cannot be used a s a function    27 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,17 +1,20 @@
-#include <vector>
-#include <map>
+int findMaxFreq(vector<int> lst) {
+    map<int, int> freqMap;
+    for (int num : lst) {
+        if (freqMap.find(num) == freqMap.end()) {
+            freqMap[num] = 1;
+        } else {
+            freqMap[num]++;
+        }
+    }
 
-int search(vector<int> lst) {
-    map<int, int> freq;
-    for (int i : lst) {
-        if (freq.find(i) == freq.end())
-            freq[i] = 1;
-        else
-            freq[i]++;
+    int maxFreqNum = -1;
+    for (auto it = freqMap.begin(); it != freqMap.end(); ++it) {
+        if (it->second >= it->first && it->first > 0) {
+            maxFreqNum = it->first;
+            break;
+        }
     }
-    for (auto p : freq) {
-        if (p.first > 0 && p.second >= p.first)
-            return p.first;
-    }
-    return -1;
+
+    return maxFreqNum;
 }
