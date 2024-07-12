d3c9ea9be409e7d0af512ce1eababa9d8deafa15
beep boop <githubbot@vadim.me>
2024-07-12 08:07:58 +0200

    Solution 89 of Python/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zx5e9imf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f3786d2-3d3c-4ad3-bb29-bf3ca51c1 c70.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_63.py
+++ Python_63.py
@@ -1,9 +1,7 @@
-from itertools import islice
-
 def fibfib(n: int):
     if n <= 1:
         return 0
-    fib_series = (0, 1)
-    for _ in range(2, n+1):  
-        fib_series = (a + b for a, b in zip(fib_series, islice(fib_series, 1, None)))
-    return sum(islice(fib_series, n))
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
