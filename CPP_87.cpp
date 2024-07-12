0033ca42b7437129430d0f8cab2470ee54a39455
beep boop <githubbot@vadim.me>
2024-07-12 02:00:35 +0200

    Solution 14 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ozl7l1g0 Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ bf20e7ef-4ba9-43d9-83bc-57379d6fc66b.cpp:33:49: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ bf20e7ef-4ba9-43d9-83bc-57379d6fc66b.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ bf20e7ef-4ba9-43d9-83bc-57379d6fc66b.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from bf20e7ef-4ba9-43d9-83bc-57379d6fc66b.cpp:32: bf20e7ef-4ba9-43d9-83bc-57379d6fc66b.cpp: In function ‘int main()’: bf20e7ef-4ba9-43d9-83bc-57379d6fc66b.cpp:45:27: error: could not convert ‘get_ro w(std::vector<std::vector<int> >, int)(3)’ from ‘vector<std::vector<int>>’ to ‘v ector<int>’    45 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~       |                           |       |                           vector<std::vector<int>>

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -3,11 +3,11 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b){
+bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
+std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x) {
     std::vector<std::vector<int>> result;
     for (int i = 0; i < lst.size(); ++i) {
         for (int j = 0; j < lst[i].size(); ++j) {
@@ -26,6 +26,6 @@ std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
 }
 
 int main() {
-    assert(issame(get_row({ {}, {1}, {1, 2, 3} }, 3), { { 2, 2 } }));
+    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3)[0], {2, 2}));
     return 0;
 }
