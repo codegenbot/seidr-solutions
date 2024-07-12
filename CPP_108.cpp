2e88ef4a3a7712499a0c62ff58ca56a11dbb9870
beep boop <githubbot@vadim.me>
2024-07-12 21:53:11 +0200

    Solution 28 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lqy3vi1c Prompt:     1 | ```       |   ^ 183fa544-b232-4b38-a17e-330c0db1f6c5.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 183fa544-b232-4b38-a17e-330c0db1f6c5.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from 183fa544-b232-4b38-a17e-330c0db1f6c5.cpp:39: 183fa544-b232-4b38-a17e-330c0db1f6c5.cpp: In function ‘int main()’: 183fa544-b232-4b38-a17e-330c0db1f6c5.cpp:41:23: error: invalid conversion from ‘ int’ to ‘int*’ [-fpermissive]    41 |     assert (count_nums({1}) == 1);       |             ~~~~~~~~~~^~~~~       |                       |       |                       int 183fa544-b232-4b38-a17e-330c0db1f6c5.cpp:41:23: error: too few arguments to func tion ‘int count_nums(int*, int)’ 183fa544-b232-4b38-a17e-330c0db1f6c5.cpp:5:5: note: declared here     5 | int count_nums(int* nums, int size) {       |     ^~~~~~~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -29,8 +29,7 @@ int count_nums(int* nums, int size) {
 int main() {
     int singleNum = 123; 
     int nums[] = {singleNum}; 
-    int size = sizeof(nums)/sizeof(nums[0]);
+    int size = 1; 
     std::cout << "The number of positive integers with an odd digital root is: " << count_nums(&nums[0], size) << std::endl;
-    assert (count_nums(&nums[0], 1) == 1); 
+    assert (count_nums(nums, size) == 1); 
     return 0;
-}
