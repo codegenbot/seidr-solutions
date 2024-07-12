8579ebeeb0a4bae4c600ebe8a8b8d93d82293665
beep boop <githubbot@vadim.me>
2024-07-12 13:56:47 +0200

    Solution 38 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3pjnfwz2 Prompt: Program did not receive expected input

--- Python_10.py
+++ Python_10.py
@@ -1,18 +1,20 @@
+```
 def is_palindrome(s: str) -> bool:
     return s == s[::-1]
 
-
 def make_palindrome(string: str) -> str:
+    if not string:
+        return string
     if is_palindrome(string):
         return string
     n = len(string)
     new_string = "#" + "#".join(list(string)) + "#"
-    i_max = n - 1
-    while not is_palindrome(new_string):
-        new_string = new_string[: i_max + 1] + "#"
-        i_max -= 1
     palindrome = ""
-    for i in range(n * 2 - 1):
-        if i < n or i > n * 2 - 2 - n:
-            palindrome += new_string[i]
+    for i in range(n):
+        i_max = n-1
+        while not is_palindrome(new_string):
+            new_string = new_string[:i_max+1] + "#"
+        palindrome += new_string[i_max]
+        new_string = new_string[1:]
+        i_max -= 1
     return palindrome
