b53755f0f1738e0973db7a29cd6d67c0cde84ad3
beep boop <githubbot@vadim.me>
2024-07-12 08:21:43 +0200

    Solution 30 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/9b9ssffg Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -2,25 +2,28 @@ def add(x: int, y: int):
     return x + y
 
 
-def main(num1: int = 0, num2: int = 0):
+def main():
     while True:
         try:
-            print(f"Previous sum: {num1}")
             num1 = int(input("Enter first number: "))
             num2 = int(input("Enter second number: "))
             break
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
-    print(f"The sum is {add(num1, num2)}")
-    cont = input("Do you want to continue? (y/n): ")
+    cont = "y"
     while True:
+        print(f"The sum is {add(num1, num2)}")
+        cont = input("Do you want to continue? (y/n): ")
         if cont.lower() in ["n", "no"]:
             break
         elif cont.lower() != "y":
             print("Invalid input. Please enter either 'y' or 'n'.")
-            cont = input("Do you want to continue? (y/n): ")
-        else:
-            main(num1, num2)
     if cont.lower() == "n":
         print("Program ended.")
+    else:
+        main()
+
+
+if __name__ == "__main__":
+    main()
