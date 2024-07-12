7f843a354780975e81619a90514d1dd35337b387
beep boop <githubbot@vadim.me>
2024-07-12 20:27:02 +0200

    Solution 78 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/glxnmwbk Prompt:       |                           ^~~~~~ 567345f2-ceb6-49f3-bf64-e9e34e37317a.cpp:35:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 567345f2-ceb6-49f3-bf64-e9e34e37317a.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 567345f2-ceb6-49f3-bf64-e9e34e37317a.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 567345f2-ceb6-49f3-bf64-e9e34e37317a.cpp:35:34: error: expected primary-expressi on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 567345f2-ceb6-49f3-bf64-e9e34e37317a.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 567345f2-ceb6-49f3-bf64-e9e34e37317a.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -12,7 +12,7 @@ std::vector<int> remove_duplicates(std::vector<int> input) {
     return result;
 }
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+bool isSame(const std::vector<int>& a, const std::vector<int>& b) { 
     return a == b;
 }
 
@@ -20,7 +20,7 @@ int main() {
     int arr[] = {1, 2, 3, 2, 4, 3, 5};
     std::vector<int> test1(arr, arr + sizeof(arr) / sizeof(arr[0]));
     std::vector<int> test2 = remove_duplicates(test1);
-    if (issame(test1, test2)) {
+    if (isSame(test1, test2)) { 
         for (int i : test2) {
             std::cout << i << " ";
         }
