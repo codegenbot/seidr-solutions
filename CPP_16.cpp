67d82f8160a8df23bf08b74112af269307da3390
beep boop <githubbot@vadim.me>
2024-07-12 18:28:09 +0200

    Solution 1 of CPP/16, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/p7ak72nm Prompt: 94d8b725-81e9-47be-91e7-563bfe30726a.cpp: In function ‘int count_distinct_charac ters(std::string)’: 94d8b725-81e9-47be-91e7-563bfe30726a.cpp:8:5: error: ‘set’ was not declared in t his scope     8 |     set<char> s(temp.begin(), temp.end());       |     ^~~ 94d8b725-81e9-47be-91e7-563bfe30726a.cpp:3:1: note: ‘std::set’ is defined in hea der ‘<set>’; did you forget to ‘#include <set>’?     2 | #include <algorithm>   +++ |+#include <set>     3 | using namespace std; 94d8b725-81e9-47be-91e7-563bfe30726a.cpp:8:9: error: expected primary-expression  before ‘char’     8 |     set<char> s(temp.begin(), temp.end());       |         ^~~~ 94d8b725-81e9-47be-91e7-563bfe30726a.cpp:9:12: error: ‘s’ was not declared in th is scope     9 |     return s.size();       |            ^

--- CPP_16.cpp
+++ CPP_16.cpp
@@ -1,10 +1,15 @@
 #include <string>
 #include <algorithm>
+#include <set>
+
 using namespace std;
 
 int count_distinct_characters(string str) {
     string temp = str;
     transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
-    set<char> s(temp.begin(), temp.end());
-    return s.size();
+    set<char> s; 
+    for (char c : temp) { 
+        s.insert(c); 
+    }
+    return s.size(); 
 }
