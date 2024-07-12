3d8df069a411b8ae7a513c0e438763d1be389710
beep boop <githubbot@vadim.me>
2024-07-12 08:52:56 +0200

    Solution 46 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2l83sobi Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/85e76503-7523-4bcd-beda-15e642ced 870.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -2,8 +2,8 @@
 def double_the_difference(lst):
     even_sum = sum(i for i in lst if i % 2 == 0)
     odd_sum = sum(i for i in lst if i % 2 != 0)
-    difference = abs(even_sum - odd_sum) if even_sum >= odd_sum else abs(odd_sum - even_sum)
-    return difference * 2
+    return (even_sum - odd_sum) * 2
+
 
 def check():
     result = double_the_difference([1, 2, 3, 4, 5])
@@ -11,5 +11,6 @@ def check():
         print("Yes, it equals two.")
     else:
         print("No, it doesn't equal two.")
+
 
 check()
