65459ecfbb58104fb171bb5ac5f1a27d92f4b06e
beep boop <githubbot@vadim.me>
2024-07-12 16:41:40 +0200

    Solution 6 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/tcq4hghd Prompt: r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 6a04f899-a867-48c2-ad6c-e4f293b14b56.cpp:4:35: note:   initializing argument 1 o f ‘std::vector<int> findPair(std::vector<int>&, int)’     4 | vector<int> findPair(vector<int>& nums, int target) {       |                      ~~~~~~~~~~~~~^~~~ 6a04f899-a867-48c2-ad6c-e4f293b14b56.cpp:27:9: error: ‘cout’ was not declared in  this scope    27 |         cout << res[0] << " " << res[1] << endl;       |         ^~~~ 6a04f899-a867-48c2-ad6c-e4f293b14b56.cpp:27:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 6a04f899-a867-48c2-ad6c-e4f293b14b56.cpp:27:44: error: ‘endl’ was not declared i n this scope    27 |         cout << res[0] << " " << res[1] << endl;       |                                            ^~~~ 6a04f899-a867-48c2-ad6c-e4f293b14b56.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
-using namespace std;
-
 #include <vector>
 using namespace std;
 
-pair<int, int> findPair(vector<int>& nums, int target) {
+vector<int> findPair(vector<int>& nums, int target) {
     unordered_map<int, int> numMap;
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
         if (numMap.find(complement) != numMap.end()) {
-            return {complement, nums[i]};
+            return {nums[i], complement};
         }
         numMap[nums[i]] = i;
     }
-    return {{}, {}};
+    return {};
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> nums(n);
     for (int i = 0; i < n; i++) {
-        cin >> nums[i];
+        int x1, x2;
+        cin >> x1 >> x2;
+        vector<int> res = findPair({x1, x2}, x1 + x2);
+        cout << res[0] << " " << res[1] << endl;
     }
-    int target;
-    cin >> target;
-    pair<int, int> result = findPair(nums, target);
-    cout << result.first << "\n" << result.second << endl;
     return 0;
 }
