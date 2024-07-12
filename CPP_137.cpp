c715892283a0c7c9a37f7afb091b521c139fba04
beep boop <githubbot@vadim.me>
2024-07-13 00:15:42 +0200

    Solution 45 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jkmyomz8 Prompt:       |     ^~~~ In file included from 92fe8a4a-8a03-41ba-b94d-3de15dfe2051.cpp:34: 92fe8a4a-8a03-41ba-b94d-3de15dfe2051.cpp: In function ‘int main()’: 92fe8a4a-8a03-41ba-b94d-3de15dfe2051.cpp:36:13: error: ‘boost’ has not been decl ared    36 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 92fe8a4a-8a03-41ba-b94d-3de15dfe2051.cpp:36:29: error: ‘string’ was not declared  in this scope    36 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 92fe8a4a-8a03-41ba-b94d-3de15dfe2051.cpp:36:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 92fe8a4a-8a03-41ba-b94d-3de15dfe2051.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 92fe8a4a-8a03-41ba-b94d-3de15dfe2051.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,13 +3,13 @@
 
 int compare_one(const std::string& s, int i) {
     if (i == 1)
-        return std::stod(s) - 10;
+        return std::stoi(s) - 10;
     else if (i == 2)
-        return std::stod(s);
+        return std::stoi(s);
     else if (i == 3)
-        return std::stod(s) + 10;
-    else
-        return 0; // default value for i not equal to 1 or 2
+        return std::stoi(s) + 10;
+    else 
+        return 0;
 }
 
 int main() {
@@ -23,8 +23,7 @@ int main() {
         int x = compare_one(a, 1);
         int y = compare_one(b, 2);
 
-        return (x < y) ? -1 : ((y < x) ? 1 : 0);
-    }
-    
-    return 0;
+        return (x < y) ? -1 : ((y < x) ? 1 : 0); 
+    } else
+        return 0;
 }
