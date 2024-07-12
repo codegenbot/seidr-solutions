be832e179f503c88b84f27cd3c45828c26ebde60
beep boop <githubbot@vadim.me>
2024-07-12 07:19:08 +0200

    Solution 76 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/t61x2loc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e9acb941-1aa2-42b9-bcf0-f7937ecc8 571.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_26.py
+++ Python_26.py
@@ -1,6 +1,7 @@
 def remove_duplicates(input_numbers=None):
     if input_numbers is None:
-        input_str = input("Enter numbers separated by space: ")
-        input_numbers = list(map(int, input_str.split()))
-    unique_numbers = list(dict.fromkeys(input_numbers))
+        input_numbers = list(map(int, input("Enter numbers separated by space: ").split()))
+    unique_numbers = []
+    for num in set(input_numbers):
+        unique_numbers.append(num)
     return unique_numbers
