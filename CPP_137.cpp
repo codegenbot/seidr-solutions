6d3ebf64a0cb438ebf80d207e33da09dfcb5b8bd
beep boop <githubbot@vadim.me>
2024-07-12 17:50:24 +0200

    Solution 2 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dhj7i5v3 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,30 +1,46 @@
-if (a.type() == typeid(int) && b.type() == typeid(int)) {
-    int x = boost::any_cast<int>(a);
-    int y = boost::any_cast<int>(b);
-    if (x > y)
-        return a;
-    else if (x < y)
-        return b;
-    else
-        return "None";
-} else if (a.type() == typeid(double) && b.type() == typeid(double)) {
-    double x = boost::any_cast<double>(a);
-    double y = boost::any_cast<double>(b);
-    if (x > y)
-        return a;
-    else if (x < y)
-        return b;
-    else
-        return "None";
-} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-    string x = boost::any_cast<string>(a);
-    string y = boost::any_cast<string>(b);
-    if (stod(x) > stod(y))
-        return a;
-    else if (stod(x) < stod(y))
-        return b;
-    else
-        return "None";
-} else {
-    throw invalid_argument("Invalid input");
+#include <boost/any.hpp>
+
+boost::any compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return (int)a > (float)b ? a : b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return (int)a > (double)b ? a : b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return (float)a > (int)b ? a : b;
+    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return (double)a > (int)b ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        try {
+            float fa = stof(a.convert<string>().c_str());
+            float fb = (float)b;
+            return fa > fb ? a : b;
+        } catch (...) {
+            return "None";
+        }
+    } else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        try {
+            double fa = stod(a.convert<string>().c_str());
+            double fb = (double)b;
+            return fa > fb ? a : b;
+        } catch (...) {
+            return "None";
+        }
+    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        try {
+            int fa = stoi(a.convert<string>().c_str());
+            int fb = (int)b;
+            return fa > fb ? a : b;
+        } catch (...) {
+            return "None";
+        }
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        try {
+            float fa = stof(a.convert<string>().c_str());
+            float fb = stof(b.convert<string>().c_str());
+            return fa > fb ? a : b;
+        } catch (...) {
+            return "None";
+        }
+    }
+    return "None";
 }
