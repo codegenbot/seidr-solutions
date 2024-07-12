5a48ab1eb627a553015cd2d34ecee74d2ec6b5ce
beep boop <githubbot@vadim.me>
2024-07-12 16:16:33 +0200

    Solution 93 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/cprp4l99 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/68fa035f-954b-4e00-958b-d6fb3efcd b43.py", line 2     def check(fix_spaces):     ^ IndentationError: expected an indented block after function definition on line 1

--- Python_140.py
+++ Python_140.py
@@ -1,9 +1,2 @@
-```
 def fix_spaces(text):
-    new_text = ''
-    for i in range(len(text)):
-        if text[i] == ' ' and i < len(text) - 1 and text[i+1] != ' ':
-            new_text += '&nbsp;'
-        else:
-            new_text += text[i]
-    return new_text
+    return text.replace("\t", " ")
