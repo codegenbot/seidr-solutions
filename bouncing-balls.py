6cf494aa5ab36fdff916734a4bed035954a1de50
beep boop <githubbot@vadim.me>
2024-07-12 15:09:44 +0200

    Solution 20 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/0khnc56a Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/79e61f1e-64d2-4725-8ebc-9b195e2f6 830.py", line 17     print("""{:.3f}\n{}.format(bouncing_balls())           ^ SyntaxError: unterminated triple-quoted string literal (detected at line 17)

--- bouncing-balls.py
+++ bouncing-balls.py
@@ -12,6 +12,6 @@ def bouncing_balls():
         total_distance += new_height * bounciness_index
         first_bounce_height = new_height
 
-    return "{}\n{}".format(bounciness_index, total_distance)
+    return "{:.3f}\n{}".format(bounciness_index, total_distance)
 
-print(*bouncing_balls())
+print("{:.3f}\n{}".format(*bouncing_balls()))
