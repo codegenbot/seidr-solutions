e6f6d874dc1023d45ccd5d0eef462ec7780a5118
beep boop <githubbot@vadim.me>
2024-07-12 18:19:17 +0200

    Solution 36 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/y11ts1n7 Prompt: Complete the following code given the task description and function signature.

--- Python_131.py
+++ Python_131.py
@@ -1,7 +1,8 @@
-```
 def digits(n):
     product = 1
-    for digit in str(n):
-        if int(digit) % 2 != 0:
-            product *= int(digit)
-    return product or 0
+    while n > 0:
+        digit = n % 10
+        if digit % 2 != 0:
+            product *= digit
+        n //= 10
+    return product
