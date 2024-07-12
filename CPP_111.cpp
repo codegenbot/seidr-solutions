9ce3656bf8583ad5a894c61974b306682247fe8c
beep boop <githubbot@vadim.me>
2024-07-12 22:44:31 +0200

    Solution 53 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ljt3szhv Prompt:       |                                      ^~~ 718cef69-8f51-43a0-8fcf-c9c08a98c6cf.cpp:32:44: error: expression list treated a s compound expression in initializer [-fpermissive]    32 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ 718cef69-8f51-43a0-8fcf-c9c08a98c6cf.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ 718cef69-8f51-43a0-8fcf-c9c08a98c6cf.cpp:6:5: note: ‘int main()’ previously defi ned here     6 | int main() {       |     ^~~~ In file included from 718cef69-8f51-43a0-8fcf-c9c08a98c6cf.cpp:31: 718cef69-8f51-43a0-8fcf-c9c08a98c6cf.cpp: In function ‘int main()’: 718cef69-8f51-43a0-8fcf-c9c08a98c6cf.cpp:46:20: error: ‘histogram’ was not decla red in this scope    46 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ 718cef69-8f51-43a0-8fcf-c9c08a98c6cf.cpp:46:19: error: ‘issame’ cannot be used a s a function    46 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,33 +1,29 @@
-```cpp
 #include <iostream>
-#include <unordered_map>
+#include <vector>
 
-bool issame(std::unordered_map<char, int> m1, std::unordered_map<char, int> m2) {
-    if (m1.size() != m2.size()) return false;
-    for (auto it = m1.begin(); it != m1.end(); ++it) {
-        if (m2.find(it->first) == m2.end() || m2[it->first] != it->second) return false;
-    }
-    return true;
-}
-
-std::unordered_map<char, int> histogram(std::string s) {
-    std::unordered_map<char, int> hist;
-    for (char c : s) {
-        if (hist.find(c) == hist.end()) hist[c] = 1;
-        else hist[c]++;
-    }
-    return hist;
-}
+using namespace std;
 
 int main() {
-    std::string str;
-    std::cout << "Enter a string: ";
-    std::cin >> str;
-
-    std::unordered_map<char, int> h = histogram(str);
-    for (auto it = h.begin(); it != h.end(); ++it) {
-        std::cout << it->first << ": " << it->second << "\n";
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-
+    
+    int max_val = *max_element(arr.begin(), arr.end());
+    int min_val = *min_element(arr.begin(), arr.end());
+    
+    cout << "Histogram: ";
+    for (int i = min_val; i <= max_val; i++) {
+        int count = 0;
+        for (int j = 0; j < n; j++) {
+            if (arr[j] == i) {
+                count++;
+            }
+        }
+        cout << count << " ";
+    }
+    
     return 0;
 }
