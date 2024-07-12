2dbe5198a03d1e0b8010fee94f67e37f410c8788
beep boop <githubbot@vadim.me>
2024-07-13 00:43:47 +0200

    Solution 2 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tzw1bm7i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0f0a0223-da6c-48e7-853a-8cb070acf 43b.py", line 26, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0f0a0223-da6c-48e7-853a-8cb070acf 43b.py", line 21, in check     sort_numbers("six five four three two one zero")   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0f0a0223-da6c-48e7-853a-8cb070acf 43b.py", line 15, in sort_numbers     return " ".join(            ^^^^^^^^^ TypeError: sequence item 0: expected str instance, list found

--- Python_19.py
+++ Python_19.py
@@ -1,3 +1,4 @@
+```
 def sort_numbers(numbers: str) -> str:
     num_dict = {
         "zero": 0,
@@ -11,4 +12,5 @@ def sort_numbers(numbers: str) -> str:
         "eight": 8,
         "nine": 9,
     }
-    return " ".join(map(str, [num_dict[i] for i in numbers.split()][0]))
+    sorted_nums = sorted([num_dict[i] for i in numbers.split()])
+    return " ".join(str(num_dict[str(n)]) for n in sorted_nums)
