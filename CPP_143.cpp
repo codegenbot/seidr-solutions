a122053936cfc52d9648c86c94b37af2cbaac6b7
beep boop <githubbot@vadim.me>
2024-07-12 23:06:07 +0200

    Solution 9 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xvsf4rbg Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: 77985020-6d51-4773-bbe7-f235526a6082.cpp:40:36:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -17,18 +17,13 @@ vector<string> split(const string& str, const string& delimiter) {
     vector<string> tokens;
     size_t pos = 0;
     while ((pos = str.find(delimiter)) != string::npos) {
-        if (pos != string::npos) { 
-            tokens.push_back(str.substr(0, pos));
-            str.erase(0, pos + delimiter.length());
-        }
-    }
-    if (!str.empty()) { 
-        tokens.push_back(str);
+        tokens.push_back(str.substr(0, pos));
+        str.erase(0, pos + delimiter.length());
     }
     return tokens;
 }
 
-string words_in_sentence(string sentence){
+string words_in_sentence(string& sentence){
     string result = "";
     for (const auto& word : split(sentence, " ")) {
         if (is_prime(word.length())) {
