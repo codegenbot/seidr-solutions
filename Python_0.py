5bf4a676282434b7e5d0ba7abde2076193ed7429
beep boop <githubbot@vadim.me>
2024-07-12 18:15:56 +0200

    Solution 44 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/nkmavojj Prompt: 30 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/619a6c6c-d1eb-4690-b07d-89921ccc1 80f.py", line 7, in <module>     check(has_close_elements)           ^^^^^^^^^^^^^^^^^^ NameError: name 'has_close_elements' is not defined

--- Python_0.py
+++ Python_0.py
@@ -1,8 +1,2 @@
-def has_close_elements(arr):
-    for i in range(len(arr) - 1):
-        if abs(arr[i] - arr[i+1]) <= 1:
-            return True
-    return False
-
-result = has_close_elements([1, 2, 10, 5])
+result = 10 + 20
 print(result)
