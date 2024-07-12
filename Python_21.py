564acdb3e4396f13fe554cc1bfa0cf54d3fabc25
beep boop <githubbot@vadim.me>
2024-07-12 09:51:18 +0200

    Solution 77 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/2k5nejhm Prompt: [0.0, 0.21052631578947367, 0.47368421052631576, 0.7368421052631579, 1.0] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a1987195-52b1-4b35-8525-97465ab7d 3e9.py", line 24, in <module>     check(rescale_to_unit)           ^^^^^^^^^^^^^^^ NameError: name 'rescale_to_unit' is not defined

--- Python_21.py
+++ Python_21.py
@@ -3,9 +3,11 @@ def normalize_numbers(numbers):
     max_num = max(numbers)
     return [(x - min_num) / (max_num - min_num) for x in numbers]
 
+
 def check():
     input_numbers = [1, 5, 10, 15, 20]
     output = normalize_numbers(input_numbers)
     print(output)
+
 
 check()
