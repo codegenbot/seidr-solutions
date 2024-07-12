cadd92219d8d6ed627eb59f71ef3a5a1b3c2c350
beep boop <githubbot@vadim.me>
2024-07-12 21:06:35 +0200

    Solution 2 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/x8d2qru1 Prompt:       |                              ^~~~~~ cbe5635f-600e-4da8-958a-149965b271be.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ cbe5635f-600e-4da8-958a-149965b271be.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ cbe5635f-600e-4da8-958a-149965b271be.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from cbe5635f-600e-4da8-958a-149965b271be.cpp:29: cbe5635f-600e-4da8-958a-149965b271be.cpp: In function ‘int main()’: cbe5635f-600e-4da8-958a-149965b271be.cpp:39:20: error: ‘reverse_delete’ was not declared in this scope    39 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ cbe5635f-600e-4da8-958a-149965b271be.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,36 +1,9 @@
-#include <vector>
-#include <algorithm>
-
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
+vector<string> issame(vector<string> a, vector<string> b) {
+    return a == b;
 }
 
-std::vector<string> reverse_delete(string s, string c) {
-    vector<string> result;
-    string temp = "";
-    for (char ch : s) {
-        bool found = false;
-        for (char cc : c) {
-            if (ch == cc) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            temp += ch;
-        }
-    }
-    result.push_back(temp);
-    string rev = temp;
-    reverse(rev.begin(), rev.end());
-    if (temp == rev) {
-        result.push_back("True");
-    } else {
-        result.push_back("False");
-    }
-    return result;
+int main() {
+    assert(issame({}, {"True"}));
+    // rest of your code...
 }
+```
