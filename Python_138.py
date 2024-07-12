c6b4a1eb4415430fb52349875d7efbab012d6621
beep boop <githubbot@vadim.me>
2024-07-12 22:47:54 +0200

    Solution 9 of Python/138, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/o4bwwa4l Prompt: Complete the following code given the task description and function signature.

--- Python_138.py
+++ Python_138.py
@@ -1,6 +1,9 @@
 ```
 def is_equal_to_sum_even(n):
-    for i in range(2, n+1, 2):
-        if sum(j for j in range(2, i+1, 2) if i == sum(range(2, j+1, 2))):
-            return True
+    for a in range(2, n+1, 2):
+        for b in range(a+2, n+1, 2):
+            for c in range(b+2, n+1, 2):
+                for d in range(c+2, n+1, 2):
+                    if a + b + c + d == n:
+                        return True
     return False
