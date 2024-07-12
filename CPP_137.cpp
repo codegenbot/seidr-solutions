b726f66ed59377ec666bd4dfd1ad6124858f5409
beep boop <githubbot@vadim.me>
2024-07-12 06:31:30 +0200

    Solution 30 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ivyc6q78 Prompt: 99333abb-defa-4420-a71f-31965ef19368.cpp:54:5: error: redefinition of ‘int main( )’    54 | int main(){       |     ^~~~ 99333abb-defa-4420-a71f-31965ef19368.cpp:48:5: note: ‘int main()’ previously def ined here    48 | int main() {       |     ^~~~ In file included from 99333abb-defa-4420-a71f-31965ef19368.cpp:53: 99333abb-defa-4420-a71f-31965ef19368.cpp: In function ‘int main()’: 99333abb-defa-4420-a71f-31965ef19368.cpp:55:13: error: ‘boost’ has not been decl ared    55 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 99333abb-defa-4420-a71f-31965ef19368.cpp:55:35: error: expected primary-expressi on before ‘>’ token    55 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,48 +1,53 @@
 #include <iostream>
-#include <any>
+#include <boost/any.hpp>
+#include <boost/lexical_cast.hpp>
 #include <string>
 #include <cassert>
 
 using namespace std;
+using namespace boost;
 
-std::any compare_one(std::any a, std::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        if (std::any_cast<int>(a) > std::any_cast<int>(b))
+any compare_one(any a, any b) {
+    if(a.type() == typeid(int) && b.type() == typeid(int)){
+        if(any_cast<int>(a) > any_cast<int>(b))
             return a;
-        else if (std::any_cast<int>(a) < std::any_cast<int>(b))
+        else if(any_cast<int>(a) < any_cast<int>(b))
             return b;
         else
-            return "None";
-    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        if (std::any_cast<float>(a) > std::any_cast<float>(b))
-            return a;
-        else if (std::any_cast<float>(a) < std::any_cast<float>(b))
-            return b;
-        else
-            return "None";
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        float a_float = stof(std::any_cast<string>(a));
-        float b_float = stof(std::any_cast<string>(b));
-        if (a_float > b_float)
-            return a;
-        else if (a_float < b_float)
-            return b;
-        else
-            return "None";
-    } else if ((a.type() == typeid(int) && b.type() == typeid(string)) || (a.type() == typeid(string) && b.type() == typeid(int))) {
-        float a_float = a.type() == typeid(int) ? static_cast<float>(std::any_cast<int>(a)) : stof(std::any_cast<string>(a));
-        float b_float = b.type() == typeid(int) ? static_cast<float>(std::any_cast<int>(b)) : stof(std::any_cast<string>(b));
-        if (a_float > b_float)
-            return a;
-        else if (a_float < b_float)
-            return b;
-        else
-            return "None";
+            return std::string("None");
     }
-    return "None";
+    else if(a.type() == typeid(float) && b.type() == typeid(float)){
+        if(any_cast<float>(a) > any_cast<float>(b))
+            return a;
+        else if(any_cast<float>(a) < any_cast<float>(b))
+            return b;
+        else
+            return std::string("None");
+    }
+    else if(a.type() == typeid(std::string) && b.type() == typeid(std::string)){
+        float a_float = lexical_cast<float>(any_cast<std::string>(a));
+        float b_float = lexical_cast<float>(any_cast<std::string>(b));
+        if(a_float > b_float)
+            return a;
+        else if(a_float < b_float)
+            return b;
+        else
+            return std::string("None");
+    }
+    else if((a.type() == typeid(int) && b.type() == typeid(std::string)) || (a.type() == typeid(std::string) && b.type() == typeid(int))){
+        float a_float = lexical_cast<float>(a.type() == typeid(int) ? lexical_cast<std::string>(any_cast<int>(a)) : any_cast<std::string>(a));
+        float b_float = lexical_cast<float>(b.type() == typeid(int) ? lexical_cast<std::string>(any_cast<int>(b)) : any_cast<std::string>(b));
+        if(a_float > b_float)
+            return a;
+        else if(a_float < b_float)
+            return b;
+        else
+            return std::string("None");
+    }
+    return std::string("None");
 }
 
 int main() {
-    assert(std::any_cast<string>(compare_one(string("1"), string("1"))) == "None");
+    assert(any_cast<std::string>(compare_one(std::string("1"), std::string("1"))) == "None");
     return 0;
 }
