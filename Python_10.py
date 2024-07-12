9ad8847f6be331efd9c8ab17d9d3ebb754ffc1b0
beep boop <githubbot@vadim.me>
2024-07-12 05:37:54 +0200

    Solution 20 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h3qtxl8o Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d4c0c093-0511-49aa-8b31-1be58bd78 8c0.py", line 13, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d4c0c093-0511-49aa-8b31-1be58bd78 8c0.py", line 10, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,11 +1,11 @@
-```
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
         return string
     else:
-        prefix = string[0]
-        suffix = string[-1]
-        rest = string[1:-1] + prefix + suffix
-        while not rest == rest[::-1]:
-            rest = prefix + rest + suffix
-        return rest
+        if string[0] != string[-1]:
+            half = int(len(string) / 2)
+            return string[:half] + string[0] + string[half:] + string[-1]
+        else:
+            for i in range(1, len(string) - 1):
+                if string[i - 1] == string[-i]:
+                    return string[:i] + string[0] + string[i:][::-1]
