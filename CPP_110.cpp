4cdacaf0677249423c8e70f174c0e967166bf8d2
beep boop <githubbot@vadim.me>
2024-07-13 00:48:54 +0200

    Solution 87 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/086csx52 Prompt: 3629674b-2dd7-412d-930b-530ed990425c.cpp:87:71: error: ‘::max’ has not been decl ared; did you mean ‘std::max’?    87 |       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n') ;       |                                                             ^~~       |                                                             std::max In file included from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/ios:40,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 3629674b-2dd7-412d-930b-530ed990425c.cpp:1: /usr/include/c++/11/bits/stl_algobase.h:300:5: note: ‘std::max’ declared here   300 |     max(const _Tp& __a, const _Tp& __b, _Compare __comp)       |     ^~~ 3629674b-2dd7-412d-930b-530ed990425c.cpp: At global scope: 3629674b-2dd7-412d-930b-530ed990425c.cpp:120:5: error: redefinition of ‘int main ()’   120 | int main(){       |     ^~~~ 3629674b-2dd7-412d-930b-530ed990425c.cpp:6:5: note: ‘int main()’ previously defi ned here     6 | int main() {       |     ^~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <vector>
 
@@ -9,24 +8,48 @@ int main() {
     int m1 = 0;
     int n2;
     int m2;
-    std::cout << "Enter the number of elements in lst1: ";
-    std::cin >> n1;
-    std::cout << "Enter the number of elements to fill lst2: ";
-    std::cin >> m2; 
-    std::cout << "Enter the number of elements in lst2: ";
-    while (!(std::cin >> m1)) {
+
+    while (!(std::cin >> n1)) {
         std::cout << "Invalid input. Please enter a positive integer: ";
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cin >> m1;
+        std::cin >> n1;
     }
+
+    while (!(std::cin >> m2)) {
+        std::cout << "Invalid input. Please enter a positive integer: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> m2;
+    }
+
+    if (!(std::cin >> m1)) {
+        std::cout << "Invalid input. Please enter a positive integer: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        while (true) {
+            std::cout << "Enter the number of elements in lst1: ";
+            if (!(std::cin >> m1)) break;
+            if (m2 < m1) {
+                std::cout << "You need to enter " << m2 - m1 << " more elements.\n";
+                break;
+            }
+        }
+    }
+
+    while (!(std::cin >> n2)) {
+        std::cout << "Invalid input. Please enter a positive integer: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> n2;
+    }
+
     if (m2 < m1) {
         std::cout << "You need to enter " << m2 - m1 << " more elements.\n";
-    } else if(m2 > n1){
-        return 0;
     } else {
-        break;
+        return 0;
     }
+
     std::vector<int> lst1;
     for (int i = 0; i < n1; i++) {
         int temp = -1;
@@ -45,8 +68,8 @@ int main() {
             lst1.push_back(temp);
         }
     }
-    int m;
-    m = m2; 
+
+    int m = m2; 
     std::vector<int> lst2;
     for (int i = 0; i < m; i++) {
         int temp = -1;
@@ -65,21 +88,25 @@ int main() {
             lst2.push_back(temp);
         }
     }
+
     if(lst2.size() != m) {
         std::cout << "You need to enter " << m - lst2.size() << " more elements.\n";
     } else {
         std::string result = exchange(lst1, lst2);
         std::cout << result << std::endl;
     }
-    return 0;
 }
 
 std::string exchange(const std::vector<int>& lst1, const std::vector<int>& lst2) {
     int oddCount = 0;
-    for (int num : lst1) {
-        if (num % 2 != 0) {
+    for (int i : lst1) {
+        if (i % 2 != 0) {
             oddCount++;
         }
     }
-    return oddCount == 0 ? "YES" : "NO";
+    if (oddCount > 0) {
+        return "Odd";
+    } else {
+        return "Even";
+    }
 }
