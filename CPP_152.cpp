3e95faf4eef34f055d21ad74b9ba2047f3480953
beep boop <githubbot@vadim.me>
2024-07-13 01:19:20 +0200

    Solution 51 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uswfokmt Prompt: d4d09316-1fe9-4a62-b164-718311806e08.cpp:58:5: error: redefinition of ‘int main( )’    58 | int main(){       |     ^~~~ d4d09316-1fe9-4a62-b164-718311806e08.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~ In file included from d4d09316-1fe9-4a62-b164-718311806e08.cpp:49: d4d09316-1fe9-4a62-b164-718311806e08.cpp: In function ‘int main()’: d4d09316-1fe9-4a62-b164-718311806e08.cpp:59:20: error: ‘compare’ was not declare d in this scope    59 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,34 +1,3 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-bool isSame(vector<int> a,vector<int> b){
-    for(int i = 0; i < a.size();i++){
-        if(a[i] != b[i]) return false;
-    }
-    return true;
-
-}
-
-vector<int> getResult(vector<int> game, vector<int> guess) {
-    vector<int> result;
-    if(isSame(game,guess)){
-        for (int i = 0; i < game.size(); i++) {
-            result.push_back(2);
-        }
-    } else {
-        for (int i = 0; i < game.size(); i++) {
-            if (game[i] == guess[i]) {
-                result.push_back(1);
-            } else {
-                result.push_back(0);
-            }
-        }
-    }
-    return result;
-
-}
-
 int main() {
     int n;
     cin >> n;
