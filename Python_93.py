a9cb193562ff1084a36c7c817ac17da51bfc8844
beep boop <githubbot@vadim.me>
2024-07-12 21:54:52 +0200

    Solution 4 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0qaeb0u7 Prompt: Complete the following code given the task description and function signature.

--- Python_93.py
+++ Python_93.py
@@ -5,10 +5,10 @@ def encode(message):
     for char in message:
         if char.isalpha():
             if char.lower() in vowels:
-                new_char = chr((ord(char) - ord('a') + 2) % 26 + ord('a'))
+                index = (ord(char.lower()) - ord('a') + 2) % 26
+                result += chr(ord('a') + index)
             else:
-                new_char = char.swapcase()
-            result += new_char
+                result += char.swapcase()
         else:
             result += char
     return result
