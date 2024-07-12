53423993b3cf715b6fcbc7fb11cfe8d29a3a316a
beep boop <githubbot@vadim.me>
2024-07-12 08:44:59 +0200

    Solution 18 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/oaf2ryzo Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9093ffef-dc21-42cb-887b-38dafe6cc 73c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,3 +1,4 @@
 def sorted_list_sum(lst):
-    even_lengths_sum = sum(len(word) for word in lst)
-    return str(even_lengths_sum)
+    even_words = [word for word in lst if len(word) % 2 == 0]
+    even_lengths_sum = sum(len(word) for word in even_words)
+    return ",".join(sorted(lst, key=len))
