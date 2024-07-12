dc3be0b0414002bce0686e5740c752230bc4adaa
beep boop <githubbot@vadim.me>
2024-07-12 19:39:33 +0200

    Solution 95 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dhj7i5v3 Prompt: 568660ee-0e63-4a26-ab08-a7d4b01f0c41.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,32 +1,68 @@
-#include <string>
 #include <iostream>
+#include <string>
 using namespace std;
 
-bool compare_one(string a, string b) {
-    try {
-        float fa = stof(a);
-        float fb = stof(b);
-        return fa > fb;
-    } catch (...) {
-        try {
-            double fa = stod(a);
-            double fb = stod(b);
-            return fa > fb;
-        } catch (...) {
-            int fa = stoi(a);
-            int fb = stoi(b);
-            return fa > fb;
-        }
-    }
+bool isNumeric(const string& s) {
+    return all_of(s.begin(), s.end(), ::isdigit);
 }
 
-int main() {
-    string a, b;
-    cin >> a >> b;
-    if (compare_one(a, b))
-        cout << a << " is greater than " << b << endl;
-    else
-        cout << b << " is greater than or equal to " << a << endl;
-
-    return 0;
+boost::any compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return (int)a > (float)b ? a : b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return (int)a > (double)b ? a : b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return (float)a > (int)b ? a : b;
+    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return (double)a > (int)b ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        try {
+            float fa = stof(a.convert<string>().c_str());
+            float fb = (float)b;
+            return fa > fb ? a : b;
+        } catch (...) {
+            return "None";
+        }
+    } else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        try {
+            double fa = stod(a.convert<string>().c_str());
+            double fb = (double)b;
+            return fa > fb ? a : b;
+        } catch (...) {
+            return "None";
+        }
+    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        try {
+            int fa = stoi(a.convert<string>().c_str());
+            int fb = (int)b;
+            return fa > fb ? a : b;
+        } catch (...) {
+            return "None";
+        }
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        try {
+            float fa = stof(a.convert<string>().c_str());
+            float fb = stof(b.convert<string>().c_str());
+            return fa > fb ? a : b;
+        } catch (...) {
+            return "None";
+        }
+    } else if (a.type() == typeid(string) && isNumeric(a.convert<string>().c_str()) && b.type() == typeid(int)) {
+        try {
+            int fa = stoi(a.convert<string>().c_str());
+            int fb = (int)b;
+            return fa > fb ? a : b;
+        } catch (...) {
+            return "None";
+        }
+    } else if (a.type() == typeid(string) && isNumeric(b.convert<string>().c_str()) && b.type() == typeid(int)) {
+        try {
+            int fa = stoi(a.convert<string>().c_str());
+            int fb = stoi(b.convert<string>().c_str());
+            return fa > fb ? a : b;
+        } catch (...) {
+            return "None";
+        }
+    }
+    return "None";
 }
