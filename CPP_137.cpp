5ca881c3a4694cd29e92eb2aafcadb39ece9a2d5
beep boop <githubbot@vadim.me>
2024-07-12 19:19:40 +0200

    Solution 77 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dhj7i5v3 Prompt:    19 | } else if (a.type() == typeid(string) && b.type() == typeid(string)) {       |   ^~~~ 7ff3f2a1-d951-4a00-947b-fe8d19ad2344.cpp:28:3: error: expected unqualified-id be fore ‘else’    28 | } else {       |   ^~~~ In file included from 7ff3f2a1-d951-4a00-947b-fe8d19ad2344.cpp:32: 7ff3f2a1-d951-4a00-947b-fe8d19ad2344.cpp: In function ‘int main()’: 7ff3f2a1-d951-4a00-947b-fe8d19ad2344.cpp:34:13: error: ‘boost’ has not been decl ared    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 7ff3f2a1-d951-4a00-947b-fe8d19ad2344.cpp:34:29: error: ‘string’ was not declared  in this scope    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 7ff3f2a1-d951-4a00-947b-fe8d19ad2344.cpp:34:37: error: ‘compare_one’ was not dec lared in this scope    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,8 +1,8 @@
 #include <boost/any.hpp>
 using namespace std;
 
-boost::any bigger(const boost::any& a, const boost::any& b) {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+shared_ptr<boost::any> maxAny(const shared_ptr<boost::any>& a, const shared_ptr<boost::any>& b) {
+    if (a->type() == typeid(int) && b->type() == typeid(int)) {
         int x = boost::any_cast<int>(a);
         int y = boost::any_cast<int>(b);
         if (x > y)
@@ -10,8 +10,8 @@ boost::any bigger(const boost::any& a, const boost::any& b) {
         else if (x < y)
             return b;
         else
-            return boost::any(typeid(int), 0);
-    } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
+            return b;  // returning b since they are equal
+    } else if (a->type() == typeid(double) && b->type() == typeid(double)) {
         double x = boost::any_cast<double>(a);
         double y = boost::any_cast<double>(b);
         if (x > y)
@@ -19,8 +19,8 @@ boost::any bigger(const boost::any& a, const boost::any& b) {
         else if (x < y)
             return b;
         else
-            return boost::any(typeid(double), 0.0);
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+            return b;  // returning b since they are equal
+    } else if (a->type() == typeid(string) && b->type() == typeid(string)) {
         string x = boost::any_cast<string>(a);
         string y = boost::any_cast<string>(b);
         if (stod(x) > stod(y))
@@ -28,7 +28,7 @@ boost::any bigger(const boost::any& a, const boost::any& b) {
         else if (stod(x) < stod(y))
             return b;
         else
-            return boost::any(typeid(string), "0");
+            return b;  // returning b since they are equal
     } else {
         throw invalid_argument("Invalid input");
     }
