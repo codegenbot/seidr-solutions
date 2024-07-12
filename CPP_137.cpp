5443da7eab2380009358b5e952be6f2b69a813ee
beep boop <githubbot@vadim.me>
2024-07-12 18:37:06 +0200

    Solution 35 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dhj7i5v3 Prompt:    37 | } else if (a.type() == typeid(double) && b.type() == typeid(string)) {       |   ^~~~ a32484d1-0d2e-4beb-ab20-da8089672292.cpp:46:3: error: expected unqualified-id be fore ‘else’    46 | } else {       |   ^~~~ In file included from a32484d1-0d2e-4beb-ab20-da8089672292.cpp:50: a32484d1-0d2e-4beb-ab20-da8089672292.cpp: In function ‘int main()’: a32484d1-0d2e-4beb-ab20-da8089672292.cpp:52:13: error: ‘boost’ has not been decl ared    52 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ a32484d1-0d2e-4beb-ab20-da8089672292.cpp:52:29: error: ‘string’ was not declared  in this scope    52 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ a32484d1-0d2e-4beb-ab20-da8089672292.cpp:52:37: error: ‘compare_one’ was not dec lared in this scope    52 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -30,26 +30,17 @@ std::any compare_one(std::any a, std::any b) {
             return b;
         else
             return "None";
+    } else if ((a.type() == typeid(std::string) && b.type() == typeid(double)) ||
+               (a.type() == typeid(double) && b.type() == typeid(std::string))) {
+        double y = boost::any_cast<double>(b);
+        std::string x = boost::any_cast<std::string>(a);
+        if (std::stod(x) > y)
+            return a;
+        else if (y > std::stod(x))
+            return b;
+        else
+            return "None";
     } else {
-        double ax, ay, bx, by;
-        try {
-            ax = boost::any_cast<double>(a);
-            ay = boost::any_cast<double>(b);
-            if (ax > ay)
-                return a;
-            else if (ay > ax)
-                return b;
-            else
-                return "None";
-        } catch (...) {
-            std::string x = boost::any_cast<std::string>(a);
-            double y = boost::any_cast<double>(b);
-            if (std::stod(x) > y)
-                return a;
-            else if (y > std::stod(x))
-                return b;
-            else
-                return "None";
-        }
+        return "None";
     }
 }
