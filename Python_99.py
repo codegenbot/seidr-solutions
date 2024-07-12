d347efea04225575873d4b61a8b0dbd37f665393
beep boop <githubbot@vadim.me>
2024-07-12 08:30:14 +0200

    Solution 19 of Python/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/rz1ujt0v Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e65d4fb7-0692-407a-b4e9-da72563aa 6c9.py", line 18, in <module>     check(closest_integer)           ^^^^^^^^^^^^^^^ NameError: name 'closest_integer' is not defined

--- Python_99.py
+++ Python_99.py
@@ -3,7 +3,12 @@ def find_closest_number():
         try:
             value = float(input("Enter a decimal number: "))
             closest_value = round(value)
-            if value == closest_value:
+            if value != closest_value:
+                continue
+            else:
                 return closest_value
         except ValueError:
             print("Invalid input. Please enter a decimal number.")
+
+closest_integer = find_closest_number()
+print(closest_integer)
