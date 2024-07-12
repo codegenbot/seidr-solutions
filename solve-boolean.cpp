20b8241d3616b4157851a6da4ec1b273d2869e63
beep boop <githubbot@vadim.me>
2024-07-12 10:34:53 +0200

    Solution 5 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/6xxan9n2 Prompt: it must return True for input f|f&t&t|t&t, but it returns False.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,40 +1,31 @@
 #include <iostream>
 #include <string>
-#include <stack>
 
 using namespace std;
-
-bool evaluateBoolean(string input) {
-    stack<bool> st;
-    
-    for (char c : input) {
-        if (c == 't') {
-            st.push(true);
-        } else if (c == 'f') {
-            st.push(false);
-        } else if (c == '|') {
-            bool op1 = st.top();
-            st.pop();
-            bool op2 = st.top();
-            st.pop();
-            st.push(op1 || op2);
-        } else if (c == '&') {
-            bool op1 = st.top();
-            st.pop();
-            bool op2 = st.top();
-            st.pop();
-            st.push(op1 && op2);
-        }
-    }
-    
-    return st.top();
-}
 
 int main() {
     string input;
     cin >> input;
     
-    if (evaluateBoolean(input)) {
+    bool result = false;
+    
+    for (int i = 0; i < input.size(); i += 2) {
+        if (input[i] == 't' || input[i] == 'T') {
+            result = true;
+        } else if (input[i] == 'f' || input[i] == 'F') {
+            result = false;
+        }
+        
+        if (i + 1 < input.size() && input[i + 1] == '|') {
+            continue;
+        } else if (i + 1 < input.size() && input[i + 1] == '&') {
+            i++;
+        } else {
+            break;
+        }
+    }
+    
+    if (result) {
         cout << "True" << endl;
     } else {
         cout << "False" << endl;
