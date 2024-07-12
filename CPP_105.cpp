47214fbad04d61b18d14620583b2f05562153a0c
beep boop <githubbot@vadim.me>
2024-07-12 21:03:28 +0200

    Solution 8 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2azl5hhy Prompt:       |                              ^~~~~~ f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp:4:37: error: ‘string’ was not declared in this scope     4 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp:4:44: error: ‘b’ was not declared in th is scope     4 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp:4:45: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp:3: f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp: In function ‘int main()’: f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp:13:20: error: ‘by_length’ was not decla red in this scope    13 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,12 +1,13 @@
-#include <vector>
+```
 #include <string>
+#include <vector>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
-        if (a[i].size() != b[i].size()) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
@@ -14,6 +15,27 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
 }
 
 int main() {
-    assert(issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));
+    int n;
+    std::cin >> n;
+    vector<string> words(n);
+    for (int i = 0; i < n; i++) {
+        cin >> words[i];
+    }
+    string longest_word = *max_element(words.begin(), words.end(),
+                                          [](const string& a, const string& b) {
+                                              return a.size() < b.size();
+                                          });
+    vector<string> same_words;
+    for (int i = 0; i < n; i++) {
+        if (issame({longest_word}, {words[i]})) {
+            same_words.push_back(words[i]);
+        }
+    }
+    cout << "Longest word: " << longest_word << endl;
+    cout << "Same words: ";
+    for (int i = 0; i < same_words.size(); i++) {
+        cout << same_words[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
