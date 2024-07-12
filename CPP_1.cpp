3527785fc5e8e42589ea97f24f3d95f1ef54f344
beep boop <githubbot@vadim.me>
2024-07-12 08:01:35 +0200

    Solution 32 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/np8ofhi1 Prompt:    25 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ea9530ef-3a0d-4220-ba23-adc21b38934b.cpp:25:44: error: ‘b’ was not declared in t his scope    25 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ea9530ef-3a0d-4220-ba23-adc21b38934b.cpp:25:45: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ea9530ef-3a0d-4220-ba23-adc21b38934b.cpp:24: ea9530ef-3a0d-4220-ba23-adc21b38934b.cpp: In function ‘int main()’: ea9530ef-3a0d-4220-ba23-adc21b38934b.cpp:34:20: error: ‘separate_paren_groups’ w as not declared in this scope    34 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ ea9530ef-3a0d-4220-ba23-adc21b38934b.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,38 +1,22 @@
-bool issame(vector<string> a, vector<string> b);
-
-vector<string> separate_paren_groups(string paren_string);
-
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
+bool issame(const string& s) {
+    stack<char> st;
+    for (char c : s) {
+        if (c == '(') {
+            st.push(c);
+        } else if (c == ')') {
+            if (st.empty() || st.top() != '(') {
+                return false;
+            }
+            st.pop();
         }
     }
-    return true;
+    return st.empty();
 }
 
 vector<string> separate_paren_groups(string paren_string) {
-    vector<string> result;
-    string temp;
-    int count = 0;
-    for (char c : paren_string) {
-        if (c == '(') {
-            if (count > 0) {
-                temp += c;
-            }
-            count++;
-        } else if (c == ')') {
-            count--;
-            if (count > 0) {
-                temp += c;
-            } else if (count == 0) {
-                result.push_back(temp);
-                temp = "";
-            }
-        }
-    }
-    return result;
+    ...
+}
+
+int main() {
+    ...
 }
