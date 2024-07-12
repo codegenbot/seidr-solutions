427b478ac1798ec36170eda25b37221ef015397f
beep boop <githubbot@vadim.me>
2024-07-12 14:51:42 +0200

    Solution 43 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fayzjz97 Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ 15234cfb-d198-45a2-a82c-9f99ec81f56d.cpp:9:29:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -5,14 +5,14 @@
 std::vector<std::string> filter_by_prefix(std::vector<std::string> strings, std::string prefix) { 
     std::vector<std::string> result; 
     for(auto s : strings){ 
-        if (!s.empty() && s.find(prefix) == 0) 
+        if (!s.empty() && s.find(prefix) == 0)
             result.push_back(s); 
     } 
     return result;
 }
 
 int main(){
-    std::vector<std::string> strings = {"hello", "hi", "hey"};
+    std::vector<std::string> strings = {"hello", "hi", "", "hey"};
     std::string prefix = "h";
     
     std::vector<std::string> filteredStrings = filter_by_prefix(strings, prefix);
