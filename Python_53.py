843423197ef12ba523c54c20f1609b76aa9cc022
beep boop <githubbot@vadim.me>
2024-07-12 16:53:27 +0200

    Solution 63 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/y4i48eyu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8aa3d94e-80f1-4abd-8ba4-98153f457 0f5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,5 +1,8 @@
-def calculate_area_and_perimeter():
-    radius = int(input("Enter the radius of the circle: "))
-    area = 3.14159 * (radius ** 2)
-    perimeter = 2 * 3.14159 * radius
-    return f"Area is {area}, Perimeter is {perimeter}"
+def check_numbers():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    
+    if (num1 >=0 and isinstance(num1,int)) and (num2>=0 and isinstance(num2,int)):
+        return num1 + num2
+    else:
+        return "Error: Please enter non-negative integers"
