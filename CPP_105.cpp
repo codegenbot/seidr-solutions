d9f0ea76482d48fc5d9fc4ef2046c4c2525c77fc
beep boop <githubbot@vadim.me>
2024-07-12 23:14:07 +0200

    Solution 84 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2azl5hhy Prompt:       |                                 ^~~~~~ In file included from 4edd3b76-99b1-4dfe-a8f5-c452da7f7818.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 4edd3b76-99b1-4dfe-a8f5-c452da7f7818.cpp:44:44: error: ‘b’ was not declared in t his scope    44 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 4edd3b76-99b1-4dfe-a8f5-c452da7f7818.cpp:43: 4edd3b76-99b1-4dfe-a8f5-c452da7f7818.cpp: In function ‘int main()’: 4edd3b76-99b1-4dfe-a8f5-c452da7f7818.cpp:53:29: error: invalid initialization of  reference of type ‘const std::vector<std::vector<std::__cxx11::basic_string<cha r> > >&’ from expression of type ‘std::vector<std::__cxx11::basic_string<char> > ’    53 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ~~~~~~~~~^~~~~~~~~~~ 4edd3b76-99b1-4dfe-a8f5-c452da7f7818.cpp:4:58: note: in passing argument 1 of ‘b ool issame(const std::vector<std::vector<std::__cxx11::basic_string<char> > >&, const std::vector<std::vector<std::__cxx11::basic_string<char> > >&)’     4 | bool issame(const std::vector<std::vector<std::string>>& a, const std::v ector<std::vector<std::string>>& b) {       |             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -29,13 +29,9 @@ std::vector<std::string> by_length(const std::initializer_list<int>& lengths) {
     return result;
 }
 
-int main() {
+int main_problem() {
     std::vector<int> lengths = {4, 8, 9};
     std::vector<std::string> output = by_length(lengths);
     
-    // Initialize a,b as required
-    std::vector<std::vector<std::string>> a({{output[0]}});
-    std::vector<std::vector<std::string>> b({{output[0]}, {output[1], output[2]}});
-    
-    assert(issame(std::vector<std::vector<std::string>>{{output[0]}}, std::vector<std::vector<std::string>>{{output[0]}, {output[1], output[2]}}));
+    a({{output[0]}}, {{output[1], output[2]} });
 }
