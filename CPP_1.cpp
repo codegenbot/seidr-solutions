cb5b40847502d86e656fda1130ed03a20430b1c3
beep boop <githubbot@vadim.me>
2024-07-12 13:03:07 +0200

    Solution 6 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/oianif5i Prompt:    25 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 1fc8e13f-1573-4a11-b4ef-b3905a8ec916.cpp:25:44: error: ‘b’ was not declared in t his scope    25 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1fc8e13f-1573-4a11-b4ef-b3905a8ec916.cpp:25:45: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1fc8e13f-1573-4a11-b4ef-b3905a8ec916.cpp:24: 1fc8e13f-1573-4a11-b4ef-b3905a8ec916.cpp: In function ‘int main()’: 1fc8e13f-1573-4a11-b4ef-b3905a8ec916.cpp:34:20: error: ‘separate_paren_groups’ w as not declared in this scope    34 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 1fc8e13f-1573-4a11-b4ef-b3905a8ec916.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,36 +1,35 @@
-```
-#include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
+```cpp
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
+}
 
 vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
-    string temp = "";
-    int count = 0;
-
-    for (char c : paren_string) {
-        if (c == '(') {
-            count++;
-            temp += c;
-        } else if (c == ')') {
-            count--;
-            temp += c;
-            if (count == 0) {
-                result.push_back(temp);
-                temp = "";
+    int start = 0;
+    for (int i = 0; i < paren_string.size(); ++i) {
+        if (paren_string[i] == '(') {
+            if (start == 0)
+                start = i + 1;
+            else if (paren_string[i] == ')') {
+                result.push_back(paren_string.substr(start - 1, i - start + 2));
+                start = 0;
+            }
+        } else if (start > 0) {
+            if (paren_string[i] == ')') {
+                result.push_back(paren_string.substr(start - 1, i - start + 1));
+                start = 0;
             }
         }
     }
-
+    if (start > 0)
+        result.push_back(paren_string.substr(start - 1));
     return result;
 }
 
 int main() {
-    string paren_string = "( ) (( )) (( )( ))";
-    vector<string> result = separate_paren_groups(paren_string);
-
-    for (string s : result) {
-        cout << s << endl;
-    }
+    string paren_string;
+    cin >> paren_string;
+    vector<string> separate_paren = separate_paren_groups(paren_string);
+    // You could do something with separate_paren here
+    return 0;
+}
