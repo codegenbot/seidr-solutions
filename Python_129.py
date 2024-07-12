254fd383453c8e445a5f412508457aeaad2400c1
beep boop <githubbot@vadim.me>
2024-07-12 23:47:12 +0200

    Solution 42 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/xv1dkpvw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/080a91e2-6daf-433c-91e1-4d92036e4 ef5.py", line 39, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/080a91e2-6daf-433c-91e1-4d92036e4 ef5.py", line 36, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/080a91e2-6daf-433c-91e1-4d92036e4 ef5.py", line 31, in minPath     return min_paths[0]            ~~~~~~~~~^^^ IndexError: list index out of range

--- Python_129.py
+++ Python_129.py
@@ -26,7 +26,7 @@ def minPath(grid, k):
     for i in range(n):
         for j in range(n):
             paths = dfs(i, j, [])
-            if not min_paths or len(min_paths[0]) > len(paths[0]):
+            if not min_paths or (paths and len(min_paths[0]) > k or (len(min_paths[0]) == k and paths < min_paths)):
                 min_paths = paths
 
     return min_paths[0] if min_paths else []
