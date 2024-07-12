4f3e5bee29fe325e6b42b6e3cf80571e408c047f
beep boop <githubbot@vadim.me>
2024-07-12 10:58:22 +0200

    Solution 82 of Python/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/53qrk9fm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2dfa13ed-dc85-4ded-91fd-e3c1f92ed 810.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_128.py
+++ Python_128.py
@@ -1,4 +1,1 @@
-def solve(grades):
-    averages = [sum(student) / len(student) for student in grades]
-    medians = [sorted(student)[len(student)//2] for student in grades]
-    return f"Average: {sum(averages) / len(averages)}, Median: {sum(medians) / len(medians)}"
+return f"Average: {sum(averages) / len(averages)}, Median: {sum(medians) / len(medians)}"
