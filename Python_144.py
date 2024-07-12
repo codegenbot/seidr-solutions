9e1e72fc6674ea419d6c818b22f67392a2200de2
beep boop <githubbot@vadim.me>
2024-07-12 08:32:51 +0200

    Solution 13 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/nb0hoatt Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eab072b3-1d9d-4a0b-9a0f-610701216 20c.py", line 19, in <module>     check(simplify)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eab072b3-1d9d-4a0b-9a0f-610701216 20c.py", line 16, in check     assert simplify("1/5", "1/5") == False, "test13"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test13

--- Python_144.py
+++ Python_144.py
@@ -1,9 +1,15 @@
 def simplify(x, n):
-    x_num, x_denom = map(int, [i for i in x.split("/")])
-    n_num, n_denom = map(int, [i for i in n.split("/")])
+    x_num, x_denom = map(int, [i for i in x.split('/')])
+    n_num, n_denom = map(int, [i for i in n.split('/')])
 
-    common_divisor = abs(x_num // math.gcd(x_num, x_denom)) * abs(
-        x_denom // math.gcd(x_num, x_denom)
-    )
+    common_divisor = min(abs(x_num), abs(x_denom), abs(n_num), abs(n_denom))
 
-    return str(x_num // common_divisor) == str(n_num // common_divisor)
+    gcd = lambda a, b: b if not a else gcd(b % a, a)
+    
+    x_gcd = gcd(x_num, x_denom)
+    n_gcd = gcd(n_num, n_denom)
+
+    new_x_num, new_x_denom = x_num//x_gcd, x_denom//x_gcd
+    new_n_num, new_n_denom = n_num//n_gcd, n_denom//n_gcd
+
+    return str(new_x_num) + "/" + str(new_x_denom) == str(new_n_num) + "/" + str(new_n_denom)
