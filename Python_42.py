b3dd090b3e342ed6532f4f01bffd03ea10949430
beep boop <githubbot@vadim.me>
2024-07-12 09:47:29 +0200

    Solution 59 of Python/42, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/dhyq0f8s Prompt: [2, 3, 4, 5, 6] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cb2ff587-48a0-450b-aceb-04e99bb25 f27.py", line 16, in <module>     check(incr_list)           ^^^^^^^^^ NameError: name 'incr_list' is not defined

--- Python_42.py
+++ Python_42.py
@@ -1,1 +1,11 @@
-return [num + 1 for num in l]
+def incr_list(l):
+    return [num + 1 for num in l]
+
+
+def check(func):
+    input_list = [1, 2, 3, 4, 5]
+    output_list = func(input_list)
+    return output_list
+
+
+print(check(incr_list))
