aeb5d37692cb23e514254482a8b9a6349332fa13
beep boop <githubbot@vadim.me>
2024-07-12 14:17:56 +0200

    Solution 5 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zj95medv Prompt:    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 179e7b04-b449-49b1-988c-672487a47bfb.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 179e7b04-b449-49b1-988c-672487a47bfb.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 179e7b04-b449-49b1-988c-672487a47bfb.cpp:9: 179e7b04-b449-49b1-988c-672487a47bfb.cpp: In function ‘int main()’: 179e7b04-b449-49b1-988c-672487a47bfb.cpp:19:20: error: ‘incr_list’ was not decla red in this scope    19 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ 179e7b04-b449-49b1-988c-672487a47bfb.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,25 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <cassert>
-
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-std::vector<int> incr_list(std::vector<int> l){
-    std::vector<int> result;
-    for(int i: l) {
-        result.push_back(i+1);
-    }
-    return result;
-}
+#include <assert.h>
+#include <vector>
 
 int main() {
-    std::vector<int> list = {1, 2, 3};
-    std::vector<int> incr_list_val = incr_list(list);
-    assert(issame(list, incr_list_val));
-    for(int i: incr_list_val) {
-        std::cout << i << " ";
-    }
+    assert(issame(incr_list({1, 2, 3}), {2, 3, 4}));
     return 0;
 }
