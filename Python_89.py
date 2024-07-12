a519b2364c9fbfd1a98e93fdb2c8ec5152fdc87c
beep boop <githubbot@vadim.me>
2024-07-12 03:03:46 +0200

    Solution 2 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2orby90f Prompt: Complete the following code given the task description and function signature.

--- Python_89.py
+++ Python_89.py
@@ -4,8 +4,7 @@ def encrypt(s):
     for char in s:
         if char.isalpha():
             ascii_offset = 97 if char.islower() else 65
-            encrypted_char = chr((ord(char) - ascii_offset + 2 * 2) % 26 + ascii_offset)
-            result += encrypted_char
+            result += chr((ord(char) - ascii_offset + 2*2) % 26 + ascii_offset)
         else:
             result += char
     return result
