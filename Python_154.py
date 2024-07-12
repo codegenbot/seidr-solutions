2d6cd3b5120f279dc1ee65ee0b845adc7abfb893
beep boop <githubbot@vadim.me>
2024-07-12 08:48:00 +0200

    Solution 29 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/427npmcm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a4d8cfa0-8336-4293-be0a-4cec97f1c a4d.py", line 15, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a4d8cfa0-8336-4293-be0a-4cec97f1c a4d.py", line 12, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,6 +1,9 @@
+```
 def cycpattern_check(a, b):
+    a = ''.join(e for e in a if e.isalnum()).lower()
+    b = ''.join(e for e in b if e.isalnum()).lower()
     for i in range(len(a)):
-        temp = a[i:][::-1] + a[:i]
-        if temp == b:
+        temp = a[i:] + a[:i]
+        if sorted(temp) == sorted(b):
             return True
     return False
