7d6e11c22f12ab353b88c2037c0262e3fbc217b2
beep boop <githubbot@vadim.me>
2024-07-12 11:22:49 +0200

    Solution 95 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/d8t6ottx Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ccb7f06e-f0f9-4f76-99a1-f9b069540 f7a.py", line 4, in <module>     numbers_list = [int(x) for x in numbers_list]                                     ^^^^^^^^^^^^ NameError: name 'numbers_list' is not defined

--- Python_26.py
+++ Python_26.py
@@ -1,7 +1,7 @@
 def remove_duplicates(numbers_list):
     return sorted(list(set(numbers_list)))
 
-if __name__ == "__main__":
-    numbers_list = list(map(int, input().split()))
-    result = remove_duplicates(numbers_list)
-    print(result)
+
+numbers_list = list(map(int, input().split()))
+result = remove_duplicates(numbers_list)
+print(result)
