8ecb35fb70afe908aebc303a935910b1d488691c
beep boop <githubbot@vadim.me>
2024-07-12 14:10:19 +0200

    Solution 32 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/aybhzbcy Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/21ea4d35-afd2-47eb-ba52-457dfbe87 f6f.py", line 9, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/21ea4d35-afd2-47eb-ba52-457dfbe87 f6f.py", line 6, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,3 +1,3 @@
 ```
 def match_parens(lst):
-    return 'Yes' if all(s.count('(') == s.count(')') for s in lst) else 'No'
+    return 'Yes' if sum(1 for s in lst if s.count('(') != s.count(')')) == 0 and all(s.count('(') <= s.count(')') for s in lst) else 'No'
