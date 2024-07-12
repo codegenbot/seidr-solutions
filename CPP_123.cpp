ef5ee28fdc24fa900499cbfe9626f37921e26a61
beep boop <githubbot@vadim.me>
2024-07-12 22:48:40 +0200

    Solution 31 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/3w4c54xg Prompt:  this scope; did you mean ‘std::end’?    24 |     for (int i : res) {       |                  ^~~       |                  std::end In file included from /usr/include/c++/11/string:54,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 14aa6ee4-99bc-44e0-a40c-f9fa01833dc3.cpp:1: /usr/include/c++/11/bits/range_access.h:110:37: note: ‘std::end’ declared here   110 |   template<typename _Tp> const _Tp* end(const valarray<_Tp>&) noexcept;       |                                     ^~~ 14aa6ee4-99bc-44e0-a40c-f9fa01833dc3.cpp: At global scope: 14aa6ee4-99bc-44e0-a40c-f9fa01833dc3.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 14aa6ee4-99bc-44e0-a40c-f9fa01833dc3.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -4,25 +4,24 @@
 
 using namespace std;
 
-vector<int> get_odd_collatz(int n) {
+vector<int> get_odd_collatz(int) {
     vector<int> result;
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
     while (n != 1) {
         if (n % 2 == 0) {
             n /= 2;  
         } else {
             result.push_back(n);
         }
+        n = (n * 3) + 1;
     }
     return result;
 }
 
-int main(); {
-    int n;
-    cout << "Enter a positive integer: ";
-    cin >> n;
-    vector<int> res = get_odd_collatz(n);
-    for (int i : res) {
-        cout << i << " ";
-    }
+int main() {
+    get_odd_collatz(0);
     cout << endl;
+    return 0;
 }
