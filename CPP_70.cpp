d95545222153537ca97787d3747f74af32adfaef
beep boop <githubbot@vadim.me>
2024-07-12 02:22:50 +0200

    Solution 62 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/nfplvphz Prompt:       |                           ^~~~~~ 9aa2e672-d902-436b-83f0-e6f6ab2ad409.cpp:30:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 9aa2e672-d902-436b-83f0-e6f6ab2ad409.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 9aa2e672-d902-436b-83f0-e6f6ab2ad409.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9aa2e672-d902-436b-83f0-e6f6ab2ad409.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9aa2e672-d902-436b-83f0-e6f6ab2ad409.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 9aa2e672-d902-436b-83f0-e6f6ab2ad409.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -11,9 +11,9 @@ std::vector<int> strange_sort_list(std::vector<int> lst){
     std::vector<int> result;
     int left = 0, right = lst.size() - 1;
     while (left <= right) {
-        result.push_back(lst[right]);
+        result.push_back(lst[left]);
         if (left != right) {
-            result.push_back(lst[left]);
+            result.push_back(lst[right]);
         }
         left++;
         right--;
@@ -21,7 +21,7 @@ std::vector<int> strange_sort_list(std::vector<int> lst){
     return result;
 }
 
-int main() {
+int main(){
     assert(issame(strange_sort_list({111111}), {111111}));
     return 0;
 }
