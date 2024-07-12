b833ec8520189eb854438df847b25cd38a986beb
beep boop <githubbot@vadim.me>
2024-07-12 09:40:12 +0200

    Solution 57 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2chutce6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5ab59073-ef1f-4d2a-94a2-b79c36929 b40.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_88.py
+++ Python_88.py
@@ -1,16 +1,15 @@
-```
 def sort_array(arr):
     return sorted(map(int, arr))
 
 while True:
     try:
         array_str = input("Enter an array: ")
-        array = list(array_str.split())
+        array = list(map(str.strip, array_str.split()))
         if len(array) < 2:
             print("Array should have at least two elements. Please enter again.")
             continue
+        array = [int(i) for i in array]
         print(sort_array(array))
         break
     except ValueError as e:
         print(f"Invalid input. Please enter numbers separated by spaces: {str(e)}")
-        print("Please try again...")
