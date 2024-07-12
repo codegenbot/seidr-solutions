3263d516f498b1cdcba40d6fb36a2827db27b8d5
beep boop <githubbot@vadim.me>
2024-07-12 15:29:22 +0200

    Solution 58 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/owlfkvjg Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const float&}; _Tp = float; _Alloc = std::allocator<float>; std::v ector<_Tp, _Alloc>::iterator = std::vector<float>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = float; _Alloc = std::all ocator<float>; std::vector<_Tp, _Alloc>::value_type = float]’ d44acc39-18ad-4d3d-80a6-4162d27f355d.cpp:22:43:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <algorithm>
 
-bool sameVector(std::vector<float> a, std::vector<float> b) {
+bool same(std::vector<float> a, std::vector<float> b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i < a.size(); i++)
@@ -13,23 +13,23 @@ bool sameVector(std::vector<float> a, std::vector<float> b) {
 }
 
 std::vector<float> sort_even(std::vector<float> l) {
-    std::vector<float> result(l.size(), 0.0);
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) {
-            std::vector<float> even_indices;
-            for (int j = 0; j < l.size(); j++) {
-                if (j % 2 == 0) {
-                    even_indices.push_back(l[j]);
-                }
-            }
+    std::vector<float> result;
+    for (float i : l) {
+        if (result.size() % 2 == 0) {
+            std::vector<float> even_indices(result.begin(), result.end());
             std::sort(even_indices.begin(), even_indices.end());
-            result[i] = even_indices[0];
+            result.clear();
+            for (float j : even_indices) {
+                result.push_back(j);
+            }
         } else {
-            result[i] = l[i];
+            result.push_back(i);
         }
     }
     return result;
 }
 
 int main() {
-    assert(sameVector({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}, sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}));
+    assert(same(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    return 0;
+}
