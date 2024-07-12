5592bdebcd9cb0d917bb7f6655b0b86e130ae797
beep boop <githubbot@vadim.me>
2024-07-12 03:10:22 +0200

    Solution 33 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/yxeg1hwz Prompt: 7b4ef1d3-4d9c-4f82-95ab-43125f823912.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 7b4ef1d3-4d9c-4f82-95ab-43125f823912.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main(){       |     ^~~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -19,14 +19,12 @@ vector<string> odd_count(vector<string> lst){
     return result;
 }
 
-int main(){
-    vector<string> expected_result {
-        "the number of odd elements 1 in the string 271 of the input.",
-        "the number of odd elements 2 in the string 137 of the input.",
-        "the number of odd elements 2 in the string 314 of the input."
-    };
-    vector<string> result = odd_count({"271", "137", "314"});
-    assert(result == expected_result);
-    cout << "Test passed successfully!" << endl;
-    return 0;
-}
+vector<string> expected_result {
+    "the number of odd elements 1 in the string 271 of the input.",
+    "the number of odd elements 2 in the string 137 of the input.",
+    "the number of odd elements 2 in the string 314 of the input."
+};
+vector<string> result = odd_count({"271", "137", "314"});
+assert(result == expected_result);
+cout << "Test passed successfully!" << endl;
+return 0;
