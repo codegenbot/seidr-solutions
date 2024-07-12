f5ea54c29ea72ceec8f282fc6a617def22e63e4d
beep boop <githubbot@vadim.me>
2024-07-12 09:13:44 +0200

    Solution 46 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2chutce6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f1d16d91-92ae-4a0a-951b-e6a18bf2c 91a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_88.py
+++ Python_88.py
@@ -1,9 +1,10 @@
+```
 def sort_array(arr):
     return sorted(arr)
 
 while True:
     try:
-        array = list(map(int, input("Enter an array: \"separated by spaces\": ").split()))
+        array = list(map(int, input("Enter an array: ").split()))
         print(sort_array(array))
         break
     except ValueError:
