42e9f88315c207d21e159bc19eb8793425f86b1e
beep boop <githubbot@vadim.me>
2024-07-12 06:17:35 +0200

    Solution 12 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/4ehkbl5f Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d273732f-063b-473d-8da0-90d4438b1 4c7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_37.py
+++ Python_37.py
@@ -1,10 +1,7 @@
-"""
-Given a list of integers, sort the even numbers first, then the odd numbers.
-"""
-
 from itertools import zip_longest
+
 
 def sort_even(l: list):
     evens = sorted([i for i in l if i % 2 == 0])
-    odds = sorted([i for i in l if i % 2 != 0])
-    return list(zip_longest(evens, odds, fillvalue=0))
+    odd_l = [i for i in l if i % 2 != 0]
+    return list(zip_longest(evens, odd_l, fillvalue=0))
