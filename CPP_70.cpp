39ed4038b8749452e106bc3d94d421375b5c0b1c
beep boop <githubbot@vadim.me>
2024-07-12 17:15:08 +0200

    Solution 81 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1o525827 Prompt: In file included from /usr/include/c++/11/vector:67,                  from 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp:24: 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp: In function ‘int main()’: 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -3,27 +3,40 @@
 #include <algorithm>
 
 bool areEqualVectors(std::vector<int> const &a, std::vector<int> const &b) {
-    if (a.size() != b.size()) {
+    if (a.size() != b.size())
         return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
+    for (int i = 0; i < a.size(); ++i)
+        if (a[i] != b[i])
             return false;
-        }
-    }
     return true;
 }
 
 std::vector<int> strange_sort_list(std::vector<int> lst) {
-    std::sort(lst.begin(), lst.end());
+    std::vector<int> result;
     while (!lst.empty()) {
-        int first = lst[0];
-        int last = lst.back();
-        result.push_back(first);
-        lst.erase(lst.begin());
-        if (first == last) {
-            return result;
+        bool isSorted = false;
+        do {
+            isSorted = std::stable_partition(lst.begin(), lst.end(),
+                [ & ] (int x) { return x < 0; }) - lst.begin() == 0;
+            if (!isSorted)
+                std::sort(lst.begin(), lst.end());
+        } while (!isSorted);
+        if (lst.front() > 0) {
+            result.push_back(lst[0]);
+            lst.erase(lst.begin());
+        } else {
+            result.push_back(-lst.back());
+            lst.pop_back();
         }
     }
     return result;
+}
+
+int main() {
+    std::vector<int> lst = { -5, -3, 0, 2, 4 };
+    if (areEqualVectors(lst, strange_sort_list(lst))) {
+        for (auto x : lst)
+            std::cout << x << " ";
+        std::cout << "\n";
+    }
 }
