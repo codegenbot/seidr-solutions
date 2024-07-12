1eaf7aa2f48674a3aa3f209d244684448c608b74
beep boop <githubbot@vadim.me>
2024-07-12 04:14:05 +0200

    Solution 25 of CPP/120, ratio 0.5454545454545454 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/fa8grmmn Prompt: c69e4d59-aec6-4829-af0d-aa071c1c5732.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ c69e4d59-aec6-4829-af0d-aa071c1c5732.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -10,9 +10,3 @@ vector<int> maximum(vector<int> arr, int k) {
     vector<int> result(arr.begin(), arr.begin() + k);
     return result;
 }
-
-int main() {
-    assert(maximum({1, 2, 3, -23, 243, -400, 0}, 0) == vector<int>{});
-    
-    return 0;
-}
