e3d8597820c2ac68a41e9e1fb64efda6978d05c4
beep boop <githubbot@vadim.me>
2024-07-12 15:26:55 +0200

    Solution 57 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/owlfkvjg Prompt: on before ‘float’    39 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 465a1181-3e19-4257-b841-9338cbf897c6.cpp:39:43: error: expression list treated a s compound expression in initializer [-fpermissive]    39 | bool issame(vector<float> a,vector<float>b){       |                                           ^ 465a1181-3e19-4257-b841-9338cbf897c6.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 465a1181-3e19-4257-b841-9338cbf897c6.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~ In file included from 465a1181-3e19-4257-b841-9338cbf897c6.cpp:38: 465a1181-3e19-4257-b841-9338cbf897c6.cpp: In function ‘int main()’: 465a1181-3e19-4257-b841-9338cbf897c6.cpp:48:19: error: ‘issame’ cannot be used a s a function    48 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <algorithm>
 
-bool equalVectors(std::vector<float> a, std::vector<float> b) {
+bool sameVector(std::vector<float> a, std::vector<float> b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i < a.size(); i++)
@@ -12,18 +12,18 @@ bool equalVectors(std::vector<float> a, std::vector<float> b) {
     return true;
 }
 
-std::vector<float> sortEven(std::vector<float> l) {
+std::vector<float> sort_even(std::vector<float> l) {
     std::vector<float> result(l.size(), 0.0);
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            std::vector<float> evenIndices;
+            std::vector<float> even_indices;
             for (int j = 0; j < l.size(); j++) {
                 if (j % 2 == 0) {
-                    evenIndices.push_back(l[j]);
+                    even_indices.push_back(l[j]);
                 }
             }
-            std::sort(evenIndices.begin(), evenIndices.end());
-            result[i] = evenIndices[0];
+            std::sort(even_indices.begin(), even_indices.end());
+            result[i] = even_indices[0];
         } else {
             result[i] = l[i];
         }
@@ -32,6 +32,4 @@ std::vector<float> sortEven(std::vector<float> l) {
 }
 
 int main() {
-    assert(equalVectors(sortEven({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
-    return 0;
-}
+    assert(sameVector({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}, sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}));
