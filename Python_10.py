6fcfe4b47450ee6d563f834f572cd4f51b66e057
beep boop <githubbot@vadim.me>
2024-07-12 13:31:32 +0200

    Solution 62 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/9n0goacs Prompt: Complete the following code given the task description and function signature.

--- Python_10.py
+++ Python_10.py
@@ -1,12 +1,13 @@
-```
 def is_palindrome(string: str) -> bool:
+    """Test if given string is a palindrome"""
     return string == string[::-1]
 
+
 def make_palindrome(string: str) -> str:
-    if string == string[::-1]:
-        return string + string[::-1]
-    for i in range(len(string)):
-        palindromic_suffix = string[i:]
-        if palindromic_suffix == palindromic_suffix[::-1]:
-            return string + palindromic_suffix[::-1]
-    return string + string[::-1]
+    s = string.lower()
+    for i in range(len(s), 0, -1):
+        prefix = s[:i]
+        postfix = s[i - 1 :: -1]
+        if is_palindrome(prefix + postfix):
+            return string + postfix
+    return string + s[::-1]
