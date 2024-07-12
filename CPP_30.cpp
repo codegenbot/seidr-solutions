86c6cb0f96e9fd7e5b15cce7d5e91789a759f2d0
beep boop <githubbot@vadim.me>
2024-07-12 10:33:45 +0200

    Solution 31 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/zoj3wa6d Prompt:       |                    ^~~~~ 2c508912-af69-49a0-ac18-3674f1274135.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 2c508912-af69-49a0-ac18-3674f1274135.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 2c508912-af69-49a0-ac18-3674f1274135.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 2c508912-af69-49a0-ac18-3674f1274135.cpp:11: 2c508912-af69-49a0-ac18-3674f1274135.cpp: In function ‘int main()’: 2c508912-af69-49a0-ac18-3674f1274135.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 2c508912-af69-49a0-ac18-3674f1274135.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,16 +1,6 @@
 #include <vector>
 
-bool issame(vector<float> a, vector<float> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
+bool issame(vector<float> a, vector<float> b);
 
 vector<float> get_positive(vector<float> l){
     vector<float> result;
@@ -22,9 +12,19 @@ vector<float> get_positive(vector<float> l){
     return result;
 }
 
+bool issame(vector<float> a, vector<float> b){
+    return a == b;
+}
+
 int main() {
-    vector<float> a = {1.2, 3.4, -2.7, 5.6};
-    vector<float> b = get_positive(a);
-    bool same = issame(a, b);
+    vector<float> vec1 = {1.1, 2.2, 3.3};
+    vector<float> vec2 = {1.1, 2.2, 3.3};
+
+    if(issame(vec1, vec2)){
+        cout << "Vectors are the same" << endl;
+    } else{
+        cout << "Vectors are different" << endl;
+    }
+
     return 0;
 }
