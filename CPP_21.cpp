cd988b9bf0ebe2e24e3e902c687763b8df979a01
beep boop <githubbot@vadim.me>
2024-07-12 10:02:24 +0200

    Solution 27 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/36ik5qai Prompt:    14 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ ad8d6fbf-0f7e-42da-b3b9-7822e9df1d43.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ ad8d6fbf-0f7e-42da-b3b9-7822e9df1d43.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from ad8d6fbf-0f7e-42da-b3b9-7822e9df1d43.cpp:13: ad8d6fbf-0f7e-42da-b3b9-7822e9df1d43.cpp: In function ‘int main()’: ad8d6fbf-0f7e-42da-b3b9-7822e9df1d43.cpp:23:20: error: ‘rescale_to_unit’ was not  declared in this scope    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ ad8d6fbf-0f7e-42da-b3b9-7822e9df1d43.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -8,5 +8,7 @@ bool issame(vector<float> a, vector<float> b) {
         result.push_back(rescaled_num);
     }
     
-    return result == b;
+    return result;
 }
+
+assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), {0.25, 0.0, 1.0, 0.5, 0.75}));
