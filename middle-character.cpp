c03f67bcf6c666ce94ef705b62545820c790ecf7
beep boop <githubbot@vadim.me>
2024-07-12 15:10:03 +0200

    Solution 47 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ag0c73oo Prompt: 646418b6-9f42-4a61-8ee8-98667124fd97.cpp:12:15: error: expected ‘;’ before ‘mid2 ’    12 |         string mid2 = s.substr(len / 2, 1);       |               ^~~~~       |               ; 646418b6-9f42-4a61-8ee8-98667124fd97.cpp:13:51: error: ‘mid1’ was not declared i n this scope    13 |         std::cout << "Middle characters are: " << mid1 + mid2 << std::en dl;       |                                                   ^~~~ 646418b6-9f42-4a61-8ee8-98667124fd97.cpp:13:58: error: ‘mid2’ was not declared i n this scope    13 |         std::cout << "Middle characters are: " << mid1 + mid2 << std::en dl;       |                                                          ^~~~ 646418b6-9f42-4a61-8ee8-98667124fd97.cpp:15:15: error: expected ‘;’ before ‘mid’    15 |         string mid = s.substr(len / 2, 1);       |               ^~~~       |               ; 646418b6-9f42-4a61-8ee8-98667124fd97.cpp:16:49: error: ‘mid’ was not declared in  this scope    16 |         std::cout << "Middle character is: " << mid << std::endl;       |                                                 ^~~

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,4 +1,3 @@
-```
 #include <string>
 #include <iostream>
 
@@ -9,9 +8,9 @@ int main() {
 
     int len = s.length();
     if (len % 2 == 0) {
-        string mid1 = s.substr(len / 2 - 1, 1);
-        string firstMid = s.substr(len / 2, 1);
-        std::cout << "Middle characters are: " << mid1 + firstMid << std::endl;
+        string firstMid = s.substr(len / 2 - 1, 1);
+        string secondMid = s.substr(len / 2, 1);
+        std::cout << "Middle characters are: " << firstMid + secondMid << std::endl;
     } else {
         string middleChar = s.substr(len / 2, 1);
         std::cout << "Middle character is: " << middleChar << std::endl;
