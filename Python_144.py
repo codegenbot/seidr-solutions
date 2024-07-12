950820f5c65c7d28351fecaadb85c2d7ab64597d
beep boop <githubbot@vadim.me>
2024-07-12 08:30:07 +0200

    Solution 4 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/nb0hoatt Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/22bdf09c-bb22-4299-8208-ada96d85c c28.py", line 15, in <module>     check(simplify)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/22bdf09c-bb22-4299-8208-ada96d85c c28.py", line 12, in check     assert simplify("1/5", "1/5") == False, "test13"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test13

--- Python_144.py
+++ Python_144.py
@@ -1,8 +1,12 @@
-```Python
+```
 def simplify(x, n):
     x_num, x_denom = map(int, [i for i in x.split('/')])
     n_num, n_denom = map(int, [i for i in n.split('/')])
 
-    common_divisor = min(abs(x_num), abs(x_denom), abs(n_num), abs(n_denom))
-    
-    return f"{x_num//common_divisor}/{x_denom//common_divisor}"
+    if (x_num * n_denom) % ((x_denom * n_num)) == 0:
+        return True
+    else:
+        common_divisor = max((abs(x_num), x_denom), key=abs).gcd(abs(n_num))
+        x_new_num, x_new_denom = x_num // common_divisor, x_denom // common_divisor
+        n_new_num, n_new_denom = n_num // common_divisor, n_denom // common_divisor
+        return f"{x_new_num}/{x_new_denom}" == f"{n_new_num}/{n_new_denom}"
