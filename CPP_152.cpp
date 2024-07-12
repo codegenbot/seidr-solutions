f60eaedbc1b09763f2bb43d36510bc77610563de
beep boop <githubbot@vadim.me>
2024-07-12 23:18:29 +0200

    Solution 1 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uswfokmt Prompt:       |                    ^~~ d17af32e-70fe-404d-84ef-c0ac254af9e2.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d17af32e-70fe-404d-84ef-c0ac254af9e2.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d17af32e-70fe-404d-84ef-c0ac254af9e2.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d17af32e-70fe-404d-84ef-c0ac254af9e2.cpp:13: d17af32e-70fe-404d-84ef-c0ac254af9e2.cpp: In function ‘int main()’: d17af32e-70fe-404d-84ef-c0ac254af9e2.cpp:23:20: error: ‘compare’ was not declare d in this scope    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ d17af32e-70fe-404d-84ef-c0ac254af9e2.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,10 +1,23 @@
+#include <vector>
+bool issame(vector<int> a,vector<int>b){
+    for(int i = 0; i < a.size();i++){
+        if(a[i] != b[i]) return false;
+    }
+    return true;
+}
 vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
-    for (int i = 0; i < game.size(); i++) {
-        if (game[i] == guess[i]) {
-            result.push_back(0);
-        } else {
-            result.push_back(abs(game[i] - guess[i]));
+    if(issame(game,guess)){
+        for (int i = 0; i < game.size(); i++) {
+            result.push_back(2);
+        }
+    } else {
+        for (int i = 0; i < game.size(); i++) {
+            if (game[i] == guess[i]) {
+                result.push_back(1);
+            } else {
+                result.push_back(0);
+            }
         }
     }
     return result;
