f046b8785e25e3701611c538599075b3b2e5d9b6
beep boop <githubbot@vadim.me>
2024-07-12 13:57:51 +0200

    Solution 21 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/zi3xq93h Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c66ea8bb-b2d1-4276-8128-9f984795a 538.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -1,19 +1,23 @@
 def minPath(grid, k):
     n = len(grid)
     res = []
-    
+
     def dfs(i, j, path):
         if len(path) == k:
             nonlocal res
             res = sorted(path)
             return
-        
-        for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
-            if 0 <= x < n and 0 <= y < n and (x, y) not in set(zip(*[grid[:i], grid[i:]])):
+
+        for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+            if (
+                0 <= x < n
+                and 0 <= y < n
+                and (x, y) not in set(zip(*[grid[:i], grid[i:]]))
+            ):
                 dfs(x, y, path + [grid[x][y]])
-    
+
     for i in range(n):
         for j in range(n):
             dfs(i, j, [grid[i][j]])
-    
+
     return res
