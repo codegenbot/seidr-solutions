cfe7a92aac843a59c4b757b50b2f7b64454a6a9e
beep boop <githubbot@vadim.me>
2024-07-12 10:47:16 +0200

    Solution 95 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/3xd4xzwr Prompt: [[5, 6, 3], [2, 1, 7], [4, 8, 9]] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/01bb87ce-f22f-4c53-9269-2268f5625 b2a.py", line 15, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/01bb87ce-f22f-4c53-9269-2268f5625 b2a.py", line 10, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,10 +1,8 @@
-from typing import List
-
-def sort_third(l: List[List[int]]):
+def sort_third(lst: list[list[int]]) -> list[list[int]]:
     try:
-        return sorted(l, key=lambda x: x[2])
+        return sorted(lst, key=lambda x: x[2])
     except (IndexError, TypeError):
         return []
 
 # Test the function with a list of lists
-print(sort_third([[5, 6, 3], [4, 8, 9], [2, 1, 7]))
+print(sort_third([[5, 6, 3], [4, 8, 9], [2, 1, 7]]))
