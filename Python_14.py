3d5a5c310bba71fa23b8a76eff50621758af6896
beep boop <githubbot@vadim.me>
2024-07-12 18:17:19 +0200

    Solution 25 of Python/14, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/0gmlr3mn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cb058376-d8c0-4a3a-825c-99c39d6e5 efc.py", line 2     return [input_string[:i+1] for i in range(len(input_string)]                                                                ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '('

--- Python_14.py
+++ Python_14.py
@@ -1,3 +1,2 @@
-input_string = input("Enter a string: ")
-result = [input_string[:i+1] for i in range(len(input_string))]
-print(result)
+def all_prefixes(input_string):
+    return [input_string[:i+1] for i in range(len(input_string))]
