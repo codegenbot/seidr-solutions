fcd0f2ca07e5886b96185f82451a8e18823df88b
beep boop <githubbot@vadim.me>
2024-07-12 06:25:21 +0200

    Solution 58 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/tkzpmhhy Prompt:   +++ |+#include <cstddef>     1 | double calculateEuclideanDistance(const std::vector<double>& vec1, const  std::vector<double>& vec2) { 3a3b4562-4fc4-420d-bb74-b9c7632792a5.cpp:3:24: error: ‘i’ was not declared in th is scope     3 |     for (size_t i = 0; i < vec1.size(); ++i) {       |                        ^ 3a3b4562-4fc4-420d-bb74-b9c7632792a5.cpp:3:28: error: ‘vec1’ was not declared in  this scope     3 |     for (size_t i = 0; i < vec1.size(); ++i) {       |                            ^~~~ 3a3b4562-4fc4-420d-bb74-b9c7632792a5.cpp:4:21: error: ‘pow’ is not a member of ‘ std’     4 |         sum += std::pow(vec1[i] - vec2[i], 2);       |                     ^~~ 3a3b4562-4fc4-420d-bb74-b9c7632792a5.cpp:4:35: error: ‘vec2’ was not declared in  this scope     4 |         sum += std::pow(vec1[i] - vec2[i], 2);       |                                   ^~~~ 3a3b4562-4fc4-420d-bb74-b9c7632792a5.cpp:6:17: error: ‘sqrt’ is not a member of ‘std’     6 |     return std::sqrt(sum);       |                 ^~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,9 +1,10 @@
-#include <vector>
 #include <cmath>
+#include <vector>
 
 double calculateEuclideanDistance(const std::vector<double>& vec1, const std::vector<double>& vec2) {
     double sum = 0.0;
-    for (size_t i = 0; i < vec1.size(); ++i) {
+    size_t i;
+    for (i = 0; i < vec1.size(); ++i) {
         sum += std::pow(vec1[i] - vec2[i], 2);
     }
     return std::sqrt(sum);
