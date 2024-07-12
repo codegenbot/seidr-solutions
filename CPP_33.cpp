da6f3fcdc1b44ad7f20c087b15e10de0c93649f1
beep boop <githubbot@vadim.me>
2024-07-12 14:22:14 +0200

    Solution 11 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d8rx6wrb Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from b78e6c71-2216-4ef3-b800-a624373c2418.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,16 +1,16 @@
 #include <algorithm>
 #include <vector>
 
-std::vector<int> sort_third(std::vector<int> l) {
-    std::vector<int> result;
+vector<int> sort_third(vector<int> l) {
+    vector<int> result;
     for (int i = 0; i < l.size(); i++) {
         if (i % 3 == 0) {
             // values at the indices that are divisible by three are equal to the values of the corresponding indicies of l, but sorted
-            std::vector<int> temp;
+            vector<int> temp;
             for (int j = i; j < l.size() && j % 3 == 0; j++) {
                 temp.push_back(l[j]);
             }
-            std::sort(temp.begin(), temp.end());
+            sort(temp.begin(), temp.end());
             for (int k = 0; k < temp.size(); k++) {
                 result.push_back(temp[k]);
             }
