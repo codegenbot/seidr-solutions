7bab7a3647db6b66782a2ae2c3e54fadefbcdcb3
beep boop <githubbot@vadim.me>
2024-07-12 08:25:32 +0200

    Solution 23 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/p0oo0rpk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7348036c-2ebe-4feb-bd50-780077594 e2b.py", line 10, in <module>     check(minPath)           ^^^^^^^ NameError: name 'minPath' is not defined

--- Python_129.py
+++ Python_129.py
@@ -1,5 +1,2 @@
-```
 def min_length(all_paths):
-    return min(sum(int(digit) for digit in path) for path in all_paths)
-
-print(min_length(['123', '456', '789']))
+    return min(sum(int(path) for path in all_paths))
