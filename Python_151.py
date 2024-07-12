df334029d4cf89e05d6e961dda27d4b078636fbb
beep boop <githubbot@vadim.me>
2024-07-12 15:43:08 +0200

    Solution 66 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gtr1sg0k Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf6ea90e-329c-4a5b-bf1e-b28439a6e 945.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf6ea90e-329c-4a5b-bf1e-b28439a6e 945.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -3,11 +3,12 @@ def double_the_difference(lst):
     return sum(i**2 for i in lst if isinstance(i, int) and i > 0) * 2
 
 def check():
-    lst = [1, 3, -1, 5, 6]
+    lst1 = [1, 3, -1, 5, 6]
     odd_sum = 25
-    if double_the_difference(lst) == odd_sum:
-        print("True")
-    else:
-        print("False")
+    lst2 = [4, 9, 8]
+    result1 = double_the_difference(lst1)
+    result2 = double_the_difference(lst2)
+    print("For list 1: " + str(result1 == odd_sum))
+    print("For list 2: " + str(result2 == odd_sum))
 
 check()
