a07ca80c8809600860589cf85645327da3f52f02
beep boop <githubbot@vadim.me>
2024-07-12 09:07:07 +0200

    Solution 13 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/y0gq3031 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d4588c0a-fb7e-40cd-80c8-abc57666e baf.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,15 +1,11 @@
-```
 def sum_squares(lst):
     total_sum = 0
     for i in range(len(lst)):
-        if i % 3 == 0 and i % 4 != 0:
+        if (i + 1) % 3 == 0 and (i + 1) % 4 != 0:
             total_sum += lst[i] ** 2
-        elif i % 4 == 0 and i % 3 != 0:
+        elif (i + 1) % 4 == 0 and (i + 1) % 3 != 0:
             total_sum += lst[i] ** 3
     return total_sum
 
 lst = input("Enter a list of numbers separated by space: ").split()
-if lst:
-    print(sum_squares(list(map(int, lst))))
-else:
-    print("Invalid input")
+print(sum_squares(list(map(int, lst))))
