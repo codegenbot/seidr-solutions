b3c6d77f2434c7af1447c132d67b4841d1611550
beep boop <githubbot@vadim.me>
2024-07-12 19:30:22 +0200

    Solution 30 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/6crlcyz6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1ec5b2bd-15c5-44f9-bc8a-bed1871c2 35d.py", line 5     return result     ^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_5.py
+++ Python_5.py
@@ -1,6 +1,6 @@
-def complete_code(numbers, delimiter):
+def format_numbers(numbers, delimeter):
     result = []
     for num in numbers:
-        result.extend([num, delimiter])
+        result.extend([num, delimeter])
     result.pop()
     return result
