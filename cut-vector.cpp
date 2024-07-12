ac17036166c1d7a7added56d2c14c9e044f386cc
beep boop <githubbot@vadim.me>
2024-07-12 23:11:13 +0200

    Solution 16 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/k7mxwgg8 Prompt: er ‘<climits>’; did you forget to ‘#include <climits>’?   +++ |+#include <climits>     1 | int main() { c9923742-7f47-43d4-b9f9-215827156bb2.cpp:21:13: error: ‘abs’ was not declared in  this scope    21 |         if (abs(leftSum - rightSum) < diff) {       |             ^~~ c9923742-7f47-43d4-b9f9-215827156bb2.cpp:28:9: error: ‘cout’ was not declared in  this scope    28 |         cout << nums[i] << endl;       |         ^~~~ c9923742-7f47-43d4-b9f9-215827156bb2.cpp:28:28: error: ‘endl’ was not declared i n this scope    28 |         cout << nums[i] << endl;       |                            ^~~~ c9923742-7f47-43d4-b9f9-215827156bb2.cpp:30:5: error: ‘cout’ was not declared in  this scope    30 |     cout << endl;       |     ^~~~ c9923742-7f47-43d4-b9f9-215827156bb2.cpp:30:13: error: ‘endl’ was not declared i n this scope    30 |     cout << endl;       |             ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,39 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <cmath>
+
 int main() {
-    vector<int> nums;
+    std::vector<int> nums;
     int num;
-    
-    while (cin >> num) {
+    while (std::cin >> num) {
         nums.push_back(num);
     }
     
-    int min_diff = INT_MAX;
-    int cut_index = 0;
+    int n = nums.size();
+    int diff = INT_MAX;
+    int cutIndex = 0;
     
-    for (int i = 1; i < nums.size(); i++) {
-        int sum_left = 0;
-        int sum_right = 0;
-        
-        for (int j = 0; j < i; j++) {
-            sum_left += nums[j];
+    for (int i = 1; i < n; ++i) {
+        int leftSum = 0, rightSum = 0;
+        for (int j = 0; j < i; ++j) {
+            leftSum += nums[j];
+        }
+        for (int j = i; j < n; ++j) {
+            rightSum += nums[j];
         }
         
-        for (int j = i; j < nums.size(); j++) {
-            sum_right += nums[j];
-        }
-        
-        int diff = abs(sum_left - sum_right);
-        if (diff < min_diff) {
-            min_diff = diff;
-            cut_index = i;
+        if (std::abs(leftSum - rightSum) < diff) {
+            diff = std::abs(leftSum - rightSum);
+            cutIndex = i;
         }
     }
     
-    for (int i = 0; i < cut_index; i++) {
-        cout << nums[i] << endl;
+    for (int i = 0; i < cutIndex; ++i) {
+        std::cout << nums[i] << std::endl;
     }
-    cout << endl;
-    for (int i = cut_index; i < nums.size(); i++) {
-        cout << nums[i] << endl;
+    std::cout << std::endl;
+    for (int i = cutIndex; i < n; ++i) {
+        std::cout << nums[i] << std::endl;
     }
     
     return 0;
