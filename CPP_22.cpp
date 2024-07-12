96e19e4959ecaebb46f59452e161ae671ce39518
beep boop <githubbot@vadim.me>
2024-07-12 19:39:38 +0200

    Solution 37 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0y4a8ldc Prompt: c0ce4fc4-0c25-43b1-900e-85fecfbf764f.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,4 +1,3 @@
-```
 #include <vector>
 #include <list>
 #include <boost/any.hpp>
@@ -26,6 +25,6 @@ std::vector<int> filter_integers(std::list<boost::any> values) {
 
 int main() {
     std::list<boost::any> values = {3, boost::any('c'), 3, 3, boost::any('a'), boost::any('b')};
-    assert(is_same(filter_integers(values), {3, 3, 3}));
+    assert(is_same(filter_integers(values), std::vector<int>{3, 3, 3}));
     return 0;
 }
