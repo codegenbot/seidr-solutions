f696bfc8ee777af46c05438c88b48573e74b2e60
beep boop <githubbot@vadim.me>
2024-07-12 09:36:46 +0200

    Solution 81 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/oaf2ryzo Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b2a3fcb3-0e21-4e96-8622-5dd4c70a7 fdc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -4,11 +4,14 @@ def check(s):
     else:
         return "ODD"
 
+
 def secure_code():
     digits = input("Enter the digits separated by spaces: ")
     unique_digits = set(int(x) for x in digits.split())
-    sorted_unique_digits = str(sorted(unique_digits)).replace(', ', ' ')
+    sorted_unique_digits = str(sorted(unique_digits)).replace(", ", " ")
     sorted_list_sum = sum(unique_digits)
     is_even = sorted_list_sum % 2 == 0
     check_result = check(str(sorted_list_sum))
-    return " ".join(map(str, sorted(unique_digits))) + f" Sum: {sorted_list_sum} - {check_result}"
+    return (
+        " ".join(map(str, unique_digits)) + f" Sum: {sorted_list_sum} - {check_result}"
+    )
