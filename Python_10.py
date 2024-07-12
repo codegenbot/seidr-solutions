0d3bae9861ac3b394349b3ca42aec84e9c2fac52
beep boop <githubbot@vadim.me>
2024-07-13 01:40:50 +0200

    Solution 30 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d45c05lr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6cc67573-814e-4664-9d20-a5738e30d b7a.py", line 13, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6cc67573-814e-4664-9d20-a5738e30d b7a.py", line 10, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,8 +1,8 @@
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
-        return string + string
+        return string
     for i in range(len(string), 0, -1):
-        for char in "abcdefghijklmnopqrstuvwxyz":
-            new_string = string + char * (i - 1) + string[::-1]
-            if new_string == new_string[::-1]:
-                return new_string
+        char = "abcdefghijklmnopqrstuvwxyz"[i-1]
+        new_string = string + char + string[::-1]
+        if new_string == new_string[::-1]:
+            return new_string
