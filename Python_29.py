e3a3167001cb83d5413608993834b5989d9c3816
beep boop <githubbot@vadim.me>
2024-07-12 06:09:06 +0200

    Solution 54 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7ealauoz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d198f10-c154-4d3e-974f-cf8af2537 1b7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_29.py
+++ Python_29.py
@@ -1,4 +1,6 @@
+```
 def filter_by_prefix(prefix="", user_input=None):
+    """ 
     if user_input is None:
         strings = input('Enter a list of strings (separated by commas): ')
     else:
@@ -6,3 +8,4 @@ def filter_by_prefix(prefix="", user_input=None):
     strings_list = [s.strip() for s in strings.split(',')]
     result = [s.strip().lower() for s in strings_list if s and s.strip().lower().startswith(prefix.lower())]
     print(result)
+"""
