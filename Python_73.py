3be67f3ab98147574828b43bb4632d34e3abc5e5
beep boop <githubbot@vadim.me>
2024-07-12 15:18:33 +0200

    Solution 13 of Python/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8x8htw6p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/977fa093-7de5-4ab5-a5e7-eea5d57b1 ea3.py", line 13, in <module>     check(smallest_change)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/977fa093-7de5-4ab5-a5e7-eea5d57b1 ea3.py", line 10, in check     assert smallest_change([0, 1]) == 1            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_73.py
+++ Python_73.py
@@ -1,6 +1,7 @@
+```
 def smallest_change(arr):
     n = len(arr)
     arr_str = [str(x) for x in arr]
-    rev_arr_str = ["".join(sorted(str(x))) for x in arr]
+    rev_arr_str = [''.join(sorted(str(x))) for x in arr]
     diff_count = sum(1 for i in range(n) if arr_str[i] != rev_arr_str[i])
     return diff_count
