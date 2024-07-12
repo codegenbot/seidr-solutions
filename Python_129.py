fedfcd47289fe262036b594ae7b57d5b053cef7f
beep boop <githubbot@vadim.me>
2024-07-12 08:27:43 +0200

    Solution 28 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/p0oo0rpk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/94c3335c-e7ea-4262-ab5a-cc26fab33 896.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -1,2 +1,4 @@
-def min_length(paths):
-    return min(sum(map(int, path)) for path in paths)
+def min_length():
+    n = int(input())
+    all_paths = [list(map(int, input().split())) for _ in range(n)]
+    return min(sum(path) for path in all_paths)
