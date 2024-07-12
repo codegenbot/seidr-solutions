10405f648436538d80c3116f213f347e0b1955bd
beep boop <githubbot@vadim.me>
2024-07-12 21:37:58 +0200

    Solution 94 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/5syp8brn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/76dc0cae-e1a3-44cf-8916-3e866b8d6 c4c.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,7 +1,2 @@
-```
-def find_zero(xs):
-    total = sum(x for x in xs)
-    if abs(total) < 1e-9:
-        return 0
-    zero = -total / sum(x**2 for x in xs)
-    return zero
+def sum_odd_coefficients(xs):
+    return sum(x for x in xs if xs.index(x) % 2 != 0)
