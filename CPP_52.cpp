fdc1e8ae5a69b7c346fb5bf22bf06190c75df7f7
beep boop <githubbot@vadim.me>
2024-07-12 14:29:36 +0200

    Solution 17 of CPP/52, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/f9d94qfj Prompt:       |           ^~~~~~ In file included from 16bbce76-6bd7-4e51-8d77-cf93a94cdef0.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 16bbce76-6bd7-4e51-8d77-cf93a94cdef0.cpp:3:29: error: expected primary-expressio n before ‘int’     3 | bool below_threshold(vector<int> l, int t) {       |                             ^~~ 16bbce76-6bd7-4e51-8d77-cf93a94cdef0.cpp:3:37: error: expected primary-expressio n before ‘int’     3 | bool below_threshold(vector<int> l, int t) {       |                                     ^~~ 16bbce76-6bd7-4e51-8d77-cf93a94cdef0.cpp:3:42: error: expression list treated as  compound expression in initializer [-fpermissive]     3 | bool below_threshold(vector<int> l, int t) {       |                                          ^ In file included from 16bbce76-6bd7-4e51-8d77-cf93a94cdef0.cpp:12: 16bbce76-6bd7-4e51-8d77-cf93a94cdef0.cpp: In function ‘int main()’: 16bbce76-6bd7-4e51-8d77-cf93a94cdef0.cpp:14:32: error: ‘below_threshold’ cannot be used as a function    14 |     assert (not(below_threshold({1, 8, 4, 10}, 10)));       |                 ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,8 +1,9 @@
-bool below_threshold(vector<int> l, int t) {
+#include <vector>
+#include <assert.h>
+
+bool below_threshold(std::vector<int> l, int t) {
     for (int i : l) {
-        if (i >= t) {
-            return false;
-        }
+        if (i >= t) return false;
     }
-    return true;
+    return true; 
 }
