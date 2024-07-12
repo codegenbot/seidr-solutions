74dc802ec433d28be014fbe49f7da3fb3cfde8a7
beep boop <githubbot@vadim.me>
2024-07-12 23:28:20 +0200

    Solution 72 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ndqvsqhd Prompt:       |                    ^~~ 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp:9:27: error: ‘vector’ was not declared in this scope     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp:9:39: error: expression list treated as  compound expression in initializer [-fpermissive]     9 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp:8: 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp: In function ‘int main()’: 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp:18:20: error: ‘make_a_pile’ was not dec lared in this scope    18 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 9617e7d8-158d-4253-82ea-9a00bb096ee2.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,16 +1,19 @@
 #include <vector>
+#include <algorithm>
 
 bool same(const std::vector<int>& a, const std::vector<int>& b) {
     if (a.size() != b.size()) {
         return false;
     }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
     return a == b;
 }
 
-std::vector<int> make_a_pile(int n) {
-    std::vector<int> pile;
+int make_a_pile(int n) {
+    vector<int> pile;
     for (int i = 1; i <= n; ++i) {
-        pile.push_back(i);
+        pile.push_back(i * 2);
     }
-    return pile;
+    return 0;
 }
