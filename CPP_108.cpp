a11e60ed0ee8144f742425ff5b7ff12462622d4d
beep boop <githubbot@vadim.me>
2024-07-12 22:53:17 +0200

    Solution 56 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lqy3vi1c Prompt: n header ‘<string>’; did you forget to ‘#include <string>’? 4589c55d-3853-418b-9a11-9ba296344e82.cpp:15:23: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    15 |             for(int j=str.length()-1; j>=0; j--) {       |                       ^~~       |                       std 4589c55d-3853-418b-9a11-9ba296344e82.cpp:16:24: error: ‘abs’ was not declared in  this scope    16 |                 sum += abs(str[j] - '0') * sign;       |                        ^~~ In file included from 4589c55d-3853-418b-9a11-9ba296344e82.cpp:26: 4589c55d-3853-418b-9a11-9ba296344e82.cpp: In function ‘int main()’: 4589c55d-3853-418b-9a11-9ba296344e82.cpp:28:23: error: invalid conversion from ‘ int’ to ‘char**’ [-fpermissive]    28 |     assert (count_nums({1}) == 1);       |             ~~~~~~~~~~^~~~~       |                       |       |                       int 4589c55d-3853-418b-9a11-9ba296344e82.cpp:28:23: error: too few arguments to func tion ‘int count_nums(char**, int)’ 4589c55d-3853-418b-9a11-9ba296344e82.cpp:1:5: note: declared here     1 | int count_nums(char** nums, int size) {       |     ^~~~~~~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,6 +1,6 @@
-```cpp
+#include <iostream>
 #include <string>
-#include <cstdlib>
+#include <cmath>
 
 int count_nums(char** nums, int size) {
     int count = 0;
@@ -8,7 +8,6 @@ int count_nums(char** nums, int size) {
         if (atoi(nums[i]) >= 0) {
             std::string str = std::to_string(atoi(nums[i]));
             int sum = 0;
-            str = std::to_string(atoi(nums[i]));
             for (char c : str) {
                 sum += c - '0';
             }
@@ -18,7 +17,7 @@ int count_nums(char** nums, int size) {
             std::string str = "-" + std::to_string(std::abs(atoi(nums[i])));
             int sum = 0, sign = 1;
             for(int j=str.length()-1; j>=0; j--) {
-                sum += std::abs(str[j] - '0') * sign;
+                sum += (str[j] - '0') * sign;
                 sign *= -1;
             }
             if (sign * sum % 2 != 0)
@@ -26,8 +25,4 @@ int count_nums(char** nums, int size) {
         }
     }
     return count;
-}
-
-int main() {
-    assert(count_nums((char*[1]) {"1"}, 1) == 1);
 }
