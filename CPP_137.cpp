fa04b5604e6016837cf3139a9841183bb1d906a9
beep boop <githubbot@vadim.me>
2024-07-13 00:35:25 +0200

    Solution 53 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jkmyomz8 Prompt:       |     ^~~~ In file included from 05cbe1c4-cf16-4aa3-8908-3fc2e5ee4447.cpp:31: 05cbe1c4-cf16-4aa3-8908-3fc2e5ee4447.cpp: In function ‘int main()’: 05cbe1c4-cf16-4aa3-8908-3fc2e5ee4447.cpp:33:13: error: ‘boost’ has not been decl ared    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 05cbe1c4-cf16-4aa3-8908-3fc2e5ee4447.cpp:33:29: error: ‘string’ was not declared  in this scope    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 05cbe1c4-cf16-4aa3-8908-3fc2e5ee4447.cpp:33:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 05cbe1c4-cf16-4aa3-8908-3fc2e5ee4447.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 05cbe1c4-cf16-4aa3-8908-3fc2e5ee4447.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,28 +1,29 @@
+#include <string>
 #include <iostream>
 
-int compare_one(int i) {
+int compare_one(const std::string& s, int i) {
     if (i == 1)
-        return -10;
+        return std::stoi(s) - 10;
     else if (i == 2)
-        return 0;
+        return std::stoi(s);
     else if (i == 3)
-        return 10;
+        return std::stoi(s) + 10;
     else 
         return 0;
 }
 
 int main() {
-    int a, b;
+    std::string a, b;
     std::cout << "Enter the first number: ";
     std::cin >> a;
     std::cout << "Enter the second number: ";
     std::cin >> b;
 
-    if (a != 0 && b != 0) {
-        int x = compare_one(1);
-        int y = compare_one(2);
+    if (a.size() > 0 && b.size() > 0) {
+        int s1 = compare_one(a, 1);
+        int s2 = compare_one(b, 2);
 
-        return (x < y) ? -1 : ((y < x) ? 1 : 0); 
+        return (s1 < s2) ? -1 : ((s2 < s1) ? 1 : 0); 
     } else
         return 0;
 }
