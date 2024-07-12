ae1397b05d419f02328e2a78171682a492192e31
beep boop <githubbot@vadim.me>
2024-07-12 15:08:05 +0200

    Solution 5 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/esdxnjzt Prompt: ared in this scope    25 |     for (int num : subvector1) {       |                    ^~~~~~~~~~ 8f13fd30-4ef3-42be-b8f5-37a7d990bd71.cpp:26:9: error: ‘cout’ was not declared in  this scope    26 |         cout << num << endl;       |         ^~~~ 8f13fd30-4ef3-42be-b8f5-37a7d990bd71.cpp:26:24: error: ‘endl’ was not declared i n this scope    26 |         cout << num << endl;       |                        ^~~~ 8f13fd30-4ef3-42be-b8f5-37a7d990bd71.cpp:28:20: error: ‘subvector2’ was not decl ared in this scope    28 |     for (int num : subvector2) {       |                    ^~~~~~~~~~ 8f13fd30-4ef3-42be-b8f5-37a7d990bd71.cpp:29:9: error: ‘cout’ was not declared in  this scope    29 |         cout << num << endl;       |         ^~~~ 8f13fd30-4ef3-42be-b8f5-37a7d990bd71.cpp:29:24: error: ‘endl’ was not declared i n this scope    29 |         cout << num << endl;       |                        ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
-
 using namespace std;
 
 int main() {
@@ -11,12 +10,12 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin >> nums[i];
     }
-    
+
     int sum = accumulate(nums.begin(), nums.end(), 0);
     int target = sum / 2;
     int prefixSum = 0;
     int idx = 0;
-    
+
     for (int i = 0; i < n; ++i) {
         prefixSum += nums[i];
         if (prefixSum >= target) {
@@ -24,16 +23,16 @@ int main() {
             break;
         }
     }
-    
+
     vector<int> subvector1(nums.begin(), nums.begin() + idx + 1);
     vector<int> subvector2(nums.begin() + idx + 1, nums.end());
-    
+
     for (int num : subvector1) {
         cout << num << endl;
     }
     for (int num : subvector2) {
         cout << num << endl;
     }
-    
+
     return 0;
 }
