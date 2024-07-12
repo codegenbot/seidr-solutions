8d5732934c988c8955165e3eeab3be537557b064
beep boop <githubbot@vadim.me>
2024-07-12 14:55:49 +0200

    Solution 4 of Python/64, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/o3klv93j Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8a96af6c-e37a-4fb7-8852-3a1608321 a1d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_64.py
+++ Python_64.py
@@ -1,10 +1,7 @@
-```
 def vowels_count(s):
     s = s.lower()
     count = 0
     for char in s:
-        if char in 'aeiouy':
-            if char == 'y' and s.index(char) != len(s) - 1:
-                continue
+        if char in 'aeiouy' and (char == 'y' or s.find(char) == len(s) - 1):
             count += 1
     return count
