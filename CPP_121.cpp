e4dc87a9701e69713c3fedc26e836739db07e3af
beep boop <githubbot@vadim.me>
2024-07-12 16:18:23 +0200

    Solution 32 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ef546zl5 Prompt: 0bf41cd4-4055-4686-ab4b-17b0f3c2b283.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 0bf41cd4-4055-4686-ab4b-17b0f3c2b283.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 0bf41cd4-4055-4686-ab4b-17b0f3c2b283.cpp:21: 0bf41cd4-4055-4686-ab4b-17b0f3c2b283.cpp: In function ‘int main()’: 0bf41cd4-4055-4686-ab4b-17b0f3c2b283.cpp:23:13: error: ‘solutions’ was not decla red in this scope; did you mean ‘solution’?    23 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,6 +1,6 @@
+```cpp
 #include <vector>
 #include <iostream>
-#include <initializer_list>
 using namespace std;
 
 int solution(vector<int> lst) {
@@ -13,7 +13,7 @@ int solution(vector<int> lst) {
     return sum;
 }
 
-int runMain() {
+int main() {
     vector<int> lst({3, 13, 2, 9});
     cout << solution(lst);
 }
