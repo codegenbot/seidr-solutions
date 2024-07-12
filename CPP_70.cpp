70b92c9b092b21789cc381c65885bafbb0a289fc
beep boop <githubbot@vadim.me>
2024-07-12 17:06:57 +0200

    Solution 78 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1o525827 Prompt: In file included from /usr/include/c++/11/vector:67,                  from 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp:24: 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp: In function ‘int main()’: 6100ebc1-4a1e-4320-9174-037e0cc20c51.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,12 +2,35 @@
 #include <vector>
 #include <algorithm>
 
-int sort_list(int n) {
-    std::vector<int> lst = {n};
+bool std::operator==(std::vector<int> const &a, std::vector<int> const &b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::vector<int> strange_sort_list() {
+    int n;
+    std::cout << "Enter number of elements: ";
+    std::cin >> n;
+
+    std::vector<int> lst;
+    for (int i = 0; i < n; i++) {
+        int temp;
+        std::cout << "Enter element: ";
+        std::cin >> temp;
+        lst.push_back(temp);
+    }
+
     std::vector<int> result;
     while (!lst.empty()) {
         std::sort(lst.begin(), lst.end());
-        if (lst[0] < lst.back()) {
+        if (lst[0] < lst[lst.size() - 1]) {
             result.push_back(lst[0]);
             lst.erase(lst.begin());
         } else {
@@ -15,10 +38,5 @@ int sort_list(int n) {
             lst.pop_back();
         }
     }
-    return result.size();
-}
-
-int main() {
-    assert(sort_list(111111) == 1);
-    return 0;
+    return result;
 }
