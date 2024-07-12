8ead3d9b31739f9421fab50fb401deb4330ce1b9
beep boop <githubbot@vadim.me>
2024-07-12 07:59:30 +0200

    Solution 80 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/rq5tl5sf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cebb7721-360e-45bd-911d-da8656079 75e.py", line 20, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cebb7721-360e-45bd-911d-da8656079 75e.py", line 17, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: fruit_distribution() takes 1 positional argument but 2 were given

--- Python_67.py
+++ Python_67.py
@@ -1,4 +1,5 @@
-def fruit_distribution(s):
+```
+def fruit_distribution(s, max_value):
     s = s.lower().replace("apples", "0").replace("oranges", "0")
     total = 0
     parts = s.split()
@@ -10,4 +11,4 @@ def fruit_distribution(s):
             if part.startswith("and "):
                 last_number = int(parts[parts.index(part) - 1])
                 total -= (-last_number // 2)
-    return -(total // 2)
+    return -(total // 2) if total <= max_value else -(max_value // 2)
