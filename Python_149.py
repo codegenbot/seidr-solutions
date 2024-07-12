e05514badb01cc9c77f3cdb728523da8a3ad80bb
beep boop <githubbot@vadim.me>
2024-07-12 08:43:48 +0200

    Solution 14 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/oaf2ryzo Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/83104f77-b444-4fb1-aca3-25837f259 c84.py", line 12, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/83104f77-b444-4fb1-aca3-25837f259 c84.py", line 9, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,6 +1,3 @@
+```
 def sorted_list_sum(lst):
-    even_words = [word for word in lst if len(word) % 2 == 0]
-    even_lengths_sum = sum(len(word) for word in even_words)
-    return ",".join(
-        sorted(lst, key=lambda x: (sum(1 for ch in x if len(x) % 2 == 0), x))
-    )
+    return ','.join(sorted(set(lst)))
