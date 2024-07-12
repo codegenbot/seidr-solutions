1506e8b068040bc0b5ebd0228d46972497198e31
beep boop <githubbot@vadim.me>
2024-07-12 03:44:47 +0200

    Solution 15 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/e30yzf7t Prompt: Complete the following code given the task description and function signature.

--- Python_119.py
+++ Python_119.py
@@ -1,2 +1,12 @@
 def match_parens(lst):
-    return "Yes" if sum(a != b for a, b in zip(*lst)) % 2 == 0 else "No"
+    a, b = lst
+    stack = []
+    for char in (a+b):
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if len(stack) > 0 and stack.pop() == '(':
+                continue
+            else:
+                return 'No'
+    return 'Yes' if len(stack) == 0 else 'No'
