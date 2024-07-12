28f5a729fd1877d00960c03d2dcbc03744666967
beep boop <githubbot@vadim.me>
2024-07-12 11:20:35 +0200

    Solution 1 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/0yui81hq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0eca6cb5-d1dd-4351-8c1b-f0bca594f d5b.py", line 9, in <module>     check(digits)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0eca6cb5-d1dd-4351-8c1b-f0bca594f d5b.py", line 6, in check     assert digits(2468) == 0            ^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0eca6cb5-d1dd-4351-8c1b-f0bca594f d5b.py", line 2, in digits     return eval("*".join(str(int(d)) for d in str(n) if int(d) % 2 != 0))            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "<string>", line 0 SyntaxError: invalid syntax

--- Python_131.py
+++ Python_131.py
@@ -1,2 +1,4 @@
+import ast
+
 def digits(n):
-    return eval("*".join(str(int(d)) for d in str(n) if int(d) % 2 != 0))
+    return ast.literal_eval("*".join(str(int(d)) for d in str(n) if int(d) % 2 != 0))
