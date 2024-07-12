7e8f3264ce13c6c30195414f1af0a2a00f0a5022
beep boop <githubbot@vadim.me>
2024-07-12 18:40:30 +0200

    Solution 6 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/deso84hn Prompt: a6246c30-0ed7-41e3-b613-b0b707050a07.cpp: In function ‘int main()’: a6246c30-0ed7-41e3-b613-b0b707050a07.cpp:9:26: error: ‘custom_strlen’ cannot be used as a function     9 |     assert (custom_strlen("asdasnakj") == 12);       |             ~~~~~~~~~~~~~^~~~~~~~~~~~~ a6246c30-0ed7-41e3-b613-b0b707050a07.cpp:9:5: error: ‘assert’ was not declared i n this scope     9 |     assert (custom_strlen("asdasnakj") == 12);       |     ^~~~~~ a6246c30-0ed7-41e3-b613-b0b707050a07.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include<cstring>   +++ |+#include <cassert>     3 | a6246c30-0ed7-41e3-b613-b0b707050a07.cpp: At global scope: a6246c30-0ed7-41e3-b613-b0b707050a07.cpp:15:5: error: redefinition of ‘int main( )’    15 | int main(){       |     ^~~~ a6246c30-0ed7-41e3-b613-b0b707050a07.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main(){       |     ^~~~

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,12 +1,12 @@
-#include <string>
-#include <cstring>
-#include <cassert>
-
-int custom_strlen(string str){
-    return str.length();
-}
+#include<string>
+#include<cstring>
+#include<cassert>
 
 int main(){
     assert (custom_strlen("asdasnakj") == 12);
+    // add more test cases if needed
+    int custom_strlen(string str){
+        return str.length();
+    }
     return 0;
 }
