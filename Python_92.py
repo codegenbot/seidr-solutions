898d7fcf057971d04639dccd6ed57de58cb9ff80
beep boop <githubbot@vadim.me>
2024-07-12 03:05:02 +0200

    Solution 0 of Python/92, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/t99h0szy Prompt: Complete the following code given the task description and function signature.

---
+++ Python_92.py
@@ -0,0 +1,7 @@
+def any_int(x, y, z):
+    return (
+        isinstance(x, int)
+        and isinstance(y, int)
+        and isinstance(z, int)
+        and (x == y + z or x == y - z or x == z - y)
+    )
