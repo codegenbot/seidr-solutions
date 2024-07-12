738f9432f3670bddc6528ce1831786f375e848d3
beep boop <githubbot@vadim.me>
2024-07-12 12:43:37 +0200

    Solution 4 of Python/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/uv3d2j2s Prompt: Complete the following code given the task description and function signature.

--- Python_130.py
+++ Python_130.py
@@ -8,5 +8,7 @@ def tri(n):
     else:
         sequence = [3, 2]
         for _ in range(2, n):
-            sequence.append(sequence[-1] + sequence[-2] + sequence[-3])
+            sequence.append(
+                sequence[-1] + sequence[-2] + (sequence[-3] if len(sequence) > 3 else 0)
+            )
         return sequence
