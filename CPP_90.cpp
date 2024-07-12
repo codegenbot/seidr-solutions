e046a4301393daed3794d41a6e9c4ce1b3e754c4
beep boop <githubbot@vadim.me>
2024-07-12 15:19:02 +0200

    Solution 36 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/x7whuw11 Prompt: fore ‘for’    10 | for(int i = 0; i < n; i++){       | ^~~ 4ceea4a1-7b11-4e7a-8918-b8c2d0a66a83.cpp:10:16: error: ‘i’ does not name a type    10 | for(int i = 0; i < n; i++){       |                ^ 4ceea4a1-7b11-4e7a-8918-b8c2d0a66a83.cpp:10:23: error: ‘i’ does not name a type    10 | for(int i = 0; i < n; i++){       |                       ^ 4ceea4a1-7b11-4e7a-8918-b8c2d0a66a83.cpp:15:1: error: expected unqualified-id be fore ‘if’    15 | if(lst.size() < 2) std::cout << "-1";       | ^~ 4ceea4a1-7b11-4e7a-8918-b8c2d0a66a83.cpp:16:1: error: expected unqualified-id be fore ‘else’    16 | else {       | ^~~~ In file included from 4ceea4a1-7b11-4e7a-8918-b8c2d0a66a83.cpp:29: 4ceea4a1-7b11-4e7a-8918-b8c2d0a66a83.cpp: In function ‘int main()’: 4ceea4a1-7b11-4e7a-8918-b8c2d0a66a83.cpp:31:13: error: ‘next_smallest’ was not d eclared in this scope    31 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ^~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,36 +1,33 @@
+```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
+using namespace std;
+
 int main() {
-    std::vector<int> lst;
+    vector<int> lst;
     int n;
-
-    std::cout << "Enter number of elements: ";
-    std::cin >> n;
-
-    if (n > 0) {
-        std::cout << "Enter " << n << " integers: ";
-        for(int i = 0; i < n; i++){
-            int x;
-            std::cin >> x;
-            lst.push_back(x);
-        }
-
-        if(lst.size() >= 2) {
-            std::sort(lst.begin(), lst.end());
-            bool same = true;
-            for(int i = 0; i < lst.size() - 1; i++){
-                if(lst[i] != lst[i+1]) {
-                    std::cout << lst[i];
-                    same = false;
-                    break;
-                }
+    cout << "Enter number of elements: ";
+    cin >> n;
+    cout << "Enter " << n << " integers: ";
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        lst.push_back(x);
+    }
+    if(lst.size() < 2) cout << "-1";
+    else {
+        sort(lst.begin(), lst.end());
+        bool same = true;
+        for(int i = 0; i < lst.size() - 1; i++){
+            if(lst[i] != lst[i+1]) {
+                cout << lst[i];
+                same = false;
+                break;
             }
-            if(same) std::cout << "-1";
-        } else std::cout << "-1";
-
-    } else std::cout << "-1";
-
+        }
+        if(same) cout << "-1";
+    }
     return 0;
 }
