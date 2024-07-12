26d08c2b487014b9d804664b41352d92fce7667c
beep boop <githubbot@vadim.me>
2024-07-12 16:34:19 +0200

    Solution 52 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q8x7adb5 Prompt:       |                                 ^~~~~~ In file included from 747c8d90-ae72-4418-87e9-1580e08b10dd.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 747c8d90-ae72-4418-87e9-1580e08b10dd.cpp:61:44: error: ‘b’ was not declared in t his scope    61 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 747c8d90-ae72-4418-87e9-1580e08b10dd.cpp:69:5: error: redefinition of ‘int main( )’    69 | int main(){       |     ^~~~ 747c8d90-ae72-4418-87e9-1580e08b10dd.cpp:50:5: note: ‘int main()’ previously def ined here    50 | int main() {       |     ^~~~ In file included from 747c8d90-ae72-4418-87e9-1580e08b10dd.cpp:60: 747c8d90-ae72-4418-87e9-1580e08b10dd.cpp: In function ‘int main()’: 747c8d90-ae72-4418-87e9-1580e08b10dd.cpp:70:20: error: ‘by_length’ was not decla red in this scope; did you mean ‘byLength’?    70 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,224 +1,58 @@
-```cpp
-vector<string> numberToWords(int num) {
-    vector<string> result;
+#include <vector>
+#include <string>
 
-    if (num == 0)
-        return {"Zero"};
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a == b)
+        return true;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
+            return false;
+    }
+    return true;
+}
 
-    if (num < 10) {
-        switch (num) {
-            case 1:
-                result.push_back("One");
-                break;
-            case 2:
-                result.push_back("Two");
-                break;
-            case 3:
-                result.push_back("Three");
-                break;
-            case 4:
-                result.push_back("Four");
-                break;
-            case 5:
-                result.push_back("Five");
-                break;
-            case 6:
-                result.push_back("Six");
-                break;
-            case 7:
-                result.push_back("Seven");
-                break;
-            case 8:
-                result.push_back("Eight");
-                break;
-            case 9:
-                result.push_back("Nine");
-                break;
-        }
-    } else if (num < 20) {
-        switch (num) {
-            case 10:
-                result.push_back("Ten");
-                break;
-            case 11:
-                result.push_back("Eleven");
-                break;
-            case 12:
-                result.push_back("Twelve");
-                break;
-            case 13:
-                result.push_back("Thirteen");
-                break;
-            case 14:
-                result.push_back("Fourteen");
-                break;
-            case 15:
-                result.push_back("Fifteen");
-                break;
-            case 16:
-                result.push_back("Sixteen");
-                break;
-            case 17:
-                result.push_back("Seventeen");
-                break;
-            case 18:
-                result.push_back("Eighteen");
-                break;
-            case 19:
-                result.push_back("Nineteen");
-                break;
-        }
-    } else if (num < 100) {
-        switch (num % 10) {
-            case 1: 
-                if (num % 100 / 10 == 6)
-                    result.push_back("Sixty-One");
-                else if (num % 100 / 10 == 7)
-                    result.push_back("Seventy-One");
-                else if (num % 100 / 10 == 8)
-                    result.push_back("Eighty-One");
-                else
-                    result.push_back("Ninety-One");
-                break;
-            case 2: 
-                if (num % 100 / 10 == 6)
-                    result.push_back("Sixty-Two");
-                else if (num % 100 / 10 == 7)
-                    result.push_back("Seventy-Two");
-                else if (num % 100 / 10 == 8)
-                    result.push_back("Eighty-Two");
-                else
-                    result.push_back("Ninety-Two");
-                break;
-            case 3: 
-                if (num % 100 / 10 == 6)
-                    result.push_back("Sixty-Three");
-                else if (num % 100 / 10 == 7)
-                    result.push_back("Seventy-Three");
-                else if (num % 100 / 10 == 8)
-                    result.push_back("Eighty-Three");
-                else
-                    result.push_back("Ninety-Three");
-                break;
-            case 4: 
-                if (num % 100 / 10 == 6)
-                    result.push_back("Sixty-Four");
-                else if (num % 100 / 10 == 7)
-                    result.push_back("Seventy-Four");
-                else if (num % 100 / 10 == 8)
-                    result.push_back("Eighty-Four");
-                else
-                    result.push_back("Ninety-Four");
-                break;
-            case 5: 
-                if (num % 100 / 10 == 6)
-                    result.push_back("Sixty-Five");
-                else if (num % 100 / 10 == 7)
-                    result.push_back("Seventy-Five");
-                else if (num % 100 / 10 == 8)
-                    result.push_back("Eighty-Five");
-                else
-                    result.push_back("Ninety-Five");
-                break;
-            case 6: 
-                if (num % 100 / 10 == 6)
-                    result.push_back("Sixty-Six");
-                else if (num % 100 / 10 == 7)
-                    result.push_back("Seventy-Six");
-                else if (num % 100 / 10 == 8)
-                    result.push_back("Eighty-Six");
-                else
-                    result.push_back("Ninety-Six");
-                break;
-            case 7: 
-                if (num % 100 / 10 == 6)
-                    result.push_back("Sixty-Seven");
-                else if (num % 100 / 10 == 7)
-                    result.push_back("Seventy-Seven");
-                else if (num % 100 / 10 == 8)
-                    result.push_back("Eighty-Seven");
-                else
-                    result.push_back("Ninety-Seven");
-                break;
-            case 8: 
-                if (num % 100 / 10 == 6)
-                    result.push_back("Sixty-Eight");
-                else if (num % 100 / 10 == 7)
-                    result.push_back("Seventy-Eight");
-                else if (num % 100 / 10 == 8)
-                    result.push_back("Eighty-Eight");
-                else
-                    result.push_back("Ninety-Eight");
-                break;
-            case 9: 
-                if (num % 100 / 10 == 6)
-                    result.push_back("Sixty-Nine");
-                else if (num % 100 / 10 == 7)
-                    result.push_back("Seventy-Nine");
-                else if (num % 100 / 10 == 8)
-                    result.push_back("Eighty-Nine");
-                else
-                    result.push_back("Ninety-Nine");
-                break;
-        }
-    } else {
-        switch (num % 100) {
-            case 1: 
-                if (num % 400 == 3 || num % 4 == 1)
-                    result = {"One", "Hundred"} | {"And"} | {"One"};
-                else
-                    result.push_back("One") | {"And"} | {"Hundred"};
-                break;
-            case 2: 
-                if (num % 400 == 3 || num % 4 == 1)
-                    result = {"Two", "Hundred"} | {"And"} | {"One"};
-                else
-                    result.push_back("Two") | {"And"} | {"Hundred"};
-                break;
-            case 3: 
-                if (num % 400 == 3 || num % 4 == 1)
-                    result = {"Three", "Hundred"} | {"And"} | {"One"};
-                else
-                    result.push_back("Three") | {"And"} | {"Hundred"};
-                break;
-            case 4: 
-                if (num % 400 == 3 || num % 4 == 1)
-                    result = {"Four", "Hundred"} | {"And"} | {"One"};
-                else
-                    result.push_back("Four") | {"And"} | {"Hundred"};
-                break;
-            case 5: 
-                if (num % 400 == 3 || num % 4 == 1)
-                    result = {"Five", "Hundred"} | {"And"} | {"One"};
-                else
-                    result.push_back("Five") | {"And"} | {"Hundred"};
-                break;
-            case 6: 
-                if (num % 400 == 3 || num % 4 == 1)
-                    result = {"Six", "Hundred"} | {"And"} | {"One"};
-                else
-                    result.push_back("Six") | {"And"} | {"Hundred"};
-                break;
-            case 7: 
-                if (num % 400 == 3 || num % 4 == 1)
-                    result = {"Seven", "Hundred"} | {"And"} | {"One"};
-                else
-                    result.push_back("Seven") | {"And"} | {"Hundred"};
-                break;
-            case 8: 
-                if (num % 400 == 3 || num % 4 == 1)
-                    result = {"Eight", "Hundred"} | {"And"} | {"One"};
-                else
-                    result.push_back("Eight") | {"And"} | {"Hundred"};
-                break;
-            case 9: 
-                if (num % 400 == 3 || num % 4 == 1)
-                    result = {"Nine", "Hundred"} | {"And"} | {"One"};
-                else
-                    result.push_back("Nine") | {"And"} | {"Hundred"};
-                break;
-        }
+std::vector<std::string> byLength(int num) {
+    std::vector<std::string> result;
+
+    switch (num % 100) {
+        case 1:
+            result.push_back("One");
+            break;
+        case 2:
+            result.push_back("Two");
+            break;
+        case 3:
+            result.push_back("Three");
+            break;
+        case 4:
+            result.push_back("Four");
+            break;
+        case 5:
+            result.push_back("Five");
+            break;
+        case 6:
+            result.push_back("Six");
+            break;
+        case 7:
+            result.push_back("Seven");
+            break;
+        case 8:
+            result.push_back("Eight");
+            break;
+        case 9:
+            result.push_back("Nine");
+            break;
     }
 
     return result;
+}
+
+int main() {
+    int num = 984;  
+    std::vector<std::string> result1 = byLength(num);
+    
+    if (issame({"One", "Nine", "Four"}, result1))
+        return 0;
+    else
+        return 1;
 }
