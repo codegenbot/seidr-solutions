b76fa2a48b295b3726f909e75f250cca3246cb37
beep boop <githubbot@vadim.me>
2024-07-12 19:55:19 +0200

    Solution 9 of CPP/82, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/e5g8bp5r Prompt: in this scope    14 |     assert(prime_length("0") == false);       |     ^~~~~~ 79f6b601-1583-471b-bdf9-ae6cb5033e9a.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <cstring>   +++ |+#include <cassert>     2 | 79f6b601-1583-471b-bdf9-ae6cb5033e9a.cpp: At global scope: 79f6b601-1583-471b-bdf9-ae6cb5033e9a.cpp:18:5: error: redefinition of ‘int main( )’    18 | int main(){       |     ^~~~ 79f6b601-1583-471b-bdf9-ae6cb5033e9a.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 79f6b601-1583-471b-bdf9-ae6cb5033e9a.cpp:17: 79f6b601-1583-471b-bdf9-ae6cb5033e9a.cpp: In function ‘int main()’: 79f6b601-1583-471b-bdf9-ae6cb5033e9a.cpp:19:26: warning: ISO C++ forbids convert ing a string constant to ‘char*’ [-Wwrite-strings]    19 |     assert (prime_length("0") == false);       |                          ^~~

--- CPP_82.cpp
+++ CPP_82.cpp
@@ -1,7 +1,7 @@
-#include <string>
+#include <cstring>
 #include <cassert>
 
-bool prime_length(const char* str){
+bool prime_length(char* str){
     int len = strlen(str);
     if(len <= 1) return false;
     for(int i=2; i*i<=len; i++){
