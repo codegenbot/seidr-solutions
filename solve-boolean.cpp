ba3e977f5e9c6ff071766a6e8272ff80e5072f1d
beep boop <githubbot@vadim.me>
2024-07-12 18:37:43 +0200

    Solution 2 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/om6uiz7z Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,20 +1,55 @@
-#include <string>
-using namespace std;
-
-bool solveBoolean(string expression) {
-    stack<char> s;
-    for (int i = 0; i < expression.size(); i++) {
-        if (expression[i] == '&') {
-            while (!s.empty() && s.top() == '&') {
-                s.pop();
+string solveBoolean(string s) {
+    bool result = false;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 't') {
+            result = true;
+        } else if (s[i] == 'f') {
+            return "False";
+        } else if (s[i] == '|') {
+            if (result) {
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (s[j] == 't') {
+                        result = true;
+                        break;
+                    } else if (s[j] == 'f') {
+                        return "False";
+                    }
+                }
+            } else {
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (s[j] == 't') {
+                        return "True";
+                    } else if (s[j] == 'f') {
+                        result = true;
+                        break;
+                    }
+                }
             }
-        } else if (expression[i] == '|') {
-            while (!s.empty()) {
-                s.pop();
+        } else if (s[i] == '&') {
+            if (!result) {
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (s[j] == 't') {
+                        result = true;
+                        break;
+                    } else if (s[j] == 'f') {
+                        return "False";
+                    }
+                }
+            } else {
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (s[j] == 't') {
+                        continue;
+                    } else if (s[j] == 'f') {
+                        return "False";
+                    }
+                }
+                result = true;
             }
-        } else {
-            s.push(expression[i]);
         }
     }
-    return s.top() == 'T';
+    if (result) {
+        return "True";
+    } else {
+        return "False";
+    }
 }
