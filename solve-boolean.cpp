02d1662398f00a88a59d9b9c965fe7ca1c9e7e96
beep boop <githubbot@vadim.me>
2024-07-13 00:45:34 +0200

    Solution 59 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/epd50z31 Prompt:     3 |     cin >> input;       |     ^~~ fd7b67d4-4a74-49b6-8ddd-f4ea10d3ec1e.cpp:3:12: error: ‘input’ was not declared i n this scope; did you mean ‘int’?     3 |     cin >> input;       |            ^~~~~       |            int fd7b67d4-4a74-49b6-8ddd-f4ea10d3ec1e.cpp:6:9: error: ‘cout’ was not declared in this scope     6 |         cout << "True" << endl;       |         ^~~~ fd7b67d4-4a74-49b6-8ddd-f4ea10d3ec1e.cpp:6:27: error: ‘endl’ was not declared in  this scope     6 |         cout << "True" << endl;       |                           ^~~~ fd7b67d4-4a74-49b6-8ddd-f4ea10d3ec1e.cpp:8:9: error: ‘cout’ was not declared in this scope     8 |         cout << "False" << endl;       |         ^~~~ fd7b67d4-4a74-49b6-8ddd-f4ea10d3ec1e.cpp:8:28: error: ‘endl’ was not declared in  this scope     8 |         cout << "False" << endl;       |                            ^~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int main() {
     string input;
     cin >> input;
 
-    if (input == "T") {
+    if (input == "t") {
         cout << "True" << endl;
     } else {
         cout << "False" << endl;
