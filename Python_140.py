0f86d60ce19c4341af6526fa2441ee70f82530ee
beep boop <githubbot@vadim.me>
2024-07-12 08:32:50 +0200

    Solution 11 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jkuz8vxn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90688008-6329-4690-b7c6-71302211b 001.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,4 +1,7 @@
-import re
-
 def fix_spaces(text):
-    return re.sub(r'\s+', '_', text)
+    while " " in text:
+        text = text.replace(" ", "_")
+    return text
+
+text_to_fix = input("Enter a text to replace spaces with underscores: ")
+print(fix_spaces(text_to_fix))
