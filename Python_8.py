9e7f6db9af339559840e85917e182e6c747a24be
beep boop <githubbot@vadim.me>
2024-07-12 07:52:52 +0200

    Solution 95 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2n6b2igf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9e5d6226-e47a-40cb-860c-0172d302f db8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -1,11 +1,7 @@
-```python
+```
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 sum_product = num1 + num2
 
-def check(n):
-    if n % 2 == 0:
-        print(f"{n} is even.")
-    else:
-        print(f"{n} is odd.")
+check(str(sum_product))
