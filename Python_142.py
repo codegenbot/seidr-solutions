198ef4d6a239cb748f8573c0f8418250d2e0b184
beep boop <githubbot@vadim.me>
2024-07-12 11:42:31 +0200

    Solution 70 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/y0gq3031 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7816c6ff-04e8-422f-ba14-678303ae2 745.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -2,15 +2,19 @@ def sum_squares(lst):
     return sum(i**2 for i in lst)
 
 while True:
+    start = input("Do you want to start? (yes/no): ")
+    if start != "yes":
+        continue
+
+    input_str = input("Enter a list of numbers separated by space: ")
     try:
-        while input("Do you want to start? (yes/no): ") != "yes":
-            pass
-        lst = input("Enter a list of numbers separated by space: ").split()
-        int_lst = [int(i) for i in lst if i.lstrip("-").isdigit()]
-        if all(-100 <= n <= 100 for n in int_lst):
-            print(sum_squares(int_lst))
-            break
-        else:
-            print("Some numbers are out of range.")
+        lst = [int(x) for x in input_str.split()]
     except ValueError as e:
         print(f"Invalid input. Please enter numeric values: {e}. Try again!")
+        continue
+
+    if all(-100 <= n <= 100 for n in lst):
+        print(sum_squares(lst))
+        break
+    else:
+        print("Some numbers are out of range.")
