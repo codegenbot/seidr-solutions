155106816b6a169ebe5970b9096a97499800e6bd
beep boop <githubbot@vadim.me>
2024-07-12 16:34:05 +0200

    Solution 71 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/y9kfabj7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ec769d1b-485e-43c4-979d-6f0185d24 156.py", line 16, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ec769d1b-485e-43c4-979d-6f0185d24 156.py", line 13, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -3,8 +3,10 @@ def is_palindrome(string: str) -> bool:
 
 
 def make_palindrome(string: str) -> str:
-    for char in range(len(string)):
-        half = string[: char + 1]
-        second_half = string[char:][::-1]
-        if half + second_half == (half + second_half)[::-1]:
-            return half + second_half
+    if is_palindrome(string):
+        return string
+    for i in range(len(string)):
+        half = string[: i + 1]
+        second_half = string[i:]
+        if is_palindrome(half + "".join(reversed(second_half))):
+            return half + "".join(reversed(second_half))
