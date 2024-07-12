0a862b25aa56645e1cf9dcebe720a61de874b264
beep boop <githubbot@vadim.me>
2024-07-12 09:50:00 +0200

    Solution 53 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4o64e9le Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c3e008c9-0237-4dd4-aad6-18a059e0a 354.py", line 19, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c3e008c9-0237-4dd4-aad6-18a059e0a 354.py", line 16, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,11 +1,12 @@
 def fizz_buzz(n: int):
     count = sum(
         1
-        for i in range(1, n)
+        for i in range(1, n + 1)
         if (
-            i % 3 == 0 and i % 5 != 0 or
-            i % 11 == 0 or "7" in str(i) or
-            i % 3 == 0 and i % 5 == 0
-        ) and i % 13 != 0 and i % 21 != 0
+            ((i % 3 == 0) and (i % 5 != 0)) or
+            (i % 11 == 0) or
+            ("7" in str(i)) or
+            ((i % 3 == 0) and (i % 5 == 0))
+        ) and (i % 13 != 0) and (i % 21 != 0) and (i % 15 != 0) and (i % 35 != 0)
     )
     return count
