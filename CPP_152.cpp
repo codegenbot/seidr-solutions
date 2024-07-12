c572cb6eef6c6c599b298f8243d1471551671460
beep boop <githubbot@vadim.me>
2024-07-12 23:23:42 +0200

    Solution 3 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uswfokmt Prompt: In file included from /usr/include/c++/11/vector:67,                  from 07b281b4-a8f8-4b17-8449-b7b0d69bae26.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 07b281b4-a8f8-4b17-8449-b7b0d69bae26.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 07b281b4-a8f8-4b17-8449-b7b0d69bae26.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 07b281b4-a8f8-4b17-8449-b7b0d69bae26.cpp:26: 07b281b4-a8f8-4b17-8449-b7b0d69bae26.cpp: In function ‘int main()’: 07b281b4-a8f8-4b17-8449-b7b0d69bae26.cpp:36:20: error: ‘compare’ was not declare d in this scope    36 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 07b281b4-a8f8-4b17-8449-b7b0d69bae26.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,23 +1,38 @@
 #include <vector>
-#include <cassert>
 
-bool issame(vector<int> a,vector<int>b){
-    return (a.size()==b.size()) && equal(a.begin(), a.end(), b.begin());
+bool issame(vector<int> a, vector<int> b) {
+    for(int i = 0; i < a.size();i++){
+        if(a[i] != b[i]) return false;
+    }
+    return true;
 }
 
 vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
-    for (int i = 0; i < game.size(); i++) {
-        if (game[i] == guess[i]) {
-            result.push_back(0);
-        } else {
-            result.push_back(abs(game[i] - guess[i]));
+    if(issame(game,guess)){
+        for (int i = 0; i < game.size(); i++) {
+            result.push_back(2);
+        }
+    } else {
+        for (int i = 0; i < game.size(); i++) {
+            if (game[i] == guess[i]) {
+                result.push_back(1);
+            } else {
+                result.push_back(0);
+            }
         }
     }
     return result;
 }
 
 int main() {
-    assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));
+    vector<int> game,guess;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n;i++){
+        cin >> game[i] >> guess[i];
+    }
+    vector<int> res = compare(game,guess);
+    for(auto x:res)cout << x << " ";
     return 0;
 }
