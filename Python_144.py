ca7dff57138383cb6d3a30b754e42026d48ca6be
beep boop <githubbot@vadim.me>
2024-07-12 18:08:47 +0200

    Solution 34 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gwb2dchn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e03f8b0f-5125-4978-873f-4022c3c81 240.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_144.py
+++ Python_144.py
@@ -1,7 +1,8 @@
 from fractions import Fraction
 
-
 def simplify():
     x = float(input("Enter numerator: "))
-    n = float(input("Enter denominator: "))
-    return Fraction(x, 1).limit_denominator() == Fraction(n)
+    y = float(input("Enter denominator: "))
+    return Fraction(x,y) == 1
+
+print(simplify())
