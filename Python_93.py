4f9447966281ad682c1b55ef0fdb79615357bda2
beep boop <githubbot@vadim.me>
2024-07-12 08:22:29 +0200

    Solution 82 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ppkt8fr3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/544c3663-124a-4aca-a1d2-193781a37 2fa.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -1,18 +1,17 @@
-```
 def encode(message):
-    vowels = 'aeiouAEIOU'
+    vowels = "aeiouAEIOU"
     result = ""
     for char in message:
         if not char.isalpha():
             result += char
-        elif char.lower() not in vowels:  
-            index = (ord(char.lower()) - ord('a')) % 26
-            result += chr((index + 1) % 26 + ord('a'))
-        else:  
-            if char.lower() in 'ouieOUIE':
-                index = (ord(char.lower()) - ord('o')) % 4
-                result += chr((index + 1) % 4 + ord('o'))
+        elif char.lower() not in vowels:
+            index = (ord(char.lower()) - ord("a")) % 26
+            result += chr((index + 3) % 26 + ord("a"))
+        else:
+            if char.lower() in "ouieOUIE":
+                index = (ord(char.lower()) - ord("o")) % 4
+                result += chr((index + 1) % 4 + ord("o"))
             else:
-                index = (ord(char.lower()) - ord('a')) % 6
-                result += chr((index + 1) % 6 + ord('a'))
+                index = (ord(char.lower()) - ord("a")) % 6
+                result += chr((index + 1) % 6 + ord("a"))
     return result
