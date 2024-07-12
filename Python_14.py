58ad3e81f2abeea95dc593b16e8c135292ba5026
beep boop <githubbot@vadim.me>
2024-07-12 06:19:26 +0200

    Solution 39 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/mrd681vd Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c9facf32-fe62-4556-8e7a-a65ef869d b75.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_14.py
+++ Python_14.py
@@ -1,5 +1,4 @@
-Here is the modified code:
-
+```
 from typing import List
 
 
@@ -11,7 +10,7 @@ def all_prefixes(string: str) -> List[str]:
     for char in string:
         temp_result = result.copy()
         temp_result.append(char)
-        for prefix in result:
+        for prefix in result[:]:  
             temp_result.append(prefix + char)
         result = temp_result
     return sorted(result)
