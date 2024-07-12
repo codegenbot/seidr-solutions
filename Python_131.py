2957ee721c9c78921533af11f925670453a71ac2
beep boop <githubbot@vadim.me>
2024-07-12 08:22:47 +0200

    Solution 14 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/63xjadsq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6c390232-0c82-44b3-bb72-b6f593713 5f6.py", line 15, in <module>     check(digits)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6c390232-0c82-44b3-bb72-b6f593713 5f6.py", line 12, in check     assert digits(2468) == 0            ^^^^^^^^^^^^^^^^^ AssertionError

--- Python_131.py
+++ Python_131.py
@@ -1,8 +1,10 @@
 def digits(n):
-    product = 0
+    product = 1
     while n:
         digit = n % 10
-        if digit != 0:
-            product += digit
+        if digit % 2 != 0:
+            product *= digit
+        else:
+            return 1 
         n //= 10
     return product
