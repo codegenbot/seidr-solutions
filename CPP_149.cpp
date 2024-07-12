7838f78a33aa2994d634abd38a6e5175f597537f
beep boop <githubbot@vadim.me>
2024-07-12 23:56:50 +0200

    Solution 28 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/y3nscp8u Prompt: In file included from /usr/include/c++/11/string:55,                  from ee7f31be-3973-42ad-bd17-c90eb6fdde28.cpp:5: /usr/include/c++/11/bits/basic_string.h:785:7: note: candidate: ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Traits , _Alloc>::operator=(std::initializer_list<_Tp>) [with _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’   785 |       operator=(initializer_list<_CharT> __l)       |       ^~~~~~~~ /usr/include/c++/11/bits/basic_string.h:785:42: note:   no known conversion for argument 1 from ‘std::remove_reference<const std::__cxx11::basic_string<char>&>: :type’ {aka ‘const std::__cxx11::basic_string<char>’} to ‘std::initializer_list< char>’   785 |       operator=(initializer_list<_CharT> __l)       |                 ~~~~~~~~~~~~~~~~~~~~~~~~~^~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:71,                  from /usr/include/c++/11/vector:60,                  from ee7f31be-3973-42ad-bd17-c90eb6fdde28.cpp:3: /usr/include/c++/11/bits/predefined_ops.h:187:7: warning: ‘__gnu_cxx::__ops::_It er_comp_val<_Compare>::_Iter_comp_val(__gnu_cxx::__ops::_Iter_comp_iter<_Compare >&&) [with _Compare = sorted_list_sum(const std::vector<std::__cxx11::basic_stri ng<char> >&)::<lambda(const string&, const string&)>]’ used but never defined   187 |       _Iter_comp_val(_Iter_comp_iter<_Compare>&& __comp)       |       ^~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -10,18 +10,16 @@ bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b
 }
 
 std::vector<std::string> sorted_list_sum(const std::vector<std::string>& lst) {
-    lst.erase(std::remove_if(lst.begin(), lst.end(),
-                              [](const std::string& s) { return s.length() % 2 == 1; }),
-              lst.end());
-    std::stable_partition(lst.begin(), lst.end(),
-                           [](const std::string& s) { return s.length() % 2 == 1; });
-    std::sort(std::next(lst.begin()), lst.end(),
-              [](const std::string& a, const std::string& b) {
-                  if (a.length() != b.length())
-                      return a.length() < b.length();
-                  else
-                      return a < b;
-              });
+    auto end = unique(lst.begin(), lst.end(),
+                       [](const std::string& a, const std::string& b){ return (a.length() + b.length()) % 2 == 1; });
+    lst.erase(end, lst.end());
+    sort(lst.begin(), lst.end(),
+         [](const std::string& a, const std::string& b) {
+             if (a.length() != b.length())
+                 return a.length() < b.length();
+             else
+                 return a < b;
+         });
     return lst;
 }
 
