c548d5bbb2f59dadde8aa7cb521a0c1b4569d72e
beep boop <githubbot@vadim.me>
2024-07-12 22:52:56 +0200

    Solution 9 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jkmyomz8 Prompt: In file included from b0d15a56-496f-4b2a-b5c3-3ca7bc88e7ae.cpp:23: b0d15a56-496f-4b2a-b5c3-3ca7bc88e7ae.cpp: In function ‘int main()’: b0d15a56-496f-4b2a-b5c3-3ca7bc88e7ae.cpp:25:13: error: ‘boost’ has not been decl ared    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ b0d15a56-496f-4b2a-b5c3-3ca7bc88e7ae.cpp:25:29: error: ‘string’ was not declared  in this scope    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ b0d15a56-496f-4b2a-b5c3-3ca7bc88e7ae.cpp:25:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from b0d15a56-496f-4b2a-b5c3-3ca7bc88e7ae.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from b0d15a56-496f-4b2a-b5c3-3ca7bc88e7ae.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,8 +1,21 @@
-#include <iostream>
+#include <any>
 #include <string>
+#include <iomanip>
 
-std::string compare_one(std::string a, std::string b) {
-    float x = std::stof(a);
-    float y = std::stof(b);
-    return (x > y) ? a : ((y > x) ? b : a);
+std::any compare_one(std::any a, std::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        int x = std::any_cast<int>(a);
+        int y = std::any_cast<int>(b);
+        return (x > y) ? a : ((y > x) ? b : a);
+    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        float x = std::any_cast<float>(a);
+        float y = std::any_cast<float>(b);
+        return (x > y) ? a : ((y > x) ? b : a);
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+        std::string x = std::any_cast<std::string>(a);
+        std::string y = std::any_cast<std::string>(b);
+        return (std::stof(x) > std::stof(y)) ? a : ((std::stof(y) > std::stof(x)) ? b : a);
+    } else {
+        return a;
+    }
 }
