661120997980c632834a4da9f7d857c49559b9b1
beep boop <githubbot@vadim.me>
2024-07-12 09:37:32 +0200

    Solution 45 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/qpmheavl Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 69fc8f2d-1725-4169-b07a-1cd48165df20.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 69fc8f2d-1725-4169-b07a-1cd48165df20.cpp:37:44: error: ‘b’ was not declared in t his scope    37 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 69fc8f2d-1725-4169-b07a-1cd48165df20.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ 69fc8f2d-1725-4169-b07a-1cd48165df20.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main(){       |     ^~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -13,22 +13,22 @@ std::vector<std::string> all_prefixes(std::string str){
     return result;
 }
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b){
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size()) {
         return false;
     }
-  
+
     for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) {
             return false;
         }
     }
-    
+
     return true;
 }
 
 int main(){
     assert (issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
-    
+
     return 0;
 }
