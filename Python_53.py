d29808748e77b345ef00fe45c6b17fafe34ec501
beep boop <githubbot@vadim.me>
2024-07-12 16:47:59 +0200

    Solution 61 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/y4i48eyu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8aa3d94e-80f1-4abd-8ba4-98153f457 0f5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,10 +1,13 @@
-def calculate_average_grades(grades):
-    total = sum(map(int, input("Enter grades separated by space: ").split()))
-    count = len(input("Enter the number of students: "))
-    
-    try:
-        average_grade = total / count
-    except ZeroDivisionError:
-        return "Error: You can't divide by zero"
-    return f"Average grade is {average_grade:.2f}"
 ```
+def check_sum(x: str, y: str):
+    try:
+        x = int(x)
+        y = int(y)
+        if type(x) is not int or type(y) is not int:
+            return "Error: Please enter valid integers"
+        elif x + y <= 10:
+            return f"The sum of {x} and {y} is {x+y}. The result is less than or equal to 10."
+        else:
+            return f"The sum of {x} and {y} is {x+y}. The result is greater than 10."
+    except ValueError:
+        return "Error: Please enter valid integers"
