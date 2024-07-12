05483cdaffea42bb2c8ec07e366c2fb5aa4add02
beep boop <githubbot@vadim.me>
2024-07-12 13:49:28 +0200

    Solution 20 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/cprp4l99 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b9f18b1-2890-43ab-8b9a-fce3e8e93 b3a.py", line 23, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b9f18b1-2890-43ab-8b9a-fce3e8e93 b3a.py", line 19, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,7 +1,14 @@
 def fix_spaces(text):
-    text = text.strip()
-    text = text.replace(" ", "_")
-    while "__" in text or "___" in text:
-        text = text.replace("__", "_")
-        text = text.replace("___", "-_")
-    return text
+    new_text = ""
+    prev_space = False
+    for char in text:
+        if char == " ":
+            if not prev_space: 
+                if len(new_text) > 0 and new_text[-1] != "-":
+                    if new_text and new_text[-1] != "_":
+                        new_text += "-"
+            prev_space = True
+        else:
+            prev_space = False
+        new_text += char
+    return new_text
