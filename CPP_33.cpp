8440f3940146dc14fd3b59550a1289a0bacdff8b
beep boop <githubbot@vadim.me>
2024-07-12 14:59:14 +0200

    Solution 50 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d8rx6wrb Prompt:  in this scope    39 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7aab76dc-54da-4d10-9f02-1aac5dfbcea9.cpp:39:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 7aab76dc-54da-4d10-9f02-1aac5dfbcea9.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 7aab76dc-54da-4d10-9f02-1aac5dfbcea9.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7aab76dc-54da-4d10-9f02-1aac5dfbcea9.cpp:39:34: error: expected primary-expressi on before ‘int’    39 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,5 +1,8 @@
-#include <vector>
 #include <algorithm>
+#include <assert.h>
+#include <vector>
+
+namespace std;
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size())
@@ -9,4 +12,27 @@ bool issame(std::vector<int> a, std::vector<int> b) {
             return false;
     }
     return true;
+}
+
+std::vector<int> sort_third(std::vector<int> l) {
+    std::vector<int> result;
+    for (int i = 0; i < l.size(); i++) {
+        if (i % 3 == 0) {
+            auto temp = l.begin() + i;
+            while(temp != l.end() && *temp % 3 == 0) {
+                temp++;
+            }
+            std::sort(std::make_reverse_iterator(temp), l.end());
+            for (; temp != l.end(); temp++) {
+                if (*temp % 3 == 0) {
+                    result.push_back(*temp);
+                } else {
+                    break;
+                }
+            }
+        } else {
+            result.push_back(l[i]);
+        }
+    }
+    return result;
 }
