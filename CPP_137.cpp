76926afde7bf6089330dc31396d44904559df2cc
beep boop <githubbot@vadim.me>
2024-07-12 22:42:05 +0200

    Solution 4 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jkmyomz8 Prompt: a29c0200-05f7-43aa-a225-bd44ffdc44bb.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ a29c0200-05f7-43aa-a225-bd44ffdc44bb.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ a29c0200-05f7-43aa-a225-bd44ffdc44bb.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ a29c0200-05f7-43aa-a225-bd44ffdc44bb.cpp:2:10: fatal error: boost/any.hpp: No su ch file or directory     2 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -6,15 +6,30 @@ boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
         int x = boost::any_cast<int>(a);
         int y = boost::any_cast<int>(b);
-        return (x > y) ? a : ((y > x) ? b : typeid(int));
+        if (x > y)
+            return a;
+        else if (y > x)
+            return b;
+        else
+            return typeid(int);
     } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
         float x = boost::any_cast<float>(a);
         float y = boost::any_cast<float>(b);
-        return (x > y) ? a : ((y > x) ? b : typeid(float));
+        if (x > y)
+            return a;
+        else if (y > x)
+            return b;
+        else
+            return typeid(float);
     } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
         std::string x = boost::any_cast<std::string>(a);
         std::string y = boost::any_cast<std::string>(b);
-        return (std::stof(x) > std::stof(y)) ? a : ((std::stof(y) > std::stof(x)) ? b : typeid(std::string));
+        if (std::stof(x) > std::stof(y))
+            return a;
+        else if (std::stof(y) > std::stof(x))
+            return b;
+        else
+            return typeid(std::string);
     } else {
         return typeid(int);
     }
