c087c5e92f5f26e07ba56f5ffd12e83eda69c686
beep boop <githubbot@vadim.me>
2024-07-12 19:19:54 +0200

    Solution 59 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mm8jcipt Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 11702c33-7c5a-406a-9592-cae7691eecc7.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 11702c33-7c5a-406a-9592-cae7691eecc7.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 11702c33-7c5a-406a-9592-cae7691eecc7.cpp:26: 11702c33-7c5a-406a-9592-cae7691eecc7.cpp: In function ‘int main()’: 11702c33-7c5a-406a-9592-cae7691eecc7.cpp:36:20: error: ‘sorted_list_sum’ was not  declared in this scope    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 11702c33-7c5a-406a-9592-cae7691eecc7.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,43 +1,21 @@
-bool issame(string a, string b) {
-    if (a.length() != b.length()) {
-        return false;
-    } else {
-        for (int i = 0; i < a.length(); i++) {
-            if (a[i] != b[i]) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-vector<string> sorted_list_sum(vector<string> lst) {
+```
+vector<string> issame(vector<string> lst) {
     vector<string> result;
 
-    // Delete strings that have odd lengths from the list
     for (const auto& str : lst) {
         if (str.length() % 2 == 0) {
             result.push_back(str);
         }
     }
 
-    // Sort the resulted vector by length and then alphabetically
-    std::sort(result.begin(), result.end(),
-              [](const string& a, const string& b) {
-                  if (a.length() != b.length()) {
-                      return a.length() < b.length();
-                  } else {
-                      for (int i = 0; i < a.length(); i++) {
-                          if (a[i] < b[i]) {
-                              return true;
-                          }
-                          else if (a[i] > b[i]) {
-                              return false;
-                          }
-                      }
-                      return a < b;
-                  }
-              });
+    sort(result.begin(), result.end(),
+         [](const string& a, const string& b) {
+             if (a.length() != b.length()) {
+                 return a.length() < b.length();
+             } else {
+                 return a < b;
+             }
+         });
 
     return result;
 }
