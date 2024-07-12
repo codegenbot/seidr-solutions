b5582c12104f6fb818aea909971193b16492c512
beep boop <githubbot@vadim.me>
2024-07-12 08:51:00 +0200

    Solution 34 of Python/154, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/427npmcm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bcaaa5fd-f872-4d30-8f65-07c67e740 35a.py", line 11, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bcaaa5fd-f872-4d30-8f65-07c67e740 35a.py", line 8, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,2 +1,8 @@
 def cycpattern_check(a, b):
-    return any(b in a[i:] + a[:i] for i in range(len(a)))
+    a = "".join(e for e in a if e.isalnum()).lower()
+    b = "".join(e for e in b if e.isalnum()).lower()
+    return sorted(
+        (a + b)
+        .translate(str.maketrans("", "", "!@#$%^&*()_+-=[]{};':,<./?~`"))[:: len(b) + 1]
+        .lower()
+    ) == sorted((a + b)[:: len(b) + 1].lower())
