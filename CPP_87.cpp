414dd51a9b55444049a8cd264f287566b70f2313
beep boop <githubbot@vadim.me>
2024-07-12 02:05:12 +0200

    Solution 19 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ozl7l1g0 Prompt: 0ca4434c-6500-4991-94bb-ebc97e3bbbc1.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 0ca4434c-6500-4991-94bb-ebc97e3bbbc1.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -27,8 +27,6 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x){
     return result;
 }
 
-int main();
-
 int main() {
     assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3)[0], {2, 2}));
     return 0;
