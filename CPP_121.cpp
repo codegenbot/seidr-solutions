8ae473b96dd8efc743c403061acd4709973ecee8
beep boop <githubbot@vadim.me>
2024-07-12 22:40:32 +0200

    Solution 21 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jlcp1g2y Prompt: 36818ed5-d25d-44ea-a64c-4522a41ba38c.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ 36818ed5-d25d-44ea-a64c-4522a41ba38c.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from 36818ed5-d25d-44ea-a64c-4522a41ba38c.cpp:19: 36818ed5-d25d-44ea-a64c-4522a41ba38c.cpp: In function ‘int main()’: 36818ed5-d25d-44ea-a64c-4522a41ba38c.cpp:21:13: error: ‘solutions’ was not decla red in this scope; did you mean ‘solution’?    21 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <cassert>
 
-int solution(std::vector<int> lst) {
+int solutions(std::vector<int> lst) {
     int sum = 0;
     for (int i = 1; i < lst.size(); i++) {
         if (lst[i] % 2 != 0) {
@@ -12,6 +12,6 @@ int solution(std::vector<int> lst) {
 }
 
 int main() {
-    assert(solution({3, 13, 2, 9}) == 16);
+    assert(solutions({3, 13, 2, 9}) == 16);
     return 0;
 }
