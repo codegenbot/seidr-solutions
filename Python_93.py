7974cbb8309050f466dc33c68fd820fe42d5a1be
beep boop <githubbot@vadim.me>
2024-07-12 08:19:25 +0200

    Solution 73 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ppkt8fr3 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bfcc8b6e-b7ae-42b5-bc6b-45e5bfe02 87d.py", line 23, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bfcc8b6e-b7ae-42b5-bc6b-45e5bfe02 87d.py", line 19, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,4 +1,3 @@
-```
 def encode(message):
     vowels = 'aeiouAEIOU'
     result = ""
@@ -7,8 +6,6 @@ def encode(message):
             result += char
         elif char.lower() in vowels:  
             index = (ord(char.lower()) - ord('a')) % 26
-            if index == 0:
-                index = 26
             result += chr((index + 1) % 26 + ord('a'))
         else:
             result += char.upper()
