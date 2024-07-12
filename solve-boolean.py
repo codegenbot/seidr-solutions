d4d6a81722c00a1de907baa7260f65f52e1418b7
beep boop <githubbot@vadim.me>
2024-07-12 18:55:14 +0200

    Solution 19 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/l87g9ae4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/030be05a-3dbb-4307-a05d-17d2b5b99 a32.py", line 5, in <module>     print(solve_boolean(input_str))           ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/030be05a-3dbb-4307-a05d-17d2b5b99 a32.py", line 2, in solve_boolean     return eval(expression.replace("T", "true").replace("F", "false").replace("| ", " or ").replace("&", " and "))            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "<string>", line 1, in <module> NameError: name 't' is not defined

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,5 +1,5 @@
 def solve_boolean(expression):
-    return eval(expression.replace("T", "true").replace("F", "false").replace("|", " or ").replace("&", " and ").lower())
+    return eval(expression.replace("T", "True").replace("F", "False").replace("|", " or ").replace("&", " and "))
 
 input_str = input().strip()
 print(solve_boolean(input_str))
