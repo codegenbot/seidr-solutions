0654a1d05f9da1e0752e6cf08a7f3949ae84608a
beep boop <githubbot@vadim.me>
2024-07-12 21:47:41 +0200

    Solution 88 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/anq24pbe Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -4,8 +4,8 @@
 #include <algorithm>
 
 bool is_happy(const std::string& s) {
-    for (int i = 0; i < s.length() - 1; i++) {
-        size_t count = std::count(s.begin(), s.end(), s[i]);
+    for (int i = 0; i <= s.length() - 2; i++) {
+        size_t count = std::count_if(s.begin(), s.end(), [&](char c){return c == s[i];});
         if (count > 1)
             return false;
     }
@@ -15,7 +15,7 @@ bool is_happy(const std::string& s) {
 int main() {
     std::cout << "Enter a string: ";
     std::string input;
-    std::cin >> input;
+    getline(std::cin, input); 
     if (input == "exit")
         return 0;
     std::cout << (is_happy(input) ? "True" : "False") << std::endl;
