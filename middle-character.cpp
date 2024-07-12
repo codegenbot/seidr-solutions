6c0120188d20ef3461f3b7c28319a100fc49bdd5
beep boop <githubbot@vadim.me>
2024-07-12 14:26:51 +0200

    Solution 34 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ag0c73oo Prompt:   +++ |+#include <iostream>     2 | using namespace std; dc72b46c-d28c-47f6-b91b-3f4290861dc5.cpp:7:5: error: ‘cin’ was not declared in t his scope     7 |     cin >> s;       |     ^~~ dc72b46c-d28c-47f6-b91b-3f4290861dc5.cpp:7:5: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’? dc72b46c-d28c-47f6-b91b-3f4290861dc5.cpp:13:61: error: ‘endl’ was not declared i n this scope    13 |         cout << "Middle characters are: " << mid1 + mid2 << endl;       |                                                             ^~~~ dc72b46c-d28c-47f6-b91b-3f4290861dc5.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std; dc72b46c-d28c-47f6-b91b-3f4290861dc5.cpp:16:51: error: ‘endl’ was not declared i n this scope    16 |         cout << "Middle character is: " << mid << endl;       |                                                   ^~~~ dc72b46c-d28c-47f6-b91b-3f4290861dc5.cpp:16:51: note: ‘std::endl’ is defined in header ‘<ostream>’; did you forget to ‘#include <ostream>’?

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
 #include <string>
-using namespace std;
+#include <iostream>
 
 int main() {
     string s;
-    cout << "Enter a string: ";
-    cin >> s;
-    
+    std::cout << "Enter a string: ";
+    std::cin >> s;
+
     int len = s.length();
     if (len % 2 == 0) {
-        string res = s.substr(len / 2 - 1, 2);
-        cout << "Middle characters are: " << res << endl;
+        string mid1 = s.substr(len / 2 - 1, 1);
+        string mid2 = s.substr(len / 2, 1);
+        std::cout << "Middle characters are: " << mid1 + mid2 << std::endl;
     } else {
-        string res = s.substr(len / 2, 1);
-        cout << "Middle character is: " << res << endl;
+        string mid = s.substr(len / 2, 1);
+        std::cout << "Middle character is: " << mid << std::endl;
     }
-    
+
     return 0;
 }
