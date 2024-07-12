fa93dc9d0d476d3bef9d57af2dc7f56c1eface88
beep boop <githubbot@vadim.me>
2024-07-12 18:19:55 +0200

    Solution 46 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/20nxyk55 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/97198b2b-0c0e-4ae3-9e6c-e9cceb8e0 662.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/97198b2b-0c0e-4ae3-9e6c-e9cceb8e0 662.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,5 +1,5 @@
-def flatten_list(list_of_lists):
-    return [item for sublist in list_of_lists for item in sublist]
-
-def sort_third_fixed(l: list):
-    return [flatten_list(sorted(l[i:i+3], reverse=True)) for i in range(0, len(l), 3)]
+def sort_third(l: list):
+    remainder = len(l) % 3
+    if remainder != 0:
+        l += [-1] * (3 - remainder)
+    return [sorted(l[i:i+3], reverse=True) for i in range(0, len(l), 3)]
