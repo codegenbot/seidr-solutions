36b8648eb4f7d328d77d808e69219f99b1f84a26
beep boop <githubbot@vadim.me>
2024-07-12 02:59:51 +0200

    Solution 8 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/67hlo01k Prompt:    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ aaf7d5b1-9656-4feb-9ae1-3d23a677e7db.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ aaf7d5b1-9656-4feb-9ae1-3d23a677e7db.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from aaf7d5b1-9656-4feb-9ae1-3d23a677e7db.cpp:32: aaf7d5b1-9656-4feb-9ae1-3d23a677e7db.cpp: In function ‘int main()’: aaf7d5b1-9656-4feb-9ae1-3d23a677e7db.cpp:42:20: error: ‘select_words’ was not de clared in this scope    42 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ aaf7d5b1-9656-4feb-9ae1-3d23a677e7db.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <cassert>
 
 using namespace std;
 
-bool is_same(vector<string> a, vector<string> b) {
-    return a == b;
+bool issame(vector<string> a, vector<string> b){
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<string> select_words(string s, int n);
+
+int main() {
+    assert(issame(select_words("a b c d e f", 1), {"b", "c", "d", "f"}));
+    return 0;
 }
 
 vector<string> select_words(string s, int n) {
