fe216ef4ecfeca10436b4d27e6dde507ad8d05a9
beep boop <githubbot@vadim.me>
2024-07-12 14:38:31 +0200

    Solution 6 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/zv5nkczt Prompt: 3180a99d-42af-445e-b5c1-f33bc83d580d.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~ 3180a99d-42af-445e-b5c1-f33bc83d580d.cpp:31:19: error: ‘string’ was not declared  in this scope    31 | int nextRollValue(string s, int frameNumber) {       |                   ^~~~~~ 3180a99d-42af-445e-b5c1-f33bc83d580d.cpp:31:29: error: expected primary-expressi on before ‘int’    31 | int nextRollValue(string s, int frameNumber) {       |                             ^~~ 3180a99d-42af-445e-b5c1-f33bc83d580d.cpp:31:44: error: expression list treated a s compound expression in initializer [-fpermissive]    31 | int nextRollValue(string s, int frameNumber) {       |                                            ^

--- bowling.cpp
+++ bowling.cpp
@@ -1,29 +1,45 @@
 ```cpp
 #include <string>
-using namespace std;
 
-int bowling(string s) {
+int bowlingScore(std::string s) {
     int score = 0;
-    bool lastRoll = false;
+    int currentFrame = 1;
+    int rollsLeftInFrame = 1;
 
-    for (int i = 0; i < 10; i++) {
-        if (s[i] == '/') {
-            score += 10 - (i + 1);
-            lastRoll = true;
-        } else if (lastRoll) {
-            score += s[i] - '0';
+    for (char c : s) {
+        if (c == 'X') { 
+            score += 10 + nextRollValue(s, currentFrame);
+            currentFrame++;
+            rollsLeftInFrame = 1;
+        } else if (c == '/') { 
+            score += 10 - (c - '/') + nextRollValue(s, currentFrame);
+            currentFrame++;
+            rollsLeftInFrame = 0;
         } else {
-            int roll = 0;
-            for (int j = i; j < s.size() && j-i+1 <= s[j] - '0'; j++) {
-                roll = roll * 10 + (s[j] - '0');
-            }
-            if (roll == 10) {
-                score += 10;
-            } else {
-                score += roll;
+            int rollValue = c - '0';
+            score += rollValue;
+
+            if (rollsLeftInFrame == 1) { 
+                currentFrame++;
+                rollsLeftInFrame = 2; 
+            } else { 
+                rollsLeftInFrame--;
             }
         }
     }
 
     return score;
+}
+
+int nextRollValue(std::string s, int frameNumber) {
+    for (char c : s) {
+        if (c == '/') {
+            continue;
+        }
+        if (c == 'X' || (frameNumber > 1 && c - '0' + nextRollValue(s, frameNumber) >= 10)) {
+            return 10;
+        }
+        return c - '0';
+    }
+    return 0;
 }
