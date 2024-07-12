1b902477b13c20412e6371601dabaadf8094f849
beep boop <githubbot@vadim.me>
2024-07-12 12:13:32 +0200

    Solution 96 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/s8kz23ml Prompt:       |                    ^~~ 24c59050-11f5-4b1a-b37c-31a0ea6035d2.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 24c59050-11f5-4b1a-b37c-31a0ea6035d2.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 24c59050-11f5-4b1a-b37c-31a0ea6035d2.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 24c59050-11f5-4b1a-b37c-31a0ea6035d2.cpp:13: 24c59050-11f5-4b1a-b37c-31a0ea6035d2.cpp: In function ‘int main()’: 24c59050-11f5-4b1a-b37c-31a0ea6035d2.cpp:23:20: error: ‘even_odd_count’ was not declared in this scope    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 24c59050-11f5-4b1a-b37c-31a0ea6035d2.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,25 +1,5 @@
 #include <vector>
-#include <string>
-#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-std::vector<int> even_odd_count(int num) {
-    std::vector<int> counts(2, 0);
-    std::string numStr = std::to_string(abs(num));
-    for (char digit : numStr) {
-        if ((digit - '0') % 2 == 0) {
-            counts[0]++;
-        } else {
-            counts[1]++;
-        }
-    }
-    return counts;
-}
-
-int main() {
-    assert(issame(even_odd_count(0), {1, 0}));
-    return 0;
+bool issame(std::vector<int> a, std::vector<int> b){
+    // function logic
 }
