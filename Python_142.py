5ddd206c2f95cd620ffc36597833d202868644bc
beep boop <githubbot@vadim.me>
2024-07-12 11:53:30 +0200

    Solution 77 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/y0gq3031 Prompt: Program did not receive expected input

--- Python_142.py
+++ Python_142.py
@@ -3,15 +3,20 @@ def sum_squares(lst):
     return sum(i**2 for i in lst)
 
 while True:
-    try:
-        while input("Do you want to start? (yes/no): ") != "yes":
-            pass
-        lst = input("Enter a list of numbers separated by space: ").split()
-        int_lst = [int(i) for i in lst if i.lstrip("-").isdigit()]
-        if all(-100 <= n <= 100 for n in int_lst):
-            print(sum_squares(int_lst))
+    while input("Do you want to start? (yes/no): ") != "yes":
+        pass
+    lst = input("Enter a list of numbers separated by space: ").split()
+    
+    int_lst = []
+    while len(int_lst) < len(lst):
+        temp = [int(i) for i in lst if i.lstrip("-").isdigit()]
+        if all(-100 <= n <= 100 for n in temp):
+            int_lst = temp
             break
         else:
-            print("Some numbers are out of range.")
-    except ValueError as e:
-        print(f"Invalid input. Please enter numeric values: {e}. Try again!")
+            print("Some numbers are out of range. Please re-enter.")
+    if int_lst:
+        print(sum_squares(int_lst))
+        break
+    else:
+        print("No valid integers found.")
