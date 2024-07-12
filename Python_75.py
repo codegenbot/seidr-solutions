cc44558369cdf9880103286f5be901c73813e085
beep boop <githubbot@vadim.me>
2024-07-12 09:20:29 +0200

    Solution 50 of Python/75, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/hx3zvaub Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ea6356f4-aa06-4c8a-b180-0af95a669 b86.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_75.py
+++ Python_75.py
@@ -1,10 +1,11 @@
-```python
+```
 def is_multiply_prime(n):
     if n < 2:
         return False
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
-            if i * i == n and is_prime(i): 
+            j = n // i
+            if is_prime(i) and is_prime(j):
                 return True
     if is_prime(n):
         return True
