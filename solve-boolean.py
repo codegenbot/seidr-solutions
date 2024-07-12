80a23db4ff9d3f5fcc2cb15f2b5ac62788d363b1
beep boop <githubbot@vadim.me>
2024-07-12 04:41:28 +0200

    Solution 6 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/ty89jro2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a6af3fd7-2897-4cfd-8271-dd9852159 e6d.py", line 6, in <module>     print(solve_boolean(input_str))           ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a6af3fd7-2897-4cfd-8271-dd9852159 e6d.py", line 2, in solve_boolean     return eval(expression, {"T": True, "F": False, "|": "or", "&": "and"})            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "<string>", line 1, in <module> NameError: name 'f' is not defined. Did you mean: 'F'?

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,6 +1,5 @@
 def solve_boolean(expression):
-    return eval(expression, {"T": True, "F": False, "|": "|", "&": "&"})
-
+    return eval(expression, {"T": True, "F": False, "|": or, "&": and})
 
 input_str = input()
 print(solve_boolean(input_str))
