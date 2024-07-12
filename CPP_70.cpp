d1970e05c1eb9c4d57c159229733dc8490bfbc1f
beep boop <githubbot@vadim.me>
2024-07-12 17:18:06 +0200

    Solution 82 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1o525827 Prompt:       |             ^~~~~~ 5fde9abb-39ba-4a39-a54b-3f52aa0b7ff6.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5fde9abb-39ba-4a39-a54b-3f52aa0b7ff6.cpp:29:39: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 5fde9abb-39ba-4a39-a54b-3f52aa0b7ff6.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 5fde9abb-39ba-4a39-a54b-3f52aa0b7ff6.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from 5fde9abb-39ba-4a39-a54b-3f52aa0b7ff6.cpp:28: 5fde9abb-39ba-4a39-a54b-3f52aa0b7ff6.cpp: In function ‘int main()’: 5fde9abb-39ba-4a39-a54b-3f52aa0b7ff6.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,31 +1,19 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-bool areEqualVectors(std::vector<int> const &a, std::vector<int> const &b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); ++i)
-        if (a[i] != b[i])
-            return false;
-    return true;
+bool operator==(const std::vector<int>& a, const std::vector<int>& b) {
+    return std::equal(a.begin(), a.end(), b.begin());
 }
 
 std::vector<int> strange_sort_list(std::vector<int> lst) {
     std::vector<int> result;
     while (!lst.empty()) {
-        bool isSorted = false;
-        do {
-            isSorted = std::stable_partition(lst.begin(), lst.end(),
-                [ & ] (int x) { return x < 0; }) - lst.begin() == 0;
-            if (!isSorted)
-                std::sort(lst.begin(), lst.end());
-        } while (!isSorted);
-        if (lst.front() > 0) {
+        std::sort(lst.begin(), lst.end());
+        if (lst[0] < lst.back()) {
             result.push_back(lst[0]);
             lst.erase(lst.begin());
         } else {
-            result.push_back(-lst.back());
+            result.push_back(lst.back());
             lst.pop_back();
         }
     }
@@ -33,10 +21,8 @@ std::vector<int> strange_sort_list(std::vector<int> lst) {
 }
 
 int main() {
-    std::vector<int> lst = { -5, -3, 0, 2, 4 };
-    if (areEqualVectors(lst, strange_sort_list(lst))) {
-        for (auto x : lst)
-            std::cout << x << " ";
-        std::cout << "\n";
-    }
+    std::vector<int> expected = {111};
+    std::vector<int> actual = strange_sort_list({111});
+    assert(actual == expected);
+    return 0;
 }
