deb8fb0b4362bfd3ff8cae5e01d71d8067978606
beep boop <githubbot@vadim.me>
2024-07-12 22:38:36 +0200

    Solution 15 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/h84ppt9f Prompt: Complete the following code given the task description and function signature.

--- Python_103.py
+++ Python_103.py
@@ -2,5 +2,5 @@ def rounded_avg(n, m):
     if n > m:
         return -1
     total = sum(range(n, m+1))
-    avg = round(total / (m-n+1))
+    avg = round(total / (m - n + 1))
     return format(avg, 'b')
