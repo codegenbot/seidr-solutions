8953427f70a0822e9e5c6a63c29f3b3dee047edc
beep boop <githubbot@vadim.me>
2024-07-12 18:28:35 +0200

    Solution 49 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/20nxyk55 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d8e617b4-4ffa-4179-b611-6934ce63b 434.py", line 12, in <module>     check(sort_third)           ^^^^^^^^^^ NameError: name 'sort_third' is not defined

--- Python_33.py
+++ Python_33.py
@@ -1,8 +1,5 @@
 def flatten_list(list_of_lists):
     return [item for sublist in list_of_lists for item in sublist]
 
-
 def sort_third(l: list):
-    return [
-        flatten_list(sorted(l[i : i + 3], reverse=True)) for i in range(0, len(l), 3)
-    ]
+    return [flatten_list(sorted(l[i:i+3], reverse=True)) for i in range(0, len(l), 3)]
