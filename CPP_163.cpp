aff39fd70cdf111fbda670471070cbd7e89e91b5
beep boop <githubbot@vadim.me>
2024-07-12 07:33:17 +0200

    Solution 8 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/q5y8rr7d Prompt:       |                    ^~~ 7649e25a-062d-4a56-be94-77cbf7470453.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7649e25a-062d-4a56-be94-77cbf7470453.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7649e25a-062d-4a56-be94-77cbf7470453.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7649e25a-062d-4a56-be94-77cbf7470453.cpp:10: 7649e25a-062d-4a56-be94-77cbf7470453.cpp: In function ‘int main()’: 7649e25a-062d-4a56-be94-77cbf7470453.cpp:20:20: error: ‘generate_integers’ was n ot declared in this scope    20 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ 7649e25a-062d-4a56-be94-77cbf7470453.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,27 +1,20 @@
 #include <vector>
 
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
 std::vector<int> generate_integers(int a, int b) {
     std::vector<int> result;
-    for (int i = a; i <= b; ++i) {
-        result.push_back(i);
+    for(int i = a; i <= b; ++i) {
+        if(i % 2 == 0) {
+            result.push_back(i);
+        }
     }
     return result;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int main() {
-    assert(issame(generate_integers(17, 89) , {}));
-    
+    assert(issame(generate_integers(17, 89), {}));
     return 0;
 }
