3f6989bc367ceb1d1b1c0cdb867464af69bbcb34
beep boop <githubbot@vadim.me>
2024-07-12 14:27:36 +0200

    Solution 20 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fayzjz97 Prompt: b4055c36-7b1c-449a-bfe2-ec9fb83c081c.cpp:4:1: error: ‘vector’ does not name a ty pe     4 | vector<string> filter_by_prefix(vector<string> strings, string prefix){       | ^~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,5 +1,7 @@
-std::vector<string> filter_by_prefix(std::vector<string> strings, std::string prefix){
-    std::vector<string> result;
+using namespace std;
+
+vector<string> filter_by_prefix(vector<string> strings, string prefix){
+    vector<string> result;
     for(auto s : strings){
         if(s.find(prefix) == 0)
             result.push_back(s);
