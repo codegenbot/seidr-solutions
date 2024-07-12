6d9a5d6608c84677f7a6d1308593723d8ff6bad4
beep boop <githubbot@vadim.me>
2024-07-12 14:44:45 +0200

    Solution 6 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1o525827 Prompt: In file included from d65f745b-3136-4e48-b939-d4125c6bffdc.cpp:23: d65f745b-3136-4e48-b939-d4125c6bffdc.cpp: In function ‘int main()’: d65f745b-3136-4e48-b939-d4125c6bffdc.cpp:33:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    33 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,30 +1,21 @@
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
-    }
-    return true;
-}
+using namespace std;
 
-std::vector<int> strange_sort_list(std::vector<int> lst) {
+vector<int> strange_sort_vector(vector<int> lst) {
     vector<int> result;
     while (!lst.empty()) {
         int min_val = *min_element(lst.begin(), lst.end());
-        int max_val = *max_element(lst.begin(), lst.end());
-        if (min_val < max_val) {
-            result.push_back(min_val);
-            lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
-        } else {
+        result.push_back(min_val);
+        lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
+
+        if (!lst.empty()) {
+            int max_val = *max_element(lst.begin(), lst.end());
             result.push_back(max_val);
             lst.erase(remove(lst.begin(), lst.end(), max_val), lst.end());
         }
     }
-    return result;
-}
 
-int main() {
-    assert(issame(strange_sort_list({111111}), {111111}));
+    return result;
 }
