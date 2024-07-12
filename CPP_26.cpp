cfd32f19897cab5f5a3e7de2d5e7cc150d332afc
beep boop <githubbot@vadim.me>
2024-07-12 14:41:35 +0200

    Solution 36 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2vwtr5ad Prompt: 01d0f659-02f8-4a27-996a-92d9b4fc6b51.cpp:3:10: fatal error: assert: No such file  or directory     3 | #include <assert>       |          ^~~~~~~~ compilation terminated.

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
@@ -26,6 +27,5 @@ vector<int> remove_duplicates(vector<int> numbers) {
 }
 
 int main() {
-    assert(issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5}));
+    assert(cassert(issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5})));
     return 0;
-}
