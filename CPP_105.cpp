650f4aae45109cad191d5e573937d2404e4ba948
beep boop <githubbot@vadim.me>
2024-07-12 15:23:55 +0200

    Solution 14 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q8x7adb5 Prompt: In file included from /usr/include/c++/11/string:39,                  from e3918c0e-5797-48ad-9188-5cab00c89836.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from e3918c0e-5797-48ad-9188-5cab00c89836.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e3918c0e-5797-48ad-9188-5cab00c89836.cpp:77:44: error: ‘b’ was not declared in t his scope    77 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e3918c0e-5797-48ad-9188-5cab00c89836.cpp:77:45: error: expression list treated a s compound expression in initializer [-fpermissive]    77 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e3918c0e-5797-48ad-9188-5cab00c89836.cpp:76: e3918c0e-5797-48ad-9188-5cab00c89836.cpp: In function ‘int main()’: e3918c0e-5797-48ad-9188-5cab00c89836.cpp:86:19: error: ‘issame’ cannot be used a s a function    86 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,39 +1,85 @@
+```cpp
 #include <vector>
 #include <string>
 
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if(a.size() != b.size()) {
+        return false;
+    }
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 std::vector<std::string> by_length(std::vector<int> arr) {
-    std::vector<std::string> result;
+    std::vector<int> sortedArr;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
             switch (num) {
                 case 1:
-                    result.push_back("One");
+                    sortedArr.push_back(1);
                     break;
                 case 2:
-                    result.push_back("Two");
+                    sortedArr.push_back(2);
                     break;
                 case 3:
-                    result.push_back("Three");
+                    sortedArr.push_back(3);
                     break;
                 case 4:
-                    result.push_back("Four");
+                    sortedArr.push_back(4);
                     break;
                 case 5:
-                    result.push_back("Five");
+                    sortedArr.push_back(5);
                     break;
                 case 6:
-                    result.push_back("Six");
+                    sortedArr.push_back(6);
                     break;
                 case 7:
-                    result.push_back("Seven");
+                    sortedArr.push_back(7);
                     break;
                 case 8:
-                    result.push_back("Eight");
+                    sortedArr.push_back(8);
                     break;
                 case 9:
-                    result.push_back("Nine");
+                    sortedArr.push_back(9);
                     break;
             }
+        }
+    }
+    reverse(sortedArr.begin(), sortedArr.end());
+    std::vector<std::string> result;
+    for (int num : sortedArr) {
+        switch (num) {
+            case 1:
+                result.push_back("One");
+                break;
+            case 2:
+                result.push_back("Two");
+                break;
+            case 3:
+                result.push_back("Three");
+                break;
+            case 4:
+                result.push_back("Four");
+                break;
+            case 5:
+                result.push_back("Five");
+                break;
+            case 6:
+                result.push_back("Six");
+                break;
+            case 7:
+                result.push_back("Seven");
+                break;
+            case 8:
+                result.push_back("Eight");
+                break;
+            case 9:
+                result.push_back("Nine");
+                break;
         }
     }
     return result;
