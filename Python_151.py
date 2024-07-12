8631155944f0b096d0f69236cc00177fe8f4e427
beep boop <githubbot@vadim.me>
2024-07-12 08:50:09 +0200

    Solution 59 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8kr0f2px Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f2502af-c04e-46fe-a212-5cceac5de 429.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f2502af-c04e-46fe-a212-5cceac5de 429.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,14 +1,6 @@
 def double_the_difference(lst):
-    return (
-        sum(x**2 for x in lst if x % 2 != 0 and x >= 0 and isinstance(x, int)) * 2
-        if lst
-        else 0
-    )
+    return sum(x**2 for x in lst if x % 2 != 0 and x >= 0 and isinstance(x, int)) * 2 if lst else 0
 
-
-def check(lst):
+def check():
+    lst = [1, 3, 5, 2, 4, 6]
     return double_the_difference(lst)
-
-
-# Call the check function with the actual list of integers
-check([1, 3, 5, 2, -4, "hello", 8, 9])
