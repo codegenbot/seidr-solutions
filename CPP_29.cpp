16ca49fb85c33b312565d48351efcc845be54d7f
beep boop <githubbot@vadim.me>
2024-07-12 15:22:16 +0200

    Solution 66 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fayzjz97 Prompt: his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4f0be68a-32de-40dc-9d96-50ad2cf2b6c1.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 4f0be68a-32de-40dc-9d96-50ad2cf2b6c1.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 4f0be68a-32de-40dc-9d96-50ad2cf2b6c1.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main(){       |     ^~~~ In file included from 4f0be68a-32de-40dc-9d96-50ad2cf2b6c1.cpp:28: 4f0be68a-32de-40dc-9d96-50ad2cf2b6c1.cpp: In function ‘int main()’: 4f0be68a-32de-40dc-9d96-50ad2cf2b6c1.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,11 +1,23 @@
-```cpp
+```
 #include <iostream>
 #include <vector>
+
+bool issame(vector<string> a, vector<string> b){
+    if(a.size() != b.size()) 
+        return false;
+    
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]) 
+            return false;
+    }
+    
+    return true;
+}
 
 std::vector<std::string> filter_by_prefix(std::vector<std::string> strings, std::string prefix) { 
     std::vector<std::string> result; 
     for(auto s : strings){ 
-        if (!s.empty() && s.find(prefix) == 0ULL) 
+        if (!s.empty() && s.find(prefix) == 0) 
             result.push_back(s); 
     } 
     return result;
@@ -20,6 +32,8 @@ int main(){
     for(auto s : filteredStrings){
         std::cout << s << "\n";
     }
+
+    assert(filteredStrings == std::vector<std::string>({"hi", "hey"}));
 
     return 0;
 }
