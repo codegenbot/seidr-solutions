c4280e9dd78fc1581de59be2c971db25855ef920
beep boop <githubbot@vadim.me>
2024-07-12 05:54:40 +0200

    Solution 19 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/i58amgxb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/07910ee7-ce48-4005-a712-19df38e74 f2e.py", line 21, in <module>     check(parse_music)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/07910ee7-ce48-4005-a712-19df38e74 f2e.py", line 18, in check     assert parse_music("o| .| o| .| o o| o o|") == [2, 1, 2, 1, 4, 2, 4, 2]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_17.py
+++ Python_17.py
@@ -2,15 +2,22 @@ def parse_music(music_string: str) -> list:
     music_list = []
     i = 0
     while i < len(music_string):
-        if music_string[i:i+2] == 'o\'':
-            music_list.append(4)
+        if music_string[i:i+2] == 'o|':
+            music_list.append(2)
             i += 2
-        elif music_string[i:i+3] == '.|..|':
+        elif music_string[i:i+3] == '| .|':
             music_list.append(1)
             i += 3
         elif music_string[i:i+5] == '..|.||.|':
             music_list.append(0.5)
             i += 5
-        else:
-            i += 1
+        elif music_string[i:i+2] == 'o o':
+            music_list.append(4)
+            i += 2
+        elif music_string[i:i+3] == 'o o|':
+            music_list.append(2)
+            i += 3
+        elif music_string[i:i+2] == 'o o|':
+            music_list.append(4)
+            i += 2
     return music_list
