3a15779675fa5f4bdefe34aadc8f3b82055d4edd
beep boop <githubbot@vadim.me>
2024-07-12 09:31:01 +0200

    Solution 74 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/oaf2ryzo Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/978fbb1b-b95e-4737-a896-f224dbd68 c29.py", line 23, in <module>     check(sorted_list_sum)           ^^^^^^^^^^^^^^^ NameError: name 'sorted_list_sum' is not defined

--- Python_149.py
+++ Python_149.py
@@ -11,4 +11,7 @@ def secure_code():
     sorted_unique_digits = str(sorted(unique_digits)).replace(', ', ' ')
     sorted_list_sum = sum(unique_digits)
     is_even = sorted_list_sum % 2 == 0
-    return " ".join(map(str, sorted(unique_digits))) + f" Sum: {sorted_list_sum} - {check(str(sorted_list_sum))}"
+    if is_even:
+        return " ".join(map(str, sorted(unique_digits))) + f" Sum: {sorted_list_sum} - {check(str(int(sorted_list_sum)))}"
+    else:
+        return " ".join(map(str, sorted(unique_digits))) + f" Sum: {sorted_list_sum} - {check(str(int(sorted_list_sum)))}"
