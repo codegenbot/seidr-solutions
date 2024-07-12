1428c4625d93993518250d07be9d6b0a02950172
beep boop <githubbot@vadim.me>
2024-07-12 07:04:39 +0200

    Solution 20 of CPP/144, ratio 0.8461538461538461 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/9rzc99fg Prompt: a4d2a76a-841e-4477-b62f-f8eab06f76f6.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ a4d2a76a-841e-4477-b62f-f8eab06f76f6.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,4 +1,10 @@
-int num1 = stoi(x.substr(0, x.find('/')));
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+bool simplify(const string& x, const string& n) {
+    int num1 = stoi(x.substr(0, x.find('/')));
     int den1 = stoi(x.substr(x.find('/') + 1));
     int num2 = stoi(n.substr(0, n.find('/')));
     int den2 = stoi(n.substr(n.find('/') + 1));
