e9c2b85ec75ee5c5eb711b072bd162035ab7e55a
beep boop <githubbot@vadim.me>
2024-07-12 09:26:26 +0200

    Solution 95 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2l83sobi Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f749474-942c-464d-bf5b-781fff5a1 818.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,12 +1,7 @@
-def find_max_min(lst):
-    max_num = min_num = lst[0]
-    for num in lst:
-        if num > max_num:
-            max_num = num
-        elif num < min_num:
-            min_num = num
-    return (max_num - min_num) * 2
+```
+def double_the_difference(lst):
+    return (max(lst) - min(lst)) * 2
 
-
-def check(lst):
-    return find_max_min(lst)
+def check():
+    lst = list(map(int, input('"Enter a list of numbers:" ').split(",")))
+    return double_the_difference(lst)
