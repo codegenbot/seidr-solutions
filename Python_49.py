dd3766d50cd748f4cbd63178932d2c8160bfdd55
beep boop <githubbot@vadim.me>
2024-07-12 07:21:30 +0200

    Solution 54 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/d8swkc5z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b79e8b69-ce34-4028-91f4-835e572fe 980.py", line 14, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b79e8b69-ce34-4028-91f4-835e572fe 980.py", line 11, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -2,9 +2,10 @@ def modp(n: int, p: int):
     if p == 0:
         raise ValueError("Modulus cannot be zero")
     result = 1
-    n %= p
+    p = pow(p, 2, p)
     while n > 0:
         if n % 2 == 1:
-            result = (result * n) % p
+            result = (result * p) % p
+        p = pow(p, 2, p)
         n //= 2
     return result
