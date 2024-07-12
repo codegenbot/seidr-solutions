027530fb7f780d767395a709a7907807b9aba8ab
beep boop <githubbot@vadim.me>
2024-07-12 14:29:40 +0200

    Solution 14 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gwb2dchn Prompt: Program did not receive expected input

--- Python_144.py
+++ Python_144.py
@@ -1,8 +1,7 @@
-```Python
 from fractions import Fraction
 
 
 def simplify():
     x = float(input("Enter numerator: "))
     n = float(input("Enter denominator: "))
-    return Fraction(x) / Fraction(n) == Fraction(1)
+    return Fraction(x).limit_denominator() == Fraction(n)
