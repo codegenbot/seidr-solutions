ad67e15c3ffb0a93a8780314c0dc90600a1f2d63
beep boop <githubbot@vadim.me>
2024-07-12 20:29:20 +0200

    Solution 82 of Python/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/xl7a449t Prompt: Complete the following code given the task description and function signature.

--- Python_141.py
+++ Python_141.py
@@ -1,11 +1,6 @@
 ```
 def file_name_check(file_name):
-    if len(file_name) > 0:
-        before_dot = file_name.split('.')[0]
-        after_dot = file_name.split('.')[1]
-        if (len(before_dot) <= 3 and 
-            before_dot[0].isalpha() and
-            after_dot in ['txt', 'exe', 'dll'] and
-            len(file_name.split('.')) == 2):
-            return 'Yes'
-    return 'No'
+    if '.' not in file_name or len(file_name.split('.')[0]) > 1 or not file_name[0].isalpha() or len([i for i in file_name if i.isdigit()]) > 3 or file_name.split('.')[1].lower() not in ['txt', 'exe', 'dll']:
+        return 'No'
+    else:
+        return 'Yes'
