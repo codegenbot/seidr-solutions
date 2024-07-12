ed485fd01230a09fa5c20cb5255a6a6c8f048e42
beep boop <githubbot@vadim.me>
2024-07-12 14:15:21 +0200

    Solution 1 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d8rx6wrb Prompt: Complete the following code given the task description and function signature.

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,10 +1,7 @@
-#include <algorithm>
-
 vector<int> sort_third(vector<int> l) {
     vector<int> result;
     for (int i = 0; i < l.size(); i++) {
         if (i % 3 == 0) {
-            // values at the indices that are divisible by three are equal to the values of the corresponding indicies of l, but sorted
             vector<int> temp;
             for (int j = i; j < l.size() && j % 3 == 0; j++) {
                 temp.push_back(l[j]);
