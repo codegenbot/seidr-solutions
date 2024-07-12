ee03d9586855385442a428f0dc440c0d30f6df1c
beep boop <githubbot@vadim.me>
2024-07-13 00:11:18 +0200

    Solution 86 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gwb2dchn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/68a100c4-6759-423a-9065-992ef40ce 235.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_144.py
+++ Python_144.py
@@ -1,13 +1,14 @@
 from fractions import Fraction
 
+
 def simplify():
     try:
         x = float(input("Enter numerator: "))
         y = float(input("Enter denominator: "))
     except ValueError:
-        return "Invalid input! Please enter a number."
-    
+        """Invalid input! Please enter a number."""
+
     if y == 0:
-        return "Denominator cannot be zero!"
+        """Denominator cannot be zero!"""
     else:
-        return str(Fraction(x,y))
+        """The simplified fraction is: {}""".format(str(Fraction(x, y)))
