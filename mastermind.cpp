f9a16f837cad67e5299f3d414b55503f723b4407
beep boop <githubbot@vadim.me>
2024-07-12 06:32:35 +0200

    Solution 10 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/dsxickbt Prompt: n this scope     6 |     vector<int> freq(6, 0);       |     ^~~~~~ f59d9e2e-94d7-4610-a35c-1fdb96505136.cpp:6:12: error: expected primary-expressio n before ‘int’     6 |     vector<int> freq(6, 0);       |            ^~~ f59d9e2e-94d7-4610-a35c-1fdb96505136.cpp:12:13: error: ‘freq’ was not declared i n this scope    12 |             freq[code[i] - 'A']++;       |             ^~~~ f59d9e2e-94d7-4610-a35c-1fdb96505136.cpp:17:18: error: ‘freq’ was not declared i n this scope    17 |     for (int f : freq) {       |                  ^~~~ f59d9e2e-94d7-4610-a35c-1fdb96505136.cpp:23:13: error: ‘max’ was not declared in  this scope    23 |     white = max(white, 0);       |             ^~~ f59d9e2e-94d7-4610-a35c-1fdb96505136.cpp:25:5: error: ‘cout’ was not declared in  this scope    25 |     cout << white << "\n" << black << "\n";       |     ^~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,24 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
 int main() {
     string code, guess;
     cin >> code >> guess;
-    
+
     int black = 0, white = 0;
-    map<char, int> codeCount, guessCount;
-    
+    vector<int> freq(6, 0);
+
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             black++;
         } else {
-            codeCount[code[i]]++;
-            guessCount[guess[i]]++;
+            freq[code[i] - 'A']++;
+            freq[guess[i] - 'A']--;
         }
     }
-    
-    for (auto& p : codeCount) {
-        white += min(p.second, guessCount[p.first]);
+
+    for (int f : freq) {
+        if (f > 0) {
+            white += f;
+        }
     }
-    
+
+    white = max(white, 0);
+
     cout << white << "\n" << black << "\n";
-    
+
     return 0;
 }
