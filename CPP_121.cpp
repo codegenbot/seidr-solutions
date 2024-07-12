99d2dfa5210d122de8e3afee567050c9f4a8e632
beep boop <githubbot@vadim.me>
2024-07-12 21:59:45 +0200

    Solution 3 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jlcp1g2y Prompt: in this scope     3 | int solution(vector<int> lst) {       |              ^~~~~~ 794c43b7-ae31-4422-b2d4-f1b289804a8e.cpp:3:14: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 794c43b7-ae31-4422-b2d4-f1b289804a8e.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 794c43b7-ae31-4422-b2d4-f1b289804a8e.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 794c43b7-ae31-4422-b2d4-f1b289804a8e.cpp:3:21: error: expected primary-expressio n before ‘int’     3 | int solution(vector<int> lst) {       |                     ^~~ In file included from 794c43b7-ae31-4422-b2d4-f1b289804a8e.cpp:13: 794c43b7-ae31-4422-b2d4-f1b289804a8e.cpp: In function ‘int main()’: 794c43b7-ae31-4422-b2d4-f1b289804a8e.cpp:15:13: error: ‘solutions’ was not decla red in this scope; did you mean ‘solution’?    15 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,7 +1,6 @@
-#include<vector>
-using namespace std;
+#include <vector>
 
-int solutions(vector<int> lst) {
+int solution(vector<int> lst) {
     int sum = 0;
     for (int i = 1; i < lst.size(); i++) {
         if (lst[i] % 2 != 0) {
@@ -12,6 +11,6 @@ int solutions(vector<int> lst) {
 }
 
 int main() {
-    assert(solutions({3, 13, 2, 9}) == 16);
+    assert(solution({3, 13, 2, 9}) == 16);
     return 0;
 }
