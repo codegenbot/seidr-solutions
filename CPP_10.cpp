1416749e4fc33e285978388ee9d13ed93220349f
beep boop <githubbot@vadim.me>
2024-07-12 18:27:30 +0200

    Solution 1 of CPP/10, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/613pwyp1 Prompt: 2b2a1622-3b2b-4bd3-9bea-68be72f910cf.cpp:4:1: error: ‘string’ does not name a ty pe     4 | string make_palindrome(string str) {       | ^~~~~~ In file included from 2b2a1622-3b2b-4bd3-9bea-68be72f910cf.cpp:14: 2b2a1622-3b2b-4bd3-9bea-68be72f910cf.cpp: In function ‘int main()’: 2b2a1622-3b2b-4bd3-9bea-68be72f910cf.cpp:16:13: error: ‘make_palindrome’ was not  declared in this scope    16 |     assert (make_palindrome("jerry") == "jerryrrej");       |             ^~~~~~~~~~~~~~~

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,9 +1,9 @@
 #include <algorithm>
-using namespace std;
+#include <string>
 
-string make_palindrome(string str) {
-    string rev = str;
-    reverse(rev.begin(), rev.end());
+std::string make_palindrome(std::string str) {
+    std::string rev = str;
+    std::reverse(rev.begin(), rev.end());
     int i = 0;
     while (i < str.length() && str[i] == rev[rev.length()-1-i]) {
         i++;
