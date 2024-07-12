ecb4e7d0be28b00db593bd4775c53a17b2b96ad2
beep boop <githubbot@vadim.me>
2024-07-12 21:41:09 +0200

    Solution 15 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/xzzgkq6t Prompt: f36e458b-237b-4e53-8ef6-3c5134bcb527.cpp:1:1: error: stray ‘`’ in program     1 | ```c++       | ^ f36e458b-237b-4e53-8ef6-3c5134bcb527.cpp:1:2: error: stray ‘`’ in program     1 | ```c++       |  ^ f36e458b-237b-4e53-8ef6-3c5134bcb527.cpp:1:3: error: stray ‘`’ in program     1 | ```c++       |   ^ f36e458b-237b-4e53-8ef6-3c5134bcb527.cpp:1:4: error: ‘c’ does not name a type     1 | ```c++       |    ^

--- twitter.cpp
+++ twitter.cpp
@@ -1,9 +1,1 @@
-std::string validateTweet(std::string tweet) {
-    if(tweet.empty()) {
-        return "You didn't type anything";
-    } else if(tweet.length() > 140) {
-        return "Too many characters";
-    } else {
-        return "Your tweet has " + std::to_string(tweet.length()) + " characters";
-    }
-}
+std::string("Your tweet has ") + std::to_string(tweet.length()) + " characters";
