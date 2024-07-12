3346cf5da49f38c677dacb03aee66da126bcb13e
beep boop <githubbot@vadim.me>
2024-07-12 06:08:21 +0200

    Solution 60 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/6ivu5nxu Prompt:       |                                     ^~~~~~ c8689896-98ca-497d-aa21-6bec85099c6a.cpp:49:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from c8689896-98ca-497d-aa21-6bec85099c6a.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from c8689896-98ca-497d-aa21-6bec85099c6a.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ c8689896-98ca-497d-aa21-6bec85099c6a.cpp:49:44: error: ‘b’ was not declared in t his scope    49 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c8689896-98ca-497d-aa21-6bec85099c6a.cpp:57:5: error: redefinition of ‘int main( )’    57 | int main(){       |     ^~~~ c8689896-98ca-497d-aa21-6bec85099c6a.cpp:42:5: note: ‘int main()’ previously def ined here    42 | int main() {       |     ^~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -2,9 +2,7 @@
 #include <vector>
 #include <cassert>
 #include <algorithm>
-#include <cctype> 
-
-using namespace std;
+#include <cctype>
 
 std::vector<std::string> split_words(std::string txt);
 bool issame(std::vector<std::string> a, std::vector<std::string> b);
@@ -39,9 +37,4 @@ std::vector<std::string> split_words(std::string txt) {
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return a == b;
-}
-
-int main() {
-    assert(issame(split_words(""), {"0"}));
-    return 0;
 }
