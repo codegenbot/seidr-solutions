aad6010b5d9ecdbdba83e2e9efb879f5f8d4690e
beep boop <githubbot@vadim.me>
2024-07-12 21:42:44 +0200

    Solution 13 of Python/74, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/iutwtrlf Prompt: Complete the following code given the task description and function signature.

--- Python_74.py
+++ Python_74.py
@@ -1,10 +1,8 @@
 def total_match(lst1, lst2):
     return min(
         [
-            lst
+            lst if sum(len(i) for i in lst) < sum(len(j) for j in lst2) else lst2
             for lst in [lst1, lst2]
-            if sum(len(word) for word in lst)
-            < sum(len(word) for word in list(set(lst2)))
         ],
-        default=lst1,
+        key=lambda x: (sum(len(i) for i in x), id(x)),
     )
