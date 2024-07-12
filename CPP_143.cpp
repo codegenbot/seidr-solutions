ab6d045820cc33419887e52491b43c4c68bcd872
beep boop <githubbot@vadim.me>
2024-07-13 00:09:33 +0200

    Solution 35 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xvsf4rbg Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ 218046b4-f89f-4ed3-94af-db4663990a4f.cpp:23:21:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,8 +1,7 @@
+```cpp
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cassert>
-#include <cstring>
 
 using namespace std;
 
@@ -16,6 +15,7 @@ bool is_prime(int n) {
 vector<string> split(const string& str, const char* delim) {
     size_t pos = 0;
     vector<string> tokens;
+    tokens.reserve(str.size() * 2 + 1);
     while ((pos = str.find(delim)) != string::npos) {
         tokens.push_back(str.substr(0, pos));
         str.erase(0, pos + strlen(delim));
