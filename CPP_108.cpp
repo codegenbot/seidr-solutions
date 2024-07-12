d519ac8e3074a98b2591459d18e7b644706e7f70
beep boop <githubbot@vadim.me>
2024-07-12 22:24:47 +0200

    Solution 43 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lqy3vi1c Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -2,22 +2,22 @@
 #include <iostream>
 #include <cassert>
 
-int count_nums(int* nums, int size) {
+int count_nums(char** nums, int size) {
     int count = 0;
     for (int i = 0; i < size; i++) {
-        if (*nums >= 0) {
+        if (atoi(nums[i]) >= 0) {
             int sum = 0;
-            while (*nums > 0) {
-                sum += abs(*nums % 10);
-                *nums /= 10;
+            while (atoi(nums[i]) > 0) {
+                sum += abs(atoi(nums[i]) % 10);
+                atoi(nums[i]) /= 10;
             }
             if (sum % 2 != 0)
                 count++;
         } else {
             int sum = 0, sign = 1;
-            while (*nums < 0) {
-                sum += abs(*nums % 10);
-                *nums /= 10;
+            while (atoi(nums[i]) < 0) {
+                sum += abs(atoi(nums[i]) % 10);
+                atoi(nums[i]) /= 10;
                 sign *= -1;
             }
             if (sign * sum % 2 != 0)
@@ -32,15 +32,21 @@ int main() {
     std::cout << "Enter the number of integers: ";
     std::cin >> size;
 
-    int* nums = new int[size];
+    char** nums = new char*[size];
     for (int i = 0; i < size; i++) {
-        std::cout << "Enter integer # " << std::to_string(i + 1) << ": ";
-        std::cin >> nums[i];
+        std::cout << "Enter integer # " << i + 1 << ": ";
+        int num;
+        std::cin >> num;
+        nums[i] = new char[10]; 
+        sprintf(nums[i], "%d", num);
     }
 
     int count = count_nums(nums, size);
+    for (int i = 0; i < size; i++) {
+        delete[] nums[i]; 
+    }
     delete[] nums;
     std::cout << "The number of positive integers with an odd digital root is: " << count << std::endl;
-    assert (count == 1); 
+    assert(count == 1); 
     return 0;
 }
