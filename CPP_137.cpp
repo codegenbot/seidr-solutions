614a872044dae38c5621dac58b90f1d3bcff88ca
beep boop <githubbot@vadim.me>
2024-07-12 23:07:36 +0200

    Solution 15 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jkmyomz8 Prompt: In file included from 3dfa52df-77b9-47a8-a13d-eda9a4eaa77f.cpp:1: /usr/include/c++/11/any: In instantiation of ‘_ValueType std::any_cast(std::any& ) [with _ValueType = std::__cxx11::basic_string<char>]’: 3dfa52df-77b9-47a8-a13d-eda9a4eaa77f.cpp:15:51:   required from here /usr/include/c++/11/any:491:21: error: static assertion failed: Template argumen t must be constructible from an lvalue.   491 |       static_assert(is_constructible_v<_ValueType, _Up&>,       |                     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/any:491:21: note: ‘std::is_constructible_v<std::__cxx11::bas ic_string<char>, std::__cxx11::basic_string<char, std::char_traits<char>, std::a llocator<char> >&>’ evaluates to false /usr/include/c++/11/any:495:16: error: invalid use of incomplete type ‘class std ::__cxx11::basic_string<char>’   495 |         return static_cast<_ValueType>(*__p);       |                ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/bits/shared_ptr.h:52,                  from /usr/include/c++/11/memory:77,                  from 3dfa52df-77b9-47a8-a13d-eda9a4eaa77f.cpp:3: /usr/include/c++/11/bits/stringfwd.h:74:11: note: declaration of ‘class std::__c xx11::basic_string<char>’    74 |     class basic_string;       |           ^~~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,6 @@
 #include <any>
+#include <typeindex>
+#include <memory>
 
 std::any compare_one(std::any a, std::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
@@ -12,7 +14,7 @@ std::any compare_one(std::any a, std::any b) {
     } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
         std::string x = std::any_cast<std::string>(a);
         std::string y = std::any_cast<std::string>(b);
-        return (std::stof(x) > std::stof(y)) ? a : ((std::stof(y) > std::stof(x)) ? b : a);
+        return (x > y) ? a : ((y > x) ? b : a);
     } else {
         return a;
     }
