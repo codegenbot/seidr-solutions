5df5a8016e780260e1ab8da9694082c4d8694310
beep boop <githubbot@vadim.me>
2024-07-12 07:47:42 +0200

    Solution 68 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/mhyy3p8m Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ff2f4206-c9b3-48ee-a15d-fb9ccc4b2 e31.py", line 1, in <module>     for ch in encoded_str:               ^^^^^^^^^^^ NameError: name 'encoded_str' is not defined

--- Python_50.py
+++ Python_50.py
@@ -1,15 +1,14 @@
-encoded_str = input("Enter the encoded string: ")
-result = ""
-for ch in encoded_str:
-    if ch.isalpha():
-        if ch.isupper():
-            char_code = ord("A")
-        else:
-            char_code = ord("a")
-        result += (
-            chr((ord(ch) - char_code - shift) % 26 + char_code)
-            if "A" <= ch <= "Z"
-            else chr((ord(ch) - char_code - shift) % 26 + char_code)
-        )
-    else:
+encoded_str = input("Enter the encoded string: ") 
+for ch in encoded_str: 
+    if ch.isalpha(): 
+        if ch.isupper(): 
+            char_code = ord("A") 
+        else: 
+            char_code = ord("a") 
+        result += ( 
+            chr((ord(ch) - char_code - shift) % 26 + char_code) 
+            if "A" <= ch <= "Z" 
+            else chr((ord(ch) - char_code - shift) % 26 + char_code) 
+        ) 
+    else: 
         result += ch
