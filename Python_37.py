309e4b369ad3ec40056f1235f722988f16fe05bc
beep boop <githubbot@vadim.me>
2024-07-12 06:14:31 +0200

    Solution 6 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/4ehkbl5f Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4a099f5f-20d0-4dd3-9f18-a21589591 d5f.py", line 10, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4a099f5f-20d0-4dd3-9f18-a21589591 d5f.py", line 5, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,3 @@
-from itertools import zip_longest
-
 def sort_even(l: list):
     evens = sorted([i for i in l if i % 2 == 0])
-    return [i if i % 2 != 0 else j for i, j in zip_longest(l, evens, fillvalue=0)]
+    return [j if i % 2 == 0 else i for i, j in zip(evens, l)]
