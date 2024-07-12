1a984dd2f7cd98b2af776a1ad05bf8fe28fbd2d4
beep boop <githubbot@vadim.me>
2024-07-12 08:13:40 +0200

    Solution 54 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ppkt8fr3 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/444f5cbd-8f10-44e4-804c-69447e944 8bd.py", line 19, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/444f5cbd-8f10-44e4-804c-69447e944 8bd.py", line 15, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -4,8 +4,8 @@ def encode(message):
     for char in message:
         if not char.isalpha():
             result += char
-        elif 'aeiou'.index(char.lower()) < 3:
+        elif char.lower() in 'aeiou':
             result += chr(ord('a') + (ord(char) - ord('a')) % 26)
         else:
-            result += char.upper()
+            result += char
     return result
