4510ca3c3e31febc7635b4f44c1e40772df36aba
beep boop <githubbot@vadim.me>
2024-07-12 08:22:56 +0200

    Solution 89 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/2hi4xk4o Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,54 +1,21 @@
-int calculateBowlingScore(string s) {
-    int score = 0, frame = 1, ball = 0;
-    for (int i = 0; i < s.length(); i++) {
+int score(string s) {
+    int res = 0, frame = 1, i = 0;
+    
+    while (frame <= 10 && i < s.size()) {
         if (s[i] == 'X') {
-            score += 10;
-            if (s[i + 1] == 'X') {
-                score += 10;
-                if (s[i + 2] == 'X') {
-                    score += 10;
-                } else {
-                    score += s[i + 2] - '0';
-                }
-            } else {
-                if (s[i + 2] == '/') {
-                    score += 10;
-                } else {
-                    score += s[i + 1] - '0' + s[i + 2] - '0';
-                }
-            }
-            frame++;
-        } else if (s[i] == '/') {
-            score += 10 - s[i - 1] + '0';
-            if (s[i + 1] == 'X') {
-                score += 10;
-            } else {
-                score += s[i + 1] - '0';
-            }
-            frame++;
-            ball = 0;
-        } else if (s[i] == '-') {
-            ball++;
-            if (ball == 2) {
-                frame++;
-                ball = 0;
-            }
-        } else {
-            score += s[i] - '0';
-            ball++;
-            if (ball == 2) {
-                frame++;
-                ball = 0;
-            }
-        }
-        if (frame == 11) break;
+            res += 10 + (s[i + 1] == 'X' ? 10 : (isdigit(s[i + 1]) ? s[i + 1] - '0' : 10));
+            if (s[i + 2] == '/')
+                res += 10 - (s[i + 1] == 'X' ? 0 : (isdigit(s[i + 1]) ? s[i + 1] - '0' : 10));
+            else
+                res += (isdigit(s[i + 2]) ? s[i + 2] - '0' : 10);
+            i++;
+        } else if (isdigit(s[i]) || s[i] == '/')
+            res += (s[i] == '/' ? 10 - (isdigit(s[i - 1]) ? s[i - 1] - '0' : 0) : s[i] - '0');
+        i++;
+        if (s[i] == '/')
+            res += (isdigit(s[i + 1]) ? s[i + 1] - '0' : 10);
+        frame++;
     }
-    return score;
-}
-
-int main() {
-    string s;
-    cin >> s;
-    cout << calculateBowlingScore(s) << endl;
-    return 0;
+    
+    return res;
 }
