30b695ffdcfc3a35c0669b5c707e45f760bf5d0e
beep boop <githubbot@vadim.me>
2024-07-12 13:49:04 +0200

    Solution 24 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/u9p0kyz0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dded5e26-b170-4994-ae1b-f7a0ecf33 b21.py", line 10, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dded5e26-b170-4994-ae1b-f7a0ecf33 b21.py", line 7, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dded5e26-b170-4994-ae1b-f7a0ecf33 b21.py", line 3, in max_fill     return math.ceil(total_water / capacity)            ^^^^ NameError: name 'math' is not defined

--- Python_115.py
+++ Python_115.py
@@ -1,5 +1,5 @@
 ```
-from math import ceil
+import math
 def max_fill(grid, capacity):
     total_water = sum(sum(row) for row in grid)
-    return ceil(total_water / capacity)
+    return math.ceil(total_water / capacity)
