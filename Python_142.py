5d55d664912ac3abe850789810a639e8df6f9a10
beep boop <githubbot@vadim.me>
2024-07-12 04:41:35 +0200

    Solution 58 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/osju79qh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7bb79ea8-14fe-48ca-aadc-4113925d8 795.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,8 +1,8 @@
-def sum_of_squares_and_cubes(lst):
+def find_max_sum(lst):
     total_sum = 0
     for num in lst:
         if num % 2 == 0:
-            total_sum += num ** 2
+            total_sum += num**2
         else:
-            total_sum += num ** 3
+            total_sum += num
     return total_sum
