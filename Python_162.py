fd4b658108904a1e66c2f8f46082a9dc9e20cd22
beep boop <githubbot@vadim.me>
2024-07-12 10:41:37 +0200

    Solution 81 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ua814v8v Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/130c9b23-880f-4bf7-9d7e-3c4273e7e df3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_162.py
+++ Python_162.py
@@ -1,20 +1,17 @@
-def problem162():
-    import hashlib
+```
+import hashlib
 
-    def get_hash(s):
-        return hashlib.md5(s.encode()).hexdigest()
+while True:
+    print("Please enter some text (alphanumeric characters only): ", end="")
+    s = input().strip()
+    
+    if not s: 
+        continue
+    
+    if s.isalnum():  
+        break
+    else:
+        print("Please enter some alphanumeric characters.")
 
-    while True:
-        print("Please enter some text (alphanumeric characters only): ", end="")
-        s = input().strip()
-
-        if not s:
-            continue
-
-        if s.isalnum():
-            break
-        else:
-            print("Please enter some alphanumeric characters.")
-
-    output = get_hash(s)
-    print(f"Output: {output}")
+output = hashlib.md5(s.encode()).hexdigest()
+print(f"Output: {output}")
