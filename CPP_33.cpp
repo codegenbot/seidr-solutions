b6c78c02aa9f08a1be24afc3a7c00a9a80343c3e
beep boop <githubbot@vadim.me>
2024-07-12 14:27:42 +0200

    Solution 18 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d8rx6wrb Prompt: In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 49fbcb93-9706-4c98-a92f-43bbb2d198f5.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 49fbcb93-9706-4c98-a92f-43bbb2d198f5.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 49fbcb93-9706-4c98-a92f-43bbb2d198f5.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 49fbcb93-9706-4c98-a92f-43bbb2d198f5.cpp:24: 49fbcb93-9706-4c98-a92f-43bbb2d198f5.cpp: In function ‘int main()’: 49fbcb93-9706-4c98-a92f-43bbb2d198f5.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,20 +1,21 @@
-#include <vector>
 #include <algorithm>
+#include <assert.h>
+#include <vector>
 
-bool issame(vector<int> a,vector<int>b){
-    return a == b;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return (a == b);
 }
 
-vector<int> sort_third(vector<int> l) {
-    vector<int> result;
+std::vector<int> sort_third(std::vector<int> l) {
+    std::vector<int> result;
     for (int i = 0; i < l.size(); i++) {
         if (i % 3 == 0) {
             // values at the indices that are divisible by three are equal to the values of the corresponding indicies of l, but sorted
-            vector<int> temp;
+            std::vector<int> temp;
             for (int j = i; j < l.size() && j % 3 == 0; j++) {
                 temp.push_back(l[j]);
             }
-            sort(temp.begin(), temp.end());
+            std::sort(temp.begin(), temp.end());
             for (int k = 0; k < temp.size(); k++) {
                 result.push_back(temp[k]);
             }
