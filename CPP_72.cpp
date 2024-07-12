0ea2728e65ce6f4abb38df7be801e56668792989
beep boop <githubbot@vadim.me>
2024-07-12 15:03:18 +0200

    Solution 17 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eui281em Prompt:       |           ^~~~~~ In file included from 478e8e49-a83e-47e1-9629-df13ff8d4ad8.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 478e8e49-a83e-47e1-9629-df13ff8d4ad8.cpp:4:32: error: expected primary-expressio n before ‘int’     4 | bool will_it_fly(vector<vector<int>>& q, int w) {       |                                ^~~ 478e8e49-a83e-47e1-9629-df13ff8d4ad8.cpp:4:42: error: expected primary-expressio n before ‘int’     4 | bool will_it_fly(vector<vector<int>>& q, int w) {       |                                          ^~~ 478e8e49-a83e-47e1-9629-df13ff8d4ad8.cpp:4:47: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | bool will_it_fly(vector<vector<int>>& q, int w) {       |                                               ^ In file included from 478e8e49-a83e-47e1-9629-df13ff8d4ad8.cpp:18: 478e8e49-a83e-47e1-9629-df13ff8d4ad8.cpp: In function ‘int main()’: 478e8e49-a83e-47e1-9629-df13ff8d4ad8.cpp:20:24: error: ‘will_it_fly’ cannot be u sed as a function    20 |     assert (will_it_fly({5}, 5) == true);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -7,16 +7,12 @@ bool will_it_fly(std::vector<std::vector<int>>& q, int w) {
     for(int i : q[0]) {
         str += std::to_string(i);
     }
-    
-    std::reverse(str.begin(), str.end());
-    if(str != std::to_string(stoi(str))) 
+    if(str != std::to_string(reverse(str).s))  // reverse function does not exist in the standard library
         return false;
-    
     int sum = 0;
     for(int i : q[0]) {
         sum += i;
     }
-    
     return sum <= w;
 }
 
