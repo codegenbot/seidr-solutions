0a593ffe7738fcd4abbb335bbb778651eba95057
beep boop <githubbot@vadim.me>
2024-07-12 15:03:37 +0200

    Solution 45 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/avzqcthh Prompt: 7c6611c2-4a8c-4076-8474-b36efee5e299.cpp:1:6: error: ‘vector’ in namespace ‘std’  does not name a template type     1 | std::vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};       |      ^~~~~~ 7c6611c2-4a8c-4076-8474-b36efee5e299.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | std::vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f}; 7c6611c2-4a8c-4076-8474-b36efee5e299.cpp: In function ‘int main()’: 7c6611c2-4a8c-4076-8474-b36efee5e299.cpp:5:5: error: ‘vector’ was not declared i n this scope     5 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 7c6611c2-4a8c-4076-8474-b36efee5e299.cpp:5:12: error: expected primary-expressio n before ‘float’     5 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~ In file included from 7c6611c2-4a8c-4076-8474-b36efee5e299.cpp:3: 7c6611c2-4a8c-4076-8474-b36efee5e299.cpp:6:13: error: ‘has_close_elements’ was n ot declared in this scope     6 |     assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false) ;       |             ^~~~~~~~~~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,10 +1,17 @@
 #include <vector>
-#include <assert.h>
+
+int has_close_elements(std::vector<float> a, float threshold) {
+    for(int i = 0; i < a.size() - 1; i++) {
+        if(std::abs(a[i] - a[i + 1]) <= threshold)
+            return 1;
+    }
+    return 0;
+}
 
 int main() {
     std::vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
-
-    assert(std::has_close_elements({1.1f, 2.2f, 3.1f, 4.1f, 5.1f}, 0.5f) == false);
+    
+    assert(std::has_close_elements(a, 0.1f) == 1);
 
     return 0;
 }
