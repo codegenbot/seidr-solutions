302c1aae22af1999dfba863af3df5c5aa65f7fde
beep boop <githubbot@vadim.me>
2024-07-13 00:29:51 +0200

    Solution 30 of mastermind, ratio 0.58 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/lpmwf7d9 Prompt:    13 |             guessCount[guess[i]]++;       |             ^~~~~~~~~~ 171cb5a7-01fc-478c-b1ff-7f2919b82a9a.cpp:17:21: error: ‘codeCount’ was not decla red in this scope    17 |     for (auto& it : codeCount) {       |                     ^~~~~~~~~ 171cb5a7-01fc-478c-b1ff-7f2919b82a9a.cpp:18:33: error: ‘guessCount’ was not decl ared in this scope    18 |         white += min(it.second, guessCount[it.first]);       |                                 ^~~~~~~~~~ 171cb5a7-01fc-478c-b1ff-7f2919b82a9a.cpp:18:18: error: ‘min’ was not declared in  this scope; did you mean ‘main’?    18 |         white += min(it.second, guessCount[it.first]);       |                  ^~~       |                  main 171cb5a7-01fc-478c-b1ff-7f2919b82a9a.cpp:21:5: error: ‘cout’ was not declared in  this scope    21 |     cout << white << endl << black << endl;       |     ^~~~ 171cb5a7-01fc-478c-b1ff-7f2919b82a9a.cpp:21:22: error: ‘endl’ was not declared i n this scope    21 |     cout << white << endl << black << endl;       |                      ^~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
-#include <vector>
-using namespace std;
+#include <string>
+#include <map>
+#include <algorithm>
 
 int main() {
-    string code, guess;
-    cin >> code >> guess;
+    std::string code, guess;
+    std::cin >> code >> guess;
     
     int black = 0, white = 0;
-    vector<int> codeFreq(6, 0), guessFreq(6, 0);
+    std::map<char, int> codeCount, guessCount;
     
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             black++;
         } else {
-            codeFreq[code[i] - 'A']++;
-            guessFreq[guess[i] - 'A']++;
+            codeCount[code[i]]++;
+            guessCount[guess[i]]++;
         }
     }
     
-    for (int i = 0; i < 6; ++i) {
-        white += min(codeFreq[i], guessFreq[i]);
+    for (auto& it : codeCount) {
+        white += std::min(it.second, guessCount[it.first]);
     }
     
-    cout << white << "\n" << black << "\n";
+    std::cout << white << std::endl << black << std::endl;
     
     return 0;
 }
