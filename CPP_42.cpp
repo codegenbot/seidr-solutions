3c0131ced48f37813767851f6022a8dc17bbb9ec
beep boop <githubbot@vadim.me>
2024-07-12 11:26:24 +0200

    Solution 15 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/q9bacny6 Prompt:     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bf49d87a-82e0-4bb2-8c9b-c2b2885128e2.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bf49d87a-82e0-4bb2-8c9b-c2b2885128e2.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bf49d87a-82e0-4bb2-8c9b-c2b2885128e2.cpp:7: bf49d87a-82e0-4bb2-8c9b-c2b2885128e2.cpp: In function ‘int main()’: bf49d87a-82e0-4bb2-8c9b-c2b2885128e2.cpp:17:20: error: ‘incr_list’ was not decla red in this scope    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ bf49d87a-82e0-4bb2-8c9b-c2b2885128e2.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,8 +1,16 @@
-bool issame(const vector<int>& a, const vector<int>& b) {
-    return a == b;
+bool issame(const vector<int> &a, const vector<int> &b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-vector<int> incr_list(const vector<int>& l) {
+vector<int> incr_list(const vector<int> &l) {
     vector<int> result = l;
     for (int i = 0; i < result.size(); i++) {
         result[i]++;
@@ -11,13 +19,15 @@ vector<int> incr_list(const vector<int>& l) {
 }
 
 int main() {
-    vector<int> input = {1, 2, 3, 4, 5};
-    vector<int> output = incr_list(input);
-    // Use the issame function to compare output with the expected result
-    if (issame(output, {2, 3, 4, 5, 6})) {
-        cout << "Output is correct!";
+    vector<int> l = {1, 2, 3};
+    vector<int> modified_l = incr_list(l);
+    vector<int> expected_l = {2, 3, 4};
+
+    if (issame(modified_l, expected_l)) {
+        cout << "Test passed." << endl;
     } else {
-        cout << "Output is incorrect!";
+        cout << "Test failed." << endl;
     }
+
     return 0;
 }
