3cf27d2de9b5792679e2fb2329c69f3ed0ebb4f4
beep boop <githubbot@vadim.me>
2024-07-12 19:20:50 +0200

    Solution 60 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mm8jcipt Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 11702c33-7c5a-406a-9592-cae7691eecc7.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 11702c33-7c5a-406a-9592-cae7691eecc7.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 11702c33-7c5a-406a-9592-cae7691eecc7.cpp:26: 11702c33-7c5a-406a-9592-cae7691eecc7.cpp: In function ‘int main()’: 11702c33-7c5a-406a-9592-cae7691eecc7.cpp:36:20: error: ‘sorted_list_sum’ was not  declared in this scope    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 11702c33-7c5a-406a-9592-cae7691eecc7.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,13 +1,14 @@
-```
-vector<string> issame(vector<string> lst) {
+vector<string> sorted_list_sum(vector<string> lst) {
     vector<string> result;
 
+    // Delete strings that have odd lengths from the list
     for (const auto& str : lst) {
         if (str.length() % 2 == 0) {
             result.push_back(str);
         }
     }
 
+    // Sort the resulted vector by length and then alphabetically
     sort(result.begin(), result.end(),
          [](const string& a, const string& b) {
              if (a.length() != b.length()) {
