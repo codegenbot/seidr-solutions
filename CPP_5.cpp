dcf206a19848a36e02fa30eeaa91721f1b1c02f6
beep boop <githubbot@vadim.me>
2024-07-12 09:01:06 +0200

    Solution 33 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/pp89dacn Prompt:       |                    ^~~ d1668242-55ab-4617-b118-fe5ac3be2e2a.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d1668242-55ab-4617-b118-fe5ac3be2e2a.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d1668242-55ab-4617-b118-fe5ac3be2e2a.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d1668242-55ab-4617-b118-fe5ac3be2e2a.cpp:12: d1668242-55ab-4617-b118-fe5ac3be2e2a.cpp: In function ‘int main()’: d1668242-55ab-4617-b118-fe5ac3be2e2a.cpp:22:20: error: ‘intersperse’ was not dec lared in this scope    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ d1668242-55ab-4617-b118-fe5ac3be2e2a.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -1,22 +1,25 @@
 #include <vector>
 
 bool is_same(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()){
+    if(a.size() != b.size()) {
         return false;
     }
-    for(int i = 0; i < a.size(); ++i){
-        if(a[i] != b[i]){
+    
+    for(int i = 0; i < a.size(); ++i) {
+        if(a[i] != b[i]) {
             return false;
         }
     }
+    
     return true;
 }
 
 int main() {
     vector<int> numbers = {1, 2, 3, 4, 5};
-    int delimiter = 0;
-    vector<int> result = intersperse(numbers, delimiter);
-
+    int delimeter = 0;
+    
+    vector<int> result = intersperse(numbers, delimeter);
+    
     for(int i = 0; i < result.size(); ++i) {
         cout << result[i] << " ";
     }
