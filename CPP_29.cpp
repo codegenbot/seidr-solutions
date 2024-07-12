0309237217b309c58bb7dd613aa4a3d034c484ed
beep boop <githubbot@vadim.me>
2024-07-12 10:41:20 +0200

    Solution 55 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/21owtffi Prompt: dfd861d6-59aa-408d-9b44-6663e6ee98c4.cpp:11:1: warning: no return statement in f unction returning non-void [-Wreturn-type]    11 | }       | ^ dfd861d6-59aa-408d-9b44-6663e6ee98c4.cpp: At global scope: dfd861d6-59aa-408d-9b44-6663e6ee98c4.cpp:30:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    30 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ dfd861d6-59aa-408d-9b44-6663e6ee98c4.cpp:9:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     9 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ dfd861d6-59aa-408d-9b44-6663e6ee98c4.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ dfd861d6-59aa-408d-9b44-6663e6ee98c4.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -12,7 +12,6 @@ bool issame(vector<string> a, vector<string> b) {
 
 int main() {
     assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"), vector<string>{"xxx", "xxxAAA", "xxx"}));
-    
     return 0;
 }
 
