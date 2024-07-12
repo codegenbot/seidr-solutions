f53d8d4fe601f515d1fa1d391689d87a4fe8cf07
beep boop <githubbot@vadim.me>
2024-07-12 13:53:21 +0200

    Solution 4 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/vt99v750 Prompt:       |     ^~~~ 22395df8-911c-46ff-a222-d187d089b16b.cpp:19:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 22395df8-911c-46ff-a222-d187d089b16b.cpp:19:13: error: ‘fixed’ was not declared in this scope    19 |     cout << fixed << setprecision(1) << shoppingList(&prices[0], &discou nts[0], n) << endl;       |             ^~~~~ 22395df8-911c-46ff-a222-d187d089b16b.cpp:19:22: error: ‘setprecision’ was not de clared in this scope    19 |     cout << fixed << setprecision(1) << shoppingList(&prices[0], &discou nts[0], n) << endl;       |                      ^~~~~~~~~~~~ 22395df8-911c-46ff-a222-d187d089b16b.cpp:19:87: error: ‘endl’ was not declared i n this scope    19 |  setprecision(1) << shoppingList(&prices[0], &discounts[0], n) << endl;       |                                                                   ^~~~ 22395df8-911c-46ff-a222-d187d089b16b.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- shopping-list.cpp
+++ shopping-list.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+```
 #include <vector>
+#include <iostream>
 #include <iomanip>
-
 using namespace std;
 
 double shoppingList(double price[], double discount[], int n) {
@@ -19,6 +19,6 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> prices[i] >> discounts[i];
     }
-    cout << fixed << setprecision(1) << shoppingList(&prices[0], &discounts[0], n) << endl;
+    cout << fixed << setprecision(2) << shoppingList(&prices[0], &discounts[0], n) << endl;
     return 0;
 }
