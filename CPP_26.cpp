614ab1ee17ad84d3b161b5f319b1558ed69ac84e
beep boop <githubbot@vadim.me>
2024-07-12 18:51:45 +0200

    Solution 13 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/glxnmwbk Prompt: 4ca27c41-70e3-4c6e-b5ae-d574977564f3.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4ca27c41-70e3-4c6e-b5ae-d574977564f3.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 4ca27c41-70e3-4c6e-b5ae-d574977564f3.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 4ca27c41-70e3-4c6e-b5ae-d574977564f3.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 4ca27c41-70e3-4c6e-b5ae-d574977564f3.cpp:24: 4ca27c41-70e3-4c6e-b5ae-d574977564f3.cpp: In function ‘int main()’: 4ca27c41-70e3-4c6e-b5ae-d574977564f3.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,26 +1,7 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-bool sameVector(std::vector<int> a, std::vector<int> b) {
+```bool areSameVectors(std::vector<int> a, std::vector<int> b) {
     if(a.size() != b.size()) return false;
     for(int i = 0; i < a.size(); i++) {
         if(a[i] != b[i]) return false;
     }
     return true;
-}
-
-std::vector<int> remove_duplicates(std::vector<int> numbers){
-    std::vector<int> result;
-    for(int num : numbers){
-        if(find(result.begin(), result.end(), num) == result.end()){
-            result.push_back(num);
-        }
-    }
-    return result;
-}
-
-int main() {
-    assert(sameVector(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5}));
-    return 0;
-}
+}``
