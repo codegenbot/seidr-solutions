da371409a9230ee01b7ab92f7cb32f9e5d59e7e9
beep boop <githubbot@vadim.me>
2024-07-12 13:07:27 +0200

    Solution 27 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/545auy28 Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 341501ef-6bc4-43e0-8586-94816f1d34e7.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 341501ef-6bc4-43e0-8586-94816f1d34e7.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 341501ef-6bc4-43e0-8586-94816f1d34e7.cpp:11: 341501ef-6bc4-43e0-8586-94816f1d34e7.cpp: In function ‘int main()’: 341501ef-6bc4-43e0-8586-94816f1d34e7.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 341501ef-6bc4-43e0-8586-94816f1d34e7.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,5 +1,4 @@
 #include <boost/any.hpp>
-#include <vector>
 
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
@@ -9,7 +8,9 @@ vector<int> filter_integers(vector<boost::any> values) {
     vector<int> result;
     for (const auto& value : values) {
         int val = boost::any_cast<int>(value);
-        if (!boost::blank(&val)) { // handle non-integer cases
+        if (!std::is_same<decltype(val), int>::value)
+            val = 0; // handle non-integer cases
+        if (val != 0) { 
             result.push_back(val);
         }
     }
@@ -17,6 +18,6 @@ vector<int> filter_integers(vector<boost::any> values) {
 }
 
 int main() {
-    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), vector<int>{3, 3, 3}));
+    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), vector<int>({3, 3, 3})));
     return 0;
 }
