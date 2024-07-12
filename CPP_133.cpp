dab7502fb4a0d2c60981748da47ea2c556f4227c
beep boop <githubbot@vadim.me>
2024-07-12 23:24:26 +0200

    Solution 31 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/y3xpzjxs Prompt:       | ^~~~~~~~~~~~~ 0dc30d25-832d-4212-8da8-e732866e633b.cpp:38:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    38 | int main(){       |         ^~ 0dc30d25-832d-4212-8da8-e732866e633b.cpp:38:9: note: remove parentheses to defau lt-initialize a variable    38 | int main(){       |         ^~       |         -- 0dc30d25-832d-4212-8da8-e732866e633b.cpp:38:9: note: or replace parentheses with  braces to value-initialize a variable 0dc30d25-832d-4212-8da8-e732866e633b.cpp:38:11: error: a function-definition is not allowed here before ‘{’ token    38 | int main(){       |           ^ 0dc30d25-832d-4212-8da8-e732866e633b.cpp:40:2: error: expected ‘}’ at end of inp ut    40 | }       |  ^ 0dc30d25-832d-4212-8da8-e732866e633b.cpp:5:40: note: to match this ‘{’     5 | int sum_squares(std::vector<float> lst){       |                                        ^

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,4 +1,11 @@
-int main {
+int sum_squares(std::vector<float> lst){
+    int total = 0;
+    for(float x : lst){
+        total += std::ceil(x)*std::ceil(x);
+    }
+    return total;
+
+int main() {
     std::vector<float> input;
 
     std::cout << "Enter numbers (enter -1 to stop): ";
