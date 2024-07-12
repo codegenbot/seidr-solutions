aba6f1484ea442c0e6171796d851ecf69aaa306f
beep boop <githubbot@vadim.me>
2024-07-12 13:48:50 +0200

    Solution 29 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3pjnfwz2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f6d0aad1-6422-48e2-a02f-23af2dd93 28f.py", line 20, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f6d0aad1-6422-48e2-a02f-23af2dd93 28f.py", line 17, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f6d0aad1-6422-48e2-a02f-23af2dd93 28f.py", line 13, in make_palindrome     palindrome += new_string[i]                   ~~~~~~~~~~^^^ IndexError: string index out of range

--- Python_10.py
+++ Python_10.py
@@ -1,6 +1,6 @@
-```
 def is_palindrome(s: str) -> bool:
     return s == s[::-1]
+
 
 def make_palindrome(string: str) -> str:
     if is_palindrome(string):
@@ -10,8 +10,8 @@ def make_palindrome(string: str) -> str:
     palindrome = ""
     for i in range(n):
         while not is_palindrome(new_string):
-            new_string = new_string[:i+1] + "#"
-        if i < n:  
-        palindrome += new_string[i]
+            new_string = new_string[: i + 1] + "#"
+        if i < n:
+            palindrome += new_string[i]
         new_string = new_string[1:]
     return palindrome
