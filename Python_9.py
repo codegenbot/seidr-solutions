c140f9e59df611a5df43c9857d17c1cb51401c8c
beep boop <githubbot@vadim.me>
2024-07-12 14:48:33 +0200

    Solution 81 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/mq5u2983 Prompt: Complete the following code given the task description and function signature.

--- Python_9.py
+++ Python_9.py
@@ -1,3 +1,8 @@
 def rolling_max(numbers: List[int]) -> List[int]:
-    result = [max(numbers[: i + 1]) for i in range(len(numbers))]
+    result = []
+    max_until_now = numbers[0]
+    for num in numbers:
+        if num > max_until_now:
+            max_until_now = num
+        result.append(max_until_now)
     return result
