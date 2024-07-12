8649dcdce2950995a3019a061bf5879eded9b14f
beep boop <githubbot@vadim.me>
2024-07-12 18:40:49 +0200

    Solution 18 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0hrfoi8h Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 68e9c623-f4d6-44cf-ba54-2c5adbc345f7.cpp:13:25:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -3,11 +3,8 @@
 using namespace std;
 
 vector<int> rolling_max(vector<int> numbers) {
-    vector<int> result;
-    if (numbers.empty()) {
-        return result; 
-    }
-    int max = numbers[0];
+    vector<int> result({}); // Initialize the result with an empty vector
+    int max = (numbers.empty()) ? INT_MIN : numbers[0];
     for (int i = 1; i < numbers.size(); i++) {
         if (numbers[i] > max) {
             max = numbers[i];
@@ -15,13 +12,14 @@ vector<int> rolling_max(vector<int> numbers) {
         result.push_back(max);
     }
     return result;
+}
 
 int main() {
     vector<int> numbers = {1, 2, 3, 4, 5};
     vector<int> maxNumbers = rolling_max(numbers);
 
     for (int i = 0; i < maxNumbers.size(); i++) {
-        cout << "Maximum up to " << i + 1 << " is " << maxNumbers[i] << endl;
+        cout << "Maximum up to " << numbers[i] << " is " << maxNumbers[i] << endl;
     }
 
     return 0;
