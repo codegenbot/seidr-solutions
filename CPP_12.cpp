c512e6908b3354dc3925f733301aeece99388611
beep boop <githubbot@vadim.me>
2024-07-13 01:10:42 +0200

    Solution 10 of CPP/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/j2dqiedd Prompt: 5923dcf8-fe7c-4806-a86f-90362420f459.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string longest(vector<string> strings){       | ^~~~~~ In file included from 5923dcf8-fe7c-4806-a86f-90362420f459.cpp:11: 5923dcf8-fe7c-4806-a86f-90362420f459.cpp: In function ‘int main()’: 5923dcf8-fe7c-4806-a86f-90362420f459.cpp:13:13: error: ‘longest’ was not declare d in this scope; did you mean ‘long’?    13 |     assert (longest({"x", "yyy", "zzzz", "www", "kkkk", "abc"}) == "zzzz ");       |             ^~~~~~~

--- CPP_12.cpp
+++ CPP_12.cpp
@@ -1,13 +1,18 @@
+#include <string>
+using namespace std;
+
 string longest(vector<string> strings){
     if(strings.empty()) return "";
     string result = strings[0];
-    for(auto s : strings){
-        if(s.length() > result.length()){
-            result = s;
-        }
-        else if(s.length() == result.length()){
-            result = s;
-        }
+    for(auto str: strings){
+        if(str.length() > result.length())
+            result = str;
     }
     return result;
+}
+
+int main(){
+    vector<string> v={"x", "yyy", "zzzz", "www", "kkkk", "abc"};
+    cout << longest(v) << endl;
+    return 0;
 }
