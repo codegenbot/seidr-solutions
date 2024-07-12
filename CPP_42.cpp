a6f2c8f49887715039aa607a31f028eafc147833
beep boop <githubbot@vadim.me>
2024-07-12 14:35:53 +0200

    Solution 27 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zj95medv Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from c9ce9a53-401d-4e30-92e8-c73edb8e7c4d.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -9,15 +9,15 @@ bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-vector<int> incr_list(const initializer_list<int>& l){
+vector<int> incr_list(initializer_list<int> l){
     vector<int> result(l.size());
-    for(int i = 0; i < l.size(); i++) {
-        result[i] = l.begin()[i]+1;
+    for(int i = 0; i < l.size(); ++i) {
+        result[i] = l[i]+1;
     }
     return result;
 }
 
 int main() {
-    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6,3, 4, 4, 10, 1, 124}));
+    assert(issame(vector<int>(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123})) , {6, 3, 6,3, 4, 4, 10, 1, 124}));
     return 0;
 }
