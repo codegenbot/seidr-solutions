0753cd63e320c10448f47435d59d87af9c7bfccb
beep boop <githubbot@vadim.me>
2024-07-12 06:12:37 +0200

    Solution 31 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7bd6h5bq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f10580fd-3f7d-4116-bf37-cccadb6a6 4d2.py", line 17, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f10580fd-3f7d-4116-bf37-cccadb6a6 4d2.py", line 12, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,10 +1,3 @@
-from itertools import groupby
-
-
+```Python
 def sort_third(lst):
-    grouped = [(i // 3, j) for i, j in enumerate(lst)]
-    grouped.sort()
-    return [
-        min(group[1:]) if group[0] % 3 != 0 else max(group[1:])
-        for key, group in groupby(grouped, key=lambda x: (x[0]))
-    ]
+    return sorted([i for i in lst if (lst.index(i) + 1) % 3 == 0])
