02597300cc7cf20007d5ab081bf52e8ba39b80c7
beep boop <githubbot@vadim.me>
2024-07-12 14:27:20 +0200

    Solution 15 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/owlfkvjg Prompt:    30 | bool issame(vector<float> a,vector<float>b){       |                    ^~~~~ 71a7c45c-b3c3-413d-9406-64726e1ef6a4.cpp:30:29: error: ‘vector’ was not declared  in this scope    30 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 71a7c45c-b3c3-413d-9406-64726e1ef6a4.cpp:30:36: error: expected primary-expressi on before ‘float’    30 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from 71a7c45c-b3c3-413d-9406-64726e1ef6a4.cpp:29: 71a7c45c-b3c3-413d-9406-64726e1ef6a4.cpp: In function ‘int main()’: 71a7c45c-b3c3-413d-9406-64726e1ef6a4.cpp:39:20: error: ‘sort_even’ was not decla red in this scope    39 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 71a7c45c-b3c3-413d-9406-64726e1ef6a4.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,6 +1,7 @@
-```
+```cpp
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 bool issame(vector<float> a, vector<float> b) {
     if (a.size() != b.size())
@@ -13,16 +14,17 @@ bool issame(vector<float> a, vector<float> b) {
 
 vector<float> sort_even(vector<float> l) {
     vector<float> result(l.size());
+    int even_index = 0;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            vector<float> even_indices;
+            vector<float> even_numbers;
             for (int j = 0; j < l.size(); j++) {
-                if (j % 2 == 0) {
-                    even_indices.push_back(l[j]);
-                }
+                if (j % 2 == 0)
+                    even_numbers.push_back(l[j]);
             }
-            sort(even_indices.begin(), even_indices.end());
-            result[i] = even_indices[0];
+            sort(even_numbers.begin(), even_numbers.end());
+            result[i] = even_numbers[even_index];
+            even_index++;
         } else {
             result[i] = l[i];
         }
