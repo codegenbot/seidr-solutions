386ef36faa947e998f8b147b35a1552655da05de
beep boop <githubbot@vadim.me>
2024-07-12 17:39:15 +0200

    Solution 95 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eui281em Prompt: rgument 1 from ‘<brace-enclosed initializer list>’ to ‘std::vector<int>::value_t ype&&’ {aka ‘int&&’}  1203 |       push_back(value_type&& __x)       |                 ~~~~~~~~~~~~~^~~ 84195519-0444-4b87-9750-0ae102044c57.cpp: At global scope: 84195519-0444-4b87-9750-0ae102044c57.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 84195519-0444-4b87-9750-0ae102044c57.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 84195519-0444-4b87-9750-0ae102044c57.cpp:32: 84195519-0444-4b87-9750-0ae102044c57.cpp: In function ‘int main()’: 84195519-0444-4b87-9750-0ae102044c57.cpp:34:24: error: converting to ‘std::vecto r<std::vector<int> >’ from initializer list would use explicit constructor ‘std: :vector<_Tp, _Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocato r_type&) [with _Tp = std::vector<int>; _Alloc = std::allocator<std::vector<int> >; std::vector<_Tp, _Alloc>::size_type = long unsigned int; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<std::vector<int> >]’    34 |     assert (will_it_fly({5}, 5) == true);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -13,11 +13,11 @@ bool will_it_fly(vector<vector<int>> q, int w) {
 int main() {
     int n;  
     cin >> n;
-    vector<vector<int>> q(n);  
+    vector<vector<int>> q(n);
     for(int i=0; i<n; i++) {
         int x, y; 
         cin >> x >> y;
-        q[i].push_back({x,y});
+        q[i].push_back(x,y);
     }
     int w;
     cin >> w;
