dca5d07899d60d371f693b15ef3b4b9f530c7f66
beep boop <githubbot@vadim.me>
2024-07-12 16:04:24 +0200

    Solution 7 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/20nxyk55 Prompt: b/python3.11/site-packages/programlib/programs/15863383-2b07-4f6a-b163-ccde99969 075.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/15863383-2b07-4f6a-b163-ccde99969 075.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/15863383-2b07-4f6a-b163-ccde99969 075.py", line 2, in sort_third     return sorted([element for sublist in [sorted(l[i:i+3]) for i in range(0, le n(l), 3)] for sublist in sublist for element in sublist])                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/15863383-2b07-4f6a-b163-ccde99969 075.py", line 2, in <listcomp>     return sorted([element for sublist in [sorted(l[i:i+3]) for i in range(0, le n(l), 3)] for sublist in sublist for element in sublist])                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: 'int' object is not iterable

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,2 @@
 def sort_third(l: list):
-    return sorted(
-        [element for i in range(0, len(l), 3) for element in sorted(l[i : i + 3])]
-    )
+    return sorted([element for sublist in [sorted(l[i:i+3]) for i in range(0, len(l), 3)] for sublist in sublist for element in sublist])
