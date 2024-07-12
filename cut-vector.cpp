ae6ce7bd7ecfe866eaef96541eef2daed5eed981
beep boop <githubbot@vadim.me>
2024-07-12 14:54:45 +0200

    Solution 2 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/esdxnjzt Prompt: ared in this scope    25 |     for (int num : subvector1) {       |                    ^~~~~~~~~~ 8f13fd30-4ef3-42be-b8f5-37a7d990bd71.cpp:26:9: error: ‘cout’ was not declared in  this scope    26 |         cout << num << endl;       |         ^~~~ 8f13fd30-4ef3-42be-b8f5-37a7d990bd71.cpp:26:24: error: ‘endl’ was not declared i n this scope    26 |         cout << num << endl;       |                        ^~~~ 8f13fd30-4ef3-42be-b8f5-37a7d990bd71.cpp:28:20: error: ‘subvector2’ was not decl ared in this scope    28 |     for (int num : subvector2) {       |                    ^~~~~~~~~~ 8f13fd30-4ef3-42be-b8f5-37a7d990bd71.cpp:29:9: error: ‘cout’ was not declared in  this scope    29 |         cout << num << endl;       |         ^~~~ 8f13fd30-4ef3-42be-b8f5-37a7d990bd71.cpp:29:24: error: ‘endl’ was not declared i n this scope    29 |         cout << num << endl;       |                        ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,42 +1,34 @@
+#include <iostream>
 int main() {
-    vector<int> nums;
-    int num;
-    
-    while (cin >> num) {
-        nums.push_back(num);
-    }
-    
-    int n = nums.size();
-    int sum = 0;
+    int n;
+    std::cin >> n;
+    std::vector<int> nums(n);
     for (int i = 0; i < n; ++i) {
-        sum += nums[i];
+        std::cin >> nums[i];
     }
-    
-    int half_sum = sum / 2;
-    int prefix_sum = 0;
-    int cut_idx = -1;
-    
+
+    int sum = std::accumulate(nums.begin(), nums.end(), 0);
+    int target = sum / 2;
+    int prefixSum = 0;
+    int idx = 0;
+
     for (int i = 0; i < n; ++i) {
-        prefix_sum += nums[i];
-        if (prefix_sum >= half_sum) {
-            cut_idx = i;
+        prefixSum += nums[i];
+        if (prefixSum >= target) {
+            idx = i;
             break;
         }
     }
-    
-    if (abs(sum - 2 * prefix_sum) < abs(sum - 2 * (prefix_sum - nums[cut_idx]))) {
-        cout << "1\n";
-        for (int i = 0; i <= cut_idx; ++i) {
-            cout << nums[i] << "\n";
-        }
-        cout << "0\n";
-    } else {
-        cout << "1\n";
-        for (int i = 0; i < cut_idx; ++i) {
-            cout << nums[i] << "\n";
-        }
-        cout << nums[cut_idx] << "\n0\n";
+
+    std::vector<int> subvector1(nums.begin(), nums.begin() + idx + 1);
+    std::vector<int> subvector2(nums.begin() + idx + 1, nums.end());
+
+    for (int num : subvector1) {
+        std::cout << num << std::endl;
     }
-    
+    for (int num : subvector2) {
+        std::cout << num << std::endl;
+    }
+
     return 0;
 }
