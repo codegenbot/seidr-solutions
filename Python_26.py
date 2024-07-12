5de9678d648feac34a870ed18051441cad3eb4d5
beep boop <githubbot@vadim.me>
2024-07-12 07:20:29 +0200

    Solution 78 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/t61x2loc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0532535a-f9f4-4cdb-b563-b94a877eb e91.py", line 11, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0532535a-f9f4-4cdb-b563-b94a877eb e91.py", line 8, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_26.py
+++ Python_26.py
@@ -1,6 +1,7 @@
+```
 def remove_duplicates(input_numbers=None):
     if input_numbers is None:
         input_str = input("Enter numbers separated by space: ")
         input_numbers = list(map(int, input_str.split()))
-    unique_numbers = list(set(input_numbers))
+    unique_numbers = [x for i, x in enumerate(input_numbers) if x not in input_numbers[:i]]
     return unique_numbers
