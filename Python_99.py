a55e1350c2a8a3c1dc8a35a1f617cbf5cd6fcb80
beep boop <githubbot@vadim.me>
2024-07-12 08:26:29 +0200

    Solution 17 of Python/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/rz1ujt0v Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a4b8e7a6-dc56-401b-916b-355459a82 4e8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_99.py
+++ Python_99.py
@@ -1,4 +1,3 @@
-```
 def find_closest_number():
     while True:
         try:
@@ -6,15 +5,7 @@ def find_closest_number():
             closest_value = round(value)
             if value != closest_value:
                 continue
-            return closest_value
+            else:
+                return closest_value
         except ValueError:
             print("Invalid input. Please enter a decimal number.")
-
-def check_closest(closest_integer):
-    if closest_integer % 2 == 0:
-        print(f"{closest_integer} is an even number.")
-    else:
-        print(f"{closest_integer} is an odd number.")
-
-find_closest_number()
-check_closest(find_closest_number())
