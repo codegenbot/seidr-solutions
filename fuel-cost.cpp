79bbb58c3c67bf5e36d5648c7cc9f33b7b8dd901
beep boop <githubbot@vadim.me>
2024-07-12 22:04:38 +0200

    Solution 10 of fuel-cost, ratio 0.86 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/blgr651f Prompt:       |            ^~~ 1410eb4f-db60-420a-849c-016b9f836432.cpp:4:5: error: ‘cin’ was not declared in t his scope     4 |     cin >> n;       |     ^~~ 1410eb4f-db60-420a-849c-016b9f836432.cpp:7:9: error: ‘nums’ was not declared in this scope; did you mean ‘num’?     7 |         nums.push_back((num / 3) - 2);       |         ^~~~       |         num 1410eb4f-db60-420a-849c-016b9f836432.cpp:11:16: error: ‘nums’ was not declared i n this scope; did you mean ‘num’?    11 |         sum += nums[i];       |                ^~~~       |                num 1410eb4f-db60-420a-849c-016b9f836432.cpp:13:5: error: ‘cout’ was not declared in  this scope    13 |     cout << sum << endl;       |     ^~~~ 1410eb4f-db60-420a-849c-016b9f836432.cpp:13:20: error: ‘endl’ was not declared i n this scope    13 |     cout << sum << endl;       |                    ^~~~

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -1,16 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 int main() {
     vector<int> nums;
-    int n;
-    while (cin >> n) {
-        nums.push_back(n);
+    int n, num;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> num;
+        nums.push_back((num / 3) - 2);
     }
-
     int sum = 0;
-    for (int num : nums) {
-        sum += max(num / 3 - 2, 0);
+    for (int i = 0; i < n; ++i) {
+        sum += nums[i];
     }
-
     cout << sum << endl;
-
     return 0;
 }
