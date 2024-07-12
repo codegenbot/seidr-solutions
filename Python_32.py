98895295183b4f2207352fcd2cc8806160763536
beep boop <githubbot@vadim.me>
2024-07-12 17:45:42 +0200

    Solution 30 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/jngxxhoi Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -2,12 +2,10 @@ def find_zero(xs: list):
     degree = len(xs) - 1
     a = xs[0]
     b = xs[-1]
-
     if degree == 1:
         return -a / b
     else:
         return b / a
-
 
 xs = list(map(int, input().split()))
 result = find_zero(xs)
