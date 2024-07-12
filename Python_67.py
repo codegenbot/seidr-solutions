351c2f96b676338e81685da5fd6bead3b68f1260
beep boop <githubbot@vadim.me>
2024-07-12 07:38:41 +0200

    Solution 30 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/rq5tl5sf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/be814c3d-1d2b-4f7f-bed5-c03197fda ea1.py", line 21, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/be814c3d-1d2b-4f7f-bed5-c03197fda ea1.py", line 18, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: fruit_distribution() takes 1 positional argument but 2 were given

--- Python_67.py
+++ Python_67.py
@@ -1,8 +1,8 @@
-def fruit_distribution(s):
-    s = s.lower().replace("apples", "0").replace("oranges", "0")
+def fruit_distribution(input_string):
+    input_string = input_string.lower().replace("apples", "0").replace("oranges", "0")
     total = 0
     current_number = ""
-    for part in s.split():
+    for part in input_string.split():
         if part.isdigit() or part == "and":
             if part.isdigit():
                 total += int(part)
