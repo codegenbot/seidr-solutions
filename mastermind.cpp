6e556e6a7ee007bc53befbee84d306a1210bd69e
beep boop <githubbot@vadim.me>
2024-07-12 13:32:14 +0200

    Solution 10 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/lpmwf7d9 Prompt:    12 |             codeFreq[code[i] - 'A']++;       |             ^~~~~~~~ 0f3d99d2-476f-4336-a980-49911d22b679.cpp:13:13: error: ‘guessFreq’ was not decla red in this scope    13 |             guessFreq[guess[i] - 'A']++;       |             ^~~~~~~~~ 0f3d99d2-476f-4336-a980-49911d22b679.cpp:18:22: error: ‘codeFreq’ was not declar ed in this scope    18 |         white += min(codeFreq[i], guessFreq[i]);       |                      ^~~~~~~~ 0f3d99d2-476f-4336-a980-49911d22b679.cpp:18:35: error: ‘guessFreq’ was not decla red in this scope    18 |         white += min(codeFreq[i], guessFreq[i]);       |                                   ^~~~~~~~~ 0f3d99d2-476f-4336-a980-49911d22b679.cpp:18:18: error: ‘min’ was not declared in  this scope; did you mean ‘main’?    18 |         white += min(codeFreq[i], guessFreq[i]);       |                  ^~~       |                  main 0f3d99d2-476f-4336-a980-49911d22b679.cpp:21:5: error: ‘cout’ was not declared in  this scope    21 |     cout << white << "\n" << black << "\n";       |     ^~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,13 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 int main() {
     string code, guess;
     cin >> code >> guess;
     
-    int whitePegs = 0, blackPegs = 0;
+    int black = 0, white = 0;
     vector<int> codeFreq(6, 0), guessFreq(6, 0);
     
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
-            blackPegs++;
+            black++;
         } else {
             codeFreq[code[i] - 'A']++;
             guessFreq[guess[i] - 'A']++;
@@ -15,10 +20,10 @@ int main() {
     }
     
     for (int i = 0; i < 6; ++i) {
-        whitePegs += min(codeFreq[i], guessFreq[i]);
+        white += std::min(codeFreq[i], guessFreq[i]);
     }
     
-    cout << whitePegs << endl << blackPegs << endl;
+    std::cout << white << "\n" << black << "\n";
     
     return 0;
 }
