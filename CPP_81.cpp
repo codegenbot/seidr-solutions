37ce6a18e7ab2b7377de616e4d651e321ff93f06
beep boop <githubbot@vadim.me>
2024-07-12 15:33:05 +0200

    Solution 36 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/59i5y8lf Prompt:       |           ^~~~~~ d2da922b-5f64-4b6d-ba88-aafe74fd5434.cpp:64:44: error: ‘b’ was not declared in t his scope    64 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d2da922b-5f64-4b6d-ba88-aafe74fd5434.cpp:72:5: error: redefinition of ‘int main( )’    72 | int main(){       |     ^~~~ d2da922b-5f64-4b6d-ba88-aafe74fd5434.cpp:51:5: note: ‘int main()’ previously def ined here    51 | int main() {       |     ^~~~ In file included from d2da922b-5f64-4b6d-ba88-aafe74fd5434.cpp:63: d2da922b-5f64-4b6d-ba88-aafe74fd5434.cpp: In function ‘int main()’: d2da922b-5f64-4b6d-ba88-aafe74fd5434.cpp:73:20: error: ‘numerical_letter_grade’ was not declared in this scope    73 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ d2da922b-5f64-4b6d-ba88-aafe74fd5434.cpp:73:19: error: ‘issame’ cannot be used a s a function    73 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,4 +1,3 @@
-```
 #include <vector>
 #include <string>
 
@@ -52,7 +51,7 @@ int main() {
     vector<float> grades = {3.4, 4.1, 2.9};
     vector<string> grades_letter = numerical_letter_grade(grades);
 
-    if(issame(numerical_letter_grade(grades), grades_letter))
+    if(issame(vector<string>(numerical_letter_grade(grades)), grades_letter))
         cout << "Grades are same." << endl;
     else
         cout << "Grades are not the same." << endl;
