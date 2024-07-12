67a09194ca9900ca8a26ba5b88b5da24c64d7858
beep boop <githubbot@vadim.me>
2024-07-12 22:40:50 +0200

    Solution 56 of coin-sums, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/9rjlixox Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/50468da2-2c65-4019-acdb-76804d0ba 50e.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- coin-sums.py
+++ coin-sums.py
@@ -13,7 +13,7 @@ def coin_sums(cents):
         combinations = new_combinations
 
     min_coins = min(combinations)
-    return (*min_coins)
+    return (*min(min_coins))
 
 cents = int(input())
 quarters, nickles, dimes, pennies = coin_sums(cents)
