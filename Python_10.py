c4c6b23c008246bd640a59fb96904ec7a472096c
beep boop <githubbot@vadim.me>
2024-07-12 06:36:50 +0200

    Solution 49 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/h3qtxl8o Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f2df422-cc24-443a-be2e-dedb72a9b 3de.py", line 3     def make_palindrome() -> str: IndentationError: unexpected indent

--- Python_10.py
+++ Python_10.py
@@ -1,13 +1,14 @@
+```
 import random
 
-def make_palindrome() -> str:
+def make_palindrome()-> str:
     while True:
-        s = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(3))
+        s = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(5))
         if is_palindrome(s):
             break
     return s
 
-def is_palindrome(s: str) -> bool:
+def is_palindrome(s: str)-> bool:
     return s == s[::-1]
 
 result = make_palindrome()
