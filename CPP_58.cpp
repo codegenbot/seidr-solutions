90a357ef37efb1a36cfabc5b6e51e50539ca254b
beep boop <githubbot@vadim.me>
2024-07-12 11:49:41 +0200

    Solution 6 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4v825prt Prompt:       |                    ^~~ a8355c0b-fe57-4611-8083-deea8b1f47dc.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a8355c0b-fe57-4611-8083-deea8b1f47dc.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a8355c0b-fe57-4611-8083-deea8b1f47dc.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a8355c0b-fe57-4611-8083-deea8b1f47dc.cpp:9: a8355c0b-fe57-4611-8083-deea8b1f47dc.cpp: In function ‘int main()’: a8355c0b-fe57-4611-8083-deea8b1f47dc.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ a8355c0b-fe57-4611-8083-deea8b1f47dc.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -3,30 +3,18 @@
 #include <vector>
 #include <iterator>
 #include <cassert>
-#include <set>
-
 using namespace std;
 
-vector<int> findCommonElements(const vector<int>& l1, const vector<int>& l2) {
-    vector<int> result;
+vector<int> solveProblem(vector<int>& l1, vector<int>& l2) {
     sort(l1.begin(), l1.end());
     sort(l2.begin(), l2.end());
+    vector<int> result;
     set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
     result.erase(unique(result.begin(), result.end()), result.end());
     return result;
 }
 
 int main() {
-    // Test cases
-    vector<int> list1 = {3, 1, 5, 2, 7};
-    vector<int> list2 = {2, 5, 9, 1};
-    vector<int> commonElements = findCommonElements(list1, list2);
-
-    cout << "Common elements: ";
-    for (int elem : commonElements) {
-        cout << elem << " ";
-    }
-    cout << endl;
-
+    // Test cases can be added here
     return 0;
 }
