8c8f8ec0b9e6acbcc72048bb6cd40a4dc65a9413
beep boop <githubbot@vadim.me>
2024-07-12 19:02:00 +0200

    Solution 60 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dhj7i5v3 Prompt:    19 | } else if (a.type() == typeid(string) && b.type() == typeid(string)) {       |   ^~~~ 7ff3f2a1-d951-4a00-947b-fe8d19ad2344.cpp:28:3: error: expected unqualified-id be fore ‘else’    28 | } else {       |   ^~~~ In file included from 7ff3f2a1-d951-4a00-947b-fe8d19ad2344.cpp:32: 7ff3f2a1-d951-4a00-947b-fe8d19ad2344.cpp: In function ‘int main()’: 7ff3f2a1-d951-4a00-947b-fe8d19ad2344.cpp:34:13: error: ‘boost’ has not been decl ared    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 7ff3f2a1-d951-4a00-947b-fe8d19ad2344.cpp:34:29: error: ‘string’ was not declared  in this scope    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 7ff3f2a1-d951-4a00-947b-fe8d19ad2344.cpp:34:37: error: ‘compare_one’ was not dec lared in this scope    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,8 +1,5 @@
 #include <boost/any.hpp>
-using namespace std;
-
-boost::any a = ...;
-boost::any b = ...;
+using namespace boost;
 
 if (a.type() == typeid(int) && b.type() == typeid(int)) {
     int x = boost::any_cast<int>(a);
@@ -12,7 +9,8 @@ if (a.type() == typeid(int) && b.type() == typeid(int)) {
     else if (x < y)
         return b;
     else
-        return a;
+        return a; // return the first integer in case of equality
+
 } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
     double x = boost::any_cast<double>(a);
     double y = boost::any_cast<double>(b);
@@ -21,17 +19,18 @@ if (a.type() == typeid(int) && b.type() == typeid(int)) {
     else if (x < y)
         return b;
     else
-        return a;
-} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-    string x = boost::any_cast<string>(a);
-    string y = boost::any_cast<string>(b);
+        return a; // return the first double in case of equality
+
+} else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+    std::string x = boost::any_cast<std::string>(a);
+    std::string y = boost::any_cast<std::string>(b);
     int xi = stoi(x), yi = stoi(y);
     if (xi > yi)
         return a;
     else if (xi < yi)
         return b;
     else
-        return a;
+        return a; // return the first string in case of equality
+
 } else {
     throw invalid_argument("Invalid input");
-}
