ec890ef38ecffbe06e3ce1a96339d479d4c39c93
beep boop <githubbot@vadim.me>
2024-07-12 15:23:42 +0200

    Solution 7 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/vp4xgvos Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,22 +1,21 @@
 if(a.type() == typeid(int) && b.type() == typeid(int)){
-    if(boost::any_cast<int>(a) > boost::any_cast<int>(b))
+    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
         return a;
-    else if(boost::any_cast<int>(a) < boost::any_cast<int>(b))
+    } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
         return b;
-}
-else if(a.type() == typeid(float) && b.type() == typeid(float)){
-    if(boost::any_cast<float>(a) > boost::any_cast<float>(b))
+    }
+} else if(a.type() == typeid(float) && b.type() == typeid(float)){
+    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
         return a;
-    else if(boost::any_cast<float>(a) < boost::any_cast<float>(b))
+    } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
         return b;
-}
-else if(a.type() == typeid(string) && b.type() == typeid(string)){
-    float val1 = stof(boost::any_cast<string>(a).replace(boost::any_cast<string>(a).find(','), 1, "."));
-    float val2 = stof(boost::any_cast<string>(b).replace(boost::any_cast<string>(b).find(','), 1, "."));
-    if(val1 > val2)
+    }
+} else if(a.type() == typeid(string) && b.type() == typeid(string)){
+    if(stod(boost::any_cast<string>(a)) > stod(boost::any_cast<string>(b))){
         return a;
-    else if(val1 < val2)
+    } else if(stod(boost::any_cast<string>(a)) < stod(boost::any_cast<string>(b))){
         return b;
+    }
 }
 return "None";
 }
