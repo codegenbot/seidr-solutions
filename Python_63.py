738d6d95b7aa7b757550ae4b62f1a40ee3453650
beep boop <githubbot@vadim.me>
2024-07-12 08:07:37 +0200

    Solution 88 of Python/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zx5e9imf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/406702a2-d602-47b0-98a5-f55286102 b04.py", line 8     fib_series = (a + b) for a, b in islice(fib_series, 1, None))                                                                 ^ SyntaxError: unmatched ')'

--- Python_63.py
+++ Python_63.py
@@ -1,10 +1,9 @@
-```Python
-from itertools import islice, accumulate
+from itertools import islice
 
 def fibfib(n: int):
     if n <= 1:
         return 0
     fib_series = (0, 1)
-    for _ in range(2, n+2):  
-        fib_series = tuple(accumulate(fib_series)[1:])
-    return sum(fib_series)
+    for _ in range(2, n+1):  
+        fib_series = (a + b for a, b in zip(fib_series, islice(fib_series, 1, None)))
+    return sum(islice(fib_series, n))
