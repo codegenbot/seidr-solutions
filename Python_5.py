d117f837bfd911b1a878fc7ba590e45ec96cda79
beep boop <githubbot@vadim.me>
2024-07-12 06:08:22 +0200

    Solution 42 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/wdm3tfxd Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/caa9c802-dda0-4ab6-89d8-7699799e3 1c5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_5.py
+++ Python_5.py
@@ -1,12 +1,14 @@
+```
 from typing import List
 
 
 def intersperse(numbers: List[int], delimiter: int) -> List[int]:
     if not numbers:
         return []
-    if delimiter is None:
-        return [num for num in numbers]
     result = [numbers[0]]
     for num in numbers[1:]:
         result.extend([delimiter, num])
+        last_delimiter_index = len(result) - 2
+        while last_delimiter_index >= 0 and result[last_delimiter_index] == delimiter:
+            result.pop(last_delimiter_index)
     return result
