18f63eee31079ead168f13246e2da140cab6a9cf
beep boop <githubbot@vadim.me>
2024-07-12 08:09:34 +0200

    Solution 13 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/8tq08lr0 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,41 +1,36 @@
 int main() {
     vector<int> nums;
     int num;
-    
     while (cin >> num) {
         nums.push_back(num);
     }
     
     int n = nums.size();
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += nums[i];
-    }
+    int diff = INT_MAX;
+    int cutIndex = -1;
     
-    int half_sum = sum / 2;
-    int prefix_sum = 0;
-    int cut_index = -1;
-    
-    for (int i = 0; i < n; i++) {
-        prefix_sum += nums[i];
-        if (prefix_sum >= half_sum) {
-            cut_index = i;
-            break;
+    for (int i = 1; i < n; ++i) {
+        int leftSum = 0, rightSum = 0;
+        for (int j = 0; j < i; ++j) {
+            leftSum += nums[j];
+        }
+        for (int j = i; j < n; ++j) {
+            rightSum += nums[j];
+        }
+        
+        int currentDiff = abs(leftSum - rightSum);
+        if (currentDiff < diff) {
+            diff = currentDiff;
+            cutIndex = i;
         }
     }
     
-    if (abs(sum - 2 * prefix_sum) < abs(sum - 2 * (prefix_sum - nums[cut_index]))) {
-        cout << "1" << endl;
-        for (int i = 0; i <= cut_index; i++) {
-            cout << nums[i] << endl;
-        }
-        cout << "0" << endl;
-    } else {
-        cout << "1" << endl;
-        for (int i = 0; i < cut_index; i++) {
-            cout << nums[i] << endl;
-        }
-        cout << "0" << endl;
+    for (int i = 0; i < cutIndex; ++i) {
+        cout << nums[i] << endl;
+    }
+    cout << endl;
+    for (int i = cutIndex; i < n; ++i) {
+        cout << nums[i] << endl;
     }
     
     return 0;
