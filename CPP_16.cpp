364e67475dce6a06428d2e029cbb8b593d1f2df4
beep boop <githubbot@vadim.me>
2024-07-12 18:25:21 +0200

    Solution 0 of CPP/16, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/p7ak72nm Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_16.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int count_distinct_characters(string str) {
+    string temp = str;
+    transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+    set<char> s(temp.begin(), temp.end());
+    return s.size();
+}
