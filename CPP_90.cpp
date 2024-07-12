645d0fa0e00bc045672386cd7f751e01cfeb0be3
beep boop <githubbot@vadim.me>
2024-07-12 14:50:52 +0200

    Solution 14 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/x7whuw11 Prompt: 85fed99f-b0d4-4682-b2f5-f3795701e914.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int next_smallest(vector<int> lst){       |                   ^~~~~~ 85fed99f-b0d4-4682-b2f5-f3795701e914.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int next_smallest(vector<int> lst){       |                          ^~~ In file included from 85fed99f-b0d4-4682-b2f5-f3795701e914.cpp:10: 85fed99f-b0d4-4682-b2f5-f3795701e914.cpp: In function ‘int main()’: 85fed99f-b0d4-4682-b2f5-f3795701e914.cpp:12:26: error: ‘next_smallest’ cannot be  used as a function    12 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,33 +1,15 @@
-#include<bits/stdc++.h>
+#include<vector>
 using namespace std;
 
-bool is_next_smaller(int x, vector<int> v) {
-    for (int i = 0; i < v.size(); i++) {
-        if (v[i] < x) return true;
+int next_smallest(vector<int> lst){
+    if(lst.size() < 2) return -1; 
+    sort(lst.begin(), lst.end());
+    for(int i = 0; i < lst.size() - 1; i++){
+        if(lst[i] != lst[i+1]) return lst[i];
     }
-    return false;
+    return -1; 
 }
 
 int main(){
-    vector<int> lst;
-    int n;
-    cout << "Enter number of elements: ";
-    cin >> n;
-    cout << "Enter " << n << " integers: ";
-    for(int i = 0; i < n; i++){
-        int x;
-        cin >> x;
-        lst.push_back(x);
-    }
-    if(lst.size() < 2) cout << "-1";
-    else {
-        sort(lst.begin(), lst.end());
-        for(int i = 0; i < lst.size(); i++){
-            if(is_next_smaller(lst[i], lst)) {
-                cout << lst[i];
-                break;
-            }
-        }
-        else cout << "-1";
-    }
+    assert(next_smallest({-35, 34, 12, -45}) == -35);
 }
