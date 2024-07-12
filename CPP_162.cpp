d20080afd5224dedcc798a86711c1c4b41cc474e
beep boop <githubbot@vadim.me>
2024-07-13 00:58:08 +0200

    Solution 12 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/qeqjd3zn Prompt: ab01e89f-7ac9-4844-aeb1-90dd23d9dfd3.cpp:5:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     5 | string string_to_md5(string text) {       | ^~~~~~       | stdin In file included from ab01e89f-7ac9-4844-aeb1-90dd23d9dfd3.cpp:24: ab01e89f-7ac9-4844-aeb1-90dd23d9dfd3.cpp: In function ‘int main()’: ab01e89f-7ac9-4844-aeb1-90dd23d9dfd3.cpp:26:13: error: ‘string_to_md5’ was not d eclared in this scope    26 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -8,16 +8,21 @@ string string_to_md5(string text) {
 
     MD5_CTX ctx;
     unsigned char mdBuffer[16];
-    stringstream ss;
 
     MD5_Init(&ctx);
     MD5_Update(&ctx, text.c_str(), text.size());
     MD5_Final(mdBuffer, &ctx);
 
+    ostringstream ss;
+
     for (int i = 0; i < 16; i++) {
-        sprintf(&ss.str()[0], "%02x", mdBuffer[i]);
-        ss.seekp(0);
+        ss << setfill('0') << setw(2) << hex << static_cast<int>(mdBuffer[i]);
     }
 
     return ss.str();
+}
+
+int main() {
+    assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
+    return 0;
 }
