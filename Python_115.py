fd0d65abc2d20851a25e046239892aa0a1715286
beep boop <githubbot@vadim.me>
2024-07-12 08:15:39 +0200

    Solution 31 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/0t6m7jpw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/20f6a770-96b4-4bb4-a5ca-f272c700c f88.py", line 21, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/20f6a770-96b4-4bb4-a5ca-f272c700c f88.py", line 18, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/20f6a770-96b4-4bb4-a5ca-f272c700c f88.py", line 5, in max_fill     m = len(grid[0])             ~~~~^^^ TypeError: 'int' object is not subscriptable

--- Python_115.py
+++ Python_115.py
@@ -1,16 +1,8 @@
-```Python
 import math
 
 
 def max_fill(n, grid):
-    total_water = 0
-    max_capacity = 0
-    for row in grid:
-        if isinstance(row, list): 
-            m = len(row)
-            for cell in row:
-                if cell > 0 and cell > max_capacity:
-                    max_capacity = cell
-                if cell > 0 and cell <= max_capacity:
-                    total_water += cell
+    m = len(grid[0])
+    total_water = sum(cell for row in grid for cell in row if cell > 0)
+    max_capacity = min(max(row) for row in grid)
     return math.ceil(total_water / (n * max_capacity))
