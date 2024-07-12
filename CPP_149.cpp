c8041aaca56965d3d1f0fc1b07794e59c23af02b
beep boop <githubbot@vadim.me>
2024-07-12 20:00:32 +0200

    Solution 89 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mm8jcipt Prompt:    19 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 27ed35e6-61a5-4705-9311-f06a740cf780.cpp:19:44: error: ‘b’ was not declared in t his scope    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 27ed35e6-61a5-4705-9311-f06a740cf780.cpp:19:45: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 27ed35e6-61a5-4705-9311-f06a740cf780.cpp:18: 27ed35e6-61a5-4705-9311-f06a740cf780.cpp: In function ‘int main()’: 27ed35e6-61a5-4705-9311-f06a740cf780.cpp:28:20: error: ‘sorted_list_sum’ was not  declared in this scope    28 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 27ed35e6-61a5-4705-9311-f06a740cf780.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,10 +1,10 @@
 #include <vector>
 #include <algorithm>
-#include <string>
+#include <functional>
 
 std::vector<std::string> sorted_list_sum(std::vector<std::string> lst) {
     auto it = std::unique(lst.begin(), lst.end(),
-        [](const std::string& a, const std::string& b){return (a.length() % 2 == 1 && a.length() != b.length());});
+        [](const std::string& a, const std::string& b){return a.length() % 2 == 1 && a.length() != b.length();});
     lst.erase(it, lst.end());
     
     std::sort(lst.begin(), lst.end(),
