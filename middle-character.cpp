e7b4aa52eb92444115e4ad8aeca713069671b285
beep boop <githubbot@vadim.me>
2024-07-12 14:20:02 +0200

    Solution 31 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ag0c73oo Prompt:   +++ |+#include <iostream>     2 | using namespace std; de8cfe89-3a3b-4099-9aeb-2c10dc4f0ed8.cpp:7:5: error: ‘cin’ was not declared in t his scope     7 |     cin >> s;       |     ^~~ de8cfe89-3a3b-4099-9aeb-2c10dc4f0ed8.cpp:7:5: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’? de8cfe89-3a3b-4099-9aeb-2c10dc4f0ed8.cpp:13:53: error: ‘endl’ was not declared i n this scope    13 |         cout << "Middle characters are: " << res << endl;       |                                                     ^~~~ de8cfe89-3a3b-4099-9aeb-2c10dc4f0ed8.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std; de8cfe89-3a3b-4099-9aeb-2c10dc4f0ed8.cpp:16:51: error: ‘endl’ was not declared i n this scope    16 |         cout << "Middle character is: " << res << endl;       |                                                   ^~~~ de8cfe89-3a3b-4099-9aeb-2c10dc4f0ed8.cpp:16:51: note: ‘std::endl’ is defined in header ‘<ostream>’; did you forget to ‘#include <ostream>’?

--- middle-character.cpp
+++ middle-character.cpp
@@ -7,7 +7,7 @@ int main() {
     string s;
     cout << "Enter a string: ";
     cin >> s;
-    
+
     int len = s.length();
     if (len % 2 == 0) {
         string res = s.substr(len / 2 - 1, 1);
@@ -17,6 +17,6 @@ int main() {
         string res = s.substr(len / 2, 1);
         cout << "Middle character is: " << res << endl;
     }
-    
+
     return 0;
 }
