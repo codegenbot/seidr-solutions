7efff5546abb0a7df8c9505d12d90552ba59b7fa
beep boop <githubbot@vadim.me>
2024-07-12 13:58:21 +0200

    Solution 12 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ag0c73oo Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,19 +1,20 @@
+```c++
 #include <string>
 using namespace std;
+
+string middleCharacter(string s) {
+    int len = s.length();
+    if (len % 2 == 0) {
+        return s.substr(len / 2 - 1, 2);
+    } else {
+        return s.substr(len / 2, 1);
+    }
+}
 
 int main() {
     string s;
     cout << "Enter a string: ";
     cin >> s;
-    
-    int len = s.length();
-    if (len % 2 == 0) {
-        string res = s.substr(len / 2 - 1, 2);
-        cout << "Middle characters are: " << res << endl;
-    } else {
-        string res = s.substr(len / 2, 1);
-        cout << "Middle character is: " << res << endl;
-    }
-    
+    cout << "The middle character is: " << middleCharacter(s) << endl;
     return 0;
 }
