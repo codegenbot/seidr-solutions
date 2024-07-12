d6e008157e06779bd61f9a7905469ea91a9227a3
beep boop <githubbot@vadim.me>
2024-07-12 14:42:26 +0200

    Solution 4 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/y9kfabj7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/09806795-06a3-4050-abca-d22f076d8 69d.py", line 17, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/09806795-06a3-4050-abca-d22f076d8 69d.py", line 14, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/09806795-06a3-4050-abca-d22f076d8 69d.py", line 7, in make_palindrome     if string.ispalindrome():        ^^^^^^^^^^^^^^^^^^^ AttributeError: 'str' object has no attribute 'ispalindrome'

--- Python_10.py
+++ Python_10.py
@@ -1,12 +1,11 @@
-```
 def is_palindrome(string: str) -> bool:
-    """ Test if given string is a palindrome """
     return string == string[::-1]
 
 
 def make_palindrome(string: str) -> str:
-    reversed_string = string[::-1]
+    if is_palindrome(string):
+        return string + string[::-1]
     for i in range(len(string)):
-        if string[:i+1] == string[:i+1][::-1]:
-            return string + reversed_string[i:]
-    return string + reversed_string
+        if is_palindrome(string[i:]):
+            return string + string[i:][::-1]
+    return string
