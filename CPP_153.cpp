d0c2d8fcd22f0b3d7fc531296ca4e302e9119c8a
beep boop <githubbot@vadim.me>
2024-07-13 00:57:48 +0200

    Solution 40 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hpkkxb14 Prompt:     3 | #include <string> 1c2d308a-ccd5-4100-a827-3a70bb4c812d.cpp: At global scope: 1c2d308a-ccd5-4100-a827-3a70bb4c812d.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ 1c2d308a-ccd5-4100-a827-3a70bb4c812d.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 1c2d308a-ccd5-4100-a827-3a70bb4c812d.cpp:24: 1c2d308a-ccd5-4100-a827-3a70bb4c812d.cpp: In function ‘int main()’: 1c2d308a-ccd5-4100-a827-3a70bb4c812d.cpp:26:33: error: invalid initialization of  reference of type ‘const std::pair<std::__cxx11::basic_string<char>, std::__cxx 11::basic_string<char> >&’ from expression of type ‘const char [3]’    26 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |                                 ^~~~ 1c2d308a-ccd5-4100-a827-3a70bb4c812d.cpp:5:76: note: in passing argument 1 of ‘s td::string Strongest_Extension(const std::pair<std::__cxx11::basic_string<char>,  std::__cxx11::basic_string<char> >&)’     5 | ongest_Extension(const std::pair<std::string, std::string>& extension) {       |                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,4 +1,4 @@
-```cpp
+```c++
 #include <iostream>
 #include <vector>
 #include <string>
@@ -13,8 +13,8 @@ std::string Strongest_Extension(const std::pair<std::string, std::string>& exten
 
 int main() {
     std::vector<std::pair<std::string, std::string>> extensions; 
-    extensions.push_back({{"Sp", "671235"}});
-    extensions.push_back({{"Bb", ""}});
+    extensions.push_back(std::make_pair("Sp", "671235"));
+    extensions.push_back(std::make_pair("Bb", ""));
 
     assert(Strongest_Extension({{"Sp", "671235"}}) == "Sp.671235");
     for (const auto& extension : extensions) {
