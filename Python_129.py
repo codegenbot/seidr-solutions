8c11333d821c18209318282a6ff2938c50a05688
beep boop <githubbot@vadim.me>
2024-07-12 15:27:08 +0200

    Solution 61 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/zi3xq93h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1d8a49d4-319e-44d2-844c-65bdd0c65 26b.py", line 27, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1d8a49d4-319e-44d2-844c-65bdd0c65 26b.py", line 24, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -8,14 +8,12 @@ def minPath(grid, k):
             return
         if len(p) > k:
             return
-        visited = set()
-        visited.add((i, j))
         for x, y in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
             ni, nj = i + x, j + y
-            if 0 <= ni < n and 0 <= nj < n and (ni, nj) not in visited:
+            if 0 <= ni < n and 0 <= nj < n:
                 dfs(ni, nj, p + [grid[ni][nj]])
 
     for i in range(n):
         for j in range(n):
             dfs(i, j, [grid[i][j]])
-    return sorted(set(res))
+    return sorted(set(tuple(x) for x in res))
