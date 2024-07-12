3b55e43ac46f0364ffd6ee8200e0a9e190caa87a
beep boop <githubbot@vadim.me>
2024-07-12 06:52:36 +0200

    Solution 26 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zb2z36u0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2aae931a-509c-44d2-85d9-12f63e0c0 aeb.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_41.py
+++ Python_41.py
@@ -1,4 +1,7 @@
 def car_race_collision():
     n = int(input("Enter number of cars: "))
-    total_distance = sum(i * (n - i) for i in range(1, n + 1))
+    total_distance = 0
+    for i in range(1, n+1):
+        distance = (i * (n - i)) // 2
+        total_distance += distance
     return total_distance
