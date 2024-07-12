4b50ca980414b35732a4db889228e49af60073cc
beep boop <githubbot@vadim.me>
2024-07-12 11:03:02 +0200

    Solution 90 of Python/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/53qrk9fm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f26bfac8-d9b8-4714-91b6-098b6d7f5 1a8.py", line 20, in <module>     check(prod_signs)           ^^^^^^^^^^ NameError: name 'prod_signs' is not defined

--- Python_128.py
+++ Python_128.py
@@ -1,17 +1,3 @@
-```Python
-def solve(grades):
-    averages = [sum(student) / len(student) for student in grades]
-    medians = [
-        (
-            sorted(student)[len(student) // 2]
-            if len(student) % 2 != 0
-            else (
-                (sorted(student)[len(student) // 2 - 1] + 
-                  sorted(student)[len(student) // 2]) / 2
-            )
-        ) for student in grades
-    ]
-    return f"Average: {sum(averages) / len(averages)}, Median: {' '.join(map(str, medians))}"
-
-grades = [[90, 80], [70, 60], [50, 40]]
+```
+grades = [[1, 2, 3], [4, 5, 6], [7, 8]]
 print(solve(grades))
