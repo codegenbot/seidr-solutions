b5d739e5c2e43cfa705afb637ff65c4419b56333
beep boop <githubbot@vadim.me>
2024-07-12 15:18:18 +0200

    Solution 50 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/owlfkvjg Prompt: on before ‘float’    39 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 43165fd5-57a7-4ae0-bbcf-273fe4e238d5.cpp:39:43: error: expression list treated a s compound expression in initializer [-fpermissive]    39 | bool issame(vector<float> a,vector<float>b){       |                                           ^ 43165fd5-57a7-4ae0-bbcf-273fe4e238d5.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 43165fd5-57a7-4ae0-bbcf-273fe4e238d5.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~ In file included from 43165fd5-57a7-4ae0-bbcf-273fe4e238d5.cpp:38: 43165fd5-57a7-4ae0-bbcf-273fe4e238d5.cpp: In function ‘int main()’: 43165fd5-57a7-4ae0-bbcf-273fe4e238d5.cpp:48:19: error: ‘issame’ cannot be used a s a function    48 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,8 +1,9 @@
+```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<float> a, std::vector<float> b) {
+bool same(std::vector<float> a, std::vector<float> b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i < a.size(); i++)
@@ -17,9 +18,8 @@ std::vector<float> sort_even(std::vector<float> l) {
         if (i % 2 == 0) {
             std::vector<float> even_indices;
             for (int j = 0; j < l.size(); j++) {
-                if (j % 2 == 0) {
+                if (j % 2 == 0)
                     even_indices.push_back(l[j]);
-                }
             }
             std::sort(even_indices.begin(), even_indices.end());
             result[i] = even_indices[0];
@@ -31,6 +31,6 @@ std::vector<float> sort_even(std::vector<float> l) {
 }
 
 int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    assert(same(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
     return 0;
 }
