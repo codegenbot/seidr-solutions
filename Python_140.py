ec5ab2c880f1866b188bf5852e85dddf6e75b978
beep boop <githubbot@vadim.me>
2024-07-12 12:54:37 +0200

    Solution 34 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ev8gu8de Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c43c5276-ce46-4275-9ef9-7c03d1487 1f3.py", line 14, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c43c5276-ce46-4275-9ef9-7c03d1487 1f3.py", line 10, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -2,4 +2,4 @@ def fix_spaces(text):
     text = text.replace(" ", "_")
     while "__" in text:
         text = text.replace("__", "_-")
-    return text.replace("_-", "_").replace("---", "_").replace("--", "_")
+    return text.replace("_-","_").replace("_ ","_").replace(" _","_")
