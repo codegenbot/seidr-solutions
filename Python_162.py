9ebe9184c579cab644c30de76a9ec63c0eed22b9
beep boop <githubbot@vadim.me>
2024-07-12 10:22:01 +0200

    Solution 69 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ua814v8v Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dca3d6ce-03b2-4601-aaab-cea93d025 988.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_162.py
+++ Python_162.py
@@ -1,10 +1,16 @@
+```
 import hashlib
 
-print("Please enter some text (alphanumeric characters only): ")
-s = input()
+while True:
+    print("Please enter some text (alphanumeric characters only): ", end="")
+    s = input()
+    if s.isalnum():  
+        break
+    else:
+        print("Please enter some alphanumeric characters.")
+
 if not s:
     print("Please enter some alphanumeric characters.")
 else:
     output = hashlib.md5(s.encode()).hexdigest()
-    if output:
-        print(f"Output: {output}")
+    print(f"Output: {output}")
