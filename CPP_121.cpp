8d8080d640c3a871312fc2a1b6aaf727370d5c30
beep boop <githubbot@vadim.me>
2024-07-12 22:04:08 +0200

    Solution 5 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jlcp1g2y Prompt: in this scope    14 |     assert(solution({3, 13, 2, 9}) == 16);       |     ^~~~~~ 09899fdf-c612-4f48-ac6d-920680262933.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | 09899fdf-c612-4f48-ac6d-920680262933.cpp: At global scope: 09899fdf-c612-4f48-ac6d-920680262933.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 09899fdf-c612-4f48-ac6d-920680262933.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 09899fdf-c612-4f48-ac6d-920680262933.cpp:18: 09899fdf-c612-4f48-ac6d-920680262933.cpp: In function ‘int main()’: 09899fdf-c612-4f48-ac6d-920680262933.cpp:20:13: error: ‘solutions’ was not decla red in this scope; did you mean ‘solution’?    20 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,12 +1,17 @@
 #include <vector>
+#include <cassert>
 
-int main() {
+int solution(vector<int> lst) {
     int sum = 0;
-    std::vector<int> lst = {3, 13, 2, 9}; 
     for (int i = 1; i < lst.size(); i++) {
         if (lst[i] % 2 != 0) {
             sum += lst[i];
         }
     }
     return sum;
+}
+
+int main() {
+    assert(solution({3, 13, 2, 9}) == 16);
+    return 0;
 }
