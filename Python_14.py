8ee700a367b0371af6244bfa963637999d161266
beep boop <githubbot@vadim.me>
2024-07-12 05:42:58 +0200

    Solution 15 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/mrd681vd Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/69d92565-7502-4972-8f35-628ec39e5 035.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_14.py
+++ Python_14.py
@@ -2,4 +2,4 @@ from typing import List
 
 
 def all_prefixes(string: str) -> List[str]:
-    return sorted([string[:i] for i in range(0, len(string) + 1)], key=len)
+    return [string[:i] for i in range(0, len(string) + 1)]
