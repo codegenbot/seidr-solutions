01874dcc8fdd8ff90340e52e1a005610272b3434
beep boop <githubbot@vadim.me>
2024-07-12 06:04:32 +0200

    Solution 40 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7ealauoz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/45c0386a-ee3f-4be2-a07e-21464e94e 244.py", line 15, in <module>     check(filter_by_prefix)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/45c0386a-ee3f-4be2-a07e-21464e94e 244.py", line 10, in check     assert filter_by_prefix(            ^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/45c0386a-ee3f-4be2-a07e-21464e94e 244.py", line 7, in filter_by_prefix     result = [s for s in strings_list if s.startswith(prefix)]              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/45c0386a-ee3f-4be2-a07e-21464e94e 244.py", line 7, in <listcomp>     result = [s for s in strings_list if s.startswith(prefix)]                                          ^^^^^^^^^^^^^^^^^^^^ TypeError: startswith first arg must be str or a tuple of str, not list

--- Python_29.py
+++ Python_29.py
@@ -1,9 +1,8 @@
-```
 def filter_by_prefix(prefix="", user_input=None):
     if user_input is None:
         strings = input('Enter a list of strings (separated by commas): ')
     else:
         strings = user_input
     strings_list = [s.strip() for s in strings.split(',')]
-    result = [s.split()[0].lower().startswith(prefix.lower()) and s for s in strings_list if s]
-    print([s for s in result if result])
+    result = [s for s in strings_list if not prefix or s.startswith(''.join(map(str, prefix)))]
+    print(result)
