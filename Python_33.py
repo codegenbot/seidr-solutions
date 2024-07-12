1d205adf7100b7cca3cef1cc0773e8488d2eaa04
beep boop <githubbot@vadim.me>
2024-07-12 06:11:54 +0200

    Solution 30 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7bd6h5bq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fe3d37e8-3ad2-4c68-9dd1-c00b48551 c24.py", line 14, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fe3d37e8-3ad2-4c68-9dd1-c00b48551 c24.py", line 9, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fe3d37e8-3ad2-4c68-9dd1-c00b48551 c24.py", line 6, in sort_third     for group in itertools.groupby(grouped, key=lambda x: (x[0]))                  ^^^^^^^^^ NameError: name 'itertools' is not defined

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,4 @@
-import itertools
+from itertools import groupby
 
 
 def sort_third(lst):
@@ -6,5 +6,5 @@ def sort_third(lst):
     grouped.sort()
     return [
         min(group[1:]) if group[0] % 3 != 0 else max(group[1:])
-        for group in itertools.groupby(grouped, key=lambda x: (x[0]))
+        for key, group in groupby(grouped, key=lambda x: (x[0]))
     ]
