65588c1f342342ff6bc259c83ddc20a1f4af7327
beep boop <githubbot@vadim.me>
2024-07-12 08:29:06 +0200

    Solution 1 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/nb0hoatt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c66ec902-f84c-46f5-b994-fafea9ee5 fd5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_144.py
+++ Python_144.py
@@ -1,6 +1,8 @@
-```
 def simplify(x, n):
-    x_num, x_denom = map(int, x.split('/'))
-    n_num, n_denom = map(int, n.split('/'))
+    x_num, x_denom = map(int, [i for i in x.split('/')])
+    n_num, n_denom = map(int, [i for i in n.split('/')])
     
-    return (x_num * n_denom) % (x_denom * n_num) == 0
+    if (x_num * n_denom) % ((x_denom * n_num)) == 0:
+        return True
+    else:
+        return False
