ba66aaa75969f47f7af71f06d0e27c7245c08ff6
beep boop <githubbot@vadim.me>
2024-07-12 08:54:32 +0200

    Solution 14 of Python/2, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/go6zbb2i Prompt: Program did not receive expected input

--- Python_2.py
+++ Python_2.py
@@ -2,5 +2,5 @@ def calculate_decimal_part(number):
     number = float(number)
     return number % 1
 
-input_number = raw_input()
+input_number = float(input())
 print(calculate_decimal_part(input_number))
