d1874a7791f38943956269253713bc424b2e9ec2
beep boop <githubbot@vadim.me>
2024-07-12 05:55:42 +0200

    Solution 29 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/g167l76a Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/feae7cc0-8916-466a-89fe-800cac925 371.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_19.py
+++ Python_19.py
@@ -1,10 +1,11 @@
-```
+```python
 import re
 
 num_dict = {str(i): str(i) for i in range(10)}
 
-def sort_numbers(numbers: str) -> str:
-    matches = re.findall(r'\b\d+\b', numbers)
+def sort_numbers(input_str: str) -> str:
+    numbers = [str(c) for c in input_str]
+    matches = re.findall('\d', ''.join(numbers))
     if not matches:
         return "No numbers found"
     return " ".join(sorted([num_dict[num] for num in matches]))
