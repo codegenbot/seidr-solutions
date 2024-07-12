62a6972b46bd440e62f46fc06d06a3a8de077d4b
beep boop <githubbot@vadim.me>
2024-07-12 13:42:49 +0200

    Solution 7 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/zi3xq93h Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a5f9d240-1d0e-4fc9-9147-12dd51a18 18e.py", line 1     Here is the solution:                 ^^^^^^^^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -4,20 +4,16 @@ def minPath(grid, k):
 
     def dfs(i, j, p):
         if len(p) == k:
-            return [p]
-        if (i, j) in visited:
-            return []
+            res.append(sorted(p))
+            return
+        visited = set()
         visited.add((i, j))
-        ress = []
         for x, y in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
             ni, nj = i + x, j + y
             if 0 <= ni < n and 0 <= nj < n and (ni, nj) not in visited:
-                ress += dfs(ni, nj, p + [grid[ni][nj]])
-        return sorted([p for r in ress for t in r])
+                dfs(ni, nj, p + [grid[ni][nj]])
 
-    visited = set()
-    for i, j in [(n // 2, n // 2)]:
-        for path in dfs(i, j, []):
-            if len(path) == k and len(set(path)) == k:
-                return sorted(path)
-    return []
+    for i in range(n):
+        for j in range(n):
+            dfs(i, j, [])
+    return sorted(res)
