313dfb7c692ce83eeef2ab4cdc9c3d7751c2646e
beep boop <githubbot@vadim.me>
2024-07-12 12:59:08 +0200

    Solution 3 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/irn5je5k Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ fd5ffa0b-3306-401c-9753-974c3cedc2d1.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ fd5ffa0b-3306-401c-9753-974c3cedc2d1.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from fd5ffa0b-3306-401c-9753-974c3cedc2d1.cpp:11: fd5ffa0b-3306-401c-9753-974c3cedc2d1.cpp: In function ‘int main()’: fd5ffa0b-3306-401c-9753-974c3cedc2d1.cpp:21:20: error: ‘filter_by_prefix’ was no t declared in this scope    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ fd5ffa0b-3306-401c-9753-974c3cedc2d1.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,8 +1,15 @@
 ```cpp
-bool issame(vector<string> a, vector<string> b){
-    if(a.size() != b.size()) return false;
+#include <vector>
+#include <string>
+
+bool issame(vector<string> a, vector<string> b) {
+    if(a.size() != b.size()) {
+        return false;
+    }
     for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
+        if(a[i] != b[i]) {
+            return false;
+        }
     }
     return true;
 }
