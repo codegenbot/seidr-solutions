785aa841ed2a1d6013fb72c01682a062e078d7ec
beep boop <githubbot@vadim.me>
2024-07-12 09:49:05 +0200

    Solution 51 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/3xd4xzwr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48382850-4db9-4f1d-920a-735913cd7 80a.py", line 2     return [item for item in sorted(zip(*(l[i::3] for i in range(3))) for item i n item]       ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '('

--- Python_33.py
+++ Python_33.py
@@ -1,2 +1,2 @@
 def sort_third(l: list):
-    return [item for item in sorted(zip(*(l[i::3] for i in range(3))) for item in item]
+    return [item for item in sorted(zip(*(l[i::3] for i in range(3)))][0] for item in item]
