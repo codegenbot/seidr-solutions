a8398e1e2e6c9d3d524fb9e6cc76f1674e08fced
beep boop <githubbot@vadim.me>
2024-07-12 02:45:25 +0200

    Solution 14 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/52q4ikre Prompt: In file included from /usr/include/c++/11/vector:67,                  from 8691ae5c-f566-46b6-8b88-11f64071e2a3.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 8691ae5c-f566-46b6-8b88-11f64071e2a3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8691ae5c-f566-46b6-8b88-11f64071e2a3.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 8691ae5c-f566-46b6-8b88-11f64071e2a3.cpp:24: 8691ae5c-f566-46b6-8b88-11f64071e2a3.cpp: In function ‘int main()’: 8691ae5c-f566-46b6-8b88-11f64071e2a3.cpp:34:20: error: ‘even_odd_palindrome’ was  not declared in this scope    34 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 8691ae5c-f566-46b6-8b88-11f64071e2a3.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,13 +1,33 @@
 #include <vector>
+#include <string>
+#include <algorithm>
+#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    if (a.size() != b.size())
-        return false;
-        
-    for (size_t i = 0; i < a.size(); ++i){
-        if (a[i] != b[i])
-            return false;
+using namespace std;
+
+vector<int> even_odd_palindrome(int n){
+    vector<int> res(2, 0);
+    for (int i = 1; i <= n; ++i) {
+        string num = to_string(i);
+        string rev = num;
+        reverse(rev.begin(), rev.end());
+        if (num == rev) {
+            if (i % 2 == 0) {
+                res[0]++;
+            } else {
+                res[1]++;
+            }
+        }
     }
-    
-    return true;
+    return res;
+}
+
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
+
+int main(){
+    assert(issame(even_odd_palindrome(1), {0, 1}));
+
+    return 0;
 }
