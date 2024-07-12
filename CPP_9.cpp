8553791d19f9632d264977b512c98c144bbf6fe7
beep boop <githubbot@vadim.me>
2024-07-12 12:59:56 +0200

    Solution 2 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2rboiixt Prompt: 4673021f-e5c1-46e7-92fe-725afb4181d6.cpp: In function ‘std::vector<int> rolling_ max(std::vector<int>)’: 4673021f-e5c1-46e7-92fe-725afb4181d6.cpp:7:15: error: ‘INT_MIN’ was not declared  in this scope     7 |     int max = INT_MIN;       |               ^~~~~~~ 4673021f-e5c1-46e7-92fe-725afb4181d6.cpp:2:1: note: ‘INT_MIN’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 |

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,14 +1,17 @@
+#include <climits>
+#include <vector>
+
+using namespace std;
+
 vector<int> rolling_max(vector<int> numbers) {
     vector<int> result;
-    int max = numbers[0];
-    result.push_back(max);
+    int max = INT_MIN;
     
-    for(int i = 1; i < numbers.size(); i++) {
-        if(numbers[i] > max) {
-            max = numbers[i];
+    for (int i : numbers) {
+        if (i > max) {
+            max = i;
         }
         result.push_back(max);
     }
     
     return result;
-}
