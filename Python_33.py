e4b178db2a3794c3536589ce7ee54a8b11db6544
beep boop <githubbot@vadim.me>
2024-07-12 19:01:18 +0200

    Solution 61 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/20nxyk55 Prompt: b/python3.11/site-packages/programlib/programs/b9c4d600-71f2-468d-8e09-fd3c847f3 7a4.py", line 10, in sort_third     return [flatten_list(sorted([l[i], l[i+1], l[i+2]], reverse=True)) for i in range(0, len(l), 3)]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b9c4d600-71f2-468d-8e09-fd3c847f3 7a4.py", line 10, in <listcomp>     return [flatten_list(sorted([l[i], l[i+1], l[i+2]], reverse=True)) for i in range(0, len(l), 3)]             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b9c4d600-71f2-468d-8e09-fd3c847f3 7a4.py", line 4, in flatten_list     return [item for sublist in list_of_lists for item in sublist]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b9c4d600-71f2-468d-8e09-fd3c847f3 7a4.py", line 4, in <listcomp>     return [item for sublist in list_of_lists for item in sublist]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: 'int' object is not iterable

--- Python_33.py
+++ Python_33.py
@@ -7,4 +7,4 @@ def sort_third(l: list):
     if len(l) % 3 != 0:
         l += [0] * (3 - len(l) % 3)
     
-    return [flatten_list(sorted([l[i], l[i+1], l[i+2]], reverse=True)) for i in range(0, len(l), 3]
+    return [flatten_list(sorted([l[i], l[i+1], l[i+2]], reverse=True)) for i in range(0, len(l), 3)]
