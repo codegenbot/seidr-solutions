e37ce722fa5918cf3020adb82dd1edc994a6d0d6
beep boop <githubbot@vadim.me>
2024-07-12 15:11:50 +0200

    Solution 21 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eui281em Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 2c83f14e-0fce-42cd-93ac-c55667f77417.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -18,7 +18,7 @@ bool will_it_fly(std::vector<std::vector<int>>& q, int w) {
 }
 
 int main() {
-    std::vector<std::vector<int>> q({{5}});
+    std::vector<std::vector<int>> q = {{5}};
     assert(will_it_fly(q, 5) == true);
     return 0;
 }
