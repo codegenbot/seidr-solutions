f881948198165eaeafa1e899a61571a7f94348db
beep boop <githubbot@vadim.me>
2024-07-12 06:14:41 +0200

    Solution 69 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7ealauoz Prompt: b/python3.11/site-packages/programlib/programs/fa044f95-6347-4a3f-aac3-d75257482 39c.py", line 15, in <module>     check(filter_by_prefix)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa044f95-6347-4a3f-aac3-d75257482 39c.py", line 10, in check     assert filter_by_prefix(            ^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa044f95-6347-4a3f-aac3-d75257482 39c.py", line 7, in filter_by_prefix     result = [str(s).lower() for s in strings_list if s and str(s).lower().start swith(prefix.lower())]              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa044f95-6347-4a3f-aac3-d75257482 39c.py", line 7, in <listcomp>     result = [str(s).lower() for s in strings_list if s and str(s).lower().start swith(prefix.lower())]       ^^^^^^^^^^^^ AttributeError: 'list' object has no attribute 'lower'

--- Python_29.py
+++ Python_29.py
@@ -4,5 +4,5 @@ def filter_by_prefix(prefix="", user_input=None):
     else:
         strings = user_input
     strings_list = [s.strip() for s in strings.split(',')]
-    result = [str(s.strip()).lower() for s in strings_list if s and any(str(s.strip()).lower().startswith(p.lower()) for p in prefix)]
+    result = [s.strip().lower() for s in strings_list if s and s.lower().startswith(prefix.lower())]
     print(result)
