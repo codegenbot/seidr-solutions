c4536f1d454b2a95ce2aea99649c85a843e15a51
beep boop <githubbot@vadim.me>
2024-07-12 08:39:40 +0200

    Solution 13 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/pp89dacn Prompt:       |                    ^~~ 67b99055-ca63-4cc3-83d4-4ad4aade21fd.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 67b99055-ca63-4cc3-83d4-4ad4aade21fd.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 67b99055-ca63-4cc3-83d4-4ad4aade21fd.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 67b99055-ca63-4cc3-83d4-4ad4aade21fd.cpp:12: 67b99055-ca63-4cc3-83d4-4ad4aade21fd.cpp: In function ‘int main()’: 67b99055-ca63-4cc3-83d4-4ad4aade21fd.cpp:22:20: error: ‘intersperse’ was not dec lared in this scope    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ 67b99055-ca63-4cc3-83d4-4ad4aade21fd.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -3,7 +3,7 @@
 
 std::vector<int> intersperse(std::vector<int> numbers, int delimiter){
     std::vector<int> result;
-    for(size_t i = 0; i < numbers.size(); ++i){
+    for(int i = 0; i < numbers.size(); ++i){
         result.push_back(numbers[i]);
         if(i < numbers.size() - 1){
             result.push_back(delimiter);
@@ -25,6 +25,6 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 }
 
 int main() {
-    assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
+    assert (issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
     return 0;
 }
