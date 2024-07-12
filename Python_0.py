bddf8a853cc9396dae9e71a70dc265b084d70839
beep boop <githubbot@vadim.me>
2024-07-12 18:53:33 +0200

    Solution 50 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/nkmavojj Prompt: True Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9310748c-d49d-46c6-9818-4967c8e65 b7e.py", line 13, in <module>     check(has_close_elements)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9310748c-d49d-46c6-9818-4967c8e65 b7e.py", line 10, in check     assert has_close_elements([1.1, 2.2, 3.1, 4.1, 5.1], 0.5) == False            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: has_close_elements() takes 1 positional argument but 2 were given

--- Python_0.py
+++ Python_0.py
@@ -1,2 +1,8 @@
-result = sum(range(int(input()) + 1))
+def has_close_elements(arr):
+    for i in range(len(arr) - 1):
+        if abs(arr[i] - arr[i+1]) <= 1:
+            return True
+    return False
+
+result = has_close_elements([1, 2, 10, 5])
 print(result)
