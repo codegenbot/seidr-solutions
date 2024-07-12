c223b1270a2f7ee19570fb94e3da882b0d46de04
beep boop <githubbot@vadim.me>
2024-07-13 00:05:40 +0200

    Solution 96 of CPP/82, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/e5g8bp5r Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_82.cpp
+++ CPP_82.cpp
@@ -1,8 +1,28 @@
-bool prime_length(string str){
-    int len = str.length();
-    if(len <= 1) return false;
-    for(int i=2; i*i<=len; i++){
-        if(len%i==0) return false;
+#include <iostream>
+#include <cstring>
+#include <cassert>
+#include <string>
+
+bool isPrime(int n) {
+    if(n <= 1) return false;
+    for(int i=2; i*i<=n; i++){
+        if(n%i==0) return false;
     }
     return true;
+}
+
+bool prime_length(const char* str){
+    std::string s = "";
+    for(int i = 0; str[i]; i++) {
+        int num = str[i] - '0';
+        if(isPrime(num)) {
+            while(!isPrime(num+1)) num++;
+            // Concatenate the converted number as a string
+            s += std::to_string(num);
+        } else {
+            // Add characters as is
+            s.push_back(str[i]);
+        }
+    }
+    return s.length() > 1;
 }
