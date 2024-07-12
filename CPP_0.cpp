bfc574def754d1c31a961d9f45ad91669545407d
beep boop <githubbot@vadim.me>
2024-07-12 20:13:13 +0200

    Solution 94 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/avzqcthh Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 78a9d963-40f0-4b4c-b1d5-f9328b1b863d.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,11 +1,16 @@
-```cpp
 #include <vector>
 #include <cassert>
 #include <initializer_list>
 
-bool has_close_elements(const std::vector<float>& vec, float epsilon);
+bool has_close_elements(const std::vector<float>& v, float epsilon) {
+    for(int i = 0; i < v.size() - 1; i++) {
+        if(abs(v[i] - v[i+1]) <= epsilon)
+            return true;
+    }
+    return false;
+}
 
 int main() {
-    std::vector<float> a(a.begin(), (a.end() - 1));
+    std::vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
     assert(has_close_elements(a, 0.5) == false);
 }
