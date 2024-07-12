ba2fa1ecbff7b00054a27ab06f9938c77392fde7
beep boop <githubbot@vadim.me>
2024-07-12 11:01:11 +0200

    Solution 87 of Python/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/53qrk9fm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5c5fc32d-568a-4236-baaf-66234af20 c8c.py", line 22, in <module>     check(prod_signs)           ^^^^^^^^^^ NameError: name 'prod_signs' is not defined

--- Python_128.py
+++ Python_128.py
@@ -2,21 +2,12 @@ def solve(grades):
     averages = [sum(student) / len(student) for student in grades]
     medians = [
         (
-            " ".join(map(str, sorted(student)[len(student) // 2 : :]))
+            sorted(student)[len(student) // 2]
             if len(student) % 2 != 0
-            else " ".join(
-                map(
-                    str,
-                    (
-                        sorted(student)[len(student) // 2 - 1]
-                        + sorted(student)[len(student) // 2]
-                    )
-                    / 2,
-                )
+            else (
+                (sorted(student)[len(student) // 2 - 1] + 
+                  sorted(student)[len(student) // 2]) / 2
             )
-        )
-        for student in grades
+        ) for student in grades
     ]
-    return (
-        f"Average: {sum(averages) / len(grades)}, Median: {' '.join(map(str, medians))}"
-    )
+    return f"Average: {sum(averages) / len(averages)}, Median: {' '.join(map(str, medians))}"
