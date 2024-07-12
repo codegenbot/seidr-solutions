b48b7d4a002805f9982297a2db237d55bc06ced3
beep boop <githubbot@vadim.me>
2024-07-12 13:12:44 +0200

    Solution 21 of CPP/18, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/35e0f8ny Prompt: in this scope     3 | int how_many_times(string str, string substring) {       |                                ^~~~~~ 894975e9-e229-4e69-873f-bbf41182cc62.cpp:3:32: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 894975e9-e229-4e69-873f-bbf41182cc62.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 894975e9-e229-4e69-873f-bbf41182cc62.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 894975e9-e229-4e69-873f-bbf41182cc62.cpp:3:48: error: expression list treated as  compound expression in initializer [-fpermissive]     3 | int how_many_times(string str, string substring) {       |                                                ^ In file included from 894975e9-e229-4e69-873f-bbf41182cc62.cpp:14: 894975e9-e229-4e69-873f-bbf41182cc62.cpp: In function ‘int main()’: 894975e9-e229-4e69-873f-bbf41182cc62.cpp:16:27: error: ‘how_many_times’ cannot b e used as a function    16 |     assert (how_many_times("john doe", "john") == 1);       |             ~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -1,7 +1,7 @@
 int how_many_times(std::string str, std::string substring) {
     int count = 0;
     size_t pos = 0;
-    string temp = str;
+    std::string temp = str;
     while ((pos = temp.find(substring)) != std::string::npos) {
         count++;
         temp.erase(0, pos + substring.size());
