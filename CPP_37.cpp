1b135023977b11f13aa6f18ee5dbccbea744cf1a
beep boop <githubbot@vadim.me>
2024-07-12 15:15:47 +0200

    Solution 48 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/owlfkvjg Prompt:       |                             ^~~~~~ 1353f252-09e5-48be-8625-9ef534e507d4.cpp:39:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 1353f252-09e5-48be-8625-9ef534e507d4.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1353f252-09e5-48be-8625-9ef534e507d4.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1353f252-09e5-48be-8625-9ef534e507d4.cpp:39:36: error: expected primary-expressi on before ‘float’    39 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 1353f252-09e5-48be-8625-9ef534e507d4.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 1353f252-09e5-48be-8625-9ef534e507d4.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-bool same_vectors(std::vector<float> a, std::vector<float> b) {
+bool issame(std::vector<float> a, std::vector<float> b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i < a.size(); i++)
@@ -11,11 +11,11 @@ bool same_vectors(std::vector<float> a, std::vector<float> b) {
     return true;
 }
 
-std::vector<float> sort_even_elements(std::vector<float> l) {
+std::vector<float> sort_even(std::vector<float> l) {
     std::vector<float> result(l.size(), 0.0);
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            std::vector<float> even_indices(l.size(), 0.0f); // Initialize with initial capacity
+            std::vector<float> even_indices;
             for (int j = 0; j < l.size(); j++) {
                 if (j % 2 == 0) {
                     even_indices.push_back(l[j]);
@@ -31,6 +31,6 @@ std::vector<float> sort_even_elements(std::vector<float> l) {
 }
 
 int main() {
-    assert(same_vectors(sort_even_elements({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
     return 0;
 }
