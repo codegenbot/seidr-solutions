4fe3d40c6581020d18babe8ffc5116e55568e7a8
beep boop <githubbot@vadim.me>
2024-07-13 01:49:28 +0200

    Solution 32 of CPP/27, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d7gwj9kv Prompt: d5179cf2-9bd7-4a23-88ef-9d0282652067.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string filp_case(string str){       | ^~~~~~ In file included from d5179cf2-9bd7-4a23-88ef-9d0282652067.cpp:14: d5179cf2-9bd7-4a23-88ef-9d0282652067.cpp: In function ‘int main()’: d5179cf2-9bd7-4a23-88ef-9d0282652067.cpp:16:13: error: ‘filp_case’ was not decla red in this scope    16 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,8 +1,14 @@
-for (int i = 0; i < str.length(); i++) {
-    if (islower(str[i])) {
-        str[i] = toupper(str[i]);
-    } else if (isupper(str[i])) {
-        str[i] = tolower(str[i]);
+#include <string>
+
+std::string filp_case(std::string str) {
+    std::string result = "";
+    for(int i=0; i<str.length(); i++){
+        if(str[i] >= 'a' && str[i] <= 'z')
+            result += toupper((char)str[i]);
+        else if(str[i] >= 'A' && str[i] <= 'Z')
+            result += tolower((char)str[i]);
+        else
+            result += str[i];
     }
+    return result;
 }
-return str;
