2a760038c98b9ebb3b72db926a52bf701ec6d862
beep boop <githubbot@vadim.me>
2024-07-12 15:58:09 +0200

    Solution 67 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q5c5whyb Prompt:   +++ |+#include <cassert>     1 | int main() { e7c7fd59-2d33-46fc-ba24-5aa7f9a42ebb.cpp: At global scope: e7c7fd59-2d33-46fc-ba24-5aa7f9a42ebb.cpp:7:5: error: redefinition of ‘int main() ’     7 | int main(){       |     ^~~~ e7c7fd59-2d33-46fc-ba24-5aa7f9a42ebb.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from e7c7fd59-2d33-46fc-ba24-5aa7f9a42ebb.cpp:6: e7c7fd59-2d33-46fc-ba24-5aa7f9a42ebb.cpp: In function ‘int main()’: e7c7fd59-2d33-46fc-ba24-5aa7f9a42ebb.cpp:8:17: error: ‘max_element’ was not decl ared in this scope     8 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ e7c7fd59-2d33-46fc-ba24-5aa7f9a42ebb.cpp:8:13: error: ‘abs’ was not declared in this scope     8 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -2,7 +2,5 @@
 #include <algorithm>
 #include <cmath>
 
-int main() {
-    assert(std::abs(std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})) - 12 < 1e-4);
-    return 0;
-}
+assert(std::abs(std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})) - 12 < 1e-4);
+return 0;
