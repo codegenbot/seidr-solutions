c00f006a4efeea8692202f9305e34edf5555c863
beep boop <githubbot@vadim.me>
2024-07-12 21:23:51 +0200

    Solution 96 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/l87g9ae4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f41e170a-8a63-4911-9d80-798fe9ffc b79.py", line 5, in <module>     print(solve_boolean(input_str))           ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f41e170a-8a63-4911-9d80-798fe9ffc b79.py", line 3, in solve_boolean     return eval(expression.replace("|", " or ").replace("&", " and "), {"__built ins__": None})            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^   File "<string>", line 1, in <module> TypeError: 'NoneType' object is not subscriptable

--- solve-boolean.py
+++ solve-boolean.py
@@ -2,5 +2,6 @@ def solve_boolean(expression):
     expression = expression.replace("T", "True").replace("F", "False")
     return eval(expression.replace("|", " or ").replace("&", " and"))
 
+
 input_str = input().strip()
 print(solve_boolean(input_str))
