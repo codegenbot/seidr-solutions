5101abba9c9b22b47df32e5048ca9138cce6afbf
beep boop <githubbot@vadim.me>
2024-07-12 05:59:25 +0200

    Solution 23 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/7ealauoz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b55390d-2c83-4b72-a2e3-8754e3161 e04.py", line 13, in <module>     check(filter_by_prefix)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b55390d-2c83-4b72-a2e3-8754e3161 e04.py", line 8, in check     assert filter_by_prefix(            ^^^^^^^^^^^^^^^^^ TypeError: filter_by_prefix() takes from 0 to 1 positional arguments but 2 were given

--- Python_29.py
+++ Python_29.py
@@ -1,5 +1,4 @@
-def filter_by_prefix(user_input=None):
-    """
+def filter_by_prefix(user_input=None): 
     strings = input('Enter a list of strings (separated by commas): ') if user_input is None else user_input
     prefix = input('Enter a prefix to filter: ')
     strings_list = [s.strip() for s in strings.split(',')]
