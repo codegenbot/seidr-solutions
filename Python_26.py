98a874d5fb7331e4d5d0945ea00855a16c57cace
beep boop <githubbot@vadim.me>
2024-07-12 07:07:30 +0200

    Solution 61 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/t61x2loc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f4e8e6bb-df24-4933-b01b-a3db8b5e3 0fe.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_26.py
+++ Python_26.py
@@ -1,8 +1,8 @@
-def remove_duplicates(input_numbers):
+def remove_duplicates(input_numbers): 
     unique_numbers = []
 
     for num in input_numbers:
         if num not in unique_numbers:
             unique_numbers.append(num)
 
-    return unique_numbers
+    return list(set(unique_numbers))
