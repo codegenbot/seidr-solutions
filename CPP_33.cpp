744608f35319cb959a585aee89b9b78c771b7563
beep boop <githubbot@vadim.me>
2024-07-12 14:50:53 +0200

    Solution 42 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d8rx6wrb Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 6e34904c-229d-4e26-9700-11015ddae173.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,1 +1,36 @@
-std::vector<std::vector<int>> sort_third(std::vector<int> l) {
+#include <algorithm>
+#include <assert.h>
+#include <vector>
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
+            return false;
+    }
+    return true;
+}
+
+std::vector<int> sort_third(std::vector<int> l) {
+    std::vector<int> result;
+    for (int i = 0; i < l.size(); i++) {
+        if (i % 3 == 0) {
+            auto temp = l.begin() + i;
+            while(temp != l.end() && *temp % 3 == 0) {
+                temp++;
+            }
+            std::sort(std::make_reverse_iterator(temp), l.end());
+            for (; temp != l.end(); temp++) {
+                if (*temp % 3 == 0) {
+                    result.push_back(*temp);
+                } else {
+                    break;
+                }
+            }
+        } else {
+            result.push_back(l[i]);
+        }
+    }
+    return result;
+}
