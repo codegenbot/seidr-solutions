12fdeb6a2fdb3c0cda0c5cae37b1380ef2423ed9
beep boop <githubbot@vadim.me>
2024-07-12 08:58:25 +0200

    Solution 40 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/oaf2ryzo Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2a9e033f-943a-475f-b945-357005bc9 7f9.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,1 +1,5 @@
-" ".join(map(str, sorted(digits)))
+def sorted_list_sum():
+    lst = input("Enter list of words: ").split()
+    digits = [int(word) for word in lst if word.isdigit()]
+    result = [" ".join(map(str, sorted(digits)))]
+    print(result)
