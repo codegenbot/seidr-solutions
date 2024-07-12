3ec325bf770a14a2ab93388185b3dbeeb2cc4592
beep boop <githubbot@vadim.me>
2024-07-12 19:49:09 +0200

    Solution 51 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/glxnmwbk Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from fb0ffe5c-caf8-4997-ad1b-b8f06d739276.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,6 +1,6 @@
-```cpp
 #include <vector>
 #include <algorithm>
+#include <initializer_list>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
@@ -12,8 +12,8 @@ std::vector<int> remove_duplicates(const std::vector<int>& vec) {
 }
 
 int main() {
-    test1 = remove_duplicates(std::vector<int>({{1, 2, 3, 2, 4, 3, 5}}));
-    test2 = {1, 2, 3, 4, 5};
+    std::vector<int> test1 = remove_duplicates({1, 2, 3, 2, 4, 3, 5});
+    std::vector<int> test2 = {1, 2, 3, 4, 5};
     if (issame(test1, test2)) {
         for (int i : test1) {
             std::cout << i << " ";
