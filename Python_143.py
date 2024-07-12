6c9c95cbc639ca592b6e12d1ff43a1a66521e37c
beep boop <githubbot@vadim.me>
2024-07-12 23:05:24 +0200

    Solution 10 of Python/143, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/20t209x4 Prompt: Complete the following code given the task description and function signature.

--- Python_143.py
+++ Python_143.py
@@ -1,7 +1,9 @@
 def words_in_sentence(sentence):
+    result = []
     primes = [
-        i
-        for i in range(len(sentence.split()))
-        if all(i % j for j in range(2, int(i**0.5) + 1))
+        i for i in range(2, 11) if all(i % j > 0 for j in range(2, int(i**0.5) + 1))
     ]
-    return " ".join([sentence.split()[i] for i in primes])
+    for word in sentence.split():
+        if len(word) in primes:
+            result.append(word)
+    return " ".join(result)
