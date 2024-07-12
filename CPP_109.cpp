fb97dff0445e31193ef48b4ab22f7621b360cf8f
beep boop <githubbot@vadim.me>
2024-07-12 02:55:02 +0200

    Solution 22 of CPP/109, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/r3wxqkv5 Prompt: /usr/include/c++/11/bits/stl_algobase.h:522:42:   required from ‘_OI std::__copy _move_a1(_II, _II, _OI) [with bool _IsMove = true; _II = std::reverse_iterator<c onst int*>; _OI = std::reverse_iterator<const int*>]’ /usr/include/c++/11/bits/stl_algobase.h:530:31:   required from ‘_OI std::__copy _move_a(_II, _II, _OI) [with bool _IsMove = true; _II = std::reverse_iterator<__ gnu_cxx::__normal_iterator<const int*, std::vector<int> > >; _OI = std::reverse_ iterator<__gnu_cxx::__normal_iterator<const int*, std::vector<int> > >]’ /usr/include/c++/11/bits/stl_algobase.h:652:38:   required from ‘_OI std::move(_ II, _II, _OI) [with _II = std::reverse_iterator<__gnu_cxx::__normal_iterator<con st int*, std::vector<int> > >; _OI = std::reverse_iterator<__gnu_cxx::__normal_i terator<const int*, std::vector<int> > >]’ /usr/include/c++/11/bits/stl_algo.h:1336:5:   required from ‘_RandomAccessIterat or std::_V2::__rotate(_RandomAccessIterator, _RandomAccessIterator, _RandomAcces sIterator, std::random_access_iterator_tag) [with _RandomAccessIterator = std::r everse_iterator<__gnu_cxx::__normal_iterator<const int*, std::vector<int> > >]’ /usr/include/c++/11/bits/stl_algo.h:1414:27:   required from ‘_FIter std::_V2::r otate(_FIter, _FIter, _FIter) [with _FIter = std::reverse_iterator<__gnu_cxx::__ normal_iterator<const int*, std::vector<int> > >]’ 17a48a67-9cbf-4e7b-b23a-f55ae25be81a.cpp:12:20:   required from here /usr/include/c++/11/bits/stl_algobase.h:405:25: error: assignment of read-only l ocation ‘__result.std::reverse_iterator<const int*>::operator*()’   405 |               *__result = std::move(*__first);       |               ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~

--- CPP_109.cpp
+++ CPP_109.cpp
@@ -5,12 +5,12 @@
 
 bool move_one_ball(const std::vector<int>& arr) {
     int n = arr.size();
+    std::vector<int> arr_copy = arr;
     for (int i = 0; i < n; ++i) {
-        if (std::is_sorted(arr.begin(), arr.end())) {
+        if (std::is_sorted(arr_copy.begin(), arr_copy.end())) {
             return true;
         }
-        std::vector<int> temp(arr);
-        std::rotate(temp.rbegin(), temp.rbegin() + 1, temp.rend());
+        std::rotate(arr_copy.rbegin(), arr_copy.rbegin() + 1, arr_copy.rend());
     }
     return false;
 }
