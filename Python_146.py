d1c78d6299d53cbc8a9143e113b6c0534b29f238
beep boop <githubbot@vadim.me>
2024-07-12 18:27:42 +0200

    Solution 36 of Python/146, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qi0zzcf8 Prompt: Complete the following code given the task description and function signature.

--- Python_146.py
+++ Python_146.py
@@ -1,3 +1,6 @@
-```
 def specialFilter(nums):
-    return sum(1 for num in nums if abs(num) > 10 and str(abs(num))[-1] in '13579')
+    return sum(
+        1
+        for num in nums
+        if abs(num) > 10 and (num % 10) % 2 != 0 and (abs(num) // 10) % 2 != 0
+    )
