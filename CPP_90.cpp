934bcbd77aa4b0719bf3a76789235b2d76252967
beep boop <githubbot@vadim.me>
2024-07-12 15:25:53 +0200

    Solution 40 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/x7whuw11 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -3,26 +3,28 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<std::string> lst;
-int n;
-
-std::cin >> n;
-std::cout << "Enter " << n << " integers: ";
-for(int i = 0; i < n; i++){
-    int x;
-    std::cin >> x;
-    lst.push_back(std::to_string(x));
-}
-if(lst.size() < 2) std::cout << "-1";
-else {
-    std::sort(lst.begin(), lst.end());
-    bool same = true;
-    for(int i = 0; i < lst.size() - 1; i++){
-        if(lst[i] != lst[i+1]) {
-            std::cout << lst[i];
-            same = false;
-            break;
-        }
+int main() {
+    std::vector<int> lst;
+    int n;
+    std::cout << "Enter number of elements: ";
+    std::cin >> n;
+    std::cout << "Enter " << n << " integers: ";
+    for(int i = 0; i < n; i++){
+        int x;
+        std::cin >> x;
+        lst.push_back(x);
     }
-    if(same) std::cout << "-1";
+    if(lst.size() < 2) std::cout << "-1";
+    else {
+        std::sort(lst.begin(), lst.end());
+        bool same = true;
+        for(int i = 0; i < lst.size() - 1; i++){
+            if(lst[i] != lst[i+1]) {
+                std::cout << lst[i];
+                same = false;
+                break;
+            }
+        }
+        if(same) std::cout << "-1";
+    }
 }
