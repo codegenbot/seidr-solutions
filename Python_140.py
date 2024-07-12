85f66b21d28afc4f4ae87eb473990d82645b40c3
beep boop <githubbot@vadim.me>
2024-07-12 09:35:21 +0200

    Solution 65 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jkuz8vxn Prompt: Program did not receive expected input

--- Python_140.py
+++ Python_140.py
@@ -4,6 +4,6 @@ import re
 def solve(text):
     return re.sub(r"(\s+)([a-zA-Z])", lambda m: "_" + m.group(2).upper(), text)
 
-text = "Enter the text: "
+text = "Enter your name or title: "
 user_input = input(text)
 print(solve(user_input))
