638b4bf35d2eda674fbef5395c5895bdfc5f1495
beep boop <githubbot@vadim.me>
2024-07-13 00:04:29 +0200

    Solution 9 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/rbtvzix5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ec879533-2958-4cd8-9cab-2642d8148 dae.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_4.py
+++ Python_4.py
@@ -1,13 +1,10 @@
-```
-from typing import List
-
-
 def mean_absolute_deviation(numbers: List[float]) -> float:
-    if not numbers:
-        return 0
-
+    if not numbers or len(numbers) == 0 or any(x <= 0 for x in numbers):
+        return None  
     try:
         mean = sum(numbers) / len(numbers)
         return sum(abs(num - mean) for num in numbers) / len(numbers)
-    except (ZeroDivisionError, TypeError):
-        return 'Error: Input list must not be empty and contain only numeric values'
+    except ZeroDivisionError:
+        return None  
+    except TypeError:
+        return None
