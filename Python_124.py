c0fb88e08d9850f2d891731d3718b76d6d6d503a
beep boop <githubbot@vadim.me>
2024-07-12 12:41:16 +0200

    Solution 3 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/q3dspwfn Prompt: Complete the following code given the task description and function signature.

--- Python_124.py
+++ Python_124.py
@@ -6,9 +6,6 @@ def valid_date(date):
     month, day, year = map(int, parts)
     if month < 1 or month > 12:
         return False
-    days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
-    if month == 2 and day > 29:
-        return False
-    elif (month in [1,3,5,7,8,10,12] and day > 31) or (month in [4,6,9,11] and day > 30):
+    if (month in [4,6,9,11] and day > 30) or (month == 2 and day > 29) or (day > 31):
         return False
     return True
