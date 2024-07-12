88a2698fd3a2a4435e9f54b7bfc549b6e66f0f1a
beep boop <githubbot@vadim.me>
2024-07-12 19:23:42 +0200

    Solution 0 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/w96m0pum Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_69.cpp
@@ -0,0 +1,16 @@
+int search(vector<int> lst) {
+    map<int, int> freq;
+    for (int x : lst) {
+        if (!freq.count(x)) {
+            freq[x] = 1;
+        } else {
+            freq[x]++;
+        }
+    }
+    for (auto p : freq) {
+        if (p.second >= p.first && p.first > 0) {
+            return p.first;
+        }
+    }
+    return -1;
+}
