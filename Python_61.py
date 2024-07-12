5e9860139c77f45d5ba6c95ec977f3d69200e1bb
beep boop <githubbot@vadim.me>
2024-07-12 20:42:21 +0200

    Solution 7 of Python/61, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/2vsp5qpm Prompt: Complete the following code given the task description and function signature.

--- Python_61.py
+++ Python_61.py
@@ -1,12 +1,11 @@
-```
 def correct_bracketing(brackets: str):
     stack = []
     for bracket in brackets:
         if bracket == "(":
             stack.append(bracket)
         elif bracket == ")":
-            if len(stack) == 0:
-                return False
+            if len(stack) > 0 and stack.pop() == "(":
+                continue
             else:
-                stack.pop()
+                return False
     return len(stack) == 0
