c0c94ab8506ad19edae80f1296054ebc5a0d5efd
beep boop <githubbot@vadim.me>
2024-07-12 13:55:58 +0200

    Solution 0 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/5y8dhe10 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,46 +1,49 @@
-```cpp
-#include <iostream>
 #include <vector>
-#include <algorithm>
+using namespace std;
 
-std::pair<std::vector<int>, std::vector<int>> cutVector(std::vector<int> vec) {
+vector<int> cutVector(vector<int>& vec) {
     int minDiff = INT_MAX;
-    int pos = 0;
-
-    for (int i = 1; i < vec.size(); ++i) {
-        int diff = abs(vec[i] - vec[i-1]);
+    int splitIndex = -1;
+    
+    for (int i = 0; i < vec.size() - 1; i++) {
+        int leftSum = 0, rightSum = 0;
+        
+        for (int j = 0; j <= i; j++)
+            leftSum += vec[j];
+        for (int j = i + 1; j < vec.size(); j++)
+            rightSum += vec[j];
+        
+        int diff = abs(leftSum - rightSum);
         if (diff < minDiff) {
             minDiff = diff;
-            pos = i;
+            splitIndex = i;
         }
     }
-
-    return {{vec.begin(), vec.begin() + pos}, {vec.begin() + pos, vec.end()}};
+    
+    vector<int> leftVec(vec.begin(), vec.begin() + splitIndex + 1);
+    vector<int> rightVec(vec.begin() + splitIndex + 1, vec.end());
+    
+    return {leftVec, rightVec};
 }
 
 int main() {
     int n;
-    std::cin >> n;
-    std::vector<int> vec(n+1);
-    for (int i = 0; i <= n; ++i)
-        std::cin >> vec[i];
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
     
-    std::pair<std::vector<int>, std::vector<int>> res = cutVector(vec);
-
-    std::cout << "[";
-    for (int num : res.first) {
-        std::cout << num;
-        if (&num != &res.first.back())
-            std::cout << " ";
-    }
-    std::cout << "] [";
-
-    for (int num : res.second) {
-        std::cout << num;
-        if (&num != &res.second.back())
-            std::cout << " ";
-    }
+    pair<vector<int>, vector<int>> result = cutVector(vec);
     
-    std::cout << "]" << std::endl;
+    cout << "Left: ";
+    for (int num : result.first)
+        cout << num << " ";
+    cout << endl;
     
+    cout << "Right: ";
+    for (int num : result.second)
+        cout << num << " ";
+    cout << endl;
+    
+    return 0;
 }
