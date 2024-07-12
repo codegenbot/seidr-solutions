547f9aae4eccf8719098db2917b4c8bacd1b1c3a
beep boop <githubbot@vadim.me>
2024-07-13 01:36:29 +0200

    Solution 0 of CPP/31, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/nqni2m05 Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_31.cpp
@@ -0,0 +1,11 @@
+bool is_divisible = false;
+if(n <= 1) {
+    return false;
+}
+for(int i=2; i*i<=n; i++) {
+    if(n%i == 0) {
+        is_divisible = true;
+        break;
+    }
+}
+return !is_divisible;
