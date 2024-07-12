5b737cb92c3f6e086d5fad0214fc0a16191dab6f
beep boop <githubbot@vadim.me>
2024-07-12 15:16:47 +0200

    Solution 1 of Python/153, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/xspmkkw1 Prompt: Complete the following code given the task description and function signature.

--- Python_153.py
+++ Python_153.py
@@ -1,10 +1,8 @@
 def Strongest_Extension(class_name, extensions):
     def strength(extension):
-        return sum(1 for char in extension if char.isupper()) - sum(
-            1 for char in extension if char.islower()
-        )
+        CAP = sum(1 for char in extension if char.isupper())
+        SM = sum(1 for char in extension if char.islower())
+        return CAP - SM
 
-    strongest_extension = max(
-        extensions, key=lambda x: (strength(x), extensions.index(x))
-    )
+    strongest_extension = max(extensions, key=strength)
     return f"{class_name}.{strongest_extension}"
