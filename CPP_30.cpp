a799a3b914e69fc2ae92f875ac3dc5602d22eff5
beep boop <githubbot@vadim.me>
2024-07-12 14:19:38 +0200

    Solution 8 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/p2ebo0ht Prompt:       |           ^~~~~~ In file included from 8b3e450b-028a-4aff-a0cd-c07bdfde8fcf.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8b3e450b-028a-4aff-a0cd-c07bdfde8fcf.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 8b3e450b-028a-4aff-a0cd-c07bdfde8fcf.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 8b3e450b-028a-4aff-a0cd-c07bdfde8fcf.cpp:13: 8b3e450b-028a-4aff-a0cd-c07bdfde8fcf.cpp: In function ‘int main()’: 8b3e450b-028a-4aff-a0cd-c07bdfde8fcf.cpp:23:20: error: ‘get_positive’ was not de clared in this scope    23 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 8b3e450b-028a-4aff-a0cd-c07bdfde8fcf.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,21 +1,16 @@
-```cpp
 #include <vector>
 #include <algorithm>
 
 bool issame(const std::vector<float>& a, const std::vector<float>& b) {
-    return a == b;
+    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
 }
 
 std::vector<float> get_positive(std::vector<float> l) {
-    vector<float> result;
+    std::vector<float> result;
     for (float num : l) {
         if (num > 0) {
             result.push_back(num);
         }
     }
     return result;
-}
-
-int main() {
-    assert(issame(get_positive({}), {}));  
 }
