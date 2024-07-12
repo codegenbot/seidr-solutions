3052237164aa6fb7e254e96cdd180c9ac73b959b
beep boop <githubbot@vadim.me>
2024-07-12 14:50:56 +0200

    Solution 70 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/3pjnfwz2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48eb216d-9e64-4603-9f41-1b2d5137c 024.py", line 22, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48eb216d-9e64-4603-9f41-1b2d5137c 024.py", line 19, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,2 +1,19 @@
+```python
 def is_palindrome(s: str) -> bool:
-    return s == s[::-1] or False
+    return s == s[::-1]
+
+
+def make_palindrome(string: str) -> str:
+    if is_palindrome(string):
+        return string
+    n = len(string)
+    new_string = "#" + "#".join(list(string)) + "#"
+    
+    while not is_palindrome(new_string):
+        middle = (len(new_string) - 1) // 2
+        for i in range(len(new_string)):
+            if i < middle or ((2 * middle + 1) > i >= middle and i % 2 == 0):
+                new_string = new_string[:i] + string[0] + new_string[i:]
+                break
+
+    return new_string
