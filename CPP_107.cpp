e1d5ca7ec9a541415edbb06fd8845043a2000b5e
beep boop <githubbot@vadim.me>
2024-07-12 02:35:19 +0200

    Solution 3 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/52q4ikre Prompt:       |                    ^~~ 9d9cb05b-72c4-47f6-b86c-38b03d42b026.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9d9cb05b-72c4-47f6-b86c-38b03d42b026.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9d9cb05b-72c4-47f6-b86c-38b03d42b026.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9d9cb05b-72c4-47f6-b86c-38b03d42b026.cpp:18: 9d9cb05b-72c4-47f6-b86c-38b03d42b026.cpp: In function ‘int main()’: 9d9cb05b-72c4-47f6-b86c-38b03d42b026.cpp:28:20: error: ‘even_odd_palindrome’ was  not declared in this scope    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 9d9cb05b-72c4-47f6-b86c-38b03d42b026.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,5 +1,22 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b){
+vector<int> even_odd_palindrome(int n){
+    vector<int> res(2, 0);
+    for (int i = 1; i <= n; ++i) {
+        string num = to_string(i);
+        string rev = num;
+        reverse(rev.begin(), rev.end());
+        if (num == rev) {
+            if (i % 2 == 0) {
+                res[0]++;
+            } else {
+                res[1]++;
+            }
+        }
+    }
+    return res;
+}
+
+bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
