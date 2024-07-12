d7c00f19e9195442bf67728169563ef6b44eb8a0
beep boop <githubbot@vadim.me>
2024-07-12 05:00:49 +0200

    Solution 4 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ivyc6q78 Prompt:    37 | return "None";       | ^~~~~~ 4a28a8c4-2518-49f7-aaf1-e2a71cc77a83.cpp:38:1: error: expected declaration befor e ‘}’ token    38 | }       | ^ In file included from 4a28a8c4-2518-49f7-aaf1-e2a71cc77a83.cpp:40: 4a28a8c4-2518-49f7-aaf1-e2a71cc77a83.cpp: In function ‘int main()’: 4a28a8c4-2518-49f7-aaf1-e2a71cc77a83.cpp:42:13: error: ‘boost’ has not been decl ared    42 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 4a28a8c4-2518-49f7-aaf1-e2a71cc77a83.cpp:42:29: error: ‘string’ was not declared  in this scope    42 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 4a28a8c4-2518-49f7-aaf1-e2a71cc77a83.cpp:42:37: error: ‘compare_one’ was not dec lared in this scope    42 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,51 +1,46 @@
-#include <iostream>
 #include <boost/any.hpp>
-#include <string>
+#include <iostream>
 #include <cassert>
 
-using namespace std;
-
-boost::any compare_one(boost::any a, boost::any b) {
-    if(a.type() == typeid(int) && b.type() == typeid(int)){
-        if(boost::any_cast<int>(a) > boost::any_cast<int>(b))
+boost::any compare_one(const boost::any& a, const boost::any& b) {
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        if (boost::any_cast<int>(a) > boost::any_cast<int>(b))
             return a;
-        else if(boost::any_cast<int>(a) < boost::any_cast<int>(b))
+        else if (boost::any_cast<int>(a) < boost::any_cast<int>(b))
             return b;
         else
-            return "None";
+            return std::string("None");
     }
-    else if(a.type() == typeid(float) && b.type() == typeid(float)){
-        if(boost::any_cast<float>(a) > boost::any_cast<float>(b))
+    else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        if (boost::any_cast<float>(a) > boost::any_cast<float>(b))
             return a;
-        else if(boost::any_cast<float>(a) < boost::any_cast<float>(b))
+        else if (boost::any_cast<float>(a) < boost::any_cast<float>(b))
             return b;
         else
-            return "None";
+            return std::string("None");
     }
-    else if(a.type() == typeid(string) && b.type() == typeid(string)){
-        float a_float = stof(boost::any_cast<string>(a));
-        float b_float = stof(boost::any_cast<string>(b));
-        if(a_float > b_float)
+    else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+        float a_float = std::stof(boost::any_cast<std::string>(a));
+        float b_float = std::stof(boost::any_cast<std::string>(b));
+        if (a_float > b_float)
             return a;
-        else if(a_float < b_float)
+        else if (a_float < b_float)
             return b;
         else
-            return "None";
+            return std::string("None");
     }
-    else if((a.type() == typeid(int) && b.type() == typeid(string)) || (a.type() == typeid(string) && b.type() == typeid(int))){
-        float a_float = stof(a.type() == typeid(int) ? to_string(boost::any_cast<int>(a)) : boost::any_cast<string>(a));
-        float b_float = stof(b.type() == typeid(int) ? to_string(boost::any_cast<int>(b)) : boost::any_cast<string>(b));
-        if(a_float > b_float)
+    else if ((a.type() == typeid(int) && b.type() == typeid(std::string)) ||
+             (a.type() == typeid(std::string) && b.type() == typeid(int))) {
+        float a_float = stof(a.type() == typeid(int) ? std::to_string(boost::any_cast<int>(a)) :
+                                                        boost::any_cast<std::string>(a));
+        float b_float = stof(b.type() == typeid(int) ? std::to_string(boost::any_cast<int>(b)) :
+                                                        boost::any_cast<std::string>(b));
+        if (a_float > b_float)
             return a;
-        else if(a_float < b_float)
+        else if (a_float < b_float)
             return b;
         else
-            return "None";
+            return std::string("None");
     }
-    return "None";
-}
-
-int main() {
-    assert(boost::any_cast<string>(compare_one(string("1"), 1)) == "None");
-    return 0;
+    return std::string("None");
 }
