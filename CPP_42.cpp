62d873e496ae59caa1c12b0a6a711a1e56711889
beep boop <githubbot@vadim.me>
2024-07-12 14:44:59 +0200

    Solution 34 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zj95medv Prompt: 8f4e5ea9-5f22-4baa-9956-2914242beab0.cpp:26:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    26 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 8f4e5ea9-5f22-4baa-9956-2914242beab0.cpp:8:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     8 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 8f4e5ea9-5f22-4baa-9956-2914242beab0.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 8f4e5ea9-5f22-4baa-9956-2914242beab0.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,23 +1,17 @@
-#include <initializer_list>
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-vector<int> incr_list(initializer_list<int> l){
-    vector<int> result;
-    for(int i: l) {
-        result.push_back(i+1);
+vector<int> incr_list(const vector<int>& l){
+    vector<int> result(l.size());
+    for(int i = 0; i < l.size(); i++) {
+        result[i] = l[i]+1;
     }
     return result;
 }
 
 int main() {
-    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6,3, 4, 4, 10, 1, 124}));
+    assert(issame({6, 3, 6,3, 4, 4, 10, 1, 124}, incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123})));
     return 0;
 }
