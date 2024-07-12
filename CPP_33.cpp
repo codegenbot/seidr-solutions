730e8b7dd9b9c4b0633b27e64b1cf51e3e574bfd
beep boop <githubbot@vadim.me>
2024-07-12 14:56:20 +0200

    Solution 47 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d8rx6wrb Prompt:    12 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ df8b5727-b80e-41b9-96b1-d493b9084117.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ df8b5727-b80e-41b9-96b1-d493b9084117.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from df8b5727-b80e-41b9-96b1-d493b9084117.cpp:11: df8b5727-b80e-41b9-96b1-d493b9084117.cpp: In function ‘int main()’: df8b5727-b80e-41b9-96b1-d493b9084117.cpp:21:20: error: ‘sort_third’ was not decl ared in this scope    21 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ df8b5727-b80e-41b9-96b1-d493b9084117.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -18,33 +18,5 @@ std::vector<int> sort_third(std::vector<int> input) {
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    std::vector<int> v1, v2, v3;
-    
-    for (int i = 0; i < n; i++) {
-        int x;
-        std::cin >> x;
-        if (i % 3 == 0) {
-            v1.push_back(x);
-        } else if (i % 3 == 1) {
-            v2.push_back(x);
-        } else {
-            v3.push_back(x);
-        }
-    }
-
-    v1 = sort_third(v1);
-    v2 = sort_third(v2);
-    v3 = sort_third(v3);
-
-    if (issame(v1, v2)) {
-        for (int i : v3) std::cout << i << " ";
-    } else {
-        for (int i : v1) std::cout << i << " ";
-        for (int i : v2) std::cout << i << " ";
-        for (int i : v3) std::cout << i << " ";
-    }
-
     return 0;
 }
