21dc746deddaf01d9df5642cffef471622f8f118
beep boop <githubbot@vadim.me>
2024-07-12 14:18:42 +0200

    Solution 4 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q5c5whyb Prompt: 2c122678-9d9d-43fc-953b-cfc920b89acc.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return *max_element(l.begin(), l.end());       | ^~~~~~ In file included from 2c122678-9d9d-43fc-953b-cfc920b89acc.cpp:3: 2c122678-9d9d-43fc-953b-cfc920b89acc.cpp: In function ‘int main()’: 2c122678-9d9d-43fc-953b-cfc920b89acc.cpp:5:17: error: ‘max_element’ was not decl ared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ 2c122678-9d9d-43fc-953b-cfc920b89acc.cpp:5:13: error: ‘abs’ was not declared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,6 +1,13 @@
 #include <algorithm>
-#include <cmath>
+using namespace std;
 
-int main() {
-    return std::abs(*std::max_element(vector_name.begin(), vector_name.end()));
+int find_max(int n) {
+    vector<int> v;
+    for (int i = 0; i < n; i++) {
+        int input;
+        cout << "Enter number (" << i + 1 << "/" << n << "): ";
+        cin >> input;
+        v.push_back(input);
+    }
+    return *max_element(v.begin(), v.end());
 }
