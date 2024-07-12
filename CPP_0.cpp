f430df5ae1eb1c308c2d74ac9b8cc55b5bceeff2
beep boop <githubbot@vadim.me>
2024-07-12 13:04:55 +0200

    Solution 8 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/avzqcthh Prompt: 940cbdce-ff8b-4568-80e3-9ded4b585f21.cpp:1:48: error: expected primary-expressio n before ‘float’     1 | bool has_close_elements(vector<float> numbers, float threshold){       |                                                ^~~~~ 940cbdce-ff8b-4568-80e3-9ded4b585f21.cpp:1:63: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool has_close_elements(vector<float> numbers, float threshold){       |                                                               ^ 940cbdce-ff8b-4568-80e3-9ded4b585f21.cpp: In function ‘int main()’: 940cbdce-ff8b-4568-80e3-9ded4b585f21.cpp:13:5: error: ‘vector’ was not declared in this scope    13 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 940cbdce-ff8b-4568-80e3-9ded4b585f21.cpp:13:12: error: expected primary-expressi on before ‘float’    13 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~ In file included from 940cbdce-ff8b-4568-80e3-9ded4b585f21.cpp:11: 940cbdce-ff8b-4568-80e3-9ded4b585f21.cpp:14:31: error: ‘has_close_elements’ cann ot be used as a function    14 |     assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false) ;       |             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,17 +1,12 @@
 #include <vector>
 #include <cmath>
 
-bool has_close_elements(std::vector<float> numbers, float threshold){
+int has_close_elements(std::vector<float> numbers, float threshold){
     for(int i = 0; i < numbers.size(); i++){
         for(int j = i + 1; j < numbers.size(); j++){
             if(abs(numbers[i] - numbers[j]) <= threshold)
-                return true;
+                return 1;
         }
     }
-    return false;
-}
-
-int main() {
-    std::vector<float> a={(float)1.0, (float)2.0, (float)3.9, (float)4.0, (float)5.0, (float)2.2};
-    assert (has_close_elements({(float)1.1, (float)2.2, (float)3.1, (float)4.1, (float)5.1}, 0.5) == false);
+    return 0;
 }
