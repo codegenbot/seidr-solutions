73a90da06cafbbb8d7b234fb258ed1c0aacef0d8
beep boop <githubbot@vadim.me>
2024-07-12 18:46:08 +0200

    Solution 44 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dhj7i5v3 Prompt:    37 | } else if (a.type() == typeid(double) && b.type() == typeid(string)) {       |   ^~~~ a32484d1-0d2e-4beb-ab20-da8089672292.cpp:46:3: error: expected unqualified-id be fore ‘else’    46 | } else {       |   ^~~~ In file included from a32484d1-0d2e-4beb-ab20-da8089672292.cpp:50: a32484d1-0d2e-4beb-ab20-da8089672292.cpp: In function ‘int main()’: a32484d1-0d2e-4beb-ab20-da8089672292.cpp:52:13: error: ‘boost’ has not been decl ared    52 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ a32484d1-0d2e-4beb-ab20-da8089672292.cpp:52:29: error: ‘string’ was not declared  in this scope    52 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ a32484d1-0d2e-4beb-ab20-da8089672292.cpp:52:37: error: ‘compare_one’ was not dec lared in this scope    52 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,45 +1,33 @@
+```cpp
 #include <boost/any.hpp>
 #include <string>
 
 std::any compare_one(std::any a, std::any b) {
-    if (a.type() == typeid(int) && a.type() == typeid(b)) {
-        int x = boost::any_cast<int>(a);
-        int y = boost::any_cast<int>(b);
-        if (x > y)
-            return a;
-        else if (y > x)
-            return b;
-        else
+    try {
+        if (a.type() == typeid(int) && b.type() == typeid(int)) {
+            int x = boost::any_cast<int>(a);
+            int y = boost::any_cast<int>(b);
+            return (x > y) ? a : ((y > x) ? b : "None");
+        } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
+            double x = boost::any_cast<double>(a);
+            double y = boost::any_cast<double>(b);
+            return (x > y) ? a : ((y > x) ? b : "None");
+        } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+            std::string x = boost::any_cast<std::string>(a);
+            std::string y = boost::any_cast<std::string>(b);
+            return (x > y) ? a : ((y > x) ? b : "None");
+        } else if (a.type() == typeid(std::string) && b.type() == typeid(double)) {
+            double y = boost::any_cast<double>(b);
+            std::string x = boost::any_cast<std::string>(a);
+            return (std::stod(x) > y) ? a : ((y > std::stod(x)) ? b : "None");
+        } else if (a.type() == typeid(double) && b.type() == typeid(std::string)) {
+            double x = boost::any_cast<double>(a);
+            std::string y = boost::any_cast<std::string>(b);
+            return (x > std::stod(y)) ? a : ((std::stod(y) > x) ? b : "None");
+        } else {
             return "None";
-    } else if (a.type() == typeid(double) && a.type() == typeid(b)) {
-        double x = boost::any_cast<double>(a);
-        double y = boost::any_cast<double>(b);
-        if (x > y)
-            return a;
-        else if (y > x)
-            return b;
-        else
-            return "None";
-    } else if (a.type() == typeid(std::string) && a.type() == typeid(b)) {
-        std::string x = boost::any_cast<std::string>(a);
-        std::string y = boost::any_cast<std::string>(b);
-        if (x > y)
-            return a;
-        else if (y > x)
-            return b;
-        else
-            return "None";
-    } else if ((a.type() == typeid(std::string) && b.type() == typeid(double)) || 
-               (a.type() == typeid(double) && b.type() == typeid(std::string))) {
-        double y = boost::any_cast<double>(b);
-        std::string x = boost::any_cast<std::string>(a);
-        if (std::stod(x) > y)
-            return a;
-        else if (y > std::stod(x))
-            return b;
-        else
-            return "None";
-    } else {
+        }
+    } catch (const boost::bad_any_cast& e) {
         return "None";
     }
 }
