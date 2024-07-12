8b019d5f1e52d46bed8c302698f5945b10204143
beep boop <githubbot@vadim.me>
2024-07-12 05:00:33 +0200

    Solution 5 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/kj1g4ec2 Prompt:    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fe9535f5-f11e-49dc-a9cb-08a3d5ea9c67.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fe9535f5-f11e-49dc-a9cb-08a3d5ea9c67.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fe9535f5-f11e-49dc-a9cb-08a3d5ea9c67.cpp:13: fe9535f5-f11e-49dc-a9cb-08a3d5ea9c67.cpp: In function ‘int main()’: fe9535f5-f11e-49dc-a9cb-08a3d5ea9c67.cpp:23:20: error: ‘largest_smallest_integer s’ was not declared in this scope    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ fe9535f5-f11e-49dc-a9cb-08a3d5ea9c67.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,10 +1,19 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
+bool issame(vector<int> a, vector<int> b) {
+    int n = a.size();
+    if(b.size() != n) {
+        return false;
+    }
+    for(int i = 0; i < n; ++i) {
+        if(a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-std::vector<int> largest_smallest_integers(std::vector<int> lst){
+std::vector<int> largest_smallest_integers(vector<int> lst){
     int largest_negative = 0, smallest_positive = 0;
     for (int num : lst) {
         if (num < 0 && num < largest_negative) {
