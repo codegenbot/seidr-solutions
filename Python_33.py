c4c617aff9253ea6cbacb672e02bc6d9cb50ef12
beep boop <githubbot@vadim.me>
2024-07-12 11:41:21 +0200

    Solution 70 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/0grq0607 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2d8fbec1-1447-4425-8604-1e3797f52 851.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2d8fbec1-1447-4425-8604-1e3797f52 851.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,12 +1,9 @@
-```
 def sort_third(l: list):
-    sorted_list = []
-    index = 0
-    for val in l:
-        if index % 3 == 2:
-            sorted_list.append(val)
-            sorted_list.extend(sorted([x for x in l if (index - l.index(x)) % 3 != 0])[::-1][1:])
-        else:
-            sorted_list.append(val)
-        index += 1
-    return tuple(sorted_list)
+    return [
+        (
+            l[i - 1]
+            if (i := len(l)) > 1 and i % 3 != 0
+            else min([x for x in l if x % 3 == 0])
+        )
+        for i in range(len(l))
+    ]
