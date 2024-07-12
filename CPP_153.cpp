839c25ee4152dac91310f978f67ce4c2c864a233
beep boop <githubbot@vadim.me>
2024-07-13 00:11:34 +0200

    Solution 22 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hpkkxb14 Prompt: 3b49f463-c042-4f88-8954-8eb4abc1ab42.cpp:8:92: error: could not convert ‘{{"Sp",  "671235"}, {"Bb"}}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector<st d::pair<std::__cxx11::basic_string<char>, std::__cxx11::basic_string<char> > >’     8 | air<std::string, std::string>> extensions = {{"Sp", "671235"}, {"Bb"}};       |                                                                      ^       |                                                                      |       |                                                                      <br ace-enclosed initializer list> In file included from 3b49f463-c042-4f88-8954-8eb4abc1ab42.cpp:33: 3b49f463-c042-4f88-8954-8eb4abc1ab42.cpp: In function ‘int main()’: 3b49f463-c042-4f88-8954-8eb4abc1ab42.cpp:35:33: error: invalid initialization of  reference of type ‘const std::vector<std::pair<std::__cxx11::basic_string<char> , std::__cxx11::basic_string<char> > >&’ from expression of type ‘const char [3] ’    35 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |                                 ^~~~ 3b49f463-c042-4f88-8954-8eb4abc1ab42.cpp:13:89: note: in passing argument 1 of ‘ std::string Strongest_Extension(const std::vector<std::pair<std::__cxx11::basic_ string<char>, std::__cxx11::basic_string<char> > >&)’    13 | ion(const std::vector<std::pair<std::string, std::string>>& extensions){       |     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,7 +1,5 @@
 int run() {
-    std::vector<std::pair<std::string, std::string>> extensions;
-    extensions.push_back({"Sp", "671235"}); 
-    extensions.push_back({"Bb", ""});
+    std::vector<std::pair<std::string, std::string>> extensions = {{"Sp", "671235"}, {"Bb", ""}}; 
     assert(Strongest_Extension(extensions) == "Sp.671235");
     return 0;
 }
