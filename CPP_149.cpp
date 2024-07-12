ce23436350c59dfd666f74bca0c1d0e6d2f7bf82
beep boop <githubbot@vadim.me>
2024-07-13 01:52:57 +0200

    Solution 74 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/y3nscp8u Prompt:    67 |     using string    = basic_string<char>;       |           ^~~~~~ a4240426-500a-429a-b113-17fc3cff5bd2.cpp:36:44: error: ‘b’ was not declared in t his scope    36 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a4240426-500a-429a-b113-17fc3cff5bd2.cpp:36:45: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a4240426-500a-429a-b113-17fc3cff5bd2.cpp:35: a4240426-500a-429a-b113-17fc3cff5bd2.cpp: In function ‘int main()’: a4240426-500a-429a-b113-17fc3cff5bd2.cpp:45:20: error: ‘sorted_list_sum’ was not  declared in this scope; did you mean ‘sortedListSum’?    45 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ a4240426-500a-429a-b113-17fc3cff5bd2.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,7 +1,5 @@
 #include <vector>
 #include <algorithm>
-#include <string>
-#include <iostream>
 
 bool issameHelper(const std::vector<std::string>& a, const std::vector<std::string>& b) {
     return a == b;
@@ -28,6 +26,6 @@ int mainFunction() {
     }
     std::cout << std::endl;
     if (!issameHelper(sortedListSum({{"aaaa", "bbbb", "dd", "cc"}}), {{"cc","dd","aaaa","bbbb"}}))
-        return 1;
+        return 1; 
     return 0;
 }
