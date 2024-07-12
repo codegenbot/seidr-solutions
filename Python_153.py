b21692c7e3fda2d5c3e6880a3e3cc7b8b4eeec81
beep boop <githubbot@vadim.me>
2024-07-12 20:34:01 +0200

    Solution 42 of Python/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/jxft3xng Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d234c7c7-3567-468f-9ef5-b75503269 a31.py", line 9, in <module>     check(Strongest_Extension)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d234c7c7-3567-468f-9ef5-b75503269 a31.py", line 6, in check     assert Strongest_Extension("Sp", ["671235", "Bb"]) == "Sp.671235"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_153.py
+++ Python_153.py
@@ -1,5 +1,2 @@
 def Strongest_Extension(extension, extensions_list):
-    if extension not in extensions_list:
-        return extension
-    extension_index = extensions_list.index(extension)
-    return f"{extension}.{extensions_list[extension_index-1]}"
+    return extension + '.' + max(extensions_list) if extension in extensions_list else extension
