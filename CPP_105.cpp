02784254f0e2282fdcfe20b914b746f251625aeb
beep boop <githubbot@vadim.me>
2024-07-12 16:39:02 +0200

    Solution 54 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q8x7adb5 Prompt:       |                                 ^~~~~~ In file included from ee0ef133-8b44-4a24-8cd9-d003f12524a5.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ ee0ef133-8b44-4a24-8cd9-d003f12524a5.cpp:59:44: error: ‘b’ was not declared in t his scope    59 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ee0ef133-8b44-4a24-8cd9-d003f12524a5.cpp:67:5: error: redefinition of ‘int main( )’    67 | int main(){       |     ^~~~ ee0ef133-8b44-4a24-8cd9-d003f12524a5.cpp:50:5: note: ‘int main()’ previously def ined here    50 | int main() {       |     ^~~~ In file included from ee0ef133-8b44-4a24-8cd9-d003f12524a5.cpp:58: ee0ef133-8b44-4a24-8cd9-d003f12524a5.cpp: In function ‘int main()’: ee0ef133-8b44-4a24-8cd9-d003f12524a5.cpp:68:20: error: ‘by_length’ was not decla red in this scope; did you mean ‘byLength’?    68 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,7 +1,9 @@
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+using namespace std;
+
+bool issame(vector<string> a, vector<string> b) {
     if (a == b)
         return true;
     for (int i = 0; i < a.size(); i++) {
@@ -11,10 +13,54 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return true;
 }
 
-std::vector<std::string> byLength(int num) {
-    std::vector<std::string> result;
+vector<string> byLength(int num) {
+    vector<string> result;
 
-    switch (num % 100) {
+    switch (num % 1000 / 100) {
+        case 1:
+            result.push_back("One");
+            break;
+        case 2:
+            result.push_back("Two");
+            break;
+        case 3:
+            result.push_back("Three");
+            break;
+        default:
+            result.push_back("Unknown");
+    }
+
+    switch (num % 100 / 10) {
+        case 1:
+            result.push_back("One");
+            break;
+        case 2:
+            result.push_back("Two");
+            break;
+        case 3:
+            result.push_back("Three");
+            break;
+        case 4:
+            result.push_back("Four");
+            break;
+        case 5:
+            result.push_back("Five");
+            break;
+        case 6:
+            result.push_back("Six");
+            break;
+        case 7:
+            result.push_back("Seven");
+            break;
+        case 8:
+            result.push_back("Eight");
+            break;
+        case 9:
+            result.push_back("Nine");
+            break;
+    }
+
+    switch (num % 10) {
         case 1:
             result.push_back("One");
             break;
@@ -49,10 +95,8 @@ std::vector<std::string> byLength(int num) {
 
 int main() {
     int num = 984;  
-    std::vector<std::string> result1 = byLength(num);
+    vector<string> result1 = byLength(num);
+    assert(issame(result1 , {"Nine", "Eight", "Four"}));
     
-    if (issame({"One", "Nine", "Four"}, result1))
-        return 0;
-    else
-        return 1;
+    return 0;
 }
