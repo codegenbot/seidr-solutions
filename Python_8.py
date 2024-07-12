883613e3b2aa99a887fc92d531847b9fbc33c180
beep boop <githubbot@vadim.me>
2024-07-12 06:53:40 +0200

    Solution 59 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2n6b2igf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c72f9b70-d772-47f3-a0a7-0586068c9 bfc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -1,23 +1,25 @@
+```
 def sum_product() -> tuple:
     total_sum = 0
     product = 1
+    count = 0
 
-    try:
-        while True:
-            user_input = input("Please enter a digit or type 'stop' to finish. ")
-            if str(user_input).lower() == "stop":
-                break
-            elif not user_input.isdigit():
-                print("Invalid input. Please enter a digit or type 'stop' to finish.")
-            else:
-                total_sum += int(user_input)
-                product *= int(user_input)
+    while True:
+        user_input = input("Please enter a digit or type 'stop' to finish. ")
+        
+        if str(user_input).lower() == "stop":
+            break
+        
+        elif not user_input.isdigit():
+            print("Invalid input. Please enter a digit or type 'stop' to finish.")
+        
+        else:
+            total_sum += int(user_input)
+            product *= int(user_input)
+            count += 1
 
-    except ValueError:
-        print("Invalid input. Please enter a digit or type 'stop' to finish.")
-
-    if total_sum > 0:
-        return total_sum, product, total_sum / product
+    if count > 0:
+        return total_sum, product, total_sum / count
     else:
         return total_sum, product, 0
 
