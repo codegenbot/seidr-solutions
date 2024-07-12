2026d89fdbd4e909772c03c8a002513de40d5609
beep boop <githubbot@vadim.me>
2024-07-13 00:32:05 +0200

    Solution 30 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/0x3ot70j Prompt: in this scope    33 |     string s;       |     ^~~~~~ 7b060b95-e8be-45a7-be45-081137a72c46.cpp:34:5: error: ‘cin’ was not declared in this scope    34 |     cin >> s;       |     ^~~ 7b060b95-e8be-45a7-be45-081137a72c46.cpp:34:12: error: ‘s’ was not declared in t his scope    34 |     cin >> s;       |            ^ 7b060b95-e8be-45a7-be45-081137a72c46.cpp:35:5: error: ‘cout’ was not declared in  this scope    35 |     cout << getBowlingScore(s) << endl;       |     ^~~~ 7b060b95-e8be-45a7-be45-081137a72c46.cpp:35:28: error: ‘getBowlingScore’ cannot be used as a function    35 |     cout << getBowlingScore(s) << endl;       |             ~~~~~~~~~~~~~~~^~~ 7b060b95-e8be-45a7-be45-081137a72c46.cpp:35:35: error: ‘endl’ was not declared i n this scope    35 |     cout << getBowlingScore(s) << endl;       |                                   ^~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,22 +1,40 @@
-int score(string s) {
-    int total = 0, frame = 0, i = 0;
-    while (frame < 10) {
+#include <iostream>
+using namespace std;
+
+int getBowlingScore(string s) {
+    int score = 0;
+    int frame = 0;
+    for (int i = 0; i < s.size() && frame < 10; i++) {
         if (s[i] == 'X') {
-            total += 10 + (s[i+2] == 'X' ? 10 : (s[i+2] == '/' ? 10 : s[i+2] - '0')) + (s[i+4] == 'X' ? 10 : (s[i+4] == '/' ? 10 : s[i+4] - '0'));
-            if (s[i+2] != 'X') {
-                i += 2;
+            score += 10;
+            if (s[i + 1] == 'X') {
+                score += 10;
+                if (s[i + 2] == 'X') {
+                    score += 10;
+                } else if (s[i + 2] == '/') {
+                    score += 10 - (s[i + 1] - '0');
+                } else {
+                    score += s[i + 2] - '0';
+                }
+            } else if (s[i + 2] == '/') {
+                score += 10;
+            } else {
+                score += s[i + 1] - '0' + s[i + 2] - '0';
             }
-            else {
-                i++;
-            }
-        } else if (s[i+1] == '/') {
-            total += 10 + (s[i+2] == 'X' ? 10 : s[i+2] - '0');
-            i += 3;
+            i++;
+        } else if (s[i] == '/') {
+            score += 10 - (s[i - 1] - '0');
         } else {
-            total += s[i] - '0' + s[i+1] - '0';
-            i += 2;
+            score += s[i] - '0';
         }
         frame++;
     }
-    return total;
+    return score;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << getBowlingScore(s) << endl;
+    return 0;
 }
