7f444e0ace49e3ed5f9f45d86e3b140529977c9e
beep boop <githubbot@vadim.me>
2024-07-12 17:07:18 +0200

    Solution 64 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q8x7adb5 Prompt:       |                              ^~~~~~ deb892a1-436b-45b0-98b9-a3741502402d.cpp:227:37: error: ‘string’ was not declare d in this scope   227 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ deb892a1-436b-45b0-98b9-a3741502402d.cpp:227:44: error: ‘b’ was not declared in this scope   227 | bool issame(vector<string> a,vector<string>b){       |                                            ^ deb892a1-436b-45b0-98b9-a3741502402d.cpp:227:45: error: expression list treated as compound expression in initializer [-fpermissive]   227 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from deb892a1-436b-45b0-98b9-a3741502402d.cpp:226: deb892a1-436b-45b0-98b9-a3741502402d.cpp: In function ‘int main()’: deb892a1-436b-45b0-98b9-a3741502402d.cpp:236:20: error: ‘by_length’ was not decl ared in this scope   236 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ deb892a1-436b-45b0-98b9-a3741502402d.cpp:236:19: error: ‘issame’ cannot be used as a function   236 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,102 +1,358 @@
-vector<string> by_length(int n) {
-    vector<string> result;
-    
-    if (n >= 1000) {
-        int q = n / 1000;
-        result.push_back(to_string(q));
-        result.push_back(" Thousand");
-        n %= 1000;
+```c++
+bool issame(vector<string> a,vector<string>b){
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]) return false;
     }
-    
-    if (n >= 90) {
-        if (n % 100 >= 10)
-            result.push_back(issame(n % 100, 10) ? " Ninety" : to_string(n % 100) + " Ten");
-        else
-            result.push_back(" Ninety");
-        n %= 90;
-    } else if (n >= 80) {
-        if (n % 100 >= 20)
-            result.push_back(issame(n % 100, 10) ? " Eighty" : to_string(n % 100) + " Ten");
-        else
-            result.push_back(" Eighty");
-        n %= 80;
-    } else if (n >= 70) {
-        if (n % 100 >= 30)
-            result.push_back(issame(n % 100, 20) ? " Seventy" : to_string(n % 100) + " Ten");
-        else
-            result.push_back(" Seventy");
-        n %= 70;
-    } else if (n >= 60) {
-        if (n % 100 >= 40)
-            result.push_back(issame(n % 100, 20) ? " Sixty" : to_string(n % 100) + " Ten");
-        else
-            result.push_back(" Sixty");
-        n %= 60;
-    } else if (n >= 50) {
-        if (n % 100 == 50)
-            result.push_back(" Fifty");
-        else
-            result.push_back(to_string(n % 50));
-        n %= 50;
-    } else if (n >= 40) {
-        if (n % 100 == 40)
-            result.push_back(" Forty");
-        else
-            result.push_back(to_string(n % 40) + " Ten");
-        n %= 40;
-    } else if (n >= 30) {
-        if (n % 100 == 30)
-            result.push_back(" Thirty");
-        else
-            result.push_back(to_string(n % 30));
-        n %= 30;
-    } else if (n >= 20) {
-        if (n % 100 >= 10)
-            result.push_back(issame(n % 100, 10) ? " Twenty" : to_string(n % 100) + " Ten");
-        else
-            result.push_back(" Twenty");
-        n %= 20;
+    return true;
+}
+
+vector<string> numberToWords(int num) {
+    vector<string> result;
+
+    if (num < 10) {
+        switch (num % 10) {
+            case 1: 
+                result.push_back("One");
+                break;
+            case 2: 
+                result.push_back("Two");
+                break;
+            case 3: 
+                result.push_back("Three");
+                break;
+            case 4: 
+                result.push_back("Four");
+                break;
+            case 5: 
+                result.push_back("Five");
+                break;
+            case 6: 
+                result.push_back("Six");
+                break;
+            case 7: 
+                result.push_back("Seven");
+                break;
+            case 8: 
+                result.push_back("Eight");
+                break;
+            case 9: 
+                result.push_back("Nine");
+                break;
+        }
+    } else if (num < 20) {
+        switch (num % 10) {
+            case 1: 
+                if (num % 100 / 10 == 1)
+                    result.push_back("Eleven");
+                else if (num % 100 / 10 == 2)
+                    result.push_back("Twelve");
+                else if (num % 100 / 10 == 3)
+                    result.push_back("Thirteen");
+                else if (num % 100 / 10 == 4)
+                    result.push_back("Fourteen");
+                else if (num % 100 / 10 == 5)
+                    result.push_back("Fifteen");
+                else if (num % 100 / 10 == 6)
+                    result.push_back("Sixteen");
+                else if (num % 100 / 10 == 7)
+                    result.push_back("Seventeen");
+                else if (num % 100 / 10 == 8)
+                    result.push_back("Eighteen");
+                else
+                    result.push_back("Nineteen");
+                break;
+            case 2: 
+                if (num % 100 / 10 == 1)
+                    result.push_back("Twenty-Two");
+                else if (num % 100 / 10 == 2)
+                    result.push_back("Twenty-Two");
+                else if (num % 100 / 10 == 3)
+                    result.push_back("Twenty-Three");
+                else if (num % 100 / 10 == 4)
+                    result.push_back("Twenty-Four");
+                else if (num % 100 / 10 == 5)
+                    result.push_back("Twenty-Five");
+                else if (num % 100 / 10 == 6)
+                    result.push_back("Twenty-Six");
+                else if (num % 100 / 10 == 7)
+                    result.push_back("Twenty-Seven");
+                else if (num % 100 / 10 == 8)
+                    result.push_back("Twenty-Eight");
+                else
+                    result.push_back("Twenty-Nine");
+                break;
+            case 3: 
+                if (num % 100 / 10 == 1)
+                    result.push_back("Thirty-One");
+                else if (num % 100 / 10 == 2)
+                    result.push_back("Thirty-Two");
+                else if (num % 100 / 10 == 3)
+                    result.push_back("Thirty-Three");
+                else if (num % 100 / 10 == 4)
+                    result.push_back("Thirty-Four");
+                else if (num % 100 / 10 == 5)
+                    result.push_back("Thirty-Five");
+                else if (num % 100 / 10 == 6)
+                    result.push_back("Thirty-Six");
+                else if (num % 100 / 10 == 7)
+                    result.push_back("Thirty-Seven");
+                else if (num % 100 / 10 == 8)
+                    result.push_back("Thirty-Eight");
+                else
+                    result.push_back("Thirty-Nine");
+                break;
+            case 4: 
+                if (num % 100 / 10 == 1)
+                    result.push_back("Forty-One");
+                else if (num % 100 / 10 == 2)
+                    result.push_back("Forty-Two");
+                else if (num % 100 / 10 == 3)
+                    result.push_back("Forty-Three");
+                else if (num % 100 / 10 == 4)
+                    result.push_back("Forty-Four");
+                else if (num % 100 / 10 == 5)
+                    result.push_back("Forty-Five");
+                else if (num % 100 / 10 == 6)
+                    result.push_back("Forty-Six");
+                else if (num % 100 / 10 == 7)
+                    result.push_back("Forty-Seven");
+                else if (num % 100 / 10 == 8)
+                    result.push_back("Forty-Eight");
+                else
+                    result.push_back("Forty-Nine");
+                break;
+            case 5: 
+                if (num % 100 / 10 == 1)
+                    result.push_back("Fifty-One");
+                else if (num % 100 / 10 == 2)
+                    result.push_back("Fifty-Two");
+                else if (num % 100 / 10 == 3)
+                    result.push_back("Fifty-Three");
+                else if (num % 100 / 10 == 4)
+                    result.push_back("Fifty-Four");
+                else if (num % 100 / 10 == 5)
+                    result.push_back("Fifty-Five");
+                else if (num % 100 / 10 == 6)
+                    result.push_back("Fifty-Six");
+                else if (num % 100 / 10 == 7)
+                    result.push_back("Fifty-Seven");
+                else if (num % 100 / 10 == 8)
+                    result.push_back("Fifty-Eight");
+                else
+                    result.push_back("Fifty-Nine");
+                break;
+            case 6: 
+                if (num % 100 / 10 == 1)
+                    result.push_back("Sixty-One");
+                else if (num % 100 / 10 == 2)
+                    result.push_back("Sixty-Two");
+                else if (num % 100 / 10 == 3)
+                    result.push_back("Sixty-Three");
+                else if (num % 100 / 10 == 4)
+                    result.push_back("Sixty-Four");
+                else if (num % 100 / 10 == 5)
+                    result.push_back("Sixty-Five");
+                else if (num % 100 / 10 == 6)
+                    result.push_back("Sixty-Six");
+                else if (num % 100 / 10 == 7)
+                    result.push_back("Sixty-Seven");
+                else if (num % 100 / 10 == 8)
+                    result.push_back("Sixty-Eight");
+                else
+                    result.push_back("Sixty-Nine");
+                break;
+            case 7: 
+                if (num % 100 / 10 == 1)
+                    result.push_back("Seventy-One");
+                else if (num % 100 / 10 == 2)
+                    result.push_back("Seventy-Two");
+                else if (num % 100 / 10 == 3)
+                    result.push_back("Seventy-Three");
+                else if (num % 100 / 10 == 4)
+                    result.push_back("Seventy-Four");
+                else if (num % 100 / 10 == 5)
+                    result.push_back("Seventy-Five");
+                else if (num % 100 / 10 == 6)
+                    result.push_back("Seventy-Six");
+                else if (num % 100 / 10 == 7)
+                    result.push_back("Seventy-Seven");
+                else if (num % 100 / 10 == 8)
+                    result.push_back("Seventy-Eight");
+                else
+                    result.push_back("Seventy-Nine");
+                break;
+            case 8: 
+                if (num % 100 / 10 == 1)
+                    result.push_back("Eighty-One");
+                else if (num % 100 / 10 == 2)
+                    result.push_back("Eighty-Two");
+                else if (num % 100 / 10 == 3)
+                    result.push_back("Eighty-Three");
+                else if (num % 100 / 10 == 4)
+                    result.push_back("Eighty-Four");
+                else if (num % 100 / 10 == 5)
+                    result.push_back("Eighty-Five");
+                else if (num % 100 / 10 == 6)
+                    result.push_back("Eighty-Six");
+                else if (num % 100 / 10 == 7)
+                    result.push_back("Eighty-Seven");
+                else if (num % 100 / 10 == 8)
+                    result.push_back("Eighty-Eight");
+                else
+                    result.push_back("Eighty-Nine");
+                break;
+            case 9: 
+                if (num % 100 / 10 == 1)
+                    result.push_back("Ninety-One");
+                else if (num % 100 / 10 == 2)
+                    result.push_back("Ninety-Two");
+                else if (num % 100 / 10 == 3)
+                    result.push_back("Ninety-Three");
+                else if (num % 100 / 10 == 4)
+                    result.push_back("Ninety-Four");
+                else if (num % 100 / 10 == 5)
+                    result.push_back("Ninety-Five");
+                else if (num % 100 / 10 == 6)
+                    result.push_back("Ninety-Six");
+                else if (num % 100 / 10 == 7)
+                    result.push_back("Ninety-Seven");
+                else if (num % 100 / 10 == 8)
+                    result.push_back("Ninety-Eight");
+                else
+                    result.push_back("Ninety-Nine");
+                break;
+        }
     } else {
-        if (n == 19)
-            result.push_back(" Nineteen");
-        else if (n >= 10) {
-            result.push_back(to_string(n));
-        } else {
-            switch (n) {
-                case 1:
-                    result.push_back(" One");
+        int hundreds = num / 100;
+        int remainder = num % 100;
+
+        if (hundreds > 1) {
+            result.push_back(std::to_string(hundreds) + " Hundred");
+        } else if (hundreds == 1) {
+            result.push_back("One Hundred");
+        }
+
+        if (remainder > 0) {
+            if (result.size() > 1) {
+                result.push_back("and");
+            }
+            switch (remainder) {
+                case 10: 
+                    result.push_back("Ten");
                     break;
-                case 2:
-                    result.push_back(" Two");
+                case 11: 
+                    result.push_back("Eleven");
                     break;
-                case 3:
-                    result.push_back(" Three");
+                case 12: 
+                    result.push_back("Twelve");
                     break;
-                case 4:
-                    result.push_back(" Four");
+                case 13: 
+                    result.push_back("Thirteen");
                     break;
-                case 5:
-                    result.push_back(" Five");
+                case 14: 
+                    result.push_back("Fourteen");
                     break;
-                case 6:
-                    result.push_back(" Six");
+                case 15: 
+                    result.push_back("Fifteen");
                     break;
-                case 7:
-                    result.push_back(" Seven");
+                case 16: 
+                    result.push_back("Sixteen");
                     break;
-                case 8:
-                    result.push_back(" Eight");
+                case 17: 
+                    result.push_back("Seventeen");
                     break;
-                case 9:
-                    result.push_back(" Nine");
+                case 18: 
+                    result.push_back("Eighteen");
                     break;
+                case 19: 
+                    result.push_back("Nineteen");
+                    break;
+                default:
+                    switch (remainder % 10) {
+                        case 2: 
+                            result.push_back("Twenty");
+                            break;
+                        case 3: 
+                            result.push_back("Thirty");
+                            break;
+                        case 4: 
+                            result.push_back("Forty");
+                            break;
+                        case 5: 
+                            result.push_back("Fifty");
+                            break;
+                        case 6: 
+                            result.push_back("Sixty");
+                            break;
+                        case 7: 
+                            result.push_back("Seventy");
+                            break;
+                        case 8: 
+                            result.push_back("Eighty");
+                            break;
+                        case 9: 
+                            result.push_back("Ninety");
+                            break;
+                    }
+                    if (remainder % 10 > 0) {
+                        switch (remainder % 10) {
+                            case 1:
+                                result.push_back("One");
+                                break;
+                            case 2:
+                                result.push_back("Two");
+                                break;
+                            case 3:
+                                result.push_back("Three");
+                                break;
+                            case 4:
+                                result.push_back("Four");
+                                break;
+                            case 5:
+                                result.push_back("Five");
+                                break;
+                            case 6:
+                                result.push_back("Six");
+                                break;
+                            case 7:
+                                result.push_back("Seven");
+                                break;
+                            case 8:
+                                result.push_back("Eight");
+                                break;
+                            case 9:
+                                result.push_back("Nine");
+                                break;
+                        }
+                    }
             }
         }
     }
-    
-    return result;
+
+    std::string result_str = "";
+    for (const auto& word : result) {
+        if (!word.empty()) {
+            result_str += word;
+            if (result.size() > 1 && !result.back().empty()) {
+                result_str += " ";
+            }
+        }
+    }
+
+    return result_str;
 }
 
-bool issame(int n, int base) {
-    return n % base == 0;
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    int num = 12345;
+    std::cout << number_to_words(num) << std::endl;
+
+    num = 9999;
+    std::cout << number_to_words(num) << std::endl;
+
+    return 0;
 }
