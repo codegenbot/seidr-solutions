5f2a6a912e99359c54ddfc580e427d8689d35523
beep boop <githubbot@vadim.me>
2024-07-12 10:20:12 +0200

    Solution 48 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/r64wtf8v Prompt: c084b340-01f1-4717-9941-d3972bb6ae27.cpp:28:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    28 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ c084b340-01f1-4717-9941-d3972bb6ae27.cpp:23:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    23 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,25 +1,3 @@
-#include <vector>
-#include <string>
-using namespace std;
-
-vector<int> parse_music(string music_string) {
-    vector<int> beats;
-    int i = 0;
-    while (i < music_string.size()) {
-        if (music_string[i] == 'o') {
-            beats.push_back(4);
-            i++;
-        } else if (music_string.substr(i, 2) == "o|") {
-            beats.push_back(2);
-            i += 2;
-        } else if (music_string.substr(i, 2) == ".|") {
-            beats.push_back(1);
-            i += 2;
-        }
-    }
-    return beats;
-}
-
-bool check_beats(vector<int> parsed_beats, vector<int> target_beats) {
-    return parsed_beats == target_beats;
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
