d3a3367a36e5d9143dea6866033028ceb69a0046
beep boop <githubbot@vadim.me>
2024-07-13 00:54:20 +0200

    Solution 24 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lpo8cfzh Prompt:     9 | "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};       |                                                             ^~~~ f2423358-4761-4b90-b537-7eea55ba1918.cpp:9:86: warning: ISO C++ forbids converti ng a string constant to ‘char*’ [-Wwrite-strings]     9 | "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};       |                                                                   ^~~ f2423358-4761-4b90-b537-7eea55ba1918.cpp: At global scope: f2423358-4761-4b90-b537-7eea55ba1918.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ f2423358-4761-4b90-b537-7eea55ba1918.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from f2423358-4761-4b90-b537-7eea55ba1918.cpp:26: f2423358-4761-4b90-b537-7eea55ba1918.cpp: In function ‘int main()’: f2423358-4761-4b90-b537-7eea55ba1918.cpp:28:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘intToMiniRoman’?    28 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -5,13 +5,13 @@ std::string intToMiniRoman(int n) {
     if (n <= 0)
         return "0";
     std::string roman = "";
-    const char* val[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    int num[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    const char* sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     int i = 0;
     while (n > 0) {
-        if (n >= *std::make_heap(num + i, num + i + 1)) {
-            n -= *std::make_heap(num + i, num + i + 1);
-            roman += *(val + i);
+        if (n >= val[i]) {
+            n -= val[i];
+            roman += sym[i];
         } else
             i++;
     }
