95567d1b513853b569873541d1970b34944d2619
beep boop <githubbot@vadim.me>
2024-07-12 08:20:29 +0200

    Solution 76 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ppkt8fr3 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05bf6b4f-1b88-4e1d-a234-654ec88cc 494.py", line 21, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05bf6b4f-1b88-4e1d-a234-654ec88cc 494.py", line 17, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -4,9 +4,8 @@ def encode(message):
     for char in message:
         if not char.isalpha():
             result += char
-        elif char.lower() not in vowels:  
-            index = (ord(char.lower()) - ord('a')) % 26
-            result += chr((index + 1) % 26 + ord('a'))
+        elif char.lower() in vowels:  
+            result += chr((ord(char.lower()) - ord('a') + 1) % 26 + ord('a'))
         else:
             result += char.upper()
     return result
