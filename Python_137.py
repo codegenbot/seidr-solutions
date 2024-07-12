e214fe319f882081e7ab895539c0d041c75ffc90
beep boop <githubbot@vadim.me>
2024-07-12 08:26:25 +0200

    Solution 6 of Python/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ci2gu070 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2335819d-63e5-49b4-8fa5-6f2c60212 6d2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_137.py
+++ Python_137.py
@@ -1,18 +1,12 @@
 ```
 def compare_one(a, b):
     if isinstance(a, str) and isinstance(b, (int, float)):
-        a = float(a.replace(',', '.')) 
-        return a if a > b else None
+        return a if float(a.replace(',', '.')) > b else None
     elif isinstance(b, str) and isinstance(a, (int, float)):
-        b = float(b.replace(',', '.')) 
-        return b if b > a else None
+        return b if float(b.replace(',', '.')) > a else None
     elif isinstance(a, str) and isinstance(b, str):
-        a = float(a.replace(',', '.')) 
-        b = float(b.replace(',', '.')) 
-        return a if a > b else None
-    elif a > b:
-        return a
-    elif b > a:
-        return b
+        return a if float(a.replace(',', '.')) > float(b.replace(',', '.')) else None
+    elif isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a if a > b else b
     else:
         return None
