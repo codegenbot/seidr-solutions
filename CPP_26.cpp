1ab787b0874c075944b404360b2913463eaf6fbc
beep boop <githubbot@vadim.me>
2024-07-12 19:36:38 +0200

    Solution 44 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/glxnmwbk Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from ab1a252a-9f1b-4e6a-9bac-d1b16da3f388.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -17,7 +17,7 @@ std::vector<int> remove_duplicates(const std::vector<int>& vec) {
 }
 
 int main() {
-    std::vector<int> test1 = remove_duplicates({1, 2, 3, 2, 4, 3, 5});
+    std::vector<int> test1 = {1, 2, 3, 2, 4, 3, 5};
     std::vector<int> test2 = {1, 2, 3, 4, 5};
     if (issame(test1, test2)) {
         for (int i : test1) {
