62f05b1f207ad2c5811a99b575b9fe08f4745726
beep boop <githubbot@vadim.me>
2024-07-12 23:52:04 +0200

    Solution 16 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/y59cn2gx Prompt:    29 |     string input;       |     ^~~~~~ b69fa3c8-5eeb-456d-bd85-58be062ae4eb.cpp:30:5: error: ‘cin’ was not declared in this scope    30 |     cin >> input;       |     ^~~ b69fa3c8-5eeb-456d-bd85-58be062ae4eb.cpp:30:12: error: ‘input’ was not declared in this scope; did you mean ‘int’?    30 |     cin >> input;       |            ^~~~~       |            int b69fa3c8-5eeb-456d-bd85-58be062ae4eb.cpp:31:5: error: ‘cout’ was not declared in  this scope    31 |     cout << calculateScore(input) << endl;       |     ^~~~ b69fa3c8-5eeb-456d-bd85-58be062ae4eb.cpp:31:27: error: ‘calculateScore’ cannot b e used as a function    31 |     cout << calculateScore(input) << endl;       |             ~~~~~~~~~~~~~~^~~~~~~ b69fa3c8-5eeb-456d-bd85-58be062ae4eb.cpp:31:38: error: ‘endl’ was not declared i n this scope    31 |     cout << calculateScore(input) << endl;       |                                      ^~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,39 +1,36 @@
-int getScore(string input) {
+#include <iostream>
+using namespace std;
+
+int calculateScore(string input) {
     int score = 0;
     int frame = 0;
-    int rolls[22] = {0};
-
-    for(char c : input) {
-        if(c == 'X') {
-            rolls[frame++] = 10;
-        } else if(c == '/') {
-            rolls[frame-1] = 10 - rolls[frame-1];
-        } else if(c == '-') {
-            rolls[frame++] = 0;
-        } else {
-            rolls[frame++] = c - '0';
-        }
-    }
-
-    for(int i = 0; i < 10; ++i) {
-        if(rolls[i*2] == 10) {
-            score += 10 + rolls[i*2+2] + rolls[i*2+3];
-            if(rolls[i*2+2] == 10) {
-                score += rolls[i*2+4];
+    for (int i = 0; i < input.size(); ++i) {
+        if (input[i] == 'X') {
+            score += 10;
+            if (input[i + 2] == 'X') {
+                score += 10 + (input[i + 4] == 'X' ? 10 : input[i + 4] - '0');
+            } else {
+                score += (input[i + 2] == '/' ? 10 : input[i + 2] - '0') + (input[i + 3] == '/' ? 10 : input[i + 3] - '0');
             }
-        } else if(rolls[i*2] + rolls[i*2+1] == 10) {
-            score += 10 + rolls[i*2+2];
+            frame++;
+        } else if (input[i] == '/') {
+            score += 10 - (input[i - 1] - '0');
+        } else if (input[i] == '-') {
+            // do nothing
         } else {
-            score += rolls[i*2] + rolls[i*2+1];
+            score += input[i] - '0';
+            if (input[i + 1] == '/') {
+                score += 10 - (input[i] - '0');
+            }
         }
+        if (frame == 10) break;
     }
-
     return score;
 }
 
 int main() {
     string input;
     cin >> input;
-    cout << getScore(input) << endl;
+    cout << calculateScore(input) << " " << endl;
     return 0;
 }
