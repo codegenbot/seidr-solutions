ba766bb96b919c8100bb4d20afba31a905690c37
beep boop <githubbot@vadim.me>
2024-07-12 08:35:26 +0200

    Solution 9 of Python/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/53qrk9fm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/156b89ef-e9aa-4c37-8465-b7d411fed 899.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_128.py
+++ Python_128.py
@@ -3,12 +3,16 @@ def calculate_average_and_median():
         user_input = input("Enter numbers separated by space: ")
         try:
             numbers = list(map(int, user_input.split()))
-            if len(numbers) > 0:
-                average = sum(numbers) / len(numbers)
-                median = sorted(numbers)[len(numbers) // 2]
-                print(f"Average: {average}, Median: {median}")
             break
         except ValueError:
             print("Invalid input. Please enter valid numbers separated by space.")
+
+    if not numbers:
+        return None
+
+    average = sum(numbers) / len(numbers)
+    median = sorted(numbers)[len(numbers) // 2]
+
+    print(f"Average: {average}, Median: {median}")
 
 calculate_average_and_median()
