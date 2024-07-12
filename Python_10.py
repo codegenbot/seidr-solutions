8270227841931ff83acf51dcc9af8407c8837ea1
beep boop <githubbot@vadim.me>
2024-07-12 16:29:50 +0200

    Solution 70 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/y9kfabj7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ec769d1b-485e-43c4-979d-6f0185d24 156.py", line 16, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ec769d1b-485e-43c4-979d-6f0185d24 156.py", line 13, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -3,13 +3,8 @@ def is_palindrome(string: str) -> bool:
 
 
 def make_palindrome(string: str) -> str:
-    if is_palindrome(string):
-        return string
-    for i in range(len(string)):
-        half = string[: i + 1]
-        second_half = string[i:]
-        rev_second_half = second_half[::-1]
-        if half + rev_second_half == half + rev_second_half[: len(half)] + (
-            second_half[len(half)] if len(half) < len(second_half) else ""
-        ):
-            return half + rev_second_half
+    for char in range(len(string)):
+        half = string[: char + 1]
+        second_half = string[char:][::-1]
+        if half + second_half == (half + second_half)[::-1]:
+            return half + second_half
