9c15f1ba41a16459718f71f6ba39c48fb1dc324f
beep boop <githubbot@vadim.me>
2024-07-12 08:24:48 +0200

    Solution 21 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/p0oo0rpk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/886fbafe-4906-4f6b-9fc4-33fe5481f 95d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -1,11 +1,2 @@
-```
-def check(result):
-    if result % 3 == 0:
-        print("The minimum length is divisible by 3")
-    else:
-        print("The minimum length is not divisible by 3")
-
 def min_length(all_paths):
-    return min(sum(map(int, ''.join(path))) for path in all_paths)
-
-check(min_length(['123', '456', '789']))
+    return min(sum(int(path) for path in all_paths))
