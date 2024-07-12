63dbf11617e87921a77c37c329b6bed596c98750
beep boop <githubbot@vadim.me>
2024-07-12 13:29:09 +0200

    Solution 55 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/9n0goacs Prompt: Complete the following code given the task description and function signature.

--- Python_10.py
+++ Python_10.py
@@ -6,11 +6,12 @@ def is_palindrome(string: str) -> bool:
 def make_palindrome(string: str) -> str:
     if string.islower():
         i = len(string) - 1
-        while i > 0 and string[i] == string[0]:
+        while i >= 0 and string[i] == string[0]:
             i -= 1
-        return string + string[i-1::-1]
+        return string + string[:i][::-1]
     else:
-        for i in range(len(string)):
-            if string[i].isalpha() and string[i].lower() != string[0].lower():
-                return string[:i].lower() + string[:i][::-1] + string[i:].lower() + string[i:][::-1]
-        return string + string[::-1]
+        s = ''.join(c for c in string if c.isalnum())
+        i = len(s) - 1
+        while i >= 0 and s[i].lower() == s[0].lower():
+            i -= 1
+        return s + s[:i][::-1] + ''.join(c for c in string if not c.isalnum())
