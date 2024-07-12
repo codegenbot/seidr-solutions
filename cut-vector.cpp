95f0584bdf767e7590bb582b975e5dbb65fc7df0
beep boop <githubbot@vadim.me>
2024-07-12 14:00:10 +0200

    Solution 9 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/f2mx6vts Prompt: 1777ac76-b398-47f7-b8b2-baa05b697e97.cpp: In function ‘int main()’: 1777ac76-b398-47f7-b8b2-baa05b697e97.cpp:30:5: error: ‘cin’ was not declared in this scope    30 |     cin >> n;       |     ^~~ 1777ac76-b398-47f7-b8b2-baa05b697e97.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; 1777ac76-b398-47f7-b8b2-baa05b697e97.cpp:36:5: error: ‘cout’ was not declared in  this scope    36 |     cout << "[";       |     ^~~~ 1777ac76-b398-47f7-b8b2-baa05b697e97.cpp:36:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’?

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -7,8 +7,8 @@ vector<vector<int>> cutVector(vector<int>& vec) {
     int n = vec.size();
     vector<vector<int>> res(2);
     
-    for (int i = 1; i < n; i++) {
-        if (vec[i] - vec[0] == vec[i-1] - vec[0]) {
+    for (int i = 1; i <= n/2; i++) {
+        if (vec[i] - vec[0] == vec[n-i-1] - vec[0]) {
             res[0] = vector<int>(vec.begin(), vec.begin() + i);
             res[1] = vector<int>(vec.begin() + i, vec.end());
             return res;
