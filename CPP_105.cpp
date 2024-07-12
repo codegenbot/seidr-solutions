d26ece869e218006a66fd3d33349851acde64a80
beep boop <githubbot@vadim.me>
2024-07-12 21:02:35 +0200

    Solution 7 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2azl5hhy Prompt:       |                              ^~~~~~ f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp:4:37: error: ‘string’ was not declared in this scope     4 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp:4:44: error: ‘b’ was not declared in th is scope     4 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp:4:45: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp:3: f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp: In function ‘int main()’: f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp:13:20: error: ‘by_length’ was not decla red in this scope    13 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ f5572c46-f906-4fd1-bf20-36cbe1b4c4ea.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,4 +1,19 @@
-int main {
-    assert (issame(by_length({9, 4, 8}) , {"Nine", "Four", "Eight"}));
+#include <vector>
+#include <string>
+
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i].size() != b[i].size()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    assert(issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));
     return 0;
 }
