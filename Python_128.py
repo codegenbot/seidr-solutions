bc9f691c00f257f7cc311d0216018a30d245d0c9
beep boop <githubbot@vadim.me>
2024-07-12 11:10:08 +0200

    Solution 99 of Python/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/53qrk9fm Prompt: Average: 65.0, Median: 90 70 50 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c5443152-c27b-4395-9bf3-83cbdd743 c57.py", line 15, in <module>     check(prod_signs)           ^^^^^^^^^^ NameError: name 'prod_signs' is not defined

--- Python_128.py
+++ Python_128.py
@@ -1,8 +1,8 @@
 def solve(grades):
     averages = [sum(student) / len(student) for student in grades]
-    medians = [' '.join(map(str, sorted(student)[len(student)//2-1:len(student)//2+1:1])) if len(student) % 2 else ' '.join(map(str, sorted(student)[len(student)//2:]))
+    medians = [' '.join(map(str, sorted(student)[len(student)//2-1:len(student)//2+1])) if len(student) % 2 else ' '.join(map(str, sorted(student)[len(student)//2:]))
                 for student in grades]
-    return f"Average: {sum(averages) / len(averages)}, Median: {' '.join(medians)}"
+    return f"Average: {sum(averages) / len(grades)}, Median: {' '.join(medians)}"
 
 grades = [[90, 80], [70, 60], [50, 40]]
 print(solve(grades))
