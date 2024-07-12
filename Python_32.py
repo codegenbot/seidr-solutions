448ef6ea0e2793003acad0f9b80bef09fee96a80
beep boop <githubbot@vadim.me>
2024-07-12 13:59:11 +0200

    Solution 61 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/jizgtxc3 Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -1,6 +1,8 @@
 def find_zero(xs: list):
     if len(xs) < 2 or xs[-1] == 0:
-        raise ValueError("xs must have at least two coefficients and the last one should be non-zero")
+        raise ValueError(
+            "xs must have at least two coefficients and the last one should be non-zero"
+        )
     if len(xs) % 2 != 0:
         raise ValueError("xs must have an even number of coefficients")
     a = xs[-1]
