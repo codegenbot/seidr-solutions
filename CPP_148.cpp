ead0b5aaf0b6e92e9aeb8ac7225048ff16d01cbd
beep boop <githubbot@vadim.me>
2024-07-12 06:13:43 +0200

    Solution 17 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/594pvkk7 Prompt: In file included from /usr/include/c++/11/string:39,                  from b98fed49-82ed-41d9-8bd4-567672b9394f.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from b98fed49-82ed-41d9-8bd4-567672b9394f.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ b98fed49-82ed-41d9-8bd4-567672b9394f.cpp:36:44: error: ‘b’ was not declared in t his scope    36 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from b98fed49-82ed-41d9-8bd4-567672b9394f.cpp:35: b98fed49-82ed-41d9-8bd4-567672b9394f.cpp: In function ‘int main()’: b98fed49-82ed-41d9-8bd4-567672b9394f.cpp:45:20: error: ‘bf’ was not declared in this scope    45 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ b98fed49-82ed-41d9-8bd4-567672b9394f.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -5,5 +5,13 @@
 #include <cassert>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
