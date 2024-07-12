5e61b98bbbdeb1983ba58d1e05d9883fc45d43f1
beep boop <githubbot@vadim.me>
2024-07-12 20:28:42 +0200

    Solution 98 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/894y32ic Prompt:       |                         ^~~~~~ 298bb694-92ab-4ee5-868f-fcb549f7b296.cpp:1:44: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name, vector<string> extensions) {       |                                            ^~~~~~ 298bb694-92ab-4ee5-868f-fcb549f7b296.cpp:1:51: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name, vector<string> extensions) {       |                                                   ^~~~~~ 298bb694-92ab-4ee5-868f-fcb549f7b296.cpp:1:59: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name, vector<string> extensions) {       |                                                           ^~~~~~~~~~ 298bb694-92ab-4ee5-868f-fcb549f7b296.cpp:1:69: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name, vector<string> extensions) {       |                                                                     ^ In file included from 298bb694-92ab-4ee5-868f-fcb549f7b296.cpp:24: 298bb694-92ab-4ee5-868f-fcb549f7b296.cpp: In function ‘int main()’: 298bb694-92ab-4ee5-868f-fcb549f7b296.cpp:26:32: error: ‘Strongest_Extension’ can not be used as a function    26 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,43 +1,47 @@
+```cpp
 #include <string>
 #include <vector>
 using namespace std;
 
 int main() {
-    int Strongest_Extension(string class_name, vector<string> extensions) {
-        int strongest = 0;
-        string result = "";
-        
-        for (string extension : extensions) {
-            int cap = 0, sm = 0;
-            
-            for (char c : extension) {
-                if (isupper(c)) cap++;
-                else if (islower(c)) sm++;
-            }
-            
-            int strength = cap - sm;
-            
-            if (strength > strongest) {
-                strongest = strength;
-                result = extension;
-            } 
-        }
-        
-        return class_name + "." + result;
+    string class_name;
+    vector<string> extensions;
+
+    cout << "Enter the class name: ";
+    cin >> class_name;
+
+    cout << "Enter the list of extensions (comma separated): ";
+    string input;
+    getline(cin, input);
+    size_t pos = 0;
+    while ((pos = input.find(',')) != string::npos) {
+        extensions.push_back(input.substr(0, pos));
+        input.erase(0, pos + 1);
+    }
+    if (!input.empty()) {
+        extensions.push_back(input);
     }
 
-    string className, extension;
-    cout << "Enter the class name: ";
-    cin >> className;
-    cout << "Enter the extensions: ";
-    getline(cin, extension);
-    vector<string> extensions;
-    while (getline(cin, extension)) {
-        extensions.push_back(extension);
+    int strongest = 0;
+    string result = "";
+
+    for (string extension : extensions) {
+        int cap = 0, sm = 0;
+
+        for (char c : extension) {
+            if (isupper(c)) cap++;
+            else if (islower(c)) sm++;
+        }
+
+        int strength = cap - sm;
+
+        if (strength > strongest) {
+            strongest = strength;
+            result = extension;
+        }
     }
-    
-    int strongest = Strongest_Extension(className, extensions);
-    cout << "Strongest Extension is: " << strongest << endl;
+
+    cout << "Strongest Extension: " << class_name + "." + result << endl;
 
     return 0;
 }
