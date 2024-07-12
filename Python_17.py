802a890e224c62c1e20518de02668ecc5b263e6c
beep boop <githubbot@vadim.me>
2024-07-12 06:43:53 +0200

    Solution 50 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/i58amgxb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb6188b8-6b9c-4ca2-bdec-ab26b31db cc5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,4 +1,3 @@
-```
 def parse_music(music_string: str) -> list:
     if not isinstance(music_string, str):
         return []
@@ -10,24 +9,27 @@ def parse_music(music_string: str) -> list:
         if not music_string[i : i + 1].isalnum():
             return []
 
-        if music_string[i:i+2] == "o|":
+        if music_string[i : i + 2] == "o|":
             music_list.append(2)
             i += 2
-        elif music_string[i:i+3] == "| .|":
+        elif music_string[i : i + 3] == "| .|":
             music_list.append(1)
             i += 3
-        elif music_string[i:i+5] == "..|.||.|":
+        elif music_string[i : i + 5] == "..|.||.|":
             music_list.append(0.5)
             i += 5
-        elif music_string[i:i+2] == "o o":
-            if not music_string[i+1].isalnum() or not music_string[i+1:i+2] == "|":
+        elif music_string[i : i + 2] == "o o":
+            if (
+                not music_string[i + 1].isalnum()
+                or not music_string[i + 1 : i + 2] == "|"
+            ):
                 return []
             music_list.append(4)
             i += 2
-        elif music_string[i:i+3] == "o o|":
+        elif music_string[i : i + 3] == "o o|":
             music_list.append(4)
             i += 3
-        elif music_string[i:i+2] == "o o|":
+        elif music_string[i : i + 2] == "o o|":
             music_list.append(4)
             i += 2
 
