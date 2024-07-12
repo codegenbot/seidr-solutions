7617fa719a5f09fba2749cc715f77afa13fee9d7
beep boop <githubbot@vadim.me>
2024-07-12 23:29:24 +0200

    Solution 54 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1oal4586 Prompt:       |             ^~~~~~ bdc3bb5c-e368-4b23-abe3-1d1d8e0a70c4.cpp:34:34: error: expected primary-expressi on before ‘int’    34 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bdc3bb5c-e368-4b23-abe3-1d1d8e0a70c4.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ bdc3bb5c-e368-4b23-abe3-1d1d8e0a70c4.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from bdc3bb5c-e368-4b23-abe3-1d1d8e0a70c4.cpp:33: bdc3bb5c-e368-4b23-abe3-1d1d8e0a70c4.cpp: In function ‘int main()’: bdc3bb5c-e368-4b23-abe3-1d1d8e0a70c4.cpp:43:27: error: invalid initialization of  reference of type ‘const std::vector<int>&’ from expression of type ‘int’    43 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ bdc3bb5c-e368-4b23-abe3-1d1d8e0a70c4.cpp:4:37: note: in passing argument 1 of ‘b ool issame(const std::vector<int>&, const std::vector<int>&)’     4 | bool issame(const std::vector<int>& a, const std::vector<int>& b) {       |             ~~~~~~~~~~~~~~~~~~~~~~~~^

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    return a == b;
+}
+
+int maximum(const std::vector<int>& vec, int start = 0) {
+    if (vec.empty()) 
+        return -1;
+    else
+        return *std::max_element(vec.begin() + start, vec.end());
+}
 
 int main() {
     std::vector<int> vec1; 
@@ -10,10 +20,10 @@ int main() {
     std::vector<int> vec2(3); 
     std::iota(vec2.begin(), vec2.end(), 1);
     
-    if (issame(vec1, vec2)) { // Compare vectors
+    if (issame({vec1}, {vec2})) { // Compare vectors
         std::cout << "Vectors are same" << std::endl;
     } else {
-        int max = *std::max_element({1, 2, 3, -23, 243, -400, 0});
+        int max = maximum(vec1);
         std::cout << "Maximum element in the vector is: " << max << std::endl;
     }
     
