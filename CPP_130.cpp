ca798c1fb4586a462219f31996f484f8dd6d9fea
beep boop <githubbot@vadim.me>
2024-07-12 22:25:00 +0200

    Solution 2 of CPP/130, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/83zmhthr Prompt: Program failed

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <vector>
 using namespace std;
 
@@ -11,7 +10,7 @@ vector<int> tri(int n) {
             if (i % 2 == 0)
                 val = 1 + i / 2;
             else
-                val = result[i - 1] + result[i - 2] + result[i];
+                val = 3 * result[i - 1];
             result.push_back(val);
         }
     }
