c8f1f27bc369c526163ce6eadead198563e9f418
beep boop <githubbot@vadim.me>
2024-07-13 02:58:42 +0200

    Solution 2 of CPP/86, ratio 0.7142857142857143 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/o32cha48 Prompt: Complete the following code given the task description and function signature.

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,24 +1,28 @@
-string anti_shuffle(string s){
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+string anti_shuffle(string s) {
     string result = "";
-    size_t pos = 0;
-    while((pos = s.find(' ')) != string::npos){
-        string word = s.substr(0, pos);
-        string new_word = "";
-        for(char c : word) {
-            new_word += (char)tolower(c);
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') {
+            result += " ";
+        } else {
+            string word;
+            while (i < s.length() && s[i] != ' ') {
+                word += s[i];
+                ++i;
+            }
+            for (int j = 0; j < word.length(); j++) {
+                int k = 0;
+                while (k < word.length()) {
+                    if (word[k] > word[j]) break;
+                    k++;
+                }
+                result += word[j];
+            }
         }
-        result += new_word + " ";
-        s.erase(0, pos+1);
-    }
-    if(s.length() > 0) {
-        string last_word = s;
-        string new_last_word = "";
-        for(char c : last_word) {
-            new_last_word += (char)tolower(c);
-        }
-        result += new_last_word;
-    } else {
-        result = s;
     }
     return result;
 }
