7a9b4743c4313f81145320fdea5aabb1c1b8453a
beep boop <githubbot@vadim.me>
2024-07-14 01:34:53 +0200

    Solution 12 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ric6vnkl Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -3,35 +3,30 @@ Here is the solution:
 #include <iostream>
 #include <string>
 
-std::string spinWords(std::string sentence) {
+std::string spinWords(std::string str) {
     std::string result = "";
-    std::string word;
-    
-    for (char c : sentence) {
-        if (c == ' ') {
-            if (word.length() >= 5) {
-                std::reverse(word.begin(), word.end());
-            }
-            result += word + " ";
-            word = "";
-        } else {
-            word += c;
-        }
+    std::size_t pos = 0;
+    while ((pos = str.find(" ")) != std::string::npos) {
+        std::size_t wordLength = str.substr(0, pos).length();
+        if (wordLength >= 5) {
+            for (int i = wordLength - 1; i >= 0; --i)
+                result += str[pos - i] == ' ' ? ' ' : str[pos - i];
+        } else
+            result += str.substr(0, pos);
+        str.erase(0, pos + 1);
     }
-    
-    if (word.length() >= 5) {
-        std::reverse(word.begin(), word.end());
-    }
-    result += word;
-    
+    if (str.length() > 4) {
+        for (int i = str.length() - 1; i >= 0; --i)
+            result += str[i] == ' ' ? ' ' : str[i];
+    } else
+        result += str;
     return result;
 }
 
 int main() {
-    std::cout << spinWords("a") << std::endl; // a
-    std::cout << spinWords("this is a test") << std::endl; // this is a test
-    std::cout << spinWords("this is another test") << std::endl; // this is rehtona test
-    std::cout << spinWords("hi") << std::endl; // hi
-    
+    std::string input;
+    while (std::cin >> input) {
+        std::cout << spinWords(input) << std::endl;
+    }
     return 0;
 }
