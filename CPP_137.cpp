bb96d70ffdce76a4fffee5be1ff7d1e4fc87205f
beep boop <githubbot@vadim.me>
2024-07-13 09:17:35 +0200

    Solution 89 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8fxvx5aq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,24 +1,37 @@
-#include <string>
-#include <algorithm>
-#include <boost/any.hpp>
-
-using namespace std;
+```cpp
+#include<boost/any_cast.hpp>
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return (int)b > a.convert_to<int>() ? b : a;
-    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string str_b = boost::any_cast<string>(b);
-        return stof(str_b) > a.convert_to<double>() ? b : a;
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str_a = boost::any_cast<string>(a);
-        string str_b = boost::any_cast<string>(b);
-        return stof(str_b) > stof(str_a) ? b : a;
-    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        string str_b = boost::any_cast<string>(b);
-        int val_a = boost::any_cast<int>(a);
-        return stof(str_b) > val_a ? b : a;
-    } else {
-        return "None";
+    if (!boost::any_cast<int>(&a) && !boost::any_cast<float>(&a)) {
+        string s = boost::any_cast<string>(a);
+        int decPos = s.find('.');
+        if (decPos == -1)
+            decPos = s.find(',');
+        if (decPos != -1) {
+            s.erase(decPos);
+            a = boost::any(s);
+        }
     }
+
+    if (!boost::any_cast<int>(&b) && !boost::any_cast<float>(&b)) {
+        string s = boost::any_cast<string>(b);
+        int decPos = s.find('.');
+        if (decPos == -1)
+            decPos = s.find(',');
+        if (decPos != -1) {
+            s.erase(decPos);
+            b = boost::any(s);
+        }
+    }
+
+    if (boost::any_cast<int>(&a) > boost::any_cast<int>(&b))
+        return a;
+    else if (boost::any_cast<float>(&a) > boost::any_cast<float>(&b))
+        return a;
+    else if (boost::any_cast<int>(&b) > boost::any_cast<int>(&a))
+        return b;
+    else if (boost::any_cast<float>(&b) > boost::any_cast<float>(&a))
+        return b;
+
+    return "None";
 }
