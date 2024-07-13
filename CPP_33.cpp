4e7765530cc0101b87d21f812d8316f163440cd0
beep boop <githubbot@vadim.me>
2024-07-13 05:40:18 +0200

    Solution 2 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/g5pw1vsa Prompt: Complete the following code given the task description and function signature.

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -9,13 +9,13 @@ vector<int> sort_third(vector<int> l) {
     for (int i = 0; i < l.size(); i++) {
         if (i % 3 == 0) {
             vector<int> temp;
-            for (int j = 0; j < l.size(); j++) {
-                if (j % 3 == 0) {
-                    temp.push_back(l[j]);
-                }
+            for (int j = i; j < l.size() && j % 3 == 0; j++) {
+                temp.push_back(l[j]);
             }
             sort(temp.begin(), temp.end());
-            result.insert(result.end(), temp.begin(), temp.end());
+            for (int k = 0; k < temp.size(); k++) {
+                result.push_back(temp[k]);
+            }
         } else {
             result.push_back(l[i]);
         }
@@ -24,18 +24,32 @@ vector<int> sort_third(vector<int> l) {
 }
 
 int main() {
-    vector<int> l = {1, 2, 3};
-    vector<int> result = sort_third(l);
-    for (int i : result) {
+    vector<int> l1 = {1, 2, 3};
+    vector<int> l2 = {5, 6, 3, 4, 8, 9, 2};
+
+    cout << "l1: ";
+    for (int i : l1) {
         cout << i << " ";
     }
     cout << endl;
 
-    l = {5, 6, 3, 4, 8, 9, 2};
-    result = sort_third(l);
-    for (int i : result) {
+    cout << "sort_third(l1): ";
+    for (int i : sort_third(l1)) {
         cout << i << " ";
     }
     cout << endl;
+
+    cout << "l2: ";
+    for (int i : l2) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    cout << "sort_third(l2): ";
+    for (int i : sort_third(l2)) {
+        cout << i << " ";
+    }
+    cout << endl;
+
     return 0;
 }
