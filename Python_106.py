2a8b518a076722a71289ad43897ce65d80133ca3
beep boop <githubbot@vadim.me>
2024-07-13 17:56:29 +0200

    Solution 68 of Python/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/8smindo4 Prompt: Complete the following code given the task description and function signature.

--- Python_106.py
+++ Python_106.py
@@ -3,10 +3,10 @@ def f(n):
     result = []
     for i in range(1, n+1):
         if i % 2 == 0:
-            factorial = 1
+            fact = 1
             for j in range(1, i+1):
-                factorial *= j
-            result.append(factorial)
+                fact *= j
+            result.append(fact)
         else:
             result.append(sum(range(1, i+1)))
     return result
