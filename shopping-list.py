20ee40ff86898d0ba2b462d3b389b043fc0f068e
beep boop <githubbot@vadim.me>
2024-07-14 01:19:08 +0200

    Solution 89 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/kgj5ftzp Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/560ae479-3ba1-4b77-aaba-bb08a0b8e a2a.py", line 13     """     ^ SyntaxError: unterminated triple-quoted string literal (detected at line 13)

--- shopping-list.py
+++ shopping-list.py
@@ -10,4 +10,4 @@ else:
             print("Error: Invalid input. Each line should contain 'price discount' pairs.")
             exit()
         total_price += sum([p - (p * int(d) / 100) for p, d in zip(prices_discounts[::2], prices_discounts[1::2])])
-    print(format(total_price, ".2f"))
+    print(f"{'{:.2f}'.format(total_price)}")
