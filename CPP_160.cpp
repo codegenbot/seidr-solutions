69b3f3c39b03f820bb3fa0e8bfd118524a4096e8
beep boop <githubbot@vadim.me>
2024-07-13 09:26:34 +0200

    Solution 32 of CPP/160, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xcyz21yy Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,3 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+#include<vector>
+#include<string>
+using namespace std;
+#include<algorithm>
+#include<stdlib.h>
+
 int do_algebra(vector<string> operato, vector<int> operand) {
     int result = operand[0];
     for (int i = 0; i < operato.size(); i++) {
