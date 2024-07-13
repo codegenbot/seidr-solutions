d1e93dcf4d8b6449609a3011ab129f67aa761a0b
beep boop <githubbot@vadim.me>
2024-07-14 01:23:22 +0200

    Solution 23 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/3yh1pnl7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/654ab221-efa8-4216-b31f-36af7d08d 2c4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,7 +1,9 @@
 def fizz_buzz(x):
-    if x % 5 == 0:
-        return "Buzz" if x % 3 else "Buzz"
+    if (x % 3 == 0) and (x % 5 == 0):
+        return "FizzBuzz"
     elif x % 3 == 0:
-        return "Fizz"
+        return "Fizz" if x % 5 != 0 else ""
+    elif x % 5 == 0:
+        return "Buzz"
     else:
         return str(x)
