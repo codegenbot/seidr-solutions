a24e56903e56c566cd54fa540bd93fba0431bdf3
beep boop <githubbot@vadim.me>
2024-07-13 09:23:14 +0200

    Solution 92 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/2rmfqz3r Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from eea9ff58-1f2e-447b-bc05-ecbca1d6a103.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,13 +1,12 @@
 #include <vector>
-#include <utility>
-#include <unordered_map>
+using namespace std;
 
-std::pair<int, int> findPair(std::vector<int>& nums, int target) {
-    std::unordered_map<int, int> numIndex;
+pair<int, int> findPair(vector<int>& nums, int target) {
+    unordered_map<int, int> numIndex;
     for (int i = 0; i < nums.size(); ++i) {
         int complement = target - nums[i];
         if (numIndex.count(complement)) {
-            return std::pair<int, int>(complement < nums[i] ? complement : nums[i], nums[i]);
+            return pair<int, int>(complement < nums[i] ? complement : nums[i], nums[i]);
         }
         numIndex[nums[i]] = i;
     }
