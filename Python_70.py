4d514dc747798aa49860d6f07925d3f1757c292c
beep boop <githubbot@vadim.me>
2024-07-13 12:59:02 +0200

    Solution 5 of Python/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/pgk8qred Prompt: Complete the following code given the task description and function signature.

--- Python_70.py
+++ Python_70.py
@@ -1,11 +1,10 @@
 ```
 def strange_sort_list(lst):
-    if len(lst) <= 1:
+    if len(lst) == 0:
         return lst
     sorted_lst = [min(lst)]
-    while len(sorted_lst) < len(lst):
-        max_val = max([i for i in lst if i not in sorted_lst])
-        sorted_lst.append(max_val)
-        min_val = min([i for i in lst if i not in sorted_lst])
-        sorted_lst.append(min_val)
+    lst.remove(min(lst))
+    while len(lst) > 0:
+        sorted_lst.append(max(lst))
+        lst.remove(max(lst))
     return sorted_lst
