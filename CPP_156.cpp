8090cabad792c52e964c0fcf9f4b8abe2b74b072
beep boop <githubbot@vadim.me>
2024-07-13 05:28:52 +0200

    Solution 80 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/l5g3qqg3 Prompt: 0b5e6dda-1168-40e7-85f5-ea26c026be9a.cpp:5:1: error: ‘string’ does not name a ty pe     5 | string int_to_mini_roman(int number) {       | ^~~~~~ In file included from 0b5e6dda-1168-40e7-85f5-ea26c026be9a.cpp:22: 0b5e6dda-1168-40e7-85f5-ea26c026be9a.cpp: In function ‘int main()’: 0b5e6dda-1168-40e7-85f5-ea26c026be9a.cpp:24:13: error: ‘int_to_mini_romank’ was not declared in this scope    24 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,12 +1,14 @@
+```
 #include <unordered_map>
-#include <string>
 
-std::string int_to_mini_roman(int number) {
-    std::unordered_map<int, std::string> roman_map = {{1000, "M"}, {900, "CM"}, {500, "D"},
+using namespace std;
+
+string int_to_mini_roman(int number) {
+    unordered_map<int, string> roman_map = {{1000, "M"}, {900, "CM"}, {500, "D"},
                                              {400, "CD"}, {100, "C"}, {90, "XC"},
                                              {50, "L"}, {40, "XL"}, {10, "X"},
                                              {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
-    std::string roman = "";
+    string roman = "";
     
     for (auto& pair : roman_map) {
         while (number >= pair.first) {
