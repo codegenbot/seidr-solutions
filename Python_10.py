82a67bbb9060b45767d04c306bbb2a798cbb335a
beep boop <githubbot@vadim.me>
2024-07-13 04:12:55 +0200

    Solution 77 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d45c05lr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b49b09ef-3fe8-492f-95c7-598117622 bae.py", line 9, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b49b09ef-3fe8-492f-95c7-598117622 bae.py", line 6, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,5 +1,15 @@
-```
 def make_palindrome(string: str) -> str:
     cleaned_string = "".join(char for char in string if char.isalnum()).lower()
-    half = cleaned_string[:len(cleaned_string)//2]
-    return "#{}#".format(half + half[::-1]).center(len(cleaned_string)*4, '#')[2:-2].upper()
+    half = "".join(
+        char for char in cleaned_string[: len(cleaned_string) // 2] if char.isalnum()
+    ).lower()
+    return (
+        "#{}#".format(
+            "".join(char for char in half + half[::-1] if char.isalnum()).lower()
+            + "".join(char for char in half + half[::-1] if char.isalnum()).lower()[
+                ::-1
+            ]
+        )
+        .center(len(cleaned_string) * 4, "#")[2:-2]
+        .upper()
+    )
