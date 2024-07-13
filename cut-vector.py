3f96fb2edee8ba48005433b53a0b706aff3338fc
beep boop <githubbot@vadim.me>
2024-07-14 00:31:47 +0200

    Solution 95 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/3osjanpi Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.py
+++ cut-vector.py
@@ -1,32 +1,12 @@
-def cut_vector(vector):
+def cut_vector(lst):
     min_diff = float("inf")
-    cut_point = -1
+    split_index = 0
 
-    for i in range(1, len(vector)):
-        left_sum = sum(vector[:i])
-        right_sum = sum(vector[i:])
-
-        if left_sum == right_sum:
-            return vector[:i], vector[i:]
-
-        diff = abs(left_sum - right_sum)
+    for i in range(1, len(lst)):
+        diff = abs(sum(lst[:i]) - sum(lst[i:]))
 
         if diff < min_diff:
             min_diff = diff
-            cut_point = i
+            split_index = i
 
-    left_sum = sum(vector[:cut_point])
-    right_sum = sum(vector[cut_point:])
-
-    if left_sum == right_sum:
-        return vector[:cut_point], vector[cut_point:]
-    else:
-        return vector[:cut_point], vector[cut_point:]
-
-
-# Testing the function
-print(cut_vector([1, 0]))  # Should print ([1], [0])
-print(cut_vector([1, 10]))  # Should print ([1], [10])
-print(cut_vector([1, 100]))  # Should print ([1], [100])
-print(cut_vector([1, 1000]))  # Should print ([1], [1000])
-print(cut_vector([1, 10000]))  # Should print ([1], [10000])
+    return [lst[:split_index], lst[split_index:]]
