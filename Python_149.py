c147692e5006ef09a7e823eb45129862f9b5f836
beep boop <githubbot@vadim.me>
2024-07-13 10:23:56 +0200

    Solution 56 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/5nnxhoku Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/db619cdc-7697-4775-9fce-2782a2a3b e3b.py", line 10, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/db619cdc-7697-4775-9fce-2782a2a3b e3b.py", line 7, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,4 +1,3 @@
-```
 def sorted_list_sum(lst):
     alpha_chars = [i for i in lst if i.isalpha()]
-    return [len(''.join(sorted(i))) for i in set(alpha_chars)]
+    return ["".join(sorted(i)) for i in set(alpha_chars)]
