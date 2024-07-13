62ca21e4f97063f9d7b5fa27d52d1a95daed856a
beep boop <githubbot@vadim.me>
2024-07-13 02:29:36 +0200

    Solution 89 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/y3nscp8u Prompt:    67 |     using string    = basic_string<char>;       |           ^~~~~~ 63686dac-ea1b-4baf-88d0-17fae506cfc8.cpp:36:44: error: ‘b’ was not declared in t his scope    36 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 63686dac-ea1b-4baf-88d0-17fae506cfc8.cpp:36:45: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 63686dac-ea1b-4baf-88d0-17fae506cfc8.cpp:35: 63686dac-ea1b-4baf-88d0-17fae506cfc8.cpp: In function ‘int main()’: 63686dac-ea1b-4baf-88d0-17fae506cfc8.cpp:45:20: error: ‘sorted_list_sum’ was not  declared in this scope; did you mean ‘sortedListSum’?    45 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 63686dac-ea1b-4baf-88d0-17fae506cfc8.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -7,7 +7,7 @@ bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b
     return a == b;
 }
 
-std::vector<std::string> sortedListSum(std::vector<std::string> lst) {
+std::vector<std::string> mySortedListSum(std::vector<std::string> lst) {
     auto it = std::stable_partition(lst.begin(), lst.end(),
                                      [&](const auto& s) { return !s.length() % 2; }),
         end = lst.erase(it, lst.end());
@@ -23,11 +23,11 @@ std::vector<std::string> sortedListSum(std::vector<std::string> lst) {
 
 int mainFunction() {
     std::cout << "Result: ";
-    for (const auto& str : sortedListSum({"aaaa", "bbbb", "dd", "cc"})) {
+    for (const auto& str : mySortedListSum({"aaaa", "bbbb", "dd", "cc"})) {
         std::cout << str << " ";
     }
     std::cout << std::endl;
-    if (!issame(sortedListSum({{"aaaa", "bbbb", "dd", "cc"}}), {{"cc","dd","aaaa","bbbb"}}))
+    if (!issame({{"aaaa", "bbbb", "dd", "cc"}}, {{"cc","dd","aaaa","bbbb"}}))
         return 1;
     return 0;
 }
