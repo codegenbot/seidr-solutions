53d6637793cc553d4d5bad8eef58aba244d5011b
beep boop <githubbot@vadim.me>
2024-07-13 07:26:25 +0200

    Solution 18 of bowling, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/b8nej3bk Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,28 +1,33 @@
-def bowling(score_str):
+def bowling_score(bowls):
     score = 0
     frame = 1
-    rolls = []
-    for char in score_str:
-        if char == "X":
-            rolls.append(10)
-            if frame < 10:
-                rolls.append(0)
-        elif char == "/":
-            rolls.append(10 - rolls[-1])
-        elif char == "-":
-            rolls.append(0)
+    bowl_index = 0
+    while frame <= 10:
+        if bowls[bowl_index] == "X":
+            score += (
+                10 + get_value(bowls, bowl_index + 1) + get_value(bowls, bowl_index + 2)
+            )
+            bowl_index += 1
+        elif bowls[bowl_index + 1] == "/":
+            score += 10 + get_value(bowls, bowl_index + 2)
+            bowl_index += 2
         else:
-            rolls.append(int(char))
-
-        if len(rolls) >= 2 and (rolls[-2] == 10 or len(rolls) % 2 == 0):
-            score += sum(rolls[-3:])
-            frame += 1
-        if frame > 10:
-            break
-
+            score += get_value(bowls, bowl_index) + get_value(bowls, bowl_index + 1)
+            bowl_index += 2
+        frame += 1
     return score
 
 
-# Read input from user
-score_str = input()
-print(bowling(score_str))
+def get_value(bowls, index):
+    if bowls[index] == "X":
+        return 10
+    elif bowls[index] == "/":
+        return 10 - int(bowls[index - 1])
+    elif bowls[index] == "-":
+        return 0
+    else:
+        return int(bowls[index])
+
+
+bowls = input()
+print(bowling_score(bowls))
