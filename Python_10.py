b5a04634f35cdc96b5e49823a5f57f820317ccaa
beep boop <githubbot@vadim.me>
2024-07-13 11:35:33 +0200

    Solution 78 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lydubfu0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2f84ade9-4e17-4145-a51f-0c7c001dc 84b.py", line 17, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2f84ade9-4e17-4145-a51f-0c7c001dc 84b.py", line 14, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2f84ade9-4e17-4145-a51f-0c7c001dc 84b.py", line 10, in make_palindrome     prefix += string[0]               ~~~~~~^^^ IndexError: string index out of range

--- Python_10.py
+++ Python_10.py
@@ -1,9 +1,4 @@
 ```
-def is_palindrome(string: str) -> bool:
-    """Test if given string is a palindrome"""
-    return string == string[::-1]
-
-
 def make_palindrome(string: str) -> str:
     n = len(string)
     prefix = ""
