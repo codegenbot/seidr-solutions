3dc68337d7f83cc6100e9e10b7ee37c1f4fc0f80
beep boop <githubbot@vadim.me>
2024-07-13 06:25:29 +0200

    Solution 67 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/xn06kl5c Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b4737da9-ce03-4f63-91d0-2b87ff4c9 e58.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_38.py
+++ Python_38.py
@@ -1,8 +1,8 @@
-```
 def decode_cyclic():
+    print("Please enter a string of alphabets only.")
     while True:
         s = str(input('Enter the cyclic string: '))
-        if s.isalpha():  
+        if set(s).issubset('abcdefghijklmnopqrstuvwxyz') or set(s).issubset('ABCDEFGHIJKLMNOPQRSTUVWXYZ'):  
             break
         else:
             print("Invalid input. Please enter a string of alphabets only.")
