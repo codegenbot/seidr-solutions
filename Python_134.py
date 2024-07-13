9307c958a14405ecc1dad60a3285969d834c3656
beep boop <githubbot@vadim.me>
2024-07-13 09:05:47 +0200

    Solution 21 of Python/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fw253inm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/59ff281b-d3a4-4fad-80bd-ebeae5984 5dd.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_134.py
+++ Python_134.py
@@ -1,4 +1,6 @@
-def check_palindrome():
-    s = input("Enter a string: ")
-    s = "".join(c for c in s if c.isalnum()).lower()
-    return s == s[::-1]
+def check_palindrome(input_string):
+    input_string = "".join(c for c in input_string if c.isalnum()).lower()
+    if len(input_string) == 0:
+        return "Error: The file is empty."
+    else:
+        return input_string == input_string[::-1]
