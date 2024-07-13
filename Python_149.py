74188790b24c77690f54dc8a623838a59dcc15e5
beep boop <githubbot@vadim.me>
2024-07-13 10:41:38 +0200

    Solution 81 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/5nnxhoku Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/24dc4985-2092-4c8a-af58-5e1ee7594 93d.py", line 12, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/24dc4985-2092-4c8a-af58-5e1ee7594 93d.py", line 9, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,5 +1,4 @@
 ```
 def sorted_list_sum(lst):
     numbers = [int(c) for s in lst for c in s if c.isdigit()]
-    sum_of_numbers = str(sum(numbers)) if numbers else '0'
-    return sorted(set([s.replace(str(x), '') for s in lst] + [lst[0], lst[-1]]))
+    return sorted(set(numbers))
