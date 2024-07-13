853fc86cde0ff17539f9d742ef05cfbe4e5c09fb
beep boop <githubbot@vadim.me>
2024-07-13 09:35:28 +0200

    Solution 51 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vgofggry Prompt: Complete the following code given the task description and function signature.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,18 +1,34 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <cryptopp/md5.h>
-
-using namespace std;
+#include <openssl/ssl.h>
+#include <openssl/mem.h>
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
-    CryptoPP::MD5 hash;
-    CryptoPP::StringSource(text, true, new CryptoPP::HashFilter(hash));
-    CryptoPP::StringSink result;
-    CryptoPP::HashFilter filter(hash, new CryptoPP::HexEncoder(new CryptoPP::StringSink(result)));
+    unsigned char buffer[1024];
+    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
+    SSL *ssl = SSL_new(ctx);
+    BIO *bio = BIO_new_mem_buf((void *)text.c_str(), text.length());
+    BIO_set_ttl(bio, 0);
+    BIO_set_read_buffer(bio, "read", buffer, 1024);
 
-    string md5 = result.GetString();
-    return md5;
+    unsigned char md[MD5_DIGEST_LENGTH];
+    MD5_CTX ctx_md5;
+    MD5_Init(&ctx_md5);
+    while (BIO_read(bio, &buffer, 1) > 0) {
+        MD5_Update(&ctx_md5, buffer, 1);
+    }
+    MD5_Final(md, &ctx_md5);
+
+    string result;
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        char buff[3];
+        sprintf(buff, "%02x", md[i]);
+        result += buff;
+    }
+
+    BIO_free_all(bio);
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+
+    return result;
 }
