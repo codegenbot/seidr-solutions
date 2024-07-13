ebb20494d5258f6b7b3878baa7bbdd2979d45551
beep boop <githubbot@vadim.me>
2024-07-13 08:15:32 +0200

    Solution 18 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/wx58ekjm Prompt: c70c473b-6c17-4813-8b3b-f9ae9787a99b.cpp:3:1: error: ‘pair’ does not name a type     3 | pair<int, int> findPair(vector<int>& nums, int target) {       | ^~~~

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,16 +1,19 @@
+```cpp
 #include <utility>
 #include <unordered_map>
 
-std::pair<int, int> findPair(std::vector<int>& nums, int target) {
-    std::unordered_map<int, int> numMap;
+using namespace std;
+
+pair<int, int> findPair(vector<int>& nums, int target) {
+    unordered_map<int, int> numMap;
     
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
         if (numMap.find(complement) != numMap.end()) {
-            return std::pair<int, int>(complement, nums[i]);
+            return make_pair(complement, nums[i]);
         }
         numMap[nums[i]] = i;
     }
     
-    return std::pair<int, int>(-1, -1);
+    return make_pair(-1, -1);
 }
