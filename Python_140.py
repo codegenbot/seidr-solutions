a00d86feb98fb2b1ba04702bf699bcda4316b052
beep boop <githubbot@vadim.me>
2024-07-13 11:30:51 +0200

    Solution 85 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/puafi84q Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c04f05f7-5916-4a76-8523-6592bf44e f66.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,13 +1,13 @@
+```
 import re
 
 
 def fix_spaces():
-    text = ""
     while True:
+        text = input("Enter a string (or 'stop' to finish): ").strip().lower()
         if text.lower() == "stop":
             break
-        text = input(f"Enter a string ({text} or 'stop' to finish): ").strip().lower()
-        if not re.match("^[a-z_]+$", text):
+        elif not re.match("^[a-z_]+$", text):
             print("Please enter alphabetic characters and/or underscores. Try again.")
         else:
             text_list = text.split()
@@ -16,6 +16,4 @@ def fix_spaces():
                 new_text = new_text.replace("--", "-")
             while "_-_" in new_text or "_--" in new_text:
                 new_text = new_text.replace("_--", "_-").replace("_-_", "_-")
-            print(
-                f"{new_text.upper() if new_text.isalpha() else new_text.capitalize()}"
-            )
+            print(f"{new_text.upper() if re.match("^[a-zA-Z_]+$", new_text) else new_text.capitalize()}")
