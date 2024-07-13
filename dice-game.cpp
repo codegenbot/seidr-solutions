16bb6f8afc69110b68dab65f9cea0cb0632a0dd4
beep boop <githubbot@vadim.me>
2024-07-14 01:27:17 +0200

    Solution 29 of dice-game, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/amsf2jsj Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- dice-game.cpp
+++ dice-game.cpp
@@ -1,5 +1,6 @@
 ```cpp
 #include <iostream>
+using namespace std;
 
 double diceGame(int n, int m) {
     double total = (double)n * m;
@@ -13,25 +14,28 @@ double diceGame(int n, int m) {
 
 int main() {
     int n, m;
-    std::cout << "Enter value of n: ";
-    std::cin >> n;
+    
+    cout << "Enter value of n: ";
+    cin >> n;
 
     if(n < 2) {
-        std::cout << "n must be at least 2. Please enter again.\n";
-        std::cout << "Enter value of n: ";
-        std::cin >> n;
+        cout << "n must be at least 2. Please enter again." << endl;
+        cout << "Enter value of n: ";
+        cin >> n;
     }
 
-    std::cout << "\nEnter value of m: ";
-    std::cin >> m;
+    cout << "\nEnter value of m: ";
+    cin >> m;
 
     if(m < 2) {
-        std::cout << "m must be at least 2. Please enter again.\n";
-        std::cout << "Enter value of m: ";
-        std::cin >> m;
+        cout << "m must be at least 2. Please enter again." << endl;
+        cout << "Enter value of m: ";
+        cin >> m;
     }
 
     double result = diceGame(n, m);
-    std::cout << "Probability that Peter rolls strictly higher than Colin is: " << result << std::endl; 
+    cout << "Probability that Peter rolls strictly higher than Colin is: " << result << endl; 
+    cout << "The answer is: " << result << endl;
+
     return 0;
 }
