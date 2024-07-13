d9851e9c3a1446eb3ee09cfeff3d21a91088c836
beep boop <githubbot@vadim.me>
2024-07-13 15:52:02 +0200

    Solution 59 of gcd, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/jpd3r025 Prompt: ::allocator_traits<std::allocator<_Tp1> >::allocate(std::allocator_traits<std::a llocator<_Tp1> >::allocator_type&, int) [with _Tp = long unsigned int; std::allo cator_traits<std::allocator<_Tp1> >::pointer = long unsigned int*; std::allocato r_traits<std::allocator<_Tp1> >::allocator_type = std::allocator<long unsigned i nt>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = long unsigned int; _Alloc = std::allocator<long unsigned int>; std::_Vect or_base<_Tp, _Alloc>::pointer = long unsigned int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const long unsigned int&}; _Tp = long unsigned int; _Alloc = std:: allocator<long unsigned int>; std::vector<_Tp, _Alloc>::iterator = std::vector<l ong unsigned int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = long unsigned int; _Allo c = std::allocator<long unsigned int>; std::vector<_Tp, _Alloc>::value_type = lo ng unsigned int]’ 82279d87-d52a-41b5-b1f0-8b0c837b8cbb.cpp:24:29:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<long unsigned int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- gcd.cpp
+++ gcd.cpp
@@ -14,8 +14,8 @@ int gcd(int a, int b) {
     return a;
 }
 
-std::vector<unsigned long> indicesOfSubstring(string text, string target) {
-    vector<unsigned long> result;
+vector<long unsigned int> indicesOfSubstring(string text, string target) {
+    vector<long unsigned int> result;
     size_t n = text.length();
     size_t m = target.length();
 
@@ -25,4 +25,5 @@ std::vector<unsigned long> indicesOfSubstring(string text, string target) {
             i += m; // Increment by the length of the target substring
         }
     }
+    return result;
 }
