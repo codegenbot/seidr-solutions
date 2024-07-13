9bdddb1365ad606671178983be50e0adb8d15571
beep boop <githubbot@vadim.me>
2024-07-14 00:01:31 +0200

    Solution 73 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/5vwkqgs9 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,31 +1,27 @@
 int bowlingScore(string s) {
     int score = 0;
-    bool lastRoll = false;
+    int roll1, roll2;
 
-    for (int frame = 1; frame <= 10; ++frame) {
-        if (s[2*frame-1] == '/') {
-            int rollsInFrame = 2;
-            if (s[2*frame-2] != 'X') {
-                rollsInFrame = 3;
+    for (int i = 0; i < 10; ++i) {
+        if (s[i] == 'X') {
+            score += 30;
+        } else if (s[i] == '/') {
+            if (s[i + 1] == 'X' || s[i + 2] == 'X') {
+                roll1 = 10 - (s[i + 1] - '0');
+                roll2 = 10 - (s[i + 2] - '0');
+                score += roll1 + roll2;
+            } else {
+                int ten_frame = 10 - (s[i + 1] - '0') - (s[i + 2] - '0');
+                score += ten_frame;
             }
-            lastRoll = true;
         } else {
-            lastRoll = false;
-        }
-
-        if (rollsInFrame == 1) {
-            score += 10;
-        } else if (rollsInFrame == 2) {
-            int roll1, roll2;
-            roll1 = s[2*frame-2] - '0';
-            roll2 = s[2*frame-1] - '0';
+            roll1 = s[i] - '0';
+            if (i < 8 && s[i + 1] == '/') {
+                roll2 = 10 - roll1;
+            } else {
+                roll2 = s[i + 1] - '0';
+            }
             score += roll1 + roll2;
-        } else {
-            int roll1, roll2, roll3;
-            roll1 = s[2*frame-3] - '0';
-            roll2 = s[2*frame-2] - '0';
-            roll3 = s[2*frame-1] - '0';
-            score += 10 + roll3;
         }
     }
 
