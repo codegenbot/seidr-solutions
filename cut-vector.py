fabf571952cf592cf83ab042940cea325e0fdf30
beep boop <githubbot@vadim.me>
2024-07-14 01:53:01 +0200

    Solution 44 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/6owj9z2i Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/42dab470-636d-4d75-b7ec-65a70d3a0 060.py", line 1     ```     ^ SyntaxError: invalid syntax

--- cut-vector.py
+++ cut-vector.py
@@ -1,8 +1,8 @@
-def cut_vector(vector):
-    left_sum = 0
-    for i, num in enumerate(vector):
-        left_sum += num
-        
-        if abs(left_sum - sum(vector[i+1:])) < abs(sum(vector[:i]) - sum(vector[i:])):
-            return tuple(vector[:i]), tuple(vector[i:])
-    return tuple([num]), tuple(vector[1:])
+    def cut_vector(vector):
+        left_sum = 0
+        for i, num in enumerate(vector):
+            left_sum += num
+            
+            if abs(left_sum - sum(vector[i+1:])) < abs(sum(vector[:i]) - sum(vector[i:])):
+                return tuple(vector[:i]), tuple(vector[i:])
+        return tuple([num]), tuple(vector[1:])
