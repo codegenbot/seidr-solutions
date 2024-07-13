cbbb0638faa46f69404d1aa68bab1086739b849f
beep boop <githubbot@vadim.me>
2024-07-13 11:25:53 +0200

    Solution 48 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lydubfu0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/65ccba9a-99ac-44e2-bdb5-3219227e3 bfd.py", line 24, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/65ccba9a-99ac-44e2-bdb5-3219227e3 bfd.py", line 21, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,4 +1,3 @@
-```
 def make_palindrome(string: str) -> str:
     lower = string.lower()
     for i in range(len(lower), -1, -1):
@@ -6,6 +5,4 @@ def make_palindrome(string: str) -> str:
             if string[0].islower():
                 return string + string[:i][::-1]
             elif string[0].isupper():
-                return (
-                    string[0].upper() + string[1 : i + 1].lower() + string[i:][::-1]
-                )
+                return string[0].upper() + string[1 : i + 1].lower() + string[i:][::-1]
