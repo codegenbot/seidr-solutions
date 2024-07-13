155e7a2461aae349c710bb9f20c5f2915238cc1b
beep boop <githubbot@vadim.me>
2024-07-13 09:26:28 +0200

    Solution 48 of indices-of-substring, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/unqojttr Prompt:   +++ |+#include <iostream>     3 | using namespace std; cebefa10-7eb3-4f7f-a155-7bbdd8e13786.cpp:27:9: error: ‘cout’ was not declared in  this scope    27 |         cout << i << " ";       |         ^~~~ cebefa10-7eb3-4f7f-a155-7bbdd8e13786.cpp:27:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? cebefa10-7eb3-4f7f-a155-7bbdd8e13786.cpp:29:5: error: ‘cout’ was not declared in  this scope    29 |     cout << endl;       |     ^~~~ cebefa10-7eb3-4f7f-a155-7bbdd8e13786.cpp:29:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? cebefa10-7eb3-4f7f-a155-7bbdd8e13786.cpp:29:13: error: ‘endl’ was not declared i n this scope    29 |     cout << endl;       |             ^~~~ cebefa10-7eb3-4f7f-a155-7bbdd8e13786.cpp:3:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     2 | #include <vector>   +++ |+#include <ostream>     3 | using namespace std;

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <vector>
 #include <iostream>
-
 using namespace std;
 
 vector<int> indicesOfSubstring(string text, string target) {
@@ -30,3 +29,4 @@ int main() {
     }
     cout << endl;
     return 0;
+}
