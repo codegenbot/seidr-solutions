317e2aa10cb46d0a0de243e0cdf02d05945aa296
beep boop <githubbot@vadim.me>
2024-07-13 06:13:51 +0200

    Solution 92 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tcf481c2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/95bb340e-d439-460e-bbf0-8bde9387e 316.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_37.py
+++ Python_37.py
@@ -1,7 +1,1 @@
-def main():
-    numbers = list(map(int, input("Enter space-separated numbers: ").split()))
-    print(sorted(set(numbers)))
-
-
-if __name__ == "__main__":
-    main()
+numbers = list(map(int, input('''Enter space-separated numbers: ''').split()))
