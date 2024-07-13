fe98041dae447e6558646ae07e9b9319cab5af48
beep boop <githubbot@vadim.me>
2024-07-13 22:38:39 +0200

    Solution 29 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/xzrffczy Prompt: 4bb7c67d-cf02-4f27-9f44-e80ab3c99f2c.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ 4bb7c67d-cf02-4f27-9f44-e80ab3c99f2c.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ 4bb7c67d-cf02-4f27-9f44-e80ab3c99f2c.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -11,16 +11,18 @@ int mastermind(string code, string guess) {
         }
     }
 
+    map<char, int> codeCount;
+    for (char c : code) {
+        codeCount[c]++;
+    }
+
     for (char c : guess) {
-        int count = 0;
-        for (int i = 0; i < 4; ++i) {
-            if (code[i] == c && code[i] != guess[i]) {
-                white++;
-                count++;
-            }
-        }
-        if (count > 1) {
-            black += count - 1;
+        if (codeCount[c] > 0 && code[c - 'A' + 1] == c) {
+            black++;
+            codeCount[c]--;
+        } else if (codeCount[c] > 0) {
+            white++;
+            codeCount[c]--;
         }
     }
 
