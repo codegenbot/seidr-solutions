69b5b492e0885e8915150fd5bc94bb9e804fccfd
beep boop <githubbot@vadim.me>
2024-07-13 06:54:34 +0200

    Solution 17 of CPP/83, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/f7z4iutz Prompt: Complete the following code given the task description and function signature.

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -5,11 +5,18 @@ using namespace std;
 int starts_one_ends(int n) {
     int count = 0;
     for (int i = 1; i <= 9; i++) {
-        if (i == 1 || i == 9) {
+        if (i == 1 || i % 10 == 1) {
             count++;
         }
-        for (int j = 1; j < n - 1; j++) {
-            count++;
+        if (n > 1) {
+            for (int j = 1; j < i; j++) {
+                if (j % 10 != 1 && j / 10 != 1) {
+                    break;
+                }
+            }
+            else {
+                count++;
+            }
         }
     }
     return count;
