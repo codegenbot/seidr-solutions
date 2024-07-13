07ba1b4a57286d0bfeee02590f85bf3332a34b6f
beep boop <githubbot@vadim.me>
2024-07-13 03:48:51 +0200

    Solution 17 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/acxuxpiz Prompt:       |      ^~~~ In file included from 5f8236cf-7ea9-489c-a90e-db936fbc8cf7.cpp:1: /usr/include/c++/11/iostream:61:18: note: ‘std::cout’ declared here    61 |   extern ostream cout;          /// Linked to standard output       |                  ^~~~ 5f8236cf-7ea9-489c-a90e-db936fbc8cf7.cpp:5:1: error: expected unqualified-id bef ore ‘for’     5 | for (int i = cutIndex + 1; i < n; i++) std::cout << nums[i] << " ";       | ^~~ 5f8236cf-7ea9-489c-a90e-db936fbc8cf7.cpp:5:28: error: ‘i’ does not name a type     5 | for (int i = cutIndex + 1; i < n; i++) std::cout << nums[i] << " ";       |                            ^ 5f8236cf-7ea9-489c-a90e-db936fbc8cf7.cpp:5:35: error: ‘i’ does not name a type     5 | for (int i = cutIndex + 1; i < n; i++) std::cout << nums[i] << " ";       |                                   ^ 5f8236cf-7ea9-489c-a90e-db936fbc8cf7.cpp:6:6: error: ‘cout’ in namespace ‘std’ d oes not name a type     6 | std::cout << std::endl;       |      ^~~~ In file included from 5f8236cf-7ea9-489c-a90e-db936fbc8cf7.cpp:1: /usr/include/c++/11/iostream:61:18: note: ‘std::cout’ declared here    61 |   extern ostream cout;          /// Linked to standard output       |                  ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,34 +1,11 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-    int n;
-    std::cin >> n;
-    std::vector<int> nums(n);
+using namespace std;
 
-    for(int i = 0; i < n; i++) {
-        std::cin >> nums[i];
-    }
-
-    int cutIndex = n / 2;
-    int minDiff = abs(nums[cutIndex] - nums[cutIndex + 1]);
-
-    for(int i = 1; i < n - 1; i++) {
-        if(abs(nums[i] - nums[i + 1]) < minDiff) {
-            cutIndex = i;
-            minDiff = abs(nums[i] - nums[i + 1]);
-        }
-    }
-
-    for(int i = 0; i <= cutIndex; i++) {
-        std::cout << nums[i] << " ";
-    }
-    std::cout << std::endl;
-
-    for(int i = cutIndex + 1; i < n; i++) {
-        std::cout << nums[i] << " ";
-    }
-    std::cout << std::endl;
-
-    return 0;
-}
+int n = nums.size();
+int cutIndex = n / 2;
+for (int i = 0; i <= cutIndex; i++) cout << nums[i] << " ";
+cout << endl;
+for (int i = cutIndex + 1; i < n; i++) cout << nums[i] << " ";
+cout << endl;
