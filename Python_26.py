2bda48d4c4dd28217326f1ba3d5111d53988989a
beep boop <githubbot@vadim.me>
2024-07-13 06:19:12 +0200

    Solution 96 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fykll3yz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/344c5d62-8801-4d86-964d-73852cb9e 965.py", line 11, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/344c5d62-8801-4d86-964d-73852cb9e 965.py", line 8, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_26.py
+++ Python_26.py
@@ -1,6 +1,8 @@
+```
 def remove_duplicates(input_list=None):
     if input_list is None:
         numbers = input("Enter comma-separated numbers: ")
         input_list = list(map(int, numbers.split(",")))
         
-    return [x for x in set(input_list)]
+    from collections import OrderedDict
+    return list(OrderedDict.fromkeys(input_list))
