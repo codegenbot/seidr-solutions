d8bf514ce04d3bde697c9c1009f84780c62266a1
beep boop <githubbot@vadim.me>
2024-07-13 17:36:41 +0200

    Solution 8 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ba2frpay Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,21 +1,25 @@
+Here is the Python solution for the bowling problem:
+
 ```
-def bowling_score(frames):
+def bowling_score(gutter_balls):
     score = 0
     frame = 1
-    for char in frames:
-        if char == 'X':
-            score += 30
+    while frame <= 10:
+        if gutter_balls[frame-1] == 'X':
+            if frame < 10:
+                score += 30
+            else:
+                score += 10 + 10 + int(gutter_balls[frame-2])
             frame += 1
-        elif char == '/':
-            first_roll = int(frames[:frames.index('/')].count('X')) * 10 + int(frames[frames.index('/')+1:frames.index('/', frames.index('/')+1)].count('X')) * 10
-            second_roll = 10 - first_roll
-            score += first_roll + second_roll
-            frame += 1
+        elif '/' in gutter_balls[frame-1]:
+            left, right = map(int, gutter_balls[frame-1].split('/'))
+            if left == 10:
+                score += left + right
+                frame += 1
+            else:
+                score += left
+                frame += 1
         else:
-            if char == '5':
-                score += 5
-                frame += 1
-            elif int(char) > 0:
-                score += int(char)
-                frame += 1
+            score += int(gutter_balls[frame-1])
+            frame += 1
     return score
