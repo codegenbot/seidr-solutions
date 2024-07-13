7f5a38ab455b699432311574df2dfabdc061c08a
beep boop <githubbot@vadim.me>
2024-07-13 04:55:07 +0200

    Solution 63 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/s15vggwr Prompt: f6652a75-c07c-48ee-aae7-e6e81d5ffbcf.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int specialFilter(vector<int> nums) {       |                   ^~~~~~ f6652a75-c07c-48ee-aae7-e6e81d5ffbcf.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int specialFilter(vector<int> nums) {       |                          ^~~ In file included from f6652a75-c07c-48ee-aae7-e6e81d5ffbcf.cpp:11: f6652a75-c07c-48ee-aae7-e6e81d5ffbcf.cpp: In function ‘int main()’: f6652a75-c07c-48ee-aae7-e6e81d5ffbcf.cpp:13:26: error: ‘specialFilter’ cannot be  used as a function    13 |     assert (specialFilter({}) == 0                   );       |             ~~~~~~~~~~~~~^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -13,8 +13,8 @@ int specialFilter(std::vector<int> nums) {
 
 int main() {
     assert(specialFilter({}) == 0);
-    std::vector<int> numbers = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-    int result = specialFilter(numbers);
-    std::cout << "The number of integers in the vector that satisfy the condition is: " << result;
+    std::vector<int> nums = {11, 12, 13, 21, 22, 23};
+    int result = specialFilter(nums);
+    std::cout << "Result: " << result << std::endl;
     return 0;
 }
