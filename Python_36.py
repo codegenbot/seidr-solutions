41b3ff99ca0c9ab854931147277b8d594f7545f1
beep boop <githubbot@vadim.me>
2024-07-13 05:46:07 +0200

    Solution 58 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/hv1pyxj2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3eaaf5ae-52dc-40d5-8ff5-be7d8c9f5 1a3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_36.py
+++ Python_36.py
@@ -1,13 +1,11 @@
 def fizz_buzz(n: int):
-    count = 0
     for i in range(1, n + 1):
         if i % 15 == 0:
             print("FizzBuzz")
-            count += 1
         elif i % 3 == 0 and i % 5 != 0:
             print("Fizz")
         elif i % 5 == 0 and i % 3 != 0:
             print("Buzz")
         else:
             print(i)
-    return count
+    return n
