f27c83ca3af9a5dbc3fdb8db83f1bca38882743d
beep boop <githubbot@vadim.me>
2024-07-13 07:14:24 +0200

    Solution 18 of Python/74, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lxim9dm8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/959c6aca-b4c5-468e-a5fb-2fa445125 c46.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_74.py
+++ Python_74.py
@@ -1,11 +1,3 @@
-```
 def total_match(lst1, lst2):
-    total_chars_lst1 = sum(len(s) for s in lst1)
-    total_chars_lst2 = sum(len(s) for s in lst2)
-    
-    if total_chars_lst1 < total_chars_lst2:
-        return lst1
-    elif total_chars_lst1 > total_chars_lst2:
-        return lst2
-    else:
-        return lst1
+    matches = [s for s1 in lst1 for s2 in lst2 if s1 == s2]
+    return matches
