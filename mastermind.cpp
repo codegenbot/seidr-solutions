a53752dcad32e32db13a9d305b702d67614d2954
beep boop <githubbot@vadim.me>
2024-07-13 04:29:08 +0200

    Solution 48 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/8xd1obee Prompt: 11bc2c95-da23-4268-be7a-eb9cc2c4aee3.cpp:1:21: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                     ^~~~~~ 11bc2c95-da23-4268-be7a-eb9cc2c4aee3.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int mastermind(std::string code, std::string guess) { 11bc2c95-da23-4268-be7a-eb9cc2c4aee3.cpp:1:39: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                                       ^~~~~~ 11bc2c95-da23-4268-be7a-eb9cc2c4aee3.cpp:1:39: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? 11bc2c95-da23-4268-be7a-eb9cc2c4aee3.cpp:1:51: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(std::string code, std::string guess) {       |                                                   ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,7 +1,7 @@
 #include <string>
-#include <algorithm>
+using namespace std;
 
-int mastermind(std::string code, std::string guess) {
+int mastermind(string code, string guess) {
     int white = 0;
     int black = 0;
 
@@ -10,22 +10,9 @@ int mastermind(std::string code, std::string guess) {
         if(code[i] == guess[i]) {
             black++;
         }
-        else if(std::count(guess.begin(), guess.end(), code[i]) > 0) {
+        else if(count(guess.begin(), guess.end(), code[i]) > 0) {
             white++;
         }
-    }
-
-    // Count the number of correct colors in correct places (black pegs)
-    for(int i=0; i<4; i++) {
-        bool found = false;
-        for(int j=0; j<4; j++) {
-            if(code[j] == guess[i]) {
-                code.erase(j, 1);
-                found = true;
-                break;
-            }
-        }
-        if(found) black++;
     }
 
     return black + white;
