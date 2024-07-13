abb2e18d33e7462045782412c92e6142c4b13569
beep boop <githubbot@vadim.me>
2024-07-13 07:32:30 +0200

    Solution 8 of Python/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/iydn96n5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e29773bc-85e1-4d11-b1f8-aa3b05a25 2b6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_69.py
+++ Python_69.py
@@ -1,6 +1,18 @@
 def search(lst):
-    max_num = 0
+    max_num = -1
     for num in lst:
-        if num > max_num and num > 0:
+        if num > max_num:
             max_num = num
     return max_num
+
+
+def main():
+    while True:
+        lst = list(map(int, input("Enter the numbers separated by space: ").split()))
+        if all(isinstance(x, int) for x in lst):
+            break
+    print(search(lst))
+
+
+if __name__ == "__main__":
+    main()
