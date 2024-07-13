06ac24429a2db6aa5023c085b575c3aa8f58ea3d
beep boop <githubbot@vadim.me>
2024-07-13 08:17:24 +0200

    Solution 14 of Python/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/q9rptr5i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ab20623e-8d7b-4172-8968-91a0fa4e2 fae.py", line 20, in <module>     check(minSubArraySum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ab20623e-8d7b-4172-8968-91a0fa4e2 fae.py", line 17, in check     assert minSubArraySum([1, -1]) == -1            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_114.py
+++ Python_114.py
@@ -6,7 +6,7 @@ def minSubArraySum(nums):
     start_index = 0
     for end_index in range(len(nums)):
         current_sum += nums[end_index]
-        while start_index <= end_index and current_sum > 0:
+        while current_sum < min_sum:
             min_sum = min(min_sum, current_sum)
             current_sum -= nums[start_index]
             start_index += 1
