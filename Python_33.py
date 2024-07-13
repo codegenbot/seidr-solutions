56be76638f7dde6adbf6652bcb6af7bb6fbd30d4
beep boop <githubbot@vadim.me>
2024-07-13 06:11:35 +0200

    Solution 76 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mmyso7cs Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4b76b494-6ddb-45b6-82c2-2dc763fdf fec.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,2 +1,3 @@
 def sort_third(lst):
-    return tuple(sorted([x[2] for x in lst if isinstance(x, (list, tuple)) and len(x) > 2]))
+    result = [x[2] for x in lst if isinstance(x, (list, tuple)) and len(x) > 2]
+    return tuple(sorted(result))
