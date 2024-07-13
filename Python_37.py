9ff9e2007fc35c8d60747a10a967b342c5708331
beep boop <githubbot@vadim.me>
2024-07-13 03:55:10 +0200

    Solution 44 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tcf481c2 Prompt: [2, 4] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/445c7ab5-1291-4fa0-a203-1ad12d5cd 27d.py", line 24, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/445c7ab5-1291-4fa0-a203-1ad12d5cd 27d.py", line 19, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,3 +1,4 @@
+```
 def sort_even(l: list):
     even_nums = sorted([x for x in l if x % 2 == 0])
     odd_nums = [x for x in l if x % 2 != 0]
@@ -10,8 +11,10 @@ def sort_even(l: list):
         else:
             result.append(odd_nums[j])
             j += 1
-    result.extend(even_nums[i:])
+    while i < len(even_nums):
+        result.append(even_nums[i])
+        i += 1
     return result
 
 if __name__ == "__main__":
-    print(sort_even([5, 3, 2, 7, 11, 1, 4]))
+    print(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10]))
