64cd115bed92369b46fb557b1d8c4f450d873965
beep boop <githubbot@vadim.me>
2024-07-13 19:47:29 +0200

    Solution 56 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/mfr89066 Prompt: 7bde2405-c46e-4f91-b19a-79b3741ca054.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string validateTweet(string tweet) {       | ^~~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -1,6 +1,7 @@
-#include <string>
+using namespace std;
 
-string validateTweet(string tweet) {
+std::string validateTweet(std::string tweet) {
     if(tweet.empty()) return "You didn't type anything";
     else if(tweet.length() > 140) return "Too many characters";
     else return "Your tweet has " + to_string(tweet.length()) + " characters";
+}
