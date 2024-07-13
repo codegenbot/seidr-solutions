c1e9001750ac80bf8dc7343198f2f1b07be7eaef
beep boop <githubbot@vadim.me>
2024-07-13 10:44:13 +0200

    Solution 86 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/5nnxhoku Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8e8d769b-b5c2-4763-a7e2-c82da6a5b 87e.py", line 10, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8e8d769b-b5c2-4763-a7e2-c82da6a5b 87e.py", line 7, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,4 +1,4 @@
 ```
 def sorted_list_sum(lst):
-    numbers = [int(c) for s in lst for c in s if c.isdigit()]
-    return [''.join(map(str, sorted(map(int, s)))) for s in set(tuple(s) for s in lst)]
+    numbers = [s for s in lst if s.isdigit()]
+    return [''.join(sorted(s)) for s in set(''.join(numbers))]
