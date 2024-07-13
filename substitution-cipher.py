057ed3894b3347fd8fa5a62a72146ed0e400426c
beep boop <githubbot@vadim.me>
2024-07-13 10:52:10 +0200

    Solution 32 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/gkja9pbr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/87d5f5e0-03a6-4338-b5e9-a8534cea4 8e9.py", line 10, in <module>     print(substitution_cipher())           ^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/87d5f5e0-03a6-4338-b5e9-a8534cea4 8e9.py", line 2, in substitution_cipher     cipher_map = [c for c1, c2 in zip(input().strip(), input().strip())]                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/87d5f5e0-03a6-4338-b5e9-a8534cea4 8e9.py", line 2, in <listcomp>     cipher_map = [c for c1, c2 in zip(input().strip(), input().strip())]                   ^ NameError: name 'c' is not defined. Did you mean: 'c1'?

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -1,4 +1,7 @@
 def substitution_cipher():
-    cipher_map = [k for k1, k2 in zip(input().strip(), input().strip())]
+    cipher_map = []
+    for c1, c2 in zip(input().strip(), input().strip()):
+        cipher_map.append(c2)
+    
     message = input().strip()
-    return "".join([c if c in cipher_map else "" for c in message])
+    return "".join([c for c in message if c in cipher_map])
