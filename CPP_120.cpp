d50f3e4f2440b53982c376a48ce7f676b8cf14da
beep boop <githubbot@vadim.me>
2024-07-13 08:04:12 +0200

    Solution 33 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/nb6n9j46 Prompt: Complete the following code given the task description and function signature.

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,4 +1,4 @@
-vector<int> maximum(vector<int> arr,int k){
+vector<int> maximum(vector<int> arr, int k) {
     vector<int> result(arr.begin(), arr.begin() + k);
     sort(result.begin(), result.end());
     return result;
