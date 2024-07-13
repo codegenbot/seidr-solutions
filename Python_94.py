07e8c0dca76e3a9dcdbf02ea59041181dd463883
beep boop <githubbot@vadim.me>
2024-07-13 07:17:44 +0200

    Solution 1 of Python/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/1gdmoaid Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6963e791-1009-4e04-9acb-f8d4149ec 334.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_94.py
+++ Python_94.py
@@ -1,5 +1,5 @@
-```
-def skjkasdkd(lst):
+def skjkasdkd():
+    num = int(input())
     def is_prime(n):
         if n < 2:
             return False
@@ -8,5 +8,5 @@ def skjkasdkd(lst):
                 return False
         return True
 
-    max_prime = max([i for i in lst if is_prime(i)])
+    max_prime = max([i for i in range(num+1) if is_prime(i)])
     return sum(int(digit) for digit in str(max_prime))
