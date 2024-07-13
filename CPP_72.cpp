7447ea491dd96ca1c0dbba373322d815f63c3595
beep boop <githubbot@vadim.me>
2024-07-13 02:28:33 +0200

    Solution 5 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/tbfgq7vb Prompt: Complete the following code given the task description and function signature.

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,12 +1,13 @@
 Here is the completed code:
 
 bool will_it_fly(vector<int> q, int w) {
-    bool balanced = true;
-    for (int i = 0; i < q.size() / 2; i++) {
-        if (q[i] != q[q.size() - i - 1]) {
-            balanced = false;
-            break;
-        }
+    string str = "";
+    for (int i : q) {
+        str += to_string(i);
     }
-    return balanced &&accumulate(q.begin(), q.end(), 0) <= w;
+    if (!str.compare(to_string(str))) {
+        return accumulate(q.begin(), q.end(), 0) <= w;
+    } else {
+        return false;
+    }
 }
