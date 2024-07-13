e50f1b9a049eecae9d3be9fc161c4288e43b2c16
beep boop <githubbot@vadim.me>
2024-07-13 20:19:47 +0200

    Solution 44 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/hz6k4g9f Prompt: f439a8a9-3ed3-4bfb-ad99-256a99e5c296.cpp: In function ‘std::vector<std::vector<i nt> > cutVector(std::vector<int>&)’: f439a8a9-3ed3-4bfb-ad99-256a99e5c296.cpp:9:20: error: ‘abs’ was not declared in this scope     9 |         int diff = abs(nums[i] - nums[i + 1]);       |                    ^~~ f439a8a9-3ed3-4bfb-ad99-256a99e5c296.cpp:47:27: error: ‘INT_MAX’ was not declare d in this scope    47 |             int minDiff = INT_MAX;       |                           ^~~~~~~ f439a8a9-3ed3-4bfb-ad99-256a99e5c296.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <vector>
 #include <limits>
 #include <cmath>
@@ -9,6 +10,8 @@ vector<vector<int>> cutVector(vector<int> &nums) {
     vector<vector<int>> res(2);
     
     for (int i = 0; i < n - 1; i++) {
+        int diff = abs(nums[i] - nums[i + 1]);
+        
         if ((i == 0 || nums[0] == nums[i]) && (n - 2 == i || nums[n - 1] == nums[i])) {
             res[0].clear();
             for (int j = 0; j <= i; j++) {
@@ -18,7 +21,7 @@ vector<vector<int>> cutVector(vector<int> &nums) {
             for (int j = i + 1; j < n; j++) {
                 res[1].push_back(nums[j]);
             }
-        } else if (abs(nums[i] - nums[i + 1]) == 0) {
+        } else if (diff == 0) {
             res[0].clear();
             for (int j = 0; j <= i; j++) {
                 res[0].push_back(nums[j]);
@@ -27,7 +30,16 @@ vector<vector<int>> cutVector(vector<int> &nums) {
             for (int j = i + 1; j < n; j++) {
                 res[1].push_back(nums[j]);
             }
-        } else if ((i == 0 || nums[i] - nums[0] <= abs(nums[i] - nums[i + 1])) && (n - 2 == i || nums[n - 1] - nums[i] <= abs(nums[i] - nums[i + 1]]))) {
+        } else if ((i == 0 || nums[0] - nums[i] <= diff) && (n - 2 == i || nums[n - 1] - nums[i] <= diff)) {
+            res[0].clear();
+            for (int j = 0; j <= i; j++) {
+                res[0].push_back(nums[j]);
+            }
+            res[1].clear();
+            for (int j = i + 1; j < n; j++) {
+                res[1].push_back(nums[j]);
+            }
+        } else if ((i == 0 || nums[i] - nums[0] <= diff) && (n - 2 == i || nums[n - 1] - nums[i] <= diff)) {
             res[0].clear();
             for (int j = 0; j < i; j++) {
                 res[0].push_back(nums[j]);
@@ -36,16 +48,40 @@ vector<vector<int>> cutVector(vector<int> &nums) {
             for (int j = i; j < n; j++) {
                 res[1].push_back(nums[j]);
             }
-        } else {
+        } else if (diff <= INT_MAX) {
             int minDiff = INT_MAX;
             int pos = -1;
+            
             for (int i2 = 0; i2 < n - 1; i2++) {
                 int diff2 = abs(nums[i2] - nums[i2 + 1]);
+                
                 if (diff2 < minDiff) {
                     minDiff = diff2;
                     pos = i2;
                 }
             }
+            
+            res[0].clear();
+            for (int j = 0; j <= pos; j++) {
+                res[0].push_back(nums[j]);
+            }
+            res[1].clear();
+            for (int j = pos + 1; j < n; j++) {
+                res[1].push_back(nums[j]);
+            }
+        } else {
+            int minDiff = INT_MAX;
+            int pos = -1;
+            
+            for (int i2 = 0; i2 < n - 1; i2++) {
+                int diff2 = abs(nums[i2] - nums[i2 + 1]);
+                
+                if (diff2 < minDiff) {
+                    minDiff = diff2;
+                    pos = i2;
+                }
+            }
+            
             res[0].clear();
             for (int j = 0; j <= pos; j++) {
                 res[0].push_back(nums[j]);
