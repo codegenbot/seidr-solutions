75000125ee40cd5097237ce11775cd18277b5874
beep boop <githubbot@vadim.me>
2024-07-13 06:49:54 +0200

    Solution 84 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/hv1pyxj2 Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/451c1023-e7fb-49e3-94c9-af64bbcda 799.py", line 20, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/451c1023-e7fb-49e3-94c9-af64bbcda 799.py", line 17, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,3 +1,4 @@
+```
 def fizz_buzz(n: int):
     count = 0
     for i in range(1, n + 1):
@@ -5,17 +6,11 @@ def fizz_buzz(n: int):
         if i % 15 == 0:
             output = "FizzBuzz"
             count += 1
+        elif i % 3 == 0:
+            output = "Fizz"
         elif i % 5 == 0:
-            if i % 3 == 0:
-                output = "FizzBuzz"
-            else:
-                output = "Buzz"
+            output = "Buzz"
         else:
-            if i % 3 == 0:
-                output = "Fizz"
-            elif i % 5 == 0:
-                output = "Buzz"
-            else:
-                output = str(i)
+            output = str(i)
         print(output)
     return count
