26c27bb00efa7d2b03f1552b20ab95eb9d48aee9
beep boop <githubbot@vadim.me>
2024-07-13 12:22:48 +0200

    Solution 28 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/io622y1d Prompt:  this scope    42 |     cout << "Enter the Boolean expression: ";       |     ^~~~ 1ab76f18-ca2e-4c91-802e-71e754354863.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; 1ab76f18-ca2e-4c91-802e-71e754354863.cpp:43:5: error: ‘cin’ was not declared in this scope    43 |     cin >> expression;       |     ^~~ 1ab76f18-ca2e-4c91-802e-71e754354863.cpp:43:5: note: ‘std::cin’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’? 1ab76f18-ca2e-4c91-802e-71e754354863.cpp:45:58: error: ‘endl’ was not declared i n this scope    45 |     cout << "Result: " << (result ? "True" : "False") << endl;       |                                                          ^~~~ 1ab76f18-ca2e-4c91-802e-71e754354863.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,39 +1,47 @@
-```cpp
-#include <string>
-#include <stack>
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-bool solveBoolean(string s) {
-    stack<char> st;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '&') {
-            while (!st.empty() && st.top() == '&') {
-                st.pop();
+bool evaluateBooleanExpression(string expression) {
+    stack<char> operatorStack;
+    for (int i = 0; i < expression.length(); i++) {
+        if (expression[i] == '&' || expression[i] == '|') {
+            while (!operatorStack.empty() && expression[i-1] != '(' && (expression[i-1] == '&' || expression[i-1] == '|')) {
+                if (expression[i-1] == '&') {
+                    operatorStack.pop();
+                    i--;
+                    break;
+                }
+                else if (expression[i-1] == '|') {
+                    operatorStack.pop();
+                    i--;
+                    break;
+                }
             }
-            if (st.empty()) return false;
-        } else if (s[i] == '|') {
-            while (!st.empty() && st.top() == '|') {
-                st.pop();
+            operatorStack.push(expression[i]);
+        }
+        else if (expression[i] == 'T' || expression[i] == 'F') {
+            int j = i + 1;
+            while (j < expression.length() && (expression[j] == '&' || expression[j] == '|')) {
+                j++;
             }
-            if (st.empty()) return true;
-        } else {
-            st.push(s[i]);
+            string operand = expression.substr(i, j - i);
+            if (operand == "T") return true;
+            else if (operand == "F") return false;
         }
     }
-    return !st.empty();
+    while (!operatorStack.empty()) {
+        if (expression[0] == '&') return false;
+        else if (expression[0] == '|') return true;
+    }
+    return true; // Default value
 }
 
 int main() {
-    string s;
-    cout << "Enter a boolean expression: ";
-    cin >> s;
-    bool result = solveBoolean(s);
-    if (result) {
-        cout << "True" << endl;
-    } else {
-        cout << "False" << endl;
-    }
+    string expression;
+    cout << "Enter the Boolean expression: ";
+    cin >> expression;
+    bool result = evaluateBooleanExpression(expression);
+    cout << "Result: " << (result ? "True" : "False") << endl;
     return 0;
 }
