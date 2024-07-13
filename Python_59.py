fe2f8360160fae42bed07336210e464b10fa227d
beep boop <githubbot@vadim.me>
2024-07-13 07:21:04 +0200

    Solution 26 of Python/59, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ydxdl1kc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ff16ed7b-df20-4952-a3a6-4752721bd 961.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_59.py
+++ Python_59.py
@@ -1,5 +1,5 @@
 ```
-`def largest_prime_factor(n):
+def largest_prime_factor(n):
     i = n
     while i > 1:
         if n % i == 0:
@@ -9,19 +9,7 @@
             if j == 1:
                 return i
         i -= 1
-    return n`
-
-`def largest_prime_factor(n):
-    i = n
-    while i > 1:
-        if n % i == 0:
-            j = i
-            while j % i == 0:
-                j //= i
-            if j == 1:
-                return i
-        i -= 1
-    return n`
+    return n
 
 while True:
     try:
