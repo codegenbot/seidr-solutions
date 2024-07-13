1768f36e181af9537f9a4fcf3c9b544854bb4d9e
beep boop <githubbot@vadim.me>
2024-07-13 19:51:55 +0200

    Solution 2 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/rg5l29f8 Prompt: Given an integer x, return "Fizz" if x is divisible by 3, "Buzz" if x is divisible by 5, "FizzBuzz" if xis divisible by 3 and 5, and a string version of x if none of the above hold.

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,42 +1,14 @@
 #include <string>
-using namespace std;
 
-class Solution {
-public:
-    string numberToWords(int num) {
-        if (num == 0)
-            return "0";
-        
-        map<int, string> m = {{1, "One"}, {2, "Two"}, {3, "Three"}, 
-                               {4, "Four"}, {5, "Five"}, {6, "Six"}, 
-                               {7, "Seven"}, {8, "Eight"}, {9, "Nine"}, 
-                               {10, "Ten"}, {11, "Eleven"}, {12, "Twelve"},
-                               {13, "Thirteen"}, {14, "Fourteen"}, {15, "Fifteen"},
-                               {16, "Sixteen"}, {17, "Seventeen"}, {18, "Eighteen"},
-                               {19, "Nineteen"}, {20, "Twenty"}, {30, "Thirty"}, 
-                               {40, "Forty"}, {50, "Fifty"}, {60, "Sixty"}, 
-                               {70, "Seventy"}, {80, "Eighty"}, {90, "Ninety"}};
-        
-        if (num >= 100) {
-            string temp = numberToWords(num / 100) + " Hundred";
-            if ((num % 100) != 0)
-                temp += " and " + numberToWords(num % 100);
-            return temp;
-        }
-        else if (num < 20)
-            return m[num];
-        
-        int i = 20;
-        while (i <= num) {
-            if ((num % i) == 0)
-                return m[i];
-            i += 10;
-        }
-        
-        if ((num % 10) == 0)
-            return m[num];
-        
-        string temp = numberToWords(num / 10 * 10);
-        return temp + " " + m[num % 10];
-    }
-};
+string fizzBuzz(int x) {
+    string result = "";
+    if (x % 3 == 0 && x % 5 == 0)
+        result = "FizzBuzz";
+    else if (x % 3 == 0)
+        result = "Fizz";
+    else if (x % 5 == 0)
+        result = "Buzz";
+    else
+        result = to_string(x);
+    return result;
+}
