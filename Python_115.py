14c2b2ffd60ea1a25c01456279f5f5d801dfa482
beep boop <githubbot@vadim.me>
2024-07-13 07:56:02 +0200

    Solution 8 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/vysg0lyt Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dd481072-4bed-4aee-9788-c3f193c34 4a6.py", line 21, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dd481072-4bed-4aee-9788-c3f193c34 4a6.py", line 18, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_115.py
+++ Python_115.py
@@ -1,3 +1,4 @@
+```
 import math
 
 
@@ -5,13 +6,10 @@ def max_fill(grid, capacity):
     total_water = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if i == 0 or j == 0:
-                total_water += (
-                    min(grid[i][j], grid[0][j], grid[i][0], capacity) - grid[i][j]
-                )
-            else:
-                total_water += min(grid[i][j], grid[i - 1][j], grid[0][j - 1]) - max(
-                    0, min(grid[i][j], grid[i - 1][j], grid[0][j - 1])
-                )
+            w = min(grid[i][j], grid[0][j], grid[i][0], capacity) - (
+                grid[i][j] if i == 0 or j == 0 else max(0, min(grid[i][j], grid[i - 1][j], grid[0][j - 1]))
+            )
+            total_water += w
+            capacity -= w if w > 0 else 0
 
     return math.ceil(total_water / capacity)
