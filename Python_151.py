31f4d377b97980102a2e5dced0a0c901362ac515
beep boop <githubbot@vadim.me>
2024-07-13 12:18:47 +0200

    Solution 85 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/etfjqq64 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6f0dc382-5ced-4a4c-8511-7f31a0059 f7b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -4,17 +4,13 @@ def double_the_difference(lst):
     else:
         return "No numbers entered. Please enter at least two numbers."
 
-
 def check():
     while True:
         input_str = input("Enter first and second set of numbers (space-separated): ")
-        if input_str:
+        if input_str: 
             try:
                 numbers = list(map(int, input_str.split()))
                 print(double_the_difference(numbers))
                 break
             except ValueError:
                 print("Invalid input. Please enter space-separated numbers.")
-
-
-check()
