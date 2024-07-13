3f91df80adc7115d543f5b1c9aca8805b785a87a
beep boop <githubbot@vadim.me>
2024-07-13 02:20:42 +0200

    Solution 26 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0eq98ygb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3fc0599e-9acb-4863-980c-eb678e468 2a4.py", line 20, in <module>     check(rescale_to_unit)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3fc0599e-9acb-4863-980c-eb678e468 2a4.py", line 11, in check     assert rescale_to_unit([12.0, 11.0, 15.0, 13.0, 14.0]) == [            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_21.py
+++ Python_21.py
@@ -7,4 +7,4 @@ def rescale_to_unit(inputs: list) -> list:
     max_val = max(inputs)
     rescaled_numbers = [(x - min_val) / (max_val - min_val) for x in inputs]
 
-    return [str(x) for x in rescaled_numbers]
+    return [str(round(x)) for x in rescaled_numbers]
