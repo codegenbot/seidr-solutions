e503474009a214ca18278eb7d6c00746c120debf
beep boop <githubbot@vadim.me>
2024-07-13 08:48:25 +0200

    Solution 81 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/8xd1obee Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from d24a698a-5223-47cc-9799-00fa4feafb5f.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<const char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ d24a698a-5223-47cc-9799-00fa4feafb5f.cpp:22:24:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,20 +1,27 @@
+```cpp
+#include <string>
+#include <algorithm>
+
 int mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
 
-    // Create a character array or vector of unique characters in the code.
     char code_chars[6] = {'A', 'B', 'C', 'D', 'E', 'F'};
-    std::vector<char> code_vector(code.begin(), code.end());
-
     for(int i=0; i<4; i++) {
         if(code[i] == guess[i]) {
             black++;
         }
-        else if(std::count(code_chars, code_chars+6, guess[i]) > 0 || 
-               std::find(code_vector.begin(), code_vector.end(), guess[i]) != code_vector.end()) {
+        else if(std::count(code_chars, code_chars+6, guess[i]) > 0) {
             white++;
         }
     }
 
     return black;
+}
+
+int main() {
+    std::string code = "ABCD";
+    std::string guess = "ABDC";
+    int result = mastermind(code, guess);
+    return 0;
 }
