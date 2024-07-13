4be6e2dd940a28ec31d6e4b1764362c2baf9d3d9
beep boop <githubbot@vadim.me>
2024-07-13 10:49:14 +0200

    Solution 59 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/t1btkm1f Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bb6c9669-e55a-4128-9f25-9aeaea357 84c.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- luhn.py
+++ luhn.py
@@ -1,14 +1,14 @@
-```Python
+```
 def luhn(card_number):
     card_number = [int(x) for x in str(card_number)]
     sum = 0
-    doubled = False
     for i, digit in enumerate(card_number):
-        if i % 2 == 1:
-            digit *= 2
-            if digit > 9:
-                digit -= 9
-        sum += digit
+        if (i % 2 == 1 and digit * 2 >= 10):
+            sum += digit * 2 - 9
+        elif i % 2 == 1:
+            sum += digit * 2
+        else:
+            sum += digit
     return sum
 
 card_number = int(input("Enter the credit card number: "))
