fee83fda2d66800800ca1d4df109d2f22d099887
beep boop <githubbot@vadim.me>
2024-07-13 09:21:26 +0200

    Solution 92 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/vl9bkmc7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/282aa571-cd37-46ba-82ee-e7c657924 51f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- mastermind.py
+++ mastermind.py
@@ -1,3 +1,4 @@
+```python
 def mastermind(code, guess):
     black_peg = 0
     white_peg = 0
@@ -5,8 +6,8 @@ def mastermind(code, guess):
     for i in range(4):
         if code[i] == guess[i]:
             black_peg += 1
-        elif str(guess[i]) in code:
+        elif guess.count(guess[i]) > 0:
             white_peg += 1
-            code = code.replace(str(guess[i]), "", 1)
+            guess = guess.replace(str(guess[i]), '', 1)
 
     return str(black_peg), str(white_peg)
