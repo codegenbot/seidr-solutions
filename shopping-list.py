5ba4433d078a06e977366642872b900c411547f4
beep boop <githubbot@vadim.me>
2024-07-14 01:17:51 +0200

    Solution 88 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/kgj5ftzp Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7c733e9b-c0cc-4667-b0f7-86da146a3 e6c.py", line 1     """     ^ SyntaxError: unterminated triple-quoted string literal (detected at line 13)

--- shopping-list.py
+++ shopping-list.py
@@ -1,3 +1,4 @@
+```
 n = int(input("Enter the number of goods: "))
 if n < 1:
     print("Error: Number of goods should be greater than zero.")
@@ -10,4 +11,3 @@ else:
             exit()
         total_price += sum([p - (p * int(d) / 100) for p, d in zip(prices_discounts[::2], prices_discounts[1::2])])
     print(format(total_price, ".2f"))
-"""
