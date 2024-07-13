83ec6cbb7830d516208aeecb3b5f1465c998425c
beep boop <githubbot@vadim.me>
2024-07-14 00:18:07 +0200

    Solution 10 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/n1rydnou Prompt: 17b13929-b3bf-466f-b092-400fbd546c56.cpp:4:18: error: ‘string’ was not declared in this scope     4 | int bowlingScore(string s) {       |                  ^~~~~~ 17b13929-b3bf-466f-b092-400fbd546c56.cpp:4:18: note: suggested alternatives: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 17b13929-b3bf-466f-b092-400fbd546c56.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 17b13929-b3bf-466f-b092-400fbd546c56.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -6,14 +6,20 @@ int bowlingScore(string s) {
     int score = 0;
     stringstream ss(s);
     char c;
+    string temp;
     while (ss >> c) {
         if (c == 'X') {
             score += 30;
         } else if (c == '/') {
             score += 10 + (score / 10 - 1);
         } else {
-            int temp = c - '0';
-            score += temp;
+            temp = "";
+            while (c != '/' && c != 'X') {
+                temp += c;
+                ss >> c;
+            }
+            int val = stoi(temp);
+            score += val;
         }
     }
     return score;
