15ff0e4c16e26345eee40062d4294bf44c8caa7b
beep boop <githubbot@vadim.me>
2024-07-13 17:59:39 +0200

    Solution 74 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/1hkra7m8 Prompt: Complete the following code given the task description and function signature.

--- Python_115.py
+++ Python_115.py
@@ -1,4 +1,4 @@
 def max_fill(grid, capacity):
-    max_water = sum(sum(row) for row in grid)
-    total_buckets = math.ceil(max_water / float(capacity))
+    total_water = sum(sum(row) for row in grid)
+    total_buckets = math.ceil(total_water / float(capacity))
     return total_buckets
