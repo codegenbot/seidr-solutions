4fab27b7570a636d77c1a97d7042c250eab98407
beep boop <githubbot@vadim.me>
2024-07-13 17:45:29 +0200

    Solution 60 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/uewad9le Prompt:     3 |     cin >> code >> guess;       |     ^~~ eaae28c4-5818-43b2-83ab-ff1efec1fdba.cpp:3:12: error: ‘code’ was not declared in  this scope     3 |     cin >> code >> guess;       |            ^~~~ eaae28c4-5818-43b2-83ab-ff1efec1fdba.cpp:3:20: error: ‘guess’ was not declared i n this scope     3 |     cin >> code >> guess;       |                    ^~~~~ eaae28c4-5818-43b2-83ab-ff1efec1fdba.cpp:19:22: error: ‘min’ was not declared in  this scope; did you mean ‘main’?    19 |         whitePegs += min(codeFreq[i], guessFreq[i]);       |                      ^~~       |                      main eaae28c4-5818-43b2-83ab-ff1efec1fdba.cpp:22:5: error: ‘cout’ was not declared in  this scope    22 |     cout << whitePegs << endl;       |     ^~~~ eaae28c4-5818-43b2-83ab-ff1efec1fdba.cpp:22:26: error: ‘endl’ was not declared i n this scope    22 |     cout << whitePegs << endl;       |                          ^~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
 int main() {
@@ -21,7 +20,7 @@ int main() {
     }
 
     for (int i = 0; i < 6; ++i) {
-        whitePegs += std::min(codeFreq[i], guessFreq[i]);
+        whitePegs += min(codeFreq[i], guessFreq[i]);
     }
 
     cout << whitePegs << endl;
