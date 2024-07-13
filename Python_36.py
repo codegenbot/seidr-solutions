dc7c1ef9f77db9e4b67db3fc68a2dbdab45b0741
beep boop <githubbot@vadim.me>
2024-07-13 04:12:46 +0200

    Solution 34 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/hv1pyxj2 Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2f0b5a11-b6e7-4d04-93c9-1589faf6d fcf.py", line 18, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2f0b5a11-b6e7-4d04-93c9-1589faf6d fcf.py", line 15, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,4 +1,3 @@
-```
 def fizz_buzz(n: int):
     count = 0
     for i in range(1, n + 1):
@@ -6,19 +5,11 @@ def fizz_buzz(n: int):
             print("FizzBuzz")
             count += 1
         elif i % 3 == 0:
-            s = "Fizz"
             if i % 5 != 0:
-                print(s)
-            else:
-                print("FizzBuzz")
-                count += 1
+                print("Fizz")
         elif i % 5 == 0:
-            s = "Buzz"
             if i % 3 != 0:
-                print(s)
-            else:
-                print("FizzBuzz")
-                count += 1
+                print("Buzz")
         else:
             print(i)
     return count
