c621972e016b59b49e8307cdcbe1196ff59e1bdc
beep boop <githubbot@vadim.me>
2024-07-13 23:17:31 +0200

    Solution 2 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/drhe2edt Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,50 +1,19 @@
-#include <vector>
-#include <iostream>
 #include <string>
+#include <sstream>
 
-using namespace std;
+std::string spinWords(std::string str) {
+    std::stringstream ss(str);
+    std::string word;
+    std::stringstream result;
 
-string spinWords(string str) {
-    string result = "";
-    int wordLength;
-    string word;
-
-    for (int i = 0; i <= str.length() - 1; i++) {
-        if (str[i] == ' ') {
-            result += str.substr(i - wordLength + 1, wordLength) + " ";
-            wordLength = 0;
-        } else {
-            wordLength++;
-        }
+    while (ss >> word) {
+        if (word.length() >= 5)
+            for (int i = word.length() - 1; i >= 0; --i)
+                result << word[i];
+        else
+            result << word;
+        result << ' ';
     }
 
-    result += str.substr(str.length() - wordLength);
-    
-    for(int i = 0; i < result.length(); i++) {
-        if(result[i] == ' ') {
-            string temp = result.substr(i + 1);
-            for(long long j = temp.length() - 1; j >= 0; j--) {
-                result.replace(i, temp.length(), temp.substr(j));
-                i += temp.length();
-                break;
-            }
-        } else if(result[i+1] == ' ') {
-            string temp = result.substr(i + 1);
-            for(long long j = temp.length() - 1; j >= 0; j--) {
-                result.replace(i + 1, temp.length(), temp.substr(j));
-                i += temp.length();
-                break;
-            }
-        }
-    }
-
-    return result;
-}
-
-int main() {
-    string str;
-    cout << "Enter a sentence: ";
-    getline(cin, str);
-    cout << spinWords(str) << endl;
-    return 0;
+    return result.str();
 }
