216fd040794fde95d7ca864f5809955aff4e50cf
beep boop <githubbot@vadim.me>
2024-07-13 17:57:11 +0200

    Solution 31 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/lvw7csn9 Prompt: 3a45c618-e42b-4cee-9be9-cd94274a283d.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -7,29 +7,35 @@ int bowlingScore(string s) {
     for (int frame = 0; frame < 10; ++frame) {
         if (s[5*frame] == '/') {
             score += 10;
-            if (frame != 9) {
-                if (s[5*(frame+1)] == 'X' || s[5*(frame+1)] - '0' + s[5*(frame+1)+1] - '0' <= 10)
-                    score += s[5*(frame+1)] - '0';
+            roll1 = 10 - (s[5*frame + 1] - '0');
+            if (s[10*frame+1] == 'X' || s[10*frame+2] == '/') {
+                score += roll1 * 2;
+            } else {
+                score += roll1 + (s[5*frame + 1] - '0');
             }
-        }
-
-        if (s[5*frame] == 'X') {
+        } 
+        else if (s[5*frame] == 'X') {
             score += 10;
-            roll1 = 10;
-            if (frame != 9) {
-                if (s[5*(frame+1)] == '/') || s[5*(frame+1)] - '0' + s[5*(frame+1)+1] - '0' <= 10)
-                    score += s[5*(frame+1)] - '0';
-                else
-                    roll2 = s[5*(frame+1)] - '0' + s[5*(frame+1)+1] - '0';
+            if (frame < 8) {
+                if (s[10*frame+1] == 'X') {
+                    score += 10;
+                } 
+                else if (s[10*frame+1] == '/') {
+                    score += 10 + (s[10*frame+1] - '0');
+                }
+                else {
+                    roll2 = s[10*frame+1] - '0';
+                    score += 10 + roll2;
+                }
             }
-        }
-
-        if (s[5*frame] >= '1' && s[5*frame] <= '9') {
+        } 
+        else {
             roll1 = s[5*frame] - '0';
-            if (s[5*frame + 1] == '/') || s[5*frame + 1] - '0' + s[5*frame + 2] - '0' > 10)
-                score += roll1 + 10;
-            else
-                score += roll1 + s[5*frame+1] - '0';
+            if (roll1 + (s[5*frame + 1] - '0') > 10) {
+                score += 10;
+            } else {
+                score += roll1 + (s[5*frame + 1] - '0');
+            }
         }
     }
 
