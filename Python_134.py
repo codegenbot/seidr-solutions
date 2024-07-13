d0d744041e836b5ecbf44ce5f397d5d92a512c59
beep boop <githubbot@vadim.me>
2024-07-13 11:25:38 +0200

    Solution 83 of Python/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fw253inm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1a234d5a-a14a-43df-ba60-91bad29b3 6a8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_134.py
+++ Python_134.py
@@ -1,7 +1,5 @@
-```
-def check_palindrome(input_string):
-    input_string = "".join(c for c in input_string if c.isalnum()).lower()
-    return input_string == input_string[::-1]
+def check_palindrome(s):
+    return s == s[::-1]
 
 if __name__ == "__main__":
     while True:
@@ -15,5 +13,4 @@ if __name__ == "__main__":
                 break
         except Exception as e:
             print(f"Unexpected error occurred: {e}")
-
-        print(check_palindrome(user_input))
+    print(check_palindrome(user_input))
