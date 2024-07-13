4df172727e38f4d6b914e6dfb3ca94c403a3cffa
beep boop <githubbot@vadim.me>
2024-07-13 16:42:03 +0200

    Solution 97 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/26hxb96n Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,28 +1,27 @@
-int bowlingScore(string s) {
+int bowlingScore(const string& s) {
     int score = 0;
-    int frame = 0;
+    int frame = 1;
     int i = 0;
-    while (i < s.size() && frame < 10) {
+
+    while (frame <= 10) {
         if (s[i] == 'X') {
             score += 10;
             if (s[i + 2] == 'X') {
                 score += 10 + (s[i + 4] == 'X' ? 10 : (s[i + 4] - '0'));
-            } else if (s[i + 3] == '/') {
-                score += 10;
             } else {
-                score += (s[i + 2] == '/' ? 10 : (s[i + 2] - '0')) + (s[i + 3] == '/' ? 10 : (s[i + 3] - '0'));
+                score += (s[i + 2] == '/' ? 10 : (s[i+2]-'0') + (s[i+4] == '/' ? 10 : (s[i+4]-'0')));
             }
             i += 2;
         } else if (s[i + 1] == '/') {
-            score += 10;
-            score += (s[i + 2] == 'X' ? 10 : (s[i + 2] - '0'));
+            score += 10 + (s[i + 2] == 'X' ? 10 : (s[i + 2] - '0'));
             i += 3;
         } else {
-            score += (s[i] - '0') + (s[i + 1] - '0');
+            score += s[i] - '0' + s[i + 1] - '0';
             i += 2;
         }
         frame++;
     }
+
     return score;
 }
 
