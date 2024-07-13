7c3269a8e23dc141b284422915541ca662d8deac
beep boop <githubbot@vadim.me>
2024-07-14 00:01:00 +0200

    Solution 71 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/5vwkqgs9 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,52 +1,44 @@
-int bowlingScore(string frames) {
+int bowlingScore(string s) {
     int score = 0;
-    int currentRoll = 0;
-    int rollsInFrame = 0;
+    int frame = 0;
 
-    for (char frame : frames) {
-        if (isdigit(frame)) {
-            currentRoll++;
-            if (frame == 'X') {
-                score += 10 + bowlingScoreHelper(frames, currentRoll);
-                currentRoll = 0;
-            } else if (currentRoll == 2) {
-                int points = (frame - '0') * 2;
-                score += points;
-                currentRoll = 0;
-            }
-        } else {
-            rollsInFrame++;
-            if (rollsInFrame == 2) {
-                if (frames[currentRoll] == '/') {
-                    int firstRoll = frames[currentRoll-1] - '0';
-                    int secondRoll = frames[currentRoll+1] - '0';
-                    score += firstRoll + secondRoll;
-                    currentRoll++;
-                    rollsInFrame = 0;
-                } else {
-                    score += bowlingScoreHelper(frames, currentRoll);
-                    currentRoll = 0;
-                    rollsInFrame = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'X') {
+            score += 30;
+            frame++;
+        } else if (s[i] == '/') {
+            int strike = s.find('/', i);
+            int spare = s.find('X', i) - i;
+
+            if (strike != string::npos && spare != string::npos) {
+                if (strike < i + 2) {
+                    score += 10;
+                    for (int j = i; j <= i + 1; j++) {
+                        if (s[j] == 'X') {
+                            score += 30;
+                        } else if (s[j] == '/') {
+                            int pos = s.find('/', j);
+                            score += pos - j;
+                        }
+                    }
+                } else if (spare < i + 2) {
+                    score += 10 + s[i + spare];
                 }
             }
-        }
-    }
 
-    return score;
-}
-
-int bowlingScoreHelper(string frames, int start) {
-    int score = 0;
-    for (int i = start; i < start+2; i++) {
-        if (frames[i] == 'X') {
-            score += 10;
-        } else if (isdigit(frames[i])) {
-            score += (frames[i] - '0');
+            i = strike > 0 ? strike : i;
         } else {
-            int firstRoll = frames[i-1] - '0';
-            int secondRoll = frames[i+1] - '0';
-            score += firstRoll + secondRoll;
+            int pos = s.find('/', i);
+            int value = s[i] - '0';
+            if (pos == string::npos) {
+                score += value * 2;
+            } else {
+                score += value + s[pos - 1] - '0';
+            }
         }
+
+        frame++;
     }
+
     return score;
 }
