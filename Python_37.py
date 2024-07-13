0dcc9ea9dd00b7e267e2f6c134613bec5837852b
beep boop <githubbot@vadim.me>
2024-07-13 05:43:32 +0200

    Solution 78 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tcf481c2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c6dedf86-bf06-4b3a-8fbe-72d80e488 363.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_37.py
+++ Python_37.py
@@ -1,4 +1,3 @@
-```
 def sort_even(l: list):
     even_list = [i for i in l if i % 2 == 0]
     even_list.sort()
@@ -10,4 +9,5 @@ def check():
     print(sort_even(numbers))
 
 
-check()
+if __name__ == "__main__":
+    check()
