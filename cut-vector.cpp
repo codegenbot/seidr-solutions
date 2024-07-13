19bef7a6fb9f180109c1be8c944c39755a47d67a
beep boop <githubbot@vadim.me>
2024-07-13 21:19:13 +0200

    Solution 80 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/hz6k4g9f Prompt: 18f4bcb6-c6db-4017-819c-4784fc4e3724.cpp: In function ‘std::vector<std::vector<i nt> > cutVector(std::vector<int>)’: 18f4bcb6-c6db-4017-819c-4784fc4e3724.cpp:5:19: error: ‘INT_MAX’ was not declared  in this scope     5 |     int minDiff = INT_MAX;       |                   ^~~~~~~ 18f4bcb6-c6db-4017-819c-4784fc4e3724.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; 18f4bcb6-c6db-4017-819c-4784fc4e3724.cpp:8:20: error: ‘abs’ was not declared in this scope     8 |         int diff = abs(v[i] - v[i + 1]);       |                    ^~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,60 +1,20 @@
-#include <iostream>
 #include <vector>
+#include <climits> // for INT_MAX
+#include <cmath>  // for abs
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+vector<vector<int>> cutVector(vector<int> v) {
     int minDiff = INT_MAX;
-    int cutIndex = -1;
-    
-    for(int i = 0; i < v.size() - 1; i++) {
-        int leftSum = 0, rightSum = 0;
-        
-        for(int j = 0; j <= i; j++) {
-            leftSum += v[j];
-        }
-        
-        for(int j = i + 1; j < v.size(); j++) {
-            rightSum += v[j];
-        }
-        
-        int diff = abs(leftSum - rightSum);
-        
-        if(diff < minDiff) {
+    int idx = -1;
+    for (int i = 0; i < v.size() - 1; i++) {
+        int diff = abs(v[i] - v[i + 1]);
+        if (diff < minDiff) {
             minDiff = diff;
-            cutIndex = i;
+            idx = i;
         }
     }
-    
-    vector<int> leftVec, rightVec;
-    
-    for(int i = 0; i <= cutIndex; i++) {
-        leftVec.push_back(v[i]);
-    }
-    
-    for(int i = cutIndex + 1; i < v.size(); i++) {
-        rightVec.push_back(v[i]);
-    }
-    
-    return {leftVec, rightVec};
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for(auto &x : v) {
-        cin >> x;
-    }
-    pair<vector<int>, vector<int>> result = cutVector(v);
-    cout << "Left: ";
-    for(auto x : result.first) {
-        cout << x << " ";
-    }
-    cout << endl;
-    cout << "Right: ";
-    for(auto x : result.second) {
-        cout << x << " ";
-    }
-    cout << endl;
-    return 0;
+    vector<vector<int>> result(2);
+    result[0].insert(result[0].end(), v.begin(), v.begin() + idx + 1);
+    result[1].insert(result[1].begin(), v.begin() + idx, v.end());
+    return result;
 }
