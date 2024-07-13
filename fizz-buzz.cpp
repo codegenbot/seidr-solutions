4b7877c5f9f4578b9d7a9ed90031dfdfb5f960c5
beep boop <githubbot@vadim.me>
2024-07-13 20:46:04 +0200

    Solution 1 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/922ye60q Prompt: Given an integer x, return "Fizz" if x is divisible by 3, "Buzz" if x is divisible by 5, "FizzBuzz" if xis divisible by 3 and 5, and a string version of x if none of the above hold.

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,82 +1,22 @@
-#include <string>
+#include <iostream>
 using namespace std;
 
-class Solution {
-public:
-    string numberToWords(int num) {
-        if (num == 0)
-            return "0";
-        
-        string words = "";
-        if (num >= 1000000) {
-            int million = num / 1000000;
-            words += numberToWords(million) + " million";
-            num %= 1000000;
-        }
-        if (num >= 1000) {
-            int thousand = num / 1000;
-            words += numberToWords(thousand) + " thousand";
-            num %= 1000;
-        }
-        if (num >= 20) {
-            string word = getTensWord(num);
-            words += word;
-            num = 0;
-        } else if (num > 0) {
-            string word = getUnitsWord(num);
-            words += word;
-            num = 0;
-        }
-        
-        return words == "" ? to_string(num) : words;
-    }
+string fizzBuzz(int x) {
+    if (x % 3 == 0 && x % 5 == 0)
+        return "FizzBuzz";
+    else if (x % 3 == 0)
+        return "Fizz";
+    else if (x % 5 == 0)
+        return "Buzz";
+    else
+        return to_string(x);
+}
 
-private:
-    string getTensWord(int n) {
-        switch (n) {
-            case 20: return "twenty";
-            case 30: return "thirty";
-            case 40: return "forty";
-            case 50: return "fifty";
-            case 60: return "sixty";
-            case 70: return "seventy";
-            case 80: return "eighty";
-            case 90: return "ninety";
-        }
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        cout << fizzBuzz(i) << endl;
     }
-
-    string getUnitsWord(int n) {
-        switch (n) {
-            case 1: return "one";
-            case 2: return "two";
-            case 3: return "three";
-            case 4: return "four";
-            case 5: return "five";
-            case 6: return "six";
-            case 7: return "seven";
-            case 8: return "eight";
-            case 9: return "nine";
-            case 10: return "ten";
-            case 11: return "eleven";
-            case 12: return "twelve";
-            case 13: return "thirteen";
-            case 14: return "fourteen";
-            case 15: return "fifteen";
-            case 16: return "sixteen";
-            case 17: return "seventeen";
-            case 18: return "eighteen";
-            case 19: return "nineteen";
-        }
-    }
-
-    string fizzBuzz(int n) {
-        if (n % 3 == 0 && n % 5 == 0)
-            return "FizzBuzz";
-        else if (n % 3 == 0)
-            return "Fizz";
-        else if (n % 5 == 0)
-            return "Buzz";
-        else
-            return to_string(n);
-    }
-};
+    return 0;
+}
