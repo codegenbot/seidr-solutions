0855e5606caace896c7ac80443d4612e2716f2c5
beep boop <githubbot@vadim.me>
2024-07-13 14:05:28 +0200

    Solution 41 of fizz-buzz, ratio 0.56 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/4mrjr97j Prompt: Program failed

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main() {
     int x;
     cin >> x;
-    if (x % 15 == 0) {
-        cout << "FizzBuzz";
-    } else if (x % 3 == 0 && x % 5 != 0) {
-        cout << "Fizz";
-    } else if (x % 5 == 0 && x % 3 != 0) {
+    if (x % 3 == 0) {
+        if (x % 5 == 0)
+            cout << "FizzBuzz";
+        else
+            cout << "Fizz";
+    } else if (x % 5 == 0) {
         cout << "Buzz";
     } else {
         cout << to_string(x);
