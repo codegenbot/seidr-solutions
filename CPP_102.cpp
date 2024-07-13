3b1b4d0f3963b9162123904c1727256b38079f02
beep boop <githubbot@vadim.me>
2024-07-13 07:25:05 +0200

    Solution 2 of CPP/102, ratio 0.625 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/um10bapm Prompt: Complete the following code given the task description and function signature.

--- CPP_102.cpp
+++ CPP_102.cpp
@@ -1,14 +1,9 @@
 int choose_num(int x, int y) {
-    if (x % 2 == 0 && y % 2 == 0) {
-        return max(x, y);
-    } else if ((x % 2 != 0 && y % 2 == 0) || (x % 2 == 0 && y % 2 != 0)) {
+    if (x % 2 == 0 && x <= y) {
+        return x;
+    } else if (y % 2 == 0 && x < y) {
+        return y;
+    } else {
         return -1;
-    } else if (x % 2 != 0 && y % 2 != 0) {
-        int maxEven = (y > x) ? y : x;
-        while (maxEven % 2 != 0) {
-            maxEven--;
-        }
-        return maxEven;
     }
-    return -1;
 }
