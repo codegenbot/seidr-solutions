d5a5ab8394bbe1458808c72d750492881c0f86ed
beep boop <githubbot@vadim.me>
2024-07-13 06:18:43 +0200

    Solution 71 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/hv1pyxj2 Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9ad2a22c-8bbf-403b-b44a-0deaf46e1 571.py", line 20, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9ad2a22c-8bbf-403b-b44a-0deaf46e1 571.py", line 17, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,10 +1,16 @@
+```
 def fizz_buzz(n: int):
     count = 0
     for i in range(1, n + 1):
+        output = ""
         if i % 15 == 0:
+            output = "FizzBuzz"
             count += 1
         elif i % 3 == 0:
-            count = 0 if i % 5 != 0 else count + 1
+            output = "Fizz"
         elif i % 5 == 0:
-            count += 1 if i % 3 == 0 or i % 15 == 0 else 0
+            output = "Buzz"
+        else:
+            output = str(i)
+        print(output)
     return count
