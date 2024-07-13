f7f14bcea7beba15e22dc303d024f5d30c787d2a
beep boop <githubbot@vadim.me>
2024-07-13 12:38:59 +0200

    Solution 93 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/acp5mi4l Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,39 +1,28 @@
-int calculateBowlingScore(string str) {
+int calculateBowlingScore(string rolls) {
     int score = 0;
-    int frame = 0;
-    int frameScore[10] = {0};
-    for (int i = 0; i < str.size(); ++i) {
-        if (str[i] == 'X') {
-            frameScore[frame] += 10;
-            if (frame < 8) {
-                frameScore[frame] += frameScore[frame + 1];
-                if (str[i + 2] == 'X')
-                    frameScore[frame] += 10;
-                else if (str[i + 2] == '/')
-                    frameScore[frame] += 10 - (str[i + 1] - '0');
-                else
-                    frameScore[frame] += str[i + 1] - '0' + str[i + 2] - '0';
-            } else if (frame == 8) {
-                frameScore[8] += frameScore[9];
-                frameScore[8] += str[i + 1] == 'X' ? 10 : str[i + 1] - '0';
-                frameScore[8] += str[i + 2] == 'X' ? 10 : str[i + 2] == '/' ? 10 - (str[i + 1] - '0') : str[i + 2] - '0';
-            }
+    int frame = 1;
+    int rollIdx = 0;
+
+    for (int i = 0; i < rolls.size(); i++) {
+        if (frame > 10) break;
+
+        if (rolls[i] == 'X') {
+            score += 10;
+            score += (rolls[i+2] == 'X') ? 10 : (rolls[i+1] - '0') + (rolls[i+2] - '0');
+            rollIdx++;
             frame++;
-        } else if (str[i] == '/') {
-            frameScore[frame] += 10 - (str[i - 1] - '0');
+        } else if (rolls[i] == '/') {
+            score += 10 - (rolls[i-1] - '0');
+            score += (rolls[i+1] == 'X') ? 10 : (rolls[i+1] - '0');
+            rollIdx += 2;
+            frame++;
+        } else if (rolls[i] == '-') {
+            score += 0;
         } else {
-            frameScore[frame] += str[i] - '0';
+            score += rolls[i] - '0';
+            rollIdx++;
         }
     }
-    for (int i = 0; i < 10; ++i) {
-        score += frameScore[i];
-    }
-    return score;
-}
 
-int main() {
-    string str;
-    cin >> str;
-    cout << calculateBowlingScore(str) << endl;
-    return 0;
+    return score;
 }
