9831785330fce42dfc6d4461ccd3150282121cfb
beep boop <githubbot@vadim.me>
2024-07-14 01:19:51 +0200

    Solution 23 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/9xhxvl3d Prompt: bbda28cc-7e4a-4d90-b290-cab5b1b8c3a3.cpp: In function ‘std::pair<int, int> findP air(std::vector<int>&, int)’: bbda28cc-7e4a-4d90-b290-cab5b1b8c3a3.cpp:5:5: error: ‘unordered_map’ was not dec lared in this scope     5 |     unordered_map<int, int> numMap;       |     ^~~~~~~~~~~~~ bbda28cc-7e4a-4d90-b290-cab5b1b8c3a3.cpp:5:19: error: expected primary-expressio n before ‘int’     5 |     unordered_map<int, int> numMap;       |                   ^~~ bbda28cc-7e4a-4d90-b290-cab5b1b8c3a3.cpp:8:13: error: ‘numMap’ was not declared in this scope     8 |         if (numMap.find(complement) != numMap.end()) {       |             ^~~~~~ bbda28cc-7e4a-4d90-b290-cab5b1b8c3a3.cpp:11:9: error: ‘numMap’ was not declared in this scope    11 |         numMap[nums[i]] = i;       |         ^~~~~~

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,14 +1,11 @@
-```cpp
-#include <vector>
-#include <utility>
-using namespace std;
+#include <unordered_map>
 
-std::pair<int, int> findPair(vector<int>& nums, int target) {
-    unordered_map<int, int> numMap;
+std::pair<int, int> findPair(std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> numMap;
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
         if (numMap.find(complement) != numMap.end()) {
-            return {nums[i], complement};
+            return {complement, nums[i]};
         }
         numMap[nums[i]] = i;
     }
