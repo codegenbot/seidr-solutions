4f4315f63037b6899121a1235c927099269b9a6a
beep boop <githubbot@vadim.me>
2024-07-13 05:56:43 +0200

    Solution 86 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tcf481c2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/563f6b92-a2b2-43ed-87d7-34af79b17 669.py", line 2     Enter space-separated numbers: 2 4 6 8 10 1 3 5           ^^^^^ SyntaxError: invalid syntax

--- Python_37.py
+++ Python_37.py
@@ -1,7 +1,2 @@
-```
-def main():
-    numbers = list(map(int, input("Enter space-separated numbers: ").split()))
-    print(numbers)
-
-if __name__ == "__main__":
-    main()
+numbers = list(map(int, input("Enter space-separated numbers: ").split()))
+print(numbers)
