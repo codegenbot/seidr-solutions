ff39bac4fbc7b042028338c6be59bbdf6111cd68
beep boop <githubbot@vadim.me>
2024-07-13 17:37:21 +0200

    Solution 9 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ba2frpay Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,25 +1,27 @@
-Here is the Python solution for the bowling problem:
-
 ```
-def bowling_score(gutter_balls):
+def bowling_score(frames):
     score = 0
-    frame = 1
-    while frame <= 10:
-        if gutter_balls[frame-1] == 'X':
-            if frame < 10:
-                score += 30
-            else:
-                score += 10 + 10 + int(gutter_balls[frame-2])
-            frame += 1
-        elif '/' in gutter_balls[frame-1]:
-            left, right = map(int, gutter_balls[frame-1].split('/'))
-            if left == 10:
-                score += left + right
-                frame += 1
-            else:
-                score += left
-                frame += 1
+    frame_index = 0
+    while frame_index < len(frames):
+        if frames[frame_index] == 'X':
+            score += 30
+            frame_index += 1
+        elif frames[frame_index] in ['/', '-']:
+            score += int(frames[:frame_index].count('X')) * 10 + int(frames[frame_index-1: frame_index])
+            frame_index += 1
         else:
-            score += int(gutter_balls[frame-1])
-            frame += 1
-    return score
+            strike_count = 0
+            while frames[frame_index] != 'X' and frames[frame_index] != '/' and frames[frame_index] != '-':
+                score += int(frames[frame_index])
+                frame_index += 1
+            if frames[frame_index] == 'X':
+                score += 10 + int(frames[:frame_index].count('X')) * 10
+                strike_count += 1
+            elif frames[frame_index] in ['/','-']:
+                score += 10 + int(frames[:frame_index-1].count('X')) * 10
+                frame_index += 1
+            else:
+                if strike_count > 0:
+                    score += int(frames[frame_index])
+                    frame_index += 1
+        return score
