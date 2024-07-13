4fa529eabf9e2ccfb833c52348902be4e304da8e
beep boop <githubbot@vadim.me>
2024-07-13 06:20:46 +0200

    Solution 29 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/u5fnzvz7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5c6c1695-706a-4ed2-b3ed-513b99a6d 8dd.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_50.py
+++ Python_50.py
@@ -1,5 +1,6 @@
-```
+```python
 decode_shift = input("Enter the string: ")
+print(decrypt(decode_shift))
 
 def decrypt(decode_shift):
     return "".join(
@@ -12,5 +13,3 @@ def decrypt(decode_shift):
             for ch in decode_shift
         ]
     )
-
-print(decrypt(decode_shift))
