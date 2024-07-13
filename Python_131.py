fcaf9006e5f823d2a6093e5c14058530b03cbb82
beep boop <githubbot@vadim.me>
2024-07-13 18:13:29 +0200

    Solution 67 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/q9muy4c0 Prompt: Complete the following code given the task description and function signature.

--- Python_131.py
+++ Python_131.py
@@ -1,8 +1,7 @@
 def digits(n):
     product = 1
     while n:
-        digit = n % 10
-        if digit % 2 != 0:
-            product *= digit
-        n //= 10
+        n, remainder = divmod(n, 10)
+        if remainder % 2:
+            product *= remainder
     return product
