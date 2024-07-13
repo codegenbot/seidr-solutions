daf89a5a367afef99830efb6131e4fee02f46e6d
beep boop <githubbot@vadim.me>
2024-07-13 06:23:53 +0200

    Solution 78 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/5f0pdse2 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,29 +1,39 @@
 int main() {
-    vector<int> nums;
-    int num;
-    while (cin >> num) {
-        nums.push_back(num);
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> vec[i];
     }
-    
-    int left_sum = 0;
-    int right_sum = accumulate(nums.begin(), nums.end(), 0);
-    int min_diff = INT_MAX;
-    int cut_index = 0;
-    
-    for (int i = 0; i < nums.size(); i++) {
-        left_sum += nums[i];
-        right_sum -= nums[i];
-        int current_diff = abs(left_sum - right_sum);
-        if (current_diff < min_diff) {
-            min_diff = current_diff;
-            cut_index = i;
+    int sum = 0;
+    for (int num : vec) {
+        sum += num;
+    }
+    int half_sum = 0;
+    for (int i = 0; i < n; ++i) {
+        half_sum += vec[i];
+        if (half_sum * 2 >= sum) {
+            if (half_sum * 2 == sum) {
+                for (int j = 0; j <= i; ++j) {
+                    cout << vec[j] << endl;
+                }
+            } else {
+                if (abs(half_sum * 2 - sum) < abs((half_sum - vec[i]) * 2 - sum)) {
+                    for (int j = 0; j <= i; ++j) {
+                        cout << vec[j] << endl;
+                    }
+                } else {
+                    for (int j = 0; j < i; ++j) {
+                        cout << vec[j] << endl;
+                    }
+                    cout << vec[i] << endl;
+                }
+            }
+            for (int j = i + 1; j < n; ++j) {
+                cout << vec[j] << endl;
+            }
+            return 0;
         }
     }
-    
-    for (int i = 0; i <= cut_index; i++) {
-        cout << nums[i] << endl;
-    }
-    cout << 0 << endl;
-    
     return 0;
 }
