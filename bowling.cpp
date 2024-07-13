6584782fcd00e0e554f37ca498d716bae2faf656
beep boop <githubbot@vadim.me>
2024-07-13 14:43:08 +0200

    Solution 84 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/cm31biqe Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from d0c1addc-94ca-4be3-b2a6-a44e777249a1.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- bowling.cpp
+++ bowling.cpp
@@ -1,6 +1,5 @@
 ```cpp
 #include <vector>
-#include <string>
 
 int bowlingScore(const std::string& input) {
     if (input.empty()) {
@@ -10,28 +9,31 @@ int bowlingScore(const std::string& input) {
     int prevRoll1 = 0;
     int prevRoll2 = 0;
 
-    int rolls[10];
-    for (int i = 0; i < 10; i++) {
-        if (input[i] == 'X') {
-            rolls[i] = 10;
-        } else if (input[i+1] != ' ') {
-            int roll = input[i] - '0' + (input[i+1] - '0');
-            rolls[i] = roll;
-            i++;
-        } else {
-            rolls[i] = input[i] - '0';
-        }
-    }
+    std::vector<int> rolls = parseInput(input);
 
     for (int i = 0; i < 10; i++) {
         if (rolls[i] == 10) {
             score += 30;
-        } else if (rolls[i] + rolls[(i+1)%10] >= 10) {
-            score += 10 + (i < 8 ? rolls[(i+2)%10] : prevRoll1 + prevRoll2);
+        } else if (rolls[i] + rolls[i+1] >= 10) {
+            score += 10 + (i < 8 ? rolls[i+2] : prevRoll1 + prevRoll2);
+            i++;
         } else {
-            score += rolls[i] + rolls[(i+1)%10];
+            score += rolls[i] + rolls[i+1];
         }
     }
 
     return score;
+}
+
+std::vector<int> parseInput(const std::string& input) {
+    std::vector<int> rolls;
+    for (int i = 0; i < input.length(); i++) {
+        if (input.substr(i, 1) == "X") {
+            rolls.push_back(10);
+        } else if (input[i] != ' ') {
+            int roll = input[i] - '0';
+            rolls.push_back(roll);
+        }
+    }
+    return rolls;
 }
