8af2ee290a59c6157ec635b44e4e731b651783c0
beep boop <githubbot@vadim.me>
2024-07-13 16:24:42 +0200

    Solution 70 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/edc9yviu Prompt: it must return koaZa for input koa-za, but it returns Cael-case ml-case e-case lcase -ase cse ae s e xample-eample-xmple-aple-mle-pe-l -e-tst-et-s-t-String.

--- camel-case.cpp
+++ camel-case.cpp
@@ -4,44 +4,23 @@
 
 std::string camelCase(std::string str) {
     std::string result = "";
-    while (str.find(" ") != std::string::npos || str.find("-") != std::string::npos) {
-        size_t pos = str.find(" ");
-        if (pos == std::string::npos) {
-            pos = str.find("-");
-        }
-        if (!result.empty()) {
-            result += tolower(str[0]); 
-            str.erase(0, 1);
-        } else {
-            result += toupper(str[0]); 
-            str.erase(0, 1);
-        }
-    }
-
-    if (!str.empty()) {
-        if (isalpha(str[0])) {
-            if (!result.empty()) {
-                result += tolower(str[0]); 
-                str.erase(0, 1);
-            } else {
-                result += toupper(str[0]); 
-                str.erase(0, 1);
-            }
-        } else {
-            result += str[0];
-            str.erase(0, 1);
-        }
-    }
-
     while (!str.empty()) {
         size_t pos = str.find(" ");
         if (pos == std::string::npos) {
             pos = str.length();
         }
-        result += str.substr(0, pos);
-        str.erase(0, pos);
+        char c = toupper(str[0]);
+        result += c;
+        str.erase(0, 1);
+        while (str.find("-") != std::string::npos || !str.empty()) {
+            size_t endPos = str.find(" ");
+            if (endPos == std::string::npos) {
+                endPos = str.length();
+            }
+            result += tolower(str.substr(0, endPos));
+            str.erase(0, endPos);
+        }
     }
-
     return result;
 }
 
