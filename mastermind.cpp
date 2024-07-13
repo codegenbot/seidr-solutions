fdd56b923b70ec0506660cdcfbf901cb9a7799e8
beep boop <githubbot@vadim.me>
2024-07-13 07:09:54 +0200

    Solution 20 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/azcp4yi0 Prompt: 2f00bb3d-02b0-4d96-8f66-68997afc222a.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ 2f00bb3d-02b0-4d96-8f66-68997afc222a.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ 2f00bb3d-02b0-4d96-8f66-68997afc222a.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,38 +1,16 @@
-#include <vector>
-#include <iostream>
-#include <string>
-#include <algorithm>
+```#include <string>
 
-using namespace std;
+int mastermind(std::string code, std::string guess) {
+    int white = 0;
+    int black = 0;
 
-int mastermind(string code, string guess) {
-    int whitePegs = 0;
-    int blackPegs = 0;
-
-    vector<char> codeVec(code.begin(), code.end());
-    vector<char> guessVec(guess.begin(), guess.end());
-
-    for (int i = 0; i < 4; i++) {
-        if (codeVec[i] == guessVec[i]) {
-            blackPegs++;
-            codeVec[i] = '\0';
-            guessVec[i] = '\0';
+    for (int i = 0; i < 4; ++i) {
+        if (code[i] == guess[i]) {
+            black++;
+        } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {
+            white++;
         }
     }
 
-    for (int i = 0; i < 4; i++) {
-        if (std::count(codeVec.begin(), codeVec.end(), guessVec[i]) > 0) {
-            whitePegs++;
-            std::count(codeVec.begin(), codeVec.end(), guessVec[i])--;
-        }
-    }
-
-    return blackPegs;
-}
-
-int main() {
-    string code, guess;
-    cin >> code >> guess;
-    cout << mastermind(code, guess) << endl;
-    return 0;
-}
+    return black + white;
+}``
