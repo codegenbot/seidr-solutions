bf06459d20f0de54099e758c1e7efb3b02a4b8b4
beep boop <githubbot@vadim.me>
2024-07-13 06:48:30 +0200

    Solution 2 of Python/78, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0wk1v9fa Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d592e058-759d-483a-ad9f-65473aeb8 761.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_78.py
+++ Python_78.py
@@ -1,12 +1,9 @@
-```
+import re
+
+
 def hex_key(num):
-    primes = {'B': '11', 'D': '13'}
     count = 0
     for char in num:
-        if char.upper() in primes:
+        if re.match("[0-9A-Fa-f]", char):
             count += 1
-        elif char.isdigit():
-            n = int(char, 16)
-            if n > 1 and all(n % i for i in range(2, int(n**0.5) + 1)):
-                count += 1
     return count
