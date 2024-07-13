0f2c05a7fedb114806b542bf45f09ab427848059
beep boop <githubbot@vadim.me>
2024-07-13 05:43:54 +0200

    Solution 42 of CPP/18, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/av94wdrt Prompt: Complete the following code given the task description and function signature.

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -2,8 +2,8 @@ int how_many_times(string str, string substring) {
     int count = 0;
     size_t pos = 0;
     while ((pos = str.find(substring)) != string::npos) {
-        count++;
-        pos += substring.size();
+        ++count;
+        str.erase(0, pos + substring.length());
     }
     return count;
 }
