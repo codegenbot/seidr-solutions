36ab78542b57639d4a21842f0f82d38143a3a394
beep boop <githubbot@vadim.me>
2024-07-13 09:17:12 +0200

    Solution 86 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8fxvx5aq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,26 +1,19 @@
-#include<stdio.h>
-#include<string>
-#include<algorithm>
-#include<boost/any.hpp>
-#include<boost/multi_io.hpp>
-using namespace std;
-
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return max(boost::any_cast<int>(a), boost::any_cast<int>(b));
-    } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
-        return (boost::any_cast<double>(a) > boost::any_cast<double>(b)) ? a : b;
-    } else if ((a.type() == typeid(string) || a.type() == typeid(wstring)) &&
-               (b.type() == typeid(string) || b.type() == typeid(wstring))) {
-        string strA = boost::any_cast<string>(a);
-        string strB = boost::any_cast<string>(b);
-        if (strA > strB)
-            return a;
-        else if (strA < strB)
-            return b;
-        else
-            return boost::any("None");
-    } else {
-        return boost::any("None");
+        return boost::any(max(a.get<int>(), b.get<int>()));
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        return a > b ? a : b;
+    }
+    else if ((a.type() == typeid(string) || a.type() == typeid(char *)) &&
+             (b.type() == typeid(string) || b.type() == typeid(char *))) {
+        return (a.get<string>().compare(b.get<string>()) > 0) ? a : ((string)boost::any_cast<b>(b));
+    }
+    else if ((a.type() == typeid(int) && b.type() == typeid(float)) ||
+             (a.type() == typeid(float) && b.type() == typeid(int))) {
+        return boost::any(a.get<int>() > b.get<float>());
+    }
+    else {
+        return "None";
     }
 }
