2b4914b8c952fdc64d2e844bbc264831dfdd6131
beep boop <githubbot@vadim.me>
2024-07-13 22:42:27 +0200

    Solution 55 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/0yn5s7nw Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- leaders.cpp
+++ leaders.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    vector<int> nums = {5, 2, 3, 4}; // replace with your input
+    vector<int> nums = {5, 2, 3, 4}; 
     vector<int> res = leaders(nums);
     for (int num : res) {
         cout << num << " ";
@@ -11,13 +11,15 @@ int main() {
 }
 
 vector<int> leaders(vector<int>& nums) {
-    int n = nums.size();
     vector<int> res;
-    res.push_back(nums.back());
-    for (int i = n - 2; i >= 0; --i) {
-        if (nums[i] >= res.back()) {
+    int rightmost = nums.back();
+    res.push_back(rightmost);
+    for (int i = nums.size() - 2; i >= 0; --i) {
+        if (nums[i] >= rightmost) {
             res.push_back(nums[i]);
+            rightmost = nums[i];
         }
     }
+    reverse(res.begin(), res.end());
     return res;
 }
