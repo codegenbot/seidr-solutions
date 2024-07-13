8fc50ce04672e64322387800b86b58ee6a52583c
beep boop <githubbot@vadim.me>
2024-07-13 07:43:05 +0200

    Solution 11 of Python/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/iydn96n5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ca8f75b1-8adf-45b6-87b7-16b762f13 ff4.py", line 13, in <module>     check(search)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ca8f75b1-8adf-45b6-87b7-16b762f13 ff4.py", line 10, in check     assert search([3, 10, 10, 9, 2]) == -1            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_69.py
+++ Python_69.py
@@ -1,7 +1,6 @@
-```
 def search(lst):
-    max_num = -1
+    max_num = 0
     for num in lst:
         if num > max_num and num > 0:
             max_num = num
-    return max_num
+    return max_num or 0
