1e5313794df26b9a6a24eee3df1344ea4d32224b
beep boop <githubbot@vadim.me>
2024-07-13 04:42:20 +0200

    Solution 47 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/t8miolwn Prompt:    16 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ca03ff42-4c37-4feb-90af-e2d102e581e8.cpp:16:44: error: ‘b’ was not declared in t his scope    16 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ca03ff42-4c37-4feb-90af-e2d102e581e8.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ca03ff42-4c37-4feb-90af-e2d102e581e8.cpp:15: ca03ff42-4c37-4feb-90af-e2d102e581e8.cpp: In function ‘int main()’: ca03ff42-4c37-4feb-90af-e2d102e581e8.cpp:25:20: error: ‘sorted_list_sum’ was not  declared in this scope    25 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ ca03ff42-4c37-4feb-90af-e2d102e581e8.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,11 +1,11 @@
 vector<string> sorted_list_sum(vector<string> lst) {
-    auto it = unique(lst.begin(), lst.end(),
+    auto it = unique(lst.begin(), lst.end(), 
         [](const string& a, const string& b) { return (stoi(a) + stoi(b)) % 2 == 1; });
     vector<string> result(it, lst.end());
     sort(result.begin(), result.end(),
         [](const string& a, const string& b) {
-            if (stoi(a) != stoi(b))
-                return stoi(a) < stoi(b);
+            if (stol(a) != stol(b))
+                return stol(a) < stol(b);
             else
                 return a < b;
         });
