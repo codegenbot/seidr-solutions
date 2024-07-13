4b3d5f1038834fc644297f54eaf1125763438f12
beep boop <githubbot@vadim.me>
2024-07-13 12:13:37 +0200

    Solution 37 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/i4l8o97e Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8de148d5-7978-49de-aee4-4e3ff1144 1c0.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8de148d5-7978-49de-aee4-4e3ff1144 1c0.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,9 +1,6 @@
 def sort_third(l: list):
     return [
-        (
-            i
-            if i < len(l) and (i % 3 != 0 or not any(x % 3 == 0 for x in l[:i]))
-            else sorted([x for x in l[:i] + l[i + 1 :] if x % 3 == 0])[::-1]
-        )
+        i if i < len(l) and (i % 3 != 0 or not any(x % 3 == 0 for x in l[:i])) 
+        else sorted([x for x in l[:i] + l[i + 1:] if x % 3 == 0])[::-1]
         for i in range(len(l))
     ]
