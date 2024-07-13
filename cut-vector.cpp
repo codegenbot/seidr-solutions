d9a166f9402c8f808e2fbcb5fa4ce8461192cff9
beep boop <githubbot@vadim.me>
2024-07-14 01:21:50 +0200

    Solution 17 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/675h5l6k Prompt: 04f8b80f-2a31-4ac9-9d77-94c462bffda0.cpp: In function ‘std::vector<std::vector<i nt> > cutVector(std::vector<int>)’: 04f8b80f-2a31-4ac9-9d77-94c462bffda0.cpp:8:20: error: ‘INT_MAX’ was not declared  in this scope     8 |     int min_diff = INT_MAX;       |                    ^~~~~~~ 04f8b80f-2a31-4ac9-9d77-94c462bffda0.cpp:3:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     2 | #include <vector>   +++ |+#include <climits>     3 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
     int min_diff = INT_MAX;
-    int cut_index = 0;
+    int split_index = -1;
     for (int i = 1; i < n; i++) {
         int diff = abs(v[i] - v[i-1]);
-        if (diff <= min_diff) {
-            min_diff = diff;
-            cut_index = i;
+        if ((v[i] >= v[i-1]) && (i == n - 1 || v[i+1] <= v[i])) {
+            if (diff < min_diff) {
+                min_diff = diff;
+                split_index = i;
+            }
         }
     }
-    res[0] = vector<int>(v.begin(), v.begin() + cut_index);
-    res[1] = vector<int>(v.begin() + cut_index, v.end());
+    res[0] = vector<int>(v.begin(), v.begin() + split_index);
+    res[1] = vector<int>(v.begin() + split_index, v.end());
     return res;
 }
 
