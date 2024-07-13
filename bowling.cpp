88e3db2fb6033f563de0d6bf7ac93e286e7936f1
beep boop <githubbot@vadim.me>
2024-07-13 10:12:47 +0200

    Solution 69 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/acp5mi4l Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,47 +1,26 @@
 int score(string s) {
-    int total = 0;
-    int frame = 0;
-    for (int i = 0; i < s.size() && frame < 10; ++i) {
+    int res = 0, frame = 0, i = 0;
+    while (frame < 10) {
         if (s[i] == 'X') {
-            total += 10;
-            if (i + 2 < s.size()) {
-                if (s[i + 2] == 'X') {
-                    total += 10;
-                    if (i + 4 < s.size()) {
-                        if (s[i + 4] == 'X') {
-                            total += 10;
-                        } else if (s[i + 4] != '/') {
-                            total += s[i + 4] - '0';
-                        }
-                    }
-                } else if (s[i + 3] == '/') {
-                    total += 10;
-                } else if (s[i + 4] == '/') {
-                    total += 10;
-                } else {
-                    total += s[i + 2] - '0' + s[i + 3] - '0';
-                }
-            }
-            frame++;
-        } else if (s[i] == '/') {
-            total += 10 - (s[i - 1] - '0');
-            if (i + 1 < s.size()) {
-                total += s[i + 1] - '0';
-            }
-            frame++;
-        } else if (s[i] == '-') {
-            // do nothing
+            res += 10 + (s[i + 1] == 'X' ? 10 + (s[i + 2] == 'X' ? 10 : (s[i + 2] - '0')) : (s[i + 1] == '/' ? 10 : (s[i + 1] - '0') + (s[i + 2] == '/' ? 10 : (s[i + 2] - '0'))));
+            i++;
+        } else if (s[i + 1] == '/') {
+            res += 10;
+            res += s[i + 2] == 'X' ? 10 : (s[i + 2] - '0');
+            i += 2;
         } else {
-            total += s[i] - '0';
-            frame++;
+            res += s[i] == '-' ? 0 : (s[i] - '0');
+            res += s[i + 1] == '-' ? 0 : (s[i + 1] - '0');
+            i += 2;
         }
+        frame++;
     }
-    return total;
+    return res;
 }
 
 int main() {
     string s;
     cin >> s;
-    cout << score(s) << endl;
+    cout << score(s);
     return 0;
 }
