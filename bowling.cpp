495ecae6bd3a7b7369f26380a4d0e9dd78a77d75
beep boop <githubbot@vadim.me>
2024-07-13 07:00:44 +0200

    Solution 22 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/cm31biqe Prompt: 252eecaa-efd0-428e-94d3-7ebbbb3f21ed.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int bowlingScore(std::string s) {       |                       ^~~~~~ 252eecaa-efd0-428e-94d3-7ebbbb3f21ed.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int bowlingScore(std::string s) {

--- bowling.cpp
+++ bowling.cpp
@@ -2,37 +2,21 @@
 
 int bowlingScore(std::string s) {
     int score = 0;
-    int prevRoll = 0;
-    bool isStrike = false;
     for (int i = 0; i < s.length(); i++) {
         if (s[i] == 'X') {
             score += 30;
-            isStrike = true;
         } else if (s[i] == '/') {
-            if (isStrike) {
-                score += prevRoll * 2;
-                isStrike = false;
-            } else {
-                int roll = bowlingScoreHelper(s.substr(0, i));
-                score += roll + 10;
+            if (i > 0) {
+                score += bowlingScoreHelper(s.substr(0, i));
             }
+            score += 10;
         } else if (s[i] == ' ') {
-            int roll = (s[i - 1] - '0') + (s[i + 1] - '0');
-            if (isStrike) {
-                score += roll + prevRoll * 2;
-                isStrike = false;
-            } else {
-                score += roll;
-            }
+            int prevRoll = s[i - 1] - '0';
+            int currentRoll = s[i + 1] - '0';
+            score += prevRoll + currentRoll;
         } else {
             int roll = s[i] - '0';
-            if (isStrike) {
-                score += roll + prevRoll * 2;
-                isStrike = false;
-            } else {
-                score += roll;
-            }
-            prevRoll = roll;
+            score += roll;
         }
     }
     return score;
