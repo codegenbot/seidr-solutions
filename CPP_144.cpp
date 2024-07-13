107511123229eea1bd8e14ec7dfeb54cc1f69ecf
beep boop <githubbot@vadim.me>
2024-07-13 08:39:25 +0200

    Solution 7 of CPP/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/cmwyitl8 Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,13 +1,27 @@
-#include <string>
-#include <sstream>
-
+#include<string>
 using namespace std;
 
 bool simplify(string x, string n) {
     int a = 0, b = 0, c = 0, d = 0;
-    stringstream s1(x), s2(n);
-    s1 >> a >> '/' >> b;
-    s2 >> c >> '/' >> d;
-    
-    return (a*d == b*c);
+    for(int i=1; i<x.size(); i++) {
+        if(x[i] == '/') break;
+        if(x[i] == '/') continue;
+        if(x[i] == ' ') continue;
+        if(x[i] != '/') a = a*10 + (x[i]-'0');
+    }
+    for(int i=x.size()-1; i>=i-x.find('/'); i--) {
+        if(x[i] == '/') break;
+        c = c*10 + (x[i]-'0');
+    }
+    for(int i=1; i<n.size(); i++) {
+        if(n[i] == '/') break;
+        if(n[i] == '/') continue;
+        if(n[i] == ' ') continue;
+        if(n[i] != '/') b = b*10 + (n[i]-'0');
+    }
+    for(int i=n.size()-1; i>=i-n.find('/'); i--) {
+        if(n[i] == '/') break;
+        d = d*10 + (n[i]-'0');
+    }
+    return a*d == c*b;
 }
