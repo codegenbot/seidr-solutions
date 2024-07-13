80503f136c19404992b64ffb3a3e5086baedfdd1
beep boop <githubbot@vadim.me>
2024-07-13 08:39:24 +0200

    Solution 2 of gcd, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/sfxvayrr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03be2866-53d1-4fbd-992c-45a3a4fdb 6b3.py", line 1     Here is the solution:                 ^^^^^^^^ SyntaxError: invalid syntax

--- gcd.py
+++ gcd.py
@@ -8,5 +8,10 @@ def indices_of_substring(text, target):
     result = []
     for i in range(len(text)):
         if text[i:i+len(target)] == target:
-            result.append(i)
+            j = i
+            while True:
+                if text[j:j+len(target)] != target:
+                    break
+                result.append(j)
+                j += 1
     return result
