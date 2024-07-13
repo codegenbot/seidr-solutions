8fd9df156d75817be5e627767b38f74b13ed2bce
beep boop <githubbot@vadim.me>
2024-07-13 08:39:43 +0200

    Solution 32 of Python/75, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/7rmfa3l3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/10224807-7709-4a28-9351-10497d8d4 c2b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_75.py
+++ Python_75.py
@@ -15,9 +15,4 @@ def is_multiply_prime(n=None):
         product = a * b
         return is_prime(product)
     else:
-        if n < 2:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
+        pass
