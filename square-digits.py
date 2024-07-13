aebbc4484a3ab4465c4274be3d3ed6d4d1bfb15d
beep boop <githubbot@vadim.me>
2024-07-13 17:36:46 +0200

    Solution 46 of square-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/wu8wmwz3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/511b4e19-69a6-4dd4-ad34-8302e79a4 ec1.py", line 2     """Given a positive integer, square each digit and concatenate the squares i nto a returned string."""     ^ IndentationError: expected an indented block after function definition on line 1

--- square-digits.py
+++ square-digits.py
@@ -1,3 +1,4 @@
 def square_digits(n):
-    """Given a positive integer, square each digit and concatenate the squares into a returned string."""
+    """Given a positive integer, square each digit and concatenate the squares into a returned string.
+    """
     return "".join(str(int(i) ** 2) for i in str(n))
