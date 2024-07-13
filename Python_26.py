788c206ac966791329b3f23841c00e3f64028060
beep boop <githubbot@vadim.me>
2024-07-13 03:01:57 +0200

    Solution 28 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fykll3yz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26e60ec7-183d-47b6-83a6-6df03fa65 a6e.py", line 8, in <module>     check(remove_duplicates)           ^^^^^^^^^^^^^^^^^ NameError: name 'remove_duplicates' is not defined

--- Python_26.py
+++ Python_26.py
@@ -1,4 +1,6 @@
-```
-def remove_duplicates(numbers):
+def check(numbers):
     seen = set()
-    return [num for num in numbers if not (num in seen or seen.add(num))][::-1]
+    return [num for num in numbers if not (num in seen or seen.add(num)) and True][::-1]
+
+input_numbers = list(map(int, input().split()))
+print(check(input_numbers))
