bd5604f35586a41d9cdcd5edef1375ea67b3a353
beep boop <githubbot@vadim.me>
2024-07-13 22:44:22 +0200

    Solution 5 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/osxx10aj Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,17 +1,50 @@
-bool solveBoolean(string s) {
-    stack<char> st;
-    for (char c : s) {
-        if (c == '&') {
-            char left = st.top(); st.pop();
-            char right = st.top(); st.pop();
-            st.push((left == 'T' && right == 'T') ? 'T' : 'F');
-        } else if (c == '|') {
-            char left = st.top(); st.pop();
-            char right = st.top(); st.pop();
-            st.push((left == 'T' || right == 'T') ? 'T' : 'F');
-        } else {
-            st.push(c);
+#include <string>
+using namespace std;
+
+bool evaluateBooleanExpression(string expression) {
+    stack<char> opstack;
+    stack<string> valstack;
+
+    for (int i = 0; i < expression.length(); i++) {
+        if (expression[i] == '&') {
+            while (!opstack.empty() && opstack.top() == '|') {
+                opstack.pop();
+                string op2 = valstack.top();
+                valstack.pop();
+                string op1 = valstack.top();
+                valstack.pop();
+                valstack.push((op1 + " & " + op2));
+            }
+            opstack.push('&');
+        } else if (expression[i] == '|') {
+            while (!opstack.empty() && opstack.top() == '&') {
+                opstack.pop();
+                string op2 = valstack.top();
+                valstack.pop();
+                string op1 = valstack.top();
+                valstack.pop();
+                valstack.push((op1 + " | " + op2));
+            }
+            opstack.push('|');
+        } else if (expression[i] == 'T' || expression[i] == 't') {
+            valstack.push("True");
+        } else if (expression[i] == 'F' || expression[i] == 'f') {
+            valstack.push("False");
         }
     }
-    return st.top() == 'T';
+
+    while (!opstack.empty()) {
+        string op2 = valstack.top();
+        valstack.pop();
+        string op1 = valstack.top();
+        valstack.pop();
+        if (opstack.top() == '&') {
+            valstack.push((op1 + " & " + op2));
+        } else {
+            valstack.push((op1 + " | " + op2));
+        }
+        opstack.pop();
+    }
+
+    return valstack.top() == "True";
 }
