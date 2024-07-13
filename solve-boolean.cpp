416be6795d3cf3aa4a282787e83c1a1e097f446c
beep boop <githubbot@vadim.me>
2024-07-13 23:14:02 +0200

    Solution 42 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/osxx10aj Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -2,26 +2,44 @@
 using namespace std;
 
 bool solveBoolean(string s) {
-    int i = 0;
     bool res = true;
+    int i = 0;
     while (i < s.length()) {
-        if (s[i] == 'T') {
+        if (s[i] == 'T') return true;
+        if (s[i] == 'F') return false;
+        if (s[i] == '|') {
             res = true;
             i++;
             continue;
         }
-        if (s[i] == 'F') {
-            res = false;
+        if (s[i] == '&') {
+            res = true;
             i++;
             continue;
         }
-        if (s[i] == '|') {
-            while (i < s.length() && s[i] != '&' && s[i] != 'T' && s[i] != 'F') {
-                i++;
-            }
-        } else if (s[i] == '&') {
-            while (i < s.length() && s[i] != '|' && s[i] != 'T' && s[i] != 'F') {
-                i++;
+        if (s[i] == '(') {
+            i++;
+            bool temp = solveBoolean(s.substr(i));
+            i += s.find(')');
+            if (!temp) return false;
+        } else if (s[i] == ')') i++;
+        else {
+            int j = i + 1;
+            while (j < s.length() && s[j] != 'T' && s[j] != 'F' && s[j] != '&' && s[j] != '|')
+                j++;
+            string temp = s.substr(i, j - i);
+            if (temp == "T") {
+                res = true;
+                i = j;
+            } else if (temp == "F") {
+                res = false;
+                i = j;
+            } else if (temp[0] == '&') {
+                res &= solveBoolean(temp.substr(1));
+                i = j;
+            } else if (temp[0] == '|') {
+                res |= solveBoolean(temp.substr(1));
+                i = j;
             }
         }
     }
