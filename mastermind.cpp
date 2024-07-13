7401a5429a36e08c2ac87fd2e3a046a6b0cf4245
beep boop <githubbot@vadim.me>
2024-07-13 22:54:03 +0200

    Solution 52 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/jkou94gz Prompt: in this scope     1 | int whitePegs(string code, string guess) {       |               ^~~~~~ 4029037e-1d54-4e40-814b-f782ee4b334a.cpp:1:28: error: ‘string’ was not declared in this scope     1 | int whitePegs(string code, string guess) {       |                            ^~~~~~ 4029037e-1d54-4e40-814b-f782ee4b334a.cpp:1:40: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int whitePegs(string code, string guess) {       |                                        ^ 4029037e-1d54-4e40-814b-f782ee4b334a.cpp:11:15: error: ‘string’ was not declared  in this scope    11 | int blackPegs(string code, string guess) {       |               ^~~~~~ 4029037e-1d54-4e40-814b-f782ee4b334a.cpp:11:28: error: ‘string’ was not declared  in this scope    11 | int blackPegs(string code, string guess) {       |                            ^~~~~~ 4029037e-1d54-4e40-814b-f782ee4b334a.cpp:11:40: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | int blackPegs(string code, string guess) {       |                                        ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -11,12 +11,12 @@ int whitePegs(string code, string guess) {
 }
 
 int blackPegs(string code, string guess) {
-    int count = 0;
     map<char, int> codeCount;
     for (int i = 0; i < 4; i++) {
         codeCount[code[i]]++;
     }
     
+    int count = 0;
     for (int i = 0; i < 4; i++) {
         if (code[i] == guess[i]) {
             codeCount[code[i]]--;
