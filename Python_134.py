6b2a9d512bb3d19591a3a6dc8d21395bc031f0cf
beep boop <githubbot@vadim.me>
2024-07-13 08:18:42 +0200

    Solution 5 of Python/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fw253inm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fd48c1f7-c736-4e75-b764-abc7cf425 1e2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_134.py
+++ Python_134.py
@@ -1,11 +1,11 @@
-```'
+```
 def check_if_last_char_is_a_letter(txt):
     if len(txt) == 0:
         return False
     last_char = txt[-1]
     if not last_char.isalpha():
-        return False
+        return True
     for i in range(len(txt)-1, -1, -1):
-        if not last_char.lower() == txt[i].lower():
+        if last_char.lower() != txt[i].lower():
             return False
-    return True'
+    return True
