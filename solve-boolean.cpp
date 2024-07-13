a32b1e219f01eb7e7f3857546ae0955735824e52
beep boop <githubbot@vadim.me>
2024-07-13 14:59:47 +0200

    Solution 44 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/io622y1d Prompt: 0a29f8d8-6505-416d-8361-cb64008742f6.cpp:3:19: error: ‘string’ was not declared in this scope     3 | bool solveBoolean(string expression) {       |                   ^~~~~~ 0a29f8d8-6505-416d-8361-cb64008742f6.cpp:3:19: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 0a29f8d8-6505-416d-8361-cb64008742f6.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 0a29f8d8-6505-416d-8361-cb64008742f6.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,9 +1,10 @@
+```cpp
 #include <stack>
 #include <string>
 
 bool solveBoolean(std::string expression) {
-    std::stack<char> opStack;
-    std::stack<bool> valStack;
+    stack<char> opStack;
+    stack<bool> valStack;
 
     for (int i = 0; i < expression.length(); i++) {
         if (expression[i] == '&') {
@@ -18,20 +19,12 @@ bool solveBoolean(std::string expression) {
         } else if (expression[i] == 'T' || expression[i] == 't') {
             valStack.push(true);
         } else if (expression[i] == 'F' || expression[i] == 'f') {
-            valStack.push(false);
+            while (!opStack.empty()) {
+                if (opStack.top() == '|') opStack.pop(), valStack.pop();
+                else break;
+            }
+            valStack.pop();
         }
-    }
-
-    while (!opStack.empty()) {
-        char op = opStack.top();
-        opStack.pop();
-        bool right = valStack.top();
-        valStack.pop();
-        bool left = valStack.top();
-        valStack.pop();
-        
-        if (op == '|') valStack.push(left || right);
-        else valStack.push(left && right);
     }
 
     return valStack.top();
