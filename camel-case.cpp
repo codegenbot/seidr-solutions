b49f801d00a6d83ba6427520fef3766df5090032
beep boop <githubbot@vadim.me>
2024-07-13 19:32:07 +0200

    Solution 84 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/crhzff04 Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: cd400fbb-f579-4e3e-8086-9544c8c0fde2.cpp:37:54:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -6,27 +6,33 @@ using namespace std;
 
 string camelCase(string s) {
     string result = "";
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == '-') {
+    for (int i = 0; i <= s.size(); i++) {
+        if (i == s.size() || s[i] == '-') {
             if (!result.empty()) {
                 result[0] = toupper(result[0]);
             }
-            result += s.substr(i + 1, 1).toupper();
-            i++;
+            if (i < s.size()) {
+                string word = s.substr(i + 1);
+                if (result.empty()) {
+                    result += word;
+                } else {
+                    result += toupper(word[0]) + word.substr(1);
+                }
+            }
+            break;
         } else if (s[i] == ' ') {
             if (!result.empty()) {
                 result[0] = toupper(result[0]);
             }
-            string word = result;
-            result = "";
-            for (char c : word) {
-                result += tolower(c);
-            }
+            string left = s.substr(0, i);
+            string right = s.substr(i + 1);
+            result += toupper(left[0]) + left.substr(1) + right;
+            break;
         } else {
             result += s[i];
         }
     }
-    return !result.empty() ? (result[0] = toupper(result[0]), result + s.substr(i)) : s;
+    return result;
 }
 
 int main() {
