9d76a357d132d566d1b74712424d3d2b1fc41c2c
beep boop <githubbot@vadim.me>
2024-07-13 23:59:23 +0200

    Solution 67 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/5vwkqgs9 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,23 +1,49 @@
 int bowlingScore(string s) {
     int score = 0;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'X') {
-            score += 30;
-            i++;
-        } else if (s[i] == '/') {
-            int next = i + 1;
-            while (next < s.length() && !isdigit(s[next])) next++;
-            int nextRoll = stoi(s.substr(next, 2));
-            score += 10 - nextRoll;
-            i = next;
-        } else {
-            int currentRoll = 0;
-            while (i < s.length() && isdigit(s[i])) {
-                currentRoll *= 10 + (s[i] - '0');
-                i++;
+    bool lastFrameStrike = false;
+    for (int i = 0; i < 10; ++i) {
+        if (s[i] == 'X') { // strike
+            score += 10 + (i < 9 ? 10 : 0);
+        } else if (s[i] == '/') { // spare
+            int j = i + 1;
+            while (j < 11 && s[j] != 'X' && s[j] != '/') {
+                ++j;
             }
-            if (currentRoll > 10) score += 10;
-            else score += currentRoll;
+            score += 10 - s[i - 1] - s[i];
+        } else { // normal roll
+            int a = s[i] - '0';
+            int b = (i < 8) ? s[i + 1] - '0' : 0;
+            if (a + b > 10) { // strike in next frame
+                score += a + b + (i < 9 ? 10 : 0);
+                lastFrameStrike = true;
+            } else { // normal roll or spare
+                score += a + b;
+            }
+        }
+    }
+    if (!lastFrameStrike) {
+        for (int i = 0; i < 3; ++i) {
+            if (s[10 - i] == 'X') { // bonus strike
+                score += 10;
+                break;
+            } else if (s[10 - i] == '/') { // bonus spare
+                int j = 10 - i - 1;
+                while (j >= 0 && s[j] != 'X' && s[j] != '/') {
+                    --j;
+                }
+                score += 10 - s[10 - i - 1] - s[10 - i];
+                break;
+            } else { // no bonus
+                int a = s[10 - i] - '0';
+                int b = (i == 2) ? 0 : s[9 - i] - '0';
+                if (a + b > 10) { // strike in next frame
+                    score += a + b + (i < 2 ? 10 : 0);
+                    break;
+                } else { // normal roll or spare
+                    score += a + b;
+                    break;
+                }
+            }
         }
     }
     return score;
