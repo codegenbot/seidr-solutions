fad08a8c175f0bb2f6444b8903f07c51bcf51d78
beep boop <githubbot@vadim.me>
2024-07-13 21:28:32 +0200

    Solution 98 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/rnfh2mzi Prompt: 78e725f8-ca22-4229-a0f2-66e736dff894.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 78e725f8-ca22-4229-a0f2-66e736dff894.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 78e725f8-ca22-4229-a0f2-66e736dff894.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 78e725f8-ca22-4229-a0f2-66e736dff894.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -4,21 +4,17 @@ string spinWords(string str) {
     while(i <= str.length()) {
         if (i == str.length() || str[i] == ' ') {
             int wordLength = i - result.length();
-            if (wordLength >= 5)
-                result.append(reverseWord(str.substr(i - wordLength, wordLength)).append(" "));
-            else
-                result.append(str.substr(i - wordLength, wordLength + 1)).append(" ");
+            if (wordLength >= 5) {
+                string reversedWord = "";
+                for(int j = wordLength - 1; j >= 0; j--) {
+                    reversedWord += str[wordLength-1-j];
+                }
+                result.append(reversedWord).append(" ");
+            } else
+                result.append(str.substr(i - wordLength, wordLength)).append(" ");
             i++;
         } else 
             i++;
     }
     return result;
-}
-
-string reverseWord(string str) {
-    string reversed = "";
-    for(int j = str.length() - 1; j >= 0; j--) {
-        reversed += str[j];
-    }
-    return reversed;
 }
