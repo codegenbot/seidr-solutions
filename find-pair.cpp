7aa99587f0f78f9956cfff30e045d87d06210e70
beep boop <githubbot@vadim.me>
2024-07-13 08:17:42 +0200

    Solution 69 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/2rmfqz3r Prompt: cc49b0f9-290d-4747-804c-ec32818d0f6b.cpp:1:1: error: ‘pair’ does not name a type     1 | pair<int, int> findPair(vector<int>& nums, int target) {       | ^~~~

--- find-pair.cpp
+++ find-pair.cpp
@@ -3,7 +3,7 @@ std::pair<int, int> findPair(vector<int>& nums, int target) {
     for (int i = 0; i < nums.size(); ++i) {
         int complement = target - nums[i];
         if (numIndex.count(complement)) {
-            return std::pair<int, int>(complement < nums[i] ? complement : nums[i], complement > nums[i] ? complement : nums[i]);
+            return pair<int, int>(complement < nums[i] ? complement : nums[i], complement > nums[i] ? complement : nums[i]);
         }
         numIndex[nums[i]] = i;
     }
