5b11d6b752fe72e325134cbc9f9745f0561e02ba
beep boop <githubbot@vadim.me>
2024-07-13 20:24:24 +0200

    Solution 67 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/ag2m3sge Prompt:   +++ |+#include <iostream>     2 | using namespace std; bdad4fb2-d369-4c60-96c5-7064ad2173e7.cpp:30:25: error: ‘cout’ was not declared i n this scope    30 |         for (int x : v) cout << x << " ";       |                         ^~~~ bdad4fb2-d369-4c60-96c5-7064ad2173e7.cpp:30:25: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’? bdad4fb2-d369-4c60-96c5-7064ad2173e7.cpp:31:9: error: ‘cout’ was not declared in  this scope    31 |         cout << endl;       |         ^~~~ bdad4fb2-d369-4c60-96c5-7064ad2173e7.cpp:31:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? bdad4fb2-d369-4c60-96c5-7064ad2173e7.cpp:31:17: error: ‘endl’ was not declared i n this scope    31 |         cout << endl;       |                 ^~~~ bdad4fb2-d369-4c60-96c5-7064ad2173e7.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int> &nums) {
+vector<vector<int>> cutVector(vector<int>& nums) {
     int n = nums.size();
     vector<vector<int>> result;
     
@@ -27,7 +27,7 @@ int main() {
         cin >> nums[i];
     }
     vector<vector<int>> res = cutVector(nums);
-    for (auto &v : res) {
+    for (auto& v : res) {
         for (int x : v) cout << x << " ";
         cout << endl;
     }
