29b77e2ac13e9e93213d2451ea4a628027832e22
beep boop <githubbot@vadim.me>
2024-07-13 08:43:31 +0200

    Solution 78 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/2rmfqz3r Prompt: 6c1777ce-0ba9-46d3-aa84-03377011c62e.cpp:3:30: error: ‘vector’ was not declared in this scope     3 | std::pair<int, int> findPair(vector<int>& nums, int target) {       |                              ^~~~~~ 6c1777ce-0ba9-46d3-aa84-03377011c62e.cpp:3:37: error: expected primary-expressio n before ‘int’     3 | std::pair<int, int> findPair(vector<int>& nums, int target) {       |                                     ^~~ 6c1777ce-0ba9-46d3-aa84-03377011c62e.cpp:3:49: error: expected primary-expressio n before ‘int’     3 | std::pair<int, int> findPair(vector<int>& nums, int target) {       |                                                 ^~~ 6c1777ce-0ba9-46d3-aa84-03377011c62e.cpp:3:61: error: expected ‘,’ or ‘;’ before  ‘{’ token     3 | std::pair<int, int> findPair(vector<int>& nums, int target) {       |                                                             ^

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,11 +1,12 @@
 #include <vector>
+#include <unordered_map>
 
 std::pair<int, int> findPair(vector<int>& nums, int target) {
     unordered_map<int, int> numIndex;
     for (int i = 0; i < nums.size(); ++i) {
         int complement = target - nums[i];
         if (numIndex.count(complement)) {
-            return std::pair<int, int>(complement < nums[i] ? complement : nums[i], nums[i]);
+            return std::pair<int, int>(min(nums[i], complement), max(nums[i], complement));
         }
         numIndex[nums[i]] = i;
     }
