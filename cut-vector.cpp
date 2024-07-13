5a341f1f0f7c73d7a14190ddd2ca1c446de04584
beep boop <githubbot@vadim.me>
2024-07-13 18:59:49 +0200

    Solution 16 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/hz6k4g9f Prompt: f439a8a9-3ed3-4bfb-ad99-256a99e5c296.cpp: In function ‘std::vector<std::vector<i nt> > cutVector(std::vector<int>&)’: f439a8a9-3ed3-4bfb-ad99-256a99e5c296.cpp:9:20: error: ‘abs’ was not declared in this scope     9 |         int diff = abs(nums[i] - nums[i + 1]);       |                    ^~~ f439a8a9-3ed3-4bfb-ad99-256a99e5c296.cpp:47:27: error: ‘INT_MAX’ was not declare d in this scope    47 |             int minDiff = INT_MAX;       |                           ^~~~~~~ f439a8a9-3ed3-4bfb-ad99-256a99e5c296.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,33 +1,70 @@
 #include <vector>
-#include <iostream>
+#include <cmath>
+#include <climits>
+using namespace std;
 
-std::pair<std::vector<int>, std::vector<int>> cutVector(const std::vector<int>& vec) {
-    int min_diff = INT_MAX;
-    int split_index = 0;
-
-    for (int i = 1; i < vec.size(); i++) {
-        int diff = abs(vec[i] - vec[0]);
-        if (diff < min_diff) {
-            min_diff = diff;
-            split_index = i;
+vector<vector<int>> cutVector(vector<int> &nums) {
+    int n = nums.size();
+    vector<vector<int>> res(2);
+    
+    for (int i = 0; i < n - 1; i++) {
+        int diff = abs(nums[i] - nums[i + 1]);
+        if ((i == 0 || nums[0] == nums[i]) && (n - 2 == i || nums[n - 1] == nums[i])) {
+            res[0].clear();
+            for (int j = 0; j <= i; j++) {
+                res[0].push_back(nums[j]);
+            }
+            res[1].clear();
+            for (int j = i + 1; j < n; j++) {
+                res[1].push_back(nums[j]);
+            }
+        } else if (diff == 0) {
+            res[0].clear();
+            for (int j = 0; j <= i; j++) {
+                res[0].push_back(nums[j]);
+            }
+            res[1].clear();
+            for (int j = i + 1; j < n; j++) {
+                res[1].push_back(nums[j]);
+            }
+        } else if ((i == 0 || nums[0] - nums[i] <= diff) && (n - 2 == i || nums[n - 1] - nums[i] <= diff)) {
+            res[0].clear();
+            for (int j = 0; j <= i; j++) {
+                res[0].push_back(nums[j]);
+            }
+            res[1].clear();
+            for (int j = i + 1; j < n; j++) {
+                res[1].push_back(nums[j]);
+            }
+        } else if ((i == 0 || nums[i] - nums[0] <= diff) && (n - 2 == i || nums[n - 1] - nums[i] <= diff)) {
+            res[0].clear();
+            for (int j = 0; j < i; j++) {
+                res[0].push_back(nums[j]);
+            }
+            res[1].clear();
+            for (int j = i; j < n; j++) {
+                res[1].push_back(nums[j]);
+            }
+        } else {
+            int minDiff = INT_MAX;
+            int pos = -1;
+            for (int i2 = 0; i2 < n - 1; i2++) {
+                int diff2 = abs(nums[i2] - nums[i2 + 1]);
+                if (diff2 < minDiff) {
+                    minDiff = diff2;
+                    pos = i2;
+                }
+            }
+            res[0].clear();
+            for (int j = 0; j <= pos; j++) {
+                res[0].push_back(nums[j]);
+            }
+            res[1].clear();
+            for (int j = pos + 1; j < n; j++) {
+                res[1].push_back(nums[j]);
+            }
         }
     }
-
-    return {{vec[0]}, {vec.begin() + 1, vec.end()}};
-}
-
-int main() {
-    int n;
-    std::cin >> n;
-
-    std::vector<int> vec(n);
-    for (int& num : vec) {
-        std::cin >> num;
-    }
-
-    std::pair<std::vector<int>, std::vector<int>> result = cutVector(vec);
-
-    std::cout << "["; for (const int& num : result.first) { std::cout << num; } std::cout << "] " << "["; for (const int& num : result.second) { std::cout << num; } std::cout << "]" << std::endl;
-
-    return 0;
+    
+    return res;
 }
