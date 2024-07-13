feac6c972f57b1ad8d495851330273fd23b0ebab
beep boop <githubbot@vadim.me>
2024-07-13 08:19:52 +0200

    Solution 13 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kxnv9mbc Prompt: No No Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb581b18-07d8-4e5d-8b8f-36e1fdb82 5a6.py", line 23, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb581b18-07d8-4e5d-8b8f-36e1fdb82 5a6.py", line 20, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,22 +1,16 @@
-```
 def match_parens(lst):
     for s in lst:
+        stack = [0]  
         match = "Yes"
-        stack = []
-        if len(s) == 1:
-            if s[0] != "(" and s[0] != ")":
-                print("No")
-            else:
-                print("No")
-        else:
-            for c in s:
-                if c == "(":
-                    stack.append(c)
-                elif c == ")":
-                    if not stack or stack.pop() != "(":
-                        match = "No"
-                        break
-            else:
-                if stack:
+        for c in s:
+            if c == "(":
+                stack.append(c)
+            elif c == ")":
+                if len(stack) == 1:  
+                    stack.pop()
+                else:
                     match = "No"
+                    break
+        if len(stack) > 0:
+            match = "No"
         print(match)
