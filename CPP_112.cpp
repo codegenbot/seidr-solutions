fbd22c31ecd5c4c6d8535a5b1f66739175c16286
beep boop <githubbot@vadim.me>
2024-07-13 05:33:20 +0200

    Solution 76 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5iilprjk Prompt:       |                              ^~~~~~ fedadadb-6d1c-4a88-8cc6-3b17bfe2c07f.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ fedadadb-6d1c-4a88-8cc6-3b17bfe2c07f.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ fedadadb-6d1c-4a88-8cc6-3b17bfe2c07f.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from fedadadb-6d1c-4a88-8cc6-3b17bfe2c07f.cpp:25: fedadadb-6d1c-4a88-8cc6-3b17bfe2c07f.cpp: In function ‘int main()’: fedadadb-6d1c-4a88-8cc6-3b17bfe2c07f.cpp:35:20: error: ‘reverse_delete’ was not declared in this scope    35 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ fedadadb-6d1c-4a88-8cc6-3b17bfe2c07f.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,3 +1,21 @@
-bool issame(vector<string> a, vector<string>b) {
-    return a == b;
+vector<string> reverse_delete(string s, string c) {
+    vector<string> result;
+    string temp = "";
+    for (int i = 0; i < s.length(); i++) {
+        if (find(c.begin(), c.end(), s[i]) == c.end()) {
+            temp += s[i];
+        }
+    }
+    bool is_palindrome = false;
+    string temp2 = temp;
+    reverse(temp2.begin(), temp2.end());
+    if (temp == temp2) {
+        is_palindrome = true;
+    }
+    result.push_back(temp);
+    if (is_palindrome)
+        result.push_back("True");
+    else
+        result.push_back("False");
+    return result;
 }
