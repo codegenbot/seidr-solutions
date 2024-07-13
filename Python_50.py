e6b932117739137236ec1af041d01a227f6f83f0
beep boop <githubbot@vadim.me>
2024-07-13 06:29:05 +0200

    Solution 34 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/u5fnzvz7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2182b5be-8d27-4f79-8b5e-f63bf8f29 77c.py", line 6     return "".join([ (chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) if ch.isal pha() and ch.lower() >= "a" and ch.lower() <= "z" else ch for ch in decode_shift ]) ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '('

--- Python_50.py
+++ Python_50.py
@@ -1,1 +1,1 @@
-return "".join([ (chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) if ch.isalpha() and ch.lower() >= "a" and ch.lower() <= "z" else ch for ch in decode_shift]))]
+return "".join([ (chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a"))) if ch.isalpha() and ch.lower() >= "a" and ch.lower() <= "z" else ch for ch in decode_shift])
