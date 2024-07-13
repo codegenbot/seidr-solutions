9fccd54d1effe12f727d393bddb01814e8aee68b
beep boop <githubbot@vadim.me>
2024-07-13 08:54:36 +0200

    Solution 1 of CPP/150, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/iqw9hs0f Prompt: Complete the following code given the task description and function signature.

--- CPP_150.cpp
+++ CPP_150.cpp
@@ -1,19 +1,15 @@
-#include <cmath>
-
-int x_or_y(int n, int x, int y) {
-    if (isPrime(n)) {
-        return x;
-    } else {
-        return y;
-    }
-}
-
 bool isPrime(int num) {
     if (num <= 1)
         return false;
-    for (int i = 2; i <= sqrt(num); i++) {
+    for (int i = 2; i * i <= num; i++)
         if (num % i == 0)
             return false;
-    }
     return true;
+}
+
+int x_or_y(int n, int x, int y) {
+    if (isPrime(n))
+        return x;
+    else
+        return y;
 }
