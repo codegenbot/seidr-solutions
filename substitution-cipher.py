7906d9129623493315a27124fa315f1c8f933496
beep boop <githubbot@vadim.me>
2024-07-13 23:55:15 +0200

    Solution 46 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/jmv4t930 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/32411055-7811-44a2-bf78-0aa8644b4 431.py", line 20, in <module>     print(substitution_cipher(cipher_map1, message))           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/32411055-7811-44a2-bf78-0aa8644b4 431.py", line 7, in substitution_cipher     cipher_map[index].lower()     ~~~~~~~~~~^^^^^^^ IndexError: string index out of range

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -1,6 +1,8 @@
-def substitution_cipher(char1, char2, message):
+def substitution_cipher():
+    char1, char2 = zip(*[list(input().strip()), list(input().strip())])
+    cipher_map = ''.join(''.join(map(str, x)) for x in [char1, char2])
+    message = input().strip()
     result = ""
-    cipher_map = "".join("".join(map(str, x)) for x in [char1, char2])
     for char in message:
         if char.isalpha():
             index = ord(char.lower()) - 97
@@ -11,10 +13,4 @@ def substitution_cipher(char1, char2, message):
             )
         else:
             result += char
-    return result
-
-
-char1, char2 = zip(*[list(input().strip()), list(input().strip())])
-cipher_map = "".join("".join(map(str, x)) for x in [char1, char2])
-
-print(substitution_cipher(char1, char2, input().strip()))
+    print(result)
