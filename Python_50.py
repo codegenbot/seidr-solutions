5a78a4e25274e2e7f24f05dbc275959dbbf5014e
beep boop <githubbot@vadim.me>
2024-07-13 07:33:29 +0200

    Solution 74 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/u5fnzvz7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f727bf8e-9035-4a09-9661-6cac85c90 220.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_50.py
+++ Python_50.py
@@ -4,8 +4,17 @@ def decode_shift():
         encoded = input("Enter the encoded message: ")
         if len(password) == 1 and len(encoded) > 0:
             shift = (ord(password[0].upper()) - 65) % 26
-            decoded = "".join([chr(((ord(ch) - 97 + 26 - shift) % 26) + 97) if ch.isalpha() or not ch.isspace() else ch for ch in encoded])
+            decoded = "".join(
+                [
+                    (
+                        chr(((ord(ch) - 97 + 26 - shift) % 26) + 97)
+                        if ch.isalpha() or not ch.isspace()
+                        else ch
+                    )
+                    for ch in encoded
+                ]
+            )
             return decoded
 
-result = decode_shift()
-print(result)
+
+print(decode_shift())
