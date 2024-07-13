736a52690f38127897a9a442917234068539ee90
beep boop <githubbot@vadim.me>
2024-07-13 04:45:16 +0200

    Solution 41 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fhfry2e5 Prompt:     1 | Here's the solution:       |     ^ 06dc72d0-2f99-4841-8cf1-cbd250d3b86e.cpp:1:5: error: missing terminating ' chara cter     1 | Here's the solution:       |     ^~~~~~~~~~~~~~~~ 06dc72d0-2f99-4841-8cf1-cbd250d3b86e.cpp:1:1: error: ‘Here’ does not name a type     1 | Here's the solution:       | ^~~~ In file included from 06dc72d0-2f99-4841-8cf1-cbd250d3b86e.cpp:15: 06dc72d0-2f99-4841-8cf1-cbd250d3b86e.cpp: In function ‘int main()’: 06dc72d0-2f99-4841-8cf1-cbd250d3b86e.cpp:17:35: error: ‘lst’ was not declared in  this scope    17 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 06dc72d0-2f99-4841-8cf1-cbd250d3b86e.cpp:17:13: error: ‘double_the_difference’ w as not declared in this scope    17 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ 06dc72d0-2f99-4841-8cf1-cbd250d3b86e.cpp:17:43: error: ‘odd_sum’ was not declare d in this scope    17 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -14,7 +14,7 @@ long long double_the_difference(std::vector<float> lst) {
 }
 
 int main() {
-    std::vector<float> lst = {1.0, 3.5, 4.0, 6.7};
+    std::vector<float> lst = {1.0f, 3.5f, 4.0f, 5.0f, 7.0f};
     int odd_sum = double_the_difference(lst);
     return 0;
 }
