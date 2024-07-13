0f3840e0b3a22b84d221867a197e0b0d9fe62c32
beep boop <githubbot@vadim.me>
2024-07-13 12:49:45 +0200

    Solution 2 of Python/65, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nj12jp9p Prompt: Complete the following code given the task description and function signature.

--- Python_65.py
+++ Python_65.py
@@ -1,5 +1,6 @@
 def circular_shift(x, shift):
-    str_x = str(x)[::-1]
-    return str(
-        int(str_x[int(len(str_x) - shift) :]) + int(str_x[: int(len(str_x) - shift)])
-    )
+    x = str(x)
+    if len(x) <= shift:
+        return x[::-1]
+    else:
+        return x[-shift:] + x[:-shift]
