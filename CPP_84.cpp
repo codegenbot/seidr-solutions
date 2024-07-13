4604a07fbb696ac4aee170521529b933d81a2716
beep boop <githubbot@vadim.me>
2024-07-13 02:49:48 +0200

    Solution 5 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/kq5vdkvl Prompt: Complete the following code given the task description and function signature.

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,21 +1,12 @@
 string solve(int N){
     int sum = 0;
-    while(N > 0) {
+    string res = "";
+    while(N > 0){
         int remainder = N % 2;
         sum += remainder;
+        if(remainder == 1)res.push_back('1');
+        else res.push_back('0');
         N /= 2;
     }
-    return toBinaryString(sum);
-}
-
-string toBinaryString(int n) {
-    string str = "";
-    while(n > 0) {
-        if(n % 2 == 0)
-            str = "0" + str;
-        else
-            str = "1" + str;
-        n /= 2;
-    }
-    return str;
+    return res;
 }
