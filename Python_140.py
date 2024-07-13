d4ed3c5343a6351de9b9933c6c427d40e8d17e05
beep boop <githubbot@vadim.me>
2024-07-13 11:39:17 +0200

    Solution 99 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/puafi84q Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fd987d09-7e6d-4d3c-941b-3a7f3c522 6dc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,3 +1,1 @@
-```
-new_text = input("Enter some text: ")
-print(f"{'' if re.match(r'^[a-zA-Z_]+$', new_text) else new_text.capitalize()}"
+print(f"{'' if re.match(r'^[a-zA-Z_]+$', new_text) else new_text.capitalize()}")
