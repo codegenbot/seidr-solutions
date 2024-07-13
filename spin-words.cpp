ab5960cc99bc0040d3673126ba649756c7971eb5
beep boop <githubbot@vadim.me>
2024-07-14 00:54:32 +0200

    Solution 77 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/drhe2edt Prompt: it must return j omrxhq iudhykho for input j qhxrmo ohkyhdui, but it returns Enter a sentence: terminate called after throwing an instance of 'std::out_of_ra nge'   what():  basic_string::substr: __pos (which is 18446744073709546156) > this->s ize() (which is 17).

--- spin-words.cpp
+++ spin-words.cpp
@@ -6,43 +6,38 @@ using namespace std;
 
 string spinWords(string str) {
     string result = "";
-    int i = 0;
-    
-    while(i <= str.length() - 1) {
-        if(str[i] == ' ') {
-            result += str.substr(i - (i - i + 1), i - i + 1) + " ";
-            i++;
+    int wordLength = 0;
+
+    for (int i = 0; i <= str.length() - 1; i++) {
+        if (str[i] == ' ') {
+            result += str.substr(i - wordLength + 1, wordLength) + " ";
+            wordLength = 0;
         } else {
-            i++;
+            wordLength++;
         }
     }
+
+    result += str.substr(str.length() - wordLength);
     
-    result += str.substr(i - (i - i));
-    
-    for(int j = 0; j < result.length(); j++) {
-        if(result[j] == ' ') {
-            string temp = result.substr(j + 1);
-            for(long long k = temp.length() - 1; k >= 0; k--) {
-                result.replace(j, temp.length(), temp.substr(k));
-                j += temp.length();
-                break;
+    for(int i = 0; i < result.length(); i++) {
+        if(result[i] == ' ') {
+            string temp = result.substr(i + 1);
+            for(long long j = temp.length() - 1; j >= 0; j--) {
+                if(temp[j].isalnum() && temp.length() > 4) {
+                    result.replace(i, temp.length(), temp.substr(j));
+                    i += temp.length();
+                    break;
+                }
             }
-        } else if(result[j+1] == ' ') {
-            string temp = result.substr(j + 1);
-            for(long long k = temp.length() - 1; k >= 0; k--) {
-                result.replace(j + 1, temp.length(), temp.substr(k));
-                j += temp.length();
-                break;
+        } else if(result[i+1] == ' ') {
+            string temp = result.substr(i + 1);
+            for(long long j = temp.length() - 1; j >= 0; j--) {
+                if(temp[j].isalnum() && temp.length() > 4) {
+                    result.replace(i + 1, temp.length(), temp.substr(j));
+                    i += temp.length();
+                    break;
+                }
             }
-        } else if(result[j+4] == ' ') {
-            string temp = result.substr(j + 1);
-            for(long long k = temp.length() - 1; k >= 0; k--) {
-                result.replace(j + 1, temp.length(), temp.substr(k));
-                j += temp.length();
-                break;
-            }
-        } else {
-            i++;
         }
     }
 
