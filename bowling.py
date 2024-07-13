b24e51cbf61637487ff36925aba6c1bccb99ba1f
beep boop <githubbot@vadim.me>
2024-07-13 05:41:12 +0200

    Solution 34 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/062uzipw Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8b4f6071-7bd8-4a6d-8e36-fb157b8c9 8dc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,35 +1,47 @@
 ```
-def bowling_score(game):
-    frames = game.split('/')
-    total_score = 0
+def calculate_score(frames):
+    score = 0
     frame_number = 1
+
     for i in range(len(frames)):
         frame = frames[i]
-        if len(frame) == 1:
-            total_score += 10
-        elif 'X' in frame:
-            if frame_number < 9 and frames[i+1][0] != 'X':
-                if frames[i+1][0] == 'X':
-                    total_score += 10 + int(str(frame.replace('X', ''))[1:])
-                else:
-                    total_score += 10 + int(frames[i+1].replace('X', '', 1)) + int(str(frame.replace('X', ''))[1:])
-            else:
-                total_score += 10
-        elif '-' in frame:
-            first_roll, second_roll = map(int, frame.split('-'))
-            if first_roll == 10:
-                total_score += 10 + int(frames[i+2].replace('X', '', 1)) + int(str(frame.replace('-', ''))[1:])
-            else:
-                total_score += first_roll + second_roll
-        else:
-            first_roll, second_roll = map(int, frame.split())
-            if first_roll == 10:
-                total_score += 10 + int(frames[i+2].replace('X', '', 1)) + int(str(frame.replace(' ', ''))[1:])
-            elif second_roll == 10:
-                total_score += 10 + first_roll + second_roll
-            else:
-                total_score += first_roll + second_roll
-        frame_number += 1
-    return total_score
 
-print(bowling_score("X1-1681357-0/XX7/4"))
+        if len(frame) == 2:
+            first_roll, second_roll = map(int, frame.split('-'))
+            if first_roll + second_roll == 10:
+                score += first_roll + second_roll
+                if frame_number < 9 and frames[i+1][0] != '/':
+                    if frames[i+1].split()[0] == 'X':
+                        score += 10
+                    else:
+                        score += int(frames[i+1].split()[1]) + int(frames[i+1].split()[2])
+                frame_number += 1
+            elif first_roll == 10 or second_roll == 10:
+                if first_roll == 10:
+                    score += 10 + int(frames[i+1].replace('/', '')[1:])
+                else:
+                    score += 10 + int(frames[i+2].replace('/', '' )[1:])
+            else:
+                score += first_roll + second_roll
+        elif len(frame) == 1:
+            if frame[0] == 'X':
+                score += 10
+                if frame_number < 9 and frames[i+1][0] != '/':
+                    if frames[i+1].split()[0] == 'X':
+                        score += 10
+                    else:
+                        score += int(frames[i+1].split()[1]) + int(frames[i+1].split()[2])
+            frame_number += 1
+        elif frame[0] == 'X' and len(frame) > 1:
+            if frame.replace('/', '')[1:] == '10':
+                score += 10 + 5
+            else:
+                score += 10 + int(frame.replace('/', '' )[1:])
+        else:
+            first_roll = int(frame.split()[0])
+            second_roll = int(frame.split()[2])
+            score += first_roll + second_roll
+
+    return score
+
+print(calculate_score("X1-1681357-0/XX7/4"))
