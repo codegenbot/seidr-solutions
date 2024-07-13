dfdad84f0022d64d96e657f609c5e03443fec6fc
beep boop <githubbot@vadim.me>
2024-07-13 03:40:59 +0200

    Solution 68 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d45c05lr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/684a8bc1-3f48-4e36-80a1-6fdf5e206 005.py", line 9, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/684a8bc1-3f48-4e36-80a1-6fdf5e206 005.py", line 6, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -2,4 +2,4 @@
 def make_palindrome(string: str) -> str:
     cleaned_string = "".join(char for char in string if char.isalnum()).lower()
     half = cleaned_string[:len(cleaned_string)//2]
-    return "#{}#".format(half + half[::-1]).center(len(cleaned_string)*4, '#')[2:-2].upper()
+    return "#{}#".format(half + (cleaned_string[len(cleaned_string)//2:] or "") + half[::-1]).center(len(cleaned_string)*4, '#')[2:-2].upper()
