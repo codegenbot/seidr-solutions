3d2c2db85d5ea5237665249e77f31608a0b4bf11
beep boop <githubbot@vadim.me>
2024-07-13 18:58:03 +0200

    Solution 32 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/58a0rhv6 Prompt: 32283738-75d1-4eba-abbe-5aef2d0cc66d.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -2,37 +2,28 @@
 
 int bowlingScore(string s) {
     int score = 0;
-    for (int i = 0; i < 10; i++) {
-        if (s[i] == '/') {
-            string firstPart = s.substr(0, i);
-            string secondPart = s.substr(i + 1);
-            int firstScore = 0;
-            int j = 0;
-            while (j < firstPart.length()) {
-                if (firstPart[j] != 'X') {
-                    firstScore += (firstPart[j] - '0');
-                }
-                else
-                    firstScore += 10;
-                j++;
+    int i = 0;
+    while (i < 10) {
+        if (s[i] == 'X') {
+            score += 10;
+            i++;
+            if (i < 10 && s[i] == '/') {
+                i++;
             }
-            int secondScore = 0;
-            j = 0;
-            while (j < secondPart.length()) {
-                if (secondPart[j] != 'X') {
-                    secondScore += (secondPart[j] - '0');
-                }
-                else
-                    secondScore += 10;
-                j++;
-            }
-            score += firstScore + secondScore;
+        } else if (s[i] == '/') {
+            int firstScore = (s[i - 1] - '0') * 2 + (s[i] - '0');
+            score += firstScore;
+            i++;
         } else {
-            if (s[i] == 'X') {
-                score += 10;
+            int framescore = (s[i] - '0') + (s[i + 1] - '0');
+            if (framescore == 10) {
+                score += framescore;
+                i += 2;
             } else {
-                score += (s[i] - '0') * 2;
+                score += framescore;
+                i += 2;
             }
         }
     }
     return score;
+}
