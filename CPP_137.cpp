e2ad768489a2ed26a5566518ea8482471aa8b89c
beep boop <githubbot@vadim.me>
2024-07-13 09:23:03 +0200

    Solution 97 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8fxvx5aq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,25 +1,84 @@
-if (a.type() == typeid(int) && b.type() == typeid(int)) {
-    int x = boost::any_cast<int>(a);
-    int y = boost::any_cast<int>(b);
-    if (x > y) return a;
-    else if (y > x) return b;
-    else return "None";
-} else if (a.type() == typeid(double) && b.type() == typeid(double)) {
-    double x = boost::any_cast<double>(a);
-    double y = boost::any_cast<double>(b);
-    if (x > y) return a;
-    else if (y > x) return b;
-    else return "None";
-} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-    string x = boost::any_cast<string>(a);
-    string y = boost::any_cast<string>(b);
-    if (x > y) return a;
-    else if (y > x) return b;
-    else return "None";
-} else if ((a.type() == typeid(int) && b.type() != typeid(int)) || 
-           (a.type() != typeid(int) && b.type() == typeid(int))) {
-    if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) return a;
-    else return b;
-} else {
-    return "None";
+Here is the solution:
+
+```cpp
+#include <boost/algorithm/string.hpp>
+using namespace boost;
+
+boost::any compare_one(boost::any a, boost::any b) {
+    if (get_if<int>(&aConvert)) {
+        int aInt = aConvert;
+        if (get_if<float>(&bConvert)) {
+            float bFloat = bConvert;
+            if (aInt < bFloat)
+                return b;
+            else if (aInt > bFloat)
+                return a;
+            else
+                return "None";
+        } else if (get_if<std::string>(&bConvert)) {
+            std::string bStr = bConvert;
+            boost::replace_all(bStr, ",", ".");
+            float bFloat = atof(bStr.c_str());
+            if (aInt < bFloat)
+                return b;
+            else if (aInt > bFloat)
+                return a;
+            else
+                return "None";
+        }
+    } else if (get_if<float>(&aConvert)) {
+        float aFloat = aConvert;
+        if (get_if<int>(&bConvert)) {
+            int bInt = bConvert;
+            if (aFloat < bInt)
+                return b;
+            else if (aFloat > bInt)
+                return a;
+            else
+                return "None";
+        } else if (get_if<std::string>(&bConvert)) {
+            std::string bStr = bConvert;
+            boost::replace_all(bStr, ",", ".");
+            float bFloat = atof(bStr.c_str());
+            if (aFloat < bFloat)
+                return b;
+            else if (aFloat > bFloat)
+                return a;
+            else
+                return "None";
+        }
+    } else if (get_if<std::string>(&aConvert)) {
+        std::string aStr = aConvert;
+        boost::replace_all(aStr, ",", ".");
+        float aFloat = atof(aStr.c_str());
+        if (get_if<int>(&bConvert)) {
+            int bInt = bConvert;
+            if (aFloat < bInt)
+                return b;
+            else if (aFloat > bInt)
+                return a;
+            else
+                return "None";
+        } else if (get_if<float>(&bConvert)) {
+            float bFloat = bConvert;
+            if (aFloat < bFloat)
+                return b;
+            else if (aFloat > bFloat)
+                return a;
+            else
+                return "None";
+        } else if (get_if<std::string>(&bConvert)) {
+            std::string bStr = bConvert;
+            boost::replace_all(bStr, ",", ".");
+            float bFloat = atof(bStr.c_str());
+            if (aFloat < bFloat)
+                return b;
+            else if (aFloat > bFloat)
+                return a;
+            else
+                return "None";
+        }
+    }
+
+    return a;
 }
