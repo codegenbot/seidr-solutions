9312052db1f1862ca7cbea832d294b3e976a3c70
beep boop <githubbot@vadim.me>
2024-07-13 05:36:01 +0200

    Solution 75 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tcf481c2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/13df5cf0-12b0-4714-ba9b-2104938f2 8f3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_37.py
+++ Python_37.py
@@ -1,4 +1,4 @@
-"""
+```
 def sort_even(l: list):
     even_list = [i for i in l if i % 2 == 0]
     even_list.sort()
@@ -8,3 +8,7 @@ def sort_even(l: list):
 def check():
     numbers = [int(i) for i in input("Enter space-separated numbers: ").split()]
     print(sort_even(numbers))
+
+
+if __name__ == "__main__":
+    check()
