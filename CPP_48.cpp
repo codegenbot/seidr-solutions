3f99f213a9621fc124131c49874aa12e2f7a3c72
beep boop <githubbot@vadim.me>
2024-07-13 02:06:39 +0200

    Solution 10 of CPP/48, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1c7qcq3m Prompt: 6c882b18-3c7c-48d9-8259-e441bfa98fd5.cpp:1:20: error: ‘string’ was not declared in this scope     1 | bool is_palindrome(string text) {       |                    ^~~~~~ In file included from 6c882b18-3c7c-48d9-8259-e441bfa98fd5.cpp:11: 6c882b18-3c7c-48d9-8259-e441bfa98fd5.cpp: In function ‘int main()’: 6c882b18-3c7c-48d9-8259-e441bfa98fd5.cpp:13:26: error: ‘is_palindrome’ cannot be  used as a function    13 |     assert (is_palindrome("xywzx") == false);       |             ~~~~~~~~~~~~~^~~~~~~~~

--- CPP_48.cpp
+++ CPP_48.cpp
@@ -1,9 +1,11 @@
-bool is_palindrome(string text) {
-    int i = 0, j = text.length() - 1;
-    while (i < j) {
-        if (text[i] != text[j]) return false;
-        i++;
-        j--;
+#include <string>
+
+bool is_palindrome(std::string text) {
+    int left = 0, right = text.length() - 1;
+    while (left < right) {
+        if (text[left] != text[right]) return false;
+        left++;
+        right--;
     }
     return true;
 }
