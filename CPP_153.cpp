bc4110c9e6bdca461227f633b49510c4c6d35e7d
beep boop <githubbot@vadim.me>
2024-07-13 04:56:06 +0200

    Solution 43 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9oxbifq3 Prompt:       |                         ^~~~~~ 4224ed1b-cfd9-4537-a03d-9ead9a185ff4.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ 4224ed1b-cfd9-4537-a03d-9ead9a185ff4.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ 4224ed1b-cfd9-4537-a03d-9ead9a185ff4.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ 4224ed1b-cfd9-4537-a03d-9ead9a185ff4.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from 4224ed1b-cfd9-4537-a03d-9ead9a185ff4.cpp:19: 4224ed1b-cfd9-4537-a03d-9ead9a185ff4.cpp: In function ‘int main()’: 4224ed1b-cfd9-4537-a03d-9ead9a185ff4.cpp:21:32: error: ‘Strongest_Extension’ can not be used as a function    21 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,16 +1,16 @@
 ```cpp
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 
 int main() {
     std::cout << Strongest_Extension("Sp", {"671235", "Bb"}) << std::endl;
     return 0;
 }
 
-std::string Strongest_Extension(std::string class_name, std::vector<std::string> extensions){
+int Strongest_Extension(string class_name, vector<string> extensions){
     int strongest = 0;
-    std::string strongest_extension;
+    string strongest_extension;
     for(auto extension : extensions){
         int cap = 0, sm = 0;
         for(char c : extension){
