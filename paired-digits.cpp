1ef0adc2e74c6f59321f2a76ac12575cc367a363
beep boop <githubbot@vadim.me>
2024-07-13 18:59:33 +0200

    Solution 69 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/67fx038w Prompt: 6e1f352e-4c4a-477a-b194-d252a71a377e.cpp:5:66: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? 6e1f352e-4c4a-477a-b194-d252a71a377e.cpp:6:26: error: ‘stoi’ is not a member of ‘std’     6 |             sum += (std::stoi(std::string(1, s[i]))) * 2;       |                          ^~~~ 6e1f352e-4c4a-477a-b194-d252a71a377e.cpp:6:36: error: ‘string’ is not a member o f ‘std’     6 |             sum += (std::stoi(std::string(1, s[i]))) * 2;       |                                    ^~~~~~ 6e1f352e-4c4a-477a-b194-d252a71a377e.cpp:6:36: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? 6e1f352e-4c4a-477a-b194-d252a71a377e.cpp:8:25: error: ‘stoi’ is not a member of ‘std’     8 |             sum += std::stoi(std::string(1, s[i]));       |                         ^~~~ 6e1f352e-4c4a-477a-b194-d252a71a377e.cpp:8:35: error: ‘string’ is not a member o f ‘std’     8 |             sum += std::stoi(std::string(1, s[i]));       |                                   ^~~~~~ 6e1f352e-4c4a-477a-b194-d252a71a377e.cpp:8:35: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’?

--- paired-digits.cpp
+++ paired-digits.cpp
@@ -1,14 +1,14 @@
-```cpp
-int pairedDigits(const char* s) {
-    std::string str = s;
+#include <string>
+#include <stdexcept>
+
+int pairedDigits(const std::string& s) {
     int sum = 0;
     size_t i = 0; 
-    for (; i < str.size() - 1; ++i) { 
-        if ((str[i] - '0') == (str[i + 1] - '0')) {
-            sum += (str[i] - '0') * 2;
+    for (; i < s.size() - 1; ++i) { 
+        if ((std::stoi(std::string(1, s[i]))) == (std::stoi(std::string(1, s[i + 1])))) {
+            sum += (std::stoi(std::string(1, s[i]))) * 2;
         } else {
-            sum += str[i] - '0';
+            sum += std::stoi(std::string(1, s[i]));
         }
     }
-    return sum; 
-}
+    return sum;
