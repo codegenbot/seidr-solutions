bd837a7d25c35f61c33a0c9b28208b90e8ea41d6
beep boop <githubbot@vadim.me>
2024-07-13 03:07:41 +0200

    Solution 31 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/7ae6bski Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/14050790-4173-4397-9006-043d90a94 33d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_29.py
+++ Python_29.py
@@ -1,6 +1,5 @@
-```
-def filter_by_prefix(path=None) -> None:
-    string_list: List[str] = []
+def filter_by_prefix() -> None:
+    string_list = []
     while True:
         s = input("Enter a list of strings (one per line), separate them with '%%': ")
         if s.lower() == "%%":
