4dc5d81f66109b3fbf083fe1d4225275e0e38975
beep boop <githubbot@vadim.me>
2024-07-13 09:28:25 +0200

    Solution 31 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vgofggry Prompt: Complete the following code given the task description and function signature.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -2,23 +2,29 @@
 #include <openssl/x509v3.h>
 
 string string_to_md5(string text) {
-    unsigned char md5[16];
-    const EVP_MD* evp_md = EVP_md5();
-    unsigned char* input = (unsigned char*)text.c_str();
-    int len = text.length();
-    unsigned int input_len = len;
-    
-    MD5_CTX ctx;
-    MD5Init(&ctx);
-    MD5Update(&ctx, input, input_len);
-    MD5Final(md5, &ctx);
+    MD5_CTX md5ctx;
+    unsigned char result[16];
+    string output;
 
-    string result;
-    for(int i=0; i<16; i++) {
-        char buff[3];
-        sprintf(buff, "%02x", md5[i]);
-        result += buff;
+    if (text.empty()) return "None";
+
+    MD5_Init(&md5ctx);
+    MD5_Update(&md5ctx, text.c_str(), text.size());
+    MD5_Final(result, &md5ctx);
+
+    for (int i = 0; i < 16; ++i) {
+        char c = (char)(result[i] >> 4);
+        if (c > '9' && c < 'A') c -= 7;
+        else if (c > 'f' && c < 'g') c -= 5;
+
+        output += c;
+
+        c = (char)(result[i] & 0x0F);
+        if (c > '9' && c < 'A') c -= 7;
+        else if (c > 'f' && c < 'g') c -= 5;
+
+        output += c;
     }
-    
-    return result;
+
+    return output;
 }
