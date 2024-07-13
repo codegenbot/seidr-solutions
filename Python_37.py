21ed00e1bad4820aa9581efc958f580c3558fc1c
beep boop <githubbot@vadim.me>
2024-07-13 02:42:03 +0200

    Solution 15 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tcf481c2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7c501e0a-1b38-4135-a712-e02551d70 5a1.py", line 10, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7c501e0a-1b38-4135-a712-e02551d70 5a1.py", line 5, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,3 +1,12 @@
 def sort_even(l: list):
     even_nums = sorted([x for x in l if x % 2 == 0])
-    return [x if i < len(even_nums) else even_nums[-1] for i, x in enumerate(l)]
+    result = []
+    i, j = 0, 0
+    while i < len(l):
+        if j < len(even_nums) and l[i] % 2 != 0:
+            result.append(even_nums[j])
+            j += 1
+        else:
+            result.append(l[i])
+        i += 1
+    return result
