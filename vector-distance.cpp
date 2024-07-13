c45008a52c7c6110ac7897d67ee1fbc5e6e71cb7
beep boop <githubbot@vadim.me>
2024-07-13 08:20:46 +0200

    Solution 62 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/q3omfljs Prompt: 568e2bf0-65d1-4406-9a47-f0ede9548863.cpp: In function ‘double vectorDistance(int )’: 568e2bf0-65d1-4406-9a47-f0ede9548863.cpp:22:16: error: ‘pow’ was not declared in  this scope    22 |         sum += pow(v1[i] - v2[i], 2);       |                ^~~ 568e2bf0-65d1-4406-9a47-f0ede9548863.cpp:24:12: error: ‘sqrt’ was not declared i n this scope    24 |     return sqrt(sum);       |            ^~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -9,18 +9,18 @@ using namespace std;
 double vectorDistance(int n) {
     cin >> n;
     vector<float> v1(n);
+    vector<float> v2(n);
+
     for (int i = 0; i < n; i++) {
         cin >> v1[i];
     }
-    cin >> n;
-    vector<float> v2(n);
     for (int i = 0; i < n; i++) {
         cin >> v2[i];
     }
 
     double sum = 0.0;
     for (int i = 0; i < n; i++) {
-        sum += pow((v1[i] - v2[i]), 2);
+        sum += pow(v1[i] - v2[i], 2);
     }
     return sqrt(sum);
 }
@@ -32,3 +32,4 @@ int main() {
     cout << fixed << setprecision(10) << vectorDistance(n) << endl;
 
     return 0;
+}
