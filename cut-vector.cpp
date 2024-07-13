df826a7d79f973effac31bb0af170bcaef6f6ea6
beep boop <githubbot@vadim.me>
2024-07-14 01:33:49 +0200

    Solution 49 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/thrw49vk Prompt: ed6a327d-faa8-46dd-9963-9fc0bc4d17c3.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<vector<int>> cutVector(vector<int> v) {       | ^~~~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,6 +1,3 @@
-#include <climits>
-#include <vector>
-#include <iostream>
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int> v) {
@@ -21,68 +18,22 @@ vector<vector<int>> cutVector(vector<int> v) {
     res.push_back(vector<int>());
     res.push_back(vector<int>());
 
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n; i++) {
         long long diff = abs(leftSum - rightSum);
-        if (leftSum == rightSum) { 
-            res[0].assign(v.begin(), v.begin() + i + 1);
-            res[1] = vector<int>(v.begin() + i + 1, v.end());
-        } else {
+        if (diff < minDiff) { 
             minDiff = diff;
-            cutIndex = i; 
+            cutIndex = i;
         }
         leftSum += v[i];
         rightSum -= v[i];
     }
     
-    res[0].resize(cutIndex);
-    for(int i = 0; i < cutIndex; i++) {
+    res[0].resize(cutIndex + 1);
+    for(int i = 0; i <= cutIndex; i++) {
         res[0].push_back(v[i]);
     }
     
-    res[1].assign(v.begin() + cutIndex, v.end());
-    
-    if(res[0].size() > res[1].size()) {
-        vector<int> temp = res[0];
-        res[0] = res[1];
-        res[1] = temp;
-    }
+    res[1].assign(v.begin() + (cutIndex + 1), v.end());
     
     return res;
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-    
-    vector<vector<int>> result = cutVector(v);
-    if(result[1].size() == 0) { 
-        cout << "[";
-        for (int i = 0; i < v.size(); i++) {
-            cout << v[i] << " ";
-        }
-        cout << "]" << endl;
-        cout << "[";
-        vector<int> empty;
-        for(int i = 0; i < v.size(); i++) {
-            empty.push_back(v[i]);
-        }
-        cout << "]";
-    } else {
-        cout << "[";
-        for (int i = 0; i < result[1].size(); i++) {
-            cout << result[1][i] << " ";
-        }
-        cout << "]" << endl;
-        cout << "[";
-        for (int i = 0; i < result[0].size(); i++) {
-            cout << result[0][i] << " ";
-        }
-        cout << "]" << endl;
-    }
-    
-    return 0;
 }
