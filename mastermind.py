1fc4d56ceed119125b210e5ac0f94facdd1820be
beep boop <githubbot@vadim.me>
2024-07-13 22:50:09 +0200

    Solution 79 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/kg0ifw5l Prompt: it must return 1 2 for input RBGB RWBB, but it returns .

--- mastermind.py
+++ mastermind.py
@@ -1,15 +1,15 @@
 def mastermind(code, guess):
     black_pegs = 0
-    white_pegs_color = 0
-    white_pegs_position = 0
+    white_pegs_correct_color = 0
+    white_pegs = 0
 
     for i in range(4):
         if code[i] == guess[i]:
             black_pegs += 1
         elif str(code[i]) in str(guess):
-            if i < len([x for x in guess if x == code[i]]):
-                white_pegs_position += 1
+            if code.index(str(code[i])) == guess.index(str(guess[i])):
+                black_pegs += 1
             else:
-                white_pegs_color += 1
+                white_pegs_correct_color += 1
 
-    return str(black_pegs) + "\n" + str(white_pegs_color) + "\n" + str(white_pegs_position)
+    return str(black_pegs) + "\n" + str(white_pegs_correct_color)
