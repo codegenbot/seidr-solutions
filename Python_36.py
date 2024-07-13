7291ac7444039f168754e5d357fafc921a4e10ec
beep boop <githubbot@vadim.me>
2024-07-13 05:54:09 +0200

    Solution 61 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/hv1pyxj2 Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/79c05b13-5d86-4a4a-b146-960139461 2ae.py", line 17, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/79c05b13-5d86-4a4a-b146-960139461 2ae.py", line 14, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -3,12 +3,10 @@ def fizz_buzz(n: int):
     for i in range(1, n + 1):
         if i % 15 == 0:
             print("FizzBuzz")
-        elif i % 3 == 0:
-            if i % 5 == 0:
-                print("FizzBuzz")
-            else:
-                print("Fizz")
-        elif i % 5 == 0:
+            count += 1
+        elif i % 3 == 0 and i % 5 != 0:
+            print("Fizz")
+        elif i % 5 == 0 and i % 3 != 0:
             print("Buzz")
         else:
             print(i)
