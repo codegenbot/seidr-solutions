4defa4bfe36a8afc776b68be18ecade0886f618d
beep boop <githubbot@vadim.me>
2024-07-13 02:52:42 +0200

    Solution 20 of twitter, ratio 0.38 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/aq210r8l Prompt: it must return Your tweet has 3 characters for input 62b, but it returns .

--- twitter.cpp
+++ twitter.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
     string tweet;
     getline(cin, tweet);
 
+    tweet.erase(remove_if(tweet.begin(), tweet.end(), ::isspace), tweet.end()); // Remove whitespaces
+
     if (tweet.empty()) {
         cout << "You didn't type anything" << endl;
-    } else if (tweet.size() >= 140) {
+    } else if (tweet.size() > 140) {
         cout << "Too many characters" << endl;
     } else {
         cout << "Your tweet has " << tweet.size() << " characters" << endl;
