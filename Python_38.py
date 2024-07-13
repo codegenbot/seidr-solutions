2c92bfa1c7b5c45871a2f9871b41eb399dae2d0b
beep boop <githubbot@vadim.me>
2024-07-13 05:53:14 +0200

    Solution 77 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ry4fgrcz Prompt: Complete the following code given the task description and function signature.

--- Python_38.py
+++ Python_38.py
@@ -1,4 +1,6 @@
 def decode_cyclic(s: str):
-    # reverse the cyclic shift in each group. Unless group has fewer elements than 3.
-    groups = [(group[-1] + group[:-1]) if len(group) == 3 else group for group in s]
+    groups = [s[(3 * i) : min((3 * i + 3), len(s))] for i in range((len(s) + 2) // 3)]
+    groups = [
+        (group[-1] + group[:-1]) if len(group) == 3 else group for group in groups
+    ]
     return "".join(groups)
