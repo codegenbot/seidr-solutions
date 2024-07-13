726177209221ecbd8829592a490bf1df89207103
beep boop <githubbot@vadim.me>
2024-07-13 02:30:14 +0200

    Solution 9 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/tbfgq7vb Prompt: Complete the following code given the task description and function signature.

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -5,7 +5,7 @@ bool will_it_fly(vector<int> q, int w) {
     for (int i : q) {
         str += to_string(i);
     }
-    if (str != reverse(str).s) {
+    if (!is_palindrome(str)) {
         return false;
     }
     int sum = 0;
@@ -13,4 +13,10 @@ bool will_it_fly(vector<int> q, int w) {
         sum += i;
     }
     return sum <= w;
+}
+
+bool is_palindrome(string s) {
+    string t = s;
+    reverse(t.begin(), t.end());
+    return s == t;
 }
