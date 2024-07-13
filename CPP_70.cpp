673c2eb97b75c75b16941ee3db12dd32b902d040
beep boop <githubbot@vadim.me>
2024-07-13 02:58:20 +0200

    Solution 40 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/80idpyzn Prompt: 15837b77-8278-4e4b-b0e1-a69db80a21e7.cpp:61:5: error: redefinition of ‘int main( )’    61 | int main(){       |     ^~~~ 15837b77-8278-4e4b-b0e1-a69db80a21e7.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~ In file included from 15837b77-8278-4e4b-b0e1-a69db80a21e7.cpp:52: 15837b77-8278-4e4b-b0e1-a69db80a21e7.cpp: In function ‘int main()’: 15837b77-8278-4e4b-b0e1-a69db80a21e7.cpp:62:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    62 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,3 +1,4 @@
+```
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -23,7 +24,7 @@ vector<int> strange_sort_vector(vector<int> lst) {
     return result;
 }
 
-int mainFunc() {
+int main() {
     vector<int> test1 = {1, 2, 3, 4};
     vector<int> test2 = {5, 5, 5, 5};
     vector<int> test3 = {};
