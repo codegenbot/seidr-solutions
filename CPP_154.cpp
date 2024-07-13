25d85162b74a3a68d95284bb4ed6ffa25a498c4d
beep boop <githubbot@vadim.me>
2024-07-13 04:16:38 +0200

    Solution 0 of CPP/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/tohu4wht Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_154.cpp
@@ -0,0 +1,11 @@
+bool cycpattern_check(string a, string b) {
+    for (int i = 0; i < b.length(); i++) {
+        string temp = b;
+        for (int j = 0; j <= b.length() - i; j++) {
+            if (a.find(temp) != string::npos)
+                return true;
+            rotate(temp.begin(), temp.end());
+        }
+    }
+    return false;
+}
