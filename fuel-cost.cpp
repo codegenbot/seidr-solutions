e68c6be4f8cc42eb0d86558c20324606623fb243
beep boop <githubbot@vadim.me>
2024-07-13 21:15:21 +0200

    Solution 89 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/f9kqk8bk Prompt: 89329620-d8c0-4a53-8d21-cf5235cc6c42.cpp:3:23: error: ‘vector’ was not declared in this scope     3 | int calculateFuelCost(vector<int> nums) {       |                       ^~~~~~ 89329620-d8c0-4a53-8d21-cf5235cc6c42.cpp:3:23: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 89329620-d8c0-4a53-8d21-cf5235cc6c42.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 89329620-d8c0-4a53-8d21-cf5235cc6c42.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 89329620-d8c0-4a53-8d21-cf5235cc6c42.cpp:3:30: error: expected primary-expressio n before ‘int’     3 | int calculateFuelCost(vector<int> nums) {       |                              ^~~

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -1,6 +1,7 @@
 #include <vector>
+using namespace std;
 
-int calculateFuelCost(std::vector<int> nums) {
+int calculateFuelCost(vector<int> nums) {
     int sum = 0;
     for (int num : nums) {
         sum += (num / 3) - 2;
