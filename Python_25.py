b7e488eee54e5f11d54a9482fe6fce5ab3f31ce9
beep boop <githubbot@vadim.me>
2024-07-13 17:48:52 +0200

    Solution 66 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/2lvtgqs9 Prompt: Complete the following code given the task description and function signature.

--- Python_25.py
+++ Python_25.py
@@ -1,19 +1,13 @@
 from typing import List
+import math
 
 
 def factorize(n: int) -> List[int]:
-    i = 2
     factors = []
-    while i * i <= n:
-        if n % i:
-            i += 1
-        else:
+    for i in range(2, math.isqrt(n) + 1):
+        while n % i == 0:
+            factors.append(i)
             n //= i
-            count = 0
-            while n % i == 0:
-                n //= i
-                count += 1
-            factors.append([i] * count)
     if n > 1:
-        factors.append([n])
-    return sum(factors, [])
+        factors.append(n)
+    return factors
