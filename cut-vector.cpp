70ce7ebda915bd9602d594428bfefbb9eb130143
beep boop <githubbot@vadim.me>
2024-07-14 00:33:03 +0200

    Solution 84 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/cd853toj Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,53 +1,32 @@
-Here is the completed code:
-
 #include <vector>
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int> vec) {
-    int minDiff = INT_MAX;
-    int splitPoint = 0;
+vector<vector<int>> cutVector(vector<int> &nums) {
+    vector<vector<int>> result;
     
-    for (int i = 1; i < vec.size(); i++) {
+    if (nums.size() == 0) {
+        return {{}};
+    }
+    
+    int minDiff = INT_MAX, cutIndex = -1;
+    for (int i = 0; i < nums.size(); ++i) {
         int leftSum = 0, rightSum = 0;
-        
-        for (int j = 0; j < i; j++) {
-            leftSum += vec[j];
+        for (int j = 0; j <= i; ++j) {
+            leftSum += nums[j];
         }
-        
-        for (int j = i; j < vec.size(); j++) {
-            rightSum += vec[j];
+        for (int j = i + 1; j < nums.size(); ++j) {
+            rightSum += nums[j];
         }
         
         int diff = abs(leftSum - rightSum);
-        
-        if (diff < minDiff) {
+        if (diff < minDiff || (diff == minDiff && i > cutIndex)) {
             minDiff = diff;
-            splitPoint = i;
+            cutIndex = i;
         }
     }
     
-    return {{vec.begin(), vec.begin() + splitPoint}, {vec.begin() + splitPoint, vec.end()}};
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) {
-        cin >> vec[i];
-    }
+    result.push_back(vector<int>(nums.begin(), nums.begin() + cutIndex));
+    result.push_back(vector<int>(nums.begin() + cutIndex, nums.end()));
     
-    vector<vector<int>> result = cutVector(vec);
-    
-    cout << "({";
-    for (int num : result[0]) {
-        cout << num << ", ";
-    }
-    cout << "}) (" << " {";
-    for (int num : result[1]) {
-        cout << num << ", ";
-    }
-    cout << "0)" << endl;
-    
-    return 0;
+    return result;
 }
