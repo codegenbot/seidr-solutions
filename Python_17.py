04307f3191ea9511262bbb965308b199fa40639c
beep boop <githubbot@vadim.me>
2024-07-13 02:55:29 +0200

    Solution 39 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9cg3dsjd Prompt: hello o o world! Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7a31090c-05d5-4a67-8f48-24268d4b2 352.py", line 12, in <module>     check(parse_music)           ^^^^^^^^^^^ NameError: name 'parse_music' is not defined

--- Python_17.py
+++ Python_17.py
@@ -2,7 +2,7 @@
 def process_music(s):
     s = s.lower()
     words = s.split()
-    processed_words = [word.replace("-", " ").replace("o", "oh") for word in words]
+    processed_words = [word.replace("-", " ") for word in words]
     output = ' '.join(processed_words)
     return output
 print(process_music("Hello o-o World!"))
