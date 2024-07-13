d91019fa23cad8f4e8658eb83522cdf6d7f733e1
beep boop <githubbot@vadim.me>
2024-07-14 00:50:05 +0200

    Solution 65 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/gab97s0c Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48de8266-5d8f-48ce-b416-3de4cf8e8 276.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,3 +1,4 @@
+```
 def bowling_score(frames):
     score = 0
     index = 0
@@ -11,12 +12,12 @@ def bowling_score(frames):
         else:
             left_pin = int(frames[index])
             right_pin = int(frames[index+1])
-            if frames[index+1] == 'X':
-                frame_score = 10 + 30
+            if right_pin == 'X':
+                frame_score = 10 + 30 - left_pin
             elif frames[index:index+2] == 'X-':
-                frame_score = 10 + 20
+                frame_score = 10 + 20 - left_pin
             else:
-                frame_score = left_pin + right_pin
+                frame_score = left_pin + int(right_pin)
             score += frame_score
             index += 2
     return score
