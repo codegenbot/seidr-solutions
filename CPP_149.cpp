ddfa1d6d207548fea80c60490fff395c47fd5e1c
beep boop <githubbot@vadim.me>
2024-07-13 04:59:22 +0200

    Solution 67 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/t8miolwn Prompt:    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8d78dec9-a7db-4b24-85c9-e24229cfdc64.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8d78dec9-a7db-4b24-85c9-e24229cfdc64.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8d78dec9-a7db-4b24-85c9-e24229cfdc64.cpp:17: 8d78dec9-a7db-4b24-85c9-e24229cfdc64.cpp: In function ‘int main()’: 8d78dec9-a7db-4b24-85c9-e24229cfdc64.cpp:27:20: error: ‘sorted_list_sum’ was not  declared in this scope    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 8d78dec9-a7db-4b24-85c9-e24229cfdc64.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,20 +1,9 @@
-#include <vector>
-using namespace std;
-
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
+    if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); i++) {
         sort(a[i].begin(), a[i].end());
         sort(b[i].begin(), b[i].end());
-        if (a[i] != b[i]) {
-            return false;
-        }
+        if (a[i] != b[i]) return false;
     }
     return true;
-}
-
-int main() {
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"ccc", "dd", "aaaa", "bbbb"}));
 }
