9d648093af167595817a2055c6134062f0bbb347
beep boop <githubbot@vadim.me>
2024-07-13 08:55:56 +0200

    Solution 3 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xmysldia Prompt: Complete the following code given the task description and function signature.

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,15 +1,13 @@
 vector<string> sorted_list_sum(vector<string> lst) {
     auto it = unique(lst.begin(), lst.end(), 
-        [](const string& a, const string& b) { return a.length() % 2 && b.length() % 2; });
+        [](const string& a, const string& b) { return a.length() % 2 == 1; });
     lst.erase(it, lst.end());
-    
     sort(lst.begin(), lst.end(),
         [](const string& a, const string& b) {
-            if (a.length() == b.length()) {
+            if (a.length() != b.length())
+                return a.length() < b.length();
+            else
                 return a < b;
-            }
-            return a.length() < b.length();
         });
-    
     return lst;
 }
