a742dc96e8ab1957f0ce4c2c158d08f8b654d95a
beep boop <githubbot@vadim.me>
2024-07-13 09:26:12 +0200

    Solution 8 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/1i3rwteh Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d437182-8cf5-4c8e-bc4a-b055a67fd 080.py", line 12, in <module>     check(simplify)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d437182-8cf5-4c8e-bc4a-b055a67fd 080.py", line 9, in check     assert simplify("1/5", "1/5") == False, "test13"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test13

--- Python_144.py
+++ Python_144.py
@@ -1,5 +1,6 @@
+```
 def simplify(x, n):
-    x_num, x_denom = map(int, [i for i in x.split("/") if i])
-    n_num, n_denom = map(int, [i for i in n.split("/") if i])
+    x_num, x_denom = map(int, [i for i in x.split('/') if i])
+    n_num, n_denom = map(int, [i for i in n.split('/') if i])
 
-    return (x_num * n_denom) == (n_num * x_denom)
+    return x_num / x_denom == n_num / n_denom
