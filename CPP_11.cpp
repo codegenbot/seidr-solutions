56f401cd63b51969971e47c478fbb0f61333bb6e
beep boop <githubbot@vadim.me>
2024-07-13 05:17:29 +0200

    Solution 12 of CPP/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jqymt5tm Prompt: Complete the following code given the task description and function signature.

--- CPP_11.cpp
+++ CPP_11.cpp
@@ -2,9 +2,9 @@ string string_xor(string a, string b) {
     string result = "";
     for (int i = 0; i < a.length(); i++) {
         if ((a[i] - '0') ^ (b[i] - '0')) {
-            result += "1";
+            result += '1';
         } else {
-            result += "0";
+            result += '0';
         }
     }
     return result;
