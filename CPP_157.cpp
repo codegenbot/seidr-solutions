daa5890e6f3d9f2bb93feaaa8ba99a18ccf4c95e
beep boop <githubbot@vadim.me>
2024-07-13 04:29:10 +0200

    Solution 3 of CPP/157, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ygvvobv9 Prompt: Complete the following code given the task description and function signature.

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,11 +1,5 @@
-Here is the completed code:
-
 bool right_angle_triangle(float a, float b, float c) {
-    float square_a = pow(a, 2);
-    float square_b = pow(b, 2);
-    float square_c = pow(c, 2);
-
-    if (square_a + square_b == square_c || square_a + square_c == square_b || square_b + square_c == square_a)
+    if (a*a + b*b == c*c || a*a + c*c == b*b || b*b + c*c == a*a)
         return true;
     else
         return false;
