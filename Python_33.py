cfcdba2b4e60ccc48435c51476028f0ef3a987e2
beep boop <githubbot@vadim.me>
2024-07-13 03:09:49 +0200

    Solution 22 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mmyso7cs Prompt: 43b.py", line 11, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d5db9826-1f5d-46c6-9343-2758cfa99 43b.py", line 6, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d5db9826-1f5d-46c6-9343-2758cfa99 43b.py", line 4, in sort_third     return [num for _ in range(len(l)) for num in ((nums_divisible_by_3 + other_ nums)[i//3] if i % 3 == 0 else [other_nums[i//3]]) for i in l]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d5db9826-1f5d-46c6-9343-2758cfa99 43b.py", line 4, in <listcomp>     return [num for _ in range(len(l)) for num in ((nums_divisible_by_3 + other_ nums)[i//3] if i % 3 == 0 else [other_nums[i//3]]) for i in l]                ^ UnboundLocalError: cannot access local variable 'i' where it is not associated w ith a value

--- Python_33.py
+++ Python_33.py
@@ -1,14 +1,5 @@
-```
 def sort_third(l):
     nums_divisible_by_3 = sorted([i for i in l if i % 3 == 0], reverse=True)
     other_nums = sorted([i for i in l if i % 3 != 0], reverse=True)
-
-    result = []
-    index = 0
-    for num in l:
-        if num % 3 == 0:
-            result.append(nums_divisible_by_3.pop(0))
-        else:
-            result.append(other_nums.pop(0))
-
-    return result
+    
+    return [num if i % 3 == 0 else other_nums[i//3] for i in range(len(l))]
