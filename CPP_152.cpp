ee30f2049480f17f2728a36dedc9756c064944d7
beep boop <githubbot@vadim.me>
2024-07-13 02:34:56 +0200

    Solution 79 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uswfokmt Prompt: b3a22cd0-d44d-4e92-998f-7508b63fa178.cpp:62:5: error: redefinition of ‘int main( )’    62 | int main(){       |     ^~~~ b3a22cd0-d44d-4e92-998f-7508b63fa178.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~ In file included from b3a22cd0-d44d-4e92-998f-7508b63fa178.cpp:53: b3a22cd0-d44d-4e92-998f-7508b63fa178.cpp: In function ‘int main()’: b3a22cd0-d44d-4e92-998f-7508b63fa178.cpp:63:27: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    63 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~       |                           |       |                           <brace-enclosed initializer list> b3a22cd0-d44d-4e92-998f-7508b63fa178.cpp:13:17: note:   initializing argument 1 of ‘int compare(int, int)’    13 | int compare(int a, int b) {       |             ~~~~^

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <cassert>
 
 using namespace std;
 
-bool issame(int a, int b) {
+bool isSame(int a, int b) {
     if(a == b)
         return true;
     else
@@ -23,7 +22,7 @@ int compare(int a, int b) {
 vector<int> getResult(vector<int> game, vector<int> guess) {
     vector<int> res;
     for(int i = 0; i < game.size();i++){
-        if(issame(game[i],guess[i]))
+        if(isSame(game[i],guess[i]))
             res.push_back(2);
         else if(compare(game[i],guess[i]) == 1)
             res.push_back(1);
