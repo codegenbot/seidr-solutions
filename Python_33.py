ea2e78d949a9a07dddf15044c11c1b50a65cbe64
beep boop <githubbot@vadim.me>
2024-07-13 06:21:19 +0200

    Solution 81 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mmyso7cs Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4322d1ec-8201-4462-a244-e0f50dda1 d07.py", line 15, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4322d1ec-8201-4462-a244-e0f50dda1 d07.py", line 10, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,6 +1,9 @@
+```
 def sort_third(lst):
+    if not all(isinstance(i, (list, tuple)) for i in lst): 
+        return "Error: Input not list/tuple"
     result = [x[2] for x in lst if isinstance(x, (list, tuple)) and len(x) > 2]
     if not result:
-        return []
+        return "Error: No sublists/tuples found with length more than 2"
     else:
-        return sorted(result)
+        return tuple(sorted(result))
