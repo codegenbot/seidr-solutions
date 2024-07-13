1072f45caeee960a16c425aa18f29a605451c26b
beep boop <githubbot@vadim.me>
2024-07-13 09:40:46 +0200

    Solution 14 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/1i3rwteh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/261ca7a6-dc67-45b7-b837-4b5a8506d bce.py", line 2     x_num, x_denom = map(int, [i for i in x.split("/")][1:] + [0 if len([i for i  in x.split("/")) == 2 else [i for i in x.split("/")][-1]])                 ^ SyntaxError: closing parenthesis ')' does not match opening parenthesis '['

--- Python_144.py
+++ Python_144.py
@@ -1,5 +1,6 @@
+```
 def simplify(x, n):
-    x_num, x_denom = map(int, [i for i in x.split("/")[1:]] + ([0] if len([i for i in x.split("/"))) == 2 else [i for i in x.split("/"))][-1]))
-    n_num, n_denom = map(int, [i for i in n.split("/")[1:]] + ([0] if len([i for i in n.split("/"))) == 2 else [i for i in n.split("/"))][-1]))
+    x_num, x_denom = map(int, [int(i) for i in (x.split("/"))[1:]] + ([0] if len((x.split("/"))) == 2 else [(x.split("/"))[-1]])
+    n_num, n_denom = map(int, [int(i) for i in (n.split("/"))[1:]] + ([0] if len((n.split("/"))) == 2 else [(n.split("/"))[-1]])
 
     return x_num * n_denom == n_num * x_denom
