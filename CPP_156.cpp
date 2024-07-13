6bd1c89ccca7b165b8cbf3f4a44afb2dc6e41a1e
beep boop <githubbot@vadim.me>
2024-07-13 09:40:54 +0200

    Solution 86 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0wbduzxx Prompt: Complete the following code given the task description and function signature.

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,14 +1,20 @@
-vector<pair<int, string>> roman = {{1000,"M"},{900,"CM"},{500,"D"},
-                                    {400,"CD"},{100,"C"},{90,"XC"},
-                                    {50,"L"},{40,"XL"},{10,"X"},
-                                    {9,"IX"},{5,"V"},{4,"IV"},{1,"I"}};
-string int_to_mini_roman(int number){
-    string roman="";
-    for(auto& pair: roman){
-        while(number>=pair.first){
-            roman+=pair.second;
-            number-=pair.first;
-        }
-    }
-    return roman;
+string int_to_mini_roman(int num) {
+    string roman[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", 
+                      "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", 
+                      "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", 
+                      "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", 
+                      "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", 
+                      "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", 
+                      "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", 
+                      "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", 
+                      "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", 
+                      "XC", "XCI", "XCII", "XCIII", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", 
+                      "C"};
+    string roman_upper[] = {"", "M", "MM", "MMM", "MMMM"};
+
+    int hundreds = num / 100;
+    int tens = (num % 100) / 10;
+    int ones = num % 10;
+
+    return roman[hundreds] + roman[tens] + roman[ones];
 }
