22ff57c42a84c28e29374c39ca2ab24535c15f88
beep boop <githubbot@vadim.me>
2024-07-13 15:55:21 +0200

    Solution 34 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/9dzg4ady Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a0142829-c0aa-426a-86eb-4edaf0181 e94.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- twitter.py
+++ twitter.py
@@ -7,8 +7,8 @@ def validate_tweet(tweet):
     else:
         return f"Your tweet has {len(tweet)} characters"
 
-print(validate_tweet("")) # Should print: You didn't type anything
-print(validate_tweet("1")) # Should print: Your tweet has 1 characters
-print(validate_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it")) # Should print: Your tweet has 140 characters
-print(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``")) # Should print: Your tweet has 90 characters
-print(validate_tweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1")) # Should print: Too many characters
+print(validate_tweet("")) 
+print(validate_tweet("1")) 
+print(validate_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it")) 
+print(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``")) 
+print(validate_tweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1"))
