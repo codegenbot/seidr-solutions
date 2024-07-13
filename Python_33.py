c231b3d69b5fe9578d7d6555b65a9f22449a3cbf
beep boop <githubbot@vadim.me>
2024-07-13 03:43:00 +0200

    Solution 31 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mmyso7cs Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7779b1ba-ad60-45b0-83fc-6543a8b43 98d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,14 +1,3 @@
-```
 def sort_third(l):
-    nums_divisible_by_3 = sorted([i for i in l if i % 3 == 0], reverse=True)
-    other_nums = sorted([i for i in l if i % 3 != 0], reverse=True)
-
-    result = []
-    index = 0
-    for num in l:
-        if num % 3 == 0:
-            result.append(nums_divisible_by_3.pop(0))
-        else:
-            result.append(other_nums.pop(0))
-
-    return result
+    l.sort(key=lambda x: (x % 3, -x))
+    return l
