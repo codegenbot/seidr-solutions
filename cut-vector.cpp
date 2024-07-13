b4a8dfc2faff25161d3561dc05572f54301c6585
beep boop <githubbot@vadim.me>
2024-07-13 03:41:52 +0200

    Solution 0 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/cqskp2vj Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

---
+++ cut-vector.cpp
@@ -0,0 +1,61 @@
+#include <vector>
+using namespace std;
+
+vector<vector<int>> cutVector(vector<int> v) {
+    int n = v.size();
+    vector<vector<int>> res(2);
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || v[i] != v[0]) {
+            int leftSum = 0, rightSum = 0;
+            for (int j = 0; j <= i; j++) {
+                leftSum += v[j];
+            }
+            for (int j = i + 1; j < n; j++) {
+                rightSum += v[j];
+            }
+            if (leftSum == rightSum) {
+                res[0] = vector<int>(v.begin(), v.begin() + i + 1);
+                res[1] = vector<int>(v.begin() + i, v.end());
+                return res;
+            } else {
+                int minDiff = abs(leftSum - rightSum);
+                for (int j = i; j < n; j++) {
+                    leftSum -= v[i];
+                    rightSum += v[j];
+                    if (abs(leftSum - rightSum) < minDiff) {
+                        minDiff = abs(leftSum - rightSum);
+                        res[0] = vector<int>(v.begin(), v.begin() + i + 1);
+                        res[1] = vector<int>(v.begin() + i, v.end());
+                    }
+                }
+            }
+        }
+    }
+    return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    vector<vector<int>> res = cutVector(v);
+    cout << "[";
+    for (int i = 0; i < res[0].size(); i++) {
+        cout << res[0][i];
+        if (i < res[0].size() - 1) {
+            cout << " ";
+        }
+    }
+    cout << "] [";
+    for (int i = 0; i < res[1].size(); i++) {
+        cout << res[1][i];
+        if (i < res[1].size() - 1) {
+            cout << " ";
+        }
+    }
+    cout << "]" << endl;
+    return 0;
+}
