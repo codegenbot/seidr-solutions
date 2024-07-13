3a37a2cebf5d29f7d291fd37601976e25e5642c3
beep boop <githubbot@vadim.me>
2024-07-13 04:01:37 +0200

    Solution 15 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5iilprjk Prompt:       |                              ^~~~~~ b81fe36d-76c0-406f-8cbc-c43fd044cc36.cpp:31:37: error: ‘string’ was not declared  in this scope    31 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b81fe36d-76c0-406f-8cbc-c43fd044cc36.cpp:31:44: error: ‘b’ was not declared in t his scope    31 | bool issame(vector<string> a,vector<string>b){       |                                            ^ b81fe36d-76c0-406f-8cbc-c43fd044cc36.cpp:31:45: error: expression list treated a s compound expression in initializer [-fpermissive]    31 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from b81fe36d-76c0-406f-8cbc-c43fd044cc36.cpp:30: b81fe36d-76c0-406f-8cbc-c43fd044cc36.cpp: In function ‘int main()’: b81fe36d-76c0-406f-8cbc-c43fd044cc36.cpp:40:20: error: ‘reverse_delete’ was not declared in this scope    40 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ b81fe36d-76c0-406f-8cbc-c43fd044cc36.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,5 +1,7 @@
+```cpp
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 bool issame(vector<string> a, vector<string>) {
@@ -9,4 +11,31 @@ bool issame(vector<string> a, vector<string>) {
 int main() {
     assert(issame(reverse_delete("mamma", "mia") , {"", "True"}));
     return 0;
+}
+
+vector<string> reverse_delete(string s, string c) {
+    vector<char> v;
+    for (char x : s) {
+        bool flag = false;
+        for (char y : c) {
+            if (x == y) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag)
+            v.push_back(x);
+    }
+    string result = "";
+    for (char x : v) {
+        result += x;
+    }
+    
+    string temp = result;
+    reverse(temp.begin(), temp.end());
+    if (result == temp) {
+        return {result, "True"};
+    } else {
+        return {result, "False"};
+    }
 }
