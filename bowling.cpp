041f4990b9fec50b1189cb9a0259d6e4b8f4c7de
beep boop <githubbot@vadim.me>
2024-07-14 00:14:26 +0200

    Solution 5 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/n1rydnou Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,35 +1,16 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
 int bowlingScore(string s) {
     int score = 0;
-    int roll = 0;
-    for (char c : s) {
+    stringstream ss(s);
+    char c;
+    while (ss >> c) {
         if (c == 'X') {
             score += 30;
-            roll++;
         } else if (c == '/') {
-            score += 10 + (roll - 1);
-            roll = 0;
+            score += 10 + (score / 10 - 1);
         } else {
             int temp = c - '0';
-            if (roll > 0) {
-                score += temp;
-                roll--;
-            } else {
-                score += temp * 2;
-            }
+            score += temp;
         }
     }
     return score;
-}
-
-int main() {
-    string s;
-    cout << "Enter the string representing the individual bowls: ";
-    cin >> s;
-    cout << "The score for this round is: " << bowlingScore(s) << endl;
-    return 0;
 }
