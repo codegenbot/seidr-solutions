933e104a520acbafdaba1ceedd6062523ea3ded0
beep boop <githubbot@vadim.me>
2024-07-13 08:37:20 +0200

    Solution 47 of Python/59, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ydxdl1kc Prompt: Program did not receive expected input

--- Python_59.py
+++ Python_59.py
@@ -3,14 +3,15 @@ import math
 
 def largest_prime_factor(n):
     i = 2
-    while i <= math.sqrt(n):
+    max_prime = 0
+    while n > 1:
         if n % i == 0:
             j = i
             while n % j == 0:
                 n //= j
-            return j
+            max_prime = j
         i += 1
-    return n
+    return max_prime
 
 
 def main():
