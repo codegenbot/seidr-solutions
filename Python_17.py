91caebe891f5c90716a975f5585bc7144e2d0dc2
beep boop <githubbot@vadim.me>
2024-07-13 04:56:00 +0200

    Solution 72 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/9cg3dsjd Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bc14da72-9e8d-4611-af50-f093e11a2 565.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,4 +1,3 @@
-```
 def parse_music(s):
     if not s:
         return []
@@ -6,10 +5,10 @@ def parse_music(s):
     beats = s.replace("|", " ").split()
     beat_counts = {}
     for beat in beats:
-        if beat:  
-            beat_count, _ = map(int, beat.split())
-            if str(beat_count) in beat_counts:
-                beat_counts[str(beat_count)] += 1
+        if beat:
+            count, _ = map(int, beat.split())
+            if str(count) in beat_counts:
+                beat_counts[str(count)] += 1
             else:
-                beat_counts[str(beat_count)] = 1
+                beat_counts[str(count)] = 1
     return [beat_counts.get(str(i), 0) for i in range(1, max(map(int, beats)) + 1)]
