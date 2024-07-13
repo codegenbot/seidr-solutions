8066632638471d441bbf4397887c0312a95c2658
beep boop <githubbot@vadim.me>
2024-07-13 08:39:18 +0200

    Solution 24 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8fxvx5aq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,32 +1,41 @@
-#include <boost/any.hpp>
-
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return b;
+        return (int)a > (float)b ? a : b;
     }
     else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string str = any_cast<string>(b);
-        istringstream iss(str);
-        float num;
-        iss >> num;
-        return a > num ? a : b;
+        string str = (string)b;
+        size_t pos = str.find(',');
+        if (pos != string::npos) {
+            str = str.substr(0, pos);
+        }
+        return (float)a > stof(str) ? a : b;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if (any_cast<string>(a) > any_cast<string>(b))
-            return a;
-        else
-            return b;
+        size_t pos1 = (string)a.find(',');
+        size_t pos2 = (string)b.find(',');
+        if (pos1 != string::npos) {
+            (string)a = (string)a.substr(0, pos1);
+        }
+        if (pos2 != string::npos) {
+            (string)b = (string)b.substr(0, pos2);
+        }
+        return stof((string)a) > stof((string)b) ? a : b;
     }
-    else if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int num1 = any_cast<int>(a);
-        int num2 = any_cast<int>(b);
-        return num1 > num2 ? a : b;
+    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        size_t pos = (string)b.find(',');
+        if (pos != string::npos) {
+            (string)b = (string)b.substr(0, pos);
+        }
+        return (int)a > stof((string)b) ? a : boost::any("None");
     }
-    else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        float num1 = any_cast<float>(a);
-        float num2 = any_cast<float>(b);
-        return num1 > num2 ? a : b;
+    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        size_t pos = (string)a.find(',');
+        if (pos != string::npos) {
+            (string)a = (string)a.substr(0, pos);
+        }
+        return stof((string)a) > (int)b ? a : boost::any("None");
     }
-    else
+    else {
         return boost::any("None");
+    }
 }
