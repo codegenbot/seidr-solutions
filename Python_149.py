c0fd4ef05bdcc5d81ca4c57b5268144eddcc3f18
beep boop <githubbot@vadim.me>
2024-07-13 10:52:07 +0200

    Solution 95 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/5nnxhoku Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/401ac91a-7793-4eaa-9d4f-55b9cc12b 13e.py", line 10, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/401ac91a-7793-4eaa-9d4f-55b9cc12b 13e.py", line 7, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,3 +1,5 @@
 def sorted_list_sum(lst):
-    numbers = [int(''.join(filter(str.isdigit, s))) for s in lst if any(c.isdigit() for c in s)]
-    return [''.join(sorted(s)) for s in set(lst)]
+    numbers = [''.join(filter(str.isdigit, s)) for s in lst if any(c.isdigit() for c in s)]
+    numbers = [int(n) for n in numbers]
+    sorted_numbers = [str(i).zfill(len(max(numbers, key=len))) for i in sorted(numbers)]
+    return ''.join(sorted_numbers)
