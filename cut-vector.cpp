a5bb8f3eaf1550a7a29e4d5677e1fc8b3625dfeb
beep boop <githubbot@vadim.me>
2024-07-13 23:54:32 +0200

    Solution 7 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/cd853toj Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -3,28 +3,35 @@ using namespace std;
 
 pair<vector<int>, vector<int>> cutVector(vector<int> v) {
     int min_diff = INT_MAX;
-    int cut_index = -1;
+    int cut_index = 0;
     
-    for (int i = 0; i < v.size() - 1; i++) {
+    for (int i = 1; i <= v.size(); i++) {
         int left_sum = 0, right_sum = 0;
         
-        for (int j = 0; j <= i; j++) {
-            left_sum += v[j];
-        }
-        
-        for (int j = i + 1; j < v.size(); j++) {
-            right_sum += v[j];
+        if (i < v.size()) {
+            for (int j = 0; j < i; j++) {
+                left_sum += v[j];
+            }
+            for (int j = i; j < v.size(); j++) {
+                right_sum += v[j];
+            }
+        } else {
+            left_sum = 0;
+            right_sum = 0;
+            for (int j = 0; j < v.size(); j++) {
+                left_sum += v[j];
+                right_sum += v[j];
+            }
         }
         
         int diff = abs(left_sum - right_sum);
-        
         if (diff < min_diff) {
             min_diff = diff;
             cut_index = i;
         }
     }
     
-    vector<int> left(v.begin(), v.begin() + cut_index + 1);
+    vector<int> left(v.begin(), v.begin() + cut_index);
     vector<int> right(v.begin() + cut_index, v.end());
     
     return {left, right};
@@ -37,20 +44,16 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> v[i];
     }
-    
     pair<vector<int>, vector<int>> result = cutVector(v);
-    
     cout << "Left: ";
-    for (int x : result.first) {
-        cout << x << " ";
+    for (int num : result.first) {
+        cout << num << " ";
     }
     cout << endl;
-    
     cout << "Right: ";
-    for (int x : result.second) {
-        cout << x << " ";
+    for (int num : result.second) {
+        cout << num << " ";
     }
     cout << endl;
-    
     return 0;
 }
