3a81d1c26e00ce4b854018db7da158d6e8b54bf5
beep boop <githubbot@vadim.me>
2024-07-13 11:39:45 +0200

    Solution 79 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kjfoknrj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cb624eca-3052-4620-8577-416745a55 81a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_162.py
+++ Python_162.py
@@ -1,2 +1,19 @@
-```
-print(``
+```Python
+import hashlib
+
+
+def string_to_md5():
+    text = input("Enter a string: ")
+    if not text.isalnum():
+        print("Invalid input. Please enter only alphanumeric characters.")
+        return None
+    else:
+        return hashlib.md5(text.encode()).hexdigest()
+
+
+if __name__ == "__main__":
+    while True:
+        input("Press Enter when ready: ")
+        result = string_to_md5()
+        if result is not None:
+            print(result)
