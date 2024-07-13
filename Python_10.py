a06484a2057d0dd6aa75a0bea2e8bd1f6dcf2759
beep boop <githubbot@vadim.me>
2024-07-13 11:13:04 +0200

    Solution 10 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lydubfu0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf9e7597-b269-4535-b60e-7f682b764 2ad.py", line 19, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf9e7597-b269-4535-b60e-7f682b764 2ad.py", line 16, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,13 +1,19 @@
-Here is the completed code:
-
 def is_palindrome(string: str) -> bool:
     return string == string[::-1]
 
+
 def make_palindrome(string: str) -> str:
-    if string.ispalindrome():
-        return string + string[::-1]
+    if string.islower():
+        for i in range(len(string), 0, -1):
+            if string[:i] == string[:i][::-1]:
+                return string + string[:i][::-1]
     else:
-        for i in range(len(string), -1, -1):
-            if string[:i].ispalindrome():
-                return string[:i] + string[i:][::-1]
-        return string + string[::-1]
+        temp = string.lower()
+        for i in range(len(temp), 0, -1):
+            if temp[:i] == temp[:i][::-1]:
+                return (
+                    string[0].upper()
+                    + temp[:i][::-1]
+                    + string[-1].lower()
+                    + temp[i - 1 :: 1]
+                )
