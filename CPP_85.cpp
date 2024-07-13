f2904de1528b6586219a5723a04dda5d9535fdb5
beep boop <githubbot@vadim.me>
2024-07-13 02:49:02 +0200

    Solution 3 of CPP/85, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1lxgtll5 Prompt: Complete the following code given the task description and function signature.

--- CPP_85.cpp
+++ CPP_85.cpp
@@ -1,9 +1,8 @@
-int add(vector<int> lst){
-    int result = 0;
-    for(int i = 1; i < lst.size(); i += 2){
-        if(lst[i] % 2 == 0) {
-            result += lst[i];
-        }
+int add(vector<int> lst) {
+    int sum = 0;
+    for (int i = 1; i < lst.size(); i += 2) {
+        if (lst[i] % 2 == 0)
+            sum += lst[i];
     }
-    return result;
+    return sum;
 }
