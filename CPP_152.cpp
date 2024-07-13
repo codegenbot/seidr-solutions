02d7f9d90c4df55d525a30e35e4666d306062fc3
beep boop <githubbot@vadim.me>
2024-07-13 02:55:44 +0200

    Solution 87 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uswfokmt Prompt:       |                |       |                __gnu_cxx::__alloc_traits<std::allocator<int>, int>::valu e_type {aka int} 025dcdcd-6576-410b-8200-6d0ee35c5c74.cpp: At global scope: 025dcdcd-6576-410b-8200-6d0ee35c5c74.cpp:53:5: error: redefinition of ‘int main( )’    53 | int main(){       |     ^~~~ 025dcdcd-6576-410b-8200-6d0ee35c5c74.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from 025dcdcd-6576-410b-8200-6d0ee35c5c74.cpp:44: 025dcdcd-6576-410b-8200-6d0ee35c5c74.cpp: In function ‘int main()’: 025dcdcd-6576-410b-8200-6d0ee35c5c74.cpp:54:27: error: could not convert ‘compar e(std::vector<int>(std::initializer_list<int>{((const int*)(& const int [4]{1, 2 , 3, 5})), 4}, std::allocator<int>()), std::vector<int>(std::initializer_list<in t>{((const int*)(& const int [4]{-1, 2, 3, 4})), 4}, std::allocator<int>()))’ fr om ‘bool’ to ‘std::vector<int>’    54 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~       |                           |       |                           bool

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -6,10 +6,20 @@ using namespace std;
 bool compare(vector<int> a, vector<int> b) {
     if(a.size() != b.size())
         return false;
-    for(int i = 0; i < a.size(); i++)
+    for(int i = 0; i < a.size();i++)
         if(a[i] != b[i])
             return false;
     return true;
+}
+
+vector<int> getResult(vector<int> game, vector<int> guess) {
+    vector<int> res;
+    for(int i = 0; i < game.size();i++){
+        if(game[i] == guess[i])res.push_back(10);
+        else if(find(guess.begin(),guess.end(),game[i]) != guess.end())res.push_back(5);
+        else res.push_back(0);
+    }
+    return res;
 }
 
 int main() {
@@ -20,10 +30,9 @@ int main() {
         cin >> game[i] >> guess[i];
     }
     
-    if(!compare({game[0],guess[0]}, {2,0,0,1})) {
+    if(!compare(game[0],guess[0])) {
         cout << "Error: Invalid input. Please check your input again." << endl;
     } else {
-        // implement your getResult function here
         vector<int> res = getResult(game,guess);
         for(auto x:res)cout << x << " ";
         cout << endl; 
