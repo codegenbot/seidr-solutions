920c8b68ef30bd200c48ef86332122dc1eab1bde
beep boop <githubbot@vadim.me>
2024-07-13 02:22:48 +0200

    Solution 31 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/xbibyc5v Prompt: a6f7f70c-818c-49b5-b0e4-c1096e151d3e.cpp: In function ‘std::string spinWords(std ::string)’: a6f7f70c-818c-49b5-b0e4-c1096e151d3e.cpp:24:31: error: ‘wordStart’ was not decla red in this scope    24 |             word = str.substr(wordStart, i-wordStart);       |                               ^~~~~~~~~ a6f7f70c-818c-49b5-b0e4-c1096e151d3e.cpp:34:13: error: ‘wordStart’ was not decla red in this scope    34 |             wordStart = i;       |             ^~~~~~~~~ a6f7f70c-818c-49b5-b0e4-c1096e151d3e.cpp: In function ‘int main()’: a6f7f70c-818c-49b5-b0e4-c1096e151d3e.cpp:50:14: error: expected ‘}’ at end of in put    50 |     return 0;       |              ^ a6f7f70c-818c-49b5-b0e4-c1096e151d3e.cpp:41:12: note: to match this ‘{’    41 | int main() {       |            ^

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <iostream>
 #include <string>
@@ -6,15 +5,15 @@
 
 using namespace std;
 
-int wordStart = 0;
 string spinWords(string str) {
     string result = "";
+    int wordStart = 0;
     
     for(int i=0; i<str.length(); i++) {
         string word;
         
         if(i == str.length() - 1) {
-            word = str.substr(i);
+            word = str.substr(wordStart, i-wordStart+1);
             
             if(word.length() >= 5) {
                 reverse(word.begin(), word.end());
@@ -23,16 +22,10 @@ string spinWords(string str) {
             result += word + " ";
         } else if (str[i] == ' ') {
             word = str.substr(wordStart, i-wordStart);
-
-            if(word.length() >= 5) {
-                reverse(word.begin(), word.end());
-            }
-
             result += word + " ";
             wordStart = i+1;
         } else {
             word += str[i];
-            wordStart = i;
         }
     }
     
@@ -49,4 +42,3 @@ int main() {
     cout << "Output: " << output << endl;
 
     return 0;
-}
