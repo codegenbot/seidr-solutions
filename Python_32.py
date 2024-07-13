88a2906feab7c7766290a9433d6a45a6c2ffe745
beep boop <githubbot@vadim.me>
2024-07-13 11:58:03 +0200

    Solution 5 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/cr2r8vg1 Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -3,5 +3,5 @@ Here is the completed code:
 def find_zero(xs: list):
     if len(xs) % 2 != 0:
         raise ValueError("xs must have even number of coefficients")
-    root = (-xs[1] / xs[0]) ** (1/float(len(xs)-1))
-    return round(root, 2)
+    x = -xs[1] / xs[0]
+    return round(x, 2)
