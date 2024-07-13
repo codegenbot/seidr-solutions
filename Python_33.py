566c388418d0484ffab2bd70424a429a97050951
beep boop <githubbot@vadim.me>
2024-07-13 06:29:11 +0200

    Solution 84 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mmyso7cs Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d5f7800b-8c6c-43c9-85d4-9b26c33ce 810.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,8 +1,9 @@
+```python
 def sort_third(lst):
     if not all(isinstance(i, (list, tuple)) for i in lst): 
-        return "Error: Input not list/tuple"
+        return tuple()
     result = [x[2] for x in lst if isinstance(x, (list, tuple)) and len(x) > 2]
     if not result:
-        return "Error: No sublists/tuples found with length more than 2"
+        return tuple()
     else:
         return tuple(sorted(result))
