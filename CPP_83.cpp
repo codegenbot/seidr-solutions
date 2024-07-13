747e466c6707768d46428a37f9512d8b0fdfefd7
beep boop <githubbot@vadim.me>
2024-07-13 02:48:53 +0200

    Solution 2 of CPP/83, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5lo6ts43 Prompt: Complete the following code given the task description and function signature.

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -1,9 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
 int starts_one_ends(int n) {
     int count = 0;
-    for (long long i = 1; i <= (long long) pow(10, n - 1); i++) {
-        if (i % 10 == 1 || to_string(i).back() == '1') {
+    for (long long i = 1; i <= (long long)pow(10, n-1); i++) {
+        string s = to_string(i);
+        if ((s[0] == '1' || s[s.length()-1] == '1') && s.length() == n)
             count++;
-        }
     }
     return count;
 }
