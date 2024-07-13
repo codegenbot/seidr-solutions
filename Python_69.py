f9f1db0adfd82eafcba819467eefd21382c80379
beep boop <githubbot@vadim.me>
2024-07-13 09:51:00 +0200

    Solution 32 of Python/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/iydn96n5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3f87bb49-1062-4666-92f4-de97ac2b8 035.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_69.py
+++ Python_69.py
@@ -1,8 +1,8 @@
 ```
 def search(lst):
-    max_num = float('-inf')
+    max_num = -1
     for num in lst:
-        if num > max_num:
+        if (max_num == -1 or num > max_num):
             max_num = num
     return max_num
 
