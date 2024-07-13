ad7dcad6b8162061fbea7e85cbc54d0bed054359
beep boop <githubbot@vadim.me>
2024-07-13 09:12:56 +0200

    Solution 79 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8fxvx5aq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,74 +1,13 @@
-```cpp
-#include <string>
-#include <algorithm>
-#include <boost/any.hpp>
-
-using namespace std;
-
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        int ai = boost::any_cast<int>(a);
-        float bf = boost::any_cast<float>(b);
-        if (bf > ai)
-            return b;
-        else
-            return a;
-    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int ai = boost::any_cast<int>(a);
-        string bs = boost::any_cast<string>(b);
-        float af = stof(bs);
-        if (af > ai)
-            return b;
-        else
-            return a;
+        return (int)std::get<int>(a) > std::get<float>(b) ? a : b;
     } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        float af = boost::any_cast<float>(a);
-        int bi = boost::any_cast<int>(b);
-        if (af > bi)
-            return a;
-        else
-            return b;
-    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        string as = boost::any_cast<string>(a);
-        float bf = boost::any_cast<float>(b);
-        float af = stof(as);
-        if (af > bf)
-            return a;
-        else
-            return b;
-    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        string as = boost::any_cast<string>(a);
-        int bi = boost::any_cast<int>(b);
-        float af = stof(as);
-        if (af > bi)
-            return a;
-        else
-            return b;
+        return (float)std::get<int>(b) > (float)std::get<int>(a) ? b : "None";
     } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string as = boost::any_cast<string>(a);
-        string bs = boost::any_cast<string>(b);
-        float af = stof(as), bf = stof(bs);
-        if (bf > af)
-            return b;
-        else
-            return a;
-    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        float af = boost::any_cast<float>(a);
-        string bs = boost::any_cast<string>(b);
-        float bf = stof(bs);
-        if (bf > af)
-            return b;
-        else
-            return a;
+        return std::string(std::get<string>(a)).compare(std::get<string>(b)) > 0 ? a : (std::get<string>(a).compare(std::get<string>(b)) < 0 ? b : "None");
     } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int ai = boost::any_cast<int>(a);
-        string bs = boost::any_cast<string>(b);
-        float bf = stof(bs);
-        if (bf > ai)
-            return b;
-        else
-            return a;
-    } else {
-        return boost::any("None");
+        return std::stoi(std::get<string>(b)) > (int)std::get<int>(a) ? b : "None";
+    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        return (int)std::get<int>(a) > std::stoi(std::get<string>(b)) ? a : "None";
     }
 }
