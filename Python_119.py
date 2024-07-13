5e56496fa395eb57a6e6bf2d2119dfb5817380ff
beep boop <githubbot@vadim.me>
2024-07-13 10:33:34 +0200

    Solution 92 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kxnv9mbc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3fc1fb7e-76cd-42ed-be80-8cff3b5b1 cc3.py", line 26, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3fc1fb7e-76cd-42ed-be80-8cff3b5b1 cc3.py", line 23, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,20 +1,19 @@
 def match_parens(lst):
     for s in lst:
-        if set(s).issubset('()' + ' '):
-            count = 0
-            has_opening = False
-            for char in s:
-                if char == "(":
-                    count += 1
-                    has_opening = True
-                elif char == ")":
-                    if not has_opening:
-                        return "No"
-                    count -= 1
-                    if count == 0:
-                        has_opening = False
-            if count > 0:
-                return "No"
-        else:
+        if any(char not in '() ' for char in s):
+            return "No"
+        count = 0
+        has_opening = False
+        for char in s:
+            if char == "(":
+                count += 1
+                has_opening = True
+            elif char == ")":
+                if not has_opening:
+                    return "No"
+                count -= 1
+                if count == 0:
+                    has_opening = False
+        if count > 0 or (has_opening and s != "("):
             return "No"
     return "Yes"
