9e144b647ee11c0f04fc78dc6f4ff1c56b02e12c
beep boop <githubbot@vadim.me>
2024-07-13 08:37:25 +0200

    Solution 19 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kxnv9mbc Prompt: No No Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7fc8a6fe-721d-41b9-bec4-fd08bb291 d56.py", line 23, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7fc8a6fe-721d-41b9-bec4-fd08bb291 d56.py", line 20, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,16 +1,21 @@
+```
 def match_parens(lst):
     for s in lst:
-        stack = [False]
+        stack = []
         match = "Yes"
         for c in s:
             if c == "(":
-                stack.append(True)
+                if not stack:  
+                    stack.append(c)
+                else:
+                    match = "No"
+                    break
             elif c == ")":
-                if len(stack) > 1:
+                if len(stack) > 0:  
                     stack.pop()
                 else:
                     match = "No"
                     break
-        if len(stack) > 1:
+        if len(stack) > 0:
             match = "No"
         print(match)
