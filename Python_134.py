6f9bb0643b34a2042c4313a5c0e89b1df9ba626a
beep boop <githubbot@vadim.me>
2024-07-13 10:42:51 +0200

    Solution 69 of Python/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fw253inm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/65823c9f-ceab-41f6-bf05-b9198e717 d73.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_134.py
+++ Python_134.py
@@ -9,8 +9,11 @@ if __name__ == "__main__":
         try:
             if not isinstance(user_input, str) or len(user_input.strip()) == 0:
                 print("Please enter a non-empty string.")
+            elif not user_input.isalnum():
+                print("Invalid input. Please enter alphanumeric characters only.")
             else:
                 break
         except Exception as e:
             print(f"Unexpected error occurred: {e}")
+
     print(check_palindrome(user_input))
