eaaf9c2290508c404303ca04c8424810cb3c0d00
beep boop <githubbot@vadim.me>
2024-07-13 16:44:39 +0200

    Solution 84 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/leb2p50q Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f202b5c-76a0-47f0-a99c-8b27dc003 51a.py", line 12, in <module>     print(shopping_list())           ^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f202b5c-76a0-47f0-a99c-8b27dc003 51a.py", line 6, in shopping_list     discount = float(input())                ^^^^^^^^^^^^^^ ValueError: could not convert string to float: '38.02 10.54'

--- shopping-list.py
+++ shopping-list.py
@@ -1,13 +1,11 @@
 def shopping_list():
     total_price = 0
-    goods_prices = []
     while True:
         try:
             prices_discounts = input().split()
-            for price_discount in zip(*[iter(prices_discounts)]*2):
-                price, discount = map(float, price_discount)
-                total_price += price - (price * (discount / 100))
-        except EOFError:
+            price, discount = map(float, prices_discounts)
+            total_price += price - (price * (discount / 100))
+        except ValueError:
             break
     return round(total_price, 2)
 
