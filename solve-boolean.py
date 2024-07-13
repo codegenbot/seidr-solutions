20961e6827b9d027e01da5b46137c756e16a0527
beep boop <githubbot@vadim.me>
2024-07-13 12:03:34 +0200

    Solution 11 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/4rv4b06w Prompt: it must return False for input f|t|t|f&f|f|f|f, but it returns .

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,31 +1,23 @@
 def solve_boolean(expression):
     ops = {'&': lambda x, y: x and y, '|': lambda x, y: x or y}
     stack = []
-    current_number = ''
-    in_parens = False
+    current_operator = ''
     for char in expression:
-        if char == '(':
-            in_parens = True
-        elif char == ')':
-            in_parens = False
-        elif char not in 'TF' and char not in ops:
-            while stack and isinstance(stack[-1], bool):
-                stack.pop()
+        if char in 'TF':
+            if char == '(':
+                continue
             stack.append(True if char == 'T' else False)
-        elif char in 'TF':
-            while stack and isinstance(stack[-1], bool) and in_parens:
-                stack.pop()
-            if current_number:
-                stack.append(True if char == 'T' else False)
-                current_number = ''
         elif char in ops:
-            while stack and isinstance(stack[-1], bool) and in_parens:
-                stack.pop()
-            if current_number:
-                stack.append(True if char == 'T' else False)
-                current_number = ''
-            b = ops[char](stack.pop(), stack.pop())
-            stack.append(b)
+            if current_operator:
+                stack.append(ops[current_operator](stack.pop(), stack.pop()))
+            current_operator = char
+        elif char == '(':
+            while expression[expression.index(char)+1:] and expression[expression.index(char)+1:] != ')':
+                current_operator += expression[expression.index(char)+1]
+            continue
         else:
-            current_number += char
+            if char == ')':
+                while current_operator:
+                    stack.append(ops[current_operator](stack.pop(), stack.pop()))
+                    current_operator = ''
     return stack[0]
