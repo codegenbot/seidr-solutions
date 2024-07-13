2070f38f3f8bf2748750d681c2552d0015ad0c8c
beep boop <githubbot@vadim.me>
2024-07-13 10:50:32 +0200

    Solution 77 of Python/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/k4cd2f1b Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2018dff8-c4d9-4aea-bf5c-1c60ea5f0 14d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_101.py
+++
@@ -1,8 +0,0 @@
-words = input("Enter a string: ").strip()
-print(check(words))
-
-def check(sentence):
-    if len(sentence.split()) > 1:
-        return "I know or quickly said the same."
-    else:
-        return "All good!"
