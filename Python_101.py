93d69034ffe4f86921cbb81832cdd4eae09d6fef
beep boop <githubbot@vadim.me>
2024-07-13 09:59:07 +0200

    Solution 50 of Python/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/k4cd2f1b Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/696f0081-6e5b-4edc-b588-76fe195e2 1da.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_101.py
+++ Python_101.py
@@ -1,7 +1,6 @@
 import re
 
-
 def words_string(s):
     if s is None or not isinstance(s, str) or len(s.strip()) == 0:
         return []
-    return [word.strip().capitalize() for word in re.split(r"\W+", s)]
+    return [word.strip().capitalize() for word in re.split(r'\W+', s)]
