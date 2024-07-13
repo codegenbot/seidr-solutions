33f84c89fc3583530a9850489ff98dbafc6f82e5
beep boop <githubbot@vadim.me>
2024-07-13 10:53:42 +0200

    Solution 97 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/5nnxhoku Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/93b9cdee-c322-4b60-ab8b-9423b41d9 24d.py", line 12, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/93b9cdee-c322-4b60-ab8b-9423b41d9 24d.py", line 9, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,4 +1,11 @@
-```Python
 def sorted_list_sum(lst):
-    numbers = [s for s in lst if any(c.isdigit() for c in s)]
-    return sorted(numbers)
+    numbers = [
+        "".join(filter(str.isdigit, s)) for s in lst if any(c.isdigit() for c in s)
+    ]
+    numbers = [int(n) for n in numbers]
+    return "".join(
+        map(
+            lambda x: str(x).zfill(len(max(map(str, numbers), key=len)))
+            for _ in range(len(numbers))
+        )
+    )
