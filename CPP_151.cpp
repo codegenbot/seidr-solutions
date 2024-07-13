c97796ad84c74b25f3f1ca5b424ed1635ad89e08
beep boop <githubbot@vadim.me>
2024-07-13 04:24:31 +0200

    Solution 19 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fhfry2e5 Prompt: 9ec206f1-04a0-40e5-90d8-e9dc7a1cb291.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 9ec206f1-04a0-40e5-90d8-e9dc7a1cb291.cpp:15: 9ec206f1-04a0-40e5-90d8-e9dc7a1cb291.cpp: In function ‘int main()’: 9ec206f1-04a0-40e5-90d8-e9dc7a1cb291.cpp:17:35: error: ‘lst’ was not declared in  this scope    17 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 9ec206f1-04a0-40e5-90d8-e9dc7a1cb291.cpp:17:13: error: ‘double_the_difference’ w as not declared in this scope    17 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ 9ec206f1-04a0-40e5-90d8-e9dc7a1cb291.cpp:17:43: error: ‘odd_sum’ was not declare d in this scope    17 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -4,8 +4,8 @@
 long long double_the_difference(std::vector<float> lst) {
     long long sum = 0;
     for (float num : lst) {
-        if (num > 0 && floor(num) == num) { // check if number is positive and integer
-            if (fmod(num, 2.0) != 0.0) { // check if number is odd
+        if (num > 0 && floor(num) == num) { 
+            if (fmod(num, 2.0) != 0.0) { 
                 sum += pow(num, 2);
             }
         }
@@ -14,19 +14,15 @@ long long double_the_difference(std::vector<float> lst) {
 }
 
 int main() {
-    std::vector<float> lst;
-    float num;
-    long long odd_sum = 0;
+    std::vector<float> lst = {1.5f, 3.0f, 4.8f};
+    float odd_sum = 0;
 
-    for(int i=1; i<=5; i++) {
-        std::cout << "Enter number " << i << ": ";
-        std::cin >> num;
-        if (num > 0 && floor(num) == num) { // check if number is positive and integer
-            if (fmod(num, 2.0) != 0.0) { // check if number is odd
+    for (float num : lst) {
+        if (num > 0 && floor(num) == num) { 
+            if (fmod(num, 2.0) != 0.0) { 
                 odd_sum += pow(num, 2);
             }
         }
-        lst.push_back(num);
     }
 
     assert(double_the_difference(lst) == odd_sum);
