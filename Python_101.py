b151676d0f9119c6e0fee14037dbd6b8103b426e
beep boop <githubbot@vadim.me>
2024-07-13 08:20:15 +0200

    Solution 23 of Python/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/k4cd2f1b Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9d8f7959-a1cd-48f0-b4cb-2495d4757 582.py", line 9, in <module>     check(words_string)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9d8f7959-a1cd-48f0-b4cb-2495d4757 582.py", line 6, in check     assert words_string("ahmed     , gamal") == ["ahmed", "gamal"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_101.py
+++ Python_101.py
@@ -1,3 +1,3 @@
 ```
 def words_string(s):
-    return [",".join(word.strip().title().split()) for word in s.split(",") if word]
+    return [",".join(word.strip().title() for word in s.replace(",", " ").split()) for _ in s.split(",") if s]
