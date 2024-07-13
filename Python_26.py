e03dbeb948182e29c557e30a056f7c07a329b567
beep boop <githubbot@vadim.me>
2024-07-13 03:33:42 +0200

    Solution 39 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fykll3yz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b1f66a6-c3f6-43e7-a663-377d64e26 4fa.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_26.py
+++ Python_26.py
@@ -1,4 +1,2 @@
 def remove_duplicates(numbers):
-    seen = set()
-    result = [num for num in numbers if num not in seen and not seen.add(num)]
-    return result[::-1]
+    return sorted(list(set(numbers)))
