0cbf93cdc98d809e91dfeb1f0b530c26c3ff6509
beep boop <githubbot@vadim.me>
2024-07-13 10:04:07 +0200

    Solution 30 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/5nnxhoku Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/55eab73c-eb88-450d-a10c-6a9ad358e bdb.py", line 10, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/55eab73c-eb88-450d-a10c-6a9ad358e bdb.py", line 7, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/55eab73c-eb88-450d-a10c-6a9ad358e bdb.py", line 2, in sorted_list_sum     lst = list(map(int, lst.split()))                         ^^^^^^^^^ AttributeError: 'list' object has no attribute 'split'

--- Python_149.py
+++ Python_149.py
@@ -1,4 +1,3 @@
-```
 def sorted_list_sum(lst):
-    lst = list(map(int, ''.join(lst).split()))
-    return sum(sorted(lst))
+    lst = [' '.join(map(str, lst))].split()
+    return sum(sorted([int(i) for i in lst if not i.startswith('a')]))
