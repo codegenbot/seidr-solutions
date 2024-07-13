30715315c197e75901e62ce76575da50ce09201a
beep boop <githubbot@vadim.me>
2024-07-13 13:39:03 +0200

    Solution 4 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/mfr89066 Prompt: Program failed

--- twitter.cpp
+++ twitter.cpp
@@ -1,22 +1,5 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
 string validateTweet(string tweet) {
-    if (tweet.empty()) {
-        return "You didn't type anything";
-    }
-    if (tweet.length() > 140) {
-        return "Too many characters";
-    }
-    return "Your tweet has " + to_string(tweet.length()) + " characters";
-}
-
-int main() {
-    string tweet;
-    cout << "Enter a tweet: ";
-    cin >> tweet;
-    cout << validateTweet(tweet) << endl;
-    return 0;
+    if(tweet.empty()) return "You didn't type anything";
+    else if(tweet.length() > 140) return "Too many characters";
+    else return "Your tweet has " + to_string(tweet.length()) + " characters";
 }
