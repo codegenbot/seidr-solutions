4c2920cc7e775e88a778a56e4d5980409f8651b7
beep boop <githubbot@vadim.me>
2024-07-13 02:49:27 +0200

    Solution 30 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/80idpyzn Prompt: 15837b77-8278-4e4b-b0e1-a69db80a21e7.cpp:61:5: error: redefinition of ‘int main( )’    61 | int main(){       |     ^~~~ 15837b77-8278-4e4b-b0e1-a69db80a21e7.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~ In file included from 15837b77-8278-4e4b-b0e1-a69db80a21e7.cpp:52: 15837b77-8278-4e4b-b0e1-a69db80a21e7.cpp: In function ‘int main()’: 15837b77-8278-4e4b-b0e1-a69db80a21e7.cpp:62:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    62 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,14 +1,50 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
+using namespace std;
+
+vector<int> strange_sort_vector(vector<int> lst) {
+    vector<int> result;
+    
+    while (!lst.empty()) {
+        int minVal = *min_element(lst.begin(), lst.end());
+        int maxVal = *max_element(lst.begin(), lst.end());
+        
+        auto itMin = find(lst.begin(), lst.end(), minVal);
+        auto itMax = find(itMin, lst.end(), maxVal);
+        
+        result.push_back(*itMin);
+        lst.erase(itMin);
+        result.push_back(*itMax);
+        lst.erase(itMax);
     }
-    return true;
+    
+    return result;
 }
 
-int main() {
-    assert(issame(strange_sort_list({111111}), {111111}));  
+int mainFunc() {
+    vector<int> test1 = {1, 2, 3, 4};
+    vector<int> test2 = {5, 5, 5, 5};
+    vector<int> test3 = {};
+    
+    cout << "Test 1: ";
+    for (auto i : strange_sort_vector(test1)) {
+        cout << i << " ";
+    }
+    cout << endl;
+    
+    cout << "Test 2: ";
+    for (auto i : strange_sort_vector(test2)) {
+        cout << i << " ";
+    }
+    cout << endl;
+    
+    cout << "Test 3: ";
+    for (auto i : strange_sort_vector(test3)) {
+        cout << i << " ";
+    }
+    cout << endl;
+    
+    return 0;
 }
