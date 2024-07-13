3a75a8727010c90633f631c063c9b53a16c8731e
beep boop <githubbot@vadim.me>
2024-07-13 09:15:59 +0200

    Solution 98 of coin-sums, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/4yarrot1 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- coin-sums.cpp
+++ coin-sums.cpp
@@ -28,20 +28,9 @@ int main() {
     pennies = cents;
 
     std::cout << "Quarters: " << quarters << std::endl;
-    if(quarters > 1) std::cout << "s" << std::endl;
-    else std::cout << std::endl;
-
     std::cout << "Dimes: " << dimes << std::endl;
-    if(dimes > 1) std::cout << "s" << std::endl;
-    else std::cout << std::endl;
-
     std::cout << "Nickels: " << nickels << std::endl;
-    if(nickels > 1) std::cout << "s" << std::endl;
-    else std::cout << std::endl;
-
     std::cout << "Pennies: " << pennies << std::endl;
-    if(pennies > 1) std::cout << "s" << std::endl;
-    else std::cout << std::endl;
-    
+
     return 0;
 }
