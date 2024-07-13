dd30a1b7f37885241a795e9420ab18d885036af8
beep boop <githubbot@vadim.me>
2024-07-13 22:14:21 +0200

    Solution 1 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/r4a7n1u6 Prompt: Given a string, return the middle character as a string if it is odd length; return the two middle characters as a string if it is even length.

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,10 +1,9 @@
 Here is the solution:
 
-string middleCharacter(string s) {
-    int len = s.length();
-    if(len%2==0){
-        return s.substr(len/2-1,len/2);
-    } else{
-        return s.substr(len/2,1);
-    }
+string middleCharacter(string str) {
+    int len = str.length();
+    if (len % 2 == 1)
+        return string(1, str[len / 2]);
+    else
+        return string(1, str[(len - 1) / 2]) + string(1, str[(len + 1) / 2]);
 }
