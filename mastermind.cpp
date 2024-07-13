adace0001642a863744f2d5ceef2c8ed9bd50135
beep boop <githubbot@vadim.me>
2024-07-13 14:52:14 +0200

    Solution 36 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/5fkijw86 Prompt: abc22f10-869c-4aae-a8a0-705fb1a35b1e.cpp:1:21: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                     ^~~~~~ abc22f10-869c-4aae-a8a0-705fb1a35b1e.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int mastermind(std::string code, std::string guess) { abc22f10-869c-4aae-a8a0-705fb1a35b1e.cpp:1:39: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                                       ^~~~~~ abc22f10-869c-4aae-a8a0-705fb1a35b1e.cpp:1:39: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? abc22f10-869c-4aae-a8a0-705fb1a35b1e.cpp:1:51: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(std::string code, std::string guess) {       |                                                   ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,30 +1,29 @@
 #include <string>
-using namespace std;
 
-int mastermind(string code, string guess) {
+int mastermind(std::string code, std::string guess) {
     int black = 0;
     int white = 0;
-    
+
     // Count correct positions
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             black++;
         }
     }
-    
+
     // Count correct colors, but wrong position
-    array<int, 6> code_counts({0});
-    array<int, 6> guess_counts({0});
+    int code_counts[6] = {0};
+    int guess_counts[6] = {0};
     for (char c : code) {
         ++code_counts[c - 'A'];
     }
     for (char g : guess) {
         ++guess_counts[g - 'A'];
     }
-    
+
     for (int i = 0; i < 6; ++i) {
-        white += min(code_counts[i], guess_counts[i]);
+        white += std::min(code_counts[i], guess_counts[i]);
     }
-    
+
     return black + white;
 }
