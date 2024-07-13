c79d2f86ba6e5b9d99f286e169ea6236aac41ca2
beep boop <githubbot@vadim.me>
2024-07-13 11:38:27 +0200

    Solution 98 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/puafi84q Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/191fc477-abb4-4478-a9fb-029015c2e ce0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,5 +1,3 @@
 ```
-import re
-
-def validate_and_capitalize(new_text):
-    print(f"{'' if re.match(r'^[a-zA-Z_]+$', new_text) else new_text.capitalize()}"
+new_text = input("Enter some text: ")
+print(f"{'' if re.match(r'^[a-zA-Z_]+$', new_text) else new_text.capitalize()}"
