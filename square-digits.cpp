be8e7e6654e3d22621046194086237ae0b687ca9
beep boop <githubbot@vadim.me>
2024-07-13 20:17:27 +0200

    Solution 96 of square-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/8s4f58n0 Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from acbc3ce7-74e4-4b44-84fa-a99a2957bbd5.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<const char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ acbc3ce7-74e4-4b44-84fa-a99a2957bbd5.cpp:6:21:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- square-digits.cpp
+++ square-digits.cpp
@@ -1,11 +1,12 @@
+```cpp
 #include <string>
 using namespace std;
 
 string squareDigits(string input) {
     string output = "";
-    for (char c : input) {
-        int num = c - '0'; 
-        output += to_string(num * num);
+    for (int i = 0; i < input.length(); i++) {
+        int digit = stoi(string(1, input[i]));
+        output += to_string(digit * digit);
     }
     return output;
 }
