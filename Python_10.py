d7149f639bbf2c52894b6bc2a26255041a3f1832
beep boop <githubbot@vadim.me>
2024-07-13 11:20:03 +0200

    Solution 28 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lydubfu0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf9e7597-b269-4535-b60e-7f682b764 2ad.py", line 19, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf9e7597-b269-4535-b60e-7f682b764 2ad.py", line 16, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -2,18 +2,13 @@
 def is_palindrome(string: str) -> bool:
     return string == string[::-1]
 
-
 def make_palindrome(string: str) -> str:
     if not string.islower():
         string = string.lower()
-    s = list(string)
-    left, right = 0, len(s) - 1
-    while left < right:
-        if s[left] != s[right]:
-            for i in range(len(s), 0, -1):
-                if ''.join(s[:i]).lower() == ''.join(s[:i]).lower()[::-1]:
-                    return string + ''.join(s[:i]).lower()[::-1]
-            break
-        left += 1
-        right -= 1
-    return ''.join(s)
+    half = len(string) // 2
+    middle = ''
+    if len(string) % 2 != 0:
+        middle = string[half]
+        half -= 1
+    first_half = string[:half+1][::-1]
+    return first_half + middle + first_half[::-1]
