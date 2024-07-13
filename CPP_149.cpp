1960bf927e5fc6d428d33b0a090a78c6f9fca952
beep boop <githubbot@vadim.me>
2024-07-13 04:34:42 +0200

    Solution 30 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/t8miolwn Prompt:    16 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ca03ff42-4c37-4feb-90af-e2d102e581e8.cpp:16:44: error: ‘b’ was not declared in t his scope    16 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ca03ff42-4c37-4feb-90af-e2d102e581e8.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ca03ff42-4c37-4feb-90af-e2d102e581e8.cpp:15: ca03ff42-4c37-4feb-90af-e2d102e581e8.cpp: In function ‘int main()’: ca03ff42-4c37-4feb-90af-e2d102e581e8.cpp:25:20: error: ‘sorted_list_sum’ was not  declared in this scope    25 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ ca03ff42-4c37-4feb-90af-e2d102e581e8.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,22 +1,8 @@
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-
 vector<string> sorted_list_sum(vector<string> lst) {
-    vector<string> result;
-    for (const auto& str : lst) {
-        int sum = 0;
-        for (char c : str) {
-            sum += c;
-        }
-        if (sum % 2 == 0) {
-            result.push_back(str);
-        }
-    }
-    sort(result.begin(), result.end(),
-         [](const string& a, const string& b) {
-             return std::to_string(a).size() + std::accumulate(a.begin(), a.end(), 0) <
-                    std::to_string(b).size() + std::accumulate(b.begin(), b.end(), 0);
-         });
-    return result;
+    vector<string> oddLengthStrings;
+    for (const string& str : lst)
+        if (str.length() % 2 == 1)
+            oddLengthStrings.push_back(str);
+    sort(oddLengthStrings.begin(), oddLengthStrings.end());
+    return oddLengthStrings;
 }
