b13e8bdcc334edd310bd76fa6c2308e199012a9a
beep boop <githubbot@vadim.me>
2024-07-13 18:40:09 +0200

    Solution 6 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/hz6k4g9f Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,34 +1,39 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int> v) {
-    int n = v.size();
-    vector<vector<int>> res(2);
-    for (int i = 0; i < n - 1; ++i) {
-        if (abs(v[i] - v[i + 1]) <= abs(v[0] - v[n - 1])) {
-            res[0].insert(res[0].end(), v.begin(), v.begin() + i + 1);
-            res[1].insert(res[1].begin(), v.begin() + i, v.end());
-            return res;
+pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
+    int min_diff = INT_MAX;
+    int cut_index = 0;
+    
+    for (int i = 1; i < vec.size(); ++i) {
+        int diff = abs(vec[i] - vec[0]);
+        if (diff <= min_diff) {
+            min_diff = diff;
+            cut_index = i;
         }
     }
-    res[0] = v;
-    res[1].clear();
-    return res;
+    
+    return {{vec[0]}, {vec.begin() + cut_index, vec.end()}};
 }
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> v[i];
+    vector<int> vec;
+    int num;
+    cout << "Enter numbers (enter -1 to stop): ";
+    while (cin >> num && num != -1) {
+        vec.push_back(num);
     }
-    vector<vector<int>> result = cutVector(v);
-    for (auto &v : result) {
-        for (int x : v) {
-            cout << x << " ";
-        }
-        cout << endl;
+    
+    pair<vector<int>, vector<int>> result = cutVector(vec);
+    cout << "Cut at index " << vec.size() - 1 << endl;
+    for (const auto& v : result.first) {
+        cout << v << " ";
     }
+    cout << endl;
+    for (const auto& v : result.second) {
+        cout << v << " ";
+    }
+    cout << endl;
+    
     return 0;
 }
