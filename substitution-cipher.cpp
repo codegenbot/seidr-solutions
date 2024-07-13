1ca280cc1a1c950a59c47dea2959f35eb59a4d7d
beep boop <githubbot@vadim.me>
2024-07-13 03:25:02 +0200

    Solution 49 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/gna44ad1 Prompt: his scope     3 |     cin >> cipher1 >> cipher2 >> message;       |     ^~~ 47d5cce5-d44b-4c45-abb2-1b350c143dac.cpp:3:12: error: ‘cipher1’ was not declared  in this scope     3 |     cin >> cipher1 >> cipher2 >> message;       |            ^~~~~~~ 47d5cce5-d44b-4c45-abb2-1b350c143dac.cpp:3:23: error: ‘cipher2’ was not declared  in this scope     3 |     cin >> cipher1 >> cipher2 >> message;       |                       ^~~~~~~ 47d5cce5-d44b-4c45-abb2-1b350c143dac.cpp:3:34: error: ‘message’ was not declared  in this scope     3 |     cin >> cipher1 >> cipher2 >> message;       |                                  ^~~~~~~ 47d5cce5-d44b-4c45-abb2-1b350c143dac.cpp:13:5: error: ‘cout’ was not declared in  this scope    13 |     cout << message << endl;       |     ^~~~ 47d5cce5-d44b-4c45-abb2-1b350c143dac.cpp:13:24: error: ‘endl’ was not declared i n this scope    13 |     cout << message << endl;       |                        ^~~~

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -8,13 +8,14 @@ int main() {
     cin >> cipher1 >> cipher2 >> message;
     
     for (char &c : message) {
-        size_t pos1 = cipher1.find(c);
-        size_t pos2 = cipher2.find(c);
-        
-        if (pos1 != string::npos) {
-            c = cipher2[pos1];
-        } else if (pos2 != string::npos) {
-            c = cipher1[pos2];
+        size_t idx = cipher1.find(c);
+        if (idx != string::npos) {
+            c = cipher2[idx];
+        } else {
+            idx = cipher2.find(c);
+            if (idx != string::npos) {
+                c = cipher1[idx];
+            }
         }
     }
     
