9003f9b5381de528e4034e509eb0e8117f947c4d
beep boop <githubbot@vadim.me>
2024-07-13 03:04:07 +0200

    Solution 30 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/7ae6bski Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/051eb8df-f938-45f4-8375-a18de6ebd ac2.py", line 18, in <module>     check(filter_by_prefix)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/051eb8df-f938-45f4-8375-a18de6ebd ac2.py", line 13, in check     assert filter_by_prefix(            ^^^^^^^^^^^^^^^^^ TypeError: filter_by_prefix() takes 0 positional arguments but 2 were given

--- Python_29.py
+++ Python_29.py
@@ -1,6 +1,6 @@
 ```
-def filter_by_prefix() -> None:
-    string_list = []
+def filter_by_prefix(path=None) -> None:
+    string_list: List[str] = []
     while True:
         s = input("Enter a list of strings (one per line), separate them with '%%': ")
         if s.lower() == "%%":
