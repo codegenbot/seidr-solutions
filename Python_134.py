2c8b9f0e3232cd4825936c25753c0056b81bf5eb
beep boop <githubbot@vadim.me>
2024-07-13 11:52:51 +0200

    Solution 99 of Python/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fw253inm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/56912ebd-bfe9-44a0-a396-98b14ce66 1e4.py", line 15, in <module>     check(check_if_last_char_is_a_letter)           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ NameError: name 'check_if_last_char_is_a_letter' is not defined

--- Python_134.py
+++ Python_134.py
@@ -1,15 +1,12 @@
-```Python
+```
+def check_if_last_char_is_a_letter(user_input):
+    return not user_input[-1].isalnum()
+
 def check_palindrome():
-    user_input = input("Enter a string: ")
+    user_input = input("Enter a string: ").lower()
     if len(user_input) != len(user_input[::-1]):
         print(f"'{user_input}' is not a palindrome.")
-        if user_input.lower() != user_input.lower()[::-1]:
+        if user_input.lower() != user_input[::-1].lower():
             print("It is not even spelled the same way backwards when ignoring case.")
     else:
-        if user_input == user_input[::-1].lower():
-            print(f"'{user_input}' is a palindrome.")
-        else:
-            print(f"'{user_input}' is a half-palindrome, i.e., it's the same forwards and backwards when ignoring case.")
-
-
-check_palindrome()
+        print(f"'{user_input}' is a palindrome.")
