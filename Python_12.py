79b804214feea611edd42aa7575cd57ae67ac05f
beep boop <githubbot@vadim.me>
2024-07-13 02:28:56 +0200

    Solution 38 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/vkgoy280 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ed8bc0b9-17af-4e2d-a54d-312670f01 5de.py", line 1, in <module>     def longest(strings: list[optional[str]]) -> optional[str]:                               ^^^^^^^^ NameError: name 'optional' is not defined

--- Python_12.py
+++ Python_12.py
@@ -1,12 +1,13 @@
 from typing import List, Optional
 
-
 def longest(strings: List[Optional[str]]) -> Optional[str]:
     def check(longest):
         if not strings:
             return None
         result = strings[0]
         for s in strings:
-            if len(s) > len(result):
+            if isinstance(s, str) and len(s) > len(result):
                 result = s
         return result
+
+    return check(check)
