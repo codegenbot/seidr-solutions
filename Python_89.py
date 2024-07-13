6ccb8c1ef759c5bd1ab7f2543e04c4fcf54064df
beep boop <githubbot@vadim.me>
2024-07-13 08:15:05 +0200

    Solution 29 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/13mtq4gf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6a683f1a-bb91-4b81-8c6c-b199792ae 799.py", line 24, in <module>     check(encrypt)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6a683f1a-bb91-4b81-8c6c-b199792ae 799.py", line 20, in check     encrypt("a") == "e" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_89.py
+++ Python_89.py
@@ -3,8 +3,8 @@ def encrypt(s):
     for char in s: 
         if char.isalpha(): 
             offset = 65 if char.isupper() else 97
-            shift = 3 if char.isupper() else 3 if char.islower() else 0
+            shift = 3
             result += chr((ord(char) - offset + shift) % 26 + offset)
         else:  
-            result += char  
+            result += char
     return result
