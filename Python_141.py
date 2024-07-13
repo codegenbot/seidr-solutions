d91120c7279f8f5c6326d77865cceaaf0ea62b74
beep boop <githubbot@vadim.me>
2024-07-13 18:40:26 +0200

    Solution 62 of Python/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/onhihsyn Prompt: Complete the following code given the task description and function signature.

--- Python_141.py
+++ Python_141.py
@@ -2,6 +2,13 @@
 def file_name_check(file_name):
     if len([c for c in file_name if c.isdigit()]) > 3:
         return 'No'
-    if '.' not in file_name or file_name.split('.')[0].lstrip().startswith(('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')) or file_name.split('.')[1].lower() not in ['txt', 'exe', 'dll']:
+    if file_name.count('.') != 1:
+        return 'No'
+    name_parts = file_name.split('.')
+    if not name_parts[0][0].isalpha():
+        return 'No'
+    if len(name_parts) != 2:
+        return 'No'
+    if name_parts[1].lower() not in ['txt', 'exe', 'dll']:
         return 'No'
     return 'Yes'
