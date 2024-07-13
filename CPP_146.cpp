538ae61581420720b90bbc8e26259e8b16ea44e4
beep boop <githubbot@vadim.me>
2024-07-13 04:34:56 +0200

    Solution 39 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/s15vggwr Prompt: 09b547f2-4287-4fbb-a84e-a444fa560be7.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int specialFilter(vector<int> nums) {       |                   ^~~~~~ 09b547f2-4287-4fbb-a84e-a444fa560be7.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int specialFilter(vector<int> nums) {       |                          ^~~ In file included from 09b547f2-4287-4fbb-a84e-a444fa560be7.cpp:11: 09b547f2-4287-4fbb-a84e-a444fa560be7.cpp: In function ‘int main()’: 09b547f2-4287-4fbb-a84e-a444fa560be7.cpp:13:26: error: ‘specialFilter’ cannot be  used as a function    13 |     assert (specialFilter({}) == 0                   );       |             ~~~~~~~~~~~~~^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <cassert>
 
 int specialFilter(std::vector<int> nums) {
     int count = 0;
@@ -9,9 +8,4 @@ int specialFilter(std::vector<int> nums) {
         }
     }
     return count;
-}
-
-int main() {
-    assert(specialFilter({}) == 0);
-    return 0;
 }
