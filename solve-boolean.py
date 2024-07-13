df636ac63318d9d42288cae1a913cdc8263b603b
beep boop <githubbot@vadim.me>
2024-07-13 13:31:36 +0200

    Solution 24 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/4rv4b06w Prompt: Program did not receive expected input

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,24 +1,22 @@
-from functools import reduce
-
-def solve_boolean(expression):
+def solve_boolean():
+    expression = input()
     ops = {'&': lambda x, y: x and y, '|': lambda x, y: x or y}
     stack = []
+    
     for char in expression:
-        if char != ' ': 
-            if char in 'TF':
-                if char == '(':
-                    stack.append(char)
-                elif char == ')':
-                    while stack[-1] != '(':
-                        stack.append(ops[stack.pop()](stack.pop(), stack.pop()))
-                    stack.pop()
-                else:
-                    stack.append('T' if char == 'T' else 'F')
-            elif char in ops:
-                while stack and (char == ')' or stack[-1] in ops):
-                    if stack[-1] == '(':
-                        break
-                    stack.append(ops[stack.pop()](stack.pop(), stack.pop()))
-                stack.append(char)
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            while stack and stack[-1] != '(':
+                stack.append(ops[stack.pop()](stack.pop(), stack.pop()))
+            stack.pop()
+        elif char in 'TF':
+            stack.append(char == 'T')
+        elif char in ops:
+            while stack and (char == ')' or stack[-1] in ops):
+                if stack[-1] == '(':
+                    break
+                stack.append(ops[stack.pop()](stack.pop(), stack.pop()))
+            stack.append(char)
 
     return reduce(lambda x, y: ops[y](x, stack.pop()), [op for op in ops if op in stack], stack[-1])
