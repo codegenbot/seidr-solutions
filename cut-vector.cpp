44d12ff45b39438efaae7c40ba3bc4480bc6a5ac
beep boop <githubbot@vadim.me>
2024-07-13 22:42:56 +0200

    Solution 90 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/zs1yd7ig Prompt:   +++ |+#include <iostream>     2 | using namespace std; 96203d7d-6aa8-4459-a9f0-d76cfc4e2aeb.cpp:48:13: error: ‘cout’ was not declared i n this scope    48 |             cout << num << " ";       |             ^~~~ 96203d7d-6aa8-4459-a9f0-d76cfc4e2aeb.cpp:48:13: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’? 96203d7d-6aa8-4459-a9f0-d76cfc4e2aeb.cpp:50:9: error: ‘cout’ was not declared in  this scope    50 |         cout << endl;       |         ^~~~ 96203d7d-6aa8-4459-a9f0-d76cfc4e2aeb.cpp:50:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 96203d7d-6aa8-4459-a9f0-d76cfc4e2aeb.cpp:50:17: error: ‘endl’ was not declared i n this scope    50 |         cout << endl;       |                 ^~~~ 96203d7d-6aa8-4459-a9f0-d76cfc4e2aeb.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,31 +1,54 @@
-#include <climits>
-#include <cmath>
-
+#include <iostream>
+#include <vector>
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int> v) {
-    int n = v.size();
-    vector<vector<int>> res(2);
-    for (int i = 0; i < n; i++) {
-        if (i == 0 || i == n - 1) {
-            res[0].push_back(v[i]);
-            res[1].push_back(v[i]);
-        } else {
-            int minDiff = INT_MAX;
-            int cutIndex = -1;
+vector<vector<int>> cutVector(vector<int>& nums) {
+    int min_diff = INT_MAX;
+    vector<vector<int>> result;
+    
+    for (int i = 1; i <= nums.size(); i++) {
+        int left_sum = 0, right_sum = 0;
+        
+        for (int j = 0; j < i; j++) {
+            left_sum += nums[j];
+        }
+        
+        for (int j = i; j < nums.size(); j++) {
+            right_sum += nums[j];
+        }
+        
+        int diff = abs(left_sum - right_sum);
+        
+        if (diff <= min_diff) {
+            min_diff = diff;
+            result = {{}, {}};
+            
             for (int j = 0; j < i; j++) {
-                int diff = abs(v[j] - v[i]);
-                if (diff < minDiff) {
-                    minDiff = diff;
-                    cutIndex = j;
-                }
+                result[0].push_back(nums[j]);
             }
-            res[0].insert(res[0].begin(), v.begin(), v.begin() + cutIndex);
-            res[1].push_back(v[i]);
-            for (int j = i; j < n; j++) {
-                res[1].push_back(v[j]);
+            
+            for (int j = i; j < nums.size(); j++) {
+                result[1].push_back(nums[j]);
             }
         }
     }
-    return res;
+    
+    return result;
+}
+
+int main() {
+    vector<int> nums;
+    int n;
+    cin >> n;
+    nums.push_back(n);
+    // Read the rest of the input
+    // ...
+    vector<vector<int>> res = cutVector(nums);
+    for (auto &v : res) {
+        for (int num : v) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
