155b1a74a6bbfa9261cb8451b432a3ab1139304e
beep boop <githubbot@vadim.me>
2024-07-13 19:21:28 +0200

    Solution 84 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/5fkijw86 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,6 +1,3 @@
-#include <string>
-#include <iostream>
-
 int mastermind(const std::string& code, const std::string& guess) {
     int black = 0;
     int white = 0;
@@ -13,12 +10,18 @@ int mastermind(const std::string& code, const std::string& guess) {
     }
 
     // Count correct colors (white pegs)
-    int code_counts[6] = {0}; 
+    int code_counts[6] = {0};
     for (int i = 0; i < 4; ++i) {
-        if (++code_counts[code[i] - 'A'] == 1) {
-            white++;
-        }
+        code_counts[code[i] - 'A']++;
     }
 
+    int temp_counts[6] = {0};
+    for (int i = 0; i < 4; ++i) {
+        temp_counts[guess[i] - 'A']++;
+    }
+
+    for (int i = 0; i < 6; ++i) {
+        white -= (code_counts[i] - temp_counts[i]);
+    }
     return black + white;
 }
