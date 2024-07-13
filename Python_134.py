7d2a956e41f71bff39e7441dd16990556482361d
beep boop <githubbot@vadim.me>
2024-07-13 08:34:14 +0200

    Solution 12 of Python/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fw253inm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e83fb17a-70b0-4790-9cc0-6f010a654 11d.py", line 13, in <module>     check(check_if_last_char_is_a_letter)           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ NameError: name 'check_if_last_char_is_a_letter' is not defined

--- Python_134.py
+++ Python_134.py
@@ -1,6 +1,9 @@
+```
 def check_palindrome(input_string):
     if len(input_string) <= 1:
         return True
     if input_string[0] != input_string[-1]:
         return False
     return check_palindrome(input_string[1:-1])
+
+print(check_palindrome(input("Enter a string: ")))
