8533e148c2ed5eb030c86fd1db547f3010869649
beep boop <githubbot@vadim.me>
2024-07-14 01:42:17 +0200

    Solution 21 of dice-game, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/fw9e5yyp Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- dice-game.cpp
+++ dice-game.cpp
@@ -1,6 +1,5 @@
-```cpp
 #include <iostream>
-#include <cmath>
+using namespace std;
 
 double probability(int n, int m) {
     double total = (double)(n * m);
@@ -15,11 +14,8 @@ double probability(int n, int m) {
 
 int main() {
     int n, m;
-    std::cout << "Enter Peter's die size: ";
-    std::cin >> n;
-    std::cout << "Enter Colin's die size: ";
-    std::cin >> m;
+    cin >> n >> m;
     double result = probability(n, m);
-    std::cout << "Probability: " << result << std::endl;
+    cout << result << endl;
     return 0;
 }
