9bba2b9ac03a7cf657033d0f83e90d85e1fbd241
beep boop <githubbot@vadim.me>
2024-07-13 03:09:31 +0200

    Solution 58 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d45c05lr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/17eaad66-d2b8-40d9-8025-266d9f7b8 101.py", line 9, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/17eaad66-d2b8-40d9-8025-266d9f7b8 101.py", line 6, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -2,4 +2,4 @@
 def make_palindrome(string: str) -> str:
     cleaned_string = "".join(char for char in string if char.isalnum()).lower()
     half = cleaned_string[:len(cleaned_string)//2]
-    return ("".join([c*2 for c in half]) + "".join([c for c in half])[::-1]).center(len(cleaned_string) * 2, '#')[1:-1].replace('#', '').upper()
+    return "#" + (half + half[::-1]) * 2 + "#".upper().replace("#", '')
