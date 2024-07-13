9e7109c68092233e4546c40eb43ccf27192f0b4c
beep boop <githubbot@vadim.me>
2024-07-13 08:38:52 +0200

    Solution 30 of Python/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/k4cd2f1b Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6da4b94a-528d-4bb9-b81f-900363044 cc7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_101.py
+++ Python_101.py
@@ -1,5 +1,6 @@
-def words_string(s):
-    if not s:
-        return []
-    words = s.replace("  ", " ").split(",")
-    return [word.strip().title() for word in words]
+class Solution:
+    def words_string(self, s):
+        if not s:
+            return []
+        words = s.replace("  ", " ").split(",")
+        return [word.strip().title() for word in words]
