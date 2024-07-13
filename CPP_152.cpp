f72e6c91dd51f06d53d6d5e6a628932bd5ca91ca
beep boop <githubbot@vadim.me>
2024-07-13 03:17:30 +0200

    Solution 95 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/uswfokmt Prompt:       |             ^~~~~~ 7bd083a2-ab24-4a35-9da1-44871a4aa44b.cpp:34:34: error: expected primary-expressi on before ‘int’    34 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7bd083a2-ab24-4a35-9da1-44871a4aa44b.cpp:34:39: error: expression list treated a s compound expression in initializer [-fpermissive]    34 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 7bd083a2-ab24-4a35-9da1-44871a4aa44b.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 7bd083a2-ab24-4a35-9da1-44871a4aa44b.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 7bd083a2-ab24-4a35-9da1-44871a4aa44b.cpp:33: 7bd083a2-ab24-4a35-9da1-44871a4aa44b.cpp: In function ‘int main()’: 7bd083a2-ab24-4a35-9da1-44871a4aa44b.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
-bool compare(vector<int> game, vector<int> guess) {
+bool compare(std::vector<int> game, std::vector<int> guess) {
     for(int i = 0; i < game.size();i++){
-        if(game[i] != guess[i]) return false;
+        if(game[i] != guess[i]){
+            return false;
+        }
     }
     return true;
 }
@@ -21,10 +22,50 @@ int main() {
         cout << "Error: Invalid input. Please check your input again." << endl;
         return 1;
     } else {
-        vector<int> res = getResult(game,guess);
+        vector<int> res = getCorrectGuess(game, guess);
         for(auto x:res)cout << x << " ";
         cout << endl; 
     }
     
     return 0;
+}
+
+vector<int> getCorrectGuess(std::vector<int> game, std::vector<int> guess) {
+    vector<int> result;
+    int correct = 0;
+    int partiallyCorrect = 0;
+    for(int i = 0; i < game.size();i++){
+        if(game[i] == guess[i]){
+            correct++;
+        } else if(guess[i] > game[i]) {
+            for(int j = i+1; j < game.size();j++) {
+                if(guess[j] > game[j] && !contains(result, j)) {
+                    result.push_back(j);
+                    break;
+                }
+            }
+        } else {
+            for(int j = i+1; j < game.size();j++) {
+                if(guess[j] < game[j] && !contains(result, j)) {
+                    result.push_back(j);
+                    break;
+                }
+            }
+        }
+    }
+    partiallyCorrect = game.size() - correct;
+    while(partiallyCorrect--) {
+        for(int i = 0; i < game.size();i++){
+            if(!contains(result, i) && !contains(guess, game[i])) {
+                result.push_back(i);
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+bool contains(vector<int> v, int x) {
+    for(auto y:v)if(y==x)return true;
+    return false;
 }
