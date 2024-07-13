c2dedde92df7262c37ed77a8a6220016e0701da7
beep boop <githubbot@vadim.me>
2024-07-13 12:02:45 +0200

    Solution 55 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/crhzff04 Prompt: df3f4562-39e8-48aa-94ac-7cf4976a7550.cpp: In function ‘std::string camelCase(std ::string)’: df3f4562-39e8-48aa-94ac-7cf4976a7550.cpp:21:52: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘toupper’    21 |             result += s.substr(i + 1).substr(0, 1).toupper() + s.substr( i + 1).substr(1);       |                                                    ^~~~~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,4 +1,9 @@
 ```cpp
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 string camelCase(string s) {
     string result = "";
     for (int i = 0; i <= s.size(); i++) {
@@ -11,7 +16,7 @@ string camelCase(string s) {
             break;
         } else if (s[i] == ' ') {
             if (i > 0) {
-                result[0] = std::toupper(result[0]);
+                result[0] = toupper(result[0]);
             }
             result += s.substr(0, i);
             result += std::tolower(s.substr(i + 1).substr(0, 1)) + s.substr(i + 1).substr(1);
@@ -21,4 +26,14 @@ string camelCase(string s) {
         }
     }
     return result;
+}
+
+int main() {
+    string str;
+    while (true) {
+        cout << "Enter a string in kebab-case: ";
+        getline(cin, str);
+        cout << "The camelCase is: " << camelCase(str) << endl;
+    }
+    return 0;
 }
