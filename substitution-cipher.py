7d815f4de74f508b2ba82f9563625fab498999ba
beep boop <githubbot@vadim.me>
2024-07-13 11:01:41 +0200

    Solution 57 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/gkja9pbr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/938cf0da-0309-4470-9751-76a64ff2f 6f4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -5,4 +5,4 @@ def substitution_cipher(cipher1, cipher2, message):
             deciphered_message += cipher2[cipher1.index(char)]
         else:
             deciphered_message += char
-    return deciphered_message/
+    return deciphered_message
