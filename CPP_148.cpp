e74be7a6e76792a7a26e4c0c4f1dec23e2f2d662
beep boop <githubbot@vadim.me>
2024-07-13 02:38:37 +0200

    Solution 84 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mt8tp5nb Prompt: 8ffffd75-fa4c-4e48-b1cf-8425decd9e3b.cpp:36:5: note: ‘int main()’ previously def ined here    36 | int main() {       |     ^~~~ In file included from 8ffffd75-fa4c-4e48-b1cf-8425decd9e3b.cpp:65: 8ffffd75-fa4c-4e48-b1cf-8425decd9e3b.cpp: In function ‘int main()’: 8ffffd75-fa4c-4e48-b1cf-8425decd9e3b.cpp:75:23: error: invalid initialization of  reference of type ‘const std::vector<std::__cxx11::basic_string<char> >&’ from expression of type ‘const char [8]’    75 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                       ^~~~~~~~~ 8ffffd75-fa4c-4e48-b1cf-8425decd9e3b.cpp:5:61: note: in passing argument 1 of ‘s td::vector<std::__cxx11::basic_string<char> > bf(const std::vector<std::__cxx11: :basic_string<char> >&, const std::vector<std::__cxx11::basic_string<char> >&, c onst std::vector<std::__cxx11::basic_string<char> >&)’     5 | std::vector<std::string> bf(const std::vector<std::string>& planets, con st std::vector<std::string>& planet1, const std::vector<std::string>& planet2) {       |                             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~ In file included from 8ffffd75-fa4c-4e48-b1cf-8425decd9e3b.cpp:65: 8ffffd75-fa4c-4e48-b1cf-8425decd9e3b.cpp:75:19: error: ‘issame’ cannot be used a s a function    75 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 #include <string>
 
-std::vector<std::string> bf(const std::vector<std::string>& planets, const std::string& planet1, const std::string& planet2) {
+std::vector<std::string> bf(const std::vector<std::string>& planets, const std::vector<std::string>& planet1, const std::vector<std::string>& planet2) {
     int index1 = -1, index2 = -1;
     
     for (int i = 0; i < planets.size(); i++) {
-        if (planets[i][0] == planet1[0]) {
+        if (planets[i][0] == planet1[0][0]) {
             index1 = i;
-        } else if (planets[i][0] == planet2[0]) {
+        } else if (planets[i][0] == planet2[0][0]) {
             index2 = i;
         }
     }
@@ -44,11 +44,11 @@ int main() {
         std::string planet;
         std::cout << "Enter the name of the " << (i+1) << "th planet: ";
         std::cin >> planet;
-        inputPlanets.push_back(planet);
+        inputPlanets.push_back({planet});
     }
     
     if(!inputPlanets.empty()) {
-        std::vector<std::string> result = bf(inputPlanets, "Mars", "Neptune");
+        std::vector<std::string> result = bf(inputPlanets, {{ "Mars" }}, {{"Neptune"}});;
         
         if (!result.empty())
             std::cout << "Output: ";
