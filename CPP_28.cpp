85cbf5b25cc09fc8529410f4067a5858bd143cc5
beep boop <githubbot@vadim.me>
2024-07-13 06:06:54 +0200

    Solution 77 of CPP/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/cuxwcb54 Prompt: Complete the following code given the task description and function signature.

--- CPP_28.cpp
+++ CPP_28.cpp
@@ -1,7 +1,7 @@
 string concatenate(vector<string> strings){
     string result = "";
-    for(string s : strings) {
-        result += s;
+    for (auto str : strings) {
+        result += str;
     }
     return result;
 }
