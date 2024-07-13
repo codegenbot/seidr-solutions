8cdcf16c8fa8ef2d1c252ab6d342381b2afc9484
beep boop <githubbot@vadim.me>
2024-07-13 08:56:18 +0200

    Solution 1 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/1i3rwteh Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b2c4152-c8c0-49a9-b526-3a558f74b 267.py", line 12, in <module>     check(simplify)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b2c4152-c8c0-49a9-b526-3a558f74b 267.py", line 9, in check     assert simplify("1/5", "1/5") == False, "test13"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test13

--- Python_144.py
+++ Python_144.py
@@ -2,4 +2,4 @@ def simplify(x, n):
     x_num, x_denom = map(int, x.split("/"))
     n_num, n_denom = map(int, n.split("/"))
 
-    return (x_num * n_denom) == (n_num * x_denom)
+    return (x_num * n_denom) == (n_num * x_denom) and (x_num != x_denom or n_num != n_denom)
