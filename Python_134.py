5e2ccb28ac3022ea323764b1a78e9298e03429ca
beep boop <githubbot@vadim.me>
2024-07-13 11:41:23 +0200

    Solution 94 of Python/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fw253inm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/246891af-c9aa-4bb4-a708-5a540a42f 6c6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_134.py
+++ Python_134.py
@@ -2,7 +2,5 @@
 user_input = input("Enter a string: ")
 if len(user_input) != len(user_input[::-1]):
     print(f"'{user_input}' is not a palindrome.")
-    if user_input.lower() != user_input.lower()[::-1]:
-        print("It is not even spelled the same way backwards when ignoring case.")
 else:
     print(f"'{user_input}' is a palindrome.")
