39c1ec8ab6333329d689d026738ab757673a3d75
beep boop <githubbot@vadim.me>
2024-07-13 08:58:33 +0200

    Solution 50 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8fxvx5aq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,46 +1,23 @@
 #include <boost/any.hpp>
-#include <string>
-#include <algorithm>
 
-using namespace std;
+using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return max(a, b);
-    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        return max(a, b);
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string sa = boost::any_cast<string>(a);
-        string sb = boost::any_cast<string>(b);
-
-        int a1 = 0, a2 = 0;
-        int b1 = 0, b2 = 0;
-
-        if (string::npos != sa.find('.')) {
-            a1 = stoi(sa.substr(0, sa.find('.')));
-            a2 = stod(sa.substr(sa.find('.') + 1));
+    if (is_any_of<string>(a)) {
+        if (is_any_of<string>(b)) {
+            return any((string)a.get() > (string)b.get()) ? b : a;
+        } else {
+            string strA = (string)a.get();
+            double numB = boost::any_cast<double>(b);
+            return any(numB > stod(strA)) ? b : a;
         }
-        if (string::npos != sb.find('.')) {
-            b1 = stoi(sb.substr(0, sb.find('.')));
-            b2 = stod(sb.substr(sb.find('.') + 1));
-        }
-
-        if ((a1 > b1) || (a1 == b1 && a2 > b2))
-            return a;
-        else if ((b1 > a1) || (b1 == a1 && b2 > a2))
-            return b;
-
-        return boost::any("None");
+    } else if (is_any_of<string>(b)) {
+        string strB = (string)b.get();
+        double numA = boost::any_cast<double>(a);
+        return any(stod(strB) > numA) ? b : a;
     } else {
-        // If the types are different, compare as strings
-        string sa = boost::any_cast<string>(a);
-        string sb = boost::any_cast<string>(b);
-
-        if (sa > sb)
-            return a;
-        else if (sb > sa)
-            return b;
-
-        return boost::any("None");
+        double numA = boost::any_cast<double>(a);
+        double numB = boost::any_cast<double>(b);
+        return any(numA > numB) ? b : (numA == numB ? a : boost::any("None"));
     }
 }
