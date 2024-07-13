f4ccb52f3221729143824bd79c6b3e076fdb96b9
beep boop <githubbot@vadim.me>
2024-07-13 11:03:28 +0200

    Solution 44 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kjfoknrj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80bb4d3c-cd14-42d4-b15a-1a5082084 47b.py", line 21, in <module>     check(string_to_md5)           ^^^^^^^^^^^^^ NameError: name 'string_to_md5' is not defined

--- Python_162.py
+++ Python_162.py
@@ -2,14 +2,15 @@
 import hashlib
 
 def string_to_md5():
-    text = input("Enter a string: ")
-    if text.isalnum(): 
-        return hashlib.md5(text.encode()).hexdigest()
-    print("Invalid input. Please enter only alphanumeric characters.")
-    return None
+    while True:
+        text = input("Enter a string: ")
+        if text.isalnum(): 
+            return hashlib.md5(text.encode()).hexdigest()
+        print("Invalid input. Please enter only alphanumeric characters.")
+
+def check():
+    while True:
+        print(string_to_md5())
 
 if __name__ == "__main__":
-    while True:
-        result = string_to_md5()
-        if result is not None:
-            print(result)
+    check()
