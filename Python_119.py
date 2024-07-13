0b33305aadcdf59635166bc462d0c2bd35ef79b2
beep boop <githubbot@vadim.me>
2024-07-13 09:43:23 +0200

    Solution 38 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kxnv9mbc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a98a3eb0-c276-4d0f-9a75-f82cff498 a59.py", line 15, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a98a3eb0-c276-4d0f-9a75-f82cff498 a59.py", line 12, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,14 +1,17 @@
-```
 def match_parens(lst):
     result = []
     for s in lst:
-        count = 0
-        for char in s:
-            if char == '(':
-                count += 1
-            elif char == ')':
-                if count == 0:
+        stack = []
+        for c in s:
+            if c == "(":
+                stack.append(c)
+            elif c == ")":
+                if not stack:
+                    result.append("No")
                     break
-                count -= 1
-        result.append("Yes" if count == 0 else "No")
+                stack.pop()
+        if stack:
+            result.append("No")
+        else:
+            result.append("Yes")
     return result
