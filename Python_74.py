2ac6dc9201a035ead1374d35df5b7bde6e698b90
beep boop <githubbot@vadim.me>
2024-07-13 12:59:56 +0200

    Solution 1 of Python/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/atm3o6wy Prompt: Complete the following code given the task description and function signature.

--- Python_74.py
+++ Python_74.py
@@ -5,7 +5,7 @@ def total_match(lst1, lst2):
     
     if sum_chars_lst1 < sum_chars_lst2:
         return lst1
-    elif sum_chars_lst2 < sum_chars_lst1:
+    elif sum_chars_lst1 > sum_chars_lst2:
         return lst2
     else:
         return lst1
