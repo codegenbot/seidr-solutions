0b6c6ec45c3b73e23ba409bfea98cdaf6b0da4d7
beep boop <githubbot@vadim.me>
2024-07-13 05:52:32 +0200

    Solution 15 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/cm31biqe Prompt: a0acb8e0-3d64-469e-bbd1-59541a77cb9d.cpp: In function ‘int bowlingScore(std::str ing)’: a0acb8e0-3d64-469e-bbd1-59541a77cb9d.cpp:42:18: error: expected ‘}’ at end of in put    42 |     return score;       |                  ^ a0acb8e0-3d64-469e-bbd1-59541a77cb9d.cpp:23:28: note: to match this ‘{’    23 | int bowlingScore(string s) {       |                            ^

--- bowling.cpp
+++ bowling.cpp
@@ -1,25 +1,3 @@
-#include <string>
-
-using namespace std;
-
-int bowlingScoreHelper(string s) {
-    int score = 0;
-    int currentRoll = 0;
-    for (char c : s) {
-        if (c == 'X') {
-            score += 30;
-            currentRoll = 0;
-        } else if (c == '/') {
-            score += currentRoll + 10;
-            currentRoll = 0;
-        } else {
-            currentRoll *= 10;
-            currentRoll += c - '0';
-        }
-    }
-    return score;
-}
-
 int bowlingScore(string s) {
     int score = 0;
     for (int i = 0; i < s.length(); i++) {
@@ -39,4 +17,4 @@ int bowlingScore(string s) {
             score += roll;
         }
     }
-    return score;
+}
