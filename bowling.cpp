7863663c573420457601b46a8133223dd9692a23
beep boop <githubbot@vadim.me>
2024-07-13 18:47:00 +0200

    Solution 27 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/58a0rhv6 Prompt: 859f61e3-eec2-4dad-aa52-640347e45b6c.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,46 +1,29 @@
 #include <string>
 
-int bowlingScore(string s) {
+int bowlingScore(std::string s) {
     int score = 0;
     int currentRolls = 0;
     int currentFrame = 1;
 
     for (char c : s) {
         if (c == '/') {
-            if (currentFrame < 10 && (currentRolls < 2 || s[stoi(s.substr(currentFrame*2-1, 1)) - '0'] == 'x')) {
-                score += isStrike() ? 30 : isSpare() ? 20 : 10;
-            }
+            if (currentFrame < 10) {
+                if (currentRolls == 2) {
+                    score += 10;
+                } else {
+                    score += 10 - (10 - (int)(c - '0') * 10);
+                }
             currentRolls = 0;
             currentFrame++;
         } else if (c >= '1' && c <= '9') {
             currentRolls++;
-            score += c - '0';
+            if (currentFrame < 10) {
+                score += c - '0';
+            } else if (currentRolls == 2) {
+                score += c - '0' + (int)(s[s.length() - 1] - '0');
+            }
         }
     }
 
     return score;
-
-}
-
-int isStrike() {
-    int count = 0;
-    for(int i = 0; i < s.length(); i++) {
-        if(s[i] == '/')
-            break;
-        count += (s[i]-'0');
-    }
-    return count >= 10;
-}
-
-int isSpare() {
-    int count = 0;
-    int strikes = 0;
-    for(int i = 0; i < s.length(); i++) {
-        if(s[i] == '/')
-            break;
-        count += (s[i]-'0');
-        if(count >= 10) 
-            return 1;
-    }
-    return 0;
 }
