ce285bd24ded5ba117b2637473945cc1b653a640
beep boop <githubbot@vadim.me>
2024-07-13 07:29:21 +0200

    Solution 46 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/r2kyzfn1 Prompt: this scope    35 |     cin >> n;       |     ^~~ b90ccea6-ba0e-4c73-9287-0de8011de2b8.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; b90ccea6-ba0e-4c73-9287-0de8011de2b8.cpp:40:5: error: ‘cout’ was not declared in  this scope    40 |     cout << "[";       |     ^~~~ b90ccea6-ba0e-4c73-9287-0de8011de2b8.cpp:40:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? b90ccea6-ba0e-4c73-9287-0de8011de2b8.cpp:43:20: error: ‘endl’ was not declared i n this scope    43 |     cout << "]" << endl;       |                    ^~~~ b90ccea6-ba0e-4c73-9287-0de8011de2b8.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -39,14 +39,14 @@ int main() {
     for(auto &x : v) 
         std::cin >> x;
     vector<vector<int>> res = cutVector(v);
-    std::cout << "[";
+    cout << "[";
     for(int i=0; i<res[0].size(); i++) 
-        std::cout << res[0][i] << " ";
-    std::cout << "]" << std::endl;
-    std::cout << "[";
+        cout << res[0][i] << " ";
+    cout << "]" << endl;
+    cout << "[";
     for(int i=0; i<res[1].size(); i++) 
-        std::cout << res[1][i] << " ";
-    std::cout << "]" << std::endl;
-    
+        cout << res[1][i] << " ";
+    cout << "]" << endl;
+
     return 0;
 }
