a4007aa59c89bbc6d7c4d3ea63a209171035918b
beep boop <githubbot@vadim.me>
2024-07-13 08:06:40 +0200

    Solution 18 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/s2jjfv94 Prompt: Complete the following code given the task description and function signature.

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,18 +1,21 @@
 string get_closest_vowel(string word) {
-    int left = 0, right = word.size() - 1;
-    while (left < right) {
-        if (!isvowel(word[left])) {
-            left++;
-        } else if (!isvowel(word[right])) {
-            right--;
-        } else {
-            return isupper(word[right]) ? "U" : "u";
+    int left = 0;
+    for (int right = word.size() - 1; right >= 0; --right) {
+        if (!isvowel(word[right])) {
+            ++left;
+            continue;
         }
+        if (left > 0 && isconsonant(word[right-1]))
+            return string(1, tolower(word[right]));
     }
     return "";
 }
 
 bool isvowel(char c) {
-    c = tolower(c);
-    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+            || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+}
+
+bool isconsonant(char c) {
+    return (!isvowel(c));
 }
