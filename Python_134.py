4617ae5d8f6b3ea216624abccc5ea178fbf596ab
beep boop <githubbot@vadim.me>
2024-07-13 11:39:58 +0200

    Solution 92 of Python/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fw253inm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/92cfada9-4b5d-483b-abf9-fba1afd1a 7b3.py", line 1, in <module>     print(f"'{user_input}' is not a palindrome.")               ^^^^^^^^^^ NameError: name 'user_input' is not defined

--- Python_134.py
+++ Python_134.py
@@ -1,4 +1,3 @@
 ```
 user_input = input("Enter a string: ")
-if len(user_input) != len(user_input[::-1]):
-    print(f"'{user_input}' is not a palindrome.")
+print(f"'{user_input}' is not a palindrome.")
