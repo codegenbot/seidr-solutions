25ea1b25f45f4fdf3aafc4bbff78d60ba89a1987
beep boop <githubbot@vadim.me>
2024-07-13 05:31:34 +0200

    Solution 71 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tcf481c2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fc888dc5-b09b-4e66-abee-e52518b79 6b3.py", line 16, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fc888dc5-b09b-4e66-abee-e52518b79 6b3.py", line 11, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,10 @@
 ```
 def sort_even(l: list):
-    even_list = [x for x in l if x % 2 == 0]
+    even_list = [i for i in l if i % 2 == 0]
     even_list.sort()
     return even_list
+
+
+def check():
+    numbers = [int(i) for i in input("Enter space-separated numbers: ").split()]
+    print(sort_even(numbers))
