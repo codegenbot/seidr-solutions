ac95dceddc1ebcef731fe52c54070dbf395fd3ae
beep boop <githubbot@vadim.me>
2024-07-13 04:26:03 +0200

    Solution 47 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/om6uiz7z Prompt: dbc90ba7-4b8b-4cf4-896f-6dd4fda96228.cpp:3:19: error: ‘string’ was not declared in this scope     3 | bool solveBoolean(string expression) {       |                   ^~~~~~ dbc90ba7-4b8b-4cf4-896f-6dd4fda96228.cpp:3:19: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from dbc90ba7-4b8b-4cf4-896f-6dd4fda96228.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from dbc90ba7-4b8b-4cf4-896f-6dd4fda96228.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,12 +1,10 @@
+#include <stack>
 #include <string>
-using namespace std;
 
-bool solveBoolean(string expression) {
+bool solveBoolean(std::string expression) {
+    stack<char> s;
     for (int i = 0; i < expression.length(); i++) {
         if (expression[i] == '|') {
-            if (i > 0 && (expression[i - 1] == '&' || expression[i - 1] == '(')) {
-                continue;
-            }
             while (!s.empty() && s.top() != '&') {
                 s.pop();
             }
@@ -23,17 +21,7 @@ bool solveBoolean(string expression) {
                 s.pop();
             }
             return false;
-        } else if (expression[i] == '(') {
-            s.push('(');
-        } else if (expression[i] == ')') {
-            while (s.top() != '(') {
-                s.pop();
-            }
-            s.pop();
         }
     }
-    while (!s.empty()) {
-        s.pop();
-    }
-    return false;
+    return true;
 }
