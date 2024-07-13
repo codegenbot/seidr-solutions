f4079acdf3257f8e4b2f5a8e9e63640a7e1b4398
beep boop <githubbot@vadim.me>
2024-07-13 10:27:12 +0200

    Solution 61 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/5nnxhoku Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c2f1c118-abdb-4154-a783-e0c203583 15c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,4 +1,3 @@
-```
 def sorted_list_sum(lst):
     alpha_chars = [i for i in lst if isinstance(i, str) and i.isalpha()]
-    return sorted(set(alpha_chars))
+    return sum(int(char) for char in "".join(sorted(set("".join(alpha_chars)))))
