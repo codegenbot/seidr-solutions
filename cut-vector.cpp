2fef1a1e12f13eeae7e18bf516213419b834aedc
beep boop <githubbot@vadim.me>
2024-07-13 04:13:41 +0200

    Solution 31 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/acxuxpiz Prompt:     9 | for (int i = 0; i <= cutIndex; i++) {       |                 ^ 41c8f074-adc8-4af2-8468-64bde1cb85da.cpp:9:32: error: ‘i’ does not name a type     9 | for (int i = 0; i <= cutIndex; i++) {       |                                ^ 41c8f074-adc8-4af2-8468-64bde1cb85da.cpp:12:1: error: ‘cout’ does not name a typ e    12 | cout << endl;       | ^~~~ 41c8f074-adc8-4af2-8468-64bde1cb85da.cpp:14:1: error: expected unqualified-id be fore ‘for’    14 | for (int i = cutIndex; i < n; i++) {       | ^~~ 41c8f074-adc8-4af2-8468-64bde1cb85da.cpp:14:24: error: ‘i’ does not name a type    14 | for (int i = cutIndex; i < n; i++) {       |                        ^ 41c8f074-adc8-4af2-8468-64bde1cb85da.cpp:14:31: error: ‘i’ does not name a type    14 | for (int i = cutIndex; i < n; i++) {       |                               ^ 41c8f074-adc8-4af2-8468-64bde1cb85da.cpp:17:1: error: ‘cout’ does not name a typ e    17 | cout << endl;       | ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
+
 using namespace std;
+
+int findCutIndex(const vector<int>& nums) {
+    int totalSum = accumulate(nums.begin(), nums.end(), 0);
+    int leftSum = 0, rightSum = totalSum;
+    int cutIndex = -1, minDiff = INT_MAX;
+
+    for (int i = 0; i < nums.size(); i++) {
+        leftSum += nums[i];
+        rightSum -= nums[i];
+        int diff = abs(leftSum - rightSum);
+
+        if (diff < minDiff) {
+            minDiff = diff;
+            cutIndex = i;
+        }
+    }
+
+    return cutIndex;
+}
 
 int main() {
     int n;
     cin >> n;
     vector<int> nums(n);
-    int totalSum = 0;
     for (int i = 0; i < n; i++) {
         cin >> nums[i];
-        totalSum += nums[i];
     }
 
-    int leftSum = 0, minDiff = INT_MAX, cutIndex = -1;
-    for (int i = 0; i < n; i++) {
-        leftSum += nums[i];
-        int rightSum = totalSum - leftSum;
-        int currentDiff = abs(leftSum - rightSum);
-        
-        if (currentDiff < minDiff) {
-            minDiff = currentDiff;
-            cutIndex = i;
-        }
-    }
+    int cutIndex = findCutIndex(nums);
 
-    for (int i = 0; i <= cutIndex; i++) cout << nums[i] << " ";
+    for (int i = 0; i <= cutIndex; i++) {
+        cout << nums[i] << " ";
+    }
     cout << endl;
-    for (auto i = cutIndex + 1; i < n; i++) cout << nums[i] << " ";
+
+    for (int i = cutIndex + 1; i < n; i++) {
+        cout << nums[i] << " ";
+    }
     cout << endl;
 
     return 0;
