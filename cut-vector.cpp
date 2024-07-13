7426d9f881b9cf3ee5a0982007d045fd67ed0dec
beep boop <githubbot@vadim.me>
2024-07-13 05:25:30 +0200

    Solution 44 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/acxuxpiz Prompt: Program did not receive expected input

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,40 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <numeric>
-#include <climits>
-#include <cmath>
-using namespace std;
-
-void findCutIndex(vector<int>& nums) {
-    int n = nums.size();
-    int totalSum = accumulate(nums.begin(), nums.end(), 0);
-    int leftSum = 0;
-    int minDiff = INT_MAX;
-    int cutIndex = -1;
-
-    for (int i = 0; i < n; i++) {
-        leftSum += nums[i];
-        int rightSum = totalSum - leftSum;
-        int diff = abs(leftSum - rightSum);
-        if (diff < minDiff) {
-            minDiff = diff;
-            cutIndex = i;
-        }
-    }
-
-    cout << "Subvector 1: ";
-    for (int i = 0; i <= cutIndex; i++) {
-        cout << nums[i] << " ";
-    }
-    cout << endl;
-
-    cout << "Subvector 2: ";
-    for (int i = cutIndex + 1; i < n; i++) {
-        cout << nums[i] << " ";
-    }
-    cout << endl;
-}
-
 int main() {
     vector<int> nums;
     int num;
@@ -47,7 +10,11 @@ int main() {
         nums.push_back(num);
     }
 
-    findCutIndex(nums);
+    if (nums.empty()) {
+        cout << "No input provided. Exiting program." << endl;
+    } else {
+        findCutIndex(nums);
+    }
     
     return 0;
 }
