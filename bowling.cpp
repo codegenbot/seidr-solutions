cd0d027970124cb14bb31768748880a344acb09a
beep boop <githubbot@vadim.me>
2024-07-14 00:18:31 +0200

    Solution 11 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/n1rydnou Prompt: f2e6d2f2-acf6-4fc5-ac4f-ffbef083bd96.cpp:5:18: error: ‘string’ was not declared in this scope     5 | int bowlingScore(string s) {       |                  ^~~~~~ f2e6d2f2-acf6-4fc5-ac4f-ffbef083bd96.cpp:5:18: note: suggested alternatives: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from f2e6d2f2-acf6-4fc5-ac4f-ffbef083bd96.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from f2e6d2f2-acf6-4fc5-ac4f-ffbef083bd96.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,26 +1,15 @@
-#include <iostream>
-#include <sstream>
-#include <string>
-
-int bowlingScore(string s) {
+int bowlingScore(std::string s) {
     int score = 0;
-    stringstream ss(s);
+    std::stringstream ss(s);
     char c;
-    string temp;
     while (ss >> c) {
         if (c == 'X') {
             score += 30;
         } else if (c == '/') {
             score += 10 + (score / 10 - 1);
         } else {
-            temp = "";
-            while (c != '/' && c != 'X') {
-                temp += c;
-                ss >> c;
-            }
-            int val = stoi(temp);
-            score += val;
+            int temp = c - '0';
+            score += temp;
         }
     }
     return score;
-}
