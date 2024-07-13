972867a735b20534cbdbb624bb1739625fc4c546
beep boop <githubbot@vadim.me>
2024-07-13 10:30:44 +0200

    Solution 53 of Python/147, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/v6hxdyk4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0fca51ef-4c0e-4b48-b307-a79f3ed6a 4b0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_147.py
+++ Python_147.py
@@ -4,13 +4,10 @@ n = int(input())
 
 def get_max_triples(n):
     if n < 1:
-        print("Invalid input. Please enter a positive integer.")
         return None
     max_sum = 0
     for i in range(1, n+1):
-        triple_sum = 0
-        for j in range(i*3-2, (i+1)*3):
-            triple_sum += j
+        triple_sum = sum(range(i*3-2, (i+1)*3))
         if triple_sum > max_sum:
             max_sum = triple_sum
     return max_sum
