f969fa7899833ed170bd17f53405459c89218456
beep boop <githubbot@vadim.me>
2024-07-13 12:12:00 +0200

    Solution 14 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/4rv4b06w Prompt: it must return False for input f|t|t|f&f|f|f|f, but it returns .

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,24 +1,5 @@
+```
 def solve_boolean(expression):
     ops = {'&': lambda x, y: x and y, '|': lambda x, y: x or y}
-    stack = []
-    current_operator = ''
-    for char in expression:
-        if char in 'TF':
-            if char == '(':
-                continue
-            stack.append(True if char == 'T' else False)
-        elif char in ops:
-            while current_operator and current_operator != '(':
-                stack.append(ops[current_operator](stack.pop(), stack.pop()))
-                current_operator = ''
-            current_operator = char
-        elif char == '(':
-            current_operator = char
-        elif char == ')':
-            while current_operator != '(':
-                if current_operator in ops:
-                    stack.append(ops[current_operator](stack.pop(), stack.pop()))
-                    current_operator = ''
-                else:
-                    break
-    return stack[0]
+    expression = expression.replace('(', '( T | )').replace(')', ' ')
+    return eval('(' + ' '.join(map(lambda x: str(x) if x in 'TF' else ops[x]() ,expression.split()))+')')
