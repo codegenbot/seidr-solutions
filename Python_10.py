84ab65c1ace91818e1946cb5f2b5aad3f02941c0
beep boop <githubbot@vadim.me>
2024-07-13 11:16:52 +0200

    Solution 25 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/lydubfu0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf9e7597-b269-4535-b60e-7f682b764 2ad.py", line 19, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf9e7597-b269-4535-b60e-7f682b764 2ad.py", line 16, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,6 +1,10 @@
+```
+def is_palindrome(string: str) -> bool:
+    return string == string[::-1]
+
 def make_palindrome(string: str) -> str:
     if not is_palindrome(string):
-        middle = len(string) // 2
-        first_half = string[:middle]
-        second_half = string[middle:][::-1]
-        return first_half + second_half
+        half = len(string) // 2
+        front = string[:half]
+        back = string[len(string) - 1 :len(string) - 1 - half : -1].lower()
+        return (front + front[::-1]).capitalize() if string[0].isupper() else front + back + back[::-1]
