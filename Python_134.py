474333f5c4c49b1d22ea6f48cbf26510c05a275a
beep boop <githubbot@vadim.me>
2024-07-13 09:48:31 +0200

    Solution 37 of Python/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fw253inm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ab8b72c0-8e3e-4eca-9589-125f42ed4 72d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_134.py
+++ Python_134.py
@@ -1,5 +1,7 @@
+```
 def check_palindrome(input_string):
     input_string = "".join(c for c in input_string if c.isalnum()).lower()
     return input_string == input_string[::-1]
 
-print(check_palindrome("A man a plan a canal Panama"))
+if __name__ == "__main__":
+    print(check_palindrome("A man a plan a canal Panama"))
