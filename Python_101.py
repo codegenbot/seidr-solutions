056f1de8708ed8619b82ab142655951077393709
beep boop <githubbot@vadim.me>
2024-07-13 10:04:04 +0200

    Solution 51 of Python/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/k4cd2f1b Prompt: Program did not receive expected input

--- Python_101.py
+++ Python_101.py
@@ -1,6 +1,9 @@
 import re
 
+
 def words_string(s):
+    if type(s) != str:
+        return "Error: Input must be a string"
     if s is None or not isinstance(s, str) or len(s.strip()) == 0:
         return []
-    return [word.strip().capitalize() for word in re.split(r'\W+', s)]
+    return [word.strip().capitalize() for word in re.split(r"\W+", s)]
