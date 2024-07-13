72b45e8a9048abfe2b27ab2fe885a76783a767ff
beep boop <githubbot@vadim.me>
2024-07-13 08:07:53 +0200

    Solution 14 of dice-game, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/a62k83ub Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- dice-game.cpp
+++ dice-game.cpp
@@ -8,22 +8,5 @@ int main() {
     cin >> n;
     cout << "Enter the number of sides for Colin's die: ";
     cin >> m;
-
-    string strN = to_string(n);
-    string strM = to_string(m);
-
-    cout << "You entered: " << strN << " and " << strM << endl;
-
-    double probability = 0.0;
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=m; j++) {
-            if(i > j) {
-                probability += 1.0 / (double)n / (double)m;
-            }
-        }
-    }
-
-    cout << "The probability that Peter rolls strictly higher than Colin is: " << probability << endl;
-
     return 0;
 }
