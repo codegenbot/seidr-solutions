eae48bc470a99575f942ca942e8f50beefa78066
beep boop <githubbot@vadim.me>
2024-07-13 14:07:45 +0200

    Solution 45 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gthfq2gz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/744ee74a-2617-4ff5-938c-fccb5d953 93f.py", line 13, in <module>     check(rounded_avg)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/744ee74a-2617-4ff5-938c-fccb5d953 93f.py", line 10, in check     assert rounded_avg(5, 5) == "0b101"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_103.py
+++ Python_103.py
@@ -1,6 +1,7 @@
+```
 def rounded_avg(n, m):
     if n > m:
         return -1
-    total = sum(range(n, m + 1))
+    total = sum(range(n, m+1))
     avg = round(total / (m - n + 1)) if m != n else 0
-    return bin(avg)[2:]
+    return bin(avg)[2:].zfill(8)
