02f905b99eea18b7f30a6134449e9d39737143e8
beep boop <githubbot@vadim.me>
2024-07-13 15:00:29 +0200

    Solution 32 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/xkhy80fd Prompt: 04bbf6e2-c8f6-4a24-85a7-6e2b254dd7b2.cpp:1:6: error: ‘string’ in namespace ‘std’  does not name a type     1 | std::string middleCharacter(std::string s) {       |      ^~~~~~ 04bbf6e2-c8f6-4a24-85a7-6e2b254dd7b2.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | std::string middleCharacter(std::string s) {

--- middle-character.cpp
+++ middle-character.cpp
@@ -3,8 +3,8 @@
 std::string middleCharacter(std::string s) {
     int len = s.length();
     if(len % 2 == 0) {
-        return s.substr(len/2-1, 2); 
+        return s.substr(len/2-1, 1); 
     } else {
-        return s.substr(len/2 - 1, 1); 
+        return s.substr(len/2 - 1, 2); 
     }
 }
