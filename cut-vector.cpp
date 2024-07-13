f4368d1ec86782f5045f36cce4e9c0c745e3ef7a
beep boop <githubbot@vadim.me>
2024-07-13 11:46:15 +0200

    Solution 60 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/8etkg2hx Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,31 +1,35 @@
 int main() {
-    int n;
-    cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> nums[i];
+    vector<int> nums;
+    int num;
+    while (cin >> num) {
+        nums.push_back(num);
     }
-    int diff = INT_MAX, idx = -1;
-    for (int i = 1; i < n; ++i) {
+    
+    int diff = INT_MAX;
+    int idx = -1;
+    for (int i = 1; i < nums.size(); ++i) {
         int left_sum = 0, right_sum = 0;
         for (int j = 0; j < i; ++j) {
             left_sum += nums[j];
         }
-        for (int j = i; j < n; ++j) {
+        for (int j = i; j < nums.size(); ++j) {
             right_sum += nums[j];
         }
-        if (abs(left_sum - right_sum) < diff) {
-            diff = abs(left_sum - right_sum);
+        int current_diff = abs(left_sum - right_sum);
+        if (current_diff < diff) {
+            diff = current_diff;
             idx = i;
         }
     }
+    
     for (int i = 0; i < idx; ++i) {
         cout << nums[i] << endl;
     }
     cout << endl;
-    for (int i = idx; i < n; ++i) {
+    for (int i = idx; i < nums.size(); ++i) {
         cout << nums[i] << endl;
     }
     cout << endl;
+    
     return 0;
 }
