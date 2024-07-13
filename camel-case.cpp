2e0bab3249cf536e7bc8028dcd7524535e503162
beep boop <githubbot@vadim.me>
2024-07-13 16:04:39 +0200

    Solution 69 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/crhzff04 Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: cba38494-3118-47c6-bf0b-e057d24b9571.cpp:37:54:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,3 +1,9 @@
+```cpp
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 string camelCase(string s) {
     string result = "";
     for (int i = 0; i < s.size(); i++) { 
@@ -5,25 +11,36 @@ string camelCase(string s) {
             if (!result.empty()) {
                 result[0] = toupper(result[0]);
             }
-            if (s[i] != '-') {
-                string word = s.substr(i + 1);
-                result += word;
-            } else {
-                i++;
-            }
+            string word = s.substr(i + 1);
+            result += word;
+            break;
         } else if (s[i] == ' ') {
-            if (!result.empty()) {
+            if (i > 0) {
                 result[0] = toupper(result[0]);
             }
             result += s.substr(0, i);
-            result += s.substr(i + 1).substr(0, 1) + tolower(s.substr(i + 1).substr(1));
-        } else {
-            if (i == 0) {
-                result = s[i];
-            } else {
-                result += s[i];
+            result += s.substr(i + 1).substr(0, 1);
+            for(int j = 1; j < s.substr(i + 1).size(); j++) {
+                result += tolower(s.substr(i + 1)[j]);
             }
+            break;
+        } else {
+            if (!result.empty()) {
+                result[0] = toupper(result[0]);
+            }
+            result += s[i];
         }
     }
     return result;
+}
+
+int main() {
+    string str;
+    while (true) {
+        cout << "Enter a string in kebab-case: ";
+        getline(cin, str);
+        if (str == "quit") break; 
+        cout << "The camelCase is: " << camelCase(str) << endl;
+    }
+    return 0;
 }
