ed3a30c67fac38492dcd64cee107ef4a80103f9d
beep boop <githubbot@vadim.me>
2024-07-13 18:07:03 +0200

    Solution 62 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/5fkijw86 Prompt:       |                                 ^~~~~~ In file included from e35bf571-8f57-4a12-8d81-992f5dcfde61.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e35bf571-8f57-4a12-8d81-992f5dcfde61.cpp:3:29: error: ‘string’ was not declared in this scope     3 | int mastermind(string code, string guess) {       |                             ^~~~~~ e35bf571-8f57-4a12-8d81-992f5dcfde61.cpp:3:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from e35bf571-8f57-4a12-8d81-992f5dcfde61.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from e35bf571-8f57-4a12-8d81-992f5dcfde61.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e35bf571-8f57-4a12-8d81-992f5dcfde61.cpp:3:41: error: expression list treated as  compound expression in initializer [-fpermissive]     3 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,7 +1,8 @@
+```cpp
 #include <string>
-using namespace std;
+#include <unordered_map>
 
-int mastermind(string code, string guess) {
+int mastermind(std::string code, std::string guess) {
     int black = 0;
     int white = 0;
 
@@ -13,16 +14,16 @@ int mastermind(string code, string guess) {
     }
 
     // Count correct colors (white pegs)
-    bool code_counts[6] = {false};
+    std::unordered_map<char, bool> code_counts;
     for (char c : code) {
-        code_counts[c - 'A'] = true;
+        code_counts[c] = true;
     }
 
     int white_count = 0;
     for (int i = 0; i < 4; ++i) {
-        if (code_counts[guess[i] - 'A']) {
+        if (code_counts.find(guess[i]) != code_counts.end() && !code_counts[guess[i]]) {
             white_count++;
-            code_counts[guess[i] - 'A'] = false;
+            code_counts[guess[i]] = false;
         }
     }
 
