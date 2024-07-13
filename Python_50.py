3e49e4dc15ca8396fa87cec66cd9c05586c95ed0
beep boop <githubbot@vadim.me>
2024-07-13 06:09:35 +0200

    Solution 18 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/u5fnzvz7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc68b650-c403-4613-a800-a36cce090 22d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_50.py
+++ Python_50.py
@@ -1,4 +1,18 @@
-```Python
-decode_shift = input("Enter the string: ")
+decode_shift = input("Enter the shift cipher text: ")
+
+
 def decrypt(decode_shift):
-    return "".join([chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) if ch.isalpha() and ch.lower() >= 'a' and ch.lower() <= 'z' else ch if ch.isalpha() or ch.isspace() else chr(((ord(ch) - 5 - ord('A')) % 26) + ord('A')) for ch in decode_shift])
+    return "".join(
+        [
+            (
+                chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a"))
+                if ch.isalpha() and ch.lower() >= "a" and ch.lower() <= "z"
+                else (
+                    ch
+                    if ch.isalpha() or ch.isspace()
+                    else chr(((ord(ch) - 5 - ord("A")) % 26) + ord("A"))
+                )
+            )
+            for ch in decode_shift
+        ]
+    )
