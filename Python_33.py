7dc23e7085980fe7b7c2e90461afd9482c39a869
beep boop <githubbot@vadim.me>
2024-07-13 06:33:34 +0200

    Solution 86 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mmyso7cs Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7fa4a8d5-c33d-4494-a15f-a8d01a3a1 2d2.py", line 15, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7fa4a8d5-c33d-4494-a15f-a8d01a3a1 2d2.py", line 10, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,5 +1,9 @@
+```
 def sort_third(lst):
     if not all(isinstance(i, (list, tuple)) for i in lst): 
-        return [i for i in lst if isinstance(i, (list, tuple))]
-    result = sorted([x[2] for x in lst if isinstance(x, (list, tuple)) and len(x) > 2])
-    return tuple(result)
+        return "Error: Input not list/tuple"
+    result = [x[2] for x in lst if isinstance(x, (list, tuple)) and len(x) >= 3]
+    if not result:
+        return "Error: No sublists/tuples found with length more than or equal to 3"
+    else:
+        return tuple(sorted(result))
