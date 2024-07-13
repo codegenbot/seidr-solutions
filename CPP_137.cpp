db91779b25d4e41961873cfe7f2c18a8cad05adc
beep boop <githubbot@vadim.me>
2024-07-13 09:05:08 +0200

    Solution 66 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8fxvx5aq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,33 +1,54 @@
-#include <boost/any_cast.hpp>
-
-using namespace boost;
-
-boost::any compare_one(boost::any a, boost::any b) {
-    if (is_any_of<a>(float())) try {
-        float fa = any_cast<float>(a);
-        float fb = any_cast<float>(b);
-
-        if (fa > fb)
+if (a.type() == boost::any::typeclass<float> ||
+    a.type() == boost::any::typeclass<double>) {
+    if (b.type() == boost::any::typeclass<float> ||
+        b.type() == boost::any::typeclass<double>) {
+        float fa = boost::any_cast<float>(a);
+        float fb = boost::any_cast<float>(b);
+        if (fa > fb) {
             return a;
-        else if (fb > fa)
+        } else if (fb > fa) {
             return b;
-
-        return "None";
-    } catch (bad_any_cast&) {}
-    
-    // At least one of the values is not a number
-    if (is_any_of<a>(std::string())) {
-        std::string sa = any_cast<std::string>(a);
-        std::string sb = any_cast<std::string>(b);
-
-        if (sa > sb)
+        } else {
+            return "None";
+        }
+    } else if (b.type() == boost::any::typeclass<std::string>) {
+        std::string fs = boost::any_cast<std::string>(b);
+        float fa = boost::any_cast<float>(a);
+        if (fa > std::stof(fs)) {
             return a;
-        else if (sb > sa)
+        } else if (std::stof(fs) > fa) {
             return b;
-
+        } else {
+            return "None";
+        }
+    } else {
         return "None";
     }
-
-    // If we are here, none of the values were numbers
+} else if (a.type() == boost::any::typeclass<std::string>) {
+    if (b.type() == boost::any::typeclass<float> ||
+        b.type() == boost::any::typeclass<double>) {
+        float fb = boost::any_cast<float>(b);
+        std::string fs = boost::any_cast<std::string>(a);
+        if (std::stof(fs) > fb) {
+            return a;
+        } else if (fb > std::stof(fs)) {
+            return b;
+        } else {
+            return "None";
+        }
+    } else if (b.type() == boost::any::typeclass<std::string>) {
+        std::string fs = boost::any_cast<std::string>(a);
+        std::string fb = boost::any_cast<std::string>(b);
+        if (fs > fb) {
+            return a;
+        } else if (fb > fs) {
+            return b;
+        } else {
+            return "None";
+        }
+    } else {
+        return "None";
+    }
+} else {
     return "None";
 }
