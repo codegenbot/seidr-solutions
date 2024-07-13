da126473a00e25b9160b414da896eeac601e0607
beep boop <githubbot@vadim.me>
2024-07-13 09:41:51 +0200

    Solution 31 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/wx58ekjm Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 210f1876-26a2-4245-baa8-a8902d5749f6.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- find-pair.cpp
+++ find-pair.cpp
@@ -2,18 +2,18 @@
 #include <vector>
 #include <unordered_map>
 
-std::pair<int, int> findPair(std::vector<int>& nums, int target) {
+std::vector<std::pair<int, int>> findPair(std::vector<int>& nums, int target) {
     std::unordered_map<int, int> numMap;
     
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
         if (numMap.find(complement) != numMap.end()) {
-            return {complement, nums[i]};
+            return {std::make_pair(complement, nums[i])};
         }
         numMap[nums[i]] = i;
     }
     
-    return {-1, -1}; 
+    return {std::make_pair(-1, -1)};
 }
 
 int main() {
