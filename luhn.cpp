a2084c463d4271dd9e0f5256021ce0680b5e07d5
beep boop <githubbot@vadim.me>
2024-07-13 14:54:54 +0200

    Solution 37 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/77d0y15m Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 14a9ef70-0c39-48f9-a33c-207ba267db7e.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- luhn.cpp
+++ luhn.cpp
@@ -1,9 +1,7 @@
-#include <vector>
-
-int luhn(std::vector<int> cardNumber) {
+int luhn(std::initializer_list<int> cardNumber) {
     int sum = 0;
     for (int i = cardNumber.size() - 1; i >= 0; i--) {
-        int digit = cardNumber[i] * ((i % 2) + 1);
+        int digit = cardNumber.begin()[i] * ((i % 2) + 1);
         if (digit > 9) {
             digit -= 9;
         }
