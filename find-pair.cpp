b7fdb074b99eea45151b6010e90c4b02e23a330b
beep boop <githubbot@vadim.me>
2024-07-13 14:47:42 +0200

    Solution 59 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/wx58ekjm Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from d5b62292-44a5-4b68-b90b-d09366e07efc.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,23 +1,4 @@
 ```cpp
-#include <vector>
-#include <utility>
-#include <unordered_map>
-
-using namespace std;
-
-pair<int, int> findPair(vector<int>& nums, int target) {
-    unordered_map<int, int> map;
-
-    for (int num : nums) {
-        int complement = target - num;
-        if (map.find(complement) != map.end()) {
-            return {complement, num};
-        }
-        if(map.find(num) != map.end()) {
-            return {num, num};
-        }
-        map[num] = 1;
-    }
-
-    return {-1, -1};
+if(map.find(num) != map.end()) {
+    return std::make_pair(num, num);
 }
