69b9254af4eba0350d98176654e6bceb0a1b6767
beep boop <githubbot@vadim.me>
2024-07-13 09:15:00 +0200

    Solution 23 of Python/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fw253inm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb61a852-8435-4846-9d79-762527cb0 66d.py", line 13, in <module>     check(check_if_last_char_is_a_letter)           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ NameError: name 'check_if_last_char_is_a_letter' is not defined

--- Python_134.py
+++ Python_134.py
@@ -1,4 +1,8 @@
-```
 def check_palindrome(input_string):
     input_string = "".join(c for c in input_string if c.isalnum()).lower()
-    return input_string == input_string[::-1]
+    if len(input_string) == 0:
+        return "Error: The file is empty."
+    else:
+        return input_string == input_string[::-1]
+
+check_palindrome("A man a plan a canal Panama")
