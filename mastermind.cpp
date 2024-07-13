3b4ca5937bab53e520337ce06b5b5857cce4a20a
beep boop <githubbot@vadim.me>
2024-07-13 20:06:07 +0200

    Solution 98 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/5fkijw86 Prompt: In file included from /usr/include/c++/11/string:55,                  from 4866dd2c-8d6d-4bee-b280-b16c081034d3.cpp:2: /usr/include/c++/11/bits/basic_string.h:6289:5: note: candidate: ‘template<class  _CharT, class _Traits, class _Alloc> bool std::operator==(const _CharT*, const std::__cxx11::basic_string<_CharT, _Traits, _Allocator>&)’  6289 |     operator==(const _CharT* __lhs,       |     ^~~~~~~~ /usr/include/c++/11/bits/basic_string.h:6289:5: note:   template argument deduct ion/substitution failed: 4866dd2c-8d6d-4bee-b280-b16c081034d3.cpp:11:47: note:   mismatched types ‘const _CharT*’ and ‘std::__cxx11::basic_string<char>’    11 |             if (guess.substr(pos, 1) == code[i]) {       |                                               ^ In file included from /usr/include/c++/11/string:41,                  from 4866dd2c-8d6d-4bee-b280-b16c081034d3.cpp:2: /usr/include/c++/11/bits/allocator.h:204:7: note: candidate: ‘bool std::operator ==(const std::allocator<char>&, const std::allocator<char>&)’   204 |       operator==(const allocator&, const allocator&) _GLIBCXX_NOTHROW       |       ^~~~~~~~ /usr/include/c++/11/bits/allocator.h:204:18: note:   no known conversion for arg ument 1 from ‘std::__cxx11::basic_string<char>’ to ‘const std::allocator<char>&’   204 |       operator==(const allocator&, const allocator&) _GLIBCXX_NOTHROW       |                  ^~~~~~~~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,6 +1,3 @@
-#include <cstddef>
-#include <string>
-
 int mastermind(std::string code, std::string guess) {
     int whitePegs = 0;
     int blackPegs = 0;
@@ -8,11 +5,13 @@ int mastermind(std::string code, std::string guess) {
     for (size_t i = 0; i < code.length(); ++i) {
         size_t pos = guess.find(code[i]);
         while (pos != std::string::npos) {
-            if (std::string(1, code[i]) == guess.substr(pos, 1)) {
-                blackPegs++;
-                guess.erase(pos, 1);
-            } else {
-                whitePegs++;
+            if (code[i] == guess.substr(pos, 1)[0]) {
+                if (guess.substr(pos, 1) == std::string(1, code[i])) {
+                    blackPegs++;
+                    guess.erase(pos, 1);
+                } else {
+                    whitePegs++;
+                }
             }
             pos = guess.find(code[i]);
         }
