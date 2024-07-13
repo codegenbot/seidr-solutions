5abd17fe403105286aba12d917925afbe2999401
beep boop <githubbot@vadim.me>
2024-07-13 08:05:58 +0200

    Solution 59 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nxdikw2k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f83672b-81c1-44eb-bc42-98c8bf8b9 241.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -3,8 +3,8 @@ import math
 def modp(n: int, p: int) -> int:
     if math.gcd(n, p) != 1:
         return -1
-    phi_val = pow(n, 1-(p-1), p)
-    if phi_val == 1:
-        return -1
-    else:
+    phi_val = (p - sum(1 for a in range(2, int(math.sqrt(p)) + 1) if p % a == 0 and all(p % i > 0 for i in range(2, int(math.sqrt(p)) + 1) if i != a and p % i == 0)))
+    if pow(n, phi_val, p) == 1:
         return phi_val
+    else:
+        return -1
