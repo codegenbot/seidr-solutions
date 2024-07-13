2b9aab055776f133c4e5a813e67a0729078e510d
beep boop <githubbot@vadim.me>
2024-07-13 10:54:14 +0200

    Solution 70 of Python/147, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/v6hxdyk4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f84e7d2-4217-49c6-9510-c92457062 386.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_147.py
+++ Python_147.py
@@ -1,3 +1,4 @@
+```Python
 print("Enter the number up to which you want to find max triples: ", end="")
 n = int(input())
 
@@ -6,7 +7,7 @@ def get_max_triples(n):
         return None
     max_sum = 0
     for i in range(1, n+1):
-        triple_sum = sum(range(1, i*3, 2)) + sum(range(i*3+1, ((i+1)*3)//2 + 1))
+        triple_sum = sum([x for x in range(1, i*3, 2)]) + sum(range(i*3+1, ((i+1)*3)//2 + 1))
         if triple_sum > max_sum:
             max_sum = triple_sum
     return max_sum
