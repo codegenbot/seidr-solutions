628e149356d903ab48c9f21223dd0ad3f9707eb5
beep boop <githubbot@vadim.me>
2024-07-13 03:58:04 +0200

    Solution 20 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/acxuxpiz Prompt: 30d87993-8ce9-4c9f-96ab-3cf8c7d80153.cpp:8:17: error: ‘i’ does not name a type     8 | for (int i = 0; i <= cutIndex; i++) cout << nums[i] << " ";       |                 ^ 30d87993-8ce9-4c9f-96ab-3cf8c7d80153.cpp:8:32: error: ‘i’ does not name a type     8 | for (int i = 0; i <= cutIndex; i++) cout << nums[i] << " ";       |                                ^ 30d87993-8ce9-4c9f-96ab-3cf8c7d80153.cpp:9:1: error: ‘cout’ does not name a type     9 | cout << endl;       | ^~~~ 30d87993-8ce9-4c9f-96ab-3cf8c7d80153.cpp:10:1: error: expected unqualified-id be fore ‘for’    10 | for (int i = cutIndex + 1; i < n; i++) cout << nums[i] << " ";       | ^~~ 30d87993-8ce9-4c9f-96ab-3cf8c7d80153.cpp:10:28: error: ‘i’ does not name a type    10 | for (int i = cutIndex + 1; i < n; i++) cout << nums[i] << " ";       |                            ^ 30d87993-8ce9-4c9f-96ab-3cf8c7d80153.cpp:10:35: error: ‘i’ does not name a type    10 | for (int i = cutIndex + 1; i < n; i++) cout << nums[i] << " ";       |                                   ^ 30d87993-8ce9-4c9f-96ab-3cf8c7d80153.cpp:11:1: error: ‘cout’ does not name a typ e    11 | cout << endl;       | ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,19 +1,10 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-void cutVector(const vector<int>& nums) {
-    int n = nums.size();
-    int cutIndex = n / 2;
-    for (int i = 0; i <= cutIndex; i++) cout << nums[i] << " ";
-    cout << endl;
-    for (int i = cutIndex + 1; i < n; i++) cout << nums[i] << " ";
-    cout << endl;
+int n = nums.size();
+int cutIndex = n / 2;
+for (int i = 0; i <= cutIndex; i++) { 
+    cout << nums[i] << " ";
 }
-
-int main() {
-    vector<int> nums = {1, 2, 3, 4, 5};
-    cutVector(nums);
-    return 0;
+cout << endl;
+for (int i = cutIndex + 1; i < n; i++) { 
+    cout << nums[i] << " ";
 }
+cout << endl;
