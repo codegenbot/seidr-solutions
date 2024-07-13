a5c0f26ad7a529b2a6db7bc9b40b1d74f9d92716
beep boop <githubbot@vadim.me>
2024-07-13 02:45:31 +0200

    Solution 28 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/80idpyzn Prompt:       |                    ^~~ 2959f927-6735-4dff-8135-f4ef0c06ab93.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2959f927-6735-4dff-8135-f4ef0c06ab93.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2959f927-6735-4dff-8135-f4ef0c06ab93.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2959f927-6735-4dff-8135-f4ef0c06ab93.cpp:16: 2959f927-6735-4dff-8135-f4ef0c06ab93.cpp: In function ‘int main()’: 2959f927-6735-4dff-8135-f4ef0c06ab93.cpp:26:20: error: ‘strange_sort_list’ was n ot declared in this scope    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 2959f927-6735-4dff-8135-f4ef0c06ab93.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,9 +2,28 @@
 #include <algorithm>
 
 bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
+    if (a.size() != b.size()) {
+        return false;
+    }
     for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
+        if (a[i] != b[i]) {
+            return false;
+        }
     }
     return true;
+}
+
+vector<int> strange_sort_list(vector<int> lst) {
+    vector<int> result;
+    if (lst.empty()) return result;
+
+    sort(lst.begin(), lst.end());
+    while (!lst.empty()) {
+        result.push_back(*lst.begin());
+        lst.erase(lst.begin());
+        if (!lst.empty())
+            sort(lst.begin(), lst.end());
+    }
+
+    return result;
 }
