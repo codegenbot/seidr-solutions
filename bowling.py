09db5a0911135bcc598ebe3acfafb6ae402a8b6e
beep boop <githubbot@vadim.me>
2024-07-13 06:43:06 +0200

    Solution 2 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/b8nej3bk Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -4,15 +4,14 @@ def bowling_score(bowls):
     bowl_index = 0
     while frame <= 10:
         if bowls[bowl_index] == "X":
-            score += 10
-            score += get_strike_bonus(bowls, bowl_index)
+            score += 10 + get_strike_bonus(bowls, bowl_index)
             bowl_index += 1
         elif bowls[bowl_index + 1] == "/":
             score += 10
-            score += get_spare_bonus(bowls, bowl_index)
+            score += int(bowls[bowl_index + 2])
             bowl_index += 2
         else:
-            score += get_frame_score(bowls, bowl_index)
+            score += int(bowls[bowl_index]) + int(bowls[bowl_index + 1])
             bowl_index += 2
         frame += 1
     return score
@@ -27,32 +26,8 @@ def get_strike_bonus(bowls, index):
         else:
             bonus += int(bowls[index + 4])
     else:
-        bonus += get_frame_score(bowls, index + 2)
+        bonus += int(bowls[index + 2]) + int(bowls[index + 3])
     return bonus
-
-
-def get_spare_bonus(bowls, index):
-    bonus = 0
-    if bowls[index + 2] == "X":
-        bonus += 10
-    else:
-        bonus += int(bowls[index + 2])
-    return bonus
-
-
-def get_frame_score(bowls, index):
-    score = 0
-    if bowls[index] == "-":
-        score += 0
-    else:
-        score += int(bowls[index])
-    if bowls[index + 1] == "-":
-        score += 0
-    elif bowls[index + 1] == "/":
-        score += 10 - int(bowls[index])
-    else:
-        score += int(bowls[index + 1])
-    return score
 
 
 bowls = input()
