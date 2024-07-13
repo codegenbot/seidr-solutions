7399b77a7f4394ac94217d804f8997f0c28b4709
beep boop <githubbot@vadim.me>
2024-07-13 05:16:18 +0200

    Solution 19 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/qcy3ynt1 Prompt: 981a901d-697d-48da-a89f-be0a5cac7b50.cpp:1:19: error: ‘vector’ is not a member o f ‘std’     1 | int basement(std::vector<int>& nums) {       |                   ^~~~~~ 981a901d-697d-48da-a89f-be0a5cac7b50.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | int basement(std::vector<int>& nums) { 981a901d-697d-48da-a89f-be0a5cac7b50.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int basement(std::vector<int>& nums) {       |                          ^~~

--- basement.cpp
+++ basement.cpp
@@ -1,9 +1,12 @@
+#include <vector>
+
 int basement(std::vector<int>& nums) {
     int sum = 0;
     for (int i = 0; i < nums.size(); i++) {
-        sum += nums[i];
+        if(i == 0) sum = nums[i];
+        else sum += nums[i];
         if (sum < 0)
-            return i;
+            return i + 1;
     }
     return -1;
 }
