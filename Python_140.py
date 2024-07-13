1c71048f3869b75ab3b77de2d4e0531f06013552
beep boop <githubbot@vadim.me>
2024-07-13 10:19:04 +0200

    Solution 41 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/puafi84q Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4e944f9d-e614-458b-bc89-fd829dcd1 569.py", line 14, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4e944f9d-e614-458b-bc89-fd829dcd1 569.py", line 10, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,6 +1,5 @@
 def fix_spaces(text):
-    text = ' '.join(text.split())  # Replace multiple spaces with single space
-    text = text.replace(" ", "_")
+    text = text.strip().lower().replace(" ", "_")
     while "_--" in text or "_-_" in text:
         text = text.replace("_--", "_-").replace("_-_","_-")
-    return text.strip()
+    return text.strip().upper() if text.isupper() else text.capitalize()
