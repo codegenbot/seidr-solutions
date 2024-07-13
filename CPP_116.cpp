c6d1705f59d0d61c2978f60565c4bd09397a831f
beep boop <githubbot@vadim.me>
2024-07-13 07:39:38 +0200

    Solution 0 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pplh9vmh Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_116.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <algorithm>
+
+vector<int> sort_vector(vector<int> arr) {
+    vector<pair<int, int>> vec;
+    for (int i : arr) {
+        vec.push_back({__builtin_popcount(i), i});
+    }
+    sort(vec.begin(), vec.end());
+    return vector<int>(vec.begin(), vec.end());
+}
