8920fd7e3e72c733bdec0a56fb60c17c5518be91
beep boop <githubbot@vadim.me>
2024-07-13 02:17:48 +0200

    Solution 63 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/3c5j25ch Prompt:       |                    ^~~~~ 9524858c-1438-45d8-8d19-2ca9eb997960.cpp:11:29: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 9524858c-1438-45d8-8d19-2ca9eb997960.cpp:11:36: error: expected primary-expressi on before ‘float’    11 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 9524858c-1438-45d8-8d19-2ca9eb997960.cpp:11:43: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 9524858c-1438-45d8-8d19-2ca9eb997960.cpp:10: 9524858c-1438-45d8-8d19-2ca9eb997960.cpp: In function ‘int main()’: 9524858c-1438-45d8-8d19-2ca9eb997960.cpp:20:20: error: ‘get_positive’ was not de clared in this scope    20 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 9524858c-1438-45d8-8d19-2ca9eb997960.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,16 +1,11 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(float x, float y) {
-    return (x - y) < 1e-9;
-}
-
-vector<float> get_positive(vector<float> l) {
-    vector<float> result;
+std::vector<float> get_positive(std::vector<float> l) {
+    std::vector<float> result;
     for (float x : l) {
         if (x > 0)
             result.push_back(x);
     }
-    sort(result.begin(), result.end());
     return result;
 }
