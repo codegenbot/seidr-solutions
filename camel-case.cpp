54162743e1788ad4a1c45dddb081b9f3fe076a58
beep boop <githubbot@vadim.me>
2024-07-13 11:42:27 +0200

    Solution 52 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/crhzff04 Prompt: df3f4562-39e8-48aa-94ac-7cf4976a7550.cpp: In function ‘std::string camelCase(std ::string)’: df3f4562-39e8-48aa-94ac-7cf4976a7550.cpp:21:52: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘toupper’    21 |             result += s.substr(i + 1).substr(0, 1).toupper() + s.substr( i + 1).substr(1);       |                                                    ^~~~~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -6,27 +6,23 @@ using namespace std;
 
 string camelCase(string s) {
     string result = "";
-    bool capitalizeNext = true;
     for (int i = 0; i <= s.size(); i++) {
         if (i == s.size() || s[i] == '-') {
-            if (capitalizeNext) {
-                result += toupper(s.substr(i + 1, 1));
-                capitalizeNext = false;
-            } else {
-                result += s.substr(i + 1);
+            if (result.size()) {
+                result[0] = toupper(result[0]);
             }
+            string word = s.substr(i + 1);
+            result += word;
+            break;
         } else if (s[i] == ' ') {
-            capitalizeNext = true;
             if (i > 0) {
-                result += s[i];
+                result[0] = toupper(result[0]);
             }
+            result += s.substr(0, i);
+            result += std::tolower(s.substr(i + 1).substr(0, 1)) + s.substr(i + 1).substr(1);
+            break;
         } else {
-            if (capitalizeNext) {
-                result += toupper(s[i]);
-                capitalizeNext = false;
-            } else {
-                result += tolower(s[i]);
-            }
+            result += s[i];
         }
     }
     return result;
