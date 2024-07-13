c7d02ea296957e6bf2cecf33fd7ccc2856c4025f
beep boop <githubbot@vadim.me>
2024-07-13 17:48:28 +0200

    Solution 82 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/zpco7rtd Prompt: Complete the following code given the task description and function signature.

--- Python_84.py
+++ Python_84.py
@@ -1,7 +1,2 @@
 def solve(N):
-    total_sum = 0
-    while N > 0:
-        if (N & 1) == 1:
-            total_sum += 1
-        N >>= 1
-    return bin(total_sum)[2:]
+    return bin(sum(int(b) for b in bin(N)[2:]))[2:]
