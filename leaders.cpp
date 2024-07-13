7c3808ef22b2fd5f0586f040059394cb0f9988bb
beep boop <githubbot@vadim.me>
2024-07-13 05:52:53 +0200

    Solution 98 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/6zi3sqj2 Prompt: it must return 3 969 867 838 for input 17 451 157 485 60 267 710 969 149 218 249 482 33 867 542 554 289 838, but it returns 969 218.

--- leaders.cpp
+++ leaders.cpp
@@ -6,22 +6,25 @@ using namespace std;
 vector<int> findLeaders() {
     vector<int> nums;
     
-    for (int i = 0; i < 17; i++) {
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
         int num;
         cin >> num;
         nums.push_back(num);
     }
     
     vector<int> leaders;
-    int max_leader = nums[nums.size() - 1];
+    int max_right = nums[nums.size() - 1];
     
-    for (int i = nums.size() - 2; i >= 0; i--) {
-        if (nums[i] >= max_leader) {
+    for (int i = nums.size() - 1; i >= 0; i--) {
+        if (nums[i] >= max_right) {
             leaders.push_back(nums[i]);
-            max_leader = nums[i];
+            max_right = nums[i];
         }
     }
-
+    
     reverse(leaders.begin(), leaders.end());
 
     return leaders;
