7c420d55a1298f9a8e110b5fada70d376d74a666
beep boop <githubbot@vadim.me>
2024-07-13 02:53:04 +0200

    Solution 25 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fykll3yz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/691ae587-f413-493e-8ca2-a22962d3e e78.py", line 1     return [num for num in numbers if not (num in seen or seen.add(num)) and Tru e][::-1]     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ SyntaxError: 'return' outside function

--- Python_26.py
+++ Python_26.py
@@ -1,5 +1,3 @@
-from typing import List
-
-def remove_duplicates(numbers: List[int]) -> List[int]:
+def solution(numbers):
     seen = set()
-    return [num for num in numbers if not (num in seen or seen.add({num}))][::-1]
+    return [num for num in numbers if not (num in seen or seen.add(num)) and True][::-1]
