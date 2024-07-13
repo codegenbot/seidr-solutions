047f7d2d2e2af96e6a293dbd311f4edbe0a528f3
beep boop <githubbot@vadim.me>
2024-07-14 01:05:01 +0200

    Solution 89 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/gab97s0c Prompt: it must return 91 for input 728/5141410/9-7--772, but it returns .

--- bowling.py
+++ bowling.py
@@ -1,32 +1,55 @@
-```
 def bowling_score(frames):
     score = 0
     index = 0
     while index < len(frames):
         if frames[index] == 'X':
-            score += 30
-            index += 1
+            score += 10
+            if index + 1 >= len(frames) or frames[index + 1] in ['/']:
+                continue
+            score += int(frames[index + 1].replace('/'))
+            index += 2
         elif frames[index] in ['/']:
-            left_pins = int(frames[index - 1]) if frames[index - 1].isdigit() else 10
-            right_pins = int(frames[index][1]) if frames[index][1].isdigit() else 10
-            frame_score = left_pins + right_pins
-            if left_pins == 10:
-                score += 10 + (30 - left_pins)
-            elif right_pins == 10:
-                score += 10 + left_pins
-            index += 2
-        else:
-            left_pin = int(frames[index][0]) if frames[index][0].isdigit() else 10
-            right_pin = int(frames[index][1]) if frames[index][1].isdigit() else 10
-            frame_score = left_pin + right_pin
-            if left_pin == 'X':
-                if right_pin in ['X', '/']:
-                    score += 30 - left_pin
+            left_pins = int(frames[index - 1][0])
+            right_pins = int(frames[index][1])
+            if left_pins == 'X':
+                if right_pins == '/':
+                    score += 10 + 10
                 else:
-                    score += 10 + (30 - left_pin)
-            elif right_pin == 'X':
-                score -= left_pin
-            elif right_pin == '/':
-                score += left_pin + 10
-            index += 2
+                    score += 10 + (int(right_pins) + int(frames[index + 1].replace('/')))
+                index += 2
+            elif left_pins == 0:
+                frame_score = right_pins
+                if index + 1 < len(frames) and frames[index + 1] in ['/']:
+                    frame_score += int(frames[index + 1].replace('/'))
+                score += frame_score
+                index += 2
+            else:
+                frame_score = left_pins + right_pins
+                score += frame_score
+                index += 2
+        else:
+            left_pin = int(frames[index][0])
+            right_pin = int(frames[index][1])
+            if left_pin == 'X':
+                if right_pin == 'X':
+                    score += 10 + 10
+                elif right_pin in ['/']:
+                    score += 10 + (int(right_pin) + int(frames[index + 1].replace('/')))
+                else:
+                    score += 10 + right_pin
+                index += 2
+            elif left_pin == 0:
+                if right_pin in ['/']:
+                    frame_score = right_pin
+                    if index + 1 < len(frames) and frames[index + 1] in ['/']:
+                        frame_score += int(frames[index + 1].replace('/'))
+                    score += frame_score
+                else:
+                    frame_score = right_pin
+                    score += frame_score
+                index += 2
+            else:
+                frame_score = left_pin + right_pin
+                score += frame_score
+                index += 2
     return score
