cd65d9969fc7c5e9ccf739d6b71e43499504e148
beep boop <githubbot@vadim.me>
2024-07-13 11:42:00 +0200

    Solution 5 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/4rv4b06w Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b1669763-3689-45f9-9726-85f5320e3 7b5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,4 +1,3 @@
-```
 def solve_boolean(expression):
     if expression == 'T':
         return True
@@ -6,17 +5,10 @@ def solve_boolean(expression):
         return False
     ops = {'&': lambda x, y: x and y, '|': lambda x, y: x or y}
     stack = []
-    nesting_level = 0
     for char in expression:
-        if char == '(':
-            nesting_level += 1
-        elif char == ')':
-            nesting_level -= 1
-        elif nesting_level > 0:
-            continue
-        elif char in ops:
+        if char in ops:
             b = ops[char](stack.pop(), stack.pop())
             stack.append(b)
         else:
-            stack.append(char == 'T')
+            stack.append(True if char == 'T' else False)
     return stack[0]
