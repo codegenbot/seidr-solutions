6a09868ff897af4a1f96325732d61d67efa2a3bc
beep boop <githubbot@vadim.me>
2024-07-13 16:03:07 +0200

    Solution 10 of Python/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/mrtp2hgu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c376f61d-5aac-4573-9042-4bca0050f 4bb.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_137.py
+++ Python_137.py
@@ -1,13 +1,17 @@
 ```
 def compare_one(a, b):
     if isinstance(a, str) and isinstance(b, (int, float)):
-        return a if a.replace(',', '.') > str(b).replace(',', '.'): b
+        return a
     elif isinstance(b, str) and isinstance(a, (int, float)):
-        return b if b.replace(',', '.') > str(a).replace(',', '.'): a
-    else:
-        if a > b:
-            return a
-        elif a < b:
-            return b
-        else:
+        return b
+    elif isinstance(a, str) and isinstance(b, str):
+        if '.' in a or ',' in a:
+            a = float(a.replace(',', '.'))
+        if '.' in b or ',' in b:
+            b = float(b.replace(',', '.'))
+        if a == b:
             return None
+        else:
+            return a if a > b else b
+    elif isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a if a > b else b
