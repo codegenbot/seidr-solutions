88944c28a8322b179cab15f3aece2d5242296825
beep boop <githubbot@vadim.me>
2024-07-13 07:04:33 +0200

    Solution 90 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/hv1pyxj2 Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/925b0e15-6ce3-4742-8f2a-ce7d04292 5f6.py", line 20, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/925b0e15-6ce3-4742-8f2a-ce7d04292 5f6.py", line 17, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,4 +1,3 @@
-```
 def fizz_buzz(n: int):
     count = 0
     for i in range(1, n + 1):
@@ -6,9 +5,9 @@ def fizz_buzz(n: int):
         if i % 15 == 0:
             output = "FizzBuzz"
             count += 1
-        elif i % 3 == 0 and not i % 5 == 0:
+        elif i % 3 == 0 and i % 5 != 0:
             output = "Fizz"
-        elif i % 5 == 0 and not i % 3 == 0:
+        elif i % 5 == 0 and i % 3 != 0:
             output = "Buzz"
         else:
             output = str(i)
