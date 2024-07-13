497bfb8354c3f5e71c139fa7038455b5487a8782
beep boop <githubbot@vadim.me>
2024-07-13 11:58:53 +0200

    Solution 86 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/acp5mi4l Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,30 +1,35 @@
-int scoreOfBowling(string s) {
+int main() {
+    string bowls;
+    cin >> bowls;
+    
     int score = 0;
     int frame = 0;
-    for (int i = 0; i < s.size(); ++i) {
-        if (frame == 10) break;
-        if (s[i] == 'X') {
+    for (int i = 0; i < bowls.size() && frame < 10; i++) {
+        if (bowls[i] == 'X') {
             score += 10;
-            if (s[i+2] == 'X') score += 10;
-            else if (s[i+2] == '/') score += 10 - s[i+1] + '0';
-            else score += s[i+1] - '0' + s[i+2] - '0';
-            ++frame;
-        } else if (s[i] == '/') {
-            score += 10 - s[i-1] + '0';
-        } else if (s[i] == '-') {
-            // do nothing
+            if (bowls[i + 2] == 'X') {
+                score += 10;
+            } else {
+                score += (bowls[i + 1] == 'X' ? 10 : (int)(bowls[i + 1] - '0'));
+                score += (bowls[i + 2] == '/' ? 10 - (int)(bowls[i + 1] - '0') : (int)(bowls[i + 2] - '0'));
+            }
+            frame++;
+        } else if (bowls[i] == '/') {
+            score += 10 - (int)(bowls[i - 1] - '0');
+            score += (bowls[i + 1] == 'X' ? 10 : (int)(bowls[i + 1] - '0'));
+            frame++;
         } else {
-            score += s[i] - '0';
-            if (s[i+1] == '/') score += 10 - s[i] + '0';
-            ++i;
+            score += (int)(bowls[i] - '0');
+            if (bowls[i] == '-') {
+                continue;
+            }
+            if (i + 1 < bowls.size() && bowls[i + 1] == '/') {
+                score += 10 - (int)(bowls[i] - '0');
+            }
         }
     }
-    return score;
-}
-
-int main() {
-    string s;
-    cin >> s;
-    cout << scoreOfBowling(s) << endl;
+    
+    cout << score << endl;
+    
     return 0;
 }
