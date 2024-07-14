380f3e8e104e56e956303003642b0932a3a0fd82
beep boop <githubbot@vadim.me>
2024-07-14 16:24:07 +0200

    Solution 12 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/oze1uxoq Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,74 +1,33 @@
+#include <vector>
 #include <iostream>
 #include <string>
 
-std::string spinWords(std::string str) {
-    std::string result = "";
-    std::string word;
-    
-    for (int i = 0; i < str.length(); i++) {
+using namespace std;
+
+string spinWords(string str) {
+    string result = "";
+    int i = 0;
+    while (i < str.length()) {
         if (str[i] == ' ') {
-            result += word + " ";
-            word = "";
+            result += " ";
+            i++;
         } else {
-            word += str[i];
-        }
-    }
-    
-    result += word;
-    
-    std::string newResult = "";
-    int j = 0;
-    
-    while (j < result.length()) {
-        if (result[j] == ' ') {
-            for (int i = j + 1; i <= result.find(' ', j); i++) {
-                newResult += result[i];
-            }
-            newResult += " ";
-            j = result.find(' ', j) + 1;
-        } else if (result.length() - j > 4) {
-            for (int i = j; i < result.find(' ', j); i++) {
-                newResult += result[i];
-            }
-            newResult += " ";
-            j = result.find(' ', j);
-        } else {
-            while (j < result.length() && result[j] != ' ') {
-                newResult += result[j];
+            int j = i;
+            while (j < str.length() && str[j] != ' ') {
                 j++;
             }
-            newResult += " ";
-            j++;
+            string word = str.substr(i, j - i);
+            if (word.length() >= 5) {
+                reverse(word.begin(), word.end());
+            }
+            result += word + " ";
+            i = j;
         }
     }
-    
-    for (int i = 0; i < newResult.length(); i++) {
-        if (newResult[i] == ' ') {
-            for (int j = i + 1; j <= newResult.find(' ', i); j++) {
-                newResult[j - i - 1] = newResult[newResult.size() - j];
-            }
-            i = newResult.find(' ', i);
-        } else if (newResult.length() - i > 4) {
-            for (int j = i; j < newResult.find(' ', i); j++) {
-                newResult[j - i] = newResult[newResult.size() - j - 1];
-            }
-            i = newResult.find(' ', i);
-        } else {
-            while (i < newResult.length() && newResult[i] != ' ') {
-                newResult[i - i] = newResult[newResult.size() - i - 1];
-                i++;
-            }
-            i++;
-        }
-    }
-    
-    return newResult;
+    return result;
 }
 
 int main() {
-    std::cout << spinWords("a") << std::endl; // a
-    std::cout << spinWords("this is a test") << std::endl; // this is a test
-    std::cout << spinWords("this is another test") << std::endl; // this is rehtona test
-    std::cout << spinWords("hi") << std::endl; // hi
+    // Your code here
     return 0;
 }
