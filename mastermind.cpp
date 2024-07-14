295fbf3f652922f6f9acdc691de49d9c087815e8
beep boop <githubbot@vadim.me>
2024-07-14 19:26:29 +0200

    Solution 71 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/raeono0a Prompt: Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,28 +1,39 @@
-int numMatchingChars(string code, string guess) {
-    int white = 0;
-    int black = 0;
+int main() {
+    string code, guess;
+    cin >> code >> guess;
 
+    int blackPegs = 0;
+    int whitePegs = 0;
+
+    map<char, int> codeCount, guessCount;
+
+    // Count the characters in the code and guess
     for (char c : code) {
-        if (c == guess[0]) {
-            if (code.find(guess[1..3].data()) != string::npos) {
-                black++;
-                break;
-            }
-            white++;
-        } else if (guess.find(c) != string::npos) {
-            int pos = 0;
-            for (char gc : guess) {
-                if (gc == c) {
-                    pos++;
-                }
-            }
-            if (pos > 1) {
-                white += pos - 1;
-            } else if (pos == 1) {
-                black++;
-            }
+        codeCount[c]++;
+    }
+    for (char c : guess) {
+        guessCount[c]++;
+    }
+
+    // Count the black pegs (correct color, correct place)
+    for (int i = 0; i < 4; i++) {
+        if (code[i] == guess[i]) {
+            codeCount[code[i]]--;
+            guessCount[guess[i]]--;
+            blackPegs++;
         }
     }
 
-    return {white, black}[0];
+    // Count the white pegs (correct color, wrong place)
+    for (char c : code) {
+        if (guessCount[c] > 0) {
+            whitePegs++;
+            guessCount[c]--;
+        }
+    }
+
+    cout << whitePegs << endl;
+    cout << blackPegs << endl;
+
+    return 0;
 }
