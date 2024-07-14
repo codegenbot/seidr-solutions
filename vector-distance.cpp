771b67abfafefc7a04e710349aa5b2e3fed56d4f
beep boop <githubbot@vadim.me>
2024-07-14 14:49:24 +0200

    Solution 57 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/lnggliix Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,13 +1,20 @@
+```cpp
 #include <iostream>
 #include <cmath>
+#include <sstream>
 
 double vectorDistance() {
     int n;
     std::cin >> n; 
     double sumOfSquares = 0;
     for (int i = 0; i < n; i++) {
+        std::string x1Str, x2Str;
+        std::cin >> x1Str >> x2Str; 
+
         double x1, x2;
-        std::cin >> x1 >> x2; 
+        std::stringstream ss1(x1Str), ss2(x2Str);
+        ss1 >> x1; ss2 >> x2;
+
         double differenceSquared = pow(x2 - x1, 2);
         sumOfSquares += differenceSquared;
     }
