7c861f0ba150e16057aaab449722a8219f99f345
beep boop <githubbot@vadim.me>
2024-07-14 20:02:31 +0200

    Solution 20 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/sbpdvwbn Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,26 +1,37 @@
+#include <vector>
+#include <iostream>
 #include <string>
-using namespace std;
 
-string spinWords(string str) {
-    string result = "";
-    int wordLength;
-    string word;
-    
-    for (int i = 0; i <= str.length(); i++) {
-        if (i == str.length() || str[i] == ' ') {
-            wordLength = word.length();
-            if (wordLength >= 5) {
-                for (int j = wordLength - 1; j >= 0; j--) {
-                    result += word[j];
-                }
-            } else {
-                result += word;
-            }
-            word = "";
+std::string spinWords(std::string str) {
+    std::string result = "";
+    std::size_t pos = 0;
+    while ((pos = str.find(" ")) != std::string::npos) {
+        std::size_t len = pos;
+        if (len >= 5) {
+            std::string word = str.substr(0, pos);
+            for (int i = word.length() - 1; i >= 0; --i)
+                result += word[i];
+            str.erase(0, pos + 1);
         } else {
-            word += str[i];
+            result += str.substr(0, pos + 1);
+            str.erase(0, pos + 1);
         }
     }
-    
+    if (str.length() > 4) {
+        std::string word = str;
+        for (int i = word.length() - 1; i >= 0; --i)
+            result += word[i];
+    } else
+        result += str;
     return result;
+}
+
+int main() {
+    // Test cases
+    cout << spinWords("a") << endl;  // a
+    cout << spinWords("this is a test") << endl;  // this is a test
+    cout << spinWords("this is another test") << endl;  // this is rehtona test
+    cout << spinWords("hi") << endl;  // hi
+
+    return 0;
 }
