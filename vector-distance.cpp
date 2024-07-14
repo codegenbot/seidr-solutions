fc85d76899223b15b260d5cf77ca16870f3eecfe
beep boop <githubbot@vadim.me>
2024-07-14 12:20:55 +0200

    Solution 37 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/inb7mtgb Prompt: 1050ead9-22e5-491c-a6ea-74f67c803c21.cpp: In function ‘double vectorDistance(int )’: 1050ead9-22e5-491c-a6ea-74f67c803c21.cpp:11:27: error: ‘x2’ was not declared in this scope    11 |         double diff = pow(x2[i] - x1[i], 2);       |                           ^~ 1050ead9-22e5-491c-a6ea-74f67c803c21.cpp:11:35: error: ‘x1’ was not declared in this scope; did you mean ‘y1’?    11 |         double diff = pow(x2[i] - x1[i], 2);       |                                   ^~       |                                   y1

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -5,18 +5,12 @@
 
 using namespace std;
 
-double vectorDistance(int n) {
-    double* x1 = new double[n], *x2 = new double[n];
+double vectorDistance(int n, const vector<float>& x1, const vector<float>& x2) {
     double sum = 0;
-    
     for (int i = 0; i < n; i++) {
-        cin >> x1[i] >> x2[i];
-        double diff = x2[i] - x1[i];
-        sum += pow(diff, 2);
+        double diff = pow(x2[i] - x1[i], 2);
+        sum += diff;
     }
-    
-    delete[] x1;
-    delete[] x2;
     
     return sqrt(sum);
 }
@@ -25,11 +19,11 @@ int main() {
     int n;
     cin >> n;
 
-    vector<double> x1(n), x2(n);
+    vector<float> x1(n), x2(n);
 
     for (int i = 0; i < n; i++) {
         cin >> x1[i] >> x2[i];
     }
 
-    cout << fixed << setprecision(10) << vectorDistance(n) << endl;
+    cout << fixed << setprecision(10) << vectorDistance(n, x1, x2) << endl;
 }
