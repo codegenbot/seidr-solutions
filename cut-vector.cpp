5e275dc0f898d444899bf58804d07bc4e65e4876
beep boop <githubbot@vadim.me>
2024-07-14 12:21:56 +0200

    Solution 38 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/n62afbof Prompt: 518c41d9-79df-49bd-bf3a-620801620b22.cpp: In function ‘std::vector<std::vector<i nt> > cutVector(std::vector<int>)’: 518c41d9-79df-49bd-bf3a-620801620b22.cpp:26:19: error: ‘INT_MAX’ was not declare d in this scope    26 |     int minDiff = INT_MAX;       |                   ^~~~~~~ 518c41d9-79df-49bd-bf3a-620801620b22.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; 518c41d9-79df-49bd-bf3a-620801620b22.cpp:37:13: error: ‘abs’ was not declared in  this scope    37 |         if (abs(sumLeft - sumRight) < minDiff) {       |             ^~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,53 +1,8 @@
+```c++
 #include <vector>
 #include <climits>
 #include <cmath>
 using namespace std;
-
-vector<vector<int>> cutVector(vector<int> v) {
-    int n = v.size();
-    vector<vector<int>> res(2);
-    
-    for (int i = 1; i <= n / 2; i++) {
-        int sumLeft = 0, sumRight = 0;
-        for (int j = 0; j < i; j++) {
-            sumLeft += v[j];
-        }
-        for (int j = i; j < n; j++) {
-            sumRight += v[j];
-        }
-        
-        if (sumLeft == sumRight) {
-            res[0] = vector<int>(i);
-            res[1] = vector<int>(n - i);
-            copy(v.begin(), v.begin() + i, res[0].begin());
-            copy(v.begin() + i, v.end(), res[1].begin());
-            return res;
-        }
-    }
-    
-    int minDiff = INT_MAX;
-    int pos = 0;
-    for (int i = 1; i <= n / 2; i++) {
-        int sumLeft = 0, sumRight = 0;
-        for (int j = 0; j < i; j++) {
-            sumLeft += v[j];
-        }
-        for (int j = i; j < n; j++) {
-            sumRight += v[j];
-        }
-        
-        if (abs(sumLeft - sumRight) < minDiff) {
-            minDiff = abs(sumLeft - sumRight);
-            pos = i;
-        }
-    }
-    
-    res[0] = vector<int>(pos);
-    res[1] = vector<int>(n - pos);
-    copy(v.begin(), v.begin() + pos, res[0].begin());
-    copy(v.begin() + pos, v.end(), res[1].begin());
-    return res;
-}
 
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
