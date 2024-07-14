b660d60cc3725d7fe3e746ed04ec8077d9b3c1db
beep boop <githubbot@vadim.me>
2024-07-14 02:47:22 +0200

    Solution 74 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/xtxh99pu Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -9,10 +9,7 @@ int main() {
     int firstBounce = static_cast<int>(startHeight) / 2; 
     double bouncinessIndex = (double)firstBounce / startHeight;
 
-    if (bouncinessIndex < 0.01)
-        cout << "The bounciness index is: 0.00" << endl;
-    else
-        cout << "The bounciness index is: " << fixed << setprecision(2) << std::to_string(bouncinessIndex) << endl;
+    cout << "The bounciness index is: " << fixed << setprecision(2) << (bouncinessIndex < 0.01 ? 0 : bouncinessIndex) << endl;
     
     double height = startHeight; 
     double totalDistance = 0.0; 
@@ -25,10 +22,7 @@ int main() {
         totalDistance += startHeight - height;
     }
 
-    if (totalDistance * bouncinessIndex < 0.01)
-        cout << "The total distance the ball travels is: 0.00" << endl;
-    else
-        cout << "The total distance the ball travels is: " << fixed << setprecision(2) << std::to_string(totalDistance * bouncinessIndex) << endl;
+    cout << "The total distance the ball travels is: " << fixed << setprecision(2) << (totalDistance * bouncinessIndex < 0.01 ? 0 : totalDistance * bouncinessIndex) << endl;
 
     return 0;
 }
