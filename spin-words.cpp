1af957d5915bd704cf52889b4d5da23dbff3368e
beep boop <githubbot@vadim.me>
2024-07-14 22:04:54 +0200

    Solution 88 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/sbpdvwbn Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,38 +1,30 @@
-#include <iostream>
 #include <string>
+using namespace std;
 
-std::string spinWords(const std::string& str) {
-    std::string result;
+string spinWords(string str) {
+    string result = "";
     size_t pos = 0;
-    
-    while ((pos = str.find(' ')) != std::string::npos) {
-        size_t len = pos + 1;
-        
-        if (len <= 4 || len > str.length()) break;
-        
-        result += str.substr(pos, len - pos) + " ";
-        str.erase(0, len);
+    while ((pos = str.find(" ")) != string::npos) {
+        size_t wordLength = str.substr(0, pos).length();
+        if (wordLength >= 5) {
+            for (size_t i = wordLength; i > 0; --i)
+                result += str[pos - i].c_str();
+        } else
+            result += str.substr(0, pos);
+        str.erase(0, pos + 1);
     }
-    
-    // Reverse the remaining string
-    for (size_t i = 0; i < str.length(); i++) {
-        result += str[str.length() - i - 1];
-    }
-    
+    if (str.length() >= 5) {
+        for (size_t i = str.length(); i > 0; --i)
+            result += str[str.length() - i].c_str();
+    } else
+        result += str;
     return result;
 }
 
-int main() {
-    std::string input;
-    
-    while (true) {
-        std::cout << "Enter a string of one or more words (or 'q' to quit): ";
-        std::cin >> input;
-        
-        if (input == "q") break;
-        
-        std::cout << spinWords(input) << std::endl;
+int main(){
+    string input;
+    while(getline(cin, input)){
+        cout << spinWords(input) << endl;
     }
-    
     return 0;
 }
