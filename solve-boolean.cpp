303960aee3b5c8fad2365b3a006caa531f3741a6
beep boop <githubbot@vadim.me>
2024-07-14 17:11:28 +0200

    Solution 72 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/v751sb59 Prompt: this scope    16 |     cin >> s;       |     ^~~ 35415d66-d498-4d34-9fca-f9466787cc45.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; 35415d66-d498-4d34-9fca-f9466787cc45.cpp:17:5: error: ‘cout’ was not declared in  this scope    17 |     cout << (solveBoolean(s) ? "True" : "False") << endl;       |     ^~~~ 35415d66-d498-4d34-9fca-f9466787cc45.cpp:17:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 35415d66-d498-4d34-9fca-f9466787cc45.cpp:17:53: error: ‘endl’ was not declared i n this scope    17 |     cout << (solveBoolean(s) ? "True" : "False") << endl;       |                                                     ^~~~ 35415d66-d498-4d34-9fca-f9466787cc45.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -6,7 +6,7 @@ using namespace std;
 bool solveBoolean(string s) {
     bool res = true;
     for (char c : s) {
-        if (c == 'f') res = false;
+        if (c == 'f' || c == 'F') res = false;
         else if (c == '&') return res;
         else if (c == '|') return !res;
     }
