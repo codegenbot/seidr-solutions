0ea2824bf148fef449f2c53c2e424ad3c11990c8
beep boop <githubbot@vadim.me>
2024-07-14 12:34:02 +0200

    Solution 82 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/66afpjtn Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: b288ec75-d6ff-4f80-ab90-b680553b93c5.cpp:39:86:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -28,27 +28,15 @@ int main() {
     std::string cipher_map1, cipher_map2, message;
 
     std::cout << "Enter the first map: ";
-    std::string temp;
-    std::getline(std::cin, temp);
-    cipher_map1 = "";
-    for(char c : temp) {
-        if(c != ' ') {
-            cipher_map1 += c;
-        }
-    }
+    std::getline(std::cin, cipher_map1);
 
     std::cout << "Enter the second map: ";
-    std::string temp2;
-    std::getline(std::cin, temp2);
-    cipher_map2 = "";
-    for(char c : temp2) {
-        if(c != ' ') {
-            cipher_map2 += c;
-        }
-    }
+    std::getline(std::cin, cipher_map2);
 
     std::cout << "Enter the message to decipher: ";
     std::getline(std::cin, message);
 
-    std::cout << "Deciphered message: " << decipher(cipher_map1, cipher_map2, message) << std::endl;
+    std::cout << "Deciphered message: ";
+    std::cout.write(decipher(cipher_map1, cipher_map2, message).c_str(), decipher(cipher_map1, cipher_map2, message).size());
+    std::cout.put('\n');
 }
