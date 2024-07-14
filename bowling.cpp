0ea09647dd4bfd49790a4c9949d9d1e32098f879
beep boop <githubbot@vadim.me>
2024-07-14 14:50:41 +0200

    Solution 76 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/cpmapcms Prompt: d9abfb61-9d5a-4a2a-92b6-13b27684f55d.cpp:1:24: error: ‘string’ does not name a t ype     1 | int bowlingScore(const string& frames) {       |                        ^~~~~~ d9abfb61-9d5a-4a2a-92b6-13b27684f55d.cpp: In function ‘int bowlingScore(const in t&)’: d9abfb61-9d5a-4a2a-92b6-13b27684f55d.cpp:4:19: error: ‘begin’ was not declared i n this scope     4 |     for (char c : frames) {       |                   ^~~~~~ d9abfb61-9d5a-4a2a-92b6-13b27684f55d.cpp:4:19: error: ‘end’ was not declared in this scope d9abfb61-9d5a-4a2a-92b6-13b27684f55d.cpp:16:21: error: ‘c1’ was not declared in this scope; did you mean ‘c’?    16 |                 if (c1 > 0 && c2 > 0) {       |                     ^~       |                     c d9abfb61-9d5a-4a2a-92b6-13b27684f55d.cpp:16:31: error: ‘c2’ was not declared in this scope; did you mean ‘c’?    16 |                 if (c1 > 0 && c2 > 0) {       |                               ^~       |                               c

--- bowling.cpp
+++ bowling.cpp
@@ -6,8 +6,6 @@ using namespace std;
 int bowlingScore(const string& frames) {
     int score = 0;
     int currentRolls = 0;
-    int c1 = 0;
-    int c2 = 0;
     for (char c : frames) {
         if (c == 'X') {
             score += 30;
@@ -20,15 +18,9 @@ int bowlingScore(const string& frames) {
             score += roll;
             currentRolls++;
             if (currentRolls == 2) {
-                if (c1 > 0 && c2 > 0) {
-                    score += c1 + c2;
-                }
-                c1 = 0;
-                c2 = 0;
-            } else if (currentRolls == 1) {
-                c1 = roll;
-            } else {
-                c2 = roll;
+                int c1 = roll, c2 = c - '0';
+                score += c1 + c2;
+                currentRolls = 0;
             }
         }
     }
