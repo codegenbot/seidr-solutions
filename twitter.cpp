634d175a364113821e22f30f0cb0360c1181f89d
beep boop <githubbot@vadim.me>
2024-07-14 12:16:35 +0200

    Solution 3 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/z8czxzqc Prompt: it must return Too many characters for input =/aLi>VT/%c9ik5~Mc/xW)8[T?NC].e%T_"e(9SPS6@i_o!iL&$i0N&Qu2W.,jlCqAp7@5J2.?U+j~LI#!i$_nmb#.#)qiP_8;u+{v[*XL%zZ;u`[QxctC@h*6r7{3@?7T2N8]ScK`?.ee6"zfPb6gy,Q?3#rem_E*>~qM`bvA1eD0tUY"JO@@U>_gbG,Z[p", but it returns Enter a tweet: Too many characters.

--- twitter.cpp
+++ twitter.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <string>
-#include <iterator>
 
 using namespace std;
 
-string validateTweet(wstring tweet) {
+string validateTweet(string tweet) {
     if (tweet.empty()) {
         return "You didn't type anything";
     }
-    if (std::distance(tweet.begin(), tweet.end()) > 140) {
+    if (tweet.length() + 1 >= 141) {
         return "Too many characters";
     }
-    return "Your tweet has " + to_string(std::distance(tweet.begin(), tweet.end())) + " characters";
+    return "Your tweet has " + to_string(tweet.size()) + " characters";
 }
 
 int main() {
-    wstring tweet;
+    string tweet;
     cout << "Enter a tweet: ";
-    getline(wcin, tweet);
+    getline(cin, tweet);
     cout << validateTweet(tweet) << endl;
     return 0;
 }
