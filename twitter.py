09ebc83d4457080ef27528e032c30e89d1ffe099
beep boop <githubbot@vadim.me>
2024-07-14 12:31:52 +0200

    Solution 9 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/b53cmyep Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8a8bdf1b-5890-451d-a22a-596732394 e40.py", line 8     return f"Your tweet has {len(re.sub(r'\W+', '', tweet))} characters"                                                                         ^ SyntaxError: f-string expression part cannot include a backslash

--- twitter.py
+++ twitter.py
@@ -1,7 +1,8 @@
+import re
 def validate_tweet(tweet):
-    if sum(c.isalnum() for c in tweet) > 140:
+    if len(re.sub(r'\W+', '', tweet)) > 140:
         return "Too many characters"
     elif tweet == "":
         return "You didn't type anything"
     else:
-        return f"Your tweet has {sum(c.isalnum() for c in tweet)} characters"
+        return f"Your tweet has {len(tweet)} characters"
