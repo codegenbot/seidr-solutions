314d60d171a442b4ab421bf8ecd115041eb9db96
beep boop <githubbot@vadim.me>
2024-07-14 14:17:19 +0200

    Solution 9 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/janq6p5d Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,21 +1,17 @@
 ```
-def bowling_score(rolls):
+def bowling_score(frames):
     score = 0
-    roll_index = 0
-    for frame in range(10):
-        if rolls[roll_index] == 'X':
-            score += 30
-            roll_index += 1
-        elif rolls[roll_index:roll_index+2].count('/'):
-            first_roll, second_roll = map(int, rolls[roll_index:roll_index+2].replace('/', '').split(' '))
-            score += first_roll + second_roll
-            roll_index += 2
+    frame = 0
+    for i in range(len(frames)):
+        if frames[i].isdigit():
+            if int(frames[i]) == 10:
+                score += 10 + (10 if i+1 < len(frames) and frames[i+1].isdigit() else 0)
+                frame += 1
+            elif int(frames[i]) < 10:
+                score += int(frames[i])
+                frame += 1
         else:
-            first_roll, second_roll = map(int, rolls[roll_index:roll_index+2].split(' '))
-            if first_roll == 10:
-                score += first_roll + second_roll
-                roll_index += 2
-            else:
-                score += first_roll
-                roll_index += 1
+            if int(frames[:i].count('/')) == 2:
+                score += (10 - int(frames[i-1:i].replace('/', ''))) + int(frames[i+1:].replace('/', ''))
+                frame += 1
     return score
