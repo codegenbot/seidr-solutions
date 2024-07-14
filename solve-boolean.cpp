baf2fd3663b131d36946d3883c14ad26b51fea03
beep boop <githubbot@vadim.me>
2024-07-14 16:31:17 +0200

    Solution 6 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/v751sb59 Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,30 +1,29 @@
 bool solveBoolean(string s) {
-    bool res = true;
-    for(int i=0; i<s.length(); i++){
-        if(s[i] == 'T') continue;
-        else if(s[i] == 'F') return false;
-        else if(s[i] == '|'){
-            res = !res;
-            while(i+1 < s.length() && s[i+1] != '&') i++;
-            i--;
-        }
-        else if(s[i] == '&'){
-            bool temp = true;
-            while(i+1 < s.length() && s[i+1] != '|') i++;
-            i--;
-            for(int j=i; j>=0; j--){
-                if(s[j] == 'T') continue;
-                else if(s[j] == 'F') return false;
-                else if(s[j] == '&'){
-                    temp = !temp;
-                    while(j+1 < s.length() && s[j+1] != '|') j++;
-                    j--;
+    bool result = true;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'f') {
+            result = false;
+            break;
+        } else if (s[i] == '|') {
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s[j] == 'f') {
+                    return false;
                 }
-                else break;
             }
-            i++;
-            res &= temp;
+        } else if (s[i] == '&') {
+            bool temp = true;
+            int k = i + 1;
+            while (k < s.length() && s[k] != '&') {
+                k++;
+            }
+            for (int j = i; j < k; j++) {
+                if (s[j] == 'f') {
+                    temp = false;
+                    break;
+                }
+            }
+            result &= temp;
         }
     }
-    return res;
+    return result;
 }
