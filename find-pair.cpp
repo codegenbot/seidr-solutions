2f1c1b06dd2a719562ec6ab96ec7840cde5cc6c4
beep boop <githubbot@vadim.me>
2024-07-14 02:12:53 +0200

    Solution 87 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/9xhxvl3d Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from e35877f9-467c-4c77-b6b0-7870ae04d4ed.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- find-pair.cpp
+++ find-pair.cpp
@@ -4,13 +4,13 @@
 
 std::pair<int, int> findPair(std::vector<int> nums, int target) {
     std::sort(nums.begin(), nums.end());
-    size_t left = 0;
-    size_t right = nums.size() - 1;
+    std::size_t left = 0;
+    std::size_t right = nums.size() - 1;
 
     while (left < right) {
         int sum = nums[left] + nums[right];
         if (sum == target) {
-            return std::make_pair(nums[left], nums[right]);
+            return {nums[left], nums[right]};
         } else if (sum < target) {
             left++;
         } else {
