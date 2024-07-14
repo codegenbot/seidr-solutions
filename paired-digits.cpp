2351c91d42155f4c867415fdbb41d68651aac7a9
beep boop <githubbot@vadim.me>
2024-07-14 08:35:12 +0200

    Solution 40 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/4by0ttu8 Prompt: 81113937-dbd3-43cb-9ee5-1c14a935b967.cpp:3:27: error: request for member ‘length ’ in ‘s’, which is of non-class type ‘const int’     3 |     for (int i = 0; i < s.length() - 1; i++) {       |                           ^~~~~~ 81113937-dbd3-43cb-9ee5-1c14a935b967.cpp:4:15: error: invalid types ‘const int[i nt]’ for array subscript     4 |         if ((s[i] - '0') == (s[i + 1] - '0')) {       |               ^ 81113937-dbd3-43cb-9ee5-1c14a935b967.cpp:4:31: error: invalid types ‘const int[i nt]’ for array subscript     4 |         if ((s[i] - '0') == (s[i + 1] - '0')) {       |                               ^ 81113937-dbd3-43cb-9ee5-1c14a935b967.cpp:5:22: error: invalid types ‘const int[i nt]’ for array subscript     5 |             sum += (s[i] - '0') * 2;       |                      ^ 81113937-dbd3-43cb-9ee5-1c14a935b967.cpp:8:16: error: expected ‘}’ at end of inp ut     8 |     return sum;       |                ^ 81113937-dbd3-43cb-9ee5-1c14a935b967.cpp:1:40: note: to match this ‘{’     1 | int pairedDigits(const std::string& s) {       |                                        ^

--- paired-digits.cpp
+++ paired-digits.cpp
@@ -1,12 +1,8 @@
 int pairedDigits(const std::string& s) {
-    int num = 0;
-    for (char c : s) {
-        num = num * 10 + (c - '0');
-    }
     int sum = 0;
-    for (int i = 0; i < to_string(num).length() - 1; i++) {
-        if ((to_string(num)[i] - '0') == (to_string(num)[i + 1] - '0')) {
-            sum += (to_string(num)[i] - '0') * 2;
+    for (int i = 0; i < s.size() - 1; i++) {
+        if (std::stoi(s.substr(i, 1)) == std::stoi(s.substr(i + 1, 1))) {
+            sum += 2 * std::stoi(s.substr(i, 1));
         }
     }
     return sum;
