af667bb1be0ad82cfe4812b58c1833f0d46182a0
beep boop <githubbot@vadim.me>
2024-07-14 14:11:54 +0200

    Solution 29 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/z8czxzqc Prompt: e87137cc-7567-43e7-bb97-e020b41e8bd9.cpp:1:1: error: stray ‘`’ in program     1 | ```std::string validateTweet(std::string tweet) {       | ^ e87137cc-7567-43e7-bb97-e020b41e8bd9.cpp:1:2: error: stray ‘`’ in program     1 | ```std::string validateTweet(std::string tweet) {       |  ^ e87137cc-7567-43e7-bb97-e020b41e8bd9.cpp:1:3: error: stray ‘`’ in program     1 | ```std::string validateTweet(std::string tweet) {       |   ^ e87137cc-7567-43e7-bb97-e020b41e8bd9.cpp:1:9: error: ‘string’ in namespace ‘std’  does not name a type     1 | ```std::string validateTweet(std::string tweet) {       |         ^~~~~~ e87137cc-7567-43e7-bb97-e020b41e8bd9.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | ```std::string validateTweet(std::string tweet) {

--- twitter.cpp
+++ twitter.cpp
@@ -1,1 +1,11 @@
-return "Your tweet has " + std::to_string(length) + """ characters";
+#include <string>
+
+std::string validateTweet(std::string tweet) {
+    auto length = tweet.length();
+    if (length > 140) {
+        return "Too many characters";
+    }
+    if (tweet.empty()) {
+        return "You didn't type anything";
+    }
+    return "Your tweet has " + std::to_string(length) + " characters";
