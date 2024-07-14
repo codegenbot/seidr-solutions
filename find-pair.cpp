5a175ad7612e5846a24c7ba905a24ea2fb7b6aaf
beep boop <githubbot@vadim.me>
2024-07-14 02:06:48 +0200

    Solution 42 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/rb6qkkyb Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from ac8e3375-4949-40ca-92b3-c1fef5b8e644.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- find-pair.cpp
+++ find-pair.cpp
@@ -5,20 +5,19 @@
 
 std::pair<int, int> findPair(std::vector<int>& nums, int target) {
     std::unordered_map<int, int> numMap;
-    for (int i = 0; i < nums.size(); i++) {
-        int complement = target - nums[i];
+    for (int num : nums) {
+        int complement = target - num;
         if (numMap.find(complement) != numMap.end()) {
-            return std::make_pair(nums[i], complement);
+            return std::make_pair(num, complement);
         }
-        numMap[nums[i]] = i;
+        numMap[num] = 1;
     };
     return std::pair<int, int>(-1, -1);
 }
 
 int main() {
-    std::vector<int> nums(5); 
-    nums[0] = 1; nums[1] = 2; nums[2] = 3; nums[3] = 4; nums[4] = 5;
     int target = 7;
+    std::vector<int> nums = {1,2,3,4,5};
     auto pair = findPair(nums, target);
     if (pair.first != -1 && pair.second != -1) {
         std::cout << "Found pair: (" << pair.first << ", " << pair.second << ")" << std::endl;
