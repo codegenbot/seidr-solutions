526ec5fd1b73a806afdaa798e166079b4059fecf
beep boop <githubbot@vadim.me>
2024-07-15 01:35:33 +0200

    Solution 78 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/cxmivso2 Prompt: Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string"-> "camelCase exampleTestString".

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,50 +1,27 @@
-Here is the completed code:
-
-#include <vector>
 #include <iostream>
 #include <string>
 
-using namespace std;
-
-string camelCase(string s) {
-    string result = "";
-    bool firstWord = true;
-    
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == '-') {
-            if (!firstWord) {
-                result += char(toupper(s[i + 1]));
-                i++;
-            } else {
-                firstWord = false;
-            }
-        } else if (s[i] == ' ') {
-            if (!firstWord) {
-                result += char(toupper(s[i + 1]));
-                i++;
-            } else {
-                firstWord = false;
-            }
-        } else {
-            if (!firstWord) {
-                result += tolower(s[i]);
-            } else {
-                result += tolower(s[i]);
-                firstWord = false;
-            }
-        }
+std::string camelCase(std::string str) {
+    std::vector<std::string> words;
+    size_t pos = 0;
+    while ((pos = str.find(" ")) != std::string::npos) {
+        words.push_back(str.substr(0, pos));
+        str.erase(0, pos + 1);
     }
-    
-    return result;
+    if (!str.empty())
+        words.push_back(str);
+
+    for (size_t i = 0; i < words.size(); ++i) {
+        if (i > 0)
+            words[i][0] = toupper(words[i][0]);
+    }
+
+    return words.size() > 1 ? words[0] + std::string(words.begin() + 1, words.end()) : str;
 }
 
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-        cout << camelCase(s) << endl;
-    }
+    std::string input;
+    std::cin >> input;
+    std::cout << camelCase(input) << std::endl;
     return 0;
 }
