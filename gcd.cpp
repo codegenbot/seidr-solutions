b1137418ba66d34abe4b5b77f61b2569898ebfa7
beep boop <githubbot@vadim.me>
2024-07-14 08:09:09 +0200

    Solution 75 of gcd, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/nznteovb Prompt:       |                                            ^~~~~~ 68c654a5-a472-49d2-895a-8548babb5e45.cpp:18:9: error: ‘result’ was not declared in this scope    18 |         result.push_back(pos);       |         ^~~~~~ 68c654a5-a472-49d2-895a-8548babb5e45.cpp:22:9: error: ‘result’ was not declared in this scope    22 |     if (result.size() == 0)       |         ^~~~~~ 68c654a5-a472-49d2-895a-8548babb5e45.cpp:26:18: error: ‘result’ was not declared  in this scope; did you mean ‘gcdResult’?    26 |     for (int i : result) {       |                  ^~~~~~       |                  gcdResult 68c654a5-a472-49d2-895a-8548babb5e45.cpp:27:21: error: ‘__gcd’ was not declared in this scope; did you mean ‘gcd’?    27 |         gcdResult = __gcd(gcdResult, i);       |                     ^~~~~       |                     gcd 68c654a5-a472-49d2-895a-8548babb5e45.cpp:30:48: error: ‘endl’ was not declared i n this scope    30 |     cout << "GCD of indices: " << gcdResult << endl;       |                                                ^~~~

--- gcd.cpp
+++ gcd.cpp
@@ -1,33 +1,36 @@
-#include <iostream>
+#include <vector>
 #include <algorithm>
-using namespace std;
+#include <iostream> 
+#include <bits/stdc++.h>
 
 int gcd(int a, int b) {
-    if (b == 0)
-        return a;
+    if (b == 0) return a;
     return gcd(b, a % b);
 }
 
 int main() {
     string str1, str2;
+    cout << "Enter the first string: ";
     cin >> str1;
+    cout << "Enter the second string: ";
     cin >> str2;
 
-    int indices = -1; 
-    vector<int> result;
-
+    vector<int> result; 
+    int indices = -1;
+    
     size_t pos = 0;
     while ((pos = str1.find(str2, pos)) != string::npos) {
         result.push_back(pos);
-        pos += 1; 
+        pos += 1;
     }
 
     if (result.size() == 0)
         return 0;
-    
-    int gcdResult = result[0];
-    for (int i = 1; i < result.size(); i++)
-        gcdResult = __gcd(gcdResult, result[i]);
+
+    int gcdResult = 1;
+    for (int i : result) {
+        gcdResult = std::__gcd(gcdResult, i); 
+    }
         
-    cout << "GCD of indices: " << gcdResult << endl;
+    cout << "GCD of indices: " << gcdResult << std::endl; 
 }
