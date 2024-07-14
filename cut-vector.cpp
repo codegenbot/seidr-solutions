13450e9fe4e692dbd1ce538338f09d7caf284801
beep boop <githubbot@vadim.me>
2024-07-14 18:22:20 +0200

    Solution 68 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/rgsv09af Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -2,48 +2,52 @@
 using namespace std;
 
 pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+    int n = v.size();
     int min_diff = INT_MAX;
-    pair<vector<int>, vector<int>> result;
-    
-    for (int i = 1; i <= v.size() - 1; i++) {
+    pair<vector<int>, vector<int>> res;
+
+    for (int i = 1; i < n; ++i) {
         int left_sum = 0, right_sum = 0;
-        
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < i; ++j)
             left_sum += v[j];
-        
-        for (int j = i; j < v.size(); j++)
+        for (int j = i; j < n; ++j)
             right_sum += v[j];
-        
-        if (left_sum == right_sum) {
-            return {{v[0], v.begin() + 1, v.end()}};
-        }
-        
+
         int diff = abs(left_sum - right_sum);
-        
-        if (diff < min_diff) {
+        if (diff <= min_diff) {
             min_diff = diff;
-            result = {{v.begin(), v.begin() + i}, {v.begin() + i, v.end()}};
+            res = {{}, {}};
+            for (int j = 0; j < i; ++j)
+                res.first.push_back(v[j]);
+            for (int j = i; j < n; ++j)
+                res.second.push_back(v[j]);
         }
     }
-    
-    return result;
+
+    return res;
 }
 
 int main() {
     int n;
+    cout << "Enter the size of the vector: ";
     cin >> n;
-    vector<int> v(n);
-    for (int &x : v)
-        cin >> x;
-    pair<vector<int>, vector<int>> res = cutVector(v);
-    cout << "[";
-    for (int i : res.first) 
-        cout << i << " ";
-    cout << "]" << endl;
-    cout << "[";
-    for (int i : res.second) 
-        cout << i << " ";
-    cout << "0]" << endl;
     
+    vector<int> v(n);
+    cout << "Enter the elements of the vector: ";
+    for (auto &x : v)
+        cin >> x;
+
+    pair<vector<int>, vector<int>> res = cutVector(v);
+
+    cout << "The first subvector is: ";
+    for (int i : res.first)
+        cout << i << " ";
+    cout << endl;
+
+    cout << "The second subvector is: ";
+    for (int i : res.second)
+        cout << i << " ";
+    cout << endl;
+
     return 0;
 }
