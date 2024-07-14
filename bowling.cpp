a98f6b0f84efe047f9648fab671229c72f423d34
beep boop <githubbot@vadim.me>
2024-07-14 16:08:39 +0200

    Solution 91 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/sc52960f Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,32 +1,48 @@
-int bowling(string s) {
+Here is the solution:
+
+int bowlingScore(string s) {
     int score = 0;
-    int currentRolls = 0;
-    vector<int> rolls;
+    int currentFrame = 1;
+    int rollsInFrame = 0;
 
     for (char c : s) {
-        if (c == '/') {
-            if (currentRolls < 2) {
-                if (currentRolls == 0) {
-                    score += rolls[rolls.size() - 1];
+        if (c == 'X') { // strike
+            score += 10 + getBonusForStrike(currentFrame);
+            currentFrame++;
+            rollsInFrame = 0;
+        } else if (c != '/') {
+            int pins = c - '0';
+            if (rollsInFrame == 1) { // second roll in frame
+                if (currentFrame < 9 && s[currentFrame*2 + 1] == 'X') { // spare
+                    score += pins + getBonusForSpare(currentFrame);
+                } else {
+                    score += pins;
                 }
-                currentRolls++;
-            } else {
-                currentRolls = 0;
-                int lastRoll = 10 - (stoi(s.substr(s.find('/'))) - 5);
-                rolls.push_back(lastRoll);
-                score += lastRoll;
+            } else { // first roll in frame
+                rollsInFrame++;
             }
-        } else if (c == 'X') {
-            rolls.push_back(10);
-            score += 10;
-            currentRolls = 0;
-        } else {
-            int roll = stoi(string(1, c));
-            rolls.push_back(roll);
-            score += roll;
-            currentRolls++;
         }
     }
 
     return score;
+}
+
+int getBonusForStrike(int frame) {
+    if (frame < 9) {
+        if (s[frame*2] == 'X') {
+            return 10 + getBonusForStrike(frame+1);
+        } else {
+            return s[frame*2 - '0'] + s[frame*2 - '0'];
+        }
+    } else {
+        return 0;
+    }
+}
+
+int getBonusForSpare(int frame) {
+    if (frame < 8) {
+        return s[(frame+1)*2] - '0';
+    } else {
+        return 0;
+    }
 }
