3081ef544e51669a7f27056cfdfccf94e91d973f
beep boop <githubbot@vadim.me>
2024-07-14 17:51:11 +0200

    Solution 62 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/spi2t09t Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,30 +1,48 @@
 int bowlingScore(string s) {
     int score = 0;
-    int roll1, roll2, prevRolls = 0;
-    
-    for(int i=0; i<s.length(); i++) {
-        if(s[i] == '/') {
-            if(i > 0) {
-                if(s[i-1] != '/') {
-                    if(roll1 < 10) score += (10 - roll1);
-                }
-                prevRolls = 0;
-            }
-            continue;
-        }
-        
-        int roll = s[i] - 'X';
-        if(roll > 0) {
-            if(prevRolls == 1) score += roll + roll1;
-            else score += roll;
-            roll1 = roll;
-            prevRolls++;
+    int frame = 0;
+    for (char c : s) {
+        if (c == 'X') {
+            score += 30;
+            frame++;
+        } else if (c == '/') {
+            score += 10 + getRemainingFrame(s, frame);
+            frame++;
         } else {
-            roll1 = (int)s[i];
-            prevRolls++;
+            int count = c - '0';
+            while (count > 0) {
+                if (count >= 10) {
+                    score += 10;
+                    frame++;
+                    count -= 10;
+                } else {
+                    score += count;
+                    frame++;
+                    count = 0;
+                }
+            }
         }
-        
     }
-    
+    return score;
+}
+
+int getRemainingFrame(string s, int frame) {
+    int score = 0;
+    for (int i = frame; i < s.size(); i++) {
+        if (s[i] == 'X') {
+            score += 30;
+            break;
+        } else if (s[i] == '/') {
+            score += 10 + getRemainingFrame(s, i);
+            break;
+        } else {
+            int count = s[i] - '0';
+            while (i < s.size() && s[i] >= '1' && s[i] <= '9') {
+                count = count * 10 + (s[i] - '0');
+                i++;
+            }
+            score += count;
+        }
+    }
     return score;
 }
