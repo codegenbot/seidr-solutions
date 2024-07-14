86b65e48d40a7775a453e920a98bddf1ee552d7d
beep boop <githubbot@vadim.me>
2024-07-14 16:00:58 +0200

    Solution 69 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/sc52960f Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,41 +1,22 @@
-#include <vector>
-using namespace std;
-
-int bowling(string s) {
+int bowlingScore(string s) {
     int score = 0;
-    int roll = 0;
-    bool spare = false;
-    
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '/') {
-            int temp = stoi(s.substr(0, i));
-            if (temp > 1) score += 10 + temp;
-            else score += 10;
-            roll++;
-            spare = false;
-            s = s.substr(i+1);
-        } else if (s[i] == 'X') {
-            score += 10;
-            roll++;
-            spare = true;
-            s = s.substr(1);
-        }
-    }
-    
-    for (int i = 0; i < s.length(); i++) {
-        if (isdigit(s[i])) {
-            int temp = stoi(s.substr(0, i+1));
-            if (temp == 10) score += 10;
-            else {
-                if (roll >= 9 && spare) score += 20 - temp;
-                else score += temp;
+    for (int i = 0; i < 10; i++) {
+        if (s[i] == 'X') {
+            score += 30;
+        } else if (s[i] == '/') {
+            int nextTwo = (i + 2 < s.length()) ? (s[i + 1] - '0') * 10 + (s[i + 2] - '0') : 10;
+            score += 10 + nextTwo;
+        } else {
+            int currentFrame = 0;
+            for (int j = i; j < s.length(); j++) {
+                if (j == s.length() - 1 || s[j + 1] == '/') {
+                    currentFrame = currentFrame * 10 + (s[j] - '0');
+                    score += currentFrame;
+                    break;
+                }
+                currentFrame = currentFrame * 10 + (s[j] - '0');
             }
-            roll++;
-            s = s.substr(i+1);
-        } else if (s[i] == '-') {
-            break;
         }
     }
-    
     return score;
 }
