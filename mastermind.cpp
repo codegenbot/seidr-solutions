fd7beab61a25918f94f6e2f7fe261e6f04b5e721
beep boop <githubbot@vadim.me>
2024-07-14 05:38:31 +0200

    Solution 90 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/hpamfn8o Prompt:

--- mastermind.cpp
+++ mastermind.cpp
@@ -2,26 +2,26 @@ int mastermind(std::string code, std::string guess) {
     int black = 0;
     int white = 0;
 
-    std::map<char, int> codeCount;
-    for (char c : code) {
-        codeCount[c]++;
-    }
-
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             black++;
-        } else {
-            if (codeCount[guess[i]] > 0) {
-                white++;
-                codeCount[guess[i]]--;
-            }
+        } else if (guessCount[guess[i]] > 0) {
+            codeCount[code[i]]--;
+            guessCount[guess[i]]--;
+            white++;
         }
     }
 
-    for (char c : code) {
-        if (codeCount[c] > 0 && c != guess[0] && c != guess[1] && c != guess[2] && c != guess[3]) {
-            white++;
-            codeCount[c]--;
+    for (int i = 0; i < 4; ++i) {
+        if (code[i] != guess[i]) {
+            int count = 0;
+            for (char c : code) {
+                if (c == guess[i] && codeCount[c] > 0) {
+                    codeCount[c]--;
+                    count++;
+                }
+            }
+            white += count;
         }
     }
 
