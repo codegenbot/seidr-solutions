11fcfb1833fbf1417681093fb2c4678aae41e65e
beep boop <githubbot@vadim.me>
2024-07-14 14:37:40 +0200

    Solution 67 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/janq6p5d Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7074d93e-6af7-4aa7-928c-ed0345d17 4c2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,9 +1,9 @@
 ```
 def get_next_frames_score():
     score = 0
-    for i in range(1, min(3, 10 - len(str(bowling_score(""))) + 1)):
-        if i < len(str(bowling_score(""))):
-            score += int(str(bowling_score(""))[-i])
+    for i in range(1, min(3, 10 - len(str(total)) + 1)):
+        if i < len(str(total)):
+            score += int(str(total)[-i])
         else:
             break
     return score
