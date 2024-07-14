60a2eeffe69563e026d3c0ca76b9fd15f41f8782
beep boop <githubbot@vadim.me>
2024-07-14 15:05:33 +0200

    Solution 50 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/ddlomphr Prompt: er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; 4cec26de-f57d-48d6-a4a9-ce2e9066dda9.cpp:15:20: error: ‘abs’ was not declared in  this scope    15 |         int diff = abs(leftSum - rightSum);       |                    ^~~ 4cec26de-f57d-48d6-a4a9-ce2e9066dda9.cpp:21:105: error: could not convert ‘{std: :vector<int>((& v)->std::vector<int>::begin(), (& v)->std::vector<int>::begin(). __gnu_cxx::__normal_iterator<int*, std::vector<int> >::operator+(((__gnu_cxx::__ normal_iterator<int*, std::vector<int> >::difference_type)splitIndex)), std::all ocator<int>()), std::vector<int>((& v)->std::vector<int>::begin().__gnu_cxx::__n ormal_iterator<int*, std::vector<int> >::operator+(((__gnu_cxx::__normal_iterato r<int*, std::vector<int> >::difference_type)splitIndex)), (& v)->std::vector<int >::end(), std::allocator<int>())}’ from ‘<brace-enclosed initializer list>’ to ‘ std::vector<int>’    21 | v.begin() + splitIndex), vector<int>(v.begin() + splitIndex, v.end())};       |                                                                      ^       |                                                                      |       |                                                                      <br ace-enclosed initializer list>

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,46 +1,21 @@
-#include <iostream>
 #include <vector>
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int> vec) {
-    int minDiff = INT_MAX;
-    vector<vector<int>> result;
-    
-    for (int i = 0; i < vec.size() - 1; i++) {
+vector<int> cutVector(vector<int>& v) {
+    int minDiff = numeric_limits<int>::max();
+    int splitIndex = 0;
+    for (int i = 1; i <= v.size(); ++i) {
         int leftSum = 0, rightSum = 0;
-        
-        for (int j = 0; j <= i; j++) {
-            leftSum += vec[j];
+        for (int j = 0; j < i; ++j) {
+            leftSum += v[j];
         }
-        for (int k = i + 1; k < vec.size(); k++) {
-            rightSum += vec[k];
+        for (int j = i; j < v.size(); ++j) {
+            rightSum += v[j];
         }
-        
         int diff = abs(leftSum - rightSum);
-        
-        if (diff == 0 || diff < minDiff) {
-            result.push_back(vector<int>(vec.begin(), vec.begin() + i + 1));
-            result.push_back(vector<int>(vec.begin() + i, vec.end()));
+        if (diff < minDiff) {
             minDiff = diff;
+            splitIndex = i;
         }
     }
-    
-    return result;
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (auto &i : vec) {
-        cin >> i;
-    }
-    vector<vector<int>> res = cutVector(vec);
-    for (const auto &v : res) {
-        for (int x : v) {
-            cout << x << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
-}
+    return {vector<int>(v.begin(), v.begin() + splitIndex), vector<int>(v.begin() + splitIndex, v.end())};
