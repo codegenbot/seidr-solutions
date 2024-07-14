da3401a54e993b847b03f254bbde86c53d280f92
beep boop <githubbot@vadim.me>
2024-07-14 03:03:33 +0200

    Solution 23 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/vg71x9me Prompt: b543f63b-8491-4720-9032-6d59741c2520.cpp: In function ‘int main()’: b543f63b-8491-4720-9032-6d59741c2520.cpp:29:9: error: ‘cout’ was not declared in  this scope    29 |         cout << i << " ";       |         ^~~~ b543f63b-8491-4720-9032-6d59741c2520.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std;

--- leaders.cpp
+++ leaders.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 vector<int> leaders(vector<int>& arr) {
@@ -13,10 +14,33 @@ vector<int> leaders(vector<int>& arr) {
                 break;
             }
         }
+        
         if(isLeader) {
             res.push_back(arr[i]);
         }
     }
     
     return res;
+}
+
+vector<int> leaders(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> res;
+    res.push_back(arr[n-1]);
+    for(int i=n-2; i>=0; i--) {
+        if(arr[i] >= arr[i+1]) {
+            res.push_front(arr[i]);
+        }
+    }
+    
+    return res;
+}
+
+int main() {
+    vector<int> arr = {16, 17, 4, 3, 5, 2, 3, 7, 1, 6};
+    vector<int> res = leaders(arr);
+    for(int i:res) {
+        cout << i << " ";
+    }
+    return 0;
 }
