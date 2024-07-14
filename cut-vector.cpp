9827bb004aad946c6e95de419e50761a00f59272
beep boop <githubbot@vadim.me>
2024-07-14 02:14:55 +0200

    Solution 41 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/675h5l6k Prompt: std::vector<int>*; std::vector<_Tp, _Alloc>::_Tp_alloc_type = std::vector<std::v ector<int>, std::allocator<std::vector<int> > >::_Tp_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::vector<int, std::allocator<int> >&}; _Tp = std::vector< int>; _Alloc = std::allocator<std::vector<int> >; std::vector<_Tp, _Alloc>::iter ator = std::vector<std::vector<int>, std::allocator<std::vector<int> > >::iterat or]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::vector<int>; _Alloc  = std::allocator<std::vector<int> >; std::vector<_Tp, _Alloc>::value_type = std ::vector<int>]’ 4f2fb53b-3486-4eb3-b5f8-8fcc7b105194.cpp:30:18:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,12 +1,11 @@
 ```cpp
 #include <iostream>
 #include <vector>
-#include <utility>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
-    pair<vector<int>, vector<int>> res;
+    vector<vector<int>> res;
     int min_diff = INT_MAX;
     int split_index = -1;
     for (int i = 1; i < n; i++) {
@@ -28,12 +27,12 @@ pair<vector<int>, vector<int>> cutVector(vector<int> v) {
     for (int i = 0; i < split_index; i++) {
         left.push_back(v[i]);
     }
-    res.first = left;
+    res.push_back(left);
     left.clear();
     for (int i = split_index; i < v.size(); i++) {
         right.push_back(v[i]);
     }
-    res.second = right;
+    res.push_back(right);
     return res;
 }
 
@@ -44,18 +43,18 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> v[i];
     }
-    pair<vector<int>, vector<int>> res = cutVector(v);
+    vector<vector<int>> res = cutVector(v);
     cout << "[";
-    for (int i = 0; i < res.first.size(); i++) {
-        cout << res.first[i];
-        if (i < res.first.size() - 1) {
+    for (int i = 0; i < res[0].size(); i++) {
+        cout << res[0][i];
+        if (i < res[0].size() - 1) {
             cout << " ";
         }
     }
     cout << "] [";
-    for (int i = 0; i < res.second.size(); i++) {
-        cout << res.second[i];
-        if (i < res.second.size() - 1) {
+    for (int i = 0; i < res[1].size(); i++) {
+        cout << res[1][i];
+        if (i < res[1].size() - 1) {
             cout << " ";
         }
     }
