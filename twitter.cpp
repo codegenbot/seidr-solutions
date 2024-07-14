fd8bdbd200eb245fcb1622df2b6a1ef6d980912c
beep boop <githubbot@vadim.me>
2024-07-14 14:40:21 +0200

    Solution 65 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/z8czxzqc Prompt:                  from bf1096c1-0650-459b-a2d7-2a358cdb9864.cpp:2: /usr/include/c++/11/istream:647:5: note: candidate: ‘std::basic_istream<_CharT, _Traits>& std::basic_istream<_CharT, _Traits>::getline(std::basic_istream<_CharT , _Traits>::char_type*, std::streamsize, std::basic_istream<_CharT, _Traits>::ch ar_type) [with _CharT = char; _Traits = std::char_traits<char>; std::basic_istre am<_CharT, _Traits>::char_type = char; std::streamsize = long int]’   647 |     basic_istream<char>::       |     ^~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/istream:647:5: note:   candidate expects 3 arguments, 2 prov ided /usr/include/c++/11/istream:427:7: note: candidate: ‘std::basic_istream<_CharT, _Traits>::__istream_type& std::basic_istream<_CharT, _Traits>::getline(std::basi c_istream<_CharT, _Traits>::char_type*, std::streamsize) [with _CharT = char; _T raits = std::char_traits<char>; std::basic_istream<_CharT, _Traits>::__istream_t ype = std::basic_istream<char>; std::basic_istream<_CharT, _Traits>::char_type =  char; std::streamsize = long int]’   427 |       getline(char_type* __s, streamsize __n)       |       ^~~~~~~ /usr/include/c++/11/istream:427:26: note:   no known conversion for argument 1 f rom ‘std::string’ {aka ‘std::__cxx11::basic_string<char>’} to ‘std::basic_istrea m<char>::char_type*’ {aka ‘char*’}   427 |       getline(char_type* __s, streamsize __n)       |               ~~~~~~~~~~~^~~

--- twitter.cpp
+++ twitter.cpp
@@ -1,12 +1,25 @@
+#include <string>
+#include <iostream>
+
 std::string validateTweet(std::string tweet) {
-    if (tweet.empty()) {
-        return "You didn't type anything";
-    }
-    size_t len = 0;
+    std::string::size_type len = 0;
     for (char c : tweet) {
         ++len;
         if (len > 140) {
             return "Too many characters";
         }
     }
+    if (tweet.empty()) {
+        return "You didn't type anything";
+    }
     return "Your tweet has " + std::to_string(len) + " characters";
+}
+
+int main() {
+    std::string tweet;
+    std::cout << "Enter your tweet: ";
+    std::cin >> tweet;
+    std::cout << validateTweet(tweet) << std::endl;
+
+    return 0;
+}
