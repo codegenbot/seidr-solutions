6e63b25761a059b3ad090bc64ecc08fd44eb3029
beep boop <githubbot@vadim.me>
2024-07-14 16:03:52 +0200

    Solution 78 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/sc52960f Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,24 +1,54 @@
-int bowlingScore(string str) {
+int bowlingScore(string frames) {
     int score = 0;
-    for (int i = 0; i < 10; i++) {
-        if (str[i] == 'X') {
-            score += 30;
-        } else if (isdigit(str[i])) {
-            int frameScore = 0;
-            int j = i + 1;
-            while (j <= 11) {
-                if (j > 11 || str[j-1] != '/') {
-                    frameScore += (str[j-1] - '0');
-                } else {
-                    break;
-                }
-                j++;
-            }
-            score += frameScore;
+    for(int i = 0; i < 10; i++) {
+        if(frames[i] == 'X') { // strike
+            score += 10 + bowlingForNextTwoFrames(frames.substr(i+1));
+            i++; i++;
+        } else if(frames[i] == '/') { // spare
+            score += 10 + frames[i+1] - '0';
+            i++;
         } else {
-            int strike = 10 + bowlingScore(str.substr(i+2));
-            score += strike;
+            int first = frames[i] - '0';
+            int second = frames[i+1] - '0';
+            if(first + second >= 10) { // next two are not enough for strike
+                score += first + second;
+                i++;
+            } else {
+                score += first + second;
+                i++;
+            }
         }
     }
     return score;
+}
+
+int bowlingForNextTwoFrames(string frames) {
+    int score = 0;
+    for(int i = 0; i < 2; i++) {
+        if(frames[i] == 'X') { // strike
+            score += 10 + bowlingForNextOneFrame(frames.substr(i+1));
+            i++; i++;
+        } else if(frames[i] == '/') { // spare
+            score += 10 - (frames[i] - '0');
+            i++;
+        } else {
+            int one = frames[i] - '0';
+            int two = frames[i+1] - '0';
+            score += one + two;
+            i++;
+        }
+    }
+    return score;
+}
+
+int bowlingForNextOneFrame(string frames) {
+    if(frames[0] == 'X') { // strike
+        return 10;
+    } else if(frames[0] == '/') { // spare
+        return 10 - (frames[0] - '0');
+    } else {
+        int one = frames[0] - '0';
+        int two = frames[1] - '0';
+        return one + two;
+    }
 }
