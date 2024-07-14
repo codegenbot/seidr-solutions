8c60c0b3491b700df5b45c28719f1385de111834
beep boop <githubbot@vadim.me>
2024-07-14 15:03:01 +0200

    Solution 2 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/kmv5zktc Prompt: Given a vector of integers, return the two elements that sum to a target integer.

--- find-pair.py
+++ find-pair.py
@@ -1,8 +1,11 @@
 ```
-def find_pair(n, arr):
-    for i in range(n):
-        for j in range(i+1, n):
-            if arr[i] + arr[j] == int(input()):
-                return str(arr[i]) + '\n' + str(arr[j])
+def find_pair():
+    n = int(input())
+    nums = [int(x) for x in input().split()]
+    target = int(input())
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return '{}\n{}'.format(nums[i], nums[j])
 
-print(find_pair(int(input()), [int(x) for x in input().split()]))
+print(find_pair())
