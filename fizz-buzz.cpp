3c2c5d44ec32a1e5e96c3e7c75ff9a481708176d
beep boop <githubbot@vadim.me>
2024-07-14 18:24:13 +0200

    Solution 13 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/ubea9v9n Prompt: Given an integer x, return "Fizz" if x is divisible by 3, "Buzz" if x is divisible by 5, "FizzBuzz" if xis divisible by 3 and 5, and a string version of x if none of the above hold.

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,12 +1,42 @@
 #include <string>
+using namespace std;
 
-string fizzBuzz(int x) {
-    if (x % 3 == 0 && x % 5 == 0)
-        return "FizzBuzz";
-    else if (x % 3 == 0)
-        return "Fizz";
-    else if (x % 5 == 0)
-        return "Buzz";
-    else
-        return to_string(x);
-}
+class Solution {
+public:
+    string numberToWords(int num) {
+        if (num == 0) return "0";
+        vector<string> units = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+        vector<string> teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        vector<string> tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+        if (num < 10) return units[num];
+        if (num < 20) return teens[num - 10];
+
+        string res = "";
+        int i = 0;
+        while (num > 0) {
+            if ((num / 100) > 0) {
+                res += units[num / 100] + " Hundred";
+                num %= 100;
+            }
+            else if ((num / 20) > 0) {
+                res += tens[num / 20];
+                num %= 20;
+            }
+            else if (num < 10) {
+                res += units[num];
+                break;
+            }
+            else if (num < 20) {
+                res += teens[num - 10];
+                break;
+            }
+            else {
+                res += tens[num / 10];
+                num %= 10;
+            }
+        }
+
+        return res;
+    }
+};
