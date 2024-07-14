1d801ebec1325d5c9c4f2d7553aac850bfe25322
beep boop <githubbot@vadim.me>
2024-07-14 19:38:09 +0200

    Solution 96 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/raeono0a Prompt: Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,44 +1,21 @@
 int mastermind(string code, string guess) {
-    int whitePegs = 0;
-    int blackPegs = 0;
+    int white = 0;
+    int black = 0;
 
-    // Counting the number of white pegs
-    vector<char> codeArray(code.begin(), code.end());
-    vector<char> guessArray(guess.begin(), guess.end());
-
-    for (int i = 0; i < 4; ++i) {
-        if (codeArray[i] == guessArray[i]) {
-            blackPegs++;
-            codeArray[i] = '\0';
-            guessArray[i] = '\0';
+    for(int i=0; i<4; i++) {
+        if(code[i] == guess[i]) {
+            black++;
+        } else {
+            bool colorExist = false;
+            for(int j=0; j<4; j++) {
+                if(guess[j] == code[i] && i != j) {
+                    colorExist = true;
+                    break;
+                }
+            }
+            if(!colorExist) white++;
         }
     }
 
-    // Counting the number of black pegs
-    for (int i = 0; i < 4; ++i) {
-        if (!contains(codeArray, guessArray[i])) {
-            continue;
-        }
-        --codeArray[find(codeArray.begin(), codeArray.end(), guessArray[i]) - codeArray.begin()];
-        blackPegs++;
-    }
-
-    return make_pair(4 - blackPegs, blackPegs).first;
-}
-
-bool contains(const vector<char>& v, char x) {
-    for (char c : v) {
-        if (c == x) {
-            return true;
-        }
-    }
-    return false;
-}
-
-int find(const vector<char>& v, char x) {
-    for (int i = 0; i < v.size(); ++i) {
-        if (v[i] == x) {
-            return i;
-        }
-    }
+    return black << 2 | white;
 }
