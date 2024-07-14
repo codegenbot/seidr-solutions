8ffd6e4c456a21d54bd0e3e00aa91b78f9963e41
beep boop <githubbot@vadim.me>
2024-07-14 14:15:32 +0200

    Solution 3 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/janq6p5d Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,36 +1,29 @@
 ```
-def bowling_score(score):
-    score = score.replace('/', '')
-    total = 0
-    frame = 1
-    for i in range(0, len(score), 2):
-        if score[i].isdigit():
-            first_roll = int(score[i])
-            second_roll = 10 - first_roll if i + 1 < len(score) and score[i+1].isdigit() else 10 - first_roll
-            if first_roll == 10:
-                total += 10 + (0 if frame == 9 else get_next_frames_score())
-                frame += 1
-            elif second_roll == 10:
-                total += second_roll
-                frame += 1
-            else:
-                total += first_roll + second_roll
-                if first_roll + second_roll < 10:
-                    total += get_next_frames_score()
-                frame += (2 if first_roll + second_roll == 10 else 1)
-    return total
-
-def get_next_frames_score():
+def bowling_score(game):
     score = 0
-    for i in range(1, min(3, 10 - len(str(bowling_score(""))) + 1)):
-        if i < len(str(bowling_score(""))):
-            score += int(str(bowling_score(""))[-i])
+    frame_index = 0
+    for i in range(1, 11):
+        if game[frame_index] == 'X':
+            score += 10 + (10 - i) * 10
+            frame_index += 2
+        elif game[frame_index: frame_index+2] == 'XX':
+            score += 20
+            frame_index += 2
         else:
-            break
+            strike = False
+            for j in range(1, 3):
+                if game[frame_index + j - 1] != '/':
+                    if j == 1:
+                        first_roll = int(game[frame_index + j - 1])
+                    else:
+                        second_roll = int(game[frame_index + j - 1])
+                else:
+                    strike = True
+                    break
+            if strike:
+                score += 10 + first_roll + second_roll
+                frame_index += 2
+            else:
+                score += first_roll + second_roll
+                frame_index += 2
     return score
-
-print(bowling_score('0'))
-print(bowling_score('XXXXXXXXXXXX'))
-print(bowling_score('5/5/5/5/5/5/5/5/5/5/5'))
-print(bowling_score('7115XXX548/279-X53'))
-print(bowling_score('532/4362X179-41447/5'))
