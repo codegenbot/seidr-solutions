ddb90e711a48f763e60d45f1857cd89fd5d8e7b1
beep boop <githubbot@vadim.me>
2024-07-14 18:01:23 +0200

    Solution 92 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/spi2t09t Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,33 +1,22 @@
 int bowlingScore(string s) {
     int score = 0;
-    int roll = 0;
-    int lastRoll = -1; // To keep track of previous rolls in case of strike or spare
-    
-    for (char c : s) {
-        if (c == '/') { // If we hit a divide, it means the frame is over
-            score += min(10, roll); 
-            roll = 0;
-        } else if (isdigit(c)) {
-            roll = roll * 10 + c - '0'; // Add the number of pins knocked down this roll
-            if (roll >= 10) { // If we hit at least 10 with this roll
-                score += 10; 
-                roll = 0;
-            } else if (lastRoll == 1) {
-                score += roll + 10; // If the previous frame was a strike, add the number of pins knocked down to this frame's score
-                roll = 0;
+    for (int i = 0; i < 10; i++) {
+        if (s[i] == 'X') {
+            score += 30;
+        } else if (s[i] == '/') {
+            int firstRoll = s[i - 1] - '0';
+            int secondRoll = s[i + 1] - '0';
+            score += firstRoll + secondRoll;
+        } else {
+            int rolls = 0;
+            for (int j = i; j < 10; j++) {
+                if (s[j] == '/') {
+                    break;
+                }
+                rolls++;
             }
-        } else { // We hit an 'X' which means we got a strike in the last two rolls
-            score += 10 + min(10, (lastRoll == -1) ? 9 : 10); 
-            roll = 0;  
+            score += rolls * s[i] - '0';
         }
-        lastRoll = roll;
     }
-    
-    if (roll > 0) { // In case we got a strike or spare in the last frame
-        score += min(10, roll); 
-    } else {
-        score += min(10, roll);
-    }
-    
     return score;
 }
