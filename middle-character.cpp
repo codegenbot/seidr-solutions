d182ce3a3d47f6b461b21f9a303a4ef3fc9f4c42
beep boop <githubbot@vadim.me>
2024-07-14 16:28:55 +0200

    Solution 68 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/9t4to1yf Prompt: Given a string, return the middle character as a string if it is odd length; return the two middle characters as a string if it is even length.

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,19 +1,10 @@
-#include <string>
-using namespace std;
+Here is the solution:
 
-string middleCharacter(string str) {
-    int len = str.length();
-    if(len % 2 == 0){
-        return str.substr(len/2 - 1, 2);
-    } else{
-        return str.substr(len/2, 1);
+string middleCharacter(string s) {
+    int length = s.length();
+    if (length % 2 == 0) {
+        return s.substr(length / 2 - 1, 2);
+    } else {
+        return s.substr(length / 2, 1);
     }
-}
-
-int main() {
-    string s;
-    cout << "Enter a string: ";
-    getline(cin, s);
-    cout << middleCharacter(s) << endl;
-    return 0;
 }
