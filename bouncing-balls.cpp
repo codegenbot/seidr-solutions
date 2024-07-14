68a2a342ec0414cdfcb7f4f3b341a2ab376b1aa4
beep boop <githubbot@vadim.me>
2024-07-14 02:08:04 +0200

    Solution 77 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/hue7raky Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,22 +1,24 @@
+```cpp
+#include <iostream>
 #include <cmath>
 using namespace std;
 
 int main() {
-    double startHeight, firstBounce;
+    string; 
     int numBounces; 
+
     cin >> startHeight >> firstBounce >> numBounces; 
 
-    double bouncinessIndex = (firstBounce / startHeight);
+    double bouncinessIndex = (stod(firstBounce) / stod(startHeight));
 
     double totalDistance = 0.0;
     for (int i = 1; i <= numBounces; ++i) {
-        startHeight *= 2/3.0; // actual height after each bounce
-        totalDistance += abs(startHeight - firstBounce); // distance traveled
+        totalDistance += pow(2, i+1)-1)*bouncinessIndex*4;
     }
 
     cout << fixed;
     cout.precision(10);
-    cout << totalDistance * bouncinessIndex * 4.0 << endl;
+    cout << totalDistance << endl;
 
     return 0;
 }
