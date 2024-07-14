d7ceebb64f925025af5d8cf13d3ea131b37a63b7
beep boop <githubbot@vadim.me>
2024-07-15 00:49:51 +0200

    Solution 37 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/68jv3mis Prompt: Program did not receive expected input

--- camel-case.cpp
+++ camel-case.cpp
@@ -3,27 +3,22 @@
 
 std::string camelCase(std::string str) {
     std::string result = "";
+    bool nextUpper = true;
     for (int i = 0; i < str.length(); i++) {
         if (str[i] == '-') {
-            if (i > 0 && str[i - 1] != ' ') {
-                result += toupper(str[i + 1]);
-                i++;
-            } else {
-                int j = i;
-                while (j < str.length() && str[j] == '-') {
-                    j++;
-                }
-                result += str.substr(i + 1, j - i - 1);
-                i = j;
-            }
+            nextUpper = true;
         } else if (str[i] != ' ') {
-            result += tolower(str[i]);
+            if (nextUpper) {
+                result += toupper(str[i]);
+                nextUpper = false;
+            } else {
+                result += tolower(str[i]);
+            }
         } else {
+            nextUpper = true;
             if (!result.empty()) {
                 result[0] = toupper(result[0]);
             }
-            result += str.substr(i, str.find(' ', i) - i);
-            i = str.find(' ', i) - 1;
         }
     }
     return result;
@@ -31,12 +26,13 @@ std::string camelCase(std::string str) {
 
 int main() {
     std::string str;
-    while (std::cin >> str && !str.empty()) {
-        if (str[0] == '-') {
-            std::cout << "Program did not receive expected input" << std::endl;
+    while (std::cin >> str) {
+        if (str.find('-') == std::string::npos && !str.empty()) {
+            std::cout << "Program did not receive expected input.\n";
             break;
+        } else {
+            std::cout << camelCase(str) << std::endl;
         }
-        std::cout << camelCase(str) << std::endl;
     }
     return 0;
 }
