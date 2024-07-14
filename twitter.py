14e268e0f9bfe88640b04bb6ae3ffa6050e16b0d
beep boop <githubbot@vadim.me>
2024-07-14 11:07:34 +0200

    Solution 83 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ge7k7pdu Prompt: it must return Your tweet has 35 characters for input KW?~+G{4?F3vU!BPCUe*~Dt0WW)UsTQ99lb, but it returns Please enter your tweet: Your tweet has 35 characters.

--- twitter.py
+++ twitter.py
@@ -1,10 +1,3 @@
 ```
-def validate_tweet(tweet=""):
-    if len(tweet) > 140:
-        return "Too many characters"
-    elif tweet == "":
-        return "You didn't type anything"
-    else:
-        return f"Your tweet has {len(tweet)} characters"
-
-print(validate_tweet(input("Please enter your tweet: ")))
+print(tweet := input("Please enter your tweet: "))
+print(validate_tweet(tweet))
