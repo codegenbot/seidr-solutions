8d24719e1b193512f20aa8448c9dd80a769b4349
beep boop <githubbot@vadim.me>
2024-07-14 20:42:10 +0200

    Solution 40 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/sbpdvwbn Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,41 +1,48 @@
+#include <vector>
 #include <iostream>
 #include <string>
 
-std::string spinWords(std::string str) {
-    std::string result = "";
-    std::size_t pos = 0;
-    
-    while ((pos = str.find(" ")) != std::string::npos) {
-        std::size_t wordPos = str.find(" ", pos);
-        if (wordPos - pos >= 5) {
-            std::string word = str.substr(pos, wordPos - pos);
-            for (int i = word.size() - 1; i >= 0; --i)
-                result += word[i];
-        } else
-            result += str.substr(pos, wordPos - pos + 1);
-        
-        if (wordPos != std::string::npos) {
-            result += " ";
-            pos = wordPos + 1;
+using namespace std;
+
+string spinWords(string str) {
+    string result = "";
+    int wordLength;
+    for (int i = 0; i <= str.length(); i++) {
+        if (str[i] == ' ') {
+            result += str.substr(wordLength, i - wordLength) + " ";
+            wordLength = i + 1;
+        }
+        else if (i == str.length()) {
+            result += str.substr(wordLength);
         }
     }
-    
-    if (pos < str.size()) {
-        std::size_t wordPos = str.find(" ", pos);
-        if (str.size() - pos >= 5)
-            for (int i = str.substr(pos).size() - 1; i >= 0; --i)
-                result += str.substr(pos)[i];
-        else
-            result += str.substr(pos);
+
+    for (int i = 0; i < result.length(); i++) {
+        if ((result[i] >= 'a' && result[i] <= 'z') || (result[i] >= 'A' && result[i] <= 'Z')) {
+            int j = i;
+            while (j > 0 && (result[j-1] >= 'a' && result[j-1] <= 'z' || result[j-1] >= 'A' && result[j-1] <= 'Z')) {
+                j--;
+            }
+            if (j - i + 1 >= 5) {
+                string temp = "";
+                for (int k = i; k < j+1; k++) {
+                    temp += result[k];
+                }
+                reverse(temp.begin(), temp.end());
+                for (int k = i; k < j+1; k++) {
+                    result[k] = temp[k-i];
+                }
+            }
+        }
     }
-    
+
     return result;
 }
 
 int main() {
-    std::string input;
-    while (std::cin >> input) {
-        std::cout << spinWords(input) << std::endl;
-    }
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    cout << spinWords(str) << endl;
     return 0;
 }
