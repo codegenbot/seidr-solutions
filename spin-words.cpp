8c0f03f498497d7fee1bd4bf4d75c316be4a0fef
beep boop <githubbot@vadim.me>
2024-07-14 20:43:06 +0200

    Solution 41 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/sbpdvwbn Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -6,42 +6,35 @@ using namespace std;
 
 string spinWords(string str) {
     string result = "";
-    int wordLength;
-    for (int i = 0; i <= str.length(); i++) {
+    int wordLength = 0;
+    for (int i = 0; i < str.length(); i++) {
         if (str[i] == ' ') {
-            result += str.substr(wordLength, i - wordLength) + " ";
-            wordLength = i + 1;
-        }
-        else if (i == str.length()) {
-            result += str.substr(wordLength);
+            if (wordLength >= 5) {
+                for (int j = i - 1; j >= i - wordLength - 1; j--) {
+                    result += str[j];
+                }
+            } else {
+                result += str.substr(i - wordLength, wordLength);
+            }
+            result += ' ';
+            wordLength = 0;
+        } else {
+            wordLength++;
         }
     }
-
-    for (int i = 0; i < result.length(); i++) {
-        if ((result[i] >= 'a' && result[i] <= 'z') || (result[i] >= 'A' && result[i] <= 'Z')) {
-            int j = i;
-            while (j > 0 && (result[j-1] >= 'a' && result[j-1] <= 'z' || result[j-1] >= 'A' && result[j-1] <= 'Z')) {
-                j--;
-            }
-            if (j - i + 1 >= 5) {
-                string temp = "";
-                for (int k = i; k < j+1; k++) {
-                    temp += result[k];
-                }
-                reverse(temp.begin(), temp.end());
-                for (int k = i; k < j+1; k++) {
-                    result[k] = temp[k-i];
-                }
-            }
+    if (wordLength >= 5) {
+        for (int j = str.length() - 1; j >= str.length() - wordLength - 1; j--) {
+            result += str[j];
         }
+    } else {
+        result += str;
     }
-
     return result;
 }
 
 int main() {
     string str;
-    cout << "Enter a string: ";
+    cout << "Enter a sentence: ";
     getline(cin, str);
     cout << spinWords(str) << endl;
     return 0;
