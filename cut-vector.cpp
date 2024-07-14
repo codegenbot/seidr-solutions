3f26b872b7313de2c2714afeeff65e13c63bee6e
beep boop <githubbot@vadim.me>
2024-07-14 04:17:50 +0200

    Solution 91 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/675h5l6k Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 2d2cedb7-2515-4886-ad84-1848896061d5.cpp:30:20:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,8 +1,3 @@
-```cpp
-#include <iostream>
-#include <vector>
-#include <utility>
-
 std::pair<std::vector<int>, std::vector<int>> cutVector(std::vector<int> v) {
     int i = 0;
     for (; i <= v.size() - 1; i++) {
@@ -10,7 +5,7 @@ std::pair<std::vector<int>, std::vector<int>> cutVector(std::vector<int> v) {
             break;
         }
     }
-    std::pair<std::vector<int>, std::vector<int>> res(std::vector<int>(), std::vector<int>());
+    std::pair<std::vector<int>, std::vector<int>> res(std::vector<int>(v.begin(), v.begin()), std::vector<int>());
     for (int j = 0; j < i; j++) {
         res.first.push_back(v[j]);
     }
@@ -18,32 +13,4 @@ std::pair<std::vector<int>, std::vector<int>> cutVector(std::vector<int> v) {
         res.second.push_back(v[j]);
     }
     return res;
-}
-
-int main() {
-    int n;
-    std::cin >> n;
-    std::vector<int> v;
-    for (int i = 0; i < n; i++) {
-        int val;
-        std::cin >> val;
-        v.push_back(val);
-    }
-    std::pair<std::vector<int>, std::vector<int>> res = cutVector(v);
-    std::cout << "[";
-    for (int i = 0; i < res.first.size(); i++) {
-        std::cout << res.first[i];
-        if (i < res.first.size() - 1) {
-            std::cout << " ";
-        }
-    }
-    std::cout << "] [";
-    for (int i = 0; i < res.second.size(); i++) {
-        std::cout << res.second[i];
-        if (i < res.second.size() - 1) {
-            std::cout << " ";
-        }
-    }
-    std::cout << "]" << std::endl;
-    return 0;
 }
