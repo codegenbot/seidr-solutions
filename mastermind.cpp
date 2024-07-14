41d06d0d853502f3e6ef00774c82cc09a4e6c674
beep boop <githubbot@vadim.me>
2024-07-14 02:52:22 +0200

    Solution 25 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/hpamfn8o Prompt: a302ea21-7058-4f7d-aecd-c798e4151646.cpp:1:21: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                     ^~~~~~ a302ea21-7058-4f7d-aecd-c798e4151646.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int mastermind(std::string code, std::string guess) { a302ea21-7058-4f7d-aecd-c798e4151646.cpp:1:39: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                                       ^~~~~~ a302ea21-7058-4f7d-aecd-c798e4151646.cpp:1:39: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? a302ea21-7058-4f7d-aecd-c798e4151646.cpp:1:51: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(std::string code, std::string guess) {       |                                                   ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,27 +1,24 @@
-#include <string>
+#include<string>
 
 int mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
 
-    // Check for black pegs (correct color, correct place)
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             black++;
         }
     }
 
-    // Check for white pegs (correct color, wrong place)
-    int codeCount[6] = {0};
+    map<char,int> codeCount;
     for (char c : code) {
-        codeCount[c - 'A']++;
+        codeCount[c]++;
     }
     for (char c : guess) {
-        if (code.find(c) != std::string::npos && codeCount[c - 'A'] > 0) {
+        if (code.find(c) != string::npos && codeCount[c] > 0) {
             white++;
-            codeCount[c - 'A']--;
+            codeCount[c]--;
         }
     }
 
     return black + white;
-}
