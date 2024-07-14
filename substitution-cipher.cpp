c3193860228fea2ffbfef27895ba9c4fc90fa488
beep boop <githubbot@vadim.me>
2024-07-14 11:49:50 +0200

    Solution 73 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/66afpjtn Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: 5158e370-994b-4f47-bc61-35b799c865ee.cpp:41:86:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -3,19 +3,20 @@
 #include <string>
 
 std::string decipher(std::string cipher_map1, std::string cipher_map2, std::string message) {
-    for (auto& c : cipher_map1) {
-        c = tolower(c);
-    }
-    for (auto& c : cipher_map2) {
-        c = tolower(c);
-    }
+    std::string cipher_map1_lower = cipher_map1;
+    std::transform(cipher_map1_lower.begin(), cipher_map1_lower.end(), cipher_map1_lower.begin(),
+                   [](unsigned char c){ return tolower((char)c); });
+
+    std::string cipher_map2_lower = cipher_map2;
+    std::transform(cipher_map2_lower.begin(), cipher_map2_lower.end(), cipher_map2_lower.begin(),
+                   [](unsigned char c){ return tolower((char)c); });
 
     std::string deciphered_message = "";
     for (int i = 0; i < message.length(); i++) {
         char c = tolower(message[i]); 
-        int pos = cipher_map1.find(c);
+        int pos = cipher_map1_lower.find(c);
         if (pos != std::string::npos) {
-            deciphered_message += cipher_map2[pos];
+            deciphered_message += cipher_map2_lower[pos];
         } else {
             deciphered_message += c;
         }
@@ -24,17 +25,25 @@ std::string decipher(std::string cipher_map1, std::string cipher_map2, std::stri
 }
 
 int main() {
+    std::string cipher_map1, cipher_map2, message;
+
     std::cout << "Enter the first map: ";
-    std::string cipher_map1;
-    std::getline(std::cin, cipher_map1);
+    getline(std::cin, quote);
+    for(int i = 0; i < quote.length(); i++) 
+        if(quote[i] == '"') quote.erase(i,"");
+    for(int i = 0; i < quote.length(); i++) cipher_map1 += tolower((char)quote[i]);
 
     std::cout << "Enter the second map: ";
-    std::string cipher_map2;
-    std::getline(std::cin, cipher_map2);
+    getline(std::cin, quote);
+    for(int i = 0; i < quote.length(); i++) 
+        if(quote[i] == '"') quote.erase(i,"");
+    for(int i = 0; i < quote.length(); i++) cipher_map2 += tolower((char)quote[i]);
 
     std::cout << "Enter the message to decipher: ";
-    std::string message;
-    std::getline(std::cin, message);
+    getline(std::cin, quote);
+    for(int i = 0; i < quote.length(); i++) 
+        if(quote[i] == '"') quote.erase(i,"");
+    for(int i = 0; i < quote.length(); i++) message += (char)quote[i];
 
     std::cout << "Deciphered message: " << decipher(cipher_map1, cipher_map2, message) << std::endl;
 }
