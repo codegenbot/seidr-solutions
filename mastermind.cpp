8e0319b2292a890305d7c58ccf708baafb0cbdec
beep boop <githubbot@vadim.me>
2024-07-14 06:09:33 +0200

    Solution 95 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/hpamfn8o Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,33 +1,46 @@
+```cpp
 #include <string>
 #include <map>
 
-int mastermind(std::string code, std::string guess) {
-    int black = 0;
-    int white = 0;
+int main() {
+    int mastermind(std::string code, std::string guess) {
+        int black = 0;
+        int white = 0;
 
-    std::map<char, int> codeCount;
+        std::map<char, int> codeCount;
 
-    for (char c : code) {
-        codeCount[c]++;
-    }
+        for (char c : code) {
+            codeCount[c]++;
+        }
 
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            black++;
-        } else {
-            if (codeCount[guess[i]] > 0) {
-                white++;
-                codeCount[guess[i]]--;
+        for (int i = 0; i < 4; ++i) {
+            if (code[i] == guess[i]) {
+                black++;
+            } else {
+                if (codeCount[guess[i]] > 0) {
+                    white++;
+                    codeCount[guess[i]]--;
+                }
             }
         }
-    }
 
-    for (char c : code) {
-        if (codeCount[c] > 0 && c != guess[0] && c != guess[1] && c != guess[2] && c != guess[3]) {
-            white++;
-            codeCount[c]--;
+        for (char c : code) {
+            if (codeCount[c] > 0 && c != guess[0] && c != guess[1] && c != guess[2] && c != guess[3]) {
+                white++;
+                codeCount[c]--;
+            }
         }
+
+        return black + white;
     }
 
-    return black + white;
+    std::string code = "abcD";
+    std::string guess = "abcd";
+
+    int result = mastermind(code, guess);
+
+    std::cout << "Black pegs: " << (result / 2) << "\n";
+    std::cout << "White pegs: " << (result % 2) << "\n";
+
+    return 0;
 }
