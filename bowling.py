f1ba1a222a9c6de71082398b747cc5af8229ed1c
beep boop <githubbot@vadim.me>
2024-07-14 09:56:11 +0200

    Solution 70 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/92dz33yi Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6d6afff8-d585-4aba-aa2f-7beb515cd f1e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,30 +1,17 @@
 ```
 def bowling_score(game):
     score = 0
-    roll = 0
-    frame = 1
-    for char in game + 'X':
-        if char.isdigit():
-            if len(char) == 1:
-                strike = False
-                if roll < 2:
-                    score += int(char)
-                    roll += 1
-                else:
-                    score += int(char) + (10 - roll)
-                    break
-            elif len(char) == 2:
-                if char[0] == 'X':
-                    strike = True
-                else:
-                    score += int(char[0])
-                    roll += 1
+    rolls = game.split('/')
+    for i in range(10):
+        if len(rolls[i]) == 1:
+            score += int(rolls[i])
+        elif len(rolls[i]) > 1 and (i < 9 or (i == 9 and len(rolls[8]) != 2)):
+            score += 10 + int(rolls[i][0])
         else:
-            if not strike and roll < 2:
-                score += int(char[0])
-                roll += 1
-            elif strike and roll < 3:
-                score += int(char[0]) + (10 - roll)
-                break
-            frame += 1
+            first_roll = int(rolls[i][0])
+            second_roll = int(rolls[i][1])
+            if first_roll + second_roll < 10:
+                score += first_roll + second_roll
+            else:
+                score += 10 + second_roll
     return score
