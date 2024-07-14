7fd0f7810fbbea4e02c46a74dcfd605e199190bb
beep boop <githubbot@vadim.me>
2024-07-14 07:47:51 +0200

    Solution 41 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/jthoi6yj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1068feae-7d7d-4aca-94c6-e66a47b69 4f7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -1,5 +1,6 @@
-def substitution_cipher(cipher1, cipher2, message):
-    mapping = {c: c2 for c, c2 in zip(cipher1.lower(), cipher2.lower())}
+```
+def substitution_cipher(cipher1, cipher2, message)::
+    mapping = {c2: c for c, c2 in zip(cipher1.lower(), cipher2.lower())}
     
     result = ""
     for char in message:
@@ -10,3 +11,5 @@ def substitution_cipher(cipher1, cipher2, message):
                 result += mapping[char.lower()]
         else: 
             result += char
+    
+    return result
