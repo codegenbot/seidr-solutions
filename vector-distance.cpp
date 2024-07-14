1389089bdc0d6eab313d64057c97729be2b1d453
beep boop <githubbot@vadim.me>
2024-07-14 12:08:43 +0200

    Solution 36 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/inb7mtgb Prompt: it must return 242.5899586833783 for input 6 30.045858503023396 9.379624659900827 49.338175658348405 52.31539128227732 -61.22183805383865 4.410755481685257 6 -88.3673180506643 -63.27785390696465 36.616477843590246 -65.14022857021973 82.77332951465317 74.10168722657949, but it returns 0.0000000000.

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -6,11 +6,17 @@
 using namespace std;
 
 double vectorDistance(int n) {
+    double* x1 = new double[n], *x2 = new double[n];
     double sum = 0;
+    
     for (int i = 0; i < n; i++) {
-        double diff = pow(x2[i] - x1[i], 2);
-        sum += diff;
+        cin >> x1[i] >> x2[i];
+        double diff = x2[i] - x1[i];
+        sum += pow(diff, 2);
     }
+    
+    delete[] x1;
+    delete[] x2;
     
     return sqrt(sum);
 }
@@ -19,7 +25,7 @@ int main() {
     int n;
     cin >> n;
 
-    vector<float> x1(n), x2(n);
+    vector<double> x1(n), x2(n);
 
     for (int i = 0; i < n; i++) {
         cin >> x1[i] >> x2[i];
