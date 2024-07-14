245425b84ffa2af4c70c0a3f7e9c8784d569170b
beep boop <githubbot@vadim.me>
2024-07-14 09:14:49 +0200

    Solution 13 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/pg3ykh3f Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 81245f67-f9c3-444b-bf0f-ec5f2b922ab8.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 81245f67-f9c3-444b-bf0f-ec5f2b922ab8.cpp:24:21:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,30 +1,33 @@
-```c++
 #include <string>
+#include <stack>
 #include <iostream>
 
 bool solveBoolean(std::string s) {
-    bool result = true;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'T') {
-            result = true;
-        } else if (s[i] == 'F') {
-            result = false;
-        } else if (s[i] == '|') {
-            bool temp = !result;
-            result = true;
-        } else if (s[i] == '&') {
-            if (!result) return false;
-            result = false;
+    std::stack<bool> stack;
+    for (char c : s) {
+        if (c == 'T') stack.push(true);
+        else if (c == 'F') stack.push(false);
+        else if (c == '|') {
+            bool right = stack.top(); stack.pop();
+            bool left = stack.top(); stack.pop();
+            stack.push(left || right);
+        } 
+        else if (c == '&') {
+            bool right = stack.top(); stack.pop();
+            bool left = stack.top(); stack.pop();
+            stack.push(left && right);
         }
     }
-    return result;
+    return stack.top();
 }
 
 int main() {
-    std::string s;
-    std::cin >> s;
+    std::string s = "T|F&";
     bool result = solveBoolean(s);
-    if (result) std::cout << "True" << std::endl;
-    else std::cout << "False" << std::endl;
+    if (result) {
+        std::cout << "True" << std::endl;
+    } else {
+        std::cout << "False" << std::endl;
+    }
     return 0;
 }
