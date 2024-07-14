bf3dc4ce712e35f7d30cc574398589563adc71ef
beep boop <githubbot@vadim.me>
2024-07-14 08:54:53 +0200

    Solution 42 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/4by0ttu8 Prompt: d42c80bf-4fbf-4f5f-bb96-71c4ae39d038.cpp:1:29: error: ‘string’ in namespace ‘std ’ does not name a type     1 | int pairedDigits(const std::string& s) {       |                             ^~~~~~ d42c80bf-4fbf-4f5f-bb96-71c4ae39d038.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int pairedDigits(const std::string& s) { d42c80bf-4fbf-4f5f-bb96-71c4ae39d038.cpp: In function ‘int pairedDigits(const in t&)’: d42c80bf-4fbf-4f5f-bb96-71c4ae39d038.cpp:3:19: error: ‘begin’ was not declared i n this scope     3 |     for (char c : s) {       |                   ^ d42c80bf-4fbf-4f5f-bb96-71c4ae39d038.cpp:3:19: error: ‘end’ was not declared in this scope d42c80bf-4fbf-4f5f-bb96-71c4ae39d038.cpp:7:25: error: ‘to_string’ was not declar ed in this scope     7 |     for (int i = 0; i < to_string(num).length() - 1; i++) {       |                         ^~~~~~~~~

--- paired-digits.cpp
+++ paired-digits.cpp
@@ -2,13 +2,13 @@
 
 int pairedDigits(const std::string& s) {
     int num = 0;
-    for (char c : s) {
+    for (auto c : s) {
         num = num * 10 + (c - '0');
     }
     int sum = 0;
-    for (int i = 0; i < s.length() - 1; i++) {
-        if ((s[i] - '0') == (s[i + 1] - '0')) {
-            sum += (s[i] - '0') * 2;
+    for (int i = 0; i < std::to_string(num).size() - 1; i++) {
+        if ((std::to_string(num)[i] - '0') == (std::to_string(num)[i + 1] - '0')) {
+            sum += (std::to_string(num)[i] - '0') * 2;
         }
     }
     return sum;
