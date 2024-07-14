6f5c09202719ff635b88ed3e1923230234a06774
beep boop <githubbot@vadim.me>
2024-07-14 12:36:09 +0200

    Solution 96 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/dx46zc4b Prompt: n this scope     2 |     vector<int> nums;       |     ^~~~~~ 90cd15fe-e9da-408c-a911-9521f240d0ac.cpp:2:12: error: expected primary-expressio n before ‘int’     2 |     vector<int> nums;       |            ^~~ 90cd15fe-e9da-408c-a911-9521f240d0ac.cpp:4:12: error: ‘cin’ was not declared in this scope     4 |     while (cin >> n) {       |            ^~~ 90cd15fe-e9da-408c-a911-9521f240d0ac.cpp:5:9: error: ‘nums’ was not declared in this scope     5 |         nums.push_back(n);       |         ^~~~ 90cd15fe-e9da-408c-a911-9521f240d0ac.cpp:9:25: error: ‘nums’ was not declared in  this scope     9 |     for (int i = 0; i < nums.size(); ++i) {       |                         ^~~~ 90cd15fe-e9da-408c-a911-9521f240d0ac.cpp:13:5: error: ‘cout’ was not declared in  this scope    13 |     cout << sum;       |     ^~~~

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 int main() {
     vector<int> nums;
     int n;
-    while (std::cin >> n) {
+    while (cin >> n) {
         nums.push_back(n);
     }
 
     int sum = 0;
     for (int i = 0; i < nums.size(); ++i) {
-        sum += (nums[i] / 3 - 2);
+        sum += (floor(nums[i] / 3.0) - 2);
     }
 
-    std::cout << sum;
+    cout << sum;
 
     return 0;
 }
