cee08e892e1dd16e4d2cd8f762f15a1e8cd21f40
beep boop <githubbot@vadim.me>
2024-07-14 04:28:47 +0200

    Solution 48 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ixsuoat1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2a871dd2-9bc9-4a34-b812-bfddad7c8 65e.py", line 14     while expression[j] != ''):                              ^ SyntaxError: unmatched ')'

--- solve-boolean.py
+++ solve-boolean.py
@@ -9,13 +9,13 @@ def solve_boolean(expression):
 
     i = 0
     while i < len(expression):
-        if expression[i] == "(" or expression[i] == ")":
+        if expression[i] == '(' or expression[i] == ')':
             j = i
-            while expression[j] != ")":
+            while expression[j] != ')':
                 j += 1
-            temp = expression[:i]
-            new_expression = solve_boolean(expression[i + 1 : j])
-            expression = temp + str(new_expression) + expression[j + 1 :]
+            temp = expression[:i+1]
+            new_expression = solve_boolean(expression[i+2:j])
+            expression = temp + str(new_expression) + expression[j:]
             i = 0
         else:
             i += 1
