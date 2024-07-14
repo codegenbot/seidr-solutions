e294b054df73d79c0be7f805f1de023f057151aa
beep boop <githubbot@vadim.me>
2024-07-14 18:24:26 +0200

    Solution 14 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/ubea9v9n Prompt: Given an integer x, return "Fizz" if x is divisible by 3, "Buzz" if x is divisible by 5, "FizzBuzz" if xis divisible by 3 and 5, and a string version of x if none of the above hold.

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,42 +1,18 @@
-#include <string>
-using namespace std;
+Here is the solution:
 
-class Solution {
-public:
-    string numberToWords(int num) {
-        if (num == 0) return "0";
-        vector<string> units = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-        vector<string> teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-        vector<string> tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+int main() {
+    int x;
+    cin >> x;
 
-        if (num < 10) return units[num];
-        if (num < 20) return teens[num - 10];
-
-        string res = "";
-        int i = 0;
-        while (num > 0) {
-            if ((num / 100) > 0) {
-                res += units[num / 100] + " Hundred";
-                num %= 100;
-            }
-            else if ((num / 20) > 0) {
-                res += tens[num / 20];
-                num %= 20;
-            }
-            else if (num < 10) {
-                res += units[num];
-                break;
-            }
-            else if (num < 20) {
-                res += teens[num - 10];
-                break;
-            }
-            else {
-                res += tens[num / 10];
-                num %= 10;
-            }
-        }
-
-        return res;
+    if(x % 3 == 0 && x % 5 == 0) {
+        cout << "FizzBuzz";
+    } else if(x % 3 == 0) {
+        cout << "Fizz";
+    } else if(x % 5 == 0) {
+        cout << "Buzz";
+    } else {
+        cout << to_string(x);
     }
-};
+
+    return 0;
+}
