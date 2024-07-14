8bdc7046eb29a52273bdf0d6cf7b299e5a9b8505
beep boop <githubbot@vadim.me>
2024-07-14 13:36:44 +0200

    Solution 47 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/inb7mtgb Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from b9c7a7b5-c54e-4cb1-8cf8-7f4e8e016a4f.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,10 +1,12 @@
 #include <vector>
 #include <cmath>
+#include <initializer_list>
 
 double vectorDistance(int n, const std::vector<float>& x1, const std::vector<float>& x2) {
     double result = 0;
     for (int i = 0; i < n; i++) {
-        result += pow((x2[i] - x1[i]), 2);
+        double diff = pow((x2[i] - x1[i]), 2);
+        result += diff;
     }
     
     return sqrt(result); 
