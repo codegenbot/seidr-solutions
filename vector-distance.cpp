94ab1c228a0dcc743ed1425a9a69c7aa2b531a58
beep boop <githubbot@vadim.me>
2024-07-14 15:12:39 +0200

    Solution 75 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/lnggliix Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,17 +1,17 @@
 ```cpp
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 double vectorDistance() {
     int n;
     std::cin >> n; 
-    double sumOfSquares = 0;
-    float v1[n], v2[n];
+    std::vector<float> v1(n), v2(n);
     for (int i = 0; i < n; i++) {
-        std::cin >> v1[i] >> v2[i];
-
-        double differenceSquared = pow(v2[i] - v1[i], 2);
-        sumOfSquares += differenceSquared;
+        float temp;
+        std::cin >> temp >> temp; // read two values
+        v1[i] = temp;
+        v2[i] = temp; 
     }
-    return sqrt(sumOfSquares);
+    return sqrt(std::inner_product(v1.begin(), v1.end(), v2.begin(), 0.0));
 }
