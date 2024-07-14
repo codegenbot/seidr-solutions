a9137f0c24fbe1c3f65ccf8563b2259947192475
beep boop <githubbot@vadim.me>
2024-07-14 13:20:07 +0200

    Solution 90 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/wd0svep5 Prompt: 4927dad5-02fe-4cbc-8f88-c24b3764f25e.cpp:4:18: error: ‘string’ was not declared in this scope     4 | int bowlingScore(string s) {       |                  ^~~~~~ 4927dad5-02fe-4cbc-8f88-c24b3764f25e.cpp:2:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?     1 | #include <vector>   +++ |+#include <string>     2 | using namespace std;

--- bowling.cpp
+++ bowling.cpp
@@ -1,37 +1,43 @@
+#include <vector>
 #include <string>
+using namespace std;
 
-int bowlingScore(string str) {
+int bowlingScore(string s) {
     int score = 0;
-    for (int i = 0; i < 10; i++) {
-        if (str[i] == '/') {
-            string first = str.substr(0, i);
-            string second = str.substr(i + 1);
-            int strike = 0;
-            int spare = 0;
-            for (char c : first) {
-                if (c == 'X') {
-                    score += 10;
-                    strike++;
+    int roll = 0;
+    vector<int> rolls(10);
+
+    for (char c : s) {
+        if (c == '/') {
+            if (roll == 1) {
+                if (rolls[0] + rolls[1] < 10) {
+                    score += 10 - (rolls[0] + rolls[1]);
                 } else {
-                    score += c - '0';
+                    score += rolls[0] + rolls[1];
                 }
+                roll = 0;
+            } else {
+                rolls[9 - roll] = c == 'X' ? 10 : (c - '0');
+                roll++;
             }
-            for (char c : second) {
-                if (c == '/') {
-                    spare = 1;
-                    break;
-                } else if (c == 'X') {
-                    score += 10;
-                    strike++;
-                } else {
-                    score += c - '0';
-                }
-            }
-            if (!spare && !strike) {
-                score += first.size() + second.size();
-            }
+        } else if (c == 'X') {
+            score += 10;
+            rolls[9 - roll] = 10;
+            roll++;
         } else {
-            score += str[i] - '0';
+            rolls[9 - roll] = c - '0';
+            roll++;
         }
     }
+
+    for (int i = 0; i < 10; i++) {
+        if (rolls[i] == 10) {
+            score += 10 + rolls[i+1] + rolls[i+2];
+            i += 2;
+        } else {
+            score += rolls[i];
+        }
+    }
+
     return score;
+}
