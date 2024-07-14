daeec1171f77baf2f152c3373d662c1662b388b4
beep boop <githubbot@vadim.me>
2024-07-14 15:10:31 +0200

    Solution 88 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/z8czxzqc Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 65902cab-7d57-4006-9950-fcae6bddc3c2.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 65902cab-7d57-4006-9950-fcae6bddc3c2.cpp:8:16:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -1,16 +1,30 @@
 ```cpp
-#include <string>
 #include <iostream>
-#include <cstddef>
+#include <string>
 
-const char* input; 
-std::string validateTweet(const char* input) {
-    if (*input == '\0') {
+using namespace std;
+
+std::string validateTweet(const string& input) {
+    if (input.empty()) {
         return "You didn't type anything";
     }
-    size_t len = strlen(input);
+    size_t len = input.length();
     if (len > 140) {
         return "Too many characters";
     }
-    return "Your tweet has ";
+    return "Your tweet has " + to_string(len) + " characters";
+}
+
+int main() {
+    string str;
+    cout << "Enter your tweet: ";
+    getline(cin, str);
+    if (str.length() > 140) {
+        cout << "Too many characters" << endl;
+    } else if (str.empty()) {
+        cout << "You didn't type anything" << endl;
+    } else {
+        cout << "Your tweet has " << str.length() << " characters" << endl;
+    }
+    return 0;
 }
