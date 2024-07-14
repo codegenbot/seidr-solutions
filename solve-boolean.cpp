9e6e8279294a7a9f53d882bb33a3bbb7ba6f4abb
beep boop <githubbot@vadim.me>
2024-07-14 11:07:46 +0200

    Solution 98 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/xmqgnmoc Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,21 +1,21 @@
 ```cpp
 #include <string>
 #include <iostream>
-#include <sstream>
 
 int main() {
-    std::stringstream ss;
+    std::string expression;
     char c;
 
-    while ((c = std::getchar()) != '\n' && c != EOF) {
-        ss << c; 
+    while ((c = getchar()) != '\n' && c != EOF) { 
+        expression.push_back(c); 
     }
-    std::string expression = ss.str();
 
-    bool result = false;
+    bool result = false;  
 
-    for (char e : expression) {
-        if (e == 'T') result = true; else if (e == 'F') result = false;
+    for (int i = 0; i < expression.length(); ++i) {
+        char e = expression[i]; 
+        if (e == 'T') result = true;
+        else if (e == 'F') result = false;
         else if (e == '|') result = !result;
         else if (e == '&') {
             bool temp = result;
