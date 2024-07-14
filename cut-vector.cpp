ae2fdc4137f2fff2daff71b45cdac2fd5fffbb9a
beep boop <githubbot@vadim.me>
2024-07-14 18:25:24 +0200

    Solution 75 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/rgsv09af Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,42 +1,52 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> &nums) {
+pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
     int minDiff = INT_MAX;
-    int pos = -1;
-    
-    for (int i = 0; i < nums.size() - 1; i++) {
-        int diff = abs(nums[i] - nums[i + 1]);
-        if (diff <= minDiff) {
+    pair<int, int> res;
+    for (int i = 1; i <= vec.size(); ++i) {
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int j = 0; j < i; ++j)
+            leftSum += vec[j];
+        for (int j = i; j < vec.size(); ++j)
+            rightSum += vec[j];
+
+        if (leftSum == rightSum) {
+            return {{vec.begin(), vec.begin() + i}, {vec.begin() + i, vec.end()}};
+        }
+
+        int diff = abs(leftSum - rightSum);
+        if (diff < minDiff) {
             minDiff = diff;
-            pos = i;
+            res = make_pair(i, 0);
         }
     }
-    
-    vector<int> left, right;
-    copy(nums.begin(), nums.begin() + pos, back_inserter(left));
-    copy(nums.begin() + pos, nums.end(), back_inserter(right));
-    
-    return make_pair(left, right);
+
+    int leftSize = res.first;
+    return {{vec.begin(), vec.begin() + leftSize}, {vec.begin() + leftSize, vec.end()}};
 }
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
-    pair<vector<int>, vector<int>> result = cutVector(nums);
-    cout << "Left: ";
-    for (int num : result.first) {
-        cout << num << " ";
-    }
+    // Your test cases here
+    vector<int> vec1 = {1};
+    pair<vector<int>, vector<int>> result1 = cutVector(vec1);
+    for (int i : *result1.first)
+        cout << i << " ";
     cout << endl;
-    cout << "Right: ";
-    for (int num : result.second) {
-        cout << num << " ";
-    }
+    for (int i : *result1.second)
+        cout << i << " ";
     cout << endl;
+
+    // Your test cases here
+    vector<int> vec2 = {1, 10};
+    pair<vector<int>, vector<int>> result2 = cutVector(vec2);
+    for (int i : *result2.first)
+        cout << i << " ";
+    cout << endl;
+    for (int i : *result2.second)
+        cout << i << " ";
+    cout << endl;
+
     return 0;
 }
