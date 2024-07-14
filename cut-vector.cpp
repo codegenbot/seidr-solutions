38dd8386e1f67b863be5dfd49d3ee29f6d41da20
beep boop <githubbot@vadim.me>
2024-07-14 10:50:45 +0200

    Solution 8 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/n62afbof Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,52 +1,36 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
-    int min_diff = INT_MAX;
-    pair<vector<int>, vector<int>> result;
-    
-    for (int i = 1; i <= vec.size(); ++i) {
-        int left_sum = 0, right_sum = 0;
-        
-        for (int j = 0; j < i; ++j) {
-            left_sum += vec[j];
-        }
-        
-        for (int j = i; j < vec.size(); ++j) {
-            right_sum += vec[j];
-        }
-        
-        int diff = abs(left_sum - right_sum);
-        
-        if (diff <= min_diff) {
-            min_diff = diff;
-            result.first = vector<int>(vec.begin(), vec.begin() + i);
-            result.second = vector<int>(vec.begin() + i, vec.end());
-        }
-    }
-    
-    return result;
-}
+vector<vector<int>> cutVector(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> res(2);
+    int minDiff = INT_MAX;
+    int splitIndex = -1;
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (auto &x : vec) {
-        cin >> x;
+    for (int i = 0; i < n; i++) {
+        int leftSum = 0, rightSum = 0;
+        for (int j = 0; j <= i; j++) {
+            leftSum += nums[j];
+        }
+        for (int j = i + 1; j < n; j++) {
+            rightSum += nums[j];
+        }
+
+        if (leftSum == rightSum) {
+            res[0] = vector<int>(nums.begin(), nums.begin() + i);
+            res[1] = vector<int>(nums.begin() + i, nums.end());
+            return res;
+        } else if (abs(leftSum - rightSum) < minDiff) {
+            minDiff = abs(leftSum - rightSum);
+            splitIndex = i;
+        }
     }
-    
-    pair<vector<int>, vector<int>> res = cutVector(vec);
-    
-    cout << "[";
-    for (const auto &x : res.first) {
-        cout << x << " ";
+
+    int leftSum = 0;
+    for (int i = 0; i <= splitIndex; i++) {
+        leftSum += nums[i];
     }
-    cout << "] [" << "[";
-    for (const auto &x : res.second) {
-        cout << x << " ";
-    }
-    cout << "] 0\n";
-    
-    return 0;
+    res[0] = vector<int>(nums.begin(), nums.begin() + splitIndex);
+    res[1] = vector<int>(nums.begin() + splitIndex, nums.end());
+    return res;
 }
