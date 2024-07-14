aaf4e65e092273546640790f703c3ec4b4d44eea
beep boop <githubbot@vadim.me>
2024-07-14 18:02:05 +0200

    Solution 16 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/rgsv09af Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,61 +1,45 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int> v) {
-    int n = v.size();
-    vector<vector<int>> res(2);
-    for (int i = 0; i < n; i++) {
-        if (i == 0 || v[i] != v[0]) {
-            int diff = INT_MAX;
-            int pos = -1;
-            for (int j = i; j >= 0 && j < n; j--) {
-                if (v[j] == v[0]) {
-                    res[0].clear();
-                    res[0].insert(res[0].end(), v.begin(), v.begin() + j);
-                    res[1].clear();
-                    res[1].insert(res[1].begin(), v.begin() + j, v.end());
-                    return res;
-                } else if (v[j] - v[i] < diff) {
-                    diff = v[j] - v[i];
-                    pos = j;
-                }
-            }
-            for (int j = i; j >= 0 && j < n; j--) {
-                if (v[j] - v[i] == diff) {
-                    res[0].clear();
-                    res[0].insert(res[0].end(), v.begin(), v.begin() + j);
-                    res[1].clear();
-                    res[1].insert(res[1].begin(), v.begin() + j, v.end());
-                    return res;
-                }
-            }
-        } else {
-            res[0].clear();
-            res[0].insert(res[0].end(), v.begin(), v.begin() + i);
-            res[1].clear();
-            res[1].insert(res[1].begin(), v.begin() + i, v.end());
-            return res;
+pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+    int min_diff = INT_MAX;
+    int cut_index = -1;
+    for (int i = 0; i < v.size() - 1; i++) {
+        int left_sum = 0, right_sum = 0;
+        for (int j = 0; j <= i; j++) {
+            left_sum += v[j];
+        }
+        for (int j = i + 1; j < v.size(); j++) {
+            right_sum += v[j];
+        }
+        int diff = abs(left_sum - right_sum);
+        if (diff < min_diff) {
+            min_diff = diff;
+            cut_index = i;
         }
     }
-    res[0].clear();
-    res[0].insert(res[0].end(), v.begin(), v.end());
-    res[1].clear();
-    return res;
+    vector<int> left(v.begin(), v.begin() + cut_index + 1);
+    vector<int> right(v.begin() + cut_index, v.end());
+    return {left, right};
 }
 
 int main() {
     int n;
     cin >> n;
     vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
+    for (auto &x : v) {
+        cin >> x;
     }
-    vector<vector<int>> res = cutVector(v);
-    for (auto& vec : res) {
-        for (int num : vec) {
-            cout << num << " ";
-        }
-        cout << endl;
+    pair<vector<int>, vector<int>> result = cutVector(v);
+    cout << "Left: ";
+    for (const auto &x : result.first) {
+        cout << x << " ";
     }
+    cout << endl;
+    cout << "Right: ";
+    for (const auto &x : result.second) {
+        cout << x << " ";
+    }
+    cout << endl;
     return 0;
 }
