b75239efdfd5de8e6aeb73c19c2fe912829973e9
beep boop <githubbot@vadim.me>
2024-07-14 10:06:05 +0200

    Solution 62 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/ovhx5wz2 Prompt: _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ c64017e6-ba82-459c-99d4-e511558c0ce4.cpp:13:42:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ c64017e6-ba82-459c-99d4-e511558c0ce4.cpp:13:42:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’

--- middle-character.cpp
+++ middle-character.cpp
@@ -4,10 +4,13 @@
 std::string middleCharacter(std::string s) {
     int len = s.length();
     if (len % 2 == 0) { 
-        return std::string(1, s.substr(len / 2 - 1, 1)[0]) + std::string(1, s.substr(len / 2, 1)[0]);
+        return std::string(1, s.substr(len / 2 - (len % 2 == 0), 1).c_str());
     } else { 
-        return std::string(1, s.substr(len / 2, 1)[0]);
+        return std::string(1, s.substr(len / 2, 1).c_str());
     }
 }
 
-int main() { std::cout << middleCharacter("your_test_string"); return 0; }
+int main() {
+    std::cout << middleCharacter("your_test_string");
+    return 0;
+}
