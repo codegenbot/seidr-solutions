10d90568f1202ed26ffc35255f6f45694a4bad5e
beep boop <githubbot@vadim.me>
2024-07-14 09:43:08 +0200

    Solution 48 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/66afpjtn Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: 499f327a-d026-448b-b08e-c3b0e321ed5e.cpp:36:44:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -3,14 +3,12 @@
 #include <string>
 
 std::string decipher(std::string cipher_map1, std::string cipher_map2, std::string message) {
-    std::string deciphered_message = "";
+    std::string deciphered_message = message;
     for (int i = 0; i < message.length(); i++) {
         char c = message[i];
         int pos = cipher_map1.find(c);
         if (pos != std::string::npos) {
-            deciphered_message += cipher_map2[pos];
-        } else {
-            deciphered_message += c;
+            deciphered_message[i] = cipher_map2[pos];
         }
     }
     return deciphered_message;
