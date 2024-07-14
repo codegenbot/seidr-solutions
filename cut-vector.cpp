dd322c92417bf01f324678dc5fb12e6a4726a0df
beep boop <githubbot@vadim.me>
2024-07-14 14:24:08 +0200

    Solution 77 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/n62afbof Prompt: f3275b46-d471-4a70-a44e-0c5b52f58cfb.cpp:19:20: error: ‘abs’ was not declared in  this scope    19 |         } else if (abs(left_sum - right_sum) < min_diff) {       |                    ^~~ f3275b46-d471-4a70-a44e-0c5b52f58cfb.cpp: In function ‘int main()’: f3275b46-d471-4a70-a44e-0c5b52f58cfb.cpp:37:5: error: ‘cout’ was not declared in  this scope    37 |     cout << "Input: ";       |     ^~~~ f3275b46-d471-4a70-a44e-0c5b52f58cfb.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; f3275b46-d471-4a70-a44e-0c5b52f58cfb.cpp:39:13: error: ‘endl’ was not declared i n this scope    39 |     cout << endl;       |             ^~~~ f3275b46-d471-4a70-a44e-0c5b52f58cfb.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,7 +1,7 @@
 ```cpp
 #include <iostream>
 #include <vector>
-#include <cmath> 
+#include <cmath> // for abs()
 using namespace std;
 
 pair<vector<int>, vector<int>> cutVector(vector<int> v) {
@@ -37,29 +37,29 @@ int main() {
     // Example inputs
     vector<int> v1({1});
     pair<vector<int>, vector<int>> res1 = cutVector(v1);
-    cout << "Input: ";
-    for (int x : v1) cout << x << " ";
-    cout << std::endl;
-    cout << "Output:" << std::endl;
-    cout << "Left: ";
-    for (int x : res1.first) cout << x << " ";
-    cout << std::endl;
-    cout << "Right: ";
-    for (int x : res1.second) cout << x << " ";
-    cout << std::endl << std::endl;
+    std::cout << "Input: ";
+    for (int x : v1) std::cout << x << " ";
+    std::cout << std::endl;
+    std::cout << "Output:" << std::endl;
+    std::cout << "Left: ";
+    for (int x : res1.first) std::cout << x << " ";
+    std::cout << std::endl;
+    std::cout << "Right: ";
+    for (int x : res1.second) std::cout << x << " ";
+    std::cout << std::endl << std::endl;
 
     vector<int> v2({1, 10});
     pair<vector<int>, vector<int>> res2 = cutVector(v2);
-    cout << "Input: ";
-    for (int x : v2) cout << x << " ";
-    cout << std::endl;
-    cout << "Output:" << std::endl;
-    cout << "Left: ";
-    for (int x : res2.first) cout << x << " ";
-    cout << std::endl;
-    cout << "Right: ";
-    for (int x : res2.second) cout << x << " ";
-    cout << std::endl << std::endl;
+    std::cout << "Input: ";
+    for (int x : v2) std::cout << x << " ";
+    std::cout << std::endl;
+    std::cout << "Output:" << std::endl;
+    std::cout << "Left: ";
+    for (int x : res2.first) std::cout << x << " ";
+    std::cout << std::endl;
+    std::cout << "Right: ";
+    for (int x : res2.second) std::cout << x << " ";
+    std::cout << std::endl << std::endl;
 
     // Add more inputs as needed
     return 0;
