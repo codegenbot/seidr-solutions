836ffaf4947a606f6702c5f13258152d0f577fe8
beep boop <githubbot@vadim.me>
2024-07-14 10:47:23 +0200

    Solution 37 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/pg3ykh3f Prompt: In file included from /usr/include/c++/11/bits/stl_deque.h:63,                  from /usr/include/c++/11/deque:67,                  from /usr/include/c++/11/stack:60,                  from 73f8679d-8dbb-4624-8a58-602db9c620d0.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,11 +1,25 @@
+#include <queue>
+#include <string>
+
 bool solveBoolean(std::string s) {
-    std::stack<bool> stack;
-    bool parentheses = 0; 
+    std::queue<bool> queue;
+    int parentheses = 0; 
     for (char c : s) {
-        if (c == '(') parentheses++;
-        else if (c == ')') parentheses--;
-        else if (c == '|') stack.push(stack.top() || stack.pop());
-        else if (c == '&') stack.push(stack.top() && stack.pop());
-        else if (c == 'T' || c == 'F') stack.push(c == 'T');
+        if (c == '(') {
+            queue.push(false);
+            parentheses++;
+        }
+        else if (c == ')') {
+            while(parentheses > 0) {
+                bool top = queue.front();
+                queue.pop();
+                if (c == ')') queue.push(top);
+                else queue.push(!top);
+                parentheses--;
+            }
+        }
+        else if (c == '|') queue.push(queue.front() || queue.pop());
+        else if (c == '&') queue.push(queue.front() && queue.pop());
+        else if (c == 'T' || c == 'F') queue.push(c == 'T');
     }
-    return stack.top();
+    return queue.front();
