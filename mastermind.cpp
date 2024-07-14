d9292a13c0e468cf6ef9afd8809ac5344c27b2d7
beep boop <githubbot@vadim.me>
2024-07-14 19:29:33 +0200

    Solution 79 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/raeono0a Prompt: Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,26 +1,34 @@
-int mastermind(string code, string guess) {
-    int white = 0;
-    int black = 0;
-
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            ++black;
+int whitePegs(string code, string guess) {
+    int count = 0;
+    for(int i=0; i<4; i++) {
+        if(code[i] == guess[i]) {
+            count++;
         }
     }
+    return 4 - count;
+}
 
-    map<char, int> codeCount, guessCount;
-    for (int i = 0; i < 4; ++i) {
-        ++codeCount[code[i]];
-        ++guessCount[guess[i]];
-    }
-
-    for (int i = 0; i < 4; ++i) {
-        if (codeCount[code[i]] > 0 && guessCount[code[i]] > 1) {
-            ++white;
-            --codeCount[code[i]];
-            --guessCount[code[i]];
+int blackPegs(string code, string guess) {
+    int count = 0;
+    vector<char> codeVector(code.begin(), code.end());
+    for(int i=0; i<4; i++) {
+        if(code[i] == guess[i]) {
+            codeVector.erase(remove(codeVector.begin(), codeVector.end(), code[i]), codeVector.end());
         }
     }
+    for(int i=0; i<4; i++) {
+        if(codeVector.size() && codeVector[0] == guess[i]) {
+            count++;
+            codeVector.erase(codeVector.begin());
+        }
+    }
+    return count;
+}
 
-    return black + white;
+int main() {
+    string code, guess;
+    cin >> code >> guess;
+    cout << whitePegs(code, guess) << endl;
+    cout << blackPegs(code, guess) << endl;
+    return 0;
 }
