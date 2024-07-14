41e8fcdc8c6c96ad52f11c156da57a72fe567f36
beep boop <githubbot@vadim.me>
2024-07-14 19:14:32 +0200

    Solution 68 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/oze1uxoq Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,39 +1,36 @@
-Here is the solution:
-
-#include <vector>
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-string spinWords(string str) {
-    string result = "";
-    int i = 0;
-    while (i < str.length()) {
-        if (str[i] == ' ') {
-            result += str.substr(i);
-            i++;
-            continue;
+string spinWords(string input) {
+    string output = "";
+    string word = "";
+
+    for (int i = 0; i < input.length(); i++) {
+        char c = input[i];
+        if (c == ' ') {
+            if (word.length() >= 5) {
+                reverse(word.begin(), word.end());
+            }
+            output += word + " ";
+            word = "";
+        } else {
+            word += c;
         }
-        int j = i + 1;
-        while (j <= str.length() && str[j-1] != ' ') {
-            j++;
-        }
-        string word = str.substr(i, j - i - 1);
-        if (word.length() >= 5) {
-            reverse(word.begin(), word.end());
-        }
-        result += word + " ";
-        i = j;
     }
-    return result;
+
+    if (word.length() >= 5) {
+        reverse(word.begin(), word.end());
+    }
+    output += word;
+
+    return output;
 }
 
 int main() {
-    // You can test your function here
-    string str;
-    cout << "Enter a sentence: ";
-    getline(cin, str);
-    cout << spinWords(str) << endl;
+    cout << spinWords("a") << endl; // a
+    cout << spinWords("this is a test") << endl; // this is a test
+    cout << spinWords("this is another test") << endl; // this is rehtona test
+    cout << spinWords("hi") << endl; // hi
     return 0;
 }
