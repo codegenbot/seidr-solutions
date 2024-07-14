e3d8f7bc1c34a138d45ad376bff0175f67f3dd44
beep boop <githubbot@vadim.me>
2024-07-14 16:02:19 +0200

    Solution 84 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/7qq1gxjp Prompt: 3fbd9c41-e35a-406d-8eda-f975bec603af.cpp:1:19: error: ‘string’ was not declared in this scope     1 | bool solveBoolean(string s) {       |                   ^~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,35 +1,25 @@
 using namespace std;
 
 bool solveBoolean(string s) {
-    bool result = true;
+    stack<char> st;
     for (int i = 0; i < s.length(); i++) {
         if (s[i] == '&') {
-            while (i + 1 < s.length() && s[i+1] == '&') i++;
-            bool left = false, right = false;
-            if (i > 0) {
-                for (int j = i - 1; j >= 0; j--) {
-                    if (s[j] == '|') break;
-                    if (s[j] == 'T') left = true;
-                    else right = true;
-                }
-            } else {
-                left = true;
+            while (!st.empty() && st.top() == '&') {
+                st.pop();
             }
-            result &= left && right;
-        } else if (s[i] == '|') {
-            while (i + 1 < s.length() && s[i+1] == '|') i++;
-            bool left = false, right = false;
-            for (int j = i + 1; j < s.length(); j++) {
-                if (s[j] == '&') break;
-                if (s[j] == 'T') left = true;
-                else right = true;
+            if (st.empty()) return false;
+            else st.pop();
+        } 
+        else if (s[i] == '|') {
+            while (!st.empty() && st.top() == '|') {
+                st.pop();
             }
-            result |= left || right;
-        } else if (s[i] == 'T') {
-            result = true;
-        } else {
-            result = false;
+            if (st.empty()) return true;
+            else st.pop();
+        } 
+        else {
+            st.push(s[i]);
         }
     }
-    return result;
+    return !st.empty();
 }
