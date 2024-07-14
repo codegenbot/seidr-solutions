3fe8c633bba094baaa736535cc422e03c9fe17a5
beep boop <githubbot@vadim.me>
2024-07-14 05:20:44 +0200

    Solution 76 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ixsuoat1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/649850c9-43c2-45fb-9628-2c8b44c9b c4d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- solve-boolean.py
+++ solve-boolean.py
@@ -17,36 +17,36 @@ def solve_boolean(expression):
         expression = expression.replace("F|", "False | ")
         expression = expression.replace("f|", "False | ")
 
-    while "&&" in expression:
-        expression = expression.replace("T&T", "(True and True)")
-        expression = expression.replace("t&t", "(True and True)")
-        expression = expression.replace("F&F", "(False and False)")
-        expression = expression.replace("f&f", "(False and False)")
-        expression = expression.replace("&T", "(True and ")
-        expression = expression.replace("&t", "(True and ")
-        expression = expression.replace("&F", "(False and ")
-        expression = expression.replace("&f", "(False and ")
+    while '&&' in expression:
+        expression = expression.replace('T&T', '(True and True)')
+        expression = expression.replace('t&t', '(True and True)')
+        expression = expression.replace('F&F', '(False and False)')
+        expression = expression.replace('f&f', '(False and False)')
+        expression = expression.replace('&T', '(True and ')
+        expression = expression.replace('&t', '(True and ')
+        expression = expression.replace('&F', '(False and ')
+        expression = expression.replace('&f', '(False and ')
 
-    while "||" in expression:
-        expression = expression.replace("T|", "(True or ")
-        expression = expression.replace("t|", "(True or ")
-        expression = expression.replace("F|", "(False or ")
-        expression = expression.replace("f|", "(False or ")
+    while '||' in expression:
+        expression = expression.replace('T|', '(True or ')
+        expression = expression.replace('t|', '(True or ')
+        expression = expression.replace('F|', '(False or ')
+        expression = expression.replace('f|', '(False or ')
 
     if expression == "F":
         return False
     elif expression == "T":
         return True
 
-    while " | " in expression:
+    while ' | ' in expression:
         left, right = expression.split(" | ")
         if eval(left):
             expression = str(right)
         else:
             expression = "False"
 
-    while " and " in expression:
-        left, right = expression.split(" and ")
+    while ' and ' in expression:
+        left, right = expression.split(' and ')
         if eval(left):
             expression = str(right)
         else:
