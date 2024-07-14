023959992f4a7a2a12e88746e88b51475fc992be
beep boop <githubbot@vadim.me>
2024-07-14 06:41:41 +0200

    Solution 86 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/sfn2h6ki Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- paired-digits.cpp
+++ paired-digits.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int sumPairedDigits(string str) {
-    int sum = 0;
+    int current = 0, sum = 0;
     for(int i=0; i < str.length() - 1; i++) { 
-        if(str[i] == str[i+1]) { 
-            sum += '0' + str[i] + '0' + str[i+1]; 
+        current = current * 10 + (str[i] - '0'); 
+        if(str[i] == str[i+1]) {
+            sum += current;
+            current = 0;
         }
     }
     return sum;
@@ -14,9 +16,6 @@ int sumPairedDigits(string str) {
 int main() {
     string input;
     cin >> input; // read input from user
-    cout << "Sum of paired digits: " << sumPairedDigits(input) << endl; // output the result
-    if (input.length() < 2) {
-        cout << "Error: Input must be at least two characters long." << endl;
-    }
+    cout << sumPairedDigits(input) << endl; // output the result
     return 0;
 }
