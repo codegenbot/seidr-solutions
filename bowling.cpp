68382954c4d4a3e285f3ccccb3785f703a665340
beep boop <githubbot@vadim.me>
2024-07-14 15:57:44 +0200

    Solution 59 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/sc52960f Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,24 +1,36 @@
 int bowlingScore(string s) {
     int score = 0;
-    for(int i = 0; i < 10; i++) {
+    int frame = 0;
+    for(int i=0; i<s.length(); i++) {
         if(s[i] == 'X') {
-            score += 30;
-        } else if (s[i] == '/') {
-            int strike = 10 - (i + 1);
-            score += 10 + strike;
-        } else {
-            int currentFrameScore = 0;
-            for(int j = i; j < s.length(); j++) {
-                if(s[j] != '/') {
-                    currentFrameScore++;
-                    if(currentFrameScore > 2) break;
-                } else {
-                    score += currentFrameScore;
-                    currentFrameScore = 0;
-                    break;
+            score += 10 + (frame < 9 ? 10 : 0);
+            frame++;
+        } else if(s[i] == '/') {
+            int prevFrameScore = 0;
+            if(i > 1 && s[i-2] == 'X') {
+                prevFrameScore = 10;
+            } else if(i > 1) {
+                for(int j=i-1; j>=i-3; j--) {
+                    if(s[j] != '/') {
+                        prevFrameScore += (s[j] - '0');
+                        break;
+                    }
                 }
             }
+            score += prevFrameScore + (s[i] - '0') + (s[i+1] - '0');
+            i++;
+            frame++;
+        } else {
+            int currentFrameScore = 0;
+            for(int j=i; s[j] != '/' && j<s.length(); j++) {
+                currentFrameScore += (s[j] - '0');
+            }
             score += currentFrameScore;
+            if(frame < 9) {
+                frame++;
+            } else {
+                i = s.length();
+            }
         }
     }
     return score;
