29c765f9fc4b7144c294494941ee7bf4c0dd1b57
beep boop <githubbot@vadim.me>
2024-07-14 14:58:35 +0200

    Solution 81 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/z8czxzqc Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from cf089046-f4f8-430e-adb5-c4580f26a36a.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ cf089046-f4f8-430e-adb5-c4580f26a36a.cpp:7:16:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -1,18 +1,16 @@
+```cpp
 #include <string>
 #include <iostream>
-#include <sstream>
 
 std::string validateTweet(const std::string& input) {
     if (input.empty()) {
         return "You didn't type anything";
     }
-    size_t len = input.size();
+    size_t len = static_cast<size_t>(input.length());
     if (len > 140) {
         return "Too many characters";
     }
-    std::stringstream ss;
-    ss << "Your tweet has " << len << " characters";
-    return ss.str();
+    return "Your tweet has " + std::to_string(input.length()) + " characters";
 }
 
 int main() {
