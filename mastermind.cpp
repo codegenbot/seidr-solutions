3c58aef691fc2c3ed99d128cda51e8f666141651
beep boop <githubbot@vadim.me>
2024-07-14 05:03:15 +0200

    Solution 20 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/jf59eagv Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,36 +1,22 @@
-#include <iostream>
-using namespace std;
-
-int mastermind(const char* code, const string guess) {
+int mastermind(const std::string& code, const std::string& guess) {
     int white = 0;
     int black = 0;
 
-    for (char c : guess) {
-        bool foundInCode = false;
-        bool foundInCorrectPosition = false;
-
-        for (int i = 0; i < 4; ++i) {
-            if (code[i] == c) {
-                foundInCode = true;
-                if (i == guess.find(c)) {
-                    foundInCorrectPosition = true;
+    for (int i = 0; i < 4; ++i) {
+        if (code[i] == guess[i]) {
+            black++;
+        }
+        else if (guess[i] != ' ') { 
+            bool found = false;
+            for (int j = 0; j < 4; ++j) {
+                if (code[j] == guess[i] && i != j) {
+                    found = true;
                     break;
                 }
             }
+            if (!found) white++;
         }
-
-        if (!foundInCorrectPosition && foundInCode) white++;
-        else if (foundInCode) black++;
     }
 
     return black;
-}
-
-int main() {
-    const char* code = "ABCD";
-    string guess = "ABDC";
-    int result = mastermind(code, guess);
-    cout << "White pegs: " << 1 << endl;
-    cout << "Black pegs: " << result << endl;
-    return 0;
 }
