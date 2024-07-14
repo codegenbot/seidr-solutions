26ecb0f2457afbb538259131480db5444eedb992
beep boop <githubbot@vadim.me>
2024-07-14 02:40:31 +0200

    Solution 7 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/hpamfn8o Prompt: aa4c42c8-6ddd-47a1-bb45-cbc019fbfa78.cpp:1:21: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                     ^~~~~~ aa4c42c8-6ddd-47a1-bb45-cbc019fbfa78.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int mastermind(std::string code, std::string guess) { aa4c42c8-6ddd-47a1-bb45-cbc019fbfa78.cpp:1:39: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                                       ^~~~~~ aa4c42c8-6ddd-47a1-bb45-cbc019fbfa78.cpp:1:39: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? aa4c42c8-6ddd-47a1-bb45-cbc019fbfa78.cpp:1:51: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(std::string code, std::string guess) {       |                                                   ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,19 +1,25 @@
+#include <string>
+
 int mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
 
+    // Check for black pegs (correct color, correct place)
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             black++;
-        } else {
-            int count = 0;
-            for (char d : code) {
-                if (d == guess[i] && d != code[count % 4]) {
-                    white++;
-                    break;
-                }
-                count++;
-            }
+        }
+    }
+
+    // Check for white pegs (correct color, wrong place)
+    int codeCount[6] = {0};
+    for (char c : code) {
+        codeCount[c - 'A']++;
+    }
+    for (char c : guess) {
+        if (code.find(c) != std::string::npos && codeCount[c - 'A'] > 0) {
+            white++;
+            codeCount[c - 'A']--;
         }
     }
 
