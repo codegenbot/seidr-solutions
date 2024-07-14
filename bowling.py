39044cd4505fbc2aa7b08d9459869972d31808cf
beep boop <githubbot@vadim.me>
2024-07-14 18:03:56 +0200

    Solution 81 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/g31gwpdx Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,21 +1,33 @@
 ```
-def bowling_score(rolls):
+def bowling_score(game):
     score = 0
-    roll_index = 0
-    for frame in range(10):
-        if rolls[roll_index] == 'X':
-            score += 30
-            roll_index += 1
-        elif re.match('^-', rolls[roll_index]):
-            score += 10 - int(rolls[roll_index][1])
-            roll_index += 1
-        else:
-            first_roll = int(rolls[roll_index])
-            roll_index += 1
-            if len(rolls) > roll_index and rolls[roll_index] != '/':
-                second_roll = int(rolls[roll_index])
-                score += first_roll + second_roll
-                roll_index += 1
-            else:
-                score += first_roll
+    frame = 1
+    for i in range(len(game)):
+        if game[i].isdigit():
+            strike = False
+            spare = False
+            while i < len(game) and (game[i].isdigit() or game[i] == '/'):
+                if game[i] == '/':
+                    break
+                if frame < 10:
+                    score += int(game[i])
+                    if score >= 10:
+                        score -= 10
+                        break
+                else:
+                    score += int(game[i])
+            if i < len(game) - 1 and game[i+1].isdigit() and not strike and not spare:
+                if frame < 10:
+                    score += int(game[i+1])
+                else:
+                    score += int(game[i+1]) // 2
+            if game[i] == 'X':
+                strike = True
+                while i < len(game) - 2 and (game[i].isdigit() or game[i] == '/'):
+                    i += 1
+            elif game[i] == '-':
+                spare = True
+                while i < len(game) - 1 and (game[i].isdigit() or game[i] == '/'):
+                    i += 1
+        frame += 1
     return score
