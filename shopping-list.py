0f99a6355d7994c7ae76e829834de30fbbc88dba
beep boop <githubbot@vadim.me>
2024-07-14 03:40:19 +0200

    Solution 36 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/hfl8rnx0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4a4a90f4-75e1-4381-a612-c9d5ceb1d c78.py", line 8, in <module>     print(shopping_list())           ^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4a4a90f4-75e1-4381-a612-c9d5ceb1d c78.py", line 3, in shopping_list     discounts = list(map(int, input().split()))                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: '29.52'

--- shopping-list.py
+++ shopping-list.py
@@ -1,7 +1,7 @@
 def shopping_list():
-    prices = list(map(float, input().split()))
-    discounts = [int(x) if '.' not in x else float(x) for x in input().split()]
-    total_price = sum((1 - d / 100) * p for p, d in zip(prices, discounts))
+    items = list(map(float, input().split()))
+    discounts = list(map(int, (x.split('.')[0] for x in input().split())))
+    total_price = sum((1 - d / 100) * p for p, d in zip(items, discounts))
     return round(total_price, 2)
 
 print(shopping_list())
