6eecfce4a9822173da8e2806b84edf055df3a061
beep boop <githubbot@vadim.me>
2024-07-14 16:10:19 +0200

    Solution 97 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/sc52960f Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,26 +1,21 @@
-int bowlingScore(string input) {
+int bowlingScore(string s) {
     int score = 0;
-    bool lastRollInFrame = false;
-    int frame = 0;
-
-    for (char c : input) {
-        if (c == 'X') {
-            score += 10 + (lastRollInFrame ? 10 : 0);
-            lastRollInFrame = true;
-        } else if (c == '/') {
-            int firstRoll = (input[frame*2] - '0') * 10 + (input[frame*2+1] - '0');
-            score += firstRoll;
-            lastRollInFrame = true;
-        } else {
-            int roll = (c - '0') * 10 + (input[2*frame+1] - '0');
-            if (lastRollInFrame) {
-                score += roll;
-            } else {
-                score += min(roll, 10);
-                frame++;
+    for (int i = 0; i < 10; ++i) {
+        if (s[i] == '-') continue;
+        else if (s[i] == 'X') {
+            score += 30;
+            if (i < 9) score += bowlingScore(s.substr(i + 1)) - 15;
+        }
+        else if (isdigit(s[i])) {
+            int balls = 0;
+            for (int j = i; j < s.length() && balls < 3; ++j) {
+                if (s[j] == '/') break;
+                balls++;
             }
+            string ballsStr = s.substr(i, balls);
+            score += (ballsStr[0] - '0') * 10 + (ballsStr[1] - '0');
+            i += balls - 1;
         }
     }
-
     return score;
 }
