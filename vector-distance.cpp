56138d77c868b9cec3eda715289bae7e55369d26
beep boop <githubbot@vadim.me>
2024-07-14 18:34:10 +0200

    Solution 70 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/7xf2j43f Prompt: be71d327-7df3-412d-a538-f4eb12a2a9d4.cpp: In function ‘double calculateEuclidean Distance(const std::vector<float>&, const std::vector<float>&)’: be71d327-7df3-412d-a538-f4eb12a2a9d4.cpp:9:21: error: ‘pow’ was not declared in this scope     9 |         distance += pow(v1[i] - v2[i], 2);       |                     ^~~ be71d327-7df3-412d-a538-f4eb12a2a9d4.cpp:11:12: error: ‘sqrt’ was not declared i n this scope    11 |     return sqrt(distance);       |            ^~~~ be71d327-7df3-412d-a538-f4eb12a2a9d4.cpp: In function ‘int main()’: be71d327-7df3-412d-a538-f4eb12a2a9d4.cpp:28:22: error: ‘setprecision’ was not de clared in this scope    28 |     cout << fixed << setprecision(10) << calculateEuclideanDistance(v1, v2) << endl;       |                      ^~~~~~~~~~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -5,30 +5,29 @@
 
 using namespace std;
 
-double vectorDistance(vector<float> v1, vector<float> v2) {
-    double sum = 0;
+double calculateEuclideanDistance(const vector<float>& v1, const vector<float>& v2) {
+    double distance = 0;
     for (int i = 0; i < v1.size(); i++) {
-        sum += pow(v1[i] - v2[i], 2);
+        distance += pow(v1[i] - v2[i], 2);
     }
-    return sqrt(sum);
+    return sqrt(distance);
 }
 
 int main() {
     int n;
     cin >> n;
 
-    vector<float> v1(n), v2(n);
-
-    for (int i = 0; i < n; i++) {
-        cin >> v1[i];
+    vector<float> v1(n);
+    for (float& val : v1) {
+        cin >> val;
     }
 
-    for (int i = 0; i < n; i++) {
-        cin >> v2[i];
+    vector<float> v2(n);
+    for (float& val : v2) {
+        cin >> val;
     }
 
-    double distance = vectorDistance(v1, v2);
-    cout << fixed << setprecision(10) << distance << endl;
+    cout << fixed << setprecision(10) << calculateEuclideanDistance(v1, v2) << endl;
 
     return 0;
 }
