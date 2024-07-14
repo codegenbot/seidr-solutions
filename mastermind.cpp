6c4f8429a199169896dfed2f802d524d0e53dc44
beep boop <githubbot@vadim.me>
2024-07-14 05:13:28 +0200

    Solution 86 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/hpamfn8o Prompt: /usr/include/c++/11/bits/stl_map.h:100:11: note:   ‘std::map’   100 |     class map       |           ^~~ In file included from 26a41565-24e1-444f-8246-a55965cb167f.cpp:2: /usr/include/c++/11/map:78:13: note:   ‘std::pmr::map’    78 |       using map       |             ^~~ 26a41565-24e1-444f-8246-a55965cb167f.cpp:14:9: error: expected primary-expressio n before ‘char’    14 |     map<char,int> codeCount;       |         ^~~~ 26a41565-24e1-444f-8246-a55965cb167f.cpp:15:5: error: ‘codeCount’ was not declar ed in this scope    15 |     codeCount['A'] = 0;       |     ^~~~~~~~~ 26a41565-24e1-444f-8246-a55965cb167f.cpp:23:9: error: expected primary-expressio n before ‘char’    23 |     map<char,int> guessCount;       |         ^~~~ 26a41565-24e1-444f-8246-a55965cb167f.cpp:24:5: error: ‘guessCount’ was not decla red in this scope    24 |     guessCount['A'] = 0;       |     ^~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -11,7 +11,7 @@ int mastermind(std::string code, std::string guess) {
         }
     }
 
-    std::map<char,int> codeCount;
+    map<char,int> codeCount;
     codeCount['A'] = 0;
     codeCount['B'] = 0;
     codeCount['C'] = 0;
@@ -20,7 +20,7 @@ int mastermind(std::string code, std::string guess) {
         codeCount[c]++;
     }
     
-    std::map<char,int> guessCount;
+    map<char,int> guessCount;
     guessCount['A'] = 0;
     guessCount['B'] = 0;
     guessCount['C'] = 0;
