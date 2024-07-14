b37bb6c72c3679c3678cc772b48320e1bfc52761
beep boop <githubbot@vadim.me>
2024-07-14 16:09:26 +0200

    Solution 92 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/6591te97 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,24 +1,25 @@
-```
-def bowling_game(s):
+def bowling_score(game):
     score = 0
-    frame = 1
-    while frame <= 10:
-        if '/' in s:
-            s = s.split('/')
-            strike = int(s[0])
-            spare = int(s[1])
-            score += strike + spare
-            s = ''
-            frame += 1
-        elif len(s) > 0 and (s[0] == 'X' or str(int(s[0])) != s[0]):
-            if len(s) > 1:
-                s = s[1:]
-            else:
-                score += 10
-                frame += 1
+    for i in range(10):
+        if game[i].isdigit():
+            score += int(game[i])
+        elif "/" in game[i]:
+            strike_or_spare = True
+            strike_count = 1
+            while i < len(game) - 1 and "/" in game[i + 1 :]:
+                if game[i + 1 :].count("/") > 1:
+                    break
+                i += 1
+                strike_count += 1
+            if strike_count == 2:
+                score += int(game[i].split("/")[0]) + int(game[i].split("/")[1])
+            elif strike_count == 3:
+                score += 10 + max(int(x) for x in game[i + 1 : i + 3].replace("/", ""))
         else:
-            if len(s) >= 2:
-                s = s[:-1]
-                score += int(s[-1])
-            frame += 1
+            spare_or_strike = game[i]
+            if spare_or_strike in "X":
+                score += 10
+            elif spare_or_strike in "/":
+                score += int(spare_or_strike.split("/")[0])
+            i += len(spare_or_strike) - 1
     return score
