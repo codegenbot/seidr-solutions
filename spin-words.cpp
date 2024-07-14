bee5fba0d5462bf6a12dd6cde380015641b15133
beep boop <githubbot@vadim.me>
2024-07-14 20:00:24 +0200

    Solution 87 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/oze1uxoq Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,43 +1,30 @@
 #include <string>
-#include <vector>
+using namespace std;
 
-std::string spinWords(std::string str) {
-    std::vector<std::string> words = split(str);
-    for (int i = 0; i < words.size(); i++) {
-        if (words[i].length() >= 5)
-            words[i] = reverse(words[i]);
+string spinWords(string input) {
+    string output = "";
+    string temp = "";
+
+    for(int i = 0; i < input.length(); i++) {
+        char c = input[i];
+        if(c == ' ') {
+            if(temp.length() >= 5)
+                for(int j = temp.length()-1; j >= 0; j--)
+                    output += temp[j];
+            else
+                output += temp;
+            temp = "";
+        } 
+        else {
+            temp += c;
+        }
     }
-    return join(words);
-}
 
-std::vector<std::string> split(std::string str) {
-    std::vector<std::string> words;
-    int start = 0, end = 0;
-    while (end < str.length()) {
-        if (str[end] == ' ') {
-            words.push_back(str.substr(start, end - start));
-            end++;
-            start = end;
-        } else
-            end++;
-    }
-    words.push_back(str.substr(start, end - start));
-    return words;
-}
+    if(temp.length() >= 5)
+        for(int i = temp.length()-1; i >= 0; i--)
+            output += temp[i];
+    else
+        output += temp;
 
-std::string join(std::vector<std::string> words) {
-    std::string result;
-    for (int i = 0; i < words.size(); i++) {
-        result += words[i];
-        if (i != words.size() - 1)
-            result += ' ';
-    }
-    return result;
-}
-
-std::string reverse(std::string str) {
-    std::string result = "";
-    for (int i = str.length() - 1; i >= 0; i--)
-        result += str[i];
-    return result;
+    return output;
 }
