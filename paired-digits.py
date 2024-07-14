2f5cc583863e56b56f0dd59866ffd9057648222c
beep boop <githubbot@vadim.me>
2024-07-14 03:09:42 +0200

    Solution 26 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/1a4sgtf4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0a827731-4c39-40d2-a44f-7fd7b41d3 965.py", line 1     ```     ^ SyntaxError: invalid syntax

--- paired-digits.py
+++ paired-digits.py
@@ -1,10 +1,2 @@
 def paired_digits(s):
-    if __name__ == "__main__":
-        s = input("Enter a string of digits: ")
-        print(
-            sum(
-                int(d)
-                for i, d in enumerate(s)
-                if i < len(s) - 1 and int(d) == int(s[i + 1])
-            )
-        )
+    return sum(int(d) for i, d in enumerate(s) if i < len(s)-1 and int(d) == int(s[i+1]))
