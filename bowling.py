78c4e39bbcc4ce37c696777875f05047086b5a43
beep boop <githubbot@vadim.me>
2024-07-14 09:59:24 +0200

    Solution 74 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/92dz33yi Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6d6afff8-d585-4aba-aa2f-7beb515cd f1e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -3,14 +3,14 @@ def bowling_score(game):
     score = 0
     rolls = game.split('/')
     for i in range(10):
-        if len(rolls[i]) == '1':
+        if len(rolls[i]) == 1:
             score += int(rolls[i])
-        elif len(rolls[i]) > '1' and (i < '9' or (i == '9' and len(rolls['8']) != '2')):
+        elif len(rolls[i]) > 1 and (i < 9 or (i == 9 and len(rolls[8]) != 2)):
             score += 10 + int(rolls[i][0])
         else:
             first_roll = int(rolls[i][0])
             second_roll = int(rolls[i][1])
-            if first_roll + second_roll < '10':
+            if first_roll + second_roll < 10:
                 score += first_roll + second_roll
             else:
                 score += 10 + second_roll
