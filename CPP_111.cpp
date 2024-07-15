ef7f93f13c140c9a95059ce6cc53f5861d97739b
beep boop <githubbot@vadim.me>
2024-07-15 17:55:34 +0200

    Solution 26 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/t0q5kcc0 Prompt:       |                             ^~~ db698682-07ec-4be5-a807-2424dc63df22.cpp:24:33: error: expected primary-expressi on before ‘char’    24 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ db698682-07ec-4be5-a807-2424dc63df22.cpp:24:38: error: expected primary-expressi on before ‘int’    24 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ db698682-07ec-4be5-a807-2424dc63df22.cpp:24:44: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from db698682-07ec-4be5-a807-2424dc63df22.cpp:23: db698682-07ec-4be5-a807-2424dc63df22.cpp: In function ‘int main()’: db698682-07ec-4be5-a807-2424dc63df22.cpp:38:20: error: ‘histogram’ was not decla red in this scope    38 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ db698682-07ec-4be5-a807-2424dc63df22.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -10,7 +10,25 @@ bool issame(map<char, int> a, map<char, int> b) {
 }
 
 map<char, int> histogram(string test) {
-    // Your existing code for the histogram function
+    map<char,int> result;
+    istringstream iss(test);
+    string word;
+    while(iss >> word){
+        for(char c : word){
+            result[c]++;
+        }
+    }
+    int maxCount = 0;
+    for(auto it : result){
+        maxCount = max(maxCount, it.second);
+    }
+    map<char,int> res;
+    for(auto it : result){
+        if(it.second == maxCount){
+            res[it.first] = it.second;
+        }
+    }
+    return res;
 }
 
 int main() {
