8f05f707fbdc3fb57091c62e98c026cdd6b43ab7
beep boop <githubbot@vadim.me>
2024-07-15 19:40:36 +0200

    Solution 73 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/iw84ztid Prompt:       |                    ^~~~~ e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:11: e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp: In function ‘int main()’: e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -10,23 +10,25 @@ vector<float> get_positive(vector<float> l){
     return result;
 }
 
-bool is_same(vector<float> a, vector<float> b) {
-    if (a.size() != b.size()) {
+bool is_same(vector<float> a, vector<float> b){
+    if(a.size() != b.size()){
         return false;
     }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]){
             return false;
         }
     }
     return true;
 }
 
-int main() {
-    vector<float> list1 = {1.5, -2.3, 3.7, -4.1};
-    vector<float> list2 = {1.5, -2.3, 3.7, -4.1};
+int main(){
+    vector<float> list1 = {1.2, -3.4, 5.6, -7.8};
+    vector<float> list2 = {1.2, 5.6};
 
-    bool result = is_same(list1, list2);
+    vector<float> positive_nums = get_positive(list1);
+
+    bool same = is_same(list1, list2);
 
     return 0;
 }
