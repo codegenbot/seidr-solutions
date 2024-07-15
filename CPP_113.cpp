e2862e1d7cb6b06883b56f5b7c0daeaa2a86c5d3
beep boop <githubbot@vadim.me>
2024-07-15 21:02:57 +0200

    Solution 55 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/li5so7yb Prompt:       |                                            ^ 3cb62db5-a40f-41d0-9fd2-c55d4c4d48a1.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3cb62db5-a40f-41d0-9fd2-c55d4c4d48a1.cpp:15: 3cb62db5-a40f-41d0-9fd2-c55d4c4d48a1.cpp: In function ‘int main()’: 3cb62db5-a40f-41d0-9fd2-c55d4c4d48a1.cpp:25:20: error: ‘odd_count’ was not decla red in this scope    25 |     assert (issame(odd_count({"271", "137", "314"}) , {       |                    ^~~~~~~~~ 3cb62db5-a40f-41d0-9fd2-c55d4c4d48a1.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(odd_count({"271", "137", "314"}) , {       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    26 |         "the number of odd elements 2n the str2ng 2 of the 2nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    27 |         "the number of odd elements 3n the str3ng 3 of the 3nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    28 |         "the number of odd elements 2n the str2ng 2 of the 2nput."       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    29 |     }));       |     ~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -1,9 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
-
 bool issame(vector<string> a, vector<string> b){
     return a == b;
 }
@@ -22,13 +16,4 @@ vector<string> odd_count(vector<string> lst){
         result.push_back("the number of odd elements " + to_string(count) + " in the string " + s + " of the input.");
     }
     return result;
-}
-
-int main() {
-    vector<string> input = {"12345", "6789", "2468"};
-    vector<string> output = odd_count(input);
-    for(string s : output){
-        cout << s << endl;
-    }
-    return 0;
 }
