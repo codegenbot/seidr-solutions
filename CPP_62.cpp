d9084f067801e94090ded3b92ef288129d262301
beep boop <githubbot@vadim.me>
2024-07-15 20:22:56 +0200

    Solution 86 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/xqtjn5h3 Prompt:       |                    ^~~~~ e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp:10:29: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp:10:43: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp:9: e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp: In function ‘int main()’: e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp:19:20: error: ‘derivative’ was not decl ared in this scope    19 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~ e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -5,21 +5,14 @@ bool issame(vector<float> a, vector<float> b);
 
 vector<float> derivative(vector<float> xs);
 
-int main() {
-    assert(issame({1.0, 2.0, 3.0}, {1.0, 2.0, 3.0}));
-    assert(!issame({1.0, 2.0, 3.0}, {1.0, 2.0, 4.0}));
-
-    return 0;
-}
-
-bool issame(vector<float> a, vector<float> b) {
+bool issame(vector<float> a, vector<float> b){
     return a == b;
 }
 
-vector<float> derivative(vector<float> xs) {
+vector<float> derivative(vector<float> xs){
     vector<float> result;
-    for (int i = 1; i < xs.size(); i++) {
-        result.push_back(xs[i] - xs[i - 1]);
+    for(int i = 1; i < xs.size(); i++){
+        result.push_back(xs[i] * i);
     }
     return result;
 }
