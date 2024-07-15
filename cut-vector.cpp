251de8f6604491f9d6826c079186b357cb3a6d17
beep boop <githubbot@vadim.me>
2024-07-15 21:28:07 +0200

    Solution 62 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/r9npe7x9 Prompt: 3fa61df7-592c-45f9-8326-a10b9bc80f75.cpp: In function ‘std::vector<std::vector<i nt> > cutVector(std::vector<int>)’: 3fa61df7-592c-45f9-8326-a10b9bc80f75.cpp:50:16: error: expected ‘}’ at end of in put    50 |     return res;       |                ^ 3fa61df7-592c-45f9-8326-a10b9bc80f75.cpp:5:46: note: to match this ‘{’     5 | vector<vector<int>> cutVector(vector<int> v) {       |                                              ^

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,4 +1,4 @@
-```
+```cpp
 #include <vector>
 using namespace std;
 
@@ -6,22 +6,56 @@ vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
     
-    for(int i = 0; i < n-1; i++) {
-        if(abs(v[i] - v[0]) <= abs(v[n-1] - v[i])) {
-            res[0].clear();
-            for(int j = 0; j <= i; j++)
-                res[0].push_back(v[j]);
-            break;
-        }
+    for(int i = 0; i < n; i++) {
+        if(i == 0 || i == n-1)
+            res[0].push_back(v[i]);
         else {
-            res[0].clear();
-            for(int j = i+1; j < n; j++)
-                res[0].push_back(v[j]);
-            break;
+            if(abs(v[0] - v[i]) <= abs(v[n-1] - v[i])) {
+                res[0].clear();
+                for(int j = 0; j < i; j++)
+                    res[0].push_back(v[j]);
+                res[0].push_back(v[i]);
+                break;
+            }
+            else {
+                res[0].clear();
+                for(int j = i; j < n; j++)
+                    res[0].push_back(v[j]);
+                break;
+            }
         }
     }
     
-    res[1] = vector<int>(v.begin() + res[0].size(), v.end());
+    if(res[0].size() != n) {
+        int diff1 = INT_MAX, diff2 = INT_MAX;
+        int pos1 = -1, pos2 = -1;
+        
+        for(int i = 0; i < n; i++) {
+            if(i == 0 || i == n-1)
+                res[1].push_back(v[i]);
+            else {
+                int temp1 = abs(v[0] - v[i]);
+                int temp2 = abs(v[n-1] - v[i]);
+                
+                if(temp1 <= temp2 && (temp1 < diff1 || pos1 == -1)) {
+                    diff1 = temp1;
+                    pos1 = i;
+                }
+                else if(temp2 < diff2) {
+                    diff2 = temp2;
+                    pos2 = i;
+                }
+            }
+        }
+        
+        res[0].clear();
+        for(int i = 0; i <= pos1; i++)
+            res[0].push_back(v[i]);
+        
+        res[1].clear();
+        for(int i = pos1 + 1; i < n; i++)
+            res[1].push_back(v[i]);
+    }
     
     return res;
-}``
+}
