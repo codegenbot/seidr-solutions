a44f71255008d742215feb3f196f69c7ada54388
beep boop <githubbot@vadim.me>
2024-07-15 14:31:06 +0200

    Solution 14 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/q9cfj4za Prompt: In file included from /usr/include/c++/11/vector:67,                  from 2bffdf2e-ca9e-4a91-90ec-8873d10f4c06.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 2bffdf2e-ca9e-4a91-90ec-8873d10f4c06.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 2bffdf2e-ca9e-4a91-90ec-8873d10f4c06.cpp:42:34: error: expected primary-expressi on before ‘int’    42 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 2bffdf2e-ca9e-4a91-90ec-8873d10f4c06.cpp:41: 2bffdf2e-ca9e-4a91-90ec-8873d10f4c06.cpp: In function ‘int main()’: 2bffdf2e-ca9e-4a91-90ec-8873d10f4c06.cpp:51:20: error: ‘pluck’ was not declared in this scope    51 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 2bffdf2e-ca9e-4a91-90ec-8873d10f4c06.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,10 +1,23 @@
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
 vector<int> pluck(vector<int> arr);
 
-bool issame(vector<int> a, vector<int> b);
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
 
 vector<int> pluck(vector<int> arr){
     vector<int> result;
@@ -26,8 +39,8 @@ vector<int> pluck(vector<int> arr){
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+int main() {
+    assert(issame(pluck({7, 9, 7, 1}), {}));
+    
+    return 0;
 }
-
-assert (issame(pluck({7, 9, 7, 1}), {}));
