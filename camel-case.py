d8f882c2a8584e0a1f8fb090c6bc1b9d96dd828e
beep boop <githubbot@vadim.me>
2024-07-15 14:20:40 +0200

    Solution 45 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/ao9xmky7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f6c37cd9-0b8b-4841-b7b3-0dbf369d3 c23.py", line 1     ```     ^ SyntaxError: invalid syntax

--- camel-case.py
+++ camel-case.py
@@ -1,3 +1,4 @@
+```
 def camel_case(s):
     result = ""
     for i, word in enumerate(map(str.split, s.replace("-", " ").split()), 1):
@@ -5,4 +6,4 @@ def camel_case(s):
             result += word[0].upper() + word[1:].lower()
         else:
             result += word.capitalize()
-    return result
+    return result```
