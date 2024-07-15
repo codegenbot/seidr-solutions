a0549190614a9cdce38d4fed7cb3cae2a0e7c166
beep boop <githubbot@vadim.me>
2024-07-15 16:52:05 +0200

    Solution 25 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/th9qyyvm Prompt:    25 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 1bb4b05b-7d28-437b-93aa-f6f7bc7cd3f8.cpp:25:36: error: expected primary-expressi on before ‘float’    25 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 1bb4b05b-7d28-437b-93aa-f6f7bc7cd3f8.cpp:25:43: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 1bb4b05b-7d28-437b-93aa-f6f7bc7cd3f8.cpp:24: 1bb4b05b-7d28-437b-93aa-f6f7bc7cd3f8.cpp: In function ‘int main()’: 1bb4b05b-7d28-437b-93aa-f6f7bc7cd3f8.cpp:34:20: error: ‘sort_even’ was not decla red in this scope    34 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 1bb4b05b-7d28-437b-93aa-f6f7bc7cd3f8.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,32 +1,17 @@
 vector<float> sort_even(vector<float> l) {
     vector<float> even_values;
-    vector<float> sorted_even_values;
 
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) {
-            even_values.push_back(l[i]);
-            sorted_even_values.push_back(l[i]);
-        }
+    for (int i = 0; i < l.size(); i += 2) {
+        even_values.push_back(l[i]);
     }
 
-    sort(sorted_even_values.begin(), sorted_even_values.end());
+    sort(even_values.begin(), even_values.end());
 
-    int sorted_even_index = 0;
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) {
-            l[i] = sorted_even_values[sorted_even_index];
-            sorted_even_index++;
-        }
+    int even_index = 0;
+    for (int i = 0; i < l.size(); i += 2) {
+        l[i] = even_values[even_index];
+        even_index++;
     }
 
     return l;
-}
-
-int main() {
-    vector<float> l = {3.5, 1.5, 4.2, 2.1, 5.8};
-    vector<float> expected_output = {1.5, 3.5, 2.1, 4.2, 5.8};
-    
-    assert(sort_even(l) == expected_output);
-    
-    return 0;
 }
