585769de661cfde684342c813fb6e4c9a049004a
beep boop <githubbot@vadim.me>
2024-07-15 21:27:54 +0200

    Solution 5 of CPP/141, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/u44xafca Prompt: Complete the following code given the task description and function signature.

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,31 +1,29 @@
-int count_digits = 0;
-    int dot_index = -1;
-    
-    for (int i = 0; i < file_name.length(); i++) {
-        if (isdigit(file_name[i])) {
-            count_digits++;
-        } else if (file_name[i] == '.') {
-            if (dot_index != -1) {
-                return "No";
+#include <string>
+using namespace std;
+
+string file_name_check(string file_name) {
+    int digits = 0;
+    int dot_count = 0;
+    string substr_before_dot, substr_after_dot;
+
+    for (char c : file_name) {
+        if (c >= '0' && c <= '9') {
+            digits++;
+        } else if (c == '.') {
+            dot_count++;
+        } else {
+            if (dot_count == 0) {
+                substr_before_dot += c;
+            } else {
+                substr_after_dot += c;
             }
-            dot_index = i;
         }
     }
 
-    if (count_digits > 3 || dot_index == -1 || dot_index == 0 || dot_index == file_name.length() - 1) {
+    if (digits <= 3 && dot_count == 1 && substr_before_dot.length() > 0 &&
+        isalpha(substr_before_dot[0]) && (substr_after_dot == "txt" || substr_after_dot == "exe" || substr_after_dot == "dll")) {
+        return "Yes";
+    } else {
         return "No";
     }
-
-    string before_dot = file_name.substr(0, dot_index);
-    string after_dot = file_name.substr(dot_index + 1);
-
-    if (!isalpha(before_dot[0])) {
-        return "No";
-    }
-
-    if (after_dot != "txt" && after_dot != "exe" && after_dot != "dll") {
-        return "No";
-    }
-
-    return "Yes";
 }
