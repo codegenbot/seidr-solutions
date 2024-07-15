0f5fac948802c8b19927bed70c1ad60585303cca
beep boop <githubbot@vadim.me>
2024-07-15 18:31:14 +0200

    Solution 50 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/f5o10j3a Prompt: ing)){       |   ^~~~ a1b47c1a-98f0-496d-b2ca-170e0dabebae.cpp:25:3: error: expected unqualified-id be fore ‘else’    25 | } else {       |   ^~~~ In file included from a1b47c1a-98f0-496d-b2ca-170e0dabebae.cpp:29: a1b47c1a-98f0-496d-b2ca-170e0dabebae.cpp: In function ‘int main()’: a1b47c1a-98f0-496d-b2ca-170e0dabebae.cpp:31:13: error: ‘boost’ has not been decl ared    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ a1b47c1a-98f0-496d-b2ca-170e0dabebae.cpp:31:29: error: ‘string’ was not declared  in this scope    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ a1b47c1a-98f0-496d-b2ca-170e0dabebae.cpp:31:37: error: ‘compare_one’ was not dec lared in this scope    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -9,7 +9,7 @@ boost::any compare_one(const boost::any& a, const boost::any& b) {
         } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
             return b;
         } else {
-            return "None";
+            return std::string("None");
         }
     } else if(a.type() == typeid(float) && b.type() == typeid(float)){
         if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
@@ -17,7 +17,7 @@ boost::any compare_one(const boost::any& a, const boost::any& b) {
         } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
             return b;
         } else {
-            return "None";
+            return std::string("None");
         }
     } else if(a.type() == typeid(std::string) && b.type() == typeid(std::string)){
         if(std::stof(boost::any_cast<std::string>(a)) > std::stof(boost::any_cast<std::string>(b))){
@@ -25,9 +25,9 @@ boost::any compare_one(const boost::any& a, const boost::any& b) {
         } else if(std::stof(boost::any_cast<std::string>(a)) < std::stof(boost::any_cast<std::string>(b))){
             return b;
         } else {
-            return "None";
+            return std::string("None");
         }
     } else {
-        return "None";
+        return std::string("None");
     }
 }
