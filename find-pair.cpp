274369e1a23ea2cc8e254577af7ac6f1709345a3
beep boop <githubbot@vadim.me>
2024-07-15 14:12:32 +0200

    Solution 24 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/jb9nedm1 Prompt:   +++ |+#include <iostream>     3 | 00a64930-23b7-49a2-be5a-b2a152112dc0.cpp:23:66: error: ‘endl’ was not declared i n this scope    23 |         cout << "The pair is: " << pair[0] << ", " << pair[1] << endl;       |                                                                  ^~~~ 00a64930-23b7-49a2-be5a-b2a152112dc0.cpp:3:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     2 | #include <unordered_map>   +++ |+#include <ostream>     3 | 00a64930-23b7-49a2-be5a-b2a152112dc0.cpp:25:9: error: ‘cout’ was not declared in  this scope    25 |         cout << "No such pair found." << endl;       |         ^~~~ 00a64930-23b7-49a2-be5a-b2a152112dc0.cpp:25:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 00a64930-23b7-49a2-be5a-b2a152112dc0.cpp:25:42: error: ‘endl’ was not declared i n this scope    25 |         cout << "No such pair found." << endl;       |                                          ^~~~ 00a64930-23b7-49a2-be5a-b2a152112dc0.cpp:25:42: note: ‘std::endl’ is defined in header ‘<ostream>’; did you forget to ‘#include <ostream>’?

--- find-pair.cpp
+++ find-pair.cpp
@@ -16,6 +16,18 @@ vector<int> findPair(vector<int>& nums, int target) {
     return {};
 }
 
+vector<int> findPair(vector<int>& nums, int target) {
+    unordered_map<int, int> numMap;
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (numMap.find(complement) != numMap.end()) {
+            return {complement, nums[i]};
+        }
+        numMap[nums[i]] = i;
+    }
+    return {};
+}
+
 int main() {
     vector<int> nums = {1, 2, 3, 4, 5};
     int target = 7;
