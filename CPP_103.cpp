6384c5b1e509ad8000db117e7b862cd745ecca27
beep boop <githubbot@vadim.me>
2024-07-15 17:48:21 +0200

    Solution 28 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/l730irx1 Prompt:       |               ^~~~~ 3e34a9f2-8515-425f-ade9-82f7c80b0da9.cpp:12:5: error: ‘string’ does not name a t ype    12 |     string binary_avg = "";       |     ^~~~~~ 3e34a9f2-8515-425f-ade9-82f7c80b0da9.cpp:13:5: error: expected unqualified-id be fore ‘while’    13 |     while (avg > 0) {       |     ^~~~~ 3e34a9f2-8515-425f-ade9-82f7c80b0da9.cpp:18:5: error: expected unqualified-id be fore ‘return’    18 |     return binary_avg;       |     ^~~~~~ 3e34a9f2-8515-425f-ade9-82f7c80b0da9.cpp:19:1: error: expected declaration befor e ‘}’ token    19 | }       | ^ In file included from 3e34a9f2-8515-425f-ade9-82f7c80b0da9.cpp:21: 3e34a9f2-8515-425f-ade9-82f7c80b0da9.cpp: In function ‘int main()’: 3e34a9f2-8515-425f-ade9-82f7c80b0da9.cpp:23:13: error: ‘rounded_avg’ was not dec lared in this scope    23 |     assert (rounded_avg(5, 5) == "101");       |             ^~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
 #include <string>
-#include <bitset>
 #include <cmath>
 
-using namespace std;
-
-string rounded_avg(int n, int m) {
+std::string find_binary_avg(int n, int m) {
     if (n > m) {
         return "-1";
     }
-
+    
     int sum = 0;
-    for (int i = n; i <= m; i++) {
+    for (int i = n; i <= m; ++i) {
         sum += i;
     }
-
+    
     int avg = round((double)sum / (m - n + 1));
-    string binary_avg = bitset<32>(avg).to_string();
-    size_t pos = binary_avg.find('1');
-    if (pos != string::npos) {
-        return binary_avg.substr(pos);
-    } else {
-        return "0";
+    
+    std::string binary_avg = "";
+    while (avg > 0) {
+        binary_avg = std::to_string(avg % 2) + binary_avg;
+        avg /= 2;
     }
+    
+    return binary_avg;
 }
 
 int main() {
-    assert(rounded_avg(5, 5) == "101");
+    int n, m;
+    std::cin >> n >> m;
+    
+    std::cout << find_binary_avg(n, m) << std::endl;
+    
     return 0;
 }
