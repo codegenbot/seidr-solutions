2450001716c582fc8e4e229eca11af4bf87b0fb5
beep boop <githubbot@vadim.me>
2024-07-16 00:06:22 +0200

    Solution 2 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/33hyua9b Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 989ff92c-a7bb-4fa8-88fd-999c63c882a4.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 989ff92c-a7bb-4fa8-88fd-999c63c882a4.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 989ff92c-a7bb-4fa8-88fd-999c63c882a4.cpp:11: 989ff92c-a7bb-4fa8-88fd-999c63c882a4.cpp: In function ‘int main()’: 989ff92c-a7bb-4fa8-88fd-999c63c882a4.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 989ff92c-a7bb-4fa8-88fd-999c63c882a4.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,3 +1,19 @@
-bool issame(vector<string>& a, vector<string>& b) {
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
+}
+
+vector<string> sort_and_filter_strings(vector<string> lst) {
+    lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s) { return s.length() % 2 != 0; }), lst.end());
+    sort(lst.begin(), lst.end(), [](const string& a, const string& b) {
+        if (a.length() == b.length()) {
+            return a < b;
+        }
+        return a.length() < b.length();
+    });
+    return lst;
+}
+
+int main() {
+    assert(issame(sort_and_filter_strings({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+    return 0;
 }
