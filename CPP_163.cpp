1dcfbf4a3c8a6f7cb30788572393effaeee200f3
beep boop <githubbot@vadim.me>
2024-07-15 19:11:26 +0200

    Solution 68 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/s1k7t141 Prompt: 9da8539a-521c-4acb-b769-1f29d208b897.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 9da8539a-521c-4acb-b769-1f29d208b897.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from 9da8539a-521c-4acb-b769-1f29d208b897.cpp:29: 9da8539a-521c-4acb-b769-1f29d208b897.cpp: In function ‘int main()’: 9da8539a-521c-4acb-b769-1f29d208b897.cpp:39:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    39 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 9da8539a-521c-4acb-b769-1f29d208b897.cpp:10:6: note: candidate: ‘bool issame(con st std::vector<int>&, const std::vector<int>&)’    10 | bool issame(const vector<int>& a, const vector<int>& b){       |      ^~~~~~ 9da8539a-521c-4acb-b769-1f29d208b897.cpp:30:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    30 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+vector<int> generate_integers(int a, int b);
+
+bool issame(const vector<int>& a, const vector<int>& b);
+
+bool issame(const vector<int>& a, const vector<int>& b){
+    return a == b;
+}
+
 vector<int> generate_integers(int a, int b) {
     vector<int> result;
     for (int i = a; i <= b; ++i) {
@@ -13,11 +21,7 @@ vector<int> generate_integers(int a, int b) {
     return result;
 }
 
-bool issame(const vector<int>& a, const vector<int>& b) {
-    return a == b;
-}
-
-int main() { 
-    assert(issame(generate_integers(17, 89), vector<int>{}));
+int main() {
+    assert(issame(generate_integers(17, 89), {}));
     return 0;
 }
