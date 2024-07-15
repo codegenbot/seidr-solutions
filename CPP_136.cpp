2e00d76342e74db8de174d18b69a7e87ecf7db69
beep boop <githubbot@vadim.me>
2024-07-15 18:22:42 +0200

    Solution 17 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ckn5i8fe Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 12b5c840-2ede-4e0f-a6c1-de41f4bad5f1.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 12b5c840-2ede-4e0f-a6c1-de41f4bad5f1.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 12b5c840-2ede-4e0f-a6c1-de41f4bad5f1.cpp:16: 12b5c840-2ede-4e0f-a6c1-de41f4bad5f1.cpp: In function ‘int main()’: 12b5c840-2ede-4e0f-a6c1-de41f4bad5f1.cpp:26:20: error: ‘largest_smallest_integer s’ was not declared in this scope    26 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 12b5c840-2ede-4e0f-a6c1-de41f4bad5f1.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -5,20 +5,20 @@ bool issame(std::vector<int> a, std::vector<int> b);
 
 std::vector<int> largest_smallest_integers(std::vector<int> lst);
 
-void annual_income(); 
-
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
+int main() {
+    // Code for main function goes here
 }
 
+void annual_income(); // Declaration of annual_income function should be before issame function
+
 std::vector<int> largest_smallest_integers(std::vector<int> lst){
-    int max_neg = 0, min_pos = 0;
+    int max_neg = INT_MIN, min_pos = INT_MAX;
     
     for(int num : lst){
-        if(num < 0 && num < max_neg){
+        if(num < 0 && num > max_neg){
             max_neg = num;
         }
-        if(num > 0 && (num < min_pos || min_pos == 0)){
+        if(num > 0 && num < min_pos){
             min_pos = num;
         }
     }
@@ -26,7 +26,6 @@ std::vector<int> largest_smallest_integers(std::vector<int> lst){
     return {max_neg, min_pos};
 }
 
-int main() {
-    // Main function content here
-    return 0;
+bool issame(std::vector<int> a, std::vector<int> b){
+    // Code for issame function
 }
