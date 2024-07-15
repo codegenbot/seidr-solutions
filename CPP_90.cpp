ca2c0396cd604d2c0d3dbc06d9113173737a7ad9
beep boop <githubbot@vadim.me>
2024-07-15 20:43:35 +0200

    Solution 65 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/gxvo1i1k Prompt:     4 |     for (int i = 1; i < lst.size(); ++i) {       |     ^~~ f3113cbb-4e1e-4436-b425-f133e58e7d5b.cpp:4:21: error: ‘i’ does not name a type     4 |     for (int i = 1; i < lst.size(); ++i) {       |                     ^ f3113cbb-4e1e-4436-b425-f133e58e7d5b.cpp:4:37: error: expected unqualified-id be fore ‘++’ token     4 |     for (int i = 1; i < lst.size(); ++i) {       |                                     ^~ f3113cbb-4e1e-4436-b425-f133e58e7d5b.cpp:13:5: error: expected unqualified-id be fore ‘return’    13 |     return -1;       |     ^~~~~~ f3113cbb-4e1e-4436-b425-f133e58e7d5b.cpp:14:1: error: expected declaration befor e ‘}’ token    14 | }       | ^ In file included from f3113cbb-4e1e-4436-b425-f133e58e7d5b.cpp:16: f3113cbb-4e1e-4436-b425-f133e58e7d5b.cpp: In function ‘int main()’: f3113cbb-4e1e-4436-b425-f133e58e7d5b.cpp:18:13: error: ‘next_smallest’ was not d eclared in this scope    18 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ^~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -19,6 +19,6 @@ int next_smallest(const std::vector<int>& lst) {
 }
 
 int main() {
-    assert (next_smallest({-35, 34, 12, -45}) == -35);
+    assert(next_smallest({-35, 34, 12, -45}) == -35);
     return 0;
 }
