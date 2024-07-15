55abea3bb54a40dc44152e09abbc019f9cd1c46e
beep boop <githubbot@vadim.me>
2024-07-15 14:09:06 +0200

    Solution 70 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/k5tuz6xw Prompt:  in this scope    16 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ d180acab-0833-4a1c-9f56-44ec58145032.cpp:16:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from d180acab-0833-4a1c-9f56-44ec58145032.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from d180acab-0833-4a1c-9f56-44ec58145032.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d180acab-0833-4a1c-9f56-44ec58145032.cpp:16:36: error: expected primary-expressi on before ‘float’    16 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from d180acab-0833-4a1c-9f56-44ec58145032.cpp:15: d180acab-0833-4a1c-9f56-44ec58145032.cpp: In function ‘int main()’: d180acab-0833-4a1c-9f56-44ec58145032.cpp:25:20: error: ‘get_positive’ was not de clared in this scope    25 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,11 +1,30 @@
 #include <vector>
+#include <iostream>
 #include <cassert>
 
+std::vector<float> get_positive(std::vector<float> v) {
+    std::vector<float> result;
+    for (float value : v) {
+        if (value > 0) {
+            result.push_back(value);
+        }
+    }
+    return result;
+}
+
 bool issame(std::vector<float> a, std::vector<float> b) {
-    return std::equal(a.begin(), a.end(), b.begin(), b.end());
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main() {
-    // Add appropriate code here or remove the assertion related to get_positive function
+    assert(issame(get_positive({}), {}));
     return 0;
 }
