45ce3df9eba493cad42d9807b6bab4ac96782793
beep boop <githubbot@vadim.me>
2024-07-15 13:18:32 +0200

    Solution 33 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/3kpfav16 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8623ca82-f4a6-49b3-b03c-287238a77 26c.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8623ca82-f4a6-49b3-b03c-287238a77 26c.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -2,8 +2,10 @@ def sort_third(l: list):
     return [
         (
             sorted([l[i], l[i + 1], l[i + 2]])
-            if len(l) % 3 != 1 and i + 2 < len(l)
-            else l[i]
+            if len(l) % 3 != 1 and len(l) % 3 != 2 and i + 2 < len(l)
+            else (
+                sorted([l[i], l[i + 1]]) if len(l) % 3 == 2 and i + 1 < len(l) else l[i]
+            )
         )
         for i in range(0, len(l), 3)
     ]
