2514238d356abbab894245015d6cd6ac3059ec1c
beep boop <githubbot@vadim.me>
2024-07-15 19:11:54 +0200

    Solution 55 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/jc6v89py Prompt:       |           ^~~~~~ In file included from d2646961-14d6-4bea-8720-f22284521168.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d2646961-14d6-4bea-8720-f22284521168.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d2646961-14d6-4bea-8720-f22284521168.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d2646961-14d6-4bea-8720-f22284521168.cpp:11: d2646961-14d6-4bea-8720-f22284521168.cpp: In function ‘int main()’: d2646961-14d6-4bea-8720-f22284521168.cpp:21:20: error: ‘eat’ was not declared in  this scope    21 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ^~~ d2646961-14d6-4bea-8720-f22284521168.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -11,7 +11,7 @@ vector<int> eat(int number, int need, int remaining) {
     int totalCarrots = number + need;
     int eatenCarrots = totalCarrots > remaining ? totalCarrots - remaining : totalCarrots;
     int leftCarrots = remaining > totalCarrots ? 0 : remaining - totalCarrots;
-    
+
     return {eatenCarrots, leftCarrots};
 }
 
