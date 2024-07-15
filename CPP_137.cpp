43fa32e83de807c543cb710304e9eed228635e03
beep boop <githubbot@vadim.me>
2024-07-15 18:22:30 +0200

    Solution 18 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/f5o10j3a Prompt:    17 | } else if (a.type() == typeid(string) && b.type() == typeid(string)) {       |   ^~~~ 6d9c3f62-04a4-452b-9090-d83fd4483a3e.cpp:25:3: error: expected unqualified-id be fore ‘else’    25 | } else {       |   ^~~~ In file included from 6d9c3f62-04a4-452b-9090-d83fd4483a3e.cpp:29: 6d9c3f62-04a4-452b-9090-d83fd4483a3e.cpp: In function ‘int main()’: 6d9c3f62-04a4-452b-9090-d83fd4483a3e.cpp:31:13: error: ‘boost’ has not been decl ared    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 6d9c3f62-04a4-452b-9090-d83fd4483a3e.cpp:31:29: error: ‘string’ was not declared  in this scope    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 6d9c3f62-04a4-452b-9090-d83fd4483a3e.cpp:31:37: error: ‘compare_one’ was not dec lared in this scope    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -4,6 +4,22 @@
 
 #include <string>
 
+if (a.type() == typeid(int) && b.type() == typeid(int)) {
+    if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
+        return a;
+    } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
+        return b;
+    } else {
+        return "None";
+    }
+} else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+    if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
+        return a;
+    } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
+        return b;
+    } else {
+        return "None";
+    }
 } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
     if (boost::any_cast<std::string>(a) > boost::any_cast<std::string>(b)) {
         return a;
@@ -12,4 +28,6 @@
     } else {
         return "None";
     }
+} else {
+    return "None";
 }
