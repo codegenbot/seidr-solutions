6a7c76e9f4dcaaa19b83b4601d64ee32047ba007
beep boop <githubbot@vadim.me>
2024-07-16 00:49:18 +0200

    Solution 55 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/yy3yfvcx Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 9d567c42-d58a-4ea9-811d-aaf089d1a86a.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 9d567c42-d58a-4ea9-811d-aaf089d1a86a.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 9d567c42-d58a-4ea9-811d-aaf089d1a86a.cpp:26: 9d567c42-d58a-4ea9-811d-aaf089d1a86a.cpp: In function ‘int main()’: 9d567c42-d58a-4ea9-811d-aaf089d1a86a.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 9d567c42-d58a-4ea9-811d-aaf089d1a86a.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -2,19 +2,19 @@
 #include <vector>
 #include <string>
 
+using namespace std;
+
 vector<string> separate_paren_groups(string paren_string);
 
 bool issame_vectors(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) {
         return false;
     }
-
     for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) {
             return false;
         }
     }
-
     return true;
 }
 
@@ -30,4 +30,29 @@ int main() {
     }
 
     return 0;
+}
+
+vector<string> separate_paren_groups(string paren_string) {
+    vector<string> result;
+    string group;
+    int count = 0;
+
+    for (char c : paren_string) {
+        if (c == '(') {
+            if (count > 0) {
+                group += c;
+            }
+            count++;
+        } else if (c == ')') {
+            count--;
+            if (count > 0) {
+                group += c;
+            } else if (count == 0) {
+                result.push_back(group);
+                group = "";
+            }
+        }
+    }
+
+    return result;
 }
