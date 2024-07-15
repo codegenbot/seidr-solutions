fe390cc290ffd1edb174382d574c42237d12ff15
beep boop <githubbot@vadim.me>
2024-07-15 15:25:53 +0200

    Solution 18 of CPP/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ep2ro3qu Prompt: 9031bbb1-33ea-43ca-9109-eb710ddbc650.cpp:21:5: error: ‘assert’ was not declared in this scope    21 |     assert(match_parens({")", "("}) == "Yes");       |     ^~~~~~ 9031bbb1-33ea-43ca-9109-eb710ddbc650.cpp:1:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?   +++ |+#include <cassert>     1 | string match_parens(vector<string> lst); 9031bbb1-33ea-43ca-9109-eb710ddbc650.cpp: At global scope: 9031bbb1-33ea-43ca-9109-eb710ddbc650.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 9031bbb1-33ea-43ca-9109-eb710ddbc650.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from 9031bbb1-33ea-43ca-9109-eb710ddbc650.cpp:25: 9031bbb1-33ea-43ca-9109-eb710ddbc650.cpp: In function ‘int main()’: 9031bbb1-33ea-43ca-9109-eb710ddbc650.cpp:27:13: error: ‘match_parens’ was not de clared in this scope    27 |     assert (match_parens({")", "("}) == "Yes" );       |             ^~~~~~~~~~~~

--- CPP_119.cpp
+++ CPP_119.cpp
@@ -1,21 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include <cassert>
-
-using namespace std;
 
 string match_parens(vector<string> lst);
 
-int main(){
-    assert(match_parens({")", "("}) == "Yes");
-    return 0;
-}
-
-string match_parens(vector<string> lst){
+string match_parens(vector<string> lst) {
     int cnt = 0;
-    for(string s : lst) {
-        for(char c : s) {
+    for (string s : lst) {
+        for (char c : s) {
             if (c == '(') {
                 cnt++;
             } else {
@@ -27,4 +17,9 @@ string match_parens(vector<string> lst){
         }
     }
     return cnt == 0 ? "Yes" : "No";
+}
+
+int main() {
+    assert(match_parens({")", "("}) == "Yes");
+    return 0;
 }
