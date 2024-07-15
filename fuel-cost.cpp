b677e8f24d4893214712d3b2964c2992ce8e94d9
beep boop <githubbot@vadim.me>
2024-07-15 23:23:19 +0200

    Solution 2 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/9jq9g96v Prompt:     4 |     while (cin >> n) {       |            ^~~ e8fd8dda-3490-49f1-b78d-be7b7930d36a.cpp:5:9: error: ‘nums’ was not declared in this scope     5 |         nums.push_back(n);       |         ^~~~ e8fd8dda-3490-49f1-b78d-be7b7930d36a.cpp:9:20: error: ‘nums’ was not declared in  this scope; did you mean ‘num’?     9 |     for (int num : nums) {       |                    ^~~~       |                    num e8fd8dda-3490-49f1-b78d-be7b7930d36a.cpp:10:16: error: ‘max’ was not declared in  this scope    10 |         sum += max(num / 3 - 2, 0);       |                ^~~ e8fd8dda-3490-49f1-b78d-be7b7930d36a.cpp:13:5: error: ‘cout’ was not declared in  this scope    13 |     cout << sum << endl;       |     ^~~~ e8fd8dda-3490-49f1-b78d-be7b7930d36a.cpp:13:20: error: ‘endl’ was not declared i n this scope    13 |     cout << sum << endl;       |                    ^~~~

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -7,16 +7,18 @@ using namespace std;
 int main() {
     vector<int> nums;
     int n;
+
     while (cin >> n) {
+        if (n == 0) break;
         nums.push_back(n);
     }
-    
+
     int sum = 0;
     for (int num : nums) {
         sum += (num / 3) - 2;
     }
-    
+
     cout << sum << endl;
-    
+
     return 0;
 }
