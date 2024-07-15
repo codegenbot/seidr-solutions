a34efbe49ab6aeaa74c72fb77e60d24d024f83e7
beep boop <githubbot@vadim.me>
2024-07-15 21:08:13 +0200

    Solution 50 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/yxhj599w Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2cc8634b-ed66-4589-b02a-f23ede91e 576.py", line 5     else chr(((ord(char.upper()) - ord('A') + 2) % 26) + ord('A')).swapcase() if  char.lower() not in vowels     ^^^^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -1,7 +1,9 @@
 def encode(message):
     vowels = set('aeiou')
     return ''.join([
-        char if not char.isalpha() else chr(((ord(char.upper()) - ord('A') + 2) % 26) + ord('A')).swapcase() if char.lower() not in vowels
+        char if not char.isalpha() or char.lower() in vowels
+        else chr(((ord(char.upper()) - ord('A') + 2) % 26) + ord('A')).swapcase()
+        if char.lower() not in vowels
         else chr(ord(char) ^ 32)
         for char in message
     ])
