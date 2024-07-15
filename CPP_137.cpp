717fd928e00519755bb74e91b1a738c595a7446a
beep boop <githubbot@vadim.me>
2024-07-15 13:40:06 +0200

    Solution 53 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/7x3qcc7p Prompt:    13 | } else if (a.type() == typeid(string) && b.type() == typeid(string)) {       |   ^~~~ 8e2447db-42d7-46d9-8560-f209e66ef642.cpp:22:1: error: expected unqualified-id be fore ‘return’    22 | return "None";       | ^~~~~~ In file included from 8e2447db-42d7-46d9-8560-f209e66ef642.cpp:24: 8e2447db-42d7-46d9-8560-f209e66ef642.cpp: In function ‘int main()’: 8e2447db-42d7-46d9-8560-f209e66ef642.cpp:26:13: error: ‘boost’ has not been decl ared    26 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 8e2447db-42d7-46d9-8560-f209e66ef642.cpp:26:29: error: ‘string’ was not declared  in this scope    26 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 8e2447db-42d7-46d9-8560-f209e66ef642.cpp:26:37: error: ‘compare_one’ was not dec lared in this scope    26 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,9 +1,8 @@
 #include <boost/any.hpp>
 #include <boost/algorithm/string/replace.hpp>
-#include <typeinfo>
-#include <string>
+#include <boost/lexical_cast.hpp>
 
-boost::any compare_one(const boost::any &a, const boost::any &b) {
+boost::any compare_one(const boost::any& a, const boost::any& b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
         if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
             return a;
@@ -17,8 +16,8 @@ boost::any compare_one(const boost::any &a, const boost::any &b) {
             return b;
         }
     } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        float a_float = std::stof(boost::algorithm::replace_all_copy(boost::any_cast<std::string>(a), ",", "."));
-        float b_float = std::stof(boost::algorithm::replace_all_copy(boost::any_cast<std::string>(b), ",", "."));
+        float a_float = boost::lexical_cast<float>(boost::algorithm::replace_all_copy(boost::any_cast<std::string>(a), ",", "."));
+        float b_float = boost::lexical_cast<float>(boost::algorithm::replace_all_copy(boost::any_cast<std::string>(b), ",", "."));
         if (a_float > b_float) {
             return a;
         } else if (a_float < b_float) {
