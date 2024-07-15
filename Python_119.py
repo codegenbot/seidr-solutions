8d46c3c63b9088f5375df527dd9f5d6d49da22d2
beep boop <githubbot@vadim.me>
2024-07-15 12:48:51 +0200

    Solution 18 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rip4z73m Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/49dea532-4c5a-416f-8a18-b82cbb38f 0f8.py", line 26, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/49dea532-4c5a-416f-8a18-b82cbb38f 0f8.py", line 23, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,10 +1,9 @@
-```
 def match_parens(lst):
-    stack = []
     for s in lst:
         if not isinstance(s, str):
             return 'No'
         parens_count = sum(1 for c in s if c in ['(', ')'])
+        stack = []
         if parens_count == 0:
             continue
         for c in s:
@@ -14,7 +13,6 @@ def match_parens(lst):
                 if len(stack) == 0:
                     return 'No'
                 stack.pop()
-    if len(stack) > 0:
-        return 'No'
-    else:
-        return 'Yes'
+        if len(stack) > 0:
+            return 'No'
+    return 'Yes'
