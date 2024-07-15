cdda2fdaae360bfcfda6e61f64420c5407db5a54
beep boop <githubbot@vadim.me>
2024-07-15 16:02:16 +0200

    Solution 67 of Python/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/a0911dst Prompt: Complete the following code given the task description and function signature.

--- Python_141.py
+++ Python_141.py
@@ -1,14 +1,13 @@
 ```
 def file_name_check(file_name):
-    if '.' in file_name:
-        before_dot = file_name.split('.')[0]
-        after_dot = file_name.split('.')[1]
-        
-        if len([c for c in before_dot if c.isdigit()]) > 3 or not before_dot[0].isalpha():
-            return 'No'
-        if after_dot not in ['txt', 'exe', 'dll']:
-            return 'No'
-    else:
-        if len([c for c in file_name if c.isdigit()]) > 3 or not file_name[0].isalpha():
-            return 'No'
+    if len([c for c in file_name if c.isdigit()]) > 3:
+        return 'No'
+    if file_name.count('.') != 1:
+        return 'No'
+    pre_dot = file_name.split('.')[0]
+    post_dot = file_name.split('.')[1].lower()
+    if not pre_dot or not set(pre_dot).issubset('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+        return 'No'
+    if post_dot not in ['txt', 'exe', 'dll']:
+        return 'No'
     return 'Yes'
