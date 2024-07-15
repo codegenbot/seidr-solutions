3f59d138d907b8cffbd488d803a3d74f1f12e56f
beep boop <githubbot@vadim.me>
2024-07-15 18:45:56 +0200

    Solution 73 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i1v1lsd5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/807a0475-1233-4c71-9735-f8e0fed5d edb.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/807a0475-1233-4c71-9735-f8e0fed5d edb.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,4 @@
 def sort_third(l: list):
     if len(l) % 3 != 0:
-        l += [float("inf")] * (3 - len(l) % 3)  # Pad the list with infinity values
-    return [sorted(l[i : i + 3]) for i in range(0, len(l), 3)]
+        l += [float('inf')] * (3 - len(l) % 3)
+    return [sorted(l[i : i + 3]) for i in range(0, len(l), 3]
