5438cb8dbc2618c15ad3c58dbcd20ee88b0b5795
beep boop <githubbot@vadim.me>
2024-07-15 23:32:49 +0200

    Solution 21 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/52xwrlmu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/739aa089-8895-4a94-9564-755e4fd73 3b9.py", line 1     ```     ^ SyntaxError: invalid syntax

--- vector-distance.py
+++ vector-distance.py
@@ -3,6 +3,5 @@ n = int(input())
 vec1 = [float(i) for i in input().split()]
 vec2 = [float(i) for i in input().split()]
 
-total_distance = sum((a - b) ** 2 for a, b in zip(vec1, vec2)) ** 0.5
-
+total_distance = sum((a - b)**2 for a, b in zip(vec1, vec2))**0.5
 print(round(total_distance, 10))
