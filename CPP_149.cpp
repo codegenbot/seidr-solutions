bc94b3bc699b2d1d171be0fb99e54a98a6226fbb
beep boop <githubbot@vadim.me>
2024-07-15 21:45:02 +0200

    Solution 77 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/y8x0260j Prompt:    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 157650da-bb79-400d-96d3-08a7c1786ef1.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 157650da-bb79-400d-96d3-08a7c1786ef1.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 157650da-bb79-400d-96d3-08a7c1786ef1.cpp:17: 157650da-bb79-400d-96d3-08a7c1786ef1.cpp: In function ‘int main()’: 157650da-bb79-400d-96d3-08a7c1786ef1.cpp:27:20: error: ‘sorted_list_sum’ was not  declared in this scope    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 157650da-bb79-400d-96d3-08a7c1786ef1.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,18 +1,4 @@
-vector<string> sorted_list_sum(const vector<string>& lst);
-
-bool issame(const vector<string>& a, const vector<string>& b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-vector<string> sorted_list_sum(const vector<string>& lst){
+vector<string> sorted_list_sum(const vector<string>& lst) {
     vector<string> result;
     for (const string& str : lst) {
         if (str.length() % 2 == 0) {
@@ -27,4 +13,12 @@ vector<string> sorted_list_sum(const vector<string>& lst){
     });
     return result;
 }
-```
+
+bool issame(const vector<string>& a, const vector<string>& b) {
+    return a == b;
+}
+
+int main() {
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+    return 0;
+}
