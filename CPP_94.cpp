3abd72921513276dbe9ec6a8d2fa69509bd3dd84
beep boop <githubbot@vadim.me>
2024-07-15 11:35:53 +0200

    Solution 33 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/gj1q8tib Prompt: d97e0ebf-5b87-43fb-96bc-d760defd2d91.cpp:1:15: error: ‘vector’ was not declared in this scope     1 | int skjkasdkd(vector<int> lst) {       |               ^~~~~~ d97e0ebf-5b87-43fb-96bc-d760defd2d91.cpp:1:22: error: expected primary-expressio n before ‘int’     1 | int skjkasdkd(vector<int> lst) {       |                      ^~~ In file included from d97e0ebf-5b87-43fb-96bc-d760defd2d91.cpp:30: d97e0ebf-5b87-43fb-96bc-d760defd2d91.cpp: In function ‘int main()’: d97e0ebf-5b87-43fb-96bc-d760defd2d91.cpp:32:22: error: ‘skjkasdkd’ cannot be use d as a function    32 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ~~~~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,17 +1,5 @@
 #include<vector>
 
-bool isPrime(int n) {
-    if (n <= 1) {
-        return false;
-    }
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int skjkasdkd(std::vector<int> lst) {
     int maxPrime = 0;
     for (int num : lst) {
@@ -29,6 +17,15 @@ int skjkasdkd(std::vector<int> lst) {
     return sumOfDigits;
 }
 
-int main() {
-    assert (skjkasdkd({127, 97, 8192}) == 10);
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+
 }
