3d1e5460c5bec7efff0d56789076160bd0e741dc
beep boop <githubbot@vadim.me>
2024-07-15 14:49:29 +0200

    Solution 95 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/r0qrt3jx Prompt:       |                    ^~~ 427050a0-e49e-4d88-b2be-c672b1ec84c8.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 427050a0-e49e-4d88-b2be-c672b1ec84c8.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 427050a0-e49e-4d88-b2be-c672b1ec84c8.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 427050a0-e49e-4d88-b2be-c672b1ec84c8.cpp:13: 427050a0-e49e-4d88-b2be-c672b1ec84c8.cpp: In function ‘int main()’: 427050a0-e49e-4d88-b2be-c672b1ec84c8.cpp:23:20: error: ‘even_odd_count’ was not declared in this scope    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 427050a0-e49e-4d88-b2be-c672b1ec84c8.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 bool issame(std::vector<int> a, std::vector<int> b) {
-    vector<int> count(2, 0);
+    std::vector<int> count(2, 0);
     while(num != 0){
         if(abs(num % 10) % 2 == 0){
             count[0]++;
