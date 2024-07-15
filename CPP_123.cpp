c2f6d4d20e461f2429fab3b25fa0cf28aa81acb1
beep boop <githubbot@vadim.me>
2024-07-15 18:00:52 +0200

    Solution 17 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/fone7umq Prompt:       |                    ^~~ 18eedf46-05f3-4c08-a81f-d5dff430f986.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 18eedf46-05f3-4c08-a81f-d5dff430f986.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 18eedf46-05f3-4c08-a81f-d5dff430f986.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 18eedf46-05f3-4c08-a81f-d5dff430f986.cpp:24: 18eedf46-05f3-4c08-a81f-d5dff430f986.cpp: In function ‘int main()’: 18eedf46-05f3-4c08-a81f-d5dff430f986.cpp:34:20: error: ‘get_odd_collatz’ was not  declared in this scope    34 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 18eedf46-05f3-4c08-a81f-d5dff430f986.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -4,36 +4,16 @@ bool issame(vector<int> a, vector<int> b){
     if(a.size() != b.size()){
         return false;
     }
-    
-    for(int i = 0; i < a.size(); ++i){
+    for(int i = 0; i < a.size(); i++){
         if(a[i] != b[i]){
             return false;
         }
     }
-    
     return true;
 }
 
 vector<int> get_odd_collatz(int n);
 
-int main() {
-    int n;
-    cin >> n;
-    
-    vector<int> result = get_odd_collatz(n);
-    
-    for(int num : result){
-        cout << num << " ";
-    }
-    
-    vector<int> a = {1, 2, 3, 4};
-    vector<int> b = {1, 2, 3, 4};
-    
-    if(issame(a, b)){
-        cout << "Same" << endl;
-    } else {
-        cout << "Different" << endl;
-    }
-    
-    return 0;
+int main(){
+    // Main function code
 }
