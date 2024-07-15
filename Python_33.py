26f80bd0e17d0b5af9ef8ce23f96e133da35afe0
beep boop <githubbot@vadim.me>
2024-07-15 11:41:40 +0200

    Solution 12 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/n4q5x7ep Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/289442a8-a08a-4107-b33b-68f83aec2 4af.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/289442a8-a08a-4107-b33b-68f83aec2 4af.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,10 +1,4 @@
-```
 def sort_third(l: list):
-    return [
-        (
-            i
-            if i % 3 != 0
-            else sorted([x for x in l[i-1:i+2] if x % 3 == 0])[::-1]
-        )
-        for i in range(1, len(l))
-    ]
+    third_zero = [i for i in l if i % 3 == 0]
+    third_one_or_two = sorted([x for x in l if x % 3 != 0])
+    return sorted(third_zero + third_one_or_two)
