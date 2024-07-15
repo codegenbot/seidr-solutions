5fc71ab972e539d1a44331b3728f32c91acdd7df
beep boop <githubbot@vadim.me>
2024-07-15 21:14:21 +0200

    Solution 49 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/o9vvyo7g Prompt: b0a9bbfc-5d06-47cc-9c74-9f79fa12ba08.cpp:1:15: error: ‘string’ was not declared in this scope     1 | int whitePegs(string code, string guess) {       |               ^~~~~~ b0a9bbfc-5d06-47cc-9c74-9f79fa12ba08.cpp:1:28: error: ‘string’ was not declared in this scope     1 | int whitePegs(string code, string guess) {       |                            ^~~~~~ b0a9bbfc-5d06-47cc-9c74-9f79fa12ba08.cpp:1:40: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int whitePegs(string code, string guess) {       |                                        ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
 #include <string>
 
-int whitePegs(std::string code, std::string guess) {
+int whitePegs(string code, string guess) {
     int codeCount[6] = {0};
     int guessCount[6] = {0};
     int blackPegsCount = 0;
@@ -9,14 +8,14 @@ int whitePegs(std::string code, std::string guess) {
         if(code[i] == guess[i]) {
             blackPegsCount++;
         } else {
-            codeCount[code[i]-'0']++;
-            guessCount[guess[i]-'0']++;
+            codeCount[code[i]-'A']++;
+            guessCount[guess[i]-'A']++;
         }
     }
     int whitePegsCount = 0;
     for(int i=0; i<6; i++) {
         if(codeCount[i] > 0 && guessCount[i] > 0) {
-            whitePegsCount += std::min(codeCount[i], guessCount[i]);
+            whitePegsCount += min(codeCount[i], guessCount[i]);
         }
     }
     return blackPegsCount + (4 - blackPegsCount - whitePegsCount);
