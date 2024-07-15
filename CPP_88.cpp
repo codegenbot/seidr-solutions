56637ec1779af59d546b895cfe65a0975d0a36af
beep boop <githubbot@vadim.me>
2024-07-15 23:09:02 +0200

    Solution 2 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/bkuwgnjb Prompt:       |                    ^~~ 944cc5d9-5b11-4447-ae7f-34093a298c44.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 944cc5d9-5b11-4447-ae7f-34093a298c44.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 944cc5d9-5b11-4447-ae7f-34093a298c44.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 944cc5d9-5b11-4447-ae7f-34093a298c44.cpp:9: 944cc5d9-5b11-4447-ae7f-34093a298c44.cpp: In function ‘int main()’: 944cc5d9-5b11-4447-ae7f-34093a298c44.cpp:19:20: error: ‘sort_array’ was not decl ared in this scope    19 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 944cc5d9-5b11-4447-ae7f-34093a298c44.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,27 +1,19 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
-}
-
-vector<int> sort_array(vector<int> array) {
-    if(array.empty())
-        return array;
+std::vector<int> sort_array(std::vector<int> array) {
+    if(array.empty()) return array;
     if((array.front() + array.back()) % 2 == 0)
-        sort(array.begin(), array.end(), greater<int>());
+        std::sort(array.begin(), array.end(), std::greater<int>());
     else
-        sort(array.begin(), array.end());
-
+        std::sort(array.begin(), array.end());
     return array;
 }
 
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
 int main() {
-    vector<int> input_array = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
-    vector<int> sorted_array = sort_array(input_array);
-
-    for (int num : sorted_array) {
-        cout << num << " ";
-    }
-
-    return 0;
+    assert(issame(sort_array({21, 14, 23, 11}), {23, 21, 14, 11}));
 }
