d6e43cd7103e0b3edee066b7a8f748592ede151e
beep boop <githubbot@vadim.me>
2024-07-15 19:02:12 +0200

    Solution 39 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/jc6v89py Prompt:       |           ^~~~~~ In file included from 11821a0d-dc5f-4839-9cda-ffb30b8293cc.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 11821a0d-dc5f-4839-9cda-ffb30b8293cc.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 11821a0d-dc5f-4839-9cda-ffb30b8293cc.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 11821a0d-dc5f-4839-9cda-ffb30b8293cc.cpp:11: 11821a0d-dc5f-4839-9cda-ffb30b8293cc.cpp: In function ‘int main()’: 11821a0d-dc5f-4839-9cda-ffb30b8293cc.cpp:21:20: error: ‘eat’ was not declared in  this scope    21 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ^~~ 11821a0d-dc5f-4839-9cda-ffb30b8293cc.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -8,7 +8,7 @@ std::vector<int> eat(int number, int need, int remaining) {
     int totalCarrots = number + need;
     int eatenCarrots = totalCarrots > remaining ? totalCarrots - remaining : totalCarrots;
     int leftCarrots = remaining > totalCarrots ? 0 : remaining - totalCarrots;
-    
+
     return {eatenCarrots, leftCarrots};
 }
 
