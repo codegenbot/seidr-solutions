be571fbeb6aafb89270727c1c814d0ff9a593c83
beep boop <githubbot@vadim.me>
2024-07-16 00:03:16 +0200

    Solution 3 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/xy0pshe5 Prompt: In file included from f59e53f1-25fb-4110-8924-83e2385a935d.cpp:10: f59e53f1-25fb-4110-8924-83e2385a935d.cpp: In function ‘int main()’: f59e53f1-25fb-4110-8924-83e2385a935d.cpp:12:22: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    12 |     assert (simplify("1/5", "1/5") == false);       |                      ^~~~~       |                      |       |                      const char* f59e53f1-25fb-4110-8924-83e2385a935d.cpp:1:29: note:   initializing argument 1 o f ‘bool simplify(const int&, const int&)’     1 | bool simplify(const string& x, const string& n) {       |               ~~~~~~~~~~~~~~^ In file included from f59e53f1-25fb-4110-8924-83e2385a935d.cpp:10: f59e53f1-25fb-4110-8924-83e2385a935d.cpp:12:29: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    12 |     assert (simplify("1/5", "1/5") == false);       |                             ^~~~~       |                             |       |                             const char* f59e53f1-25fb-4110-8924-83e2385a935d.cpp:1:46: note:   initializing argument 2 o f ‘bool simplify(const int&, const int&)’     1 | bool simplify(const string& x, const string& n) {       |                                ~~~~~~~~~~~~~~^

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,12 +1,13 @@
-#include <string>
-
-bool checkDivisibility(string x) {
-    string n = "some_string_value";
-    
+bool simplify(const string& x, const string& n) {
     int num1 = stoi(x.substr(0, x.find('/')));
     int den1 = stoi(x.substr(x.find('/') + 1));
     int num2 = stoi(n.substr(0, n.find('/')));
     int den2 = stoi(n.substr(n.find('/') + 1));
 
     return (num1 * num2) % (den1 * den2) == 0;
+}
+
+int main() {
+    assert(simplify("1/5", "1/5") == false);
+    return 0;
 }
