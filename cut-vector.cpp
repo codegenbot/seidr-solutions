ad8cc04a3cd2047d1417287240c798704a5b661b
beep boop <githubbot@vadim.me>
2024-07-15 17:05:29 +0200

    Solution 16 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/nt0h9tp3 Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,43 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
 
 int main() {
-    std::vector<int> nums;
-    int num;
-    while (std::cin >> num) {
-        nums.push_back(num);
-    }
-
-    int sum = 0;
-    for (int x : nums) {
-        sum += x;
-    }
-
-    int target = sum / 2;
-    int curr_sum = 0;
-    for (int i = 0; i < nums.size(); i++) {
-        curr_sum += nums[i];
-        if (curr_sum >= target) {
-            if (curr_sum - target <= target - (curr_sum - nums[i])) {
-                for (int j = 0; j <= i; j++) {
-                    std::cout << nums[j] << " ";
-                }
-            } else {
-                for (int j = 0; j < i; j++) {
-                    std::cout << nums[j] << " ";
-                }
-            }
-            std::cout << std::endl;
-
-            for (int j = i + 1; j < nums.size(); j++) {
-                std::cout << nums[j] << " ";
-            }
-            std::cout << std::endl;
-
-            break;
-        }
-    }
-
+    // Your code to solve the cut-vector problem
     return 0;
 }
