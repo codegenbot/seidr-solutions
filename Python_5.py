37e1fe6c09ffc794ca67eb96d5515676fe6ac80c
beep boop <githubbot@vadim.me>
2024-07-15 19:34:16 +0200

    Solution 32 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ppboz38r Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9a27768c-10da-4f96-8657-1efbad051 5a6.py", line 1     return [     ^^^^^^^^ SyntaxError: 'return' outside function

--- Python_5.py
+++ Python_5.py
@@ -1,7 +1,6 @@
 def complete_code(numbers, delimiter):
     return [
-        val for pair in zip(numbers, [delimiter] * (len(numbers) - 1) + [0]) for val in pair
+        val
+        for pair in zip(numbers, [delimiter] * (len(numbers) - 1) + [0])
+        for val in pair
     ]
-
-result = complete_code([1, 2, 3, 4, 5], '-')
-print(result)
