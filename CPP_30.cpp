5ab834091a1eb3ae7ab41f31b32479cd80549907
beep boop <githubbot@vadim.me>
2024-07-15 19:41:39 +0200

    Solution 79 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/iw84ztid Prompt:       |                    ^~~~~ e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:11: e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp: In function ‘int main()’: e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,11 +1,24 @@
 #include <vector>
 
-bool is_same(vector<float> a, vector<float> b){
+vector<float> get_positive(vector<float> l){
     vector<float> result;
-    for(float num : a){
+    for(float num : l){
         if(num > 0){
             result.push_back(num);
         }
     }
     return result;
+}
+
+bool is_same(vector<float> a, vector<float> b){
+    return a == b;
+}
+
+int main() {
+    vector<float> input1 = {1.1, 2.2, 3.3};
+    vector<float> input2 = {1.1, 4.4, 5.5};
+    
+    bool result = is_same(input1, input2);
+
+    return 0;
 }
