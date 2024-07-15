7ced98f82a8c36f801f13457041ec51bf134c127
beep boop <githubbot@vadim.me>
2024-07-15 13:07:05 +0200

    Solution 8 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/56a0g59v Prompt:       |                    ^~~ c214b48a-5d5a-4a91-8baf-1d7ecafb0742.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c214b48a-5d5a-4a91-8baf-1d7ecafb0742.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c214b48a-5d5a-4a91-8baf-1d7ecafb0742.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c214b48a-5d5a-4a91-8baf-1d7ecafb0742.cpp:10: c214b48a-5d5a-4a91-8baf-1d7ecafb0742.cpp: In function ‘int main()’: c214b48a-5d5a-4a91-8baf-1d7ecafb0742.cpp:20:20: error: ‘sum_product’ was not dec lared in this scope; did you mean ‘product’?    20 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ c214b48a-5d5a-4a91-8baf-1d7ecafb0742.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
 #include <vector>
+#include <tuple>
 
-std::vector<int> sum_product(std::vector<int> numbers) {
+std::tuple<int, int> sum_product(const std::vector<int>& numbers) {
     int sum = 0;
     int product = 1;
     for (int num : numbers) {
@@ -10,6 +12,11 @@ std::vector<int> sum_product(std::vector<int> numbers) {
     return {sum, product};
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(std::tuple<int, int> a, std::tuple<int, int> b){
     return a == b;
+}
+
+int main() {
+    assert(issame(sum_product({10}), {10, 10}));
+    return 0;
 }
