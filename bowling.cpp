dd055237a6f05d6204fc63fba05ff0ec726ef73a
beep boop <githubbot@vadim.me>
2024-07-15 18:22:55 +0200

    Solution 47 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/wv0y0vie Prompt: 8b5ec380-a461-4b5b-84b0-31c59427aa9e.cpp:1:1: error: expected unqualified-id bef ore ‘if’     1 | if (currentRolls == 2) {       | ^~

--- bowling.cpp
+++ bowling.cpp
@@ -1,36 +1,52 @@
 ```c++
-int calculateBowlingScore(string input) {
-    int score = 0;
-    int currentRolls = 0;
-    int currentScore = 0;
-    bool inFrame = false;
+#include <iostream>
+using namespace std;
 
-    for (char c : input) {
-        if (c == '/') {
-            inFrame = false;
-            currentRolls = 2;
-            currentScore = 0;
-        } else if (isdigit(c)) {
-            currentScore = currentScore * 10 + (c - '0');
-            currentRolls++;
-            if (!inFrame) {
-                inFrame = true;
-            }
-        }
-        if (currentRolls == 2) {
-            if (currentScore >= 10) {
-                score += 10;
+int main() {
+    int score = 0, currentScore = 0, totalRolls = 0, inFrame = false, currentRolls = 0;
+    char bowl[11];
+
+    cout << "Enter the individual bowls: ";
+    cin >> bowl;
+
+    for(int i = 0; i < 10; i++) {
+        if(bowl[i] != '/') {
+            if(inFrame) {
+                totalRolls++;
+                if(bowl[i] == 'X') {
+                    score += 30;
+                    inFrame = false;
+                } else if(bowl[i] - '0' <= 9) {
+                    currentScore += bowl[i] - '0';
+                    if(currentScore < 10) {
+                        currentRolls++;
+                    } else {
+                        score += 10;
+                        totalRolls++;
+                        currentRolls = 0;
+                    }
+                }
             } else {
-                score += currentScore;
+                if(bowl[i] == 'X') {
+                    score += 30;
+                    inFrame = true;
+                    currentRolls = 2;
+                } else if(bowl[i] - '0' <= 9) {
+                    currentScore += bowl[i] - '0';
+                    totalRolls++;
+                    if(currentScore < 10) {
+                        currentRolls = 1;
+                    }
+                }
             }
-            currentScore = 0;
-            inFrame = false;
-            currentRolls = 0;
         }
     }
 
-    if (!inFrame && currentScore > 0) {
+    if(!inFrame && currentScore >= 10) {
+        score += 10;
+    } else if(!inFrame) {
         score += currentScore;
     }
-    return score;
+
+    return 0;
 }
