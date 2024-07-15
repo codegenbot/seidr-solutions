8860f3848f4ec262259555d01236133d5e848c98
beep boop <githubbot@vadim.me>
2024-07-15 23:44:56 +0200

    Solution 76 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/fou7detl Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 2fdef6e6-2e9d-4395-80ca-b9edcdeac9fd.cpp:10:29:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- leaders.cpp
+++ leaders.cpp
@@ -7,6 +7,7 @@ std::vector<int> leaders(std::vector<int>& vec) {
     int maxRight = vec.back();
     for (int i = vec.size() - 1; i >= 0; --i) {
         if (i == vec.size() - 1 || vec[i] >= maxRight) {
+            result.reserve(result.size() + 1); 
             result.push_back(vec[i]);
             maxRight = vec[i];
         }
@@ -16,14 +17,9 @@ std::vector<int> leaders(std::vector<int>& vec) {
 
 int main() {
     std::vector<int> vec = {7, 5, 2, 4};
-    if (vec.empty()) {
-        std::cout << "Error: Input vector is empty." << std::endl;
-    } else {
-        std::vector<int> result = leaders(vec);
-        for (int num : result) {
-            std::cout << num << " ";
-        }
-        std::cout << std::endl;
+    std::vector<int> result = leaders(vec);
+    for (int num : result) {
+        std::cout << num << " ";
     }
     return 0;
 }
