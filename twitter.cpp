bf76af1e8c657f44f8ef9f771dc129ccd35900b9
beep boop <githubbot@vadim.me>
2024-07-15 23:16:59 +0200

    Solution 10 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/wb1264jh Prompt: 0dab7b99-372a-40e9-85e6-66b852f89dd3.cpp:1:1: error: expected unqualified-id bef ore ‘if’     1 | if (tweet.empty()) {       | ^~ 0dab7b99-372a-40e9-85e6-66b852f89dd3.cpp:3:3: error: expected unqualified-id bef ore ‘else’     3 | } else if (tweet.size() > 140) {       |   ^~~~ 0dab7b99-372a-40e9-85e6-66b852f89dd3.cpp:5:3: error: expected unqualified-id bef ore ‘else’     5 | } else {       |   ^~~~

--- twitter.cpp
+++ twitter.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <string>
 
-std::string validateTweet(std::string tweet) {
-    if (tweet.empty()) {
-        return "You didn't type anything";
-    } else if (tweet.size() > 140) {
-        return "Too many characters";
-    } else {
-        return "Your tweet has " + std::to_string(tweet.size()) + " characters";
-    }
+std::string tweet;
+std::cin >> tweet;
+
+if (tweet.empty()) {
+    std::cout << "You didn't type anything" << std::endl;
+} else if (tweet.size() > 140) {
+    std::cout << "Too many characters" << std::endl;
+} else {
+    std::cout << "Your tweet has " << tweet.size() << " characters" << std::endl;
 }
