aaabc55c17ead7abcc72fd3664852a77103c602c
beep boop <githubbot@vadim.me>
2024-07-15 17:58:21 +0200

    Solution 10 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i1v1lsd5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d327a456-f1e4-473d-a770-ba5d25309 006.py", line 14, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d327a456-f1e4-473d-a770-ba5d25309 006.py", line 9, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,5 +1,7 @@
 def sort_third(l: list):
-    l_copy = l.copy()
-    for i in range(0, len(l_copy) - 2, 3):
-        l_copy[i : i + 3] = sorted(l_copy[i : i + 3])
-    return l_copy
+    return [
+        sorted(l[i:i+3])
+        for i in range(0, len(l), 3)
+        for j in range(3)
+        if i + j < len(l)
+    ]
