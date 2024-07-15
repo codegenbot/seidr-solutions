1b9337026455de87232989c85b811d9b509f6fa0
beep boop <githubbot@vadim.me>
2024-07-15 23:13:53 +0200

    Solution 87 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/jl6zpipe Prompt: In file included from be862c8a-a947-4c28-a87d-449e7d0e05fc.cpp:23: be862c8a-a947-4c28-a87d-449e7d0e05fc.cpp: In function ‘int main()’: be862c8a-a947-4c28-a87d-449e7d0e05fc.cpp:33:20: error: ‘common’ was not declared  in this scope    33 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -7,18 +7,25 @@ using namespace std;
 class CustomVectorFunctions {
 public:
     static vector<int> common(const vector<int>& a, const vector<int>& b);
-
-    static vector<int> common(const vector<int>& a, const vector<int>& b) {
-        vector<int> result;
-        for (int num : a) {
-            if (find(b.begin(), b.end(), num) != b.end()) {
-                result.push_back(num);
-            }
-        }
-        return result;
-    }
-
-    static bool issame(const vector<int>& a, const vector<int>& b) {
-        return a == b;
-    }
+    static bool issame(const vector<int>& a, const vector<int>& b);
 };
+
+vector<int> CustomVectorFunctions::common(const vector<int>& a, const vector<int>& b) {
+    vector<int> result;
+    for (int num : a) {
+        if (find(b.begin(), b.end(), num) != b.end()) {
+            result.push_back(num);
+        }
+    }
+    return result;
+}
+
+bool CustomVectorFunctions::issame(const vector<int>& a, const vector<int>& b) {
+    return a == b;
+}
+
+int main() {
+    assert(CustomVectorFunctions::issame(CustomVectorFunctions::common({4, 3, 2, 8}, {}), {}));
+    
+    return 0;
+}
