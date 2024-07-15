e61eb9178ce197c05201913030f78e97e42e7952
beep boop <githubbot@vadim.me>
2024-07-15 23:27:18 +0200

    Solution 60 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/9060k3zb Prompt: Program did not receive expected input

--- Python_8.py
+++ Python_8.py
@@ -1,7 +1,5 @@
 try:
     n, *numbers = map(int, input().strip().split())
-    if len(numbers) != n:
-        raise ValueError
     res = [num for num in numbers if num % 2 == 0]
     print(sum(res))
 except ValueError:
