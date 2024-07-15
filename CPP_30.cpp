ceb4b01eb5c296aa93205de54d207695dc1b5218
beep boop <githubbot@vadim.me>
2024-07-15 22:20:31 +0200

    Solution 2 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/6j1jycto Prompt:       |                    ^~~~~ 05f9918e-4323-42b5-8bab-3b7856da1ebf.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 05f9918e-4323-42b5-8bab-3b7856da1ebf.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 05f9918e-4323-42b5-8bab-3b7856da1ebf.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 05f9918e-4323-42b5-8bab-3b7856da1ebf.cpp:11: 05f9918e-4323-42b5-8bab-3b7856da1ebf.cpp: In function ‘int main()’: 05f9918e-4323-42b5-8bab-3b7856da1ebf.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 05f9918e-4323-42b5-8bab-3b7856da1ebf.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,16 +1,8 @@
+#include <iostream>
 #include <vector>
+#include <cassert>
 
-bool issame(vector<float> a, vector<float> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
+using namespace std;
 
 vector<float> get_positive(vector<float> l){
     vector<float> result;
@@ -20,4 +12,14 @@ vector<float> get_positive(vector<float> l){
         }
     }
     return result;
+}
+
+bool issame(vector<float> a, vector<float> b){
+    return a == b;
+}
+
+int main() {
+    assert(issame(get_positive({}), {}));
+    
+    return 0;
 }
