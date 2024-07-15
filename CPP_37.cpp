cad43bfefc3af6c0cc6dfd0deb4e9aa527787376
beep boop <githubbot@vadim.me>
2024-07-15 19:58:27 +0200

    Solution 88 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/bygrg8t1 Prompt:    26 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ fea8fba2-4273-4d42-a918-c96aa8f42026.cpp:26:36: error: expected primary-expressi on before ‘float’    26 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ fea8fba2-4273-4d42-a918-c96aa8f42026.cpp:26:43: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from fea8fba2-4273-4d42-a918-c96aa8f42026.cpp:25: fea8fba2-4273-4d42-a918-c96aa8f42026.cpp: In function ‘int main()’: fea8fba2-4273-4d42-a918-c96aa8f42026.cpp:35:20: error: ‘sort_even’ was not decla red in this scope    35 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ fea8fba2-4273-4d42-a918-c96aa8f42026.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,5 +1,13 @@
 bool issame(vector<float>& a, vector<float>& b) {
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 vector<float> sort_even(vector<float> l) {
@@ -29,5 +37,6 @@ vector<float> sort_even(vector<float> l) {
 
 int main() {
     assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    
     return 0;
 }
