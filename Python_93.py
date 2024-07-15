cb90b7dc08cc7a7d9063e4779417b9bb457b2538
beep boop <githubbot@vadim.me>
2024-07-15 21:36:48 +0200

    Solution 88 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/yxhj599w Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f171cb31-2b47-42c3-8282-3af5d972f 134.py", line 24, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f171cb31-2b47-42c3-8282-3af5d972f 134.py", line 20, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,11 +1,14 @@
 def encode(message):
     vowels = set("aeiouAEIOU")
     return "".join(
-        char if not char.isalpha()
-        else (
-            chr(((ord(char.lower()) - ord("a") - 2) % 26) + ord("a")).swapcase()
-            if char.lower() not in vowels
-            else char.swapcase()
+        (
+            char
+            if not char.isalpha()
+            else (
+                chr(((ord(char.upper()) - ord("A") + 1) % 26) + ord("A")).swapcase()
+                if char.lower() not in vowels
+                else (chr((ord(char) ^ 32)) if char.isalpha() else char)
+            )
         )
         for char in message
     )
