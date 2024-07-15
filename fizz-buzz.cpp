1d1ba5cdce7e0d0903db8a49c1f304616d5703da
beep boop <githubbot@vadim.me>
2024-07-15 12:43:17 +0200

    Solution 11 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/a08td4t2 Prompt: n this scope   153 |     cout << "Enter a number: ";       |     ^~~~ 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:154:5: error: ‘cin’ was not declared in  this scope   154 |     cin >> num;       |     ^~~ 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:154:5: note: ‘std::cin’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:155:44: error: ‘endl’ was not declared in this scope   155 |     cout << solution.numberToWords(num) << endl;       |                                            ^~~~ 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,141 +1,65 @@
-```c++
 #include <string>
 #include <iostream>
+
 using namespace std;
 
 class Solution {
 public:
     string numberToWords(int num) {
-        if (num == 0) return "zero";
+        if (num == 0) return "0";
         
         string words = "";
         
         int temp = num;
-        if (temp / 100 > 0) {
-            words += threeDigit(temp / 100) + " hundred";
-            temp %= 100;
-        }
-        
-        if (temp / 10 > 0) {
-            if (!words.empty() && temp % 10 == 0) {
-                words += " and zero";
+        if (temp / 3 > 0) {
+            if ((temp / 5 != 0 && temp % 15 != 0) || (temp / 3 != 0)) {
+                if (!words.empty()) {
+                    words += " ";
+                }
+                words += "Fizz";
             } else {
-                words += " and " + twoDigit(temp / 10);
+                words = "FizzBuzz";
             }
-            temp %= 10;
+            temp %= 3;
         }
         
-        if (temp > 0) {
-            if (!words.empty()) {
-                words += " ";
+        if (temp / 5 > 0) {
+            if ((temp / 3 != 0 && temp % 15 != 0) || (temp / 5 != 0)) {
+                if (!words.empty()) {
+                    words += " ";
+                }
+                words += "Buzz";
+            } else {
+                words = "FizzBuzz";
             }
-            switch (temp % 10) {
-                case 1: 
-                    words += (temp == 11 ? "eleven" : temp == 12 ? "twelve" : temp == 13 ? "thirteen" : temp == 14 ? "fourteen" : temp == 15 ? "fifteen" : temp == 16 ? "sixteen" : temp == 17 ? "seventeen" : temp == 18 ? "eighteen" : temp == 19 ? "nineteen" : "one") + (temp > 1 ? " one" : "");
-                    break;
-                case 2: words += "two";
-                    break;
-                case 3: words += "three";
-                    break;
-                case 4: words += "four";
-                    break;
-                case 5: words += "five";
-                    break;
-                case 6: words += "six";
-                    break;
-                case 7: words += "seven";
-                    break;
-                case 8: words += "eight";
-                    break;
-                case 9: words += "nine";
-                    break;
+            temp %= 5;
+        }
+        
+        for(int i=1; i<=num; i++) {
+            if(i % 3 == 0 && i % 5 != 0) {
+                if(words.empty()) {
+                    words = "Fizz";
+                } else {
+                    words += " Fizz";
+                }
+            } 
+            else if(i % 5 == 0 && i % 3 != 0) {
+                if(words.empty()) {
+                    words = "Buzz";
+                } else {
+                    words += " Buzz";
+                }
+            } 
+            else if(i % 15 == 0) {
+                words = "FizzBuzz";
+            } 
+            else {
+                words += to_string(i);
             }
+            words += " ";
         }
         
         return words;
-    }
-    
-    string threeDigit(int num) {
-        if (num == 0) return "zero";
-        if (num < 10) {
-            return oneDigit(num);
-        }
-        if (num < 20) {
-            switch (num) {
-                case 10: return "ten";
-                case 11: return "eleven";
-                case 12: return "twelve";
-                case 13: return "thirteen";
-                case 14: return "fourteen";
-                case 15: return "fifteen";
-                case 16: return "sixteen";
-                case 17: return "seventeen";
-                case 18: return "eighteen";
-                case 19: return "nineteen";
-            }
-        }
-        if (num < 30) {
-            return "twenty" + oneDigit(num - 20);
-        }
-        if (num < 40) {
-            return "thirty" + oneDigit(num - 30);
-        }
-        if (num < 50) {
-            return "forty" + oneDigit(num - 40);
-        }
-        if (num < 60) {
-            return "fifty" + oneDigit(num - 50);
-        }
-        if (num < 70) {
-            return "sixty" + oneDigit(num - 60);
-        }
-        if (num < 80) {
-            return "seventy" + oneDigit(num - 70);
-        }
-        if (num < 90) {
-            return "eighty" + oneDigit(num - 80);
-        }
-        return "ninety" + oneDigit(num - 90);
-    }
-    
-    string twoDigit(int num) {
-        switch (num) {
-            case 0: return "zero";
-            case 1: return "one";
-            case 2: return "two";
-            case 3: return "three";
-            case 4: return "four";
-            case 5: return "five";
-            case 6: return "six";
-            case 7: return "seven";
-            case 8: return "eight";
-            case 9: return "nine";
-            case 10: return "ten";
-            case 11: return "eleven";
-            case 12: return "twelve";
-            case 13: return "thirteen";
-            case 14: return "fourteen";
-            case 15: return "fifteen";
-            case 16: return "sixteen";
-            case 17: return "seventeen";
-            case 18: return "eighteen";
-            case 19: return "nineteen";
-        }
-    }
-    
-    string oneDigit(int num) {
-        switch (num) {
-            case 0: return "zero";
-            case 1: return "one";
-            case 2: return "two";
-            case 3: return "three";
-            case 4: return "four";
-            case 5: return "five";
-            case 6: return "six";
-            case 7: return "seven";
-            case 8: return "eight";
-            case 9: return "nine";
-        }
     }
 };
 
