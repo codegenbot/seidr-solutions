9eb01f079eb280341e5ec205cac6f0aefe751ba3
beep boop <githubbot@vadim.me>
2024-07-15 15:54:35 +0200

    Solution 27 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/fdteuljy Prompt:    28 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e74856d2-4cf4-4a1d-b57a-06de5917ce02.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e74856d2-4cf4-4a1d-b57a-06de5917ce02.cpp:28:45: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e74856d2-4cf4-4a1d-b57a-06de5917ce02.cpp:27: e74856d2-4cf4-4a1d-b57a-06de5917ce02.cpp: In function ‘int main()’: e74856d2-4cf4-4a1d-b57a-06de5917ce02.cpp:37:20: error: ‘separate_paren_groups’ w as not declared in this scope    37 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ e74856d2-4cf4-4a1d-b57a-06de5917ce02.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,5 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
 vector<string> separate_paren_groups(string paren_string);
 bool issame(vector<string> a, vector<string> b);
+
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
