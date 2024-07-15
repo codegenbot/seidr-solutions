1b947a399b6b03a28c23222627b651973910407c
beep boop <githubbot@vadim.me>
2024-07-15 15:52:10 +0200

    Solution 14 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/fdteuljy Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8685e965-48bb-45d7-9ad4-442019853399.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8685e965-48bb-45d7-9ad4-442019853399.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8685e965-48bb-45d7-9ad4-442019853399.cpp:25: 8685e965-48bb-45d7-9ad4-442019853399.cpp: In function ‘int main()’: 8685e965-48bb-45d7-9ad4-442019853399.cpp:35:20: error: ‘separate_paren_groups’ w as not declared in this scope    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 8685e965-48bb-45d7-9ad4-442019853399.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,25 +1,24 @@
 #include <vector>
 #include <string>
 
-bool issame(string& s) {
-    if (s.size() < 2) return false;
-    return s[0] == s[s.size() - 1];
+bool issame(char a, char b) {
+    return (a == '(' && b == ')');
 }
 
-vector<string> split_paren_groups(string paren_string) {
+vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
     string current_group;
     int open_braces = 0;
 
     for (char c : paren_string) {
-        if (c == '(') {
-            if (open_braces > 0 && issame(current_group)) {
+        if (issame(c, '(')) {
+            if (open_braces > 0) {
                 current_group.push_back(c);
             }
             open_braces++;
-        } else if (c == ')') {
+        } else if (issame(c, ')')) {
             open_braces--;
-            if (open_braces > 0 && issame(current_group)) {
+            if (open_braces > 0) {
                 current_group.push_back(c);
             } else {
                 result.push_back(current_group);
