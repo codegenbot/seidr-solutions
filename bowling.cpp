d863b3f6fcdc7693ec606d21d2898400652155f1
beep boop <githubbot@vadim.me>
2024-07-15 13:54:46 +0200

    Solution 35 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/aqmostk9 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <string>
 
@@ -7,21 +6,14 @@ int bowlingScore(char c) {
     if (c == 'X') {
         score += 30;
     } else if (c == '/') {
-        return score + 10; 
+        int count = 10 - (s[0] - '0');
+        score += count;
     } else {
         int count = 0;
-        int i = 0;
         while (c >= '1' && c <= '9') {
             count *= 10;
             count += c - '0';
-            char temp = '\0'; 
-            if (i < s.length() - 1) {
-                if (s[i + 1] == '/') {
-                    break;
-                }
-                temp = s[++i];
-            }
-            c = temp; 
+            c = '\0'; 
         }
         score += count;
     }
@@ -29,12 +21,12 @@ int bowlingScore(char c) {
 }
 
 int main() {
-    std::string s;
+    std::string s; 
     std::cout << "Enter the input string: ";
     std::getline(std::cin, s);
     
     int totalScore = 0;
-    for (char c : s) {
+    for (char c : s) { 
         totalScore += bowlingScore(c);
     }
     std::cout << "The total score is: " << totalScore << std::endl;
