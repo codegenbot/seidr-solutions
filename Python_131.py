e3644e1655fd6fceb52b5a579319a0af6c0658f7
beep boop <githubbot@vadim.me>
2024-07-15 14:16:05 +0200

    Solution 1 of Python/131, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/9oyg1zfd Prompt: Complete the following code given the task description and function signature.

--- Python_131.py
+++ Python_131.py
@@ -1,8 +1,6 @@
 def digits(n):
-    product = 1
-    while n > 0:
-        digit = n % 10
-        if digit % 2 != 0:
-            product *= digit
-        n //= 10
-    return product
+    return (
+        eval("*".join(str(int(d)) for d in str(n) if int(d) % 2 != 0))
+        if any(int(d) % 2 != 0 for d in str(n))
+        else 0
+    )
