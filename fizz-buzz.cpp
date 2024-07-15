1a46a5cd500a653a89db34ac170365691d8081b8
beep boop <githubbot@vadim.me>
2024-07-15 12:39:07 +0200

    Solution 9 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/a08td4t2 Prompt: n this scope   153 |     cout << "Enter a number: ";       |     ^~~~ 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:154:5: error: ‘cin’ was not declared in  this scope   154 |     cin >> num;       |     ^~~ 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:154:5: note: ‘std::cin’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:155:44: error: ‘endl’ was not declared in this scope   155 |     cout << solution.numberToWords(num) << endl;       |                                            ^~~~ 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -106,29 +106,10 @@ public:
     }
     
     string twoDigit(int num) {
-        switch (num) {
-            case 0: return "zero";
-            case 1: return "one";
-            case 2: return "two";
-            case 3: return "three";
-            case 4: return "four";
-            case 5: return "five";
-            case 6: return "six";
-            case 7: return "seven";
-            case 8: return "eight";
-            case 9: return "nine";
-            case 10: return "ten";
-            case 11: return "eleven";
-            case 12: return "twelve";
-            case 13: return "thirteen";
-            case 14: return "fourteen";
-            case 15: return "fifteen";
-            case 16: return "sixteen";
-            case 17: return "seventeen";
-            case 18: return "eighteen";
-            case 19: return "nineteen";
-            default: return tenDigit(num);
+        if (num < 20) {
+            return oneDigit(num);
         }
+        return "twenty" + oneDigit(num - 20);
     }
     
     string oneDigit(int num) {
