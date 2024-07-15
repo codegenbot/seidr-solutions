42b6c6d6601ad625ec34b95bdcd1c9c30a3a07b6
beep boop <githubbot@vadim.me>
2024-07-15 22:56:53 +0200

    Solution 2 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/9jbrva5m Prompt: f578f379-1c58-4ace-80d5-25f23c3b440f.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f578f379-1c58-4ace-80d5-25f23c3b440f.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f578f379-1c58-4ace-80d5-25f23c3b440f.cpp:16: f578f379-1c58-4ace-80d5-25f23c3b440f.cpp: In function ‘int main()’: f578f379-1c58-4ace-80d5-25f23c3b440f.cpp:26:20: error: ‘total_match’ was not dec lared in this scope    26 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ f578f379-1c58-4ace-80d5-25f23c3b440f.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ f578f379-1c58-4ace-80d5-25f23c3b440f.cpp: In function ‘int total_chars(int)’: f578f379-1c58-4ace-80d5-25f23c3b440f.cpp:7:1: warning: control reaches end of no n-void function [-Wreturn-type]     7 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,31 +1,7 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cassert>
-
-using namespace std;
-
-int total_chars(const vector<string>& lst) {
-    int total = 0;
-    for (const string& str : lst) {
-        total += str.size();
-    }
-    return total;
-}
-
 bool issame(const vector<string>& a, const vector<string>& b) {
-    return a == b;
+    return (a.size() == b.size() && equal(a.begin(), a.end(), b.begin()));
 }
 
 vector<string> total_match(const vector<string>& lst1, const vector<string>& lst2) {
-    if (total_chars(lst1) < total_chars(lst2)) {
-        return lst1;
-    }
-    return total_chars(lst1) == total_chars(lst2) ? lst1 : lst2;
-}
-
-int main() {
-    assert(issame(total_match({"this"}, {}), {}));
-    
-    return 0;
+    return total_chars(lst1) < total_chars(lst2) ? lst1 : total_chars(lst1) == total_chars(lst2) ? lst1 : lst2;
 }
