246a54c42451f3413c63568c80850376623e6942
beep boop <githubbot@vadim.me>
2024-07-15 18:27:34 +0200

    Solution 5 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ghonsvvs Prompt: fore ‘if’    19 |     if (!(before_dot[0] >= 'a' && before_dot[0] <= 'z') && !(before_dot[ 0] >= 'A' && before_dot[0] <= 'Z')) {       |     ^~ 00bcc2e6-bf0d-4a62-9f6c-7e7b99b55b20.cpp:23:5: error: expected unqualified-id be fore ‘if’    23 |     if (after_dot != "txt" && after_dot != "exe" && after_dot != "dll") {       |     ^~ 00bcc2e6-bf0d-4a62-9f6c-7e7b99b55b20.cpp:27:5: error: expected unqualified-id be fore ‘return’    27 |     return "Yes";       |     ^~~~~~ 00bcc2e6-bf0d-4a62-9f6c-7e7b99b55b20.cpp:28:1: error: expected declaration befor e ‘}’ token    28 | }       | ^ In file included from 00bcc2e6-bf0d-4a62-9f6c-7e7b99b55b20.cpp:30: 00bcc2e6-bf0d-4a62-9f6c-7e7b99b55b20.cpp: In function ‘int main()’: 00bcc2e6-bf0d-4a62-9f6c-7e7b99b55b20.cpp:32:13: error: ‘file_name_check’ was not  declared in this scope    32 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -3,7 +3,7 @@
 string file_name_check(string file_name) {
     int num_digits = 0;
     int dot_pos = -1;
-
+    
     for (int i = 0; i < file_name.size(); ++i) {
         if (file_name[i] >= '0' && file_name[i] <= '9') {
             num_digits++;
@@ -11,30 +11,30 @@ string file_name_check(string file_name) {
             dot_pos = i;
         }
     }
-
+    
     if (num_digits > 3 || dot_pos == -1 || dot_pos == 0 || dot_pos == file_name.size() - 1) {
         return "No";
     }
-
+    
     string before_dot = file_name.substr(0, dot_pos);
     string after_dot = file_name.substr(dot_pos + 1);
-
+    
     if (!isalpha(before_dot[0])) {
         return "No";
     }
-
+    
     if (after_dot != "txt" && after_dot != "exe" && after_dot != "dll") {
         return "No";
     }
-
+    
     return "Yes";
 }
 
 int main() {
     string file_name;
-    cin >> file_name;
-    
-    cout << file_name_check(file_name) << endl;
-    
+    std::cin >> file_name;
+
+    std::cout << file_name_check(file_name);
+
     return 0;
 }
