2fc3465ca46d8d319fd6beae96aa21b31cec6d59
beep boop <githubbot@vadim.me>
2024-07-15 15:53:55 +0200

    Solution 12 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/oq9mdlpd Prompt: ea0a57ba-cdde-4bb4-82bc-db972928d654.cpp: In function ‘int basement(std::vector< int>)’: ea0a57ba-cdde-4bb4-82bc-db972928d654.cpp:5:5: error: ‘size_t’ was not declared i n this scope; did you mean ‘std::size_t’?     5 |     size_t i; // Declare i outside the for loop       |     ^~~~~~       |     std::size_t In file included from /usr/include/c++/11/bits/stl_algobase.h:59,                  from /usr/include/c++/11/vector:60,                  from ea0a57ba-cdde-4bb4-82bc-db972928d654.cpp:1: /usr/include/x86_64-linux-gnu/c++/11/bits/c++config.h:280:33: note: ‘std::size_t ’ declared here   280 |   typedef __SIZE_TYPE__         size_t;       |                                 ^~~~~~ ea0a57ba-cdde-4bb4-82bc-db972928d654.cpp:6:10: error: ‘i’ was not declared in th is scope     6 |     for (i = 0; i < nums.size(); ++i) {       |          ^

--- basement.cpp
+++ basement.cpp
@@ -2,7 +2,8 @@
 
 int basement(std::vector<int> nums) {
     int sum = 0;
-    for (std::size_t i = 0; i < nums.size(); ++i) {
+    std::size_t i; // Declare i outside the for loop
+    for (i = 0; i < nums.size(); ++i) {
         sum += nums[i];
         if (sum < 0) {
             return i;
