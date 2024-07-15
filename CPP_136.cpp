972c3b1406a78314a9fba2ecb48ba7b297172a1c
beep boop <githubbot@vadim.me>
2024-07-15 18:20:25 +0200

    Solution 9 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ckn5i8fe Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 17cc77bf-8549-400c-aef9-3ebca64c50ce.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 17cc77bf-8549-400c-aef9-3ebca64c50ce.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 17cc77bf-8549-400c-aef9-3ebca64c50ce.cpp:16: 17cc77bf-8549-400c-aef9-3ebca64c50ce.cpp: In function ‘int main()’: 17cc77bf-8549-400c-aef9-3ebca64c50ce.cpp:26:20: error: ‘largest_smallest_integer s’ was not declared in this scope    26 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 17cc77bf-8549-400c-aef9-3ebca64c50ce.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,4 +1,4 @@
-bool issame(vector<int> a, vector<int> b) {
+bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
@@ -19,4 +19,5 @@ vector<int> largest_smallest_integers(vector<int> lst){
 
 int main() {
     assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
+    return 0;
 }
