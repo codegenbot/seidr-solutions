106b1f105bd66f8f33605158391dc7ec950d87a1
beep boop <githubbot@vadim.me>
2024-07-15 16:46:53 +0200

    Solution 9 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/th9qyyvm Prompt:    24 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ c7126cda-e6f4-4c36-8b5d-c1cdfdb25dff.cpp:24:36: error: expected primary-expressi on before ‘float’    24 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ c7126cda-e6f4-4c36-8b5d-c1cdfdb25dff.cpp:24:43: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from c7126cda-e6f4-4c36-8b5d-c1cdfdb25dff.cpp:23: c7126cda-e6f4-4c36-8b5d-c1cdfdb25dff.cpp: In function ‘int main()’: c7126cda-e6f4-4c36-8b5d-c1cdfdb25dff.cpp:33:20: error: ‘sort_even’ was not decla red in this scope    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ c7126cda-e6f4-4c36-8b5d-c1cdfdb25dff.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,15 +1,15 @@
 vector<float> sort_even(vector<float> l) {
     vector<float> even_values;
     vector<float> result = l;
-
+    
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
             even_values.push_back(l[i]);
         }
     }
-
+    
     sort(even_values.begin(), even_values.end());
-
+    
     int even_index = 0;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
@@ -17,6 +17,6 @@ vector<float> sort_even(vector<float> l) {
             even_index++;
         }
     }
-
+    
     return result;
 }
