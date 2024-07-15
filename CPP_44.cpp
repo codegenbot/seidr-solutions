9cd9389899e4f7012196e6cb72b0f52bcf84dcc5
beep boop <githubbot@vadim.me>
2024-07-15 22:53:05 +0200

    Solution 81 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/twiyy66a Prompt:    28 | int main(){       |     ^~~~ 7ef1937d-e76e-4056-a821-9328a31d896d.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 7ef1937d-e76e-4056-a821-9328a31d896d.cpp:27: 7ef1937d-e76e-4056-a821-9328a31d896d.cpp: In function ‘int main()’: 7ef1937d-e76e-4056-a821-9328a31d896d.cpp:31:42: error: ‘to_string’ was not decla red in this scope; did you mean ‘std::__cxx11::to_string’?    31 |         assert (change_base(x, x + 1) == to_string(x));       |                                          ^~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 7ef1937d-e76e-4056-a821-9328a31d896d.cpp:1: /usr/include/c++/11/bits/basic_string.h:6739:3: note: ‘std::__cxx11::to_string’ declared here  6739 |   to_string(long double __val)       |   ^~~~~~~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -13,17 +13,6 @@ std::string change_base(int num, int base) {
     return result;
 }
 
-int main_old() {
-    int x = 2;
-    for (int i = 2; i < 10; ++i) {
-        std::cout << "Base " << i << ": " << change_base(x, i) << std::endl;
-    }
-
-    assert(change_base(x, x + 1) == std::to_string(x));
-
-    return 0;
-}
-
 int main() {
     int x = 2;
     for (int i = 2; i < 10; ++i) {
