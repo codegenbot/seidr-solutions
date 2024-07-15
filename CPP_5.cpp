4da61f8f3dd2183e2d5163b7f065961625e5ab74
beep boop <githubbot@vadim.me>
2024-07-15 23:30:34 +0200

    Solution 82 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/vioanjrt Prompt:       |           ^~~~~~ In file included from 8f3cf9a7-2311-41c7-b46b-aba6f674c47b.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8f3cf9a7-2311-41c7-b46b-aba6f674c47b.cpp:4:34: error: expected primary-expressio n before ‘int’     4 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8f3cf9a7-2311-41c7-b46b-aba6f674c47b.cpp:4:39: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8f3cf9a7-2311-41c7-b46b-aba6f674c47b.cpp:3: 8f3cf9a7-2311-41c7-b46b-aba6f674c47b.cpp: In function ‘int main()’: 8f3cf9a7-2311-41c7-b46b-aba6f674c47b.cpp:13:20: error: ‘intersperse’ was not dec lared in this scope    13 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ 8f3cf9a7-2311-41c7-b46b-aba6f674c47b.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -1,29 +1,22 @@
 #include <vector>
+#include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-
-    return true;
+    return a == b;
 }
 
-std::vector<int> intersperse(std::vector<int> numbers, int delimiter) {
+std::vector<int> intersperse(const std::vector<int>& vec, int val) {
     std::vector<int> result;
-
-    for (int i = 0; i < numbers.size(); ++i) {
-        result.push_back(numbers[i]);
-
-        if (i < numbers.size() - 1) {
-            result.push_back(delimiter);
+    for (size_t i = 0; i < vec.size(); ++i) {
+        result.push_back(vec[i]);
+        if (i != vec.size() - 1) {
+            result.push_back(val);
         }
     }
-
     return result;
+}
+
+int main() {
+    assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
+    return 0;
 }
