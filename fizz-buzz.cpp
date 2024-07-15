5982924404c131ee1befaa84b4a031b7e720a73b
beep boop <githubbot@vadim.me>
2024-07-15 12:15:00 +0200

    Solution 10 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/38d3tueh Prompt: 0d4a856e-0cdf-416f-9cde-e5533348870d.cpp: In function ‘int main()’: 0d4a856e-0cdf-416f-9cde-e5533348870d.cpp:20:5: error: ‘cout’ was not declared in  this scope    20 |     cout << sol.fizzBuzz(15) << endl;       |     ^~~~ 0d4a856e-0cdf-416f-9cde-e5533348870d.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; 0d4a856e-0cdf-416f-9cde-e5533348870d.cpp:20:33: error: ‘endl’ was not declared i n this scope    20 |     cout << sol.fizzBuzz(15) << endl;       |                                 ^~~~ 0d4a856e-0cdf-416f-9cde-e5533348870d.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,11 +1,23 @@
 #include <string>
-std::string fizzBuzz(int x) {
-    if (x % 3 == 0 && x % 5 == 0)
-        return "FizzBuzz";
-    else if (x % 3 == 0)
-        return "Fizz";
-    else if (x % 5 == 0)
-        return "Buzz";
-    else
-        return std::to_string(x);
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    string fizzBuzz(int x) {
+        if(x % 3 == 0 && x % 5 == 0)
+            return "FizzBuzz";
+        else if(x % 3 == 0)
+            return "Fizz";
+        else if(x % 5 == 0)
+            return "Buzz";
+        else
+            return to_string(x);
+    }
+};
+
+int main() {
+    Solution sol;
+    cout << sol.fizzBuzz(15) << endl;
+    return 0;
 }
