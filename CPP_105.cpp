0cfd2db24e76d3d8e90c1ef54f95daeedbeadf28
beep boop <githubbot@vadim.me>
2024-07-15 20:54:14 +0200

    Solution 52 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/wktqj69z Prompt:       |                              ^~~~~~ 81d52fc5-de55-4bc8-9617-eda501b6869a.cpp:28:37: error: ‘string’ was not declared  in this scope    28 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 81d52fc5-de55-4bc8-9617-eda501b6869a.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 81d52fc5-de55-4bc8-9617-eda501b6869a.cpp:28:45: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 81d52fc5-de55-4bc8-9617-eda501b6869a.cpp:27: 81d52fc5-de55-4bc8-9617-eda501b6869a.cpp: In function ‘int main()’: 81d52fc5-de55-4bc8-9617-eda501b6869a.cpp:37:20: error: ‘by_length’ was not decla red in this scope    37 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 81d52fc5-de55-4bc8-9617-eda501b6869a.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,20 +1,6 @@
 #include <string>
 
-vector<string> by_length(vector<int> arr);
-
-bool issame(vector<string> a, vector<string> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
-}
+bool issame(vector<string> a, vector<string> b);
 
 vector<string> by_length(vector<int> arr){
     vector<string> result;
