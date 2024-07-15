109571acad7aa8e251408c661621a4ad4b44a8fb
beep boop <githubbot@vadim.me>
2024-07-15 19:20:09 +0200

    Solution 12 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/o3u5v4z3 Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,20 +1,29 @@
-map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
+map<string, int> number_map = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
+map<int, string> reverse_map;
+for(auto itr = number_map.begin(); itr != number_map.end(); ++itr) {
+    reverse_map[itr->second] = itr->first;
+}
 
-string sort_numbers(string numbers){
-    map<int, string> revNumMap;
-    for(auto const& pair : numMap) {
-        revNumMap[pair.second] = pair.first;
+sort(numbers.begin(), numbers.end());
+
+vector<int> nums;
+string result;
+string current_number;
+for(char c : numbers) {
+    if (c == ' ') {
+        nums.push_back(number_map[current_number]);
+        current_number = "";
+    } else {
+        current_number += c;
     }
-    
-    string result;
-    string token;
-    size_t pos = 0;
-    while ((pos = numbers.find(' ')) != string::npos) {
-        token = numbers.substr(0, pos);
-        result += revNumMap[numMap[token]] + " ";
-        numbers.erase(0, pos + 1);
-    }
-    result += revNumMap[numMap[numbers]];
-    
-    return result;
+}
+
+nums.push_back(number_map[current_number]);
+sort(nums.begin(), nums.end());
+
+for(int num : nums) {
+    result += reverse_map[num] + " ";
+}
+result.pop_back(); // remove extra space at the end
+return result;
 }
