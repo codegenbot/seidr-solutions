8894ee12ffa4a0aa1bc2a7a4c3e6789e7d08f795
beep boop <githubbot@vadim.me>
2024-07-15 18:20:03 +0200

    Solution 40 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i1v1lsd5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d886a56b-dcfe-40f1-9a6b-db8e3a3cf d3a.py", line 13, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d886a56b-dcfe-40f1-9a6b-db8e3a3cf d3a.py", line 8, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,6 @@
 def sort_third(l: list):
-    sorted_list = [sorted(l[i:i+3]) for i in range(0, len(l), 3) if i + 2 < len(l)]
-    sorted_list.sort(key=lambda x: x[2] if len(x) >= 3 else x[-1])
-    return sorted_list
+    return sorted([
+        sorted(l[i:i+3])
+        for i in range(0, len(l), 3)
+        if len(l[i:i+3]) == 3
+    ], key=lambda x: x[2])
