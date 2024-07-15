efe91a996705537a386f589eb5561ed25d601e6f
beep boop <githubbot@vadim.me>
2024-07-15 18:38:36 +0200

    Solution 21 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/44pqs2ak Prompt:       |     ^~~ 11eb2f7a-e89d-4897-9c79-bd69888375f1.cpp:7:5: error: expected unqualified-id bef ore ‘return’     7 |     return result;       |     ^~~~~~ 11eb2f7a-e89d-4897-9c79-bd69888375f1.cpp:8:1: error: expected declaration before  ‘}’ token     8 | }       | ^ In file included from 11eb2f7a-e89d-4897-9c79-bd69888375f1.cpp:10: 11eb2f7a-e89d-4897-9c79-bd69888375f1.cpp: In function ‘int main()’: 11eb2f7a-e89d-4897-9c79-bd69888375f1.cpp:12:35: error: ‘lst’ was not declared in  this scope    12 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 11eb2f7a-e89d-4897-9c79-bd69888375f1.cpp:12:13: error: ‘double_the_difference’ w as not declared in this scope    12 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ 11eb2f7a-e89d-4897-9c79-bd69888375f1.cpp:12:43: error: ‘odd_sum’ was not declare d in this scope    12 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -6,8 +6,8 @@
 long long double_the_difference(std::vector<float>& lst) {
     long long result = 0;
     for (float num : lst) {
-        if (num > 0 && num == (int)num && (int)num % 2 != 0) {
-            result += (long long)pow(num, 2);
+        if (num > 0 && static_cast<int>(num) == num && static_cast<int>(num) % 2 != 0) {
+            result += static_cast<long long>(std::pow(num, 2));
         }
     }
     return result;
