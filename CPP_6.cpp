6d8501fe0439a7a49f9ee5a30b76c40b70a15cfb
beep boop <githubbot@vadim.me>
2024-07-16 00:47:24 +0200

    Solution 38 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/7dt2upw7 Prompt:       |                    ^~~ 9d5f6971-0e3d-4be9-b4ba-3cb745c2dd5d.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9d5f6971-0e3d-4be9-b4ba-3cb745c2dd5d.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9d5f6971-0e3d-4be9-b4ba-3cb745c2dd5d.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9d5f6971-0e3d-4be9-b4ba-3cb745c2dd5d.cpp:24: 9d5f6971-0e3d-4be9-b4ba-3cb745c2dd5d.cpp: In function ‘int main()’: 9d5f6971-0e3d-4be9-b4ba-3cb745c2dd5d.cpp:34:20: error: ‘parse_nested_parens’ was  not declared in this scope    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ 9d5f6971-0e3d-4be9-b4ba-3cb745c2dd5d.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -5,34 +5,11 @@ bool issame(const vector<int>& v1, const vector<int>& v2) {
         return false;
     }
 
-    for (size_t i = 0; i < v1.size(); i++) {
+    for (int i = 0; i < v1.size(); i++) {
         if (v1[i] != v2[i]) {
             return false;
         }
     }
 
     return true;
-}
-
-vector<int> parse_nested_parens(string paren_string) {
-    vector<int> depths;
-    int max_depth = 0;
-    int current_depth = 0;
-
-    for (char c : paren_string) {
-        if (c == '(') {
-            current_depth++;
-            max_depth = max(max_depth, current_depth);
-        } else if (c == ')') {
-            current_depth--;
-        } else if (c == ' ') {
-            depths.push_back(max_depth);
-            max_depth = 0;
-            current_depth = 0;
-        }
-    }
-
-    depths.push_back(max_depth);
-
-    return depths;
 }
