f584d26885c3dc08a45744e8d9e58c8936938f7a
beep boop <githubbot@vadim.me>
2024-07-15 15:46:47 +0200

    Solution 36 of CPP/145, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/hnekootv Prompt: 394dfa11-3cee-47aa-9c30-fb3d1e737711.cpp:39:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    39 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 394dfa11-3cee-47aa-9c30-fb3d1e737711.cpp:7:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     7 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 394dfa11-3cee-47aa-9c30-fb3d1e737711.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 394dfa11-3cee-47aa-9c30-fb3d1e737711.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main(){       |     ^~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,19 +1,27 @@
-sort(nums.begin(), nums.end(), [](int a, int b) {
-    int sum_a = 0, sum_b = 0;
-    if (a < 0) a = -a;
-    if (b < 0) b = -b;
-    while (a > 0) {
-        sum_a += a % 10;
-        a /= 10;
-    }
-    while (b > 0) {
-        sum_b += b % 10;
-        b /= 10;
-    }
-    if (sum_a == sum_b) {
-        return a < b;
-    }
-    return sum_a < sum_b;
-});
+#include <vector>
+#include <algorithm>
+#include <cassert>
 
-return nums;
+using namespace std;
+
+vector<int> order_by_points(vector<int> nums) {
+    sort(nums.begin(), nums.end(), [](int a, int b) {
+        int sum_a = 0, sum_b = 0;
+        a = (a < 0) ? -a : a;
+        b = (b < 0) ? -b : b;
+        while (a > 0) {
+            sum_a += a % 10;
+            a /= 10;
+        }
+        while (b > 0) {
+            sum_b += b % 10;
+            b /= 10;
+        }
+        if (sum_a == sum_b) {
+            return a < b;
+        }
+        return sum_a < sum_b;
+    });
+
+    return nums;
+}
