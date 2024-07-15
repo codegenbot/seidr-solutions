7fdd2a424ea4be8fbd4d0f7b08c70413f9a7c153
beep boop <githubbot@vadim.me>
2024-07-15 19:39:19 +0200

    Solution 65 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/iw84ztid Prompt:       |                    ^~~~~ e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:11: e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp: In function ‘int main()’: e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ e2edfb31-26a1-4b3b-a3a3-c4e8811f861d.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-vector<float> get_positive(vector<float> l){
+bool is_same(vector<float> a, vector<float> b){
     vector<float> result;
     for(float num : l){
         if(num > 0){
@@ -8,20 +8,4 @@ vector<float> get_positive(vector<float> l){
         }
     }
     return result;
-}
-
-bool is_same(vector<float> a, vector<float> b){
-    return a == b;
-}
-
-int main(){
-    vector<float> v1 = {1.5, -2.0, 3.7, -4.2};
-    vector<float> v2 = {1.5, 3.7};
-    
-    vector<float> positive_v1 = get_positive(v1);
-    vector<float> positive_v2 = get_positive(v2);
-    
-    bool same = is_same(positive_v1, positive_v2);
-    
-    return 0;
 }
