be74e434f17aa5398089e08bd81a31b3f3dc8631
beep boop <githubbot@vadim.me>
2024-07-15 15:03:56 +0200

    Solution 2 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/kc1b65jm Prompt:       |                              ^~~~~~ 3c5760de-e1b8-4c9b-a625-1e5b444a85b3.cpp:14:37: error: ‘string’ was not declared  in this scope    14 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 3c5760de-e1b8-4c9b-a625-1e5b444a85b3.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3c5760de-e1b8-4c9b-a625-1e5b444a85b3.cpp:14:45: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3c5760de-e1b8-4c9b-a625-1e5b444a85b3.cpp:13: 3c5760de-e1b8-4c9b-a625-1e5b444a85b3.cpp: In function ‘int main()’: 3c5760de-e1b8-4c9b-a625-1e5b444a85b3.cpp:23:20: error: ‘reverse_delete’ was not declared in this scope    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 3c5760de-e1b8-4c9b-a625-1e5b444a85b3.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,11 +1,28 @@
-string result = "";
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
+}
+
+vector<string> reverse_delete(string s, string c) {
+    string result;
     for (char ch : s) {
         if (c.find(ch) == string::npos) {
             result += ch;
         }
     }
-    string reverse_result = result;
-    reverse(reverse_result.begin(), reverse_result.end());
-    
-    return {result, result == reverse_result ? "True" : "False"};
+    string reversed = result;
+    reverse(reversed.begin(), reversed.end());
+    return {result, result == reversed ? "True" : "False"};
+}
+
+int main() {
+    assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
+    return 0;
 }
