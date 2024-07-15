e21a6a2732301b350c9d27a9216e96ab7cbd9e2b
beep boop <githubbot@vadim.me>
2024-07-15 11:51:38 +0200

    Solution 24 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0h1ogjge Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b4ebba2f-58fd-4444-ab4d-22601ae57 672.py", line 38, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b4ebba2f-58fd-4444-ab4d-22601ae57 672.py", line 8, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,4 +1,3 @@
-```
 def sum_squares(lst):
-    total = sum(len(str(num)) ** len(str(num)) for num in lst)
-    return total
+    total = sum(num ** len(str(num)) for num in lst)
+    return total % 2 == 0
