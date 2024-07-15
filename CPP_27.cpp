3d59affdff919e009e3c06aebb7f6e998feb254f
beep boop <githubbot@vadim.me>
2024-07-15 16:29:21 +0200

    Solution 8 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/yd9ohg86 Prompt: 490886e1-a348-4877-bbff-3eb450e59f02.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string flip_case(string str){       | ^~~~~~ In file included from 490886e1-a348-4877-bbff-3eb450e59f02.cpp:12: 490886e1-a348-4877-bbff-3eb450e59f02.cpp: In function ‘int main()’: 490886e1-a348-4877-bbff-3eb450e59f02.cpp:14:13: error: ‘filp_case’ was not decla red in this scope    14 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,19 +1,21 @@
-#include <iostream>
 #include <string>
-#include <cctype>
+#include <iostream>
 #include <cassert>
 
-std::string flip_case(std::string str){
+using namespace std;
+
+string flip_case(string str){
     for(int i=0; i<str.length(); i++){
-        if(std::islower(str[i])){
-            str[i] = std::toupper(str[i]);
-        } else if(std::isupper(str[i])){
-            str[i] = std::tolower(str[i]);
+        if(islower(str[i])){
+            str[i] = toupper(str[i]);
+        } else if(isupper(str[i])){
+            str[i] = tolower(str[i]);
         }
     }
     return str;
 }
 
-int main() {
+int main(){
     assert(flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
+    return 0;
 }
