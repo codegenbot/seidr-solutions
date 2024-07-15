dd21710991af4703659f480e1df8785530d932a7
beep boop <githubbot@vadim.me>
2024-07-15 20:24:19 +0200

    Solution 95 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/xqtjn5h3 Prompt:       |                    ^~~~~ e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp:10:29: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp:10:43: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp:9: e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp: In function ‘int main()’: e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp:19:20: error: ‘derivative’ was not decl ared in this scope    19 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~ e9125c4b-f5c4-43bf-84d9-1daf52da46d6.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -1,33 +1,12 @@
 #include <vector>
 #include <cassert>
 
-bool issame(vector<float> a, vector<float> b);
+bool issame(std::vector<float> a, std::vector<float> b);
 
-vector<float> derivative(vector<float> xs);
-
-bool issame(vector<float> a, vector<float> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(size_t i = 0; i < a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
-vector<float> derivative(vector<float> xs){
-    vector<float> result;
-    for(size_t i = 1; i < xs.size(); i++){
-        result.push_back(xs[i] - xs[i-1]);
+std::vector<float> derivative(std::vector<float> xs){
+    std::vector<float> result;
+    for(int i = 1; i < xs.size(); i++){
+        result.push_back(xs[i] * i);
     }
     return result;
-}
-
-int main(){
-    vector<float> vec1 = {1.0, 2.0, 3.0, 4.0};
-    vector<float> vec2 = derivative(vec1);
-
-    assert(issame(vec2, {1.0, 1.0, 1.0}));
 }
