170f18700585683187001c65c386540529b33de7
beep boop <githubbot@vadim.me>
2024-07-15 11:42:55 +0200

    Solution 11 of Python/95, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ge16vpko Prompt: Complete the following code given the task description and function signature.

--- Python_95.py
+++ Python_95.py
@@ -1,6 +1,6 @@
-```
 def check_dict_case(dict):
     if not dict:
         return False
-    key_cases = set([key[:].lower() for key in dict])
-    return len(key_cases) <= 1
+    all_upper = all(key.isupper() for key in dict)
+    all_lower = all(key.islower() for key in dict)
+    return all_upper or all_lower
