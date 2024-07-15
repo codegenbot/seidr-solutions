1b7c62f9d383f58454dd612689b9a274fd5f3842
beep boop <githubbot@vadim.me>
2024-07-15 15:24:46 +0200

    Solution 42 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/psgckefr Prompt:       |             ^~~~~~ c5fe9f1d-8de8-4576-848e-27a48ef9a410.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c5fe9f1d-8de8-4576-848e-27a48ef9a410.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ c5fe9f1d-8de8-4576-848e-27a48ef9a410.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ c5fe9f1d-8de8-4576-848e-27a48ef9a410.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from c5fe9f1d-8de8-4576-848e-27a48ef9a410.cpp:24: c5fe9f1d-8de8-4576-848e-27a48ef9a410.cpp: In function ‘int main()’: c5fe9f1d-8de8-4576-848e-27a48ef9a410.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -3,10 +3,6 @@
 #include <algorithm>
 #include <cassert>
 
-bool is_same(std::vector<int> a, std::vector<int> b){
-    return a == b;
-}
-
 std::vector<int> sort_array(std::vector<int> arr) {
     std::sort(arr.begin(), arr.end(), [](int a, int b) {
         int count_a = __builtin_popcount(a);
@@ -20,7 +16,7 @@ std::vector<int> sort_array(std::vector<int> arr) {
 }
 
 int main() {
-    assert(is_same(sort_array({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
-    
+    assert(sort_array({2, 4, 8, 16, 32}) == std::vector<int>{2, 4, 8, 16, 32});
+
     return 0;
 }
