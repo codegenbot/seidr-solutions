bf9b7e8ac6b57ffde9cb790e45b960c7e60fa6d5
beep boop <githubbot@vadim.me>
2024-07-15 11:35:17 +0200

    Solution 29 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wwuv2xgc Prompt:       |                    ^~~ 8dcc362d-ebbc-43de-991f-eb1c7d79cd4f.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8dcc362d-ebbc-43de-991f-eb1c7d79cd4f.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8dcc362d-ebbc-43de-991f-eb1c7d79cd4f.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8dcc362d-ebbc-43de-991f-eb1c7d79cd4f.cpp:9: 8dcc362d-ebbc-43de-991f-eb1c7d79cd4f.cpp: In function ‘int main()’: 8dcc362d-ebbc-43de-991f-eb1c7d79cd4f.cpp:19:20: error: ‘maximum’ was not declare d in this scope    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 8dcc362d-ebbc-43de-991f-eb1c7d79cd4f.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -2,10 +2,14 @@
 #include <algorithm>
 
 bool issame() {
-    // this function is not used anywhere in the given problem description, 
-    // so we will leave it blank for now.
+    int main() {
+        assert(maximum({1, 2, 3, -23, 243, -400, 0}, 7) == vector<int>({0, 1, 2})); 
+    }
 }
 
-int main() {
-    assert(maximum({1, 2, 3, -23, 243, -400, 0}, 7) == vector<int>({0, -23, 1, 2, 3})); 
+vector<int> maximum(vector<int> arr, int k) {
+    vector<int> result(arr.begin(), arr.begin() + k);
+    sort(result.begin(), result.end());
+    return result;
 }
+```
