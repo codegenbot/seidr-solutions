90a2212183d27286b0398741d0e5b8e0cf620e0f
beep boop <githubbot@vadim.me>
2024-07-16 00:12:57 +0200

    Solution 76 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/brlulnfb Prompt: e7963048-5522-49ab-b533-33daee32b3a5.cpp: At global scope: e7963048-5522-49ab-b533-33daee32b3a5.cpp:16:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    16 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ e7963048-5522-49ab-b533-33daee32b3a5.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ e7963048-5522-49ab-b533-33daee32b3a5.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ e7963048-5522-49ab-b533-33daee32b3a5.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~ In file included from e7963048-5522-49ab-b533-33daee32b3a5.cpp:15: e7963048-5522-49ab-b533-33daee32b3a5.cpp: In function ‘int main()’: e7963048-5522-49ab-b533-33daee32b3a5.cpp:25:20: error: ‘get_odd_collatz’ was not  declared in this scope    25 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -3,12 +3,24 @@
 #include <algorithm>
 using namespace std;
 
-vector<int> get_odd_collatz(int n) {
-    // Implement the get_odd_collatz function here
-}
-
 bool issame(vector<int> a, vector<int> b) {
     return std::equal(a.begin(), a.end(), b.begin(), b.end());
+}
+
+vector<int> get_odd_collatz(int n) {
+    vector<int> collatz_seq;
+    collatz_seq.push_back(n);
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        if (n % 2 != 0) {
+            collatz_seq.push_back(n);
+        }
+    }
+    return collatz_seq;
 }
 
 int main() {
