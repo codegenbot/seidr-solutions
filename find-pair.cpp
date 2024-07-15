98c6037d118c5eb15c86faa4013809e2ea3d51e5
beep boop <githubbot@vadim.me>
2024-07-15 23:14:56 +0200

    Solution 12 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/r672b7kc Prompt: n this scope     6 |     if (result.first != -1 && result.second != -1) {       |         ^~~~~~ dd2a58c9-078b-410d-b46a-e96c202d7a7d.cpp:7:9: error: ‘cout’ was not declared in this scope     7 |         cout << nums[result.first] << endl;       |         ^~~~ dd2a58c9-078b-410d-b46a-e96c202d7a7d.cpp:7:17: error: ‘nums’ was not declared in  this scope     7 |         cout << nums[result.first] << endl;       |                 ^~~~ dd2a58c9-078b-410d-b46a-e96c202d7a7d.cpp:7:39: error: ‘endl’ was not declared in  this scope     7 |         cout << nums[result.first] << endl;       |                                       ^~~~ dd2a58c9-078b-410d-b46a-e96c202d7a7d.cpp:10:9: error: ‘cout’ was not declared in  this scope    10 |         cout << "Pair not found." << endl;       |         ^~~~ dd2a58c9-078b-410d-b46a-e96c202d7a7d.cpp:10:38: error: ‘endl’ was not declared i n this scope    10 |         cout << "Pair not found." << endl;       |                                      ^~~~

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-pair<int, int> findPair(vector<int>& nums, int target) {
+pair<int, int> findPair(vector<int> nums, int target) {
     for (int i = 0; i < nums.size(); ++i) {
         for (int j = i + 1; j < nums.size(); ++j) {
             if (nums[i] + nums[j] == target) {
@@ -12,19 +11,4 @@ pair<int, int> findPair(vector<int>& nums, int target) {
         }
     }
     return make_pair(-1, -1);
-}
-
-int main() {
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
-    pair<int, int> result = findPair(nums, target);
-
-    if (result.first != -1 && result.second != -1) {
-        cout << nums[result.first] << endl;
-        cout << nums[result.second] << endl;
-    } else {
-        cout << "Pair not found." << endl;
-    }
-
-    return 0;
 }
