add6b4d54c96002d81af16c7c13f5df2a82c06e5
beep boop <githubbot@vadim.me>
2024-07-15 14:18:40 +0200

    Solution 96 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/9x2q9hvg Prompt:       |                    ^~~ 1de611eb-8d70-49c7-8575-9469b49e7cf4.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1de611eb-8d70-49c7-8575-9469b49e7cf4.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1de611eb-8d70-49c7-8575-9469b49e7cf4.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1de611eb-8d70-49c7-8575-9469b49e7cf4.cpp:9: 1de611eb-8d70-49c7-8575-9469b49e7cf4.cpp: In function ‘int main()’: 1de611eb-8d70-49c7-8575-9469b49e7cf4.cpp:19:20: error: ‘compare’ was not declare d in this scope    19 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 1de611eb-8d70-49c7-8575-9469b49e7cf4.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -3,17 +3,25 @@
 #include <cassert>
 
 bool issame(vector<int> a, vector<int> b) {
-    vector<int> compare(vector<int> game, vector<int> guess);
-
-    vector<int> compare(vector<int> game, vector<int> guess) {
-        vector<int> result;
-        for (int i = 0; i < game.size(); ++i) {
-            result.push_back(abs(game[i] - guess[i]));
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
         }
-        return result;
     }
+    return true;
+}
 
-    int main() {
-        // Main function implementation
-        return 0;
-   }
+vector<int> compare(vector<int> game, vector<int> guess);
+
+vector<int> compare(vector<int> game, vector<int> guess) {
+    vector<int> result;
+    for (int i = 0; i < game.size(); ++i) {
+        result.push_back(abs(game[i] - guess[i]));
+    }
+    return result;
+}
+
+int main() {
+    // Main function implementation can be added here
+    return 0;
+}
