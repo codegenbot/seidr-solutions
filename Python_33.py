7b041083ffa0d0586bb8139b511d3178b9777652
beep boop <githubbot@vadim.me>
2024-07-15 11:58:32 +0200

    Solution 65 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/n4q5x7ep Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/913dcfe1-d536-432a-b42a-a29e0e973 c5f.py", line 16, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/913dcfe1-d536-432a-b42a-a29e0e973 c5f.py", line 11, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,8 +1,8 @@
-return [
-    (
-        sorted([x for x in l[i // 3 * 3 + min(i % 3, 3 - 1) :] if x % 3 == 0])[::-1]
-        if i % 3 != 0
-        else i
-    )
-    for i in range(len(l))
-]
+`return [
+        (
+            sorted([x for x in l[i//3*3+min(i%3,3-1):] if x % 3 == 0])[::-1]
+            if i % 3 != 0
+            else i
+        )
+        for i in range(len(l))
+    ]`
