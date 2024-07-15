8a2606819111e3dbb72e70dc5a40e54fa71bee70
beep boop <githubbot@vadim.me>
2024-07-15 12:39:04 +0200

    Solution 8 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/dgatj4fp Prompt: 82020908-8a35-42ec-ad3a-cf2aab79f11c.cpp: In function ‘std::string camelCase(std ::string)’: 82020908-8a35-42ec-ad3a-cf2aab79f11c.cpp:22:17: error: ‘j’ was not declared in t his scope    22 |             i = j - 1;       |                 ^

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,25 +1,30 @@
-#include <vector>
-#include <iostream>
-#include <string>
-
-using namespace std;
-
+```
 string camelCase(string s) {
     string result = "";
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == '-') {
-            for (++i; i <= s.size() && s[i] != '-'; i++) {
-                if (i == s.size()) break;
+            i++; // skip the '-'
+            while(i < s.size() && s[i] != '-' && s[i] != ' ') {
+                if (i == 0) {
+                    result += toupper(s[i]);
+                } else {
+                    result += tolower(s[i]);
+                }
+                i++;
             }
-            result += toupper(s[i]);
         } else if (s[i] == ' ') {
-            result += toupper(s[i]);
-            for (int k = i + 1; k < s.size(); k++) {
-                if (s[k] != ' ')
-                    result += tolower(s[k]);
-                else
+            for (int j = i + 1; j < s.size(); ) {
+                if (s[j] == ' ') {
                     break;
+                }
+                if (j == i + 1) {
+                    result += toupper(s[j]);
+                } else {
+                    result += tolower(s[j]);
+                }
+                j++;
             }
+            i = j - 1;
         } else {
             if (i > 0) {
                 result += tolower(s[i]);
@@ -29,12 +34,4 @@ string camelCase(string s) {
         }
     }
     return result;
-}
-
-int main() {
-    string s;
-    while (cin >> s) {
-        cout << camelCase(s) << endl;
-    }
-    return 0;
 }
