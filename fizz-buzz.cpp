f610d4035a4412b9cbb3d6981f674675f4bc505f
beep boop <githubbot@vadim.me>
2024-07-15 12:33:50 +0200

    Solution 6 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/a08td4t2 Prompt: n this scope   153 |     cout << "Enter a number: ";       |     ^~~~ 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:154:5: error: ‘cin’ was not declared in  this scope   154 |     cin >> num;       |     ^~~ 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:154:5: note: ‘std::cin’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:155:44: error: ‘endl’ was not declared in this scope   155 |     cout << solution.numberToWords(num) << endl;       |                                            ^~~~ 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,22 +1,13 @@
-```c++
-#include <iostream>
+#include <string>
 using namespace std;
 
 class Solution {
 public:
     string fizzBuzz(int num) {
-        if (num % 15 == 0) {
-            return "FizzBuzz";
-        }
-        else if (num % 5 == 0) {
-            return "Buzz";
-        }
-        else if (num % 3 == 0) {
-            return "Fizz";
-        }
-        else {
-            return to_string(num);
-        }
+        if (num % 15 == 0) return "FizzBuzz";
+        else if (num % 3 == 0 && num % 5 != 0) return "Fizz";
+        else if (num % 5 == 0 && num % 3 != 0) return "Buzz";
+        else return to_string(num);
     }
 };
 
