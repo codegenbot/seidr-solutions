2249a5c527f3fc42381ef2eb322b368972742c8c
beep boop <githubbot@vadim.me>
2024-07-15 14:11:08 +0200

    Solution 4 of Python/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/cot7hwaa Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/151151ae-bc6b-49ab-a3e2-fe59de1b4 db3.py", line 11, in <module>     check(add_elements)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/151151ae-bc6b-49ab-a3e2-fe59de1b4 db3.py", line 7, in check     add_elements([1], 1) == 1 AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_122.py
+++ Python_122.py
@@ -1,2 +1,2 @@
 def add_elements(arr, k):
-    return sum(num for num in arr[:k] if isinstance(num, int) and 10 <= num <= 99)
+    return sum(arr[:k]) if k == 0 else sum(num for num in arr[:k] if 10 <= num <= 99)
