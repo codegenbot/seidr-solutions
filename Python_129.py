1c405086932cb542df326f570716814c4c5019c4
beep boop <githubbot@vadim.me>
2024-07-15 19:06:06 +0200

    Solution 25 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/fhwk9d0c Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bddd24e6-df9f-4066-a35e-6b0a53f26 4d6.py", line 12, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bddd24e6-df9f-4066-a35e-6b0a53f26 4d6.py", line 9, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -1,5 +1,5 @@
 def minPath(grid, k):
     n = len(grid)
     flattened_grid = [elem for row in grid for elem in row]
-    idx = min(range(n * n), key=lambda x: flattened_grid[x])
+    idx = min(range(n*n), key=lambda x: flattened_grid[x])
     return [flattened_grid[idx]] * k
