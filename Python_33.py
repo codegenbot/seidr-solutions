557f53b7d2b5c2a6b7f27b2e5ba70e9fbd7dfc1a
beep boop <githubbot@vadim.me>
2024-07-15 18:11:53 +0200

    Solution 30 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i1v1lsd5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8043eff4-5328-4fcd-ad92-5a3ff9c0a 805.py", line 16, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8043eff4-5328-4fcd-ad92-5a3ff9c0a 805.py", line 11, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8043eff4-5328-4fcd-ad92-5a3ff9c0a 805.py", line 3, in sort_third     [   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8043eff4-5328-4fcd-ad92-5a3ff9c0a 805.py", line 4, in <listcomp>     sorted(l[i : i + 3], key=lambda x: x[2])   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8043eff4-5328-4fcd-ad92-5a3ff9c0a 805.py", line 4, in <lambda>     sorted(l[i : i + 3], key=lambda x: x[2])                                        ~^^^ TypeError: 'int' object is not subscriptable

--- Python_33.py
+++ Python_33.py
@@ -1,6 +1,9 @@
 def sort_third(l: list):
-    return sorted([
-        sorted(l[i:i+3])
-        for i in range(0, len(l), 3)
-        if i + 2 < len(l)
-    ], key=lambda x: x[2])
+    return sorted(
+        [
+            sorted(l[i : i + 3], key=lambda x: x)
+            for i in range(0, len(l), 3)
+            if i + 2 < len(l)
+        ],
+        key=lambda x: x[2],
+    )
