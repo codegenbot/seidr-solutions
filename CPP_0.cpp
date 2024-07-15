837893e6b69117b090c8fdaad9f87c93f152b54c
beep boop <githubbot@vadim.me>
2024-07-15 15:53:49 +0200

    Solution 23 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/7tkyptfg Prompt: 7609bd81-21ef-427f-b70e-e38b6a49d412.cpp:7:5: error: expected unqualified-id bef ore ‘return’     7 |     return false;       |     ^~~~~~ 7609bd81-21ef-427f-b70e-e38b6a49d412.cpp:8:1: error: expected declaration before  ‘}’ token     8 | }       | ^ 7609bd81-21ef-427f-b70e-e38b6a49d412.cpp: In function ‘int main()’: 7609bd81-21ef-427f-b70e-e38b6a49d412.cpp:12:5: error: ‘vector’ was not declared in this scope    12 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 7609bd81-21ef-427f-b70e-e38b6a49d412.cpp:12:12: error: expected primary-expressi on before ‘float’    12 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~ In file included from 7609bd81-21ef-427f-b70e-e38b6a49d412.cpp:10: 7609bd81-21ef-427f-b70e-e38b6a49d412.cpp:13:13: error: ‘has_close_elements’ was not declared in this scope    13 |     assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false) ;       |             ^~~~~~~~~~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -10,7 +10,6 @@ bool has_close_elements(std::vector<float> numbers, float threshold) {
     }
     return false;
 }
-
 int main() {
     std::vector<float> a = {1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
     assert(has_close_elements(a, 0.5) == false);
