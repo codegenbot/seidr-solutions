d88aaa93aebd73939ca7cea4677963627a244d82
beep boop <githubbot@vadim.me>
2024-07-15 18:33:17 +0200

    Solution 21 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ozm81xwh Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 96b71615-744d-4444-b19c-e5a25fe050d2.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 96b71615-744d-4444-b19c-e5a25fe050d2.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 96b71615-744d-4444-b19c-e5a25fe050d2.cpp:19: 96b71615-744d-4444-b19c-e5a25fe050d2.cpp: In function ‘int main()’: 96b71615-744d-4444-b19c-e5a25fe050d2.cpp:29:20: error: ‘order_by_points’ was not  declared in this scope    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 96b71615-744d-4444-b19c-e5a25fe050d2.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -8,7 +8,7 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 }
 
 std::vector<int> order_by_points(std::vector<int> nums) {
-    sort(nums.begin(), nums.end(), [](int a, int b) {
+    std::sort(nums.begin(), nums.end(), [](int a, int b) {
         int sum_a = 0, sum_b = 0;
         int temp_a = abs(a), temp_b = abs(b);
         while (temp_a > 0) {
@@ -20,7 +20,7 @@ std::vector<int> order_by_points(std::vector<int> nums) {
             temp_b /= 10;
         }
         if (sum_a == sum_b) {
-            return find(nums.begin(), nums.end(), a) < find(nums.begin(), nums.end(), b);
+            return std::find(nums.begin(), nums.end(), a) < std::find(nums.begin(), nums.end(), b);
         }
         return sum_a < sum_b;
     });
