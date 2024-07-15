d6b1c76aa49f01ba3a76fef53472ef63bf71e439
beep boop <githubbot@vadim.me>
2024-07-15 20:08:49 +0200

    Solution 13 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/v69eczs4 Prompt: Complete the following code given the task description and function signature.

--- Python_17.py
+++ Python_17.py
@@ -1,3 +1,5 @@
 def parse_music(music_string: str) -> List[int]:
-    notes_duration = {"o": 4, "o|": 2, ".|": 1}
-    return [notes_duration[note] for note in music_string.split()]
+    beats = {"o": 4, "o|": 2, ".|": 1}
+
+    notes = music_string.split()
+    return [beats[note] for note in notes]
