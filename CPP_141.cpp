798185528950812f1f60a09f7f0293ee53a83810
beep boop <githubbot@vadim.me>
2024-07-15 15:31:41 +0200

    Solution 2 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/yb3wiuku Prompt: fc7ab8d1-f347-4f53-a099-84e4a8b25f11.cpp:24:22: error: ‘file_name’ was not decla red in this scope    24 |     char firstChar = file_name[0];       |                      ^~~~~~~~~ fc7ab8d1-f347-4f53-a099-84e4a8b25f11.cpp:25:5: error: expected unqualified-id be fore ‘if’    25 |     if (!((firstChar >= 'a' && firstChar <= 'z') || (firstChar >= 'A' &&  firstChar <= 'Z'))) {       |     ^~ fc7ab8d1-f347-4f53-a099-84e4a8b25f11.cpp:29:5: error: expected unqualified-id be fore ‘return’    29 |     return "Yes";       |     ^~~~~~ fc7ab8d1-f347-4f53-a099-84e4a8b25f11.cpp:30:1: error: expected declaration befor e ‘}’ token    30 | }       | ^ In file included from fc7ab8d1-f347-4f53-a099-84e4a8b25f11.cpp:32: fc7ab8d1-f347-4f53-a099-84e4a8b25f11.cpp: In function ‘int main()’: fc7ab8d1-f347-4f53-a099-84e4a8b25f11.cpp:34:13: error: ‘file_name_check’ was not  declared in this scope    34 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,20 +1,27 @@
-int digit_count = 0;
-    int dot_count = 0;
-    int dot_index = -1;
-    for (int i = 0; i < file_name.length(); i++) {
+string checkFileName(string file_name) {
+    for (int i = 0; i < file_name.size(); i++) {
         if (file_name[i] >= '0' && file_name[i] <= '9') {
-            digit_count++;
-        } else if (file_name[i] == '.') {
-            dot_count++;
-            dot_index = i;
+            digitCount++;
+        }
+        else if (file_name[i] == '.') {
+            dotCount++;
+            dotIndex = i;
         }
     }
-    if (digit_count > 3 || dot_count != 1 || dot_index == 0 || dot_index == file_name.length() - 1) {
+
+    if (digitCount > 3 || dotCount != 1 || dotIndex == 0 || dotIndex == file_name.size() - 1) {
         return "No";
     }
-    string extension = file_name.substr(dot_index + 1);
+
+    string extension = file_name.substr(dotIndex + 1);
     if (extension != "txt" && extension != "exe" && extension != "dll") {
         return "No";
     }
+
+    char firstChar = file_name[0];
+    if (!((firstChar >= 'a' && firstChar <= 'z') || (firstChar >= 'A' && firstChar <= 'Z'))) {
+        return "No";
+    }
+
     return "Yes";
 }
