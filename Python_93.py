ccaaeee8263ce7f58e19bc50ad8e6cdbefe4e02a
beep boop <githubbot@vadim.me>
2024-07-15 21:17:56 +0200

    Solution 64 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/yxhj599w Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ade4a99e-baf5-42fb-b5b9-e0d9e6b10 a71.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ade4a99e-baf5-42fb-b5b9-e0d9e6b10 a71.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -4,8 +4,8 @@ def encode(message):
         char if not char.isalpha() else (
             chr(((ord(char.upper()) - ord("A") + 2) % 26) + ord("A")).swapcase()
                 if char.lower() not in vowels
-                else chr(ord(char) ^ 32)
+                else chr(ord(char) ^ 32) if char.islower() else char
         )
-        if char.isalpha() else char
+        if char.isalpha() or char.isspace() else char
         for char in message
     ])
