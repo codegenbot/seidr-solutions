8b375670ea49b595999b7aebae94315b42223e03
beep boop <githubbot@vadim.me>
2024-07-15 21:57:35 +0200

    Solution 20 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/yohdzufg Prompt:     9 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b0e633a8-d322-4466-a883-5af0ae9eda51.cpp:9:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from b0e633a8-d322-4466-a883-5af0ae9eda51.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from b0e633a8-d322-4466-a883-5af0ae9eda51.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ b0e633a8-d322-4466-a883-5af0ae9eda51.cpp:9:44: error: ‘b’ was not declared in th is scope     9 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from b0e633a8-d322-4466-a883-5af0ae9eda51.cpp:8: b0e633a8-d322-4466-a883-5af0ae9eda51.cpp: In function ‘int main()’: b0e633a8-d322-4466-a883-5af0ae9eda51.cpp:18:20: error: ‘separate_paren_groups’ w as not declared in this scope    18 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,12 +1,6 @@
 #include <vector>
 #include <string>
-#include <cassert>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+bool issame(std::vector<std::string> a, std::vector<std::string> b){
     return a == b;
-}
-
-int main() {
-    assert(issame(std::vector<std::string>{"()", "(())", "(()())"}, separate_paren_groups("( ) (( )) (( )( ))")));
-    return 0;
 }
