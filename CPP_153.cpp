454cf376c15454f1e805245f523a88eedf53e212
beep boop <githubbot@vadim.me>
2024-07-15 15:37:30 +0200

    Solution 2 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/408xzutd Prompt: 086589e4-20ec-4ad8-ac91-c333c34975b1.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string Strongest_Extension(string class_name, vector<string> extensions) {       | ^~~~~~ In file included from 086589e4-20ec-4ad8-ac91-c333c34975b1.cpp:25: 086589e4-20ec-4ad8-ac91-c333c34975b1.cpp: In function ‘int main()’: 086589e4-20ec-4ad8-ac91-c333c34975b1.cpp:27:13: error: ‘Strongest_Extension’ was  not declared in this scope    27 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,18 +1,21 @@
 string Strongest_Extension(string class_name, vector<string> extensions){
-    string strongest_extension = extensions[0];
-    int max_strength = 0;
+    string strongest_extension = "";
+    int max_strength = INT_MIN;
     
-    for (const string& ext : extensions) {
+    for(const string& extension : extensions){
         int cap_count = 0, sm_count = 0;
-        for (char c : ext) {
-            if (isupper(c)) cap_count++;
-            else if (islower(c)) sm_count++;
+        for(char c : extension){
+            if(isupper(c)){
+                cap_count++;
+            } else if(islower(c)){
+                sm_count++;
+            }
         }
-        
         int strength = cap_count - sm_count;
-        if (strength > max_strength || (strength == max_strength && ext < strongest_extension)) {
+        
+        if(strength > max_strength || (strength == max_strength && extension < strongest_extension)){
             max_strength = strength;
-            strongest_extension = ext;
+            strongest_extension = extension;
         }
     }
     
