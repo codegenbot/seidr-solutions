7ff39b39b19debb79fa4ce8e4fc42a28965e6c13
beep boop <githubbot@vadim.me>
2024-07-15 12:49:35 +0200

    Solution 15 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hj5kthnw Prompt: Program did not receive expected input

--- Python_17.py
+++ Python_17.py
@@ -1,7 +1,7 @@
 def parse_music(music_string: str) -> list:
     if not music_string:
         return []
-    music_string = ''.join(e for e in music_string if e in 'o.' or e in '|..- ')
+    music_string = ''.join(e for e in music_string if e in 'o\.|\\.\.\|')
     music_list = []
     i = 0
     while i < len(music_string):
@@ -14,4 +14,6 @@ def parse_music(music_string: str) -> list:
         elif music_string[i:i+3] == '.|..|':
             music_list.append([8, 4, 2])
             i += 3
+        else:
+            i += 1
     return music_list
