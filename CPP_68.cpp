bee9a91a8d689c4fd71726ed2ffbd01b21c83a08
beep boop <githubbot@vadim.me>
2024-07-15 14:25:27 +0200

    Solution 4 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/q9cfj4za Prompt:       |                    ^~~ 5ff48886-c71e-4743-874f-0de29cb35402.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5ff48886-c71e-4743-874f-0de29cb35402.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5ff48886-c71e-4743-874f-0de29cb35402.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5ff48886-c71e-4743-874f-0de29cb35402.cpp:21: 5ff48886-c71e-4743-874f-0de29cb35402.cpp: In function ‘int main()’: 5ff48886-c71e-4743-874f-0de29cb35402.cpp:31:20: error: ‘pluck’ was not declared in this scope    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 5ff48886-c71e-4743-874f-0de29cb35402.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,23 +1,12 @@
 #include <vector>
+#include <cassert>
 
-vector<int> pluck(vector<int> arr);
-
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
 }
 
-vector<int> pluck(vector<int> arr){
-    vector<int> result;
+std::vector<int> pluck(std::vector<int> arr) {
+    std::vector<int> result;
     int smallest_even = -1;
     int smallest_even_index = -1;
     
@@ -36,4 +25,8 @@ vector<int> pluck(vector<int> arr){
     return result;
 }
 
-assert(issame(pluck({7, 9, 7, 1}), {}));
+int main() {
+    assert(issame(pluck({7, 9, 7, 1}), std::vector<int>{}));
+    
+    return 0;
+}
