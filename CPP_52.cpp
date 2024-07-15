9f492628cde800a79455688a675ee0bcbb8fd98f
beep boop <githubbot@vadim.me>
2024-07-15 17:18:18 +0200

    Solution 54 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/cw5bfdcm Prompt: 41c5229d-e93a-408a-8e97-04b343755db7.cpp:1:26: note: to match this ‘(’     1 | auto below_threshold = [](const std::vector<int>& l, int t) -> bool {       |                          ^ 41c5229d-e93a-408a-8e97-04b343755db7.cpp: In lambda function: 41c5229d-e93a-408a-8e97-04b343755db7.cpp:8:2: error: expected ‘{’ before ‘;’ tok en     8 | };       |  ^ In file included from 41c5229d-e93a-408a-8e97-04b343755db7.cpp:10: 41c5229d-e93a-408a-8e97-04b343755db7.cpp: In function ‘int main()’: 41c5229d-e93a-408a-8e97-04b343755db7.cpp:12:32: error: no match for call to ‘(<l ambda(int)>) (<brace-enclosed initializer list>, int)’    12 |     assert (not(below_threshold({1, 8, 4, 10}, 10)));       |                 ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~ 41c5229d-e93a-408a-8e97-04b343755db7.cpp:12:32: note: candidate: ‘void (*)(int)’  (conversion) 41c5229d-e93a-408a-8e97-04b343755db7.cpp:12:32: note:   candidate expects 2 argu ments, 3 provided 41c5229d-e93a-408a-8e97-04b343755db7.cpp:1:24: note: candidate: ‘<lambda(int)>’     1 | auto below_threshold = [](const std::vector<int>& l, int t) -> bool {       |                        ^ 41c5229d-e93a-408a-8e97-04b343755db7.cpp:1:24: note:   candidate expects 1 argum ent, 2 provided

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,10 +1,1 @@
-auto below_threshold = [](const std::vector<int>& l, int t) -> bool {
-    for(int num : l){
-        if(num >= t){
-            return false;
-        }
-    }
-    return true;
-};
-
 assert (not(below_threshold(std::vector<int>{1, 8, 4, 10}, 10)));
