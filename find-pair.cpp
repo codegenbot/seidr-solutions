8d5fff2a4bf7815a94c8b3b5574a8e81bb400062
beep boop <githubbot@vadim.me>
2024-07-15 12:28:22 +0200

    Solution 2 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/2cwq16x1 Prompt: this scope    12 |         if (mp.find(target - nums[i]) != mp.end()) {       |             ^~ 941da3b3-5263-4982-9f21-0326b6c07f41.cpp:12:30: error: ‘nums’ was not declared i n this scope    12 |         if (mp.find(target - nums[i]) != mp.end()) {       |                              ^~~~ 941da3b3-5263-4982-9f21-0326b6c07f41.cpp:13:13: error: ‘cout’ was not declared i n this scope    13 |             cout << nums[i] << endl;       |             ^~~~ 941da3b3-5263-4982-9f21-0326b6c07f41.cpp:13:32: error: ‘endl’ was not declared i n this scope    13 |             cout << nums[i] << endl;       |                                ^~~~ 941da3b3-5263-4982-9f21-0326b6c07f41.cpp:17:9: error: ‘mp’ was not declared in t his scope    17 |         mp[nums[i]] = i;       |         ^~ 941da3b3-5263-4982-9f21-0326b6c07f41.cpp:17:12: error: ‘nums’ was not declared i n this scope    17 |         mp[nums[i]] = i;       |            ^~~~

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,11 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
 int main() {
-    int n;
+    int n, target;
     cin >> n;
     vector<int> nums(n);
     for (int i = 0; i < n; ++i) {
         cin >> nums[i];
     }
-    int target;
     cin >> target;
     
     map<int, int> mp;
