b42d975c5c070561c954c11f1517b25934059c99
beep boop <githubbot@vadim.me>
2024-07-15 13:35:48 +0200

    Solution 36 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/meqxroy8 Prompt: his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0e3c2cff-1198-49ac-8ce2-98277cc26263.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 0e3c2cff-1198-49ac-8ce2-98277cc26263.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 0e3c2cff-1198-49ac-8ce2-98277cc26263.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 0e3c2cff-1198-49ac-8ce2-98277cc26263.cpp:28: 0e3c2cff-1198-49ac-8ce2-98277cc26263.cpp: In function ‘int main()’: 0e3c2cff-1198-49ac-8ce2-98277cc26263.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -3,12 +3,6 @@
 #include <algorithm>
 #include <cassert>
 
-namespace std {
-    bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b){
-        return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
-    }
-}
-
 std::vector<std::string> filter_by_prefix(const std::vector<std::string>& vec, const std::string& prefix) {
     std::vector<std::string> filtered;
     for (const auto &str : vec) {
@@ -20,7 +14,8 @@ std::vector<std::string> filter_by_prefix(const std::vector<std::string>& vec, c
 }
 
 int main() {
-    assert(std::issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"),
-                  std::vector<std::string>{"xxx", "xxxAAA", "xxx"}));
+    assert(std::equal(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx").begin(),
+                      filter_by_prefix({"xxx", "xxxAAA", "xxx"}, "xxx").end(),
+                      std::vector<std::string>{"xxx", "xxxAAA", "xxx"}.begin()));
     return 0;
 }
