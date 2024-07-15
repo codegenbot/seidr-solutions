8c6f7f4367fe9ff719b8050124a856f32fc5815e
beep boop <githubbot@vadim.me>
2024-07-15 13:36:31 +0200

    Solution 49 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zdp78d1z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/566bde03-9655-4e24-aa5d-252368178 8ec.py", line 13, in <module>     check(intersperse)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/566bde03-9655-4e24-aa5d-252368178 8ec.py", line 10, in check     assert intersperse([2, 2, 2], 2) == [2, 2, 2, 2, 2]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_5.py
+++ Python_5.py
@@ -1,8 +1,6 @@
 def intersperse(numbers: list[int], delimiter: str) -> list[str]:
     if not numbers:
-        return []
-    result = [str(numbers[0])]
-    for num in numbers[1:]:
-        result.extend([str(delimiter), str(num)])
+        return [str(delimiter)]
+    result = [str(delimiter)] + [str(num) + str(delimiter) for num in numbers]
     result.append(str(delimiter))
     return result
