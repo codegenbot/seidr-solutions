75d5469367ba49609bedaee583a4c546a2da3ae7
beep boop <githubbot@vadim.me>
2024-07-15 19:35:42 +0200

    Solution 95 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/jc6v89py Prompt:       |                           ^~~~~~ a757597c-9d7c-4be4-940d-491873bc0b8a.cpp:21:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from a757597c-9d7c-4be4-940d-491873bc0b8a.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from a757597c-9d7c-4be4-940d-491873bc0b8a.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a757597c-9d7c-4be4-940d-491873bc0b8a.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a757597c-9d7c-4be4-940d-491873bc0b8a.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ a757597c-9d7c-4be4-940d-491873bc0b8a.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -13,7 +13,5 @@ std::vector<int> eat(int number, int need, int remaining) {
     return {eatenCarrots, leftCarrots};
 }
 
-int main() {
-    assert(issame(eat(4, 5, 1), {5, 0}));
-    return 0;
-}
+assert(issame(eat(4, 5, 1), {5, 0}));
+return 0;
