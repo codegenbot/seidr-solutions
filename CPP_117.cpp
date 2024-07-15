1c92e333a0d0e2560d1fa4dabeb74b291017a5be
beep boop <githubbot@vadim.me>
2024-07-15 21:08:07 +0200

    Solution 69 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/jgn2mnho Prompt:    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0684cc52-d86e-4883-b469-b1228ca13704.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0684cc52-d86e-4883-b469-b1228ca13704.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0684cc52-d86e-4883-b469-b1228ca13704.cpp:28: 0684cc52-d86e-4883-b469-b1228ca13704.cpp: In function ‘int main()’: 0684cc52-d86e-4883-b469-b1228ca13704.cpp:38:20: error: ‘select_words’ was not de clared in this scope    38 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 0684cc52-d86e-4883-b469-b1228ca13704.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,11 +1,11 @@
 vector<string> select_words(string s, int n);
 
-bool issame(vector<string> a, vector<string> b){
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
 int main() {
-    assert (issame(select_words("a b c d e f", 1), {"b", "c", "d", "f"}));
+    assert(issame(select_words("a b c d e f", 1), {"b", "c", "d", "f"}));
     return 0;
 }
 
