7abd3f03ea5f470a267a51ca4d68443b0e194762
beep boop <githubbot@vadim.me>
2024-07-15 15:02:50 +0200

    Solution 45 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/qbbzyyut Prompt: [1, ',', 2, ',', 3, ',', 4] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/081b5225-2df7-420b-a4a5-e54259f08 cc2.py", line 16, in <module>     check(intersperse)           ^^^^^^^^^^^ NameError: name 'intersperse' is not defined

--- Python_5.py
+++ Python_5.py
@@ -1,6 +1,5 @@
 def add_delimiter(numbers, delimiter):
-    return [str(num) + delimiter for num in numbers[:-1]] + [str(numbers[-1])]
-
+    return delimiter.join([str(num) for num in numbers])
 
 numbers = [1, 2, 3, 4]
 delimiter = ","
