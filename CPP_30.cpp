e5625a61760ef087ab8be5be061bb5b547f217a8
beep boop <githubbot@vadim.me>
2024-07-15 16:33:21 +0200

    Solution 8 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/hy8wxero Prompt:       |                    ^~~~~ 7f966ed2-2c64-459d-b28e-fb71407de74f.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 7f966ed2-2c64-459d-b28e-fb71407de74f.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 7f966ed2-2c64-459d-b28e-fb71407de74f.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 7f966ed2-2c64-459d-b28e-fb71407de74f.cpp:11: 7f966ed2-2c64-459d-b28e-fb71407de74f.cpp: In function ‘int main()’: 7f966ed2-2c64-459d-b28e-fb71407de74f.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 7f966ed2-2c64-459d-b28e-fb71407de74f.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,20 +1,19 @@
 #include <vector>
-#include <cassert>
 
-bool issame(std::vector<float> a, std::vector<float> b){
+vector<float> get_positive(vector<float> l){
+    vector<float> result;
+    for (float num : l) {
+        if (num > 0) {
+            result.push_back(num);
+        }
+    }
+    return result;
+}
+
+bool issame(vector<float> a, vector<float> b){
     return a == b;
 }
 
 int main(){
-    std::vector<float> get_positive(std::vector<float> l){
-        std::vector<float> result;
-        for (float num : l) {
-            if (num > 0) {
-                result.push_back(num);
-            }
-        }
-        return result;
-    }
-
-    assert(issame(get_positive({}), {}));
+    assert (issame(get_positive({}), {}));
 }
