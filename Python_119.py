cfb4cbec06f94f1d4c08681ffddb0351e3af9d66
beep boop <githubbot@vadim.me>
2024-07-15 14:13:28 +0200

    Solution 71 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rip4z73m Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f4d64cf6-1e5a-4800-b8d0-a60b30531 e78.py", line 16, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f4d64cf6-1e5a-4800-b8d0-a60b30531 e78.py", line 13, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,18 +1,9 @@
 def match_parens(lst):
-    stack = []
     for s in lst:
         if not s:  
             continue
-        parens_count = sum(1 for c in s if c in ['(', ')'])
-        for c in s:
-            if c == '(':
-                stack.append(c)
-            elif c == ')':
-                while len(stack) > 0 and stack.pop() != '(':
-                    pass
-                if len(stack) == 0:
-                    return 
-    if len(stack) > 0:
-        return 
-    else:
-        return
+        open_count = s.count('(')
+        close_count = s.count(')')
+        if abs(open_count - close_count) > 0:
+            return "No"
+    return "Yes"
