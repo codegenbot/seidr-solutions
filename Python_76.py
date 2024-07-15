05a035a10e1fb69b6d2568eeb4760a56a07bd28e
beep boop <githubbot@vadim.me>
2024-07-15 19:04:03 +0200

    Solution 24 of Python/76, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/rm9f1wge Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5fa82592-98a0-4638-8c88-423e6d1a9 4b2.py", line 1     return x > 0 and n > 0 and x == n ** int(x ** (1 / n))     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_76.py
+++ Python_76.py
@@ -1,8 +1,2 @@
 def check_expression(x, n):
     return x > 0 and n > 0 and x == n ** int(x ** (1 / n))
-
-x = int(input("Enter value for x: "))
-n = int(input("Enter value for n: "))
-
-result = check_expression(x, n)
-print(result)
