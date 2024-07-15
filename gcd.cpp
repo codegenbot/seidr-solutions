1d2bb794949f07ded10a347da2d64ffa0653c373
beep boop <githubbot@vadim.me>
2024-07-15 13:00:52 +0200

    Solution 18 of gcd, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/r5g9uq8h Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 18847d61-d454-42cd-868a-e8731fab950f.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- gcd.cpp
+++ gcd.cpp
@@ -1,42 +1,4 @@
 ```cpp
-#include <vector>
-#include <string>
-
-using namespace std;
-
-vector<int> findIndices(string text, string target) {
-    vector<int> indices;
-    int targetLength = target.length();
-    for (int i = 0; i <= text.length() - targetLength; i++) {
-        bool found = true;
-        for (int j = 0; j < targetLength; j++) {
-            if (text[i + j] != target[j]) {
-                found = false;
-                break;
-            }
-        }
-        if (found) {
-            indices.push_back(i);
-            while (i + targetLength <= text.length()) {
-                i++;
-                found = true;
-                for (int j = 0; j < targetLength; j++) {
-                    if (text[i + j] != target[j]) {
-                        found = false;
-                        break;
-                    }
-                }
-                if (found) {
-                    indices.push_back(i);
-                } else {
-                    break;
-                }
-            }
-        }
-    }
-    return indices;
-}
-
 int gcd(int a, int b) {
     while (b != 0) {
         int temp = b;
