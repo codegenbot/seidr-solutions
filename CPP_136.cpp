f9abe9b8072086f712fb8bf5bafbe4d54d147a2f
beep boop <githubbot@vadim.me>
2024-07-15 15:31:33 +0200

    Solution 4 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/uzf6m8sx Prompt:    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c1e5651a-cd16-4d6a-8475-58e6fb4fe290.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c1e5651a-cd16-4d6a-8475-58e6fb4fe290.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c1e5651a-cd16-4d6a-8475-58e6fb4fe290.cpp:24: c1e5651a-cd16-4d6a-8475-58e6fb4fe290.cpp: In function ‘int main()’: c1e5651a-cd16-4d6a-8475-58e6fb4fe290.cpp:34:20: error: ‘largest_smallest_integer s’ was not declared in this scope    34 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ c1e5651a-cd16-4d6a-8475-58e6fb4fe290.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -5,8 +5,10 @@
 
 using namespace std;
 
+bool issame(vector<int> a, vector<int> b);
+
 vector<int> largest_smallest_integers(vector<int> lst){
-    vector<int> result = {0, 0};
+    vector<int> result = {INT_MIN, INT_MAX};
     int max_neg = INT_MIN;
     int min_pos = INT_MAX;
     
@@ -28,11 +30,11 @@ vector<int> largest_smallest_integers(vector<int> lst){
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a[0] == b[0] && a[1] == b[1];
-}
-
 int main() {
     assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
     return 0;
+}
+
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
 }
