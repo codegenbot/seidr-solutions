8b3b6e76409a2991bc51342a3a1cba790d5c2871
beep boop <githubbot@vadim.me>
2024-07-15 12:39:14 +0200

    Solution 7 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rip4z73m Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d9c2de44-91ae-4075-a51b-776bfa5fa 5e3.py", line 18, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d9c2de44-91ae-4075-a51b-776bfa5fa 5e3.py", line 15, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -2,10 +2,12 @@ def match_parens(lst):
     stack = []
     for s in lst:
         for c in s:
-            if c == "(":
+            if c == '(':
                 stack.append(c)
-            elif c == ")":
+            elif c == ')':
                 if len(stack) == 0:
-                    return "No"
+                    return 'No'
                 stack.pop()
-    return "Yes" if len(stack) == 0 else "No"
+        if len(stack) > 0:
+            return 'No'
+    return 'Yes' if len(stack) == 0 else 'No'
