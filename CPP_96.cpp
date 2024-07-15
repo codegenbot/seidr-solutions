58b9afcc115e1e46d00e84a428327afae0ac4f78
beep boop <githubbot@vadim.me>
2024-07-15 14:56:30 +0200

    Solution 25 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/06y12ino Prompt:     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8b88b414-b7fd-45d5-875d-7748559e670c.cpp:8:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 8b88b414-b7fd-45d5-875d-7748559e670c.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 8b88b414-b7fd-45d5-875d-7748559e670c.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8b88b414-b7fd-45d5-875d-7748559e670c.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 8b88b414-b7fd-45d5-875d-7748559e670c.cpp:7: 8b88b414-b7fd-45d5-875d-7748559e670c.cpp: In function ‘int main()’: 8b88b414-b7fd-45d5-875d-7748559e670c.cpp:17:20: error: ‘count_up_to’ was not dec lared in this scope    17 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,12 +1,12 @@
 #include <vector>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) {
+    if (a.size() != b.size()) {
         return false;
     }
     
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
