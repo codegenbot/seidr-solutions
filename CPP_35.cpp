857a4c3def3f127d3257600dd71a2bb6c42a3119
beep boop <githubbot@vadim.me>
2024-07-15 16:45:09 +0200

    Solution 14 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/oon0lph0 Prompt: bc6a0523-7ce2-4685-b752-33d4534d92c9.cpp:2:35: error: ‘i’ does not name a type     2 |     for (int i = 1; i < l.size(); i++) {       |                                   ^ bc6a0523-7ce2-4685-b752-33d4534d92c9.cpp:7:5: error: expected unqualified-id bef ore ‘return’     7 |     return max;       |     ^~~~~~ bc6a0523-7ce2-4685-b752-33d4534d92c9.cpp:8:1: error: expected declaration before  ‘}’ token     8 | }       | ^ In file included from bc6a0523-7ce2-4685-b752-33d4534d92c9.cpp:10: bc6a0523-7ce2-4685-b752-33d4534d92c9.cpp: In function ‘int main()’: bc6a0523-7ce2-4685-b752-33d4534d92c9.cpp:12:17: error: ‘max_element’ was not dec lared in this scope    12 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ bc6a0523-7ce2-4685-b752-33d4534d92c9.cpp:12:13: error: ‘abs’ was not declared in  this scope    12 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -15,6 +15,6 @@ float max_element(std::vector<float> l) {
 }
 
 int main() {
-    assert (std::abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}) - 124) < 1e-4);
+    assert(std::abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}) - 124) < 1e-4);
     return 0;
 }
