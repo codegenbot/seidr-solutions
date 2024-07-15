6988dbabad9270f37cd6cfaf992673b5f598de77
beep boop <githubbot@vadim.me>
2024-07-16 00:09:22 +0200

    Solution 8 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/xy0pshe5 Prompt: In file included from f6b1f372-3288-4784-8b12-04bdb618b8b3.cpp:15: f6b1f372-3288-4784-8b12-04bdb618b8b3.cpp: In function ‘int main()’: f6b1f372-3288-4784-8b12-04bdb618b8b3.cpp:17:22: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    17 |     assert (simplify("1/5", "1/5") == false);       |                      ^~~~~       |                      |       |                      const char* f6b1f372-3288-4784-8b12-04bdb618b8b3.cpp:1:29: note:   initializing argument 1 o f ‘bool simplify(const int&, const int&)’     1 | bool simplify(const string& x, const string& n) {       |               ~~~~~~~~~~~~~~^ In file included from f6b1f372-3288-4784-8b12-04bdb618b8b3.cpp:15: f6b1f372-3288-4784-8b12-04bdb618b8b3.cpp:17:29: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    17 |     assert (simplify("1/5", "1/5") == false);       |                             ^~~~~       |                             |       |                             const char* f6b1f372-3288-4784-8b12-04bdb618b8b3.cpp:1:46: note:   initializing argument 2 o f ‘bool simplify(const int&, const int&)’     1 | bool simplify(const string& x, const string& n) {       |                                ~~~~~~~~~~~~~~^

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
 bool simplify(const string& x, const string& n) {
     int num1 = stoi(x.substr(0, x.find('/')));
     int den1 = stoi(x.substr(x.find('/') + 1));
@@ -9,5 +15,6 @@ bool simplify(const string& x, const string& n) {
 
 int main() {
     assert(simplify("1/5", "1/5") == false);
+
     return 0;
 }
