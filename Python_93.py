bc0c108c93a8ed6aeacc6d25f2ceb314dad2d941
beep boop <githubbot@vadim.me>
2024-07-15 20:41:27 +0200

    Solution 15 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/yxhj599w Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f508afae-59f4-4043-be5a-8c9da791c ad0.py", line 12, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f508afae-59f4-4043-be5a-8c9da791c ad0.py", line 8, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,3 +1,2 @@
 def encode(message):
-    vowels = 'aeiouAEIOU'
-    return ''.join([chr(ord(char) + 2) if char.isalpha() and char.lower() in vowels else char.swapcase() if char.isalpha() and char.islower() else char for char in message if char != ' '])
+    return ''.join([chr(ord(char) + 2) if char.isalpha() and char.lower() in 'aeiou' else char.swapcase() if char.isalpha() else char for char in message])
