471e10881c1113a6bb4652e509ef258e501682e3
beep boop <githubbot@vadim.me>
2024-07-15 13:07:09 +0200

    Solution 6 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/kbt8z4h5 Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0571de47-d65c-4ce4-a1be-1d31c28aa966.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0571de47-d65c-4ce4-a1be-1d31c28aa966.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0571de47-d65c-4ce4-a1be-1d31c28aa966.cpp:26: 0571de47-d65c-4ce4-a1be-1d31c28aa966.cpp: In function ‘int main()’: 0571de47-d65c-4ce4-a1be-1d31c28aa966.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 0571de47-d65c-4ce4-a1be-1d31c28aa966.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,3 +1,40 @@
-bool issame(vector<string> a, vector<string> b){
-    return a == b;
+bool issame (vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+vector<string> separate_paren_groups(string paren_string);
+
+vector<string> separate_paren_groups(string paren_string) {
+    vector<string> result;
+    string group;
+    int count = 0;
+
+    for (char c : paren_string) {
+        if (c == '(') {
+            if (count > 0) {
+                group += c;
+            }
+            count++;
+        } else if (c == ')') {
+            count--;
+            if (count > 0) {
+                group += c;
+            } else if (count == 0 && !group.empty()) {
+                result.push_back(group);
+                group.clear();
+            }
+        }
+    }
+
+    return result;
 }
