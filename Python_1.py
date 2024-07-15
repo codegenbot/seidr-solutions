7d2064b4bfe6376feeb8986e0400a1d35396a6b8
beep boop <githubbot@vadim.me>
2024-07-15 23:04:55 +0200

    Solution 75 of Python/1, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ms6xvrq2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6d64f067-7ba7-4fd3-958a-8b0709fa0 203.py", line 23, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6d64f067-7ba7-4fd3-958a-8b0709fa0 203.py", line 20, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ AssertionError

--- Python_1.py
+++ Python_1.py
@@ -1,6 +1,6 @@
 def separate_paren_groups(input_str):
-    stack = []
     result = []
+    stack = []
     current_sequence = ''
     
     for char in input_str:
@@ -12,9 +12,6 @@ def separate_paren_groups(input_str):
             current_sequence += ')'
             if not stack:
                 result.append(current_sequence)
-                current_sequence = ''  # Reset current_sequence here
-        else:
-            stack = []
-            current_sequence = ''  # Reset current_sequence here
+                current_sequence = ''
     
     return result
