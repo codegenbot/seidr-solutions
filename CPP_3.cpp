5d391d806876993c6bf5a321bb65327750e33949
beep boop <githubbot@vadim.me>
2024-07-15 11:37:49 +0200

    Solution 2 of CPP/3, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4bqx9w6m Prompt: 7a107a32-0405-49ef-b884-087ab15a76aa.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | bool below_zero(vector<int> operations){       |                 ^~~~~~ 7a107a32-0405-49ef-b884-087ab15a76aa.cpp:1:24: error: expected primary-expressio n before ‘int’     1 | bool below_zero(vector<int> operations){       |                        ^~~ In file included from 7a107a32-0405-49ef-b884-087ab15a76aa.cpp:10: 7a107a32-0405-49ef-b884-087ab15a76aa.cpp: In function ‘int main()’: 7a107a32-0405-49ef-b884-087ab15a76aa.cpp:12:23: error: ‘below_zero’ cannot be us ed as a function    12 |     assert (below_zero({1, -2, 2, -2, 5, -5, 4, -4}) == true);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_3.cpp
+++ CPP_3.cpp
@@ -1,17 +1,12 @@
-#include <iostream>
 #include <vector>
 
-bool below_zero(std::vector<int> operations){
+using namespace std;
+
+bool below_zero(vector<int> operations){
     int balance = 0;
     for(int operation : operations){
         balance += operation;
         if(balance < 0) return true;
     }
     return false;
-}
-
-int main(){
-    assert (below_zero({1, -2, 2, -2, 5, -5, 4, -4}) == true);
-    std::cout << "Code works fine." << std::endl;
-    return 0;
 }
