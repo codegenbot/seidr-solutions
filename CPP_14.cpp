117a45ec65583ffe1d6df679f5d411576181d262
beep boop <githubbot@vadim.me>
2024-07-15 22:10:04 +0200

    Solution 6 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/n7m88gtx Prompt: d586d2d6-b679-4754-b24d-9280c63f2bc9.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ d586d2d6-b679-4754-b24d-9280c63f2bc9.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -9,28 +9,6 @@ vector<string> all_prefixes(string str);
 
 bool issame(vector<string> a, vector<string> b);
 
-vector<string> all_prefixes(string str) {
-    vector<string> prefixes;
-    string prefix = "";
-    for (char c : str) {
-        prefix += c;
-        prefixes.push_back(prefix);
-    }
-    return prefixes;
-}
-
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int main() {
     assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
     cout << "Test passed" << endl;
