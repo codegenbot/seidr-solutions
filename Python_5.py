d72ca5fdbc2e54f595861df00e5b995823a8867b
beep boop <githubbot@vadim.me>
2024-07-15 13:55:03 +0200

    Solution 70 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zdp78d1z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/497c9c5b-3bb9-4563-bc42-e7db06f44 a4c.py", line 10, in <module>     check(intersperse)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/497c9c5b-3bb9-4563-bc42-e7db06f44 a4c.py", line 7, in check     assert intersperse([2, 2, 2], 2) == [2, 2, 2, 2, 2]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_5.py
+++ Python_5.py
@@ -1,9 +1,6 @@
 def intersperse(numbers: list[int], delimiter: str) -> list[str]:
-    if len(numbers) == 0:
-        return []
-    result = [str(delimiter)]
-    for i in range(len(numbers)):
+    result = [str(numbers[0])]
+    for i in range(1, len(numbers)):
+        result.extend([str(delimiter)])
         result.append(str(numbers[i]))
-        if i < len(numbers) - 1:
-            result.append(str(delimiter))
     return result
