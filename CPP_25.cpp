4ee0f78417ef378b1a1b9f6b195473eb38cc6c5e
beep boop <githubbot@vadim.me>
2024-07-15 13:37:04 +0200

    Solution 36 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/rv9h2tpo Prompt:       |             ^~~~~~ aeb6a0d9-dc57-4940-8006-cea810d64a6f.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ aeb6a0d9-dc57-4940-8006-cea810d64a6f.cpp:29:39: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<int> a,vector<int>b){       |                                       ^ aeb6a0d9-dc57-4940-8006-cea810d64a6f.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ aeb6a0d9-dc57-4940-8006-cea810d64a6f.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from aeb6a0d9-dc57-4940-8006-cea810d64a6f.cpp:28: aeb6a0d9-dc57-4940-8006-cea810d64a6f.cpp: In function ‘int main()’: aeb6a0d9-dc57-4940-8006-cea810d64a6f.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -18,3 +18,9 @@ std::vector<int> factorize(int n) {
 bool is_same(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
+
+int main() {
+    assert(is_same(factorize(3 * 2 * 3), {2, 3, 3}));
+
+    return 0;
+}
