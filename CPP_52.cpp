ef0c9b91efcc45ab753bf70154b3c92ac3b6ec37
beep boop <githubbot@vadim.me>
2024-07-15 17:09:27 +0200

    Solution 21 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/cw5bfdcm Prompt: cf97f0ae-00bb-498c-8762-a65298e95985.cpp:1:1: error: expected unqualified-id bef ore ‘for’     1 | for (int num : l) {       | ^~~ cf97f0ae-00bb-498c-8762-a65298e95985.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return true;       |     ^~~~~~ cf97f0ae-00bb-498c-8762-a65298e95985.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from cf97f0ae-00bb-498c-8762-a65298e95985.cpp:9: cf97f0ae-00bb-498c-8762-a65298e95985.cpp: In function ‘int main()’: cf97f0ae-00bb-498c-8762-a65298e95985.cpp:11:17: error: ‘below_threshold’ was not  declared in this scope    11 |     assert (not(below_threshold({1, 8, 4, 10}, 10)));       |                 ^~~~~~~~~~~~~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,5 +1,3 @@
-assert(below_threshold({1, 2, 3, 4, 5}, 10));
-assert(!below_threshold({8, 9, 10, 11, 12}, 7));
-assert(below_threshold({0, 0, 0}, 1));
-assert(!below_threshold({2, 4, 6, 8, 10}, 3));
-assert(!below_threshold({-1, -3, -5}, -10));
+assert(below_threshold({8, 9, 10}, 12) == true);
+assert(below_threshold({5, 6, 7}, 5) == false);
+assert(below_threshold({2, 3, 4}, 1) == true);
