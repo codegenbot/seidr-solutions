25aedcf0290b98d44c7e8a041a65cf6ac093b140
beep boop <githubbot@vadim.me>
2024-07-15 17:32:00 +0200

    Solution 22 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/qnepzmfc Prompt: 1920be02-b027-4a7a-96ea-ef6466ae59bf.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1920be02-b027-4a7a-96ea-ef6466ae59bf.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1920be02-b027-4a7a-96ea-ef6466ae59bf.cpp:17: 1920be02-b027-4a7a-96ea-ef6466ae59bf.cpp: In function ‘int main()’: 1920be02-b027-4a7a-96ea-ef6466ae59bf.cpp:27:20: error: ‘total_match’ was not dec lared in this scope    27 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 1920be02-b027-4a7a-96ea-ef6466ae59bf.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 1920be02-b027-4a7a-96ea-ef6466ae59bf.cpp: In function ‘int total_chars(int)’: 1920be02-b027-4a7a-96ea-ef6466ae59bf.cpp:7:1: warning: control reaches end of no n-void function [-Wreturn-type]     7 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -11,15 +11,19 @@ int total_chars(const std::vector<std::string>& lst) {
 }
 
 bool total_match(const std::vector<std::string>& lst1, const std::vector<std::string>& lst2) {
-    return total_chars(lst1) < total_chars(lst2) ? true : (total_chars(lst1) == total_chars(lst2) ? true : false);
+    if (total_chars(lst1) < total_chars(lst2)) {
+        return true;
+    } else {
+        return total_chars(lst1) == total_chars(lst2) ? true : false;
+    }
 }
 
 int main() {
-    // Test your code here using assert statements
-    std::vector<std::string> lst1 = {"apple", "banana"};
-    std::vector<std::string> lst2 = {"kiwi", "orange"};
+    // Main function for testing the code
+    std::vector<std::string> test1 = {"apple", "banana", "orange"};
+    std::vector<std::string> test2 = {"car", "bus"};
 
-    assert(!total_match(lst1, lst2));
+    assert(total_match(test1, test2) == true);
 
     return 0;
 }
