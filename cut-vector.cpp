88dc7e8faeeccdd78280937b33e373595e9e060d
beep boop <githubbot@vadim.me>
2024-07-15 12:24:01 +0200

    Solution 0 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/r9npe7x9 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,40 +1,55 @@
 #include <vector>
 using namespace std;
 
-vector<int> cutVector(vector<int>& nums) {
-    int minDiff = INT_MAX;
-    int cutIndex = -1;
+vector<vector<int>> cutVector(vector<int> v) {
+    int n = v.size();
+    vector<vector<int>> res(2);
     
-    for(int i=0; i<nums.size()-1; i++){
-        int diff = abs(nums[i] - nums[i+1]);
-        if(diff < minDiff){
-            minDiff = diff;
-            cutIndex = i;
+    for(int i = 0; i < n; i++) {
+        if(i == 0 || i == n-1)
+            res[0].push_back(v[i]);
+        else {
+            if(abs(v[i] - v[0]) <= abs(v[n-1] - v[i])) {
+                res[0].clear();
+                for(int j = 0; j < i; j++)
+                    res[0].push_back(v[j]);
+                res[0].push_back(v[i]);
+                break;
+            }
+            else {
+                res[0].clear();
+                for(int j = i; j < n; j++)
+                    res[0].push_back(v[j]);
+                break;
+            }
         }
     }
     
-    vector<int> left, right;
-    left.assign(cutIndex+1, nums.begin());
-    right.assign(nums.size()-cutIndex-1, nums.begin()+cutIndex+1);
+    if(res[0].size() != n) {
+        for(int i = 0; i < n; i++) {
+            if(i == 0 || i == n-1)
+                res[1].push_back(v[i]);
+            else {
+                if(abs(v[i] - v[0]) <= abs(v[n-1] - v[i])) {
+                    res[1].clear();
+                    for(int j = i; j < n; j++)
+                        res[1].push_back(v[j]);
+                    break;
+                }
+                else {
+                    res[1].clear();
+                    for(int j = 0; j < i; j++)
+                        res[1].push_back(v[j]);
+                    break;
+                }
+            }
+        }
+    }
     
-    return {left, right};
+    return res;
 }
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> nums(n);
-    for(int i=0; i<n; i++){
-        cin >> nums[i];
-    }
-    pair<vector<int>, vector<int>> result = cutVector(nums);
-    cout << "1 ";
-    for(auto x: result.first) {
-        cout << x << " ";
-    }cout<<endl;
-    cout << "0 ";
-    for(auto x: result.second) {
-        cout << x << " ";
-    }cout<<endl;
+    // Your code here
     return 0;
 }
