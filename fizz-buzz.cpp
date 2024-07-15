65282cf33e2a3159897e4b67f6864c1e92e66fa9
beep boop <githubbot@vadim.me>
2024-07-15 12:28:10 +0200

    Solution 14 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/38d3tueh Prompt: 8a7d4f8c-a72d-40bd-a658-da7718927c66.cpp: In function ‘int main()’: 8a7d4f8c-a72d-40bd-a658-da7718927c66.cpp:20:5: error: ‘cout’ was not declared in  this scope    20 |     cout << sol.fizzBuzz(15) << endl;       |     ^~~~ 8a7d4f8c-a72d-40bd-a658-da7718927c66.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; 8a7d4f8c-a72d-40bd-a658-da7718927c66.cpp:20:33: error: ‘endl’ was not declared i n this scope    20 |     cout << sol.fizzBuzz(15) << endl;       |                                 ^~~~ 8a7d4f8c-a72d-40bd-a658-da7718927c66.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
+```c
 #include <string>
+#include <iostream>
+
 using namespace std;
 
 class Solution {
 public:
     string fizzBuzz(int x) {
-        if(x % 3 == 0 && x % 5 == 0)
+        if(x % 15 == 0)
             return "FizzBuzz";
         else if(x % 3 == 0)
             return "Fizz";
