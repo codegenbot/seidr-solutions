c0e2e405f04ae8d5595cfeaa94c15b26d237594f
beep boop <githubbot@vadim.me>
2024-07-15 13:39:55 +0200

    Solution 20 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/dob9ydvf Prompt:                  from bf06b93b-5222-4f66-9133-059cc2351d7e.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from bf06b93b-5222-4f66-9133-059cc2351d7e.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ bf06b93b-5222-4f66-9133-059cc2351d7e.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bf06b93b-5222-4f66-9133-059cc2351d7e.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ bf06b93b-5222-4f66-9133-059cc2351d7e.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -3,7 +3,17 @@
 #include <vector>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    
+    return true;
 }
 
 std::vector<int> unique(const std::vector<int>& l) {
@@ -11,4 +21,9 @@ std::vector<int> unique(const std::vector<int>& l) {
     std::sort(temp.begin(), temp.end());
     temp.erase(std::unique(temp.begin(), temp.end()), temp.end());
     return temp;
+}
+
+int main() {
+    assert(issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}), std::vector<int>{0, 2, 3, 5, 9, 123}));
+    return 0;
 }
