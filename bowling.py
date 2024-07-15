a259a6b23e6e8041802406b20c0fea07d15cc302
beep boop <githubbot@vadim.me>
2024-07-15 16:08:20 +0200

    Solution 69 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/s9idvauz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b1bad2cd-3d6a-49ff-8734-1d6e7c4a5 922.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,10 +1,10 @@
-```
+```Python
 def bowling_score(frames):
     frames_list = [int(x) for x in frames.replace('/', '-').split('-')]
     
     score = 0
-    
     roll = 0
+    
     for i, frame in enumerate(frames_list[:10]):
         if len(str(frame)) == 1:
             if i < 8:
@@ -26,8 +26,11 @@ def bowling_score(frames):
                 if i == 9:
                     score += (frames_list[8] * 3) + frames_list[9]
         else:
-            if int(str(frame)[0]) + int(str(frame)[1]) < 11:
-                score += int(str(frame)[:2])
+            if frame == 10:
+                score += frame * 2
+                roll = 3
+            elif int(str(frame)[0]) + int(str(frame)[1]) < 11:
+                score += frame
                 roll = 2
             else:
                 first_roll = int(str(frame)[0])
@@ -36,10 +39,7 @@ def bowling_score(frames):
                     next_frame = frames_list[9] + frames_list[10]
                     score += (first_roll + second_roll * 2)
                 elif i < 8 and len(str(frames_list[i+1])) > 1:
-                    if roll == 2:
-                        score += int(str(frame)[0]) + int(str(frame)[1])
-                    else:
-                        score += first_roll
+                    score += frame
         if roll == 3:
             roll = 0
     return score
