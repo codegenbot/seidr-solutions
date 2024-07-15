0ca6b55fedb595755837ea99d2db6bb997aaaaff
beep boop <githubbot@vadim.me>
2024-07-15 22:52:28 +0200

    Solution 29 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/jl6zpipe Prompt: a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:28: a38359ad-e862-46a8-9f86-54f9d5edb461.cpp: In function ‘int main()’: a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:38:20: error: ‘common’ was not declared  in this scope; did you mean ‘CustomVectorFunctions::common’?    38 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:12:22: note: ‘CustomVectorFunctions::co mmon’ declared here    12 |     std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {       |                      ^~~~~~ In file included from a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:28: a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -21,8 +21,6 @@ namespace CustomVectorFunctions {
 
 int main() {
     using namespace CustomVectorFunctions;
-    
-    assert(issame(common({4, 3, 2, 8}, {}), {}));
-    
+    assert(issame(CustomVectorFunctions::common({4, 3, 2, 8}, {}), {}));
     return 0;
 }
