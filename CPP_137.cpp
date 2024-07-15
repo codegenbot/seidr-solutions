5c7ab30268e6b22b51313c8ad174cf983eade6fa
beep boop <githubbot@vadim.me>
2024-07-15 15:32:29 +0200

    Solution 18 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/tla8pqbu Prompt:    29 | int main(){       |     ^~~~ 2a580819-56f0-46e6-a32f-9ea680079fcb.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 2a580819-56f0-46e6-a32f-9ea680079fcb.cpp:28: 2a580819-56f0-46e6-a32f-9ea680079fcb.cpp: In function ‘int main()’: 2a580819-56f0-46e6-a32f-9ea680079fcb.cpp:30:13: error: ‘boost’ has not been decl ared    30 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 2a580819-56f0-46e6-a32f-9ea680079fcb.cpp:30:35: error: expected primary-expressi on before ‘>’ token    30 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 2a580819-56f0-46e6-a32f-9ea680079fcb.cpp:30:37: error: ‘compare_one’ was not dec lared in this scope; did you mean ‘compare’?    30 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,16 +1,26 @@
-#include <any>
+#include <iostream>
 #include <string>
+#include <boost/any.hpp>
 
 using namespace std;
 
-any compare(any a, any b) {
+boost::any compare(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return (any_cast<int>(a) >= any_cast<int>(b)) ? a : b;
+        return (boost::any_cast<int>(a) >= boost::any_cast<int>(b)) ? a : b;
     } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        return (any_cast<float>(a) >= any_cast<float>(b)) ? a : b;
+        return (boost::any_cast<float>(a) >= boost::any_cast<float>(b)) ? a : b;
     } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return (stof(any_cast<string>(a)) >= stof(any_cast<string>(b))) ? a : b;
+        return (stof(boost::any_cast<string>(a)) >= stof(boost::any_cast<string>(b))) ? a : b;
     }
 
     return a; // Handle other types, return the first argument by default
+}
+
+int main() {
+    // Input from user
+    int int1, int2;
+    cin >> int1 >> int2;
+    cout << boost::any_cast<int>(compare(int1, int2)) << endl;
+
+    return 0;
 }
