3a16c75ba46d6d5d2fd374a5850861a8f4d86ed3
beep boop <githubbot@vadim.me>
2024-07-15 19:40:21 +0200

    Solution 47 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/r9npe7x9 Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -6,48 +6,29 @@ using namespace std;
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
-    
-    for(int i = 0; i < n; i++) {
-        if(i == 0 || i == n-1)
-            res[0].push_back(v[i]);
-        else {
-            if(abs(v[i] - v[0]) <= abs(v[n-1] - v[i])) {
-                res[0].clear();
-                for(int j = 0; j < i; j++)
-                    res[0].push_back(v[j]);
-                res[0].push_back(v[i]);
-                break;
-            }
-            else {
-                res[0].clear();
-                for(int j = i; j < n; j++)
-                    res[0].push_back(v[j]);
-                break;
-            }
+
+    if(n == 1) {
+        res[0] = v;
+        return res;
+    }
+
+    int minDiff = INT_MAX, idx = -1;
+    for(int i = 1; i < n; i++) {
+        int diff = abs(v[i] - v[0]);
+        if(diff <= minDiff) {
+            minDiff = diff;
+            idx = i;
         }
     }
-    
-    if(res[0].size() != n) {
-        for(int i = 0; i < n; i++) {
-            if(i == 0 || i == n-1)
-                res[1].push_back(v[i]);
-            else {
-                if(abs(v[i] - v[0]) <= abs(v[n-1] - v[i])) {
-                    res[1].clear();
-                    for(int j = i; j < n; j++)
-                        res[1].push_back(v[j]);
-                    break;
-                }
-                else {
-                    res[1].clear();
-                    for(int j = 0; j < i; j++)
-                        res[1].push_back(v[j]);
-                    break;
-                }
-            }
-        }
-    }
-    
+
+    res[0].resize(idx);
+    for(int i = 0; i < idx; i++)
+        res[0].push_back(v[i]);
+
+    res[1] = v;
+    for(int i = idx; i < n; i++)
+        res[1].push_back(v[i]);
+
     return res;
 }
 
