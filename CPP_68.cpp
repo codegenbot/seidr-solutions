ebb174991fad8d00f59d98217c6ae03432cdef77
beep boop <githubbot@vadim.me>
2024-07-15 22:56:04 +0200

    Solution 5 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/eda9pndp Prompt: in this scope     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 98545bc3-3c38-4c35-9274-87a8d1d7ca55.cpp:9:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 98545bc3-3c38-4c35-9274-87a8d1d7ca55.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 98545bc3-3c38-4c35-9274-87a8d1d7ca55.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 98545bc3-3c38-4c35-9274-87a8d1d7ca55.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 98545bc3-3c38-4c35-9274-87a8d1d7ca55.cpp:8: 98545bc3-3c38-4c35-9274-87a8d1d7ca55.cpp: In function ‘int main()’: 98545bc3-3c38-4c35-9274-87a8d1d7ca55.cpp:18:20: error: ‘pluck’ was not declared in this scope    18 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,6 +1,6 @@
 #include <vector>
-#include <cassert>
+#include <algorithm>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b){
-    return a == b;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return std::is_permutation(a.begin(), a.end(), b.begin(), b.end());
 }
