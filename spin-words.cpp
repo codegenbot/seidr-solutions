3182e683b5ea46deccf753cead57971e19fc26b8
beep boop <githubbot@vadim.me>
2024-07-15 23:32:02 +0200

    Solution 53 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/jrlqtwl5 Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 786cb273-c375-4d14-84a0-794549b9e67c.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<const char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 786cb273-c375-4d14-84a0-794549b9e67c.cpp:5:26:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,1 +1,27 @@
-std::string::size_type pos = 0;
+```cpp
+#include <string>
+
+std::string spinWords(std::string str) {
+    std::string result = "";
+    std::string::size_type pos = 0;
+    
+    while ((pos = str.find(" ")) != std::string::npos) {
+        std::string::size_type len = str.find(" ", pos + 1);
+        if (len == std::string::npos)
+            len = str.length();
+        
+        if (len - pos > 4) {
+            std::string temp = str.substr(pos, len - pos);
+            std::reverse(temp.begin(), temp.end());
+            result += temp + " ";
+        } else
+            result += str.substr(pos, len - pos) + " ";
+        
+        pos = len;
+    }
+    
+    if (!str.empty())
+        result += str;
+
+    return result;
+}
