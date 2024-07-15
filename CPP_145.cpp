2ee4581d2765b889249a5e970c342998c37a04c3
beep boop <githubbot@vadim.me>
2024-07-15 18:35:14 +0200

    Solution 32 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ozm81xwh Prompt:    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2d184e1e-9b54-4b32-88b2-b9db6bc8fcc3.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2d184e1e-9b54-4b32-88b2-b9db6bc8fcc3.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2d184e1e-9b54-4b32-88b2-b9db6bc8fcc3.cpp:9: 2d184e1e-9b54-4b32-88b2-b9db6bc8fcc3.cpp: In function ‘int main()’: 2d184e1e-9b54-4b32-88b2-b9db6bc8fcc3.cpp:19:20: error: ‘order_by_points’ was not  declared in this scope    19 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 2d184e1e-9b54-4b32-88b2-b9db6bc8fcc3.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,17 +1,9 @@
-vector<int> order_by_points(vector<int> nums) {
-    sort(nums.begin(), nums.end(), [](int a, int b) {
+bool isSame(vector<int> a, vector<int> b) {
+    sort(a.begin(), a.end(), [](int a, int b) {
         int sumA = abs(a), sumB = abs(b);
         while (sumA >= 10) sumA = sumA / 10 + sumA % 10;
         while (sumB >= 10) sumB = sumB / 10 + sumB % 10;
         return sumA == sumB ? a < b : sumA < sumB;
     });
-    return nums;
-}
-
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6}));
+    return a;
 }
