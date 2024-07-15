1b0e61dc6cc005d563cfd8bacdb4a91737ee302a
beep boop <githubbot@vadim.me>
2024-07-15 22:52:43 +0200

    Solution 30 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/jl6zpipe Prompt: a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:28: a38359ad-e862-46a8-9f86-54f9d5edb461.cpp: In function ‘int main()’: a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:38:20: error: ‘common’ was not declared  in this scope; did you mean ‘CustomVectorFunctions::common’?    38 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:12:22: note: ‘CustomVectorFunctions::co mmon’ declared here    12 |     std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {       |                      ^~~~~~ In file included from a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:28: a38359ad-e862-46a8-9f86-54f9d5edb461.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,26 +1,3 @@
-#include <vector>
-#include <algorithm>
-#include <iterator>
-#include <set>
-#include <cassert>
+// Remove the int main() function from the given code.
 
-namespace CustomVectorFunctions {
-    bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-        return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
-    }
-
-    std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
-        std::vector<int> result;
-        std::sort(l1.begin(), l1.end());
-        std::sort(l2.begin(), l2.end());
-        std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), std::back_inserter(result));
-        result.erase(std::unique(result.begin(), result.end()), result.end());
-        return result;
-    }
-}
-
-int main() {
-    using namespace CustomVectorFunctions;
-    assert(issame(CustomVectorFunctions::common({4, 3, 2, 8}, {}), {}));
-    return 0;
-}
+assert(CustomVectorFunctions::issame(CustomVectorFunctions::common({4, 3, 2, 8}, {}), {}));
