396a703781c8c42282e9444e51f10a43fff5cb61
beep boop <githubbot@vadim.me>
2024-07-15 15:29:52 +0200

    Solution 41 of Python/158, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/kqsanf3w Prompt: Complete the following code given the task description and function signature.

--- Python_158.py
+++ Python_158.py
@@ -1,4 +1,4 @@
 ```
 def find_max(words):
-    max_word = max((word for word in words), key=lambda x: len(set(x)))
-    return max((word for word in words if len(set(word)) == len(set(max_word))), default=max_word)
+    max_word = max((word for word in words), key=lambda w: len(set(w)))
+    return max(((word for word in words if len(set(word)) == len(set(max_word)))) or [max_word], key=ord)
