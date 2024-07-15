a8baacb2ffbd30ba9ea253ec17ce40c968f2fd55
beep boop <githubbot@vadim.me>
2024-07-15 13:51:10 +0200

    Solution 16 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/mvm6ec7t Prompt: 9f7bfa6a-dd96-44b0-93f3-162829784c80.cpp:26:13: error: ‘vector’ was not declared  in this scope    26 | bool issame(vector<float> a,vector<float>b){       |             ^~~~~~ 9f7bfa6a-dd96-44b0-93f3-162829784c80.cpp:26:20: error: expected primary-expressi on before ‘float’    26 | bool issame(vector<float> a,vector<float>b){       |                    ^~~~~ 9f7bfa6a-dd96-44b0-93f3-162829784c80.cpp:26:29: error: ‘vector’ was not declared  in this scope    26 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 9f7bfa6a-dd96-44b0-93f3-162829784c80.cpp:26:36: error: expected primary-expressi on before ‘float’    26 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from 9f7bfa6a-dd96-44b0-93f3-162829784c80.cpp:25: 9f7bfa6a-dd96-44b0-93f3-162829784c80.cpp: In function ‘int main()’: 9f7bfa6a-dd96-44b0-93f3-162829784c80.cpp:35:20: error: ‘sort_even’ was not decla red in this scope    35 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,15 +1,9 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
 bool issame(const vector<float>& a, const vector<float>& b) {
     return a == b;
-}
-
-vector<float> sort_even(const vector<float>& l);
-
-int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
-    return 0;
 }
 
 vector<float> sort_even(const vector<float>& l) {
@@ -18,9 +12,9 @@ vector<float> sort_even(const vector<float>& l) {
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
             even_values.push_back(l[i]);
-            sorted_even_values.push_back(l[i]);
         }
     }
+    sorted_even_values = even_values;
     sort(sorted_even_values.begin(), sorted_even_values.end());
     int sorted_index = 0;
     for (int i = 0; i < l.size(); i++) {
@@ -30,4 +24,9 @@ vector<float> sort_even(const vector<float>& l) {
         }
     }
     return l;
+}
+
+int main() {
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    return 0;
 }
