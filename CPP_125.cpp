6081cc706c698262333408e1fc90f5001333e97e
beep boop <githubbot@vadim.me>
2024-07-15 23:51:17 +0200

    Solution 24 of CPP/125, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/0fb5qu7s Prompt: aaaebfc5-8bd2-429a-9b00-9aa4eda45152.cpp:52:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    52 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ aaaebfc5-8bd2-429a-9b00-9aa4eda45152.cpp:28:6: note: ‘bool issame(std::vector<st d::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> > )’ previously defined here    28 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ aaaebfc5-8bd2-429a-9b00-9aa4eda45152.cpp:60:5: error: redefinition of ‘int main( )’    60 | int main(){       |     ^~~~ aaaebfc5-8bd2-429a-9b00-9aa4eda45152.cpp:42:5: note: ‘int main()’ previously def ined here    42 | int main() {       |     ^~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -24,17 +24,3 @@ vector<string> split_words(string s) {
     
     return words;
 }
-
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
-}
