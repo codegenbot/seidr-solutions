9e30514d221419c106fd5197d025fe0521f7152c
beep boop <githubbot@vadim.me>
2024-07-15 14:24:37 +0200

    Solution 2 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/xfsyi856 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/91544483-f2f9-4f4e-920d-a19edf48a cd8.py", line 14, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/91544483-f2f9-4f4e-920d-a19edf48a cd8.py", line 10, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,2 +1,4 @@
-def fix_spaces(text):
-    return '_'.join([word if len(word) <= 2 else '-'.join(word.split()) for word in text.split()])
+def fix_spaces(text: str) -> str:
+    text = text.replace(" ", "_")
+    text = '-'.join(text.split('-'))
+    return text
