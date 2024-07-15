b7e0cc9506005352075a1aaf801db1bc486b1c11
beep boop <githubbot@vadim.me>
2024-07-15 23:39:21 +0200

    Solution 1 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/brlulnfb Prompt:       |                    ^~~ 7b20206f-b430-4bb0-b77c-766a20aa6c7f.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7b20206f-b430-4bb0-b77c-766a20aa6c7f.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7b20206f-b430-4bb0-b77c-766a20aa6c7f.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7b20206f-b430-4bb0-b77c-766a20aa6c7f.cpp:21: 7b20206f-b430-4bb0-b77c-766a20aa6c7f.cpp: In function ‘int main()’: 7b20206f-b430-4bb0-b77c-766a20aa6c7f.cpp:31:20: error: ‘get_odd_collatz’ was not  declared in this scope    31 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 7b20206f-b430-4bb0-b77c-766a20aa6c7f.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,4 +1,11 @@
-vector<int> result;
+#include <vector>
+
+bool is_same(std::vector<int> a, std::vector<int> b){
+    return a == b;
+}
+
+std::vector<int> get_odd_collatz(int n){
+    std::vector<int> result;
     while (n != 1) {
         result.push_back(n);
         if (n % 2 == 0) {
@@ -8,12 +15,17 @@ vector<int> result;
         }
     }
     result.push_back(1);
-    vector<int> odd_result;
+    std::vector<int> odd_result;
     for (int num : result) {
         if (num % 2 != 0) {
             odd_result.push_back(num);
         }
     }
-    sort(odd_result.begin(), odd_result.end());
+    std::sort(odd_result.begin(), odd_result.end());
     return odd_result;
+}
+
+int main() {
+    assert(is_same(get_odd_collatz(1) , {1}));
+    return 0;
 }
