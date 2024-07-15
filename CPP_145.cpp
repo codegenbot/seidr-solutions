699811aa061dca4875796331e533ed812a3d3dc1
beep boop <githubbot@vadim.me>
2024-07-16 00:03:17 +0200

    Solution 4 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/zw92urwu Prompt:    33 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9c62e4d3-5d67-45d8-bd1e-191b43f27393.cpp:33:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 9c62e4d3-5d67-45d8-bd1e-191b43f27393.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 9c62e4d3-5d67-45d8-bd1e-191b43f27393.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9c62e4d3-5d67-45d8-bd1e-191b43f27393.cpp:33:34: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 9c62e4d3-5d67-45d8-bd1e-191b43f27393.cpp:32: 9c62e4d3-5d67-45d8-bd1e-191b43f27393.cpp: In function ‘int main()’: 9c62e4d3-5d67-45d8-bd1e-191b43f27393.cpp:42:20: error: ‘order_by_points’ was not  declared in this scope    42 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -2,12 +2,21 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b){
+using namespace std;
+
+bool issame(const vector<int>& a, const vector<int>& b){
     return a == b;
 }
 
-std::vector<int> order_by_points(std::vector<int> nums) {
-    std::sort(nums.begin(), nums.end(), [](int a, int b) {
+vector<int> order_by_points(vector<int> nums);
+
+int main() {
+    assert(std::issame(order_by_points({0,6,6,-76,-21,23,4}), {-76, -21, 0, 4, 23, 6, 6}));
+    return 0;
+}
+
+vector<int> order_by_points(vector<int> nums) {
+    sort(nums.begin(), nums.end(), [](int a, int b) {
         int sum_a = 0, sum_b = 0;
         if (a < 0) a *= -1;
         if (b < 0) b *= -1;
@@ -25,8 +34,4 @@ std::vector<int> order_by_points(std::vector<int> nums) {
         return sum_a < sum_b;
     });
     return nums;
-}
-
-int main() {
-    assert (issame(order_by_points({0,6,6,-76,-21,23,4}), {-76, -21, 0, 4, 23, 6, 6}));
 }
