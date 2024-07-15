d309121b977f62ccbc5ea372c005a2a7c43f6315
beep boop <githubbot@vadim.me>
2024-07-15 23:32:31 +0200

    Solution 39 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/23jizrlm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/25c45f98-77b2-4e15-9e31-f7835c432 ab1.py", line 12, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/25c45f98-77b2-4e15-9e31-f7835c432 ab1.py", line 9, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,7 +1,7 @@
 def make_palindrome(string: str) -> str:
     i = len(string)
-    while i >= len(string) / 2:
-        if string[:i] == string[i-1::-1]:
+    while i > 0:
+        if string[:i] == string[i - 1 :: -1]:
             break
         i -= 1
-    return string + string[i-1::-1] if i >= len(string) / 2 else string
+    return string + string[i - 1 :: -1] if i > 0 else string
