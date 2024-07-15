e20eec3f050a6ea1aa2f930f2f76ac775b32e4f2
beep boop <githubbot@vadim.me>
2024-07-15 18:07:54 +0200

    Solution 52 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/fone7umq Prompt:       |             ^~~~~~ 99a53cbc-2d22-42ae-98b1-5d2d96071c0e.cpp:34:34: error: expected primary-expressi on before ‘int’    34 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 99a53cbc-2d22-42ae-98b1-5d2d96071c0e.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 99a53cbc-2d22-42ae-98b1-5d2d96071c0e.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from 99a53cbc-2d22-42ae-98b1-5d2d96071c0e.cpp:33: 99a53cbc-2d22-42ae-98b1-5d2d96071c0e.cpp: In function ‘int main()’: 99a53cbc-2d22-42ae-98b1-5d2d96071c0e.cpp:43:20: error: ‘get_odd_collatz’ was not  declared in this scope    43 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 99a53cbc-2d22-42ae-98b1-5d2d96071c0e.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -2,14 +2,16 @@
 #include <algorithm>
 #include <cassert>
 
-std::vector<int> get_odd_collatz(int n);
+using namespace std;
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(vector<int>& a, vector<int>& b){
     return a == b;
 }
 
-std::vector<int> get_odd_collatz(int n) {
-    std::vector<int> result;
+vector<int> get_odd_collatz(int n);
+
+vector<int> get_odd_collatz(int n) {
+    vector<int> result;
     while (n != 1) {
         result.push_back(n);
         if (n % 2 == 0) {
@@ -19,13 +21,13 @@ std::vector<int> get_odd_collatz(int n) {
         }
     }
     result.push_back(1);
-    std::vector<int> odd_result;
+    vector<int> odd_result;
     for (int num : result) {
         if (num % 2 != 0) {
             odd_result.push_back(num);
         }
     }
-    std::sort(odd_result.begin(), odd_result.end());
+    sort(odd_result.begin(), odd_result.end());
     return odd_result;
 }
 
