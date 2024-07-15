4f74765ede565998859de05652d4c0fc14deadb7
beep boop <githubbot@vadim.me>
2024-07-15 15:49:47 +0200

    Solution 4 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/os7gal40 Prompt:       |                    ^~~ f283e8ec-9387-48ea-9f5a-1c3831d325d8.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f283e8ec-9387-48ea-9f5a-1c3831d325d8.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f283e8ec-9387-48ea-9f5a-1c3831d325d8.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f283e8ec-9387-48ea-9f5a-1c3831d325d8.cpp:9: f283e8ec-9387-48ea-9f5a-1c3831d325d8.cpp: In function ‘int main()’: f283e8ec-9387-48ea-9f5a-1c3831d325d8.cpp:19:20: error: ‘generate_integers’ was n ot declared in this scope    19 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ f283e8ec-9387-48ea-9f5a-1c3831d325d8.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,17 +1,15 @@
 #include <vector>
 
-std::vector<int> generate_integers(int a, int b) {
-    std::vector<int> result;
-    for (int i = a; i <= b; ++i) {
-        if (i % 2 == 0) {
-            result.push_back(i);
-        }
-    }
-    return result;
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
+vector<int> generate_integers(int a, int b) {
+    vector<int> result;
+    for (int i = a; i <= b; ++i) {
+        result.push_back(i);
+    }
+    return result;
 }
 
 int main() {
