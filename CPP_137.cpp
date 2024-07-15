99e669140f6b421bf12b7aca896492dc31931601
beep boop <githubbot@vadim.me>
2024-07-15 18:24:41 +0200

    Solution 26 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/f5o10j3a Prompt:       |   ^~~~ d5bc120b-b55c-4115-b081-5116b5dc84ac.cpp:27:3: error: expected unqualified-id be fore ‘else’    27 | } else if((a.type() == typeid(int) && b.type() == typeid(string)) || (a. type() == typeid(string) && b.type() == typeid(int))) {       |   ^~~~ In file included from d5bc120b-b55c-4115-b081-5116b5dc84ac.cpp:39: d5bc120b-b55c-4115-b081-5116b5dc84ac.cpp: In function ‘int main()’: d5bc120b-b55c-4115-b081-5116b5dc84ac.cpp:41:13: error: ‘boost’ has not been decl ared    41 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ d5bc120b-b55c-4115-b081-5116b5dc84ac.cpp:41:29: error: ‘string’ was not declared  in this scope    41 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ d5bc120b-b55c-4115-b081-5116b5dc84ac.cpp:41:37: error: ‘compare_one’ was not dec lared in this scope    41 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,5 +3,40 @@
 
 boost::any compare_one(const boost::any& a, const boost::any& b);
 
-// Existing code
-// Make sure the code inside main function is correct after these modifications
+if(a.type() == typeid(int) && b.type() == typeid(int)) {
+    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
+        return a;
+    } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
+        return b;
+    } else {
+        return "None";
+    }
+} else if(a.type() == typeid(float) && b.type() == typeid(float)) {
+    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
+        return a;
+    } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
+        return b;
+    } else {
+        return "None";
+    }
+} else if(a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+    float numA = stof(boost::any_cast<std::string>(a).replace(boost::any_cast<std::string>(a).find(','), 1, "."));
+    float numB = stof(boost::any_cast<std::string>(b).replace(boost::any_cast<std::string>(b).find(','), 1, "."));
+    if(numA > numB) {
+        return a;
+    } else if(numA < numB) {
+        return b;
+    } else {
+        return "None";
+    }
+} else if((a.type() == typeid(int) && b.type() == typeid(std::string)) || (a.type() == typeid(std::string) && b.type() == typeid(int))) {
+    float numA = a.type() == typeid(int) ? boost::any_cast<int>(a) : stof(boost::any_cast<std::string>(a).replace(boost::any_cast<std::string>(a).find(','), 1, "."));
+    float numB = b.type() == typeid(int) ? boost::any_cast<int>(b) : stof(boost::any_cast<std::string>(b).replace(boost::any_cast<std::string>(b).find(','), 1, "."));
+    if(numA > numB) {
+        return a;
+    } else if(numA < numB) {
+        return b;
+    } else {
+        return "None";
+    }
+}
