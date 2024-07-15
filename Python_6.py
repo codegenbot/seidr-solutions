4c86ae37c40aa2fbbc3659c43aa73f2c36ce7d2a
beep boop <githubbot@vadim.me>
2024-07-15 23:32:53 +0200

    Solution 9 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/1hk0axry Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1a439981-ea89-428c-a896-d9a089169 cc4.py", line 1, in <module>     def parse_nested_parens(paren_string: str) -> List[int]:                                                   ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_6.py
+++ Python_6.py
@@ -1,19 +1,14 @@
 from typing import List
 
+
 def parse_nested_parens(paren_string: str) -> List[int]:
     return [
-        (
-            max(
-                [
-                    paren_string.count("(" * i)
-                    for i in range(1, len(paren_string) // 2 + 1)
-                ]
-            )
-            // 2
+        max(
+            [
+                paren_string.count("(" * i)
+                for i in range(1, (len(paren_string) // 2) + 1)
+            ]
         )
+        // 2
         for paren_string in paren_string.split(" ")
     ]
-
-input_string = input("Enter the nested parentheses string: ")
-result = parse_nested_parens(input_string)
-print(result)
