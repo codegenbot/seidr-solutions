72b24c3dc2fa47da23d705b375cd5d37403913e3
beep boop <githubbot@vadim.me>
2024-07-15 14:59:23 +0200

    Solution 30 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/06y12ino Prompt:       |                           ^~~~~~ 64fb32ca-8561-496e-ad37-990b6e4de10f.cpp:31:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 64fb32ca-8561-496e-ad37-990b6e4de10f.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 64fb32ca-8561-496e-ad37-990b6e4de10f.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 64fb32ca-8561-496e-ad37-990b6e4de10f.cpp:31:34: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 64fb32ca-8561-496e-ad37-990b6e4de10f.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 64fb32ca-8561-496e-ad37-990b6e4de10f.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,18 +1,19 @@
+
 #include <vector>
 #include <cassert>
 
 std::vector<int> count_up_to(int n) {
     std::vector<int> primes;
-    for (int i = 2; i <= n; ++i) {
+    for (int num = 2; num <= n; ++num) {
         bool is_prime = true;
-        for (int j = 2; j*j <= i; ++j) {
-            if (i % j == 0) {
+        for (int i = 2; i * i <= num; ++i) {
+            if (num % i == 0) {
                 is_prime = false;
                 break;
             }
         }
         if (is_prime) {
-            primes.push_back(i);
+            primes.push_back(num);
         }
     }
     return primes;
@@ -23,6 +24,6 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 }
 
 int main() {
-    assert (issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
+    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
     return 0;
 }
