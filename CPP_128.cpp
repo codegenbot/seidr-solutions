2be0c4b0f4dbb4b265a18ca5a043c4a1cae4b7ab
beep boop <githubbot@vadim.me>
2024-07-15 15:19:47 +0200

    Solution 13 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/bhf0cj39 Prompt: 3a317780-c5f3-42d6-a6d3-208926bf60e0.cpp:1:1: error: ‘product’ does not name a t ype     1 | product = (num > 0) ? product : (num < 0) ? -product : 0;       | ^~~~~~~ In file included from 3a317780-c5f3-42d6-a6d3-208926bf60e0.cpp:3: 3a317780-c5f3-42d6-a6d3-208926bf60e0.cpp: In function ‘int main()’: 3a317780-c5f3-42d6-a6d3-208926bf60e0.cpp:5:13: error: ‘prod_signs’ was not decla red in this scope     5 |     assert (prod_signs({-1, 1, 1, 0}) == 0);       |             ^~~~~~~~~~

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -5,16 +5,16 @@
 int product;
 int prod_signs(const std::vector<int>& nums);
 
+int main() {
+    assert (prod_signs({-1, 1, 1, 0}) == 0);
+    
+    return 0;
+}
+
 int prod_signs(const std::vector<int>& nums) {
     product = 1;
     for (int num : nums) {
         product = (num > 0) ? product : (num < 0) ? -product : 0;
     }
     return product;
-}
-
-int main() {
-    assert (prod_signs({-1, 1, 1, 0}) == 0);
-    
-    return 0;
 }
