60314ccd1f6af1a09d9a98036dd2390160f9fb32
beep boop <githubbot@vadim.me>
2024-07-15 23:06:15 +0200

    Solution 29 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/9jbrva5m Prompt:       |                                            ^ b8a9d5c8-6345-4163-974f-ffcac1bd2ae5.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ b8a9d5c8-6345-4163-974f-ffcac1bd2ae5.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ b8a9d5c8-6345-4163-974f-ffcac1bd2ae5.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from b8a9d5c8-6345-4163-974f-ffcac1bd2ae5.cpp:29: b8a9d5c8-6345-4163-974f-ffcac1bd2ae5.cpp: In function ‘int main()’: b8a9d5c8-6345-4163-974f-ffcac1bd2ae5.cpp:39:20: error: ‘total_match’ was not dec lared in this scope    39 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ b8a9d5c8-6345-4163-974f-ffcac1bd2ae5.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -2,10 +2,9 @@
 #include <vector>
 #include <string>
 #include <cassert>
-#include <algorithm>
 
 bool areEqual(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
+    return a == b;
 }
 
 int main() {
