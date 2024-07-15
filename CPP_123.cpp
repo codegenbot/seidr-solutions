51109d11e6165f3b0c60a095441c93f9b90eaa81
beep boop <githubbot@vadim.me>
2024-07-15 23:40:17 +0200

    Solution 4 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/brlulnfb Prompt:       |             ^~~~~~ b67c93fa-4189-46fb-8d20-d59e47afc09f.cpp:34:34: error: expected primary-expressi on before ‘int’    34 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b67c93fa-4189-46fb-8d20-d59e47afc09f.cpp:34:39: error: expression list treated a s compound expression in initializer [-fpermissive]    34 | bool issame(vector<int> a,vector<int>b){       |                                       ^ b67c93fa-4189-46fb-8d20-d59e47afc09f.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ b67c93fa-4189-46fb-8d20-d59e47afc09f.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from b67c93fa-4189-46fb-8d20-d59e47afc09f.cpp:33: b67c93fa-4189-46fb-8d20-d59e47afc09f.cpp: In function ‘int main()’: b67c93fa-4189-46fb-8d20-d59e47afc09f.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
 bool is_same(std::vector<int> a, std::vector<int> b){
     return a == b;
@@ -27,7 +26,7 @@ std::vector<int> get_odd_collatz(int n){
     return odd_result;
 }
 
-int main() {
-    assert(is_same(get_odd_collatz(1), std::vector<int>{1}));
+int main(){
+    assert(is_same(get_odd_collatz(1), {1}));
     return 0;
 }
