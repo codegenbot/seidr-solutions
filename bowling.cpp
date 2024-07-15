0eafe52b85373a05a9d66076d58d6756c37162d9
beep boop <githubbot@vadim.me>
2024-07-15 20:05:52 +0200

    Solution 61 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/qgoa7j8c Prompt: in this scope    49 |     string s;       |     ^~~~~~ a1feec69-3495-4e2d-8f1e-6c6ed1f14b43.cpp:50:5: error: ‘cin’ was not declared in this scope    50 |     cin >> s;       |     ^~~ a1feec69-3495-4e2d-8f1e-6c6ed1f14b43.cpp:50:12: error: ‘s’ was not declared in t his scope    50 |     cin >> s;       |            ^ a1feec69-3495-4e2d-8f1e-6c6ed1f14b43.cpp:51:5: error: ‘cout’ was not declared in  this scope    51 |     cout << bowlingScore(s) << endl;       |     ^~~~ a1feec69-3495-4e2d-8f1e-6c6ed1f14b43.cpp:51:25: error: ‘bowlingScore’ cannot be used as a function    51 |     cout << bowlingScore(s) << endl;       |             ~~~~~~~~~~~~^~~ a1feec69-3495-4e2d-8f1e-6c6ed1f14b43.cpp:51:32: error: ‘endl’ was not declared i n this scope    51 |     cout << bowlingScore(s) << endl;       |                                ^~~~

--- bowling.cpp
+++ bowling.cpp
@@ -2,7 +2,49 @@
 
 int bowlingScore(std::string s) {
     int score = 0;
-    // Rest of the existing bowlingScore function remains the same
+    int frame = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == 'X') {
+            score += 10;
+            if (s[i + 2] == '/') {
+                score += 10;
+            } else {
+                if (s[i + 1] == 'X') {
+                    score += 10;
+                } else {
+                    score += s[i + 1] - '0';
+                }
+                if (s[i + 2] == 'X') {
+                    score += 10;
+                } else if (s[i + 2] == '/') {
+                    score += 10 - (s[i + 1] - '0');
+                } else {
+                    score += s[i + 2] - '0';
+                }
+            }
+            frame++;
+        } else if (s[i] == '/') {
+            score += 10 - (s[i - 1] - '0');
+            if (s[i + 1] == 'X') {
+                score += 10;
+            } else {
+                score += s[i + 1] - '0';
+            }
+            frame++;
+        } else if (s[i] == '-') {
+            continue;
+        } else {
+            score += s[i] - '0';
+            if (s[i + 1] == '/') {
+                score += 10 - (s[i] - '0');
+            }
+            frame++;
+        }
+        if (frame == 10) {
+            break;
+        }
+    }
+    return score;
 }
 
 int main() {
