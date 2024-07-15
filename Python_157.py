b2e92eff0a56a8a2069a740a7124175388522b7f
beep boop <githubbot@vadim.me>
2024-07-15 15:01:25 +0200

    Solution 12 of Python/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/bcmpmlei Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2d8036ea-c09d-4139-b7d2-8363d46cc bdc.py", line 4, in <module>     result = check_pythagorean_triplet(a_value, b_value, c_value)                                        ^^^^^^^ NameError: name 'a_value' is not defined

--- Python_157.py
+++ Python_157.py
@@ -1,9 +1,9 @@
-a_value = int(input("Enter the value for a: "))
-b_value = int(input("Enter the value for b: "))
-c_value = int(input("Enter the value for c: "))
-
 def check_pythagorean_triplet(a, b, c):
     return a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2
 
+
+a_value = int(input("Enter a value for a: "))
+b_value = int(input("Enter a value for b: "))
+c_value = int(input("Enter a value for c: "))
+
 result = check_pythagorean_triplet(a_value, b_value, c_value)
-print(result)
