9e6b7c1ab4f0a5fcab6457b895af1a2ffae9f14d
beep boop <githubbot@vadim.me>
2024-07-15 12:39:42 +0200

    Solution 9 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/dgatj4fp Prompt: 82020908-8a35-42ec-ad3a-cf2aab79f11c.cpp: In function ‘std::string camelCase(std ::string)’: 82020908-8a35-42ec-ad3a-cf2aab79f11c.cpp:22:17: error: ‘j’ was not declared in t his scope    22 |             i = j - 1;       |                 ^

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,35 +1,20 @@
-```
 string camelCase(string s) {
     string result = "";
+    int j = 0;
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == '-') {
-            i++; // skip the '-'
-            while(i < s.size() && s[i] != '-' && s[i] != ' ') {
-                if (i == 0) {
-                    result += toupper(s[i]);
-                } else {
-                    result += tolower(s[i]);
-                }
-                i++;
+            result += toupper(s[++i]);
+            while (++i <= s.size() && s[i] != '-' && s[i] != ' ') {
+                result += tolower(s[i]);
             }
         } else if (s[i] == ' ') {
-            for (int j = i + 1; j < s.size(); ) {
-                if (s[j] == ' ') {
-                    break;
-                }
-                if (j == i + 1) {
-                    result += toupper(s[j]);
-                } else {
-                    result += tolower(s[j]);
-                }
-                j++;
-            }
-            i = j - 1;
+            for (; i < s.size() && s[i] == ' '; i++) {}
+            j = i;
         } else {
-            if (i > 0) {
-                result += tolower(s[i]);
-            } else {
+            if (!result.size()) {
                 result += toupper(s[i]);
+            } else {
+                result += tolower(s[i]);
             }
         }
     }
