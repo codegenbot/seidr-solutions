65abe5f4f86927218969c64327bd5b6abaf2bd4e
beep boop <githubbot@vadim.me>
2024-07-15 22:08:29 +0200

    Solution 1 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/n7m88gtx Prompt:       |                              ^~~~~~ a9dc19ea-9e3e-4c5e-8315-e23d4521a829.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a9dc19ea-9e3e-4c5e-8315-e23d4521a829.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a9dc19ea-9e3e-4c5e-8315-e23d4521a829.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a9dc19ea-9e3e-4c5e-8315-e23d4521a829.cpp:9: a9dc19ea-9e3e-4c5e-8315-e23d4521a829.cpp: In function ‘int main()’: a9dc19ea-9e3e-4c5e-8315-e23d4521a829.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ a9dc19ea-9e3e-4c5e-8315-e23d4521a829.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,7 +1,36 @@
-vector<string> all_prefixes(string str){
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+vector<string> all_prefixes(string str);
+
+bool issame(vector<string> a, vector<string> b);
+
+int main() {
+    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    cout << "Test passed" << endl;
+    return 0;
+}
+
+vector<string> all_prefixes(string str) {
     vector<string> result;
     for (int i = 1; i <= str.size(); ++i) {
         result.push_back(str.substr(0, i));
     }
     return result;
+}
+
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
