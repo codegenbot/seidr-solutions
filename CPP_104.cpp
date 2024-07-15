234ac474b877950948cbcd0b42611ac8846adfc1
beep boop <githubbot@vadim.me>
2024-07-15 15:23:44 +0200

    Solution 86 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/y01tjfvb Prompt: In file included from /usr/include/c++/11/vector:67,                  from 21db70be-a079-4a69-8843-4a32556a635e.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 21db70be-a079-4a69-8843-4a32556a635e.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 21db70be-a079-4a69-8843-4a32556a635e.cpp:33:34: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 21db70be-a079-4a69-8843-4a32556a635e.cpp:33:39: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 21db70be-a079-4a69-8843-4a32556a635e.cpp:32: 21db70be-a079-4a69-8843-4a32556a635e.cpp: In function ‘int main()’: 21db70be-a079-4a69-8843-4a32556a635e.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame_digits(std::vector<int> a, std::vector<int> b){
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
@@ -27,14 +27,4 @@ std::vector<int> unique_digits(std::vector<int> x){
     return result;
 }
 
-int main(){
-    std::vector<int> input = {123, 456, 789};
-    std::vector<int> output = unique_digits(input);
-    for(int num : output){
-        std::cout << num << " ";
-    }
-    
-    assert(issame_digits(unique_digits({135, 103, 31}), {31, 135}));
-
-    return 0;
-}
+assert(issame( unique_digits({135,103,31}), {31, 135}) );
