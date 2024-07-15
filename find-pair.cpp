78de9630f885a381f0b7ecb1425813f8e27bf1c3
beep boop <githubbot@vadim.me>
2024-07-15 12:30:11 +0200

    Solution 4 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/gtivpx5f Prompt: eefc23dc-c175-4c8e-9148-ca0c5de4e138.cpp:26:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? eefc23dc-c175-4c8e-9148-ca0c5de4e138.cpp:26:33: error: ‘endl’ was not declared i n this scope    26 |         cout << result.first << endl;       |                                 ^~~~ eefc23dc-c175-4c8e-9148-ca0c5de4e138.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std; eefc23dc-c175-4c8e-9148-ca0c5de4e138.cpp:29:9: error: ‘cout’ was not declared in  this scope    29 |         cout << "No such pair found" << endl;       |         ^~~~ eefc23dc-c175-4c8e-9148-ca0c5de4e138.cpp:29:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? eefc23dc-c175-4c8e-9148-ca0c5de4e138.cpp:29:41: error: ‘endl’ was not declared i n this scope    29 |         cout << "No such pair found" << endl;       |                                         ^~~~ eefc23dc-c175-4c8e-9148-ca0c5de4e138.cpp:29:41: note: ‘std::endl’ is defined in header ‘<ostream>’; did you forget to ‘#include <ostream>’?

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,28 +1,33 @@
+#include <iostream>
 #include <vector>
 using namespace std;
 
-pair<int, int> findPair(vector<int>& nums, int target) {
-    unordered_map<int, int> numIndex;
-    for (int i = 0; i < nums.size(); i++) {
-        int complement = target - nums[i];
-        if (numIndex.count(complement)) {
-            return {complement, nums[i]};
+pair<int, int> findPair(vector<int>& numbers, int target) {
+    unordered_map<int, int> numToIndex;
+    for (int i = 0; i < numbers.size(); i++) {
+        int complement = target - numbers[i];
+        if (numToIndex.count(complement)) {
+            return {complement, numbers[i]};
         }
-        numIndex[nums[i]] = i;
+        numToIndex[numbers[i]] = i;
     }
-    return {-1, -1}; // or throw an exception
+    return {};
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
+    vector<int> numbers(n);
+    for (int& x : numbers) cin >> x;
     int target;
     cin >> target;
-    pair<int, int> result = findPair(nums, target);
-    cout << result.first << "\n" << result.second << "\n";
+
+    pair<int, int> result = findPair(numbers, target);
+    if (!result.empty()) {
+        cout << result.first << endl;
+        cout << result.second << endl;
+    } else {
+        cout << "No such pair found" << endl;
+    }
+    
     return 0;
-}
