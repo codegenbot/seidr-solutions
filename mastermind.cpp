539d435afe3afa0043b766d2c8d6a2666941c174
beep boop <githubbot@vadim.me>
2024-07-16 00:00:27 +0200

    Solution 58 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/ak149vls Prompt: 88d3b064-0b51-422c-9722-0efd5af58c26.cpp:1:20: error: ‘string’ is not a member o f ‘std’     1 | int blackPegs(std::string code, std::string guess) {       |                    ^~~~~~ 88d3b064-0b51-422c-9722-0efd5af58c26.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int blackPegs(std::string code, std::string guess) { 88d3b064-0b51-422c-9722-0efd5af58c26.cpp:1:38: error: ‘string’ is not a member o f ‘std’     1 | int blackPegs(std::string code, std::string guess) {       |                                      ^~~~~~ 88d3b064-0b51-422c-9722-0efd5af58c26.cpp:1:38: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? 88d3b064-0b51-422c-9722-0efd5af58c26.cpp:1:50: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int blackPegs(std::string code, std::string guess) {       |                                                  ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,41 +1,24 @@
-#include <iostream>
 #include <string>
 #include <unordered_map>
 
-int whitePegs(std::string code, std::string guess) {
-    int count = 0;
-    for(int i=0; i<4; i++) {
-        if(code[i] == guess[i]) {
-            count++;
-        }
-    }
-    return count;
-}
-
 int blackPegs(std::string code, std::string guess) {
     int blackCount = 0;
-    int whiteCount = whitePegs(code, guess);
+    int whiteCount = 0;
     
     std::unordered_map<char,int> codeMap;
     for(int i=0; i<4; i++) {
-        codeMap[code[i]]++;
-    }
-    
-    blackCount = 4 - whiteCount;
-    for(int i=0; i<4; i++) {
-        if(codeMap.find(guess[i]) != codeMap.end()) {
+        if(code[i] == guess[i]) {
             blackCount++;
-            codeMap[guess[i]]--;
+        } else {
+            if(codeMap.find(guess[i]) != codeMap.end()) {
+                codeMap[guess[i]]++;
+            }
         }
     }
-    return blackCount;
-}
-
-int main() {
-    std::string code, guess;
-    std::cin >> code >> guess;
-    int white = whitePegs(code, guess);
-    int black = blackPegs(code, guess);
-    std::cout << white << " " << black << std::endl;
-    return 0;
+    
+    for(auto it = codeMap.begin(); it != codeMap.end(); it++) {
+        whiteCount += std::min(it->second, 1);
+    }
+    
+    return blackCount + whiteCount;
 }
