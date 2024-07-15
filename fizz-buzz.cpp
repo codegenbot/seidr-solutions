8fe3ebf42d5ab642719315f41cbd0c236d4178f0
beep boop <githubbot@vadim.me>
2024-07-15 13:57:15 +0200

    Solution 17 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/a08td4t2 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from cb29e0eb-89c3-4f96-9d16-61f9282d518f.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,26 +1,19 @@
 #include <iostream>
-#include <string>
 
-class Solution {
-public:
-    string fizzBuzz(int n) {
-        for (int i = 1; i <= n; ++i) {
-            if (i % 3 == 0 && i % 5 == 0)
-                std::cout << "FizzBuzz" << std::endl;
-            else if (i % 3 == 0)
-                std::cout << "Fizz" << std::endl;
-            else if (i % 5 == 0)
-                std::cout << "Buzz" << std::endl;
-            else
-                std::cout << i << std::endl;
-        }
-    }
-};
+using namespace std;
 
 int main() {
-    Solution s;
     int n;
-    std::cin >> n;
-    s.fizzBuzz(n);
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        if (i % 3 == 0 && i % 5 == 0)
+            cout << "FizzBuzz" << endl;
+        else if (i % 3 == 0)
+            cout << "Fizz" << endl;
+        else if (i % 5 == 0)
+            cout << "Buzz" << endl;
+        else
+            cout << to_string(i) << endl;
+    }
     return 0;
 }
