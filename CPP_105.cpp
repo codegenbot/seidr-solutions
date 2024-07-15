046955b15790113c1506850079324a7b82e23267
beep boop <githubbot@vadim.me>
2024-07-15 20:59:48 +0200

    Solution 81 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/wktqj69z Prompt:       |                              ^~~~~~ f7ed17a2-4dd6-4268-9f20-0cb5341e3402.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f7ed17a2-4dd6-4268-9f20-0cb5341e3402.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f7ed17a2-4dd6-4268-9f20-0cb5341e3402.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f7ed17a2-4dd6-4268-9f20-0cb5341e3402.cpp:25: f7ed17a2-4dd6-4268-9f20-0cb5341e3402.cpp: In function ‘int main()’: f7ed17a2-4dd6-4268-9f20-0cb5341e3402.cpp:35:20: error: ‘by_length’ was not decla red in this scope    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ f7ed17a2-4dd6-4268-9f20-0cb5341e3402.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -2,7 +2,15 @@
 #include <cassert>
 
 bool is_same(vector<string> a, vector<string> b){
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 vector<string> sort_numbers_by_length(vector<int> arr){
