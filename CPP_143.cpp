5e83a0bcd1e9f157774a9dd95c6112201650c9cb
beep boop <githubbot@vadim.me>
2024-07-15 15:33:10 +0200

    Solution 2 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/1c6zb7vs Prompt: a47d59d8-4fe6-4b6b-8e01-111e2621cfbc.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string words_in_sentence(string sentence){       | ^~~~~~ In file included from a47d59d8-4fe6-4b6b-8e01-111e2621cfbc.cpp:36: a47d59d8-4fe6-4b6b-8e01-111e2621cfbc.cpp: In function ‘int main()’: a47d59d8-4fe6-4b6b-8e01-111e2621cfbc.cpp:38:13: error: ‘words_in_sentence’ was n ot declared in this scope    38 |     assert (words_in_sentence("here is") == "is");       |             ^~~~~~~~~~~~~~~~~

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,19 +1,23 @@
+#include <string>
+
+using namespace std;
+
 string words_in_sentence(string sentence){
     string result = "";
     string word = "";
-    for (char c : sentence) {
-        if (c != ' ') {
-            word += c;
+    for(int i=0; i<sentence.length(); i++){
+        if(sentence[i] != ' '){
+            word += sentence[i];
         } else {
             int len = word.length();
             bool isPrime = true;
-            for (int i = 2; i * i <= len; i++) {
-                if (len % i == 0) {
+            for(int j=2; j*j<=len; j++){
+                if(len % j == 0){
                     isPrime = false;
                     break;
                 }
             }
-            if (isPrime && len > 1) {
+            if(isPrime && len > 1){
                 result += word + " ";
             }
             word = "";
@@ -21,14 +25,19 @@ string words_in_sentence(string sentence){
     }
     int len = word.length();
     bool isPrime = true;
-    for (int i = 2; i * i <= len; i++) {
-        if (len % i == 0) {
+    for(int j=2; j*j<=len; j++){
+        if(len % j == 0){
             isPrime = false;
             break;
         }
     }
-    if (isPrime && len > 1) {
+    if(isPrime && len > 1){
         result += word;
     }
     return result;
+}
+
+int main(){
+    assert (words_in_sentence("here is") == "is");
+    return 0;
 }
