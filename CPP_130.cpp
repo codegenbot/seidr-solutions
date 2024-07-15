0129209d3a42de903add525c2bf61a59835cf39f
beep boop <githubbot@vadim.me>
2024-07-15 15:19:51 +0200

    Solution 10 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/c9n11wpk Prompt: In file included from /usr/include/c++/11/vector:67,                  from 88df200c-d03c-4e82-8043-c7dc521ed77c.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 88df200c-d03c-4e82-8043-c7dc521ed77c.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 88df200c-d03c-4e82-8043-c7dc521ed77c.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 88df200c-d03c-4e82-8043-c7dc521ed77c.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 88df200c-d03c-4e82-8043-c7dc521ed77c.cpp:24: 88df200c-d03c-4e82-8043-c7dc521ed77c.cpp: In function ‘int main()’: 88df200c-d03c-4e82-8043-c7dc521ed77c.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,31 +1,10 @@
 #include <vector>
 #include <cassert>
 
-using namespace std;
-
-bool issame(vector<int> a, vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
-vector<int> tri(int n) {
-    vector<int> result;
-    if (n >= 0) {
-        result.push_back(3);
-        if (n > 0) {
-            result.push_back(1);
-            for (int i = 2; i <= n; ++i) {
-                if (i % 2 == 0) {
-                    result.push_back(1 + i / 2);
-                } else {
-                    result.push_back(result[i - 1] + result[i - 2] + result[i + 1]);
-                }
-            }
-        }
-    }
-    return result;
-}
-
-int main() {
-    assert(issame(tri(1), {1, 3}));
-    return 0;
+int main(){
+    assert(issame(tri(1), std::vector<int>{1, 3}));
 }
