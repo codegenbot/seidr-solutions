382d1d3d6878e8e5237851967d8f69c80f02dff3
beep boop <githubbot@vadim.me>
2024-07-15 15:41:29 +0200

    Solution 11 of CPP/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/cqqatjyo Prompt: o ‘char’  1178 |       operator+=(_CharT __c)       |                  ~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h:1191:7: note: candidate: ‘std::__cxx11:: basic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Trait s, _Alloc>::operator+=(std::initializer_list<_Tp>) [with _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’  1191 |       operator+=(initializer_list<_CharT> __l)       |       ^~~~~~~~ /usr/include/c++/11/bits/basic_string.h:1191:43: note:   no known conversion for  argument 1 from ‘const string’ {aka ‘const std::__cxx11::basic_string<char>’} t o ‘std::initializer_list<char>’  1191 |       operator+=(initializer_list<_CharT> __l)       |                  ~~~~~~~~~~~~~~~~~~~~~~~~~^~~ 833c1158-4ff7-4715-85ff-bc57f9e9c12e.cpp: At global scope: 833c1158-4ff7-4715-85ff-bc57f9e9c12e.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ 833c1158-4ff7-4715-85ff-bc57f9e9c12e.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_154.cpp
+++ CPP_154.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <cassert>
@@ -6,13 +7,17 @@ bool cycpattern_check(const std::string& a, const std::string& b) {
     int n = a.size();
     int m = b.size();
     if(n != m) return false;
-    std::string new_a = a;
-    new_a.append(a);
-    return new_a.find(b) != std::string::npos;
+    std::string str = a;
+    str.append(a);
+    return str.find(b) != std::string::npos;
 }
 
-assert(cycpattern_check("winemtt", "tinem") == false);
-assert(cycpattern_check("abcdef", "defabc") == true);
-assert(cycpattern_check("hellohello", "hello") == true);
-assert(cycpattern_check("software", "soft") == false);
-assert(cycpattern_check("programming", "ingpro") == false);
+int main() {
+    assert(cycpattern_check("winemtt", "tinemtt") == true);
+    assert(cycpattern_check("abcdef", "defabc") == true);
+    assert(cycpattern_check("hellohello", "ohell") == true);
+    assert(cycpattern_check("software", "soft") == false);
+    assert(cycpattern_check("programming", "ingpro") == false);
+    
+    return 0;
+}
