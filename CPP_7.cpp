d9ca85ce9778e438b83e413c2ce05a50fa184cd5
beep boop <githubbot@vadim.me>
2024-07-15 16:03:37 +0200

    Solution 38 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/3dpudf6u Prompt:    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3c555377-de86-49ab-a30c-9ee0f1977cba.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 3c555377-de86-49ab-a30c-9ee0f1977cba.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main(){       |     ^~~~ In file included from 3c555377-de86-49ab-a30c-9ee0f1977cba.cpp:18: 3c555377-de86-49ab-a30c-9ee0f1977cba.cpp: In function ‘int main()’: 3c555377-de86-49ab-a30c-9ee0f1977cba.cpp:28:20: error: ‘filter_by_substring’ was  not declared in this scope    28 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 3c555377-de86-49ab-a30c-9ee0f1977cba.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,16 +1,3 @@
-vector<string> filter_by_substring(vector<string> strings, string substring);
-vector<string> filter_by_substring(vector<string> strings, string substring){
-    vector<string> result;
-    for(const auto& str : strings){
-        if(str.find(substring) != string::npos){
-            result.push_back(str);
-        }
-    }
-    return result;
-}
 bool issame(vector<string> a, vector<string> b){
     return a == b;
-}
-int main(){
-    assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
 }
