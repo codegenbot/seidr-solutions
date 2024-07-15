011d36d1f50977ed17b3e23b7b5ab514afdec906
beep boop <githubbot@vadim.me>
2024-07-15 23:29:10 +0200

    Solution 1 of CPP/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/deshtqhw Prompt:     4 |         for (int i = 0; i < rows; ++i) {       |         ^~~ 238e1f20-dd32-4b8f-ae89-b72dec483d80.cpp:4:25: error: ‘i’ does not name a type     4 |         for (int i = 0; i < rows; ++i) {       |                         ^ 238e1f20-dd32-4b8f-ae89-b72dec483d80.cpp:4:35: error: expected unqualified-id be fore ‘++’ token     4 |         for (int i = 0; i < rows; ++i) {       |                                   ^~ 238e1f20-dd32-4b8f-ae89-b72dec483d80.cpp:9:9: error: expected unqualified-id bef ore ‘return’     9 |         return (total_water + capacity - 1) / capacity;       |         ^~~~~~ 238e1f20-dd32-4b8f-ae89-b72dec483d80.cpp:10:5: error: expected declaration befor e ‘}’ token    10 |     }       |     ^ In file included from 238e1f20-dd32-4b8f-ae89-b72dec483d80.cpp:12: 238e1f20-dd32-4b8f-ae89-b72dec483d80.cpp: In function ‘int main()’: 238e1f20-dd32-4b8f-ae89-b72dec483d80.cpp:14:13: error: ‘max_fill’ was not declar ed in this scope    14 |     assert (max_fill({{1,1,1,1}, {1,1,1,1}}, 9) == 2);       |             ^~~~~~~~

--- CPP_115.cpp
+++ CPP_115.cpp
@@ -1,10 +1,11 @@
-int rows = grid.size();
-        int cols = grid[0].size();
-        int total_water = 0;
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                total_water += grid[i][j];
-            }
+int max_fill(vector<vector<int>>& grid, int capacity) {
+    int rows = grid.size();
+    int cols = grid[0].size();
+    int total_water = 0;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            total_water += grid[i][j];
         }
-        return (total_water + capacity - 1) / capacity;
     }
+    return (total_water + capacity - 1) / capacity;
+}
