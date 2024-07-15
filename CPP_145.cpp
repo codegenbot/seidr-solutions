2286bbbcea1d9a3b2632ac1056aa381218437cb8
beep boop <githubbot@vadim.me>
2024-07-15 15:45:37 +0200

    Solution 34 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/hnekootv Prompt: 394dfa11-3cee-47aa-9c30-fb3d1e737711.cpp:39:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    39 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 394dfa11-3cee-47aa-9c30-fb3d1e737711.cpp:7:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     7 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 394dfa11-3cee-47aa-9c30-fb3d1e737711.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 394dfa11-3cee-47aa-9c30-fb3d1e737711.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main(){       |     ^~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -4,10 +4,6 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 vector<int> order_by_points(vector<int> nums) {
     sort(nums.begin(), nums.end(), [](int a, int b) {
         int sum_a = 0, sum_b = 0;
@@ -30,8 +26,7 @@ vector<int> order_by_points(vector<int> nums) {
     return nums;
 }
 
-int main() {
-    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6});
-    
+int main(){
+    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6}));
     return 0;
 }
