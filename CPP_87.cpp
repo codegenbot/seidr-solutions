19b71a5913a050c72cacb4ef15dfe7dbadbc44ec
beep boop <githubbot@vadim.me>
2024-07-15 23:17:14 +0200

    Solution 42 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/88i3msvv Prompt:    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 2e8c84d0-e98e-4c45-a1e7-59f31441a0c0.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 2e8c84d0-e98e-4c45-a1e7-59f31441a0c0.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~ In file included from 2e8c84d0-e98e-4c45-a1e7-59f31441a0c0.cpp:17: 2e8c84d0-e98e-4c45-a1e7-59f31441a0c0.cpp: In function ‘int main()’: 2e8c84d0-e98e-4c45-a1e7-59f31441a0c0.cpp:30:27: error: invalid initialization of  reference of type ‘const std::vector<std::vector<int> >&’ from expression of ty pe ‘const std::vector<int>’    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~ 2e8c84d0-e98e-4c45-a1e7-59f31441a0c0.cpp:4:50: note: in passing argument 1 of ‘b ool issame(const std::vector<std::vector<int> >&, const std::vector<std::vector< int> >&)’     4 | bool issame(const std::vector<std::vector<int>>& a, const std::vector<st d::vector<int>>& b) {       |             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -6,7 +6,7 @@ bool issame(const std::vector<std::vector<int>>& a, const std::vector<std::vecto
 }
 
 const std::vector<int>& get_row(const std::vector<std::vector<int>>& lst, int x) {
-    return lst.at(x);
+    return lst.at(x - 1);
 }
 
 int main() {
