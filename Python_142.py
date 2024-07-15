958999937a64bb4b5485f5d84f3be7d6ab0974ba
beep boop <githubbot@vadim.me>
2024-07-15 12:23:35 +0200

    Solution 64 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0h1ogjge Prompt: 14 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/714d80c2-fd76-4933-946e-827dd2b85 8e0.py", line 48, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/714d80c2-fd76-4933-946e-827dd2b85 8e0.py", line 18, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,11 +1,12 @@
+```
 def sum_squares(lst):
     total = 0
     for num in lst:
         if isinstance(num, int):
             total += num**2
         else:
-            print("Error: The list contains a non-integer value.")
-    
+            return None
+
     return total
 
 
