cf06b65992e5c8896c2207da4bd9e1a0bda4d48c
beep boop <githubbot@vadim.me>
2024-07-15 13:57:35 +0200

    Solution 64 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/dob9ydvf Prompt:       |                           ^~~~~~ 91b3ba3e-311d-4f64-9803-1c0a7b6268fa.cpp:22:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 91b3ba3e-311d-4f64-9803-1c0a7b6268fa.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 91b3ba3e-311d-4f64-9803-1c0a7b6268fa.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 91b3ba3e-311d-4f64-9803-1c0a7b6268fa.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 91b3ba3e-311d-4f64-9803-1c0a7b6268fa.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 91b3ba3e-311d-4f64-9803-1c0a7b6268fa.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -13,7 +13,5 @@ std::vector<int> unique(const std::vector<int>& l) {
     return temp;
 }
 
-int main() {
-    assert(issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}), {0, 2, 3, 5, 9, 123}));
-    return 0;
-}
+assert(issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}), {0, 2, 3, 5, 9, 123}));
+return 0;
