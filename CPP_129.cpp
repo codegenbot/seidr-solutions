3797ca2a82794a48908a55abffba0809894464bb
beep boop <githubbot@vadim.me>
2024-07-15 18:22:46 +0200

    Solution 75 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/gsz1tm3r Prompt:       |             ^~~~~~ 164c15e8-e473-40e6-b71b-0c2aff033e5f.cpp:17:20: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 164c15e8-e473-40e6-b71b-0c2aff033e5f.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 164c15e8-e473-40e6-b71b-0c2aff033e5f.cpp:17:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 164c15e8-e473-40e6-b71b-0c2aff033e5f.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 164c15e8-e473-40e6-b71b-0c2aff033e5f.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 164c15e8-e473-40e6-b71b-0c2aff033e5f.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -11,4 +11,4 @@ std::vector<int> minPath(std::vector<std::vector<int>> grid, int k){
 }
 
 // Example usage:
-// assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
+// assert(issame(minPath(std::vector<std::vector<int>> {{1, 3}, {3, 2}}, 10), std::vector<int>{1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
