bb95a82b2735d2be9ba7a8c23d0ae7581513a3b8
beep boop <githubbot@vadim.me>
2024-07-15 12:28:46 +0200

    Solution 2 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/8972drb4 Prompt: ared in this scope    27 |     for (int num : subvector1) {       |                    ^~~~~~~~~~ 3b0b55c4-5ce8-4814-be0e-e6ab660fda5c.cpp:28:9: error: ‘cout’ was not declared in  this scope    28 |         cout << num << endl;       |         ^~~~ 3b0b55c4-5ce8-4814-be0e-e6ab660fda5c.cpp:28:24: error: ‘endl’ was not declared i n this scope    28 |         cout << num << endl;       |                        ^~~~ 3b0b55c4-5ce8-4814-be0e-e6ab660fda5c.cpp:30:20: error: ‘subvector2’ was not decl ared in this scope    30 |     for (int num : subvector2) {       |                    ^~~~~~~~~~ 3b0b55c4-5ce8-4814-be0e-e6ab660fda5c.cpp:31:9: error: ‘cout’ was not declared in  this scope    31 |         cout << num << endl;       |         ^~~~ 3b0b55c4-5ce8-4814-be0e-e6ab660fda5c.cpp:31:24: error: ‘endl’ was not declared i n this scope    31 |         cout << num << endl;       |                        ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,35 +1,38 @@
+#include <iostream>
+#include <vector>
+
 int main() {
     int n;
-    cin >> n;
-    vector<int> nums(n);
+    std::cin >> n;
+
+    std::vector<int> nums(n);
     for (int i = 0; i < n; ++i) {
-        cin >> nums[i];
+        std::cin >> nums[i];
     }
-    
-    int diff = INT_MAX;
-    int cutIndex = -1;
-    for (int i = 1; i < n; ++i) {
-        int leftSum = 0, rightSum = 0;
-        for (int j = 0; j < i; ++j) {
-            leftSum += nums[j];
-        }
-        for (int j = i; j < n; ++j) {
-            rightSum += nums[j];
-        }
-        int currentDiff = abs(leftSum - rightSum);
-        if (currentDiff < diff) {
-            diff = currentDiff;
-            cutIndex = i;
-        }
+
+    int sum = 0;
+    for (int num : nums) {
+        sum += num;
     }
-    
-    for (int i = 0; i < cutIndex; ++i) {
-        cout << nums[i] << endl;
+
+    int halfSum = sum / 2;
+    int currSum = 0;
+    int idx = 0;
+
+    while (currSum <= halfSum) {
+        currSum += nums[idx];
+        idx++;
     }
-    cout << endl;
-    for (int i = cutIndex; i < n; ++i) {
-        cout << nums[i] << endl;
+
+    std::vector<int> subvector1(nums.begin(), nums.begin() + idx);
+    std::vector<int> subvector2(nums.begin() + idx, nums.end());
+
+    for (int num : subvector1) {
+        std::cout << num << std::endl;
     }
-    
+    for (int num : subvector2) {
+        std::cout << num << std::endl;
+    }
+
     return 0;
 }
