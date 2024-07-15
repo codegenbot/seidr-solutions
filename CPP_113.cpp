bcac00dd357c0f4c6def6f79db9f09580febf367
beep boop <githubbot@vadim.me>
2024-07-15 21:01:35 +0200

    Solution 47 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/li5so7yb Prompt:       |                                            ^ dd16699f-4ded-40ce-8cc0-46505422e31b.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from dd16699f-4ded-40ce-8cc0-46505422e31b.cpp:15: dd16699f-4ded-40ce-8cc0-46505422e31b.cpp: In function ‘int main()’: dd16699f-4ded-40ce-8cc0-46505422e31b.cpp:25:20: error: ‘odd_count’ was not decla red in this scope    25 |     assert (issame(odd_count({"271", "137", "314"}) , {       |                    ^~~~~~~~~ dd16699f-4ded-40ce-8cc0-46505422e31b.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(odd_count({"271", "137", "314"}) , {       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    26 |         "the number of odd elements 2n the str2ng 2 of the 2nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    27 |         "the number of odd elements 3n the str3ng 3 of the 3nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    28 |         "the number of odd elements 2n the str2ng 2 of the 2nput."       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    29 |     }));       |     ~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -21,7 +21,7 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b){
 }
 
 int main() {
-    assert(issame(odd_count({"271", "137", "314"}), {
+    assert (issame(odd_count({"271", "137", "314"}), {
         "the number of odd elements 2 in the string 271 of the input.",
         "the number of odd elements 2 in the string 137 of the input.",
         "the number of odd elements 2 in the string 314 of the input."
