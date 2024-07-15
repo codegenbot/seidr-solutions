20b8f5d61c81aab32e90d57263c355f4270acabc
beep boop <githubbot@vadim.me>
2024-07-15 21:16:21 +0200

    Solution 85 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/8otbk1bh Prompt:       |                    ^~~ 8ce6c5f5-5ce3-4777-b7f6-17ea415e2317.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8ce6c5f5-5ce3-4777-b7f6-17ea415e2317.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8ce6c5f5-5ce3-4777-b7f6-17ea415e2317.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8ce6c5f5-5ce3-4777-b7f6-17ea415e2317.cpp:24: 8ce6c5f5-5ce3-4777-b7f6-17ea415e2317.cpp: In function ‘int main()’: 8ce6c5f5-5ce3-4777-b7f6-17ea415e2317.cpp:34:20: error: ‘get_odd_collatz’ was not  declared in this scope    34 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 8ce6c5f5-5ce3-4777-b7f6-17ea415e2317.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
@@ -13,8 +13,8 @@ bool issame(vector<int> a, vector<int> b) {
     return true;
 }
 
-vector<int> get_odd_collatz(int n) {
-    vector<int> result;
+std::vector<int> get_odd_collatz(int n) {
+    std::vector<int> result;
     while (n != 1) {
         result.push_back(n);
         if (n % 2 == 0) {
@@ -25,14 +25,14 @@ vector<int> get_odd_collatz(int n) {
     }
     result.push_back(1);
 
-    vector<int> odd_numbers;
+    std::vector<int> odd_numbers;
     for (int num : result) {
         if (num % 2 != 0) {
             odd_numbers.push_back(num);
         }
     }
 
-    sort(odd_numbers.begin(), odd_numbers.end());
+    std::sort(odd_numbers.begin(), odd_numbers.end());
 
     return odd_numbers;
 }
