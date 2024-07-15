d2888089820a30e05c5534fc9e9a5e024b8247de
beep boop <githubbot@vadim.me>
2024-07-15 22:19:33 +0200

    Solution 30 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/bt8oymu6 Prompt:       |                           ^~~~~~ a2bd0f55-cc24-4e49-a779-ff2a70c88cc7.cpp:33:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from a2bd0f55-cc24-4e49-a779-ff2a70c88cc7.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from a2bd0f55-cc24-4e49-a779-ff2a70c88cc7.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a2bd0f55-cc24-4e49-a779-ff2a70c88cc7.cpp:33:34: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a2bd0f55-cc24-4e49-a779-ff2a70c88cc7.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ a2bd0f55-cc24-4e49-a779-ff2a70c88cc7.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -24,7 +24,6 @@ std::vector<int> parse_music(std::string music_string) {
     return beats;
 }
 
-int main() {
+void main() {
     assert(issame(parse_music("o| .| o| .| o o| o o|"), std::vector<int>{2, 1, 2, 1, 1, 2, 1, 2}));
-    return 0;
 }
