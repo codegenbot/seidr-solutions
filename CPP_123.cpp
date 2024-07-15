8f22161f5eaea232d2121997736c34ef9415fd25
beep boop <githubbot@vadim.me>
2024-07-16 00:09:42 +0200

    Solution 69 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/brlulnfb Prompt: in this scope     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6a56d346-a313-4b51-bc59-fd65cdf127af.cpp:7:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 6a56d346-a313-4b51-bc59-fd65cdf127af.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 6a56d346-a313-4b51-bc59-fd65cdf127af.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 6a56d346-a313-4b51-bc59-fd65cdf127af.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 6a56d346-a313-4b51-bc59-fd65cdf127af.cpp:6: 6a56d346-a313-4b51-bc59-fd65cdf127af.cpp: In function ‘int main()’: 6a56d346-a313-4b51-bc59-fd65cdf127af.cpp:16:20: error: ‘get_odd_collatz’ was not  declared in this scope    16 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,26 +1,5 @@
 #include <vector>
-#include <algorithm>
-#include <cassert>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
+using namespace std;
+bool issame(vector<int> a, vector<int> b) {
     return std::equal(a.begin(), a.end(), b.begin(), b.end());
-}
-
-std::vector<int> get_odd_collatz(int n) {
-    std::vector<int> result;
-    while (n != 1) {
-        result.push_back(n);
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else {
-            n = 3 * n + 1;
-        }
-    }
-    result.push_back(1);
-    return result;
-}
-
-int main() {
-    assert(issame(get_odd_collatz(1), {1}));
-    return 0;
 }
