f38dbcbd8e6ec00e6e2808b73789c392403103da
beep boop <githubbot@vadim.me>
2024-07-15 13:41:08 +0200

    Solution 24 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/dob9ydvf Prompt:                  from 33b3b3d0-c743-450b-804c-23394fdd256b.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 33b3b3d0-c743-450b-804c-23394fdd256b.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 33b3b3d0-c743-450b-804c-23394fdd256b.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 33b3b3d0-c743-450b-804c-23394fdd256b.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 33b3b3d0-c743-450b-804c-23394fdd256b.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -12,8 +12,3 @@ std::vector<int> unique(const std::vector<int>& l) {
     temp.erase(std::unique(temp.begin(), temp.end()), temp.end());
     return temp;
 }
-
-int main() {
-    assert(issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}), std::vector<int>{0, 2, 3, 5, 9, 123}));
-    return 0;
-}
