c1f02e43bf827eeafb97ca299d625e8ac8c23eb4
beep boop <githubbot@vadim.me>
2024-07-15 15:04:45 +0200

    Solution 3 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/1gzy9ccx Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 45f9d05f-f521-43ef-a496-0bd02bb32ace.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 45f9d05f-f521-43ef-a496-0bd02bb32ace.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 45f9d05f-f521-43ef-a496-0bd02bb32ace.cpp:26: 45f9d05f-f521-43ef-a496-0bd02bb32ace.cpp: In function ‘int main()’: 45f9d05f-f521-43ef-a496-0bd02bb32ace.cpp:36:20: error: ‘select_words’ was not de clared in this scope    36 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 45f9d05f-f521-43ef-a496-0bd02bb32ace.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,14 +1,15 @@
-vector<string> issame(vector<string> a, vector<string> b) {
-    return a == b;
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
 }
 
 vector<string> select_words(string s, int n);
 
 int main() {
-    assert(issame(select_words("apple banana orange", 2), {"apple", "orange"}));
-    assert(issame(select_words("hello world", 3), {"world"}));
-    assert(issame(select_words("cpp code contest", 4), {"contest"}));
-    return 0;
+    assert(issame(select_words("hello world goodbye", 2), {"hello", "goodbye"}));
 }
 
 vector<string> select_words(string s, int n) {
