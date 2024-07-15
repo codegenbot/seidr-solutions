57e514300a1dbc1003001ebbafd726b017f95f27
beep boop <githubbot@vadim.me>
2024-07-15 19:06:00 +0200

    Solution 59 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/4iab2tci Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5b5bab62-e580-483d-bf98-fa9c652b0df9.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5b5bab62-e580-483d-bf98-fa9c652b0df9.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 5b5bab62-e580-483d-bf98-fa9c652b0df9.cpp:11: 5b5bab62-e580-483d-bf98-fa9c652b0df9.cpp: In function ‘int main()’: 5b5bab62-e580-483d-bf98-fa9c652b0df9.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 5b5bab62-e580-483d-bf98-fa9c652b0df9.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,14 +1,11 @@
-#include <vector>
-#include <string>
-
 bool issame(const string& a, const string& b) {
-    return a == b;
+    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
 }
 
 vector<string> filter_by_substring(vector<string> strings, string substring) {
     vector<string> result;
     for (const string& str : strings) {
-        if (issame(str, substring)) {
+        if (str.find(substring) != string::npos) {
             result.push_back(str);
         }
     }
