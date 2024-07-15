1feb594a04efd589848ffc6adae279b379331c9b
beep boop <githubbot@vadim.me>
2024-07-15 18:52:03 +0200

    Solution 32 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/v4s4ghmm Prompt:     9 | for(int i = 0; i < 16; i++) {       | ^~~ ad3be531-62f8-437e-a28c-6f1b171db870.cpp:9:16: error: ‘i’ does not name a type     9 | for(int i = 0; i < 16; i++) {       |                ^ ad3be531-62f8-437e-a28c-6f1b171db870.cpp:9:24: error: ‘i’ does not name a type     9 | for(int i = 0; i < 16; i++) {       |                        ^ ad3be531-62f8-437e-a28c-6f1b171db870.cpp:13:1: error: expected unqualified-id be fore ‘return’    13 | return mdString;       | ^~~~~~ ad3be531-62f8-437e-a28c-6f1b171db870.cpp:14:1: error: expected declaration befor e ‘}’ token    14 | }       | ^ In file included from ad3be531-62f8-437e-a28c-6f1b171db870.cpp:16: ad3be531-62f8-437e-a28c-6f1b171db870.cpp: In function ‘int main()’: ad3be531-62f8-437e-a28c-6f1b171db870.cpp:18:13: error: ‘string_to_md5’ was not d eclared in this scope    18 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,9 +1,8 @@
-
 #include <iostream>
 #include <string>
 #include <openssl/md5.h>
 
-std::string string_to_md5(std::string text) {
+std::string string_to_md5(const std::string& text) {
     if (text.empty()) {
         return "None";
     }
@@ -11,7 +10,7 @@ std::string string_to_md5(std::string text) {
     unsigned char digest[MD5_DIGEST_LENGTH];
     MD5((unsigned char*)text.c_str(), text.length(), digest);
 
-    char mdString[33];
+    char mdString[33] = {0};
     for(int i = 0; i < 16; i++) {
         sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
     }
@@ -20,12 +19,7 @@ std::string string_to_md5(std::string text) {
 }
 
 int main() {
-    std::string input_text;
-    std::cout << "Enter the text to generate MD5 hash: ";
-    std::cin >> input_text;
-
-    std::string md5_hash = string_to_md5(input_text);
-    std::cout << "MD5 hash of the input text is: " << md5_hash << std::endl;
+    assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
 
     return 0;
 }
