bb47ea073b7091c10604e18eced067511666ba14
beep boop <githubbot@vadim.me>
2024-07-15 12:30:50 +0200

    Solution 2 of Python/102, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/alr2oci9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/76c8694b-43d2-4c2b-ae57-b288ac698 056.py", line 12, in <module>     check(choose_num)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/76c8694b-43d2-4c2b-ae57-b288ac698 056.py", line 9, in check     assert choose_num(546, 546) == 546            ^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_102.py
+++ Python_102.py
@@ -1,8 +1,8 @@
-```
+```Python
 def choose_num(x, y):
-    if x == y:
-        return min(x, y)
-    for i in range(max(x, y), min(x, y), -1):
-        if i % 2 == 0:
+    if x % 2 == 0 and y % 2 == 0:
+        return max(x, y)
+    for i in range(max(x, y), min(x, y) - 1, -1):
+        if i % 2 != 0:
             return i
     return -1
