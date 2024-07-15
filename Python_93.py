764afdf96775defb6c2c3fde3fa04e7280ae795c
beep boop <githubbot@vadim.me>
2024-07-15 21:36:22 +0200

    Solution 87 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/yxhj599w Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f171cb31-2b47-42c3-8282-3af5d972f 134.py", line 24, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f171cb31-2b47-42c3-8282-3af5d972f 134.py", line 20, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -3,13 +3,9 @@ def encode(message):
     return "".join(
         char if not char.isalpha()
         else (
-            chr(((ord(char.upper()) - ord("A") + 2) % 26) + ord("A")).swapcase()
+            chr(((ord(char.lower()) - ord("a") - 2) % 26) + ord("a")).swapcase()
             if char.lower() not in vowels
-            else (
-                chr(ord(char) ^ 32)
-                if char.isalpha()
-                else char
-            )
+            else char.swapcase()
         )
         for char in message
     )
