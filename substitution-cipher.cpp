b666ed9ba85d3b6e2768c80ded5da149948c895b
beep boop <githubbot@vadim.me>
2024-07-15 16:08:17 +0200

    Solution 31 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/1q3yel05 Prompt: his scope     3 |     cin >> cipher1 >> cipher2 >> message;       |     ^~~ 6be2a400-892b-4832-aca3-98eb2d5f1b51.cpp:3:12: error: ‘cipher1’ was not declared  in this scope     3 |     cin >> cipher1 >> cipher2 >> message;       |            ^~~~~~~ 6be2a400-892b-4832-aca3-98eb2d5f1b51.cpp:3:23: error: ‘cipher2’ was not declared  in this scope     3 |     cin >> cipher1 >> cipher2 >> message;       |                       ^~~~~~~ 6be2a400-892b-4832-aca3-98eb2d5f1b51.cpp:3:34: error: ‘message’ was not declared  in this scope     3 |     cin >> cipher1 >> cipher2 >> message;       |                                  ^~~~~~~ 6be2a400-892b-4832-aca3-98eb2d5f1b51.cpp:11:5: error: ‘cout’ was not declared in  this scope    11 |     cout << message << endl;       |     ^~~~ 6be2a400-892b-4832-aca3-98eb2d5f1b51.cpp:11:24: error: ‘endl’ was not declared i n this scope    11 |     cout << message << endl;       |                        ^~~~

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -7,9 +7,8 @@ int main() {
     cin >> cipher1 >> cipher2 >> message;
 
     for (char &c : message) {
-        size_t index = cipher1.find(c);
-        if (index != string::npos) {
-            c = cipher2[index];
+        if (c == cipher1[0]) {
+            c = cipher2[0];
         }
     }
 
