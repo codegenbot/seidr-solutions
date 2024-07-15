9554a44bcfeac3f51bf889b3ca049529772783e3
beep boop <githubbot@vadim.me>
2024-07-15 22:21:50 +0200

    Solution 6 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/vioanjrt Prompt:       |                    ^~~ b545d06b-f4fb-41e8-a9f8-ce32add98ba3.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b545d06b-f4fb-41e8-a9f8-ce32add98ba3.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b545d06b-f4fb-41e8-a9f8-ce32add98ba3.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b545d06b-f4fb-41e8-a9f8-ce32add98ba3.cpp:12: b545d06b-f4fb-41e8-a9f8-ce32add98ba3.cpp: In function ‘int main()’: b545d06b-f4fb-41e8-a9f8-ce32add98ba3.cpp:22:20: error: ‘intersperse’ was not dec lared in this scope    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ b545d06b-f4fb-41e8-a9f8-ce32add98ba3.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -1,32 +1,30 @@
 #include <vector>
+#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    if(a.size() != b.size()){
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b){
+    if(a.size() != b.size())
         return false;
-    }
-    
-    for(int i = 0; i < a.size(); ++i){
-        if(a[i] != b[i]){
+    for(int i=0; i<a.size(); ++i){
+        if(a[i] != b[i])
             return false;
-        }
     }
-    
     return true;
 }
 
-std::vector<int> intersperse(std::vector<int> numbers, int delimiter){
-    std::vector<int> result;
-    for (int i = 0; i < numbers.size(); ++i) {
+vector<int> intersperse(vector<int> numbers, int delimeter){ 
+    vector<int> result;
+    for(int i = 0; i < numbers.size(); ++i){
         result.push_back(numbers[i]);
-        if (i < numbers.size() - 1) {
-            result.push_back(delimiter);
+        if(i != numbers.size() - 1){
+            result.push_back(delimeter);
         }
     }
     return result;
 }
 
 int main(){
-    // Test case using issame and intersperse
-    // assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
+    assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
     return 0;
 }
