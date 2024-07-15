8c20b8bc5dda544e21f0e066b010eb4f8f109cc2
beep boop <githubbot@vadim.me>
2024-07-15 14:09:10 +0200

    Solution 44 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/mvm6ec7t Prompt:       |                             ^~~~~~ 7bdbe143-3d9d-4cef-92d4-229768115a5d.cpp:35:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 7bdbe143-3d9d-4cef-92d4-229768115a5d.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 7bdbe143-3d9d-4cef-92d4-229768115a5d.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7bdbe143-3d9d-4cef-92d4-229768115a5d.cpp:35:36: error: expected primary-expressi on before ‘float’    35 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 7bdbe143-3d9d-4cef-92d4-229768115a5d.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 7bdbe143-3d9d-4cef-92d4-229768115a5d.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -24,3 +24,9 @@ std::vector<float> sort_even(const std::vector<float>& l) {
     }
     return l;
 }
+
+int main() {
+    assert(issame(sort_even({5.0f, 8.0f, -12.0f, 4.0f, 23.0f, 2.0f, 3.0f, 11.0f, 12.0f, -10.0f}), {-12.0f, 8.0f, 3.0f, 4.0f, 5.0f, 2.0f, 12.0f, 11.0f, 23.0f, -10.0f}));
+    
+    return 0;
+}
