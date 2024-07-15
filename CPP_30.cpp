42381b274169153903a224ae4a67d62ff2e345a0
beep boop <githubbot@vadim.me>
2024-07-15 14:06:04 +0200

    Solution 66 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/k5tuz6xw Prompt: in this scope     8 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 9392f8f5-76c0-4b3c-a505-b52380b044e0.cpp:8:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 9392f8f5-76c0-4b3c-a505-b52380b044e0.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 9392f8f5-76c0-4b3c-a505-b52380b044e0.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9392f8f5-76c0-4b3c-a505-b52380b044e0.cpp:8:36: error: expected primary-expressio n before ‘float’     8 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from 9392f8f5-76c0-4b3c-a505-b52380b044e0.cpp:7: 9392f8f5-76c0-4b3c-a505-b52380b044e0.cpp: In function ‘int main()’: 9392f8f5-76c0-4b3c-a505-b52380b044e0.cpp:17:20: error: ‘get_positive’ was not de clared in this scope    17 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,49 +1,5 @@
-#include <iostream>
 #include <vector>
 
 bool issame(std::vector<float> a, std::vector<float> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
-}
-
-std::vector<float> get_positive(std::vector<float> numbers) {
-    std::vector<float> positive_numbers;
-    
-    for (float num : numbers) {
-        if (num > 0) {
-            positive_numbers.push_back(num);
-        }
-    }
-    
-    return positive_numbers;
-}
-
-int main() {
-    std::vector<float> vec1 = {1.5, -2.3, 3.4, 0.0, 5.2};
-    std::vector<float> vec2 = {1.5, -2.3, 3.4, 0.0, 5.2};
-    
-    if (issame(vec1, vec2)) {
-        std::cout << "Vectors are the same." << std::endl;
-    } else {
-        std::cout << "Vectors are different." << std::endl;
-    }
-    
-    std::vector<float> positive_nums = get_positive(vec1);
-    
-    std::cout << "Positive numbers: ";
-    for (float num : positive_nums) {
-        std::cout << num << " ";
-    }
-    std::cout << std::endl;
-
-    return 0;
+    return std::is_permutation(a.begin(), a.end(), b.begin());
 }
