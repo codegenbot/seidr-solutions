ffc6686be8478a040b9d0196bd177fde4c34b4bf
beep boop <githubbot@vadim.me>
2024-07-15 19:15:06 +0200

    Solution 83 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/w2ctav9j Prompt: In file included from 27dbc443-4bb6-42d3-8891-607cd5b591a3.cpp:20: 27dbc443-4bb6-42d3-8891-607cd5b591a3.cpp: In function ‘int main()’: 27dbc443-4bb6-42d3-8891-607cd5b591a3.cpp:22:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    22 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -19,8 +19,8 @@ string int_to_mini_roman(int number){
 }
 
 int main() {
-    int input;
-    cin >> input;
-    cout << int_to_mini_roman(input) << endl;
+    int number;
+    cin >> number;
+    cout << int_to_mini_roman(number) << endl;
     return 0;
 }
