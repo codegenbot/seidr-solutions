504d11aecbd987ceb1da6bf6e74f73050305122b
beep boop <githubbot@vadim.me>
2024-07-15 20:34:16 +0200

    Solution 4 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/fm0gg5wu Prompt: Complete the following code given the task description and function signature.

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,12 +1,12 @@
 int is_bored(string S){
     int count = 0;
-    bool is_boredom = false;
+    bool isI = false;
     for (int i = 0; i < S.length(); ++i) {
-        if (S[i] == 'I' && (i == 0 || S[i-1] == '.' || S[i-1] == '?' || S[i-1] == '!')) {
-            is_boredom = true;
-        } else if ((S[i] == '.' || S[i] == '?' || S[i] == '!') && is_boredom) {
+        if (S[i] == 'I' && (i == 0 || S[i - 1] == '.' || S[i - 1] == '?' || S[i - 1] == '!')) {
+            isI = true;
+        } else if (isI && (S[i] == '.' || S[i] == '?' || S[i] == '!')) {
             count++;
-            is_boredom = false;
+            isI = false;
         }
     }
     return count;
