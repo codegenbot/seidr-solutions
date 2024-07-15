981ddbe5de37ca3247268af80e78efbf98109e30
beep boop <githubbot@vadim.me>
2024-07-15 16:29:19 +0200

    Solution 30 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ano08tdj Prompt:    14 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 1d63db81-15f5-4fb2-ae50-afe4c2afce6e.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 1d63db81-15f5-4fb2-ae50-afe4c2afce6e.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 1d63db81-15f5-4fb2-ae50-afe4c2afce6e.cpp:13: 1d63db81-15f5-4fb2-ae50-afe4c2afce6e.cpp: In function ‘int main()’: 1d63db81-15f5-4fb2-ae50-afe4c2afce6e.cpp:23:20: error: ‘rescale_to_unit’ was not  declared in this scope    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 1d63db81-15f5-4fb2-ae50-afe4c2afce6e.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,18 +1,10 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
 bool issame(vector<float> a, vector<float> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
+    return a == b;
 }
 
 vector<float> rescale_to_unit(vector<float> numbers) {
@@ -30,4 +22,6 @@ vector<float> rescale_to_unit(vector<float> numbers) {
 
 int main() {
     assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), {0.25, 0.0, 1.0, 0.5, 0.75}));
+    
+    return 0;
 }
