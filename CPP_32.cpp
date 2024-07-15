0e39bffaf00b76aa88844b847076e26760aa94f4
beep boop <githubbot@vadim.me>
2024-07-15 16:34:25 +0200

    Solution 10 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/p00md7m8 Prompt: 400b2728-36f2-406c-b155-2c52c139de2e.cpp:20:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    20 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 400b2728-36f2-406c-b155-2c52c139de2e.cpp:23:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    23 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 400b2728-36f2-406c-b155-2c52c139de2e.cpp:23:36: error: ‘find_zero’ cannot be use d as a function    23 |         solution = find_zero(coeffs);       |                                    ^ In file included from 400b2728-36f2-406c-b155-2c52c139de2e.cpp:7: 400b2728-36f2-406c-b155-2c52c139de2e.cpp:24:21: error: ‘poly’ was not declared i n this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 400b2728-36f2-406c-b155-2c52c139de2e.cpp:24:17: error: ‘abs’ was not declared in  this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -6,7 +6,7 @@
 double poly(vector<double> coeffs, double x){
     double result = 0;
     for(int i=0; i<coeffs.size(); i++){
-        result += coeffs[i]*pow(x, i);
+        result += coeffs[i] * pow(x, i);
     }
     return result;
 }
@@ -17,16 +17,16 @@ double find_zero(vector<double> coeffs){
     return -b/a;
 }
 
-std::vector<double> coeffs;
+vector<double> coeffs;
 double solution;
 
+// Read coefficients input from user into coeffs vector
 for(int i=0; i<2; i++){
     double coeff;
-    std::cout << "Enter coefficient " << i+1 << ": ";
     std::cin >> coeff;
     coeffs.push_back(coeff);
 }
 
 solution = find_zero(coeffs);
 
-assert(abs(poly(coeffs, solution)) < 1e-3);
+assert(abs(poly(coeffs, solution)) < 1e-3;
