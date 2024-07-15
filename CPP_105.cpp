ab8f31e95948f3e24fba57b8071225e8affd15cd
beep boop <githubbot@vadim.me>
2024-07-15 14:55:16 +0200

    Solution 2 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/mxsocjn4 Prompt:       |                              ^~~~~~ f8a6d853-09bb-473a-b3a3-d25d8a6451a2.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f8a6d853-09bb-473a-b3a3-d25d8a6451a2.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f8a6d853-09bb-473a-b3a3-d25d8a6451a2.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f8a6d853-09bb-473a-b3a3-d25d8a6451a2.cpp:25: f8a6d853-09bb-473a-b3a3-d25d8a6451a2.cpp: In function ‘int main()’: f8a6d853-09bb-473a-b3a3-d25d8a6451a2.cpp:35:20: error: ‘by_length’ was not decla red in this scope    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ f8a6d853-09bb-473a-b3a3-d25d8a6451a2.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,7 +1,16 @@
+#include <string>
+
+vector<string> by_length(vector<int> arr);
+bool issame(vector<string> a, vector<string> b);
+
 vector<string> by_length(vector<int> arr){
     vector<string> result;
-    vector<int> valid_nums;
+    map<int, string> num_to_name = {
+        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
+        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
+    };
 
+    vector<int> valid_nums;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
             valid_nums.push_back(num);
@@ -11,13 +20,8 @@ vector<string> by_length(vector<int> arr){
     sort(valid_nums.begin(), valid_nums.end());
     reverse(valid_nums.begin(), valid_nums.end());
 
-    map<int, string> num_to_str = {
-        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
-        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
-    };
-
     for (int num : valid_nums) {
-        result.push_back(num_to_str[num]);
+        result.push_back(num_to_name[num]);
     }
 
     return result;
