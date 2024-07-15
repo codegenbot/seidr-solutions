905de09cd070918e868df4d49d825fc2540ebbaa
beep boop <githubbot@vadim.me>
2024-07-15 20:50:31 +0200

    Solution 16 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/h6hh3slp Prompt:       |                    ^~~ 9b32eee2-66a4-4ed1-b9f8-5a41d39057bf.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9b32eee2-66a4-4ed1-b9f8-5a41d39057bf.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9b32eee2-66a4-4ed1-b9f8-5a41d39057bf.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9b32eee2-66a4-4ed1-b9f8-5a41d39057bf.cpp:18: 9b32eee2-66a4-4ed1-b9f8-5a41d39057bf.cpp: In function ‘int main()’: 9b32eee2-66a4-4ed1-b9f8-5a41d39057bf.cpp:28:20: error: ‘even_odd_palindrome’ was  not declared in this scope    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 9b32eee2-66a4-4ed1-b9f8-5a41d39057bf.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,10 +1,29 @@
-vector<int> result(2, 0);
-    for (int i = 1; i <= n; ++i) {
+#include <vector>
+#include <algorithm>
+#include <string>
+
+bool issame(const string& a, const string& b) {
+    return a == b;
+}
+
+vector<int> even_odd_palindrome(int n);
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> result = even_odd_palindrome(n);
+    cout << result[0] << " " << result[1];
+    return 0;
+}
+
+vector<int> even_odd_palindrome(int n){
+    vector<int> result(2, 0);
+    for(int i=1; i<=n; ++i){
         string num = to_string(i);
-        string temp = num;
-        reverse(temp.begin(), temp.end());
-        if (num == temp) {
-            if (i % 2 == 0) {
+        string rev_num = num;
+        reverse(rev_num.begin(), rev_num.end());
+        if(issame(num, rev_num)){
+            if(i % 2 == 0){
                 result[0]++;
             } else {
                 result[1]++;
