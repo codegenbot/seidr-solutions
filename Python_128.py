2daabf941bfab663208acb45c96d3a30e7a9ec35
beep boop <githubbot@vadim.me>
2024-07-15 11:32:59 +0200

    Solution 2 of Python/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lae19hqy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ea6e8f6f-f804-4fcd-9292-bb3fa816a 190.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_128.py
+++ Python_128.py
@@ -1,8 +1,8 @@
 ```
 def prod_signs(arr):
-    if len(arr) == 0:
+    if not arr:
         return None
-    sign = 1
+    sign_product = 1
     for num in arr:
-        sign *= -1 if num < 0 else (1 if num > 0 else 0)
-    return sum(abs(num) for num in arr) * sign
+        sign_product *= -1 if num < 0 else (1 if num == 0 else 1)
+    return sum(abs(num) for num in arr) * sign_product
