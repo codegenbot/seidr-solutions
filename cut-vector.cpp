cdbf97c7b19dbc6613ee1855712f0982aed575fb
beep boop <githubbot@vadim.me>
2024-07-15 21:17:46 +0200

    Solution 61 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/r9npe7x9 Prompt: 3fa61df7-592c-45f9-8326-a10b9bc80f75.cpp: In function ‘std::vector<std::vector<i nt> > cutVector(std::vector<int>)’: 3fa61df7-592c-45f9-8326-a10b9bc80f75.cpp:50:16: error: expected ‘}’ at end of in put    50 |     return res;       |                ^ 3fa61df7-592c-45f9-8326-a10b9bc80f75.cpp:5:46: note: to match this ‘{’     5 | vector<vector<int>> cutVector(vector<int> v) {       |                                              ^

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,4 +1,4 @@
-```c++
+```
 #include <vector>
 using namespace std;
 
@@ -6,35 +6,22 @@ vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
     
-    for(int i = 0; i < n; i++) {
-        if(i == 0 || i == n-1)
-            res[0].push_back(v[i]);
+    for(int i = 0; i < n-1; i++) {
+        if(abs(v[i] - v[0]) <= abs(v[n-1] - v[i])) {
+            res[0].clear();
+            for(int j = 0; j <= i; j++)
+                res[0].push_back(v[j]);
+            break;
+        }
         else {
-            if(abs(v[0] - v[i]) <= abs(v[n-1] - v[i])) {
-                res[0].clear();
-                for(int j = 0; j < i; j++)
-                    res[0].push_back(v[j]);
-                res[0].push_back(v[i]);
-                break;
-            }
-            else {
-                res[0].clear();
-                for(int j = i; j < n; j++)
-                    res[0].push_back(v[j]);
-                break;
-            }
+            res[0].clear();
+            for(int j = i+1; j < n; j++)
+                res[0].push_back(v[j]);
+            break;
         }
     }
     
-    if(res[0].size() != n) {
-        int half = res[0].size();
-        for(int i = 0; i < half; i++) {
-            res[1].push_back(v[i]);
-        }
-        for(int i = half; i < n; i++) {
-            res[1].push_back(v[i]);
-        }
-    }
+    res[1] = vector<int>(v.begin() + res[0].size(), v.end());
     
     return res;
 }``
