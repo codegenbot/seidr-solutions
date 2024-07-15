efccec9045962e2a5d50e8a9a1eade59b985a7a6
beep boop <githubbot@vadim.me>
2024-07-15 17:23:30 +0200

    Solution 20 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/hx3vam35 Prompt:       |                    ^~~~~ 8cf77a9f-5489-4d95-ba42-38813987dd35.cpp:9:29: error: ‘vector’ was not declared in this scope     9 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 8cf77a9f-5489-4d95-ba42-38813987dd35.cpp:9:36: error: expected primary-expressio n before ‘float’     9 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 8cf77a9f-5489-4d95-ba42-38813987dd35.cpp:9:43: error: expression list treated as  compound expression in initializer [-fpermissive]     9 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 8cf77a9f-5489-4d95-ba42-38813987dd35.cpp:8: 8cf77a9f-5489-4d95-ba42-38813987dd35.cpp: In function ‘int main()’: 8cf77a9f-5489-4d95-ba42-38813987dd35.cpp:18:20: error: ‘derivative’ was not decl ared in this scope    18 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~ 8cf77a9f-5489-4d95-ba42-38813987dd35.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -1,18 +1,13 @@
 #include <vector>
 
-bool issame(std::vector<float> a, std::vector<float> b){
+bool issame(float a, float b){
     return a == b;
 }
 
-std::vector<float> derivative(std::vector<float> xs){
+vector<float> derivative(std::vector<float> xs) {
     std::vector<float> result;
     for(int i = 1; i < xs.size(); i++){
         result.push_back(xs[i] * i);
     }
     return result;
-}
-
-int main(){
-    assert(issame(derivative({1}), {}));
-    return 0;
 }
