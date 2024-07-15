4feb630368215f81fa163a1b973edc872aa3873d
beep boop <githubbot@vadim.me>
2024-07-15 18:40:30 +0200

    Solution 28 of CPP/141, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ghonsvvs Prompt: 2fc401b1-70bf-49ed-9e7a-e45b4ca7bb9b.cpp:18:5: error: ‘string’ does not name a t ype    18 |     string extension = file_name.substr(dotIndex + 1);       |     ^~~~~~ 2fc401b1-70bf-49ed-9e7a-e45b4ca7bb9b.cpp:19:5: error: expected unqualified-id be fore ‘if’    19 |     if (extension != "txt" && extension != "exe" && extension != "dll") {       |     ^~ 2fc401b1-70bf-49ed-9e7a-e45b4ca7bb9b.cpp:23:5: error: expected unqualified-id be fore ‘return’    23 |     return "Yes";       |     ^~~~~~ 2fc401b1-70bf-49ed-9e7a-e45b4ca7bb9b.cpp:24:1: error: expected declaration befor e ‘}’ token    24 | }       | ^ In file included from 2fc401b1-70bf-49ed-9e7a-e45b4ca7bb9b.cpp:26: 2fc401b1-70bf-49ed-9e7a-e45b4ca7bb9b.cpp: In function ‘int main()’: 2fc401b1-70bf-49ed-9e7a-e45b4ca7bb9b.cpp:28:13: error: ‘file_name_check’ was not  declared in this scope    28 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,28 +1,27 @@
-int num_digits = 0;
-    int dot_pos = -1;
-    
-    for (int i = 0; i < file_name.size(); ++i) {
+#include <string>
+
+std::string file_name_check(const std::string& file_name) {
+    int digits = 0;
+    int dot = 0;
+    int dotIndex = -1;
+
+    for (int i = 0; i < file_name.length(); i++) {
         if (file_name[i] >= '0' && file_name[i] <= '9') {
-            num_digits++;
+            digits++;
         } else if (file_name[i] == '.') {
-            dot_pos = i;
+            dot++;
+            dotIndex = i;
         }
     }
-    
-    if (num_digits > 3 || dot_pos == -1 || dot_pos == 0 || dot_pos == file_name.size() - 1) {
-        return "No";
+
+    if (digits > 3 || dot != 1 || dotIndex == 0 || dotIndex == file_name.length() - 1) {
+        return std::string("No");
     }
-    
-    string before_dot = file_name.substr(0, dot_pos);
-    string after_dot = file_name.substr(dot_pos + 1);
-    
-    if (!(before_dot[0] >= 'a' && before_dot[0] <= 'z') && !(before_dot[0] >= 'A' && before_dot[0] <= 'Z')) {
-        return "No";
+
+    std::string extension = file_name.substr(dotIndex + 1);
+    if (extension != "txt" && extension != "exe" && extension != "dll") {
+        return std::string("No");
     }
-    
-    if (after_dot != "txt" && after_dot != "exe" && after_dot != "dll") {
-        return "No";
-    }
-    
-    return "Yes";
+
+    return std::string("Yes");
 }
