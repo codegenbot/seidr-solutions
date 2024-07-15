a78878217fb83660d9944846567e8c3d064b0b8c
beep boop <githubbot@vadim.me>
2024-07-15 20:50:03 +0200

    Solution 31 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/wktqj69z Prompt:       |                              ^~~~~~ c691863f-1455-44b0-a71d-f4a981254c50.cpp:27:37: error: ‘string’ was not declared  in this scope    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c691863f-1455-44b0-a71d-f4a981254c50.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c691863f-1455-44b0-a71d-f4a981254c50.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c691863f-1455-44b0-a71d-f4a981254c50.cpp:26: c691863f-1455-44b0-a71d-f4a981254c50.cpp: In function ‘int main()’: c691863f-1455-44b0-a71d-f4a981254c50.cpp:36:20: error: ‘by_length’ was not decla red in this scope    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ c691863f-1455-44b0-a71d-f4a981254c50.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -35,6 +35,6 @@ std::vector<std::string> by_length(std::vector<int> arr){
 }
 
 int main() {
-    assert (issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
+    assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
     return 0;
 }
