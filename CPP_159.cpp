2b32a1f110d35a96464e49c27600d32936b61a99
beep boop <githubbot@vadim.me>
2024-07-15 19:05:22 +0200

    Solution 44 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/jc6v89py Prompt:       |           ^~~~~~ In file included from 01122f29-4d10-44c3-9c92-76a547796304.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 01122f29-4d10-44c3-9c92-76a547796304.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 01122f29-4d10-44c3-9c92-76a547796304.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 01122f29-4d10-44c3-9c92-76a547796304.cpp:11: 01122f29-4d10-44c3-9c92-76a547796304.cpp: In function ‘int main()’: 01122f29-4d10-44c3-9c92-76a547796304.cpp:21:20: error: ‘eat’ was not declared in  this scope    21 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ^~~ 01122f29-4d10-44c3-9c92-76a547796304.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
@@ -14,7 +14,7 @@ std::vector<int> eat(int number, int need, int remaining) {
 }
 
 int main() {
-    assert(issame(eat(4, 5, 1), {5, 0}));
+    assert(issame(eat(4, 5, 1), std::vector<int>{5, 0}));
     
     return 0;
 }
