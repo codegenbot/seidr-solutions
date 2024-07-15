893675327410e11e08f1dcea7ba082028fa3e4eb
beep boop <githubbot@vadim.me>
2024-07-15 21:25:42 +0200

    Solution 13 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/5auxt708 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,23 +1,24 @@
-if(a.type() == typeid(int) && b.type() == typeid(int)){
+if(typeid(a) == typeid(int) && typeid(b) == typeid(int)){
     if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
         return a;
-    } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
+    }
+    else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
         return b;
     }
-} else if(a.type() == typeid(float) && b.type() == typeid(float)){
+}
+else if(typeid(a) == typeid(float) && typeid(b) == typeid(float)){
     if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
         return a;
-    } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
+    }
+    else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
         return b;
     }
-} else if(a.type() == typeid(string) && b.type() == typeid(string)){
-    string sa = boost::any_cast<string>(a);
-    string sb = boost::any_cast<string>(b);
-    replace(sa.begin(), sa.end(), ',', '.');
-    replace(sb.begin(), sb.end(), ',', '.');
-    if(stof(sa) > stof(sb)){
+}
+else if(typeid(a) == typeid(string) && typeid(b) == typeid(string)){
+    if(stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))){
         return a;
-    } else if(stof(sa) < stof(sb)){
+    }
+    else if(stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b))){
         return b;
     }
 }
