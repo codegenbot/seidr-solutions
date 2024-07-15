46220db0a18a16cc07a07bc150da2a303b09281b
beep boop <githubbot@vadim.me>
2024-07-15 12:43:03 +0200

    Solution 12 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hj5kthnw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8fdfba7e-78d0-481e-83ee-1206c2a9e 9f0.py", line 22, in <module>     check(parse_music)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8fdfba7e-78d0-481e-83ee-1206c2a9e 9f0.py", line 19, in check     assert parse_music("o| .| o| .| o o| o o|") == [2, 1, 2, 1, 4, 2, 4, 2]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8fdfba7e-78d0-481e-83ee-1206c2a9e 9f0.py", line 4, in parse_music     music_string = ''.join(e for e in music_string if e in 'o'|'.|..| ')                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8fdfba7e-78d0-481e-83ee-1206c2a9e 9f0.py", line 4, in <genexpr>     music_string = ''.join(e for e in music_string if e in 'o'|'.|..| ')                                                            ~~~^~~~~~~~~ TypeError: unsupported operand type(s) for |: 'str' and 'str'

--- Python_17.py
+++ Python_17.py
@@ -1,8 +1,7 @@
-```
 def parse_music(music_string: str) -> list:
     if not music_string:
         return []
-    music_string = ''.join(e for e in music_string if e in 'o' or e in '.|. '..')
+    music_string = ''.join(e for e in music_string if e in {'o', '.', '..'})
     music_list = []
     i = 0
     while i < len(music_string):
@@ -12,7 +11,7 @@ def parse_music(music_string: str) -> list:
         elif music_string[i:i+2] == 'o|':
             music_list.append(2)
             i += 2
-        elif music_string[i:i+3] == '.|..|':
+        elif music_string[i:i+3] == '..|..|':
             music_list.append([8, 4, 2])
             i += 3
     return music_list
