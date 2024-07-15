8f25e229b3131729c8595a30b08aac2c30aedb43
beep boop <githubbot@vadim.me>
2024-07-15 22:35:19 +0200

    Solution 20 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/rksfuoz8 Prompt:       |             ^~~~~~ 33d655f4-7e54-41df-b0f5-76035ddf8597.cpp:20:20: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 33d655f4-7e54-41df-b0f5-76035ddf8597.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 33d655f4-7e54-41df-b0f5-76035ddf8597.cpp:20:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 33d655f4-7e54-41df-b0f5-76035ddf8597.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 33d655f4-7e54-41df-b0f5-76035ddf8597.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 33d655f4-7e54-41df-b0f5-76035ddf8597.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -15,6 +15,38 @@ std::vector<int> incr_list(const std::vector<int>& l) {
 }
 
 int main() {
-    // Add your implementation here
+    std::vector<int> a, b;
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int num;
+        std::cin >> num;
+        a.push_back(num);
+    }
+    for (int i = 0; i < n; ++i) {
+        int num;
+        std::cin >> num;
+        b.push_back(num);
+    }
+
+    if (issame(a, b)) {
+        std::cout << "Vectors are the same\n";
+    } else {
+        std::cout << "Vectors are different\n";
+    }
+
+    std::vector<int> c;
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int num;
+        std::cin >> num;
+        c.push_back(num);
+    }
+
+    std::vector<int> result = incr_list(c);
+    for (int num : result) {
+        std::cout << num << " ";
+    }
+
     return 0;
 }
