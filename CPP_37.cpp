8936405e5ad6ebf2fe38d6cc7746ef9b7b223a96
beep boop <githubbot@vadim.me>
2024-07-15 17:05:00 +0200

    Solution 58 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/th9qyyvm Prompt:    29 | bool issame(vector<float> a,vector<float>b){       |                    ^~~~~ 0fd5d407-cdc0-4842-9e76-541c7e222fa9.cpp:29:29: error: ‘vector’ was not declared  in this scope    29 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 0fd5d407-cdc0-4842-9e76-541c7e222fa9.cpp:29:36: error: expected primary-expressi on before ‘float’    29 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from 0fd5d407-cdc0-4842-9e76-541c7e222fa9.cpp:28: 0fd5d407-cdc0-4842-9e76-541c7e222fa9.cpp: In function ‘int main()’: 0fd5d407-cdc0-4842-9e76-541c7e222fa9.cpp:38:20: error: ‘sort_even’ was not decla red in this scope    38 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 0fd5d407-cdc0-4842-9e76-541c7e222fa9.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -6,8 +6,6 @@ bool issame(vector<float> a, vector<float> b) {
     return a == b;
 }
 
-vector<float> sort_even(vector<float> l);
-
 vector<float> sort_even(vector<float> l) {
     vector<float> even_values;
     vector<float> result = l;
@@ -31,8 +29,4 @@ vector<float> sort_even(vector<float> l) {
     return result;
 }
 
-int main(){
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
-    
-    return 0;
-}
+assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
