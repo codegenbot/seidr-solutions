085c018c74f410e8bdb644b3890d3d13bc823ecb
beep boop <githubbot@vadim.me>
2024-07-16 00:34:20 +0200

    Solution 29 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/yy3yfvcx Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 74f4665e-7c3c-4d8a-a18a-9f18cba57d89.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 74f4665e-7c3c-4d8a-a18a-9f18cba57d89.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 74f4665e-7c3c-4d8a-a18a-9f18cba57d89.cpp:25: 74f4665e-7c3c-4d8a-a18a-9f18cba57d89.cpp: In function ‘int main()’: 74f4665e-7c3c-4d8a-a18a-9f18cba57d89.cpp:35:20: error: ‘separate_paren_groups’ w as not declared in this scope    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 74f4665e-7c3c-4d8a-a18a-9f18cba57d89.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,9 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
-
 vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
     string current_group;
@@ -33,18 +27,16 @@ bool issame(vector<string> v1, vector<string> v2) {
     if (v1.size() != v2.size()) {
         return false;
     }
-
     for (int i = 0; i < v1.size(); ++i) {
         if (v1[i] != v2[i]) {
             return false;
         }
     }
-
     return true;
 }
 
 int main() {
-    vector<string> expected_result = {"", "", ""};
+    vector<string> expected_result = {"", "", "", ""};
     vector<string> result = separate_paren_groups("( ) (( )) (( )( ))");
 
     if (issame(result, expected_result)) {
