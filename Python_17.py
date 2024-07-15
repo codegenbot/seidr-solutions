1baa56d73375e9563a2a34f30c0e9fa6a38b715c
beep boop <githubbot@vadim.me>
2024-07-15 12:49:10 +0200

    Solution 14 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hj5kthnw Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9f0bfce5-069c-49ab-a553-8bbe0be00 b21.py", line 5     music_string = ''.join(e for e in music_string if e in 'o' or e in '.|. '..' )                                                                                ^ SyntaxError: unterminated string literal (detected at line 5)

--- Python_17.py
+++ Python_17.py
@@ -1,7 +1,7 @@
 def parse_music(music_string: str) -> list:
     if not music_string:
         return []
-    music_string = ''.join(e for e in music_string if e in 'o\.|\\.\.\|')
+    music_string = ''.join(e for e in music_string if e in 'o.' or e in '|..- ')
     music_list = []
     i = 0
     while i < len(music_string):
