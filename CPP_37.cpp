a8eccd80325efe2ee36823e852984a4634bd1e14
beep boop <githubbot@vadim.me>
2024-07-15 19:44:41 +0200

    Solution 23 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/bygrg8t1 Prompt:    25 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 0dd85b2d-1bee-4b6d-935d-d1784c5fbf54.cpp:25:36: error: expected primary-expressi on before ‘float’    25 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 0dd85b2d-1bee-4b6d-935d-d1784c5fbf54.cpp:25:43: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 0dd85b2d-1bee-4b6d-935d-d1784c5fbf54.cpp:24: 0dd85b2d-1bee-4b6d-935d-d1784c5fbf54.cpp: In function ‘int main()’: 0dd85b2d-1bee-4b6d-935d-d1784c5fbf54.cpp:34:20: error: ‘sort_even’ was not decla red in this scope    34 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 0dd85b2d-1bee-4b6d-935d-d1784c5fbf54.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,21 +1,13 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<float> a, vector<float> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(std::vector<float> a, std::vector<float> b) {
+    return a == b;
 }
 
-vector<float> sort(vector<float> l) {
-    vector<float> even_indices;
-    vector<float> sorted_even_indices;
+std::vector<float> sort(std::vector<float> l) {
+    std::vector<float> even_indices;
+    std::vector<float> sorted_even_indices;
 
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
@@ -24,7 +16,7 @@ vector<float> sort(vector<float> l) {
         }
     }
 
-    sort(sorted_even_indices.begin(), sorted_even_indices.end());
+    std::sort(sorted_even_indices.begin(), sorted_even_indices.end());
 
     int sorted_index = 0;
     for (int i = 0; i < l.size(); i++) {
