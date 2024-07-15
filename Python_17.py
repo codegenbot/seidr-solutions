08f7669fe88fcdcdf4bd0a6f0dfef0c87ed2efd5
beep boop <githubbot@vadim.me>
2024-07-15 12:51:18 +0200

    Solution 21 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hj5kthnw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4c41f776-cc08-481f-86a1-37cb942fd b93.py", line 28, in <module>     check(parse_music)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4c41f776-cc08-481f-86a1-37cb942fd b93.py", line 25, in check     assert parse_music("o| .| o| .| o o| o o|") == [2, 1, 2, 1, 4, 2, 4, 2]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_17.py
+++ Python_17.py
@@ -12,11 +12,8 @@ def parse_music(music_string: str) -> list:
             else:
                 music_list.append(4)
                 i += 1
-        elif music_string[i:i+2] == 'o|':
-            music_list.append(2)
-            i += 2
         elif music_string[i:i+3] == '.|..|':
-            music_list.append([8, 4, 2])
+            music_list.extend([8, 4, 2])
             i += 3
         else:
             i += 1
