a98271479dcd0ef8184908b0f2817d9eb40b4d3f
beep boop <githubbot@vadim.me>
2024-07-15 13:07:47 +0200

    Solution 12 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/a08td4t2 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from cb29e0eb-89c3-4f96-9d16-61f9282d518f.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,5 +1,6 @@
-#include <string>
+#include <vector>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,67 +8,52 @@ class Solution {
 public:
     string numberToWords(int num) {
         if (num == 0) return "0";
-        
         string words = "";
-        
-        int temp = num;
-        if (temp / 3 > 0) {
-            if ((temp / 5 != 0 && temp % 15 != 0) || (temp / 3 != 0)) {
-                if (!words.empty()) {
-                    words += " ";
-                }
-                words += "Fizz";
-            } else {
-                words = "FizzBuzz";
-            }
-            temp %= 3;
+        if (num >= 1000000) {
+            int q = num / 1000000;
+            words += numberToWords(q) + " million ";
+            num %= 1000000;
         }
-        
-        if (temp / 5 > 0) {
-            if ((temp / 3 != 0 && temp % 15 != 0) || (temp / 5 != 0)) {
-                if (!words.empty()) {
-                    words += " ";
-                }
-                words += "Buzz";
-            } else {
-                words = "FizzBuzz";
-            }
-            temp %= 5;
+        if (num >= 1000) {
+            int q = num / 1000;
+            words += numberToWords(q) + " thousand ";
+            num %= 1000;
         }
-        
-        for(int i=1; i<=num; i++) {
-            if(i % 3 == 0 && i % 5 != 0) {
-                if(words.empty()) {
-                    words = "Fizz";
-                } else {
-                    words += " Fizz";
-                }
-            } 
-            else if(i % 5 == 0 && i % 3 != 0) {
-                if(words.empty()) {
-                    words = "Buzz";
-                } else {
-                    words += " Buzz";
-                }
-            } 
-            else if(i % 15 == 0) {
-                words = "FizzBuzz";
-            } 
-            else {
-                words += to_string(i);
-            }
-            words += " ";
+        if (num == 0) return words;
+        if (num < 20) {
+            static string v[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+            words = v[num] + " ";
+        } else if (num < 100) {
+            static string w[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+            words = w[num / 10] + " ";
+            num %= 10;
+            if (num > 0) words += numberToWords(num);
+        } else {
+            words = numberToWords(num / 100) + " hundred ";
+            num %= 100;
+            if (num > 0) words += numberToWords(num);
         }
-        
         return words;
+    }
+
+    string fizzBuzz(int n) {
+        for (int i = 1; i <= n; ++i) {
+            if (i % 3 == 0 && i % 5 == 0)
+                cout << "FizzBuzz" << endl;
+            else if (i % 3 == 0)
+                cout << "Fizz" << endl;
+            else if (i % 5 == 0)
+                cout << "Buzz" << endl;
+            else
+                cout << to_string(i) << endl;
+        }
     }
 };
 
 int main() {
-    Solution solution;
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    cout << solution.numberToWords(num) << endl;
+    Solution s;
+    int n;
+    cin >> n;
+    s.fizzBuzz(n);
     return 0;
 }
