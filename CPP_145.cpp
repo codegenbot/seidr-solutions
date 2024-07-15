38cba212d1482469386efb88ca286580b70ec364
beep boop <githubbot@vadim.me>
2024-07-15 15:39:45 +0200

    Solution 19 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/hnekootv Prompt:     4 | d4c0b25a-7ae1-4a50-b817-f17d66b7cb75.cpp:48:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    48 | int main(){       |         ^~ d4c0b25a-7ae1-4a50-b817-f17d66b7cb75.cpp:48:9: note: remove parentheses to defau lt-initialize a variable    48 | int main(){       |         ^~       |         -- d4c0b25a-7ae1-4a50-b817-f17d66b7cb75.cpp:48:9: note: or replace parentheses with  braces to value-initialize a variable d4c0b25a-7ae1-4a50-b817-f17d66b7cb75.cpp:48:11: error: a function-definition is not allowed here before ‘{’ token    48 | int main(){       |           ^ d4c0b25a-7ae1-4a50-b817-f17d66b7cb75.cpp:51:2: error: expected ‘}’ at end of inp ut    51 | }       |  ^ d4c0b25a-7ae1-4a50-b817-f17d66b7cb75.cpp:33:12: note: to match this ‘{’    33 | int main() {       |            ^

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -30,7 +30,8 @@ vector<int> order_by_points(vector<int> nums) {
     return nums;
 }
 
-int main() { }
-
-assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6});
-return 0;
+int main() {
+    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6});
+    
+    return 0;
+}
