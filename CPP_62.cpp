a12084c4ee2e7a0a4dcee174ea0463d5e4eb3603
beep boop <githubbot@vadim.me>
2024-07-15 22:47:42 +0200

    Solution 1 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/mgrivjqv Prompt:       |                    ^~~~~ 7fd80a7e-a286-4b2e-b893-9974038d44e2.cpp:10:29: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 7fd80a7e-a286-4b2e-b893-9974038d44e2.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 7fd80a7e-a286-4b2e-b893-9974038d44e2.cpp:10:43: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 7fd80a7e-a286-4b2e-b893-9974038d44e2.cpp:9: 7fd80a7e-a286-4b2e-b893-9974038d44e2.cpp: In function ‘int main()’: 7fd80a7e-a286-4b2e-b893-9974038d44e2.cpp:19:20: error: ‘derivative’ was not decl ared in this scope    19 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~ 7fd80a7e-a286-4b2e-b893-9974038d44e2.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -1,7 +1,16 @@
+#include <vector>
+
+bool issame(vector<float> a, vector<float> b){
+}
+
 vector<float> derivative(vector<float> xs){
     vector<float> result;
     for(int i = 1; i < xs.size(); i++){
         result.push_back(xs[i] * i);
     }
     return result;
+}
+
+int main(){
+    assert(issame(derivative({1}), {}));
 }
