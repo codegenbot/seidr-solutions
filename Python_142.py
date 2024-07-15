9e50a42b1cc8ef9434b8ed4808e0ff5082e4e6b6
beep boop <githubbot@vadim.me>
2024-07-15 11:30:32 +0200

    Solution 14 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0h1ogjge Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/34bd5243-8571-45a3-aed0-47e1b75c8 2f7.py", line 38, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/34bd5243-8571-45a3-aed0-47e1b75c8 2f7.py", line 8, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,4 +1,11 @@
-```
 def sum_squares(lst):
-    total = sum(num ** len(str(num)) for num in lst)
+    total = 0
+    for i in lst:
+        if isinstance(i, int):
+            if i % 3 == 0:
+                total += i**2
+            else:
+                total += i**3
+        else:
+            return "Error: Input must be an integer."
     return total
