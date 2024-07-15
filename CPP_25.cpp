aadf72fa0bcde794af7411aa22a8b4e7c0f26265
beep boop <githubbot@vadim.me>
2024-07-15 13:46:18 +0200

    Solution 47 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/rv9h2tpo Prompt: In file included from /usr/include/c++/11/vector:67,                  from f2b3b806-20e8-4868-98bb-66a6cb4a830a.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from f2b3b806-20e8-4868-98bb-66a6cb4a830a.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ f2b3b806-20e8-4868-98bb-66a6cb4a830a.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f2b3b806-20e8-4868-98bb-66a6cb4a830a.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f2b3b806-20e8-4868-98bb-66a6cb4a830a.cpp:22: f2b3b806-20e8-4868-98bb-66a6cb4a830a.cpp: In function ‘int main()’: f2b3b806-20e8-4868-98bb-66a6cb4a830a.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -19,10 +19,10 @@ namespace std {
     bool is_same(std::vector<int> a, std::vector<int> b) {
         return a == b;
     }
-} 
+}
 
 int main() {
-    assert (is_same(factorize(3 * 2 * 3) , {2, 3, 3}));
-    
+    assert(is_same(factorize(3 * 2 * 3), {2, 3, 3}));
+
     return 0;
 }
