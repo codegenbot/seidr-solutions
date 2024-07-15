8688ddcea3965a3e42e4d2620ab727a14a7e6b38
beep boop <githubbot@vadim.me>
2024-07-15 13:14:05 +0200

    Solution 2 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/25w5xifw Prompt:    11 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 7de0b45d-c431-4d69-a17c-ed7628fb83fa.cpp:11:36: error: expected primary-expressi on before ‘float’    11 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 7de0b45d-c431-4d69-a17c-ed7628fb83fa.cpp:11:43: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 7de0b45d-c431-4d69-a17c-ed7628fb83fa.cpp:10: 7de0b45d-c431-4d69-a17c-ed7628fb83fa.cpp: In function ‘int main()’: 7de0b45d-c431-4d69-a17c-ed7628fb83fa.cpp:20:20: error: ‘rescale_to_unit’ was not  declared in this scope    20 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 7de0b45d-c431-4d69-a17c-ed7628fb83fa.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,9 +1,13 @@
-float min_val = *min_element(numbers.begin(), numbers.end());
-    float max_val = *max_element(numbers.begin(), numbers.end());
+bool issame(vector<float> a, vector<float> b) {
+    return a == b;
+}
+
+vector<float> rescale_to_unit(vector<float> numbers) {
+    float min_num = *min_element(numbers.begin(), numbers.end());
+    float max_num = *max_element(numbers.begin(), numbers.end());
     vector<float> result;
     for (float num : numbers) {
-        float rescaled = (num - min_val) / (max_val - min_val);
-        result.push_back(rescaled);
+        result.push_back((num - min_num) / (max_num - min_num));
     }
     return result;
 }
