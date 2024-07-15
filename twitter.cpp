85f741023aca570fe9f077a915db13db9fb94b96
beep boop <githubbot@vadim.me>
2024-07-16 00:01:38 +0200

    Solution 24 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/zyfbh7lw Prompt: it must return Your tweet has 123 characters for input J,095FAN{Rn6c\?vGAiD`v<g:87%$RN)1!tqIg:(2J";$")|n0t9,W5<1c@<\RrOTo<.=(CqHH%8KR37_)8N8<@{GJ"c0!%3o]dP~K+/x[<mIn-I{9}J?mI%&7%, but it returns Enter a tweet: Your tweet has 123 characters.

--- twitter.cpp
+++ twitter.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
 #include <string>
-#include <regex>
 
 using namespace std;
 
 string validateTweet(string tweet) {
     if(tweet.empty()) return "You didn't type anything";
-    regex special_chars("[^a-zA-Z0-9\s]");
-    string clean_tweet;
-    for(auto c : tweet) {
-        if(c != char('\t') && !std::regex_match(&c, special_chars)) 
-            clean_tweet += c;
-    }
-    int len = clean_tweet.length();
-    if(len > 140) return "Too many characters";
-    return "Your tweet has " + to_string(len) + " characters";
+    if(tweet.length() > 140) return "Too many characters";
+    return "Your tweet has " + to_string(tweet.capacity()) + " characters";
 }
 
 int main() {
