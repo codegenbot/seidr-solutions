955527d34f179f5187cd71ae567d894a2c4560e0
beep boop <githubbot@vadim.me>
2024-07-15 20:37:43 +0200

    Solution 57 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/r9npe7x9 Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -2,59 +2,56 @@
 #include <cmath>
 using namespace std;
 
+int main() {
+    vector<int> input;
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        int val;
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> val;
+        input.push_back(val);
+    }
+    
+    vector<vector<int>> result = cutVector(input);
+    
+    for(int i = 0; i < 2; i++) {
+        cout << "Subvector " << i + 1 << ":";
+        for(auto num : result[i])
+            cout << " " << num;
+        cout << endl;
+    }
+    
+    return 0;
+}
+
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
+    
+    int minDiff = INT_MAX;
+    int pos = -1;
     
     for(int i = 0; i < n; i++) {
         if(i == 0 || i == n-1)
             res[0].push_back(v[i]);
         else {
-            int leftSum = 0, rightSum = 0;
-            for(int j = 0; j <= i; j++)
-                leftSum += v[j];
-            for(int j = i; j < n; j++)
-                rightSum += v[j];
-            
-            if(abs(leftSum - rightSum) <= abs(v[0] - v[i])) {
-                res[0].clear();
-                for(int j = 0; j <= i; j++)
-                    res[0].push_back(v[j]);
-                break;
-            }
-            else {
-                res[0].clear();
-                for(int j = i; j < n; j++)
-                    res[0].push_back(v[j]);
-                break;
+            int diff = abs(v[0] - v[i]);
+            if(diff <= minDiff) {
+                minDiff = diff;
+                pos = i;
             }
         }
     }
     
-    return res;
-}
-
-int main() {
-    vector<int> input;
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
     for(int i = 0; i < n; i++) {
-        int num;
-        cout << "Enter element " << (i+1) << ": ";
-        cin >> num;
-        input.push_back(num);
+        if(i == 0 || i == n-1)
+            res[1].push_back(v[i]);
+        else if(i != pos) 
+            res[1].push_back(v[i]);
     }
     
-    vector<vector<int>> result = cutVector(input);
-    cout << "Left subvector: ";
-    for(int i = 0; i < result[0].size(); i++)
-        cout << result[0][i] << " ";
-    cout << endl;
-    cout << "Right subvector: ";
-    for(int i = 0; i < result[1].size(); i++)
-        cout << result[1][i] << " ";
-    cout << endl;
-    
-    return 0;
+    return res;
 }
