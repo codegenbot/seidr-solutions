3478bdb4ef028e1d7780eca02bffd53eb3e98e99
beep boop <githubbot@vadim.me>
2024-07-15 16:14:18 +0200

    Solution 9 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/0t1zsb6j Prompt:       |                              ^~~~~~ d22bbf0a-2f2c-46fd-bd24-cbda34c2669c.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d22bbf0a-2f2c-46fd-bd24-cbda34c2669c.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d22bbf0a-2f2c-46fd-bd24-cbda34c2669c.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d22bbf0a-2f2c-46fd-bd24-cbda34c2669c.cpp:9: d22bbf0a-2f2c-46fd-bd24-cbda34c2669c.cpp: In function ‘int main()’: d22bbf0a-2f2c-46fd-bd24-cbda34c2669c.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ d22bbf0a-2f2c-46fd-bd24-cbda34c2669c.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cassert>
-
-using namespace std;
-
-vector<string> all_prefixes(string str){
-    vector<string> result;
-    for (int i = 1; i <= str.size(); ++i) {
-        result.push_back(str.substr(0, i));
-    }
-    return result;
-}
-
-bool issame(vector<string> a, vector<string> b){
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i=0; i<a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int main() {
-    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
-    
-    return 0;
+#include <iostream>  
+#include <vector>  
+#include <string>  
+#include <cassert>  
+  
+using namespace std;  
+  
+vector<string> all_prefixes(string str){  
+    vector<string> result;  
+    for (int i = 1; i <= str.size(); ++i) {  
+        result.push_back(str.substr(0, i));  
+    }  
+    return result;  
+}  
+  
+bool issame(vector<string> a, vector<string> b){  
+    if(a.size() != b.size()) {  
+        return false;  
+    }  
+    for(int i=0; i<a.size(); i++) {  
+        if(a[i] != b[i]) {  
+            return false;  
+        }  
+    }  
+    return true;  
+}  
+  
+int main() {  
+    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));  
+      
+    return 0;  
 }
