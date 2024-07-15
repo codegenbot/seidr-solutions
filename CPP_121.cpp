b0d8c8c13f5a1d73c56167f94e62e960b739ea09
beep boop <githubbot@vadim.me>
2024-07-15 11:34:42 +0200

    Solution 26 of CPP/121, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rzvharwr Prompt: acc1dddf-df8e-4d22-b0bd-c3b1e8af9550.cpp:1:15: error: ‘vector’ was not declared in this scope     1 | int solutions(vector<int> lst) {       |               ^~~~~~ acc1dddf-df8e-4d22-b0bd-c3b1e8af9550.cpp:1:22: error: expected primary-expressio n before ‘int’     1 | int solutions(vector<int> lst) {       |                      ^~~ In file included from acc1dddf-df8e-4d22-b0bd-c3b1e8af9550.cpp:11: acc1dddf-df8e-4d22-b0bd-c3b1e8af9550.cpp: In function ‘int main()’: acc1dddf-df8e-4d22-b0bd-c3b1e8af9550.cpp:13:22: error: ‘solutions’ cannot be use d as a function    13 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,8 +1,13 @@
+#include <vector>
+using namespace std;
+
 int solutions(vector<int> lst) {
     int sum = 0;
-    for (int i = 1; i < lst.size(); i++) {
-        if (lst[i] % 2 != 0) {
-            sum += lst[i];
+    for (int i = 1; i <= lst.size(); i++) {
+        if (i % 2 != 0) {
+            sum += i;
+        } else if(i<lst.size() && i%2==0){
+            sum += lst[i-1];
         }
     }
     return sum;
