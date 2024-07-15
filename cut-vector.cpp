4a4a1229a87fc4cd62622976e7db1c697aa3eda1
beep boop <githubbot@vadim.me>
2024-07-15 20:25:32 +0200

    Solution 56 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/r9npe7x9 Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -5,52 +5,56 @@ using namespace std;
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
-
-    for(int i = 0; i <= n/2; i++) {
-        if(abs(v[i] - v[0]) <= abs(v[n-1] - v[i])) {
-            res[0].clear();
-            for(int j = 0; j <= i; j++)
-                res[0].push_back(v[j]);
-            break;
-        }
+    
+    for(int i = 0; i < n; i++) {
+        if(i == 0 || i == n-1)
+            res[0].push_back(v[i]);
         else {
-            res[0].clear();
+            int leftSum = 0, rightSum = 0;
+            for(int j = 0; j <= i; j++)
+                leftSum += v[j];
             for(int j = i; j < n; j++)
-                res[0].push_back(v[j]);
-            break;
-        }
-    }
-
-    if(res[0].size() != n) {
-        for(int i = 0; i <= n/2; i++) {
-            if(abs(v[i] - v[0]) <= abs(v[n-1] - v[i])) {
-                res[1].clear();
-                for(int j = i; j < n; j++)
-                    res[1].push_back(v[j]);
+                rightSum += v[j];
+            
+            if(abs(leftSum - rightSum) <= abs(v[0] - v[i])) {
+                res[0].clear();
+                for(int j = 0; j <= i; j++)
+                    res[0].push_back(v[j]);
                 break;
             }
             else {
-                res[1].clear();
-                for(int j = 0; j <= i; j++)
-                    res[1].push_back(v[j]);
+                res[0].clear();
+                for(int j = i; j < n; j++)
+                    res[0].push_back(v[j]);
                 break;
             }
         }
     }
-
+    
     return res;
 }
 
 int main() {
-    vector<int> v = {1, 2, 3, 4, 5}; 
-    auto res = cutVector(v);
-    
-    for(auto vec : res) {
-        for(int i : vec) {
-            cout << i << " ";
-        }
-        cout << endl;
+    vector<int> input;
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int num;
+        cout << "Enter element " << (i+1) << ": ";
+        cin >> num;
+        input.push_back(num);
     }
+    
+    vector<vector<int>> result = cutVector(input);
+    cout << "Left subvector: ";
+    for(int i = 0; i < result[0].size(); i++)
+        cout << result[0][i] << " ";
+    cout << endl;
+    cout << "Right subvector: ";
+    for(int i = 0; i < result[1].size(); i++)
+        cout << result[1][i] << " ";
+    cout << endl;
     
     return 0;
 }
