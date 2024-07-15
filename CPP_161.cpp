88ab02dded85ce375d91c114e808824c0d050ed8
beep boop <githubbot@vadim.me>
2024-07-15 15:48:23 +0200

    Solution 4 of CPP/161, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/f9j7n3y7 Prompt: ee985cb2-9062-4025-8918-2f491c6ff972.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string solve(string s){       | ^~~~~~ In file included from ee985cb2-9062-4025-8918-2f491c6ff972.cpp:15: ee985cb2-9062-4025-8918-2f491c6ff972.cpp: In function ‘int main()’: ee985cb2-9062-4025-8918-2f491c6ff972.cpp:17:13: error: ‘solve’ was not declared in this scope    17 |     assert (solve("#ccc") == "#CCC");       |             ^~~~~

--- CPP_161.cpp
+++ CPP_161.cpp
@@ -1,6 +1,9 @@
-#include <string>
+#include <iostream>
+#include <algorithm>
 
-std::string solve(std::string s){
+using namespace std;
+
+string solve(string s){
     bool hasLetter = false;
     for(char &c : s){
         if(isalpha(c)){
@@ -15,5 +18,6 @@ std::string solve(std::string s){
 }
 
 int main() {
-    assert(solve("#ccc") == "#CCC");
+    assert (solve("#ccc") == "#CCC");
+    return 0;
 }
