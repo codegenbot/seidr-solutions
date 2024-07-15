d45fde39cd39435f0df2aebc0e104c0c979baa09
beep boop <githubbot@vadim.me>
2024-07-15 18:26:40 +0200

    Solution 47 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i1v1lsd5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c69c4bf6-d46e-4d00-99af-29918d281 e3d.py", line 13, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c69c4bf6-d46e-4d00-99af-29918d281 e3d.py", line 8, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,11 +1,6 @@
 def sort_third(l: list):
-    return sorted(
-        [
-            sorted(
-                l[i : i + 3], key=lambda x: x[2]
-            )  # Sort sublist based on the third element
-            for i in range(0, len(l), 3)
-            if i + 2 < len(l)
-        ],
-        key=lambda x: x[2],
-    )  # Sort the main list based on the third element of each sublist
+    return sorted([
+        sorted(l[i:i+3], key=lambda x: x[2])  
+        for i in range(0, len(l), 3)
+        if i + 2 < len(l)
+    ], key=lambda x: x[2])  
