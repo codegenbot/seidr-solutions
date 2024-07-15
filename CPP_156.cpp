c10b80b8813877cd14c302e3000d3ce7dff66cf1
beep boop <githubbot@vadim.me>
2024-07-15 18:43:39 +0200

    Solution 20 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/w2ctav9j Prompt: beb7a442-a320-46dd-a9b0-fdac7346024a.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_romank(int number){       | ^~~~~~ In file included from beb7a442-a320-46dd-a9b0-fdac7346024a.cpp:17: beb7a442-a320-46dd-a9b0-fdac7346024a.cpp: In function ‘int main()’: beb7a442-a320-46dd-a9b0-fdac7346024a.cpp:19:13: error: ‘int_to_mini_romank’ was not declared in this scope    19 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,28 +1,24 @@
-
-#include <iostream>
-#include <vector>
 #include <string>
-using namespace std;
+#include <vector>
 
-string int_to_mini_romank(int number) {
-    string roman = "";
-    vector<pair<int, string>> roman_numerals = {
-        {1000, "m"}, {900, "cm"}, {500, "d"}, {400, "cd"},
-        {100, "c"}, {90, "xc"}, {50, "l"}, {40, "xl"},
-        {10, "x"}, {9, "ix"}, {5, "v"}, {4, "iv"}, {1, "i"}
-    };
-
-    for (const auto& numeral : roman_numerals) {
-        while (number >= numeral.first) {
-            roman += numeral.second;
-            number -= numeral.first;
-        }
-    }
-
-    return roman;
-}
+string int_to_mini_romank(int number);
 
 int main() {
-    assert(int_to_mini_romank(1000) == "m");
-    return 0;
+    // main function body can be added if needed
+}
+
+string int_to_mini_romank(int number){
+    vector<string> roman = {"i", "iv", "v", "ix", "x", "xl", "l", "xc", "c", "cd", "d", "cm", "m"};
+    vector<int> values = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    string result = "";
+    int i = 12;
+    while(number > 0){
+        int div = number / values[i];
+        number %= values[i];
+        while(div--){
+            result += roman[i];
+        }
+        i--;
+    }
+    return result;
 }
