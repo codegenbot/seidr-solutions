7944f629c478305697589beddb04a9c067dd6b7e
beep boop <githubbot@vadim.me>
2024-07-15 13:34:48 +0200

    Solution 44 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rip4z73m Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c07de6a5-590b-4371-bf30-1a9d2460c ff6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -1,10 +1,7 @@
 ```
 def match_parens(lst):
-    stack = []
     for s in lst:
-        if not s:  
-            return 'Yes'
-        parens_count = sum(1 for c in s if c in ['(', ')'])
+        stack = []
         for c in s:
             if c == '(':
                 stack.append(c)
@@ -12,7 +9,6 @@ def match_parens(lst):
                 if len(stack) == 0:
                     return 'No'
                 stack.pop()
-    if len(stack) > 0:
-        return 'No'
-    else:
-        return 'Yes'
+        if len(stack) > 0:
+            return 'No'
+    return 'Yes'
