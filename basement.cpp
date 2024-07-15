443c7fdc005694cc53fe79d15e805b8bb08d916e
beep boop <githubbot@vadim.me>
2024-07-15 12:36:06 +0200

    Solution 27 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/y6dy1rw4 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 104b2def-c1f4-44b6-9525-3d59ffb974f7.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- basement.cpp
+++ basement.cpp
@@ -1,13 +1,11 @@
-```
 #include <vector>
 #include <initializer_list>
-
 using namespace std;
 
 int basement(vector<int> v) {
     int sum = 0;
     int i = 0;
-    for (auto num : v) {
+    for (int num : v) {
         sum += num;
         if (sum < 0)
             return i;
