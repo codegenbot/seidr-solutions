ccbe84818f12401765cc6e418479ef44b58423f6
beep boop <githubbot@vadim.me>
2024-07-15 18:52:39 +0200

    Solution 28 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/s1k7t141 Prompt:       |                    ^~~ 3bb3ecf7-7aa0-4525-a53c-bee5b8d0493f.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3bb3ecf7-7aa0-4525-a53c-bee5b8d0493f.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3bb3ecf7-7aa0-4525-a53c-bee5b8d0493f.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3bb3ecf7-7aa0-4525-a53c-bee5b8d0493f.cpp:10: 3bb3ecf7-7aa0-4525-a53c-bee5b8d0493f.cpp: In function ‘int main()’: 3bb3ecf7-7aa0-4525-a53c-bee5b8d0493f.cpp:20:20: error: ‘generate_integers’ was n ot declared in this scope    20 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ 3bb3ecf7-7aa0-4525-a53c-bee5b8d0493f.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,5 +1,9 @@
 #include <vector>
 
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
 std::vector<int> generate_integers(int a, int b) {
     std::vector<int> result;
     for (int i = a; i <= b; ++i) {
@@ -10,11 +14,9 @@ std::vector<int> generate_integers(int a, int b) {
     return result;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
 int main() {
-    assert (issame(generate_integers(17, 89), {}));
+    // Function calls and assertions
+    assert(issame(generate_integers(17, 89), {}));
+
     return 0;
 }
