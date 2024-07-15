d26e5c4a8ac0f621f2846853d55825949ec6284d
beep boop <githubbot@vadim.me>
2024-07-15 18:21:06 +0200

    Solution 12 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/f5o10j3a Prompt:    17 | } else if (a.type() == typeid(string) && b.type() == typeid(string)) {       |   ^~~~ 6d9c3f62-04a4-452b-9090-d83fd4483a3e.cpp:25:3: error: expected unqualified-id be fore ‘else’    25 | } else {       |   ^~~~ In file included from 6d9c3f62-04a4-452b-9090-d83fd4483a3e.cpp:29: 6d9c3f62-04a4-452b-9090-d83fd4483a3e.cpp: In function ‘int main()’: 6d9c3f62-04a4-452b-9090-d83fd4483a3e.cpp:31:13: error: ‘boost’ has not been decl ared    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 6d9c3f62-04a4-452b-9090-d83fd4483a3e.cpp:31:29: error: ‘string’ was not declared  in this scope    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 6d9c3f62-04a4-452b-9090-d83fd4483a3e.cpp:31:37: error: ‘compare_one’ was not dec lared in this scope    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,13 +1,20 @@
 #include <boost/any.hpp>
+#include <string>
 
-boost::any compare_one(boost::any a, boost::any b) {
-    if(a.type() == typeid(int) && b.type() == typeid(int)){
-        return boost::any_cast<int>(a) > boost::any_cast<int>(b) ? a : b;
-    } else if(a.type() == typeid(float) && b.type() == typeid(float)){
-        return boost::any_cast<float>(a) > boost::any_cast<float>(b) ? a : b;
-    } else if(a.type() == typeid(std::string) && b.type() == typeid(std::string)){
-        return stof(boost::any_cast<std::string>(a)) > stof(boost::any_cast<std::string>(b)) ? a : b;
-    } else {
-        return "None";
+template<typename T>
+const boost::any compare_one(const boost::any& a, const boost::any& b) {
+    if (a.type() == typeid(T) && b.type() == typeid(T)) {
+        if (boost::any_cast<T>(a) > boost::any_cast<T>(b)) {
+            return a;
+        } else if (boost::any_cast<T>(a) < boost::any_cast<T>(b)) {
+            return b;
+        } else {
+            return "None";
+        }
     }
+    return "None";
+}
+
+const boost::any compare(const boost::any& a, const boost::any& b) {
+    return compare_one<int>(a, b), compare_one<float>(a, b), compare_one<std::string>(a, b);
 }
