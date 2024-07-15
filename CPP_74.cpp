6b4dcf443eb88eb8ade0d2fa221ce147c9fa5f43
beep boop <githubbot@vadim.me>
2024-07-15 20:55:31 +0200

    Solution 38 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/xwfmyeei Prompt: his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f5f983b3-e17d-402b-af79-5a8d016687ca.cpp:21:45: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f5f983b3-e17d-402b-af79-5a8d016687ca.cpp:20: f5f983b3-e17d-402b-af79-5a8d016687ca.cpp: In function ‘int main()’: f5f983b3-e17d-402b-af79-5a8d016687ca.cpp:30:20: error: ‘total_match’ was not dec lared in this scope    30 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ f5f983b3-e17d-402b-af79-5a8d016687ca.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ f5f983b3-e17d-402b-af79-5a8d016687ca.cpp: In function ‘int countTotalChars(int)’ : f5f983b3-e17d-402b-af79-5a8d016687ca.cpp:7:1: warning: control reaches end of no n-void function [-Wreturn-type]     7 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -12,18 +12,22 @@ int countTotalChars(const vector<string>& lst) {
 
 vector<string> total_match(vector<string> lst1, vector<string> lst2);
 
-int main() {
-    // Your main function code here
-    return 0;
+bool issame(const string a, const string b) {
+    return a == b;
 }
 
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     int total_chars_lst1 = countTotalChars(lst1);
     int total_chars_lst2 = countTotalChars(lst2);
-    
+
     if (total_chars_lst1 < total_chars_lst2) {
         return lst1;
     } else {
         return lst2;
     }
+}
+
+int main() {
+    // Test your functions here
+    return 0;
 }
