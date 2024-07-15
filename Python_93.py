61e815c8726d290c76bc8b4409b569693962ae26
beep boop <githubbot@vadim.me>
2024-07-15 13:22:47 +0200

    Solution 82 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/npq5hi6i Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cfb51f34-fa81-4895-b6ce-856835eb6 b20.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -5,15 +5,15 @@ def encode(message):
         if char.isalpha():
             if char.lower() in 'aeiou':
                 if char.lower() == 'a':
-                    result += 'y'
+                    result += 'c'
                 elif char.lower() == 'e':
-                    result += 'h'
+                    result += 'g'
                 elif char.lower() == 'i':
                     result += 'k'
                 elif char.lower() == 'o':
                     result += 'q'
                 elif char.lower() == 'u':
-                    result += 'x'
+                    result += 'y'
             else:
                 if char.isupper():
                     result += chr((ord(char) - 65 + 3) % 26 + 65)
