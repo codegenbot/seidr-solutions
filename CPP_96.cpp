1a018d704e2fc9c1218c09aaa169d1d1cb0d2743
beep boop <githubbot@vadim.me>
2024-07-15 14:51:51 +0200

    Solution 13 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/06y12ino Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 4f552d2f-014c-4362-9543-1231ad4c1ca9.cpp:6:27: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4f552d2f-014c-4362-9543-1231ad4c1ca9.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 4f552d2f-014c-4362-9543-1231ad4c1ca9.cpp:5: 4f552d2f-014c-4362-9543-1231ad4c1ca9.cpp: In function ‘int main()’: 4f552d2f-014c-4362-9543-1231ad4c1ca9.cpp:15:20: error: ‘count_up_to’ was not dec lared in this scope    15 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 4f552d2f-014c-4362-9543-1231ad4c1ca9.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -4,26 +4,17 @@ std::vector<int> count_up_to(int n) {
     std::vector<int> result;
     int num = 2;
     while (result.size() < n) {
-        bool is_prime = true;
-        for (int i = 2; i < num; ++i) {
+        bool isPrime = true;
+        for (int i = 2; i*i <= num; ++i) {
             if (num % i == 0) {
-                is_prime = false;
+                isPrime = false;
                 break;
             }
         }
-        if (is_prime) {
+        if (isPrime) {
             result.push_back(num);
         }
-        ++num;
+        num++;
     }
     return result;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-int main() {
-    assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-    return 0;
 }
