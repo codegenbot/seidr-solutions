59caae483009774ad4789c520ac5e6796a5ec365
beep boop <githubbot@vadim.me>
2024-07-15 23:13:30 +0200

    Solution 86 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/jl6zpipe Prompt: In file included from 2c234d5b-de23-45a9-8d87-77f43dfd6655.cpp:24: 2c234d5b-de23-45a9-8d87-77f43dfd6655.cpp: In function ‘int main()’: 2c234d5b-de23-45a9-8d87-77f43dfd6655.cpp:34:20: error: ‘common’ was not declared  in this scope    34 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -7,15 +7,18 @@ using namespace std;
 class CustomVectorFunctions {
 public:
     static vector<int> common(const vector<int>& a, const vector<int>& b);
-    static bool issame(const vector<int>& a, const vector<int>& b);
-};
 
-vector<int> CustomVectorFunctions::common(const vector<int>& a, const vector<int>& b) {
-    vector<int> result;
-    for (int num : a) {
-        if (find(b.begin(), b.end(), num) != b.end()) {
-            result.push_back(num);
+    static vector<int> common(const vector<int>& a, const vector<int>& b) {
+        vector<int> result;
+        for (int num : a) {
+            if (find(b.begin(), b.end(), num) != b.end()) {
+                result.push_back(num);
+            }
         }
+        return result;
     }
-    return result;
-}
+
+    static bool issame(const vector<int>& a, const vector<int>& b) {
+        return a == b;
+    }
+};
