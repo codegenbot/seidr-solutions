664f49feffc40e6396868e83ddd80d229d0880f4
beep boop <githubbot@vadim.me>
2024-07-15 14:18:19 +0200

    Solution 76 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rip4z73m Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/894dc737-a345-40a6-9581-5d935a86a 064.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -1,9 +1,10 @@
 def match_parens(lst):
     for s in lst:
-        if not s:  
-            continue
         open_count = s.count('(')
         close_count = s.count(')')
         if open_count != close_count:
             return "No"
     return "Yes"
+
+print(match_parens(["(hello)","(world)"]))  
+print(match_parens(["(hello)","(world",)]))
