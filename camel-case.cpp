ff98b26b40c28f28ee6ca8fb0d412f23fc0f46fc
beep boop <githubbot@vadim.me>
2024-07-15 12:32:47 +0200

    Solution 6 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/7xl679a6 Prompt:  in this scope    16 |                 result += toupper(c);       |                 ^~~~~~ c08e11c5-1367-4fc8-92b4-d42d936b9d39.cpp:16:27: error: ‘toupper’ was not declare d in this scope    16 |                 result += toupper(c);       |                           ^~~~~~~ c08e11c5-1367-4fc8-92b4-d42d936b9d39.cpp:18:17: error: ‘result’ was not declared  in this scope    18 |                 result += c;       |                 ^~~~~~ c08e11c5-1367-4fc8-92b4-d42d936b9d39.cpp:24:5: error: ‘cout’ was not declared in  this scope    24 |     cout << result << endl;       |     ^~~~ c08e11c5-1367-4fc8-92b4-d42d936b9d39.cpp:24:13: error: ‘result’ was not declared  in this scope    24 |     cout << result << endl;       |             ^~~~~~ c08e11c5-1367-4fc8-92b4-d42d936b9d39.cpp:24:23: error: ‘endl’ was not declared i n this scope    24 |     cout << result << endl;       |                       ^~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
-#include <string>
-using namespace std;
+#include <cctype>
 
 int main() {
     string input;
     getline(cin, input);
     
-    bool capitalize = true;
-    for (char &c : input) {
+    string result = "";
+    bool capitalize = false;
+    
+    for (char c : input) {
         if (c == '-') {
             capitalize = true;
-        } else if (capitalize) {
-            cout << (char)toupper(c);
+        } else if (c == ' ') {
             capitalize = false;
+            result += c;
         } else {
-            cout << c;
+            if (capitalize) {
+                result += toupper(c);
+            } else {
+                result += c;
+            }
+            capitalize = false;
         }
     }
+    
+    cout << result << endl;
     
     return 0;
 }
