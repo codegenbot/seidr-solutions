1fb71b74572249b3406d841f1d1b6aae37e6d476
beep boop <githubbot@vadim.me>
2024-07-15 22:51:08 +0200

    Solution 5 of CPP/67, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/3to8y5hh Prompt: 9295cd7b-750b-440f-951b-17bcae198e1f.cpp:2:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?     1 | #include <cstdio>   +++ |+#include <string>     2 | 9295cd7b-750b-440f-951b-17bcae198e1f.cpp: In function ‘int fruit_distribution(co nst int&, int)’: 9295cd7b-750b-440f-951b-17bcae198e1f.cpp:5:14: error: request for member ‘c_str’  in ‘s’, which is of non-class type ‘const int’     5 |     sscanf(s.c_str(), "%d apples and %d oranges", &apples, &oranges);       |              ^~~~~ In file included from 9295cd7b-750b-440f-951b-17bcae198e1f.cpp:10: 9295cd7b-750b-440f-951b-17bcae198e1f.cpp: In function ‘int main()’: 9295cd7b-750b-440f-951b-17bcae198e1f.cpp:12:32: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    12 |     assert (fruit_distribution("1 apples and 100 oranges",120) == 19);       |                                ^~~~~~~~~~~~~~~~~~~~~~~~~~       |                                |       |                                const char* 9295cd7b-750b-440f-951b-17bcae198e1f.cpp:3:43: note:   initializing argument 1 o f ‘int fruit_distribution(const int&, int)’     3 | int fruit_distribution(const std::string& s, int n) {       |                        ~~~~~~~~~~~~~~~~~~~^

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,5 +1,8 @@
-int apples, oranges, mango;
-    sscanf(s.c_str(), "%d apples and %d oranges", &apples, &oranges);
+#include <string>
+
+int fruit_distribution(const char* s, int n) {
+    int apples, oranges, mango;
+    sscanf(s, "%d apples and %d oranges", &apples, &oranges);
     mango = n - apples - oranges;
     return mango;
 }
