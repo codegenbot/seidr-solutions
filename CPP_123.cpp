18bea45362d670dcabe6303bdd0e66350ccbb3d4
beep boop <githubbot@vadim.me>
2024-07-15 17:59:21 +0200

    Solution 9 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/fone7umq Prompt:       |                    ^~~ 89228873-7090-4cdb-96b5-5a64582b7046.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 89228873-7090-4cdb-96b5-5a64582b7046.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 89228873-7090-4cdb-96b5-5a64582b7046.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 89228873-7090-4cdb-96b5-5a64582b7046.cpp:21: 89228873-7090-4cdb-96b5-5a64582b7046.cpp: In function ‘int main()’: 89228873-7090-4cdb-96b5-5a64582b7046.cpp:31:20: error: ‘get_odd_collatz’ was not  declared in this scope    31 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 89228873-7090-4cdb-96b5-5a64582b7046.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -15,12 +15,14 @@ std::vector<int> get_odd_collatz(int n) {
         }
     }
     result.push_back(1);
+    
     std::vector<int> odd_result;
     for (int num : result) {
         if (num % 2 != 0) {
             odd_result.push_back(num);
         }
     }
-    sort(odd_result.begin(), odd_result.end());
+    std::sort(odd_result.begin(), odd_result.end());
+    
     return odd_result;
 }
