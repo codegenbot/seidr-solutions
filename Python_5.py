0c4c5666b6345aeeee7ae9deacceba7a27e06fd1
beep boop <githubbot@vadim.me>
2024-07-15 13:46:37 +0200

    Solution 59 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zdp78d1z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a42ddab7-9967-4e8b-b9a8-8077f3f2c 812.py", line 13, in <module>     check(intersperse)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a42ddab7-9967-4e8b-b9a8-8077f3f2c 812.py", line 10, in check     assert intersperse([2, 2, 2], 2) == [2, 2, 2, 2, 2]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_5.py
+++ Python_5.py
@@ -1,8 +1,7 @@
-def intersperse(delimiter: str, *numbers: int) -> list[str]:
+def intersperse(numbers: list[int], delimiter: str) -> list[str]:
     if not numbers:
         return [str(delimiter)]
     result = [str(numbers[0])]
-    for num in numbers[1:]:
-        result.extend([str(delimiter), str(num)])
-    result.append(str(delimiter))
+    for i in range(1, len(numbers)):
+        result.extend([str(delimiter), str(numbers[i])])
     return result
