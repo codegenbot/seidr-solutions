dac0e81bc42ff00005b5f56adbcaaf75f6254e7f
beep boop <githubbot@vadim.me>
2024-07-15 11:18:19 +0200

    Solution 3 of CPP/133, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ovue7rp1 Prompt: 078deee2-d98a-46a3-8e47-61fc7d4d5bdc.cpp: In function ‘int sum_squares(std::vect or<float>)’: 078deee2-d98a-46a3-8e47-61fc7d4d5bdc.cpp:10:21: error: ‘square’ was not declared  in this scope    10 |         sum += (int)square(ceil_num);       |                     ^~~~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -3,6 +3,10 @@
 #include<vector>
 using namespace std;
 
+double square(double n) {
+    return pow(n, 2);
+}
+
 int sum_squares(vector<float> lst){
     int sum = 0;
     for(float num : lst){
@@ -10,8 +14,4 @@ int sum_squares(vector<float> lst){
         sum += (int)square(ceil_num);
     }
     return sum;
-}
-
-double square(double n) {
-    return pow(n, 2);
 }
