a5b0033614bb3fd9a117ec89b8a109b574179610
beep boop <githubbot@vadim.me>
2024-07-15 12:33:28 +0200

    Solution 15 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/1uo2a246 Prompt: Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,25 +1,38 @@
-int main() {
-    string code, guess;
-    cin >> code >> guess;
-
-    int white = 0, black = 0;
-    vector<int> freq(6, 0);
-
+int white_pegs(string s, string t) {
+    int count = 0;
+    map<char, int> freq_s, freq_t;
     for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i])
-            black++;
-        else
-            freq[code[i] - 'A']++;
-    }
-
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] != guess[i] && freq[guess[i] - 'A'] > 0) {
-            white++;
-            freq[guess[i] - 'A']--;
+        if (s[i] == t[i]) {
+            count++;
+        } else {
+            freq_s[s[i]]++;
+            freq_t[t[i]]++;
         }
     }
+    for (auto iter = freq_s.begin(); iter != freq_s.end(); ++iter) {
+        if (freq_t.find(iter->first) != freq_t.end()) {
+            count += min(iter->second, freq_t[iter->first]);
+        }
+    }
+    return count;
+}
 
-    cout << white << endl << black << endl;
+int black_pegs(string s, string t) {
+    int count = 0;
+    for (int i = 0; i < 4; ++i) {
+        if (s[i] == t[i]) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+
+    cout << white_pegs(s, t) << endl;
+    cout << black_pegs(s, t) << endl;
 
     return 0;
 }
