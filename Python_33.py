608a214deaaeed6aada285d01f4c21a16987d518
beep boop <githubbot@vadim.me>
2024-07-15 18:49:43 +0200

    Solution 76 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i1v1lsd5 Prompt: b/python3.11/site-packages/programlib/programs/3d027c1b-fcd7-49c0-8870-e3d8057c2 275.py", line 13, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d027c1b-fcd7-49c0-8870-e3d8057c2 275.py", line 8, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d027c1b-fcd7-49c0-8870-e3d8057c2 275.py", line 2, in sort_third     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d027c1b-fcd7-49c0-8870-e3d8057c2 275.py", line 3, in <listcomp>     sorted(l[i : i + 3], key=lambda x: x[2])   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d027c1b-fcd7-49c0-8870-e3d8057c2 275.py", line 3, in <lambda>     sorted(l[i : i + 3], key=lambda x: x[2])                                        ~^^^ TypeError: 'int' object is not subscriptable

--- Python_33.py
+++ Python_33.py
@@ -1,6 +1,6 @@
 def sort_third(l: list):
     return [
-        sorted(l[i : i + 3])
+        sorted(l[i : i + 3], key=lambda x: x if isinstance(x, int) else x[2])
         for i in range(0, len(l), 3)
         if i + 2 < len(l)
     ]
