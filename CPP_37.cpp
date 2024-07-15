dabd69c1803b09a8efd529735fe80b1eb6183810
beep boop <githubbot@vadim.me>
2024-07-15 17:14:17 +0200

    Solution 82 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/th9qyyvm Prompt: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 07f1b1ec-1563-40fd-805c-6b498c56d11e.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 07f1b1ec-1563-40fd-805c-6b498c56d11e.cpp:32:36: error: expected primary-expressi on before ‘float’    32 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 07f1b1ec-1563-40fd-805c-6b498c56d11e.cpp:32:43: error: expression list treated a s compound expression in initializer [-fpermissive]    32 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 07f1b1ec-1563-40fd-805c-6b498c56d11e.cpp:31: 07f1b1ec-1563-40fd-805c-6b498c56d11e.cpp: In function ‘int main()’: 07f1b1ec-1563-40fd-805c-6b498c56d11e.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -30,6 +30,6 @@ std::vector<float> sort_even(std::vector<float> l) {
 }
 
 int main() {
-    assert(is_same(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    assert(is_same(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10});
     return 0;
 }
