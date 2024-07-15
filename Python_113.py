d95ae2f976988cc57a2f6d443fe5b97230ca5cf0
beep boop <githubbot@vadim.me>
2024-07-15 18:46:59 +0200

    Solution 0 of Python/113, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/n9n72e6g Prompt: Complete the following code given the task description and function signature.

---
+++ Python_113.py
@@ -0,0 +1,2 @@
+def odd_count(lst):
+    return [f"the number of odd elements {sum(int(digit) % 2 != 0 for digit in string)}n the str{idx + 1}ng {idx + 1} of the {idx + 1}nput." for idx, string in enumerate(lst)]
