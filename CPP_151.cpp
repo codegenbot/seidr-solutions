a877893bd89172af07576d6de66e6372586df963
beep boop <githubbot@vadim.me>
2024-07-15 21:44:42 +0200

    Solution 55 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/3vq17j67 Prompt:       |     ^~~ 152519d0-7f4b-4c6a-a642-70b4d00b1310.cpp:7:5: error: expected unqualified-id bef ore ‘return’     7 |     return result;       |     ^~~~~~ 152519d0-7f4b-4c6a-a642-70b4d00b1310.cpp:8:1: error: expected declaration before  ‘}’ token     8 | }       | ^ In file included from 152519d0-7f4b-4c6a-a642-70b4d00b1310.cpp:10: 152519d0-7f4b-4c6a-a642-70b4d00b1310.cpp: In function ‘int main()’: 152519d0-7f4b-4c6a-a642-70b4d00b1310.cpp:12:35: error: ‘lst’ was not declared in  this scope    12 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 152519d0-7f4b-4c6a-a642-70b4d00b1310.cpp:12:13: error: ‘double_the_difference’ w as not declared in this scope    12 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ 152519d0-7f4b-4c6a-a642-70b4d00b1310.cpp:12:43: error: ‘odd_sum’ was not declare d in this scope    12 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -6,14 +6,14 @@ double double_the_difference(double a, double b) {
     return 2 * (a - b);
 }
 
-long long CPP151(std::vector<float> lst) {
+long long main_function(std::vector<float> lst) {
     std::vector<float> lst;
     double double_the_difference;
     long long odd_sum = 0;
 
     for (float num : lst) {
-        if (num > 0 && std::fmod(num, 1) == 0 && ((int)num) % 2 != 0) {
-            odd_sum += (long long)std::pow(num, 2);
+        if (num > 0 && fmod(num, 1) == 0 && ((int)num) % 2 != 0) {
+            odd_sum += (long long)pow(num, 2);
         }
     }
     return odd_sum;
