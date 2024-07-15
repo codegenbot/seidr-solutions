465596a9b699433f38125f5e89b39c9b6d99c61d
beep boop <githubbot@vadim.me>
2024-07-16 00:24:07 +0200

    Solution 51 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/v7a9r6ak Prompt:     3 | cc4c2869-3b97-4404-9c00-a1b7c8e63d9c.cpp:31:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    31 | int main(){       |         ^~ cc4c2869-3b97-4404-9c00-a1b7c8e63d9c.cpp:31:9: note: remove parentheses to defau lt-initialize a variable    31 | int main(){       |         ^~       |         -- cc4c2869-3b97-4404-9c00-a1b7c8e63d9c.cpp:31:9: note: or replace parentheses with  braces to value-initialize a variable cc4c2869-3b97-4404-9c00-a1b7c8e63d9c.cpp:31:11: error: a function-definition is not allowed here before ‘{’ token    31 | int main(){       |           ^ cc4c2869-3b97-4404-9c00-a1b7c8e63d9c.cpp:33:2: error: expected ‘}’ at end of inp ut    33 | }       |  ^ cc4c2869-3b97-4404-9c00-a1b7c8e63d9c.cpp:16:12: note: to match this ‘{’    16 | int main() {       |            ^

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -14,7 +14,7 @@ bool issame(const std::vector<std::vector<int>>& a, const std::vector<std::vecto
 }
 
 int main() {
-    assert(issame(minPath({{1, 3}, {3, 2}}, 10), std::vector<std::vector<int>>(10, std::vector<int>({1, 3})));
+    assert(issame(minPath({{1, 3}, {3, 2}}, 10), std::vector<std::vector<int>>(10, std::vector<int>{1, 3}));
 
     return 0;
 }
