a5e83377e31a4763ba3b1416b4ffe5948a6b4c00
beep boop <githubbot@vadim.me>
2024-07-15 15:44:05 +0200

    Solution 70 of coin-sums, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/i5xtdvzs Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/46df9ae2-fd34-45a0-8da8-5c2603148 d50.py", line 1     ```     ^ SyntaxError: invalid syntax

--- coin-sums.py
+++ coin-sums.py
@@ -1,13 +1,11 @@
-```
 def coin_sums(cents):
     coins = [25, 10, 5, 1]
-    result = ["quarters", "dimes", "nickles", "pennies"]
-    result_value = [0, 0, 0, 0]
+    result = [0, 0, 0, 0]
 
     for i in range(len(coins)):
         while cents >= coins[i]:
             quotient = int(cents / coins[i])
-            result_value[i] += quotient
+            result[i] += quotient
             cents -= coins[i] * quotient
 
-    return dict(zip(result, result_value))
+    return result
