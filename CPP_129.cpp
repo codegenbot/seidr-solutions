035e1376e87f1c2a3a682d34dcc270b385e257ec
beep boop <githubbot@vadim.me>
2024-07-16 00:09:03 +0200

    Solution 32 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/v7a9r6ak Prompt: c5adb806-e073-4972-895e-7788165c49dc.cpp:1:1: error: expected unqualified-id bef ore numeric constant     1 | 1. bool issame(const std::vector<int>& a, const std::vector<int>& b){       | ^~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,4 +1,3 @@
 bool issame(const std::vector<int>& a, const std::vector<int>& b){
-    assert(minPath({{1, 3}, {3, 2}}, 10) == std::vector<std::pair<int, int>>{{1, 0}, {1, 1}});
-    assert(minPath({{1, 3}, {3, 2}}, 10) == std::vector<std::pair<int, int>>{{1, 0},{0, 0}});
-}
+assert(minPath({{1, 3}, {3, 2}}, 10) == std::vector<std::pair<int, int>>{{1, 0}, {1, 1}});
+assert (minPath({{1, 3}, {3, 2}}, 10) == std::vector<std::pair<int, int>>{{1, 0},{0, 0}});
