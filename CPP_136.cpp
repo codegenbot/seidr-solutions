ef9fcd2029f76995af172e8866db755860ea3f0f
beep boop <githubbot@vadim.me>
2024-07-15 15:53:16 +0200

    Solution 15 of CPP/136, ratio 0.7272727272727273 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/uzf6m8sx Prompt: 899e3d8b-0521-4c02-bc2c-7056300e0b1b.cpp:41:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    41 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 899e3d8b-0521-4c02-bc2c-7056300e0b1b.cpp:31:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    31 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 899e3d8b-0521-4c02-bc2c-7056300e0b1b.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 899e3d8b-0521-4c02-bc2c-7056300e0b1b.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -27,8 +27,3 @@ vector<int> largest_smallest_integers(vector<int> lst){
     
     return result;
 }
-
-int main() {
-    assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
-    return 0;
-}
