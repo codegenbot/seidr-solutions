cd6de67a8c1b27e1964cbab568bf32244231e1d6
beep boop <githubbot@vadim.me>
2024-07-15 23:42:43 +0200

    Solution 11 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/0fb5qu7s Prompt:       |                                     ^~~~~~ 811b8f7d-fe5d-4fbf-8ee7-a0ad071a7827.cpp:45:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 811b8f7d-fe5d-4fbf-8ee7-a0ad071a7827.cpp:5: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 811b8f7d-fe5d-4fbf-8ee7-a0ad071a7827.cpp:5: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 811b8f7d-fe5d-4fbf-8ee7-a0ad071a7827.cpp:45:44: error: ‘b’ was not declared in t his scope    45 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 811b8f7d-fe5d-4fbf-8ee7-a0ad071a7827.cpp:53:5: error: redefinition of ‘int main( )’    53 | int main(){       |     ^~~~ 811b8f7d-fe5d-4fbf-8ee7-a0ad071a7827.cpp:39:5: note: ‘int main()’ previously def ined here    39 | int main() {       |     ^~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -35,3 +35,8 @@ std::vector<std::string> split_words(std::string txt) {
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return a == b;
 }
+
+int main() {
+    assert(issame(split_words(""), {"0"}));
+    return 0;
+}
