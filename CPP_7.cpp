3af7b5afe8842621f1206bddf2a18db89a352671
beep boop <githubbot@vadim.me>
2024-07-15 22:58:20 +0200

    Solution 32 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/6nd3o6n4 Prompt:    92 | #  define assert(expr) \       | 9c93aaa1-3cd8-437b-9a2e-d871a9abd9b3.cpp: In function ‘int main()’: 9c93aaa1-3cd8-437b-9a2e-d871a9abd9b3.cpp:19:5: error: ‘assert’ was not declared in this scope    19 |     assert(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"} , "run") == vector<string>{"grunt", "prune"});       |     ^~~~~~ 9c93aaa1-3cd8-437b-9a2e-d871a9abd9b3.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <cassert>   +++ |+#include <cassert>     5 | 9c93aaa1-3cd8-437b-9a2e-d871a9abd9b3.cpp: At global scope: 9c93aaa1-3cd8-437b-9a2e-d871a9abd9b3.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 9c93aaa1-3cd8-437b-9a2e-d871a9abd9b3.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main(){       |     ^~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -16,7 +16,7 @@ vector<string> filter_by_substring(vector<string> strings, string substring){
 }
 
 int main(){
-    filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run");
+    assert(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run") == vector<string>{"grunt", "prune"});
 
     return 0;
 }
