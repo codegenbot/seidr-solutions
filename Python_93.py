87b42431a7762f4a88fd65ad7f84aad3e6004f86
beep boop <githubbot@vadim.me>
2024-07-15 21:09:42 +0200

    Solution 52 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/yxhj599w Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03507294-36e7-423a-b7f8-0c79d3ad7 939.py", line 16, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03507294-36e7-423a-b7f8-0c79d3ad7 939.py", line 12, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,7 +1,8 @@
 def encode(message):
-    vowels = set('aeiouAEIOU')
+    vowels = set('aeiou')
     return ''.join([
         char if not char.isalpha() else chr(((ord(char.upper()) - ord('A') + 2) % 26) + ord('A')).swapcase() if char.lower() not in vowels
         else chr(ord(char) ^ 32)
         for char in message
+        if char.isalnum()
     ])
