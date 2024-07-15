39e4d7e09548655b1c981a59740c5576cab5bff0
beep boop <githubbot@vadim.me>
2024-07-15 23:15:13 +0200

    Solution 97 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/700k7zd0 Prompt: Program did not receive expected input

--- Python_151.py
+++ Python_151.py
@@ -1,7 +1,3 @@
-def double_the_difference(lst):
-    return sum((x * 2) for x in lst if x > 0 and x % 2 != 0) if lst is not None else 0
-
-
 lst = list(map(int, input().strip().split()))
 result = double_the_difference(lst)
 print(result)
