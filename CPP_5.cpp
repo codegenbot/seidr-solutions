b46b9651f773c821e648a732b10429ecfd1f8c70
beep boop <githubbot@vadim.me>
2024-07-15 21:59:52 +0200

    Solution 11 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/9a8ma1bl Prompt:       |             ^~~~~~ 12d530ab-f6f8-47b7-9931-8ce715f8deee.cpp:38:34: error: expected primary-expressi on before ‘int’    38 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 12d530ab-f6f8-47b7-9931-8ce715f8deee.cpp:38:39: error: expression list treated a s compound expression in initializer [-fpermissive]    38 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 12d530ab-f6f8-47b7-9931-8ce715f8deee.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ 12d530ab-f6f8-47b7-9931-8ce715f8deee.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main(){       |     ^~~~ In file included from 12d530ab-f6f8-47b7-9931-8ce715f8deee.cpp:37: 12d530ab-f6f8-47b7-9931-8ce715f8deee.cpp: In function ‘int main()’: 12d530ab-f6f8-47b7-9931-8ce715f8deee.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -24,8 +24,10 @@ std::vector<int> intersperse(std::vector<int> numbers, int delimiter){
     return result;
 }
 
-void main(){
+int main(){
     assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
     assert(issame(intersperse({1, 3, 5, 7}, 0), {1, 0, 3, 0, 5, 0, 7}));
     assert(issame(intersperse({4, 4, 4, 4}, 9), {4, 9, 4, 9, 4, 9, 4}));
+    
+    return 0;
 }
