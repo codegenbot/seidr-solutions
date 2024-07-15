054359e9ccd0c487e070da53ca2e3667003759f2
beep boop <githubbot@vadim.me>
2024-07-15 22:14:03 +0200

    Solution 5 of Python/76, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/xtgjjcxh Prompt: Complete the following code given the task description and function signature.

--- Python_76.py
+++ Python_76.py
@@ -1,2 +1,6 @@
 def is_simple_power(x, n):
-    return x > 0 and x == n ** int(x ** (1 / n))
+    return (
+        x > 0
+        and n > 0
+        and (x == 1 or (n != 1 and x % n == 0 and is_simple_power(x // n, n)))
+    )
