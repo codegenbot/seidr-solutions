1f77974f15236c255648039bc36b22b1ee7bbc22
beep boop <githubbot@vadim.me>
2024-07-15 12:37:20 +0200

    Solution 8 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/a08td4t2 Prompt: n this scope   153 |     cout << "Enter a number: ";       |     ^~~~ 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:154:5: error: ‘cin’ was not declared in  this scope   154 |     cin >> num;       |     ^~~ 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:154:5: note: ‘std::cin’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:155:44: error: ‘endl’ was not declared in this scope   155 |     cout << solution.numberToWords(num) << endl;       |                                            ^~~~ 034a5339-3ec4-41bd-8100-c557ab312f6a.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,17 +1,149 @@
 #include <string>
+#include <iostream>
+
 using namespace std;
 
 class Solution {
 public:
-    string fizzBuzz(int num) {
-        if (num % 3 == 0 && num % 5 == 0)
-            return "FizzBuzz";
-        else if (num % 3 == 0)
-            return "Fizz";
-        else if (num % 5 == 0)
-            return "Buzz";
-        else
-            return to_string(num);
+    string numberToWords(int num) {
+        if (num == 0) return "0";
+        
+        string words = "";
+        
+        int temp = num;
+        if (temp / 100 > 0) {
+            words += threeDigit(temp / 100) + " hundred";
+            temp %= 100;
+        }
+        
+        if (temp / 10 > 0) {
+            if (!words.empty() && temp % 10 == 0) {
+                words += " and zero";
+            } else {
+                words += " and " + twoDigit(temp / 10);
+            }
+            temp %= 10;
+        }
+        
+        if (temp > 0) {
+            if (!words.empty()) {
+                words += " ";
+            }
+            if (temp == 1) {
+                words += "one";
+            } else if (temp == 2) {
+                words += "two";
+            } else if (temp == 3) {
+                words += "three";
+            } else if (temp == 4) {
+                words += "four";
+            } else if (temp == 5) {
+                words += "five";
+            } else if (temp == 6) {
+                words += "six";
+            } else if (temp == 7) {
+                words += "seven";
+            } else if (temp == 8) {
+                words += "eight";
+            } else if (temp == 9) {
+                words += "nine";
+            }
+        }
+        
+        return words;
+    }
+    
+    string threeDigit(int num) {
+        if (num == 0) return "zero";
+        if (num < 10) {
+            return oneDigit(num);
+        }
+        if (num < 20) {
+            if (num == 10) {
+                return "ten";
+            } else if (num == 11) {
+                return "eleven";
+            } else if (num == 12) {
+                return "twelve";
+            } else if (num == 13) {
+                return "thirteen";
+            } else if (num == 14) {
+                return "fourteen";
+            } else if (num == 15) {
+                return "fifteen";
+            } else if (num == 16) {
+                return "sixteen";
+            } else if (num == 17) {
+                return "seventeen";
+            } else if (num == 18) {
+                return "eighteen";
+            } else if (num == 19) {
+                return "nineteen";
+            }
+        }
+        if (num < 30) {
+            return "twenty" + oneDigit(num - 20);
+        }
+        if (num < 40) {
+            return "thirty" + oneDigit(num - 30);
+        }
+        if (num < 50) {
+            return "forty" + oneDigit(num - 40);
+        }
+        if (num < 60) {
+            return "fifty" + oneDigit(num - 50);
+        }
+        if (num < 70) {
+            return "sixty" + oneDigit(num - 60);
+        }
+        if (num < 80) {
+            return "seventy" + oneDigit(num - 70);
+        }
+        if (num < 90) {
+            return "eighty" + oneDigit(num - 80);
+        }
+        return "ninety" + oneDigit(num - 90);
+    }
+    
+    string twoDigit(int num) {
+        switch (num) {
+            case 0: return "zero";
+            case 1: return "one";
+            case 2: return "two";
+            case 3: return "three";
+            case 4: return "four";
+            case 5: return "five";
+            case 6: return "six";
+            case 7: return "seven";
+            case 8: return "eight";
+            case 9: return "nine";
+            case 10: return "ten";
+            case 11: return "eleven";
+            case 12: return "twelve";
+            case 13: return "thirteen";
+            case 14: return "fourteen";
+            case 15: return "fifteen";
+            case 16: return "sixteen";
+            case 17: return "seventeen";
+            case 18: return "eighteen";
+            case 19: return "nineteen";
+            default: return tenDigit(num);
+        }
+    }
+    
+    string oneDigit(int num) {
+        switch (num) {
+            case 0: return "zero";
+            case 1: return "one";
+            case 2: return "two";
+            case 3: return "three";
+            case 4: return "four";
+            case 5: return "five";
+            case 6: return "six";
+            case 7: return "seven";
+            case 8: return "eight";
+            case 9: return "nine";
+        }
     }
 };
 
@@ -20,6 +152,6 @@ int main() {
     int num;
     cout << "Enter a number: ";
     cin >> num;
-    cout << solution.fizzBuzz(num) << endl;
+    cout << solution.numberToWords(num) << endl;
     return 0;
 }
