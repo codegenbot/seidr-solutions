47b3aa0fa99136c8a73cd11103ac242732c3029d
beep boop <githubbot@vadim.me>
2024-07-15 20:13:41 +0200

    Solution 4 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/qbn5dc76 Prompt: 46856ee3-5fd0-42c4-81a9-6e07fa3c822a.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,22 +1,45 @@
-int bowlingScore(string s) {
+#include <string>
+#include <vector>
+
+int bowlingScore(std::string s) {
     int score = 0;
-    for (int i = 0; i < s.length(); i++) {
+    std::vector<int> rolls(21);
+    for (int i = 0; i < s.size(); i++) {
         if (s[i] == 'X') {
-            score += 30;
-            i++;
-        } else if (s[i] == '/') {
-            score += 10 - (stoi(s.substr(i + 1, 2)) * 10);
-            i += 3;
-        } else {
-            int frameScore = stoi(s.substr(i, 1));
-            if (frameScore < 10) {
-                score += frameScore;
-                i++;
-            } else {
+            rolls[i / 2] = 10;
+            if (i % 2 != 0) {
                 score += 10;
+            }
+        } else if (s[i] == '/') {
+            int next = i + 1;
+            while (next < s.size() && !std::isdigit(s[next])) {
+                next++;
+            }
+            int roll = 10 - std::stoi(std::string(1, s[i + 1]) + std::string(1, s[next]));
+            rolls[i / 2] = roll;
+            score += roll;
+        } else {
+            int roll = 0;
+            while (i < s.size() && std::isdigit(s[i])) {
+                roll = roll * 10 + std::stoi(std::string(1, s[i]));
                 i++;
             }
+            if (i > 0) {
+                score += roll;
+            }
+            rolls[i / 2] = roll;
         }
     }
+
+    for (int i = 0; i < 10; i++) {
+        if (rolls[i] == 10) {
+            if (i + 1 < 10 && rolls[i + 1] != 0) {
+                score += rolls[i + 1];
+            }
+        } else if (rolls[i] > 0) {
+            score += std::min(rolls[i], 10);
+        }
+    }
+
     return score;
 }
