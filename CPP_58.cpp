aa3873749afb70ee01e09e75cf270c6ab976c8e0
beep boop <githubbot@vadim.me>
2024-07-15 22:44:21 +0200

    Solution 6 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/jl6zpipe Prompt:   731 |         __copy_move_b(const _Tp* __first, const _Tp* __last, _Tp* __resu lt)       |         ^~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_algobase.h:731:9: note:   template argument deducti on/substitution failed: /usr/include/c++/11/bits/stl_algobase.h:760:65: note:   deduced conflicting type s for parameter ‘_Tp’ (‘int’ and ‘const int’)   758 |     return std::__copy_move_backward<_IsMove,       |            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   759 |                                      __memcpyable<_BI2, _BI1>::__value,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   760 |                                      _Category>::__copy_move_b(__first,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~   761 |                                                                __last,       |                                                                ~~~~~~~   762 |                                                                __result) ;       |                                                                ~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -12,13 +12,13 @@ std::vector<int> common(const std::vector<int>& l1, const std::vector<int>& l2) 
     std::vector<int> result;
     std::sort(l1.begin(), l1.end());
     std::sort(l2.begin(), l2.end());
-    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), std::back_inserter(result));
+    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), std::back_inserter(std::back_inserter(result)));
     result.erase(std::unique(result.begin(), result.end()), result.end());
     return result;
 }
 
 int main() {
-    assert(issame(common({4, 3, 2, 8}, {}), std::vector<int>{}));
+    assert(issame(common({4, 3, 2, 8}, {}), {}));
     
     return 0;
 }
