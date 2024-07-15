0aa4925549e97c8e477b38dc85d6f7269e25ee6e
beep boop <githubbot@vadim.me>
2024-07-15 12:38:03 +0200

    Solution 23 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/6oq0eug2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/02dc7474-101c-4988-9508-89e674061 3d9.py", line 1, in <module>     numbers = [int(input()) for _ in range(int(input()))]               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/02dc7474-101c-4988-9508-89e674061 3d9.py", line 1, in <listcomp>     numbers = [int(input()) for _ in range(int(input()))]                ^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: '1609 9225 7552 6618 7650'

--- fuel-cost.py
+++ fuel-cost.py
@@ -1,3 +1,3 @@
-numbers = [int(input()) for _ in range(int(input()))]
+numbers = [int(input()) for _ in range(int(input("Enter number of elements: ")))]
 result = sum((num // 3 - 2) for num in numbers)
 print(result)
