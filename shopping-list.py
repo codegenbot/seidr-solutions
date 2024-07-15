d09eb511c6c95cddaaf38b9f9ad7c14991d2426a
beep boop <githubbot@vadim.me>
2024-07-15 20:50:09 +0200

    Solution 24 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/n6wviqs9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5d2fffc9-7dae-41e4-a588-6f430a6c3 97e.py", line 11, in <module>     print(shopping_list())           ^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5d2fffc9-7dae-41e4-a588-6f430a6c3 97e.py", line 7, in shopping_list     total_price += (1 - discounts[i] / 100) * prices[i]                         ~~~~~~~~~^^^ IndexError: list index out of range

--- shopping-list.py
+++ shopping-list.py
@@ -1,10 +1,11 @@
-def shopping_list():
-    n = int(input())
-    total_price = 0
-    for i in range(n):
-        price, discount = map(float, input().split())
-        total_price += (1 - discount / 100) * price
-    return format(total_price, ".1f")
+```
+n = int(input())
+items = int(input())
 
+prices = [float(x) for x in input().split()]
+discounts = [int(x) for x in input().split()]
 
-print(shopping_list())
+total_price = 0
+for i in range(items):
+    total_price += (1 - discounts[i] / 100) * prices[i]
+print(format(total_price, ".1f"))
